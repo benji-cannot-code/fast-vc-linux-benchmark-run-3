e15ed9120b9367695d262db4914a6cccbd32e9fc
Chris Mason <chris.mason@oracle.com>
2007-02-26 10:40:21 -0500

    Btrfs: Add fsx-style randomized tree tester Add debug-tree command to print the tree Add extent-tree.c to the repo Comment ctree.h
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

---
+++ fs/btrfs/debug-tree.c
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <stdio.h>
+#include <stdlib.h>
+#include "kerncompat.h"
+#include "radix-tree.h"
+#include "ctree.h"
+#include "disk-io.h"
+#include "print-tree.h"
+
+int main() {
+	struct ctree_super_block super;
+	struct ctree_root *root;
+	radix_tree_init();
+	root = open_ctree("dbfile", &super);
+	printf("root tree\n");
+	print_tree(root, root->node);
+	printf("map tree\n");
+	print_tree(root->extent_root, root->extent_root->node);
+	return 0;
+}
