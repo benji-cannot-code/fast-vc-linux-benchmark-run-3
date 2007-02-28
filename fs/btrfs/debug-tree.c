5668cb82ec38b227458b39db0b715232205045ac
Chris Mason <chris.mason@oracle.com>
2007-02-28 09:40:58 -0500

    Btrfs: Add sparse checking to Makefile
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/debug-tree.c
+++ fs/btrfs/debug-tree.c
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "disk-io.h"
 #include "print-tree.h"
 
-int main() {
+int main(int ac, char **av) {
 	struct ctree_super_block super;
 	struct ctree_root *root;
 	radix_tree_init();
