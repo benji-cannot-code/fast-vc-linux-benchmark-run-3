c605c45f6028934f03086c0c1bf41d73dc9661bd
Chris Mason <chris.mason@oracle.com>
2007-03-15 12:57:43 -0400

    Btrfs: add hash.h
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

---
+++ fs/btrfs/hash.h
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __HASH__
+#define __HASH__
+int btrfs_name_hash(const char *name, int len, u64 *hash_result);
+#endif
