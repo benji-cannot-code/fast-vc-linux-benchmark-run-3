8ba176cb0d548e39a29298fa3ce660d772cdb165
Chris Mason <chris.mason@oracle.com>
2007-03-15 19:03:33 -0400

    Btrfs: add inode item
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

---
+++ fs/btrfs/file-item.c
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <stdio.h>
+#include <stdlib.h>
+#include "kerncompat.h"
+#include "radix-tree.h"
+#include "ctree.h"
+#include "disk-io.h"
+
