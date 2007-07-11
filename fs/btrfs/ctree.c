f034a76adb1bcb607074bf527b9c1ce54304c15b
Aneesh <aneesh.kumar@gmail.com>
2007-07-11 10:03:27 -0400

    Btrfs: Some code cleanups
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -173,11 +173,13 @@ static int check_node(struct btrfs_root *root, struct btrfs_path *path,
 
 	if (path->nodes[level + 1])
 		parent = btrfs_buffer_node(path->nodes[level + 1]);
-	parent_slot = path->slots[level + 1];
+
 	slot = path->slots[level];
 	BUG_ON(nritems == 0);
 	if (parent) {
 		struct btrfs_disk_key *parent_key;
+
+		parent_slot = path->slots[level + 1];
 		parent_key = &parent->ptrs[parent_slot].key;
 		BUG_ON(memcmp(parent_key, &node->ptrs[0].key,
 			      sizeof(struct btrfs_disk_key)));
@@ -209,7 +211,7 @@ static int check_leaf(struct btrfs_root *root, struct btrfs_path *path,
 
 	if (path->nodes[level + 1])
 		parent = btrfs_buffer_node(path->nodes[level + 1]);
-	parent_slot = path->slots[level + 1];
+
 	BUG_ON(btrfs_leaf_free_space(root, leaf) < 0);
 
 	if (nritems == 0)
@@ -217,6 +219,8 @@ static int check_leaf(struct btrfs_root *root, struct btrfs_path *path,
 
 	if (parent) {
 		struct btrfs_disk_key *parent_key;
+
+		parent_slot = path->slots[level + 1];
 		parent_key = &parent->ptrs[parent_slot].key;
 		BUG_ON(memcmp(parent_key, &leaf->items[0].key,
 		       sizeof(struct btrfs_disk_key)));
