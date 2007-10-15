36bf20d6c218874e326af41f24ce9102eb9b12c5
Chris Mason <chris.mason@oracle.com>
2007-10-15 16:22:39 -0400

    Btrfs: Defrag only leaves, and only when the parent node has a single objectid
    This allows us to defrag huge directories, but skip the expensive defrag
    case in more common usage, where it does not help as much.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -354,7 +354,6 @@ int btrfs_defrag_root(struct btrfs_root *root, int cacheonly)
 
 	if (root->defrag_running)
 		return 0;
-
 	trans = btrfs_start_transaction(root, 1);
 	while (1) {
 		root->defrag_running = 1;
@@ -362,7 +361,6 @@ int btrfs_defrag_root(struct btrfs_root *root, int cacheonly)
 		nr = trans->blocks_used;
 		btrfs_end_transaction(trans, root);
 		mutex_unlock(&info->fs_mutex);
-
 		btrfs_btree_balance_dirty(info->tree_root, nr);
 		cond_resched();
 
