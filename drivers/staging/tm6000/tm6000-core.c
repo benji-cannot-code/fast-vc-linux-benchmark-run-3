805e57be74edcb24ba7b83e9a72954eb28653fb0
Mauro Carvalho Chehab <mchehab@redhat.com>
2007-11-02 11:16:35 -0300

    V4L/DVB (12809): tm6000: Remove an old hack
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/tm6000/tm6000-core.c
+++ drivers/staging/tm6000/tm6000-core.c
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-common.h>
 #include <media/tuner.h>
 
-#ifdef HACK /* HACK */
-#include "tm6000-hack.c"
-#endif
-
 #define USB_TIMEOUT	5*HZ /* ms */
 
 int tm6000_read_write_usb (struct tm6000_core *dev, u8 req_type, u8 req,
@@ -314,11 +310,6 @@ int tm6000_init (struct tm6000_core *dev)
 {
 	int board, rc=0, i;
 
-#ifdef HACK /* HACK */
-	init_tm6000(dev);
-	return 0;
-#else
-
 	/* Load board's initialization table */
 	for (i=0; i< ARRAY_SIZE(init_tab); i++) {
 		rc= tm6000_set_reg (dev, REQ_07_SET_GET_AVREG,
@@ -375,8 +366,6 @@ int tm6000_init (struct tm6000_core *dev)
 	}
 
 	msleep(50);
-
-#endif /* HACK */
 
 	return 0;
 }
