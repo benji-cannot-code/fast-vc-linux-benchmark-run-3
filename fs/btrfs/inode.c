e4425e33f5cc4c6eb368075f42076063a6b8bc1a
Chris Mason <chris.mason@oracle.com>
2008-01-03 14:51:00 -0500

    Btrfs: Change st_blocksize to 4k
    Some programs (python) do rwm cycles at the granularity returned by
    stat.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -2702,7 +2702,7 @@ static int btrfs_getattr(struct vfsmount *mnt,
 {
 	struct inode *inode = dentry->d_inode;
 	generic_fillattr(inode, stat);
-	stat->blksize = 256 * 1024;
+	stat->blksize = PAGE_CACHE_SIZE;
 	return 0;
 }
 
