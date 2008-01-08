3f3b185a42951701c61790a8e02a8756372c6f7f
Mauro Carvalho Chehab <mchehab@redhat.com>
2008-01-08 11:25:57 -0300

    V4L/DVB (12823): tm6000: Uses another method for handling incomplete packets
    This requires a little more memory, and some memcpy to work, but the logic is
    simpler than the previous method.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/tm6000/tm6000-regs.h
+++ drivers/staging/tm6000/tm6000-regs.h
@@ -78,7 +78,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Define TV Master TM5600/TM6000 URB message codes and length
  */
 
-#define TM6000_URB_MSG_LEN 180
 enum {
 	TM6000_URB_MSG_VIDEO=1,
 	TM6000_URB_MSG_AUDIO,
