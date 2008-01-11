1a626915cfe3242af41f1d06868f5415b771fd99
Mauro Carvalho Chehab <mchehab@redhat.com>
2008-01-11 13:19:45 -0300

    V4L/DVB (12832): tm6000: allow compilation of tm6000-alsa module
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/tm6000/tm6000-core.c
+++ drivers/staging/tm6000/tm6000-core.c
@@ -481,7 +481,7 @@ int tm6000_init (struct tm6000_core *dev)
 	return 0;
 }
 
-int tm6000_set_audio_bitrate (struct tm6000_core *dev, int bitrate)
+int tm6000_set_audio_bitrate(struct tm6000_core *dev, int bitrate)
 {
 	int val;
 
@@ -505,3 +505,4 @@ printk("Original value=%d\n",val);
 
 	return val;
 }
+EXPORT_SYMBOL_GPL(tm6000_set_audio_bitrate);
