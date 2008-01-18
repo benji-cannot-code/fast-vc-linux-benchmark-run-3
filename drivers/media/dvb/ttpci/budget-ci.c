c833eba3a2124a3741bae694efb9d68ea056c515
Manu Abraham <abraham.manu@gmail.com>
2008-01-18 14:15:17 -0300

    V4L/DVB (9444): Initialize post process events to NULL
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/ttpci/budget-ci.c
+++ drivers/media/dvb/ttpci/budget-ci.c
@@ -1656,6 +1656,8 @@ static struct stb0899_config tt3200_config = {
 	.init_s2_fec		= tt3200_stb0899_s2_init_4,
 	.init_tst		= tt3200_stb0899_s1_init_5,
 
+	.postproc		= NULL,
+
 	.demod_address 		= 0x68,
 
 	.xtal_freq		= 27000000,
@@ -1680,7 +1682,7 @@ static struct stb0899_config tt3200_config = {
 	.tuner_set_frequency	= stb6100_set_frequency,
 	.tuner_set_bandwidth	= stb6100_set_bandwidth,
 	.tuner_get_bandwidth	= stb6100_get_bandwidth,
-	.tuner_set_rfsiggain	= NULL,
+	.tuner_set_rfsiggain	= NULL
 };
 
 struct stb6100_config tt3200_stb6100_config = {
