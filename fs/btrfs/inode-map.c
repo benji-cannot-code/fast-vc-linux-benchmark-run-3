4358a93740461f993b2ee353fc9af047f799edf3
Yan <yanzheng@21cn.com>
2008-01-22 12:46:56 -0500

    Fix for btrfs_find_free_objectid
    btrfs_find_free_objectid may return a used objectid due to arithmetic
    underflow. This bug may happen when parameter 'root' is tree root,  so
    it may cause serious problems when creating snapshot or sub-volume.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/inode-map.c
+++ fs/btrfs/inode-map.c
@@ -63,7 +63,6 @@ int btrfs_find_free_objectid(struct btrfs_trans_handle *trans,
 	struct btrfs_path *path;
 	struct btrfs_key key;
 	int ret;
-	u64 hole_size = 0;
 	int slot = 0;
 	u64 last_ino = 0;
 	int start_found;
@@ -110,8 +109,7 @@ int btrfs_find_free_objectid(struct btrfs_trans_handle *trans,
 			if (start_found) {
 				if (last_ino < search_start)
 					last_ino = search_start;
-				hole_size = key.objectid - last_ino;
-				if (hole_size > 0) {
+				if (key.objectid > last_ino) {
 					*objectid = last_ino;
 					goto found;
 				}
