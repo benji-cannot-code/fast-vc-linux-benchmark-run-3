7eccd1ac2453d091b7ecd865c3f9ced467c6ed04
Chris Mason <chris.mason@oracle.com>
2008-02-07 10:50:54 -0500

    Btrfs: Create larger bios for btree blocks
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -2860,8 +2860,8 @@ EXPORT_SYMBOL(extent_buffer_uptodate);
 
 int read_extent_buffer_pages(struct extent_io_tree *tree,
 			     struct extent_buffer *eb,
-			     u64 start,
-			     int wait)
+			     u64 start, int wait,
+			     get_extent_t *get_extent)
 {
 	unsigned long i;
 	unsigned long start_i;
@@ -2869,6 +2869,8 @@ int read_extent_buffer_pages(struct extent_io_tree *tree,
 	int err;
 	int ret = 0;
 	unsigned long num_pages;
+	struct bio *bio = NULL;
+
 
 	if (eb->flags & EXTENT_UPTODATE)
 		return 0;
@@ -2900,7 +2902,8 @@ int read_extent_buffer_pages(struct extent_io_tree *tree,
 			lock_page(page);
 		}
 		if (!PageUptodate(page)) {
-			err = page->mapping->a_ops->readpage(NULL, page);
+			err = __extent_read_full_page(tree, page,
+						      get_extent, &bio);
 			if (err) {
 				ret = err;
 			}
@@ -2908,6 +2911,9 @@ int read_extent_buffer_pages(struct extent_io_tree *tree,
 			unlock_page(page);
 		}
 	}
+
+	if (bio)
+		submit_one_bio(READ, bio);
 
 	if (ret || !wait) {
 		return ret;
