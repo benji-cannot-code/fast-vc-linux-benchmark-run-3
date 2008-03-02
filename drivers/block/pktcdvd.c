2de1dc53fdb1cf1aaba1146c79c2ced06911cc6b
Al Viro <viro@zeniv.linux.org.uk>
2008-03-02 09:09:22 -0500

    [PATCH] beginning of methods conversion
    To keep the size of changesets sane we split the switch by drivers;
    to keep the damn thing bisectable we do the following:
    	1) rename the affected methods, add ones with correct
    prototypes, make (few) callers handle both.  That's this changeset.
    	2) for each driver convert to new methods.  *ALL* drivers
    are converted in this series.
    	3) kill the old (renamed) methods.
    
    Note that it _is_ a flagday; all in-tree drivers are converted and by the
    end of this series no trace of old methods remain.  The only reason why
    we do that this way is to keep the damn thing bisectable and allow per-driver
    debugging if anything goes wrong.
    
    New methods:
    	open(bdev, mode)
    	release(disk, mode)
    	ioctl(bdev, mode, cmd, arg)		/* Called without BKL */
    	compat_ioctl(bdev, mode, cmd, arg)
    	locked_ioctl(bdev, mode, cmd, arg)	/* Called with BKL, legacy */
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/block/pktcdvd.c
+++ drivers/block/pktcdvd.c
@@ -2848,9 +2848,9 @@ static int pkt_media_changed(struct gendisk *disk)
 
 static struct block_device_operations pktcdvd_ops = {
 	.owner =		THIS_MODULE,
-	.open =			pkt_open,
-	.release =		pkt_close,
-	.ioctl =		pkt_ioctl,
+	.__open =			pkt_open,
+	.__release =		pkt_close,
+	.__ioctl =		pkt_ioctl,
 	.media_changed =	pkt_media_changed,
 };
 
