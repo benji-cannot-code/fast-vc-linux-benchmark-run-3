dcf962bce76ffe21a4ea7f445b35e3ea8120ec3b
Roland McGrath <roland@redhat.com>
2008-03-14 17:46:38 -0700

    x86: tracehook_signal_handler
    This makes the x86 signal handling code use tracehook_signal_handler() in
    place of calling into ptrace guts.  The call is moved after the sa_mask
    processing, but there is no other change.  This cleanup doesn't matter to
    existing debuggers, but is the sensible thing: have all facets of the
    handler setup complete before the debugger inspects the task again.
    
    Signed-off-by: Roland McGrath <roland@redhat.com>

--- arch/x86/kernel/signal_32.c
+++ arch/x86/kernel/signal_32.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/sched.h>
 #include <linux/wait.h>
+#include <linux/tracehook.h>
 #include <linux/elf.h>
 #include <linux/smp.h>
 #include <linux/mm.h>
@@ -559,8 +560,6 @@ handle_signal(unsigned long sig, siginfo_t *info, struct k_sigaction *ka,
 	 * handler too.
 	 */
 	regs->flags &= ~X86_EFLAGS_TF;
-	if (test_thread_flag(TIF_SINGLESTEP))
-		ptrace_notify(SIGTRAP);
 
 	spin_lock_irq(&current->sighand->siglock);
 	sigorsets(&current->blocked, &current->blocked, &ka->sa.sa_mask);
@@ -568,6 +567,9 @@ handle_signal(unsigned long sig, siginfo_t *info, struct k_sigaction *ka,
 		sigaddset(&current->blocked, sig);
 	recalc_sigpending();
 	spin_unlock_irq(&current->sighand->siglock);
+
+	tracehook_signal_handler(sig, info, ka, regs,
+				 test_thread_flag(TIF_SINGLESTEP));
 
 	return 0;
 }
