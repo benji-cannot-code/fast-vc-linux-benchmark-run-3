104de66fe4fc5634ffe1cf96be85838f8ff846ce
Chris Mason <chris.mason@oracle.com>
2008-03-26 10:28:07 -0400

    Create a btrfs backing dev info
    This allows intelligent versions of unplug and congestion functions
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/workqueue.h>
 #include <linux/completion.h>
+#include <linux/backing-dev.h>
 #include <asm/kmap_types.h>
 #include "bit-radix.h"
 #include "extent_io.h"
@@ -473,6 +474,7 @@ struct btrfs_fs_info {
 	struct block_device *__bdev;
 	struct super_block *sb;
 	struct inode *btree_inode;
+	struct backing_dev_info bdi;
 	spinlock_t hash_lock;
 	struct mutex trans_mutex;
 	struct mutex fs_mutex;
