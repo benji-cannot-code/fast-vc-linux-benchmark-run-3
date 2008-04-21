84aee504be216500f947787d90d13b84698c50fb
Adrian Bunk <bunk@kernel.org>
2008-04-22 00:31:51 +0300

    [SCSI] mpt fusion: make struct mpt_proc_root_dir static
    This patch makes the needlessly global struct mpt_proc_root_dir static.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Acked-by: "Prakash, Sathya" <Sathya.Prakash@lsi.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/message/fusion/mptbase.c
+++ drivers/message/fusion/mptbase.c
@@ -104,7 +104,7 @@ static int mfcounter = 0;
  *  Public data...
  */
 
-struct proc_dir_entry *mpt_proc_root_dir;
+static struct proc_dir_entry *mpt_proc_root_dir;
 
 #define WHOINIT_UNKNOWN		0xAA
 
@@ -7452,7 +7452,6 @@ EXPORT_SYMBOL(mpt_resume);
 EXPORT_SYMBOL(mpt_suspend);
 #endif
 EXPORT_SYMBOL(ioc_list);
-EXPORT_SYMBOL(mpt_proc_root_dir);
 EXPORT_SYMBOL(mpt_register);
 EXPORT_SYMBOL(mpt_deregister);
 EXPORT_SYMBOL(mpt_event_register);
