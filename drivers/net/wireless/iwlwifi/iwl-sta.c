569699e7b26a4c859798dc66668027fec03d8ae2
Ron Rindjunsky <ron.rindjunsky@intel.com>
2008-04-23 17:15:06 -0700

    iwlwifi: iwl-sta redundant includes clean up
    This patch cleans unnecessary includes in iwl-sta.[hc]
    
    Signed-off-by: Ron Rindjunsky <ron.rindjunsky@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-sta.c
+++ drivers/net/wireless/iwlwifi/iwl-sta.c
@@ -37,8 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-sta.h"
 #include "iwl-io.h"
 #include "iwl-helpers.h"
-#include "iwl-4965.h"
-#include "iwl-sta.h"
 
 u8 iwl_find_station(struct iwl_priv *priv, const u8 *addr)
 {
--- drivers/net/wireless/iwlwifi/iwl-sta.h
+++ drivers/net/wireless/iwlwifi/iwl-sta.h
@@ -30,14 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_sta_h__
 #define __iwl_sta_h__
 
-#include <net/mac80211.h>
-
-#include "iwl-eeprom.h"
-#include "iwl-core.h"
-#include "iwl-4965.h"
-#include "iwl-io.h"
-#include "iwl-helpers.h"
-
 int iwl_get_free_ucode_key_index(struct iwl_priv *priv);
 int iwl_send_static_wepkey_cmd(struct iwl_priv *priv, u8 send_if_empty);
 int iwl_remove_default_wep_key(struct iwl_priv *priv,
