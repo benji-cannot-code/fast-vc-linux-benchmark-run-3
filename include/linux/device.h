f15a17806e0c449c8f157c4bd325ae479e44bcf2
Greg Kroah-Hartman <gregkh@suse.de>
2008-05-02 06:02:41 +0200

    driver core: remove DEVICE_ID_SIZE define
    There is no such thing as a "device id size" in the driver core, so
    remove the define and fix up any users of this odd define in the rest of
    the kernel.
    
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/device.h
+++ include/linux/device.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/atomic.h>
 #include <asm/device.h>
 
-#define DEVICE_ID_SIZE		32
 #define BUS_ID_SIZE		20
 
 struct device;
