cc85ae97510cf3464ccc2d6e0a62b29c67dd27cf
Allan Stephens <allan.stephens@windriver.com>
2008-05-05 01:21:54 -0700

    tipc: Fix null pointer dereference in debug code
    This patch eliminates an obsolete use of the DBG_OUTPUT print
    buffer which could lead to a null pointer crash in tipc_printf()
    if TIPC's debugging capabilities are configured.
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/name_distr.c
+++ net/tipc/name_distr.c
@@ -42,9 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "msg.h"
 #include "name_distr.h"
 
-#undef  DBG_OUTPUT
-#define DBG_OUTPUT NULL
-
 #define ITEM_SIZE sizeof(struct distr_item)
 
 /**
