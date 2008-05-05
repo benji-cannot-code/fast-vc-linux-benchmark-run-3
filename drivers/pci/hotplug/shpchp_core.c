4b12476f52c1f8f764c0e1f09391c4e8b426c959
Adrian Bunk <bunk@kernel.org>
2008-05-05 21:21:51 +0300

    PCI: make {pciehp,shpchp}_slot_with_bus static
    This patch makes the needlessly global {pciehp,shpchp}_slot_with_bus
    static.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/hotplug/shpchp_core.c
+++ drivers/pci/hotplug/shpchp_core.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int shpchp_debug;
 int shpchp_poll_mode;
 int shpchp_poll_time;
-int shpchp_slot_with_bus;
+static int shpchp_slot_with_bus;
 struct workqueue_struct *shpchp_wq;
 
 #define DRIVER_VERSION	"0.4"
