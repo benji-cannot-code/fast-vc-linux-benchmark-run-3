98cc71b07659186737c263f9ce650c6298f71d41
Ester Kummer <ester.kummer@intel.com>
2008-05-06 11:05:14 +0800

    iwlwifi: adding parameter of fw_restart
    This patch adds a module parameter of fw_restart which determine if
    the uCode will be restarted or not in case of error.
    
    Signed-off-by: Ester Kummer <ester.kummer@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl4965-base.c
+++ drivers/net/wireless/iwlwifi/iwl4965-base.c
@@ -3570,7 +3570,8 @@ static void iwl4965_irq_handle_error(struct iwl_priv *priv)
 			       sizeof(priv->recovery_rxon));
 			priv->error_recovering = 1;
 		}
-		queue_work(priv->workqueue, &priv->restart);
+		if (priv->cfg->mod_params->restart_fw)
+			queue_work(priv->workqueue, &priv->restart);
 	}
 }
 
@@ -6847,6 +6848,7 @@ static int __init iwl4965_init(void)
 	}
 
 	return ret;
+
 
 #ifdef CONFIG_IWLWIFI_DEBUG
 	pci_unregister_driver(&iwl_driver);
