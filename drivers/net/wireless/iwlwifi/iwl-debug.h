5d7cb540524efd861e00d4a467d03b33b4a6444d
Ester Kummer <ester.kummer@intel.com>
2008-05-06 11:05:11 +0800

    iwlwifi: move debug_level to sysfs/bus/pci/devices
    This patch ports the debug_level from sysfs/bus/pci/drivers/iwl4965
    to /sys/class/net/wlanX/device/debug_level
    
    Signed-off-by: Ester Kummer <ester.kummer@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-debug.h
+++ drivers/net/wireless/iwlwifi/iwl-debug.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __iwl_debug_h__
 
 #ifdef CONFIG_IWLWIFI_DEBUG
-extern u32 iwl_debug_level;
 #define IWL_DEBUG(level, fmt, args...) \
 do { if (priv->debug_level & (level)) \
   dev_printk(KERN_ERR, &(priv->hw->wiphy->dev), "%c %s " fmt, \
