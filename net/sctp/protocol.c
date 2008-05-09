826ee5fc5624af5524551e1f8fa66cd56d762487
Neil Horman <nhorman@tuxdriver.com>
2008-05-09 15:14:50 -0700

    sctp: add sctp/remaddr table to complete RFC remote address table OID
    Add support for RFC3873 remote address table OID.
    
          +--(5) sctpAssocRemAddrTable
          |   |
          |   |--(-) sctpAssocId (shared index)
          |   |
          |   +--(1) sctpAssocRemAddrType (index)
          .   |
          .   +--(2) sctpAssocRemAddr (index)
          .   |
              +--(3) sctpAssocRemAddrActive
              |
              +--(4) sctpAssocRemAddrHBActive
              |
              +--(5) sctpAssocRemAddrRTO
              |
              +--(6) sctpAssocRemAddrMaxPathRtx
              |
              +--(7) sctpAssocRemAddrRtx
              |
              +--(8) sctpAssocRemAddrStartTime
    
    This patch places all the requsite data in /proc/net/sctp/remaddr.
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: Vlad Yasevich <vladislav.yasevich@hp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/protocol.c
+++ net/sctp/protocol.c
@@ -114,6 +114,8 @@ static __init int sctp_proc_init(void)
 		goto out_nomem;
 	if (sctp_assocs_proc_init())
 		goto out_nomem;
+	if (sctp_remaddr_proc_init())
+		goto out_nomem;
 
 	return 0;
 
@@ -130,6 +132,7 @@ static void sctp_proc_exit(void)
 	sctp_snmp_proc_exit();
 	sctp_eps_proc_exit();
 	sctp_assocs_proc_exit();
+	sctp_remaddr_proc_exit();
 
 	if (proc_net_sctp) {
 		proc_net_sctp = NULL;
