8708c882e21148e1c203e5a25a439a034ea35390
Ankita Garg <ankita@in.ibm.com>
2008-05-12 21:20:58 +0200

    ftrace: fix conversion of task state to char in latency tracer
    The conversion of task states to a character in the sched_switch tracer (part
    of latency tracer infrastructure), seems to be incorrect. We currently do it
    by indexing into the state_to_char array using the state value. The state
    values do not map directly into the array index and are thus incorrect. The
    following patch addresses this issue. This is also what is being done even
    in the show_task() routine in kernel/sched.c
    
    The patch has been compile and run tested.
    
    Signed-off-by: Ankita Garg <ankita@in.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -1209,6 +1209,7 @@ print_lat_fmt(struct trace_iterator *iter, unsigned int trace_idx, int cpu)
 	char *comm;
 	int S, T;
 	int i;
+	unsigned state;
 
 	if (!next_entry)
 		next_entry = entry;
@@ -1239,11 +1240,11 @@ print_lat_fmt(struct trace_iterator *iter, unsigned int trace_idx, int cpu)
 		break;
 	case TRACE_CTX:
 	case TRACE_WAKE:
-		S = entry->ctx.prev_state < sizeof(state_to_char) ?
-			state_to_char[entry->ctx.prev_state] : 'X';
 		T = entry->ctx.next_state < sizeof(state_to_char) ?
 			state_to_char[entry->ctx.next_state] : 'X';
 
+		state = entry->ctx.prev_state ? __ffs(entry->ctx.prev_state) + 1 : 0;
+		S = state < sizeof(state_to_char) - 1 ? state_to_char[state] : 'X';
 		comm = trace_find_cmdline(entry->ctx.next_pid);
 		trace_seq_printf(s, " %5d:%3d:%c %s %5d:%3d:%c %s\n",
 				 entry->ctx.prev_pid,
