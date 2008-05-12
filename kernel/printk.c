f8479aee20a52e0a8420c00368a83b1111dc47c1
Jiri Slaby <jirislaby@gmail.com>
2008-05-12 15:44:40 +0200

    x86, generic: mark early_printk as asmlinkage
    It's not explicitly marked as asmlinkage, but invoked from x86_32
    startup code with parameters on stack.
    
    No other architectures define early_printk and none of them are affected
    by this change, since defines asmlinkage as empty token.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/printk.c
+++ kernel/printk.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Architectures can override it:
  */
-void __attribute__((weak)) early_printk(const char *fmt, ...)
+void asmlinkage __attribute__((weak)) early_printk(const char *fmt, ...)
 {
 }
 
