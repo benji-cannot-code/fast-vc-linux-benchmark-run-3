d95fa23e8dac55e4cfa0014a8ab1f86333a10ff6
Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
2008-05-12 21:20:41 +0200

    ftrace: annotate core code that should not be traced
    Mark with "notrace" functions in core code that should not be
    traced.  The "notrace" attribute will prevent gcc from adding
    a call to ftrace on the annotated funtions.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- lib/smp_processor_id.c
+++ lib/smp_processor_id.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kallsyms.h>
 #include <linux/sched.h>
 
-unsigned int debug_smp_processor_id(void)
+notrace unsigned int debug_smp_processor_id(void)
 {
 	unsigned long preempt_count = preempt_count();
 	int this_cpu = raw_smp_processor_id();
