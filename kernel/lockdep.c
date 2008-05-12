f3f860fe2017d8db33079fbe292f130003506d59
Steven Rostedt <rostedt@goodmis.org>
2008-05-12 21:21:00 +0200

    lockdep: update lockdep_recursion on graph_lock
    With the introduction of ftrace, it is possible to recurse into
    the lockdep functions via the mcount call. To prevent possible
    lockups, updating the lockdep_recursion counter on grabbing the internal
    lockdep_lock should prevent deadlocks.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -83,6 +83,8 @@ static int graph_lock(void)
 		__raw_spin_unlock(&lockdep_lock);
 		return 0;
 	}
+	/* prevent any recursions within lockdep from causing deadlocks */
+	current->lockdep_recursion++;
 	return 1;
 }
 
@@ -91,6 +93,7 @@ static inline int graph_unlock(void)
 	if (debug_locks && !__raw_spin_is_locked(&lockdep_lock))
 		return DEBUG_LOCKS_WARN_ON(1);
 
+	current->lockdep_recursion--;
 	__raw_spin_unlock(&lockdep_lock);
 	return 0;
 }
