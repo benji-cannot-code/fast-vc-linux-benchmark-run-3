2d1eb0e025aae7ec3e33b89ada7498810df74b35
Steven Rostedt <rostedt@goodmis.org>
2008-05-12 21:20:54 +0200

    ftrace: add TRACE_STACK and TRACE_SPECIAL to selftest validation
    The selftest validation code checks for valid entries in the trace buffer.
    TRACE_STACK and TRACE_SPECIAL have been added to the code but not to
    the validator. This patch adds the two to prevent them from flagging a
    failure in the selftest.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/trace/trace_selftest.c
+++ kernel/trace/trace_selftest.c
@@ -10,6 +10,8 @@ static inline int trace_valid_entry(struct trace_entry *entry)
 	case TRACE_FN:
 	case TRACE_CTX:
 	case TRACE_WAKE:
+	case TRACE_STACK:
+	case TRACE_SPECIAL:
 		return 1;
 	}
 	return 0;
@@ -181,7 +183,7 @@ int trace_selftest_startup_dynamic_tracing(struct tracer *trace,
 
 	/* we should only have one item */
 	if (!ret && count != 1) {
-		printk(KERN_CONT ".. filter failed ..");
+		printk(KERN_CONT ".. filter failed count=%ld ..", count);
 		ret = -1;
 		goto out;
 	}
