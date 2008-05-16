f75e67f1d873d30792f5b69de3adbfa6629c82aa
Jonathan Corbet <corbet@lwn.net>
2008-05-16 13:46:14 -0600

    spidev: BKL pushdown
    Add the BKL to spidev_open(), even though the existing locking looks
    adequate.
    
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
+#include <linux/smp_lock.h>
 
 #include <linux/spi/spi.h>
 #include <linux/spi/spidev.h>
@@ -394,6 +395,7 @@ static int spidev_open(struct inode *inode, struct file *filp)
 	struct spidev_data	*spidev;
 	int			status = -ENXIO;
 
+	lock_kernel();
 	mutex_lock(&device_list_lock);
 
 	list_for_each_entry(spidev, &device_list, device_entry) {
@@ -419,6 +421,7 @@ static int spidev_open(struct inode *inode, struct file *filp)
 		pr_debug("spidev: nothing for minor %d\n", iminor(inode));
 
 	mutex_unlock(&device_list_lock);
+	unlock_kernel();
 	return status;
 }
 
