f6106f88d31888019c81f04459dc4001a48b62cd
Ingo Molnar <mingo@elte.hu>
2008-05-16 10:41:53 +0200

    ftrace: fix include file dependency
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_FTRACE
 
 #include <linux/linkage.h>
+#include <linux/fs.h>
 
 extern int ftrace_enabled;
 extern int
