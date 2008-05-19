e657abf919c26a35d2c321b2553292154a82ff1c
Christoph Hellwig <hch@infradead.org>
2008-05-19 16:34:34 +1000

    [XFS] Kill attr_capable checks as already done in xattr_permission.
    No need for addition permission checks in the xattr handler,
    fs/xattr.c:xattr_permission() already does them, and in fact slightly more
    strict then what was in the attr_capable handlers.
    
    SGI-PV: 981809
    SGI-Modid: xfs-linux-melb:xfs-kern:31164a
    
    Signed-off-by: Christoph Hellwig <hch@infradead.org>
    Signed-off-by: Tim Shimmin <tes@sgi.com>
    Signed-off-by: Lachlan McIlroy <lachlan@sgi.com>

--- fs/xfs/xfs_attr.h
+++ fs/xfs/xfs_attr.h
@@ -43,7 +43,6 @@ typedef int (*attrset_t)(bhv_vnode_t *, char *, void *, size_t, int);
 typedef int (*attrget_t)(bhv_vnode_t *, char *, void *, size_t, int);
 typedef int (*attrremove_t)(bhv_vnode_t *, char *, int);
 typedef int (*attrexists_t)(bhv_vnode_t *);
-typedef int (*attrcapable_t)(bhv_vnode_t *, struct cred *);
 
 typedef struct attrnames {
 	char *		attr_name;
@@ -53,7 +52,6 @@ typedef struct attrnames {
 	attrset_t	attr_set;
 	attrremove_t	attr_remove;
 	attrexists_t	attr_exists;
-	attrcapable_t	attr_capable;
 } attrnames_t;
 
 #define ATTR_NAMECOUNT	4
