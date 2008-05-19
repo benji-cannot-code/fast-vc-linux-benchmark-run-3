1993d5582d0fae94c7c24f2ac3e4074862139617
Michael Krufky <mkrufky@linuxtv.org>
2008-05-19 18:58:40 -0300

    V4L/DVB (8260): sms1xxx: build fixes
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/mdtv/smschar.c
+++ drivers/media/mdtv/smschar.c
@@ -566,6 +566,7 @@ int smschar_initialize(void)
 
 	return smscore_register_hotplug(smschar_hotplug);
 }
+EXPORT_SYMBOL(smschar_initialize);
 
 void smschar_terminate(void)
 {
@@ -574,3 +575,4 @@ void smschar_terminate(void)
 	unregister_chrdev_region(devno, SMSCHAR_NR_DEVS);
 	smscore_unregister_hotplug(smschar_hotplug);
 }
+EXPORT_SYMBOL(smschar_terminate);
