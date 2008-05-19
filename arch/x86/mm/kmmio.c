24ccc98afb0d4cf36ee9cea4f5dece4a28c4ad9d
Ingo Molnar <mingo@elte.hu>
2008-05-19 13:35:24 +0200

    - fix mmioftrace + rcu merge interaction
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/mm/kmmio.c
+++ arch/x86/mm/kmmio.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/list.h>
+#include <linux/rculist.h>
 #include <linux/spinlock.h>
 #include <linux/hash.h>
 #include <linux/init.h>
