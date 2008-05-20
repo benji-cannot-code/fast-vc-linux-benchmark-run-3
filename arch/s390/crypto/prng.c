d7544bdaa65406aebe6303902f690d284146f822
Arnd Bergmann <arnd@arndb.de>
2008-05-20 19:15:45 +0200

    crypto-prng: BKL pushdown
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/s390/crypto/prng.c
+++ arch/s390/crypto/prng.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/smp_lock.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -49,6 +50,7 @@ static unsigned char parm_block[32] = {
 
 static int prng_open(struct inode *inode, struct file *file)
 {
+	cycle_kernel_lock();
 	return nonseekable_open(inode, file);
 }
 
