8a1e125f022c137ec2334c33e9ab36f6026ba2c9
Prakash, Sathya <sathya.prakash@lsi.com>
2008-05-21 00:55:33 +0530

    [SCSI] mpt fusion: Driver version upgrade to 3.04.07
    Updating driver version to 3.04.07 from 3.04.06
    
    Signed-off-by: Sathya Prakash <sathya.prakash@lsi.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/message/fusion/mptbase.h
+++ drivers/message/fusion/mptbase.h
@@ -77,8 +77,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define COPYRIGHT	"Copyright (c) 1999-2007 " MODULEAUTHOR
 #endif
 
-#define MPT_LINUX_VERSION_COMMON	"3.04.06"
-#define MPT_LINUX_PACKAGE_NAME		"@(#)mptlinux-3.04.06"
+#define MPT_LINUX_VERSION_COMMON	"3.04.07"
+#define MPT_LINUX_PACKAGE_NAME		"@(#)mptlinux-3.04.07"
 #define WHAT_MAGIC_STRING		"@" "(" "#" ")"
 
 #define show_mptmod_ver(s,ver)  \
