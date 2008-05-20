98ef6dd33b6b6aa73a486630bc9ba41f15599a76
Arnd Bergmann <arnd@arndb.de>
2008-05-20 19:16:11 +0200

    macintosh-smu: BKL pushdown
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/macintosh/smu.c
+++ drivers/macintosh/smu.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *    the userland interface
  */
 
+#include <linux/smp_lock.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/device.h>
@@ -1084,10 +1085,12 @@ static int smu_open(struct inode *inode, struct file *file)
 	pp->mode = smu_file_commands;
 	init_waitqueue_head(&pp->wait);
 
+	lock_kernel();
 	spin_lock_irqsave(&smu_clist_lock, flags);
 	list_add(&pp->list, &smu_clist);
 	spin_unlock_irqrestore(&smu_clist_lock, flags);
 	file->private_data = pp;
+	unlock_kernel();
 
 	return 0;
 }
