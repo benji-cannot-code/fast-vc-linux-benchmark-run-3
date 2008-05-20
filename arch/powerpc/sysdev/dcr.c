3d3388d83e9a695bfc759eacb27f49296f53d865
Josh Boyer <jwboyer@linux.vnet.ibm.com>
2008-05-20 07:59:23 -0500

    [POWERPC] 4xx: Fix compiler warning on DCR_NATIVE only builds
    With the recent DCR code rework, we get a compiler warning about
    find_dcr_parent being defined but not used.  This fixes it by only defining
    the function if CONFIG_PPC_DCR_MMIO is set.
    
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>

--- arch/powerpc/sysdev/dcr.c
+++ arch/powerpc/sysdev/dcr.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/prom.h>
 #include <asm/dcr.h>
 
+#ifdef CONFIG_PPC_DCR_MMIO
 static struct device_node *find_dcr_parent(struct device_node *node)
 {
 	struct device_node *par, *tmp;
@@ -42,6 +43,7 @@ static struct device_node *find_dcr_parent(struct device_node *node)
 	}
 	return par;
 }
+#endif
 
 #if defined(CONFIG_PPC_DCR_NATIVE) && defined(CONFIG_PPC_DCR_MMIO)
 
