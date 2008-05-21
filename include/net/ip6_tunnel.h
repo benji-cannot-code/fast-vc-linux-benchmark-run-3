3080f1999c53ae9fca3268ce2b412aa8e57acb97
Pavel Emelyanov <xemul@openvz.org>
2008-05-21 14:17:05 -0700

    ip6tnl: Use on-device stats instead of private ones.
    This tunnel uses its own private structure and requires separate
    patch to switch from private stats to on-device ones.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ip6_tunnel.h
+++ include/net/ip6_tunnel.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ip6_tnl {
 	struct ip6_tnl *next;	/* next tunnel in list */
 	struct net_device *dev;	/* virtual device associated with tunnel */
-	struct net_device_stats stat;	/* statistics for tunnel device */
 	int recursion;		/* depth of hard_start_xmit recursion */
 	struct ip6_tnl_parm parms;	/* tunnel configuration parameters */
 	struct flowi fl;	/* flowi template for xmit */
