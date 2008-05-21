d0f2b9b6ab8b5ab6b7e32567fecdb5bd9cfa9429
Barry Naujok <bnaujok@sgi.com>
2008-05-21 16:58:22 +1000

    [XFS] Return case-insensitive match for dentry cache
    This implements the code to store the actual filename found during a
    lookup in the dentry cache and to avoid multiple entries in the dcache
    pointing to the same inode.
    
    To avoid polluting the dcache, we implement a new directory inode
    operations for lookup. xfs_vn_ci_lookup() stores the correct case name in
    the dcache.
    
    The "actual name" is only allocated and returned for a case- insensitive
    match and not an actual match.
    
    Another unusual interaction with the dcache is not storing negative
    dentries like other filesystems doing a d_add(dentry, NULL) when an ENOENT
    is returned. During the VFS lookup, if a dentry returned has no inode,
    dput is called and ENOENT is returned. By not doing a d_add, this actually
    removes it completely from the dcache to be reused. create/rename have to
    be modified to support unhashed dentries being passed in.
    
    SGI-PV: 981521
    SGI-Modid: xfs-linux-melb:xfs-kern:31208a
    
    Signed-off-by: Barry Naujok <bnaujok@sgi.com>
    Signed-off-by: Christoph Hellwig <hch@infradead.org>

--- fs/xfs/linux-2.6/xfs_iops.h
+++ fs/xfs/linux-2.6/xfs_iops.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern const struct inode_operations xfs_inode_operations;
 extern const struct inode_operations xfs_dir_inode_operations;
+extern const struct inode_operations xfs_dir_ci_inode_operations;
 extern const struct inode_operations xfs_symlink_inode_operations;
 
 extern const struct file_operations xfs_file_operations;
