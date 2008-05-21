89e4bc69a6f19afbd58ee07307b8b0eeda0dd2d9
Barry Naujok <bnaujok@sgi.com>
2008-05-21 16:58:55 +1000

    [XFS] XFS: ASCII case-insensitive support
    Implement ASCII case-insensitive support. It's primary purpose is for
    supporting existing filesystems that already use this case-insensitive
    mode migrated from IRIX. But, if you only need ASCII-only case-insensitive
    support (ie. English only) and will never use another language, then this
    mode is perfectly adequate.
    
    ASCII-CI is implemented by generating hashes based on lower-case letters
    and doing lower-case compares. It implements a new xfs_nameops vector for
    doing the hashes and comparisons for all filename operations.
    
    To create a filesystem with this CI mode, use: # mkfs.xfs -n version=ci
    <device>
    
    SGI-PV: 981516
    SGI-Modid: xfs-linux-melb:xfs-kern:31209a
    
    Signed-off-by: Barry Naujok <bnaujok@sgi.com>
    Signed-off-by: Christoph Hellwig <hch@infradead.org>

--- fs/xfs/linux-2.6/xfs_linux.h
+++ fs/xfs/linux-2.6/xfs_linux.h
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/log2.h>
 #include <linux/spinlock.h>
 #include <linux/random.h>
+#include <linux/ctype.h>
 
 #include <asm/page.h>
 #include <asm/div64.h>
