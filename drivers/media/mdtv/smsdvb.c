a45eb9dd5f745b793d6db91b7595586855e60713
Steven Toth <stoth@hauppauge.com>
2008-05-22 15:46:32 -0300

    V4L/DVB (8263): sms1xxx: merge ksyms
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/mdtv/smsdvb.c
+++ drivers/media/mdtv/smsdvb.c
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_demux.h"
 #include "dvb_frontend.h"
 
-#include "smskdefs.h" // page, scatterlist, kmutex
 #include "smscoreapi.h"
 #include "smstypes.h"
 
