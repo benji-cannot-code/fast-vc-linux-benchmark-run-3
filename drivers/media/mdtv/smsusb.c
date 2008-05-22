b87453789e05c01a73c99ff3865d5abef77be9af
Steven Toth <stoth@hauppauge.com>
2008-05-22 18:07:39 -0300

    V4L/DVB (8271): sms1xxx: usbvid table
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/mdtv/smsusb.c
+++ drivers/media/mdtv/smsusb.c
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "smscoreapi.h"
 
-#define USB_VID_SIANO	0x187f
-#define USB_PID_0010	0x0010
-#define USB_PID_0100	0x0100
-#define USB_PID_0200	0x0200
-
 #define USB1_BUFFER_SIZE		0x1000
 #define USB2_BUFFER_SIZE		0x4000
 
@@ -58,14 +53,6 @@ typedef struct _smsusb_device
 	int				response_alignment;
 	int				buffer_size;
 } *psmsusb_device_t;
-
-static struct usb_device_id smsusb_id_table [] = {
-	{ USB_DEVICE(USB_VID_SIANO, USB_PID_0010) },
-	{ USB_DEVICE(USB_VID_SIANO, USB_PID_0100) },
-	{ USB_DEVICE(USB_VID_SIANO, USB_PID_0200) },
-	{ }		/* Terminating entry */
-};
-MODULE_DEVICE_TABLE (usb, smsusb_id_table);
 
 int smsusb_submit_urb(smsusb_device_t* dev, smsusb_urb_t* surb);
 
@@ -316,7 +303,7 @@ int smsusb_init_device(struct usb_interface *intf)
 
 	switch (dev->udev->descriptor.idProduct)
 	{
-		case USB_PID_0100:
+		case 0x100:
 			dev->buffer_size = USB1_BUFFER_SIZE;
 
 			params.setmode_handler = smsusb1_setmode;
@@ -414,6 +401,14 @@ void smsusb_disconnect(struct usb_interface *intf)
 {
 	smsusb_term_device(intf);
 }
+
+static struct usb_device_id smsusb_id_table [] = {
+	{ USB_DEVICE(0x187F, 0x0010) },
+	{ USB_DEVICE(0x187F, 0x0100) },
+	{ USB_DEVICE(0x187F, 0x0200) },
+	{ }		/* Terminating entry */
+};
+MODULE_DEVICE_TABLE (usb, smsusb_id_table);
 
 static struct usb_driver smsusb_driver = {
 	.name			= "smsusb",
