a5b3aae28c712fd3a3eb91172e5be2e43269911b
Steven Toth <stoth@hauppauge.com>
2008-05-22 15:48:54 -0300

    V4L/DVB (8264): sms1xxx: remove smstypes.h
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/mdtv/smscoreapi.c
+++ drivers/media/mdtv/smscoreapi.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 
 #include "smscoreapi.h"
-#include "smstypes.h"
 
 typedef struct _smscore_device_notifyee
 {
--- drivers/media/mdtv/smsusb.c
+++ drivers/media/mdtv/smsusb.c
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 
 #include "smscoreapi.h"
-#include "smstypes.h"
 
 #define USB_VID_SIANO	0x187f
 #define USB_PID_0010	0x0010
