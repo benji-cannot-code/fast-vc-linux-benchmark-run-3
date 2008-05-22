4bb6499acff0cd207a4173ffd908fb5578814a36
Steven Toth <stoth@hauppauge.com>
2008-05-22 18:05:26 -0300

    V4L/DVB (8269): sms1xxx: copyrights
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/mdtv/smscoreapi.c
+++ drivers/media/mdtv/smscoreapi.c
@@ -1,4 +1,26 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+
+/*
+ *  Driver for the Siano SMS10xx USB dongle
+ *
+ *  Copyright (c) 2008 <TODO: INSERT ALL COPYRIGHT OWNERS and MAINT. NAMES HERE>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 /*!
 
 	\file		smscoreapi.c
