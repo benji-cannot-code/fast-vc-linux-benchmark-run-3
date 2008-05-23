bc2d9bb62635d1145f7009042259c8a23d860173
Anton Vorontsov <avorontsov@ru.mvista.com>
2008-05-23 20:38:56 +0400

    powerpc/QE: add support for QE USB clocks routing
    This patch adds a function to the qe_lib to setup QE USB clocks routing.
    To setup clocks safely, cmxgcr register needs locking, so I just reused
    ucc_lock since it was used only to protect cmxgcr.
    
    The idea behind placing clocks routing functions into the qe_lib is that
    later we'll hopefully switch to the generic Linux Clock API, thus, for
    example, FHCI driver may be used for QE and CPM chips without nasty #ifdefs.
    
    This patch also fixes QE_USB_RESTART_TX command definition in the qe.h.
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Acked-By: Timur Tabi <timur@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/sysdev/qe_lib/ucc.c
+++ arch/powerpc/sysdev/qe_lib/ucc.c
@@ -27,7 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/qe.h>
 #include <asm/ucc.h>
 
-static DEFINE_SPINLOCK(ucc_lock);
+DEFINE_SPINLOCK(cmxgcr_lock);
+EXPORT_SYMBOL(cmxgcr_lock);
 
 int ucc_set_qe_mux_mii_mng(unsigned int ucc_num)
 {
@@ -36,10 +37,10 @@ int ucc_set_qe_mux_mii_mng(unsigned int ucc_num)
 	if (ucc_num > UCC_MAX_NUM - 1)
 		return -EINVAL;
 
-	spin_lock_irqsave(&ucc_lock, flags);
+	spin_lock_irqsave(&cmxgcr_lock, flags);
 	clrsetbits_be32(&qe_immr->qmx.cmxgcr, QE_CMXGCR_MII_ENET_MNG,
 		ucc_num << QE_CMXGCR_MII_ENET_MNG_SHIFT);
-	spin_unlock_irqrestore(&ucc_lock, flags);
+	spin_unlock_irqrestore(&cmxgcr_lock, flags);
 
 	return 0;
 }
