449392860bfa66e21a26bd9869e65a85fbb13d57
Stephen Rothwell <sfr@canb.auug.org.au>
2008-05-23 16:28:54 +1000

    drivers/net: Use linux/of_{device,platform}.h instead of asm
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- drivers/net/ibm_newemac/core.h
+++ drivers/net/ibm_newemac/core.h
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/dma-mapping.h>
 #include <linux/spinlock.h>
+#include <linux/of_platform.h>
 
-#include <asm/of_platform.h>
 #include <asm/io.h>
 #include <asm/dcr.h>
 
