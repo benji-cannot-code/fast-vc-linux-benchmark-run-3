9ff594fafbc2b01aead2abe79a5e407ba033845a
Michael Krufky <mkrufky@linuxtv.org>
2008-05-25 00:25:08 -0300

    V4L/DVB (7917): au8522.c shouldn't #include "dvb-pll.h"
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/au8522.c
+++ drivers/media/dvb/frontends/au8522.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/delay.h>
 #include "dvb_frontend.h"
-#include "dvb-pll.h"
 #include "au8522.h"
 
 struct au8522_state {
