777d231b6489a3b9b1a1f0ea477e82bf5fb7399b
Avi Kivity <avi@qumranet.com>
2008-05-26 20:06:35 +0300

    KVM: MTRR support
    Add emulation for the memory type range registers, needed by VMware esx 3.5,
    and by pci device assignment.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- include/asm-x86/kvm_host.h
+++ include/asm-x86/kvm_host.h
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KVM_MIN_FREE_MMU_PAGES 5
 #define KVM_REFILL_PAGES 25
 #define KVM_MAX_CPUID_ENTRIES 40
+#define KVM_NR_VAR_MTRR 8
 
 extern spinlock_t kvm_lock;
 extern struct list_head vm_list;
@@ -291,6 +292,8 @@ struct kvm_vcpu_arch {
 	struct page *time_page;
 
 	bool nmi_pending;
+
+	u64 mtrr[0x100];
 };
 
 struct kvm_mem_alias {
