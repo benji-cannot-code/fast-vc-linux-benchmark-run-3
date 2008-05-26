0922c3d6b1e4aa3740f78978e0b0ef709058406c
Mike Isely <isely@pobox.com>
2008-05-26 05:54:24 -0300

    V4L/DVB (7938): pvrusb2: Increase enforced encoder wait delay to improve reliability
    The driver enforces a "quiet period" on the encoder in certain
    situations before attempting to operate it.  This seems to help avoid
    video encoding errors / corruption.  The quiet period was 50msec, but
    through experimentation it has been observed to improve further if the
    interval is increased to 100msec.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pvrusb2/pvrusb2-hdw.c
+++ drivers/media/video/pvrusb2/pvrusb2-hdw.c
@@ -45,8 +45,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TIME_MSEC_DECODER_WAIT 50
 
 /* This defines a minimum interval that the encoder must remain quiet
-   before we are allowed to configure it. */
-#define TIME_MSEC_ENCODER_WAIT 50
+   before we are allowed to configure it.  I had this originally set to
+   50msec, but Martin Dauskardt <martin.dauskardt@gmx.de> reports that
+   things work better when it's set to 100msec. */
+#define TIME_MSEC_ENCODER_WAIT 100
 
 /* This defines the minimum interval that the encoder must successfully run
    before we consider that the encoder has run at least once since its
