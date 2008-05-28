5e39ab72ebe152655ac06b8be5a0778dc109088b
Eric Miao <eric.miao@marvell.com>
2008-05-28 10:44:16 +0800

    [ARM] pxa: add GPIO expander (PCA9539) support for zylonite
         And also reserve 32 IRQs for the two GPIO expanders.
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-pxa/irqs.h
+++ include/asm-arm/arch-pxa/irqs.h
@@ -185,6 +185,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
       defined(CONFIG_MACH_PCM027) || \
       defined(CONFIG_MACH_MAGICIAN)
 #define NR_IRQS			(IRQ_BOARD_END)
+#elif defined(CONFIG_MACH_ZYLONITE)
+#define NR_IRQS			(IRQ_BOARD_START + 32)
 #else
 #define NR_IRQS			(IRQ_BOARD_START)
 #endif
