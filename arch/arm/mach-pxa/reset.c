3591d21c5568e2e427652310e863fc30c4bd2c4d
Philipp Zabel <philipp.zabel@gmail.com>
2008-05-29 21:18:07 +0100

    [ARM] 5069/1: pxa: include pxa2xx-regs.h in reset.c for RSCR
    The RCSR definition was moved into pxa2xx-regs.h.
    
    Signed-off-by: Philipp Zabel <philipp.zabel@gmail.com>
    Acked-by: Dmitry Baryshkov <dbaryshkov@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/reset.c
+++ arch/arm/mach-pxa/reset.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/proc-fns.h>
 
 #include <asm/arch/pxa-regs.h>
+#include <asm/arch/pxa2xx-regs.h>
 
 static void do_hw_reset(void);
 
