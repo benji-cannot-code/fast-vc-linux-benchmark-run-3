964c542caeb78cd8f3e68880051adafe65ad7faa
Ben Hutchings <bhutchings@solarflare.com>
2008-05-30 22:27:04 +0100

    sfc: Use kernel I2C system and i2c-algo-bit driver
    Remove our own implementation of I2C bit-banging.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/sfc/efx.c
+++ drivers/net/sfc/efx.c
@@ -1816,6 +1816,7 @@ static struct efx_board efx_dummy_board_info = {
 	.init    = efx_nic_dummy_op_int,
 	.init_leds = efx_port_dummy_op_int,
 	.set_fault_led = efx_port_dummy_op_blink,
+	.fini	= efx_port_dummy_op_void,
 };
 
 /**************************************************************************
@@ -1942,6 +1943,7 @@ static void efx_pci_remove_main(struct efx_nic *efx)
 	efx_fini_port(efx);
 
 	/* Shutdown the board, then the NIC and board state */
+	efx->board_info.fini(efx);
 	falcon_fini_interrupt(efx);
 
 	efx_fini_napi(efx);
