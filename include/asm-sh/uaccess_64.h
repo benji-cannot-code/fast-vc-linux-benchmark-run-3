4848dea9fa7701cf38e3447227a40eb12534a971
Paul Mundt <lethal@linux-sh.org>
2008-06-03 19:37:35 +0900

    sh: Use the common segment definitions for the _64 uaccess routines.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/uaccess_64.h
+++ include/asm-sh/uaccess_64.h
@@ -27,25 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VERIFY_READ    0
 #define VERIFY_WRITE   1
 
-/*
- * The fs value determines whether argument validity checking should be
- * performed or not.  If get_fs() == USER_DS, checking is performed, with
- * get_fs() == KERNEL_DS, checking is bypassed.
- *
- * For historical reasons (Data Segment Register?), these macros are misnamed.
- */
-
-#define MAKE_MM_SEG(s)	((mm_segment_t) { (s) })
-
-#define KERNEL_DS	MAKE_MM_SEG(0xFFFFFFFF)
-#define USER_DS		MAKE_MM_SEG(0x80000000)
-
-#define get_ds()	(KERNEL_DS)
-#define get_fs()        (current_thread_info()->addr_limit)
-#define set_fs(x)       (current_thread_info()->addr_limit=(x))
-
-#define segment_eq(a,b)	((a).seg == (b).seg)
-
 #define __addr_ok(addr) ((unsigned long)(addr) < (current_thread_info()->addr_limit.seg))
 
 /*
