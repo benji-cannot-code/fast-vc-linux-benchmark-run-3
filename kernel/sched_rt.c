9cdf9cf9ace2369e1a6aa18c8cbd31efe888bfe3
Gregory Haskins <ghaskins@novell.com>
2008-06-04 15:04:05 -0400

    sched: fix cpupri hotplug support
    The RT folks over at RedHat found an issue w.r.t. hotplug support which
    was traced to problems with the cpupri infrastructure in the scheduler:
    
    https://bugzilla.redhat.com/show_bug.cgi?id=449676
    
    This bug affects 23-rt12+, 24-rtX, 25-rtX, and sched-devel.  This patch
    applies to 25.4-rt4, though it should trivially apply to most cpupri enabled
    kernels mentioned above.
    
    It turned out that the issue was that offline cpus could get inadvertently
    registered with cpupri so that they were erroneously selected during
    migration decisions.  The end result would be an OOPS as the offline cpu
    had tasks routed to it.
    
    This patch generalizes the old join/leave domain interface into an
    online/offline interface, and adjusts the root-domain/hotplug code to
    utilize it.
    
    I was able to easily reproduce the issue prior to this patch, and am no
    longer able to reproduce it after this patch.  I can offline cpus
    indefinately and everything seems to be in working order.
    
    Thanks to Arnaldo (acme), Thomas, and Peter for doing the legwork to point
    me in the right direction.  Also thank you to Peter for reviewing the
    early iterations of this patch.
    
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched_rt.c
+++ kernel/sched_rt.c
@@ -13,6 +13,9 @@ static inline int rt_overloaded(struct rq *rq)
 
 static inline void rt_set_overload(struct rq *rq)
 {
+	if (!rq->online)
+		return;
+
 	cpu_set(rq->cpu, rq->rd->rto_mask);
 	/*
 	 * Make sure the mask is visible before we set
@@ -27,6 +30,9 @@ static inline void rt_set_overload(struct rq *rq)
 
 static inline void rt_clear_overload(struct rq *rq)
 {
+	if (!rq->online)
+		return;
+
 	/* the order here really doesn't matter */
 	atomic_dec(&rq->rd->rto_count);
 	cpu_clear(rq->cpu, rq->rd->rto_mask);
@@ -395,7 +401,10 @@ void inc_rt_tasks(struct sched_rt_entity *rt_se, struct rt_rq *rt_rq)
 	if (rt_se_prio(rt_se) < rt_rq->highest_prio) {
 		struct rq *rq = rq_of_rt_rq(rt_rq);
 		rt_rq->highest_prio = rt_se_prio(rt_se);
-		cpupri_set(&rq->rd->cpupri, rq->cpu, rt_se_prio(rt_se));
+
+		if (rq->online)
+			cpupri_set(&rq->rd->cpupri, rq->cpu,
+				   rt_se_prio(rt_se));
 	}
 #endif
 #ifdef CONFIG_SMP
@@ -449,7 +458,10 @@ void dec_rt_tasks(struct sched_rt_entity *rt_se, struct rt_rq *rt_rq)
 
 	if (rt_rq->highest_prio != highest_prio) {
 		struct rq *rq = rq_of_rt_rq(rt_rq);
-		cpupri_set(&rq->rd->cpupri, rq->cpu, rt_rq->highest_prio);
+
+		if (rq->online)
+			cpupri_set(&rq->rd->cpupri, rq->cpu,
+				   rt_rq->highest_prio);
 	}
 
 	update_rt_migration(rq_of_rt_rq(rt_rq));
@@ -1155,7 +1167,7 @@ static void set_cpus_allowed_rt(struct task_struct *p,
 }
 
 /* Assumes rq->lock is held */
-static void join_domain_rt(struct rq *rq)
+static void rq_online_rt(struct rq *rq)
 {
 	if (rq->rt.overloaded)
 		rt_set_overload(rq);
@@ -1164,7 +1176,7 @@ static void join_domain_rt(struct rq *rq)
 }
 
 /* Assumes rq->lock is held */
-static void leave_domain_rt(struct rq *rq)
+static void rq_offline_rt(struct rq *rq)
 {
 	if (rq->rt.overloaded)
 		rt_clear_overload(rq);
@@ -1332,8 +1344,8 @@ static const struct sched_class rt_sched_class = {
 	.load_balance		= load_balance_rt,
 	.move_one_task		= move_one_task_rt,
 	.set_cpus_allowed       = set_cpus_allowed_rt,
-	.join_domain            = join_domain_rt,
-	.leave_domain           = leave_domain_rt,
+	.rq_online              = rq_online_rt,
+	.rq_offline             = rq_offline_rt,
 	.pre_schedule		= pre_schedule_rt,
 	.post_schedule		= post_schedule_rt,
 	.task_wake_up		= task_wake_up_rt,
