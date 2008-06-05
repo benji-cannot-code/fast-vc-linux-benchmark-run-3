5a7262ae22f218877564d06d2e6adf7ddf945006
Sam Ravnborg <sam@ravnborg.org>
2008-06-05 23:08:51 +0200

    sparc: Merge asm-sparc{,64}/poll.h
    :$ diff -u include/asm-sparc/poll.h include/asm-sparc64/poll.h
    :-- include/asm-sparc/poll.h	2008-06-13 06:42:07.000000000 +0200
    :++ include/asm-sparc64/poll.h	2008-06-13 06:42:07.000000000 +0200
    :@@ -1,5 +1,5 @@
    :-#ifndef __SPARC_POLL_H
    :-#define __SPARC_POLL_H
    :+#ifndef __SPARC64_POLL_H
    :+#define __SPARC64_POLL_H
    :
    : #define POLLWRNORM	POLLOUT
    : #define POLLWRBAND	256
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- include/asm-sparc64/poll.h
+++ include/asm-sparc64/poll.h
@@ -1,13 +1,2 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef __SPARC64_POLL_H
-#define __SPARC64_POLL_H
-
-#define POLLWRNORM	POLLOUT
-#define POLLWRBAND	256
-#define POLLMSG		512
-#define POLLREMOVE	1024
-#define POLLRDHUP       2048
-
-#include <asm-generic/poll.h>
-
-#endif
+#include <asm-sparc/poll.h>
