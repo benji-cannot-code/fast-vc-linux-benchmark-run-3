96c250db342a9efaf3bbb007bfc989e6c2188f2d
Sam Ravnborg <sam@ravnborg.org>
2008-06-06 21:35:43 +0200

    sparc: Merge asm-sparc{,64}/types.h
    Copy content of sparc64 file to sparc file.
    There is only minimal possibilities for further unification.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- include/asm-sparc/types.h
+++ include/asm-sparc/types.h
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SPARC_TYPES_H
 #define _SPARC_TYPES_H
-
 /*
  * This file is never included by application software unless
  * explicitly requested (e.g., via linux/types.h) in which case the
@@ -9,6 +8,35 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * not a major issue.  However, for interoperability, libraries still
  * need to be careful to avoid a name clashes.
  */
+
+#if defined(__sparc__) && defined(__arch64__)
+
+/*** SPARC 64 bit ***/
+#include <asm-generic/int-l64.h>
+
+#ifndef __ASSEMBLY__
+
+typedef unsigned short umode_t;
+
+#endif /* __ASSEMBLY__ */
+
+#ifdef __KERNEL__
+
+#define BITS_PER_LONG 64
+
+#ifndef __ASSEMBLY__
+
+/* Dma addresses come in generic and 64-bit flavours.  */
+
+typedef u32 dma_addr_t;
+typedef u64 dma64_addr_t;
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* __KERNEL__ */
+#else
+
+/*** SPARC 32 bit ***/
 #include <asm-generic/int-ll64.h>
 
 #ifndef __ASSEMBLY__
@@ -29,5 +57,7 @@ typedef u32 dma64_addr_t;
 #endif /* __ASSEMBLY__ */
 
 #endif /* __KERNEL__ */
+
+#endif /* defined(__sparc__) && defined(__arch64__) */
 
 #endif /* defined(_SPARC_TYPES_H) */
