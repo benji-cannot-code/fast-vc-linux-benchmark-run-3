44f6c3d4219136888dfe64dbbb402b134bdd64ca
Mark Brown <broonie@sirena.org.uk>
2008-06-10 12:30:05 +0100

    [ARM] 5085/2: PXA: Move AC97 over to the new central device declaration model
    As well as moving all the device declarations to a single one in devices.c
    this causes all platforms to register the I/O and interrupt resources for
    the AC97 controller.
    
    Cc: eric miao <eric.miao@marvell.com>
    Cc: Mike Rapoport <mike@compulab.co.il>
    Cc: Lennert Buytenhek <buytenh@wantstofly.org>
    Cc: Jürgen Schindele <linux@schindele.name>
    Cc: Juergen Beisert <jbe@pengutronix.de>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/lpd270.c
+++ arch/arm/mach-pxa/lpd270.c
@@ -169,11 +169,6 @@ static struct platform_device smc91x_device = {
 	.resource	= smc91x_resources,
 };
 
-static struct platform_device lpd270_audio_device = {
-	.name		= "pxa2xx-ac97",
-	.id		= -1,
-};
-
 static struct resource lpd270_flash_resources[] = {
 	[0] = {
 		.start	= PXA_CS0_PHYS,
@@ -413,7 +408,6 @@ __setup("lcd=", lpd270_set_lcd);
 static struct platform_device *platform_devices[] __initdata = {
 	&smc91x_device,
 	&lpd270_backlight_device,
-	&lpd270_audio_device,
 	&lpd270_flash_device[0],
 	&lpd270_flash_device[1],
 };
@@ -456,6 +450,8 @@ static void __init lpd270_init(void)
 	pxa_gpio_mode(GPIO16_PWM0_MD);
 
 	platform_add_devices(platform_devices, ARRAY_SIZE(platform_devices));
+
+	pxa_set_ac97_info(NULL);
 
 	if (lpd270_lcd_to_use != NULL)
 		set_pxa_fb_info(lpd270_lcd_to_use);
