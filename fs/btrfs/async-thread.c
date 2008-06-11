6f35ccadc6e2a2813c7318fbcd5cb7c8000b3d6a
Chris Mason <chris.mason@oracle.com>
2008-06-11 17:09:48 -0400

    Btrfs: Add backport for the kthread work on kernels older than 2.6.20
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/async-thread.c
+++ fs/btrfs/async-thread.c
@@ -17,10 +17,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Boston, MA 021110-1307, USA.
  */
 
+#include <linux/version.h>
 #include <linux/kthread.h>
 #include <linux/list.h>
 #include <linux/spinlock.h>
-#include <linux/freezer.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,20)
+# include <linux/freezer.h>
+#else
+# include <linux/sched.h>
+#endif
+
 #include "async-thread.h"
 
 /*
