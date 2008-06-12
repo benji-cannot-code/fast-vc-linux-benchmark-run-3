96b4aa07202ae7d4f3d327b21a907e22fd3498b7
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2008-06-12 09:46:52 +0800

    iwlwifi: removes the RUN_TIME_CALIB ifdef
    This patch removes the possibility not to compile the run time
    calibrations. It also renames priv->sensitivity_work to
    priv->run_time_calib_work, and moves bg_run_time_calib to iwl4965_base
    since it is common to both: 4965 and 5000.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-calib.h
+++ drivers/net/wireless/iwlwifi/iwl-calib.h
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-core.h"
 #include "iwl-dev.h"
 
-#ifdef CONFIG_IWLWIFI_RUN_TIME_CALIB
 void iwl_chain_noise_calibration(struct iwl_priv *priv,
 				struct iwl4965_notif_statistics *stat_resp);
 void iwl_sensitivity_calibration(struct iwl_priv *priv,
@@ -87,24 +86,5 @@ static inline void iwl_chain_noise_reset(struct iwl_priv *priv)
 	    priv->cfg->ops->utils->chain_noise_reset)
 		priv->cfg->ops->utils->chain_noise_reset(priv);
 }
-#else
-static inline void iwl_chain_noise_calibration(struct iwl_priv *priv,
-				struct iwl4965_notif_statistics *stat_resp)
-{
-}
-static inline void iwl_sensitivity_calibration(struct iwl_priv *priv,
-				struct iwl4965_notif_statistics *resp)
-{
-}
-static inline void iwl_init_sensitivity(struct iwl_priv *priv)
-{
-}
-static inline void iwl_chain_noise_reset(struct iwl_priv *priv)
-{
-}
-static inline void iwl_reset_run_time_calib(struct iwl_priv *priv)
-{
-}
-#endif
 
 #endif /* __iwl_calib_h__ */
