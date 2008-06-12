0cc647dd7ffdc6c7c4ec42ba78ce1d5db90a8db5
Kumar Gala <galak@kernel.crashing.org>
2008-06-12 08:05:09 -0500

    cpm_uart: Remove !CONFIG_PPC_CPM_NEW_BINDING code
    Now that arch/ppc is gone we always define CONFIG_PPC_CPM_NEW_BINDING so
    we can remove all the code associated with !CONFIG_PPC_CPM_NEW_BINDING.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- drivers/serial/cpm_uart/cpm_uart_cpm2.h
+++ drivers/serial/cpm_uart/cpm_uart_cpm2.h
@@ -13,16 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/cpm2.h>
 
-/* defines for IRQs */
-#ifndef CONFIG_PPC_CPM_NEW_BINDING
-#define SMC1_IRQ	SIU_INT_SMC1
-#define SMC2_IRQ	SIU_INT_SMC2
-#define SCC1_IRQ	SIU_INT_SCC1
-#define SCC2_IRQ	SIU_INT_SCC2
-#define SCC3_IRQ	SIU_INT_SCC3
-#define SCC4_IRQ	SIU_INT_SCC4
-#endif
-
 static inline void cpm_set_brg(int brg, int baud)
 {
 	cpm_setbrg(brg, baud);
