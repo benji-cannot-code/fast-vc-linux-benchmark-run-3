7d0154252a37034fe99bb0e228534f541cfff590
Kumar Gala <galak@kernel.crashing.org>
2008-06-12 08:32:13 -0500

    fs_enet: Remove !CONFIG_PPC_CPM_NEW_BINDING code
    Now that arch/ppc is gone we always define CONFIG_PPC_CPM_NEW_BINDING so
    we can remove all the code associated with !CONFIG_PPC_CPM_NEW_BINDING.
    
    Also fixed some asm/of_platform.h to linux/of_platform.h (and of_device.h)
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- drivers/net/fs_enet/mac-fec.c
+++ drivers/net/fs_enet/mac-fec.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/fs.h>
 #include <linux/platform_device.h>
+#include <linux/of_device.h>
 
 #include <asm/irq.h>
 #include <asm/uaccess.h>
@@ -42,10 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/mpc8xx.h>
 #include <asm/cpm1.h>
-#endif
-
-#ifdef CONFIG_PPC_CPM_NEW_BINDING
-#include <asm/of_device.h>
 #endif
 
 #include "fs_enet.h"
@@ -100,7 +97,6 @@ static int whack_reset(fec_t __iomem *fecp)
 
 static int do_pd_setup(struct fs_enet_private *fep)
 {
-#ifdef CONFIG_PPC_CPM_NEW_BINDING
 	struct of_device *ofdev = to_of_device(fep->dev);
 
 	fep->interrupt = of_irq_to_resource(ofdev->node, 0, NULL);
@@ -112,23 +108,6 @@ static int do_pd_setup(struct fs_enet_private *fep)
 		return -EINVAL;
 
 	return 0;
-#else
-	struct platform_device *pdev = to_platform_device(fep->dev);
-	struct resource	*r;
-
-	/* Fill out IRQ field */
-	fep->interrupt = platform_get_irq_byname(pdev,"interrupt");
-	if (fep->interrupt < 0)
-		return -EINVAL;
-
-	r = platform_get_resource_byname(pdev, IORESOURCE_MEM, "regs");
-	fep->fec.fecp = ioremap(r->start, r->end - r->start + 1);
-
-	if(fep->fec.fecp == NULL)
-		return -EINVAL;
-
-	return 0;
-#endif
 }
 
 #define FEC_NAPI_RX_EVENT_MSK	(FEC_ENET_RXF | FEC_ENET_RXB)
