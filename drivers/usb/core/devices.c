2f99bc7b5e1e9e1d1d0ec01b49e7e572d9e2c3af
Felipe Balbi <felipe.balbi@nokia.com>
2008-06-12 10:49:47 +0300

    usb: hub: add check for unsupported bus topology
    We can't allow hubs on the 7th tier as they would allow
    devices on the 8th tier.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@nokia.com>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/devices.c
+++ drivers/usb/core/devices.c
@@ -62,8 +62,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "usb.h"
 #include "hcd.h"
 
-#define MAX_TOPO_LEVEL		6
-
 /* Define ALLOW_SERIAL_NUMBER if you want to see the serial number of devices */
 #define ALLOW_SERIAL_NUMBER
 
--- drivers/usb/core/hcd.h
+++ drivers/usb/core/hcd.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/rwsem.h>
 
+#define MAX_TOPO_LEVEL		6
+
 /* This file contains declarations of usbcore internals that are mostly
  * used or exposed by Host Controller Drivers.
  */
