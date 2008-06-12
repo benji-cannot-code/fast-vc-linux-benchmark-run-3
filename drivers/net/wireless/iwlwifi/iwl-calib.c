bd8991284187275a6022ace6df9b0f89db0881f6
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2008-06-12 09:47:18 +0800

    iwlwifi: general code clean up
    This patch cleans up iwlwifi's code: Add missing include, remove empty
    lines etc...
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-calib.c
+++ drivers/net/wireless/iwlwifi/iwl-calib.c
@@ -61,13 +61,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************/
 
-#include <linux/kernel.h>
 #include <net/mac80211.h>
 
 #include "iwl-dev.h"
 #include "iwl-core.h"
 #include "iwl-calib.h"
-#include "iwl-eeprom.h"
 
 /* "false alarms" are signals that our DSP tries to lock onto,
  *   but then determines that they are either noise, or transmissions
--- drivers/net/wireless/iwlwifi/iwl-calib.h
+++ drivers/net/wireless/iwlwifi/iwl-calib.h
@@ -63,14 +63,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_calib_h__
 #define __iwl_calib_h__
 
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/version.h>
-
-#include <net/mac80211.h>
-#include "iwl-eeprom.h"
-#include "iwl-core.h"
 #include "iwl-dev.h"
+#include "iwl-core.h"
+#include "iwl-commands.h"
 
 void iwl_chain_noise_calibration(struct iwl_priv *priv,
 				struct iwl4965_notif_statistics *stat_resp);
