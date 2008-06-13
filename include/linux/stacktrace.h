1758649b6bba0f441b2bdfc96c936fdc107354d5
Vegard Nossum <vegard.nossum@gmail.com>
2008-06-13 15:31:11 +0200

    stacktrace: add forward-declaration struct task_struct
    This is needed if the header is to be free-standing.
    
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- include/linux/stacktrace.h
+++ include/linux/stacktrace.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct task_struct;
 
 #ifdef CONFIG_STACKTRACE
+struct task_struct;
+
 struct stack_trace {
 	unsigned int nr_entries, max_entries;
 	unsigned long *entries;
