cf0cf413153816d254da1d6e0563c5359d743a3a
Becky Bruce <becky.bruce@freescale.com>
2008-06-14 09:41:42 +1000

    powerpc: Change BAT code to use phys_addr_t
    Currently, the physical address is an unsigned long, but it should
    be phys_addr_t in set_bat, [v/p]_mapped_by_bat.  Also, create a
    macro that can convert a large physical address into the correct
    format for programming the BAT registers.
    
    Signed-off-by: Becky Bruce <becky.bruce@freescale.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/mmu-hash32.h
+++ include/asm-powerpc/mmu-hash32.h
@@ -29,6 +29,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BPP_RW	0x02		/* Read/write */
 
 #ifndef __ASSEMBLY__
+/* Contort a phys_addr_t into the right format/bits for a BAT */
+#ifdef CONFIG_PHYS_64BIT
+#define BAT_PHYS_ADDR(x) ((u32)((x & 0x00000000fffe0000ULL) | \
+				((x & 0x0000000e00000000ULL) >> 24) | \
+				((x & 0x0000000100000000ULL) >> 30)))
+#else
+#define BAT_PHYS_ADDR(x) (x)
+#endif
+
 struct ppc_bat {
 	struct {
 		unsigned long bepi:15;	/* Effective page index (virtual address) */
