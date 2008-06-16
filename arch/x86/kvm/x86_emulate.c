ec0f4a123ce7b3f9fabb9ca1d3aa19b330bafe74
Avi Kivity <avi@qumranet.com>
2008-06-15 21:13:41 -0700

    KVM: x86 emulator: handle undecoded rex.b with r/m = 5 in certain cases
    x86_64 does not decode rex.b in certain cases, where the r/m field = 5.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- arch/x86/kvm/x86_emulate.c
+++ arch/x86/kvm/x86_emulate.c
@@ -751,6 +751,7 @@ static int decode_modrm(struct x86_emulate_ctxt *ctxt,
 
 			switch (base_reg) {
 			case 5:
+			case 13:
 				if (c->modrm_mod != 0)
 					c->modrm_ea += c->regs[base_reg];
 				else
@@ -768,6 +769,7 @@ static int decode_modrm(struct x86_emulate_ctxt *ctxt,
 			}
 			break;
 		case 5:
+		case 13:
 			if (c->modrm_mod != 0)
 				c->modrm_ea += c->regs[c->modrm_rm];
 			else if (ctxt->mode == X86EMUL_MODE_PROT64)
