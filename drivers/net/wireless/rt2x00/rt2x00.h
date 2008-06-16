dff5a69ad7428848a3b9c7f86734934935d50ac7
Ivo van Doorn <ivdoorn@gmail.com>
2008-06-16 19:58:00 +0200

    rt2x00: Release rt2x00 2.1.8
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00.h
+++ drivers/net/wireless/rt2x00/rt2x00.h
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Module information.
  */
-#define DRV_VERSION	"2.1.7"
+#define DRV_VERSION	"2.1.8"
 #define DRV_PROJECT	"http://rt2x00.serialmonkey.com"
 
 /*
