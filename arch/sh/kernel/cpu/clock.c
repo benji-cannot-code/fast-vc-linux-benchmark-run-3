cbf6ccd3480fe64bfa549d6cade6a48e722258a1
Adrian Bunk <bunk@kernel.org>
2008-06-18 03:36:50 +0300

    sh/kernel/ cleanups
    This patch contains the following cleanups:
    - make the following needlessly global code static:
      - cf-enabler.c: cf_init()
      - cpu/clock.c: __clk_enable()
      - cpu/clock.c: __clk_disable()
      - process_32.c: default_idle()
      - time_32.c: struct clocksource_sh
      - timers/timer-tmu.c: struct tmu_timer_ops
    - remove the following unused functions (no CONFIG_BLK_DEV_FD on sh):
      - process_{32,64}.c: disable_hlt()
      - process_{32,64}.c: enable_hlt()
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpu/clock.c
+++ arch/sh/kernel/cpu/clock.c
@@ -89,7 +89,7 @@ static void propagate_rate(struct clk *clk)
 	}
 }
 
-int __clk_enable(struct clk *clk)
+static int __clk_enable(struct clk *clk)
 {
 	/*
 	 * See if this is the first time we're enabling the clock, some
@@ -112,7 +112,6 @@ int __clk_enable(struct clk *clk)
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(__clk_enable);
 
 int clk_enable(struct clk *clk)
 {
@@ -132,7 +131,7 @@ static void clk_kref_release(struct kref *kref)
 	/* Nothing to do */
 }
 
-void __clk_disable(struct clk *clk)
+static void __clk_disable(struct clk *clk)
 {
 	int count = kref_put(&clk->kref, clk_kref_release);
 
@@ -144,7 +143,6 @@ void __clk_disable(struct clk *clk)
 			clk->ops->disable(clk);
 	}
 }
-EXPORT_SYMBOL_GPL(__clk_disable);
 
 void clk_disable(struct clk *clk)
 {
