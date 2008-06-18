06fae80b9e5433e0341f745d6cb324c6e8cfaf16
Rami Rosen <ramirose@gmail.com>
2008-06-18 00:51:09 -0700

    ipv6: minor cleanup in net/ipv6/tcp_ipv6.c [RESEND ].
    In net/ipv6/tcp_ipv6.c:
    
      - Remove unneeded tcp_v6_send_check() declaration.
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -71,8 +71,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void	tcp_v6_send_reset(struct sock *sk, struct sk_buff *skb);
 static void	tcp_v6_reqsk_send_ack(struct sk_buff *skb, struct request_sock *req);
-static void	tcp_v6_send_check(struct sock *sk, int len,
-				  struct sk_buff *skb);
 
 static int	tcp_v6_do_rcv(struct sock *sk, struct sk_buff *skb);
 
