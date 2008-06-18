05363aaad5218f2e7767beb0f8d2b07be28fa0a2
Dmitri Vorobiev <dmitri.vorobiev@movial.fi>
2008-06-18 10:18:23 +0300

    [MIPS] Add an appropriate header into display.c
    The following errors were caught by sparse:
    
    >>>>>>>>>>>
    
    arch/mips/mips-boards/generic/display.c:30:6: warning: symbol
    'mips_display_message' was not declared. Should it be static?
    
    arch/mips/mips-boards/generic/display.c:58:6: warning: symbol
    'mips_scroll_message' was not declared. Should it be static?
    
    >>>>>>>>>>>
    
    This patch includes the asm/mips-boards/prom.h header file into
    arch/mips/mips-boards/generic/display.c. This adds the needed
    function declarations, and the errors are gone.
    
    Compile-tested using defconfigs for Malta, Atlas and SEAD boards.
    Runtime test was successfully performed by booting a Malta 4Kc
    board up to the shell prompt.
    
    Signed-off-by: Dmitri Vorobiev <dmitri.vorobiev@movial.fi>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/generic/display.c
+++ arch/mips/mips-boards/generic/display.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <asm/io.h>
 #include <asm/mips-boards/generic.h>
+#include <asm/mips-boards/prom.h>
 
 extern const char display_string[];
 static unsigned int display_count;
