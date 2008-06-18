8c327e3cbb6e750aeb81675e11974603c8f2d2ff
Jiri Slaby <jirislaby@gmail.com>
2008-06-18 23:36:49 +0200

    HID: move apple quirks
    Move them from the core code to a separate driver.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/usbhid/usbmouse.c
+++ drivers/hid/usbhid/usbmouse.c
@@ -32,6 +32,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb/input.h>
 #include <linux/hid.h>
 
+/* for apple IDs */
+#ifdef CONFIG_USB_HID_MODULE
+#include "../hid-ids.h"
+#endif
+
 /*
  * Version Information
  */
