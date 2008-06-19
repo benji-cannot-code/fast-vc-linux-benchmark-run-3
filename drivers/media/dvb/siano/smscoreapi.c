66b5895a9546aa1dc6bf88cd963c62a821573ea5
Michael Krufky <mkrufky@linuxtv.org>
2008-06-19 20:59:08 -0300

    V4L/DVB (8295): sms1xxx: add debug module option, to enable debug messages
    All dmesg spam turned off by default, for now.  Values for debug:
    
    (info=1, adv=2 (or-able)
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/siano/smscoreapi.c
+++ drivers/media/dvb/siano/smscoreapi.c
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "smscoreapi.h"
 
+int sms_debug;
+module_param_named(debug, sms_debug, int, 0644);
+MODULE_PARM_DESC(debug, "set debug level (info=1, adv=2 (or-able))");
+
 #define PERROR(fmt, args...)\
 	sms_err("smscore error: line %d- %s(): " fmt, \
 		__LINE__,  __func__, ## args)
