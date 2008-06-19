5a271912214a01b385c0e3a5e537aee17857a14b
Eric Miao <eric.miao@marvell.com>
2008-06-19 17:39:03 +0800

    [NET] smc91x: add SMC91X_NOWAIT flag to platform data
    And also favors the usage of SMC91X_NOWAIT over the hardcoded SMC_NOWAIT
    by converting "nowait" (module parameter overridable) to platform flag.
    
    There are several possibilities:
    
      1. platform data present - preferred and use as is
      2. platform data absent  - use "nowait", it can be:
           a. SMC_NOWAIT if defined
           b. default to 0 if SMC_NOWAIT isn't defined
           c. overriden by module parameter
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>
    Acked-by: Nicolas Pitre <nico@cam.org>
    Acked-by: Jeff Garzik <jgarzik@pobox.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/linux/smc91x.h
+++ include/linux/smc91x.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SMC91X_USE_16BIT (1 << 1)
 #define SMC91X_USE_32BIT (1 << 2)
 
+#define SMC91X_NOWAIT		(1 << 3)
+
 struct smc91x_platdata {
 	unsigned long flags;
 };
