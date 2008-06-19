24fc711761af3cf74a4cedd20fd8ca221a7cdc11
Ingo Molnar <mingo@elte.hu>
2008-06-19 13:28:11 +0200

    x86, MM: virtual address debug, cleanups
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/mmdebug.h
+++ include/linux/mmdebug.h
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_DEBUG_VM
 #define VM_BUG_ON(cond) BUG_ON(cond)
 #else
-#define VM_BUG_ON(cond) do { } while(0)
+#define VM_BUG_ON(cond) do { } while (0)
 #endif
 
 #ifdef CONFIG_DEBUG_VIRTUAL
 #define VIRTUAL_BUG_ON(cond) BUG_ON(cond)
 #else
-#define VIRTUAL_BUG_ON(cond) do { } while(0)
+#define VIRTUAL_BUG_ON(cond) do { } while (0)
 #endif
 
 #endif
