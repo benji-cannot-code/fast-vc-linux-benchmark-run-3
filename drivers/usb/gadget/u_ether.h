b55539fcedd6d3e2d128789acd8f4442856f1543
David Brownell <dbrownell@users.sourceforge.net>
2008-06-19 18:20:04 -0700

    usb ethernet gadget: split RNDIS function
    This is a RNDIS function driver, extracted from the all-in-one
    Ethernet gadget driver.
    
    Lightly tested ... there seems to be a pre-existing problem when
    talking to Windows XP SP2, not quite sure what's up with that yet.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/u_ether.h
+++ drivers/usb/gadget/u_ether.h
@@ -35,10 +35,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * This represents the USB side of an "ethernet" link, managed by a USB
  * function which provides control and (maybe) framing.  Two functions
- * in different configurations could share the same ethernet link/netdev.
+ * in different configurations could share the same ethernet link/netdev,
+ * using different host interaction models.
  *
- * There is currently a limitation that one instance of this function
- * may be present in any given configuration.
+ * There is a current limitation that only one instance of this link may
+ * be present in any given configuration.  When that's a problem, network
+ * layer facilities can be used to package multiple logical links on this
+ * single "physical" one.
  */
 struct gether {
 	struct usb_function		func;
@@ -107,5 +110,19 @@ static inline bool can_support_ecm(struct usb_gadget *gadget)
 /* each configuration may bind one instance of an ethernet link */
 int geth_bind_config(struct usb_configuration *c, u8 ethaddr[ETH_ALEN]);
 int ecm_bind_config(struct usb_configuration *c, u8 ethaddr[ETH_ALEN]);
+
+#ifdef CONFIG_USB_ETH_RNDIS
+
+int rndis_bind_config(struct usb_configuration *c, u8 ethaddr[ETH_ALEN]);
+
+#else
+
+static inline int
+rndis_bind_config(struct usb_configuration *c, u8 ethaddr[ETH_ALEN])
+{
+	return 0;
+}
+
+#endif
 
 #endif /* __U_ETHER_H */
