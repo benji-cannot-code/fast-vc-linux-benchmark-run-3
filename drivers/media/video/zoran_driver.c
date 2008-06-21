2de104e3aaa320a7ee7b48841e92d1c8c325b10e
Huang Weiyi <weiyi.huang@gmail.com>
2008-06-21 14:30:33 -0300

    V4L/DVB (8095): zoran_driver.c: Removed duplicated include
    Removed duplicated include file <asm/byteorder.h> in
    drivers/media/video/zoran_driver.c.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/zoran_driver.c
+++ drivers/media/video/zoran_driver.c
@@ -95,7 +95,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				V4L2_CAP_VIDEO_OVERLAY \
 			      )
 
-#include <asm/byteorder.h>
 
 #if defined(CONFIG_VIDEO_V4L1_COMPAT)
 #define ZFMT(pal, fcc, cs) \
