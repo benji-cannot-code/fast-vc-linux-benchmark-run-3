de3962e9c17fc3298211d4e4fc75f432de8e2ed3
Michael Ellerman <michael@ellerman.id.au>
2008-06-24 11:32:39 +1000

    powerpc: Consolidate CPU and firmware feature fixup macros
    The CPU and firmware feature fixup macros are currently spread across
    three files, firmware.h, cputable.h and asm-compat.h.  Consolidate them
    into their own file, feature-fixups.h
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Acked-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/cputable.h
+++ include/asm-powerpc/cputable.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 
 #include <asm/asm-compat.h>
+#include <asm/feature-fixups.h>
 
 #ifndef __ASSEMBLY__
 
@@ -489,19 +490,6 @@ static inline int cpu_has_feature(unsigned long feature)
 }
 
 #endif /* !__ASSEMBLY__ */
-
-#ifdef __ASSEMBLY__
-
-#define BEGIN_FTR_SECTION_NESTED(label)	label:
-#define BEGIN_FTR_SECTION		BEGIN_FTR_SECTION_NESTED(97)
-#define END_FTR_SECTION_NESTED(msk, val, label) \
-	MAKE_FTR_SECTION_ENTRY(msk, val, label, __ftr_fixup)
-#define END_FTR_SECTION(msk, val)		\
-	END_FTR_SECTION_NESTED(msk, val, 97)
-
-#define END_FTR_SECTION_IFSET(msk)	END_FTR_SECTION((msk), (msk))
-#define END_FTR_SECTION_IFCLR(msk)	END_FTR_SECTION((msk), 0)
-#endif /* __ASSEMBLY__ */
 
 #endif /* __KERNEL__ */
 #endif /* __ASM_POWERPC_CPUTABLE_H */
