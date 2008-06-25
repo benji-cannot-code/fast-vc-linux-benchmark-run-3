567eaaf7ab012dcbfd9b1e1e8f0714dbb87ee810
Sylver Bruneau <sylver.bruneau@googlemail.com>
2008-06-25 23:41:09 +0200

    [ARM] Orion: fix for tsx09-common.c compilation problem
    In some cases, compilation of the tsx09 common file was failing due
    to an incomplete list of includes.
    
    Signed-off-by: Sylver Bruneau <sylver.bruneau@googlemail.com>
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>

--- arch/arm/mach-orion5x/tsx09-common.c
+++ arch/arm/mach-orion5x/tsx09-common.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/pci.h>
 #include <linux/mv643xx_eth.h>
+#include <linux/timex.h>
 #include <linux/serial_reg.h>
 #include "tsx09-common.h"
 
