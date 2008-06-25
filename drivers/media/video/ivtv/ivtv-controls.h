0c344ac2ac0bf3db04539b82103b036c69a69b3c
Hans Verkuil <hverkuil@xs4all.nl>
2008-06-25 06:32:44 -0300

    V4L/DVB (8113): ivtv/cx18: remove s/g_ctrl, now all controls are handled through s/g_ext_ctrl
    videodev converts old-style controls to an extended control so the ivtv and
    cx18 drivers no longer have to handle both.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/ivtv/ivtv-controls.h
+++ drivers/media/video/ivtv/ivtv-controls.h
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IVTV_CONTROLS_H
 
 int ivtv_queryctrl(struct file *file, void *fh, struct v4l2_queryctrl *a);
-int ivtv_g_ctrl(struct file *file, void *fh, struct v4l2_control *a);
-int ivtv_s_ctrl(struct file *file, void *fh, struct v4l2_control *a);
 int ivtv_g_ext_ctrls(struct file *file, void *fh, struct v4l2_ext_controls *a);
 int ivtv_s_ext_ctrls(struct file *file, void *fh, struct v4l2_ext_controls *a);
 int ivtv_try_ext_ctrls(struct file *file, void *fh, struct v4l2_ext_controls *a);
