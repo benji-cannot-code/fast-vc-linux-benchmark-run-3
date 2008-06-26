07d4657141d6e6fb7d40d1e8b310dc43625f89e5
Philipp Zabel <philipp.zabel@gmail.com>
2008-06-26 21:03:54 +0100

    [ARM] 5125/1: magician: move gpio pin configuration into __initdata section
    The pin configuration array is only used during board init.
    
    Signed-off-by: Philipp Zabel <philipp.zabel@gmail.com>
    Acked-by: Eric Miao <eric.miao@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/magician.c
+++ arch/arm/mach-pxa/magician.c
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "devices.h"
 #include "generic.h"
 
-static unsigned long magician_pin_config[] = {
+static unsigned long magician_pin_config[] __initdata = {
 
 	/* SDRAM and Static Memory I/O Signals */
 	GPIO20_nSDCS_2,
