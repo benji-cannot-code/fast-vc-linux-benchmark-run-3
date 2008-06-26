834f00418dcbce22e4bcf59c8dae91465dddcd64
Philipp Zabel <philipp.zabel@gmail.com>
2008-06-26 21:04:31 +0100

    [ARM] 5126/1: magician: remove superfluous mtd includes
    These were only needed for hardcoded flash partition tables, which were
    never submitted. It is better to have the bootloader pass the partition
    table to the kernel instead.
    
    Signed-off-by: Philipp Zabel <philipp.zabel@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/magician.c
+++ arch/arm/mach-pxa/magician.c
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input.h>
 #include <linux/mfd/htc-egpio.h>
 #include <linux/mfd/htc-pasic3.h>
-#include <linux/mtd/mtd.h>
-#include <linux/mtd/map.h>
 #include <linux/mtd/physmap.h>
 #include <linux/pda_power.h>
 #include <linux/pwm_backlight.h>
