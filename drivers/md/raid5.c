657a87b6f132e980fa851b6ae2f4c36dfa4a269d
Dan Williams <dan.j.williams@intel.com>
2008-06-28 08:31:53 +1000

    md: use stripe_head_state in ops_run_io()
    From: Dan Williams <dan.j.williams@intel.com>
    
    In handle_stripe after taking sh->lock we sample some bits into 's' (struct
    stripe_head_state):
    
    	s.syncing = test_bit(STRIPE_SYNCING, &sh->state);
    	s.expanding = test_bit(STRIPE_EXPAND_SOURCE, &sh->state);
    	s.expanded = test_bit(STRIPE_EXPAND_READY, &sh->state);
    
    Use these values from 's' in ops_run_io() rather than re-sampling the bits.
    This ensures a consistent snapshot (as seen under sh->lock) is used.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Neil Brown <neilb@suse.de>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -391,7 +391,7 @@ raid5_end_read_request(struct bio *bi, int error);
 static void
 raid5_end_write_request(struct bio *bi, int error);
 
-static void ops_run_io(struct stripe_head *sh)
+static void ops_run_io(struct stripe_head *sh, struct stripe_head_state *s)
 {
 	raid5_conf_t *conf = sh->raid_conf;
 	int i, disks = sh->disks;
@@ -426,9 +426,7 @@ static void ops_run_io(struct stripe_head *sh)
 		rcu_read_unlock();
 
 		if (rdev) {
-			if (test_bit(STRIPE_SYNCING, &sh->state) ||
-				test_bit(STRIPE_EXPAND_SOURCE, &sh->state) ||
-				test_bit(STRIPE_EXPAND_READY, &sh->state))
+			if (s->syncing || s->expanding || s->expanded)
 				md_sync_acct(rdev->bdev, STRIPE_SECTORS);
 
 			set_bit(STRIPE_IO_STARTED, &sh->state);
@@ -2903,10 +2901,9 @@ static void handle_stripe5(struct stripe_head *sh)
 	if (pending)
 		raid5_run_ops(sh, pending);
 
-	ops_run_io(sh);
+	ops_run_io(sh, &s);
 
 	return_io(return_bi);
-
 }
 
 static void handle_stripe6(struct stripe_head *sh, struct page *tmp_page)
