909b04245c844cb16baecc40c815717cbe8da962
Devin Heitmueller <devin.heitmueller@gmail.com>
2008-06-28 08:57:06 -0300

    V4L/DVB (8123): Add support for em2860 based PointNix Intra-Oral Camera
    em28xx-cards.c
    em28xx-input.c
    em28xx-video.c
    em28xx.h
     - Add support for the PointNix Intra-Oral Camera, which required addition of
       a construct for reading the "snapshot" button (provided on the em2860 and
       em2880 chips, but this is the first case where I have seen it actually used
       in a product). The button is wired to pin 56 on the em2880.
    
       http://www.pointnix.com/ENG/dental/product_02.asp
    
       Thanks to Roberto Mantovani <rmantovani@libero.it> for testing the changes
    
    Signed-off-by: Devin Heitmueller <devin.heitmueller@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/em28xx/em28xx.h
+++ drivers/media/video/em28xx/em28xx.h
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2880_BOARD_HAUPPAUGE_WINTV_HVR_950	16
 #define EM2880_BOARD_PINNACLE_PCTV_HD_PRO	17
 #define EM2880_BOARD_HAUPPAUGE_WINTV_HVR_900_R2	18
+#define EM2860_BOARD_POINTNIX_INTRAORAL_CAMERA  19
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
@@ -250,6 +251,7 @@ struct em28xx_board {
 	unsigned int has_12mhz_i2s:1;
 	unsigned int max_range_640_480:1;
 	unsigned int has_dvb:1;
+	unsigned int has_snapshot_button:1;
 
 	enum em28xx_decoder decoder;
 
@@ -329,6 +331,7 @@ struct em28xx {
 	unsigned int has_12mhz_i2s:1;
 	unsigned int max_range_640_480:1;
 	unsigned int has_dvb:1;
+	unsigned int has_snapshot_button:1;
 
 	/* Some older em28xx chips needs a waiting time after writing */
 	unsigned int wait_after_write;
@@ -419,6 +422,11 @@ struct em28xx {
 	/* Caches GPO and GPIO registers */
 	unsigned char	reg_gpo, reg_gpio;
 
+	/* Snapshot button */
+	char snapshot_button_path[30];	/* path of the input dev */
+	struct input_dev *sbutton_input_dev;
+	struct delayed_work sbutton_query_work;
+
 	struct em28xx_dvb *dvb;
 };
 
@@ -484,6 +492,8 @@ int em28xx_get_key_terratec(struct IR_i2c *ir, u32 *ir_key, u32 *ir_raw);
 int em28xx_get_key_em_haup(struct IR_i2c *ir, u32 *ir_key, u32 *ir_raw);
 int em28xx_get_key_pinnacle_usb_grey(struct IR_i2c *ir, u32 *ir_key,
 				     u32 *ir_raw);
+void em28xx_register_snapshot_button(struct em28xx *dev);
+void em28xx_deregister_snapshot_button(struct em28xx *dev);
 
 /* printk macros */
 
