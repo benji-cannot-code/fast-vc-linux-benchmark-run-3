ddcf2ae73428cba5277b70187725e0fab76b9f3e
Michael Krufky <mkrufky@linuxtv.org>
2008-06-28 17:09:28 -0300

    V4L/DVB (8303): sms1xxx: update MODULE_DESCRIPTION
    set MODULE_DESCRIPTION to "Driver for the Siano SMS1XXX USB dongle"
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/siano/smsusb.c
+++ drivers/media/dvb/siano/smsusb.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- *  Driver for the Siano SMS10xx USB dongle
+ *  Driver for the Siano SMS1xxx USB dongle
  *
  *  author: Anatoly Greenblat
  *
