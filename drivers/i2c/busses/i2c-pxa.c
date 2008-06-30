f48fd010dbe4ee3ce5026521c236fa39371e0ff5
Philipp Zabel <philipp.zabel@gmail.com>
2008-06-30 18:08:11 +0100

    [ARM] 5135/1: pxa: drop superfluous asm/arch/pxa2xx-gpio.h includes
    Both i2c-pxa.c and irq.c still include pxa2xx-gpio.h although is is not
    needed anymore.
    
    Signed-off-by: Philipp Zabel <philipp.zabel@gmail.com>
    Acked-by: Eric Miao <eric.miao@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/i2c/busses/i2c-pxa.c
+++ drivers/i2c/busses/i2c-pxa.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/arch/i2c.h>
 #include <asm/arch/pxa-regs.h>
-#include <asm/arch/pxa2xx-gpio.h>
 
 struct pxa_i2c {
 	spinlock_t		lock;
