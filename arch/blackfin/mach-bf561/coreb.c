f38a5e1c004e5f0f776d9373b47adf02bf74ad97
Jonathan Corbet <corbet@lwn.net>
2008-06-30 16:45:15 -0600

    bf561-coreb: BKL unneeded for open()
    According to Mike Frysinger:
    
    	http://permalink.gmane.org/gmane.linux.kernel/699279
    
    open() does not need the BKL, so take it back out.

--- arch/blackfin/mach-bf561/coreb.c
+++ arch/blackfin/mach-bf561/coreb.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/ioport.h>
 #include <linux/module.h>
-#include <linux/smp_lock.h>
 #include <linux/uaccess.h>
 #include <linux/fs.h>
 #include <asm/dma.h>
@@ -196,9 +195,9 @@ static loff_t coreb_lseek(struct file *file, loff_t offset, int origin)
 	return ret;
 }
 
+/* No BKL needed here */
 static int coreb_open(struct inode *inode, struct file *file)
 {
-	lock_kernel();
 	spin_lock_irq(&coreb_lock);
 
 	if (coreb_status & COREB_IS_OPEN)
@@ -207,12 +206,10 @@ static int coreb_open(struct inode *inode, struct file *file)
 	coreb_status |= COREB_IS_OPEN;
 
 	spin_unlock_irq(&coreb_lock);
-	unlock_kernel();
 	return 0;
 
  out_busy:
 	spin_unlock_irq(&coreb_lock);
-	unlock_kernel();
 	return -EBUSY;
 }
 
