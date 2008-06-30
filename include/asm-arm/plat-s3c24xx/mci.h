05de6967b3ca96ea782737ef992d5277366fc09b
Ben Dooks <ben-linux@fluff.org>
2008-06-30 22:40:30 +0100

    MMC: S3C24XX: Add support to invert write protect line
    Support for inverting the sense of the MMC driver's write
    protect detection line.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- include/asm-arm/plat-s3c24xx/mci.h
+++ include/asm-arm/plat-s3c24xx/mci.h
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ARCH_MCI_H
 
 struct s3c24xx_mci_pdata {
+	unsigned int	wprotect_invert : 1;
+
 	unsigned int	gpio_detect;
 	unsigned int	gpio_wprotect;
 	unsigned long	ocr_avail;
