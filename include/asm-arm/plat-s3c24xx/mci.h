823093e5127b04180367dc7cbc59b167563492d3
Ben Dooks <ben-linux@fluff.org>
2008-06-30 22:40:29 +0100

    MMC: S3C24XX: Add platform data for MMC/SD driver
    This patch adds platform data support to the s3mci driver.  This allows
    flexible board-specific configuration of set_power, card detect and read only
    pins.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

---
+++ include/asm-arm/plat-s3c24xx/mci.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _ARCH_MCI_H
+#define _ARCH_MCI_H
+
+struct s3c24xx_mci_pdata {
+	unsigned int	gpio_detect;
+	unsigned int	gpio_wprotect;
+	unsigned long	ocr_avail;
+	void		(*set_power)(unsigned char power_mode,
+				     unsigned short vdd);
+};
+
+#endif /* _ARCH_NCI_H */
