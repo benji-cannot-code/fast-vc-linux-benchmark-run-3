c508d872cd3540a95d5d898757e4f7314d4c2675
Tony Breeds <tony@bakeyournoodle.com>
2008-07-01 11:30:06 +1000

    powerpc: Fix building of arch/powerpc/mm/mem.o when MEMORY_HOTPLUG=y and SPARSEMEM=n
    Currently the kernel fails to build with the above config options with:
      CC      arch/powerpc/mm/mem.o
    arch/powerpc/mm/mem.c: In function 'arch_add_memory':
    arch/powerpc/mm/mem.c:130: error: implicit declaration of function 'create_section_mapping'
    
    This explicitly includes asm/sparsemem.h in arch/powerpc/mm/mem.c and
    moves the guards in include/asm-powerpc/sparsemem.h to protect the
    SPARSEMEM specific portions only.
    
    Signed-off-by: Tony Breeds <tony@bakeyournoodle.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/mm/mem.c
+++ arch/powerpc/mm/mem.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/btext.h>
 #include <asm/tlb.h>
 #include <asm/sections.h>
+#include <asm/sparsemem.h>
 #include <asm/vdso.h>
 #include <asm/fixmap.h>
 
