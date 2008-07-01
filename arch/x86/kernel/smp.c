d53f2277654436f88385fdab5d86762cd731ee91
Jens Axboe <jens.axboe@oracle.com>
2008-07-01 13:12:04 +0200

    generic-ipi: fixlet
    create proper stackframe.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/smp.c
+++ arch/x86/kernel/smp.c
@@ -199,7 +199,7 @@ void smp_call_function_interrupt(struct pt_regs *regs)
 	irq_exit();
 }
 
-void smp_call_function_single_interrupt(void)
+void smp_call_function_single_interrupt(struct pt_regs *regs)
 {
 	ack_APIC_irq();
 	irq_enter();
