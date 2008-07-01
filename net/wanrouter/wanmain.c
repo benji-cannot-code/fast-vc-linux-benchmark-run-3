2f68f2b48953f2a9ae8038a1d27ccaebc3397b61
Krzysztof Hałasa <khc@pm.waw.pl>
2008-07-01 20:24:10 +0200

    wanmain.c doesn't need syncppp.h
    Signed-off-by: Krzysztof Hałasa <khc@pm.waw.pl>

--- net/wanrouter/wanmain.c
+++ net/wanrouter/wanmain.c
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>	/* vmalloc, vfree */
 #include <asm/uaccess.h>        /* copy_to/from_user */
 #include <linux/init.h>         /* __initfunc et al. */
-#include <net/syncppp.h>
 
 #define KMEM_SAFETYZONE 8
 
