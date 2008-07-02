0e4ece1142bdb28d620c50859a7c7fa48db7bffa
Christof Schmitt <christof.schmitt@de.ibm.com>
2008-07-02 10:56:34 +0200

    [SCSI] zfcp: Small QDIO cleanups
    QBUFF_PER_PAGE is only used inside the qdio module, so move it to
    zfcp_qdio.c
    
    zfcp_qdio_zero_sbals is now only used in the qdio module, so make it
    static.
    
    Signed-off-by: Christof Schmitt <christof.schmitt@de.ibm.com>
    Signed-off-by: Swen Schillig <swen@vnet.ibm.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/s390/scsi/zfcp_qdio.c
+++ drivers/s390/scsi/zfcp_qdio.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* FIXME(tune): free space should be one max. SBAL chain plus what? */
 #define ZFCP_QDIO_PCI_INTERVAL	(QDIO_MAX_BUFFERS_PER_Q \
 				- (ZFCP_MAX_SBALS_PER_REQ + 4))
+#define QBUFF_PER_PAGE		(PAGE_SIZE / sizeof(struct qdio_buffer))
 
 static int zfcp_qdio_buffers_enqueue(struct qdio_buffer **sbal)
 {
@@ -62,6 +63,16 @@ static void zfcp_qdio_handler_error(struct zfcp_adapter *adapter, u8 id)
 	zfcp_erp_adapter_reopen(adapter,
 				ZFCP_STATUS_ADAPTER_LINK_UNPLUGGED |
 				ZFCP_STATUS_COMMON_ERP_FAILED, id, NULL);
+}
+
+static void zfcp_qdio_zero_sbals(struct qdio_buffer *sbal[], int first, int cnt)
+{
+	int i, sbal_idx;
+
+	for (i = first; i < first + cnt; i++) {
+		sbal_idx = i % QDIO_MAX_BUFFERS_PER_Q;
+		memset(sbal[sbal_idx], 0, sizeof(struct qdio_buffer));
+	}
 }
 
 static void zfcp_qdio_int_req(struct ccw_device *cdev, unsigned int status,
@@ -364,22 +375,6 @@ int zfcp_qdio_send(struct zfcp_fsf_req *fsf_req)
 	req_q->first %= QDIO_MAX_BUFFERS_PER_Q;
 	req_q->pci_batch = pci_batch;
 	return 0;
-}
-
-/**
- * zfcp_qdio_zero_sbals - zero all sbals of the specified area and queue
- * @buf: pointer to array of SBALS
- * @first: integer specifying the SBAL number to start
- * @count: integer specifying the number of SBALS to process
- */
-void zfcp_qdio_zero_sbals(struct qdio_buffer *sbal[], int first, int count)
-{
-	int i, sbal_idx;
-
-	for (i = first; i < first + count; i++) {
-		sbal_idx = i % QDIO_MAX_BUFFERS_PER_Q;
-		memset(sbal[sbal_idx], 0, sizeof(struct qdio_buffer));
-	}
 }
 
 /**
