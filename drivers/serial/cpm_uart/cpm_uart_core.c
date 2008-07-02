669e76c249f0a32b626b5d7c95ae9a6b57b70984
Laurent Pinchart <laurentp@cse-semaphore.com>
2008-07-02 10:58:45 +0200

    cpm_uart: Support uart_wait_until_sent()
    Set port->fifosize to the software FIFO size, and update the port timeout
    when the baud rate is modified. SCC ports have an optional 32 byte hardware
    FIFO which is currently not taken into account, as there is no documented way
    to check when the FIFO becomes empty.
    
    Signed-off-by: Laurent Pinchart <laurentp@cse-semaphore.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- drivers/serial/cpm_uart/cpm_uart_core.c
+++ drivers/serial/cpm_uart/cpm_uart_core.c
@@ -492,6 +492,11 @@ static void cpm_uart_set_termios(struct uart_port *port,
 	}
 
 	/*
+	 * Update the timeout
+	 */
+	uart_update_timeout(port, termios->c_cflag, baud);
+
+	/*
 	 * Set up parity check flag
 	 */
 #define RELEVANT_IFLAG(iflag) (iflag & (IGNBRK|BRKINT|IGNPAR|PARMRK|INPCK))
@@ -939,6 +944,7 @@ static int cpm_uart_init_port(struct device_node *np,
 	pinfo->port.type = PORT_CPM;
 	pinfo->port.ops = &cpm_uart_pops,
 	pinfo->port.iotype = UPIO_MEM;
+	pinfo->port.fifosize = pinfo->tx_nrfifos * pinfo->tx_fifosize;
 	spin_lock_init(&pinfo->port.lock);
 
 	pinfo->port.irq = of_irq_to_resource(np, 0, NULL);
