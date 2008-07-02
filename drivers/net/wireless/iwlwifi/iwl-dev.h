a64f62212a9c39843c0d03d969830017537fa1b2
Helmut Schaa <hschaa@suse.de>
2008-07-02 12:17:06 +0200

    iwlwifi: fix typo which caused iwl_get_tx_fail_reason to ever return an empty string
    Signed-off-by: Helmut Schaa <hschaa@suse.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-dev.h
+++ drivers/net/wireless/iwlwifi/iwl-dev.h
@@ -1104,7 +1104,7 @@ static inline void iwl_txq_ctx_deactivate(struct iwl_priv *priv, int txq_id)
 	clear_bit(txq_id, &priv->txq_ctx_active_msk);
 }
 
-#ifdef CONFIG_IWLWIF_DEBUG
+#ifdef CONFIG_IWLWIFI_DEBUG
 const char *iwl_get_tx_fail_reason(u32 status);
 #else
 static inline const char *iwl_get_tx_fail_reason(u32 status) { return ""; }
