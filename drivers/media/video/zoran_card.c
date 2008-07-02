dc7f822acf088f5994c8978382b08a9a23bfea37
Jean Delvare <khali@linux-fr.org>
2008-07-02 15:59:29 -0300

    V4L/DVB (8315): zr36067: Delete dead code
    This has been commented out for years, it's about time to get rid of
    it.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/zoran_card.c
+++ drivers/media/video/zoran_card.c
@@ -359,14 +359,6 @@ i2cid_to_modulename (u16 i2c_id)
 	case I2C_DRIVERID_VPX3220:
 		name = "vpx3220";
 		break;
-/*	case I2C_DRIVERID_VPX3224:
-		name = "vpx3224";
-		break;
-	case I2C_DRIVERID_MSE3000:
-		name = "mse3000";
-		break;*/
-	default:
-		break;
 	}
 
 	return name;
@@ -386,8 +378,6 @@ codecid_to_modulename (u16 codecid)
 		break;
 	case CODEC_TYPE_ZR36016:
 		name = "zr36016";
-		break;
-	default:
 		break;
 	}
 
@@ -429,7 +419,6 @@ static struct card_info zoran_cards[NUM_CARDS] __devinitdata = {
 		.type = DC10_old,
 		.name = "DC10(old)",
 		.i2c_decoder = I2C_DRIVERID_VPX3220,
-		/*.i2c_encoder = I2C_DRIVERID_MSE3000,*/
 		.video_codec = CODEC_TYPE_ZR36050,
 		.video_vfe = CODEC_TYPE_ZR36016,
 
