916a64e162e77f2d62c7e1c3243c0306cb9ee22c
Rusty Russell <rusty@rustcorp.com.au>
2008-07-03 03:45:32 -0700

    tun: Interface to query tun/tap features.
    The problem with introducing checksum offload and gso to tun is they
    need to set dev->features to enable GSO and/or checksumming, which is
    supposed to be done before register_netdevice(), ie. as part of
    TUNSETIFF.
    
    Unfortunately, TUNSETIFF has always just ignored flags it doesn't
    understand, so there's no good way of detecting whether the kernel
    supports new IFF_ flags.
    
    This patch implements a TUNGETFEATURES ioctl which returns all the valid IFF
    flags.  It could be extended later to include other features.
    
    Here's an example program which uses it:
    
    #include <linux/if_tun.h>
    #include <sys/types.h>
    #include <sys/ioctl.h>
    #include <sys/stat.h>
    #include <fcntl.h>
    #include <err.h>
    #include <stdio.h>
    
    static struct {
    	unsigned int flag;
    	const char *name;
    } known_flags[] = {
    	{ IFF_TUN, "TUN" },
    	{ IFF_TAP, "TAP" },
    	{ IFF_NO_PI, "NO_PI" },
    	{ IFF_ONE_QUEUE, "ONE_QUEUE" },
    };
    
    int main()
    {
    	unsigned int features, i;
    
    	int netfd = open("/dev/net/tun", O_RDWR);
    	if (netfd < 0)
    		err(1, "Opening /dev/net/tun");
    
    	if (ioctl(netfd, TUNGETFEATURES, &features) != 0) {
    		printf("Kernel does not support TUNGETFEATURES, guessing\n");
    		features = (IFF_TUN|IFF_TAP|IFF_NO_PI|IFF_ONE_QUEUE);
    	}
    	printf("Available features are: ");
    	for (i = 0; i < sizeof(known_flags)/sizeof(known_flags[0]); i++) {
    		if (features & known_flags[i].flag) {
    			features &= ~known_flags[i].flag;
    			printf("%s ", known_flags[i].name);
    		}
    	}
    	if (features)
    		printf("(UNKNOWN %#x)", features);
    	printf("\n");
    	return 0;
    }
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Max Krasnyansky <maxk@qualcomm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_tun.h
+++ include/linux/if_tun.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TUNSETOWNER   _IOW('T', 204, int)
 #define TUNSETLINK    _IOW('T', 205, int)
 #define TUNSETGROUP   _IOW('T', 206, int)
+#define TUNGETFEATURES _IOR('T', 207, unsigned int)
 
 /* TUNSETIFF ifr flags */
 #define IFF_TUN		0x0001
