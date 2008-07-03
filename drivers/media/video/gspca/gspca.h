21d21122005206640d545a07db67687ef557c6d7
Hans de Goede <j.w.r.degoede@hhs.nl>
2008-07-03 06:48:22 -0300

    V4L/DVB (8191): gspca: Make CONFIG_VIDEO_ADV_DEBUG actually work.
    Signed-off-by: Hans de Goede <j.w.r.degoede@hhs.nl>
    Signed-off-by: Jean-Francois Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/gspca/gspca.h
+++ drivers/media/video/gspca/gspca.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define V4L2_CID_SHARPNESS  (V4L2_CID_BASE+27)
 #endif
 
-#ifdef VIDEO_ADV_DEBUG
+#ifdef CONFIG_VIDEO_ADV_DEBUG
 /* GSPCA our debug messages */
 extern int gspca_debug;
 #define PDEBUG(level, fmt, args...) \
