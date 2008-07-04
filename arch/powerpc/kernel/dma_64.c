6dbb0424637f97a1e1400ba9f13fee2ef2e633cd
Mark Nelson <markn@au1.ibm.com>
2008-07-05 05:05:45 +1000

    powerpc: move device_to_mask() to dma-mapping.h
    Move device_to_mask() to dma-mapping.h because we need to use it from
    outside dma_64.c in a later patch.
    
    Signed-off-by: Mark Nelson <markn@au1.ibm.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kernel/dma_64.c
+++ arch/powerpc/kernel/dma_64.c
@@ -16,15 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Generic iommu implementation
  */
 
-static inline unsigned long device_to_mask(struct device *dev)
-{
-	if (dev->dma_mask && *dev->dma_mask)
-		return *dev->dma_mask;
-	/* Assume devices without mask can take 32 bit addresses */
-	return 0xfffffffful;
-}
-
-
 /* Allocates a contiguous real buffer and creates mappings over it.
  * Returns the virtual address of the buffer and sets dma_handle
  * to the dma address (mapping) of the first page.
