bfd9d727f7bc5548990009c3eda4ca028b5a481f
Heiko Carstens <heiko.carstens@de.ibm.com>
2008-07-04 11:26:40 +0200

    generic-ipi: fix s390 build bug
    forgot to remove #include <linux/spinlock.h> from linux/smp.h while
    fixing the original s390 build bug.
    
    Patch below fixes this build bug caused by header inclusion dependencies:
    
      CC      kernel/timer.o
    In file included from include/linux/spinlock.h:87,
                     from include/linux/smp.h:11,
                     from include/linux/kernel_stat.h:4,
                     from kernel/timer.c:22:
    include/asm/spinlock.h: In function '__raw_spin_lock':
    include/asm/spinlock.h:69: error: implicit declaration of function 'smp_processor_id'
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/smp.h
+++ include/linux/smp.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/errno.h>
 #include <linux/list.h>
-#include <linux/spinlock.h>
 #include <linux/cpumask.h>
 
 extern void cpu_idle(void);
