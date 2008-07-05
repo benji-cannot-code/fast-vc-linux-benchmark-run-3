8a800d63c82b84c10780e7494a51b8793fd01f0c
Sascha Hauer <s.hauer@pengutronix.de>
2008-07-05 10:02:43 +0200

    MXC: do not include board specific header from architecture include
    do not include board-mx31ads.h from hardware.h, instead include it
    directly only where needed.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx3/mx31ads.c
+++ arch/arm/mach-mx3/mx31ads.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/memory.h>
 #include <asm/mach/map.h>
 #include <asm/arch/common.h>
+#include <asm/arch/board-mx31ads.h>
 
 /*!
  * @file mx31ads.c
