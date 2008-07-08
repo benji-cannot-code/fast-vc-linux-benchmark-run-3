42063cf1b83515d7d87cbb22a89cfecbadc800b1
Haavard Skinnemoen <haavard.skinnemoen@atmel.com>
2008-07-08 11:59:42 -0700

    dmaengine: Driver for the Synopsys DesignWare DMA controller
    This adds a driver for the Synopsys DesignWare DMA controller (aka
    DMACA on AVR32 systems.) This DMA controller can be found integrated
    on the AT32AP7000 chip and is primarily meant for peripheral DMA
    transfer, but can also be used for memory-to-memory transfers.
    
    This patch is based on a driver from David Brownell which was based on
    an older version of the DMA Engine framework. It also implements the
    proposed extensions to the DMA Engine API for slave DMA operations.
    
    The dmatest client shows no problems, but there may still be room for
    improvement performance-wise. DMA slave transfer performance is
    definitely "good enough"; reading 100 MiB from an SD card running at ~20
    MHz yields ~7.2 MiB/s average transfer rate.
    
    Full documentation for this controller can be found in the Synopsys
    DW AHB DMAC Databook:
    
    http://www.synopsys.com/designware/docs/iip/DW_ahb_dmac/latest/doc/dw_ahb_dmac_db.pdf
    
    The controller has lots of implementation options, so it's usually a
    good idea to check the data sheet of the chip it's intergrated on as
    well. The AT32AP7000 data sheet can be found here:
    
    http://www.atmel.com/dyn/products/datasheets.asp?family_id=682
    
    
    Changes since v4:
      * Use client_count instead of dma_chan_is_in_use()
      * Add missing include
      * Unmap buffers unless client told us not to
    
    Changes since v3:
      * Update to latest DMA engine and DMA slave APIs
      * Embed the hw descriptor into the sw descriptor
      * Clean up and update MODULE_DESCRIPTION, copyright date, etc.
    
    Changes since v2:
      * Dequeue all pending transfers in terminate_all()
      * Rename dw_dmac.h -> dw_dmac_regs.h
      * Define and use controller-specific dma_slave data
      * Fix up a few outdated comments
      * Define hardware registers as structs (doesn't generate better
        code, unfortunately, but it looks nicer.)
      * Get number of channels from platform_data instead of hardcoding it
        based on CONFIG_WHATEVER_CPU.
      * Give slave clients exclusive access to the channel
    
    Acked-by: Maciej Sosnowski <maciej.sosnowski@intel.com>,
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- arch/avr32/mach-at32ap/at32ap700x.c
+++ arch/avr32/mach-at32ap/at32ap700x.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/clk.h>
 #include <linux/delay.h>
+#include <linux/dw_dmac.h>
 #include <linux/fb.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
@@ -600,6 +601,17 @@ static void __init genclk_init_parent(struct clk *clk)
 	clk->parent = parent;
 }
 
+static struct dw_dma_platform_data dw_dmac0_data = {
+	.nr_channels	= 3,
+};
+
+static struct resource dw_dmac0_resource[] = {
+	PBMEM(0xff200000),
+	IRQ(2),
+};
+DEFINE_DEV_DATA(dw_dmac, 0);
+DEV_CLK(hclk, dw_dmac0, hsb, 10);
+
 /* --------------------------------------------------------------------
  *  System peripherals
  * -------------------------------------------------------------------- */
@@ -705,17 +717,6 @@ static struct clk pico_clk = {
 	.get_rate	= cpu_clk_get_rate,
 	.users		= 1,
 };
-
-static struct resource dmaca0_resource[] = {
-	{
-		.start	= 0xff200000,
-		.end	= 0xff20ffff,
-		.flags	= IORESOURCE_MEM,
-	},
-	IRQ(2),
-};
-DEFINE_DEV(dmaca, 0);
-DEV_CLK(hclk, dmaca0, hsb, 10);
 
 /* --------------------------------------------------------------------
  * HMATRIX
@@ -829,7 +830,7 @@ void __init at32_add_system_devices(void)
 	platform_device_register(&at32_eic0_device);
 	platform_device_register(&smc0_device);
 	platform_device_register(&pdc_device);
-	platform_device_register(&dmaca0_device);
+	platform_device_register(&dw_dmac0_device);
 
 	platform_device_register(&at32_tcb0_device);
 	platform_device_register(&at32_tcb1_device);
@@ -1892,7 +1893,7 @@ struct clk *at32_clock_list[] = {
 	&smc0_mck,
 	&pdc_hclk,
 	&pdc_pclk,
-	&dmaca0_hclk,
+	&dw_dmac0_hclk,
 	&pico_clk,
 	&pio0_mck,
 	&pio1_mck,
