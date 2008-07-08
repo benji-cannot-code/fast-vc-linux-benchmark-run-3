effc49b7d132507f8c9347763b2e64b03626d6b3
Patrick McHardy <kaber@trash.net>
2008-07-08 03:24:14 -0700

    vlan: remove useless struct hlist_node declaration from if_vlan.h
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_vlan.h
+++ include/linux/if_vlan.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_IF_VLAN_H_
 
 #ifdef __KERNEL__
-
-/* externally defined structs */
-struct hlist_node;
-
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 
