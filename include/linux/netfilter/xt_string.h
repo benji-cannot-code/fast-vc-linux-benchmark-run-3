c9b074c51ce0aef6bfbdb6ea16b0d804ba754834
Joonwoo Park <joonwpark81@gmail.com>
2008-07-08 02:38:56 -0700

    netfilter: fix string extension for case insensitive pattern matching
    The flag XT_STRING_FLAG_IGNORECASE indicates case insensitive string
    matching. netfilter can find cmd.exe, Cmd.exe, cMd.exe and etc easily.
    
    A new revision 1 was added, in the meantime invert of xt_string_info
    was moved into flags as a flag. If revision is 1, The flag
    XT_STRING_FLAG_INVERT indicates invert matching.
    
    Signed-off-by: Joonwoo Park <joonwpark81@gmail.com>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netfilter/xt_string.h
+++ include/linux/netfilter/xt_string.h
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define XT_STRING_MAX_PATTERN_SIZE 128
 #define XT_STRING_MAX_ALGO_NAME_SIZE 16
 
+enum {
+	XT_STRING_FLAG_INVERT		= 0x01,
+	XT_STRING_FLAG_IGNORECASE	= 0x02
+};
+
 struct xt_string_info
 {
 	u_int16_t from_offset;
@@ -12,7 +17,15 @@ struct xt_string_info
 	char	  algo[XT_STRING_MAX_ALGO_NAME_SIZE];
 	char 	  pattern[XT_STRING_MAX_PATTERN_SIZE];
 	u_int8_t  patlen;
-	u_int8_t  invert;
+	union {
+		struct {
+			u_int8_t  invert;
+		} v0;
+
+		struct {
+			u_int8_t  flags;
+		} v1;
+	} u;
 
 	/* Used internally by the kernel */
 	struct ts_config __attribute__((aligned(8))) *config;
