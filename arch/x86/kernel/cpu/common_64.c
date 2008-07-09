689b7f26805a8df38052b4d8f81e38491430851b
Alexander Beregalov <a.beregalov@gmail.com>
2008-07-09 22:28:24 +0400

    arch/x86/kernel/cpu/common_64.c: remove double inclusions
    x86: remove double inclusions in arch/x86/kernel/cpu/common_64.c
    
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Cc: yhlu.kernel@gmail.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/common_64.c
+++ arch/x86/kernel/cpu/common_64.c
@@ -8,12 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/kgdb.h>
 #include <linux/topology.h>
-#include <linux/string.h>
 #include <linux/delay.h>
 #include <linux/smp.h>
-#include <linux/module.h>
 #include <linux/percpu.h>
-#include <asm/processor.h>
 #include <asm/i387.h>
 #include <asm/msr.h>
 #include <asm/io.h>
