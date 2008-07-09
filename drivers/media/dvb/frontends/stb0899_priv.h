b8b6816166db28323c35509658903703fcce0b00
Reinhard Nissl <rnissl@gmx.de>
2008-07-09 15:33:38 -0300

    V4L/DVB (9467): Fix runtime verbosity
    Signed-off-by: Reinhard Nissl <rnissl@gmx.de>
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/stb0899_priv.h
+++ drivers/media/dvb/frontends/stb0899_priv.h
@@ -34,16 +34,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define dprintk(x, y, z, format, arg...) do {						\
 	if (z) {									\
-		if	((x > FE_ERROR) && (x > y))					\
+		if	((*x > FE_ERROR) && (*x > y))					\
 			printk(KERN_ERR "%s: " format "\n", __func__ , ##arg);		\
-		else if	((x > FE_NOTICE) && (x > y))					\
+		else if	((*x > FE_NOTICE) && (*x > y))					\
 			printk(KERN_NOTICE "%s: " format "\n", __func__ , ##arg);	\
-		else if ((x > FE_INFO) && (x > y))					\
+		else if ((*x > FE_INFO) && (*x > y))					\
 			printk(KERN_INFO "%s: " format "\n", __func__ , ##arg);		\
-		else if ((x > FE_DEBUG) && (x > y))					\
+		else if ((*x > FE_DEBUG) && (*x > y))					\
 			printk(KERN_DEBUG "%s: " format "\n", __func__ , ##arg);	\
 	} else {									\
-		if (x > y)								\
+		if (*x > y)								\
 			printk(format, ##arg);						\
 	}										\
 } while(0)
@@ -216,7 +216,7 @@ struct stb0899_state {
 	struct stb0899_config		*config;
 	struct dvb_frontend		frontend;
 
-	u32				verbose;	/* Cached module verbosity level	*/
+	u32				*verbose;	/* Cached module verbosity level	*/
 
 	struct stb0899_internal		internal;	/* Device internal parameters		*/
 
