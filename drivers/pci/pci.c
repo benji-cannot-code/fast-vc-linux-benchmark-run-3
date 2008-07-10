e245fb0d150262b3825835d55cc675e0604cc34e
Stephen Rothwell <sfr@canb.auug.org.au>
2008-07-10 02:16:44 +0200

    PCI: include linux/pm_wakeup.h for device_set_wakeup_capable
    drivers/pci/pci.c needs pm_wakeup.h since it uses device_set_wakup_capable().
    The latter also needs to be stubbed out for !CONFIG_PM.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/log2.h>
 #include <linux/pci-aspm.h>
+#include <linux/pm_wakeup.h>
 #include <asm/dma.h>	/* isa_dma_bridge_buggy */
 #include "pci.h"
 
