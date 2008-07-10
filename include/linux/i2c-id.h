3849d5b134a15287212c45aea1b48513b31f51c6
Jean Delvare <khali@linux-fr.org>
2008-07-10 11:04:10 -0300

    V4L/DVB (8245): ovcamchip: Delete stray I2C bus ID
    I2C_HW_SMBUS_OVFX2 is referenced in ovcamchip_core.c, but no bus uses
    this driver ID, so we can remove the reference. As far as I can see,
    the Cypress FX2 webcam is handled by a different driver (dvb-usb).
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/linux/i2c-id.h
+++ include/linux/i2c-id.h
@@ -159,7 +159,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I2C_HW_SMBUS_W9968CF	0x04000d
 #define I2C_HW_SMBUS_OV511	0x04000e /* OV511(+) USB 1.1 webcam ICs */
 #define I2C_HW_SMBUS_OV518	0x04000f /* OV518(+) USB 1.1 webcam ICs */
-#define I2C_HW_SMBUS_OVFX2	0x040011 /* Cypress/OmniVision FX2 webcam */
 #define I2C_HW_SMBUS_CAFE	0x040012 /* Marvell 88ALP01 "CAFE" cam  */
 #define I2C_HW_SMBUS_ALI1563	0x040013
 
