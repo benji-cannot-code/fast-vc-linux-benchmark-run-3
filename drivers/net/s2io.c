b489d4fc0847f67294e22748bf16d984944f6c31
Sreenivasa Honnur <Sreenivasa.Honnur@neterion.com>
2008-07-09 23:50:13 -0400

    S2io: Version update for IOMMU overflow checking and enable msi-x link interrupts patches.
    - Updated version number
    
    Signed-off-by: Santosh Rastapur <santosh.rastapur@neterion.com>
    Signed-off-by: Ramkrishna Vepa <ram.vepa@neterion.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/s2io.c
+++ drivers/net/s2io.c
@@ -87,7 +87,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "s2io.h"
 #include "s2io-regs.h"
 
-#define DRV_VERSION "2.0.26.24"
+#define DRV_VERSION "2.0.26.25"
 
 /* S2io Driver name & version. */
 static char s2io_driver_name[] = "Neterion";
