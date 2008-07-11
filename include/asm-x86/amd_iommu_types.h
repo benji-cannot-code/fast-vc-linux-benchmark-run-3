4624c2f553f5c3c802bd3c90b094fb0c34265ab8
Joerg Roedel <joerg.roedel@amd.com>
2008-07-11 17:14:34 +0200

    x86, AMD IOMMU: fix device table entry size
    A device table entry is actually only 256 *bits* large. Not 256 bytes.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Cc: iommu@lists.linux-foundation.org
    Cc: bhavna.sarathy@amd.com
    Cc: robert.richter@amd.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/amd_iommu_types.h
+++ include/asm-x86/amd_iommu_types.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * some size calculation constants
  */
-#define DEV_TABLE_ENTRY_SIZE		256
+#define DEV_TABLE_ENTRY_SIZE		32
 #define ALIAS_TABLE_ENTRY_SIZE		2
 #define RLOOKUP_TABLE_ENTRY_SIZE	(sizeof(void *))
 
