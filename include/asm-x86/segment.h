80312cf32e520d7d834b00a48c1480252993d08a
Glauber Costa <gcosta@redhat.com>
2008-07-11 13:45:46 -0300

    x86: use user_mode macro
    Instead of using SEGMENT_IS_KERNEL_CODE, use the
    "user_mode" macro, which can play the same role. Delete
    the former, since it now lacks any user.
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/segment.h
+++ include/asm-x86/segment.h
@@ -132,9 +132,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Matching rules for certain types of segments.
  */
 
-/* Matches only __KERNEL_CS, ignoring PnP / USER / APM segments */
-#define SEGMENT_IS_KERNEL_CODE(x) (((x) & 0xfc) == GDT_ENTRY_KERNEL_CS * 8)
-
 /* Matches __KERNEL_CS and __USER_CS (they must be 2 entries apart) */
 #define SEGMENT_IS_FLAT_CODE(x)  (((x) & 0xec) == GDT_ENTRY_KERNEL_CS * 8)
 
