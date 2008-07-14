985ddc338e7d2b62ca197525a364def50acd81ff
Stoyan Gaydarov <stoyboyker@gmail.com>
2008-07-13 21:03:29 -0400

    ext4: replace __FUNCTION__ occurrences
    __FUNCTION__ is gcc-specific, use __func__ instead
    
    Signed-off-by: Stoyan Gaydarov <stoyboyker@gmail.com>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Cc: Mingming Cao <cmm@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -46,7 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ext4_debug(f, a...)						\
 	do {								\
 		printk (KERN_DEBUG "EXT4-fs DEBUG (%s, %d): %s:",	\
-			__FILE__, __LINE__, __FUNCTION__);		\
+			__FILE__, __LINE__, __func__);			\
 		printk (KERN_DEBUG f, ## a);				\
 	} while (0)
 #else
@@ -1164,7 +1164,7 @@ struct ext4_group_info *ext4_get_group_info(struct super_block *sb,
 #define ext4_std_error(sb, errno)				\
 do {								\
 	if ((errno))						\
-		__ext4_std_error((sb), __FUNCTION__, (errno));	\
+		__ext4_std_error((sb), __func__, (errno));	\
 } while (0)
 
 /*
