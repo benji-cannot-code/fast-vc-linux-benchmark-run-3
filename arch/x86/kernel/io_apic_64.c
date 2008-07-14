1a58610bc37f69775bf888449555af287d066380
Maciej W. Rozycki <macro@linux-mips.org>
2008-07-14 17:18:04 +0100

    x86: I/O APIC: Include <asm/i8259.h> required by some code
    Include <asm/i8259.h> for i8259A_lock used in print_PIC() -- #if-0-ed out
    by default.  The 32-bit version gets it right already.
    
    The plan is to enable this code with "apic=debug" eventually.  This will
    aid with debugging strange problems without the need to ask people to
    apply patches.
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/io_apic_64.c
+++ arch/x86/kernel/io_apic_64.c
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/proto.h>
 #include <asm/acpi.h>
 #include <asm/dma.h>
+#include <asm/i8259.h>
 #include <asm/nmi.h>
 #include <asm/msidef.h>
 #include <asm/hypertransport.h>
