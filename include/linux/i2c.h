f549c20a66cd2e437f0e4f27b14c9c6d432c9020
Jon Smirl <jonsmirl@gmail.com>
2008-07-14 22:38:35 +0200

    i2c: Export the i2c_bus_type symbol
    Export the root of the i2c bus so that PowerPC device tree code can
    iterate over devices on the i2c bus.
    
    Signed-off-by: Jon Smirl <jonsmirl@gmail.com>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>	/* for completion */
 #include <linux/mutex.h>
 
+extern struct bus_type i2c_bus_type;
+
 /* --- General options ------------------------------------------------	*/
 
 struct i2c_msg;
