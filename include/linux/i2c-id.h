8b5b39cb0f88a0796a233a77d1542cc04e59bb63
Adrian Bunk <bunk@kernel.org>
2008-07-14 22:38:22 +0200

    i2c: Remove 3 deprecated bus drivers
    This patch contains the scheduled removal of i2c-i810, i2c-prosavage
    and i2c-savage4.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- include/linux/i2c-id.h
+++ include/linux/i2c-id.h
@@ -112,7 +112,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I2C_HW_B_RIVA		0x010010 /* Riva based graphics cards */
 #define I2C_HW_B_IOC		0x010011 /* IOC bit-wiggling */
 #define I2C_HW_B_IXP2000	0x010016 /* GPIO on IXP2000 systems */
-#define I2C_HW_B_S3VIA		0x010018 /* S3Via ProSavage adapter */
 #define I2C_HW_B_ZR36067	0x010019 /* Zoran-36057/36067 based boards */
 #define I2C_HW_B_PCILYNX	0x01001a /* TI PCILynx I2C adapter */
 #define I2C_HW_B_CX2388x	0x01001b /* connexant 2388x based tv cards */
