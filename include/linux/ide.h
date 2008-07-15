f4358dea5250291a3efd1501c31aaa9694532183
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-07-15 21:21:56 +0200

    ide-{floppy,tape}: merge pc->idefloppy_callback and pc->idetape_callback
    Merge pc->idefloppy_callback and pc->idetape_callback into pc->callback.
    
    There should be no functional changes caused by this patch.
    
    Cc: Borislav Petkov <petkovbb@gmail.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- include/linux/ide.h
+++ include/linux/ide.h
@@ -641,8 +641,8 @@ struct ide_atapi_pc {
 	 * to change/removal later.
 	 */
 	u8 pc_buf[256];
-	void (*idefloppy_callback) (ide_drive_t *);
-	void (*idetape_callback) (ide_drive_t *);
+
+	void (*callback)(ide_drive_t *);
 
 	/* idetape only */
 	struct idetape_bh *bh;
