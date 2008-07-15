6e2ecf1070db036a1ff4da7f5e67ee0a5371792a
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-07-15 21:22:01 +0200

    ide-{floppy,tape}: move checking of ->failed_pc to ->callback
    Move checking/resetting of ->failed_pc from ide*_pc_intr() to ->callback
    as a preparation for adding generic ide_pc_intr() helper.
    
    There should be no functional changes caused by this patch.
    
    Cc: Borislav Petkov <petkovbb@gmail.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>---

--- drivers/ide/ide-tape.c
+++ drivers/ide/ide-tape.c
@@ -628,6 +628,9 @@ static void ide_tape_callback(ide_drive_t *drive)
 
 	debug_log(DBG_PROCS, "Enter %s\n", __func__);
 
+	if (tape->failed_pc == pc)
+		tape->failed_pc = NULL;
+
 	if (pc->c[0] == REQUEST_SENSE) {
 		if (uptodate)
 			idetape_analyze_error(drive, pc->buf);
@@ -839,8 +842,6 @@ static ide_startstop_t idetape_pc_intr(ide_drive_t *drive)
 			idetape_postpone_request(drive);
 			return ide_stopped;
 		}
-		if (tape->failed_pc == pc)
-			tape->failed_pc = NULL;
 		/* Command finished - Call the callback function */
 		pc->callback(drive);
 		return ide_stopped;
@@ -1051,8 +1052,6 @@ static ide_startstop_t idetape_media_access_finished(ide_drive_t *drive)
 			return ide_stopped;
 		}
 		pc->error = 0;
-		if (tape->failed_pc == pc)
-			tape->failed_pc = NULL;
 	} else {
 		pc->error = IDETAPE_ERROR_GENERAL;
 		tape->failed_pc = NULL;
