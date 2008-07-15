a8c4d397d359b1ef1ef7105f0297f900cbffdb5c
Robert Reif <reif@earthlink.net>
2008-07-14 22:57:29 -0700

    sparc32: fix init.c allnoconfig build error
    Fix allnoconfig build error.
    
    Signed-off-by: Robert Reif <reif@earthlink.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/init.c
+++ arch/sparc/mm/init.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/highmem.h>
 #include <linux/bootmem.h>
+#include <linux/pagemap.h>
 
 #include <asm/system.h>
 #include <asm/vac-ops.h>
