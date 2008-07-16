5249ac2f94f3940004a9b4066866008dd1acc3d5
Harvey Harrison <harvey.harrison@gmail.com>
2008-07-15 18:44:07 -0700

    mac80211: wme.h remove unused QOS_CONTROL_LEN
    linux/ieee80211.h now has IEEE80211_QOS_CTL_LEN for this purpose.
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/wme.h
+++ net/mac80211/wme.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include "ieee80211_i.h"
 
-#define QOS_CONTROL_LEN 2
-
 #define QOS_CONTROL_ACK_POLICY_NORMAL 0
 #define QOS_CONTROL_ACK_POLICY_NOACK 1
 
