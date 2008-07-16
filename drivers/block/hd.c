c23fada7360529dacf4cc5a894dfdb4cb47136c4
Adrian Bunk <bunk@kernel.org>
2008-07-16 20:33:48 +0200

    hd.c: remove the #include <linux/mc146818rtc.h>
    The code that needed this #include was removed one year ago.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Cc: rmk@arm.linux.org.uk
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/block/hd.c
+++ drivers/block/hd.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/ioport.h>
-#include <linux/mc146818rtc.h> /* CMOS defines */
 #include <linux/init.h>
 #include <linux/blkpg.h>
 #include <linux/hdreg.h>
