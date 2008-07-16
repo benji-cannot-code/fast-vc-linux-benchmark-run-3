a4ed050ac67db03f69ac5c859ddc7387fd2a1940
Borislav Petkov <petkovbb@googlemail.com>
2008-07-16 20:33:46 +0200

    ide-cd: move request prep chunk from cdrom_do_newpc_cont to rq issue path
    As a nice side effect, this minimizes the IRQ handler execution time.
    
    There should be no functionality change resulting from this patch.
    
    [bart: remove extra newlines from ide_cd_do_request()]
    
    Signed-off-by: Borislav Petkov <petkovbb@gmail.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-cd.c
+++ drivers/ide/ide-cd.c
@@ -1144,9 +1144,6 @@ static ide_startstop_t cdrom_do_newpc_cont(ide_drive_t *drive)
 {
 	struct request *rq = HWGROUP(drive)->rq;
 
-	if (!rq->timeout)
-		rq->timeout = ATAPI_WAIT_PC;
-
 	return cdrom_transfer_packet_command(drive, rq, cdrom_newpc_intr);
 }
 
@@ -1243,6 +1240,10 @@ static ide_startstop_t ide_cd_do_request(ide_drive_t *drive, struct request *rq,
 		   rq->cmd_type == REQ_TYPE_ATA_PC) {
 		xferlen = rq->data_len;
 		fn = cdrom_do_newpc_cont;
+
+		if (!rq->timeout)
+			rq->timeout = ATAPI_WAIT_PC;
+
 		cdrom_do_block_pc(drive, rq);
 	} else if (blk_special_request(rq)) {
 		/* right now this can only be a reset... */
@@ -1256,8 +1257,6 @@ static ide_startstop_t ide_cd_do_request(ide_drive_t *drive, struct request *rq,
 
 	return cdrom_start_packet_command(drive, xferlen, fn);
 }
-
-
 
 /*
  * Ioctl handling.
