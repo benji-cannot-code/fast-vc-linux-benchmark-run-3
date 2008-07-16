d3f280207eba1e837df8a264067192b7d34ef7f6
Sebastian Siewior <bigeasy@linutronix.de>
2008-07-16 23:31:17 +0200

    ftrace: copy + paste typo in asm/ftrace.h
    Signed-off-by: Sebastian Siewior <bigeasy@linutronix.de>
    Cc: Steven Rostedt <srostedt@redhat.co>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/ftrace.h
+++ include/asm-x86/ftrace.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_FTRACE
-#define _ASM_SPARC64_FTRACE
+#define _ASM_X86_FTRACE
 
 #ifdef CONFIG_FTRACE
 #define MCOUNT_ADDR		((long)(mcount))
