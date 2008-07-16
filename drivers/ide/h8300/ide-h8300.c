1b784c115e3ba86bfc4d8809c3ccd7f826448a4d
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-07-16 20:33:42 +0200

    ide: print message on error in ide_find_port_slot()
    * Add DRV_NAME define to ide-h8300.c.
    
    * Fix ide-h8300.c, swarm.c and sgiioc4.c to set .name field in
      struct ide_port_info to DRV_NAME, then convert these host drivers
      to use ide_find_port_slot() instead of ide_find_port().
    
    * Print message on error in ide_find_port_slot().
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/h8300/ide-h8300.c
+++ drivers/ide/h8300/ide-h8300.c
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/irq.h>
 
+#define DRV_NAME "ide-h8300"
+
 #define bswap(d) \
 ({					\
 	u16 r;				\
@@ -197,11 +199,9 @@ static int __init h8300_ide_init(void)
 
 	hw_setup(&hw);
 
-	hwif = ide_find_port();
-	if (hwif == NULL) {
-		printk(KERN_ERR "ide-h8300: IDE I/F register failed\n");
+	hwif = ide_find_port_slot(&h8300_port_info);
+	if (hwif == NULL)
 		return -ENOENT;
-	}
 
 	index = hwif->index;
 	ide_init_port_hw(hwif, &hw);
