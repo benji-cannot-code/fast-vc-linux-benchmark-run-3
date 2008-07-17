eae414b48bc9aaa8377bde249e8475410761866f
Adrian Bunk <bunk@kernel.org>
2008-07-17 21:16:20 +0200

    drivers/video/c2p.c: add MODULE_LICENSE
    This patch adds the missing MODULE_LICENSE("GPL").
    
    Reported-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/c2p.c
+++ drivers/video/c2p.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  for more details.
  */
 
+#include <linux/module.h>
 #include <linux/string.h>
 #include "c2p.h"
 
@@ -228,3 +229,4 @@ void c2p(u8 *dst, const u8 *src, u32 dx, u32 dy, u32 width, u32 height,
     }
 }
 
+MODULE_LICENSE("GPL");
