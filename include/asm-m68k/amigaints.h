b925d91d0f73ec62671e38a734b19d3ce5e2e09c
Adrian Bunk <bunk@kernel.org>
2008-07-17 21:16:23 +0200

    m68k/amiga/: possible cleanups
    This patch contains the following possible cleanups:
    - amiints.c: add a proper prototype for amiga_init_IRQ() in
                 include/asm-m68k/amigaints.h
    - make the following needlessly global code static:
      - config.c: amiga_model
      - config.c: amiga_psfreq
      - config.c: amiga_serial_console_write()
    - #if 0 the following unused functions:
      - config.c: amiga_serial_puts()
      - config.c: amiga_serial_console_wait_key()
      - config.c: amiga_serial_gets()
    - remove the following unused variable:
      - config.c: amiga_masterclock
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-m68k/amigaints.h
+++ include/asm-m68k/amigaints.h
@@ -99,6 +99,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CIA_ICR_ALL	0x1f
 #define CIA_ICR_SETCLR	0x80
 
+extern void amiga_init_IRQ(void);
+
 /* to access the interrupt control registers of CIA's use only
 ** these functions, they behave exactly like the amiga os routines
 */
