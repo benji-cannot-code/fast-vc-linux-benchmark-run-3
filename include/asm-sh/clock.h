58dd0c54dc09e74e3a3cad57767afc6c4017e6da
Magnus Damm <damm@igel.co.jp>
2008-07-17 19:09:51 +0900

    sh: Introduce clk_always_enable() function
    Add SuperH specific funcion clk_always_enable(), useful to enable MSTPCR
    bits in processor or board specific code.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/clock.h
+++ include/asm-sh/clock.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/seq_file.h>
 #include <linux/clk.h>
+#include <linux/err.h>
 
 struct clk;
 
@@ -47,6 +48,22 @@ void clk_recalc_rate(struct clk *);
 
 int clk_register(struct clk *);
 void clk_unregister(struct clk *);
+
+static inline int clk_always_enable(const char *id)
+{
+	struct clk *clk;
+	int ret;
+
+	clk = clk_get(NULL, id);
+	if (IS_ERR(clk))
+		return PTR_ERR(clk);
+
+	ret = clk_enable(clk);
+	if (ret)
+		clk_put(clk);
+
+	return ret;
+}
 
 /* the exported API, in addition to clk_set_rate */
 /**
