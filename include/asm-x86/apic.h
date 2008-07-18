0bc858bc70f3ae8b659c8a86ff07da0cf2a03c67
Maciej W. Rozycki <macro@linux-mips.org>
2008-07-18 01:47:44 +0100

    x86: more apic debugging
    [ mingo@elte.hu: picked up this patch from Maciej, lets make apic=debug
                     print out more info - we had a lot of APIC changes ]
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/apic.h
+++ include/asm-x86/apic.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ARCH_APICTIMER_STOPS_ON_C3	1
 
-#define Dprintk(x...)
+#define Dprintk printk
 
 /*
  * Debugging macros
