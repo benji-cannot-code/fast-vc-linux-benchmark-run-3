360146d4af0f0a2401e208ffdd4c5d56877aa448
Ben Nizette <bn@niasdigital.com>
2008-07-18 16:48:09 +1000

    avr32: clean up mci platform code
    This patch does a few small cleanups around the atmel mci platform code
    and in the atmel-mci driver.  The platform changes simply removes an
    unused variable, uses the fact that by the end we always have some form
    of platform data and notes that GPIO_PIN_NONE != 0.  This last point
    could cause the incorrect attempt to twice reserve pin PA0.
    
    While we've got the hood up, add linux/err.h to the atmel-mci.c include
    list.  It needs it and generally pulls it by voodoo but I did once
    stumble across a config which don't build.
    
    This is against Linus' latest git.
    
    Signed-off-by: Ben Nizette <bn@niasdigital.com>
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- drivers/mmc/host/atmel-mci.c
+++ drivers/mmc/host/atmel-mci.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/blkdev.h>
 #include <linux/clk.h>
 #include <linux/device.h>
+#include <linux/err.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/ioport.h>
