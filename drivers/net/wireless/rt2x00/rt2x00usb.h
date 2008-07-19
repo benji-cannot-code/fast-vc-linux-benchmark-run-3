ead916b8a4afdfaa65a39d7d64342d72c4ff6920
Iwo Mergler <iwo@call-direct.com.au>
2008-07-19 16:17:16 +0200

    rt2x00: Large vendor requests for rt73usb firmware upload and beacons
    Switches rt73usb to use large vendor requests for firmware
    and beacons. This also fixes the garbled beacon bug.
    
    Signed-off-by: Iwo Mergler <Iwo@call-direct.com.au>
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00usb.h
+++ drivers/net/wireless/rt2x00/rt2x00usb.h
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Cache size
  */
 #define CSR_CACHE_SIZE			64
-#define CSR_CACHE_SIZE_FIRMWARE		64
 
 /*
  * USB request types.
