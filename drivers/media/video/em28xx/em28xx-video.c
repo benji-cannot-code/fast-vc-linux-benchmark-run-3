5b9bb1375619fee96128bd9eb475ebb04c1428b6
Hans Verkuil <hverkuil@xs4all.nl>
2008-07-20 06:31:39 -0300

    V4L/DVB (8428): videodev: rename 'dev' to 'parent'
    The field 'dev' is not the video device, but the parent of the video device.
    Rename accordingly.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/em28xx/em28xx-video.c
+++ drivers/media/video/em28xx/em28xx-video.c
@@ -1893,7 +1893,7 @@ static struct video_device *em28xx_vdev_init(struct em28xx *dev,
 		return NULL;
 	*vfd = *template;
 	vfd->minor   = -1;
-	vfd->dev = &dev->udev->dev;
+	vfd->parent = &dev->udev->dev;
 	vfd->release = video_device_release;
 	vfd->type = type;
 	vfd->debug = video_debug;
