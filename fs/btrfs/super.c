1e2e80f28c119e8018c8802f91e15504b15fc506
Balaji Rao <balajirrao@gmail.com>
2008-07-21 02:01:56 +0530

    NFS support for btrfs - v3
    Date: Mon, 21 Jul 2008 02:01:56 +0530
    Here's an implementation of NFS support for btrfs. It relies on the
    fixes which are going in to 2.6.28 for the NFS readdir/lookup deadlock.
    
    This uses the btrfs_iget helper introduced previously.
    
    [dwmw2: Tidy up a little, switch to d_obtain_alias() w/compat routine,
    	change fh_type,	store parent's root object ID where needed,
    	fix some get_parent() and fs_to_dentry() bugs]
    
    Signed-off-by: Balaji Rao <balajirrao@gmail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xattr.h"
 #include "volumes.h"
 #include "version.h"
+#include "export.h"
 
 #define BTRFS_SUPER_MAGIC 0x9123683E
 
@@ -304,6 +305,7 @@ static int btrfs_fill_super(struct super_block * sb,
 	sb->s_maxbytes = MAX_LFS_FILESIZE;
 	sb->s_magic = BTRFS_SUPER_MAGIC;
 	sb->s_op = &btrfs_super_ops;
+	sb->s_export_op = &btrfs_export_ops;
 	sb->s_xattr = btrfs_xattr_handlers;
 	sb->s_time_gran = 1;
 	sb->s_flags |= MS_POSIXACL;
