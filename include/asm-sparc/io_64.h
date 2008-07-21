ae361c3e9b192a4b281248dfe6b7a63e52cb2e24
David S. Miller <davem@davemloft.net>
2008-07-21 13:17:38 -0700

    sparc64: Do not define BIO_VMERGE_BOUNDARY.
    The IOMMU code and the block layer can split things
    up using different rules, so this can't work reliably.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc/io_64.h
+++ include/asm-sparc/io_64.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* BIO layer definitions. */
 extern unsigned long kern_base, kern_size;
 #define page_to_phys(page)	(page_to_pfn(page) << PAGE_SHIFT)
-#define BIO_VMERGE_BOUNDARY	8192
 
 static inline u8 _inb(unsigned long addr)
 {
