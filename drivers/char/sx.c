c7592ce7ff6e86c7dfbc3e312b0738623b2c10d2
Alan Cox <alan@redhat.com>
2008-07-22 11:18:03 +0100

    tty: rework break handling
    Some hardware needs to do break handling itself and may have partial
    support only. Make break_ctl return an error code. Add a tty driver flag
    so you can indicate driver hardware side break support.
    
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/sx.c
+++ drivers/char/sx.c
@@ -1841,7 +1841,7 @@ static int sx_fw_ioctl(struct inode *inode, struct file *filp,
 	return rc;
 }
 
-static void sx_break(struct tty_struct *tty, int flag)
+static int sx_break(struct tty_struct *tty, int flag)
 {
 	struct sx_port *port = tty->driver_data;
 	int rv;
@@ -1858,6 +1858,7 @@ static void sx_break(struct tty_struct *tty, int flag)
 			read_sx_byte(port->board, CHAN_OFFSET(port, hi_hstat)));
 	unlock_kernel();
 	func_exit();
+	return 0;
 }
 
 static int sx_tiocmget(struct tty_struct *tty, struct file *file)
