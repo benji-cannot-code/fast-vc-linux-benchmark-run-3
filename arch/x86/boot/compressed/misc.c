a1978796b2e9f369d8db125f038dea580597fe3c
Vegard Nossum <vegard.nossum@gmail.com>
2008-07-22 21:53:53 +0200

    x86: consolidate header guards
    This patch consolidates the header guard names which are also used
    externally, i.e. in .c files.
    
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- arch/x86/boot/compressed/misc.c
+++ arch/x86/boot/compressed/misc.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #undef CONFIG_PARAVIRT
 #ifdef CONFIG_X86_32
-#define _ASM_DESC_H_ 1
+#define ASM_X86__DESC_H 1
 #endif
 
 #ifdef CONFIG_X86_64
