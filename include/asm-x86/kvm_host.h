8cdaf257b7f58b010dd5d77aedc568df40c047df
Vegard Nossum <vegard.nossum@gmail.com>
2008-07-22 21:27:11 +0200

    x86: fix spurious '#' in kvm header
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- include/asm-x86/kvm_host.h
+++ include/asm-x86/kvm_host.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#/*
+/*
  * Kernel-based Virtual Machine driver for Linux
  *
  * This header defines architecture specific interfaces, x86 version
