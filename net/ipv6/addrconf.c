742aa7af3353b5a7ff6e1ed551c30bce6eff249d
Adrian Bunk <bunk@kernel.org>
2008-07-22 14:21:58 -0700

    ipv6: make struct ipv6_devconf static
    struct ipv6_devconf can now become static.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -154,7 +154,7 @@ static int ipv6_chk_same_addr(struct net *net, const struct in6_addr *addr,
 
 static ATOMIC_NOTIFIER_HEAD(inet6addr_chain);
 
-struct ipv6_devconf ipv6_devconf __read_mostly = {
+static struct ipv6_devconf ipv6_devconf __read_mostly = {
 	.forwarding		= 0,
 	.hop_limit		= IPV6_DEFAULT_HOPLIMIT,
 	.mtu6			= IPV6_MIN_MTU,
