b084c18025f842f843064d23034eada8883ace2e
Henrique de Moraes Holschuh <hmh@hmh.eng.br>
2008-07-21 21:18:18 -0300

    rfkill: fix led-trigger unregister order in error unwind
    rfkill needs to unregister the led trigger AFTER a call to
    rfkill_remove_switch(), otherwise it will not update the LED state,
    possibly leaving it ON when it should be OFF.
    
    To make led-trigger unregistering safer, guard against unregistering a
    trigger twice, and also against issuing trigger events to a led trigger
    that was unregistered.  This makes the error unwind paths more resilient.
    
    Refer to "rfkill: Register LED triggers before registering switch".
    
    Signed-off-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Acked-by: Ivo van Doorn <IvDoorn@gmail.com>
    Cc: Michael Buesch <mb@bu3sch.de>
    Cc: Dmitry Baryshkov <dbaryshkov@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/rfkill/rfkill.c
+++ net/rfkill/rfkill.c
@@ -591,8 +591,10 @@ static void rfkill_led_trigger_register(struct rfkill *rfkill)
 static void rfkill_led_trigger_unregister(struct rfkill *rfkill)
 {
 #ifdef CONFIG_RFKILL_LEDS
-	if (rfkill->led_trigger.name)
+	if (rfkill->led_trigger.name) {
 		led_trigger_unregister(&rfkill->led_trigger);
+		rfkill->led_trigger.name = NULL;
+	}
 #endif
 }
 
@@ -628,8 +630,8 @@ int rfkill_register(struct rfkill *rfkill)
 
 	error = device_add(dev);
 	if (error) {
-		rfkill_led_trigger_unregister(rfkill);
 		rfkill_remove_switch(rfkill);
+		rfkill_led_trigger_unregister(rfkill);
 		return error;
 	}
 
