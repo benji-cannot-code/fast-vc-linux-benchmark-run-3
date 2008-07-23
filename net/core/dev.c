7ef7db7558d38087192fd3cfbabbffeaecdf92b8
David S. Miller <davem@davemloft.net>
2008-07-23 14:01:29 -0700

    netdev: Remove warning from __netif_schedule().
    It isn't helping anything and we aren't going to be able to change all
    the drivers that do queue wakeups in strange situations.
    
    Just letting a noop_qdisc get scheduled will work because when
    qdisc_run() executes via net_tx_work() it will simply find no packets
    pending when it makes the ->dequeue() call in qdisc_restart.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1342,9 +1342,6 @@ static void dev_queue_xmit_nit(struct sk_buff *skb, struct net_device *dev)
 
 void __netif_schedule(struct Qdisc *q)
 {
-	if (WARN_ON_ONCE(q == &noop_qdisc))
-		return;
-
 	if (!test_and_set_bit(__QDISC_STATE_SCHED, &q->state)) {
 		struct softnet_data *sd;
 		unsigned long flags;
