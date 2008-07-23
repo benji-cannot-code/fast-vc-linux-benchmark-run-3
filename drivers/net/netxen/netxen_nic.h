3948357aaa7748c0531ecebf7a84ea78dbcac1b1
David S. Miller <davem@davemloft.net>
2008-07-22 18:29:10 -0700

    netxen: Needs to include linux/vmalloc.h
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/netxen/netxen_nic.h
+++ drivers/net/netxen/netxen_nic.h
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/mm.h>
 #include <linux/mman.h>
+#include <linux/vmalloc.h>
 
 #include <asm/system.h>
 #include <asm/io.h>
