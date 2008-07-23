ba48ba86b25f19ec79fa49d90ecb28c31ddbf923
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-07-23 19:55:56 +0200

    ide: filter out "default" transfer mode values in set_xfer_rate()
    * Filter out "default" transfer mode values (0x00 - default PIO mode,
      0x01 - default PIO mode w/ IORDY disabled) in write handler for obsoleted
      /proc/ide/hd?/settings:current_speed setting.
    
      Allowing "default" transfer mode values is a dangerous thing to do as
      we don't support programming controller to the "default" transfer mode
      and devices often use different values for the default and maximum PIO
      mode (i.e. PIO2 default and PIO4 maximum) so the controller will stay
      programmed for higher PIO mode while device will use the lower PIO mode.
    
      There is no functionality loss as by using special IOCTLs device can
      still be programmed to "default" transfer modes (it is only useful for
      debugging/testing purposes anyway).
    
    * Remove no longer needed IDE_HFLAG_ABUSE_SET_DMA_MODE host flag, it was
      previously used by few host drivers to program the controller to PIO0
      timings for "default" transfer mode == 0x01 (although some host drivers
      would program invalid PIO timings instead).
    
    * Cleanup ide_set_xfer_rate() and add BUG_ON().
    
    Suggested-by: Sergei Shtylyov <sshtylyov@ru.mvista.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/pci/aec62xx.c
+++ drivers/ide/pci/aec62xx.c
@@ -196,7 +196,6 @@ static const struct ide_port_info aec62xx_chipsets[] __devinitdata = {
 		.host_flags	= IDE_HFLAG_SERIALIZE |
 				  IDE_HFLAG_NO_ATAPI_DMA |
 				  IDE_HFLAG_NO_DSC |
-				  IDE_HFLAG_ABUSE_SET_DMA_MODE |
 				  IDE_HFLAG_OFF_BOARD,
 		.pio_mask	= ATA_PIO4,
 		.mwdma_mask	= ATA_MWDMA2,
@@ -206,7 +205,6 @@ static const struct ide_port_info aec62xx_chipsets[] __devinitdata = {
 		.init_chipset	= init_chipset_aec62xx,
 		.port_ops	= &atp86x_port_ops,
 		.host_flags	= IDE_HFLAG_NO_ATAPI_DMA | IDE_HFLAG_NO_AUTODMA |
-				  IDE_HFLAG_ABUSE_SET_DMA_MODE |
 				  IDE_HFLAG_OFF_BOARD,
 		.pio_mask	= ATA_PIO4,
 		.mwdma_mask	= ATA_MWDMA2,
@@ -217,7 +215,6 @@ static const struct ide_port_info aec62xx_chipsets[] __devinitdata = {
 		.enablebits	= {{0x4a,0x02,0x02}, {0x4a,0x04,0x04}},
 		.port_ops	= &atp86x_port_ops,
 		.host_flags	= IDE_HFLAG_NO_ATAPI_DMA |
-				  IDE_HFLAG_ABUSE_SET_DMA_MODE |
 				  IDE_HFLAG_NON_BOOTABLE,
 		.pio_mask	= ATA_PIO4,
 		.mwdma_mask	= ATA_MWDMA2,
@@ -227,7 +224,6 @@ static const struct ide_port_info aec62xx_chipsets[] __devinitdata = {
 		.init_chipset	= init_chipset_aec62xx,
 		.port_ops	= &atp86x_port_ops,
 		.host_flags	= IDE_HFLAG_NO_ATAPI_DMA |
-				  IDE_HFLAG_ABUSE_SET_DMA_MODE |
 				  IDE_HFLAG_OFF_BOARD,
 		.pio_mask	= ATA_PIO4,
 		.mwdma_mask	= ATA_MWDMA2,
@@ -238,7 +234,6 @@ static const struct ide_port_info aec62xx_chipsets[] __devinitdata = {
 		.enablebits	= {{0x4a,0x02,0x02}, {0x4a,0x04,0x04}},
 		.port_ops	= &atp86x_port_ops,
 		.host_flags	= IDE_HFLAG_NO_ATAPI_DMA |
-				  IDE_HFLAG_ABUSE_SET_DMA_MODE |
 				  IDE_HFLAG_OFF_BOARD,
 		.pio_mask	= ATA_PIO4,
 		.mwdma_mask	= ATA_MWDMA2,
