5fecd96395db74e9276168f38c54e65722a8f77f
Jaswinder Singh <jaswinder@infradead.org>
2008-07-23 17:31:02 +0530

    x86: vm86_32.c declare functions before they get used
    declared following syscalls in asm-x86/syscalls.h:
    sys_vm86old, sys_vm86
    
    Signed-off-by: Jaswinder Singh <jaswinder@infradead.org>

--- include/asm-x86/syscalls.h
+++ include/asm-x86/syscalls.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This file is released under the GPLv2.
  * See the file COPYING for more details.
  *
- * Please do not call me directly, include linux/syscalls.h
  */
 
 #ifndef _ASM_X86_SYSCALLS_H
@@ -60,6 +59,10 @@ asmlinkage int sys_olduname(struct oldold_utsname __user *);
 /* kernel/tls.c */
 asmlinkage int sys_set_thread_area(struct user_desc __user *);
 asmlinkage int sys_get_thread_area(struct user_desc __user *);
+
+/* kernel/vm86_32.c */
+asmlinkage int sys_vm86old(struct pt_regs);
+asmlinkage int sys_vm86(struct pt_regs);
 
 #else /* CONFIG_X86_32 */
 
