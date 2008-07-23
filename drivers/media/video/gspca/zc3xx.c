efb2e394645391c38a515e35beb5c6673210e18e
Jean-Francois Moine <moinejf@free.fr>
2008-07-23 03:39:42 -0300

    V4L/DVB (8512): gspca: Do not use the driver_info field of usb_device_id.
    The field driver_info will be used to handle the specific per webcam
    information.
    
    Signed-off-by: Jean-Francois Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/gspca/zc3xx.c
+++ drivers/media/video/gspca/zc3xx.c
@@ -7154,7 +7154,6 @@ static int sd_config(struct gspca_dev *gspca_dev,
 	}
 
 	cam = &gspca_dev->cam;
-	cam->dev_name = (char *) id->driver_info;
 	cam->epaddr = 0x01;
 /*fixme:test*/
 	gspca_dev->nbalt--;
