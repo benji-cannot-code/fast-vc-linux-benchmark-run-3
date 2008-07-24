16991f5af421432635803eb57cbcbfd8c76b90f0
Krzysztof Helt <krzysztof.h1@wp.pl>
2008-07-23 21:31:05 -0700

    tridentfb: source code improvements
    This patch contains general source code improvments:
     - more simple functions are inline
     - removes some meaningless output and the VERSION
       string as it is no use
     - eng_par is moved into the tridentfb_par
     - removed small section of code for CyberBladeXPAi1
       which is maybe right for only one resolution
       and refresh rate and is probably redundant now
     - other minor improvements
    
    Signed-off-by: Krzysztof Helt <krzysztof.h1@wp.pl>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/video/trident.h
+++ include/video/trident.h
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #if TRIDENTFB_DEBUG
-#define debug(f,a...)	printk("%s:" f,  __FUNCTION__ , ## a);mdelay(100);
+#define debug(f, a...)	printk("%s:" f,  __func__ , ## a);
 #else
-#define debug(f,a...)
+#define debug(f, a...)
 #endif
 
 #define output(f, a...) pr_info("tridentfb: " f, ## a)
