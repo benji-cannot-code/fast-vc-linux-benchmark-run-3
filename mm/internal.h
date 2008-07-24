d28a22b5eb6b9ee87ff331f952837cff87ae57ae
Andi Kleen <ak@suse.de>
2008-07-23 21:27:46 -0700

    mm: export prep_compound_page to mm
    hugetlb will need to get compound pages from bootmem to handle the case of
    them being greater than or equal to MAX_ORDER.  Export the constructor
    function needed for this.
    
    Acked-by: Adam Litke <agl@us.ibm.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/internal.h
+++ mm/internal.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void free_pgtables(struct mmu_gather *tlb, struct vm_area_struct *start_vma,
 		unsigned long floor, unsigned long ceiling);
 
+extern void prep_compound_page(struct page *page, unsigned long order);
+
 static inline void set_page_count(struct page *page, int v)
 {
 	atomic_set(&page->_count, v);
