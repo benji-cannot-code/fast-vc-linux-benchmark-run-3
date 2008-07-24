0ea230176e7b96cd37d14dcc9e8e143d42079b94
Rafael J. Wysocki <rjw@sisk.pl>
2008-07-23 21:28:37 -0700

    pm: drop unnecessary includes from pm.h
    Drop unnecessary includes from include/linux/pm.h .
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_PM_H
 
 #include <linux/list.h>
-#include <asm/atomic.h>
-#include <asm/errno.h>
 
 /*
  * Callbacks for platform drivers to implement.
