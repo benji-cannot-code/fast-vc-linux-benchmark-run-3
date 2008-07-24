dfcc030af5a791130f85b0e9477f572394e9149f
Jiri Slaby <jirislaby@gmail.com>
2008-07-23 21:29:58 -0700

    vt: hold console_sem across sysfs operations
    Hold console sem while creating/destroying sysfs files.  Serialisation is
    so far done by BKL held in tty release_dev and chrdev_open, but no other
    locks are held in open path.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Cc: Aristeu Rozanski <aris@ruivo.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/vt.c
+++ drivers/char/vt.c
@@ -2750,8 +2750,8 @@ static int con_open(struct tty_struct *tty, struct file *filp)
 				tty->termios->c_iflag |= IUTF8;
 			else
 				tty->termios->c_iflag &= ~IUTF8;
-			release_console_sem();
 			vcs_make_sysfs(tty);
+			release_console_sem();
 			return ret;
 		}
 	}
@@ -2776,8 +2776,8 @@ static void con_close(struct tty_struct *tty, struct file *filp)
 		if (vc)
 			vc->vc_tty = NULL;
 		tty->driver_data = NULL;
-		release_console_sem();
 		vcs_remove_sysfs(tty);
+		release_console_sem();
 		mutex_unlock(&tty_mutex);
 		/*
 		 * tty_mutex is released, but we still hold BKL, so there is
