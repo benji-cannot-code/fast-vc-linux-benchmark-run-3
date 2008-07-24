cac58016457e2dd755c6f3922aaea495a9252b18
Ulrich Drepper <drepper@redhat.com>
2008-07-23 21:29:43 -0700

    flag parameters add-on: remove epoll_create size param
    Remove the size parameter from the new epoll_create syscall and renames the
    syscall itself.  The updated test program follows.
    
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    #include <fcntl.h>
    #include <stdio.h>
    #include <time.h>
    #include <unistd.h>
    #include <sys/syscall.h>
    
    #ifndef __NR_epoll_create2
    # ifdef __x86_64__
    #  define __NR_epoll_create2 291
    # elif defined __i386__
    #  define __NR_epoll_create2 329
    # else
    #  error "need __NR_epoll_create2"
    # endif
    #endif
    
    #define EPOLL_CLOEXEC O_CLOEXEC
    
    int
    main (void)
    {
      int fd = syscall (__NR_epoll_create2, 0);
      if (fd == -1)
        {
          puts ("epoll_create2(0) failed");
          return 1;
        }
      int coe = fcntl (fd, F_GETFD);
      if (coe == -1)
        {
          puts ("fcntl failed");
          return 1;
        }
      if (coe & FD_CLOEXEC)
        {
          puts ("epoll_create2(0) set close-on-exec flag");
          return 1;
        }
      close (fd);
    
      fd = syscall (__NR_epoll_create2, EPOLL_CLOEXEC);
      if (fd == -1)
        {
          puts ("epoll_create2(EPOLL_CLOEXEC) failed");
          return 1;
        }
      coe = fcntl (fd, F_GETFD);
      if (coe == -1)
        {
          puts ("fcntl failed");
          return 1;
        }
      if ((coe & FD_CLOEXEC) == 0)
        {
          puts ("epoll_create2(EPOLL_CLOEXEC) set close-on-exec flag");
          return 1;
        }
      close (fd);
    
      puts ("OK");
    
      return 0;
    }
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Ulrich Drepper <drepper@redhat.com>
    Acked-by: Davide Libenzi <davidel@xmailserver.org>
    Cc: Michael Kerrisk <mtk.manpages@googlemail.com>
    Cc: <linux-arch@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/eventpoll.h
+++ include/linux/eventpoll.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fcntl.h>
 #include <linux/types.h>
 
-/* Flags for epoll_create2.  */
+/* Flags for epoll_create1.  */
 #define EPOLL_CLOEXEC O_CLOEXEC
 
 /* Valid opcodes to issue to sys_epoll_ctl() */
