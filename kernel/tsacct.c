39629cf046f5e98dbda33bdceb50cf20fd687bda
David Howells <dhowells@redhat.com>
2008-07-25 01:48:50 -0700

    tsacct: fix bacct_add_tsk()'s use of do_div()
    Fix bacct_add_tsk()'s use of do_div() on an s64 by making ac_etime a u64
    instead and dividing that.
    
    Possibly this should be guarded lest the interval calculation turn up
    negative, but the possible negativity of the result of the division is
    cast away, and it shouldn't end up negative anyway.
    
    This was introduced by patch f3cef7a99469afc159fec3a61b42dc7ca5b6824f.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Cc: Jay Lan <jlan@engr.sgi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/tsacct.c
+++ kernel/tsacct.c
@@ -29,14 +29,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void bacct_add_tsk(struct taskstats *stats, struct task_struct *tsk)
 {
 	struct timespec uptime, ts;
-	s64 ac_etime;
+	u64 ac_etime;
 
 	BUILD_BUG_ON(TS_COMM_LEN < TASK_COMM_LEN);
 
 	/* calculate task elapsed time in timespec */
 	do_posix_clock_monotonic_gettime(&uptime);
 	ts = timespec_sub(uptime, tsk->start_time);
-	/* rebase elapsed time to usec */
+	/* rebase elapsed time to usec (should never be negative) */
 	ac_etime = timespec_to_ns(&ts);
 	do_div(ac_etime, NSEC_PER_USEC);
 	stats->ac_etime = ac_etime;
