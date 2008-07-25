24391f1935443e6a70200c8c8538ccc5a933181b
Ingo Molnar <mingo@elte.hu>
2008-07-25 12:57:41 +0200

    mm/hugetlb.c: fix build failure with !CONFIG_SYSCTL
    on !CONFIG_SYSCTL on x86 with latest -git i get:
    
         mm/hugetlb.c: In function 'decrement_hugepage_resv_vma':
         mm/hugetlb.c:83: error: 'reserve' undeclared (first use in this function)
         mm/hugetlb.c:83: error: (Each undeclared identifier is reported only once
         mm/hugetlb.c:83: error: for each function it appears in.)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1027,6 +1027,17 @@ static void __init report_hugepages(void)
 	}
 }
 
+static unsigned int cpuset_mems_nr(unsigned int *array)
+{
+	int node;
+	unsigned int nr = 0;
+
+	for_each_node_mask(node, cpuset_current_mems_allowed)
+		nr += array[node];
+
+	return nr;
+}
+
 #ifdef CONFIG_SYSCTL
 #ifdef CONFIG_HIGHMEM
 static void try_to_free_low(struct hstate *h, unsigned long count)
@@ -1375,17 +1386,6 @@ static int __init hugetlb_default_setup(char *s)
 	return 1;
 }
 __setup("default_hugepagesz=", hugetlb_default_setup);
-
-static unsigned int cpuset_mems_nr(unsigned int *array)
-{
-	int node;
-	unsigned int nr = 0;
-
-	for_each_node_mask(node, cpuset_current_mems_allowed)
-		nr += array[node];
-
-	return nr;
-}
 
 int hugetlb_sysctl_handler(struct ctl_table *table, int write,
 			   struct file *file, void __user *buffer,
