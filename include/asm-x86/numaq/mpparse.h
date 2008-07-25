13ba3520daa50caf1556e7f9b83f3e314e7782d5
Yinghai Lu <yhlu.kernel@gmail.com>
2008-07-25 02:17:55 -0700

    x86: mach-numaq to numaq
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/numaq/mpparse.h
+++ include/asm-x86/numaq/mpparse.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef ASM_X86__MACH_NUMAQ__MACH_MPPARSE_H
-#define ASM_X86__MACH_NUMAQ__MACH_MPPARSE_H
+#ifndef __ASM_NUMAQ_MPPARSE_H
+#define __ASM_NUMAQ_MPPARSE_H
 
 extern void numaq_mps_oem_check(struct mp_config_table *mpc, char *oem,
 				char *productid);
 
-#endif /* ASM_X86__MACH_NUMAQ__MACH_MPPARSE_H */
+#endif /* __ASM_NUMAQ_MPPARSE_H */
