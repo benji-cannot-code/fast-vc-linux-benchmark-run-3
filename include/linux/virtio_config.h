eb0afa45304fbdbeb3157f50cb00b7c2801ff1a9
Rusty Russell <rusty@rustcorp.com.au>
2008-07-25 12:06:03 -0500

    virtio: clarify that ABI is usable by any implementations
    We want others to implement and use virtio, so it makes sense to BSD
    license the non-__KERNEL__ parts of the headers to make this crystal
    clear.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Acked-by: Mark McLoughlin <markmc@redhat.com>
    Acked-by: Ryan Harper <ryanh@us.ibm.com>
    Acked-by: Eric Van Hensbergen <ericvh@gmail.com>
    Acked-by: Anthony Liguori <aliguori@us.ibm.com>

--- include/linux/virtio_config.h
+++ include/linux/virtio_config.h
@@ -1,6 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_VIRTIO_CONFIG_H
 #define _LINUX_VIRTIO_CONFIG_H
+/* This header, excluding the #ifdef __KERNEL__ part, is BSD licensed so
+ * anyone can use the definitions to implement compatible drivers/servers. */
+
 /* Virtio devices use a standardized configuration space to define their
  * features and pass configuration information, but each implementation can
  * store and access that space differently. */
