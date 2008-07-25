1922f771c6f1187050fe80fb31ab56322306141e
Arthur Jones <ajones@riverbed.com>
2008-07-25 01:49:06 -0700

    edac: i5100 fix enable ecc hardware
    It is possible that the BIOS did not enable ECC at boot time.  We check
    for that case and fail to load if it is true.
    
    Signed-off-by: Arthur Jones <ajones@riverbed.com>
    Signed-off-by: Doug Thompson <dougthompson@xmission.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/edac/i5100_edac.c
+++ drivers/edac/i5100_edac.c
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* register addresses and bit field accessors... */
 
 /* device 16, func 1 */
+#define I5100_MC		0x40	/* Memory Control Register */
+#define		I5100_MC_ERRDETEN(a)	((a) >> 5 & 1)
 #define I5100_MS		0x44	/* Memory Status Register */
 #define I5100_SPDDATA		0x48	/* Serial Presence Detect Status Reg */
 #define		I5100_SPDDATA_RDO(a)	((a) >> 15 & 1)
@@ -686,6 +688,14 @@ static int __devinit i5100_init_one(struct pci_dev *pdev,
 	rc = pci_enable_device(pdev);
 	if (rc < 0) {
 		ret = rc;
+		goto bail;
+	}
+
+	/* ECC enabled? */
+	pci_read_config_dword(pdev, I5100_MC, &dw);
+	if (!I5100_MC_ERRDETEN(dw)) {
+		printk(KERN_INFO "i5100_edac: ECC not enabled.\n");
+		ret = -ENODEV;
 		goto bail;
 	}
 
