6ddca2dd24da88c63bd3ab991a598f25eb119dec
Rene Herman <rene.herman@keyaccess.nl>
2008-07-25 19:44:42 -0700

    pnp: set the pnp_card dma_mask for use by ISAPnP cards
    dma_alloc_coherent() on x86 currently takes a passed in NULL device
    pointer to mean that it should allocate an ISA compatible (24-bit) buffer
    which is a bit of a hack.
    
    The ALSA ISA drivers are the main consumers of this but have a struct
    device in fact readily available.
    
    For the PnP drivers, the specific pnp_dev->dev device pointer is not
    always available at the right time so for now we want to pass the
    pnp_card->dev instead which is always available.  Set its dma_mask in
    preparation for doing so.
    
    This does not fix a current bug -- 2.6.26-rc1 stumbled over the NULL hack
    in dma_alloc_coherent() but this has already been fixed in commit
    4a367f3a9dbf2e7ffcee4702203479809236ee6e by Takashi Iwai.
    
    Signed-off-by: Rene Herman <rene.herman@gmail.com>
    Acked-by: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Acked-by: Takashi Iwai <tiwai@suse.de>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/pnp/card.c
+++ drivers/pnp/card.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/slab.h>
 #include <linux/pnp.h>
+#include <linux/dma-mapping.h>
 #include "base.h"
 
 LIST_HEAD(pnp_cards);
@@ -167,6 +168,9 @@ struct pnp_card *pnp_alloc_card(struct pnp_protocol *protocol, int id, char *pnp
 	card->dev.parent = &card->protocol->dev;
 	sprintf(card->dev.bus_id, "%02x:%02x", card->protocol->number,
 		card->number);
+
+	card->dev.coherent_dma_mask = DMA_24BIT_MASK;
+	card->dev.dma_mask = &card->dev.coherent_dma_mask;
 
 	dev_id = pnp_add_card_id(card, pnpid);
 	if (!dev_id) {
