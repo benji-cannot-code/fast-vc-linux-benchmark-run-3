71a22e1e74060541d92747c583ee66a65177bca7
Hans Verkuil <hverkuil@xs4all.nl>
2008-07-27 06:30:21 -0300

    V4L/DVB (8504): s2255drv: add missing header
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/s2255drv.c
+++ drivers/media/video/s2255drv.c
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/videodev2.h>
 #include <linux/version.h>
+#include <linux/mm.h>
 #include <media/videobuf-vmalloc.h>
 #include <media/v4l2-common.h>
 #include <media/v4l2-ioctl.h>
