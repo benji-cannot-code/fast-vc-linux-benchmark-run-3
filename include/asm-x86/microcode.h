50a68e4eff569c25672e09bb433fbc7795f38027
Peter Oruba <peter.oruba@amd.com>
2008-07-28 18:44:21 +0200

    x86: major refactoring
    Refactored code by introducing a two-module solution.
    
    There is one general module in which vendor specific modules can hook into.
    However, that is exclusive, there is only one vendor specific module
    allowed at a time. A CPU vendor check makes sure only the correct
    module for the underlying system gets called.
    
    Functinally in terms of patch loading itself there are no changes. This
    refactoring provides a basis for future implementations of other vendors'
    patch loaders.
    
    Signed-off-by: Peter Oruba <peter.oruba@amd.com>
    Cc: Tigran Aivazian <tigran@aivazian.fsnet.co.uk>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/microcode.h
+++ include/asm-x86/microcode.h
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+extern int microcode_init(void *opaque, struct module *module);
+extern void microcode_exit(void);
+
 struct microcode_ops {
 	long (*get_next_ucode)(void **mc, long offset);
 	long (*microcode_get_next_ucode)(void **mc, long offset);
