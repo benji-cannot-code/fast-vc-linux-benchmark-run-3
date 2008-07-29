08989432659a9a05e75b8db56fd6126ed2bfc420
Eric Miao <eric.miao@marvell.com>
2008-07-29 14:39:34 +0800

    [ARM] sa1100_wdt: use reset_status to remember watchdog reset status
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- drivers/watchdog/sa1100_wdt.c
+++ drivers/watchdog/sa1100_wdt.c
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/pxa-regs.h>
 #endif
 
+#include <asm/arch/reset.h>
+
 #include <asm/hardware.h>
 #include <asm/uaccess.h>
 
@@ -163,7 +165,8 @@ static int __init sa1100dog_init(void)
 	 * we suspend, RCSR will be cleared, and the watchdog
 	 * reset reason will be lost.
 	 */
-	boot_status = (RCSR & RCSR_WDR) ? WDIOF_CARDRESET : 0;
+	boot_status = (reset_status & RESET_STATUS_WATCHDOG) ?
+				WDIOF_CARDRESET : 0;
 	pre_margin = OSCR_FREQ * margin;
 
 	ret = misc_register(&sa1100dog_miscdev);
