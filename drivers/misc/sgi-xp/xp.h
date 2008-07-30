65b62f9eb24619dbcb1f19f4b052f04b8ceb5c87
Dean Nelson <dcn@sgi.com>
2008-07-29 22:34:14 -0700

    sgi-xp: use standard bitops macros and functions
    Change sgi-xp to use the standard bitops macros and functions instead of
    trying to invent its own mechanism.
    
    Signed-off-by: Dean Nelson <dcn@sgi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/misc/sgi-xp/xp.h
+++ drivers/misc/sgi-xp/xp.h
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sn/arch.h>
 #endif
 
-/* ??? Add this #define to some linux header file some day? */
-#define BYTES_PER_WORD	sizeof(void *)
-
 #ifdef USE_DBUG_ON
 #define DBUG_ON(condition)	BUG_ON(condition)
 #else
