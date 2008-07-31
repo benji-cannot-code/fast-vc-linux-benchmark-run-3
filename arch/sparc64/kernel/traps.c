1d6e354b2f872c2ce958438dab4d516431e5bb26
David S. Miller <davem@davemloft.net>
2008-07-31 01:06:02 -0700

    sparc64: Kill smp_report_regs().
    All the call sites are #if 0'd out and we have a much more
    useful global cpu dumping facility these days.  smp_report_regs()
    is way too verbose to be usable.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/traps.c
+++ arch/sparc64/kernel/traps.c
@@ -2178,7 +2178,6 @@ static inline struct reg_window *kernel_stack_up(struct reg_window *rw)
 void die_if_kernel(char *str, struct pt_regs *regs)
 {
 	static int die_counter;
-	extern void smp_report_regs(void);
 	int count = 0;
 	
 	/* Amuse the user. */
@@ -2216,11 +2215,6 @@ void die_if_kernel(char *str, struct pt_regs *regs)
 		}
 		user_instruction_dump ((unsigned int __user *) regs->tpc);
 	}
-#if 0
-#ifdef CONFIG_SMP
-	smp_report_regs();
-#endif
-#endif                                                	
 	if (regs->tstate & TSTATE_PRIV)
 		do_exit(SIGKILL);
 	do_exit(SIGSEGV);
