7856a47d3611aad97ecee4dbbc161ccaa2480b02
Jaroslav Kysela <perex@perex.cz>
2008-08-01 13:36:04 +0200

    sound: Revert "ALSA: Fix limit of 8 PCM devices in SNDRV_CTL_IOCTL_PCM_NEXT_DEVICE"
    This reverts commit fb3d6f2b77bdec75d45aa9d4464287ed87927866.
    
    New, updated patch with same subject replaces this commit.
    
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- sound/core/sound.c
+++ sound/core/sound.c
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kmod.h>
 #include <linux/mutex.h>
 
+#define SNDRV_OS_MINORS 256
+
 static int major = CONFIG_SND_MAJOR;
 int snd_major;
 EXPORT_SYMBOL(snd_major);
