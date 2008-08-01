a2db22fc4e07d1fceaab592d3ce5bc47866e40d9
Misha Zhilin <misha@epiphan.com>
2008-08-01 12:45:14 +0200

    ALSA: ice1724 - Support for Terrasoniq/MUSONIK TS22 PCI card
    Added support for Terrasoniq/MUSONIK TS22 PCI card.
    
    Signed-off-by: Misha Zhilin <misha@epiphan.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- sound/pci/ice1712/phase.h
+++ sound/pci/ice1712/phase.h
@@ -26,10 +26,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */      
 
 #define PHASE_DEVICE_DESC "{Terratec,Phase 22},"\
-                          "{Terratec,Phase 28},"
+                          "{Terratec,Phase 28},"\
+                          "{Terrasoniq,TS22},"
 
 #define VT1724_SUBDEVICE_PHASE22	0x3b155011
 #define VT1724_SUBDEVICE_PHASE28	0x3b154911
+#define VT1724_SUBDEVICE_TS22		0x3b157b11
 
 /* entry point */
 extern struct snd_ice1712_card_info snd_vt1724_phase_cards[];
