15bf29ec96400c9b146cb18436754b52df91882d
Dominik Brodowski <linux@dominikbrodowski.net>
2008-08-02 16:25:26 +0200

    pcmcia: remove remaining in-kernel pcmcia_get_configuration_info() users
    Remove the three remaining pcmcia_get_configuration_info() users:
    - pcmciamtd is marked broken anyway.
    - serial_cs.c can access the relevant structs directly
    - ipwireless didn't use the output
    
    CC: linux-serial@vger.kernel.org
    CC: Russell King <rmk+kernel@arm.linux.org.uk>
    Acked-by: David Sterba <dsterba@suse.cz>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/char/pcmcia/ipwireless/main.c
+++ drivers/char/pcmcia/ipwireless/main.c
@@ -84,7 +84,6 @@ static int config_ipwireless(struct ipw_dev *ipw)
 {
 	struct pcmcia_device *link = ipw->link;
 	int ret;
-	config_info_t conf;
 	tuple_t tuple;
 	unsigned short buf[64];
 	cisparse_t parse;
@@ -296,15 +295,6 @@ static int config_ipwireless(struct ipw_dev *ipw)
 	if (ret != CS_SUCCESS) {
 		cs_error(link, RequestIRQ, ret);
 		goto exit3;
-	}
-
-	/* Look up current Vcc */
-
-	ret = pcmcia_get_configuration_info(link, &conf);
-
-	if (ret != CS_SUCCESS) {
-		cs_error(link, GetConfigurationInfo, ret);
-		goto exit4;
 	}
 
 	printk(KERN_INFO IPWIRELESS_PCCARD_NAME ": Card type %s\n",
