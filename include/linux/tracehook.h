fc54d218c0dae3e3028e2ce5bd988814724efe1a
Roland McGrath <roland@redhat.com>
2008-08-04 13:56:01 -0700

    tracehook: kerneldoc fix
    My last change to tracehook.h made it confuse the kerneldoc parser.
    Move the #define's before the comment so it's happy again.
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Acked-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/tracehook.h
+++ include/linux/tracehook.h
@@ -488,6 +488,9 @@ static inline int tracehook_notify_jctl(int notify, int why)
 	return notify || (current->ptrace & PT_PTRACED);
 }
 
+#define DEATH_REAP			-1
+#define DEATH_DELAYED_GROUP_LEADER	-2
+
 /**
  * tracehook_notify_death - task is dead, ready to notify parent
  * @task:		@current task now exiting
@@ -502,8 +505,6 @@ static inline int tracehook_notify_jctl(int notify, int why)
  *
  * Called with write_lock_irq(&tasklist_lock) held.
  */
-#define DEATH_REAP			-1
-#define DEATH_DELAYED_GROUP_LEADER	-2
 static inline int tracehook_notify_death(struct task_struct *task,
 					 void **death_cookie, int group_dead)
 {
