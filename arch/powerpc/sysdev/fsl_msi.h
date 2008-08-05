6a972a9d8632ad4cb5d122ca4ae83c96f5ca69f9
Michael Ellerman <michael@ellerman.id.au>
2008-08-06 09:10:00 +1000

    powerpc: fsl_msi doesn't need it's own of_node
    The FSL MSI code keeps a pointer to the of_node from the device
    it represents.  However it also has an irq_host, which contains
    a pointer to the of_node, so use that one instead.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/sysdev/fsl_msi.h
+++ arch/powerpc/sysdev/fsl_msi.h
@@ -23,9 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FSL_PIC_IP_IPIC	0x00000002
 
 struct fsl_msi {
-	/* Device node of the MSI interrupt*/
-	struct device_node *of_node;
-
 	struct irq_host *irqhost;
 
 	unsigned long cascade_irq;
