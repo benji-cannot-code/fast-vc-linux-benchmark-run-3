e329aca9f0a007e6800bcfe260378adea43f2f74
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2008-08-05 11:45:19 +0200

    x86: configuration options to compile out x86 CPU support code
    This patch adds some configuration options that allow to compile out
    CPU vendor-specific code in x86 kernels (in arch/x86/kernel/cpu). The
    new configuration options are only visible when CONFIG_EMBEDDED is
    selected, as they are mostly interesting for space savings reasons.
    
    An example of size saving, on x86 with only Intel CPU support:
    
       text	   data	    bss	    dec	    hex	filename
    1125479	 118760	 212992	1457231	 163c4f	vmlinux.old
    1121355	 116536	 212992	1450883	 162383	vmlinux
      -4124   -2224       0   -6348   -18CC +/-
    
    However, I'm not exactly sure that the Kconfig wording is correct with
    regard to !64BIT / 64BIT.
    
    [ mingo@elte.hu: convert macro to inline ]
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/bugs.h
+++ include/asm-x86/bugs.h
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X86_BUGS_H
 
 extern void check_bugs(void);
+
+#ifdef CONFIG_CPU_SUP_INTEL_32
 int ppro_with_ram_bug(void);
+#else
+static inline int ppro_with_ram_bug(void) { return 0; }
+#endif
 
 #endif /* _ASM_X86_BUGS_H */
