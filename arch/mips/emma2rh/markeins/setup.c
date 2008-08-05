a0c6ef1b00042b687087542f2c125a7d8eb84a1c
Adrian Bunk <bunk@kernel.org>
2008-08-05 18:17:00 +0200

    remove unneeded #include <linux/ide.h>'s
    This patch remove unneeded #include <linux/ide.h>'s.
    
    It also adds a required #include <linux/interrupt.h> that was previously 
    implicitely pulled by ide.h
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    [bart: revert change to tests/lkdtm.c (spotted by Stephen Rothwell)]
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- arch/mips/emma2rh/markeins/setup.c
+++ arch/mips/emma2rh/markeins/setup.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/initrd.h>
 #include <linux/irq.h>
-#include <linux/ide.h>
 #include <linux/ioport.h>
 #include <linux/param.h>	/* for HZ */
 #include <linux/root_dev.h>
