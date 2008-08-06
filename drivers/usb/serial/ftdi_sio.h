3b68c9d48c8f12bdba2f52ade913f4a57ce1c811
Lex Ross <lross@mail.ru>
2008-08-06 16:25:08 +0400

    USB: ftdi_sio: add support for PHI Fisco data cable (FT232BM based, VID/PID 0403:e40b)
    Support for PHI Fisco USB to Serial data cable (FTDI FT232BM based).
    PHI Fisco cable is supplied for connecting Philips Xenium 9@9++ mobile phones.
    PIDs were missing.
    
    Tested successfully with PHI Fisco Data Cable (VID/PID 0403:e40b)
    
    Signed-off-by: Lex V. Ross <lross@mail.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -527,6 +527,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FTDI_ELV_WS500_PID	0xE0E9	/* PC-Wetterstation (WS 500) */
 #define FTDI_ELV_HS485_PID	0xE0EA	/* USB to RS-485 adapter */
 #define FTDI_ELV_EM1010PC_PID	0xE0EF	/* Engery monitor EM 1010 PC */
+#define FTDI_PHI_FISCO_PID      0xE40B  /* PHI Fisco USB to Serial cable */
 
 /*
  * Definitions for ID TECH (www.idt-net.com) devices
