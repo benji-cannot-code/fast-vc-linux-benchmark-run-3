44ef948a4a2e1401f729450cff9578f2a7194ba7
Sujith <Sujith.Manoharan@atheros.com>
2008-08-07 10:53:10 +0530

    ath9k: Remove redundant data structure ath9k_txq_info
    Use ath9k_tx_queue_info which contains the same elements,
    and merge get/set functions of tx queue properties.
    Also, fix whitespace damage in struct ath_softc.
    
    Signed-off-by: Sujith Manoharan <Sujith.Manoharan@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath9k/beacon.c
+++ drivers/net/wireless/ath9k/beacon.c
@@ -31,9 +31,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static int ath_beaconq_config(struct ath_softc *sc)
 {
 	struct ath_hal *ah = sc->sc_ah;
-	struct ath9k_txq_info qi;
+	struct ath9k_tx_queue_info qi;
 
-	ath9k_hw_gettxqueueprops(ah, sc->sc_bhalq, &qi);
+	ath9k_hw_get_txq_props(ah, sc->sc_bhalq, &qi);
 	if (sc->sc_opmode == ATH9K_M_HOSTAP) {
 		/* Always burst out beacon and CAB traffic. */
 		qi.tqi_aifs = 1;
@@ -46,7 +46,7 @@ static int ath_beaconq_config(struct ath_softc *sc)
 		qi.tqi_cwmax = sc->sc_beacon_qi.tqi_cwmax;
 	}
 
-	if (!ath9k_hw_settxqueueprops(ah, sc->sc_bhalq, &qi)) {
+	if (!ath9k_hw_set_txq_props(ah, sc->sc_bhalq, &qi)) {
 		DPRINTF(sc, ATH_DBG_FATAL,
 			"%s: unable to update h/w beacon queue parameters\n",
 			__func__);
@@ -336,7 +336,7 @@ static void ath_beacon_start_adhoc(struct ath_softc *sc, int if_id)
 
 int ath_beaconq_setup(struct ath_hal *ah)
 {
-	struct ath9k_txq_info qi;
+	struct ath9k_tx_queue_info qi;
 
 	memzero(&qi, sizeof(qi));
 	qi.tqi_aifs = 1;
