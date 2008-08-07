19c9fd2f6fed2753e067d05f965fa9db6faa83af
Haavard Skinnemoen <haavard.skinnemoen@atmel.com>
2008-08-07 14:08:49 +0200

    atmel-mci: Platform code for supporting multiple mmc slots
    Add the necessary platform infrastructure to support multiple mmc/sdcard
    slots all at once through a single controller. Currently, the driver
    will use the first valid slot it finds and stick with that, but later
    patches will add support for switching between several slots on the fly.
    
    Extend the platform data structure with per-slot information: MMC/SDcard
    bus width and card detect/write protect pins. This will affect the pin
    muxing as well as the capabilities announced to the mmc core.
    
    Note that board code is now required to supply a mci_platform_data
    struct to at32_add_device_mci().
    
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- arch/avr32/include/asm/atmel-mci.h
+++ arch/avr32/include/asm/atmel-mci.h
@@ -2,9 +2,29 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_AVR32_ATMEL_MCI_H
 #define __ASM_AVR32_ATMEL_MCI_H
 
-struct mci_platform_data {
+/**
+ * struct mci_slot_pdata - board-specific per-slot configuration
+ * @bus_width: Number of data lines wired up the slot
+ * @detect_pin: GPIO pin wired to the card detect switch
+ * @wp_pin: GPIO pin wired to the write protect sensor
+ *
+ * If a given slot is not present on the board, @bus_width should be
+ * set to 0. The other fields are ignored in this case.
+ *
+ * Any pins that aren't available should be set to a negative value.
+ */
+struct mci_slot_pdata {
+	unsigned int		bus_width;
 	int			detect_pin;
 	int			wp_pin;
+};
+
+/**
+ * struct mci_platform_data - board-specific MMC/SDcard configuration
+ * @slot: Per-slot configuration data.
+ */
+struct mci_platform_data {
+	struct mci_slot_pdata	slot[2];
 };
 
 #endif /* __ASM_AVR32_ATMEL_MCI_H */
