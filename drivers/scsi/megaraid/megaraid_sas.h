bb8c5ab7d0893130851c82fb3c0c4c2a999f71cc
Yang, Bo <Bo.Yang@lsi.com>
2008-08-10 12:42:39 -0700

    [SCSI] megaraid_sas: version and Documentation Update
    Signed-off-by: Bo Yang <bo.yang@lsi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/megaraid/megaraid_sas.h
+++ drivers/scsi/megaraid/megaraid_sas.h
@@ -19,9 +19,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * MegaRAID SAS Driver meta data
  */
-#define MEGASAS_VERSION			"00.00.03.20-rc1"
-#define MEGASAS_RELDATE			"March 10, 2008"
-#define MEGASAS_EXT_VERSION		"Mon. March 10 11:02:31 PDT 2008"
+#define MEGASAS_VERSION				"00.00.04.01"
+#define MEGASAS_RELDATE				"July 24, 2008"
+#define MEGASAS_EXT_VERSION			"Thu July 24 11:41:51 PST 2008"
 
 /*
  * Device IDs
