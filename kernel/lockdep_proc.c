89f4c33234061cb9ec4ee1c0550e6ffa857ea0d0
Rabin Vincent <rabin@rab.in>
2008-08-11 09:30:26 +0200

    lockdep: handle chains involving classes defined in modules
    Solve this by marking the classes as unused and not printing information
    about the unused classes.
    
    Reported-by: Eric Sesterhenn <snakebyte@gmx.de>
    Signed-off-by: Rabin Vincent <rabin@rab.in>
    Acked-by: Huang Ying <ying.huang@intel.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/lockdep_proc.c
+++ kernel/lockdep_proc.c
@@ -202,6 +202,9 @@ static int lc_show(struct seq_file *m, void *v)
 
 	for (i = 0; i < chain->depth; i++) {
 		class = lock_chain_get_class(chain, i);
+		if (!class->key)
+			continue;
+
 		seq_printf(m, "[%p] ", class->key);
 		print_name(m, class);
 		seq_puts(m, "\n");
