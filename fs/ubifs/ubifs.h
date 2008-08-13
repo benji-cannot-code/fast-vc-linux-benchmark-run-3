b7ec9e1fc77e492417469e60804688992390acc2
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2008-08-13 14:13:26 +0300

    UBIFS: few commentary fixes
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/ubifs.h
+++ fs/ubifs/ubifs.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *          Adrian Hunter
  */
 
-/* Implementation version 0.7 */
-
 #ifndef __UBIFS_H__
 #define __UBIFS_H__
 
