7d4c7587b4791b2d3a441175e6a15ed4126f62ab
Stefan Richter <stefanr@s5r6.in-berlin.de>
2008-08-16 00:11:48 +0200

    ieee1394: raw1394: replace BKL by local mutex, make ioctl() and mmap() thread-safe
    This removes the last usage of the Big Kernel Lock from the ieee1394
    stack, i.e. from raw1394's (unlocked_)ioctl and compat_ioctl.
    
    The ioctl()s don't need to take the BKL, but they need to be serialized
    per struct file *.  In particular, accesses to ->iso_state need to be
    serial.  We simply use a blocking mutex for this purpose because
    libraw1394 does not use O_NONBLOCK.  In practice, there is no lock
    contention anyway because most if not all libraw1394 clients use a
    libraw1394 handle only in a single thread.
    
    mmap() also accesses ->iso_state.  Until now this was unprotected
    against concurrent changes by ioctls.  Fix this bug while we are at it.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/ieee1394/raw1394.c
+++ drivers/ieee1394/raw1394.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/poll.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/vmalloc.h>
@@ -2542,11 +2543,18 @@ static int raw1394_read_cycle_timer(struct file_info *fi, void __user * uaddr)
 static int raw1394_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	struct file_info *fi = file->private_data;
+	int ret;
+
+	mutex_lock(&fi->state_mutex);
 
 	if (fi->iso_state == RAW1394_ISO_INACTIVE)
-		return -EINVAL;
+		ret = -EINVAL;
+	else
+		ret = dma_region_mmap(&fi->iso_handle->data_buf, file, vma);
 
-	return dma_region_mmap(&fi->iso_handle->data_buf, file, vma);
+	mutex_unlock(&fi->state_mutex);
+
+	return ret;
 }
 
 /* ioctl is only used for rawiso operations */
@@ -2660,10 +2668,12 @@ static long do_raw1394_ioctl(struct file *file, unsigned int cmd,
 static long raw1394_ioctl(struct file *file, unsigned int cmd,
 							unsigned long arg)
 {
+	struct file_info *fi = file->private_data;
 	long ret;
-	lock_kernel();
+
+	mutex_lock(&fi->state_mutex);
 	ret = do_raw1394_ioctl(file, cmd, arg);
-	unlock_kernel();
+	mutex_unlock(&fi->state_mutex);
 	return ret;
 }
 
@@ -2725,7 +2735,7 @@ static long raw1394_compat_ioctl(struct file *file,
 	void __user *argp = (void __user *)arg;
 	long err;
 
-	lock_kernel();
+	mutex_lock(&fi->state_mutex);
 	switch (cmd) {
 	/* These requests have same format as long as 'int' has same size. */
 	case RAW1394_IOC_ISO_RECV_INIT:
@@ -2758,7 +2768,7 @@ static long raw1394_compat_ioctl(struct file *file,
 		err = -EINVAL;
 		break;
 	}
-	unlock_kernel();
+	mutex_unlock(&fi->state_mutex);
 
 	return err;
 }
@@ -2792,6 +2802,7 @@ static int raw1394_open(struct inode *inode, struct file *file)
 	fi->notification = (u8) RAW1394_NOTIFY_ON;	/* busreset notification */
 
 	INIT_LIST_HEAD(&fi->list);
+	mutex_init(&fi->state_mutex);
 	fi->state = opened;
 	INIT_LIST_HEAD(&fi->req_pending);
 	INIT_LIST_HEAD(&fi->req_complete);
