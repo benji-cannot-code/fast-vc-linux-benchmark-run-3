dfb1fbffe6f3c37de7e20eb097cabe2891ab9138
Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
2008-08-14 17:25:37 -0700

    x86_64: uml: fix rename header guard
    In unistd_64.h, the guard macro _ASM_X86_64_UNISTD_H_ is renamed to
    ASM_X86__UNISTD_64_H.
    
    This change should be applied to arch/um/sys-x86_64/syscall_table.c.
    
    Signed-off-by: Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/um/sys-x86_64/syscall_table.c
+++ arch/um/sys-x86_64/syscall_table.c
@@ -42,12 +42,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define stub_rt_sigreturn sys_rt_sigreturn
 
 #define __SYSCALL(nr, sym) extern asmlinkage void sym(void) ;
-#undef _ASM_X86_64_UNISTD_H_
+#undef ASM_X86__UNISTD_64_H
 #include <asm-x86/unistd_64.h>
 
 #undef __SYSCALL
 #define __SYSCALL(nr, sym) [ nr ] = sym,
-#undef _ASM_X86_64_UNISTD_H_
+#undef ASM_X86__UNISTD_64_H
 
 typedef void (*sys_call_ptr_t)(void);
 
