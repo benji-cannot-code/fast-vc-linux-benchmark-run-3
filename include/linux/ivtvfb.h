5017578fced226767225c597800e88df4413d0f3
David Woodhouse <dwmw2@infradead.org>
2008-08-16 11:55:04 +0100

    Fix header export of videodev2.h, ivtv.h, ivtvfb.h
    The exported copy of videodev2.h contains this line:
    
    	#define #include <sys/time.h>
    
    This is because for some reason it defines __user for itself -- despite
    the fact that we remove all instances of __user when exporting headers.
    _All_ pointers in userspace are user pointers. Fix it by removing the
    unnecessary '#define __user' from the file.
    
    The new headers ivtv.h and ivtvfb.h would have the same problem... if
    whoever put them there had actually remembered to add them to the Kbuild
    file while he was at it. Fix those too, and export them as was
    presumably intended.
    
    Note that includes of <linux/compiler.h> are also stripped by the header
    export process, so those don't need to be conditional.
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>
    Acked-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/ivtvfb.h
+++ include/linux/ivtvfb.h
@@ -22,11 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_IVTVFB_H__
 #define __LINUX_IVTVFB_H__
 
-#ifdef __KERNEL__
-#include <linux/compiler.h> /* need __user */
-#else
-#define __user
-#endif
+#include <linux/compiler.h>
 #include <linux/types.h>
 
 /* Framebuffer external API */
