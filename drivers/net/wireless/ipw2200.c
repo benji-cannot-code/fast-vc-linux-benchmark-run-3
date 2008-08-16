1906ffb904191a3ab3d2510097c91aa5089a39e5
Huang Weiyi <weiyi.huang@gmail.com>
2008-08-17 07:50:47 +0800

    removed unused #include <version.h>
    The drivers below do not use LINUX_VERSION_CODE nor KERNEL_VERSION.
      drivers/net/wireless/ath5k/base.c
      drivers/net/wireless/b43/main.c
      drivers/net/wireless/ipw2100.c
      drivers/net/wireless/ipw2200.c
      drivers/net/wireless/iwlwifi/iwl-3945.c
      drivers/net/wireless/iwlwifi/iwl-4965.c
      drivers/net/wireless/iwlwifi/iwl-5000.c
      drivers/net/wireless/iwlwifi/iwl-agn.c
      drivers/net/wireless/iwlwifi/iwl-core.c
      drivers/net/wireless/iwlwifi/iwl-eeprom.c
      drivers/net/wireless/iwlwifi/iwl-hcmd.c
      drivers/net/wireless/iwlwifi/iwl-power.c
      drivers/net/wireless/iwlwifi/iwl3945-base.c
    
    This patch removes the said #include <version.h>.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ipw2200.c
+++ drivers/net/wireless/ipw2200.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 ******************************************************************************/
 
 #include "ipw2200.h"
-#include <linux/version.h>
 
 
 #ifndef KBUILD_EXTMOD
