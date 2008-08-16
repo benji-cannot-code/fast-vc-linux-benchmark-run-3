938dfc242e76babb80697ba6b5d46e5546b8adf9
Cyrill Gorcunov <gorcunov@gmail.com>
2008-08-16 23:21:50 +0400

    x86: apic - unify clear_local_APIC
    - Remove redundant masking of APIC_LVTTHMR register in apic_32.c
    
    - Add masking of APIC_LVTTHMR register to apic_64.c. We use a bit
      complicated #ifdef here: CONFIG_X86_MCE_P4THERMAL is 32bit specific
      and X86_MCE_INTEL is 64bit specific so the appropriate config variable
      will be set by Kconfig.
    
    - the APIC_ESR register clearing in apic_64.c now uses not straightforward
      way but this is allowed tradeoff.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic_32.c
+++ arch/x86/kernel/apic_32.c
@@ -755,7 +755,7 @@ void clear_local_APIC(void)
 	}
 
 	/* lets not touch this if we didn't frob it */
-#ifdef CONFIG_X86_MCE_P4THERMAL
+#if defined(CONFIG_X86_MCE_P4THERMAL) || defined(X86_MCE_INTEL)
 	if (maxlvt >= 5) {
 		v = apic_read(APIC_LVTTHMR);
 		apic_write(APIC_LVTTHMR, v | APIC_LVT_MASKED);
@@ -772,10 +772,6 @@ void clear_local_APIC(void)
 	if (maxlvt >= 4)
 		apic_write(APIC_LVTPC, APIC_LVT_MASKED);
 
-#ifdef CONFIG_X86_MCE_P4THERMAL
-	if (maxlvt >= 5)
-		apic_write(APIC_LVTTHMR, APIC_LVT_MASKED);
-#endif
 	/* Integrated APIC (!82489DX) ? */
 	if (lapic_is_integrated()) {
 		if (maxlvt > 3)
