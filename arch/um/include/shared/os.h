ea857a8ce75a3b79497e6335a6ffdc50391c9f07
Al Viro <viro@zeniv.linux.org.uk>
2008-08-17 13:48:37 -0400

    x86, um: take arch/um/include/* out of the way
    We can't just plop asm/* into it - userland helpers are built with it
    in search path and seeing asm/* show up there suddenly would be a bad
    idea.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/um/include/shared/os.h
+++ arch/um/include/shared/os.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "longjmp.h"
 #include "mm_id.h"
 #include "sysdep/tls.h"
-#include "../os/include/file.h"
+#include "../../os/include/file.h"
 
 #define CATCH_EINTR(expr) while ((errno = 0, ((expr) < 0)) && (errno == EINTR))
 
