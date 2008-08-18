ed647b69ea79023a9f0eb3ff22a5b02fe031f61e
Cyrill Gorcunov <gorcunov@gmail.com>
2008-08-18 20:45:49 +0400

    x86: apic - rearrange maxcpu definition
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic_32.c
+++ arch/x86/kernel/apic_32.c
@@ -115,6 +115,8 @@ static DEFINE_PER_CPU(struct clock_event_device, lapic_events);
 static int enabled_via_apicbase;
 
 static unsigned long apic_phys;
+unsigned int __cpuinitdata maxcpus = NR_CPUS;
+
 
 /*
  * Get the LAPIC version
@@ -1459,8 +1461,6 @@ void disconnect_bsp_APIC(int virt_wire_setup)
 		apic_write(APIC_LVT1, value);
 	}
 }
-
-unsigned int __cpuinitdata maxcpus = NR_CPUS;
 
 void __cpuinit generic_processor_info(int apicid, int version)
 {
