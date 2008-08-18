302e6444f7cbcd484a8f06e9c495990593788b88
Al Viro <viro@zeniv.linux.org.uk>
2008-08-18 04:01:47 -0400

    x86, um: get rid of uml unistd.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/um/sys-i386/syscalls.c
+++ arch/um/sys-i386/syscalls.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "linux/sched.h"
 #include "linux/shm.h"
 #include "linux/ipc.h"
+#include "linux/syscalls.h"
 #include "asm/mman.h"
 #include "asm/uaccess.h"
 #include "asm/unistd.h"
