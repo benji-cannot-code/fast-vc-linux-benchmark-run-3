9fee58ef7e596329dd2bc2e94bbb1a0384e2b284
Josh Boyer <jwboyer@linux.vnet.ibm.com>
2008-08-19 11:27:05 -0400

    powerpc/44x: Add explicit Yosemite support
    Add the Yosemite board to the explicitly supported list for ppc44x_simple
    boards and remove the compatible entry for bamboo from the DTS file.
    
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>

--- arch/powerpc/platforms/44x/ppc44x_simple.c
+++ arch/powerpc/platforms/44x/ppc44x_simple.c
@@ -58,7 +58,8 @@ static char *board[] __initdata = {
 	"amcc,katmai",
 	"amcc,rainier",
 	"amcc,sequoia",
-	"amcc,taishan"
+	"amcc,taishan",
+	"amcc,yosemite"
 };
 
 static int __init ppc44x_probe(void)
