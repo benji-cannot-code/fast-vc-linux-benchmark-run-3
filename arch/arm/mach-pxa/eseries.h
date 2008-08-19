b9873463aae07fe7bbe1ffa49b4ee9f74c8f7de1
Ian Molton <spyro@f2s.com>
2008-08-19 12:14:21 +0100

    [ARM] eseries: Split machine definitions
    This patchset breaks out the e-series machine definitions into
    one-per-machine.
    
    Signed-off-by: Ian Molton <spyro@f2s.com>

---
+++ arch/arm/mach-pxa/eseries.h
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+void __init eseries_fixup(struct machine_desc *desc,
+	struct tag *tags, char **cmdline, struct meminfo *mi);
+
