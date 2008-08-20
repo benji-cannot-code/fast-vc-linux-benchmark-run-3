41e628df0810d9e5698e5da7105498cf2ea5a70c
Yinghai Lu <yhlu.kernel@gmail.com>
2008-08-19 20:49:48 -0700

    x86: use nr_irqs
    also add first_free_entry and pin_map_size, which were NR_IRQS derived
    constants.
    
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/irq.h
+++ include/asm-x86/irq.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/apicdef.h>
 #include <asm/irq_vectors.h>
 
+extern int pin_map_size;
+extern int first_free_entry;
+
 static inline int irq_canonicalize(int irq)
 {
 	return ((irq == 2) ? 9 : irq);
