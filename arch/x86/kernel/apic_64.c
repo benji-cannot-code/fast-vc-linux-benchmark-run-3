da0018cf9afced651e9102e60d98c29415e30f9c
Yinghai Lu <yhlu.kernel@gmail.com>
2008-08-19 20:50:36 -0700

    x86: order variables in io_apic_xx.c
    move first_system_vector to apic_64.c.
    
    also add #ifdef CONFIG_INTR_REMAP to prepare 32 bit to use
    same file.
    
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic_64.c
+++ arch/x86/kernel/apic_64.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/smp.h>
 #include <asm/mtrr.h>
 #include <asm/mpspec.h>
+#include <asm/desc.h>
 #include <asm/hpet.h>
 #include <asm/pgalloc.h>
 #include <asm/nmi.h>
@@ -59,6 +60,10 @@ int x2apic_preenabled;
 /* Local APIC timer works in C2 */
 int local_apic_timer_c2_ok;
 EXPORT_SYMBOL_GPL(local_apic_timer_c2_ok);
+
+int first_system_vector = 0xfe;
+
+char system_vectors[NR_VECTORS] = { [0 ... NR_VECTORS-1] = SYS_VECTOR_FREE};
 
 /*
  * Debug level, exported for io_apic.c
