5aa3b05f557aa235de55f0d4f362ba02bfeda2a7
David Kilroy <kilroyd@gmail.com>
2008-08-21 23:27:53 +0100

    orinoco: Extend hermes_dld routines for Agere firmware
    Add programming initialisation and termination functions.
    Add checks to avoid overrunning the firmware image or PDA areas.
    Extra algorithm to program PDA values using defaults where necessary.
    
    Signed-off-by: David Kilroy <kilroyd@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/hermes_dld.h
+++ drivers/net/wireless/hermes_dld.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "hermes.h"
 
+int hermesi_program_init(hermes_t *hw, u32 offset);
+int hermesi_program_end(hermes_t *hw);
 int hermes_program(hermes_t *hw, const char *first_block, const char *end);
 
 int hermes_read_pda(hermes_t *hw,
@@ -38,6 +40,9 @@ int hermes_read_pda(hermes_t *hw,
 int hermes_apply_pda(hermes_t *hw,
 		     const char *first_pdr,
 		     const __le16 *pda);
+int hermes_apply_pda_with_defaults(hermes_t *hw,
+				   const char *first_pdr,
+				   const __le16 *pda);
 
 size_t hermes_blocks_length(const char *first_block);
 
