b3561029afbc57010e3cd1adeb7e48ccd5b3b79e
Dan Williams <dcbw@redhat.com>
2008-08-21 17:46:18 -0400

    libertas: convert CMD_802_11_DEAUTHENTICATE to a direct command
    and remove DISASSOCIATE because it's not in any of the specs and has
    never been used.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/assoc.h
+++ drivers/net/wireless/libertas/assoc.h
@@ -21,7 +21,7 @@ int lbs_cmd_80211_ad_hoc_start(struct lbs_private *priv,
 					struct cmd_ds_command *cmd,
 					void *pdata_buf);
 int lbs_cmd_80211_deauthenticate(struct lbs_private *priv,
-					  struct cmd_ds_command *cmd);
+				 u8 bssid[ETH_ALEN], u16 reason);
 int lbs_cmd_80211_associate(struct lbs_private *priv,
 				     struct cmd_ds_command *cmd,
 				     void *pdata_buf);
@@ -29,12 +29,9 @@ int lbs_cmd_80211_associate(struct lbs_private *priv,
 int lbs_ret_80211_ad_hoc_start(struct lbs_private *priv,
 					struct cmd_ds_command *resp);
 int lbs_ret_80211_ad_hoc_stop(struct lbs_private *priv);
-int lbs_ret_80211_disassociate(struct lbs_private *priv);
 int lbs_ret_80211_associate(struct lbs_private *priv,
 				     struct cmd_ds_command *resp);
 
 int lbs_stop_adhoc_network(struct lbs_private *priv);
-
-int lbs_send_deauthentication(struct lbs_private *priv);
 
 #endif /* _LBS_ASSOC_H */
