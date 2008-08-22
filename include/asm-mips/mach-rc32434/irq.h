f0021a9315bcdd551819f79ad19100e80c030d23
Florian Fainelli <florian@openwrt.org>
2008-08-22 17:00:22 +0200

    MIPS: RB532: Cleanup and group definitions to their right places
    This patch moves GPIO related definitions to gpio.h and IRQ
    related to irq.h
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mach-rc32434/irq.h
+++ include/asm-mips/mach-rc32434/irq.h
@@ -6,4 +6,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach-generic/irq.h>
 
+#define ETH0_DMA_RX_IRQ   	(GROUP1_IRQ_BASE + 0)
+#define ETH0_DMA_TX_IRQ   	(GROUP1_IRQ_BASE + 1)
+#define ETH0_RX_OVR_IRQ   	(GROUP3_IRQ_BASE + 9)
+#define ETH0_TX_UND_IRQ   	(GROUP3_IRQ_BASE + 10)
+
 #endif  /* __ASM_RC32434_IRQ_H */
