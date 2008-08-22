e599c4823c70a2cec82bf82ad775a76ae27f92d7
Laurent Pinchart <laurent.pinchart@skynet.be>
2008-08-22 17:25:10 -0300

    V4L/DVB (8754): uvcvideo: Implement the USB power management reset_resume method.
    When a suspended device has been reset instead of being resumed, USB core
    calls the reset_resume method if available instead of unbinding and rebinding
    the device.
    
    This patch implements reset_resume by reusing the current resume
    implementation and simplifies the resume method by skipping the controls
    restore stage. Resuming from autosuspend should be faster.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@skynet.be>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/uvc/uvc_driver.c
+++ drivers/media/video/uvc/uvc_driver.c
@@ -1664,7 +1664,7 @@ static int uvc_suspend(struct usb_interface *intf, pm_message_t message)
 	return uvc_video_suspend(&dev->video);
 }
 
-static int uvc_resume(struct usb_interface *intf)
+static int __uvc_resume(struct usb_interface *intf, int reset)
 {
 	struct uvc_device *dev = usb_get_intfdata(intf);
 	int ret;
@@ -1673,7 +1673,7 @@ static int uvc_resume(struct usb_interface *intf)
 		intf->cur_altsetting->desc.bInterfaceNumber);
 
 	if (intf->cur_altsetting->desc.bInterfaceSubClass == SC_VIDEOCONTROL) {
-		if ((ret = uvc_ctrl_resume_device(dev)) < 0)
+		if (reset && (ret = uvc_ctrl_resume_device(dev)) < 0)
 			return ret;
 
 		return uvc_status_resume(dev);
@@ -1686,6 +1686,16 @@ static int uvc_resume(struct usb_interface *intf)
 	}
 
 	return uvc_video_resume(&dev->video);
+}
+
+static int uvc_resume(struct usb_interface *intf)
+{
+	return __uvc_resume(intf, 0);
+}
+
+static int uvc_reset_resume(struct usb_interface *intf)
+{
+	return __uvc_resume(intf, 1);
 }
 
 /* ------------------------------------------------------------------------
@@ -1953,6 +1963,7 @@ struct uvc_driver uvc_driver = {
 		.disconnect	= uvc_disconnect,
 		.suspend	= uvc_suspend,
 		.resume		= uvc_resume,
+		.reset_resume	= uvc_reset_resume,
 		.id_table	= uvc_ids,
 		.supports_autosuspend = 1,
 	},
