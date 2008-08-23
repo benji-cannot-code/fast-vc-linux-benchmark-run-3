e00cc1ea07f205a895b15840b9e0d0006b6892a0
Hans Verkuil <hverkuil@xs4all.nl>
2008-08-23 05:31:47 -0300

    V4L/DVB (8780): v4l: replace the last uses of video_exclusive_open/release
    Handle the video_exclusive_open/release functionality inside the
    driver.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/meye.c
+++ drivers/media/video/meye.c
@@ -844,21 +844,16 @@ again:
 
 static int meye_open(struct inode *inode, struct file *file)
 {
-	int i, err;
+	int i;
 
-	lock_kernel();
-	err = video_exclusive_open(inode, file);
-	if (err < 0) {
-		unlock_kernel();
-		return err;
-	}
+	if (test_and_set_bit(0, &meye.in_use))
+		return -EBUSY;
 
 	mchip_hic_stop();
 
 	if (mchip_dma_alloc()) {
 		printk(KERN_ERR "meye: mchip framebuffer allocation failed\n");
-		video_exclusive_release(inode, file);
-		unlock_kernel();
+		clear_bit(0, &meye.in_use);
 		return -ENOBUFS;
 	}
 
@@ -866,7 +861,6 @@ static int meye_open(struct inode *inode, struct file *file)
 		meye.grab_buffer[i].state = MEYE_BUF_UNUSED;
 	kfifo_reset(meye.grabq);
 	kfifo_reset(meye.doneq);
-	unlock_kernel();
 	return 0;
 }
 
@@ -874,7 +868,7 @@ static int meye_release(struct inode *inode, struct file *file)
 {
 	mchip_hic_stop();
 	mchip_dma_free();
-	video_exclusive_release(inode, file);
+	clear_bit(0, &meye.in_use);
 	return 0;
 }
 
