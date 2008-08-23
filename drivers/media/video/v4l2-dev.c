f6946a3c424657a65b934f143aa06f9a90f2c35b
Hans Verkuil <hverkuil@xs4all.nl>
2008-08-23 06:27:59 -0300

    V4L/DVB (8784): v4l2-dev: make the video_device's release callback mandatory
    Now that all drivers set the release callback in the video_device
    struct we can put in a BUG_ON in video_register_device to ensure that
    the callback is always there.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/v4l2-dev.c
+++ drivers/media/video/v4l2-dev.c
@@ -85,11 +85,6 @@ static void video_release(struct device *cd)
 {
 	struct video_device *vfd = container_of(cd, struct video_device, dev);
 
-#if 1
-	/* needed until all drivers are fixed */
-	if (!vfd->release)
-		return;
-#endif
 	vfd->release(vfd);
 }
 
@@ -234,8 +229,8 @@ int video_register_device_index(struct video_device *vfd, int type, int nr,
 	int ret;
 	char *name_base;
 
-	if (vfd == NULL)
-		return -EINVAL;
+	/* the release callback MUST be present */
+	BUG_ON(!vfd->release);
 
 	if (vfd == NULL)
 		return -EINVAL;
@@ -313,13 +308,6 @@ int video_register_device_index(struct video_device *vfd, int type, int nr,
 		goto fail_minor;
 	}
 
-#if 1
-	/* needed until all drivers are fixed */
-	if (!vfd->release)
-		printk(KERN_WARNING "videodev: \"%s\" has no release callback. "
-		       "Please fix your driver for proper sysfs support, see "
-		       "http://lwn.net/Articles/36850/\n", vfd->name);
-#endif
 	return 0;
 
 fail_minor:
