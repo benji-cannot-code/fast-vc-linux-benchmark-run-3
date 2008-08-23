1fccc5496304a53f1e4fff1e7920a99d614a9a7e
Florian Fainelli <florian@openwrt.org>
2008-08-23 18:53:24 +0200

    MIPS: RB532: Remove obsolute reference to setup_serial_port
    We are no longer using setup_serial_port. So just remove it
    from the prom code.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/rb532/prom.c
+++ arch/mips/rb532/prom.c
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-rc32434/ddr.h>
 #include <asm/mach-rc32434/prom.h>
 
-extern void __init setup_serial_port(void);
-
 unsigned int idt_cpu_freq = 132000000;
 EXPORT_SYMBOL(idt_cpu_freq);
 
