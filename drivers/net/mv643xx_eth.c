4afba3c27096bf7999ca3a011d5c27b6d1fe9506
Lennert Buytenhek <buytenh@wantstofly.org>
2008-08-24 07:33:05 +0200

    mv643xx_eth: bump version to 1.3
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>

--- drivers/net/mv643xx_eth.c
+++ drivers/net/mv643xx_eth.c
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/system.h>
 
 static char mv643xx_eth_driver_name[] = "mv643xx_eth";
-static char mv643xx_eth_driver_version[] = "1.2";
+static char mv643xx_eth_driver_version[] = "1.3";
 
 #define MV643XX_ETH_CHECKSUM_OFFLOAD_TX
 #define MV643XX_ETH_NAPI
