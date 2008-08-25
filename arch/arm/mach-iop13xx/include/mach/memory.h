9bfd36ca99f63c1335312904419966b71b08c83c
Lennert Buytenhek <buytenh@wantstofly.org>
2008-08-25 21:03:32 +0100

    [ARM] 5222/1: Allow configuring user:kernel split via Kconfig
    This patch adds a config option (CONFIG_VMSPLIT_*) to allow choosing
    between 3:1, 2:2 and 1:3 user:kernel memory splits.
    
    Tested-by: Riku Voipio <riku.voipio@iki.fi>
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-iop13xx/include/mach/memory.h
+++ arch/arm/mach-iop13xx/include/mach/memory.h
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Physical DRAM offset.
  */
 #define PHYS_OFFSET	UL(0x00000000)
-#define TASK_SIZE  	UL(0x3f000000)
-#define PAGE_OFFSET	UL(0x40000000)
-#define TASK_UNMAPPED_BASE ((TASK_SIZE + 0x01000000) / 3)
 
 #ifndef __ASSEMBLY__
 
