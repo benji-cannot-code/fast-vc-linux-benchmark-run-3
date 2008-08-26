ca42db5ba170f214876ffa592f6a08ff46b8e2b9
Jens Axboe <jens.axboe@oracle.com>
2008-08-26 09:15:47 +0200

    block: use linux/uaccess.h in elevator.c instead of asm variant
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/elevator.c
+++ block/elevator.c
@@ -35,8 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/blktrace_api.h>
 #include <linux/hash.h>
-
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 static DEFINE_SPINLOCK(elv_list_lock);
 static LIST_HEAD(elv_list);
