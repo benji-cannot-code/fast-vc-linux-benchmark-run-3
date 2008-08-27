8dd20798984a75bb3dc3d70ea14e16952cf654c7
David S. Miller <davem@davemloft.net>
2008-08-26 22:25:03 -0700

    sparc: Delete bare sbus char bpp driver, obsoleted by parport_sunbpp
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/ebus.c
+++ arch/sparc64/kernel/ebus.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ebus.h>
 #include <asm/oplib.h>
 #include <asm/prom.h>
-#include <asm/bpp.h>
 #include <asm/irq.h>
 #include <asm/io.h>
 
