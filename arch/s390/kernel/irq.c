f9e5285a279f59ab83ec6ddc9f1fdb32b407c490
Ingo Molnar <mingo@elte.hu>
2008-08-28 08:56:33 +0200

    sparseirq: export nr_irqs on m68k/sparc/s390
    Stephen Rothwell reported such build failures on m68k/sparc/s390:
    
    > ERROR: "nr_irqs" [drivers/net/hamradio/baycom_ser_fdx.ko] undefined!
    > ERROR: "nr_irqs" [drivers/net/3c59x.ko] undefined!
    
    export nr_irqs on these architectures too.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/s390/kernel/irq.c
+++ arch/s390/kernel/irq.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/profile.h>
 
 int nr_irqs = NR_IRQS;
+EXPORT_SYMBOL(nr_irqs);
 
 /*
  * show_interrupts is needed by /proc/interrupts.
--- arch/sparc/kernel/irq.c
+++ arch/sparc/kernel/irq.c
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif /* SMP */
 
 int nr_irqs = NR_IRQS;
+EXPORT_SYMBOL(nr_irqs);
 
 unsigned long __raw_local_irq_save(void)
 {
