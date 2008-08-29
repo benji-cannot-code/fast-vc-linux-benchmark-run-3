d75fb74a6787ebec0cc399f893998445252f12e0
David S. Miller <davem@davemloft.net>
2008-08-28 22:16:15 -0700

    sparc64: Use generic CMOS driver.
    Based largely upon a patch by Krzysztof Helt <krzysztof.h1@poczta.fm>
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/mc146818rtc_64.h
+++ arch/sparc/include/asm/mc146818rtc_64.h
@@ -8,12 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 
 #ifndef RTC_PORT
-#ifdef CONFIG_PCI
-extern unsigned long ds1287_regs;
-#else
-#define ds1287_regs (0UL)
-#endif
-#define RTC_PORT(x)	(ds1287_regs + (x))
+extern unsigned long cmos_regs;
+#define RTC_PORT(x)	(cmos_regs + (x))
 #define RTC_ALWAYS_BCD	0
 #endif
 
@@ -29,7 +25,5 @@ inb_p(RTC_PORT(1)); \
 outb_p((addr),RTC_PORT(0)); \
 outb_p((val),RTC_PORT(1)); \
 })
-
-#define RTC_IRQ 8
 
 #endif /* __ASM_SPARC64_MC146818RTC_H */
