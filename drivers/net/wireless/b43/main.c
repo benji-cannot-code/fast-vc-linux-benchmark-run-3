d6b0c52dba6d52459ec243f6a8118f3f88d5e793
Michael Buesch <mb@bu3sch.de>
2008-08-30 02:27:19 +0200

    b43: Move code from nphy.* to phy_n.*
    Sourcecode files for PHY code are named by phy_XXX.{c,h}
    where XXX is the PHY type.
    Move the N-PHY code to match the other files.
    
    Signed-off-by: Michael Buesch <mb@bu3sch.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/main.c
+++ drivers/net/wireless/b43/main.c
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "debugfs.h"
 #include "phy_common.h"
 #include "phy_g.h"
-#include "nphy.h"
+#include "phy_n.h"
 #include "dma.h"
 #include "pio.h"
 #include "sysfs.h"
--- drivers/net/wireless/b43/phy_common.c
+++ drivers/net/wireless/b43/phy_common.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "phy_common.h"
 #include "phy_g.h"
 #include "phy_a.h"
-#include "nphy.h"
+#include "phy_n.h"
 #include "b43.h"
 #include "main.h"
 
