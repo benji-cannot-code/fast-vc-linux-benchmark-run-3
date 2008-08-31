de7e94e0f7475e08c656c6da611713db1e2f52c2
Dominik Brodowski <linux@dominikbrodowski.net>
2008-08-31 15:20:26 +0200

    pcmcia: card services header cleanup
    16-bit PCMCIA device handling function definitions are moved to ds.h,
    internal definitions to cs_internal.h.
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/cs.c
+++ drivers/pcmcia/cs.c
@@ -753,7 +753,7 @@ EXPORT_SYMBOL(pccard_register_pcmcia);
  * CIS register.
  */
 
-int pccard_reset_card(struct pcmcia_socket *skt)
+int pcmcia_reset_card(struct pcmcia_socket *skt)
 {
 	int ret;
 
@@ -792,7 +792,7 @@ int pccard_reset_card(struct pcmcia_socket *skt)
 
 	return ret;
 } /* reset_card */
-EXPORT_SYMBOL(pccard_reset_card);
+EXPORT_SYMBOL(pcmcia_reset_card);
 
 
 /* These shut down or wake up a socket.  They are sort of user
