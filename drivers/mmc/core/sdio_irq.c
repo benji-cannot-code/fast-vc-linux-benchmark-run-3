218210257a5186486e7a85079ef9c7831027ed3f
Pierre Ossman <drzeus@drzeus.cx>
2008-08-31 13:38:54 +0200

    sdio: make sleep on error interruptable
    Make sure we can be woken from the forced sleep that is done on errors.
    Removing a card often results in -ENOMEDIUM or -EILSEQ so we previously
    locked up the removal process for a second.
    
    We could completely exit on -ENOMEDIUM, but it might be a transient
    glitch so treat it like any other error.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/core/sdio_irq.c
+++ drivers/mmc/core/sdio_irq.c
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Created:     June 18, 2007
  * Copyright:   MontaVista Software Inc.
  *
+ * Copyright 2008 Pierre Ossman
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at
@@ -108,11 +110,14 @@ static int sdio_irq_thread(void *_host)
 
 		/*
 		 * Give other threads a chance to run in the presence of
-		 * errors.  FIXME: determine if due to card removal and
-		 * possibly exit this thread if so.
+		 * errors.
 		 */
-		if (ret < 0)
-			ssleep(1);
+		if (ret < 0) {
+			set_current_state(TASK_INTERRUPTIBLE);
+			if (!kthread_should_stop())
+				schedule_timeout(HZ);
+			set_current_state(TASK_RUNNING);
+		}
 
 		/*
 		 * Adaptive polling frequency based on the assumption
