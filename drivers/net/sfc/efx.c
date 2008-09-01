36a40b07ce0aff5c01dc0c4418acf02abead0556
Ben Hutchings <bhutchings@solarflare.com>
2008-09-01 12:46:16 +0100

    sfc: Remove inclusion of workarounds.h from efx.c
    efx.c does not use hardware workarounds.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/sfc/efx.c
+++ drivers/net/sfc/efx.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "efx.h"
 #include "mdio_10g.h"
 #include "falcon.h"
-#include "workarounds.h"
 #include "mac.h"
 
 #define EFX_MAX_MTU (9 * 1024)
