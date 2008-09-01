96c3d7a12edc316ce81aea0e7cf7f18e6d0989b8
David S. Miller <davem@davemloft.net>
2008-08-31 21:48:12 -0700

    sparc64: Clean up CPU chip type probing code.
    Three main things:
    
    1) Make prober an arch initcall instead of using hard-coded invocation
       from paging_init()
    
    2) Shrink table size, the fpu ident stuff was never used.
    
    3) Use named struct initialized in table.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/entry.h
+++ arch/sparc64/kernel/entry.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/init.h>
 
-extern char *sparc_cpu_type;
-extern char *sparc_fpu_type;
+extern const char *sparc_cpu_type;
+extern const char *sparc_fpu_type;
 
 extern void __init per_cpu_patch(void);
 extern void __init sun4v_patch(void);
