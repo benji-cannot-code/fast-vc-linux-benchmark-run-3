fb5ccfca78bd257f70b821b52bbcd8c5a81c04f2
Ben Hutchings <bhutchings@solarflare.com>
2008-09-01 12:48:03 +0100

    sfc: Remove efx_channel::evqnum field
    It is redundant with efx_channel::channel.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/sfc/net_driver.h
+++ drivers/net/sfc/net_driver.h
@@ -323,7 +323,6 @@ enum efx_rx_alloc_method {
  * queue.
  *
  * @efx: Associated Efx NIC
- * @evqnum: Event queue number
  * @channel: Channel instance number
  * @used_flags: Channel is used by net driver
  * @enabled: Channel enabled indicator
@@ -354,7 +353,6 @@ enum efx_rx_alloc_method {
  */
 struct efx_channel {
 	struct efx_nic *efx;
-	int evqnum;
 	int channel;
 	int used_flags;
 	bool enabled;
