9710e323dfc4b535706f24149a594093c221e5a5
Michael Buesch <mb@bu3sch.de>
2008-09-02 13:00:34 +0200

    b43: Split PHY alloc and init
    This splits the PHY allocation from the PHY init.
    This is needed in order to properly support Analog handling.
    
    Signed-off-by: Michael Buesch <mb@bu3sch.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/phy_common.c
+++ drivers/net/wireless/b43/phy_common.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "main.h"
 
 
-int b43_phy_operations_setup(struct b43_wldev *dev)
+int b43_phy_allocate(struct b43_wldev *dev)
 {
 	struct b43_phy *phy = &(dev->phy);
 	int err;
@@ -69,6 +69,12 @@ int b43_phy_operations_setup(struct b43_wldev *dev)
 		phy->ops = NULL;
 
 	return err;
+}
+
+void b43_phy_free(struct b43_wldev *dev)
+{
+	dev->phy.ops->free(dev);
+	dev->phy.ops = NULL;
 }
 
 int b43_phy_init(struct b43_wldev *dev)
--- drivers/net/wireless/b43/phy_lp.h
+++ drivers/net/wireless/b43/phy_lp.h
@@ -531,7 +531,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 struct b43_phy_lp {
-	bool initialised;
+	//TODO
 };
 
 
