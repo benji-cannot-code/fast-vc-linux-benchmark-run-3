0a716e1dc86590f5cfe9201d8c2f88150af25023
Steven Toth <stoth@hauppauge.com>
2008-09-03 17:12:12 -0300

    V4L/DVB (8805): Steven Toth email address change
    I need this so I can better isolate my linux email from my
    corporate email.
    
    Signed-off-by: Steven Toth <stoth@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/s5h1409.c
+++ drivers/media/dvb/frontends/s5h1409.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
     Samsung S5H1409 VSB/QAM demodulator driver
 
-    Copyright (C) 2006 Steven Toth <stoth@hauppauge.com>
+    Copyright (C) 2006 Steven Toth <stoth@linuxtv.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
