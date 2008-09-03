eb92ab4642f84b573296fc4c0a58df8fd8586355
Suresh Siddha <suresh.b.siddha@intel.com>
2008-09-03 16:58:35 -0700

    dmar: fix dmar_parse_dev() devices_cnt error condition check
    It is possible that,
    instead of PCI endpoint/sub-hierarchy structures, only IO-APIC/HPET
    devices may be reported under device scope structures. Fix the devices_cnt
    error check, which cares about only PCI structures and removes the
    dma-remapping unit structure (dmaru) when the devices_cnt is zero
    and include_all flag is not set.
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Acked-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/pci/dmar.c
+++ drivers/pci/dmar.c
@@ -213,7 +213,7 @@ dmar_parse_dev(struct dmar_drhd_unit *dmaru)
 		include_all = 1;
 	}
 
-	if (ret || (dmaru->devices_cnt == 0 && !dmaru->include_all)) {
+	if (ret) {
 		list_del(&dmaru->list);
 		kfree(dmaru);
 	}
