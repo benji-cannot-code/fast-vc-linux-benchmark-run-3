542a26eda94a50d841b4cf2e5832e89ae8631095
Oliver Endriss <o.endriss@gmx.de>
2008-09-04 20:35:19 -0300

    V4L/DVB (8890): budget: Add callback to load firmware for the TDHD1 tuner
    Supply callback to load firmware for the TDHD1 tuner (using request_firmware).
    
    Signed-off-by: Oliver Endriss <o.endriss@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/tdhd1.h
+++ drivers/media/dvb/frontends/tdhd1.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "tda1004x.h"
 
+static int alps_tdhd1_204_request_firmware(struct dvb_frontend *fe, const struct firmware **fw, char *name);
+
 static struct tda1004x_config alps_tdhd1_204a_config = {
 	.demod_address = 0x8,
 	.invert = 1,
@@ -36,6 +38,7 @@ static struct tda1004x_config alps_tdhd1_204a_config = {
 	.xtal_freq = TDA10046_XTAL_4M,
 	.agc_config = TDA10046_AGC_DEFAULT,
 	.if_freq = TDA10046_FREQ_3617,
+	.request_firmware = alps_tdhd1_204_request_firmware
 };
 
 static int alps_tdhd1_204a_tuner_set_params(struct dvb_frontend *fe, struct dvb_frontend_parameters *params)
