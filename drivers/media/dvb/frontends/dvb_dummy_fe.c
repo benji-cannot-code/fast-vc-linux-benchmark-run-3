d0035b7a4442478fde7b5283cbd15c7b8cc6ef48
Hans Verkuil <hverkuil@xs4all.nl>
2008-09-04 03:33:43 -0300

    V4L/DVB (8906): v4l-dvb: fix assorted sparse warnings
    Fix sparse warnings. None are serious, but cutting down on these helps find
    future serious sparse warnings/errors.
    
    Redid the av7710.c patch based on a suggestion by Oliver Endriss.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/dvb_dummy_fe.c
+++ drivers/media/dvb/frontends/dvb_dummy_fe.c
@@ -133,7 +133,7 @@ error:
 
 static struct dvb_frontend_ops dvb_dummy_fe_qpsk_ops;
 
-struct dvb_frontend* dvb_dummy_fe_qpsk_attach()
+struct dvb_frontend *dvb_dummy_fe_qpsk_attach(void)
 {
 	struct dvb_dummy_fe_state* state = NULL;
 
@@ -153,7 +153,7 @@ error:
 
 static struct dvb_frontend_ops dvb_dummy_fe_qam_ops;
 
-struct dvb_frontend* dvb_dummy_fe_qam_attach()
+struct dvb_frontend *dvb_dummy_fe_qam_attach(void)
 {
 	struct dvb_dummy_fe_state* state = NULL;
 
