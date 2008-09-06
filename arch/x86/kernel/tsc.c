583df472d491145570293d3c707570d78f99f8a5
Ingo Molnar <mingo@elte.hu>
2008-09-06 23:55:40 +0200

    x86, tsc calibration: fix
    my brown paperbag day ...
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/tsc.c
+++ arch/x86/kernel/tsc.c
@@ -320,7 +320,7 @@ static unsigned long quick_pit_calibrate(void)
 		/*
 		 * Make sure we can rely on the second TSC timestamp:
 		 */
-		if (!pit_expect_msb(--expect))
+		if (!pit_expect_msb(expect))
 			goto failed;
 
 		/*
