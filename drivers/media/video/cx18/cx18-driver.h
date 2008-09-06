6438262d36b322999a849920b3351e8e3850526f
Andy Walls <awalls@radix.net>
2008-09-06 16:43:59 -0300

    V4L/DVB (8924): cx18: Set mmio throttling delay default to 0 nsec.
    cx18: Set mmio throttling delay default to 0 nsec.  Not doing so makes analog
    tuner audio not work on some mahcines.
    
    Signed-off-by: Andy Walls <awalls@radix.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-driver.h
+++ drivers/media/video/cx18/cx18-driver.h
@@ -64,8 +64,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  error "This driver requires kernel PCI support."
 #endif
 
-/* Default delay to throttle mmio access to the CX23418 so it doesn't choke */
-#define CX18_DEFAULT_MMIO_NDELAY 31 /* 30.3 ns = 1 PCI clock(s) / 33 MHz */
+/* Default delay to throttle mmio access to the CX23418 */
+#define CX18_DEFAULT_MMIO_NDELAY 0 /* 0 ns = 0 PCI clock(s) / 33 MHz */
 
 #define CX18_MEM_OFFSET	0x00000000
 #define CX18_MEM_SIZE	0x04000000
