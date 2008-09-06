e5ffe512dfbf41da7dca9513a109d1cc631f5f1a
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-09-06 12:13:59 +0100

    [ARM] Convert asm/bitops.h to linux/bitops.h
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-omap2/clock.c
+++ arch/arm/mach-omap2/clock.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/clk.h>
 #include <linux/io.h>
-#include <asm/bitops.h>
+#include <linux/bitops.h>
 
 #include <mach/clock.h>
 #include <mach/sram.h>
