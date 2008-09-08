79dc50d298e15bc7e06f8dff5a7f9716c5550997
Yinghai Lu <yhlu.kernel@gmail.com>
2008-09-07 17:58:51 -0700

    x86: remove duplicated force_mwait
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/amd_64.c
+++ arch/x86/kernel/cpu/amd_64.c
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "cpu.h"
 
-int force_mwait __cpuinitdata;
-
 #ifdef CONFIG_NUMA
 static int __cpuinit nearby_node(int apicid)
 {
