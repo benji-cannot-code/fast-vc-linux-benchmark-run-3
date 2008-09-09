85efa96c59dc231397e7beb248ab0f6ed54f7b3a
David Fries <david@fries.net>
2008-09-08 22:01:14 -0500

    [netdrvr] ne: Fix suspend and resume for ISA PnP cards.
    A call to pnp_stop_dev and pnp_start_dev now shuts down and
    initializes plug and play devices for suspend and resume.
    
    Signed-off-by: David Fries <david@fries.net>
    Cc: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Cc: Paul Gortmaker <p_gortmaker@yahoo.com>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Cc: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/ne.c
+++ drivers/net/ne.c
@@ -845,8 +845,12 @@ static int ne_drv_suspend(struct platform_device *pdev, pm_message_t state)
 {
 	struct net_device *dev = platform_get_drvdata(pdev);
 
-	if (netif_running(dev))
+	if (netif_running(dev)) {
+		struct pnp_dev *idev = (struct pnp_dev *)ei_status.priv;
 		netif_device_detach(dev);
+		if (idev)
+			pnp_stop_dev(idev);
+	}
 	return 0;
 }
 
@@ -855,6 +859,9 @@ static int ne_drv_resume(struct platform_device *pdev)
 	struct net_device *dev = platform_get_drvdata(pdev);
 
 	if (netif_running(dev)) {
+		struct pnp_dev *idev = (struct pnp_dev *)ei_status.priv;
+		if (idev)
+			pnp_start_dev(idev);
 		ne_reset_8390(dev);
 		NS8390p_init(dev, 1);
 		netif_device_attach(dev);
