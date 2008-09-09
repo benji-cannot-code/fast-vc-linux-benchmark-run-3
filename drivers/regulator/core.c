f138c9f21550a1b356488455f551a3f20c7fd5d1
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-09-09 16:21:18 +0100

    regulator: check for init_data on registration
    Since it is now mandatory to supply constraints via init_data on device
    registration check for that when registering, saving us from oopsing
    later on.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1692,6 +1692,9 @@ struct regulator_dev *regulator_register(struct regulator_desc *regulator_desc,
 	    !regulator_desc->type == REGULATOR_CURRENT)
 		return ERR_PTR(-EINVAL);
 
+	if (!init_data)
+		return ERR_PTR(-EINVAL);
+
 	rdev = kzalloc(sizeof(struct regulator_dev), GFP_KERNEL);
 	if (rdev == NULL)
 		return ERR_PTR(-ENOMEM);
