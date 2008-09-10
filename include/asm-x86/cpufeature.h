25793d7bb16f3fffc8f4d818c61b39605f22aa76
Sheng Yang <sheng.yang@intel.com>
2008-09-10 18:53:34 +0800

    x86: extended "flags" to show virtualization HW feature in /proc/cpuinfo
    The hardware virtualization technology evolves very fast. But currently
    it's hard to tell if your CPU support a certain kind of HW technology
    without digging into the source code.
    
    The patch add a new catagory in "flags" under /proc/cpuinfo. Now "flags"
    can indicate the (important) HW virtulization features the CPU supported
    as well.
    
    Current implementation just cover Intel VMX side.
    
    Signed-off-by: Sheng Yang <sheng.yang@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/cpufeature.h
+++ include/asm-x86/cpufeature.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/required-features.h>
 
-#define NCAPINTS	8	/* N 32-bit words worth of info */
+#define NCAPINTS	9	/* N 32-bit words worth of info */
 
 /*
  * Note: If the comment begins with a quoted string, that string is used
@@ -150,6 +150,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * CPUID levels like 0x6, 0xA etc
  */
 #define X86_FEATURE_IDA		(7*32+ 0) /* Intel Dynamic Acceleration */
+
+/* Virtualization flags: Linux defined */
+#define X86_FEATURE_TPR_SHADOW  (8*32+ 0) /* Intel TPR Shadow */
+#define X86_FEATURE_VNMI        (8*32+ 1) /* Intel Virtual NMI */
+#define X86_FEATURE_FLEXPRIORITY (8*32+ 2) /* Intel FlexPriority */
+#define X86_FEATURE_EPT         (8*32+ 3) /* Intel Extended Page Table */
+#define X86_FEATURE_VPID        (8*32+ 4) /* Intel Virtual Processor ID */
 
 #if defined(__KERNEL__) && !defined(__ASSEMBLY__)
 
