d3cedf07303ecd7040ccad43d0af13e813b54e9b
Johannes Berg <johannes@sipsolutions.net>
2008-09-11 00:01:56 +0200

    mac80211: clean up some comments
    Some comments refer to 80211.o or similar; also remove
    a comment about implementing fragments better, we really
    have better things to do.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/wme.h
+++ net/mac80211/wme.h
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * IEEE 802.11 driver (80211.o) - QoS datatypes
  * Copyright 2004, Instant802 Networks, Inc.
  * Copyright 2005, Devicescape Software, Inc.
  *
