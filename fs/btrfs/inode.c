f79f004f3f382f98dcd62740e60dbb9812290a75
Chris Mason <chris.mason@oracle.com>
2008-09-11 15:51:21 -0400

    Btrfs: Update the highest objectid in a root after log replay is done
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -2240,8 +2240,9 @@ static int btrfs_set_inode_index(struct inode *dir, struct inode *inode,
 
 	if (BTRFS_I(dir)->index_cnt == (u64)-1) {
 		ret = btrfs_set_inode_index_count(dir);
-		if (ret)
+		if (ret) {
 			return ret;
+		}
 	}
 
 	*index = BTRFS_I(dir)->index_cnt;
