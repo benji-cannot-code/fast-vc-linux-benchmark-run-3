f2088e9bdb862251310ef56c71750c0300a93e47
David S. Miller <davem@davemloft.net>
2008-09-11 23:33:53 -0700

    sysctl: Use header file for sysctl knob declarations on sparc.
    This also takes care of a sparse warning as scons_pwroff's definition
    point.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/reboot.c
+++ arch/sparc64/kernel/reboot.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/pm.h>
 
+#include <asm/system.h>
 #include <asm/oplib.h>
 #include <asm/prom.h>
 
