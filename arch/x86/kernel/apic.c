26522cf35fe30c4a9f223ec598fdf09d1d9818fb
Cyrill Gorcunov <gorcunov@gmail.com>
2008-09-14 11:55:38 +0400

    x86: apic - skip writting ESR register if we dont have on
    On 82489DX we don't have ESR register so we should not
    write it.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic.c
+++ arch/x86/kernel/apic.c
@@ -1132,7 +1132,7 @@ void __cpuinit setup_local_APIC(void)
 
 #ifdef CONFIG_X86_32
 	/* Pound the ESR really hard over the head with a big hammer - mbligh */
-	if (esr_disable) {
+	if (lapic_is_integrated() && esr_disable) {
 		apic_write(APIC_ESR, 0);
 		apic_write(APIC_ESR, 0);
 		apic_write(APIC_ESR, 0);
