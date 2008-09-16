0676ca6d48ee9dabc8cafbbf518c184e2645dc21
Andres Salomon <dilinger@queued.net>
2008-09-16 12:30:33 -0400

    Input: psmouse - add psmouse_queue_work() for ps/2 extension to make use of
    psmouse_queue_work is passed a delayed_work struct, and queues up the work
    with kpsmouse_wq.  Since we're dealing with delayed_work stuff, this
    also switches resync_work to a delayed_work struct as well, and makes
    use of psmouse_queue_work when doing a resync within psmouse-base.
    
    Signed-off-by: Andres Salomon <dilinger@debian.org>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/mouse/psmouse.h
+++ drivers/input/mouse/psmouse.h
@@ -40,7 +40,7 @@ struct psmouse {
 	void *private;
 	struct input_dev *dev;
 	struct ps2dev ps2dev;
-	struct work_struct resync_work;
+	struct delayed_work resync_work;
 	char *vendor;
 	char *name;
 	unsigned char packet[8];
@@ -93,6 +93,8 @@ enum psmouse_type {
 	PSMOUSE_AUTO		/* This one should always be last */
 };
 
+void psmouse_queue_work(struct psmouse *psmouse, struct delayed_work *work,
+		unsigned long delay);
 int psmouse_sliced_command(struct psmouse *psmouse, unsigned char command);
 int psmouse_reset(struct psmouse *psmouse);
 void psmouse_set_state(struct psmouse *psmouse, enum psmouse_state new_state);
