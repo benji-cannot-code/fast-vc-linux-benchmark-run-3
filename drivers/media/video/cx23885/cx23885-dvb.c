1db6db0716c5b6e349e1c9bcf32f9d1d0de82023
Michael Krufky <mkrufky@linuxtv.org>
2008-09-16 02:15:30 -0300

    V4L/DVB (8968): replace xc3028 firmware filenames with defined default firmware names
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx23885/cx23885-dvb.c
+++ drivers/media/video/cx23885/cx23885-dvb.c
@@ -408,7 +408,7 @@ static int dvb_register(struct cx23885_tsport *port)
 				.callback  = cx23885_tuner_callback,
 			};
 			static struct xc2028_ctrl ctl = {
-				.fname       = "xc3028-v27.fw",
+				.fname       = XC2028_DEFAULT_FIRMWARE,
 				.max_len     = 64,
 				.scode_table = XC3028_FE_OREN538,
 			};
@@ -448,7 +448,7 @@ static int dvb_register(struct cx23885_tsport *port)
 				.callback  = cx23885_tuner_callback,
 			};
 			static struct xc2028_ctrl ctl = {
-				.fname   = "xc3028L-v36.fw",
+				.fname   = XC3028L_DEFAULT_FIRMWARE,
 				.max_len = 64,
 				.demod   = 5000,
 				.d2633   = 1
@@ -490,7 +490,7 @@ static int dvb_register(struct cx23885_tsport *port)
 				.callback  = cx23885_tuner_callback,
 			};
 			static struct xc2028_ctrl ctl = {
-				.fname       = "xc3028-v27.fw",
+				.fname       = XC2028_DEFAULT_FIRMWARE,
 				.max_len     = 64,
 				.demod       = XC3028_FE_ZARLINK456,
 			};
@@ -517,7 +517,7 @@ static int dvb_register(struct cx23885_tsport *port)
 				.callback  = cx23885_tuner_callback,
 			};
 			static struct xc2028_ctrl ctl = {
-				.fname       = "xc3028-v27.fw",
+				.fname       = XC2028_DEFAULT_FIRMWARE,
 				.max_len     = 64,
 				.demod       = XC3028_FE_ZARLINK456,
 			};
