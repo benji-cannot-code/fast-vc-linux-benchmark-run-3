18d1407e28fe8855c2cad59ea4c2a7d3bfe439fe
Paul Mundt <lethal@linux-sh.org>
2008-09-17 23:24:59 +0900

    sh: Fix up headers_check regression.
    linux/mmzone.h isn't exported, kill it off from asm/setup.h and simply
    deal with it in the places that have a dependency instead.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/setup.h
+++ arch/sh/include/asm/setup.h
@@ -2,12 +2,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SH_SETUP_H
 #define _SH_SETUP_H
 
-#include <linux/mmzone.h>
-
 #define COMMAND_LINE_SIZE 256
 
 #ifdef __KERNEL__
-
 /*
  * This is set up by the setup-routine at boot-time
  */
