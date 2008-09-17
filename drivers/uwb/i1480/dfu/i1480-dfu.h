75165c90f34a646b0307243b01d4e298e3fcdcfb
Anderson Lizardo <anderson.lizardo@indt.org.br>
2008-09-17 16:34:37 +0100

    uwb: i1480: remove MAC/PHY information checking function
    Some hardware/firmware combinations (most notably an IOGear HWA using the i1480
    firmware) kill the host controller after issuing a GET_MAC_PHY_INFO command.
    Removing this check seems harmless otherwise.
    
    The patch fixes the issue where the HC is killed, showing the message:
    
    ehci_hcd 0000:00:1d.7: HC died; cleaning up
    
    After this error, USB comes back only after reloading the ehci_hcd module.
    
    Signed-off-by: Anderson Lizardo <anderson.lizardo@indt.org.br>
    Signed-off-by: David Vrabel <david.vrabel@csr.com>

--- drivers/uwb/i1480/dfu/i1480-dfu.h
+++ drivers/uwb/i1480/dfu/i1480-dfu.h
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *       request_firmware()
  *       i1480_mpi_write()
  *         i1480->cmd()           [i1480_{usb,pci}_cmd()]
- *       i1480_check_info()
  *
  * Once the probe function enumerates the device and uploads the
  * firmware, we just exit with -ENODEV, as we don't really want to
@@ -131,8 +130,6 @@ struct i1480 {
 	void *evt_buf, *cmd_buf;
 	ssize_t evt_result;
 	struct completion evt_complete;
-
-	u8 quirk_no_check_info:1;
 };
 
 static inline
