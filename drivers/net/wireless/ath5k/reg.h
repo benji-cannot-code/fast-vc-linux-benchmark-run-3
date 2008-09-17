79f9da9c63182034278ab6156696ee0bb770e9f4
Nick Kossifidis <mick@madwifi.org>
2008-09-17 03:33:19 +0300

    ath5k: Fix bad udelay calls on AR5210 code
     * Fix bad udelay calls (using > 2000us) in AR5210 code and clean up
     some bits on nic_reset (AR5210 support is still in bad shape)
    
     Changes-licensed-under: ISC
     Signed-off-by: Nick Kossifidis <mickflemm@gmail.com>
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath5k/reg.h
+++ drivers/net/wireless/ath5k/reg.h
@@ -821,8 +821,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AR5K_RESET_CTL_MAC	0x00000004	/* MAC reset (PCU+Baseband ?) [5210] */
 #define AR5K_RESET_CTL_PHY	0x00000008	/* PHY reset [5210] */
 #define AR5K_RESET_CTL_PCI	0x00000010	/* PCI Core reset (interrupts etc) */
-#define AR5K_RESET_CTL_CHIP	(AR5K_RESET_CTL_PCU | AR5K_RESET_CTL_DMA |	\
-				AR5K_RESET_CTL_MAC | AR5K_RESET_CTL_PHY)
 
 /*
  * Sleep control register
