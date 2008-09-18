e64242f55331db10ddfbd73471e3487fda563a35
Benjamin Li <benli@broadcom.com>
2008-09-18 16:40:49 -0700

    bnx2: Remove timer_interval field from the bnx2 structure
    The timer_interval field is only assigned once, and never reassigned.
    We can safely replace all instances of the timer_interval with a
    constant value.
    
    Signed-off-by: Benjamin Li <benli@broadcom.com>
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Acked-by: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.h
+++ drivers/net/bnx2.h
@@ -6655,6 +6655,8 @@ struct bnx2_napi {
 	struct bnx2_tx_ring_info	tx_ring;
 };
 
+#define BNX2_TIMER_INTERVAL			HZ
+
 struct bnx2 {
 	/* Fields used in the tx and intr/napi performance paths are grouped */
 	/* together in the beginning of the structure. */
@@ -6702,7 +6704,6 @@ struct bnx2 {
 
 	/* End of fields used in the performance code paths. */
 
-	int			timer_interval;
 	int			current_interval;
 	struct			timer_list timer;
 	struct work_struct	reset_task;
