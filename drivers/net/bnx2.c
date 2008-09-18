fb6ba02c3cf341ea883f0cbc0019d71e259095ff
Benjamin Li <benli@broadcom.com>
2008-09-18 16:38:24 -0700

    bnx2: In bnx2_set_mac_link() return void rather then int
    bnx2_set_mac_link() doesn't need to return any error codes.  And
    all the callers don't check the return code. It is safe to
    change the return type to a void.
    
    Signed-off-by: Benjamin Li <benli@broadcom.com>
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Acked-by: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -1128,7 +1128,7 @@ bnx2_init_all_rx_contexts(struct bnx2 *bp)
 	}
 }
 
-static int
+static void
 bnx2_set_mac_link(struct bnx2 *bp)
 {
 	u32 val;
@@ -1194,8 +1194,6 @@ bnx2_set_mac_link(struct bnx2 *bp)
 
 	if (CHIP_NUM(bp) == CHIP_NUM_5709)
 		bnx2_init_all_rx_contexts(bp);
-
-	return 0;
 }
 
 static void
