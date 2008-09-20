a4992f2eeaf145f77fc7f8adca39544dc5d9c4ee
Andreas Eversberg <andreas@eversberg.eu>
2008-09-20 13:43:28 +0200

    mISDN: Minor fixes
    minor typo fix
    compiler warning fix
    
    Signed-off-by: Andreas Eversberg <andreas@eversberg.eu>
    Signed-off-by: Karsten Keil <kkeil@suse.de>

--- drivers/isdn/hardware/mISDN/hfcmulti.c
+++ drivers/isdn/hardware/mISDN/hfcmulti.c
@@ -136,7 +136,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	This register is needed for the TBR3 certification, so don't change it.
  *
  * clock:
- *	NOTE: only one clockdelay_te value must be given once
+ *	NOTE: only one clock value must be given once
  *	Selects interface with clock source for mISDN and applications.
  *	Set to card number starting with 1. Set to -1 to disable.
  *	By default, the first card is used as clock source.
