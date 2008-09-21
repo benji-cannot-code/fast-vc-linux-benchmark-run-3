549ccb57914fc6ef52c52bcca744fc024ddcae59
David Brownell <david-b@pacbell.net>
2008-09-21 00:28:26 -0700

    sparc: arch/sparc/kernel/pmc.c -- extra #include?
    I have no SPARC compiler handy to verify, but it looks like this
    is another file that doesn't need <linux/miscdevices.h> ...
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/pmc.c
+++ arch/sparc/kernel/pmc.c
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/errno.h>
 #include <linux/init.h>
-#include <linux/miscdevice.h>
 #include <linux/pm.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
@@ -25,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * #define PMC_NO_IDLE
  */
 
-#define PMC_MINOR	MISC_DYNAMIC_MINOR
 #define PMC_OBPNAME	"SUNW,pmc"
 #define PMC_DEVNAME "pmc"
 
