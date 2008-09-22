7c405d9a5e92f6a68e44a80233e16aa86a7a3702
Gaetan Carlier <gcpatch@gmail.com>
2008-09-22 15:00:09 -0700

    usb: ftdi_sio: add support for Domintell devices
    Support for Domintell devices (FTDI FT232BM based) : DGQG and DUSB01
    module.  PIDs were missing.
    
    Signed-off-by: Gaetan Carlier <gcpatch@gmail.com>
    Cc: Greg Kroah-Hartman <gregkh@suse.de>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -840,6 +840,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Rig Expert Ukraine devices */
 #define FTDI_REU_TINY_PID		0xED22	/* RigExpert Tiny */
 
+/* Domintell products  http://www.domintell.com */
+#define FTDI_DOMINTELL_DGQG_PID	0xEF50	/* Master */
+#define FTDI_DOMINTELL_DUSB_PID	0xEF51	/* DUSB01 module */
+
 /* Commands */
 #define FTDI_SIO_RESET 		0 /* Reset the port */
 #define FTDI_SIO_MODEM_CTRL 	1 /* Set the modem control register */
