d46635865a1a95a62a202b9cb07dfe3758119fa5
Igor M. Liplianin <liplianin@me.by>
2008-09-23 15:43:57 -0300

    V4L/DVB (9026): Add support for ST STV0288 demodulator and cards with it.
    Add support for ST STV0288 demodulator and cards with it,
    such as TeVii S420.
    Patch is co-authored with Georg Acher <acher@baycom.de>
    
    Signed-off-by: Georg Acher <acher@baycom.de>
    Signed-off-by: Igor M. Liplianin <liplianin@me.by>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dm1105/dm1105.c
+++ drivers/media/dvb/dm1105/dm1105.c
@@ -40,8 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb-pll.h"
 
 #include "stv0299.h"
-/*#include "stv0288.h"
- *#include "stb6000.h"*/
+#include "stv0288.h"
+#include "stb6000.h"
 #include "si21xx.h"
 #include "cx24116.h"
 #include "z0194a.h"
@@ -595,13 +595,12 @@ static void dm1105dvb_hw_exit(struct dm1105dvb *dm1105dvb)
 
 	dm1105dvb_dma_unmap(dm1105dvb);
 }
-#if 0
+
 static struct stv0288_config earda_config = {
 	.demod_address = 0x68,
 	.min_delay_ms = 100,
 };
 
-#endif /* keep */
 static struct si21xx_config serit_config = {
 	.demod_address = 0x68,
 	.min_delay_ms = 100,
@@ -628,7 +627,7 @@ static int __devinit frontend_init(struct dm1105dvb *dm1105dvb)
 			dvb_attach(dvb_pll_attach, dm1105dvb->fe, 0x60,
 					&dm1105dvb->i2c_adap, DVB_PLL_OPERA1);
 		}
-#if 0
+
 		if (!dm1105dvb->fe) {
 			dm1105dvb->fe = dvb_attach(
 				stv0288_attach, &earda_config,
@@ -640,7 +639,7 @@ static int __devinit frontend_init(struct dm1105dvb *dm1105dvb)
 						&dm1105dvb->i2c_adap);
 			}
 		}
-#endif /* keep */
+
 		if (!dm1105dvb->fe) {
 			dm1105dvb->fe = dvb_attach(
 				si21xx_attach, &serit_config,
