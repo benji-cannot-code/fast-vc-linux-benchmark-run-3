3d4a8ea32e708fe369944864c41203a75f7b9edb
Harvey Harrison <harvey.harrison@gmail.com>
2008-09-23 15:05:46 -0700

    x86: suppress trivial sparse signedness warnings
    Could just as easily change the three casts to cast to the correct
    type...this patch changes the type of ftrace_nop instead.
    
    Supresses sparse warnings:
    
     arch/x86/kernel/ftrace.c:157:14: warning: incorrect type in assignment (different signedness)
     arch/x86/kernel/ftrace.c:157:14:    expected long *static [toplevel] ftrace_nop
     arch/x86/kernel/ftrace.c:157:14:    got unsigned long *<noident>
     arch/x86/kernel/ftrace.c:161:14: warning: incorrect type in assignment (different signedness)
     arch/x86/kernel/ftrace.c:161:14:    expected long *static [toplevel] ftrace_nop
     arch/x86/kernel/ftrace.c:161:14:    got unsigned long *<noident>
     arch/x86/kernel/ftrace.c:165:14: warning: incorrect type in assignment (different signedness)
     arch/x86/kernel/ftrace.c:165:14:    expected long *static [toplevel] ftrace_nop
     arch/x86/kernel/ftrace.c:165:14:    got unsigned long *<noident>
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/ftrace.c
+++ arch/x86/kernel/ftrace.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /* Long is fine, even if it is only 4 bytes ;-) */
-static long *ftrace_nop;
+static unsigned long *ftrace_nop;
 
 union ftrace_code_union {
 	char code[MCOUNT_INSN_SIZE];
