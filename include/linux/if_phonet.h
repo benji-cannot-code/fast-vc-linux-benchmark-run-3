1b544e31a601d62084463f0b3e020e2dbde3189a
Remi Denis-Courmont <remi.denis-courmont@nokia.com>
2008-09-22 20:08:04 -0700

    Phonet: provide MAC header operations
    Signed-off-by: Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_phonet.h
+++ include/linux/if_phonet.h
@@ -13,3 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* 6 bytes header + 65535 bytes payload */
 #define PHONET_MAX_MTU		65541
 #define PHONET_DEV_MTU		PHONET_MAX_MTU
+
+#ifdef __KERNEL__
+extern struct header_ops phonet_header_ops;
+#endif
