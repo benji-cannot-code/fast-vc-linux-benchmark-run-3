dcded846db4200cd3e4097e70ec303ada1b468cb
Steve French <sfrench@us.ibm.com>
2008-09-24 19:22:52 +0000

    [CIFS] update DOS attributes in cifsInode if we successfully changed them
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/inode.c
+++ fs/cifs/inode.c
@@ -753,6 +753,9 @@ cifs_set_file_info(struct inode *inode, struct iattr *attrs, int xid,
 
 set_via_filehandle:
 	rc = CIFSSMBSetFileInfo(xid, pTcon, &info_buf, netfid, netpid);
+	if (!rc)
+		cifsInode->cifsAttrs = dosattr;
+
 	if (open_file == NULL)
 		CIFSSMBClose(xid, pTcon, netfid);
 	else
@@ -903,6 +906,7 @@ psx_del_no_retry:
 			if (rc == 0)
 				drop_nlink(inode);
 		}
+		cifsInode->cifsAttrs = dosattr;
 	}
 out_reval:
 	if (inode) {
