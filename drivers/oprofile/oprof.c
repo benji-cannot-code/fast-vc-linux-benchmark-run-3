2e1b89b772bd0840413c0bfa939b16058ff69dd9
Robert Richter <robert.richter@amd.com>
2008-09-24 11:08:52 +0200

    Revert "Oprofile Multiplexing Patch"
    Reverting commit 1a960b402a51d80abf54e3f8e4972374ffe5f22d for the main
    branch. Multiplexing will be tracked on a separate feature branch.
    
    Conflicts:
    
        arch/x86/oprofile/nmi_int.c

--- drivers/oprofile/oprof.c
+++ drivers/oprofile/oprof.c
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/oprofile.h>
 #include <linux/moduleparam.h>
-#include <linux/workqueue.h>
-#include <linux/time.h>
 #include <asm/mutex.h>
 
 #include "oprof.h"
@@ -22,18 +20,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cpu_buffer.h"
 #include "buffer_sync.h"
 #include "oprofile_stats.h"
-
-static unsigned long is_setup;
-static void switch_worker(struct work_struct *work);
-static DECLARE_DELAYED_WORK(switch_work, switch_worker);
-static DEFINE_MUTEX(start_mutex);
  
 struct oprofile_operations oprofile_ops;
 
-unsigned long timeout_jiffies;
 unsigned long oprofile_started;
 unsigned long backtrace_depth;
-/* Multiplexing defaults at 1 msec*/
+static unsigned long is_setup;
+static DEFINE_MUTEX(start_mutex);
 
 /* timer
    0 - use performance monitoring hardware if available
@@ -95,16 +88,6 @@ out:
 	return err;
 }
 
-static void start_switch_worker(void)
-{
-	schedule_delayed_work(&switch_work, timeout_jiffies);
-}
-
-static void switch_worker(struct work_struct *work)
-{
-	if (!oprofile_ops.switch_events())
-		start_switch_worker();
-}
 
 /* Actually start profiling (echo 1>/dev/oprofile/enable) */
 int oprofile_start(void)
@@ -112,6 +95,7 @@ int oprofile_start(void)
 	int err = -EINVAL;
  
 	mutex_lock(&start_mutex);
+ 
 	if (!is_setup)
 		goto out;
 
@@ -124,9 +108,6 @@ int oprofile_start(void)
 
 	if ((err = oprofile_ops.start()))
 		goto out;
-
-	if (oprofile_ops.switch_events)
-		start_switch_worker();
 
 	oprofile_started = 1;
 out:
@@ -143,7 +124,6 @@ void oprofile_stop(void)
 		goto out;
 	oprofile_ops.stop();
 	oprofile_started = 0;
-	cancel_delayed_work_sync(&switch_work);
 	/* wake up the daemon to read what remains */
 	wake_up_buffer_waiter();
 out:
@@ -176,32 +156,6 @@ post_sync:
 	mutex_unlock(&start_mutex);
 }
 
-/* User inputs in ms, converts to jiffies */
-int oprofile_set_timeout(unsigned long val_msec)
-{
-	int err = 0;
-
-	mutex_lock(&start_mutex);
-
-	if (oprofile_started) {
-		err = -EBUSY;
-		goto out;
-	}
-
-	if (!oprofile_ops.switch_events) {
-		err = -EINVAL;
-		goto out;
-	}
-
-	timeout_jiffies = msecs_to_jiffies(val_msec);
-	if (timeout_jiffies == MAX_JIFFY_OFFSET)
-		timeout_jiffies = msecs_to_jiffies(1);
-
-out:
-	mutex_unlock(&start_mutex);
-	return err;
-
-}
 
 int oprofile_set_backtrace(unsigned long val)
 {
@@ -226,16 +180,10 @@ out:
 	return err;
 }
 
-static void __init oprofile_switch_timer_init(void)
-{
-	timeout_jiffies = msecs_to_jiffies(1);
-}
-
 static int __init oprofile_init(void)
 {
 	int err;
 
-	oprofile_switch_timer_init();
 	err = oprofile_arch_init(&oprofile_ops);
 
 	if (err < 0 || timer) {
