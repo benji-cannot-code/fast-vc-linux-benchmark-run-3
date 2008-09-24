66dd969dbcf3a0ae7945f44449f320651c1a5be4
Weidong Han <weidong.han@intel.com>
2008-09-24 13:39:57 +0800

    KVM: Remove useless intel-iommu.h header inclusion
    Currently "#include <linux/intel-iommu.h>" is not needed in
    virt/kvm/kvm_main.c.
    
    Signed-off-by: Weidong Han <weidong.han@intel.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pagemap.h>
 #include <linux/mman.h>
 #include <linux/swap.h>
-#include <linux/intel-iommu.h>
 
 #include <asm/processor.h>
 #include <asm/io.h>
