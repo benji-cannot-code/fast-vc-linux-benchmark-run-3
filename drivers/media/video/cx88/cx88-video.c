76e7a728180f8350212ef520c55c12a95df25616
Darron Broad <darron@kewl.org>
2008-09-25 16:51:11 -0300

    V4L/DVB (9069): cx88: Bugfix: all client disconnects put the frontend to sleep.
    From the author:
    
    "This fixes the problem where previously all client disconnects
    put the analogue frontend into standby.
    
    In the following example, the first command is succesfully
    watching TV but the second command which returns EBUSY
    detunes the receiver by entering it into the standby state.
    
    tvtime -d /dev/video0 &
    cat /dev/video0
    "
    
    Signed-off-by: Steven Toth <stoth@linuxtv.org>
    Signed-off-by: Darron Broad <darron@kewl.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx88/cx88-video.c
+++ drivers/media/video/cx88/cx88-video.c
@@ -840,6 +840,8 @@ static int video_open(struct inode *inode, struct file *file)
 	}
 	unlock_kernel();
 
+	atomic_inc(&core->users);
+
 	return 0;
 }
 
@@ -927,7 +929,8 @@ static int video_release(struct inode *inode, struct file *file)
 	file->private_data = NULL;
 	kfree(fh);
 
-	cx88_call_i2c_clients (dev->core, TUNER_SET_STANDBY, NULL);
+	if(atomic_dec_and_test(&dev->core->users))
+		cx88_call_i2c_clients (dev->core, TUNER_SET_STANDBY, NULL);
 
 	return 0;
 }
