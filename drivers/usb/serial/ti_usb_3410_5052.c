09f51b16a4878c5f73411f1ae432309c6805045d
Huang Weiyi <hwy@cn.fujitsu.com>
2008-09-25 13:11:28 +0800

    USB: ti_usb_3410_5052: removed duplicated include
    Removed duplicated #include <linux/firmware.h> in
    drivers/usb/serial/ti_usb_3410_5052.c.
    
    Signed-off-by: Huang Weiyi <hwy@cn.fujitsu.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ti_usb_3410_5052.c
+++ drivers/usb/serial/ti_usb_3410_5052.c
@@ -86,7 +86,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/usb.h>
 #include <linux/usb/serial.h>
-#include <linux/firmware.h>
 
 #include "ti_usb_3410_5052.h"
 
