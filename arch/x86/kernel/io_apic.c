724a4c8075351bb856939699bf282783fe7f50b0
Yinghai Lu <yhlu.kernel@gmail.com>
2008-09-25 11:53:11 -0700

    x86: print out irq nr for msi/ht, v3
    v2: fix hpet compiling error
    v3: Bjorn want to use dev_printk instead
    
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Cc: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/io_apic.c
+++ arch/x86/kernel/io_apic.c
@@ -3355,6 +3355,8 @@ static int setup_msi_irq(struct pci_dev *dev, struct msi_desc *desc, int irq)
 #endif
 		set_irq_chip_and_handler_name(irq, &msi_chip, handle_edge_irq, "edge");
 
+	dev_printk(KERN_DEBUG, &dev->dev, "irq %d for MSI/MSI-X\n", irq);
+
 	return 0;
 }
 
@@ -3587,6 +3589,7 @@ int arch_setup_hpet_msi(unsigned int irq)
 	hpet_msi_write(irq, &msg);
 	set_irq_chip_and_handler_name(irq, &hpet_msi_type, handle_edge_irq,
 		"edge");
+
 	return 0;
 }
 #endif
@@ -3683,6 +3686,8 @@ int arch_setup_ht_irq(unsigned int irq, struct pci_dev *dev)
 
 		set_irq_chip_and_handler_name(irq, &ht_irq_chip,
 					      handle_edge_irq, "edge");
+
+		dev_printk(KERN_DEBUG, &dev->dev, "irq %d for HT\n", irq);
 	}
 	return err;
 }
