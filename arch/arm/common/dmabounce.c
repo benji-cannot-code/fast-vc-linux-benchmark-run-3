063cb8b23ab03e90a504e309d511090e9d1a2782
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-09-25 16:30:57 +0100

    [ARM] Update dma_map_sg()/dma_unmap_sg() API
    Update the ARM DMA scatter gather APIs for the scatterlist changes.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/common/dmabounce.c
+++ arch/arm/common/dmabounce.c
@@ -436,6 +436,7 @@ int
 dma_map_sg(struct device *dev, struct scatterlist *sg, int nents,
 		enum dma_data_direction dir)
 {
+	struct scatterlist *s;
 	int i;
 
 	dev_dbg(dev, "%s(sg=%p,nents=%d,dir=%x)\n",
@@ -443,14 +444,13 @@ dma_map_sg(struct device *dev, struct scatterlist *sg, int nents,
 
 	BUG_ON(dir == DMA_NONE);
 
-	for (i = 0; i < nents; i++, sg++) {
-		struct page *page = sg_page(sg);
-		unsigned int offset = sg->offset;
-		unsigned int length = sg->length;
+	for_each_sg(sg, s, nents, i) {
+		struct page *page = sg_page(s);
+		unsigned int offset = s->offset;
+		unsigned int length = s->length;
 		void *ptr = page_address(page) + offset;
 
-		sg->dma_address =
-			map_single(dev, ptr, length, dir);
+		s->dma_address = map_single(dev, ptr, length, dir);
 	}
 
 	return nents;
@@ -460,6 +460,7 @@ void
 dma_unmap_sg(struct device *dev, struct scatterlist *sg, int nents,
 		enum dma_data_direction dir)
 {
+	struct scatterlist *s;
 	int i;
 
 	dev_dbg(dev, "%s(sg=%p,nents=%d,dir=%x)\n",
@@ -467,9 +468,9 @@ dma_unmap_sg(struct device *dev, struct scatterlist *sg, int nents,
 
 	BUG_ON(dir == DMA_NONE);
 
-	for (i = 0; i < nents; i++, sg++) {
-		dma_addr_t dma_addr = sg->dma_address;
-		unsigned int length = sg->length;
+	for_each_sg(sg, s, nents, i) {
+		dma_addr_t dma_addr = s->dma_address;
+		unsigned int length = s->length;
 
 		unmap_single(dev, dma_addr, length, dir);
 	}
@@ -503,6 +504,7 @@ void
 dma_sync_sg_for_cpu(struct device *dev, struct scatterlist *sg, int nents,
 			enum dma_data_direction dir)
 {
+	struct scatterlist *s;
 	int i;
 
 	dev_dbg(dev, "%s(sg=%p,nents=%d,dir=%x)\n",
@@ -510,9 +512,9 @@ dma_sync_sg_for_cpu(struct device *dev, struct scatterlist *sg, int nents,
 
 	BUG_ON(dir == DMA_NONE);
 
-	for (i = 0; i < nents; i++, sg++) {
-		dma_addr_t dma_addr = sg->dma_address;
-		unsigned int length = sg->length;
+	for_each_sg(sg, s, nents, i) {
+		dma_addr_t dma_addr = s->dma_address;
+		unsigned int length = s->length;
 
 		sync_single(dev, dma_addr, length, dir);
 	}
@@ -522,6 +524,7 @@ void
 dma_sync_sg_for_device(struct device *dev, struct scatterlist *sg, int nents,
 			enum dma_data_direction dir)
 {
+	struct scatterlist *s;
 	int i;
 
 	dev_dbg(dev, "%s(sg=%p,nents=%d,dir=%x)\n",
@@ -529,9 +532,9 @@ dma_sync_sg_for_device(struct device *dev, struct scatterlist *sg, int nents,
 
 	BUG_ON(dir == DMA_NONE);
 
-	for (i = 0; i < nents; i++, sg++) {
-		dma_addr_t dma_addr = sg->dma_address;
-		unsigned int length = sg->length;
+	for_each_sg(sg, s, nents, i) {
+		dma_addr_t dma_addr = s->dma_address;
+		unsigned int length = s->length;
 
 		sync_single(dev, dma_addr, length, dir);
 	}
