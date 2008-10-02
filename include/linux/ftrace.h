7943cbe86c5b9cd870f1e9ba007ce597e9bd9ba3
Ingo Molnar <mingo@elte.hu>
2008-10-02 17:45:47 +0200

    tracing/fastboot: build fix
    fix:
    
     In file included from kernel/sysctl.c:52:
     include/linux/ftrace.h:217: error: 'KSYM_NAME_LEN' undeclared here (not in a function)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -2,13 +2,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_FTRACE_H
 #define _LINUX_FTRACE_H
 
-#ifdef CONFIG_FTRACE
-
 #include <linux/linkage.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/types.h>
 #include <linux/kallsyms.h>
+
+#ifdef CONFIG_FTRACE
 
 extern int ftrace_enabled;
 extern int
