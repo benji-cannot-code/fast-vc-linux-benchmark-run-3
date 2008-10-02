b0bf4f7359e0c55efb21843c58d23d43ada18fdd
Lennert Buytenhek <buytenh@wantstofly.org>
2008-10-02 00:52:39 +0000

    mv643xx_eth: include linux/ip.h to fix build
    mv643xx_eth uses ip_hdr() (defined in linux/ip.h), but relied on
    another header file to include the needed header file indirectly.
    In latest net-next this indirect include chain is gone, so the
    driver fails to build.  Include linux/ip.h explicitly to fix this.
    
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/mv643xx_eth.c
+++ drivers/net/mv643xx_eth.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/dma-mapping.h>
 #include <linux/in.h>
+#include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
 #include <linux/etherdevice.h>
