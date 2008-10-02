6c124e5b00973e4fcfa8eda2c0e02cf946e73929
Jeremy Fitzhardinge <jeremy@goop.org>
2008-10-02 16:42:35 -0700

    xen: remove unused balloon.h
    The balloon driver doesn't have any externally callable functions at
    the moment, so remove the (effectively empty) header.  We can add it
    back if we need to.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/xen/balloon.c
+++ drivers/xen/balloon.c
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/tlb.h>
 
 #include <xen/interface/memory.h>
-#include <xen/balloon.h>
 #include <xen/xenbus.h>
 #include <xen/features.h>
 #include <xen/page.h>
