4d8d208ee8ab0faac17e4e6e2a176eea3cad246a
Alexander van Heukelum <heukelum@fastmail.fm>
2008-10-03 22:00:36 +0200

    traps: i386: expand clear_mem_error and remove from mach_traps.h
    This is the last user of clear_mem_error, which is defined
    only on i386. Expand the inline function and remove it from
    include/asm-x86/mach-default/mach_traps.h
    
    Signed-off-by: Alexander van Heukelum <heukelum@fastmail.fm>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/mach-default/mach_traps.h
+++ include/asm-x86/mach-default/mach_traps.h
@@ -8,12 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mc146818rtc.h>
 
-static inline void clear_mem_error(unsigned char reason)
-{
-	reason = (reason & 0xf) | 4;
-	outb(reason, 0x61);
-}
-
 static inline unsigned char get_nmi_reason(void)
 {
 	return inb(0x61);
