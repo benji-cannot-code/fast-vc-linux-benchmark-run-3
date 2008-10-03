616272eb7ec4957118e38a9144154182a9a37d2d
Dmitry Baryshkov <dbaryshkov@gmail.com>
2008-10-04 00:50:36 +0200

    mfd: support tmiofb cell on tc6393xb
    Add support for tmiofb cell found in tc6393xb chip.
    
    Signed-off-by: Dmitry Baryshkov <dbaryshkov@gmail.com>
    Cc: Ian Molton <spyro@f2s.com>
    Signed-off-by: Samuel Ortiz <sameo@openedhand.com>

--- include/linux/mfd/tc6393xb.h
+++ include/linux/mfd/tc6393xb.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef MFD_TC6393XB_H
 #define MFD_TC6393XB_H
 
+#include <linux/fb.h>
+
 /* Also one should provide the CK3P6MI clock */
 struct tc6393xb_platform_data {
 	u16	scr_pll2cr;	/* PLL2 Control */
@@ -34,11 +36,16 @@ struct tc6393xb_platform_data {
 	void	(*teardown)(struct platform_device *dev);
 
 	struct tmio_nand_data	*nand_data;
+	struct tmio_fb_data	*fb_data;
 
 	unsigned resume_restore : 1; /* make special actions
 					to preserve the state
 					on suspend/resume */
 };
+
+extern int tc6393xb_lcd_mode(struct platform_device *fb,
+			     const struct fb_videomode *mode);
+extern int tc6393xb_lcd_set_power(struct platform_device *fb, bool on);
 
 /*
  * Relative to irq_base
@@ -46,6 +53,7 @@ struct tc6393xb_platform_data {
 #define	IRQ_TC6393_NAND		0
 #define	IRQ_TC6393_MMC		1
 #define	IRQ_TC6393_OHCI		2
+#define	IRQ_TC6393_FB		4
 
 #define	TC6393XB_NR_IRQS	8
 
