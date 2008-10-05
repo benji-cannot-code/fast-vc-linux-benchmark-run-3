8d62f177cd05137c9256efddc989ec76e81d61a7
Haavard Skinnemoen <haavard.skinnemoen@atmel.com>
2008-10-05 15:16:59 +0200

    atmel-mci: Don't overwrite error bits when NOTBUSY is set
    After a data error, we wait for the NOTBUSY bit to be set so that we can
    be sure the data transfer is completely finished. However, when NOTBUSY
    is set, the interrupt handler copies the contents of SR into
    data_status, overwriting any error bits we may have detected earlier.
    
    To avoid this, initialize data_status to 0 before starting a request, and
    don't overwrite it unless it still contains 0.
    
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- drivers/mmc/host/atmel-mci.c
+++ drivers/mmc/host/atmel-mci.c
@@ -695,6 +695,7 @@ static void atmci_start_request(struct atmel_mci *host,
 
 	host->pending_events = 0;
 	host->completed_events = 0;
+	host->data_status = 0;
 
 	if (host->need_reset) {
 		mci_writel(host, CR, MCI_CR_SWRST);
@@ -1409,7 +1410,8 @@ static irqreturn_t atmci_interrupt(int irq, void *dev_id)
 		if (pending & MCI_NOTBUSY) {
 			mci_writel(host, IDR,
 					ATMCI_DATA_ERROR_FLAGS | MCI_NOTBUSY);
-			host->data_status = status;
+			if (!host->data_status)
+				host->data_status = status;
 			smp_wmb();
 			atmci_set_pending(host, EVENT_DATA_COMPLETE);
 			tasklet_schedule(&host->tasklet);
