ce153b29cd1db88f0be8dfc29574af5f34055f7e
Alexey Dobriyan <adobriyan@gmail.com>
2008-10-07 01:58:45 +0400

    proc: remove now unneeded ADDBUF macro
    After local seq_file conversion it was forgotten.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -87,11 +87,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 #include "internal.h"
 
-/* Gcc optimizes away "strlen(x)" for constant x */
-#define ADDBUF(buffer, string) \
-do { memcpy(buffer, string, strlen(string)); \
-     buffer += strlen(string); } while (0)
-
 static inline void task_name(struct seq_file *m, struct task_struct *p)
 {
 	int i;
