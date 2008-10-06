3bb60a9c7a0f83fcce5e5c868f6b52037413c781
Thomas Gleixner <tglx@linutronix.de>
2008-10-06 14:15:24 +0200

    AMD IOMMU: use iommu_device_max_index, fix
    include/linux/iommu-helper.h has no header guards, which breaks
    sparc64 build. Add them.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Joerg Roedel <joerg.roedel@amd.com>
    Cc: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/iommu-helper.h
+++ include/linux/iommu-helper.h
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _LINUX_IOMMU_HELPER_H
+#define _LINUX_IOMMU_HELPER_H
+
 static inline unsigned long iommu_device_max_index(unsigned long size,
 						   unsigned long offset,
 						   u64 dma_mask)
@@ -20,3 +23,5 @@ extern unsigned long iommu_area_alloc(unsigned long *map, unsigned long size,
 				      unsigned long align_mask);
 extern void iommu_area_free(unsigned long *map, unsigned long start,
 			    unsigned int nr);
+
+#endif
