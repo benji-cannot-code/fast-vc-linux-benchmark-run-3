35012490d7a6ab2a4090eff94b28e021d0e51a25
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-10-08 13:02:20 +0100

    ALSA: ASoC: Check for machine type in GTA01 machine driver
    Since there are now multiple OpenMoko platforms it is more important to
    check that the machine driver is running on the correct system.  This
    was orgininally generated as part of the initial GTA02 machine port.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- sound/soc/s3c24xx/neo1973_wm8753.c
+++ sound/soc/s3c24xx/neo1973_wm8753.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc-dapm.h>
 #include <sound/tlv.h>
 
+#include <asm/mach-types.h>
 #include <asm/hardware/scoop.h>
 #include <mach/regs-clock.h>
 #include <mach/regs-gpio.h>
@@ -716,6 +717,12 @@ static int __init neo1973_init(void)
 	int ret;
 
 	DBG("Entered %s\n", __func__);
+
+	if (!machine_is_neo1973_gta01()) {
+		printk(KERN_INFO
+			"Only GTA01 hardware supported by ASoC driver\n");
+		return -ENODEV;
+	}
 
 	neo1973_snd_device = platform_device_alloc("soc-audio", -1);
 	if (!neo1973_snd_device)
