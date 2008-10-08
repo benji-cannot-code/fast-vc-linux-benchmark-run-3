7d0b8e843449cd0d507a1de3341eb6c9ad1ed00c
Alexey Dobriyan <adobriyan@gmail.com>
2008-10-08 11:35:08 +0200

    netfilter: netns nf_conntrack: per-netns net.netfilter.nf_conntrack_count sysctl
    Note, sysctl table is always duplicated, this is simpler and less
    special-cased.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- include/net/netns/conntrack.h
+++ include/net/netns/conntrack.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <asm/atomic.h>
 
+struct ctl_table_header;
 struct nf_conntrack_ecache;
 
 struct netns_ct {
@@ -16,6 +17,9 @@ struct netns_ct {
 	struct ip_conntrack_stat *stat;
 #ifdef CONFIG_NF_CONNTRACK_EVENTS
 	struct nf_conntrack_ecache *ecache;
+#endif
+#ifdef CONFIG_SYSCTL
+	struct ctl_table_header	*sysctl_header;
 #endif
 	int			hash_vmalloc;
 	int			expect_vmalloc;
