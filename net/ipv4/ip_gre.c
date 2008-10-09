2453ff987675141ec354499f49ac3903af53fe1d
Herbert Xu <herbert@gondor.apana.org.au>
2008-10-09 12:03:17 -0700

    inet: Make tunnel RX/TX byte counters more consistent
    This patch makes the RX/TX byte counters for IPIP, GRE and SIT more
    consistent.  Previously we included the external IP headers on the
    way out but not when the packet is inbound.
    
    The new scheme is to count payload only in both directions.  For
    IPIP and SIT this simply means the exclusion of the external IP
    header.  For GRE this means that we exclude the GRE header as
    well.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_gre.c
+++ net/ipv4/ip_gre.c
@@ -478,6 +478,7 @@ static int ipgre_rcv(struct sk_buff *skb)
 	struct ip_tunnel *tunnel;
 	int    offset = 4;
 	__be16 gre_proto;
+	unsigned int len;
 
 	if (!pskb_may_pull(skb, 16))
 		goto drop_nolock;
@@ -568,6 +569,8 @@ static int ipgre_rcv(struct sk_buff *skb)
 			tunnel->i_seqno = seqno + 1;
 		}
 
+		len = skb->len;
+
 		/* Warning: All skb pointers will be invalidated! */
 		if (tunnel->dev->type == ARPHRD_ETHER) {
 			if (!pskb_may_pull(skb, ETH_HLEN)) {
@@ -582,7 +585,7 @@ static int ipgre_rcv(struct sk_buff *skb)
 		}
 
 		stats->rx_packets++;
-		stats->rx_bytes += skb->len;
+		stats->rx_bytes += len;
 		skb->dev = tunnel->dev;
 		dst_release(skb->dst);
 		skb->dst = NULL;
@@ -771,7 +774,7 @@ static int ipgre_tunnel_xmit(struct sk_buff *skb, struct net_device *dev)
 		old_iph = ip_hdr(skb);
 	}
 
-	skb->transport_header = skb->network_header;
+	skb_reset_transport_header(skb);
 	skb_push(skb, gre_hlen);
 	skb_reset_network_header(skb);
 	memset(&(IPCB(skb)->opt), 0, sizeof(IPCB(skb)->opt));
