64540680a7f4d9f0438dfb929e69ba3fe829fec4
akeemting <akeem@jmicron.com>
2008-10-08 19:50:03 -0700

    jme: Faulty IRQ handle bug fix
    Fix IRQ handle bug when interrupt mode.
    
    The driver was incorrectly handled and returned IRQ_HANDLED
    while the device is not generating the interrupt.
    It happened due to faulty determination of interrupt status register.
    
    Found by: "Ethan" <ethanhsiao@jmicron.com>
    Fixed by: "akeemting" <akeem@jmicron.com>
    Signed-off-by: Guo-Fu Tseng <cooldavid@cooldavid.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/jme.c
+++ drivers/net/jme.c
@@ -1464,7 +1464,7 @@ jme_intr(int irq, void *dev_id)
 	/*
 	 * Check if it's really an interrupt for us
 	 */
-	if (unlikely(intrstat == 0))
+	if (unlikely((intrstat & INTR_ENABLE) == 0))
 		return IRQ_NONE;
 
 	/*
