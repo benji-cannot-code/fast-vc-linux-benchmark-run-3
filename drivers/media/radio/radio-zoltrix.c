e02e66a20a96d5c0e4b0c57dce22d788dd04e746
Alexey Klimov <klimov.linux@gmail.com>
2008-10-09 13:46:59 -0300

    V4L/DVB (9152): radio-zoltrix: Add checking for frequency
    This patch adds printk messages, adds checking if frequency is set,
    adds return -EINVAL in right places in zoltrix-radio.
    
    Signed-off-by: Alexey Klimov <klimov.linux@gmail.com>
    Signed-off-by: Douglas Schilling Landgraf <dougsland@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/radio/radio-zoltrix.c
+++ drivers/media/radio/radio-zoltrix.c
@@ -124,8 +124,11 @@ static int zol_setfreq(struct zol_device *dev, unsigned long freq)
 	unsigned int stereo = dev->stereo;
 	int i;
 
-	if (freq == 0)
-		return 1;
+	if (freq == 0) {
+		printk(KERN_WARNING "zoltrix: received zero freq. Failed to set.\n");
+		return -EINVAL;
+	}
+
 	m = (freq / 160 - 8800) * 2;
 	f = (unsigned long long) m + 0x4d1c;
 
@@ -280,7 +283,10 @@ static int vidioc_s_frequency(struct file *file, void *priv,
 	struct zol_device *zol = video_drvdata(file);
 
 	zol->curfreq = f->frequency;
-	zol_setfreq(zol, zol->curfreq);
+	if (zol_setfreq(zol, zol->curfreq) != 0) {
+		printk(KERN_WARNING "zoltrix: Set frequency failed.\n");
+		return -EINVAL;
+	}
 	return 0;
 }
 
@@ -344,7 +350,10 @@ static int vidioc_s_ctrl(struct file *file, void *priv,
 		return 0;
 	}
 	zol->stereo = 1;
-	zol_setfreq(zol, zol->curfreq);
+	if (zol_setfreq(zol, zol->curfreq) != 0) {
+		printk(KERN_WARNING "zoltrix: Set frequency failed.\n");
+		return -EINVAL;
+	}
 #if 0
 /* FIXME: Implement stereo/mono switch on V4L2 */
 			if (v->mode & VIDEO_SOUND_STEREO) {
