ef569a05727097f4eef2d9955bec42e971c08a40
Lennert Buytenhek <buytenh@marvell.com>
2008-10-09 09:45:04 -0700

    phylib: two dynamic mii_bus allocation fallout fixes
    1. arch/powerpc/platforms/pasemi/gpio_mdio.c also needs to be
       converted over to mdiobus_{alloc,free}().
    
    2. drivers/net/phy/fixed.c used to embed a struct mii_bus into its
       struct fixed_mdio_bus and then use container_of() to go from the
       former to the latter.  Since mii bus structures are no longer
       embedded, we need to do something like use the mii bus private
       pointer to go from mii_bus to fixed_mdio_bus instead.
    
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/fixed.c
+++ drivers/net/phy/fixed.c
@@ -116,8 +116,7 @@ static int fixed_phy_update_regs(struct fixed_phy *fp)
 
 static int fixed_mdio_read(struct mii_bus *bus, int phy_id, int reg_num)
 {
-	struct fixed_mdio_bus *fmb = container_of(bus, struct fixed_mdio_bus,
-						  mii_bus);
+	struct fixed_mdio_bus *fmb = bus->priv;
 	struct fixed_phy *fp;
 
 	if (reg_num >= MII_REGS_NUM)
@@ -222,6 +221,7 @@ static int __init fixed_mdio_bus_init(void)
 
 	snprintf(fmb->mii_bus->id, MII_BUS_ID_SIZE, "0");
 	fmb->mii_bus->name = "Fixed MDIO Bus";
+	fmb->mii_bus->priv = fmb;
 	fmb->mii_bus->parent = &pdev->dev;
 	fmb->mii_bus->read = &fixed_mdio_read;
 	fmb->mii_bus->write = &fixed_mdio_write;
