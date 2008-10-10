ae2fd569091e379a159d353cb936881f5e6a40d4
Paul Moore <paul.moore@hp.com>
2008-10-10 10:16:33 -0400

    selinux: Cache NetLabel secattrs in the socket's security struct
    Previous work enabled the use of address based NetLabel selectors, which
    while highly useful, brought the potential for additional per-packet overhead
    when used.  This patch attempts to mitigate some of that overhead by caching
    the NetLabel security attribute struct within the SELinux socket security
    structure.  This should help eliminate the need to recreate the NetLabel
    secattr structure for each packet resulting in less overhead.
    
    Signed-off-by: Paul Moore <paul.moore@hp.com>
    Acked-by: James Morris <jmorris@namei.org>

--- security/selinux/include/netlabel.h
+++ security/selinux/include/netlabel.h
@@ -42,6 +42,7 @@ void selinux_netlbl_cache_invalidate(void);
 
 void selinux_netlbl_err(struct sk_buff *skb, int error, int gateway);
 
+void selinux_netlbl_sk_security_free(struct sk_security_struct *ssec);
 void selinux_netlbl_sk_security_reset(struct sk_security_struct *ssec,
 				      int family);
 
@@ -74,6 +75,12 @@ static inline void selinux_netlbl_cache_invalidate(void)
 static inline void selinux_netlbl_err(struct sk_buff *skb,
 				      int error,
 				      int gateway)
+{
+	return;
+}
+
+static inline void selinux_netlbl_sk_security_free(
+					       struct sk_security_struct *ssec)
 {
 	return;
 }
