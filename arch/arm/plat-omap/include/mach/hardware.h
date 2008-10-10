edd2f6076666a45d9225c24855a6c131f6b9ed87
Nishant Kamat <nskamat@ti.com>
2008-10-10 11:28:23 +0300

    ARM: OMAP3: Add basic board support for OMAP LDP
    This adds minimal board support for the OMAP3430 LDP development platform.
    
    Signed-off-by: Nishant Kamat <nskamat@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/mach/hardware.h
+++ arch/arm/plat-omap/include/mach/hardware.h
@@ -327,6 +327,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-omap3beagle.h"
 #endif
 
+#ifdef CONFIG_MACH_OMAP_LDP
+#include "board-ldp.h"
+#endif
+
 #ifdef CONFIG_MACH_OMAP_APOLLON
 #include "board-apollon.h"
 #endif
