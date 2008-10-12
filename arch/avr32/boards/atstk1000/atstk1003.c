a9d1b7031b87de2d5b7dac5aa9ac7c9d95aaa600
Haavard Skinnemoen <haavard.skinnemoen@atmel.com>
2008-10-12 16:02:46 +0200

    avr32: Fix build failures in board code
    Fix a few instances of board code breakage introduced by the atmel-mci
    platform interface changes.
    
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- arch/avr32/boards/atstk1000/atstk1003.c
+++ arch/avr32/boards/atstk1000/atstk1003.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spi/spi.h>
 
 #include <asm/setup.h>
+#include <asm/atmel-mci.h>
 
 #include <mach/at32ap700x.h>
 #include <mach/board.h>
--- arch/avr32/boards/atstk1000/atstk1004.c
+++ arch/avr32/boards/atstk1000/atstk1004.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <video/atmel_lcdc.h>
 
 #include <asm/setup.h>
+#include <asm/atmel-mci.h>
 
 #include <mach/at32ap700x.h>
 #include <mach/board.h>
