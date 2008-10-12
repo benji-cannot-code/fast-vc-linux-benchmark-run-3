d7fa2a28003dec88a2e8b8691780be001194d52a
Ingo Molnar <mingo@elte.hu>
2008-10-12 15:06:29 +0200

    x86: memory corruption check - cleanup
    Move the prototypes from the generic kernel.h header to the more
    appropriate include/asm-x86/bios_ebda.h header file.
    
    Also, remove the check from the power management code - this is a
    pure x86 matter for now.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nmi.h>
 
 #include <asm/processor.h>
+#include <asm/bios_ebda.h>
 #include <asm/system.h>
 #include <asm/uaccess.h>
 #include <asm/pgtable.h>
