59421da20aa1631d2c7708fc69f9f9fd1184fe22
Tzafrir Cohen <tzafrir@cohens.org.il>
2008-10-12 08:13:21 +0200

    Staging: echo: Replace __BLACKFIN__ASM__ with __bfin__
    Signed-off-by: Tzafrir Cohen <tzafrir@cohens.org.il>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/echo/echo.c
+++ drivers/staging/echo/echo.c
@@ -128,7 +128,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* adapting coeffs using the traditional stochastic descent (N)LMS algorithm */
 
 
-#ifdef __BLACKFIN_ASM__
+#ifdef __bfin__
 static void __inline__ lms_adapt_bg(struct oslec_state *ec, int clean, int shift)
 {
     int i, j;
