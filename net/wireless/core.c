3ef54c17f59bd9bc6d68aec88e3880f7586afc2c
Huang Weiyi <weiyi.huang@gmail.com>
2008-10-12 21:03:38 -0700

    wireless: remove duplicated #include
    Removed duplicated include <linux/list.h> in 
    net/wireless/core.c.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Acked-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include <linux/notifier.h>
 #include <linux/device.h>
-#include <linux/list.h>
 #include <net/genetlink.h>
 #include <net/cfg80211.h>
 #include <net/wireless.h>
