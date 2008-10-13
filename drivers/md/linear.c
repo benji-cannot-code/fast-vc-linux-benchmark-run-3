4ca9b7bde05c0d598df7f65840353aa9f5430e3b
NeilBrown <neilb@suse.de>
2008-10-13 11:55:12 +1100

    md: Remove unnecessary #includes, #defines, and function declarations.
    A lot of cruft has gathered over the years.  Time to remove it.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/linear.c
+++ drivers/md/linear.c
@@ -17,15 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  
 */
 
-#include <linux/module.h>
-
-#include <linux/raid/md.h>
-#include <linux/slab.h>
 #include <linux/raid/linear.h>
-
-#define MAJOR_NR MD_MAJOR
-#define MD_DRIVER
-#define MD_PERSONALITY
 
 /*
  * find which device holds a particular offset 
