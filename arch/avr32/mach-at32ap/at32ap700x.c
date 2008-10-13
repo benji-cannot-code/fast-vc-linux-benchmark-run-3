99cafc47cf84cb229a62caa448ee74f314ae1750
Mark Jackson <mpfj@mimc.co.uk>
2008-10-13 10:46:27 +0000

    avr32: Fix MIMC200 board use of SPD network pins
    The MIMC200 board uses the SPD output pin from the Ethernet MACs for
    other purposes.
    
    One of these is as a board-reset, so I've had to #define off the SPD
    output pin declaration.
    
    This is probably not the best way of achieving this, but works in the
    current framework.
    
    Signed-off-by: Mark Jackson <mpfj@mimc.co.uk>
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- arch/avr32/mach-at32ap/at32ap700x.c
+++ arch/avr32/mach-at32ap/at32ap700x.c
@@ -1092,7 +1092,9 @@ at32_add_device_eth(unsigned int id, struct eth_platform_data *data)
 			pin_mask |= (1 << 11);	/* RXD2 */
 			pin_mask |= (1 << 12);	/* RXD3 */
 			pin_mask |= (1 << 14);	/* RXCK */
+#ifndef CONFIG_BOARD_MIMC200
 			pin_mask |= (1 << 18);	/* SPD  */
+#endif
 		}
 
 		select_peripheral(PIOC, pin_mask, PERIPH_A, 0);
@@ -1113,8 +1115,10 @@ at32_add_device_eth(unsigned int id, struct eth_platform_data *data)
 		pin_mask |= (1 << 3);	/* MDC  */
 		pin_mask |= (1 << 2);	/* MDIO */
 
+#ifndef CONFIG_BOARD_MIMC200
 		if (!data->is_rmii)
 			pin_mask |= (1 << 15);	/* SPD  */
+#endif
 
 		select_peripheral(PIOD, pin_mask, PERIPH_B, 0);
 
