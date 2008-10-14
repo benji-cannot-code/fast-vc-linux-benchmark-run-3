3e04c4d6c01117e2bef27caca8bf6bb43c9b18fe
Stephen Rothwell <sfr@canb.auug.org.au>
2008-10-15 09:09:21 +1100

    md: build failure due to missing delay.h
    Today's linux-next build (powerpc ppc64_defconfig) failed like this:
    
    drivers/md/raid1.c: In function 'sync_request':
    drivers/md/raid1.c:1759: error: implicit declaration of function 'msleep_interruptible'
    make[3]: *** [drivers/md/raid1.o] Error 1
    make[3]: *** Waiting for unfinished jobs....
    drivers/md/raid10.c: In function 'sync_request':
    drivers/md/raid10.c:1749: error: implicit declaration of function 'msleep_interruptible'
    make[3]: *** [drivers/md/raid10.o] Error 1
    drivers/md/md.c: In function 'md_do_sync':
    drivers/md/md.c:5915: error: implicit declaration of function 'msleep'
    
    Caused by commit 6caa3b0bbdb474647f6bdd8a958ffc46f78d8d58 ("md: Remove
    unnecessary #includes, #defines, and function declarations").  I added
    the following patch.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/random.h>
 #include <linux/reboot.h>
 #include <linux/file.h>
+#include <linux/delay.h>
 
 #define MAJOR_NR MD_MAJOR
 
--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "dm-bio-list.h"
+#include <linux/delay.h>
 #include <linux/raid/raid10.h>
 #include <linux/raid/bitmap.h>
 
