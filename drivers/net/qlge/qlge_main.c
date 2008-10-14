b674bd9763c1687935b308b1f5962e54fc29f523
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2008-10-13 18:41:01 -0700

    net: fix driver build errors due to missing net/ip6_checksum.h include
      2.6.27-git2 kernel build fails with allyesconfig on powerpc with 
    build error 
    
    <introduced by commit 01f2e4ead2c51226ed1283ef6a8388ca6f4cff8f> 
    
    CC    drivers/net/enic/enic_main.o
    drivers/net/enic/enic_main.c: In function âenic_queue_wq_skb_tsoâ:
    drivers/net/enic/enic_main.c:576: error: implicit declaration of function âcsum_ipv6_magicâ
    make[3]: *** [drivers/net/enic/enic_main.o] Error 1
    
    <introduced by commit c4e84bde1d595d857d3c74b49b9c45cc770df792>
    
    drivers/net/qlge/qlge_main.c: In function âql_tsoâ:
    drivers/net/qlge/qlge_main.c:1862: error: implicit declaration of function âcsum_ipv6_magicâ
    make[3]: *** [drivers/net/qlge/qlge_main.o] Error 1
      
    <introduced by commit 95252236e73e789dd186ce796a2abc60b3a61ebe>
    
    drivers/net/jme.c: In function âjme_tx_tsoâ:
    drivers/net/jme.c:1784: error: implicit declaration of function âcsum_ipv6_magicâ
    make[2]: *** [drivers/net/jme.o] Error 1
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Signed-off-by: Ron Mercer <ron.mercer@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlge/qlge_main.c
+++ drivers/net/qlge/qlge_main.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/mm.h>
 #include <linux/vmalloc.h>
+#include <net/ip6_checksum.h>
 
 #include "qlge.h"
 
