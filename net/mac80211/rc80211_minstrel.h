1178148b2db3c1202548f3cafd59a564ea4a8677
Felix Fietkau <nbd@openwrt.org>
2008-10-15 19:13:59 +0200

    minstrel: improve performance for non-MRR drivers
    This patch enhances minstrel's performance for non-MRR setups,
    by preventing it from sampling slower rates with >95% success
    probability and by putting at least 1 non-sample frame between
    several sample frames.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/rc80211_minstrel.h
+++ net/mac80211/rc80211_minstrel.h
@@ -17,6 +17,7 @@ struct minstrel_rate {
 	unsigned int perfect_tx_time;
 	unsigned int ack_time;
 
+	int sample_limit;
 	unsigned int retry_count;
 	unsigned int retry_count_cts;
 	unsigned int retry_count_rtscts;
@@ -58,6 +59,7 @@ struct minstrel_sta_info {
 
 	int n_rates;
 	struct minstrel_rate *r;
+	bool prev_sample;
 
 	/* sampling table */
 	u8 *sample_table;
