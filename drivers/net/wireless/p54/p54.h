c8a9ddf41b734e4ef93c4ef21fe3968dc9250118
Christian Lamparter <chunkeey@web.de>
2008-10-15 04:07:56 +0200

    p54: borrow some setup code from stlc45xx
    This patch initialize all remaining values which are necessary for
    SPI firmwares.
    
    Signed-off-by: Christian Lamparter <chunkeey@web.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/p54/p54.h
+++ drivers/net/wireless/p54/p54.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 enum control_frame_types {
-	P54_CONTROL_TYPE_FILTER_SET = 0,
+	P54_CONTROL_TYPE_SETUP = 0,
 	P54_CONTROL_TYPE_CHANNEL_CHANGE,
 	P54_CONTROL_TYPE_FREQDONE,
 	P54_CONTROL_TYPE_DCFINIT,
@@ -81,7 +81,7 @@ struct p54_common {
 	struct mutex conf_mutex;
 	u8 mac_addr[ETH_ALEN];
 	u8 bssid[ETH_ALEN];
-	__le16 filter_type;
+	u16 mac_mode;
 	struct pda_iq_autocal_entry *iq_autocal;
 	unsigned int iq_autocal_len;
 	struct pda_channel_output_limit *output_limit;
