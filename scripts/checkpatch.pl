eddada20d4d903959cea1567615d3a1ad4ee8739
Andy Whitcroft <apw@shadowen.org>
2008-10-15 22:02:31 -0700

    checkpatch: macros which define structure members are not complex
    We often see macros which define structure members, these are not complex
    and necessarily do not have braces or brackets.  For example:
    
        #define _PLIST_HEAD_INIT(head)                      \
            .prio_list = LIST_HEAD_INIT((head).prio_list),  \
            .node_list = LIST_HEAD_INIT((head).node_list)
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2156,6 +2156,7 @@ sub process {
 				if ($dstat ne '' &&
 				    $dstat !~ /^(?:$Ident|-?$Constant)$/ &&
 				    $dstat !~ /$exceptions/ &&
+				    $dstat !~ /^\.$Ident\s*=/ &&
 				    $dstat =~ /$Operators/)
 				{
 					ERROR("Macros with complex values should be enclosed in parenthesis\n" . "$here\n$ctx\n");
