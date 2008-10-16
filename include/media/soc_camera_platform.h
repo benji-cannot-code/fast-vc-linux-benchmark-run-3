1cb80eb012d0b8e597d2018a0fe979a718b63f05
Magnus Damm <damm@igel.co.jp>
2008-10-16 19:51:47 -0300

    V4L/DVB (9245): video: add header to soc_camera_platform include file
    Update the soc_camera_platform header with licensing information.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/soc_camera_platform.h
+++ include/media/soc_camera_platform.h
@@ -1,4 +1,14 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Generic Platform Camera Driver Header
+ *
+ * Copyright (C) 2008 Magnus Damm
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
 #ifndef __SOC_CAMERA_H__
 #define __SOC_CAMERA_H__
 
