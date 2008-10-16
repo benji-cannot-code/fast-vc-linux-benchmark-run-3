41bfd4c3f7a08331079a48303af8a0d86298d650
Hartley Sweeten <hartleys@visionengravers.com>
2008-10-16 23:57:03 +0100

    [ARM] 5311/1: ep93xx: add core support for built in i2c bus
    Allow the ep93xx platform init code to register the built-in i2c bus.
    
    The EP93xx processor has two GPIO pins dedicated for an I2C bus. This
    patch registers the platform supplied i2c_board_info and the necessary
    platform_device information for the i2c-gpio driver to use these pins.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Ryan Mallon <ryan@bluewatersys.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ep93xx/core.c
+++ arch/arm/mach-ep93xx/core.c
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/amba/bus.h>
 #include <linux/amba/serial.h>
 #include <linux/io.h>
+#include <linux/i2c.h>
+#include <linux/i2c-gpio.h>
 
 #include <asm/types.h>
 #include <asm/setup.h>
@@ -496,6 +498,26 @@ void __init ep93xx_register_eth(struct ep93xx_eth_data *data, int copy_addr)
 
 	ep93xx_eth_data = *data;
 	platform_device_register(&ep93xx_eth_device);
+}
+
+static struct i2c_gpio_platform_data ep93xx_i2c_data = {
+	.sda_pin		= EP93XX_GPIO_LINE_EEDAT,
+	.sda_is_open_drain	= 0,
+	.scl_pin		= EP93XX_GPIO_LINE_EECLK,
+	.scl_is_open_drain	= 0,
+	.udelay			= 2,
+};
+
+static struct platform_device ep93xx_i2c_device = {
+	.name			= "i2c-gpio",
+	.id			= 0,
+	.dev.platform_data	= &ep93xx_i2c_data,
+};
+
+void __init ep93xx_register_i2c(struct i2c_board_info *devices, int num)
+{
+	i2c_register_board_info(0, devices, num);
+	platform_device_register(&ep93xx_i2c_device);
 }
 
 extern void ep93xx_gpio_init(void);
