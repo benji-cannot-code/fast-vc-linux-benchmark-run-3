a7832a5668d47b673dddb9d294ffcc1099cdd9db
Theodore Ts'o <tytso@mit.edu>
2008-10-16 10:06:27 -0400

    ext4: let the block device know when unused blocks can be discarded
    Let the block device know when unused blocks can be discarded, using
    the new sb_issue_discard() interface.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/mballoc.h
+++ fs/ext4/mballoc.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pagemap.h>
 #include <linux/seq_file.h>
 #include <linux/version.h>
+#include <linux/blkdev.h>
+#include <linux/marker.h>
 #include "ext4_jbd2.h"
 #include "ext4.h"
 #include "group.h"
