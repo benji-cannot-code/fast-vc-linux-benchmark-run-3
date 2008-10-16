59e3824a247041bfc9010931102594071d81ff4f
Dmitry Baryshkov <dbaryshkov@gmail.com>
2008-10-16 18:43:48 +0400

    [MTD] sharpsl-nand: use platform_data for model-specific values
    Add platform_data which holds all model-specific values, like badblocks
    pattern, oobinfo, partitions.
    
    Signed-off-by: Dmitry Baryshkov <dbaryshkov@gmail.com>

---
+++ include/linux/mtd/sharpsl.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * SharpSL NAND support
+ *
+ * Copyright (C) 2008 Dmitry Baryshkov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/mtd/nand.h>
+#include <linux/mtd/nand_ecc.h>
+#include <linux/mtd/partitions.h>
+
+struct sharpsl_nand_platform_data {
+	struct nand_bbt_descr	*badblock_pattern;
+	struct nand_ecclayout	*ecc_layout;
+	struct mtd_partition	*partitions;
+	unsigned int		nr_partitions;
+};
