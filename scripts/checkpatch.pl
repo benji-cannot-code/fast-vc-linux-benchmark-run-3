f09af03fd667793b9705ec92b95b5551545279ed
Andy Whitcroft <apw@shadowen.org>
2008-10-15 22:02:23 -0700

    checkpatch: %Lx tests should hand %% as a literal
    Ensure that we handle literal %'s correctly when adjacent to a %Lx.
    
    	%Lx	bad
    	%%Lx	good
    	%%%Lx	bad
    
    Signed-off-by: Andy Whitcroft <apw@shadowen.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2393,6 +2393,7 @@ sub process {
 		my $string;
 		while ($line =~ /(?:^|")([X\t]*)(?:"|$)/g) {
 			$string = substr($rawline, $-[1], $+[1] - $-[1]);
+			$string =~ s/%%/__/g;
 			if ($string =~ /(?<!%)%L[udi]/) {
 				WARN("\%Ld/%Lu are not-standard C, use %lld/%llu\n" . $herecurr);
 				last;
