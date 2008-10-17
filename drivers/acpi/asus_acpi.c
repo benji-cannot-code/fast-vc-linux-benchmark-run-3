9029f826b7474628e26ec67a9b8db77a8750408d
Zhang Rui <rui.zhang@intel.com>
2008-10-17 01:42:41 -0400

    don't load asus-acpi if model is not supported
    asus_hotk_get_info should return -ENODEV if the model is not supported.
    http://bugzilla.kernel.org/show_bug.cgi?id=10389
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/asus_acpi.c
+++ drivers/acpi/asus_acpi.c
@@ -1245,6 +1245,8 @@ static int asus_hotk_get_info(void)
 			       "default values\n", string);
 			printk(KERN_NOTICE
 			       "  send /proc/acpi/dsdt to the developers\n");
+			kfree(model);
+			return -ENODEV;
 		}
 		hotk->methods = &model_conf[hotk->model];
 		return AE_OK;
