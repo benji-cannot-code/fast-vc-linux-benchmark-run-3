17885bf304518ff9e1941e80f1f2ac865121b5ee
Isaku Yamahata <yamahata@valinux.co.jp>
2008-10-17 11:18:07 +0900

    ia64/pv_ops/xen: implement xen pv_irq_ops.
    implement xen pv_irq_ops to paravirtualize irq handling
    with xen event channel.
    
    Cc: Jeremy Fitzhardinge <jeremy@goop.org>
    Signed-off-by: Akio Takebe <takebe_akio@jp.fujitsu.com>
    Signed-off-by: Alex Williamson <alex.williamson@hp.com>
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/xen/xen_pv_ops.c
+++ arch/ia64/xen/xen_pv_ops.c
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/xen/xencomm.h>
 #include <asm/xen/privop.h>
 
+#include "irq_xen.h"
+
 /***************************************************************************
  * general info
  */
@@ -355,6 +357,7 @@ xen_setup_pv_ops(void)
 	pv_init_ops = xen_init_ops;
 	pv_cpu_ops = xen_cpu_ops;
 	pv_iosapic_ops = xen_iosapic_ops;
+	pv_irq_ops = xen_irq_ops;
 
 	paravirt_cpu_asm_init(&xen_cpu_asm_switch);
 }
