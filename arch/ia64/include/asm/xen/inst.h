2430ef9c0ec426967bfa47058f3018ca298e556c
Isaku Yamahata <yamahata@valinux.co.jp>
2008-10-17 11:18:03 +0900

    ia64/pv_ops/xen: paravirtualize ivt.S for xen.
    paravirtualize ivt.S for xen by multi compile.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/include/asm/xen/inst.h
+++ arch/ia64/include/asm/xen/inst.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/xen/privop.h>
 
+#define ia64_ivt				xen_ivt
 #define DO_SAVE_MIN				XEN_DO_SAVE_MIN
 
 #define MOV_FROM_IFA(reg)	\
