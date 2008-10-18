dd880a64eec4590cc54d3dec47994fab0a7bb569
Huang Weiyi <weiyi.huang@gmail.com>
2008-10-18 12:41:57 +0800

    Staging: SLICOSS: remove unused #include <version.h>
    The file(s) below do not use LINUX_VERSION_CODE nor KERNEL_VERSION.
      drivers/staging/slicoss/slicoss.c
    
    This patch removes the said #include <version.h>.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/slicoss/slicoss.c
+++ drivers/staging/slicoss/slicoss.c
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *       IS-NIC driver.
  */
 
-#include <linux/version.h>
 
 #define SLIC_DUMP_ENABLED               0
 #define KLUDGE_FOR_4GB_BOUNDARY         1
