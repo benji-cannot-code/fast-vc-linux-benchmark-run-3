59efbdb23dd7fdb9ac812dfd2537b16dacdcd0ce
Lee Schermerhorn <lee.schermerhorn@hp.com>
2008-10-18 20:26:40 -0700

    unevictable lru: add event counting with statistics
    Fix to unevictable-lru-page-statistics.patch
    
    Add unevictable lru infrastructure vm events to the statistics patch.
    Rename the "NORECL_" and "noreclaim_" symbols and text strings to
    "UNEVICTABLE_" and "unevictable_", respectively.
    
    Currently, both the infrastructure and the mlocked pages event are
    added by a single patch later in the series.  This makes it difficult
    to add or rework the incremental patches.  The events actually "belong"
    with the stats, so pull them up to here.
    
    Also, restore the event counting to putback_lru_page().  This was removed
    from previous patch in series where it was "misplaced".  The actual events
    weren't defined that early.
    
    Signed-off-by: Lee Schermerhorn <lee.schermerhorn@hp.com>
    Cc: Rik van Riel <riel@redhat.com>
    Reviewed-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -485,6 +485,7 @@ void putback_lru_page(struct page *page)
 {
 	int lru;
 	int active = !!TestClearPageActive(page);
+	int was_unevictable = PageUnevictable(page);
 
 	VM_BUG_ON(PageLRU(page));
 
@@ -525,6 +526,11 @@ redo:
 		 * nothing to do here.
 		 */
 	}
+
+	if (was_unevictable && lru != LRU_UNEVICTABLE)
+		count_vm_event(UNEVICTABLE_PGRESCUED);
+	else if (!was_unevictable && lru == LRU_UNEVICTABLE)
+		count_vm_event(UNEVICTABLE_PGCULLED);
 
 	put_page(page);		/* drop ref from isolate */
 }
