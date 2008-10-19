12f22f3c69b1eb31ab5a8d0a2d6c86d13edf2774
Hans Verkuil <hverkuil@xs4all.nl>
2008-10-19 18:54:26 -0300

    V4L/DVB (9327): v4l: use video_device.num instead of minor in video%d
    The kernel number of a v4l2 node (e.g. videoX, radioX or vbiX) is now
    independent of the minor number. So instead of using the minor field
    of the video_device struct one has to use the num field: this always
    contains the kernel number of the device node.
    
    I forgot about this when I did the v4l2 core change, so this patch
    converts all drivers that use it in one go. Luckily the change is
    trivial.
    
    Cc: michael@mihu.de
    Cc: mchehab@infradead.org
    Cc: corbet@lwn.net
    Cc: luca.risolia@studio.unibo.it
    Cc: isely@pobox.com
    Cc: pe1rxq@amsat.org
    Cc: royale@zerezo.com
    Cc: mkrufky@linuxtv.org
    Cc: stoth@linuxtv.org
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/usbvision/usbvision-video.c
+++ drivers/media/video/usbvision/usbvision-video.c
@@ -1441,7 +1441,7 @@ static void usbvision_unregister_video(struct usb_usbvision *usbvision)
 	// vbi Device:
 	if (usbvision->vbi) {
 		PDEBUG(DBG_PROBE, "unregister /dev/vbi%d [v4l2]",
-		       usbvision->vbi->minor & 0x1f);
+		       usbvision->vbi->num);
 		if (usbvision->vbi->minor != -1) {
 			video_unregister_device(usbvision->vbi);
 		} else {
@@ -1453,7 +1453,7 @@ static void usbvision_unregister_video(struct usb_usbvision *usbvision)
 	// Radio Device:
 	if (usbvision->rdev) {
 		PDEBUG(DBG_PROBE, "unregister /dev/radio%d [v4l2]",
-		       usbvision->rdev->minor & 0x1f);
+		       usbvision->rdev->num);
 		if (usbvision->rdev->minor != -1) {
 			video_unregister_device(usbvision->rdev);
 		} else {
@@ -1465,7 +1465,7 @@ static void usbvision_unregister_video(struct usb_usbvision *usbvision)
 	// Video Device:
 	if (usbvision->vdev) {
 		PDEBUG(DBG_PROBE, "unregister /dev/video%d [v4l2]",
-		       usbvision->vdev->minor & 0x1f);
+		       usbvision->vdev->num);
 		if (usbvision->vdev->minor != -1) {
 			video_unregister_device(usbvision->vdev);
 		} else {
@@ -1491,7 +1491,7 @@ static int __devinit usbvision_register_video(struct usb_usbvision *usbvision)
 		goto err_exit;
 	}
 	printk(KERN_INFO "USBVision[%d]: registered USBVision Video device /dev/video%d [v4l2]\n",
-	       usbvision->nr,usbvision->vdev->minor & 0x1f);
+	       usbvision->nr, usbvision->vdev->num);
 
 	// Radio Device:
 	if (usbvision_device_data[usbvision->DevModel].Radio) {
@@ -1508,7 +1508,7 @@ static int __devinit usbvision_register_video(struct usb_usbvision *usbvision)
 			goto err_exit;
 		}
 		printk(KERN_INFO "USBVision[%d]: registered USBVision Radio device /dev/radio%d [v4l2]\n",
-		       usbvision->nr, usbvision->rdev->minor & 0x1f);
+		       usbvision->nr, usbvision->rdev->num);
 	}
 	// vbi Device:
 	if (usbvision_device_data[usbvision->DevModel].vbi) {
@@ -1524,7 +1524,7 @@ static int __devinit usbvision_register_video(struct usb_usbvision *usbvision)
 			goto err_exit;
 		}
 		printk(KERN_INFO "USBVision[%d]: registered USBVision VBI device /dev/vbi%d [v4l2] (Not Working Yet!)\n",
-		       usbvision->nr,usbvision->vbi->minor & 0x1f);
+		       usbvision->nr, usbvision->vbi->num);
 	}
 	// all done
 	return 0;
