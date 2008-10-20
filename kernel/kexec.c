3efbd8bdd626e5bd15dea812156bdf85b25941f3
Luck, Tony <tony.luck@intel.com>
2008-10-20 15:23:40 -0700

    kexec: fix crash_save_vmcoreinfo_init build problem
    This fixes
    
      kernel/kexec.c: In function 'crash_save_vmcoreinfo_init':
      kernel/kexec.c:1374: error: 'vmlist' undeclared (first use in this function)
      kernel/kexec.c:1374: error: (Each undeclared identifier is reported only once
      kernel/kexec.c:1374: error: for each function it appears in.)
      kernel/kexec.c:1410: error: invalid use of undefined type 'struct vm_struct'
      make[1]: *** [kernel/kexec.o] Error 1
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/kexec.c
+++ kernel/kexec.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm.h>
 #include <linux/cpu.h>
 #include <linux/console.h>
+#include <linux/vmalloc.h>
 
 #include <asm/page.h>
 #include <asm/uaccess.h>
