830e2137e426ea1401070d6a365d6073f0ecf0d7
Huang Weiyi <weiyi.huang@gmail.com>
2008-10-21 06:43:33 +0800

    mm: remove duplicated #include's
    Removed duplicated #include <linux/vmalloc.h> in mm/vmalloc.c and
    "internal.h" in mm/memory.c.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/seq_file.h>
 #include <linux/debugobjects.h>
-#include <linux/vmalloc.h>
 #include <linux/kallsyms.h>
 #include <linux/list.h>
 #include <linux/rbtree.h>
