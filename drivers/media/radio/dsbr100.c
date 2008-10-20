35eae43718c812fd49c54f331ead4c53e410d303
Alexey Klimov <klimov.linux@gmail.com>
2008-10-20 00:00:03 -0300

    V4L/DVB (9306): dsbr100: Add frequency check
    Add checking for frequency and printk if -1 returned.
    
    Signed-off-by: Alexey Klimov <klimov.linux@gmail.com>
    Signed-off-by: Douglas Schilling Landgraf <dougsland@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/radio/dsbr100.c
+++ drivers/media/radio/dsbr100.c
@@ -412,6 +412,7 @@ static int vidioc_s_audio(struct file *file, void *priv,
 static int usb_dsbr100_open(struct inode *inode, struct file *file)
 {
 	struct dsbr100_device *radio = video_drvdata(file);
+	int retval;
 
 	lock_kernel();
 	radio->users = 1;
@@ -424,7 +425,12 @@ static int usb_dsbr100_open(struct inode *inode, struct file *file)
 		unlock_kernel();
 		return -EIO;
 	}
-	dsbr100_setfreq(radio, radio->curfreq);
+
+	retval = dsbr100_setfreq(radio, radio->curfreq);
+
+	if (retval == -1)
+		printk(KERN_WARNING KBUILD_MODNAME ": Set frequency failed\n");
+
 	unlock_kernel();
 	return 0;
 }
