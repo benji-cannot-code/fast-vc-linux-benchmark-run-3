1ecc35c85ad774db37b8b6415e9494f1ef6537ef
Huang Weiyi <weiyi.huang@gmail.com>
2008-10-21 06:33:42 +0800

    [MTD] [NAND] OMAP2: remove duplicated #include
    Removed duplicated #include <asm/arch/gpmc.h> in
    drivers/mtd/onenand/omap2.c.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/onenand/omap2.c
+++ drivers/mtd/onenand/omap2.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/gpmc.h>
 #include <asm/arch/onenand.h>
 #include <asm/arch/gpio.h>
-#include <asm/arch/gpmc.h>
 #include <asm/arch/pm.h>
 
 #include <linux/dma-mapping.h>
