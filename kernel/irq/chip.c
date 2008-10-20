8f9f694deac030ab79245ae6c127d588d517bc78
Chris Friesen <cfriesen@nortel.com>
2008-10-20 12:41:58 -0600

    genirq: fix set_irq_type() when recording trigger type
    Impact: fix boot hang on a G5
    
    In set_irq_type() we want to pass the type rather than the current
    interrupt state.
    
    Signed-off-by: Chris Friesen <cfriesen@nortel.com>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -128,7 +128,7 @@ int set_irq_type(unsigned int irq, unsigned int type)
 		return 0;
 
 	spin_lock_irqsave(&desc->lock, flags);
-	ret = __irq_set_trigger(desc, irq, flags);
+	ret = __irq_set_trigger(desc, irq, type);
 	spin_unlock_irqrestore(&desc->lock, flags);
 	return ret;
 }
