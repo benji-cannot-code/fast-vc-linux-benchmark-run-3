146c15477866aab5893fad8f949cc8348d4a8910
Kumar Gala <galak@kernel.crashing.org>
2008-10-20 03:16:55 +0000

    powerpc: Fix build issue with CONFIG_RELOCATABLE=y
    There are two issues when we enable CONFIG_RELOCATABLE.  The first is due
    to the fact that phys_addr_t is now defined in linux/types.h.  The second
    is due to the fact that the DMA code changes expose memstart_addr to
    prom_init.c
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/page.h
+++ arch/powerpc/include/asm/page.h
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/asm-compat.h>
 #include <asm/kdump.h>
-#include <asm/types.h>
+#ifndef __ASSEMBLY__
+#include <linux/types.h>
+#endif
 
 /*
  * On PPC32 page size is 4K. For PPC64 we support either 4K or 64K software
@@ -74,6 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if defined(CONFIG_RELOCATABLE)
 #ifndef __ASSEMBLY__
+
 extern phys_addr_t memstart_addr;
 extern phys_addr_t kernstart_addr;
 #endif
