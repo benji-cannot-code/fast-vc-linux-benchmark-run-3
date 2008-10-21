0c27e0f4c61c84d8b14e61fe530e2b270bffd507
Ben Dooks <ben-linux@fluff.org>
2008-10-21 14:06:26 +0100

    [ARM] S3C24XX: Split map.h into plat-s3c24xx and mach-s3c2410
    Split the map.h definitions into common S3C24XX code by
    adding arch/arm/plat-s3c24xx/include/plat/map.h and
    altering the machine specific header for the S3C24A0.
    
    As we add a new <plat/map.h> we move the original one
    in arch/arm/plat-s3c include directory to be called
    map-base.h to distinguish the two files.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c24a0/include/mach/map.h
+++ arch/arm/mach-s3c24a0/include/mach/map.h
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_ARCH_24A0_MAP_H
 #define __ASM_ARCH_24A0_MAP_H __FILE__
 
+#include <plat/map-base.h>
+#include <plat/map.h>
+
 #define S3C24A0_PA_IO_BASE	(0x40000000)
 #define S3C24A0_PA_CLKPWR	(0x40000000)
 #define S3C24A0_PA_IRQ		(0x40200000)
@@ -75,5 +78,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define S3C24XX_PA_RTC		S3C24A0_PA_RTC
 #define S3C24XX_PA_ADC		S3C24A0_PA_ADC
 #define S3C24XX_PA_SPI		S3C24A0_PA_SPI
+#define S3C24XX_PA_SDI		S3C24A0_PA_SDI
+#define S3C24XX_PA_NAND		S3C24A0_PA_NAND
 
 #endif /* __ASM_ARCH_24A0_MAP_H */
