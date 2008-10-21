5f4379b2a301cc53f60e61a8e67dfe1a275c5de0
Ben Dooks <ben-linux@fluff.org>
2008-10-21 14:06:40 +0100

    [ARM] S3C64XX: Initial arch header files
    Add the initial header files for the S3C64XX support to satisfy the
    minimal requirements to build a kernel. Some definitions will therefore
    be placeholders or empty functions that will ensure that the system can
    build and have base functionality. These will be filled in at a later
    date.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

---
+++ arch/arm/mach-s3c6400/include/mach/irqs.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* linux/arch/arm/mach-s3c6400/include/mach/irqs.h
+ *
+ * Copyright 2008 Openmoko, Inc.
+ * Copyright 2008 Simtec Electronics
+ *      Ben Dooks <ben@simtec.co.uk>
+ *      http://armlinux.simtec.co.uk/
+ *
+ * S3C6400 - IRQ definitions
+ */
+
+#ifndef __ASM_ARCH_IRQS_H
+#define __ASM_ARCH_IRQS_H __FILE__
+
+#ifndef __ASM_ARM_IRQ_H
+#error "Do not include this directly, instead #include <asm/irq.h>"
+#endif
+
+#include <plat/irqs.h>
+
+#endif /* __ASM_ARCH_IRQ_H */
