b6d322aa035ed83eb268c80318946c607173f042
Ben Dooks <ben-linux@fluff.org>
2008-10-21 14:07:02 +0100

    [ARM] S3C64XX: Clock support for S3C6400/S3C6410
    Add the PLL clock initialisation and clock registration
    and include the clocks sourced via CLKDIVx for most of
    the on-chip peripherals.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c6410/cpu.c
+++ arch/arm/mach-s3c6410/cpu.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/cpu.h>
 #include <plat/devs.h>
 #include <plat/clock.h>
+#include <plat/s3c6400.h>
 #include <plat/s3c6410.h>
 
 /* Initial IO mappings */
@@ -58,6 +59,8 @@ void __init s3c6410_init_clocks(int xtal)
 	printk(KERN_INFO "%s: initialising clocks\n", __func__);
 	s3c24xx_register_baseclocks(xtal);
 	s3c64xx_register_clocks();
+	s3c6400_register_clocks();
+	s3c6400_setup_clocks();
 }
 
 void __init s3c6410_init_irq(void)
