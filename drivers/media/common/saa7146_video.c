9f6742aeaed63abdeb3beefe2f51b131e6975373
Mauro Carvalho Chehab <mchehab@redhat.com>
2008-10-21 11:27:20 -0300

    V4L/DVB (9330): Get rid of inode parameter at v4l_compat_translate_ioctl()
    The inode parameter at v4l_compat_translate_ioctl() were just passed over several
    places just to keep compatible with fops.ioctl. However, it weren't used anywere.
    
    This patch gets hid of this unused parameter.
    
    Cc: Laurent Pinchart <laurent.pinchart@skynet.be>
    Cc: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/saa7146_video.c
+++ drivers/media/common/saa7146_video.c
@@ -835,7 +835,7 @@ static int video_end(struct saa7146_fh *fh, struct file *file)
  * copying is done already, arg is a kernel pointer.
  */
 
-int saa7146_video_do_ioctl(struct inode *inode, struct file *file, unsigned int cmd, void *arg)
+static int __saa7146_video_do_ioctl(struct file *file, unsigned int cmd, void *arg)
 {
 	struct saa7146_fh *fh  = file->private_data;
 	struct saa7146_dev *dev = fh->dev;
@@ -1216,10 +1216,16 @@ int saa7146_video_do_ioctl(struct inode *inode, struct file *file, unsigned int 
 	}
 #endif
 	default:
-		return v4l_compat_translate_ioctl(inode,file,cmd,arg,
-						  saa7146_video_do_ioctl);
+		return v4l_compat_translate_ioctl(file, cmd, arg,
+						  __saa7146_video_do_ioctl);
 	}
 	return 0;
+}
+
+int saa7146_video_do_ioctl(struct inode *inode, struct file *file,
+				    unsigned int cmd, void *arg)
+{
+	return __saa7146_video_do_ioctl(file, cmd, arg);
 }
 
 /*********************************************************************************/
