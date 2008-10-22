e11c670e2eaeceded9226542bf878d4ba052773a
Krishna Kumar <krkumar2@in.ibm.com>
2008-10-22 14:48:36 +0530

    nfsd: Fix memory leak in nfsd_getxattr
    Fix a memory leak in nfsd_getxattr. nfsd_getxattr should free up memory
    	that it allocated if vfs_getxattr fails.
    
    Signed-off-by: Krishna Kumar <krkumar2@in.ibm.com>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -411,6 +411,7 @@ out_nfserr:
 static ssize_t nfsd_getxattr(struct dentry *dentry, char *key, void **buf)
 {
 	ssize_t buflen;
+	ssize_t ret;
 
 	buflen = vfs_getxattr(dentry, key, NULL, 0);
 	if (buflen <= 0)
@@ -420,7 +421,10 @@ static ssize_t nfsd_getxattr(struct dentry *dentry, char *key, void **buf)
 	if (!*buf)
 		return -ENOMEM;
 
-	return vfs_getxattr(dentry, key, *buf, buflen);
+	ret = vfs_getxattr(dentry, key, *buf, buflen);
+	if (ret < 0)
+		kfree(*buf);
+	return ret;
 }
 #endif
 
