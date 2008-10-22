3777235861aa1b2b086c974b83b2c3251d8aece2
Stephen Rothwell <sfr@canb.auug.org.au>
2008-10-22 13:39:55 +1100

    PCI: don't export linux/io.h from pci.h
    Move the include of io.h down into the #ifdef __KERNEL__ protected
    region.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LINUX_PCI_H
 
 #include <linux/pci_regs.h>	/* The pci register defines */
-#include <linux/io.h>
 
 /*
  * The PCI interface treats multi-function devices as independent
@@ -53,6 +52,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kobject.h>
 #include <asm/atomic.h>
 #include <linux/device.h>
+#include <linux/io.h>
 
 /* Include the ID list */
 #include <linux/pci_ids.h>
