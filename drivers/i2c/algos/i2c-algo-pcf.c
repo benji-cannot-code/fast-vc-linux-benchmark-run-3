3ab6631efb062c8480817d9a875c5afdd52f11bc
David Miller <davem@davemloft.net>
2008-10-22 20:21:30 +0200

    i2c-algo-pcf: Add adapter hooks around xfer begin and end
    Some I2C bus implementations need to synchronize with external
    entities, such as system firmware, which might also be programming the
    same I2C bus.
    
    In order to facilitate this add ->xfer_begin() and ->xfer_end() hooks
    which are invoked around pcf_xfer().
    
    [JD: Make these hooks optional.]
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- drivers/i2c/algos/i2c-algo-pcf.c
+++ drivers/i2c/algos/i2c-algo-pcf.c
@@ -332,13 +332,16 @@ static int pcf_xfer(struct i2c_adapter *i2c_adap,
 	int i;
 	int ret=0, timeout, status;
     
+	if (adap->xfer_begin)
+		adap->xfer_begin(adap->data);
 
 	/* Check for bus busy */
 	timeout = wait_for_bb(adap);
 	if (timeout) {
 		DEB2(printk(KERN_ERR "i2c-algo-pcf.o: "
 		            "Timeout waiting for BB in pcf_xfer\n");)
-		return -EIO;
+		i = -EIO;
+		goto out;
 	}
 	
 	for (i = 0;ret >= 0 && i < num; i++) {
@@ -360,12 +363,14 @@ static int pcf_xfer(struct i2c_adapter *i2c_adap,
 		if (timeout) {
 			if (timeout == -EINTR) {
 				/* arbitration lost */
-				return (-EINTR);
+				i = -EINTR;
+				goto out;
 			}
 			i2c_stop(adap);
 			DEB2(printk(KERN_ERR "i2c-algo-pcf.o: Timeout waiting "
 				    "for PIN(1) in pcf_xfer\n");)
-			return (-EREMOTEIO);
+			i = -EREMOTEIO;
+			goto out;
 		}
     
 #ifndef STUB_I2C
@@ -373,7 +378,8 @@ static int pcf_xfer(struct i2c_adapter *i2c_adap,
 		if (status & I2C_PCF_LRB) {
 			i2c_stop(adap);
 			DEB2(printk(KERN_ERR "i2c-algo-pcf.o: No LRB(1) in pcf_xfer\n");)
-			return (-EREMOTEIO);
+			i = -EREMOTEIO;
+			goto out;
 		}
 #endif
     
@@ -405,6 +411,9 @@ static int pcf_xfer(struct i2c_adapter *i2c_adap,
 		}
 	}
 
+out:
+	if (adap->xfer_end)
+		adap->xfer_end(adap->data);
 	return (i);
 }
 
