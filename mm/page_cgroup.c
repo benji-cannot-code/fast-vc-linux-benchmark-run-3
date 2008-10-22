f308627379d28c6bb5cee1e4f5d63d5bbf2726fd
Paul Mundt <lethal@linux-sh.org>
2008-10-22 14:14:58 -0700

    mm: page_cgroup needs linux/vmalloc.h for vmalloc_node()/vfree().
    mm/page_cgroup.c: In function 'init_section_page_cgroup':
    mm/page_cgroup.c:111: error: implicit declaration of function 'vmalloc_node'
    mm/page_cgroup.c:111: warning: assignment makes pointer from integer without a cast
    mm/page_cgroup.c: In function '__free_page_cgroup':
    mm/page_cgroup.c:140: error: implicit declaration of function 'vfree'
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>
    Reviewed-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_cgroup.c
+++ mm/page_cgroup.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/page_cgroup.h>
 #include <linux/hash.h>
 #include <linux/memory.h>
+#include <linux/vmalloc.h>
 
 static void __meminit
 __init_page_cgroup(struct page_cgroup *pc, unsigned long pfn)
