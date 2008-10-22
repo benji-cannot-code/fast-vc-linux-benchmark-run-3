61e4013c7dd3efa0f7a81154f3670c5ac0e01236
Pekka Enberg <penberg@cs.helsinki.fi>
2008-10-22 11:03:54 +0300

    Staging: w35und: merge wblinux struct to adapter
    Zaps another compatability layer from the driver code.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Acked-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/linux/sysdef.h
+++ drivers/staging/winbond/linux/sysdef.h
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../sme_api.h"
 #include "../gl_80211.h"
 #include "../mto.h"
-#include "../wblinux_s.h"
 #include "../wbhal_s.h"
 
 
