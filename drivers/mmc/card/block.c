d1e3d0a190747bfa3524f54052f92b6790dc0923
Harvey Harrison <harvey.harrison@gmail.com>
2008-10-22 17:09:00 -0700

    mmc: trivial annotation of 'blocks'
    sg_init_one is reading a be32, annotate as such.
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -146,7 +146,7 @@ struct mmc_blk_request {
 static u32 mmc_sd_num_wr_blocks(struct mmc_card *card)
 {
 	int err;
-	u32 blocks;
+	__be32 blocks;
 
 	struct mmc_request mrq;
 	struct mmc_command cmd;
@@ -205,9 +205,7 @@ static u32 mmc_sd_num_wr_blocks(struct mmc_card *card)
 	if (cmd.error || data.error)
 		return (u32)-1;
 
-	blocks = ntohl(blocks);
-
-	return blocks;
+	return ntohl(blocks);
 }
 
 static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
