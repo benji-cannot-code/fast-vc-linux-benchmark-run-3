c8207ba2620b6ad337e8aaceffdd3d345a08fbdf
Linus Torvalds <torvalds@linux-foundation.org>
2008-10-23 12:07:17 -0700

    Fix compile warning in kernel/params.c
    Move free_module_param_attrs() into the CONFIG_MODULES section, since
    it's only used inside there. Thus avoiding the warning
    
      kernel/params.c:514: warning: 'free_module_param_attrs' defined but not used
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/params.c
+++ kernel/params.c
@@ -511,6 +511,7 @@ fail:
 	return err;
 }
 
+#ifdef CONFIG_MODULES
 static void free_module_param_attrs(struct module_kobject *mk)
 {
 	kfree(mk->mp->grp.attrs);
@@ -518,7 +519,6 @@ static void free_module_param_attrs(struct module_kobject *mk)
 	mk->mp = NULL;
 }
 
-#ifdef CONFIG_MODULES
 /*
  * module_param_sysfs_setup - setup sysfs support for one module
  * @mod: module
