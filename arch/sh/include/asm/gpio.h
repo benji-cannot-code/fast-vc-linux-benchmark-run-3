30d8d9c03c41f7e45e4fadfb3d5c05ae4e7bd28b
Paul Mundt <lethal@linux-sh.org>
2008-10-23 12:37:20 +0900

    sh: asm/gpio.h needs linux/kernel.h for might_sleep()/WARN_ON().
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/gpio.h
+++ arch/sh/include/asm/gpio.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SH_GPIO_H
 #define __ASM_SH_GPIO_H
 
+#include <linux/kernel.h>
 #include <linux/errno.h>
 
 #if defined(CONFIG_CPU_SH3)
