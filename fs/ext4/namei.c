cbe2ebb17ce0de8ca14b960b24ac3dd6b6a563c2
Christoph Hellwig <hch@lst.de>
2008-10-23 20:33:29 +0200

    ext4: remove unused variable in ext4_get_parent
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    [ All users removed in "switch all filesystems over to d_obtain_alias",
      aka commit 440037287c5ebb07033ab927ca16bb68c291d309 ]
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1062,7 +1062,6 @@ static struct dentry *ext4_lookup(struct inode *dir, struct dentry *dentry, stru
 struct dentry *ext4_get_parent(struct dentry *child)
 {
 	unsigned long ino;
-	struct dentry *parent;
 	struct inode *inode;
 	static const struct qstr dotdot = {
 		.name = "..",
