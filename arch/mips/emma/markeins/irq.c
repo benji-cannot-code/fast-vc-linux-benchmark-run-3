9f92c4a53bf936f0535bff00e61ddfdfc841148d
Shinya Kuribayashi <skuribay@ruby.dti.ne.jp>
2008-10-24 01:33:10 +0900

    MIPS: Markeins: Remove runtime debug prints
    Remove runtime db_* macros as we don't need them any more.  In general,
    such helpers are useful for initial porting, but once approved, they are
    not indispensable.
    
    Signed-off-by: Shinya Kuribayashi <shinya.kuribayashi@necel.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/emma/markeins/irq.c
+++ arch/mips/emma/markeins/irq.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq_cpu.h>
 #include <asm/system.h>
 #include <asm/mipsregs.h>
-#include <asm/debug.h>
 #include <asm/addrspace.h>
 #include <asm/bootinfo.h>
 
@@ -68,7 +67,6 @@ void ll_emma2rh_irq_enable(int emma2rh_irq)
 		    (emma2rh_irq / 32);
 	reg_value = emma2rh_in32(reg_index);
 	reg_bitmask = 0x1 << (emma2rh_irq % 32);
-	db_assert((reg_value & reg_bitmask) == 0);
 	emma2rh_out32(reg_index, reg_value | reg_bitmask);
 }
 
@@ -83,7 +81,6 @@ void ll_emma2rh_irq_disable(int emma2rh_irq)
 		    (emma2rh_irq / 32);
 	reg_value = emma2rh_in32(reg_index);
 	reg_bitmask = 0x1 << (emma2rh_irq % 32);
-	db_assert((reg_value & reg_bitmask) != 0);
 	emma2rh_out32(reg_index, reg_value & ~reg_bitmask);
 }
 
@@ -119,9 +116,6 @@ void ll_emma2rh_sw_irq_enable(int irq)
 {
 	u32 reg;
 
-	db_assert(irq >= 0);
-	db_assert(irq < NUM_EMMA2RH_IRQ_SW);
-
 	reg = emma2rh_in32(EMMA2RH_BHIF_SW_INT_EN);
 	reg |= 1 << irq;
 	emma2rh_out32(EMMA2RH_BHIF_SW_INT_EN, reg);
@@ -130,9 +124,6 @@ void ll_emma2rh_sw_irq_enable(int irq)
 void ll_emma2rh_sw_irq_disable(int irq)
 {
 	u32 reg;
-
-	db_assert(irq >= 0);
-	db_assert(irq < 32);
 
 	reg = emma2rh_in32(EMMA2RH_BHIF_SW_INT_EN);
 	reg &= ~(1 << irq);
@@ -171,9 +162,6 @@ void ll_emma2rh_gpio_irq_enable(int irq)
 {
 	u32 reg;
 
-	db_assert(irq >= 0);
-	db_assert(irq < NUM_EMMA2RH_IRQ_GPIO);
-
 	reg = emma2rh_in32(EMMA2RH_GPIO_INT_MASK);
 	reg |= 1 << irq;
 	emma2rh_out32(EMMA2RH_GPIO_INT_MASK, reg);
@@ -182,9 +170,6 @@ void ll_emma2rh_gpio_irq_enable(int irq)
 void ll_emma2rh_gpio_irq_disable(int irq)
 {
 	u32 reg;
-
-	db_assert(irq >= 0);
-	db_assert(irq < NUM_EMMA2RH_IRQ_GPIO);
 
 	reg = emma2rh_in32(EMMA2RH_GPIO_INT_MASK);
 	reg &= ~(1 << irq);
@@ -314,8 +299,6 @@ void emma2rh_irq_dispatch(void)
 void __init arch_init_irq(void)
 {
 	u32 reg;
-
-	db_run(printk("markeins_irq_setup invoked.\n"));
 
 	/* by default, interrupts are disabled. */
 	emma2rh_out32(EMMA2RH_BHIF_INT_EN_0, 0);
