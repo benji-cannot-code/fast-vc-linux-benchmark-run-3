3abecc929a013b772cf038f04de7b928e3b3bb29
Nick Kossifidis <mick@madwifi.org>
2008-10-26 20:40:25 +0200

    ath5k: Update interrupt masking code
    *Properly get/set all available ISR/IMR values and review common/uncommon bits
    *Better handling of per-txq interrupts (we can now resolve what q is generating
     each interrupt -this will help in debuging wme later)
    *Some minor updates from legacy-hal
    *Properly handle RXNOFRM and TXNOFRM interrupt masking (even when we don't set
     them on IMR they keep showing up, so we disable them by zeroing AR5K_RXNOFRM
     and AR5K_TXNOFRM registers). This doesn't exist on legacy-hal but i've tested
     it on various cards and it works fine.
    
    Changes-Licensed-under: ISC
    Signed-Off-by: Nick Kossifidis <mickflemm@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath5k/reg.h
+++ drivers/net/wireless/ath5k/reg.h
@@ -235,6 +235,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AR5K_TXNOFRM		0x004c
 #define	AR5K_TXNOFRM_M		0x000003ff
 #define	AR5K_TXNOFRM_QCU	0x000ffc00
+#define	AR5K_TXNOFRM_QCU_S	10
 
 /*
  * Receive frame gap timeout register
@@ -351,7 +352,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define AR5K_SISR3		0x0090			/* Register Address [5211+] */
 #define AR5K_SISR3_QCBRORN	0x000003ff	/* Mask for QCBRORN */
-#define AR5K_SISR3_QCBORN_S	0
+#define AR5K_SISR3_QCBRORN_S	0
 #define AR5K_SISR3_QCBRURN	0x03ff0000	/* Mask for QCBRURN */
 #define AR5K_SISR3_QCBRURN_S	16
 
