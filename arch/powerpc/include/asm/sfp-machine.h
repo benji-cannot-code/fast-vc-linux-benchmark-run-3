0deda60396d155a4dd1c4616762d36aaa239d3be
Liu Yu <yu.liu@freescale.com>
2008-10-28 11:50:18 +0800

    powerpc/math-emu: Fix single float point division bug
    PowerPC float point division emulation is derived from gcc.
    I reported this problem on gcc maillist and got this reply:
    http://gcc.gnu.org/ml/gcc/2008-03/msg00543.html
    
    Since UDIV_NEEDS_NORMALIZATION is not used by kernel, we should use
    _FP_DIV_MEAT_1_udiv_norm to make sure the single float point
    is normalized before udiv_qrnnd.
    
    Signed-off-by: Liu Yu <yu.liu@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/include/asm/sfp-machine.h
+++ arch/powerpc/include/asm/sfp-machine.h
@@ -83,7 +83,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _FP_MUL_MEAT_S(R,X,Y)   _FP_MUL_MEAT_1_wide(_FP_WFRACBITS_S,R,X,Y,umul_ppmm)
 #define _FP_MUL_MEAT_D(R,X,Y)   _FP_MUL_MEAT_2_wide(_FP_WFRACBITS_D,R,X,Y,umul_ppmm)
 
-#define _FP_DIV_MEAT_S(R,X,Y)	_FP_DIV_MEAT_1_udiv(S,R,X,Y)
+#define _FP_DIV_MEAT_S(R,X,Y)	_FP_DIV_MEAT_1_udiv_norm(S,R,X,Y)
 #define _FP_DIV_MEAT_D(R,X,Y)	_FP_DIV_MEAT_2_udiv(D,R,X,Y)
 
 /* These macros define what NaN looks like. They're supposed to expand to
