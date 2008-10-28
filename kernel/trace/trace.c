4370ffa9c846e5c4ebc80efa49a03dd2673e0119
Steven Rostedt <rostedt@goodmis.org>
2008-10-28 10:44:24 -0400

    ftrace: fix current_tracer error return
    The commit (in linux-tip) c2931e05ec5965597cbfb79ad332d4a29aeceb23
     ( ftrace: return an error when setting a nonexistent tracer )
    added useful code that would error when a bad tracer was written into
    the current_tracer file.
    
    But this had a bug if the amount written was more than the amount read by
    that code. The first iteration would set the tracer correctly, but since
    it did not consume the rest of what was written (usually whitespace), the
    userspace utility would continue to write what was not consumed. This
    second iteration would fail to find a tracer and return -EINVAL. Funny
    thing is that the tracer would have already been set.
    
    This patch just consumes all the data that is written to the file.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -2378,9 +2378,10 @@ tracing_set_trace_write(struct file *filp, const char __user *ubuf,
 	int i;
 	size_t ret;
 
+	ret = cnt;
+
 	if (cnt > max_tracer_type_len)
 		cnt = max_tracer_type_len;
-	ret = cnt;
 
 	if (copy_from_user(&buf, ubuf, cnt))
 		return -EFAULT;
@@ -2413,8 +2414,8 @@ tracing_set_trace_write(struct file *filp, const char __user *ubuf,
  out:
 	mutex_unlock(&trace_types_lock);
 
-	if (ret == cnt)
-		filp->f_pos += cnt;
+	if (ret > 0)
+		filp->f_pos += ret;
 
 	return ret;
 }
