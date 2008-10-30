b4c44dc10354ef2137e57b1f87448326336a0794
Pekka Enberg <penberg@cs.helsinki.fi>
2008-10-30 19:04:53 +0200

    Staging: w35und: make functions local to wb35rx.c static
    While there are no functional changes, the diff is quite large because we need
    to shuffle code around to avoid forward declarations.
    
    Acked-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/wb35rx_f.h
+++ drivers/staging/winbond/wb35rx_f.h
@@ -8,15 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 //====================================
 // Interface function declare
 //====================================
-void		Wb35Rx_reset_descriptor(  phw_data_t pHwData );
 unsigned char		Wb35Rx_initial(  phw_data_t pHwData );
 void		Wb35Rx_destroy(  phw_data_t pHwData );
 void		Wb35Rx_stop(  phw_data_t pHwData );
-u16		Wb35Rx_indicate(struct ieee80211_hw *hw);
-void		Wb35Rx_adjust(  PDESCRIPTOR pRxDes );
 void		Wb35Rx_start(struct ieee80211_hw *hw);
-
-void		Wb35Rx(struct ieee80211_hw *hw);
-void		Wb35Rx_Complete(struct urb *urb);
 
 #endif
