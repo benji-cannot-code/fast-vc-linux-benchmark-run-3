b62b7c680f1985e2beef9d320e5a74fc46669a7e
Pekka Enberg <penberg@cs.helsinki.fi>
2008-10-30 13:04:58 +0200

    Staging: w35und: remove some dead code
    This patch removes the WBLinux_ReceivePacket(), WBLINUX_GetNextPacket(),
    WBLINUX_GetNextPacketCompleted, and sme_get_rssi() functions, which execute
    BUG() unconditionally, and related dead code.
    
    Acked-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/wblinux_f.h
+++ drivers/staging/winbond/wblinux_f.h
@@ -10,11 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 //
 // wblinux_f.h
 //
-void WBLinux_ReceivePacket(  struct wb35_adapter *adapter,  PRXLAYER1 pRxLayer1 );
 unsigned char WBLINUX_Initial(  struct wb35_adapter *adapter );
 int wb35_start_xmit(struct sk_buff *skb, struct net_device *netdev );
-void WBLINUX_GetNextPacket(  struct wb35_adapter *adapter,  PDESCRIPTOR pDes );
-void WBLINUX_GetNextPacketCompleted(  struct wb35_adapter *adapter,  PDESCRIPTOR pDes );
 void WBLINUX_stop(  struct wb35_adapter *adapter );
 void WBLINUX_Destroy(  struct wb35_adapter *adapter );
 void wb35_set_multicast( struct net_device *netdev );
