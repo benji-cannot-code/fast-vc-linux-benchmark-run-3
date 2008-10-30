a9f2be0991bbfe4f7c5047fbbfb817af50d6d530
Pekka Enberg <penberg@cs.helsinki.fi>
2008-10-30 19:04:56 +0200

    Staging: w35und: remove rxisr.c as dead code
    The vRxTimerStart() function is never called nor does the timer do anything
    useful so remove the code.
    
    Acked-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/sysdef.h
+++ drivers/staging/winbond/sysdef.h
@@ -38,7 +38,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PE_USB_INI_DUMP_
 #endif
 
-// Kernel Timer resolution, NDIS is 10ms, 10000us
-#define MIN_TIMEOUT_VAL	(10) //ms
-
 #endif
