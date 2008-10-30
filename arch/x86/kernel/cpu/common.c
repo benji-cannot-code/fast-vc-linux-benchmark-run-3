b4f1616620f437f15f86c419875ac5825a311aa2
James Bottomley <James.Bottomley@HansenPartnership.com>
2008-10-30 16:00:59 -0500

    x86/voyager: fix compile breakage caused by dc1e35c6e95e8923cf1d3510438b63c600fee1e2
    Impact: build fix on x86/Voyager
    
    Given commits like this:
    
    | Author: Suresh Siddha <suresh.b.siddha@intel.com>
    | Date:   Tue Jul 29 10:29:19 2008 -0700
    |
    |     x86, xsave: enable xsave/xrstor on cpus with xsave support
    
    Which deliberately expose boot cpu dependence to pieces of the system,
    I think it's time to explicitly have a variable for it to prevent this
    continual misassumption that the boot CPU is zero.
    
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1135,7 +1135,7 @@ void __cpuinit cpu_init(void)
 	/*
 	 * Boot processor to setup the FP and extended state context info.
 	 */
-	if (!smp_processor_id())
+	if (smp_processor_id() == boot_cpu_id)
 		init_thread_xstate();
 
 	xsave_init();
