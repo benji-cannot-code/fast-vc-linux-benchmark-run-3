e713eacd0622454571a4d533778c8c1f9c1fd6ff
Ben Dooks <ben-linux@fluff.org>
2008-10-30 10:14:36 +0000

    [ARM] S3C: Move regs-ac97.h to arch/arm/plat-s3c/include/plat.
    Move regs-ac97.h to arch/arm/plat-s3c/include/plat ready
    to clean out old include directories.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c2412/dma.c
+++ arch/arm/mach-s3c2412/dma.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat/regs-serial.h>
 #include <mach/regs-gpio.h>
-#include <asm/plat-s3c/regs-ac97.h>
+#include <plat/regs-ac97.h>
 #include <mach/regs-mem.h>
 #include <mach/regs-lcd.h>
 #include <mach/regs-sdi.h>
