725b0a56b7aabeb21ac2ed62afc1cb3b2c24a7ae
David S. Miller <davem@davemloft.net>
2008-10-29 21:25:00 -0700

    sparc64: Run the kernel always in the TSO memory model.
    The fact of the matter is, all UltraSPARC-III and later chips only
    implement TSO.  They don't implement PSO and RMO memory models at all.
    
    Only the Ultra-I and Ultra-II family chips implement RMO and they are
    only helped marginally by using this setting when executing kernel
    code.
    
    The big plus to doing this is that we can eliminate all of the non-Sync
    memory barriers in the kernel except for the ones used in the optimized
    memcpy/memset code (these use block load and store operations which
    have their own memory ordering rules).
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/spinlock_64.h
+++ arch/sparc/include/asm/spinlock_64.h
@@ -14,17 +14,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * and rebuild your kernel.
  */
 
-/* All of these locking primitives are expected to work properly
- * even in an RMO memory model, which currently is what the kernel
- * runs in.
- *
- * There is another issue.  Because we play games to save cycles
- * in the non-contention case, we need to be extra careful about
- * branch targets into the "spinning" code.  They live in their
- * own section, but the newer V9 branches have a shorter range
- * than the traditional 32-bit sparc branch variants.  The rule
- * is that the branches that go into and out of the spinner sections
- * must be pre-V9 branches.
+/* Because we play games to save cycles in the non-contention case, we
+ * need to be extra careful about branch targets into the "spinning"
+ * code.  They live in their own section, but the newer V9 branches
+ * have a shorter range than the traditional 32-bit sparc branch
+ * variants.  The rule is that the branches that go into and out of
+ * the spinner sections must be pre-V9 branches.
  */
 
 #define __raw_spin_is_locked(lp)	((lp)->lock != 0)
