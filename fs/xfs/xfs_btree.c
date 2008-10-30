bc79b4181b0e977b9cb947030fcdb6d92e6a7561
Lachlan McIlroy <lachlan@sgi.com>
2008-10-30 17:05:26 +1100

    [XFS] Make xfs_btree_check_ptr() debug-only code.
    SGI-PV: 985583
    
    SGI-Modid: xfs-linux-melb:xfs-kern:32224a
    
    Signed-off-by: Lachlan McIlroy <lachlan@sgi.com>
    Signed-off-by: Christoph Hellwig <hch@infradead.org>

--- fs/xfs/xfs_btree.c
+++ fs/xfs/xfs_btree.c
@@ -161,6 +161,7 @@ xfs_btree_check_lptr(
 	return 0;
 }
 
+#ifdef DEBUG
 /*
  * Check that (short) pointer is ok.
  */
@@ -198,6 +199,7 @@ xfs_btree_check_ptr(
 				be32_to_cpu((&ptr->s)[index]), level);
 	}
 }
+#endif
 
 /*
  * Delete the btree cursor.
