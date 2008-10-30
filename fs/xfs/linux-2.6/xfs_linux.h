f7df576e645f1ccc9244caeb9fd7147cc2cef783
David Chinner <david@fromorbit.com>
2008-10-30 18:32:43 +1100

    [XFS] Fix race when looking up reclaimable inodes
    If we get a race looking up a reclaimable inode, we can end up with the
    winner proceeding to use the inode before it has been completely
    re-initialised. This is a Bad Thing.
    
    Fix the race by checking whether we are still initialising the inod eonce
    we have a reference to it, and if so wait for the initialisation to
    complete before continuing.
    
    While there, fix a leaked reference count in the same code when
    encountering an unlinked inode and we are not doing a lookup for a create
    operation.
    
    SGI-PV: 987246
    
    SGI-Modid: xfs-linux-melb:xfs-kern:32429a
    
    Signed-off-by: David Chinner <david@fromorbit.com>
    Signed-off-by: Lachlan McIlroy <lachlan@sgi.com>

--- fs/xfs/linux-2.6/xfs_linux.h
+++ fs/xfs/linux-2.6/xfs_linux.h
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/random.h>
 #include <linux/ctype.h>
+#include <linux/writeback.h>
 
 #include <asm/page.h>
 #include <asm/div64.h>
