e1ad6c36a72c08766f8f3b4bcb33b60569f4368f
Frederic CAND <frederic.cand@anevia.com>
2008-10-30 04:50:05 -0300

    V4L/DVB (9496): cx88-blackbird: bugfix: cx88-blackbird-mpeg-users
    Allows multiple access to the mpeg device
    
    Signed-off-by: Frederic CAND <frederic.cand@anevia.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx88/cx88-blackbird.c
+++ drivers/media/video/cx88/cx88-blackbird.c
@@ -1079,7 +1079,7 @@ static int mpeg_open(struct inode *inode, struct file *file)
 		}
 	}
 
-	if (blackbird_initialize_codec(dev) < 0) {
+	if (!atomic_read(&dev->core->mpeg_users) && blackbird_initialize_codec(dev) < 0) {
 		if (drv)
 			drv->request_release(drv);
 		unlock_kernel();
@@ -1110,6 +1110,8 @@ static int mpeg_open(struct inode *inode, struct file *file)
 			fh->mpegq.field);
 	unlock_kernel();
 
+	atomic_inc(&dev->core->mpeg_users);
+
 	return 0;
 }
 
@@ -1119,7 +1121,7 @@ static int mpeg_release(struct inode *inode, struct file *file)
 	struct cx8802_dev *dev = fh->dev;
 	struct cx8802_driver *drv = NULL;
 
-	if (dev->mpeg_active)
+	if (dev->mpeg_active && atomic_read(&dev->core->mpeg_users) == 1)
 		blackbird_stop_codec(dev);
 
 	cx8802_cancel_buffers(fh->dev);
@@ -1138,6 +1140,8 @@ static int mpeg_release(struct inode *inode, struct file *file)
 	drv = cx8802_get_driver(dev, CX88_MPEG_BLACKBIRD);
 	if (drv)
 		drv->request_release(drv);
+
+	atomic_dec(&dev->core->mpeg_users);
 
 	return 0;
 }
