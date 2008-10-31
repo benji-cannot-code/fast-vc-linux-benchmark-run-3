aa0729b01394120c6e978af84ff322529530cec6
Huang Weiyi <weiyi.huang@gmail.com>
2008-10-31 22:49:58 +0800

    wusb: remove unused #include <version.h>
    The file(s) below do not use LINUX_VERSION_CODE nor KERNEL_VERSION.
      drivers/usb/host/hwa-hc.c
      drivers/usb/host/whci/hcd.c
      drivers/usb/host/whci/int.c
      drivers/usb/host/whci/wusb.c
      drivers/usb/wusbcore/cbaf.c
    
    This patch removes the said #include <version.h>.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: David Vrabel <david.vrabel@csr.com>

--- drivers/usb/host/whci/hcd.c
+++ drivers/usb/host/whci/hcd.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/uwb/umc.h>
