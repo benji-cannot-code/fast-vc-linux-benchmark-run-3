ccaf757b706bf2b9287e6d5e4b98c16b4d9272a2
Kevin Hilman <khilman@deeprootsystems.com>
2008-10-31 11:08:42 -0700

    OMAP3: PM: Add wake-up bit defintiions for CONTROL_PADCONF_X
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/plat-omap/include/mach/control.h
+++ arch/arm/plat-omap/include/mach/control.h
@@ -195,6 +195,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP3_IVA2_BOOTMOD_MASK		(0xf << 0)
 #define OMAP3_IVA2_BOOTMOD_IDLE		(0x1 << 0)
 
+/* CONTROL_PADCONF_X bits */
+#define OMAP3_PADCONF_WAKEUPEVENT0	(1 << 15)
+#define OMAP3_PADCONF_WAKEUPENABLE0	(1 << 14)
+
 #ifndef __ASSEMBLY__
 #if defined(CONFIG_ARCH_OMAP2) || defined(CONFIG_ARCH_OMAP3)
 extern void __iomem *omap_ctrl_base_get(void);
