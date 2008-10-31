be20aa23d290c54fc5a020c0049923245751736b
Steven Rostedt <rostedt@goodmis.org>
2008-10-31 09:36:38 -0400

    ftrace: fix hardirq header for non ftrace archs
    Impact: build fix for non-ftrace architectures
    
    Not all archs implement ftrace, and therefore do not have an asm/ftrace.h.
    This patch corrects the problem.
    
    The ftrace_nmi_enter/exit now must be defined for all archs that implement
    dynamic ftrace. Currently, only x86 does.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/sh/include/asm/ftrace.h
+++ arch/sh/include/asm/ftrace.h
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_SH_FTRACE_H
 
 #ifndef __ASSEMBLY__
-static inline void ftrace_nmi_enter(void) { }
-static inline void ftrace_nmi_exit(void) { }
-#endif
-
-#ifndef __ASSEMBLY__
 extern void mcount(void);
 #endif
 
