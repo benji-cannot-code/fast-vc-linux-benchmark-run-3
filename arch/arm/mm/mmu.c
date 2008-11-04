522103512035f199722710dd569aaec100db0f07
Nicolas Pitre <nico@cam.org>
2008-11-04 00:48:42 -0500

    [ARM] ignore high memory with VIPT aliasing caches
    VIPT aliasing caches have issues of their own which are not yet handled.
    Usage of discard_old_kernel_data() in copypage-v6.c is not highmem ready,
    kmap/fixmap stuff doesn't take account of cache colouring, etc.
    If/when those issues are handled then this could be reverted.
    
    Signed-off-by: Nicolas Pitre <nico@marvell.com>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cputype.h>
 #include <asm/mach-types.h>
 #include <asm/sections.h>
+#include <asm/cachetype.h>
 #include <asm/setup.h>
 #include <asm/sizes.h>
 #include <asm/tlb.h>
@@ -678,6 +679,10 @@ static void __init sanity_check_meminfo(void)
 		    bank->size > VMALLOC_MIN - __va(bank->start)) {
 			if (meminfo.nr_banks >= NR_BANKS) {
 				printk(KERN_CRIT "NR_BANKS too low, "
+						 "ignoring high memory\n");
+			} else if (cache_is_vipt_aliasing()) {
+				printk(KERN_CRIT "HIGHMEM is not yet supported "
+						 "with VIPT aliasing cache, "
 						 "ignoring high memory\n");
 			} else {
 				memmove(bank + 1, bank,
