8248ac670e7e341e96edd104a67c6db0e404e248
Tony Lindgren <tony@atomide.com>
2008-11-04 13:35:08 -0800

    ARM: OMAP: Fix define for twl4030 irqs
    Otherwise twl4030 gpios won't work.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/mach/irqs.h
+++ arch/arm/plat-omap/include/mach/irqs.h
@@ -373,7 +373,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* External TWL4030 gpio interrupts are optional */
 #define TWL4030_GPIO_IRQ_BASE	TWL4030_PWR_IRQ_END
-#ifdef	CONFIG_TWL4030_GPIO
+#ifdef	CONFIG_GPIO_TWL4030
 #define TWL4030_GPIO_NR_IRQS	18
 #else
 #define	TWL4030_GPIO_NR_IRQS	0
