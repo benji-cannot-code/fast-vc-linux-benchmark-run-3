7eca9d40254b9935421d39c90509b93f73631132
Jianjun Kong <jianjun@zeuux.org>
2008-11-04 21:47:07 +0800

    drivers: remove duplicated #include
    Signed-off-by: Jianjun Kong <jianjun@zeuux.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/atl1e/atl1e.h
+++ drivers/net/atl1e/atl1e.h
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 #include <linux/pagemap.h>
 #include <linux/tcp.h>
-#include <linux/mii.h>
 #include <linux/ethtool.h>
 #include <linux/if_vlan.h>
 #include <linux/workqueue.h>
