e52ba87947c7b9312900abdc7a4c8787609411ff
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-11-04 21:24:00 +0000

    [ARM] omap: don't use clkops_omap2_dflt_wait for non-ICLK/FCLK clocks
    The original code in omap2_clk_wait_ready() used to check the low 8
    bits to determine whether they were within the FCLKEN or ICLKEN
    registers.  Specifically, the test is satisfied when these offsets
    are used:
    
     CM_FCLKEN, CM_FCLKEN1, CM_CLKEN, OMAP24XX_CM_FCLKEN2, CM_ICLKEN,
     CM_ICLKEN1, CM_ICLKEN2, CM_ICLKEN3, OMAP24XX_CM_ICLKEN4
     OMAP3430_CM_CLKEN_PLL, OMAP3430ES2_CM_CLKEN2
    
    If one of these offsets isn't used, omap2_clk_wait_ready() merely
    returns without doing anything.  So we should use the non-wait clkops
    version instead and eliminate that conditional.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-omap2/clock24xx.h
+++ arch/arm/mach-omap2/clock24xx.h
@@ -891,7 +891,7 @@ static const struct clksel common_clkout_src_clksel[] = {
 
 static struct clk sys_clkout_src = {
 	.name		= "sys_clkout_src",
-	.ops		= &clkops_omap2_dflt_wait,
+	.ops		= &clkops_omap2_dflt,
 	.parent		= &func_54m_ck,
 	.flags		= CLOCK_IN_OMAP242X | CLOCK_IN_OMAP243X |
 				RATE_PROPAGATES,
@@ -938,7 +938,7 @@ static struct clk sys_clkout = {
 /* In 2430, new in 2420 ES2 */
 static struct clk sys_clkout2_src = {
 	.name		= "sys_clkout2_src",
-	.ops		= &clkops_omap2_dflt_wait,
+	.ops		= &clkops_omap2_dflt,
 	.parent		= &func_54m_ck,
 	.flags		= CLOCK_IN_OMAP242X | RATE_PROPAGATES,
 	.clkdm_name	= "wkup_clkdm",
@@ -975,7 +975,7 @@ static struct clk sys_clkout2 = {
 
 static struct clk emul_ck = {
 	.name		= "emul_ck",
-	.ops		= &clkops_omap2_dflt_wait,
+	.ops		= &clkops_omap2_dflt,
 	.parent		= &func_54m_ck,
 	.flags		= CLOCK_IN_OMAP242X,
 	.clkdm_name	= "wkup_clkdm",
