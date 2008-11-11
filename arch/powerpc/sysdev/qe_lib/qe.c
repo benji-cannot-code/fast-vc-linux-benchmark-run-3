ff4f3ea3661dad88e8c5219b73facb600889ce9d
Anton Vorontsov <avorontsov@ru.mvista.com>
2008-11-11 18:31:39 +0300

    powerpc/qe: Move cmxgcr_lock definition from the ucc.c into the qe.c
    With this patch we can compile the qe_lib/usb.c without the UCC
    support (that is, without UCC_GETH and/or SERIAL_QE).
    
    Fixes following link error (CONFIG_SMP should be =y to trigger this):
    
    arch/powerpc/sysdev/built-in.o: In function `qe_usb_clock_set':
    (.text+0x3cae): undefined reference to `cmxgcr_lock'
    make: *** [.tmp_vmlinux1] Error 1
    
    While at it, also add missing spinlock.h includes.
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Acked-By: Timur Tabi <timur@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/sysdev/qe_lib/qe.c
+++ arch/powerpc/sysdev/qe_lib/qe.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/param.h>
 #include <linux/string.h>
+#include <linux/spinlock.h>
 #include <linux/mm.h>
 #include <linux/interrupt.h>
 #include <linux/bootmem.h>
@@ -39,6 +40,8 @@ static void qe_snums_init(void);
 static int qe_sdma_init(void);
 
 static DEFINE_SPINLOCK(qe_lock);
+DEFINE_SPINLOCK(cmxgcr_lock);
+EXPORT_SYMBOL(cmxgcr_lock);
 
 /* QE snum state */
 enum qe_snum_state {
