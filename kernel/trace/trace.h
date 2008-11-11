2172e62c96c226d519ae50c7be75405e5d6b1c16
Frederic Weisbecker <fweisbec@gmail.com>
2008-11-11 23:21:31 +0100

    tracing/fastboot: move boot tracer structs and funcs into their own header.
    Impact: Cleanups on the boot tracer and ftrace
    
    This patch bring some cleanups about the boot tracer headers. The
    functions and structures of this tracer have nothing related to ftrace
    and should have so their own header file.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.h
+++ kernel/trace/trace.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ring_buffer.h>
 #include <linux/mmiotrace.h>
 #include <linux/ftrace.h>
+#include <trace/boot.h>
 
 enum trace_type {
 	__TRACE_FIRST_TYPE = 0,
