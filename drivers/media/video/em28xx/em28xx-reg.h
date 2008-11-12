c9fb889be774d722a1d03e93a6da8cc85982dc32
Devin Heitmueller <devin.heitmueller@gmail.com>
2008-11-12 02:05:24 -0300

    V4L/DVB (9589): Properly support capture start on em2874
    Properly support capture start on em2874
    
    The transport stream enable register moved in the em2874, so make it work
    properly.
    
    Thanks for Ray Lu from Empia for providing the em2874 datasheet.
    
    Signed-off-by: Devin Heitmueller <devin.heitmueller@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-reg.h
+++ drivers/media/video/em28xx/em28xx-reg.h
@@ -78,7 +78,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM28XX_R14_VIDEO_AC97	0x14
 
 /* em2874 registers */
+#define EM2874_R5F_TS_ENABLE    0x5f
 #define EM2874_R80_GPIO         0x80
+
+/* em2874 Transport Stream Enable Register (0x5f) */
+#define EM2874_TS1_CAPTURE_ENABLE (1 << 0)
+#define EM2874_TS1_FILTER_ENABLE  (1 << 1)
+#define EM2874_TS1_NULL_DISCARD   (1 << 2)
+#define EM2874_TS2_CAPTURE_ENABLE (1 << 4)
+#define EM2874_TS2_FILTER_ENABLE  (1 << 5)
+#define EM2874_TS2_NULL_DISCARD   (1 << 6)
 
 /* register settings */
 #define EM2800_AUDIO_SRC_TUNER  0x0d
