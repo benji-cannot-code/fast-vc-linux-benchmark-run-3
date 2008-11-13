fab9aa7a47e9673f8729ec03216a0b672c7c2759
Paul Mundt <lethal@linux-sh.org>
2008-11-13 15:38:02 +0900

    sh: Don't factor in PAGE_OFFSET for valid_phys_addr_range() check.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/mmap.c
+++ arch/sh/mm/mmap.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 int valid_phys_addr_range(unsigned long addr, size_t count)
 {
-	if (addr < (PAGE_OFFSET + (PFN_START << PAGE_SHIFT)))
+	if (addr < __MEMORY_START)
 		return 0;
 	if (addr + count > __pa(high_memory))
 		return 0;
