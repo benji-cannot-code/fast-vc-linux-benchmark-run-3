0cb841f4870e018aa0b00863b9ce9b33dd2a7049
Bob Moore <robert.moore@intel.com>
2008-11-13 11:19:24 +0800

    ACPICA: Consolidate method arg count validation code
    Merge the code that validates control method argument counts into
    the predefined validation module. Eliminates possible multiple
    warnings for incorrect counts.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acnamesp.h
+++ include/acpi/acnamesp.h
@@ -183,6 +183,8 @@ acpi_status acpi_ns_evaluate(struct acpi_evaluate_info *info);
  */
 acpi_status
 acpi_ns_check_predefined_names(struct acpi_namespace_node *node,
+			       u32 user_param_count,
+			       acpi_status return_status,
 			       union acpi_operand_object **return_object);
 
 const union acpi_predefined_info *acpi_ns_check_for_predefined_name(struct
@@ -192,6 +194,7 @@ const union acpi_predefined_info *acpi_ns_check_for_predefined_name(struct
 void
 acpi_ns_check_parameter_count(char *pathname,
 			      struct acpi_namespace_node *node,
+			      u32 user_param_count,
 			      const union acpi_predefined_info *info);
 
 /*
