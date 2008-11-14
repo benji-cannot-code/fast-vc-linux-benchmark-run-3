c512440d09fc7b9fb237f661aa73dc8cb2fb69d9
Greg Kroah-Hartman <gregkh@suse.de>
2008-11-13 16:36:22 -0800

    Staging: comedi: fix up a lot of checkpatch.pl warnings
    Only clean up some of the easier ones in the .h files
    
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Cc: David Schleef <ds@schleef.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/comedi_compat32.h
+++ drivers/staging/comedi/comedi_compat32.h
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern long comedi_compat_ioctl(struct file *file, unsigned int cmd,
 		unsigned long arg);
-#define comedi_register_ioctl32() do{}while(0)
-#define comedi_unregister_ioctl32() do{}while(0)
+#define comedi_register_ioctl32() do {} while (0)
+#define comedi_unregister_ioctl32() do {} while (0)
 
 #else /* HAVE_COMPAT_IOCTL */
 
@@ -51,8 +51,8 @@ extern void comedi_unregister_ioctl32(void);
 #else /* CONFIG_COMPAT */
 
 #define comedi_compat_ioctl 0	/* NULL */
-#define comedi_register_ioctl32() do{}while(0)
-#define comedi_unregister_ioctl32() do{}while(0)
+#define comedi_register_ioctl32() do {} while (0)
+#define comedi_unregister_ioctl32() do {} while (0)
 
 #endif /* CONFIG_COMPAT */
 
--- drivers/staging/comedi/wrapper.h
+++ drivers/staging/comedi/wrapper.h
@@ -20,13 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __COMPAT_LINUX_WRAPPER_H_
 #define __COMPAT_LINUX_WRAPPER_H_
 
-#include <linux/version.h>
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,5,0)
 #define mem_map_reserve(p)      set_bit(PG_reserved, &((p)->flags))
 #define mem_map_unreserve(p)    clear_bit(PG_reserved, &((p)->flags))
-#else
-#include_next <linux/wrapper.h>
-#endif
 
 #endif /* __COMPAT_LINUX_WRAPPER_H_ */
