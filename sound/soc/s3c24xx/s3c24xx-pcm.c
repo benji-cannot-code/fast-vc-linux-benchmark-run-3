49d903035fb365ed049bf9c0f172dbc87a8ea57f
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-11-14 14:40:46 +0000

    ASoC: Revert "ASoC: Add new parameter to s3c24xx_pcm_enqueue"
    This reverts commit 8dc840f88d9c9f75f46d5dbe489242f8a114fab6.  Christian
    Pellegrin <chripell@gmail.com> reported that on some systems the patch
    caused DMA to fail which is much more serious than the original skipped
    audio issue.  Further investigation by Dave shows that the behaviour
    depends on the clock speed of the SoC - a better fix is neeeded.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/s3c24xx-pcm.c
+++ sound/soc/s3c24xx/s3c24xx-pcm.c
@@ -79,8 +79,7 @@ struct s3c24xx_runtime_data {
  * place a dma buffer onto the queue for the dma system
  * to handle.
 */
-static void s3c24xx_pcm_enqueue(struct snd_pcm_substream *substream,
-				int dma_max)
+static void s3c24xx_pcm_enqueue(struct snd_pcm_substream *substream)
 {
 	struct s3c24xx_runtime_data *prtd = substream->runtime->private_data;
 	dma_addr_t pos = prtd->dma_pos;
@@ -88,10 +87,7 @@ static void s3c24xx_pcm_enqueue(struct snd_pcm_substream *substream,
 
 	DBG("Entered %s\n", __func__);
 
-	if (!dma_max)
-		dma_max = prtd->dma_limit;
-
-	while (prtd->dma_loaded < dma_max) {
+	while (prtd->dma_loaded < prtd->dma_limit) {
 		unsigned long len = prtd->dma_period;
 
 		DBG("dma_loaded: %d\n", prtd->dma_loaded);
@@ -137,7 +133,7 @@ static void s3c24xx_audio_buffdone(struct s3c2410_dma_chan *channel,
 	spin_lock(&prtd->lock);
 	if (prtd->state & ST_RUNNING) {
 		prtd->dma_loaded--;
-		s3c24xx_pcm_enqueue(substream, 0);
+		s3c24xx_pcm_enqueue(substream);
 	}
 
 	spin_unlock(&prtd->lock);
@@ -254,7 +250,7 @@ static int s3c24xx_pcm_prepare(struct snd_pcm_substream *substream)
 	prtd->dma_pos = prtd->dma_start;
 
 	/* enqueue dma buffers */
-	s3c24xx_pcm_enqueue(substream, 1);
+	s3c24xx_pcm_enqueue(substream);
 
 	return ret;
 }
