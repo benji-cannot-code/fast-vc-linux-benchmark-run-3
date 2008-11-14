f172a49f433c80f449b9f353e75e8bf8860e0139
David Schleef <ds@schleef.org>
2008-11-14 14:20:56 -0800

    Staging: comedi: add local copy of interrupt.h
    This is needed for some internal comedi driver macros that were
    built up over the years to handle multiple kernel versions.  It will be
    removed eventually.
    
    From: David Schleef <ds@schleef.org>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/comedidev.h
+++ drivers/staging/comedi/comedidev.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/vmalloc.h>
 #include <linux/mm.h>
-#include <linux/interrupt.h>
+#include "interrupt.h"
 #include <linux/dma-mapping.h>
 #include <linux/uaccess.h>
 #include <linux/io.h>
