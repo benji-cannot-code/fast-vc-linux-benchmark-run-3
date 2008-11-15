57a5aeb3c2fe48a90e823971153b0de88fedfbf9
Christian Pellegrin <chripell@gmail.com>
2008-11-15 08:58:16 +0100

    ASoC: UDA134x codec driver
    Signed-off-by: Christian Pellegrin <chripell@fsfe.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

---
+++ include/sound/uda134x.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * uda134x.h  --  UDA134x ALSA SoC Codec driver
+ *
+ * Copyright 2007 Dension Audio Systems Ltd.
+ * Author: Zoltan Devai
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _UDA134X_H
+#define _UDA134X_H
+
+#include <sound/l3.h>
+
+struct uda134x_platform_data {
+	struct l3_pins l3;
+	void (*power) (int);
+	int model;
+#define UDA134X_UDA1340 1
+#define UDA134X_UDA1341 2
+#define UDA134X_UDA1344 3
+};
+
+#endif /* _UDA134X_H */
