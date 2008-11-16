ba1cb85b97caee8ba207408dff61c25f939d4bd0
Mauro Carvalho Chehab <mchehab@redhat.com>
2008-11-16 10:40:21 -0300

    V4L/DVB (9641): Add chip ID's for em2820 and em2840
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-reg.h
+++ drivers/media/video/em28xx/em28xx-reg.h
@@ -112,6 +112,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* FIXME: Need to be populated with the other chip ID's */
 enum em28xx_chip_id {
+	CHIP_ID_EM2820 = 18,
+	CHIP_ID_EM2840 = 20,
 	CHIP_ID_EM2860 = 34,
 	CHIP_ID_EM2883 = 36,
 	CHIP_ID_EM2874 = 65,
