6c921c6823cd4151a7e0cecfbff0cebc3d88150c
Ingo Molnar <mingo@elte.hu>
2008-11-16 09:50:34 +0100

    markers/tracpoints: fix non-modular build
    fix:
    
     kernel/marker.c: In function 'marker_module_notify':
     kernel/marker.c:905: error: 'MODULE_STATE_COMING' undeclared (first use in this function)
     [...]
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/tracepoint.c
+++ kernel/tracepoint.c
@@ -543,6 +543,8 @@ void tracepoint_iter_reset(struct tracepoint_iter *iter)
 }
 EXPORT_SYMBOL_GPL(tracepoint_iter_reset);
 
+#ifdef CONFIG_MODULES
+
 int tracepoint_module_notify(struct notifier_block *self,
 			     unsigned long val, void *data)
 {
@@ -571,3 +573,5 @@ static int init_tracepoints(void)
 	return register_module_notifier(&tracepoint_module_nb);
 }
 __initcall(init_tracepoints);
+
+#endif /* CONFIG_MODULES */
