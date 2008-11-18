6d22f1845128fb773861cd636b8a405ebd4b9a34
Paul Mundt <lethal@linux-sh.org>
2008-11-18 17:33:48 +0900

    sh: Migrate necessary libgcc bits in to arch/sh/lib for SUPERH32.
    This moves in the necessary libgcc bits for SUPERH32 and drops the
    libgcc linking for the regular targets. This in turn allows us to rip
    out quite a few hacks both in sh_ksyms_32 and arch/sh/Makefile.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

---
+++ arch/sh/lib/libgcc.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ASM_LIBGCC_H
+#define __ASM_LIBGCC_H
+
+#include <asm/byteorder.h>
+
+typedef int word_type __attribute__ ((mode (__word__)));
+
+#ifdef __BIG_ENDIAN
+struct DWstruct {
+	int high, low;
+};
+#elif defined(__LITTLE_ENDIAN)
+struct DWstruct {
+	int low, high;
+};
+#else
+#error I feel sick.
+#endif
+
+typedef union
+{
+	struct DWstruct s;
+	long long ll;
+} DWunion;
+
+#endif /* __ASM_LIBGCC_H */
