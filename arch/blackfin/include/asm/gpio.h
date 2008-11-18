10e5719ab6df926d17ca67e6f886f12ebe5c6d30
Bryan Wu <cooloney@kernel.org>
2008-11-18 17:48:22 +0800

    Blackfin arch: fix bug - gpio_bank() macros messed up bank number caculating with positioning a gpio
    The whole story:
    
    Before BF51x merged, all the MAX_BLACKFIN_GPIOS are integral multiple of GPIO_BANKSIZE (= 16).
    But BF51x provides MAX_BLACKFIN_GPIOS = 40 which includes 3 banks and the 3rd bank has only 8
    GPIO pins.
    
    Therefore, gpio_bank() macros is correct when you try to find a GPIO in which bank (GPIO_35 is
    in bank 2). But on BF51x gpio_bank(MAX_BLACKFIN_GPIOS) only gives out 2 banks instead of 3
    banks for some static array initialization.
    
    This patch add a new macros gpio_bank_n() and GPIO_BANK_NUM to do bank number caculating and
    remain the gpio_bank() macros for positioning a gpio in which bank.
    
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/include/asm/gpio.h
+++ arch/blackfin/include/asm/gpio.h
@@ -86,10 +86,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_BLACKFIN_GPIO_H__
 
 #define gpio_bank(x) ((x) >> 4)
+#define gpio_bank_n(x) ((x) & 0xF ? ((x) >> 4) + 1 : (x) >> 4)
 #define gpio_bit(x)  (1<<((x) & 0xF))
 #define gpio_sub_n(x) ((x) & 0xF)
 
 #define GPIO_BANKSIZE 16
+#define GPIO_BANK_NUM gpio_bank_n(MAX_BLACKFIN_GPIOS)
 
 #define	GPIO_0	0
 #define	GPIO_1	1
