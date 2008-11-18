fdcfb6f9223396f349d4fb7f045c4dbf0cea50ea
Li Zefan <lizf@cn.fujitsu.com>
2008-11-18 14:02:03 +0800

    cpuset: fix regression when failed to generate sched domains
    Impact: properly rebuild sched-domains on kmalloc() failure
    
    When cpuset failed to generate sched domains due to kmalloc()
    failure, the scheduler should fallback to the single partition
    'fallback_doms' and rebuild sched domains, but now it only
    destroys but not rebuilds sched domains.
    
    The regression was introduced by:
    
    | commit dfb512ec4834116124da61d6c1ee10fd0aa32bd6
    | Author: Max Krasnyansky <maxk@qualcomm.com>
    | Date:   Fri Aug 29 13:11:41 2008 -0700
    |
    |    sched: arch_reinit_sched_domains() must destroy domains to force rebuild
    
    After the above commit, partition_sched_domains(0, NULL, NULL) will
    only destroy sched domains and partition_sched_domains(1, NULL, NULL)
    will create the default sched domain.
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Max Krasnyansky <maxk@qualcomm.com>
    Cc: <stable@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -588,7 +588,6 @@ static int generate_sched_domains(cpumask_t **domains,
 	int ndoms;		/* number of sched domains in result */
 	int nslot;		/* next empty doms[] cpumask_t slot */
 
-	ndoms = 0;
 	doms = NULL;
 	dattr = NULL;
 	csa = NULL;
@@ -675,10 +674,8 @@ restart:
 	 * Convert <csn, csa> to <ndoms, doms> and populate cpu masks.
 	 */
 	doms = kmalloc(ndoms * sizeof(cpumask_t), GFP_KERNEL);
-	if (!doms) {
-		ndoms = 0;
+	if (!doms)
 		goto done;
-	}
 
 	/*
 	 * The rest of the code, including the scheduler, can deal with
@@ -732,6 +729,13 @@ restart:
 
 done:
 	kfree(csa);
+
+	/*
+	 * Fallback to the default domain if kmalloc() failed.
+	 * See comments in partition_sched_domains().
+	 */
+	if (doms == NULL)
+		ndoms = 1;
 
 	*domains    = doms;
 	*attributes = dattr;
