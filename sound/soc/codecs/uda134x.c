cf2022ca0267cab2dc3330525248d7d7361c71d1
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-11-18 12:25:46 +0000

    ASoC: Move uda134x_codec.h to uda134x.h
    For consistency with other ASoC codec drivers.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/uda134x.c
+++ sound/soc/codecs/uda134x.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/uda134x.h>
 #include <sound/l3.h>
 
-#include "uda134x_codec.h"
+#include "uda134x.h"
 
 
 #define POWER_OFF_ON_STANDBY 1
--- sound/soc/s3c24xx/s3c24xx_uda134x.c
+++ sound/soc/s3c24xx/s3c24xx_uda134x.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "s3c24xx-pcm.h"
 #include "s3c24xx-i2s.h"
-#include "../codecs/uda134x_codec.h"
+#include "../codecs/uda134x.h"
 
 
 /* #define ENFORCE_RATES 1 */
