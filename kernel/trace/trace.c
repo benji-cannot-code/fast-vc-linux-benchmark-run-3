77140510f5f5e565e84a0c2ff01b4cdbf39b7f42
Heiko Carstens <heiko.carstens@de.ibm.com>
2008-11-18 18:06:35 +0100

    ftrace: preemptoff selftest not working
    Impact: fix preemptoff and preemptirqsoff tracer self-tests
    
    I was wondering why the preemptoff and preemptirqsoff tracer selftests
    don't work on s390. After all its just that they get called from
    non-preemptible context:
    
    kernel_init() will execute all initcalls, however the first line in
    kernel_init() is lock_kernel(), which causes the preempt_count to be
    increased. Any later calls to add_preempt_count() (especially those
    from the selftests) will therefore not result in a call to
    trace_preempt_off() since the check below in add_preempt_count()
    will be false:
    
            if (preempt_count() == val)
                    trace_preempt_off(CALLER_ADDR0, get_parent_ip(CALLER_ADDR1));
    
    Hence the trace buffer will be empty.
    
    Fix this by releasing the BKL during the self-tests.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -533,6 +533,13 @@ int register_tracer(struct tracer *type)
 	}
 
 #ifdef CONFIG_FTRACE_STARTUP_TEST
+	/*
+	 * When this gets called we hold the BKL which means that preemption
+	 * is disabled. Various trace selftests however need to disable
+	 * and enable preemption for successful tests. So we drop the BKL here
+	 * and grab it after the tests again.
+	 */
+	unlock_kernel();
 	if (type->selftest) {
 		struct tracer *saved_tracer = current_trace;
 		struct trace_array *tr = &global_trace;
@@ -563,6 +570,7 @@ int register_tracer(struct tracer *type)
 		}
 		printk(KERN_CONT "PASSED\n");
 	}
+	lock_kernel();
 #endif
 
 	type->next = trace_types;
