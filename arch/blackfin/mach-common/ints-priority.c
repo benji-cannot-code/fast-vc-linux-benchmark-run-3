e72491458bb22bad47155dc7b4c54f9be209d88c
Mike Frysinger <vapier.adi@gmail.com>
2008-11-18 17:48:22 +0800

    Blackfin arch: move SIC_SYSIRQ() to the one file that actually uses it
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/mach-common/ints-priority.c
+++ arch/blackfin/mach-common/ints-priority.c
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/gpio.h>
 #include <asm/irq_handler.h>
 
+#define SIC_SYSIRQ(irq)	(irq - (IRQ_CORETMR + 1))
+
 #ifdef BF537_FAMILY
 # define BF537_GENERIC_ERROR_INT_DEMUX
 #else
