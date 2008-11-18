933e73b237989b3b47f42d8e5e1c9eaa6f6c9398
Finn Thain <fthain@telegraphics.com.au>
2008-11-18 20:45:21 +0100

    m68k: mac baboon interrupt enable/disable
    No-one seems to know how to mask individual baboon interrupts, so we just 
    mask the umbrella IRQ. This will work as long as only the IDE driver uses 
    the baboon chip (it can't deadlock). Use mac_enable_irq/mac_disable_irq 
    rather than enable_irq/disable_irq because the latter routines count the 
    depth of nested calls which triggers a warning and call trace because 
    IRQ_NUBUS_C is enabled twice in a row (once when the baboon handler is 
    registered, and once when the IDE IRQ is registered).
    
    Signed-off-by: Finn Thain <fthain@telegraphics.com.au>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/mac/macints.c
+++ arch/m68k/mac/macints.c
@@ -215,8 +215,8 @@ irqreturn_t mac_debug_handler(int, void *);
 
 /* #define DEBUG_MACINTS */
 
-static void mac_enable_irq(unsigned int irq);
-static void mac_disable_irq(unsigned int irq);
+void mac_enable_irq(unsigned int irq);
+void mac_disable_irq(unsigned int irq);
 
 static struct irq_controller mac_irq_controller = {
 	.name		= "mac",
@@ -275,7 +275,7 @@ void __init mac_init_IRQ(void)
  * These routines are just dispatchers to the VIA/OSS/PSC routines.
  */
 
-static void mac_enable_irq(unsigned int irq)
+void mac_enable_irq(unsigned int irq)
 {
 	int irq_src = IRQ_SRC(irq);
 
@@ -308,7 +308,7 @@ static void mac_enable_irq(unsigned int irq)
 	}
 }
 
-static void mac_disable_irq(unsigned int irq)
+void mac_disable_irq(unsigned int irq)
 {
 	int irq_src = IRQ_SRC(irq);
 
