b424d4c4a63a3a0670cded710028ae0690690cb2
Mike Frysinger <vapier.adi@gmail.com>
2008-11-18 17:48:22 +0800

    Blackfin arch: move ack_bad_irq() prototype to hardirq.h to match other arches
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/include/asm/irq.h
+++ arch/blackfin/include/asm/irq.h
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/irq.h>
 #include <asm/ptrace.h>
 
-extern void ack_bad_irq(unsigned int irq);
-
 static __inline__ int irq_canonicalize(int irq)
 {
 	return irq;
