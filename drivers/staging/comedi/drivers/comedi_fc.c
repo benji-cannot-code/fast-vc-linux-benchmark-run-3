b1d017490691ad879adcb3af64f3c5ce83576d31
Greg Kroah-Hartman <gregkh@suse.de>
2008-11-19 09:59:41 -0800

    Staging: comedi: fix checkpatch.pl issues in comedi_fc.c
    This fixes all of the checkpatch.pl issues in the comedi_fc.c file.
    
    Cc: David Schleef <ds@schleef.org>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/comedi_fc.c
+++ drivers/staging/comedi/drivers/comedi_fc.c
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "comedi_fc.h"
 
-static void increment_scan_progress(comedi_subdevice * subd,
-	unsigned int num_bytes)
+static void increment_scan_progress(comedi_subdevice *subd,
+				    unsigned int num_bytes)
 {
 	comedi_async *async = subd->async;
 	unsigned int scan_length = cfc_bytes_per_scan(subd);
@@ -43,8 +43,8 @@ static void increment_scan_progress(comedi_subdevice * subd,
 }
 
 /* Writes an array of data points to comedi's buffer */
-unsigned int cfc_write_array_to_buffer(comedi_subdevice * subd, void *data,
-	unsigned int num_bytes)
+unsigned int cfc_write_array_to_buffer(comedi_subdevice *subd, void *data,
+				       unsigned int num_bytes)
 {
 	comedi_async *async = subd->async;
 	unsigned int retval;
@@ -66,9 +66,10 @@ unsigned int cfc_write_array_to_buffer(comedi_subdevice * subd, void *data,
 
 	return num_bytes;
 }
+EXPORT_SYMBOL(cfc_write_array_to_buffer);
 
-unsigned int cfc_read_array_from_buffer(comedi_subdevice * subd, void *data,
-	unsigned int num_bytes)
+unsigned int cfc_read_array_from_buffer(comedi_subdevice *subd, void *data,
+					unsigned int num_bytes)
 {
 	comedi_async *async = subd->async;
 
@@ -83,8 +84,9 @@ unsigned int cfc_read_array_from_buffer(comedi_subdevice * subd, void *data,
 
 	return num_bytes;
 }
+EXPORT_SYMBOL(cfc_read_array_from_buffer);
 
-unsigned int cfc_handle_events(comedi_device * dev, comedi_subdevice * subd)
+unsigned int cfc_handle_events(comedi_device *dev, comedi_subdevice *subd)
 {
 	unsigned int events = subd->async->events;
 
@@ -98,6 +100,7 @@ unsigned int cfc_handle_events(comedi_device * dev, comedi_subdevice * subd)
 
 	return events;
 }
+EXPORT_SYMBOL(cfc_handle_events);
 
 MODULE_AUTHOR("Frank Mori Hess <fmhess@users.sourceforge.net>");
 MODULE_DESCRIPTION("Shared functions for Comedi low-level drivers");
@@ -107,13 +110,10 @@ static int __init comedi_fc_init_module(void)
 {
 	return 0;
 }
+
 static void __exit comedi_fc_cleanup_module(void)
 {
 }
 
 module_init(comedi_fc_init_module);
 module_exit(comedi_fc_cleanup_module);
-
-EXPORT_SYMBOL(cfc_write_array_to_buffer);
-EXPORT_SYMBOL(cfc_read_array_from_buffer);
-EXPORT_SYMBOL(cfc_handle_events);
