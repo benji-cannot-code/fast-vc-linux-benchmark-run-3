4b4200cf8ced6290202ad1f6138c377002df5292
Randy Dunlap <randy.dunlap@oracle.com>
2008-11-19 11:30:27 -0800

    coda: fix creds reference
    Needs a header file for credentials struct:
    
    linux-next-20081023/fs/coda/file.c:177: error: dereferencing pointer to incomplete type
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Jan Harkes <jaharkes@cs.cmu.edu>
    Cc: David Howells <dhowells@redhat.com>
    Cc: James Morris <jmorris@namei.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: James Morris <jmorris@namei.org>

--- fs/coda/file.c
+++ fs/coda/file.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/stat.h>
+#include <linux/cred.h>
 #include <linux/errno.h>
 #include <linux/smp_lock.h>
 #include <linux/string.h>
