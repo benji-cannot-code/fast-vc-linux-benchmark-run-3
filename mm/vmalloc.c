52c6533f0f3a74725ce50050f58ad9a95c01c3f5
Nick Piggin <npiggin@suse.de>
2008-11-19 15:36:32 -0800

    mm: vmalloc allocator off by one
    Fix off by one bug in the KVA allocator that can leave gaps in the address
    space.
    
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -363,7 +363,7 @@ retry:
 				goto found;
 		}
 
-		while (addr + size >= first->va_start && addr + size <= vend) {
+		while (addr + size > first->va_start && addr + size <= vend) {
 			addr = ALIGN(first->va_end + PAGE_SIZE, align);
 
 			n = rb_next(&first->rb_node);
