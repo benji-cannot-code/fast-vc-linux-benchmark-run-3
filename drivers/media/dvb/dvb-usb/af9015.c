a7db564030a7dc0955e022097a70e780be826173
Hans Verkuil <hverkuil@xs4all.nl>
2008-11-20 05:37:05 -0300

    V4L/DVB (9678): af9015: Cleanup switch for usb ID
    It is much more readable to swap the byteorder in the switch rather
    than in each case statement.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/af9015.c
+++ drivers/media/dvb/dvb-usb/af9015.c
@@ -746,8 +746,8 @@ static int af9015_read_config(struct usb_device *udev)
 				break;
 			}
 		} else {
-			switch (udev->descriptor.idVendor) {
-			case cpu_to_le16(USB_VID_LEADTEK):
+			switch (le16_to_cpu(udev->descriptor.idVendor)) {
+			case USB_VID_LEADTEK:
 				af9015_properties[i].rc_key_map =
 				  af9015_rc_keys_leadtek;
 				af9015_properties[i].rc_key_map_size =
@@ -757,7 +757,7 @@ static int af9015_read_config(struct usb_device *udev)
 				af9015_config.ir_table_size =
 				  ARRAY_SIZE(af9015_ir_table_leadtek);
 				break;
-			case cpu_to_le16(USB_VID_VISIONPLUS):
+			case USB_VID_VISIONPLUS:
 				if (udev->descriptor.idProduct ==
 				cpu_to_le16(USB_PID_AZUREWAVE_AD_TU700)) {
 					af9015_properties[i].rc_key_map =
@@ -770,7 +770,7 @@ static int af9015_read_config(struct usb_device *udev)
 					  ARRAY_SIZE(af9015_ir_table_twinhan);
 				}
 				break;
-			case cpu_to_le16(USB_VID_KWORLD_2):
+			case USB_VID_KWORLD_2:
 				/* TODO: use correct rc keys */
 				af9015_properties[i].rc_key_map =
 				  af9015_rc_keys_twinhan;
@@ -783,7 +783,7 @@ static int af9015_read_config(struct usb_device *udev)
 			/* Check USB manufacturer and product strings and try
 			   to determine correct remote in case of chip vendor
 			   reference IDs are used. */
-			case cpu_to_le16(USB_VID_AFATECH):
+			case USB_VID_AFATECH:
 				memset(manufacturer, 0, sizeof(manufacturer));
 				usb_string(udev, udev->descriptor.iManufacturer,
 					manufacturer, sizeof(manufacturer));
@@ -811,7 +811,7 @@ static int af9015_read_config(struct usb_device *udev)
 					  ARRAY_SIZE(af9015_ir_table_msi);
 				}
 				break;
-			case cpu_to_le16(USB_VID_AVERMEDIA):
+			case USB_VID_AVERMEDIA:
 				af9015_properties[i].rc_key_map =
 				  af9015_rc_keys_avermedia;
 				af9015_properties[i].rc_key_map_size =
