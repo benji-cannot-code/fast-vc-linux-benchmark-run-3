7f16cd299decacda0d49fa2bc58a9ae7bbda7579
Mark McLoughlin <markmc@redhat.com>
2008-11-20 15:49:50 +0000

    intel-iommu: move iommu_prepare_gfx_mapping() out of dma_remapping.h
    Signed-off-by: Mark McLoughlin <markmc@redhat.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- include/linux/dma_remapping.h
+++ include/linux/dma_remapping.h
@@ -21,11 +21,4 @@ extern void free_dmar_iommu(struct intel_iommu *iommu);
 
 extern int dmar_disabled;
 
-#ifndef CONFIG_DMAR_GFX_WA
-static inline void iommu_prepare_gfx_mapping(void)
-{
-	return;
-}
-#endif /* !CONFIG_DMAR_GFX_WA */
-
 #endif
