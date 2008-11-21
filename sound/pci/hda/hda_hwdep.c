16be59d9dff7af2e9ba64263d818a0d2989b2bcb
Takashi Iwai <tiwai@suse.de>
2008-11-21 09:26:20 +0100

    ALSA: hda - Make CONFIG_SND_HDA_RECONFIG for codec reconfiguration
    Make the codec re-configuration feature selectable via Kconfig,
    CONFIG_SND_HDA_RECONFIG.
    Also mark it as experimental (as it really is).
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_hwdep.c
+++ sound/pci/hda/hda_hwdep.c
@@ -146,6 +146,8 @@ int __devinit snd_hda_create_hwdep(struct hda_codec *codec)
 	return 0;
 }
 
+#ifdef CONFIG_SND_HDA_RECONFIG
+
 /*
  * sysfs interface
  */
@@ -348,3 +350,5 @@ int snd_hda_hwdep_add_sysfs(struct hda_codec *codec)
 					  hwdep->device, &codec_attrs[i]);
 	return 0;
 }
+
+#endif /* CONFIG_SND_HDA_RECONFIG */
