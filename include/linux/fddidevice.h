a4cd8fccfd05015b695b8210a60dd2276d3ab4f9
Stephen Hemminger <shemminger@vyatta.com>
2008-11-20 20:29:48 -0800

    fddi: convert to new network device ops
    Similar to ethernet. Convert infrastructure and the one lone FDDI
    driver (for the one lone user of that hardware??). Compile tested only.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/fddidevice.h
+++ include/linux/fddidevice.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 extern __be16	fddi_type_trans(struct sk_buff *skb,
 				struct net_device *dev);
+extern int fddi_change_mtu(struct net_device *dev, int new_mtu);
 extern struct net_device *alloc_fddidev(int sizeof_priv);
 #endif
 
