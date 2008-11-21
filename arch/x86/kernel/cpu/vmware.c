e5bdeaaf5a6da4a962ed6f9ada4dafabf783d709
Hannes Eder <hannes@hanneseder.net>
2008-11-21 23:17:09 +0100

    x86: vmware - fix sparse warnings
    Impact: fix sparse build warning
    
    Fix the following sparse warnings:
    
    arch/x86/kernel/cpu/vmware.c:69:5: warning: symbol 'vmware_platform'
    was not declared. Should it be static?
    arch/x86/kernel/cpu/vmware.c:89:15: warning: symbol
    'vmware_get_tsc_khz' was not declared. Should it be static?
    arch/x86/kernel/cpu/vmware.c:107:16: warning: symbol
    'vmware_set_feature_bits' was not declared. Should it be static?
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Cc: "Alok N Kataria" <akataria@vmware.com>
    Cc: "Dan Hecht" <dhecht@vmware.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/vmware.c
+++ arch/x86/kernel/cpu/vmware.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/dmi.h>
 #include <asm/div64.h>
+#include <asm/vmware.h>
 
 #define CPUID_VMWARE_INFO_LEAF	0x40000000
 #define VMWARE_HYPERVISOR_MAGIC	0x564D5868
