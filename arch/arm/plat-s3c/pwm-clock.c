71d19ceae5be87196f194150ad1c98c25481ebac
Ben Dooks <ben-linux@fluff.org>
2008-11-21 10:36:06 +0000

    [ARM] S3C: Fix scaler1 clock rate information
    The pwm-scaler0 and pwm-scaler1 clocks have their
    .id field set to -1 as they are not referenced to
    any specific device. However, parts of the pwm-clock
    code used the .id field to identify which scaler
    clock was being used.
    
    Fix the problem by comparing against the pointer to
    the clock to identify the scalers.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-s3c/pwm-clock.c
+++ arch/arm/plat-s3c/pwm-clock.c
@@ -74,11 +74,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * tclk -------------------------/
 */
 
+static struct clk clk_timer_scaler[];
+
 static unsigned long clk_pwm_scaler_get_rate(struct clk *clk)
 {
 	unsigned long tcfg0 = __raw_readl(S3C2410_TCFG0);
 
-	if (clk->id == 1) {
+	if (clk == &clk_timer_scaler[1]) {
 		tcfg0 &= S3C2410_TCFG_PRESCALER1_MASK;
 		tcfg0 >>= S3C2410_TCFG_PRESCALER1_SHIFT;
 	} else {
@@ -115,7 +117,7 @@ static int clk_pwm_scaler_set_rate(struct clk *clk, unsigned long rate)
 	local_irq_save(flags);
 	tcfg0 = __raw_readl(S3C2410_TCFG0);
 
-	if (clk->id == 1) {
+	if (clk == &clk_timer_scaler[1]) {
 		tcfg0 &= ~S3C2410_TCFG_PRESCALER1_MASK;
 		tcfg0 |= divisor << S3C2410_TCFG_PRESCALER1_SHIFT;
 	} else {
