2d592bd70601eedd3f5f1126432296941dd4908a
Hannes Eder <hannes@hanneseder.net>
2008-11-21 22:56:17 +0100

    x86: hypervisor - fix sparse warnings
    Impact: fix sparse build warning
    
    Fix the following sparse warnings:
    
      arch/x86/kernel/cpu/hypervisor.c:37:15: warning: symbol
      'get_hypervisor_tsc_freq' was not declared. Should it be static?
      arch/x86/kernel/cpu/hypervisor.c:53:16: warning: symbol
      'init_hypervisor' was not declared. Should it be static?
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Cc: "Alok N Kataria" <akataria@vmware.com>
    Cc: "Dan Hecht" <dhecht@vmware.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/hypervisor.c
+++ arch/x86/kernel/cpu/hypervisor.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/processor.h>
 #include <asm/vmware.h>
+#include <asm/hypervisor.h>
 
 static inline void __cpuinit
 detect_hypervisor_vendor(struct cpuinfo_x86 *c)
