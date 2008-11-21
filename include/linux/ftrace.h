f1bc19d3a09e3fcf37085d371bd3b4e050234ca3
Steven Rostedt <srostedt@redhat.com>
2008-11-21 12:59:38 -0500

    ftrace: add ftrace_off_permanent
    Impact: add new API to disable all of ftrace on anomalies
    
    It case of a serious anomaly being detected (like something caught by
    lockdep) it is a good idea to disable all tracing immediately, without
    grabing any locks.
    
    This patch adds ftrace_off_permanent that disables the tracers, function
    tracing and ring buffers without a way to enable them again. This should
    only be used when something serious has been detected.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -258,6 +258,7 @@ extern int ftrace_dump_on_oops;
 
 extern void tracing_start(void);
 extern void tracing_stop(void);
+extern void ftrace_off_permanent(void);
 
 extern void
 ftrace_special(unsigned long arg1, unsigned long arg2, unsigned long arg3);
@@ -291,6 +292,7 @@ ftrace_printk(const char *fmt, ...) __attribute__ ((format (printf, 1, 0)));
 
 static inline void tracing_start(void) { }
 static inline void tracing_stop(void) { }
+static inline void ftrace_off_permanent(void) { }
 static inline int
 ftrace_printk(const char *fmt, ...)
 {
