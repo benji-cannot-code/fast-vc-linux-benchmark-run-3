9458ac3f797feac15133d2e55ce11f8d49bc6a90
Bruce Allan <bruce.w.allan@intel.com>
2008-11-21 17:02:41 -0800

    e1000e: check return code from NVM accesses and fix bank detection
    Check return code for all NVM accesses[1] and error out accordingly; log
    a debug message for failed accesses.
    
    For ICH8/9, the valid NVM bank detect function was not checking whether the
    SEC1VAL (sector 1 valid) bit in the EECD register was itself valid (bits 8
    and 9 also have to be set).  If invalid, it would have defaulted to the
    possibly invalid bank 0.  Instead, try to use the valid bank detection
    method used by ICH10 which has been cleaned up a bit.
    
    [1] - reads and updates only; not writes because those are only writing to
    the Shadow RAM, the update following the write is the only thing actually
    writing the modified Shadow RAM contents to the NVM.
    
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/e1000e/defines.h
+++ drivers/net/e1000e/defines.h
@@ -573,6 +573,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define E1000_EECD_FLUPD     0x00080000 /* Update FLASH */
 #define E1000_EECD_AUPDEN    0x00100000 /* Enable Autonomous FLASH update */
 #define E1000_EECD_SEC1VAL   0x00400000 /* Sector One Valid */
+#define E1000_EECD_SEC1VAL_VALID_MASK (E1000_EECD_AUTO_RD | E1000_EECD_PRES)
 
 #define E1000_NVM_RW_REG_DATA   16   /* Offset to data in NVM read/write registers */
 #define E1000_NVM_RW_REG_DONE   2    /* Offset to READ/WRITE done bit */
