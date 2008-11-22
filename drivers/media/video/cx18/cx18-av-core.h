e3556833b287cc2027caafb27cc4077023c4601e
Andy Walls <awalls@radix.net>
2008-11-22 01:37:34 -0300

    V4L/DVB (9728): cx18: Copyright attribution update for files modified by awalls
    Add copyright attribution for files modified by awalls in 2008
    
    Signed-off-by: Andy Walls <awalls@radix.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-av-core.h
+++ drivers/media/video/cx18/cx18-av-core.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Derived from cx25840-core.h
  *
  *  Copyright (C) 2007  Hans Verkuil <hverkuil@xs4all.nl>
+ *  Copyright (C) 2008  Andy Walls <awalls@radix.net>
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
