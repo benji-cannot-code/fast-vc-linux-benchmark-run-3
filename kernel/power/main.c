f830fbaedae6e3a16c36149fd4d13d4132bb5d77
Ingo Molnar <mingo@elte.hu>
2008-11-23 10:37:12 +0100

    tracing: allow tracing of suspend/resume & hibernation code again
    Impact: widen function-tracing to suspend+resume (and hibernation) sequences
    
    Now that the ftrace kernel thread is gone, we can allow tracing
    during suspend/resume again.
    
    So revert these two commits:
    
      f42ac38c5 "ftrace: disable tracing for suspend to ram"
      41108eb10 "ftrace: disable tracing for hibernation"
    
    This should be tested very carefully, as it could interact with
    altneratives instruction patching, etc.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/freezer.h>
 #include <linux/vmstat.h>
 #include <linux/syscalls.h>
-#include <linux/ftrace.h>
 
 #include "power.h"
 
@@ -318,7 +317,7 @@ static int suspend_enter(suspend_state_t state)
  */
 int suspend_devices_and_enter(suspend_state_t state)
 {
-	int error, ftrace_save;
+	int error;
 
 	if (!suspend_ops)
 		return -ENOSYS;
@@ -329,7 +328,6 @@ int suspend_devices_and_enter(suspend_state_t state)
 			goto Close;
 	}
 	suspend_console();
-	ftrace_save = __ftrace_enabled_save();
 	suspend_test_start();
 	error = device_suspend(PMSG_SUSPEND);
 	if (error) {
@@ -361,7 +359,6 @@ int suspend_devices_and_enter(suspend_state_t state)
 	suspend_test_start();
 	device_resume(PMSG_RESUME);
 	suspend_test_finish("resume devices");
-	__ftrace_enabled_restore(ftrace_save);
 	resume_console();
  Close:
 	if (suspend_ops->end)
