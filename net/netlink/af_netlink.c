f258c8259053f034806f56a4fe803cf0474ab487
Eric Dumazet <dada1@cosmosbay.com>
2008-11-23 15:48:22 -0800

    net: af_netlink should update its inuse counter
    In order to have relevant information for NETLINK protocol, in
    /proc/net/protocols, we should use sock_prot_inuse_add() to
    update a (percpu and pernamespace) counter of inuse sockets.
    
    Signed-off-by: Eric Dumazet <dada1@cosmosbay.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -453,6 +453,7 @@ static int netlink_create(struct net *net, struct socket *sock, int protocol)
 	if (err < 0)
 		goto out_module;
 
+	sock_prot_inuse_add(net, &netlink_proto, 1);
 	nlk = nlk_sk(sock->sk);
 	nlk->module = module;
 out:
@@ -512,6 +513,7 @@ static int netlink_release(struct socket *sock)
 	kfree(nlk->groups);
 	nlk->groups = NULL;
 
+	sock_prot_inuse_add(sock_net(sk), &netlink_proto, -1);
 	sock_put(sk);
 	return 0;
 }
