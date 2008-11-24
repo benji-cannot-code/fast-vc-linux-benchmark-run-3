d80aca4da8ff642c21a05b24850fb3ecc41d40f8
Adrian Hunter <ext-adrian.hunter@nokia.com>
2008-11-24 14:44:36 +0200

    [MTD] [NAND] OMAP: OneNAND: header file relocation (part 2)
    Include <linux/dma-mapping.h> and <linux/io.h>, not files from <asm/*>
    
    Signed-off-by: Adrian Hunter <ext-adrian.hunter@nokia.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/onenand/omap2.c
+++ drivers/mtd/onenand/omap2.c
@@ -33,16 +33,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
+#include <linux/dma-mapping.h>
+#include <linux/io.h>
 
-#include <asm/io.h>
 #include <asm/mach/flash.h>
 #include <mach/gpmc.h>
 #include <mach/onenand.h>
 #include <mach/gpio.h>
 #include <mach/pm.h>
 
-#include <linux/dma-mapping.h>
-#include <asm/dma-mapping.h>
 #include <mach/dma.h>
 
 #include <mach/board.h>
