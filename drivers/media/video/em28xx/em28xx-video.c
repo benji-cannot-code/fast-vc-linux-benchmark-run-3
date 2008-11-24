ec1f7774b3ced422b0209526d688508a9715351a
Hans Verkuil <hverkuil@xs4all.nl>
2008-11-24 09:53:22 -0300

    V4L/DVB (9748): em28xx: fix compile warning
    Label fail_unreg is no longer used.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-video.c
+++ drivers/media/video/em28xx/em28xx-video.c
@@ -2106,8 +2106,6 @@ static int em28xx_init_dev(struct em28xx **devhandle, struct usb_device *udev,
 
 	return 0;
 
-fail_unreg:
-	em28xx_release_resources(dev);
 fail_reg_devices:
 	mutex_unlock(&dev->lock);
 	return retval;
