2de36e23e324a01c75cd883628a6e4afea095045
Markus Metzger <markus.t.metzger@intel.com>
2008-11-25 08:45:13 +0100

    x86, bts: exclude ds.c from build when disabled
    Impact: cleanup
    
    Move the CONFIG guard from the .c file into the makefile.
    
    Reported-by: Andi Kleen <andi-suse@firstfloor.org>
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/ds.c
+++ arch/x86/kernel/ds.c
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 
-#ifdef CONFIG_X86_DS
-
 #include <asm/ds.h>
 
 #include <linux/errno.h>
@@ -879,4 +877,3 @@ void ds_free(struct ds_context *context)
 	while (leftovers--)
 		ds_put_context(context);
 }
-#endif /* CONFIG_X86_DS */
