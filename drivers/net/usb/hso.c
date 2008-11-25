be44597025e3a7a3a839177c13d82cb643778d6a
Denis Joseph Barrow <D.Barow@option.com>
2008-11-25 00:36:10 -0800

    hso: Hook up ->reset_resume
    Made usb_drivers reset_resume function point to hso_resume this 
    fixes problems a usb reset is done when the network interface
    is left idle for a few minutes. Possibly reset_resume should
    initialise hardware more but this works in the common case.
    
    Signed-off-by: Denis Joseph Barrow <D.Barow@option.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/hso.c
+++ drivers/net/usb/hso.c
@@ -3278,6 +3278,7 @@ static struct usb_driver hso_driver = {
 	.id_table = hso_ids,
 	.suspend = hso_suspend,
 	.resume = hso_resume,
+	.reset_resume = hso_resume,
 	.supports_autosuspend = 1,
 };
 
