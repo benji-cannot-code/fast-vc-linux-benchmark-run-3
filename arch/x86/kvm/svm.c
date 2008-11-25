3131ba55553a8d8a48470302905c8a5094a71eab
Alexander Graf <agraf@suse.de>
2008-11-25 20:17:10 +0100

    KVM: SVM: Allow setting the SVME bit
    Normally setting the SVME bit in EFER is not allowed, as we did
    not support SVM. Not since we do, we should also allow enabling
    SVM mode.
    
    v2 comes as last patch, so we don't enable half-ready code
    v4 introduces a module option to enable SVM
    v6 warns that nesting is enabled
    
    Acked-by: Joerg Roedel <joro@8bytes.org>
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -70,6 +70,9 @@ static int npt = 1;
 
 module_param(npt, int, S_IRUGO);
 
+static int nested = 0;
+module_param(nested, int, S_IRUGO);
+
 static void kvm_reput_irq(struct vcpu_svm *svm);
 static void svm_flush_tlb(struct kvm_vcpu *vcpu);
 
@@ -443,6 +446,11 @@ static __init int svm_hardware_setup(void)
 
 	if (boot_cpu_has(X86_FEATURE_NX))
 		kvm_enable_efer_bits(EFER_NX);
+
+	if (nested) {
+		printk(KERN_INFO "kvm: Nested Virtualization enabled\n");
+		kvm_enable_efer_bits(EFER_SVME);
+	}
 
 	for_each_online_cpu(cpu) {
 		r = svm_cpu_init(cpu);
