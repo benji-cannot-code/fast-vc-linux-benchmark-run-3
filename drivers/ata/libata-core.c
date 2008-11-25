8c1e274dbf2d41f25a2eea023a13a2e9b3ed8625
Qinghuang Feng <qhfeng.kernel@gmail.com>
2008-11-25 11:37:19 -0500

    [libata] Update kernel-doc comments to match source code
    Signed-off-by: Qinghuang Feng <qhfeng.kernel@gmail.com>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -4733,7 +4733,6 @@ static struct ata_queued_cmd *ata_qc_new(struct ata_port *ap)
 /**
  *	ata_qc_new_init - Request an available ATA command, and initialize it
  *	@dev: Device from whom we request an available command structure
- *	@tag: command tag
  *
  *	LOCKING:
  *	None.
