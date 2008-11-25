7470f96197afa79ccb91c7a44b8e3bbea3c0cb1d
Greg Kroah-Hartman <gregkh@suse.de>
2008-11-24 21:15:22 -0800

    Staging: benet: fix build error.
    udelay() was not being defined properly, this patch fixes it.
    
    Reported-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Subbu Seetharaman <subbus@serverengines.com>
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/benet/mpu.c
+++ drivers/staging/benet/mpu.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 209 N. Fair Oaks Ave
  * Sunnyvale, CA 94085
  */
+#include <linux/delay.h>
 #include "hwlib.h"
 #include "bestatus.h"
 
