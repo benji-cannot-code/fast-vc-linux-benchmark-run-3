225854bbe70dce0169d82fd51b880d4d433f5adb
Steven Rostedt <rostedt@goodmis.org>
2008-11-26 00:16:26 -0500

    ftrace: add thread comm to function graph tracer
    Impact: enhancement to function graph tracer
    
    Export the trace_find_cmdline so the function graph tracer can
    use it to print the comms of the threads.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -805,7 +805,7 @@ static void trace_save_cmdline(struct task_struct *tsk)
 	spin_unlock(&trace_cmdline_lock);
 }
 
-static char *trace_find_cmdline(int pid)
+char *trace_find_cmdline(int pid)
 {
 	char *cmdline = "<...>";
 	unsigned map;
