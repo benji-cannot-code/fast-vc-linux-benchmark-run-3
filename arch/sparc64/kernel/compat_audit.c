9eb1865d8674cbd233c31a960040e858d92bf402
Stephen Rothwell <sfr@canb.auug.org.au>
2008-11-26 00:51:32 -0800

    sparc: combine unistd_{32,64}.h
    This is complicated a little because compat_audit.c wants to see only
    the 32bit syscall numbers, but is being built in a 64bit compile.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/compat_audit.c
+++ arch/sparc64/kernel/compat_audit.c
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#include <asm/unistd_32.h>
+#define __32bit_syscall_numbers__
+#include <asm/unistd.h>
 
 unsigned sparc32_dir_class[] = {
 #include <asm-generic/audit_dir_write.h>
