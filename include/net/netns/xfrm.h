486a5c1b49c1468e7159a317fce2e540dcc82f6e
Alexey Dobriyan <adobriyan@gmail.com>
2008-11-25 17:16:11 -0800

    netns xfrm: per-netns xfrm_state_all list
    This is done to get
    a) simple "something leaked" check
    b) cover possible DoSes when other netns puts many, many xfrm_states
       onto a list.
    c) not miss "alien xfrm_state" check in some of list iterators in future.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/xfrm.h
+++ include/net/netns/xfrm.h
@@ -2,7 +2,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NETNS_XFRM_H
 #define __NETNS_XFRM_H
 
+#include <linux/list.h>
+
 struct netns_xfrm {
+	struct list_head	state_all;
 };
 
 #endif
