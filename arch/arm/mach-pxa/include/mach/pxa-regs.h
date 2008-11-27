044763a61ceda7df55cad6bd009950d321c75aba
Eric Miao <eric.miao@marvell.com>
2008-11-27 17:08:42 +0800

    [ARM] pxa: remove unused PWM register definitions, use generic PWM API
    We now have generic PWM API for PXA, the PWM registers definitions are
    now used nowhere, and it is not encouraged to manipulate them directly
    by driver code.
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/include/mach/pxa-regs.h
+++ arch/arm/mach-pxa/include/mach/pxa-regs.h
@@ -421,19 +421,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /*
- * Pulse Width Modulator
- */
-
-#define PWM_CTRL0	__REG(0x40B00000)  /* PWM 0 Control Register */
-#define PWM_PWDUTY0	__REG(0x40B00004)  /* PWM 0 Duty Cycle Register */
-#define PWM_PERVAL0	__REG(0x40B00008)  /* PWM 0 Period Control Register */
-
-#define PWM_CTRL1	__REG(0x40C00000)  /* PWM 1Control Register */
-#define PWM_PWDUTY1	__REG(0x40C00004)  /* PWM 1 Duty Cycle Register */
-#define PWM_PERVAL1	__REG(0x40C00008)  /* PWM 1 Period Control Register */
-
-
-/*
  * Interrupt Controller
  */
 
