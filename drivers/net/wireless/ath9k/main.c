a734b525449f7e5a1854104da72de3328d113294
Sujith <Sujith.Manoharan@atheros.com>
2008-11-28 22:21:08 +0530

    ath9k: Add a debugfs file for dumping DMA status
    Debugfs file location: ath9k/<wiphy>/dma
    Contains values in DMA debug registers.
    
    Signed-off-by: Sujith <Sujith.Manoharan@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath9k/main.c
+++ drivers/net/wireless/ath9k/main.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nl80211.h>
 #include "core.h"
 #include "reg.h"
+#include "hw.h"
 
 #define ATH_PCI_VERSION "0.1"
 
