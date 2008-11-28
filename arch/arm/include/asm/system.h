aefa7bde87496a10857024d828b6c0adb1465ac9
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-11-28 22:37:15 +0000

    [ARM] asm/system.h does not require asm/memory.h
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/system.h
+++ arch/arm/include/asm/system.h
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __KERNEL__
 
-#include <asm/memory.h>
-
 #define CPU_ARCH_UNKNOWN	0
 #define CPU_ARCH_ARMv3		1
 #define CPU_ARCH_ARMv4		2
