e274453a2c25a91e2c1a0d6c5d463f56f0c1b786
Eric Miao <eric.miao@marvell.com>
2008-11-28 14:57:33 +0800

    [ARM] pxa: further cleanup of pxa-regs.h
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/include/mach/pxa-regs.h
+++ arch/arm/mach-pxa/include/mach/pxa-regs.h
@@ -125,18 +125,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DCMD_LENGTH	0x01fff		/* length mask (max = 8K - 1) */
 
 /*
- * I2C registers - moved into drivers/i2c/busses/i2c-pxa.c
- */
-
-/*
- * Serial Audio Controller - moved into sound/soc/pxa/pxa2xx-i2s.c
- */
-
-/*
- * Fast Infrared Communication Port - moved into drivers/net/irda/pxaficp_ir.c
- */
-
-/*
  * Real Time Clock
  */
 
@@ -271,22 +259,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GEDR(x)		(*((((x) & 0x7f) < 96) ? &_GEDR(x) : &GEDR3))
 #define GAFR(x)		(*((((x) & 0x7f) < 96) ? &_GAFR(x) : \
 			 ((((x) & 0x7f) < 112) ? &GAFR3_L : &GAFR3_U)))
-
-
-/*
- * Power Manager - see pxa2xx-regs.h
- */
-
-/*
- * SSP Serial Port Registers - see arch/arm/mach-pxa/include/mach/regs-ssp.h
- */
-
-/*
- * MultiMediaCard (MMC) controller - see drivers/mmc/host/pxamci.h
- */
-
-/*
- * Core Clock - see arch/arm/mach-pxa/include/mach/pxa2xx-regs.h
- */
 
 #endif
