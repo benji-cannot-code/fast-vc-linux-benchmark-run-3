aefbebdc380b15174fd4b8beee07ce2916363efb
Takashi Iwai <tiwai@suse.de>
2008-11-28 12:57:38 +0100

    ALSA: hda - Remove unused proc entry in hda_bus struct
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_codec.h
+++ sound/pci/hda/hda_codec.h
@@ -616,8 +616,6 @@ struct hda_bus {
 	/* unsolicited event queue */
 	struct hda_bus_unsolicited *unsol;
 
-	struct snd_info_entry *proc;
-
 	/* assigned PCMs */
 	DECLARE_BITMAP(pcm_dev_bits, SNDRV_PCM_DEVICES);
 
