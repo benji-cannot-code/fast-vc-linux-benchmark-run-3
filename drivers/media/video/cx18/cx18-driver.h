984b4325000970de52d722ae4886a0459e9150c0
Andy Walls <awalls@radix.net>
2008-11-28 00:04:21 -0300

    V4L/DVB (9800): cx18: Eliminate q_io from stream buffer handling
    Eliminate q_io from stream buffer handling in anticipation of upcoming
    changes in buffer handling.  q_io was a holdover from ivtv and it's function
    in cx18 was trivial and not necessary.  We just push things back onto the
    front of q_full now, instead of maintaining a 1 buffer q_io queue.
    
    Signed-off-by: Andy Walls <awalls@radix.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-driver.h
+++ drivers/media/video/cx18/cx18-driver.h
@@ -291,7 +291,6 @@ struct cx18_stream {
 	/* Buffer Queues */
 	struct cx18_queue q_free;	/* free buffers */
 	struct cx18_queue q_full;	/* full buffers */
-	struct cx18_queue q_io;		/* waiting for I/O */
 
 	/* DVB / Digital Transport */
 	struct cx18_dvb dvb;
--- drivers/media/video/cx18/cx18-queue.c
+++ drivers/media/video/cx18/cx18-queue.c
@@ -43,8 +43,8 @@ void cx18_queue_init(struct cx18_queue *q)
 	q->bytesused = 0;
 }
 
-void cx18_enqueue(struct cx18_stream *s, struct cx18_buffer *buf,
-		struct cx18_queue *q)
+void _cx18_enqueue(struct cx18_stream *s, struct cx18_buffer *buf,
+		   struct cx18_queue *q, int to_front)
 {
 	/* clear the buffer if it is going to be enqueued to the free queue */
 	if (q == &s->q_free) {
@@ -54,7 +54,10 @@ void cx18_enqueue(struct cx18_stream *s, struct cx18_buffer *buf,
 		buf->skipped = 0;
 	}
 	mutex_lock(&s->qlock);
-	list_add_tail(&buf->list, &q->list);
+	if (to_front)
+		list_add(&buf->list, &q->list); /* LIFO */
+	else
+		list_add_tail(&buf->list, &q->list); /* FIFO */
 	atomic_inc(&q->buffers);
 	q->bytesused += buf->bytesused - buf->readpos;
 	mutex_unlock(&s->qlock);
@@ -160,7 +163,6 @@ static void cx18_queue_flush(struct cx18_stream *s, struct cx18_queue *q)
 
 void cx18_flush_queues(struct cx18_stream *s)
 {
-	cx18_queue_flush(s, &s->q_io);
 	cx18_queue_flush(s, &s->q_full);
 }
 
