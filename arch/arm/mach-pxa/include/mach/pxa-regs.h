f8de32c08998ef462eba7854d1f85d8f9a6fc968
Eric Miao <eric.miao@marvell.com>
2008-11-28 13:49:22 +0800

    [ARM] pxa: move pxa2xx specific PWRMODE definitions into pxa2xx-regs.h
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/include/mach/pxa-regs.h
+++ arch/arm/mach-pxa/include/mach/pxa-regs.h
@@ -554,11 +554,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Core Clock - see arch/arm/mach-pxa/include/mach/pxa2xx-regs.h
  */
 
-/* PWRMODE register M field values */
-
-#define PWRMODE_IDLE		0x1
-#define PWRMODE_STANDBY		0x2
-#define PWRMODE_SLEEP		0x3
-#define PWRMODE_DEEPSLEEP	0x7
-
 #endif
--- arch/arm/mach-pxa/include/mach/pxa2xx-regs.h
+++ arch/arm/mach-pxa/include/mach/pxa2xx-regs.h
@@ -244,4 +244,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OSCC_OON	(1 << 1)	/* 32.768kHz OON (write-once only bit) */
 #define OSCC_OOK	(1 << 0)	/* 32.768kHz OOK (read-only bit) */
 
+/* PWRMODE register M field values */
+
+#define PWRMODE_IDLE		0x1
+#define PWRMODE_STANDBY		0x2
+#define PWRMODE_SLEEP		0x3
+#define PWRMODE_DEEPSLEEP	0x7
+
 #endif
