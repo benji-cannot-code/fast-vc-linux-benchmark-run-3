e55a43e6efdf5668493e8e07c265292c076ce646
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-11-29 18:48:07 +0000

    [ARM] Rename ISA mach/dma.h header to mach/isa-dma.h
    This avoids confusion with platform specific DMA implementations in
    mach/dma.h
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/dma.h
+++ arch/arm/include/asm/dma.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 typedef unsigned int dmach_t;
 
-#include <mach/dma.h>
+#include <mach/isa-dma.h>
 
 /*
  * DMA modes
--- arch/arm/mach-h720x/include/mach/isa-dma.h
+++ arch/arm/mach-h720x/include/mach/isa-dma.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/arm/mach-h720x/include/mach/dma.h
+ * arch/arm/mach-h720x/include/mach/isa-dma.h
  *
  * Architecture DMA routes
  *
