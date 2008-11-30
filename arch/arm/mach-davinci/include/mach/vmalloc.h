6f0bf51b47546d9ad6b8fffe38c9dbf3a827f602
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-11-30 14:42:43 +0000

    [ARM] davinci: asm/memory.h is not required for mach/vmalloc.h
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-davinci/include/mach/vmalloc.h
+++ arch/arm/mach-davinci/include/mach/vmalloc.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * is licensed "as is" without any warranty of any kind, whether express
  * or implied.
  */
-#include <asm/memory.h>
 #include <mach/io.h>
 
 /* Allow vmalloc range until the IO virtual range minus a 2M "hole" */
