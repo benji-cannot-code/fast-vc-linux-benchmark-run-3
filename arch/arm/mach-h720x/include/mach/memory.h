2d2791ea2313199fda4dd160553d14fe007f8d70
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-11-30 13:26:47 +0000

    [ARM] Ensure that both MAX_DMA_ADDRESS and ISA_DMA_THRESHOLD are defined
    Both of these symbols should be defined by a platform, or neither
    should be defined.  Ensure that all platforms conform.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-h720x/include/mach/memory.h
+++ arch/arm/mach-h720x/include/mach/memory.h
@@ -13,10 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This is the maximum DMA address that can be DMAd to.
  * There should not be more than (0xd0000000 - 0xc0000000)
  * bytes of RAM.
- *
- * If you set this, you must also set ISA_DMA_THRESHOLD and setup a DMA
- * zone if this does not cover all possible RAM.
  */
+#define ISA_DMA_THRESHOLD	(PHYS_OFFSET + SZ_256M - 1)
 #define MAX_DMA_ADDRESS		(PAGE_OFFSET + SZ_256M)
 
 #endif
