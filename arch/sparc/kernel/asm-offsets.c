381bc9dd1adde7b5600c11ea45b97189b06a6b47
Sam Ravnborg <sam@ravnborg.org>
2008-11-29 21:51:05 -0800

    sparc,sparc64: unify asm-offsets.c
    sparc64 does not use constants generated from asm-offsets
    but to prepare it to do so the parts that could be
    shared do now generate constants for sparc64 too.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/asm-offsets.c
+++ arch/sparc/kernel/asm-offsets.c
@@ -15,15 +15,28 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // #include <linux/mm.h>
 #include <linux/kbuild.h>
 
-int foo(void)
+#ifdef CONFIG_SPARC32
+int sparc32_foo(void)
 {
-	DEFINE(AOFF_task_thread, offsetof(struct task_struct, thread));
-	BLANK();
 	DEFINE(AOFF_thread_fork_kpsr,
 			offsetof(struct thread_struct, fork_kpsr));
+	return 0;
+}
+#else
+int sparc64_foo(void)
+{
+	return 0;
+}
+#endif
+
+int foo(void)
+{
+	BLANK();
+	DEFINE(AOFF_task_thread, offsetof(struct task_struct, thread));
 	BLANK();
 	DEFINE(AOFF_mm_context, offsetof(struct mm_struct, context));
 
 	/* DEFINE(NUM_USER_SEGMENTS, TASK_SIZE>>28); */
 	return 0;
 }
+
--- arch/sparc64/kernel/asm-offsets.c
+++ arch/sparc64/kernel/asm-offsets.c
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* Dummy asm-offsets.c file. Required by kbuild and ready to be used - hint! */
+#include "../../sparc/kernel/asm-offsets.c"
