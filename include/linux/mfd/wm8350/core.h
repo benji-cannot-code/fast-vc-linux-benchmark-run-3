f9e3e92d4bd23ed612f06d7b645b95c3a670dbbb
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-11-30 22:45:14 +0100

    power_supply: Add battery health reporting for WM8350
    Implement support for reporting battery health in the WM8350 battery
    interface. Since we are now able to report this via the classs remove
    the diagnostics from the interrupt handler.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Anton Vorontsov <cbouatmailru@gmail.com>
    Signed-off-by: Samuel Ortiz <sameo@openedhand.com>

--- include/linux/mfd/wm8350/core.h
+++ include/linux/mfd/wm8350/core.h
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WM8350_OVER_CURRENT_INT_STATUS_MASK     0x25
 #define WM8350_GPIO_INT_STATUS_MASK             0x26
 #define WM8350_COMPARATOR_INT_STATUS_MASK       0x27
+#define WM8350_CHARGER_OVERRIDES		0xE2
 #define WM8350_MISC_OVERRIDES			0xE3
 #define WM8350_COMPARATOR_OVERRIDES		0xE7
 #define WM8350_STATE_MACHINE_STATUS		0xE9
@@ -532,6 +533,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WM8350_DC3_STS                          0x0004
 #define WM8350_DC2_STS                          0x0002
 #define WM8350_DC1_STS                          0x0001
+
+/*
+ * R226 (0xE2) - Charger status
+ */
+#define WM8350_CHG_BATT_HOT_OVRDE		0x8000
+#define WM8350_CHG_BATT_COLD_OVRDE		0x4000
 
 /*
  * R227 (0xE3) - Misc Overrides
