faa77c0b0d6769bfc26c1e95f62c5629a8bacc0c
Takashi Iwai <tiwai@suse.de>
2008-12-01 16:29:47 +0100

    ALSA: soc - Remove obsoleted sound/driver.h inclusion
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/soc/pxa/em-x270.c
+++ sound/soc/pxa/em-x270.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/moduleparam.h>
 #include <linux/device.h>
 
-#include <sound/driver.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/soc.h>
