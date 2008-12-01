c3e267c1d275a6f0feaf6e810cb383981e043fbd
Eric Miao <eric.miao@marvell.com>
2008-12-01 11:43:08 +0800

    [ARM] pxa: explicit #include <mach/dma.h> in various drivers
    Where 'pxa_dma_desc' and 'pxa_{request,free}_dma' are referenced.
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- drivers/mtd/nand/pxa3xx_nand.c
+++ drivers/mtd/nand/pxa3xx_nand.c
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/partitions.h>
 #include <linux/io.h>
 #include <linux/irq.h>
-#include <asm/dma.h>
 
+#include <mach/dma.h>
 #include <mach/pxa-regs.h>
 #include <mach/pxa3xx_nand.h>
 
