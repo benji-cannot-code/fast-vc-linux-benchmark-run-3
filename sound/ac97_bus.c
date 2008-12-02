b535e9b3e8e1c0b00e79a4cf4074bd4d94a6b54c
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-12-02 15:15:50 +0000

    ALSA: ac97 - Include ac97_codec.h for ac97_bus_type declaration
    This fixes a sparse warning caused by the lack of a connection with the
    prototype for ac97_bus_type.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/ac97_bus.c
+++ sound/ac97_bus.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/string.h>
+#include <sound/ac97_codec.h>
 
 /*
  * Let drivers decide whether they want to support given codec from their
