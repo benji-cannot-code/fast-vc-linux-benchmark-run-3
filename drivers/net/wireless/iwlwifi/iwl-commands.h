fb08813b2e17715a51c92f2e6b4a350fbf57cfb2
Chatre, Reinette <reinette.chatre@intel.com>
2008-12-02 12:14:05 -0800

    iwlwifi: store ucode version number
    We store the ucode version number as part of
    iwl_priv/iwl3945_priv. This enables us to determine
    if particular ucode has support for features in order
    to have driver support more than one ucode API.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-commands.h
+++ drivers/net/wireless/iwlwifi/iwl-commands.h
@@ -70,6 +70,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_commands_h__
 #define __iwl_commands_h__
 
+/* uCode version contains 4 values: Major/Minor/API/Serial */
+#define IWL_UCODE_MAJOR(ver)	(((ver) & 0xFF000000) >> 24)
+#define IWL_UCODE_MINOR(ver)	(((ver) & 0x00FF0000) >> 16)
+#define IWL_UCODE_API(ver)	(((ver) & 0x0000FF00) >> 8)
+#define IWL_UCODE_SERIAL(ver)	((ver) & 0x000000FF)
+
 enum {
 	REPLY_ALIVE = 0x1,
 	REPLY_ERROR = 0x2,
