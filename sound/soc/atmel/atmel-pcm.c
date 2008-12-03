ff0b8743ae467440e64db038fdcb6b54e463c3da
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-12-03 18:17:28 +0000

    ASoC: Remove device from platform suspend and resume operations
    None of the platforms are actually using the SoC device so remove it
    (only atmel actually has a suspend method).
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/atmel/atmel-pcm.c
+++ sound/soc/atmel/atmel-pcm.c
@@ -418,8 +418,7 @@ static void atmel_pcm_free_dma_buffers(struct snd_pcm *pcm)
 }
 
 #ifdef CONFIG_PM
-static int atmel_pcm_suspend(struct platform_device *pdev,
-	struct snd_soc_dai *dai)
+static int atmel_pcm_suspend(struct snd_soc_dai *dai)
 {
 	struct snd_pcm_runtime *runtime = dai->runtime;
 	struct atmel_runtime_data *prtd;
@@ -443,8 +442,7 @@ static int atmel_pcm_suspend(struct platform_device *pdev,
 	return 0;
 }
 
-static int atmel_pcm_resume(struct platform_device *pdev,
-	struct snd_soc_dai *dai)
+static int atmel_pcm_resume(struct snd_soc_dai *dai)
 {
 	struct snd_pcm_runtime *runtime = dai->runtime;
 	struct atmel_runtime_data *prtd;
