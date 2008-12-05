83a76adcbca5b97dfe58364743dc88127712ef8f
David S. Miller <davem@davemloft.net>
2008-12-05 00:40:43 -0800

    sparc: Mark prom_early_alloc non-static.
    A subsequent changeset will use this.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/prom_32.c
+++ arch/sparc/kernel/prom_32.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static unsigned int prom_early_allocated;
 
-static void * __init prom_early_alloc(unsigned long size)
+void * __init prom_early_alloc(unsigned long size)
 {
 	void *ret;
 
--- arch/sparc/kernel/prom_64.c
+++ arch/sparc/kernel/prom_64.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static unsigned int prom_early_allocated __initdata;
 
-static void * __init prom_early_alloc(unsigned long size)
+void * __init prom_early_alloc(unsigned long size)
 {
 	unsigned long paddr = lmb_alloc(size, SMP_CACHE_BYTES);
 	void *ret;
