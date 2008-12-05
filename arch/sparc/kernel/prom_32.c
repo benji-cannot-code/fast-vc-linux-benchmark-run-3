e55e00e826c0c4ced0a5b676564997abfaa74742
David S. Miller <davem@davemloft.net>
2008-12-05 00:50:22 -0800

    sparc: Move 'unique_id' into prom_common.c and rename to 'prom_unique_id'
    This will be used in a subsequent changeset.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/prom_32.c
+++ arch/sparc/kernel/prom_32.c
@@ -197,8 +197,6 @@ static char * __init build_full_name(struct device_node *dp)
 	return n;
 }
 
-static unsigned int unique_id;
-
 static struct property * __init build_one_prop(phandle node, char *prev, char *special_name, void *special_val, int special_len)
 {
 	static struct property *tmp = NULL;
@@ -212,7 +210,7 @@ static struct property * __init build_one_prop(phandle node, char *prev, char *s
 		tmp = NULL;
 	} else {
 		p = prom_early_alloc(sizeof(struct property) + 32);
-		p->unique_id = unique_id++;
+		p->unique_id = prom_unique_id++;
 	}
 
 	p->name = (char *) (p + 1);
@@ -287,7 +285,7 @@ static struct device_node * __init create_node(phandle node)
 		return NULL;
 
 	dp = prom_early_alloc(sizeof(*dp));
-	dp->unique_id = unique_id++;
+	dp->unique_id = prom_unique_id++;
 
 	kref_init(&dp->kref);
 
