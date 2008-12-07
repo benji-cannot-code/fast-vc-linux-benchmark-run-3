7500fc4761d1dfe1ff7b093d433d22e4c3275b5d
Alan Cox <alan@redhat.com>
2008-12-06 23:46:04 -0800

    USB: tty: SprogII DCC controller identifiers
    Someone on rmweb reminded me this had been overlooked from ages ago..
    
    Add the identifiers for the Sprog II USB. This is a DCC control interface
    using the FTDI-SIO hardware: http://www.sprog-dcc.co.uk/. People have been
    using it with insmod options for ages, this just puts it into the driver
    data.
    
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -76,6 +76,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* OpenDCC (www.opendcc.de) product id */
 #define FTDI_OPENDCC_PID	0xBFD8
 
+/* Sprog II (Andrew Crosland's SprogII DCC interface) */
+#define FTDI_SPROG_II		0xF0C8
+
 /* www.crystalfontz.com devices - thanx for providing free devices for evaluation ! */
 /* they use the ftdi chipset for the USB interface and the vendor id is the same */
 #define FTDI_XF_632_PID 0xFC08	/* 632: 16x2 Character Display */
