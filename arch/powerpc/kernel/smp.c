27967ee994f669a83109c01d519ad91622eeaa42
Nathan Lynch <ntl@pobox.com>
2008-12-10 14:28:41 +0000

    powerpc: Move smp_hw_index to 32-bit code
    smp_hw_index isn't used on 64-bit, so move it from smp.c to
    setup_32.c.
    
    Signed-off-by: Nathan Lynch <ntl@pobox.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/smp.c
+++ arch/powerpc/kernel/smp.c
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DBG(fmt...)
 #endif
 
-int smp_hw_index[NR_CPUS];
 struct thread_info *secondary_ti;
 
 cpumask_t cpu_possible_map = CPU_MASK_NONE;
