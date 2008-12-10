7a793d1c610e86fad06d954b1d6b7dffa428846e
Paul Mundt <lethal@linux-sh.org>
2008-12-10 20:14:15 +0900

    sh: Provide linux/regset.h interface for SH-5.
    Plugs in general and FPU regsets.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/ptrace.h
+++ arch/sh/include/asm/ptrace.h
@@ -87,6 +87,7 @@ struct pt_dspregs {
 	unsigned long	re;
 	unsigned long	mod;
 };
+#endif
 
 #define PTRACE_GETREGS		12	/* General registers */
 #define PTRACE_SETREGS		13
@@ -101,7 +102,6 @@ struct pt_dspregs {
 
 #define	PTRACE_GETDSPREGS	55	/* DSP registers */
 #define	PTRACE_SETDSPREGS	56
-#endif
 
 #ifdef __KERNEL__
 #include <asm/addrspace.h>
