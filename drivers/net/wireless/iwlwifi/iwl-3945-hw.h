5762f2630ab457305b410a1a2e5505c50cd07536
Zhu, Yi <yi.zhu@intel.com>
2008-12-11 10:33:36 -0800

    iwlwifi: use iwl_poll_direct_bit in EEPROM reading
    The patch replaces the current reading EEPROM loop iterations with
    iwl_poll_direct_bit(). It also fixes some comment error.
    
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-3945-hw.h
+++ drivers/net/wireless/iwlwifi/iwl-3945-hw.h
@@ -104,7 +104,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Driver reads 16-bit value from bits 31-16 of CSR_EEPROM_REG.
  */
 #define IWL_EEPROM_ACCESS_TIMEOUT	5000 /* uSec */
-#define IWL_EEPROM_ACCESS_DELAY		10   /* uSec */
 
 /*
  * Regulatory channel usage flags in EEPROM struct iwl_eeprom_channel.flags.
