6e2353fec83cfd078efae5f9ee7ef06b3c410ff2
Harvey Harrison <harvey.harrison@gmail.com>
2008-12-11 12:11:21 +0100

    i2c-highlander: Trivial endian casting fixes
    Fixes sparse warnings:
    drivers/i2c/busses/i2c-highlander.c:95:26: warning: incorrect type in argument 1 (different base types)
    drivers/i2c/busses/i2c-highlander.c:95:26:    expected restricted __be16 const [usertype] *p
    drivers/i2c/busses/i2c-highlander.c:95:26:    got unsigned short [usertype] *<noident>
    drivers/i2c/busses/i2c-highlander.c:106:15: warning: incorrect type in assignment (different base types)
    drivers/i2c/busses/i2c-highlander.c:106:15:    expected unsigned short [unsigned] [short] [usertype] <noident>
    drivers/i2c/busses/i2c-highlander.c:106:15:    got restricted __be16
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ben Dooks <ben-linux@fluff.org>
    Acked-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- drivers/i2c/busses/i2c-highlander.c
+++ drivers/i2c/busses/i2c-highlander.c
@@ -93,7 +93,7 @@ static void highlander_i2c_setup(struct highlander_i2c_dev *dev)
 static void smbus_write_data(u8 *src, u16 *dst, int len)
 {
 	for (; len > 1; len -= 2) {
-		*dst++ = be16_to_cpup((u16 *)src);
+		*dst++ = be16_to_cpup((__be16 *)src);
 		src += 2;
 	}
 
@@ -104,7 +104,7 @@ static void smbus_write_data(u8 *src, u16 *dst, int len)
 static void smbus_read_data(u16 *src, u8 *dst, int len)
 {
 	for (; len > 1; len -= 2) {
-		*(u16 *)dst = cpu_to_be16p(src++);
+		*(__be16 *)dst = cpu_to_be16p(src++);
 		dst += 2;
 	}
 
