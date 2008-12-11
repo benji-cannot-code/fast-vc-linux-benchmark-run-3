d19067819aa16e87770152520f511b6018a129cb
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-12-11 16:37:06 +0000

    [ARM] dma: Use sensible DMA parameters for Acorn drivers
    The hardware supports transfers up to a page boundary per buffer.
    Currently, we work around that in the DMA code by splitting each
    buffer up as we run through the scatterlist.  Avoid this by telling
    the block layers about the hardware restriction.
    
    Eventually, this will allow us to phase out the splitting code,
    but not until the old IDE layer allows us to control the value it
    gives to blk_queue_segment_boundary().
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-rpc/include/mach/isa-dma.h
+++ arch/arm/mach-rpc/include/mach/isa-dma.h
@@ -24,5 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DMA_FLOPPY		DMA_VIRTUAL_FLOPPY
 
+#define IOMD_DMA_BOUNDARY	(PAGE_SIZE - 1)
+
 #endif /* _ASM_ARCH_DMA_H */
 
