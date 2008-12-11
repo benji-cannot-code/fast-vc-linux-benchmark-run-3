36d4bf31e6e5399b0d385723df89cc455c90e0c2
Stanley.Miao <stanley.miao@windriver.com>
2008-12-10 17:36:53 -0800

    ARM: OMAP3: LDP: Add Ethernet device support to make ldp boot succeess
    Add Ethernet device support in board-ldp.c to make ldp can boot and mount
    nfs successfully.
    
    Signed-off-by: Stanley.Miao <stanley.miao@windriver.com>

--- arch/arm/plat-omap/include/mach/board-ldp.h
+++ arch/arm/plat-omap/include/mach/board-ldp.h
@@ -33,5 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void twl4030_bci_battery_init(void);
 
 #define TWL4030_IRQNUM		INT_34XX_SYS_NIRQ
-
+#define LDP_SMC911X_CS         1
+#define LDP_SMC911X_GPIO       152
+#define DEBUG_BASE             0x08000000
+#define OMAP34XX_ETHR_START    DEBUG_BASE
 #endif /* __ASM_ARCH_OMAP_LDP_H */
