cd932e2c298349f70cf86de55578c65d5928a7d6
Robert Reif <reif@earthlink.net>
2008-12-11 20:24:58 -0800

    sparc: use sparc64 version of scatterlist.h
    Use sparc64 version of scatterlist.h.
    
    There are three main differences:
        dma_addr_t replaces __u32
        dma_address replaces dvma_address
        dma_length replaces dvma_length
    
    dma_addr_t is a u32 on sparc32.
    
    Boot tested on sparc32.
    
    Signed-off-by: Robert Reif <reif@earthlink.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/scatterlist.h
+++ arch/sparc/include/asm/scatterlist.h
@@ -1,9 +1,28 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef ___ASM_SPARC_SCATTERLIST_H
-#define ___ASM_SPARC_SCATTERLIST_H
-#if defined(__sparc__) && defined(__arch64__)
-#include <asm/scatterlist_64.h>
-#else
-#include <asm/scatterlist_32.h>
+#ifndef _SPARC_SCATTERLIST_H
+#define _SPARC_SCATTERLIST_H
+
+#include <asm/page.h>
+#include <asm/types.h>
+
+struct scatterlist {
+#ifdef CONFIG_DEBUG_SG
+	unsigned long	sg_magic;
 #endif
-#endif
+	unsigned long	page_link;
+	unsigned int	offset;
+
+	unsigned int	length;
+
+	dma_addr_t	dma_address;
+	__u32		dma_length;
+};
+
+#define sg_dma_address(sg)	((sg)->dma_address)
+#define sg_dma_len(sg)     	((sg)->dma_length)
+
+#define ISA_DMA_THRESHOLD	(~0UL)
+
+#define ARCH_HAS_SG_CHAIN
+
+#endif /* !(_SPARC_SCATTERLIST_H) */
