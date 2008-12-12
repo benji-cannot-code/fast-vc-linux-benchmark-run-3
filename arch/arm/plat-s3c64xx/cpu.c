526d75c3169560a0ffdc4ffd7e835ecf9de23f2e
Ben Dooks <ben-linux@fluff.org>
2008-12-12 00:24:31 +0000

    [ARM] S3C64XX: Add generic s3c64xx sys device.
    Add an s3c64xx_sysclass and device for items that currently
    want to bind to any s3c64xx processor. The first user of this
    will be parts of the s3c64xx suspend support which need to
    save device state over suspend/resume.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-s3c64xx/cpu.c
+++ arch/arm/plat-s3c64xx/cpu.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/interrupt.h>
 #include <linux/ioport.h>
+#include <linux/sysdev.h>
 #include <linux/serial_core.h>
 #include <linux/platform_device.h>
 #include <linux/io.h>
@@ -105,6 +106,16 @@ static struct map_desc s3c_iodesc[] __initdata = {
 	},
 };
 
+
+struct sysdev_class s3c64xx_sysclass = {
+	.name	= "s3c64xx-core",
+};
+
+static struct sys_device s3c64xx_sysdev = {
+	.cls	= &s3c64xx_sysclass,
+};
+
+
 /* read cpu identification code */
 
 void __init s3c64xx_init_io(struct map_desc *mach_desc, int size)
@@ -118,3 +129,11 @@ void __init s3c64xx_init_io(struct map_desc *mach_desc, int size)
 	idcode = __raw_readl(S3C_VA_SYS + 0x118);
 	s3c_init_cpu(idcode, cpu_ids, ARRAY_SIZE(cpu_ids));
 }
+
+static __init int s3c64xx_sysdev_init(void)
+{
+	sysdev_class_register(&s3c64xx_sysclass);
+	return sysdev_register(&s3c64xx_sysdev);
+}
+
+core_initcall(s3c64xx_sysdev_init);
