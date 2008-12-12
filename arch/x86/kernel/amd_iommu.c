4150970d2390ced1b24ed030a5323a29fd203a58
Joerg Roedel <joerg.roedel@amd.com>
2008-12-12 16:13:04 +0100

    AMD IOMMU: add statistics about total number of map requests
    Impact: see total number of map requests in debugfs
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- arch/x86/kernel/amd_iommu.c
+++ arch/x86/kernel/amd_iommu.c
@@ -76,6 +76,7 @@ DECLARE_STATS_COUNTER(cross_page);
 DECLARE_STATS_COUNTER(domain_flush_single);
 DECLARE_STATS_COUNTER(domain_flush_all);
 DECLARE_STATS_COUNTER(alloced_io_mem);
+DECLARE_STATS_COUNTER(total_map_requests);
 
 static struct dentry *stats_dir;
 static struct dentry *de_isolate;
@@ -113,6 +114,7 @@ static void amd_iommu_stats_init(void)
 	amd_iommu_stats_add(&domain_flush_single);
 	amd_iommu_stats_add(&domain_flush_all);
 	amd_iommu_stats_add(&alloced_io_mem);
+	amd_iommu_stats_add(&total_map_requests);
 }
 
 #endif
@@ -1229,6 +1231,8 @@ static dma_addr_t __map_single(struct device *dev,
 
 	pages = iommu_num_pages(paddr, size, PAGE_SIZE);
 	paddr &= PAGE_MASK;
+
+	INC_STATS_COUNTER(total_map_requests);
 
 	if (pages > 1)
 		INC_STATS_COUNTER(cross_page);
