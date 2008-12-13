19daec403ddbb08388f0dbf8b800458fa16f434d
Ben Hutchings <bhutchings@solarflare.com>
2008-12-12 22:06:54 -0800

    sfc: Use model numbers for PHY type names
    Some of the PHY type names are overly generic.  Change them to include
    the model numbers of the PHYs they represent.
    
    Correct the model number reference at the top of xfp_phy.c.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/xfp_phy.c
+++ drivers/net/sfc/xfp_phy.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * by the Free Software Foundation, incorporated herein by reference.
  */
 /*
- * Driver for XFP optical PHYs (plus some support specific to the Quake 2032)
+ * Driver for XFP optical PHYs (plus some support specific to the Quake 2022/32)
  * See www.amcc.com for details (search for qt2032)
  */
 
