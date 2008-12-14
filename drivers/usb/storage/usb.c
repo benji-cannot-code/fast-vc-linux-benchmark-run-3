3b0846214e1ba42bdf08059fa30ba3232b4d13be
Dan Williams <dcbw@redhat.com>
2008-12-14 12:39:22 -0500

    USB: unusual dev for Option N.V. ZeroCD modems
    Many newer Option mobile broadband devices initially provide a
    usb-storage "driver CD" device that's pretty useless on Linux since
    any software on it most likely wouldn't be compatible with your
    kernel or distro anyway.  Thus, by default just kill the driver
    CD device by sending the SCSI 'rezero' command, but allow override
    of the default behavior via usb-storage module parameter so users
    can keep the ZeroCD device if they really want to.  Inspired by
    the Sierra TruInstall patch.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Cc: Peter Henn <p.henn@option.com
    Cc: Denis Joseph Barrow <D.Barow@option.com>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/storage/usb.c
+++ drivers/usb/storage/usb.c
@@ -101,6 +101,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cypress_atacb.h"
 #endif
 #include "sierra_ms.h"
+#include "option_ms.h"
 
 /* Some informational data */
 MODULE_AUTHOR("Matthew Dharm <mdharm-usb@one-eyed-alien.net>");
