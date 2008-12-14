f90bd9e3aab41813404e6b93aed0a4a5a2c27cd8
Ben Dooks <ben-linux@fluff.org>
2008-12-14 22:33:02 +0000

    [ARM] S3C: Remove unnecessary <linux/delay.h> includes
    As per Russell King's last review comment, find and remove
    all unnecessary includes of <linux/delay.h> in the files
    that do not need them.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c2412/mach-jive.c
+++ arch/arm/mach-s3c2412/mach-jive.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <linux/init.h>
 #include <linux/sysdev.h>
-#include <linux/delay.h>
 #include <linux/serial_core.h>
 #include <linux/platform_device.h>
 #include <linux/i2c.h>
