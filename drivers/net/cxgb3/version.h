b8f8d6564f73d2d4f04176de3f4addd667f8bf0f
Divy Le Ray <divy@chelsio.com>
2008-12-16 01:09:39 -0800

    cxgb3: Add multiple Tx queue support.
    Implement NIC Tx multiqueue.
    Bump up driver version.
    
    Signed-off-by: Divy Le Ray <divy@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/cxgb3/version.h
+++ drivers/net/cxgb3/version.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRV_DESC "Chelsio T3 Network Driver"
 #define DRV_NAME "cxgb3"
 /* Driver version */
-#define DRV_VERSION "1.1.0-ko"
+#define DRV_VERSION "1.1.1-ko"
 
 /* Firmware version */
 #define FW_VERSION_MAJOR 7
