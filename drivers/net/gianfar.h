eb0a6f2a960f94b5f5a32daade39e52eb1f7534a
Dai Haruki <dai.haruki@freescale.com>
2008-12-16 15:34:50 -0800

    gianfar: Make all BD status writes 32-bit
    Whenever we want to update the status field in a BD, we usually want to
    update the length field, too.  By combining them into one 32-bit field, we
    reduce the number of stores to memory shared with the controller, and we
    eliminate the need for order-enforcement, as the length and "READY" bit are
    now updated atomically at the same time.
    
    Signed-off-by: Dai Haruki <Dai.Haruki@freescale.com>
    Signed-off-by: Andy Fleming <afleming@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/gianfar.h
+++ drivers/net/gianfar.h
@@ -312,6 +312,8 @@ extern const char gfar_driver_version[];
 #define ATTRELI_EI_MASK		0x00003fff
 #define ATTRELI_EI(x) (x)
 
+#define BD_LFLAG(flags) ((flags) << 16)
+#define BD_LENGTH_MASK		0x00ff
 
 /* TxBD status field bits */
 #define TXBD_READY		0x8000
@@ -375,8 +377,13 @@ extern const char gfar_driver_version[];
 
 struct txbd8
 {
-	u16	status;	/* Status Fields */
-	u16	length;	/* Buffer length */
+	union {
+		struct {
+			u16	status;	/* Status Fields */
+			u16	length;	/* Buffer length */
+		};
+		u32 lstatus;
+	};
 	u32	bufPtr;	/* Buffer Pointer */
 };
 
@@ -391,8 +398,13 @@ struct txfcb {
 
 struct rxbd8
 {
-	u16	status;	/* Status Fields */
-	u16	length;	/* Buffer Length */
+	union {
+		struct {
+			u16	status;	/* Status Fields */
+			u16	length;	/* Buffer Length */
+		};
+		u32 lstatus;
+	};
 	u32	bufPtr;	/* Buffer Pointer */
 };
 
