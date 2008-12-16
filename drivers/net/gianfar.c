90bdf010ad104daef1e3e7120dbf29f1fcd773df
Dai Haruki <dai.haruki@freescale.com>
2008-12-16 15:30:20 -0800

    gianfar: Fix eTSEC configuration procedure
    Fix some bugs in the ethtool configuration functions:
    * gfar_clean_rx_ring should not be called with interrupts disabled.
    * Update last transmission time to avoid tx timeout.
    * Delete redundant NETIF_F_IP_CSUM check in gfar_start_xmit
    * Use netif_tx_lock_bh when reconfiguring the tx csum
    
    Signed-off-by: Dai Haruki <dai.haruki@freescale.com>
    Signed-off-by: Andy Fleming <afleming@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/gianfar.c
+++ drivers/net/gianfar.c
@@ -893,6 +893,8 @@ void gfar_start(struct net_device *dev)
 
 	/* Unmask the interrupts we look for */
 	gfar_write(&regs->imask, IMASK_DEFAULT);
+
+	dev->trans_start = jiffies;
 }
 
 /* Bring the controller up and running */
@@ -1234,8 +1236,7 @@ static int gfar_start_xmit(struct sk_buff *skb, struct net_device *dev)
 	status = txbdp->status & TXBD_WRAP;
 
 	/* Set up checksumming */
-	if (likely((dev->features & NETIF_F_IP_CSUM)
-			&& (CHECKSUM_PARTIAL == skb->ip_summed))) {
+	if (CHECKSUM_PARTIAL == skb->ip_summed) {
 		fcb = gfar_add_fcb(skb, txbdp);
 		status |= TXBD_TOE;
 		gfar_tx_checksum(skb, fcb);
