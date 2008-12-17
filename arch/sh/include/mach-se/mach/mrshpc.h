2ec4651dc1426879bb2842a2fe0c501010cf2307
Paul Mundt <lethal@linux-sh.org>
2008-12-17 14:58:04 +0900

    sh: mrshpc_setup_windows() needs to be inline.
    While no one should be including this file multiple times, flag it
    inline anyways just in case.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/mach-se/mach/mrshpc.h
+++ arch/sh/include/mach-se/mach/mrshpc.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/io.h>
 
-static void __init mrshpc_setup_windows(void)
+static inline void __init mrshpc_setup_windows(void)
 {
 	if ((__raw_readw(MRSHPC_CSR) & 0x000c) != 0)
 		return;	/* Not detected */
