74be27f8df5561428caceae809e42d0d5a4bf419
Paul Mundt <lethal@linux-sh.org>
2008-12-17 12:17:20 +0900

    sh: Kill off the cayman and microdev special heartbeat code.
    These can use the generic code instead.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-microdev/setup.c
+++ arch/sh/boards/mach-microdev/setup.c
@@ -19,12 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/machvec.h>
 
-extern void microdev_heartbeat(void);
-
-
-/****************************************************************************/
-
-
 	/*
 	 * Setup for the SMSC FDC37C93xAPM
 	 */
@@ -399,8 +393,4 @@ static struct sh_machine_vector mv_sh4202_microdev __initmv = {
 	.mv_outsl		= microdev_outsl,
 
 	.mv_init_irq		= init_microdev_irq,
-
-#ifdef CONFIG_HEARTBEAT
-	.mv_heartbeat		= microdev_heartbeat,
-#endif
 };
