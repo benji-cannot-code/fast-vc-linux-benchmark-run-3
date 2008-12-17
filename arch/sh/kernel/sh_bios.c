9cb0c8bde17fb8b60e0a44c2e690ceb03d476413
Paul Mundt <lethal@linux-sh.org>
2008-12-17 11:47:59 +0900

    sh: Kill off sh_bios_in_gdb_mode().
    With the reworked kgdb support, we always detach and reinitialize the
    stub. This was mostly a feature for handoffs between sh-ipl+g and the
    kgdb stub, but virtually no sh-ipl+g versions ever had this working
    right in the first place.
    
    Given that the sh-ipl+g stubs in general use today don't even support
    the GDB stub, and we have already killed off the special casing in the
    sh-sci serial driver, kill off this now unused symbol too.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/sh_bios.c
+++ arch/sh/kernel/sh_bios.c
@@ -10,11 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sh_bios.h>
 
 #define BIOS_CALL_CONSOLE_WRITE		0
-#define BIOS_CALL_READ_BLOCK		1
 #define BIOS_CALL_ETH_NODE_ADDR		10
 #define BIOS_CALL_SHUTDOWN		11
 #define BIOS_CALL_CHAR_OUT		0x1f	/* TODO: hack */
-#define BIOS_CALL_GDB_GET_MODE_PTR	0xfe
 #define BIOS_CALL_GDB_DETACH		0xff
 
 static inline long sh_bios_call(long func, long arg0, long arg1, long arg2,
@@ -40,21 +38,6 @@ void sh_bios_console_write(const char *buf, unsigned int len)
 void sh_bios_char_out(char ch)
 {
 	sh_bios_call(BIOS_CALL_CHAR_OUT, ch, 0, 0, 0);
-}
-
-int sh_bios_in_gdb_mode(void)
-{
-	static char queried = 0;
-	static char *gdb_mode_p = 0;
-
-	if (!queried) {
-		/* Query the gdb stub for address of its gdb mode variable */
-		long r = sh_bios_call(BIOS_CALL_GDB_GET_MODE_PTR, 0, 0, 0, 0);
-		if (r != ~0)	/* BIOS returns -1 for unknown function */
-			gdb_mode_p = (char *)r;
-		queried = 1;
-	}
-	return (gdb_mode_p != 0 ? *gdb_mode_p : 0);
 }
 
 void sh_bios_gdb_detach(void)
