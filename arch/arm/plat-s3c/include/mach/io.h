977e828daea4d4cd33da54e8987c9c3b6f857aa5
Russell King <rmk@dyn-67.arm.linux.org.uk>
2008-12-18 23:29:15 +0000

    [ARM] s3c: define __io using the typesafe version
    as per 0560cf5aa51216b06874333a2fa26ca034d97bdb
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/plat-s3c/include/mach/io.h
+++ arch/arm/plat-s3c/include/mach/io.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARM_ARCH_IO_H
 
 /* No current ISA/PCI bus support. */
-#define __io(a)		((void __iomem *)(a))
+#define __io(a)		__typesafe_io(a)
 #define __mem_pci(a)	(a)
 
 #define IO_SPACE_LIMIT (0xFFFFFFFF)
