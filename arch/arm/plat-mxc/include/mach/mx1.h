3c09feeba43e079b5f5bdaf95b9c71596b857ca6
Sascha Hauer <s.hauer@pengutronix.de>
2008-12-18 11:24:03 +0100

    [ARM] MXC: do not include mach/hardware.h from mach/timex.h
    Instead of including other header files, define CLOCK_TICK_RATE directly
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/mx1.h
+++ arch/arm/plat-mxc/include/mach/mx1.h
@@ -19,11 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/vmalloc.h>
 
-/*
- * defines the hardware clock tick rate
- */
-#define CLOCK_TICK_RATE	16000000
-
 #define PHYS_OFFSET	UL(0x08000000)
 
 /*
--- arch/arm/plat-mxc/include/mach/mx31.h
+++ arch/arm/plat-mxc/include/mach/mx31.h
@@ -16,11 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #error "Do not include directly."
 #endif
 
-/*!
- * defines the hardware clock tick rate
- */
-#define CLOCK_TICK_RATE		16625000
-
 /*
  * MX31 memory map:
  *
