d96e13c0d2a20df6037f1ce8f44fcf0e0c696836
Anton Vorontsov <avorontsov@ru.mvista.com>
2008-12-18 08:23:29 +0000

    ucc_geth: Fix IRQ freeing code in ucc_geth_open()
    open() routine calls stop() in case of errors, the function will try
    to free the requested IRQ. But we don't know if it was actually
    requested, so the code might issue bogus free_irq(0, dev) call.
    
    Fix this by rearranging the code so that now request_irq() is the last
    call in the open() routine, and move free_irq() into the close().
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ucc_geth.c
+++ drivers/net/ucc_geth.c
@@ -2301,8 +2301,6 @@ static void ucc_geth_stop(struct ucc_geth_private *ugeth)
 	tempval &= ~(MACCFG1_ENABLE_RX | MACCFG1_ENABLE_TX);
 	out_be32(&ug_regs->maccfg1, tempval);
 
-	free_irq(ugeth->ug_info->uf_info.irq, ugeth->dev);
-
 	ucc_geth_memclean(ugeth);
 }
 
@@ -3760,21 +3758,20 @@ static int ucc_geth_open(struct net_device *dev)
 
 	phy_start(ugeth->phydev);
 
-	err =
-	    request_irq(ugeth->ug_info->uf_info.irq, ucc_geth_irq_handler, 0,
-			"UCC Geth", dev);
-	if (err) {
-		if (netif_msg_ifup(ugeth))
-			ugeth_err("%s: Cannot get IRQ for net device, aborting.",
-				  dev->name);
-		ucc_geth_stop(ugeth);
-		goto out_err;
-	}
-
 	err = ugeth_enable(ugeth, COMM_DIR_RX_AND_TX);
 	if (err) {
 		if (netif_msg_ifup(ugeth))
 			ugeth_err("%s: Cannot enable net device, aborting.", dev->name);
+		ucc_geth_stop(ugeth);
+		goto out_err;
+	}
+
+	err = request_irq(ugeth->ug_info->uf_info.irq, ucc_geth_irq_handler,
+			  0, "UCC Geth", dev);
+	if (err) {
+		if (netif_msg_ifup(ugeth))
+			ugeth_err("%s: Cannot get IRQ for net device, aborting.",
+				  dev->name);
 		ucc_geth_stop(ugeth);
 		goto out_err;
 	}
@@ -3799,6 +3796,8 @@ static int ucc_geth_close(struct net_device *dev)
 	napi_disable(&ugeth->napi);
 
 	ucc_geth_stop(ugeth);
+
+	free_irq(ugeth->ug_info->uf_info.irq, ugeth->dev);
 
 	phy_disconnect(ugeth->phydev);
 	ugeth->phydev = NULL;
