55a4afeafc6f476672f1b0bf4f7a11f8b40cea62
Hannes Eder <hannes@hanneseder.net>
2008-12-18 21:22:24 +0100

    drm/i915: fix sparse warnings: move 'extern' decls to header file
    Move 'extern'-decls from "intel_dvo.c" to "dvo.h", as "dvo.h" is
    included by and only by files where the symbols are either defined or
    used.
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/gpu/drm/i915/intel_dvo.c
+++ drivers/gpu/drm/i915/intel_dvo.c
@@ -38,12 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CH7xxx_ADDR	0x76
 #define TFP410_ADDR	0x38
 
-extern struct intel_dvo_dev_ops sil164_ops;
-extern struct intel_dvo_dev_ops ch7xxx_ops;
-extern struct intel_dvo_dev_ops ivch_ops;
-extern struct intel_dvo_dev_ops tfp410_ops;
-extern struct intel_dvo_dev_ops ch7017_ops;
-
 static struct intel_dvo_device intel_dvo_devices[] = {
 	{
 		.type = INTEL_DVO_CHIP_TMDS,
