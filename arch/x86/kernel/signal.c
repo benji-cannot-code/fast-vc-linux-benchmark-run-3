1ee7f6d2fc8909f926f3dcb1b986fa7f47aa4e2a
Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
2008-12-17 18:50:32 -0800

    x86: signal: move sigframe.h to arch/x86/include/asm
    Impact: cleanup, move header file
    
    Move arch/x86/kernel/sigframe.h to arch/x86/include/asm/sigframe.h.
    It will be used in arch/x86/ia32/ia32_signal.c.
    
    Signed-off-by: Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/signal.c
+++ arch/x86/kernel/signal.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/syscall.h>
 #include <asm/syscalls.h>
 
-#include "sigframe.h"
+#include <asm/sigframe.h>
 
 #define _BLOCKABLE (~(sigmask(SIGKILL) | sigmask(SIGSTOP)))
 
