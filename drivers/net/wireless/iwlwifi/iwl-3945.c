332c35cffacf281ff09b9f9dbf494e06b8009eb0
Tomas Winkler <tomas.winkler@intel.com>
2008-12-19 10:37:04 +0800

    iwlwifi: use iwl-commands.h also in 3945
    This patch uses iwl-commands.h also for iwl3945
    more clean ups are required but this get to stage where it
    compiles cleanly.
    Most massive changes are in spectrum and power managment.
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Acked-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-3945.c
+++ drivers/net/wireless/iwlwifi/iwl-3945.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "iwl-3945-core.h"
 #include "iwl-3945-fh.h"
+#include "iwl-commands.h"
 #include "iwl-3945-commands.h"
 #include "iwl-3945.h"
 #include "iwl-helpers.h"
