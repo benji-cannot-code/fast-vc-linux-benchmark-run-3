2594fe640d0feee8ed763bdf6106c0f81d976120
Tomas Winkler <tomas.winkler@intel.com>
2008-12-19 10:37:07 +0800

    iwlwifi: 3945 remove iwl-3945-commands.h
    This patch remove iwl-3945-commands.h
    eliminating duplicated and moving all definitions to iwl-commands.h
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Acked-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-3945-led.c
+++ drivers/net/wireless/iwlwifi/iwl-3945-led.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/unaligned.h>
 
 #include "iwl-commands.h"
-#include "iwl-3945-commands.h"
 #include "iwl-3945.h"
 
 
