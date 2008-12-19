05d0835bce4568b172813e23566c3ddf23e41bae
Abhijeet Kolekar <abhijeet.kolekar@intel.com>
2008-12-19 10:37:28 +0800

    iwl3945: replaces iwl3945_priv with iwl_priv
    The patch replaces iwl3945_priv to iwl_priv. It adds 3945 specific
    data members to iwl_priv.
    
    Signed-off-by: Abhijeet Kolekar <abhijeet.kolekar@intel.com>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-3945-io.h
+++ drivers/net/wireless/iwlwifi/iwl-3945-io.h
@@ -62,7 +62,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define _iwl3945_write32(priv, ofs, val) iowrite32((val), (priv)->hw_base + (ofs))
 #ifdef CONFIG_IWL3945_DEBUG
-static inline void __iwl3945_write32(const char *f, u32 l, struct iwl3945_priv *priv,
+static inline void __iwl3945_write32(const char *f, u32 l, struct iwl_priv *priv,
 				 u32 ofs, u32 val)
 {
 	IWL_DEBUG_IO("write32(0x%08X, 0x%08X) - %s %d\n", ofs, val, f, l);
@@ -76,7 +76,7 @@ static inline void __iwl3945_write32(const char *f, u32 l, struct iwl3945_priv *
 
 #define _iwl3945_read32(priv, ofs) ioread32((priv)->hw_base + (ofs))
 #ifdef CONFIG_IWL3945_DEBUG
-static inline u32 __iwl3945_read32(char *f, u32 l, struct iwl3945_priv *priv, u32 ofs)
+static inline u32 __iwl3945_read32(char *f, u32 l, struct iwl_priv *priv, u32 ofs)
 {
 	IWL_DEBUG_IO("read_direct32(0x%08X) - %s %d\n", ofs, f, l);
 	return _iwl3945_read32(priv, ofs);
@@ -86,7 +86,7 @@ static inline u32 __iwl3945_read32(char *f, u32 l, struct iwl3945_priv *priv, u3
 #define iwl3945_read32(p, o) _iwl3945_read32(p, o)
 #endif
 
-static inline int _iwl3945_poll_bit(struct iwl3945_priv *priv, u32 addr,
+static inline int _iwl3945_poll_bit(struct iwl_priv *priv, u32 addr,
 				u32 bits, u32 mask, int timeout)
 {
 	int i = 0;
@@ -102,7 +102,7 @@ static inline int _iwl3945_poll_bit(struct iwl3945_priv *priv, u32 addr,
 }
 #ifdef CONFIG_IWL3945_DEBUG
 static inline int __iwl3945_poll_bit(const char *f, u32 l,
-				 struct iwl3945_priv *priv, u32 addr,
+				 struct iwl_priv *priv, u32 addr,
 				 u32 bits, u32 mask, int timeout)
 {
 	int ret = _iwl3945_poll_bit(priv, addr, bits, mask, timeout);
@@ -117,13 +117,13 @@ static inline int __iwl3945_poll_bit(const char *f, u32 l,
 #define iwl3945_poll_bit(p, a, b, m, t) _iwl3945_poll_bit(p, a, b, m, t)
 #endif
 
-static inline void _iwl3945_set_bit(struct iwl3945_priv *priv, u32 reg, u32 mask)
+static inline void _iwl3945_set_bit(struct iwl_priv *priv, u32 reg, u32 mask)
 {
 	_iwl3945_write32(priv, reg, _iwl3945_read32(priv, reg) | mask);
 }
 #ifdef CONFIG_IWL3945_DEBUG
 static inline void __iwl3945_set_bit(const char *f, u32 l,
-				 struct iwl3945_priv *priv, u32 reg, u32 mask)
+				 struct iwl_priv *priv, u32 reg, u32 mask)
 {
 	u32 val = _iwl3945_read32(priv, reg) | mask;
 	IWL_DEBUG_IO("set_bit(0x%08X, 0x%08X) = 0x%08X\n", reg, mask, val);
@@ -134,13 +134,13 @@ static inline void __iwl3945_set_bit(const char *f, u32 l,
 #define iwl3945_set_bit(p, r, m) _iwl3945_set_bit(p, r, m)
 #endif
 
-static inline void _iwl3945_clear_bit(struct iwl3945_priv *priv, u32 reg, u32 mask)
+static inline void _iwl3945_clear_bit(struct iwl_priv *priv, u32 reg, u32 mask)
 {
 	_iwl3945_write32(priv, reg, _iwl3945_read32(priv, reg) & ~mask);
 }
 #ifdef CONFIG_IWL3945_DEBUG
 static inline void __iwl3945_clear_bit(const char *f, u32 l,
-				   struct iwl3945_priv *priv, u32 reg, u32 mask)
+				   struct iwl_priv *priv, u32 reg, u32 mask)
 {
 	u32 val = _iwl3945_read32(priv, reg) & ~mask;
 	IWL_DEBUG_IO("clear_bit(0x%08X, 0x%08X) = 0x%08X\n", reg, mask, val);
@@ -151,7 +151,7 @@ static inline void __iwl3945_clear_bit(const char *f, u32 l,
 #define iwl3945_clear_bit(p, r, m) _iwl3945_clear_bit(p, r, m)
 #endif
 
-static inline int _iwl3945_grab_nic_access(struct iwl3945_priv *priv)
+static inline int _iwl3945_grab_nic_access(struct iwl_priv *priv)
 {
 	int ret;
 #ifdef CONFIG_IWL3945_DEBUG
@@ -177,7 +177,7 @@ static inline int _iwl3945_grab_nic_access(struct iwl3945_priv *priv)
 
 #ifdef CONFIG_IWL3945_DEBUG
 static inline int __iwl3945_grab_nic_access(const char *f, u32 l,
-					       struct iwl3945_priv *priv)
+					       struct iwl_priv *priv)
 {
 	if (atomic_read(&priv->restrict_refcnt))
 		IWL_DEBUG_INFO("Grabbing access while already held at "
@@ -193,7 +193,7 @@ static inline int __iwl3945_grab_nic_access(const char *f, u32 l,
 	_iwl3945_grab_nic_access(priv)
 #endif
 
-static inline void _iwl3945_release_nic_access(struct iwl3945_priv *priv)
+static inline void _iwl3945_release_nic_access(struct iwl_priv *priv)
 {
 #ifdef CONFIG_IWL3945_DEBUG
 	if (atomic_dec_and_test(&priv->restrict_refcnt))
@@ -203,7 +203,7 @@ static inline void _iwl3945_release_nic_access(struct iwl3945_priv *priv)
 }
 #ifdef CONFIG_IWL3945_DEBUG
 static inline void __iwl3945_release_nic_access(const char *f, u32 l,
-					    struct iwl3945_priv *priv)
+					    struct iwl_priv *priv)
 {
 	if (atomic_read(&priv->restrict_refcnt) <= 0)
 		IWL_ERROR("Release unheld nic access at line %d.\n", l);
@@ -218,13 +218,13 @@ static inline void __iwl3945_release_nic_access(const char *f, u32 l,
 	_iwl3945_release_nic_access(priv)
 #endif
 
-static inline u32 _iwl3945_read_direct32(struct iwl3945_priv *priv, u32 reg)
+static inline u32 _iwl3945_read_direct32(struct iwl_priv *priv, u32 reg)
 {
 	return _iwl3945_read32(priv, reg);
 }
 #ifdef CONFIG_IWL3945_DEBUG
 static inline u32 __iwl3945_read_direct32(const char *f, u32 l,
-					struct iwl3945_priv *priv, u32 reg)
+					struct iwl_priv *priv, u32 reg)
 {
 	u32 value = _iwl3945_read_direct32(priv, reg);
 	if (!atomic_read(&priv->restrict_refcnt))
@@ -239,14 +239,14 @@ static inline u32 __iwl3945_read_direct32(const char *f, u32 l,
 #define iwl3945_read_direct32 _iwl3945_read_direct32
 #endif
 
-static inline void _iwl3945_write_direct32(struct iwl3945_priv *priv,
+static inline void _iwl3945_write_direct32(struct iwl_priv *priv,
 					 u32 reg, u32 value)
 {
 	_iwl3945_write32(priv, reg, value);
 }
 #ifdef CONFIG_IWL3945_DEBUG
 static void __iwl3945_write_direct32(u32 line,
-				   struct iwl3945_priv *priv, u32 reg, u32 value)
+				   struct iwl_priv *priv, u32 reg, u32 value)
 {
 	if (!atomic_read(&priv->restrict_refcnt))
 		IWL_ERROR("Nic access not held from line %d\n", line);
@@ -258,7 +258,7 @@ static void __iwl3945_write_direct32(u32 line,
 #define iwl3945_write_direct32 _iwl3945_write_direct32
 #endif
 
-static inline void iwl3945_write_reg_buf(struct iwl3945_priv *priv,
+static inline void iwl3945_write_reg_buf(struct iwl_priv *priv,
 					       u32 reg, u32 len, u32 *values)
 {
 	u32 count = sizeof(u32);
@@ -269,7 +269,7 @@ static inline void iwl3945_write_reg_buf(struct iwl3945_priv *priv,
 	}
 }
 
-static inline int _iwl3945_poll_direct_bit(struct iwl3945_priv *priv,
+static inline int _iwl3945_poll_direct_bit(struct iwl_priv *priv,
 					   u32 addr, u32 mask, int timeout)
 {
 	return _iwl3945_poll_bit(priv, addr, mask, mask, timeout);
@@ -277,7 +277,7 @@ static inline int _iwl3945_poll_direct_bit(struct iwl3945_priv *priv,
 
 #ifdef CONFIG_IWL3945_DEBUG
 static inline int __iwl3945_poll_direct_bit(const char *f, u32 l,
-					    struct iwl3945_priv *priv,
+					    struct iwl_priv *priv,
 					    u32 addr, u32 mask, int timeout)
 {
 	int ret  = _iwl3945_poll_direct_bit(priv, addr, mask, timeout);
@@ -296,14 +296,14 @@ static inline int __iwl3945_poll_direct_bit(const char *f, u32 l,
 #define iwl3945_poll_direct_bit _iwl3945_poll_direct_bit
 #endif
 
-static inline u32 _iwl3945_read_prph(struct iwl3945_priv *priv, u32 reg)
+static inline u32 _iwl3945_read_prph(struct iwl_priv *priv, u32 reg)
 {
 	_iwl3945_write_direct32(priv, HBUS_TARG_PRPH_RADDR, reg | (3 << 24));
 	rmb();
 	return _iwl3945_read_direct32(priv, HBUS_TARG_PRPH_RDAT);
 }
 #ifdef CONFIG_IWL3945_DEBUG
-static inline u32 __iwl3945_read_prph(u32 line, struct iwl3945_priv *priv, u32 reg)
+static inline u32 __iwl3945_read_prph(u32 line, struct iwl_priv *priv, u32 reg)
 {
 	if (!atomic_read(&priv->restrict_refcnt))
 		IWL_ERROR("Nic access not held from line %d\n", line);
@@ -316,7 +316,7 @@ static inline u32 __iwl3945_read_prph(u32 line, struct iwl3945_priv *priv, u32 r
 #define iwl3945_read_prph _iwl3945_read_prph
 #endif
 
-static inline void _iwl3945_write_prph(struct iwl3945_priv *priv,
+static inline void _iwl3945_write_prph(struct iwl_priv *priv,
 					     u32 addr, u32 val)
 {
 	_iwl3945_write_direct32(priv, HBUS_TARG_PRPH_WADDR,
@@ -325,7 +325,7 @@ static inline void _iwl3945_write_prph(struct iwl3945_priv *priv,
 	_iwl3945_write_direct32(priv, HBUS_TARG_PRPH_WDAT, val);
 }
 #ifdef CONFIG_IWL3945_DEBUG
-static inline void __iwl3945_write_prph(u32 line, struct iwl3945_priv *priv,
+static inline void __iwl3945_write_prph(u32 line, struct iwl_priv *priv,
 					      u32 addr, u32 val)
 {
 	if (!atomic_read(&priv->restrict_refcnt))
@@ -342,7 +342,7 @@ static inline void __iwl3945_write_prph(u32 line, struct iwl3945_priv *priv,
 #define _iwl3945_set_bits_prph(priv, reg, mask) \
 	_iwl3945_write_prph(priv, reg, (_iwl3945_read_prph(priv, reg) | mask))
 #ifdef CONFIG_IWL3945_DEBUG
-static inline void __iwl3945_set_bits_prph(u32 line, struct iwl3945_priv *priv,
+static inline void __iwl3945_set_bits_prph(u32 line, struct iwl_priv *priv,
 					u32 reg, u32 mask)
 {
 	if (!atomic_read(&priv->restrict_refcnt))
@@ -361,7 +361,7 @@ static inline void __iwl3945_set_bits_prph(u32 line, struct iwl3945_priv *priv,
 
 #ifdef CONFIG_IWL3945_DEBUG
 static inline void __iwl3945_set_bits_mask_prph(u32 line,
-		struct iwl3945_priv *priv, u32 reg, u32 bits, u32 mask)
+		struct iwl_priv *priv, u32 reg, u32 bits, u32 mask)
 {
 	if (!atomic_read(&priv->restrict_refcnt))
 		IWL_ERROR("Nic access not held from line %d\n", line);
@@ -373,28 +373,28 @@ static inline void __iwl3945_set_bits_mask_prph(u32 line,
 #define iwl3945_set_bits_mask_prph _iwl3945_set_bits_mask_prph
 #endif
 
-static inline void iwl3945_clear_bits_prph(struct iwl3945_priv
+static inline void iwl3945_clear_bits_prph(struct iwl_priv
 						 *priv, u32 reg, u32 mask)
 {
 	u32 val = _iwl3945_read_prph(priv, reg);
 	_iwl3945_write_prph(priv, reg, (val & ~mask));
 }
 
-static inline u32 iwl3945_read_targ_mem(struct iwl3945_priv *priv, u32 addr)
+static inline u32 iwl3945_read_targ_mem(struct iwl_priv *priv, u32 addr)
 {
 	iwl3945_write_direct32(priv, HBUS_TARG_MEM_RADDR, addr);
 	rmb();
 	return iwl3945_read_direct32(priv, HBUS_TARG_MEM_RDAT);
 }
 
-static inline void iwl3945_write_targ_mem(struct iwl3945_priv *priv, u32 addr, u32 val)
+static inline void iwl3945_write_targ_mem(struct iwl_priv *priv, u32 addr, u32 val)
 {
 	iwl3945_write_direct32(priv, HBUS_TARG_MEM_WADDR, addr);
 	wmb();
 	iwl3945_write_direct32(priv, HBUS_TARG_MEM_WDAT, val);
 }
 
-static inline void iwl3945_write_targ_mem_buf(struct iwl3945_priv *priv, u32 addr,
+static inline void iwl3945_write_targ_mem_buf(struct iwl_priv *priv, u32 addr,
 					  u32 len, u32 *values)
 {
 	iwl3945_write_direct32(priv, HBUS_TARG_MEM_WADDR, addr);
--- drivers/net/wireless/iwlwifi/iwl-3945-rs.c
+++ drivers/net/wireless/iwlwifi/iwl-3945-rs.c
@@ -53,7 +53,7 @@ struct iwl3945_rate_scale_data {
 
 struct iwl3945_rs_sta {
 	spinlock_t lock;
-	struct iwl3945_priv *priv;
+	struct iwl_priv *priv;
 	s32 *expected_tpt;
 	unsigned long last_partial_flush;
 	unsigned long last_flush;
@@ -184,7 +184,7 @@ static int iwl3945_rate_scale_flush_windows(struct iwl3945_rs_sta *rs_sta)
 	int unflushed = 0;
 	int i;
 	unsigned long flags;
-	struct iwl3945_priv *priv = rs_sta->priv;
+	struct iwl_priv *priv = rs_sta->priv;
 
 	/*
 	 * For each rate, if we have collected data on that rate
@@ -217,7 +217,7 @@ static int iwl3945_rate_scale_flush_windows(struct iwl3945_rs_sta *rs_sta)
 static void iwl3945_bg_rate_scale_flush(unsigned long data)
 {
 	struct iwl3945_rs_sta *rs_sta = (void *)data;
-	struct iwl3945_priv *priv = rs_sta->priv;
+	struct iwl_priv *priv = rs_sta->priv;
 	int unflushed = 0;
 	unsigned long flags;
 	u32 packet_count, duration, pps;
@@ -291,7 +291,7 @@ static void iwl3945_collect_tx_data(struct iwl3945_rs_sta *rs_sta,
 {
 	unsigned long flags;
 	s32 fail_count;
-	struct iwl3945_priv *priv = rs_sta->priv;
+	struct iwl_priv *priv = rs_sta->priv;
 
 	if (!retries) {
 		IWL_DEBUG_RATE("leave: retries == 0 -- should be at least 1\n");
@@ -345,7 +345,7 @@ static void rs_rate_init(void *priv_r, struct ieee80211_supported_band *sband,
 			 struct ieee80211_sta *sta, void *priv_sta)
 {
 	struct iwl3945_rs_sta *rs_sta = priv_sta;
-	struct iwl3945_priv *priv = (struct iwl3945_priv *)priv_r;
+	struct iwl_priv *priv = (struct iwl_priv *)priv_r;
 	int i;
 
 	IWL_DEBUG_RATE("enter\n");
@@ -389,7 +389,7 @@ static void *rs_alloc_sta(void *iwl_priv, struct ieee80211_sta *sta, gfp_t gfp)
 {
 	struct iwl3945_rs_sta *rs_sta;
 	struct iwl3945_sta_priv *psta = (void *) sta->drv_priv;
-	struct iwl3945_priv *priv = iwl_priv;
+	struct iwl_priv *priv = iwl_priv;
 	int i;
 
 	/*
@@ -439,7 +439,7 @@ static void rs_free_sta(void *iwl_priv, struct ieee80211_sta *sta,
 {
 	struct iwl3945_sta_priv *psta = (void *) sta->drv_priv;
 	struct iwl3945_rs_sta *rs_sta = priv_sta;
-	struct iwl3945_priv *priv = rs_sta->priv;
+	struct iwl_priv *priv = rs_sta->priv;
 
 	psta->rs_sta = NULL;
 
@@ -453,7 +453,7 @@ static void rs_free_sta(void *iwl_priv, struct ieee80211_sta *sta,
 /**
  * rs_tx_status - Update rate control values based on Tx results
  *
- * NOTE: Uses iwl3945_priv->retry_rate for the # of retries attempted by
+ * NOTE: Uses iwl_priv->retry_rate for the # of retries attempted by
  * the hardware for each rate.
  */
 static void rs_tx_status(void *priv_rate, struct ieee80211_supported_band *sband,
@@ -463,7 +463,7 @@ static void rs_tx_status(void *priv_rate, struct ieee80211_supported_band *sband
 	s8 retries = 0, current_count;
 	int scale_rate_index, first_index, last_index;
 	unsigned long flags;
-	struct iwl3945_priv *priv = (struct iwl3945_priv *)priv_rate;
+	struct iwl_priv *priv = (struct iwl_priv *)priv_rate;
 	struct iwl3945_rs_sta *rs_sta = priv_sta;
 	struct ieee80211_tx_info *info = IEEE80211_SKB_CB(skb);
 
@@ -557,7 +557,7 @@ static u16 iwl3945_get_adjacent_rate(struct iwl3945_rs_sta *rs_sta,
 {
 	u8 high = IWL_RATE_INVALID;
 	u8 low = IWL_RATE_INVALID;
-	struct iwl3945_priv *priv = rs_sta->priv;
+	struct iwl_priv *priv = rs_sta->priv;
 
 	/* 802.11A walks to the next literal adjacent rate in
 	 * the rate table */
@@ -652,7 +652,7 @@ static void rs_get_rate(void *priv_r, struct ieee80211_sta *sta,
 	struct ieee80211_hdr *hdr = (struct ieee80211_hdr *)skb->data;
 	u16 fc;
 	u16 rate_mask = 0;
-	struct iwl3945_priv *priv = (struct iwl3945_priv *)priv_r;
+	struct iwl_priv *priv = (struct iwl_priv *)priv_r;
 	struct ieee80211_tx_info *info = IEEE80211_SKB_CB(skb);
 
 	IWL_DEBUG_RATE("enter\n");
@@ -891,7 +891,7 @@ static struct rate_control_ops rs_ops = {
 
 void iwl3945_rate_scale_init(struct ieee80211_hw *hw, s32 sta_id)
 {
-	struct iwl3945_priv *priv = hw->priv;
+	struct iwl_priv *priv = hw->priv;
 	s32 rssi = 0;
 	unsigned long flags;
 	struct iwl3945_rs_sta *rs_sta;
