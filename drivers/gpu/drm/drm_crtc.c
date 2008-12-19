7a516f9ce27dc558c2a7b69f44f932468bdf5f28
Jakob Bornecrantz <jakob@tungstengraphics.com>
2008-12-19 14:50:50 +1000

    drm: sanitise drm modesetting API + remove unused hotplug
    The initially merged modesetting API has some uglies in it, this
    cleans up the struct members and ioctl ordering for initial submission.
    
    It also removes the unneeded hotplug infrastructure.
    
    airlied:- I've pulled this patch in from git modesetting-gem tree.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -805,7 +805,6 @@ void drm_mode_config_init(struct drm_device *dev)
 	dev->mode_config.num_connector = 0;
 	dev->mode_config.num_crtc = 0;
 	dev->mode_config.num_encoder = 0;
-	dev->mode_config.hotplug_counter = 0;
 }
 EXPORT_SYMBOL(drm_mode_config_init);
 
@@ -900,16 +899,6 @@ void drm_mode_config_cleanup(struct drm_device *dev)
 
 }
 EXPORT_SYMBOL(drm_mode_config_cleanup);
-
-int drm_mode_hotplug_ioctl(struct drm_device *dev,
-			   void *data, struct drm_file *file_priv)
-{
-	struct drm_mode_hotplug *arg = data;
-
-	arg->counter = dev->mode_config.hotplug_counter;
-
-	return 0;
-}
 
 /**
  * drm_crtc_convert_to_umode - convert a drm_display_mode into a modeinfo
@@ -1544,9 +1533,9 @@ int drm_mode_cursor_ioctl(struct drm_device *dev,
 	}
 
 	mutex_lock(&dev->mode_config.mutex);
-	obj = drm_mode_object_find(dev, req->crtc, DRM_MODE_OBJECT_CRTC);
+	obj = drm_mode_object_find(dev, req->crtc_id, DRM_MODE_OBJECT_CRTC);
 	if (!obj) {
-		DRM_DEBUG("Unknown CRTC ID %d\n", req->crtc);
+		DRM_DEBUG("Unknown CRTC ID %d\n", req->crtc_id);
 		ret = -EINVAL;
 		goto out;
 	}
@@ -1623,7 +1612,7 @@ int drm_mode_addfb(struct drm_device *dev,
 		goto out;
 	}
 
-	r->buffer_id = fb->base.id;
+	r->fb_id = fb->base.id;
 	list_add(&fb->filp_head, &file_priv->fbs);
 
 out:
@@ -1715,7 +1704,7 @@ int drm_mode_getfb(struct drm_device *dev,
 	int ret = 0;
 
 	mutex_lock(&dev->mode_config.mutex);
-	obj = drm_mode_object_find(dev, r->buffer_id, DRM_MODE_OBJECT_FB);
+	obj = drm_mode_object_find(dev, r->fb_id, DRM_MODE_OBJECT_FB);
 	if (!obj) {
 		DRM_ERROR("invalid framebuffer id\n");
 		ret = -EINVAL;
@@ -2327,7 +2316,7 @@ int drm_mode_replacefb(struct drm_device *dev,
 
 	/* right replace the current bo attached to this fb with a new bo */
 	mutex_lock(&dev->mode_config.mutex);
-	obj = drm_mode_object_find(dev, r->buffer_id, DRM_MODE_OBJECT_FB);
+	obj = drm_mode_object_find(dev, r->fb_id, DRM_MODE_OBJECT_FB);
 	if (!obj) {
 		ret = -EINVAL;
 		goto out;
