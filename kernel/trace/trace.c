7484f5ee2b1ba78ffa930e08285806d17e40b5c1
Ingo Molnar <mingo@elte.hu>
2008-12-19 01:03:29 +0100

    tracing: fix warning in kernel/trace/trace.c
    this warning:
    
      kernel/trace/trace.c: In function ‘print_lat_fmt’:
      kernel/trace/trace.c:1826: warning: unused variable ‘state’
    
    Triggers because 'state' has become unused - remove it.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -1817,7 +1817,6 @@ print_lat_fmt(struct trace_iterator *iter, unsigned int trace_idx, int cpu)
 	char *comm;
 	int S, T;
 	int i;
-	unsigned state;
 
 	if (entry->type == TRACE_CONT)
 		return TRACE_TYPE_HANDLED;
