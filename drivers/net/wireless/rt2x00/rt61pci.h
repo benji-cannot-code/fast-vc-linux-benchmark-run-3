982a5278cfd80c95002edea3ed9e1692bc0ad139
Ivo van Doorn <ivdoorn@gmail.com>
2008-12-20 10:52:42 +0100

    rt2x00: Implement Powersaving
    Listen to IEEE80211_CONF_PS to determine if the device
    should drop into powersaving mode. This feature depends
    on the dynamic power save functionality in mac80211.
    
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt61pci.h
+++ drivers/net/wireless/rt2x00/rt61pci.h
@@ -89,8 +89,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * SOFT_RESET_CSR
+ * FORCE_CLOCK_ON: Host force MAC clock ON
  */
 #define SOFT_RESET_CSR			0x0010
+#define SOFT_RESET_CSR_FORCE_CLOCK_ON	FIELD32(0x00000002)
 
 /*
  * MCU_INT_SOURCE_CSR: MCU interrupt source/mask register.
@@ -1055,8 +1057,10 @@ struct hw_pairwise_ta_entry {
 
 /*
  * IO_CNTL_CSR
+ * RF_PS: Set RF interface value to power save
  */
 #define IO_CNTL_CSR			0x3498
+#define IO_CNTL_CSR_RF_PS		FIELD32(0x00000004)
 
 /*
  * UART_INT_SOURCE_CSR
