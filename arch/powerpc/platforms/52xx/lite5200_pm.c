c42e95dc2877dd7cb9c718e7f884653c1103e368
Grant Likely <grant.likely@secretlab.ca>
2008-12-21 02:54:26 -0700

    powerpc/mpc5200: Document and tidy irq driver
    This patch adds documentation to the mpc5200 interrupt controller
    driver and cleans up some minor coding conventions.  It also moves the
    contents of mpc52xx_pic.h into the driver proper (except for a small
    common bit that is moved to the common mpc52xx.h) because the
    information encoded there is not required by any other part of kernel
    code.  Finally for code readability sake, the L2_OFFSET shift value
    is removed because the code using it resolves to a noop.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/powerpc/platforms/52xx/lite5200_pm.c
+++ arch/powerpc/platforms/52xx/lite5200_pm.c
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/time.h>
 #include <asm/mpc52xx.h>
-#include "mpc52xx_pic.h"
 
 /* defined in lite5200_sleep.S and only used here */
 extern void lite5200_low_power(void __iomem *sram, void __iomem *mbar);
