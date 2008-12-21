4760cbd8a6a3fed6f6db71ad42e38b91557e5577
Tim Yamin <plasm@roo.me.uk>
2008-12-21 02:54:28 -0700

    powerpc/mpc5200: Bestcomm fixes to ATA support
    1) ata.h has dst_pa in the wrong place (needs to match what the BestComm
       task microcode in bcom_ata_task.c expects); fix it.
    
    2) The BestComm ATA task priority was changed to maximum in bestcomm_priv.h;
       this fixes a deadlock issue experienced with heavy DMA occurring on
       both the ATA and Ethernet BestComm tasks, e.g. when downloading a large
       file over a LAN to disk.
    
    Signed-off-by: Tim Yamin <plasm@roo.me.uk>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/powerpc/sysdev/bestcomm/ata.h
+++ arch/powerpc/sysdev/bestcomm/ata.h
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct bcom_ata_bd {
 	u32	status;
-	u32	dst_pa;
 	u32	src_pa;
+	u32	dst_pa;
 };
 
 extern struct bcom_task *
