f93aed6201f4119731a39fbdde9ff5cd5e6503aa
Jean-Francois Moine <moinejf@free.fr>
2008-12-21 15:02:54 -0300

    V4L/DVB (9995): gspca - ov534 and m5602: Set static some functions/variables.
    Signed-off-by: Jean-Francois Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/m5602/m5602_core.c
+++ drivers/media/video/gspca/m5602/m5602_core.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Kernel module parameters */
 int force_sensor;
-int dump_bridge;
+static int dump_bridge;
 int dump_sensor;
 
 static const __devinitdata struct usb_device_id m5602_table[] = {
--- drivers/media/video/gspca/ov534.c
+++ drivers/media/video/gspca/ov534.c
@@ -500,8 +500,8 @@ discard:
 }
 
 /* get stream parameters (framerate) */
-int sd_get_streamparm(struct gspca_dev *gspca_dev,
-		      struct v4l2_streamparm *parm)
+static int sd_get_streamparm(struct gspca_dev *gspca_dev,
+			     struct v4l2_streamparm *parm)
 {
 	struct v4l2_captureparm *cp = &parm->parm.capture;
 	struct v4l2_fract *tpf = &cp->timeperframe;
@@ -518,8 +518,8 @@ int sd_get_streamparm(struct gspca_dev *gspca_dev,
 }
 
 /* set stream parameters (framerate) */
-int sd_set_streamparm(struct gspca_dev *gspca_dev,
-		      struct v4l2_streamparm *parm)
+static int sd_set_streamparm(struct gspca_dev *gspca_dev,
+			     struct v4l2_streamparm *parm)
 {
 	struct v4l2_captureparm *cp = &parm->parm.capture;
 	struct v4l2_fract *tpf = &cp->timeperframe;
