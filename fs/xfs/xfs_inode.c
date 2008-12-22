b1718523be3ba5dccbd633cf962a34d772f9ec30
Lachlan McIlroy <lachlan@redback.melbourne.sgi.com>
2008-12-22 17:52:58 +1100

    [XFS] Remove XFS_BUF_SHUT() and friends
    Code does nothing so remove it.
    
    Reviewed-by: Christoph Hellwig <hch@infradead.org>
    Signed-off-by: Lachlan McIlroy <lachlan@sgi.com>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -2805,7 +2805,6 @@ cluster_corrupt_out:
 			XFS_BUF_CLR_BDSTRAT_FUNC(bp);
 			XFS_BUF_UNDONE(bp);
 			XFS_BUF_STALE(bp);
-			XFS_BUF_SHUT(bp);
 			XFS_BUF_ERROR(bp,EIO);
 			xfs_biodone(bp);
 		} else {
