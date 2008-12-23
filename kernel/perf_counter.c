e2949ce91e321a94db889f3b18acee095c076d1c
Ingo Molnar <mingo@elte.hu>
2008-12-23 12:29:25 +0100

    perfcounters: remove ->nr_inherited
    Impact: remove dead code
    
    nr_inherited was not maintained correctly (not decremented) - and also
    not used - remove it.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/perf_counter.c
+++ kernel/perf_counter.c
@@ -1309,7 +1309,6 @@ inherit_counter(struct perf_counter *parent_counter,
 	child_ctx->nr_counters++;
 
 	child_counter->parent = parent_counter;
-	parent_counter->nr_inherited++;
 	/*
 	 * inherit into child's child as well:
 	 */
