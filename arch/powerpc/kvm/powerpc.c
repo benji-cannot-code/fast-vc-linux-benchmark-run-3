bc88b4b090097b6d6f9a24dc42a31b73b87d362f
Paul Mackerras <paulus@samba.org>
2008-12-23 14:57:26 +1100

    powerpc: Fix KVM build on ppc440
    Commit 2a4aca1144394653269720ffbb5a325a77abd5fa ("powerpc/mm: Split
    low level tlb invalidate for nohash processors") changed a call to
    _tlbia to _tlbil_all but didn't include the header that defines
    _tlbil_all, leading to a build failure on 440 if KVM is enabled.
    This fixes it.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kvm/powerpc.c
+++ arch/powerpc/kvm/powerpc.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <asm/kvm_ppc.h>
 #include <asm/tlbflush.h>
+#include "../mm/mmu_decl.h"
 
 
 gfn_t unalias_gfn(struct kvm *kvm, gfn_t gfn)
