06bf84171474e1d2c05e50c831b202b6718e8407
Trond Myklebust <Trond.Myklebust@netapp.com>
2008-12-23 15:21:43 -0500

    NFSv4: Add a recovery marking scheme for state owners
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4_fs.h
+++ fs/nfs/nfs4_fs.h
@@ -95,10 +95,16 @@ struct nfs4_state_owner {
 
 	spinlock_t	     so_lock;
 	atomic_t	     so_count;
+	unsigned long	     so_flags;
 	struct list_head     so_states;
 	struct list_head     so_delegations;
 	struct nfs_seqid_counter so_seqid;
 	struct rpc_sequence  so_sequence;
+};
+
+enum {
+	NFS_OWNER_RECLAIM_REBOOT,
+	NFS_OWNER_RECLAIM_NOGRACE
 };
 
 /*
@@ -167,6 +173,7 @@ struct nfs4_exception {
 };
 
 struct nfs4_state_recovery_ops {
+	int owner_flag_bit;
 	int state_flag_bit;
 	int (*recover_open)(struct nfs4_state_owner *, struct nfs4_state *);
 	int (*recover_lock)(struct nfs4_state *, struct file_lock *);
