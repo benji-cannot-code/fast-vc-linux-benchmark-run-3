d89d66ace7d63ef3b148899892d0cf3e67f6e4e8
Suresh Siddha <suresh.b.siddha@intel.com>
2008-12-22 17:33:28 -0800

    x86: use logical apicid in x2apic_cluster's x2apic_cpu_mask_to_apicid_and()
    These commits:
    
    	commit 95d313cf1c1ecedc8bec5727b09bdacbf67dfc45
    	Author: Mike Travis <travis@sgi.com>
    	Date:   Tue Dec 16 17:33:54 2008 -0800
    
    	    x86: Add cpu_mask_to_apicid_and
    
    and
    	commit 6eeb7c5a99434596c5953a95baa17d2f085664e3
    	Author: Mike Travis <travis@sgi.com>
    	Date:   Tue Dec 16 17:33:55 2008 -0800
    
    	    x86: update add-cpu_mask_to_apicid_and to use struct cpumask*
    
    broke interrupt delivery on x2apic platforms.  As x2apic cluster mode uses
    logical delivery mode, we need to use logical apicid instead of physical apicid
    in x2apic_cpu_mask_to_apicid_and()
    
    Impact: fixes the broken interrupt delivery issue on generic x2apic platforms.
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Acked-by: Mike Travis <travis@sgi.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/genx2apic_cluster.c
+++ arch/x86/kernel/genx2apic_cluster.c
@@ -115,7 +115,7 @@ static unsigned int x2apic_cpu_mask_to_apicid(const struct cpumask *cpumask)
 	int cpu;
 
 	/*
-	 * We're using fixed IRQ delivery, can only return one phys APIC ID.
+	 * We're using fixed IRQ delivery, can only return one logical APIC ID.
 	 * May as well be the first.
 	 */
 	cpu = cpumask_first(cpumask);
@@ -131,14 +131,14 @@ static unsigned int x2apic_cpu_mask_to_apicid_and(const struct cpumask *cpumask,
 	int cpu;
 
 	/*
-	 * We're using fixed IRQ delivery, can only return one phys APIC ID.
+	 * We're using fixed IRQ delivery, can only return one logical APIC ID.
 	 * May as well be the first.
 	 */
 	for_each_cpu_and(cpu, cpumask, andmask)
 		if (cpumask_test_cpu(cpu, cpu_online_mask))
 			break;
 	if (cpu < nr_cpu_ids)
-		return per_cpu(x86_cpu_to_apicid, cpu);
+		return per_cpu(x86_cpu_to_logical_apicid, cpu);
 	return BAD_APICID;
 }
 
