a24e2a2609d679baaa261a3d6e7df26d0da8d19a
David Kilroy <kilroyd@googlemail.com>
2008-12-23 14:03:38 +0000

    wireless: Add channel/frequency conversions to ieee80211.h
    Added mappings for FHSS, DSSS and OFDM channels - with macros to point
    HR DSSS and ERP to the DSSS mappings. Currently just static inline
    functions.
    
    Use the new functions in the older fullmac drivers. This eliminates a
    number of const static buffers and removes a couple of range checks that
    are now redundant.
    
    Signed-off-by: David Kilroy <kilroyd@googlemail.com>
    Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Acked-by: Richard Farina <sidhayn@gmail.com>
    Acked-by: Jeroen Vreeken <pe1rxq@amsat.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl3501_cs.c
+++ drivers/net/wireless/wl3501_cs.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/wireless.h>
+#include <linux/ieee80211.h>
 
 #include <net/iw_handler.h>
 
@@ -111,12 +112,6 @@ static void wl3501_release(struct pcmcia_device *link);
  * database.
  */
 static dev_info_t wl3501_dev_info = "wl3501_cs";
-
-static int wl3501_chan2freq[] = {
-	[0]  = 2412, [1]  = 2417, [2]  = 2422, [3]  = 2427, [4] = 2432,
-	[5]  = 2437, [6]  = 2442, [7]  = 2447, [8]  = 2452, [9] = 2457,
-	[10] = 2462, [11] = 2467, [12] = 2472, [13] = 2477,
-};
 
 static const struct {
 	int reg_domain;
@@ -1511,7 +1506,7 @@ static int wl3501_get_freq(struct net_device *dev, struct iw_request_info *info,
 {
 	struct wl3501_card *this = netdev_priv(dev);
 
-	wrqu->freq.m = wl3501_chan2freq[this->chan - 1] * 100000;
+	wrqu->freq.m = ieee80211_dsss_chan_to_freq(this->chan) * 100000;
 	wrqu->freq.e = 1;
 	return 0;
 }
