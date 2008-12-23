f42d51f623974677305cf00180ec3249a2a438a0
Robert Richter <robert.richter@amd.com>
2008-12-23 04:03:05 +0100

    oprofile: rename add_sample() in cpu_buffer.c
    Rename the fucntion to op_add_sample() since there is a collision with
    another one with the same name in buffer_sync.c.
    
    Signed-off-by: Robert Richter <robert.richter@amd.com>

--- drivers/oprofile/cpu_buffer.c
+++ drivers/oprofile/cpu_buffer.c
@@ -189,8 +189,8 @@ unsigned long op_cpu_buffer_entries(int cpu)
 }
 
 static inline int
-add_sample(struct oprofile_cpu_buffer *cpu_buf,
-	   unsigned long pc, unsigned long event)
+op_add_sample(struct oprofile_cpu_buffer *cpu_buf,
+	      unsigned long pc, unsigned long event)
 {
 	struct op_entry entry;
 	int ret;
@@ -208,7 +208,7 @@ add_sample(struct oprofile_cpu_buffer *cpu_buf,
 static inline int
 add_code(struct oprofile_cpu_buffer *buffer, unsigned long value)
 {
-	return add_sample(buffer, ESCAPE_CODE, value);
+	return op_add_sample(buffer, ESCAPE_CODE, value);
 }
 
 /* This must be safe from any context. It's safe writing here
@@ -250,7 +250,7 @@ static int log_sample(struct oprofile_cpu_buffer *cpu_buf, unsigned long pc,
 			goto fail;
 	}
 
-	if (add_sample(cpu_buf, pc, event))
+	if (op_add_sample(cpu_buf, pc, event))
 		goto fail;
 
 	return 1;
@@ -338,14 +338,14 @@ void oprofile_add_ibs_sample(struct pt_regs * const regs,
 	}
 
 	fail = fail || add_code(cpu_buf, ibs_code);
-	fail = fail || add_sample(cpu_buf, ibs_sample[0], ibs_sample[1]);
-	fail = fail || add_sample(cpu_buf, ibs_sample[2], ibs_sample[3]);
-	fail = fail || add_sample(cpu_buf, ibs_sample[4], ibs_sample[5]);
+	fail = fail || op_add_sample(cpu_buf, ibs_sample[0], ibs_sample[1]);
+	fail = fail || op_add_sample(cpu_buf, ibs_sample[2], ibs_sample[3]);
+	fail = fail || op_add_sample(cpu_buf, ibs_sample[4], ibs_sample[5]);
 
 	if (ibs_code == IBS_OP_BEGIN) {
-		fail = fail || add_sample(cpu_buf, ibs_sample[6], ibs_sample[7]);
-		fail = fail || add_sample(cpu_buf, ibs_sample[8], ibs_sample[9]);
-		fail = fail || add_sample(cpu_buf, ibs_sample[10], ibs_sample[11]);
+		fail = fail || op_add_sample(cpu_buf, ibs_sample[6], ibs_sample[7]);
+		fail = fail || op_add_sample(cpu_buf, ibs_sample[8], ibs_sample[9]);
+		fail = fail || op_add_sample(cpu_buf, ibs_sample[10], ibs_sample[11]);
 	}
 
 	if (!fail)
@@ -377,7 +377,7 @@ void oprofile_add_trace(unsigned long pc)
 	if (pc == ESCAPE_CODE)
 		goto fail;
 
-	if (add_sample(cpu_buf, pc, 0))
+	if (op_add_sample(cpu_buf, pc, 0))
 		goto fail;
 
 	return;
