b4d236ac5226bff98483574936ed1ea02cea8ec2
Heiko Carstens <heiko.carstens@de.ibm.com>
2008-12-25 13:37:59 +0100

    [S390] cpu topology: dont destroy cpu sets on topology change
    Call rebuild_sched_domains instead of arch_reinit_sched_domains if
    cpu topology changes. This leaves cpu sets alone which otherwise would
    be destroyed.
    If and how it makes sense to define cpu sets on a virtualized
    architecture is another question.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/topology.c
+++ arch/s390/kernel/topology.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/workqueue.h>
 #include <linux/cpu.h>
 #include <linux/smp.h>
+#include <linux/cpuset.h>
 #include <asm/delay.h>
 #include <asm/s390_ext.h>
 #include <asm/sysinfo.h>
@@ -235,7 +236,7 @@ void arch_update_cpu_topology(void)
 
 static void topology_work_fn(struct work_struct *work)
 {
-	arch_reinit_sched_domains();
+	rebuild_sched_domains();
 }
 
 void topology_schedule_update(void)
