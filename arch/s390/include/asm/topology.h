6560477de86818ced0681819e711530e27bf3f98
Rusty Russell <rusty@rustcorp.com.au>
2008-12-26 22:23:42 +1030

    cpumask: cpu_coregroup_mask(): s390
    Like cpu_coregroup_map, but returns a (const) pointer.
    
    Compile-tested on s390 (defconfig).
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Mike Travis <travis@sgi.com>

--- arch/s390/include/asm/topology.h
+++ arch/s390/include/asm/topology.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define mc_capable()	(1)
 
 cpumask_t cpu_coregroup_map(unsigned int cpu);
+const struct cpumask *cpu_coregroup_mask(unsigned int cpu);
 
 extern cpumask_t cpu_core_map[NR_CPUS];
 
