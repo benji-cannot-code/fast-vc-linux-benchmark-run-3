19fc8457f25ef804a4d93ad0ef3f10b94391348d
Brice Goglin <brice@myri.com>
2008-12-25 18:09:16 -0800

    myri10ge: update driver version to 1.4.4-1.395
    Update myri10ge driver version to 1.4.4-1.395.
    
    Signed-off-by: Brice Goglin <brice@myri.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/myri10ge/myri10ge.c
+++ drivers/net/myri10ge/myri10ge.c
@@ -76,7 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "myri10ge_mcp.h"
 #include "myri10ge_mcp_gen_header.h"
 
-#define MYRI10GE_VERSION_STR "1.4.3-1.378"
+#define MYRI10GE_VERSION_STR "1.4.4-1.395"
 
 MODULE_DESCRIPTION("Myricom 10G driver (10GbE)");
 MODULE_AUTHOR("Maintainer: help@myri.com");
