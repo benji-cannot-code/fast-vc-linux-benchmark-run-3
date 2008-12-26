a9b99924b00ea3598fd6ffd016865418e4d33d66
Sam Ravnborg <sam@ravnborg.org>
2008-12-26 15:38:17 -0800

    sparc: unify module.c
    o Copy module_64.c to module.c
    o Add all sparc specific bits to module.c
    o delete module_32.c
    o update Makefile
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/module.c
+++ arch/sparc/kernel/module.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 #include <linux/fs.h>
 #include <linux/string.h>
+#include <linux/ctype.h>
 #include <linux/slab.h>
 #include <linux/mm.h>
 
@@ -35,6 +36,21 @@ static void *module_map(unsigned long size)
 
 static char *dot2underscore(char *name)
 {
+	return name;
+}
+#else
+static void *module_map(unsigned long size)
+{
+	return vmalloc(size);
+}
+
+/* Replace references to .func with _Func */
+static char *dot2underscore(char *name)
+{
+	if (name[0] == '.') {
+		name[0] = '_';
+                name[1] = toupper(name[1]);
+	}
 	return name;
 }
 #endif /* CONFIG_SPARC64 */
@@ -171,6 +187,7 @@ int apply_relocate_add(Elf_Shdr *sechdrs,
 #endif /* CONFIG_SPARC64 */
 
 		case R_SPARC_32:
+		case R_SPARC_UA32:
 			location[0] = v >> 24;
 			location[1] = v >> 16;
 			location[2] = v >>  8;
@@ -224,6 +241,13 @@ int module_finalize(const Elf_Ehdr *hdr,
 	}
 
 	return 0;
+}
+#else
+int module_finalize(const Elf_Ehdr *hdr,
+                    const Elf_Shdr *sechdrs,
+                    struct module *me)
+{
+        return 0;
 }
 #endif /* CONFIG_SPARC64 */
 
