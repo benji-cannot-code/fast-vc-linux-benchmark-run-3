d13646c5f495b5a0311a458837ab8036cd18d434
Sam Ravnborg <sam@ravnborg.org>
2008-12-26 15:33:07 -0800

    sparc: fix hardirq.h removal fallout
    When hardirq.h are removed from asm-generic/local.h a few
    bits fails to build.  Fix these upfront.
    Reported by Alexey Dobriyan.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/mdesc.c
+++ arch/sparc/kernel/mdesc.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/miscdevice.h>
 
+#include <asm/cpudata.h>
 #include <asm/hypervisor.h>
 #include <asm/mdesc.h>
 #include <asm/prom.h>
