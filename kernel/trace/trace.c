af6e9511e08d4bb060442e30ba220d8213039ac8
Huang Weiyi <weiyi.huang@gmail.com>
2008-12-30 06:41:44 +0800

    tracing: removed duplicated #include
    Removed duplicated #include in kernel/trace/trace.c.
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gfp.h>
 #include <linux/fs.h>
 #include <linux/kprobes.h>
-#include <linux/seq_file.h>
 #include <linux/writeback.h>
 
 #include <linux/stacktrace.h>
