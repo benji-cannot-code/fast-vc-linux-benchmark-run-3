7da75d9f25e8a25792874c681fe7c5c96c190dcd
Helge Deller <deller@gmx.de>
2008-12-29 04:44:44 -0800

    Input: HIL drivers - add MODULE_ALIAS()
    Add MODULE_ALIAS() to the HIL keyboard (hil_kbd.c) and HIL mouse
    (hil_ptr.c) drivers to make kernel module autoloader functional.
    Report HIL port number ID in serio id.id field.
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/mouse/hil_ptr.c
+++ drivers/input/mouse/hil_ptr.c
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 MODULE_AUTHOR("Brian S. Julin <bri@calyx.com>");
 MODULE_DESCRIPTION(HIL_GENERIC_NAME " driver");
 MODULE_LICENSE("Dual BSD/GPL");
-
+MODULE_ALIAS("serio:ty03pr25id0Fex*");
 
 #define TABLET_SIMULATES_MOUSE	/* allow tablet to be used as mouse */
 #undef  TABLET_AUTOADJUST	/* auto-adjust valid tablet ranges */
