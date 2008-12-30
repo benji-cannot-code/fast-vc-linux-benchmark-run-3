43fb5fb0fc7e182d391815403de0a04fec9f5a18
Ingo Molnar <mingo@elte.hu>
2008-12-30 12:07:27 +0100

    tracing/kmemtrace: export kmemtrace_mark_alloc_node() / kmemtrace_mark_free()
    Impact: build fix
    
    Also fix up Kconfig dependencies and include files.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/kmemtrace.c
+++ kernel/trace/kmemtrace.c
@@ -297,6 +297,7 @@ void kmemtrace_mark_alloc_node(enum kmemtrace_type_id type_id,
 
 	trace_wake_up();
 }
+EXPORT_SYMBOL(kmemtrace_mark_alloc_node);
 
 void kmemtrace_mark_free(enum kmemtrace_type_id type_id,
 		       unsigned long call_site,
@@ -326,6 +327,7 @@ void kmemtrace_mark_free(enum kmemtrace_type_id type_id,
 
 	trace_wake_up();
 }
+EXPORT_SYMBOL(kmemtrace_mark_free);
 
 static struct tracer kmem_tracer __read_mostly = {
 	.name		= "kmemtrace",
