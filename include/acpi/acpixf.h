26ca905e3078bec213eb0c89aed4575aad3ce88b
Lin Ming <ming.m.lin@intel.com>
2008-12-31 09:23:57 +0800

    ACPI: osl.c: replace return_ACPI_STATUS with return
    return_ACPI_STATUS is an internal acpica function, replace it with return.
    acpi_gbl_permanent_mmap moved from acglobal.h to acpixf.h for external use
    
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -49,6 +49,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "actypes.h"
 #include "actbl.h"
 
+extern u8 acpi_gbl_permanent_mmap;
+
 /*
  * Global interfaces
  */
