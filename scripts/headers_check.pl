3c44a370a174bcd88f0e47b4f3861d861626cb4e
Sam Ravnborg <sam@ravnborg.org>
2008-12-31 09:32:30 +0100

    kbuild: ignore a few files in headers_check
    The new check for asm/types.h and linux/types.h had
    a few false positives.
    
    o We cannot let linux/types.h include linux/types.h
    o The int-ll64.h and int-ll64.h define the types
      and are included by linux/types.h
    
    Handle this by hardcoding the filenames in the headers_check script.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- scripts/headers_check.pl
+++ scripts/headers_check.pl
@@ -79,6 +79,9 @@ sub check_config
 my $linux_asm_types;
 sub check_asm_types()
 {
+	if ($filename =~ /types.h|int-l64.h|int-ll64.h/o) {
+		return;
+	}
 	if ($lineno == 1) {
 		$linux_asm_types = 0;
 	} elsif ($linux_asm_types >= 1) {
@@ -96,6 +99,9 @@ sub check_asm_types()
 my $linux_types;
 sub check_sizetypes
 {
+	if ($filename =~ /types.h|int-l64.h|int-ll64.h/o) {
+		return;
+	}
 	if ($lineno == 1) {
 		$linux_types = 0;
 	} elsif ($linux_types >= 1) {
