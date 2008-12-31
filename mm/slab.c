64cb4483e176b79a9315747727a8513ad6ffea48
Ingo Molnar <mingo@elte.hu>
2008-12-31 09:43:46 +0100

    tracing/kmemtrace: fix typo
    Impact: build fix
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- mm/slab.c
+++ mm/slab.c
@@ -103,7 +103,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include	<linux/cpu.h>
 #include	<linux/sysctl.h>
 #include	<linux/module.h>
-#include	<tracing/kmemtrace.h>
+#include	<trace/kmemtrace.h>
 #include	<linux/rcupdate.h>
 #include	<linux/string.h>
 #include	<linux/uaccess.h>
