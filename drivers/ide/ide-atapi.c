d22699f61d74055eff204b559d8e32344ed323e0
Borislav Petkov <petkovbb@googlemail.com>
2009-01-02 16:12:54 +0100

    ide: remove the last ide-scsi remnants
    Signed-off-by: Borislav Petkov <petkovbb@gmail.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-atapi.c
+++ drivers/ide/ide-atapi.c
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline int dev_is_idecd(ide_drive_t *drive)
 {
-	return (drive->media == ide_cdrom || drive->media == ide_optical) &&
-		!(drive->dev_flags & IDE_DFLAG_SCSI);
+	return drive->media == ide_cdrom || drive->media == ide_optical;
 }
 
 /*
