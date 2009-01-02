d5861b1e85eabfc55174b8e267ddc0f5513c0c21
Jesper Juhl <jj@chaosbits.net>
2009-01-03 00:16:03 +0100

    swiotlb: Don't include linux/swiotlb.h twice in lib/swiotlb.c
    There's no point in including the linux/swiotlb.h header twice in
    lib/swiotlb.c - this patch gets rid of the unneeded include.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- lib/swiotlb.c
+++ lib/swiotlb.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/swiotlb.h>
 #include <linux/string.h>
-#include <linux/swiotlb.h>
 #include <linux/types.h>
 #include <linux/ctype.h>
 #include <linux/highmem.h>
