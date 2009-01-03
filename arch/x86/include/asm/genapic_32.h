4150dd61025f6635bf0da9128eca09b07896febd
Jaswinder Singh Rajput <jaswinder@infradead.org>
2009-01-03 15:48:52 +0530

    x86: rename mpc_config_processor to mpc_cpu
    Impact: cleanup, solve 80 columns wrap problems
    
    mpc_config_processor should be renamed to mpc_cpu.
    
    The reason: the 'c' in MPC already means 'config' -
    no need to repeat that in the type name.
    
    Plus 'processor' is a lot longer than 'cpu' - so we try to use 'cpu' in all
    type names, as much as possible.
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/genapic_32.h
+++ arch/x86/include/asm/genapic_32.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct mpc_bus;
 struct mpc_table;
-struct mpc_config_processor;
+struct mpc_cpu;
 
 struct genapic {
 	char *name;
