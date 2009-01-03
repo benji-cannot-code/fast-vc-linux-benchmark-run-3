60cb52a2e7e643d6f6a6a570f57d008c50c23c2f
Laurent Pinchart <laurent.pinchart@skynet.be>
2009-01-03 19:12:40 -0300

    V4L/DVB (10197): uvcvideo: Whitespace and comments cleanup, copyright updates.
    Signed-off-by: Laurent Pinchart <laurent.pinchart@skynet.be>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/uvc/uvc_status.c
+++ drivers/media/video/uvc/uvc_status.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *      uvc_status.c  --  USB Video Class driver - Status endpoint
  *
- *      Copyright (C) 2007-2008
+ *      Copyright (C) 2007-2009
  *          Laurent Pinchart (laurent.pinchart@skynet.be)
  *
  *      This program is free software; you can redistribute it and/or modify
