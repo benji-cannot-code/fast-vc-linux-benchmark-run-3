c84f154ed95fb0618312c1aadfd6887f07e78831
Jay Fenlason, Stefan Richter <stefanr@s5r6.in-berlin.de>
2009-01-04 16:23:29 +0100

    firewire: cdev: add ioctls for isochronous resource management
    Based on
        Date: Tue, 18 Nov 2008 11:41:27 -0500
        From: Jay Fenlason <fenlason@redhat.com>
        Subject: [Patch V4] Add ISO resource management support
    with several changes to the ABI and implementation.  Only the part of
    the ABI which enables auto-reallocation and auto-deallocation is
    included here.
    
    This implements ioctls for kernel-assisted allocation of isochronous
    channels and isochronous bandwidth.  The benefits are:
      - The client does not have to have write access to the /dev/fw* device
        corresponding to the IRM.
      - The client does not have to perform reallocation after bus resets.
      - Channel and bandwidth are deallocated by the kernel if the file is
        closed before the client deallocated the resources.  Thus resources
        are released even if the client crashes.
    
    It is anticipated that future in-kernel code (firewire-core IRM code;
    the firewire port of firedtv), will use the fw-iso.c portions of this
    code too.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>
    Tested-by: David Moore <dcm@acm.org>

--- drivers/firewire/fw-transaction.h
+++ drivers/firewire/fw-transaction.h
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CSR_SPEED_MAP			0x2000
 #define CSR_SPEED_MAP_END		0x3000
 
+#define BANDWIDTH_AVAILABLE_INITIAL	4915
 #define BROADCAST_CHANNEL_INITIAL	(1 << 31 | 31)
 #define BROADCAST_CHANNEL_VALID		(1 << 30)
 
@@ -343,6 +344,9 @@ int fw_iso_context_start(struct fw_iso_context *ctx,
 			 int cycle, int sync, int tags);
 int fw_iso_context_stop(struct fw_iso_context *ctx);
 void fw_iso_context_destroy(struct fw_iso_context *ctx);
+
+void fw_iso_resource_manage(struct fw_card *card, int generation,
+		u64 channels_mask, int *channel, int *bandwidth, bool allocate);
 
 struct fw_card_driver {
 	/*
