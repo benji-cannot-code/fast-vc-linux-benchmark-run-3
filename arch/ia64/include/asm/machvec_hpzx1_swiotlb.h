a33ddea0b86ea68f2cc9565a69999873558cf97b
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-01-05 23:36:17 +0900

    remove hwsw_dma_ops
    This removes remove hwsw_dma_ops (and hwsw_*
    functions). hwsw_dma_get_ops can select swiotlb_dma_ops and
    sba_dma_ops appropriately.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/ia64/include/asm/machvec_hpzx1_swiotlb.h
+++ arch/ia64/include/asm/machvec_hpzx1_swiotlb.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_IA64_MACHVEC_HPZX1_SWIOTLB_h
 
 extern ia64_mv_setup_t				dig_setup;
+extern ia64_mv_dma_get_ops			hwsw_dma_get_ops;
 
 /*
  * This stuff has dual use!
@@ -14,5 +15,6 @@ extern ia64_mv_setup_t				dig_setup;
 #define platform_name				"hpzx1_swiotlb"
 #define platform_setup				dig_setup
 #define platform_dma_init			machvec_noop
+#define platform_dma_get_ops			hwsw_dma_get_ops
 
 #endif /* _ASM_IA64_MACHVEC_HPZX1_SWIOTLB_h */
