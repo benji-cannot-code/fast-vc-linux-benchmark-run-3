b574bdc0af2cc1b8ac811a81e5083d1f8d6a9ba0
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-01-05 23:36:11 +0900

    add dma_mapping_ops for SWIOTLB
    There is already dma_mapping_ops for SWIOTLB but there are some
    missing hooks.
    
    This is for IA64_DIG_VTD, IA64_HP_ZX1_SWIOTLB, IA64_SGI_UV,
    IA64_HP_SIM, IA64_XEN_GUEST and IA64_GENERIC.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/ia64/kernel/pci-swiotlb.c
+++ arch/ia64/kernel/pci-swiotlb.c
@@ -14,12 +14,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int swiotlb __read_mostly;
 EXPORT_SYMBOL(swiotlb);
 
+/* Set this to 1 if there is a HW IOMMU in the system */
+int iommu_detected __read_mostly;
+
 struct dma_mapping_ops swiotlb_dma_ops = {
-	.mapping_error = swiotlb_dma_mapping_error,
 	.alloc_coherent = swiotlb_alloc_coherent,
 	.free_coherent = swiotlb_free_coherent,
 	.map_single = swiotlb_map_single,
 	.unmap_single = swiotlb_unmap_single,
+	.map_single_attrs = swiotlb_map_single_attrs,
+	.unmap_single_attrs = swiotlb_unmap_single_attrs,
+	.map_sg_attrs = swiotlb_map_sg_attrs,
+	.unmap_sg_attrs	= swiotlb_unmap_sg_attrs,
 	.sync_single_for_cpu = swiotlb_sync_single_for_cpu,
 	.sync_single_for_device = swiotlb_sync_single_for_device,
 	.sync_single_range_for_cpu = swiotlb_sync_single_range_for_cpu,
@@ -29,6 +35,7 @@ struct dma_mapping_ops swiotlb_dma_ops = {
 	.map_sg = swiotlb_map_sg,
 	.unmap_sg = swiotlb_unmap_sg,
 	.dma_supported_op = swiotlb_dma_supported,
+	.mapping_error = swiotlb_dma_mapping_error,
 };
 
 void __init pci_swiotlb_init(void)
