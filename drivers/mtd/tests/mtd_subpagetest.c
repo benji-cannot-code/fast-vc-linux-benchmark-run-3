125aac09a294ea70757ac2c074523ca33c187b66
David Woodhouse <David.Woodhouse@intel.com>
2009-01-05 16:31:03 +0000

    [MTD] [TESTS] Fix some size_t printk format warnings
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/tests/mtd_subpagetest.c
+++ drivers/mtd/tests/mtd_subpagetest.c
@@ -126,7 +126,7 @@ static int write_eraseblock(int ebnum)
 		       (long long)addr);
 		if (written != subpgsize) {
 			printk(PRINT_PREF "  write size: %#x\n", subpgsize);
-			printk(PRINT_PREF "  written: %#x\n", written);
+			printk(PRINT_PREF "  written: %#zx\n", written);
 		}
 		return err ? err : -1;
 	}
@@ -140,7 +140,7 @@ static int write_eraseblock(int ebnum)
 		       (long long)addr);
 		if (written != subpgsize) {
 			printk(PRINT_PREF "  write size: %#x\n", subpgsize);
-			printk(PRINT_PREF "  written: %#x\n", written);
+			printk(PRINT_PREF "  written: %#zx\n", written);
 		}
 		return err ? err : -1;
 	}
@@ -165,7 +165,7 @@ static int write_eraseblock2(int ebnum)
 			if (written != subpgsize) {
 				printk(PRINT_PREF "  write size: %#x\n",
 				       subpgsize * k);
-				printk(PRINT_PREF "  written: %#08x\n",
+				printk(PRINT_PREF "  written: %#08zx\n",
 				       written);
 			}
 			return err ? err : -1;
