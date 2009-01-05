a8188b9042d32d4bb2506db8a8a20fa7b902c482
Greg Kroah-Hartman <gregkh@suse.de>
2009-01-05 15:45:21 -0800

    Staging: epl: fix netdev->priv b0rkage
    netdev->priv is now gone, use netdev_priv() instead.
    This fixes the build error in the network driver within the epl stack.
    
    Cc: Daniel Krueger <daniel.krueger@systec-electronic.com>
    Cc: Ronald Sieber <Ronald.Sieber@systec-electronic.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/epl/VirtualEthernetLinux.c
+++ drivers/staging/epl/VirtualEthernetLinux.c
@@ -167,7 +167,7 @@ static int VEthOpen(struct net_device *pNetDevice_p)
 	tEplKernel Ret = kEplSuccessful;
 
 	//open the device
-//    struct net_device_stats* pStats = (struct net_device_stats*)pNetDevice_p->priv;
+//	struct net_device_stats* pStats = netdev_priv(pNetDevice_p);
 
 	//start the interface queue for the network subsystem
 	netif_start_queue(pNetDevice_p);
@@ -200,8 +200,7 @@ static int VEthXmit(struct sk_buff *pSkb_p, struct net_device *pNetDevice_p)
 	tEplFrameInfo FrameInfo;
 
 	//transmit function
-	struct net_device_stats *pStats =
-	    (struct net_device_stats *)pNetDevice_p->priv;
+	struct net_device_stats *pStats = netdev_priv(pNetDevice_p);
 
 	//save timestemp
 	pNetDevice_p->trans_start = jiffies;
@@ -234,7 +233,7 @@ static struct net_device_stats *VEthGetStats(struct net_device *pNetDevice_p)
 {
 	EPL_DBGLVL_VETH_TRACE0("VEthGetStats\n");
 
-	return (struct net_device_stats *)pNetDevice_p->priv;
+	return netdev_priv(pNetDevice_p);
 }
 
 static void VEthTimeout(struct net_device *pNetDevice_p)
@@ -251,8 +250,7 @@ static tEplKernel VEthRecvFrame(tEplFrameInfo * pFrameInfo_p)
 {
 	tEplKernel Ret = kEplSuccessful;
 	struct net_device *pNetDevice = pVEthNetDevice_g;
-	struct net_device_stats *pStats =
-	    (struct net_device_stats *)pNetDevice->priv;
+	struct net_device_stats *pStats = netdev_priv(pNetDevice);
 	struct sk_buff *pSkb;
 
 	EPL_DBGLVL_VETH_TRACE1("VEthRecvFrame: FrameSize=%u\n",
