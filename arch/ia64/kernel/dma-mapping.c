e55193c33a94326ef85fba553f81dcc1348148e2
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-01-05 23:36:16 +0900

    add dma_get_ops to struct ia64_machine_vector
    This adds dma_get_ops hook to struct ia64_machine_vector. We use
    dma_get_ops() in arch/ia64/kernel/dma-mapping.c, which simply returns
    the global dma_ops. This is for removing hwsw_dma_ops.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/ia64/kernel/dma-mapping.c
+++ arch/ia64/kernel/dma-mapping.c
@@ -3,3 +3,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct dma_mapping_ops *dma_ops;
 EXPORT_SYMBOL(dma_ops);
+
+struct dma_mapping_ops *dma_get_ops(struct device *dev)
+{
+	return dma_ops;
+}
+EXPORT_SYMBOL(dma_get_ops);
