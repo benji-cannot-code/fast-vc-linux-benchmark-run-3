a74a66156debcf81747488f6226a5ac15b0010c5
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-01-06 17:20:48 +0100

    ide: add ->cur_port to struct ide_host and use it for serialized hosts
    * Pass 'ide_hwif_t *' instead of 'ide_hwgroup_t *' to unexpected_intr().
    
    * Cache pointer to the port currently being serviced in ->cur_port
      and use it instead of hwif->hwgroup on serialized hosts.
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- include/linux/ide.h
+++ include/linux/ide.h
@@ -853,6 +853,7 @@ struct ide_host {
 	unsigned int	(*init_chipset)(struct pci_dev *);
 	unsigned long	host_flags;
 	void		*host_priv;
+	ide_hwif_t	*cur_port;	/* for hosts requiring serialization */
 };
 
 /*
