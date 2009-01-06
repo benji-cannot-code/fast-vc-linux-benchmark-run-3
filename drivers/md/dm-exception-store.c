05a9c5125fb624a0edb2142b793f4b9c4a2ef6d0
Jonathan Brassow <jbrassow@redhat.com>
2009-01-06 03:05:15 +0000

    dm snapshot: separate out exception store interface
    Pull structures that bridge the gap between snapshot and
    exception store out of dm-snap.h and put them in a new
    .h file - dm-exception-store.h.  This file will define the
    API for new exception stores.
    
    Ultimately, dm-snap.h is unnecessary, since only dm-snap.c
    should be using it.
    
    Signed-off-by: Jonathan Brassow <jbrassow@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-exception-store.c
+++ drivers/md/dm-exception-store.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This file is released under the GPL.
  */
 
+#include "dm-exception-store.h"
 #include "dm-snap.h"
 
 #include <linux/mm.h>
