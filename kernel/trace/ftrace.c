a8f55c6321ed0cad7301f13abedec488f93742fd
Steven Rostedt <srostedt@redhat.com>
2009-01-06 12:43:01 -0500

    ftrace: convert unsigned index to signed
    Impact: fix to unsigned compared to less than zero
    
    Roel Kluin pointed out that there is a compare of an unsigned number
    to less than zero. A previous clean up had the unsigned index set
    to -1 for certain cases, but never converted it to signed.
    
    Frederic Weisbecker noticed that another index is used to compare
    the above index to and it also needs to be converted to signed.
    
    [
      Converted ftrace_page->index to int from unsigned long as
      Andrew Morton pointed out that there's no need for it to
      stay a long.
    ]
    
    Reported-by: Roel Kluin <roel.kluin@gmail.com>
    Reported-by: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -290,7 +290,7 @@ static DEFINE_MUTEX(ftrace_regex_lock);
 
 struct ftrace_page {
 	struct ftrace_page	*next;
-	unsigned long		index;
+	int			index;
 	struct dyn_ftrace	records[];
 };
 
@@ -787,7 +787,7 @@ enum {
 
 struct ftrace_iterator {
 	struct ftrace_page	*pg;
-	unsigned		idx;
+	int			idx;
 	unsigned		flags;
 	unsigned char		buffer[FTRACE_BUFF_MAX+1];
 	unsigned		buffer_idx;
