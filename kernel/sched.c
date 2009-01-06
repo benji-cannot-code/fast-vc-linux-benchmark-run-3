acd9b8b0e934130a44be2997ec4a75b1a955d6f5
Li Zefan <lizf@cn.fujitsu.com>
2009-01-06 17:39:06 +0800

    sched: fix double kfree in failure path
    It's not the responsibility of init_rootdomain() to free root_domain
    allocated by alloc_rootdomain().
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Reviewed-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -6971,7 +6971,7 @@ static int init_rootdomain(struct root_domain *rd, bool bootmem)
 	}
 
 	if (!alloc_cpumask_var(&rd->span, GFP_KERNEL))
-		goto free_rd;
+		goto out;
 	if (!alloc_cpumask_var(&rd->online, GFP_KERNEL))
 		goto free_span;
 	if (!alloc_cpumask_var(&rd->rto_mask, GFP_KERNEL))
@@ -6987,8 +6987,7 @@ free_online:
 	free_cpumask_var(rd->online);
 free_span:
 	free_cpumask_var(rd->span);
-free_rd:
-	kfree(rd);
+out:
 	return -ENOMEM;
 }
 
