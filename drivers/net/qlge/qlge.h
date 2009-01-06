3dfad6674baf524ac2d271820187b3dc5275ed13
Ron Mercer <ron.mercer@qlogic.com>
2009-01-05 18:19:59 -0800

    qlge: Fix sparse warnings for tx ring indexes.
    Warnings:
    drivers/net/qlge/qlge_main.c:1474:34: warning: restricted degrades to integer
    drivers/net/qlge/qlge_main.c:1475:36: warning: restricted degrades to integer
    drivers/net/qlge/qlge_main.c:1592:51: warning: restricted degrades to integer
    drivers/net/qlge/qlge_main.c:1941:20: warning: incorrect type in assignment (different base types)
    drivers/net/qlge/qlge_main.c:1941:20:    expected restricted unsigned int [usertype] tid
    drivers/net/qlge/qlge_main.c:1941:20:    got int [signed] index
    drivers/net/qlge/qlge_main.c:1945:24: warning: incorrect type in assignment (different base types)
    drivers/net/qlge/qlge_main.c:1945:24:    expected restricted unsigned int [usertype] txq_idx
    drivers/net/qlge/qlge_main.c:1945:24:    got unsigned int [unsigned] [usertype] tx_ring_idx
    
    Signed-off-by: Ron Mercer <ron.mercer@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlge/qlge.h
+++ drivers/net/qlge/qlge.h
@@ -852,8 +852,8 @@ struct ob_mac_iocb_req {
 	__le16 frame_len;
 #define OB_MAC_IOCB_LEN_MASK 0x3ffff
 	__le16 reserved2;
-	__le32 tid;
-	__le32 txq_idx;
+	u32 tid;
+	u32 txq_idx;
 	__le32 reserved3;
 	__le16 vlan_tci;
 	__le16 reserved4;
@@ -872,8 +872,8 @@ struct ob_mac_iocb_rsp {
 	u8 flags2;		/* */
 	u8 flags3;		/* */
 #define OB_MAC_IOCB_RSP_B	0x80	/* */
-	__le32 tid;
-	__le32 txq_idx;
+	u32 tid;
+	u32 txq_idx;
 	__le32 reserved[13];
 } __attribute((packed));
 
@@ -895,8 +895,8 @@ struct ob_mac_tso_iocb_req {
 #define OB_MAC_TSO_IOCB_V	0x04
 	__le32 reserved1[2];
 	__le32 frame_len;
-	__le32 tid;
-	__le32 txq_idx;
+	u32 tid;
+	u32 txq_idx;
 	__le16 total_hdrs_len;
 	__le16 net_trans_offset;
 #define OB_MAC_TRANSPORT_HDR_SHIFT 6
@@ -917,8 +917,8 @@ struct ob_mac_tso_iocb_rsp {
 	u8 flags2;		/* */
 	u8 flags3;		/* */
 #define OB_MAC_TSO_IOCB_RSP_B	0x8000
-	__le32 tid;
-	__le32 txq_idx;
+	u32 tid;
+	u32 txq_idx;
 	__le32 reserved2[13];
 } __attribute((packed));
 
@@ -1119,7 +1119,7 @@ struct map_list {
 struct tx_ring_desc {
 	struct sk_buff *skb;
 	struct ob_mac_iocb_req *queue_entry;
-	int index;
+	u32 index;
 	struct oal oal;
 	struct map_list map[MAX_SKB_FRAGS + 1];
 	int map_cnt;
@@ -1132,7 +1132,7 @@ struct bq_desc {
 		struct sk_buff *skb;
 	} p;
 	__le64 *addr;
-	int index;
+	u32 index;
 	 DECLARE_PCI_UNMAP_ADDR(mapaddr);
 	 DECLARE_PCI_UNMAP_LEN(maplen);
 };
