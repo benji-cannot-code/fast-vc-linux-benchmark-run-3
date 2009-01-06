6a11ff8498395da9ca2d231448b68c1d203ee546
Milan Broz <mbroz@redhat.com>
2009-01-06 03:05:12 +0000

    dm: add name and uuid to sysfs
    Implement simple read-only sysfs entry for device-mapper block device.
    
    This patch adds a simple sysfs directory named "dm" under block device
    properties and implements
    	- name attribute (string containing mapped device name)
    	- uuid attribute (string containing UUID, or empty string if not set)
    
    The kobject is embedded in mapped_device struct, so no additional
    memory allocation is needed for initializing sysfs entry.
    
    During the processing of sysfs attribute we need to lock mapped device
    which is done by a new function dm_get_from_kobj, which returns the md
    associated with kobject and increases the usage count.
    
    Each 'show attribute' function is responsible for its own locking.
    
    Signed-off-by: Milan Broz <mbroz@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright (C) 2001, 2002 Sistina Software (UK) Limited.
- * Copyright (C) 2004-2006 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2004-2008 Red Hat, Inc. All rights reserved.
  *
  * This file is released under the GPL.
  */
@@ -168,6 +168,9 @@ struct mapped_device {
 
 	/* forced geometry settings */
 	struct hd_geometry geometry;
+
+	/* sysfs handle */
+	struct kobject kobj;
 };
 
 #define MIN_IOS 256
@@ -1286,6 +1289,8 @@ int dm_create(int minor, struct mapped_device **result)
 	if (!md)
 		return -ENXIO;
 
+	dm_sysfs_init(md);
+
 	*result = md;
 	return 0;
 }
@@ -1361,6 +1366,7 @@ void dm_put(struct mapped_device *md)
 			dm_table_presuspend_targets(map);
 			dm_table_postsuspend_targets(map);
 		}
+		dm_sysfs_exit(md);
 		dm_table_put(map);
 		__unbind(md);
 		free_dev(md);
@@ -1698,6 +1704,27 @@ void dm_uevent_add(struct mapped_device *md, struct list_head *elist)
 struct gendisk *dm_disk(struct mapped_device *md)
 {
 	return md->disk;
+}
+
+struct kobject *dm_kobject(struct mapped_device *md)
+{
+	return &md->kobj;
+}
+
+/*
+ * struct mapped_device should not be exported outside of dm.c
+ * so use this check to verify that kobj is part of md structure
+ */
+struct mapped_device *dm_get_from_kobject(struct kobject *kobj)
+{
+	struct mapped_device *md;
+
+	md = container_of(kobj, struct mapped_device, kobj);
+	if (&md->kobj != kobj)
+		return NULL;
+
+	dm_get(md);
+	return md;
 }
 
 int dm_suspended(struct mapped_device *md)
