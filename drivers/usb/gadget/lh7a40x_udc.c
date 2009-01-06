40e6f2bccd2a953a013af460d0e4a2e060a44190
Kay Sievers <kay.sievers@vrfy.org>
2009-01-06 10:44:42 -0800

    gadget: struct device - replace bus_id with dev_name(), dev_set_name()
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/lh7a40x_udc.c
+++ drivers/usb/gadget/lh7a40x_udc.c
@@ -1982,7 +1982,7 @@ static struct lh7a40x_udc memory = {
 		   .ep0 = &memory.ep[0].ep,
 		   .name = driver_name,
 		   .dev = {
-			   .bus_id = "gadget",
+			   .init_name = "gadget",
 			   .release = nop_release,
 			   },
 		   },
