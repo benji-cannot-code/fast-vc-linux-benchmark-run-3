d69ae79d83dee200a8958e40e3e01def8f390306
Mike Frysinger <vapier.adi@gmail.com>
2009-01-07 23:14:38 +0800

    Blackfin arch: drop irq_callback from struct dma_channel
    the irq member already serves the same purpose
    
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/kernel/bfin_dma_5xx.c
+++ arch/blackfin/kernel/bfin_dma_5xx.c
@@ -141,7 +141,7 @@ int request_dma(unsigned int channel, const char *device_id)
 #endif
 
 	dma_ch[channel].device_id = device_id;
-	dma_ch[channel].irq_callback = NULL;
+	dma_ch[channel].irq = 0;
 
 	/* This is to be enabled by putting a restriction -
 	 * you have to request DMA, before doing any operations on
@@ -170,7 +170,6 @@ int set_dma_callback(unsigned int channel, irq_handler_t callback, void *data)
 			       "Request irq in DMA engine failed.\n");
 			return -EPERM;
 		}
-		dma_ch[channel].irq_callback = callback;
 	}
 	return 0;
 }
@@ -186,7 +185,7 @@ void free_dma(unsigned int channel)
 	disable_dma(channel);
 	clear_dma_buffer(channel);
 
-	if (dma_ch[channel].irq_callback != NULL)
+	if (dma_ch[channel].irq)
 		free_irq(dma_ch[channel].irq, dma_ch[channel].data);
 
 	/* Clear the DMA Variable in the Channel */
