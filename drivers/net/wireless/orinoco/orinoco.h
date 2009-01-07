1db42282f9c0c1f1259c772f02787e17c1bed9f6
David Kilroy <kilroyd@googlemail.com>
2009-01-07 00:43:54 +0000

    orinoco: Remove unused variable rx_data
    Probably something leftover from experimentation with tasklets. Now the
    structure declaration orinoco_rx_data can be relocated to orinoco.c
    
    Signed-off-by: David Kilroy <kilroyd@googlemail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/orinoco/orinoco.h
+++ drivers/net/wireless/orinoco/orinoco.h
@@ -60,14 +60,6 @@ struct xbss_element {
 	struct list_head list;
 };
 
-struct hermes_rx_descriptor;
-
-struct orinoco_rx_data {
-	struct hermes_rx_descriptor *desc;
-	struct sk_buff *skb;
-	struct list_head list;
-};
-
 struct firmware;
 
 struct orinoco_private {
@@ -84,7 +76,6 @@ struct orinoco_private {
 	/* Interrupt tasklets */
 	struct tasklet_struct rx_tasklet;
 	struct list_head rx_list;
-	struct orinoco_rx_data *rx_data;
 
 	/* driver state */
 	int open;
