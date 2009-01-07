7c773c9a168583df9fe584fe01f8c6aa6f5312b1
Michael Hennerich <michael.hennerich@analog.com>
2009-01-07 23:14:39 +0800

    Blackfin arch: fix bug - kernel crash after config IP for ethernet port
    Fix typo - kernel crash on bf533-stamp/bf533-ezkit after config IP for ethernet port
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/mach-bf533/include/mach/gpio.h
+++ arch/blackfin/mach-bf533/include/mach/gpio.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MACH_GPIO_H_
 #define _MACH_GPIO_H_
 
-#define MAX_BLACKFIN_GPIOS 48
+#define MAX_BLACKFIN_GPIOS 16
 
 #define	GPIO_PF0	0
 #define	GPIO_PF1	1
