4d67a8f8d2231635906e7c6a6cd21cfc0c3d6d77
Sonic Zhang <sonic.zhang@analog.com>
2009-01-07 23:14:38 +0800

    Blackfin arch: disable pthread stack check for SMP at runtime
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/include/asm/l1layout.h
+++ arch/blackfin/include/asm/l1layout.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/blackfin.h>
 
+#ifndef CONFIG_SMP
 #ifndef __ASSEMBLY__
 
 /* Data that is "mapped" into the process VM at the start of the L1 scratch
@@ -28,6 +29,7 @@ struct l1_scratch_task_info
 #define L1_SCRATCH_TASK_INFO ((struct l1_scratch_task_info *)\
 						get_l1_scratch_start())
 
+#endif
 #endif
 
 #endif
