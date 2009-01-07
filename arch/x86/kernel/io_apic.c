babfa5ac0a1e66dfb056b31dbd8f9a99df6adc10
Jaswinder Singh Rajput <jaswinder@infradead.org>
2009-01-07 21:38:59 +0530

    x86: smp.h move boot_cpu_id declartion to cpu.h
    Impact: cleanup
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/io_apic.c
+++ arch/x86/kernel/io_apic.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/idle.h>
 #include <asm/io.h>
 #include <asm/smp.h>
+#include <asm/cpu.h>
 #include <asm/desc.h>
 #include <asm/proto.h>
 #include <asm/acpi.h>
