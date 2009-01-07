9166352a5a01566ec6556bb22a787f6fe5c16893
Mike Frysinger <vapier.adi@gmail.com>
2009-01-07 23:14:38 +0800

    Blackfin arch: dma header file: strip down includes to only necessary ones
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/include/asm/dma.h
+++ arch/blackfin/include/asm/dma.h
@@ -9,16 +9,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _BLACKFIN_DMA_H_
 #define _BLACKFIN_DMA_H_
 
-#include <asm/io.h>
-#include <linux/slab.h>
-#include <asm/irq.h>
-#include <asm/signal.h>
-
-#include <linux/kernel.h>
-#include <mach/dma.h>
-#include <linux/mm.h>
 #include <linux/interrupt.h>
+#include <mach/dma.h>
 #include <asm/blackfin.h>
+#include <asm/page.h>
 
 #define MAX_DMA_ADDRESS PAGE_OFFSET
 
@@ -111,6 +105,7 @@ struct dma_register {
 
 };
 
+struct mutex;
 struct dma_channel {
 	struct mutex dmalock;
 	const char *device_id;
