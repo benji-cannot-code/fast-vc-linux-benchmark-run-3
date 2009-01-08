52132b9cac7ae6fc81b6fef9b896cabe69aec85b
NeilBrown <neilb@suse.de>
2009-01-09 08:31:11 +1100

    md: don't retry recovery of raid1 that fails due to error on source drive.
    If a raid1 has only one working drive and it has a sector which
    gives an error on read, then an attempt to recover onto a spare will
    fail, but as the single remaining drive is not removed from the
    array, the recovery will be immediately re-attempted, resulting
    in an infinite recovery loop.
    
    So detect this situation and don't retry recovery once an error
    on the lone remaining drive is detected.
    
    Allow recovery to be retried once every time a spare is added
    in case the problem wasn't actually a media error.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -1501,6 +1501,9 @@ static int bind_rdev_to_array(mdk_rdev_t * rdev, mddev_t * mddev)
 
 	list_add_rcu(&rdev->same_set, &mddev->disks);
 	bd_claim_by_disk(rdev->bdev, rdev->bdev->bd_holder, mddev->gendisk);
+
+	/* May as well allow recovery to be retried once */
+	mddev->recovery_disabled = 0;
 	return 0;
 
  fail:
@@ -6176,7 +6179,7 @@ static int remove_and_add_spares(mddev_t *mddev)
 			}
 		}
 
-	if (mddev->degraded && ! mddev->ro) {
+	if (mddev->degraded && ! mddev->ro && !mddev->recovery_disabled) {
 		list_for_each_entry(rdev, &mddev->disks, same_set) {
 			if (rdev->raid_disk >= 0 &&
 			    !test_bit(In_sync, &rdev->flags) &&
