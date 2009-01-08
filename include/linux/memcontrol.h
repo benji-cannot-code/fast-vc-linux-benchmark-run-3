5cf6470860421c8ee0ae785c1418fd6c8613f6de
KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
2009-01-07 18:08:08 -0800

    memcg: avoid unnecessary system-wide-oom-killer
    Current mmtom has new oom function as pagefault_out_of_memory().  It's
    added for select bad process rathar than killing current.
    
    When memcg hit limit and calls OOM at page_fault, this handler called and
    system-wide-oom handling happens.  (means kernel panics if panic_on_oom is
    true....)
    
    To avoid overkill, check memcg's recent behavior before starting
    system-wide-oom.
    
    And this patch also fixes to guarantee "don't accnout against process with
    TIF_MEMDIE".  This is necessary for smooth OOM.
    
    [akpm@linux-foundation.org: build fix]
    Signed-off-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Balbir Singh <balbir@in.ibm.com>
    Cc: Daisuke Nishimura <nishimura@mxp.nes.nec.co.jp>
    Cc: Badari Pulavarty <pbadari@us.ibm.com>
    Cc: Jan Blunck <jblunck@suse.de>
    Cc: Hirokazu Takahashi <taka@valinux.co.jp>
    Cc: Nick Piggin <nickpiggin@yahoo.com.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -103,6 +103,8 @@ static inline bool mem_cgroup_disabled(void)
 	return false;
 }
 
+extern bool mem_cgroup_oom_called(struct task_struct *task);
+
 #else /* CONFIG_CGROUP_MEM_RES_CTLR */
 struct mem_cgroup;
 
@@ -234,6 +236,11 @@ static inline long mem_cgroup_calc_reclaim(struct mem_cgroup *mem,
 static inline bool mem_cgroup_disabled(void)
 {
 	return true;
+}
+
+static inline bool mem_cgroup_oom_called(struct task_struct *task)
+{
+	return false;
 }
 #endif /* CONFIG_CGROUP_MEM_CONT */
 
