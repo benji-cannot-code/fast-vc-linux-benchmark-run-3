aa354ed104c9fb10cc0c591799a92e73c235bb12
Reinette Chatre <reinette.chatre@intel.com>
2009-01-08 10:20:02 -0800

    iwlwifi: update copyright year to 2009
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl3945-base.c
+++ drivers/net/wireless/iwlwifi/iwl3945-base.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /******************************************************************************
  *
- * Copyright(c) 2003 - 2008 Intel Corporation. All rights reserved.
+ * Copyright(c) 2003 - 2009 Intel Corporation. All rights reserved.
  *
  * Portions of this file are derived from the ipw3945 project, as well
  * as portions of the ieee80211 subsystem header files.
@@ -80,7 +80,7 @@ static int iwl3945_tx_queue_update_write_ptr(struct iwl_priv *priv,
 #endif
 
 #define IWL39_VERSION "1.2.26k" VD VS
-#define DRV_COPYRIGHT	"Copyright(c) 2003-2008 Intel Corporation"
+#define DRV_COPYRIGHT	"Copyright(c) 2003-2009 Intel Corporation"
 #define DRV_AUTHOR     "<ilw@linux.intel.com>"
 #define DRV_VERSION     IWL39_VERSION
 
