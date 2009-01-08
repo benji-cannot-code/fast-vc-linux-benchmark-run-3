15181e21f078a1c053771eabe0ac4fcc191cd520
Florian Fainelli <florian@openwrt.org>
2009-01-08 11:03:57 -0800

    r6040: bump release number to 0.20
    This patch bumps release number to 0.20 and 07Jan2009.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/r6040.c
+++ drivers/net/r6040.c
@@ -50,8 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 
 #define DRV_NAME	"r6040"
-#define DRV_VERSION	"0.19"
-#define DRV_RELDATE	"18Dec2008"
+#define DRV_VERSION	"0.20"
+#define DRV_RELDATE	"07Jan2009"
 
 /* PHY CHIP Address */
 #define PHY1_ADDR	1	/* For MAC1 */
