2ab4339492c58759632ac44ad477a9452737b98a
Sam Ravnborg <sam@ravnborg.org>
2009-01-08 16:58:05 -0800

    sparc: move EXPORT_SYMBOL to the symbols definition
    Move all applicable EXPORT_SYMBOL()s to the file where the respective
    symbol is defined.
    
    Removed all the includes that are no longer needed in sparc_ksyms_32.c
    
    Comment all remaining EXPORT_SYMBOL()s in sparc_ksyms_32.c
    
    Two symbols are shared with sparc64 thus the exports were removed from
    the sparc_ksyms_64.c too, along with the include their ommission made
    redundant.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    
    Additions by Julian Calaby:
    * Moved EXPORT_SYMBOL()s for prom functions to their rightful places.
    * Made some minor cleanups to the includes and comments of sparc_ksyms_32.c
    * Made another subtraction from sparc_ksyms_64.c
    * Updated and tidied commit message.
    * Rebased patch over sparc-2.6.git HEAD.
    * Ensured that all modified files have the correct includes.
    
    Signed-off-by: Julian Calaby <julian.calaby@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/sparc_ksyms_64.c
+++ arch/sparc/kernel/sparc_ksyms_64.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/io.h>
 #include <asm/irq.h>
-#include <asm/idprom.h>
 #include <asm/elf.h>
 #include <asm/head.h>
 #include <asm/smp.h>
@@ -67,9 +66,6 @@ extern int io_remap_pfn_range(struct vm_area_struct *vma, unsigned long from,
 extern int __ashrdi3(int, int);
 
 extern int dump_fpu (struct pt_regs * regs, elf_fpregset_t * fpregs);
-
-/* Per-CPU information table */
-EXPORT_PER_CPU_SYMBOL(__cpu_data);
 
 /* used by various drivers */
 #ifdef CONFIG_SMP
@@ -135,7 +131,6 @@ EXPORT_SYMBOL(die_if_kernel);
 EXPORT_SYMBOL(kernel_thread);
 
 /* prom symbols */
-EXPORT_SYMBOL(idprom);
 EXPORT_SYMBOL(prom_root_node);
 EXPORT_SYMBOL(prom_getchild);
 EXPORT_SYMBOL(prom_getsibling);
