08a7936723e847e74593ee8b7cf7e8f97558f680
Heiko Carstens <heiko.carstens@de.ibm.com>
2009-01-09 12:14:52 +0100

    [S390] types: add/fix types.h include in header files
    /include/asm/chpid.h:12: include of <linux/types.h> is preferred over <asm/types.h>
    /include/asm/chsc.h:15: found __[us]{8,16,32,64} type without #include <linux/types.h>
    /include/asm/cmb.h:28: found __[us]{8,16,32,64} type without #include <linux/types.h>
    /include/asm/dasd.h:195: found __[us]{8,16,32,64} type without #include <linux/types.h>
    /include/asm/kvm.h:16: include of <linux/types.h> is preferred over <asm/types.h>
    /include/asm/kvm.h:30: found __[us]{8,16,32,64} type without #include <linux/types.h>
    /include/asm/qeth.h:24: found __[us]{8,16,32,64} type without #include <linux/types.h>
    /include/asm/schid.h:5: found __[us]{8,16,32,64} type without #include <linux/types.h>
    /include/asm/swab.h:12: include of <linux/types.h> is preferred over <asm/types.h>
    /include/asm/swab.h:19: found __[us]{8,16,32,64} type without #include <linux/types.h>
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/dasd.h
+++ arch/s390/include/asm/dasd.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef DASD_H
 #define DASD_H
+#include <linux/types.h>
 #include <linux/ioctl.h>
 
 #define DASD_IOCTL_LETTER 'D'
