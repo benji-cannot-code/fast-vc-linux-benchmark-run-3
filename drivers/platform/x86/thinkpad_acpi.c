002242d8efc497208bba0e8fc22dfa1507a7d315
Henrique de Moraes Holschuh <hmh@hmh.eng.br>
2009-01-11 03:01:10 -0200

    ACPI: thinkpad-acpi: bump up version to 0.22
    It is about time to bump up the version.
    
    Features added since 0.21:  fan suspend/resume support, preserve radio
    state across power off (for some radio types), built-in UWB radio
    rfkill support and thermal alarm events support.
    
    Signed-off-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/platform/x86/thinkpad_acpi.c
+++ drivers/platform/x86/thinkpad_acpi.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  02110-1301, USA.
  */
 
-#define TPACPI_VERSION "0.21"
+#define TPACPI_VERSION "0.22"
 #define TPACPI_SYSFS_VERSION 0x020200
 
 /*
