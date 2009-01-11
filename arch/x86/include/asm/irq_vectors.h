83f4bbf5ad430aaf6c479ffa301ad907186398ba
Mike Travis <travis@sgi.com>
2009-01-10 22:24:07 -0800

    irq: initialize nr_irqs based on nr_cpu_ids
    Impact: Reduce memory usage.
    
    This is the second half of the changes to make the irq_desc_ptrs be
    variable sized based on nr_cpu_ids.  This is done by adding a new
    "max_nr_irqs" macro to irq_vectors.h (and a dummy in irqnr.h) to
    return a max NR_IRQS value based on NR_CPUS or nr_cpu_ids.
    
    This necessitated moving the define of MAX_IO_APICS to a separate
    file (asm/apicnum.h) so it could be included without the baggage
    of the other asm/apicdef.h declarations.
    
    Signed-off-by: Mike Travis <travis@sgi.com>

--- arch/x86/include/asm/irq_vectors.h
+++ arch/x86/include/asm/irq_vectors.h
@@ -106,6 +106,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if defined(CONFIG_X86_IO_APIC) && !defined(CONFIG_X86_VOYAGER)
 
+#include <asm/apicnum.h>	/* need MAX_IO_APICS */
+
 #ifndef CONFIG_SPARSE_IRQ
 # if NR_CPUS < MAX_IO_APICS
 #  define NR_IRQS (NR_VECTORS + (32 * NR_CPUS))
@@ -113,11 +115,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  define NR_IRQS (NR_VECTORS + (32 * MAX_IO_APICS))
 # endif
 #else
-# if (8 * NR_CPUS) > (32 * MAX_IO_APICS)
-#  define NR_IRQS (NR_VECTORS + (8 * NR_CPUS))
-# else
-#  define NR_IRQS (NR_VECTORS + (32 * MAX_IO_APICS))
-# endif
+
+/* defined as a macro so nr_irqs = max_nr_irqs(nr_cpu_ids) can be used */
+# define max_nr_irqs(nr_cpus)				\
+	((8 * nr_cpus) > (32 * MAX_IO_APICS) ?		\
+		(NR_VECTORS + (8 * NR_CPUS)) :		\
+		(NR_VECTORS + (32 * MAX_IO_APICS)))	\
+
+# define NR_IRQS max_nr_irqs(NR_CPUS)
+
 #endif
 
 #elif defined(CONFIG_X86_VOYAGER)
