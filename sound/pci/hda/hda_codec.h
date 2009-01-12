98e17d5ae8a58f10eca2362eb5c1d56389abf4b9
Takashi Iwai <tiwai@suse.de>
2009-01-12 10:09:24 +0100

    ALSA: hda - Use own workqueue
    snd-hda-intel driver used schedule_work() fot the delayed DMA pointer
    updates, but this has several potential problems:
    - it may block other eventsd works longer
    - it may deadlock when probing fails and flush_scheduled_work() is
      called during probe callback (as probe callback itself could be
      invoked from eventd)
    
    This patch adds an own workq for each driver instance to solve these
    problems.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_codec.h
+++ sound/pci/hda/hda_codec.h
@@ -615,6 +615,7 @@ struct hda_bus {
 
 	/* unsolicited event queue */
 	struct hda_bus_unsolicited *unsol;
+	struct workqueue_struct *workq;	/* common workqueue for codecs */
 
 	/* assigned PCMs */
 	DECLARE_BITMAP(pcm_dev_bits, SNDRV_PCM_DEVICES);
