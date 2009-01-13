768c0a45f847a6e8ff494056a3dbe1ff8c0883b6
Erik Andr?n <erik.andren@gmail.com>
2009-01-13 14:18:53 -0300

    V4L/DVB (11526): gspca - m5602-mt9m111: Add red balance ctrl
    Signed-off-by: Erik Andr?n <erik.andren@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/m5602/m5602_mt9m111.h
+++ drivers/media/video/gspca/m5602/m5602_mt9m111.h
@@ -96,6 +96,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEFAULT_GAIN 				283
 #define MT9M111_GREEN_GAIN_DEFAULT		0x20
 #define MT9M111_BLUE_GAIN_DEFAULT		0x20
+#define MT9M111_RED_GAIN_DEFAULT		0x20
 
 /*****************************************************************************/
 
