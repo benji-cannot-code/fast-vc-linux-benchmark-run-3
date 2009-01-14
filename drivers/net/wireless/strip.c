20722a97df704b0bf97d0139bf0fe1d480625937
Jan Engelhardt <jengelh@medozas.de>
2009-01-14 13:52:18 -0800

    net: constify VFTs
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/strip.c
+++ drivers/net/wireless/strip.c
@@ -1126,7 +1126,7 @@ static int strip_seq_show(struct seq_file *seq, void *v)
 }
 
 
-static struct seq_operations strip_seq_ops = {
+static const struct seq_operations strip_seq_ops = {
 	.start = strip_seq_start,
 	.next  = strip_seq_next,
 	.stop  = strip_seq_stop,
