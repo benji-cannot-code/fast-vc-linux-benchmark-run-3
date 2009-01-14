2fd15798b771d83d19105f4a94cedf76f982ae5d
Qinghuang Feng <qhfeng.kernel@gmail.com>
2009-01-14 15:45:13 +0800

    SYSFS: use standard magic.h for sysfs
    SYSFS_MAGIC has been added into magic.h, so only use that definition
    in magic.h to avoid potential consistency problem.
    
    Signed-off-by: Qinghuang Feng <qhfeng.kernel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/sysfs/mount.c
+++ fs/sysfs/mount.c
@@ -18,11 +18,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pagemap.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/magic.h>
 
 #include "sysfs.h"
 
-/* Random magic number */
-#define SYSFS_MAGIC 0x62656572
 
 static struct vfsmount *sysfs_mount;
 struct super_block * sysfs_sb = NULL;
