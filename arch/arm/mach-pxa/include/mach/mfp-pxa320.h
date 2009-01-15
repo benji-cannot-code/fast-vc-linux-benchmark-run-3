dd4942dd3d8390fff7c8ea8f8dd565f6cbd57047
Eric Miao <eric.miao@marvell.com>
2009-01-15 16:42:56 +0800

    [ARM] pxa: move common MFP handling code into plat-pxa
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/include/mach/mfp-pxa320.h
+++ arch/arm/mach-pxa/include/mach/mfp-pxa320.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_ARCH_MFP_PXA320_H
 #define __ASM_ARCH_MFP_PXA320_H
 
-#include <mach/mfp.h>
 #include <mach/mfp-pxa3xx.h>
 
 /* GPIO */
