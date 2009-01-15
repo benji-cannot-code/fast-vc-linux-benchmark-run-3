fe7ced2e1bc8324d6c72e42902be984b5df91157
Andy Whitcroft <apw@canonical.com>
2009-01-15 13:51:06 -0800

    checkpatch: if should not continue a preceeding brace
    We should not be continuing a braced section with an if, for example:
    
    	if (...) {
    	} if (...) {
    	}
    
    Detect this and suggest adding a newline.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2109,6 +2109,11 @@ sub process {
 				ERROR("trailing statements should be on next line\n" . $herecurr);
 			}
 		}
+# if should not continue a brace
+		if ($line =~ /}\s*if\b/) {
+			ERROR("trailing statements should be on next line\n" .
+				$herecurr);
+		}
 # case and default should not have general statements after them
 		if ($line =~ /^.\s*(?:case\s*.*|default\s*):/g &&
 		    $line !~ /\G(?:
