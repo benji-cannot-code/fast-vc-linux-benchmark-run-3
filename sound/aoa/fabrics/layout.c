31c05ba35b61426269889cd4cbb0368b4d3eb263
Johannes Berg <johannes@sipsolutions.net>
2009-01-15 18:22:31 +0100

    ALSA: snd-aoa: handle master-amp if present
    Some machines have a master amp GPIO that needs to be toggled to
    get sound output, in addition to speaker/headphone/line-out amps.
    This makes snd-aoa handle it, if present in the device tree, thus
    making snd-aoa be able to output sound on PowerMac3,6, which was
    previously handled by snd-powermac which also doesn't use the
    master amp GPIO.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/aoa/fabrics/layout.c
+++ sound/aoa/fabrics/layout.c
@@ -601,6 +601,7 @@ struct layout_dev {
 	struct snd_kcontrol *headphone_ctrl;
 	struct snd_kcontrol *lineout_ctrl;
 	struct snd_kcontrol *speaker_ctrl;
+	struct snd_kcontrol *master_ctrl;
 	struct snd_kcontrol *headphone_detected_ctrl;
 	struct snd_kcontrol *lineout_detected_ctrl;
 
@@ -652,6 +653,7 @@ static struct snd_kcontrol_new n##_ctl = {				\
 AMP_CONTROL(headphone, "Headphone Switch");
 AMP_CONTROL(speakers, "Speakers Switch");
 AMP_CONTROL(lineout, "Line-Out Switch");
+AMP_CONTROL(master, "Master Switch");
 
 static int detect_choice_get(struct snd_kcontrol *kcontrol,
 			     struct snd_ctl_elem_value *ucontrol)
@@ -892,6 +894,11 @@ static void layout_attached_codec(struct aoa_codec *codec)
  	lineout = codec->gpio->methods->get_detect(codec->gpio,
 						   AOA_NOTIFY_LINE_OUT);
 
+	if (codec->gpio->methods->set_master) {
+		ctl = snd_ctl_new1(&master_ctl, codec->gpio);
+		ldev->master_ctrl = ctl;
+		aoa_snd_ctl_add(ctl);
+	}
 	while (cc->connected) {
 		if (cc->connected & CC_SPEAKERS) {
 			if (headphones <= 0 && lineout <= 0)
