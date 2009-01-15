0174f89e90f7199d891934c4d5dbeb840ea50431
Cesar Eduardo Barros <cesarb@cesarb.net>
2009-01-14 20:34:24 -0800

    sc92031: add a link to the datasheet
    Signed-off-by: Cesar Eduardo Barros <cesarb@cesarb.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sc92031.c
+++ drivers/net/sc92031.c
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Both are almost identical and seem to be based on pci-skeleton.c
  *
  *  Rewritten for 2.6 by Cesar Eduardo Barros
+ *
+ *  A datasheet for this chip can be found at
+ *  http://www.silan.com.cn/english/products/pdf/SC92031AY.pdf
  */
 
 /* Note about set_mac_address: I don't know how to change the hardware
