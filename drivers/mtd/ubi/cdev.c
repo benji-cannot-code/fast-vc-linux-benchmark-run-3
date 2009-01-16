9de640b70eedc34b18e9844a57f6c588725a9e76
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2009-01-16 18:02:08 +0200

    UBI: remove unnecessry header inclusion
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/cdev.c
+++ drivers/mtd/ubi/cdev.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioctl.h>
 #include <linux/capability.h>
 #include <linux/uaccess.h>
-#include <linux/smp_lock.h>
 #include <mtd/ubi-user.h>
 #include <asm/div64.h>
 #include "ubi.h"
