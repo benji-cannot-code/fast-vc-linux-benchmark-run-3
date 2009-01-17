12de3a1fbf9f92810772c4836729248cb8c102ff
Ivo van Doorn <ivdoorn@gmail.com>
2009-01-17 20:42:32 +0100

    rt2x00: Update copyright year to 2009
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00link.c
+++ drivers/net/wireless/rt2x00/rt2x00link.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
-	Copyright (C) 2004 - 2008 rt2x00 SourceForge Project
+	Copyright (C) 2004 - 2009 rt2x00 SourceForge Project
 	<http://rt2x00.serialmonkey.com>
 
 	This program is free software; you can redistribute it and/or modify
