bcd9559e7de87646535e0be67a7969ff1234a0c5
Stefan Richter <stefanr@s5r6.in-berlin.de>
2009-01-19 19:20:31 +0100

    ieee1394: sbp2: update a help string
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/ieee1394/sbp2.c
+++ drivers/ieee1394/sbp2.c
@@ -116,8 +116,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 static int sbp2_max_speed = IEEE1394_SPEED_MAX;
 module_param_named(max_speed, sbp2_max_speed, int, 0644);
-MODULE_PARM_DESC(max_speed, "Force max speed "
-		 "(3 = 800Mb/s, 2 = 400Mb/s, 1 = 200Mb/s, 0 = 100Mb/s)");
+MODULE_PARM_DESC(max_speed, "Limit data transfer speed (5 <= 3200, "
+		 "4 <= 1600, 3 <= 800, 2 <= 400, 1 <= 200, 0 = 100 Mb/s)");
 
 /*
  * Set serialize_io to 0 or N to use dynamically appended lists of command ORBs.
