91294d29ce2251f402a61ec167bcb8e4ce9ab6a4
Eric Miao <eric.miao@marvell.com>
2009-01-19 15:28:07 +0800

    [ARM] pxa: remove unnecessary #include of pxa-regs.h and hardware.h
    pxa-regs.h and hardware.h are not intended for use directly in driver
    code, remove those unnecessary references.
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- drivers/pcmcia/pxa2xx_base.c
+++ drivers/pcmcia/pxa2xx_base.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/irq.h>
 #include <asm/system.h>
-#include <mach/pxa-regs.h>
 #include <mach/pxa2xx-regs.h>
 #include <asm/mach-types.h>
 
