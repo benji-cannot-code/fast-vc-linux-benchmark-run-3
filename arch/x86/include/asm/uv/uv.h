992c3cda7a2c6364d9d10279253886eff3ed815a
Nick Piggin <npiggin@suse.de>
2009-01-20 04:36:04 +0100

    x86: make UV support configurable
    Make X86 SGI Ultraviolet support configurable. Saves about 13K of text size
    on my modest config.
    
       text    data     bss     dec     hex filename
    6770537 1158680  694356 8623573  8395d5 vmlinux
    6757492 1157664  694228 8609384  835e68 vmlinux.nouv
    
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/uv/uv.h
+++ arch/x86/include/asm/uv/uv.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 enum uv_system_type {UV_NONE, UV_LEGACY_APIC, UV_X2APIC, UV_NON_UNIQUE_APIC};
 
-#ifdef CONFIG_X86_64
+#ifdef CONFIG_X86_UV
 
 extern enum uv_system_type get_uv_system_type(void);
 extern int is_uv_system(void);
@@ -16,7 +16,7 @@ extern const struct cpumask *uv_flush_tlb_others(const struct cpumask *cpumask,
 						 unsigned long va,
 						 unsigned int cpu);
 
-#else	/* X86_64 */
+#else	/* X86_UV */
 
 static inline enum uv_system_type get_uv_system_type(void) { return UV_NONE; }
 static inline int is_uv_system(void)	{ return 0; }
@@ -29,6 +29,6 @@ uv_flush_tlb_others(const struct cpumask *cpumask, struct mm_struct *mm,
 		    unsigned long va, unsigned int cpu)
 { return cpumask; }
 
-#endif	/* X86_64 */
+#endif	/* X86_UV */
 
 #endif	/* _ASM_X86_UV_UV_H */
