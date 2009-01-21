f6cd942d297e65a4351dc1f1dcdad572cc97edb5
Harvey Harrison <harvey.harrison@gmail.com>
2009-01-20 17:50:51 -0800

    m68k{nommu}: fixups after the header move
    swab.h seems to have been missed during the header merge.
    Add conditionals similar to byteorder.h and remove the
    now unnecessary byteorder_no/mm.h
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/m68k/include/asm/byteorder.h
+++ arch/m68k/include/asm/byteorder.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifdef __uClinux__
-#include "byteorder_no.h"
-#else
-#include "byteorder_mm.h"
-#endif
+#ifndef _M68K_BYTEORDER_H
+#define _M68K_BYTEORDER_H
+
+#include <linux/byteorder/big_endian.h>
+
+#endif /* _M68K_BYTEORDER_H */
