4e9ab84a4a580d0ba9d050a9c2bed6bc71a62511
Erik Andrén <erik.andren@gmail.com>
2009-01-21 13:39:17 -0300

    V4L/DVB (11636): gspca - m5602-ov7660: Design probe function
    Signed-off-by: Erik Andrén <erik.andren@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/m5602/m5602_ov7660.h
+++ drivers/media/video/gspca/m5602/m5602_ov7660.h
@@ -85,6 +85,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEFAULT_SATURATION	0x00
 #define DEFAULT_EXPOSURE	0x20
 
+/* Kernel module parameters */
+extern int force_sensor;
+extern int dump_sensor;
+
 int ov7660_probe(struct sd *sd);
 int ov7660_init(struct sd *sd);
 int ov7660_start(struct sd *sd);
