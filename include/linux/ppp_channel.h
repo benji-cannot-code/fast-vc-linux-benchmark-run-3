07d76a9024b4e291d9116f5069e263bf79464b4c
Cyrill Gorcunov <gorcunov@gmail.com>
2009-01-21 15:55:35 -0800

    net: ppp_generic - introduce net-namespace functionality v2
    - Each namespace contains ppp channels and units separately
      with appropriate locks
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/ppp_channel.h
+++ include/linux/ppp_channel.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/skbuff.h>
 #include <linux/poll.h>
+#include <net/net_namespace.h>
 
 struct ppp_channel;
 
@@ -56,6 +57,9 @@ extern void ppp_input(struct ppp_channel *, struct sk_buff *);
 /* Called by the channel when an input error occurs, indicating
    that we may have missed a packet. */
 extern void ppp_input_error(struct ppp_channel *, int code);
+
+/* Attach a channel to a given PPP unit in specified net. */
+extern int ppp_register_net_channel(struct net *, struct ppp_channel *);
 
 /* Attach a channel to a given PPP unit. */
 extern int ppp_register_channel(struct ppp_channel *);
