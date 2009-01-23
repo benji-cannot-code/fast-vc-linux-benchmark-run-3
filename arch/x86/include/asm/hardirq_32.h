5fe12293fdcb8f4819a6081033280dcffa2149f5
Brian Gerst <brgerst@gmail.com>
2009-01-23 11:03:28 +0900

    x86: remove idle_timestamp from 32bit irq_cpustat_t
    Impact: bogus irq_cpustat field removed
    
    idle_timestamp is left over from the removed irqbalance code.
    
    Signed-off-by: Brian Gerst <brgerst@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- arch/x86/include/asm/hardirq_32.h
+++ arch/x86/include/asm/hardirq_32.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 typedef struct {
 	unsigned int __softirq_pending;
-	unsigned long idle_timestamp;
 	unsigned int __nmi_count;	/* arch dependent */
 	unsigned int apic_timer_irqs;	/* arch dependent */
 	unsigned int irq0_irqs;
