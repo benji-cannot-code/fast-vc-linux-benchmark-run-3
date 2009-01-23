ef59026ee55e12d111a158c2e6342f4b8de8d12c
Ingo Molnar <mingo@elte.hu>
2009-01-23 11:09:15 +0100

    x86, xen: fix hardirq.h merge fallout
    Impact: build fix
    
    This build error:
    
     arch/x86/xen/suspend.c:22: error: implicit declaration of function 'fix_to_virt'
     arch/x86/xen/suspend.c:22: error: 'FIX_PARAVIRT_BOOTMAP' undeclared (first use in this function)
     arch/x86/xen/suspend.c:22: error: (Each undeclared identifier is reported only once
     arch/x86/xen/suspend.c:22: error: for each function it appears in.)
    
    triggers because the hardirq.h unification removed an implicit fixmap.h
    include - on which arch/x86/xen/suspend.c depended. Add the fixmap.h
    include explicitly.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/xen/suspend.c
+++ arch/x86/xen/suspend.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/xen/hypercall.h>
 #include <asm/xen/page.h>
+#include <asm/fixmap.h>
 
 #include "xen-ops.h"
 #include "mmu.h"
