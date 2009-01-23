f4ac55b1883cd8904cfdc5a0fab9251b01704d9a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-01-23 15:07:45 +0000

    ASoC: Include header file in cs4270 and wm9705
    Ensures that the DAI and socdev exported by the codec match up with
    their exported prototype.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/cs4270.c
+++ sound/soc/codecs/cs4270.c
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/initval.h>
 #include <linux/i2c.h>
 
+#include "cs4270.h"
+
 /* Private data for the CS4270 */
 struct cs4270_private {
 	unsigned int mclk; /* Input frequency of the MCLK pin */
--- sound/soc/codecs/wm9705.c
+++ sound/soc/codecs/wm9705.c
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
 
+#include "wm9705.h"
+
 /*
  * WM9705 register cache
  */
