35c325f8c1430d504ea54882fe2421b10ffd1f11
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-01-23 11:23:32 +0000

    ASoC: Fix merge with PXA tree
    Fix a merge issue caused by context overlap.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/pxa/e800_wm9712.c
+++ sound/soc/pxa/e800_wm9712.c
@@ -19,12 +19,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
 
-#include <mach/pxa-regs.h>
-#include <mach/hardware.h>
+#include <asm/mach-types.h>
 #include <mach/audio.h>
 #include <mach/eseries-gpio.h>
-
-#include <asm/mach-types.h>
 
 #include "../codecs/wm9712.h"
 #include "pxa2xx-pcm.h"
