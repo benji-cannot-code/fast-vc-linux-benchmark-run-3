11f47105c705a059aa3390aec2d0e0e6b80d7988
Brian Gerst <brgerst@gmail.com>
2009-01-23 11:03:29 +0900

    x86: remove include of apic.h from hardirq_64.h
    Impact: cleanup
    
    APIC definitions aren't needed here.  Remove the include and fix
    up the fallout.
    
    tj: added include to mce_intel_64.c.
    
    Signed-off-by: Brian Gerst <brgerst@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- arch/x86/include/asm/hardirq_64.h
+++ arch/x86/include/asm/hardirq_64.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/threads.h>
 #include <linux/irq.h>
-#include <asm/apic.h>
 
 typedef struct {
 	unsigned int __softirq_pending;
