e12ceaa04654b4083539ea6f15b165c36f515667
Kay Sievers <kay.sievers@vrfy.org>
2009-01-25 15:17:37 +0100

    driver core: get rid of struct device's bus_id string array
    Now that all users of bus_id is gone, we can remove it from struct
    device.
    
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/device.h
+++ include/linux/device.h
@@ -375,7 +375,6 @@ struct device {
 	struct device		*parent;
 
 	struct kobject kobj;
-	char	bus_id[BUS_ID_SIZE];	/* position on parent bus */
 	unsigned		uevent_suppress:1;
 	const char		*init_name; /* initial name of the device */
 	struct device_type	*type;
@@ -428,8 +427,7 @@ struct device {
 
 static inline const char *dev_name(const struct device *dev)
 {
-	/* will be changed into kobject_name(&dev->kobj) in the near future */
-	return dev->bus_id;
+	return kobject_name(&dev->kobj);
 }
 
 extern int dev_set_name(struct device *dev, const char *name, ...)
