7923b7bae8edec186ad2132d1d0860701db99787
Douglas Schilling Landgraf <dougsland@redhat.com>
2009-01-25 19:19:23 -0300

    V4L/DVB (10327): em28xx: Add check before call em28xx_isoc_audio_deinit()
    Just call em28xx_isoc_audio_deinit() if em28xx sent a usb_submit().
    
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-audio.c
+++ drivers/media/video/em28xx/em28xx-audio.c
@@ -62,7 +62,7 @@ static int em28xx_isoc_audio_deinit(struct em28xx *dev)
 	int i;
 
 	dprintk("Stopping isoc\n");
-	for (i = 0; i < EM28XX_AUDIO_BUFS; i++) {
+	for (i = 0; i < dev->isoc_ctl.num_bufs; i++) {
 		if (!irqs_disabled())
 			usb_kill_urb(dev->adev.urb[i]);
 		else
@@ -74,6 +74,7 @@ static int em28xx_isoc_audio_deinit(struct em28xx *dev)
 		dev->adev.transfer_buffer[i] = NULL;
 	}
 
+	dev->isoc_ctl.num_bufs = 0;
 	return 0;
 }
 
@@ -157,6 +158,8 @@ static int em28xx_init_audio_isoc(struct em28xx *dev)
 
 	dprintk("Starting isoc transfers\n");
 
+	dev->isoc_ctl.num_bufs = 0;
+
 	for (i = 0; i < EM28XX_AUDIO_BUFS; i++) {
 		struct urb *urb;
 		int j, k;
@@ -198,10 +201,19 @@ static int em28xx_init_audio_isoc(struct em28xx *dev)
 	for (i = 0; i < EM28XX_AUDIO_BUFS; i++) {
 		errCode = usb_submit_urb(dev->adev.urb[i], GFP_ATOMIC);
 		if (errCode) {
-			em28xx_isoc_audio_deinit(dev);
+			if (dev->isoc_ctl.num_bufs == 0) {
+				usb_free_urb(dev->adev.urb[i]);
+				dev->adev.urb[i] = NULL;
+				kfree(dev->adev.transfer_buffer[i]);
+				dev->adev.transfer_buffer[i] = NULL;
+			} else
+				em28xx_isoc_audio_deinit(dev);
 
 			return errCode;
 		}
+		mutex_lock(&dev->lock);
+		dev->isoc_ctl.num_bufs++;
+		mutex_unlock(&dev->lock);
 	}
 
 	return 0;
