1131c55f6a01cfc0f88dd31d9b92afbba6f501f7
Holger Schurig <hs4233@mail.mn-solutions.de>
2009-01-26 16:34:51 +0100

    arm/imx2x: removes a bunch of sparse-warnings
    Here are some of the warnings that get fixed by this:
    
    > 200 times: warning: cast adds address space to expression (<asn:2>)
    twelve times: warning: symbol 'xxx' was not declared. Should it be static
    two times: warning: symbol 'clock' shadows an earlier one
    five times: warning: incorrect type in initializer (different address spaces)
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/plat-mxc/devices.c
+++ arch/arm/plat-mxc/devices.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
+#include <mach/common.h>
 
 int __init mxc_register_device(struct platform_device *pdev, void *data)
 {
