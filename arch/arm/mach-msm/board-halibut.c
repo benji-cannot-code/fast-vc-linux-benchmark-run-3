99108edc84db7fb9c0833bc7ab57ea7235597b69
Russell King <rmk@dyn-67.arm.linux.org.uk>
2009-01-27 22:44:12 +0000

    [ARM] msm: fix build errors
    arch/arm/mach-msm/board-halibut.c:45: error: implicit declaration of function 'MSM_GPIO_TO_INT'
    arch/arm/mach-msm/board-halibut.c:45: error: initializer element is not constant
    arch/arm/mach-msm/board-halibut.c:45: error: (near initialization for 'smc91x_resources[1].start')
    arch/arm/mach-msm/board-halibut.c:46: error: initializer element is not constant
    arch/arm/mach-msm/board-halibut.c:46: error: (near initialization for 'smc91x_resources[1].end')
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-msm/board-halibut.c
+++ arch/arm/mach-msm/board-halibut.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/map.h>
 #include <asm/mach/flash.h>
 
+#include <mach/irqs.h>
 #include <mach/board.h>
 #include <mach/msm_iomap.h>
 
