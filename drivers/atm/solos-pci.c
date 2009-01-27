750dc3d533266981a0351e934fb7ae4562aab0ea
David Woodhouse <David.Woodhouse@intel.com>
2009-01-28 10:39:23 +1100

    solos: Fix under-allocation of skb size for get/set parameters
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/atm/solos-pci.c
+++ drivers/atm/solos-pci.c
@@ -160,7 +160,7 @@ static ssize_t solos_param_show(struct device *dev, struct device_attribute *att
 
 	buflen = strlen(attr->attr.name) + 10;
 
-	skb = alloc_skb(buflen, GFP_KERNEL);
+	skb = alloc_skb(sizeof(*header) + buflen, GFP_KERNEL);
 	if (!skb) {
 		dev_warn(&card->dev->dev, "Failed to allocate sk_buff in solos_param_show()\n");
 		return -ENOMEM;
@@ -216,7 +216,7 @@ static ssize_t solos_param_store(struct device *dev, struct device_attribute *at
 
 	buflen = strlen(attr->attr.name) + 11 + count;
 
-	skb = alloc_skb(buflen, GFP_KERNEL);
+	skb = alloc_skb(sizeof(*header) + buflen, GFP_KERNEL);
 	if (!skb) {
 		dev_warn(&card->dev->dev, "Failed to allocate sk_buff in solos_param_store()\n");
 		return -ENOMEM;
