d3de0519940f3090ee86fb4e16b36c593fab2ac1
Ingo Molnar <mingo@elte.hu>
2009-01-27 23:54:23 +0100

    x86, genapic: provide IPI callbacks unconditionally
    64-bit x86 uses the IPI callbacks even on UP - so provide them
    generally.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/genapic.h
+++ arch/x86/include/asm/genapic.h
@@ -74,7 +74,6 @@ struct genapic {
 	unsigned int (*cpu_mask_to_apicid_and)(const struct cpumask *cpumask,
 					       const struct cpumask *andmask);
 
-#ifdef CONFIG_SMP
 	/* ipi */
 	void (*send_IPI_mask)(const struct cpumask *mask, int vector);
 	void (*send_IPI_mask_allbutself)(const struct cpumask *mask,
@@ -82,7 +81,7 @@ struct genapic {
 	void (*send_IPI_allbutself)(int vector);
 	void (*send_IPI_all)(int vector);
 	void (*send_IPI_self)(int vector);
-#endif
+
 	/* wakeup_secondary_cpu */
 	int (*wakeup_cpu)(int apicid, unsigned long start_eip);
 
