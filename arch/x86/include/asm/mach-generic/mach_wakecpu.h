532b7258ad70c1551dbc3ba7fc32ddb63887de5f
Ingo Molnar <mingo@elte.hu>
2009-01-28 16:31:52 +0100

    x86, smp: refactor ->inquire_remote_apic() methods
    Nothing exciting - a few subarches dont want APIC remote reads to
    be performed - the others are content with the default method.
    
     - extend the generic code to handle NULL methods
    
     - clear out dummy methods and replace them with NULL
    
     - clean up: remove wrapper macros, etc.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/mach-generic/mach_wakecpu.h
+++ arch/x86/include/asm/mach-generic/mach_wakecpu.h
@@ -2,6 +2,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_MACH_GENERIC_MACH_WAKECPU_H
 #define _ASM_X86_MACH_GENERIC_MACH_WAKECPU_H
 
-#define inquire_remote_apic (apic->inquire_remote_apic)
-
 #endif /* _ASM_X86_MACH_GENERIC_MACH_APIC_H */
