67fe404a23c9f12b94f00d46a59f93244176d44d
Ingo Molnar <mingo@elte.hu>
2009-01-28 07:17:26 +0100

    x86, apic: streamline the ->multi_timer_check() quirk
    only NUMAQ uses this quirk: to prevent the timer IRQ from being added
    on secondary nodes.
    
    All other genapic templates can have a NULL ->multi_timer_check()
    callback.
    
    Also, extend the generic code to treat a NULL pointer accordingly.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/mach-generic/mach_apic.h
+++ arch/x86/include/asm/mach-generic/mach_apic.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/genapic.h>
 
-#define multi_timer_check (apic->multi_timer_check)
 #define apicid_to_node (apic->apicid_to_node)
 #define cpu_to_logical_apicid (apic->cpu_to_logical_apicid) 
 #define cpu_present_to_apicid (apic->cpu_present_to_apicid)
