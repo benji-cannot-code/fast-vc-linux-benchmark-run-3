ae2a310e2094e75fc7df5fab439fc9870e923206
Ingo Molnar <mingo@elte.hu>
2009-01-28 12:43:18 +0100

    x86: refactor ->enable_apic_mode() subarch methods
    Only ES7000 has a real ->enable_apic_mode() method, the other
    subarchitectures define it but keep it empty.
    
    So mark the vector as NULL, extend the generic code to handle
    NULL -setup_portio_remap() entries and remove all the empty
    handlers.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/mach-generic/mach_apic.h
+++ arch/x86/include/asm/mach-generic/mach_apic.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define cpu_mask_to_apicid (apic->cpu_mask_to_apicid)
 #define cpu_mask_to_apicid_and (apic->cpu_mask_to_apicid_and)
-#define enable_apic_mode (apic->enable_apic_mode)
 #define phys_pkg_id (apic->phys_pkg_id)
 #define wakeup_secondary_cpu (apic->wakeup_cpu)
 
--- arch/x86/kernel/apic.c
+++ arch/x86/kernel/apic.c
@@ -1764,7 +1764,8 @@ void __init connect_bsp_APIC(void)
 		outb(0x01, 0x23);
 	}
 #endif
-	enable_apic_mode();
+	if (apic->enable_apic_mode)
+		apic->enable_apic_mode();
 }
 
 /**
