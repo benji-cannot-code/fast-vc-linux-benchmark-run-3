b7da117a9a3ade91d0960585ae6b92f0a707b0d5
Sascha Hauer <s.hauer@pengutronix.de>
2009-01-28 23:03:05 +0000

    fec: use linux/*.h instead of asm/*.h
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/fec.c
+++ drivers/net/fec.c
@@ -37,15 +37,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/workqueue.h>
 #include <linux/bitops.h>
+#include <linux/io.h>
+#include <linux/irq.h>
 
-#include <asm/irq.h>
-#include <asm/uaccess.h>
-#include <asm/io.h>
-#include <asm/pgtable.h>
 #include <asm/cacheflush.h>
-
 #include <asm/coldfire.h>
 #include <asm/mcfsim.h>
+
 #include "fec.h"
 
 #if defined(CONFIG_FEC2)
