47e198fc72efb5ed09fda9782e8be18893cfce29
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-01-28 21:53:17 +0900

    IA64: fix VT-d dma_mapping_error
    dma_mapping_error is used to see if dma_map_single and dma_map_page
    succeed. IA64 VT-d dma_mapping_error always says that dma_map_single
    is successful even though it could fail. Note that X86 VT-d works
    properly in this regard.
    
    This patch fixes IA64 VT-d dma_mapping_error by adding VT-d's own
    dma_mapping_error() that works for both X86_64 and IA64. VT-d uses
    zero as an error dma address so VT-d's dma_mapping_error returns 1 if
    a passed dma address is zero (as x86's VT-d dma_mapping_error does
    now).
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/pci/intel-iommu.c
+++ drivers/pci/intel-iommu.c
@@ -2582,6 +2582,11 @@ int intel_map_sg(struct device *hwdev, struct scatterlist *sglist, int nelems,
 	return nelems;
 }
 
+static int intel_mapping_error(struct device *dev, dma_addr_t dma_addr)
+{
+	return !dma_addr;
+}
+
 struct dma_map_ops intel_dma_ops = {
 	.alloc_coherent = intel_alloc_coherent,
 	.free_coherent = intel_free_coherent,
@@ -2589,6 +2594,7 @@ struct dma_map_ops intel_dma_ops = {
 	.unmap_sg = intel_unmap_sg,
 	.map_page = intel_map_page,
 	.unmap_page = intel_unmap_page,
+	.mapping_error = intel_mapping_error,
 };
 
 static inline int iommu_domain_cache_init(void)
