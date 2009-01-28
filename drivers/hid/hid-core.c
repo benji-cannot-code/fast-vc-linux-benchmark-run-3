92a3ce5114bda5541cbbeb51b7d086cabd73c773
Jiri Kosina <jkosina@suse.cz>
2009-01-28 23:06:37 +0100

    HID: document difference between hid_blacklist and hid_ignore_list
    Many people get it wrong and add device IDs into hid_blacklist instead
    of hid_ignore_list. Let's put a little comment in place.
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -1219,6 +1219,7 @@ int hid_connect(struct hid_device *hdev, unsigned int connect_mask)
 }
 EXPORT_SYMBOL_GPL(hid_connect);
 
+/* a list of devices for which there is a specialized driver on HID bus */
 static const struct hid_device_id hid_blacklist[] = {
 	{ HID_USB_DEVICE(USB_VENDOR_ID_A4TECH, USB_DEVICE_ID_A4TECH_WCP32PU) },
 	{ HID_USB_DEVICE(USB_VENDOR_ID_A4TECH, USB_DEVICE_ID_A4TECH_X5_005D) },
@@ -1477,6 +1478,7 @@ static struct bus_type hid_bus_type = {
 	.uevent		= hid_uevent,
 };
 
+/* a list of devices that shouldn't be handled by HID core at all */
 static const struct hid_device_id hid_ignore_list[] = {
 	{ HID_USB_DEVICE(USB_VENDOR_ID_ACECAD, USB_DEVICE_ID_ACECAD_FLAIR) },
 	{ HID_USB_DEVICE(USB_VENDOR_ID_ACECAD, USB_DEVICE_ID_ACECAD_302) },
