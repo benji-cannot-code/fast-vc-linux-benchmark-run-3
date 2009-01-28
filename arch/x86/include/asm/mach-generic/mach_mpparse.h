105cfcfcc3c296de1187187a1f9b0d22ab907027
Ingo Molnar <mingo@elte.hu>
2009-01-28 03:43:47 +0100

    x86: clean up apic->acpi_madt_oem_check methods
    Impact: refactor code
    
    x86 subarchitectures each defined a "acpi_madt_oem_check()" method,
    which could be an inline function, or an extern, or a static function,
    and which was also the name of a genapic field.
    
    Untangle this namespace spaghetti by setting ->acpi_madt_oem_check()
    to NULL on those subarchitectures that have no detection quirks,
    and rename the other ones (summit, es7000) that do.
    
    Also change default_acpi_madt_oem_check() to handle NULL entries,
    and clean its control flow up as well.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/mach-generic/mach_mpparse.h
+++ arch/x86/include/asm/mach-generic/mach_mpparse.h
@@ -5,6 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int mps_oem_check(struct mpc_table *, char *, char *);
 
-extern int acpi_madt_oem_check(char *, char *);
+extern int default_acpi_madt_oem_check(char *, char *);
 
 #endif /* _ASM_X86_MACH_GENERIC_MACH_MPPARSE_H */
