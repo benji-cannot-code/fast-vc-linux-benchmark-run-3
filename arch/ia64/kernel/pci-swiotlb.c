094f0f2769aa9528ea1e968a693aa091d3c06f08
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-01-28 21:53:16 +0900

    IA64: fix swiotlb alloc_coherent for non DMA_64BIT_MASK devices
    Before the dma ops unification, IA64 always uses GFP_DMA for
    dma_alloc_coherent like:
    
    #define dma_alloc_coherent(dev, size, handle, gfp)	\
    	platform_dma_alloc_coherent(dev, size, handle, (gfp) | GFP_DMA)
    
    This GFP_DMA enforcement doesn't make sense for IOMMUs since they can
    do address translation to give addresses that devices can access
    to. The IOMMU drivers ignore the zone flag. However, this is still
    necessary for swiotlb since it can't do address translation.
    
    We don't always need to use GFP_DMA for swiotlb. We need GFP_DMA for
    devices incapable of 64bit DMA.
    
    This patch is sorta updated version of:
    
    http://marc.info/?l=linux-kernel&m=122638215612705&w=2
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/ia64/kernel/pci-swiotlb.c
+++ arch/ia64/kernel/pci-swiotlb.c
@@ -14,8 +14,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int swiotlb __read_mostly;
 EXPORT_SYMBOL(swiotlb);
 
+static void *ia64_swiotlb_alloc_coherent(struct device *dev, size_t size,
+					 dma_addr_t *dma_handle, gfp_t gfp)
+{
+	if (dev->coherent_dma_mask != DMA_64BIT_MASK)
+		gfp |= GFP_DMA;
+	return swiotlb_alloc_coherent(dev, size, dma_handle, gfp);
+}
+
 struct dma_map_ops swiotlb_dma_ops = {
-	.alloc_coherent = swiotlb_alloc_coherent,
+	.alloc_coherent = ia64_swiotlb_alloc_coherent,
 	.free_coherent = swiotlb_free_coherent,
 	.map_page = swiotlb_map_page,
 	.unmap_page = swiotlb_unmap_page,
