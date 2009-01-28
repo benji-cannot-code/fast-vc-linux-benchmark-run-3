e339efd0f8a7b5c25493f467af225e4bbb168587
Ingo Molnar <mingo@elte.hu>
2009-01-28 05:08:44 +0100

    x86: rename ->ESR_DISABLE to ->disable_esr
    the ->ESR_DISABLE shouting variant was used to enable the esr_disable
    macro wrappers. Those ugly macros are removed now so we can rename
    ->ESR_DISABLE to ->disable_esr
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic.c
+++ arch/x86/kernel/apic.c
@@ -1108,7 +1108,7 @@ static void __cpuinit lapic_setup_esr(void)
 		return;
 	}
 
-	if (apic->ESR_DISABLE) {
+	if (apic->disable_esr) {
 		/*
 		 * Something untraceable is creating bad interrupts on
 		 * secondary quads ... for the moment, just leave the
@@ -1158,7 +1158,7 @@ void __cpuinit setup_local_APIC(void)
 
 #ifdef CONFIG_X86_32
 	/* Pound the ESR really hard over the head with a big hammer - mbligh */
-	if (lapic_is_integrated() && apic->ESR_DISABLE) {
+	if (lapic_is_integrated() && apic->disable_esr) {
 		apic_write(APIC_ESR, 0);
 		apic_write(APIC_ESR, 0);
 		apic_write(APIC_ESR, 0);
