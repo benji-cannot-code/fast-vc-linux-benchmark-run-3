3ed6ceca31461018161e6f8a7525aa181cf7c4fe
Trent Piepho <xyzzy@speakeasy.org>
2009-01-28 21:32:58 -0300

    V4L/DVB (10558): bttv: norm value should be unsigned
    The norm value in the driver is an index into an array and the the driver
    doesn't allow it to be negative or otherwise invalid.  It should be
    unsigned but wasn't in all places.
    
    Fix some structs and functions to have the norm be unsigned.  Get rid of
    useless checks for "< 0".  Most of the driver code can't handle a norm
    value that's out of range, so change some ">= BTTV_TVNORMS" checks to
    BUG_ON().  There's no point in silently ignoring invalid driver state just
    to crash because of it later.
    
    Reported-by: Roel Kluin <roel.kluin@gmail.com>
    Signed-off-by: Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/bt8xx/bttv-driver.c
+++ drivers/media/video/bt8xx/bttv-driver.c
@@ -1278,7 +1278,7 @@ bttv_crop_calc_limits(struct bttv_crop *c)
 }
 
 static void
-bttv_crop_reset(struct bttv_crop *c, int norm)
+bttv_crop_reset(struct bttv_crop *c, unsigned int norm)
 {
 	c->rect = bttv_tvnorms[norm].cropcap.defrect;
 	bttv_crop_calc_limits(c);
@@ -1291,16 +1291,13 @@ set_tvnorm(struct bttv *btv, unsigned int norm)
 	const struct bttv_tvnorm *tvnorm;
 	v4l2_std_id id;
 
-	if (norm < 0 || norm >= BTTV_TVNORMS)
-		return -EINVAL;
+	BUG_ON(norm >= BTTV_TVNORMS);
+	BUG_ON(btv->tvnorm >= BTTV_TVNORMS);
 
 	tvnorm = &bttv_tvnorms[norm];
 
-	if (btv->tvnorm < 0 ||
-	    btv->tvnorm >= BTTV_TVNORMS ||
-	    0 != memcmp(&bttv_tvnorms[btv->tvnorm].cropcap,
-			&tvnorm->cropcap,
-			sizeof (tvnorm->cropcap))) {
+	if (!memcmp(&bttv_tvnorms[btv->tvnorm].cropcap, &tvnorm->cropcap,
+		    sizeof (tvnorm->cropcap))) {
 		bttv_crop_reset(&btv->crop[0], norm);
 		btv->crop[1] = btv->crop[0]; /* current = default */
 
