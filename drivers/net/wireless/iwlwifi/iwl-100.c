26423b0dfb8d27997287b4f4c280fa8d6d697fa1
Jay Sternberg <jay.e.sternberg@linux.intel.com>
2009-01-29 11:09:14 -0800

    iwlwifi: correct API command overlap
    Correct the API commands where same command id used for two different
    commands. Update max api versions for affected devices.
    
    TX_ANT_CONFIGURATION_CMD was already using id 0x98, so
    REPLY_TX_POWER_DBM_CMD moved to 0x95
    
    Older API interfaces may used original value so V1 defines provided.
    
    Signed-off-by: Jay Sternberg <jay.e.sternberg@linux.intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-100.c
+++ drivers/net/wireless/iwlwifi/iwl-100.c
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-5000-hw.h"
 
 /* Highest firmware API version supported */
-#define IWL100_UCODE_API_MAX 1
+#define IWL100_UCODE_API_MAX 2
 
 /* Lowest firmware API version supported */
 #define IWL100_UCODE_API_MIN 1
--- drivers/net/wireless/iwlwifi/iwl-6000.c
+++ drivers/net/wireless/iwlwifi/iwl-6000.c
@@ -47,8 +47,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-5000-hw.h"
 
 /* Highest firmware API version supported */
-#define IWL6000_UCODE_API_MAX 1
-#define IWL6050_UCODE_API_MAX 1
+#define IWL6000_UCODE_API_MAX 2
+#define IWL6050_UCODE_API_MAX 2
 
 /* Lowest firmware API version supported */
 #define IWL6000_UCODE_API_MIN 1
