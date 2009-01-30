13fc06553f7f78e6e906e59b1412315d2cc9368c
Chris Wilson <chris@chris-wilson.co.uk>
2009-01-30 21:10:22 +0000

    drm/i915: Unref the object after failing to set tiling mode.
    Cleanup the object reference on the error paths.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/gpu/drm/i915/i915_gem_tiling.c
+++ drivers/gpu/drm/i915/i915_gem_tiling.c
@@ -259,8 +259,10 @@ i915_gem_set_tiling(struct drm_device *dev, void *data,
 		return -EINVAL;
 	obj_priv = obj->driver_private;
 
-	if (!i915_tiling_ok(dev, args->stride, obj->size, args->tiling_mode))
+	if (!i915_tiling_ok(dev, args->stride, obj->size, args->tiling_mode)) {
+		drm_gem_object_unreference(obj);
 		return -EINVAL;
+	}
 
 	mutex_lock(&dev->struct_mutex);
 
@@ -290,6 +292,7 @@ i915_gem_set_tiling(struct drm_device *dev, void *data,
 			     "failed to unbind object for tiling switch");
 			args->tiling_mode = obj_priv->tiling_mode;
 			mutex_unlock(&dev->struct_mutex);
+			drm_gem_object_unreference(obj);
 
 			return ret;
 		}
