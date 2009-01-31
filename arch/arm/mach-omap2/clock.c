e08a68c653b5cc2dd8f67df9113b6957ef84263a
Russell King <rmk@dyn-67.arm.linux.org.uk>
2009-01-31 11:02:37 +0000

    [ARM] omap: fix clockdomain enable/disable ordering
    Based on a patch from Paul Walmsley <paul@pwsan.com>:
    
     omap2_clk_enable() should enable a clock's clockdomain before
     attempting to enable its parent clock's clockdomain.  Similarly, in
     the unlikely event that the parent clock enable fails, the clockdomain
     should be disabled.
    
     linux-omap source commit is 6d6e285e5a7912b1ea68fadac387304c914aaba8.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-omap2/clock.c
+++ arch/arm/mach-omap2/clock.c
@@ -420,20 +420,17 @@ int omap2_clk_enable(struct clk *clk)
 	int ret = 0;
 
 	if (clk->usecount++ == 0) {
+		if (clk->clkdm)
+			omap2_clkdm_clk_enable(clk->clkdm, clk);
+
 		if (clk->parent) {
 			ret = omap2_clk_enable(clk->parent);
 			if (ret)
 				goto err;
 		}
 
-		if (clk->clkdm)
-			omap2_clkdm_clk_enable(clk->clkdm, clk);
-
 		ret = _omap2_clk_enable(clk);
 		if (ret) {
-			if (clk->clkdm)
-				omap2_clkdm_clk_disable(clk->clkdm, clk);
-
 			if (clk->parent)
 				omap2_clk_disable(clk->parent);
 
@@ -443,6 +440,8 @@ int omap2_clk_enable(struct clk *clk)
 	return ret;
 
 err:
+	if (clk->clkdm)
+		omap2_clkdm_clk_disable(clk->clkdm, clk);
 	clk->usecount--;
 	return ret;
 }
