cc3380f48a44d35476d3345199a78b84a18c50a7
Randy Dunlap <randy.dunlap@oracle.com>
2009-01-31 12:32:26 -0800

    x86: UV: fix header struct usage
    Impact: Fixes warning
    
    Fix uv.h struct usage:
    
    arch/x86/include/asm/uv/uv.h:16: warning: 'struct mm_struct' declared inside parameter list
    arch/x86/include/asm/uv/uv.h:16: warning: its scope is only this definition or declaration, which is probably not what you want
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/uv/uv.h
+++ arch/x86/include/asm/uv/uv.h
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 enum uv_system_type {UV_NONE, UV_LEGACY_APIC, UV_X2APIC, UV_NON_UNIQUE_APIC};
 
+struct cpumask;
+struct mm_struct;
+
 #ifdef CONFIG_X86_UV
 
 extern enum uv_system_type get_uv_system_type(void);
