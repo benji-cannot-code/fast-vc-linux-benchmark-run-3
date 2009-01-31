9108ccb930f68e912305376a66493c5ef3891b6d
Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
2009-01-31 10:38:16 +0530

    headers_check fix: arm, a.out.h
    fix the following 'make headers_check' warnings:
    
      usr/include/asm-arm/a.out.h:5: include of <linux/types.h> is preferred over <asm/types.h>
      usr/include/asm-arm/a.out.h:9: found __[us]{8,16,32,64} type without #include <linux/types.h>
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>

--- arch/arm/include/asm/a.out.h
+++ arch/arm/include/asm/a.out.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARM_A_OUT_H__
 
 #include <linux/personality.h>
-#include <asm/types.h>
+#include <linux/types.h>
 
 struct exec
 {
