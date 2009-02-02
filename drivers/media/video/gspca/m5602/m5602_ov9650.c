cca22fc467aa68776d3a38424bdbe743836e2848
Erik Andrén <erik.andren@gmail.com>
2009-02-02 16:08:06 -0300

    V4L/DVB (11687): gspca - m5602-ov9650: Add missing v4l2 ctrl ids
    Red and blue balance missed their id fields
    
    Signed-off-by: Erik Andrén <erik.andren@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/m5602/m5602_ov9650.c
+++ drivers/media/video/gspca/m5602/m5602_ov9650.c
@@ -132,6 +132,7 @@ static const struct ctrl ov9650_ctrls[] = {
 #define RED_BALANCE_IDX 2
 	{
 		{
+			.id		= V4L2_CID_RED_BALANCE,
 			.type 		= V4L2_CTRL_TYPE_INTEGER,
 			.name 		= "red balance",
 			.minimum 	= 0x00,
@@ -146,6 +147,7 @@ static const struct ctrl ov9650_ctrls[] = {
 #define BLUE_BALANCE_IDX 3
 	{
 		{
+			.id		= V4L2_CID_BLUE_BALANCE,
 			.type 		= V4L2_CTRL_TYPE_INTEGER,
 			.name 		= "blue balance",
 			.minimum 	= 0x00,
