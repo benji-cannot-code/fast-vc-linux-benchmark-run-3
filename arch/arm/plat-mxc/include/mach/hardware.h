c13179adaee32c2c1927f454f2e5862a7c2417c9
Sascha Hauer <s.hauer@pengutronix.de>
2009-02-02 14:11:54 +0100

    [ARM] MX35: Add register definitions for the i.MX35
    This patch moves the stuff common to i.MX31 and i.MX35 to mx3x.h and the
    specifics to mx31.h/mx35.h. We can build a kernel which runs on i.MX31 and
    i.MX35, so always include mx31.h and mx35.h
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/hardware.h
+++ arch/arm/plat-mxc/include/mach/hardware.h
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sizes.h>
 
 #ifdef CONFIG_ARCH_MX3
-# include <mach/mx31.h>
+#include <mach/mx3x.h>
+#include <mach/mx31.h>
+#include <mach/mx35.h>
 #endif
 
 #ifdef CONFIG_ARCH_MX2
