6a6f4e7ec1904d7fbaa584074ae8d0b84016e004
Eric Anholt <eric@anholt.net>
2009-02-03 12:10:21 -0800

    drm/i915: Quiet the message on get/setparam ioctl with an unknown value.
    Getting an unknown get/setparam used to be more significant back when they
    didn't change much.  However, now that we're in the git world we're using
    them instead of a monotonic version number to signal feature availability,
    so clients ask about unknown params on older kernels more often.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -736,7 +736,7 @@ static int i915_getparam(struct drm_device *dev, void *data,
 		value = dev_priv->num_fence_regs - dev_priv->fence_reg_start;
 		break;
 	default:
-		DRM_ERROR("Unknown parameter %d\n", param->param);
+		DRM_DEBUG("Unknown parameter %d\n", param->param);
 		return -EINVAL;
 	}
 
@@ -776,7 +776,7 @@ static int i915_setparam(struct drm_device *dev, void *data,
 		dev_priv->fence_reg_start = param->value;
 		break;
 	default:
-		DRM_ERROR("unknown parameter %d\n", param->param);
+		DRM_DEBUG("unknown parameter %d\n", param->param);
 		return -EINVAL;
 	}
 
