71a342105a244ab36096c8c51a74f33d78fca2ee
Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
2009-02-03 19:28:24 +0530

    headers_check fix cleanup: linux/nubus.h
    These are only for kernel internals as pointed by Arnd Bergmann:
       struct nubus_board
       struct nubus_dev
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>

--- include/linux/nubus.h
+++ include/linux/nubus.h
@@ -238,6 +238,7 @@ struct nubus_dirent
 	int mask;
 };
 
+#ifdef __KERNEL__
 struct nubus_board {
 	struct nubus_board* next;
 	struct nubus_dev* first_dev;
@@ -297,7 +298,6 @@ struct nubus_dev {
 	struct nubus_board* board;
 };
 
-#ifdef __KERNEL__
 /* This is all NuBus devices (used to find devices later on) */
 extern struct nubus_dev* nubus_devices;
 /* This is all NuBus cards */
