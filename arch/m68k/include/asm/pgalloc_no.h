ae570313e8a8ca2f2a447ca7d1cb6936353859ef
Greg Ungerer <gerg@uclinux.org>
2009-02-05 09:58:44 +1000

    m68knommu: mv definition of check_pgt_cache()
    Move the definition of check_pgt_cache() to be consistent with where
    m68k defines it. (Will make merging of these headers easier later on).
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/asm/pgalloc_no.h
+++ arch/m68k/include/asm/pgalloc_no.h
@@ -4,6 +4,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/setup.h>
 
-#define check_pgt_cache()	do { } while (0)
-
 #endif /* _M68KNOMMU_PGALLOC_H */
