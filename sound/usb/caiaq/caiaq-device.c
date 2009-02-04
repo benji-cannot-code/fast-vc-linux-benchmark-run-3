1d93d2002ff25206da263966022510c7055848e2
Mark Hills <mark@pogo.org.uk>
2009-02-04 22:34:34 +0000

    ALSA: snd-usb-caiaq: Increase version number to 1.3.12
    Indicates fixes affecting control messages and switching of input mode
    on Audio 8 DJ and Audio 4 DJ.
    
    Signed-off-by: Mark Hills <mark@pogo.org.uk>
    Acked-by: Daniel Mack <daniel@caiaq.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/caiaq/caiaq-device.c
+++ sound/usb/caiaq/caiaq-device.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 MODULE_AUTHOR("Daniel Mack <daniel@caiaq.de>");
-MODULE_DESCRIPTION("caiaq USB audio, version 1.3.11");
+MODULE_DESCRIPTION("caiaq USB audio, version 1.3.12");
 MODULE_LICENSE("GPL");
 MODULE_SUPPORTED_DEVICE("{{Native Instruments, RigKontrol2},"
 			 "{Native Instruments, RigKontrol3},"
