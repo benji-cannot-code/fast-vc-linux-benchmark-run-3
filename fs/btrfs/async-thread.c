8438b0c9ff8bf8f6ea695a46ec520b74b27aa6ba
Chris Mason <chris.mason@oracle.com>
2009-02-04 09:19:41 -0500

    Btrfs: Catch missed bios in the async bio submission thread
    The async bio submission thread was missing some bios that were
    added after it had decided there was no work left to do.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/async-thread.c
+++ fs/btrfs/async-thread.c
@@ -350,6 +350,7 @@ int btrfs_requeue_work(struct btrfs_work *work)
 {
 	struct btrfs_worker_thread *worker = work->worker;
 	unsigned long flags;
+	int wake = 0;
 
 	if (test_and_set_bit(WORK_QUEUED_BIT, &work->flags))
 		goto out;
@@ -368,10 +369,16 @@ int btrfs_requeue_work(struct btrfs_work *work)
 			       &worker->workers->worker_list);
 		spin_unlock_irqrestore(&worker->workers->lock, flags);
 	}
+	if (!worker->working) {
+		wake = 1;
+		worker->working = 1;
+	}
 
 	spin_unlock_irqrestore(&worker->lock, flags);
-
+	if (wake)
+		wake_up_process(worker->task);
 out:
+
 	return 0;
 }
 
@@ -398,6 +405,7 @@ int btrfs_queue_worker(struct btrfs_workers *workers, struct btrfs_work *work)
 	}
 
 	spin_lock_irqsave(&worker->lock, flags);
+
 	atomic_inc(&worker->num_pending);
 	check_busy_worker(worker);
 	list_add_tail(&work->list, &worker->pending);
