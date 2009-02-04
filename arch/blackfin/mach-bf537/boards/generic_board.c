506e0bdec06f39372395aa9f4f366d66ed543c05
Mike Frysinger <vapier.adi@gmail.com>
2009-02-04 16:49:45 +0800

    Blackfin arch: fixup board init function name
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/mach-bf537/boards/generic_board.c
+++ arch/blackfin/mach-bf537/boards/generic_board.c
@@ -709,7 +709,7 @@ static struct platform_device *stamp_devices[] __initdata = {
 #endif
 };
 
-static int __init stamp_init(void)
+static int __init generic_init(void)
 {
 	printk(KERN_INFO "%s(): registering device resources\n", __func__);
 	platform_add_devices(stamp_devices, ARRAY_SIZE(stamp_devices));
@@ -721,7 +721,7 @@ static int __init stamp_init(void)
 	return 0;
 }
 
-arch_initcall(stamp_init);
+arch_initcall(generic_init);
 
 void native_machine_restart(char *cmd)
 {
