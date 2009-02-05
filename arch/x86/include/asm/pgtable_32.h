e8e1279663624072d10de146b41089607c496377
Jeremy Fitzhardinge <jeremy@goop.org>
2009-02-05 11:31:15 -0800

    x86: unify io_remap_pfn_range
    Impact: cleanup
    
    Unify io_remap_pfn_range.  Don't demacro yet.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- arch/x86/include/asm/pgtable_32.h
+++ arch/x86/include/asm/pgtable_32.h
@@ -134,7 +134,4 @@ do {						\
 #define kern_addr_valid(kaddr)	(0)
 #endif
 
-#define io_remap_pfn_range(vma, vaddr, pfn, size, prot)	\
-	remap_pfn_range(vma, vaddr, pfn, size, prot)
-
 #endif /* _ASM_X86_PGTABLE_32_H */
