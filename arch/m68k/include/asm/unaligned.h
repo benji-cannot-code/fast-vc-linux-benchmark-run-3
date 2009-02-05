ffc19af8209ae3e0767d6d0f69b4c7e103fd906e
Geert Uytterhoeven <geert@linux-m68k.org>
2009-02-05 10:07:46 +1000

    m68k: Restore correct include guards for <asm/unaligned.h>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/unaligned.h
+++ arch/m68k/include/asm/unaligned.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _ASM_M68KNOMMU_UNALIGNED_H
-#define _ASM_M68KNOMMU_UNALIGNED_H
+#ifndef _ASM_M68K_UNALIGNED_H
+#define _ASM_M68K_UNALIGNED_H
 
 
 #ifdef CONFIG_COLDFIRE
@@ -23,4 +23,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #endif
 
-#endif /* _ASM_M68KNOMMU_UNALIGNED_H */
+#endif /* _ASM_M68K_UNALIGNED_H */
