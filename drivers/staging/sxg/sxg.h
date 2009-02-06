c0de89b9fdb8ef65ba1a32ae8731f22c3d40c0a8
Mithlesh Thukral <mithlesh@linsyssoft.com>
2009-02-06 19:31:40 +0530

    Staging: sxg: Removed unnecessary checks while taking Transmit Locks
    Fix the locking issue of locks in transmit code path.
    There was an unnecessary check for interrupt context in transmit code path.
    Removed that.
    
    Signed-off-by: LinSysSoft Sahara Team <saharaproj@linsyssoft.com>
    Signed-off-by: Mithlesh Thukral <mithlesh@linsyssoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/sxg/sxg.h
+++ drivers/staging/sxg/sxg.h
@@ -245,20 +245,14 @@ struct sxg_stats {
 }
 
 /* SGL macros */
-#define SXG_FREE_SGL_BUFFER(_pAdapt, _Sgl, _NB, _irq) {				\
-	if(!_irq)								\
-		spin_lock_irqsave(&(_pAdapt)->SglQLock, sgl_flags);		\
-	else									\
-		spin_lock(&(_pAdapt)->SglQLock);				\
+#define SXG_FREE_SGL_BUFFER(_pAdapt, _Sgl, _NB) {				\
+	spin_lock_irqsave(&(_pAdapt)->SglQLock, sgl_flags);			\
 	(_pAdapt)->FreeSglBufferCount++;					\
 	ASSERT((_pAdapt)->AllSglBufferCount >= (_pAdapt)->FreeSglBufferCount);	\
 	ASSERT(!((_Sgl)->State & SXG_BUFFER_FREE));				\
 	(_Sgl)->State = SXG_BUFFER_FREE;					\
 	InsertTailList(&(_pAdapt)->FreeSglBuffers, &(_Sgl)->FreeList);		\
-	if(!_irq)								\
-		spin_unlock_irqrestore(&(_pAdapt)->SglQLock, sgl_flags);	\
-	else									\
-		spin_unlock(&(_pAdapt)->SglQLock);				\
+	spin_unlock_irqrestore(&(_pAdapt)->SglQLock, sgl_flags);		\
 }
 
 /*
@@ -281,7 +275,7 @@ struct sxg_stats {
 	if(!_irq)							\
 		spin_lock_irqsave(&(_pAdapt)->SglQLock, sgl_flags);	\
 	else								\
-		spin_lock(&(_pAdapt)->SglQLock);			\
+		spin_lock_irqsave(&(_pAdapt)->SglQLock, sgl_flags);	\
 	if((_pAdapt)->FreeSglBufferCount) {				\
 		ASSERT(!(IsListEmpty(&(_pAdapt)->FreeSglBuffers)));	\
 		_ple = RemoveHeadList(&(_pAdapt)->FreeSglBuffers);	\
@@ -295,7 +289,7 @@ struct sxg_stats {
 	if(!_irq)							\
 		spin_unlock_irqrestore(&(_pAdapt)->SglQLock, sgl_flags);\
 	else								\
-		spin_unlock(&(_pAdapt)->SglQLock);			\
+		spin_unlock_irqrestore(&(_pAdapt)->SglQLock, sgl_flags);\
 }
 
 /*
