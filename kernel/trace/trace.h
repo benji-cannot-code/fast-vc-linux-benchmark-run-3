0f3d40e2a1c9120d7d2a243e936a587ffaadcaab
Frederic Weisbecker <fweisbec@gmail.com>
2009-02-07 22:16:12 +0100

    tracing/power: move the power trace headers to a dedicated file
    Impact: cleanup
    
    Move the power tracer headers to trace/power.h to keep ftrace.h and power bits
    more easy to maintain as separated topics.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Arjan van de Ven <arjan@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.h
+++ kernel/trace/trace.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ftrace.h>
 #include <trace/boot.h>
 #include <trace/kmemtrace.h>
+#include <trace/power.h>
 
 enum trace_type {
 	__TRACE_FIRST_TYPE = 0,
--- kernel/trace/trace_power.c
+++ kernel/trace/trace_power.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/init.h>
 #include <linux/debugfs.h>
-#include <linux/ftrace.h>
+#include <trace/power.h>
 #include <linux/kallsyms.h>
 #include <linux/module.h>
 
