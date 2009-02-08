77533bd3bf8a544d8da804c477817c17c332ea35
Michael Neuling <mikey@neuling.org>
2009-02-08 13:04:14 +0000

    powerpc/cell: Add missing #include for oprofile
    arch/powerpc/oprofile/cell/spu_profiler.c is missing a asm/time.h
    include which is required for ppc_proc_freq.  This can cause compile
    failures for some config combinations.
    
    Signed-off-by: Michael Neuling <mikey@neuling.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/oprofile/cell/spu_profiler.c
+++ arch/powerpc/oprofile/cell/spu_profiler.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smp.h>
 #include <linux/slab.h>
 #include <asm/cell-pmu.h>
+#include <asm/time.h>
 #include "pr_util.h"
 
 #define SCALE_SHIFT 14
