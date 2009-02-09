90fcafbe46d9e07e0622d66d411d0b2ea131c4ae
Nick Kossifidis <mick@madwifi-project.org>
2009-02-09 06:17:45 +0200

    ath5k: Don't call reset on config_interface
     * We call reset too often and this can result various PHY problems.
      On config_interface we don't need to reset the whole device.
    
      TODO: Create a function hw_fast_reset that only resets the PCU
      (tx/rx stuff) and not the whole device so that we can use this for
      stuck beacons etc.
    
      Signed-off-by: Nick Kossifidis <mickflemm@gmail.com>
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath5k/base.c
+++ drivers/net/wireless/ath5k/base.c
@@ -2885,7 +2885,7 @@ ath5k_config_interface(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 {
 	struct ath5k_softc *sc = hw->priv;
 	struct ath5k_hw *ah = sc->ah;
-	int ret;
+	int ret = 0;
 
 	mutex_lock(&sc->lock);
 	if (sc->vif != vif) {
@@ -2911,9 +2911,7 @@ ath5k_config_interface(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 		}
 		ath5k_beacon_update(sc, beacon);
 	}
-	mutex_unlock(&sc->lock);
 
-	return ath5k_reset_wake(sc);
 unlock:
 	mutex_unlock(&sc->lock);
 	return ret;
