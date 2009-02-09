34a5153fe5f9d915c319b16d827316f58e7179b7
Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
2009-02-09 16:00:04 +0900

    PCI/ACPI: rename pci_osc_control_set()
    - Rename pci_osc_control_set() to acpi_pci_osc_control_set() according
      to the other API names in drivers/acpi/pci_root.c.
    
    - Move _OSC related definitions to include/linux/acpi.h because _OSC
      related API is implemented in drivers/acpi/pci_root.c now.
    
    Signed-off-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Reviewed-by: Andrew Patterson <andrew.patterson@hp.com>
    Tested-by: Andrew Patterson <andrew.patterson@hp.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/hotplug/acpi_pcihp.c
+++ drivers/pci/hotplug/acpi_pcihp.c
@@ -31,9 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/pci.h>
 #include <linux/pci_hotplug.h>
+#include <linux/acpi.h>
 #include <linux/pci-acpi.h>
-#include <acpi/acpi.h>
-#include <acpi/acpi_bus.h>
 
 #define MY_NAME	"acpi_pcihp"
 
@@ -409,7 +408,7 @@ int acpi_get_hp_hw_control_from_firmware(struct pci_dev *dev, u32 flags)
 		acpi_get_name(handle, ACPI_FULL_PATHNAME, &string);
 		dbg("Trying to get hotplug control for %s\n",
 				(char *)string.pointer);
-		status = pci_osc_control_set(handle, flags);
+		status = acpi_pci_osc_control_set(handle, flags);
 		if (ACPI_SUCCESS(status))
 			goto got_one;
 		kfree(string.pointer);
