35672eb5008e5db4c07b07768988d461a0aeb281
Jeremy Fitzhardinge <jeremy@goop.org>
2009-02-09 16:39:58 -0800

    xen/blkback: don't include xen/evtchn.h
    It's a user-mode header for users of /dev/evtchn
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@goop.org>

--- drivers/xen/blkback/common.h
+++ drivers/xen/blkback/common.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/setup.h>
 #include <asm/pgalloc.h>
-#include <xen/evtchn.h>
 #include <asm/hypervisor.h>
 #include <xen/blkif.h>
 #include <xen/grant_table.h>
