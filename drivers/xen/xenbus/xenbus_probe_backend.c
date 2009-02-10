8bfe473ce08c064c3f09ab3d8f97d127387c6357
Jeremy Fitzhardinge <jeremy@goop.org>
2009-02-09 16:37:23 -0800

    xen: remove xen/evtchn.h
    Impact: cleanup
    
    It's a usermode header for users of /dev/xen/evtchn.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@goop.org>
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    [corresponds to b60e48cec12f in git://git.kernel.org/pub/scm/linux/kernel/git/jeremy/xen.git]
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/xenbus/xenbus_probe_backend.c
+++ drivers/xen/xenbus/xenbus_probe_backend.c
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/xen/hypervisor.h>
 #include <asm/hypervisor.h>
 #include <xen/xenbus.h>
-#include <xen/evtchn.h>
 #include <xen/features.h>
 
 #include "xenbus_comms.h"
