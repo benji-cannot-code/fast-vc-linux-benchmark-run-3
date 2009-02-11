36f0db31fb95f0c24196ab3b45ecd3be0d3d1c93
Indika Katugampala <indika_20012001@yahoo.co.jp>
2009-02-11 11:13:05 -0300

    V4L/DVB (10528): em28xx: support added for IO-DATA GV/MVP SZ - EMPIA-2820 chipset
    [dougsland@redhat.com: Fixed CodingStyle]
    Signed-off-by: Indika Katugampala <indika_20012001@yahoo.co.jp>
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx.h
+++ drivers/media/video/em28xx/em28xx.h
@@ -102,6 +102,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2820_BOARD_GADMEI_TVR200		  62
 #define EM2860_BOARD_KAIOMY_TVNPC_U2              63
 #define EM2860_BOARD_EASYCAP                      64
+#define EM2820_BOARD_IODATA_GVMVP_SZ		  65
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
