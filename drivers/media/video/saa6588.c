53a34f6592d7753f7f6e44e5176515b376fcbc7b
Hans Verkuil <hverkuil@xs4all.nl>
2009-02-11 19:28:30 -0300

    V4L/DVB (10538): saa6588: add g_chip_ident support.
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/saa6588.c
+++ drivers/media/video/saa6588.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/rds.h>
 #include <media/v4l2-device.h>
+#include <media/v4l2-chip-ident.h>
 #include <media/v4l2-i2c-drv-legacy.h>
 
 /* Addresses to scan */
@@ -432,6 +433,13 @@ static long saa6588_ioctl(struct v4l2_subdev *sd, unsigned int cmd, void *arg)
 	return 0;
 }
 
+static int saa6588_g_chip_ident(struct v4l2_subdev *sd, struct v4l2_dbg_chip_ident *chip)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+
+	return v4l2_chip_ident_i2c_client(client, chip, V4L2_IDENT_SAA6588, 0);
+}
+
 static int saa6588_command(struct i2c_client *client, unsigned cmd, void *arg)
 {
 	return v4l2_subdev_command(i2c_get_clientdata(client), cmd, arg);
@@ -440,6 +448,7 @@ static int saa6588_command(struct i2c_client *client, unsigned cmd, void *arg)
 /* ----------------------------------------------------------------------- */
 
 static const struct v4l2_subdev_core_ops saa6588_core_ops = {
+	.g_chip_ident = saa6588_g_chip_ident,
 	.ioctl = saa6588_ioctl,
 };
 
