8866ddbcc589fb5d29bed5fa3bf768acfe6eafbb
Eilon Greenstein <eilong@broadcom.com>
2009-02-12 08:36:27 +0000

    bnx2x: Removing microcode assertion check
    This check is unreliable since latest MC can issue warnings on rare occasions
    which are not fatal errors
    
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x_main.c
+++ drivers/net/bnx2x_main.c
@@ -8733,8 +8733,7 @@ static const struct {
 	{ "nvram_test (online)" },
 	{ "interrupt_test (online)" },
 	{ "link_test (online)" },
-	{ "idle check (online)" },
-	{ "MC errors (online)" }
+	{ "idle check (online)" }
 };
 
 static int bnx2x_self_test_count(struct net_device *dev)
@@ -9203,9 +9202,6 @@ static void bnx2x_self_test(struct net_device *dev,
 			buf[5] = 1;
 			etest->flags |= ETH_TEST_FL_FAILED;
 		}
-	buf[7] = bnx2x_mc_assert(bp);
-	if (buf[7] != 0)
-		etest->flags |= ETH_TEST_FL_FAILED;
 
 #ifdef BNX2X_EXTRA_DEBUG
 	bnx2x_panic_dump(bp);
