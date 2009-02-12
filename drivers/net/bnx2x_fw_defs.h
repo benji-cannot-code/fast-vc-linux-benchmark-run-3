2e0a9c072998656d6a118b0302c5e53d36c0d8ae
Eilon Greenstein <eilong@broadcom.com>
2009-02-12 08:36:11 +0000

    bnx2x: Multi-queue
    Adding Tx multi-queue and enabling multi-queue by default
    
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x_fw_defs.h
+++ drivers/net/bnx2x_fw_defs.h
@@ -260,6 +260,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ETH_MAX_AGGREGATION_QUEUES_E1 (32)
 #define ETH_MAX_AGGREGATION_QUEUES_E1H (64)
 
+/* ETH RSS modes */
+#define ETH_RSS_MODE_DISABLED 0
+#define ETH_RSS_MODE_REGULAR 1
+
 
 /**
 * This file defines HSI constatnts common to all microcode flows
