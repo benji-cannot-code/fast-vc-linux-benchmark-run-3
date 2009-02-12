f7ddd6f4d68eeca162d3c280f917e6ec53fbf5c1
Patrick Ohly <patrick.ohly@intel.com>
2009-02-12 05:03:43 +0000

    igb: use timecompare to implement hardware time stamping
    Both TX and RX hardware time stamping are implemented.  Due to
    hardware limitations it is not possible to verify reliably which
    packet was time stamped when multiple were pending for sending; this
    could be solved by only allowing one packet marked for hardware time
    stamping into the queue (not implemented yet).
    
    RX time stamping relies on the flag in the packet descriptor which
    marks packets that were time stamped. In "all packet" mode this flag
    is not set. TODO: also support that mode (even though it'll suffer
    from race conditions).
    
    Signed-off-by: John Ronciak <john.ronciak@intel.com>
    Signed-off-by: Patrick Ohly <patrick.ohly@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/igb/e1000_defines.h
+++ drivers/net/igb/e1000_defines.h
@@ -105,6 +105,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define E1000_RXD_STAT_UDPCS    0x10    /* UDP xsum calculated */
 #define E1000_RXD_STAT_TCPCS    0x20    /* TCP xsum calculated */
 #define E1000_RXD_STAT_DYNINT   0x800   /* Pkt caused INT via DYNINT */
+#define E1000_RXD_STAT_TS       0x10000 /* Pkt was time stamped */
 #define E1000_RXD_ERR_CE        0x01    /* CRC Error */
 #define E1000_RXD_ERR_SE        0x02    /* Symbol Error */
 #define E1000_RXD_ERR_SEQ       0x04    /* Sequence Error */
--- drivers/net/igb/igb.h
+++ drivers/net/igb/igb.h
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "e1000_82575.h"
 
 #include <linux/clocksource.h>
+#include <linux/timecompare.h>
+#include <linux/net_tstamp.h>
 
 struct igb_adapter;
 
@@ -256,6 +258,8 @@ struct igb_adapter {
 	struct net_device_stats net_stats;
 	struct cyclecounter cycles;
 	struct timecounter clock;
+	struct timecompare compare;
+	struct hwtstamp_config hwtstamp_config;
 
 	/* structs defined in e1000_hw.h */
 	struct e1000_hw hw;
