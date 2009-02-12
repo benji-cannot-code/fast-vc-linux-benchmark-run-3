5dfa2eab6eabaf54977c5c86bc8848b9892725ba
Andi Kleen <andi@firstfloor.org>
2009-02-12 13:49:31 +0100

    x86, mce, cmci: factor out threshold interrupt handler
    Impact: cleanup; preparation for feature
    
    The mce_amd_64 code has an own private MC threshold vector with an own
    interrupt handler. Since Intel needs a similar handler
    it makes sense to share the vector because both can not
    be active at the same time.
    
    I factored the common APIC handler code into a separate file which can
    be used by both the Intel or AMD MC code.
    
    This is needed for the next patch which adds an Intel specific
    CMCI handler.
    
    This patch should be a nop for AMD, it just moves some code
    around.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ arch/x86/kernel/cpu/mcheck/threshold.c
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* Common corrected MCE threshold handler code */
+#include <linux/kernel.h>
+#include <linux/interrupt.h>
+#include <asm/mce.h>
+#include <asm/irq_vectors.h>
+#include <asm/idle.h>
+
+static void default_threshold_interrupt(void)
+{
+	printk(KERN_ERR "Unexpected threshold interrupt at vector %x\n",
+			 THRESHOLD_APIC_VECTOR);
+}
+
+void (*mce_threshold_vector)(void) = default_threshold_interrupt;
+
+asmlinkage void mce_threshold_interrupt(void)
+{
+	ack_APIC_irq();
+	exit_idle();
+	irq_enter();
+	inc_irq_stat(irq_threshold_count);
+	mce_threshold_vector();
+	irq_exit();
+}
