b2db10c0bb2456442db026df1537ba2f323df463
Andi Kleen <andi@firstfloor.org>
2009-02-12 13:49:30 +0100

    x86, mce, cmci: export MAX_NR_BANKS
    Impact: Cleanup (code movement)
    
    Move MAX_NR_BANKS into mce.h because it's needed there
    for followup patches.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/kernel/cpu/mcheck/mce_64.c
+++ arch/x86/kernel/cpu/mcheck/mce_64.c
@@ -38,12 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MISC_MCELOG_MINOR 227
 
-/*
- * To support more than 128 would need to escape the predefined
- * Linux defined extended banks first.
- */
-#define MAX_NR_BANKS (MCE_EXTENDED_BANK - 1)
-
 atomic_t mce_entry;
 
 static int mce_dont_init;
