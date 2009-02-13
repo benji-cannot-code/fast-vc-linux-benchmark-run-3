56fb520ace9c1cf6a7494ca81e65ccb11382600f
Ingo Molnar <mingo@elte.hu>
2009-02-13 13:36:47 +0100

    x86 headers: protect page_32.h via __ASSEMBLY__
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/page_32.h
+++ arch/x86/include/asm/page_32.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/page_32_types.h>
 
+#ifndef __ASSEMBLY__
+
 #ifdef CONFIG_HUGETLB_PAGE
 #define HAVE_ARCH_HUGETLB_UNMAPPED_AREA
 #endif
@@ -19,8 +21,6 @@ extern unsigned long __phys_addr(unsigned long);
 #ifdef CONFIG_FLATMEM
 #define pfn_valid(pfn)		((pfn) < max_mapnr)
 #endif /* CONFIG_FLATMEM */
-
-#ifndef __ASSEMBLY__
 
 #ifdef CONFIG_X86_USE_3DNOW
 #include <asm/mmx.h>
