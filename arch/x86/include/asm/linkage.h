db5fd3ce0a019b87ccdb9c04d72cee261f8fd3fb
Cyrill Gorcunov <gorcunov@gmail.com>
2009-02-14 00:50:18 +0300

    x86: asm linkage - introduce GLOBAL macro
    If the code is time critical and this entry is called
    from other places we use ENTRY to have it globally defined
    and especially aligned.
    
    Contrary we have some snippets which are size
    critical. So we use plane ".globl name; name:"
    directive. Introduce GLOBAL macro for this.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/linkage.h
+++ arch/x86/include/asm/linkage.h
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #endif
 
+#define GLOBAL(name)	\
+	.globl name;	\
+	name:
+
 #ifdef CONFIG_X86_ALIGNMENT_16
 #define __ALIGN .align 16,0x90
 #define __ALIGN_STR ".align 16,0x90"
