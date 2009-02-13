27f9f802b8a0aba51fda226e01a3ff77ca938e3e
Ingo Molnar <mingo@elte.hu>
2009-02-13 13:24:19 +0100

    x86 headers: include page_types.h in pgtable_types.h
    To properly pick up details like PTE_FLAGS_MASK.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/pgtable_types.h
+++ arch/x86/include/asm/pgtable_types.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X86_PGTABLE_DEFS_H
 
 #include <linux/const.h>
+#include <asm/page_types.h>
 
 #define FIRST_USER_ADDRESS	0
 
