83e747cf3ffb9d01d4a7714d677fbd818e4015df
Ingo Molnar <mingo@elte.hu>
2009-02-13 13:23:02 +0100

    x86 headers: include linux/types.h
    To properly pick up types relied on by prototypes like 'bool'.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/page.h
+++ arch/x86/include/asm/page.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_PAGE_H
 #define _ASM_X86_PAGE_H
 
+#include <linux/types.h>
+
 #ifdef __KERNEL__
 
 #include <asm/page_types.h>
