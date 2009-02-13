5e458010519d11b80ddbcad355df3b80e2d9defb
wengang wang <wen.gang.wang@oracle.com>
2009-02-13 10:11:47 +0800

    ocfs2: add IO error check in ocfs2_get_sector()
    Check for IO error in ocfs2_get_sector().
    
    Signed-off-by: Wengang Wang <wen.gang.wang@oracle.com>
    Signed-off-by: Mark Fasheh <mfasheh@suse.com>

--- fs/ocfs2/super.c
+++ fs/ocfs2/super.c
@@ -1538,6 +1538,13 @@ static int ocfs2_get_sector(struct super_block *sb,
 	unlock_buffer(*bh);
 	ll_rw_block(READ, 1, bh);
 	wait_on_buffer(*bh);
+	if (!buffer_uptodate(*bh)) {
+		mlog_errno(-EIO);
+		brelse(*bh);
+		*bh = NULL;
+		return -EIO;
+	}
+
 	return 0;
 }
 
