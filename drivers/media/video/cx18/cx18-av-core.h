843b0d46747b4f2a73255635066e91ec2d942998
Andy Walls <awalls@radix.net>
2009-02-14 02:32:39 -0300

    V4L/DVB (10755): cx18: Convert the integrated A/V decoder core interface to a v4l2_subdev
    This is the next step in converting the cx18 driver to use the v4l2_device/
    v4l2_subdevice framework.  This is a straightforward conversion of the
    cx18_av_*[ch] files.  It compiles, but leaves the driver in an unlinkable
    state at the moment.
    
    Note, the cx18 integrated A/V digitizer will now make a host match at address 1,
    as far as v4l2-dbg is concerned.  That means it identifies itself as a separate
    "chip", and acts as an alias to the integrated A/V decoder registers that are
    also available with the host match at address 0.
    
    Signed-off-by: Andy Walls <awalls@radix.net>
    [mchehab@redhat.com: fix merge conflicts due to the removal of v4l2_ctrl_query_fill_std()]
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-av-core.h
+++ drivers/media/video/cx18/cx18-av-core.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _CX18_AV_CORE_H_
 #define _CX18_AV_CORE_H_
 
+#include <media/v4l2-device.h>
+
 struct cx18;
 
 enum cx18_av_video_input {
@@ -74,6 +76,7 @@ enum cx18_av_audio_input {
 };
 
 struct cx18_av_state {
+	struct v4l2_subdev sd;
 	int radio;
 	v4l2_std_id std;
 	enum cx18_av_video_input vid_input;
@@ -316,6 +319,11 @@ struct cx18_av_state {
 #define CXADEC_SELECT_AUDIO_STANDARD_FM    0xF9  /* FM radio */
 #define CXADEC_SELECT_AUDIO_STANDARD_AUTO  0xFF  /* Auto detect */
 
+static inline struct cx18_av_state *to_cx18_av_state(struct v4l2_subdev *sd)
+{
+	return container_of(sd, struct cx18_av_state, sd);
+}
+
 /* ----------------------------------------------------------------------- */
 /* cx18_av-core.c 							   */
 int cx18_av_write(struct cx18 *cx, u16 addr, u8 value);
@@ -328,8 +336,11 @@ u8 cx18_av_read(struct cx18 *cx, u16 addr);
 u32 cx18_av_read4(struct cx18 *cx, u16 addr);
 int cx18_av_and_or(struct cx18 *cx, u16 addr, unsigned mask, u8 value);
 int cx18_av_and_or4(struct cx18 *cx, u16 addr, u32 mask, u32 value);
-int cx18_av_cmd(struct cx18 *cx, unsigned int cmd, void *arg);
 void cx18_av_std_setup(struct cx18 *cx);
+
+int cx18_av_cmd(struct cx18 *cx, int cmd, void *arg); /* FIXME - Remove */
+int cx18_av_init(struct cx18 *cx);
+void cx18_av_fini(struct cx18 *cx);
 
 /* ----------------------------------------------------------------------- */
 /* cx18_av-firmware.c                                                      */
