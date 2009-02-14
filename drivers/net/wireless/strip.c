80786ce267bb1e1e3a85cd173fa35c17dc33390a
Hannes Eder <hannes@hanneseder.net>
2009-02-14 11:48:44 +0000

    drivers/net/wireless: fix sparse warning: context imbalance
    Impact: Attribute function with __acquires(...) resp. __releases(...).
    
    Fix this sparse warnings:
      drivers/net/wireless/strip.c:955:21: warning: context imbalance in 'strip_seq_start' - wrong count at exit
      drivers/net/wireless/strip.c:975:13: warning: context imbalance in 'strip_seq_stop' - unexpected unlock
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/strip.c
+++ drivers/net/wireless/strip.c
@@ -951,6 +951,7 @@ static struct strip *strip_get_idx(loff_t pos)
 }
 
 static void *strip_seq_start(struct seq_file *seq, loff_t *pos)
+	__acquires(RCU)
 {
 	rcu_read_lock();
 	return *pos ? strip_get_idx(*pos - 1) : SEQ_START_TOKEN;
@@ -974,6 +975,7 @@ static void *strip_seq_next(struct seq_file *seq, void *v, loff_t *pos)
 }
 
 static void strip_seq_stop(struct seq_file *seq, void *v)
+	__releases(RCU)
 {
 	rcu_read_unlock();
 }
