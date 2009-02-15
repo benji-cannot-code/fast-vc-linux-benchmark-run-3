93c1ee80288ea714521d5ca6fcc00101dec46e75
Yinghai Lu <yinghai@kernel.org>
2009-02-14 23:57:28 -0800

    x86: make 32bit to call enable_IO_APIC early like 64bit
    Impact: cleanup
    
    So we remove some #ifdefs.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/io_apic.c
+++ arch/x86/kernel/io_apic.c
@@ -3058,13 +3058,9 @@ out:
 void __init setup_IO_APIC(void)
 {
 
-#ifdef CONFIG_X86_32
-	enable_IO_APIC();
-#else
 	/*
 	 * calling enable_IO_APIC() is moved to setup_local_APIC for BP
 	 */
-#endif
 
 	io_apic_irqs = ~PIC_IRQS;
 
