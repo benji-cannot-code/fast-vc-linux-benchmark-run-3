4d76efd80b22352f64d642b6c49e9b03d0331342
Gustavo F. Padovan <gustavo@las.ic.unicamp.br>
2009-02-15 21:48:56 -0300

    x86, fixmap: add CONFIG_EFI
    Impact: new fixmap allocation
    
    FIX_EFI_IO_MAP_FIRST_PAGE is used only when EFI is enabled.
    
    Signed-off-by: Gustavo F. Padovan <gustavo@las.ic.unicamp.br>
    Acked-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/fixmap_64.h
+++ arch/x86/include/asm/fixmap_64.h
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/apicdef.h>
 #include <asm/page.h>
 #include <asm/vsyscall.h>
+#ifdef CONFIG_EFI
 #include <asm/efi.h>
+#endif
 
 /*
  * Here we define all the compile-time 'special' virtual
@@ -48,9 +50,11 @@ enum fixed_addresses {
 	FIX_IO_APIC_BASE_0,
 	FIX_IO_APIC_BASE_END = FIX_IO_APIC_BASE_0 + MAX_IO_APICS - 1,
 #endif
+#ifdef CONFIG_EFI
 	FIX_EFI_IO_MAP_LAST_PAGE,
 	FIX_EFI_IO_MAP_FIRST_PAGE = FIX_EFI_IO_MAP_LAST_PAGE
 				  + MAX_EFI_IO_PAGES - 1,
+#endif
 #ifdef CONFIG_PARAVIRT
 	FIX_PARAVIRT_BOOTMAP,
 #endif
