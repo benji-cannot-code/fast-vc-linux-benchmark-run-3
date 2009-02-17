ee79e77eda923cc1f118ff3b5d4fedf97dd3363d
Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
2009-02-17 14:15:16 +0900

    PCI: fix wrong assumption in pci_read_bridge_bases
    Current pci_read_bridge_bases() has an assumption that pci_bus->self
    is NULL on the pci root bus (It checks pci_bus->self to see if the pci
    bus is root bus). But is might not true on some platforms. We must
    check pci_bus->parent instead.
    
    Signed-off-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -288,7 +288,7 @@ void __devinit pci_read_bridge_bases(struct pci_bus *child)
 	struct resource *res;
 	int i;
 
-	if (!dev)		/* It's a host bus, nothing to read */
+	if (!child->parent)	/* It's a host bus, nothing to read */
 		return;
 
 	if (dev->transparent) {
