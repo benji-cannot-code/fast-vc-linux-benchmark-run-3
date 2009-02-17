140f255b717d625ce5b34a28117e59726bde36d6
Takashi Iwai <tiwai@suse.de>
2009-02-17 07:59:40 +0100

    ALSA: au88x0 - Fix &&|| typo
    Fixed a typo of || and &&.
    As it's in a disabled code section, there is no behavior change, though.
    
    Reported-by: Jörg-Volker Peetz <jvpeetz@web.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/au88x0/au88x0_core.c
+++ sound/pci/au88x0/au88x0_core.c
@@ -2801,7 +2801,7 @@ vortex_translateformat(vortex_t * vortex, char bits, char nch, int encod)
 {
 	int a, this_194;
 
-	if ((bits != 8) || (bits != 16))
+	if ((bits != 8) && (bits != 16))
 		return -1;
 
 	switch (encod) {
