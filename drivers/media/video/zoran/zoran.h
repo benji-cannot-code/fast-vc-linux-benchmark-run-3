ee1dfe9ebb713f06a3bf8dd41c3c52618ec108b9
Hans Verkuil <hverkuil@xs4all.nl>
2009-02-18 13:13:31 -0300

    V4L/DVB (10703): zoran: convert to video_ioctl2 and remove 'ready_to_be_freed' hack.
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/zoran/zoran.h
+++ drivers/media/video/zoran/zoran.h
@@ -313,7 +313,6 @@ struct zoran_jpg_struct {
 	struct zoran_jpg_buffer buffer[BUZ_MAX_FRAME];	/* buffers */
 	int num_buffers, buffer_size;
 	u8 allocated;		/* Flag if buffers are allocated  */
-	u8 ready_to_be_freed;	/* hack - see zoran_driver.c */
 	u8 need_contiguous;	/* Flag if contiguous buffers are needed */
 };
 
@@ -322,7 +321,6 @@ struct zoran_v4l_struct {
 	struct zoran_v4l_buffer buffer[VIDEO_MAX_FRAME];	/* buffers */
 	int num_buffers, buffer_size;
 	u8 allocated;		/* Flag if buffers are allocated  */
-	u8 ready_to_be_freed;	/* hack - see zoran_driver.c */
 };
 
 struct zoran;
