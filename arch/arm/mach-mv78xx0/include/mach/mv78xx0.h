1344d7c6a740188977d6733ef94d328e283e10a7
Lennert Buytenhek <buytenh@wantstofly.org>
2009-02-20 02:31:35 +0100

    [ARM] mv78xx0: distinguish between different chip steppings
    During boot, identify which chip stepping we're running on (determined
    by looking at the first PCIe unit's device ID and revision registers),
    and print a message with the details about what we found.
    
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>

--- arch/arm/mach-mv78xx0/include/mach/mv78xx0.h
+++ arch/arm/mach-mv78xx0/include/mach/mv78xx0.h
@@ -81,6 +81,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  TIMER_VIRT_BASE	(BRIDGE_VIRT_BASE | 0x0300)
 
 /*
+ * Supported devices and revisions.
+ */
+#define MV78X00_Z0_DEV_ID	0x6381
+#define MV78X00_REV_Z0		1
+
+#define MV78100_DEV_ID		0x7810
+#define MV78100_REV_A0		1
+
+#define MV78200_DEV_ID		0x7820
+#define MV78200_REV_A0		1
+
+/*
  * Register Map
  */
 #define DDR_VIRT_BASE		(MV78XX0_REGS_VIRT_BASE | 0x00000)
