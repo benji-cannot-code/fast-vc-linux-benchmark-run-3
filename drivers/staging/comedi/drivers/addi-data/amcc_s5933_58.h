0966f0113ce34b53eb33c74175584ac657e82ffd
Greg Kroah-Hartman <gregkh@suse.de>
2009-02-19 17:09:52 -0800

    Staging: comedi: addi-data header file cleanups
    Clean up lots of coding style issues in the addi-data header
    files.
    
    Cc: David Schleef <ds@schleef.org>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/addi-data/amcc_s5933_58.h
+++ drivers/staging/comedi/drivers/addi-data/amcc_s5933_58.h
@@ -220,21 +220,25 @@ struct pcilst_struct *amcc_devices;	// ptr to root list of all amcc devices
 void v_pci_card_list_init(unsigned short pci_vendor, char display);
 void v_pci_card_list_cleanup(unsigned short pci_vendor);
 struct pcilst_struct *ptr_find_free_pci_card_by_device(unsigned short vendor_id,
-	unsigned short device_id);
+						       unsigned short
+						       device_id);
 int i_find_free_pci_card_by_position(unsigned short vendor_id,
-	unsigned short device_id, unsigned short pci_bus,
-	unsigned short pci_slot, struct pcilst_struct **card);
+				     unsigned short device_id,
+				     unsigned short pci_bus,
+				     unsigned short pci_slot,
+				     struct pcilst_struct **card);
 struct pcilst_struct *ptr_select_and_alloc_pci_card(unsigned short vendor_id,
-	unsigned short device_id, unsigned short pci_bus,
-	unsigned short pci_slot);
+						    unsigned short device_id,
+						    unsigned short pci_bus,
+						    unsigned short pci_slot);
 
 int i_pci_card_alloc(struct pcilst_struct *amcc);
 int i_pci_card_free(struct pcilst_struct *amcc);
 void v_pci_card_list_display(void);
 int i_pci_card_data(struct pcilst_struct *amcc,
-	unsigned char *pci_bus, unsigned char *pci_slot,
-	unsigned char *pci_func, unsigned short *io_addr, unsigned short *irq,
-	unsigned short *master);
+		    unsigned char *pci_bus, unsigned char *pci_slot,
+		    unsigned char *pci_func, unsigned short *io_addr,
+		    unsigned short *irq, unsigned short *master);
 
 /****************************************************************************/
 
@@ -274,7 +278,7 @@ void v_pci_card_list_init(unsigned short pci_vendor, char display)
 			amcc->pci_func = PCI_FUNC(pcidev->devfn);
 			for (i = 0; i < 5; i++)
 				amcc->io_addr[i] =
-					pcidev->base_address[i] & ~3UL;
+				    pcidev->base_address[i] & ~3UL;
 			amcc->irq = pcidev->irq;
 #else
 			amcc->vendor = pcidev->vendor;
@@ -287,7 +291,7 @@ void v_pci_card_list_init(unsigned short pci_vendor, char display)
 			amcc->pci_func = PCI_FUNC(pcidev->devfn);
 			for (i = 0; i < 5; i++)
 				amcc->io_addr[i] =
-					pcidev->resource[i].start & ~3UL;
+				    pcidev->resource[i].start & ~3UL;
 			amcc->irq = pcidev->irq;
 #endif
 
@@ -315,14 +319,14 @@ void v_pci_card_list_cleanup(unsigned short pci_vendor)
 /****************************************************************************/
 /* find first unused card with this device_id */
 struct pcilst_struct *ptr_find_free_pci_card_by_device(unsigned short vendor_id,
-	unsigned short device_id)
+						       unsigned short device_id)
 {
 	struct pcilst_struct *amcc, *next;
 
 	for (amcc = amcc_devices; amcc; amcc = next) {
 		next = amcc->next;
 		if ((!amcc->used) && (amcc->device == device_id)
-			&& (amcc->vendor == vendor_id))
+		    && (amcc->vendor == vendor_id))
 			return amcc;
 
 	}
@@ -333,8 +337,10 @@ struct pcilst_struct *ptr_find_free_pci_card_by_device(unsigned short vendor_id,
 /****************************************************************************/
 /* find card on requested position */
 int i_find_free_pci_card_by_position(unsigned short vendor_id,
-	unsigned short device_id, unsigned short pci_bus,
-	unsigned short pci_slot, struct pcilst_struct **card)
+				     unsigned short device_id,
+				     unsigned short pci_bus,
+				     unsigned short pci_slot,
+				     struct pcilst_struct **card)
 {
 	struct pcilst_struct *amcc, *next;
 
@@ -342,15 +348,15 @@ int i_find_free_pci_card_by_position(unsigned short vendor_id,
 	for (amcc = amcc_devices; amcc; amcc = next) {
 		next = amcc->next;
 		if ((amcc->vendor == vendor_id) && (amcc->device == device_id)
-			&& (amcc->pci_bus == pci_bus)
-			&& (amcc->pci_slot == pci_slot)) {
+		    && (amcc->pci_bus == pci_bus)
+		    && (amcc->pci_slot == pci_slot)) {
 			if (!(amcc->used)) {
 				*card = amcc;
 				return 0;	// ok, card is found
 			} else {
 				rt_printk
-					(" - \nCard on requested position is used b:s %d:%d!\n",
-					pci_bus, pci_slot);
+				    (" - \nCard on requested position is used b:s %d:%d!\n",
+				     pci_bus, pci_slot);
 				return 2;	// card exist but is used
 			}
 		}
@@ -396,7 +402,11 @@ void v_pci_card_list_display(void)
 
 	for (amcc = amcc_devices; amcc; amcc = next) {
 		next = amcc->next;
-		printk("%2d   %2d   %2d  0x%4x 0x%4x   %3s   0x%4x 0x%4x  %2d  %2d\n", amcc->pci_bus, amcc->pci_slot, amcc->pci_func, amcc->vendor, amcc->device, amcc->master ? "yes" : "no", amcc->io_addr[0], amcc->io_addr[2], amcc->irq, amcc->used);
+		printk
+		    ("%2d   %2d   %2d  0x%4x 0x%4x   %3s   0x%4x 0x%4x  %2d  %2d\n",
+		     amcc->pci_bus, amcc->pci_slot, amcc->pci_func,
+		     amcc->vendor, amcc->device, amcc->master ? "yes" : "no",
+		     amcc->io_addr[0], amcc->io_addr[2], amcc->irq, amcc->used);
 
 	}
 }
@@ -404,9 +414,9 @@ void v_pci_card_list_display(void)
 /****************************************************************************/
 /* return all card information for driver */
 int i_pci_card_data(struct pcilst_struct *amcc,
-	unsigned char *pci_bus, unsigned char *pci_slot,
-	unsigned char *pci_func, unsigned short *io_addr, unsigned short *irq,
-	unsigned short *master)
+		    unsigned char *pci_bus, unsigned char *pci_slot,
+		    unsigned char *pci_func, unsigned short *io_addr,
+		    unsigned short *irq, unsigned short *master)
 {
 	int i;
 
@@ -425,29 +435,32 @@ int i_pci_card_data(struct pcilst_struct *amcc,
 /****************************************************************************/
 /* select and alloc card */
 struct pcilst_struct *ptr_select_and_alloc_pci_card(unsigned short vendor_id,
-	unsigned short device_id, unsigned short pci_bus,
-	unsigned short pci_slot)
+						    unsigned short device_id,
+						    unsigned short pci_bus,
+						    unsigned short pci_slot)
 {
 	struct pcilst_struct *card;
 
 	if ((pci_bus < 1) & (pci_slot < 1)) {	// use autodetection
 		if ((card = ptr_find_free_pci_card_by_device(vendor_id,
-					device_id)) == NULL) {
+							     device_id)) ==
+		    NULL) {
 			rt_printk(" - Unused card not found in system!\n");
 			return NULL;
 		}
 	} else {
 		switch (i_find_free_pci_card_by_position(vendor_id, device_id,
-				pci_bus, pci_slot, &card)) {
+							 pci_bus, pci_slot,
+							 &card)) {
 		case 1:
 			rt_printk
-				(" - Card not found on requested position b:s %d:%d!\n",
-				pci_bus, pci_slot);
+			    (" - Card not found on requested position b:s %d:%d!\n",
+			     pci_bus, pci_slot);
 			return NULL;
 		case 2:
 			rt_printk
-				(" - Card on requested position is used b:s %d:%d!\n",
-				pci_bus, pci_slot);
+			    (" - Card on requested position is used b:s %d:%d!\n",
+			     pci_bus, pci_slot);
 			return NULL;
 		}
 	}
--- drivers/staging/comedi/drivers/addi-data/hwdrv_apci16xx.h
+++ drivers/staging/comedi/drivers/addi-data/hwdrv_apci16xx.h
@@ -1,27 +1,20 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/**
-@verbatim
-
-Copyright (C) 2004,2005  ADDI-DATA GmbH for the source code of this module.
-
-        ADDI-DATA GmbH
-        Dieselstrasse 3
-        D-77833 Ottersweier
-        Tel: +19(0)7223/9493-0
-        Fax: +49(0)7223/9493-92
-        http://www.addi-data-com
-        info@addi-data.com
-
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-You shoud also find the complete GPL in the COPYING file accompanying this source code.
-
-@endverbatim
-*/
+/*
+ * Copyright (C) 2004,2005  ADDI-DATA GmbH for the source code of this module.
+ *
+ *	ADDI-DATA GmbH
+ *	Dieselstrasse 3
+ *	D-77833 Ottersweier
+ *	Tel: +19(0)7223/9493-0
+ *	Fax: +49(0)7223/9493-92
+ *	http://www.addi-data-com
+ *	info@addi-data.com
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ */
 
 #ifndef COMEDI_SUBD_TTLIO
 #define COMEDI_SUBD_TTLIO   11	/* Digital Input Output But TTL */
@@ -51,17 +44,17 @@ You shoud also find the complete GPL in the COPYING file accompanying this sourc
 
 static const comedi_lrange range_apci16xx_ttl = { 12,
 	{BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-			BIP_RANGE(1),
-		BIP_RANGE(1)}
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1),
+	 BIP_RANGE(1)}
 };
 
 /*
@@ -70,8 +63,9 @@ static const comedi_lrange range_apci16xx_ttl = { 12,
 +----------------------------------------------------------------------------+
 */
 
-int i_APCI16XX_InsnConfigInitTTLIO(comedi_device * dev,
-	comedi_subdevice * s, comedi_insn * insn, lsampl_t * data);
+int i_APCI16XX_InsnConfigInitTTLIO(comedi_device *dev,
+				   comedi_subdevice *s, comedi_insn *insn,
+				   lsampl_t *data);
 
 /*
 +----------------------------------------------------------------------------+
@@ -79,11 +73,13 @@ int i_APCI16XX_InsnConfigInitTTLIO(comedi_device * dev,
 +----------------------------------------------------------------------------+
 */
 
-int i_APCI16XX_InsnBitsReadTTLIO(comedi_device * dev,
-	comedi_subdevice * s, comedi_insn * insn, lsampl_t * data);
+int i_APCI16XX_InsnBitsReadTTLIO(comedi_device *dev,
+				 comedi_subdevice *s, comedi_insn *insn,
+				 lsampl_t *data);
 
-int i_APCI16XX_InsnReadTTLIOAllPortValue(comedi_device * dev,
-	comedi_subdevice * s, comedi_insn * insn, lsampl_t * data);
+int i_APCI16XX_InsnReadTTLIOAllPortValue(comedi_device *dev,
+					 comedi_subdevice *s,
+					 comedi_insn *insn, lsampl_t *data);
 
 /*
 +----------------------------------------------------------------------------+
@@ -91,8 +87,9 @@ int i_APCI16XX_InsnReadTTLIOAllPortValue(comedi_device * dev,
 +----------------------------------------------------------------------------+
 */
 
-int i_APCI16XX_InsnBitsWriteTTLIO(comedi_device * dev,
-	comedi_subdevice * s, comedi_insn * insn, lsampl_t * data);
+int i_APCI16XX_InsnBitsWriteTTLIO(comedi_device *dev,
+				  comedi_subdevice *s, comedi_insn *insn,
+				  lsampl_t *data);
 
-int i_APCI16XX_Reset(comedi_device * dev);
+int i_APCI16XX_Reset(comedi_device *dev);
 #endif
