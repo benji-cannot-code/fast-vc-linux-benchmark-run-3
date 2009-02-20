ff022c02cb61005f099cf60ae42ac373b8ec97d1
Ingo Molnar <mingo@elte.hu>
2009-02-20 23:13:36 +0100

    x86, mm: fault.c, update copyrights
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *  Copyright (C) 1995  Linus Torvalds
  *  Copyright (C) 2001, 2002 Andi Kleen, SuSE Labs.
+ *  Copyright (C) 2008-2009, Red Hat Inc., Ingo Molnar
  */
 #include <linux/interrupt.h>
 #include <linux/mmiotrace.h>
