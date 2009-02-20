a3c18361ae8adbfc90207eea9fd8d5408df3fed5
Alexander Duyck <alexander.h.duyck@intel.com>
2009-02-19 20:39:44 -0800

    igb: add pf side of VMDq support
    Add the pf portion of vmdq support.  This provides enough support so that
    VMDq is enabled, and the pf is functional without enabling vfs.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/igb/e1000_defines.h
+++ drivers/net/igb/e1000_defines.h
@@ -400,6 +400,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define E1000_RAH_AV  0x80000000        /* Receive descriptor valid */
 #define E1000_RAL_MAC_ADDR_LEN 4
 #define E1000_RAH_MAC_ADDR_LEN 2
+#define E1000_RAH_POOL_MASK 0x03FC0000
+#define E1000_RAH_POOL_1 0x00040000
 
 /* Error Codes */
 #define E1000_ERR_NVM      1
