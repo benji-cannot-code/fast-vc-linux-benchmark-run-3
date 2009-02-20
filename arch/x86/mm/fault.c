c6b409b83ca97be3bd59e9cf4642c446ff04ab45
Ingo Molnar <mingo@elte.hu>
2009-02-20 22:18:08 +0100

    x86, mm: fault.c, enable PF_RSVD checks on 32-bit too
    Impact: improve page fault handling robustness
    
    The 'PF_RSVD' flag (bit 3) of the page-fault error_code is a
    relatively recent addition to x86 CPUs, so the 32-bit do_fault()
    implementation never had it. This flag gets set when the CPU
    detects nonzero values in any reserved bits of the page directory
    entries.
    
    Extend the existing 64-bit check for PF_RSVD in do_page_fault()
    to 32-bit too. If we detect such a fault then we print a more
    informative oops and the pagetables.
    
    This unifies the code some more, removes an ugly #ifdef and improves
    the 32-bit page fault code robustness a bit. It slightly increases
    the 32-bit kernel text size.
    
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -478,7 +478,6 @@ show_fault_oops(struct pt_regs *regs, unsigned long error_code,
 	dump_pagetable(address);
 }
 
-#ifdef CONFIG_X86_64
 static noinline void
 pgtable_bad(struct pt_regs *regs, unsigned long error_code,
 	    unsigned long address)
@@ -504,7 +503,6 @@ pgtable_bad(struct pt_regs *regs, unsigned long error_code,
 
 	oops_end(flags, regs, sig);
 }
-#endif
 
 static noinline void
 no_context(struct pt_regs *regs, unsigned long error_code,
@@ -1016,10 +1014,8 @@ void __kprobes do_page_fault(struct pt_regs *regs, unsigned long error_code)
 			local_irq_enable();
 	}
 
-#ifdef CONFIG_X86_64
 	if (unlikely(error_code & PF_RSVD))
 		pgtable_bad(regs, error_code, address);
-#endif
 
 	/*
 	 * If we're in an interrupt, have no user context or are running
