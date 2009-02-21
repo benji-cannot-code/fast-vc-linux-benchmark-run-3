25d56d5f478d5899ce384a4a1e307e37d27d87b4
Vikram Pandita <vikram.pandita@ti.com>
2009-02-21 15:31:44 -0800

    USB: musb: fix srp sysfs entry deletion
    The SRP sysfs attribute is dependent on gadget mode; any
    gadget may support SRP.  But "rmmod musb_hdrc" didn't
    remove that attribute; fix.
    
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>
    Acked-by: Felipe Balbi <me@felipebalbi.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1817,7 +1817,7 @@ static void musb_free(struct musb *musb)
 #ifdef CONFIG_SYSFS
 	device_remove_file(musb->controller, &dev_attr_mode);
 	device_remove_file(musb->controller, &dev_attr_vbus);
-#ifdef CONFIG_USB_MUSB_OTG
+#ifdef CONFIG_USB_GADGET_MUSB_HDRC
 	device_remove_file(musb->controller, &dev_attr_srp);
 #endif
 #endif
@@ -2065,7 +2065,7 @@ fail2:
 #ifdef CONFIG_SYSFS
 	device_remove_file(musb->controller, &dev_attr_mode);
 	device_remove_file(musb->controller, &dev_attr_vbus);
-#ifdef CONFIG_USB_MUSB_OTG
+#ifdef CONFIG_USB_GADGET_MUSB_HDRC
 	device_remove_file(musb->controller, &dev_attr_srp);
 #endif
 #endif
