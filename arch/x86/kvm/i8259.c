61651d9e7fb3b06d2691440ab5322ac27b98a338
Hannes Eder <hannes@hanneseder.net>
2009-02-21 02:18:13 +0100

    KVM: fix sparse warnings: context imbalance
    Impact: Attribute function with __acquires(...) resp. __releases(...).
    
    Fix this sparse warnings:
      arch/x86/kvm/i8259.c:34:13: warning: context imbalance in 'pic_lock' - wrong count at exit
      arch/x86/kvm/i8259.c:39:13: warning: context imbalance in 'pic_unlock' - unexpected unlock
    
    Signed-off-by: Hannes Eder <hannes@hanneseder.net>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/i8259.c
+++ arch/x86/kvm/i8259.c
@@ -33,11 +33,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kvm_host.h>
 
 static void pic_lock(struct kvm_pic *s)
+	__acquires(&s->lock)
 {
 	spin_lock(&s->lock);
 }
 
 static void pic_unlock(struct kvm_pic *s)
+	__releases(&s->lock)
 {
 	struct kvm *kvm = s->kvm;
 	unsigned acks = s->pending_acks;
