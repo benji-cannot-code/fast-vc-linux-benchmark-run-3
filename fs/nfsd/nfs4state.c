c64cd3124d04d1f1474fcb0d3dbff97b4ccad2d4
J. Bruce Fields <bfields@citi.umich.edu>
2009-02-21 15:23:01 -0800

    nfsd4: rename io_during_grace_disallowed
    Use a slightly clearer, more concise name.  Also removed unused
    argument.
    
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -2020,7 +2020,7 @@ check_special_stateids(svc_fh *current_fh, stateid_t *stateid, int flags)
  * that are not able to provide mandatory locking.
  */
 static inline int
-io_during_grace_disallowed(struct inode *inode, int flags)
+grace_disallows_io(struct inode *inode)
 {
 	return locks_in_grace() && mandatory_lock(inode);
 }
@@ -2064,7 +2064,7 @@ nfs4_preprocess_stateid_op(struct svc_fh *current_fh, stateid_t *stateid, int fl
 	if (filpp)
 		*filpp = NULL;
 
-	if (io_during_grace_disallowed(ino, flags))
+	if (grace_disallows_io(ino))
 		return nfserr_grace;
 
 	if (ZERO_STATEID(stateid) || ONE_STATEID(stateid))
