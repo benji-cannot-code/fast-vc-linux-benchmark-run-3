37fbe02a994bc3bc0eaa247f54ad569cbbd81692
Russell King <rmk@dyn-67.arm.linux.org.uk>
2009-02-21 21:36:22 +0000

    [ARM] dma: move IOMD and floppy DMA structures to RiscPC DMA code
    There's no point these being in a generic include file when they're
    only used in arch/arm/mach-rpc/dma.c.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-rpc/dma.c
+++ arch/arm/mach-rpc/dma.c
@@ -27,6 +27,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/dma.h>
 #include <asm/hardware/iomd.h>
 
+struct iomd_dma {
+	struct dma_struct	dma;
+	unsigned int		state;
+	unsigned long		base;		/* Controller base address */
+	int			irq;		/* Controller IRQ */
+	struct scatterlist	cur_sg;		/* Current controller buffer */
+};
+
 #if 0
 typedef enum {
 	dma_size_8	= 1,
@@ -241,6 +249,11 @@ static struct dma_ops iomd_dma_ops = {
 
 static struct fiq_handler fh = {
 	.name	= "floppydma"
+};
+
+struct floppy_dma {
+	struct dma_struct	dma;
+	unsigned int		fiq;
 };
 
 static void floppy_enable_dma(unsigned int chan, dma_t *dma)
