5e5787e1940bcd8d09de84bba00866c398a3e5e0
Alexander Clouter <alex@digriz.org.uk>
2009-02-22 12:03:56 +0800

    hwrng: timeriomem - New driver
    Some hardware platforms, the TS-7800[1] is one for example, can
    supply the kernel with an entropy source, albeit a slow one for
    TS-7800 users, by just reading a particular IO address.  This
    source must not be read above a certain rate otherwise the quality
    suffers.
    
    The driver is then hooked into by calling
    platform_device_(register|add|del) passing a structure similar to:
    ------
    static struct timeriomem_rng_data ts78xx_ts_rng_data = {
            .address        = (u32 *__iomem) TS_RNG,
            .period         = 1000000, /* one second */
    };
    
    static struct platform_device ts78xx_ts_rng_device = {
            .name           = "timeriomem_rng",
            .id             = -1,
            .dev            = {
                    .platform_data  = &ts78xx_ts_rng_data,
            },
            .num_resources  = 0,
    };
    ------
    
    [1] http://www.embeddedarm.com/products/board-detail.php?product=TS-7800
    
    Signed-off-by: Alexander Clouter <alex@digriz.org.uk>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

---
+++ include/linux/timeriomem-rng.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * linux/include/linux/timeriomem-rng.h
+ *
+ * Copyright (c) 2009 Alexander Clouter <alex@digriz.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/completion.h>
+
+struct timeriomem_rng_data {
+	struct completion	completion;
+	unsigned int		present:1;
+
+	u32 __iomem		*address;
+
+	/* measures in usecs */
+	unsigned int		period;
+};
