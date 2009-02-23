7dbd440e6571d5a50974b54801043354d1895a9b
Cyrill Gorcunov <gorcunov@gmail.com>
2009-02-23 22:56:59 +0300

    x86: invalid_vm86_irq -- use predefined macros
    Impact: cleanup
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: heukelum@fastmail.fm
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/irq_vectors.h
+++ arch/x86/include/asm/irq_vectors.h
@@ -129,7 +129,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 static inline int invalid_vm86_irq(int irq)
 {
-	return irq < 3 || irq > 15;
+	return irq < FIRST_VM86_IRQ || irq > LAST_VM86_IRQ;
 }
 #endif
 
