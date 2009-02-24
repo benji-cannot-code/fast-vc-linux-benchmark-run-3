468a362795d367739f76ac0420fb74b2dcb2b5b1
Steven Rostedt <srostedt@redhat.com>
2009-02-24 10:22:57 -0500

    tracing: add schedule events to event trace
    This patch changes the trace/sched.h to use the DECLARE_TRACE_FMT
    such that they are automatically registered with the event tracer.
    
    And it also adds the tracing sched headers to kernel/trace/events.c
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>

---
+++ kernel/trace/events.c
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * This is the place to register all trace points as events.
+ * Include the trace/<type>.h at the top.
+ * Include the trace/<type>_event_types.h at the bottom.
+ */
+
+/* trace/<type>.h here */
+#include <trace/sched.h>
+
+#include "trace_events.h"
+
+/* trace/<type>_event_types.h here */
+#include <trace/sched_event_types.h>
