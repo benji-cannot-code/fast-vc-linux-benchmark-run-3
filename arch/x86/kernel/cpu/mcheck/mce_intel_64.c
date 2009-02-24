3a85e87dd7b86d7e417f771ecc8fb2eeb3d6bf58
H. Peter Anvin <hpa@zytor.com>
2009-02-24 13:19:02 -0800

    x86, mce, cmci: remove incorrect __cpuinit/__cpuexit annotations
    Impact: Bug fix on UP
    
    The MCE code is reinitialized from resume, so we can't use
    __cpuinit/__cpuexit for most of the code.  Remove those annotations
    for anything downstream of mce_init().
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/kernel/cpu/mcheck/mce_intel_64.c
+++ arch/x86/kernel/cpu/mcheck/mce_intel_64.c
@@ -105,7 +105,7 @@ static DEFINE_SPINLOCK(cmci_discover_lock);
 
 #define CMCI_THRESHOLD 1
 
-static __cpuinit int cmci_supported(int *banks)
+static int cmci_supported(int *banks)
 {
 	u64 cap;
 
@@ -148,7 +148,7 @@ static void print_update(char *type, int *hdr, int num)
  * on this CPU. Use the algorithm recommended in the SDM to discover shared
  * banks.
  */
-static __cpuinit void cmci_discover(int banks, int boot)
+static void cmci_discover(int banks, int boot)
 {
 	unsigned long *owned = (void *)&__get_cpu_var(mce_banks_owned);
 	int hdr = 0;
@@ -193,7 +193,7 @@ static __cpuinit void cmci_discover(int banks, int boot)
  * Just in case we missed an event during initialization check
  * all the CMCI owned banks.
  */
-__cpuinit void cmci_recheck(void)
+void cmci_recheck(void)
 {
 	unsigned long flags;
 	int banks;
@@ -209,7 +209,7 @@ __cpuinit void cmci_recheck(void)
  * Disable CMCI on this CPU for all banks it owns when it goes down.
  * This allows other CPUs to claim the banks on rediscovery.
  */
-void __cpuexit cmci_clear(void)
+void cmci_clear(void)
 {
 	int i;
 	int banks;
@@ -234,7 +234,7 @@ void __cpuexit cmci_clear(void)
  * After a CPU went down cycle through all the others and rediscover
  * Must run in process context.
  */
-void __cpuexit cmci_rediscover(int dying)
+void cmci_rediscover(int dying)
 {
 	int banks;
 	int cpu;
