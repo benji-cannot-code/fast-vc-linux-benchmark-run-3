b384443421a91e117484a92b047ae870a3b08778
Greg Kroah-Hartman <gregkh@suse.de>
2009-02-25 18:11:24 -0800

    Staging: slicoss: remove slic_os.h
    It's no longer needed, and empty, so remove it.
    
    Cc: Lior Dotan <liodot@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/slicoss/slicinc.h
+++ drivers/staging/slicoss/slicinc.h
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SLIC_INCLUDE_H_
 #define _SLIC_INCLUDE_H_
 
-#include "slic_os.h"
 #include "slicdbg.h"
 #include "slichw.h"
 #include "slic.h"
