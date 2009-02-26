877a527c055ebb7887aa08ee8cbb65170d79fc97
Greg Kroah-Hartman <gregkh@suse.de>
2009-02-26 14:09:38 -0800

    Staging: slicoss: remove SLIC_PING_TIMER_ENABLED
    It was always enabled, so just enable it properly.
    
    Cc: Lior Dotan <liodot@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/slicoss/slicoss.c
+++ drivers/staging/slicoss/slicoss.c
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUG_MICROCODE                 1
 #define DBG                             1
 #define SLIC_ASSERT_ENABLED		        1
-#define SLIC_PING_TIMER_ENABLED		    1
 #define SLIC_INTERRUPT_PROCESS_LIMIT	1
 #define SLIC_OFFLOAD_IP_CHECKSUM		1
 #define STATS_TIMER_INTERVAL			2
@@ -1393,13 +1392,13 @@ static void slic_init_cleanup(struct adapter *adapter)
 		adapter->pshmem = NULL;
 		adapter->phys_shmem = (dma_addr_t) NULL;
 	}
-#if SLIC_PING_TIMER_ENABLED
+
 	if (adapter->pingtimerset) {
 		DBG_MSG("pingtimer ");
 		adapter->pingtimerset = 0;
 		del_timer(&adapter->pingtimer);
 	}
-#endif
+
 	slic_rspqueue_free(adapter);
 	slic_cmdq_free(adapter);
 	slic_rcvqueue_free(adapter);
@@ -1753,7 +1752,7 @@ static int slic_if_init(struct adapter *adapter)
 
 		card->loadtimerset = 1;
 	}
-#if SLIC_PING_TIMER_ENABLED
+
 	if (!adapter->pingtimerset) {
 		DBG_MSG("slicoss: %s start card_ping_timer(slic)\n",
 			__func__);
@@ -1766,7 +1765,6 @@ static int slic_if_init(struct adapter *adapter)
 		adapter->pingtimerset = 1;
 		adapter->card->pingstatus = ISR_PINGMASK;
 	}
-#endif
 
 	/*
 	 *    clear any pending events, then enable interrupts
