0e3114c4cfcc551c6178f3eafe7e10e4e16a7f2d
Maciej Sosnowski <maciej.sosnowski@intel.com>
2009-02-26 11:05:07 +0100

    I/OAT: cancel watchdog before dma remove
    Channel watchdog should be canceled before the rest of dma remove stuff.
    
    Signed-off-by: Maciej Sosnowski <maciej.sosnowski@intel.com>
    Signed-off-by: Shannon Nelson <shannon.nelson@intel.com>
    Acked-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/dma/ioat_dma.c
+++ drivers/dma/ioat_dma.c
@@ -1706,6 +1706,9 @@ void ioat_dma_remove(struct ioatdma_device *device)
 	struct dma_chan *chan, *_chan;
 	struct ioat_dma_chan *ioat_chan;
 
+	if (device->version != IOAT_VER_3_0)
+		cancel_delayed_work(&device->work);
+
 	ioat_dma_remove_interrupts(device);
 
 	dma_async_device_unregister(&device->common);
@@ -1716,10 +1719,6 @@ void ioat_dma_remove(struct ioatdma_device *device)
 	iounmap(device->reg_base);
 	pci_release_regions(device->pdev);
 	pci_disable_device(device->pdev);
-
-	if (device->version != IOAT_VER_3_0) {
-		cancel_delayed_work(&device->work);
-	}
 
 	list_for_each_entry_safe(chan, _chan,
 				 &device->common.channels, device_node) {
