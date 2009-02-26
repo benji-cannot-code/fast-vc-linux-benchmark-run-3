42ae7163a73477ab63f4791987cba8633218dd39
Michael Krufky <mkrufky@linuxtv.org>
2009-02-26 18:32:36 -0300

    V4L/DVB (10753): siano: convert EXPORT_SYMBOL to EXPORT_SYMBOL_GPL
    As pointed out by Mauro Chehab, we should always use EXPORT_SYMBOL_GPL
    instead of EXPORT_SYMBOL wherever possible.
    
    A message was posted to the linux-media mailing list about this, checking
    with Uri Shkolnik of Siano Mobile Silicon if this is okay to convert.
    
    As per Uri's response to this email, it is OK for us to make this conversion.
    
    http://www.spinics.net/lists/linux-media/msg02200.html
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/siano/sms-cards.c
+++ drivers/media/dvb/siano/sms-cards.c
@@ -87,7 +87,7 @@ struct sms_board *sms_get_board(int id)
 
 	return &sms_boards[id];
 }
-EXPORT_SYMBOL(sms_get_board);
+EXPORT_SYMBOL_GPL(sms_get_board);
 
 static int sms_set_gpio(struct smscore_device_t *coredev, int pin, int enable)
 {
@@ -140,7 +140,7 @@ int sms_board_setup(struct smscore_device_t *coredev)
 	}
 	return 0;
 }
-EXPORT_SYMBOL(sms_board_setup);
+EXPORT_SYMBOL_GPL(sms_board_setup);
 
 int sms_board_power(struct smscore_device_t *coredev, int onoff)
 {
@@ -162,7 +162,7 @@ int sms_board_power(struct smscore_device_t *coredev, int onoff)
 	}
 	return 0;
 }
-EXPORT_SYMBOL(sms_board_power);
+EXPORT_SYMBOL_GPL(sms_board_power);
 
 int sms_board_led_feedback(struct smscore_device_t *coredev, int led)
 {
@@ -185,7 +185,7 @@ int sms_board_led_feedback(struct smscore_device_t *coredev, int led)
 	}
 	return 0;
 }
-EXPORT_SYMBOL(sms_board_led_feedback);
+EXPORT_SYMBOL_GPL(sms_board_led_feedback);
 
 int sms_board_lna_control(struct smscore_device_t *coredev, int onoff)
 {
@@ -204,7 +204,7 @@ int sms_board_lna_control(struct smscore_device_t *coredev, int onoff)
 	}
 	return -EINVAL;
 }
-EXPORT_SYMBOL(sms_board_lna_control);
+EXPORT_SYMBOL_GPL(sms_board_lna_control);
 
 int sms_board_load_modules(int id)
 {
@@ -221,4 +221,4 @@ int sms_board_load_modules(int id)
 	}
 	return 0;
 }
-EXPORT_SYMBOL(sms_board_load_modules);
+EXPORT_SYMBOL_GPL(sms_board_load_modules);
