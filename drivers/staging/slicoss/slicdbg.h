78a242bf5ae7a875291e48c926b0f3c309113d11
Greg Kroah-Hartman <gregkh@suse.de>
2009-02-26 15:37:08 -0800

    Staging: slicoss: remove ASSERTMSG macro
    No one uses it, so drop it.
    
    Cc: Lior Dotan <liodot@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/slicoss/slicdbg.h
+++ drivers/staging/slicoss/slicdbg.h
@@ -58,23 +58,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	}                                                                 \
     }
 #endif
-#ifndef ASSERTMSG
-#define ASSERTMSG(a,msg)                                                  \
-    {                                                                     \
-	if (!(a)) {                                                       \
-		printk(KERN_ERR "ASSERT() Failure: file %s, function %s" \
-			"line %d: %s\n",\
-			__FILE__, __func__, __LINE__, (msg));            \
-		slic_assert_fail();                                      \
-	}                                                                \
-    }
-#endif
 #else
 #ifndef ASSERT
 #define ASSERT(a)
-#endif
-#ifndef ASSERTMSG
-#define ASSERTMSG(a, msg)
 #endif
 #endif /* SLIC_ASSERT_ENABLED  */
 
