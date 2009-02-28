4492cfaa96ae6ac4441b3f35e186f5dc91851ca6
Michael Krufky <mkrufky@linuxtv.org>
2009-02-28 19:42:59 -0300

    V4L/DVB (10781): mxl5007t: mxl5007t_get_status should report if tuner is locked
    report TUNER_STATUS_LOCKED if rf_locked or ref_locked
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/tuners/mxl5007t.c
+++ drivers/media/common/tuners/mxl5007t.c
@@ -583,8 +583,9 @@ fail:
 static int mxl5007t_get_status(struct dvb_frontend *fe, u32 *status)
 {
 	struct mxl5007t_state *state = fe->tuner_priv;
-	int rf_locked, ref_locked;
-	int ret;
+	int rf_locked, ref_locked, ret;
+
+	*status = 0;
 
 	if (fe->ops.i2c_gate_ctrl)
 		fe->ops.i2c_gate_ctrl(fe, 1);
@@ -594,6 +595,9 @@ static int mxl5007t_get_status(struct dvb_frontend *fe, u32 *status)
 		goto fail;
 	mxl_debug("%s%s", rf_locked ? "rf locked " : "",
 		  ref_locked ? "ref locked" : "");
+
+	if ((rf_locked) || (ref_locked))
+		*status |= TUNER_STATUS_LOCKED;
 fail:
 	if (fe->ops.i2c_gate_ctrl)
 		fe->ops.i2c_gate_ctrl(fe, 0);
