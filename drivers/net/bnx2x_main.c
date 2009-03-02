7674b7de17b3685c114fa88cd4d897a3d5c32fab
Eilon Greenstein <eilong@broadcom.com>
2009-03-02 07:59:56 +0000

    bnx2x: Calling tx disable unconditionally
    On unload, the FW assumes that no packets will be sent after the driver sends
    the FW stop command. To ensure that, the driver must always call
    netif_tx_disable
    
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x_main.c
+++ drivers/net/bnx2x_main.c
@@ -6574,10 +6574,8 @@ static void bnx2x_netif_stop(struct bnx2x *bp, int disable_hw)
 {
 	bnx2x_int_disable_sync(bp, disable_hw);
 	bnx2x_napi_disable(bp);
-	if (netif_running(bp->dev)) {
-		netif_tx_disable(bp->dev);
-		bp->dev->trans_start = jiffies;	/* prevent tx timeout */
-	}
+	netif_tx_disable(bp->dev);
+	bp->dev->trans_start = jiffies;	/* prevent tx timeout */
 }
 
 /*
