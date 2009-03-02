bf14af4f9b668686a3aacfd3335e8951fb4af1f5
Wei Yongjun <yjwei@cn.fujitsu.com>
2009-03-02 09:46:11 +0000

    sctp: remove dup code in net/sctp/socket.c
    Remove dup check of "if (optlen < sizeof(int))".
    
    Signed-off-by: Wei Yongjun <yjwei@cn.fujitsu.com>
    Signed-off-by: Vlad Yasevich <vladislav.yasevich@hp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -3070,9 +3070,6 @@ static int sctp_setsockopt_maxburst(struct sock *sk,
 	int val;
 	int assoc_id = 0;
 
-	if (optlen < sizeof(int))
-		return -EINVAL;
-
 	if (optlen == sizeof(int)) {
 		printk(KERN_WARNING
 		   "SCTP: Use of int in max_burst socket option deprecated\n");
@@ -5283,9 +5280,6 @@ static int sctp_getsockopt_maxburst(struct sock *sk, int len,
 	struct sctp_assoc_value params;
 	struct sctp_sock *sp;
 	struct sctp_association *asoc;
-
-	if (len < sizeof(int))
-		return -EINVAL;
 
 	if (len == sizeof(int)) {
 		printk(KERN_WARNING
