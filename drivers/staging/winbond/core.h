5dfd8565d1fdb85994c0c7556d6eb995eafc1f86
Pekka Enberg <penberg@cs.helsinki.fi>
2009-03-02 12:19:56 +0200

    Staging: w35und: remove unused bssdscpt.h header
    The bssdscpt.h header file contains definitions that are not actually used for
    anything.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/core.h
+++ drivers/staging/winbond/core.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/wireless.h>
 
-#include "bssdscpt.h"
+#include "mlme_s.h"
 #include "wbhal_s.h"
 #include "mto.h"
 
@@ -16,7 +16,6 @@ struct wbsoft_priv {
 	u32 adapterIndex;	// 20060703.4 Add for using padapterContext global adapter point
 
 	WB_LOCALDESCRIPT sLocalPara;	// Myself connected parameters
-	PWB_BSSDESCRIPTION asBSSDescriptElement;
 
 	MLME_FRAME sMlmeFrame;	// connect to peerSTA parameters
 
