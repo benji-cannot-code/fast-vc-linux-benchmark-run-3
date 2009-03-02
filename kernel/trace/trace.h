c6cb11ebd4e6440386e44541c64aeb0f6a89cf01
Steven Rostedt <srostedt@redhat.com>
2009-03-02 14:04:40 -0500

    tracing: make trace_seq_reset global and rename to trace_seq_init
    Impact: clean up
    
    The trace_seq functions may be used separately outside of the ftrace
    iterator. The trace_seq_reset is needed for these operations.
    
    This patch also renames trace_seq_reset to the more appropriate
    trace_seq_init.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>

--- kernel/trace/trace.h
+++ kernel/trace/trace.h
@@ -396,6 +396,14 @@ struct trace_seq {
 	unsigned int		readpos;
 };
 
+static inline void
+trace_seq_init(struct trace_seq *s)
+{
+	s->len = 0;
+	s->readpos = 0;
+}
+
+
 #define TRACE_PIPE_ALL_CPU	-1
 
 /*
