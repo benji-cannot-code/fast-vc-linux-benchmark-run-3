fa9155ee2b83faeb2b4ea8fa87dfc90a0bdfc7b3
Oskar Schirmer <os@emlix.com>
2009-03-04 16:21:30 +0100

    xtensa: enforce slab alignment to maximum register width
    XCHAL_DATA_WIDTH is the maximum register width, slab caches should be
    aligned to this.
    
    Theoretical fix as all variants have had an XCHAL_DATA_WIDTH of 4
    (wordsize) for now.  But the S6000 variant will raise this to 16.
    
    Signed-off-by: Oskar Schirmer <os@emlix.com>
    Signed-off-by: Johannes Weiner <jw@emlix.com>
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- arch/xtensa/include/asm/processor.h
+++ arch/xtensa/include/asm/processor.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # error Linux requires the Xtensa Windowed Registers Option.
 #endif
 
+#define ARCH_SLAB_MINALIGN	XCHAL_DATA_WIDTH
+
 /*
  * User space process size: 1 GB.
  * Windowed call ABI requires caller and callee to be located within the same
