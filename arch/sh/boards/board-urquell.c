d2f1ddb20f23144b7a5c53912e8038d76beb5426
Kuninori Morimoto <morimoto.kuninori@renesas.com>
2009-03-05 17:37:12 +0900

    sh: urquell: Add smc91x support and update defconfig accordingly.
    Signed-off-by: Kuninori Morimoto <morimoto.kuninori@renesas.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/board-urquell.c
+++ arch/sh/boards/board-urquell.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/fb.h>
+#include <linux/smc91x.h>
 #include <linux/mtd/physmap.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
@@ -41,6 +42,32 @@ static struct platform_device heartbeat_device = {
 	},
 	.num_resources	= ARRAY_SIZE(heartbeat_resources),
 	.resource	= heartbeat_resources,
+};
+
+static struct smc91x_platdata smc91x_info = {
+	.flags = SMC91X_USE_16BIT | SMC91X_NOWAIT,
+};
+
+static struct resource smc91x_eth_resources[] = {
+	[0] = {
+		.name   = "SMC91C111" ,
+		.start  = 0x05800300,
+		.end    = 0x0580030f,
+		.flags  = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start  = 11,
+		.flags  = IORESOURCE_IRQ,
+	},
+};
+
+static struct platform_device smc91x_eth_device = {
+	.name           = "smc91x",
+	.num_resources  = ARRAY_SIZE(smc91x_eth_resources),
+	.resource       = smc91x_eth_resources,
+	.dev	= {
+		.platform_data	= &smc91x_info,
+	},
 };
 
 static struct mtd_partition nor_flash_partitions[] = {
@@ -93,6 +120,7 @@ static struct platform_device nor_flash_device = {
 
 static struct platform_device *urquell_devices[] __initdata = {
 	&heartbeat_device,
+	&smc91x_eth_device,
 	&nor_flash_device,
 };
 
@@ -112,6 +140,11 @@ static void urquell_power_off(void)
 	__raw_writew(0xa5a5, UBOARDREG(SRSTR));
 }
 
+static void __init urquell_init_irq(void)
+{
+	plat_irq_setup_pins(IRQ_MODE_IRL3210_MASK);
+}
+
 /* Initialize the board */
 static void __init urquell_setup(char **cmdline_p)
 {
@@ -126,4 +159,5 @@ static void __init urquell_setup(char **cmdline_p)
 static struct sh_machine_vector mv_urquell __initmv = {
 	.mv_name	= "Urquell",
 	.mv_setup	= urquell_setup,
+	.mv_init_irq	= urquell_init_irq,
 };
