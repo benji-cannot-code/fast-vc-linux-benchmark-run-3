f3fc774c91a514288fd33344c2d4ad58bc2153e1
Daniel Mack <daniel@caiaq.de>
2009-03-05 14:21:26 +0100

    ASoC: add two more bitfields for PXA SSP
    Add two more bitfields for the PSP register. As they seem to exist
    for PXA3xx only, define them conditionally.
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- arch/arm/mach-pxa/include/mach/regs-ssp.h
+++ arch/arm/mach-pxa/include/mach/regs-ssp.h
@@ -107,6 +107,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SSSR_TINT		(1 << 19)	/* Receiver Time-out Interrupt */
 #define SSSR_PINT		(1 << 18)	/* Peripheral Trailing Byte Interrupt */
 
+#if defined(CONFIG_PXA3xx)
+#define SSPSP_EDMYSTOP(x)	((x) << 28)     /* Extended Dummy Stop */
+#define SSPSP_EDMYSTRT(x)	((x) << 26)     /* Extended Dummy Start */
+#endif
+
 #define SSPSP_FSRT		(1 << 25)	/* Frame Sync Relative Timing */
 #define SSPSP_DMYSTOP(x)	((x) << 23)	/* Dummy Stop */
 #define SSPSP_SFRMWDTH(x)	((x) << 16)	/* Serial Frame Width */
