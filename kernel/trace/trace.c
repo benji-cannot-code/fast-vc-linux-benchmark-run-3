0c1965708f1648c0926c89bef3c2aa7024d79e82
Steven Rostedt <srostedt@redhat.com>
2009-03-04 20:31:11 -0500

    tracing: fix seq read from trace files
    The buffer used by trace_seq was updated incorrectly. Instead
    of consuming what was actually read, it consumed the rest of the
    buffer on reads.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -362,7 +362,7 @@ ssize_t trace_seq_to_user(struct trace_seq *s, char __user *ubuf, size_t cnt)
 
 	cnt -= ret;
 
-	s->readpos += len;
+	s->readpos += cnt;
 	return cnt;
 }
 
@@ -381,7 +381,7 @@ ssize_t trace_seq_to_buffer(struct trace_seq *s, void *buf, size_t cnt)
 	if (!ret)
 		return -EFAULT;
 
-	s->readpos += len;
+	s->readpos += cnt;
 	return cnt;
 }
 
