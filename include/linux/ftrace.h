ee67591cc60fe52b73fb625e6f3f347272754fdd
Frederic Weisbecker <fweisbec@gmail.com>
2009-03-05 01:49:22 +0100

    tracing/function-graph-tracer: use the more lightweight local clock
    Impact: decrease hangs risks with the graph tracer on slow systems
    
    Since the function graph tracer can spend too much time on timer
    interrupts, it's better now to use the more lightweight local
    clock. Anyway, the function graph traces are more reliable on a
    per cpu trace.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <49af243d.06e9300a.53ad.ffff840c@mx.google.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -2,15 +2,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_FTRACE_H
 #define _LINUX_FTRACE_H
 
-#include <linux/linkage.h>
-#include <linux/fs.h>
-#include <linux/ktime.h>
-#include <linux/init.h>
-#include <linux/types.h>
-#include <linux/module.h>
+#include <linux/trace_clock.h>
 #include <linux/kallsyms.h>
+#include <linux/linkage.h>
 #include <linux/bitops.h>
+#include <linux/module.h>
+#include <linux/ktime.h>
 #include <linux/sched.h>
+#include <linux/types.h>
+#include <linux/init.h>
+#include <linux/fs.h>
 
 #include <asm/ftrace.h>
 
