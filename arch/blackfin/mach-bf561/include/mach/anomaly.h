6bcfa748e950079d4c74b2b5af49e6aeb62deb73
Mike Frysinger <vapier.adi@gmail.com>
2009-03-06 00:20:49 +0800

    Blackfin arch: add stubs for anomalies 447 and 448
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/mach-bf561/include/mach/anomaly.h
+++ arch/blackfin/mach-bf561/include/mach/anomaly.h
@@ -288,5 +288,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ANOMALY_05000386 (1)
 #define ANOMALY_05000432 (0)
 #define ANOMALY_05000435 (0)
+#define ANOMALY_05000447 (0)
+#define ANOMALY_05000448 (0)
 
 #endif
