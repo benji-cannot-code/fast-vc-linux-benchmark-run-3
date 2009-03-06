442c57acf44f2c65ec69e3536e32bdab0b3f81a3
Steven Rostedt <srostedt@redhat.com>
2009-03-05 21:35:29 -0500

    tracing: add format files for ftrace default entries
    Impact: allow user apps to read binary format of basic ftrace entries
    
    Currently, only defined raw events export their formats so a binary
    reader can parse them. There's no reason that the default ftrace entries
    can't export their formats.
    
    This patch adds a subsystem called "ftrace" in the events directory
    that includes the ftrace entries for basic ftrace recorded items.
    
    These only have three files in the events directory:
    
     type             : printf
     available_types  : printf
     format           : format for the event entry
    
    For example:
    
     # cat /debug/tracing/events/ftrace/wakeup/format
    name: wakeup
    ID: 3
    format:
            field:unsigned char type;       offset:0;       size:1;
            field:unsigned char flags;      offset:1;       size:1;
            field:unsigned char preempt_count;      offset:2;       size:1;
            field:int pid;  offset:4;       size:4;
            field:int tgid; offset:8;       size:4;
    
            field:unsigned int prev_pid;    offset:12;      size:4;
            field:unsigned char prev_prio;  offset:16;      size:1;
            field:unsigned char prev_state; offset:17;      size:1;
            field:unsigned int next_pid;    offset:20;      size:4;
            field:unsigned char next_prio;  offset:24;      size:1;
            field:unsigned char next_state; offset:25;      size:1;
            field:unsigned int next_cpu;    offset:28;      size:4;
    
    print fmt: "%u:%u:%u  ==+ %u:%u:%u [%03u]"
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>

--- kernel/trace/trace_format.h
+++ kernel/trace/trace_format.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #undef TRACE_EVENT_FORMAT
 #define TRACE_EVENT_FORMAT(call, proto, args, fmt, tstruct, tpfmt)	\
-int									\
+static int								\
 ftrace_format_##call(struct trace_seq *s)				\
 {									\
 	struct ftrace_raw_##call field;					\
