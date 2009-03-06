8c9458fcc3ea28cfd590cac1f5a970c9db2012be
Faisal Latif <faisal.latif@intel.com>
2009-03-06 15:12:11 -0800

    RDMA/nes: Remove LLTX
    NETIF_F_LLTX is deprecated. Remove private TX locking from the driver
    and remove the NETIF_F_LLTX feature flag.  This also fixes a warning
    in some configs that comes from doing skb_linearize() call in the
    hard_start_xmit method with IRQs disabled (if HIGHMEM is enabled,
    skb_linearize() may end up enabling BHs, which is a no-no if hard IRQs
    are disabled in that context).  By getting rid of LLTX, we do not
    disable IRQs when skb_linearize() is called.
    
    Remove the sq_lock as it is not needed for non-LLTX.  Fix ethtool not
    to show the counter for sq_lock.
    
    Reported-by: aluno3@poczta.onet.pl
    Signed-off-by: Faisal Latif <faisal.latif@intel.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/nes/nes_hw.h
+++ drivers/infiniband/hw/nes/nes_hw.h
@@ -877,7 +877,6 @@ struct nes_hw_nic {
 	u8 replenishing_rq;
 	u8 reserved;
 
-	spinlock_t sq_lock;
 	spinlock_t rq_lock;
 };
 
@@ -1149,7 +1148,6 @@ struct nes_ib_device;
 struct nes_vnic {
 	struct nes_ib_device *nesibdev;
 	u64 sq_full;
-	u64 sq_locked;
 	u64 tso_requests;
 	u64 segmented_tso_requests;
 	u64 linearized_skbs;
