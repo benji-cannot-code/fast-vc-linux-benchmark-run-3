c24060d8604ee292944c9811463ea1f7d8eb1573
Paul Mundt <lethal@linux-sh.org>
2009-03-06 18:51:33 +0900

    rtc: rtc-sh: Bump version up to reflect single IRQ support changes.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/rtc/rtc-sh.c
+++ drivers/rtc/rtc-sh.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/rtc.h>
 
 #define DRV_NAME	"sh-rtc"
-#define DRV_VERSION	"0.2.0"
+#define DRV_VERSION	"0.2.1"
 
 #define RTC_REG(r)	((r) * rtc_reg_size)
 
