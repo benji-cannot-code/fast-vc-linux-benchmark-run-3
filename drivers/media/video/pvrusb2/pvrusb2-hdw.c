3fab1863ca54030807cfe1799a8924b946648d81
Mike Isely <isely@pobox.com>
2009-03-07 00:54:02 -0300

    V4L/DVB (11182): pvrusb2: Tie in cx25840 sub-device support
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/pvrusb2/pvrusb2-hdw.c
+++ drivers/media/video/pvrusb2/pvrusb2-hdw.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "pvrusb2-fx2-cmd.h"
 #include "pvrusb2-wm8775.h"
 #include "pvrusb2-video-v4l.h"
+#include "pvrusb2-cx2584x-v4l.h"
 #include "pvrusb2-audio.h"
 
 #define TV_MIN_FREQ     55250000L
@@ -116,6 +117,7 @@ static const pvr2_subdev_update_func pvr2_module_update_functions[] = {
 	[PVR2_CLIENT_ID_WM8775] = pvr2_wm8775_subdev_update,
 	[PVR2_CLIENT_ID_SAA7115] = pvr2_saa7115_subdev_update,
 	[PVR2_CLIENT_ID_MSP3400] = pvr2_msp3400_subdev_update,
+	[PVR2_CLIENT_ID_CX25840] = pvr2_cx25840_subdev_update,
 };
 
 static const char *module_names[] = {
