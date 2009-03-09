6d095cd2a6ad0b7e3880ecf8d0e82f2fab93c412
Douglas Gilbert <dgilbert@interlog.com>
2009-03-09 10:51:38 -0400

    [SCSI] bsg: add linux/types.h include to bsg.h
    Since bsg.h has recently been added to the list of kernel
    headers that should be exported to the user space, this
    attachment makes bsg.h more user space "friendly".
    Specifically autotools dislike headers that don't compile
    freestanding and bsg.h's use of __u32 types (and friends)
    are not standard C (C90 or C99). The inclusion of
    linux/types.h fixes that.
    
    Signed-off-by: Douglas Gilbert <dgilbert@interlog.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- include/linux/bsg.h
+++ include/linux/bsg.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef BSG_H
 #define BSG_H
 
+#include <linux/types.h>
+
 #define BSG_PROTOCOL_SCSI		0
 
 #define BSG_SUB_PROTOCOL_SCSI_CMD	0
