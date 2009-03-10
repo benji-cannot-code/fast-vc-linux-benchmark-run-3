ce8678314fda702f8a65c04a783945c2fdb8521b
Hugo Villeneuve <hugo@hugovil.com>
2009-03-09 23:32:08 -0400

    ALSA: ASoC: Davinci: Updated sffsdr_hw_params() function to new format
    Signed-off-by: Hugo Villeneuve <hugo@hugovil.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/davinci/davinci-sffsdr.c
+++ sound/soc/davinci/davinci-sffsdr.c
@@ -46,8 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		      SND_SOC_DAIFMT_IB_NF)
 
 static int sffsdr_hw_params(struct snd_pcm_substream *substream,
-			    struct snd_pcm_hw_params *params,
-			    struct snd_soc_dai *dai)
+			    struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = rtd->dai->cpu_dai;
