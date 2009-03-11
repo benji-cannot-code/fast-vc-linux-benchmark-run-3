fe3d6287e4487f14cfff3fe6827b3d4da07d3120
Josef Jiru <josef.jiru@esk.fraunhofer.de>
2009-03-11 15:50:48 +0100

    Staging: rt2870: add Linksys WUSB600N device id
    Updated usb device list to support Linksys WUSB600N wireless adapter
    
    Signed-off-by: Josef Jiru <josef.jiru@gmx.net>

--- drivers/staging/rt2870/rt2870.h
+++ drivers/staging/rt2870/rt2870.h
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RT2870_USB_DEVICES	\
 {	\
 	{USB_DEVICE(0x148F,0x2770)}, /* Ralink */		\
+	{USB_DEVICE(0x1737,0x0071)}, /* Linksys WUSB600N */	\
 	{USB_DEVICE(0x148F,0x2870)}, /* Ralink */		\
 	{USB_DEVICE(0x148F,0x3070)}, /* Ralink */		\
 	{USB_DEVICE(0x0B05,0x1731)}, /* Asus */			\
