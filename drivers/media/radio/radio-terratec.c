26d987d3f0a9eed941e50f8bcb706ef02c80774e
Alexey Klimov <klimov.linux@gmail.com>
2009-03-11 07:37:04 -0300

    V4L/DVB (10961): radio-terratec: remove linux/delay.h which hadn't been used.
    Signed-off-by: Alexey Klimov <klimov.linux@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/radio/radio-terratec.c
+++ drivers/media/radio/radio-terratec.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>	/* Modules 			*/
 #include <linux/init.h>		/* Initdata			*/
 #include <linux/ioport.h>	/* request_region		*/
-#include <linux/delay.h>	/* udelay			*/
 #include <linux/videodev2.h>	/* kernel radio structs		*/
 #include <linux/mutex.h>
 #include <linux/version.h>      /* for KERNEL_VERSION MACRO     */
