f7c0d876372e2461a83bb0a4e75a239138720342
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-03-11 11:02:33 +0000

    [ARM] Revert extraneous changes from the S3C audio header move
    These changes were included in the S3C audio header move but are not
    directly related to it.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- arch/arm/mach-shark/include/mach/io.h
+++ arch/arm/mach-shark/include/mach/io.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCIO_BASE	0xe0000000
 #define IO_SPACE_LIMIT	0xffffffff
 
-#define __io(a)		__typesafe_io(PCIO_BASE + (a))
+#define __io(a)		((void __iomem *)(PCIO_BASE + (a)))
 #define __mem_pci(addr)	(addr)
 
 #endif
