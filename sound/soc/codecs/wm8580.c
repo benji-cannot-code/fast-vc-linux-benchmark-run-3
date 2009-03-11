be2ed02248e18b00d8027ccc7b32655821a6621b
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-03-11 14:12:28 +0000

    ASoC: Remove version display from WM8580 driver
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8580.c
+++ sound/soc/codecs/wm8580.c
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "wm8580.h"
 
-#define WM8580_VERSION "0.1"
-
 struct pll_state {
 	unsigned int in;
 	unsigned int out;
@@ -972,8 +970,6 @@ static int wm8580_probe(struct platform_device *pdev)
 	struct snd_soc_codec *codec;
 	struct wm8580_priv *wm8580;
 	int ret = 0;
-
-	pr_info("WM8580 Audio Codec %s\n", WM8580_VERSION);
 
 	setup = socdev->codec_data;
 	codec = kzalloc(sizeof(struct snd_soc_codec), GFP_KERNEL);
