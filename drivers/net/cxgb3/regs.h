6ca3e726d456a2bd1263a5f2a81dc182d007218d
Divy Le Ray <divy@chelsio.com>
2009-03-12 21:14:09 +0000

    cxgb3: disable high freq non-data interrupts
    Under RX pressure, The HW might generate a high load of interrupts
    to signal mac fifo or free lists overflow.
    Disable the interrupts, and poll the relevant status bits
    to maintain stats.
    
    Signed-off-by: Divy Le Ray <divy@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/cxgb3/regs.h
+++ drivers/net/cxgb3/regs.h
@@ -171,6 +171,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define S_RSPQ0DISABLED    8
 
+#define S_FL0EMPTY    16
+#define V_FL0EMPTY(x) ((x) << S_FL0EMPTY)
+#define F_FL0EMPTY    V_FL0EMPTY(1U)
+
 #define A_SG_EGR_RCQ_DRB_THRSH 0x54
 
 #define S_HIRCQDRBTHRSH    16
@@ -258,6 +262,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define S_RSPQCREDITOVERFOW    2
 #define V_RSPQCREDITOVERFOW(x) ((x) << S_RSPQCREDITOVERFOW)
 #define F_RSPQCREDITOVERFOW    V_RSPQCREDITOVERFOW(1U)
+
+#define S_FLEMPTY    1
+#define V_FLEMPTY(x) ((x) << S_FLEMPTY)
+#define F_FLEMPTY    V_FLEMPTY(1U)
 
 #define A_SG_INT_ENABLE 0x60
 
