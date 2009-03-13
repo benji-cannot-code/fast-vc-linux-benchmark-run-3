b1f275c5f5ff3e741a3fef3ad9d96006b66c73bd
Ingo Molnar <mingo@elte.hu>
2009-03-13 01:30:40 +0100

    locking: rename trace_softirq_[enter|exit] => lockdep_softirq_[enter|exit]
    Impact: cleanup
    
    The naming clashes with upcoming softirq tracepoints, so rename the
    APIs to lockdep_*().
    
    Requested-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -181,7 +181,7 @@ asmlinkage void __do_softirq(void)
 	account_system_vtime(current);
 
 	__local_bh_disable((unsigned long)__builtin_return_address(0));
-	trace_softirq_enter();
+	lockdep_softirq_enter();
 
 	cpu = smp_processor_id();
 restart:
@@ -221,7 +221,7 @@ restart:
 	if (pending)
 		wakeup_softirqd();
 
-	trace_softirq_exit();
+	lockdep_softirq_exit();
 
 	account_system_vtime(current);
 	_local_bh_enable();
