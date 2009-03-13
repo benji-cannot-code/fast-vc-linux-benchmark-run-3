3cb7e0cc5de265dd8e329636632f6f337a4ad1b8
Daniel Mack <daniel@caiaq.de>
2009-03-13 16:37:09 +0100

    [ARM] pxa: add basic support for Colibri PXA300 module
    This patch add basic support for Toradex' Colibri PXA300 module.
    Ethernet is enabled conditionally, depdending on CONFIG_AX88796.
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/include/mach/colibri.h
+++ arch/arm/mach-pxa/include/mach/colibri.h
@@ -1,15 +1,32 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _COLIBRI_H_
 #define _COLIBRI_H_
+/*
+ * common settings for all modules
+ */
 
 /* physical memory regions */
 #define COLIBRI_SDRAM_BASE	0xa0000000      /* SDRAM region */
+
+/* definitions for Colibri PXA270 */
 
 #define COLIBRI_PXA270_FLASH_PHYS	(PXA_CS0_PHYS)  /* Flash region */
 #define COLIBRI_PXA270_ETH_PHYS		(PXA_CS2_PHYS)  /* Ethernet */
 #define COLIBRI_PXA270_ETH_IRQ_GPIO	114
 #define COLIBRI_PXA270_ETH_IRQ		\
 	gpio_to_irq(mfp_to_gpio(COLIBRI_PXA270_ETH_IRQ_GPIO))
+
+/* definitions for Colibri PXA300 */
+
+#define COLIBRI_PXA300_ETH_IRQ_GPIO     26
+#define COLIBRI_PXA300_ETH_IRQ          \
+	gpio_to_irq(mfp_to_gpio(COLIBRI_PXA300_ETH_IRQ_GPIO))
+
+/* definitions for Colibri PXA320 */
+
+#define COLIBRI_PXA320_ETH_IRQ_GPIO     36
+#define COLIBRI_PXA320_ETH_IRQ          \
+	gpio_to_irq(mfp_to_gpio(COLIBRI_PXA320_ETH_IRQ_GPIO))
 
 #endif /* _COLIBRI_H_ */
 
