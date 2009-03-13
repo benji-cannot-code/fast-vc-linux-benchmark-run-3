e516bccbf0019334b0e1cfb2a07b4cc0b1db1b2a
Ilya Yanok <yanok@emcraft.com>
2009-03-13 09:51:46 -0700

    dnet: replace obsolete *netif_rx_* functions with *napi_*
    *netif_rx_* functions is obsolete and removed in newer kernels so
    we need to use corresponding *napi_* functions instead.
    
    Signed-off-by: Ilya Yanok <yanok@emcraft.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dnet.c
+++ drivers/net/dnet.c
@@ -409,7 +409,7 @@ static int dnet_poll(struct napi_struct *napi, int budget)
 		 * packets waiting
 		 */
 		if (!(dnet_readl(bp, RX_FIFO_WCNT) >> 16)) {
-			netif_rx_complete(napi);
+			napi_complete(napi);
 			int_enable = dnet_readl(bp, INTR_ENB);
 			int_enable |= DNET_INTR_SRC_RX_CMDFIFOAF;
 			dnet_writel(bp, int_enable, INTR_ENB);
@@ -448,7 +448,7 @@ static int dnet_poll(struct napi_struct *napi, int budget)
 	if (npackets < budget) {
 		/* We processed all packets available.  Tell NAPI it can
 		 * stop polling then re-enable rx interrupts */
-		netif_rx_complete(napi);
+		napi_complete(napi);
 		int_enable = dnet_readl(bp, INTR_ENB);
 		int_enable |= DNET_INTR_SRC_RX_CMDFIFOAF;
 		dnet_writel(bp, int_enable, INTR_ENB);
@@ -508,7 +508,7 @@ static irqreturn_t dnet_interrupt(int irq, void *dev_id)
 	}
 
 	if (int_current & DNET_INTR_SRC_RX_CMDFIFOAF) {
-		if (netif_rx_schedule_prep(&bp->napi)) {
+		if (napi_schedule_prep(&bp->napi)) {
 			/*
 			 * There's no point taking any more interrupts
 			 * until we have processed the buffers
@@ -517,7 +517,7 @@ static irqreturn_t dnet_interrupt(int irq, void *dev_id)
 			int_enable = dnet_readl(bp, INTR_ENB);
 			int_enable &= ~DNET_INTR_SRC_RX_CMDFIFOAF;
 			dnet_writel(bp, int_enable, INTR_ENB);
-			__netif_rx_schedule(&bp->napi);
+			__napi_schedule(&bp->napi);
 		}
 		handled = 1;
 	}
