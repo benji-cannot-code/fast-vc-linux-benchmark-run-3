f9f5035ab82ee03b9136261a12c617235173facc
Frederic Weisbecker <fweisbec@gmail.com>
2009-03-13 15:42:12 +0100

    tracing/syscalls: support for syscalls tracing on x86
    Extend x86 architecture syscall tracing support with syscall
    metadata table details.
    
    (The upcoming core syscall tracing modifications rely on this.)
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    LKML-Reference: <1236955332-10133-3-git-send-email-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/ftrace.h
+++ arch/x86/include/asm/ftrace.h
@@ -29,6 +29,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #endif
 
+/* FIXME: I don't want to stay hardcoded */
+#ifdef CONFIG_X86_64
+# define FTRACE_SYSCALL_MAX     296
+#else
+# define FTRACE_SYSCALL_MAX     333
+#endif
+
 #ifdef CONFIG_FUNCTION_TRACER
 #define MCOUNT_ADDR		((long)(mcount))
 #define MCOUNT_INSN_SIZE	5 /* sizeof mcount call */
