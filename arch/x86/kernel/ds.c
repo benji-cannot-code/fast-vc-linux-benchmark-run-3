eb9fa3efc6f352d5099a6796f4960c726760b76a
Markus Metzger <markus.t.metzger@intel.com>
2009-03-13 10:45:07 +0100

    x86, bts: add selftest for BTS
    Perform a selftest of branch trace store when a cpu is initialized.
    
    WARN and disable branch trace store support if the selftest fails.
    
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>
    LKML-Reference: <20090313104507.A30125@sedona.ch.intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/ds.c
+++ arch/x86/kernel/ds.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/kernel.h>
 
+#include "ds_selftest.h"
 
 /*
  * The configuration for a particular DS hardware implementation.
@@ -939,6 +940,26 @@ ds_configure(const struct ds_configuration *cfg,
 	if (!cpu_has(cpu, X86_FEATURE_PEBS)) {
 		ds_cfg.sizeof_rec[ds_pebs] = 0;
 		printk(KERN_INFO "[ds] pebs not available\n");
+	}
+
+	if (ds_cfg.sizeof_rec[ds_bts]) {
+		int error;
+
+		error = ds_selftest_bts();
+		if (error) {
+			WARN(1, "[ds] selftest failed. disabling bts.\n");
+			ds_cfg.sizeof_rec[ds_bts] = 0;
+		}
+	}
+
+	if (ds_cfg.sizeof_rec[ds_pebs]) {
+		int error;
+
+		error = ds_selftest_pebs();
+		if (error) {
+			WARN(1, "[ds] selftest failed. disabling pebs.\n");
+			ds_cfg.sizeof_rec[ds_pebs] = 0;
+		}
 	}
 
 	printk(KERN_INFO "[ds] sizes: address: %u bit, ",
