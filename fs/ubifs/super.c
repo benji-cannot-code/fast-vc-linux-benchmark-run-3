0d83ae6dc8aa453ad90554df8eb64edb1355e1b2
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2009-03-15 17:20:22 +0200

    UBIFS: fix bogus assertion
    Empty journal head LEBs are accounted as taken empty as well, so
    the GC LEB does not have to be the only taken empty LEB when
    nounting/remounting.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/super.c
+++ fs/ubifs/super.c
@@ -1319,11 +1319,15 @@ static int mount_ubifs(struct ubifs_info *c)
 		else {
 			c->need_recovery = 0;
 			ubifs_msg("recovery completed");
-			/* GC LEB has to be empty and taken at this point */
-			ubifs_assert(c->lst.taken_empty_lebs == 1);
+			/*
+			 * GC LEB has to be empty and taken at this point. But
+			 * the journal head LEBs may also be accounted as
+			 * "empty taken" if they are empty.
+			 */
+			ubifs_assert(c->lst.taken_empty_lebs > 0);
 		}
 	} else
-		ubifs_assert(c->lst.taken_empty_lebs == 1);
+		ubifs_assert(c->lst.taken_empty_lebs > 0);
 
 	err = dbg_check_filesystem(c);
 	if (err)
@@ -1776,7 +1780,7 @@ static int ubifs_remount_fs(struct super_block *sb, int *flags, char *data)
 		c->bu.buf = NULL;
 	}
 
-	ubifs_assert(c->lst.taken_empty_lebs == 1);
+	ubifs_assert(c->lst.taken_empty_lebs > 0);
 	return 0;
 }
 
