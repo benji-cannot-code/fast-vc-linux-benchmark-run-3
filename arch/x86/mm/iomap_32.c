4743b4a661a847b62bb4eabbce179dbc63784440
Akinobu Mita <akinobu.mita@gmail.com>
2009-03-16 00:15:18 +0900

    x86, mm: remove unnecessary include file from iomap_32.c
    asm/highmem.h inclusion is added to use kmap_atomic_prot_pfn()
    by commit bb6d59ca927d855ffac567b35c0a790c67016103
    
    Now kmap_atomic_prot_pfn is moved to iomap_32.c
    by commit dd63fdcc63f0f853b116b52e56200a0e0227cf5f
    
    So the asm/highmem.h inclusion in iomap_32.c is unnecessary now.
    
    Signed-off-by: Akinobu Mita <akinobu.mita@gmail.com>
    LKML-Reference: <20090315151517.GA29074@localhost.localdomain>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/iomap_32.c
+++ arch/x86/mm/iomap_32.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/iomap.h>
 #include <asm/pat.h>
-#include <asm/highmem.h>
 #include <linux/module.h>
 
 int is_io_mapping_possible(resource_size_t base, unsigned long size)
