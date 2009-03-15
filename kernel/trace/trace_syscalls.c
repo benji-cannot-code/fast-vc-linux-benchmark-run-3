4f4bc701395d94a09918d194d0f39a2bd43e209a
Frederic Weisbecker <fweisbec@gmail.com>
2009-03-15 22:10:35 +0100

    tracing/syscalls: fix missing release of tracing
    Impact: fix 'stuck' syscall tracer
    
    The syscall tracer uses a refcounter to enable several users
    simultaneously.
    
    But the refcounter did not behave correctly and always restored
    its value to 0 after calling start_syscall_tracing(). Therefore,
    stop_syscall_tracing() couldn't release correctly the tasks from
    tracing.
    
    Also the tracer forgot to reset the buffer when it is released.
    
    Drop the pointless refcount decrement on start_syscall_tracing()
    and reset the buffer when we release the tracer.
    
    This fixes two reported issue:
    
    - when we switch from syscall tracer to another tracer, syscall
      tracing continued.
    
    - incorrect use of the refcount.
    
    Reported-by: Andrew Morton <akpm@linux-foundation.org>
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <1237151439-6755-1-git-send-email-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace_syscalls.c
+++ kernel/trace/trace_syscalls.c
@@ -97,8 +97,9 @@ void start_ftrace_syscalls(void)
 	unsigned long flags;
 	struct task_struct *g, *t;
 
+	/* Don't enable the flag on the tasks twice */
 	if (atomic_inc_return(&refcount) != 1)
-		goto out;
+		return;
 
 	arch_init_ftrace_syscalls();
 	read_lock_irqsave(&tasklist_lock, flags);
@@ -108,8 +109,6 @@ void start_ftrace_syscalls(void)
 	} while_each_thread(g, t);
 
 	read_unlock_irqrestore(&tasklist_lock, flags);
-out:
-	atomic_dec(&refcount);
 }
 
 void stop_ftrace_syscalls(void)
@@ -117,8 +116,9 @@ void stop_ftrace_syscalls(void)
 	unsigned long flags;
 	struct task_struct *g, *t;
 
+	/* There are perhaps still some users */
 	if (atomic_dec_return(&refcount))
-		goto out;
+		return;
 
 	read_lock_irqsave(&tasklist_lock, flags);
 
@@ -127,8 +127,6 @@ void stop_ftrace_syscalls(void)
 	} while_each_thread(g, t);
 
 	read_unlock_irqrestore(&tasklist_lock, flags);
-out:
-	atomic_inc(&refcount);
 }
 
 void ftrace_syscall_enter(struct pt_regs *regs)
@@ -202,6 +200,7 @@ static int init_syscall_tracer(struct trace_array *tr)
 static void reset_syscall_tracer(struct trace_array *tr)
 {
 	stop_ftrace_syscalls();
+	tracing_reset_online_cpus(tr);
 }
 
 static struct trace_event syscall_enter_event = {
