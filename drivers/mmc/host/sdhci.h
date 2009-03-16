84c8b8d0a63012a446cbef16e2a4da9fb4bc4bd1
Ben Dooks <ben-linux@fluff.org>
2009-03-17 00:13:57 +0300

    sdhci: Add get_{max,timeout}_clock callbacks
    Some controllers do not provide clock information in their capabilities
    (in the Samsung case, it is because there are multiple clock sources
    available to the controller). Add hooks to allow the system to supply
    clock information.
    
    p.s.
    In the original Ben's patch there was a bug that makes sdhci_add_host()
    return -ENODEV even if callbacks were specified. This is fixed now.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -286,6 +286,8 @@ struct sdhci_ops {
 #endif
 
 	int		(*enable_dma)(struct sdhci_host *host);
+	unsigned int	(*get_max_clock)(struct sdhci_host *host);
+	unsigned int	(*get_timeout_clock)(struct sdhci_host *host);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
