ba07b28e846931da8275d7a01248472d58c1cf62
Alex Deucher <alexdeucher@gmail.com>
2009-03-16 15:37:02 -0400

    drm/radeon: fix logic in r600_page_table_init() to match ati_gart
    This fixes page table init on rs600.
    
    Signed-off-by: Alex Deucher <alexdeucher@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/r600_cp.c
+++ drivers/gpu/drm/radeon/r600_cp.c
@@ -173,7 +173,6 @@ int r600_page_table_init(struct drm_device *dev)
 		if (entry->busaddr[i] == 0) {
 			DRM_ERROR("unable to map PCIGART pages!\n");
 			r600_page_table_cleanup(dev, gart_info);
-			ret = -EINVAL;
 			goto done;
 		}
 		entry_addr = entry->busaddr[i];
@@ -192,6 +191,7 @@ int r600_page_table_init(struct drm_device *dev)
 			entry_addr += ATI_PCIGART_PAGE_SIZE;
 		}
 	}
+	ret = 1;
 done:
 	return ret;
 }
@@ -2096,7 +2096,7 @@ int r600_do_init_cp(struct drm_device *dev, drm_radeon_init_t *init,
 			  dev_priv->gart_info.addr,
 			  dev_priv->pcigart_offset);
 
-		if (r600_page_table_init(dev)) {
+		if (!r600_page_table_init(dev)) {
 			DRM_ERROR("Failed to init GART table\n");
 			r600_do_cleanup_cp(dev);
 			return -EINVAL;
