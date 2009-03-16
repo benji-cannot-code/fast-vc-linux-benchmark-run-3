cf747f1bebb4a1518221149df53b0461a2a40b0f
Frederic Weisbecker <fweisbec@gmail.com>
2009-03-16 01:45:03 +0100

    tracing/core: fix early free of cpumasks
    Impact: fix crashes when tracing cpumasks
    
    While ring-buffer allocation, the cpumasks are allocated too,
    including the tracing cpumask and the per-cpu file mask handler.
    But these cpumasks are freed accidentally just after.
    Fix it.
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <1237164303-11476-1-git-send-email-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -4126,7 +4126,8 @@ __init static int tracer_alloc_buffers(void)
 				       &trace_panic_notifier);
 
 	register_die_notifier(&trace_die_notifier);
-	ret = 0;
+
+	return 0;
 
 out_free_cpumask:
 	free_cpumask_var(tracing_reader_cpumask);
