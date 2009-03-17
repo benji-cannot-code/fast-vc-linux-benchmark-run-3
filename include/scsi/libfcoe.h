5e184c6d2e01c3d59141e99fa3932e5d8c719986
Robert Love <robert.w.love@intel.com>
2009-03-17 11:41:35 -0700

    [SCSI] fcoe: Use percpu kernel funcs for struct fcoe_percpu_s
    Convert fcoe_percpu array to use the per-cpu variables
    that the kernel provides. Use the kernel's functions to
    access this structure.
    
    The cpu member of the fcoe_percpu_s is no longer needed,
    so this patch removes it too.
    
    Signed-off-by: Yi Zou <yi.zou@intel.com>
    Signed-off-by: Robert Love <robert.w.love@intel.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- include/scsi/libfcoe.h
+++ include/scsi/libfcoe.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * this percpu struct for fcoe
  */
 struct fcoe_percpu_s {
-	unsigned int cpu;
 	struct task_struct *thread;
 	struct sk_buff_head fcoe_rx_list;
 	struct page *crc_eof_page;
