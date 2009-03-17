240f645b81c5d03603da70f5258eda1d217ab5f6
Vasu Dev <vasu.dev@intel.com>
2009-03-17 11:42:13 -0700

    [SCSI] fcoe, libfc: add libfcoe module
    Just sets up build environment for libfcoe module towards a
    libfcoe library for libfc LLDs using FCoE as libfc transport.
    
    Common library code to libfcoe is added in next patch.
    
    Also, updated MODULE_LICENSE from "GPL" string to "GPL v2" for
    libfc, libfcoe and fcoe modules to accurately match the licenses.
    
    Signed-off-by: Vasu Dev <vasu.dev@intel.com>
    Signed-off-by: Robert Love <robert.w.love@intel.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/libfc/fc_fcp.c
+++ drivers/scsi/libfc/fc_fcp.c
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 MODULE_AUTHOR("Open-FCoE.org");
 MODULE_DESCRIPTION("libfc");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
 
 static int fc_fcp_debug;
 
