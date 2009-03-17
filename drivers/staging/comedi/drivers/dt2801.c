79f1001573583a48aac4fafd29e0fd0d0ec4345b
Bill Pemberton <wfp5p@virginia.edu>
2009-03-16 22:14:41 -0400

    Staging: comedi: Remove dt2801_private typedef
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/dt2801.c
+++ drivers/staging/comedi/drivers/dt2801.c
@@ -221,11 +221,13 @@ static const struct dt2801_board boardtypes[] = {
 #define n_boardtypes ((sizeof(boardtypes))/(sizeof(boardtypes[0])))
 #define boardtype (*(const struct dt2801_board *)dev->board_ptr)
 
-typedef struct {
+struct dt2801_private {
+
 	const struct comedi_lrange *dac_range_types[2];
 	unsigned int ao_readback[2];
-} dt2801_private;
-#define devpriv ((dt2801_private *)dev->private)
+};
+
+#define devpriv ((struct dt2801_private *)dev->private)
 
 static int dt2801_ai_insn_read(struct comedi_device * dev, struct comedi_subdevice * s,
 	struct comedi_insn * insn, unsigned int * data);
@@ -522,7 +524,7 @@ static int dt2801_attach(struct comedi_device * dev, struct comedi_devconfig * i
 	if ((ret = alloc_subdevices(dev, 4)) < 0)
 		goto out;
 
-	if ((ret = alloc_private(dev, sizeof(dt2801_private))) < 0)
+	if ((ret = alloc_private(dev, sizeof(struct dt2801_private))) < 0)
 		goto out;
 
 	dev->board_name = boardtype.name;
