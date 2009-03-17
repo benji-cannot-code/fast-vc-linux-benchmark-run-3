31871327f953a50bf15ea73570d2e19d2bf202c4
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2009-03-16 17:08:48 -0700

    x86-64: non-paravirt systems always has PSE and PGE
    A paravirtualized system may not have PSE or PGE available to
    guests, so they are not required features.  However, without
    paravirt we can assume that any x86-64 implementation will have
    them available.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- arch/x86/include/asm/required-features.h
+++ arch/x86/include/asm/required-features.h
@@ -49,9 +49,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #ifdef CONFIG_X86_64
+#ifdef CONFIG_PARAVIRT
+/* Paravirtualized systems may not have PSE or PGE available */
 #define NEED_PSE	0
-#define NEED_MSR	(1<<(X86_FEATURE_MSR & 31))
 #define NEED_PGE	0
+#else
+#define NEED_PSE	(1<<(X86_FEATURE_PSE) & 31)
+#define NEED_PGE	(1<<(X86_FEATURE_PGE) & 31)
+#endif
+#define NEED_MSR	(1<<(X86_FEATURE_MSR & 31))
 #define NEED_FXSR	(1<<(X86_FEATURE_FXSR & 31))
 #define NEED_XMM	(1<<(X86_FEATURE_XMM & 31))
 #define NEED_XMM2	(1<<(X86_FEATURE_XMM2 & 31))
