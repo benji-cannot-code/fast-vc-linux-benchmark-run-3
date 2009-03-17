927703b52369a66221f6e33bf744e1bc0c7c61a9
Bill Pemberton <wfp5p@virginia.edu>
2009-03-16 22:03:40 -0400

    Staging: comedi: Add spaces around colons as needed
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/rtd520.c
+++ drivers/staging/comedi/drivers/rtd520.c
@@ -280,7 +280,7 @@ typedef struct rtdBoard_struct {
 static const rtdBoard rtd520Boards[] = {
 	{
 	      name:	"DM7520",
-	      device_id:0x7520,
+	      device_id : 0x7520,
 	      aiChans:	16,
 	      aiBits:	12,
 	      aiMaxGain:32,
@@ -289,7 +289,7 @@ static const rtdBoard rtd520Boards[] = {
 		},
 	{
 	      name:	"PCI4520",
-	      device_id:0x4520,
+	      device_id : 0x4520,
 	      aiChans:	16,
 	      aiBits:	12,
 	      aiMaxGain:128,
@@ -686,9 +686,9 @@ static int rtd_detach(comedi_device *dev);
 
 static comedi_driver rtd520Driver = {
       driver_name: DRV_NAME,
-      module:THIS_MODULE,
-      attach:rtd_attach,
-      detach:rtd_detach,
+      module : THIS_MODULE,
+      attach : rtd_attach,
+      detach : rtd_detach,
 };
 
 static int rtd_ai_rinsn(comedi_device *dev, comedi_subdevice *s,
