b64179d8bef1b46916658b2f9fa79d62dcf65c1e
Bill Pemberton <wfp5p@virginia.edu>
2009-03-16 22:06:47 -0400

    Staging: comedi: Remove DMABUF typedef
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/s626.h
+++ drivers/staging/comedi/drivers/s626.h
@@ -740,8 +740,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*   unsigned int enc; */
 /* }CallCounter; */
 
-typedef struct bufferDMA {
+struct bufferDMA {
 	dma_addr_t PhysicalBase;
 	void *LogicalBase;
 	uint32_t DMAHandle;
-} DMABUF;
+};
