deed9f8261664cc597869d537b14d6c62dca3e78
Herbert Xu <herbert@gondor.apana.org.au>
2009-03-17 13:10:52 -0700

    gro: Fix vlan/netpoll check again
    Jarek Poplawski pointed out that my previous fix is broken for
    VLAN+netpoll as if netpoll is enabled we'd end up in the normal
    receive path instead of the VLAN receive path.
    
    This patch fixes it by calling the VLAN receive hook.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/8021q/vlan_core.c
+++ net/8021q/vlan_core.c
@@ -80,9 +80,6 @@ static int vlan_gro_common(struct napi_struct *napi, struct vlan_group *grp,
 {
 	struct sk_buff *p;
 
-	if (netpoll_rx_on(skb))
-		return GRO_NORMAL;
-
 	if (skb_bond_should_drop(skb))
 		goto drop;
 
@@ -108,6 +105,9 @@ drop:
 int vlan_gro_receive(struct napi_struct *napi, struct vlan_group *grp,
 		     unsigned int vlan_tci, struct sk_buff *skb)
 {
+	if (netpoll_rx_on(skb))
+		return vlan_hwaccel_receive_skb(skb, grp, vlan_tci);
+
 	skb_gro_reset_offset(skb);
 
 	return napi_skb_finish(vlan_gro_common(napi, grp, vlan_tci, skb), skb);
@@ -121,6 +121,9 @@ int vlan_gro_frags(struct napi_struct *napi, struct vlan_group *grp,
 
 	if (!skb)
 		return NET_RX_DROP;
+
+	if (netpoll_rx_on(skb))
+		return vlan_hwaccel_receive_skb(skb, grp, vlan_tci);
 
 	return napi_frags_finish(napi, skb,
 				 vlan_gro_common(napi, grp, vlan_tci, skb));
