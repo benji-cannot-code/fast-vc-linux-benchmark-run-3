a1d931e319995cf6f0ab470a5feae124661c6b9a
Wim Van Sebroeck <wim@iguana.be>
2009-03-18 08:35:09 +0000

    [WATCHDOG] More coding-style and trivial clean-up
    Some more cleaning-up of the watchdog drivers.
    
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/w83697ug_wdt.c
+++ drivers/watchdog/w83697ug_wdt.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	w83697ug/uf WDT driver
  *
  *	(c) Copyright 2008 Flemming Fransen <ff@nrvissing.net>
- *              reused original code to supoprt w83697ug/uf.
+ *		reused original code to support w83697ug/uf.
  *
  *	Based on w83627hf_wdt.c which is based on advantechwdt.c
  *	which is based on wdt.c.
