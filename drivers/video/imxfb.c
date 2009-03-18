eb849f41c2d69b9606c0ada97ce384ea735d62fa
Sascha Hauer <s.hauer@pengutronix.de>
2009-03-18 11:29:31 +0100

    imxfb: calculate bpix value from bits_per_pixel
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/video/imxfb.c
+++ drivers/video/imxfb.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/math64.h>
 
 #include <mach/imxfb.h>
+#include <mach/hardware.h>
 
 /*
  * Complain if VAR is out of range.
@@ -531,8 +532,24 @@ static int imxfb_activate_var(struct fb_var_screeninfo *var, struct fb_info *inf
 				lcd_clk / pcr);
 	}
 
+	switch (var->bits_per_pixel) {
+	case 32:
+		pcr |= PCR_BPIX_18;
+		break;
+	case 16:
+	default:
+		if (cpu_is_mx1())
+			pcr |= PCR_BPIX_12;
+		else
+			pcr |= PCR_BPIX_16;
+		break;
+	case 8:
+		pcr |= PCR_BPIX_8;
+		break;
+	}
+
 	/* add sync polarities */
-	pcr |= fbi->pcr & ~0x3F;
+	pcr |= fbi->pcr & ~(0x3f | (7 << 25));
 
 	writel(pcr, fbi->regs + LCDC_PCR);
 	writel(fbi->pwmr, fbi->regs + LCDC_PWMR);
