e40562e45bf5e6b10763fba3b414aafd16e2ee47
Wim Van Sebroeck <wim@iguana.be>
2009-03-18 09:09:26 +0000

    [WATCHDOG] Fix io.h & uaccess.h includes.
    Fix following includes:
    * #include <asm/io.h> should be #include <linux/io.h>
    * #include <asm/uaccess.h> should be #include <linux/uaccess.h>
    
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/riowd.c
+++ drivers/watchdog/riowd.c
@@ -15,9 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/watchdog.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-
-#include <asm/io.h>
-#include <asm/uaccess.h>
+#include <linux/io.h>
+#include <linux/uaccess.h>
 
 
 /* RIO uses the NatSemi Super I/O power management logical device
