45cbcf1cdf86855bf39361476a27877e9ac27da8
Daniel Mack <daniel@caiaq.de>
2009-03-18 11:03:53 +0100

    ALSA: snd-usb-caiaq: only warn once on streaming errors
    Limit the number of printed warnings to one in case of streaming errors.
    printk() happens to be expensive, especially in code called as often as
    here.
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/caiaq/caiaq-audio.c
+++ sound/usb/caiaq/caiaq-audio.c
@@ -115,6 +115,7 @@ static int stream_start(struct snd_usb_caiaqdev *dev)
 	dev->output_panic = 0;
 	dev->first_packet = 1;
 	dev->streaming = 1;
+	dev->warned = 0;
 
 	for (i = 0; i < N_URBS; i++) {
 		ret = usb_submit_urb(dev->data_urbs_in[i], GFP_ATOMIC);
@@ -407,10 +408,11 @@ static void read_in_urb(struct snd_usb_caiaqdev *dev,
 		break;
 	}
 
-	if (dev->input_panic || dev->output_panic) {
+	if ((dev->input_panic || dev->output_panic) && !dev->warned) {
 		debug("streaming error detected %s %s\n", 
 				dev->input_panic ? "(input)" : "",
 				dev->output_panic ? "(output)" : "");
+		dev->warned = 1;
 	}
 }
 
