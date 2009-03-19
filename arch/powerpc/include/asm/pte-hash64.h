c34db826fd5a7fed322061ad55fe7b956643a930
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2009-03-19 19:34:15 +0000

    powerpc/mm: Add option for non-atomic PTE updates to ppc64
    ppc32 has it already, add it to ppc64 as a preliminary for adding
    support for Book3E 64-bit support
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/pte-hash64.h
+++ arch/powerpc/include/asm/pte-hash64.h
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PTEIDX_SECONDARY	0x8
 #define _PTEIDX_GROUP_IX	0x7
 
+/* Hash table based platforms need atomic updates of the linux PTE */
+#define PTE_ATOMIC_UPDATES	1
 
 #ifdef CONFIG_PPC_64K_PAGES
 #include <asm/pte-hash64-64k.h>
