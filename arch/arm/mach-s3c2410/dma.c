bc3ff0fe447615fbf16fcb0385940ee12542e8b3
Ben Dooks <ben@simtec.co.uk>
2009-03-19 15:02:35 +0000

    [ARM] S3C24XX: DMA: Split hardware regs out of <mach/dma.h>
    The <mach/dma.h> is for the driver API for the DMA system and should
    not have anything relying on the CPU specific registers.
    
    Remove the registers to <plat/dma-regs.h> for the code that really
    needs to know about them.
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c2410/dma.c
+++ arch/arm/mach-s3c2410/dma.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/regs-serial.h>
 #include <mach/regs-gpio.h>
 #include <plat/regs-ac97.h>
+#include <plat/regs-dma.h>
 #include <mach/regs-mem.h>
 #include <mach/regs-lcd.h>
 #include <mach/regs-sdi.h>
--- arch/arm/plat-s3c24xx/dma.c
+++ arch/arm/plat-s3c24xx/dma.c
@@ -32,10 +32,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq.h>
 #include <mach/hardware.h>
 #include <mach/dma.h>
-
 #include <mach/map.h>
 
 #include <plat/dma-plat.h>
+#include <plat/regs-dma.h>
 
 /* io map for dma */
 static void __iomem *dma_base;
