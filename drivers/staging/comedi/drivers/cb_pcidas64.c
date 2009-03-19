12fd1add81074c6f83b47831bf67a9761377ba41
Bill Pemberton <wfp5p@virginia.edu>
2009-03-19 17:57:22 -0400

    Staging: comedi: Remove pcidas64_private typedef
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/cb_pcidas64.c
+++ drivers/staging/comedi/drivers/cb_pcidas64.c
@@ -1066,7 +1066,8 @@ struct ext_clock_info {
 };
 
 /* this structure is for data unique to this hardware driver. */
-typedef struct {
+struct pcidas64_private {
+
 	struct pci_dev *hw_dev;	// pointer to board's pci_dev struct
 	// base addresses (physical)
 	resource_size_t plx9080_phys_iobase;
@@ -1110,12 +1111,13 @@ typedef struct {
 	unsigned int ai_fifo_segment_length;
 	struct ext_clock_info ext_clock;
 	short ao_bounce_buffer[DAC_FIFO_SIZE];
-} pcidas64_private;
+};
+
 
 /* inline function that makes it easier to
  * access the private structure.
  */
-static inline pcidas64_private *priv(struct comedi_device * dev)
+static inline struct pcidas64_private *priv(struct comedi_device * dev)
 {
 	return dev->private;
 }
@@ -1686,7 +1688,7 @@ static int attach(struct comedi_device *dev, struct comedi_devconfig *it)
 /*
  * Allocate the private structure area.
  */
-	if (alloc_private(dev, sizeof(pcidas64_private)) < 0)
+	if (alloc_private(dev, sizeof(struct pcidas64_private)) < 0)
 		return -ENOMEM;
 
 /*
