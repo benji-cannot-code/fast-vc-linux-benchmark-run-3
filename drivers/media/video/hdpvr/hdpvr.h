8025fd2323c00b60871b107f15cc020814febfb5
Janne Grunau <j@jannau.net>
2009-03-19 19:00:35 -0300

    V4L/DVB (11125): fix mispelled Hauppauge in HD PVR and PVR USB2 driver comments
    Signed-off-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/hdpvr/hdpvr.h
+++ drivers/media/video/hdpvr/hdpvr.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Hauppage HD PVR USB driver
+ * Hauppauge HD PVR USB driver
  *
  * Copyright (C) 2008      Janne Grunau (j@jannau.net)
  *
