d763553916d45f82557b99b81bc03fa5060fefab
Takashi Iwai <tiwai@suse.de>
2009-03-19 09:59:21 +0100

    ALSA: pcm - avoid unnecessary inline
    Remove unnecessary explicit inlininig of internal functions.
    Let compiler optimize.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -149,8 +149,9 @@ static void xrun(struct snd_pcm_substream *substream)
 	}
 }
 
-static inline snd_pcm_uframes_t snd_pcm_update_hw_ptr_pos(struct snd_pcm_substream *substream,
-							  struct snd_pcm_runtime *runtime)
+static snd_pcm_uframes_t
+snd_pcm_update_hw_ptr_pos(struct snd_pcm_substream *substream,
+			  struct snd_pcm_runtime *runtime)
 {
 	snd_pcm_uframes_t pos;
 
@@ -168,8 +169,8 @@ static inline snd_pcm_uframes_t snd_pcm_update_hw_ptr_pos(struct snd_pcm_substre
 	return pos;
 }
 
-static inline int snd_pcm_update_hw_ptr_post(struct snd_pcm_substream *substream,
-					     struct snd_pcm_runtime *runtime)
+static int snd_pcm_update_hw_ptr_post(struct snd_pcm_substream *substream,
+				      struct snd_pcm_runtime *runtime)
 {
 	snd_pcm_uframes_t avail;
 
@@ -201,7 +202,7 @@ static inline int snd_pcm_update_hw_ptr_post(struct snd_pcm_substream *substream
 		}							\
 	} while (0)
 
-static inline int snd_pcm_update_hw_ptr_interrupt(struct snd_pcm_substream *substream)
+static int snd_pcm_update_hw_ptr_interrupt(struct snd_pcm_substream *substream)
 {
 	struct snd_pcm_runtime *runtime = substream->runtime;
 	snd_pcm_uframes_t pos;
