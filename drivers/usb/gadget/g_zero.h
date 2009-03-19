bc9df4dd978a684c6630fbfa020f1e6337455a3e
David Brownell <dbrownell@users.sourceforge.net>
2009-03-19 14:16:09 -0700

    USB: gadget: gadget zero uses new suspend/resume hooks
    Use the new device-level suspend/resume hooks for Gadget Zero;
    always enable them with the OTG test mode; and support remote
    wakeup on both configurations even in non-OTG mode.
    
    This ensures that both configurations can pass the USBCV remote
    wakeup tests when the OTG test mode is enabled.  This changes
    behavior by adding autoresume support to the loopback config
    even in non-OTG mode; the test failure was that it didn't work
    in OTG mode.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/g_zero.h
+++ drivers/usb/gadget/g_zero.h
@@ -20,7 +20,7 @@ void disable_endpoints(struct usb_composite_dev *cdev,
 		struct usb_ep *in, struct usb_ep *out);
 
 /* configuration-specific linkup */
-int sourcesink_add(struct usb_composite_dev *cdev);
-int loopback_add(struct usb_composite_dev *cdev);
+int sourcesink_add(struct usb_composite_dev *cdev, bool autoresume);
+int loopback_add(struct usb_composite_dev *cdev, bool autoresume);
 
 #endif /* __G_ZERO_H */
