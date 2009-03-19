7f5300af44d448083cd2be1d976de980774ba8df
Daniel Mack <daniel@caiaq.de>
2009-03-19 15:45:19 +0100

    [ARM] pxa: add pxa320 missing pin function for CS2 on GPIO3
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/include/mach/mfp-pxa320.h
+++ arch/arm/mach-pxa/include/mach/mfp-pxa320.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GPIO17_2_GPIO		MFP_CFG(GPIO17_2, AF0)
 
 /* Chip Select */
+#define GPIO3_nCS2		MFP_CFG(GPIO3, AF1)
 #define GPIO4_nCS3		MFP_CFG(GPIO4, AF1)
 
 /* AC97 */
