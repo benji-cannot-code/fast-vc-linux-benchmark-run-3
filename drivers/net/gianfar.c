18f1ee07ca6a756da0150b7637e4ffdb149db3f3
Anton Vorontsov <avorontsov@ru.mvista.com>
2009-03-21 13:27:55 -0700

    gianfar: Fix build with CONFIG_PM enabled
    commit 4826857f1bf07f9c0f1495e9b05d125552c88a85 ("gianfar: pass the
    proper dev to DMA ops") introduced this build breakage:
    
      CC      drivers/net/gianfar.o
    drivers/net/gianfar.c: In function 'gfar_suspend':
    drivers/net/gianfar.c:552: error: 'struct gfar_private' has no member named 'dev'
    drivers/net/gianfar.c: In function 'gfar_resume':
    drivers/net/gianfar.c:601: error: 'struct gfar_private' has no member named 'dev'
    make[2]: *** [drivers/net/gianfar.o] Error 1
    
    Fix this by converting suspend and resume routines to use
    gfar_private->ndev.
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Acked-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/gianfar.c
+++ drivers/net/gianfar.c
@@ -550,7 +550,7 @@ static int gfar_remove(struct of_device *ofdev)
 static int gfar_suspend(struct of_device *ofdev, pm_message_t state)
 {
 	struct gfar_private *priv = dev_get_drvdata(&ofdev->dev);
-	struct net_device *dev = priv->dev;
+	struct net_device *dev = priv->ndev;
 	unsigned long flags;
 	u32 tempval;
 
@@ -599,7 +599,7 @@ static int gfar_suspend(struct of_device *ofdev, pm_message_t state)
 static int gfar_resume(struct of_device *ofdev)
 {
 	struct gfar_private *priv = dev_get_drvdata(&ofdev->dev);
-	struct net_device *dev = priv->dev;
+	struct net_device *dev = priv->ndev;
 	unsigned long flags;
 	u32 tempval;
 	int magic_packet = priv->wol_en &&
