ed0a8ad3018c4fd6c0a287b10fb6de33b80f1491
Jeremy Fitzhardinge <jeremy@goop.org>
2009-03-22 14:48:44 -0700

    x86/dmi: fix dmi_alloc() section mismatches
    Impact: section mismatch fix
    
    Ingo reports these warnings:
    > WARNING: vmlinux.o(.text+0x6a288e): Section mismatch in reference from
    > the function dmi_alloc() to the function .init.text:extend_brk()
    > The function dmi_alloc() references
    > the function __init extend_brk().
    > This is often because dmi_alloc lacks a __init annotation or the
    > annotation of extend_brk is wrong.
    
    dmi_alloc() is a static inline, and so should be immune to this
    kind of error.  But force it to be inlined and make it __init
    anyway, just to be extra sure.
    
    All of dmi_alloc()'s callers are already __init.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Cc: Yinghai Lu <yinghai@kernel.org>
    LKML-Reference: <49C6B23C.2040308@goop.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/dmi.h
+++ arch/x86/include/asm/dmi.h
@@ -2,10 +2,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_DMI_H
 #define _ASM_X86_DMI_H
 
+#include <linux/compiler.h>
+#include <linux/init.h>
+
 #include <asm/io.h>
 #include <asm/setup.h>
 
-static inline void *dmi_alloc(unsigned len)
+static __always_inline __init void *dmi_alloc(unsigned len)
 {
 	return extend_brk(len, sizeof(int));
 }
