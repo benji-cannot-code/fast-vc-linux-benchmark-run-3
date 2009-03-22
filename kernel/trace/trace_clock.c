f62eca69bb27a51a50e931cd2711303ea1ef054c
Dmitri Vorobiev <dmitri.vorobiev@movial.com>
2009-03-22 19:11:11 +0200

    tracing: fix four sparse warnings
    Impact: cleanup.
    
    This patch fixes the following sparse warnings:
    
     kernel/trace/trace.c:385:9: warning: symbol 'trace_seq_to_buffer' was
     not declared. Should it be static?
    
     kernel/trace/trace_clock.c:29:13: warning: symbol 'trace_clock_local'
     was not declared. Should it be static?
    
     kernel/trace/trace_clock.c:54:13: warning: symbol 'trace_clock' was not
     declared. Should it be static?
    
     kernel/trace/trace_clock.c:74:13: warning: symbol 'trace_clock_global'
     was not declared. Should it be static?
    
    Signed-off-by: Dmitri Vorobiev <dmitri.vorobiev@movial.com>
    LKML-Reference: <1237741871-5827-4-git-send-email-dmitri.vorobiev@movial.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace_clock.c
+++ kernel/trace/trace_clock.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/percpu.h>
 #include <linux/sched.h>
 #include <linux/ktime.h>
+#include <linux/trace_clock.h>
 
 /*
  * trace_clock_local(): the simplest and least coherent tracing clock.
