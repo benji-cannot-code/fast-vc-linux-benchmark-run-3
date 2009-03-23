224ea04a2a1f00792a3185d60e8eba9f7e62468b
Greg Kroah-Hartman <gregkh@suse.de>
2009-03-23 13:12:00 -0700

    Staging: epl: fix sparse warning in proc_fs.c
    proc_fs.c needs to include proc_fs.h
    
    Cc: Daniel Krueger <daniel.krueger@systec-electronic.com>
    Cc: Ronald Sieber <Ronald.Sieber@systec-electronic.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/epl/proc_fs.c
+++ drivers/staging/epl/proc_fs.c
@@ -95,6 +95,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "fec.h"
 #endif
 
+#include "proc_fs.h"
+
 /***************************************************************************/
 /*                                                                         */
 /*                                                                         */
