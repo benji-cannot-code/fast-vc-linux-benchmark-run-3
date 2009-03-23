db4808a10ffddbce3481a071021c977197ab394a
Greg Kroah-Hartman <gregkh@suse.de>
2009-03-23 12:10:47 -0700

    Staging: epl: remove REENTRANT
    It's not used and is not needed.
    
    Cc: Daniel Krueger <daniel.krueger@systec-electronic.com>
    Cc: Ronald Sieber <Ronald.Sieber@systec-electronic.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/epl/global.h
+++ drivers/staging/epl/global.h
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CONST const		// variables mapped to ROM (i.e. flash)
 #endif
 
-#define REENTRANT
-
 #define TRACE  printk
 
 #ifndef SHORT
