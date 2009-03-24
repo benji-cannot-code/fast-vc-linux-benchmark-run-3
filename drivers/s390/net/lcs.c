2a55b226180ec6e3d4756a2e9388401a0928478f
Klaus-Dieter Wacker <kdwacker@de.ibm.com>
2009-03-24 03:27:44 +0000

    lcs: invalid return codes from hard_start_xmit.
    Lcs hard_start_xmit routine issued return codes other than
    defined for this interface. Now lcs returns only either
    NETDEV_TX_OK or NETDEV_TX_BUSY.
    
    Signed-off-by: Klaus-Dieter Wacker <kdwacker@de.ibm.com>
    Signed-off-by: Ursula Braun <ursula.braun@de.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/s390/net/lcs.c
+++ drivers/s390/net/lcs.c
@@ -1563,7 +1563,7 @@ __lcs_start_xmit(struct lcs_card *card, struct sk_buff *skb,
 	if (skb == NULL) {
 		card->stats.tx_dropped++;
 		card->stats.tx_errors++;
-		return -EIO;
+		return 0;
 	}
 	if (card->state != DEV_STATE_UP) {
 		dev_kfree_skb(skb);
@@ -1588,7 +1588,7 @@ __lcs_start_xmit(struct lcs_card *card, struct sk_buff *skb,
 		card->tx_buffer = lcs_get_buffer(&card->write);
 		if (card->tx_buffer == NULL) {
 			card->stats.tx_dropped++;
-			rc = -EBUSY;
+			rc = NETDEV_TX_BUSY;
 			goto out;
 		}
 		card->tx_buffer->callback = lcs_txbuffer_cb;
