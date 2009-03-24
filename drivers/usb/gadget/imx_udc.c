0b7116832412e8c15c5d18e36ffb80f2b8f9234b
Kay Sievers <kay.sievers@vrfy.org>
2009-03-24 16:38:23 -0700

    usb: struct device - replace bus_id with dev_name(), dev_set_name()
    Acked-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>

--- drivers/usb/gadget/imx_udc.c
+++ drivers/usb/gadget/imx_udc.c
@@ -1187,8 +1187,8 @@ static struct imx_udc_struct controller = {
 		.ep0		= &controller.imx_ep[0].ep,
 		.name		= driver_name,
 		.dev = {
-			 .bus_id	= "gadget",
-		 },
+			.init_name	= "gadget",
+		},
 	},
 
 	.imx_ep[0] = {
