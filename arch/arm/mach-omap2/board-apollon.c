cbf9629da04d55e6106ebe4fe3b2f6486cef0d01
Tony Lindgren <tony@atomide.com>
2009-03-23 18:07:35 -0700

    ARM: OMAP: No need to include board-apollon.h from hardware.h
    Move the defines to the associated board file and remove
    the now unnecessary header file.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-apollon.c
+++ arch/arm/mach-omap2/board-apollon.c
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define APOLLON_FLASH_CS	0
 #define APOLLON_ETH_CS		1
+#define APOLLON_ETHR_GPIO_IRQ	74
 
 static struct mtd_partition apollon_partitions[] = {
 	{
--- arch/arm/plat-omap/include/mach/hardware.h
+++ arch/arm/plat-omap/include/mach/hardware.h
@@ -307,10 +307,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-ldp.h"
 #endif
 
-#ifdef CONFIG_MACH_OMAP_APOLLON
-#include "board-apollon.h"
-#endif
-
 #ifdef CONFIG_MACH_VOICEBLUE
 #include "board-voiceblue.h"
 #endif
