014ef2bdc24680a008399c05c31e6c9c8f84df56
Tony Lindgren <tony@atomide.com>
2009-03-23 18:07:36 -0700

    ARM: OMAP: No need to include board-h4.h from hardware.h
    Move the defines to the associated board file and remove
    the now unnecessary header file. Also rename
    OMAP24XX_ETHR_GPIO_IRQ to H4_ETHR_GPIO_IRQ.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/mach/hardware.h
+++ arch/arm/plat-omap/include/mach/hardware.h
@@ -295,10 +295,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * ---------------------------------------------------------------------------
  */
 
-#ifdef CONFIG_MACH_OMAP_H4
-#include "board-h4.h"
-#endif
-
 #ifdef CONFIG_MACH_OMAP3_BEAGLE
 #include "board-omap3beagle.h"
 #endif
