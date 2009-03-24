65c6d8e202476e23fc18d334e2fd4851be66372b
Tony Lindgren <tony@atomide.com>
2009-03-23 18:07:32 -0700

    ARM: OMAP: No need to include board-perseus2.h or board-fsample.h from hardware.h
    Move defines to the board file and remove the now
    unnecessary headers.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/mach/hardware.h
+++ arch/arm/plat-omap/include/mach/hardware.h
@@ -303,14 +303,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-h2.h"
 #endif
 
-#ifdef CONFIG_MACH_OMAP_PERSEUS2
-#include "board-perseus2.h"
-#endif
-
-#ifdef CONFIG_MACH_OMAP_FSAMPLE
-#include "board-fsample.h"
-#endif
-
 #ifdef CONFIG_MACH_OMAP_H3
 #include "board-h3.h"
 #endif
