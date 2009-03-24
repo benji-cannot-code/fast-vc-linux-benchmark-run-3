f6e2cc15e4072facbebdf8c83d8dbc991b52e51d
Hiroshi DOYU <Hiroshi.DOYU@nokia.com>
2009-03-23 18:07:32 -0700

    omap mailbox: remove unnecessary header file inclusion
    Signed-off-by: Hiroshi DOYU <Hiroshi.DOYU@nokia.com>

--- arch/arm/plat-omap/mailbox.c
+++ arch/arm/plat-omap/mailbox.c
@@ -22,15 +22,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/sched.h>
 #include <linux/interrupt.h>
 #include <linux/device.h>
-#include <linux/blkdev.h>
-#include <linux/err.h>
 #include <linux/delay.h>
-#include <linux/io.h>
+
 #include <mach/mailbox.h>
 
 static int enable_seq_bit;
