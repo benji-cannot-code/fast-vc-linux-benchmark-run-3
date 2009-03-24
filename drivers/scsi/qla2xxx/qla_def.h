fbc53ec34cbb8892dce5ff959456854313413a6a
Andrew Vasquez <andrew.vasquez@qlogic.com>
2009-03-24 09:08:15 -0700

    [SCSI] qla2xxx: Reduce request queue-size overhead with recent ISPs.
    The original code to 'resize request-queues' based on iocb-counts
    and employed during early ISP23xx testing was too
    overly-pessimistic with regards to latencies in the firmware
    pulling requests.  Recent ISPs can easily keep up processing a
    stream of commands from an abbreviated (effectively, half the
    original size) queue.
    
    Signed-off-by: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -177,8 +177,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* ISP request and response entry counts (37-65535) */
 #define REQUEST_ENTRY_CNT_2100		128	/* Number of request entries. */
 #define REQUEST_ENTRY_CNT_2200		2048	/* Number of request entries. */
-#define REQUEST_ENTRY_CNT_2XXX_EXT_MEM	4096	/* Number of request entries. */
-#define REQUEST_ENTRY_CNT_24XX		4096	/* Number of request entries. */
+#define REQUEST_ENTRY_CNT_24XX		2048	/* Number of request entries. */
 #define RESPONSE_ENTRY_CNT_2100		64	/* Number of response entries.*/
 #define RESPONSE_ENTRY_CNT_2300		512	/* Number of response entries.*/
 
