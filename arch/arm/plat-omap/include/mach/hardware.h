bdffb500c75b54ea8725da3a8e570b810b0c60f2
Tony Lindgren <tony@atomide.com>
2009-03-23 18:07:35 -0700

    ARM: OMAP: No need to include board-omap2430sdp.h from hardware.h
    Move the defines to the associated board file and remove
    the now unnecessary header file. Also rename
    SDP2430_ETHR_GPIO_IRQ to SDP2430_ETHR_GPIO_IRQ.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/mach/hardware.h
+++ arch/arm/plat-omap/include/mach/hardware.h
@@ -299,10 +299,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-h4.h"
 #endif
 
-#ifdef CONFIG_MACH_OMAP_2430SDP
-#include "board-2430sdp.h"
-#endif
-
 #ifdef CONFIG_MACH_OMAP3_BEAGLE
 #include "board-omap3beagle.h"
 #endif
