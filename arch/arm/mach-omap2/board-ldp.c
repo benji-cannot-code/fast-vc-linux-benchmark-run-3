5fab6fe2557f334d2d343566cbf08cd72ee5836b
Tony Lindgren <tony@atomide.com>
2009-03-23 18:07:37 -0700

    ARM: OMAP: No need to include board-ldp.h from hardware.h
    Move the defines to the associated board file and remove
    the now unnecessary header file. Also rename
    OMAP34XX_ETHR_START to LDP_ETHR_START.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-ldp.c
+++ arch/arm/mach-omap2/board-ldp.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 
-#include <mach/board-ldp.h>
 #include <mach/mcspi.h>
 #include <mach/gpio.h>
 #include <mach/board.h>
@@ -42,12 +41,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "mmc-twl4030.h"
 
-#define SDP3430_SMC91X_CS	3
+#define LDP_SMC911X_CS		1
+#define LDP_SMC911X_GPIO	152
+#define DEBUG_BASE		0x08000000
+#define LDP_ETHR_START		DEBUG_BASE
 
 static struct resource ldp_smc911x_resources[] = {
 	[0] = {
-		.start	= OMAP34XX_ETHR_START,
-		.end	= OMAP34XX_ETHR_START + SZ_4K,
+		.start	= LDP_ETHR_START,
+		.end	= LDP_ETHR_START + SZ_4K,
 		.flags	= IORESOURCE_MEM,
 	},
 	[1] = {
--- arch/arm/plat-omap/include/mach/hardware.h
+++ arch/arm/plat-omap/include/mach/hardware.h
@@ -299,10 +299,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-omap3beagle.h"
 #endif
 
-#ifdef CONFIG_MACH_OMAP_LDP
-#include "board-ldp.h"
-#endif
-
 #ifdef CONFIG_MACH_VOICEBLUE
 #include "board-voiceblue.h"
 #endif
