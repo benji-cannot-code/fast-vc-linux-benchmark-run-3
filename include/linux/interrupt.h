0524bebff3f842c3ecdf3733a94faf5933f4af32
Thomas Gleixner <tglx@linutronix.de>
2009-03-24 11:46:22 +0100

    genirq: threaded irq handlers review fixups
    Delta patch to address the review comments.
    
          - Implement warning when IRQ_WAKE_THREAD is requested and no
            thread handler installed
          - coding style fixes
    
    Pointed-out-by: Christoph Hellwig <hch@infradead.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -64,10 +64,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Bits used by threaded handlers:
  * IRQTF_RUNTHREAD - signals that the interrupt handler thread should run
  * IRQTF_DIED      - handler thread died
+ * IRQTF_WARNED    - warning "IRQ_WAKE_THREAD w/o thread_fn" has been printed
  */
 enum {
 	IRQTF_RUNTHREAD,
 	IRQTF_DIED,
+	IRQTF_WARNED,
 };
 
 typedef irqreturn_t (*irq_handler_t)(int, void *);
