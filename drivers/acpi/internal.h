0660a22b57a2f1b882f43c7f20eb4a865ca49c23
Bjorn Helgaas <bjorn.helgaas@hp.com>
2009-03-24 16:49:58 -0600

    ACPI: call acpi_system_init() explicitly rather than as initcall
    This patch makes acpi_init() call acpi_system_init() directly.
    Previously, both were subsys_initcalls.  acpi_system_init()
    must happen after acpi_init(), and it's better to call it
    explicitly rather than rely on link ordering.
    
    Signed-off-by: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/internal.h
+++ drivers/acpi/internal.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* For use by Linux/ACPI infrastructure, not drivers */
 
 int acpi_scan_init(void);
+int acpi_system_init(void);
 
 /* --------------------------------------------------------------------------
                                   Power Resource
--- drivers/acpi/system.c
+++ drivers/acpi/system.c
@@ -572,12 +572,9 @@ static int acpi_system_procfs_init(void)
 }
 #endif
 
-static int __init acpi_system_init(void)
+int __init acpi_system_init(void)
 {
-	int result = 0;
-
-	if (acpi_disabled)
-		return 0;
+	int result;
 
 	result = acpi_system_procfs_init();
 	if (result)
@@ -587,5 +584,3 @@ static int __init acpi_system_init(void)
 
 	return result;
 }
-
-subsys_initcall(acpi_system_init);
