fe9dc058e84eada57a3cc34bb31dc624541259e1
Bill Pemberton <wfp5p@virginia.edu>
2009-03-25 11:05:14 -0400

    Staging: comedi: Remove LONG and *PLONG typedefs in addi-data
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/addi-data/addi_common.h
+++ drivers/staging/comedi/drivers/addi-data/addi_common.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SUCCESS	1
 
 /* variable type definition */
-typedef int LONG, *PLONG;		/* 32-bit */
 typedef unsigned int ULONG, *PULONG;	/* 32-bit */
 typedef unsigned int DWORD, *PDWORD;	/* 32-bit */
 typedef unsigned long ULONG_PTR;
