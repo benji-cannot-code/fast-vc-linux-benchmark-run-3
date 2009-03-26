eff8b54ccccb85f1c4b775ce046357c21941b8d6
Janne Grunau <j@jannau.net>
2009-03-26 20:29:39 -0300

    V4L/DVB (11228): hdpvr: use debugging macro for buffer status
    Signed-off-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/hdpvr/hdpvr-video.c
+++ drivers/media/video/hdpvr/hdpvr-video.c
@@ -29,6 +29,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define BULK_URB_TIMEOUT 1250 /* 1.25 seconds */
 
+#define print_buffer_status() v4l2_dbg(MSG_BUFFER, hdpvr_debug, dev->video_dev,\
+				       "%s:%d buffer stat: %d free, %d proc\n",\
+				       __func__, __LINE__,		\
+				       list_size(&dev->free_buff_list),	\
+				       list_size(&dev->rec_buff_list))
+
 struct hdpvr_fh {
 	struct hdpvr_device	*dev;
 };
@@ -192,10 +198,7 @@ static int hdpvr_submit_buffers(struct hdpvr_device *dev)
 		list_move_tail(&buf->buff_list, &dev->rec_buff_list);
 	}
 err:
-	v4l2_dbg(MSG_BUFFER, hdpvr_debug, dev->video_dev,
-		 "buffer queue stat: %d free, %d proc\n",
-		 list_size(&dev->free_buff_list),
-		 list_size(&dev->rec_buff_list));
+	print_buffer_status();
 	mutex_unlock(&dev->io_mutex);
 	return ret;
 }
@@ -400,11 +403,7 @@ static ssize_t hdpvr_read(struct file *file, char __user *buffer, size_t count,
 			mutex_unlock(&dev->io_mutex);
 			goto err;
 		}
-
-		v4l2_dbg(MSG_BUFFER, hdpvr_debug, dev->video_dev,
-			 "buffer queue stat: %d free, %d proc\n",
-			 list_size(&dev->free_buff_list),
-			 list_size(&dev->rec_buff_list));
+		print_buffer_status();
 	}
 	mutex_unlock(&dev->io_mutex);
 
@@ -464,10 +463,7 @@ static ssize_t hdpvr_read(struct file *file, char __user *buffer, size_t count,
 
 			list_move_tail(&buf->buff_list, &dev->free_buff_list);
 
-			v4l2_dbg(MSG_BUFFER, hdpvr_debug, dev->video_dev,
-				 "buffer queue stat: %d free, %d proc\n",
-				 list_size(&dev->free_buff_list),
-				 list_size(&dev->rec_buff_list));
+			print_buffer_status();
 
 			mutex_unlock(&dev->io_mutex);
 
@@ -500,10 +496,7 @@ static unsigned int hdpvr_poll(struct file *filp, poll_table *wait)
 			dev->status = STATUS_IDLE;
 		}
 
-		v4l2_dbg(MSG_BUFFER, hdpvr_debug, dev->video_dev,
-			 "buffer queue stat: %d free, %d proc\n",
-			 list_size(&dev->free_buff_list),
-			 list_size(&dev->rec_buff_list));
+		print_buffer_status();
 	}
 	mutex_unlock(&dev->io_mutex);
 
