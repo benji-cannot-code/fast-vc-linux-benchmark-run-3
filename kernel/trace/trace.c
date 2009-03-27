f1e6999d0f77c26aa2d214f78ad157d7bb8bb616
Frederic Weisbecker <fweisbec@gmail.com>
2009-03-27 14:22:10 +0100

    tracing/ftrace: fix missing include string.h
    Building a kernel with tracing can raise the following warning on
    tip/master:
    
    kernel/trace/trace.c:1249: error: implicit declaration of function 'vbin_printf'
    
    We are missing an include to string.h
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <1238160130-7437-1-git-send-email-fweisbec@gmail.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/percpu.h>
 #include <linux/splice.h>
 #include <linux/kdebug.h>
+#include <linux/string.h>
 #include <linux/ctype.h>
 #include <linux/init.h>
 #include <linux/poll.h>
