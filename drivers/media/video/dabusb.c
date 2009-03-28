99b31b7a1de4942bc42c102030568b812d931f87
Hans Verkuil <hverkuil@xs4all.nl>
2009-03-28 09:38:29 -0300

    V4L/DVB (11256): dabusb: fix compile warning.
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/dabusb.c
+++ drivers/media/video/dabusb.c
@@ -676,7 +676,6 @@ static int dabusb_release (struct inode *inode, struct file *file)
 
 static long dabusb_ioctl (struct file *file, unsigned int cmd, unsigned long arg)
 {
-	lock_kernel();
 	pdabusb_t s = (pdabusb_t) file->private_data;
 	pbulk_transfer_t pbulk;
 	int ret = 0;
@@ -684,6 +683,7 @@ static long dabusb_ioctl (struct file *file, unsigned int cmd, unsigned long arg
 
 	dbg("dabusb_ioctl");
 
+	lock_kernel();
 	if (s->remove_pending) {
 		unlock_kernel();
 		return -EIO;
