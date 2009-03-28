55e443440422b371c2015cd4bd480eb5fdc2a999
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2009-03-28 18:18:51 +0300

    [ARM] pxa/sharpsl_pm: merge the two sharpsl_pm.c since it's now pxa specific
    collie_pm was the only non-PXA user of sharpsl_pm. Now as it's gone we
    can merge code into one single file to allow further cleanup.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/sharpsl.h
+++ arch/arm/mach-pxa/sharpsl.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#include <asm/hardware/sharpsl_pm.h>
+#include <mach/sharpsl_pm.h>
 
 /*
  * SharpSL SSP Driver
