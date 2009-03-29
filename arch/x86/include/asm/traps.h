1554af474f2729b7392ddd5e0d42b9c922262573
Ingo Molnar <mingo@elte.hu>
2009-03-29 23:47:48 +0200

    x86/mm: further cleanups of fault.c's include file section
    Impact: cleanup
    
    Eliminate more than 20 unnecessary #include lines in fault.c
    
    Also fix include file dependency bug in asm/traps.h. (this was
    masked before, by implicit inclusion)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    LKML-Reference: <tip-56aea8468746e673a4bf50b6a13d97b2d1cbe1e8@git.kernel.org>
    Acked-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/traps.h
+++ arch/x86/include/asm/traps.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X86_TRAPS_H
 
 #include <asm/debugreg.h>
+#include <asm/siginfo.h>			/* TRAP_TRACE, ... */
 
 #ifdef CONFIG_X86_32
 #define dotraplinkage
