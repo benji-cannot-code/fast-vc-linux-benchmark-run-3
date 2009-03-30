835de06e755610f8d1b36eed032d99c5411c296a
Mauro Carvalho Chehab <mchehab@redhat.com>
2009-03-30 08:18:10 -0300

    V4L/DVB (11308): msp3400: use the V4L2 header since no V4L1 code is there
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/msp3400-driver.c
+++ drivers/media/video/msp3400-driver.c
@@ -54,7 +54,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/kthread.h>
 #include <linux/freezer.h>
-#include <linux/videodev.h>
+#include <linux/videodev2.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-i2c-drv-legacy.h>
