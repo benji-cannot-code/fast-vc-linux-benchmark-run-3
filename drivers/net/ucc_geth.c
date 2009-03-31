65226dc86d0013e76951d8158294fe8f5e2569e3
Kumar Gala <galak@kernel.crashing.org>
2009-03-31 03:42:59 +0000

    ucc_geth: Remove unused header
    Now that the driver is exclusively an of_platform driver we no longer
    use the structs and #defines in fsl_devices.h
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ucc_geth.c
+++ drivers/net/ucc_geth.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/mm.h>
 #include <linux/dma-mapping.h>
-#include <linux/fsl_devices.h>
 #include <linux/mii.h>
 #include <linux/phy.h>
 #include <linux/workqueue.h>
