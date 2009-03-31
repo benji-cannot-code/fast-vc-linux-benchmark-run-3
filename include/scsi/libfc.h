33852123a752b365070f4198e21aff8f20370ccd
Robert Love <robert.w.love@intel.com>
2009-03-31 15:51:50 -0700

    [SCSI] fcoe: Use per-CPU kernel function for dev_stats instead of an array
    Remove the hotplug creation of dev_stats, we allocate for all possible CPUs
    now when we allocate the lport.
    
    v2: Durring the 2.6.30 merge window, before these patches were comitted,
    'percpu_ptr' was renamed 'per_cpu_ptr'. This latest update updates this
    patch for the name change.
    
    Signed-off-by: Yi Zou <yi.zou@intel.com>
    Signed-off-by: Robert Love <robert.w.love@intel.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- include/scsi/libfc.h
+++ include/scsi/libfc.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/timer.h>
 #include <linux/if.h>
+#include <linux/percpu.h>
 
 #include <scsi/scsi_transport.h>
 #include <scsi/scsi_transport_fc.h>
@@ -662,7 +663,8 @@ struct fc_lport {
 	unsigned long		boot_time;
 
 	struct fc_host_statistics host_stats;
-	struct fcoe_dev_stats	*dev_stats[NR_CPUS];
+	struct fcoe_dev_stats	*dev_stats;
+
 	u64			wwpn;
 	u64			wwnn;
 	u8			retry_count;
@@ -721,6 +723,25 @@ static inline void fc_lport_state_enter(struct fc_lport *lp,
 	if (state != lp->state)
 		lp->retry_count = 0;
 	lp->state = state;
+}
+
+static inline int fc_lport_init_stats(struct fc_lport *lp)
+{
+	/* allocate per cpu stats block */
+	lp->dev_stats = alloc_percpu(struct fcoe_dev_stats);
+	if (!lp->dev_stats)
+		return -ENOMEM;
+	return 0;
+}
+
+static inline void fc_lport_free_stats(struct fc_lport *lp)
+{
+	free_percpu(lp->dev_stats);
+}
+
+static inline struct fcoe_dev_stats *fc_lport_get_stats(struct fc_lport *lp)
+{
+	return per_cpu_ptr(lp->dev_stats, smp_processor_id());
 }
 
 
