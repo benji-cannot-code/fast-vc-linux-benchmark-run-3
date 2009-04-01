13a701b36e4855c425af0ddf940f9e48ae03d8c4
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-04-01 21:42:26 +0200

    remove <linux/ata.h> include from <linux/hdreg.h>
    All <linux/hdreg.h> users that need <linux/ata.h> have been fixed
    to include it directly.
    
    Cc: Christoph Hellwig <hch@infradead.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- include/linux/hdreg.h
+++ include/linux/hdreg.h
@@ -2,10 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_HDREG_H
 #define _LINUX_HDREG_H
 
-#ifdef __KERNEL__
-#include <linux/ata.h>
-#endif /* __KERNEL__ */
-
 #include <linux/types.h>
 
 /*
