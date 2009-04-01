6a8b96c442ad6511f96e3e9edfcd1dc482de26b5
Alexander Beregalov <a.beregalov@gmail.com>
2009-04-01 15:43:40 +0000

    parisc: asm/pdc.h should include asm/page.h
    Fixes this build error:
    arch/parisc/kernel/pdc_cons.c:117: error: '__PAGE_OFFSET' undeclared
    
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Signed-off-by: Kyle McMartin <kyle@mcmartin.ca>

--- arch/parisc/include/asm/pdc.h
+++ arch/parisc/include/asm/pdc.h
@@ -344,6 +344,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __KERNEL__
 
+#include <asm/page.h> /* for __PAGE_OFFSET */
+
 extern int pdc_type;
 
 /* Values for pdc_type */
