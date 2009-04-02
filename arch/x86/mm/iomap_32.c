8cfbc94009987ad0b5a60682217e5ddfb2942178
Andrew Morton <akpm@linux-foundation.org>
2009-04-02 16:44:38 -0700

    x86: fix is_io_mapping_possible() build warning on i386 allnoconfig
    i386 allnoconfig:
    
     arch/x86/mm/iomap_32.c: In function 'is_io_mapping_possible':
     arch/x86/mm/iomap_32.c:27: warning: comparison is always false due to limited range of data type
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/iomap_32.c
+++ arch/x86/mm/iomap_32.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int is_io_mapping_possible(resource_size_t base, unsigned long size)
 {
-#ifndef CONFIG_X86_PAE
+#if !defined(CONFIG_X86_PAE) && defined(CONFIG_PHYS_ADDR_T_64BIT)
 	/* There is no way to map greater than 1 << 32 address without PAE */
 	if (base + size > 0x100000000ULL)
 		return 0;
