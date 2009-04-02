4fd99878b63269575e4c7020ac154b035620085d
Jean Delvare <khali@linux-fr.org>
2009-04-02 11:52:24 +0200

    DRM: drm_crtc_helper.h doesn't actually need i2c.h
    Remove an include that isn't actually needed to prevent needless
    rebuilds.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/drm/drm_crtc_helper.h
+++ include/drm/drm_crtc_helper.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DRM_CRTC_HELPER_H__
 #define __DRM_CRTC_HELPER_H__
 
-#include <linux/i2c.h>
 #include <linux/spinlock.h>
 #include <linux/types.h>
 #include <linux/idr.h>
