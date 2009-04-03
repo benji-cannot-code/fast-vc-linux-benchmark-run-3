a065ca8d4e145533d05429659c4d3264d58828e2
Eric W. Biederman <ebiederm@xmission.com>
2009-04-03 05:29:59 -0700

    sysctl powerpc: Remove dead binary sysctl support
    Now that sys_sysctl is a generic wrapper around /proc/sys  .ctl_name
    and .strategy members of sysctl tables are dead code.  Remove them.
    
    Cc: Paul Mackerras <paulus@samba.org>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- arch/powerpc/kernel/idle.c
+++ arch/powerpc/kernel/idle.c
@@ -111,7 +111,6 @@ int powersave_nap;
  */
 static ctl_table powersave_nap_ctl_table[]={
 	{
-		.ctl_name	= KERN_PPC_POWERSAVE_NAP,
 		.procname	= "powersave-nap",
 		.data		= &powersave_nap,
 		.maxlen		= sizeof(int),
@@ -122,7 +121,6 @@ static ctl_table powersave_nap_ctl_table[]={
 };
 static ctl_table powersave_nap_sysctl_root[] = {
 	{
-		.ctl_name	= CTL_KERN,
 		.procname	= "kernel",
 		.mode		= 0555,
 		.child		= powersave_nap_ctl_table,
