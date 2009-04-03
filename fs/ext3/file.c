8f2dfb152dbbe736cd203149374d07cb184a29b0
Theodore Ts'o <tytso@mit.edu>
2009-04-03 01:34:35 -0400

    ext3: Add replace-on-truncate hueristics for data=writeback mode
    In data=writeback mode, start an asynchronous flush when closing a
    file which had been previously truncated down to zero.  This lowers
    the probability of data loss in the case of applications that attempt
    to replace a file using truncate.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext3/file.c
+++ fs/ext3/file.c
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 static int ext3_release_file (struct inode * inode, struct file * filp)
 {
+	if (EXT3_I(inode)->i_state & EXT3_STATE_FLUSH_ON_CLOSE) {
+		filemap_flush(inode->i_mapping);
+		EXT3_I(inode)->i_state &= ~EXT3_STATE_FLUSH_ON_CLOSE;
+	}
 	/* if we are the last writer on the inode, drop the block reservation */
 	if ((filp->f_mode & FMODE_WRITE) &&
 			(atomic_read(&inode->i_writecount) == 1))
