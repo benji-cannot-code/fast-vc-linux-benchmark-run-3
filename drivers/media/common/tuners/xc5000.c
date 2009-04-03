dd174fb7031c3c7e65df1b45dcb27de0fab29599
Devin Heitmueller <dheitmueller@kernellabs.com>
2009-04-02 22:45:17 -0300

    V4L/DVB (11797): xc5000: cleanup firmware loading messages
    Make it a little more obvious in the dmesg output what is going on during
    firmware upload.  This is more important for boards like the HVR-950q that
    take nearly seven seconds to do the upload.
    
    Signed-off-by: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/tuners/xc5000.c
+++ drivers/media/common/tuners/xc5000.c
@@ -556,7 +556,7 @@ static int xc5000_fwupload(struct dvb_frontend *fe)
 		ret = XC_RESULT_RESET_FAILURE;
 		goto out;
 	} else {
-		printk(KERN_INFO "xc5000: firmware read %Zu bytes.\n",
+		printk(KERN_DEBUG "xc5000: firmware read %Zu bytes.\n",
 		       fw->size);
 		ret = XC_RESULT_SUCCESS;
 	}
@@ -565,8 +565,9 @@ static int xc5000_fwupload(struct dvb_frontend *fe)
 		printk(KERN_ERR "xc5000: firmware incorrect size\n");
 		ret = XC_RESULT_RESET_FAILURE;
 	} else {
-		printk(KERN_INFO "xc5000: firmware upload\n");
+		printk(KERN_INFO "xc5000: firmware uploading...\n");
 		ret = xc_load_i2c_sequence(fe,  fw->data);
+		printk(KERN_INFO "xc5000: firmware upload complete...\n");
 	}
 
 out:
