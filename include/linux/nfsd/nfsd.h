3478711bdb7ac94b787b982886c0e49765914569
Marc Eshel <eshel@almaden.ibm.com>
2009-04-03 08:29:02 +0300

    nfsd41: provide support for minor version 1 at rpc level
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- include/linux/nfsd/nfsd.h
+++ include/linux/nfsd/nfsd.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * nfsd version
  */
-#define NFSD_SUPPORTED_MINOR_VERSION	0
+#define NFSD_SUPPORTED_MINOR_VERSION	1
 
 /*
  * Flags for nfsd_permission
