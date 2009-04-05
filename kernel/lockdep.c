2d57f052069050374cefc01d2f1c1fb3c2bc624f
Frederic Weisbecker <fweisbec@gmail.com>
2009-04-06 01:49:33 +0200

    tracing/lockdep: report the time waited for a lock
    While trying to optimize the new lock on reiserfs to replace
    the bkl, I find the lock tracing very useful though it lacks
    something important for performance (and latency) instrumentation:
    the time a task waits for a lock.
    
    That's what this patch implements:
    
      bash-4816  [000]   202.652815: lock_contended: lock_contended: &sb->s_type->i_mutex_key
      bash-4816  [000]   202.652819: lock_acquired: &rq->lock (0.000 us)
     <...>-4787  [000]   202.652825: lock_acquired: &rq->lock (0.000 us)
     <...>-4787  [000]   202.652829: lock_acquired: &rq->lock (0.000 us)
      bash-4816  [000]   202.652833: lock_acquired: &sb->s_type->i_mutex_key (16.005 us)
    
    As shown above, the "lock acquired" field is followed by the time
    it has been waiting for the lock. Usually, a lock contended entry
    is followed by a near lock_acquired entry with a non-zero time waited.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    LKML-Reference: <1238975373-15739-1-git-send-email-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -3062,6 +3062,8 @@ found_it:
 	put_lock_stats(stats);
 }
 
+DEFINE_TRACE(lock_acquired);
+
 static void
 __lock_acquired(struct lockdep_map *lock, unsigned long ip)
 {
@@ -3099,6 +3101,8 @@ found_it:
 		waittime = now - hlock->waittime_stamp;
 		hlock->holdtime_stamp = now;
 	}
+
+	trace_lock_acquired(lock, ip, waittime);
 
 	stats = get_lock_stats(hlock_class(hlock));
 	if (waittime) {
@@ -3138,13 +3142,9 @@ void lock_contended(struct lockdep_map *lock, unsigned long ip)
 }
 EXPORT_SYMBOL_GPL(lock_contended);
 
-DEFINE_TRACE(lock_acquired);
-
 void lock_acquired(struct lockdep_map *lock, unsigned long ip)
 {
 	unsigned long flags;
-
-	trace_lock_acquired(lock, ip);
 
 	if (unlikely(!lock_stat))
 		return;
