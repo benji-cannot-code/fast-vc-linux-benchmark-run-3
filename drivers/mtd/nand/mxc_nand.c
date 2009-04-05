70c5f22a466c6df7e58143e8e1eb8ef8a2c6308b
David Woodhouse <David.Woodhouse@intel.com>
2009-04-05 07:49:48 -0700

    [MTD] Remove mtd->{suspend,resume} calls from board drivers
    Now the MTD core will do this for us, we don't need to hook it up from
    the board drivers.
    
    Shame we can't do shutdown from the class too...
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/nand/mxc_nand.c
+++ drivers/mtd/nand/mxc_nand.c
@@ -1016,8 +1016,6 @@ static int mxcnd_suspend(struct platform_device *pdev, pm_message_t state)
 	int ret = 0;
 
 	DEBUG(MTD_DEBUG_LEVEL0, "MXC_ND : NAND suspend\n");
-	if (info)
-		ret = info->suspend(info);
 
 	/* Disable the NFC clock */
 	clk_disable(nfc_clk);	/* FIXME */
@@ -1033,9 +1031,6 @@ static int mxcnd_resume(struct platform_device *pdev)
 	DEBUG(MTD_DEBUG_LEVEL0, "MXC_ND : NAND resume\n");
 	/* Enable the NFC clock */
 	clk_enable(nfc_clk);	/* FIXME */
-
-	if (info)
-		info->resume(info);
 
 	return ret;
 }
