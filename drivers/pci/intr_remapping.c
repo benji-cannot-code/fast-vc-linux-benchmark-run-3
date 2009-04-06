301c5cb5e19e1d75e5fb4ff069ffed405f80a4de
Alexander Beregalov <a.beregalov@gmail.com>
2009-04-06 16:45:28 +0100

    drivers/pci/intr_remapping.c: include acpi.h
    Fix this build error:
    drivers/pci/intr_remapping.c: In function 'ir_parse_ioapic_scope':
    drivers/pci/intr_remapping.c:617: error: invalid use of undefined type
    				         'struct acpi_dmar_hardware_unit'
    
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/pci/intr_remapping.c
+++ drivers/pci/intr_remapping.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpu.h>
 #include <linux/intel-iommu.h>
 #include "intr_remapping.h"
+#include <acpi/acpi.h>
 
 static struct ioapic_scope ir_ioapic[MAX_IO_APICS];
 static int ir_ioapic_num;
