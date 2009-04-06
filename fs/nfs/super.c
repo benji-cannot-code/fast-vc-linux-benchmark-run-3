eb74ed4adacb053f814ac65763bc2e473ab75c65
Trond Myklebust <Trond.Myklebust@netapp.com>
2009-04-06 16:41:35 -0700

    NFS: Fix a double free in nfs_parse_mount_options()
    Due to an apparent typo, commit a67d18f89f5782806135aad4ee012ff78d45aae7
    (NFS: load the rpc/rdma transport module automatically) lead to the
    'proto=' mount option doing a double free, while Opt_mountproto leaks a
    string.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -1229,7 +1229,6 @@ static int nfs_parse_mount_options(char *raw,
 				goto out_nomem;
 			token = match_token(string,
 					    nfs_xprt_protocol_tokens, args);
-			kfree(string);
 
 			switch (token) {
 			case Opt_xprt_udp:
@@ -1259,6 +1258,7 @@ static int nfs_parse_mount_options(char *raw,
 				goto out_nomem;
 			token = match_token(string,
 					    nfs_xprt_protocol_tokens, args);
+			kfree(string);
 
 			switch (token) {
 			case Opt_xprt_udp:
