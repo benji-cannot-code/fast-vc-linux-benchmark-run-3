34fcebe6d50111a93b34baaba99cd6308414e99e
Miroslav Sustek <sustmidown@centrum.cz>
2009-04-06 20:07:04 -0300

    V4L/DVB (11441): cx88-dsp: fixing 64bit math
    cx88-dsp: fixing 64bit math on 32bit kernels
    
    Some gcc versions report the missing of __divdi3
    
    [mchehab.redhat.com: CodingStyle fixes]
    
    Signed-off-by: Miroslav Sustek <sustmidown@centrum.cz>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx88/cx88-dsp.c
+++ drivers/media/video/cx88/cx88-dsp.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/jiffies.h>
+#include <asm/div64.h>
 
 #include "cx88.h"
 #include "cx88-reg.h"
@@ -101,13 +102,25 @@ static u32 int_goertzel(s16 x[], u32 N, u32 freq)
 	s32 s_prev2 = 0;
 	s32 coeff = 2*int_cos(freq);
 	u32 i;
+
+	u64 tmp;
+	u32 divisor;
+
 	for (i = 0; i < N; i++) {
 		s32 s = x[i] + ((s64)coeff*s_prev/32768) - s_prev2;
 		s_prev2 = s_prev;
 		s_prev = s;
 	}
-	return (u32)(((s64)s_prev2*s_prev2 + (s64)s_prev*s_prev -
-		      (s64)coeff*s_prev2*s_prev/32768)/N/N);
+
+	tmp = (s64)s_prev2 * s_prev2 + (s64)s_prev * s_prev -
+		      (s64)coeff * s_prev2 * s_prev / 32768;
+
+	/* XXX: N must be low enough so that N*N fits in s32.
+	 * Else we need two divisions. */
+	divisor = N * N;
+	do_div(tmp, divisor);
+
+	return (u32) tmp;
 }
 
 static u32 freq_magnitude(s16 x[], u32 N, u32 freq)
