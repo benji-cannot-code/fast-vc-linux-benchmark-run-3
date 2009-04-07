56a1f0aebd53f02280ed246a63b8ef56fc252ae2
Geert Uytterhoeven <geert@linux-m68k.org>
2009-04-07 23:40:39 +0200

    dlm: Make name input parameter of {,dlm_}new_lockspace() const
    | fs/gfs2/lock_dlm.c:207: warning: passing argument 1 of 'dlm_new_lockspace' discards qualifiers from pointer target type
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: David Teigland <teigland@redhat.com>

--- fs/dlm/lockspace.c
+++ fs/dlm/lockspace.c
@@ -385,7 +385,7 @@ static void threads_stop(void)
 	dlm_astd_stop();
 }
 
-static int new_lockspace(char *name, int namelen, void **lockspace,
+static int new_lockspace(const char *name, int namelen, void **lockspace,
 			 uint32_t flags, int lvblen)
 {
 	struct dlm_ls *ls;
@@ -615,7 +615,7 @@ static int new_lockspace(char *name, int namelen, void **lockspace,
 	return error;
 }
 
-int dlm_new_lockspace(char *name, int namelen, void **lockspace,
+int dlm_new_lockspace(const char *name, int namelen, void **lockspace,
 		      uint32_t flags, int lvblen)
 {
 	int error = 0;
