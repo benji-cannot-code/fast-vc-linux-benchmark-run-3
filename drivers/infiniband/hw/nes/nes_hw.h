6e2372a8544ae8531c123a37df0b58fe4e9aad50
Chien Tung <chien.tin.tung@intel.com>
2009-04-08 14:27:18 -0700

    RDMA/nes: Add wide_ppm_offset parm for switch compatibility
    We have observed unstable link with a new BNT switch.
    
    Add wide_ppm_offset parameter to allow the user to control the clock
    ppm offset on the CX4 interface for better compatibility.  Default is
    100ppm, setting it to 1 will increase it to 300ppm.  Change default
    SerDes1 reference clock to external source.
    
    Signed-off-by: Chien Tung <chien.tin.tung@intel.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/nes/nes_hw.h
+++ drivers/infiniband/hw/nes/nes_hw.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/inet_lro.h>
 
+#define NES_PHY_TYPE_CX4       1
 #define NES_PHY_TYPE_1G        2
 #define NES_PHY_TYPE_IRIS      3
 #define NES_PHY_TYPE_ARGUS     4
