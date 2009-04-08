049d5442cd07f98d08bc476814f68464be8625d9
Jerome Glisse <glisse@freedesktop.org>
2009-04-08 18:34:28 +0200

    drm: include kernel list header file in hashtab header
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/drm/drm_hashtab.h
+++ include/drm/drm_hashtab.h
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DRM_HASHTAB_H
 #define DRM_HASHTAB_H
 
+#include <linux/list.h>
+
 #define drm_hash_entry(_ptr, _type, _member) container_of(_ptr, _type, _member)
 
 struct drm_hash_item {
