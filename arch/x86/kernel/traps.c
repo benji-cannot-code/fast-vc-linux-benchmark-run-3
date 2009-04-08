a5e081d758b3551bc8046c95ac7d689c57253a9e
Jiri Slaby <jirislaby@gmail.com>
2009-04-08 13:31:58 +0200

    x86_32: introduce restore_fpu_checking()
    Impact: cleanup, prepare FPU code unificaton
    
    Like on x86_64, return an error from restore_fpu and kill the task
    if it fails.
    
    Also rename restore_fpu to restore_fpu_checking which allows ifdefs
    to be removed in math_state_restore().
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    LKML-Reference: <1239190320-23952-1-git-send-email-jirislaby@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -840,9 +840,6 @@ asmlinkage void math_state_restore(void)
 	}
 
 	clts();				/* Allow maths ops (or we recurse) */
-#ifdef CONFIG_X86_32
-	restore_fpu(tsk);
-#else
 	/*
 	 * Paranoid restore. send a SIGSEGV if we fail to restore the state.
 	 */
@@ -851,7 +848,7 @@ asmlinkage void math_state_restore(void)
 		force_sig(SIGSEGV, tsk);
 		return;
 	}
-#endif
+
 	thread->status |= TS_USEDFPU;	/* So we fnsave on switch_to() */
 	tsk->fpu_counter++;
 }
