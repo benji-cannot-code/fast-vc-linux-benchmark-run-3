39c2d949ba3a741e679472b06d04bc4edcc6b328
Ingo Molnar <mingo@elte.hu>
2009-04-08 10:47:17 +0200

    mm, x86, ptrace, bts: defer branch trace stopping, cleanup
    Andrew Morton noticed that mm.h needlessly includes sched.h - remove it.
    
    Reported-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/prio_tree.h>
 #include <linux/debug_locks.h>
 #include <linux/mm_types.h>
-#include <linux/sched.h>
 
 struct mempolicy;
 struct anon_vma;
