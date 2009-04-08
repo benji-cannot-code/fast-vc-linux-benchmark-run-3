f55b366d0441ee9167cfa99a7bf7b85ee25bd60e
Peter Zijlstra <a.p.zijlstra@chello.nl>
2009-04-08 15:01:32 +0200

    perf_counter: move PERF_RECORD_TIME
    Move PERF_RECORD_TIME so that all the fixed length items come before
    the variable length ones.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    LKML-Reference: <20090408130409.307926436@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/perf_counter.c
+++ kernel/perf_counter.c
@@ -1851,6 +1851,16 @@ static void perf_counter_output(struct perf_counter *counter,
 		header.size += sizeof(tid_entry);
 	}
 
+	if (record_type & PERF_RECORD_TIME) {
+		/*
+		 * Maybe do better on x86 and provide cpu_clock_nmi()
+		 */
+		time = sched_clock();
+
+		header.type |= PERF_RECORD_TIME;
+		header.size += sizeof(u64);
+	}
+
 	if (record_type & PERF_RECORD_GROUP) {
 		header.type |= PERF_RECORD_GROUP;
 		header.size += sizeof(u64) +
@@ -1868,16 +1878,6 @@ static void perf_counter_output(struct perf_counter *counter,
 		}
 	}
 
-	if (record_type & PERF_RECORD_TIME) {
-		/*
-		 * Maybe do better on x86 and provide cpu_clock_nmi()
-		 */
-		time = sched_clock();
-
-		header.type |= PERF_RECORD_TIME;
-		header.size += sizeof(u64);
-	}
-
 	ret = perf_output_begin(&handle, counter, header.size, nmi, 1);
 	if (ret)
 		return;
@@ -1889,6 +1889,9 @@ static void perf_counter_output(struct perf_counter *counter,
 
 	if (record_type & PERF_RECORD_TID)
 		perf_output_put(&handle, tid_entry);
+
+	if (record_type & PERF_RECORD_TIME)
+		perf_output_put(&handle, time);
 
 	if (record_type & PERF_RECORD_GROUP) {
 		struct perf_counter *leader, *sub;
@@ -1910,9 +1913,6 @@ static void perf_counter_output(struct perf_counter *counter,
 
 	if (callchain)
 		perf_output_copy(&handle, callchain, callchain_size);
-
-	if (record_type & PERF_RECORD_TIME)
-		perf_output_put(&handle, time);
 
 	perf_output_end(&handle);
 }
