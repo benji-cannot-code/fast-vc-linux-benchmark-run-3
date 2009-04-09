cd4ac936b076951adfbfc9fce15d1dee4cfc6151
Zhaolei <zhaolei@cn.fujitsu.com>
2009-04-09 14:08:18 +0800

    tracing, net, skb tracepoint: make skb tracepoint use the TRACE_EVENT() macro
    TRACE_EVENT is a more generic way to define a tracepoint.
    Doing so adds these new capabilities to this tracepoint:
    
      - zero-copy and per-cpu splice() tracing
      - binary tracing without printf overhead
      - structured logging records exposed under /debug/tracing/events
      - trace events embedded in function tracer output and other plugins
      - user-defined, per tracepoint filter expressions
    
    Signed-off-by: Zhao Lei <zhaolei@cn.fujitsu.com>
    Acked-by: Neil Horman <nhorman@tuxdriver.com>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: "Steven Rostedt ;" <rostedt@goodmis.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Tom Zanussi <tzanussi@gmail.com>
    LKML-Reference: <49DD90D2.5020604@cn.fujitsu.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/trace/trace_event_types.h
+++ include/trace/trace_event_types.h
@@ -4,3 +4,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <trace/sched_event_types.h>
 #include <trace/irq_event_types.h>
 #include <trace/lockdep_event_types.h>
+#include <trace/skb_event_types.h>
