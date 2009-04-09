f7d51b83143bc3d6ef22aad296657de59d744e6f
Pekka Enberg <penberg@cs.helsinki.fi>
2009-04-09 11:52:29 +0300

    x86: define IA32_SYSCALL_VECTOR on 32-bit to reduce ifdefs
    Impact: cleanup
    
    We can remove some #ifdefs if we define IA32_SYSCALL_VECTOR on 32-bit.
    
    Reviewed-by Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/irq_vectors.h
+++ arch/x86/include/asm/irq_vectors.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_X86_32
 # define SYSCALL_VECTOR			0x80
+# define IA32_SYSCALL_VECTOR		0x80
 #else
 # define IA32_SYSCALL_VECTOR		0x80
 #endif
