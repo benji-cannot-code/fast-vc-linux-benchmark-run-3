91abc5cf729d098ba5adbd5ccbaeee1a4709a04d
Larry Finger <Larry.Finger@lwfinger.net>
2009-04-09 22:14:19 -0500

    rfkill: Fix broken rfkill LED in 2.6.30-rc1
    The rfkill system fails to issue a LED trigger event when the rfkill state
    changes.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/rfkill/rfkill.c
+++ net/rfkill/rfkill.c
@@ -97,6 +97,7 @@ static void update_rfkill_state(struct rfkill *rfkill)
 		}
 		mutex_unlock(&rfkill->mutex);
 	}
+	rfkill_led_trigger(rfkill, rfkill->state);
 }
 
 /**
@@ -137,8 +138,9 @@ static int rfkill_toggle_radio(struct rfkill *rfkill,
 	oldstate = rfkill->state;
 
 	if (rfkill->get_state && !force &&
-	    !rfkill->get_state(rfkill->data, &newstate))
+	    !rfkill->get_state(rfkill->data, &newstate)) {
 		rfkill->state = newstate;
+	}
 
 	switch (state) {
 	case RFKILL_STATE_HARD_BLOCKED:
@@ -173,6 +175,7 @@ static int rfkill_toggle_radio(struct rfkill *rfkill,
 	if (force || rfkill->state != oldstate)
 		rfkill_uevent(rfkill);
 
+	rfkill_led_trigger(rfkill, rfkill->state);
 	return retval;
 }
 
@@ -205,6 +208,7 @@ static void __rfkill_switch_all(const enum rfkill_type type,
 			mutex_lock(&rfkill->mutex);
 			rfkill_toggle_radio(rfkill, state, 0);
 			mutex_unlock(&rfkill->mutex);
+			rfkill_led_trigger(rfkill, rfkill->state);
 		}
 	}
 }
@@ -257,6 +261,7 @@ void rfkill_epo(void)
 				RFKILL_STATE_SOFT_BLOCKED;
 	}
 	mutex_unlock(&rfkill_global_mutex);
+	rfkill_led_trigger(rfkill, rfkill->state);
 }
 EXPORT_SYMBOL_GPL(rfkill_epo);
 
@@ -359,6 +364,7 @@ int rfkill_force_state(struct rfkill *rfkill, enum rfkill_state state)
 		rfkill_uevent(rfkill);
 
 	mutex_unlock(&rfkill->mutex);
+	rfkill_led_trigger(rfkill, rfkill->state);
 
 	return 0;
 }
@@ -521,6 +527,7 @@ static int rfkill_resume(struct device *dev)
 				1);
 
 		mutex_unlock(&rfkill->mutex);
+		rfkill_led_trigger(rfkill, rfkill->state);
 	}
 
 	return 0;
