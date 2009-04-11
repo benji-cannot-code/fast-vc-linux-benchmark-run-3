88f7e44e7b35eead824fd3f0092b339531482a42
Larry Finger <Larry.Finger@lwfinger.net>
2009-04-11 11:25:24 -0500

    b43legacy: Fixes for beaconing
    This patch ports the beaconing fixes from commit a82d992261f "b43: Beaconing
    fixes" to b43legacy. Basically it prevents the card from triggering the beacon
    IRQ over and over again.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Acked-by: Michael Buesch <mb@bu3sch.de>
    Tested-by: David Ellingsworth <david@identd.dyndns.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43legacy/b43legacy.h
+++ drivers/net/wireless/b43legacy/b43legacy.h
@@ -60,7 +60,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define B43legacy_MMIO_XMITSTAT_1		0x174
 #define B43legacy_MMIO_REV3PLUS_TSF_LOW	0x180 /* core rev >= 3 only */
 #define B43legacy_MMIO_REV3PLUS_TSF_HIGH	0x184 /* core rev >= 3 only */
-
+#define B43legacy_MMIO_TSF_CFP_REP	0x188
+#define B43legacy_MMIO_TSF_CFP_START	0x18C
 /* 32-bit DMA */
 #define B43legacy_MMIO_DMA32_BASE0	0x200
 #define B43legacy_MMIO_DMA32_BASE1	0x220
@@ -617,6 +618,7 @@ struct b43legacy_wl {
 	struct sk_buff *current_beacon;
 	bool beacon0_uploaded;
 	bool beacon1_uploaded;
+	struct work_struct beacon_update_trigger;
 };
 
 /* Pointers to the firmware data and meta information about it. */
