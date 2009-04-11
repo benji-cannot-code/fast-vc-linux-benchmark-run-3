7fe37e56f045713570d294935afd4efd659f350e
Alexander Beregalov <a.beregalov@gmail.com>
2009-04-11 14:50:23 +0000

    drivers/net: replace BUG() with BUG_ON() if possible
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ixgb/ixgb_osdep.h
+++ drivers/net/ixgb/ixgb_osdep.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 
 #undef ASSERT
-#define ASSERT(x)	if (!(x)) BUG()
+#define ASSERT(x)	BUG_ON(!(x))
 #define MSGOUT(S, A, B)	printk(KERN_DEBUG S "\n", A, B)
 
 #ifdef DBG
