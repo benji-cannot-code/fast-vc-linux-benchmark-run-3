a86c5dcfa7e8abced0a21453e90af0a63dbae7a9
Jaswinder Singh Rajput <jaswinder@kernel.org>
2009-04-12 20:37:25 +0530

    x86: vdso/vma.c declare vdso_enabled and arch_setup_additional_pages before they get used
    Impact: cleanup, address sparse warnings
    
    Addresses the problem pointed out by these sparse warning:
      arch/x86/vdso/vma.c:19:28: warning: symbol 'vdso_enabled' was not declared. Should it be static?
      arch/x86/vdso/vma.c:101:5: warning: symbol 'arch_setup_additional_pages' was not declared. Should it be static?
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
    LKML-Reference: <1239548845.4170.2.camel@localhost.localdomain>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/vdso/vma.c
+++ arch/x86/vdso/vma.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/init.h>
 #include <linux/random.h>
+#include <linux/elf.h>
 #include <asm/vsyscall.h>
 #include <asm/vgtod.h>
 #include <asm/proto.h>
