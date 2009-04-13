23af42ef81266119fb87895bd1eebcc101886c12
Steven Rostedt <srostedt@redhat.com>
2009-04-13 11:20:49 -0400

    tracing/events: move declarations from trace directory to core include
    In preparation to allowing trace events to happen in modules, we need
    to move some of the local declarations in the kernel/trace directory
    into include/linux.
    
    This patch simply moves the declarations and performs no context changes.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/trace_output.h
+++ kernel/trace/trace_output.h
@@ -5,18 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/trace_seq.h>
 #include "trace.h"
 
-typedef enum print_line_t (*trace_print_func)(struct trace_iterator *iter,
-					      int flags);
-
-struct trace_event {
-	struct hlist_node	node;
-	int			type;
-	trace_print_func	trace;
-	trace_print_func	raw;
-	trace_print_func	hex;
-	trace_print_func	binary;
-};
-
 extern enum print_line_t
 trace_print_bprintk_msg_only(struct trace_iterator *iter);
 extern enum print_line_t
@@ -34,8 +22,6 @@ extern int trace_print_context(struct trace_iterator *iter);
 extern int trace_print_lat_context(struct trace_iterator *iter);
 
 extern struct trace_event *ftrace_find_event(int type);
-extern int register_ftrace_event(struct trace_event *event);
-extern int unregister_ftrace_event(struct trace_event *event);
 
 extern enum print_line_t trace_nop_print(struct trace_iterator *iter,
 					 int flags);
