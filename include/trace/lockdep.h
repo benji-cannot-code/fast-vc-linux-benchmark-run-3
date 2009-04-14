c32ccecac2de73a3691d5c757c002e6c6c6234fb
Steven Rostedt <srostedt@redhat.com>
2009-04-14 18:49:38 -0400

    tracing/events: fix lockdep system name
    Impact: fix compile error of lockdep event tracer
    
    Ingo Molnar pointed out that the system name for the lockdep tracer was "lock"
    which is used to include the event trace file name. It should be "lockdep"
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/trace/lockdep.h
+++ include/trace/lockdep.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tracepoint.h>
 
 #undef TRACE_SYSTEM
-#define TRACE_SYSTEM lock
+#define TRACE_SYSTEM lockdep
 
 #ifdef CONFIG_LOCKDEP
 
