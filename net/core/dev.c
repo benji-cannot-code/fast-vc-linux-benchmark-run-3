85a4b8c51001f7321b833032a30c25284fbbf44e
Herbert Xu <herbert@gondor.apana.org.au>
2009-04-14 15:11:06 -0700

    gro: Restore correct value to gso_size
    Since everybody has been focusing on baremetal GRO performance
    no one noticed when I added a bug that zapped gso_size for all
    GRO packets.  This only gets picked up when you forward the skb
    out of an interface.
    
    Thanks to Mark Wagner for noticing this bug when testing kvm.
    
    Reported-by: Mark Wagner <mwagner@redhat.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2329,8 +2329,10 @@ static int napi_gro_complete(struct sk_buff *skb)
 	struct list_head *head = &ptype_base[ntohs(type) & PTYPE_HASH_MASK];
 	int err = -ENOENT;
 
-	if (NAPI_GRO_CB(skb)->count == 1)
+	if (NAPI_GRO_CB(skb)->count == 1) {
+		skb_shinfo(skb)->gso_size = 0;
 		goto out;
+	}
 
 	rcu_read_lock();
 	list_for_each_entry_rcu(ptype, head, list) {
@@ -2349,7 +2351,6 @@ static int napi_gro_complete(struct sk_buff *skb)
 	}
 
 out:
-	skb_shinfo(skb)->gso_size = 0;
 	return netif_receive_skb(skb);
 }
 
