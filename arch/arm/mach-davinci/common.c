3cee49452d9a09cd4dbba4087fe091528420d70c
Mark A. Greer <mgreer@mvista.com>
2009-04-15 12:41:40 -0700

    davinci: Integrate cp_intc support into low-level irq code
    Integrate the Common Platform Interrupt Controller (cp_intc)
    support into the low-level irq handling for davinci and similar
    platforms.  Do it such that support for cp_intc and the original
    aintc can coexist in the same kernel binary.
    
    Signed-off-by: Mark A. Greer <mgreer@mvista.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/common.c
+++ arch/arm/mach-davinci/common.c
@@ -26,6 +26,7 @@ struct davinci_soc_info davinci_soc_info;
 EXPORT_SYMBOL(davinci_soc_info);
 
 void __iomem *davinci_intc_base;
+int davinci_intc_type;
 
 void davinci_get_mac_addr(struct memory_accessor *mem_acc, void *context)
 {
@@ -100,6 +101,7 @@ void __init davinci_common_init(struct davinci_soc_info *soc_info)
 	}
 
 	davinci_intc_base = davinci_soc_info.intc_base;
+	davinci_intc_type = davinci_soc_info.intc_type;
 	return;
 
 err:
