cca15c0507625e4bafc7692f394abeae88937753
Michal Simek <monstr@monstr.eu>
2009-04-16 11:05:26 +0200

    microblaze: Move task_pt_regs up
    This change is important for easier merge with Microblaze MMU code.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/processor.h
+++ arch/microblaze/include/asm/processor.h
@@ -27,6 +27,9 @@ extern const struct seq_operations cpuinfo_op;
 
 # endif /* __ASSEMBLY__ */
 
+#define task_pt_regs(tsk) \
+		(((struct pt_regs *)(THREAD_SIZE + task_stack_page(tsk))) - 1)
+
 /*
  * User space process size: memory size
  *
@@ -84,9 +87,6 @@ extern unsigned long get_wchan(struct task_struct *p);
  * create a kernel thread without removing it from tasklists
  */
 extern int kernel_thread(int (*fn)(void *), void *arg, unsigned long flags);
-
-# define task_pt_regs(tsk) \
-		(((struct pt_regs *)(THREAD_SIZE + task_stack_page(tsk))) - 1)
 
 # define KSTK_EIP(tsk)	(0)
 # define KSTK_ESP(tsk)	(0)
