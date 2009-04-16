58c506030f04a73712286e74168f4fa84160a0ac
Ben Dooks <ben-linux@fluff.org>
2009-04-16 10:32:21 +0100

    ASoC: Fix include build error in s3c2412-i2s.c
    Fix accidental change of <mach/regs-gpio.h> to
    <plat/regs-gpio.h> in s3c2412-i2s.c
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/s3c2412-i2s.c
+++ sound/soc/s3c24xx/s3c2412-i2s.c
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat/regs-s3c2412-iis.h>
 
-#include <plat/regs-gpio.h>
 #include <plat/audio.h>
+#include <mach/regs-gpio.h>
 #include <mach/dma.h>
 
 #include "s3c24xx-pcm.h"
