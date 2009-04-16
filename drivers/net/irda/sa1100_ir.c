8da7844598112cfd32d3010722931ddf53c60db7
Alexander Beregalov <a.beregalov@gmail.com>
2009-04-16 15:23:03 +0000

    irda: include etherdevice.h for eth_*() functions
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/irda/sa1100_ir.c
+++ drivers/net/irda/sa1100_ir.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/errno.h>
 #include <linux/netdevice.h>
+#include <linux/etherdevice.h>
 #include <linux/slab.h>
 #include <linux/rtnetlink.h>
 #include <linux/interrupt.h>
