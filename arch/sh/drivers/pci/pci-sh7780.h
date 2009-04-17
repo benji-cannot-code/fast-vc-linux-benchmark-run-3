2cc55476908d48b223bb725753b5576c53f27777
Paul Mundt <lethal@linux-sh.org>
2009-04-17 15:05:19 +0900

    sh: pci: Rework SH7780 host controller detection.
    This reworks how the host controller is probed, and makes it a bit more
    verbose in the event a new type of controller is detected. Additionally,
    we also log the revision information.
    
    This now uses the proper access sizes for the vendor/device registers,
    rather than relying on a larger access that encapsulated both of them.
    Not all devices support 32-bit read cycles for these registers.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/drivers/pci/pci-sh7780.h
+++ arch/sh/drivers/pci/pci-sh7780.h
@@ -21,9 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SH7785_DEVICE_ID	0x0007
 
 /* SH7780 Control Registers */
-#define	SH7780_PCI_VCR0		0xFE000000
-#define	SH7780_PCI_VCR1		0xFE000004
-#define	SH7780_PCI_VCR2		0xFE000008
+#define	PCIECR			0xFE000008
+#define PCIECR_ENBL		0x01
 
 /* SH7780 Specific Values */
 #define SH7780_PCI_CONFIG_BASE	0xFD000000	/* Config space base addr */
