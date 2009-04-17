ef3271721f9a9b1909b4c8e38c5ee5393ba51813
Steven Rostedt <srostedt@redhat.com>
2009-04-17 13:02:22 -0400

    tracing/events: enable code with EVENT_TRACING not EVENT_TRACER
    The CONFIG_EVENT_TRACER is the way to turn on event tracing when no
    other tracing has been configured. All code to get enabled should
    depend on CONFIG_EVENT_TRACING. That is what is enabled when TRACING
    (or CONFIG_EVENT_TRACER) is selected.
    
    This patch enables the include/trace/ftrace.h file when
    CONFIG_EVENT_TRACING is enabled.
    
    [ Impact: fix warning in event tracer selftest ]
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/trace/define_trace.h
+++ include/trace/define_trace.h
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include TRACE_INCLUDE(TRACE_INCLUDE_FILE)
 
-#ifdef CONFIG_EVENT_TRACER
+#ifdef CONFIG_EVENT_TRACING
 #include <trace/ftrace.h>
 #endif
 
