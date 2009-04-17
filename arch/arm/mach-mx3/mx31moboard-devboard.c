8b776ff2028e0e1c198e08f42fe7563e6b696da5
Valentin Longchamp <valentin.longchamp@epfl.ch>
2009-04-17 15:20:25 +0200

    mx31moboard: clean-up of board support
    Various improvements (includes in alphabetical order, platform devices
    declaration order change, ...)
    
    The pins now are claimed in a single function call from a main table for
    every board for a better clarity and to adopt the current agreement for
    mx31 devices.
    
    Signed-off-by: Valentin Longchamp <valentin.longchamp@epfl.ch>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx3/mx31moboard-devboard.c
+++ arch/arm/mach-mx3/mx31moboard-devboard.c
@@ -17,25 +17,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <linux/types.h>
 #include <linux/init.h>
-
 #include <linux/platform_device.h>
+#include <linux/types.h>
 
-#include <mach/hardware.h>
 #include <mach/common.h>
 #include <mach/imx-uart.h>
 #include <mach/iomux-mx3.h>
+#include <mach/hardware.h>
 
 #include "devices.h"
 
-static struct imxuart_platform_data uart_pdata = {
-	.flags = IMXUART_HAVE_RTSCTS,
-};
-
-static int mxc_uart1_pins[] = {
+static unsigned int devboard_pins[] = {
+	/* UART1 */
 	MX31_PIN_CTS2__CTS2, MX31_PIN_RTS2__RTS2,
 	MX31_PIN_TXD2__TXD2, MX31_PIN_RXD2__RXD2,
+};
+
+static struct imxuart_platform_data uart_pdata = {
+	.flags = IMXUART_HAVE_RTSCTS,
 };
 
 /*
@@ -44,6 +44,9 @@ static int mxc_uart1_pins[] = {
 void __init mx31moboard_devboard_init(void)
 {
 	printk(KERN_INFO "Initializing mx31devboard peripherals\n");
-	mxc_iomux_setup_multiple_pins(mxc_uart1_pins, ARRAY_SIZE(mxc_uart1_pins), "uart1");
+
+	mxc_iomux_setup_multiple_pins(devboard_pins, ARRAY_SIZE(devboard_pins),
+		"devboard");
+
 	mxc_register_device(&mxc_uart_device1, &uart_pdata);
 }
--- arch/arm/mach-mx3/mx31moboard-marxbot.c
+++ arch/arm/mach-mx3/mx31moboard-marxbot.c
@@ -17,13 +17,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <linux/types.h>
 #include <linux/init.h>
-
 #include <linux/platform_device.h>
+#include <linux/types.h>
 
-#include <mach/hardware.h>
 #include <mach/common.h>
+#include <mach/hardware.h>
 #include <mach/imx-uart.h>
 #include <mach/iomux-mx3.h>
 
