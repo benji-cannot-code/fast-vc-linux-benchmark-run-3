3b44ca9a4e05c57febc2c3a68b69df0182ef8e5f
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2009-04-17 20:35:57 -0700

    Input: fp801-gp - add MODULE_DESCRIPTION()
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/gameport/fm801-gp.c
+++ drivers/input/gameport/fm801-gp.c
@@ -168,5 +168,6 @@ module_exit(fm801_gp_exit);
 
 MODULE_DEVICE_TABLE(pci, fm801_gp_id_table);
 
+MODULE_DESCRIPTION("FM801 gameport driver");
 MODULE_AUTHOR("Takashi Iwai <tiwai@suse.de>");
 MODULE_LICENSE("GPL");
