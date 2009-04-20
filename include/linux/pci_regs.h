3e9df7a203be3b87822968b3bcc6458e42d60a5c
Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
2009-04-20 10:54:52 +0900

    PCI MSI: Remove unused/obsolete macros and definitions
    Impact: cleanup, spec compliance
    
    This patch does:
    
     - Remove unused msi/msix_enable/disable macros.
       User should use msi/msix_set_enable() functions instead.
    
     - Remove unused msix_mask/unmask/pending macros.
       These macros are useless because they are not based on any of
       the PCI Local Bus Specifications properly.
       It seems that they were written based on a draft of PCI spec,
       and that the draft was the MSI-X ECN that underwent membership
       review in September 2002.
       (* In the draft, the size of a entry in MSI-X table was 64bit,
          containing 32bit message data and DWORD aligned lower address
          plus a pending bit and a mask bit.(30+1+1bit)  The higher
          address was placed in MSI-X capability structure and shared
          by all entries.)
    
     - Remove PCI_MSIX_FLAGS_BITMASK.
       This definition also come from the draft ECN.
    
    Signed-off-by: Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
    Reviewed-by: Matthew Wilcox <willy@linux.intel.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- include/linux/pci_regs.h
+++ include/linux/pci_regs.h
@@ -305,7 +305,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  PCI_MSIX_FLAGS_ENABLE	(1 << 15)
 #define  PCI_MSIX_FLAGS_MASKALL	(1 << 14)
 #define PCI_MSIX_FLAGS_BIRMASK	(7 << 0)
-#define PCI_MSIX_FLAGS_BITMASK	(1 << 0)
 
 /* CompactPCI Hotswap Register */
 
