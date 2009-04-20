817ac4624de67358244e414a99d166472db19983
David Howells <dhowells@redhat.com>
2009-04-20 12:46:24 +0100

    FRV: Don't attempt to #include <linux/blk.h> as it doesn't exist
    Stop the FRV arch from attempting to #include <linux/blk.h> as it doesn't
    exist.
    
    Reported-by: Robert P. J. Day <rpjday@crashcourse.ca>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/frv/kernel/setup.c
+++ arch/frv/kernel/setup.c
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 
 #ifdef CONFIG_BLK_DEV_INITRD
-#include <linux/blk.h>
 #include <asm/pgtable.h>
 #endif
 
