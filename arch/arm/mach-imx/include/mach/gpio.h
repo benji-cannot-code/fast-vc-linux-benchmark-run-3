f8d811aefd32b48f0ece93721576ce4d305ae22b
David Brownell <david-b@pacbell.net>
2009-04-20 03:33:09 +0100

    [ARM] 5457/1: mach-imx gpio buildfix
    From: David Brownell <dbrownell@users.sourceforge.net>
    
    Fix GPIO-related build error on mach-imx platform:
    
      CC      drivers/spi/spi_gpio.o
    In file included from /home/db/kernel/scratch/arch/arm/include/asm/gpio.h:5,
                     from include/linux/gpio.h:7,
                     from drivers/spi/spi_gpio.c:23:
    arch/arm/mach-imx/include/mach/gpio.h: In function 'imx_gpio_get_value':
    arch/arm/mach-imx/include/mach/gpio.h:27: error: implicit declaration of function '__REG2'
    arch/arm/mach-imx/include/mach/gpio.h:27: error: 'IMX_IO_BASE' undeclared (first use in this function)
    arch/arm/mach-imx/include/mach/gpio.h:27: error: (Each undeclared identifier is reported only once
    arch/arm/mach-imx/include/mach/gpio.h:27: error: for each function it appears in.)
    arch/arm/mach-imx/include/mach/gpio.h: In function 'imx_gpio_set_value_inline':
    arch/arm/mach-imx/include/mach/gpio.h:36: error: 'IMX_IO_BASE' undeclared (first use in this function)
    arch/arm/mach-imx/include/mach/gpio.h:36: error: lvalue required as left operand of assignment
    arch/arm/mach-imx/include/mach/gpio.h:38: error: lvalue required as left operand of assignment
    	...
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-imx/include/mach/gpio.h
+++ arch/arm/mach-imx/include/mach/gpio.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _IMX_GPIO_H
 
 #include <linux/kernel.h>
+#include <mach/hardware.h>
 #include <mach/imx-regs.h>
 
 #define IMX_GPIO_ALLOC_MODE_NORMAL	0
