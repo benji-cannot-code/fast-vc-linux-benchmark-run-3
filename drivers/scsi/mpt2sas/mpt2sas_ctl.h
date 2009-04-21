fd27cae8baf478739a72f9bffb260f240fc4c4ca
Eric Moore <eric.moore@lsi.com>
2009-04-21 15:37:20 -0600

    [SCSI] mpt2sas : unique ioctl magic number
    The current magic number is shared with mptsas driver. This to be unique to
    fix issues with register_ioctls32_conversion in older kernels.  We are making
    this change across all versions of the sas2.0 drivers.
    
    Signed-off-by: Eric Moore <eric.moore@lsi.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/mpt2sas/mpt2sas_ctl.h
+++ drivers/scsi/mpt2sas/mpt2sas_ctl.h
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #define MPT2SAS_DEV_NAME	"mpt2ctl"
-#define MPT2_MAGIC_NUMBER	'm'
+#define MPT2_MAGIC_NUMBER	'L'
 #define MPT2_IOCTL_DEFAULT_TIMEOUT (10) /* in seconds */
 
 /**
