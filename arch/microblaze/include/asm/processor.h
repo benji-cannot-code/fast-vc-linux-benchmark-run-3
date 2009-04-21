0190ed3b707931865d60598860f074a6183cbd0d
Michal Simek <monstr@monstr.eu>
2009-04-21 14:08:47 +0200

    microblaze: Add STATE_SAVE_ARG_SPACE for noMMU kernel too
    For noMMU kernel this change nothing bring but this change
    helps with MMU synchronization.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/processor.h
+++ arch/microblaze/include/asm/processor.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/setup.h>
 #include <asm/registers.h>
 #include <asm/segment.h>
+#include <asm/entry.h>
+#include <asm/current.h>
 
 # ifndef __ASSEMBLY__
 /* from kernel/cpu/mb.c */
