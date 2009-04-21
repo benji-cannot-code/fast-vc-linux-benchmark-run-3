fa36dbdd7af7af0fd34f3a0ba73a216ed73fd6ff
Frederic Weisbecker <fweisbec@gmail.com>
2009-04-22 00:41:09 +0200

    tracing/events: protect __get_str()
    The __get_str() macro is used in a code part then its content should be
    protected with parenthesis.
    
    [ Impact: make macro definition more robust ]
    
    Reported-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- include/trace/ftrace.h
+++ include/trace/ftrace.h
@@ -124,7 +124,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TP_printk(fmt, args...) fmt "\n", args
 
 #undef __get_str
-#define __get_str(field)	(char *)__entry + __entry->__str_loc_##field
+#define __get_str(field)	((char *)__entry + __entry->__str_loc_##field)
 
 #undef TRACE_EVENT
 #define TRACE_EVENT(call, proto, args, tstruct, assign, print)		\
