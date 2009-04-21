11722b98e1ce59230facbe5bc91ce0ffbb193b83
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2009-04-22 00:43:22 +0400

    [ARM] pxa/csb726: register ac97 controller
    Add a call to pxa_set_ac97_info() to enable the audio support.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/csb726.c
+++ arch/arm/mach-pxa/csb726.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/mmc.h>
 #include <mach/ohci.h>
 #include <mach/pxa2xx-regs.h>
+#include <mach/audio.h>
 
 #include "generic.h"
 #include "devices.h"
@@ -316,6 +317,7 @@ static void __init csb726_init(void)
 	pxa27x_set_i2c_power_info(NULL);
 	pxa_set_mci_info(&csb726_mci);
 	pxa_set_ohci_info(&csb726_ohci_platform_data);
+	pxa_set_ac97_info(NULL);
 
 	platform_add_devices(devices, ARRAY_SIZE(devices));
 }
