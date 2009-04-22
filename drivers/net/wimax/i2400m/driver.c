64c7214dd1c97cb5e26e698a0a825a1c3c959144
Inaky Perez-Gonzalez <inaky@linux.intel.com>
2009-04-22 16:53:08 -0700

    wimax/i2400m: generate fake source MAC address with random_ether_addr()
    The WiMAX i2400m driver needs to generate a fake source MAC address to
    fake an ethernet header (for destination, the card's MAC is
    used). This is the source of the packet, which is the basestation it
    came from. The basestation's mac address is not usable for this, as it
    uses its own namespace and it is not always available.
    
    Currently the fake source MAC address was being set to all zeros,
    which was causing trouble with bridging.
    
    Use random_ether_addr() to generate a proper one that creates no
    trouble.
    
    Signed-off-by: Inaky Perez-Gonzalez <inaky@linux.intel.com>

--- drivers/net/wimax/i2400m/driver.c
+++ drivers/net/wimax/i2400m/driver.c
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   unregister_netdev()
  */
 #include "i2400m.h"
+#include <linux/etherdevice.h>
 #include <linux/wimax/i2400m.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -651,6 +652,7 @@ int i2400m_setup(struct i2400m *i2400m, enum i2400m_bri bm_flags)
 	result = i2400m_read_mac_addr(i2400m);
 	if (result < 0)
 		goto error_read_mac_addr;
+	random_ether_addr(i2400m->src_mac_addr);
 
 	result = register_netdev(net_dev);	/* Okey dokey, bring it up */
 	if (result < 0) {
