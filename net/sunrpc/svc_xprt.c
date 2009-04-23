7f37904845f1a61e9dae416b13bcbe433bfc0521
H Hartley Sweeten <hartleys@visionengravers.com>
2009-04-22 20:18:19 -0400

    net/sunrpc/svc_xprt.c: fix sparse warnings
    Fix the following sparse warnings in net/sunrpc/svc_xprt.c.
    
      warning: symbol 'svc_recv' was not declared. Should it be static?
      warning: symbol 'svc_drop' was not declared. Should it be static?
      warning: symbol 'svc_send' was not declared. Should it be static?
      warning: symbol 'svc_close_all' was not declared. Should it be static?
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- net/sunrpc/svc_xprt.c
+++ net/sunrpc/svc_xprt.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <linux/sunrpc/stats.h>
 #include <linux/sunrpc/svc_xprt.h>
+#include <linux/sunrpc/svcsock.h>
 
 #define RPCDBG_FACILITY	RPCDBG_SVCXPRT
 
