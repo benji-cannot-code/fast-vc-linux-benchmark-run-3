14b670436d0d2d8fb554a425c97efa9c27aa65f5
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2009-04-24 12:55:48 -0300

    V4L/DVB (11609): soc-camera: remove an extra device generation from struct soc_camera_host
    Make camera devices direct children of host platform devices, move the
    inheritance management into the soc_camera.c core driver.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/soc_camera.h
+++ include/media/soc_camera.h
@@ -61,7 +61,7 @@ struct soc_camera_file {
 
 struct soc_camera_host {
 	struct list_head list;
-	struct device dev;
+	struct device *dev;
 	unsigned char nr;				/* Host number */
 	void *priv;
 	const char *drv_name;
@@ -118,7 +118,7 @@ static inline struct soc_camera_device *to_soc_camera_dev(struct device *dev)
 
 static inline struct soc_camera_host *to_soc_camera_host(struct device *dev)
 {
-	return container_of(dev, struct soc_camera_host, dev);
+	return dev_get_drvdata(dev);
 }
 
 extern int soc_camera_host_register(struct soc_camera_host *ici);
