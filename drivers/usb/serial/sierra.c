a8be2dc1aab6cde683da1d1c73d528991c2f3f6a
Elina Pasheva <epasheva@sierrawireless.com>
2009-04-24 18:41:49 -0700

    USB: serial: sierra driver performance improvements
    - Version number set to 1.3.4
     - Increased the number of input/output URBs for improved performance
       (numbers based on an measurement study triggered by a user request).
       We performed the testing using a network simulator that provided full
       speeds in the uplink and downlink directions and this combination of
       URBs provided the best throughput.
    
    Signed-off-by: Elina Pasheva <epasheva@sierrawireless.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/sierra.c
+++ drivers/usb/serial/sierra.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
   Whom based his on the Keyspan driver by Hugh Blemings <hugh@blemings.org>
 */
 
-#define DRIVER_VERSION "v.1.3.3"
+#define DRIVER_VERSION "v.1.3.4"
 #define DRIVER_AUTHOR "Kevin Lloyd <klloyd@sierrawireless.com>"
 #define DRIVER_DESC "USB Driver for Sierra Wireless USB modems"
 
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SWIMS_USB_REQUEST_SetPower	0x00
 #define SWIMS_USB_REQUEST_SetNmea	0x07
 
-#define N_IN_URB	4
-#define N_OUT_URB	4
+#define N_IN_URB	8
+#define N_OUT_URB	64
 #define IN_BUFLEN	4096
 
 static int debug;
