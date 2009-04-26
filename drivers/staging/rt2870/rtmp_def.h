6959e39e61406756430d593bc0e3fbc58fc31d34
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-04-26 16:05:09 +0200

    Staging: rt2870: remove dead QOS_DLS_SUPPORT code
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2870/rtmp_def.h
+++ drivers/staging/rt2870/rtmp_def.h
@@ -625,10 +625,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AP_CNTL_STATE_MACHINE           15
 #define AP_WPA_STATE_MACHINE            16
 
-#ifdef QOS_DLS_SUPPORT
-#define DLS_STATE_MACHINE               26
-#endif // QOS_DLS_SUPPORT //
-
 //
 // STA's CONTROL/CONNECT state machine: states, events, total function #
 //
