65a65586fdf6621b8b5a79db1a381f9e94b09ab6
Sergio Luis <sergio@larces.uece.br>
2009-04-28 00:26:50 +0200

    x86: unify power/cpu_(32|64) global variables
    Aiming total unification of cpu_32.c and cpu_64.c, in this step
    we do unify the global variables and existing forward declarations
    for such files.
    
    Signed-off-by: Sergio Luis <sergio@larces.uece.br>
    Signed-off-by: Lauro Salmito <laurosalmito@gmail.com>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- arch/x86/power/cpu_64.c
+++ arch/x86/power/cpu_64.c
@@ -20,9 +20,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/xcr.h>
 #include <asm/suspend.h>
 
+#ifdef CONFIG_X86_32
+static struct saved_context saved_context;
+
+unsigned long saved_context_ebx;
+unsigned long saved_context_esp, saved_context_ebp;
+unsigned long saved_context_esi, saved_context_edi;
+unsigned long saved_context_eflags;
+#else
+/* CONFIG_X86_64 */
 static void fix_processor_context(void);
 
 struct saved_context saved_context;
+#endif
 
 /**
  *	__save_processor_state - save CPU registers before creating a
