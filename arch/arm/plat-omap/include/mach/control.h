6202906fa43df3ec40e1eb74d4fc1b4bff15e083
Kevin Hilman <khilman@deeprootsystems.com>
2009-04-28 15:27:44 -0700

    OMAP3: PM: Add D2D clocks and auto-idle setup to PRCM init
    Add D2D clocks (modem_fck, sad2d_ick, mad2d_ick) to clock framework
    and ensure that auto-idle bits are set for these clocks during PRCM
    init.
    
    Also add omap3_d2d_idle() function called durint PRCM setup which
    ensures D2D pins are MUX'd correctly to enable retention for
    standalone (no-modem) devices.
    
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/plat-omap/include/mach/control.h
+++ arch/arm/plat-omap/include/mach/control.h
@@ -145,6 +145,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP343X_CONTROL_PBIAS_LITE	(OMAP2_CONTROL_GENERAL + 0x02b0)
 #define OMAP343X_CONTROL_TEMP_SENSOR	(OMAP2_CONTROL_GENERAL + 0x02b4)
 
+/* 34xx D2D idle-related pins, handled by PM core */
+#define OMAP3_PADCONF_SAD2D_MSTANDBY   0x250
+#define OMAP3_PADCONF_SAD2D_IDLEACK    0x254
+
 /*
  * REVISIT: This list of registers is not comprehensive - there are more
  * that should be added.
