341c2258ec49b85b977ffeac788c7ee64367918b
Pekka Enberg <penberg@cs.helsinki.fi>
2009-04-28 16:00:49 +0300

    x86: move max_pfn_mapped and max_low_pfn_mapped to setup.c
    This patch moves the max_pfn_mapped and max_low_pfn_mapped global
    variables to kernel/setup.c where they're initialized.
    
    [ Impact: cleanup ]
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    LKML-Reference: <1240923649.1982.21.camel@penberg-laptop>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -50,10 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/paravirt.h>
 #include <asm/setup.h>
 #include <asm/cacheflush.h>
+#include <asm/page_types.h>
 #include <asm/init.h>
-
-unsigned long max_low_pfn_mapped;
-unsigned long max_pfn_mapped;
 
 DEFINE_PER_CPU(struct mmu_gather, mmu_gathers);
 unsigned long highstart_pfn, highend_pfn;
