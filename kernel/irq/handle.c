d4f76b00b331e54e9f31e6f8122b65764864c390
Heiko Carstens <heiko.carstens@de.ibm.com>
2009-04-29 13:51:39 +0200

    tracing: fix build failure on s390
    "tracing: create automated trace defines" causes this compile error on s390,
    as reported by Sachin Sant against linux-next:
    
     kernel/built-in.o: In function `__do_softirq':
     (.text+0x1c680): undefined reference to `__tracepoint_softirq_entry'
    
    This happens because the definitions of the softirq tracepoints were moved
    from kernel/softirq.c to kernel/irq/handle.c. Since s390 doesn't support
    generic hardirqs handle.c doesn't get compiled and the definitions are
    missing.
    
    So move the tracepoints to softirq.c again.
    
    [ Impact: fix build failure on s390 ]
    
    Reported-by: Sachin Sant <sachinp@in.ibm.com>
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: fweisbec@gmail.com
    LKML-Reference: <20090429135139.5fac79b8@osiris.boeblingen.de.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/irq/handle.c
+++ kernel/irq/handle.c
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rculist.h>
 #include <linux/hash.h>
 #include <linux/bootmem.h>
-
-#define CREATE_TRACE_POINTS
 #include <trace/events/irq.h>
 
 #include "internals.h"
--- kernel/softirq.c
+++ kernel/softirq.c
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ftrace.h>
 #include <linux/smp.h>
 #include <linux/tick.h>
+
+#define CREATE_TRACE_POINTS
 #include <trace/events/irq.h>
 
 #include <asm/irq.h>
