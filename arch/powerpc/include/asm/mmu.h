bad999b067908caff02d0025c9e8c95202b9617d
Milton Miller <miltonm@bga.com>
2009-04-29 20:58:01 +0000

    powerpc: Add 2.06 tlbie mnemonics
    This adds the PowerPC 2.06 tlbie mnemonics and keeps backwards
    compatibilty for CPUs before 2.06.
    
    Only useful for bare metal systems.
    
    Signed-off-by: Milton Miller <miltonm@bga.com>
    Signed-off-by: Michael Neuling <mikey@neuling.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/mmu.h
+++ arch/powerpc/include/asm/mmu.h
@@ -53,6 +53,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define MMU_FTR_NEED_DTLB_SW_LRU	ASM_CONST(0x00200000)
 
+/* This indicates that the processor uses the ISA 2.06 server tlbie
+ * mnemonics
+ */
+#define MMU_FTR_TLBIE_206		ASM_CONST(0x00400000)
+
 #ifndef __ASSEMBLY__
 #include <asm/cputable.h>
 
