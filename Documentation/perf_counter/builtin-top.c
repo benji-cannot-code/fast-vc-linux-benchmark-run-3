e0e61904b430bea3fabfa8e1af28ee9b406b1811
Robert Richter <robert.richter@amd.com>
2009-04-29 12:47:26 +0200

    perf_counter: update 'perf top' documentation
    The documentation about the perf-top build was outdated after
    perfstat has been implemented. This updates it.
    
    [ Impact: update documentation ]
    
    Signed-off-by: Robert Richter <robert.richter@amd.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    LKML-Reference: <1241002046-8832-30-git-send-email-robert.richter@amd.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- Documentation/perf_counter/builtin-top.c
+++ Documentation/perf_counter/builtin-top.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
    Build with:
 
-     cc -O6 -Wall -c -o kerneltop.o kerneltop.c -lrt
+     make -C Documentation/perf_counter/
 
    Sample output:
 
