76d163bab689b240a36a80ab249d0f609a9d9a49
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2009-04-30 13:56:28 -0700

    iwlwifi: "is_fat" bit in rate scale match RXON flag
    This patch change the "is_fat" checking in rate scale to use
    iwl_is_fat_tx_allowed() to match the sta and RX_ON command setting.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Tested-by: Conrad Kostecki <ConiKost@gmx.de>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn-rs.c
+++ drivers/net/wireless/iwlwifi/iwl-agn-rs.c
@@ -1208,8 +1208,7 @@ static int rs_switch_to_mimo2(struct iwl_priv *priv,
 	tbl->action = 0;
 	rate_mask = lq_sta->active_mimo2_rate;
 
-	if (priv->current_ht_config.supported_chan_width
-					== IWL_CHANNEL_WIDTH_40MHZ)
+	if (iwl_is_fat_tx_allowed(priv, &sta->ht_cap))
 		tbl->is_fat = 1;
 	else
 		tbl->is_fat = 0;
@@ -1274,8 +1273,7 @@ static int rs_switch_to_mimo3(struct iwl_priv *priv,
 	tbl->action = 0;
 	rate_mask = lq_sta->active_mimo3_rate;
 
-	if (priv->current_ht_config.supported_chan_width
-					== IWL_CHANNEL_WIDTH_40MHZ)
+	if (iwl_is_fat_tx_allowed(priv, &sta->ht_cap))
 		tbl->is_fat = 1;
 	else
 		tbl->is_fat = 0;
@@ -1333,8 +1331,7 @@ static int rs_switch_to_siso(struct iwl_priv *priv,
 	tbl->action = 0;
 	rate_mask = lq_sta->active_siso_rate;
 
-	if (priv->current_ht_config.supported_chan_width
-	    == IWL_CHANNEL_WIDTH_40MHZ)
+	if (iwl_is_fat_tx_allowed(priv, &sta->ht_cap))
 		tbl->is_fat = 1;
 	else
 		tbl->is_fat = 0;
