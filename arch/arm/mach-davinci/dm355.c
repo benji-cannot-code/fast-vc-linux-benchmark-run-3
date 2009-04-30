8a716597f9dcee805fd8bb61faf1f5fa4a886d05
Kevin Hilman <khilman@deeprootsystems.com>
2009-04-30 11:20:24 -0700

    davinci: cleanup: move dm355 UART2 define to dm355.c
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/dm355.c
+++ arch/arm/mach-davinci/dm355.c
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "clock.h"
 #include "mux.h"
 
+#define DM355_UART2_BASE	(IO_PHYS + 0x206000)
+
 /*
  * Device specific clocks
  */
