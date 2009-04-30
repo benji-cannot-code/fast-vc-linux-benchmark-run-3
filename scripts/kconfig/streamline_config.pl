ea3dea0b816166cf0803c200e9f8bdabfd0bbe45
Steven Rostedt <srostedt@redhat.com>
2009-04-30 14:39:48 -0400

    kconfig: search for a config to base the local(mod|yes)config on
    Instead of using the .config in the local directory. This patch
    changes streamline_config.pl to search various locations for a config.
    
    Here's the list and order of search:
    
      /proc/config.gz
      /boot/vmlinuz-`uname -r`
      vmlinux  # local to the directory
      /lib/modules/`uname -r`/kernel/kernel/configs.ko
      kernel/configs.ko
      kernel/configs.o
      .config
    
    Once it finds a file that contains a config (it checks if the binary
    objects have configs first) it then uses it to create the .config
    with minimum modules needed.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- scripts/kconfig/streamline_config.pl
+++ scripts/kconfig/streamline_config.pl
@@ -46,7 +46,68 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 my $config = ".config";
 my $linuxpath = ".";
 
-open(CIN,$config) || die "Can't open current config file: $config";
+my $uname = `uname -r`;
+chomp $uname;
+
+my @searchconfigs = (
+	{
+	    "file" => "/proc/config.gz",
+	    "exec" => "zcat",
+	},
+	{
+	    "file" => "/boot/vmlinuz-$uname",
+	    "exec" => "scripts/extract-ikconfig",
+	    "test" => "scripts/extract-ikconfig",
+	},
+	{
+	    "file" => "vmlinux",
+	    "exec" => "scripts/extract-ikconfig",
+	    "test" => "scripts/extract-ikconfig",
+	},
+	{
+	    "file" => "/lib/modules/$uname/kernel/kernel/configs.ko",
+	    "exec" => "scripts/extract-ikconfig",
+	    "test" => "scripts/extract-ikconfig",
+	},
+	{
+	    "file" => "kernel/configs.ko",
+	    "exec" => "scripts/extract-ikconfig",
+	    "test" => "scripts/extract-ikconfig",
+	},
+	{
+	    "file" => "kernel/configs.o",
+	    "exec" => "scripts/extract-ikconfig",
+	    "test" => "scripts/extract-ikconfig",
+	},
+	{
+	    "file" => ".config",
+	    "exec" => "cat",
+	},
+);
+
+sub find_config {
+    foreach my $conf (@searchconfigs) {
+	my $file = $conf->{"file"};
+
+	next if ( ! -f "$file");
+
+	if (defined($conf->{"test"})) {
+	    `$conf->{"test"} $conf->{"file"} 2>/dev/null`;
+	    next if ($?);
+	}
+
+	my $exec = $conf->{"exec"};
+
+	print STDERR "using config: '$file'\n";
+
+	open(CIN, "$exec $file |") || die "Failed to run $exec $file";
+	return;
+    }
+    die "No config file found";
+}
+
+find_config;
+
 my @makefiles = `find $linuxpath -name Makefile`;
 my %depends;
 my %selects;
