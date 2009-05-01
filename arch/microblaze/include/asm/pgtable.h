1fde31193a932f1c918360a54843f33f348914c5
Arnd Bergmann <arnd@arndb.de>
2009-05-01 21:44:51 +0000

    microblaze: add a dummy pgprot_noncached
    Some device drivers call this, so add a macro
    that pretends to do this. Since there is no
    MMU support, it won't actually result in an
    uncached mapping, though.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/pgtable.h
+++ arch/microblaze/include/asm/pgtable.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PAGE_READONLY		__pgprot(0) /* these mean nothing to non MMU */
 #define PAGE_KERNEL		__pgprot(0) /* these mean nothing to non MMU */
 
+#define pgprot_noncached(x)	(x)
+
 #define __swp_type(x)		(0)
 #define __swp_offset(x)		(0)
 #define __swp_entry(typ, off)	((swp_entry_t) { ((typ) | ((off) << 7)) })
