2c3b66224f000f6eabd5d65327bb2d1c14562995
Thomas Gleixner <tglx@linutronix.de>
2009-05-02 20:00:44 +0000

    sh: remove obsolete no_irq_type
    Impact: cleanup
    
    convert the last remaining users to no_irq_chip
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/sh_ksyms_32.c
+++ arch/sh/kernel/sh_ksyms_32.c
@@ -20,13 +20,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ftrace.h>
 
 extern int dump_fpu(struct pt_regs *, elf_fpregset_t *);
-extern struct hw_interrupt_type no_irq_type;
 
 /* platform dependent support */
 EXPORT_SYMBOL(dump_fpu);
 EXPORT_SYMBOL(kernel_thread);
 EXPORT_SYMBOL(irq_desc);
-EXPORT_SYMBOL(no_irq_type);
+EXPORT_SYMBOL(no_irq_chip);
 
 EXPORT_SYMBOL(strlen);
 
