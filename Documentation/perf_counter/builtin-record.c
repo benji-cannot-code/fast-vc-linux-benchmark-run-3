02f7d05992a6ec3605f4627c485b0b1595c8394b
Mike Galbraith <efault@gmx.de>
2009-05-02 08:02:36 +0200

    perf_counter tools: fix build error
    ctype.h crawled out of the bit bucket :)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- Documentation/perf_counter/builtin-record.c
+++ Documentation/perf_counter/builtin-record.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
-#include <ctype.h>
 #include <time.h>
 #include <sched.h>
 #include <pthread.h>
