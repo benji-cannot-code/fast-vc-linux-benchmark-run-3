f2bc5d5a23fe2290000b21b704cabdbe3381dc38
Paul Mundt <lethal@linux-sh.org>
2009-05-03 18:18:14 +0900

    sh: Kill off the now unused ARCH_USES_GETTIMEOFFSET code.
    Now that the stragglers (MTU2/CMT/etc.) have been rewritten and we are
    selecting both GENERIC_TIME and GENERIC_CLOCKEVENTS, the get_offset()
    timer op is completely unused. As a result, we are now able to kill off
    the ARCH_USES_GETTIMEOFFSET references.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/timer.h
+++ arch/sh/include/asm/timer.h
@@ -10,9 +10,6 @@ struct sys_timer_ops {
 	int (*init)(void);
 	int (*start)(void);
 	int (*stop)(void);
-#ifdef CONFIG_ARCH_USES_GETTIMEOFFSET
-	unsigned long (*get_offset)(void);
-#endif
 };
 
 struct sys_timer {
@@ -26,13 +23,6 @@ struct sys_timer {
 
 extern struct sys_timer tmu_timer;
 extern struct sys_timer *sys_timer;
-
-#ifdef CONFIG_ARCH_USES_GETTIMEOFFSET
-static inline unsigned long get_timer_offset(void)
-{
-	return sys_timer->ops->get_offset();
-}
-#endif
 
 /* arch/sh/kernel/timers/timer.c */
 struct sys_timer *get_sys_timer(void);
