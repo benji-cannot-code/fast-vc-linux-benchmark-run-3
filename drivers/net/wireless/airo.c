8613200d14625a04c02a30c9d3a8377ec7b3c20a
John W. Linville <linville@tuxdriver.com>
2009-05-04 11:18:57 -0400

    airo: airo_get_encode{,ext} potential buffer overflow
    Feeding the return code of get_wep_key directly to the length parameter
    of memcpy is a bad idea since it could be -1...
    
    Reported-by: Eugene Teo <eugeneteo@kernel.sg>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/airo.c
+++ drivers/net/wireless/airo.c
@@ -6502,7 +6502,10 @@ static int airo_get_encode(struct net_device *dev,
 
 	/* Copy the key to the user buffer */
 	dwrq->length = get_wep_key(local, index, &buf[0], sizeof(buf));
-	memcpy(extra, buf, dwrq->length);
+	if (dwrq->length != -1)
+		memcpy(extra, buf, dwrq->length);
+	else
+		dwrq->length = 0;
 
 	return 0;
 }
@@ -6660,7 +6663,10 @@ static int airo_get_encodeext(struct net_device *dev,
 	
 	/* Copy the key to the user buffer */
 	ext->key_len = get_wep_key(local, idx, &buf[0], sizeof(buf));
-	memcpy(extra, buf, ext->key_len);
+	if (ext->key_len != -1)
+		memcpy(extra, buf, ext->key_len);
+	else
+		ext->key_len = 0;
 
 	return 0;
 }
