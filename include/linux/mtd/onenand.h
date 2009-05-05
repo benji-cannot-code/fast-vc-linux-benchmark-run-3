8f51c1ea4e0f49eef4434c288de28078e237671f
Adrian Hunter <adrian.hunter@nokia.com>
2009-05-05 11:04:19 +0300

    mtd: OneNAND: add support for OneNAND manufactured by Numonyx
    In addition to adding the Numonyx manufacturer code, this patch
    also ensures 'sync. write' is disabled when reading identification
    data - something that the Numonyx chip objects to, but the
    Samsung chip seems to ignore.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@nokia.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- include/linux/mtd/onenand.h
+++ include/linux/mtd/onenand.h
@@ -177,6 +177,7 @@ struct onenand_chip {
  * OneNAND Flash Manufacturer ID Codes
  */
 #define ONENAND_MFR_SAMSUNG	0xec
+#define ONENAND_MFR_NUMONYX	0x20
 
 /**
  * struct onenand_manufacturers - NAND Flash Manufacturer ID Structure
