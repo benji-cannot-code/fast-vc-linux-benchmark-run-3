3b25aea5ae80c2bf0d04e52deaacead225791f44
Rik van Riel <riel@redhat.com>
2009-05-05 17:28:56 -0400

    x86: 46 bit physical address support on 64 bits
    Extend the maximum addressable memory on x86-64 from 2^44 to
    2^46 bytes. This requires some shuffling around of the vmalloc
    and virtual memmap memory areas, to keep them away from the
    direct mapping of up to 64TB of physical memory.
    
    This patch also introduces a guard hole between the vmalloc
    area and the virtual memory map space.  There's really no
    good reason why we wouldn't have a guard hole there.
    
    [ Impact: future hardware enablement ]
    
    Signed-off-by: Rik van Riel <riel@redhat.com>
    LKML-Reference: <20090505172856.6820db22@cuia.bos.redhat.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/page_64_types.h
+++ arch/x86/include/asm/page_64_types.h
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __START_KERNEL		(__START_KERNEL_map + __PHYSICAL_START)
 #define __START_KERNEL_map	_AC(0xffffffff80000000, UL)
 
-/* See Documentation/x86_64/mm.txt for a description of the memory map. */
+/* See Documentation/x86/x86_64/mm.txt for a description of the memory map. */
 #define __PHYSICAL_MASK_SHIFT	46
 #define __VIRTUAL_MASK_SHIFT	48
 
