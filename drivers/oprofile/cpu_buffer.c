76fc4cca92a4ddf7164f247444ebeedc3ab97290
Robert Richter <robert.richter@amd.com>
2009-05-07 16:07:41 +0200

    oprofile: remove obselete include headers
    This became obsolete with this commit:
    
     6dad828 oprofile: port to the new ring_buffer
    
    Signed-off-by: Robert Richter <robert.richter@amd.com>

--- drivers/oprofile/cpu_buffer.c
+++ drivers/oprofile/cpu_buffer.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/sched.h>
 #include <linux/oprofile.h>
-#include <linux/vmalloc.h>
 #include <linux/errno.h>
 
 #include "event_buffer.h"
