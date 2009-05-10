18a98aa4dbbc528cf56ba56de4449a813f4ad1fc
Steven Toth <stoth@kernellabs.com>
2009-05-10 11:17:58 -0300

    V4L/DVB (12926): SAA7164: Email address change
    SAA7164: Email address change
    
    Signed-off-by: Steven Toth <stoth@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/saa7164/saa7164-core.c
+++ drivers/media/video/saa7164/saa7164-core.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "saa7164.h"
 
 MODULE_DESCRIPTION("Driver for NXP SAA7164 based TV cards");
-MODULE_AUTHOR("Steven Toth <stoth@hauppauge.com>");
+MODULE_AUTHOR("Steven Toth <stoth@kernellabs.com>");
 MODULE_LICENSE("GPL");
 
 /*
