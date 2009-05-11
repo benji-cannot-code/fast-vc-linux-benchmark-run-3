7088100c4c2db6900c9e34ae9dd64d9a8489c8ab
Michael Krufky <mkrufky@kernellabs.com>
2009-05-11 13:37:26 -0300

    V4L/DVB (11766): cx23885: mark functions encoder_on_port[bc] as static inline
    Signed-off-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx23885/cx23885-core.c
+++ drivers/media/video/cx23885/cx23885-core.c
@@ -1734,12 +1734,12 @@ out:
 	return IRQ_RETVAL(handled);
 }
 
-int encoder_on_portb(struct cx23885_dev *dev)
+static inline int encoder_on_portb(struct cx23885_dev *dev)
 {
 	return cx23885_boards[dev->board].portb == CX23885_MPEG_ENCODER;
 }
 
-int encoder_on_portc(struct cx23885_dev *dev)
+static inline int encoder_on_portc(struct cx23885_dev *dev)
 {
 	return cx23885_boards[dev->board].portc == CX23885_MPEG_ENCODER;
 }
