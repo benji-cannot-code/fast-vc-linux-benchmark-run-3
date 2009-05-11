43282b7e746fe71f5de716808b4a3f91d2105442
Andy Yan <ayan@marvell.com>
2009-05-11 22:19:25 +0800

    [SCSI] mvsas: performance improvement using domain_device->lldd_dev
    Using sticky field to improve retrieve performance by eliminating some
    lookups in . Remove some spurious casts.
    
    Signed-off-by: Ying Chu <jasonchu@marvell.com>
    Signed-off-by: Andy Yan <ayan@marvell.com>
    Signed-off-by: Ke Wei <kewei@marvell.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/mvsas/mv_sas.h
+++ drivers/scsi/mvsas/mv_sas.h
@@ -232,7 +232,9 @@ struct mvs_phy {
 };
 
 struct mvs_device {
+	struct list_head		dev_entry;
 	enum sas_dev_type dev_type;
+	struct mvs_info *mvi_info;
 	struct domain_device *sas_device;
 	u32 attached_phy;
 	u32 device_id;
@@ -240,7 +242,6 @@ struct mvs_device {
 	u8 taskfileset;
 	u8 dev_status;
 	u16 reserved;
-	struct list_head		dev_entry;
 };
 
 struct mvs_slot_info {
