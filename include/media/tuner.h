a80f83df3726ad46e7f48f289f5fa9473e523bfd
Dmitri Belimov <d.belimov@gmail.com>
2009-05-11 08:16:06 -0300

    V4L/DVB (11775): tuner: add support Philips MK5 tuner
    Signed-off-by: Beholder Intl. Ltd. Dmitry Belimov <d.belimov@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -125,6 +125,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TUNER_XC5000			76	/* Xceive Silicon Tuner */
 #define TUNER_TCL_MF02GIP_5N		77	/* TCL MF02GIP_5N */
 #define TUNER_PHILIPS_FMD1216MEX_MK3	78
+#define TUNER_PHILIPS_FM1216MK5		79
 
 /* tv card specific */
 #define TDA9887_PRESENT 		(1<<0)
