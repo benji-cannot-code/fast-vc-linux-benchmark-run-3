42afc067df9b49f5a50e787a634fb619d0ae34d7
Amerigo Wang <amwang@redhat.com>
2009-05-11 23:29:09 -0400

    x86: process.c, remove useless headers
    <stdarg.h> is not needed by these files, remove them.
    
    [ Impact: cleanup ]
    
    Signed-off-by: WANG Cong <amwang@redhat.com>
    Cc: akpm@linux-foundation.org
    LKML-Reference: <20090512032956.5040.77055.sendpatchset@localhost.localdomain>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This file handles the architecture-dependent parts of process handling..
  */
 
-#include <stdarg.h>
-
 #include <linux/stackprotector.h>
 #include <linux/cpu.h>
 #include <linux/errno.h>
