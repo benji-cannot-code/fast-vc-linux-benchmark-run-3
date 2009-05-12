fb7edd4638db807b82df0ffd837b11274a6c1ca0
David Daney <ddaney@caviumnetworks.com>
2009-05-12 12:41:54 -0700

    MIPS: Remove execution hazard barriers for Octeon.
    The Octeon has no execution hazards, so we can remove them and save an
    instruction per TLB handler invocation.
    
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    Reviewed by: David VomLehn <dvomlehn@cisco.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-cavium-octeon/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-cavium-octeon/cpu-feature-overrides.h
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define cpu_has_mips32r2	0
 #define cpu_has_mips64r1	0
 #define cpu_has_mips64r2	1
+#define cpu_has_mips_r2_exec_hazard 0
 #define cpu_has_dsp		0
 #define cpu_has_mipsmt		0
 #define cpu_has_userlocal	0
