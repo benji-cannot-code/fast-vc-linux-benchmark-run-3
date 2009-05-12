c113e08ad530ff57fe3f32d8991c07f739083500
Magnus Damm <damm@igel.co.jp>
2009-05-12 10:25:54 +0000

    sh: remove old TMU driver
    This patch removes the old TMU driver (CONFIG_SH_TMU/timer-tmu.c)
    
    As replacement, select the sh_tmu driver with CONFIG_SH_TIMER_TMU
    and configure timer channel using platform data.
    
    If multiple TMU channels are enabled using platform data, use the
    earlytimer parameter on the kernel command line to select channel.
    For instance, use "earlytimer=sh_tmu.0" to select the first channel.
    
    To verify which timer is being used, look at printouts or the timer
    irq count in /proc/interrupts.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/timers/timer.c
+++ arch/sh/kernel/timers/timer.c
@@ -15,9 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/timer.h>
 
 static struct sys_timer *sys_timers[] = {
-#ifdef CONFIG_SH_TMU
-	&tmu_timer,
-#endif
 	NULL,
 };
 
