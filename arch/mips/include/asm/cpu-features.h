ada45b4b8e65b16d3c3ecefff0e53092e91828c8
David Daney <ddaney@caviumnetworks.com>
2009-05-13 15:59:55 -0700

    MIPS: Allow CPU specific overriding of CP0 hwrena impl bits.
    Some CPUs have implementation dependent rdhwr registers.  Allow them
    to be enabled on a per CPU basis.
    
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -235,4 +235,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define cpu_scache_line_size()	cpu_data[0].scache.linesz
 #endif
 
+#ifndef cpu_hwrena_impl_bits
+#define cpu_hwrena_impl_bits		0
+#endif
+
 #endif /* __ASM_CPU_FEATURES_H */
