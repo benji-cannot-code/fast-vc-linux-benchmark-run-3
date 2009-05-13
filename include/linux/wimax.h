c5b1173a95a33682765107568d9c6e32b1fcda8b
Paulius Zaleckas <paulius.zaleckas@teltonika.lt>
2009-05-13 06:20:29 -0700

    wimax: Add netlink interface to get device state
    wimax connection manager / daemon has to know what is current
    state of the device. Previously it was only possible to get
    notification whet state has changed.
    
    Note:
    
     By mistake, the new generic netlink's number for
     WIMAX_GNL_OP_STATE_GET was declared inserting into the existing list
     of API calls, not appending; thus, it'd break existing API.
    
     Fixed by Inaky Perez-Gonzalez <inaky@linux.intel.com> by moving to
     the tail, where we add to the interface, not modify the interface.
    
     Thanks to Stephen Hemminger <shemminger@vyatta.com> for catching this.
    
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@teltonika.lt>

--- include/linux/wimax.h
+++ include/linux/wimax.h
@@ -79,6 +79,7 @@ enum {
 	WIMAX_GNL_OP_RFKILL,	/* Run wimax_rfkill() */
 	WIMAX_GNL_OP_RESET,	/* Run wimax_rfkill() */
 	WIMAX_GNL_RE_STATE_CHANGE,	/* Report: status change */
+	WIMAX_GNL_OP_STATE_GET,		/* Request for current state */
 };
 
 
@@ -114,6 +115,10 @@ enum {
 	WIMAX_GNL_RESET_IFIDX = 1,
 };
 
+/* Atributes for wimax_state_get() */
+enum {
+	WIMAX_GNL_STGET_IFIDX = 1,
+};
 
 /*
  * Attributes for the Report State Change
