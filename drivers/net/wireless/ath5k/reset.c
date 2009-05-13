9168e7210b7c4545bb166d59d74258945fc13c26
Forrest Zhang <forrest@hifulltech.com>
2009-05-13 11:14:39 -0400

    ath5k: fix exp off-by-one when computing OFDM delta slope
    Commit e8f055f0c3b ("ath5k: Update reset code") subtly changed the
    code that computes floating point values for the PHY3_TIMING register
    such that the exponent is off by a decimal point, which can cause
    problems with OFDM channel operation.
    
    get_bitmask_order() actually returns the highest bit set plus one,
    whereas the previous code wanted the highest bit set.  Instead, use
    ilog2 which is what this code is really calculating.  Also check
    coef_scaled to handle the (invalid) case where we need log2(0).
    
    Signed-off-by: Bob Copeland <me@bobcopeland.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath5k/reset.c
+++ drivers/net/wireless/ath5k/reset.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 \*****************************/
 
 #include <linux/pci.h> 		/* To determine if a card is pci-e */
-#include <linux/bitops.h>	/* For get_bitmask_order */
+#include <linux/log2.h>
 #include "ath5k.h"
 #include "reg.h"
 #include "base.h"
@@ -70,10 +70,10 @@ static inline int ath5k_hw_write_ofdm_timings(struct ath5k_hw *ah,
 
 	/* Get exponent
 	 * ALGO: coef_exp = 14 - highest set bit position */
-	coef_exp = get_bitmask_order(coef_scaled);
+	coef_exp = ilog2(coef_scaled);
 
 	/* Doesn't make sense if it's zero*/
-	if (!coef_exp)
+	if (!coef_scaled || !coef_exp)
 		return -EINVAL;
 
 	/* Note: we've shifted coef_scaled by 24 */
