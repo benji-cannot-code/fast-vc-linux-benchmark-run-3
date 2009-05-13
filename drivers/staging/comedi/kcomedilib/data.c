1538a12de31cfc5baf2a925df9e850bd60d38ae8
Alexander Beregalov <a.beregalov@gmail.com>
2009-05-13 17:23:42 +0400

    Staging: comedi: data.c should include delay.h
    Fix this build error:
    .../data.c:86: error: implicit declaration of function 'udelay'
    
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/kcomedilib/data.c
+++ drivers/staging/comedi/kcomedilib/data.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../comedidev.h"
 
 #include <linux/string.h>
+#include <linux/delay.h>
 
 int comedi_data_write(void *dev, unsigned int subdev, unsigned int chan,
 	unsigned int range, unsigned int aref, unsigned int data)
