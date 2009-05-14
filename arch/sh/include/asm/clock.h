a9155e32f654ce244246b30c15dae1acb90740a3
Paul Mundt <lethal@linux-sh.org>
2009-05-14 17:38:46 +0900

    sh: clkfwk: Add MSTP bits to SH7785 clock framework.
    This plugs in all of the MSTP functions in to the clock framework,
    and hands them off to the platform devices that want them.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/clock.h
+++ arch/sh/include/asm/clock.h
@@ -37,6 +37,9 @@ struct clk {
 	unsigned long		rate;
 	unsigned long		flags;
 
+	void __iomem		*enable_reg;
+	unsigned int		enable_bit;
+
 	unsigned long		arch_flags;
 	void			*priv;
 	struct dentry		*dentry;
