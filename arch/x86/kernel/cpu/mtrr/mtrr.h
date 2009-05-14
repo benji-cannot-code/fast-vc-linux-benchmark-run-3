4d6abfef10efbf06c13a4d61cd4416323239e21c
Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
2009-05-14 12:06:12 +0530

    x86, mtrr: replace MTRRcap_MSR with msr-index's MSR_MTRRcap
    Use standard msr-index.h's MSR declaration and no need to declare again.
    
    [ Impact: cleanup, no object code change ]
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/kernel/cpu/mtrr/mtrr.h
+++ arch/x86/kernel/cpu/mtrr/mtrr.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/stddef.h>
 
-#define MTRRcap_MSR     0x0fe
 #define MTRRdefType_MSR 0x2ff
 
 #define MTRRfix64K_00000_MSR 0x250
