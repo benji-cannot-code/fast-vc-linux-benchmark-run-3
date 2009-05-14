4987c65210bc15b28dc850c2fc0b51bf5428d693
Takashi Iwai <tiwai@suse.de>
2009-05-14 10:56:04 +0200

    ALSA: ctxfi - Add missing inclusion of linux/delay.h
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ctxfi/cthw20k2.c
+++ sound/pci/ctxfi/cthw20k2.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
+#include <linux/delay.h>
 
 #define CT_XFI_DMA_MASK		DMA_BIT_MASK(32) /* 32 bits */
 
