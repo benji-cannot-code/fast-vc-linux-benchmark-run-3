b3a212975e2766e8b60013e8bed3ccd8b77ad5c0
David Kilroy <kilroyd@googlemail.com>
2009-05-16 23:13:46 +0100

    cfg80211: mark ops as pointer to const
    This allows drivers to mark their cfg80211_ops tables const.
    
    Signed-off-by: David Kilroy <kilroyd@googlemail.com>
    Acked-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/wireless/core.h
+++ net/wireless/core.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "reg.h"
 
 struct cfg80211_registered_device {
-	struct cfg80211_ops *ops;
+	const struct cfg80211_ops *ops;
 	struct list_head list;
 	/* we hold this mutex during any call so that
 	 * we cannot do multiple calls at once, and also
