f44449e8233421f7f46c0259b95304e39cd4fab4
Russell King <rmk@dyn-67.arm.linux.org.uk>
2009-05-16 11:41:53 +0100

    [ARM] smp: SCU is used on non-realview platforms
    The SCU can be used by non-realview platforms, so make it visible
    for other people to use rather than having them copy the header file.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-realview/platsmp.c
+++ arch/arm/mach-realview/platsmp.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/board-eb.h>
 #include <mach/board-pb11mp.h>
-#include <mach/scu.h>
+#include <asm/smp_scu.h>
 
 #include "core.h"
 
