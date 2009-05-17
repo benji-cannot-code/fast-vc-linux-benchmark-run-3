c8c3e92a6783b8c342c722096416312947223e3f
Uri Shkolnik <uris@siano-ms.com>
2009-05-17 06:02:46 -0300

    V4L/DVB (11822): Siano: smscore - bug fix at get_device_mode
    Fix bug that cause error log to echo also if success
    
    Signed-off-by: Uri Shkolnik <uris@siano-ms.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/siano/smscoreapi.c
+++ drivers/media/dvb/siano/smscoreapi.c
@@ -906,7 +906,7 @@ int smscore_set_device_mode(struct smscore_device_t *coredev, int mode)
 		coredev->device_flags &= ~SMS_DEVICE_NOT_READY;
 	}
 
-	if (rc != 0)
+	if (rc < 0)
 		sms_err("return error code %d.", rc);
 	return rc;
 }
