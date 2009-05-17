37a9132417dff8f023c2e0d7d0081dd7e6131f15
Ben Dooks <ben-linux@fluff.org>
2009-05-17 23:40:30 +0100

    [ARM] S3C64XX: Use common watchdog reset for system reset.
    Use the newly moved <plat/watchdog-reset.h> to perform the
    arch_reset() call which has been unimplemented for a while.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-s3c/include/plat/watchdog-reset.h
+++ arch/arm/plat-s3c/include/plat/watchdog-reset.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 
 #include <plat/regs-watchdog.h>
+#include <mach/map.h>
 
 #include <linux/clk.h>
 #include <linux/err.h>
