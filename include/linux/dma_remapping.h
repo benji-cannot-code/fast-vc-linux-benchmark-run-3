8daa4a512b3052c55d02fa86253f5dfe46182af2
Yu Zhao <yu.zhao@intel.com>
2009-05-18 13:51:37 +0800

    VT-d: support the device IOTLB
    Enable the device IOTLB (i.e. ATS) for both the bare metal and KVM
    environments.
    
    Signed-off-by: Yu Zhao <yu.zhao@intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- include/linux/dma_remapping.h
+++ include/linux/dma_remapping.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DMA_PTE_SNP (1 << 11)
 
 #define CONTEXT_TT_MULTI_LEVEL	0
+#define CONTEXT_TT_DEV_IOTLB	1
 #define CONTEXT_TT_PASS_THROUGH 2
 
 struct intel_iommu;
