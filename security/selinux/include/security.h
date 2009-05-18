eee156e580e888d01ce5b183a94845dfe4d99087
Eric Paris <eparis@redhat.com>
2009-05-18 10:26:10 -0400

    SELinux: move SELINUX_MAGIC into magic.h
    The selinuxfs superblock magic is used inside the IMA code, but is being
    defined in two places and could someday get out of sync.  This patch moves the
    declaration into magic.h so it is only done once.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -9,13 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SELINUX_SECURITY_H_
 #define _SELINUX_SECURITY_H_
 
+#include <linux/magic.h>
 #include "flask.h"
 
 #define SECSID_NULL			0x00000000 /* unspecified SID */
 #define SECSID_WILD			0xffffffff /* wildcard SID */
 #define SECCLASS_NULL			0x0000 /* no class */
-
-#define SELINUX_MAGIC 0xf97cff8c
 
 /* Identify specific policy version changes */
 #define POLICYDB_VERSION_BASE		15
