c6817501784f967cfb01d29f943d4b4f26bd3afe
Jens Axboe <jens.axboe@oracle.com>
2009-05-18 08:20:32 +0200

    writeback: get rid of pdflush completely
    It is now unused, so kill it off.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -31,6 +31,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define inode_to_bdi(inode)	((inode)->i_mapping->backing_dev_info)
 
 /*
+ * We don't actually have pdflush, but this one is exported though /proc...
+ */
+int nr_pdflush_threads;
+
+/*
  * Work items for the bdi_writeback threads
  */
 struct bdi_work {
