ed9829ad916d0d657d0bfe1b9e4e3f5fa17d8ad3
Yu Zhao <yu.zhao@intel.com>
2009-05-18 13:51:32 +0800

    PCI: support the ATS capability
    The PCIe ATS capability makes the Endpoint be able to request the
    DMA address translation from the IOMMU and cache the translation
    in the device side, thus alleviate IOMMU pressure and improve the
    hardware performance in the I/O virtualization environment.
    
    Signed-off-by: Yu Zhao <yu.zhao@intel.com>
    Acked-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -189,6 +189,7 @@ struct pci_cap_saved_state {
 struct pcie_link_state;
 struct pci_vpd;
 struct pci_sriov;
+struct pci_ats;
 
 /*
  * The pci_dev structure is used to describe PCI devices.
@@ -286,6 +287,7 @@ struct pci_dev {
 		struct pci_sriov *sriov;	/* SR-IOV capability related */
 		struct pci_dev *physfn;	/* the PF this VF is associated with */
 	};
+	struct pci_ats	*ats;	/* Address Translation Service */
 #endif
 };
 
