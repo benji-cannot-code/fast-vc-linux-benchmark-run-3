7fed27531bbc3491e6e4eaef30ab4f6fefcb9031
Boaz Harrosh <bharrosh@panasas.com>
2009-05-20 18:50:34 +0300

    osdblk: Adjust queue limits to lower device's limits
    call blk_queue_stack_limits() to copy queue limits from
    the underline osd scsi_device. This is absolutely needed
    because osdblk cannot sleep when allocating a lower-request and
    therefore cannot be bouncing.
    
    TODO: Dynamic changes of limits to the lower device queue
    will not reflect in the upper driver
    
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- drivers/block/osdblk.c
+++ drivers/block/osdblk.c
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/osd_initiator.h>
 #include <scsi/osd_attributes.h>
 #include <scsi/osd_sec.h>
+#include <scsi/scsi_device.h>
 
 #define DRV_NAME "osdblk"
 #define PFX DRV_NAME ": "
@@ -437,6 +438,12 @@ static int osdblk_init_disk(struct osdblk_device *osdev)
 		put_disk(disk);
 		return rc;
 	}
+
+	/* Set our limits to the lower device limits, because osdblk cannot
+	 * sleep when allocating a lower-request and therefore cannot be
+	 * bouncing.
+	 */
+	blk_queue_stack_limits(q, osd_request_queue(osdev->osd));
 
 	blk_queue_prep_rq(q, blk_queue_start_tag);
 	blk_queue_ordered(q, QUEUE_ORDERED_DRAIN_FLUSH, osdblk_prepare_flush);
