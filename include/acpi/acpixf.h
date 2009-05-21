485bd6b6ca11591f409ed968213b8f5848ba8a97
Bob Moore <robert.moore@intel.com>
2009-05-21 11:06:53 +0800

    ACPICA: Update version to 20090521.
    Update version number.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20090422
+#define ACPI_CA_VERSION                 0x20090521
 
 #include "actypes.h"
 #include "actbl.h"
