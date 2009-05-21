3341dc069d4fd7db5a034ff7de205ff29885fdc6
Alan Stern <stern@rowland.harvard.edu>
2009-05-21 17:37:58 -0400

    USB: usb-storage: fix return values from init functions
    This patch (as1242) fixes the return values from the special
    init functions in usb-storage.  They are supposed to return 0 for
    success, not USB_STOR_TRANSPORT_GOOD.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/storage/option_ms.c
+++ drivers/usb/storage/option_ms.c
@@ -97,7 +97,7 @@ int option_ms_init(struct us_data *us)
 	    udev->descriptor.bDeviceSubClass != 0 ||
 	    udev->descriptor.bDeviceProtocol != 0 ||
 	    udev->actconfig->desc.bNumInterfaces == 3)
-		return USB_STOR_TRANSPORT_GOOD;
+		return 0;
 
 	US_DEBUGP("Option MS: option_ms_init called\n");
 
@@ -108,7 +108,7 @@ int option_ms_init(struct us_data *us)
 	    iface_desc->desc.bInterfaceProtocol != 0x50) {
 		US_DEBUGP("Option MS: mass storage interface not found, no action "
 		          "required\n");
-		return USB_STOR_TRANSPORT_GOOD;
+		return 0;
 	}
 
 	/* Find the mass storage bulk endpoints */
@@ -128,7 +128,7 @@ int option_ms_init(struct us_data *us)
 	if (!ep_in_size || !ep_out_size) {
 		US_DEBUGP("Option MS: mass storage endpoints not found, no action "
 		          "required\n");
-		return USB_STOR_TRANSPORT_GOOD;
+		return 0;
 	}
 
 	/* Force Modem mode */
@@ -144,6 +144,6 @@ int option_ms_init(struct us_data *us)
 		          " requests it\n");
 	}
 
-	return USB_STOR_TRANSPORT_GOOD;
+	return 0;
 }
 
