5c293cf39fd875e0d8fab89a42c2cbee67d8e5fd
Li Zefan <lizf@cn.fujitsu.com>
2009-05-21 13:59:18 +0800

    tracing/events: change the type of __str_loc_item to unsigned short
    When defining a dynamic size string, we add __str_loc_##item to the
    trace entry, and it stores the location of the actual string in
    entry->_str_data[]
    
    'unsigned short' should be sufficient to store this information, thus
    we save 2 bytes per dyn-size string in the ring buffer.
    
    [ Impact: reduce memory occupied by dyn-size strings in ring buffer ]
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    LKML-Reference: <4A14EDB6.2050507@cn.fujitsu.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- include/trace/ftrace.h
+++ include/trace/ftrace.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __field(type, item)		type	item;
 
 #undef __string
-#define __string(item, src)		int	__str_loc_##item;
+#define __string(item, src)		unsigned short	__str_loc_##item;
 
 #undef TP_STRUCT__entry
 #define TP_STRUCT__entry(args...) args
