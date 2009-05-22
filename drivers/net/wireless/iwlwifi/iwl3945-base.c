e8c200d94051240be673b3e497244762adda3ef6
Mohamed Abbas <mohamed.abbas@intel.com>
2009-05-22 11:01:52 -0700

    iwlcore: Add support for periodic RX interrupt
    Periodic RX interrupt needed with ICT interrupt to prevent RX race.
    Sending RX interrupt require many steps to be done in the
    the device:
     1- write interrupt to current index in ICT table.
     2- dma RX frame.
     3- update RX shared data to indicate last write index.
     4- send interrupt.
    This could lead to RX race, driver could receive RX interrupt
    but the shared data changes does not reflect that.
    this could lead to RX race, RX periodic will solve this race
    
    Signed-off-by: Mohamed Abbas <mohamed.abbas@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl3945-base.c
+++ drivers/net/wireless/iwlwifi/iwl3945-base.c
@@ -1945,9 +1945,9 @@ static void iwl3945_irq_tasklet(struct iwl_priv *priv)
 		priv->isr_stats.unhandled++;
 	}
 
-	if (inta & ~CSR_INI_SET_MASK) {
+	if (inta & ~priv->inta_mask) {
 		IWL_WARN(priv, "Disabled INTA bits 0x%08x were pending\n",
-			 inta & ~CSR_INI_SET_MASK);
+			 inta & ~priv->inta_mask);
 		IWL_WARN(priv, "   with FH_INT = 0x%08x\n", inta_fh);
 	}
 
@@ -4185,6 +4185,7 @@ static int iwl3945_pci_probe(struct pci_dev *pdev, const struct pci_device_id *e
 	IWL_DEBUG_INFO(priv, "*** LOAD DRIVER ***\n");
 	priv->cfg = cfg;
 	priv->pci_dev = pdev;
+	priv->inta_mask = CSR_INI_SET_MASK;
 
 #ifdef CONFIG_IWLWIFI_DEBUG
 	priv->debug_level = iwl3945_mod_params.debug;
