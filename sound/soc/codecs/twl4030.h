735f25be29aec9f7715dd0da4016b99814a68886
Peter Ujfalusi <peter.ujfalusi@nokia.com>
2009-05-22 10:13:15 +0300

    ASoC: TWL4030: Add support for platform dependent configuration
    twl4030_setup_data structure can be passed from platform drivers to
    the codec via the snd_soc_device->codec_data pointer.
    
    Currently the setup data has support for the Headset pop-removal
    related configuration, which differs from board to board.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@nokia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/twl4030.h
+++ sound/soc/codecs/twl4030.h
@@ -267,4 +267,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern struct snd_soc_dai twl4030_dai[2];
 extern struct snd_soc_codec_device soc_codec_dev_twl4030;
 
+struct twl4030_setup_data {
+	unsigned int ramp_delay_value;
+	unsigned int sysclk;
+};
+
 #endif	/* End of __TWL4030_AUDIO_H__ */
