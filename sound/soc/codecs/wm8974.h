5dcaa62af5483a033fe4d2ff4725ba60cf0c9bdd
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-05-23 12:27:03 +0100

    ASoC: Update WM8974 to use standard I2C device probe methods
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8974.h
+++ sound/soc/codecs/wm8974.h
@@ -94,11 +94,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WM8974_MCLKDIV_8	(6 << 5)
 #define WM8974_MCLKDIV_12	(7 << 5)
 
-
-struct wm8974_setup_data {
-	unsigned short i2c_address;
-};
-
 extern struct snd_soc_dai wm8974_dai;
 extern struct snd_soc_codec_device soc_codec_dev_wm8974;
 
