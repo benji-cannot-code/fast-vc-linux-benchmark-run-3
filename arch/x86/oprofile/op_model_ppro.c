9bf634aec7390c1fe9b88399a37ce6873da244b4
Robert Richter <robert.richter@amd.com>
2009-05-25 15:05:50 +0200

    x86/oprofile: fix and cleanup CTRL_SET_* macros
    This patch fixes missing braces around macro parameters. Macro
    definitions from intel_arch_perfmon.h are used where possible.
    
    Signed-off-by: Robert Richter <robert.richter@amd.com>

--- arch/x86/oprofile/op_model_ppro.c
+++ arch/x86/oprofile/op_model_ppro.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/msr.h>
 #include <asm/apic.h>
 #include <asm/nmi.h>
-#include <asm/intel_arch_perfmon.h>
 
 #include "op_x86_model.h"
 #include "op_counter.h"
