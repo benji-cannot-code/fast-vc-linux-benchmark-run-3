a9f1fbc60bb4fb9a127ec7a267fcf73445cd76c3
Mike Frysinger <vapier@gentoo.org>
2009-05-26 19:33:16 -0400

    mtd: uclinux: allow systems to override map addr/size
    Due to a processor anomaly (05000263 to be exact), most Blackfin parts
    cannot keep the embedded filesystem image directly after the kernel in
    RAM.  Instead, the filesystem needs to be relocated to the end of memory.
    As such, we need to tweak the map addr/size during boot for Blackfin
    systems.  This can be done in any early arch/board init code.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    CC: Paul Mundt <lethal@linux-sh.org>
    CC: Greg Ungerer <gerg@uclinux.org>
    CC: uclinux-dev@uclinux.org
    CC: linux-mtd@lists.infradead.org
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/maps/uclinux.c
+++ drivers/mtd/maps/uclinux.c
@@ -23,8 +23,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /****************************************************************************/
 
+extern char _ebss;
+
 struct map_info uclinux_ram_map = {
 	.name = "RAM",
+	.phys = (unsigned long)&_ebss,
+	.size = 0,
 };
 
 struct mtd_info *uclinux_ram_mtdinfo;
@@ -56,12 +60,10 @@ static int __init uclinux_mtd_init(void)
 {
 	struct mtd_info *mtd;
 	struct map_info *mapp;
-	extern char _ebss;
-	unsigned long addr = (unsigned long) &_ebss;
 
 	mapp = &uclinux_ram_map;
-	mapp->phys = addr;
-	mapp->size = PAGE_ALIGN(ntohl(*((unsigned long *)(addr + 8))));
+	if (!mapp->size)
+		mapp->size = PAGE_ALIGN(ntohl(*((unsigned long *)(mapp->phys + 8))));
 	mapp->bankwidth = 4;
 
 	printk("uclinux[mtd]: RAM probe address=0x%x size=0x%x\n",
