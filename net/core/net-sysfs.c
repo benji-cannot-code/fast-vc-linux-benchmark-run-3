8ba628df9d923bded5665bd93b9166179b346f32
David S. Miller <davem@davemloft.net>
2009-05-26 21:05:19 -0700

    net: Remove bogus reference to BUS_ID_SIZE in sysfs code.
    BUS_ID_SIZE is really no more, and device names are dynamically
    allocated and thus can be any necessary size.
    
    So remove the BUG check here making sure BUS_ID_SIZE is at least
    as large as IFNAMSIZ.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -500,7 +500,6 @@ int netdev_register_kobject(struct net_device *net)
 	dev->platform_data = net;
 	dev->groups = groups;
 
-	BUILD_BUG_ON(BUS_ID_SIZE < IFNAMSIZ);
 	dev_set_name(dev, "%s", net->name);
 
 #ifdef CONFIG_SYSFS
