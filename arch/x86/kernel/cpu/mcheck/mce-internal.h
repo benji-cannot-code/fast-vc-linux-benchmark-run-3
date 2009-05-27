6cf117a4f97a762415d8f3536e5a0bc950a11aec
Andi Kleen <andi@firstfloor.org>
2009-05-27 21:56:54 +0200

    x86, mce: add table driven machine check grading
    The machine check grading (as in deciding what should be done for a given
    register value) has to be done multiple times soon and it's also getting
    more complicated.
    So it makes sense to consolidate it into a single function. To get smaller
    and more straight forward and possibly more extensible code I opted towards
    a new table driven method. The various rules are put into a table
    when is then executed by a very simple interpreter.
    
    The grading engine is in a new file mce-severity.c. I also added a private
    include file mce-internal.h, because mce.h is already a bit too cluttered.
    
    This is dead code right now, but will be used in followon patches.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ arch/x86/kernel/cpu/mcheck/mce-internal.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <asm/mce.h>
+
+enum severity_level {
+	MCE_NO_SEVERITY,
+	MCE_SOME_SEVERITY,
+	MCE_UC_SEVERITY,
+	MCE_PANIC_SEVERITY,
+};
+
+int mce_severity(struct mce *a, int tolerant, char **msg);
