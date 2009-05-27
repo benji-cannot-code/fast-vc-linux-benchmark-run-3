b2670b45e900daa41d0c208589b2d226f35929f0
Ingo Molnar <mingo@elte.hu>
2009-05-27 13:19:59 +0200

    perf report: Remove <ctype.h> include
    Pekka reported build failure in builtin-report.c:
    
        CC builtin-report.o
        In file included from builtin-report.c:7:
        /usr/include/ctype.h:102: error: expected expression before token
    
    And observed:
    
    | Removing #include <ctype.h> from builtin-report.c makes the problem
    | go away. I am running Ubuntu 9.04 that has gcc 4.3.3 and libc 2.9.
    
    Reported-by: Pekka J Enberg <penberg@cs.helsinki.fi>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: John Kacur <jkacur@redhat.com>
    LKML-Reference: <new-submission>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- Documentation/perf_counter/builtin-report.c
+++ Documentation/perf_counter/builtin-report.c
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <libelf.h>
 #include <gelf.h>
 #include <elf.h>
-#include <ctype.h>
 
 #include "util/list.h"
 #include "util/cache.h"
