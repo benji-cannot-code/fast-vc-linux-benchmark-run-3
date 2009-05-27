2d9fd4f26f18969dfe3243044809bbfa866e10da
Andi Kleen <andi@firstfloor.org>
2009-05-27 21:56:58 +0200

    x86, mce: define MCE_VECTOR
    Add MCE_VECTOR for the #MC exception.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/irq_vectors.h
+++ arch/x86/include/asm/irq_vectors.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #define NMI_VECTOR			0x02
+#define MCE_VECTOR			0x12
 
 /*
  * IDT vectors usable for external interrupt sources start
