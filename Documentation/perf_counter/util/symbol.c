dd5bec32cd78bbb7ed4d68223de70781ba3a7b91
Arnaldo Carvalho de Melo <acme@redhat.com>
2009-05-28 14:55:19 -0300

    perf_counter tools: Consolidate dso methods to load kernel symbols
    Now one has just to use dso__load_kernel() optionally passing a vmlinux
    filename.
    
    Will make things easier for perf top that will want to pass a callback
    to filter some symbols.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- Documentation/perf_counter/util/symbol.c
+++ Documentation/perf_counter/util/symbol.c
@@ -156,7 +156,7 @@ static int hex2long(char *ptr, unsigned long *long_val)
 	return p - ptr;
 }
 
-int dso__load_kallsyms(struct dso *self)
+static int dso__load_kallsyms(struct dso *self)
 {
 	struct rb_node *nd, *prevnd;
 	char *line = NULL;
@@ -411,7 +411,7 @@ out:
 	return ret;
 }
 
-int dso__load_vmlinux(struct dso *self, const char *vmlinux)
+static int dso__load_vmlinux(struct dso *self, const char *vmlinux)
 {
 	int err, fd = open(vmlinux, O_RDONLY);
 
@@ -420,6 +420,19 @@ int dso__load_vmlinux(struct dso *self, const char *vmlinux)
 
 	err = dso__load_sym(self, fd, vmlinux);
 	close(fd);
+
+	return err;
+}
+
+int dso__load_kernel(struct dso *self, const char *vmlinux)
+{
+	int err = -1;
+
+	if (vmlinux)
+		err = dso__load_vmlinux(self, vmlinux);
+
+	if (err)
+		err = dso__load_kallsyms(self);
 
 	return err;
 }
