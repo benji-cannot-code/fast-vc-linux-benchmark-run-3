14c7a546729476dfa43a249bfb490692385cd24d
Mike Snitzer <snitzer@redhat.com>
2009-05-28 11:04:53 +0200

    block: export blk_stack_limits()
    DM needs to use blk_stack_limits(), so it needs to be exported.
    
    Acked-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/blk-settings.c
+++ block/blk-settings.c
@@ -501,6 +501,7 @@ int blk_stack_limits(struct queue_limits *t, struct queue_limits *b,
 
 	return 0;
 }
+EXPORT_SYMBOL(blk_stack_limits);
 
 /**
  * disk_stack_limits - adjust queue limits for stacked drivers
