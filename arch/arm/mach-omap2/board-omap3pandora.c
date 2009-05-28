dc72828c5e15e1c23cfc4301b1f66fbf8e317907
Paul Walmsley <paul@pwsan.com>
2009-05-28 14:03:59 -0700

    ARM: OMAP3: SDRC: add timing data for Micron MT46H32M32LF-6, v2
    Add timing data for the Micron MT46H32M32LF-6 SDRAM chip, used on the
    OMAP3 Beagle and EVM boards.  Original timing data is from the Micron
    datasheet PDF downloaded from:
    
    http://download.micron.com/pdf/datasheets/dram/mobile/1gb_ddr_mobile_sdram_t48m.pdf
    
    Thanks to Rajendra Nayak <rnayak@ti.com> for his help identifying
    the chips used on Beagle & OMAP3EVM.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-omap3pandora.c
+++ arch/arm/mach-omap2/board-omap3pandora.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/mcspi.h>
 #include <mach/usb.h>
 
+#include "sdram-micron-mt46h32m32lf-6.h"
 #include "mmc-twl4030.h"
 
 #define OMAP3_PANDORA_TS_GPIO		94
@@ -119,7 +120,7 @@ static int __init omap3pandora_i2c_init(void)
 
 static void __init omap3pandora_init_irq(void)
 {
-	omap2_init_common_hw(NULL);
+	omap2_init_common_hw(mt46h32m32lf6_sdrc_params);
 	omap_init_irq();
 	omap_gpio_init();
 }
