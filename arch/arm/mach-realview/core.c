dfcca3d64a76c05b8a1f7e5d987d2be3a77b1bad
Catalin Marinas <catalin.marinas@arm.com>
2009-05-30 14:00:17 +0100

    RealView: Move the IRQ_* definitions out of the board-*.h files
    The IRQ_* macros need to be made visible via the mach/irqs.h file but
    without the additional macros defined in the board-*.h files.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/mach-realview/core.c
+++ arch/arm/mach-realview/core.c
@@ -49,6 +49,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/hardware/gic.h>
 
+#include <mach/platform.h>
+#include <mach/irqs.h>
+
 #include "core.h"
 #include "clock.h"
 
