35af6680ef487f15b8c1561e6d7a623a04af3784
Takashi Iwai <tiwai@suse.de>
2009-06-02 09:18:26 +0200

    ALSA: ctxfi - Fix a typo in MODULE_LICENSE
    A space has to be put between GPL and v2.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ctxfi/xfi.c
+++ sound/pci/ctxfi/xfi.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 MODULE_AUTHOR("Creative Technology Ltd");
 MODULE_DESCRIPTION("X-Fi driver version 1.03");
-MODULE_LICENSE("GPLv2");
+MODULE_LICENSE("GPL v2");
 MODULE_SUPPORTED_DEVICE("{{Creative Labs, Sound Blaster X-Fi}");
 
 static unsigned int reference_rate = 48000;
