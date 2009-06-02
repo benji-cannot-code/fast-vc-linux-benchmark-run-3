d6271e646a47c2fe8d7594ef93123add8bbedf12
Jeff Layton <jlayton@redhat.com>
2009-06-02 06:55:20 -0400

    cifs: fix IPv6 address length check
    For IPv6 the userspace mount helper sends an address in the "ip="
    option.  This check fails if the length is > 35 characters. I have no
    idea where the magic 35 character limit came from, but it's clearly not
    enough for IPv6. Fix it by making it use the INET6_ADDRSTRLEN #define.
    
    While we're at it, use the same #define for the address length in SPNEGO
    upcalls.
    
    Reported-by: Charles R. Anderson <cra@wpi.edu>
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/namei.h>
 #include <asm/uaccess.h>
 #include <asm/processor.h>
+#include <linux/inet.h>
 #include <net/ipv6.h>
 #include "cifspdu.h"
 #include "cifsglob.h"
@@ -961,7 +962,8 @@ cifs_parse_mount_options(char *options, const char *devname,
 		} else if (strnicmp(data, "ip", 2) == 0) {
 			if (!value || !*value) {
 				vol->UNCip = NULL;
-			} else if (strnlen(value, 35) < 35) {
+			} else if (strnlen(value, INET6_ADDRSTRLEN) <
+							INET6_ADDRSTRLEN) {
 				vol->UNCip = value;
 			} else {
 				printk(KERN_WARNING "CIFS: ip address "
