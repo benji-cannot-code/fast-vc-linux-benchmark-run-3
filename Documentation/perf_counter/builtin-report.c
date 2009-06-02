280d225222c6da32235da52c8d097f0b5a8ce14c
Peter Zijlstra <a.p.zijlstra@chello.nl>
2009-06-02 14:13:24 +0200

    perf_counter: tools: Expand the COMM,MMAP event synthesizer
    Include code to pre-construct mappings based on /proc,
    on system wide recording.
    
    Fix the existing code to properly fill out ->pid and ->tid.
    
    The PID should be the Thread Group ID (PIDTYPE_PID of task->group_leader)
    The TID should be the Thread ID (PIDTYPE_PID of task)
    
    Furthermore, change the default sorting of report to comm,dso for a
    better quick overview.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: John Kacur <jkacur@redhat.com>
    LKML-Reference: <new-submission>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- Documentation/perf_counter/builtin-report.c
+++ Documentation/perf_counter/builtin-report.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static char		const *input_name = "perf.data";
 static char		*vmlinux = NULL;
-static char		*sort_order = "pid,symbol";
+static char		*sort_order = "comm,dso";
 static int		input;
 static int		show_mask = SHOW_KERNEL | SHOW_USER | SHOW_HV;
 
