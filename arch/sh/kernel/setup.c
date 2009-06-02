41a1ab62846eda86f69ec2d865a3f2af6989d0c2
Magnus Damm <damm@igel.co.jp>
2009-06-02 09:22:02 +0000

    sh: rework mode pin code
    This patch reworks the mode pin code to keep the pin
    definitions in one place. The mode pins values are now
    the value of the bit instead of bit number.
    
    With this patch in place the sh7785 header file contains
    mode pin comments. The sh7785 clock code and the sh7785lcr
    board code are updated to reflect the new shared mode pins.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/setup.c
+++ arch/sh/kernel/setup.c
@@ -430,7 +430,7 @@ int generic_mode_pins(void)
 
 int test_mode_pin(int pin)
 {
-	return sh_mv.mv_mode_pins() & (1 << pin);
+	return sh_mv.mv_mode_pins() & pin;
 }
 
 static const char *cpu_name[] = {
