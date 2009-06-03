674a2fe6007f022a0a787bff8ed1c1fca959389d
Greg Kroah-Hartman <gregkh@suse.de>
2009-06-03 14:47:21 -0700

    Staging: udlfb: fix some sparse warnings.
    There are others remaining due to the __iomem namespace of the
    framebuffer data pointer.
    
    Cc: Roberto De Ioris <roberto@unbit.it>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/udlfb/udlfb.c
+++ drivers/staging/udlfb/udlfb.c
@@ -423,7 +423,7 @@ copyarea(struct dlfb_data *dev_info, int dx, int dy, int sx, int sy,
 	return 1;
 }
 
-void dlfb_copyarea(struct fb_info *info, const struct fb_copyarea *area)
+static void dlfb_copyarea(struct fb_info *info, const struct fb_copyarea *area)
 {
 
 	struct dlfb_data *dev = info->par;
@@ -435,7 +435,7 @@ void dlfb_copyarea(struct fb_info *info, const struct fb_copyarea *area)
 
 }
 
-void dlfb_imageblit(struct fb_info *info, const struct fb_image *image)
+static void dlfb_imageblit(struct fb_info *info, const struct fb_image *image)
 {
 
 	int ret;
@@ -448,7 +448,7 @@ void dlfb_imageblit(struct fb_info *info, const struct fb_image *image)
 	/* printk("IMAGE BLIT (2) %d %d %d %d DEPTH %d {%p} %d!!!\n", image->dx, image->dy, image->width, image->height, image->depth, dev->udev, ret); */
 }
 
-void dlfb_fillrect(struct fb_info *info, const struct fb_fillrect *region)
+static void dlfb_fillrect(struct fb_info *info, const struct fb_fillrect *region)
 {
 
 	unsigned char red, green, blue;
