77c0a3a3e7408c8b0cffbe492041b99718d2457f
Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>
2009-06-04 11:10:53 +0000

    ixgbe: Harden the 82599 multispeed fiber autotry mechanism
    82599 supports multispeed fiber optical modules (10Gbps/1Gbps).  Some
    scenarios can cause the autotry mechanism to not negotiate link properly.
    What needs to happen is the driver must flap the Tx laser to induce an Rx
    Loss of Signal on the link partner.  This will restart the autotry
    mechanism to get link into a known state.  The software definable pin (SDP)
    3 on the 0x10fb NIC is wired to cause a Tx LOS event, which triggers the
    Rx LOS we require.
    
    Signed-off-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>
    Acked-by: Mallikarjuna R Chilakala <mallikarjuna.chilakala@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ixgbe/ixgbe_type.h
+++ drivers/net/ixgbe/ixgbe_type.h
@@ -1266,8 +1266,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IXGBE_STATUS_LAN_ID_1   0x00000004 /* LAN ID 1 */
 
 /* ESDP Bit Masks */
-#define IXGBE_ESDP_SDP0 0x00000001
-#define IXGBE_ESDP_SDP1 0x00000002
+#define IXGBE_ESDP_SDP0 0x00000001 /* SDP0 Data Value */
+#define IXGBE_ESDP_SDP1 0x00000002 /* SDP1 Data Value */
+#define IXGBE_ESDP_SDP2 0x00000004 /* SDP2 Data Value */
+#define IXGBE_ESDP_SDP3 0x00000008 /* SDP3 Data Value */
 #define IXGBE_ESDP_SDP4 0x00000010 /* SDP4 Data Value */
 #define IXGBE_ESDP_SDP5 0x00000020 /* SDP5 Data Value */
 #define IXGBE_ESDP_SDP6 0x00000040 /* SDP6 Data Value */
@@ -2282,6 +2284,7 @@ struct ixgbe_mac_info {
 	bool                            orig_link_settings_stored;
 	bool                            autoneg;
 	bool                            autoneg_succeeded;
+	bool                            autotry_restart;
 };
 
 struct ixgbe_phy_info {
