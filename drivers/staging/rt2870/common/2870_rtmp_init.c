f3048a7d7be6c1fadc3a9de9c943d8e98552df1d
Greg Kroah-Hartman <gregkh@suse.de>
2009-06-04 11:29:54 -0700

    Staging: rt2870: fix build warnings
    This fixes some build warnings in the rt2870 driver.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2870/common/2870_rtmp_init.c
+++ drivers/staging/rt2870/common/2870_rtmp_init.c
@@ -764,7 +764,7 @@ NDIS_STATUS	 CreateThreads(
 	// Creat MLME Thread
 #ifndef RT30xx
 	pObj->MLMEThr_task = NULL;
-	tsk = kthread_run(MlmeThread, pAd, pAd->net_dev->name);
+	tsk = kthread_run(MlmeThread, pAd, "%s", pAd->net_dev->name);
 
 	if (IS_ERR(tsk)) {
 #endif
@@ -790,7 +790,7 @@ NDIS_STATUS	 CreateThreads(
 	// Creat Command Thread
 #ifndef RT30xx
 	pObj->RTUSBCmdThr_task = NULL;
-	tsk = kthread_run(RTUSBCmdThread, pAd, pAd->net_dev->name);
+	tsk = kthread_run(RTUSBCmdThread, pAd, "%s", pAd->net_dev->name);
 
 	if (IS_ERR(tsk) < 0)
 #endif
@@ -814,7 +814,7 @@ NDIS_STATUS	 CreateThreads(
 
 #ifndef RT30xx
 	pObj->TimerQThr_task = NULL;
-	tsk = kthread_run(TimerQThread, pAd, pAd->net_dev->name);
+	tsk = kthread_run(TimerQThread, pAd, "%s", pAd->net_dev->name);
 	if (IS_ERR(tsk) < 0)
 #endif
 #ifdef RT30xx
