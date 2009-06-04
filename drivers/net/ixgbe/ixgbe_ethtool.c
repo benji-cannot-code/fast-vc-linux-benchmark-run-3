42c8c0d600af68de7807b64f66a87be5441f30cd
Alexander Duyck <alexander.h.duyck@intel.com>
2009-06-04 16:00:09 +0000

    ixgbe: move v_idx into q_vector and use as index only
    The v_idx value was being used as both a bitmask and an index.  This change
    makes it so that the q_vector contains the index and allows for much of the
    code to be simplified since disabling a q_vector involves only clearing one
    bit in the interrupt bitmask.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ixgbe/ixgbe_ethtool.c
+++ drivers/net/ixgbe/ixgbe_ethtool.c
@@ -841,7 +841,6 @@ static int ixgbe_set_ringparam(struct net_device *netdev,
 				}
 				goto err_setup;
 			}
-			temp_tx_ring[i].v_idx = adapter->tx_ring[i].v_idx;
 		}
 		need_update = true;
 	}
@@ -871,7 +870,6 @@ static int ixgbe_set_ringparam(struct net_device *netdev,
 				}
 				goto err_setup;
 			}
-			temp_rx_ring[i].v_idx = adapter->rx_ring[i].v_idx;
 		}
 		need_update = true;
 	}
@@ -1988,8 +1986,7 @@ static int ixgbe_set_coalesce(struct net_device *netdev,
 		else
 			/* rx only or mixed */
 			q_vector->eitr = adapter->eitr_param;
-		ixgbe_write_eitr(adapter, i,
-		                 EITR_INTS_PER_SEC_TO_REG(q_vector->eitr));
+		ixgbe_write_eitr(q_vector);
 	}
 
 	return 0;
