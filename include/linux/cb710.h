68ea0608d86710cf6008dd56f28f65e774b31afa
Pierre Ossman <pierre@ossman.eu>
2009-06-04 08:00:40 +0200

    cb710: handle DEBUG define in Makefile
    Signed-off-by: Pierre Ossman <pierre@ossman.eu>

--- include/linux/cb710.h
+++ include/linux/cb710.h
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef LINUX_CB710_DRIVER_H
 #define LINUX_CB710_DRIVER_H
 
-#ifdef CONFIG_CB710_DEBUG
-#define DEBUG
-#endif
-
 /* verify assumptions on platform_device framework */
 #define CONFIG_CB710_DEBUG_ASSUMPTIONS
 
