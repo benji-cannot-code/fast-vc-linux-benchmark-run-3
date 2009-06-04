515cae296fa14d8e269facc079bd82a3afccad91
Rami Rosen <ramirose@gmail.com>
2009-06-03 21:43:26 -0700

    ipv4: remove ip_mc_drop_socket() declaration from af_inet.c.
    ip_mc_drop_socket() method is declared in linux/igmp.h, which
    is included anyhow in af_inet.c. So there is no need for this declaration.
    This patch removes it from af_inet.c.
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -117,7 +117,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mroute.h>
 #endif
 
-extern void ip_mc_drop_socket(struct sock *sk);
 
 /* The inetsw table contains everything that inet_create needs to
  * build a new socket.
