1c9ec9bb08e227dd2c09f453afa5ea42317c8ebf
Robert Richter <robert.richter@amd.com>
2009-06-04 02:36:44 +0200

    x86/oprofile: replace CTR*_IS_RESERVED macros
    The patch replaces all CTR*_IS_RESERVED macros.
    
    Signed-off-by: Robert Richter <robert.richter@amd.com>

--- arch/x86/oprofile/op_x86_model.h
+++ arch/x86/oprofile/op_x86_model.h
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/types.h>
 #include <asm/intel_arch_perfmon.h>
 
-#define CTR_IS_RESERVED(msrs, c)	((msrs)->counters[(c)].addr ? 1 : 0)
-#define CTRL_IS_RESERVED(msrs, c)	((msrs)->controls[(c)].addr ? 1 : 0)
-
 struct op_saved_msr {
 	unsigned int high;
 	unsigned int low;
