591241cdb89b42e0940e00081d6686e4fcfe317a
Ingo Molnar <mingo@elte.hu>
2009-06-06 21:17:03 +0200

    perf annotate: Automatically pick up vmlinux in the local directory
    Right now kernel debug info does not get resolved by default, because
    we dont know where to look for the vmlinux.
    
    The -k option can be used for that - but if no option is given, pick
    up vmlinux files in the current directory - in case a kernel hacker
    runs profiling from the source directory that the kernel was built in.
    
    The real solution would be to embedd the location (and perhaps the
    date/timestamp) of the vmlinux file in /proc/kallsyms, so that
    tools can pick it up automatically.
    
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    LKML-Reference: <new-submission>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/builtin-annotate.c
+++ tools/perf/builtin-annotate.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SHOW_HV		4
 
 static char		const *input_name = "perf.data";
-static char		*vmlinux = NULL;
+static char		*vmlinux = "vmlinux";
 
 static char		default_sort_order[] = "comm,symbol";
 static char		*sort_order = default_sort_order;
