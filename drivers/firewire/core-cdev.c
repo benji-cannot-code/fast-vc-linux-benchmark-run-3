bc76372cce067c5e71bbcbd32f36bd421dad10c9
Stefan Richter <stefanr@s5r6.in-berlin.de>
2009-06-06 18:36:24 +0200

    firewire: core: include linux/uaccess.h instead of asm/uaccess.h
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/core-cdev.c
+++ drivers/firewire/core-cdev.c
@@ -36,12 +36,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/preempt.h>
 #include <linux/spinlock.h>
 #include <linux/time.h>
+#include <linux/uaccess.h>
 #include <linux/vmalloc.h>
 #include <linux/wait.h>
 #include <linux/workqueue.h>
 
 #include <asm/system.h>
-#include <asm/uaccess.h>
 
 #include "core.h"
 
