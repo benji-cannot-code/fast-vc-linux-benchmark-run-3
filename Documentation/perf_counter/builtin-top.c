6f7215a900a0ad68699f98d61e9fbc2797b6f629
Thomas Gleixner <tglx@linutronix.de>
2009-06-06 12:24:17 +0200

    perf_counter tools: Add 'perf list' to list available events
    perf list: List all the available event types which can be used in
    -e (--event) options.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- Documentation/perf_counter/builtin-top.c
+++ Documentation/perf_counter/builtin-top.c
@@ -607,11 +607,10 @@ static const char * const top_usage[] = {
 	NULL
 };
 
-static char events_help_msg[EVENTS_HELP_MAX];
-
 static const struct option options[] = {
 	OPT_CALLBACK('e', "event", NULL, "event",
-		     events_help_msg, parse_events),
+		     "event selector. use 'perf list' to list available events",
+		     parse_events),
 	OPT_INTEGER('c', "count", &default_interval,
 		    "event period to sample"),
 	OPT_INTEGER('p', "pid", &target_pid,
@@ -648,8 +647,6 @@ int cmd_top(int argc, const char **argv, const char *prefix)
 	int counter;
 
 	page_size = sysconf(_SC_PAGE_SIZE);
-
-	create_events_help(events_help_msg);
 
 	argc = parse_options(argc, argv, options, top_usage, 0);
 	if (argc)
