3670ffcf37f9b11a68d5161bd330dc9f37dfa01d
Takashi Iwai <tiwai@suse.de>
2009-06-09 08:19:02 +0200

    ALSA: ctxfi - Clear PCM resources at hw_params and hw_free
    Currently the PCM resources are allocated only once and ever in prepare
    callback, assuming that the PCM parameters are never changed.  But it's
    not true.
    
    This patch adds the call of atc->pcm_release_resources() at hw_params
    and hw_free callbacks to assure that the PCM setup is done correctly
    for each h/w parameter changes.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ctxfi/ctatc.c
+++ sound/pci/ctxfi/ctatc.c
@@ -524,7 +524,7 @@ atc_pcm_capture_get_resources(struct ct_atc *atc, struct ct_atc_pcm *apcm)
 	struct src_node_conf_t src_node_conf[2] = {{0} };
 
 	/* first release old resources */
-	atc->pcm_release_resources(atc, apcm);
+	atc_pcm_release_resources(atc, apcm);
 
 	/* The numbers of converting SRCs and SRCIMPs should be determined
 	 * by pitch value. */
@@ -803,7 +803,7 @@ static int spdif_passthru_playback_get_resources(struct ct_atc *atc,
 	unsigned int pitch, rsr = atc->pll_rate;
 
 	/* first release old resources */
-	atc->pcm_release_resources(atc, apcm);
+	atc_pcm_release_resources(atc, apcm);
 
 	/* Get SRC resource */
 	desc.multi = apcm->substream->runtime->channels;
