47562ee4664600b39de6cb691b7792a41a387bdd
Kevin Cernekee <kpc.mtd@gmail.com>
2009-06-09 10:59:19 -0700

    UBI: add reboot notifier
    Terminate the UBI background thread prior to restarting the system.
    
    [Artem: amended comments a little]
    
    Signed-off-by: Kevin Cernekee <kpc.mtd@gmail.com>
    Tested-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -37,9 +37,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/string.h>
 #include <linux/vmalloc.h>
+#include <linux/notifier.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/ubi.h>
-#include <linux/notifier.h>
 
 #include "ubi-media.h"
 #include "scan.h"
@@ -349,6 +349,7 @@ struct ubi_wl_entry;
  * @bgt_thread: background thread description object
  * @thread_enabled: if the background thread is enabled
  * @bgt_name: background thread name
+ * @reboot_notifier: notifier to terminate background thread before rebooting
  *
  * @flash_size: underlying MTD device size (in bytes)
  * @peb_count: count of physical eraseblocks on the MTD device
@@ -432,6 +433,7 @@ struct ubi_device {
 	struct task_struct *bgt_thread;
 	int thread_enabled;
 	char bgt_name[sizeof(UBI_BGT_NAME_PATTERN)+2];
+	struct notifier_block reboot_notifier;
 
 	/* I/O sub-system's stuff */
 	long long flash_size;
