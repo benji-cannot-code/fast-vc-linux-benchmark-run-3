603f652d06590578585dccfb72987a7942ca05a7
john cooper <john.cooper@redhat.com>
2009-06-09 14:41:40 +0200

    Add serial number support for virtio_blk, V4a
    This patch extracts the opaque data from pci i/o
    region 0 via the added VIRTIO_BLK_F_IDENTIFY
    field.  By convention this data takes the form of
    that returned by an ATA IDENTIFY DEVICE command,
    however the driver (except for structure size)
    makes no interpretation of the data.  The structure
    data is copied wholesale to userspace via a
    HDIO_GET_IDENTITY ioctl command (eg: hdparm -i <dev>).
    
    Signed-off-by: john cooper <john.cooper@redhat.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- include/linux/virtio_blk.h
+++ include/linux/virtio_blk.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VIRTIO_BLK_F_RO		5	/* Disk is read-only */
 #define VIRTIO_BLK_F_BLK_SIZE	6	/* Block size of disk is available*/
 #define VIRTIO_BLK_F_SCSI	7	/* Supports scsi command passthru */
+#define VIRTIO_BLK_F_IDENTIFY	8	/* ATA IDENTIFY supported */
+
+#define VIRTIO_BLK_ID_BYTES	(sizeof(__u16[256]))	/* IDENTIFY DATA */
 
 struct virtio_blk_config
 {
@@ -34,6 +37,7 @@ struct virtio_blk_config
 	} geometry;
 	/* block size of device (if VIRTIO_BLK_F_BLK_SIZE) */
 	__u32 blk_size;
+	__u8 identify[VIRTIO_BLK_ID_BYTES];
 } __attribute__((packed));
 
 /* These two define direction. */
