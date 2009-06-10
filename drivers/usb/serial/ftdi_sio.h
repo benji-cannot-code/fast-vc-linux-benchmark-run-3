f3fe08b5314be14a2dce0c22a3427f999b1b55e4
Jan Capek <jan@ccsinfo.com>
2009-06-10 18:58:52 +0200

    USB: ftdi_sio - product ID's for CCS PIC programmers
    The product ID's for the following devices have been added:
    - LOAD-n-GO
    - ICD-U64
    - PRIME-8
    
    Signed-off-by: Jan Capek <jan@ccsinfo.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -615,6 +615,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FTDI_CCSICDU20_0_PID    0xF9D0
 #define FTDI_CCSICDU40_1_PID    0xF9D1
 #define FTDI_CCSMACHX_2_PID     0xF9D2
+#define FTDI_CCSLOAD_N_GO_3_PID 0xF9D3
+#define FTDI_CCSICDU64_4_PID    0xF9D4
+#define FTDI_CCSPRIME8_5_PID    0xF9D5
 
 /* Inside Accesso contactless reader (http://www.insidefr.com) */
 #define INSIDE_ACCESSO		0xFAD0
