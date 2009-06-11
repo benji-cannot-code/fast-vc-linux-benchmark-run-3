9ac59de23baa4d0d5eadfa054451db7038a2bbbc
Alexey Klimov <klimov.linux@gmail.com>
2009-06-11 00:17:06 -0300

    V4L/DVB (11954): dsbr100: remove radio->users counter
    Patch removes radio->users counter because it is not in use.
    
    Signed-off-by: Alexey Klimov <klimov.linux@gmail.com>
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/radio/dsbr100.c
+++ drivers/media/radio/dsbr100.c
@@ -146,7 +146,6 @@ struct dsbr100_device {
 	struct mutex lock;	/* buffer locking */
 	int curfreq;
 	int stereo;
-	int users;
 	int removed;
 	int muted;
 };
@@ -550,14 +549,12 @@ static int usb_dsbr100_open(struct file *file)
 	int retval;
 
 	lock_kernel();
-	radio->users = 1;
 	radio->muted = 1;
 
 	retval = dsbr100_start(radio);
 	if (retval < 0) {
 		dev_warn(&radio->usbdev->dev,
 			 "Radio did not start up properly\n");
-		radio->users = 0;
 		unlock_kernel();
 		return -EIO;
 	}
@@ -578,10 +575,6 @@ static int usb_dsbr100_close(struct file *file)
 
 	if (!radio)
 		return -ENODEV;
-
-	mutex_lock(&radio->lock);
-	radio->users = 0;
-	mutex_unlock(&radio->lock);
 
 	if (!radio->removed) {
 		retval = dsbr100_stop(radio);
@@ -696,7 +689,6 @@ static int usb_dsbr100_probe(struct usb_interface *intf,
 	mutex_init(&radio->lock);
 
 	radio->removed = 0;
-	radio->users = 0;
 	radio->usbdev = interface_to_usbdev(intf);
 	radio->curfreq = FREQ_MIN * FREQ_MUL;
 
