d6009976b33bf1fb6ccdca38ec3aa5d0e72e4356
Chris Mason <chris.mason@oracle.com>
2009-06-11 09:30:13 -0400

    Btrfs: fix -o nodatasum printk spelling
    It was printing nodatacsum, which was not the correct option name.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -160,7 +160,7 @@ int btrfs_parse_options(struct btrfs_root *root, char *options)
 			 */
 			break;
 		case Opt_nodatasum:
-			printk(KERN_INFO "btrfs: setting nodatacsum\n");
+			printk(KERN_INFO "btrfs: setting nodatasum\n");
 			btrfs_set_opt(info->mount_opt, NODATASUM);
 			break;
 		case Opt_nodatacow:
