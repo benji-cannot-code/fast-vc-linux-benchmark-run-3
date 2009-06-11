84ebdf8b9fb15c7cbadb9d3a1a0ca958c8a63610
Trent Piepho <xyzzy@speakeasy.org>
2009-06-11 19:21:34 -0300

    V4L/DVB (12289): dvb-pll: Add support for Samsung TBMU24112 DVB-S NIM
    Tuner parameters determined from code in flexcop driver.  That code rounded
    the divisor down instead of to the nearest value.  This was probably not
    intentional and the dvb-pll version will round to nearest.
    
    Signed-off-by: Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/dvb-pll.h
+++ drivers/media/dvb/frontends/dvb-pll.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_SAMSUNG_DTOS403IH102A  14
 #define DVB_PLL_SAMSUNG_TDTC9251DH0    15
 #define DVB_PLL_SAMSUNG_TBDU18132      16
+#define DVB_PLL_SAMSUNG_TBMU24112      17
 
 /**
  * Attach a dvb-pll to the supplied frontend structure.
