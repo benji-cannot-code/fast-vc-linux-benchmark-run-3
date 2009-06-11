a01072116b6066aa1d9d2520a9e1c196c7e0a7b2
Paul Mundt <lethal@linux-sh.org>
2009-06-12 01:33:22 +0300

    sh: Wire up sys_perf_counter_open.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/unistd_64.h
+++ arch/sh/include/asm/unistd_64.h
@@ -385,10 +385,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_preadv		361
 #define __NR_pwritev		362
 #define __NR_rt_tgsigqueueinfo	363
+#define __NR_perf_counter_open	364
 
 #ifdef __KERNEL__
 
-#define NR_syscalls 364
+#define NR_syscalls 365
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
