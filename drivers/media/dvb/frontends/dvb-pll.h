14bd68cd275376c863d667351992c04575376ca1
Trent Piepho <xyzzy@speakeasy.org>
2009-06-11 19:21:34 -0300

    V4L/DVB (12288): dvb-pll: Add support for Samsung TBDU18132 DVB-S NIM
    Tuner parameters taken from flexcop driver.  This PLL has a 17 bit divisor
    while the dvb-pll driver is designed for 15 bit divisors.  It's not a
    problem as 15 bits is enough for the tuner's entire range.  But if a larger
    range was wanted, it could be done by adding additional bands with the
    extra divisor bits appearing as band switch bits.
    
    Signed-off-by: Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/dvb-pll.h
+++ drivers/media/dvb/frontends/dvb-pll.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_OPERA1                 13
 #define DVB_PLL_SAMSUNG_DTOS403IH102A  14
 #define DVB_PLL_SAMSUNG_TDTC9251DH0    15
+#define DVB_PLL_SAMSUNG_TBDU18132      16
 
 /**
  * Attach a dvb-pll to the supplied frontend structure.
