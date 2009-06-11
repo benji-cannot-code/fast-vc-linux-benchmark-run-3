37d5794ba9227e538e0f6d6cad23f8a018cd675c
Karsten Keil <keil@b1-systems.de>
2009-06-11 15:52:10 +0200

    mISDN: cleanup mISDNhw.h
    Remove unused stuff.
    
    Signed-off-by: Karsten Keil <keil@b1-systems.de>

--- include/linux/mISDNhw.h
+++ include/linux/mISDNhw.h
@@ -90,11 +90,6 @@ struct dchannel {
 	void			(*phfunc) (struct dchannel *);
 	u_int			state;
 	void			*l1;
-	/* HW access */
-	u_char			(*read_reg) (void *, u_char);
-	void			(*write_reg) (void *, u_char, u_char);
-	void			(*read_fifo) (void *, u_char *, int);
-	void			(*write_fifo) (void *, u_char *, int);
 	void			*hw;
 	int			slot;	/* multiport card channel slot */
 	struct timer_list	timer;
@@ -152,11 +147,6 @@ struct bchannel {
 	u_long			Flags;
 	struct work_struct	workq;
 	u_int			state;
-	/* HW access */
-	u_char			(*read_reg) (void *, u_char);
-	void			(*write_reg) (void *, u_char, u_char);
-	void			(*read_fifo) (void *, u_char *, int);
-	void			(*write_fifo) (void *, u_char *, int);
 	void			*hw;
 	int			slot;	/* multiport card channel slot */
 	struct timer_list	timer;
