ed67521f422b8973b525576084ceca5674bf6bec
Matt Fleming <matt@console-pimps.org>
2009-06-11 09:26:43 +0100

    sh: Update my email address
    Use my current email address as my gentoo account will be closed at
    some point.
    
    Signed-off-by: Matt Fleming <matt@console-pimps.org>

--- arch/sh/kernel/ftrace.c
+++ arch/sh/kernel/ftrace.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (C) 2008 Matt Fleming <mjf@gentoo.org>
+ * Copyright (C) 2008 Matt Fleming <matt@console-pimps.org>
  * Copyright (C) 2008 Paul Mundt <lethal@linux-sh.org>
  *
  * Code for replacing ftrace calls with jumps.
