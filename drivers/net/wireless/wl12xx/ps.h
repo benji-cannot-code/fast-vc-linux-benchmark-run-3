1132561dda93a5b838e1e71654a41b5410c09dd0
Kalle Valo <kalle.valo@nokia.com>
2009-06-12 14:14:19 +0300

    wl12xx: cmd and acx interface rework
    Rework cmd and acx interfaces, it was just too confusing earlier. Now
    all commands need to contain all the needed headers, either just cmd
    headers or both cmd and acx headers. This accomplish to remove the
    extra copy done for each command. The interfaces are now properly
    documented as well.
    
    Also try to make all commands safe for DMA transfers. I might have missed
    some, but most of them should be fixed now.
    
    And this is not all! As a free bonus you will also get some cosmetic
    cleanups and code reorganisation. Order today!
    
    Signed-off-by: Kalle Valo <kalle.valo@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/ps.h
+++ drivers/net/wireless/wl12xx/ps.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wl12xx.h"
 #include "acx.h"
 
-int wl12xx_ps_set_mode(struct wl12xx *wl, enum acx_ps_mode mode);
+int wl12xx_ps_set_mode(struct wl12xx *wl, enum wl12xx_cmd_ps_mode mode);
 void wl12xx_ps_elp_sleep(struct wl12xx *wl);
 int wl12xx_ps_elp_wakeup(struct wl12xx *wl);
 
