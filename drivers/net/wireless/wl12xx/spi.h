813c3a7e4f8e6d38a9c7d0b7e58323f840008e72
Kalle Valo <kalle.valo@nokia.com>
2009-06-12 14:14:55 +0300

    wl12xx: allocate buffer the spi busy word from struct wl12xx
    Needed for DMA transfers.
    
    Signed-off-by: Kalle Valo <kalle.valo@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/spi.h
+++ drivers/net/wireless/wl12xx/spi.h
@@ -66,9 +66,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define WSPI_INIT_CMD_LEN           8
 
-#define TNETWIF_READ_OFFSET_BYTES  8
 #define HW_ACCESS_WSPI_FIXED_BUSY_LEN \
-		((TNETWIF_READ_OFFSET_BYTES - 4) / sizeof(u32))
+		((WL12XX_BUSY_WORD_LEN - 4) / sizeof(u32))
 #define HW_ACCESS_WSPI_INIT_CMD_MASK  0
 
 
