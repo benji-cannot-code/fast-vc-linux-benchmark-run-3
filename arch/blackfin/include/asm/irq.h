d6b29a40e8ea6d27005b5afd1e35c89061bb0390
Mike Frysinger <vapier@gentoo.org>
2009-06-13 11:32:34 -0400

    Blackfin: convert irq/process to asm-generic
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/include/asm/irq.h
+++ arch/blackfin/include/asm/irq.h
@@ -46,9 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		: "d" (bfin_irq_flags) \
 	)
 
-static inline int irq_canonicalize(int irq)
-{
-	return irq;
-}
+#include <asm-generic/irq.h>
 
 #endif				/* _BFIN_IRQ_H_ */
