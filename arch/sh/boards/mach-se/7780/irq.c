80308b08ff52a4f24055263cf47e90b05a96623c
Huang Weiyi <weiyi.huang@gmail.com>
2009-06-13 12:21:11 +0000

    sh: pci: remove duplicated #include's
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-se/7780/irq.c
+++ arch/sh/boards/mach-se/7780/irq.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
-#include <linux/irq.h>
 #include <linux/io.h>
 #include <mach-se/mach/se7780.h>
 
