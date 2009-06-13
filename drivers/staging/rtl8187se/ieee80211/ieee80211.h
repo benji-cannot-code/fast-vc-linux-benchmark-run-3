5ae7a3ce321088edb82ed8a546594683493d9481
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-06-13 18:35:00 +0200

    Staging: rtl8187se/ieee80211: remove superfluous JOHN_* ifdefs
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8187se/ieee80211/ieee80211.h
+++ drivers/staging/rtl8187se/ieee80211/ieee80211.h
@@ -46,14 +46,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 */
 
-//#ifdef JOHN_HWSEC
 #define KEY_TYPE_NA		0x0
 #define KEY_TYPE_WEP40 		0x1
 #define KEY_TYPE_TKIP		0x2
 #define KEY_TYPE_CCMP		0x4
 #define KEY_TYPE_WEP104		0x5
-//#endif
-
 
 #define aSifsTime					10
 
