9617134e23090a842d815a6b2a7d2c1e8bcd3c2b
Paul Mundt <lethal@linux-sh.org>
2009-06-14 23:27:41 +0900

    sh: Derive COMMAND_LINE_SIZE from asm-generic/setup.h.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/setup.h
+++ arch/sh/include/asm/setup.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SH_SETUP_H
 #define _SH_SETUP_H
 
-#define COMMAND_LINE_SIZE 256
+#include <asm-generic/setup.h>
 
 #ifdef __KERNEL__
 /*
