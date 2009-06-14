e37fa29449d4c47e5c9e4d9f6ee5dab8abad5308
Paul Mundt <lethal@linux-sh.org>
2009-06-15 00:00:42 +0900

    sh: Convert sh64 to use the generic checksum code.
    This plugs in GENERIC_CSUM support on sh64, and kills off all of the old
    references.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/checksum.h
+++ arch/sh/include/asm/checksum.h
@@ -2,5 +2,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_SUPERH32
 # include "checksum_32.h"
 #else
-# include "checksum_64.h"
+# include <asm-generic/checksum.h>
 #endif
