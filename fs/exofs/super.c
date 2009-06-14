d1e77c475cfef3d5af9d90c3894425fe35fc88e0
Boaz Harrosh <bharrosh@panasas.com>
2009-06-14 16:52:10 +0300

    exofs: Avoid using file_fsync()
    The use of file_fsync() in exofs_file_sync() is not necessary since it
    does some extra stuff not used by exofs. Open code just the parts that
    are currently needed.
    
    TODO: Farther optimization can be done to sync the sb only on inode
    update of new files, Usually the sb update is not needed in exofs.
    
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- fs/exofs/super.c
+++ fs/exofs/super.c
@@ -199,7 +199,7 @@ static const struct export_operations exofs_export_ops;
 /*
  * Write the superblock to the OSD
  */
-static int exofs_sync_fs(struct super_block *sb, int wait)
+int exofs_sync_fs(struct super_block *sb, int wait)
 {
 	struct exofs_sb_info *sbi;
 	struct exofs_fscb *fscb;
