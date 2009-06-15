ddac56d854268708f7fcb3e6272e1dbae77b8a6c
Tony Luck <tony.luck@intel.com>
2009-06-15 16:22:09 -0700

    [IA64] ia64 does not need umount2() syscall
    ia64 doesn't have old and new versions of the umount system call.
    It just has the new version.
    
    Fixes this build warning:
    <stdin>:395:2: warning: #warning syscall umount2 not implemented
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/include/asm/unistd.h
+++ arch/ia64/include/asm/unistd.h
@@ -330,6 +330,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __IGNORE_utime		/* utimes() */
 #define __IGNORE_getpgrp	/* getpgid() */
 #define __IGNORE_vfork		/* clone() */
+#define __IGNORE_umount2	/* umount() */
 
 #define __ARCH_WANT_SYS_RT_SIGACTION
 #define __ARCH_WANT_SYS_RT_SIGSUSPEND
