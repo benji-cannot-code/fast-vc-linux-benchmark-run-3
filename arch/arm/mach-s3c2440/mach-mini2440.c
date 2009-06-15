febbdd05df1c396a4f387abe7fecae5fc126b6b4
Ben Dooks <ben-linux@fluff.org>
2009-06-15 22:07:48 +0100

    [ARM] MINI2440: Cleanup duplicated header includes
    Remove duplicate mach headers for gpio.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c2440/mach-mini2440.c
+++ arch/arm/mach-s3c2440/mach-mini2440.c
@@ -50,9 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/udc.h>
 
 #include <plat/regs-serial.h>
-#include <mach/regs-gpio.h>
-#include <mach/regs-mem.h>
-#include <mach/regs-lcd.h>
 
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/nand.h>
