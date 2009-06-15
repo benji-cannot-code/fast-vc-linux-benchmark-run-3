15d81b2a42091d67aed2296468e6b9b42464b5b5
Paul Mackerras <paulus@samba.org>
2009-06-15 21:45:16 +1000

    perf_counter: Make set_perf_counter_pending() declaration common
    At present, every architecture that supports perf_counters has to
    declare set_perf_counter_pending() in its arch-specific headers.
    This consolidates the declarations into a single declaration in one
    common place, include/linux/perf_counter.h.  On powerpc, we continue
    to provide a static inline definition of set_perf_counter_pending()
    in the powerpc hw_irq.h.
    
    Also, this removes from the x86 perf_counter.h the unused null
    definitions of {test,clear}_perf_counter_pending.
    
    Reported-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: benh@kernel.crashing.org
    LKML-Reference: <18998.13388.920691.523227@cargo.ozlabs.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/powerpc/include/asm/perf_counter.h
+++ arch/powerpc/include/asm/perf_counter.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/types.h>
 
+#include <asm/hw_irq.h>
+
 #define MAX_HWCOUNTERS		8
 #define MAX_EVENT_ALTERNATIVES	8
 #define MAX_LIMITED_HWCOUNTERS	2
