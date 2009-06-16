a3437522d10e892ebe37d764fd9fd3d08a7851ea
Li Zefan <lizf@cn.fujitsu.com>
2009-06-16 11:19:36 +0200

    block: remove some includings of blktrace_api.h
    When porting blktrace to tracepoints, we changed to trace/block.h
    for trace prober declarations.
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/swap.h>
 #include <linux/writeback.h>
 #include <linux/task_io_accounting_ops.h>
-#include <linux/blktrace_api.h>
 #include <linux/fault-inject.h>
 
 #define CREATE_TRACE_POINTS
