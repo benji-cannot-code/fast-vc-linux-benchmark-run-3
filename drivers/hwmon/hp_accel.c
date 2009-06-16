02835ee342c30dccd3da7bc7b4634dbf61e710ea
Eric Piel <eric.piel@tremplin-utc.net>
2009-06-16 15:34:15 -0700

    lis3: use input_polled_device
    Now that there is no need to hookup on the open/close of the joystick,
    it's possible to use the simplified interface input_polled_device, instead
    of creating our own kthread.
    
    [randy.dunlap@oracle.com: fix Kconfig]
    [randy.dunlap@oracle.com: fix Kconfig some more]
    Signed-off-by: Eric Piel <eric.piel@tremplin-utc.net>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/hwmon/hp_accel.c
+++ drivers/hwmon/hp_accel.c
@@ -28,9 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/platform_device.h>
 #include <linux/interrupt.h>
-#include <linux/input.h>
-#include <linux/kthread.h>
-#include <linux/semaphore.h>
 #include <linux/delay.h>
 #include <linux/wait.h>
 #include <linux/poll.h>
--- drivers/hwmon/lis3lv02d.h
+++ drivers/hwmon/lis3lv02d.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#include <linux/platform_device.h>
+#include <linux/input-polldev.h>
 
 /*
  * The actual chip is STMicroelectronics LIS3LV02DL or LIS3LV02DQ that seems to
@@ -170,8 +172,7 @@ struct lis3lv02d {
 	s16 (*read_data) (struct lis3lv02d *lis3, int reg);
 	int			mdps_max_val;
 
-	struct input_dev	*idev;     /* input device */
-	struct task_struct	*kthread;  /* kthread for input */
+	struct input_polled_dev	*idev;     /* input device */
 	struct platform_device	*pdev;     /* platform device */
 	atomic_t		count;     /* interrupt count after last read */
 	int			xcalib;    /* calibrated null value for x */
