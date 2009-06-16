12d2cf7b1cfd458d4d3fac0dcd5a8675d1a94b13
Chris Wright <chrisw@sous-sol.org>
2009-06-16 03:01:37 -0400

    amd-iommu: resume cleanup
    Now that enable_iommus() will call iommu_disable() for each iommu,
    the call to disable_iommus() during resume is redundant.  Also, the order
    for an invalidation is to invalidate device table entries first, then
    domain translations.
    
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- arch/x86/kernel/amd_iommu_init.c
+++ arch/x86/kernel/amd_iommu_init.c
@@ -1080,12 +1080,6 @@ static void disable_iommus(void)
 
 static int amd_iommu_resume(struct sys_device *dev)
 {
-	/*
-	 * Disable IOMMUs before reprogramming the hardware registers.
-	 * IOMMU is still enabled from the resume kernel.
-	 */
-	disable_iommus();
-
 	/* re-load the hardware */
 	enable_iommus();
 
@@ -1093,8 +1087,8 @@ static int amd_iommu_resume(struct sys_device *dev)
 	 * we have to flush after the IOMMUs are enabled because a
 	 * disabled IOMMU will never execute the commands we send
 	 */
-	amd_iommu_flush_all_domains();
 	amd_iommu_flush_all_devices();
+	amd_iommu_flush_all_domains();
 
 	return 0;
 }
