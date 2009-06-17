b51dda2a045983a12aab3d55f73b752f288ca265
Sukanto Ghosh <sukanto.cse.iitb@gmail.com>
2009-06-17 16:27:50 -0700

    sysctl.c: remove unused variable
    Remoce the unused variable 'val' from __do_proc_dointvec()
    
    The integer has been declared and used as 'val = -val' and there is no
    reference to it anywhere.
    
    Signed-off-by: Sukanto Ghosh <sukanto.cse.iitb@gmail.com>
    Cc: Jaswinder Singh Rajput <jaswinder@kernel.org>
    Cc: Sukanto Ghosh <sukanto.cse.iitb@gmail.com>
    Cc: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2284,7 +2284,7 @@ static int __do_proc_dointvec(void *tbl_data, struct ctl_table *table,
 		  void *data)
 {
 #define TMPBUFLEN 21
-	int *i, vleft, first=1, neg, val;
+	int *i, vleft, first = 1, neg;
 	unsigned long lval;
 	size_t left, len;
 	
@@ -2337,8 +2337,6 @@ static int __do_proc_dointvec(void *tbl_data, struct ctl_table *table,
 			len = p-buf;
 			if ((len < left) && *p && !isspace(*p))
 				break;
-			if (neg)
-				val = -val;
 			s += len;
 			left -= len;
 
