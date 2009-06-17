45548bf474885be932000c8c05cd43ca874694b6
Johannes Berg <johannes@sipsolutions.net>
2009-06-17 17:43:30 +0200

    net: export __dev_addr_sync/__dev_addr_unsync
    For mac80211, with the master netdev removal, we need to be
    able to sync a multicast address list onto another list that
    is not tracked within a netdev, so we need access to the
    functions doing that.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -3924,6 +3924,7 @@ int __dev_addr_sync(struct dev_addr_list **to, int *to_count,
 	}
 	return err;
 }
+EXPORT_SYMBOL_GPL(__dev_addr_sync);
 
 void __dev_addr_unsync(struct dev_addr_list **to, int *to_count,
 		       struct dev_addr_list **from, int *from_count)
@@ -3943,6 +3944,7 @@ void __dev_addr_unsync(struct dev_addr_list **to, int *to_count,
 		da = next;
 	}
 }
+EXPORT_SYMBOL_GPL(__dev_addr_unsync);
 
 /**
  *	dev_unicast_sync - Synchronize device's unicast list to another device
