ca68b7cb2116856a2cb95c07677961c57b55d49e
Wolfgang Denk <wd@denx.de>
2009-06-17 00:30:22 -0600

    powerpc/5xxx: Add common mpc5xxx_get_bus_frequency() function
    So far, MPC512x used mpc512x_find_ips_freq() to get the bus frequency,
    while MPC52xx used mpc52xx_find_ipb_freq().  Despite the different
    clock names (IPS vs. IPB) the code was identical.
    
    Use common code for both processor families.
    
    Signed-off-by: Wolfgang Denk <wd@denx.de>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/powerpc/platforms/512x/clock.c
+++ arch/powerpc/platforms/512x/clock.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 
 #include <linux/of_platform.h>
-#include <asm/mpc512x.h>
+#include <asm/mpc5xxx.h>
 #include <asm/clk_interface.h>
 
 #undef CLK_DEBUG
