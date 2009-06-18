3db8a5e972caf8d2cb8ad8087376942f672d9c5b
Baruch Siach <baruch@tkos.co.il>
2009-06-18 16:48:58 -0700

    gpio: driver for PrimeCell PL061 GPIO controller
    This is a driver for the ARM PrimeCell PL061 GPIO AMBA peripheral.  The
    driver is implemented using the gpiolib framework.
    
    This driver also includes support for the use of the PL061 as an interrupt
    controller (secondary).
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Cc: David Brownell <david-b@pacbell.net>
    Acked-by: Russell King <rmk@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ include/linux/amba/pl061.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* platform data for the PL061 GPIO driver */
+
+struct pl061_platform_data {
+	/* number of the first GPIO */
+	unsigned	gpio_base;
+
+	/* number of the first IRQ.
+	 * If the IRQ functionality in not desired this must be set to
+	 * (unsigned) -1.
+	 */
+	unsigned	irq_base;
+
+	u8		directions;	/* startup directions, 1: out, 0: in */
+	u8		values;		/* startup values */
+};
