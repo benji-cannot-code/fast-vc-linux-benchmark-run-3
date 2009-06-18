5e2a5a326ae1ff01e7fe002fbd0649005f22f034
Arnd Bergmann <arnd@arndb.de>
2009-06-18 21:48:16 +0200

    x86: add copies of some headers to convert to asm-generic
    Just an intermediate step to make reviewing easier.
    These files are identical copies of the existing headers.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    LKML-Reference: <cover.1245354003.git.arnd@arndb.de>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ arch/x86/include/asm/generic-types.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _ASM_X86_TYPES_H
+#define _ASM_X86_TYPES_H
+
+#include <asm-generic/int-ll64.h>
+
+#ifndef __ASSEMBLY__
+
+typedef unsigned short umode_t;
+
+#endif /* __ASSEMBLY__ */
+
+/*
+ * These aren't exported outside the kernel to avoid name space clashes
+ */
+#ifdef __KERNEL__
+
+#ifndef __ASSEMBLY__
+
+typedef u64 dma64_addr_t;
+#if defined(CONFIG_X86_64) || defined(CONFIG_HIGHMEM64G)
+/* DMA addresses come in 32-bit and 64-bit flavours. */
+typedef u64 dma_addr_t;
+#else
+typedef u32 dma_addr_t;
+#endif
+
+#endif /* __ASSEMBLY__ */
+#endif /* __KERNEL__ */
+
+#endif /* _ASM_X86_TYPES_H */
