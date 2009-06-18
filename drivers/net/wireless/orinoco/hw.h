3ce905f3a39e65f4df8e5dfa2a7b0353064a2738
David Kilroy <kilroyd@googlemail.com>
2009-06-18 23:21:33 +0100

    orinoco: convert scanning to cfg80211
    This removes the custom scan cache used by orinoco.
    
    We also have to avoid calling cfg80211_scan_done from the hard
    interrupt, so we offload the entirety of scan processing to a workqueue.
    
    This may behave strangely if you start scanning just prior to
    suspending...
    
    Signed-off-by: David Kilroy <kilroyd@googlemail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/orinoco/hw.h
+++ drivers/net/wireless/orinoco/hw.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <linux/wireless.h>
+#include <net/cfg80211.h>
 
 /* Hardware BAPs */
 #define USER_BAP 0
@@ -48,5 +49,7 @@ int orinoco_hw_get_essid(struct orinoco_private *priv, int *active,
 int orinoco_hw_get_freq(struct orinoco_private *priv);
 int orinoco_hw_get_bitratelist(struct orinoco_private *priv,
 			       int *numrates, s32 *rates, int max);
+int orinoco_hw_trigger_scan(struct orinoco_private *priv,
+			    const struct cfg80211_ssid *ssid);
 
 #endif /* _ORINOCO_HW_H_ */
