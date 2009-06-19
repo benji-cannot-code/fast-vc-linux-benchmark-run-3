92680f51d34bdb2682cd8288e7c0a99013da327f
Ralf Baechle <ralf@linux-mips.org>
2009-06-19 14:05:26 +0100

    MIPS: Build fix - include <linux/smp.h> into all smp_processor_id() users.
    Some of the were relying into smp.h being dragged in by another header
    which of course is fragile.  <asm/cpu-info.h> uses smp_processor_id()
    only in macros and including smp.h there leads to an include loop, so
    don't change cpu-info.h.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/sgi-ip27/ip27-irq.c
+++ arch/mips/sgi-ip27/ip27-irq.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioport.h>
 #include <linux/timex.h>
 #include <linux/slab.h>
+#include <linux/smp.h>
 #include <linux/random.h>
 #include <linux/kernel.h>
 #include <linux/kernel_stat.h>
