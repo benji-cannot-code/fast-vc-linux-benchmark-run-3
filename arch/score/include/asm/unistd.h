c7b4a0362f539f894f8f54b0baf0397c63694a2e
Arnd Bergmann <arnd@arndb.de>
2009-06-19 11:11:55 +0200

    score: unset __ARCH_WANT_IPC_PARSE_VERSION
    This really should not be needed. The change for not changing
    the IPC code for every new architecture just went into 2.6.31,
    so we can skip it now.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/score/include/asm/unistd.h
+++ arch/score/include/asm/unistd.h
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_SCORE_UNISTD_H
 
 #define __ARCH_HAVE_MMU
-#define __ARCH_WANT_IPC_PARSE_VERSION
 
 #include <asm-generic/unistd.h>
 
