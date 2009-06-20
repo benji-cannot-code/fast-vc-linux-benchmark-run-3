8450769611a4bb51b44c4a59ada203dd69277383
Frederic Weisbecker <fweisbec@gmail.com>
2009-06-20 02:01:40 +0200

    perfcounter: Handle some IO return values
    Building perfcounter tools raises the following warnings:
    
     builtin-record.c: In function ‘atexit_header’:
     builtin-record.c:464: erreur: ignoring return value of ‘pwrite’, declared with attribute warn_unused_result
     builtin-record.c: In function ‘__cmd_record’:
     builtin-record.c:503: erreur: ignoring return value of ‘read’, declared with attribute warn_unused_result
    
     builtin-report.c: In function ‘__cmd_report’:
     builtin-report.c:1403: erreur: ignoring return value of ‘read’, declared with attribute warn_unused_result
    
    This patch handles these IO return values.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <1245456100-5477-1-git-send-email-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/builtin-report.c
+++ tools/perf/builtin-report.c
@@ -1401,7 +1401,10 @@ static int __cmd_report(void)
 		exit(0);
 	}
 
-	read(input, &file_header, sizeof(file_header));
+	if (read(input, &file_header, sizeof(file_header)) == -1) {
+		perror("failed to read file headers");
+		exit(-1);
+	}
 
 	if (sort__has_parent &&
 	    !(file_header.sample_type & PERF_SAMPLE_CALLCHAIN)) {
