88b015556e8ed3e15ac197f674182a6db676ca3e
Yevgeny Petrilin <yevgenyp@mellanox.co.il>
2009-06-20 22:16:02 +0000

    mlx4_en: Removed redundant check on lso header size
    This check that verifies that the LSO header along with control
    segment and first data segment do not cross 128 bytes is no longer
    required.
    
    Signed-off-by: Yevgeny Petrilin <yevgenyp@mellanox.co.il>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/mlx4/mlx4_en.h
+++ drivers/net/mlx4/mlx4_en.h
@@ -100,7 +100,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RSS_FACTOR		2
 #define TXBB_SIZE		64
 #define HEADROOM		(2048 / TXBB_SIZE + 1)
-#define MAX_LSO_HDR_SIZE	92
 #define STAMP_STRIDE		64
 #define STAMP_DWORDS		(STAMP_STRIDE / 4)
 #define STAMP_SHIFT		31
