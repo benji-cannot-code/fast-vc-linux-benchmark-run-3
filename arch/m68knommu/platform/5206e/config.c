0108991c63233fbe9088fc171efc780c6d141fe4
sfking@fdwdc.com <sfking@fdwdc.com>
2009-06-19 18:11:02 -0700

    generic GPIO support for the Freescale Coldire 5206e.
    Add support for the 5206e.
    
    Signed-off-by: Steven King <sfking@fdwdc.com>
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68knommu/platform/5206e/config.c
+++ arch/m68knommu/platform/5206e/config.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/machdep.h>
 #include <asm/coldfire.h>
 #include <asm/mcfsim.h>
+#include <asm/mcfuart.h>
 #include <asm/mcfdma.h>
 #include <asm/mcfuart.h>
 
