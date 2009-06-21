15590ba6ade847ec4dfdf2b5cc3867a8fab21b0a
Moritz Muehlenhoff <jmm@debian.org>
2009-06-21 22:43:44 +0200

    Staging: wlan-ng: Remove some superflous comments
    Signed-off-by: Moritz Muehlenhoff <jmm@debian.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/wlan-ng/hfa384x_usb.c
+++ drivers/staging/wlan-ng/hfa384x_usb.c
@@ -129,9 +129,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define SUBMIT_URB(u,f)  usb_submit_urb(u,f)
 
-/*================================================================*/
-/* Project Includes */
-
 #include "p80211types.h"
 #include "p80211hdr.h"
 #include "p80211mgmt.h"
--- drivers/staging/wlan-ng/prism2mgmt.c
+++ drivers/staging/wlan-ng/prism2mgmt.c
@@ -74,9 +74,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb.h>
 #include <linux/bitops.h>
 
-/*================================================================*/
-/* Project Includes */
-
 #include "p80211types.h"
 #include "p80211hdr.h"
 #include "p80211mgmt.h"
