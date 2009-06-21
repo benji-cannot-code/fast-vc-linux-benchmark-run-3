413b9d9be95242c47e5bb263b97e085443cb50c0
Laurent Pinchart <laurent.pinchart@skynet.be>
2009-06-21 23:20:39 +0200

    USB: Move vendor subclass definition from usb/audio.h to usb/ch9.h
    USB_SUBCLASS_VENDOR_SPEC is common to several USB classes and as such belongs
    to usb/ch9.h.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@skynet.be>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/usb/audio.h
+++ include/linux/usb/audio.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_SUBCLASS_AUDIOCONTROL	0x01
 #define USB_SUBCLASS_AUDIOSTREAMING	0x02
 #define USB_SUBCLASS_MIDISTREAMING	0x03
-#define USB_SUBCLASS_VENDOR_SPEC	0xff
 
 /* A.5 Audio Class-Specific AC interface Descriptor Subtypes*/
 #define HEADER				0x01
