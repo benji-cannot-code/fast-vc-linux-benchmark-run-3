6d0b6dbf75ed89aea04a3c5c80904b7bd1872b52
Mike Frysinger <vapier@gentoo.org>
2009-06-22 18:41:47 +0100

    serial: bfin_5xx: fix building as module when early printk is enabled
    Since early printk only makes sense/works when the serial driver is built
    into the kernel, disable the option for this driver when it is going to be
    built as a module.  Otherwise we get build failures due to the ifdef
    handling.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/serial/bfin_5xx.c
+++ drivers/serial/bfin_5xx.c
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cacheflush.h>
 #endif
 
+#ifdef CONFIG_SERIAL_BFIN_MODULE
+# undef CONFIG_EARLY_PRINTK
+#endif
+
 /* UART name and device definitions */
 #define BFIN_SERIAL_NAME	"ttyBF"
 #define BFIN_SERIAL_MAJOR	204
