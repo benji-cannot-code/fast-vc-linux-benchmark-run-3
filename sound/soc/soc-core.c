1720861e470f760b0a3a4e40fd1b361f7bdef16a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-06-22 13:16:51 +0100

    ASoC: Add a shutdown callback
    Ensure that the audio subsystem is powered down cleanly when the system
    shuts down by providing a shutdown operation. This ensures that all the
    components have been returned to an off state cleanly which should avoid
    audio issues from partially charged capacitors or noise on digital inputs
    if the system is restarted quickly.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Tested-by: Ben Dooks <ben-linux@fluff.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1021,6 +1021,21 @@ static int soc_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static void soc_shutdown(struct platform_device *pdev)
+{
+	struct snd_soc_device *socdev = platform_get_drvdata(pdev);
+	struct snd_soc_card *card = socdev->card;
+
+	if (!card->instantiated)
+		return;
+
+	/* Flush out pmdown_time work - we actually do want to run it
+	 * now, we're shutting down so no imminent restart. */
+	run_delayed_work(&card->delayed_work);
+
+	snd_soc_dapm_shutdown(socdev);
+}
+
 /* ASoC platform driver */
 static struct platform_driver soc_driver = {
 	.driver		= {
@@ -1031,6 +1046,7 @@ static struct platform_driver soc_driver = {
 	.remove		= soc_remove,
 	.suspend	= soc_suspend,
 	.resume		= soc_resume,
+	.shutdown	= soc_shutdown,
 };
 
 /* create a new pcm */
