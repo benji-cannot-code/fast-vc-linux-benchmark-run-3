fd63b723a7ca29d75c3b56c91ba08fa91e9df05f
Byungho Min <bhminjames@gmail.com>
2009-06-23 21:39:42 +0900

    ARM: S5PC100: Kconfigs and Makefiles
    S5PC100 is a new SoC with ARM coretex-A8 and numerous peripherals. This SoC is
    successor of S3C64XX. S5PC100 has peripherals which are still similar to S3C
    families so some drivers in "arch/arm/plat-s3c" can be shared. S5PC100 specific
    drivers will be added in "arch/arm/plat-s5pcxx" or "arch/arm/mach-s5pc100"
    
    Signed-off-by: Byungho Min <bhmin@samsung.com>
    [ben-linux@fluff.org: tidy and edit description]
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s5pc100/include/mach/hardware.h
+++ arch/arm/mach-s5pc100/include/mach/hardware.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright 2009 Samsung Electronics Co.
  *      Byungho Min <bhmin@samsung.com>
  *
- * S3C6400 - Hardware support
+ * S5PC100 - Hardware support
  */
 
 #ifndef __ASM_ARCH_HARDWARE_H
