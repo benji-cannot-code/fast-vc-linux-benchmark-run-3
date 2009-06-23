b675750ebab04dc31e70e485270463d9f489ee14
Byungho Min <bhminjames@gmail.com>
2009-06-23 21:40:15 +0900

    ARM: S5PC100: CPU initialization
    Signed-off-by: Byungho Min <bhmin@samsung.com>
    [ben-linux@fluff.org: subject fixup]
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

---
+++ arch/arm/mach-s5pc100/include/mach/hardware.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* linux/arch/arm/mach-s5pc100/include/mach/hardware.h
+ *
+ * Copyright 2009 Samsung Electronics Co.
+ *      Byungho Min <bhmin@samsung.com>
+ *
+ * S3C6400 - Hardware support
+ */
+
+#ifndef __ASM_ARCH_HARDWARE_H
+#define __ASM_ARCH_HARDWARE_H __FILE__
+
+/* currently nothing here, placeholder */
+
+#endif /* __ASM_ARCH_HARDWARE_H */
