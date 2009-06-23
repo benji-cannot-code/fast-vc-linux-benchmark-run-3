7364482f8589e2487080738d04a73570afd78d0a
Huang Weiyi <weiyi.huang@gmail.com>
2009-06-23 20:58:57 +0800

    [ARM] MINI2440: remove duplicated #include
    Remove duplicated #include('s) in
      arch/arm/mach-s3c2440/mach-mini2440.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c2440/mach-mini2440.c
+++ arch/arm/mach-s3c2440/mach-mini2440.c
@@ -49,8 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/mci.h>
 #include <plat/udc.h>
 
-#include <plat/regs-serial.h>
-
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/nand.h>
 #include <linux/mtd/nand_ecc.h>
