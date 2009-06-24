378ed13879a0344e8a68870d75f82c9912395486
Kyle McMartin <kyle@mcmartin.ca>
2009-06-23 21:38:49 -0400

    parisc: perf: wire up sys_perf_counter_open
    Signed-off-by: Kyle McMartin <kyle@mcmartin.ca>

--- tools/perf/perf.h
+++ tools/perf/perf.h
@@ -20,6 +20,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define cpu_relax()	asm volatile("" ::: "memory");
 #endif
 
+#ifdef __hppa__
+#include "../../arch/parisc/include/asm/unistd.h"
+#define rmb()		asm volatile("" ::: "memory")
+#define cpu_relax()	asm volatile("" ::: "memory");
+#endif
+
 #include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
