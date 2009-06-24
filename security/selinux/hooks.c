9ea3a2009811a836fad07cf8bded54a2648081b5
Thomas Liu <tliu@redhat.com>
2009-06-24 17:58:05 -0400

    selinux: clean up avc node cache when disabling selinux
    Added a call to free the avc_node_cache when inside selinux_disable because
    it should not waste resources allocated during avc_init if SELinux is disabled
    and the cache will never be used.
    
    Signed-off-by: Thomas Liu <tliu@redhat.com>
    Acked-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -5684,6 +5684,9 @@ int selinux_disable(void)
 	selinux_disabled = 1;
 	selinux_enabled = 0;
 
+	/* Try to destroy the avc node cache */
+	avc_disable();
+
 	/* Reset security_ops to the secondary module, dummy or capability. */
 	security_ops = secondary_ops;
 
