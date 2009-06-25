52b715317de2636ae1266912d2b8431ccb626528
Robert P. J. Day <rpjday@crashcourse.ca>
2009-06-25 16:20:48 -0400

    x86, setup: Fix typo "CONFIG_x86_64" in <asm/boot.h>
    CONFIG_X86_64 was misspelled (wrong case), which caused the x86-64
    kernel to advertise itself as more relocatable than it really is.
    This could in theory cause boot failures once bootloaders start
    support the new relocation fields.
    
    Signed-off-by: Robert P. J. Day <rpjday@crashcourse.ca>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/boot.h
+++ arch/x86/include/asm/boot.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				& ~(CONFIG_PHYSICAL_ALIGN - 1))
 
 /* Minimum kernel alignment, as a power of two */
-#ifdef CONFIG_x86_64
+#ifdef CONFIG_X86_64
 #define MIN_KERNEL_ALIGN_LG2	PMD_SHIFT
 #else
 #define MIN_KERNEL_ALIGN_LG2	(PAGE_SHIFT+1)
