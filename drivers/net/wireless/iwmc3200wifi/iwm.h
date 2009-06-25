38f415eb6d24b700a83741230da2a5e58bad5b90
Zhu Yi <yi.zhu@intel.com>
2009-06-25 18:28:31 +0800

    iwmc3200wifi: simplify calibration map
    The patch simplifies calibration map by combining the init_calib_map
    and periodic_calib_map into one calib_map in struct iwm_conf. Now the
    initial calibration map is stored in the lower 16 bits of calib_map
    and the periodic calibration map is stored in the higher 16 bits.
    
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwmc3200wifi/iwm.h
+++ drivers/net/wireless/iwmc3200wifi/iwm.h
@@ -66,8 +66,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct iwm_conf {
 	u32 sdio_ior_timeout;
-	unsigned long init_calib_map;
-	unsigned long periodic_calib_map;
+	unsigned long calib_map;
 	bool reset_on_fatal_err;
 	bool auto_connect;
 	bool wimax_not_present;
