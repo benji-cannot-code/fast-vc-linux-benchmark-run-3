130a44af3789940b8a10e8dae3b68ef8e1295437
Hartley Sweeten <hartleys@visionengravers.com>
2009-06-26 21:39:27 +0100

    [ARM] 5574/1: ep93xx: gpio.c: fix header includes and __iomem pointers
    Fix ep93xx gpio.c header includes and __iomem pointers.
    
      1. <linux/gpio.h> should be included instead of <asm/gpio.h>
      2. <mach/hardware.h> should be included instead of <mach/ep93xx-regs.h>
      3. data_reg and data_dir_reg in struct ep93xx_gpio_chip should be
         void __iomem pointers not unsigned int
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Ryan Mallon <ryan@bluewatersys.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ep93xx/gpio.c
+++ arch/arm/mach-ep93xx/gpio.c
@@ -18,15 +18,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/seq_file.h>
 #include <linux/io.h>
+#include <linux/gpio.h>
 
-#include <mach/ep93xx-regs.h>
-#include <asm/gpio.h>
+#include <mach/hardware.h>
 
 struct ep93xx_gpio_chip {
 	struct gpio_chip	chip;
 
-	unsigned int		data_reg;
-	unsigned int		data_dir_reg;
+	void __iomem		*data_reg;
+	void __iomem		*data_dir_reg;
 };
 
 #define to_ep93xx_gpio_chip(c) container_of(c, struct ep93xx_gpio_chip, chip)
