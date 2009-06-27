e13b798fa3a615018bff55ac0835df4d281f7e09
Arnd Bergmann <arnd@arndb.de>
2009-06-27 15:22:00 +0200

    score: make irq.h definitions local
    Some internal definitions of the interrupt controller are
    only needed in irq.c, so move them out of the global irq.h
    header. Also add proper __iomem annotations for sparse.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/score/include/asm/irq.h
+++ arch/score/include/asm/irq.h
@@ -19,16 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define irq_canonicalize(irq)	(irq)
 
-#define P_INT_PNDL		0x95F50000
-#define P_INT_PNDH		0x95F50004
-#define P_INT_PRIORITY_M	0x95F50008
-#define P_INT_PRIORITY_SG0	0x95F50010
-#define P_INT_PRIORITY_SG1	0x95F50014
-#define P_INT_PRIORITY_SG2	0x95F50018
-#define P_INT_PRIORITY_SG3	0x95F5001C
-#define P_INT_MASKL		0x95F50020
-#define P_INT_MASKH		0x95F50024
-
 #define IRQ_TIMER (7)		/* Timer IRQ number of SPCT6600 */
 
 #endif /* _ASM_SCORE_IRQ_H */
