66cd4ebe435013151dc3b47ba917dffc2a179c91
Arnd Bergmann <arnd@arndb.de>
2009-06-27 14:46:35 +0200

    score: cleanups: dead code, 0 as pointer, shadowed variables
    A few smaller issues found by sparse, some code that was
    never used, two instances of '0' instead of 'NULL' and
    local variables shadowing another one.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/score/kernel/sys_score.c
+++ arch/score/kernel/sys_score.c
@@ -32,10 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/syscalls.h>
 #include <asm/syscalls.h>
 
-unsigned long shm_align_mask = PAGE_SIZE - 1;
-EXPORT_SYMBOL(shm_align_mask);
-
-asmlinkage unsigned
+asmlinkage long 
 sys_mmap2(unsigned long addr, unsigned long len, unsigned long prot,
 	  unsigned long flags, unsigned long fd, unsigned long pgoff)
 {
@@ -105,15 +102,6 @@ score_execve(struct pt_regs *regs)
 
 	putname(filename);
 	return error;
-}
-
-/*
- * If we ever come here the user sp is bad.  Zap the process right away.
- * Due to the bad stack signaling wouldn't work.
- */
-void bad_stack(void)
-{
-	do_exit(SIGSEGV);
 }
 
 /*
