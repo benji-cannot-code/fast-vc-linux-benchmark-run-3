1caff68ba91792ffca95ac912e1bd4f2f436b1a7
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-06-28 16:19:23 +0200

    Staging: rtl8187se: remove dead code
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8187se/r8180_dm.c
+++ drivers/staging/rtl8187se/r8180_dm.c
@@ -1381,12 +1381,6 @@ SwitchAntenna(
 
 	if(priv->CurrAntennaIndex == 0)
 	{
-#if 0//lzm del 080826
-//by amy 080312
-		if(priv->bSwAntennaDiverity)
-			bResult = SetAntennaConfig87SE(dev, 1, true);
-		else
-#endif
 			bResult = SetAntenna8185(dev, 1);
 //by amy 080312
 //		printk("SwitchAntenna(): switching to antenna 1 ......\n");
@@ -1394,12 +1388,6 @@ SwitchAntenna(
 	}
 	else
 	{
-#if 0//lzm del 080826
-//by amy 080312
-		if(priv->bSwAntennaDiverity)
-			bResult = SetAntennaConfig87SE(dev, 0, true);
-		else
-#endif
 			bResult = SetAntenna8185(dev, 0);
 //by amy 080312
 //		printk("SwitchAntenna(): switching to antenna 0 ......\n");
