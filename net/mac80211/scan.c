94f47c85220d5810a3e191891c28dfde9141c5e9
Helmut Schaa <helmut.schaa@googlemail.com>
2009-06-30 14:49:18 +0200

    mac80211: shorten the passive dwell time for sw scans
    mac80211's software scan implementation uses a passive dwell time of
    (HZ / 5) which means we stay 200ms on each passive channel. Compared
    to iwlwifi's hw scan and the old ipw* drivers which use values around
    120ms this is quite long.
    
    Reducing the passive dwell time from 200ms to 125ms should save us
    something around a second on cards capable of 11a and we should still be
    able to catch beacons from most access points (assuming a ~100ms beacon
    interval).
    
    Signed-off-by: Helmut Schaa <helmut.schaa@googlemail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/scan.c
+++ net/mac80211/scan.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define IEEE80211_PROBE_DELAY (HZ / 33)
 #define IEEE80211_CHANNEL_TIME (HZ / 33)
-#define IEEE80211_PASSIVE_CHANNEL_TIME (HZ / 5)
+#define IEEE80211_PASSIVE_CHANNEL_TIME (HZ / 8)
 
 struct ieee80211_bss *
 ieee80211_rx_bss_get(struct ieee80211_local *local, u8 *bssid, int freq,
