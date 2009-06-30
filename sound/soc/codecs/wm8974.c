7a4b1afc5c12a6f3b5ad0aa234b0b3b83c3e25a0
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-06-30 19:37:02 +0100

    ASoC: WM8974 cosmetic cleanups
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8974.c
+++ sound/soc/codecs/wm8974.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * wm8974.c  --  WM8974 ALSA Soc Audio driver
  *
- * Copyright 2006 Wolfson Microelectronics PLC.
+ * Copyright 2006-2009 Wolfson Microelectronics PLC.
  *
  * Author: Liam Girdwood <linux@wolfsonmicro.com>
  *
@@ -358,7 +358,7 @@ static void pll_factors(unsigned int target, unsigned int source)
 
 	if ((Ndiv < 6) || (Ndiv > 12))
 		printk(KERN_WARNING
-			"WM8974 N value %u outwith recommended range!d\n",
+			"WM8974 N value %u outwith recommended range!\n",
 			Ndiv);
 
 	pll_div.n = Ndiv;
@@ -547,6 +547,7 @@ static int wm8974_pcm_hw_params(struct snd_pcm_substream *substream,
 		adn |= 0x1 << 1;
 		break;
 	case SNDRV_PCM_RATE_44100:
+	case SNDRV_PCM_RATE_48000:
 		break;
 	}
 
@@ -823,7 +824,7 @@ MODULE_DEVICE_TABLE(i2c, wm8974_i2c_id);
 
 static struct i2c_driver wm8974_i2c_driver = {
 	.driver = {
-		.name = "WM8974 I2C Codec",
+		.name = "WM8974",
 		.owner = THIS_MODULE,
 	},
 	.probe =    wm8974_i2c_probe,
