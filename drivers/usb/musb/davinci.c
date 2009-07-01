d6d83fe0e054a7fd5ce782826ed5ed815f0b8656
Kevin Hilman <khilman@deeprootsystems.com>
2009-07-01 03:33:46 -0700

    USB: musb: davinci dm6446evm GPIO renumbering
    Numbering for GPIOs on the pcf857x chips on the dm644x EVM board
    changed when DaVinci chips with more GPIOs were supported.  Update
    the GPIO number used for nVBUS_DRV.
    
    Longer term, we need a better abstraction of board-specific setup in
    this code so we're not hard-coding board specific GPIOs into the
    driver, but for now this at least gets it back to working with
    mainline davinci core code.
    
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/musb/davinci.c
+++ drivers/usb/musb/davinci.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "musb_core.h"
 
 #ifdef CONFIG_MACH_DAVINCI_EVM
-#define GPIO_nVBUS_DRV		87
+#define GPIO_nVBUS_DRV		144
 #endif
 
 #include "davinci.h"
