77b5f5f3f7b15ddf4c0c3a3d372155ad3fa081f7
Alessandro Rubini <rubini@gnudd.com>
2009-07-02 19:06:47 +0100

    [ARM] 5590/1: Add basic support for ST Nomadik 8815 SoC and evaluation board
    This patch adds the basic infrastructure for the Nomadik 8815
    CPU and the "Nomadik Hardware Kit" NHK8815. This patch only
    includes the serial console and core stuff, no drivers.
    
    Signed-off-by: Alessandro Rubini <rubini@unipv.it>
    Acked-by: Andrea Gallo <andrea.gallo@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/mach-nomadik/include/mach/setup.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+
+/*
+ * These symbols are needed for board-specific files to call their
+ * own cpu-specific files
+ */
+
+#ifndef __ASM_ARCH_SETUP_H
+#define __ASM_ARCH_SETUP_H
+
+#include <asm/mach/time.h>
+#include <linux/init.h>
+
+#ifdef CONFIG_NOMADIK_8815
+
+extern void cpu8815_map_io(void);
+extern void cpu8815_platform_init(void);
+extern void cpu8815_init_irq(void);
+extern struct sys_timer nomadik_timer;
+
+#endif /* NOMADIK_8815 */
+
+#endif /*  __ASM_ARCH_SETUP_H */
