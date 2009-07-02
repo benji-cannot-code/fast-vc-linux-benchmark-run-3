e4f8607fda8295e1eeea6e1780f71b9e4f2aacb2
Wu Zhangjin <wuzhangjin@gmail.com>
2009-07-02 23:27:12 +0800

    MIPS: Loongson: Add a machtype kernel command line argument
    The difference between some loongson-based machines is very small, so, if
    there is no necessary to add new kernel config options to cope with this
    difference, it will be better to share the same kernel image file between
    them, benefit from this, the linux distribution developers only have a need
    to compile the kernel one time.
    
    This machtype kernel command line argument will be used later to share the
    same kernel image file between two different machines(menglong & yeeloong)
    made by lemote.
    
    Thanks very much to Zhang Le for cleaning up the machtype implementation.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-loongson/machine.h
+++ arch/mips/include/asm/mach-loongson/machine.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define LOONGSON_UART_BASE (BONITO_PCIIO_BASE + 0x3f8)
 
-#define LOONGSON_MACHNAME "lemote-fuloong-2e-box"
+#define LOONGSON_MACHTYPE MACH_LEMOTE_FL2E
 
 #endif
 
