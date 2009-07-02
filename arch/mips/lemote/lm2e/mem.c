d50bc4f9d6d29896ef1615f6deb2a3f2f291032e
Wu Zhangjin <wuzj@lemote.com>
2009-07-02 23:22:36 +0800

    MIPS: Loongson: Split the implementation of prom and setup parts
    This patch split the old initilization and setup implementation to
    several file, one file one logic function.
    
    the other main changes include:
    
    	1. as the script/checkpatch.pl suggests, use strict_strtol instead
    	of simple_strtol in arch/mips/lemote/lm2e/cmdline.c
    
    	2. use the existed macros in asm/mips-boards/bonito64.h as the
    	arguments of set_io_port_base() and remove the un-needed ones in
    	asm/mach-lemote/pci.h
    
    Signed-off-by: Wu Zhangjin <wuzj@lemote.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/lemote/lm2e/mem.c
+++ arch/mips/lemote/lm2e/mem.c
@@ -9,6 +9,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fcntl.h>
 #include <linux/mm.h>
 
+#include <asm/bootinfo.h>
+
+extern unsigned long memsize, highmemsize;
+
+void __init prom_init_memory(void)
+{
+    add_memory_region(0x0, (memsize << 20), BOOT_MEM_RAM);
+#ifdef CONFIG_64BIT
+    if (highmemsize > 0)
+		add_memory_region(0x20000000, highmemsize << 20, BOOT_MEM_RAM);
+#endif /* CONFIG_64BIT */
+}
+
 /* override of arch/mips/mm/cache.c: __uncached_access */
 int __uncached_access(struct file *file, unsigned long addr)
 {
