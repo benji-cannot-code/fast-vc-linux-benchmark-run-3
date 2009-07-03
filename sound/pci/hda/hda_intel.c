5e06a76ddfa50335fb4d7345f485fd06500c35b4
Takashi Iwai <tiwai@suse.de>
2009-07-03 23:06:45 +0200

    ALSA: hda - Call snd_pcm_lib_hw_rates() again after codec open callback
    The PCM rates bit field may have been changed by the codec open callback.
    In that case, we need to reset rate_min and rate_max.  So, simply call
    snd_pcm_lib_hw_rates() again after the codec open callback.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1455,6 +1455,7 @@ static int azx_pcm_open(struct snd_pcm_substream *substream)
 		mutex_unlock(&chip->open_mutex);
 		return err;
 	}
+	snd_pcm_limit_hw_rates(runtime);
 	spin_lock_irqsave(&chip->reg_lock, flags);
 	azx_dev->substream = substream;
 	azx_dev->running = 0;
