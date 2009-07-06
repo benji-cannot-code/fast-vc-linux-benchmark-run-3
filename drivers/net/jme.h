1e68f6c0c899fd6082de62c009fcc944d12260b5
Guo-Fu Tseng <cooldavid@cooldavid.org>
2009-07-06 04:45:58 +0000

    jme: Advance driver version number
    Advance driver version number after modifications.
    
    Signed-off-by: Guo-Fu Tseng <cooldavid@cooldavid.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/jme.h
+++ drivers/net/jme.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __JME_H_INCLUDED__
 
 #define DRV_NAME	"jme"
-#define DRV_VERSION	"1.0.4"
+#define DRV_VERSION	"1.0.5"
 #define PFX		DRV_NAME ": "
 
 #define PCI_DEVICE_ID_JMICRON_JMC250	0x0250
