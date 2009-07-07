c3457287f553efdd0fcde2618b3ed05876a08e48
Len Brown <len.brown@intel.com>
2009-07-06 23:40:19 -0400

    ACPI: linux/acpi.h should not include linux/dmi.h
    users of acpi.h that need dmi.h should include it directly.
    
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/pci_slot.c
+++ drivers/acpi/pci_slot.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/acpi.h>
 #include <acpi/acpi_bus.h>
 #include <acpi/acpi_drivers.h>
+#include <linux/dmi.h>
 
 static int debug;
 static int check_sta_before_sun;
