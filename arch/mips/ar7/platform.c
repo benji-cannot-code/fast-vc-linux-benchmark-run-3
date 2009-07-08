f0268c76d93cedd66045b6a2c1df409297bc9edb
Huang Weiyi <weiyi.huang@gmail.com>
2009-07-09 06:14:37 +0800

    MIPS: AR7: Remove unused inclusions of <linux/version.h>.
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ar7/platform.c
+++ arch/mips/ar7/platform.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial_8250.h>
 #include <linux/ioport.h>
 #include <linux/io.h>
-#include <linux/version.h>
 #include <linux/vlynq.h>
 #include <linux/leds.h>
 #include <linux/string.h>
