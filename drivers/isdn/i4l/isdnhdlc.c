16492184b91cf183b621e68960bd853194dd96b5
Karsten Keil <keil@b1-systems.de>
2009-07-08 14:21:12 +0200

    ISDN: Make isdnhdlc usable for other ISDN drivers
    isdnhdlc is useful for other ISDN drivers as well.
    Move the include file to a central location and the source
    to the central isdn location.
    
    Signed-off-by: Karsten Keil <keil@b1-systems.de>

--- drivers/isdn/i4l/isdnhdlc.c
+++ drivers/isdn/i4l/isdnhdlc.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/crc-ccitt.h>
-#include "isdnhdlc.h"
+#include <linux/isdn/hdlc.h>
 
 /*-------------------------------------------------------------------*/
 
