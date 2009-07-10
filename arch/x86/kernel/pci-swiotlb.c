21e577f11587f9bf0047bbc2b7e154d3c2b57542
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-07-10 10:04:51 +0900

    swiotlb: remove unused swiotlb_alloc()
    Nobody uses swiotlb_alloc().
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: Becky Bruce <beckyb@kernel.crashing.org>

--- arch/x86/kernel/pci-swiotlb.c
+++ arch/x86/kernel/pci-swiotlb.c
@@ -14,11 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int swiotlb __read_mostly;
 
-void *swiotlb_alloc(unsigned order, unsigned long nslabs)
-{
-	return (void *)__get_free_pages(GFP_DMA | __GFP_NOWARN, order);
-}
-
 dma_addr_t swiotlb_phys_to_bus(struct device *hwdev, phys_addr_t paddr)
 {
 	return paddr;
