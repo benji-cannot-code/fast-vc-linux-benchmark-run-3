8fc05a2887cb82be97f340395e358c2ed73f4a5a
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-07-13 20:14:02 +0200

    Staging: rtl8192su/ieee80211: remove OPENSUSE_SLED ifdefs
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192su/ieee80211/ieee80211_crypt_wep.c
+++ drivers/staging/rtl8192su/ieee80211/ieee80211_crypt_wep.c
@@ -28,9 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 MODULE_AUTHOR("Jouni Malinen");
 MODULE_DESCRIPTION("Host AP crypt: WEP");
 MODULE_LICENSE("GPL");
-#ifndef OPENSUSE_SLED
-#define OPENSUSE_SLED 0
-#endif
 
 struct prism2_wep_data {
 	u32 iv;
