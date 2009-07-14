12fb4377e4df4827e046504d7245de9e8fa407d0
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2009-07-14 13:25:41 -0500

    9p: Possible regression in p9_client_stat
    Fix a possible regression with p9_client_stat where it can try to kfree 
    an ERR_PTR after an erroneous p9pdu_readf. Also remove an unnecessary data 
    buffer increment in p9_client_read.
    
    Signed-off-by: Abhishek Kulkarni <adkulkar@umail.iu.edu>
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

--- net/9p/client.c
+++ net/9p/client.c
@@ -1099,7 +1099,6 @@ p9_client_read(struct p9_fid *fid, char *data, char __user *udata, u64 offset,
 
 	if (data) {
 		memmove(data, dataptr, count);
-		data += count;
 	}
 
 	if (udata) {
@@ -1193,9 +1192,9 @@ struct p9_wstat *p9_client_stat(struct p9_fid *fid)
 
 	err = p9pdu_readf(req->rc, clnt->dotu, "wS", &ignored, ret);
 	if (err) {
-		ret = ERR_PTR(err);
 		p9pdu_dump(1, req->rc);
-		goto free_and_error;
+		p9_free_req(clnt, req);
+		goto error;
 	}
 
 	P9_DPRINTK(P9_DEBUG_9P,
@@ -1212,8 +1211,6 @@ struct p9_wstat *p9_client_stat(struct p9_fid *fid)
 	p9_free_req(clnt, req);
 	return ret;
 
-free_and_error:
-	p9_free_req(clnt, req);
 error:
 	kfree(ret);
 	return ERR_PTR(err);
