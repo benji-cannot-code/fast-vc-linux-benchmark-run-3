41df4f28b866eef3c1e933a32bbb563f59bbc8c4
jolsa@redhat.com <jolsa@redhat.com>
2009-07-16 21:44:26 +0200

    tracing: Remove unused fields/variables
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: rostedt@goodmis.org
    LKML-Reference: <1247773468-11594-2-git-send-email-jolsa@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -4266,7 +4266,6 @@ void ftrace_dump(void)
 
 __init static int tracer_alloc_buffers(void)
 {
-	struct trace_array_cpu *data;
 	int ring_buf_size;
 	int i;
 	int ret = -ENOMEM;
@@ -4316,7 +4315,7 @@ __init static int tracer_alloc_buffers(void)
 
 	/* Allocate the first page for all buffers */
 	for_each_tracing_cpu(i) {
-		data = global_trace.data[i] = &per_cpu(global_trace_cpu, i);
+		global_trace.data[i] = &per_cpu(global_trace_cpu, i);
 		max_tr.data[i] = &per_cpu(max_data, i);
 	}
 
