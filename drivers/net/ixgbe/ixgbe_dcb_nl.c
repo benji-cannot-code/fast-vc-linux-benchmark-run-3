e35a220eb708e2aa41f129397ca4e475f81e4b12
Lucy Liu <lucy.liu@intel.com>
2009-07-16 13:43:31 +0000

    ixgbe: Remove DPRINTK messages in DCB mode
    Remove debug DPRINTK in DCB mode netlink interface.
    
    Signed-off-by: Lucy Liu <lucy.liu@intel.com>
    Acked-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ixgbe/ixgbe_dcb_nl.c
+++ drivers/net/ixgbe/ixgbe_dcb_nl.c
@@ -107,8 +107,6 @@ static u8 ixgbe_dcbnl_get_state(struct net_device *netdev)
 {
 	struct ixgbe_adapter *adapter = netdev_priv(netdev);
 
-	DPRINTK(DRV, INFO, "Get DCB Admin Mode.\n");
-
 	return !!(adapter->flags & IXGBE_FLAG_DCB_ENABLED);
 }
 
@@ -116,8 +114,6 @@ static u8 ixgbe_dcbnl_set_state(struct net_device *netdev, u8 state)
 {
 	u8 err = 0;
 	struct ixgbe_adapter *adapter = netdev_priv(netdev);
-
-	DPRINTK(DRV, INFO, "Set DCB Admin Mode.\n");
 
 	if (state > 0) {
 		/* Turn on DCB */
