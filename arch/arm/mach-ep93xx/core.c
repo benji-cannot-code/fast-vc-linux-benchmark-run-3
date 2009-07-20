7125bb993ea0b467c2830f48469c9e5735e7dc89
Hartley Sweeten <hartleys@visionengravers.com>
2009-07-20 18:22:36 +0100

    [ARM] 5612/1: ep93xx: add platform LEDs
    The EP93xx has two gpio pins specifically assigned to drive
    external LEDs.  Add core support for these LEDs.
    
    On the EDB93xx development boards, the rdled is connected to
    an external reset circuit.  Turning this led on for an extended
    amount of time will cause the circuit to issue a manual reset.
    
    Refer to Cirrus App Note AN258 for more information.
    
    	http://www.cirrus.com/en/pubs/appNote/AN258REV2.pdf
    
    This led can be safely used as the system heartbeat with the
    ledtrig-heartbeat driver.
    
    	echo heartbeat > /sys/class/leds/platform:rdled/trigger
    
    The grled can be used for any desired purpose.
    
    Tested-by: Matthieu Crapet <mcrapet@gmail.com>
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ep93xx/core.c
+++ arch/arm/mach-ep93xx/core.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timex.h>
 #include <linux/io.h>
 #include <linux/gpio.h>
+#include <linux/leds.h>
 #include <linux/termios.h>
 #include <linux/amba/bus.h>
 #include <linux/amba/serial.h>
@@ -569,6 +570,34 @@ void __init ep93xx_register_i2c(struct i2c_board_info *devices, int num)
 	platform_device_register(&ep93xx_i2c_device);
 }
 
+
+/*************************************************************************
+ * EP93xx LEDs
+ *************************************************************************/
+static struct gpio_led ep93xx_led_pins[] = {
+	{
+		.name			= "platform:grled",
+		.gpio			= EP93XX_GPIO_LINE_GRLED,
+	}, {
+		.name			= "platform:rdled",
+		.gpio			= EP93XX_GPIO_LINE_RDLED,
+	},
+};
+
+static struct gpio_led_platform_data ep93xx_led_data = {
+	.num_leds	= ARRAY_SIZE(ep93xx_led_pins),
+	.leds		= ep93xx_led_pins,
+};
+
+static struct platform_device ep93xx_leds = {
+	.name		= "leds-gpio",
+	.id		= -1,
+	.dev		= {
+		.platform_data	= &ep93xx_led_data,
+	},
+};
+
+
 extern void ep93xx_gpio_init(void);
 
 void __init ep93xx_init_devices(void)
@@ -584,4 +613,5 @@ void __init ep93xx_init_devices(void)
 
 	platform_device_register(&ep93xx_rtc_device);
 	platform_device_register(&ep93xx_ohci_device);
+	platform_device_register(&ep93xx_leds);
 }
