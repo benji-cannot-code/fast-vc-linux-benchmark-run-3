539c48addeb62e372b9d01f99d8f3d706e7aed1d
Michal Simek <monstr@monstr.eu>
2009-07-21 12:58:21 +0200

    microblaze: trivial: Add one empty line
    Add one empty line to microblaze_ksyms.c.
    Remove one empty line to cacheflush.h.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/kernel/microblaze_ksyms.c
+++ arch/microblaze/kernel/microblaze_ksyms.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern char *_ebss;
 EXPORT_SYMBOL_GPL(_ebss);
+
 #ifdef CONFIG_FUNCTION_TRACER
 extern void _mcount(void);
 EXPORT_SYMBOL(_mcount);
