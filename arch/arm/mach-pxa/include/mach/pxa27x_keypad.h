1cb05482c76bce9987339898a0fc8760af7bf432
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2009-07-22 21:51:40 -0700

    Input: pxa27x_keypad - remove extra clk_disable
    clk_disable() in remove method is not needed since we already
    have clk_disable in pxa27x_keypad_close().
    
    Also make sure the driver uses resource_size() and helpers from
    include/input/matrix_keypad.h
    
    Tested-by: Mike Rapoport <mike@compulab.co.il>
    Acked-by: Eric Miao <eric.y.miao@gmail.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- arch/arm/mach-pxa/include/mach/pxa27x_keypad.h
+++ arch/arm/mach-pxa/include/mach/pxa27x_keypad.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARCH_PXA27x_KEYPAD_H
 
 #include <linux/input.h>
+#include <linux/input/matrix_keypad.h>
 
 #define MAX_MATRIX_KEY_ROWS	(8)
 #define MAX_MATRIX_KEY_COLS	(8)
@@ -51,8 +52,6 @@ struct pxa27x_keypad_platform_data {
 	/* key debounce interval */
 	unsigned int	debounce_interval;
 };
-
-#define KEY(row, col, val)	(((row) << 28) | ((col) << 24) | (val))
 
 extern void pxa_set_keypad_info(struct pxa27x_keypad_platform_data *info);
 
