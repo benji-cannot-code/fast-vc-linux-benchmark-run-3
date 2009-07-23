16384bf002963708c1895af7ce2f2930a8ce136a
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2009-07-23 08:33:56 +0200

    Staging: fix typos "man[ae]g?ment" -> "management"
    This patch was generated by running
    
    	git grep -E -l 'man[ae]g?ment' drivers/staging | xargs -r perl -p -i -e 's/\bman[ae]g?ment\b/management/g'
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Cc: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6655/80211mgr.c
+++ drivers/staging/vt6655/80211mgr.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * File: 80211mgr.c
  *
- * Purpose: Handles the 802.11 managment support functions
+ * Purpose: Handles the 802.11 management support functions
  *
  * Author: Lyndon Chen
  *
