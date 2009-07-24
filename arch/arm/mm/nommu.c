6070355a0896863bf976506da546263839751c0f
Catalin Marinas <catalin.marinas@arm.com>
2009-07-24 12:35:03 +0100

    nommu: Include asm/setup.h in arch/arm/mm/nommu.c
    This is needed for the struct meminfo definition.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/mm/nommu.c
+++ arch/arm/mm/nommu.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cacheflush.h>
 #include <asm/sections.h>
 #include <asm/page.h>
+#include <asm/setup.h>
 #include <asm/mach/arch.h>
 
 #include "mm.h"
