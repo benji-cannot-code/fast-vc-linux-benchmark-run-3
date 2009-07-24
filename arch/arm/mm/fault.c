56af7fd79fd058e2cf9a83afff9162a90088c765
Catalin Marinas <catalin.marinas@arm.com>
2009-07-24 12:37:09 +0100

    Include linux/sched.h in arch/arm/mm/fault.c
    When building with !MMU, task_struct is not defined. Just include the
    relevant file.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kprobes.h>
 #include <linux/uaccess.h>
 #include <linux/page-flags.h>
+#include <linux/sched.h>
 
 #include <asm/system.h>
 #include <asm/pgtable.h>
