7f7e401f17c1df87ddc7fca572310e6e805ea771
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2009-07-24 11:13:02 -0700

    iwlwifi: Thermal Throttling Management - Part 1
    Part 1 of Thermal Throttling Management -
    
    Thermal Throttling feature is used to put NIC into low power state when
    driver detect the Radio temperature reach pre-defined threshold
    
    Two Thermal Throttling Management Methods; this patch introduce the
    Legacy Thermal Management:
       IWL_TI_0: normal temperature, system power state
       IWL_TI_1: high temperature detect, low power state
       IWL_TI_2: higher temperature detected, lower power state
       IWL_TI_CT_KILL: critical temperature detected, lowest power state
    
    Once get into CT_KILL state, uCode go into sleep, driver will stop all
    the active queues, then move to IWL_TI_CT_KILL state; also set up 5
    seconds timer to toggle CSR flag, uCode wake up upon CSR flag change,
    then measure the temperature.
    If temperature is above CT_KILL exit threshold, uCode go backto sleep;
    if temperature is below CT_KILL exit threshold, uCode send Card State
    Notification response with appropriate CT_KILL status flag, and uCode
    remain awake, Driver receive Card State Notification Response and update
    the card temperature to the CT_KILL exit threshold.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-4965.c
+++ drivers/net/wireless/iwlwifi/iwl-4965.c
@@ -1798,6 +1798,7 @@ static void iwl4965_temperature_calib(struct iwl_priv *priv)
 	}
 
 	priv->temperature = temp;
+	iwl_tt_handler(priv);
 	set_bit(STATUS_TEMPERATURE, &priv->status);
 
 	if (!priv->disable_tx_power_cal &&
