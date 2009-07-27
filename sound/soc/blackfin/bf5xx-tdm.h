4c1bfced4df20ff0b9360be421b96d810870e4e0
Barry Song <21cnbao@gmail.com>
2009-07-27 18:06:39 +0800

    ASoC: blackfin I2S(TDM mode) CPU DAI driver
    The I2S DAI driver for blackfin SPORT, but works in TDM mode.
    I2S is not a special case of TDM with only left and right two slots for
    SPORT interface. I2S coordinates with TDM in SPORT, but not a part of
    TDM. TDM require different hardware configuration with I2S, not only
    different slot number.  One is "Stereo Serial Operation" mode of SPORT,
    the other one is "Multichannel Operation" mode. They are incompatible
    at the same time.
    Hardware and DMA description and data transfer flow are much different
    for I2S and TDM. Merging them as a whole will be very ugly and difficult
    to maintain.
    So we don't define a new DAI type, but give two DAI instances for standard
    I2S and TDM, both in I2S-family DAI type. The TDM instance still uses the
    I2S-family DAI type.
    
    Signed-off-by: Barry Song <21cnbao@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

---
+++ sound/soc/blackfin/bf5xx-tdm.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * linux/sound/arm/bf5xx-i2s.h
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _BF5XX_TDM_H
+#define _BF5XX_TDM_H
+
+extern struct snd_soc_dai bf5xx_tdm_dai;
+
+#endif
