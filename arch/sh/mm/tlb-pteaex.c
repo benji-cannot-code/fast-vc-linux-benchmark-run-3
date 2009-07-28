eb19362b8435ab6208919d326639373de642be09
Paul Mundt <lethal@linux-sh.org>
2009-07-29 00:12:17 +0900

    sh: update_mmu_cache() consolidation.
    This splits out a separate __update_cache()/__update_tlb() for
    update_mmu_cache() to wrap in to. This lets us share the common
    __update_cache() bits while keeping special __update_tlb() handling
    broken out.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/tlb-pteaex.c
+++ arch/sh/mm/tlb-pteaex.c
@@ -17,15 +17,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mmu_context.h>
 #include <asm/cacheflush.h>
 
-void update_mmu_cache(struct vm_area_struct * vma,
-		      unsigned long address, pte_t pte)
+void __update_tlb(struct vm_area_struct *vma, unsigned long address, pte_t pte)
 {
-	unsigned long flags;
-	unsigned long pteval;
-	unsigned long vpn;
+	unsigned long flags, pteval, vpn;
 
-	/* Ptrace may call this routine. */
-	if (vma && current->active_mm != vma->vm_mm)
+	/*
+	 * Handle debugger faulting in for debugee.
+	 */
+	if (current->active_mm != vma->vm_mm)
 		return;
 
 	local_irq_save(flags);
