ceecd9c9ae85f868faf53598feea0d70820fafc0
Dan Williams <dan.j.williams@intel.com>
2009-07-28 14:44:04 -0700

    ioat: fix type mismatch for ->dmacount
    ->dmacount tracks the sequence number of active descriptors.  It is
    written to the DMACOUNT register to update the channel's view of pending
    descriptors in the chain.  The register is 16-bits so ->dmacount should
    be unsigned and 16-bit as well.  Also modify ->desccount to maintain
    alignment.
    
    This was never a problem in practice because we never compared dmacount
    values, but this is a bug waiting to happen.
    
    Signed-off-by: Maciej Sosnowski <maciej.sosnowski@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/dma/ioat/dma.h
+++ drivers/dma/ioat/dma.h
@@ -103,8 +103,8 @@ struct ioat_dma_chan {
 	struct delayed_work work;
 
 	int pending;
-	int dmacount;
-	int desccount;
+	u16 dmacount;
+	u16 desccount;
 
 	struct ioatdma_device *device;
 	struct dma_chan common;
