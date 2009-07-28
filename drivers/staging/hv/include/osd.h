d2446416ea8f219341d3dbdbb87893d7e12cce22
Bill Pemberton <wfp5p@virginia.edu>
2009-07-28 13:46:26 -0400

    Staging: hv: remove HANDLE typedef
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/include/osd.h
+++ drivers/staging/hv/include/osd.h
@@ -47,7 +47,6 @@ typedef struct _DLIST_ENTRY {
 /* Other types */
 
 /* typedef unsigned char		GUID[16]; */
-typedef void*				HANDLE;
 
 typedef void (*PFN_WORKITEM_CALLBACK)(void* context);
 typedef void (*PFN_TIMER_CALLBACK)(void* context);
