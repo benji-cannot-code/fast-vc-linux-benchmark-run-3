e30328ef4c5abf5568789e3f180eca67d3a1e1a3
David Woodhouse <David.Woodhouse@intel.com>
2009-07-29 10:25:58 +0100

    agp: kill phys_to_gart() and gart_to_phys()
    There seems to be no reason for these -- they're a 1:1 mapping on all
    platforms.
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- arch/parisc/include/asm/agp.h
+++ arch/parisc/include/asm/agp.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define unmap_page_from_agp(page)	/* nothing */
 #define flush_agp_cache()		mb()
 
-/* Convert a physical address to an address suitable for the GART. */
-#define phys_to_gart(x) (x)
-#define gart_to_phys(x) (x)
-
 /* GATT allocation. Returns/accepts GATT kernel virtual address. */
 #define alloc_gatt_pages(order)		\
 	((char *)__get_free_pages(GFP_KERNEL, (order)))
