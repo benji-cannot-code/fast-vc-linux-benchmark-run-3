683adc0f1aeaa15aa3da07da6ac78b0b47d0a102
Geert Uytterhoeven <Geert.Uytterhoeven@sonycom.com>
2009-07-29 02:06:42 +0000

    powerpc/cell: Move CBE_IOPTE_* to <asm/cell-regs.h>
    As <asm/iommu.h> doesn't contain any other hardware specific definitions
    but only interfaces.
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Geert Uytterhoeven <Geert.Uytterhoeven@sonycom.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/platforms/ps3/system-bus.c
+++ arch/powerpc/platforms/ps3/system-bus.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/udbg.h>
 #include <asm/lv1call.h>
 #include <asm/firmware.h>
-#include <asm/iommu.h>
+#include <asm/cell-regs.h>
 
 #include "platform.h"
 
