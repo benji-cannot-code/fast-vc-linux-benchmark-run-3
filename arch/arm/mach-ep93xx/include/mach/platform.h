829365051b94efa764e219cf00c3c9dd50c60577
Hartley Sweeten <hartleys@visionengravers.com>
2009-07-29 22:41:06 +0100

    ARM: 5628/1: ep93xx: Introduce Pulse Width Modulator (PWM) driver
    The EP93xx features two PWMs (one on the EP9307) with the following
    features:
    
      * Configurable dual output
      * Separate input clocks for each PWM output
      * 16-bit resolution
      * Programmable pulse width (duty cycle), interval (frequency), and
        polarity
    
    This adds the necessary core support as well as the driver.  A sysfs
    interface is provided to control the PWM outputs.
    
    Signed-off-by: Matthieu Crapet <mcrapet@gmail.com>
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Ryan Mallon <ryan@bluewatersys.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ep93xx/include/mach/platform.h
+++ arch/arm/mach-ep93xx/include/mach/platform.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 
 struct i2c_board_info;
+struct platform_device;
 
 struct ep93xx_eth_data
 {
@@ -33,6 +34,9 @@ static inline void ep93xx_devcfg_clear_bits(unsigned int bits)
 
 void ep93xx_register_eth(struct ep93xx_eth_data *data, int copy_addr);
 void ep93xx_register_i2c(struct i2c_board_info *devices, int num);
+void ep93xx_register_pwm(int pwm0, int pwm1);
+int ep93xx_pwm_acquire_gpio(struct platform_device *pdev);
+void ep93xx_pwm_release_gpio(struct platform_device *pdev);
 
 void ep93xx_init_devices(void);
 extern struct sys_timer ep93xx_timer;
