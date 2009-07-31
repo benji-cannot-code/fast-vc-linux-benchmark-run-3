6c49312af468f28eb5146bff4fe3fa8ab0162582
Magnus Damm <damm@igel.co.jp>
2009-07-31 07:47:17 +0000

    sh: Runtime PM pdev hwblk
    These patches extend struct platform device data for a bunch of
    SuperH Mobile processors and embedded boards. The patches simply
    add hardware block ids to on-chip platform devices. Platform
    devices off chip (such as external ethernet controllers or flash
    chips) are left out which gives them a special case hardware block
    id of zero.
    
    Upcoming Runtime PM code will make use of the hardware block id
    to group devices together. The hardware block id can also be used
    to extend the SuperH Mobile clock framework implementation.
    
    This series of patches depend on the following:
    "Driver Core: Add platform device arch data V3".
    
    This patch adds a hwblk_id member to struct pdev_archdata. This member
    should be used to point out on-chip hardware block id.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/device.h
+++ arch/sh/include/asm/device.h
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * This file is released under the GPLv2
  */
-#include <asm-generic/device.h>
+
+struct dev_archdata {
+};
 
 struct platform_device;
 /* allocate contiguous memory chunk and fill in struct resource */
@@ -13,3 +15,6 @@ int platform_resource_setup_memory(struct platform_device *pdev,
 
 void plat_early_device_setup(void);
 
+struct pdev_archdata {
+	int hwblk_id;
+};
