0f51dce5d76349721a6db24ba3111c7f2c2b51bd
Magnus Damm <damm@igel.co.jp>
2009-07-31 07:01:36 +0000

    sh: clean up MSTPCRn register definitions
    This patch removes the unused MSTPCRn register definitions
    from the SuperH Mobile code for sh7722, sh7723 and sh7724.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpu/sh4a/clock-sh7722.c
+++ arch/sh/kernel/cpu/sh4a/clock-sh7722.c
@@ -33,9 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SCLKBCR		0xa415000c
 #define IRDACLKCR	0xa4150018
 #define PLLCR		0xa4150024
-#define MSTPCR0		0xa4150030
-#define MSTPCR1		0xa4150034
-#define MSTPCR2		0xa4150038
 #define DLLFRQ		0xa4150050
 
 /* Fixed 32 KHz root clock for RTC and Power Management purposes */
