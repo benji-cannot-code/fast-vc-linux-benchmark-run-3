09910ec062d5734b7acaea12558c882d99bc9e53
Frederic Weisbecker <fweisbec@gmail.com>
2009-08-01 03:42:44 +0200

    tracing/filters: Cleanup useless headers
    Cleanup remaining headers inclusion that were only useful when
    the filter framework and its tracing related filesystem user interface
    weren't yet separated.
    
    v2: Keep module.h, needed for EXPORT_SYMBOL_GPL
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Tom Zanussi <tzanussi@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Li Zefan <lizf@cn.fujitsu.com>

--- kernel/trace/trace_events_filter.c
+++ kernel/trace/trace_events_filter.c
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2009 Tom Zanussi <tzanussi@gmail.com>
  */
 
-#include <linux/debugfs.h>
-#include <linux/uaccess.h>
 #include <linux/module.h>
 #include <linux/ctype.h>
 #include <linux/mutex.h>
