6e8aa4f2256ef11caa581d0a5746b25c7249f5dd
Lubomir Rintel <lkundrak@v3.sk>
2009-08-02 18:14:44 +0200

    ALSA: pcm - Tell user that stream to be rewound is suspended
    Return STRPIPE instead of EBADF when userspace attempts to rewind
    of forward a stream that was suspended in meanwhile, so that it
    can be recovered by snd_pcm_recover().
    
    This was causing Pulseaudio to unload the ALSA sink module under a race
    condition when it attempted to rewind the stream right after resume from
    suspend, before writing to the stream which would cause it to revive the
    stream otherwise. Tested to work with Pulseaudio patched to attempt to
    snd_pcm_recover() upon receiving an error from snd_pcm_rewind().
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -2209,6 +2209,9 @@ static snd_pcm_sframes_t snd_pcm_playback_rewind(struct snd_pcm_substream *subst
 	case SNDRV_PCM_STATE_XRUN:
 		ret = -EPIPE;
 		goto __end;
+	case SNDRV_PCM_STATE_SUSPENDED:
+		ret = -ESTRPIPE;
+		goto __end;
 	default:
 		ret = -EBADFD;
 		goto __end;
@@ -2253,6 +2256,9 @@ static snd_pcm_sframes_t snd_pcm_capture_rewind(struct snd_pcm_substream *substr
 		/* Fall through */
 	case SNDRV_PCM_STATE_XRUN:
 		ret = -EPIPE;
+		goto __end;
+	case SNDRV_PCM_STATE_SUSPENDED:
+		ret = -ESTRPIPE;
 		goto __end;
 	default:
 		ret = -EBADFD;
@@ -2300,6 +2306,9 @@ static snd_pcm_sframes_t snd_pcm_playback_forward(struct snd_pcm_substream *subs
 	case SNDRV_PCM_STATE_XRUN:
 		ret = -EPIPE;
 		goto __end;
+	case SNDRV_PCM_STATE_SUSPENDED:
+		ret = -ESTRPIPE;
+		goto __end;
 	default:
 		ret = -EBADFD;
 		goto __end;
@@ -2345,6 +2354,9 @@ static snd_pcm_sframes_t snd_pcm_capture_forward(struct snd_pcm_substream *subst
 		/* Fall through */
 	case SNDRV_PCM_STATE_XRUN:
 		ret = -EPIPE;
+		goto __end;
+	case SNDRV_PCM_STATE_SUSPENDED:
+		ret = -ESTRPIPE;
 		goto __end;
 	default:
 		ret = -EBADFD;
