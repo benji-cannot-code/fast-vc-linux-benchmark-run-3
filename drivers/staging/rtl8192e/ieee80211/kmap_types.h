cde37f9ab226dcc19a7561a0814d89888e1b64a9
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-04 15:57:55 -0700

    Staging: add Realtek 8192 PCI wireless driver
    This wireless driver should work for the Realtek 8192 PCI devices.
    
    It comes directly from Realtek and has been tested to work on at least
    one laptop in the wild.
    
    Cc: Anthony Wong <awong1@novell.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

---
+++ drivers/staging/rtl8192e/ieee80211/kmap_types.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __KMAP_TYPES_H
+
+#define __KMAP_TYPES_H
+
+
+enum km_type {
+	KM_BOUNCE_READ,
+	KM_SKB_SUNRPC_DATA,
+	KM_SKB_DATA_SOFTIRQ,
+	KM_USER0,
+	KM_USER1,
+	KM_BH_IRQ,
+	KM_SOFTIRQ0,
+	KM_SOFTIRQ1,
+	KM_TYPE_NR
+};
+
+#define _ASM_KMAP_TYPES_H
+
+#endif
