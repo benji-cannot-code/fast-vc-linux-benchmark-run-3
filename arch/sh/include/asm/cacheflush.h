36d7723527cf060e7fcf4b3e64e488b2bb67b875
Paul Mundt <lethal@linux-sh.org>
2009-08-04 16:02:43 +0900

    sh: Provide __flush_anon_page().
    This provides a __flush_anon_page() that handles both the aliasing and
    non-aliasing cases. This fixes up some crashes with heavy
    get_user_pages() users.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/cacheflush.h
+++ arch/sh/include/asm/cacheflush.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SH_CACHEFLUSH_H
 #define __ASM_SH_CACHEFLUSH_H
 
+#include <linux/mm.h>
+
 #ifdef __KERNEL__
 
 #ifdef CONFIG_CACHE_OFF
@@ -42,6 +44,18 @@ extern void __flush_wback_region(void *start, int size);
 extern void __flush_purge_region(void *start, int size);
 /* Flush (invalidate only) a region (smaller than a page) */
 extern void __flush_invalidate_region(void *start, int size);
+#endif
+
+#ifdef CONFIG_MMU
+#define ARCH_HAS_FLUSH_ANON_PAGE
+extern void __flush_anon_page(struct page *page, unsigned long);
+
+static inline void flush_anon_page(struct vm_area_struct *vma,
+				   struct page *page, unsigned long vmaddr)
+{
+	if (boot_cpu_data.dcache.n_aliases && PageAnon(page))
+		__flush_anon_page(page, vmaddr);
+}
 #endif
 
 #define ARCH_HAS_FLUSH_KERNEL_DCACHE_PAGE
