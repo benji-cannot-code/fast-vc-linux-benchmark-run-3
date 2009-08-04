1b6fd7a62d326e4bdfa65f580a276f155bcd25b1
Benjamin LaHaise <ben.lahaise@neterion.com>
2009-08-04 10:22:19 +0000

    vxge: Version update
    Update the driver version number for any bug reports from end users.
    
    Signed-off-by: Benjamin LaHaise <ben.lahaise@neterion.com>
    Signed-off-by: Ramkrishna Vepa <ram.vepa@neterion.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vxge/vxge-version.h
+++ drivers/net/vxge/vxge-version.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VXGE_VERSION_MAJOR	"2"
 #define VXGE_VERSION_MINOR	"0"
-#define VXGE_VERSION_FIX	"4"
-#define VXGE_VERSION_BUILD	"17899"
+#define VXGE_VERSION_FIX	"5"
+#define VXGE_VERSION_BUILD	"18053"
 #define VXGE_VERSION_FOR	"k"
 #endif
