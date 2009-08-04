acea7203a806de0dff97eda6f041a767f25ab08b
Jeff Mahoney <jeffm@suse.com>
2009-08-04 10:26:23 -0400

    Staging: rtl8192su: add linux/vmalloc.h
    Some arches don't provide vmalloc.h through the includes already included
    in rtl8192su. This patch adds it.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192su/r8192U_core.c
+++ drivers/staging/rtl8192su/r8192U_core.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Jerry chuang <wlanfae@realtek.com>
  */
 
+#include <linux/vmalloc.h>
 
 #undef LOOP_TEST
 #undef DUMP_RX
