2ff3d6f2296e6553d1b5271dec775d5e306de970
Gleb Natapov <gleb@redhat.com>
2009-08-04 15:30:27 +0300

    KVM: Call kvm_vcpu_kick() inside pic spinlock
    d5ecfdd25 moved it out because back than it was impossible to
    call it inside spinlock. This restriction no longer exists.
    
    Signed-off-by: Gleb Natapov <gleb@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/i8259.c
+++ arch/x86/kvm/i8259.c
@@ -44,11 +44,9 @@ static void pic_unlock(struct kvm_pic *s)
 {
 	struct kvm *kvm = s->kvm;
 	unsigned acks = s->pending_acks;
-	bool wakeup = s->wakeup_needed;
 	struct kvm_vcpu *vcpu;
 
 	s->pending_acks = 0;
-	s->wakeup_needed = false;
 
 	spin_unlock(&s->lock);
 
@@ -56,12 +54,6 @@ static void pic_unlock(struct kvm_pic *s)
 		kvm_notify_acked_irq(kvm, SELECT_PIC(__ffs(acks)),
 				     __ffs(acks));
 		acks &= acks - 1;
-	}
-
-	if (wakeup) {
-		vcpu = s->kvm->bsp_vcpu;
-		if (vcpu)
-			kvm_vcpu_kick(vcpu);
 	}
 }
 
@@ -528,7 +520,7 @@ static void pic_irq_request(void *opaque, int level)
 	s->output = level;
 	if (vcpu && level && (s->pics[0].isr_ack & (1 << irq))) {
 		s->pics[0].isr_ack &= ~(1 << irq);
-		s->wakeup_needed = true;
+		kvm_vcpu_kick(vcpu);
 	}
 }
 
