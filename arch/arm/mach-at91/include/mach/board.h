d5f1834df420d18391cf5185428c3d41a76bf98f
sedji gaouaou <sedji.gaouaou@atmel.com>
2009-08-06 15:20:22 +0100

    ARM: 5640/1: This patch modifies the support of AC97 on the at91sam9263 ek board
    This patch modifies the support of AC97 on the at91sam9263 ek board, so it would
    share the code with AVR32.
    Plus it removes a typo in at91sam9263_devices.c.
    
    Signed-off-by: Sedji Gaouaou <sedji.gaouaou@atmel.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-at91/include/mach/board.h
+++ arch/arm/mach-at91/include/mach/board.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/leds.h>
 #include <linux/spi/spi.h>
 #include <linux/usb/atmel_usba_udc.h>
+#include <sound/atmel-ac97c.h>
 
  /* USB Device */
 struct at91_udc_data {
@@ -175,10 +176,7 @@ struct atmel_lcdfb_info;
 extern void __init at91_add_device_lcdc(struct atmel_lcdfb_info *data);
 
  /* AC97 */
-struct atmel_ac97_data {
-	u8		reset_pin;	/* reset */
-};
-extern void __init at91_add_device_ac97(struct atmel_ac97_data *data);
+extern void __init at91_add_device_ac97(struct ac97c_platform_data *data);
 
  /* ISI */
 extern void __init at91_add_device_isi(void);
