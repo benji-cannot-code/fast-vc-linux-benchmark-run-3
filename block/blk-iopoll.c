5da36128a5c22a3c902b996caec7ee92d120579c
Jens Axboe <jens.axboe@oracle.com>
2009-08-06 20:50:48 +0200

    block: adjust default budget for blk-iopoll
    It's not exported, I doubt we'll have a reason to change this...
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/blk-iopoll.c
+++ block/blk-iopoll.c
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int blk_iopoll_enabled = 1;
 EXPORT_SYMBOL(blk_iopoll_enabled);
 
+static unsigned int blk_iopoll_budget __read_mostly = 256;
+
 static DEFINE_PER_CPU(struct list_head, blk_cpu_iopoll);
 
 /**
@@ -79,8 +81,8 @@ EXPORT_SYMBOL(blk_iopoll_complete);
 static void blk_iopoll_softirq(struct softirq_action *h)
 {
 	struct list_head *list = &__get_cpu_var(blk_cpu_iopoll);
+	int rearm = 0, budget = blk_iopoll_budget;
 	unsigned long start_time = jiffies;
-	int rearm = 0, budget = 64;
 
 	local_irq_disable();
 
