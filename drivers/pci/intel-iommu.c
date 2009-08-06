a65bce82ca95f590c90de8b404e2d26161f11451
Sheng Yang <sheng@linux.intel.com>
2009-08-06 13:31:56 +0800

    intel-iommu: Fix enabling snooping feature by mistake
    Two defects work together result in KVM device passthrough randomly can't
    work:
    1. iommu_snooping is not initialized to zero when vm_iommu_init() called.
    So it is possible to get a random value.
    2. One line added by commit 2c2e2c38("IOMMU Identity Mapping Support")
    change the code path, let it bypass domain_update_iommu_cap(), as well as
    missing the increment of domain iommu reference count.
    
    The latter is also likely to cause a leak of domains on repeated VMM 
    assignment and deassignment.
    
    Signed-off-by: Sheng Yang <sheng@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/pci/intel-iommu.c
+++ drivers/pci/intel-iommu.c
@@ -1506,7 +1506,6 @@ static int domain_context_mapping_one(struct dmar_domain *domain, int segment,
 			}
 
 			set_bit(num, iommu->domain_ids);
-			set_bit(iommu->seq_id, &domain->iommu_bmp);
 			iommu->domains[num] = domain;
 			id = num;
 		}
@@ -3410,6 +3409,7 @@ static int md_domain_init(struct dmar_domain *domain, int guest_width)
 
 	domain->iommu_count = 0;
 	domain->iommu_coherency = 0;
+	domain->iommu_snooping = 0;
 	domain->max_addr = 0;
 
 	/* always allocate the top pgd */
