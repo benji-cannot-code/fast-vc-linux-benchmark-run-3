bf60266248edf62057791fc1f998c0b6cf0a6098
Mauro Carvalho Chehab <mchehab@redhat.com>
2009-08-07 12:13:31 -0300

    V4L/DVB (12403): em28xx: properly reports some em2710 chips
    As reported by hermann pitton <hermann-pitton@arcor.de>, some devices
    has a different chip id for em2710 (likely the older ones):
    
    em28xx: New device @ 480 Mbps (eb1a:2710, interface 0, class 0)
    em28xx #0: Identified as EM2710/EM2750/EM2751 webcam grabber (card=22)
    em28xx #0: em28xx chip ID = 17
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-reg.h
+++ drivers/media/video/em28xx/em28xx-reg.h
@@ -177,7 +177,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* FIXME: Need to be populated with the other chip ID's */
 enum em28xx_chip_id {
-	CHIP_ID_EM2820 = 18,	/* Also used by em2710 */
+	CHIP_ID_EM2710 = 17,
+	CHIP_ID_EM2820 = 18,	/* Also used by some em2710 */
 	CHIP_ID_EM2840 = 20,
 	CHIP_ID_EM2750 = 33,
 	CHIP_ID_EM2860 = 34,
