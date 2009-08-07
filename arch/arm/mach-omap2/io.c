8ad16ce26fee88c6060a221cf784ac5ac55cb070
Tomi Valkeinen <tomi.valkeinen@nokia.com>
2009-08-07 12:01:55 +0300

    OMAP: Add VRAM manager
    Add a Video RAM manager for OMAP 2 and 3 platforms. VRAM manager is used
    to allocate large continuous blocks of SDRAM or SRAM. The features VRAM
    manager has that are missing from dma_alloc_* functions are:
    
    - Support for OMAP2's SRAM
    - Allocate without ioremapping
    - Allocate at defined physical addresses
    - Allows larger VRAM area and larger allocations
    
    The upcoming DSS2 uses VRAM manager.
    
    VRAM area size can be defined in kernel config, board file or with
    kernel boot parameters. Board file definition overrides kernel config,
    and boot parameter overrides kernel config and board file.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/sdrc.h>
 #include <plat/gpmc.h>
 #include <plat/serial.h>
+#include <plat/vram.h>
 
 #ifndef CONFIG_ARCH_OMAP4	/* FIXME: Remove this once clkdev is ready */
 #include "clock.h"
@@ -265,6 +266,7 @@ void __init omap2_map_common_io(void)
 	omap2_check_revision();
 	omap_sram_init();
 	omapfb_reserve_sdram();
+	omap_vram_reserve_sdram();
 }
 
 /*
