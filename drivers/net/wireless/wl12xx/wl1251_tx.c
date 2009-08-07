80b266d15232618dc053bbaddf6656e6f2ea9314
Bob Copeland <me@bobcopeland.com>
2009-08-07 13:33:26 +0300

    wl1251: move module probe methods into spi.c
    This change moves all of the spi specific code from main.c into
    spi.c.  The module initialization code also moves, but common
    code for initializing mac80211 etc. stays in main.c, as this will
    eventually form a common library module also used by wl1251_sdio.
    
    Signed-off-by: Bob Copeland <me@bobcopeland.com>
    Signed-off-by: Kalle Valo <kalle.valo@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1251_tx.c
+++ drivers/net/wireless/wl12xx/wl1251_tx.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "wl1251.h"
 #include "reg.h"
-#include "wl1251_spi.h"
 #include "wl1251_tx.h"
 #include "wl1251_ps.h"
 #include "wl1251_io.h"
