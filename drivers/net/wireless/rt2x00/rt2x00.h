7bd7b078f61d10eca0263cd8a6467ebcd5ee2fca
Lars Ericsson <Lars_Ericsson@telia.com>
2009-08-08 23:54:51 +0200

    rt2x00: Fix rounding errors in RSSI average calculation
    Small changes in signal level was not detected up by the
    MOVING_AVERAGE() due to a rounding error, using 'int' type.
    
    rt2x00lib_antenna_diversity_eval:
    rssi:	  -62 -62 -62 -62 -62 -62 -62 -62 -62 -62 -62 -62 -62 -62 -62
    rssi_avg: -57 -57 -57 -57 -57 -57 -57 -57 -57 -57 -57 -57 -57 -57 -57
    
    The signal level reported back could be significantly (5dBm)
    different from the actual value. A level +3dBm is the same as
    double the AP output power.
    
    Signed-off-by: Lars Ericsson <Lars_Ericsson@telia.com>
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00.h
+++ drivers/net/wireless/rt2x00/rt2x00.h
@@ -135,6 +135,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				  GET_DURATION(IEEE80211_HEADER + ACK_SIZE, 10) )
 
 /*
+ * Structure for average calculation
+ * The avg field contains the actual average value,
+ * but avg_weight is internally used during calculations
+ * to prevent rounding errors.
+ */
+struct avg_val {
+	int avg;
+	int avg_weight;
+};
+
+/*
  * Chipset identification
  * The chipset on the device is composed of a RT and RF chip.
  * The chipset combination is important for determining device capabilities.
@@ -257,7 +268,7 @@ struct link_ant {
 	 * Similar to the avg_rssi in the link_qual structure
 	 * this value is updated by using the walking average.
 	 */
-	int rssi_ant;
+	struct avg_val rssi_ant;
 };
 
 /*
@@ -286,7 +297,7 @@ struct link {
 	/*
 	 * Currently active average RSSI value
 	 */
-	int avg_rssi;
+	struct avg_val avg_rssi;
 
 	/*
 	 * Currently precalculated percentages of successful
