db5b0ed26334da892fca0e45081b8d2acf16e2f2
Chuck Lever <chuck.lever@oracle.com>
2009-08-09 15:09:30 -0400

    NFS: Add ability to send MOUNTPROC_UMNT to the kernel's mountd client
    After certain failure modes of an NFS mount, an NFS client should send
    a MOUNTPROC_UMNT request to remove the just-added mount entry from the
    server's mount table.  While no-one should rely on the accuracy of the
    server's mount table, sending a UMNT is simply being a good internet
    neighbor.
    
    Since NFS mount processing is handled in the kernel now, we will need
    a function in the kernel's mountd client that can post a MOUNTRPC_UMNT
    request, in order to handle these failure modes.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -103,6 +103,7 @@ struct nfs_mount_request {
 };
 
 extern int nfs_mount(struct nfs_mount_request *info);
+extern void nfs_umount(const struct nfs_mount_request *info);
 
 /* client.c */
 extern struct rpc_program nfs_program;
