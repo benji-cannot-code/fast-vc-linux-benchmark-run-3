c4693256533bf52aa5257e56efcc2a62a74c5b3b
Martyn Welch <martyn.welch@gefanuc.com>
2009-08-11 14:37:15 +0100

    Staging: Update VME vme_user module
    * Add ifdef wrapper to vme_user.h
     * Correct Initialisation routine
     * Add remove routine to correct probe routine
     * Remove old test routines
    
    Signed-off-by: Martyn Welch <martyn.welch@gefanuc.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vme/devices/vme_user.h
+++ drivers/staging/vme/devices/vme_user.h
@@ -1,10 +1,14 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _VME_USER_H_
+#define _VME_USER_H_
+
+#define USER_BUS_MAX                  1
 
 /*
  * VMEbus Master Window Configuration Structure
  */
-typedef struct {
-	char enable;			/* State of Window */
+struct vme_master {
+	int enable;			/* State of Window */
 	unsigned long long vme_addr;	/* Starting Address on the VMEbus */
 	unsigned long long size;	/* Window Size */
 	vme_address_t aspace;		/* Address Space */
@@ -15,7 +19,7 @@ typedef struct {
 	int prefetchSize;		/* Prefetch Read Size (Cache Lines) */
 	char wrPostEnable;		/* Write Post State */
 #endif
-} vme_master_t;
+};
 
 
 /*
@@ -27,8 +31,8 @@ typedef struct {
 
 
 /* VMEbus Slave Window Configuration Structure */
-typedef struct {
-	char enable;			/* State of Window */
+struct vme_slave {
+	int enable;			/* State of Window */
 	unsigned long long vme_addr;	/* Starting Address on the VMEbus */
 	unsigned long long size;	/* Window Size */
 	vme_address_t aspace;		/* Address Space */
@@ -38,7 +42,12 @@ typedef struct {
 	char rmwLock;			/* Lock PCI during RMW Cycles */
 	char data64BitCapable;		/* non-VMEbus capable of 64-bit Data */
 #endif
-} vme_slave_t;
+};
 
-#define VME_SET_SLAVE _IOW(VME_IOC_MAGIC, 10, vme_slave_t)
+#define VME_GET_SLAVE _IOR(VME_IOC_MAGIC, 1, struct vme_slave)
+#define VME_SET_SLAVE _IOW(VME_IOC_MAGIC, 2, struct vme_slave)
+#define VME_GET_MASTER _IOR(VME_IOC_MAGIC, 3, struct vme_master)
+#define VME_SET_MASTER _IOW(VME_IOC_MAGIC, 4, struct vme_master)
+
+#endif /* _VME_USER_H_ */
 
