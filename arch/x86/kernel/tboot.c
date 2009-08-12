1cb30e65b44b9115e36b10b8f3de0514ded32125
H. Peter Anvin <hpa@zytor.com>
2009-08-12 05:45:34 -0700

    x86, intel_txt: tboot.c needs <asm/fixmap.h>
    arch/x86/kernel/tboot.c needs <asm/fixmap.h>.  In most configurations
    that ends up getting implicitly included, but not in all, causing
    build failures in some configurations.
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Cc: Joseph Cihula <joseph.cihula@intel.com>
    Cc: Shane Wang <shane.wang@intel.com>

--- arch/x86/kernel/tboot.c
+++ arch/x86/kernel/tboot.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/bootparam.h>
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
+#include <asm/fixmap.h>
 #include <asm/setup.h>
 #include <asm/tboot.h>
 #include <asm/e820.h>
