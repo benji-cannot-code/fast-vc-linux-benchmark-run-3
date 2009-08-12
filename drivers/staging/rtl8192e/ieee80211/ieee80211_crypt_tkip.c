92ed5a2ad9fcbc6c1c5739611a95a7ea5a10c83c
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-12 16:50:57 -0700

    Staging: rtl8192e: remove unneeded ieee80211 files
    These files are not even built or used, so just remove them.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192e/ieee80211/ieee80211_crypt_tkip.c
+++ drivers/staging/rtl8192e/ieee80211/ieee80211_crypt_tkip.c
@@ -23,9 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/string.h>
 
 #include "ieee80211.h"
-#if (LINUX_VERSION_CODE > KERNEL_VERSION(2,6,20))
-//#include "crypto_compat.h"
-#endif
 
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,5,0))
--- drivers/staging/rtl8192e/ieee80211/ieee80211_crypt_wep.c
+++ drivers/staging/rtl8192e/ieee80211/ieee80211_crypt_wep.c
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ieee80211.h"
 
-#if (LINUX_VERSION_CODE > KERNEL_VERSION(2,6,20))
-//#include "crypto_compat.h"
-#endif
-
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,5,0))
 #include "rtl_crypto.h"
