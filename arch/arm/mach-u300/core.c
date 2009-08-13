1dad4f2b2b230f55ddd0a7b0e8ddead7ffdd9961
Linus Walleij <linus.walleij@stericsson.com>
2009-08-13 21:42:01 +0100

    ARM: 5668/2: U300 I2C board setup
    This sets up the U300 I2C subdevices so that the AB3100
    analog baseband ASIC is properly detected and also the
    camera devices in the U335 reference design get properly
    registered.
    
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-u300/core.c
+++ arch/arm/mach-u300/core.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "clock.h"
 #include "mmc.h"
 #include "spi.h"
+#include "i2c.h"
 
 /*
  * Static I/O mappings that are needed for booting the U300 platforms. The
@@ -380,14 +381,14 @@ static struct platform_device wdog_device = {
 };
 
 static struct platform_device i2c0_device = {
-	.name = "stddci2c",
+	.name = "stu300",
 	.id = 0,
 	.num_resources = ARRAY_SIZE(i2c0_resources),
 	.resource = i2c0_resources,
 };
 
 static struct platform_device i2c1_device = {
-	.name = "stddci2c",
+	.name = "stu300",
 	.id = 1,
 	.num_resources = ARRAY_SIZE(i2c1_resources),
 	.resource = i2c1_resources,
@@ -625,6 +626,9 @@ void __init u300_init_devices(void)
 	u300_unclock_primecells();
 
 	u300_assign_physmem();
+
+	/* Register subdevices on the I2C buses */
+	u300_i2c_register_board_devices();
 
 	/* Register subdevices on the SPI bus */
 	u300_spi_register_board_devices();
