b855b742b6332beb153d5cdf57311616ad4a0f12
Neil Horman <nhorman@tuxdriver.com>
2009-08-13 05:19:44 +0000

    net: skb ftracer - add tracepoint to skb_copy_datagram_iovec (v3)
    skb allocation / cosumption tracer - Add consumption tracepoint
    
    This patch adds a tracepoint to skb_copy_datagram_iovec, which is called each
    time a userspace process copies a frame from a socket receive queue to a user
    space buffer.  It allows us to hook in and examine each sk_buff that the system
    receives on a per-socket bases, and can be use to compile a list of which skb's
    were received by which processes.
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    
     include/trace/events/skb.h |   20 ++++++++++++++++++++
     net/core/datagram.c        |    3 +++
     2 files changed, 23 insertions(+)
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/datagram.c
+++ net/core/datagram.c
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/checksum.h>
 #include <net/sock.h>
 #include <net/tcp_states.h>
+#include <trace/events/skb.h>
 
 /*
  *	Is a socket 'connection oriented' ?
@@ -284,6 +285,8 @@ int skb_copy_datagram_iovec(const struct sk_buff *skb, int offset,
 	int start = skb_headlen(skb);
 	int i, copy = start - offset;
 	struct sk_buff *frag_iter;
+
+	trace_skb_copy_datagram_iovec(skb, len);
 
 	/* Copy header. */
 	if (copy > 0) {
