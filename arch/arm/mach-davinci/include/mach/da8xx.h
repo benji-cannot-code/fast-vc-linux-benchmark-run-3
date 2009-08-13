bb81921da2d8eee2cbce10b740d211c7f2dd3ae2
Sudhakar Rajashekhara <sudhakar.raj@ti.com>
2009-08-13 17:36:25 -0400

    davinci: Add NOR flash support for da850/omap-l138
    This patch adds platform data for the 8MB NOR flash
    found on da850/omap-l138 EVM. Both NOR and NAND can
    co-exist on da850/omap-l138 as they are using different
    chip selects.
    
    Signed-off-by: Sudhakar Rajashekhara <sudhakar.raj@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/include/mach/da8xx.h
+++ arch/arm/mach-davinci/include/mach/da8xx.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DA8XX_PSC1_BASE		0x01e27000
 #define DA8XX_LCD_CNTRL_BASE	0x01e13000
 #define DA8XX_MMCSD0_BASE	0x01c40000
+#define DA8XX_AEMIF_CS2_BASE	0x60000000
 #define DA8XX_AEMIF_CS3_BASE	0x62000000
 #define DA8XX_AEMIF_CTL_BASE	0x68000000
 
@@ -114,6 +115,7 @@ extern const short da850_mcasp_pins[];
 extern const short da850_lcdcntl_pins[];
 extern const short da850_mmcsd0_pins[];
 extern const short da850_nand_pins[];
+extern const short da850_nor_pins[];
 
 int da8xx_pinmux_setup(const short pins[]);
 
