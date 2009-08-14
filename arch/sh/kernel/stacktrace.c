abb9680cb10306ac0a6f894037189f96b3de3d4e
Paul Mundt <lethal@linux-sh.org>
2009-08-15 01:05:46 +0900

    sh: stacktrace: Add reliability checks in address saving ops.
    This adopts the reliability checks from the x86 stacktrace code so known
    bad addresses are not recorded in the stack trace buffer.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/stacktrace.c
+++ arch/sh/kernel/stacktrace.c
@@ -39,6 +39,9 @@ static void save_stack_address(void *data, unsigned long addr, int reliable)
 {
 	struct stack_trace *trace = data;
 
+	if (!reliable)
+		return;
+
 	if (trace->skip > 0) {
 		trace->skip--;
 		return;
@@ -67,6 +70,9 @@ static void
 save_stack_address_nosched(void *data, unsigned long addr, int reliable)
 {
 	struct stack_trace *trace = (struct stack_trace *)data;
+
+	if (!reliable)
+		return;
 
 	if (in_sched_functions(addr))
 		return;
