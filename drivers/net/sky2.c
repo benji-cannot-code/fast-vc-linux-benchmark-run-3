396ab2fb5e27a94fbafd57acc2e021243e515ab7
Stephen Hemminger <shemminger@vyatta.com>
2009-08-14 05:15:20 +0000

    sky2: version 1.24
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sky2.c
+++ drivers/net/sky2.c
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sky2.h"
 
 #define DRV_NAME		"sky2"
-#define DRV_VERSION		"1.23"
+#define DRV_VERSION		"1.24"
 #define PFX			DRV_NAME " "
 
 /*
