8afcb365b3411f29a50abb365a77ee98143db849
Paul Mundt <lethal@linux-sh.org>
2009-08-15 11:25:32 +0900

    sh: consolidate flush_dcache_mmap_lock/unlock() definitions.
    All of the flush_dcache_mmap_lock()/flush_dcache_mmap_unlock()
    definitions are identical across all CPUs, so just provide them
    generically in asm/cacheflush.h.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/cpu-common/cpu/cacheflush.h
+++ arch/sh/include/cpu-common/cpu/cacheflush.h
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define flush_cache_range(vma, start, end)	do { } while (0)
 #define flush_cache_page(vma, vmaddr, pfn)	do { } while (0)
 #define flush_dcache_page(page)			do { } while (0)
-#define flush_dcache_mmap_lock(mapping)		do { } while (0)
-#define flush_dcache_mmap_unlock(mapping)	do { } while (0)
 #define flush_icache_range(start, end)		do { } while (0)
 #define flush_icache_page(vma,pg)		do { } while (0)
 #define flush_icache_user_range(vma,pg,adr,len)	do { } while (0)
