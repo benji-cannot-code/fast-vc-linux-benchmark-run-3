fad136c075c8a197db12e635afe5b0ff46935034
Matt Fleming <matt@console-pimps.org>
2009-08-16 15:56:35 +0100

    sh: unwinder: Set the flags for DW_CFA_val_offset ops as DWARF_VAL_OFFSET
    The handling of DW_CFA_val_offset ops was incorrectly using the
    DWARF_REG_OFFSET flag but the register's value cannot be calculated
    using the DWARF_REG_OFFSET method. Create a new flag to indicate that a
    different method must be used to calculate the register's value even
    though there is no implementation for DWARF_VAL_OFFSET yet; it's mainly
    just a place holder.
    
    Signed-off-by: Matt Fleming <matt@console-pimps.org>

--- arch/sh/include/asm/dwarf.h
+++ arch/sh/include/asm/dwarf.h
@@ -297,6 +297,7 @@ struct dwarf_reg {
 	unsigned long addr;
 	unsigned long flags;
 #define DWARF_REG_OFFSET	(1 << 0)
+#define DWARF_VAL_OFFSET	(1 << 1)
 };
 
 /*
--- arch/sh/kernel/dwarf.c
+++ arch/sh/kernel/dwarf.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * TODO:
  *	- DWARF64 doesn't work.
+ *	- Registers with DWARF_VAL_OFFSET rules aren't handled properly.
  */
 
 /* #define DEBUG */
@@ -500,7 +501,7 @@ static int dwarf_cfa_execute_insns(unsigned char *insn_start,
 			count = dwarf_read_leb128(current_insn, &offset);
 			offset *= cie->data_alignment_factor;
 			regp = dwarf_frame_alloc_reg(frame, reg);
-			regp->flags |= DWARF_REG_OFFSET;
+			regp->flags |= DWARF_VAL_OFFSET;
 			regp->addr = offset;
 			break;
 		case DW_CFA_GNU_args_size:
