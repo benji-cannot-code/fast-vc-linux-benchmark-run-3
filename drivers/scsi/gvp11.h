a1608c553bfde085b16c4be69534d4ee988f56c7
Geert Uytterhoeven <geert@linux-m68k.org>
2009-08-16 11:17:35 +0200

    m68k: amiga - GVP Series II SCSI zorro_driver conversion
    Acked-by: James Bottomley <James.Bottomley@suse.de>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- drivers/scsi/gvp11.h
+++ drivers/scsi/gvp11.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-int gvp11_detect(struct scsi_host_template *);
-int gvp11_release(struct Scsi_Host *);
-
 #ifndef CMD_PER_LUN
 #define CMD_PER_LUN		2
 #endif
@@ -22,8 +19,6 @@ int gvp11_release(struct Scsi_Host *);
 #ifndef CAN_QUEUE
 #define CAN_QUEUE		16
 #endif
-
-#ifndef HOSTS_C
 
 /*
  * if the transfer address ANDed with this results in a non-zero
@@ -54,7 +49,5 @@ struct gvp11_scsiregs {
 #define GVP11_DMAC_INT_PENDING	(1<<1)
 #define GVP11_DMAC_INT_ENABLE	(1<<3)
 #define GVP11_DMAC_DIR_WRITE	(1<<4)
-
-#endif /* else def HOSTS_C */
 
 #endif /* GVP11_H */
