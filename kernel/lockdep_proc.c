4cfdb116eb62147a6f9c25961a08d187bc8dfda5
Li Zefan <lizf@cn.fujitsu.com>
2009-08-17 13:39:49 +0800

    lockdep: Fix missing entries in /proc/lock_chains
    Two entries are missing in the output of /proc/lock_chains.
    
    One is chains[1]. When lc_next() is called the 1st time,
    chains[0] is returned. And when it's called the 2nd time,
    chains[2] is returned.
    
    The other missing ons is, when lc_start() is called the 2nd
    time, we should start from chains[@pos-1] but not chains[@pos],
    because pos == 0 is the header.
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <4A88ED25.2040306@cn.fujitsu.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/lockdep_proc.c
+++ kernel/lockdep_proc.c
@@ -161,8 +161,8 @@ static void *lc_next(struct seq_file *m, void *v, loff_t *pos)
 	else {
 		chain = v;
 
-		if (*pos < nr_lock_chains)
-			chain = lock_chains + *pos;
+		if (*pos - 1 < nr_lock_chains)
+			chain = lock_chains + (*pos - 1);
 		else
 			chain = NULL;
 	}
@@ -175,8 +175,8 @@ static void *lc_start(struct seq_file *m, loff_t *pos)
 	if (*pos == 0)
 		return SEQ_START_TOKEN;
 
-	if (*pos < nr_lock_chains)
-		return lock_chains + *pos;
+	if (*pos - 1 < nr_lock_chains)
+		return lock_chains + (*pos - 1);
 
 	return NULL;
 }
