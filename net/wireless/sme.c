0589c9598214e934ef856cb74af72bdb5a650a4f
David Kilroy <kilroyd@googlemail.com>
2009-08-19 00:43:31 +0100

    cfg80211: fix leaks of wdev->conn->ie
    This only occurs in the following error situations:
     - driver calls connect_result with failure
     - error scheduling authentication on connect
     - error initiating scan (to get BSSID and channel) on
       connect
     - userspace calls disconnect while in the SCANNING or
       SCAN_AGAIN states
    
    Signed-off-by: David Kilroy <kilroyd@googlemail.com>
    Reviewed-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/wireless/sme.c
+++ net/wireless/sme.c
@@ -396,6 +396,8 @@ void __cfg80211_connect_result(struct net_device *dev, const u8 *bssid,
 
 	if (status != WLAN_STATUS_SUCCESS) {
 		wdev->sme_state = CFG80211_SME_IDLE;
+		if (wdev->conn)
+			kfree(wdev->conn->ie);
 		kfree(wdev->conn);
 		wdev->conn = NULL;
 		kfree(wdev->connect_keys);
@@ -780,6 +782,7 @@ int __cfg80211_connect(struct cfg80211_registered_device *rdev,
 			}
 		}
 		if (err) {
+			kfree(wdev->conn->ie);
 			kfree(wdev->conn);
 			wdev->conn = NULL;
 			wdev->sme_state = CFG80211_SME_IDLE;
@@ -849,6 +852,7 @@ int __cfg80211_disconnect(struct cfg80211_registered_device *rdev,
 		    (wdev->conn->state == CFG80211_CONN_SCANNING ||
 		     wdev->conn->state == CFG80211_CONN_SCAN_AGAIN)) {
 			wdev->sme_state = CFG80211_SME_IDLE;
+			kfree(wdev->conn->ie);
 			kfree(wdev->conn);
 			wdev->conn = NULL;
 			wdev->ssid_len = 0;
