cc9b22a231cd45dba8b64c7b8e4b49cd5f42843f
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-17 17:21:51 -0700

    Staging: hv: cleanup coding style issues in VersionInfo.h
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/VersionInfo.h
+++ drivers/staging/hv/VersionInfo.h
@@ -22,9 +22,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#ifndef __HV_VERSION_INFO
+#define __HV_VERSION_INFO
 
-#pragma once
+static const char VersionDate[] = __DATE__;
+static const char VersionTime[] = __TIME__;
+static const char VersionDesc[] = "Version 2.0";
 
-static const char VersionDate[]=__DATE__;
-static const char VersionTime[]=__TIME__;
-static const char VersionDesc[]= "Version 2.0";
+#endif
