084f763c07b7bc54d66a8fc6a94ab04b76523429
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-19 16:21:28 -0700

    Staging: hv: move vmbus.h
    This moves vmbus.h from the include/ subdirectory.  It doesn't
    belong there.
    
    No code changes happened here.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/storvsc_drv.c
+++ drivers/staging/hv/storvsc_drv.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "osd.h"
 #include "include/logging.h"
-#include "include/vmbus.h"
+#include "vmbus.h"
 #include "include/StorVscApi.h"
 
 
