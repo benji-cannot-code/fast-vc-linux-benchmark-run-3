51e0ba835f852e573fcfeae56d72daf7a1dd7f6a
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-19 16:20:41 -0700

    Staging: hv: remove include/HvHalApi.h
    It isn't needed at all, was only being used for one typedef,
    which is now removed.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/Hv.h
+++ drivers/staging/hv/Hv.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __HV_H__
 
 #include "include/HvStatus.h"
-#include "include/HvHalApi.h"
 #include "include/HvVpApi.h"
 #include "include/HvSynicApi.h"
 #include "include/HvHcApi.h"
