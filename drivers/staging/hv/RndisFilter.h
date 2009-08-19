2649c53402b6346821b9c3d7e33394c41f824474
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-19 16:26:24 -0700

    Staging: hv: rndis.h: typedef removal, part 1
    This removes about half of the typedefs in rndis.h
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/RndisFilter.h
+++ drivers/staging/hv/RndisFilter.h
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "rndis.h"
 
-#define RNDIS_HEADER_SIZE	(sizeof(RNDIS_MESSAGE) - \
-				 sizeof(RNDIS_MESSAGE_CONTAINER))
+#define RNDIS_HEADER_SIZE	(sizeof(struct rndis_message) - \
+				 sizeof(union rndis_message_container))
 
 #define NDIS_PACKET_TYPE_DIRECTED	0x00000001
 #define NDIS_PACKET_TYPE_MULTICAST	0x00000002
