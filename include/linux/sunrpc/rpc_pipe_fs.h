6db0472f42a222e1fbaea90bc791ef223714f7c8
J. Bruce Fields <bfields@citi.umich.edu>
2009-08-20 17:08:39 -0400

    nfs: fix compile error in rpc_pipefs.h
    This include is needed for the definition of delayed_work.
    
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- include/linux/sunrpc/rpc_pipe_fs.h
+++ include/linux/sunrpc/rpc_pipe_fs.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __KERNEL__
 
+#include <linux/workqueue.h>
+
 struct rpc_pipe_msg {
 	struct list_head list;
 	void *data;
