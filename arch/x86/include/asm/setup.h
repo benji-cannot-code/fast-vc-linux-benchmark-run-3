7161a26835811211504ef0b434231d36731ed129
Thomas Gleixner <tglx@linutronix.de>
2009-08-20 10:35:46 +0200

    x86: Move traps_init to x86_init_ops
    Replace the quirks by a simple x86_init_ops function.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/setup.h
+++ arch/x86/include/asm/setup.h
@@ -17,10 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct x86_quirks {
 	int (*arch_pre_time_init)(void);
 	int (*arch_time_init)(void);
-	int (*arch_trap_init)(void);
 };
-
-extern void x86_quirk_trap_init(void);
 
 extern void x86_quirk_pre_time_init(void);
 extern void x86_quirk_time_init(void);
