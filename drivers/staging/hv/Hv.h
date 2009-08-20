95809520af9a9b78107fa94046721e0a10e14f46
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-20 12:13:27 -0700

    Staging: hv: create hv_api.h
    Merge the different include/Hv*Api.h files together into
    hv_api.h as they really don't justify separate files.
    
    No code was changed here, only moving stuff around.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/Hv.h
+++ drivers/staging/hv/Hv.h
@@ -27,10 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __HV_H__
 
 #include "include/HvStatus.h"
-#include "include/HvVpApi.h"
-#include "include/HvSynicApi.h"
-#include "include/HvHcApi.h"
-#include "include/HvPtApi.h"
+#include "hv_api.h"
 
 enum {
 	VMBUS_MESSAGE_CONNECTION_ID	= 1,
