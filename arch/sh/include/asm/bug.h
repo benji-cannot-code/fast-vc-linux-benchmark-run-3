d218a6c72da50e003d211974cb2a5652200e5999
Paul Mundt <lethal@linux-sh.org>
2009-08-22 05:28:25 +0900

    sh: unwinder: Use a special bug flag for unwinder traps.
    This simplifies the unwinder trap handling, dropping the use of the
    special trapa vector and simply piggybacking on top of the BUG support. A
    new BUGFLAG_UNWINDER is added for flagging the unwinder fault, before
    continuing on with regular BUG dispatch.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/bug.h
+++ arch/sh/include/asm/bug.h
@@ -2,8 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SH_BUG_H
 #define __ASM_SH_BUG_H
 
-#define TRAPA_UNWINDER_BUG_OPCODE 0xc33b /* trapa #0x3b */
 #define TRAPA_BUG_OPCODE	0xc33e	/* trapa #0x3e */
+#define BUGFLAG_UNWINDER	(1 << 1)
 
 #ifdef CONFIG_GENERIC_BUG
 #define HAVE_ARCH_BUG
@@ -74,15 +74,16 @@ do {							\
 	unlikely(__ret_warn_on);				\
 })
 
-#define UNWINDER_BUG()						\
+#define UNWINDER_BUG()					\
 do {							\
 	__asm__ __volatile__ (				\
 		"1:\t.short %O0\n"			\
-		_EMIT_BUG_ENTRY		\
+		_EMIT_BUG_ENTRY				\
 		 :					\
-		 : "n" (TRAPA_UNWINDER_BUG_OPCODE),	\
+		 : "n" (TRAPA_BUG_OPCODE),		\
 		   "i" (__FILE__),			\
-		   "i" (__LINE__), "i" (0),		\
+		   "i" (__LINE__),			\
+		   "i" (BUGFLAG_UNWINDER),		\
 		   "i" (sizeof(struct bug_entry)));	\
 } while (0)
 
