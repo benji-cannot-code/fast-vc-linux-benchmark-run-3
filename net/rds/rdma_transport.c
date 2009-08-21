ae5a3768dc6dde3df8a0cacfe251fefeb607d69e
Andy Grover <andy.grover@oracle.com>
2009-08-21 12:28:33 +0000

    RDS: Modularize RDMA and TCP transports
    Enable the building of transports as modules.
    
    Also, improve consistency of Kconfig messages in relation to other
    protocols, and move build dependency on IB from the RDS core code
    to the rds_rdma module.
    
    Signed-off-by: Andy Grover <andy.grover@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/rds/rdma_transport.c
+++ net/rds/rdma_transport.c
@@ -204,6 +204,7 @@ err_iw_init:
 out:
 	return ret;
 }
+module_init(rds_rdma_init);
 
 void rds_rdma_exit(void)
 {
@@ -212,4 +213,9 @@ void rds_rdma_exit(void)
 	rds_ib_exit();
 	rds_iw_exit();
 }
+module_exit(rds_rdma_exit);
+
+MODULE_AUTHOR("Oracle Corporation <rds-devel@oss.oracle.com>");
+MODULE_DESCRIPTION("RDS: IB/iWARP transport");
+MODULE_LICENSE("Dual BSD/GPL");
 
