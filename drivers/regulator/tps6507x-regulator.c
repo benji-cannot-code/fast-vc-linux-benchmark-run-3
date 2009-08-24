8ac4a5b2845513d558123de0d5d43ac583fa3d18
Liam Girdwood <lrg@slimlogic.co.uk>
2009-08-24 10:31:34 +0100

    regulator: tps650xx - build fixes for x86_64
    Fixes the following errors on both tps650xx regulator drivers :-
    
    drivers/regulator/tps65023-regulator: struct i2c_device_id is 32 bytes.  The last of 1 is:
    0x74 0x70 0x73 0x36 0x35 0x30 0x32 0x33 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
    FATAL: drivers/regulator/tps65023-regulator: struct i2c_device_id is not terminated with a NULL entry!
    
    This patch also fixes the GPL v2 licence string for both drivers.
    
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- drivers/regulator/tps6507x-regulator.c
+++ drivers/regulator/tps6507x-regulator.c
@@ -671,9 +671,10 @@ static const struct tps_info tps6507x_regs[] = {
 	},
 };
 
-static const struct i2c_device_id tps_6507x_id = {
-	.name = "tps6507x",
-	.driver_data = (unsigned long) &tps6507x_regs[0],
+static const struct i2c_device_id tps_6507x_id[] = {
+	{.name = "tps6507x",
+	.driver_data = (unsigned long) tps6507x_regs,},
+	{ },
 };
 MODULE_DEVICE_TABLE(i2c, tps_6507x_id);
 
@@ -684,7 +685,7 @@ static struct i2c_driver tps_6507x_i2c_driver = {
 	},
 	.probe = tps_6507x_probe,
 	.remove = __devexit_p(tps_6507x_remove),
-	.id_table = &tps_6507x_id,
+	.id_table = tps_6507x_id,
 };
 
 /**
@@ -711,4 +712,4 @@ module_exit(tps_6507x_cleanup);
 
 MODULE_AUTHOR("Texas Instruments");
 MODULE_DESCRIPTION("TPS6507x voltage regulator driver");
-MODULE_LICENSE("GPLv2");
+MODULE_LICENSE("GPL v2");
