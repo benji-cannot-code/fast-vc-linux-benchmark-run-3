367778dec6506776244f4740df922af2eb969f95
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2009-08-25 11:06:22 -0300

    V4L/DVB (12505): soc_camera_platform: pass device pointer from soc-camera core on .add_device()
    Add a struct device pointer to struct soc_camera_platform_info and let the user
    (ap325rxa) pass it down to soc_camera_platform.c in its .add_device() method.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Acked-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/soc_camera_platform.h
+++ include/media/soc_camera_platform.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev2.h>
 #include <media/soc_camera.h>
 
+struct device;
+
 struct soc_camera_platform_info {
 	int iface;
 	char *format_name;
@@ -22,6 +24,7 @@ struct soc_camera_platform_info {
 	struct v4l2_pix_format format;
 	unsigned long bus_param;
 	void (*power)(int);
+	struct device *dev;
 	int (*set_capture)(struct soc_camera_platform_info *info, int enable);
 	struct soc_camera_link link;
 };
