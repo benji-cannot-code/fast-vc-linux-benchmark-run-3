19394c834042ba2bbb8b5776eb30dfbdb062ab7a
Joe Eykholt <jeykholt@cisco.com>
2009-08-25 13:58:42 -0700

    [SCSI] fcoe: libfcoe: extra semicolon in CHECK_LOGGING macros causes compile error
    If using code like this:
    	if (foo)
    		FCOE_DBG("foo\n);
    	else
    		FCOE_DBG("bar\n");
    
    one gets compile errors because FCOE_DBG expands with its own semicolon,
    making one too many for the if-statement.
    
    Remove the offending semicolon in fcoe.h and also a similar case
    in libfcoe.c.
    
    Signed-off-by: Joe Eykholt <jeykholt@cisco.com>
    Signed-off-by: Robert Love <robert.w.love@intel.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/fcoe/fcoe.h
+++ drivers/scsi/fcoe/fcoe.h
@@ -54,7 +54,7 @@ do {                                                            	\
 		do {							\
 			CMD;						\
 		} while (0);						\
-} while (0);
+} while (0)
 
 #define FCOE_DBG(fmt, args...)						\
 	FCOE_CHECK_LOGGING(FCOE_LOGGING,				\
