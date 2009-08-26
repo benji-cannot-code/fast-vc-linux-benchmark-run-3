942be502eaf91644368ad821e8d577fff7e98b84
Steven Whitehouse <swhiteho@redhat.com>
2009-08-26 18:51:04 +0100

    GFS2: Rename eattr.[ch] as xattr.[ch]
    Use the more conventional name for the extended attribute
    support code. Update all the places which care.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/acl.c
+++ fs/gfs2/acl.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "gfs2.h"
 #include "incore.h"
 #include "acl.h"
-#include "eattr.h"
+#include "xattr.h"
 #include "glock.h"
 #include "inode.h"
 #include "meta_io.h"
--- fs/gfs2/ops_inode.c
+++ fs/gfs2/ops_inode.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "acl.h"
 #include "bmap.h"
 #include "dir.h"
-#include "eattr.h"
+#include "xattr.h"
 #include "glock.h"
 #include "inode.h"
 #include "meta_io.h"
