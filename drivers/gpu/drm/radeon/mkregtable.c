cf27528f9219723625b9f561c45f627965fdc53e
Dave Airlie <airlied@redhat.com>
2009-08-27 11:51:23 +1000

    drm/radeon: cleanup mkregtable.c
    This cleans up the code in mkregtable.c to be more kernel style.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/mkregtable.c
+++ drivers/gpu/drm/radeon/mkregtable.c
@@ -26,10 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 #define container_of(ptr, type, member) ({          \
-    const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type,member) );})
-
-
+	const typeof(((type *)0)->member)*__mptr = (ptr);    \
+		     (type *)((char *)__mptr - offsetof(type, member)); })
 
 /*
  * Simple doubly linked list implementation.
@@ -64,8 +62,7 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
  */
 #ifndef CONFIG_DEBUG_LIST
 static inline void __list_add(struct list_head *new,
-			      struct list_head *prev,
-			      struct list_head *next)
+			      struct list_head *prev, struct list_head *next)
 {
 	next->prev = new;
 	new->next = next;
@@ -74,8 +71,7 @@ static inline void __list_add(struct list_head *new,
 }
 #else
 extern void __list_add(struct list_head *new,
-			      struct list_head *prev,
-			      struct list_head *next);
+		       struct list_head *prev, struct list_head *next);
 #endif
 
 /**
@@ -90,7 +86,6 @@ static inline void list_add(struct list_head *new, struct list_head *head)
 {
 	__list_add(new, head, head->next);
 }
-
 
 /**
  * list_add_tail - add a new entry
@@ -112,7 +107,7 @@ static inline void list_add_tail(struct list_head *new, struct list_head *head)
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static inline void __list_del(struct list_head * prev, struct list_head * next)
+static inline void __list_del(struct list_head *prev, struct list_head *next)
 {
 	next->prev = prev;
 	prev->next = next;
@@ -128,8 +123,8 @@ static inline void __list_del(struct list_head * prev, struct list_head * next)
 static inline void list_del(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
-	entry->next = (void*)0xDEADBEEF;
-	entry->prev = (void*)0xBEEFDEAD;
+	entry->next = (void *)0xDEADBEEF;
+	entry->prev = (void *)0xBEEFDEAD;
 }
 #else
 extern void list_del(struct list_head *entry);
@@ -142,8 +137,7 @@ extern void list_del(struct list_head *entry);
  *
  * If @old was empty, it will be overwritten.
  */
-static inline void list_replace(struct list_head *old,
-				struct list_head *new)
+static inline void list_replace(struct list_head *old, struct list_head *new)
 {
 	new->next = old->next;
 	new->next->prev = new;
@@ -152,7 +146,7 @@ static inline void list_replace(struct list_head *old,
 }
 
 static inline void list_replace_init(struct list_head *old,
-					struct list_head *new)
+				     struct list_head *new)
 {
 	list_replace(old, new);
 	INIT_LIST_HEAD(old);
@@ -197,7 +191,7 @@ static inline void list_move_tail(struct list_head *list,
  * @head: the head of the list
  */
 static inline int list_is_last(const struct list_head *list,
-				const struct list_head *head)
+			       const struct list_head *head)
 {
 	return list->next == head;
 }
@@ -240,7 +234,8 @@ static inline int list_is_singular(const struct list_head *head)
 }
 
 static inline void __list_cut_position(struct list_head *list,
-		struct list_head *head, struct list_head *entry)
+				       struct list_head *head,
+				       struct list_head *entry)
 {
 	struct list_head *new_first = entry->next;
 	list->next = head->next;
@@ -266,12 +261,12 @@ static inline void __list_cut_position(struct list_head *list,
  *
  */
 static inline void list_cut_position(struct list_head *list,
-		struct list_head *head, struct list_head *entry)
+				     struct list_head *head,
+				     struct list_head *entry)
 {
 	if (list_empty(head))
 		return;
-	if (list_is_singular(head) &&
-		(head->next != entry && head != entry))
+	if (list_is_singular(head) && (head->next != entry && head != entry))
 		return;
 	if (entry == head)
 		INIT_LIST_HEAD(list);
@@ -280,8 +275,7 @@ static inline void list_cut_position(struct list_head *list,
 }
 
 static inline void __list_splice(const struct list_head *list,
-				 struct list_head *prev,
-				 struct list_head *next)
+				 struct list_head *prev, struct list_head *next)
 {
 	struct list_head *first = list->next;
 	struct list_head *last = list->prev;
@@ -299,7 +293,7 @@ static inline void __list_splice(const struct list_head *list,
  * @head: the place to add it in the first list.
  */
 static inline void list_splice(const struct list_head *list,
-				struct list_head *head)
+			       struct list_head *head)
 {
 	if (!list_empty(list))
 		__list_splice(list, head, head->next);
@@ -311,7 +305,7 @@ static inline void list_splice(const struct list_head *list,
  * @head: the place to add it in the first list.
  */
 static inline void list_splice_tail(struct list_head *list,
-				struct list_head *head)
+				    struct list_head *head)
 {
 	if (!list_empty(list))
 		__list_splice(list, head->prev, head);
@@ -377,7 +371,7 @@ static inline void list_splice_tail_init(struct list_head *list,
  */
 #define list_for_each(pos, head) \
 	for (pos = (head)->next; prefetch(pos->next), pos != (head); \
-        	pos = pos->next)
+		pos = pos->next)
 
 /**
  * __list_for_each	-	iterate over a list
@@ -399,7 +393,7 @@ static inline void list_splice_tail_init(struct list_head *list,
  */
 #define list_for_each_prev(pos, head) \
 	for (pos = (head)->prev; prefetch(pos->prev), pos != (head); \
-        	pos = pos->prev)
+		pos = pos->prev)
 
 /**
  * list_for_each_safe - iterate over a list safe against removal of list entry
@@ -556,172 +550,172 @@ static inline void list_splice_tail_init(struct list_head *list,
 	     pos = n, n = list_entry(n->member.prev, typeof(*n), member))
 
 struct offset {
-    struct list_head    list;
-    unsigned            offset;
+	struct list_head list;
+	unsigned offset;
 };
 
 struct table {
-    struct list_head    offsets;
-    unsigned            offset_max;
-    unsigned            nentry;
-    unsigned            *table;
-    char 	*gpu_prefix;
+	struct list_head offsets;
+	unsigned offset_max;
+	unsigned nentry;
+	unsigned *table;
+	char *gpu_prefix;
 };
 
-struct offset* offset_new(unsigned o)
+struct offset *offset_new(unsigned o)
 {
-    struct offset *offset;
+	struct offset *offset;
 
-    offset = (struct offset*)malloc(sizeof(struct offset));
-    if (offset) {
-        INIT_LIST_HEAD(&offset->list);
-        offset->offset = o;
-    }
-    return offset;
+	offset = (struct offset *)malloc(sizeof(struct offset));
+	if (offset) {
+		INIT_LIST_HEAD(&offset->list);
+		offset->offset = o;
+	}
+	return offset;
 }
 
 void table_offset_add(struct table *t, struct offset *offset)
 {
-    list_add_tail(&offset->list, &t->offsets);
+	list_add_tail(&offset->list, &t->offsets);
 }
 
 void table_init(struct table *t)
 {
-    INIT_LIST_HEAD(&t->offsets);
-    t->offset_max = 0;
-    t->nentry = 0;
-    t->table = NULL;
+	INIT_LIST_HEAD(&t->offsets);
+	t->offset_max = 0;
+	t->nentry = 0;
+	t->table = NULL;
 }
 
 void table_print(struct table *t)
 {
-    unsigned nlloop, i, j, n, c, id;
+	unsigned nlloop, i, j, n, c, id;
 
-    nlloop = (t->nentry + 3) / 4;
-    c = t->nentry;
-    printf("static const unsigned %s_reg_safe_bm[%d] = {\n", t->gpu_prefix, t->nentry);
-    for(i = 0, id = 0; i < nlloop; i++) {
-        n = 4;
-        if (n > c) {
-            n = c;
-        }
-        c -= n;
-        for(j = 0; j < n; j++) {
-	    if (j == 0) printf("\t");
-	    else printf(" ");
-            printf("0x%08X,", t->table[id++]);
-        }
-        printf("\n");
-    }
-    printf("};\n");
+	nlloop = (t->nentry + 3) / 4;
+	c = t->nentry;
+	printf("static const unsigned %s_reg_safe_bm[%d] = {\n", t->gpu_prefix,
+	       t->nentry);
+	for (i = 0, id = 0; i < nlloop; i++) {
+		n = 4;
+		if (n > c)
+			n = c;
+		c -= n;
+		for (j = 0; j < n; j++) {
+			if (j == 0)
+				printf("\t");
+			else
+				printf(" ");
+			printf("0x%08X,", t->table[id++]);
+		}
+		printf("\n");
+	}
+	printf("};\n");
 }
 
 int table_build(struct table *t)
 {
-    struct offset *offset;
-    unsigned i, m;
+	struct offset *offset;
+	unsigned i, m;
 
-    t->nentry = ((t->offset_max >> 2) + 31) / 32;
-    t->table = (unsigned*)malloc(sizeof(unsigned) * t->nentry);
-    if (t->table == NULL) {
-        return -1;
-    }
-    memset(t->table, 0xff, sizeof(unsigned) * t->nentry);
-    list_for_each_entry(offset, &t->offsets, list) {
-        i = (offset->offset >> 2) / 32;
-        m = (offset->offset >> 2) & 31;
-        m = 1 << m;
-        t->table[i] ^= m;
-    }
-    return 0;
+	t->nentry = ((t->offset_max >> 2) + 31) / 32;
+	t->table = (unsigned *)malloc(sizeof(unsigned) * t->nentry);
+	if (t->table == NULL)
+		return -1;
+	memset(t->table, 0xff, sizeof(unsigned) * t->nentry);
+	list_for_each_entry(offset, &t->offsets, list) {
+		i = (offset->offset >> 2) / 32;
+		m = (offset->offset >> 2) & 31;
+		m = 1 << m;
+		t->table[i] ^= m;
+	}
+	return 0;
 }
 
 static char gpu_name[10];
 int parser_auth(struct table *t, const char *filename)
 {
-    FILE *file;
-    regex_t mask_rex;
-    regmatch_t match[4];
-    char buf[1024];
-    size_t end;
-    int len;
-    int done = 0;
-    int r;
-    unsigned o;
-    struct offset *offset;
-    char last_reg_s[10];
-    int last_reg;
-    
-    if (regcomp(&mask_rex, "(0x[0-9a-fA-F]*) *([_a-zA-Z0-9]*)", REG_EXTENDED)) {
-        fprintf(stderr, "Failed to compile regular expression\n");
-        return -1;
-    }
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Failed to open: %s\n", filename);
-        return -1;
-    }
-    fseek(file, 0, SEEK_END);
-    end = ftell(file);
-    fseek(file, 0, SEEK_SET);
+	FILE *file;
+	regex_t mask_rex;
+	regmatch_t match[4];
+	char buf[1024];
+	size_t end;
+	int len;
+	int done = 0;
+	int r;
+	unsigned o;
+	struct offset *offset;
+	char last_reg_s[10];
+	int last_reg;
 
-    /* get header */
-    if (fgets(buf, 1024, file) == NULL)
-        return -1;
+	if (regcomp
+	    (&mask_rex, "(0x[0-9a-fA-F]*) *([_a-zA-Z0-9]*)", REG_EXTENDED)) {
+		fprintf(stderr, "Failed to compile regular expression\n");
+		return -1;
+	}
+	file = fopen(filename, "r");
+	if (file == NULL) {
+		fprintf(stderr, "Failed to open: %s\n", filename);
+		return -1;
+	}
+	fseek(file, 0, SEEK_END);
+	end = ftell(file);
+	fseek(file, 0, SEEK_SET);
 
-    /* first line will contain the last register 
-     * and gpu name */
-    sscanf(buf, "%s %s", gpu_name, last_reg_s);
-    t->gpu_prefix = gpu_name;
-    last_reg = strtol(last_reg_s, NULL, 16);
+	/* get header */
+	if (fgets(buf, 1024, file) == NULL)
+		return -1;
 
-    do {
-        if (fgets(buf, 1024, file) == NULL)
-            return -1;
-        len = strlen(buf);
-        if (ftell(file) == end) {
-            done = 1;
-        }
-        if (len) {
-            r = regexec(&mask_rex, buf, 4, match, 0);
-            if (r == REG_NOMATCH) {
-            } else if (r) {
-                fprintf(stderr, "Error matching regular expression %d in %s\n",
-                        r, filename);
-                return -1;
-            } else {
-                buf[match[0].rm_eo] = 0;
-                buf[match[1].rm_eo] = 0;
-                buf[match[2].rm_eo] = 0;
-                o = strtol(&buf[match[1].rm_so], NULL, 16);
-                offset = offset_new(o);
-                table_offset_add(t, offset);
-                if (o > t->offset_max) {
-                    t->offset_max = o;
-                }
-            }
-        }
-    } while (!done);
-    fclose(file);
-    if (t->offset_max < last_reg)
-	t->offset_max = last_reg;
-    return table_build(t);
+	/* first line will contain the last register
+	 * and gpu name */
+	sscanf(buf, "%s %s", gpu_name, last_reg_s);
+	t->gpu_prefix = gpu_name;
+	last_reg = strtol(last_reg_s, NULL, 16);
+
+	do {
+		if (fgets(buf, 1024, file) == NULL)
+			return -1;
+		len = strlen(buf);
+		if (ftell(file) == end)
+			done = 1;
+		if (len) {
+			r = regexec(&mask_rex, buf, 4, match, 0);
+			if (r == REG_NOMATCH) {
+			} else if (r) {
+				fprintf(stderr,
+					"Error matching regular expression %d in %s\n",
+					r, filename);
+				return -1;
+			} else {
+				buf[match[0].rm_eo] = 0;
+				buf[match[1].rm_eo] = 0;
+				buf[match[2].rm_eo] = 0;
+				o = strtol(&buf[match[1].rm_so], NULL, 16);
+				offset = offset_new(o);
+				table_offset_add(t, offset);
+				if (o > t->offset_max)
+					t->offset_max = o;
+			}
+		}
+	} while (!done);
+	fclose(file);
+	if (t->offset_max < last_reg)
+		t->offset_max = last_reg;
+	return table_build(t);
 }
 
 int main(int argc, char *argv[])
 {
-    struct table t;
+	struct table t;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <authfile>\n",
-                argv[0]);
-        exit(1);
-    }
-    table_init(&t);
-    if (parser_auth(&t, argv[1])) {
-        fprintf(stderr, "Failed to parse file %s\n", argv[1]);
-        return -1;
-    }
-    table_print(&t);
-    return 0;
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <authfile>\n", argv[0]);
+		exit(1);
+	}
+	table_init(&t);
+	if (parser_auth(&t, argv[1])) {
+		fprintf(stderr, "Failed to parse file %s\n", argv[1]);
+		return -1;
+	}
+	table_print(&t);
+	return 0;
 }
