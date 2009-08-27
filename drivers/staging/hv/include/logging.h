2fe2f2d6de0e8a803a7cf54d5b6d71f1db69f15e
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-27 15:55:48 -0700

    Staging: hv: fix up some coding style issues in logging.h
    It's now clean.
    
    Well, from a coding style guide, not from a logic standpoint, the whole
    file needs to be tossed overboard and cheered on as the sharks tear it
    to individual bits.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/include/logging.h
+++ drivers/staging/hv/include/logging.h
@@ -63,11 +63,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern unsigned int vmbus_loglevel;
 
 #define ASSERT(expr)	\
-        if (!(expr)) {	\
+	if (!(expr)) {	\
 		printk(KERN_CRIT "Assertion failed! %s,%s,%s,line=%d\n", \
 		       #expr, __FILE__, __func__, __LINE__);	\
 		__asm__ __volatile__("int3");	\
-        }
+	}
 
 #define DPRINT(mod, lvl, fmt, args...) do {\
 	if ((mod & (HIWORD(vmbus_loglevel))) &&	\
@@ -90,7 +90,7 @@ extern unsigned int vmbus_loglevel;
 #define DPRINT_WARN(mod, fmt, args...) do {\
 	if ((mod & (HIWORD(vmbus_loglevel))) &&		\
 	    (WARNING_LVL <= LOWORD(vmbus_loglevel)))	\
-	    printk(KERN_WARNING #mod": WARNING! " fmt "\n", ## args);\
+		printk(KERN_WARNING #mod": WARNING! " fmt "\n", ## args);\
 	} while (0)
 
 #define DPRINT_ERR(mod, fmt, args...) do {\
@@ -104,7 +104,7 @@ extern unsigned int vmbus_loglevel;
 #define DPRINT_ENTER(mod) do {\
 	if ((mod & (HIWORD(vmbus_loglevel))) && \
 	    (DEBUG_LVL_ENTEREXIT <= LOWORD(vmbus_loglevel)))	\
-	    printk(KERN_DEBUG "["#mod"]: %s() enter\n", __func__);\
+		printk(KERN_DEBUG "["#mod"]: %s() enter\n", __func__);\
 	} while (0)
 
 #define DPRINT_EXIT(mod) do {\
