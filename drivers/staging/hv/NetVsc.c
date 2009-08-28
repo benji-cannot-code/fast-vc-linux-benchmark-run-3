656f060afad113c97e59a322b728259021618b4f
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-28 16:22:59 -0700

    Staging: hv: move logging.h
    Move it out of the include subdirectory.
    
    No code changes here, just file movements.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/NetVsc.c
+++ drivers/staging/hv/NetVsc.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <asm/io.h>
 #include "osd.h"
-#include "include/logging.h"
+#include "logging.h"
 #include "NetVsc.h"
 #include "RndisFilter.h"
 
--- drivers/staging/hv/vmbus_drv.c
+++ drivers/staging/hv/vmbus_drv.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/sysctl.h>
 #include "osd.h"
-#include "include/logging.h"
+#include "logging.h"
 #include "vmbus.h"
 
 
