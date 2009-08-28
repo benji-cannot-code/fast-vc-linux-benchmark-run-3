9c704575b03349e5d956bda2f11b0b93d01bd91e
Evgeniy Polyakov <zbr@ioremap.net>
2009-08-28 17:57:58 +0400

    Staging: pohmelfs: sync with the development tree
    * cache coherency protocol fix
     * proper timeout handling
     * implement dump/del all config group command
     	(Signed-off-by: Pierpaolo Giacomin <yrz@anche.no>)
    
    Signed-off-by: Evgeniy Polyakov <zbr@ioremap.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/pohmelfs/inode.c
+++ drivers/staging/pohmelfs/inode.c
@@ -1505,7 +1505,9 @@ static void pohmelfs_flush_inode(struct pohmelfs_inode *pi, unsigned int count)
 		inode->i_sb->s_op->write_inode(inode, 0);
 	}
 
+#ifdef POHMELFS_TRUNCATE_ON_INODE_FLUSH
 	truncate_inode_pages(inode->i_mapping, 0);
+#endif
 
 	pohmelfs_data_unlock(pi, 0, ~0, POHMELFS_WRITE_LOCK);
 	mutex_unlock(&inode->i_mutex);
@@ -1744,11 +1746,10 @@ static int pohmelfs_root_handshake(struct pohmelfs_sb *psb)
 	err = wait_event_interruptible_timeout(psb->wait,
 			(psb->flags != ~0),
 			psb->wait_on_page_timeout);
-	if (!err) {
+	if (!err)
 		err = -ETIMEDOUT;
-	} else {
+	else if (err > 0)
 		err = -psb->flags;
-	}
 
 	if (err)
 		goto err_out_exit;
