43e2c97ca709163a56f4ab52183cbd97a714984f
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-28 16:23:49 -0700

    Staging: hv: move StorVscApi.h
    Move it out of the include subdirectory.
    
    No code changes here, just file movements.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/StorVsc.c
+++ drivers/staging/hv/StorVsc.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include "osd.h"
 #include "logging.h"
-#include "include/StorVscApi.h"
+#include "StorVscApi.h"
 #include "include/VmbusPacketFormat.h"
 #include "vstorage.h"
 
