244c0a6e80feafa4d8f5c16f464083b93e964e1b
Sarah Sharp <sarah.a.sharp@linux.intel.com>
2009-08-28 14:28:18 -0700

    USB: xhci: Set -EREMOTEIO when xHC gives bad transfer length.
    The xHCI hardware reports the number of bytes untransferred for a given
    transfer buffer.  If the hardware reports a bytes untransferred value
    greater than the submitted buffer size, we want to play it safe and say no
    data was transferred.  If the driver considers a short packet to be an
    error, remember to set -EREMOTEIO.
    
    Signed-off-by: Sarah Sharp <sarah.a.sharp@linux.intel.com>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -1105,6 +1105,11 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 							"of %d bytes left\n",
 							TRB_LEN(event->transfer_len));
 					td->urb->actual_length = 0;
+					if (td->urb->transfer_flags &
+							URB_SHORT_NOT_OK)
+						status = -EREMOTEIO;
+					else
+						status = 0;
 				}
 				/* Don't overwrite a previously set error code */
 				if (status == -EINPROGRESS) {
@@ -1188,6 +1193,10 @@ td_cleanup:
 					urb->transfer_buffer_length,
 					urb->actual_length);
 			urb->actual_length = 0;
+			if (td->urb->transfer_flags & URB_SHORT_NOT_OK)
+				status = -EREMOTEIO;
+			else
+				status = 0;
 		}
 		list_del(&td->td_list);
 		/* Was this TD slated to be cancelled but completed anyway? */
