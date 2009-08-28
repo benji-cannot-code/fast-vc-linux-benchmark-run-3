34a56b1b2f49a5232ed6c8f966c6f4b8a9025301
Greg Kroah-Hartman <gregkh@suse.de>
2009-08-28 16:24:34 -0700

    Staging: hv: move VmbusChannelInterface.h
    Move it out of the include subdirectory.
    
    No code changes here, just file movements.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/ChannelMgmt.h
+++ drivers/staging/hv/ChannelMgmt.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "List.h"
 #include "RingBuffer.h"
 
-#include "include/VmbusChannelInterface.h"
+#include "VmbusChannelInterface.h"
 #include "include/VmbusPacketFormat.h"
 
 /* Version 1 messages */
--- drivers/staging/hv/NetVsc.h
+++ drivers/staging/hv/NetVsc.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _NETVSC_H_
 
 #include "include/VmbusPacketFormat.h"
-#include "include/VmbusChannelInterface.h"
+#include "VmbusChannelInterface.h"
 #include "List.h"
 #include "NetVscApi.h"
 
