d70570cd5e78f4a894e0b442a2629fe63bc40205
Chen Liqin <liqin.chen@sunplusct.com>
2009-08-30 12:36:41 +0800

    score: make init_thread_union align to THREAD_SIZE

--- arch/score/kernel/init_task.c
+++ arch/score/kernel/init_task.c
@@ -36,7 +36,7 @@ static struct sighand_struct init_sighand = INIT_SIGHAND(init_sighand);
  * "init_task" linker map entry..
  */
 union thread_union init_thread_union
-	__attribute__((__section__(".data.init_task"))) =
+	__attribute__((__section__(".data.init_task"), __aligned__(THREAD_SIZE))) =
 		{ INIT_THREAD_INFO(init_task) };
 
 /*
