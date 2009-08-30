b776ea31caeca71e76dadee50f1ffafe3ae3ae51
Mauro Carvalho Chehab <mchehab@redhat.com>
2009-08-30 13:05:56 -0300

    V4L/DVB (12591): em28xx: Add entry for GADMEI UTV330+ and related IR keymap
    [mchehab@redhat.com: Fix a few wrong IR keymaps]
    Signed-off-by: Shine Liu <shinel@foxmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx.h
+++ drivers/media/video/em28xx/em28xx.h
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2882_BOARD_KWORLD_ATSC_315U		  69
 #define EM2882_BOARD_EVGA_INDTUBE		  70
 #define EM2820_BOARD_SILVERCREST_WEBCAM           71
+#define EM2861_BOARD_GADMEI_UTV330PLUS           72
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
