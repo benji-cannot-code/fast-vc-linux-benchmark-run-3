a5079600b5f9c11adb5ec8d04452e3237c6f7abd
Heiko Carstens <heiko.carstens@de.ibm.com>
2009-08-31 14:43:35 +0200

    locking, m68k: Calculate thread_info offset with asm offset
    m68k has the thread_info structure embedded in its task struct.
    Therefore its not possible to implement current_thread_info()
    by looking at the stack pointer and do some simple calculations
    like most other architectures do it.
    
    To return the thread_info pointer for a task two defines are
    used. This works until the spinlock function bodies get moved
    into an own header file and CONFIG_SPINLOCK_DEBUG is turned on.
    That results into this compile error:
    
      In file included from include/linux/spinlock.h:378,
                       from include/linux/seqlock.h:29,
                       from include/linux/time.h:8,
                       from include/linux/timex.h:56,
                       from include/linux/sched.h:54,
                       from arch/m68k/kernel/asm-offsets.c:12:
      include/linux/spinlock_api_smp.h: In function '__spin_unlock_irq':
      include/linux/spinlock_api_smp.h:371: error: 'current' undeclared (first use in this function)
      include/linux/spinlock_api_smp.h:371: error: (Each undeclared identifier is reported only once
      include/linux/spinlock_api_smp.h:371: error: for each function it appears in.)
    
    Including asm/current.h to asm-offsets.c wouldn't help since
    the definition of struct task is needed. So we end up with ugly
    header file include dependencies.
    
    To solve this calculate the offset of the thread_info structure
    into the task struct in asm-offsets.h and use the offset in
    task_thread_info(). This works just like it does for IA64 as
    well.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Nick Piggin <nickpiggin@yahoo.com.au>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Horst Hartmann <horsth@linux.vnet.ibm.com>
    Cc: Christian Ehrhardt <ehrhardt@linux.vnet.ibm.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: David Miller <davem@davemloft.net>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Roman Zippel <zippel@linux-m68k.org>
    Cc: <linux-arch@vger.kernel.org>
    LKML-Reference: <20090831124417.329662275@de.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/m68k/kernel/asm-offsets.c
+++ arch/m68k/kernel/asm-offsets.c
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * #defines from the assembly-language output.
  */
 
+#define ASM_OFFSETS_C
+
 #include <linux/stddef.h>
 #include <linux/sched.h>
 #include <linux/kernel_stat.h>
@@ -28,6 +30,9 @@ int main(void)
 	DEFINE(TASK_INFO, offsetof(struct task_struct, thread.info));
 	DEFINE(TASK_MM, offsetof(struct task_struct, mm));
 	DEFINE(TASK_ACTIVE_MM, offsetof(struct task_struct, active_mm));
+#ifdef CONFIG_MMU
+	DEFINE(TASK_TINFO, offsetof(struct task_struct, thread.info));
+#endif
 
 	/* offsets into the thread struct */
 	DEFINE(THREAD_KSP, offsetof(struct thread_struct, ksp));
