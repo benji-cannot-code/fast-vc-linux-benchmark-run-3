651cfa64a7714cff17ac2478f5bab3040dac23f0
Sekhar Nori <nsekhar@ti.com>
2009-08-31 15:48:03 +0530

    davinci: support changing the clock rate in clock framework
    clk_round_rate, clk_set_rate have been updated to handle dynamic
    frequency changes.
    
    The motivation behind the changes is to support dynamic CPU frequency
    change.
    
    davinci_set_pllrate() changes the PLL rate of a given PLL. This function
    has been presented as a generic function though it has been tested only
    on OMAP-L138 EVM. No other currently available DaVinci device will probably
    use this function, but any future device specific changes will hopefully be
    small enough to get taken care using a cpu_is_xxx() macro.
    
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/clock.h
+++ arch/arm/mach-davinci/clock.h
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* PLL/Reset register offsets */
 #define PLLCTL          0x100
 #define PLLCTL_PLLEN    BIT(0)
+#define PLLCTL_PLLPWRDN	BIT(1)
+#define PLLCTL_PLLRST	BIT(3)
+#define PLLCTL_PLLDIS	BIT(4)
+#define PLLCTL_PLLENSRC	BIT(5)
 #define PLLCTL_CLKMODE  BIT(8)
 
 #define PLLM		0x110
@@ -75,6 +79,8 @@ struct clk {
 	struct pll_data         *pll_data;
 	u32                     div_reg;
 	unsigned long (*recalc) (struct clk *);
+	int (*set_rate) (struct clk *clk, unsigned long rate);
+	int (*round_rate) (struct clk *clk, unsigned long rate);
 };
 
 /* Clock flags */
@@ -98,6 +104,8 @@ struct davinci_clk {
 	}
 
 int davinci_clk_init(struct davinci_clk *clocks);
+int davinci_set_pllrate(struct pll_data *pll, unsigned int prediv,
+				unsigned int mult, unsigned int postdiv);
 
 extern struct platform_device davinci_wdt_device;
 
