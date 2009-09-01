89ea558a0dde6dd400fa604cc8b355ac64634386
Paul Mundt <lethal@linux-sh.org>
2009-09-01 17:45:35 +0900

    sh: Kill off kgdb's magical NMI debouncing.
    The kgdb stub has traditionally tied in to the NMI slot, and manually
    handled debounce. Now that we have a generic way to do this instead, all
    of the stub-specific debounce silliness can be killed off.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/kgdb.c
+++ arch/sh/kernel/kgdb.c
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <asm/cacheflush.h>
 
-char in_nmi = 0;	/* Set during NMI to prevent re-entry */
-
 /* Macros for single step instruction identification */
 #define OPCODE_BT(op)		(((op) & 0xff00) == 0x8900)
 #define OPCODE_BF(op)		(((op) & 0xff00) == 0x8b00)
