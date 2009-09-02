411683f19f195f7239f566d5d1dce71a7d1921ff
Chris Mason <chris.mason@oracle.com>
2009-09-02 15:11:07 -0400

    Btrfs: don't lock bits in the extent tree during writepage
    At writepage time, we have the page locked and we have the
    extent_map entry for this extent pinned in the extent_map tree.
    So, the page can't go away and its mapping can't change.
    
    There is no need for the extra extent_state lock bits during writepage.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -2220,16 +2220,10 @@ static int __extent_writepage(struct page *page, struct writeback_control *wbc,
 			goto done_unlocked;
 		}
 	}
-	lock_extent_bits(tree, start, page_end, 0, &cached_state, GFP_NOFS);
-
-	unlock_start = start;
-
 	if (tree->ops && tree->ops->writepage_start_hook) {
 		ret = tree->ops->writepage_start_hook(page, start,
 						      page_end);
 		if (ret == -EAGAIN) {
-			unlock_extent_cached(tree, start, page_end,
-					     &cached_state, GFP_NOFS);
 			redirty_page_for_writepage(wbc, page);
 			update_nr_written(page, wbc, nr_written);
 			unlock_page(page);
@@ -2245,13 +2239,7 @@ static int __extent_writepage(struct page *page, struct writeback_control *wbc,
 	update_nr_written(page, wbc, nr_written + 1);
 
 	end = page_end;
-	if (test_range_bit(tree, start, page_end, EXTENT_DELALLOC, 0))
-		printk(KERN_ERR "btrfs delalloc bits after lock_extent\n");
-
 	if (last_byte <= start) {
-		clear_extent_bit(tree, start, page_end,
-				 EXTENT_LOCKED | EXTENT_DIRTY,
-				 1, 0, NULL, GFP_NOFS);
 		if (tree->ops && tree->ops->writepage_end_io_hook)
 			tree->ops->writepage_end_io_hook(page, start,
 							 page_end, NULL, 1);
@@ -2263,8 +2251,6 @@ static int __extent_writepage(struct page *page, struct writeback_control *wbc,
 
 	while (cur <= end) {
 		if (cur >= last_byte) {
-			unlock_extent_cached(tree, unlock_start, page_end,
-					     &cached_state, GFP_NOFS);
 			if (tree->ops && tree->ops->writepage_end_io_hook)
 				tree->ops->writepage_end_io_hook(page, cur,
 							 page_end, NULL, 1);
@@ -2296,10 +2282,6 @@ static int __extent_writepage(struct page *page, struct writeback_control *wbc,
 		 */
 		if (compressed || block_start == EXTENT_MAP_HOLE ||
 		    block_start == EXTENT_MAP_INLINE) {
-			unlock_extent_cached(tree, unlock_start,
-					     cur + iosize - 1, &cached_state,
-					     GFP_NOFS);
-
 			/*
 			 * end_io notification does not happen here for
 			 * compressed extents
@@ -2367,9 +2349,6 @@ done:
 		set_page_writeback(page);
 		end_page_writeback(page);
 	}
-	if (unlock_start <= page_end)
-		unlock_extent_cached(tree, unlock_start, page_end,
-				     &cached_state, GFP_NOFS);
 	unlock_page(page);
 
 done_unlocked:
