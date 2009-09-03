19e5a54eb61ee78fb20ff8d8384fd22b5edc8bec
Alexander Beregalov <a.beregalov@gmail.com>
2009-09-03 20:38:14 +0400

    Staging: vt665x: fix built-in compiling
    Fix this build error:
    undefined reference to "__this_module"
    
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6656/device.h
+++ drivers/staging/vt6656/device.h
@@ -30,13 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DEVICE_H__
 #define __DEVICE_H__
 
-#ifdef MODULE
-#ifdef MODVERSIONS
-#include <linux/modversions.h>
-#endif /* MODVERSIONS */
 #include <linux/module.h>
-#endif /* MODULE */
-
 #include <linux/types.h>
 #include <linux/init.h>
 #include <linux/mm.h>
