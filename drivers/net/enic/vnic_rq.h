a3748297df6023e422861fa1293c027ee41ee0fe
Scott Feldman <scofeldm@cisco.com>
2009-09-03 17:02:45 +0000

    enic: organize device initialization/deinit into separate functions
    To unclutter probe() a little bit, put all device initialization code
    in one spot and device deinit code in another spot.  Also remove unused
    rq->buf_index variable/func.
    
    Signed-off-by: Scott Feldman <scofeldm@cisco.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/enic/vnic_rq.h
+++ drivers/net/enic/vnic_rq.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright 2008 Cisco Systems, Inc.  All rights reserved.
+ * Copyright 2008, 2009 Cisco Systems, Inc.  All rights reserved.
  * Copyright 2007 Nuova Systems, Inc.  All rights reserved.
  *
  * This program is free software; you may redistribute it and/or modify
@@ -80,7 +80,6 @@ struct vnic_rq {
 	struct vnic_rq_buf *to_use;
 	struct vnic_rq_buf *to_clean;
 	void *os_buf_head;
-	unsigned int buf_index;
 	unsigned int pkts_outstanding;
 };
 
@@ -104,11 +103,6 @@ static inline void *vnic_rq_next_desc(struct vnic_rq *rq)
 static inline unsigned int vnic_rq_next_index(struct vnic_rq *rq)
 {
 	return rq->to_use->index;
-}
-
-static inline unsigned int vnic_rq_next_buf_index(struct vnic_rq *rq)
-{
-	return rq->buf_index++;
 }
 
 static inline void vnic_rq_post(struct vnic_rq *rq,
@@ -205,6 +199,10 @@ static inline int vnic_rq_fill(struct vnic_rq *rq,
 void vnic_rq_free(struct vnic_rq *rq);
 int vnic_rq_alloc(struct vnic_dev *vdev, struct vnic_rq *rq, unsigned int index,
 	unsigned int desc_count, unsigned int desc_size);
+void vnic_rq_init_start(struct vnic_rq *rq, unsigned int cq_index,
+	unsigned int fetch_index, unsigned int posted_index,
+	unsigned int error_interrupt_enable,
+	unsigned int error_interrupt_offset);
 void vnic_rq_init(struct vnic_rq *rq, unsigned int cq_index,
 	unsigned int error_interrupt_enable,
 	unsigned int error_interrupt_offset);
