aa00bf16a559c7522ccfbcccadca08e14f93419b
Michael Buesch <mb@bu3sch.de>
2009-09-04 22:55:00 +0200

    b43: Remove DMA/PIO queue locks
    This removes the DMA/PIO queue locks. Locking is handled by
    wl->mutex now.
    
    Signed-off-by: Michael Buesch <mb@bu3sch.de>
    Tested-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/pio.h
+++ drivers/net/wireless/b43/pio.h
@@ -71,7 +71,6 @@ struct b43_pio_txpacket {
 
 struct b43_pio_txqueue {
 	struct b43_wldev *dev;
-	spinlock_t lock;
 	u16 mmio_base;
 
 	/* The device queue buffer size in bytes. */
@@ -104,7 +103,6 @@ struct b43_pio_txqueue {
 
 struct b43_pio_rxqueue {
 	struct b43_wldev *dev;
-	spinlock_t lock;
 	u16 mmio_base;
 
 	/* Work to reduce latency issues on RX. */
