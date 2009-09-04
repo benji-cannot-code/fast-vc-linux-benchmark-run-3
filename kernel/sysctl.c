dc8436ebea89dde55ea0653f74d8b2b551d7454c
Jaswinder Singh Rajput <jaswinder@kernel.org>
2009-09-04 23:13:21 +0530

    Security/SELinux: includecheck fix kernel/sysctl.c
    fix the following 'make includecheck' warning:
    
      kernel/sysctl.c: linux/security.h is included more than once.
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/acpi.h>
 #include <linux/reboot.h>
 #include <linux/ftrace.h>
-#include <linux/security.h>
 #include <linux/slow-work.h>
 #include <linux/perf_counter.h>
 
