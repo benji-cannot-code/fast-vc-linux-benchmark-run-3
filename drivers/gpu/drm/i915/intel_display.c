1159ee299751d4838fea0d4c5cc5ce559f2146f9
Eric Anholt <eric@anholt.net>
2009-09-06 15:18:53 -0700

    drm/i915: Put the idle reclocking work on our private workqueue as well.
    Fixes (again) whole-system lockups due to GPU lockups.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3202,7 +3202,7 @@ static void intel_gpu_idle_timer(unsigned long arg)
 
 	dev_priv->busy = false;
 
-	schedule_work(&dev_priv->idle_work);
+	queue_work(dev_priv->wq, &dev_priv->idle_work);
 }
 
 void intel_increase_renderclock(struct drm_device *dev, bool schedule)
@@ -3336,7 +3336,7 @@ static void intel_crtc_idle_timer(unsigned long arg)
 
 	intel_crtc->busy = false;
 
-	schedule_work(&dev_priv->idle_work);
+	queue_work(dev_priv->wq, &dev_priv->idle_work);
 }
 
 static void intel_increase_pllclock(struct drm_crtc *crtc, bool schedule)
