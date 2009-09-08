69c7cd514e3fa537fa2592fb5ced08305fbfbeec
Thomas Abraham <thomas.ab@samsung.com>
2009-09-08 14:30:48 +0900

    ARM: S3C6410: update clk->parent when setting clock source
    This ensures the clock hierarchy data structures are updated when we
    change the clock source in the actual hardware registers.
    
    Signed-off-by: Thomas Abraham <thomas.ab@samsung.com>
    [ben-linux@fluff.org: Minor re-indentation of subject]
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-s3c64xx/s3c6400-clock.c
+++ arch/arm/plat-s3c64xx/s3c6400-clock.c
@@ -329,6 +329,8 @@ static int s3c64xx_setparent_clksrc(struct clk *clk, struct clk *parent)
 		clksrc |= src_nr << sclk->shift;
 
 		__raw_writel(clksrc, S3C_CLK_SRC);
+
+		clk->parent = parent;
 		return 0;
 	}
 
