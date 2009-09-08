ba539cf603f2844ac0acc4ee5a8c4f3b5a9d367c
Chuck Lever <chuck.lever@oracle.com>
2009-09-08 19:49:47 -0400

    NFS: Mount option parser should detect missing "port="
    The meaning of not specifying the "port=" mount option is different
    for "-t nfs" and "-t nfs4" mounts.  The default port value for
    NFSv2/v3 mounts is 0, but the default for NFSv4 mounts is 2049.
    
    To support "-t nfs -o vers=4", the mount option parser must detect
    when "port=" is missing so that the correct default port value can be
    set depending on which NFS version is requested.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -50,6 +50,11 @@ struct nfs_clone_mount {
 #define NFS_MAX_SECFLAVORS	(12)
 
 /*
+ * Value used if the user did not specify a port value.
+ */
+#define NFS_UNSPEC_PORT		(-1)
+
+/*
  * In-kernel mount arguments
  */
 struct nfs_parsed_mount_data {
@@ -72,7 +77,7 @@ struct nfs_parsed_mount_data {
 		size_t			addrlen;
 		char			*hostname;
 		u32			version;
-		unsigned short		port;
+		int			port;
 		unsigned short		protocol;
 	} mount_server;
 
@@ -81,7 +86,7 @@ struct nfs_parsed_mount_data {
 		size_t			addrlen;
 		char			*hostname;
 		char			*export_path;
-		unsigned short		port;
+		int			port;
 		unsigned short		protocol;
 	} nfs_server;
 
