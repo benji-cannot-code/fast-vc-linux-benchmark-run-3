df7a0d8459e9f6987fb24654af7be236f962a3a4
Dave Airlie <airlied@redhat.com>
2009-09-08 11:32:08 +1000

    drm: include seq_file.h for debugfs builds.
    Fixes a warning seen on powerpc.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/drm/drm_mm.h
+++ include/drm/drm_mm.h
@@ -38,6 +38,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Generic range manager structs
  */
 #include <linux/list.h>
+#ifdef CONFIG_DEBUG_FS
+#include <linux/seq_file.h>
+#endif
 
 struct drm_mm_node {
 	struct list_head fl_entry;
