5406f0e2bbab1b728a1aa16e530261061e50ef13
David S. Miller <davem@davemloft.net>
2009-09-08 23:20:39 -0700

    sparc64: Kill unnecessary cast in profile_timer_exceptions_notify().
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/oprofile/init.c
+++ arch/sparc/oprofile/init.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static int profile_timer_exceptions_notify(struct notifier_block *self,
 					   unsigned long val, void *data)
 {
-	struct die_args *args = (struct die_args *)data;
+	struct die_args *args = data;
 	int ret = NOTIFY_DONE;
 
 	switch (val) {
