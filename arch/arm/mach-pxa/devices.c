03884ab0a7268288caa9efbc9c9a41d8cafa96f9
Haojian Zhuang <haojian.zhuang@marvell.com>
2009-09-10 13:55:23 +0800

    pxa3xx_nand: move pxa3xx_nand.h common into plat directory
    Since the same nand controller is shared between ARCH_PXA and ARCH_MMP. Move
    the pxa3xx_nand.h from mach directory to plat directoy.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@marvell.com>
    Cc: David Woodhouse <david.woodhouse@intel.com>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-pxa/devices.c
+++ arch/arm/mach-pxa/devices.c
@@ -9,13 +9,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/pxafb.h>
 #include <mach/mmc.h>
 #include <mach/irda.h>
-#include <plat/i2c.h>
 #include <mach/ohci.h>
 #include <mach/pxa27x_keypad.h>
 #include <mach/pxa2xx_spi.h>
 #include <mach/camera.h>
 #include <mach/audio.h>
-#include <mach/pxa3xx_nand.h>
+#include <plat/i2c.h>
+#include <plat/pxa3xx_nand.h>
 
 #include "devices.h"
 #include "generic.h"
--- drivers/mtd/nand/pxa3xx_nand.c
+++ drivers/mtd/nand/pxa3xx_nand.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 
 #include <mach/dma.h>
-#include <mach/pxa3xx_nand.h>
+#include <plat/pxa3xx_nand.h>
 
 #define	CHIP_DELAY_TIMEOUT	(2 * HZ/10)
 
