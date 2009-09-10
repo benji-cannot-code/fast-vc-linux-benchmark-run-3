4bedbdda9d2f487caca5684ebf8792053a885fc9
Jesse Barnes <jbarnes@virtuousgeek.org>
2009-09-10 15:28:06 -0700

    drm/i915: framebuffer compression for pre-GM45
    This patch adds framebuffer compression (good for about ~0.5W power
    savings in the best case) support for pre-GM45 chips.  GM45+ have a new,
    more flexible FBC scheme that will be added in a separate patch.
    
    FBC can't always be enabled: the compressed buffer must be physically
    contiguous and reside in stolen space.  So if you have a large display
    and a small amount of stolen memory, you may not be able to take
    advantage of FBC.  In some cases, a BIOS setting controls how much
    stolen space is available.  Increasing this to 8 or 16M can help.
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -344,6 +344,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   FBC_CTL_PLANEA	(0<<0)
 #define   FBC_CTL_PLANEB	(1<<0)
 #define FBC_FENCE_OFF		0x0321b
+#define FBC_TAG			0x03300
 
 #define FBC_LL_SIZE		(1536)
 
