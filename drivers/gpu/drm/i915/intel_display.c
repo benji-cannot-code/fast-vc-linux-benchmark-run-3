8f6d81fcd39a2e15884fb6f5276551354fde01e8
Jesse Barnes <jbarnes@virtuousgeek.org>
2009-09-10 15:28:03 -0700

    drm/i915: force mode set at lid open time
    Some laptop platforms will disable pipes and/or planes at lid close time
    and not restore them when the lid is opened again.  So catch the lid
    event, and if the lid was opened, force a mode restore.
    
    Fixes fdo bug #21230.
    
    Acked-by: Matthew Garrett <mjg@redhat.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	Eric Anholt <eric@anholt.net>
  */
 
+#include <linux/module.h>
+#include <linux/input.h>
 #include <linux/i2c.h>
 #include <linux/kernel.h>
 #include "drmP.h"
