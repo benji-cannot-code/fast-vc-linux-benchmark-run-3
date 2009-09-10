d40ad470f5541d993e6a1e5d84588f03910892ab
Zhao Yakui <yakui.zhao@intel.com>
2009-09-10 15:45:47 +0800

    drm/i915: Add the brightness property for SDVO-LVDS
    When the sdvo device is detected as SDVO-LVDS, we will check whether the
    brightness is supported by issue SDVO enhancement command.
    If it is supported, we will add the brightness property and then brightness
    can be adjusted.
    
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/intel_sdvo.c
+++ drivers/gpu/drm/i915/intel_sdvo.c
@@ -1873,7 +1873,7 @@ void intel_sdvo_destroy_enhance_property(struct drm_connector *connector)
 		if (sdvo_priv->hue_property)
 			drm_property_destroy(dev, sdvo_priv->hue_property);
 	}
-	if (sdvo_priv->is_tv) {
+	if (sdvo_priv->is_tv || sdvo_priv->is_lvds) {
 		if (sdvo_priv->brightness_property)
 			drm_property_destroy(dev,
 					sdvo_priv->brightness_property);
@@ -1901,7 +1901,7 @@ static void intel_sdvo_destroy(struct drm_connector *connector)
 		drm_property_destroy(connector->dev,
 				     sdvo_priv->tv_format_property);
 
-	if (sdvo_priv->is_tv)
+	if (sdvo_priv->is_tv || sdvo_priv->is_lvds)
 		intel_sdvo_destroy_enhance_property(connector);
 
 	drm_sysfs_connector_remove(connector);
@@ -1941,7 +1941,7 @@ intel_sdvo_set_property(struct drm_connector *connector,
 		changed = true;
 	}
 
-	if (sdvo_priv->is_tv) {
+	if (sdvo_priv->is_tv || sdvo_priv->is_lvds) {
 		cmd = 0;
 		temp_value = val;
 		if (sdvo_priv->left_property == property) {
@@ -2628,7 +2628,7 @@ static void intel_sdvo_create_enhance_property(struct drm_connector *connector)
 					data_value[0], data_value[1], response);
 		}
 	}
-	if (sdvo_priv->is_tv) {
+	if (sdvo_priv->is_tv || sdvo_priv->is_lvds) {
 		if (sdvo_data.brightness) {
 			intel_sdvo_write_cmd(intel_output,
 				SDVO_CMD_GET_MAX_BRIGHTNESS, NULL, 0);
@@ -2751,10 +2751,12 @@ bool intel_sdvo_init(struct drm_device *dev, int output_device)
 	drm_encoder_helper_add(&intel_output->enc, &intel_sdvo_helper_funcs);
 
 	drm_mode_connector_attach_encoder(&intel_output->base, &intel_output->enc);
-	if (sdvo_priv->is_tv) {
+	if (sdvo_priv->is_tv)
 		intel_sdvo_tv_create_property(connector);
+
+	if (sdvo_priv->is_tv || sdvo_priv->is_lvds)
 		intel_sdvo_create_enhance_property(connector);
-	}
+
 	drm_sysfs_connector_add(connector);
 
 	intel_sdvo_select_ddc_bus(sdvo_priv);
