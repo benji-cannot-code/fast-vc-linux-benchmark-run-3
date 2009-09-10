d654e635e94f782718106e7cbcf65c90e1135e2a
Jesse Barnes <jbarnes@virtuousgeek.org>
2009-09-10 15:28:02 -0700

    ACPI button: provide lid status functions
    Some drivers need to know when a lid event occurs and get the current
    status.  This can be useful for when a platform firmware clobbers some
    hardware state at lid time, and a driver needs to restore things when
    the lid is opened again.
    
    Acked-by: Matthew Garrett <mjg59@srcf.ucam.org>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Eric Anholt <eric@anholt.net>

---
+++ include/acpi/button.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef ACPI_BUTTON_H
+#define ACPI_BUTTON_H
+
+#include <linux/notifier.h>
+
+extern int acpi_lid_notifier_register(struct notifier_block *nb);
+extern int acpi_lid_notifier_unregister(struct notifier_block *nb);
+extern int acpi_lid_open(void);
+
+#endif /* ACPI_BUTTON_H */
