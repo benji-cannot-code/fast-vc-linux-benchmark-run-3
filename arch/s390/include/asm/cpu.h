ac44cacde63dd592c3bb08dbf3d4aa7f82deab4a
Heiko Carstens <heiko.carstens@de.ibm.com>
2009-09-11 10:29:03 +0200

    [S390] Limit cpu detection to 256 physical cpus.
    Saves us more than 65k pointless IPIs.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

---
+++ arch/s390/include/asm/cpu.h
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _ASM_S390_CPU_H
+#define _ASM_S390_CPU_H
+
+#define MAX_CPU_ADDRESS 255
+
+#endif /* _ASM_S390_CPU_H */
