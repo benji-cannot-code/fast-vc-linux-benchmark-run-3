4746d569afaec5e4afc91f2c85c3ddfa2973ba42
Kuninori Morimoto <morimoto.kuninori@renesas.com>
2009-09-14 11:23:16 +0000

    sh: mach-ecovec24: need asm/clock.h.
    Fixes up build failures.
    
    Signed-off-by: Kuninori Morimoto <morimoto.kuninori@renesas.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-ecovec24/setup.c
+++ arch/sh/boards/mach-ecovec24/setup.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/heartbeat.h>
 #include <asm/sh_eth.h>
 #include <asm/sh_keysc.h>
+#include <asm/clock.h>
 #include <cpu/sh7724.h>
 
 /*
