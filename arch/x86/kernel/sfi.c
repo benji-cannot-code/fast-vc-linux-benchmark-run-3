74d8102ed8723c1386c44b5846a501fdcee42b2c
Feng Tang <feng.tang@intel.com>
2009-09-14 13:01:53 +0800

    SFI: remove unneeded includes
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- arch/x86/kernel/sfi.c
+++ arch/x86/kernel/sfi.c
@@ -22,22 +22,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KMSG_COMPONENT "SFI"
 #define pr_fmt(fmt) KMSG_COMPONENT ": " fmt
 
-#include <linux/bootmem.h>
-#include <linux/cpumask.h>
-#include <linux/module.h>
 #include <linux/acpi.h>
 #include <linux/init.h>
-#include <linux/efi.h>
-#include <linux/irq.h>
 #include <linux/sfi.h>
 #include <linux/io.h>
 
 #include <asm/io_apic.h>
-#include <asm/pgtable.h>
 #include <asm/mpspec.h>
 #include <asm/setup.h>
 #include <asm/apic.h>
-#include <asm/e820.h>
 
 #ifdef CONFIG_X86_LOCAL_APIC
 static unsigned long sfi_lapic_addr __initdata = APIC_DEFAULT_PHYS_BASE;
--- drivers/sfi/sfi_core.c
+++ drivers/sfi/sfi_core.c
@@ -63,16 +63,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bootmem.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/string.h>
 #include <linux/errno.h>
 #include <linux/types.h>
 #include <linux/acpi.h>
 #include <linux/init.h>
-#include <linux/irq.h>
 #include <linux/sfi.h>
-#include <linux/smp.h>
-
-#include <asm/pgtable.h>
 
 #include "sfi_core.h"
 
