e3ac065903bdb01f6eba651703d0bd791e5d7e84
Antti Palosaari <crope@iki.fi>
2009-09-15 14:42:13 -0300

    V4L/DVB (12951): em28xx: add Reddo DVB-C USB TV Box
    Support for Reddo DVB-C USB TV Box device. Remote is not working yet.
    Thanks to Benjamin Larsson <banan@ludd.ltu.se>
    
    Cc: Benjamin Larsson <banan@ludd.ltu.se>
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx.h
+++ drivers/media/video/em28xx/em28xx.h
@@ -110,6 +110,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2882_BOARD_EVGA_INDTUBE		  70
 #define EM2820_BOARD_SILVERCREST_WEBCAM           71
 #define EM2861_BOARD_GADMEI_UTV330PLUS           72
+#define EM2870_BOARD_REDDO_DVB_C_USB_BOX          73
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
