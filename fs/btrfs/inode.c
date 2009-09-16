cb25114631c2387748ae08828e4f385b902f0e56
Andi Kleen <andi@firstfloor.org>
2009-09-16 11:50:18 +0200

    HWPOISON: Enable error_remove_page on btrfs
    Cc: chris.mason@oracle.com
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -5270,6 +5270,7 @@ static struct address_space_operations btrfs_aops = {
 	.invalidatepage = btrfs_invalidatepage,
 	.releasepage	= btrfs_releasepage,
 	.set_page_dirty	= btrfs_set_page_dirty,
+	.error_remove_page = generic_error_remove_page,
 };
 
 static struct address_space_operations btrfs_symlink_aops = {
