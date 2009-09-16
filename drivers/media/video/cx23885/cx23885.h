3924a62df50261a8521207771e028c70147a538b
Abylay Ospan <aospan@netup.ru>
2009-09-16 13:08:06 -0300

    V4L/DVB (12956): Fix gpio mutex in NetUP Dual DVB-S2 CI card.
    The card uses the same cx23885 gpio lines for two adapters.
    In case of there is several cards in system we must implement
    gpio mutex per cx23885 chip.
    
    Signed-off-by: Abylay Ospan <aospan@netup.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx23885/cx23885.h
+++ drivers/media/video/cx23885/cx23885.h
@@ -326,6 +326,7 @@ struct cx23885_dev {
 
 	int                        nr;
 	struct mutex               lock;
+	struct mutex               gpio_lock;
 
 	/* board details */
 	unsigned int               board;
