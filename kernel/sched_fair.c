b633042c3229c63b9a2bda205fba89b3ea246171
Peter Zijlstra <peterz@infradead.org>
2009-09-16 12:31:31 +0200

    sched: Add new wakeup preemption mode: WAKEUP_RUNNING
    Create a new wakeup preemption mode, preempt towards tasks that run
    shorter on avg. It sets next buddy to be sure we actually run the task
    we preempted for.
    
    Test results:
    
     root@twins:~# while :; do :; done &
     [1] 6537
     root@twins:~# while :; do :; done &
     [2] 6538
     root@twins:~# while :; do :; done &
     [3] 6539
     root@twins:~# while :; do :; done &
     [4] 6540
    
     root@twins:/home/peter# ./latt -c4 sleep 4
     Entries: 48 (clients=4)
    
     Averages:
     ------------------------------
            Max          4750 usec
            Avg           497 usec
            Stdev         737 usec
    
     root@twins:/home/peter# echo WAKEUP_RUNNING > /debug/sched_features
    
     root@twins:/home/peter# ./latt -c4 sleep 4
     Entries: 48 (clients=4)
    
     Averages:
     ------------------------------
            Max            14 usec
            Avg             5 usec
            Stdev           3 usec
    
    Disabled by default - needs more testing.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Acked-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    LKML-Reference: <new-submission>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1606,9 +1606,6 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 		return;
 	}
 
-	if (!sched_feat(WAKEUP_PREEMPT))
-		return;
-
 	if ((sched_feat(WAKEUP_SYNC) && sync) ||
 	    (sched_feat(WAKEUP_OVERLAP) &&
 	     (se->avg_overlap < sysctl_sched_migration_cost &&
@@ -1616,6 +1613,17 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 		resched_task(curr);
 		return;
 	}
+
+	if (sched_feat(WAKEUP_RUNNING)) {
+		if (pse->avg_running < se->avg_running) {
+			set_next_buddy(pse);
+			resched_task(curr);
+			return;
+		}
+	}
+
+	if (!sched_feat(WAKEUP_PREEMPT))
+		return;
 
 	find_matching_se(&se, &pse);
 
