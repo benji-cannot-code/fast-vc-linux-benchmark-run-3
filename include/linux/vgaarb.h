1adfa512f29751c020e4f76731cafe14c859e799
Jesse Barnes <jbarnes@virtuousgeek.org>
2009-09-17 09:40:31 -0700

    PCI: fix VGA arbiter header file
    Remove reference to vgaarb.c and replace it with a comment about the
    arbiter itself.
    
    Reported-by: Tiago Vignatti <tiago.vignatti@nokia.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- include/linux/vgaarb.h
+++ include/linux/vgaarb.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * vgaarb.c
+ * The VGA aribiter manages VGA space routing and VGA resource decode to
+ * allow multiple VGA devices to be used in a system in a safe way.
  *
  * (C) Copyright 2005 Benjamin Herrenschmidt <benh@kernel.crashing.org>
  * (C) Copyright 2007 Paulo R. Zanoni <przanoni@gmail.com>
