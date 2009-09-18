c236ae98a9fa9b5f2a390e5369eff3adc70d63f0
Christian Lamparter <chunkeey@googlemail.com>
2009-09-18 18:20:03 +0200

    iwlwifi: drop lib80211 dependency
    Ever since Johannes' "iwlwifi: improve scan support" iwlwifi
    no longer needs any of lib80211's functions or definitions.
    
    This patch updates iwlwifi's Kconfig _selections_ and
    removes all left lib80211.h inclusions from the source files.
    
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>
    Acked-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-scan.c
+++ drivers/net/wireless/iwlwifi/iwl-scan.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *****************************************************************************/
 #include <linux/types.h>
 #include <linux/etherdevice.h>
-#include <net/lib80211.h>
 #include <net/mac80211.h>
 
 #include "iwl-eeprom.h"
--- drivers/net/wireless/iwlwifi/iwl3945-base.c
+++ drivers/net/wireless/iwlwifi/iwl3945-base.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 
 #include <net/ieee80211_radiotap.h>
-#include <net/lib80211.h>
 #include <net/mac80211.h>
 
 #include <asm/div64.h>
