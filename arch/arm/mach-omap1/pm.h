fac7c96f93af69b6b88ed830619c8909ec83a0c1
Alistair Buxton <a.j.buxton@gmail.com>
2009-09-18 20:53:04 +0100

    OMAP850: PM: Add an ARCH_OMAP850 check
    This just makes the same warning be printed on omap850 and omap730.
    
    Signed-off-by: Alistair Buxton <a.j.buxton@gmail.com>

--- arch/arm/mach-omap1/pm.h
+++ arch/arm/mach-omap1/pm.h
@@ -106,6 +106,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP7XX_IDLE_LOOP_REQUEST	0x0C00
 
 #if     !defined(CONFIG_ARCH_OMAP730) && \
+	!defined(CONFIG_ARCH_OMAP850) && \
 	!defined(CONFIG_ARCH_OMAP15XX) && \
 	!defined(CONFIG_ARCH_OMAP16XX)
 #warning "Power management for this processor not implemented yet"
