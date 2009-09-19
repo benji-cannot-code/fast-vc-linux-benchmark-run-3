d61ea59ea2bd5b1076908e23f0141858c4b08671
Zhenyu Wang <zhenyuw@linux.intel.com>
2009-09-19 14:54:09 +0800

    drm/i915: Fix LVDS panel fitting on Arrandale
    Arrandale has new window based method for panel fitting.
    This one enables full screen aspect scaling on LVDS. It fixes
    standard mode display failure on LVDS for Arrandale.
    
    Cc: Stable Team <stable@kernel.org>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -2001,6 +2001,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  PF_ENABLE              (1<<31)
 #define PFA_WIN_SZ		0x68074
 #define PFB_WIN_SZ		0x68874
+#define PFA_WIN_POS		0x68070
+#define PFB_WIN_POS		0x68870
 
 /* legacy palette */
 #define LGC_PALETTE_A           0x4a000
