6530c5caec040ca6b5bb52cf22b0ae2f66d470ae
Chris Wilson <chris@chris-wilson.co.uk>
2009-09-20 23:03:19 +0100

    drm/i915: Whitespace correction for madv
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -1479,7 +1479,7 @@ i915_gem_object_put_pages(struct drm_gem_object *obj)
 		i915_gem_object_save_bit_17_swizzle(obj);
 
 	if (obj_priv->madv == I915_MADV_DONTNEED)
-	    obj_priv->dirty = 0;
+		obj_priv->dirty = 0;
 
 	for (i = 0; i < page_count; i++) {
 		if (obj_priv->pages[i] == NULL)
@@ -1489,7 +1489,7 @@ i915_gem_object_put_pages(struct drm_gem_object *obj)
 			set_page_dirty(obj_priv->pages[i]);
 
 		if (obj_priv->madv == I915_MADV_WILLNEED)
-		    mark_page_accessed(obj_priv->pages[i]);
+			mark_page_accessed(obj_priv->pages[i]);
 
 		page_cache_release(obj_priv->pages[i]);
 	}
