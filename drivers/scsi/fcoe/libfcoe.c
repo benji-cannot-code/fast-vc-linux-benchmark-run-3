fbc971b480cc0cbb4b6a52a3d7650f39f9b81951
Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
2009-09-20 15:20:55 +0530

    includecheck fix: drivers/scsi, libfcoe.c
    fix the following 'make includecheck' warning:
    
      drivers/scsi/fcoe/libfcoe.c: linux/netdevice.h is included more than once.
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
    Cc: James Bottomley <James.Bottomley@HansenPartnership.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    LKML-Reference: <1247066936.4382.76.camel@ht.satnam>

--- drivers/scsi/fcoe/libfcoe.c
+++ drivers/scsi/fcoe/libfcoe.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ethtool.h>
 #include <linux/if_ether.h>
 #include <linux/if_vlan.h>
-#include <linux/netdevice.h>
 #include <linux/errno.h>
 #include <linux/bitops.h>
 #include <net/rtnetlink.h>
