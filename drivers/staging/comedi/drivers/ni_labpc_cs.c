168e3ae277fc1056f8698735c96b46afb42e52f2
Bart Massey <bart@cs.pdx.edu>
2009-09-21 11:32:36 -0700

    Staging: comedi: ni_labbc_cs: cleaned up debug define a bit
    Signed-off-by: Bart Massey <bart@cs.pdx.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/ni_labpc_cs.c
+++ drivers/staging/comedi/drivers/ni_labpc_cs.c
@@ -60,8 +60,7 @@ NI manuals:
 
 */
 
-#undef LABPC_DEBUG
-			    /* #define LABPC_DEBUG *//*  enable debugging messages */
+#undef LABPC_DEBUG  /* debugging messages */
 
 #include "../comedidev.h"
 
