77c7d1f2040f61793400be44d8900f50d877351e
Vijay Kumar B <vijaykumar@bravegnu.org>
2009-09-21 11:23:59 +0530

    Staging: poch: Include linux/types.h
    Include linux/types.h in poch.h, so that poch.h can be included in
    user application header files.
    
    Signed-off-by: Vijay Kumar B. <vijaykumar@bravegnu.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/poch/poch.h
+++ drivers/staging/poch/poch.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * include/linux for final version.
  *
  */
+
+#include <linux/types.h>
+
 struct poch_counters {
 	__u32 fifo_empty;
 	__u32 fifo_overflow;
