f5ae2ce1ee5539cf185830b112eb7f69d66858fd
Bjorn Helgaas <bjorn.helgaas@hp.com>
2009-09-21 13:35:29 -0600

    ACPI: remove acpi_device.flags.hardware_id
    Every acpi_device has at least one ID (if there's no _HID or _CID, we
    give it a synthetic or default ID).  So there's no longer a need to
    check whether an ID exists; we can just use it.
    
    Signed-off-by: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -142,7 +142,6 @@ struct acpi_device_status {
 
 struct acpi_device_flags {
 	u32 dynamic_status:1;
-	u32 hardware_id:1;
 	u32 bus_address:1;
 	u32 unique_id:1;
 	u32 removable:1;
@@ -153,7 +152,7 @@ struct acpi_device_flags {
 	u32 performance_manageable:1;
 	u32 wake_capable:1;	/* Wakeup(_PRW) supported? */
 	u32 force_power_state:1;
-	u32 reserved:20;
+	u32 reserved:21;
 };
 
 /* File System */
