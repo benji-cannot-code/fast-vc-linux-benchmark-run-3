cad82b296f2281e60874d1b5a5095f441402ffd5
Mike Frysinger <vapier@gentoo.org>
2009-09-21 01:42:52 +0000

    Blackfin: asm/irq.h: pull in mach/anomaly.h for anomaly defines
    The asm/irq.h header uses anomaly defines, but doesn't make sure to
    explicitly include the anomaly header for them.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/include/asm/irq.h
+++ arch/blackfin/include/asm/irq.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/irqflags.h>
 
+#include <mach/anomaly.h>
+
 /* SYS_IRQS and NR_IRQS are defined in <mach-bf5xx/irq.h> */
 #include <mach/irq.h>
 
