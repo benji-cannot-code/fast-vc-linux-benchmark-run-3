20a979baf0a464e577623b9723e678c002739582
Jean Delvare <khali@linux-fr.org>
2009-09-22 16:47:50 -0700

    matroxfb: get rid of CONFIG_FB_MATROX_32MB
    CONFIG_FB_MATROX_32MB is always enabled, so there is no point in having
    ifdefs all around.  And it is bad practice to use CONFIG_* as a name for
    something which is not a Kconfig option.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Acked-by: Petr Vandrovec <vandrove@vc.cvut.cz>
    Cc: Krzysztof Helt <krzysztof.h1@poczta.fm>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/matrox/matroxfb_base.h
+++ drivers/video/matrox/matroxfb_base.h
@@ -55,9 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../macmodes.h"
 #endif
 
-/* always compile support for 32MB... It cost almost nothing */
-#define CONFIG_FB_MATROX_32MB
-
 #ifdef MATROXFB_DEBUG
 
 #define DEBUG
@@ -465,9 +462,7 @@ struct matrox_fb_info {
 		int		nopciretry;
 		int		noinit;
 		int		sgram;
-#ifdef CONFIG_FB_MATROX_32MB
 		int		support32MB;
-#endif
 
 		int		accelerator;
 		int		text_type_aux;
