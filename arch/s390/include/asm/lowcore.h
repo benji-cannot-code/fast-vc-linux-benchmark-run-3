7056788feffca8ee496ed5cb57fdf55eff95a373
Michael Holzheu <michael.holzheu@linux.vnet.ibm.com>
2009-09-22 22:58:53 +0200

    [S390] hibernate: Do real CPU swap at resume time
    Currently, when the physical resume CPU is not equal to the physical suspend
    CPU, we swap the CPUs logically, by modifying the logical/physical CPU mapping.
    This has two major drawbacks: First the change is visible from user space (e.g.
    CPU sysfs files) and second it is hard to ensure that nowhere in the kernel
    the physical CPU ID is stored before suspend.
    To fix this, we now really swap the physical CPUs, if the resume CPU is not
    the pysical suspend CPU. We restart the suspend CPU and stop the resume CPU
    using SIGP restart and SIGP stop. If the suspend CPU is no longer available,
    we write a message and load a disabled wait PSW.
    
    Signed-off-by: Michael Holzheu <michael.holzheu@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/lowcore.h
+++ arch/s390/include/asm/lowcore.h
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __LC_PGM_OLD_PSW		0x0150
 #define __LC_MCK_OLD_PSW		0x0160
 #define __LC_IO_OLD_PSW			0x0170
+#define __LC_RESTART_PSW		0x01a0
 #define __LC_EXT_NEW_PSW		0x01b0
 #define __LC_SVC_NEW_PSW		0x01c0
 #define __LC_PGM_NEW_PSW		0x01d0
