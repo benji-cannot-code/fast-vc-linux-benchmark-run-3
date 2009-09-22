a6e1844acf4f3cfc80d14d0df541b303131d4309
Ben Dooks <ben@simtec.co.uk>
2009-09-22 16:46:12 -0700

    spi_s3c24xx: fix header includes
    The driver includes <asm/io.h> where it should be including <linux/io.h>
    and also includes <mach/hardware.h> and <asm/dma.h> without using anything
    from these.
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>
    Cc: David Brownell <david-b@pacbell.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/spi/spi_s3c24xx.c
+++ drivers/spi/spi_s3c24xx.c
@@ -21,13 +21,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/platform_device.h>
 #include <linux/gpio.h>
+#include <linux/io.h>
 
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_bitbang.h>
-
-#include <asm/io.h>
-#include <asm/dma.h>
-#include <mach/hardware.h>
 
 #include <plat/regs-spi.h>
 #include <mach/spi.h>
