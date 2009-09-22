49b370dec5410a35de3f3c49e171cb1fbc78e6cb
Rob Emanuele <rob@emanuele.us>
2009-09-22 16:45:22 -0700

    AT91: atmel-mci: Platform configuration to the the atmel-mci driver
    Created a modified version of the at91sam9g20 evaluation kit platform
    (board-sam9g20ek-2slot-mmc.c) and device support to make use of the
    updated atmel-mci driver.
    
    As the use of two slots modify GPIO pin allocation, we create another
    board file.
    
    This requires getting the most updated arch/arm/tools/mach-types from
    http://www.arm.linux.org.uk/developer/machines/download.php to have the machine
    type for the at91sam9g20ek-2slot-mmc board.
    
    [nicolas.ferre@atmel.com: printk, slot_count modification in at91sam9260_devices.c file]
    [akpm@linux-foundation.org: coding-style fixes]
    Signed-off-by: Rob Emanuele <rob@emanuele.us>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Cc: Haavard Skinnemoen <hskinnemoen@atmel.com>
    Cc: Andrew Victor <linux@maxim.org.za>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Cc: <linux-mmc@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm/mach-at91/include/mach/board.h
+++ arch/arm/mach-at91/include/mach/board.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/leds.h>
 #include <linux/spi/spi.h>
 #include <linux/usb/atmel_usba_udc.h>
+#include <linux/atmel-mci.h>
 #include <sound/atmel-ac97c.h>
 
  /* USB Device */
@@ -65,6 +66,7 @@ struct at91_cf_data {
 extern void __init at91_add_device_cf(struct at91_cf_data *data);
 
  /* MMC / SD */
+  /* at91_mci platform config */
 struct at91_mmc_data {
 	u8		det_pin;	/* card detect IRQ */
 	unsigned	slot_b:1;	/* uses Slot B */
@@ -73,6 +75,9 @@ struct at91_mmc_data {
 	u8		vcc_pin;	/* power switching (high == on) */
 };
 extern void __init at91_add_device_mmc(short mmc_id, struct at91_mmc_data *data);
+
+  /* atmel-mci platform config */
+extern void __init at91_add_device_mci(short mmc_id, struct mci_platform_data *data);
 
  /* Ethernet (EMAC & MACB) */
 struct at91_eth_data {
