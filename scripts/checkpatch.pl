3903ff3adc7ebf2e48c7113005ef10bf6d3ee3e4
Andy Whitcroft <apw@canonical.com>
2009-09-21 17:04:36 -0700

    checkpatch: indent checks -- stop when we run out of continuation lines
    Ensure we terminate when there are no futher continuation lines when
    trying to determine relative indent of conditionals and their blocks.
    
    Reported-by: John Daiker <daikerjohn@gmail.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -1548,8 +1548,9 @@ sub process {
 				    $s =~ /^\s*#\s*?/ ||
 				    $s =~ /^\s*$Ident\s*:/) {
 					$continuation = ($s =~ /^.*?\\\n/) ? 1 : 0;
-					$s =~ s/^.*?\n//;
-					$cond_lines++;
+					if ($s =~ s/^.*?\n//) {
+						$cond_lines++;
+					}
 				}
 			}
 
