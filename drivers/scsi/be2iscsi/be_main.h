708d43124a24fc088ec3b6e0270a1a9e05c37b52
Jayamohan Kallickal <jayamohank@serverengines.com>
2009-09-22 08:21:22 +0530

    [SCSI] libiscsi: iscsi_session_setup to allow for private space
    This patch contains changes that allow iscsi_session_setup
    to allocate private space for LLD's
    
    Signed-off-by: Jayamohan Kallickal <jayamohank@serverengines.com>
    Acked-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/be2iscsi/be_main.h
+++ drivers/scsi/be2iscsi/be_main.h
@@ -328,6 +328,10 @@ struct beiscsi_hba {
 	struct be_ctrl_info ctrl;
 };
 
+struct beiscsi_session {
+	struct pci_pool *bhs_pool;
+};
+
 /**
  * struct beiscsi_conn - iscsi connection structure
  */
@@ -339,6 +343,7 @@ struct beiscsi_conn {
 	struct beiscsi_endpoint *ep;
 	unsigned short login_in_progress;
 	struct sgl_handle *plogin_sgl_handle;
+	struct beiscsi_session *beiscsi_sess;
 };
 
 /* This structure is used by the chip */
