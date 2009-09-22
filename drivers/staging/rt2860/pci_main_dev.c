05ecc1579ae2fe4e8ee7184ce5f675d48ef1acd1
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-09-22 20:44:33 +0200

    Staging: remove no longer needed rt3090 driver
    rt2860 handles now all rt2860/rt3090 chipsets.
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2860/pci_main_dev.c
+++ drivers/staging/rt2860/pci_main_dev.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 MODULE_AUTHOR("Jett Chen <jett_chen@ralinktech.com>");
 MODULE_DESCRIPTION("RT2860/RT3090 Wireless Lan Linux Driver");
 MODULE_LICENSE("GPL");
+MODULE_ALIAS("rt3090sta");
 
 //
 // Function declarations
