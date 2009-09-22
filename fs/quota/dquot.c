34580d4844f101e4f79f11470337fc35253d48da
Alexey Dobriyan <adobriyan@gmail.com>
2009-09-21 17:01:08 -0700

    const: make struct super_block::dq_op const
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/quota/dquot.c
+++ fs/quota/dquot.c
@@ -1840,7 +1840,7 @@ EXPORT_SYMBOL(dquot_commit_info);
 /*
  * Definitions of diskquota operations.
  */
-struct dquot_operations dquot_operations = {
+const struct dquot_operations dquot_operations = {
 	.initialize	= dquot_initialize,
 	.drop		= dquot_drop,
 	.alloc_space	= dquot_alloc_space,
