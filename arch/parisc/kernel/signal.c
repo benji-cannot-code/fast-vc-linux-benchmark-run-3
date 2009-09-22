4a198b3ec4d7977ee6996299235ae1aa9dd3d7ac
Jaswinder Singh Rajput <jaswinder@kernel.org>
2009-09-22 13:35:32 +0000

    parisc: includecheck fix: signal.c
    fix the following 'make includecheck' warning:
    
      arch/parisc/kernel/signal.c: linux/compat.h is included more than once.
    
    Signed-off-by: Jaswinder Singh Rajput <jaswinderrajput@gmail.com>
    Signed-off-by: Kyle McMartin <kyle@mcmartin.ca>

--- arch/parisc/kernel/signal.c
+++ arch/parisc/kernel/signal.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/asm-offsets.h>
 
 #ifdef CONFIG_COMPAT
-#include <linux/compat.h>
 #include "signal32.h"
 #endif
 
