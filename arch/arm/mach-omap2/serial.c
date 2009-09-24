6b7f48fa1d9575c13d4b3057be046776830cd719
Tony Lindgren <tony@atomide.com>
2009-09-24 16:23:07 -0700

    omap: Fix 44xx compile
    Looks like these patches were not tested that well..
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/serial.c
+++ arch/arm/mach-omap2/serial.c
@@ -580,7 +580,7 @@ static struct omap_uart_state omap_uart[OMAP_MAX_NR_PORTS] = {
 	{
 		.pdev = {
 			.name			= "serial8250",
-			.id			= 3
+			.id			= 3,
 			.dev			= {
 				.platform_data	= serial_platform_data3,
 			},
