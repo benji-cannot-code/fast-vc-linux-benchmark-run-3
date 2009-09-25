128ee65f82591e9a6cdf14483b8f025774e39df7
Jiri Pirko <jpirko@redhat.com>
2009-09-25 03:28:09 +0000

    bonding: introduce primary_reselect option
    In some cases there is not desirable to switch back to primary interface when
    it's link recovers and rather stay with currently active one. We need to avoid
    packetloss as much as we can in some cases. This is solved by introducing
    primary_reselect option. Note that enslaved primary slave is set as current
    active no matter what.
    
    Patch modified by Jay Vosburgh as follows: fixed bug in action
    after change of option setting via sysfs, revised the documentation
    update, and bumped the bonding version number.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>
    Signed-off-by: Jay Vosburgh <fubar@us.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bond_3ad.h"
 #include "bond_alb.h"
 
-#define DRV_VERSION	"3.5.0"
-#define DRV_RELDATE	"November 4, 2008"
+#define DRV_VERSION	"3.6.0"
+#define DRV_RELDATE	"September 26, 2009"
 #define DRV_NAME	"bonding"
 #define DRV_DESCRIPTION	"Ethernet Channel Bonding Driver"
 
@@ -132,6 +132,7 @@ struct bond_params {
 	int lacp_fast;
 	int ad_select;
 	char primary[IFNAMSIZ];
+	int primary_reselect;
 	__be32 arp_targets[BOND_MAX_ARP_TARGETS];
 };
 
@@ -191,6 +192,7 @@ struct bonding {
 	struct   slave *curr_active_slave;
 	struct   slave *current_arp_slave;
 	struct   slave *primary_slave;
+	bool     force_primary;
 	s32      slave_cnt; /* never change this value outside the attach/detach wrappers */
 	rwlock_t lock;
 	rwlock_t curr_slave_lock;
@@ -258,6 +260,10 @@ static inline bool bond_is_lb(const struct bonding *bond)
 	return bond->params.mode == BOND_MODE_TLB
 		|| bond->params.mode == BOND_MODE_ALB;
 }
+
+#define BOND_PRI_RESELECT_ALWAYS	0
+#define BOND_PRI_RESELECT_BETTER	1
+#define BOND_PRI_RESELECT_FAILURE	2
 
 #define BOND_FOM_NONE			0
 #define BOND_FOM_ACTIVE			1
@@ -349,6 +355,7 @@ extern const struct bond_parm_tbl bond_mode_tbl[];
 extern const struct bond_parm_tbl xmit_hashtype_tbl[];
 extern const struct bond_parm_tbl arp_validate_tbl[];
 extern const struct bond_parm_tbl fail_over_mac_tbl[];
+extern const struct bond_parm_tbl pri_reselect_tbl[];
 extern struct bond_parm_tbl ad_select_tbl[];
 
 #if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
