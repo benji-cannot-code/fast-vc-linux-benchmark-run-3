26d2a7277b4ef60e6b6833d42ed56a7e2330bcd9
Zhenyu Wang <zhenyuw@linux.intel.com>
2009-09-25 08:01:28 +0000

    drm/i915: Fix FDI M/N setting according with correct color depth
    FDI M/N calculation hasn't taken the current pipe color depth into account,
    but always set as 24bpp. This one checks current pipe color depth setting,
    and change FDI M/N calculation a little to use bits_per_pixel first, then
    convert to bytes_per_pixel later.
    
    This fixes display corrupt issue on Arrandle LVDS with 1600x900 panel
    in 18bpp dual-channel mode.
    
    Cc: Stable Team <stable@kernel.org>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -1783,6 +1783,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   PIPE_START_VBLANK_INTERRUPT_STATUS	(1UL<<2) /* 965 or later */
 #define   PIPE_VBLANK_INTERRUPT_STATUS		(1UL<<1)
 #define   PIPE_OVERLAY_UPDATED_STATUS		(1UL<<0)
+#define   PIPE_BPC_MASK 			(7 << 5) /* Ironlake */
+#define   PIPE_8BPC				(0 << 5)
+#define   PIPE_10BPC				(1 << 5)
+#define   PIPE_6BPC				(2 << 5)
+#define   PIPE_12BPC				(3 << 5)
 
 #define DSPARB			0x70030
 #define   DSPARB_CSTART_MASK	(0x7f << 7)
