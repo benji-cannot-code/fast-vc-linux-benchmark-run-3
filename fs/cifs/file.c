e440f4b670c48875398dc00280c042afed0744a2
Jeff Layton <jlayton@redhat.com>
2009-09-25 09:53:37 -0400

    cifs: fix locking and list handling code in cifs_open and its helper
    The patch to remove cifs_init_private introduced a locking imbalance. It
    didn't remove the leftover list addition code and the unlocking in that
    function. cifs_new_fileinfo does the list addition now, so there should
    be no need to do it outside of that function.
    
    pCifsInode will never be NULL, so we don't need to check for that. This
    patch also gets rid of the ugly locking and unlocking across function
    calls.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Acked-by: Steve French <sfrench@us.ibm.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -202,17 +202,6 @@ static inline int cifs_open_inode_helper(struct inode *inode, struct file *file,
 	struct timespec temp;
 	int rc;
 
-	/* want handles we can use to read with first
-	   in the list so we do not have to walk the
-	   list to search for one in write_begin */
-	if ((file->f_flags & O_ACCMODE) == O_WRONLY) {
-		list_add_tail(&pCifsFile->flist,
-			      &pCifsInode->openFileList);
-	} else {
-		list_add(&pCifsFile->flist,
-			 &pCifsInode->openFileList);
-	}
-	write_unlock(&GlobalSMBSeslock);
 	if (pCifsInode->clientCanCacheRead) {
 		/* we have the inode open somewhere else
 		   no need to discard cache data */
@@ -398,6 +387,7 @@ int cifs_open(struct inode *inode, struct file *file)
 		cFYI(1, ("cifs_open returned 0x%x", rc));
 		goto out;
 	}
+
 	pCifsFile = cifs_new_fileinfo(inode, netfid, file, file->f_path.mnt,
 					file->f_flags);
 	file->private_data = pCifsFile;
@@ -406,14 +396,8 @@ int cifs_open(struct inode *inode, struct file *file)
 		goto out;
 	}
 
-	pCifsInode = CIFS_I(file->f_path.dentry->d_inode);
-	if (pCifsInode) {
-		rc = cifs_open_inode_helper(inode, file, pCifsInode,
-					    pCifsFile, tcon,
-					    &oplock, buf, full_path, xid);
-	} else {
-		write_unlock(&GlobalSMBSeslock);
-	}
+	rc = cifs_open_inode_helper(inode, file, pCifsInode, pCifsFile, tcon,
+				    &oplock, buf, full_path, xid);
 
 	if (oplock & CIFS_CREATE_ACTION) {
 		/* time to set mode which we can not set earlier due to
