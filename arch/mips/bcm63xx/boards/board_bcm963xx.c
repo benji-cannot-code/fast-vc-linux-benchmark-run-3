1f54ae3bac23a2250c979f5981ded6713da665a2
Maxime Bizon <mbizon@freebox.fr>
2009-09-28 14:49:43 +0200

    MIPS: BCM63xx: Add PCMCIA & Cardbus support.
    Signed-off-by: Maxime Bizon <mbizon@freebox.fr>
    Reviewed-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/bcm63xx/boards/board_bcm963xx.c
+++ arch/mips/bcm63xx/boards/board_bcm963xx.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcm63xx_dev_pci.h>
 #include <bcm63xx_dev_enet.h>
 #include <bcm63xx_dev_dsp.h>
+#include <bcm63xx_dev_pcmcia.h>
 #include <bcm63xx_dev_uart.h>
 #include <board_bcm963xx.h>
 
@@ -795,6 +796,9 @@ int __init board_register_devices(void)
 	u32 val;
 
 	bcm63xx_uart_register();
+
+	if (board.has_pccard)
+		bcm63xx_pcmcia_register();
 
 	if (board.has_enet0 &&
 	    !board_get_mac_address(board.enet0.mac_addr))
