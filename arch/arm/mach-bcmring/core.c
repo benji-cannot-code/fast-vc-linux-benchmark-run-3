4ec01a4c1a54524b67e22ce47c162d58d3b32a9d
Leo Chen <leochen@broadcom.com>
2009-09-28 21:56:19 +0100

    ARM: 5733/1: fix bcmring compile error
    The movement of the MMCI header file made bcmring break. It turns
    out it was including asm/mmc.h without using it so fixing the
    problem boils down to removing the offending include.
    
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Acked-by: Scott Branden <sbranden@broadcom.com>
    Signed-off-by: Leo Hao Chen <leochen@broadcom.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-bcmring/core.c
+++ arch/arm/mach-bcmring/core.c
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/irq.h>
 #include <asm/mach/time.h>
 #include <asm/mach/map.h>
-#include <asm/mach/mmc.h>
 
 #include <cfg_global.h>
 
