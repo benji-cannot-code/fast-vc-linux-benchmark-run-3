0274f2dff88ab1cb849f6fbb5eac8f42bed6137d
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2009-09-29 21:07:19 -0300

    V4L/DVB (13152): uvcvideo: Rely on videodev to reference-count the device
    The uvcvideo driver has a driver-wide lock and a reference count to protect
    against a disconnect/open race. Now that videodev handles the race itself,
    reference-counting in the driver can be removed.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/uvc/uvcvideo.h
+++ drivers/media/video/uvc/uvcvideo.h
@@ -476,7 +476,6 @@ struct uvc_device {
 	char name[32];
 
 	enum uvc_device_state state;
-	struct kref kref;
 	struct list_head list;
 	atomic_t users;
 
@@ -489,6 +488,7 @@ struct uvc_device {
 
 	/* Video Streaming interfaces */
 	struct list_head streams;
+	atomic_t nstreams;
 
 	/* Status Interrupt Endpoint */
 	struct usb_host_endpoint *int_ep;
@@ -511,8 +511,6 @@ struct uvc_fh {
 
 struct uvc_driver {
 	struct usb_driver driver;
-
-	struct mutex open_mutex;	/* protects from open/disconnect race */
 
 	struct list_head devices;	/* struct uvc_device list */
 	struct list_head controls;	/* struct uvc_control_info list */
@@ -573,7 +571,6 @@ extern unsigned int uvc_trace_param;
 
 /* Core driver */
 extern struct uvc_driver uvc_driver;
-extern void uvc_delete(struct kref *kref);
 
 /* Video buffers queue management. */
 extern void uvc_queue_init(struct uvc_video_queue *queue,
