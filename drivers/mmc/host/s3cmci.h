8f4b4dcb8b98ac628c8c6767aca7366024ee7aa8
Ben Dooks <ben@simtec.co.uk>
2009-10-01 15:44:19 -0700

    s3cmci: DMA fixes
    Fixes for the DMA transfer mode of the driver to try and improve the state
    of the code:
    
    - Ensure that dma_complete is set during the end of the command phase
      so that transfers do not stall awaiting the completion
    
    - Update the DMA debugging to provide a bit more useful information
      such as how many DMA descriptors where not processed and print the
      DMA addresses in hexadecimal.
    
    - Fix the DMA channel request code to actually request DMA for the
      S3CMCI block instead of whatever '0' signified.
    
    - Add fallback to PIO if we cannot get the DMA channel, as many of the
      devices with this block only have a limited number of DMA channels.
    
    - Only try and claim and free the DMA channel if we are trying to use it.
    
    This improves the driver DMA code to the point where it can now identify a
    card and read the partition table.  However the DMA can still stall when
    trying to move data between the host and memory.
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>
    Cc: <linux-mmc@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/mmc/host/s3cmci.h
+++ drivers/mmc/host/s3cmci.h
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
-/* FIXME: DMA Resource management ?! */
-#define S3CMCI_DMA 0
-
 enum s3cmci_waitfor {
 	COMPLETION_NONE,
 	COMPLETION_FINALIZE,
