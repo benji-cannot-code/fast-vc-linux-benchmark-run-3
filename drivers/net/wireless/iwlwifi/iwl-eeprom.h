37d251450ca8050111d8c2f7c1b0054d3ef6f2c8
Johannes Berg <johannes@sipsolutions.net>
2009-10-02 13:44:02 -0700

    iwlwifi: device tracing
    In order to have an easier way to debug issues, create
    trace events (using the ftrace framework) that will
    allow us to follow exactly what the driver is doing
    with the device.
    
    The text format isn't all that useful, but the binary
    format can also be obtained easily via debugfs and
    then analysed on the fly or offline with debugging
    tools.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-eeprom.h
+++ drivers/net/wireless/iwlwifi/iwl-eeprom.h
@@ -64,6 +64,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_eeprom_h__
 #define __iwl_eeprom_h__
 
+#include <net/mac80211.h>
+
 struct iwl_priv;
 
 /*
