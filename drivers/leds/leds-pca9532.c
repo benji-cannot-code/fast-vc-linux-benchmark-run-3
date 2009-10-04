8d9c000662817c05df2648b2bb9d1fa5a7c84505
Jean Delvare <khali@linux-fr.org>
2009-10-04 22:53:43 +0200

    leds: leds-pca9532 - Drop unused module parameters
    The I2C_CLIENT_INSMOD_1 macro is only useful for i2c drivers which
    implement device detection. The leds-pca9532 driver doesn't, so there
    is no point in calling it.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Cc: Riku Voipio <riku.voipio@iki.fi>

--- drivers/leds/leds-pca9532.c
+++ drivers/leds/leds-pca9532.c
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/workqueue.h>
 #include <linux/leds-pca9532.h>
 
-static const unsigned short normal_i2c[] = { /*0x60,*/ I2C_CLIENT_END};
-I2C_CLIENT_INSMOD_1(pca9532);
-
 #define PCA9532_REG_PSC(i) (0x2+(i)*2)
 #define PCA9532_REG_PWM(i) (0x3+(i)*2)
 #define PCA9532_REG_LS0  0x6
