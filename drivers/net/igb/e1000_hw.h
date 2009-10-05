7266973b4c8bbca6c0187adce63c7c59032c613f
Alexander Duyck <alexander.h.duyck@intel.com>
2009-10-05 06:34:44 +0000

    igb: remove microwire support from igb
    igb doesn't have any devices that use a microwire interface for NVM.  As
    such the code related to this can be removed.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/igb/e1000_hw.h
+++ drivers/net/igb/e1000_hw.h
@@ -75,7 +75,6 @@ enum e1000_nvm_type {
 	e1000_nvm_unknown = 0,
 	e1000_nvm_none,
 	e1000_nvm_eeprom_spi,
-	e1000_nvm_eeprom_microwire,
 	e1000_nvm_flash_hw,
 	e1000_nvm_flash_sw
 };
@@ -84,8 +83,6 @@ enum e1000_nvm_override {
 	e1000_nvm_override_none = 0,
 	e1000_nvm_override_spi_small,
 	e1000_nvm_override_spi_large,
-	e1000_nvm_override_microwire_small,
-	e1000_nvm_override_microwire_large
 };
 
 enum e1000_phy_type {
