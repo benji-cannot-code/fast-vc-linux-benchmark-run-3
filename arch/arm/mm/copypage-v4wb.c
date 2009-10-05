6b6d97823b0b1b1dc26aa4271e92947bab255959
Russell King <rmk+kernel@arm.linux.org.uk>
2009-10-05 15:17:45 +0100

    ARM: Pass VMA to copy_user_highpage() implementations
    Our copy_user_highpage() implementations may require cache maintainence.
    Ensure that implementations have all necessary details to perform this
    maintainence.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/copypage-v4wb.c
+++ arch/arm/mm/copypage-v4wb.c
@@ -49,7 +49,7 @@ v4wb_copy_user_page(void *kto, const void *kfrom)
 }
 
 void v4wb_copy_user_highpage(struct page *to, struct page *from,
-	unsigned long vaddr)
+	unsigned long vaddr, struct vm_area_struct *vma)
 {
 	void *kto, *kfrom;
 
