83d6c92aa151641cb7e18b54279949399a54e81a
Albert Herranz <albert_herranz@yahoo.es>
2009-10-07 00:07:44 +0200

    b43: do not stack-allocate pio rx/tx header and tail buffers
    The DMA-API debugging facility complains about b43 mapping memory from
    stack for SDIO-based cards.
    
    Indeed, b43 currently allocates the PIO RX/TX header and tail buffers
    from stack. The solution here is to use heap-allocated buffers instead.
    
    Signed-off-by: Albert Herranz <albert_herranz@yahoo.es>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/xmit.c
+++ drivers/net/wireless/b43/xmit.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 */
 
-#include "xmit.h"
+#include "b43.h"
 #include "phy_common.h"
 #include "dma.h"
 #include "pio.h"
