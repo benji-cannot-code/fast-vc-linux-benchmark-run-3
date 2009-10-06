51f213693e5e7e33278857cf0c8f17adbcc2388d
Gabe Black <gabe.black@ni.com>
2009-10-06 10:45:19 -0500

    PCI: populate subsystem vendor and device IDs for PCI bridges
    Change to populate the subsystem vendor and subsytem device IDs for
    PCI-PCI bridges that implement the PCI Subsystem Vendor ID capability.
    Previously bridges left subsystem vendor IDs unpopulated.
    
    Signed-off-by: Gabe Black <gabe.black@ni.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- include/linux/pci_regs.h
+++ include/linux/pci_regs.h
@@ -366,6 +366,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  PCI_X_STATUS_266MHZ	0x40000000	/* 266 MHz capable */
 #define  PCI_X_STATUS_533MHZ	0x80000000	/* 533 MHz capable */
 
+/* PCI Bridge Subsystem ID registers */
+
+#define PCI_SSVID_VENDOR_ID     4	/* PCI-Bridge subsystem vendor id register */
+#define PCI_SSVID_DEVICE_ID     6	/* PCI-Bridge subsystem device id register */
+
 /* PCI Express capability registers */
 
 #define PCI_EXP_FLAGS		2	/* Capabilities register */
