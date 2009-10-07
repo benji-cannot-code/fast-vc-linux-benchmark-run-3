6077a96203b2fb23519841dea6f9fde743e201c5
Brian Haley <brian.haley@hp.com>
2009-10-07 13:57:10 -0700

    IPv6: Fix 6RD build error
    Fix build error introduced in commit fa857afcf - ipv6 sit: 6rd
    (IPv6 Rapid Deployment) Support.  Struct in6_addr is the issue.
    I'm only seeing this on x86_64 systems, not on 32-bit with same
    IPv6 config options, so it could be there's a missing forward
    declaration somewhere, but including the correct header file
    fixes the problem too.
    
      CC [M]  net/ipv6/ip6_tunnel.o
    In file included from net/ipv6/ip6_tunnel.c:31:
    include/linux/if_tunnel.h:59: error: field ‘prefix’ has incomplete type
    make[2]: *** [net/ipv6/ip6_tunnel.o] Error 1
    make[1]: *** [net/ipv6] Error 2
    
    Signed-off-by: Brian Haley <brian.haley@hp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_tunnel.h
+++ include/linux/if_tunnel.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __KERNEL__
 #include <linux/ip.h>
+#include <linux/in6.h>
 #endif
 
 #define SIOCGETTUNNEL   (SIOCDEVPRIVATE + 0)
