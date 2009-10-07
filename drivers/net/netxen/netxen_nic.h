ac9ca262a126e0e7a3ce175be41d8c48cebfb7e4
Ajit Khaparde <ajitkhaparde@gmail.com>
2009-10-07 02:45:34 +0000

    netxen: Use the instance of net_device_stats from net_device.
    Since net_device has an instance of net_device_stats,
    we can remove the instance of this from the private adapter structure.
    
    Signed-off-by: Ajit Khaparde <ajitk@serverengines.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/netxen/netxen_nic.h
+++ drivers/net/netxen/netxen_nic.h
@@ -1204,8 +1204,6 @@ struct netxen_adapter {
 
 	struct work_struct  tx_timeout_task;
 
-	struct net_device_stats net_stats;
-
 	nx_nic_intr_coalesce_t coal;
 
 	unsigned long state;
--- drivers/net/netxen/netxen_nic_main.c
+++ drivers/net/netxen/netxen_nic_main.c
@@ -1924,7 +1924,7 @@ request_reset:
 struct net_device_stats *netxen_nic_get_stats(struct net_device *netdev)
 {
 	struct netxen_adapter *adapter = netdev_priv(netdev);
-	struct net_device_stats *stats = &adapter->net_stats;
+	struct net_device_stats *stats = &netdev->stats;
 
 	memset(stats, 0, sizeof(*stats));
 
