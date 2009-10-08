46cb80953919b862f7f24b69fcdfc0af6e429895
Michal Simek <monstr@monstr.eu>
2009-10-08 13:06:42 +0200

    microblaze: Fix cache_line_lenght
    We used cache_line as cache_line_lenght. For this reason
    we did cache flushing 4 times longer than was necessary.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/cpuinfo.h
+++ arch/microblaze/include/asm/cpuinfo.h
@@ -44,7 +44,7 @@ struct cpuinfo {
 	u32 use_icache;
 	u32 icache_tagbits;
 	u32 icache_write;
-	u32 icache_line;
+	u32 icache_line_length;
 	u32 icache_size;
 	unsigned long icache_base;
 	unsigned long icache_high;
@@ -52,7 +52,7 @@ struct cpuinfo {
 	u32 use_dcache;
 	u32 dcache_tagbits;
 	u32 dcache_write;
-	u32 dcache_line;
+	u32 dcache_line_length;
 	u32 dcache_size;
 	unsigned long dcache_base;
 	unsigned long dcache_high;
