3a2d214f8bbd0f9debae5d4aa302055752c03969
Joerg Roedel <joerg.roedel@amd.com>
2009-10-09 16:08:32 +0200

    KVM: SVM: Add tracepoint for skinit instruction
    This patch adds a tracepoint for the event that the guest
    executed the SKINIT instruction. This information is
    important because SKINIT is an SVM extenstion not yet
    implemented by nested SVM and we may need this information
    for debugging hypervisors that do not yet run on nested SVM.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4990,3 +4990,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_nested_vmexit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_nested_vmexit_inject);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_nested_intr_vmexit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_invlpga);
+EXPORT_TRACEPOINT_SYMBOL_GPL(kvm_skinit);
