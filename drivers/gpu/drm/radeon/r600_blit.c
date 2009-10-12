ccbbc98512bb4f0a4b5f6e60f5ed400109109dd6
Dave Airlie <airlied@redhat.com>
2009-10-12 13:54:10 +1000

    drm/radeon: Revert "drm/r600: avoid assigning vb twice in blit code"
    This reverts commit 49c458e544ae14514209ed80ea6829ca1b18ddf0. It
    seems to have some side effects in the non-kms cases.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/r600_blit.c
+++ drivers/gpu/drm/radeon/r600_blit.c
@@ -583,6 +583,8 @@ r600_blit_copy(struct drm_device *dev,
 	u64 vb_addr;
 	u32 *vb;
 
+	vb = r600_nomm_get_vb_ptr(dev);
+
 	if ((size_bytes & 3) || (src_gpu_addr & 3) || (dst_gpu_addr & 3)) {
 		max_bytes = 8192;
 
@@ -618,8 +620,8 @@ r600_blit_copy(struct drm_device *dev,
 				if (!dev_priv->blit_vb)
 					return;
 				set_shaders(dev);
+				vb = r600_nomm_get_vb_ptr(dev);
 			}
-			vb = r600_nomm_get_vb_ptr(dev);
 
 			vb[0] = i2f(dst_x);
 			vb[1] = 0;
@@ -707,8 +709,8 @@ r600_blit_copy(struct drm_device *dev,
 					return;
 
 				set_shaders(dev);
+				vb = r600_nomm_get_vb_ptr(dev);
 			}
-			vb = r600_nomm_get_vb_ptr(dev);
 
 			vb[0] = i2f(dst_x / 4);
 			vb[1] = 0;
@@ -776,6 +778,8 @@ r600_blit_swap(struct drm_device *dev,
 	u64 vb_addr;
 	u32 *vb;
 
+	vb = r600_nomm_get_vb_ptr(dev);
+
 	if ((dev_priv->blit_vb->used + 48) > dev_priv->blit_vb->total) {
 
 		r600_nomm_put_vb(dev);
@@ -784,8 +788,8 @@ r600_blit_swap(struct drm_device *dev,
 			return;
 
 		set_shaders(dev);
+		vb = r600_nomm_get_vb_ptr(dev);
 	}
-	vb = r600_nomm_get_vb_ptr(dev);
 
 	if (cpp == 4) {
 		cb_format = COLOR_8_8_8_8;
