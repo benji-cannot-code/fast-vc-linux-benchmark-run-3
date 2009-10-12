44b47e60f3e9ba7bd486a952c6e6cb391068a19e
Juuso Oikarinen <juuso.oikarinen@nokia.com>
2009-10-12 15:08:50 +0300

    wl1271: Update interrupt handling by removing an extra SPI read
    Remove separate interrupt register reading from the interrupt handling
    routine. This will slightly improve interrupt performance.
    
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Reviewed-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271_reg.h
+++ drivers/net/wireless/wl12xx/wl1271_reg.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define REGISTERS_WORK_SIZE 0x0000b000
 
 #define HW_ACCESS_ELP_CTRL_REG_ADDR         0x1FFFC
-#define STATUS_MEM_ADDRESS                  0x40400
+#define FW_STATUS_ADDR                      (0x14FC0 + 0xA000)
 
 /* ELP register commands */
 #define ELPCTRL_WAKE_UP             0x1
