c2326c76a1050266114b3e433dca56023bdfea30
Vincent Legoll <vincent.legoll@gmail.com>
2009-10-13 10:18:16 +0200

    perf tools: Do not manually count string lengths
    Use strlen & macros instead of manually counting string lengths as
    this is error prone and may lend to bugs.
    
    Signed-off-by: Vincent Legoll <vincent.legoll@gmail.com>
    Cc: Linus Torvalds <torvalds@osdl.org>
    LKML-Reference: <4727185d0910130118m5387058dndb02ac9b384af9f0@mail.gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/cache.h
+++ tools/perf/util/cache.h
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "strbuf.h"
 #include "../perf.h"
 
+#define CMD_EXEC_PATH "--exec-path"
+#define CMD_PERF_DIR "--perf-dir="
+#define CMD_WORK_TREE "--work-tree="
+#define CMD_DEBUGFS_DIR "--debugfs-dir="
+
 #define PERF_DIR_ENVIRONMENT "PERF_DIR"
 #define PERF_WORK_TREE_ENVIRONMENT "PERF_WORK_TREE"
 #define DEFAULT_PERF_DIR_ENVIRONMENT ".perf"
