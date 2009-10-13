b37f02e20985834e7707f2160b17d3b742ac90b9
Paul Mundt <lethal@linux-sh.org>
2009-10-13 13:32:19 +0900

    sh: Tidy up the dwarf module helpers.
    This enables us to build the dwarf unwinder both with modules enabled and
    disabled in addition to reducing code size in the latter case. The
    helpers are also consolidated, and modified to resemble the BUG module
    helpers.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/dwarf.h
+++ arch/sh/include/asm/dwarf.h
@@ -199,6 +199,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/bug.h>
 #include <linux/list.h>
+#include <linux/module.h>
 
 /*
  * Read either the frame pointer (r14) or the stack pointer (r15).
@@ -383,8 +384,10 @@ static inline unsigned int DW_CFA_operand(unsigned long insn)
 extern struct dwarf_frame *dwarf_unwind_stack(unsigned long,
 					      struct dwarf_frame *);
 extern void dwarf_free_frame(struct dwarf_frame *);
-extern int dwarf_parse_section(char *, char *, struct module *);
-extern void dwarf_module_unload(struct module *);
+
+extern int module_dwarf_finalize(const Elf_Ehdr *, const Elf_Shdr *,
+				 struct module *);
+extern void module_dwarf_cleanup(struct module *);
 
 #endif /* !__ASSEMBLY__ */
 
@@ -413,6 +416,10 @@ extern void dwarf_module_unload(struct module *);
 static inline void dwarf_unwinder_init(void)
 {
 }
+
+#define module_dwarf_finalize(hdr, sechdrs, me)	(0)
+#define module_dwarf_cleanup(mod)		do { } while (0)
+
 #endif
 
 #endif /* CONFIG_DWARF_UNWINDER */
