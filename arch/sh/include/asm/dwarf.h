d101d61d3fbf661a216dcd46531de2e21166a6f3
Paul Mundt <lethal@linux-sh.org>
2009-10-13 13:10:14 +0900

    sh: Generalize CALLER_ADDRx support.
    This splits out the unwinder implementation and adds a new
    return_address() abstraction modelled after the ARM code. The DWARF
    unwinder is tied in to this, returning NULL otherwise in the case of
    being unable to support arbitrary depths.
    
    This enables us to get correct behaviour with the unwinder enabled,
    as well as disabling the arbitrary depth support when frame pointers are
    enabled, as arbitrary depths with __builtin_return_address() are not
    supported regardless.
    
    With this abstraction it's also possible to layer on a simplified
    implementation with frame pointers in the event that the unwinder isn't
    enabled, although this is left as a future exercise.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/dwarf.h
+++ arch/sh/include/asm/dwarf.h
@@ -195,6 +195,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DWARF_ARCH_RA_REG	17
 
 #ifndef __ASSEMBLY__
+
+#include <linux/compiler.h>
+#include <linux/bug.h>
+#include <linux/list.h>
+
 /*
  * Read either the frame pointer (r14) or the stack pointer (r15).
  * NOTE: this MUST be inlined.
