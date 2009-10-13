7b76ea01eeba56da568f360c20460ffe3e8d50bc
Amit Kumar Salecha <amit@netxen.com>
2009-10-13 05:31:41 +0000

    netxen: remove sub 64-bit mem accesses
    Sub 64-bit / unaligned access to oncard memory was only used
    by old diagnostic tools, causes some intermittent issues when
    memory controller agent is used.  The new access method was
    added by commit ea6828b8aa3a8ebae8d7740f32f212ba1d2f0742
    ("netxen: improve pci memory access").  Firmware init anyway
    uses 8-byte strides.
    
    This also fixes address/offset calculation for NX2031 context
    memory (SIU). For NX3031, SIU uses same register offsets
    as packet memory (MIU).
    
    Signed-off-by: Amit Kumar Salecha <amit@netxen.com>
    Signed-off-by: Dhananjay Phadke <dhananjay@netxen.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/netxen/netxen_nic_init.c
+++ drivers/net/netxen/netxen_nic_init.c
@@ -703,7 +703,10 @@ netxen_load_firmware(struct netxen_adapter *adapter)
 
 		for (i = 0; i < size; i++) {
 			data = cpu_to_le64(ptr64[i]);
-			adapter->pci_mem_write(adapter, flashaddr, &data, 8);
+			if (adapter->pci_mem_write(adapter,
+						flashaddr, data))
+				return -EIO;
+
 			flashaddr += 8;
 		}
 
@@ -717,7 +720,7 @@ netxen_load_firmware(struct netxen_adapter *adapter)
 			data = cpu_to_le64(ptr64[i]);
 
 			if (adapter->pci_mem_write(adapter,
-						flashaddr, &data, 8))
+						flashaddr, data))
 				return -EIO;
 
 			flashaddr += 8;
@@ -731,17 +734,17 @@ netxen_load_firmware(struct netxen_adapter *adapter)
 
 		for (i = 0; i < size; i++) {
 			if (netxen_rom_fast_read(adapter,
-					flashaddr, &lo) != 0)
+					flashaddr, (int *)&lo) != 0)
 				return -EIO;
 			if (netxen_rom_fast_read(adapter,
-					flashaddr + 4, &hi) != 0)
+					flashaddr + 4, (int *)&hi) != 0)
 				return -EIO;
 
 			/* hi, lo are already in host endian byteorder */
 			data = (((u64)hi << 32) | lo);
 
 			if (adapter->pci_mem_write(adapter,
-						flashaddr, &data, 8))
+						flashaddr, data))
 				return -EIO;
 
 			flashaddr += 8;
