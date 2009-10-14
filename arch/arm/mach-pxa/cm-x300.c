e5119eb8bf4d359c550986450de09313e0e5d9a5
Igor Grinberg <grinberg@compulab.co.il>
2009-10-14 09:20:20 +0200

    [ARM] pxa/cm-x300: add ac97 controller registration
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
    Signed-off-by: Mike Rapoport <mike@compulab.co.il>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-pxa/cm-x300.c
+++ arch/arm/mach-pxa/cm-x300.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/ohci.h>
 #include <plat/i2c.h>
 #include <plat/pxa3xx_nand.h>
+#include <mach/audio.h>
 
 #include <asm/mach/map.h>
 
@@ -306,6 +307,15 @@ static void __init cm_x300_init_spi(void)
 static inline void cm_x300_init_spi(void) {}
 #endif
 
+#if defined(CONFIG_SND_PXA2XX_LIB_AC97)
+static void __init cm_x300_init_ac97(void)
+{
+	pxa_set_ac97_info(NULL);
+}
+#else
+static inline void cm_x300_init_ac97(void) {}
+#endif
+
 #if defined(CONFIG_MTD_NAND_PXA3xx) || defined(CONFIG_MTD_NAND_PXA3xx_MODULE)
 static struct mtd_partition cm_x300_nand_partitions[] = {
 	[0] = {
@@ -532,6 +542,7 @@ static void __init cm_x300_init(void)
 	cm_x300_init_i2c();
 	cm_x300_init_spi();
 	cm_x300_init_rtc();
+	cm_x300_init_ac97();
 }
 
 static void __init cm_x300_fixup(struct machine_desc *mdesc, struct tag *tags,
