32b025c0dd3b355c9ede66d96bd0203e12fc3f97
Steven Rostedt <srostedt@redhat.com>
2009-10-14 15:43:41 -0400

    perf tools: Handle both versions of ftrace output
    The ftrace output events can have either arguments or no
    arguments. The parser needs to be able to handle both.
    
    Signed-off-by: Steven Rostedt <srostedt@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    LKML-Reference: <20091014194359.790221427@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/trace-event-parse.c
+++ tools/perf/util/trace-event-parse.c
@@ -1820,7 +1820,7 @@ static int event_read_print(struct event *event)
 	if (ret < 0)
 		return -1;
 
-	return 0;
+	return ret;
 
  fail:
 	free_token(token);
@@ -3089,6 +3089,9 @@ int parse_ftrace_file(char *buf, unsigned long size)
 	if (ret < 0)
 		die("failed to read ftrace event print fmt");
 
+	/* New ftrace handles args */
+	if (ret > 0)
+		return 0;
 	/*
 	 * The arguments for ftrace files are parsed by the fields.
 	 * Set up the fields as their arguments.
