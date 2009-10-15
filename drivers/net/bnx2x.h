f161a478a3b71d2fd9b81a12b0054cd0085a95f2
Eilon Greenstein <eilong@broadcom.com>
2009-10-15 00:18:27 -0700

    bnx2x: Adding FW mailbox mutex
    DCC commands are not protected with the RTNL lock, so a mutex should be added
    
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x.h
+++ drivers/net/bnx2x.h
@@ -1024,6 +1024,9 @@ struct bnx2x {
 	/* used to synchronize dmae accesses */
 	struct mutex		dmae_mutex;
 
+	/* used to protect the FW mail box */
+	struct mutex		fw_mb_mutex;
+
 	/* used to synchronize stats collecting */
 	int			stats_state;
 	/* used by dmae command loader */
