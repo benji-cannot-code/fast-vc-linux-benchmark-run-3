79de1ca9ad2c0e10251fb3c97adaa8117e6ff535
Holger Schurig <hs4233@mail.mn-solutions.de>
2009-10-16 17:33:23 +0200

    libertas: harmonize cmd.h
    * move declarations for functions of cmd.c/cmdresp.c into cmd.h
    * move declarations from cmd.h that are in main.c to decl.h
    * group command functions
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Acked-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/cmdresp.c
+++ drivers/net/wireless/libertas/cmdresp.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "host.h"
 #include "decl.h"
+#include "cmd.h"
 #include "defs.h"
 #include "dev.h"
 #include "assoc.h"
