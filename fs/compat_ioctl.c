c65bd61798e73e20a91d70a15e529c46805635e1
Arnd Bergmann <arnd@arndb.de>
2009-10-16 02:25:25 +0200

    compat_ioctl: Remove BKL
    We have always called ioctl conversion handlers under the big kernel lock,
    although that is generally not necessary.  In particular it is not needed
    for conversion of data structures and for calling sys_ioctl or
    do_vfs_ioctl, which will get the BKL again if needed.
    
    Handlers doing more than those two have been moved out, so we can kill off
    the BKL from compat_sys_ioctl.  This may significantly improve latencies
    with 32 bit applications, and it avoids a common scenario where a thread
    acquires the BKL twice.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -1919,9 +1919,7 @@ asmlinkage long compat_sys_ioctl(unsigned int fd, unsigned int cmd,
 
  found_handler:
 	if (t->handler) {
-		lock_kernel();
 		error = t->handler(fd, cmd, arg, filp);
-		unlock_kernel();
 		goto out_fput;
 	}
 
