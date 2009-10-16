badc549a707d22c833f5979907e71e5a2f148d99
Wu Zhangjin <wuzhangjin@gmail.com>
2009-10-16 14:17:19 +0800

    MIPS: Bonito64: Make Loongson independent from Bonito64 code.
    The built-in Loongson 2E/2F northbridge in is bonito64-compatible but not
    identical with it.  To avoid influencing the original bonito64 support and
    make the loongson support more maintainable, it's better to separate the
    Bonito64 code from the Loongson code.
    
    This also prepares the kernel for the coming Loongson 2f machines family
    support.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>
    Cc: Linux-MIPS <linux-mips@linux-mips.org>
    Cc: yanh@lemote.com
    Cc: huhb@lemote.com
    Cc: Zhang Le <r0bertz@gentoo.org>
    Cc: zhangfx@lemote.com,
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/loongson/common/bonito-irq.c
+++ arch/mips/loongson/common/bonito-irq.c
@@ -18,13 +18,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline void bonito_irq_enable(unsigned int irq)
 {
-	BONITO_INTENSET = (1 << (irq - BONITO_IRQ_BASE));
+	LOONGSON_INTENSET = (1 << (irq - LOONGSON_IRQ_BASE));
 	mmiowb();
 }
 
 static inline void bonito_irq_disable(unsigned int irq)
 {
-	BONITO_INTENCLR = (1 << (irq - BONITO_IRQ_BASE));
+	LOONGSON_INTENCLR = (1 << (irq - LOONGSON_IRQ_BASE));
 	mmiowb();
 }
 
@@ -45,8 +45,8 @@ void bonito_irq_init(void)
 {
 	u32 i;
 
-	for (i = BONITO_IRQ_BASE; i < BONITO_IRQ_BASE + 32; i++)
+	for (i = LOONGSON_IRQ_BASE; i < LOONGSON_IRQ_BASE + 32; i++)
 		set_irq_chip_and_handler(i, &bonito_irq_type, handle_level_irq);
 
-	setup_irq(BONITO_IRQ_BASE + 10, &dma_timeout_irqaction);
+	setup_irq(LOONGSON_IRQ_BASE + 10, &dma_timeout_irqaction);
 }
