772758b97ad1aca8f0bf2c001ba1aee3bc18c262
Kumar Gala <galak@kernel.crashing.org>
2009-10-16 18:48:40 -0500

    powerpc/fsl-booke64: Use TLB CAMs to cover linear mapping on FSL 64-bit chips
    On Freescale parts typically have TLB array for large mappings that we can
    bolt the linear mapping into.  We utilize the code that already exists
    on PPC32 on the 64-bit side to setup the linear mapping to be cover by
    bolted TLB entries.  We utilize a quarter of the variable size TLB array
    for this purpose.
    
    Additionally, we limit the amount of memory to what we can cover via
    bolted entries so we don't get secondary faults in the TLB miss
    handlers.  We should fix this limitation in the future.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/mm/fsl_booke_mmu.c
+++ arch/powerpc/mm/fsl_booke_mmu.c
@@ -57,11 +57,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 unsigned int tlbcam_index;
 
-
-#if defined(CONFIG_LOWMEM_CAM_NUM_BOOL) && (CONFIG_LOWMEM_CAM_NUM >= NUM_TLBCAMS)
-#error "LOWMEM_CAM_NUM must be less than NUM_TLBCAMS"
-#endif
-
 #define NUM_TLBCAMS	(64)
 struct tlbcam TLBCAM[NUM_TLBCAMS];
 
@@ -186,6 +181,12 @@ unsigned long map_mem_in_cams(unsigned long ram, int max_cam_idx)
 	return amount_mapped;
 }
 
+#ifdef CONFIG_PPC32
+
+#if defined(CONFIG_LOWMEM_CAM_NUM_BOOL) && (CONFIG_LOWMEM_CAM_NUM >= NUM_TLBCAMS)
+#error "LOWMEM_CAM_NUM must be less than NUM_TLBCAMS"
+#endif
+
 unsigned long __init mmu_mapin_ram(unsigned long top)
 {
 	return tlbcam_addrs[tlbcam_index - 1].limit - PAGE_OFFSET + 1;
@@ -217,3 +218,4 @@ void __init adjust_total_lowmem(void)
 
 	__initial_memory_limit_addr = memstart_addr + __max_low_memory;
 }
+#endif
