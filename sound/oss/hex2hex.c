c1020deb09470cc71e212c7eb99b567338d5d52f
Josh Triplett <josh@joshtriplett.org>
2009-10-16 16:03:49 -0700

    oss: Mark loadhex static in hex2hex.c
    Nothing outside of hex2hex.c references loadhex.
    
    Signed-off-by: Josh Triplett <josh@joshtriplett.org>

--- sound/oss/hex2hex.c
+++ sound/oss/hex2hex.c
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_SIZE (256*1024)
 unsigned char buf[MAX_SIZE];
 
-int loadhex(FILE *inf, unsigned char *buf)
+static int loadhex(FILE *inf, unsigned char *buf)
 {
 	int l=0, c, i;
 
