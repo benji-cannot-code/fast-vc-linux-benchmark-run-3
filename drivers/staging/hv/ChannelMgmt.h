891f0f4f85c52f15cc9062c22f0e591b904ce252
Hank Janssen <hjanssen@microsoft.com>
2009-10-16 20:11:36 +0000

    Staging: hv: Fix vmbus load hang caused by faulty data packing
    Fix vmbus load hang caused by wrong data packing.
    
    Signed-off-by: Hank Janssen<hjanssen@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/ChannelMgmt.h
+++ drivers/staging/hv/ChannelMgmt.h
@@ -55,7 +55,7 @@ enum vmbus_channel_message_type {
 	ChannelMessageViewRangeRemove		= 18,
 #endif
 	ChannelMessageCount
-} __attribute__((packed));
+};
 
 struct vmbus_channel_message_header {
 	enum vmbus_channel_message_type MessageType;
