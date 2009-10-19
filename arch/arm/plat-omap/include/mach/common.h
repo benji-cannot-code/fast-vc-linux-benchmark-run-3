c9169de83cb526af10fb3de2e5fbe74dee0d5d70
Tony Lindgren <tony@atomide.com>
2009-10-19 15:25:26 -0700

    omap: Use ioremap for omap4 L4 code
    Use ioremap for omap4 L4 code
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/mach/common.h
+++ arch/arm/plat-omap/include/mach/common.h
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct sys_timer;
 
+/* used by omap-smp.c and board-4430sdp.c */
+extern void __iomem *gic_cpu_base_addr;
+
 extern void omap_map_common_io(void);
 extern struct sys_timer omap_timer;
 #if defined(CONFIG_I2C_OMAP) || defined(CONFIG_I2C_OMAP_MODULE)
