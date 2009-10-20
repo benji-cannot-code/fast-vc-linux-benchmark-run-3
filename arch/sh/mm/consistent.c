382a8270a0c05a4fe532cd51a9c3308dfd5ed920
Paul Mundt <lethal@linux-sh.org>
2009-10-20 12:55:56 +0900

    sh: Convert to asm-generic/dma-mapping-common.h
    This converts the old DMA mapping support to the new generic
    dma-mapping-common.h abstraction.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/consistent.c
+++ arch/sh/mm/consistent.c
@@ -16,14 +16,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 #include <linux/dma-debug.h>
 #include <linux/io.h>
+#include <linux/module.h>
 #include <asm/cacheflush.h>
 #include <asm/addrspace.h>
 
 #define PREALLOC_DMA_DEBUG_ENTRIES	4096
 
+struct dma_map_ops *dma_ops;
+EXPORT_SYMBOL(dma_ops);
+
 static int __init dma_init(void)
 {
 	dma_debug_init(PREALLOC_DMA_DEBUG_ENTRIES);
+
+	no_iommu_init();
 	return 0;
 }
 fs_initcall(dma_init);
