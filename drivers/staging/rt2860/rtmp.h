f509a606f0cec6a8b52785db2012256bf08e377f
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-10-21 22:44:27 +0200

    Staging: rt28x0: remove support for private driver parameters
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2860/rtmp.h
+++ drivers/staging/rt2860/rtmp.h
@@ -2454,9 +2454,6 @@ NDIS_STATUS RTMPAllocAdapterBlock(
 NDIS_STATUS RTMPAllocTxRxRingMemory(
 	IN  PRTMP_ADAPTER   pAd);
 
-NDIS_STATUS	RTMPReadParametersHook(
-	IN	PRTMP_ADAPTER pAd);
-
 NDIS_STATUS	RTMPSetProfileParameters(
 	IN RTMP_ADAPTER *pAd,
 	IN PSTRING		pBuffer);
