5d60e832b88242fd1f0ace99cb49bc40d0ff4f66
Klaas van Gend <klaas.van.gend@mvista.com>
2009-10-22 07:51:00 +0200

    staging: comedi: multiq3: add KERN_
    Add KERN_ to printk statements to reduce the number of warnings
    shown by checkpatch.pl.
    
    Signed-off-by: Klaas van Gend <klaas.van.gend@mvista.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/multiq3.c
+++ drivers/staging/comedi/drivers/multiq3.c
@@ -253,9 +253,9 @@ static int multiq3_attach(struct comedi_device *dev,
 	struct comedi_subdevice *s;
 
 	iobase = it->options[0];
-	printk("comedi%d: multiq3: 0x%04lx ", dev->minor, iobase);
+	printk(KERN_INFO "comedi%d: multiq3: 0x%04lx ", dev->minor, iobase);
 	if (!request_region(iobase, MULTIQ3_SIZE, "multiq3")) {
-		printk("comedi%d: I/O port conflict\n", dev->minor);
+		printk(KERN_ERR "comedi%d: I/O port conflict\n", dev->minor);
 		return -EIO;
 	}
 
@@ -263,9 +263,10 @@ static int multiq3_attach(struct comedi_device *dev,
 
 	irq = it->options[1];
 	if (irq)
-		printk("comedi%d: irq = %u ignored\n", dev->minor, irq);
+		printk(KERN_WARNING "comedi%d: irq = %u ignored\n",
+			dev->minor, irq);
 	else
-		printk("comedi%d: no irq\n", dev->minor);
+		printk(KERN_WARNING "comedi%d: no irq\n", dev->minor);
 	dev->board_name = "multiq3";
 	result = alloc_subdevices(dev, 5);
 	if (result < 0)
@@ -329,7 +330,7 @@ static int multiq3_attach(struct comedi_device *dev,
 
 static int multiq3_detach(struct comedi_device *dev)
 {
-	printk("comedi%d: multiq3: remove\n", dev->minor);
+	printk(KERN_INFO "comedi%d: multiq3: remove\n", dev->minor);
 
 	if (dev->iobase)
 		release_region(dev->iobase, MULTIQ3_SIZE);
