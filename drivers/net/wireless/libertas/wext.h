e171cf1e1e6440ce7672a24d2b8939d7c5147e5f
Holger Schurig <hs4233@mail.mn-solutions.de>
2009-10-22 15:30:57 +0200

    libertas: move mic failure event to wext.c
    ... because for cfg80211 we'll need a completely different
    implementation.
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/wext.h
+++ drivers/net/wireless/libertas/wext.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	_LBS_WEXT_H_
 
 void lbs_send_disconnect_notification(struct lbs_private *priv);
-void lbs_send_iwevcustom_event(struct lbs_private *priv, s8 *str);
+void lbs_send_mic_failureevent(struct lbs_private *priv, u32 event);
 
 extern struct iw_handler_def lbs_handler_def;
 extern struct iw_handler_def mesh_handler_def;
