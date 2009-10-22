74dfa53b960827b5dfe3fd3ddeb3c6dd339246c3
Holger Schurig <hs4233@mail.mn-solutions.de>
2009-10-22 15:30:52 +0200

    libertas: move lbs_send_iwevcustom_event() to wext.c
    ... because it's purely a WEXT function.
    
    No functional change.
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/wext.h
+++ drivers/net/wireless/libertas/wext.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	_LBS_WEXT_H_
 #define	_LBS_WEXT_H_
 
+void lbs_send_iwevcustom_event(struct lbs_private *priv, s8 *str);
+
 extern struct iw_handler_def lbs_handler_def;
 extern struct iw_handler_def mesh_handler_def;
 
