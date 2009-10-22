0c31c51c2ba383e64f2bf3dae2cd50b510f4b2ec
Pavel Machek <pavel@ucw.cz>
2009-10-22 22:16:34 +0200

    [ARM] pxa/zaurus: rename spitz_battery_levels_* to sharpsl_*
    Battery power levels are shared between spitz and corgi, rename
    variable to reflect it.
    
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-pxa/sharpsl_pm.c
+++ arch/arm/mach-pxa/sharpsl_pm.c
@@ -79,7 +79,7 @@ DEFINE_LED_TRIGGER(sharpsl_charge_led_trigger);
 
 
 
-struct battery_thresh spitz_battery_levels_acin[] = {
+struct battery_thresh sharpsl_battery_levels_acin[] = {
 	{ 213, 100},
 	{ 212,  98},
 	{ 211,  95},
@@ -122,7 +122,7 @@ struct battery_thresh spitz_battery_levels_acin[] = {
 	{   0,   0},
 };
 
-struct battery_thresh  spitz_battery_levels_noac[] = {
+struct battery_thresh sharpsl_battery_levels_noac[] = {
 	{ 213, 100},
 	{ 212,  98},
 	{ 211,  95},
