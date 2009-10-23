2a83c6a7adf155332611b464e78dd8fc2f34b659
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2009-10-23 13:42:33 -0700

    iwlwifi: remove duplicated define
    Duplicated define for listen interval (IWL_CONN_MAX_LISTEN_INTERVAL and
    IWL_CONN_LISTEN_INTERVAL), remove IWL_CONN_LISTEN_INTERVAL
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Reported-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-power.h
+++ drivers/net/wireless/iwlwifi/iwl-power.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "iwl-commands.h"
 
-#define IWL_CONN_LISTEN_INTERVAL	10
-
 #define IWL_ABSOLUTE_ZERO		0
 #define IWL_ABSOLUTE_MAX		0xFFFFFFFF
 #define IWL_TT_INCREASE_MARGIN	5
