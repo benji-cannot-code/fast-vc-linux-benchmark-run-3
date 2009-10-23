ce1590c105a606d5656d0e8c96050b4b11e26db4
Ben Hutchings <bhutchings@solarflare.com>
2009-10-23 08:28:45 +0000

    sfc: Remove redundant header gmii.h
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/falcon_gmac.c
+++ drivers/net/sfc/falcon_gmac.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mac.h"
 #include "falcon_hwdefs.h"
 #include "falcon_io.h"
-#include "gmii.h"
 
 /**************************************************************************
  *
