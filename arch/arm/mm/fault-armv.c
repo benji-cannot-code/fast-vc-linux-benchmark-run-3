72ee59a331d1ab73bc2f1bf432ef4eb18829ce7a
Russell King <rmk+kernel@arm.linux.org.uk>
2009-10-24 14:11:59 +0100

    ARM: Reduce __flush_dcache_page() visibility
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/fault-armv.c
+++ arch/arm/mm/fault-armv.c
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
 
+#include "mm.h"
+
 static unsigned long shared_pte_mask = L_PTE_MT_BUFFERABLE;
 
 /*
