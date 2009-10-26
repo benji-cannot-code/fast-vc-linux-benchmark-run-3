ba2ddf3769eba2749d3fed3cf63f8d66bedca29a
Shawn Bohrer <shawn.bohrer@gmail.com>
2009-10-25 22:46:48 -0500

    Staging: vt6656 remove duplicate includes
    Signed-off-by: Shawn Bohrer <shawn.bohrer@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6656/wcmd.c
+++ drivers/staging/vt6656/wcmd.c
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wmgr.h"
 #include "power.h"
 #include "wctl.h"
-#include "card.h"
 #include "baseband.h"
 #include "control.h"
 #include "rxtx.h"
