ba527ac9c489a88b185bb8ba760d1091435bff23
Shawn Bohrer <shawn.bohrer@gmail.com>
2009-10-25 22:46:45 -0500

    Staging: vt6655 remove kcompat.h
    The vt6655 driver is integrated in the kernel so it no longer needs the
    compatibility header.
    
    Signed-off-by: Shawn Bohrer <shawn.bohrer@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6655/device.h
+++ drivers/staging/vt6655/device.h
@@ -80,7 +80,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // device specific
 //
 
-#include "kcompat.h"
 #include "device_cfg.h"
 #include "ttype.h"
 #include "80211hdr.h"
