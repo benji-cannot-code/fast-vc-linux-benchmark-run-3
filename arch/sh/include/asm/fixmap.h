47494c225d047d9e1e0745623739e5f89138451c
Stuart Menefy <stuart.menefy@st.com>
2009-10-27 15:14:06 +0000

    sh: Improve performance of SH4 versions of copy/clear_user_highpage
    The previous implementation of clear_user_highpage and copy_user_highpage
    checked to see if there was a D-cache aliasing issue between the user
    and kernel mappings of a page, but if there was they always did a
    flush with writeback on the dirtied kernel alias.
    
    However as we now have the ability to map a page into kernel space
    with the same cache colour as the user mapping, there is no need to
    write back this data.
    
    Currently we also invalidate the kernel alias as a precaution, however
    I'm not sure if this is actually required.
    
    Also correct the definition of FIX_CMAP_END so that the mappings created
    by kmap_coherent() are actually at the correct colour.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/fixmap.h
+++ arch/sh/include/asm/fixmap.h
@@ -47,9 +47,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * fix-mapped?
  */
 enum fixed_addresses {
+	/*
+	 * The FIX_CMAP entries are used by kmap_coherent() to get virtual
+	 * addresses which are of a known color, and so their values are
+	 * important. __fix_to_virt(FIX_CMAP_END - n) must give an address
+	 * which is the same color as a page (n<<PAGE_SHIFT).
+	 */
 #define FIX_N_COLOURS 8
 	FIX_CMAP_BEGIN,
-	FIX_CMAP_END = FIX_CMAP_BEGIN + (FIX_N_COLOURS * NR_CPUS),
+	FIX_CMAP_END = FIX_CMAP_BEGIN + (FIX_N_COLOURS * NR_CPUS) - 1,
 	FIX_UNCACHED,
 #ifdef CONFIG_HIGHMEM
 	FIX_KMAP_BEGIN,	/* reserved pte's for temporary kernel mappings */
