48ab481932180cf55a686fbf29de2dc7df5e72d2
Eric Dumazet <eric.dumazet@gmail.com>
2009-10-29 00:10:37 +0000

    net: fix kmemcheck annotations
    struct sk_buff kmemcheck annotations enlarged this structure by 8/16 bytes
    
    Fix this by moving 'protocol' inside flags1 bitfield,
    and queue_mapping inside flags2 bitfield.
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -355,8 +355,8 @@ struct sk_buff {
 				ipvs_property:1,
 				peeked:1,
 				nf_trace:1;
+	__be16			protocol:16;
 	kmemcheck_bitfield_end(flags1);
-	__be16			protocol;
 
 	void			(*destructor)(struct sk_buff *skb);
 #if defined(CONFIG_NF_CONNTRACK) || defined(CONFIG_NF_CONNTRACK_MODULE)
@@ -368,7 +368,6 @@ struct sk_buff {
 #endif
 
 	int			iif;
-	__u16			queue_mapping;
 #ifdef CONFIG_NET_SCHED
 	__u16			tc_index;	/* traffic control index */
 #ifdef CONFIG_NET_CLS_ACT
@@ -377,6 +376,7 @@ struct sk_buff {
 #endif
 
 	kmemcheck_bitfield_begin(flags2);
+	__u16			queue_mapping:16;
 #ifdef CONFIG_IPV6_NDISC_NODETYPE
 	__u8			ndisc_nodetype:2;
 #endif
