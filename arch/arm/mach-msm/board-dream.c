bc8968d8997c9881909a9ceed74116504a788413
Pavel Machek <pavel@ucw.cz>
2009-11-02 11:55:12 +0100

    msm: Add memory map for HTC Dream
    Add memory map to HTC Dream, so that boot can proceed further.
    
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>

--- arch/arm/mach-msm/board-dream.c
+++ arch/arm/mach-msm/board-dream.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
+#include <asm/setup.h>
 
 #include <mach/board.h>
 #include <mach/hardware.h>
@@ -43,6 +44,15 @@ extern struct sys_timer msm_timer;
 static void __init trout_init_irq(void)
 {
 	msm_init_irq();
+}
+
+static void __init trout_fixup(struct machine_desc *desc, struct tag *tags,
+				char **cmdline, struct meminfo *mi)
+{
+	mi->nr_banks = 1;
+	mi->bank[0].start = PHYS_OFFSET;
+	mi->bank[0].node = PHYS_TO_NID(PHYS_OFFSET);
+	mi->bank[0].size = (101*1024*1024);
 }
 
 static void __init trout_init(void)
@@ -76,6 +86,7 @@ MACHINE_START(TROUT, "HTC Dream")
 	.phys_io	= MSM_DEBUG_UART_PHYS,
 	.io_pg_offst	= ((MSM_DEBUG_UART_BASE) >> 18) & 0xfffc,
 	.boot_params	= 0x10000100,
+	.fixup		= trout_fixup,
 	.map_io		= trout_map_io,
 	.init_irq	= trout_init_irq,
 	.init_machine	= trout_init,
