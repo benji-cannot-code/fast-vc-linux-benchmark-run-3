6b0ba4d7118d0f01bdf993f8201c87570c5f18e3
Jean-Francois Moine <moinejf@free.fr>
2009-11-02 09:58:49 -0300

    V4L/DVB (13305): gspca - sonixj: The V flip control does not exist for all sensors.
    Signed-off-by: Jean-Francois Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/sonixj.c
+++ drivers/media/video/gspca/sonixj.c
@@ -1550,6 +1550,8 @@ static void setvflip(struct sd *sd)
 {
 	u8 comn;
 
+	if (sd->gspca_dev.ctrl_dis & (1 << VFLIP_IDX))
+		return;
 	if (sd->sensor == SENSOR_OV7630) {
 		comn = 0x02;
 		if (!sd->vflip)
