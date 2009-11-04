b8b2dda7b3d1e2c093b3e30cc082bb95698d0565
Catalin Marinas <catalin.marinas@arm.com>
2009-11-04 12:19:05 +0000

    RealView: Add sparsemem support for the RealView PBX platform
    The RealView PBX board has two 512MB blocks of memory - one at
    0x70000000 (with 256MB mirror at 0) and another at 0x20000000. Only the
    block at 0x70000000 (or the mirror at 0) may be used for DMA (e.g.
    framebuffer). This patch adds the sparsemem definitions to allow the use
    of all the memory split as follows:
    
      256MB @ 0x00000000 (ZONE_DMA)
      512MB @ 0x20000000 (ZONE_NORMAL)
      256MB @ 0x80000000 (ZONE_NORMAL)
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/mach-realview/core.c
+++ arch/arm/mach-realview/core.c
@@ -60,6 +60,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* used by entry-macro.S and platsmp.c */
 void __iomem *gic_cpu_base_addr;
 
+#ifdef CONFIG_ZONE_DMA
+/*
+ * Adjust the zones if there are restrictions for DMA access.
+ */
+void __init realview_adjust_zones(int node, unsigned long *size,
+				  unsigned long *hole)
+{
+	unsigned long dma_size = SZ_256M >> PAGE_SHIFT;
+
+	if (!machine_is_realview_pbx() || node || (size[0] <= dma_size))
+		return;
+
+	size[ZONE_NORMAL] = size[0] - dma_size;
+	size[ZONE_DMA] = dma_size;
+	hole[ZONE_NORMAL] = hole[0];
+	hole[ZONE_DMA] = 0;
+}
+#endif
+
 /*
  * This is the RealView sched_clock implementation.  This has
  * a resolution of 41.7ns, and a maximum value of about 179s.
@@ -544,7 +563,7 @@ static int realview_clcd_setup(struct clcd_fb *fb)
 	fb->panel		= realview_clcd_panel();
 
 	fb->fb.screen_base = dma_alloc_writecombine(&fb->dev->dev, framesize,
-						    &dma, GFP_KERNEL);
+						    &dma, GFP_KERNEL | GFP_DMA);
 	if (!fb->fb.screen_base) {
 		printk(KERN_ERR "CLCD: unable to map framebuffer\n");
 		return -ENOMEM;
