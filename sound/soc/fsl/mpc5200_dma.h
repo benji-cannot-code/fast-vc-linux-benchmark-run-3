76b7dcec75d44a35842b0e905d9df09cfd76903f
Grant Likely <grant.likely@secretlab.ca>
2009-11-07 01:34:05 -0700

    ASoC/mpc5200: get rid of the appl_ptr tracking nonsense
    Sound drivers PCM DMA is supposed to free-run until told to stop
    by the trigger callback.  The current code tries to track appl_ptr,
    to avoid stale buffer data getting played out at the end of the
    data stream.  Unfortunately it also results in race conditions
    which can cause the audio to stall.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/fsl/mpc5200_dma.h
+++ sound/soc/fsl/mpc5200_dma.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct psc_dma_stream {
 	struct snd_pcm_runtime *runtime;
-	snd_pcm_uframes_t appl_ptr;
-
 	int active;
 	struct psc_dma *psc_dma;
 	struct bcom_task *bcom_task;
