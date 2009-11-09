7611068782bc07d389380d2e9244d878bd31f523
Rui Paulo <rpaulo@gmail.com>
2009-11-09 23:46:41 +0000

    mac80211: implement the meshconf formation info field
    The Mesh Configuration Formation Info field contains the number of
    neighbors.  This means that the beacon must be updated every time a
    peer joins or leaves.
    
    Signed-off-by: Rui Paulo <rpaulo@gmail.com>
    Signed-off-by: Javier Cardona <rpaulo@gmail.com>
    Reviewed-by: Andrey Yurovsky <andrey@cozybit.com>
    Tested-by: Brian Cavagnolo <brian@cozybit.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/mesh_plink.c
+++ net/mac80211/mesh_plink.c
@@ -66,6 +66,7 @@ void mesh_plink_inc_estab_count(struct ieee80211_sub_if_data *sdata)
 {
 	atomic_inc(&sdata->u.mesh.mshstats.estab_plinks);
 	mesh_accept_plinks_update(sdata);
+	ieee80211_bss_info_change_notify(sdata, BSS_CHANGED_BEACON);
 }
 
 static inline
@@ -73,6 +74,7 @@ void mesh_plink_dec_estab_count(struct ieee80211_sub_if_data *sdata)
 {
 	atomic_dec(&sdata->u.mesh.mshstats.estab_plinks);
 	mesh_accept_plinks_update(sdata);
+	ieee80211_bss_info_change_notify(sdata, BSS_CHANGED_BEACON);
 }
 
 /**
