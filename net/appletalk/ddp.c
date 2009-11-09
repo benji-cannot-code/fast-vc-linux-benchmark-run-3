d4285243fac09263f36d76e67732f8873704f1e8
Stephen Rothwell <sfr@canb.auug.org.au>
2009-11-08 20:41:03 -0800

    net/appletalk: using compat_ptr needs inclusion of linux/compat.h
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/appletalk/ddp.c
+++ net/appletalk/ddp.c
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 #include <linux/smp_lock.h>
 #include <linux/termios.h>	/* For TIOCOUTQ/INQ */
+#include <linux/compat.h>
 #include <net/datalink.h>
 #include <net/psnap.h>
 #include <net/sock.h>
