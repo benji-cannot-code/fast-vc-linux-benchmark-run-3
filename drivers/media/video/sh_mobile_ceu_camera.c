2084a38321f10942843abb2004a7f75aea750a51
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2009-11-09 16:11:34 -0300

    V4L/DVB (13343): v4l: add more missing linux/sched.h includes
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/sh_mobile_ceu_camera.c
+++ drivers/media/video/sh_mobile_ceu_camera.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/videodev2.h>
 #include <linux/pm_runtime.h>
+#include <linux/sched.h>
 
 #include <media/v4l2-common.h>
 #include <media/v4l2-dev.h>
