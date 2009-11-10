94432d391ca78edb3a216724795c69929e746092
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-11-10 19:46:19 +0900

    swiotlb: Defer swiotlb init printing, export swiotlb_print_info()
    This enables us to avoid printing swiotlb memory info when we
    initialize swiotlb. After swiotlb initialization, we could find
    that we don't need swiotlb.
    
    This patch removes the code to print swiotlb memory info in
    swiotlb_init() and exports the function to do that.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Cc: chrisw@sous-sol.org
    Cc: dwmw2@infradead.org
    Cc: joerg.roedel@amd.com
    Cc: muli@il.ibm.com
    Cc: tony.luck@intel.com
    Cc: benh@kernel.crashing.org
    LKML-Reference: <1257849980-22640-9-git-send-email-fujita.tomonori@lab.ntt.co.jp>
    [ -v2: merge up conflict ]
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/swiotlb.h
+++ include/linux/swiotlb.h
@@ -21,8 +21,7 @@ struct scatterlist;
  */
 #define IO_TLB_SHIFT 11
 
-extern void
-swiotlb_init(void);
+extern void swiotlb_init(int verbose);
 
 extern void
 *swiotlb_alloc_coherent(struct device *hwdev, size_t size,
@@ -95,4 +94,5 @@ extern void __init swiotlb_free(void);
 static inline void swiotlb_free(void) { }
 #endif
 
+extern void swiotlb_print_info(void);
 #endif /* __LINUX_SWIOTLB_H */
