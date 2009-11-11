8ab71f994a66e25c7fcb85bd0f535516d6959d8b
Ben Dooks <ben@simtec.co.uk>
2009-11-10 21:01:31 -0800

    Input: gpio_keys - use <linux/gpio.h> instead of <asm/gpio.h>
    The gpio keys driver should be using <linux/gpio.h> instead
    of <asm/gpio.h>
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>
    Signed-off-by: Simtec Linux Team <linux@simtec.co.uk>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -24,8 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input.h>
 #include <linux/gpio_keys.h>
 #include <linux/workqueue.h>
-
-#include <asm/gpio.h>
+#include <linux/gpio.h>
 
 struct gpio_button_data {
 	struct gpio_keys_button *button;
