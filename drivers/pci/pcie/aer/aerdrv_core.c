c8b3bf8ef9cbb5044b180ec8b0a7d81ca43080f3
Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
2009-11-11 14:31:38 +0900

    PCIe AER: use pci_pcie_cap()
    Use pcie_cap() instead of pci_find_capability() to get PCIe capability
    offset in PCIe AER driver. This avoids unnecessary search in PCI
    configuration space.
    
    Signed-off-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pcie/aer/aerdrv_core.c
+++ drivers/pci/pcie/aer/aerdrv_core.c
@@ -43,7 +43,7 @@ int pci_enable_pcie_error_reporting(struct pci_dev *dev)
 	if (!pos)
 		return -EIO;
 
-	pos = pci_find_capability(dev, PCI_CAP_ID_EXP);
+	pos = pci_pcie_cap(dev);
 	if (!pos)
 		return -EIO;
 
@@ -67,7 +67,7 @@ int pci_disable_pcie_error_reporting(struct pci_dev *dev)
 	if (dev->aer_firmware_first)
 		return -EIO;
 
-	pos = pci_find_capability(dev, PCI_CAP_ID_EXP);
+	pos = pci_pcie_cap(dev);
 	if (!pos)
 		return -EIO;
 
@@ -225,7 +225,7 @@ static int find_device_iter(struct pci_dev *dev, void *data)
 	 */
 	if (atomic_read(&dev->enable_cnt) == 0)
 		return 0;
-	pos = pci_find_capability(dev, PCI_CAP_ID_EXP);
+	pos = pci_pcie_cap(dev);
 	if (!pos)
 		return 0;
 	/* Check if AER is enabled */
@@ -619,7 +619,7 @@ void aer_enable_rootport(struct aer_rpc *rpc)
 	u16 reg16;
 	u32 reg32;
 
-	pos = pci_find_capability(pdev, PCI_CAP_ID_EXP);
+	pos = pci_pcie_cap(pdev);
 	/* Clear PCIE Capability's Device Status */
 	pci_read_config_word(pdev, pos+PCI_EXP_DEVSTA, &reg16);
 	pci_write_config_word(pdev, pos+PCI_EXP_DEVSTA, reg16);
