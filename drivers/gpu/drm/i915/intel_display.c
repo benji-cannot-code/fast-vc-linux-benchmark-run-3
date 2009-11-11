32bedc68b454c674d983e7463fdfc9ea2951f29d
Kristian Høgsberg <krh@bitplanet.net>
2009-11-11 12:19:17 -0500

    drm/i915: Hold struct_mutex while unreffing pwrctx object
    This also extends the mutex to cover fbc disabling, which is safe.
    
    Signed-off-by: Kristian Høgsberg <krh@bitplanet.net>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -4585,8 +4585,6 @@ void intel_modeset_cleanup(struct drm_device *dev)
 	intel_increase_renderclock(dev, false);
 	del_timer_sync(&dev_priv->idle_timer);
 
-	mutex_unlock(&dev->struct_mutex);
-
 	if (dev_priv->display.disable_fbc)
 		dev_priv->display.disable_fbc(dev);
 
@@ -4594,6 +4592,8 @@ void intel_modeset_cleanup(struct drm_device *dev)
 		i915_gem_object_unpin(dev_priv->pwrctx);
 		drm_gem_object_unreference(dev_priv->pwrctx);
 	}
+
+	mutex_unlock(&dev->struct_mutex);
 
 	drm_mode_config_cleanup(dev);
 }
