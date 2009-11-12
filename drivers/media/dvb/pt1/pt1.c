66f9092f78ac17c17f90a10efae431fa57b527dd
Huang Weiyi <weiyi.huang@gmail.com>
2009-11-12 10:16:09 -0300

    V4L/DVB: pt1: remove duplicated #include
    Remove duplicated #include('s) in
      drivers/media/dvb/pt1/pt1.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/pt1/pt1.c
+++ drivers/media/dvb/pt1/pt1.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/kthread.h>
 #include <linux/freezer.h>
-#include <linux/vmalloc.h>
 
 #include "dvbdev.h"
 #include "dvb_demux.h"
