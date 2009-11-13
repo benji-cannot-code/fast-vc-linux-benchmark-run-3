25abf6be975533e1a1fed7869a2c41418f7d3c93
Richard Röjfors <richard.rojfors@mocean-labs.com>
2009-11-13 12:28:55 +0100

    xilinx_spi: add support for the DS570 IP.
    This patch adds in support for the DS570 IP.
    
    It's register compatible with the DS464, but adds support for 8/16/32 SPI.
    
    The 8/16/32 support is added by attaching callbacks reading/writing the
    proper amount of data. To indicate to the driver which amount of bits
    to use a new field is introduced in the platform data struct.
    
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    Tested-by: John Linn <John.Linn@xilinx.com>
    Signed-off-by: Richard Röjfors <richard.rojfors@mocean-labs.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/spi/xilinx_spi.h
+++ include/linux/spi/xilinx_spi.h
@@ -4,14 +4,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * struct xspi_platform_data - Platform data of the Xilinx SPI driver
- * @num_chipselect:	Number of chip select by the IP
- * @little_endian	If registers should be accessed little endian or not
+ * @num_chipselect:	Number of chip select by the IP.
+ * @little_endian:	If registers should be accessed little endian or not.
+ * @bits_per_word:	Number of bits per word.
  * @devices:		Devices to add when the driver is probed.
  * @num_devices:	Number of devices in the devices array.
  */
 struct xspi_platform_data {
 	u16 num_chipselect;
 	bool little_endian;
+	u8 bits_per_word;
 	struct spi_board_info *devices;
 	u8 num_devices;
 };
