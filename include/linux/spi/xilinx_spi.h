e44d0caef0d13f7269c4202bb3e04e256386246a
Richard Röjfors <richard.rojfors@mocean-labs.com>
2009-11-13 12:28:39 +0100

    xilinx_spi: Split into of driver and generic part.
    This patch splits the xilinx_spi driver into a generic part and a
    OF driver part.
    
    The reason for this is to later add in a platform driver as well.
    
    Tested-by: John Linn <John.Linn@xilinx.com>
    Signed-off-by: Richard Röjfors <richard.rojfors@mocean-labs.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

---
+++ include/linux/spi/xilinx_spi.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __LINUX_SPI_XILINX_SPI_H
+#define __LINUX_SPI_XILINX_SPI_H
+
+/**
+ * struct xspi_platform_data - Platform data of the Xilinx SPI driver
+ * @num_chipselect:	Number of chip select by the IP
+ * @devices:		Devices to add when the driver is probed.
+ * @num_devices:	Number of devices in the devices array.
+ */
+struct xspi_platform_data {
+	u16 num_chipselect;
+	struct spi_board_info *devices;
+	u8 num_devices;
+};
+
+#endif /* __LINUX_SPI_XILINX_SPI_H */
