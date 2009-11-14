e80df57148566af1597ed43fdc8d14f25a73568a
Arnaldo Carvalho de Melo <acme@redhat.com>
2009-11-14 09:02:48 -0800

    alpha: Fixup recvmmsg syscall glue
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/alpha/include/asm/unistd.h
+++ arch/alpha/include/asm/unistd.h
@@ -434,10 +434,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_signalfd			476
 #define __NR_timerfd			477
 #define __NR_eventfd			478
+#define __NR_recvmmsg			479
 
 #ifdef __KERNEL__
 
-#define NR_SYSCALLS			479
+#define NR_SYSCALLS			480
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
