682f1f6fe64acc58b1b8fb6481884a47f193f095
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2009-11-13 19:51:39 -0800

    rcu: Eliminate __rcu_pending() false positives
    Now that there are both ->gpnum and ->completed fields in the
    rcu_node structure, __rcu_pending() should check rdp->gpnum and
    rdp->completed against rnp->gpnum and rdp->completed, respectively,
    instead of the prior comparison against the rcu_state fields
    rsp->gpnum and rsp->completed.
    
    Given the old comparison, __rcu_pending() could return 1, resulting
    in a needless raise_softirq(RCU_SOFTIRQ).  This useless work would
    happen if RCU responded to a scheduling-clock interrupt after the
    rcu_state fields had been updated, but before the rcu_node fields
    had been updated.
    
    Changing the comparison from the rcu_state fields to the rcu_node
    fields prevents this useless work from happening.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: laijs@cn.fujitsu.com
    Cc: dipankar@in.ibm.com
    Cc: mathieu.desnoyers@polymtl.ca
    Cc: josh@joshtriplett.org
    Cc: dvhltc@us.ibm.com
    Cc: niv@us.ibm.com
    Cc: peterz@infradead.org
    Cc: rostedt@goodmis.org
    Cc: Valdis.Kletnieks@vt.edu
    Cc: dhowells@redhat.com
    LKML-Reference: <12581706991966-git-send-email->
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -1404,6 +1404,8 @@ EXPORT_SYMBOL_GPL(call_rcu_bh);
  */
 static int __rcu_pending(struct rcu_state *rsp, struct rcu_data *rdp)
 {
+	struct rcu_node *rnp = rdp->mynode;
+
 	rdp->n_rcu_pending++;
 
 	/* Check for CPU stalls, if enabled. */
@@ -1428,13 +1430,13 @@ static int __rcu_pending(struct rcu_state *rsp, struct rcu_data *rdp)
 	}
 
 	/* Has another RCU grace period completed?  */
-	if (ACCESS_ONCE(rsp->completed) != rdp->completed) { /* outside lock */
+	if (ACCESS_ONCE(rnp->completed) != rdp->completed) { /* outside lock */
 		rdp->n_rp_gp_completed++;
 		return 1;
 	}
 
 	/* Has a new RCU grace period started? */
-	if (ACCESS_ONCE(rsp->gpnum) != rdp->gpnum) { /* outside lock */
+	if (ACCESS_ONCE(rnp->gpnum) != rdp->gpnum) { /* outside lock */
 		rdp->n_rp_gp_started++;
 		return 1;
 	}
