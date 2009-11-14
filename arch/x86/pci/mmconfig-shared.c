c8269d8b57ea057384a1909b0f1e2c7faa0b9f11
Bjorn Helgaas <bjorn.helgaas@hp.com>
2009-11-13 17:33:58 -0700

    x86/PCI: MMCONFIG: simplify tests for empty pci_mmcfg_config table
    We never set pci_mmcfg_config unless we increment pci_mmcfg_config_num,
    so there's no need to test both pci_mmcfg_config_num and pci_mmcfg_config.
    
    Reviewed-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Bjorn Helgaas <bjorn.helgaas@hp.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- arch/x86/pci/mmconfig-shared.c
+++ arch/x86/pci/mmconfig-shared.c
@@ -473,7 +473,6 @@ static void __init pci_mmcfg_reject_broken(int early)
 	int i;
 
 	if ((pci_mmcfg_config_num == 0) ||
-	    (pci_mmcfg_config == NULL) ||
 	    (pci_mmcfg_config[0].address == 0))
 		return;
 
@@ -619,7 +618,6 @@ static void __init __pci_mmcfg_init(int early)
 	pci_mmcfg_reject_broken(early);
 
 	if ((pci_mmcfg_config_num == 0) ||
-	    (pci_mmcfg_config == NULL) ||
 	    (pci_mmcfg_config[0].address == 0))
 		return;
 
@@ -653,7 +651,6 @@ static int __init pci_mmcfg_late_insert_resources(void)
 	if ((pci_mmcfg_resources_inserted == 1) ||
 	    (pci_probe & PCI_PROBE_MMCONF) == 0 ||
 	    (pci_mmcfg_config_num == 0) ||
-	    (pci_mmcfg_config == NULL) ||
 	    (pci_mmcfg_config[0].address == 0))
 		return 1;
 
