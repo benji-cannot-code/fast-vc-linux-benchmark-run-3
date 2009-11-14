1eeefa21394e366327ccb63235813fc92a820837
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-11-14 20:46:38 +0900

    swiotlb: Remove duplicate swiotlb_force extern declarations
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Cc: tony.luck@intel.com
    LKML-Reference: <1258199198-16657-4-git-send-email-fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/swiotlb.h
+++ arch/x86/include/asm/swiotlb.h
@@ -4,10 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/swiotlb.h>
 
-/* SWIOTLB interface */
-
-extern int swiotlb_force;
-
 #ifdef CONFIG_SWIOTLB
 extern int swiotlb;
 extern int pci_swiotlb_init(void);
