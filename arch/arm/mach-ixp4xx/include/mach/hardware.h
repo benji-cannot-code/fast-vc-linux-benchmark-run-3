7ac30b32a7cefbd375722933f7c4520996f24ce7
Krzysztof Hałasa <khc@pm.waw.pl>
2009-11-15 18:02:10 +0100

    IXP4xx: Extend PCI MMIO indirect address space to 1 GB.
    IXP4xx CPUs can indirectly access the whole 4 GB PCI MMIO address space (using
    the non-prefetch registers). Previously the available space depended on the CPU
    variant, since one of the IXP43x platforms needed more than the usual 128 MB.
    1 GB should be enough for everyone, and if not, we can trivially increase it.
    
    Signed-off-by: Krzysztof Hałasa <khc@pm.waw.pl>

--- arch/arm/mach-ixp4xx/include/mach/hardware.h
+++ arch/arm/mach-ixp4xx/include/mach/hardware.h
@@ -19,7 +19,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARCH_HARDWARE_H__
 
 #define PCIBIOS_MIN_IO		0x00001000
-#define PCIBIOS_MIN_MEM		(cpu_is_ixp43x() ? 0x40000000 : 0x48000000)
+#ifdef CONFIG_IXP4XX_INDIRECT_PCI
+#define PCIBIOS_MIN_MEM		0x10000000 /* 1 GB of indirect PCI MMIO space */
+#define PCIBIOS_MAX_MEM		0x4FFFFFFF
+#else
+#define PCIBIOS_MIN_MEM		0x48000000 /* 64 MB of PCI MMIO space */
+#define PCIBIOS_MAX_MEM		0x4BFFFFFF
+#endif
 
 /*
  * We override the standard dma-mask routines for bouncing.
