e0cbbc06ac84e299f8b58ce593ab2bdf1245b392
Krzysztof Hałasa <khc@pm.waw.pl>
2009-11-16 21:56:56 +0100

    IXP4xx: move DSM G600 platform macros to the platform code.
    Signed-off-by: Krzysztof Hałasa <khc@pm.waw.pl>

--- arch/arm/mach-ixp4xx/include/mach/hardware.h
+++ arch/arm/mach-ixp4xx/include/mach/hardware.h
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Platform specific details */
 #include "prpmc1100.h"
-#include "dsmg600.h"
 #include "fsg.h"
 
 #endif  /* _ASM_ARCH_HARDWARE_H */
