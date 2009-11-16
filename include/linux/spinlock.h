e30480653d0adb296c8d6063a270e60bc31acade
Thomas Gleixner <tglx@linutronix.de>
2009-11-16 19:50:38 +0100

    locking: Remove unused prototype
    commit 910067d1(remove generic__raw_read_trylock()) removed the
    implementation but left the prototype around. Remove it.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/spinlock.h
+++ include/linux/spinlock.h
@@ -80,8 +80,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/spinlock_types.h>
 
-extern int __lockfunc generic__raw_read_trylock(raw_rwlock_t *lock);
-
 /*
  * Pull the __raw*() functions/declarations (UP-nondebug doesnt need them):
  */
