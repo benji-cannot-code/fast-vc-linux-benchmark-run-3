314116992c6f4f9f955a7322c3361422cbf8b93d
Krzysztof Hałasa <khc@pm.waw.pl>
2009-11-16 15:39:11 +0100

    IXP4xx: move AVILA platform macros to the platform code.
    Signed-off-by: Krzysztof Hałasa <khc@pm.waw.pl>

--- arch/arm/mach-ixp4xx/include/mach/hardware.h
+++ arch/arm/mach-ixp4xx/include/mach/hardware.h
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "platform.h"
 
 /* Platform specific details */
-#include "avila.h"
 #include "coyote.h"
 #include "prpmc1100.h"
 #include "nslu2.h"
