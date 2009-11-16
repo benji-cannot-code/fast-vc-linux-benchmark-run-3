4fe13ea3db5afdb118c0a54a7a676741b5b51718
Takashi Iwai <tiwai@suse.de>
2009-11-16 15:33:49 +0100

    ALSA: hda - Fix beep_mode option value
    The beep_mode option value was wrongly defined: it must be 0 = off and
    1 = on.
    
    Also, evaluate the beep_mode value at snd_hda_attach_beep_device()
    properly so that no device is created when beep_mode=0 is given.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_beep.h
+++ sound/pci/hda/hda_beep.h
@@ -25,8 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "hda_codec.h"
 
-#define HDA_BEEP_MODE_ON	0
-#define HDA_BEEP_MODE_OFF	1
+#define HDA_BEEP_MODE_OFF	0
+#define HDA_BEEP_MODE_ON	1
 #define HDA_BEEP_MODE_SWREG	2
 
 /* beep information */
