c311b1d12cc03468b8bafab82d4ebb61434a32f4
Clemens Ladisch <clemens@ladisch.de>
2009-11-16 12:26:30 +0100

    sound: usxxx: cleanup chip field
    The chip field is no longer needed.  Move those of its fields that are
    actually used to the device structure itself.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/usx2y/us122l.h
+++ sound/usb/usx2y/us122l.h
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 struct us122l {
-	struct snd_usb_audio 	chip;
+	struct usb_device	*dev;
+	int			card_index;
 	int			stride;
 	struct usb_stream_kernel sk;
 
