29f367b2ce63a3094e43dc078bf535f6974ec3d6
Krzysztof Hałasa <khc@pm.waw.pl>
2009-11-16 16:02:02 +0100

    IXP4xx: move NSLU2 platform macros to the platform code.
    Signed-off-by: Krzysztof Hałasa <khc@pm.waw.pl>

--- arch/arm/mach-ixp4xx/include/mach/hardware.h
+++ arch/arm/mach-ixp4xx/include/mach/hardware.h
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Platform specific details */
 #include "prpmc1100.h"
-#include "nslu2.h"
 #include "nas100d.h"
 #include "dsmg600.h"
 #include "fsg.h"
--- arch/arm/mach-ixp4xx/include/mach/irqs.h
+++ arch/arm/mach-ixp4xx/include/mach/irqs.h
@@ -80,13 +80,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	IRQ_PRPMC1100_PCI_INTD	IRQ_IXP4XX_GPIO8
 
 /*
- * NSLU2 board IRQs
- */
-#define        IRQ_NSLU2_PCI_INTA      IRQ_IXP4XX_GPIO11
-#define        IRQ_NSLU2_PCI_INTB      IRQ_IXP4XX_GPIO10
-#define        IRQ_NSLU2_PCI_INTC      IRQ_IXP4XX_GPIO9
-
-/*
  * NAS100D board IRQs
  */
 #define        IRQ_NAS100D_PCI_INTA    IRQ_IXP4XX_GPIO11
