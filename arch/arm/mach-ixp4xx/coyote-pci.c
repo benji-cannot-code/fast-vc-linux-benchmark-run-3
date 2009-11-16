42b80540dfcddca3ac06f8ec700a146d0d9cc4cd
Krzysztof Hałasa <khc@pm.waw.pl>
2009-11-16 15:57:41 +0100

    IXP4xx: move Coyote platform macros to the platform code.
    Signed-off-by: Krzysztof Hałasa <khc@pm.waw.pl>

--- arch/arm/mach-ixp4xx/coyote-pci.c
+++ arch/arm/mach-ixp4xx/coyote-pci.c
@@ -19,12 +19,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/init.h>
 #include <linux/irq.h>
-
 #include <asm/mach-types.h>
 #include <mach/hardware.h>
 #include <asm/irq.h>
-
 #include <asm/mach/pci.h>
+
+#define COYOTE_PCI_SLOT0_DEVID	14
+#define COYOTE_PCI_SLOT1_DEVID	15
+
+/* PCI controller GPIO to IRQ pin mappings */
+#define COYOTE_PCI_SLOT0_PIN	6
+#define COYOTE_PCI_SLOT1_PIN	11
+
+#define IRQ_COYOTE_PCI_SLOT0	IRQ_IXP4XX_GPIO6
+#define IRQ_COYOTE_PCI_SLOT1	IRQ_IXP4XX_GPIO11
 
 void __init coyote_pci_preinit(void)
 {
