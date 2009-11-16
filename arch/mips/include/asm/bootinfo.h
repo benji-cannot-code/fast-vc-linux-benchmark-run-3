05247c7d86f2de59b3fb39dab963e099f978e71a
Wu Zhangjin <wuzhangjin@gmail.com>
2009-11-17 00:58:14 +0800

    MIPS: Lemote 2F: Add NAS support
    Kernel support for this machine is almost the same as Fuloong 2F; the only
    difference is that it uses the serial port provided by Loongson 2F processor
    as Yeeloong 2F does.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/656/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/bootinfo.h
+++ arch/mips/include/asm/bootinfo.h
@@ -68,7 +68,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MACH_LEMOTE_ML2F7      3
 #define MACH_LEMOTE_YL2F89     4
 #define MACH_DEXXON_GDIUM2F10  5
-#define MACH_LOONGSON_END      6
+#define MACH_LEMOTE_NAS        6
+#define MACH_LOONGSON_END      7
 
 extern char *system_type;
 const char *get_system_type(void);
