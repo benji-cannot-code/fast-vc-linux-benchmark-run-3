e83e200af11ad5cf4175cfc3d3df9482df22cf34
Christoph Hellwig <hch@lst.de>
2009-11-17 14:39:31 -0800

    avr32: convert to asm-generic/hardirq.h
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Cc: Haavard Skinnemoen <hskinnemoen@atmel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- arch/avr32/kernel/irq.c
+++ arch/avr32/kernel/irq.c
@@ -17,15 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/sysdev.h>
 
-/*
- * 'what should we do if we get a hw irq event on an illegal vector'.
- * each architecture has to answer this themselves.
- */
-void ack_bad_irq(unsigned int irq)
-{
-	printk("unexpected IRQ %u\n", irq);
-}
-
 /* May be overridden by platform code */
 int __weak nmi_enable(void)
 {
