be84214fa3f25b627ee0f25be7527e4a586927b9
Kalle Valo <kalle.valo@nokia.com>
2009-11-17 18:48:45 +0200

    wl1251: allocate space for firmware with vmalloc()
    Earlier firmware was stored to a memory area allocated with kmalloc()
    but finding a a contiguous area of memory long enough for the firmware is
    very difficult in certain cases. better to allocate the memory for firmware
    with vmalloc() instead and use a small buffer for DMA transfers.
    
    Thanks to Eero Tamminen for the idea.
    
    Signed-off-by: Kalle Valo <kalle.valo@nokia.com>
    Reviewed-by: Vidhya Govindan <vidhya.govindan@nokia.com>
    Signed-off-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1251_main.c
+++ drivers/net/wireless/wl12xx/wl1251_main.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/crc32.h>
 #include <linux/etherdevice.h>
+#include <linux/vmalloc.h>
 
 #include "wl1251.h"
 #include "wl12xx_80211.h"
@@ -84,7 +85,7 @@ static int wl1251_fetch_firmware(struct wl1251 *wl)
 	}
 
 	wl->fw_len = fw->size;
-	wl->fw = kmalloc(wl->fw_len, GFP_KERNEL);
+	wl->fw = vmalloc(wl->fw_len);
 
 	if (!wl->fw) {
 		wl1251_error("could not allocate memory for the firmware");
@@ -1428,7 +1429,7 @@ int wl1251_free_hw(struct wl1251 *wl)
 
 	kfree(wl->target_mem_map);
 	kfree(wl->data_path);
-	kfree(wl->fw);
+	vfree(wl->fw);
 	wl->fw = NULL;
 	kfree(wl->nvs);
 	wl->nvs = NULL;
