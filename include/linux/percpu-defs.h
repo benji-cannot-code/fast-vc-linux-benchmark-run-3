cadb78e6ab388a8aa5866625ce545234cc066d7e
Tejun Heo <tj@kernel.org>
2009-11-18 17:53:21 +0900

    percpu: remove compile warnings caused by __verify_pcpu_ptr()
    If percpu pointer is const, __verify_pcpu_ptr() triggers warnings like
    the following.
    
     drivers/net/loopback.c: In function 'loopback_get_stats':
     drivers/net/loopback.c:109: warning: initialization discards qualifiers from pointer target type
    
    Fix it by adding const to the verification target pointer used in
    __verify_pcpu_ptr().
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/percpu-defs.h
+++ include/linux/percpu-defs.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * input parameter is a percpu pointer.
  */
 #define __verify_pcpu_ptr(ptr)	do {					\
-	void __percpu *__vpp_verify = (typeof(ptr))NULL;		\
+	const void __percpu *__vpp_verify = (typeof(ptr))NULL;		\
 	(void)__vpp_verify;						\
 } while (0)
 
