b932643a471a9b5fed92c287ed23f1904c433fa2
Sudhakar Rajashekhara <sudhakar.raj@ti.com>
2009-11-18 11:48:37 +0530

    davinci: da850/omap-l138: Modify NOR partition info
    On DA850/OMAP-L138, NOR flash partition was starting from offset
    ZERO erasing the UBL and u-boot when the complete NOR is erased.
    This patch moves the start of the partition to 512K, after the
    bootloaders and u-boot env variables.
    
    This patch also creates a new partition on NOR Flash to store
    Linux kernel image.
    
    Signed-off-by: Sudhakar Rajashekhara <sudhakar.raj@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/board-da850-evm.c
+++ arch/arm/mach-davinci/board-da850-evm.c
@@ -47,8 +47,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct mtd_partition da850_evm_norflash_partition[] = {
 	{
-		.name           = "NOR filesystem",
+		.name           = "bootloaders + env",
 		.offset         = 0,
+		.size           = SZ_512K,
+		.mask_flags     = MTD_WRITEABLE,
+	},
+	{
+		.name           = "kernel",
+		.offset         = MTDPART_OFS_APPEND,
+		.size           = SZ_2M,
+		.mask_flags     = 0,
+	},
+	{
+		.name           = "filesystem",
+		.offset         = MTDPART_OFS_APPEND,
 		.size           = MTDPART_SIZ_FULL,
 		.mask_flags     = 0,
 	},
