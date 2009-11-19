ed38a916d0e3c3f5d568a322d6859244e032ae46
Jesse Barnes <jbarnes@virtuousgeek.org>
2009-11-19 10:50:22 -0800

    drm: use page flip event to signal flip completion
    We don't actually know which frame number the flip will complete on, so
    userspace needs a specific flip notification to tell it when the last flip
    completed.
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Acked-by: Kristian Høgsberg <krh@bitplanet.net>

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -2528,7 +2528,7 @@ int drm_mode_page_flip_ioctl(struct drm_device *dev,
 			goto out;
 		}
 
-		e->event.base.type = DRM_EVENT_VBLANK;
+		e->event.base.type = DRM_EVENT_FLIP_COMPLETE;
 		e->event.base.length = sizeof e->event;
 		e->event.user_data = page_flip->user_data;
 		e->base.event = &e->event.base;
