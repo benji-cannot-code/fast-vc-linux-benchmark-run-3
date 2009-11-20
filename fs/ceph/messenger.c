9980531d6374214b6882e55e1a9a82b9a8767d87
Sage Weil <sage@newdream.net>
2009-11-20 15:14:15 -0800

    ceph: reset msgr backoff during open, not after successful handshake
    Reset the backoff delay when we reopen the connection, so that the delays
    for any initial connection problems are reasonable.  We were resetting only
    after a successful handshake, which was of limited utility.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- fs/ceph/messenger.c
+++ fs/ceph/messenger.c
@@ -349,6 +349,7 @@ void ceph_con_open(struct ceph_connection *con, struct ceph_entity_addr *addr)
 	set_bit(OPENING, &con->state);
 	clear_bit(CLOSED, &con->state);
 	memcpy(&con->peer_addr, addr, sizeof(*addr));
+	con->delay = 0;      /* reset backoff memory */
 	queue_con(con);
 }
 
@@ -1163,8 +1164,6 @@ static int process_connect(struct ceph_connection *con)
 		     con->connect_seq);
 		WARN_ON(con->connect_seq !=
 			le32_to_cpu(con->in_reply.connect_seq));
-
-		con->delay = 0;  /* reset backoff memory */
 		prepare_read_tag(con);
 		break;
 
