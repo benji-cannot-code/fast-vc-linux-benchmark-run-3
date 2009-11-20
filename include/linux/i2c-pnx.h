9e4428b60903572be5b0defa6c602a53c88a9788
Russell King <rmk+kernel@arm.linux.org.uk>
2009-11-20 10:50:34 +0000

    ARM: PNX4008: move i2c suspend/resume callbacks into driver
    Acked-by: Vitaly Wool <vitalywool@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/linux/i2c-pnx.h
+++ include/linux/i2c-pnx.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __I2C_PNX_H__
 #define __I2C_PNX_H__
 
-#include <linux/pm.h>
-
 struct platform_device;
 
 struct i2c_pnx_mif {
@@ -35,8 +33,6 @@ struct i2c_pnx_algo_data {
 };
 
 struct i2c_pnx_data {
-	int (*suspend) (struct platform_device *pdev, pm_message_t state);
-	int (*resume) (struct platform_device *pdev);
 	u32 (*calculate_input_freq) (struct platform_device *pdev);
 	int (*set_clock_run) (struct platform_device *pdev);
 	int (*set_clock_stop) (struct platform_device *pdev);
