3432cab8c61b9fb8c818a6739f671599b616279d
Russell King <rmk+kernel@arm.linux.org.uk>
2009-11-20 10:32:41 +0000

    ARM: PNX4008: convert to clkdev
    Acked-by: Vitaly Wool <vitalywool@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pnx4008/clock.h
+++ arch/arm/mach-pnx4008/clock.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_ARM_PNX4008_CLOCK_H__
 
 struct clk {
-	struct list_head node;
-	struct module *owner;
 	const char *name;
 	struct clk *parent;
 	struct clk *propagate_next;
