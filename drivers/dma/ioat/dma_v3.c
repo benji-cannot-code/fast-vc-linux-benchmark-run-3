bc1a469f7b8dffc5af29493b3b4d60b04ecb25cf
Dan Williams <dan.j.williams@intel.com>
2009-11-19 17:11:03 -0700

    ioat3: fix pq completion versus channel deallocation race
    The completion of a pq operation is notified with a null descriptor
    appended to the end of the chain.  This descriptor needs to be visible
    to dma clients otherwise the client is precluded from ensuring all
    operations are quiesced before freeing channel resources, i.e. due to
    descriptor polling it may get the completion notification ahead of the
    interrupt delivered by the null descriptor.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/dma/ioat/dma_v3.c
+++ drivers/dma/ioat/dma_v3.c
@@ -572,7 +572,7 @@ __ioat3_prep_xor_lock(struct dma_chan *c, enum sum_check_flags *result,
 	dump_desc_dbg(ioat, compl_desc);
 
 	/* we leave the channel locked to ensure in order submission */
-	return &desc->txd;
+	return &compl_desc->txd;
 }
 
 static struct dma_async_tx_descriptor *
@@ -731,7 +731,7 @@ __ioat3_prep_pq_lock(struct dma_chan *c, enum sum_check_flags *result,
 	dump_desc_dbg(ioat, compl_desc);
 
 	/* we leave the channel locked to ensure in order submission */
-	return &desc->txd;
+	return &compl_desc->txd;
 }
 
 static struct dma_async_tx_descriptor *
