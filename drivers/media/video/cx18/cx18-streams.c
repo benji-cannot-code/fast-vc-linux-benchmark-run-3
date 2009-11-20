f29566080da649fcbce60251fc15d7dc0f9692df
Devin Heitmueller <dheitmueller@kernellabs.com>
2009-11-19 23:23:57 -0300

    V4L/DVB: cx18: export more symbols required by cx18-alsa
    Export a couple of more symbols required by the cx18-alsa module.
    
    This work was sponsored by ONELAN Limited.
    
    Signed-off-by: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-streams.c
+++ drivers/media/video/cx18/cx18-streams.c
@@ -746,6 +746,7 @@ int cx18_start_v4l2_encode_stream(struct cx18_stream *s)
 	atomic_inc(&cx->tot_capturing);
 	return 0;
 }
+EXPORT_SYMBOL(cx18_start_v4l2_encode_stream);
 
 void cx18_stop_all_captures(struct cx18 *cx)
 {
@@ -811,6 +812,7 @@ int cx18_stop_v4l2_encode_stream(struct cx18_stream *s, int gop_end)
 
 	return 0;
 }
+EXPORT_SYMBOL(cx18_stop_v4l2_encode_stream);
 
 u32 cx18_find_handle(struct cx18 *cx)
 {
