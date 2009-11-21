cbd96a894ddd4f8a8e052c82829d6c4d64e9aafa
David S. Miller <davem@davemloft.net>
2009-11-21 11:22:25 -0800

    tcp: Don't make syn cookies initial setting depend on CONFIG_SYSCTL
    That's extremely non-intuitive, noticed by William Allen Simpson.
    
    And let's make the default be on, it's been suggested by a lot of
    people so we'll give it a try.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_minisocks.c
+++ net/ipv4/tcp_minisocks.c
@@ -27,13 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/inet_common.h>
 #include <net/xfrm.h>
 
-#ifdef CONFIG_SYSCTL
-#define SYNC_INIT 0 /* let the user enable it */
-#else
-#define SYNC_INIT 1
-#endif
-
-int sysctl_tcp_syncookies __read_mostly = SYNC_INIT;
+int sysctl_tcp_syncookies __read_mostly = 1;
 EXPORT_SYMBOL(sysctl_tcp_syncookies);
 
 int sysctl_tcp_abort_on_overflow __read_mostly;
