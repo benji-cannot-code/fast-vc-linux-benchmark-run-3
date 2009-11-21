3f359282eb72325bd3938acb5c83278d93755d4a
Andy Walls <awalls@radix.net>
2009-11-21 12:52:34 -0300

    V4L/DVB (13444): ivtv: Add explicit IR controller initialization for the AVerTV M116
    Add explicit support for the AVerTV M116 for use with the ir-kbd-i2c module.
    This also eases future support for other AVerMedia ivtv boards with the same
    microcontroller program at I2C address 0x40.
    
    This is a reworked version of an earlier patch that was...
    
    Reviewed-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Andy Walls <awalls@radix.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/ivtv/ivtv-driver.h
+++ drivers/media/video/ivtv/ivtv-driver.h
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-device.h>
 #include <media/tuner.h>
 #include <media/cx2341x.h>
+#include <media/ir-kbd-i2c.h>
 
 #include <linux/ivtv.h>
 
@@ -682,6 +683,7 @@ struct ivtv {
 	int i2c_state;                  /* i2c bit state */
 	struct mutex i2c_bus_lock;      /* lock i2c bus */
 
+	struct IR_i2c_init_data ir_i2c_init_data;
 
 	/* Program Index information */
 	u32 pgm_info_offset;            /* start of pgm info in encoder memory */
