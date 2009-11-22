3bdd4128070c9bee45fef219f4a3b2ff1cfcf51c
Ladislav Michl <Ladislav.Michl@seznam.cz>
2009-11-23 00:06:50 +0100

    mtd: fix M29W800D dev_id and uaddr
    This one sits in my tree for more than two years...
    Using device code found on page 12 (http://www.btdesigner.com/pdfs/M29W800D.pdf)
    and unlock address from page 15 MTD subsytem happily detects ST M29W800DB
    in 16-bit mode. I do believe original author used only 8-bit mode and thus
    didn't hit this bug.
    
    Signed-off-by: Ladislav Michl <ladis@linux-mips.org>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/chips/jedec_probe.c
+++ drivers/mtd/chips/jedec_probe.c
@@ -143,8 +143,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* ST - www.st.com */
 #define M29F800AB	0x0058
-#define M29W800DT	0x00D7
-#define M29W800DB	0x005B
+#define M29W800DT	0x22D7
+#define M29W800DB	0x225B
 #define M29W400DT	0x00EE
 #define M29W400DB	0x00EF
 #define M29W160DT	0x22C4
@@ -1576,7 +1576,7 @@ static const struct amd_flash_info jedec_table[] = {
 		.dev_id		= M29W800DT,
 		.name		= "ST M29W800DT",
 		.devtypes	= CFI_DEVICETYPE_X16|CFI_DEVICETYPE_X8,
-		.uaddr		= MTD_UADDR_0x5555_0x2AAA,	/* ???? */
+		.uaddr		= MTD_UADDR_0x0AAA_0x0555,
 		.dev_size	= SIZE_1MiB,
 		.cmd_set	= P_ID_AMD_STD,
 		.nr_regions	= 4,
@@ -1591,7 +1591,7 @@ static const struct amd_flash_info jedec_table[] = {
 		.dev_id		= M29W800DB,
 		.name		= "ST M29W800DB",
 		.devtypes	= CFI_DEVICETYPE_X16|CFI_DEVICETYPE_X8,
-		.uaddr		= MTD_UADDR_0x5555_0x2AAA,	/* ???? */
+		.uaddr		= MTD_UADDR_0x0AAA_0x0555,
 		.dev_size	= SIZE_1MiB,
 		.cmd_set	= P_ID_AMD_STD,
 		.nr_regions	= 4,
