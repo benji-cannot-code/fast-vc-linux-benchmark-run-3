5fac187f2fd454f5729f01a4da002fee50485ff2
Frederic Weisbecker <fweisbec@gmail.com>
2009-11-22 05:21:34 +0100

    hw-breakpoints: Remove x86 specific headers from core file
    Remove asm/processor.h and asm/debugreg.h as these headers are
    not used anymore in the hw-breakpoints core file.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Prasad <prasad@linux.vnet.ibm.com>
    LKML-Reference: <1258863695-10464-3-git-send-email-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/hw_breakpoint.c
+++ kernel/hw_breakpoint.c
@@ -41,12 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/hw_breakpoint.h>
 
-#include <asm/processor.h>
-
-#ifdef CONFIG_X86
-#include <asm/debugreg.h>
-#endif
-
 /*
  * Constraints data
  */
