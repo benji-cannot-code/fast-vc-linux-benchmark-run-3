5f4e923a790dca686a0ffbcac5d685ad353ae974
Ben Hutchings <bhutchings@solarflare.com>
2009-11-23 16:05:12 +0000

    sfc: Make board information explicitly Falcon-specific
    Rename struct efx_board to struct falcon_board.
    
    Introduce and use inline function to look up board info from struct
    efx_nic, in preparation for moving it.
    
    Move board init and fini calls into NIC probe and remove functions.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/net_driver.h
+++ drivers/net/sfc/net_driver.h
@@ -396,7 +396,7 @@ enum efx_led_mode {
 };
 
 /**
- * struct efx_board - board information
+ * struct falcon_board - board information
  * @type: Board model type
  * @major: Major rev. ('A', 'B' ...)
  * @minor: Minor rev. (0, 1, ...)
@@ -408,7 +408,7 @@ enum efx_led_mode {
  * @hwmon_client: I2C client for hardware monitor
  * @ioexp_client: I2C client for power/port control
  */
-struct efx_board {
+struct falcon_board {
 	int type;
 	int major;
 	int minor;
@@ -753,7 +753,7 @@ struct efx_nic {
 	unsigned int irq_rx_moderation;
 
 	struct i2c_adapter i2c_adap;
-	struct efx_board board_info;
+	struct falcon_board board_info;
 
 	enum nic_state state;
 	enum reset_type reset_pending;
