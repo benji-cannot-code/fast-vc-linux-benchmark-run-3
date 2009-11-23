5537df9aa67f430acedb1e2c4ed534b47f7dde1c
Ben Hutchings <bhutchings@solarflare.com>
2009-11-23 16:07:30 +0000

    sfc: Combine high-level header files
    All files that include ethtool.h, rx.h or tx.h are also including
    efx.h, and there is no good reason to separate out the few
    declarations they contain.  Therefore fold them into efx.h.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/tx.c
+++ drivers/net/sfc/tx.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_ether.h>
 #include <linux/highmem.h>
 #include "net_driver.h"
-#include "tx.h"
 #include "efx.h"
 #include "falcon.h"
 #include "workarounds.h"
