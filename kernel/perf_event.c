b2f43418e9dd4d234e74f4bf71f9ac30dac0da1f
Peter Zijlstra <a.p.zijlstra@chello.nl>
2009-11-23 11:37:26 +0100

    perf_events: Update the context time on exit
    It appeared we did call update_event_times() on exit, but we
    failed to update the context time, which renders the former
    moot.
    
    Locking is a bit iffy, we call update_event_times under
    ctx->mutex instead of ctx->lock - the next patch fixes this.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    LKML-Reference: <20091123103819.764207355@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/perf_event.c
+++ kernel/perf_event.c
@@ -4984,6 +4984,7 @@ void perf_event_exit_task(struct task_struct *child)
 	 * the events from it.
 	 */
 	unclone_ctx(child_ctx);
+	update_context_time(child_ctx);
 	spin_unlock_irqrestore(&child_ctx->lock, flags);
 
 	/*
