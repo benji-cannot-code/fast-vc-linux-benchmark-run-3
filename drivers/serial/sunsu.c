a062f988936674d5834c627f4df88499bdf90deb
David S. Miller <davem@davemloft.net>
2009-11-24 13:58:52 -0800

    sunsu: Fix detection of SU ports which are RSC console or control.
    These device nodes are named "rsc-console" and "rsc-control" rather
    than 'serial', but the device_type property is 'serial' so we'll
    tip off of that for detection.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/serial/sunsu.c
+++ drivers/serial/sunsu.c
@@ -1518,6 +1518,10 @@ static const struct of_device_id su_match[] = {
 		.name = "serial",
 		.compatible = "su",
 	},
+	{
+		.type = "serial",
+		.compatible = "su",
+	},
 	{},
 };
 MODULE_DEVICE_TABLE(of, su_match);
@@ -1544,6 +1548,12 @@ static int __init sunsu_init(void)
 			num_uart++;
 	}
 	for_each_node_by_name(dp, "serial") {
+		if (of_device_is_compatible(dp, "su")) {
+			if (su_get_type(dp) == SU_PORT_PORT)
+				num_uart++;
+		}
+	}
+	for_each_node_by_type(dp, "serial") {
 		if (of_device_is_compatible(dp, "su")) {
 			if (su_get_type(dp) == SU_PORT_PORT)
 				num_uart++;
