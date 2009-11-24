3530fe9091a99d6c25490da990926d285c730c9f
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2009-11-25 08:46:28 +0900

    x86: Fix iommu=soft boot option
    iommu=soft boot option forces the kernel to use swiotlb.
    
    ( This has the side-effect of enabling the swiotlb over the
      GART if this boot option is provided. This is the desired
      behavior of the swiotlb boot option and works like that
      for all other hw-IOMMU drivers. )
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Cc: yinghai@kernel.org
    LKML-Reference: <20091125084611O.fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/pci-swiotlb.c
+++ arch/x86/kernel/pci-swiotlb.c
@@ -51,6 +51,8 @@ static struct dma_map_ops swiotlb_dma_ops = {
  */
 int __init pci_swiotlb_init(void)
 {
+	int use_swiotlb = swiotlb | swiotlb_force;
+
 	/* don't initialize swiotlb if iommu=off (no_iommu=1) */
 #ifdef CONFIG_X86_64
 	if (!no_iommu && max_pfn > MAX_DMA32_PFN)
@@ -64,5 +66,5 @@ int __init pci_swiotlb_init(void)
 		dma_ops = &swiotlb_dma_ops;
 	}
 
-	return swiotlb_force;
+	return use_swiotlb;
 }
