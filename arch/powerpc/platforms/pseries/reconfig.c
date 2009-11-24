e9363e64f060fe845ce370a061167c6f08befe00
Nathan Fontenot <nfont@austin.ibm.com>
2009-11-24 21:10:49 +0000

    powerpc/pseries: Kernel DLPAR Infrastructure
    The Dynamic Logical Partitioning capabilities of the powerpc pseries platform
    allows for the addition and removal of resources (i.e. CPU's, memory, and PCI
    devices) from a partition. The removal of a resource involves
    removing the resource's node from the device tree and then returning the
    resource to firmware via the rtas set-indicator call.  To add a resource, it
    is first obtained from firmware via the rtas set-indicator call and then a
    new device tree node is created using the ibm,configure-coinnector rtas call
    and added to the device tree.
    
    This patch provides the kernel DLPAR infrastructure in a new filed named
    dlpar.c.  The functionality provided is for acquiring and releasing a resource
    from firmware and the parsing of information returned from the
    ibm,configure-connector rtas call.  Additionally this exports the pSeries
    reconfiguration notifier chain so that it can be invoked when device tree
    updates are made.
    
    Signed-off-by: Nathan Fontenot <nfont@austin.ibm.com>
    Acked-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/platforms/pseries/reconfig.c
+++ arch/powerpc/platforms/pseries/reconfig.c
@@ -97,7 +97,7 @@ static struct device_node *derive_parent(const char *path)
 	return parent;
 }
 
-static BLOCKING_NOTIFIER_HEAD(pSeries_reconfig_chain);
+BLOCKING_NOTIFIER_HEAD(pSeries_reconfig_chain);
 
 int pSeries_reconfig_notifier_register(struct notifier_block *nb)
 {
