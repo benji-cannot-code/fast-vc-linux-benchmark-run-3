649ce7a9d1eed155dceae26c2bc9a298fee7a6a1
Paul Mackerras <paulus@samba.org>
2009-11-24 15:19:43 +1100

    perf tools: Fix compilation on powerpc
    Currently, perf fails to compile on powerpc with this error:
    
         CC util/header.o
     In file included from util/../perf.h:17,
                      from util/header.c:9:
     util/../../../arch/powerpc/include/asm/unistd.h:360:27: error:
     linux/linkage.h: No such file or directory make: ***
     [util/header.o] Error 1
    
    The reason is that we still have a #define __KERNEL__ in effect
    at the point where <asm/unistd.h> gets included, which means we
    get extra stuff that we don't need or want.
    
    This fixes the problem by undefining __KERNEL__ once we have
    included the file for which we need __KERNEL__ defined.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
    Cc: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <19211.24287.453183.78836@cargo.ozlabs.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/include/linux/bitops.h
+++ tools/perf/util/include/linux/bitops.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CONFIG_GENERIC_FIND_FIRST_BIT
 #include "../../../../include/linux/bitops.h"
 
+#undef __KERNEL__
+
 static inline void set_bit(int nr, unsigned long *addr)
 {
 	addr[nr / BITS_PER_LONG] |= 1UL << (nr % BITS_PER_LONG);
