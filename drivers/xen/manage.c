42e2e7fc1de71f9a0545d0497534d873382498c3
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2009-11-24 09:58:49 -0800

    xen: don't call dpm_resume_noirq() with interrupts disabled.
    dpm_resume_noirq() takes a mutex, so it can't be called from a no-interrupt
    context.  Don't call it from within the stop-machine function, but just
    afterwards, since we're resuming anyway, regardless of what happened.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Cc: Stable Kernel <stable@kernel.org>

--- drivers/xen/manage.c
+++ drivers/xen/manage.c
@@ -44,7 +44,6 @@ static int xen_suspend(void *data)
 	if (err) {
 		printk(KERN_ERR "xen_suspend: sysdev_suspend failed: %d\n",
 			err);
-		dpm_resume_noirq(PMSG_RESUME);
 		return err;
 	}
 
@@ -70,7 +69,6 @@ static int xen_suspend(void *data)
 	}
 
 	sysdev_resume();
-	dpm_resume_noirq(PMSG_RESUME);
 
 	return 0;
 }
@@ -109,6 +107,9 @@ static void do_suspend(void)
 	}
 
 	err = stop_machine(xen_suspend, &cancelled, cpumask_of(0));
+
+	dpm_resume_noirq(PMSG_RESUME);
+
 	if (err) {
 		printk(KERN_ERR "failed to start xen_suspend: %d\n", err);
 		goto out;
@@ -119,8 +120,6 @@ static void do_suspend(void)
 		xs_resume();
 	} else
 		xs_suspend_cancel();
-
-	dpm_resume_noirq(PMSG_RESUME);
 
 resume_devices:
 	dpm_resume_end(PMSG_RESUME);
