e6407c0cdff5cab1d85f0ccca32cb0b0c98a4cdf
Paul Mundt <lethal@linux-sh.org>
2009-11-25 16:23:35 +0900

    serial: sh-sci: Depend on HAVE_CLK unconditionally.
    The sh-sci code conditionalized the clock framework support in order to
    give the other platforms a chance to catch up. sh64 supported this some
    time ago and the forthcoming ARM changes handle this as well, this leaves
    h8300 as the odd one out. H8300 has had since 2.5 to merge it's sh-sci
    support upstream, and has yet to do so. At this point I will no longer be
    holding back the driver to support an unreponsive architecture, 7 years
    is quite enough of a grace period. Support is easily implemented on the
    architecture if and when it ever decides to merge its changes upstream.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/serial/sh-sci.h
+++ drivers/serial/sh-sci.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial_core.h>
-#include <asm/io.h>
+#include <linux/io.h>
 #include <linux/gpio.h>
 
 #if defined(CONFIG_H83007) || defined(CONFIG_H83068)
