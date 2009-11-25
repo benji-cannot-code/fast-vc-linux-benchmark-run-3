94bf534594449bedd8e386d457944e22385bcf38
Boaz Harrosh <bharrosh@panasas.com>
2009-11-25 15:19:17 -0800

    sparc: remove unused nfsd #includes
    Some unused includes removed.
    
    In an effort to cleanup nfsd headers and move private
    definitions to source directory.
    
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/sys_sparc32.c
+++ arch/sparc/kernel/sys_sparc32.c
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nfs_fs.h>
 #include <linux/quota.h>
 #include <linux/module.h>
-#include <linux/sunrpc/svc.h>
-#include <linux/nfsd/nfsd.h>
-#include <linux/nfsd/cache.h>
-#include <linux/nfsd/xdr.h>
-#include <linux/nfsd/syscall.h>
 #include <linux/poll.h>
 #include <linux/personality.h>
 #include <linux/stat.h>
