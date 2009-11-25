b05a8c8e3b71edf781d6d7040cc93f1062ea6c61
Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
2009-11-25 21:00:53 +0900

    PCI: portdrv: remove redundant pcie_port_device_probe
    We don't need pcie_port_device_probe() because we can get pci
    device/port type using pci_is_pcie() and 'pcie_type' fields in struct
    pci_dev. Remove pcie_port_device_probe().
    
    Signed-off-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pcie/portdrv.h
+++ drivers/pci/pcie/portdrv.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define get_descriptor_id(type, service) (((type - 4) << 4) | service)
 
 extern struct bus_type pcie_port_bus_type;
-extern int pcie_port_device_probe(struct pci_dev *dev);
 extern int pcie_port_device_register(struct pci_dev *dev);
 #ifdef CONFIG_PM
 extern int pcie_port_device_suspend(struct device *dev);
