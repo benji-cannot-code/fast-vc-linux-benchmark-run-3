fc45ab51167d6e95c4fa31edd6fde2cfd6357f3e
Li Zefan <lizf@cn.fujitsu.com>
2009-11-26 15:04:10 +0800

    tracing: Convert some kmem events to DEFINE_EVENT
    Use DECLARE_EVENT_CLASS to remove duplicate code:
    
       text    data     bss     dec     hex filename
     333987   69800   27228  431015   693a7 mm/built-in.o.old
     330030   69800   27228  427058   68432 mm/built-in.o
    
    8 events are converted:
    
      kmem_alloc: kmalloc, kmem_cache_alloc
      kmem_alloc_node: kmalloc_node, kmem_cache_alloc_node
      kmem_free: kfree, kmem_cache_free
      mm_page: mm_page_alloc_zone_locked, mm_page_pcpu_drain
    
    No change in functionality.
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Acked-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mel Gorman <mel@csn.ul.ie>
    LKML-Reference: <4B0E286A.2000405@cn.fujitsu.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- mm/util.c
+++ mm/util.c
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <asm/uaccess.h>
 
-#define CREATE_TRACE_POINTS
-#include <trace/events/kmem.h>
-
 /**
  * kstrdup - allocate space for and copy an existing string
  * @s: the string to duplicate
