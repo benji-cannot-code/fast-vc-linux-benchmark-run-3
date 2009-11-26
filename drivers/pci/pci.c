a74b42dcec0bfac9f40c8df7657c64d7a6762810
Yinghai Lu <yinghai@kernel.org>
2009-11-25 16:28:50 -0800

    PCI: add debug output for DMA mask info
    This allows us to find out what DMA mask is used for each PCI device at boot
    time; useful for debugging.
    
    After the patch:
    ehci_hcd 0000:00:02.1: using 31bit consistent DMA mask
    e1000 0000:0b:01.0: using 64bit DMA mask
    e1000 0000:0b:01.0: using 64bit consistent DMA mask
    e1000e 0000:04:00.0: using 64bit DMA mask
    e1000e 0000:04:00.0: using 64bit consistent DMA mask
    ixgb 0000:0c:01.0: using 64bit DMA mask
    ixgb 0000:0c:01.0: using 64bit consistent DMA mask
    aacraid 0000:86:00.0: using 32bit DMA mask
    aacraid 0000:86:00.0: using 32bit consistent DMA mask
    aacraid 0000:86:00.0: using 64bit DMA mask
    aacraid 0000:86:00.0: using 64bit consistent DMA mask
    qla2xxx 0000:0c:02.0: using 64bit consistent DMA mask
    qla2xxx 0000:0c:02.1: using 64bit consistent DMA mask
    lpfc 0000:06:00.0: using 64bit DMA mask
    lpfc 0000:06:00.1: using 64bit DMA mask
    pata_amd 0000:00:06.0: using 32bit DMA mask
    pata_amd 0000:00:06.0: using 32bit consistent DMA mask
    mptsas 0000:0c:04.0: using 64bit DMA mask
    mptsas 0000:0c:04.0: using 64bit consistent DMA mask
    
    forcedeth 0000:00:08.0: using 39bit DMA mask
    forcedeth 0000:00:08.0: using 39bit consistent DMA mask
    niu 0000:02:00.0: using 44bit DMA mask
    niu 0000:02:00.0: using 44bit consistent DMA mask
    sata_nv 0000:00:05.0: using 32bit DMA mask
    sata_nv 0000:00:05.0: using 32bit consistent DMA mask
    ib_mthca 0000:03:00.0: using 64bit DMA mask
    ib_mthca 0000:03:00.0: using 64bit consistent DMA mask
    
    Reviewed-by: Grant Grundler <grundler@google.com>
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -2102,6 +2102,7 @@ pci_set_dma_mask(struct pci_dev *dev, u64 mask)
 		return -EIO;
 
 	dev->dma_mask = mask;
+	dev_dbg(&dev->dev, "using %dbit DMA mask\n", fls64(mask));
 
 	return 0;
 }
@@ -2113,6 +2114,7 @@ pci_set_consistent_dma_mask(struct pci_dev *dev, u64 mask)
 		return -EIO;
 
 	dev->dev.coherent_dma_mask = mask;
+	dev_dbg(&dev->dev, "using %dbit consistent DMA mask\n", fls64(mask));
 
 	return 0;
 }
