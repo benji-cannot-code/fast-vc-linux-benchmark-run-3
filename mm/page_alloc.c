745f18b728c6c8d775e2b9993e37aee65185a6ac
Ingo Molnar <mingo@elte.hu>
2009-11-26 13:11:46 +0100

    tracing: Fix kmem event exports
    Commit 53d0422 ("tracing: Convert some kmem events to DEFINE_EVENT")
    moved the kmem tracepoint creation from util.c to page_alloc.c,
    but forgot to move the exports.
    
    Move them back.
    
    Cc: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Pekka Enberg <penberg@cs.helsinki.fi>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mel Gorman <mel@csn.ul.ie>
    LKML-Reference: <4B0E286A.2000405@cn.fujitsu.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -49,13 +49,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/page_cgroup.h>
 #include <linux/debugobjects.h>
 #include <linux/kmemleak.h>
+#include <trace/events/kmem.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
 #include "internal.h"
-
-#define CREATE_TRACE_POINTS
-#include <trace/events/kmem.h>
 
 /*
  * Array of node states.
