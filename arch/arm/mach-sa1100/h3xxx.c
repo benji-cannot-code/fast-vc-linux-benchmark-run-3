85e0b3e34bd8e57e7f93c0f7c1889926d3e03896
Dmitry Artamonow <mad_soft@inbox.ru>
2009-11-27 12:12:25 +0100

    ARM: 5822/1: SA1100: h3100/h3600: clean up #includes
    After a code reorganization and following split, there's some #includes
    now unused. Clean them up and sort remaining alphabetticaly where possible.
    
    Compile tested.
    
    Signed-off-by: Dmitry Artamonow <mad_soft@inbox.ru>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-sa1100/h3xxx.c
+++ arch/arm/mach-sa1100/h3xxx.c
@@ -11,28 +11,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#include <linux/module.h>
-#include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/tty.h>
-#include <linux/pm.h>
-#include <linux/device.h>
+#include <linux/gpio.h>
 #include <linux/mfd/htc-egpio.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
-#include <linux/serial_core.h>
-#include <linux/gpio.h>
 #include <linux/platform_device.h>
+#include <linux/serial_core.h>
 
-#include <asm/irq.h>
-#include <mach/hardware.h>
-#include <asm/mach-types.h>
-#include <asm/setup.h>
-
-#include <asm/mach/irq.h>
-#include <asm/mach/arch.h>
 #include <asm/mach/flash.h>
-#include <asm/mach/irda.h>
 #include <asm/mach/map.h>
 #include <asm/mach/serial_sa1100.h>
 
