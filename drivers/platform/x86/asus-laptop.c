ddf2e53f03a9e47f351dd8ca00a53cb6aa07d594
Corentin Chary <corentincj@iksaif.net>
2009-11-28 10:35:37 +0100

    asus-laptop: remove unecessary hotk != NULL check
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

--- drivers/platform/x86/asus-laptop.c
+++ drivers/platform/x86/asus-laptop.c
@@ -942,10 +942,6 @@ static void asus_hotk_notify(struct acpi_device *device, u32 event)
 	static struct key_entry *key;
 	u16 count;
 
-	/* TODO Find a better way to handle events count. */
-	if (!hotk)
-		return;
-
 	/*
 	 * We need to tell the backlight device when the backlight power is
 	 * switched
@@ -958,6 +954,7 @@ static void asus_hotk_notify(struct acpi_device *device, u32 event)
 		lcd_blank(FB_BLANK_POWERDOWN);
 	}
 
+	/* TODO Find a better way to handle events count. */
 	count = hotk->event_count[event % 128]++;
 	acpi_bus_generate_proc_event(hotk->device, event, count);
 	acpi_bus_generate_netlink_event(hotk->device->pnp.device_class,
