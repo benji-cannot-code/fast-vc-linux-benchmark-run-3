07e45bfc8722b35056a034e7a722ec7d4f062f18
Corentin Chary <corentincj@iksaif.net>
2009-11-28 09:45:14 +0100

    staging/android: fix build issues
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
+#include <linux/sched.h>
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
