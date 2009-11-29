d52fd5edf128037f74a89e35e3a00764a2df416e
Mauro Carvalho Chehab <mchehab@redhat.com>
2009-11-29 08:19:59 -0300

    V4L/DVB (13537): ir: Prepare the code for dynamic keycode table allocation
    Currently, the IR table is initialized by calling ir_input_init(). However,
    this function doesn't return any error code, nor has a function to be called
    when de-initializing the IR's.
    
    Change the return argment to integer and make sure that each driver will
    handle the error code. Also adds a function to free any resources that may
    be allocating there: ir_input_free().
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-input.c
+++ drivers/media/video/em28xx/em28xx-input.c
@@ -368,7 +368,11 @@ int em28xx_ir_init(struct em28xx *dev)
 	usb_make_path(dev->udev, ir->phys, sizeof(ir->phys));
 	strlcat(ir->phys, "/input0", sizeof(ir->phys));
 
-	ir_input_init(input_dev, &ir->ir, IR_TYPE_OTHER, dev->board.ir_codes);
+	err = ir_input_init(input_dev, &ir->ir, IR_TYPE_OTHER,
+			     dev->board.ir_codes);
+	if (err < 0)
+		goto err_out_free;
+
 	input_dev->name = ir->name;
 	input_dev->phys = ir->phys;
 	input_dev->id.bustype = BUS_USB;
@@ -393,6 +397,7 @@ int em28xx_ir_init(struct em28xx *dev)
 	em28xx_ir_stop(ir);
 	dev->ir = NULL;
  err_out_free:
+	ir_input_free(input_dev);
 	input_free_device(input_dev);
 	kfree(ir);
 	return err;
@@ -407,6 +412,7 @@ int em28xx_ir_fini(struct em28xx *dev)
 		return 0;
 
 	em28xx_ir_stop(ir);
+	ir_input_free(ir->input);
 	input_unregister_device(ir->input);
 	kfree(ir);
 
