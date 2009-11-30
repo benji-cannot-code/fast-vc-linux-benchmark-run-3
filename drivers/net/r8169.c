47d839d1401ed0d3964296bbc76601e9ab15ea86
Ivan Vecera <ivecera@redhat.com>
2009-11-29 23:12:52 -0800

    r8169: restore mac addr in rtl8169_remove_one and rtl_shutdown
    The newer chipsets (all PCI-E) are known that they need full power cycle
    (AC or battery removal) to reset MAC address to a  hardwired one. Previous
    patch to address this problem loads the original MAC address from EEPROM.
    But it brought other problem for which it is necessary to introduce a new
    module parameter.
    However, it might suffice to restore the initial MAC address before
    shutdown/reboot/kexec and when removing the module.
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/r8169.c
+++ drivers/net/r8169.c
@@ -3236,6 +3236,10 @@ static void __devexit rtl8169_remove_one(struct pci_dev *pdev)
 	flush_scheduled_work();
 
 	unregister_netdev(dev);
+
+	/* restore original MAC address */
+	rtl_rar_set(tp, dev->perm_addr);
+
 	rtl_disable_msi(pdev, tp);
 	rtl8169_release_board(pdev, dev, tp->mmio_addr);
 	pci_set_drvdata(pdev, NULL);
@@ -4881,6 +4885,9 @@ static void rtl_shutdown(struct pci_dev *pdev)
 	void __iomem *ioaddr = tp->mmio_addr;
 
 	rtl8169_net_suspend(dev);
+
+	/* restore original MAC address */
+	rtl_rar_set(tp, dev->perm_addr);
 
 	spin_lock_irq(&tp->lock);
 
