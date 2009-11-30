8123bf2b7fb8468b1dabf9d5c27a1108fe04309b
Lennert Buytenhek <buytenh@wantstofly.org>
2009-11-30 18:13:34 +0100

    mwl8k: don't forget to call pci_disable_device()
    Don't forget to call pci_disable_device() if pci_request_regions()
    fails during probe.
    
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwl8k.c
+++ drivers/net/wireless/mwl8k.c
@@ -3379,7 +3379,7 @@ static int __devinit mwl8k_probe(struct pci_dev *pdev,
 	if (rc) {
 		printk(KERN_ERR "%s: Cannot obtain PCI resources\n",
 		       MWL8K_NAME);
-		return rc;
+		goto err_disable_device;
 	}
 
 	pci_set_master(pdev);
@@ -3610,6 +3610,8 @@ err_iounmap:
 
 err_free_reg:
 	pci_release_regions(pdev);
+
+err_disable_device:
 	pci_disable_device(pdev);
 
 	return rc;
