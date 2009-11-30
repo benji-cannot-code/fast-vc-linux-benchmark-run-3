2dd7a12c905cd6a0adb92383e46f3f2759a3f410
Kalle Valo <kalle.valo@nokia.com>
2009-11-30 10:17:59 +0200

    wl1251: reduce ELP wakeup timeout
    The original TI driver uses 100 ms timeout ELP wakeup timeout, better
    to use the same. Otherwise problems with wakeup might get unnoticed.
    
    Signed-off-by: Kalle Valo <kalle.valo@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1251_ps.c
+++ drivers/net/wireless/wl12xx/wl1251_ps.c
@@ -27,7 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wl1251_cmd.h"
 #include "wl1251_io.h"
 
-#define WL1251_WAKEUP_TIMEOUT 2000
+/* in ms */
+#define WL1251_WAKEUP_TIMEOUT 100
 
 void wl1251_elp_work(struct work_struct *work)
 {
