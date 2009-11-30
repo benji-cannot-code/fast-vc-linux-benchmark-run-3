abc5a3ae2007c9b89eb2016736a73ac4bd7f6342
Daniel Mack <daniel@caiaq.de>
2009-11-30 14:06:37 +0100

    ASoC: pxa/raumfeld: adopt new snd_soc_dai_set_pll() API
    ALSA's for-2.6.33 branch has a new source argument to
    snd_soc_dai_set_pll().
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/pxa/raumfeld.c
+++ sound/soc/pxa/raumfeld.c
@@ -117,7 +117,7 @@ static int raumfeld_cs4270_hw_params(struct snd_pcm_substream *substream,
 		return ret;
 
 	/* setup the CPU DAI */
-	ret = snd_soc_dai_set_pll(cpu_dai, 0, 0, clk);
+	ret = snd_soc_dai_set_pll(cpu_dai, 0, 0, 0, clk);
 	if (ret < 0)
 		return ret;
 
@@ -206,7 +206,7 @@ static int raumfeld_ak4104_hw_params(struct snd_pcm_substream *substream,
 		return ret;
 
 	/* setup the CPU DAI */
-	ret = snd_soc_dai_set_pll(cpu_dai, 0, 0, clk);
+	ret = snd_soc_dai_set_pll(cpu_dai, 0, 0, 0, clk);
 	if (ret < 0)
 		return ret;
 
