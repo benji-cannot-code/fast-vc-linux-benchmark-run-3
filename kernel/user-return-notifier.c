64b8f2e7621321b8615f7895b09d692af58de509
Avi Kivity <avi@redhat.com>
2009-11-30 12:54:51 +0200

    core: Clean up user return notifers use of per_cpu
    Instead of using per_cpu(..., raw_smp_processor_id()), use
    __get_cpu_var(...).
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    LKML-Reference: <1259578491-4589-1-git-send-email-avi@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/user-return-notifier.c
+++ kernel/user-return-notifier.c
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static DEFINE_PER_CPU(struct hlist_head, return_notifier_list);
 
-#define URN_LIST_HEAD per_cpu(return_notifier_list, raw_smp_processor_id())
-
 /*
  * Request a notification when the current cpu returns to userspace.  Must be
  * called in atomic context.  The notifier will also be called in atomic
@@ -17,7 +15,7 @@ static DEFINE_PER_CPU(struct hlist_head, return_notifier_list);
 void user_return_notifier_register(struct user_return_notifier *urn)
 {
 	set_tsk_thread_flag(current, TIF_USER_RETURN_NOTIFY);
-	hlist_add_head(&urn->link, &URN_LIST_HEAD);
+	hlist_add_head(&urn->link, &__get_cpu_var(return_notifier_list));
 }
 EXPORT_SYMBOL_GPL(user_return_notifier_register);
 
@@ -28,7 +26,7 @@ EXPORT_SYMBOL_GPL(user_return_notifier_register);
 void user_return_notifier_unregister(struct user_return_notifier *urn)
 {
 	hlist_del(&urn->link);
-	if (hlist_empty(&URN_LIST_HEAD))
+	if (hlist_empty(&__get_cpu_var(return_notifier_list)))
 		clear_tsk_thread_flag(current, TIF_USER_RETURN_NOTIFY);
 }
 EXPORT_SYMBOL_GPL(user_return_notifier_unregister);
