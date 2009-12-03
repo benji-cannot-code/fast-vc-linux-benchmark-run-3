6f07bddeb1b31817ef4d2001a3084aa3c3ff3d0d
Mike Frysinger <vapier@gentoo.org>
2009-12-03 23:37:07 +0000

    Blackfin: ip0x: pull in asm/portmux.h for P_xxx defines
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/mach-bf533/boards/ip0x.c
+++ arch/blackfin/mach-bf533/boards/ip0x.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 #include <asm/irq.h>
 #include <asm/bfin5xx_spi.h>
+#include <asm/portmux.h>
 
 /*
  * Name the Board for the /proc/cpuinfo
