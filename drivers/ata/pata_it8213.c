a7f98be7b1a00b188a1b922528ec46c90e9593de
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2009-12-03 20:32:11 +0100

    pata_it8213: fix it8213_pre_reset() documentation
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/pata_it8213.c
+++ drivers/ata/pata_it8213.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRV_VERSION	"0.0.3"
 
 /**
- *	it8213_pre_reset	-	check for 40/80 pin
+ *	it8213_pre_reset	-	probe begin
  *	@link: link
  *	@deadline: deadline jiffies for the operation
  *
