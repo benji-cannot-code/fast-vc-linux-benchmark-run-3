7fa46cc450ad5eaa3e5049d74ca66d68090664ff
Amit Kucheria <amit.kucheria@canonical.com>
2009-12-03 22:36:41 +0200

    mxc: TrustZone interrupt controller (TZIC) for Freescale i.MX5 family
    Freescale i.MX51 processor uses a new interrupt controller. Add
    driver for TrustZone Interrupt Controller
    
    Signed-off-by: Amit Kucheria <amit.kucheria@canonical.com>

--- arch/arm/plat-mxc/include/mach/irqs.h
+++ arch/arm/plat-mxc/include/mach/irqs.h
@@ -13,9 +13,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARCH_MXC_IRQS_H__
 
 /*
- * So far all i.MX SoCs have 64 internal interrupts
+ * SoCs with TZIC interrupt controller have 128 IRQs, those with AVIC have 64
  */
+#ifdef CONFIG_MXC_TZIC
+#define MXC_INTERNAL_IRQS	128
+#else
 #define MXC_INTERNAL_IRQS	64
+#endif
 
 #define MXC_GPIO_IRQ_START	MXC_INTERNAL_IRQS
 
