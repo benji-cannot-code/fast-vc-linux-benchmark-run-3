016d5702ce1de00cd8261532bb0d57bd991c9d17
Manu Abraham <abraham.manu@gmail.com>
2009-12-04 04:43:04 -0300

    V4L/DVB (13724): [Mantis/VP-1041] Initial support for Mantis VP-1041
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/mantis/mantis_core.c
+++ drivers/media/dvb/mantis/mantis_core.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mantis_core.h"
 #include "mantis_vp1033.h"
 #include "mantis_vp1034.h"
+#include "mantis_vp1041.h"
 #include "mantis_vp2033.h"
 #include "mantis_vp2040.h"
 #include "mantis_vp3030.h"
@@ -111,6 +112,9 @@ static void mantis_load_config(struct mantis_pci *mantis)
 		break;
 	case MANTIS_VP_1034_DVB_S:	// VP-1034
 		mantis->hwconfig = &vp1034_mantis_config;
+		break;
+	case MANTIS_VP_1041_DVB_S2:	// VP-1041
+		mantis->hwconfig = &vp1041_mantis_config;
 		break;
 	case MANTIS_VP_2033_DVB_C:	// VP-2033
 		mantis->hwconfig = &vp2033_mantis_config;
