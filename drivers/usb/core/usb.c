70b96eed7df279dabdab0196e47bd5aa4e011f6e
Felipe Balbi <felipe.balbi@nokia.com>
2009-12-04 15:47:42 +0200

    USB: core: fix sparse warning for static function
    Fix the following sparse warning:
    
    drivers/usb/core/usb.c:1033:15: warning: symbol 'usb_debug_devices' was not declared. Should it be static?
    
    Signed-off-by: Felipe Balbi <felipe.balbi@nokia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/usb.c
+++ drivers/usb/core/usb.c
@@ -1068,7 +1068,7 @@ static struct notifier_block usb_bus_nb = {
 struct dentry *usb_debug_root;
 EXPORT_SYMBOL_GPL(usb_debug_root);
 
-struct dentry *usb_debug_devices;
+static struct dentry *usb_debug_devices;
 
 static int usb_debugfs_init(void)
 {
