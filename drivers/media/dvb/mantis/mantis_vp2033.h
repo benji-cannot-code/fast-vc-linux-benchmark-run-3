9f099d83f31342871d788c052ea814c0aa3c68d1
Manu Abraham <abraham.manu@gmail.com>
2009-12-04 04:40:50 -0300

    V4L/DVB (13722): [Mantis] Revert 13560
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/mantis/mantis_vp2033.h
+++ drivers/media/dvb/mantis/mantis_vp2033.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tda1002x.h"
 
 #define MANTIS_VP_2033_DVB_C	0x0008
-#define TERRATEC_CINERGY_C_PCI	0x1178
 
 extern struct tda1002x_config philips_cu1216_config;
 extern struct mantis_hwconfig vp2033_mantis_config;
