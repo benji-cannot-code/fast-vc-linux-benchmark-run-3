2b969b4ba6adb97f85ca247545da6a6a7e719999
Bart Van Assche <bart.vanassche@gmail.com>
2009-12-04 20:43:37 +0100

    [SCSI] ibmvscsi: fix a typo in a source code comment
    Signed-off-by: Bart Van Assche <bart.vanassche@gmail.com>
    Acked-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/ibmvscsi/ibmvscsi.c
+++ drivers/scsi/ibmvscsi/ibmvscsi.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * (CRQ), which is just a buffer of 16 byte entries in the receiver's 
  * Senders cannot access the buffer directly, but send messages by
  * making a hypervisor call and passing in the 16 bytes.  The hypervisor
- * puts the message in the next 16 byte space in round-robbin fashion,
+ * puts the message in the next 16 byte space in round-robin fashion,
  * turns on the high order bit of the message (the valid bit), and 
  * generates an interrupt to the receiver (if interrupts are turned on.) 
  * The receiver just turns off the valid bit when they have copied out
