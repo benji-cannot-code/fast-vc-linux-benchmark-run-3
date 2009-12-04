756eeb24e431f33723743ddefb0fb262bfda25d3
Manu Abraham <abraham.manu@gmail.com>
2009-12-04 05:16:24 -0300

    V4L/DVB (13751): [Mantis] GPIO_CONTROL: Do not toggle GPIO CW's on HIF operations
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/mantis/mantis_hif.c
+++ drivers/media/dvb/mantis/mantis_hif.c
@@ -57,6 +57,7 @@ static int mantis_hif_sbuf_opdone_wait(struct mantis_ca *ca)
 	return rc;
 }
 
+
 int mantis_hif_read_mem(struct mantis_ca *ca, u32 addr)
 {
 	struct mantis_pci *mantis = ca->ca_priv;
@@ -68,7 +69,7 @@ int mantis_hif_read_mem(struct mantis_ca *ca, u32 addr)
 	hif_addr &= ~MANTIS_GPIF_PCMCIAIOM;
 	hif_addr |=  addr;
 
-	mmwrite(hif_addr, MANTIS_GPIF_BRADDR);
+	mmwrite(hif_addr | MANTIS_HIF_STATUS, MANTIS_GPIF_BRADDR);
 	mmwrite(count, MANTIS_GPIF_BRBYTES);
 
 	udelay(20);
@@ -101,7 +102,7 @@ int mantis_hif_write_mem(struct mantis_ca *ca, u32 addr, u8 data)
 
 	mmwrite(slot->slave_cfg, MANTIS_GPIF_CFGSLA); /* Slot0 alone for now */
 
-	mmwrite(hif_addr, MANTIS_GPIF_ADDR);
+	mmwrite(hif_addr | MANTIS_HIF_STATUS, MANTIS_GPIF_ADDR);
 	mmwrite(data, MANTIS_GPIF_DOUT);
 	ca->hif_job_queue = MANTIS_HIF_MEMWR;
 
@@ -125,7 +126,7 @@ int mantis_hif_read_iom(struct mantis_ca *ca, u32 addr)
 	hif_addr |=  MANTIS_GPIF_PCMCIAIOM;
 	hif_addr |=  addr;
 
-	mmwrite(hif_addr, MANTIS_GPIF_ADDR);
+	mmwrite(hif_addr | MANTIS_HIF_STATUS, MANTIS_GPIF_ADDR);
 	ca->hif_job_queue = MANTIS_HIF_IOMRD;
 
 	if (mantis_hif_sbuf_opdone_wait(ca) != 0) {
@@ -153,7 +154,7 @@ int mantis_hif_write_iom(struct mantis_ca *ca, u32 addr, u8 data)
 	hif_addr |=  MANTIS_GPIF_PCMCIAIOM;
 	hif_addr |=  addr;
 
-	mmwrite(hif_addr, MANTIS_GPIF_ADDR);
+	mmwrite(hif_addr | MANTIS_HIF_STATUS, MANTIS_GPIF_ADDR);
 	mmwrite(data, MANTIS_GPIF_DOUT);
 
 	ca->hif_job_queue = MANTIS_HIF_IOMWR;
