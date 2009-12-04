26aa7567854bf9b3f8c844dd81107e73405a78ac
Greg Kroah-Hartman <gregkh@suse.de>
2009-12-03 16:24:47 -0800

    Staging: samsung-laptop: remove old kernel code
    Don't test for the kernel version, we know what version we are in,
    the latest.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/samsung-laptop/samsung-laptop.c
+++ drivers/staging/samsung-laptop/samsung-laptop.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * the Free Software Foundation.
  *
  */
-#include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -275,7 +274,6 @@ static struct backlight_ops backlight_ops = {
 	.update_status	= update_status,
 };
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,31)
 static int rfkill_set(void *data, bool blocked)
 {
 	/* Do something with blocked...*/
@@ -318,44 +316,6 @@ static void destroy_wireless(void)
 	rfkill_unregister(rfk);
 	rfkill_destroy(rfk);
 }
-
-#else
-
-static int rfkill_set(void *data, enum rfkill_state state)
-{
-	if (state ==  RFKILL_STATE_UNBLOCKED)
-		sabi_set_command(SET_WIRELESS_BUTTON, 1);
-	else
-		sabi_set_command(SET_WIRELESS_BUTTON, 0);
-
-	return 0;
-}
-
-static int init_wireless(struct platform_device *sdev)
-{
-	int retval;
-
-	rfk = rfkill_allocate(&sdev->dev, RFKILL_TYPE_WLAN);
-	if (!rfk)
-		return -ENOMEM;
-	rfk->toggle_radio = rfkill_set;
-	rfk->name = "samsung-wifi";
-
-	retval = rfkill_register(rfk);
-	if (retval) {
-		rfkill_free(rfk);
-		return -ENODEV;
-	}
-
-	return 0;
-}
-
-static void destroy_wireless(void)
-{
-	rfkill_unregister(rfk);
-}
-
-#endif
 
 static ssize_t get_silent_state(struct device *dev,
 				struct device_attribute *attr, char *buf)
