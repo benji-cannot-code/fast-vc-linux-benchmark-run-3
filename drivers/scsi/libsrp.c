db89ff972a9da6cc3c2ee41f038afb8dc952d3e6
Bart Van Assche <bart.vanassche@gmail.com>
2009-12-04 20:38:49 +0100

    [SCSI] libsrp: fix typo -- replace RDAM by RDMA
    Fixed a typo in libsrp.c: replaced two occurrences of 'RDAM' by 'RDMA'.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@gmail.com>
    Acked-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/libsrp.c
+++ drivers/scsi/libsrp.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * SCSI RDAM Protocol lib functions
+ * SCSI RDMA Protocol lib functions
  *
  * Copyright (C) 2006 FUJITA Tomonori <tomof@acm.org>
  *
@@ -441,6 +441,6 @@ int srp_cmd_queue(struct Scsi_Host *shost, struct srp_cmd *cmd, void *info,
 }
 EXPORT_SYMBOL_GPL(srp_cmd_queue);
 
-MODULE_DESCRIPTION("SCSI RDAM Protocol lib functions");
+MODULE_DESCRIPTION("SCSI RDMA Protocol lib functions");
 MODULE_AUTHOR("FUJITA Tomonori");
 MODULE_LICENSE("GPL");
