d7d0b77a0d8ad0d28b2b96f83951a8c9ec7af92d
Sage Weil <sage@newdream.net>
2009-12-05 10:13:33 -0800

    ceph: use kref for ceph_buffer
    Signed-off-by: Sage Weil <sage@newdream.net>

--- fs/ceph/buffer.h
+++ fs/ceph/buffer.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __FS_CEPH_BUFFER_H
 #define __FS_CEPH_BUFFER_H
 
+#include <linux/kref.h>
 #include <linux/mm.h>
 #include <linux/vmalloc.h>
 #include <linux/types.h>
@@ -14,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * sizes.
  */
 struct ceph_buffer {
-	atomic_t nref;
+	struct kref kref;
 	struct kvec vec;
 	size_t alloc_len;
 	bool is_vmalloc;
@@ -25,21 +26,16 @@ int ceph_buffer_alloc(struct ceph_buffer *b, int len, gfp_t gfp);
 
 static inline struct ceph_buffer *ceph_buffer_get(struct ceph_buffer *b)
 {
-	atomic_inc(&b->nref);
+	kref_get(&b->kref);
 	return b;
 }
 
+void ceph_buffer_release(struct kref *kref);
+
 static inline void ceph_buffer_put(struct ceph_buffer *b)
 {
-	if (b && atomic_dec_and_test(&b->nref)) {
-		if (b->vec.iov_base) {
-			if (b->is_vmalloc)
-				vfree(b->vec.iov_base);
-			else
-				kfree(b->vec.iov_base);
-		}
-		kfree(b);
-	}
+	if (b)
+		kref_put(&b->kref, ceph_buffer_release);
 }
 
 static inline struct ceph_buffer *ceph_buffer_new_alloc(int len, gfp_t gfp)
