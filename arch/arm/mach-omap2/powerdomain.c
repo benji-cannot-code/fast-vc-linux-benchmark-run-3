06995f17057739b7dc2a3819f797a19cc76726ed
Paul Walmsley <paul@pwsan.com>
2009-12-08 16:33:10 -0700

    OMAP clockdomain/powerdomain: remove CONFIG_OMAP_DEBUG_{CLOCK,POWER}DOMAIN
    Avoid cluttering the Kconfig space with debug options that are rarely
    used.  These can now be enabled and disabled by patching the "#undef DEBUG"
    in the source files with "#define DEBUG", conforming to the practice for
    the rest of the linux-omap code.
    
    Also, while we're here, some lines in plat-omap/Kconfig use sets of
    leading spaces when those lines should start with tabs.  Convert most
    of them to use tabs.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Cc: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/powerdomain.c
+++ arch/arm/mach-omap2/powerdomain.c
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#ifdef CONFIG_OMAP_DEBUG_POWERDOMAIN
-# define DEBUG
-#endif
+#undef DEBUG
 
 #include <linux/kernel.h>
 #include <linux/module.h>
