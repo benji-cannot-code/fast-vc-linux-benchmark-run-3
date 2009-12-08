e029332680e89c8beb5ed62453c1dd1631243fb5
Dave Airlie <airlied@redhat.com>
2009-12-08 15:45:13 +1000

    drm/radeon/kms: fix avivo tiling regression since radeon object rework
    The object rework moved the tiling flag setup around wrongly,
    so tiling we getting setup then overwritten by fb format.
    
    Fixes regression with drm-radeon-next on rv530 laptop tiling test.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/atombios_crtc.c
+++ drivers/gpu/drm/radeon/atombios_crtc.c
@@ -600,8 +600,6 @@ int atombios_crtc_set_base(struct drm_crtc *crtc, int x, int y,
 	}
 	radeon_bo_get_tiling_flags(rbo, &tiling_flags, NULL);
 	radeon_bo_unreserve(rbo);
-	if (tiling_flags & RADEON_TILING_MACRO)
-		fb_format |= AVIVO_D1GRPH_MACRO_ADDRESS_MODE;
 
 	switch (crtc->fb->bits_per_pixel) {
 	case 8:
@@ -630,6 +628,9 @@ int atombios_crtc_set_base(struct drm_crtc *crtc, int x, int y,
 			  crtc->fb->bits_per_pixel);
 		return -EINVAL;
 	}
+
+	if (tiling_flags & RADEON_TILING_MACRO)
+		fb_format |= AVIVO_D1GRPH_MACRO_ADDRESS_MODE;
 
 	if (tiling_flags & RADEON_TILING_MICRO)
 		fb_format |= AVIVO_D1GRPH_TILED;
