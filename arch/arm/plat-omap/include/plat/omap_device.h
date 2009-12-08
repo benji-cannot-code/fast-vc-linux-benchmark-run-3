4dd43b60924cd1f1c82c037325e831a61241dc8a
Kevin Hilman <khilman@deeprootsystems.com>
2009-12-08 16:34:26 -0700

    OMAP: omap_device: track latency in nanoseconds
    Rather than having to do a usecs = nsecs / NSECS_PER_USEC to
    track latency in usecs, just track it in nanoseconds.
    
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/plat-omap/include/plat/omap_device.h
+++ arch/arm/plat-omap/include/plat/omap_device.h
@@ -51,8 +51,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @pm_lats: ptr to an omap_device_pm_latency table
  * @pm_lats_cnt: ARRAY_SIZE() of what is passed to @pm_lats
  * @pm_lat_level: array index of the last odpl entry executed - -1 if never
- * @dev_wakeup_lat: dev wakeup latency in microseconds
- * @_dev_wakeup_lat_limit: dev wakeup latency limit in usec - set by OMAP PM
+ * @dev_wakeup_lat: dev wakeup latency in nanoseconds
+ * @_dev_wakeup_lat_limit: dev wakeup latency limit in nsec - set by OMAP PM
  * @_state: one of OMAP_DEVICE_STATE_* (see above)
  * @flags: device flags
  *
