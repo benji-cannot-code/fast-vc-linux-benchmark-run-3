6e88ff7231b41f3a655e728c22b2c231dd3f0ed9
Jassi Brar <jassi.brar@samsung.com>
2009-12-09 13:29:20 +0900

    ASoC: S3C64XX: Remove unnecessary header includes
    Removed redundant header includes which make no difference to compilation.
    
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/s3c64xx-i2s.c
+++ sound/soc/s3c24xx/s3c64xx-i2s.c
@@ -16,16 +16,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
-#include <linux/delay.h>
 #include <linux/clk.h>
-#include <linux/kernel.h>
 #include <linux/gpio.h>
 #include <linux/io.h>
 
-#include <sound/core.h>
-#include <sound/pcm.h>
-#include <sound/pcm_params.h>
-#include <sound/initval.h>
 #include <sound/soc.h>
 
 #include <plat/regs-s3c2412-iis.h>
