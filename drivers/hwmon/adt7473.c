fd0ca033cdff423cc5a20cda4f6dac791b003f17
Jean Delvare <khali@linux-fr.org>
2009-12-09 20:36:02 +0100

    hwmon: (adt7475) Add support for the ADT7473
    Add support for the ADT7473 to the adt7475 driver, and mark the
    adt7473 driver for removal. The ADT7473 and ADT7475 chips are almost
    the same chip and essentially compatible, so there's no point in
    having separate drivers for them.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Cc: "Mark M. Hoffman" <mhoffman@lightlink.com>
    Cc: Hans de Goede <hdegoede@redhat.com>
    Cc: Jordan Crouse <jordan@cosmicpenguin.net>
    Cc: "Darrick J. Wong" <djwong@us.ibm.com>

--- drivers/hwmon/adt7473.c
+++ drivers/hwmon/adt7473.c
@@ -175,7 +175,6 @@ static const struct i2c_device_id adt7473_id[] = {
 	{ "adt7473", adt7473 },
 	{ }
 };
-MODULE_DEVICE_TABLE(i2c, adt7473_id);
 
 static struct i2c_driver adt7473_driver = {
 	.class		= I2C_CLASS_HWMON,
@@ -1167,6 +1166,8 @@ static int adt7473_remove(struct i2c_client *client)
 
 static int __init adt7473_init(void)
 {
+	pr_notice("The adt7473 driver is deprecated, please use the adt7475 "
+		  "driver instead\n");
 	return i2c_add_driver(&adt7473_driver);
 }
 
