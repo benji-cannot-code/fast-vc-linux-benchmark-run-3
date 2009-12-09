ce66ec39b56e018abf76e9ed249542e95115928d
Frederic Weisbecker <fweisbec@gmail.com>
2009-12-09 18:31:53 +0100

    nvram: Fix missing smp_lock.h in nvram
    The bkl has been removed from nvram_llseek() and smp_lock.h was removed
    because another patch in the same tree zapped the remaining usage of bkl
    in the same file.  But this patch must have been excluded later, then we
    still need the smp_lock.h headers for the bkl use in nvram_open().
    
    This fixes the following build error:
    
      drivers/char/nvram.c: In function ‘nvram_open’:
      drivers/char/nvram.c:332: erreur: implicit declaration of function ‘lock_kernel’
      drivers/char/nvram.c:339: erreur: implicit declaration of function ‘unlock_kernel’
      make[2]: *** [drivers/char/nvram.o] Erreur 1
      make[1]: *** [drivers/char] Erreur 2
      make: *** [drivers] Erreur 2
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/nvram.c
+++ drivers/char/nvram.c
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/io.h>
 #include <linux/uaccess.h>
+#include <linux/smp_lock.h>
 
 #include <asm/system.h>
 
