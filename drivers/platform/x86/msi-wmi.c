d9a3fb2f296f730f2efa922f3b76fc7c7c74a413
Anisse Astier <anisse@astier.eu>
2009-12-10 14:18:14 +0100

    msi-wmi: remove useless includes
    Signed-off-by: Anisse Astier <anisse@astier.eu>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/platform/x86/msi-wmi.c
+++ drivers/platform/x86/msi-wmi.c
@@ -24,14 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/types.h>
 #include <linux/input.h>
-#include <acpi/acpi_drivers.h>
 #include <linux/acpi.h>
-#include <linux/string.h>
-#include <linux/hrtimer.h>
 #include <linux/backlight.h>
 
 MODULE_AUTHOR("Thomas Renninger <trenn@suse.de>");
