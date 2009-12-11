beebb0adb26acdfe978e487584201717e56ae4ba
Luciano Coelho <luciano.coelho@nokia.com>
2009-12-11 15:40:52 +0200

    wl1271: add gpio_power file in debugfs to power the chip on and off
    Some debugging tools require the chip to be powered on before they can work.
    With these tools, we shouldn't upload the firmware nor boot the firmware
    ourselves, so this debufs file is provided.  It always contains the gpio
    power setting (0 = off, 1 = on).  To change the power setting, just write 0
    or 1 to the file.
    
    Signed-off-by: Luciano Coelho <luciano.coelho@nokia.com>
    Reviewed-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Reviewed-by: Kalle Valo <kalle.valo@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271.h
+++ drivers/net/wireless/wl12xx/wl1271.h
@@ -277,6 +277,7 @@ struct wl1271_debugfs {
 
 	struct dentry *retry_count;
 	struct dentry *excessive_retries;
+	struct dentry *gpio_power;
 };
 
 #define NUM_TX_QUEUES              4
@@ -443,6 +444,8 @@ struct wl1271 {
 	struct conf_drv_settings conf;
 
 	struct list_head list;
+
+	bool gpio_power;
 };
 
 int wl1271_plt_start(struct wl1271 *wl);
