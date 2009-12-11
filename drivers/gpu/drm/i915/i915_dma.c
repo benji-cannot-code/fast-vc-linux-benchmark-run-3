369619ad1812e58063a8515944f513889165bb57
Jesse Barnes <jbarnes@virtuousgeek.org>
2009-12-11 11:07:17 -0800

    drm/i915: only enable hotplug for detected outputs
    This patch changes around our hotplug enable code a bit to only enable
    it for ports we actually detect and initialize.  This prevents problems
    with stuck or spurious interrupts on outputs that aren't actually wired
    up, and is generally more correct.
    
    Fixes FDO bug #23183.
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1250,6 +1250,8 @@ static int i915_load_modeset_init(struct drm_device *dev,
 	if (ret)
 		goto destroy_ringbuffer;
 
+	intel_modeset_init(dev);
+
 	ret = drm_irq_install(dev);
 	if (ret)
 		goto destroy_ringbuffer;
@@ -1263,8 +1265,6 @@ static int i915_load_modeset_init(struct drm_device *dev,
 	 */
 
 	I915_WRITE(INSTPM, (1 << 5) | (1 << 21));
-
-	intel_modeset_init(dev);
 
 	drm_helper_initial_config(dev);
 
