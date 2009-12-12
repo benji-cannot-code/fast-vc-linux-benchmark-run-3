e295af62c03cca61c890db3c44bd372eac8ba05e
Santosh Shilimkar <santosh.shilimkar@ti.com>
2009-12-11 16:16:35 -0800

    OMAP4: Remove the secondary wait loop
    The secondary cores wakes up in time so the wait loop is not
    necessary anymore.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap-smp.c
+++ arch/arm/mach-omap2/omap-smp.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/init.h>
 #include <linux/device.h>
-#include <linux/jiffies.h>
 #include <linux/smp.h>
 #include <linux/io.h>
 
@@ -63,8 +62,6 @@ void __cpuinit platform_secondary_init(unsigned int cpu)
 
 int __cpuinit boot_secondary(unsigned int cpu, struct task_struct *idle)
 {
-	unsigned long timeout;
-
 	/*
 	 * Set synchronisation state between this boot processor
 	 * and the secondary one
@@ -80,10 +77,6 @@ int __cpuinit boot_secondary(unsigned int cpu, struct task_struct *idle)
 	omap_modify_auxcoreboot0(0x200, 0x0);
 	flush_cache_all();
 	smp_wmb();
-
-	timeout = jiffies + (1 * HZ);
-	while (time_before(jiffies, timeout))
-		;
 
 	/*
 	 * Now the secondary core is starting up let it run its
