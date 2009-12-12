156ecff7fee7bfa8e9acbe4adb6e5e1be0e8c1c2
Tony Lindgren <tony@atomide.com>
2009-12-11 16:16:32 -0800

    omap: mux: Add new style pin multiplexing data for 34xx
    Add new style mux data for 34xx. This should also
    work with 3630 easily by adding the processor subset
    and ball data.
    
    Note that this data is __initdata, and gets optimized
    out except for the GPIO pins if CONFIG_OMAP_MUX
    is not set.
    
    Also note that this data uses omap3630 naming for
    the SDMMC registers instead of 34xx naming with just
    MMC.
    
    Cc: Benoit Cousson <b-cousson@ti.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/mux.h
+++ arch/arm/mach-omap2/mux.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
+#include "mux34xx.h"
+
 #define OMAP_MUX_TERMINATOR	0xffff
 
 /* 34xx mux mode options for each pin. See TRM for options */
