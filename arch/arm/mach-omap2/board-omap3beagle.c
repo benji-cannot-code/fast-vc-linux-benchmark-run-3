4c49d5d55d0da38199de81114e04eac0ea70076c
Tony Lindgren <tony@atomide.com>
2009-12-11 16:16:33 -0800

    omap: mux: Remove old mux code for 34xx
    Remove old mux code for 34xx
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-omap3beagle.c
+++ arch/arm/mach-omap2/board-omap3beagle.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/common.h>
 #include <plat/gpmc.h>
 #include <plat/nand.h>
-#include <plat/mux.h>
 #include <plat/usb.h>
 #include <plat/timer-gp.h>
 
--- arch/arm/mach-omap2/board-omap3pandora.c
+++ arch/arm/mach-omap2/board-omap3pandora.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/hardware.h>
 #include <plat/mcspi.h>
 #include <plat/usb.h>
-#include <plat/mux.h>
 
 #include "mux.h"
 #include "sdram-micron-mt46h32m32lf-6.h"
