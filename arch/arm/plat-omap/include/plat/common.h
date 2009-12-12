84541c93cc999d4670a7642f1230741aecdd06e1
Tony Lindgren <tony@atomide.com>
2009-12-11 16:16:32 -0800

    omap: Split i2c platform init for mach-omap1 and mach-omap2
    Otherwise we cannot limit new mux code to mach-omap2.
    The same signal names should eventually work for other
    omaps under mach-omap2.
    
    Note that these pins don't need to be OMAP_PIN_INPUT_PULLUP,
    just OMAP_PIN_INPUT is enough.
    
    Cc: Jarkko Nikula <jhnikula@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/common.h
+++ arch/arm/plat-omap/include/plat/common.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ARCH_ARM_MACH_OMAP_COMMON_H
 #define __ARCH_ARM_MACH_OMAP_COMMON_H
 
-#include <linux/i2c.h>
+#include <plat/i2c.h>
 
 struct sys_timer;
 
@@ -37,18 +37,6 @@ extern void __iomem *gic_cpu_base_addr;
 
 extern void omap_map_common_io(void);
 extern struct sys_timer omap_timer;
-#if defined(CONFIG_I2C_OMAP) || defined(CONFIG_I2C_OMAP_MODULE)
-extern int omap_register_i2c_bus(int bus_id, u32 clkrate,
-				 struct i2c_board_info const *info,
-				 unsigned len);
-#else
-static inline int omap_register_i2c_bus(int bus_id, u32 clkrate,
-				 struct i2c_board_info const *info,
-				 unsigned len)
-{
-	return 0;
-}
-#endif
 
 /* IO bases for various OMAP processors */
 struct omap_globals {
