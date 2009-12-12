589c6d872a837c031318493a1a14e1f5316a611c
Andrew Lunn <andrew@lunn.ch>
2009-12-12 23:39:45 +0100

    Staging: batman-adv: Split originator handling parts out of routing.c
    Bug found and fixed in origional version by Linus Luessing.
    
    Signed-off-by: Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
    Signed-off-by: Linus Luessing <linus.luessing@web.de>
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/batman-adv/main.c
+++ drivers/staging/batman-adv/main.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "proc.h"
 #include "routing.h"
 #include "send.h"
+#include "originator.h"
 #include "soft-interface.h"
 #include "device.h"
 #include "translation-table.h"
