7d9845c8066ff330e71b68e54f06f05e4433a0f8
Márton Németh <nm127@freemail.hu>
2009-12-13 17:06:10 -0300

    V4L/DVB (13687): cx231xx: use NULL when pointer is needed
    The gpio field in the cx231xx_board.input structure is a pointer. Eliminate the
    following sparse warnings (see "make C=1"):
     * cx231xx-cards.c:72:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:77:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:84:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:111:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:116:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:123:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:151:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:156:13: warning: Using plain integer as NULL pointer
     * cx231xx-cards.c:163:13: warning: Using plain integer as NULL pointer
    
    Signed-off-by: Márton Németh <nm127@freemail.hu>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx-cards.c
+++ drivers/media/video/cx231xx/cx231xx-cards.c
@@ -69,19 +69,19 @@ struct cx231xx_board cx231xx_boards[] = {
 				.type = CX231XX_VMUX_TELEVISION,
 				.vmux = CX231XX_VIN_3_1,
 				.amux = CX231XX_AMUX_VIDEO,
-				.gpio = 0,
+				.gpio = NULL,
 			}, {
 				.type = CX231XX_VMUX_COMPOSITE1,
 				.vmux = CX231XX_VIN_2_1,
 				.amux = CX231XX_AMUX_LINE_IN,
-				.gpio = 0,
+				.gpio = NULL,
 			}, {
 				.type = CX231XX_VMUX_SVIDEO,
 				.vmux = CX231XX_VIN_1_1 |
 					(CX231XX_VIN_1_2 << 8) |
 					CX25840_SVIDEO_ON,
 				.amux = CX231XX_AMUX_LINE_IN,
-				.gpio = 0,
+				.gpio = NULL,
 			}
 		},
 	},
@@ -108,19 +108,19 @@ struct cx231xx_board cx231xx_boards[] = {
 				.type = CX231XX_VMUX_TELEVISION,
 				.vmux = CX231XX_VIN_3_1,
 				.amux = CX231XX_AMUX_VIDEO,
-				.gpio = 0,
+				.gpio = NULL,
 			}, {
 				.type = CX231XX_VMUX_COMPOSITE1,
 				.vmux = CX231XX_VIN_2_1,
 				.amux = CX231XX_AMUX_LINE_IN,
-				.gpio = 0,
+				.gpio = NULL,
 			}, {
 				.type = CX231XX_VMUX_SVIDEO,
 				.vmux = CX231XX_VIN_1_1 |
 					(CX231XX_VIN_1_2 << 8) |
 					CX25840_SVIDEO_ON,
 				.amux = CX231XX_AMUX_LINE_IN,
-				.gpio = 0,
+				.gpio = NULL,
 			}
 		},
 	},
@@ -148,19 +148,19 @@ struct cx231xx_board cx231xx_boards[] = {
 				.type = CX231XX_VMUX_TELEVISION,
 				.vmux = CX231XX_VIN_3_1,
 				.amux = CX231XX_AMUX_VIDEO,
-				.gpio = 0,
+				.gpio = NULL,
 			}, {
 				.type = CX231XX_VMUX_COMPOSITE1,
 				.vmux = CX231XX_VIN_2_1,
 				.amux = CX231XX_AMUX_LINE_IN,
-				.gpio = 0,
+				.gpio = NULL,
 			}, {
 				.type = CX231XX_VMUX_SVIDEO,
 				.vmux = CX231XX_VIN_1_1 |
 					(CX231XX_VIN_1_2 << 8) |
 					CX25840_SVIDEO_ON,
 				.amux = CX231XX_AMUX_LINE_IN,
-				.gpio = 0,
+				.gpio = NULL,
 			}
 		},
 	},
