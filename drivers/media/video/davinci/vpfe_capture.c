af289101712daf0f457292fd0b10e85441b52de5
Huang Weiyi <weiyi.huang@gmail.com>
2009-12-13 11:05:21 -0300

    V4L/DVB: Davinci VPFE Capture: remove unused #include <linux/version.h>
    Remove unused #include <linux/version.h>('s) in
      drivers/media/video/davinci/vpfe_capture.c
    
    Cc: Muralidharan Karicheri <m-karicheri2@ti.com>
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/davinci/vpfe_capture.c
+++ drivers/media/video/davinci/vpfe_capture.c
@@ -71,7 +71,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/interrupt.h>
-#include <linux/version.h>
 #include <media/v4l2-common.h>
 #include <linux/io.h>
 #include <media/davinci/vpfe_capture.h>
