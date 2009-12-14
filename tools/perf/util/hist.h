21aab8871ed9512ab54a9c4772cdd8133bb9d471
Arnaldo Carvalho de Melo <acme@redhat.com>
2009-12-14 11:37:11 -0200

    perf tools: No need for three rb_trees for sorting hist entries
    All hist entries are in only one of them, so use just one and a
    temporary rb_root while sorting/collapsing.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Frédéric Weisbecker <fweisbec@gmail.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Paul Mackerras <paulus@samba.org>
    LKML-Reference: <1260797831-11220-1-git-send-email-acme@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/hist.h
+++ tools/perf/util/hist.h
@@ -26,16 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sort.h"
 
 extern struct rb_root hist;
-extern struct rb_root collapse_hists;
-extern struct rb_root output_hists;
 extern int callchain;
 extern struct callchain_param callchain_param;
-extern unsigned long total;
-extern unsigned long total_mmap;
-extern unsigned long total_comm;
-extern unsigned long total_fork;
-extern unsigned long total_unknown;
-extern unsigned long total_lost;
 
 struct hist_entry *__hist_entry__add(struct addr_location *al,
 				     struct symbol *parent,
@@ -43,9 +35,7 @@ struct hist_entry *__hist_entry__add(struct addr_location *al,
 extern int64_t hist_entry__cmp(struct hist_entry *, struct hist_entry *);
 extern int64_t hist_entry__collapse(struct hist_entry *, struct hist_entry *);
 extern void hist_entry__free(struct hist_entry *);
-extern void collapse__insert_entry(struct hist_entry *);
 extern void collapse__resort(void);
-extern void output__insert_entry(struct hist_entry *, u64);
 extern void output__resort(u64);
 
 #endif	/* __PERF_HIST_H */
