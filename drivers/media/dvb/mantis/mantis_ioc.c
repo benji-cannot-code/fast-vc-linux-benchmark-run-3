901e7b5a29f0a4fb932db42bad0cd5d5b609a8e2
Manu Abraham <abraham.manu@gmail.com>
2009-12-15 06:17:54 -0300

    V4L/DVB (13802): [Mantis/Hopper] Fix all build related warnings
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/mantis/mantis_ioc.c
+++ drivers/media/dvb/mantis/mantis_ioc.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/i2c.h>
 
-#include <asm/irq.h>
 #include <linux/signal.h>
 #include <linux/sched.h>
 #include <linux/interrupt.h>
