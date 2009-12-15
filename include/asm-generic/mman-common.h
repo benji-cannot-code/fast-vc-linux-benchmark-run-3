1f1b73999fa8e66bce7fcd46e63ef623b02027cf
Jie Zhang <jie.zhang@analog.com>
2009-12-14 18:00:02 -0800

    nommu: fix malloc performance by adding uninitialized flag
    The NOMMU code currently clears all anonymous mmapped memory.  While this
    is what we want in the default case, all memory allocation from userspace
    under NOMMU has to go through this interface, including malloc() which is
    allowed to return uninitialized memory.  This can easily be a significant
    performance penalty.  So for constrained embedded systems were security is
    irrelevant, allow people to avoid clearing memory unnecessarily.
    
    This also alters the ELF-FDPIC binfmt such that it obtains uninitialised
    memory for the brk and stack region.
    
    Signed-off-by: Jie Zhang <jie.zhang@analog.com>
    Signed-off-by: Robin Getz <rgetz@blackfin.uclinux.org>
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Paul Mundt <lethal@linux-sh.org>
    Acked-by: Greg Ungerer <gerg@snapgear.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-generic/mman-common.h
+++ include/asm-generic/mman-common.h
@@ -20,6 +20,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAP_TYPE	0x0f		/* Mask for type of mapping */
 #define MAP_FIXED	0x10		/* Interpret addr exactly */
 #define MAP_ANONYMOUS	0x20		/* don't use a file */
+#ifdef CONFIG_MMAP_ALLOW_UNINITIALIZED
+# define MAP_UNINITIALIZED 0x4000000	/* For anonymous mmap, memory could be uninitialized */
+#else
+# define MAP_UNINITIALIZED 0x0		/* Don't support this flag */
+#endif
 
 #define MS_ASYNC	1		/* sync memory asynchronously */
 #define MS_INVALIDATE	2		/* invalidate the caches */
