64cf36db5b14fc77774220e612dabef2b4832d9d
Balaji T K <balajitk@ti.com>
2009-12-15 20:09:02 +0530

    mfd: twl: fix twl4030 rename for remaining driver, board files
    Recent drivers/mfd/twl4030* renames to twl broke compile for
    various boards as the series was missing a patch to change
    the board-*.c files.
    
    This patch renames include twl4030.h to include twl.h
    and also renames twl4030_i2c_ routines.
    
    Signed-off-by: Balaji T K <balajitk@ti.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Reviewed-by: Felipe Balbi <felipe.balbi@nokia.com>
    Cc: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-rx51-peripherals.c
+++ arch/arm/mach-omap2/board-rx51-peripherals.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spi/spi.h>
 #include <linux/spi/wl12xx.h>
 #include <linux/i2c.h>
-#include <linux/i2c/twl4030.h>
+#include <linux/i2c/twl.h>
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/regulator/machine.h>
