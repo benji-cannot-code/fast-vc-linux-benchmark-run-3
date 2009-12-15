fd75bc9640ed0c01a24dbfdd43187e19f866642b
Ping Cheng <pingc@wacom.com>
2009-12-15 00:35:24 -0800

    Input: wacom - ensure the device is initialized properly upon resume
    Call wacom_query_tablet_data() from wacom_resume() so the device will be
    switched to Wacom mode upon resume. Devices that require this are: regular
    tablets and two finger touch devices.
    
    Signed-off-by: Ping Cheng <pingc@wacom.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/tablet/wacom_sys.c
+++ drivers/input/tablet/wacom_sys.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * drivers/input/tablet/wacom_sys.c
  *
- *  USB Wacom Graphire and Wacom Intuos tablet support - system specific code
+ *  USB Wacom tablet support - system specific code
  */
 
 /*
@@ -563,9 +563,10 @@ static int wacom_resume(struct usb_interface *intf)
 	int rv;
 
 	mutex_lock(&wacom->lock);
-	if (wacom->open)
+	if (wacom->open) {
 		rv = usb_submit_urb(wacom->irq, GFP_NOIO);
-	else
+		wacom_query_tablet_data(intf);
+	} else
 		rv = 0;
 	mutex_unlock(&wacom->lock);
 
