342273d126bb391a6594df754755707b8489b2ba
Roel Kluin <roel.kluin@gmail.com>
2009-12-14 17:57:49 -0800

    page-types: unsigned cannot be less than 0 in add_page()
    If not signed, testing of the read() return value in this function
    will not work.
    
    Signed-off-by: Roel Kluin <roel.kluin@gmail.com>
    Cc: Wu Fengguang <fengguang.wu@intel.com>
    Cc: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- Documentation/vm/page-types.c
+++ Documentation/vm/page-types.c
@@ -561,7 +561,7 @@ static void walk_pfn(unsigned long voffset,
 {
 	uint64_t buf[KPAGEFLAGS_BATCH];
 	unsigned long batch;
-	unsigned long pages;
+	long pages;
 	unsigned long i;
 
 	while (count) {
