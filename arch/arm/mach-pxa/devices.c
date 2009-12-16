d94013d3774771a9a6f7dc4b4c34772292f1b429
Eric Miao <eric.y.miao@gmail.com>
2009-12-16 21:35:52 +0800

    [ARM] pxa: fix no reference of cpu_is_pxa25x() in devices.c
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-pxa/devices.c
+++ arch/arm/mach-pxa/devices.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/pxa2xx_spi.h>
 #include <mach/camera.h>
 #include <mach/audio.h>
+#include <mach/hardware.h>
 #include <plat/i2c.h>
 #include <plat/pxa3xx_nand.h>
 
