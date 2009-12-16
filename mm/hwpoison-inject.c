b9f6a4b99b74995040fc7726ad67bcc8a8a83039
Andi Kleen <andi@firstfloor.org>
2009-12-16 12:20:01 +0100

    HWPOISON: Don't do early filtering if filter is disabled
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- mm/hwpoison-inject.c
+++ mm/hwpoison-inject.c
@@ -19,6 +19,8 @@ static int hwpoison_inject(void *data, u64 val)
 	if (!capable(CAP_SYS_ADMIN))
 		return -EPERM;
 
+	if (!hwpoison_filter_enable)
+		goto inject;
 	if (!pfn_valid(pfn))
 		return -ENXIO;
 
@@ -49,6 +51,7 @@ static int hwpoison_inject(void *data, u64 val)
 	if (err)
 		return 0;
 
+inject:
 	printk(KERN_INFO "Injecting memory failure at pfn %lx\n", pfn);
 	return __memory_failure(pfn, 18, MF_COUNT_INCREASED);
 }
