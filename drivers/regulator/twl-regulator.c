e37a26405d81e6df601a1a414dbf9271feaa6320
Juha Keski-Saari <ext-juha.1.keski-saari@nokia.com>
2009-12-16 15:27:58 +0200

    twl-regulator: Restore REMAP configuration in regulator probe
    This change ensures the regulator REMAP register configuration is in a known
    state so state transitions will function as intended regardless of
    possible bootloader effects on it
    
    Signed-off-by: Juha Keski-Saari <ext-juha.1.keski-saari@nokia.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- drivers/regulator/twl-regulator.c
+++ drivers/regulator/twl-regulator.c
@@ -577,6 +577,9 @@ static int twlreg_probe(struct platform_device *pdev)
 	}
 	platform_set_drvdata(pdev, rdev);
 
+	twlreg_write(info, TWL_MODULE_PM_RECEIVER, VREG_REMAP,
+						info->remap);
+
 	/* NOTE:  many regulators support short-circuit IRQs (presentable
 	 * as REGULATOR_OVER_CURRENT notifications?) configured via:
 	 *  - SC_CONFIG
