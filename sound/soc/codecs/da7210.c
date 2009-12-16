3929e64496c13c01d4e2dbf7c9f015521e6677b4
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-12-16 20:36:37 +0000

    ASoC: Fix build of DA7210
    DAC_VOICE_EN was not defined - looks to have been overly enthusiastically
    deleted from a previous revision of the patch, pull the value from v1.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/da7210.c
+++ sound/soc/codecs/da7210.c
@@ -82,6 +82,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DA7210_ADC_L_EN			(1 << 3)
 #define DA7210_ADC_R_EN			(1 << 7)
 
+/* DAC_HPF fields */
+#define DA7210_DAC_VOICE_EN		(1 << 7)
+
 /* DAC_SEL bit fields */
 #define DA7210_DAC_L_SRC_DAI_L		(4 << 0)
 #define DA7210_DAC_L_EN			(1 << 3)
