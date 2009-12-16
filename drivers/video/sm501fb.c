55e637f94bdb2fa913e1919d5b5f60b2be1dca82
Ben Dooks <ben@simtec.co.uk>
2009-12-15 16:46:34 -0800

    sm501: fix use of old <asm/io.h> instead of <linux/io.h>
    Fix the old style use of <asm/io.h> by replacing it with <linux/io.h>.
    
    Signed-off-by: Ben Dooks <ben@simtec.co.uk>
    Signed-off-by: Simtec Linux Team <linux@simtec.co.uk>
    Cc: Vincent Sanders <vince@simtec.co.uk>
    Cc: Krzysztof Helt <krzysztof.h1@poczta.fm>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/sm501fb.c
+++ drivers/video/sm501fb.c
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/console.h>
+#include <linux/io.h>
 
-#include <asm/io.h>
 #include <asm/uaccess.h>
 #include <asm/div64.h>
 
