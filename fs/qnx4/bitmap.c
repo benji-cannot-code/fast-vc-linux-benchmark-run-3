0d9ce866f0e66dfa5d0705418c7e741e87f23088
Anders Larsen <al@alarsen.net>
2009-12-15 16:48:23 -0800

    qnx4fs: remove remains of the (defunct) write support
    commit 945ffe54bbd56ceed62de3b908800fd7c6ffb284 ("qnx4: remove write support") removed the (defunct)
    write support but missed a chunk of related, dead code.
    
    Signed-off-by: Anders Larsen <al@alarsen.net>
    Cc: Jiri Kosina <jkosina@suse.cz>
    Acked-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/qnx4/bitmap.c
+++ fs/qnx4/bitmap.c
@@ -18,13 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include "qnx4.h"
 
-#if 0
-int qnx4_new_block(struct super_block *sb)
-{
-	return 0;
-}
-#endif  /*  0  */
-
 static void count_bits(register const char *bmPart, register int size,
 		       int *const tf)
 {
