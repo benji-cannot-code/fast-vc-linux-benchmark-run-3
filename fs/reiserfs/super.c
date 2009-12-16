8aadd957cc9b8f843f52e6d807dc4440feabfe07
Alexey Dobriyan <adobriyan@gmail.com>
2009-12-15 16:46:52 -0800

    reiserfs: remove /proc/fs/reiserfs/version
    /proc/fs/reiserfs/version is on the way of removing ->read_proc interface.
     It's empty however, so simply remove it instead of doing dummy
    conversion.  It's hard to see what information userspace can extract from
    empty file.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Jeff Mahoney <jeffm@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/reiserfs/super.c
+++ fs/reiserfs/super.c
@@ -2223,8 +2223,6 @@ static int __init init_reiserfs_fs(void)
 	}
 
 	reiserfs_proc_info_global_init();
-	reiserfs_proc_register_global("version",
-				      reiserfs_global_version_in_proc);
 
 	ret = register_filesystem(&reiserfs_fs_type);
 
@@ -2232,7 +2230,6 @@ static int __init init_reiserfs_fs(void)
 		return 0;
 	}
 
-	reiserfs_proc_unregister_global("version");
 	reiserfs_proc_info_global_done();
 	destroy_inodecache();
 
@@ -2241,7 +2238,6 @@ static int __init init_reiserfs_fs(void)
 
 static void __exit exit_reiserfs_fs(void)
 {
-	reiserfs_proc_unregister_global("version");
 	reiserfs_proc_info_global_done();
 	unregister_filesystem(&reiserfs_fs_type);
 	destroy_inodecache();
