c6f0f7db054925f1376c968e5f512c4b48f64be1
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2009-12-16 19:07:20 +0100

    imx35: define and use MX35_IO_ADDRESS
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Cc: Sascha Hauer <kernel@pengutronix.de>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Jürgen Beisert <j.beisert@pengutronix.de>
    Cc: Rabin Vincent <rabin@rab.in>
    Cc: Valentin Longchamp <valentin.longchamp@epfl.ch>

--- arch/arm/plat-mxc/include/mach/mx35.h
+++ arch/arm/plat-mxc/include/mach/mx35.h
@@ -105,6 +105,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MX35_NFC_BASE_ADDR		0xbb000000
 #define MX35_PCMCIA_MEM_BASE_ADDR	0xbc000000
 
+#define MX35_IO_ADDRESS(x) (						\
+	IMX_IO_ADDRESS(x, MX35_AIPS1) ?:				\
+	IMX_IO_ADDRESS(x, MX35_AIPS2) ?:				\
+	IMX_IO_ADDRESS(x, MX35_AVIC) ?:					\
+	IMX_IO_ADDRESS(x, MX35_X_MEMC) ?:				\
+	IMX_IO_ADDRESS(x, MX35_SPBA0))
+
 /*
  * Interrupt numbers
  */
