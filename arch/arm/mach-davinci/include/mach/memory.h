739f331fe3822114093788f0f5cd21755f4523e6
Sekhar Nori <nsekhar@ti.com>
2009-12-17 18:29:31 +0530

    davinci: add power management support
    This patch adds core power management (suspend-to-RAM)
    support for DaVinci SoCs.
    
    The code depends on the the "deepsleep" feature to suspend
    the SoC and saves power by gating the input clock.
    
    The wakeup can be based on an external event as supported
    by the SoC.
    
    Assembly code (in sleep.S) is added to aid gating DDR2
    clocks. Code doing this work should not be accessing DDR2.
    The assembly code is relocated to SRAM by the code in pm.c
    
    The support has been validated on DA850/OMAP-L138 only
    though the code is (hopefully) generic enough that other
    SoCs supporting deepsleep feature simply requires SoC
    specific code to start using this driver.
    
    Note that all the device drivers don't support suspend/resume
    still and are being worked on.
    
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/include/mach/memory.h
+++ arch/arm/mach-davinci/include/mach/memory.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DDR2_SDRCR_OFFSET	0xc
 #define DDR2_SRPD_BIT		BIT(23)
+#define DDR2_MCLKSTOPEN_BIT	BIT(30)
 #define DDR2_LPMODEN_BIT	BIT(31)
 
 /*
