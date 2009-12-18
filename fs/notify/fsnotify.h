14b3f11b32ccee951c70eed3c54933e3be493a94
Eric Paris <eparis@redhat.com>
2009-12-17 21:24:23 -0500

    fsnotify: rename fsnotify_groups to fsnotify_inode_groups
    Simple renaming patch.  fsnotify is about to support mount point listeners
    so I am renaming fsnotify_groups and fsnotify_mask to indicate these are lists
    used only for groups which have watches on inodes.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- fs/notify/fsnotify.h
+++ fs/notify/fsnotify.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* protects reads of fsnotify_groups */
 extern struct srcu_struct fsnotify_grp_srcu;
-/* all groups which receive fsnotify events */
-extern struct list_head fsnotify_groups;
-/* all bitwise OR of all event types (FS_*) for all fsnotify_groups */
-extern __u32 fsnotify_mask;
+/* all groups which receive inode fsnotify events */
+extern struct list_head fsnotify_inode_groups;
+/* all bitwise OR of all event types (FS_*) for all fsnotify_inode_groups */
+extern __u32 fsnotify_inode_mask;
 
 /* destroy all events sitting in this groups notification queue */
 extern void fsnotify_flush_notify(struct fsnotify_group *group);
