6cbaac8304daa32b7826630f8b8711afbaf2a7f6
Andrew Jones <drjones@redhat.com>
2009-12-18 10:31:31 +0100

    xen: support CONFIG_MAXSMP
    The MAXSMP config option requires CPUMASK_OFFSTACK, which in turn
    requires we init the memory for the maps while we bring up the cpus.
    MAXSMP also increases NR_CPUS to 4096. This increase in size exposed an
    issue in the argument construction for multicalls from
    xen_flush_tlb_others. The args should only need space for the actual
    number of cpus.
    
    Also in 2.6.39 it exposes a bootup problem.
    
    BUG: unable to handle kernel NULL pointer dereference at           (null)
    IP: [<ffffffff8157a1d3>] set_cpu_sibling_map+0x123/0x30d
    ...
    Call Trace:
    [<ffffffff81039a3f>] ? xen_restore_fl_direct_reloc+0x4/0x4
    [<ffffffff819dc4db>] xen_smp_prepare_cpus+0x36/0x135
    ..
    
    CC: stable@kernel.org
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    [v2: Updated to compile on 3.0]
    [v3: Updated to compile when CONFIG_SMP is not defined]
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- arch/x86/xen/mmu.c
+++ arch/x86/xen/mmu.c
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/page.h>
 #include <asm/init.h>
 #include <asm/pat.h>
+#include <asm/smp.h>
 
 #include <asm/xen/hypercall.h>
 #include <asm/xen/hypervisor.h>
@@ -1232,7 +1233,7 @@ static void xen_flush_tlb_others(const struct cpumask *cpus,
 {
 	struct {
 		struct mmuext_op op;
-		DECLARE_BITMAP(mask, NR_CPUS);
+		DECLARE_BITMAP(mask, num_processors);
 	} *args;
 	struct multicall_space mcs;
 
