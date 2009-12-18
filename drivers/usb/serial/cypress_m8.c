7862eab6a478c60eed9ed49fc8c8293581878d5a
Mike Frysinger <vapier@gentoo.org>
2009-12-18 16:33:01 -0500

    USB: cypress_m8: stop using USB debug driver config
    The USB_SERIAL_DEBUG Kconfig is for the USB serial debug driver, not for
    generically enabling debug output in random USB serial drivers.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/cypress_m8.c
+++ drivers/usb/serial/cypress_m8.c
@@ -71,11 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cypress_m8.h"
 
 
-#ifdef CONFIG_USB_SERIAL_DEBUG
-	static int debug = 1;
-#else
-	static int debug;
-#endif
+static int debug;
 static int stats;
 static int interval;
 
