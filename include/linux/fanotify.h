478f94ab716cb1c3904eb56aae966f7cc0e3ff53
Eric Paris <eparis@redhat.com>
2009-12-17 21:24:33 -0500

    fanotify: allow ignored_masks to survive modify
    Some users may want to truely ignore an inode even if it has been modified.
    Say you are wanting a mount which contains a log file and you really don't
    want any notification about that file.  This patch allows the listener to
    do that.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- include/linux/fanotify.h
+++ include/linux/fanotify.h
@@ -32,13 +32,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FAN_MARK_ONLYDIR	0x00000008
 #define FAN_MARK_MOUNT		0x00000010
 #define FAN_MARK_IGNORED_MASK	0x00000020
+#define FAN_MARK_IGNORED_SURV_MODIFY	0x00000040
 
 #define FAN_ALL_MARK_FLAGS	(FAN_MARK_ADD |\
 				 FAN_MARK_REMOVE |\
 				 FAN_MARK_DONT_FOLLOW |\
 				 FAN_MARK_ONLYDIR |\
 				 FAN_MARK_MOUNT |\
-				 FAN_MARK_IGNORED_MASK)
+				 FAN_MARK_IGNORED_MASK |\
+				 FAN_MARK_IGNORED_SURV_MODIFY)
 
 /*
  * All of the events - we build the list by hand so that we can add flags in
