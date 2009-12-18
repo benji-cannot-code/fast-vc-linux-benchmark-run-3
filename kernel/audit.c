cdfc853ad2cd84aaa4c30226424858ef45888be7
Eric Paris <eparis@redhat.com>
2009-12-17 20:12:04 -0500

    Audit: clean up the audit_watch split
    No real changes, just cleanup to the audit_watch split patch which we done
    with minimal code changes for easy review.  Now fix interfaces to make
    things work better.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/netlink.h>
 #include <linux/skbuff.h>
 #include <linux/netlink.h>
-#include <linux/inotify.h>
 #include <linux/freezer.h>
 #include <linux/tty.h>
 
