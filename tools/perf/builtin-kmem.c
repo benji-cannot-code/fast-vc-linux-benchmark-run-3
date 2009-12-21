ec437c81cc8e0eded0befc58c8a89a49c95429c8
Wenji Huang <wenji.huang@oracle.com>
2009-12-21 17:52:55 +0800

    perf kmem: Fix statistics typo
    Replace bytes_req with bytes_alloc.
    
    Signed-off-by: Wenji Huang <wenji.huang@oracle.com>
    Reviewed-by: Li Zefan <lizf@cn.fujitsu.com>
    Cc: acme@redhat.com
    LKML-Reference: <1261389175-2227-1-git-send-email-wenji.huang@oracle.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/builtin-kmem.c
+++ tools/perf/builtin-kmem.c
@@ -138,7 +138,7 @@ static void insert_alloc_stat(unsigned long call_site, unsigned long ptr,
 	if (data && data->ptr == ptr) {
 		data->hit++;
 		data->bytes_req += bytes_req;
-		data->bytes_alloc += bytes_req;
+		data->bytes_alloc += bytes_alloc;
 	} else {
 		data = malloc(sizeof(*data));
 		if (!data)
@@ -178,7 +178,7 @@ static void insert_caller_stat(unsigned long call_site,
 	if (data && data->call_site == call_site) {
 		data->hit++;
 		data->bytes_req += bytes_req;
-		data->bytes_alloc += bytes_req;
+		data->bytes_alloc += bytes_alloc;
 	} else {
 		data = malloc(sizeof(*data));
 		if (!data)
