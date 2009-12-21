ec203552b03898ac77ec5675827a141d237b4665
Theodore Ts'o <tytso@mit.edu>
2009-12-21 10:56:09 -0500

    ext4: add module aliases for ext2 and ext3
    Add module aliases for ext2 and ext3 when CONFIG_EXT4_USE_FOR_EXT23 is
    set.  This makes the existing user-space stuff like mkinitrd working
    as is.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -4006,6 +4006,7 @@ static inline void unregister_as_ext2(void)
 {
 	unregister_filesystem(&ext2_fs_type);
 }
+MODULE_ALIAS("ext2");
 #else
 static inline void register_as_ext2(void) { }
 static inline void unregister_as_ext2(void) { }
@@ -4032,6 +4033,7 @@ static inline void unregister_as_ext3(void)
 {
 	unregister_filesystem(&ext3_fs_type);
 }
+MODULE_ALIAS("ext3");
 #else
 static inline void register_as_ext3(void) { }
 static inline void unregister_as_ext3(void) { }
