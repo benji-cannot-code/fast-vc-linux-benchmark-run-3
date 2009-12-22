6f81536ad493ac188c55189760ad69d7ea2504f8
Peter Huewe <peterhuewe@gmx.de>
2009-12-22 05:38:14 -0300

    V4L/DVB (13947): add __init/__exit macros to drivers/media/dvb/bt8xx/bt878.c
    Trivial patch which adds the __init/__exit macros to the module_init/
    module_exit functions of
    
    drivers/media/dvb/bt8xx/bt878.c
    
    Please have a look at the small patch and either pull it through
    your tree, or please ack' it so Jiri can pull it through the trivial
    tree.
    
    Patch against linux-next-tree, 22. Dez 08:38:18 CET 2009
    but also present in linus tree.
    
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>
    Acked-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/bt8xx/bt878.c
+++ drivers/media/dvb/bt8xx/bt878.c
@@ -583,7 +583,7 @@ static int bt878_pci_driver_registered;
 /* Module management functions */
 /*******************************/
 
-static int bt878_init_module(void)
+static int __init bt878_init_module(void)
 {
 	bt878_num = 0;
 	bt878_pci_driver_registered = 0;
@@ -601,7 +601,7 @@ static int bt878_init_module(void)
 	return pci_register_driver(&bt878_pci_driver);
 }
 
-static void bt878_cleanup_module(void)
+static void __exit bt878_cleanup_module(void)
 {
 	if (bt878_pci_driver_registered) {
 		bt878_pci_driver_registered = 0;
