c3dc98957c5ed53400355ac8c0722359be9b4d0e
Helge Deller <deller@gmx.de>
2009-12-22 22:50:33 +0000

    parisc: wire up sys_recvmmsg
    Signed-off-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Kyle McMartin <kyle@mcmartin.ca>

--- arch/parisc/include/asm/unistd.h
+++ arch/parisc/include/asm/unistd.h
@@ -812,8 +812,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_pwritev		(__NR_Linux + 316)
 #define __NR_rt_tgsigqueueinfo	(__NR_Linux + 317)
 #define __NR_perf_event_open	(__NR_Linux + 318)
+#define __NR_recvmmsg		(__NR_Linux + 319)
 
-#define __NR_Linux_syscalls	(__NR_perf_event_open + 1)
+#define __NR_Linux_syscalls	(__NR_recvmmsg + 1)
 
 
 #define __IGNORE_select		/* newselect */
