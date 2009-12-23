0c70ac94f895d97047cc428ecf66a98992af36be
Matthias Benesch <twoof7@freenet.de>
2009-12-23 05:55:02 -0300

    V4L/DVB: ngene: Added module parameter "one_adapter"
    If parameter "one_adapter" is set, only one adapter per device will be attached.
    Otherwise an adapter for every frontend will be attached.
    
    Signed-off-by: Matthias Benesch <twoof7@freenet.de>
    Signed-off-by: Oliver Endriss <o.endriss@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/ngene/ngene.h
+++ drivers/media/dvb/ngene/ngene.h
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _NGENE_H_
 #define _NGENE_H_
 
-/*#define ONE_ADAPTER*/
-
 #include <linux/types.h>
 #include <linux/sched.h>
 #include <linux/interrupt.h>
@@ -650,9 +648,6 @@ struct ngene_channel {
 	struct dmx_frontend   mem_frontend;
 	int                   users;
 	struct video_device  *v4l_dev;
-#ifndef ONE_ADAPTER
-	struct dvb_adapter    dvb_adapter;
-#endif
 	struct tasklet_struct demux_tasklet;
 
 	struct SBufferHeader *nextBuffer;
@@ -729,9 +724,6 @@ struct ngene {
 	struct pci_dev       *pci_dev;
 	unsigned char        *iomem;
 
-#ifdef ONE_ADAPTER
-	struct dvb_adapter    dvb_adapter;
-#endif
 	/*struct i2c_adapter  i2c_adapter;*/
 
 	u32                   device_version;
@@ -765,6 +757,7 @@ struct ngene {
 	int                   i2c_current_bus;
 	spinlock_t            cmd_lock;
 
+	struct dvb_adapter    adapter[MAX_STREAM];
 	struct ngene_channel  channel[MAX_STREAM];
 
 	struct ngene_info    *card_info;
