13cc16ded2b9d75d827ebbf46dc522765106ae0a
Zhu Yi <yi.zhu@intel.com>
2009-12-24 15:31:10 +0800

    iwlwifi: remove linux/utsrelease.h dependency
    Commit 250cce26d5d03337aec4ff8405121f026adb4a89 uses UTS_RELEASE
    as the the in-tree iwlwifi driver version. However the inclusion
    of generated/utsrelease.h makes it a unpleasant behaviour to
    recompile the driver everytime when utsrelease.h is updated. In
    fact, the driver module is already built with the UTS_RELEASE
    information via vermagic of modinfo. Mark the in-tree driver
    with the version string "in-tree" to distinguish with those old
    out-of-tree drivers.
    
    Reported-by: David Miller <davem@davemloft.net>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-core.h
+++ drivers/net/wireless/iwlwifi/iwl-core.h
@@ -64,8 +64,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_core_h__
 #define __iwl_core_h__
 
-#include <linux/utsrelease.h>
-
 /************************
  * forward declarations *
  ************************/
@@ -73,7 +71,7 @@ struct iwl_host_cmd;
 struct iwl_cmd;
 
 
-#define IWLWIFI_VERSION UTS_RELEASE "-k"
+#define IWLWIFI_VERSION "in-tree:"
 #define DRV_COPYRIGHT	"Copyright(c) 2003-2009 Intel Corporation"
 #define DRV_AUTHOR     "<ilw@linux.intel.com>"
 
