fa25081f3ff5626de31c3a6f4a4663dc0974f49f
Simon Horman <horms@verge.net.au>
2009-12-24 22:41:13 +1100

    Staging: DT3155: Use pci_get_device()
    The use of pci_find_device() is deprecated.
    
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Cc: Scott Smedley <ss@aao.gov.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/dt3155/dt3155_drv.c
+++ drivers/staging/dt3155/dt3155_drv.c
@@ -964,7 +964,7 @@ static int find_PCI (void)
   unsigned long base;
   unsigned char irq;
 
-  while ((pci_dev = pci_find_device
+  while ((pci_dev = pci_get_device
 	  (DT3155_VENDORID, DT3155_DEVICEID, pci_dev)) != NULL)
     {
       pci_index ++;
@@ -984,7 +984,7 @@ static int find_PCI (void)
 	       "for %d devices\n"
 	       "DT3155: Please change MAXBOARDS in dt3155.h\n",
 	       pci_index, MAXBOARDS);
-	return DT_3155_FAILURE;
+	goto err;
       }
 
       /* Now, just go out and make sure that this/these device(s) is/are
@@ -993,7 +993,7 @@ static int find_PCI (void)
 					  (u_int *) &base)))
 	{
 	  printk("DT3155: Was not able to find device \n");
-	  return DT_3155_FAILURE;
+	  goto err;
 	}
 
       DT_3155_DEBUG_MSG("DT3155: Base address 0 for device is %lx \n", base);
@@ -1008,13 +1008,13 @@ static int find_PCI (void)
       if ( !dt3155_lbase[pci_index-1] )
 	{
 	  printk("DT3155: Unable to remap control registers\n");
-	  return DT_3155_FAILURE;
+	  goto err;
 	}
 
       if ( (error = pci_read_config_byte( pci_dev, PCI_INTERRUPT_LINE, &irq)) )
 	{
 	  printk("DT3155: Was not able to find device \n");
-	  return DT_3155_FAILURE;
+	  goto err;
 	}
 
       DT_3155_DEBUG_MSG("DT3155: IRQ is %d \n",irq);
@@ -1030,6 +1030,10 @@ static int find_PCI (void)
   ndevices = pci_index;
 
   return DT_3155_SUCCESS;
+
+err:
+  pci_dev_put(pci_dev);
+  return DT_3155_FAILURE;
 }
 
 u_long allocatorAddr = 0;
