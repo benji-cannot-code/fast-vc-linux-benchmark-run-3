c06785c72702418b17ab4f1648f218f5d617f2f4
Devin Heitmueller <dheitmueller@kernellabs.com>
2009-12-27 17:50:43 -0300

    [media] xc4000: turn off debug logging by default
    Don't dump debug into to dmesg by default (something I had enabled during
    bringup of the xc4000 driver).
    
    Signed-off-by: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/tuners/xc4000.c
+++ drivers/media/common/tuners/xc4000.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tuner-i2c.h"
 #include "tuner-xc2028-types.h"
 
-static int debug=1;
+static int debug;
 module_param(debug, int, 0644);
 MODULE_PARM_DESC(debug, "Turn on/off debugging (default:off).");
 
