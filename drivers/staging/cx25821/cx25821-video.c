56d80ff61631d289eac011188a53a8271a06e2fc
Dan Carpenter <error27@gmail.com>
2009-12-28 19:00:24 +0200

    Staging: cx25821: off by one in cx25821_enum_input()
    The INPUT(n) macro indexes an array of size 2.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/cx25821/cx25821-video.c
+++ drivers/staging/cx25821/cx25821-video.c
@@ -877,7 +877,7 @@ int cx25821_enum_input(struct cx25821_dev *dev, struct v4l2_input *i)
 	dprintk(1, "%s()\n", __func__);
 
 	n = i->index;
-	if (n > 2)
+	if (n >= 2)
 		return -EINVAL;
 
 	if (0 == INPUT(n)->type)
