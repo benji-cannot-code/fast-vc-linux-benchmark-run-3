92e62e34d4bf7da327a61b70ed045594fc1a9e7e
Dominik Brodowski <linux@dominikbrodowski.net>
2010-01-02 14:14:23 +0100

    pcmcia: improve check for same card in slot after resume
    During a suspend/resume cycle, an user may change the card in the
    PCMCIA/CardBus slot. The pcmcia_core can at least look at the
    socket state to check whether it is the same.
    
    For PCMCIA devices, move the detection and handling of such a
    change to ds.c.
    
    For CardBus devices, the PCI hotplug interface doesn't offer a "rescan"
    facility which also _removes_ devices no longer to be found behind a
    bridge. Therefore, remove and re-add all devices unconditionally.
    
    CC: Jesse Barnes <jbarnes@virtuousgeek.org>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    Tested-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/cistpl.c
+++ drivers/pcmcia/cistpl.c
@@ -378,6 +378,7 @@ int verify_cis_cache(struct pcmcia_socket *s)
 	kfree(buf);
 	return 0;
 }
+EXPORT_SYMBOL(verify_cis_cache);
 
 /*======================================================================
 
