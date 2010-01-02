287bc1164c5b4f5f2a95192367c39cd7c07d8646
Dominik Brodowski <linux@dominikbrodowski.net>
2010-01-02 17:34:09 +0100

    pcmcia: call CIS cleanup from ds.c
    As ds.c is the only real user of CIS access functions, call the
    cleanup functions from ds.c, too.
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/ds.c
+++ drivers/pcmcia/ds.c
@@ -1242,10 +1242,12 @@ static int ds_event(struct pcmcia_socket *skt, event_t event, int priority)
 		s->pcmcia_state.present = 0;
 		pcmcia_card_remove(skt, NULL);
 		handle_event(skt, event);
+		destroy_cis_cache(s);
 		break;
 
 	case CS_EVENT_CARD_INSERTION:
 		s->pcmcia_state.present = 1;
+		destroy_cis_cache(s); /* to be on the safe side... */
 		pcmcia_card_add(skt);
 		handle_event(skt, event);
 		break;
@@ -1367,6 +1369,7 @@ static void pcmcia_bus_remove_socket(struct device *dev,
 	/* unregister any unbound devices */
 	mutex_lock(&socket->skt_mutex);
 	pcmcia_card_remove(socket, NULL);
+	release_cis_mem(socket);
 	mutex_unlock(&socket->skt_mutex);
 
 	pcmcia_put_socket(socket);
