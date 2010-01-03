b512d722fc07fa1db92faf7d470f6eb0d2d3ad0b
pascal@pabr.org <pascal@pabr.org>
2010-01-03 13:39:12 +0100

    MIPS: Support 36-bit iomem on 32-bit Au1x00
    I believe these changes are needed on Alchemy SoCs in order to
    use iomem above 4G with the usual platform_device machinery:
    
    - Set CONFIG_ARCH_PHYS_ADDR_T_64BIT to make resource_size_t 64-bit.
    - Increase IOMEM_RESOURCE_END so that platforms can register resources.
    
    To: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/814/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-au1x00/au1000.h
+++ arch/mips/include/asm/mach-au1x00/au1000.h
@@ -1660,7 +1660,7 @@ enum soc_au1200_ints {
 #define IOPORT_RESOURCE_START	0x00001000	/* skip legacy probing */
 #define IOPORT_RESOURCE_END	0xffffffff
 #define IOMEM_RESOURCE_START	0x10000000
-#define IOMEM_RESOURCE_END	0xffffffff
+#define IOMEM_RESOURCE_END	0xfffffffffULL
 
 #else /* Au1000 and Au1100 and Au1200 */
 
@@ -1668,7 +1668,7 @@ enum soc_au1200_ints {
 #define IOPORT_RESOURCE_START	0x10000000
 #define IOPORT_RESOURCE_END	0xffffffff
 #define IOMEM_RESOURCE_START	0x10000000
-#define IOMEM_RESOURCE_END	0xffffffff
+#define IOMEM_RESOURCE_END	0xfffffffffULL
 
 #define PCI_IO_START	0
 #define PCI_IO_END	0
