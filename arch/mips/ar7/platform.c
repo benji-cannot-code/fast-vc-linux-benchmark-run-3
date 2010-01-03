bc6c07d8918a6cf2228ded69d5eb08ee0dbd3716
Florian Fainelli <florian@openwrt.org>
2010-01-03 21:16:51 +0100

    MIPS: AR7: Implement gpiolib
    This patch implements gpiolib for the AR7 SoC.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    To: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/816/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ar7/platform.c
+++ arch/mips/ar7/platform.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/etherdevice.h>
 #include <linux/phy.h>
 #include <linux/phy_fixed.h>
+#include <linux/gpio.h>
 
 #include <asm/addrspace.h>
 #include <asm/mach-ar7/ar7.h>
