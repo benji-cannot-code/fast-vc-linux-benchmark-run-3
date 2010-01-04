7e4fbeb1cf305660fdfc5f7ad3fee6b8e499b512
Wu Zhangjin <wuzhangjin@gmail.com>
2010-01-04 17:16:47 +0800

    MIPS: Loongson: Move prom_argc and prom_argv into prom_init_cmdline()
    prom_argc and prom_argv are only used by prom_init_cmdline(), move them
    into the function.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>
    Cc: linux-mips@linux-mips.org
    Cc: yanh@lemote.com
    Cc: huhb@lemote.com
    Cc: zhangfx@lemote.com
    Patchwork: http://patchwork.linux-mips.org/patch/825/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/loongson/common/cmdline.c
+++ arch/mips/loongson/common/cmdline.c
@@ -22,12 +22,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <loongson.h>
 
-int prom_argc;
-/* pmon passes arguments in 32bit pointers */
-int *_prom_argv;
-
 void __init prom_init_cmdline(void)
 {
+	int prom_argc;
+	/* pmon passes arguments in 32bit pointers */
+	int *_prom_argv;
 	int i;
 	long l;
 
