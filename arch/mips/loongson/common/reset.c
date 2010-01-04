ae33f9d0777eab5f8ec09e374414d3ab24bfa862
Wu Zhangjin <wuzhangjin@gmail.com>
2010-01-04 17:16:45 +0800

    MIPS: Loongson: Convert loongson_halt() to use unreachable()
    Use the new unreachable() macro instead of while(1);
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>
    Cc: linux-mips@linux-mips.org
    Cc: yanh@lemote.com
    Cc: huhb@lemote.com
    Cc: zhangfx@lemote.com
    Patchwork: http://patchwork.linux-mips.org/patch/823/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/loongson/common/reset.c
+++ arch/mips/loongson/common/reset.c
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2007 Lemote, Inc. & Institute of Computing Technology
  * Author: Fuxin Zhang, zhangfx@lemote.com
- * Copyright (C) 2009 Lemote, Inc. & Institute of Computing Technology
+ * Copyright (C) 2009 Lemote, Inc.
  * Author: Zhangjin Wu, wuzj@lemote.com
  */
 #include <linux/init.h>
@@ -29,8 +29,7 @@ static void loongson_restart(char *command)
 static void loongson_halt(void)
 {
 	mach_prepare_shutdown();
-	while (1)
-		;
+	unreachable();
 }
 
 static int __init mips_reboot_setup(void)
