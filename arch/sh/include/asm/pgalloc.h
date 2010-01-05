eb11e64d1a5e9ce67ae55957da7ea7ab78a97f28
Paul Mundt <lethal@linux-sh.org>
2010-01-05 12:35:00 +0900

    sh: Drop down to a single quicklist.
    We previously had 2 quicklists, one for the PGD case and one for PTEs.
    Now that the PGD/PMD cases are handled through slab caches due to the
    multi-level configurability, only the PTE quicklist remains. As such,
    reduce NR_QUICK to its appropriate size and bump down the PTE quicklist
    index.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/pgalloc.h
+++ arch/sh/include/asm/pgalloc.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/quicklist.h>
 #include <asm/page.h>
 
-#define QUICK_PT 1	/* Other page table pages that are zero on free */
+#define QUICK_PT 0	/* Other page table pages that are zero on free */
 
 extern pgd_t *pgd_alloc(struct mm_struct *);
 extern void pgd_free(struct mm_struct *mm, pgd_t *pgd);
