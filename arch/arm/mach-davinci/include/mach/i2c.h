6d21f90e1378b668ad07de9f220ec8696262ff19
Stefan Weil <weil@mail.berlios.de>
2010-01-07 00:03:52 +0100

    Fix spelling of 'platform' in comments and doc
    Replace platfrom -> platform.
    
    This is a frequent spelling bug.
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- arch/arm/mach-davinci/include/mach/i2c.h
+++ arch/arm/mach-davinci/include/mach/i2c.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * DaVinci I2C controller platfrom_device info
+ * DaVinci I2C controller platform_device info
  *
  * Author: Vladimir Barinov, MontaVista Software, Inc. <source@mvista.com>
  *
