e9ed8f19cee8f221829430afe4eaeaff2367b41b
Amit Kumar Salecha <amit.salecha@qlogic.com>
2010-01-07 22:10:14 +0000

    netxen: fix tx ring memory leak
    o While unloading driver or resetting the context, tx ring was not
      getting free.
    
    Signed-off-by: Amit Kumar Salecha <amit.salecha@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/netxen/netxen_nic_init.c
+++ drivers/net/netxen/netxen_nic_init.c
@@ -185,6 +185,8 @@ skip_rds:
 
 	tx_ring = adapter->tx_ring;
 	vfree(tx_ring->cmd_buf_arr);
+	kfree(tx_ring);
+	adapter->tx_ring = NULL;
 }
 
 int netxen_alloc_sw_resources(struct netxen_adapter *adapter)
