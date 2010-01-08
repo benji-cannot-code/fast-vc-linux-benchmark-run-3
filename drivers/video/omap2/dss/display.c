74bf014b72479823db098e25fb7b49cedd06ca7e
Tomi Valkeinen <tomi.valkeinen@nokia.com>
2010-01-08 16:52:48 +0200

    OMAP: DSS2: move get/set_rotate()
    Move get/set_rotate() from omap_dss_device to omap_dss_driver.
    
    This is part of a larger patch-set, which moves the control from omapdss
    driver to the display driver.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- drivers/video/omap2/dss/display.c
+++ drivers/video/omap2/dss/display.c
@@ -186,9 +186,9 @@ static ssize_t display_rotate_show(struct device *dev,
 {
 	struct omap_dss_device *dssdev = to_dss_device(dev);
 	int rotate;
-	if (!dssdev->get_rotate)
+	if (!dssdev->driver->get_rotate)
 		return -ENOENT;
-	rotate = dssdev->get_rotate(dssdev);
+	rotate = dssdev->driver->get_rotate(dssdev);
 	return snprintf(buf, PAGE_SIZE, "%u\n", rotate);
 }
 
@@ -199,12 +199,12 @@ static ssize_t display_rotate_store(struct device *dev,
 	unsigned long rot;
 	int r;
 
-	if (!dssdev->set_rotate || !dssdev->get_rotate)
+	if (!dssdev->driver->set_rotate || !dssdev->driver->get_rotate)
 		return -ENOENT;
 
 	rot = simple_strtoul(buf, NULL, 0);
 
-	r = dssdev->set_rotate(dssdev, rot);
+	r = dssdev->driver->set_rotate(dssdev, rot);
 	if (r)
 		return r;
 
