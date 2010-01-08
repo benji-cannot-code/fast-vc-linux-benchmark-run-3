7f28d314ccb43c9bfd6dfe87240f55bf4aba482e
Huang Weiyi <weiyi.huang@gmail.com>
2010-01-08 15:23:03 -0700

    OMAP2: remove duplicated #include
    Remove duplicated #include('s) in
      arch/arm/mach-omap2/clock34xx.c
      arch/arm/mach-omap2/io.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/sdrc.h>
 #include <plat/gpmc.h>
 #include <plat/serial.h>
-#include <plat/mux.h>
 #include <plat/vram.h>
 
 #include "clock.h"
