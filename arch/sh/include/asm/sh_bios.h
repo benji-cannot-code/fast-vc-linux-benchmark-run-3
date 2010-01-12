d854580d0672c97104965a3aede6a2397d2dc02d
Paul Mundt <lethal@linux-sh.org>
2010-01-12 15:26:11 +0900

    sh: Kill off more unused sh_bios callbacks.
    sh_bios_char_out() is not used by anything in-tree these days, so just
    get rid of it.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/sh_bios.h
+++ arch/sh/include/asm/sh_bios.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * usually from within the early stages of kernel boot.
  */
 extern void sh_bios_console_write(const char *buf, unsigned int len);
-extern void sh_bios_char_out(char ch);
 extern void sh_bios_gdb_detach(void);
 
 extern void sh_bios_get_node_addr(unsigned char *node_addr);
