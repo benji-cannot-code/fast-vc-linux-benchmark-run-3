21b2392b1cbbad4156cf8de84d8ece24f59bfaaa
Corentin Chary <corentincj@iksaif.net>
2010-01-13 21:49:10 +0100

    asus-laptop: stop using read_status for bluetooth and wlan
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

--- drivers/platform/x86/asus-laptop.c
+++ drivers/platform/x86/asus-laptop.c
@@ -240,6 +240,9 @@ struct asus_laptop {
 
 	struct asus_laptop_leds leds;
 
+	int wireless_status;
+	bool have_rsts;
+
 	acpi_handle handle;	/* the handle of the hotk device */
 	char status;		/* status of the hotk, for LEDs, ... */
 	u32 ledd_status;	/* status of the LED display */
@@ -329,23 +332,6 @@ static int write_acpi_int(acpi_handle handle, const char *method, int val)
 	return write_acpi_int_ret(handle, method, val, NULL);
 }
 
-static int read_wireless_status(struct asus_laptop *asus, int mask)
-{
-	unsigned long long status;
-	acpi_status rv = AE_OK;
-
-	if (!wireless_status_handle)
-		return (asus->status & mask) ? 1 : 0;
-
-	rv = acpi_evaluate_integer(wireless_status_handle, NULL, NULL, &status);
-	if (ACPI_FAILURE(rv))
-		pr_warning("Error reading Wireless status\n");
-	else
-		return (status & mask) ? 1 : 0;
-
-	return (asus->status & mask) ? 1 : 0;
-}
-
 static int read_gps_status(struct asus_laptop *asus)
 {
 	unsigned long long status;
@@ -363,10 +349,7 @@ static int read_gps_status(struct asus_laptop *asus)
 /* Generic LED functions */
 static int read_status(struct asus_laptop *asus, int mask)
 {
-	/* There is a special method for both wireless devices */
-	if (mask == BT_ON || mask == WL_ON)
-		return read_wireless_status(asus, mask);
-	else if (mask == GPS_ON)
+	if (mask == GPS_ON)
 		return read_gps_status(asus);
 
 	return (asus->status & mask) ? 1 : 0;
@@ -813,6 +796,25 @@ static ssize_t store_ledd(struct device *dev, struct device_attribute *attr,
 }
 
 /*
+ * Wireless
+ */
+static int asus_wireless_status(struct asus_laptop *asus, int mask)
+{
+	unsigned long long status;
+	acpi_status rv = AE_OK;
+
+	if (!asus->have_rsts)
+		return (asus->wireless_status & mask) ? 1 : 0;
+
+	rv = acpi_evaluate_integer(wireless_status_handle, NULL, NULL, &status);
+	if (ACPI_FAILURE(rv)) {
+		pr_warning("Error reading Wireless status\n");
+		return -EINVAL;
+	}
+	return !!(status & mask);
+}
+
+/*
  * WLAN
  */
 static ssize_t show_wlan(struct device *dev,
@@ -820,7 +822,7 @@ static ssize_t show_wlan(struct device *dev,
 {
 	struct asus_laptop *asus = dev_get_drvdata(dev);
 
-	return sprintf(buf, "%d\n", read_status(asus, WL_ON));
+	return sprintf(buf, "%d\n", asus_wireless_status(asus, WL_ON));
 }
 
 static ssize_t store_wlan(struct device *dev, struct device_attribute *attr,
@@ -839,7 +841,7 @@ static ssize_t show_bluetooth(struct device *dev,
 {
 	struct asus_laptop *asus = dev_get_drvdata(dev);
 
-	return sprintf(buf, "%d\n", read_status(asus, BT_ON));
+	return sprintf(buf, "%d\n", asus_wireless_status(asus, BT_ON));
 }
 
 static ssize_t store_bluetooth(struct device *dev,
@@ -1372,7 +1374,8 @@ static int asus_laptop_get_info(struct asus_laptop *asus)
 	if (hwrs_result & BT_HWRS)
 		ASUS_HANDLE_INIT(bt_switch);
 
-	ASUS_HANDLE_INIT(wireless_status);
+	if (!ASUS_HANDLE_INIT(wireless_status))
+		asus->have_rsts = true;
 
 	ASUS_HANDLE_INIT(brightness_set);
 	ASUS_HANDLE_INIT(brightness_get);
@@ -1425,8 +1428,8 @@ static int __devinit asus_acpi_init(struct asus_laptop *asus)
 		write_status(asus, wl_switch_handle, !!wireless_status, WL_ON);
 
 	/* If the h/w switch is off, we need to check the real status */
-	write_status(asus, NULL, read_status(asus, BT_ON), BT_ON);
-	write_status(asus, NULL, read_status(asus, WL_ON), WL_ON);
+	write_status(asus, NULL, asus_wireless_status(asus, BT_ON), BT_ON);
+	write_status(asus, NULL, asus_wireless_status(asus, WL_ON), WL_ON);
 
 	/* LCD Backlight is on by default */
 	write_status(asus, NULL, 1, LCD_ON);
