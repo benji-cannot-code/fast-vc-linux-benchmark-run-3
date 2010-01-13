5507af824fcb4b5bf55d79a3a41d18384cd7a213
Maurus Cuelenaere <mcuelenaere@gmail.com>
2010-01-14 00:30:31 +0100

    ARM: SAMSUNG: Move S3C24XX ADC driver to plat-samsung
    Move S3C24XX ADC driver to plat-samsung
    
    Signed-off-by: Maurus Cuelenaere <mcuelenaere@gmail.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-samsung/include/plat/adc.h
+++ arch/arm/plat-samsung/include/plat/adc.h
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* arch/arm/plat-s3c/include/plat/adc.h
+/* arch/arm/plat-samsung/include/plat/adc.h
  *
  * Copyright (c) 2008 Simtec Electronics
  *	http://armlinux.simnte.co.uk/
  *	Ben Dooks <ben@simtec.co.uk>
  *
- * S3C24XX ADC driver information
+ * S3C ADC driver information
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
