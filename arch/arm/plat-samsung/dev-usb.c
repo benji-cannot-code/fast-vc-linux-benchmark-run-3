34ac59bc2152d319d2fae149c9b1a2697449f5c6
Ben Dooks <ben-linux@fluff.org>
2010-01-13 14:59:46 +0900

    ARM: SAMSUNG: Add platform data registration for OHCI
    Add a platform data helper for the OHCI device
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-samsung/dev-usb.c
+++ arch/arm/plat-samsung/dev-usb.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/map.h>
 
 #include <plat/devs.h>
-
+#include <plat/usb-control.h>
 
 static struct resource s3c_usb_resource[] = {
 	[0] = {
@@ -49,3 +49,22 @@ struct platform_device s3c_device_ohci = {
 };
 
 EXPORT_SYMBOL(s3c_device_ohci);
+
+/**
+ * s3c_ohci_set_platdata - initialise OHCI device platform data
+ * @info: The platform data.
+ *
+ * This call copies the @info passed in and sets the device .platform_data
+ * field to that copy. The @info is copied so that the original can be marked
+ * __initdata.
+ */
+void __init s3c_ohci_set_platdata(struct s3c2410_hcd_info *info)
+{
+	struct s3c2410_hcd_info *npd;
+
+	npd = kmemdup(info, sizeof(struct s3c2410_hcd_info), GFP_KERNEL);
+	if (!npd)
+		printk(KERN_ERR "%s: no memory for platform data\n", __func__);
+
+	s3c_device_ohci.dev.platform_data = npd;
+}
