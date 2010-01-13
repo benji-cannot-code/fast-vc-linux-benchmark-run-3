8ea36e905ddd3212fbd0c3817892c56854cc45ac
Heiko Carstens <heiko.carstens@de.ibm.com>
2010-01-13 20:44:35 +0100

    [S390] smp: remove volatile type quilifier from __cpu_logical_map
    Remove pointless qualifier.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/sigp.h
+++ arch/s390/include/asm/sigp.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/atomic.h>
 
 /* get real cpu address from logical cpu number */
-extern volatile int __cpu_logical_map[];
+extern int __cpu_logical_map[];
 
 typedef enum
 {
