04f1b43a86f4aaaa8c19a6b6040e914d65ae9831
Baruch Siach <baruch@tkos.co.il>
2010-01-14 11:24:14 +0200

    mx25: add NAND support
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/mx25.h
+++ arch/arm/plat-mxc/include/mach/mx25.h
@@ -32,7 +32,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define UART2_BASE_ADDR			0x43f94000
 
 #define MX25_FEC_BASE_ADDR		0x50038000
+#define MX25_NFC_BASE_ADDR		0xbb000000
 
 #define MX25_INT_FEC	57
+#define MX25_INT_NANDFC	33
 
 #endif /* ifndef __MACH_MX25_H__ */
