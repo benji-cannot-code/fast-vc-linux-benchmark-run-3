68ba9624761261100130dece1631c886fa22dbba
Oliver Neukum <oliver@neukum.org>
2010-01-14 16:12:01 +0100

    USB: BKL removal: ftdi-elan
    BKL was not needed at all. Removed without replacement.
    
    Signed-off-by: Oliver Neukum <oliver@neukum.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/misc/ftdi-elan.c
+++ drivers/usb/misc/ftdi-elan.c
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/kref.h>
 #include <linux/mutex.h>
-#include <linux/smp_lock.h>
 #include <asm/uaccess.h>
 #include <linux/usb.h>
 #include <linux/workqueue.h>
@@ -628,27 +627,22 @@ static int ftdi_elan_open(struct inode *inode, struct file *file)
 	int subminor;
 	struct usb_interface *interface;
 
-	lock_kernel();
         subminor = iminor(inode);
         interface = usb_find_interface(&ftdi_elan_driver, subminor);
 
         if (!interface) {
-		unlock_kernel();
                 printk(KERN_ERR "can't find device for minor %d\n", subminor);
                 return -ENODEV;
         } else {
                 struct usb_ftdi *ftdi = usb_get_intfdata(interface);
                 if (!ftdi) {
-			unlock_kernel();
                         return -ENODEV;
                 } else {
                         if (down_interruptible(&ftdi->sw_lock)) {
-				unlock_kernel();
                                 return -EINTR;
                         } else {
                                 ftdi_elan_get_kref(ftdi);
                                 file->private_data = ftdi;
-				unlock_kernel();
                                 return 0;
                         }
                 }
