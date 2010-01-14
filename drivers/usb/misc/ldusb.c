1fd09149c1879c94b8d454e4be7e782a156ebf16
Oliver Neukum <oliver@neukum.org>
2010-01-14 16:12:27 +0100

    USB: BKL removal: ldusb
    BKL was not needed at all. Removed without replacement.
    
    Signed-off-by: Oliver Neukum <oliver@neukum.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/misc/ldusb.c
+++ drivers/usb/misc/ldusb.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/smp_lock.h>
 
 #include <asm/uaccess.h>
 #include <linux/input.h>
@@ -298,14 +297,12 @@ static int ld_usb_open(struct inode *inode, struct file *file)
 	int retval;
 	struct usb_interface *interface;
 
-	lock_kernel();
 	nonseekable_open(inode, file);
 	subminor = iminor(inode);
 
 	interface = usb_find_interface(&ld_usb_driver, subminor);
 
 	if (!interface) {
-		unlock_kernel();
 		err("%s - error, can't find device for minor %d\n",
 		     __func__, subminor);
 		return -ENODEV;
@@ -313,16 +310,12 @@ static int ld_usb_open(struct inode *inode, struct file *file)
 
 	dev = usb_get_intfdata(interface);
 
-	if (!dev) {
-		unlock_kernel();
+	if (!dev)
 		return -ENODEV;
-	}
 
 	/* lock this device */
-	if (mutex_lock_interruptible(&dev->mutex)) {
-		unlock_kernel();
+	if (mutex_lock_interruptible(&dev->mutex))
 		return -ERESTARTSYS;
-	}
 
 	/* allow opening only once */
 	if (dev->open_count) {
@@ -361,7 +354,6 @@ static int ld_usb_open(struct inode *inode, struct file *file)
 
 unlock_exit:
 	mutex_unlock(&dev->mutex);
-	unlock_kernel();
 
 	return retval;
 }
