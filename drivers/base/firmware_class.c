60cc9af5dd27139640e0be0dec51ef684dd54d8e
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-01-13 21:18:15 -0500

    driver-core: firmware_class: remove base.h header inclusion
    base.h is used by base drivers for sharing internal structures.
    Turns out firmware_class does not depend on it at all so remove it.
    
    Cc: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kthread.h>
 #include <linux/highmem.h>
 #include <linux/firmware.h>
-#include "base.h"
 
 #define to_dev(obj) container_of(obj, struct device, kobj)
 
