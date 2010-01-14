3701f8cc2a5100c5caf832e19d340cc84b057982
Yehuda Sadeh <yehuda@hq.newdream.net>
2010-01-13 17:03:23 -0800

    ceph: keep reserved replies on the request structure
    This includes treating all the data preallocation and revokation
    at the same place, not having to have a special case for
    the reserved pages.
    
    Signed-off-by: Yehuda Sadeh <yehuda@hq.newdream.net>

--- fs/ceph/osd_client.h
+++ fs/ceph/osd_client.h
@@ -45,7 +45,7 @@ struct ceph_osd_request {
 	struct ceph_osd *r_osd;
 	struct ceph_pg   r_pgid;
 
-	struct ceph_connection *r_con_filling_pages;
+	struct ceph_connection *r_con_filling_msg;
 
 	struct ceph_msg  *r_request, *r_reply;
 	int               r_result;
@@ -76,6 +76,9 @@ struct ceph_osd_request {
 	struct page     **r_pages;            /* pages for data payload */
 	int               r_pages_from_pool;
 	int               r_own_pages;        /* if true, i own page list */
+
+	struct ceph_msg   *replies[2];
+	int		  cur_reply;
 };
 
 struct ceph_osd_client {
@@ -99,8 +102,7 @@ struct ceph_osd_client {
 
 	mempool_t              *req_mempool;
 
-	struct ceph_msgpool   msgpool_op;
-	struct ceph_msgpool   msgpool_op_reply;
+	struct ceph_msgpool	msgpool_op;
 };
 
 extern int ceph_osdc_init(struct ceph_osd_client *osdc,
