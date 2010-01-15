f08711d464b8fd1b9606cec7cf3e4e83c22c9ea6
Russell King <rmk+kernel@arm.linux.org.uk>
2010-01-15 22:12:46 +0000

    ARM: Integrator: pass 'khz' to integrator_time_init
    This is now what the clocksource/clockevent initialization functions
    want, so give them the timer tick rate directly.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-integrator/common.h
+++ arch/arm/mach-integrator/common.h
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-extern void integrator_time_init(unsigned long, unsigned int);
+extern void integrator_time_init(u32, unsigned int);
