2257dee4be418c58e559ad101489ba1ea07ada26
Paul Mundt <lethal@linux-sh.org>
2010-01-15 12:11:30 +0900

    sh: mach-sdk7786: heartbeat support.
    Hand off the user LEDs to the heartbeat driver.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-sdk7786/setup.c
+++ arch/sh/boards/mach-sdk7786/setup.c
@@ -16,7 +16,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/irq.h>
 #include <asm/machvec.h>
+#include <asm/heartbeat.h>
 #include <asm/sizes.h>
+
+static struct resource heartbeat_resource = {
+	.start		= 0x07fff8b0,
+	.end		= 0x07fff8b0 + sizeof(u16) - 1,
+	.flags		= IORESOURCE_MEM | IORESOURCE_MEM_16BIT,
+};
+
+static struct platform_device heartbeat_device = {
+	.name		= "heartbeat",
+	.id		= -1,
+	.num_resources	= 1,
+	.resource	= &heartbeat_resource,
+};
 
 static struct resource smsc911x_resources[] = {
 	[0] = {
@@ -83,6 +97,7 @@ static struct i2c_board_info __initdata sdk7786_i2c_devices[] = {
 };
 
 static struct platform_device *sh7786_devices[] __initdata = {
+	&heartbeat_device,
 	&smsc911x_device,
 	&smbus_fpga_device,
 	&smbus_pcie_device,
