de38452e9579005aede89921331d66cd05b2528c
Frederic Weisbecker <fweisbec@gmail.com>
2010-01-16 14:21:15 +0100

    perf: Fix implicit declaration of getline in util.c
    getline() is considered as undeclared in util/util.c because
    it includes string.h, that in turn includes stdio.h, without
    having defined _GNU_SOURCE.
    
    But util.c also includes util.h that handles the _GNU_SOURCE and
    all the needed inclusions already. Let's include only util.h
    and sys/mman.h which is the only one header not handled by
    util.h
    
    This fixes the following build error:
    
     util/util.c: In function 'slow_copyfile':
     util/util.c:49: erreur: implicit declaration of function
     'getline' util/util.c:49: erreur: nested extern declaration of 'getline'
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Paul Mackerras <paulus@samba.org>
    LKML-Reference: <1263648075-3858-1-git-send-regression-fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/util.c
+++ tools/perf/util/util.c
@@ -1,11 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
 #include "util.h"
+#include <sys/mman.h>
 
 int mkdir_p(char *path, mode_t mode)
 {
