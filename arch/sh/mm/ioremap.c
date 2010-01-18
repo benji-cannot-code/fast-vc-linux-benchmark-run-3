7731017ec936786f92a481fb2eaf4d4629a508ab
Paul Mundt <lethal@linux-sh.org>
2010-01-18 21:45:00 +0900

    sh: Flag __ioremap_caller() __init_refok.
    The mem_init_done test makes sure that this path is only entered in
    __init cases, so leaving ioremap_fixed() as __init and flagging the
    caller __init_refok is sufficient.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/ioremap.c
+++ arch/sh/mm/ioremap.c
@@ -34,8 +34,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * have to convert them into an offset in a page-aligned mapping, but the
  * caller shouldn't need to know that small detail.
  */
-void __iomem *__ioremap_caller(unsigned long phys_addr, unsigned long size,
-			       unsigned long flags, void *caller)
+void __iomem * __init_refok
+__ioremap_caller(unsigned long phys_addr, unsigned long size,
+		 unsigned long flags, void *caller)
 {
 	struct vm_struct *area;
 	unsigned long offset, last_addr, addr, orig_addr;
