2f9980e1c538d48572a207b588a8a48bf540112d
Dave Chinner <david@fromorbit.com>
2010-01-20 10:48:05 +1100

    xfs: clean up sign warnings in dir2 code
    We are now consistently using unsigned char strings for names
    so fix up the remaining warnings in the dir2 code to complete
    the cleanup.
    
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_dir2.c
+++ fs/xfs/xfs_dir2.c
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xfs_vnodeops.h"
 #include "xfs_trace.h"
 
-struct xfs_name xfs_name_dotdot = {"..", 2};
+struct xfs_name xfs_name_dotdot = { (unsigned char *)"..", 2};
 
 /*
  * ASCII case-insensitive (ie. A-Z) support for directories that was
