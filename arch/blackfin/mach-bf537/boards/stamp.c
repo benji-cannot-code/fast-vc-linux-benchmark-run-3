788323a7b2534b14668fe7f32d7bc3b78f8aa025
Mike Frysinger <vapier@gentoo.org>
2010-01-19 07:04:29 +0000

    Blackfin: bf537-stamp: move ADF7021 MAC storage into platform resources
    Just generate a random MAC on the demo board since the ADF702x lacks
    dedicated storage for such things.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/mach-bf537/boards/stamp.c
+++ arch/blackfin/mach-bf537/boards/stamp.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 #include <linux/i2c.h>
 #include <linux/i2c/adp5588.h>
+#include <linux/etherdevice.h>
 #include <linux/ata_platform.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
@@ -817,6 +818,12 @@ static struct adf702x_platform_data adf7021_platform_data = {
 	.adf702x_regs = adf7021_regs,
 	.tx_reg = TXREG,
 };
+static inline void adf702x_mac_init(void)
+{
+	random_ether_addr(adf7021_platform_data.mac_addr);
+}
+#else
+static inline void adf702x_mac_init(void) {}
 #endif
 
 #if defined(CONFIG_MTD_DATAFLASH) \
@@ -1968,6 +1975,7 @@ static int __init stamp_init(void)
 {
 	printk(KERN_INFO "%s(): registering device resources\n", __func__);
 	bfin_plat_nand_init();
+	adf702x_mac_init();
 	platform_add_devices(stamp_devices, ARRAY_SIZE(stamp_devices));
 	i2c_register_board_info(0, bfin_i2c_board_info,
 				ARRAY_SIZE(bfin_i2c_board_info));
