f4e6a6d4a40790f41eefe5f69365e53ee4a3e86e
Oliver Endriss <o.endriss@gmx.de>
2010-01-20 19:03:22 -0300

    V4L/DVB: ngene: Take care of late interrupts
    Request might already have been processed when the interrupt arrives.
    
    Signed-off-by: Oliver Endriss <o.endriss@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/ngene/ngene-core.c
+++ drivers/media/dvb/ngene/ngene-core.c
@@ -254,7 +254,8 @@ static irqreturn_t irq_handler(int irq, void *dev_id)
 		spin_unlock(&dev->channel[i].state_lock);
 	}
 
-	return rc;
+	/* Request might have been processed by a previous call. */
+	return IRQ_HANDLED;
 }
 
 /****************************************************************************/
