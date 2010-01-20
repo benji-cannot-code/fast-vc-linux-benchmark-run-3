3bb8827ba90b93451bd856bd4d986fbb933f250e
Alexey Dobriyan <adobriyan@gmail.com>
2010-01-20 22:27:56 +0200

    dnotify: move dir_notify_enable declaration
    Move dir_notify_enable declaration to where it belongs -- dnotify.h .
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/times.h>
 #include <linux/limits.h>
 #include <linux/dcache.h>
+#include <linux/dnotify.h>
 #include <linux/syscalls.h>
 #include <linux/vmstat.h>
 #include <linux/nfs_fs.h>
