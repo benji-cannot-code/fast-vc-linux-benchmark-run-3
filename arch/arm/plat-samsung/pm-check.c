34807701e658146e0bb53f4053e58771f9e0d828
Ben Dooks <ben-linux@fluff.org>
2010-01-20 12:29:25 +0900

    ARM: SAMSUNG: Move pm-check.c to plat-samsung
    Move the pm-check.c file to plat-samsung for all Samsung SoC users, and
    update Kconfig names to make them SAMSUNG_ instead of S3C2410_
    
    Sed expresions used to make the change:
    
        s/S3C2410_PM_DEBUG/SAMSUNG_PM_DEBUG/g
        s/S3C2410_PM_CHECK/SAMSUNG_PM_CHECK/g
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-samsung/pm-check.c
+++ arch/arm/plat-samsung/pm-check.c
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat/pm.h>
 
-#if CONFIG_S3C2410_PM_CHECK_CHUNKSIZE < 1
-#error CONFIG_S3C2410_PM_CHECK_CHUNKSIZE must be a positive non-zero value
+#if CONFIG_SAMSUNG_PM_CHECK_CHUNKSIZE < 1
+#error CONFIG_SAMSUNG_PM_CHECK_CHUNKSIZE must be a positive non-zero value
 #endif
 
 /* suspend checking code...
@@ -30,12 +30,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * this next area does a set of crc checks over all the installed
  * memory, so the system can verify if the resume was ok.
  *
- * CONFIG_S3C2410_PM_CHECK_CHUNKSIZE defines the block-size for the CRC,
+ * CONFIG_SAMSUNG_PM_CHECK_CHUNKSIZE defines the block-size for the CRC,
  * increasing it will mean that the area corrupted will be less easy to spot,
  * and reducing the size will cause the CRC save area to grow
 */
 
-#define CHECK_CHUNKSIZE (CONFIG_S3C2410_PM_CHECK_CHUNKSIZE * 1024)
+#define CHECK_CHUNKSIZE (CONFIG_SAMSUNG_PM_CHECK_CHUNKSIZE * 1024)
 
 static u32 crc_size;	/* size needed for the crc block */
 static u32 *crcs;	/* allocated over suspend/resume */
