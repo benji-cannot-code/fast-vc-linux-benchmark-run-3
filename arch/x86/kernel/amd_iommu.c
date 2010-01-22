75049746fcfde142be710ac461c182323cac21bc
Joerg Roedel <joerg.roedel@amd.com>
2010-01-22 17:44:35 +0100

    x86/amd-iommu: Fix IOMMU-API initialization for iommu=pt
    This patch moves the initialization of the iommu-api out of
    the dma-ops initialization code. This ensures that the
    iommu-api is initialized even with iommu=pt.
    
    Cc: stable@kernel.org
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- arch/x86/kernel/amd_iommu.c
+++ arch/x86/kernel/amd_iommu.c
@@ -2222,6 +2222,12 @@ static struct dma_map_ops amd_iommu_dma_ops = {
 /*
  * The function which clues the AMD IOMMU driver into dma_ops.
  */
+
+void __init amd_iommu_init_api(void)
+{
+	register_iommu(&amd_iommu_ops);
+}
+
 int __init amd_iommu_init_dma_ops(void)
 {
 	struct amd_iommu *iommu;
@@ -2256,8 +2262,6 @@ int __init amd_iommu_init_dma_ops(void)
 
 	/* Make the driver finally visible to the drivers */
 	dma_ops = &amd_iommu_dma_ops;
-
-	register_iommu(&amd_iommu_ops);
 
 	amd_iommu_stats_init();
 
