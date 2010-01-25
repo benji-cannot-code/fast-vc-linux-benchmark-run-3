d0668c11554283c586cc5cbc5bcaf6953786b5c2
Haojian Zhuang <haojian.zhuang@gmail.com>
2010-01-25 06:03:54 -0500

    [ARM] mmp2: add gpio initialization
    Signed-off-by: Haojian Zhuang <haojian.zhuang@marvell.com>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-mmp/mmp2.c
+++ arch/arm/mach-mmp/mmp2.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/cputype.h>
 #include <mach/irqs.h>
 #include <mach/mfp.h>
+#include <mach/gpio.h>
 #include <mach/devices.h>
 
 #include "common.h"
@@ -29,11 +30,33 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MFPR_VIRT_BASE	(APB_VIRT_BASE + 0x1e000)
 
+#define APMASK(i)	(GPIO_REGS_VIRT + BANK_OFF(i) + 0x9c)
+
 static struct mfp_addr_map mmp2_addr_map[] __initdata = {
 	MFP_ADDR(PMIC_INT, 0x2c4),
 
 	MFP_ADDR_END,
 };
+
+static void __init mmp2_init_gpio(void)
+{
+	int i;
+
+	/* enable GPIO clock */
+	__raw_writel(APBC_APBCLK | APBC_FNCLK, APBC_MMP2_GPIO);
+
+	/* unmask GPIO edge detection for all 6 banks -- APMASKx */
+	for (i = 0; i < 6; i++)
+		__raw_writel(0xffffffff, APMASK(i));
+
+	pxa_init_gpio(IRQ_MMP2_GPIO, 0, 167, NULL);
+}
+
+void __init mmp2_init_irq(void)
+{
+	mmp2_init_icu();
+	mmp2_init_gpio();
+}
 
 /* APB peripheral clocks */
 static APBC_CLK(uart1, MMP2_UART1, 1, 26000000);
