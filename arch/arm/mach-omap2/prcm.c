a5d85380848840ea17730e36c34a2dfda617e065
Paul Walmsley <paul@pwsan.com>
2010-01-26 20:13:11 -0700

    OMAP2/3/4 clock: omap2_clk_prepare_for_reboot() is OMAP2xxx-only
    omap2_clk_prepare_for_reboot() is only applicable to OMAP2xxx chips,
    so rename it to omap2xxx_clk_prepare_for_reboot() and only call it when
    running on OMAP2xxx chips.  Remove the old stub in the OMAP3 clock code.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/prcm.c
+++ arch/arm/mach-omap2/prcm.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/control.h>
 
 #include "clock.h"
+#include "clock2xxx.h"
 #include "cm.h"
 #include "prm.h"
 #include "prm-regbits-24xx.h"
@@ -134,11 +135,12 @@ EXPORT_SYMBOL(omap_prcm_get_reset_sources);
 void omap_prcm_arch_reset(char mode)
 {
 	s16 prcm_offs;
-	omap2_clk_prepare_for_reboot();
 
-	if (cpu_is_omap24xx())
+	if (cpu_is_omap24xx()) {
+		omap2xxx_clk_prepare_for_reboot();
+
 		prcm_offs = WKUP_MOD;
-	else if (cpu_is_omap34xx()) {
+	} else if (cpu_is_omap34xx()) {
 		u32 l;
 
 		prcm_offs = OMAP3430_GR_MOD;
