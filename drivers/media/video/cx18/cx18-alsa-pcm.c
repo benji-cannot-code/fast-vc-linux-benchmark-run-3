acd9ce50fe5f720ea3bad49046052ecf6c003d28
Stephen Rothwell <sfr@canb.auug.org.au>
2010-01-27 03:53:39 -0300

    V4L/DVB: using vmalloc requires include of linux/vmalloc.h
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Andy Walls <awalls@radix.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-alsa-pcm.c
+++ drivers/media/video/cx18/cx18-alsa-pcm.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/vmalloc.h>
 
 #include <media/v4l2-device.h>
 
