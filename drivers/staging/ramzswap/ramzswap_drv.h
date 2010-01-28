16be173aa8cee822ff7371c7834600bcea780815
Nitin Gupta <ngupta@vflare.org>
2010-01-28 21:21:35 +0530

    Staging: ramzswap: Update copyright notice
    Update copyright notice.
    
    Signed-off-by: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ramzswap/ramzswap_drv.h
+++ drivers/staging/ramzswap/ramzswap_drv.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Compressed RAM based swap device
  *
- * Copyright (C) 2008, 2009  Nitin Gupta
+ * Copyright (C) 2008, 2009, 2010  Nitin Gupta
  *
  * This code is released using a dual license strategy: BSD/GPL
  * You can choose the licence that better fits your requirements.
