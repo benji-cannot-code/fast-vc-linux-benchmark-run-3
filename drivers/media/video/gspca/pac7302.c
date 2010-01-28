8745bf3c446ad77fc79b029a6eb8039b3c17f9a8
Jean-Francois Moine <moinejf@free.fr>
2010-01-28 16:35:40 -0300

    V4L/DVB: gspca - pac7302: Change latin1 text to utf-8.
    Signed-off-by: Jean-Francois Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/pac7302.c
+++ drivers/media/video/gspca/pac7302.c
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * V4L2 by Jean-Francois Moine <http://moinejf.free.fr>
  *
- * Separated from Pixart PAC7311 library by M�rton N�meth <nm127@freemail.hu>
+ * Separated from Pixart PAC7311 library by Márton Németh
  * Camera button input handling by Márton Németh <nm127@freemail.hu>
  * Copyright (C) 2009-2010 Márton Németh <nm127@freemail.hu>
  *
