c670d12c38477b82acd81cc0882832e767777636
Joel Becker <joel.becker@oracle.com>
2010-01-28 15:00:49 -0800

    ocfs2_dlmfs: Move to its own directory
    We're going to remove the tie between ocfs2_dlmfs and o2dlm.
    ocfs2_dlmfs doesn't belong in the fs/ocfs2/dlm directory anymore.  Here
    we move it to fs/ocfs2/dlmfs.
    
    Signed-off-by: Joel Becker <joel.becker@oracle.com>

--- fs/ocfs2/dlmfs/userdlm.c
+++ fs/ocfs2/dlmfs/userdlm.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cluster/heartbeat.h"
 #include "cluster/tcp.h"
 
-#include "dlmapi.h"
+#include "dlm/dlmapi.h"
 
 #include "userdlm.h"
 
