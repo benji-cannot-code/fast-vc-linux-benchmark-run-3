34e8b12c6e897dc218b24c1ea97a9d3bf453bf41
Kukjin Kim <kgene.kim@samsung.com>
2010-01-29 10:17:20 +0900

    ARM: S5P6442: Add IRQ support
    This patch adds IRQ support for S5P6442. This patch adds interrupt
    register definitions, IRQ definitions for various interrupt sources
    and new VIC base for VIC2 in plat-s5p common irq code.
    
    Signed-off-by: Adityapratap Sharma <aditya.ps@samsung.com>
    Signed-off-by: Atul Dahiya <atul.dahiya@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-s5p/include/plat/irqs.h
+++ arch/arm/plat-s5p/include/plat/irqs.h
@@ -29,11 +29,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define S5P_VIC0_BASE		S5P_IRQ(0)
 #define S5P_VIC1_BASE		S5P_IRQ(32)
+#define S5P_VIC2_BASE		S5P_IRQ(64)
 
 #define VIC_BASE(x)		(S5P_VIC0_BASE + ((x)*32))
 
 #define IRQ_VIC0_BASE		S5P_VIC0_BASE
 #define IRQ_VIC1_BASE		S5P_VIC1_BASE
+#define IRQ_VIC2_BASE		S5P_VIC2_BASE
 
 /* UART interrupts, each UART has 4 intterupts per channel so
  * use the space between the ISA and S3C main interrupts. Note, these
@@ -74,6 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define S5P_IRQ_VIC0(x)		(S5P_VIC0_BASE + (x))
 #define S5P_IRQ_VIC1(x)		(S5P_VIC1_BASE + (x))
+#define S5P_IRQ_VIC2(x)		(S5P_VIC2_BASE + (x))
 
 #define S5P_TIMER_IRQ(x)	S5P_IRQ(11 + (x))
 
