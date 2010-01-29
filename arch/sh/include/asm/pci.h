03cf0641f7e9799be0b623d56b74155425130147
Paul Mundt <lethal@linux-sh.org>
2010-01-29 22:38:13 +0900

    sh: support PCI domains.
    Newer SH parts are now commonly shipping with multiple controllers, so
    we wire up PCI domain support to deal with them. Shamelessly cloned from
    the MIPS implementation.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/pci.h
+++ arch/sh/include/asm/pci.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct pci_channel {
 	struct pci_channel	*next;
+	struct pci_bus		*bus;
 
 	struct pci_ops		*pci_ops;
 	struct resource		*io_resource;
@@ -25,8 +26,10 @@ struct pci_channel {
 	unsigned long		mem_offset;
 
 	unsigned long		reg_base;
-
 	unsigned long		io_map_base;
+
+	unsigned int		index;
+	unsigned int		need_domain_info;
 };
 
 extern void register_pci_controller(struct pci_channel *hose);
@@ -108,6 +111,14 @@ extern void pcibios_resource_to_bus(struct pci_dev *dev,
 
 extern void pcibios_bus_to_resource(struct pci_dev *dev, struct resource *res,
 				    struct pci_bus_region *region);
+
+#define pci_domain_nr(bus) ((struct pci_channel *)(bus)->sysdata)->index
+
+static inline int pci_proc_domain(struct pci_bus *bus)
+{
+	struct pci_channel *hose = bus->sysdata;
+	return hose->need_domain_info;
+}
 
 /* Chances are this interrupt is wired PC-style ...  */
 static inline int pci_get_legacy_ide_irq(struct pci_dev *dev, int channel)
