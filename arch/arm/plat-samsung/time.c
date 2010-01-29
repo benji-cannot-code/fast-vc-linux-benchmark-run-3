9db991c059bd79e6cdab8db50ebc8bd0e4dcfa85
Ben Dooks <ben-linux@fluff.org>
2010-01-29 09:02:16 +0000

    ARM: SAMSUNG: Move the last build from plat-s3c to plat-samsung
    Move the init.c and time.c files to plat-samsung from plat-s3c, thus
    clearing the last files that are being built in here.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-samsung/time.c
+++ arch/arm/plat-samsung/time.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/plat-s3c24xx/time.c
+/* linux/arch/arm/plat-samsung/time.c
  *
  * Copyright (C) 2003-2005 Simtec Electronics
  *	Ben Dooks, <ben@simtec.co.uk>
