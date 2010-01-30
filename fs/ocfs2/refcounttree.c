16d7c9667add603882e121a69552e8eba8381a05
Tao Ma <tao.ma@oracle.com>
2010-01-30 23:32:19 +0800

    ocfs2: Fix memory overflow in cow_by_page.
    In ocfs2_duplicate_clusters_by_page, we calculate map_end
    by shifting page_index. But actually in case we meet with
    a large offset(say in a i686 box, poff_t is only 32 bits
    and page_index=2056240), we will overflow. So change the
    type of page_index to loff_t.
    
    Signed-off-by: Tao Ma <tao.ma@oracle.com>
    Signed-off-by: Joel Becker <joel.becker@oracle.com>

--- fs/ocfs2/refcounttree.c
+++ fs/ocfs2/refcounttree.c
@@ -2946,7 +2946,7 @@ static int ocfs2_duplicate_clusters_by_page(handle_t *handle,
 
 	while (offset < end) {
 		page_index = offset >> PAGE_CACHE_SHIFT;
-		map_end = (page_index + 1) << PAGE_CACHE_SHIFT;
+		map_end = ((loff_t)page_index + 1) << PAGE_CACHE_SHIFT;
 		if (map_end > end)
 			map_end = end;
 
@@ -3171,7 +3171,7 @@ static int ocfs2_cow_sync_writeback(struct super_block *sb,
 
 	while (offset < end) {
 		page_index = offset >> PAGE_CACHE_SHIFT;
-		map_end = (page_index + 1) << PAGE_CACHE_SHIFT;
+		map_end = ((loff_t)page_index + 1) << PAGE_CACHE_SHIFT;
 		if (map_end > end)
 			map_end = end;
 
