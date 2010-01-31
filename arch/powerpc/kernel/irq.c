546d67dfe9ac6556a7e73faafec5752ce1f345bd
Anton Blanchard <anton@samba.org>
2010-01-31 20:30:23 +0000

    powerpc: Reduce footprint of irq_stat
    PowerPC is currently using asm-generic/hardirq.h which statically allocates an
    NR_CPUS irq_stat array. Switch to an arch specific implementation which uses
    per cpu data:
    
    On a kernel with NR_CPUS=1024, this saves quite a lot of memory:
    
       text    data     bss      dec         hex    filename
    8767938 2944132 1636796 13348866         cbb002 vmlinux.baseline
    8767779 2944260 1505724 13217763         c9afe3 vmlinux.irq_cpustat
    
    A saving of around 128kB.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kernel/irq.c
+++ arch/powerpc/kernel/irq.c
@@ -74,6 +74,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CREATE_TRACE_POINTS
 #include <asm/trace.h>
 
+DEFINE_PER_CPU_SHARED_ALIGNED(irq_cpustat_t, irq_stat);
+EXPORT_PER_CPU_SYMBOL(irq_stat);
+
 int __irq_offset_value;
 static int ppc_spurious_interrupts;
 
