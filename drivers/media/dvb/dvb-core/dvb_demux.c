bc291a13df2085749915ca37303a4c97a1d5d912
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-02-01 10:35:22 -0300

    V4L/DVB: Fix the risk of an oops at dvb_dmx_release
    dvb_dmx_init tries to allocate virtual memory for 2 pointers: filter and feed.
    
    If the second vmalloc fails, filter is freed, but the pointer keeps pointing
    to the old place. Later, when dvb_dmx_release() is called, it will try to
    free an already freed memory, causing an OOPS.
    
    Reviewed-by: Andy Walls <awalls@radix.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-core/dvb_demux.c
+++ drivers/media/dvb/dvb-core/dvb_demux.c
@@ -1247,6 +1247,7 @@ int dvb_dmx_init(struct dvb_demux *dvbdemux)
 	dvbdemux->feed = vmalloc(dvbdemux->feednum * sizeof(struct dvb_demux_feed));
 	if (!dvbdemux->feed) {
 		vfree(dvbdemux->filter);
+		dvbdemux->filter = NULL;
 		return -ENOMEM;
 	}
 	for (i = 0; i < dvbdemux->filternum; i++) {
