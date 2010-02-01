1b6ee426efbc27dff0bb1b84ed61c10d6e80b5ff
Paul Mundt <lethal@linux-sh.org>
2010-02-01 13:11:25 +0900

    sh: Handle PCI controller resource conflicts.
    register_pci_controller() can fail, but presently is a void function.
    Change this over to an int so that we can bail early before continuing on
    with post-registration initialization (such as throwing the controller in
    to 66MHz mode in the case of the SH7780 host controller).
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/drivers/pci/pci-sh7780.c
+++ arch/sh/drivers/pci/pci-sh7780.c
@@ -72,6 +72,7 @@ static int __init sh7780_pci_init(void)
 	size_t memsize;
 	unsigned int id;
 	const char *type;
+	int ret;
 
 	printk(KERN_NOTICE "PCI: Starting intialization.\n");
 
@@ -198,7 +199,9 @@ static int __init sh7780_pci_init(void)
 	__raw_writel(SH4_PCICR_PREFIX | SH4_PCICR_CFIN | SH4_PCICR_FTO,
 		     chan->reg_base + SH4_PCICR);
 
-	register_pci_controller(chan);
+	ret = register_pci_controller(chan);
+	if (unlikely(ret))
+		return ret;
 
 	sh7780_pci66_init(chan);
 
