e9e2ea5742170ce6290f4d72fedd7c5bc6a905a1
Peter Zijlstra <peterz@infradead.org>
2010-02-01 11:13:39 +0100

    sched: Remove unused update_shares_locked()
    Commit f492e12ef050e02bf0185b6b57874992591b9be1 ("sched: Remove
    load_balance_newidle()") removed the only user of this function,
    so remove it too.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    LKML-Reference: <1265019219.24455.128.camel@laptop>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -1634,16 +1634,6 @@ static void update_shares(struct sched_domain *sd)
 	}
 }
 
-static void update_shares_locked(struct rq *rq, struct sched_domain *sd)
-{
-	if (root_task_group_empty())
-		return;
-
-	raw_spin_unlock(&rq->lock);
-	update_shares(sd);
-	raw_spin_lock(&rq->lock);
-}
-
 static void update_h_load(long cpu)
 {
 	if (root_task_group_empty())
@@ -1655,10 +1645,6 @@ static void update_h_load(long cpu)
 #else
 
 static inline void update_shares(struct sched_domain *sd)
-{
-}
-
-static inline void update_shares_locked(struct rq *rq, struct sched_domain *sd)
 {
 }
 
