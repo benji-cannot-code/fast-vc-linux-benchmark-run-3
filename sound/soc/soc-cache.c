3fd98f37107a5fbdbddb37961c110739756b8d72
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-02-01 18:48:03 +0000

    ASoC: Add a cache_sync bit to the CODEC structure
    Add a bit to the CODEC structure indicating if a cache sync is required.
    By default this will be set if a cache only write is done to a soc-cache
    register cache.  This allows us to avoid syncing the cache back after
    using cache only writes if there were no changes.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/soc-cache.c
+++ sound/soc/soc-cache.c
@@ -40,8 +40,10 @@ static int snd_soc_4_12_write(struct snd_soc_codec *codec, unsigned int reg,
 	if (reg < codec->reg_cache_size)
 		cache[reg] = value;
 
-	if (codec->cache_only)
+	if (codec->cache_only) {
+		codec->cache_sync = 1;
 		return 0;
+	}
 
 	ret = codec->hw_write(codec->control_data, data, 2);
 	if (ret == 2)
@@ -106,8 +108,10 @@ static int snd_soc_7_9_write(struct snd_soc_codec *codec, unsigned int reg,
 	if (reg < codec->reg_cache_size)
 		cache[reg] = value;
 
-	if (codec->cache_only)
+	if (codec->cache_only) {
+		codec->cache_sync = 1;
 		return 0;
+	}
 
 	ret = codec->hw_write(codec->control_data, data, 2);
 	if (ret == 2)
@@ -162,8 +166,10 @@ static int snd_soc_8_8_write(struct snd_soc_codec *codec, unsigned int reg,
 	if (reg < codec->reg_cache_size)
 		cache[reg] = value;
 
-	if (codec->cache_only)
+	if (codec->cache_only) {
+		codec->cache_sync = 1;
 		return 0;
+	}
 
 	if (codec->hw_write(codec->control_data, data, 2) == 2)
 		return 0;
@@ -193,8 +199,10 @@ static int snd_soc_8_16_write(struct snd_soc_codec *codec, unsigned int reg,
 	if (!snd_soc_codec_volatile_register(codec, reg))
 		reg_cache[reg] = value;
 
-	if (codec->cache_only)
+	if (codec->cache_only) {
+		codec->cache_sync = 1;
 		return 0;
+	}
 
 	if (codec->hw_write(codec->control_data, data, 3) == 3)
 		return 0;
@@ -314,8 +322,10 @@ static int snd_soc_16_8_write(struct snd_soc_codec *codec, unsigned int reg,
 	if (reg < codec->reg_cache_size)
 		cache[reg] = value;
 
-	if (codec->cache_only)
+	if (codec->cache_only) {
+		codec->cache_sync = 1;
 		return 0;
+	}
 
 	ret = codec->hw_write(codec->control_data, data, 3);
 	if (ret == 3)
