43e777031252bc5481ed628f4bb73ac9ed17d4a4
Jiri Slaby <jirislaby@gmail.com>
2010-02-02 12:43:46 +0000

    isdn: mISDN, don't compile unused stuff
    Remove these compiler warnings:
    
    drivers/isdn/hardware/mISDN/w6692.c:534: warning: `setvolume' defined but not used
    drivers/isdn/hardware/mISDN/w6692.c:561: warning: `enable_pots' defined but not used
    
    by moving the functions inside #if 0 ...  #endif.  And an alternative is
    to remove them completely if nobody has plans to use them.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Cc: Karsten Keil <isdn@linux-pingi.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/isdn/hardware/mISDN/w6692.c
+++ drivers/isdn/hardware/mISDN/w6692.c
@@ -530,6 +530,7 @@ W6692_fill_Bfifo(struct w6692_ch *wch)
 	}
 }
 
+#if 0
 static int
 setvolume(struct w6692_ch *wch, int mic, struct sk_buff *skb)
 {
@@ -572,6 +573,7 @@ enable_pots(struct w6692_ch *wch)
 	WriteW6692(card, W_PCTL, card->pctl);
 	return 0;
 }
+#endif
 
 static int
 disable_pots(struct w6692_ch *wch)
