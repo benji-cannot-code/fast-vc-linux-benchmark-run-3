dbd584651d19c357cca071493ce08b5eb1685918
Haojian Zhuang <haojian.zhuang@marvell.com>
2010-02-03 10:01:18 -0500

    [ARM] mmp2: add handling on PMIC IRQ
    Since PMIC INT pin is a special pin of CPU, the status of PMIC INT pin needs
    to be cleared after PMIC IRQ occured. Now append the clear operation in
    irq chip handler.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@marvell.com>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-mmp/common.h
+++ arch/arm/mach-mmp/common.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct sys_timer;
 
 extern void timer_init(int irq);
+extern void mmp2_clear_pmic_int(void);
 
 extern struct sys_timer pxa168_timer;
 extern struct sys_timer pxa910_timer;
