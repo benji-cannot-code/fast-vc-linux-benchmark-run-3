73659c71427c897dcdb300b36a642a7a4197570c
Ben Hutchings <bhutchings@solarflare.com>
2010-02-03 09:32:06 +0000

    sfc: Do not include unneeded headers
    Earlier refactoring has made these inclusions unnecessary.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/net_driver.h
+++ drivers/net/sfc/net_driver.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/etherdevice.h>
 #include <linux/ethtool.h>
 #include <linux/if_vlan.h>
-#include <linux/timer.h>
 #include <linux/mdio.h>
 #include <linux/list.h>
 #include <linux/pci.h>
--- drivers/net/sfc/selftest.c
+++ drivers/net/sfc/selftest.c
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "nic.h"
 #include "selftest.h"
 #include "workarounds.h"
-#include "spi.h"
-#include "io.h"
 
 /*
  * Loopback test packet structure
