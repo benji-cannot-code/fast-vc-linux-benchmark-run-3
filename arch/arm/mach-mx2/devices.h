2f63be6f302cdb3fbdfd19e14b4154e59c13ff7e
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-02-04 14:11:02 +0100

    arm/mx2: use cpp magic to create imx_gpt devices
    This makes the source shorter and easier to verify.  While at it switch
    to use the SoC-prefixed constants.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx2/devices.h
+++ arch/arm/mach-mx2/devices.h
@@ -1,9 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern struct platform_device mxc_gpt1;
 extern struct platform_device mxc_gpt2;
+#ifdef CONFIG_MACH_MX27
 extern struct platform_device mxc_gpt3;
 extern struct platform_device mxc_gpt4;
 extern struct platform_device mxc_gpt5;
+#endif
 extern struct platform_device mxc_wdt;
 extern struct platform_device mxc_uart_device0;
 extern struct platform_device mxc_uart_device1;
