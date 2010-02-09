5264be962676c57b28e0e138732065e16002dd50
Wengang Wang <wen.gang.wang@oracle.com>
2010-02-09 14:57:45 +0800

    ocfs2: Clean up the checks for CoW and direct I/O.
    When ocfs2 has to do CoW for refcounted extents, we disable direct I/O
    and go through the buffered I/O path.  This makes the combined check
    easier to read.
    
    Signed-off-by: Wengang Wang <wen.gang.wang@oracle.com>
    Signed-off-by: Joel Becker <joel.becker@oracle.com>

--- fs/ocfs2/file.c
+++ fs/ocfs2/file.c
@@ -1837,6 +1837,8 @@ static int ocfs2_prepare_inode_for_write(struct dentry *dentry,
 							       &meta_level);
 			if (has_refcount)
 				*has_refcount = 1;
+			if (direct_io)
+				*direct_io = 0;
 		}
 
 		if (ret < 0) {
@@ -1860,10 +1862,6 @@ static int ocfs2_prepare_inode_for_write(struct dentry *dentry,
 			break;
 		}
 
-		if (has_refcount && *has_refcount == 1) {
-			*direct_io = 0;
-			break;
-		}
 		/*
 		 * Allowing concurrent direct writes means
 		 * i_size changes wouldn't be synchronized, so
