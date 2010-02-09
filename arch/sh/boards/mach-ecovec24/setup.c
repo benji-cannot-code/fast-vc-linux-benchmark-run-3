6999d3db478598e8efb6803a46723c3435c0d8a5
Magnus Damm <damm@opensource.se>
2010-02-09 06:50:04 +0000

    sh: sh7724/Ecovec24/KFR2R09/MS7724SE SDHI vector merge
    Merge the SDHI vectors in the sh7724 INTC table
    and update the SDHI platform data for Ecovec24,
    KFR2R09 and MS7724SE.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-ecovec24/setup.c
+++ arch/sh/boards/mach-ecovec24/setup.c
@@ -454,7 +454,7 @@ static struct resource sdhi0_resources[] = {
 		.flags  = IORESOURCE_MEM,
 	},
 	[1] = {
-		.start  = 101,
+		.start  = 100,
 		.flags  = IORESOURCE_IRQ,
 	},
 };
@@ -490,7 +490,7 @@ static struct resource sdhi1_resources[] = {
 		.flags  = IORESOURCE_MEM,
 	},
 	[1] = {
-		.start  = 24,
+		.start  = 23,
 		.flags  = IORESOURCE_IRQ,
 	},
 };
--- arch/sh/boards/mach-se/7724/setup.c
+++ arch/sh/boards/mach-se/7724/setup.c
@@ -452,7 +452,7 @@ static struct resource sdhi0_cn7_resources[] = {
 		.flags  = IORESOURCE_MEM,
 	},
 	[1] = {
-		.start  = 101,
+		.start  = 100,
 		.flags  = IORESOURCE_IRQ,
 	},
 };
@@ -475,7 +475,7 @@ static struct resource sdhi1_cn8_resources[] = {
 		.flags  = IORESOURCE_MEM,
 	},
 	[1] = {
-		.start  = 24,
+		.start  = 23,
 		.flags  = IORESOURCE_IRQ,
 	},
 };
