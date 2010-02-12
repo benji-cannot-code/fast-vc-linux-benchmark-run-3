399eccf31524b97f3887309cc1ec6fe345146d71
Jacob Pan <jacob.jun.pan@intel.com>
2010-02-12 03:08:30 -0800

    x86, mrst: Add platform timer info parsing code
    Moorestown platform timer information is obtained from SFI FW tables.
    This patch parses SFI table then assign the irq information to mp_irqs.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@intel.com>
    LKML-Reference: <43F901BD926A4E43B106BF17856F07559FB80D0B@orsmsx508.amr.corp.intel.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/mrst.h
+++ arch/x86/include/asm/mrst.h
@@ -13,4 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X86_MRST_H
 extern int pci_mrst_init(void);
 
+#define SFI_MTMR_MAX_NUM 8
+
 #endif /* _ASM_X86_MRST_H */
