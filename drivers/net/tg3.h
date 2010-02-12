c8338fb60bf03692bd2db71aa2b5a605c152042b
Matt Carlson <mcarlson@broadcom.com>
2010-02-12 14:47:12 +0000

    tg3: Fix AC131 loopback test errors for 5785
    The AC131 does not enable the forced transmit clock settings
    immediately.  The workaround is to read the register again to get the
    setting to take effect.
    
    Signed-off-by: Matt Carlson <mcarlson@broadcom.com>
    Reviewed-by: Michael Chan <mchan@broadcom.com>
    Reviewed-by: Benjamin Li <benli@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tg3.h
+++ drivers/net/tg3.h
@@ -2113,6 +2113,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Fast Ethernet Tranceiver definitions */
 #define MII_TG3_FET_PTEST		0x17
+#define  MII_TG3_FET_PTEST_FRC_TX_LINK	0x1000
+#define  MII_TG3_FET_PTEST_FRC_TX_LOCK	0x0800
+
 #define MII_TG3_FET_TEST		0x1f
 #define  MII_TG3_FET_SHADOW_EN		0x0080
 
