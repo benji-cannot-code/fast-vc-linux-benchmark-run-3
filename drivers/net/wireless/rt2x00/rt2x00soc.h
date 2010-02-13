0a2578e280f054cc1c3e5872fe83042a7de5d3c4
Gertjan van Wingerde <gwingerde@gmail.com>
2010-02-13 20:55:48 +0100

    rt2x00: Reorganize RT chipset setting for PCI/SOC devices.
    Don't set the RT chipset for a device from within the generic PCI/SOC code,
    but rather from the individual drivers, so that individual drivers have
    more control over what RT chipset is set.
    Preparation for chip handling updates for rt2800 devices.
    
    Signed-off-by: Gertjan van Wingerde <gwingerde@gmail.com>
    Acked-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00soc.h
+++ drivers/net/wireless/rt2x00/rt2x00soc.h
@@ -29,18 +29,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define KSEG1ADDR(__ptr) __ptr
 
-#define __rt2x00soc_probe(__chipset, __ops) \
-static int __rt2x00soc_probe(struct platform_device *pdev) \
-{ \
-	return rt2x00soc_probe(pdev, (__chipset), (__ops)); \
-}
-
 /*
  * SoC driver handlers.
  */
-int rt2x00soc_probe(struct platform_device *pdev,
-		    const unsigned short chipset,
-		    const struct rt2x00_ops *ops);
+int rt2x00soc_probe(struct platform_device *pdev, const struct rt2x00_ops *ops);
 int rt2x00soc_remove(struct platform_device *pdev);
 #ifdef CONFIG_PM
 int rt2x00soc_suspend(struct platform_device *pdev, pm_message_t state);
