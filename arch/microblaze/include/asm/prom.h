d63f1b14e71050afde05e463d286f7803d43668f
Grant Likely <grant.likely@secretlab.ca>
2010-02-14 14:14:00 -0700

    of: remove undefined request_OF_resource & release_OF_resource
    Neither request_OF_resource or release_OF_resource are defined
    anywhere.  Remove the declarations.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/prom.h
+++ arch/microblaze/include/asm/prom.h
@@ -32,10 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Other Prototypes */
 extern int early_uartlite_console(void);
 
-extern struct resource *request_OF_resource(struct device_node *node,
-				int index, const char *name_postfix);
-extern int release_OF_resource(struct device_node *node, int index);
-
 /*
  * OF address retreival & translation
  */
