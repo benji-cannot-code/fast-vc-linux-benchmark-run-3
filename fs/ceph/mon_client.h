5c697e4061ad826df01d0282f81c7e67d87a3014
Sage Weil <sage@newdream.net>
2010-02-15 14:47:28 -0800

    ceph: use rbtree for mon statfs requests
    An rbtree is lighter weight, particularly given we will generally have
    very few in-flight statfs requests.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- fs/ceph/mon_client.h
+++ fs/ceph/mon_client.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _FS_CEPH_MON_CLIENT_H
 
 #include <linux/completion.h>
-#include <linux/radix-tree.h>
+#include <linux/rbtree.h>
 
 #include "messenger.h"
 #include "msgpool.h"
@@ -46,6 +46,7 @@ struct ceph_mon_request {
  */
 struct ceph_mon_statfs_request {
 	u64 tid;
+	struct rb_node node;
 	int result;
 	struct ceph_statfs *buf;
 	struct completion completion;
@@ -76,7 +77,7 @@ struct ceph_mon_client {
 	struct ceph_msgpool msgpool_auth_reply;
 
 	/* pending statfs requests */
-	struct radix_tree_root statfs_request_tree;
+	struct rb_root statfs_request_tree;
 	int num_statfs_requests;
 	u64 last_tid;
 
