e412ee2c8bd37652fd9e7417fa31453a1b0fbcfc
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2010-02-15 17:27:00 +0000

    net/9p: Add multi channel support.
    This is needed for supporting multiple mount points.
    
    We can find out the device names to be used with mount by checking
    
    /sys/devices/virtio-pci/virtio*/device file
    
    if the device file have value 9 then the specific virtio device can
    be used for mounting.
    
    ex:
     #cat /sys/devices/virtio-pci/virtio1/device
     9
    
    now we can mount using
    # mount -t 9p -o trans=virtio virtio1  /mnt/
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

--- include/linux/virtio_9p.h
+++ include/linux/virtio_9p.h
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/virtio_config.h>
 
 /* Maximum number of virtio channels per partition (1 for now) */
-#define MAX_9P_CHAN	1
+#define MAX_9P_CHAN	10
 
 #endif /* _LINUX_VIRTIO_9P_H */
