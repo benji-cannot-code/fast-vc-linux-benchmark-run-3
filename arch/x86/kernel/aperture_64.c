b82b03db254ebc4a05d3ba79b61646c93ab088ae
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-02-15 11:33:04 +0900

    x86/gart: Unexport gart_iommu_aperture
    I wrongly exported gart_iommu_aperture in the commit
    42590a75019a50012f25a962246498dead428433. It's not necessary so
    let's unexport it.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Cc: Joerg Roedel <joerg.roedel@amd.com>
    LKML-Reference: <20100215113241P.fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/aperture_64.c
+++ arch/x86/kernel/aperture_64.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/x86_init.h>
 
 int gart_iommu_aperture;
-EXPORT_SYMBOL_GPL(gart_iommu_aperture);
 int gart_iommu_aperture_disabled __initdata;
 int gart_iommu_aperture_allowed __initdata;
 
