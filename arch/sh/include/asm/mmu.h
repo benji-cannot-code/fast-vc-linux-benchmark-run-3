56623c311ca2ed90703374a6cc5eea4e27e5732c
Paul Mundt <lethal@linux-sh.org>
2010-02-16 18:39:30 +0900

    sh: Merge the legacy PMB mapping and entry synchronization code.
    This merges the code for iterating over the legacy PMB mappings and the
    code for synchronizing software state with the hardware mappings. There's
    really no reason to do the same iteration twice, and this also buys us
    the legacy entry logging facility for the dynamic PMB case.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/mmu.h
+++ arch/sh/include/asm/mmu.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PMB_C			0x00000008
 #define PMB_WT			0x00000001
 #define PMB_UB			0x00000200
+#define PMB_CACHE_MASK		(PMB_C | PMB_WT | PMB_UB)
 #define PMB_V			0x00000100
 
 #define PMB_NO_ENTRY		(-1)
