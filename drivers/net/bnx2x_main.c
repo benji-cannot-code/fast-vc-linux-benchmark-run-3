22acc7210cf0e3c3b47765bcf3dd0a275776b74d
Vladislav Zolotarov <vladz@broadcom.com>
2010-02-17 02:05:54 +0000

    bnx2x: version number and date
    Updated release version and date: 1.52.1-6 and 2010/02/16
    
    Signed-off-by: Vladislav Zolotarov <vladz@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x_main.c
+++ drivers/net/bnx2x_main.c
@@ -58,8 +58,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bnx2x_init_ops.h"
 #include "bnx2x_dump.h"
 
-#define DRV_MODULE_VERSION	"1.52.1-5"
-#define DRV_MODULE_RELDATE	"2009/11/09"
+#define DRV_MODULE_VERSION	"1.52.1-6"
+#define DRV_MODULE_RELDATE	"2010/02/16"
 #define BNX2X_BC_VER		0x040200
 
 #include <linux/firmware.h>
