de957d76797668d419db52ac03c4c1857fce0625
Nick Nunley <nicholasx.d.nunley@intel.com>
2010-02-17 01:02:59 +0000

    igb: remove adaptive IFS from driver
    Adaptive IFS support has been included in the igb driver since its
    initial release, but it is not a feature on any igb NICs. This patch
    removes it from the driver.
    
    Signed-off-by: Nicholas Nunley <nicholasx.d.nunley@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/igb/e1000_defines.h
+++ drivers/net/igb/e1000_defines.h
@@ -314,12 +314,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define E1000_PBA_34K 0x0022
 #define E1000_PBA_64K 0x0040    /* 64KB */
 
-#define IFS_MAX       80
-#define IFS_MIN       40
-#define IFS_RATIO     4
-#define IFS_STEP      10
-#define MIN_NUM_XMITS 1000
-
 /* SW Semaphore Register */
 #define E1000_SWSM_SMBI         0x00000001 /* Driver Semaphore bit */
 #define E1000_SWSM_SWESMBI      0x00000002 /* FW Semaphore bit */
