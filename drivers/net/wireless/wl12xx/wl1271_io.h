bb39781d87e990792e5b1ccb255c5941374ded52
Teemu Paasikivi <ext-teemu.3.paasikivi@nokia.com>
2010-02-18 13:25:56 +0200

    wl1271: Added IO reset and init functions
    Added reset and init functions to IO layer of the driver.
    
    Signed-off-by: Teemu Paasikivi <ext-teemu.3.paasikivi@nokia.com>
    Reviewed-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271_io.h
+++ drivers/net/wireless/wl12xx/wl1271_io.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct wl1271;
 
+void wl1271_io_reset(struct wl1271 *wl);
+void wl1271_io_init(struct wl1271 *wl);
 
 /* Raw target IO, address is not translated */
 void wl1271_raw_write(struct wl1271 *wl, int addr, void *buf,
