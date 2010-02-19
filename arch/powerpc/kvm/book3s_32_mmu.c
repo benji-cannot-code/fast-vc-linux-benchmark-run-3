395809446bb3d6bcfc186225db891f4481cf8121
Alexander Graf <agraf@suse.de>
2010-02-19 11:00:36 +0100

    KVM: PPC: Fix typo in book3s_32 debug code
    There's a typo in the debug ifdef of the book3s_32 mmu emulation. While trying
    to debug something I stumbled across that and wanted to save anyone after me
    (or myself later) from having to debug that again.
    
    So let's fix the ifdef.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/powerpc/kvm/book3s_32_mmu.c
+++ arch/powerpc/kvm/book3s_32_mmu.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define dprintk(X...) do { } while(0)
 #endif
 
-#ifdef DEBUG_PTE
+#ifdef DEBUG_MMU_PTE
 #define dprintk_pte(X...) printk(KERN_INFO X)
 #else
 #define dprintk_pte(X...) do { } while(0)
