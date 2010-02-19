a17af7c180a3893f29af119607cea7ab3be77c3b
John Holland <john.holland@cellent-fs.de>
2010-02-19 23:11:01 +0100

    [ARM] eSATA SheevaPlug: configure SoC SATA interface
    Enable the kirkwood SATA SoC interface on the eSATA SheevaPlug.
    
    Signed-off-by: John Holland <john.holland@cellent-fs.de>
    Signed-off-by: Nicolas Pitre <nico@marvell.com>
    --

--- arch/arm/mach-kirkwood/sheevaplug-setup.c
+++ arch/arm/mach-kirkwood/sheevaplug-setup.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
+#include <linux/ata_platform.h>
 #include <linux/mtd/partitions.h>
 #include <linux/mv643xx_eth.h>
 #include <linux/gpio.h>
@@ -41,6 +42,10 @@ static struct mtd_partition sheevaplug_nand_parts[] = {
 
 static struct mv643xx_eth_platform_data sheevaplug_ge00_data = {
 	.phy_addr	= MV643XX_ETH_PHY_ADDR(0),
+};
+
+static struct mv_sata_platform_data sheeva_esata_sata_data = {
+	.n_ports	= 2,
 };
 
 static struct mvsdio_platform_data sheevaplug_mvsdio_data = {
@@ -92,6 +97,11 @@ static void __init sheevaplug_init(void)
 	kirkwood_ehci_init();
 
 	kirkwood_ge00_init(&sheevaplug_ge00_data);
+
+	/* honor lower power consumption for plugs with out eSATA */
+	if (machine_is_sheeva_esata())
+		kirkwood_sata_init(&sheeva_esata_sata_data);
+
 	kirkwood_sdio_init(&sheevaplug_mvsdio_data);
 
 	platform_device_register(&sheevaplug_leds);
