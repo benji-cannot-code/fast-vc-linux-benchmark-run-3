fec60d8b85912a844c4e2f17c10c1640e812ae50
Marek Lindner <lindner_marek@yahoo.de>
2010-02-19 16:18:07 +0100

    Staging: batman-adv: remove obsolete variables
    This patch removes a variable that became obsolete since the skb handling
    replaced the packet handling thread.
    
    Signed-off-by: Marek Lindner <lindner_marek@yahoo.de>
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/batman-adv/routing.c
+++ drivers/staging/batman-adv/routing.c
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 DECLARE_WAIT_QUEUE_HEAD(thread_wait);
 
-atomic_t exit_cond;
-
 void slide_own_bcast_window(struct batman_if *batman_if)
 {
 	HASHIT(hashit);
--- drivers/staging/batman-adv/routing.h
+++ drivers/staging/batman-adv/routing.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "types.h"
 
 extern wait_queue_head_t thread_wait;
-extern atomic_t exit_cond;
 
 void slide_own_bcast_window(struct batman_if *batman_if);
 void receive_bat_packet(struct ethhdr *ethhdr,
