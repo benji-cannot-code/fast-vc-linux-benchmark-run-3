9610916617efe66c517e20cc791ef25d2835b61c
Michal Marek <mmarek@suse.cz>
2010-02-19 16:18:41 +0100

    tags: Use $SRCARCH
    $ make mrproper
    $ make tags
      GEN     tags
      find: `arch/x86_64/': No such file or directory
    
    Caused by commit f81b1be (tags: include headers before source files)
    
    Cc: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Acked-by: WANG Cong <xiyou.wangcong@gmail.com>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/tags.sh
+++ scripts/tags.sh
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # mode may be any of: tags, TAGS, cscope
 #
 # Uses the following environment variables:
-# ARCH, SUBARCH, srctree, src, obj
+# ARCH, SUBARCH, SRCARCH, srctree, src, obj
 
 if [ "$KBUILD_VERBOSE" = "1" ]; then
 	set -x
@@ -71,7 +71,7 @@ find_sources()
 
 all_sources()
 {
-	find_arch_include_sources ${ARCH} '*.[chS]'
+	find_arch_include_sources ${SRCARCH} '*.[chS]'
 	if [ ! -z "$archinclude" ]; then
 		find_arch_include_sources $archinclude '*.[chS]'
 	fi
