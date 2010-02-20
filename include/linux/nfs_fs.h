87b401043a460f2943f5cb201373d627727bbf98
Trond Myklebust <Trond.Myklebust@netapp.com>
2010-02-19 16:53:39 -0800

    NFS: Add a count of the number of unstable writes carried by an inode
    In order to know when we should do opportunistic commits of the unstable
    writes, when the VM is doing a background flush, we add a field to count
    the number of unstable writes.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- include/linux/nfs_fs.h
+++ include/linux/nfs_fs.h
@@ -167,6 +167,7 @@ struct nfs_inode {
 	struct radix_tree_root	nfs_page_tree;
 
 	unsigned long		npages;
+	unsigned long		ncommit;
 
 	/* Open contexts for shared mmap writes */
 	struct list_head	open_files;
