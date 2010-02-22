b989afe457f01ce1748a2872c49d4f1444c5b7b7
Colin Cross <ccross@android.com>
2010-02-21 17:46:23 -0800

    [ARM] tegra: SMP support
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Erik Gilling <konkers@android.com>

---
+++ arch/arm/mach-tegra/localtimer.c
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ *  arch/arm/mach-tegra/localtimer.c
+ *
+ *  Copyright (C) 2002 ARM Ltd.
+ *  All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/init.h>
+#include <linux/smp.h>
+#include <linux/clockchips.h>
+#include <asm/irq.h>
+#include <asm/smp_twd.h>
+#include <asm/localtimer.h>
+
+/*
+ * Setup the local clock events for a CPU.
+ */
+void __cpuinit local_timer_setup(struct clock_event_device *evt)
+{
+	evt->irq = IRQ_LOCALTIMER;
+	twd_timer_setup(evt);
+}
