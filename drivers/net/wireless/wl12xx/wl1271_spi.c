40ee27423c914273c743ca7d25332f59227da712
Teemu Paasikivi <ext-teemu.3.paasikivi@nokia.com>
2010-02-22 08:38:24 +0200

    wl1271: Inlined IO functions
    Changed IO functions to static inline.
    
    Signed-off-by: Teemu Paasikivi <ext-teemu.3.paasikivi@nokia.com>
    Reviewed-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271_spi.c
+++ drivers/net/wireless/wl12xx/wl1271_spi.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wl1271_spi.h"
 #include "wl1271_io.h"
 
-static struct spi_device *wl_to_spi(struct wl1271 *wl)
+static inline struct spi_device *wl_to_spi(struct wl1271 *wl)
 {
 	return wl->if_priv;
 }
