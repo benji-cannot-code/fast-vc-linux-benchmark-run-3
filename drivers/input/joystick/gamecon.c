aadf2144a92a56cb600d3a49573221225d5b93d2
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2010-02-21 20:55:31 -0800

    Input: gamecon - use pr_err() and friends
    Tested-by: Scott Moreau <oreaus@gmail.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/joystick/gamecon.c
+++ drivers/input/joystick/gamecon.c
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Vojtech Pavlik, Simunkova 1594, Prague 8, 182 00 Czech Republic
  */
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/module.h>
@@ -819,13 +821,13 @@ static int __init gc_setup_pad(struct gc *gc, int idx, int pad_type)
 	int err;
 
 	if (pad_type < 1 || pad_type > GC_MAX) {
-		printk(KERN_WARNING "gamecon.c: Pad type %d unknown\n", pad_type);
+		pr_err("Pad type %d unknown\n", pad_type);
 		return -EINVAL;
 	}
 
 	pad->dev = input_dev = input_allocate_device();
 	if (!input_dev) {
-		printk(KERN_ERR "gamecon.c: Not enough memory for input device\n");
+		pr_err("Not enough memory for input device\n");
 		return -ENOMEM;
 	}
 
@@ -869,7 +871,7 @@ static int __init gc_setup_pad(struct gc *gc, int idx, int pad_type)
 
 		err = gc_n64_init_ff(input_dev, idx);
 		if (err) {
-			printk(KERN_WARNING "gamecon.c: Failed to initiate rumble for N64 device %d\n", idx);
+			pr_warning("Failed to initiate rumble for N64 device %d\n", idx);
 			goto err_free_dev;
 		}
 
@@ -937,21 +939,21 @@ static struct gc __init *gc_probe(int parport, int *pads, int n_pads)
 
 	pp = parport_find_number(parport);
 	if (!pp) {
-		printk(KERN_ERR "gamecon.c: no such parport\n");
+		pr_err("no such parport %d\n", parport);
 		err = -EINVAL;
 		goto err_out;
 	}
 
 	pd = parport_register_device(pp, "gamecon", NULL, NULL, NULL, PARPORT_DEV_EXCL, NULL);
 	if (!pd) {
-		printk(KERN_ERR "gamecon.c: parport busy already - lp.o loaded?\n");
+		pr_err("parport busy already - lp.o loaded?\n");
 		err = -EBUSY;
 		goto err_put_pp;
 	}
 
 	gc = kzalloc(sizeof(struct gc), GFP_KERNEL);
 	if (!gc) {
-		printk(KERN_ERR "gamecon.c: Not enough memory\n");
+		pr_err("Not enough memory\n");
 		err = -ENOMEM;
 		goto err_unreg_pardev;
 	}
@@ -972,7 +974,7 @@ static struct gc __init *gc_probe(int parport, int *pads, int n_pads)
 	}
 
 	if (count == 0) {
-		printk(KERN_ERR "gamecon.c: No valid devices specified\n");
+		pr_err("No valid devices specified\n");
 		err = -EINVAL;
 		goto err_free_gc;
 	}
@@ -1016,7 +1018,7 @@ static int __init gc_init(void)
 			continue;
 
 		if (gc_cfg[i].nargs < 2) {
-			printk(KERN_ERR "gamecon.c: at least one device must be specified\n");
+			pr_err("at least one device must be specified\n");
 			err = -EINVAL;
 			break;
 		}
