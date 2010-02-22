ff96b47a8586d06e6544d22baeaacc4b402e97de
Stephen Hemminger <shemminger@vyatta.com>
2010-02-22 15:17:09 -0800

    scripts: improve checkstack
    Cleanup checkstack script:
      * Turn on strict checking
      * Fix resulting error message because the declaration syntax
        was incorrect.
      * Remove incorrect and misleading use of prototype
         - prototype not required for this type of sort function
           because $a and $b are being used in this contex
         - if prototype was being used it should be for both arguments
      * Use closure for sort function
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Arjan van de Ven <arjan@infradead.org>
    Cc: Cong Wang <amwang@redhat.com>
    Cc: Michal Marek <mmarek@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/checkstack.pl
+++ scripts/checkstack.pl
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #
 #	TODO :	Port to all architectures (one regex per arch)
 
+use strict;
+
 # check for arch
 #
 # $re is used for two matches:
@@ -105,19 +107,11 @@ my (@stack, $re, $dre, $x, $xs);
 	}
 }
 
-sub bysize($) {
-	my ($asize, $bsize);
-	($asize = $a) =~ s/.*:	*(.*)$/$1/;
-	($bsize = $b) =~ s/.*:	*(.*)$/$1/;
-	$bsize <=> $asize
-}
-
 #
 # main()
 #
 my $funcre = qr/^$x* <(.*)>:$/;
-my $func;
-my $file, $lastslash;
+my ($func, $file, $lastslash);
 
 while (my $line = <STDIN>) {
 	if ($line =~ m/$funcre/) {
@@ -174,4 +168,6 @@ while (my $line = <STDIN>) {
 	}
 }
 
-print sort bysize @stack;
+# Sort output by size (last field)
+print sort { ($b =~ /:\t*(\d+)$/)[0] <=> ($a =~ /:\t*(\d+)$/)[0] } @stack;
+
