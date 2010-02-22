3b3523997d9780d8389ec7241e5d179de519f420
Teemu Paasikivi <ext-teemu.3.paasikivi@nokia.com>
2010-02-22 08:38:23 +0200

    wl1271: Implemented abstraction of IO functions.
    Changed the driver to use if_ops structure to abstract access to the IO
    layer (SPI or SDIO).
    
    Signed-off-by: Teemu Paasikivi <ext-teemu.3.paasikivi@nokia.com>
    Reviewed-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1271_io.c
+++ drivers/net/wireless/wl12xx/wl1271_io.c
@@ -32,14 +32,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wl1271_spi.h"
 #include "wl1271_io.h"
 
+struct device *wl1271_wl_to_dev(struct wl1271 *wl)
+{
+	return wl->if_ops->dev(wl);
+}
+
 void wl1271_disable_interrupts(struct wl1271 *wl)
 {
-	wl1271_spi_disable_interrupts(wl);
+	wl->if_ops->disable_irq(wl);
 }
 
 void wl1271_enable_interrupts(struct wl1271 *wl)
 {
-	wl1271_spi_enable_interrupts(wl);
+	wl->if_ops->enable_irq(wl);
 }
 
 static int wl1271_translate_addr(struct wl1271 *wl, int addr)
@@ -128,24 +133,24 @@ int wl1271_set_partition(struct wl1271 *wl,
 
 void wl1271_io_reset(struct wl1271 *wl)
 {
-	wl1271_spi_reset(wl);
+	wl->if_ops->reset(wl);
 }
 
 void wl1271_io_init(struct wl1271 *wl)
 {
-	wl1271_spi_init(wl);
+	wl->if_ops->init(wl);
 }
 
 void wl1271_raw_write(struct wl1271 *wl, int addr, void *buf,
 		      size_t len, bool fixed)
 {
-	wl1271_spi_raw_write(wl, addr, buf, len, fixed);
+	wl->if_ops->write(wl, addr, buf, len, fixed);
 }
 
 void wl1271_raw_read(struct wl1271 *wl, int addr, void *buf,
 		     size_t len, bool fixed)
 {
-	wl1271_spi_raw_read(wl, addr, buf, len, fixed);
+	wl->if_ops->read(wl, addr, buf, len, fixed);
 }
 
 void wl1271_read(struct wl1271 *wl, int addr, void *buf, size_t len,
@@ -155,7 +160,7 @@ void wl1271_read(struct wl1271 *wl, int addr, void *buf, size_t len,
 
 	physical = wl1271_translate_addr(wl, addr);
 
-	wl1271_spi_raw_read(wl, physical, buf, len, fixed);
+	wl1271_raw_read(wl, physical, buf, len, fixed);
 }
 
 void wl1271_write(struct wl1271 *wl, int addr, void *buf, size_t len,
@@ -165,7 +170,7 @@ void wl1271_write(struct wl1271 *wl, int addr, void *buf, size_t len,
 
 	physical = wl1271_translate_addr(wl, addr);
 
-	wl1271_spi_raw_write(wl, physical, buf, len, fixed);
+	wl1271_raw_write(wl, physical, buf, len, fixed);
 }
 
 u32 wl1271_read32(struct wl1271 *wl, int addr)
--- drivers/net/wireless/wl12xx/wl1271_spi.c
+++ drivers/net/wireless/wl12xx/wl1271_spi.c
@@ -34,6 +34,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wl1271_spi.h"
 #include "wl1271_io.h"
 
+static struct spi_device *wl_to_spi(struct wl1271 *wl)
+{
+	return wl->if_priv;
+}
+
+static struct device *wl1271_spi_wl_to_dev(struct wl1271 *wl)
+{
+	return &(wl_to_spi(wl)->dev);
+}
 
 void wl1271_spi_disable_interrupts(struct wl1271 *wl)
 {
@@ -66,7 +75,7 @@ void wl1271_spi_reset(struct wl1271 *wl)
 	t.len = WSPI_INIT_CMD_LEN;
 	spi_message_add_tail(&t, &m);
 
-	spi_sync(wl->spi, &m);
+	spi_sync(wl_to_spi(wl), &m);
 
 	wl1271_dump(DEBUG_SPI, "spi reset -> ", cmd, WSPI_INIT_CMD_LEN);
 }
@@ -120,7 +129,7 @@ void wl1271_spi_init(struct wl1271 *wl)
 	t.len = WSPI_INIT_CMD_LEN;
 	spi_message_add_tail(&t, &m);
 
-	spi_sync(wl->spi, &m);
+	spi_sync(wl_to_spi(wl), &m);
 
 	wl1271_dump(DEBUG_SPI, "spi init -> ", cmd, WSPI_INIT_CMD_LEN);
 }
@@ -152,7 +161,7 @@ static void wl1271_spi_read_busy(struct wl1271 *wl, void *buf, size_t len)
 			t[0].rx_buf = buf + (len - num_busy_bytes);
 			t[0].len = num_busy_bytes;
 			spi_message_add_tail(&t[0], &m);
-			spi_sync(wl->spi, &m);
+			spi_sync(wl_to_spi(wl), &m);
 			return;
 		}
 	}
@@ -172,7 +181,7 @@ static void wl1271_spi_read_busy(struct wl1271 *wl, void *buf, size_t len)
 		t[0].rx_buf = busy_buf;
 		t[0].len = sizeof(u32);
 		spi_message_add_tail(&t[0], &m);
-		spi_sync(wl->spi, &m);
+		spi_sync(wl_to_spi(wl), &m);
 
 		if (*busy_buf & 0x1) {
 			spi_message_init(&m);
@@ -180,7 +189,7 @@ static void wl1271_spi_read_busy(struct wl1271 *wl, void *buf, size_t len)
 			t[0].rx_buf = buf;
 			t[0].len = len;
 			spi_message_add_tail(&t[0], &m);
-			spi_sync(wl->spi, &m);
+			spi_sync(wl_to_spi(wl), &m);
 			return;
 		}
 	}
@@ -226,7 +235,7 @@ void wl1271_spi_raw_read(struct wl1271 *wl, int addr, void *buf,
 	t[2].len = len;
 	spi_message_add_tail(&t[2], &m);
 
-	spi_sync(wl->spi, &m);
+	spi_sync(wl_to_spi(wl), &m);
 
 	/* FIXME: Check busy words, removed due to SPI bug */
 	/* if (!(busy_buf[WL1271_BUSY_WORD_CNT - 1] & 0x1))
@@ -264,7 +273,7 @@ void wl1271_spi_raw_write(struct wl1271 *wl, int addr, void *buf,
 	t[1].len = len;
 	spi_message_add_tail(&t[1], &m);
 
-	spi_sync(wl->spi, &m);
+	spi_sync(wl_to_spi(wl), &m);
 
 	wl1271_dump(DEBUG_SPI, "spi_write cmd -> ", cmd, sizeof(*cmd));
 	wl1271_dump(DEBUG_SPI, "spi_write buf -> ", buf, len);
@@ -307,6 +316,16 @@ static struct platform_device wl1271_device = {
 	},
 };
 
+static struct wl1271_if_operations spi_ops = {
+	.read		= wl1271_spi_raw_read,
+	.write		= wl1271_spi_raw_write,
+	.reset		= wl1271_spi_reset,
+	.init		= wl1271_spi_init,
+	.dev		= wl1271_spi_wl_to_dev,
+	.enable_irq	= wl1271_spi_enable_interrupts,
+	.disable_irq	= wl1271_spi_disable_interrupts
+};
+
 static int __devinit wl1271_probe(struct spi_device *spi)
 {
 	struct wl12xx_platform_data *pdata;
@@ -327,7 +346,9 @@ static int __devinit wl1271_probe(struct spi_device *spi)
 	wl = hw->priv;
 
 	dev_set_drvdata(&spi->dev, wl);
-	wl->spi = spi;
+	wl->if_priv = spi;
+
+	wl->if_ops = &spi_ops;
 
 	/* This is the only SPI value that we need to set here, the rest
 	 * comes from the board-peripherals file */
