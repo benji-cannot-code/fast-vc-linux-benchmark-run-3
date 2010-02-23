8e9bbe1d867f72ace2a583438c86595e5291306e
Richard Woodruff <r-woodruff2@ti.com>
2010-02-22 22:09:08 -0700

    OMAP3 clock: introduce DPLL4 Jtype
    DPLL4 for 3630 introduces a changed block called j type dpll, requiring
    special divisor bits and additional reg fields. To allow for silicons to
    use this, this is introduced as a flag and is enabled for 3630 silicon.
    OMAP4 also has j type dpll for usb.
    
    Tested with 3630 ZOOM3 and OMAP3430 ZOOM2
    
    Signed-off-by: Richard Woodruff <r-woodruff2@ti.com>
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Vishwanath BS <Vishwanath.bs@ti.com>
    [paul@pwsan.com: added some comments; updated copyrights and credits; fixed
     some style issues]
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/clock.h
+++ arch/arm/mach-omap2/clock.h
@@ -48,6 +48,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DPLL_LOW_POWER_BYPASS	0x5
 #define DPLL_LOCKED		0x7
 
+/* DPLL Type and DCO Selection Flags */
+#define DPLL_J_TYPE		0x1
+#define DPLL_NO_DCO_SEL		0x2
+
 int omap2_clk_enable(struct clk *clk);
 void omap2_clk_disable(struct clk *clk);
 long omap2_clk_round_rate(struct clk *clk, unsigned long rate);
