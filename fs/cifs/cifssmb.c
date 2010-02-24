4a2c7e0c3c26d5555fd2e39199e80b2bb55ffdf8
Steve French <sfrench@us.ibm.com>
2010-02-24 21:56:48 +0000

    [CIFS] Use unsigned ea length for clarity
    Jeff correctly noted that using unsigned ea length is more intuitive.
    CC: Jeff Lyaton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifssmb.c
+++ fs/cifs/cifssmb.c
@@ -5393,7 +5393,7 @@ QAllEAsRetry:
 	temp_fea = ea_response_data->list;
 	temp_ptr = (char *)temp_fea;
 	while (list_len > 0) {
-		int name_len;
+		unsigned int name_len;
 		__u16 value_len;
 
 		list_len -= 4;
