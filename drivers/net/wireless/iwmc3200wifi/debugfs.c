28346defeae7d325cb67b0f39ac166676c397b92
Zhu Yi <yi.zhu@intel.com>
2010-02-25 14:15:28 +0800

    iwmc3200wifi: protect rx_tickets and rx_packets[] lists
    Protect rx_tickets and rx_packets[] lists with spinlocks to fix the
    race condition for concurrent list operations. In iwmc3200wifi both
    sdio_isr_worker and rx_worker workqueues can access the rx ticket
    and packets lists at the same time under high rx load.
    
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwmc3200wifi/debugfs.c
+++ drivers/net/wireless/iwmc3200wifi/debugfs.c
@@ -281,6 +281,7 @@ static ssize_t iwm_debugfs_rx_ticket_read(struct file *filp,
 	if (!buf)
 		return -ENOMEM;
 
+	spin_lock(&iwm->ticket_lock);
 	list_for_each_entry(ticket, &iwm->rx_tickets, node) {
 		len += snprintf(buf + len, buf_len - len, "Ticket #%d\n",
 				ticket->ticket->id);
@@ -289,6 +290,7 @@ static ssize_t iwm_debugfs_rx_ticket_read(struct file *filp,
 		len += snprintf(buf + len, buf_len - len, "\tflags:  0x%x\n",
 				ticket->ticket->flags);
 	}
+	spin_unlock(&iwm->ticket_lock);
 
 	for (i = 0; i < IWM_RX_ID_HASH; i++) {
 		struct iwm_rx_packet *packet;
@@ -297,6 +299,7 @@ static ssize_t iwm_debugfs_rx_ticket_read(struct file *filp,
 		if (!list_empty(pkt_list)) {
 			len += snprintf(buf + len, buf_len - len,
 					"Packet hash #%d\n", i);
+			spin_lock(&iwm->packet_lock[i]);
 			list_for_each_entry(packet, pkt_list, node) {
 				len += snprintf(buf + len, buf_len - len,
 						"\tPacket id:     %d\n",
@@ -305,6 +308,7 @@ static ssize_t iwm_debugfs_rx_ticket_read(struct file *filp,
 						"\tPacket length: %lu\n",
 						packet->pkt_size);
 			}
+			spin_unlock(&iwm->packet_lock[i]);
 		}
 	}
 
