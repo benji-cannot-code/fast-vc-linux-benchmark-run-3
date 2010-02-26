0a717d02e750b20b66fa02a02575df78572fe02e
Rafał Miłecki <zajec5@gmail.com>
2010-02-26 08:44:57 +0000

    Revert "drm/radeon/kms: disable HDMI audio for now on rv710/rv730"
    This commit "disabled" audio on RV710 and RV740 only, leaving RV770 and RV730.
    The order is: CHIP_RV770 < CHIP_RV730 < CHIP_RV710 < CHIP_RV740.
    
    It is not needed anway, as we do not even try to enable audio on RV770 and
    newer. We call initializing function in r600.c only, not in rv770.c.
    
    If there is something causing green tinges, it's HDMI mode setting for encoder
    and I will try to debug that.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/r600_audio.c
+++ drivers/gpu/drm/radeon/r600_audio.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 static int r600_audio_chipset_supported(struct radeon_device *rdev)
 {
-	return (rdev->family >= CHIP_R600 && rdev->family < CHIP_RV710)
+	return rdev->family >= CHIP_R600
 		|| rdev->family == CHIP_RS600
 		|| rdev->family == CHIP_RS690
 		|| rdev->family == CHIP_RS740;
