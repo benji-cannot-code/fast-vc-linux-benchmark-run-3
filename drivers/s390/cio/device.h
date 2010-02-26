a9a3cb398263c3afa5d0232e7eb372c9bdd90fc5
Sebastian Ott <sebott@linux.vnet.ibm.com>
2010-02-26 22:37:28 +0100

    [S390] ccw_device_notify: improve return codes
    Callers of ccw_device_notify could not distinguish between a driver
    who has no notifier registered and a driver who doesn't want to keep
    a device after a certain event. Change this by adding proper return
    codes.
    
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/cio/device.h
+++ drivers/s390/cio/device.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ccwdev.h>
 #include <asm/atomic.h>
 #include <linux/wait.h>
-
+#include <linux/notifier.h>
 #include "io_sch.h"
 
 /*
