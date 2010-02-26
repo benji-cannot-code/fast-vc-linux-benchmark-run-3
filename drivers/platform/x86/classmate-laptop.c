38b3988e2425f2536b5b7e0c263b8aec729deaad
Bruno Prémont <bonbons@linux-vserver.org>
2010-02-26 12:59:39 +0100

    backlight: classmate-laptop - Fix missing registration failure handling
    Check newly registered backlight_device for error and properly
    return error to parent.
    Mark struct backlight_ops as const.
    
    Signed-off-by: Bruno Prémont <bonbons@linux-vserver.org>
    Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

--- drivers/platform/x86/classmate-laptop.c
+++ drivers/platform/x86/classmate-laptop.c
@@ -456,7 +456,7 @@ static int cmpc_bl_update_status(struct backlight_device *bd)
 		return -1;
 }
 
-static struct backlight_ops cmpc_bl_ops = {
+static const struct backlight_ops cmpc_bl_ops = {
 	.get_brightness = cmpc_bl_get_brightness,
 	.update_status = cmpc_bl_update_status
 };
@@ -470,6 +470,8 @@ static int cmpc_bl_add(struct acpi_device *acpi)
 	props.max_brightness = 7;
 	bd = backlight_device_register("cmpc_bl", &acpi->dev, acpi->handle,
 				       &cmpc_bl_ops, &props);
+	if (IS_ERR(bd))
+		return PTR_ERR(bd);
 	dev_set_drvdata(&acpi->dev, bd);
 	return 0;
 }
