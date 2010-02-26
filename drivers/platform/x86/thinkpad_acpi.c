671b21dfc5ac822b3cbe397123048062aeaea3cb
Henrique de Moraes Holschuh <hmh@hmh.eng.br>
2010-02-25 22:22:07 -0300

    thinkpad-acpi: fix bluetooth/wwan resume
    Studying the DSDTs of various thinkpads, it looks like bit 3 of the
    argument to SBDC and SWAN is not "set radio to last state on resume".
    Rather, it seems to be "if this bit is set, enable radio on resume,
    otherwise disable it on resume".
    
    So, the proper way to prepare the radios for S3 suspend is: disable
    radio and clear bit 3 on the SBDC/SWAN call to to resume with radio
    disabled, and enable radio and set bit 3 on the SBDC/SWAN call to
    resume with the radio enabled.
    
    Also, for persistent devices, the rfkill core does not restore state,
    so we really need to get the firmware to do the right thing.
    
    We don't sync the radio state on suspend, instead we trust the BIOS to
    not do anything weird if we never touched the radio state since boot.
    Time will tell if that's a wise way of doing things...
    
    Signed-off-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Cc: stable@kernel.org

--- drivers/platform/x86/thinkpad_acpi.c
+++ drivers/platform/x86/thinkpad_acpi.c
@@ -3879,7 +3879,7 @@ enum {
 	TP_ACPI_BLUETOOTH_HWPRESENT	= 0x01,	/* Bluetooth hw available */
 	TP_ACPI_BLUETOOTH_RADIOSSW	= 0x02,	/* Bluetooth radio enabled */
 	TP_ACPI_BLUETOOTH_RESUMECTRL	= 0x04,	/* Bluetooth state at resume:
-						   off / last state */
+						   0 = disable, 1 = enable */
 };
 
 enum {
@@ -3925,10 +3925,11 @@ static int bluetooth_set_status(enum tpacpi_rfkill_state state)
 	}
 #endif
 
-	/* We make sure to keep TP_ACPI_BLUETOOTH_RESUMECTRL off */
-	status = TP_ACPI_BLUETOOTH_RESUMECTRL;
 	if (state == TPACPI_RFK_RADIO_ON)
-		status |= TP_ACPI_BLUETOOTH_RADIOSSW;
+		status = TP_ACPI_BLUETOOTH_RADIOSSW
+			  | TP_ACPI_BLUETOOTH_RESUMECTRL;
+	else
+		status = 0;
 
 	if (!acpi_evalf(hkey_handle, NULL, "SBDC", "vd", status))
 		return -EIO;
@@ -4079,7 +4080,7 @@ enum {
 	TP_ACPI_WANCARD_HWPRESENT	= 0x01,	/* Wan hw available */
 	TP_ACPI_WANCARD_RADIOSSW	= 0x02,	/* Wan radio enabled */
 	TP_ACPI_WANCARD_RESUMECTRL	= 0x04,	/* Wan state at resume:
-						   off / last state */
+						   0 = disable, 1 = enable */
 };
 
 #define TPACPI_RFK_WWAN_SW_NAME		"tpacpi_wwan_sw"
@@ -4116,10 +4117,11 @@ static int wan_set_status(enum tpacpi_rfkill_state state)
 	}
 #endif
 
-	/* We make sure to set TP_ACPI_WANCARD_RESUMECTRL */
-	status = TP_ACPI_WANCARD_RESUMECTRL;
 	if (state == TPACPI_RFK_RADIO_ON)
-		status |= TP_ACPI_WANCARD_RADIOSSW;
+		status = TP_ACPI_WANCARD_RADIOSSW
+			 | TP_ACPI_WANCARD_RESUMECTRL;
+	else
+		status = 0;
 
 	if (!acpi_evalf(hkey_handle, NULL, "SWAN", "vd", status))
 		return -EIO;
