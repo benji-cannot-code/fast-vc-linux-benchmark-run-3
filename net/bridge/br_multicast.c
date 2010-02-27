735818e600c56788885e2cdc930738578ade791f
Herbert Xu <herbert@gondor.apana.org.au>
2010-02-27 19:41:51 +0000

    bridge: Add hash elasticity/max sysfs entries
    This patch allows the user to control the hash elasticity/max
    parameters.  The elasticity setting does not take effect until
    the next new multicast group is added.  At which point it is
    checked and if after rehashing it still can't be satisfied then
    snooping will be disabled.
    
    The max setting on the other hand takes effect immediately.  It
    must be a power of two and cannot be set to a value less than the
    current number of multicast group entries.  This is the only way
    to shrink the multicast hash.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/igmp.h>
 #include <linux/jhash.h>
 #include <linux/kernel.h>
+#include <linux/log2.h>
 #include <linux/netdevice.h>
 #include <linux/netfilter_bridge.h>
 #include <linux/random.h>
@@ -1255,6 +1256,46 @@ rollback:
 			continue;
 
 		__br_multicast_enable_port(port);
+	}
+
+unlock:
+	spin_unlock(&br->multicast_lock);
+
+	return err;
+}
+
+int br_multicast_set_hash_max(struct net_bridge *br, unsigned long val)
+{
+	int err = -ENOENT;
+	u32 old;
+
+	spin_lock(&br->multicast_lock);
+	if (!netif_running(br->dev))
+		goto unlock;
+
+	err = -EINVAL;
+	if (!is_power_of_2(val))
+		goto unlock;
+	if (br->mdb && val < br->mdb->size)
+		goto unlock;
+
+	err = 0;
+
+	old = br->hash_max;
+	br->hash_max = val;
+
+	if (br->mdb) {
+		if (br->mdb->old) {
+			err = -EEXIST;
+rollback:
+			br->hash_max = old;
+			goto unlock;
+		}
+
+		err = br_mdb_rehash(&br->mdb, br->hash_max,
+				    br->hash_elasticity);
+		if (err)
+			goto rollback;
 	}
 
 unlock:
