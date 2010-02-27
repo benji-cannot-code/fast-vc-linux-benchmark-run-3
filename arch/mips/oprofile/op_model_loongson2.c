09853331cf7e3174370aac27989f356c4035a7b0
Ralf Baechle <ralf@linux-mips.org>
2010-02-27 12:53:39 +0100

    MIPS: Loongson: Remove pointless sample_lock from oprofile code.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/oprofile/op_model_loongson2.c
+++ arch/mips/oprofile/op_model_loongson2.c
@@ -48,8 +48,6 @@ static struct loongson2_register_config {
 	int cnt1_enabled, cnt2_enabled;
 } reg;
 
-static DEFINE_SPINLOCK(sample_lock);
-
 static char *oprofid = "LoongsonPerf";
 static irqreturn_t loongson2_perfcount_handler(int irq, void *dev_id);
 /* Compute all of the registers in preparation for enabling profiling.  */
@@ -116,7 +114,6 @@ static irqreturn_t loongson2_perfcount_handler(int irq, void *dev_id)
 	uint64_t counter, counter1, counter2;
 	struct pt_regs *regs = get_irq_regs();
 	int enabled;
-	unsigned long flags;
 
 	/*
 	 * LOONGSON2 defines two 32-bit performance counters.
@@ -137,8 +134,6 @@ static irqreturn_t loongson2_perfcount_handler(int irq, void *dev_id)
 	counter1 = counter & 0xffffffff;
 	counter2 = counter >> 32;
 
-	spin_lock_irqsave(&sample_lock, flags);
-
 	if (counter1 & LOONGSON2_PERFCNT_OVERFLOW) {
 		if (reg.cnt1_enabled)
 			oprofile_add_sample(regs, 0);
@@ -149,8 +144,6 @@ static irqreturn_t loongson2_perfcount_handler(int irq, void *dev_id)
 			oprofile_add_sample(regs, 1);
 		counter2 = reg.reset_counter2;
 	}
-
-	spin_unlock_irqrestore(&sample_lock, flags);
 
 	write_c0_perfcnt((counter2 << 32) | counter1);
 
