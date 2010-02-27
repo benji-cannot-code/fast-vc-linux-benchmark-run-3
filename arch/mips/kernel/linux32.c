078d04ae6f984482fcb488ff777f69f4c586661a
Ralf Baechle <ralf@linux-mips.org>
2010-02-27 12:52:57 +0100

    MIPS: Don't include <linux/smp_lock.h> unnecessarily.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/linux32.c
+++ arch/mips/kernel/linux32.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/errno.h>
 #include <linux/file.h>
-#include <linux/smp_lock.h>
 #include <linux/highuid.h>
 #include <linux/resource.h>
 #include <linux/highmem.h>
