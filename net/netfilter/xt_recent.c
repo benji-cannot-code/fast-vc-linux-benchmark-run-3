2c00e9a7d7f6efe09a12e125966b242563545f66
Jan Engelhardt <jengelh@medozas.de>
2010-02-28 23:22:35 +0100

    netfilter: xt_recent: update description
    It had IPv6 for quite a while already :-)
    
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>

--- net/netfilter/xt_recent.c
+++ net/netfilter/xt_recent.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 MODULE_AUTHOR("Patrick McHardy <kaber@trash.net>");
 MODULE_AUTHOR("Jan Engelhardt <jengelh@medozas.de>");
-MODULE_DESCRIPTION("Xtables: \"recently-seen\" host matching for IPv4");
+MODULE_DESCRIPTION("Xtables: \"recently-seen\" host matching");
 MODULE_LICENSE("GPL");
 MODULE_ALIAS("ipt_recent");
 MODULE_ALIAS("ip6t_recent");
