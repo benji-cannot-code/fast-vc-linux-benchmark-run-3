29bcb54ca255f436904164c3a7a5a94a16e64c11
Dave Airlie <airlied@redhat.com>
2010-03-01 21:50:01 +1100

    vga_switcheroo: fix build on platforms with no ACPI
    radeon was always including the atpx code unnecessarily, also core
    switcheroo was including acpi headers.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/radeon_atpx_handler.c
+++ drivers/gpu/drm/radeon/radeon_atpx_handler.c
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * ATPX support for both Intel/ATI
  */
-
 #include <linux/vga_switcheroo.h>
 #include <acpi/acpi.h>
 #include <acpi/acpi_bus.h>
--- drivers/gpu/vga/vga_switcheroo.c
+++ drivers/gpu/vga/vga_switcheroo.c
@@ -26,9 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include <linux/fb.h>
 
-#include <acpi/acpi.h>
-#include <acpi/acpi_bus.h>
-
 #include <linux/pci.h>
 #include <linux/vga_switcheroo.h>
 
