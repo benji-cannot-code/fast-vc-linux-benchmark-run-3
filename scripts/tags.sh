85d0eefa5ef6ae45eb86815c73366795ddadad7e
John Kacur <jkacur@redhat.com>
2010-03-02 15:38:10 +0100

    tags: Fix spelling error in comment (is->if)
    Signed-off-by: John Kacur <jkacur@redhat.com>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/tags.sh
+++ scripts/tags.sh
@@ -18,7 +18,7 @@ ignore="( -name SCCS -o -name BitKeeper -o -name .svn -o \
           -name .git )                                   \
           -prune -o"
 
-# Do not use full path is we do not use O=.. builds
+# Do not use full path if we do not use O=.. builds
 if [ "${KBUILD_SRC}" = "" ]; then
 	tree=
 else
