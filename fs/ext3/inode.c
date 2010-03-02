83cb01dc351bdbf9b27a9cab350ff64066e69f1a
Dmitry Monakhov <dmonakhov@openvz.org>
2010-03-02 15:51:02 +0300

    ext3: add writepage sanity checks
    - There is theoretical possibility to perform writepage on
       RO superblock. Add explicit check for what case.
    - Page must being locked before writepage.
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/ext3/inode.c
+++ fs/ext3/inode.c
@@ -1529,6 +1529,7 @@ static int ext3_ordered_writepage(struct page *page,
 	int err;
 
 	J_ASSERT(PageLocked(page));
+	WARN_ON_ONCE(IS_RDONLY(inode));
 
 	/*
 	 * We give up here if we're reentered, because it might be for a
@@ -1601,6 +1602,9 @@ static int ext3_writeback_writepage(struct page *page,
 	int ret = 0;
 	int err;
 
+	J_ASSERT(PageLocked(page));
+	WARN_ON_ONCE(IS_RDONLY(inode));
+
 	if (ext3_journal_current_handle())
 		goto out_fail;
 
@@ -1642,6 +1646,9 @@ static int ext3_journalled_writepage(struct page *page,
 	handle_t *handle = NULL;
 	int ret = 0;
 	int err;
+
+	J_ASSERT(PageLocked(page));
+	WARN_ON_ONCE(IS_RDONLY(inode));
 
 	if (ext3_journal_current_handle())
 		goto no_write;
