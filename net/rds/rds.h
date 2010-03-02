d49d71f7b56936c59068355bcd5872bdc01a8d67
Andy Grover <andy.grover@oracle.com>
2010-03-01 16:04:59 -0800

    RDS: Move some variables around for consistency
    Also, add a comment.
    
    Signed-off-by: Andy Grover <andy.grover@oracle.com>

--- net/rds/rds.h
+++ net/rds/rds.h
@@ -302,7 +302,13 @@ struct rds_message {
 	 */
 	spinlock_t		m_rs_lock;
 	struct rds_sock		*m_rs;
+
+	/* cookie to send to remote, in rds header */
 	rds_rdma_cookie_t	m_rdma_cookie;
+
+	unsigned int		m_used_sgs;
+	unsigned int		m_total_sgs;
+
 	struct {
 		struct rm_atomic_op {
 			int			op_type;
@@ -344,8 +350,6 @@ struct rds_message {
 			struct scatterlist	*op_sg;
 		} data;
 	};
-	unsigned int		m_used_sgs;
-	unsigned int		m_total_sgs;
 };
 
 /*
