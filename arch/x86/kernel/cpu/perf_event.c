f790f6fe2f067ae1d76f9c0bae1b1b828551a681
Peter Zijlstra <a.p.zijlstra@chello.nl>
2010-03-02 21:16:55 +0100

    perf, x86: Use unlocked bitops
    There is no concurrency on these variables, so don't use LOCK'ed ops.
    
    As to the intel_pmu_handle_irq() status bit clean, nobody uses that so
    remove it all together.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: paulus@samba.org
    Cc: eranian@google.com
    Cc: robert.richter@amd.com
    Cc: fweisbec@gmail.com
    Cc: Arnaldo Carvalho de Melo <acme@infradead.org>
    LKML-Reference: <20100304140100.240023029@chello.nl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/perf_event.c
+++ arch/x86/kernel/cpu/perf_event.c
@@ -644,7 +644,7 @@ static int x86_schedule_events(struct cpu_hw_events *cpuc, int n, int *assign)
 		if (test_bit(hwc->idx, used_mask))
 			break;
 
-		set_bit(hwc->idx, used_mask);
+		__set_bit(hwc->idx, used_mask);
 		if (assign)
 			assign[i] = hwc->idx;
 	}
@@ -693,7 +693,7 @@ static int x86_schedule_events(struct cpu_hw_events *cpuc, int n, int *assign)
 			if (j == X86_PMC_IDX_MAX)
 				break;
 
-			set_bit(j, used_mask);
+			__set_bit(j, used_mask);
 
 			if (assign)
 				assign[i] = j;
@@ -843,7 +843,7 @@ void hw_perf_enable(void)
 			 * clear active_mask and events[] yet it preserves
 			 * idx
 			 */
-			set_bit(hwc->idx, cpuc->active_mask);
+			__set_bit(hwc->idx, cpuc->active_mask);
 			cpuc->events[hwc->idx] = event;
 
 			x86_pmu.enable(event);
@@ -1058,7 +1058,7 @@ static void x86_pmu_stop(struct perf_event *event)
 	 * Must be done before we disable, otherwise the nmi handler
 	 * could reenable again:
 	 */
-	clear_bit(idx, cpuc->active_mask);
+	__clear_bit(idx, cpuc->active_mask);
 	x86_pmu.disable(event);
 
 	/*
