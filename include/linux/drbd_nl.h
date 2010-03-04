e268beff7655dbe1a70864f9355ac0d09c86a06c
Philipp Reisner <philipp.reisner@linbit.com>
2010-03-04 15:51:01 +0100

    drbd: Renamed overwrite_peer to primary_force
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

--- include/linux/drbd_nl.h
+++ include/linux/drbd_nl.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 NL_PACKET(primary, 1,
-       NL_BIT(		1,	T_MAY_IGNORE,	overwrite_peer)
+       NL_BIT(		1,	T_MAY_IGNORE,	primary_force)
 )
 
 NL_PACKET(secondary, 2, )
