7c193469b221cc207bb448dc835774a867c00851
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-03-04 16:39:53 +0100

    ARM: mx3: rename mach-mx35pdk.c to mach-mx35_3ds.c matching its arch number
    Additional document all known names of that machine.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx3/mach-mx35_3ds.c
+++ arch/arm/mach-mx3/mach-mx35_3ds.c
@@ -19,6 +19,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/*
+ * This machine is known as:
+ *  - i.MX35 3-Stack Development System
+ *  - i.MX35 Platform Development Kit (i.MX35 PDK)
+ */
+
 #include <linux/types.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
