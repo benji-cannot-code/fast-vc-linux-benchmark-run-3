7894f9e867660fc3e204889f9d093c797294da97
Jean Delvare <khali@linux-fr.org>
2010-03-05 22:17:13 +0100

    hwmon: (lm90) Add support for the Winbond/Nuvoton W83L771AWG/ASG
    This chips is found on several Zotac Ion ITX boards, amongst others.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Cc: MC Matti <mcmatti17@googlemail.com>
    Cc: Manuel Lamotte-Schubert <mls@pronego.com>

--- drivers/hwmon/lm90.c
+++ drivers/hwmon/lm90.c
@@ -94,7 +94,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const unsigned short normal_i2c[] = {
 	0x18, 0x19, 0x1a, 0x29, 0x2a, 0x2b, 0x4c, 0x4d, 0x4e, I2C_CLIENT_END };
 
-enum chips { lm90, adm1032, lm99, lm86, max6657, adt7461, max6680, max6646 };
+enum chips { lm90, adm1032, lm99, lm86, max6657, adt7461, max6680, max6646,
+	w83l771 };
 
 /*
  * The LM90 registers
@@ -174,6 +175,7 @@ static const struct i2c_device_id lm90_id[] = {
 	{ "max6659", max6657 },
 	{ "max6680", max6680 },
 	{ "max6681", max6680 },
+	{ "w83l771", w83l771 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, lm90_id);
@@ -758,6 +760,14 @@ static int lm90_detect(struct i2c_client *new_client,
 		 && (reg_config1 & 0x3f) == 0x00
 		 && reg_convrate <= 0x07) {
 			name = "max6646";
+		}
+	} else
+	if (address == 0x4C
+	 && man_id == 0x5C) { /* Winbond/Nuvoton */
+		if ((chip_id & 0xFE) == 0x10 /* W83L771AWG/ASG */
+		 && (reg_config1 & 0x2A) == 0x00
+		 && reg_convrate <= 0x08) {
+			name = "w83l771";
 		}
 	}
 
