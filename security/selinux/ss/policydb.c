8aa977b6b8da6ff6cacc044c27025be9eda0ac3e
Stephen Hemminger <shemminger@vyatta.com>
2010-03-04 21:59:03 -0800

    selinux: const strings in tables
    Several places strings tables are used that should be declared
    const.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/ss/policydb.c
+++ security/selinux/ss/policydb.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _DEBUG_HASHES
 
 #ifdef DEBUG_HASHES
-static char *symtab_name[SYM_NUM] = {
+static const char *symtab_name[SYM_NUM] = {
 	"common prefixes",
 	"classes",
 	"roles",
