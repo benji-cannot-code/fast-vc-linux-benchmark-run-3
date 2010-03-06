ccf9b1f6b940b320771bf87a20a2672ef6bfac79
Krishna Gudipati <kgudipat@brocade.com>
2010-03-05 19:38:52 -0800

    [SCSI] bfa: Update the driver version to 2.1.2.1.
    Upgrade the upstream driver from 2.0.0.0 to 2.1.2.1.
    
    Signed-off-by: Krishna Gudipati <kgudipat@brocade.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/bfa/bfad_drv.h
+++ drivers/scsi/bfa/bfad_drv.h
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef BFA_DRIVER_VERSION
 #define BFAD_DRIVER_VERSION    BFA_DRIVER_VERSION
 #else
-#define BFAD_DRIVER_VERSION    "2.0.0.0"
+#define BFAD_DRIVER_VERSION    "2.1.2.1"
 #endif
 
 
