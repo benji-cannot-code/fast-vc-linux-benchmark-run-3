87a74371173923184605f740f34eb15d27492570
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-03-08 11:18:20 +0100

    ARM: imx: remove empty and unused board headers
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx3/mach-armadillo5x0.c
+++ arch/arm/mach-mx3/mach-armadillo5x0.c
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/common.h>
 #include <mach/imx-uart.h>
 #include <mach/iomux-mx3.h>
-#include <mach/board-armadillo5x0.h>
 #include <mach/mmc.h>
 #include <mach/ipu.h>
 #include <mach/mx3fb.h>
