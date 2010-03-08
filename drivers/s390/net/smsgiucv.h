78caa7d8b706ef2165426c31ef8674c44257f5b8
Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
2010-03-08 12:26:26 +0100

    [S390] smsgiucv_app: deliver z/VM CP special messages (SMSG) as uevents
    The smsgiucv_app driver registers a callback with the smsgiucv driver
    to receive z/VM CP special messages (SMSG) starting with "APP".
    
    When the callback is called for special messages, the driver creates
    an uevent for the received message.  The uevent consists of additional
    environment data containing the message prefix ("APP"), message sender,
    and message content.
    
    udev rules can be used to trigger application specific actions through
    matching the content or sender of the special message.
    
    Signed-off-by: Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/net/smsgiucv.h
+++ drivers/s390/net/smsgiucv.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Author(s): Martin Schwidefsky (schwidefsky@de.ibm.com)
  */
 
+#define SMSGIUCV_DRV_NAME     "SMSGIUCV"
+
 int  smsg_register_callback(const char *,
 			    void (*)(const char *, char *));
 void smsg_unregister_callback(const char *,
