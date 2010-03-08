25cb02cae1503479530be68fe2143688445d1e4a
Joerg Roedel <joerg.roedel@amd.com>
2010-03-08 14:20:07 +0100

    x86/amd-iommu: Use helper function to destroy domain
    In the amd_iommu_domain_destroy the protection_domain_free
    function is partly reimplemented. The 'partly' is the bug
    here because the domain is not deleted from the domain list.
    This results in use-after-free errors and data-corruption.
    Fix it by just using protection_domain_free instead.
    
    Cc: stable@kernel.org
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- arch/x86/kernel/amd_iommu.c
+++ arch/x86/kernel/amd_iommu.c
@@ -2381,9 +2381,7 @@ static void amd_iommu_domain_destroy(struct iommu_domain *dom)
 
 	free_pagetable(domain);
 
-	domain_id_free(domain->id);
-
-	kfree(domain);
+	protection_domain_free(domain);
 
 	dom->priv = NULL;
 }
