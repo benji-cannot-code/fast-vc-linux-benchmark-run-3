20ac6d1ef305980284d545bf3aa49cffddd633d1
Mike Frysinger <vapier@gentoo.org>
2010-03-09 12:25:38 -0500

    serial: bfin_sport_uart: pull in bfin_sport.h for SPORT defines
    Now that the SPORT MMR defines have been unified, switch over to it.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/serial/bfin_sport_uart.c
+++ drivers/serial/bfin_sport_uart.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty_flip.h>
 #include <linux/serial_core.h>
 
+#include <asm/bfin_sport.h>
 #include <asm/delay.h>
 #include <asm/portmux.h>
 
