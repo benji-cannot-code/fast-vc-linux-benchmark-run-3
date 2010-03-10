e733dc670addf8da2810e74caa33bdf592ad3f87
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2010-03-10 15:21:30 -0800

    viafb: yet another dead code removal
    Remove some functions that were never executed and a related undocumented
    module parameter.
    
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Cc: Scott Fang <ScottFang@viatech.com.cn>
    Cc: Joseph Chan <JosephChan@via.com.tw>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/via/global.h
+++ drivers/video/via/global.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "debug.h"
 
-#include "iface.h"
 #include "viafbdev.h"
 #include "chip.h"
 #include "accel.h"
