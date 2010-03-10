ef550c9941afa21727fff8c268edfc4da364da1d
Jassi Brar <jassi.brar@samsung.com>
2010-03-10 16:48:56 +0900

    ASoC: s3c64xx-i2s remove unncessary headers
    s3c64xx-i2s remove unncessary headers
    
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/s3c64xx-i2s.c
+++ sound/soc/s3c24xx/s3c64xx-i2s.c
@@ -13,9 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/device.h>
 #include <linux/clk.h>
 #include <linux/gpio.h>
 #include <linux/io.h>
