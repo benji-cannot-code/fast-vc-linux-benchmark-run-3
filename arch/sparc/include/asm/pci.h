ee427d9b1a0f052dd98749e07235445de272aaaf
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-03-10 15:23:28 -0800

    pci-dma: sparc: use include/linux/pci-dma.h
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: David S. Miller <davem@davemloft.net>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/sparc/include/asm/pci.h
+++ arch/sparc/include/asm/pci.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pci_32.h>
 #endif
 
+#include <linux/pci-dma.h>
+
 #include <asm-generic/pci-dma-compat.h>
 
 #endif
