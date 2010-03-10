02c2f1e798a6f19d0c4e24f930018f2898a0ff3b
Shane Wang <shane.wang@intel.com>
2010-03-10 14:36:10 +0800

    x86, tboot: Add support for S3 memory integrity protection
    This patch adds support for S3 memory integrity protection within an Intel(R)
    TXT launched kernel, for all kernel and userspace memory.  All RAM used by the
    kernel and userspace, as indicated by memory ranges of type E820_RAM and
    E820_RESERVED_KERN in the e820 table, will be integrity protected.
    
    The MAINTAINERS file is also updated to reflect the maintainers of the
    TXT-related code.
    
    All MACing is done in tboot, based on a complexity analysis and tradeoff.
    
    v3: Compared with v2, this patch adds a check of array size in
    tboot.c, and a note to specify which c/s of tboot supports this kind
    of MACing in intel_txt.txt.
    
    Signed-off-by: Shane Wang <shane.wang@intel.com>
    LKML-Reference: <4B973DDA.6050902@intel.com>
    Signed-off-by: Joseph Cihula <joseph.cihula@intel.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Acked-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/e820.h
+++ arch/x86/include/asm/e820.h
@@ -46,7 +46,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define E820_NVS	4
 #define E820_UNUSABLE	5
 
-/* reserved RAM used by kernel itself */
+/*
+ * reserved RAM used by kernel itself
+ * if CONFIG_INTEL_TXT is enabled, memory of this type will be
+ * included in the S3 integrity calculation and so should not include
+ * any memory that BIOS might alter over the S3 transition
+ */
 #define E820_RESERVED_KERN        128
 
 #ifndef __ASSEMBLY__
