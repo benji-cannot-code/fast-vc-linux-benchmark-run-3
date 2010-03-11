cd94d67c00669f692927f3a77bebf932f54d5b02
Alex Deucher <alexdeucher@gmail.com>
2010-03-11 10:01:17 -0500

    drm/radeon/kms: expose thermal/fan i2c buses
    Look up i2c bus in the power table and expose it.
    You'll need to load a hwmon driver for any chips
    on the bus, this patch just exposes the bus.
    
    Signed-off-by: Alex Deucher <alexdeucher@gmail.com>

--- drivers/gpu/drm/radeon/r420.c
+++ drivers/gpu/drm/radeon/r420.c
@@ -268,6 +268,7 @@ int r420_suspend(struct radeon_device *rdev)
 
 void r420_fini(struct radeon_device *rdev)
 {
+	radeon_pm_fini(rdev);
 	r100_cp_fini(rdev);
 	r100_wb_fini(rdev);
 	r100_ib_fini(rdev);
