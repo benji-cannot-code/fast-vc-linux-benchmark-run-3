39a4c20bc6e8c73c1c3ee46dc2cbfc89e6768eb8
Huang Weiyi <weiyi.huang@gmail.com>
2010-03-12 21:05:41 +0800

    Staging: wlan-ng: remove duplicated #include
    Remove duplicated #include('s) in
      drivers/staging/wlan-ng/p80211wext.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/wlan-ng/p80211wext.c
+++ drivers/staging/wlan-ng/p80211wext.c
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <asm/byteorder.h>
 #include <linux/if_ether.h>
-#include <linux/bitops.h>
 
 #include "p80211types.h"
 #include "p80211hdr.h"
