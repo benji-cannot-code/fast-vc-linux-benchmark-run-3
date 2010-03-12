1d4cf9a8b7392347a380f0d78c6493de747eb8d7
Michael Krufky <mkrufky@kernellabs.com>
2010-03-11 23:50:51 -0300

    V4L/DVB: lgdt3305: update lgdt3305.h header to match the header in lgdt3305.c
    Signed-off-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/lgdt3305.h
+++ drivers/media/dvb/frontends/lgdt3305.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- *    Support for LGDT3305 - VSB/QAM
+ *    Support for LG Electronics LGDT3304 and LGDT3305 - VSB/QAM
  *
  *    Copyright (C) 2008, 2009 Michael Krufky <mkrufky@linuxtv.org>
  *
