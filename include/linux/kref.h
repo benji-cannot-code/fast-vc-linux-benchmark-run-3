af5b9dd8fbc48257763ca62dae069597fd19ce07
Robert P. J. Day <rpjday@crashcourse.ca>
2010-03-13 07:58:13 -0500

    kobj: kref.h incorrectly describes itself as kref.c.
    Signed-off-by: Robert P. J. Day <rpjday@crashcourse.ca>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- include/linux/kref.h
+++ include/linux/kref.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * kref.c - library routines for handling generic reference counted objects
+ * kref.h - library routines for handling generic reference counted objects
  *
  * Copyright (C) 2004 Greg Kroah-Hartman <greg@kroah.com>
  * Copyright (C) 2004 IBM Corp.
