08348074d3f550f31e0bab78473ae5fb6d904c35
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2010-03-14 02:01:51 +0900

    nilfs2: fix discrepancy in use of static specifier
    Two segbuf functions, nilfs_segbuf_write and nilfs_segbuf_wait, are
    declared with the static storage class specifier, but their
    implementations are not.
    
    This fixes the discrepancy.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>

--- fs/nilfs2/segbuf.c
+++ fs/nilfs2/segbuf.c
@@ -471,8 +471,8 @@ static int nilfs_segbuf_submit_bh(struct nilfs_segment_buffer *segbuf,
  *
  * %-ENOMEM - Insufficient memory available.
  */
-int nilfs_segbuf_write(struct nilfs_segment_buffer *segbuf,
-		       struct the_nilfs *nilfs)
+static int nilfs_segbuf_write(struct nilfs_segment_buffer *segbuf,
+			      struct the_nilfs *nilfs)
 {
 	struct nilfs_write_info wi;
 	struct buffer_head *bh;
@@ -515,7 +515,7 @@ int nilfs_segbuf_write(struct nilfs_segment_buffer *segbuf,
  *
  * %-EIO - I/O error
  */
-int nilfs_segbuf_wait(struct nilfs_segment_buffer *segbuf)
+static int nilfs_segbuf_wait(struct nilfs_segment_buffer *segbuf)
 {
 	int err = 0;
 
