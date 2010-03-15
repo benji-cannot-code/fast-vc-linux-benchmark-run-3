293deb6c1a63bf263130c5b433f9d0323f1f9d94
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-03-15 18:25:26 +0000

    ASoC: Add WM8903 interrupt support
    Currently used to detect completion of the write sequencer.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- include/sound/wm8903.h
+++ include/sound/wm8903.h
@@ -233,6 +233,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WM8903_GP5_DB_WIDTH                          1  /* GP5_DB */
 
 struct wm8903_platform_data {
+	bool irq_active_low;   /* Set if IRQ active low, default high */
+
         /* Default register value for R6 (Mic bias), used to configure
 	 * microphone detection.  In conjunction with gpio_cfg this
 	 * can be used to route the microphone status signals out onto
