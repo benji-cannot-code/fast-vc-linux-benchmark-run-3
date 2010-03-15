f2f6c9223593af5b3ce795549c5485a6207a3492
Martin K. Petersen <martin.petersen@oracle.com>
2010-03-15 12:46:51 +0100

    block: Fix overrun in lcm() and move it to lib
    lcm() was defined to take integer-sized arguments.  The supplied
    arguments are multiplied, however, causing us to overflow given
    sufficiently large input.  That in turn led to incorrect optimal I/O
    size reporting in some cases (RAID over RAID).
    
    Switch lcm() over to unsigned long similar to gcd() and move the
    function from blk-settings.c to lib.
    
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

---
+++ lib/lcm.c
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <linux/kernel.h>
+#include <linux/gcd.h>
+#include <linux/module.h>
+
+/* Lowest common multiple */
+unsigned long lcm(unsigned long a, unsigned long b)
+{
+	if (a && b)
+		return (a * b) / gcd(a, b);
+	else if (b)
+		return b;
+
+	return a;
+}
+EXPORT_SYMBOL_GPL(lcm);
