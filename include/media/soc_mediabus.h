aab990f00c85f465ac1261542a4843559bc78b6d
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2010-03-15 19:33:31 -0300

    [media] v4l: Move the media/v4l2-mediabus.h header to include/linux
    The header defines the v4l2_mbus_framefmt structure which will be used
    by the V4L2 subdevs userspace API.
    
    Change the type of the v4l2_mbus_framefmt::code field to __u32, as enum
    sizes can differ between different ABIs on the same architectures.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/soc_mediabus.h
+++ include/media/soc_mediabus.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SOC_MEDIABUS_H
 
 #include <linux/videodev2.h>
-
-#include <media/v4l2-mediabus.h>
+#include <linux/v4l2-mediabus.h>
 
 /**
  * enum soc_mbus_packing - data packing types on the media-bus
