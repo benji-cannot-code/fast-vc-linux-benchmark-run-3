3d819671ea42f15e9115201adbd24b64f942d34c
Ruslan Pisarev <ruslan@rpisarev.org.ua>
2010-03-15 16:05:09 +0200

    Staging: wavelan: fix macros, spaces and TAB coding style issue in wavelan_cs.p.h
    This is a patch  to the wavelan.h file that fixed up a TAB and
    spaces Errors found by the checkpatch.pl tools, like
    	ERROR: spaces required around that '=' (ctx:VxV)
    and
    	ERROR: space required before the open brace '{'
    and
    	Macros complex expr -> (complex expr)
    
    Signed-off-by: Ruslan Pisarev <ruslan@rpisarev.org.ua>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/wavelan/wavelan_cs.p.h
+++ drivers/staging/wavelan/wavelan_cs.p.h
@@ -124,7 +124,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * writing a Wavelan ISA driver for the MACH microkernel. Girish
  * Welling <welling@paul.rutgers.edu> had also worked on it.
  * Keith Moore modify this for the Pcmcia hardware.
- * 
+ *
  * Robert Morris <rtm@das.harvard.edu> port these two drivers to BSDI
  * and add specific Pcmcia support (there is currently no equivalent
  * of the PCMCIA package under BSD...).
@@ -284,7 +284,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Changes made in sixth release (2.9.1a) :
  * --------------------------------------
  *	- Change the detection code for multi manufacturer code support
- *	- Correct bug (hang kernel) in init when we were "rejecting" a card 
+ *	- Correct bug (hang kernel) in init when we were "rejecting" a card
  *
  * Changes made in seventh release (2.9.1b) :
  * ----------------------------------------
@@ -515,12 +515,12 @@ static const char *version = "wavelan_cs.c : v24 (SMP + wireless extensions) 11/
 /* ------------------------ PRIVATE IOCTL ------------------------ */
 
 #define SIOCSIPQTHR	SIOCIWFIRSTPRIV		/* Set quality threshold */
-#define SIOCGIPQTHR	SIOCIWFIRSTPRIV + 1	/* Get quality threshold */
-#define SIOCSIPROAM     SIOCIWFIRSTPRIV + 2	/* Set roaming state */
-#define SIOCGIPROAM     SIOCIWFIRSTPRIV + 3	/* Get roaming state */
+#define SIOCGIPQTHR	(SIOCIWFIRSTPRIV + 1)	/* Get quality threshold */
+#define SIOCSIPROAM     (SIOCIWFIRSTPRIV + 2)	/* Set roaming state */
+#define SIOCGIPROAM     (SIOCIWFIRSTPRIV + 3)	/* Get roaming state */
 
-#define SIOCSIPHISTO	SIOCIWFIRSTPRIV + 4	/* Set histogram ranges */
-#define SIOCGIPHISTO	SIOCIWFIRSTPRIV + 5	/* Get histogram values */
+#define SIOCSIPHISTO	(SIOCIWFIRSTPRIV + 4)	/* Set histogram ranges */
+#define SIOCGIPHISTO	(SIOCIWFIRSTPRIV + 5)	/* Get histogram values */
 
 /*************************** WaveLAN Roaming  **************************/
 #ifdef WAVELAN_ROAMING		/* Conditional compile, see above in options */
@@ -533,17 +533,16 @@ static const char *version = "wavelan_cs.c : v24 (SMP + wireless extensions) 11/
 #define SEARCH_THRESH_LOW	10	/* SNR to enter cell search */
 #define SEARCH_THRESH_HIGH	13	/* SNR to leave cell search */
 #define WAVELAN_ROAMING_DELTA	1	/* Hysteresis value (+/- SNR) */
-#define CELL_TIMEOUT		2*HZ	/* in jiffies */
+#define CELL_TIMEOUT		(2*HZ)	/* in jiffies */
 
 #define FAST_CELL_SEARCH	1	/* Boolean values... */
 #define NWID_PROMISC		1	/* for code clarity. */
 
-typedef struct wavepoint_beacon
-{
+typedef struct wavepoint_beacon {
   unsigned char		dsap,		/* Unused */
 			ssap,		/* Unused */
 			ctrl,		/* Unused */
-			O,U,I,		/* Unused */
+			O, U, I,		/* Unused */
 			spec_id1,	/* Unused */
 			spec_id2,	/* Unused */
 			pdu_type,	/* Unused */
@@ -552,8 +551,7 @@ typedef struct wavepoint_beacon
 			nwid;		/* WavePoint NWID */
 } wavepoint_beacon;
 
-typedef struct wavepoint_history
-{
+typedef struct wavepoint_history {
   unsigned short	nwid;		/* WavePoint's NWID */
   int			average_slow;	/* SNR running average */
   int			average_fast;	/* SNR running average */
@@ -565,8 +563,7 @@ typedef struct wavepoint_history
   unsigned long		last_seen;	/* Time of last beacon recvd, jiffies */
 } wavepoint_history;
 
-struct wavepoint_table
-{
+struct wavepoint_table {
   wavepoint_history	*head;		/* Start of ringbuffer */
   int			num_wavepoints;	/* No. of WavePoints visible */
   unsigned char		locked;		/* Table lock */
@@ -593,12 +590,11 @@ typedef u_char		mac_addr[WAVELAN_ADDR_SIZE];	/* Hardware address */
  * keep the generic data (same format for everybody) and "net_local" keep
  * the additional specific data.
  */
-struct net_local
-{
+struct net_local {
   dev_node_t 	node;		/* ???? What is this stuff ???? */
-  struct net_device *	dev;		/* Reverse link... */
+  struct net_device *dev;		/* Reverse link... */
   spinlock_t	spinlock;	/* Serialize access to the hardware (SMP) */
-  struct pcmcia_device *	link;		/* pcmcia structure */
+  struct pcmcia_device *link;		/* pcmcia structure */
   int		nresets;	/* Number of hw resets */
   u_char	configured;	/* If it is configured */
   u_char	reconfig_82593;	/* Need to reconfigure the controller */
@@ -624,7 +620,7 @@ struct net_local
   u_long	domain_id;	/* Domain ID we lock on for roaming */
   int		filter_domains;	/* Check Domain ID of beacon found */
  struct wavepoint_table	wavepoint_table;	/* Table of visible WavePoints*/
-  wavepoint_history *	curr_point;		/* Current wavepoint */
+  wavepoint_history *curr_point;		/* Current wavepoint */
   int			cell_search;		/* Searching for new cell? */
   struct timer_list	cell_timer;		/* Garbage collection */
 #endif	/* WAVELAN_ROAMING */
@@ -674,7 +670,7 @@ static void
 		 int);		/* number of registers */
 /* ---------------------- I82593 SUBROUTINES ----------------------- */
 static int
-	wv_82593_cmd(struct net_device *,	/* synchronously send a command to i82593 */ 
+	wv_82593_cmd(struct net_device *,	/* synchronously send a command to i82593 */
 		     char *,
 		     int,
 		     int);
