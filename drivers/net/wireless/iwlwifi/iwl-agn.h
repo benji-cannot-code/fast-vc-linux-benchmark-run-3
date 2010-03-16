e9af5adbfda1abd118201974c04a35de74e4207a
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2010-03-16 10:23:31 -0700

    iwlwifi: move hcmd related code to separate file
    Multiple iwlagn based devices shared the same hcmd related functions.
    Move from iwl-5000.c to iwl-agn-hcmd.c file.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-agn.h
+++ drivers/net/wireless/iwlwifi/iwl-agn.h
@@ -67,6 +67,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-dev.h"
 
 extern struct iwl_ucode_ops iwlagn_ucode;
+extern struct iwl_hcmd_ops iwlagn_hcmd;
+extern struct iwl_hcmd_utils_ops iwlagn_hcmd_utils;
 
 int iwl_reset_ict(struct iwl_priv *priv);
 void iwl_disable_ict(struct iwl_priv *priv);
