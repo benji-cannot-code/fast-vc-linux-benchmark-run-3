f3eb46b2ef86d22f32b70fcf472ebc21956849af
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2010-03-16 10:23:29 -0700

    iwlwifi: move ucode loading related code to separated file
    Multiple iwlagn based devices shared the same ucode loading procedure.
    Move loading related code from iwl-5000.c to iwl-agn-ucode.c file.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-agn.h
+++ drivers/net/wireless/iwlwifi/iwl-agn.h
@@ -66,6 +66,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "iwl-dev.h"
 
+extern struct iwl_ucode_ops iwlagn_ucode;
+
 int iwl_reset_ict(struct iwl_priv *priv);
 void iwl_disable_ict(struct iwl_priv *priv);
 int iwl_alloc_isr_ict(struct iwl_priv *priv);
