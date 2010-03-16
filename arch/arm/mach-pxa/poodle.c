477fe6cf97b596aec313577b6fe3ab0adb4faae2
Eric Miao <eric.y.miao@gmail.com>
2010-03-16 17:21:57 +0800

    [ARM] pxa: remove unnecessary #include of <mach/ssp.h>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-pxa/poodle.c
+++ arch/arm/mach-pxa/poodle.c
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/poodle.h>
 #include <mach/pxafb.h>
 #include <mach/sharpsl.h>
-#include <mach/ssp.h>
 #include <mach/pxa2xx_spi.h>
 
 #include <asm/hardware/scoop.h>
