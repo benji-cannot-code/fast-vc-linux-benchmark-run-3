d7ad91b1cab98a1fabd4626a471bf195b3e13008
Ingo Molnar <mingo@elte.hu>
2010-03-17 12:13:28 +0100

    perf probe: Fix !dwarf build
    Fix the !drawf build.
    
    This uses the existing NO_DWARF_SUPPORT mechanism we use for that,
    but it's really fragile and needs a cleanup. (in a separate patch)
    
    1) Such uses:
    
     #ifndef NO_DWARF_SUPPORT
    
    are double inverted logic a'la 'not not'. Instead the flag should
    be called DWARF_SUPPORT.
    
    2) Furthermore, assymetric #ifdef polluted code flow like:
    
            if (need_dwarf)
     #ifdef NO_DWARF_SUPPORT
                    die("Debuginfo-analysis is not supported");
     #else   /* !NO_DWARF_SUPPORT */
                    pr_debug("Some probes require debuginfo.\n");
    
            fd = open_vmlinux();
    
    is very fragile and not acceptable. Instead of that helper functions
    should be created and the dwarf/no-dwarf logic should be separated more
    cleanly.
    
    3) Local variable #ifdefs like this:
    
     #ifndef NO_DWARF_SUPPORT
            int fd;
     #endif
    
    Are fragile as well and should be eliminated. Helper functions achieve
    that too.
    
    Cc: Masami Hiramatsu <mhiramat@redhat.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    LKML-Reference: <20100316220612.32050.33806.stgit@localhost6.localdomain6>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- tools/perf/util/probe-event.c
+++ tools/perf/util/probe-event.c
@@ -1013,8 +1013,10 @@ end_dwarf:
 	if (!sym)
 		die("Kernel symbol \'%s\' not found - probe not added.",
 		    tev->point.symbol);
+#ifndef NO_DWARF_SUPPORT
 found:
 	close(fd);
+#endif
 	return ntevs;
 }
 
@@ -1173,10 +1175,13 @@ void show_line_range(struct line_range *lr)
 	unsigned int l = 1;
 	struct line_node *ln;
 	FILE *fp;
+#ifndef NO_DWARF_SUPPORT
 	int fd, ret;
+#endif
 
 	/* Search a line range */
 	init_vmlinux();
+#ifndef NO_DWARF_SUPPORT
 	fd = open_vmlinux();
 	if (fd < 0)
 		die("Could not open debuginfo file.");
@@ -1184,6 +1189,7 @@ void show_line_range(struct line_range *lr)
 	if (ret <= 0)
 		die("Source line is not found.\n");
 	close(fd);
+#endif
 
 	setup_pager();
 
