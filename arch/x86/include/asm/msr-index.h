6e31868e00200783b0e9e3df339ba9de73433bcd
Lin Ming <ming.m.lin@intel.com>
2010-03-18 18:33:12 +0800

    perf, x86: Add cache events for the Pentium-4 PMU
    Move the HT bit setting code from p4_pmu_event_map to
    p4_hw_config. So the cache events can get HT bit set correctly.
    
    Tested on my P4 desktop, below 6 cache events work:
    
     L1-dcache-load-misses
     LLC-load-misses
     dTLB-load-misses
     dTLB-store-misses
     iTLB-loads
     iTLB-load-misses
    
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    LKML-Reference: <1268908392.13901.128.camel@minggr.sh.intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -358,6 +358,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MSR_P4_U2L_ESCR0		0x000003b0
 #define MSR_P4_U2L_ESCR1		0x000003b1
 
+#define MSR_P4_PEBS_MATRIX_VERT		0x000003f2
+
 /* Intel Core-based CPU performance counters */
 #define MSR_CORE_PERF_FIXED_CTR0	0x00000309
 #define MSR_CORE_PERF_FIXED_CTR1	0x0000030a
