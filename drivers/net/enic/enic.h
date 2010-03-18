77305e5e743034efdb217842ac4e08d8403d8d3c
Vasanthy Kolluri <vkolluri@cisco.com>
2010-03-18 16:20:04 +0000

    enic: Clean up: Change driver description; Fix tab space; Update MAINTAINERS
    1) Change enic driver description to "Cisco VIC Ethernet NIC Driver"
    2) Fix tab space
    3) Update MAINTAINERS list
    
    Signed-off-by: Scott Feldman <scofeldm@cisco.com>
    Signed-off-by: Vasanthy Kolluri <vkolluri@cisco.com>
    Signed-off-by: Roopa Prabhu <roprabhu@cisco.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/enic/enic.h
+++ drivers/net/enic/enic.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vnic_rss.h"
 
 #define DRV_NAME		"enic"
-#define DRV_DESCRIPTION		"Cisco 10G Ethernet Driver"
+#define DRV_DESCRIPTION		"Cisco VIC Ethernet NIC Driver"
 #define DRV_VERSION		"1.3.1.1"
 #define DRV_COPYRIGHT		"Copyright 2008-2009 Cisco Systems, Inc"
 #define PFX			DRV_NAME ": "
