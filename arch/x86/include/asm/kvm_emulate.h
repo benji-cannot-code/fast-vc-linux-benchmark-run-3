4165af0b36dbc952510a01bde338b06f19717fd1
Gleb Natapov <gleb@redhat.com>
2010-03-18 15:20:27 +0200

    KVM: x86 emulator: introduce pio in string read ahead.
    To optimize "rep ins" instruction do IO in big chunks ahead of time
    instead of doing it only when required during instruction emulation.
    
    Signed-off-by: Gleb Natapov <gleb@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/include/asm/kvm_emulate.h
+++ arch/x86/include/asm/kvm_emulate.h
@@ -152,6 +152,12 @@ struct fetch_cache {
 	unsigned long end;
 };
 
+struct read_cache {
+	u8 data[1024];
+	unsigned long pos;
+	unsigned long end;
+};
+
 struct decode_cache {
 	u8 twobyte;
 	u8 b;
@@ -179,6 +185,7 @@ struct decode_cache {
 	void *modrm_ptr;
 	unsigned long modrm_val;
 	struct fetch_cache fetch;
+	struct read_cache io_read;
 };
 
 struct x86_emulate_ctxt {
