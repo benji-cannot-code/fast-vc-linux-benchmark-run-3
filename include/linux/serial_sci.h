6590591d92a8fa95f8c983be2d7f5375b1370fe7
Magnus Damm <damm@opensource.se>
2010-03-19 04:47:19 +0000

    dmaengine: shdma: Enable on SH-Mobile ARM
    Enable the shdma dmaengine driver on SH-Mobile ARM.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/linux/serial_sci.h
+++ include/linux/serial_sci.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/serial_core.h>
 #ifdef CONFIG_SERIAL_SH_SCI_DMA
-#include <asm/dmaengine.h>
+#include <linux/sh_dma.h>
 #endif
 
 /*
