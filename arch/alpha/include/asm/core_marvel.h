4978dd458f8ef67400c9439f998be6178038cade
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-03-18 22:35:28 -0400

    alpha: fix compile errors in dma-mapping-common.h
    The commit c186caca3dbe7f44da624cb4f9d78e1b1dfb13b8 leads to the
    following errors with CONFIG_ALPHA_{TSUNAMI, TITAN, RAWHIDE, MARVEL}:
    /home/fujita/git/linux-2.6/include/asm-generic/dma-mapping-common.h: In
    function `dma_map_sg_attrs':
    /home/fujita/git/linux-2.6/include/asm-generic/dma-mapping-common.h:49:
    error: implicit declaration of function `for_each_sg'
    /home/fujita/git/linux-2.6/include/asm-generic/dma-mapping-common.h:50:
    error: syntax error before "kmemcheck_mark_initialized"
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Reported-by: Michael Cree <mcree@orcon.net.nz>
    Signed-off-by: Matt Turner <mattst88@gmail.com>

--- arch/alpha/include/asm/core_marvel.h
+++ arch/alpha/include/asm/core_marvel.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ALPHA_MARVEL__H__
 
 #include <linux/types.h>
-#include <linux/pci.h>
 #include <linux/spinlock.h>
 
 #include <asm/compiler.h>
