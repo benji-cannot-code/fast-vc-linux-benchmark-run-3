504d8c107a5f7b740da43ace038256c2b738dce4
Dominik Brodowski <linux@dominikbrodowski.net>
2010-03-20 14:06:24 +0100

    pcmcia: remove unused mem_op.h
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/net/wireless/ray_cs.c
+++ drivers/net/wireless/ray_cs.c
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <pcmcia/cistpl.h>
 #include <pcmcia/cisreg.h>
 #include <pcmcia/ds.h>
-#include <pcmcia/mem_op.h>
 
 #include <linux/wireless.h>
 #include <net/iw_handler.h>
--- drivers/staging/netwave/netwave_cs.c
+++ drivers/staging/netwave/netwave_cs.c
@@ -62,7 +62,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <pcmcia/cistpl.h>
 #include <pcmcia/cisreg.h>
 #include <pcmcia/ds.h>
-#include <pcmcia/mem_op.h>
 
 #include <asm/system.h>
 #include <asm/io.h>
