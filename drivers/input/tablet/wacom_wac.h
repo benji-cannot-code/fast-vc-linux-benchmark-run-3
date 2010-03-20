3995d46128bd0ac37be1923bd06810db245ebfc8
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2010-03-19 22:18:15 -0700

    Input: wacom - do not allocate wacom_wac separately
    There is no reason for allocating struct wacom_wac separately from
    struct wacom since both have the same lifetime rules and are not
    shared. Also make 'open' field a boolean.
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/tablet/wacom_wac.h
+++ drivers/input/tablet/wacom_wac.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef WACOM_WAC_H
 #define WACOM_WAC_H
 
+#include <linux/types.h>
+
 /* maximum packet length for USB devices */
 #define WACOM_PKGLEN_MAX	32
 
