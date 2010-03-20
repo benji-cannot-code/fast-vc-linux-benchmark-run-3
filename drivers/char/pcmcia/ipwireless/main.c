e72b280381d184bff8ba61c2283e57cf8a8730b4
Dominik Brodowski <linux@dominikbrodowski.net>
2010-03-20 19:43:26 +0100

    pcmcia: dev_node removal (remaining drivers)
    As a fourth step, remove any remaining usages of
    dev_node_t from drivers:
    
    - ipwireless can be simplified a bit, as we do not need
      to pass around the (write-only) dev_node_t around.
    
    - avma1_cs can be simplified as well, if we only keep the
      minor number around as "priv" data, not a full-fledged
      struct.
    
    Acked-by: Jiri Kosina <jkosina@suse.cz>
    Acked-by: Karsten Keil  <isdn@linux-pingi.de>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/char/pcmcia/ipwireless/main.c
+++ drivers/char/pcmcia/ipwireless/main.c
@@ -229,8 +229,7 @@ static int config_ipwireless(struct ipw_dev *ipw)
 	if (!ipw->network)
 		goto exit;
 
-	ipw->tty = ipwireless_tty_create(ipw->hardware, ipw->network,
-			ipw->nodes);
+	ipw->tty = ipwireless_tty_create(ipw->hardware, ipw->network);
 	if (!ipw->tty)
 		goto exit;
 
@@ -244,8 +243,6 @@ static int config_ipwireless(struct ipw_dev *ipw)
 
 	if (ret != 0)
 		goto exit;
-
-	link->dev_node = &ipw->nodes[0];
 
 	return 0;
 
@@ -306,9 +303,6 @@ static int ipwireless_attach(struct pcmcia_device *link)
 
 	ipw->link = link;
 	link->priv = ipw;
-
-	/* Link this device into our device list. */
-	link->dev_node = &ipw->nodes[0];
 
 	ipw->hardware = ipwireless_hardware_create();
 	if (!ipw->hardware) {
