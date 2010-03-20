893eb6bcdc121adf2ba926e79d9cef3f0b762abb
Dan Carpenter <error27@gmail.com>
2010-03-20 11:24:15 +0000

    Btrfs: handle kmalloc() failure in inode lookup ioctl
    Return -ENOMEM if kmalloc() fails.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -1212,6 +1212,9 @@ static noinline int btrfs_ioctl_ino_lookup(struct file *file,
 		return -EPERM;
 
 	args = kmalloc(sizeof(*args), GFP_KERNEL);
+	if (!args)
+		return -ENOMEM;
+
 	if (copy_from_user(args, argp, sizeof(*args))) {
 		kfree(args);
 		return -EFAULT;
