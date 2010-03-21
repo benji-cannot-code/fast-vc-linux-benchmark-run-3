5e60f1da7f9516f913c7aa12fbb34877ad2ffd73
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-03-21 13:00:55 -0300

    V4L/DVB: saa7134: don't wait too much to generate an IR event on raw_decode
    At raw_decode mode, the key is processed after the end of a timer. The
    previous code resets the timer every time something is received at the IR
    port. While this works fine with IR's that don't implement repeat, like
    Avermedia RM-JX IR, it keeps waiting until keydown, on IR's that implement
    NEC repeat command, like the Terratec yellow.
    
    The solution is to change the behaviour to do the timeout after the first
    received data.
    
    The timeout is currently set to 15 ms, as it works fine with NEC protcocol.
    It may need some adjustments to support other protocols and to better handle
    spurious detections that may happen with some IR sensors.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/ir-core.h
+++ include/media/ir-core.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/kfifo.h>
 #include <linux/time.h>
+#include <linux/timer.h>
 
 extern int ir_core_debug;
 #define IR_dprintk(level, fmt, arg...)	if (ir_core_debug >= level) \
@@ -64,6 +65,7 @@ struct ir_raw_event {
 struct ir_raw_event_ctrl {
 	struct kfifo			kfifo;		/* fifo for the pulse/space events */
 	struct timespec			last_event;	/* when last event occurred */
+	struct timer_list		timer_keyup;	/* timer for key release */
 };
 
 struct ir_input_dev {
