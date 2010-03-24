30849dd95b2f92cfdb9e10977473898f6cb900ff
Thomas Weber <weber@corscience.de>
2010-03-24 12:52:13 +0000

    omap: Devkit8000: Remove unnecessary include file
    Remove include otg.h.
    
    Signed-off-by: Thomas Weber <weber@corscience.de>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-devkit8000.c
+++ arch/arm/mach-omap2/board-devkit8000.c
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input/matrix_keypad.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/ads7846.h>
-#include <linux/usb/otg.h>
 #include <linux/dm9000.h>
 #include <linux/interrupt.h>
 
