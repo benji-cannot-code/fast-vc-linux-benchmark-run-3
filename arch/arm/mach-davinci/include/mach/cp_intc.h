2fb85eb9171a3146616d51976ef54bbf84cc1df9
Cyril Chemparathy <cyril@ti.com>
2010-03-25 17:43:46 -0400

    Davinci: cpintc host map configuration
    Host map configuration instructs the interrupt controller to route interrupt
    channels to FIQ or IRQ lines.  Currently, DA8xx family of devices leave these
    registers at their reset-default values.
    
    TNETV107X however does not have sane reset defaults, and therefore this
    architecture needs to reconfigure the host-map such that channels 0 and 1
    go to FIQ, and the remaining channels raise IRQs.
    
    This patch adds an optional host map argument to cp_intc_init() for this.
    
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/include/mach/cp_intc.h
+++ arch/arm/mach-davinci/include/mach/cp_intc.h
@@ -53,6 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CP_INTC_VECTOR_ADDR(n)		(0x2000 + (n << 2))
 
 void __init cp_intc_init(void __iomem *base, unsigned short num_irq,
-			 u8 *irq_prio);
+			 u8 *irq_prio, u32 *host_map);
 
 #endif	/* __ASM_HARDWARE_CP_INTC_H */
