75e1c369e59352482cf9c270e3717d4d99a3d7de
Anand Gadiyar <gadiyar@ti.com>
2010-03-25 13:25:25 +0200

    usb: musb: omap2430.c: remove unnecessary includes
    We don't need mach-types and hardware.h
    
    Signed-off-by: Anand Gadiyar <gadiyar@ti.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@nokia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/musb/omap2430.c
+++ drivers/usb/musb/omap2430.c
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/io.h>
 
-#include <asm/mach-types.h>
-#include <mach/hardware.h>
 #include <plat/mux.h>
 
 #include "musb_core.h"
