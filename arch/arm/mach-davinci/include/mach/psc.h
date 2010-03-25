199895c6d42eec0b371f5dccafd93e1006c4fda5
Cyril Chemparathy <cyril@ti.com>
2010-03-25 17:43:47 -0400

    Davinci: support LPSC SwRstDisable state
    The current clock control code always gates the clock (PSC state Disable = 2)
    on clk_disable().  Some on-chip peripherals (e.g. LCD controller on TNETV107X)
    need to be put into SwRstDisable = 0 on clock disable, to maintain
    hardware sanity.
    
    This patch extends the davinci_psc_config() arguments to pass in the desired
    module state instead of a boolean enable/disable.  Further, clk_disable() now
    checks for the PSC_SWRSTDISABLE clk flag before selecting the target state.
    
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/include/mach/psc.h
+++ arch/arm/mach-davinci/include/mach/psc.h
@@ -190,13 +190,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MDSTAT		0x800
 #define MDCTL		0xA00
 
+/* PSC module states */
+#define PSC_STATE_SWRSTDISABLE	0
+#define PSC_STATE_SYNCRST	1
+#define PSC_STATE_DISABLE	2
+#define PSC_STATE_ENABLE	3
+
 #define MDSTAT_STATE_MASK 0x1f
 
 #ifndef __ASSEMBLER__
 
 extern int davinci_psc_is_clk_active(unsigned int ctlr, unsigned int id);
 extern void davinci_psc_config(unsigned int domain, unsigned int ctlr,
-		unsigned int id, char enable);
+		unsigned int id, u32 next_state);
 
 #endif
 
