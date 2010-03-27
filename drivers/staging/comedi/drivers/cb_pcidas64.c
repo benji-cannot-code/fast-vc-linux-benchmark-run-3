6f6ed0cedf65d630e138e8f8720668b288f6ba0d
Jason Wong <tsanghan@gmail.com>
2010-03-27 09:48:43 +0800

    Staging: comedi: cb_pcidas64: fixed a coding style missed in the previous patch
    Fixed a coding style issue.
    
    Signed-off-by: Jason Wong <tsanghan@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/cb_pcidas64.c
+++ drivers/staging/comedi/drivers/cb_pcidas64.c
@@ -1922,7 +1922,7 @@ static int detach(struct comedi_device *dev)
 						    ao_dma_desc_bus_addr);
 			if (priv(dev)->main_phys_iobase)
 				comedi_pci_disable(priv(dev)->hw_dev);
-	
+
 			pci_dev_put(priv(dev)->hw_dev);
 		}
 	}
