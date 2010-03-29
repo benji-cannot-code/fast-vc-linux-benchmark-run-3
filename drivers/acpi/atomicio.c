ae71d2afc9ab149b72e9eb3ce1903b41a5f217fc
Tejun Heo <tj@kernel.org>
2010-03-30 02:52:44 +0900

    acpi: update gfp/slab.h includes
    Implicit slab.h inclusion via percpu.h is about to go away.  Make sure
    gfp.h or slab.h is included as necessary.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Len Brown <lenb@kernel.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- drivers/acpi/atomicio.c
+++ drivers/acpi/atomicio.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kref.h>
 #include <linux/rculist.h>
 #include <linux/interrupt.h>
+#include <linux/slab.h>
 #include <acpi/atomicio.h>
 
 #define ACPI_PFX "ACPI: "
