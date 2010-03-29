1ec5f8c55ffe4186bd0e3f2833e0bfef07447071
Stephen Rothwell <sfr@canb.auug.org.au>
2010-03-29 16:53:13 +1100

    mtd: SmartMedia/xD FTL: use of kmalloc/kfree requires the include of slab.h
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/sm_ftl.c
+++ drivers/mtd/sm_ftl.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/freezer.h>
 #include <linux/sysfs.h>
 #include <linux/bitops.h>
+#include <linux/slab.h>
 #include <linux/mtd/nand_ecc.h>
 #include "nand/sm_common.h"
 #include "sm_ftl.h"
