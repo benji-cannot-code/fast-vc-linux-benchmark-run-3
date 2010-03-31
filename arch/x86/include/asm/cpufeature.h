bc392db274648a89a7e718fe1387565d5e3c1a81
Borislav Petkov <borislav.petkov@amd.com>
2010-03-31 21:56:41 +0200

    x86, cpu: Add AMD core boosting feature flag to /proc/cpuinfo
    By semi-popular demand, this adds the Core Performance Boost feature
    flag to /proc/cpuinfo. Possible use case for this is userspace tools
    like cpufreq-aperf, for example, so that they don't have to jump through
    hoops of accessing "/dev/cpu/%d/cpuid" in order to check for CPB hw
    support, or call cpuid from userspace.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>
    LKML-Reference: <1270065406-1814-2-git-send-email-bp@amd64.org>
    Reviewed-by: Thomas Renninger <trenn@suse.de>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/cpufeature.h
+++ arch/x86/include/asm/cpufeature.h
@@ -162,6 +162,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define X86_FEATURE_IDA		(7*32+ 0) /* Intel Dynamic Acceleration */
 #define X86_FEATURE_ARAT	(7*32+ 1) /* Always Running APIC Timer */
+#define X86_FEATURE_CPB		(7*32+ 2) /* AMD Core Performance Boost */
 
 /* Virtualization flags: Linux defined */
 #define X86_FEATURE_TPR_SHADOW  (8*32+ 0) /* Intel TPR Shadow */
