e14a07fe7059816268388c70d75ded3fe3eb7ee8
Stephen Rothwell <sfr@canb.auug.org.au>
2010-04-01 00:28:49 -0700

    net-caif: using kmalloc/kfree requires the include of slab.h
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/caif/cfcnfg.c
+++ net/caif/cfcnfg.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/kernel.h>
 #include <linux/stddef.h>
+#include <linux/slab.h>
 #include <net/caif/caif_layer.h>
 #include <net/caif/cfpkt.h>
 #include <net/caif/cfcnfg.h>
