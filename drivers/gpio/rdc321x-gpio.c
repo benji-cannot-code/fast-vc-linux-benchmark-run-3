650d9f657ff96d52fc79a65acddaa40781c6e87d
Tejun Heo <tj@kernel.org>
2010-04-02 18:24:20 +0200

    gpio: Update gfp/slab.h includes for rdc321x
    Implicit slab.h inclusion via percpu.h is about to go away.  Make sure
    gfp.h or slab.h is included as necessary.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/gpio/rdc321x-gpio.c
+++ drivers/gpio/rdc321x-gpio.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/gpio.h>
 #include <linux/mfd/rdc321x.h>
+#include <linux/slab.h>
 
 struct rdc321x_gpio {
 	spinlock_t		lock;
