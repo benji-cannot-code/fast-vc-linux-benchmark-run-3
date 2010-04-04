d4a5c71fad90d579adef923da7a1ef7e8d8d7b8a
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-04-04 01:51:03 +0000

    bnx2x: use the DMA API instead of the pci equivalents
    The DMA API is preferred.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: Vladislav Zolotarov <vladz@broadcom.com>
    Acked-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x.h
+++ drivers/net/bnx2x.h
@@ -164,7 +164,7 @@ do {								 \
 
 struct sw_rx_bd {
 	struct sk_buff	*skb;
-	DECLARE_PCI_UNMAP_ADDR(mapping)
+	DEFINE_DMA_UNMAP_ADDR(mapping);
 };
 
 struct sw_tx_bd {
@@ -177,7 +177,7 @@ struct sw_tx_bd {
 
 struct sw_rx_page {
 	struct page	*page;
-	DECLARE_PCI_UNMAP_ADDR(mapping)
+	DEFINE_DMA_UNMAP_ADDR(mapping);
 };
 
 union db_prod {
