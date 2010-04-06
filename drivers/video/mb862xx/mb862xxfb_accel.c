77b5f9e910dd4685267500018ef309f4697f8d72
Randy Dunlap <randy.dunlap@oracle.com>
2010-04-06 14:34:57 -0700

    mb862xxfb: fix acceleration module license
    mb862xxfb_accel built as a separate module, but it does not have a
    MODULE_LICENSE, so it taints the kernel.  Add a MODULE_LICENSE to it (same
    as mb862xxfb license).
    
    mb862xxfb_accel: module license 'unspecified' taints kernel.
    
    Or should mb862xxfb_accel be built into the mb862xxfb binary file instead?
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Acked-by: Alexander Shishkin <virtuoso@slind.org>
    Cc: Valentin Sitdikov <v.sitdikov@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/mb862xx/mb862xxfb_accel.c
+++ drivers/video/mb862xx/mb862xxfb_accel.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
+#include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/slab.h>
 #if defined(CONFIG_OF)
@@ -331,3 +332,5 @@ void mb862xxfb_init_accel(struct fb_info *info, int xres)
 	info->fix.accel = 0xff;	/*FIXME: add right define */
 }
 EXPORT_SYMBOL(mb862xxfb_init_accel);
+
+MODULE_LICENSE("GPL v2");
