4c0f3bb1cb4bddf277a2739555cea1afa2ea21c1
Huang Weiyi <weiyi.huang@gmail.com>
2010-04-08 19:49:36 +0800

    Staging: crystalhd: remove unused #include <linux/version.h>
    Remove unused #include <linux/version.h>('s) in
      drivers/staging/crystalhd/crystalhd_lnx.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/crystalhd/crystalhd_lnx.c
+++ drivers/staging/crystalhd/crystalhd_lnx.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
   along with this driver.  If not, see <http://www.gnu.org/licenses/>.
 ***************************************************************************/
 
-#include <linux/version.h>
 #include <linux/smp_lock.h>
 #include <linux/slab.h>
 
