b9e917644058f6b28ba355f5cc593b8907cff45d
Stephen Rothwell <sfr@canb.auug.org.au>
2010-04-08 13:42:03 +1000

    drm/ttm: using kmalloc/kfree requires including slab.h
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/ttm/ttm_page_alloc.c
+++ drivers/gpu/drm/ttm/ttm_page_alloc.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/mm.h>
 #include <linux/seq_file.h> /* for seq_printf */
+#include <linux/slab.h>
 
 #include <asm/atomic.h>
 #include <asm/agp.h>
