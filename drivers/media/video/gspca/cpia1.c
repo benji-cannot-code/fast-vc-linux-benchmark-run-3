900ab9e8c6f39e40bc61d861466a62cfb7280994
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-04-08 19:09:13 -0300

    V4L/DVB: cpia1: comment unused command_setlights()
    drivers/media/video/gspca/cpia1.c:1063: warning: ‘command_setlights’ defined but not used
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/cpia1.c
+++ drivers/media/video/gspca/cpia1.c
@@ -1060,6 +1060,7 @@ static int command_resume(struct gspca_dev *gspca_dev)
 			  0, sd->params.streamStartLine, 0, 0);
 }
 
+#if 0 /* Currently unused */
 static int command_setlights(struct gspca_dev *gspca_dev)
 {
 	struct sd *sd = (struct sd *) gspca_dev;
@@ -1079,6 +1080,7 @@ static int command_setlights(struct gspca_dev *gspca_dev)
 	return do_command(gspca_dev, CPIA_COMMAND_WriteMCPort, 2, 0,
 			  p1 | p2 | 0xE0, 0);
 }
+#endif
 
 static int set_flicker(struct gspca_dev *gspca_dev, int on, int apply)
 {
