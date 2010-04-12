27ebd6dfe2bbbcdb324ad10974545dc9493829d1
Ming Lei <tom.leiming@gmail.com>
2010-04-13 00:28:53 +0800

    ath9k-htc:respect usb buffer cacheline alignment in ath9k_hif_usb_alloc_rx_urbs
    In ath9k_hif_usb_alloc_rx_urbs, ath9k-htc will pass skb->data into
    usb hcd and usb hcd will do dma mapping and unmapping to the buffer
    pointed by skb->data, so we should pass a cache-line aligned address.
    
    This patch replace __dev_alloc_skb with alloc_skb to make skb->data
    pointed to a cacheline aligned address simply since ath9k-htc does not
    skb_push on the skb and pass it to mac80211, also use kfree_skb to free
    the skbs allocated by alloc_skb(we can use kfree_skb safely in hardirq
    context since skb->destructor is NULL always in the path).
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/hif_usb.c
+++ drivers/net/wireless/ath/ath9k/hif_usb.c
@@ -468,7 +468,7 @@ resubmit:
 
 	return;
 free:
-	dev_kfree_skb_any(skb);
+	kfree_skb(skb);
 }
 
 static void ath9k_hif_usb_reg_in_cb(struct urb *urb)
@@ -626,7 +626,7 @@ static int ath9k_hif_usb_alloc_rx_urbs(struct hif_device_usb *hif_dev)
 		}
 
 		/* Allocate buffer */
-		skb = __dev_alloc_skb(MAX_RX_BUF_SIZE, GFP_KERNEL);
+		skb = alloc_skb(MAX_RX_BUF_SIZE, GFP_KERNEL);
 		if (!skb) {
 			ret = -ENOMEM;
 			goto err_skb;
@@ -658,7 +658,7 @@ static int ath9k_hif_usb_alloc_rx_urbs(struct hif_device_usb *hif_dev)
 	return 0;
 
 err_submit:
-	dev_kfree_skb_any(skb);
+	kfree_skb(skb);
 err_skb:
 	usb_free_urb(urb);
 err_urb:
