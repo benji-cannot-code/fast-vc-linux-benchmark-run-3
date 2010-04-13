fd34810da9936610765f487e36989dbb8f1b5918
Grant Likely <grant.likely@secretlab.ca>
2010-04-13 16:12:59 -0700

    arch/powerpc: Move dma_mask from of_device into pdev_archdata
    By moving dma_mask into pdev_archdata, and adding archdata to
    struct of_device, it makes it possible to substitute of_device
    with struct platform_device, which is a stepping stone to
    removing the of_platform bus entirely.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/powerpc/include/asm/of_device.h
+++ arch/powerpc/include/asm/of_device.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct of_device
 {
-	u64			dma_mask;	/* DMA mask */
 	struct device		dev;		/* Generic device interface */
+	struct pdev_archdata	archdata;
 };
 
 extern struct of_device *of_device_alloc(struct device_node *np,
