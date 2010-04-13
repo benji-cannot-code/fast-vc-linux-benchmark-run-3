275c33ab5b4fea086aff91dec5a67fecb4543ebc
Zhitong Wang <zhitong.wangzt@alibaba-inc.com>
2010-04-13 11:25:41 +0200

    netfilter: fix some coding styles and remove moduleparam.h
    Fix some coding styles and remove moduleparam.h
    
    Signed-off-by: Zhitong Wang <zhitong.wangzt@alibaba-inc.com>
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/netfilter/nf_conntrack_proto.c
+++ net/netfilter/nf_conntrack_proto.c
@@ -14,12 +14,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netfilter.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/skbuff.h>
 #include <linux/vmalloc.h>
 #include <linux/stddef.h>
 #include <linux/err.h>
 #include <linux/percpu.h>
-#include <linux/moduleparam.h>
 #include <linux/notifier.h>
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
