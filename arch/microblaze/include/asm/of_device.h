cfc0b9020f31126e810b628199645836b13ecf75
Grant Likely <grant.likely@secretlab.ca>
2010-04-13 16:13:00 -0700

    arch/microblaze: Move dma_mask from of_device into pdev_archdata
    By moving dma_mask into pdev_archdata, and adding archdata to
    struct of_device, it makes it possible to substitute of_device
    with struct platform_device, which is a stepping stone to
    removing the of_platform bus entirely.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/microblaze/include/asm/of_device.h
+++ arch/microblaze/include/asm/of_device.h
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * probed using OF properties.
  */
 struct of_device {
-	u64			dma_mask; /* DMA mask */
 	struct device		dev; /* Generic device interface */
+	struct pdev_archdata	archdata;
 };
 
 extern ssize_t of_device_get_modalias(struct of_device *ofdev,
