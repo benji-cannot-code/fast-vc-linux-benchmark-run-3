57825bfe5b4d1bda9c2d6248e28067cab020aa2a
Takashi Iwai <tiwai@suse.de>
2010-04-14 14:33:57 +0200

    ALSA: hda - Fix control element allocations in VIA codec parser
    The commit 5b0cb1d850c26893b1468b3a519433a1b7a176be
        ALSA: hda - add more NID->Control mapping
    breaks the control element allocation by returning a wrong value.
    Let's fix it.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_via.c
+++ sound/pci/hda/patch_via.c
@@ -477,7 +477,7 @@ static struct snd_kcontrol_new *via_clone_control(struct via_spec *spec,
 	knew->name = kstrdup(tmpl->name, GFP_KERNEL);
 	if (!knew->name)
 		return NULL;
-	return 0;
+	return knew;
 }
 
 static void via_free_kctls(struct hda_codec *codec)
