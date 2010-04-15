28cd1e2f0490e936426eefe2806754c4c3718631
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-04-15 17:39:15 -0400

    ath9k_hw: add OFDM spur mitigation for AR9003
    We add this now as OFDM spur mitigation required accessing
    the EEPROM for the AR9003 devices.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/phy.h
+++ drivers/net/wireless/ath/ath9k/phy.h
@@ -60,4 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 			(_bank)[i] = INI_RA((_iniarray), i, _col);;	\
 	} while (0)
 
+#define	AR_PHY_TIMING11_SPUR_FREQ_SD		0x3FF00000
+#define	AR_PHY_TIMING11_SPUR_FREQ_SD_S		20
+
 #endif
