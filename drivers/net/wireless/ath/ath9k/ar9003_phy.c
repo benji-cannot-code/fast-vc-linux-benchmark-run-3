11bc4527dd4403cb699f531fb826bcb5c187516c
Felix Fietkau <nbd@openwrt.org>
2010-04-15 17:38:31 -0400

    ath9k_hw: Add AR9003 PHY register definitions
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/ar9003_phy.c
+++ drivers/net/wireless/ath/ath9k/ar9003_phy.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "hw.h"
+#include "ar9003_phy.h"
 
 /**
  * ar9003_hw_set_channel - set channel on single-chip device
