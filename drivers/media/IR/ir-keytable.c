d53db758801a29509a200076babde1932046947c
David Härdeman <david@hardeman.nu>
2010-04-15 18:46:35 -0300

    V4L/DVB: ir-core: fix some confusing comments
    Fix some confusing comments in drivers/media/IR/*
    
    Signed-off-by: David Härdeman <david@hardeman.nu>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/ir-keytable.c
+++ drivers/media/IR/ir-keytable.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* ir-register.c - handle IR scancode->keycode tables
+/* ir-keytable.c - handle IR scancode->keycode tables
  *
  * Copyright (C) 2009 by Mauro Carvalho Chehab <mchehab@redhat.com>
  *
