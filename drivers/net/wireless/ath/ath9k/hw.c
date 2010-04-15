6d3f9df9f2c01025057877326347605e9619ed04
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-04-15 17:38:36 -0400

    ath9k_hw: split initvals.h by hardware family
    The initvals.h file is over 7000 lines now, so instead of adding
    AR9003 initvals to it instead lets split the current initvals.h by
    hardware family: AR5008, AR9001, AR9002
    
    The AR9003 family will have its own initval file later.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/hw.c
+++ drivers/net/wireless/ath/ath9k/hw.c
@@ -21,7 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hw.h"
 #include "hw-ops.h"
 #include "rc.h"
-#include "initvals.h"
+#include "ar5008_initvals.h"
+#include "ar9001_initvals.h"
+#include "ar9002_initvals.h"
 
 #define ATH9K_CLOCK_RATE_CCK		22
 #define ATH9K_CLOCK_RATE_5GHZ_OFDM	40
