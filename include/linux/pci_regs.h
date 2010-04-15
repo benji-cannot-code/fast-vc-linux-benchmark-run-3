42a3cf01595101fe4cca4bfd0daf011045ac2610
Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
2010-04-15 13:17:33 +0900

    PCI: aerdrv: redefine PCI_ERR_ROOT_*_SRC
    The Error Source Identification Register (Offset 34h) is 4 byte
    which contains a couple of 2 byte field, "[15:0] ERR_COR Source
    Identification" and "[31:16] ERR_FATAL/NONFATAL Source Identification."
    
    This patch defines PCI_ERR_ROOT_ERR_SRC to make dword access sensible.
    
    Signed-off-by: Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
    Reviewed-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- include/linux/pci_regs.h
+++ include/linux/pci_regs.h
@@ -564,8 +564,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCI_ERR_ROOT_FIRST_FATAL	0x00000010	/* First Fatal */
 #define PCI_ERR_ROOT_NONFATAL_RCV	0x00000020	/* Non-Fatal Received */
 #define PCI_ERR_ROOT_FATAL_RCV		0x00000040	/* Fatal Received */
-#define PCI_ERR_ROOT_COR_SRC	52
-#define PCI_ERR_ROOT_SRC	54
+#define PCI_ERR_ROOT_ERR_SRC	52	/* Error Source Identification */
 
 /* Virtual Channel */
 #define PCI_VC_PORT_REG1	4
