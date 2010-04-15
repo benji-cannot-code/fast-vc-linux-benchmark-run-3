a67e95b099c25a750699f946fa6a39ee475accc1
Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
2010-04-15 13:18:26 +0900

    PCI: aerdrv: remove magical ROOT_ERR_STATUS_MASKS
    Make it clear that we only interest in 2 *_RCV bits.
    
    Signed-off-by: Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
    Reviewed-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pcie/aer/aerdrv.h
+++ drivers/pci/pcie/aer/aerdrv.h
@@ -18,9 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AER_FATAL			1
 #define AER_CORRECTABLE			2
 
-/* Root Error Status Register Bits */
-#define ROOT_ERR_STATUS_MASKS		0x0f
-
 #define SYSTEM_ERROR_INTR_ON_MESG_MASK	(PCI_EXP_RTCTL_SECEE|	\
 					PCI_EXP_RTCTL_SENFEE|	\
 					PCI_EXP_RTCTL_SEFEE)
