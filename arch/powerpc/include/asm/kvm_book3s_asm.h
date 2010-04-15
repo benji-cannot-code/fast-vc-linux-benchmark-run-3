54abf80b08e926d31cada71f5c1d4a0c83a61de6
Alexander Graf <agraf@suse.de>
2010-04-16 00:11:41 +0200

    KVM: PPC: Use KVM_BOOK3S_HANDLER
    So far we had a lot of conditional code on CONFIG_KVM_BOOK3S_64_HANDLER.
    As we're moving towards common code between 32 and 64 bits, most of
    these ifdefs can be moved to a more generic term define, called
    CONFIG_KVM_BOOK3S_HANDLER.
    
    This patch adds the new generic config option and moves ifdefs over.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/powerpc/include/asm/kvm_book3s_asm.h
+++ arch/powerpc/include/asm/kvm_book3s_asm.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __ASSEMBLY__
 
-#ifdef CONFIG_KVM_BOOK3S_64_HANDLER
+#ifdef CONFIG_KVM_BOOK3S_HANDLER
 
 #include <asm/kvm_asm.h>
 
@@ -56,7 +56,7 @@ kvmppc_resume_\intno:
 .macro DO_KVM intno
 .endm
 
-#endif /* CONFIG_KVM_BOOK3S_64_HANDLER */
+#endif /* CONFIG_KVM_BOOK3S_HANDLER */
 
 #else  /*__ASSEMBLY__ */
 
