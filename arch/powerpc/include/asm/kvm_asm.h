e4c054af42cfa64c008e89c0fbd043d34c8eae3c
Alexander Graf <agraf@suse.de>
2010-04-20 02:49:54 +0200

    KVM: PPC: Enable native paired singles
    When we're on a paired single capable host, we can just always enable
    paired singles and expose them to the guest directly.
    
    This approach breaks when multiple VMs run and access PS concurrently,
    but this should suffice until we get a proper framework for it in Linux.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/powerpc/include/asm/kvm_asm.h
+++ arch/powerpc/include/asm/kvm_asm.h
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BOOK3S_HFLAG_DCBZ32			0x1
 #define BOOK3S_HFLAG_SLB			0x2
 #define BOOK3S_HFLAG_PAIRED_SINGLE		0x4
+#define BOOK3S_HFLAG_NATIVE_PS			0x8
 
 #define RESUME_FLAG_NV          (1<<0)  /* Reload guest nonvolatile state? */
 #define RESUME_FLAG_HOST        (1<<1)  /* Resume host? */
