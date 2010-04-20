92a47ccd725963ef0d4406a948f2c45e1816a4e2
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2010-04-19 22:24:22 -0700

    rcu: make SRCU usable in modules
    Add a #include for mutex.h to allow SRCU to be more easily used in
    kernel modules.
    
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/srcu.h
+++ include/linux/srcu.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SRCU_H
 #define _LINUX_SRCU_H
 
+#include <linux/mutex.h>
+
 struct srcu_struct_array {
 	int c[2];
 };
