efbfc79527541a52c17eec2b95c89494f2a5730e
Jan Engelhardt <jengelh@medozas.de>
2010-04-21 14:45:51 +0200

    netfilter: x_tables: move sleeping allocation outside BH-disabled region
    The jumpstack allocation needs to be moved out of the critical region.
    Corrects this notice:
    
    BUG: sleeping function called from invalid context at mm/slub.c:1705
    [  428.295762] in_atomic(): 1, irqs_disabled(): 0, pid: 9111, name: iptables
    [  428.295771] Pid: 9111, comm: iptables Not tainted 2.6.34-rc1 #2
    [  428.295776] Call Trace:
    [  428.295791]  [<c012138e>] __might_sleep+0xe5/0xed
    [  428.295801]  [<c019e8ca>] __kmalloc+0x92/0xfc
    [  428.295825]  [<f865b3bb>] ? xt_jumpstack_alloc+0x36/0xff [x_tables]
    
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/netfilter/x_tables.c
+++ net/netfilter/x_tables.c
@@ -802,6 +802,12 @@ xt_replace_table(struct xt_table *table,
 	struct xt_table_info *private;
 	int ret;
 
+	ret = xt_jumpstack_alloc(newinfo);
+	if (ret < 0) {
+		*error = ret;
+		return NULL;
+	}
+
 	/* Do the substitution. */
 	local_bh_disable();
 	private = table->private;
@@ -812,12 +818,6 @@ xt_replace_table(struct xt_table *table,
 			 num_counters, private->number);
 		local_bh_enable();
 		*error = -EAGAIN;
-		return NULL;
-	}
-
-	ret = xt_jumpstack_alloc(newinfo);
-	if (ret < 0) {
-		*error = ret;
 		return NULL;
 	}
 
