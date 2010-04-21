79a46983b7e93aa7991b9a71ff2a9d573609b22c
Thomas Koeller <thomas.koeller@baslerweb.com>
2010-04-21 15:33:32 +0200

    davinci: mach/common.h: add missing includes
    linux/compiler.h is required for __iomem
    linux/types.h is required u32
    
    Signed-off-by: Thomas Koeller <thomas.koeller@baslerweb.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/include/mach/common.h
+++ arch/arm/mach-davinci/include/mach/common.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ARCH_ARM_MACH_DAVINCI_COMMON_H
 #define __ARCH_ARM_MACH_DAVINCI_COMMON_H
 
+#include <linux/compiler.h>
+#include <linux/types.h>
+
 struct sys_timer;
 
 extern struct sys_timer davinci_timer;
