4881fd03c984b42dfc176ee68ca60740bb868f5a
Amit Kumar Salecha <amit.salecha@qlogic.com>
2010-04-22 02:51:42 +0000

    qlcnic: update version 5.0.2
    Update version to indicate IDC(fw recovery) changes.
    
    Signed-off-by: Amit Kumar Salecha <amit.salecha@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlcnic/qlcnic.h
+++ drivers/net/qlcnic/qlcnic.h
@@ -52,8 +52,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define _QLCNIC_LINUX_MAJOR 5
 #define _QLCNIC_LINUX_MINOR 0
-#define _QLCNIC_LINUX_SUBVERSION 1
-#define QLCNIC_LINUX_VERSIONID  "5.0.1"
+#define _QLCNIC_LINUX_SUBVERSION 2
+#define QLCNIC_LINUX_VERSIONID  "5.0.2"
 
 #define QLCNIC_VERSION_CODE(a, b, c)	(((a) << 24) + ((b) << 16) + (c))
 #define _major(v)	(((v) >> 24) & 0xff)
