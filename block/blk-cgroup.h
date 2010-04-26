c214597cb07e1939d4649edd39d9d27383a7172c
Vivek Goyal <vgoyal@redhat.com>
2010-04-26 19:27:56 +0200

    blk-cgroup: config options re-arrangement
    This patch fixes few usability and configurability issues.
    
    o All the cgroup based controller options are configurable from
      "Genral Setup/Control Group Support/" menu. blkio is the only exception.
      Hence make this option visible in above menu and make it configurable from
      there to bring it inline with rest of the cgroup based controllers.
    
    o Get rid of CONFIG_DEBUG_CFQ_IOSCHED.
    
      This option currently does two things.
    
      - Enable printing of cgroup paths in blktrace
      - Enables CONFIG_DEBUG_BLK_CGROUP, which in turn displays additional stat
        files in cgroup.
    
      If we are using group scheduling, blktrace data is of not really much use
      if cgroup information is not present. To get this data, currently one has to
      also enable CONFIG_DEBUG_CFQ_IOSCHED, which in turn brings the overhead of
      all the additional debug stat files which is not desired.
    
      Hence, this patch moves printing of cgroup paths under
      CONFIG_CFQ_GROUP_IOSCHED.
    
      This allows us to get rid of CONFIG_DEBUG_CFQ_IOSCHED completely. Now all
      the debug stat files are controlled only by CONFIG_DEBUG_BLK_CGROUP which
      can be enabled through config menu.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Acked-by: Divyesh Shah <dpshah@google.com>
    Reviewed-by: Gui Jianfeng <guijianfeng@cn.fujitsu.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/blk-cgroup.h
+++ block/blk-cgroup.h
@@ -109,10 +109,8 @@ struct blkio_group {
 	void *key;
 	struct hlist_node blkcg_node;
 	unsigned short blkcg_id;
-#ifdef CONFIG_DEBUG_BLK_CGROUP
 	/* Store cgroup path */
 	char path[128];
-#endif
 	/* The device MKDEV(major, minor), this group has been created for */
 	dev_t dev;
 
@@ -148,6 +146,11 @@ struct blkio_policy_type {
 extern void blkio_policy_register(struct blkio_policy_type *);
 extern void blkio_policy_unregister(struct blkio_policy_type *);
 
+static inline char *blkg_path(struct blkio_group *blkg)
+{
+	return blkg->path;
+}
+
 #else
 
 struct blkio_group {
@@ -159,6 +162,8 @@ struct blkio_policy_type {
 static inline void blkio_policy_register(struct blkio_policy_type *blkiop) { }
 static inline void blkio_policy_unregister(struct blkio_policy_type *blkiop) { }
 
+static inline char *blkg_path(struct blkio_group *blkg) { return NULL; }
+
 #endif
 
 #define BLKIO_WEIGHT_MIN	100
@@ -166,10 +171,6 @@ static inline void blkio_policy_unregister(struct blkio_policy_type *blkiop) { }
 #define BLKIO_WEIGHT_DEFAULT	500
 
 #ifdef CONFIG_DEBUG_BLK_CGROUP
-static inline char *blkg_path(struct blkio_group *blkg)
-{
-	return blkg->path;
-}
 void blkiocg_update_avg_queue_size_stats(struct blkio_group *blkg);
 void blkiocg_update_dequeue_stats(struct blkio_group *blkg,
 				unsigned long dequeue);
@@ -198,7 +199,6 @@ BLKG_FLAG_FNS(idling)
 BLKG_FLAG_FNS(empty)
 #undef BLKG_FLAG_FNS
 #else
-static inline char *blkg_path(struct blkio_group *blkg) { return NULL; }
 static inline void blkiocg_update_avg_queue_size_stats(
 						struct blkio_group *blkg) {}
 static inline void blkiocg_update_dequeue_stats(struct blkio_group *blkg,
