24da3c65ed7e6ca6c5b57214c55646c16df132cd
Jani Nikula <ext-jani.1.nikula@nokia.com>
2010-04-27 14:40:30 +0300

    OMAP: DSS2: Taal: Remove platform enable/disable
    After the addition of proper hardware reset, taal_hw_reset(), there's no
    need for an additional platform enable/disable. Remove them.
    
    Signed-off-by: Jani Nikula <ext-jani.1.nikula@nokia.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- drivers/video/omap2/displays/panel-taal.c
+++ drivers/video/omap2/displays/panel-taal.c
@@ -671,12 +671,6 @@ static int taal_power_on(struct omap_dss_device *dssdev)
 	u8 id1, id2, id3;
 	int r;
 
-	if (dssdev->platform_enable) {
-		r = dssdev->platform_enable(dssdev);
-		if (r)
-			return r;
-	}
-
 	/* it seems we have to wait a bit until taal is ready */
 	msleep(5);
 
@@ -747,8 +741,6 @@ err:
 	omapdss_dsi_display_disable(dssdev);
 err0:
 	dsi_bus_unlock();
-	if (dssdev->platform_disable)
-		dssdev->platform_disable(dssdev);
 
 	return r;
 }
@@ -776,9 +768,6 @@ static void taal_power_off(struct omap_dss_device *dssdev)
 	}
 
 	omapdss_dsi_display_disable(dssdev);
-
-	if (dssdev->platform_disable)
-		dssdev->platform_disable(dssdev);
 
 	td->enabled = 0;
 
