a4452396648efc12a7185297e5d4cfb3e13c53d1
Joern Engel <joern@logfs.org>
2010-04-27 13:45:31 +0200

    logfs: Return -EINVAL if filesystem image doesn't match
    Signed-off-by: Joern Engel <joern@logfs.org>

--- fs/logfs/super.c
+++ fs/logfs/super.c
@@ -414,7 +414,7 @@ static int __logfs_read_sb(struct super_block *sb)
 
 	page = find_super_block(sb);
 	if (!page)
-		return -EIO;
+		return -EINVAL;
 
 	ds = page_address(page);
 	super->s_size = be64_to_cpu(ds->ds_filesystem_size);
