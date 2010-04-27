5d31b787e0e74d8c021ada6dff40ffc89be3ca48
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-04-27 20:01:56 +0100

    ASoC: Remove redundant WM8960 SYSCLKSEL clkdiv option
    The SYSCLK source is automatically managed when configuring the PLL.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8960.h
+++ sound/soc/codecs/wm8960.h
@@ -77,7 +77,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WM8960_OPCLKDIV			2
 #define WM8960_DCLKDIV			3
 #define WM8960_TOCLKSEL			4
-#define WM8960_SYSCLKSEL		5
 
 #define WM8960_SYSCLK_DIV_1		(0 << 1)
 #define WM8960_SYSCLK_DIV_2		(2 << 1)
