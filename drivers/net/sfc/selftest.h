27816b50754d262e38ac99bfe360f008a5af3b5c
Ben Hutchings <bhutchings@solarflare.com>
2010-04-28 09:30:43 +0000

    sfc: Create multiple TX queues
    Create a core TX queue and 2 hardware TX queues for each channel.
    If separate_tx_channels is set, create equal numbers of RX and TX
    channels instead.
    
    Rewrite the channel and queue iteration macros accordingly.
    Eliminate efx_channel::used_flags as redundant.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/selftest.h
+++ drivers/net/sfc/selftest.h
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 struct efx_loopback_self_tests {
-	int tx_sent[EFX_TX_QUEUE_COUNT];
-	int tx_done[EFX_TX_QUEUE_COUNT];
+	int tx_sent[EFX_TXQ_TYPES];
+	int tx_done[EFX_TXQ_TYPES];
 	int rx_good;
 	int rx_bad;
 };
