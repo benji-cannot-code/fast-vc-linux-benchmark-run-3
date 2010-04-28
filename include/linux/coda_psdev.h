591054e6aacd531511ad5736f965b45054f9ae35
Jens Axboe <jens.axboe@oracle.com>
2010-04-28 09:20:33 +0200

    coda: move backing-dev.h kernel include inside __KERNEL__
    Otherwise we must export backing-dev.h as well, which doesn't make
    any sense.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- include/linux/coda_psdev.h
+++ include/linux/coda_psdev.h
@@ -2,13 +2,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __CODA_PSDEV_H
 #define __CODA_PSDEV_H
 
-#include <linux/backing-dev.h>
 #include <linux/magic.h>
 
 #define CODA_PSDEV_MAJOR 67
 #define MAX_CODADEVS  5	   /* how many do we allow */
 
 #ifdef __KERNEL__
+#include <linux/backing-dev.h>
+
 struct kstatfs;
 
 /* communication pending/processing queues */
