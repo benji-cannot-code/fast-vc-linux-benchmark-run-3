54df5e2243297e55ca19c2f455a9679f7d9f3bf4
John W. Linville <linville@tuxdriver.com>
2010-04-30 15:37:00 -0400

    mac80211: remove deprecated noise field from ieee80211_rx_status
    Also remove associated IEEE80211_HW_NOISE_DBM from ieee80211_hw_flags.
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/main.c
+++ drivers/net/wireless/b43/main.c
@@ -4905,8 +4905,7 @@ static int b43_wireless_init(struct ssb_device *dev)
 
 	/* fill hw info */
 	hw->flags = IEEE80211_HW_RX_INCLUDES_FCS |
-		    IEEE80211_HW_SIGNAL_DBM |
-		    IEEE80211_HW_NOISE_DBM;
+		    IEEE80211_HW_SIGNAL_DBM;
 
 	hw->wiphy->interface_modes =
 		BIT(NL80211_IFTYPE_AP) |
