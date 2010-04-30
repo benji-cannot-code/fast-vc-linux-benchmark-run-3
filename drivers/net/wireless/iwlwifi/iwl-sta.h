c9e15ebd6a3fa766d599526b63e8fd9b25c32432
Johannes Berg <johannes.berg@intel.com>
2010-04-30 11:30:45 -0700

    iwlwifi: use iwl_find_station less
    Since we now store the station ID in each station
    struct, many places need not look at the station
    table any more since they can just pull the station
    ID out of the struct. Remove iwl_get_sta_id() and
    use iwl_sta_id() instead as appropriate.
    
    This reduces the amount of code needed to find the
    right station significantly, and works since
    mac80211 passes the station only after it has been
    fully initialised, ie. even if TX races with
    station addition it will only be passed to TX once
    the addition is complete.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-sta.h
+++ drivers/net/wireless/iwlwifi/iwl-sta.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_sta_h__
 #define __iwl_sta_h__
 
+#include "iwl-dev.h"
+
 #define HW_KEY_DYNAMIC 0
 #define HW_KEY_DEFAULT 1
 
@@ -66,7 +68,6 @@ void iwl_clear_ucode_stations(struct iwl_priv *priv);
 int iwl_alloc_bcast_station(struct iwl_priv *priv, bool init_lq);
 void iwl_dealloc_bcast_station(struct iwl_priv *priv);
 int iwl_get_free_ucode_key_index(struct iwl_priv *priv);
-int iwl_get_sta_id(struct iwl_priv *priv, struct ieee80211_hdr *hdr);
 int iwl_send_add_sta(struct iwl_priv *priv,
 		     struct iwl_addsta_cmd *sta, u8 flags);
 int iwl_add_local_station(struct iwl_priv *priv, const u8 *addr, bool init_rs,
