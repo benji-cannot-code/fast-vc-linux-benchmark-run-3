59f451af9bbce80b042550e6b54ff416e04a442b
Gregory Bean <gbean@codeaurora.org>
2010-04-30 22:22:07 -0700

    msm: clock support for the MSM7x30 CPU.
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/clock.h
+++ arch/arm/mach-msm/clock.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/clk.h>
 
 #include "clock-pcom.h"
+#include "clock-7x30.h"
 
 #define CLKFLAG_INVERT			0x00000001
 #define CLKFLAG_NOINVERT		0x00000002
