75bb7c2ede82d672a7cb815da83e904f196cbfcf
Charles Clément <caratorn@gmail.com>
2010-05-02 18:50:16 -0700

    Staging: crystalhd: remove unused #include <linux/version.h>
    Signed-off-by: Charles Clément <caratorn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/crystalhd/crystalhd_misc.h
+++ drivers/staging/crystalhd/crystalhd_misc.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/ioctl.h>
 #include <linux/dma-mapping.h>
-#include <linux/version.h>
 #include <linux/sched.h>
 #include <asm/system.h>
 #include "bc_dts_glob_lnx.h"
