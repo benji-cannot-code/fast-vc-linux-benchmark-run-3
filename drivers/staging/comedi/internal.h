c7e22036da7a83c65915e3e018172fae2f80604e
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-03 15:27:52 -0700

    Staging: comedi: make comedi_reset_async_buf local to comedi core
    No one outside of the comedi core calls this function, so don't export
    it to the world.
    
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/internal.h
+++ drivers/staging/comedi/internal.h
@@ -2,3 +2,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int comedi_alloc_board_minor(struct device *hardware_device);
 void comedi_free_board_minor(unsigned minor);
+void comedi_reset_async_buf(struct comedi_async *async);
