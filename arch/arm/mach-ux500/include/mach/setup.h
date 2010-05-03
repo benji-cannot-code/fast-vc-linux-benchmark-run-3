112026628da188b0a4671279cad43283cc720960
Rabin Vincent <rabin.vincent@stericsson.com>
2010-05-03 07:39:02 +0100

    ARM: 6080/1: ux500: move irq and common maps to cpu.c
    Move IRQ initialization and common io mapping setup code to cpu.c,
    renaming U8500* to UX500* along the way.
    
    Acked-by: Linus Walleij <linus.walleij@stericsson.com>
    Acked-by: Srinidhi Kasagar <srinidhi.kasagar@stericsson.com>
    Signed-off-by: Rabin Vincent <rabin.vincent@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ux500/include/mach/setup.h
+++ arch/arm/mach-ux500/include/mach/setup.h
@@ -15,13 +15,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/time.h>
 #include <linux/init.h>
 
-extern void u8500_map_io(void);
+extern void __init ux500_map_io(void);
+extern void __init u8500_map_io(void);
+
 extern void u8500_init_devices(void);
-extern void u8500_init_irq(void);
+extern void __init ux500_init_irq(void);
 /* We re-use nomadik_timer for this platform */
 extern void nmdk_timer_init(void);
 
 struct sys_timer;
 extern struct sys_timer u8500_timer;
+
+#define __IO_DEV_DESC(x, sz)	{		\
+	.virtual	= IO_ADDRESS(x),	\
+	.pfn		= __phys_to_pfn(x),	\
+	.length		= sz,			\
+	.type		= MT_DEVICE,		\
+}
 
 #endif /*  __ASM_ARCH_SETUP_H */
