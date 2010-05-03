c51dc8f8ecf3a767e4ead434cbf17f5e142b0c4f
Rabin Vincent <rabin.vincent@stericsson.com>
2010-05-03 07:43:48 +0100

    ARM: 6081/1: ux500: move gpio macros to common header
    Move GPIO macros to a common header, and allow them to use the correct
    macros for the appropriate SoC, and be named accordingly.
    
    Acked-by: Linus Walleij <linus.walleij@stericsson.com>
    Acked-by: Srinidhi Kasagar <srinidhi.kasagar@stericsson.com>
    Signed-off-by: Rabin Vincent <rabin.vincent@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-ux500/cpu-u8500.c
+++ arch/arm/mach-ux500/cpu-u8500.c
@@ -24,36 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/hardware.h>
 #include <mach/setup.h>
 
-#define GPIO_RESOURCE(block)						\
-	{								\
-		.start	= U8500_GPIOBANK##block##_BASE,			\
-		.end	= U8500_GPIOBANK##block##_BASE + 127,		\
-		.flags	= IORESOURCE_MEM,				\
-	},								\
-	{								\
-		.start	= IRQ_GPIO##block,				\
-		.end	= IRQ_GPIO##block,				\
-		.flags	= IORESOURCE_IRQ,				\
-	}
-
-#define GPIO_DEVICE(block)						\
-	{								\
-		.name 		= "gpio",				\
-		.id		= block,				\
-		.num_resources 	= 2,					\
-		.resource	= &u8500_gpio_resources[block * 2],	\
-		.dev = {						\
-			.platform_data = &u8500_gpio_data[block],	\
-		},							\
-	}
-
-#define GPIO_DATA(_name, first)						\
-	{								\
-		.name 		= _name,				\
-		.first_gpio 	= first,				\
-		.first_irq 	= NOMADIK_GPIO_TO_IRQ(first),		\
-	}
-
 static struct nmk_gpio_platform_data u8500_gpio_data[] = {
 	GPIO_DATA("GPIO-0-31", 0),
 	GPIO_DATA("GPIO-32-63", 32), /* 37..63 not routed to pin */
