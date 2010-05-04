b7e6d6c1099db7c045ecdcee42fbee60225400fb
Andres More <more.andres@gmail.com>
2010-05-04 20:40:09 -0300

    Staging: vt6656: removed SUCCESS define as it is not used
    Signed-off-by: Andres More <more.andres@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6656/ttype.h
+++ drivers/staging/vt6656/ttype.h
@@ -65,11 +65,6 @@ typedef int             BOOL;
 #define FALSE           0
 #endif
 
-
-#if !defined(SUCCESS)
-#define SUCCESS         0
-#endif
-
 //2007-0809-01<Add>by MikeLiu
 #ifndef  update_BssList
 #define update_BssList
