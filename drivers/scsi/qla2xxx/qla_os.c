4182030a469b7d97cb441cb2cc0affa81ad2074b
Lalit Chandivade <lalit.chandivade@qlogic.com>
2010-05-04 15:01:23 -0700

    [SCSI] qla2xxx: Clear error status after uncorrectable non-fatal errors.
    Currently error status is cleared only after the uncorrectable fatal errors
    in the qla2xxx_pci_slot_reset. This fix is added to clear the error status in
    qla2xxx_pci_resume. This way for both fatal and non-fatal errors the error
    status gets cleared properly.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=572258
    
    Signed-off-by: Giridhar Malavali <giridhar.malavali@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -3698,7 +3698,6 @@ qla2xxx_pci_slot_reset(struct pci_dev *pdev)
 		ret =  PCI_ERS_RESULT_RECOVERED;
 	clear_bit(ABORT_ISP_ACTIVE, &base_vha->dpc_flags);
 
-	pci_cleanup_aer_uncorrect_error_status(pdev);
 
 	DEBUG17(qla_printk(KERN_WARNING, ha,
 	    "slot_reset-return:ret=%x\n", ret));
@@ -3721,6 +3720,8 @@ qla2xxx_pci_resume(struct pci_dev *pdev)
 		    "the device failed to resume I/O "
 		    "from slot/link_reset");
 	}
+
+	pci_cleanup_aer_uncorrect_error_status(pdev);
 
 	ha->flags.eeh_busy = 0;
 }
