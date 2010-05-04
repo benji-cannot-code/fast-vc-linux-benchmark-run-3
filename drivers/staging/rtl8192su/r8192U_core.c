fb2789f84c77d09525353135252bb17273094765
Florian Schilhabel <florian.c.schilhabel@googlemail.com>
2010-05-04 14:23:43 +0200

    Staging: rtl8192su: check for skb == NULL
    added 2 checks for skb == NULL.
    plus cosmetics
    
    Signed-off-by: Florian Schilhabel <florian.c.schilhabel@googlemail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192su/r8192U_core.c
+++ drivers/staging/rtl8192su/r8192U_core.c
@@ -1297,7 +1297,6 @@ static int rtl8192_rx_initiate(struct net_device*dev)
                         kfree_skb(skb);
                         break;
                 }
-//		printk("nomal packet IN request!\n");
                 usb_fill_bulk_urb(entry, priv->udev,
                                   usb_rcvbulkpipe(priv->udev, 3), skb_tail_pointer(skb),
                                   RX_URB_SIZE, rtl8192_rx_isr, skb);
@@ -1311,7 +1310,6 @@ static int rtl8192_rx_initiate(struct net_device*dev)
 
 	/* command packet rx procedure */
         while (skb_queue_len(&priv->rx_queue) < MAX_RX_URB + 3) {
-//		printk("command packet IN request!\n");
                 skb = __dev_alloc_skb(RX_URB_SIZE ,GFP_KERNEL);
                 if (!skb)
                         break;
