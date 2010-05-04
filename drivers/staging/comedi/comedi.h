14e149dcb9194476cafff0a53af976d7a4200f1d
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-04 16:00:02 -0700

    Staging: comedi: mark a variable as __user
    This is really a userspace pointer, so mark it as such.
    
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/comedi.h
+++ drivers/staging/comedi/comedi.h
@@ -355,7 +355,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		unsigned int __user *chanlist;	/* channel/range list */
 		unsigned int chanlist_len;
 
-		short *data;	/* data list, size depends on subd flags */
+		short __user *data;	/* data list, size depends on subd flags */
 		unsigned int data_len;
 	};
 
