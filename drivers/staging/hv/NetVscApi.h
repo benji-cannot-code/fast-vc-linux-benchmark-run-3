db3cc2ce07956031de77c22fb4b12a83d7667caa
Stephen Hemminger <shemminger@vyatta.com>
2010-05-04 09:58:53 -0700

    Staging: hv: ring parameter
    The ring size parameter should be number of pages (not bytes).
    Add module parameter information as well.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Acked-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/NetVscApi.h
+++ drivers/staging/hv/NetVscApi.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "VmbusApi.h"
 
 /* Defines */
-#define NETVSC_DEVICE_RING_BUFFER_SIZE	(64*PAGE_SIZE)
 #define HW_MACADDR_LEN			6
 
 /* Fwd declaration */
