b66bbf84df95136863064b34cdaadf9c2effde93
Daniel Walker <dwalker@codeaurora.org>
2010-05-05 06:45:20 -0700

    msm: irqs: add irqs-8x50.h for QSD8x50 support
    Signed-off-by: Daniel Walker <dwalker@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>
    Signed-off-by: Gregory Bean <gbean@codeaurora.org>
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/include/mach/irqs.h
+++ arch/arm/mach-msm/include/mach/irqs.h
@@ -20,7 +20,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MSM_IRQ_BIT(irq)     (1 << ((irq) & 31))
 
-#if defined(CONFIG_ARCH_MSM_ARM11)
+#if defined(CONFIG_ARCH_QSD8X50)
+#include "irqs-8x50.h"
+#include "sirc.h"
+#elif defined(CONFIG_ARCH_MSM_ARM11)
 #include "irqs-7x00.h"
 #else
 #error "Unknown architecture specification"
