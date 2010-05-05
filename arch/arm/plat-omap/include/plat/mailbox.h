aa655acb7dfd0d52eaee4713227fdd5824155bd0
Ohad Ben-Cohen <ohad@wizery.com>
2010-05-05 15:33:09 +0000

    omap: mailbox: convert block api to kfifo
    The underlying buffering implementation of mailbox
    is converted from block API to kfifo due to the simplicity
    and speed of kfifo.
    
    The default size of the kfifo buffer is set to 256 bytes.
    This value is configurable at compile time (via
    CONFIG_OMAP_MBOX_KFIFO_SIZE), and can be changed at
    runtime (via the mbox_kfifo_size module parameter).
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>
    Signed-off-by: Hiroshi DOYU <Hiroshi.DOYU@nokia.com>

--- arch/arm/plat-omap/include/plat/mailbox.h
+++ arch/arm/plat-omap/include/plat/mailbox.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/wait.h>
 #include <linux/workqueue.h>
-#include <linux/blkdev.h>
 #include <linux/interrupt.h>
+#include <linux/kfifo.h>
 
 typedef u32 mbox_msg_t;
 struct omap_mbox;
@@ -43,7 +43,7 @@ struct omap_mbox_ops {
 
 struct omap_mbox_queue {
 	spinlock_t		lock;
-	struct request_queue	*queue;
+	struct kfifo		fifo;
 	struct work_struct	work;
 	struct tasklet_struct	tasklet;
 	int	(*callback)(void *);
