23863a8c38230881dd8495f49981a08ae4790476
Tobias Klauser <tklauser@distanz.ch>
2010-05-05 10:35:22 +0200

    serial: Add driver for the Altera JTAG UART
    Add an UART driver for the JTAG UART component available as a SOPC
    (System on Programmable Chip) component for Altera FPGAs.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -183,6 +183,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Aeroflex Gaisler GRLIB APBUART */
 #define PORT_APBUART    90
 
+/* Altera UARTs */
+#define PORT_ALTERA_JTAGUART	91
+
 #ifdef __KERNEL__
 
 #include <linux/compiler.h>
