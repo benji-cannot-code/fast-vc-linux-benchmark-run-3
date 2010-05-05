b69e6455d39985b86911945165abf72a2fda593e
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-05 09:12:13 -0700

    Staging: comedi: cb_pcidas64: fix up build warnings
    Now that the 8255.h file is being included properly, we were
    using the incorrect function prototypes, which causes a build
    warning now.  This fixes it up and preserves the __iomem markings that
    sparse wants to see.
    
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/cb_pcidas64.c
+++ drivers/staging/comedi/drivers/cb_pcidas64.c
@@ -1184,8 +1184,8 @@ static int ao_cmdtest(struct comedi_device *dev, struct comedi_subdevice *s,
 static irqreturn_t handle_interrupt(int irq, void *d);
 static int ai_cancel(struct comedi_device *dev, struct comedi_subdevice *s);
 static int ao_cancel(struct comedi_device *dev, struct comedi_subdevice *s);
-static int dio_callback(int dir, int port, int data, void __iomem *base);
-static int dio_callback_4020(int dir, int port, int data, void __iomem *base);
+static int dio_callback(int dir, int port, int data, unsigned long arg);
+static int dio_callback_4020(int dir, int port, int data, unsigned long arg);
 static int di_rbits(struct comedi_device *dev, struct comedi_subdevice *s,
 		    struct comedi_insn *insn, unsigned int *data);
 static int do_wbits(struct comedi_device *dev, struct comedi_subdevice *s,
@@ -3659,8 +3659,9 @@ static int ao_cancel(struct comedi_device *dev, struct comedi_subdevice *s)
 	return 0;
 }
 
-static int dio_callback(int dir, int port, int data, void __iomem *iobase)
+static int dio_callback(int dir, int port, int data, unsigned long arg)
 {
+	void __iomem *iobase = (void __iomem *)arg;
 	if (dir) {
 		writeb(data, iobase + port);
 		DEBUG_PRINT("wrote 0x%x to port %i\n", data, port);
@@ -3670,8 +3671,9 @@ static int dio_callback(int dir, int port, int data, void __iomem *iobase)
 	}
 }
 
-static int dio_callback_4020(int dir, int port, int data, void __iomem *iobase)
+static int dio_callback_4020(int dir, int port, int data, unsigned long arg)
 {
+	void __iomem *iobase = (void __iomem *)arg;
 	if (dir) {
 		writew(data, iobase + 2 * port);
 		return 0;
