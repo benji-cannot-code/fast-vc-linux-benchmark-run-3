2682e76cd6123adfb14f781f927ee2a56fe093a1
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-05-05 17:16:35 -0300

    V4L/DVB: videobuf-vmalloc: remove __videobuf_sync()
    videobuf-core checks if .sync ops is defined before calling.
    
    So, we don't need a do-nothing function.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/videobuf-vmalloc.c
+++ drivers/media/video/videobuf-vmalloc.c
@@ -230,12 +230,6 @@ static int __videobuf_iolock(struct videobuf_queue *q,
 	return 0;
 }
 
-static int __videobuf_sync(struct videobuf_queue *q,
-			   struct videobuf_buffer *buf)
-{
-	return 0;
-}
-
 static int __videobuf_mmap_mapper(struct videobuf_queue *q,
 				  struct videobuf_buffer *buf,
 				  struct vm_area_struct *vma)
@@ -302,7 +296,6 @@ static struct videobuf_qtype_ops qops = {
 
 	.alloc        = __videobuf_alloc,
 	.iolock       = __videobuf_iolock,
-	.sync         = __videobuf_sync,
 	.mmap_mapper  = __videobuf_mmap_mapper,
 	.vaddr        = videobuf_to_vmalloc,
 };
