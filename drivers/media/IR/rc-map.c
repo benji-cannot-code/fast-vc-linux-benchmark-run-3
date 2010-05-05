cbce16863657b56926b2cbf89eefbe0582338874
Peter Huewe <PeterHuewe@gmx.de>
2010-05-05 12:20:21 -0300

    V4L/DVB: media/IR: Add missing include file to rc-map.c
    This patch adds a missing include linux/delay.h to prevent
    build failures[1-5]
    
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/rc-map.c
+++ drivers/media/IR/rc-map.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/ir-core.h>
 #include <linux/spinlock.h>
+#include <linux/delay.h>
 
 /* Used to handle IR raw handler extensions */
 static LIST_HEAD(rc_map_list);
