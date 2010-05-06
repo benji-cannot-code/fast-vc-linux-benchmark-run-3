5a70a056dd102b20ccd9d66460b77cdfcde1b5fb
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-05 22:36:31 -0700

    Staging: hv: rename ChannelInterface.c and .h to channel_interface.c and .h
    All of the uppercase .c files are now gone.
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/VmbusPrivate.h
+++ drivers/staging/hv/VmbusPrivate.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "VmbusApi.h"
 #include "channel.h"
 #include "channel_mgmt.h"
-#include "ChannelInterface.h"
+#include "channel_interface.h"
 #include "ring_buffer.h"
 #include <linux/list.h>
 
