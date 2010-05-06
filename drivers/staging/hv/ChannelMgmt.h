9311fbeaa217a8321ed83984ae2a14ae5f0f8485
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-05 22:22:35 -0700

    Staging: hv: rename RingBuffer.c and .h to ring_buffer.c and .h
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/ChannelMgmt.h
+++ drivers/staging/hv/ChannelMgmt.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/list.h>
 #include <linux/timer.h>
-#include "RingBuffer.h"
+#include "ring_buffer.h"
 #include "VmbusChannelInterface.h"
 #include "VmbusPacketFormat.h"
 
--- drivers/staging/hv/VmbusPrivate.h
+++ drivers/staging/hv/VmbusPrivate.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "channel.h"
 #include "ChannelMgmt.h"
 #include "ChannelInterface.h"
-#include "RingBuffer.h"
+#include "ring_buffer.h"
 #include <linux/list.h>
 
 
