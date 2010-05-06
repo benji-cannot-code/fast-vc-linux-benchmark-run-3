5ffb022ce51e6ba37ae258b2861e050ee25c72a6
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-05 22:47:13 -0700

    Staging: hv: rename VmbusChannelInterface.h to vmbus_channel_interface.h
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/channel_mgmt.h
+++ drivers/staging/hv/channel_mgmt.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/timer.h>
 #include "ring_buffer.h"
-#include "VmbusChannelInterface.h"
+#include "vmbus_channel_interface.h"
 #include "VmbusPacketFormat.h"
 
 /* Version 1 messages */
--- drivers/staging/hv/hv_utils.c
+++ drivers/staging/hv/hv_utils.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "osd.h"
 #include "vmbus.h"
 #include "VmbusPacketFormat.h"
-#include "VmbusChannelInterface.h"
+#include "vmbus_channel_interface.h"
 #include "VersionInfo.h"
 #include "channel.h"
 #include "vmbus_private.h"
