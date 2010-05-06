7b2974cd76aefb09b0d6fa651817f161fb18b423
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-05 22:40:43 -0700

    Staging: hv: rename StorVscApi.h to storvsc_api.h
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/blkvsc_drv.c
+++ drivers/staging/hv/blkvsc_drv.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "logging.h"
 #include "VersionInfo.h"
 #include "vmbus.h"
-#include "StorVscApi.h"
+#include "storvsc_api.h"
 
 
 #define BLKVSC_MINORS	64
--- drivers/staging/hv/storvsc.c
+++ drivers/staging/hv/storvsc.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include "osd.h"
 #include "logging.h"
-#include "StorVscApi.h"
+#include "storvsc_api.h"
 #include "VmbusPacketFormat.h"
 #include "vstorage.h"
 
