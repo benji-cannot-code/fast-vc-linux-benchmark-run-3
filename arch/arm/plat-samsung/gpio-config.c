07bfdd6a8529687d0d463a0f98eb05b573325388
Ben Dooks <ben-linux@fluff.org>
2010-05-06 15:42:23 +0900

    ARM: SAMSUNG: Add spinlock locking to GPIO banks
    Add locking to each GPIO bank to allow for SMP capable code
    to use the gpiolib functions. See the gpio-core.h header file
    for more information.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/plat-samsung/gpio-config.c
+++ arch/arm/plat-samsung/gpio-config.c
@@ -34,9 +34,9 @@ int s3c_gpio_cfgpin(unsigned int pin, unsigned int config)
 
 	offset = pin - chip->chip.base;
 
-	local_irq_save(flags);
+	s3c_gpio_lock(chip, flags);
 	ret = s3c_gpio_do_setcfg(chip, offset, config);
-	local_irq_restore(flags);
+	s3c_gpio_unlock(chip, flags);
 
 	return ret;
 }
@@ -52,9 +52,9 @@ unsigned s3c_gpio_getcfg(unsigned int pin)
 	if (chip) {
 		offset = pin - chip->chip.base;
 
-		local_irq_save(flags);
+		s3c_gpio_lock(chip, flags);
 		ret = s3c_gpio_do_getcfg(chip, offset);
-		local_irq_restore(flags);
+		s3c_gpio_unlock(chip, flags);
 	}
 
 	return ret;
@@ -73,9 +73,9 @@ int s3c_gpio_setpull(unsigned int pin, s3c_gpio_pull_t pull)
 
 	offset = pin - chip->chip.base;
 
-	local_irq_save(flags);
+	s3c_gpio_lock(chip, flags);
 	ret = s3c_gpio_do_setpull(chip, offset, pull);
-	local_irq_restore(flags);
+	s3c_gpio_unlock(chip, flags);
 
 	return ret;
 }
