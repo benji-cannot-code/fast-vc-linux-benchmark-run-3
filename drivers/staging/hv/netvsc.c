ae2e08e3e42228ad53f3d25785d11e00f04e1690
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-05 22:27:31 -0700

    Staging: hv: rename NetVsc.c and .h to netvsc.c and .h
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/netvsc.c
+++ drivers/staging/hv/netvsc.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include "osd.h"
 #include "logging.h"
-#include "NetVsc.h"
+#include "netvsc.h"
 #include "RndisFilter.h"
 
 
