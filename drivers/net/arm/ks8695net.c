6d614fc4fed6ba6e55aaa203fded881811fb0c0d
Tobias Klauser <tklauser@distanz.ch>
2010-05-06 05:40:29 +0000

    KS8695: Remove unnecessary memset of netdev private data
    The memory for the private data is allocated using kzalloc in
    alloc_etherdev (or alloc_netdev_mq respectively) so there is no need to
    set it to 0 again.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/arm/ks8695net.c
+++ drivers/net/arm/ks8695net.c
@@ -1473,7 +1473,6 @@ ks8695_probe(struct platform_device *pdev)
 
 	/* Configure our private structure a little */
 	ksp = netdev_priv(ndev);
-	memset(ksp, 0, sizeof(struct ks8695_priv));
 
 	ksp->dev = &pdev->dev;
 	ksp->ndev = ndev;
