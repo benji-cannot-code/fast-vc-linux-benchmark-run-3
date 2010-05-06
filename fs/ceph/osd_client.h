09d6a72f9f9a13b98d9fece85b1c83482ac14660
Sage Weil <sage@newdream.net>
2010-05-05 21:30:35 -0700

    ceph: don't use writeback_control in writepages completion
    The ->writepages writeback_control is not still valid in the writepages
    completion.  We were touching it solely to adjust pages_skipped when there
    was a writeback error (EIO, ENOSPC, EPERM due to bad osd credentials),
    causing an oops in the writeback code shortly thereafter.  Updating
    pages_skipped on error isn't correct anyway, so let's just rip out this
    (clearly broken) code to pass the wbc to the completion.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- fs/ceph/osd_client.h
+++ fs/ceph/osd_client.h
@@ -67,7 +67,6 @@ struct ceph_osd_request {
 	struct list_head  r_unsafe_item;
 
 	struct inode *r_inode;         	      /* for use by callbacks */
-	struct writeback_control *r_wbc;      /* ditto */
 
 	char              r_oid[40];          /* object name */
 	int               r_oid_len;
