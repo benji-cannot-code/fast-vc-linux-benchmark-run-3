7d708f53d85e5aa8838854634797d18739bfdb53
Greg Kroah-Hartman <gregkh@suse.de>
2010-05-05 22:32:08 -0700

    Staging: hv: rename RndisFilter.c and .h to rndis_filter.c and .h
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/rndis_filter.c
+++ drivers/staging/hv/rndis_filter.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "osd.h"
 #include "logging.h"
 #include "NetVscApi.h"
-#include "RndisFilter.h"
+#include "rndis_filter.h"
 
 /* Data types */
 struct rndis_filter_driver_object {
