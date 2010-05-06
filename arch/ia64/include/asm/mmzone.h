56702921f8af5eb438ce1f8cc6c22643395f0f68
Jiri Olsa <jolsa@redhat.com>
2010-05-06 19:36:06 +0200

    [IA64] removing redundant ifdef
    Pointless to use #ifdef CONFIG_NUMA in code that is
    already inside another #ifdef CONFIG_NUMA.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/include/asm/mmzone.h
+++ arch/ia64/include/asm/mmzone.h
@@ -20,16 +20,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline int pfn_to_nid(unsigned long pfn)
 {
-#ifdef CONFIG_NUMA
 	extern int paddr_to_nid(unsigned long);
 	int nid = paddr_to_nid(pfn << PAGE_SHIFT);
 	if (nid < 0)
 		return 0;
 	else
 		return nid;
-#else
-	return 0;
-#endif
 }
 
 #ifdef CONFIG_IA64_DIG /* DIG systems are small */
