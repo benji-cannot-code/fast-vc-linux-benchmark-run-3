5ad8a5e6c84c97f046240507a701d7cf425e1086
Takanori Suzuki <mail.tks@gmail.com>
2010-05-08 22:56:24 +0900

    Staging: panel: change asm/uaccess.h to linux/uaccess.h
    This patch replaces <asm/uaccess.h> with <linux/uaccess.h> to comply with
    the checkpatch.pl hint.
    
    Signed-off-by: Takanori Suzuki <mail.tks@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/panel/panel.c
+++ drivers/staging/panel/panel.c
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <generated/utsrelease.h>
 
 #include <linux/io.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <asm/system.h>
 
 #define LCD_MINOR		156
