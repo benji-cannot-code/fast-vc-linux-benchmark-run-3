e70c384cfef40f19c063c1cf108cf7e4f213436a
Rajashekhara, Mahesh <Mahesh_Rajashekhara@adaptec.com>
2010-05-10 04:05:50 -0700

    [SCSI] aacraid: respond automatically to volumes added by config tool
    Problem description:
    --------------------
    
    When the JBOD is created from the OS using Adaptec Storage Manager
    utility device is not available under FDISK until a system restart is
    done.
    
    Solution:
    ---------
    
    AIF handling: If there is a JBOD drive added to the system, identify
    the old one with scsi_device_lookup() and remove it to enable a fresh
    scsi_add_device(); else the new JBOD is not available until reboot.
    
    Signed-off-by: Mahesh Rajashekhara <aacraid@adaptec.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/aacraid/aacraid.h
+++ drivers/scsi/aacraid/aacraid.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *----------------------------------------------------------------------------*/
 
 #ifndef AAC_DRIVER_BUILD
-# define AAC_DRIVER_BUILD 24702
+# define AAC_DRIVER_BUILD 26000
 # define AAC_DRIVER_BRANCH "-ms"
 #endif
 #define MAXIMUM_NUM_CONTAINERS	32
