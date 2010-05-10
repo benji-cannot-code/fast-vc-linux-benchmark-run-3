e4d9302245624f0c0379dbb7f62b778b2148b9fc
Magnus Damm <damm@opensource.se>
2010-05-10 14:01:34 +0000

    sh: allow registering clocks without name
    Modify the SuperH clock code to support struct clk
    with NULL as name. Such clocks will not be hooked
    up to debugfs.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpu/clock.c
+++ arch/sh/kernel/cpu/clock.c
@@ -416,7 +416,7 @@ struct clk *clk_get(struct device *dev, const char *id)
 
 	mutex_lock(&clock_list_sem);
 	list_for_each_entry(p, &clock_list, node) {
-		if (p->id == idno &&
+		if (p->name && p->id == idno &&
 		    strcmp(id, p->name) == 0 && try_module_get(p->owner)) {
 			clk = p;
 			goto found;
@@ -424,7 +424,8 @@ struct clk *clk_get(struct device *dev, const char *id)
 	}
 
 	list_for_each_entry(p, &clock_list, node) {
-		if (strcmp(id, p->name) == 0 && try_module_get(p->owner)) {
+		if (p->name &&
+		    strcmp(id, p->name) == 0 && try_module_get(p->owner)) {
 			clk = p;
 			break;
 		}
@@ -595,7 +596,7 @@ static int clk_debugfs_register(struct clk *c)
 			return err;
 	}
 
-	if (!c->dentry) {
+	if (!c->dentry && c->name) {
 		err = clk_debugfs_register_one(c);
 		if (err)
 			return err;
