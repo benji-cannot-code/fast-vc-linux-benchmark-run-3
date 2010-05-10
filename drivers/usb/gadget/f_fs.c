470fee51c38510d135b2896d1712a3726dc8ec50
Randy Dunlap <randy.dunlap@oracle.com>
2010-05-10 10:51:36 -0700

    USB: gadget: f_fs.c needs to include pagemap.h
    Fix g_ffs build error, add a needed header file:
    
    drivers/usb/gadget/f_fs.c:1064:error: 'PAGE_CACHE_SIZE' undeclared (first use in this function)
    drivers/usb/gadget/f_fs.c:1065:error: 'PAGE_CACHE_SHIFT' undeclared (first use in this function)
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Acked-by: Michał Nazarewicz <m.nazarewicz@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/f_fs.c
+++ drivers/usb/gadget/f_fs.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* #define VERBOSE_DEBUG */
 
 #include <linux/blkdev.h>
+#include <linux/pagemap.h>
 #include <asm/unaligned.h>
 #include <linux/smp_lock.h>
 
