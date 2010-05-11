7a929579ab2432e3985cde4aae98d1ae25d144dd
Julia Lawall <julia@diku.dk>
2010-05-11 20:26:57 +0200

    Staging: Drop memory allocation cast
    Drop cast on the result of kmalloc and similar functions.
    
    The semantic patch that makes this change is as follows:
    (http://coccinelle.lip6.fr/)
    
    // <smpl>
    @@
    type T;
    @@
    
    - (T *)
      (\(kmalloc\|kzalloc\|kcalloc\|kmem_cache_alloc\|kmem_cache_zalloc\|
       kmem_cache_alloc_node\|kmalloc_node\|kzalloc_node\)(...))
    // </smpl>
    
    Signed-off-by: Julia Lawall <julia@diku.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/cx25821/cx25821-audio-upstream.c
+++ drivers/staging/cx25821/cx25821-audio-upstream.c
@@ -752,8 +752,7 @@ int cx25821_audio_upstream_init(struct cx25821_dev *dev, int channel_select)
 
 	if (dev->input_audiofilename) {
 		str_length = strlen(dev->input_audiofilename);
-		dev->_audiofilename =
-		    (char *)kmalloc(str_length + 1, GFP_KERNEL);
+		dev->_audiofilename = kmalloc(str_length + 1, GFP_KERNEL);
 
 		if (!dev->_audiofilename)
 			goto error;
@@ -767,8 +766,7 @@ int cx25821_audio_upstream_init(struct cx25821_dev *dev, int channel_select)
 		}
 	} else {
 		str_length = strlen(_defaultAudioName);
-		dev->_audiofilename =
-		    (char *)kmalloc(str_length + 1, GFP_KERNEL);
+		dev->_audiofilename = kmalloc(str_length + 1, GFP_KERNEL);
 
 		if (!dev->_audiofilename)
 			goto error;
