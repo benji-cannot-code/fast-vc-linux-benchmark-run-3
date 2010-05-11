c2bba9c75541a237b1a0f3455c39546e010231db
Bob Peterson <rpeterso@redhat.com>
2010-05-11 17:35:34 -0400

    GFS2: Eliminate useless err variable
    This patch removes an unneeded "err" variable that is always
    returned as zero.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com> 
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/meta_io.c
+++ fs/gfs2/meta_io.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int gfs2_aspace_writepage(struct page *page, struct writeback_control *wbc)
 {
-	int err;
 	struct buffer_head *bh, *head;
 	int nr_underway = 0;
 	int write_op = (1 << BIO_RW_META) | ((wbc->sync_mode == WB_SYNC_ALL ?
@@ -87,11 +86,10 @@ static int gfs2_aspace_writepage(struct page *page, struct writeback_control *wb
 	} while (bh != head);
 	unlock_page(page);
 
-	err = 0;
 	if (nr_underway == 0)
 		end_page_writeback(page);
 
-	return err;
+	return 0;
 }
 
 const struct address_space_operations gfs2_meta_aops = {
