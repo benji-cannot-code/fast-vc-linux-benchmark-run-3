9189c2bf447381645a83aa140f06d5d894f03d56
Allan Stephens <allan.stephens@windriver.com>
2010-05-11 14:30:08 +0000

    tipc: Add support for "-s" configuration option
    Provide initial support for displaying overall TIPC status/statistics
    information at runtime.  Currently, only version info for the TIPC
    kernel module is displayed.
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/core.h
+++ net/tipc/core.h
@@ -60,6 +60,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 
+
+#define TIPC_MOD_VER "2.0.0"
+
 /*
  * TIPC sanity test macros
  */
