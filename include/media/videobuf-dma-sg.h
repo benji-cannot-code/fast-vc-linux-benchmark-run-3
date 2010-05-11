7b6c810924a5d101888ed23c41352c3d6182b7b2
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2010-05-11 10:36:34 -0300

    V4L/DVB: videobuf: Rename vmalloc fields to vaddr
    The videobuf_dmabuf and videobuf_vmalloc_memory fields have a vmalloc
    field to store the kernel virtual address of vmalloc'ed buffers. Rename
    the field to vaddr.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/videobuf-dma-sg.h
+++ include/media/videobuf-dma-sg.h
@@ -52,7 +52,7 @@ struct videobuf_dmabuf {
 	struct page         **pages;
 
 	/* for kernel buffers */
-	void                *vmalloc;
+	void                *vaddr;
 
 	/* for overlay buffers (pci-pci dma) */
 	dma_addr_t          bus_addr;
--- include/media/videobuf-vmalloc.h
+++ include/media/videobuf-vmalloc.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct videobuf_vmalloc_memory {
 	u32                 magic;
 
-	void                *vmalloc;
+	void                *vaddr;
 
 	/* remap_vmalloc_range seems to need to run
 	 * after mmap() on some cases */
