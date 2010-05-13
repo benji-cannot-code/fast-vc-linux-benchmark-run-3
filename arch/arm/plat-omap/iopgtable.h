96e0ee3af7af2069dc32f1f3d9bd22d711c45cd2
Hiroshi DOYU <Hiroshi.DOYU@nokia.com>
2010-05-13 09:45:35 +0300

    omap iommu: Introduce iopgd_is_table MACRO
    A bit more strict comparison.
    
    Signed-off-by: Hiroshi DOYU <Hiroshi.DOYU@nokia.com>

--- arch/arm/plat-omap/iopgtable.h
+++ arch/arm/plat-omap/iopgtable.h
@@ -64,6 +64,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IOPGD_SECTION		(2 << 0)
 #define IOPGD_SUPER		(1 << 18 | 2 << 0)
 
+#define iopgd_is_table(x)	(((x) & 3) == IOPGD_TABLE)
+
 #define IOPTE_SMALL		(2 << 0)
 #define IOPTE_LARGE		(1 << 0)
 
