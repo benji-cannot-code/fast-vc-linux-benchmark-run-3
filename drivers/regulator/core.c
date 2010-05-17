6036767440899b80fc9baecc4186764c4978d23e
Sundar R Iyer <sundar.iyer@stericsson.com>
2010-05-17 21:24:48 +0530

    regulator: return set_mode is same mode is requested
    On Mon, 2010-05-17 at 17:34 +0200, Mark Brown wrote:
    > This doesn't seem like the right error handling - if the driver has a
    > set_mode() you'd *expect* it to have a get_mode() but there's no need
    > for it to be a strict requirement.
    True. In such a case, even a valid request would be lost! So now
    in the updated patch:
     - check if get_mode is present to avoid oops;
     - if get_mode is not present, proceed anyways for the request.
    
    Here is the updated patch:
    
    >From bad0d5eb51ef84be5b100e3dd0f5a590ea0529b6 Mon Sep 17 00:00:00 2001
    From: Sundar R Iyer <sundar.iyer@stericsson.com>
    Date: Fri, 14 May 2010 15:14:17 +0530
    Subject: [PATCH 1/1] regulator: return set_mode when same mode is requested
    
    save I/O costs by returning when the same mode is
    requested for the regulator
    
    Cc: Liam Girdwood <lrg@slimlogic.co.uk>
    Cc: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Sundar R Iyer <sundar.iyer@stericsson.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1746,6 +1746,7 @@ int regulator_set_mode(struct regulator *regulator, unsigned int mode)
 {
 	struct regulator_dev *rdev = regulator->rdev;
 	int ret;
+	int regulator_curr_mode;
 
 	mutex_lock(&rdev->mutex);
 
@@ -1753,6 +1754,15 @@ int regulator_set_mode(struct regulator *regulator, unsigned int mode)
 	if (!rdev->desc->ops->set_mode) {
 		ret = -EINVAL;
 		goto out;
+	}
+
+	/* return if the same mode is requested */
+	if (rdev->desc->ops->get_mode) {
+		regulator_curr_mode = rdev->desc->ops->get_mode(rdev);
+		if (regulator_curr_mode == mode) {
+			ret = 0;
+			goto out;
+		}
 	}
 
 	/* constraints check */
