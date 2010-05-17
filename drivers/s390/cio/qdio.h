e284add87a250f0eae835825704547001c8efc7f
Jan Glauber <jang@linux.vnet.ibm.com>
2010-05-17 10:00:17 +0200

    [S390] qdio: dont convert timestamps to microseconds
    Don't convert timestamps to microseconds, use timestamps returned by
    get_clock() directly.
    
    Signed-off-by: Jan Glauber <jang@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/cio/qdio.h
+++ drivers/s390/cio/qdio.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/debug.h>
 #include "chsc.h"
 
-#define QDIO_BUSY_BIT_PATIENCE		100	/* 100 microseconds */
-#define QDIO_INPUT_THRESHOLD		500	/* 500 microseconds */
+#define QDIO_BUSY_BIT_PATIENCE		(100 << 12)	/* 100 microseconds */
+#define QDIO_INPUT_THRESHOLD		(500 << 12)	/* 500 microseconds */
 
 /*
  * if an asynchronous HiperSockets queue runs full, the 10 seconds timer wait
@@ -369,11 +369,6 @@ static inline int multicast_outbound(struct qdio_q *q)
 {
 	return (q->irq_ptr->nr_output_qs > 1) &&
 	       (q->nr == q->irq_ptr->nr_output_qs - 1);
-}
-
-static inline unsigned long long get_usecs(void)
-{
-	return monotonic_clock() >> 12;
 }
 
 #define pci_out_supported(q) \
