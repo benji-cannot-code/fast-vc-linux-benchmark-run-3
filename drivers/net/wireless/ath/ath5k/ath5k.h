f4a7869c94e8511dc5614c0d4747d50ec3aa2978
Bruno Randolf <br1@einfach.org>
2010-05-19 10:31:10 +0900

    ath5k: run NF calibration only every 60 seconds
    Since NF calibration interferes with TX and RX and also has been the cause of
    other problems (when it's run concurrently with ath5k_reset) we want to run it
    less often - every 60 seconds for now.
    
    Signed-off-by: Bruno Randolf <br1@einfach.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath5k/ath5k.h
+++ drivers/net/wireless/ath/ath5k/ath5k.h
@@ -205,6 +205,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AR5K_TUNE_TPC_TXPOWER			false
 #define ATH5K_TUNE_CALIBRATION_INTERVAL_FULL    10000   /* 10 sec */
 #define ATH5K_TUNE_CALIBRATION_INTERVAL_ANI	1000	/* 1 sec */
+#define ATH5K_TUNE_CALIBRATION_INTERVAL_NF	60000	/* 60 sec */
 
 #define AR5K_INIT_CARR_SENSE_EN			1
 
@@ -1119,6 +1120,7 @@ struct ath5k_hw {
 	/* Calibration timestamp */
 	unsigned long		ah_cal_next_full;
 	unsigned long		ah_cal_next_ani;
+	unsigned long		ah_cal_next_nf;
 
 	/* Calibration mask */
 	u8			ah_cal_mask;
