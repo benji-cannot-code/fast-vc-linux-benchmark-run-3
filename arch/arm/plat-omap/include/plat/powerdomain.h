5a681ea5d401b4ce360049a1a3e8123597661769
Rajendra Nayak <rnayak@ti.com>
2010-05-18 20:24:01 -0600

    OMAP4 powerdomain: Fix pwrsts flags for ALWAYS ON domains
    The pwrsts flag for ALWAYS ON domains like always_on_core_pwrdm
    and wkup_pwrdm is wrongly populated with the define for a
    powerdomain power state, instead of the allowable state
    bitfields.
    This causes a few api's to fail sensing invalid pwrst
    requested.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/plat-omap/include/plat/powerdomain.h
+++ arch/arm/plat-omap/include/plat/powerdomain.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PWRDM_MAX_PWRSTS	4
 
 /* Powerdomain allowable state bitfields */
+#define PWRSTS_ON		(1 << PWRDM_POWER_ON)
 #define PWRSTS_OFF_ON		((1 << PWRDM_POWER_OFF) | \
 				 (1 << PWRDM_POWER_ON))
 
