c7ef96ac8ea89331990239af372e2748b361d1c9
Huang Weiyi <weiyi.huang@gmail.com>
2010-05-20 07:11:58 +0800

    ARM: SPEAR6xx: remove duplicated #include
    Remove duplicated #include('s) in
      arch/arm/mach-spear6xx/spear6xx.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-spear6xx/spear6xx.c
+++ arch/arm/mach-spear6xx/spear6xx.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <linux/amba/pl061.h>
-#include <linux/types.h>
 #include <linux/ptrace.h>
 #include <linux/io.h>
 #include <asm/hardware/vic.h>
