6359bec663781607caf45175a58e6a35d8a8f73a
H. Peter Anvin <hpa@linux.intel.com>
2010-05-19 13:40:14 -0700

    x86, mrst: make mrst_identify_cpu() an inline returning enum
    We have an enum, might as well use it.  While we're at it, make it an
    inline... there is really no point in calling a function for this
    stuff.
    
    LKML-Reference: <1274295685-6774-3-git-send-email-jacob.jun.pan@linux.intel.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Jacob Pan <jacob.jun.pan@linux.intel.com>

--- arch/x86/include/asm/mrst.h
+++ arch/x86/include/asm/mrst.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_MRST_H
 #define _ASM_X86_MRST_H
 extern int pci_mrst_init(void);
-extern int mrst_identify_cpu(void);
 extern int mrst_timer_options __cpuinitdata;
 int __init sfi_parse_mrtc(struct sfi_table_header *table);
 
@@ -27,6 +26,12 @@ enum mrst_cpu_type {
 	MRST_CPU_CHIP_LINCROFT = 1,
 	MRST_CPU_CHIP_PENWELL,
 };
+
+extern enum mrst_cpu_type __mrst_cpu_chip;
+static enum mrst_cpu_type mrst_identify_cpu(void)
+{
+	return __mrst_cpu_chip;
+}
 
 enum mrst_timer_options {
 	MRST_TIMER_DEFAULT,
