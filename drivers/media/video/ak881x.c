ef477f4a28c1be349a554bcff99854eae9d9f27e
Randy Dunlap <randy.dunlap@oracle.com>
2010-05-20 18:08:23 -0300

    V4L/DVB: ak881x needs slab.h
    Add slab.h to fix ak881x build:
    
    drivers/media/video/ak881x.c:265:error: implicit declaration of function 'kzalloc'
    drivers/media/video/ak881x.c:265:warning: assignment makes pointer from integer without a cast
    drivers/media/video/ak881x.c:283:error: implicit declaration of function 'kfree'
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/ak881x.c
+++ drivers/media/video/ak881x.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
 #include <linux/videodev2.h>
 
 #include <media/ak881x.h>
