678005ff4e1e632f0faab7cfb4b98cedaee0a0bd
Marek Szyprowski <m.szyprowski@samsung.com>
2010-05-20 08:13:06 +0200

    ARM: S5PV210: add common I2C device helpers
    This patch adds I2C platform helpers required by s3c2440-i2c driver.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s5pv210/setup-i2c2.c
+++ arch/arm/mach-s5pv210/setup-i2c2.c
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/mach-s5pv210/setup-i2c0.c
+/* linux/arch/arm/mach-s5pv210/setup-i2c2.c
  *
  * Copyright (c) 2009-2010 Samsung Electronics Co., Ltd.
  *		http://www.samsung.com/
  *
- * I2C0 GPIO configuration.
+ * I2C2 GPIO configuration.
  *
  * Based on plat-s3c64xx/setup-i2c0.c
  *
@@ -22,10 +22,10 @@ struct platform_device; /* don't need the contents */
 #include <plat/iic.h>
 #include <plat/gpio-cfg.h>
 
-void s3c_i2c0_cfg_gpio(struct platform_device *dev)
+void s3c_i2c2_cfg_gpio(struct platform_device *dev)
 {
-	s3c_gpio_cfgpin(S5PV210_GPD1(0), S3C_GPIO_SFN(2));
-	s3c_gpio_setpull(S5PV210_GPD1(0), S3C_GPIO_PULL_UP);
-	s3c_gpio_cfgpin(S5PV210_GPD1(1), S3C_GPIO_SFN(2));
-	s3c_gpio_setpull(S5PV210_GPD1(1), S3C_GPIO_PULL_UP);
+	s3c_gpio_cfgpin(S5PV210_GPD1(4), S3C_GPIO_SFN(2));
+	s3c_gpio_setpull(S5PV210_GPD1(4), S3C_GPIO_PULL_UP);
+	s3c_gpio_cfgpin(S5PV210_GPD1(5), S3C_GPIO_SFN(2));
+	s3c_gpio_setpull(S5PV210_GPD1(5), S3C_GPIO_PULL_UP);
 }
