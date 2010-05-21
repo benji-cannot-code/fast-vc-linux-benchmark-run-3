2ccb63ecf5eb1ed42304fae9a80b2217a097940d
Al Viro <viro@zeniv.linux.org.uk>
2010-05-21 11:26:35 -0400

    fix handling of offsets in cris eeprom.c, get rid of fake on-stack files
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/cris/arch-v10/drivers/eeprom.c
+++ arch/cris/arch-v10/drivers/eeprom.c
@@ -438,10 +438,7 @@ static loff_t eeprom_lseek(struct file * file, loff_t offset, int orig)
 
 static int eeprom_read_buf(loff_t addr, char * buf, int count)
 {
-  struct file f;
-
-  f.f_pos = addr;
-  return eeprom_read(&f, buf, count, &addr);
+  return eeprom_read(NULL, buf, count, &addr);
 }
 
 
@@ -451,7 +448,7 @@ static int eeprom_read_buf(loff_t addr, char * buf, int count)
 static ssize_t eeprom_read(struct file * file, char * buf, size_t count, loff_t *off)
 {
   int read=0;
-  unsigned long p = file->f_pos;
+  unsigned long p = *off;
 
   unsigned char page;
 
@@ -496,7 +493,7 @@ static ssize_t eeprom_read(struct file * file, char * buf, size_t count, loff_t 
   
   if(read > 0)
   {
-    file->f_pos += read;
+    *off += read;
   }
 
   mutex_unlock(&eeprom.lock);
@@ -507,11 +504,7 @@ static ssize_t eeprom_read(struct file * file, char * buf, size_t count, loff_t 
 
 static int eeprom_write_buf(loff_t addr, const char * buf, int count)
 {
-  struct file f;
-
-  f.f_pos = addr;
-  
-  return eeprom_write(&f, buf, count, &addr);
+  return eeprom_write(NULL, buf, count, &addr);
 }
 
 
@@ -535,7 +528,7 @@ static ssize_t eeprom_write(struct file * file, const char * buf, size_t count,
   {
     restart = 0;
     written = 0;
-    p = file->f_pos;
+    p = *off;
    
     
     while( (written < count) && (p < eeprom.size))
@@ -661,10 +654,10 @@ static ssize_t eeprom_write(struct file * file, const char * buf, size_t count,
   } /* for  */
 
   mutex_unlock(&eeprom.lock);
-  if (written == 0 && file->f_pos >= eeprom.size){
+  if (written == 0 && p >= eeprom.size){
     return -ENOSPC;
   }
-  file->f_pos += written;
+  *off = p;
   return written;
 }
 
