c8201773be53e4dce5c55116d836d15ab25c2850
Al Viro <viro@zeniv.linux.org.uk>
2010-05-21 11:02:14 -0400

    switch ecryptfs_get_locked_page() to struct inode *
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ecryptfs/mmap.c
+++ fs/ecryptfs/mmap.c
@@ -45,17 +45,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Returns locked and up-to-date page (if ok), with increased
  * refcnt.
  */
-struct page *ecryptfs_get_locked_page(struct file *file, loff_t index)
+struct page *ecryptfs_get_locked_page(struct inode *inode, loff_t index)
 {
-	struct dentry *dentry;
-	struct inode *inode;
-	struct address_space *mapping;
-	struct page *page;
-
-	dentry = file->f_path.dentry;
-	inode = dentry->d_inode;
-	mapping = inode->i_mapping;
-	page = read_mapping_page(mapping, index, (void *)file);
+	struct page *page = read_mapping_page(inode->i_mapping, index, NULL);
 	if (!IS_ERR(page))
 		lock_page(page);
 	return page;
