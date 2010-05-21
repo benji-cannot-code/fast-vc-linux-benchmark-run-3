454df97d8793c506c2d203b80832fc8de2499278
H Hartley Sweeten <hartleys@visionengravers.com>
2010-05-21 18:41:01 +0200

    i2c: Use <linux/io.h> instead of <asm/io.h>
    As warned by checkpatch.pl, <linux/io.h> should be used instead of
    <asm/io.h>.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- drivers/i2c/busses/i2c-amd756.c
+++ drivers/i2c/busses/i2c-amd756.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/init.h>
 #include <linux/acpi.h>
-#include <asm/io.h>
+#include <linux/io.h>
 
 /* AMD756 SMBus address offsets */
 #define SMB_ADDR_OFFSET		0xE0
