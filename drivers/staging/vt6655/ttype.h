52562edf45e2e0f0e7cf19e2ecf80b2e27ca0297
Charles Clément <caratorn@gmail.com>
2010-05-21 12:55:33 -0700

    Staging: vt6655: remove double definition of Calcu_LinkQual
    Calcu_LinkQual is already defined a couple lines earlier.
    
    Signed-off-by: Charles Clément <caratorn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6655/ttype.h
+++ drivers/staging/vt6655/ttype.h
@@ -66,10 +66,6 @@ typedef int             BOOL;
 #define Calcu_LinkQual
 #endif
 
-#ifndef Calcu_LinkQual
-#define Calcu_LinkQual
-#endif
-
 /****** Simple typedefs  ***************************************************/
 
 /* These lines assume that your compiler's longs are 32 bits and
