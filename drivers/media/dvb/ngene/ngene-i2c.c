fdd35767201bbac9e7070736d1a619f7d306966a
Huang Weiyi <weiyi.huang@gmail.com>
2010-05-22 14:12:06 -0300

    V4L/DVB: ngene: remove unused #include <linux/version.h>
    Remove unused #include <linux/version.h>('s) in
      drivers/media/dvb/ngene/ngene-dvb.c
      drivers/media/dvb/ngene/ngene-i2c.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/ngene/ngene-i2c.c
+++ drivers/media/dvb/ngene/ngene-i2c.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci_ids.h>
 #include <linux/smp_lock.h>
 #include <linux/timer.h>
-#include <linux/version.h>
 #include <linux/byteorder/generic.h>
 #include <linux/firmware.h>
 #include <linux/vmalloc.h>
