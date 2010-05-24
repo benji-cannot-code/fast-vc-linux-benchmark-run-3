132d2ac6c39de79c71a95492cc246cd99b70d255
Ben Hutchings <ben@decadent.org.uk>
2010-05-23 17:02:30 -0700

    fusion: fix kernel-doc notation
    The function name must be followed by a space, hypen, space, and a
    short description.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Eric Moore <Eric.Moore@lsi.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/message/fusion/mptscsih.c
+++ drivers/message/fusion/mptscsih.c
@@ -2571,9 +2571,7 @@ mptscsih_getclear_scsi_lookup(MPT_ADAPTER *ioc, int i)
 }
 
 /**
- * mptscsih_set_scsi_lookup
- *
- * writes a scmd entry into the ScsiLookup[] array list
+ * mptscsih_set_scsi_lookup - write a scmd entry into the ScsiLookup[] array list
  *
  * @ioc: Pointer to MPT_ADAPTER structure
  * @i: index into the array
@@ -2736,7 +2734,7 @@ mptscsih_scandv_complete(MPT_ADAPTER *ioc, MPT_FRAME_HDR *req,
 
 
 /**
- *	mptscsih_get_completion_code -
+ *	mptscsih_get_completion_code - get completion code from MPT request
  *	@ioc: Pointer to MPT_ADAPTER structure
  *	@req: Pointer to original MPT request frame
  *	@reply: Pointer to MPT reply frame (NULL if TurboReply)
