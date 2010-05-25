b77e4cf94e1855b8e388ab1a30d5d431711eb662
Huang Weiyi <weiyi.huang@gmail.com>
2010-05-25 14:07:25 +0000

    sh: remove duplicated #include
    Remove duplicated #include('s) in
      arch/sh/boards/mach-ecovec24/setup.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-ecovec24/setup.c
+++ arch/sh/boards/mach-ecovec24/setup.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/host.h>
 #include <linux/input.h>
 #include <linux/input/sh_keysc.h>
-#include <linux/mfd/sh_mobile_sdhi.h>
 #include <video/sh_mobile_lcdc.h>
 #include <sound/sh_fsi.h>
 #include <media/sh_mobile_ceu.h>
