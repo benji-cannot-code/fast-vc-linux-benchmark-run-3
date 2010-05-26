49892528cac6e6f528e45b47e4a7df31841cbc02
Lee Schermerhorn <lee.schermerhorn@hp.com>
2010-05-26 14:44:56 -0700

    numa: add generic percpu var numa_node_id() implementation
    Rework the generic version of the numa_node_id() function to use the new
    generic percpu variable infrastructure.
    
    Guard the new implementation with a new config option:
    
            CONFIG_USE_PERCPU_NUMA_NODE_ID.
    
    Archs which support this new implemention will default this option to 'y'
    when NUMA is configured.  This config option could be removed if/when all
    archs switch over to the generic percpu implementation of numa_node_id().
    Arch support involves:
    
      1) converting any existing per cpu variable implementations to use
         this implementation.  x86_64 is an instance of such an arch.
      2) archs that don't use a per cpu variable for numa_node_id() will
         need to initialize the new per cpu variable "numa_node" as cpus
         are brought on-line.  ia64 is an example.
      3) Defining USE_PERCPU_NUMA_NODE_ID in arch dependent Kconfig--e.g.,
         when NUMA is configured.  This is required because I have
         retained the old implementation by default to allow archs to
         be modified incrementally, as desired.
    
    Subsequent patches will convert x86_64 and ia64 to use this implemenation.
    
    Signed-off-by: Lee Schermerhorn <lee.schermerhorn@hp.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Mel Gorman <mel@csn.ul.ie>
    Reviewed-by: Christoph Lameter <cl@linux-foundation.org>
    Cc: Nick Piggin <npiggin@suse.de>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Eric Whitney <eric.whitney@hp.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: "Luck, Tony" <tony.luck@intel.com>
    Cc: Pekka Enberg <penberg@cs.helsinki.fi>
    Cc: <linux-arch@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -58,6 +58,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/div64.h>
 #include "internal.h"
 
+#ifdef CONFIG_USE_PERCPU_NUMA_NODE_ID
+DEFINE_PER_CPU(int, numa_node);
+EXPORT_PER_CPU_SYMBOL(numa_node);
+#endif
+
 /*
  * Array of node states.
  */
