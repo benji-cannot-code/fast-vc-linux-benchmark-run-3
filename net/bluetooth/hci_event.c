a449a8ffcca074a19bbda19e7308b4cc84fab3cb
Ron Shaffer <rshaffer@codeaurora.org>
2010-05-28 11:53:46 -0400

    Bluetooth: Reassigned copyright to Code Aurora Forum
    Qualcomm, Inc. has reassigned rights to Code Aurora Forum. Accordingly,
    as files are modified by Code Aurora Forum members, the copyright
    statement will be updated.
    
    Signed-off-by: Ron Shaffer <rshaffer@codeaurora.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
    BlueZ - Bluetooth protocol stack for Linux
-   Copyright (C) 2000-2001 Qualcomm Incorporated
+   Copyright (c) 2000-2001, 2010, Code Aurora Forum. All rights reserved.
 
    Written 2000,2001 by Maxim Krasnyansky <maxk@qualcomm.com>
 
