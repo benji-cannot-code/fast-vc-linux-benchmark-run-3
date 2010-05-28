a3ebbf3c6c41b8ed4470e24dc3c080adf45f80c8
Peter Horton <phorton@bitbox.co.uk>
2010-05-28 16:37:26 +0100

    AT91: Fix AT91SAM9G20 reset as per the errata in the data sheet
    If the SDRAM is not cleanly shutdown before reset it can be left driving
    the bus, which then stops the bootloader booting from NAND.
    
    Signed-off-by: Peter Horton <phorton@bitbox.co.uk>
    [nicolas.ferre@atmel.com: change file header line order]
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/at91sam9260.c
+++ arch/arm/mach-at91/at91sam9260.c
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "generic.h"
 #include "clock.h"
 
+extern void at91sam9g20_reset(void);
+
 static struct map_desc at91sam9260_io_desc[] __initdata = {
 	{
 		.virtual	= AT91_VA_BASE_SYS,
@@ -328,7 +330,11 @@ void __init at91sam9260_initialize(unsigned long main_clock)
 	else
 		iotable_init(at91sam9260_sram_desc, ARRAY_SIZE(at91sam9260_sram_desc));
 
-	at91_arch_reset = at91sam9260_reset;
+	if (cpu_is_at91sam9g20())
+		at91_arch_reset = at91sam9g20_reset;
+	else
+		at91_arch_reset = at91sam9260_reset;
+
 	pm_power_off = at91sam9260_poweroff;
 	at91_extern_irq = (1 << AT91SAM9260_ID_IRQ0) | (1 << AT91SAM9260_ID_IRQ1)
 			| (1 << AT91SAM9260_ID_IRQ2);
