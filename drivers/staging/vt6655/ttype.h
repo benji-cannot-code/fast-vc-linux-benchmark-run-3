c32e46d082dbf1e5253c0eac36f79b919d3e8e28
Charles Clément <caratorn@gmail.com>
2010-05-28 09:17:44 -0700

    Staging: vt6655: remove unused OUT definition
    Signed-off-by: Charles Clément <caratorn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6655/ttype.h
+++ drivers/staging/vt6655/ttype.h
@@ -34,10 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /******* Common definitions and typedefs ***********************************/
 
-#ifndef OUT
-#define OUT
-#endif
-
 #ifndef TxInSleep
 #define TxInSleep
 #endif
