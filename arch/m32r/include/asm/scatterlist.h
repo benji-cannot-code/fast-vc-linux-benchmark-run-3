d11c6fbef0f310475045cd45f0930c1d191dfb0e
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-05-31 15:59:04 +0900

    remove needless ISA_DMA_THRESHOLD
    Architectures don't need to define ISA_DMA_THRESHOLD anymore.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Acked-by: James Bottomley <James.Bottomley@suse.de>
    Acked-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- arch/m32r/include/asm/scatterlist.h
+++ arch/m32r/include/asm/scatterlist.h
@@ -4,6 +4,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm-generic/scatterlist.h>
 
-#define ISA_DMA_THRESHOLD (0x1fffffff)
-
 #endif /* _ASM_M32R_SCATTERLIST_H */
