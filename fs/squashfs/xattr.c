48a2fbaa6526dd41b1aaee1f4493b348c9c33c01
Phillip Lougher <phillip@lougher.demon.co.uk>
2010-05-31 18:50:22 +0100

    squashfs: fix filename in header comment
    Signed-off-by: Phillip Lougher <phillip@lougher.demon.co.uk>

--- fs/squashfs/xattr.c
+++ fs/squashfs/xattr.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program; if not, write to the Free Software
  * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * xattr_id.c
+ * xattr.c
  */
 
 #include <linux/init.h>
