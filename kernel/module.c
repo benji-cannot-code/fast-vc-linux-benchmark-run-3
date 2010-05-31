686236f5bf1f2eaa318bf7e46a2777eb4f02e0ae
Rusty Russell <rusty@rustcorp.com.au>
2010-05-31 19:53:50 +0930

    module: fix reference to mod->percpu after freeing module.
    Rafael sees a sometimes crash at precpu_modfree from kernel/module.c; it
    only occurred with another (since-reverted) patch, but that patch simply
    changed timing to uncover this bug, it was otherwise unrelated.
    
    The comment about the mod being freed is self-explanatory, but neither
    Tejun nor I read it.  This bug was introduced in 259354deaa, after it
    had previously been fixed in 6e2b75740b.  How embarrassing.
    
    Reported-by: "Rafael J. Wysocki" <rjw@sisk.pl>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Embarrassingly-Acked-by: Tejun Heo <tj@kernel.org>
    Cc: Masami Hiramatsu <mhiramat@redhat.com>
    Tested-by: "Rafael J. Wysocki" <rjw@sisk.pl>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/module.c
+++ kernel/module.c
@@ -2015,6 +2015,7 @@ static noinline struct module *load_module(void __user *umod,
 	long err = 0;
 	void *ptr = NULL; /* Stops spurious gcc warning */
 	unsigned long symoffs, stroffs, *strmap;
+	void __percpu *percpu;
 
 	mm_segment_t old_fs;
 
@@ -2159,6 +2160,8 @@ static noinline struct module *load_module(void __user *umod,
 			goto free_mod;
 		sechdrs[pcpuindex].sh_flags &= ~(unsigned long)SHF_ALLOC;
 	}
+	/* Keep this around for failure path. */
+	percpu = mod_percpu(mod);
 
 	/* Determine total sizes, and put offsets in sh_entsize.  For now
 	   this is done generically; there doesn't appear to be any
@@ -2464,7 +2467,7 @@ static noinline struct module *load_module(void __user *umod,
 	module_free(mod, mod->module_core);
 	/* mod will be freed with core. Don't access it beyond this line! */
  free_percpu:
-	percpu_modfree(mod);
+	free_percpu(percpu);
  free_mod:
 	kfree(args);
 	kfree(strmap);
