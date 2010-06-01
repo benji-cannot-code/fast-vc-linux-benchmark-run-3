5eef8ae0f1f0626147c90cdfb4dd72056d2b95cf
Ben Hutchings <bhutchings@solarflare.com>
2010-06-01 11:32:43 +0000

    sfc: Get port number from CS_PORT_NUM, not PCI function number
    A single shared memory region used to communicate with firmware is
    mapped into both PCI PFs of the SFC9020 and SFL9021.  Drivers must be
    able to identify which port they are addressing in order to use the
    correct sub-region.  Currently we use the PCI function number, but the
    PCI address may be virtualised.  Use the CS_PORT_NUM register field
    defined for just this purpose.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/net_driver.h
+++ drivers/net/sfc/net_driver.h
@@ -650,6 +650,7 @@ union efx_multicast_hash {
  * struct efx_nic - an Efx NIC
  * @name: Device name (net device name or bus id before net device registered)
  * @pci_dev: The PCI device
+ * @port_num: Index of this host port within the controller
  * @type: Controller type attributes
  * @legacy_irq: IRQ number
  * @workqueue: Workqueue for port reconfigures and the HW monitor.
@@ -733,6 +734,7 @@ union efx_multicast_hash {
 struct efx_nic {
 	char name[IFNAMSIZ];
 	struct pci_dev *pci_dev;
+	unsigned port_num;
 	const struct efx_nic_type *type;
 	int legacy_irq;
 	struct workqueue_struct *workqueue;
@@ -835,7 +837,7 @@ static inline const char *efx_dev_name(struct efx_nic *efx)
 
 static inline unsigned int efx_port_num(struct efx_nic *efx)
 {
-	return PCI_FUNC(efx->pci_dev->devfn);
+	return efx->port_num;
 }
 
 /**
