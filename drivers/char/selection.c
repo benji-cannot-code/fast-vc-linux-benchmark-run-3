fe0b1595452f082b5a9897c228b2469fd7a5eecd
Arnd Bergmann <arnd@arndb.de>
2010-06-01 22:53:08 +0200

    tty: remove tty_lock_nested
    This changes all remaining users of tty_lock_nested
    to be non-recursive, which lets us kill this function.
    As a consequence, we won't need to keep the lock count
    any more, which allows more simplifications later.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/char/selection.c
+++ drivers/char/selection.c
@@ -314,7 +314,8 @@ int paste_selection(struct tty_struct *tty)
 	struct  tty_ldisc *ld;
 	DECLARE_WAITQUEUE(wait, current);
 
-	tty_lock_nested(); /* always called with BTM from vt_ioctl */
+	/* always called with BTM from vt_ioctl */
+	WARN_ON(!tty_locked());
 
 	acquire_console_sem();
 	poke_blanked_console();
@@ -344,6 +345,5 @@ int paste_selection(struct tty_struct *tty)
 	__set_current_state(TASK_RUNNING);
 
 	tty_ldisc_deref(ld);
-	tty_unlock();
 	return 0;
 }
