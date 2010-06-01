1d68f62bb5eed7c89daf9e96dcd4e98832bd9b73
Jani Nikula <ext-jani.1.nikula@nokia.com>
2010-06-01 15:12:12 +0300

    OMAP: DSS2: OMAPFB: Check fb2display() return value
    Make sure NULL return value of fb2display() is not referenced. Found by
    Coverity.
    
    Signed-off-by: Jani Nikula <ext-jani.1.nikula@nokia.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- drivers/video/omap2/omapfb/omapfb-ioctl.c
+++ drivers/video/omap2/omapfb/omapfb-ioctl.c
@@ -859,7 +859,7 @@ int omapfb_ioctl(struct fb_info *fbi, unsigned int cmd, unsigned long arg)
 			break;
 		}
 
-		if (!display->driver->enable_te) {
+		if (!display || !display->driver->enable_te) {
 			r = -ENODEV;
 			break;
 		}
