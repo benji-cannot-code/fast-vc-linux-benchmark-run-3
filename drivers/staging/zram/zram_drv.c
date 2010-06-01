f0ffb0ddcd4cb591d1bfa026731b955913bddc77
Nitin Gupta <ngupta@vflare.org>
2010-06-01 13:31:24 +0530

    Staging: Rename ramzswap files to zram
    Related changes:
     - Modify revelant Kconfig and Makefile accordingly.
     - Change include filenames in code.
     - Remove dependency on CONFIG_SWAP in Kconfig as zram usage
    is no longer limited to swap disks.
    
    Signed-off-by: Nitin Gupta <ngupta@vflare.org>
    Acked-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/swapops.h>
 #include <linux/vmalloc.h>
 
-#include "ramzswap_drv.h"
+#include "zram_drv.h"
 
 /* Globals */
 static int ramzswap_major;
