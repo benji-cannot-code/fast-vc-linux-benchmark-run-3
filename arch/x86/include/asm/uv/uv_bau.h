c62798df7496b0962eb0a48d332da21defb62b56
Cliff Wickman <cpw@sgi.com>
2010-06-02 16:22:02 -0500

    x86, UV: Modularize BAU send and wait
    Streamline the large uv_flush_send_and_wait() function by use of
    a couple of helper functions.
    
    And remove some excess comments.
    
    Signed-off-by: Cliff Wickman <cpw@sgi.com>
    Cc: gregkh@suse.de
    LKML-Reference: <E1OJvNy-0004ay-IH@eag09.americas.sgi.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/uv/uv_bau.h
+++ arch/x86/include/asm/uv/uv_bau.h
@@ -76,7 +76,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DESC_STATUS_DESTINATION_TIMEOUT	2
 #define DESC_STATUS_SOURCE_TIMEOUT	3
 
-#define TIMEOUT_DELAY			10
 /*
  * delay for 'plugged' timeout retries, in microseconds
  */
