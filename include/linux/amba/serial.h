a3e27ee22674748e4ae797f8bb80e80d610a0363
Linus Walleij <linus.walleij@stericsson.com>
2010-06-02 20:40:22 +0100

    ARM: 6158/2: PL011 baudrate extension for ST-Ericssons derivative
    Implementation of the ST-Ericsson baudrate extension in the PL011
    block. In this modified variant it is possible to change the
    sampling factor from 16 to 8, and thanks to this we can get higher
    baudrates while still using the same peripheral clock.
    
    Also replace the simple division to determine the baud divisor
    with DIV_ROUND_CLOSEST() rather than a simple integer division.
    
    Cc: Alessandro Rubini <rubini@unipv.it>
    Cc: Jerzy Kasenberg <jerzy.kasenberg@tieto.com>
    Signed-off-by: Marcin Mielczarczyk <marcin.mielczarczyk@tieto.com>
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/linux/amba/serial.h
+++ include/linux/amba/serial.h
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define UART010_CR_TIE 		0x0020
 #define UART010_CR_RIE 		0x0010
 #define UART010_CR_MSIE		0x0008
+#define ST_UART011_CR_OVSFACT	0x0008	/* Oversampling factor */
 #define UART01x_CR_IIRLP	0x0004	/* SIR low power mode */
 #define UART01x_CR_SIREN	0x0002	/* SIR enable */
 #define UART01x_CR_UARTEN	0x0001	/* UART enable */
