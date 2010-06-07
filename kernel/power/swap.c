b2321ba22304210b2ed03d4c5b8de0d39c29a2b5
Cesar Eduardo Barros <cesarb@cesarb.net>
2010-06-07 22:23:12 +0200

    PM / Hibernate: Fix typos in comments in kernel/power/swap.c
    There are a few typos in kernel/power/swap.c.  Fix them.
    
    Signed-off-by: Cesar Eduardo Barros <cesarb@cesarb.net>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- kernel/power/swap.c
+++ kernel/power/swap.c
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *	The swap map is a data structure used for keeping track of each page
  *	written to a swap partition.  It consists of many swap_map_page
- *	structures that contain each an array of MAP_PAGE_SIZE swap entries.
+ *	structures that contain each an array of MAP_PAGE_ENTRIES swap entries.
  *	These structures are stored on the swap and linked together with the
  *	help of the .next_swap member.
  *
@@ -149,7 +149,7 @@ sector_t alloc_swapdev_block(int swap)
 
 /**
  *	free_all_swap_pages - free swap pages allocated for saving image data.
- *	It also frees the extents used to register which swap entres had been
+ *	It also frees the extents used to register which swap entries had been
  *	allocated.
  */
 
