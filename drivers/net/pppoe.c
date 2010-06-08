803b0e0f742cd06d7def86f8bc2b077f11a4174c
Rami Rosen <ramirose@gmail.com>
2010-06-08 19:07:56 +0000

    cleanup: remove pppoe_xmit() declaration.
    There is no need for pppoe_xmit() forward declaration in
    drivers/net/pppoe.c. This patch removes this  pppoe_xmit() declaration.
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/pppoe.c
+++ drivers/net/pppoe.c
@@ -90,7 +90,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PPPOE_HASH_SIZE (1 << PPPOE_HASH_BITS)
 #define PPPOE_HASH_MASK	(PPPOE_HASH_SIZE - 1)
 
-static int pppoe_xmit(struct ppp_channel *chan, struct sk_buff *skb);
 static int __pppoe_xmit(struct sock *sk, struct sk_buff *skb);
 
 static const struct proto_ops pppoe_ops;
