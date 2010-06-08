37892e027bc52643faca94c8af273136e7ba5cf9
Huang Ying <ying.huang@intel.com>
2010-06-08 14:09:10 +0800

    x86, mce: Fix MSR_IA32_MCI_CTL2 CMCI threshold setup
    It is reported that CMCI is not raised when number of corrected error
    reaches preset threshold. After inspection, it is found that
    MSR_IA32_MCI_CTL2 threshold field is not setup properly. This patch
    fixed it.
    
    Value of MCI_CTL2_CMCI_THRESHOLD_MASK is fixed according to x86_64
    Software Developer's Manual too.
    
    Reported-by: Shaohui Zheng <shaohui.zheng@intel.com>
    Signed-off-by: Huang Ying <ying.huang@intel.com>
    LKML-Reference: <1275977350.3444.660.camel@yhuang-dev.sh.intel.com>
    Reviewed-by: Hidetoshi Seto <seto.hidetoshi@jp.fujitsu.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/mce.h
+++ arch/x86/include/asm/mce.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* CTL2 register defines */
 #define MCI_CTL2_CMCI_EN		(1ULL << 30)
-#define MCI_CTL2_CMCI_THRESHOLD_MASK	0xffffULL
+#define MCI_CTL2_CMCI_THRESHOLD_MASK	0x7fffULL
 
 #define MCJ_CTX_MASK		3
 #define MCJ_CTX(flags)		((flags) & MCJ_CTX_MASK)
