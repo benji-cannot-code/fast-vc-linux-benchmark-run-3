a68bb328f3cda245a584cab6e29ce008c4e720c8
Randy Dunlap <randy.dunlap@oracle.com>
2010-06-09 15:44:09 -0700

    niu: always include of_device.h
    The niu driver uses struct of_device when built on any arch, not
    only SPARC64, so always #include <linux/of_device.h>.
    
    drivers/net/niu.c:9700: warning: 'struct of_device' declared inside parameter list
    drivers/net/niu.c:9700: warning: its scope is only this definition or declaration, which is probably not what you want
    drivers/net/niu.c:9716: warning: assignment from incompatible pointer type
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Acked-by: Dave S. Miller <davem@davemloft.net>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- drivers/net/niu.c
+++ drivers/net/niu.c
@@ -29,10 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 
 #include <linux/io.h>
-
-#ifdef CONFIG_SPARC64
 #include <linux/of_device.h>
-#endif
 
 #include "niu.h"
 
