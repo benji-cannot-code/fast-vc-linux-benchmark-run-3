9209ba786803a4236923595f66e2b3b5af01b5b4
Samuel Ortiz <sameo@linux.intel.com>
2010-06-10 16:54:16 +0200

    Staging: rtl8192e: Do not mess with carrier settings while scanning
    Toggling the link carrier is a non sense and is the grossest locking I can
    think of. Moreover, it's giving a completely inaccurate status to userspace
    who could for example decide to turn the interface down on carrier off
    detection.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192e/r8192E_core.c
+++ drivers/staging/rtl8192e/r8192E_core.c
@@ -4319,7 +4319,6 @@ RESET_START:
 			del_timer_sync(&ieee->associate_timer);
                         cancel_delayed_work(&ieee->associate_retry_wq);
 			ieee80211_stop_scan(ieee);
-			netif_carrier_off(dev);
 			up(&ieee->wx_sem);
 		}
 		else{
