01bf7072e3201b4a67561c2100d45d20d5892d3c
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-06-10 17:48:04 +0200

    ARM: imx1: rename imx_i2c_device to follow a common naming scheme
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx1/devices.c
+++ arch/arm/mach-mx1/devices.c
@@ -67,7 +67,7 @@ static struct resource imx_i2c_resources[] = {
 	},
 };
 
-struct platform_device imx_i2c_device = {
+struct platform_device imx_i2c_device0 = {
 	.name           = "imx-i2c",
 	.id             = 0,
 	.resource       = imx_i2c_resources,
