ab25e47f2e250781d272b6b05f1225f2079f4c1d
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-06-11 12:17:04 +0200

    Staging: vt665*: fix typos concerning "management"
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6656/80211mgr.h
+++ drivers/staging/vt6656/80211mgr.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * File: 80211mgr.h
  *
- * Purpose: 802.11 managment frames pre-defines.
+ * Purpose: 802.11 management frames pre-defines.
  *
  *
  * Author: Lyndon Chen
