45193951bf39956cc36a1038cd1cf15d86b56b45
Daniel Halperin <dhalperi@cs.washington.edu>
2010-06-14 13:10:29 -0700

    iwlwifi: update LQ for bcast station on channel change
    The rate table in the bcast LQ is computed only when the station is
    allocated, and chooses the lowest rate for the band. Because of when this
    occurs, this is the 2.4 GHz band and uses the 0x420a (CCK, 1 Mbps) rate. In 5 GHz
    beaconing mode, this rate will prevent beacons from being sent and any other
    packets from being received.
    
    We can fix this by re-initializing the bcast station's LQ command when the
    channel is changed.
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-5000.c
+++ drivers/net/wireless/iwlwifi/iwl-5000.c
@@ -394,6 +394,7 @@ static struct iwl_lib_ops iwl5000_lib = {
 		.set_ct_kill = iwl5000_set_ct_threshold,
 	 },
 	.manage_ibss_station = iwlagn_manage_ibss_station,
+	.update_bcast_station = iwl_update_bcast_station,
 	.debugfs_ops = {
 		.rx_stats_read = iwl_ucode_rx_stats_read,
 		.tx_stats_read = iwl_ucode_tx_stats_read,
@@ -456,6 +457,7 @@ static struct iwl_lib_ops iwl5150_lib = {
 		.set_ct_kill = iwl5150_set_ct_threshold,
 	 },
 	.manage_ibss_station = iwlagn_manage_ibss_station,
+	.update_bcast_station = iwl_update_bcast_station,
 	.debugfs_ops = {
 		.rx_stats_read = iwl_ucode_rx_stats_read,
 		.tx_stats_read = iwl_ucode_tx_stats_read,
