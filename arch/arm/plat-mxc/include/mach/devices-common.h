2721799d58e13c10154093d46e4b1f21b8371500
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-06-15 18:14:19 +0200

    ARM: imx: Change the way nand devices are registered (generic part)
    Instead of having a platform device defined in a devices.c and register
    it in the machine file allocate it dynamically.  This reduces the size
    of needed memory as the new function is discarded after init and only
    the existing devices are created.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/devices-common.h
+++ arch/arm/plat-mxc/include/mach/devices-common.h
@@ -14,3 +14,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct platform_device *imx_add_platform_device(const char *name, int id,
 		const struct resource *res, unsigned int num_resources,
 		const void *data, size_t size_data);
+
+#include <mach/mxc_nand.h>
+struct platform_device *__init imx_add_mxc_nand_v1(resource_size_t iobase,
+		int irq, const struct mxc_nand_platform_data *pdata);
+struct platform_device *__init imx_add_mxc_nand_v21(resource_size_t iobase,
+		int irq, const struct mxc_nand_platform_data *pdata);
