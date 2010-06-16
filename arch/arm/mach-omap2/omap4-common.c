c86fd253d1c6552d7c01bee6c11748202b362283
Santosh Shilimkar <santosh.shilimkar@ti.com>
2010-06-16 22:19:47 +0530

    ARM: OMAP4: PM: Add WakeupGen module as OMAP gic_arch_extn
    OMAP WakeupGen is the interrupt controller extension used along
    with ARM GIC to wake the CPU out from low power states on
    external interrupts.
    
    The WakeupGen unit is responsible for generating the wakeup event
    from the incoming interrupts and enable bits. It is implemented
    in the MPU always ON power domain. During normal operation,
    WakeupGen delivers the external interrupts directly to the GIC.
    
    WakeupGen specification has one restriction as per Veyron version 1.6.
    It is SW responsibility to program interrupt enabling/disabling
    coherently in the GIC and in the WakeupGen enable registers. That is, a
    given interrupt for a given CPU is either enable at both GIC and WakeupGen,
    or disable at both, but no mix. That's the reason the WakeupGen is
    implemented as an extension of GIC.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Acked-by: Jean Pihet <j-pihet@ti.com>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Tested-by: Vishwanath BS <vishwanath.bs@ti.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/irqs.h>
 
 #include <mach/hardware.h>
+#include <mach/omap-wakeupgen.h>
 
 #include "common.h"
 #include "omap4-sar-layout.h"
@@ -45,6 +46,8 @@ void __init gic_init_irq(void)
 	/* Static mapping, never released */
 	omap_irq_base = ioremap(OMAP44XX_GIC_CPU_BASE, SZ_512);
 	BUG_ON(!omap_irq_base);
+
+	omap_wakeupgen_init();
 
 	gic_init(0, 29, gic_dist_base_addr, omap_irq_base);
 }
