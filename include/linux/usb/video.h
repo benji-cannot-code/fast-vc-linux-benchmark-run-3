050e8563d6f73a0b9a0a0a01e54bcdd6f06ff5ea
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2010-06-17 11:11:51 -0300

    V4L/DVB: uvcvideo: Define control information bits using macros
    Use the macros instead of hardcoding numerical constants for the
    controls information bitfield.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/linux/usb/video.h
+++ include/linux/usb/video.h
@@ -161,5 +161,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define UVC_STATUS_TYPE_CONTROL				1
 #define UVC_STATUS_TYPE_STREAMING			2
 
+/* 4.1.2. Control Capabilities */
+#define UVC_CONTROL_CAP_GET				(1 << 0)
+#define UVC_CONTROL_CAP_SET				(1 << 1)
+#define UVC_CONTROL_CAP_DISABLED			(1 << 2)
+#define UVC_CONTROL_CAP_AUTOUPDATE			(1 << 3)
+#define UVC_CONTROL_CAP_ASYNCHRONOUS			(1 << 4)
+
 #endif /* __LINUX_USB_VIDEO_H */
 
