fedab357a88c6cf0ea8c051e7baeb4a9930e98a5
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2010-06-22 01:25:50 +0200

    Staging: batman-adv: Add release information for version 2010.0.0
    Signed-off-by: Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/batman-adv/main.h
+++ drivers/staging/batman-adv/main.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRIVER_DESC   "B.A.T.M.A.N. advanced"
 #define DRIVER_DEVICE "batman-adv"
 
-#define SOURCE_VERSION "0.2.2-beta"
+#define SOURCE_VERSION "2010.0.0"
 
 
 /* B.A.T.M.A.N. parameters */
