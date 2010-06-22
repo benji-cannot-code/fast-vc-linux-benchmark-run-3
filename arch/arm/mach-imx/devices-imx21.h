316b22f336154aeff302057457586ed9251a44dd
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-06-22 09:27:33 +0200

    ARM: imx: dynamically register imx-i2c devices (imx21)
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-imx/devices-imx21.h
+++ arch/arm/mach-imx/devices-imx21.h
@@ -10,5 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/mx21.h>
 #include <mach/devices-common.h>
 
+#define imx21_add_i2c_imx(pdata)	\
+	imx_add_imx_i2c(0, MX2x_I2C_BASE_ADDR, SZ_4K, MX2x_INT_I2C, pdata)
+
 #define imx21_add_mxc_nand(pdata)	\
 	imx_add_mxc_nand_v1(MX21_NFC_BASE_ADDR, MX21_INT_NANDFC, pdata)
