ddaab572221f76713492ef6d2159481a48f73306
Colin Cross <ccross@android.com>
2010-06-23 15:49:17 -0700

    [ARM] tegra: Add support for reading fuses
    The Tegra SOC contains fuses to identify the CPU type and
    bin, and a unique id.  The CPU info is required to determine
    the correct voltages for each cpu and core frequency.
    
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "board.h"
 #include "clock.h"
+#include "fuse.h"
 
 static __initdata struct tegra_clk_init_table common_clk_init_table[] = {
 	/* name		parent		rate		enabled */
@@ -56,6 +57,7 @@ void __init tegra_init_cache(void)
 
 void __init tegra_common_init(void)
 {
+	tegra_init_fuse();
 	tegra_init_clock();
 	tegra_clk_init_from_table(common_clk_init_table);
 	tegra_init_cache();
