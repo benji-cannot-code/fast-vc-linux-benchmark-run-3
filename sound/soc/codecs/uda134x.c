d1766d2e43177b5a5048ddcb28834ea68b7abf47
Vladimir Zapolskiy <vzapolskiy@gmail.com>
2010-06-24 15:17:07 +0400

    ASoC: uda134x: add DATA011 register found in codecs family
    In UDA1340, UDA1344 and UDA1345 codecs there is one more functional
    register in part of DATA0 tranfser. For UDA1341 this register
    coincides with EA register.
    
    Signed-off-by: Vladimir Zapolskiy <vzapolskiy@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/uda134x.c
+++ sound/soc/codecs/uda134x.c
@@ -46,7 +46,7 @@ static const char uda134x_reg[UDA134X_REGS_NUM] = {
 	/* Extended address registers */
 	0x04, 0x04, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00,
 	/* Status, data regs */
-	0x00, 0x83, 0x00, 0x40, 0x80, 0x00,
+	0x00, 0x83, 0x00, 0x40, 0x80, 0xC0, 0x00,
 };
 
 /*
@@ -105,6 +105,7 @@ static int uda134x_write(struct snd_soc_codec *codec, unsigned int reg,
 	case UDA134X_DATA000:
 	case UDA134X_DATA001:
 	case UDA134X_DATA010:
+	case UDA134X_DATA011:
 		addr = UDA134X_DATA0_ADDR;
 		break;
 	case UDA134X_DATA1:
