d3dcda593f15815a74cac1f8610ad93c86bdd3a5
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2010-06-24 13:18:35 -0700

    iwlwifi: add support for device tx flush request
    "Flush" request can come from two different sources, it can either from
    mac80211, or from device when the operation is needed. Here
    adding the support for device issue "flush" request.
    
    When receive tx complete with status is TX_STATUS_FAIL_RFKILL_FLUSH,
    issue REPLY_TXFIFO_FLUSH command to uCode to flush out all the tx frames
    in queues.
    
    In this condition, since mac80211 has no knowledge of "flush" operation,
    driver need to stop all the tx queues and wait for the operation
    completed before wake up the queues for frames transmission.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-5000.c
+++ drivers/net/wireless/iwlwifi/iwl-5000.c
@@ -404,6 +404,7 @@ static struct iwl_lib_ops iwl5000_lib = {
 	.check_plcp_health = iwl_good_plcp_health,
 	.check_ack_health = iwl_good_ack_health,
 	.txfifo_flush = iwlagn_txfifo_flush,
+	.dev_txfifo_flush = iwlagn_dev_txfifo_flush,
 };
 
 static struct iwl_lib_ops iwl5150_lib = {
@@ -468,6 +469,7 @@ static struct iwl_lib_ops iwl5150_lib = {
 	.check_plcp_health = iwl_good_plcp_health,
 	.check_ack_health = iwl_good_ack_health,
 	.txfifo_flush = iwlagn_txfifo_flush,
+	.dev_txfifo_flush = iwlagn_dev_txfifo_flush,
 };
 
 static const struct iwl_ops iwl5000_ops = {
--- drivers/net/wireless/iwlwifi/iwl-dev.h
+++ drivers/net/wireless/iwlwifi/iwl-dev.h
@@ -1346,6 +1346,7 @@ struct iwl_priv {
 	struct work_struct ct_enter;
 	struct work_struct ct_exit;
 	struct work_struct start_internal_scan;
+	struct work_struct tx_flush;
 
 	struct tasklet_struct irq_tasklet;
 
