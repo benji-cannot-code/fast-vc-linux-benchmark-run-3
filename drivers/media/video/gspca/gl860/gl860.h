c71d61206e9872a31d2e3e80573d56f7ceb0f79d
Olivier Lorin <o.lorin@laposte.net>
2010-06-24 04:22:37 -0300

    V4L/DVB: gspca - gl860: new driver for MI2020 sensor
    - new MI2020 driver version made from a webcam gift
    - all previous flavors of this driver removed
    
    Signed-off-by: Olivier Lorin <o.lorin@laposte.net>
    Signed-off-by: Jean-François Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/gl860/gl860.h
+++ drivers/media/video/gspca/gl860/gl860.h
@@ -33,12 +33,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ID_OV2640   2
 #define ID_OV9655   4
 #define ID_MI2020   8
-#define ID_MI2020b 16
 
 #define _MI1320_  (((struct sd *) gspca_dev)->sensor == ID_MI1320)
 #define _MI2020_  (((struct sd *) gspca_dev)->sensor == ID_MI2020)
-#define _MI2020b_ (((struct sd *) gspca_dev)->sensor == ID_MI2020b)
-#define _MI2020c_ 0
 #define _OV2640_  (((struct sd *) gspca_dev)->sensor == ID_OV2640)
 #define _OV9655_  (((struct sd *) gspca_dev)->sensor == ID_OV9655)
 
