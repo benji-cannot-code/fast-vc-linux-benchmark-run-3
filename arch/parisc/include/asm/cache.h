6b5e0187c7434ca942fdc8363a07cad4096c06d5
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-06-29 16:29:04 +0900

    parisc: remove homegrown L1_CACHE_ALIGN macro
    Let's use the standard L1_CACHE_ALIGN macro instead.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Kyle McMartin <kyle@redhat.com>

--- arch/parisc/include/asm/cache.h
+++ arch/parisc/include/asm/cache.h
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
-#define L1_CACHE_ALIGN(x)       (((x)+(L1_CACHE_BYTES-1))&~(L1_CACHE_BYTES-1))
-
 #define SMP_CACHE_BYTES L1_CACHE_BYTES
 
 #define ARCH_DMA_MINALIGN	L1_CACHE_BYTES
