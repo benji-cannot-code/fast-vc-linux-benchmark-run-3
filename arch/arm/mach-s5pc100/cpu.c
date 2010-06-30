0d2ea859b6fa721e4fe93feeae9c16c5d37e513d
Naveen Krishna Ch <ch.naveen@samsung.com>
2010-06-30 21:50:24 +0900

    ARM: S5PC100: Add Touchscreen support.
    This patch adds touchscreen support for S5PC100.
    
    Signed-off-by: Naveen Krishna Ch <ch.naveen@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s5pc100/cpu.c
+++ arch/arm/mach-s5pc100/cpu.c
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/ata-core.h>
 #include <plat/iic-core.h>
 #include <plat/sdhci.h>
+#include <plat/adc-core.h>
 #include <plat/onenand-core.h>
 
 #include <plat/s5pc100.h>
@@ -88,6 +89,8 @@ void __init s5pc100_map_io(void)
 	s5pc100_default_sdhci0();
 	s5pc100_default_sdhci1();
 	s5pc100_default_sdhci2();
+
+	s3c_adc_setname("s3c64xx-adc");
 
 	/* the i2c devices are directly compatible with s3c2440 */
 	s3c_i2c0_setname("s3c2440-i2c");
