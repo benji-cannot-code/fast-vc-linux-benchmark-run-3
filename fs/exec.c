7ff29d687576dccffa154acc2676970e36508c62
Arnd Bergmann <arnd@arndb.de>
2010-07-04 00:02:25 +0200

    do_coredump: Do not take BKL
    core_pattern is not actually protected and hasn't been
    ever since we introduced procfs support for sysctl -- a
    _long_ time. Don't take it here either.
    
    Also nothing inside do_coredump appears to require bkl
    protection.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    [ remove smp_lock.h headers ]
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/stat.h>
 #include <linux/fcntl.h>
-#include <linux/smp_lock.h>
 #include <linux/swap.h>
 #include <linux/string.h>
 #include <linux/init.h>
@@ -1892,13 +1891,7 @@ void do_coredump(long signr, int exit_code, struct pt_regs *regs)
 	 */
 	clear_thread_flag(TIF_SIGPENDING);
 
-	/*
-	 * lock_kernel() because format_corename() is controlled by sysctl, which
-	 * uses lock_kernel()
-	 */
- 	lock_kernel();
 	ispipe = format_corename(corename, signr);
-	unlock_kernel();
 
  	if (ispipe) {
 		int dump_count;
