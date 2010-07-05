df74cc17118da803effe99751ca4c6ca471d41f3
Tony Lindgren <tony@atomide.com>
2010-07-05 16:31:40 +0300

    omap: mux: Remove old mux code
    All mach-omap2 omaps should now use the new mux code.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-generic.c
+++ arch/arm/mach-omap2/board-generic.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/map.h>
 
 #include <mach/gpio.h>
-#include <plat/mux.h>
 #include <plat/usb.h>
 #include <plat/board.h>
 #include <plat/common.h>
--- arch/arm/mach-omap2/board-rx51-peripherals.c
+++ arch/arm/mach-omap2/board-rx51-peripherals.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/host.h>
 
 #include <plat/mcspi.h>
-#include <plat/mux.h>
 #include <plat/board.h>
 #include <plat/common.h>
 #include <plat/dma.h>
