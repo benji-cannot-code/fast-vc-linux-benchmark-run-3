a65293ec28c85b3bc510c4ded187681dddd44eba
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2010-07-06 15:39:00 -0700

    memblock: Expose MEMBLOCK_ALLOC_ANYWHERE
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- mm/memblock.c
+++ mm/memblock.c
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/memblock.h>
 
-#define MEMBLOCK_ALLOC_ANYWHERE	0
-
 struct memblock memblock;
 
 static int memblock_debug;
