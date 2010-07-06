747510d0b0869d6fc5c8bc838267cd931ca5c699
Palash Bandyopadhyay <palash.bandyopadhyay@conexant.com>
2010-07-06 18:12:25 -0300

    [media] cx231xx: Added support for Carraera, Shelby, RDx_253S and VIDEO_GRABBER
    Added support for new cx231xx boards - Carraera, Shelby, RDx_253S and
    VIDEO_GRABBER.
    
    [mchehab@redhat.com: Fix a merge conflict with BKL removal patches]
    Signed-off-by: Palash Bandyopadhyay <palash.bandyopadhyay@conexant.com>
    Signed-off-by: Devin Heitmueller <dheitmueller@hauppauge.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx-i2c.c
+++ drivers/media/video/cx231xx/cx231xx-i2c.c
@@ -360,7 +360,7 @@ static int cx231xx_i2c_xfer(struct i2c_adapter *i2c_adap,
 
 	if (num <= 0)
 		return 0;
-
+	mutex_lock(&dev->i2c_lock);
 	for (i = 0; i < num; i++) {
 
 		addr = msgs[i].addr >> 1;
@@ -373,6 +373,7 @@ static int cx231xx_i2c_xfer(struct i2c_adapter *i2c_adap,
 			rc = cx231xx_i2c_check_for_device(i2c_adap, &msgs[i]);
 			if (rc < 0) {
 				dprintk2(2, " no device\n");
+				mutex_lock(&dev->i2c_lock);
 				return rc;
 			}
 
@@ -385,7 +386,7 @@ static int cx231xx_i2c_xfer(struct i2c_adapter *i2c_adap,
 			}
 		} else if (i + 1 < num && (msgs[i + 1].flags & I2C_M_RD) &&
 			   msgs[i].addr == msgs[i + 1].addr
-			   && (msgs[i].len <= 2) && (bus->nr < 2)) {
+			   && (msgs[i].len <= 2) && (bus->nr < 3)) {
 			/* read bytes */
 			rc = cx231xx_i2c_recv_bytes_with_saddr(i2c_adap,
 							       &msgs[i],
@@ -408,10 +409,11 @@ static int cx231xx_i2c_xfer(struct i2c_adapter *i2c_adap,
 		if (i2c_debug >= 2)
 			printk("\n");
 	}
-
+	mutex_unlock(&dev->i2c_lock);
 	return num;
 err:
 	dprintk2(2, " ERROR: %i\n", rc);
+	mutex_unlock(&dev->i2c_lock);
 	return rc;
 }
 
