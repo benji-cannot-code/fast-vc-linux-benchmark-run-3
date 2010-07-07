356c328834d60ebb463435a7f97f13d6349d153a
Stefan Richter <stefanr@s5r6.in-berlin.de>
2010-07-07 14:13:14 +0200

    firewire: core: ensure some userspace API constants match corresponding kernel API constants
    The FW_ISO_ constants of the in-kernel API of firewire-core and
    FW_CDEV_ISO_ constants of the userspace API of firewire-core have
    nothing to do with each other --- except that the core-cdev.c
    implementation relies on them having the same values.
    
    Hence put some compile-time assertions into core-cdev.c.  It's lame but
    I prefer it over including the userspace API header into the kernelspace
    API header and defining kernelspace API constants from userspace API
    constants.  Nor do I want to expose the kernelspace constants in one of
    the two firewire headers that are exported to userland since this only
    concerns the core-cdev.c implementation.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/core-cdev.c
+++ drivers/firewire/core-cdev.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <linux/bug.h>
 #include <linux/compat.h>
 #include <linux/delay.h>
 #include <linux/device.h>
@@ -910,6 +911,9 @@ static int ioctl_create_iso_context(struct client *client, union ioctl_arg *arg)
 	struct fw_cdev_create_iso_context *a = &arg->create_iso_context;
 	struct fw_iso_context *context;
 
+	BUILD_BUG_ON(FW_CDEV_ISO_CONTEXT_TRANSMIT != FW_ISO_CONTEXT_TRANSMIT ||
+		     FW_CDEV_ISO_CONTEXT_RECEIVE  != FW_ISO_CONTEXT_RECEIVE);
+
 	if (a->channel > 63)
 		return -EINVAL;
 
@@ -1060,6 +1064,13 @@ static int ioctl_queue_iso(struct client *client, union ioctl_arg *arg)
 static int ioctl_start_iso(struct client *client, union ioctl_arg *arg)
 {
 	struct fw_cdev_start_iso *a = &arg->start_iso;
+
+	BUILD_BUG_ON(
+	    FW_CDEV_ISO_CONTEXT_MATCH_TAG0 != FW_ISO_CONTEXT_MATCH_TAG0 ||
+	    FW_CDEV_ISO_CONTEXT_MATCH_TAG1 != FW_ISO_CONTEXT_MATCH_TAG1 ||
+	    FW_CDEV_ISO_CONTEXT_MATCH_TAG2 != FW_ISO_CONTEXT_MATCH_TAG2 ||
+	    FW_CDEV_ISO_CONTEXT_MATCH_TAG3 != FW_ISO_CONTEXT_MATCH_TAG3 ||
+	    FW_CDEV_ISO_CONTEXT_MATCH_ALL_TAGS != FW_ISO_CONTEXT_MATCH_ALL_TAGS);
 
 	if (client->iso_context == NULL || a->handle != 0)
 		return -EINVAL;
