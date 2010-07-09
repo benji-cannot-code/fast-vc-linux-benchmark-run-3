bef8c61efc513bf8ef48fa9fba013594d2c66cde
Jing Huang <huangj@brocade.com>
2010-07-08 19:51:28 -0700

    [SCSI] bfa: fix rport speed setting
    When a rport goes offline, its speed setting was not reset. Subsequently, if
    the rport was not deleted due to it coming back online within rport del
    timeout, previously discovered speed would continue to show up. The fix is to
    reset the speed when processing rport offline transition.
    
    In rport attributes, rport's with unknown speed were indicated as TRL
    enforced.  The right thing do to would be to use TRL default speed to
    determine if TRL is enforced, when TRL is enabled.
    
    Signed-off-by: Jing Huang <huangj@brocade.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/bfa/rport_ftrs.c
+++ drivers/scsi/bfa/rport_ftrs.c
@@ -275,6 +275,7 @@ void  bfa_fcs_rpf_rport_offline(struct bfa_fcs_rport_s *rport)
 	if (__fcs_min_cfg(rport->port->fcs))
 		return;
 
+	rport->rpf.rpsc_speed = 0;
 	bfa_sm_send_event(&rport->rpf, RPFSM_EVENT_RPORT_OFFLINE);
 }
 
