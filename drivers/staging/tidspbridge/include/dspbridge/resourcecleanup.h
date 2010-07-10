22313893e0d51f7cffad60ac5bc6615a3628c9d7
Rene Sapiens <rene.sapiens@ti.com>
2010-07-09 21:23:56 -0500

    staging: ti dspbridge: Rename words with camel case.
    The intention of this patch is to rename the remaining variables with camel
    case. Variables will be renamed avoiding camel case and Hungarian notation.
    The words to be renamed in this patch are:
    ========================================
    hDevContext to dev_ctxt
    hDevExtension to dev_extension
    hdevObject to device_obj
    hDispObject to disp_obj
    hDrVObject to driver_obj
    hDRVObject to driver_obj
    hMGRHandle to mgr_handle
    hNldrObject to nldr
    hNode1 to node1
    hNode2 to node2
    hNodeRes to node_resource
    hPCtxt to process_ctxt
    hProc to proc
    hStreamHandle to stream_handle
    ========================================
    
    Signed-off-by: Rene Sapiens <rene.sapiens@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/include/dspbridge/resourcecleanup.h
+++ drivers/staging/tidspbridge/include/dspbridge/resourcecleanup.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern int drv_get_proc_ctxt_list(struct process_context **pPctxt,
 					 struct drv_object *hdrv_obj);
 
-extern int drv_insert_proc_context(struct drv_object *hDrVObject,
-					  void *hPCtxt);
+extern int drv_insert_proc_context(struct drv_object *driver_obj,
+					  void *process_ctxt);
 
 extern int drv_remove_all_dmm_res_elements(void *ctxt);
 
@@ -32,7 +32,7 @@ extern int drv_proc_set_pid(void *ctxt, s32 process);
 
 extern int drv_remove_all_resources(void *pPctxt);
 
-extern int drv_remove_proc_context(struct drv_object *hDRVObject,
+extern int drv_remove_proc_context(struct drv_object *driver_obj,
 					  void *pr_ctxt);
 
 extern int drv_get_node_res_element(void *hnode, void *node_res,
@@ -41,11 +41,11 @@ extern int drv_get_node_res_element(void *hnode, void *node_res,
 extern int drv_insert_node_res_element(void *hnode, void *node_res,
 					      void *ctxt);
 
-extern void drv_proc_node_update_heap_status(void *hNodeRes, s32 status);
+extern void drv_proc_node_update_heap_status(void *node_resource, s32 status);
 
 extern int drv_remove_node_res_element(void *node_res, void *status);
 
-extern void drv_proc_node_update_status(void *hNodeRes, s32 status);
+extern void drv_proc_node_update_status(void *node_resource, s32 status);
 
 extern int drv_proc_update_strm_res(u32 num_bufs, void *strm_res);
 
