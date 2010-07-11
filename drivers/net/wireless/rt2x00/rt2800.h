fa85a59775bc301cc33e7ad0017d0d12e67f1027
Helmut Schaa <helmut.schaa@googlemail.com>
2010-07-11 12:28:23 +0200

    rt2x00: Use pretbtt irq for fetching beacons on rt2800pci
    Updating the beacon on pre tbtt instead of beacondone allows much lower
    latency in regard to TIM updates. Hence, use the pre tbtt interrupt for
    updating the beacon in rt2800pci (older devices don't provide a pre tbtt
    interrupt).
    
    Also, add a new driver flag to indicate if a driver has pre tbtt support
    or not and implement the according behavior in rt2x00lib.
    
    Signed-off-by: Helmut Schaa <helmut.schaa@googlemail.com>
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2800.h
+++ drivers/net/wireless/rt2x00/rt2800.h
@@ -720,14 +720,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TBTT_TIMER			0x1124
 
 /*
- * INT_TIMER_CFG:
+ * INT_TIMER_CFG: timer configuration
+ * PRE_TBTT_TIMER: leadtime to tbtt for pretbtt interrupt in units of 1/16 TU
+ * GP_TIMER: period of general purpose timer in units of 1/16 TU
  */
 #define INT_TIMER_CFG			0x1128
+#define INT_TIMER_CFG_PRE_TBTT_TIMER	FIELD32(0x0000ffff)
+#define INT_TIMER_CFG_GP_TIMER		FIELD32(0xffff0000)
 
 /*
  * INT_TIMER_EN: GP-timer and pre-tbtt Int enable
  */
 #define INT_TIMER_EN			0x112c
+#define INT_TIMER_EN_PRE_TBTT_TIMER	FIELD32(0x00000001)
+#define INT_TIMER_EN_GP_TIMER		FIELD32(0x00000002)
 
 /*
  * CH_IDLE_STA: channel idle time
