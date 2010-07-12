324b3b0bb99b2ebf2871ec55f00cc8263dc15e1d
Andres More <more.andres@gmail.com>
2010-07-12 13:55:43 -0300

    staging: vt6656: removed Adhoc_STA definition
    Removed an always defined macro, perhaps used to patch the driver
    
    Signed-off-by: Andres More <more.andres@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6656/ttype.h
+++ drivers/staging/vt6656/ttype.h
@@ -32,11 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /******* Common definitions and typedefs ***********************************/
 
-//2008-0131-02<Add>by MikeLiu
-#ifndef Adhoc_STA
-#define Adhoc_STA
-#endif
-
 typedef int             BOOL;
 
 #if !defined(TRUE)
