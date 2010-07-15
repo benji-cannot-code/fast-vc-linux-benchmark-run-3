16d12da46416829a1a02062c35aba375413d80d2
Florian Schilhabel <florian.c.schilhabel@googlemail.com>
2010-07-15 19:02:31 +0200

    staging: rtl8192su: remove a section mismatch warning
    Signed-off-by: Florian Schilhabel <florian.c.schilhabel@googlemail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192su/ieee80211/ieee80211_crypt.c
+++ drivers/staging/rtl8192su/ieee80211/ieee80211_crypt.c
@@ -201,7 +201,7 @@ static struct ieee80211_crypto_ops ieee80211_crypt_null = {
 	.owner			= THIS_MODULE,
 };
 
-int __init ieee80211_crypto_init(void)
+int ieee80211_crypto_init(void)
 {
 	int ret = -ENOMEM;
 
@@ -221,7 +221,7 @@ out:
 	return ret;
 }
 
-void __exit ieee80211_crypto_deinit(void)
+void ieee80211_crypto_deinit(void)
 {
 	struct list_head *ptr, *n;
 	struct ieee80211_crypto_alg *alg = NULL;
