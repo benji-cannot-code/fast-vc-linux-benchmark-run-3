798a6da1ac5d5f456711c30e2e2c70ed173b4739
Jon Mason <jon.mason@exar.com>
2010-07-15 08:47:28 +0000

    vxge: Version update
    Version update
    
    Signed-off-by: Jon Mason <jon.mason@exar.com>
    Signed-off-by: Sreenivasa Honnur <sreenivasa.honnur@exar.com>
    Signed-off-by: Ramkrishna Vepa <ramkrishna.vepa@exar.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vxge/vxge-version.h
+++ drivers/net/vxge/vxge-version.h
@@ -13,12 +13,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright(c) 2002-2010 Exar Corp.
  ******************************************************************************/
 #ifndef VXGE_VERSION_H
-
 #define VXGE_VERSION_H
 
 #define VXGE_VERSION_MAJOR	"2"
 #define VXGE_VERSION_MINOR	"0"
-#define VXGE_VERSION_FIX	"8"
-#define VXGE_VERSION_BUILD	"20182"
+#define VXGE_VERSION_FIX	"9"
+#define VXGE_VERSION_BUILD	"20840"
 #define VXGE_VERSION_FOR	"k"
 #endif
