fa9c1728616c3f15f6f2015a7e5b281699d18e86
Jon Mason <jon.mason@exar.com>
2010-07-15 08:47:26 +0000

    vxge: Update copyright information
    Update copyright information to reflect the Exar purchase of Neterion
    
    Signed-off-by: Jon Mason <jon.mason@exar.com>
    Signed-off-by: Sreenivasa Honnur <sreenivasa.honnur@exar.com>
    Signed-off-by: Ramkrishna Vepa <ramkrishna.vepa@exar.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vxge/vxge-main.c
+++ drivers/net/vxge/vxge-main.c
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 * system is licensed under the GPL.
 * See the file COPYING in this distribution for more information.
 *
-* vxge-main.c: Driver for Neterion Inc's X3100 Series 10GbE PCIe I/O
+* vxge-main.c: Driver for Exar Corp's X3100 Series 10GbE PCIe I/O
 *              Virtualized Server Adapter.
-* Copyright(c) 2002-2009 Neterion Inc.
+* Copyright(c) 2002-2010 Exar Corp.
 *
 * The module loadable parameters that are supported by the driver and a brief
 * explanation of all the variables:
@@ -4434,7 +4434,7 @@ vxge_starter(void)
 	char version[32];
 	snprintf(version, 32, "%s", DRV_VERSION);
 
-	printk(KERN_INFO "%s: Copyright(c) 2002-2009 Neterion Inc\n",
+	printk(KERN_INFO "%s: Copyright(c) 2002-2010 Exar Corp.\n",
 		VXGE_DRIVER_NAME);
 	printk(KERN_INFO "%s: Driver version: %s\n",
 			VXGE_DRIVER_NAME, version);
