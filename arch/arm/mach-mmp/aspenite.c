cd974da25e8c8cb7d4e794d20102fbe53a12da1b
Eric Miao <eric.y.miao@gmail.com>
2010-07-15 17:50:13 +0800

    [ARM] mmp: rename pxa_map_io() to mmp_map_io()
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-mmp/aspenite.c
+++ arch/arm/mach-mmp/aspenite.c
@@ -152,7 +152,7 @@ static void __init common_init(void)
 MACHINE_START(ASPENITE, "PXA168-based Aspenite Development Platform")
 	.phys_io        = APB_PHYS_BASE,
 	.io_pg_offst    = (APB_VIRT_BASE >> 18) & 0xfffc,
-	.map_io		= pxa_map_io,
+	.map_io		= mmp_map_io,
 	.init_irq       = pxa168_init_irq,
 	.timer          = &pxa168_timer,
 	.init_machine   = common_init,
@@ -161,7 +161,7 @@ MACHINE_END
 MACHINE_START(ZYLONITE2, "PXA168-based Zylonite2 Development Platform")
 	.phys_io        = APB_PHYS_BASE,
 	.io_pg_offst    = (APB_VIRT_BASE >> 18) & 0xfffc,
-	.map_io		= pxa_map_io,
+	.map_io		= mmp_map_io,
 	.init_irq       = pxa168_init_irq,
 	.timer          = &pxa168_timer,
 	.init_machine   = common_init,
