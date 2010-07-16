0c35e074975c054e72e01397eb3da4c93879c0a0
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2010-07-16 19:51:06 +0900

    ASoC: fsi: Add specified ID for soc-audio
    Specified ID is necessary, when some codecs are used with FSI.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/sound/sh_fsi.h
+++ include/sound/sh_fsi.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
+#define FSI_PORT_A	0
+#define FSI_PORT_B	1
+
 /* flags format
 
  * 0xABCDEEFF
