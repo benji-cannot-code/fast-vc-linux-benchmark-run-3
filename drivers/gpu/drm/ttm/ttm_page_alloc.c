3583148f77bce24ba9b929da9243138c6cd88d01
Ben Hutchings <ben@decadent.org.uk>
2010-07-18 21:51:42 +0100

    drm/ttm: Fix build on architectures without AGP
    Make inclusion of <asm/agp.h> conditional on TTM_HAS_AGP.  The use
    of the functions declared in it is already conditional.
    
    Reported-by: Geert Stappers <stappers@stappers.nl>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Tested-by: Geert Stappers <stappers@stappers.nl>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/ttm/ttm_page_alloc.c
+++ drivers/gpu/drm/ttm/ttm_page_alloc.c
@@ -41,7 +41,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 
 #include <asm/atomic.h>
+#ifdef TTM_HAS_AGP
 #include <asm/agp.h>
+#endif
 
 #include "ttm/ttm_bo_driver.h"
 #include "ttm/ttm_page_alloc.h"
