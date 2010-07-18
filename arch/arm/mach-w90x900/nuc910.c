b9c871c7e0614b2d06b0e21e887fbbd08c51a760
wanzongshun <mcuos.com@gmail.com>
2010-07-18 15:15:11 +0100

    ARM: 6231/1: re-organize the nuc900 lcd arch platform data setting
    The patch re-organizes the nuc900 lcd arch platform data,
    remove the lcd platform data setting from nuc950.c to dev.c
    
    It can maximize the share codes between nuc900 series MCUs.
    
    Signed-off-by: Wan ZongShun <mcuos.com@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-w90x900/nuc910.c
+++ arch/arm/mach-w90x900/nuc910.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct platform_device *nuc910_dev[] __initdata = {
 	&nuc900_device_ts,
 	&nuc900_device_rtc,
+	&nuc900_device_lcd,
 };
 
 /* define specific CPU platform io map */
