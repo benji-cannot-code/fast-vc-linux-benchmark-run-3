ddd69adbaf4658fbdd1aee729f7dbcfe2cd64ff5
Randy Dunlap <randy.dunlap@oracle.com>
2010-07-19 16:49:17 -0700

    writeback.h: needs linux/device.h
    include/trace/events/writeback.h uses dev_name(), so it needs to
    include linux/device.h.
    
    include/trace/events/writeback.h:12: error: implicit declaration of function 'dev_name'
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- include/trace/events/writeback.h
+++ include/trace/events/writeback.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _TRACE_WRITEBACK_H
 
 #include <linux/backing-dev.h>
+#include <linux/device.h>
 #include <linux/writeback.h>
 
 struct wb_writeback_work;
