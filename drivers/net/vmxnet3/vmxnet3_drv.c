c759cb6ef664d5e7ffe33907563af4189db0e31d
Shreyas Bhatewara <sbhatewara@vmware.com>
2010-07-19 07:02:13 +0000

    net-next: vmxnet3 fixes [4/5] Do not reset when the device is not opened
    Hold rtnl_lock to get the right link state.
    
    While asynchronously resetting the device, hold rtnl_lock to get the
    right value from netif_running. If a reset is scheduled, and the device
    goes thru close and open, it may happen that reset and open may run in
    parallel. Holding rtnl_lock will avoid this.
    
    Signed-off-by: Shreyas Bhatewara <sbhatewara@vmware.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vmxnet3/vmxnet3_drv.c
+++ drivers/net/vmxnet3/vmxnet3_drv.c
@@ -2366,6 +2366,7 @@ vmxnet3_reset_work(struct work_struct *data)
 		return;
 
 	/* if the device is closed, we must leave it alone */
+	rtnl_lock();
 	if (netif_running(adapter->netdev)) {
 		printk(KERN_INFO "%s: resetting\n", adapter->netdev->name);
 		vmxnet3_quiesce_dev(adapter);
@@ -2374,6 +2375,7 @@ vmxnet3_reset_work(struct work_struct *data)
 	} else {
 		printk(KERN_INFO "%s: already closed\n", adapter->netdev->name);
 	}
+	rtnl_unlock();
 
 	clear_bit(VMXNET3_STATE_BIT_RESETTING, &adapter->state);
 }
