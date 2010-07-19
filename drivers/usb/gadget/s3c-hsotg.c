ab748a7c1cd4d9f7997ad1416de00478c39ea181
Marek Szyprowski <m.szyprowski@samsung.com>
2010-07-19 16:01:40 +0200

    USB: s3c-hsotg: fix compilation problem
    drivers/usb/gadget/s3c-hsotg.c: In function ‘s3c_hsotg_otgreset’:
    drivers/usb/gadget/s3c-hsotg.c:2816: error: ‘MHZ’ undeclared (first use in this function)
    drivers/usb/gadget/s3c-hsotg.c:2816: error: (Each undeclared identifier is reported only once
    drivers/usb/gadget/s3c-hsotg.c:2816: error: for each function it appears in.)
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/s3c-hsotg.c
+++ drivers/usb/gadget/s3c-hsotg.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/regs-usb-hsotg.h>
 #include <mach/regs-sys.h>
 #include <plat/udc-hs.h>
+#include <plat/cpu.h>
 
 #define DMA_ADDR_INVALID (~((dma_addr_t)0))
 
