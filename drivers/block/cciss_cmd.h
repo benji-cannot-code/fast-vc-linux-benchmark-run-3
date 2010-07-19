80e2ff25dfbf0bbe37caf58004f9eff19fcf6675
Stephen M. Cameron <scameron@beardog.cce.hp.com>
2010-07-19 13:46:22 -0500

    cciss: fix hard reset code.
    cciss: Fix hard reset code.
    Smart Array controllers newer than the P600 do not honor the
    PCI power state method of resetting the controllers.  Instead,
    in these cases we can get them to reset via the "doorbell" register.
    
    This escaped notice until we began using "performant" mode because
    the fact that the controllers did not reset did not normally
    impede subsequent operation, and so things generally appeared to
    "work".  Once the performant mode code was added, if the controller
    does not reset, it remains in performant mode.  The code immediately
    after the reset presumes the controller is in "simple" mode
    (which previously, it had remained in simple mode the whole time).
    If the controller remains in performant mode any code which presumes
    it is in simple mode will not work.  So the reset needs to be fixed.
    
    Unfortunately there are some controllers which cannot be reset by
    either method. (eg. p800).  We detect these cases by noticing that
    the controller seems to remain in performant mode even after a
    reset has been attempted.  In those cases we ignore the controller,
    as any commands outstanding on it will result in stale completions.
    To sum up, we try to do a better job of resetting the controller if
    "reset_devices" is set, and if it doesn't work, we ignore that
    controller.
    
    Signed-off-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- drivers/block/cciss_cmd.h
+++ drivers/block/cciss_cmd.h
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Configuration Table */
 #define CFGTBL_ChangeReq        0x00000001l
 #define CFGTBL_AccCmds          0x00000001l
+#define DOORBELL_CTLR_RESET     0x00000004l
 
 #define CFGTBL_Trans_Simple     0x00000002l
 #define CFGTBL_Trans_Performant 0x00000004l
@@ -231,6 +232,9 @@ typedef struct _CfgTable_struct {
   DWORD            MaxPhysicalDrives;
   DWORD            MaxPhysicalDrivesPerLogicalUnit;
   DWORD            MaxPerformantModeCommands;
+  u8		   reserved[0x78 - 0x58];
+  u32		   misc_fw_support; /* offset 0x78 */
+#define MISC_FW_DOORBELL_RESET (0x02)
 } CfgTable_struct;
 
 struct TransTable_struct {
