b52af859494d4a75467059b81b99905289096c15
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2010-07-20 10:08:56 -0300

    V4L/DVB: rj54n1cb0c: fix a comment in the driver
    RJ54N1CB0C is a Sharp camera sensor.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/rj54n1cb0c.c
+++ drivers/media/video/rj54n1cb0c.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Driver for RJ54N1CB0C CMOS Image Sensor from Micron
+ * Driver for RJ54N1CB0C CMOS Image Sensor from Sharp
  *
  * Copyright (C) 2009, Guennadi Liakhovetski <g.liakhovetski@gmx.de>
  *
