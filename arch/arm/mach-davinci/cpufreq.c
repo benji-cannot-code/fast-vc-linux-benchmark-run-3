555c6c354334463052e89ffc017af1e9462b04e1
Sekhar Nori <nsekhar@ti.com>
2010-07-20 16:46:50 +0530

    davinci: cpufreq: add support for keeping an additional clock constant
    On OMAP-L138 SoC, some of the sysclks need not be at a fixed ratio
    to CPU clock and can be kept at a relatively constant rate by
    adjusting the PLLDIVn ratio even as cpufreq goes ahead and changes
    the CPU clock.
    
    This feature can be used to keep the EMIFA (PLL0 SYSCLK3) clock at a
    constant rate so that the EMIF timings need not be re-programmed
    whenever the CPU frequency changes.
    
    This patch adds the required suppport to cpufreq driver.
    
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/cpufreq.c
+++ arch/arm/mach-davinci/cpufreq.c
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct davinci_cpufreq {
 	struct device *dev;
 	struct clk *armclk;
+	struct clk *asyncclk;
+	unsigned long asyncrate;
 };
 static struct davinci_cpufreq cpufreq;
 
@@ -115,6 +117,12 @@ static int davinci_target(struct cpufreq_policy *policy,
 	if (ret)
 		goto out;
 
+	if (cpufreq.asyncclk) {
+		ret = clk_set_rate(cpufreq.asyncclk, cpufreq.asyncrate);
+		if (ret)
+			goto out;
+	}
+
 	/* if moving to lower freq, lower the voltage after lowering freq */
 	if (pdata->set_voltage && freqs.new < freqs.old)
 		pdata->set_voltage(idx);
@@ -192,6 +200,7 @@ static struct cpufreq_driver davinci_driver = {
 static int __init davinci_cpufreq_probe(struct platform_device *pdev)
 {
 	struct davinci_cpufreq_config *pdata = pdev->dev.platform_data;
+	struct clk *asyncclk;
 
 	if (!pdata)
 		return -EINVAL;
@@ -206,12 +215,21 @@ static int __init davinci_cpufreq_probe(struct platform_device *pdev)
 		return PTR_ERR(cpufreq.armclk);
 	}
 
+	asyncclk = clk_get(cpufreq.dev, "async");
+	if (!IS_ERR(asyncclk)) {
+		cpufreq.asyncclk = asyncclk;
+		cpufreq.asyncrate = clk_get_rate(asyncclk);
+	}
+
 	return cpufreq_register_driver(&davinci_driver);
 }
 
 static int __exit davinci_cpufreq_remove(struct platform_device *pdev)
 {
 	clk_put(cpufreq.armclk);
+
+	if (cpufreq.asyncclk)
+		clk_put(cpufreq.asyncclk);
 
 	return cpufreq_unregister_driver(&davinci_driver);
 }
