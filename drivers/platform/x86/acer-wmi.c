a8010e92558b3b556f3926999c99a7c816c2b525
Axel Lin <axel.lin@gmail.com>
2010-07-20 15:19:52 -0700

    acer-wmi: fix memory leaks in WMID_set_capabilities and get_wmid_devices
    When acpi_evaluate_object() is passed ACPI_ALLOCATE_BUFFER, the caller
    must kfree the returned buffer if AE_OK is returned.
    
    The callers of wmi_query_block() pass ACPI_ALLOCATE_BUFFER, and thus must
    check its return value before accessing or kfree() on the buffer.
    
    This patch adds a missing kfree(out.pointer) before exit
    WMID_set_capabilities() and get_wmid_devices().
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Acked-by: Carlos Corbacho <carlos@strangeworlds.co.uk>
    Cc: Matthew Garrett <mjg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>

--- drivers/platform/x86/acer-wmi.c
+++ drivers/platform/x86/acer-wmi.c
@@ -773,6 +773,7 @@ static acpi_status WMID_set_capabilities(void)
 		obj->buffer.length == sizeof(u32)) {
 		devices = *((u32 *) obj->buffer.pointer);
 	} else {
+		kfree(out.pointer);
 		return AE_ERROR;
 	}
 
@@ -789,6 +790,7 @@ static acpi_status WMID_set_capabilities(void)
 	if (!(devices & 0x20))
 		max_brightness = 0x9;
 
+	kfree(out.pointer);
 	return status;
 }
 
@@ -1095,6 +1097,7 @@ static u32 get_wmid_devices(void)
 	struct acpi_buffer out = {ACPI_ALLOCATE_BUFFER, NULL};
 	union acpi_object *obj;
 	acpi_status status;
+	u32 devices = 0;
 
 	status = wmi_query_block(WMID_GUID2, 1, &out);
 	if (ACPI_FAILURE(status))
@@ -1103,10 +1106,11 @@ static u32 get_wmid_devices(void)
 	obj = (union acpi_object *) out.pointer;
 	if (obj && obj->type == ACPI_TYPE_BUFFER &&
 		obj->buffer.length == sizeof(u32)) {
-		return *((u32 *) obj->buffer.pointer);
-	} else {
-		return 0;
+		devices = *((u32 *) obj->buffer.pointer);
 	}
+
+	kfree(out.pointer);
+	return devices;
 }
 
 /*
