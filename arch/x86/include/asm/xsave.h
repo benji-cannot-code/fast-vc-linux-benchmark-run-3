19fe0a5a69b31f6b95a9582e06e5bcf8f1ac201a
Robert Richter <robert.richter@amd.com>
2010-07-20 20:50:48 +0200

    x86, xsave: Do not include asm/i387.h in asm/xsave.h
    There are no dependencies to asm/i387.h. Instead, if including only
    xsave.h the following error occurs:
    
     .../arch/x86/include/asm/i387.h:110: error: ‘XSTATE_FP’ undeclared (first use in this function)
     .../arch/x86/include/asm/i387.h:110: error: (Each undeclared identifier is reported only once
     .../arch/x86/include/asm/i387.h:110: error: for each function it appears in.)
    
    This patch fixes this.
    
    Signed-off-by: Robert Richter <robert.richter@amd.com>
    LKML-Reference: <1279651857-24639-2-git-send-email-robert.richter@amd.com>
    Acked-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/xsave.h
+++ arch/x86/include/asm/xsave.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <asm/processor.h>
-#include <asm/i387.h>
 
 #define XSTATE_FP	0x1
 #define XSTATE_SSE	0x2
