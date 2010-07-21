87db25c0ae0c2602f6ac0ce7a05ebc37a515b5f9
Joe Perches <joe@perches.com>
2010-07-21 14:44:18 -0700

    drivers/net/qlge: Use pr_<level>, shrink text a bit
    Add and use a few neatening macros
    Remove PFX
    Add #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
    Convert printk(KERN_ERR to pr_err(
    
    $ size drivers/net/qlge/built-in.o.*
      text	   data	    bss	    dec	    hex	filename
     116456	   2312	  25712	 144480	  23460	drivers/net/qlge/built-in.o.old
     114909	   2312	  25728	 142949	  22e65	drivers/net/qlge/built-in.o.new
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlge/qlge.h
+++ drivers/net/qlge/qlge.h
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRV_STRING 	"QLogic 10 Gigabit PCI-E Ethernet Driver "
 #define DRV_VERSION	"v1.00.00.25.00.00-01"
 
-#define PFX "qlge: "
-
 #define WQ_ADDR_ALIGN	0x3	/* 4 byte alignment */
 
 #define QLGE_VENDOR_ID    0x1077
