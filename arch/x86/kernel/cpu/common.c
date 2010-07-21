98519436b7a9269950ced4da824c4c9a99ca8e41
Robert Richter <robert.richter@amd.com>
2010-07-21 19:03:52 +0200

    x86, xsave: Separate fpu and xsave initialization
    As xsave also supports other than fpu features, it should be
    initialized independently of the fpu. This patch moves this out of fpu
    initialization.
    
    There is also a lot of cross referencing between fpu and xsave
    code. This patch reduces this by making xsave_cntxt_init() and
    init_thread_xstate() static functions.
    
    The patch moves the cpu_has_xsave check at the beginning of
    xsave_init(). All other checks may removed then.
    
    Signed-off-by: Robert Richter <robert.richter@amd.com>
    LKML-Reference: <1279731838-1522-2-git-send-email-robert.richter@amd.com>
    Acked-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1211,6 +1211,7 @@ void __cpuinit cpu_init(void)
 	dbg_restore_debug_regs();
 
 	fpu_init();
+	xsave_init();
 
 	raw_local_save_flags(kernel_eflags);
 
@@ -1271,6 +1272,7 @@ void __cpuinit cpu_init(void)
 	clear_used_math();
 	mxcsr_feature_mask_init();
 
+	fpu_init();
 	xsave_init();
 }
 #endif
