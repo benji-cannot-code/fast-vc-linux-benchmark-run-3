ba803fe0c865324babaeee6bbbae86b3eb01469a
Menon, Nishanth <nm@ti.com>
2010-07-22 16:03:45 -0500

    staging: tidspbridge: remove GlobalTypes.h
    Remove custom globaltypes.h header
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/hw/MMURegAcM.h
+++ drivers/staging/tidspbridge/hw/MMURegAcM.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MMU_REG_ACM_H
 #define _MMU_REG_ACM_H
 
-#include <GlobalTypes.h>
 #include <linux/io.h>
 #include <EasiGlobal.h>
 
--- drivers/staging/tidspbridge/hw/hw_defs.h
+++ drivers/staging/tidspbridge/hw/hw_defs.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _HW_DEFS_H
 #define _HW_DEFS_H
 
-#include <GlobalTypes.h>
-
 /* Page size */
 #define HW_PAGE_SIZE4KB   0x1000
 #define HW_PAGE_SIZE64KB  0x10000
