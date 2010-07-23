600064c38e1322659f647cfc96c919fe11fb4fb7
Armando Uribe <x0095078@ti.com>
2010-07-22 20:25:30 -0500

    staging: tidspbridge: Remove redundant macro from cod.c
    This patch removes a redundant macro from cod.c simply used
    to verify that a field was not NULL
    
    Signed-off-by: Armando Uribe <x0095078@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/pmgr/cod.c
+++ drivers/staging/tidspbridge/pmgr/cod.c
@@ -47,9 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* magic number for handle validation */
 #define MAGIC	 0xc001beef
 
-/* macro to validate COD manager handles */
-#define IS_VALID(h)    ((h) != NULL && (h)->ul_magic == MAGIC)
-
 /*
  *  ======== cod_manager ========
  */
@@ -200,7 +197,7 @@ void cod_close(struct cod_libraryobj *lib)
 
 	DBC_REQUIRE(refs > 0);
 	DBC_REQUIRE(lib != NULL);
-	DBC_REQUIRE(IS_VALID(((struct cod_libraryobj *)lib)->cod_mgr));
+	DBC_REQUIRE(lib->cod_mgr);
 
 	hmgr = lib->cod_mgr;
 	hmgr->fxns.close_fxn(lib->dbll_lib);
@@ -286,7 +283,7 @@ int cod_create(struct cod_manager **mgr, char *str_zl_file,
 void cod_delete(struct cod_manager *cod_mgr_obj)
 {
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(cod_mgr_obj));
+	DBC_REQUIRE(cod_mgr_obj);
 
 	if (cod_mgr_obj->base_lib) {
 		if (cod_mgr_obj->loaded)
@@ -329,7 +326,7 @@ int cod_get_base_lib(struct cod_manager *cod_mgr_obj,
 	int status = 0;
 
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(cod_mgr_obj));
+	DBC_REQUIRE(cod_mgr_obj);
 	DBC_REQUIRE(plib != NULL);
 
 	*plib = (struct dbll_library_obj *)cod_mgr_obj->base_lib;
@@ -346,7 +343,7 @@ int cod_get_base_name(struct cod_manager *cod_mgr_obj, char *sz_name,
 	int status = 0;
 
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(cod_mgr_obj));
+	DBC_REQUIRE(cod_mgr_obj);
 	DBC_REQUIRE(sz_name != NULL);
 
 	if (usize <= COD_MAXPATHLENGTH)
@@ -366,7 +363,7 @@ int cod_get_base_name(struct cod_manager *cod_mgr_obj, char *sz_name,
 int cod_get_entry(struct cod_manager *cod_mgr_obj, u32 *entry_pt)
 {
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(cod_mgr_obj));
+	DBC_REQUIRE(cod_mgr_obj);
 	DBC_REQUIRE(entry_pt != NULL);
 
 	*entry_pt = cod_mgr_obj->ul_entry;
@@ -385,7 +382,7 @@ int cod_get_loader(struct cod_manager *cod_mgr_obj,
 	int status = 0;
 
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(cod_mgr_obj));
+	DBC_REQUIRE(cod_mgr_obj);
 	DBC_REQUIRE(loader != NULL);
 
 	*loader = (struct dbll_tar_obj *)cod_mgr_obj->target;
@@ -407,7 +404,7 @@ int cod_get_section(struct cod_libraryobj *lib, char *str_sect,
 
 	DBC_REQUIRE(refs > 0);
 	DBC_REQUIRE(lib != NULL);
-	DBC_REQUIRE(IS_VALID(lib->cod_mgr));
+	DBC_REQUIRE(lib->cod_mgr);
 	DBC_REQUIRE(str_sect != NULL);
 	DBC_REQUIRE(addr != NULL);
 	DBC_REQUIRE(len != NULL);
@@ -441,7 +438,7 @@ int cod_get_sym_value(struct cod_manager *cod_mgr_obj, char *str_sym,
 	struct dbll_sym_val *dbll_sym;
 
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(cod_mgr_obj));
+	DBC_REQUIRE(cod_mgr_obj);
 	DBC_REQUIRE(str_sym != NULL);
 	DBC_REQUIRE(pul_value != NULL);
 
@@ -506,7 +503,7 @@ int cod_load_base(struct cod_manager *cod_mgr_obj, u32 num_argc, char *args[],
 	u32 i;
 
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(cod_mgr_obj));
+	DBC_REQUIRE(cod_mgr_obj);
 	DBC_REQUIRE(num_argc > 0);
 	DBC_REQUIRE(args != NULL);
 	DBC_REQUIRE(args[0] != NULL);
@@ -562,7 +559,7 @@ int cod_open(struct cod_manager *hmgr, char *sz_coff_path,
 	struct cod_libraryobj *lib = NULL;
 
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(hmgr));
+	DBC_REQUIRE(hmgr);
 	DBC_REQUIRE(sz_coff_path != NULL);
 	DBC_REQUIRE(flags == COD_NOLOAD || flags == COD_SYMB);
 	DBC_REQUIRE(lib_obj != NULL);
@@ -599,7 +596,7 @@ int cod_open_base(struct cod_manager *hmgr, char *sz_coff_path,
 	struct dbll_library_obj *lib;
 
 	DBC_REQUIRE(refs > 0);
-	DBC_REQUIRE(IS_VALID(hmgr));
+	DBC_REQUIRE(hmgr);
 	DBC_REQUIRE(sz_coff_path != NULL);
 
 	/* if we previously opened a base image, close it now */
@@ -637,7 +634,7 @@ int cod_read_section(struct cod_libraryobj *lib, char *str_sect,
 
 	DBC_REQUIRE(refs > 0);
 	DBC_REQUIRE(lib != NULL);
-	DBC_REQUIRE(IS_VALID(lib->cod_mgr));
+	DBC_REQUIRE(lib->cod_mgr);
 	DBC_REQUIRE(str_sect != NULL);
 	DBC_REQUIRE(str_content != NULL);
 
