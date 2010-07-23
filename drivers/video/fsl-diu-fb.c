54d2826a8212621d5a36d0cfc420c9dbe2bfff02
Anatolij Gustschin <agust@denx.de>
2010-07-23 04:00:36 +0000

    powerpc/5121: move fsl-diu-fb.h to include/linux
    Some DIU structures will be used in platform code in
    subsequent MPC5121 DIU patch, so we move this header
    to be able to include it elsewhere.
    
    Signed-off-by: Anatolij Gustschin <agust@denx.de>
    Acked-by: Timur Tabi <timur@freescale.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- drivers/video/fsl-diu-fb.c
+++ drivers/video/fsl-diu-fb.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_platform.h>
 
 #include <sysdev/fsl_soc.h>
-#include "fsl-diu-fb.h"
+#include <linux/fsl-diu-fb.h>
 
 /*
  * These parameters give default parameters
