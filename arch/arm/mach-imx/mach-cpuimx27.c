7232d796d1a736ab8adb552229530eed81eca8af
Eric Bénard <eric@eukrea.com>
2010-07-23 14:56:40 +0200

    eukrea-baseboards: merge includes in one file
    as per suggested by Sascha
    
    Signed-off-by: Eric Bénard <eric@eukrea.com>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/mach-cpuimx27.c
+++ arch/arm/mach-imx/mach-cpuimx27.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/time.h>
 #include <asm/mach/map.h>
 
-#include <mach/board-eukrea_cpuimx27.h>
+#include <mach/eukrea-baseboards.h>
 #include <mach/common.h>
 #include <mach/hardware.h>
 #include <mach/iomux-mx27.h>
--- arch/arm/mach-mx3/mach-cpuimx35.c
+++ arch/arm/mach-mx3/mach-cpuimx35.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/time.h>
 #include <asm/mach/map.h>
 
-#include <mach/board-eukrea_cpuimx35.h>
+#include <mach/eukrea-baseboards.h>
 #include <mach/hardware.h>
 #include <mach/common.h>
 #include <mach/iomux-mx35.h>
