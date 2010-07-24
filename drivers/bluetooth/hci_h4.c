4c8e6c90f2643d40dc4d16c6ffd5cda308d4cdce
Gustavo F. Padovan <padovan@profusion.mobi>
2010-07-24 02:04:44 -0300

    Bluetooth: Add __init and __exit marks to UART drivers
    Those marks are useful to save space in the binary and in the memory.
    
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_h4.c
+++ drivers/bluetooth/hci_h4.c
@@ -174,7 +174,7 @@ static struct hci_uart_proto h4p = {
 	.flush		= h4_flush,
 };
 
-int h4_init(void)
+int __init h4_init(void)
 {
 	int err = hci_uart_register_proto(&h4p);
 
@@ -186,7 +186,7 @@ int h4_init(void)
 	return err;
 }
 
-int h4_deinit(void)
+int __exit h4_deinit(void)
 {
 	return hci_uart_unregister_proto(&h4p);
 }
--- drivers/bluetooth/hci_ll.c
+++ drivers/bluetooth/hci_ll.c
@@ -518,7 +518,7 @@ static struct hci_uart_proto llp = {
 	.flush		= ll_flush,
 };
 
-int ll_init(void)
+int __init ll_init(void)
 {
 	int err = hci_uart_register_proto(&llp);
 
@@ -530,7 +530,7 @@ int ll_init(void)
 	return err;
 }
 
-int ll_deinit(void)
+int __exit ll_deinit(void)
 {
 	return hci_uart_unregister_proto(&llp);
 }
