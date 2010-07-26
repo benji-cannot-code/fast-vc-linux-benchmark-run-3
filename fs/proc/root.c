a99185b7e5d15f10b1ecad78a51d4c172077ab30
Al Viro <viro@zeniv.linux.org.uk>
2010-07-26 13:09:36 +0400

    setting ->proc_mnt doesn't belong in proc_get_sb()
    take that to kern_mount_data()-using callers
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/proc/root.c
+++ fs/proc/root.c
@@ -80,7 +80,6 @@ static int proc_get_sb(struct file_system_type *fs_type,
 		}
 
 		sb->s_flags |= MS_ACTIVE;
-		ns->proc_mnt = mnt;
 	}
 
 	simple_set_mnt(mnt, sb);
@@ -116,6 +115,7 @@ void __init proc_root_init(void)
 		return;
 	}
 
+	init_pid_ns.proc_mnt = proc_mnt;
 	proc_symlink("mounts", NULL, "self/mounts");
 
 	proc_net_init();
@@ -214,6 +214,7 @@ int pid_ns_prepare_proc(struct pid_namespace *ns)
 	if (IS_ERR(mnt))
 		return PTR_ERR(mnt);
 
+	ns->proc_mnt = mnt;
 	return 0;
 }
 
