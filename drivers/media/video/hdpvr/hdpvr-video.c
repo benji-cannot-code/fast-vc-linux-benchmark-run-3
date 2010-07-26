7e8b000bda54223ae1d7bd359fefbf8e58cda8ab
Alan Young <ayoung@teleport.com>
2010-07-26 08:50:32 -0300

    [media] hdpvr: decrease URB timeout to 90ms
    Based on USB traces of the windows driver.
    
    Signed-off-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/hdpvr/hdpvr-video.c
+++ drivers/media/video/hdpvr/hdpvr-video.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-ioctl.h>
 #include "hdpvr.h"
 
-#define BULK_URB_TIMEOUT 1250 /* 1.25 seconds */
+#define BULK_URB_TIMEOUT   90 /* 0.09 seconds */
 
 #define print_buffer_status() { \
 		v4l2_dbg(MSG_BUFFER, hdpvr_debug, &dev->v4l2_dev,	\
