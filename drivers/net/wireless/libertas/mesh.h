4686ca6e0f3c1131e38655cc19053d02ac411d25
Dan Williams <dcbw@redhat.com>
2010-07-27 13:03:46 -0700

    libertas: convert CMD_FWT_ACCESS to a direct command
    Slightly different approach here since there are so many arguments to
    the firmware command.  Just let the caller fill them in before pushing
    the command to the firmware.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/mesh.h
+++ drivers/net/wireless/libertas/mesh.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/iw_handler.h>
 #include <net/lib80211.h>
 
+#include "host.h"
 
 #ifdef CONFIG_LIBERTAS_MESH
 
@@ -58,8 +59,9 @@ int lbs_mesh_bt_get_inverted(struct lbs_private *priv, bool *inverted);
 int lbs_mesh_bt_set_inverted(struct lbs_private *priv, bool inverted);
 int lbs_mesh_bt_get_entry(struct lbs_private *priv, u32 id, u8 *addr1);
 
-int lbs_cmd_fwt_access(struct cmd_ds_command *cmd,
-	u16 cmd_action, void *pdata_buf);
+int lbs_cmd_fwt_access(struct lbs_private *priv, u16 cmd_action,
+			struct cmd_ds_fwt_access *cmd);
+
 int lbs_mesh_access(struct lbs_private *priv, uint16_t cmd_action,
 		    struct cmd_ds_mesh_access *cmd);
 int lbs_mesh_config_send(struct lbs_private *priv,
