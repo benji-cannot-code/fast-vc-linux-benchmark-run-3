5d2b9a137a2d73af2ff70fd3ece12353e37aceb3
Dmitry Kravkov <dmitry@broadcom.com>
2010-07-27 12:31:10 +0000

    bnx2x: Create separate folder for bnx2x driver
    This commit includes files movement to newly created folder
    using git-mv command and fixes references in cnic and bnx2x code
    to each other.
    
    files moved using following:
    #!/bin/bash
    mkdir drivers/net/bnx2x/
    list=$(cd drivers/net/ && ls bnx2x*.[ch])
    for f in $list; do
            git mv -f drivers/net/$f drivers/net/bnx2x/$f
    done
    
    Signed-off-by: Dmitry Kravkov <dmitry@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/cnic.c
+++ drivers/net/cnic.c
@@ -41,9 +41,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "cnic_if.h"
 #include "bnx2.h"
-#include "bnx2x_reg.h"
-#include "bnx2x_fw_defs.h"
-#include "bnx2x_hsi.h"
+#include "bnx2x/bnx2x_reg.h"
+#include "bnx2x/bnx2x_fw_defs.h"
+#include "bnx2x/bnx2x_hsi.h"
 #include "../scsi/bnx2i/57xx_iscsi_constants.h"
 #include "../scsi/bnx2i/57xx_iscsi_hsi.h"
 #include "cnic.h"
