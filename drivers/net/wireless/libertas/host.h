b0ddba702deb9fec9570b04358b7468c53bbb53e
Dan Williams <dcbw@redhat.com>
2010-07-27 12:58:08 -0700

    libertas: remove Beacon Control
    For now; it's a pretty easy command to hook up and whenever OLPC
    figures out how they want the userspace interface to look (ie,
    not iwpriv commands) we can easily add it back in.  Since the
    cfg80211 conversion it wasn't working anyway.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/host.h
+++ drivers/net/wireless/libertas/host.h
@@ -596,6 +596,8 @@ struct cmd_ds_802_11_radio_control {
 } __packed;
 
 struct cmd_ds_802_11_beacon_control {
+	struct cmd_header hdr;
+
 	__le16 action;
 	__le16 beacon_enable;
 	__le16 beacon_period;
@@ -973,7 +975,6 @@ struct cmd_ds_command {
 
 		struct cmd_ds_bt_access bt;
 		struct cmd_ds_fwt_access fwt;
-		struct cmd_ds_802_11_beacon_control bcn_ctrl;
 	} params;
 } __packed;
 #endif
