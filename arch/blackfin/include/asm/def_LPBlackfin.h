edc2a756c4fc25ab9a57f9a6a5d5947db5827f4e
Mike Frysinger <vapier@gentoo.org>
2010-07-28 19:50:47 +0000

    Blackfin: add EVT_OVERRIDE/IPRIO core MMR helpers
    These were partially defined, so fill out the def/cdef pieces properly.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/include/asm/def_LPBlackfin.h
+++ arch/blackfin/include/asm/def_LPBlackfin.h
@@ -395,6 +395,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EVT13              0xFFE02034	/* Event Vector 13 ESR Address */
 #define EVT14              0xFFE02038	/* Event Vector 14 ESR Address */
 #define EVT15              0xFFE0203C	/* Event Vector 15 ESR Address */
+#define EVT_OVERRIDE       0xFFE02100	/* Event Vector Override Register */
 #define IMASK              0xFFE02104	/* Interrupt Mask Register */
 #define IPEND              0xFFE02108	/* Interrupt Pending Register */
 #define ILAT               0xFFE0210C	/* Interrupt Latch Register */
