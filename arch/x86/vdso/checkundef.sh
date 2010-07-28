b17202d7c1415c36429c02d0bd5b6f6ebbe0d1ba
H. Peter Anvin <hpa@zytor.com>
2010-07-27 23:52:29 -0700

    x86, vdso: Don't quote $nm in the script for checking vdso references
    Don't quote $nm in the script for checking the vdso for external
    references.  Doing so breaks multiword constructs, like using
    CROSS_COMPILE='ccache '.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    LKML-Reference: <20100728134252.2e4c27cf.sfr@canb.auug.org.au>

--- arch/x86/vdso/checkundef.sh
+++ arch/x86/vdso/checkundef.sh
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #!/bin/sh
 nm="$1"
 file="$2"
-"$nm" "$file" | grep '^ *U' > /dev/null 2>&1
+$nm "$file" | grep '^ *U' > /dev/null 2>&1
 if [ $? -eq 1 ]; then
     exit 0
 else
