192551034e927cfd609b9e920a4b93dd33d0e4a8
Yinghai Lu <yinghai@kernel.org>
2010-07-28 15:20:58 +1000

    memblock: Export MEMBLOCK_ERROR
    will used by x86 memblock_x86_find_in_range_node and nobootmem replacement
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- include/linux/memblock.h
+++ include/linux/memblock.h
@@ -19,7 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/memblock.h>
 
-#define INIT_MEMBLOCK_REGIONS 128
+#define INIT_MEMBLOCK_REGIONS	128
+#define MEMBLOCK_ERROR		(~(phys_addr_t)0)
 
 struct memblock_region {
 	phys_addr_t base;
