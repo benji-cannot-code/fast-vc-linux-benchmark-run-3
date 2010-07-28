889b30748b90139cb3457b5970a23313e6b9c168
Ernesto Ramos <ernesto@ti.com>
2010-07-28 09:45:33 -0500

    staging:ti dspbridge: remove DSP_FAILED macro definition
    Since status succeeded is now 0 macro DSP_FAILED
    is not necessary anymore. This patch removes this
    define.
    
    Signed-off-by: Ernesto Ramos <ernesto@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/include/dspbridge/dbdefs.h
+++ drivers/staging/tidspbridge/include/dspbridge/dbdefs.h
@@ -92,9 +92,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DSPWORD       unsigned char
 #define DSPWORDSIZE     sizeof(DSPWORD)
 
-/* Failure macro */
-#define DSP_FAILED(status)         unlikely((s32)(status) < 0)
-
 /* Power control enumerations */
 #define PROC_PWRCONTROL             0x8070
 
