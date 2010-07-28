6b12381e7defebc9435eac685d0e77e3d07456f8
Jerome Marchand <jmarchan@redhat.com>
2010-07-28 10:18:37 -0400

    inotify: Fix mask checks
    The mask checks in inotify_update_existing_watch() and
    inotify_new_watch() are useless because inotify_arg_to_mask() sets
    FS_IN_IGNORED and FS_EVENT_ON_CHILD bits anyway.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -567,7 +567,7 @@ static int inotify_update_existing_watch(struct fsnotify_group *group,
 
 	/* don't allow invalid bits: we don't want flags set */
 	mask = inotify_arg_to_mask(arg);
-	if (unlikely(!mask))
+	if (unlikely(!(mask & IN_ALL_EVENTS)))
 		return -EINVAL;
 
 	fsn_mark = fsnotify_find_inode_mark(group, inode);
@@ -625,7 +625,7 @@ static int inotify_new_watch(struct fsnotify_group *group,
 
 	/* don't allow invalid bits: we don't want flags set */
 	mask = inotify_arg_to_mask(arg);
-	if (unlikely(!mask))
+	if (unlikely(!(mask & IN_ALL_EVENTS)))
 		return -EINVAL;
 
 	tmp_i_mark = kmem_cache_alloc(inotify_inode_mark_cachep, GFP_KERNEL);
