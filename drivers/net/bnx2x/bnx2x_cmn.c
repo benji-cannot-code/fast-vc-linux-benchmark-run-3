06f14056e8f5348cfd44a4762b5c89c76e4f08da
Stephen Rothwell <sfr@canb.auug.org.au>
2010-07-28 22:20:34 -0700

    net: bnx2x_cmn.c needs net/ip6_checksum.h for csum_ipv6_magic
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x/bnx2x_cmn.c
+++ drivers/net/bnx2x/bnx2x_cmn.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/etherdevice.h>
 #include <linux/ip.h>
 #include <linux/ipv6.h>
+#include <net/ip6_checksum.h>
 #include "bnx2x_cmn.h"
 
 #ifdef BCM_VLAN
