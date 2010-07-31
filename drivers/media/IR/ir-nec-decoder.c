a2997ee28e05bfb0a163a5cc1f8c0b0ca3723b6b
Maxim Levitsky <maximlevitsky@gmail.com>
2010-07-31 11:59:19 -0300

    V4L/DVB: IR: nec decoder: fix repeat
    Repeat space is 4 units, not 8.
    Current code would never trigger a repeat.
    
    However that isn't true for NECX, so repeat there
    must be handled differently.
    
    Signed-off-by: Maxim Levitsky <maximlevitsky@gmail.com>
    Reviewed-by: Andy Walls <awalls@md.metrocast.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/ir-nec-decoder.c
+++ drivers/media/IR/ir-nec-decoder.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NEC_HEADER_PULSE	(16 * NEC_UNIT)
 #define NECX_HEADER_PULSE	(8  * NEC_UNIT) /* Less common NEC variant */
 #define NEC_HEADER_SPACE	(8  * NEC_UNIT)
-#define NEC_REPEAT_SPACE	(8  * NEC_UNIT)
+#define NEC_REPEAT_SPACE	(4  * NEC_UNIT)
 #define NEC_BIT_PULSE		(1  * NEC_UNIT)
 #define NEC_BIT_0_SPACE		(1  * NEC_UNIT)
 #define NEC_BIT_1_SPACE		(3  * NEC_UNIT)
