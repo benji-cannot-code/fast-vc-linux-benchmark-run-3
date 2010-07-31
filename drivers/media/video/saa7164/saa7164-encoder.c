676e9db8fbf790f19ff1b35ec40212c113521d56
Steven Toth <stoth@kernellabs.com>
2010-07-31 15:10:52 -0300

    [media] saa7164: measure via histograms various irq and queue latencies
    saa7164: measure via histograms various irq and queue latencies
    Attempting to determine where buffering issues under high load are due
    to highly latent irq or work queue handling.
    
    Signed-off-by: Steven Toth <stoth@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/saa7164/saa7164-encoder.c
+++ drivers/media/video/saa7164/saa7164-encoder.c
@@ -1031,6 +1031,14 @@ static ssize_t fops_read(struct file *file, char __user *buffer,
 	int rem, cnt;
 	u8 *p;
 
+	port->last_read_msecs_diff = port->last_read_msecs;
+	port->last_read_msecs = jiffies_to_msecs(jiffies);
+	port->last_read_msecs_diff = port->last_read_msecs -
+		port->last_read_msecs_diff;
+
+	saa7164_histogram_update(&port->read_interval,
+		port->last_read_msecs_diff);
+
 	if (*pos)
 		return -ESPIPE;
 
@@ -1114,6 +1122,14 @@ static unsigned int fops_poll(struct file *file, poll_table *wait)
 	struct saa7164_port *port = fh->port;
 	struct saa7164_user_buffer *ubuf;
 	unsigned int mask = 0;
+
+	port->last_poll_msecs_diff = port->last_poll_msecs;
+	port->last_poll_msecs = jiffies_to_msecs(jiffies);
+	port->last_poll_msecs_diff = port->last_poll_msecs -
+		port->last_poll_msecs_diff;
+
+	saa7164_histogram_update(&port->poll_interval,
+		port->last_poll_msecs_diff);
 
 	if (!video_is_registered(port->v4l_device)) {
 		return -EIO;
