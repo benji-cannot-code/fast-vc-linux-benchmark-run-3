822f441908b2838a1b2bf856bd9837914b5c276b
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-08-01 13:48:20 -0300

    V4L/DVB: standardize names at rc-dib0700 tables
    Use a more standard way to name those tables, as they're currently used
    by the script that coverts those tables to be loaded via userspace.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/keymaps/rc-dib0700-nec.c
+++ drivers/media/IR/keymaps/rc-dib0700-nec.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/rc-map.h>
 
-static struct ir_scancode dib0700_table[] = {
+static struct ir_scancode dib0700_nec_table[] = {
 	/* Key codes for the Pixelview SBTVD remote */
 	{ 0x8613, KEY_MUTE },
 	{ 0x8612, KEY_POWER },
@@ -99,10 +99,10 @@ static struct ir_scancode dib0700_table[] = {
 	{ 0x4542, KEY_SELECT }, /* Select video input, 'Select' for Teletext */
 };
 
-static struct rc_keymap dib0700_map = {
+static struct rc_keymap dib0700_nec_map = {
 	.map = {
-		.scan    = dib0700_table,
-		.size    = ARRAY_SIZE(dib0700_table),
+		.scan    = dib0700_nec_table,
+		.size    = ARRAY_SIZE(dib0700_nec_table),
 		.ir_type = IR_TYPE_NEC,
 		.name    = RC_MAP_DIB0700_NEC_TABLE,
 	}
@@ -110,12 +110,12 @@ static struct rc_keymap dib0700_map = {
 
 static int __init init_rc_map(void)
 {
-	return ir_register_map(&dib0700_map);
+	return ir_register_map(&dib0700_nec_map);
 }
 
 static void __exit exit_rc_map(void)
 {
-	ir_unregister_map(&dib0700_map);
+	ir_unregister_map(&dib0700_nec_map);
 }
 
 module_init(init_rc_map)
