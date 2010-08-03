d6311eda680f027ffd08740dbad88b10ef3af028
Changli Gao <xiaosuo@gmail.com>
2010-08-02 22:45:49 -0700

    net: cleanup inclusion
    Commit ab95bfe01f9872459c8678572ccadbf646badad0 replaces bridge and macvlan
    hooks in __netif_receive_skb(), so dev.c doesn't need to include their headers.
    
    Signed-off-by: Changli Gao <xiaosuo@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -102,8 +102,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/stat.h>
-#include <linux/if_bridge.h>
-#include <linux/if_macvlan.h>
 #include <net/dst.h>
 #include <net/pkt_sched.h>
 #include <net/checksum.h>
