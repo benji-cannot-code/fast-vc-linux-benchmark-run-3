4a714a4c5470cc8c244b7649780a2176198200c2
Kukjin Kim <kgene.kim@samsung.com>
2010-08-05 07:54:49 +0900

    ARM: SAMSUNG: Fix on inclusion mach/gpio.h for Samsung SoCs
    This patch fixes on inclusion <mach/gpio.h> to <linux/gpio.h>
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Cc: Ben Dooks <ben-linux@fluff.org>

--- arch/arm/mach-s3c64xx/setup-fb-24bpp.c
+++ arch/arm/mach-s3c64xx/setup-fb-24bpp.c
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/fb.h>
+#include <linux/gpio.h>
 
 #include <mach/regs-fb.h>
-#include <mach/gpio.h>
 #include <plat/fb.h>
 #include <plat/gpio-cfg.h>
 
--- arch/arm/mach-s5p6442/dev-audio.c
+++ arch/arm/mach-s5p6442/dev-audio.c
@@ -11,11 +11,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/platform_device.h>
 #include <linux/dma-mapping.h>
+#include <linux/gpio.h>
 
 #include <plat/gpio-cfg.h>
 #include <plat/audio.h>
 
-#include <mach/gpio.h>
 #include <mach/map.h>
 #include <mach/dma.h>
 #include <mach/irqs.h>
