661a6f88ddc14d8c1c753b8f15448d4483ce1d9a
Chris Wilson <chris@chris-wilson.co.uk>
2010-08-04 14:19:46 +0100

    drm: Use ENOENT consistently for the error return for an unmatched handle.
    This is consistent with trying to access a filename that not exist
    within a directory which is a good analogy here. The main reason for the
    change is that it is easy to confuse the error code of EBADF as an
    performing an ioctl on an invalid file descriptor (rather than an
    unknown object).
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/nouveau/nv04_crtc.c
+++ drivers/gpu/drm/nouveau/nv04_crtc.c
@@ -918,7 +918,7 @@ nv04_crtc_cursor_set(struct drm_crtc *crtc, struct drm_file *file_priv,
 
 	gem = drm_gem_object_lookup(dev, file_priv, buffer_handle);
 	if (!gem)
-		return -EINVAL;
+		return -ENOENT;
 	cursor = nouveau_gem_object(gem);
 
 	ret = nouveau_bo_map(cursor);
