002414e64c80d06c72f9d9b88360de2761201d19
Andrea Gelmini <andrea.gelmini@gelma.net>
2010-08-05 15:51:36 +0200

    net: sunrpc: removed duplicated #include
    Signed-off-by: Andrea Gelmini <andrea.gelmini@gelma.net>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- net/sunrpc/cache.c
+++ net/sunrpc/cache.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sunrpc/cache.h>
 #include <linux/sunrpc/stats.h>
 #include <linux/sunrpc/rpc_pipe_fs.h>
-#include <linux/smp_lock.h>
 
 #define	 RPCDBG_FACILITY RPCDBG_CACHE
 
