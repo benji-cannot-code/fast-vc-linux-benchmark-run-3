c1bdb153f3870fa1ad82f00ac00c5370d1f414ef
Ralf Baechle <ralf@linux-mips.org>
2010-08-05 13:25:59 +0100

    MIPS: Remove unnecessary header file inclusion from fault.c.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mm/fault.c
+++ arch/mips/mm/fault.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mman.h>
 #include <linux/mm.h>
 #include <linux/smp.h>
-#include <linux/vt_kern.h>		/* For unblank_screen() */
 #include <linux/module.h>
 
 #include <asm/branch.h>
