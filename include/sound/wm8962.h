c71e55d27a503f0167264aad9fec16bb0eb47ac4
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-08-05 13:20:59 +0100

    ASoC: Add initial WM8962 CODEC driver
    The WM8962 is a low power, high performance stereo CODEC designed for
    portable digital audio applications.
    
    This initial driver release supports the key audio paths of the WM8962.
    Extended functionality, such as microphone detection, digital microphones
    and the advanced DSP signal enhancements provided by the device are not
    yet supported.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

---
+++ include/sound/wm8962.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * wm8962.h  --  WM8962 Soc Audio driver platform data
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _WM8962_PDATA_H
+#define _WM8962_PDATA_H
+
+#define WM8962_MAX_GPIO 6
+
+/* Use to set GPIO default values to zero */
+#define WM8962_GPIO_SET 0x10000
+
+struct wm8962_pdata {
+	u32 gpio_init[WM8962_MAX_GPIO];
+
+	bool spk_mono;   /* Speaker outputs tied together as mono */
+};
+
+#endif
