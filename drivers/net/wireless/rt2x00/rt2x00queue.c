fb39d3aaa124a839fe71e6ed282b7fea3fb4fc45
Ivo van Doorn <ivdoorn@gmail.com>
2010-08-06 20:45:38 +0200

    rt2x00: Move USB tx/rx done handling to workqueue
    Move all TX and RX completion handling into a work structure,
    which is handeled on the mac80211 workqueue. This simplifies
    the code in rt2x00lib since it no longer needs to check if the
    device is USB or PCI to decide which mac80211 function should be used.
    
    In the watchdog some changes are needed since it can no longer rely
    on the TX completion function to be run while looping through the
    entries. (Both functions now work on the same workqueue, so this
    would deadlock). So the watchdog now waits for the URB to return,
    and handle the TX status report directly.
    
    As a side-effect, the debugfs entry for the RX queue now correctly
    displays the positions of the INDEX and INDEX_DONE counters. This
    also implies that it is not possible to perform checks like queue_empty()
    and queue_full() on the RX queue.
    
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00queue.c
+++ drivers/net/wireless/rt2x00/rt2x00queue.c
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
-	Copyright (C) 2004 - 2009 Ivo van Doorn <IvDoorn@gmail.com>
+	Copyright (C) 2010 Willow Garage <http://www.willowgarage.com>
+	Copyright (C) 2004 - 2010 Ivo van Doorn <IvDoorn@gmail.com>
 	Copyright (C) 2004 - 2009 Gertjan van Wingerde <gwingerde@gmail.com>
 	<http://rt2x00.serialmonkey.com>
 
@@ -731,9 +732,9 @@ void rt2x00queue_init_queues(struct rt2x00_dev *rt2x00dev)
 		rt2x00queue_reset(queue);
 
 		for (i = 0; i < queue->limit; i++) {
-			queue->entries[i].flags = 0;
-
 			rt2x00dev->ops->lib->clear_entry(&queue->entries[i]);
+			if (queue->qid == QID_RX)
+				rt2x00queue_index_inc(queue, Q_INDEX);
 		}
 	}
 }
