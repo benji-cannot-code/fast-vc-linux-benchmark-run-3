58417231f30ee7528c1a3178fc087a4dfb52ddb1
Hans Verkuil <hverkuil@xs4all.nl>
2010-08-06 11:08:54 -0300

    V4L/DVB: saa5246a/saa5249: Remove obsolete teletext drivers
    These old i2c teletext drivers are not supported by any hardware and cannot be
    tested anymore.
    
    Note that while the mxb board seemingly used the saa5246a driver, in reality
    this teletext driver never worked.
    
    These drivers are removed as part of the vtx feature removal, originally
    scheduled for 2.6.35.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/mxb.c
+++ drivers/media/video/mxb.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tea6415c.h"
 #include "tea6420.h"
 
-#define	I2C_SAA5246A  0x11
 #define I2C_SAA7111A  0x24
 #define	I2C_TDA9840   0x42
 #define	I2C_TEA6415C  0x43
@@ -198,10 +197,6 @@ static int mxb_probe(struct saa7146_dev *dev)
 			"tda9840", "tda9840", I2C_TDA9840, NULL);
 	mxb->tuner = v4l2_i2c_new_subdev(&dev->v4l2_dev, &mxb->i2c_adapter,
 			"tuner", "tuner", I2C_TUNER, NULL);
-	if (v4l2_i2c_new_subdev(&dev->v4l2_dev, &mxb->i2c_adapter,
-			"saa5246a", "saa5246a", I2C_SAA5246A, NULL)) {
-		printk(KERN_INFO "mxb: found teletext decoder\n");
-	}
 
 	/* check if all devices are present */
 	if (!mxb->tea6420_1 || !mxb->tea6420_2 || !mxb->tea6415c ||
