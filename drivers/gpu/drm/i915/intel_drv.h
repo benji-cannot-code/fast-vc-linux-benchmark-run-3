acb73c1c5f36c51efed3b18201bd1c0eb569e252
Chris Wilson <chris@chris-wilson.co.uk>
2010-08-07 12:16:25 +0100

    drm/i915: Avoid using msleep under kdb and wait_for()
    wait_for() uses msleep() to yield the cpu whilst spinning waiting for a
    register to change. kdb asserts that mode changes are atomic and so
    prohibits msleep. The alternative would be to use mdelay or to simply
    probe the register more often instead of busy waiting.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 			ret__ = -ETIMEDOUT;				\
 			break;						\
 		}							\
-		if (W) msleep(W);					\
+		if (W && !in_dbg_master()) msleep(W);			\
 	}								\
 	ret__;								\
 })
