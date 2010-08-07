c8ddce7001a53880a070ed10065ad57eb347daaf
Chris Wilson <chris@chris-wilson.co.uk>
2010-08-07 11:01:35 +0100

    drm/i915: Ensure that while(INREG()) are bounded (v2)
    Add a new macro, wait_for, to simplify the act of waiting on a register
    to change state. wait_for() takes three arguments, the condition to
    inspect on every loop, the maximum amount of time to wait and whether to
    yield the cpu for a length of time after each check.
    
    v2: Upgrade failure messages to DRM_ERROR on the suggestion of
    Eric Anholt. We do not expect to hit these conditions as they reflect
    programming errors, so if we do we want to be notified.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -33,6 +33,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "drm_crtc.h"
 
 #include "drm_crtc_helper.h"
+
+#define wait_for(COND, MS, W) ({ \
+	unsigned long timeout__ = jiffies + msecs_to_jiffies(MS);	\
+	int ret__ = 0;							\
+	while (! (COND)) {						\
+		if (time_after(jiffies, timeout__)) {			\
+			ret__ = -ETIMEDOUT;				\
+			break;						\
+		}							\
+		if (W) msleep(W);					\
+	}								\
+	ret__;								\
+})
+
 /*
  * Display related stuff
  */
