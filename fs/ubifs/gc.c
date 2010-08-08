a6e1344efe229aa8ae003b438883d8c920a019a6
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2010-08-08 12:45:23 +0300

    UBIFS: fix assertion warnings in comparison function
    When running the integrity test ('integck' from mtd-utils) on current
    UBIFS on 2.6.35, I see that assertions in UBIFS 'list_sort()' comparison
    functions trigger sometimes, e.g.:
    
    UBIFS assert failed in data_nodes_cmp at 132 (pid 28311)
    
    My investigation showed that this happens when 'list_sort()' calls the 'cmp()'
    function with equivalent arguments. In this case, the 'struct list_head'
    parameter, passed to 'cmp()' is bogus, and it does not belong to any element in
    the original list.
    
    And this issue seems to be introduced by commit:
    
    commit 835cc0c8477fdbc59e0217891d6f11061b1ac4e2
    Author: Don Mullis <don.mullis@gmail.com>
    Date:   Fri Mar 5 13:43:15 2010 -0800
    
    It is easy to work around the issue by doing:
    
    if (a == b)
    	return 0;
    
    in UBIFS. It works, but 'lib_sort()' should nevertheless be fixed. Although it
    is harmless to have this piece of code in UBIFS.
    
    This patch adds that code to both UBIFS 'cmp()' functions:
    'data_nodes_cmp()' and 'nondata_nodes_cmp()'.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/gc.c
+++ fs/ubifs/gc.c
@@ -126,6 +126,9 @@ int data_nodes_cmp(void *priv, struct list_head *a, struct list_head *b)
 	struct ubifs_scan_node *sa, *sb;
 
 	cond_resched();
+	if (a == b)
+		return 0;
+
 	sa = list_entry(a, struct ubifs_scan_node, list);
 	sb = list_entry(b, struct ubifs_scan_node, list);
 
@@ -166,6 +169,9 @@ int nondata_nodes_cmp(void *priv, struct list_head *a, struct list_head *b)
 	struct ubifs_scan_node *sa, *sb;
 
 	cond_resched();
+	if (a == b)
+		return 0;
+
 	sa = list_entry(a, struct ubifs_scan_node, list);
 	sb = list_entry(b, struct ubifs_scan_node, list);
 
