ff7415b46c85df538a9d94d83bfcd083dff9bbe7
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-08-10 18:57:01 -0300

    i7core_edac: Properly mark const static vars as such
    There are two groups of sysfs attributes: one for rdimm and another
    for udimm. Instead of changing dynamically the unique static struct
    for handling udimm's, declare two vars and make them constant.
    
    This avoids the risk of having two or more memory controllers, each
    needing a different set of attributes.
    
    While here, use const on all places where it is applicable.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>
    
    edac_core: use const for constant sysfs arguments
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/edac/edac_core.h
+++ drivers/edac/edac_core.h
@@ -329,7 +329,7 @@ struct csrow_info {
 
 struct mcidev_sysfs_group {
 	const char *name;				/* group name */
-	struct mcidev_sysfs_attribute *mcidev_attr;	/* group attributes */
+	const struct mcidev_sysfs_attribute *mcidev_attr; /* group attributes */
 };
 
 struct mcidev_sysfs_group_kobj {
@@ -337,7 +337,7 @@ struct mcidev_sysfs_group_kobj {
 
 	struct kobject kobj;		/* kobj for the group */
 
-	struct mcidev_sysfs_group *grp;	/* group description table */
+	const struct mcidev_sysfs_group *grp;	/* group description table */
 	struct mem_ctl_info *mci;	/* the parent */
 };
 
@@ -348,7 +348,7 @@ struct mcidev_sysfs_group_kobj {
 struct mcidev_sysfs_attribute {
 	/* It should use either attr or grp */
 	struct attribute attr;
-	struct mcidev_sysfs_group *grp;	/* Points to a group of attributes */
+	const struct mcidev_sysfs_group *grp;	/* Points to a group of attributes */
 
 	/* Ops for show/store values at the attribute - not used on group */
         ssize_t (*show)(struct mem_ctl_info *,char *);
@@ -441,7 +441,7 @@ struct mem_ctl_info {
 	 * If attributes are desired, then set to array of attributes
 	 * If no attributes are desired, leave NULL
 	 */
-	struct mcidev_sysfs_attribute *mc_driver_sysfs_attributes;
+	const struct mcidev_sysfs_attribute *mc_driver_sysfs_attributes;
 
 	/* work struct for this MC */
 	struct delayed_work work;
