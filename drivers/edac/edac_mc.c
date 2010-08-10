5431a675784e859c8a088d549821a3c966971406
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-08-10 11:22:01 -0300

    i7core_edac: Be sure that the edac pci handler will be properly released
    With multi-sockets, more than one edac pci handler is enabled. Be sure to
    un-register all instances.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/edac/edac_mc.c
+++ drivers/edac/edac_mc.c
@@ -240,13 +240,14 @@ void edac_mc_free(struct mem_ctl_info *mci)
 EXPORT_SYMBOL_GPL(edac_mc_free);
 
 
-/*
+/**
  * find_mci_by_dev
  *
  *	scan list of controllers looking for the one that manages
  *	the 'dev' device
+ * @dev: pointer to a struct device related with the MCI
  */
-static struct mem_ctl_info *find_mci_by_dev(struct device *dev)
+struct mem_ctl_info *find_mci_by_dev(struct device *dev)
 {
 	struct mem_ctl_info *mci;
 	struct list_head *item;
@@ -262,6 +263,7 @@ static struct mem_ctl_info *find_mci_by_dev(struct device *dev)
 
 	return NULL;
 }
+EXPORT_SYMBOL_GPL(find_mci_by_dev);
 
 /*
  * handler for EDAC to check if NMI type handler has asserted interrupt
