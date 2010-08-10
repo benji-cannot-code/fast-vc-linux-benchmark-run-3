f06d0ef181f2ce8f18b7d6bed6e06cb9e258bb22
Alan Cox <alan@linux.intel.com>
2010-08-10 10:28:04 +0100

    intel_mid_battery: Fix battery scaling
    There are 3600 seconds per not 3600 hours per second. Correcting this
    along with the previous fix gives sensible numbers.
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Anton Vorontsov <cbouatmailru@gmail.com>

--- drivers/power/intel_mid_battery.c
+++ drivers/power/intel_mid_battery.c
@@ -428,7 +428,7 @@ static int pmic_usb_get_property(struct power_supply *psy,
 static inline unsigned long mAStouAh(unsigned long v)
 {
 	/* seconds to hours, mA to µA */
-	return v * 3600 * 1000;
+	return (v * 1000) / 3600;
 }
 
 /**
