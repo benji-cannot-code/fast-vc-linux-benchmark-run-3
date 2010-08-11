601a3969e0e0461369f4ac587758c6ac941c8ffc
Anton Vorontsov <avorontsov@mvista.com>
2010-08-10 18:01:49 -0700

    sdhci-pltfm: add support for CNS3xxx SoC devices
    There's nothing special, just SoC-specific ops and quirks.
    
    Signed-off-by: Anton Vorontsov <avorontsov@mvista.com>
    Cc: Ben Dooks <ben@simtec.co.uk>
    Cc: Richard R?jfors <richard.rojfors@pelagicore.com>
    Cc: <linux-mmc@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/mmc/host/sdhci-pltfm.h
+++ drivers/mmc/host/sdhci-pltfm.h
@@ -12,4 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _DRIVERS_MMC_SDHCI_PLTFM_H
 #define _DRIVERS_MMC_SDHCI_PLTFM_H
 
+#include <linux/sdhci-pltfm.h>
+
+extern struct sdhci_pltfm_data sdhci_cns3xxx_pdata;
+
 #endif /* _DRIVERS_MMC_SDHCI_PLTFM_H */
