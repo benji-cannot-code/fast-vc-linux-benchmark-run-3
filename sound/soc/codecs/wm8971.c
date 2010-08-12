8366a6119f2a070e3e09365e24f38f4ae1fa27c2
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-08-12 10:57:05 +0100

    ASoC: Remove version display from WM8971 driver
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/codecs/wm8971.c
+++ sound/soc/codecs/wm8971.c
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "wm8971.h"
 
-#define WM8971_VERSION "0.9"
-
 #define	WM8971_REG_COUNT		43
 
 static struct workqueue_struct *wm8971_workq = NULL;
@@ -639,8 +637,6 @@ static int wm8971_probe(struct snd_soc_codec *codec)
 	struct wm8971_priv *wm8971 = snd_soc_codec_get_drvdata(codec);
 	int ret = 0;
 	u16 reg;
-
-	pr_info("WM8971 Audio Codec %s", WM8971_VERSION);
 
 	ret = snd_soc_codec_set_cache_io(codec, 7, 9, wm8971->control_type);
 	if (ret < 0) {
