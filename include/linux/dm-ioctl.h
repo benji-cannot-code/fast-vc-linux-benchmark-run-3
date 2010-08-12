8b6cdad2d747b630fd05738d827fbd3cbb6de9b7
Peter Rajnoha <prajnoha@redhat.com>
2010-08-12 04:14:05 +0100

    dm: allow autoloading of dm mod
    Add devname:mapper/control and MAPPER_CTRL_MINOR module alias
    to support dm-mod module autoloading.
    
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Peter Rajnoha <prajnoha@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- include/linux/dm-ioctl.h
+++ include/linux/dm-ioctl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 #define DM_DIR "mapper"		/* Slashes not supported */
+#define DM_CONTROL_NODE "control"
 #define DM_MAX_TYPE_NAME 16
 #define DM_NAME_LEN 128
 #define DM_UUID_LEN 129
