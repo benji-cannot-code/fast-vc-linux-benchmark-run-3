ddc3b664333620f609c28ba98dadfbd7a9b1fa76
Dan Carpenter <error27@gmail.com>
2010-08-12 09:53:52 +0200

    mtd/m25p80: retlen is never NULL
    This is just a cleanup, it doesn't fix any bugs.
    
    These functions all check retlen inconsistently and it generates a
    warning in Smatch (http://smatch.sf.net).  If retlen were ever NULL it
    would cause an oops and the code has been this way since 2006 so someone
    would have complained.  Also I looked at other places that implemented
    the mtd read and write functions and they dereference retlen without
    checking.
    
    I removed the checks.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -348,8 +348,7 @@ static int m25p80_read(struct mtd_info *mtd, loff_t from, size_t len,
 	spi_message_add_tail(&t[1], &m);
 
 	/* Byte count starts at zero. */
-	if (retlen)
-		*retlen = 0;
+	*retlen = 0;
 
 	mutex_lock(&flash->lock);
 
@@ -395,8 +394,7 @@ static int m25p80_write(struct mtd_info *mtd, loff_t to, size_t len,
 			dev_name(&flash->spi->dev), __func__, "to",
 			(u32)to, len);
 
-	if (retlen)
-		*retlen = 0;
+	*retlen = 0;
 
 	/* sanity checks */
 	if (!len)
@@ -467,8 +465,7 @@ static int m25p80_write(struct mtd_info *mtd, loff_t to, size_t len,
 
 			spi_sync(flash->spi, &m);
 
-			if (retlen)
-				*retlen += m.actual_length - m25p_cmdsz(flash);
+			*retlen += m.actual_length - m25p_cmdsz(flash);
 		}
 	}
 
@@ -486,8 +483,7 @@ static int sst_write(struct mtd_info *mtd, loff_t to, size_t len,
 	size_t actual;
 	int cmd_sz, ret;
 
-	if (retlen)
-		*retlen = 0;
+	*retlen = 0;
 
 	/* sanity checks */
 	if (!len)
