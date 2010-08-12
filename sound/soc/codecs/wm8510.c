d526f020155b9d26c3961279ca12a7819f480597
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-08-12 10:57:04 +0100

    ASoC: Remove version display from WM8510 driver
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/codecs/wm8510.c
+++ sound/soc/codecs/wm8510.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "wm8510.h"
 
-#define WM8510_VERSION "0.6"
-
 /*
  * wm8510 register cache
  * We can't read the WM8510 register space when we are
@@ -563,8 +561,6 @@ static int wm8510_probe(struct snd_soc_codec *codec)
 {
 	struct wm8510_priv *wm8510 = snd_soc_codec_get_drvdata(codec);
 	int ret;
-
-	pr_info("WM8510 Audio Codec %s", WM8510_VERSION);
 
 	codec->control_data = wm8510->control_data;
 	ret = snd_soc_codec_set_cache_io(codec, 7, 9,  wm8510->control_type);
