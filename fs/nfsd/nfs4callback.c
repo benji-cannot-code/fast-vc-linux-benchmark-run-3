e27190ce350214ca748834e92dceb14c5fbf2b4f
Andy Adamson <andros@netapp.com>
2010-08-13 17:31:16 -0400

    nfsd: remove duplicate NFS4_STATEID_SIZE declaration
    Use NFS4_STATEID_SIZE from include/linux/nfs4
    
    Signed-off-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4callback.c
+++ fs/nfsd/nfs4callback.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NFSPROC4_CB_NULL 0
 #define NFSPROC4_CB_COMPOUND 1
-#define NFS4_STATEID_SIZE 16
 
 /* Index of predefined Linux callback client operations */
 
