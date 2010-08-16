557ab158a0e094811ddd2e815822967b394e970d
Julia Lawall <julia@diku.dk>
2010-08-16 06:26:57 +0000

    net/ax25: Use available error codes
    Error codes are stored in err, but the return value is always 0.  Return
    err instead.
    
    The semantic match that finds this problem is as follows:
    (http://coccinelle.lip6.fr/)
    
    // <smpl>
    @r@
    local idexpression x;
    constant C;
    @@
    
    if (...) { ...
      x = -C
      ... when != x
    (
      return <+...x...+>;
    |
      return NULL;
    |
      return;
    |
    * return ...;
    )
    }
    // </smpl>
    
    Signed-off-by: Julia Lawall <julia@diku.dk>
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ax25/ax25_route.c
+++ net/ax25/ax25_route.c
@@ -413,7 +413,7 @@ int ax25_rt_autobind(ax25_cb *ax25, ax25_address *addr)
 {
 	ax25_uid_assoc *user;
 	ax25_route *ax25_rt;
-	int err;
+	int err = 0;
 
 	if ((ax25_rt = ax25_get_route(addr, NULL)) == NULL)
 		return -EHOSTUNREACH;
@@ -454,7 +454,7 @@ int ax25_rt_autobind(ax25_cb *ax25, ax25_address *addr)
 put:
 	ax25_put_route(ax25_rt);
 
-	return 0;
+	return err;
 }
 
 struct sk_buff *ax25_rt_build_path(struct sk_buff *skb, ax25_address *src,
