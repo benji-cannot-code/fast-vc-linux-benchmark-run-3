e81da31de99ee30502acad17a737bc70662819c6
David Miller <davem@davemloft.net>
2010-08-16 21:20:07 -0700

    arcmsr_hba: Missing slab.h include
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/scsi/arcmsr/arcmsr_hba.c
+++ drivers/scsi/arcmsr/arcmsr_hba.c
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/dma-mapping.h>
 #include <linux/timer.h>
+#include <linux/slab.h>
 #include <linux/pci.h>
 #include <linux/aer.h>
 #include <asm/dma.h>
