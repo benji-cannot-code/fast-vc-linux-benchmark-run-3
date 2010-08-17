59e102b6b3c2cb6f16126a928851c720f5727c8a
Grant Likely <grant.likely@secretlab.ca>
2010-08-16 23:44:49 -0600

    of: Fix missing includes
    This patch fixes missing includes from a number of .c files because
    the code (wrongfully) depended on prom.h including them.  The include
    of linux/of_address.h was removed in microblaze prom.h in commit
    "of/address: Clean up function declarations" (sha1 id 22ae782f8), but
    not fixed in some callers.  This patch fixes them up.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Tested-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/pci/pci-common.c
+++ arch/microblaze/pci/pci-common.c
@@ -28,10 +28,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
 
 #include <asm/processor.h>
 #include <asm/io.h>
-#include <asm/prom.h>
 #include <asm/pci-bridge.h>
 #include <asm/byteorder.h>
 
