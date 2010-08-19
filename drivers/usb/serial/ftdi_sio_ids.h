c6e3dab95fd55d5c1dd6a88542fbee0270e52997
Galen Seitz <galens@seitzassoc.com>
2010-08-19 11:15:20 -0700

    USB: ftdi_sio: add product ID for Lenz LI-USB
    Add ftdi product ID for Lenz LI-USB, a model train interface.  This
    was NOT tested against 2.6.35, but a similar patch was tested with the
    CentOS 2.6.18-194.11.1.el5 kernel.  It wasn't clear to me what
    ordering is being used in ftdi_sio.c, so I inserted the ID after another
    model train entry(SPROG_II).
    
    Signed-off-by: Galen Seitz <galens@seitzassoc.com>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -111,6 +111,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Propox devices */
 #define FTDI_PROPOX_JTAGCABLEII_PID	0xD738
 
+/* Lenz LI-USB Computer Interface. */
+#define FTDI_LENZ_LIUSB_PID	0xD780
+
 /*
  * Xsens Technologies BV products (http://www.xsens.com).
  */
