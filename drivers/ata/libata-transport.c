f8de107e0a2aa3d4c6b83a7f76ba74b98d32aca3
Jeff Garzik <jeff@garzik.org>
2010-08-19 16:11:32 -0400

    drivers/ata/libata-transport.c: include linux/slab.h
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/libata-transport.c
+++ drivers/ata/libata-transport.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/blkdev.h>
 #include <linux/spinlock.h>
+#include <linux/slab.h>
 #include <scsi/scsi_transport.h>
 #include <linux/libata.h>
 #include <linux/hdreg.h>
