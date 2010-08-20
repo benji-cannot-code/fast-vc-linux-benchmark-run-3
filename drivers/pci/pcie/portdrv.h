07c8f62a1fc0907f2a534336255861dcdcaa0c3e
Rafael J. Wysocki <rjw@sisk.pl>
2010-08-21 01:51:44 +0200

    PCI: PCIe: Introduce commad line switch for disabling port services
    Introduce kernel command line switch pcie_ports= allowing one to
    disable all of the native PCIe port services, so that PCIe ports
    are treated like PCI-to-PCI bridges.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pcie/portdrv.h
+++ drivers/pci/pcie/portdrv.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define get_descriptor_id(type, service) (((type - 4) << 4) | service)
 
+extern bool pcie_ports_disabled;
+
 extern struct bus_type pcie_port_bus_type;
 extern int pcie_port_device_register(struct pci_dev *dev);
 #ifdef CONFIG_PM
