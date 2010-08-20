a59f4f1e3a055f9dab5a87334c738800a5332012
Tvrtko Ursulin <tvrtko.ursulin@sophos.com>
2010-08-20 10:02:15 +0100

    fanotify: resize pid and reorder structure
    resize pid and reorder the fanotify_event_metadata so it is naturally
    aligned and we can work towards dropping the packed attributed
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@sophos.com>
    Cc: Andreas Dilger <adilger@dilger.ca>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- include/linux/fanotify.h
+++ include/linux/fanotify.h
@@ -66,14 +66,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				 FAN_ALL_PERM_EVENTS |\
 				 FAN_Q_OVERFLOW)
 
-#define FANOTIFY_METADATA_VERSION	1
+#define FANOTIFY_METADATA_VERSION	2
 
 struct fanotify_event_metadata {
 	__u32 event_len;
 	__u32 vers;
-	__s32 fd;
 	__u64 mask;
-	__s64 pid;
+	__s32 fd;
+	__s32 pid;
 } __attribute__ ((packed));
 
 struct fanotify_response {
