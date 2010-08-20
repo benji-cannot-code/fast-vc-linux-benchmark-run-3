86fbcda03476e1466dccce2a3653e42fce710817
Rafael J. Wysocki <rjw@sisk.pl>
2010-08-21 01:58:22 +0200

    PCI: PCIe: Move PCIe PME code to the pcie directory
    The PCIe PME code only consists of one file, so it doesn't need to
    occupy its own directory.  Move it to drivers/pci/pcie/pme.c and
    remove the contents of drivers/pci/pcie/pme .
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pcie/pme.c
+++ drivers/pci/pcie/pme.c
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci-acpi.h>
 #include <linux/pm_runtime.h>
 
-#include "../../pci.h"
-#include "../portdrv.h"
+#include "../pci.h"
+#include "portdrv.h"
 
 #define PCI_EXP_RTSTA_PME	0x10000 /* PME status */
 #define PCI_EXP_RTSTA_PENDING	0x20000 /* PME pending */
