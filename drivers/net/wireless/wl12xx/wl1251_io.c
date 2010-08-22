0ab6b4d5e0df97f45293a7a551a8b7d4c0bfaeb2
Kalle Valo <kvalo@adurom.com>
2010-08-22 22:46:43 +0300

    wl1251: remove copyright contact person
    As my nokia address doesn't work anymore, it's better just to remove the
    copyright contact altogether. We have MODULE_AUTHOR() scripts for these
    anyway.
    
    Signed-off-by: Kalle Valo <kvalo@adurom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/wl1251_io.c
+++ drivers/net/wireless/wl12xx/wl1251_io.c
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2008 Nokia Corporation
  *
- * Contact: Kalle Valo <kalle.valo@nokia.com>
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
