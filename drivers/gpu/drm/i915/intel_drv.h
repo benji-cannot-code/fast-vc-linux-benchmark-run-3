b307965eb83a062481cca9af6fa22ce18834021e
Chris Wilson <chris@chris-wilson.co.uk>
2010-08-23 17:43:35 +0100

    drm/i915: Drop the msleep parameter to wait_for()
    Jesse's feedback from using the wait_for() macro was that the msleep
    argument was that it was superfluous and made the macro more difficult
    to use and to read. As the actually amount of time to sleep is not
    critical, the crucial part is to sleep and let the processor schedule
    something else whilst we wait for the event, replace the argument with a
    hardcoded value.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "drm_crtc_helper.h"
 
-#define wait_for(COND, MS, W) ({ \
+#define _wait_for(COND, MS, W) ({ \
 	unsigned long timeout__ = jiffies + msecs_to_jiffies(MS);	\
 	int ret__ = 0;							\
 	while (! (COND)) {						\
@@ -46,6 +46,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	}								\
 	ret__;								\
 })
+
+#define wait_for(COND, MS) _wait_for(COND, MS, 1)
+#define wait_for_atomic(COND, MS) _wait_for(COND, MS, 0)
 
 /*
  * Display related stuff
