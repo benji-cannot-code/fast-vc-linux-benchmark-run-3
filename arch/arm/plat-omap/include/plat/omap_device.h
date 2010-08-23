9a1b88fbf9f5c7227ae03c733d13f3197ee7ac45
Kevin Hilman <khilman@deeprootsystems.com>
2010-08-23 08:10:55 -0700

    OMAP: omap_device: make all devices a child of a new parent device
    In order to help differentiate omap_devices from normal
    platform_devices, make them all a parent of a new common parent
    device.
    
    Then, in order to determine if a platform_device is also an
    omap_device, checking the parent is all that is needed.
    
    Users of this feature are the runtime PM core for OMAP, where we need
    to know if a device being passed in is an omap_device or not in order
    to know whether to call the omap_device API with it.
    
    In addition, all omap_devices will now show up under /sys/devices/omap
    instead of /sys/devices/platform
    
    Acked-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/plat-omap/include/plat/omap_device.h
+++ arch/arm/plat-omap/include/plat/omap_device.h
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat/omap_hwmod.h>
 
+extern struct device omap_device_parent;
+
 /* omap_device._state values */
 #define OMAP_DEVICE_STATE_UNKNOWN	0
 #define OMAP_DEVICE_STATE_ENABLED	1
