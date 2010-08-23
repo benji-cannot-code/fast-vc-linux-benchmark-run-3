d07618e3a35520b38a5a87ce74864ac7a381776e
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2010-08-23 07:57:10 -0700

    iwlagn: parsing uart message and take actions
    1. Based on uart message from uCode, re-configure BT kill ack mask
    messages from uCode
    2. send REPLY_BT_COEX_SCO command to uCode based on the uart frame
    received from uCode
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-dev.h
+++ drivers/net/wireless/iwlwifi/iwl-dev.h
@@ -1371,6 +1371,9 @@ struct iwl_priv {
 	bool bt_sco_active;
 	bool bt_full_concurrent;
 	bool bt_ant_couple_ok;
+	__le32 kill_ack_mask;
+	__le32 kill_cts_mask;
+	__le16 bt_valid;
 	u8 bt_ci_compliance;
 	struct work_struct bt_traffic_change_work;
 
@@ -1393,6 +1396,7 @@ struct iwl_priv {
 	struct work_struct start_internal_scan;
 	struct work_struct tx_flush;
 	struct work_struct bt_full_concurrency;
+	struct work_struct bt_runtime_config;
 
 	struct tasklet_struct irq_tasklet;
 
