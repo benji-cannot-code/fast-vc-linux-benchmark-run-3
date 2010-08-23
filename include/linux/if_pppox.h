1dbd4c767c36ef4bc34553d04137ca41a654b26e
Changli Gao <xiaosuo@gmail.com>
2010-08-22 18:37:27 -0700

    net: use __be16 instead of u16 for the userspace code
    Signed-off-by: Changli Gao <xiaosuo@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_pppox.h
+++ include/linux/if_pppox.h
@@ -51,8 +51,8 @@ struct pppoe_addr {
  * PPTP addressing definition
  */
 struct pptp_addr {
-	u16             call_id;
-	struct in_addr  sin_addr;
+	__be16		call_id;
+	struct in_addr	sin_addr;
 };
 
 /************************************************************************
