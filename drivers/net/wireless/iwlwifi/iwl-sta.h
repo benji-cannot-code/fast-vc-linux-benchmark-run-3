9100b9dae261f6d428ce21e5ba43cca55ea371ef
Johannes Berg <johannes.berg@intel.com>
2010-08-23 10:46:42 +0200

    iwlwifi: move AP sta ID to context
    Each context needs to use a different AP sta
    ID, so we need to move that into the context
    struct instead of hardcoding it.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-sta.h
+++ drivers/net/wireless/iwlwifi/iwl-sta.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int iwl_remove_default_wep_key(struct iwl_priv *priv,
 			       struct ieee80211_key_conf *key);
 int iwl_set_default_wep_key(struct iwl_priv *priv,
+			    struct iwl_rxon_context *ctx,
 			    struct ieee80211_key_conf *key);
 int iwl_restore_default_wep_keys(struct iwl_priv *priv);
 int iwl_set_dynamic_key(struct iwl_priv *priv, struct iwl_rxon_context *ctx,
