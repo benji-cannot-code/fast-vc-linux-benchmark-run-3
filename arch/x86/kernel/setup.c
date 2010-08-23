cf4331f0b96c7f06e2f0ec0422bad887eb539d83
Alok Kataria <akataria@vmware.com>
2010-08-23 14:49:11 -0700

    x86, vmware: Remove deprecated VMI kernel support
    With the recent innovations in CPU hardware acceleration technologies
    from Intel and AMD, VMware ran a few experiments to compare these
    techniques to guest paravirtualization technique on VMware's platform.
    These hardware assisted virtualization techniques have outperformed the
    performance benefits provided by VMI in most of the workloads. VMware
    expects that these hardware features will be ubiquitous in a couple of
    years, as a result, VMware has started a phased retirement of this
    feature from the hypervisor.
    
    Please note that VMI has always been an optimization and non-VMI kernels
    still work fine on VMware's platform.
    Latest versions of VMware's product which support VMI are,
    Workstation 7.0 and VSphere 4.0 on ESX side, future maintainence
    releases for these products will continue supporting VMI.
    
    For more details about VMI retirement take a look at this,
    http://blogs.vmware.com/guestosguide/2009/09/vmi-retirement.html
    
    This feature removal was scheduled for 2.6.37 back in September 2009.
    
    Signed-off-by: Alok N Kataria <akataria@vmware.com>
    LKML-Reference: <1282600151.19396.22.camel@ank32.eng.vmware.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -84,7 +84,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/dmi.h>
 #include <asm/io_apic.h>
 #include <asm/ist.h>
-#include <asm/vmi.h>
 #include <asm/setup_arch.h>
 #include <asm/bios_ebda.h>
 #include <asm/cacheflush.h>
@@ -735,10 +734,10 @@ void __init setup_arch(char **cmdline_p)
 	printk(KERN_INFO "Command line: %s\n", boot_command_line);
 #endif
 
-	/* VMI may relocate the fixmap; do this before touching ioremap area */
-	vmi_init();
-
-	/* OFW also may relocate the fixmap */
+	/*
+	 * If we have OLPC OFW, we might end up relocating the fixmap due to
+	 * reserve_top(), so do this before touching the ioremap area.
+	 */
 	olpc_ofw_detect();
 
 	early_trap_init();
@@ -838,9 +837,6 @@ void __init setup_arch(char **cmdline_p)
 	parse_early_param();
 
 	x86_report_nx();
-
-	/* Must be before kernel pagetables are setup */
-	vmi_activate();
 
 	/* after early param, so could get panic from serial */
 	reserve_early_setup_data();
--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
 #include <asm/mtrr.h>
-#include <asm/vmi.h>
 #include <asm/apic.h>
 #include <asm/setup.h>
 #include <asm/uv/uv.h>
@@ -312,7 +311,6 @@ notrace static void __cpuinit start_secondary(void *unused)
 	__flush_tlb_all();
 #endif
 
-	vmi_bringup();
 	cpu_init();
 	preempt_disable();
 	smp_callin();
