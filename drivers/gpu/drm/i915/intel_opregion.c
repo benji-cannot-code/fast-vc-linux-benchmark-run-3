a074bf81b7c1493441076d01e6c5b4ac0997fe80
Chris Wilson <chris@chris-wilson.co.uk>
2010-08-24 09:02:58 +0100

    drm/i915: Rename i915_opregion.c to intel_opregion.c
    It's part of the generic Intel driver infrastructure so rename it in
    prepreparation for using it for VBT.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_opregion.c
+++ drivers/gpu/drm/i915/intel_opregion.c
@@ -212,7 +212,7 @@ static u32 asle_set_pfit(struct drm_device *dev, u32 pfit)
 	return 0;
 }
 
-void opregion_asle_intr(struct drm_device *dev)
+void intel_opregion_asle_intr(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	struct opregion_asle *asle = dev_priv->opregion.asle;
@@ -274,7 +274,8 @@ static u32 asle_set_backlight_ironlake(struct drm_device *dev, u32 bclp)
 	return 0;
 }
 
-void ironlake_opregion_gse_intr(struct drm_device *dev)
+/* Only present on Ironlake+ */
+void intel_opregion_gse_intr(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	struct opregion_asle *asle = dev_priv->opregion.asle;
@@ -316,7 +317,7 @@ void ironlake_opregion_gse_intr(struct drm_device *dev)
 #define ASLE_PFIT_EN   (1<<2)
 #define ASLE_PFMB_EN   (1<<3)
 
-void opregion_enable_asle(struct drm_device *dev)
+void intel_opregion_enable_asle(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	struct opregion_asle *asle = dev_priv->opregion.asle;
@@ -511,7 +512,7 @@ int intel_opregion_init(struct drm_device *dev, int resume)
 	if (mboxes & MBOX_ASLE) {
 		DRM_DEBUG_DRIVER("ASLE supported\n");
 		opregion->asle = base + OPREGION_ASLE_OFFSET;
-		opregion_enable_asle(dev);
+		intel_opregion_enable_asle(dev);
 	}
 
 	if (!resume)
