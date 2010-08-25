fd201687cfd2b3fe533beb136a034c57ae0acd5c
Ivan Gomez Castellanos <ivan.gomez@ti.com>
2010-08-25 17:08:55 -0500

    staging: tidspbridge: Remove ntfy.c
    As the services directory is going to be removed, this patch is needed.
    
    ntfy.c only contained the function dsp_notifier_event(), and since this
    function calls sync_set_event(), then the dsp_notifier_event() is moved
    to the sync.c file.
    
    Signed-off-by: Ivan Gomez Castellanos <ivan.gomez@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/core/sync.c
+++ drivers/staging/tidspbridge/core/sync.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*  ----------------------------------- This */
 #include <dspbridge/sync.h>
+#include <dspbridge/ntfy.h>
 
 DEFINE_SPINLOCK(sync_lock);
 
@@ -103,3 +104,19 @@ func_end:
 	return status;
 }
 
+/**
+ * dsp_notifier_event() - callback function to nofity events
+ * @this:		pointer to itself struct notifier_block
+ * @event:	event to be notified.
+ * @data:		Currently not used.
+ *
+ */
+int dsp_notifier_event(struct notifier_block *this, unsigned long event,
+							void *data)
+{
+	struct  ntfy_event *ne = container_of(this, struct ntfy_event,
+							noti_block);
+	if (ne->event & event)
+		sync_set_event(&ne->sync_obj);
+	return NOTIFY_OK;
+}
