34c9c6c8c3a2005f3f159334e44e1d13cf726c80
Johannes Berg <johannes.berg@intel.com>
2010-08-25 14:34:01 +0200

    mac80211: fix mesh advertisement
    When a mac80211-based driver advertises mesh mode
    support, this will be advertised to userspace.
    However, if mac80211 was compiled without mesh
    support, then that won't actually be true. Fix
    this by removing the bit for mesh if mesh isn't
    compiled in.
    
    Since this synchronizes what we advertise to
    cfg80211 and actually support, it means we can
    now rely on cfg80211's interface type checks
    and need not check again in mac80211.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -20,33 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rate.h"
 #include "mesh.h"
 
-static bool nl80211_type_check(enum nl80211_iftype type)
-{
-	switch (type) {
-	case NL80211_IFTYPE_ADHOC:
-	case NL80211_IFTYPE_STATION:
-	case NL80211_IFTYPE_MONITOR:
-#ifdef CONFIG_MAC80211_MESH
-	case NL80211_IFTYPE_MESH_POINT:
-#endif
-	case NL80211_IFTYPE_AP:
-	case NL80211_IFTYPE_AP_VLAN:
-	case NL80211_IFTYPE_WDS:
-		return true;
-	default:
-		return false;
-	}
-}
-
-static bool nl80211_params_check(enum nl80211_iftype type,
-				 struct vif_params *params)
-{
-	if (!nl80211_type_check(type))
-		return false;
-
-	return true;
-}
-
 static int ieee80211_add_iface(struct wiphy *wiphy, char *name,
 			       enum nl80211_iftype type, u32 *flags,
 			       struct vif_params *params)
@@ -55,9 +28,6 @@ static int ieee80211_add_iface(struct wiphy *wiphy, char *name,
 	struct net_device *dev;
 	struct ieee80211_sub_if_data *sdata;
 	int err;
-
-	if (!nl80211_params_check(type, params))
-		return -EINVAL;
 
 	err = ieee80211_if_add(local, name, &dev, type, params);
 	if (err || type != NL80211_IFTYPE_MONITOR || !flags)
@@ -85,9 +55,6 @@ static int ieee80211_change_iface(struct wiphy *wiphy,
 
 	if (ieee80211_sdata_running(sdata))
 		return -EBUSY;
-
-	if (!nl80211_params_check(type, params))
-		return -EINVAL;
 
 	ret = ieee80211_if_change_type(sdata, type);
 	if (ret)
