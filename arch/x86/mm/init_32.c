2d703a8cc7123410f98fb843542c97c6b7afb088
Yinghai Lu <yinghai@kernel.org>
2010-08-25 13:39:15 -0700

    bootmem, x86: Add weak version of reserve_bootmem_generic
    It will be used memblock_x86_to_bootmem converting
    
    It is an wrapper for reserve_bootmem, and x86 64bit is using special one.
    
    Also clean up that version for x86_64. We don't need to take care of numa
    path for that, bootmem can handle it how
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -1070,9 +1070,3 @@ void mark_rodata_ro(void)
 #endif
 }
 #endif
-
-int __init reserve_bootmem_generic(unsigned long phys, unsigned long len,
-				   int flags)
-{
-	return reserve_bootmem(phys, len, flags);
-}
