999669020ba581f6ed0959ae4bd7846003bbabcd
Ivan Gomez Castellanos <ivan.gomez@ti.com>
2010-08-25 17:09:00 -0500

    staging: tidspbridge: Remove cfg_get_object()
    As the services directory is going to be removed, the cfg_get_object
    function has also to be removed.
    
    Since the driver object handle is retrieved from the drv_data structure,
    then the word "Registry" is replaced by "driver data" in the comments.
    
    Also, the hdrv_obj is not used in function omap34_xx_bridge_remove(), so
    it is removed.
    
    Signed-off-by: Ivan Gomez Castellanos <ivan.gomez@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/include/dspbridge/cfg.h
+++ drivers/staging/tidspbridge/include/dspbridge/cfg.h
@@ -42,23 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern int cfg_get_cd_version(u32 *version);
 
 /*
- *  ======== cfg_get_object ========
- *  Purpose:
- *      Retrieve the Driver Object handle From the Registry
- *  Parameters:
- *      value:      Ptr to location to store the value.
- *      dw_type      Type of Object to Get
- *  Returns:
- *      0:    Success.
- *  Requires:
- *      CFG initialized.
- *  Ensures:
- *      0:    *value is set to the retrieved u32(non-Zero).
- *      else:       *value is set to 0L.
- */
-extern int cfg_get_object(u32 *value, u8 dw_type);
-
-/*
  *  ======== cfg_get_perf_value ========
  *  Purpose:
  *      Retrieve a flag indicating whether PERF should log statistics for the
