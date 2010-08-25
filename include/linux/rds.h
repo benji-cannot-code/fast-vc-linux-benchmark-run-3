65324099bf7bb0fab14aaa4198781376ab72b18b
Andy Grover <andy.grover@oracle.com>
2010-08-25 09:32:17 -0700

    RDS: Add rds.h to exported headers list
    Also, a number of changes were made based on the assumption that
    rds.h wasn't exported, so roll these back.
    
    Signed-off-by: Andy Grover <andy.grover@oracle.com>

--- include/linux/rds.h
+++ include/linux/rds.h
@@ -96,7 +96,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct rds_info_counter {
 	u_int8_t	name[32];
 	u_int64_t	value;
-} __packed;
+} __attribute__((packed));
 
 #define RDS_INFO_CONNECTION_FLAG_SENDING	0x01
 #define RDS_INFO_CONNECTION_FLAG_CONNECTING	0x02
@@ -111,7 +111,7 @@ struct rds_info_connection {
 	__be32		faddr;
 	u_int8_t	transport[TRANSNAMSIZ];		/* null term ascii */
 	u_int8_t	flags;
-} __packed;
+} __attribute__((packed));
 
 struct rds_info_flow {
 	__be32		laddr;
@@ -119,7 +119,7 @@ struct rds_info_flow {
 	u_int32_t	bytes;
 	__be16		lport;
 	__be16		fport;
-} __packed;
+} __attribute__((packed));
 
 #define RDS_INFO_MESSAGE_FLAG_ACK               0x01
 #define RDS_INFO_MESSAGE_FLAG_FAST_ACK          0x02
@@ -132,7 +132,7 @@ struct rds_info_message {
 	__be16		lport;
 	__be16		fport;
 	u_int8_t	flags;
-} __packed;
+} __attribute__((packed));
 
 struct rds_info_socket {
 	u_int32_t	sndbuf;
@@ -142,7 +142,7 @@ struct rds_info_socket {
 	__be16		connected_port;
 	u_int32_t	rcvbuf;
 	u_int64_t	inum;
-} __packed;
+} __attribute__((packed));
 
 struct rds_info_tcp_socket {
 	__be32          local_addr;
@@ -154,7 +154,7 @@ struct rds_info_tcp_socket {
 	u_int32_t       last_sent_nxt;
 	u_int32_t       last_expected_una;
 	u_int32_t       last_seen_una;
-} __packed;
+} __attribute__((packed));
 
 #define RDS_IB_GID_LEN	16
 struct rds_info_rdma_connection {
