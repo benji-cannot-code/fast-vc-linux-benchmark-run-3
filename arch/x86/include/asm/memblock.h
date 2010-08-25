ad6c5c7373731f7de638cbd7d7f954aad9aa0ab4
Yinghai Lu <yinghai@kernel.org>
2010-08-25 13:39:15 -0700

    x86, memblock: Add memblock_x86_to_bootmem()
    memblock_x86_to_bootmem() will reserve memblock.reserved.region in
    bootmem after bootmem is set up.
    
    We can use it to with all arches that support memblock later.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/memblock.h
+++ arch/x86/include/asm/memblock.h
@@ -5,5 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARCH_DISCARD_MEMBLOCK
 
 u64 memblock_x86_find_in_range_size(u64 start, u64 *sizep, u64 align);
+void memblock_x86_to_bootmem(u64 start, u64 end);
 
 #endif
