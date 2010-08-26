fab0dfc3d39d98d7eeeae0ab51da84dcda0ba3d3
Mark F. Brown <mark.brown314@gmail.com>
2010-08-26 05:07:29 -0400

    ARM: pxa168: fix corrected reset vector
    Reset vector for pxa168 is 0xffff_0000 not 0x0. This fix allows
    reboot to work
    
    Signed-off-by: Mark F. Brown <mark.brown314@gmail.com>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-mmp/include/mach/system.h
+++ arch/arm/mach-mmp/include/mach/system.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_MACH_SYSTEM_H
 #define __ASM_MACH_SYSTEM_H
 
+#include <mach/cputype.h>
+
 static inline void arch_idle(void)
 {
 	cpu_do_idle();
@@ -17,6 +19,9 @@ static inline void arch_idle(void)
 
 static inline void arch_reset(char mode, const char *cmd)
 {
-	cpu_reset(0);
+	if (cpu_is_pxa168())
+		cpu_reset(0xffff0000);
+	else
+		cpu_reset(0);
 }
 #endif /* __ASM_MACH_SYSTEM_H */
