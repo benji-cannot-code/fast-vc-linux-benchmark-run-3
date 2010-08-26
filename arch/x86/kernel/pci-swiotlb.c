e42878687fe630a0f10a8d82b447f7221ae94e41
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2010-08-26 13:58:00 -0400

    x86, swiotlb: Make SWIOTLB use IOMMU_INIT_* macros.
    We utilize the IOMMU_INIT macros to create this dependency:
    
           [pci_xen_swiotlb_detect]
                     |
           [pci_swiotlb_detect_override]
                     |
           [pci_swiotlb_detect_4gb]
    
    And set the SWIOTLB IOMMU_INIT to utilize 'pci_swiotlb_init'
    for .init and 'pci_swiotlb_late_init' for .late_init.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    LKML-Reference: <1282845485-8991-6-git-send-email-konrad.wilk@oracle.com>
    CC: Fujita Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/pci-swiotlb.c
+++ arch/x86/kernel/pci-swiotlb.c
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/iommu.h>
 #include <asm/swiotlb.h>
 #include <asm/dma.h>
-
+#include <asm/xen/swiotlb-xen.h>
+#include <asm/iommu_table.h>
 int swiotlb __read_mostly;
 
 static void *x86_swiotlb_alloc_coherent(struct device *hwdev, size_t size,
@@ -56,6 +57,10 @@ int __init pci_swiotlb_detect_override(void)
 
 	return use_swiotlb;
 }
+IOMMU_INIT_FINISH(pci_swiotlb_detect_override,
+		  pci_xen_swiotlb_detect,
+		  pci_swiotlb_init,
+		  pci_swiotlb_late_init);
 
 /*
  * if 4GB or more detected (and iommu=off not set) return 1
@@ -70,6 +75,10 @@ int __init pci_swiotlb_detect_4gb(void)
 #endif
 	return swiotlb;
 }
+IOMMU_INIT(pci_swiotlb_detect_4gb,
+	   pci_swiotlb_detect_override,
+	   pci_swiotlb_init,
+	   pci_swiotlb_late_init);
 
 void __init pci_swiotlb_init(void)
 {
