1e415df293dad7941c6541d49ae3425f3f6a05cb
Johannes Berg <johannes.berg@intel.com>
2010-08-27 09:44:50 -0700

    iwlwifi: send PAN parameters
    In order for the microcode to be able to handle
    multiple interfaces, we need to give it the PAN
    parameters that state how to allocate the time
    between the two interfaces. Do this, and update
    it wherever necessary.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-scan.c
+++ drivers/net/wireless/iwlwifi/iwl-scan.c
@@ -573,6 +573,9 @@ static void iwl_bg_scan_completed(struct work_struct *work)
 		iwlcore_commit_rxon(priv, ctx);
 
  out:
+	if (priv->cfg->ops->hcmd->set_pan_params)
+		priv->cfg->ops->hcmd->set_pan_params(priv);
+
 	mutex_unlock(&priv->mutex);
 
 	/*
