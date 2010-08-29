d4f1e30f60bb80887f0438f10e63316854e78614
Stephen Rothwell <sfr@canb.auug.org.au>
2010-08-29 17:04:53 +0000

    IPVS: include net/ip6_checksum.h for csum_ipv6_magic
    Fixes this build error:
    
    net/netfilter/ipvs/ip_vs_core.c: In function 'ip_vs_nat_icmp_v6':
    net/netfilter/ipvs/ip_vs_core.c:640: error: implicit declaration of function 'csum_ipv6_magic'
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/ipvs/ip_vs_core.c
+++ net/netfilter/ipvs/ip_vs_core.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/udp.h>
 #include <net/icmp.h>                   /* for icmp_send */
 #include <net/route.h>
+#include <net/ip6_checksum.h>
 
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
