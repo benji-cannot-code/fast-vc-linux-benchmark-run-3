7e314574254e4d40a952e24ec536002d75b4f2dd
Gerrit Renker <gerrit@erg.abdn.ac.uk>
2010-08-29 19:23:14 +0000

    dccp ccid-3: use per-route RTO or TCP RTO as fallback
    This makes RTAX_RTO_MIN also available to CCID-3, replacing the compile-time
    RTO lower bound with a per-route tunable value.
    
    The original Kconfig option solved the problem that a very low RTT (in the
    order of HZ) can trigger too frequent and unnecessary reductions of the
    sending rate.
    
    This tunable does not affect the initial RTO value of 2 seconds specified in
    RFC 5348, section 4.2 and Appendix B. But like the hardcoded Kconfig value,
    it allows to adapt to network conditions.
    
    The same effect as the original Kconfig option of 100ms is now achieved by
    
    > ip route replace to unicast 192.168.0.0/24 rto_min 100j dev eth0
    
    (assuming HZ=1000).
    
    Signed-off-by: Gerrit Renker <gerrit@erg.abdn.ac.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ccids/ccid3.h
+++ net/dccp/ccids/ccid3.h
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "lib/tfrc.h"
 #include "../ccid.h"
 
-/* Two seconds as per RFC 3448 4.2 */
+/* Two seconds as per RFC 5348, 4.2 */
 #define TFRC_INITIAL_TIMEOUT	   (2 * USEC_PER_SEC)
 
 /* In usecs - half the scheduling granularity as per RFC3448 4.6 */
