7effbc90fd4ae9cb675cbec533f61524ff479959
Yusuke Goda <yusuke.goda.sx@renesas.com>
2010-08-30 11:50:19 +0100

    mmc: Allow 2 byte requests in 4-bit mode for tmio_mmc
    Adjust the tmio_mmc block size check to accept 2-byte requests in 4-bit
    mode if the hardware supports it.
    
    Tested with the SDHI hardware block included in sh7724.
    
    Signed-off-by: Yusuke Goda <yusuke.goda.sx@renesas.com>
    Signed-off-by: Matt Fleming <matt@console-pimps.org>
    Acked-by: Magnus Damm <damm@opensource.se>
    Tested-by: Arnd Hannemann <arnd@arndnet.de>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/tmio.h
+++ include/linux/mfd/tmio.h
@@ -53,6 +53,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* tmio MMC platform flags */
 #define TMIO_MMC_WRPROTECT_DISABLE	(1 << 0)
+/*
+ * Some controllers can support a 2-byte block size when the bus width
+ * is configured in 4-bit mode.
+ */
+#define TMIO_MMC_BLKSZ_2BYTES		(1 << 1)
 
 int tmio_core_mmc_enable(void __iomem *cnf, int shift, unsigned long base);
 int tmio_core_mmc_resume(void __iomem *cnf, int shift, unsigned long base);
