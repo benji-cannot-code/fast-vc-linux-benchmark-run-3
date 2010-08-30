8eaee902113e0d56607253d1af22e12a0ba7d0d3
Mats Randgaard <mats.randgaard@tandberg.com>
2010-08-30 10:30:36 -0300

    V4L/DVB: vpif_cap/disp: Removed section mismatch warning
    Signed-off-by: Mats Randgaard <mats.randgaard@tandberg.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@tandberg.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/davinci/vpif_capture.c
+++ drivers/media/video/davinci/vpif_capture.c
@@ -2115,7 +2115,7 @@ static const struct dev_pm_ops vpif_dev_pm_ops = {
 	.resume = vpif_resume,
 };
 
-static struct platform_driver vpif_driver = {
+static __refdata struct platform_driver vpif_driver = {
 	.driver	= {
 		.name	= "vpif_capture",
 		.owner	= THIS_MODULE,
