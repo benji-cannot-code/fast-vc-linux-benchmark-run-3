2ae945d985f60053aa953dfcb3e6cb0b3117df91
jvrao <jvrao@linux.vnet.ibm.com>
2010-08-30 13:23:20 -0500

    fs/9p: Remove the redundant rsize calculation in v9fs_file_write()
    the same calculation is done in p9_client_write
    
    Signed-off-by: Venkateswararao Jujjuri <jvrao@linux.vnet.ibm.com>
    Signed-off-by: Badari Pulavarty <pbadari@us.ibm.com>
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

--- fs/9p/vfs_file.c
+++ fs/9p/vfs_file.c
@@ -222,7 +222,7 @@ v9fs_file_write(struct file *filp, const char __user * data,
 {
 	ssize_t retval;
 	size_t total = 0;
-	int n, rsize;
+	int n;
 	struct p9_fid *fid;
 	struct p9_client *clnt;
 	struct inode *inode = filp->f_path.dentry->d_inode;
@@ -234,8 +234,6 @@ v9fs_file_write(struct file *filp, const char __user * data,
 
 	fid = filp->private_data;
 	clnt = fid->clnt;
-
-	rsize = fid->iounit ? fid->iounit : clnt->msize - P9_IOHDRSZ;
 
 	retval = generic_write_checks(filp, &origin, &count, 0);
 	if (retval)
@@ -249,11 +247,7 @@ v9fs_file_write(struct file *filp, const char __user * data,
 		goto out;
 
 	do {
-		if (count < rsize)
-			rsize = count;
-
-		n = p9_client_write(fid, NULL, data+total, origin+total,
-									rsize);
+		n = p9_client_write(fid, NULL, data+total, origin+total, count);
 		if (n <= 0)
 			break;
 		count -= n;
