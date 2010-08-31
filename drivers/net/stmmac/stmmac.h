020e0544c68986e95aa748d49b7b584694f961e2
David S. Miller <davem@davemloft.net>
2010-08-30 21:55:17 -0700

    stmmac: Fix build warnings.
    In file included from drivers/net/stmmac/stmmac_ethtool.c:30:
    drivers/net/stmmac/stmmac.h:111: warning: 'struct platform_device' declared inside parameter list
    drivers/net/stmmac/stmmac.h:111: warning: its scope is only this definition or declaration, which is probably not what you want
    drivers/net/stmmac/stmmac_main.c: In function 'stmmac_dvr_probe':
    drivers/net/stmmac/stmmac_main.c:1744: warning: cast from pointer to integer of different size
    In file included from drivers/net/stmmac/stmmac_mdio.c:31:
    drivers/net/stmmac/stmmac.h:111: warning: 'struct platform_device' declared inside parameter list
    drivers/net/stmmac/stmmac.h:111: warning: its scope is only this definition or declaration, which is probably not what you want
    drivers/net/stmmac/dwmac1000_core.c: In function 'dwmac1000_dump_regs':
    drivers/net/stmmac/dwmac1000_core.c:56: warning: cast from pointer to integer of different size
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/stmmac/stmmac.h
+++ drivers/net/stmmac/stmmac.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 *******************************************************************************/
 
 #define DRV_MODULE_VERSION	"Apr_2010"
+#include <linux/platform_device.h>
 #include <linux/stmmac.h>
 
 #include "common.h"
