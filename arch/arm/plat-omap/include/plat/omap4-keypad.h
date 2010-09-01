e5362bedf962a71c11d993baef7c31a0637a31f7
Abraham Arce <x0066660@ti.com>
2010-08-31 17:05:27 -0700

    Input: add support for OMAP4 keyboard controller
    OMAP4 keyboard controller includes:
      - built-in scanning algorithm
      - debouncing feature
    
    Driver implementation is based on matrix_keypad.c
    
    Signed-off-by: Syed Rafiuddin <rafiuddin.syed@ti.com>
    Signed-off-by: Abraham Arce <x0066660@ti.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

---
+++ arch/arm/plat-omap/include/plat/omap4-keypad.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef ARCH_ARM_PLAT_OMAP4_KEYPAD_H
+#define ARCH_ARM_PLAT_OMAP4_KEYPAD_H
+
+#include <linux/input/matrix_keypad.h>
+
+struct omap4_keypad_platform_data {
+	const struct matrix_keymap_data *keymap_data;
+
+	u8 rows;
+	u8 cols;
+
+	u16 irq;
+	void __iomem *base;
+};
+
+#endif
