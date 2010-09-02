71e0881095e96b6d78015d2a9e4518f7ea3c7284
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-09-02 09:14:19 +0100

    ARM: 6348/1: s3c64xx: don't put smartq7_leds in .init.data
    This fixes a section mismatch found by modpost:
    
    	WARNING: arch/arm/mach-s3c64xx/built-in.o(.data+0x2c1c): Section mismatch in reference from the variable smartq7_led_data to the (unknown reference) .init.data:(unknown)
    	The variable smartq7_led_data references
    	the (unknown reference) __initdata (unknown)
    	If the reference is valid then annotate the
    	variable with __init* or __refdata (see linux/init.h) or name the variable:
    	*_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s3c64xx/mach-smartq7.c
+++ arch/arm/mach-s3c64xx/mach-smartq7.c
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "mach-smartq.h"
 
-static struct gpio_led smartq7_leds[] __initdata = {
+static struct gpio_led smartq7_leds[] = {
 	{
 		.name			= "smartq7:red",
 		.active_low		= 1,
