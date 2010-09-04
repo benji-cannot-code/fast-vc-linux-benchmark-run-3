9cd68baac323d6d9572998e7709a421a5a459ef7
Sven Eckelmann <sven.eckelmann@gmx.de>
2010-09-05 01:58:31 +0200

    Staging: batman-adv: Prepare vis packets directly inside a skb
    The vis information structure is used in a way that it can be transfered
    directly as packet. It still had to be copied into a skb because of an
    extra buffer used for the actual preparation of the data. This is
    unnecessary and can be replaced by a simple clone instead of an full
    copy before each send.
    
    This makes also the send_raw_packet function obsolete.
    
    Reported-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/batman-adv/send.h
+++ drivers/staging/batman-adv/send.h
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int send_skb_packet(struct sk_buff *skb,
 				struct batman_if *batman_if,
 				uint8_t *dst_addr);
-void send_raw_packet(unsigned char *pack_buff, int pack_buff_len,
-		     struct batman_if *batman_if, uint8_t *dst_addr);
 void schedule_own_packet(struct batman_if *batman_if);
 void schedule_forward_packet(struct orig_node *orig_node,
 			     struct ethhdr *ethhdr,
