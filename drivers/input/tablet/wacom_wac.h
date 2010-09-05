803d48418adc0a6cf4c3ddd11aff898cb4b4b8e5
Henrik Rydberg <rydberg@euromail.se>
2010-09-05 12:57:13 -0700

    Input: wacom - add a quirk for low resolution Bamboo devices
    The Bamboo Touch reports a sub-screen resolution of 480x320.  The
    signal-to-noise ratio is only about 100, so filtering is needed in
    order to reduce the jitter to a usable level. However, the low
    resolution leads to round-off errors in the EWMA filter, resulting in
    extremely jerky pointer motion. This patch explicitly sets a higher
    resolution for those devices, and tells this to the completion handler
    via a low-resolution quirk.
    
    Signed-off-by: Henrik Rydberg <rydberg@euromail.se>
    Acked-by: Ping Cheng <pingc@wacom.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/tablet/wacom_wac.h
+++ drivers/input/tablet/wacom_wac.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* device quirks */
 #define WACOM_QUIRK_MULTI_INPUT		0x0001
+#define WACOM_QUIRK_BBTOUCH_LOWRES	0x0002
 
 /* largest reported tracking id */
 #define MAX_TRACKING_ID			0xfff
