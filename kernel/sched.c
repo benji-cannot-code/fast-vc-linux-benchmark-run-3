e7c3bb389a12d0071bc9b6c9251dc0468f78516e
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2010-09-06 16:37:05 +0200

    sched: Remove unnecessary #ifdef CONFIG_SMP
    The CONFIG_SMP ifdef isn't necessary at this point, because it
    is checked in an outer ifdef level already and has no effect
    here.
    
    Cleanup only, no functional effect.
    
    Signed-off-by: Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
    Cc: vamos-dev@i4.informatik.uni-erlangen.de
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Tejun Heo <tj@kernel.org>
    LKML-Reference: <7a3a39ef3f765a4473cb026b1f204059568a7098.1283782701.git.qy03fugy@stud.informatik.uni-erlangen.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -427,9 +427,7 @@ struct root_domain {
 	 */
 	cpumask_var_t rto_mask;
 	atomic_t rto_count;
-#ifdef CONFIG_SMP
 	struct cpupri cpupri;
-#endif
 };
 
 /*
@@ -438,7 +436,7 @@ struct root_domain {
  */
 static struct root_domain def_root_domain;
 
-#endif
+#endif /* CONFIG_SMP */
 
 /*
  * This is the main, per-CPU runqueue data structure.
