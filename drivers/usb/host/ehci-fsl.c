6cb96412bc39e35959a140b810bf3349d165ab1d
Matthieu CASTET <matthieu.castet@parrot.com>
2010-09-06 18:26:56 +0200

    USB: ehci tdi : let's tdi_reset set host mode
    tdi_reset is already taking care of setting host mode for tdi devices.
    Don't duplicate code in platform driver.
    
    Make ehci_halt a nop if the controller is not in host mode (otherwise it 
    will fail), and let's ehci_reset do the tdi_reset.
    We need to move hcd->has_tt flags before ehci_halt, in order ehci_halt 
    knows we are a tdi device.
    
    
    Before the setup routine was doing :
    - put controller in host mode
    - ehci_halt
    - ehci_init
    - hcd->has_tt = 1;
    - ehci_reset
    
    Now we do :
    - hcd->has_tt = 1;
    - ehci_halt
    - ehci_init
    - ehci_reset
    
    PS : now we handle correctly the device -> host transition.
    
    Signed-off-by: Matthieu CASTET <matthieu.castet@parrot.com>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/ehci-fsl.c
+++ drivers/usb/host/ehci-fsl.c
@@ -234,8 +234,6 @@ static void mpc83xx_usb_setup(struct usb_hcd *hcd)
 			mpc83xx_setup_phy(ehci, pdata->phy_mode, 1);
 	}
 
-	/* put controller in host mode. */
-	ehci_writel(ehci, 0x00000003, non_ehci + FSL_SOC_USB_USBMODE);
 #ifdef CONFIG_PPC_85xx
 	out_be32(non_ehci + FSL_SOC_USB_PRICTRL, 0x00000008);
 	out_be32(non_ehci + FSL_SOC_USB_AGECNTTHRSH, 0x00000080);
@@ -271,6 +269,8 @@ static int ehci_fsl_setup(struct usb_hcd *hcd)
 	/* cache this readonly data; minimize chip reads */
 	ehci->hcs_params = ehci_readl(ehci, &ehci->caps->hcs_params);
 
+	hcd->has_tt = 1;
+
 	retval = ehci_halt(ehci);
 	if (retval)
 		return retval;
@@ -279,8 +279,6 @@ static int ehci_fsl_setup(struct usb_hcd *hcd)
 	retval = ehci_init(hcd);
 	if (retval)
 		return retval;
-
-	hcd->has_tt = 1;
 
 	ehci->sbrn = 0x20;
 
--- drivers/usb/host/ehci-fsl.h
+++ drivers/usb/host/ehci-fsl.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	PORT_PTS_SERIAL		(3<<30)
 #define PORT_PTS_PTW		(1<<28)
 #define FSL_SOC_USB_PORTSC2	0x188
-#define FSL_SOC_USB_USBMODE	0x1a8
 #define FSL_SOC_USB_SNOOP1	0x400	/* NOTE: big-endian */
 #define FSL_SOC_USB_SNOOP2	0x404	/* NOTE: big-endian */
 #define FSL_SOC_USB_AGECNTTHRSH	0x408	/* NOTE: big-endian */
