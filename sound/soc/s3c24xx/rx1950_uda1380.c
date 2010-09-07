a5a43085b25b6ca5e3a129c1ac7c1db0ef7fff1b
Vasily Khoruzhick <anarsoul@gmail.com>
2010-09-07 17:04:16 +0300

    ASoC: rx1950: remove unnecessary headers
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/rx1950_uda1380.c
+++ sound/soc/s3c24xx/rx1950_uda1380.c
@@ -20,18 +20,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/timer.h>
-#include <linux/delay.h>
-#include <linux/interrupt.h>
 #include <linux/platform_device.h>
-#include <linux/spinlock.h>
 #include <linux/i2c.h>
 #include <linux/gpio.h>
 #include <linux/clk.h>
 
-#include <sound/core.h>
-#include <sound/pcm.h>
-#include <sound/pcm_params.h>
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
 #include <sound/uda1380.h>
