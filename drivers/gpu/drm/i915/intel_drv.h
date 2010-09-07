c75023c03f1211ff0d4b6aa427dba7f7b42a3b9b
Chris Wilson <chris@chris-wilson.co.uk>
2010-09-07 20:54:59 +0100

    drm/i915: Use the real FDI frequency for determining b/w
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -50,6 +50,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define wait_for(COND, MS) _wait_for(COND, MS, 1)
 #define wait_for_atomic(COND, MS) _wait_for(COND, MS, 0)
 
+#define KHz(x) (1000*x)
+#define MHz(x) KHz(1000*x)
+
 /*
  * Display related stuff
  */
