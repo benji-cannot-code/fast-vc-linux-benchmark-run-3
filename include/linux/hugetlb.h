26585369d137e40350e802940a1fc35ac0169f31
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2010-09-08 10:19:33 +0900

    hugetlb: add allocate function for hugepage migration
    We can't use existing hugepage allocation functions to allocate hugepage
    for page migration, because page migration can happen asynchronously with
    the running processes and page migration users should call the allocation
    function with physical addresses (not virtual addresses) as arguments.
    
    ChangeLog since v3:
    - unify alloc_buddy_huge_page() and alloc_buddy_huge_page_node()
    
    ChangeLog since v2:
    - remove unnecessary get/put_mems_allowed() (thanks to David Rientjes)
    
    ChangeLog since v1:
    - add comment on top of alloc_huge_page_no_vma()
    
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Acked-by: Mel Gorman <mel@csn.ul.ie>
    Signed-off-by: Jun'ichi Nomura <j-nomura@ce.jp.nec.com>
    Reviewed-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -229,6 +229,8 @@ struct huge_bootmem_page {
 	struct hstate *hstate;
 };
 
+struct page *alloc_huge_page_node(struct hstate *h, int nid);
+
 /* arch callback */
 int __init alloc_bootmem_huge_page(struct hstate *h);
 
@@ -304,6 +306,7 @@ static inline struct hstate *page_hstate(struct page *page)
 
 #else
 struct hstate {};
+#define alloc_huge_page_node(h, nid) NULL
 #define alloc_bootmem_huge_page(h) NULL
 #define hstate_file(f) NULL
 #define hstate_vma(v) NULL
