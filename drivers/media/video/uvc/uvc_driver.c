957e0db965ceb85ada237658d5d9df7e7b97b5d8
Martin Rubli <martin_rubli@logitech.com>
2010-09-08 04:15:23 -0300

    [media] uvcvideo: Remove sysadmin requirements for UVCIOC_CTRL_MAP
    This patch removes the sysadmin requirements for UVCIOC_CTRL_MAP (and the stub
    implementation of UVCIOC_CTRL_ADD). This requirement no longer makes sense with
    the new XU control access mechanisms since XU controls can be accessed without
    adding control mappings first.
    
    A maximum number (currently 1024) of control mappings per device is enforced to
    avoid excess memory consumption caused by careless user space applications.
    
    Signed-off-by: Martin Rubli <martin_rubli@logitech.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/uvc/uvc_driver.c
+++ drivers/media/video/uvc/uvc_driver.c
@@ -1761,6 +1761,7 @@ static int uvc_probe(struct usb_interface *intf,
 	INIT_LIST_HEAD(&dev->streams);
 	atomic_set(&dev->nstreams, 0);
 	atomic_set(&dev->users, 0);
+	atomic_set(&dev->nmappings, 0);
 
 	dev->udev = usb_get_dev(udev);
 	dev->intf = usb_get_intf(intf);
