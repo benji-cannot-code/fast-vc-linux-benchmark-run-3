1464602882596d8217699b387b8affc421c6e2d3
Frederic Weisbecker <fweisbec@gmail.com>
2010-09-08 14:26:00 +0200

    irq: Fix circular headers dependency
    asm-generic/hardirq.h needs asm/irq.h which might include
    linux/interrupt.h as in the sparc 32 case. At this point
    we need irq_cpustat generic definitions, but those are
    included later in asm-generic/hardirq.h.
    
    Then delay a bit the inclusion of irq.h from
    asm-generic/hardirq.h, it doesn't need to be included early.
    
    This fixes:
    
     include/linux/interrupt.h: In function '__raise_softirq_irqoff':
     include/linux/interrupt.h:414: error: implicit declaration of function 'local_softirq_pending'
     include/linux/interrupt.h:414: error: lvalue required as left operand of assignment
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Lai Jiangshan <laijs@cn.fujitsu.com>
    Cc: Koki Sanagi <sanagi.koki@jp.fujitsu.com>
    Cc: mathieu.desnoyers@efficios.com
    Cc: rostedt@goodmis.org
    Cc: nhorman@tuxdriver.com
    Cc: scott.a.mcmillan@intel.com
    Cc: eric.dumazet@gmail.com
    Cc: kaneshige.kenji@jp.fujitsu.com
    Cc: davem@davemloft.net
    Cc: izumi.taku@jp.fujitsu.com
    Cc: kosaki.motohiro@jp.fujitsu.com
    LKML-Reference: <20100908122557.GA5310@nowhere>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-generic/hardirq.h
+++ include/asm-generic/hardirq.h
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/cache.h>
 #include <linux/threads.h>
-#include <linux/irq.h>
 
 typedef struct {
 	unsigned int __softirq_pending;
 } ____cacheline_aligned irq_cpustat_t;
 
 #include <linux/irq_cpustat.h>	/* Standard mappings for irq_cpustat_t above */
+#include <linux/irq.h>
 
 #ifndef ack_bad_irq
 static inline void ack_bad_irq(unsigned int irq)
