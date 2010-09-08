45668e512cfc8299484b96cb3eab7de6d2df2c48
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2010-09-08 17:13:55 +0000

    viafb: vt1636 cleanup
    This patch merges tbl1636 into vt1636 and cleans it up as the data was
    only used there anyway. No runtime changes are expected.
    
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Cc: Joseph Chan <JosephChan@via.com.tw>

--- drivers/video/via/global.h
+++ drivers/video/via/global.h
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "via_utility.h"
 #include "vt1636.h"
 #include "tblDPASetting.h"
-#include "tbl1636.h"
 
 /* External struct*/
 
