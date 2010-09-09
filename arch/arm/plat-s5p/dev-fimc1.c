fde7e0ec7dab38b8b28c2db2769dbb39f486a86f
Marek Szyprowski <m.szyprowski@samsung.com>
2010-09-09 21:29:52 +0900

    ARM: S5P: Fix DMA coherent mask for FIMC
    FIMC driver uses DMA_coherent allocator, which requires proper dma mask
    to be set.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    [kgene.kim@samsung.com: minor title fix]
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-s5p/dev-fimc1.c
+++ arch/arm/plat-s5p/dev-fimc1.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/kernel.h>
+#include <linux/dma-mapping.h>
 #include <linux/platform_device.h>
 #include <linux/interrupt.h>
 #include <linux/ioport.h>
@@ -29,9 +30,15 @@ static struct resource s5p_fimc1_resource[] = {
 	},
 };
 
+static u64 s5p_fimc1_dma_mask = DMA_BIT_MASK(32);
+
 struct platform_device s5p_device_fimc1 = {
 	.name		= "s5p-fimc",
 	.id		= 1,
 	.num_resources	= ARRAY_SIZE(s5p_fimc1_resource),
 	.resource	= s5p_fimc1_resource,
+	.dev		= {
+		.dma_mask		= &s5p_fimc1_dma_mask,
+		.coherent_dma_mask	= DMA_BIT_MASK(32),
+	},
 };
