f6891cd32df41b1712f9b0c76b47532a090a5f0a
Johan Hovold <jhovold@gmail.com>
2010-09-12 16:31:45 +0200

    USB: ftdi_sio: revert "USB: ftdi_sio: fix DTR/RTS line modes"
    This reverts commit 6a1a82df91fa0eb1cc76069a9efe5714d087eccd.
    
    RTS and DTR should not be modified based on CRTSCTS when calling
    set_termios.
    
    Modem control lines are raised at port open by the tty layer and should stay
    raised regardless of whether hardware flow control is enabled or not.
    
    This is in conformance with the way serial ports work today and many
    applications depend on this behaviour to be able to talk to hardware
    implementing hardware flow control (without the applications actually using
    it).
    
    Hardware which expects different behaviour on these lines can always
    use TIOCMSET/TIOCMBI[SC] after port open to change them.
    
    Reported-by: Daniel Mack <daniel@caiaq.de>
    Reported-by: Dave Mielke <dave@mielke.cc>
    Signed-off-by: Johan Hovold <jhovold@gmail.com>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -2030,8 +2030,6 @@ static void ftdi_set_termios(struct tty_struct *tty,
 				"urb failed to set to rts/cts flow control\n");
 		}
 
-		/* raise DTR/RTS */
-		set_mctrl(port, TIOCM_DTR | TIOCM_RTS);
 	} else {
 		/*
 		 * Xon/Xoff code
@@ -2079,8 +2077,6 @@ static void ftdi_set_termios(struct tty_struct *tty,
 			}
 		}
 
-		/* lower DTR/RTS */
-		clear_mctrl(port, TIOCM_DTR | TIOCM_RTS);
 	}
 	return;
 }
