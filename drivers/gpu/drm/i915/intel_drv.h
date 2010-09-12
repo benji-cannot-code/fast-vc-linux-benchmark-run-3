eb739ff2609ee6ff5b57b26b7f7641494110b0f4
Chris Wilson <chris@chris-wilson.co.uk>
2010-09-12 13:34:08 +0100

    drm/i915: Use msleep instead of mdelay during wait_vblank_off
    Avoid a potentially long busy-wait if we not in the process of
    atomically switching to the kdb console.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -50,6 +50,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define wait_for(COND, MS) _wait_for(COND, MS, 1)
 #define wait_for_atomic(COND, MS) _wait_for(COND, MS, 0)
 
+#define MSLEEP(x) do { \
+	if (in_dbg_master()) \
+	       	mdelay(x); \
+	else \
+		msleep(x); \
+} while(0)
+
 #define KHz(x) (1000*x)
 #define MHz(x) KHz(1000*x)
 
