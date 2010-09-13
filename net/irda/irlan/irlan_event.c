60959d1609b5b32a2ba9913a7789360936bc49b0
Joe Perches <joe@perches.com>
2010-09-13 18:24:01 +0000

    net/irda: Use static const char * const where possible
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/irda/irlan/irlan_event.c
+++ net/irda/irlan/irlan_event.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <net/irda/irlan_event.h>
 
-char *irlan_state[] = {
+const char * const irlan_state[] = {
 	"IRLAN_IDLE",
 	"IRLAN_QUERY",
 	"IRLAN_CONN",
