eaadd175c0783c0e6a45eb16891f3e08c56baaa2
Stanislaw Gruszka <sgruszka@redhat.com>
2010-09-13 14:46:45 +0200

    iwlwifi: apply settings when finishing scan
    Even is someone else complete scanning in mac80211, apply rxon and
    tx power settings if gets scan complete notification from hardware.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Acked-by: Wey-Yi W Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-scan.c
+++ drivers/net/wireless/iwlwifi/iwl-scan.c
@@ -574,7 +574,7 @@ static void iwl_bg_scan_completed(struct work_struct *work)
 
 	if (!test_and_clear_bit(STATUS_SCANNING, &priv->status)) {
 		IWL_DEBUG_SCAN(priv, "Scan already completed.\n");
-		goto out;
+		goto out_settings;
 	}
 
 	if (priv->is_internal_short_scan && !aborted) {
@@ -600,6 +600,7 @@ static void iwl_bg_scan_completed(struct work_struct *work)
 out_complete:
 	iwl_complete_scan(priv, aborted);
 
+out_settings:
 	/* Can we still talk to firmware ? */
 	if (!iwl_is_ready_rf(priv))
 		goto out;
