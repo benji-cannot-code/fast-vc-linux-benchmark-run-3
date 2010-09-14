c2a12aeaedc34aa3dc3f6137a34947da320d8fc9
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2010-09-13 23:53:55 -0700

    Input: serio - do not include linux/freezer.h
    Commit ea486e681188d64c6a101d8d06414aef0f9f0cd3 changed kseriod thread
    to become not freezable so we do not need this include anymore.
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/serio/serio.c
+++ drivers/input/serio/serio.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/kthread.h>
 #include <linux/mutex.h>
-#include <linux/freezer.h>
 
 MODULE_AUTHOR("Vojtech Pavlik <vojtech@ucw.cz>");
 MODULE_DESCRIPTION("Serio abstraction core");
