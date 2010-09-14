25531d540fdf0834b072a5af70e7aaf2fb03efdd
Jason Cooper <jason@lakedaemon.net>
2010-09-14 09:45:36 -0400

    staging: brcm80211: fix "ERROR: while should follow close brace '}'"
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/util/bcmutils.c
+++ drivers/staging/brcm80211/util/bcmutils.c
@@ -681,8 +681,7 @@ char *bcmstrtok(char **string, const char *delimiters, char *tokdelim)
 	/* Set bits in delimiter table */
 	do {
 		map[*delimiters >> 5] |= (1 << (*delimiters & 31));
-	}
-	while (*delimiters++);
+	} while (*delimiters++);
 
 	str = (unsigned char *)*string;
 
