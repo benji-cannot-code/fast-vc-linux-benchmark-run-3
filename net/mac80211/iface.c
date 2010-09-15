d05b93f7d168e02a94130adb28a6a1d1f989b919
Johannes Berg <johannes.berg@intel.com>
2010-09-15 15:13:13 +0200

    mac80211: set running state earlier
    When an interface is brought up, the recent changes
    to allow changing type-while-up only set the running
    bit after everything was done. This broke a number
    of things, including idle calculation for monitor
    interfaces, and it also broke WDS station insertion
    (although nobody noticed yet).
    
    Thus, change the code to set the running bit earlier,
    but keep it after the driver's add_interface was
    called because otherwise drivers may iterate over
    interfaces they haven't fully set up yet.
    
    Reported-by: Rajkumar Manoharan <rmanoharan@atheros.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -281,6 +281,8 @@ static int ieee80211_do_open(struct net_device *dev, bool coming_up)
 			netif_carrier_on(dev);
 	}
 
+	set_bit(SDATA_STATE_RUNNING, &sdata->state);
+
 	if (sdata->vif.type == NL80211_IFTYPE_WDS) {
 		/* Create STA entry for the WDS peer */
 		sta = sta_info_alloc(sdata, sdata->u.wds.remote_addr,
@@ -332,8 +334,6 @@ static int ieee80211_do_open(struct net_device *dev, bool coming_up)
 
 	netif_tx_start_all_queues(dev);
 
-	set_bit(SDATA_STATE_RUNNING, &sdata->state);
-
 	return 0;
  err_del_interface:
 	drv_remove_interface(local, &sdata->vif);
@@ -344,6 +344,7 @@ static int ieee80211_do_open(struct net_device *dev, bool coming_up)
 	sdata->bss = NULL;
 	if (sdata->vif.type == NL80211_IFTYPE_AP_VLAN)
 		list_del(&sdata->u.vlan.list);
+	clear_bit(SDATA_STATE_RUNNING, &sdata->state);
 	return res;
 }
 
