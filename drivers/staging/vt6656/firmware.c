48e75410f80a626d22f925e4c05ecb87f7dbfc95
Diego H. Iglesias <diegohi@gmail.com>
2010-09-16 15:14:18 -0300

    staging: vt6656: removed not required parentheses.
    Removed not required parentheses in return statements.
    
    Signed-off-by: Diego H. Iglesias <diegohi@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6656/firmware.c
+++ drivers/staging/vt6656/firmware.c
@@ -807,7 +807,7 @@ FIRMWAREbDownload(
                 if (pBuffer)
                     kfree(pBuffer);
                 spin_lock_irq(&pDevice->lock);
-                return (FALSE);
+		return FALSE;
             }
         }
     }
