e288a16fe05453e631ff6a72cd6c8b32d800c71c
Hans J. Koch <hjk@linutronix.de>
2010-09-17 18:17:42 +0200

    ARM: Add TCC8xxx system timer
    Add the system timer using clockevents with the internal TC32 timer.
    This also adds a clocksource using the same timer.
    
    Signed-off-by: "Hans J. Koch" <hjk@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/arm/mach-tcc8k/common.h
+++ arch/arm/mach-tcc8k/common.h
@@ -2,7 +2,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef MACH_TCC8K_COMMON_H
 #define MACH_TCC8K_COMMON_H
 
+struct clk;
+
 extern void tcc_clocks_init(unsigned long xi_freq, unsigned long xti_freq);
+extern void tcc8k_timer_init(struct clk *clock, void __iomem *base, int irq);
 extern void tcc8k_init_irq(void);
 
 #endif
