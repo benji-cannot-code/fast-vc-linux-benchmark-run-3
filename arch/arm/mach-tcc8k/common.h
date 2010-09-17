2f2951110ff0d02922e5dde65d38507f8fed9d19
Hans J. Koch <hjk@linutronix.de>
2010-09-17 18:15:11 +0200

    ARM: Add the clock framework for Telechips TCC8xxx processors.
    This adds definitions and low-level functions to handle clocks in
    TCC8xxx processors.
    
    Signed-off-by: "Hans J. Koch" <hjk@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

---
+++ arch/arm/mach-tcc8k/common.h
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef MACH_TCC8K_COMMON_H
+#define MACH_TCC8K_COMMON_H
+
+extern void tcc_clocks_init(unsigned long xi_freq, unsigned long xti_freq);
+
+#endif
