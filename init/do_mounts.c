0b76813c3adb0df0cf07f61a1bef72f4612ef51d
Jens Axboe <jaxboe@fusionio.com>
2010-09-17 10:00:46 +0200

    do_mounts: only enable PARTUUID for CONFIG_BLOCK
    When CONFIG_BLOCK is not enabled:
    
    init/do_mounts.c:71: error: implicit declaration of function 'dev_to_part'
    init/do_mounts.c:71: warning: initialization makes pointer from integer without a cast
    init/do_mounts.c:73: error: dereferencing pointer to incomplete type
    init/do_mounts.c:76: error: dereferencing pointer to incomplete type
    init/do_mounts.c:76: error: dereferencing pointer to incomplete type
    init/do_mounts.c:102: error: implicit declaration of function 'part_pack_uuid'
    init/do_mounts.c:104: error: 'block_class' undeclared (first use in this function)
    
    Reported-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -59,6 +59,7 @@ static int __init readwrite(char *str)
 __setup("ro", readonly);
 __setup("rw", readwrite);
 
+#ifdef CONFIG_BLOCK
 /**
  * match_dev_by_uuid - callback for finding a partition using its uuid
  * @dev:	device passed in by the caller
@@ -112,6 +113,7 @@ static dev_t __init devt_from_partuuid(char *uuid_str)
 done:
 	return res;
 }
+#endif
 
 /*
  *	Convert a name into device number.  We accept the following variants:
@@ -139,6 +141,7 @@ dev_t name_to_dev_t(char *name)
 	dev_t res = 0;
 	int part;
 
+#ifdef CONFIG_BLOCK
 	if (strncmp(name, "PARTUUID=", 9) == 0) {
 		name += 9;
 		if (strlen(name) != 36)
@@ -148,6 +151,7 @@ dev_t name_to_dev_t(char *name)
 			goto fail;
 		goto done;
 	}
+#endif
 
 	if (strncmp(name, "/dev/", 5) != 0) {
 		unsigned maj, min;
