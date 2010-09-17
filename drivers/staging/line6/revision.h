22c004eb095438373f91022bef766861e1754ae9
Markus Grabner <grabner@icg.tugraz.at>
2010-09-17 23:33:25 +0200

    Staging: line6: workaround for null pointer bug
    Signed-off-by: Markus Grabner <grabner@icg.tugraz.at>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/line6/revision.h
+++ drivers/staging/line6/revision.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DRIVER_REVISION
 /* current subversion revision */
-#define DRIVER_REVISION " (revision 684)"
+#define DRIVER_REVISION " (revision 690)"
 #endif
