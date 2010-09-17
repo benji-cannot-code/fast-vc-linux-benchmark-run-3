3b5975c6926d23b9feb5dbd1a1109283ac3766bb
Charles Clément <caratorn@gmail.com>
2010-09-17 11:46:29 +0200

    Staging: vt6655: remove unused update_BssList definition
    Signed-off-by: Charles Clément <caratorn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vt6655/ttype.h
+++ drivers/staging/vt6655/ttype.h
@@ -38,13 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TxInSleep
 #endif
 
-//2007-0809-01<Add>by MikeLiu
-#ifndef  update_BssList
-#define update_BssList
-#endif
-
-
-
 #ifndef WPA_SM_Transtatus
 #define WPA_SM_Transtatus
 #endif
