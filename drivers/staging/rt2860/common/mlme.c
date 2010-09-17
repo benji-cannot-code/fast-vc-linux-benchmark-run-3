a481b186a70dfc284c1cd5e23e538cabdbe33d7d
Andy Shevchenko <andy.shevchenko@gmail.com>
2010-09-17 11:24:43 +0300

    staging: rt2860: '&pointer[0]' to 'pointer' fix
    Signed-off-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2860/common/mlme.c
+++ drivers/staging/rt2860/common/mlme.c
@@ -1538,8 +1538,7 @@ void MlmeAutoReconnectLastSSID(struct rt_rtmp_adapter *pAd)
 	if (pAd->StaCfg.bAutoConnectByBssid) {
 		DBGPRINT(RT_DEBUG_TRACE,
 			("Driver auto reconnect to last OID_802_11_BSSID "
-				"setting - %pM\n",
-				&pAd->MlmeAux.Bssid[0]));
+				"setting - %pM\n", pAd->MlmeAux.Bssid));
 
 		pAd->MlmeAux.Channel = pAd->CommonCfg.Channel;
 		MlmeEnqueue(pAd,
