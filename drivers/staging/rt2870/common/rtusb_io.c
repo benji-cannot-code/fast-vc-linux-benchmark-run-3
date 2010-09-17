4cc0098eb71448399f946cc0b020228e3573f3f9
Andy Shevchenko <andy.shevchenko@gmail.com>
2010-09-17 11:24:48 +0300

    staging: rt2870: '&pointer[0]' to 'pointer' fix
    Signed-off-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2870/common/rtusb_io.c
+++ drivers/staging/rt2870/common/rtusb_io.c
@@ -1883,7 +1883,7 @@ void CMDHandler(struct rt_rtmp_adapter *pAd)
 						("UpdateRxWCIDTable(): Aid=%d, "
 							"Addr=%pM!\n",
 							pEntry->Aid,
-							&pEntry->Addr[0]));
+							pEntry->Addr));
 				}
 				break;
 
