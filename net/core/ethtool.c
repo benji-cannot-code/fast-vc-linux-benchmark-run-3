c6ddf65135a9831fc3dd2dac5545d5b8c408842e
Eric Dumazet <eric.dumazet@gmail.com>
2010-09-17 11:56:18 -0700

    ethtool: change ethtool_set_gro() to use ethtool_op_get_rx_csum
    To be able to switch on GRO on a device, ethtool_set_gro() checks this
    device provides a get_rx_csum() method.
    
    Some devices dont provide this method, while they do support RX
    checksumming.
    
    This patch allows bonding to support GRO :
    
    ethtool -K bond0 gro on
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/ethtool.c
+++ net/core/ethtool.c
@@ -1182,8 +1182,11 @@ static int ethtool_set_gro(struct net_device *dev, char __user *useraddr)
 		return -EFAULT;
 
 	if (edata.data) {
-		if (!dev->ethtool_ops->get_rx_csum ||
-		    !dev->ethtool_ops->get_rx_csum(dev))
+		u32 rxcsum = dev->ethtool_ops->get_rx_csum ?
+				dev->ethtool_ops->get_rx_csum(dev) :
+				ethtool_op_get_rx_csum(dev);
+
+		if (!rxcsum)
 			return -EINVAL;
 		dev->features |= NETIF_F_GRO;
 	} else
