c95b13e816d49c7d26593fcebd5a8fe556132de0
Hans J. Koch <hjk@linutronix.de>
2010-09-17 18:16:29 +0200

    ARM: Introduce plat-tcc irq framework
    Introduce lowlevel interrupt routines.
    
    Signed-off-by: "Hans J. Koch" <hjk@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/arm/mach-tcc8k/common.h
+++ arch/arm/mach-tcc8k/common.h
@@ -3,5 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MACH_TCC8K_COMMON_H
 
 extern void tcc_clocks_init(unsigned long xi_freq, unsigned long xti_freq);
+extern void tcc8k_init_irq(void);
 
 #endif
