bea081225576560263319112de78b012d79f9027
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2010-09-17 13:48:05 +0900

    ASoC: fsi: Add fsi_dma_get_area
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/sh/fsi.c
+++ sound/soc/sh/fsi.c
@@ -358,6 +358,11 @@ static int fsi_get_fifo_residue(struct fsi_priv *fsi, int is_play)
 	return residue;
 }
 
+static u8 *fsi_dma_get_area(struct fsi_priv *fsi)
+{
+	return fsi->substream->runtime->dma_area + fsi->byte_offset;
+}
+
 /************************************************************************
 
 
@@ -551,8 +556,7 @@ static int fsi_data_push(struct fsi_priv *fsi, int startup)
 	if (fifo_free < send)
 		send = fifo_free;
 
-	start = runtime->dma_area;
-	start += fsi->byte_offset;
+	start = fsi_dma_get_area(fsi);
 
 	switch (width) {
 	case 2:
@@ -634,8 +638,7 @@ static int fsi_data_pop(struct fsi_priv *fsi, int startup)
 	if (free < fifo_fill)
 		fifo_fill = free;
 
-	start = runtime->dma_area;
-	start += fsi->byte_offset;
+	start = fsi_dma_get_area(fsi);
 
 	switch (width) {
 	case 2:
