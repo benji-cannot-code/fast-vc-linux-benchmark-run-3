c8ce5a8cf175386c043e29929bb053369d7b302a
Mike McCormack <mikem@ring3k.org>
2010-09-20 23:11:49 +0900

    staging: rtl8192e: Remove empty function force_pci_posting()
    Signed-off-by: Mike McCormack <mikem@ring3k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192e/r8180_93cx6.c
+++ drivers/staging/rtl8192e/r8180_93cx6.c
@@ -31,7 +31,6 @@ static void eprom_cs(struct net_device *dev, short bit)
 		write_nic_byte(dev, EPROM_CMD, read_nic_byte(dev, EPROM_CMD)
 			       &~(1<<EPROM_CS_SHIFT)); //disable EPROM
 
-	force_pci_posting(dev);
 	udelay(EPROM_DELAY);
 }
 
@@ -40,11 +39,9 @@ static void eprom_ck_cycle(struct net_device *dev)
 {
 	write_nic_byte(dev, EPROM_CMD,
 		       (1<<EPROM_CK_SHIFT) | read_nic_byte(dev, EPROM_CMD));
-	force_pci_posting(dev);
 	udelay(EPROM_DELAY);
 	write_nic_byte(dev, EPROM_CMD,
 		       read_nic_byte(dev, EPROM_CMD) & ~(1<<EPROM_CK_SHIFT));
-	force_pci_posting(dev);
 	udelay(EPROM_DELAY);
 }
 
@@ -58,7 +55,6 @@ static void eprom_w(struct net_device *dev, short bit)
 		write_nic_byte(dev, EPROM_CMD, read_nic_byte(dev, EPROM_CMD)
 			       &~(1<<EPROM_W_SHIFT));
 
-	force_pci_posting(dev);
 	udelay(EPROM_DELAY);
 }
 
@@ -100,7 +96,6 @@ u32 eprom_read(struct net_device *dev, u32 addr)
         //enable EPROM programming
 	write_nic_byte(dev, EPROM_CMD,
 		       (EPROM_CMD_PROGRAM<<EPROM_CMD_OPERATING_MODE_SHIFT));
-	force_pci_posting(dev);
 	udelay(EPROM_DELAY);
 
 	if (priv->epromtype == EPROM_93c56) {
