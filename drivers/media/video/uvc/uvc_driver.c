b57b548367356b81ba1c68362a0dd67af8320a45
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2010-09-20 05:53:21 -0300

    [media] uvcvideo: Hardcode the index/selector relationship for XU controls
    Devices advertise XU controls using a bitmask, in which each bit
    corresponds to a control. The control selector, used to query the
    control, isn't available in the USB descriptors.
    
    All known UVC devices use control selectors equal to the control bit
    index plus one. Hardcode that relationship in the driver, making the
    UVCIOC_CTRL_ADD ioctl obsolete. All necessary information about XU
    controls can be obtained by the driver at enumeration time.
    
    The UVCIOC_CTRL_ADD ioctl is still supported for compatibility reasons,
    but now always returns -EEXIST.
    
    Finally, control mappings are now on a per-device basis and no longer
    global.
    
    As this changes the userspace interface, bump the driver version number
    to 1.0.0 (it was about time).
    
    Signed-off-by: Martin Rubli <martin_rubli@logitech.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/uvc/uvc_driver.c
+++ drivers/media/video/uvc/uvc_driver.c
@@ -42,9 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRIVER_AUTHOR		"Laurent Pinchart " \
 				"<laurent.pinchart@ideasonboard.com>"
 #define DRIVER_DESC		"USB Video Class driver"
-#ifndef DRIVER_VERSION
-#define DRIVER_VERSION		"v0.1.0"
-#endif
 
 unsigned int uvc_clock_param = CLOCK_MONOTONIC;
 unsigned int uvc_no_drop_param;
@@ -2290,12 +2287,6 @@ static int __init uvc_init(void)
 {
 	int result;
 
-	INIT_LIST_HEAD(&uvc_driver.devices);
-	INIT_LIST_HEAD(&uvc_driver.controls);
-	mutex_init(&uvc_driver.ctrl_mutex);
-
-	uvc_ctrl_init();
-
 	result = usb_register(&uvc_driver.driver);
 	if (result == 0)
 		printk(KERN_INFO DRIVER_DESC " (" DRIVER_VERSION ")\n");
@@ -2305,7 +2296,6 @@ static int __init uvc_init(void)
 static void __exit uvc_cleanup(void)
 {
 	usb_deregister(&uvc_driver.driver);
-	uvc_ctrl_cleanup();
 }
 
 module_init(uvc_init);
