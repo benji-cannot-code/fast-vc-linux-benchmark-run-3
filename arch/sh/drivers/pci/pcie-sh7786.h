71cfc283fb55e8a90558485a70cc495b7e70b771
Paul Mundt <lethal@linux-sh.org>
2010-09-20 17:10:02 +0900

    sh: pci: Use I/O accessors consistently in SH7786 PCIe init code.
    Some of the existing code is flipping between __raw_xxx() and
    pci_{read,write}_reg(). As the latter are just wrappers for the former,
    flip over to using them consistently.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/drivers/pci/pcie-sh7786.h
+++ arch/sh/drivers/pci/pcie-sh7786.h
@@ -569,6 +569,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PCI_REG(x)		((x) + 0x40000)
 
+#define U64_TO_U32_LOW(val)	((u32)((val) & 0x00000000ffffffffULL))
+#define U64_TO_U32_HIGH(val)	((u32)((val) >> 32))
+#define RES_TO_U32_LOW(val)     \
+	((sizeof(resource_size_t) > sizeof(u32)) ? U64_TO_U32_LOW(val) : (val))
+#define RES_TO_U32_HIGH(val)    \
+	((sizeof(resource_size_t) > sizeof(u32)) ? U64_TO_U32_HIGH(val) : (0))
+
 static inline void
 pci_write_reg(struct pci_channel *chan, unsigned long val, unsigned long reg)
 {
