dd928c557a13876f4e7b1dd717bcac504b8c6552
Rajeev Kulkarni <rajeevk@ti.com>
2010-09-21 10:34:09 -0600

    OMAP4: prcm: Fix global warm reset bit position
    OMAP4 platform has different register bits for Warm and Cold Resets.
    Write one into appropriate bits.
    
    Signed-off-by: Rajeev Kulkarni <rajeevk@ti.com>
    Cc: Leed Aguilar <leed.aguilar@ti.com>
    [b-cousson@ti.com: Change the define with the proper one from omap4 headers]
    Signed-off-by: Benoit Cousson <b-cousson@ti.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Tested-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-omap2/prcm.c
+++ arch/arm/mach-omap2/prcm.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cm.h"
 #include "prm.h"
 #include "prm-regbits-24xx.h"
+#include "prm-regbits-44xx.h"
 
 static void __iomem *prm_base;
 static void __iomem *cm_base;
@@ -162,8 +163,8 @@ void omap_prcm_arch_reset(char mode, const char *cmd)
 		prm_set_mod_reg_bits(OMAP_RST_DPLL3_MASK, prcm_offs,
 						 OMAP2_RM_RSTCTRL);
 	if (cpu_is_omap44xx())
-		prm_set_mod_reg_bits(OMAP_RST_DPLL3_MASK, prcm_offs,
-						 OMAP4_RM_RSTCTRL);
+		prm_set_mod_reg_bits(OMAP4430_RST_GLOBAL_WARM_SW_MASK,
+				     prcm_offs, OMAP4_RM_RSTCTRL);
 }
 
 static inline u32 __omap_prcm_read(void __iomem *base, s16 module, u16 reg)
