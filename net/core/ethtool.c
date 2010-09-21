737edd8e3e5b710064144f4f5e5f7fd572f1d932
David S. Miller <davem@davemloft.net>
2010-09-21 16:12:11 -0700

    ethtool: Fix build due to lack of ethtool.h include.
    net/core/ethtool.c: In function 'ethtool_get_regs':
    net/core/ethtool.c:818:2: error: implicit declaration of function 'vmalloc'
    net/core/ethtool.c:818:9: warning: assignment makes pointer from integer without a cast
    net/core/ethtool.c:833:2: error: implicit declaration of function 'vfree'
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/ethtool.c
+++ net/core/ethtool.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/bitops.h>
 #include <linux/uaccess.h>
+#include <linux/vmalloc.h>
 #include <linux/slab.h>
 
 /*
