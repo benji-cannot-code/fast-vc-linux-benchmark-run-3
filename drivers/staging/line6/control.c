02c3f569708b141a6628bdcc5c7fe635ba3a20b6
Greg Kroah-Hartman <gregkh@suse.de>
2010-09-21 16:18:13 -0700

    staging: line6: remove unneeded EXPORT_SYMBOL() usage
    These symbols don't need to be exported as nothing uses them, so don't.
    
    Cc: Markus Grabner <grabner@icg.tugraz.at>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/line6/control.c
+++ drivers/staging/line6/control.c
@@ -733,8 +733,6 @@ int line6_pod_create_files(int firmware, int type, struct device *dev)
 	return 0;
 }
 
-EXPORT_SYMBOL(line6_pod_create_files);
-
 void line6_pod_remove_files(int firmware, int type, struct device *dev)
 {
 	device_remove_file(dev, &dev_attr_tweak);
@@ -909,8 +907,6 @@ void line6_pod_remove_files(int firmware, int type, struct device *dev)
 			device_remove_file(dev, &dev_attr_band_6_gain__bass);
 }
 
-EXPORT_SYMBOL(line6_pod_remove_files);
-
 int line6_variax_create_files(int firmware, int type, struct device *dev)
 {
 	int err;
@@ -956,8 +952,6 @@ int line6_variax_create_files(int firmware, int type, struct device *dev)
 	return 0;
 }
 
-EXPORT_SYMBOL(line6_variax_create_files);
-
 void line6_variax_remove_files(int firmware, int type, struct device *dev)
 {
 	device_remove_file(dev, &dev_attr_body);
@@ -1000,5 +994,3 @@ void line6_variax_remove_files(int firmware, int type, struct device *dev)
 	device_remove_file(dev, &dev_attr_mix1);
 	device_remove_file(dev, &dev_attr_pickup_wiring);
 }
-
-EXPORT_SYMBOL(line6_variax_remove_files);
