388961b0a6d7a2a10b55cca5c324b281e7a6d52c
Jonathan Cameron <jic23@cam.ac.uk>
2010-09-21 14:40:56 +0100

    staging: iio: Remove unused bit_count from struct iio_scan_el
    The job this was intended to do (never implemented) is now done
    by explicit definition of _type attributes in all drivers
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Manuel Stahl <manuel.stahl@iis.fraunhofer.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/max1363_core.c
+++ drivers/staging/iio/adc/max1363_core.c
@@ -43,11 +43,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Here we claim all are 16 bits. This currently does no harm and saves
  * us a lot of scan element listings */
 
-#define MAX1363_SCAN_EL(number)						\
-	IIO_SCAN_EL_C(in##number, number, IIO_UNSIGNED(16), 0, NULL);
+#define MAX1363_SCAN_EL(number)				\
+	IIO_SCAN_EL_C(in##number, number, 0, NULL);
 #define MAX1363_SCAN_EL_D(p, n, number)					\
-	IIO_SCAN_NAMED_EL_C(in##p##m##in##n, in##p-in##n,		\
-			number, IIO_SIGNED(16), 0, NULL);
+	IIO_SCAN_NAMED_EL_C(in##p##m##in##n, in##p-in##n, number, 0, NULL);
 
 static MAX1363_SCAN_EL(0);
 static MAX1363_SCAN_EL(1);
