65c09e3828ef382943a983de19699cfe84f0a4b9
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2010-09-22 12:24:23 +0530

    fs/9p: Add missing iput in v9fs_vfs_lookup
    Make sure we drop inode reference in the error path
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Venkateswararao Jujjuri <jvrao@linux.vnet.ibm.com>
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

--- fs/9p/vfs_inode.c
+++ fs/9p/vfs_inode.c
@@ -1038,7 +1038,7 @@ static struct dentry *v9fs_vfs_lookup(struct inode *dir, struct dentry *dentry,
 
 	result = v9fs_fid_add(dentry, fid);
 	if (result < 0)
-		goto error;
+		goto error_iput;
 
 inst_out:
 	if (v9ses->cache)
@@ -1049,6 +1049,8 @@ inst_out:
 	d_add(dentry, inode);
 	return NULL;
 
+error_iput:
+	iput(inode);
 error:
 	p9_client_clunk(fid);
 
