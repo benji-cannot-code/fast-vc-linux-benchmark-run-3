8e2105537bd265e17e596d135f0b36cce8a925f2
Nicolas Pitre <nico@fluxnic.net>
2010-09-23 16:52:40 -0400

    ARM: do not define VMALLOC_END relative to PAGE_OFFSET
    VMALLOC_END is supposed to be an absolute value, while PAGE_OFFSET may
    vary depending on the selected user:kernel memory split mode through
    CONFIG_VMSPLIT_*.  In fact, the goal of moving PAGE_OFFSET down is to
    accommodate more directly addressed RAM by the kernel below the vmalloc
    area, and having VMALLOC_END move along PAGE_OFFSET is rather against
    the very reason why PAGE_OFFSET can be moved in the first place.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/mach-h720x/include/mach/vmalloc.h
+++ arch/arm/mach-h720x/include/mach/vmalloc.h
@@ -6,6 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ARCH_ARM_VMALLOC_H
 #define __ARCH_ARM_VMALLOC_H
 
-#define VMALLOC_END       (PAGE_OFFSET + 0x10000000)
+#define VMALLOC_END       0xd0000000
 
 #endif
