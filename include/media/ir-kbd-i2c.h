a5030241e36aadf8cefceba27ebe3d590cc8a405
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-09-23 01:23:10 -0300

    V4L/DVB: saa7134: get rid of I2C_HW_SAA7134
    The only reason for keeping I2C_HW_SAA7134 is to allow setting a
    per-device polling interval. Just move this info to the platform
    data, allowing drivers to change it per device, where needed.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/ir-kbd-i2c.h
+++ include/media/ir-kbd-i2c.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/ir-common.h>
 
+#define DEFAULT_POLLING_INTERVAL	100	/* ms */
+
 struct IR_i2c;
 
 struct IR_i2c {
@@ -15,6 +17,8 @@ struct IR_i2c {
 	u64                    ir_type;
 	/* Used to avoid fast repeating */
 	unsigned char          old;
+
+	u32                    polling_interval; /* in ms */
 
 	struct delayed_work    work;
 	char                   name[32];
@@ -35,8 +39,9 @@ enum ir_kbd_get_key_fn {
 /* Can be passed when instantiating an ir_video i2c device */
 struct IR_i2c_init_data {
 	char			*ir_codes;
-	const char             *name;
-	u64          type; /* IR_TYPE_RC5, etc */
+	const char		*name;
+	u64			type; /* IR_TYPE_RC5, etc */
+	u32			polling_interval; /* 0 means DEFAULT_POLLING_INTERVAL */
 	/*
 	 * Specify either a function pointer or a value indicating one of
 	 * ir_kbd_i2c's internal get_key functions
