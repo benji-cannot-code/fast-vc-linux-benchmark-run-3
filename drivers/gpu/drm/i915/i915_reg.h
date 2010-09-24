281aefd4d76ebfda4d099b96b3c4f898bdcd3a0d
David Härdeman <david@hardeman.nu>
2010-09-24 21:44:32 +0200

    i915: enable AVI infoframe for intel_hdmi.c [v4]
    This patch enables the sending of AVI infoframes in
    drivers/gpu/drm/i915/intel_hdmi.c.
    
    My receiver currently loses sync when the HDMI output on my computer
    (DG45FC motherboard) is switched from 800x600 (the BIOS resolution) to
    1920x1080 as part of the boot. Fixable by switching inputs on the receiver
    a couple of times.
    
    With this patch, my receiver has not lost sync yet (> 40 tries).
    
    Fourth version, now based on drm-intel-next from:
    git://git.kernel.org/pub/scm/linux/kernel/git/ickle/drm-intel.git
    
    Two questions still remain:
    
    I'm assuming that the sdvo hardware also stores a header ECC byte in
    the MSB of the first dword - is this correct?
    
    Does the SDVOB and SDVOC handling in intel_hdmi_set_avi_infoframe()
    look correct?
    
    Signed-off-by: David Härdeman <david@hardeman.nu>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -1354,6 +1354,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   LVDS_B0B3_POWER_DOWN		(0 << 2)
 #define   LVDS_B0B3_POWER_UP		(3 << 2)
 
+/* Video Data Island Packet control */
+#define VIDEO_DIP_DATA		0x61178
+#define VIDEO_DIP_CTL		0x61170
+#define   VIDEO_DIP_ENABLE		(1 << 31)
+#define   VIDEO_DIP_PORT_B		(1 << 29)
+#define   VIDEO_DIP_PORT_C		(2 << 29)
+#define   VIDEO_DIP_ENABLE_AVI		(1 << 21)
+#define   VIDEO_DIP_ENABLE_VENDOR	(2 << 21)
+#define   VIDEO_DIP_ENABLE_SPD		(8 << 21)
+#define   VIDEO_DIP_SELECT_AVI		(0 << 19)
+#define   VIDEO_DIP_SELECT_VENDOR	(1 << 19)
+#define   VIDEO_DIP_SELECT_SPD		(3 << 19)
+#define   VIDEO_DIP_FREQ_ONCE		(0 << 16)
+#define   VIDEO_DIP_FREQ_VSYNC		(1 << 16)
+#define   VIDEO_DIP_FREQ_2VSYNC		(2 << 16)
+
 /* Panel power sequencing */
 #define PP_STATUS	0x61200
 #define   PP_ON		(1 << 31)
