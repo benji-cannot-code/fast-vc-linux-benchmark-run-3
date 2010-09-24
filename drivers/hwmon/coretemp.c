fa22f2dbe6a51f4b613da66de18383d147878c6e
Jan Beulich <jbeulich@novell.com>
2010-09-23 22:31:10 -0700

    x86/hwmon: {core, pkg, via}cpu_temp_device_remove() can all be __cpuinit
    ... as they're being called only from a __cpuinit function.
    
    Signed-off-by: Jan Beulich <jbeulich@novell.com>
    Signed-off-by: Guenter Roeck <guenter.roeck@ericsson.com>

--- drivers/hwmon/coretemp.c
+++ drivers/hwmon/coretemp.c
@@ -491,7 +491,7 @@ exit:
 	return err;
 }
 
-static void coretemp_device_remove(unsigned int cpu)
+static void __cpuinit coretemp_device_remove(unsigned int cpu)
 {
 	struct pdev_entry *p;
 	unsigned int i;
--- drivers/hwmon/via-cputemp.c
+++ drivers/hwmon/via-cputemp.c
@@ -239,7 +239,7 @@ exit:
 }
 
 #ifdef CONFIG_HOTPLUG_CPU
-static void via_cputemp_device_remove(unsigned int cpu)
+static void __cpuinit via_cputemp_device_remove(unsigned int cpu)
 {
 	struct pdev_entry *p, *n;
 	mutex_lock(&pdev_list_mutex);
