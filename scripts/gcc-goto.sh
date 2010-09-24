041fcf50daf5fa660ff573a382f36546707418ae
Ingo Molnar <mingo@elte.hu>
2010-09-24 09:12:25 +0200

    jump label: Fix GCC feature check when distcc is used
    The following build bug occurs on distcc builds:
    
       CC      arch/x86/kernel/asm-offsets.s
     In file included from include/linux/module.h:24,
                      from include/linux/crypto.h:22,
                      from arch/x86/kernel/asm-offsets_64.c:9,
                      from arch/x86/kernel/asm-offsets.c:5:
     include/trace/events/module.h: In function 'trace_module_load':
     include/trace/events/module.h:18: error: expected '(' before 'goto'
     include/trace/events/module.h:18: error: expected identifier or '*' before '(' token
    
    It triggers because distcc is invoked by turning $CC into "distcc gcc",
    but gcc-goto.sh check script was using $1 not $@ to expand parameters.
    
    Cc: Jason Baron <jbaron@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    LKML-Reference: <20100923034910.867858597@goodmis.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- scripts/gcc-goto.sh
+++ scripts/gcc-goto.sh
@@ -3,4 +3,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # Test for gcc 'asm goto' suport
 # Copyright (C) 2010, Jason Baron <jbaron@redhat.com>
 
-echo "int main(void) { entry: asm goto (\"\"::::entry); return 0; }" | $1 -x c - -c -o /dev/null >/dev/null 2>&1 && echo "y"
+echo "int main(void) { entry: asm goto (\"\"::::entry); return 0; }" | $@ -x c - -c -o /dev/null >/dev/null 2>&1 && echo "y"
