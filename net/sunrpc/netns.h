39b4575e50920e8e16612292de358a32b59ca9e5
Pavel Emelyanov <xemul@parallels.com>
2010-09-27 14:01:27 +0400

    sunrpc: The per-net skeleton
    Register empty per-net operations for the sunrpc layer.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

---
+++ net/sunrpc/netns.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __SUNRPC_NETNS_H__
+#define __SUNRPC_NETNS_H__
+
+#include <net/net_namespace.h>
+#include <net/netns/generic.h>
+
+struct sunrpc_net {
+};
+
+extern int sunrpc_net_id;
+
+#endif
