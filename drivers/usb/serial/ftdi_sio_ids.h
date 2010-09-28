05013251bd620eb79a810d4410a5929ea22999ec
Rainer Keller <mail@rainerkeller.de>
2010-09-28 12:27:43 +0200

    USB: add PID for FTDI based OpenDCC hardware
    The OpenDCC project is developing a new hardware. This patch adds its
    PID to the list of known FTDI devices. The PID can be found at
    http://www.opendcc.de/elektronik/usb/opendcc_usb.html
    
    Signed-off-by: Rainer Keller <mail@rainerkeller.de>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FTDI_OPENDCC_SNIFFER_PID	0xBFD9
 #define FTDI_OPENDCC_THROTTLE_PID	0xBFDA
 #define FTDI_OPENDCC_GATEWAY_PID	0xBFDB
+#define FTDI_OPENDCC_GBM_PID	0xBFDC
 
 /*
  * RR-CirKits LocoBuffer USB (http://www.rr-cirkits.com)
