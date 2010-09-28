79968fbf0240f60c8d27d8e5e0ac96f6c869fae3
Randy Dunlap <randy.dunlap@oracle.com>
2010-09-28 10:34:27 -0700

    pch_gbe: add header files
    Fix build errors, more header files needed.
    
    drivers/net/pch_gbe/pch_gbe_main.c:965: error: implicit declaration of function 'tcp_hdr'
    drivers/net/pch_gbe/pch_gbe_main.c:965: error: invalid type argument of '->' (have 'int')
    drivers/net/pch_gbe/pch_gbe_main.c:968: error: invalid type argument of '->' (have 'int')
    drivers/net/pch_gbe/pch_gbe_main.c:976: error: implicit declaration of function 'udp_hdr'
    drivers/net/pch_gbe/pch_gbe_main.c:976: error: invalid type argument of '->' (have 'int')
    drivers/net/pch_gbe/pch_gbe_main.c:980: error: invalid type argument of '->' (have 'int')
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/pch_gbe/pch_gbe.h
+++ drivers/net/pch_gbe/pch_gbe.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ethtool.h>
 #include <linux/vmalloc.h>
 #include <net/ip.h>
+#include <net/tcp.h>
+#include <net/udp.h>
 
 /**
  * pch_gbe_regs_mac_adr - Structure holding values of mac address registers
