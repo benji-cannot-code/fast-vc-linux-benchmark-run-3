5dfa0f9c0e43abf651b79b0c02c46edbc90bc01f
Michal Simek <monstr@monstr.eu>
2010-09-28 15:43:59 +1000

    microblaze: Remove hardcoded asm instraction for PVR loading
    It comes from past where pvr wasn't supported in msr instruction.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/kernel/cpu/pvr.c
+++ arch/microblaze/kernel/cpu/pvr.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	register unsigned tmp __asm__("r3");			\
 	tmp = 0x0;	/* Prevent warning about unused */	\
 	__asm__ __volatile__ (					\
-			".byte 0x94,0x60,0xa0, " #pvrid "\n\t"	\
+			"mfs	%0, rpvr" #pvrid ";"	\
 			: "=r" (tmp) : : "memory"); 		\
 	val = tmp;						\
 }
