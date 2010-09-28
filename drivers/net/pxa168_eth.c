cd24c99bc1497bf6102b654911bad164e560da80
Philip Rakity <prakity@marvell.com>
2010-09-28 04:26:30 +0000

    net: pxa168_etc.c recognize additional contributors
    Signed-off-by: Philip Rakity <prakity@marvell.com>
    Signed-off-by: Sachin Sanap <ssanap@marvell.com>
    Signed-off-by: Mark Brown <markb@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/pxa168_eth.c
+++ drivers/net/pxa168_eth.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2010 Marvell International Ltd.
  *		Sachin Sanap <ssanap@marvell.com>
+ *		Zhangfei Gao <zgao6@marvell.com>
  *		Philip Rakity <prakity@marvell.com>
  *		Mark Brown <markb@marvell.com>
  *
