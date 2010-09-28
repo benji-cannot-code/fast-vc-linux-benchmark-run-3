735a00c6083d6ba4b9ce834bc7a11164ad572935
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-09-28 16:01:20 -0700

    ASoC: Initial WM8962 IRQ support
    Provide an initial hookup for interrupts on the WM8962. Currently we simply
    report error status via log messages if an IRQ is provided for the device.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- include/sound/wm8962.h
+++ include/sound/wm8962.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct wm8962_pdata {
 	u32 gpio_init[WM8962_MAX_GPIO];
 
+	bool irq_active_low;
+
 	bool spk_mono;   /* Speaker outputs tied together as mono */
 };
 
