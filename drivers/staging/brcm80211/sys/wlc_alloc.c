9c35f4c094115170dad4824e25b6570efeb2af89
Brett Rudley <brudley@broadcom.com>
2010-09-29 11:41:56 -0700

    staging: brcm80211: cleanup headers
    Purge unused and extraneous header, brcm80211/include/proto/802.11e.h
    
    Signed-off-by: Brett Rudley <brudley@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/sys/wlc_alloc.c
+++ drivers/staging/brcm80211/sys/wlc_alloc.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmutils.h>
 #include <siutils.h>
 #include <proto/802.11.h>
-#include <proto/802.11e.h>
 #include <proto/wpa.h>
 #include <wlioctl.h>
 #include <bcmwpa.h>
--- drivers/staging/brcm80211/sys/wlc_mac80211.c
+++ drivers/staging/brcm80211/sys/wlc_mac80211.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmendian.h>
 #include <proto/802.1d.h>
 #include <proto/802.11.h>
-#include <proto/802.11e.h>
 #include <proto/wpa.h>
 #include <hndsoc.h>
 #include <sbchipc.h>
