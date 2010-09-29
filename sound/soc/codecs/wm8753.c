44cef99229ce78e6200bd3f8aef907caf5b3cfe9
Dimitris Papastamos <dp@opensource.wolfsonmicro.com>
2010-09-29 11:48:19 +0100

    ASoC: Remove unneeded use of spi_bus_type
    No need to explicitly set the bus type, spi_register_driver does
    that for us.
    
    Signed-off-by: Dimitris Papastamos <dp@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8753.c
+++ sound/soc/codecs/wm8753.c
@@ -1652,7 +1652,6 @@ static int __devexit wm8753_spi_remove(struct spi_device *spi)
 static struct spi_driver wm8753_spi_driver = {
 	.driver = {
 		.name	= "wm8753-codec",
-		.bus	= &spi_bus_type,
 		.owner	= THIS_MODULE,
 	},
 	.probe		= wm8753_spi_probe,
