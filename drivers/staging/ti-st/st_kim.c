3e350a99dc920ef44ecdff2c8126d5b5873ba421
Pavan Savoy <pavan_savoy@ti.com>
2010-09-30 16:13:30 -0400

    staging: ti-st: mv ti_wilink_st header
    Move the header to a standard linux device driver location.
    This should pave the way for other drivers to be moved into the relevant
    directories.
    
    ti_wilink_st.h is a common header file used by the TI's shared transport device
    driver for WiLink chipsets. Each individual protocol drivers like bluetooth
    driver, FM V4L2 driver and GPS drivers will make use of this header.
    
    Signed-off-by: Pavan Savoy <pavan_savoy@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ti-st/st_kim.c
+++ drivers/staging/ti-st/st_kim.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/bluetooth/hci_core.h>
 #include <net/bluetooth/hci.h>
 
-#include "ti_wilink_st.h"
+#include <linux/ti_wilink_st.h>
 
 
 static int kim_probe(struct platform_device *pdev);
