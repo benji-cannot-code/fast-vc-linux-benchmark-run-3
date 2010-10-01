7d9d2a3faab815280fa90622fed4a2cfed2c8008
Fernando Guzman Lugo <x0095840@ti.com>
2010-09-30 21:12:48 -0500

    staging: tidspbridge: replace iommu custom for opensource implementation
    Now the tidspbridge uses the API's from
    iovmm module.
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/core/_tiomap.h
+++ drivers/staging/tidspbridge/core/_tiomap.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/clockdomain.h>
 #include <mach-omap2/prm-regbits-34xx.h>
 #include <mach-omap2/cm-regbits-34xx.h>
+#include <plat/iommu.h>
+#include <plat/iovmm.h>
 #include <dspbridge/devdefs.h>
 #include <hw_defs.h>
 #include <dspbridge/dspioctl.h>	/* for bridge_ioctl_extproc defn */
@@ -329,7 +331,7 @@ struct bridge_dev_context {
 	u32 dw_internal_size;	/* Internal memory size */
 
 	struct omap_mbox *mbox;		/* Mail box handle */
-
+	struct iommu *dsp_mmu;      /* iommu for iva2 handler */
 	struct cfg_hostres *resources;	/* Host Resources */
 
 	/*
