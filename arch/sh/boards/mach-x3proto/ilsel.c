33eba01b7e40d154b17e196a5b9705db64ca1dbb
Paul Mundt <lethal@linux-sh.org>
2010-10-04 02:50:32 +0900

    sh: mach-x3proto: Move the ilsel header to a better place.
    We'll be adding more headers for this board, so move this over to its own
    directory.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-x3proto/ilsel.c
+++ arch/sh/boards/mach-x3proto/ilsel.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/bitmap.h>
 #include <linux/io.h>
-#include <asm/ilsel.h>
+#include <mach/ilsel.h>
 
 /*
  * ILSEL is split across:
--- arch/sh/boards/mach-x3proto/setup.c
+++ arch/sh/boards/mach-x3proto/setup.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/usb/r8a66597.h>
 #include <linux/usb/m66592.h>
-#include <asm/ilsel.h>
+#include <mach/ilsel.h>
 #include <asm/smp-ops.h>
 
 static struct resource heartbeat_resources[] = {
