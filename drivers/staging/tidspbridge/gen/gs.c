993140c73d73f131865c3ed6494a2b3b56a0fb05
Nicolas Kaiser <nikai@nikai.net>
2010-10-04 14:12:39 +0200

    staging: tidspbridge: remove duplicated include
    Remove duplicated include.
    
    Signed-off-by: Nicolas Kaiser <nikai@nikai.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/gen/gs.c
+++ drivers/staging/tidspbridge/gen/gs.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 /*  ----------------------------------- DSP/BIOS Bridge */
 #include <dspbridge/dbdefs.h>
-#include <linux/types.h>
 
 /*  ----------------------------------- This */
 #include <dspbridge/gs.h>
