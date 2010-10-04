43cc7f3bf966efea97bc0ec9b023ca774b256ff8
Nicolas Kaiser <nikai@nikai.net>
2010-10-04 12:39:47 +0200

    arm/davinci: remove duplicated include
    Remove duplicated include.
    
    Signed-off-by: Nicolas Kaiser <nikai@nikai.net>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-davinci/board-da850-evm.c
+++ arch/arm/mach-davinci/board-da850-evm.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/physmap.h>
 #include <linux/regulator/machine.h>
 #include <linux/regulator/tps6507x.h>
-#include <linux/mfd/tps6507x.h>
 #include <linux/input/tps6507x-ts.h>
 
 #include <asm/mach-types.h>
