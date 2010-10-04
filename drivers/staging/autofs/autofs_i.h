25afdceabf05723401bf9a7c5300cfc1a51dacb1
Arnd Bergmann <arnd@arndb.de>
2010-10-04 22:28:10 +0200

    autofs3: move to drivers/staging
    Nobody appears to be interested in fixing autofs3 bugs
    any more and it uses the BKL, which is going away.
    
    Move this to staging for retirement. Unless someone
    complains until 2.6.38, we can remove it for good.
    
    The include/linux/auto_fs.h header file is still used
    by autofs4, so it remains in place.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Ian Kent <raven@themaw.net>
    Cc: autofs@linux.kernel.org
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Acked-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/autofs/autofs_i.h
+++ drivers/staging/autofs/autofs_i.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* -*- linux-c -*- ------------------------------------------------------- *
  *   
- * linux/fs/autofs/autofs_i.h
+ * drivers/staging/autofs/autofs_i.h
  *
  *   Copyright 1997-1998 Transmeta Corporation - All Rights Reserved
  *
--- drivers/staging/autofs/root.c
+++ drivers/staging/autofs/root.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* -*- linux-c -*- --------------------------------------------------------- *
  *
- * linux/fs/autofs/root.c
+ * drivers/staging/autofs/root.c
  *
  *  Copyright 1997-1998 Transmeta Corporation -- All Rights Reserved
  *
