30d7f9fe5b82b6e8ff1a9f663e3c1b0523b733b5
Namhyung Kim <namhyung@gmail.com>
2010-10-05 00:13:59 +0900

    jbd: Remove unnecessary goto statement
    Remove goto statement which jumps to very next line. Also remove
    target label because it is no longer used anywhere.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/jbd/transaction.c
+++ fs/jbd/transaction.c
@@ -294,9 +294,7 @@ handle_t *journal_start(journal_t *journal, int nblocks)
 		jbd_free_handle(handle);
 		current->journal_info = NULL;
 		handle = ERR_PTR(err);
-		goto out;
 	}
-out:
 	return handle;
 }
 
