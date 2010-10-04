023e3e0c9dcfe0b013724dcffcbe310dd073da2a
Nicolas Kaiser <nikai@nikai.net>
2010-10-04 20:38:42 +0200

    video/omap: remove mux.h include
    Including mux.h should no longer be needed for omap2/3/4 SoCs
    outside arch/arm/mach-omap2 files.
    
    Signed-off-by: Nicolas Kaiser <nikai@nikai.net>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- drivers/video/omap/lcd_omap3beagle.c
+++ drivers/video/omap/lcd_omap3beagle.c
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/i2c/twl.h>
 
-#include <plat/mux.h>
-#include <plat/mux.h>
 #include <asm/mach-types.h>
 
 #include "omapfb.h"
