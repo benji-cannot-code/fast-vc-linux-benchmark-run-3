42b0b852a3014dbf206ad01202905dd472365143
Fernando Guzman Lugo <x0095840@ti.com>
2010-10-05 15:35:38 -0500

    staging: tidspbridge - fix mmufault support
    With changes for iommu migration mmufault report and dsp track
    dump is broken, this patch fixes that.
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/core/tiomap3430.c
+++ drivers/staging/tidspbridge/core/tiomap3430.c
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "_tiomap.h"
 #include "_tiomap_pwr.h"
 #include "tiomap_io.h"
+#include "_deh.h"
 
 /* Offset in shared mem to write to in order to synchronize start with DSP */
 #define SHMSYNCOFFSET 4		/* GPP byte offset */
@@ -368,6 +369,7 @@ static int bridge_brd_start(struct bridge_dev_context *dev_ctxt,
 	}
 	if (!status) {
 		dev_context->dsp_mmu = mmu;
+		mmu->isr = mmu_fault_isr;
 		sm_sg = &dev_context->sh_s;
 		sg0_da = iommu_kmap(mmu, sm_sg->seg0_da, sm_sg->seg0_pa,
 			sm_sg->seg0_size, IOVMF_ENDIAN_LITTLE | IOVMF_ELSZ_32);
