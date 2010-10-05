8a483c433c2b40c13281499fead02c5d7ce06b05
Emil Tantilov <emil.s.tantilov@intel.com>
2010-10-05 13:11:30 +0000

    ixgbevf: declare functions as static
    Following patch fixes warnings reported by `make namespacecheck`
    
    Reported by Stephen Hemminger
    
    CC: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>
    Acked-by: Greg Rose <greg.v.rose@intel.com>
    Tested-by: Jeff Pieper <jeffrey.e.pieper@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ixgbevf/mbx.h
+++ drivers/net/ixgbevf/mbx.h
@@ -96,6 +96,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* forward declaration of the HW struct */
 struct ixgbe_hw;
 
-s32 ixgbevf_init_mbx_params_vf(struct ixgbe_hw *);
-
 #endif /* _IXGBE_MBX_H_ */
