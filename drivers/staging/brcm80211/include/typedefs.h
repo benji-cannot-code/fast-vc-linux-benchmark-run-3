8d2239ea18a2be2de5a06fa2f88f9d9c905a23d1
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-05 09:48:23 -0700

    Staging: brcm80211: remove bool redefinition
    The core kernel type code handles this properly, no driver
    should ever do it.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/include/typedefs.h
+++ drivers/staging/brcm80211/include/typedefs.h
@@ -33,7 +33,6 @@ typedef unsigned long long int uintptr;
 #define TYPEDEF_USHORT
 #define TYPEDEF_ULONG
 #include <linux/version.h>
-#define TYPEDEF_BOOL
 
 /* Do not support the (u)int64 types with strict ansi for GNU C */
 #if defined(__GNUC__) && defined(__STRICT_ANSI__)
@@ -46,9 +45,6 @@ typedef unsigned long long int uintptr;
 /*
  * Default Typedefs
  */
-#ifndef TYPEDEF_BOOL
-typedef /* @abstract@ */ unsigned char bool;
-#endif
 
 /* define uchar, ushort, uint, ulong */
 
@@ -131,7 +127,6 @@ typedef signed long long int64;
 
 #define	AUTO	(-1)		/* Auto = -1 */
 
-#undef TYPEDEF_BOOL
 #undef TYPEDEF_UCHAR
 #undef TYPEDEF_USHORT
 #undef TYPEDEF_UINT
