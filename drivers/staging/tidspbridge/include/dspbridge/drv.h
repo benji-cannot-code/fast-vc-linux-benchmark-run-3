47fadc020ce489d47099f4da269b0c38a9fa6d12
Fernando Guzman Lugo <x0095840@ti.com>
2010-10-05 15:35:42 -0500

    staging: tidspbridge - remove reserved memory clean up
    Now iommv module keeps track of iommu memory used,
    we do not need resource cleanup for reserved
    memories anymore.
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/include/dspbridge/drv.h
+++ drivers/staging/tidspbridge/include/dspbridge/drv.h
@@ -166,10 +166,6 @@ struct process_context {
 	struct list_head dmm_map_list;
 	spinlock_t dmm_map_lock;
 
-	/* DMM reserved memory resources */
-	struct list_head dmm_rsv_list;
-	spinlock_t dmm_rsv_lock;
-
 	/* DSP Heap resources */
 	struct dspheap_res_object *pdspheap_list;
 
