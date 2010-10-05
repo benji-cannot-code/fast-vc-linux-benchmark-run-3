7ad0dbf889352a402cfa2a3afea57fcbbb5a7087
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-05 09:42:56 -0700

    Staging: brcm80211: remove PTRSZ definition
    It was never used.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/include/typedefs.h
+++ drivers/staging/brcm80211/include/typedefs.h
@@ -135,11 +135,6 @@ typedef signed long long int64;
 
 #define	AUTO	(-1)		/* Auto = -1 */
 
-/* define PTRSZ */
-#ifndef PTRSZ
-#define	PTRSZ	sizeof(char *)
-#endif
-
 #undef TYPEDEF_BOOL
 #undef TYPEDEF_UCHAR
 #undef TYPEDEF_USHORT
