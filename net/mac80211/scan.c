e46ce05e0cdd9ea5c51de28802eb1d52e50285ec
John W. Linville <linville@tuxdriver.com>
2010-10-06 16:40:40 -0400

    mac80211: avoid uninitialized var warning in ieee80211_scan_cancel
    net/mac80211/scan.c: In function ‘ieee80211_scan_cancel’:
    net/mac80211/scan.c:794: warning: ‘finish’ may be used uninitialized in this function
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/scan.c
+++ net/mac80211/scan.c
@@ -792,7 +792,8 @@ int ieee80211_request_internal_scan(struct ieee80211_sub_if_data *sdata,
  */
 void ieee80211_scan_cancel(struct ieee80211_local *local)
 {
-	bool abortscan, finish;
+	bool abortscan;
+	bool finish = false;
 
 	/*
 	 * We are only canceling software scan, or deferred scan that was not
@@ -819,7 +820,7 @@ void ieee80211_scan_cancel(struct ieee80211_local *local)
 	if (abortscan) {
 		/* The scan is canceled, but stop work from being pending */
 		cancel_delayed_work_sync(&local->scan_work);
-		if (finish)
-			__ieee80211_scan_completed_finish(&local->hw, false);
 	}
+	if (finish)
+		__ieee80211_scan_completed_finish(&local->hw, false);
 }
