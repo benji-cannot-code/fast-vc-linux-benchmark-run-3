27af5e6e8729a67a0c080c88a2bfeda78c2d1e4f
Michael Hennerich <michael.hennerich@analog.com>
2010-10-06 17:49:02 +0200

    staging: iio: adc: max1363: remove unused variable
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/max1363_ring.c
+++ drivers/staging/iio/adc/max1363_ring.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int max1363_single_channel_from_ring(long mask, struct max1363_state *st)
 {
 	struct iio_ring_buffer *ring = st->indio_dev->ring;
-	unsigned long numvals;
 	int count = 0, ret;
 	u8 *ring_data;
 	if (!(st->current_mode->modemask & mask)) {
