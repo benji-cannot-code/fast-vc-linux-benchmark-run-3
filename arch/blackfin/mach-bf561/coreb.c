c6651813f16282b0139665421edbd76fc740d7d7
Mike Frysinger <vapier@gentoo.org>
2010-10-06 06:33:08 +0000

    Blackfin: coreb: update ioctl numbers
    We have to use ioctl numbers that don't collide with common code.
    Otherwise, these ones never even get called because the common fs
    code swalled all invocations.
    
    Reported-by: Kay Duenzer <kduenzer@maku.eu>
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- arch/blackfin/mach-bf561/coreb.c
+++ arch/blackfin/mach-bf561/coreb.c
@@ -19,9 +19,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/miscdevice.h>
 #include <linux/module.h>
 
-#define CMD_COREB_START		2
-#define CMD_COREB_STOP		3
-#define CMD_COREB_RESET		4
+#define CMD_COREB_START		_IO('b', 0)
+#define CMD_COREB_STOP		_IO('b', 1)
+#define CMD_COREB_RESET		_IO('b', 2)
 
 static long
 coreb_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
