4d88c64857b97cd4bf2c50f1c0b2399ea061f7fe
Borislav Petkov <borislav.petkov@amd.com>
2010-10-07 18:29:15 +0200

    amd64_edac: Rework printk macros
    Add a macro per printk level, shorten up error messages. Add relevant
    information to KERN_INFO level. No functional change.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>

--- drivers/edac/edac_core.h
+++ drivers/edac/edac_core.h
@@ -69,9 +69,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EDAC_PCI "PCI"
 #define EDAC_DEBUG "DEBUG"
 
+extern const char *edac_mem_types[];
+
 #ifdef CONFIG_EDAC_DEBUG
 extern int edac_debug_level;
-extern const char *edac_mem_types[];
 
 #define edac_debug_printk(level, fmt, arg...)                           \
 	do {                                                            \
