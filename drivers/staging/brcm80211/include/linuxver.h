f701d43e87d9e01712f92057df651a293635adcc
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-07 16:11:01 -0700

    Staging: brcm80211: remove MY_INIT_WORK macro
    Use the real macro instead.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/include/linuxver.h
+++ drivers/staging/brcm80211/include/linuxver.h
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/workqueue.h>
 
-#define	MY_INIT_WORK(_work, _func)	INIT_WORK(_work, _func)
-
 typedef irqreturn_t(*FN_ISR) (int irq, void *dev_id, struct pt_regs * ptregs);
 
 #include <linux/sched.h>
