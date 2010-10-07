b360126e82e28dafec496e3cabff01f5b7861018
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-07 16:30:25 -0700

    Staging: brcm80211: remove unused irq typedef
    If it's not needed, why define it?
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/include/linuxver.h
+++ drivers/staging/brcm80211/include/linuxver.h
@@ -34,9 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 
 #include <linux/workqueue.h>
-
-typedef irqreturn_t(*FN_ISR) (int irq, void *dev_id, struct pt_regs * ptregs);
-
 #include <linux/sched.h>
 #include <linux/ieee80211.h>
 
