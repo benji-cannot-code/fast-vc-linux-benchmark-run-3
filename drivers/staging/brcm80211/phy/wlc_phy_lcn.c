c9f833b967d3e86174754c5b5bc8a26e803ae16f
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-08 11:44:45 -0700

    Staging: brcm80211: remove OFFSETOF macro
    Use the real 'offsetof' definition instead.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/phy/wlc_phy_lcn.c
+++ drivers/staging/brcm80211/phy/wlc_phy_lcn.c
@@ -141,7 +141,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(0 != (read_phy_reg((pi), 0x43b) & (0x1 << 6)))
 
 #define wlc_lcnphy_total_tx_frames(pi) \
-	wlapi_bmac_read_shm((pi)->sh->physhim, M_UCODE_MACSTAT + OFFSETOF(macstat_t, txallfrm))
+	wlapi_bmac_read_shm((pi)->sh->physhim, M_UCODE_MACSTAT + offsetof(macstat_t, txallfrm))
 
 typedef struct {
 	u16 gm_gain;
