ef5b184ed686abb07b9b242d50ea83a095c10b7c
Jonathan Cameron <jic23@cam.ac.uk>
2010-10-08 12:14:13 +0100

    staging: iio: adc add numbers to naming of all adc channels as needed to associate events with them.
    Given event codes are associated by number not name, all adc channels
    including those with names need to have numbers.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/adc.h
+++ drivers/staging/iio/adc/adc.h
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IIO_DEV_ATTR_IN_RAW(_num, _show, _addr)				\
 	IIO_DEVICE_ATTR(in##_num##_raw, S_IRUGO, _show, NULL, _addr)
 
-#define IIO_DEV_ATTR_IN_NAMED_RAW(_name, _show, _addr)			\
-	IIO_DEVICE_ATTR(in_##_name##_raw, S_IRUGO, _show, NULL, _addr)
+#define IIO_DEV_ATTR_IN_NAMED_RAW(_num, _name, _show, _addr)		\
+	IIO_DEVICE_ATTR(in##_num##_##_name##_raw, S_IRUGO, _show, NULL, _addr)
 
 #define IIO_DEV_ATTR_IN_DIFF_RAW(_nump, _numn, _show, _addr)		\
 	IIO_DEVICE_ATTR_NAMED(in##_nump##min##_numn##_raw,		\
@@ -29,11 +29,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 			      _addr)
 
 
-#define IIO_CONST_ATTR_IN_NAMED_OFFSET(_name, _string)			\
-	IIO_CONST_ATTR(in_##_name##_offset, _string)
+#define IIO_CONST_ATTR_IN_NAMED_OFFSET(_num, _name, _string)	\
+	IIO_CONST_ATTR(in##_num##_##_name##_offset, _string)
 
-#define IIO_CONST_ATTR_IN_NAMED_SCALE(_name, _string)			\
-	IIO_CONST_ATTR(in_##_name##_scale, _string)
+#define IIO_CONST_ATTR_IN_NAMED_SCALE(_num, _name, _string)	\
+	IIO_CONST_ATTR(in##_num##_##_name##_scale, _string)
 
 #define IIO_EVENT_CODE_IN_HIGH_THRESH(a)				\
 	IIO_UNMOD_EVENT_CODE(IIO_EV_CLASS_IN, a, IIO_EV_TYPE_THRESH,	\
