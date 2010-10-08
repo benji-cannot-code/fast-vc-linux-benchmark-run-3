76f873ffb0ab18b9bf6da49e67442ec1b9cbe21d
Paul Walmsley <paul@pwsan.com>
2010-10-08 11:40:19 -0600

    OMAP: McBSP: implement McBSP CLKR and FSR signal muxing via mach-omap2/mcbsp.c
    The OMAP ASoC McBSP code implemented CLKR and FSR signal muxing via
    direct System Control Module writes on OMAP2+.  This required the
    omap_ctrl_{read,write}l() functions to be exported, which is against
    policy: the only code that should call those functions directly is
    OMAP core code, not device drivers.  omap_ctrl_{read,write}*() are no
    longer exported, so the driver no longer builds as a module.
    
    Fix the pinmuxing part of the problem by removing calls to
    omap_ctrl_{read,write}l() from the OMAP ASoC McBSP code and
    implementing signal muxing functions in arch/arm/mach-omap2/mcbsp.c.
    Due to the unfortunate way that McBSP support is implemented in ASoC
    and the OMAP tree, these symbols must be exported for use by
    sound/soc/omap/omap-mcbsp.c.
    
    Going forward, the McBSP device driver should be moved from
    arch/arm/*omap* into drivers/ or sound/soc/*, and the CPU DAI driver
    should be implemented as a platform_driver as many other ASoC CPU DAI
    drivers are.  These two steps should resolve many of the layering
    problems, which will rapidly reappear during a McBSP hwmod/PM runtime
    conversion.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Acked-by: Jarkko Nikula <jhnikula@gmail.com>
    Acked-by: Peter Ujfalusi <peter.ujfalusi@nokia.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- arch/arm/plat-omap/include/plat/control.h
+++ arch/arm/plat-omap/include/plat/control.h
@@ -224,6 +224,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP2_MMCSDIO1ADPCLKISEL	(1 << 24) /* MMC1 loop back clock */
 #define OMAP24XX_USBSTANDBYCTRL		(1 << 15)
 #define OMAP2_MCBSP2_CLKS_MASK		(1 << 6)
+#define OMAP2_MCBSP1_FSR_MASK		(1 << 4)
+#define OMAP2_MCBSP1_CLKR_MASK		(1 << 3)
 #define OMAP2_MCBSP1_CLKS_MASK		(1 << 2)
 
 /* CONTROL_DEVCONF1 bits */
--- arch/arm/plat-omap/include/plat/mcbsp.h
+++ arch/arm/plat-omap/include/plat/mcbsp.h
@@ -313,6 +313,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RFSREN			0x0002
 #define RSYNCERREN		0x0001
 
+/* CLKR signal muxing options */
+#define CLKR_SRC_CLKR		0
+#define CLKR_SRC_CLKX		1
+
+/* FSR signal muxing options */
+#define FSR_SRC_FSR		0
+#define FSR_SRC_FSX		1
+
 /* we don't do multichannel for now */
 struct omap_mcbsp_reg_cfg {
 	u16 spcr2;
@@ -502,7 +510,6 @@ int omap_mcbsp_recv_buffer(unsigned int id, dma_addr_t buffer, unsigned int leng
 int omap_mcbsp_spi_master_xmit_word_poll(unsigned int id, u32 word);
 int omap_mcbsp_spi_master_recv_word_poll(unsigned int id, u32 * word);
 
-
 /* SPI specific API */
 void omap_mcbsp_set_spi_mode(unsigned int id, const struct omap_mcbsp_spi_cfg * spi_cfg);
 
@@ -510,6 +517,10 @@ void omap_mcbsp_set_spi_mode(unsigned int id, const struct omap_mcbsp_spi_cfg * 
 int omap_mcbsp_pollread(unsigned int id, u16 * buf);
 int omap_mcbsp_pollwrite(unsigned int id, u16 buf);
 int omap_mcbsp_set_io_type(unsigned int id, omap_mcbsp_io_type_t io_type);
+
+/* McBSP signal muxing API */
+void omap2_mcbsp1_mux_clkr_src(u8 mux);
+void omap2_mcbsp1_mux_fsr_src(u8 mux);
 
 #ifdef CONFIG_ARCH_OMAP3
 /* Sidetone specific API */
