5e186d5274c8735e41b736fc8cb5af774b308031
Henry Ptasinski <henryp@broadcom.com>
2010-10-07 19:52:06 -0700

    staging: brcm80211: Don't include bitfuncs.h
    Remove include of bitfuncs.h, as it's completely uncessary for these files.
    
    Signed-off-by: Henry Ptasinski <henryp@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/phy/wlc_phy_n.c
+++ drivers/staging/brcm80211/phy/wlc_phy_n.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <osl.h>
 #include <siutils.h>
 #include <sbchipc.h>
-#include <bitfuncs.h>
 #include <hndpmu.h>
 #include <bcmendian.h>
 
