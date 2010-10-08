e3739ce3b023f317450f7b428561f5741fe9eddb
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-10-07 20:41:04 -0700

    ASoC: Compress WM8962 register access map
    Reduce the source code size still futher by only specifying non-zero
    rows in the WM8962 access map.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/codecs/wm8962.h
+++ sound/soc/codecs/wm8962.h
@@ -3776,15 +3776,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WM8962_VSS_ENA_SHIFT                         0  /* VSS_ENA */
 #define WM8962_VSS_ENA_WIDTH                         1  /* VSS_ENA */
 
-struct wm8962_reg_access {
-	u16 read;
-	u16 write;
-	u16 vol;
-};
-
-extern
-const struct wm8962_reg_access wm8962_reg_access[WM8962_MAX_REGISTER + 1];
-
 int wm8962_mic_detect(struct snd_soc_codec *codec, struct snd_soc_jack *jack);
 
 #endif
