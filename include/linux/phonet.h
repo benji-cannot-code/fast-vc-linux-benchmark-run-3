cdecfd34708d1adb11adf1ae17521b0e9ff47a18
Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
2010-10-08 04:02:02 +0000

    Phonet: cleanup pipe enable socket option
    The current code works like this:
    
      int garbage, status;
      socklen_t len = sizeof(status);
    
      /* enable pipe */
      setsockopt(fd, SOL_PNPIPE, PNPIPE_ENABLE, &garbage, sizeof(garbage));
      /* disable pipe */
      setsockopt(fd, SOL_PNPIPE, PNPIPE_DISABLE, &garbage, sizeof(garbage));
      /* get status */
      getsockopt(fd, SOL_PNPIPE, PNPIPE_INQ, &status, &len);
    
    ...which does not follow the usual socket option pattern. This patch
    merges all three "options" into a single gettable&settable option,
    before Linux 2.6.37 gets out:
    
      int status;
      socklen_t len = sizeof(status);
    
      /* enable pipe */
      status = 1;
      setsockopt(fd, SOL_PNPIPE, PNPIPE_ENABLE, &status, sizeof(status));
      /* disable pipe */
      status = 0;
      setsockopt(fd, SOL_PNPIPE, PNPIPE_ENABLE, &status, sizeof(status));
      /* get status */
      getsockopt(fd, SOL_PNPIPE, PNPIPE_ENABLE, &status, &len);
    
    This also fixes the error code from EFAULT to ENOTCONN.
    
    Signed-off-by: Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
    Cc: Kumar Sanghvi <kumar.sanghvi@stericsson.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/phonet.h
+++ include/linux/phonet.h
@@ -39,9 +39,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PNPIPE_IFINDEX		2
 #define PNPIPE_CREATE           3
 #define PNPIPE_ENABLE           4
-#define PNPIPE_DISABLE          5
+/* unused slot */
 #define PNPIPE_DESTROY          6
-#define PNPIPE_INQ              7
 
 #define PNADDR_ANY		0
 #define PNADDR_BROADCAST	0xFC
