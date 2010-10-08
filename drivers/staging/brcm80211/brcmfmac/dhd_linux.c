97ac105aa1369960ab52cae53ac4f4cb49b50751
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-08 11:34:59 -0700

    Staging: brcm80211: remove broken MIN() implementation
    Use the kernel-provided version, this one is broken.
    
    Note, there are more compiler warnings now, that's due to different
    types being compared, which shows how the original macro was wrong in at
    least one way.  They need to be fixed up.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/dhd_linux.c
+++ drivers/staging/brcm80211/brcmfmac/dhd_linux.c
@@ -1683,7 +1683,7 @@ static int dhd_ioctl_entry(struct net_device *net, struct ifreq *ifr, int cmd)
 
 	/* Copy out any buffer passed */
 	if (ioc.buf) {
-		buflen = MIN(ioc.len, DHD_IOCTL_MAXLEN);
+		buflen = min(ioc.len, DHD_IOCTL_MAXLEN);
 		/* optimization for direct ioctl calls from kernel */
 		/*
 		   if (segment_eq(get_fs(), KERNEL_DS)) {
