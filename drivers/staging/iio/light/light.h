9c1200c0fe4a58b0f0bb72650285d4aa0eb1b6f1
Jonathan Cameron <jic23@cam.ac.uk>
2010-10-08 12:14:05 +0100

    staging: iio: tsl2563 move to new event numbering scheme and remove old definitions
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/light/light.h
+++ drivers/staging/iio/light/light.h
@@ -3,6 +3,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Light to digital sensor attributes */
 
-#define IIO_EVENT_CODE_LIGHT_THRESH IIO_EVENT_CODE_LIGHT_BASE
-
 
