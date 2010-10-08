1f34d3837e83b835ae85f478617432128f92d0dc
Kevin Hilman <khilman@deeprootsystems.com>
2010-10-08 22:43:45 +0000

    OMAP3: PM: fix scratchpad memory accesses for off-mode
    Commit 914bab936fe0388a529079679e2f137aa4ff548d (OMAP: mach-omap2: Fix
    incorrect assignment warnings) changed a pointer from 'u32 *' to
    'void *' without also fixing up the pointer arithmetic.
    
    Fix the scratchpad offsets so they are byte offsets instead of
    word offsets and thus work correctly with a void pointer base.
    
    Special thanks to Jean Pihet for taking the time track down this
    problem and propose an initial solution.
    
    Tested with off-idle and off-suspend on 36xx/Zoom3 and 34xx/omap3evm.
    
    Cc: Manjunath Kondaiah G <manjugk@ti.com>
    Reported-by: Jean Pihet <jean.pihet@newoldbits.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>
    Tested-by: Jean Pihet <jean.pihet@newoldbits.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/pm34xx.c
+++ arch/arm/mach-omap2/pm34xx.c
@@ -51,9 +51,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "control.h"
 
 /* Scratchpad offsets */
-#define OMAP343X_TABLE_ADDRESS_OFFSET	   0x31
-#define OMAP343X_TABLE_VALUE_OFFSET	   0x30
-#define OMAP343X_CONTROL_REG_VALUE_OFFSET  0x32
+#define OMAP343X_TABLE_ADDRESS_OFFSET	   0xc4
+#define OMAP343X_TABLE_VALUE_OFFSET	   0xc0
+#define OMAP343X_CONTROL_REG_VALUE_OFFSET  0xc8
 
 struct power_state {
 	struct powerdomain *pwrdm;
