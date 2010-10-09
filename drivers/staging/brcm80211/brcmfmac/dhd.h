5651c029b2af07dc0616772047ff4db60dc8c515
nohee ko <noheek@broadcom.com>
2010-10-09 10:41:05 -0700

    staging: brcm80211: remove unnecessary cflag, LINUX
    remove unnecessary cflag, LINUX
    
    Signed-off-by: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/dhd.h
+++ drivers/staging/brcm80211/brcmfmac/dhd.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _dhd_h_
 #define _dhd_h_
 
-#if defined(LINUX)
 #include <linux/sched.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -39,14 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/wakelock.h>
 #endif			/* defined (CONFIG_HAS_WAKELOCK) */
 /* The kernel threading is sdio-specific */
-#else				/* LINUX */
-#define ENOMEM		1
-#define EFAULT      2
-#define EINVAL		3
-#define EIO			4
-#define ETIMEDOUT	5
-#define ERESTARTSYS 6
-#endif				/* LINUX */
 
 #include <wlioctl.h>
 
