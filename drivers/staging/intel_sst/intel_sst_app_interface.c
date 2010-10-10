f3cd946cddcd265c5deeba124c13a89abe4053fc
Vasiliy Kulikov <segooon@gmail.com>
2010-10-10 21:28:43 +0400

    staging: intel_sst: use signed int for error codes
    As retval stores error code, it should be signed int.
    
    Signed-off-by: Vasiliy Kulikov <segooon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/intel_sst/intel_sst_app_interface.c
+++ drivers/staging/intel_sst/intel_sst_app_interface.c
@@ -99,7 +99,7 @@ static int intel_sst_check_device(void)
  */
 int intel_sst_open(struct inode *i_node, struct file *file_ptr)
 {
-	unsigned int retval = intel_sst_check_device();
+	int retval = intel_sst_check_device();
 	if (retval)
 		return retval;
 
@@ -138,7 +138,7 @@ int intel_sst_open(struct inode *i_node, struct file *file_ptr)
  */
 int intel_sst_open_cntrl(struct inode *i_node, struct file *file_ptr)
 {
-	unsigned int retval = intel_sst_check_device();
+	int retval = intel_sst_check_device();
 	if (retval)
 		return retval;
 
