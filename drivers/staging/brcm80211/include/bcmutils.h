3c6a87c37b8adc820b8b90a60e5efcfcd69a789b
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-10 05:52:59 -0700

    Staging: brcm80211: remove __cplusplus markers
    It's not needed within the kernel, so remove them.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Nohee Ko <noheek@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/include/bcmutils.h
+++ drivers/staging/brcm80211/include/bcmutils.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	_bcmutils_h_
 #define	_bcmutils_h_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /* Buffer structure for collecting string-formatted data
 * using bcm_bprintf() API.
 * Use bcm_binit() to initialize before use
@@ -513,9 +509,5 @@ extern "C" {
 	extern uint bcm_mkiovar(char *name, char *data, uint datalen, char *buf,
 				uint len);
 	extern uint bcm_bitcount(u8 *bitmap, uint bytelength);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif				/* _bcmutils_h_ */
