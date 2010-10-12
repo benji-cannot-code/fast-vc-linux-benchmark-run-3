4e7f58ca832d9ae0e4333f22fc24bd87db92c644
Paul Mundt <lethal@linux-sh.org>
2010-10-13 07:43:50 +0900

    sh: oprofile: Make sure the backtrace op is available for timer-fallback.
    Presently with hardware counter support disabled the backtrace op never
    gets initialized. This is a regression over the previous behaviour, so
    simply add it back in.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/oprofile/common.c
+++ arch/sh/oprofile/common.c
@@ -22,9 +22,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <asm/processor.h>
 
-#ifdef CONFIG_HW_PERF_EVENTS
 extern void sh_backtrace(struct pt_regs * const regs, unsigned int depth);
 
+#ifdef CONFIG_HW_PERF_EVENTS
 /*
  * This will need to be reworked when multiple PMUs are supported.
  */
@@ -58,7 +58,7 @@ void __exit oprofile_arch_exit(void)
 #else
 int __init oprofile_arch_init(struct oprofile_operations *ops)
 {
-	pr_info("oprofile: hardware counters not available\n");
+	ops->backtrace = sh_backtrace;
 	return -ENODEV;
 }
 void __exit oprofile_arch_exit(void) {}
