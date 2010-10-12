e9d9652536d3c55a87b781c84c3b5e7f88c3621e
Brett Rudley <brudley@broadcom.com>
2010-10-12 15:01:58 -0700

    staging: brcm80211: Purge unused bcmwpa.[ch]
    Move two macros into wlc_mac80211.h and purge the rest.
    
    Signed-off-by: Brett Rudley <brudley@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/sys/wlc_mac80211.c
+++ drivers/staging/brcm80211/sys/wlc_mac80211.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmsrom.h>
 #include <wlioctl.h>
 #include <epivers.h>
-#include <bcmwpa.h>
 #include <sbhnddma.h>
 #include <hnddma.h>
 #include <hndpmu.h>
--- drivers/staging/brcm80211/sys/wlc_stf.c
+++ drivers/staging/brcm80211/sys/wlc_stf.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmendian.h>
 #include <proto/802.11.h>
 #include <wlioctl.h>
-#include <bcmwpa.h>
 #include <bcmwifi.h>
 #include <d11.h>
 #include <wlc_rate.h>
