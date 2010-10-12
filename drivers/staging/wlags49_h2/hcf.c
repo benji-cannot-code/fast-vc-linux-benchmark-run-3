cb4a3fc7e51196bb1942062a7b7fc96e7c817793
Andy Shevchenko <andy.shevchenko@gmail.com>
2010-10-12 16:22:49 +0300

    staging: wlags49_h2: remove custom macros min() and max()
    Signed-off-by: Andy Shevchenko <ext-andriy.shevchenko@nokia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/wlags49_h2/hcf.c
+++ drivers/staging/wlags49_h2/hcf.c
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hcf.h"				// HCF and MSF common include file
 #include "hcfdef.h"				// HCF specific include file
 #include "mmd.h"				// MoreModularDriver common include file
+#include <linux/kernel.h>
 
 #if ! defined offsetof
 #define offsetof(s,m)   ((unsigned int)&(((s *)0)->m))
