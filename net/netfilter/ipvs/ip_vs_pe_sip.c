9d267d639cf3f08c3521159ec11fbbdf287e343c
Simon Horman <horms@verge.net.au>
2010-10-13 21:22:35 +0200

    IPVS: ip_vs_dbg_callid() is only needed for debugging
    ip_vs_dbg_callid() and IP_VS_DEBUG_CALLID() are only needed
    it CONFIG_IP_VS_DEBUG is defined.
    
    This resolves the following build warning when CONFIG_IP_VS_DEBUG is
    not defined.
    
    net/netfilter/ipvs/ip_vs_pe_sip.c:11: warning: 'ip_vs_dbg_callid' defined but not used
    
    Reported-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/netfilter/ipvs/ip_vs_pe_sip.c
+++ net/netfilter/ipvs/ip_vs_pe_sip.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/netfilter/nf_conntrack.h>
 #include <linux/netfilter/nf_conntrack_sip.h>
 
+#ifdef CONFIG_IP_VS_DEBUG
 static const char *ip_vs_dbg_callid(char *buf, size_t buf_len,
 				    const char *callid, size_t callid_len,
 				    int *idx)
@@ -23,6 +24,7 @@ static const char *ip_vs_dbg_callid(char *buf, size_t buf_len,
 #define IP_VS_DEBUG_CALLID(callid, len)					\
 	ip_vs_dbg_callid(ip_vs_dbg_buf, sizeof(ip_vs_dbg_buf),		\
 			 callid, len, &ip_vs_dbg_idx)
+#endif
 
 static int get_callid(const char *dptr, unsigned int dataoff,
 		      unsigned int datalen,
