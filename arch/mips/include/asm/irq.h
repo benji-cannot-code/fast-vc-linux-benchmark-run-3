a40ecdefa497f46746ec5d920506ca8877bb3505
Dezhong Diao <dediao@cisco.com>
2010-10-13 00:52:46 -0600

    of/mips: Add device tree support to MIPS
    Add the ability to enable CONFIG_OF on the MIPS architecture.
    
    Signed-off-by: Dezhong Diao <dediao@cisco.com>
    [grant.likely@secretlab.ca: cleared out obsolete hooks,
    	removed ARCH_HAS_DEVTREE_MEM,
    	remove __init tags from header file,
    	removed debugfs support hunk]
    [ddaney@linux-mips.org: backed out over aggressive trimming of hooks]
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Tested-by: David Daney <ddaney@caviumnetworks.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/mips/include/asm/irq.h
+++ arch/mips/include/asm/irq.h
@@ -17,6 +17,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <irq.h>
 
+static inline void irq_dispose_mapping(unsigned int virq)
+{
+	return;
+}
+
 #ifdef CONFIG_I8259
 static inline int irq_canonicalize(int irq)
 {
--- arch/mips/kernel/setup.c
+++ arch/mips/kernel/setup.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/setup.h>
 #include <asm/smp-ops.h>
 #include <asm/system.h>
+#include <asm/prom.h>
 
 struct cpuinfo_mips cpu_data[NR_CPUS] __read_mostly;
 
@@ -488,6 +489,7 @@ static void __init arch_mem_init(char **cmdline_p)
 	}
 
 	bootmem_init();
+	device_tree_init();
 	sparse_init();
 	paging_init();
 }
