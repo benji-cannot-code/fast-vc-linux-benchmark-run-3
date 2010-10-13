cb2c847bf8c67de09cf3e477a6f49a38585313d5
Paul Mundt <lethal@linux-sh.org>
2010-10-14 07:37:01 +0900

    sh: pci: Support slot 4 routing on SDK7786.
    SDK7786 supports connecting either slot3 or 4 to the same PCIe port by
    way of FPGA muxing. By default the vertical slot 3 on the baseboard is
    enabled, so this adds in a command line option for forcibly enabling the
    slot 4 edge connector.
    
    If nothing has been specified on the command line, we fall back to
    reading the resistor values for card presence to figure out where to
    route the port to.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/mach-sdk7786/mach/fpga.h
+++ arch/sh/include/mach-sdk7786/mach/fpga.h
@@ -32,7 +32,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EXTASR		0x110
 #define SPCAR		0x120
 #define INTMSR		0x130
+
 #define PCIECR		0x140
+#define  PCIECR_PCIEMUX1	BIT(15)
+#define  PCIECR_PCIEMUX0	BIT(14)
+#define  PCIECR_PRST4		BIT(12) /* slot 4 card present */
+#define  PCIECR_PRST3		BIT(11) /* slot 3 card present */
+#define  PCIECR_PRST2		BIT(10) /* slot 2 card present */
+#define  PCIECR_PRST1		BIT(9)  /* slot 1 card present */
+#define  PCIECR_CLKEN		BIT(4)
+
 #define FAER		0x150
 #define USRGPIR		0x160
 /* 0x170 reserved */
