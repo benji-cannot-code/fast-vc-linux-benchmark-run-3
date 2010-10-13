ad002d84179e44fc218222bf223813a5bc3db431
Mark Brown <broonie@opensource.wolfsonmicro.com>
2010-10-13 18:23:39 +0900

    ARM: S3C64XX: Ensure samsung_keypad_cfg_gpio() is prototyped
    Since it's exported we should make sure we're using the prototype
    others see.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c64xx/setup-keypad.c
+++ arch/arm/mach-s3c64xx/setup-keypad.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/gpio.h>
 #include <plat/gpio-cfg.h>
+#include <plat/keypad.h>
 
 void samsung_keypad_cfg_gpio(unsigned int rows, unsigned int cols)
 {
