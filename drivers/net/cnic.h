724b7301784d0cd98e1bb43238a50ff6c6520bb6
Michael Chan <mchan@broadcom.com>
2010-10-13 14:06:47 +0000

    cnic: Defer iscsi connection cleanup
    The bnx2x devices require a 2 second quiet time before sending the last
    RAMROD command to destroy a connection.  This sleep wait adds up to a
    long delay when iscsid is serially destroying maultiple connections.
    
    Create a workqueue to perform the final connection cleanup in the
    background to speed up the process.  This significantly speeds up the
    process as the wait time can be done in parallel for multiple connections.
    
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/cnic.h
+++ drivers/net/cnic.h
@@ -171,6 +171,7 @@ struct cnic_context {
 	unsigned long		timestamp;
 	unsigned long		ctx_flags;
 #define	CTX_FL_OFFLD_START	0
+#define	CTX_FL_DELETE_WAIT	1
 	u8			ulp_proto_id;
 	union {
 		struct cnic_iscsi	*iscsi;
@@ -287,6 +288,8 @@ struct cnic_local {
 	int			r2tq_size;
 	int			hq_size;
 	int			num_cqs;
+
+	struct delayed_work	delete_task;
 
 	struct cnic_ctx		*ctx_arr;
 	int			ctx_blks;
