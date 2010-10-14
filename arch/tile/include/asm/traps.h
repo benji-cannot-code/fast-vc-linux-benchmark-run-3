62965d730add117bb6978eb5e7f75e5f1ed8dfbf
Chris Metcalf <cmetcalf@tilera.com>
2010-10-14 16:32:41 -0400

    arch/tile: enable single-step support for TILE-Gx
    This is not quite the complete support, since we're not yet shipping
    intvec_64.S, but it is the support relevant to the set of files we are
    currently shipping, and makes it easier to track changes between
    our internal sources and our public GIT repository.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- arch/tile/include/asm/traps.h
+++ arch/tile/include/asm/traps.h
@@ -60,4 +60,8 @@ void do_hardwall_trap(struct pt_regs *, int fault_num);
 void do_breakpoint(struct pt_regs *, int fault_num);
 
 
+#ifdef __tilegx__
+void gx_singlestep_handle(struct pt_regs *, int fault_num);
+#endif
+
 #endif /* _ASM_TILE_SYSCALLS_H */
