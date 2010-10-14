e45348110f6755adbfcd32b3aec741e5c63940ab
Marek Belisko <marek.belisko@gmail.com>
2010-10-14 13:36:11 +0200

    staging: ft1000: Use specific error return value.
    Signed-off-by: Marek Belisko <marek.belisko@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ft1000/ft1000-usb/ft1000_hw.c
+++ drivers/staging/ft1000/ft1000-usb/ft1000_hw.c
@@ -124,12 +124,12 @@ static int ft1000_control(struct ft1000_device *ft1000dev,unsigned int pipe,
     if (ft1000dev == NULL )
     {
         DEBUG("NULL ft1000dev, failure\n");
-        return STATUS_FAILURE;
+	return -ENODEV;
     }
     else if ( ft1000dev->dev == NULL )
     {
         DEBUG("NULL ft1000dev->dev, failure\n");
-        return STATUS_FAILURE;
+	return -ENODEV;
     }
 
     ret = usb_control_msg(ft1000dev->dev,
@@ -142,11 +142,8 @@ static int ft1000_control(struct ft1000_device *ft1000dev,unsigned int pipe,
                           size,
                           LARGE_TIMEOUT);
 
-    if (ret>0)
-        ret = STATUS_SUCCESS;
-    else
-        ret = STATUS_FAILURE;
-
+	if (ret > 0)
+		ret = 0;
 
     return ret;
 
@@ -187,8 +184,6 @@ u16 ft1000_read_register(struct ft1000_device *ft1000dev, u16* Data, u16 nRegInd
    //DEBUG("ft1000_read_register: ret is  %d \n", ret);
 
    //DEBUG("ft1000_read_register: data is  %x \n", *Data);
-   if ( ret != STATUS_SUCCESS )
-       return STATUS_FAILURE;
 
    return ret;
 
@@ -224,9 +219,6 @@ u16 ft1000_write_register(struct ft1000_device *ft1000dev, USHORT value, u16 nRe
                            NULL,
                            0,
                            LARGE_TIMEOUT );
-
-   if ( ret != STATUS_SUCCESS )
-       return STATUS_FAILURE;
 
     return ret;
 }
@@ -266,8 +258,6 @@ u16 ft1000_read_dpram32(struct ft1000_device *ft1000dev, USHORT indx, PUCHAR buf
    //DEBUG("ft1000_read_dpram32: ret is  %d \n", ret);
 
    //DEBUG("ft1000_read_dpram32: ret=%d \n", ret);
-   if ( ret != STATUS_SUCCESS )
-       return STATUS_FAILURE;
 
    return ret;
 
@@ -306,11 +296,6 @@ u16 ft1000_write_dpram32(struct ft1000_device *ft1000dev, USHORT indx, PUCHAR bu
                            buffer,                             //buffer
                            cnt,                                //buffer size
                            LARGE_TIMEOUT );
-
-
-   if ( ret != STATUS_SUCCESS )
-       return STATUS_FAILURE;
-
 
     return ret;
 }
@@ -358,8 +343,6 @@ u16 ft1000_read_dpram16(struct ft1000_device *ft1000dev, USHORT indx, PUCHAR buf
 
 
    //DEBUG("ft1000_read_dpram16: data is  %x \n", *buffer);
-   if ( ret != STATUS_SUCCESS )
-       return STATUS_FAILURE;
 
    return ret;
 
@@ -406,11 +389,6 @@ u16 ft1000_write_dpram16(struct ft1000_device *ft1000dev, USHORT indx, USHORT va
                            NULL,                               //buffer
                            0,                                   //buffer size
                            LARGE_TIMEOUT );
-
-
-   if ( ret != STATUS_SUCCESS )
-       return STATUS_FAILURE;
-
 
     return ret;
 }
