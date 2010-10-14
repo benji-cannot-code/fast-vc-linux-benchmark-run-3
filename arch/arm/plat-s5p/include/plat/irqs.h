64d2abe9628e1b2f282438ac60910917af62feb5
Kukjin Kim <kgene.kim@samsung.com>
2010-10-14 15:35:07 +0900

    ARM: S5P: Fix missed IRQ_EINT_BIT()
    When merged patches, missed IRQ_EINT_BIT() definition from commit ea31fd43
    (ARM: S5PV210: Add Power Management Support). The IRQ_EINT_BIT() is used
    in the Power Management operation (plat-samsung/pm.c).
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-s5p/include/plat/irqs.h
+++ arch/arm/plat-s5p/include/plat/irqs.h
@@ -95,6 +95,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 						((irq) - S5P_EINT_BASE1) : \
 						((irq) + 16 - S5P_EINT_BASE2))
 
+#define IRQ_EINT_BIT(x)		EINT_OFFSET(x)
+
 /* Typically only a few gpio chips require gpio interrupt support.
    To avoid memory waste irq descriptors are allocated only for
    S5P_GPIOINT_GROUP_COUNT chips, each with total number of
