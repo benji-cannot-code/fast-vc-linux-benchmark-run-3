00735398813a36e403a00c213e66d79f7e715a2d
Luis R. Rodriguez <lrodriguez@atheros.com>
2010-10-14 11:44:27 -0700

    ath9k_hw: remove AR9003 2.0 support
    These chipsets will not hit the market, all customers will be
    on >= AR9003 2.2. This shaves down the ath9k_hw size by
    24161 bytes (24 KB) on my system.
    
    Before:
    
    $ size drivers/net/wireless/ath/ath9k/ath9k_hw.ko
       text	   data	    bss	    dec	    hex	filename
     292328	    616	   1824	 294768	  47f70	drivers/net/wireless/ath/ath9k/ath9k_hw.ko
    
    $ du -b drivers/net/wireless/ath/ath9k/ath9k_hw.ko
    5987825	drivers/net/wireless/ath/ath9k/ath9k_hw.ko
    
    After:
    
    $ size drivers/net/wireless/ath/ath9k/ath9k_hw.ko
       text	   data	    bss	    dec	    hex	filename
     277192	    616	   1824	 279632	  44450	drivers/net/wireless/ath/ath9k/ath9k_hw.ko
    
    $ du -b drivers/net/wireless/ath/ath9k/ath9k_hw.ko
    5963664	drivers/net/wireless/ath/ath9k/ath9k_hw.ko
    
    Cc: Yixiang Li <yixiang.li@atheros.com>
    Cc: Don Breslin <don.breslin@atheros.com>
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/reg.h
+++ drivers/net/wireless/ath/ath9k/reg.h
@@ -854,9 +854,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define AR_SREV_9300(_ah) \
 	(((_ah)->hw_version.macVersion == AR_SREV_VERSION_9300))
-#define AR_SREV_9300_20(_ah) \
-	(((_ah)->hw_version.macVersion == AR_SREV_VERSION_9300) && \
-	 ((_ah)->hw_version.macRev == AR_SREV_REVISION_9300_20))
 #define AR_SREV_9300_20_OR_LATER(_ah) \
 	(((_ah)->hw_version.macVersion > AR_SREV_VERSION_9300) || \
 	 (((_ah)->hw_version.macVersion == AR_SREV_VERSION_9300) && \
