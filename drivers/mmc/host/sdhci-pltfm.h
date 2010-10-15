5a710c2ab934c8388564f11152ae14de90b683db
Wolfram Sang <w.sang@pengutronix.de>
2010-10-15 12:20:59 +0200

    mmc: sdhci-pltfm: Add structure for host-specific data
    We need to carry some information per host, e.g. the clock. Add a
    structure for it and initialize it in the generic part. Also improve
    the check for a parent.
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Cc: Richard Röjfors <richard.rojfors.ext@mocean-labs.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-pltfm.h
+++ drivers/mmc/host/sdhci-pltfm.h
@@ -12,7 +12,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _DRIVERS_MMC_SDHCI_PLTFM_H
 #define _DRIVERS_MMC_SDHCI_PLTFM_H
 
+#include <linux/clk.h>
+#include <linux/types.h>
 #include <linux/sdhci-pltfm.h>
+
+struct sdhci_pltfm_host {
+	struct clk *clk;
+	u32 scratchpad; /* to handle quirks across io-accessor calls */
+};
 
 extern struct sdhci_pltfm_data sdhci_cns3xxx_pdata;
 
