2e9d0143f772a69a3909512ee313b4f4aae25113
Borislav Petkov <borislav.petkov@amd.com>
2010-10-15 15:20:18 +0200

    EDAC, MCE: Add a BIT_64() macro
    Add a macro for 64-bit vectors to use when accessing MSR contents.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>

--- drivers/edac/mce_amd.h
+++ drivers/edac/mce_amd.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mce.h>
 
+#define BIT_64(n)			(U64_C(1) << (n))
+
 #define ERROR_CODE(x)			((x) & 0xffff)
 #define EXT_ERROR_CODE(x)		(((x) >> 16) & 0x1f)
 
