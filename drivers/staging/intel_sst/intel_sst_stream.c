bbbec4508575779d78530914a78c39d8c1e1d86f
Dan Carpenter <error27@gmail.com>
2010-10-15 22:36:48 +0200

    Staging: intel_sst: off by one bug
    This should be >= instead of > or we go passed the end of the array.
    
    Also the arrays are declared with size MAX_NUM_STREAMS.  This is the
    only place that uses MAX_NUM_STREAMS_MFLD.  It seems like asking for
    trouble to use two variables for the same information.  I've changed
    everything to use MAX_NUM_STREAMS.
    
    This bug isn't really harmful.  In the worst case, if you enabled
    debugging then you would see a message.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/intel_sst/intel_sst_stream.c
+++ drivers/staging/intel_sst/intel_sst_stream.c
@@ -46,7 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 int sst_check_device_type(u32 device, u32 num_chan, u32 *pcm_slot)
 {
-	if (device > MAX_NUM_STREAMS_MFLD) {
+	if (device >= MAX_NUM_STREAMS) {
 		pr_debug("sst: device type invalid %d\n", device);
 		return -EINVAL;
 	}
