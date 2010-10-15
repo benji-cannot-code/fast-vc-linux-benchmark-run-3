cc6a120492d22ad1a64040578d1e9bd99b41b1c3
Jarod Wilson <jarod@redhat.com>
2010-10-15 11:07:37 -0300

    [media] IR/nuvoton: address all checkpatch.pl issues
    The driver was missing KERN_ facilities on a number of printks. The
    register dump functions have been updated to use KERN_INFO, so that the
    register dump gets logged in syslog (they only run on driver load, and
    only when debug is enabled). The buffer dump routine now uses
    KERN_DEBUG, as that spew will happen quite frequently (several times
    every IR signal), and shouldn't need to be logged.
    
    Also split up the small handful of lines that were just over 80
    characaters, and fixed the ioctl.h include.
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/IR/nuvoton-cir.h
+++ drivers/media/IR/nuvoton-cir.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/spinlock.h>
-#include <asm/ioctl.h>
+#include <linux/ioctl.h>
 
 /* platform driver name to register */
 #define NVT_DRIVER_NAME "nuvoton-cir"
