f4913b08ec137a59fc70ec2adf0afceec9f5854a
dann frazier <dannf@hp.com>
2010-10-15 10:14:34 -0600

    hpilo: Despecificate driver from iLO generation
    This driver supports iLO, iLO2 and iLO3. However, comments and Kconfig
    reference only iLO and iLO2. Let's just call it "iLO" to avoid having to
    update strings for each iLO generation. This is similar to the change made
    to hpwdt in commit 36e3ff44cebd7e46756dec88f30c982bebefdab7.
    
    Signed-off-by: dann frazier <dannf@hp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/misc/hpilo.c
+++ drivers/misc/hpilo.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Driver for HP iLO/iLO2 management processor.
+ * Driver for the HP iLO management processor.
  *
  * Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
  *	David Altobelli <david.altobelli@hp.com>
