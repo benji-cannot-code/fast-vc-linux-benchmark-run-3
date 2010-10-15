4a4a1c4cbe8ce182dbd2b26df78185b723c80b03
Christopher Brannon <cmbrannon79@gmail.com>
2010-10-14 19:23:54 -0500

    staging: speakup: speakup_dummy.c: style fixes
    * Clean this file based on reports from checkpatch.pl.
    
    Signed-off-by: Christopher Brannon <chris@the-brannons.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/speakup/speakup_dummy.c
+++ drivers/staging/speakup/speakup_dummy.c
@@ -33,13 +33,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SYNTH_CLEAR '!'
 
 static struct var_t vars[] = {
-	{ CAPS_START, .u.s = {"CAPS_START\n" }},
-	{ CAPS_STOP, .u.s = {"CAPS_STOP\n" }},
-	{ RATE, .u.n = {"RATE %d\n", 8, 1, 16, 0, 0, NULL }},
-	{ PITCH, .u.n = {"PITCH %d\n", 8, 0, 16, 0, 0, NULL }},
-	{ VOL, .u.n = {"VOL %d\n", 8, 0, 16, 0, 0, NULL }},
-	{ TONE, .u.n = {"TONE %d\n", 8, 0, 16, 0, 0, NULL }},
-	{ DIRECT, .u.n = {NULL, 0, 0, 1, 0, 0, NULL }},
+	{ CAPS_START, .u.s = {"CAPS_START\n" } },
+	{ CAPS_STOP, .u.s = {"CAPS_STOP\n" } },
+	{ RATE, .u.n = {"RATE %d\n", 8, 1, 16, 0, 0, NULL } },
+	{ PITCH, .u.n = {"PITCH %d\n", 8, 0, 16, 0, 0, NULL } },
+	{ VOL, .u.n = {"VOL %d\n", 8, 0, 16, 0, 0, NULL } },
+	{ TONE, .u.n = {"TONE %d\n", 8, 0, 16, 0, 0, NULL } },
+	{ DIRECT, .u.n = {NULL, 0, 0, 1, 0, 0, NULL } },
 	V_LAST_VAR
 };
 
