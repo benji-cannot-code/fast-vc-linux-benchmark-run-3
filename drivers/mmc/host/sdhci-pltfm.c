d37c275354186f623f874bf3c5e8692dfe2a2869
Wolfram Sang <w.sang@pengutronix.de>
2010-10-15 12:21:00 +0200

    mmc: sdhci-pltfm: move .h file into appropriate subdir
    Make use of the include/linux/mmc directory.
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Acked-by: Anton Vorontsov <cbouatmailru@gmail.com>
    Tested-by: Eric Bénard <eric@eukrea.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-pltfm.c
+++ drivers/mmc/host/sdhci-pltfm.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/host.h>
 
 #include <linux/io.h>
-#include <linux/sdhci-pltfm.h>
+#include <linux/mmc/sdhci-pltfm.h>
 
 #include "sdhci.h"
 #include "sdhci-pltfm.h"
--- drivers/mmc/host/sdhci-pltfm.h
+++ drivers/mmc/host/sdhci-pltfm.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk.h>
 #include <linux/types.h>
-#include <linux/sdhci-pltfm.h>
+#include <linux/mmc/sdhci-pltfm.h>
 
 struct sdhci_pltfm_host {
 	struct clk *clk;
