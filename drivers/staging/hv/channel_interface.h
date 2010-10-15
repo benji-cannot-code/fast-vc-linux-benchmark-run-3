5d761f5463d9a694c9d5a2dd324dc1fd960a0a2e
Haiyang Zhang <haiyangz@microsoft.com>
2010-10-15 10:14:05 -0700

    staging: hv: Convert camel cased parameter in channel_interface.h to lower case
    Convert camel cased parameter in channel_interface.h to lower case
    
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/channel_interface.h
+++ drivers/staging/hv/channel_interface.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "vmbus_api.h"
 
-void get_channel_info(struct hv_device *Device,
-		    struct hv_device_info *DeviceInfo);
+void get_channel_info(struct hv_device *device,
+		    struct hv_device_info *info);
 
 #endif /* _CHANNEL_INTERFACE_H_ */
