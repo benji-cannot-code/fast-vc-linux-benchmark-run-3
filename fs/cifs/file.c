2d7894c245abeb6760f747e819c4d5b60fb2a74b
Jeff Layton <jlayton@redhat.com>
2010-10-15 15:33:58 -0400

    cifs: eliminate the inode argument from cifs_new_fileinfo
    It already takes a file pointer. The inode associated with that had damn
    well better be the same one we're passing in anyway. Thus, there's no
    need for a separate argument here.
    
    Also, get rid of the bogus check for a null pCifsInode pointer. The
    CIFS_I macro uses container_of(), and that will virtually never return a
    NULL pointer anyway.
    
    Finally, move the setting of the canCache* flags outside of the lock.
    Other places in the code don't hold that lock when setting it, so I
    assume it's not really needed here either.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Reviewed-by: Suresh Jayaraman <sjayaraman@suse.de>
    Acked-by: Dave Kleikamp <shaggy@linux.vnet.ibm.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -273,8 +273,8 @@ int cifs_open(struct inode *inode, struct file *file)
 		if (rc == 0) {
 			cFYI(1, "posix open succeeded");
 
-			pCifsFile = cifs_new_fileinfo(inode, netfid, file,
-							tlink, oplock);
+			pCifsFile = cifs_new_fileinfo(netfid, file, tlink,
+						      oplock);
 			if (pCifsFile == NULL) {
 				CIFSSMBClose(xid, tcon, netfid);
 				rc = -ENOMEM;
@@ -366,7 +366,7 @@ int cifs_open(struct inode *inode, struct file *file)
 	if (rc != 0)
 		goto out;
 
-	pCifsFile = cifs_new_fileinfo(inode, netfid, file, tlink, oplock);
+	pCifsFile = cifs_new_fileinfo(netfid, file, tlink, oplock);
 	if (pCifsFile == NULL) {
 		rc = -ENOMEM;
 		goto out;
