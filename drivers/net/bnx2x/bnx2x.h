d4d5a34cbec9f2d151e6eea4544bb03632e14975
Dmitry Kravkov <dmitry@broadcom.com>
2010-10-17 23:08:55 +0000

    bnx2x: update version to 1.60.00-2
    Signed-off-by: Dmitry Kravkov <dmitry@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x/bnx2x.h
+++ drivers/net/bnx2x/bnx2x.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * (you will need to reboot afterwards) */
 /* #define BNX2X_STOP_ON_ERROR */
 
-#define DRV_MODULE_VERSION      "1.60.00-1"
-#define DRV_MODULE_RELDATE      "2010/10/06"
+#define DRV_MODULE_VERSION      "1.60.00-2"
+#define DRV_MODULE_RELDATE      "2010/10/18"
 #define BNX2X_BC_VER            0x040200
 
 #if defined(CONFIG_VLAN_8021Q) || defined(CONFIG_VLAN_8021Q_MODULE)
