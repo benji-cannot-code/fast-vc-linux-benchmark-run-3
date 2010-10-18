3fb7df70199827fecefb01efff4fccee8e6369c4
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2010-10-18 10:00:40 +0300

    UBIFS: do not allocate unneeded scan buffer
    In 'ubifs_replay_journal()' we allocate 'sbuf' for scanning the log.
    However, we already have 'c->sbuf' for these purposes, so do not
    allocate yet another one. This reduces UBIFS memory consumption while
    recovering.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/replay.c
+++ fs/ubifs/replay.c
@@ -1014,7 +1014,6 @@ out:
 int ubifs_replay_journal(struct ubifs_info *c)
 {
 	int err, i, lnum, offs, free;
-	void *sbuf = NULL;
 
 	BUILD_BUG_ON(UBIFS_TRUN_KEY > 5);
 
@@ -1028,10 +1027,6 @@ int ubifs_replay_journal(struct ubifs_info *c)
 			  c->ihead_offs);
 		return -EINVAL;
 	}
-
-	sbuf = vmalloc(c->leb_size);
-	if (!sbuf)
-		return -ENOMEM;
 
 	dbg_mnt("start replaying the journal");
 	c->replaying = 1;
@@ -1047,7 +1042,7 @@ int ubifs_replay_journal(struct ubifs_info *c)
 			lnum = UBIFS_LOG_LNUM;
 			offs = 0;
 		}
-		err = replay_log_leb(c, lnum, offs, sbuf);
+		err = replay_log_leb(c, lnum, offs, c->sbuf);
 		if (err == 1)
 			/* We hit the end of the log */
 			break;
@@ -1080,7 +1075,6 @@ int ubifs_replay_journal(struct ubifs_info *c)
 out:
 	destroy_replay_tree(c);
 	destroy_bud_list(c);
-	vfree(sbuf);
 	c->replaying = 0;
 	return err;
 }
