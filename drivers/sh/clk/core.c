d4efab0fdf69aabab6c5c823db422cd7d528041b
Paul Mundt <lethal@linux-sh.org>
2010-10-18 21:32:58 +0900

    sh: clkfwk: Shuffle around to match the intc split up.
    This shuffles the clock framework code around to a drivers/sh/clk subdir,
    to follow the intc split up. This will make it easier to subsequently
    break things out as well as plug in different helpers for non-CPG users.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/sh/clk/core.c
+++ drivers/sh/clk/core.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * drivers/sh/clk.c - SuperH clock framework
+ * SuperH clock framework
  *
  *  Copyright (C) 2005 - 2010  Paul Mundt
  *
--- drivers/sh/clk/cpg.c
+++ drivers/sh/clk/cpg.c
@@ -1,4 +1,13 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Helper routines for SuperH Clock Pulse Generator blocks (CPG).
+ *
+ *  Copyright (C) 2010  Magnus Damm
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ */
 #include <linux/clk.h>
 #include <linux/compiler.h>
 #include <linux/slab.h>
