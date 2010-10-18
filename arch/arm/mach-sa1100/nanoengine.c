d0edfe4c8cd715aaca89382a0323d533e2d01e06
Marcelo Roberto Jimenez <mroberto@cpti.cetuc.puc-rio.br>
2010-10-18 22:41:29 +0100

    ARM: 6458/1: pcmcia: Adds nanoEngine PCMCIA support.
    This patch adds nanoEngine PCMCIA support, with support for two sockets.
    
    In order to have a fully functional pcmcia subsystem in a BSE
    nanoEngine board you should carefully read this:
    
    http://cambuca.ldhs.cetuc.puc-rio.br/nanoengine/
    
    Acked-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Signed-off-by: Marcelo Roberto Jimenez <mroberto@cpti.cetuc.puc-rio.br>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-sa1100/nanoengine.c
+++ arch/arm/mach-sa1100/nanoengine.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Bright Star Engineering's nanoEngine board init code.
  *
- * Copyright (C) 2009 Marcelo Roberto Jimenez <mroberto@cpti.cetuc.puc-rio.br>
+ * Copyright (C) 2010 Marcelo Roberto Jimenez <mroberto@cpti.cetuc.puc-rio.br>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
