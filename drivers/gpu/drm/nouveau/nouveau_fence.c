125a70c12d0f2e08ad7cd93cc324a67445846935
Francisco Jerez <currojerez@riseup.net>
2010-10-18 03:56:14 +0200

    drm/nouveau: Make fences take a weak channel reference.
    Fences didn't increment the channel reference count, and the fenced
    channel could go away at any time. Fixes a potential race in
    nouveau_fence_update().
    
    Signed-off-by: Francisco Jerez <currojerez@riseup.net>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_fence.c
+++ drivers/gpu/drm/nouveau/nouveau_fence.c
@@ -65,6 +65,7 @@ nouveau_fence_del(struct kref *ref)
 	struct nouveau_fence *fence =
 		container_of(ref, struct nouveau_fence, refcount);
 
+	nouveau_channel_ref(NULL, &fence->channel);
 	kfree(fence);
 }
 
@@ -114,7 +115,7 @@ nouveau_fence_new(struct nouveau_channel *chan, struct nouveau_fence **pfence,
 	if (!fence)
 		return -ENOMEM;
 	kref_init(&fence->refcount);
-	fence->channel = chan;
+	nouveau_channel_ref(chan, &fence->channel);
 
 	if (emit)
 		ret = nouveau_fence_emit(fence);
