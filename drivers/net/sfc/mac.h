f705d41eb0230d7b8c83454683d4b8a0b1635add
stephen hemminger <shemminger@vyatta.com>
2010-10-18 05:27:31 +0000

    sfc: make functions static
    Make local functions and variable static. Do some rearrangement
    of the string table stuff to put it where it gets used.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Acked-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sfc/mac.h
+++ drivers/net/sfc/mac.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern struct efx_mac_operations falcon_xmac_operations;
 extern struct efx_mac_operations efx_mcdi_mac_operations;
-extern void falcon_reconfigure_xmac_core(struct efx_nic *efx);
 extern int efx_mcdi_mac_stats(struct efx_nic *efx, dma_addr_t dma_addr,
 			      u32 dma_len, int enable, int clear);
 
