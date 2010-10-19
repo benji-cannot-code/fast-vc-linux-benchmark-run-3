6670ca103e79b07e5d74ac3f05b2b144025d3064
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-10-18 21:18:49 -0300

    [media] cx231xx: Fix compilation breakage if DVB is not selected
    In file included from drivers/media/video/cx231xx/cx231xx-audio.c:40:
    drivers/media/video/cx231xx/cx231xx.h:559: error: field ‘frontends’ has incomplete type
    make[4]: ** [drivers/media/video/cx231xx/cx231xx-audio.o] Erro 1
    make[3]: ** [drivers/media/video/cx231xx] Erro 2
    make[2]: ** [drivers/media/video] Erro 2
    make[1]: ** [drivers/media] Erro 2
    make: ** [drivers] Erro 2
    
    Reported-by: Marcio Araujo Alves <froooozen@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx.h
+++ drivers/media/video/cx231xx/cx231xx.h
@@ -36,10 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/videobuf-vmalloc.h>
 #include <media/v4l2-device.h>
 #include <media/ir-core.h>
-#if defined(CONFIG_VIDEO_CX231XX_DVB) || \
-	defined(CONFIG_VIDEO_CX231XX_DVB_MODULE)
 #include <media/videobuf-dvb.h>
-#endif
 
 #include "cx231xx-reg.h"
 #include "cx231xx-pcb-cfg.h"
