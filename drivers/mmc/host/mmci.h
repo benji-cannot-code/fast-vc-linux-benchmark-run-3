97fb3776d9ba3259b262c5a895f10c807ff2f8a5
Linus Walleij <linus.walleij@stericsson.com>
2010-10-19 13:41:24 +0100

    ARM: 6399/3: mmci: handle broken MCI_DATABLOCKEND hardware
    On the U300 the MCI_DATAEND and MCI_DATABLOCKEND IRQs can arrive
    out-of-order. Replace an ugly #ifdef hack with a proper runtime
    solution which models what is really happening.
    
    In the U300 DMA mode and on all Ux500 models, the MCI_DATABLOCKEND
    flag isn't properly cleared in hardware following and ACK leading
    to all kind of weird behaviour when the flag is still up in
    subsequent interrupts, so we add two flags indicating the
    error and handle this runtime.
    
    Cc: Rabin Vincent <rabin.vincent@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/mmc/host/mmci.h
+++ drivers/mmc/host/mmci.h
@@ -178,6 +178,9 @@ struct mmci_host {
 	struct timer_list	timer;
 	unsigned int		oldstat;
 
+	bool			blockend;
+	bool			dataend;
+
 	/* pio stuff */
 	struct sg_mapping_iter	sg_miter;
 	unsigned int		size;
