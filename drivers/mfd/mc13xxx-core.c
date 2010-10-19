38e3a0e8e7ddf907deb38ce51fac2c769d0fa677
Samuel Ortiz <sameo@linux.intel.com>
2010-10-19 11:14:33 +0200

    mfd: Remove DEBUG defines from mc13xxx-core
    DEBUG and VERBOSE_DEBUG are not used.
    
    Reported-by: David Jander <david@protonic.nl>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/mc13xxx-core.c
+++ drivers/mfd/mc13xxx-core.c
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * the terms of the GNU General Public License version 2 as published by the
  * Free Software Foundation.
  */
-#define DEBUG
-#define VERBOSE_DEBUG
 
 #include <linux/slab.h>
 #include <linux/module.h>
