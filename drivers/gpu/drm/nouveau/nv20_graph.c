1b777117ff3d8ef78c9fe003dce894b0f97ae792
Ben Skeggs <bskeggs@redhat.com>
2010-10-19 19:57:34 +1000

    drm/nouveau: use object class structs more extensively
    The structs themselves, as well as the non-sw object creation function are
    probably very misnamed now.  That's a problem for later :)
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nv20_graph.c
+++ drivers/gpu/drm/nouveau/nv20_graph.c
@@ -758,6 +758,7 @@ nv30_graph_init(struct drm_device *dev)
 }
 
 struct nouveau_pgraph_object_class nv20_graph_grclass[] = {
+	{ 0x506e, NVOBJ_ENGINE_SW, NULL }, /* nvsw */
 	{ 0x0030, NVOBJ_ENGINE_GR, NULL }, /* null */
 	{ 0x0039, NVOBJ_ENGINE_GR, NULL }, /* m2mf */
 	{ 0x004a, NVOBJ_ENGINE_GR, NULL }, /* gdirect */
@@ -778,6 +779,7 @@ struct nouveau_pgraph_object_class nv20_graph_grclass[] = {
 };
 
 struct nouveau_pgraph_object_class nv30_graph_grclass[] = {
+	{ 0x506e, NVOBJ_ENGINE_SW, NULL }, /* nvsw */
 	{ 0x0030, NVOBJ_ENGINE_GR, NULL }, /* null */
 	{ 0x0039, NVOBJ_ENGINE_GR, NULL }, /* m2mf */
 	{ 0x004a, NVOBJ_ENGINE_GR, NULL }, /* gdirect */
