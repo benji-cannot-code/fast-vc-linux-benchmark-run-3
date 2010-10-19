53b6dc287649f187e1e9fd504d6860d604c81227
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2010-10-19 22:00:11 +0300

    UBI: remember copy_flag while scanning
    While scanning the flash we read all VID headers and store some important
    information in 'struct ubi_scan_leb'. Store also the 'copy_flag' value there
    as it is needed when comparing LEBs. We do not increase memory consumption
    because this is just one bit and we have plenty of spare bits in
    'struct ubi_scan_leb' (sizeof(struct ubi_scan_leb) is 48 both with and
    without this patch).
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/scan.h
+++ drivers/mtd/ubi/scan.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @pnum: physical eraseblock number
  * @lnum: logical eraseblock number
  * @scrub: if this physical eraseblock needs scrubbing
+ * @copy_flag: this LEB is a copy (@copy_flag is set in VID header of this LEB)
  * @sqnum: sequence number
  * @u: unions RB-tree or @list links
  * @u.rb: link in the per-volume RB-tree of &struct ubi_scan_leb objects
@@ -43,7 +44,8 @@ struct ubi_scan_leb {
 	int ec;
 	int pnum;
 	int lnum;
-	int scrub;
+	unsigned int scrub:1;
+	unsigned int copy_flag:1;
 	unsigned long long sqnum;
 	union {
 		struct rb_node rb;
