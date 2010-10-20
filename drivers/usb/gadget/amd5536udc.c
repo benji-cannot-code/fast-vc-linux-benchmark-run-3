779951c885860889e792eb0df3d9fe2a76b4dd77
Rahul Ruikar <rahul.ruikar@gmail.com>
2010-10-20 16:01:51 -0700

    USB: gadget: amd5536udc.c: fix error path
    In function udc_probe() call put_device() when device_register() fails.
    
    Signed-off-by: Rahul Ruikar <rahul.ruikar@gmail.com>
    Acked-by: Thomas Dahlmann <dahlmann.thomas@arcor.de>
    Cc: David Brownell <david-b@pacbell.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/amd5536udc.c
+++ drivers/usb/gadget/amd5536udc.c
@@ -3384,8 +3384,10 @@ static int udc_probe(struct udc *dev)
 	udc = dev;
 
 	retval = device_register(&dev->gadget.dev);
-	if (retval)
+	if (retval) {
+		put_device(&dev->gadget.dev);
 		goto finished;
+	}
 
 	/* timer init */
 	init_timer(&udc_timer);
