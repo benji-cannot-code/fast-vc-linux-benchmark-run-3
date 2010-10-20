c1a2a85dcacc30336f6a11873da11be260d8a6e4
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-20 16:07:11 -0700

    Staging: hv: make get_channel_info static
    This is done by moving it to the only .c file it is used in.
    
    This also allowed us to delete the channel_interface.h file, as it's no
    longer needed.
    
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/vmbus_private.h
+++ drivers/staging/hv/vmbus_private.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vmbus_api.h"
 #include "channel.h"
 #include "channel_mgmt.h"
-#include "channel_interface.h"
 #include "ring_buffer.h"
 #include <linux/list.h>
 
