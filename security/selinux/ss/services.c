fc84eafbfa923788b7bf7b39090f893b44af2a0d
Stephen Rothwell <sfr@canb.auug.org.au>
2010-10-20 16:08:00 +1100

    selinux: include vmalloc.h for vmalloc_user
    Include vmalloc.h for vmalloc_user (fixes ppc build warning).
    Acked-by: Eric Paris <eparis@redhat.com>
    
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/selinux.h>
 #include <linux/flex_array.h>
+#include <linux/vmalloc.h>
 #include <net/netlabel.h>
 
 #include "flask.h"
