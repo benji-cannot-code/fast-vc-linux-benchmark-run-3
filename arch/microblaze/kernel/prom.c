c7a5c5149d33752f8db884ab0319756c57036562
Grant Likely <grant.likely@secretlab.ca>
2010-10-20 11:45:14 -0600

    of/flattree: Eliminate need to provide early_init_dt_scan_chosen_arch
    This patch refactors the early init parsing of the chosen node so that
    architectures aren't forced to provide an empty implementation of
    early_init_dt_scan_chosen_arch.  Instead, if an architecture wants to
    do something different, it can either use a wrapper function around
    early_init_dt_scan_chosen(), or it can replace it altogether.
    
    This patch was written in preparation to adding device tree support to
    both x86 ad MIPS.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Tested-by: David Daney <ddaney@caviumnetworks.com>

--- arch/microblaze/kernel/prom.c
+++ arch/microblaze/kernel/prom.c
@@ -43,11 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sections.h>
 #include <asm/pci-bridge.h>
 
-void __init early_init_dt_scan_chosen_arch(unsigned long node)
-{
-	/* No Microblaze specific code here */
-}
-
 void __init early_init_dt_add_memory_arch(u64 base, u64 size)
 {
 	memblock_add(base, size);
