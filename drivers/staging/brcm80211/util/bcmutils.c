dfc002fa485923c4df32da54c8cf3e1e01ad3958
Henry Ptasinski <henryp@broadcom.com>
2010-10-21 11:04:11 -0700

    staging: brcm80211: Remove unnecessary includes from bcmutils.c
    Removed includ of bcmip.h and vlan.h, as they're no longer needed.
    
    Signed-off-by: Henry Ptasinski <henryp@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/util/bcmutils.c
+++ drivers/staging/brcm80211/util/bcmutils.c
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmendian.h>
 #include <bcmdevs.h>
 #include <proto/ethernet.h>
-#include <proto/vlan.h>
-#include <proto/bcmip.h>
 #include <proto/802.1d.h>
 #include <proto/802.11.h>
 
