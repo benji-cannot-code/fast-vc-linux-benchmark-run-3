be1271f476b7f4f2fa6f27123a467837202a1a62
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2010-10-22 01:25:25 +0400

    ieee802154: it's IEEE 802.15.4, not ZigBee
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- net/ieee802154/dgram.c
+++ net/ieee802154/dgram.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * ZigBee socket interface
+ * IEEE 802.15.4 dgram socket interface
  *
  * Copyright 2007, 2008 Siemens AG
  *
