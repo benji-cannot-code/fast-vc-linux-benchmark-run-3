1790ac5c35a9d29626f76037ce82bb553aec59f6
Grant Likely <grant.likely@secretlab.ca>
2010-10-21 11:34:55 -0600

    of/irq: of_irq.c needs to include linux/irq.h
    It works on current architectures simply because asm/prom.h includes
    it, but it broke when x86 turned on CONFIG_OF.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/of_irq.h
+++ include/linux/of_irq.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct of_irq;
 #include <linux/types.h>
 #include <linux/errno.h>
+#include <linux/irq.h>
 #include <linux/ioport.h>
 #include <linux/of.h>
 
