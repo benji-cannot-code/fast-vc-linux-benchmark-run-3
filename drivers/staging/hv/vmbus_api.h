aa5eb8becfc4bafbfbd2ac258e93c7e1d366b9dc
Greg Kroah-Hartman <gregkh@suse.de>
2010-10-21 08:50:16 -0700

    Staging: hv: remove EstablishGpadl from struct vmbus_channel_interface
    No one calls it anymore, so remove it.
    
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/vmbus_api.h
+++ drivers/staging/hv/vmbus_api.h
@@ -94,7 +94,6 @@ struct hv_device_info {
  * @SendPacketMultiPageBuffer: Send a multiple page buffers
  * @RecvPacket:        Receive packet
  * @RecvPacketRaw:     Receive Raw packet
- * @EstablishGpadl:    Set up GPADL for ringbuffer
  *
  * This structure contains function pointer to control vmbus channel
  * behavior. None of these functions is externally callable, but they
@@ -122,8 +121,6 @@ struct vmbus_channel_interface {
 			  u32 *BufferActualLen, u64 *RequestId);
 	int (*RecvPacketRaw)(struct hv_device *dev, void *buf, u32 buflen,
 			     u32 *BufferActualLen, u64 *RequestId);
-	int (*EstablishGpadl)(struct hv_device *dev, void *buf, u32 buflen,
-			      u32 *GpadlHandle);
 };
 
 extern const struct vmbus_channel_interface vmbus_ops;
