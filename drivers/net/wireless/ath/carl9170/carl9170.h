6e28ed5014c42ecec5ce789bcff1d447a5314b7c
Hauke Mehrtens <hauke@hauke-m.de>
2010-10-23 19:51:32 +0200

    carl9170: fix typos
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Acked-by: Christian Lamparter <chunkeey@googlemail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/carl9170/carl9170.h
+++ drivers/net/wireless/ath/carl9170/carl9170.h
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb.h>
 #ifdef CONFIG_CARL9170_LEDS
 #include <linux/leds.h>
-#endif /* CONFIG_CARL170_LEDS */
+#endif /* CONFIG_CARL9170_LEDS */
 #ifdef CONFIG_CARL9170_WPC
 #include <linux/input.h>
 #endif /* CONFIG_CARL9170_WPC */
