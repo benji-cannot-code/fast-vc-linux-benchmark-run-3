b5083b3665f28b1787aa25b8321afe0c5bf03f4d
Eric Dumazet <eric.dumazet@gmail.com>
2010-10-24 21:33:16 +0000

    tunnels: add _rcu annotations
    (struct ip6_tnl)->next is rcu protected :
    (struct ip_tunnel)->next is rcu protected :
    (struct xfrm6_tunnel)->next is rcu protected :
    
    add __rcu annotation and proper rcu primitives.
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/ipip.h
+++ include/net/ipip.h
@@ -17,7 +17,7 @@ struct ip_tunnel_6rd_parm {
 };
 
 struct ip_tunnel {
-	struct ip_tunnel	*next;
+	struct ip_tunnel __rcu	*next;
 	struct net_device	*dev;
 
 	int			err_count;	/* Number of arrived ICMP errors */
