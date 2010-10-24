be0721cb47834ee3a3ecac04407cf53fadb614f3
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2010-10-24 04:02:14 +0000

    viafb: add initial VX900 support
    This patch adds basic support for the new VX900 IGP. Almost everything
    that was implemented for other IGPs is expected to work also on VX900
    after this patch. The only known issue is that on the CRT output mode
    setting does not always work.
    It is clear that the possibility for regressions is zero.
    
    A big thanks to VIA Technologies for making this possible and
    supporting this work.
    
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Cc: Joseph Chan <JosephChan@via.com.tw>
    Cc: Jonathan Corbet <corbet@lwn.net>

--- drivers/video/via/chip.h
+++ drivers/video/via/chip.h
@@ -72,6 +72,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define     UNICHROME_VX855         12
 #define     UNICHROME_VX855_DID     0x5122
 
+#define     UNICHROME_VX900         13
+#define     UNICHROME_VX900_DID     0x7122
+
 /**************************************************/
 /* Definition TMDS Trasmitter Information         */
 /**************************************************/
