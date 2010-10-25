acb73b453f28f6de97cd5aa67ead9421a65ae1d0
Somnath Kotur <somnath.kotur@emulex.com>
2010-10-25 01:11:10 +0000

    be2net: Call netif_carier_off() after register_netdev()
    Calling netif_carrier_off before register_netdev was causing the network interface
    to miss a linkwatch pending event leading to  an inconsistent state if the link
    is not up when interface is initialized.This is now invoked after register_netdev.
    
    Signed-off-by: Somnath Kotur <somnath.kotur@emulex.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/benet/be_main.c
+++ drivers/net/benet/be_main.c
@@ -2557,7 +2557,6 @@ static void be_netdev_init(struct net_device *netdev)
 	netif_napi_add(netdev, &adapter->tx_eq.napi, be_poll_tx_mcc,
 		BE_NAPI_WEIGHT);
 
-	netif_carrier_off(netdev);
 	netif_stop_queue(netdev);
 }
 
@@ -2866,6 +2865,7 @@ static int __devinit be_probe(struct pci_dev *pdev,
 	status = register_netdev(netdev);
 	if (status != 0)
 		goto unsetup;
+	netif_carrier_off(netdev);
 
 	dev_info(&pdev->dev, "%s port %d\n", nic_name(pdev), adapter->port_num);
 	return 0;
