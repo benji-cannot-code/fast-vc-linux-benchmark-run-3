43ce21b6e4df04d333629eada6a297e4a5e34baf
Borislav Petkov <bp@alien8.de>
2010-10-25 18:25:23 +0200

    x86-32, mm: Remove duplicated #include
    b40827fa7268fda8a62490728a61c2856f33830b added an include
    directive which is needless and is taken care of by a previous
    one. Remove it.
    
    Caught-by: Jaswinder Singh Rajput <jaswinderlinux@gmail.com>
    Signed-off-by: Borislav Petkov <bp@alien8.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Jaswinder Singh Rajput <jaswinderlinux@gmail.com>
    Cc: H. Peter Anvin <hpa@linux.intel.com>
    LKML-Reference: <20101025162523.GA4712@a1.tnic>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/acpi/sleep.c
+++ arch/x86/kernel/acpi/sleep.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_X86_32
 #include <asm/pgtable.h>
-#include <asm/pgtable_32.h>
 #endif
 
 #include "realmode/wakeup.h"
