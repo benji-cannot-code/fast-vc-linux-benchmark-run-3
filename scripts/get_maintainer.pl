359e567ae47a8a01445fa95f0163dbfe3eabae91
Joe Perches <joe@perches.com>
2010-10-26 14:22:51 -0700

    scripts/get_maintainer.pl: Add --git-blame --rolestats "Authored lines" information
    When options --git-blame and --rolestats are specified, add
    the maintainers with the qualifying --git-min-percent amount
    of lines authored of the complete file.  Does not add more
    authors than specified by --git-max-maintainers.
    
    For anyone using hg, this option works but is _very_ slow.
    It's orders of magnitude slower than git slow.
    
    The get_maintainer.pl version was incremented to 0.25.
    
    This can be used with or without --git.
    
    For instance:
    
    $ ./scripts/get_maintainer.pl --git-blame --nogit --rolestats -f lib/bitmap.c
    Paul Jackson <pj@sgi.com> (authored lines:406/613=66%,commits:7/20=35%)
    Akinobu Mita <mita@miraclelinux.com> (authored lines:87/613=14%,commits:3/20=15%)
    Reinette Chatre <reinette.chatre@linux.intel.com> (authored lines:42/613=7%)
    Andrew Morton <akpm@linux-foundation.org> (commits:16/20=80%)
    Paul Mundt <lethal@linux-sh.org> (commits:3/20=15%)
    Randy Dunlap <randy.dunlap@oracle.com> (commits:2/20=10%)
    
    $ ./scripts/get_maintainer.pl --git-blame --git --rolestats -f lib/bitmap.c
    Andrew Morton <akpm@linux-foundation.org> (commit_signer:4/5=80%,commits:16/20=80%)
    Akinobu Mita <akinobu.mita@gmail.com> (commit_signer:2/5=40%,authored lines:87/613=14%,commits:3/20=15%)
    Jack Steiner <steiner@sgi.com> (commit_signer:1/5=20%)
    Ben Hutchings <ben@decadent.org.uk> (commit_signer:1/5=20%)
    Lee Schermerhorn <lee.schermerhorn@hp.com> (commit_signer:1/5=20%)
    Paul Jackson <pj@sgi.com> (authored lines:406/613=66%,commits:7/20=35%)
    Reinette Chatre <reinette.chatre@linux.intel.com> (authored lines:42/613=7%)
    Paul Mundt <lethal@linux-sh.org> (commits:3/20=15%)
    Randy Dunlap <randy.dunlap@oracle.com> (commits:2/20=10%)
    linux-kernel@vger.kernel.org (open list)
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Cc: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/get_maintainer.pl
+++ scripts/get_maintainer.pl
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 use strict;
 
 my $P = $0;
-my $V = '0.24';
+my $V = '0.25';
 
 use Getopt::Long qw(:config no_auto_abbrev);
 
@@ -89,6 +89,7 @@ my %VCS_cmds_git = (
     "available" => '(which("git") ne "") && (-d ".git")',
     "find_signers_cmd" => "git log --no-color --since=\$email_git_since -- \$file",
     "find_commit_signers_cmd" => "git log --no-color -1 \$commit",
+    "find_commit_author_cmd" => "git log -1 --format=\"%an <%ae>\" \$commit",
     "blame_range_cmd" => "git blame -l -L \$diff_start,+\$diff_length \$file",
     "blame_file_cmd" => "git blame -l \$file",
     "commit_pattern" => "^commit [0-9a-f]{40,40}",
@@ -102,6 +103,7 @@ my %VCS_cmds_hg = (
 	"hg log --date=\$email_hg_since" .
 		" --template='commit {node}\\n{desc}\\n' -- \$file",
     "find_commit_signers_cmd" => "hg log --template='{desc}\\n' -r \$commit",
+    "find_commit_author_cmd" => "hg log -l 1 --template='{author}\\n' -r \$commit",
     "blame_range_cmd" => "",		# not supported
     "blame_file_cmd" => "hg blame -c \$file",
     "commit_pattern" => "^commit [0-9a-f]{40,40}",
@@ -1015,6 +1017,9 @@ sub vcs_find_signers {
     if (!$email_git_penguin_chiefs) {
 	@lines = grep(!/${penguin_chiefs}/i, @lines);
     }
+
+    return (0, @lines) if !@lines;
+
     # cut -f2- -d":"
     s/.*:\s*(.+)\s*/$1/ for (@lines);
 
@@ -1026,6 +1031,28 @@ sub vcs_find_signers {
     }
 
     return ($commits, @lines);
+}
+
+sub vcs_find_author {
+    my ($cmd) = @_;
+    my @lines = ();
+
+    @lines = &{$VCS_cmds{"execute_cmd"}}($cmd);
+
+    if (!$email_git_penguin_chiefs) {
+	@lines = grep(!/${penguin_chiefs}/i, @lines);
+    }
+
+    return @lines if !@lines;
+
+## Reformat email addresses (with names) to avoid badly written signatures
+
+    foreach my $line (@lines) {
+	my ($name, $address) = parse_email($line);
+	$line = format_email($name, $address, 1);
+    }
+
+    return @lines;
 }
 
 sub vcs_save_commits {
@@ -1085,6 +1112,10 @@ sub vcs_blame {
 	@commits = vcs_save_commits($cmd);
     }
 
+    foreach my $commit (@commits) {
+	$commit =~ s/^\^//g;
+    }
+
     return @commits;
 }
 
@@ -1121,6 +1152,8 @@ sub vcs_assign {
     if ($email_remove_duplicates) {
 	@lines = mailmap(@lines);
     }
+
+    return if (@lines <= 0);
 
     @lines = sort(@lines);
 
@@ -1166,14 +1199,17 @@ sub vcs_file_blame {
     my ($file) = @_;
 
     my @signers = ();
+    my @all_commits = ();
     my @commits = ();
     my $total_commits;
+    my $total_lines;
 
     return if (!vcs_exists());
 
-    @commits = vcs_blame($file);
-    @commits = uniq(@commits);
+    @all_commits = vcs_blame($file);
+    @commits = uniq(@all_commits);
     $total_commits = @commits;
+    $total_lines = @all_commits;
 
     foreach my $commit (@commits) {
 	my $commit_count;
@@ -1183,10 +1219,28 @@ sub vcs_file_blame {
 	$cmd =~ s/(\$\w+)/$1/eeg;	#interpolate $cmd
 
 	($commit_count, @commit_signers) = vcs_find_signers($cmd);
+
 	push(@signers, @commit_signers);
     }
 
     if ($from_filename) {
+	if ($output_rolestats) {
+	    my @blame_signers;
+	    foreach my $commit (@commits) {
+		my $i;
+		my $cmd = $VCS_cmds{"find_commit_author_cmd"};
+		$cmd =~ s/(\$\w+)/$1/eeg;	#interpolate $cmd
+		my @author = vcs_find_author($cmd);
+		next if !@author;
+		my $count = grep(/$commit/, @all_commits);
+		for ($i = 0; $i < $count ; $i++) {
+		    push(@blame_signers, $author[0]);
+		}
+	    }
+	    if (@blame_signers) {
+		vcs_assign("authored lines", $total_lines, @blame_signers);
+	    }
+	}
 	vcs_assign("commits", $total_commits, @signers);
     } else {
 	vcs_assign("modified commits", $total_commits, @signers);
