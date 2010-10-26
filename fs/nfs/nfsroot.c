653f629a2e5f86b43e3f93b92726bb81cc8eec46
Trond Myklebust <Trond.Myklebust@netapp.com>
2010-10-26 13:47:04 -0400

    NFS: Fix a compile issue in nfs_root
    Stephen Rothwell reports:
    
    > /home/test/linux-2.6/fs/nfs/nfsroot.c: In function 'nfs_root_debug':
    > /home/test/linux-2.6/fs/nfs/nfsroot.c:110:2: error: 'nfs_debug'
    > undeclared (first use in this function)
    > /home/test/linux-2.6/fs/nfs/nfsroot.c:110:2: note: each undeclared
    > identifier is reported only once for each function it appears in
    > make[3]: *** [fs/nfs/nfsroot.o] Error 1
    > make[2]: *** [fs/nfs] Error 2
    > make[1]: *** [fs] Error 2
    > make: *** [sub-make] Error 2
    
    Which is caused by commit 306a075362a288683f6346185f97dd0e06df19da
    (NFS: Allow NFSROOT debugging messages to be enabled dynamically)
    
    Fix is to disable this code when RPC_DEBUG is disabled.
    
    Reported-by: Zimny Lech <napohybelskurwysynom2010@gmail.com>
    Tested-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfsroot.c
+++ fs/nfs/nfsroot.c
@@ -102,6 +102,7 @@ static char nfs_export_path[NFS_MAXPATHLEN + 1] __initdata = "";
 /* server:export path string passed to super.c */
 static char nfs_root_device[NFS_MAXPATHLEN + 1] __initdata = "";
 
+#ifdef RPC_DEBUG
 /*
  * When the "nfsrootdebug" kernel command line option is specified,
  * enable debugging messages for NFSROOT.
@@ -113,6 +114,7 @@ static int __init nfs_root_debug(char *__unused)
 }
 
 __setup("nfsrootdebug", nfs_root_debug);
+#endif
 
 /*
  *  Parse NFS server and directory information passed on the kernel
