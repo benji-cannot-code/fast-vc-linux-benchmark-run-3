0a3ad3a1178c455f6407472f1f7bd42c5acbcdd3
Peter Zijlstra <a.p.zijlstra@chello.nl>
2010-10-26 14:22:45 -0700

    kernel: remove PF_FLUSHER
    PF_FLUSHER is only ever set, not tested, remove it.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/backing-dev.c
+++ mm/backing-dev.c
@@ -363,7 +363,7 @@ static int bdi_forker_thread(void *ptr)
 {
 	struct bdi_writeback *me = ptr;
 
-	current->flags |= PF_FLUSHER | PF_SWAPWRITE;
+	current->flags |= PF_SWAPWRITE;
 	set_freezable();
 
 	/*
