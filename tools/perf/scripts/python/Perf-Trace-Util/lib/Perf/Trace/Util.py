b51a67429868aaca2f6d87e5b581e2be4d118a96
Arnaldo Carvalho de Melo <acme@redhat.com>
2010-10-26 17:07:33 -0200

    perf python scripting: Add futex-contention script
    The equivalent to this SystemTAP script:
    
    http://sourceware.org/systemtap/wiki/WSFutexContention
    
    [root@doppio ~]# perf trace futex-contention
    Press control+C to stop and show the summary
    ^Cnpviewer.bin[15242] lock 7f0a8be19104 contended 29 times, 72806 avg ns
    npviewer.bin[15242] lock 7f0a8be19130 contended 2 times, 1355 avg ns
    synergyc[17245] lock f127f4 contended 1 times, 1830569 avg ns
    firefox[15116] lock 7f2b7238af0c contended 168 times, 1230390 avg ns
    synergyc[17245] lock f2fc20 contended 1 times, 33149 avg ns
    npviewer.bin[15255] lock 7f0a8be19074 contended 155 times, 73047 avg ns
    npviewer.bin[15255] lock 7f0a8be190a0 contended 127 times, 7088 avg ns
    synergyc[17247] lock f12854 contended 1 times, 46741 avg ns
    synergyc[17245] lock f12610 contended 1 times, 7358 avg ns
    [root@doppio ~]#
    
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Tom Zanussi <tzanussi@gmail.com>
    LKML-Reference: <new-submission>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/scripts/python/Perf-Trace-Util/lib/Perf/Trace/Util.py
+++ tools/perf/scripts/python/Perf-Trace-Util/lib/Perf/Trace/Util.py
@@ -9,6 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 import errno, os
 
+FUTEX_WAIT = 0
+FUTEX_WAKE = 1
+FUTEX_PRIVATE_FLAG = 128
+FUTEX_CLOCK_REALTIME = 256
+FUTEX_CMD_MASK = ~(FUTEX_PRIVATE_FLAG | FUTEX_CLOCK_REALTIME)
+
 NSECS_PER_SEC    = 1000000000
 
 def avg(total, n):
@@ -26,6 +32,18 @@ def nsecs_nsecs(nsecs):
 def nsecs_str(nsecs):
     str = "%5u.%09u" % (nsecs_secs(nsecs), nsecs_nsecs(nsecs)),
     return str
+
+def add_stats(dict, key, value):
+	if not dict.has_key(key):
+		dict[key] = (value, value, value, 1)
+	else:
+		min, max, avg, count = dict[key]
+		if value < min:
+			min = value
+		if value > max:
+			max = value
+		avg = (avg + value) / 2
+		dict[key] = (min, max, avg, count + 1)
 
 def clear_term():
     print("\x1b[H\x1b[2J")
