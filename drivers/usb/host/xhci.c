41d9e24225368b3212b54e086c9b50cc577339a6
Andiry Xu <andiry.xu@amd.com>
2010-10-27 16:44:52 +0800

    xHCI: release spinlock when setup interrupt
    Jiri Slaby reports spinlock is held while calling kmalloc(GFP_KERNEL)
    and request_irq() in xhci_resume().
    
    Release the spinlock when setup interrupt.
    
    Reported-by: Jiri Slaby <jirislaby@gmail.com>
    Signed-off-by: Andiry Xu <andiry.xu@amd.com>
    Signed-off-by: Sarah Sharp <sarah.a.sharp@linux.intel.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -715,6 +715,7 @@ int xhci_resume(struct xhci_hcd *xhci, bool hibernated)
 		return retval;
 	}
 
+	spin_unlock_irq(&xhci->lock);
 	/* Re-setup MSI-X */
 	if (hcd->irq)
 		free_irq(hcd->irq, hcd);
@@ -737,6 +738,7 @@ int xhci_resume(struct xhci_hcd *xhci, bool hibernated)
 		hcd->irq = pdev->irq;
 	}
 
+	spin_lock_irq(&xhci->lock);
 	/* step 4: set Run/Stop bit */
 	command = xhci_readl(xhci, &xhci->op_regs->command);
 	command |= CMD_RUN;
