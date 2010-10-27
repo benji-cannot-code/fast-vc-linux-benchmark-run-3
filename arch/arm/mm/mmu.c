4eefc689868c54b38d48fab5ff5ff39c67042101
Russell King <rmk+kernel@arm.linux.org.uk>
2010-10-27 18:14:56 +0100

    ARM: ensure membank array is always sorted
    This was missing from the noMMU code, so there was the possibility
    of things not working as expected if out of order memory information
    was passed.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mman.h>
 #include <linux/nodemask.h>
 #include <linux/memblock.h>
-#include <linux/sort.h>
 #include <linux/fs.h>
 
 #include <asm/cputype.h>
@@ -1012,13 +1011,6 @@ static void __init map_lowmem(void)
 	}
 }
 
-static int __init meminfo_cmp(const void *_a, const void *_b)
-{
-	const struct membank *a = _a, *b = _b;
-	long cmp = bank_pfn_start(a) - bank_pfn_start(b);
-	return cmp < 0 ? -1 : cmp > 0 ? 1 : 0;
-}
-
 /*
  * paging_init() sets up the page tables, initialises the zone memory
  * maps, and sets up the zero page, bad page and bad page tables.
@@ -1026,8 +1018,6 @@ static int __init meminfo_cmp(const void *_a, const void *_b)
 void __init paging_init(struct machine_desc *mdesc)
 {
 	void *zero_page;
-
-	sort(&meminfo.bank, meminfo.nr_banks, sizeof(meminfo.bank[0]), meminfo_cmp, NULL);
 
 	build_mem_type_table();
 	sanity_check_meminfo();
