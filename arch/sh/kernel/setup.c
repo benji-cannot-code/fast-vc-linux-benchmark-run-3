b013436816605eeccd27c11c6273f9c947e25508
Paul Mundt <lethal@linux-sh.org>
2010-10-27 14:24:34 +0900

    sh: Fix the sparsemem disabled build.
    The introduction of MAX_PHYSMEM_BITS in to the initial cpuinfo struct
    causes a build error when sparsemem is disabled and asm/sparsemem.h is
    not brought in by other means. Include it explicitly.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/setup.c
+++ arch/sh/kernel/setup.c
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/smp.h>
 #include <asm/mmu_context.h>
 #include <asm/mmzone.h>
+#include <asm/sparsemem.h>
 
 /*
  * Initialize loops_per_jiffy as 10000000 (1000MIPS).
