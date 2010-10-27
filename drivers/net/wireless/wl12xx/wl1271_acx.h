76a7cf2a5e3dd283ea2b7cef2e276ab58e235db7
Eliad Peller <eliad@wizery.com>
2010-10-27 14:09:57 +0200

    wl1271: handle HW watchdog interrupt
    unmask the WL1271_ACX_INTR_WATCHDOG interrupt.
    when getting it - enqueue a recovery work and bail out.
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>
    Reviewed-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: Luciano Coelho <luciano.coelho@nokia.com>

--- drivers/net/wireless/wl12xx/wl1271_acx.h
+++ drivers/net/wireless/wl12xx/wl1271_acx.h
@@ -62,7 +62,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 					    WL1271_ACX_INTR_HW_AVAILABLE  | \
 					    WL1271_ACX_INTR_DATA)
 
-#define WL1271_INTR_MASK                   (WL1271_ACX_INTR_EVENT_A      | \
+#define WL1271_INTR_MASK                   (WL1271_ACX_INTR_WATCHDOG     | \
+					    WL1271_ACX_INTR_EVENT_A      | \
 					    WL1271_ACX_INTR_EVENT_B      | \
 					    WL1271_ACX_INTR_HW_AVAILABLE | \
 					    WL1271_ACX_INTR_DATA)
