01184e449be8e1b07c37b2883bb2d251b4f36ada
Namhyung Kim <namhyung@gmail.com>
2010-10-27 15:33:50 -0700

    ptrace: cleanup arch_ptrace() on avr32
    use new 'datap' variable type of void pointer in order to remove unnecessary
    castings.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>
    Acked-by: Haavard Skinnemoen <hskinnemoen@atmel.com>
    Cc: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/avr32/kernel/ptrace.c
+++ arch/avr32/kernel/ptrace.c
@@ -151,6 +151,7 @@ long arch_ptrace(struct task_struct *child, long request,
 		 unsigned long addr, unsigned long data)
 {
 	int ret;
+	void __user *datap = (void __user *) data;
 
 	switch (request) {
 	/* Read the word at location addr in the child process */
@@ -160,8 +161,7 @@ long arch_ptrace(struct task_struct *child, long request,
 		break;
 
 	case PTRACE_PEEKUSR:
-		ret = ptrace_read_user(child, addr,
-				       (unsigned long __user *)data);
+		ret = ptrace_read_user(child, addr, datap);
 		break;
 
 	/* Write the word in data at location addr */
@@ -175,11 +175,11 @@ long arch_ptrace(struct task_struct *child, long request,
 		break;
 
 	case PTRACE_GETREGS:
-		ret = ptrace_getregs(child, (void __user *)data);
+		ret = ptrace_getregs(child, datap);
 		break;
 
 	case PTRACE_SETREGS:
-		ret = ptrace_setregs(child, (const void __user *)data);
+		ret = ptrace_setregs(child, datap);
 		break;
 
 	default:
