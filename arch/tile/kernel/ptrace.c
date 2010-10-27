20b5921ff7145b718f1a3e0d8f971604d289419d
Namhyung Kim <namhyung@gmail.com>
2010-10-27 15:34:04 -0700

    ptrace: cleanup arch_ptrace() on tile
    Remove checking @addr less than 0 because @addr is now unsigned.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>
    Acked-by: Chris Metcalf <cmetcalf@tilera.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/tile/kernel/ptrace.c
+++ arch/tile/kernel/ptrace.c
@@ -59,7 +59,7 @@ long arch_ptrace(struct task_struct *child, long request,
 	switch (request) {
 
 	case PTRACE_PEEKUSR:  /* Read register from pt_regs. */
-		if (addr < 0 || addr >= PTREGS_SIZE)
+		if (addr >= PTREGS_SIZE)
 			break;
 		childreg = (char *)task_pt_regs(child) + addr;
 #ifdef CONFIG_COMPAT
@@ -78,7 +78,7 @@ long arch_ptrace(struct task_struct *child, long request,
 		break;
 
 	case PTRACE_POKEUSR:  /* Write register in pt_regs. */
-		if (addr < 0 || addr >= PTREGS_SIZE)
+		if (addr >= PTREGS_SIZE)
 			break;
 		childreg = (char *)task_pt_regs(child) + addr;
 #ifdef CONFIG_COMPAT
