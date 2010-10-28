daab4f9e0af70f93d6e0afdce25f4ec2865627f7
Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
2010-10-28 17:21:59 -0400

    fanotify: Fix FAN_CLOSE comments
    The comments for FAN_CLOSE_WRITE and FAN_CLOSE_NOWRITE do not match
    FS_CLOSE_WRITE and FS_CLOSE_NOWRITE, respectively.  WRITE is for
    writable files while NOWRITE is for non-writable files.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- include/linux/fanotify.h
+++ include/linux/fanotify.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* the following events that user-space can register for */
 #define FAN_ACCESS		0x00000001	/* File was accessed */
 #define FAN_MODIFY		0x00000002	/* File was modified */
-#define FAN_CLOSE_WRITE		0x00000008	/* Unwrittable file closed */
-#define FAN_CLOSE_NOWRITE	0x00000010	/* Writtable file closed */
+#define FAN_CLOSE_WRITE		0x00000008	/* Writtable file closed */
+#define FAN_CLOSE_NOWRITE	0x00000010	/* Unwrittable file closed */
 #define FAN_OPEN		0x00000020	/* File was opened */
 
 #define FAN_Q_OVERFLOW		0x00004000	/* Event queued overflowed */
