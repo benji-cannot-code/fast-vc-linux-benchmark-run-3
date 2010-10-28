5eaf568e71322918bb548b293a28e24c51bc9a8a
Alexey Chernov <4ernov@gmail.com>
2010-10-28 18:12:02 -0300

    [media] Patch for cx18 module with added support of GoTView PCI DVD3 Hybrid tuner
    I've got code which adds support of GoTView PCI DVD3 Hybrid tuner in
    cx18 module and Andy Walls in ivtv mailing-list gave me some advice on
    making a patch and sending it here. So here's the patch against
    staging/2.6.37-rc1 branch (the tutorial recommends to include it as
    plain text but if it's the case I can surely send as an attachment):
    
    Several comments on the patch:
    
    1. Both users on the official Gotview forum and support said that PCI
    DVD3 is very similar to Yuan MPC718 card so the main part of code is
    taken from Yuan configuration. Some users reported it to work properly.
    
    2. Everything is being initialized correctly including analog, dvb,
    radio and alsa parts. Analogue part and alsa virtual card is tested by
    myself using original Gotview card.
    
    [mchehab@redhat.com: Whitespace cleanups]
    Signed-off-by: Alexey Chernov <4ernov@gmail.com>
    Signed-off-by: Andy Walls <awalls@md.metrocast.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-driver.h
+++ drivers/media/video/cx18/cx18-driver.h
@@ -85,7 +85,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CX18_CARD_TOSHIBA_QOSMIO_DVBT 5 /* Toshiba Qosmio Interal DVB-T/Analog*/
 #define CX18_CARD_LEADTEK_PVR2100     6 /* Leadtek WinFast PVR2100 */
 #define CX18_CARD_LEADTEK_DVR3100H    7 /* Leadtek WinFast DVR3100 H */
-#define CX18_CARD_LAST 		      7
+#define CX18_CARD_GOTVIEW_PCI_DVD3    8 /* GoTView PCI DVD3 Hybrid */
+#define CX18_CARD_LAST		      8
 
 #define CX18_ENC_STREAM_TYPE_MPG  0
 #define CX18_ENC_STREAM_TYPE_TS   1
@@ -107,6 +108,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CX18_PCI_ID_CONEXANT		0x14f1
 #define CX18_PCI_ID_TOSHIBA		0x1179
 #define CX18_PCI_ID_LEADTEK		0x107D
+#define CX18_PCI_ID_GOTVIEW		0x5854
 
 /* ======================================================================== */
 /* ========================== START USER SETTABLE DMA VARIABLES =========== */
