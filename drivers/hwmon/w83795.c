17ae34970932059b3a64d7f67ce4cf029c5be665
Jean Delvare <khali@linux-fr.org>
2010-10-28 20:31:49 +0200

    hwmon: (w83795) Add myself as co-author and maintainer
    I've made so many changes to the w83795 driver that it's only fair to
    list myself as a co-author. I'll also maintain the driver for some
    time. There's more work needed on the driver for sure.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Acked-by: Guenter Roeck <guenter.roeck@ericsson.com>

--- drivers/hwmon/w83795.c
+++ drivers/hwmon/w83795.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  w83795.c - Linux kernel driver for hardware monitoring
  *  Copyright (C) 2008 Nuvoton Technology Corp.
  *                Wei Song
+ *  Copyright (C) 2010 Jean Delvare <khali@linux-fr.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -2098,7 +2099,7 @@ static void __exit sensors_w83795_exit(void)
 	i2c_del_driver(&w83795_driver);
 }
 
-MODULE_AUTHOR("Wei Song");
+MODULE_AUTHOR("Wei Song, Jean Delvare <khali@linux-fr.org>");
 MODULE_DESCRIPTION("W83795G/ADG hardware monitoring driver");
 MODULE_LICENSE("GPL");
 
