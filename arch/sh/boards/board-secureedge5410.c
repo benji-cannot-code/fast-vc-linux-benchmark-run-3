22898c5ebea7c260264f4a1176ee749685650304
Paul Mundt <lethal@linux-sh.org>
2010-10-29 19:06:53 +0900

    sh: mach-snapgear: Kill off machtype, consolidate board def.
    Only the secureedge5410 was ever supported by this code, so make the
    board specification explicit rather than perpetuating a mach group.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/board-secureedge5410.c
+++ arch/sh/boards/board-secureedge5410.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/sched.h>
 #include <asm/machvec.h>
-#include <mach/snapgear.h>
+#include <mach/secureedge5410.h>
 #include <asm/irq.h>
 #include <asm/io.h>
 #include <cpu/timer.h>
