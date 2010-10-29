e4549e7d170f3f70715fa3fd50e09235267ee9e9
Stephen Hemminger <stephen.hemminger@vyatta.com>
2010-10-29 08:20:11 -0700

    beceem: eliminate dead code
    Get rid of empty header file and unused declarations
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>

--- drivers/staging/bcm/headers.h
+++ drivers/staging/bcm/headers.h
@@ -70,7 +70,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "InterfaceMacros.h"
 #include "InterfaceAdapter.h"
 #include "InterfaceIsr.h"
-#include "Interfacemain.h"
 #include "InterfaceMisc.h"
 #include "InterfaceRx.h"
 #include "InterfaceTx.h"
