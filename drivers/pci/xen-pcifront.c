aa057488c9551f4012c50868d8ffde5780181245
Jesper Juhl <jj@chaosbits.net>
2010-10-30 14:51:30 +0200

    xen-pcifront: Remove duplicate inclusion of headers.
    In drivers/pci/xen-pcifront.c the xen/xenbus.h header is included twice -
    once is enough.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/pci/xen-pcifront.c
+++ drivers/pci/xen-pcifront.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/pci.h>
 #include <linux/msi.h>
-#include <xen/xenbus.h>
 #include <xen/interface/io/pciif.h>
 #include <asm/xen/pci.h>
 #include <linux/interrupt.h>
