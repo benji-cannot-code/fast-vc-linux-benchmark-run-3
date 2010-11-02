0bb60822c80d0547efd1ab020f80cae58ad16a6d
Marek Belisko <marek.belisko@gmail.com>
2010-11-02 14:51:47 +0100

    staging: ft1000: Check return value.
    Function ft1000_submit_rx_urb() could fail so add checking
    for return value.
    
    Signed-off-by: Marek Belisko <marek.belisko@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ft1000/ft1000-usb/ft1000_hw.c
+++ drivers/staging/ft1000/ft1000-usb/ft1000_hw.c
@@ -1348,6 +1348,7 @@ static int ft1000_open (struct net_device *dev)
 {
 	struct ft1000_info *pInfo = (struct ft1000_info *)netdev_priv(dev);
     struct timeval tv; //mbelian
+	int ret;
 
     DEBUG("ft1000_open is called for card %d\n", pInfo->CardNumber);
     //DEBUG("ft1000_open: dev->addr=%x, dev->addr_len=%d\n", dev->addr, dev->addr_len);
@@ -1365,8 +1366,9 @@ static int ft1000_open (struct net_device *dev)
 
     netif_carrier_on(dev); //mbelian
 
-    ft1000_submit_rx_urb(pInfo);
-    return 0;
+	ret = ft1000_submit_rx_urb(pInfo);
+
+	return ret;
 }
 
 //---------------------------------------------------------------------------
