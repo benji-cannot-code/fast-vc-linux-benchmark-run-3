926d2fd62fc8ffd423e83b56d16efe8302e38855
Marek Belisko <marek.belisko@gmail.com>
2010-11-03 11:19:52 +0100

    staging: ft1000: Get rid of ULONG typedef usage.
    Signed-off-by: Marek Belisko <marek.belisko@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ft1000/ft1000-usb/ft1000_usb.h
+++ drivers/staging/ft1000/ft1000-usb/ft1000_usb.h
@@ -99,7 +99,6 @@ struct prov_record {
 /*end of Jim*/
 #define DEBUG(args...) printk(KERN_INFO args)
 
-#define ULONG               u32 /* WTF ??? */
 #define BOOLEAN             u8
 #define PCHAR               u8 *
 #define UINT                u32
@@ -573,7 +572,7 @@ extern size_t FileLength;
 extern int numofmsgbuf;
 
 int ft1000_close (struct net_device *dev);
-u16 scram_dnldr(struct ft1000_device *ft1000dev, void *pFileStart, ULONG  FileLength);
+u16 scram_dnldr(struct ft1000_device *ft1000dev, void *pFileStart, u32  FileLength);
 
 extern struct list_head freercvpool;
 extern spinlock_t free_buff_lock;   // lock to arbitrate free buffer list for receive command data
