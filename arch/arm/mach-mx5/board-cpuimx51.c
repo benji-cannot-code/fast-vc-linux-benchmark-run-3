85df33de12277ac5934713ce69e70df5ac6952ae
Sascha Hauer <s.hauer@pengutronix.de>
2010-11-04 21:20:43 +0100

    ARM i.MX51 boards: Do not use PHYS_OFFSET
    PHYS_OFFSET may become a variable once the runtime PHYS_OFFSET
    patch is merged, so use MX51_PHYS_OFFSET for boot_params.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx5/board-cpuimx51.c
+++ arch/arm/mach-mx5/board-cpuimx51.c
@@ -293,7 +293,7 @@ static struct sys_timer mxc_timer = {
 
 MACHINE_START(EUKREA_CPUIMX51, "Eukrea CPUIMX51 Module")
 	/* Maintainer: Eric Bénard <eric@eukrea.com> */
-	.boot_params = PHYS_OFFSET + 0x100,
+	.boot_params = MX51_PHYS_OFFSET + 0x100,
 	.map_io = mx51_map_io,
 	.init_irq = mx51_init_irq,
 	.init_machine = eukrea_cpuimx51_init,
