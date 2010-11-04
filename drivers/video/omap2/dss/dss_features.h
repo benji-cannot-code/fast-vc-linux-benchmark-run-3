fdbd96ff0d274f35029334f8e0cd70675ecf1e42
Samreen <samreen@ti.com>
2010-11-04 12:28:41 +0100

    OMAP3: DSS2: Split OMAP3 has feature for 3430 & 3630
    Splitting the OMAP3 dss has feature separately for
    omap3430 & omap3630
    
    Signed-off-by: Samreen <samreen@ti.com>
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- drivers/video/omap2/dss/dss_features.h
+++ drivers/video/omap2/dss/dss_features.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum dss_feat_id {
 	FEAT_GLOBAL_ALPHA	= 1 << 0,
 	FEAT_GLOBAL_ALPHA_VID1	= 1 << 1,
+	FEAT_PRE_MULT_ALPHA	= 1 << 2,
 };
 
 /* DSS register field id */
