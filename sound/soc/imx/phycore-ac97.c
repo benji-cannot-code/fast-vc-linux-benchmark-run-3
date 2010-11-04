e9992fdaad6e32ca14f138e02a061c55054f055f
Sascha Hauer <s.hauer@pengutronix.de>
2010-11-04 17:05:41 +0100

    ASoC i.MX phycore ac97: remove unnecessary includes
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/imx/phycore-ac97.c
+++ sound/soc/imx/phycore-ac97.c
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc-dapm.h>
 #include <asm/mach-types.h>
 
-#include "../codecs/wm9712.h"
-#include "imx-ssi.h"
-
 static struct snd_soc_card imx_phycore;
 
 static struct snd_soc_ops imx_phycore_hifi_ops = {
