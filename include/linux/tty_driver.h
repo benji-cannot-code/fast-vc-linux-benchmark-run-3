fafd39d6ba7a5ca1adf4c3b5929643f712ba6c7c
Jiri Slaby <jslaby@suse.cz>
2010-11-04 16:20:23 +0100

    TTY: include termios.h in tty_driver.h
    We reference termios and termiox in tty_driver.h, but we do not include
    linux/termios.h where these are defined. Add the #include properly.
    
    Otherwise when we include tty_driver.h, we get compile errors.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Cc: Greg KH <gregkh@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/tty_driver.h
+++ include/linux/tty_driver.h
@@ -236,6 +236,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/list.h>
 #include <linux/cdev.h>
+#include <linux/termios.h>
 
 struct tty_struct;
 struct tty_driver;
