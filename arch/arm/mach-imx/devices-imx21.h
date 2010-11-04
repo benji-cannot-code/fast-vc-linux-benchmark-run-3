b8340e027262606b8c3712a7f4ea192decc6375c
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-11-04 10:09:10 +0100

    ARM: imx: dynamically allocate imx2-wdt devices
    Currently there is no platform data used in the driver.  In case this
    changes and for consistency NULL is passed unused to the soc specific
    functions.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-imx/devices-imx21.h
+++ arch/arm/mach-imx/devices-imx21.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/mx21.h>
 #include <mach/devices-common.h>
 
+extern const struct imx_imx2_wdt_data imx21_imx2_wdt_data __initconst;
+#define imx21_add_imx2_wdt(pdata)	\
+	imx_add_imx2_wdt(&imx21_imx2_wdt_data)
+
 extern const struct imx_imx_i2c_data imx21_imx_i2c_data __initconst;
 #define imx21_add_imx_i2c(pdata)	\
 	imx_add_imx_i2c(&imx21_imx_i2c_data, pdata)
