4e520208cd54c72b53f45bcaf588a8f213173f5d
Daniel Vetter <daniel.vetter@ffwll.ch>
2010-11-05 18:12:18 +0100

    drm/i915/gtt: call chipset flush directly
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -2193,7 +2193,7 @@ i915_gem_flush(struct drm_device *dev,
 	drm_i915_private_t *dev_priv = dev->dev_private;
 
 	if (flush_domains & I915_GEM_DOMAIN_CPU)
-		drm_agp_chipset_flush(dev);
+		intel_gtt_chipset_flush();
 
 	if ((flush_domains | invalidate_domains) & I915_GEM_GPU_DOMAINS) {
 		if (flush_rings & RING_RENDER)
@@ -2921,14 +2921,13 @@ i915_gem_object_flush_gtt_write_domain(struct drm_gem_object *obj)
 static void
 i915_gem_object_flush_cpu_write_domain(struct drm_gem_object *obj)
 {
-	struct drm_device *dev = obj->dev;
 	uint32_t old_write_domain;
 
 	if (obj->write_domain != I915_GEM_DOMAIN_CPU)
 		return;
 
 	i915_gem_clflush_object(obj);
-	drm_agp_chipset_flush(dev);
+	intel_gtt_chipset_flush();
 	old_write_domain = obj->write_domain;
 	obj->write_domain = 0;
 
@@ -5070,7 +5069,7 @@ void i915_gem_detach_phys_object(struct drm_device *dev,
 			page_cache_release(page);
 		}
 	}
-	drm_agp_chipset_flush(dev);
+	intel_gtt_chipset_flush();
 
 	obj_priv->phys_obj->cur_obj = NULL;
 	obj_priv->phys_obj = NULL;
@@ -5162,7 +5161,7 @@ i915_gem_phys_pwrite(struct drm_device *dev, struct drm_gem_object *obj,
 			return -EFAULT;
 	}
 
-	drm_agp_chipset_flush(dev);
+	intel_gtt_chipset_flush();
 	return 0;
 }
 
--- include/drm/intel-gtt.h
+++ include/drm/intel-gtt.h
@@ -14,6 +14,7 @@ const struct intel_gtt {
 	unsigned int gtt_mappable_entries;
 } *intel_gtt_get(void);
 
+void intel_gtt_chipset_flush(void);
 
 /* Special gtt memory types */
 #define AGP_DCACHE_MEMORY	1
@@ -27,4 +28,3 @@ const struct intel_gtt {
 #define AGP_USER_CACHED_MEMORY_GFDT (1 << 3)
 
 #endif
-
