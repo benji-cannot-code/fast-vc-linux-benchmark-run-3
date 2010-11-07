c98192443068e7e257f9b9ff91bc8fc793d14129
Jesper Juhl <jj@chaosbits.net>
2010-11-07 23:24:44 +0100

    infiniband: Only include mutex.h once in drivers/infiniband/hw/cxgb4/iw_cxgb4.h
    Only include the header linux/mutex.h once inside
    drivers/infiniband/hw/cxgb4/iw_cxgb4.h
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/infiniband/hw/cxgb4/iw_cxgb4.h
+++ drivers/infiniband/hw/cxgb4/iw_cxgb4.h
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <linux/io.h>
 #include <linux/kfifo.h>
-#include <linux/mutex.h>
 
 #include <asm/byteorder.h>
 
