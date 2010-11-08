86b997cc85cf266fc6c933e51acb8097328abf7a
Felipe Balbi <balbi@ti.com>
2010-11-08 06:56:15 +0000

    arm: omap: zoom: substitute gpio number with symbolic name
    It's easier to understand what that number means and
    also avoids problems if we ever have to change it.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-zoom.c
+++ arch/arm/mach-omap2/board-zoom.c
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sdram-micron-mt46h32m32lf-6.h"
 #include "sdram-hynix-h8mbx00u0mer-0em.h"
 
+#define ZOOM3_EHCI_RESET_GPIO		64
+
 static void __init omap_zoom_init_irq(void)
 {
 	if (machine_is_omap_zoom2())
@@ -113,7 +115,7 @@ static const struct ehci_hcd_omap_platform_data ehci_pdata __initconst = {
 	.port_mode[2]		= EHCI_HCD_OMAP_MODE_UNKNOWN,
 	.phy_reset		= true,
 	.reset_gpio_port[0]	= -EINVAL,
-	.reset_gpio_port[1]	= 64,
+	.reset_gpio_port[1]	= ZOOM3_EHCI_RESET_GPIO,
 	.reset_gpio_port[2]	= -EINVAL,
 };
 
@@ -123,7 +125,7 @@ static void __init omap_zoom_init(void)
 		omap3_mux_init(board_mux, OMAP_PACKAGE_CBB);
 	} else if (machine_is_omap_zoom3()) {
 		omap3_mux_init(board_mux, OMAP_PACKAGE_CBP);
-		omap_mux_init_gpio(64, OMAP_PIN_OUTPUT);
+		omap_mux_init_gpio(ZOOM3_EHCI_RESET_GPIO, OMAP_PIN_OUTPUT);
 		usb_ehci_init(&ehci_pdata);
 	}
 
