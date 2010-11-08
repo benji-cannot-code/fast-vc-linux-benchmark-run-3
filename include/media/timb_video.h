4cf18ae5d7f4fb77c5503e5440f8b53c3921ed15
Richard Röjfors <richard.rojfors@pelagicore.com>
2010-11-08 10:45:44 -0300

    [media] media: Add timberdale video-in driver
    This patch adds the timberdale video-in driver.
    
    The video IP of timberdale delivers the video data via DMA.
    The driver uses the DMA api to handle DMA transfers, and make use
    of the V4L2 video buffers to handle buffers against user space.
    
    If available the driver uses an encoder to get/set the video standard
    
    Signed-off-by: Richard Röjfors <richard.rojfors@pelagicore.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/timb_video.h
+++ include/media/timb_video.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * timb_radio.h Platform struct for the Timberdale radio driver
- * Copyright (c) 2009 Intel Corporation
+ * timb_video.h Platform struct for the Timberdale video driver
+ * Copyright (c) 2009-2010 Intel Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -17,20 +17,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#ifndef _TIMB_RADIO_
-#define _TIMB_RADIO_ 1
+#ifndef _TIMB_VIDEO_
+#define _TIMB_VIDEO_ 1
 
 #include <linux/i2c.h>
 
-struct timb_radio_platform_data {
-	int i2c_adapter; /* I2C adapter where the tuner and dsp are attached */
-	struct {
-		struct i2c_board_info *info;
-	} tuner;
+struct timb_video_platform_data {
+	int dma_channel;
+	int i2c_adapter; /* The I2C adapter where the encoder is attached */
 	struct {
 		const char *module_name;
 		struct i2c_board_info *info;
-	} dsp;
+	} encoder;
 };
 
 #endif
