7e13782b16b3bdf708c398130714836b1c91b29a
Steven Rostedt <srostedt@redhat.com>
2010-11-08 16:39:57 -0500

    ktest: Write to stdout if no log file is given
    If no LOG_FILE option is set, then write what would be logged to
    that file to standard output.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -231,7 +231,7 @@ sub read_config {
     }
 }
 
-sub logit {
+sub _logit {
     if (defined($opt{"LOG_FILE"})) {
 	open(OUT, ">> $opt{LOG_FILE}") or die "Can't write to $opt{LOG_FILE}";
 	print OUT @_;
@@ -239,9 +239,17 @@ sub logit {
     }
 }
 
+sub logit {
+    if (defined($opt{"LOG_FILE"})) {
+	_logit @_;
+    } else {
+	print @_;
+    }
+}
+
 sub doprint {
     print @_;
-    logit @_;
+    _logit @_;
 }
 
 sub run_command;
