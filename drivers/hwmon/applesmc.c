a5a29ebf27f9237caa0abbbed9631d02e8e5e2e8
Henrik Rydberg <rydberg@euromail.se>
2010-11-09 15:15:11 +0000

    hwmon: (applesmc) Update copyright information
    With the preceding patches, git blame assigns about half of
    the file to the present author. Add a line to the copyright
    to reflect this.
    
    Signed-off-by: Henrik Rydberg <rydberg@euromail.se>
    Signed-off-by: Guenter Roeck <guenter.roeck@ericsson.com>

--- drivers/hwmon/applesmc.c
+++ drivers/hwmon/applesmc.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * computers.
  *
  * Copyright (C) 2007 Nicolas Boichat <nicolas@boichat.ch>
+ * Copyright (C) 2010 Henrik Rydberg <rydberg@euromail.se>
  *
  * Based on hdaps.c driver:
  * Copyright (C) 2005 Robert Love <rml@novell.com>
