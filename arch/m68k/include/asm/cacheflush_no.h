846db68e18568614b5977652446825be81202355
Greg Ungerer <gerg@uclinux.org>
2010-11-09 14:27:50 +1000

    m68knommu: make cache push code ColdFire generic
    Currently the code to push cache lines is only available to version 4
    cores. Version 3 cores may also need to use this if we support copy-
    back caches on them. Move this code to make it more generic, and
    useful for all version ColdFire cores.
    
    With this in place we can now have a single cache_flush_all() code
    path that does all the right things on all version cores.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/cacheflush_no.h
+++ arch/m68k/include/asm/cacheflush_no.h
@@ -31,9 +31,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define copy_from_user_page(vma, page, vaddr, dst, src, len) \
 	memcpy(dst, src, len)
 
-#ifndef __flush_cache_all
+void mcf_cache_push(void);
+
 static inline void __flush_cache_all(void)
 {
+#ifdef CACHE_PUSH
+	mcf_cache_push();
+#endif
 #ifdef CACHE_INVALIDATE
 	__asm__ __volatile__ (
 		"movel	%0, %%d0\n\t"
@@ -42,6 +46,5 @@ static inline void __flush_cache_all(void)
 		: : "i" (CACHE_INVALIDATE) : "d0" );
 #endif
 }
-#endif /* __flush_cache_all */
 
 #endif /* _M68KNOMMU_CACHEFLUSH_H */
