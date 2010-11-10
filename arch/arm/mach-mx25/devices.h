2835ee7240e3fa6572cdc9f8bb07efb08a0b1451
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-11-10 21:27:55 +0100

    ARM: mx25: dynamically allocate mxc-ehci devices
    According to the reference manual of the i.MX25 the host controller uses an
    offset of 0x200 not 0x400 as was specified in the resources for mxc_usbh2.
    
    Needs-Testing: yes
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx25/devices.h
+++ arch/arm/mach-mx25/devices.h
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-extern struct platform_device mxc_otg;
 extern struct platform_device otg_udc_device;
-extern struct platform_device mxc_usbh2;
 extern struct platform_device mxc_pwm_device0;
 extern struct platform_device mxc_pwm_device1;
 extern struct platform_device mxc_pwm_device2;
