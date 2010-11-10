f5754c81c06d86fa697bd360be19022d2295d4e4
Feng Tang <feng.tang@intel.com>
2010-11-10 17:29:00 +0000

    x86: mrst: Add vrtc driver which serves as a wall clock device
    Moorestown platform doesn't have a m146818 RTC device like traditional
    x86 PC, but a firmware emulated virtual RTC device(vrtc), which provides
    some basic RTC functions like get/set time. vrtc serves as the only
    wall clock device on Moorestown platform.
    
    [ tglx: Changed the exports to _GPL ]
    
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    LKML-Reference: <20101110172837.3311.40483.stgit@localhost.localdomain>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/platform/mrst/mrst.c
+++ arch/x86/platform/mrst/mrst.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/apb_timer.h>
 #include <asm/reboot.h>
 
-
 /*
  * the clockevent devices on Moorestown/Medfield can be APBT or LAPIC clock,
  * cmdline option x86_mrst_timer can be used to override the configuration
@@ -241,11 +240,6 @@ void __init mrst_time_init(void)
 	/* we need at least one APB timer */
 	pre_init_apic_IRQ0();
 	apbt_time_init();
-}
-
-void __init mrst_rtc_init(void)
-{
-	sfi_table_parse(SFI_SIG_MRTC, NULL, NULL, sfi_parse_mrtc);
 }
 
 void __cpuinit mrst_arch_setup(void)
