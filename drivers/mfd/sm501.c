5ed43c0a8cb946c4cdf0ec33388f07b74a7de8ce
Axel Lin <axel.lin@gmail.com>
2010-11-10 15:47:51 +0800

    mfd: Include <linux/io.h> instead of <asm/io.h>
    As warned by checkpatch.pl, use #include <linux/io.h> instead of <asm/io.h>
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Acked-by: Ben Dooks <ben@simtec.co.uk>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/sm501.c
+++ drivers/mfd/sm501.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sm501-regs.h>
 #include <linux/serial_8250.h>
 
-#include <asm/io.h>
+#include <linux/io.h>
 
 struct sm501_device {
 	struct list_head		list;
