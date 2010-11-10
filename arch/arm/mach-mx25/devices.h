1cea4f9e4fd39bc49f501beff6ae2cc331a86f0f
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-11-10 22:15:45 +0100

    ARM: mx25: dynamically allocate mxc_pwm devices
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx25/devices.h
+++ arch/arm/mach-mx25/devices.h
@@ -1,8 +1,4 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-extern struct platform_device mxc_pwm_device0;
-extern struct platform_device mxc_pwm_device1;
-extern struct platform_device mxc_pwm_device2;
-extern struct platform_device mxc_pwm_device3;
 extern struct platform_device mxc_keypad_device;
 extern struct platform_device mx25_rtc_device;
 extern struct platform_device mx25_fb_device;
