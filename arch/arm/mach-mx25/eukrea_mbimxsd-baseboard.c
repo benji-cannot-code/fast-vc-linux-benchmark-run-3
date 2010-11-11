764a8ae763470cabf30eeae91e53fe418f5b4cfb
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-11-11 17:30:25 +0100

    ARM: mx25: remove now empty devices.h
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx25/eukrea_mbimxsd-baseboard.c
+++ arch/arm/mach-mx25/eukrea_mbimxsd-baseboard.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/audmux.h>
 
 #include "devices-imx25.h"
-#include "devices.h"
 
 static struct pad_desc eukrea_mbimxsd_pads[] = {
 	/* LCD */
