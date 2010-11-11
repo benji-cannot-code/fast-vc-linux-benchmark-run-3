4dff07e919dcc7359b69783a0dd1a438bd283e30
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-11-11 11:57:21 +0100

    ARM: mx25: dynamically allocate imxdi_rtc devices
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx25/devices.h
+++ arch/arm/mach-mx25/devices.h
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-extern struct platform_device mx25_rtc_device;
 extern struct platform_device mx25_fb_device;
 extern struct platform_device mxc_wdt;
 extern struct platform_device mx25_csi_device;
