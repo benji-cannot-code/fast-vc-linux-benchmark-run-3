e03e8fe20fd7c5ac69cfba2a0e785663e2df931c
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2010-11-11 15:45:04 +0100

    fbdev: when parsing E-EDID blocks, also use SVD entries
    Add parsing of E-EDID SVD entries. In this first version only a few
    CEA/EIA-861E modes are implemented, more can be added as needed.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -1152,6 +1152,7 @@ struct fb_videomode {
 
 extern const char *fb_mode_option;
 extern const struct fb_videomode vesa_modes[];
+extern const struct fb_videomode cea_modes[64];
 
 struct fb_modelist {
 	struct list_head list;
