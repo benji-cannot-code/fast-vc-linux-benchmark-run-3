1ae6841a806ad739d54d1528d1d4f7b14976e240
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2010-11-12 11:49:34 +0100

    ARM: mx3: dynamically allocate mxc_rnga devices
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- arch/arm/mach-mx3/devices.c
+++ arch/arm/mach-mx3/devices.c
@@ -30,23 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "devices.h"
 
-#ifdef CONFIG_ARCH_MX31
-static struct resource rnga_resources[] = {
-	{
-		.start = MX3x_RNGA_BASE_ADDR,
-		.end = MX3x_RNGA_BASE_ADDR + 0x28,
-		.flags = IORESOURCE_MEM,
-	},
-};
-
-struct platform_device mxc_rnga_device = {
-	.name = "mxc_rnga",
-	.id = -1,
-	.num_resources = 1,
-	.resource = rnga_resources,
-};
-#endif /* CONFIG_ARCH_MX31 */
-
 /* i.MX31 Image Processing Unit */
 
 /* The resource order is important! */
@@ -263,7 +246,6 @@ static int __init mx3_devices_init(void)
 	if (cpu_is_mx31()) {
 		imx_wdt_resources[0].start = MX31_WDOG_BASE_ADDR;
 		imx_wdt_resources[0].end = MX31_WDOG_BASE_ADDR + 0x3fff;
-		mxc_register_device(&mxc_rnga_device, NULL);
 	}
 #endif
 #if defined(CONFIG_ARCH_MX35)
