2af36d7648a03dc036f7b794ea6b0dbb993c33ca
Alan Cox <alan@linux.intel.com>
2010-11-12 10:46:23 +0000

    serial: ifx6x60: Fix missing include for msleep
    Noted by Stephen Rothwell
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/serial/ifx6x60.c
+++ drivers/serial/ifx6x60.c
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm.h>
 #include <linux/pm_runtime.h>
 #include <linux/spi/ifx_modem.h>
+#include <linux/delay.h>
 
 #include "ifx6x60.h"
 
