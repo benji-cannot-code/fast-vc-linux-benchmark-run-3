9b1b2debac60a555f36b35aa5ec03f4fc5bd5308
Felix Fietkau <nbd@openwrt.org>
2010-11-14 15:20:12 +0100

    ath9k: more tx setup cleanups
    - remove the BUF_HT flag, and instead check for IEEE80211_TX_CTL_AMPDU
      before calling ath_tx_send_ampdu.
    - remove a few unused variables
    - calculate frame length before adding the frame padding
    - merge the misnamed ath_tx_start_dma function into ath_tx_start
    - remove an unused argument for assign_aggr_tid_seqno
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/ath9k.h
+++ drivers/net/wireless/ath/ath9k/ath9k.h
@@ -87,7 +87,6 @@ struct ath_config {
 /**
  * enum buffer_type - Buffer type flags
  *
- * @BUF_HT: Send this buffer using HT capabilities
  * @BUF_AMPDU: This buffer is an ampdu, as part of an aggregate (during TX)
  * @BUF_AGGR: Indicates whether the buffer can be aggregated
  *	(used in aggregation scheduling)
@@ -95,7 +94,6 @@ struct ath_config {
  * @BUF_XRETRY: To denote excessive retries of the buffer
  */
 enum buffer_type {
-	BUF_HT			= BIT(1),
 	BUF_AMPDU		= BIT(2),
 	BUF_AGGR		= BIT(3),
 	BUF_RETRY		= BIT(4),
@@ -103,7 +101,6 @@ enum buffer_type {
 };
 
 #define bf_retries      	bf_state.bfs_retries
-#define bf_isht(bf)		(bf->bf_state.bf_type & BUF_HT)
 #define bf_isampdu(bf)		(bf->bf_state.bf_type & BUF_AMPDU)
 #define bf_isaggr(bf)		(bf->bf_state.bf_type & BUF_AGGR)
 #define bf_isretried(bf)	(bf->bf_state.bf_type & BUF_RETRY)
@@ -266,6 +263,7 @@ struct ath_tx_control {
 	struct ath_txq *txq;
 	int if_id;
 	enum ath9k_internal_frame_type frame_type;
+	int frmlen;
 	u8 paprd;
 };
 
