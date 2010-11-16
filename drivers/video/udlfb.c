bc21f0f0ee8376d7eaa9d4a7a18b0ebab01a500a
Paul Mundt <lethal@linux-sh.org>
2010-11-16 14:00:24 +0900

    fbdev: move udlfb out of staging.
    udlfb has undergone a fair bit of cleanup recently and is effectively at
    the point where it can be liberated from staging purgatory and promoted
    to a real driver.
    
    The outstanding cleanups are all minor, with some of them dependent on
    drivers/video headers, so these will be done incrementally from udlfb's
    new home.
    
    Requested-by: Bernie Thompson <bernie@plugable.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/video/udlfb.c
+++ drivers/video/udlfb.c
@@ -27,8 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 #include <linux/slab.h>
 #include <linux/delay.h>
-
-#include "udlfb.h"
+#include <video/udlfb.h>
 
 static struct fb_fix_screeninfo dlfb_fix = {
 	.id =           "udlfb",
