f2500abdf2174dc302aa357d6dd5d9dc1a9e8d7f
David S. Miller <davem@davemloft.net>
2010-11-16 12:24:16 -0800

    sparc64: Unexport prom_service_exists().
    Only used by functions in misc_64.c so make it private
    to that file.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/prom/misc_64.c
+++ arch/sparc/prom/misc_64.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/system.h>
 #include <asm/ldc.h>
 
-int prom_service_exists(const char *service_name)
+static int prom_service_exists(const char *service_name)
 {
 	unsigned long args[5];
 
