16a4e169f6b8333de07b29cd3a7bc5dc74d8d147
Yong Wang <yong.y.wang@linux.intel.com>
2010-11-17 22:09:54 +0800

    staging: spectra: asynchronous init
    The spectra nand driver takes quite some time to initialize because
    it needs to scan the whole nand disk to find the latest block table.
    This patch initializes the spectra nand driver asynchronously so that
    other things in the kernel can initialize in parallel to the scanning
    operation.
    
    Signed-off-by: Yong Wang <yong.y.wang@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/spectra/ffsport.c
+++ drivers/staging/spectra/ffsport.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/log2.h>
 #include <linux/init.h>
 #include <linux/slab.h>
+#include <linux/async.h>
 
 /**** Helper functions used for Div, Remainder operation on u64 ****/
 
@@ -730,7 +731,7 @@ static void create_sysfs_entry(struct device *dev)
 }
 */
 
-int register_spectra_ftl()
+static void register_spectra_ftl_async(void *unused, async_cookie_t cookie)
 {
 	int i;
 
@@ -739,7 +740,7 @@ int register_spectra_ftl()
 	if (PASS != GLOB_FTL_IdentifyDevice(&IdentifyDeviceData)) {
 		printk(KERN_ERR "Spectra: Unable to Read Flash Device. "
 		       "Aborting\n");
-		return -ENOMEM;
+		return;
 	} else {
 		nand_dbg_print(NAND_DBG_WARN, "In GLOB_SBD_init: "
 			       "Num blocks=%d, pagesperblock=%d, "
@@ -773,15 +774,19 @@ int register_spectra_ftl()
 		       "Spectra: module loaded with major number %d\n",
 		       GLOB_SBD_majornum);
 
-	return PASS;
+	return;
 
 out_blk_register:
 	unregister_blkdev(GLOB_SBD_majornum, GLOB_SBD_NAME);
 out_ftl_flash_register:
 	GLOB_FTL_Cache_Release();
 	printk(KERN_ERR "Spectra: Module load failed.\n");
+}
 
-	return FAIL;
+int register_spectra_ftl()
+{
+	async_schedule(register_spectra_ftl_async, NULL);
+	return 0;
 }
 EXPORT_SYMBOL_GPL(register_spectra_ftl);
 
