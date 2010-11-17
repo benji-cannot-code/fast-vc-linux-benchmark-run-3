36ff39d9b916de1eee2942b81f3fc247b26d2f49
Avi Kivity <avi@redhat.com>
2010-11-17 15:28:21 +0200

    KVM: x86 emulator: preserve an operand's segment identity
    Currently the x86 emulator converts the segment register associated with
    an operand into a segment base which is added into the operand address.
    This loss of information results in us not doing segment limit checks properly.
    
    Replace struct operand's addr.mem field by a segmented_address structure
    which holds both the effetive address and segment.  This will allow us to
    do the limit check at the point of access.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/include/asm/kvm_emulate.h
+++ arch/x86/include/asm/kvm_emulate.h
@@ -160,7 +160,10 @@ struct operand {
 	};
 	union {
 		unsigned long *reg;
-		unsigned long mem;
+		struct segmented_address {
+			ulong ea;
+			unsigned seg;
+		} mem;
 	} addr;
 	union {
 		unsigned long val;
