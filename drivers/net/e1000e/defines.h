93797e9fddaa81e2402cc599a4d61b7953135668
Bruce Allan <bruce.w.allan@intel.com>
2010-11-16 19:50:13 -0800

    e1000e: 82571 SerDes link handle null code word from partner
    SerDes Link detection on certain 82571 mezzanine cards can fail when the
    link is forced, the link partner does not recognize forced link and the
    link partner sends null code words.  Detect the null code words and return
    to auto-negotiation state which causes the link partner to begin responding
    with valid code words.  Within a reasonable interval the link will finally
    settle as forced by both partners.
    
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/e1000e/defines.h
+++ drivers/net/e1000e/defines.h
@@ -517,6 +517,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define E1000_TXCW_ANE        0x80000000        /* Auto-neg enable */
 
 /* Receive Configuration Word */
+#define E1000_RXCW_CW         0x0000ffff        /* RxConfigWord mask */
 #define E1000_RXCW_IV         0x08000000        /* Receive config invalid */
 #define E1000_RXCW_C          0x20000000        /* Receive config */
 #define E1000_RXCW_SYNCH      0x40000000        /* Receive config synch */
