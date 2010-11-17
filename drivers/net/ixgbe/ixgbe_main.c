7e9f4f3f9c065a170d607d2d76acce792abfbed7
Alexander Duyck <alexander.h.duyck@intel.com>
2010-11-16 19:26:52 -0800

    ixgbe: move ixgbe_clear_interrupt_scheme to before pci_save_state
    The main reason for this change is to keep the suspend/resume logic matched
    up. The clear_interrupt_scheme function will disable MSI-X which will
    effect the PCIe configuration space. Therefore we will want to do it before
    we save state to avoid having the interrupt state restored by
    pci_restore_state, and then trying to re-enable MSI/MSI-X interrupts via
    ixgbe_setup_interrupt_scheme.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Tested-by: Ross Brattain <ross.b.brattain@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ixgbe/ixgbe_main.c
+++ drivers/net/ixgbe/ixgbe_main.c
@@ -5328,6 +5328,8 @@ static int __ixgbe_shutdown(struct pci_dev *pdev, bool *enable_wake)
 		ixgbe_free_all_rx_resources(adapter);
 	}
 
+	ixgbe_clear_interrupt_scheme(adapter);
+
 #ifdef CONFIG_PM
 	retval = pci_save_state(pdev);
 	if (retval)
@@ -5360,8 +5362,6 @@ static int __ixgbe_shutdown(struct pci_dev *pdev, bool *enable_wake)
 		pci_wake_from_d3(pdev, false);
 
 	*enable_wake = !!wufc;
-
-	ixgbe_clear_interrupt_scheme(adapter);
 
 	ixgbe_release_hw_control(adapter);
 
