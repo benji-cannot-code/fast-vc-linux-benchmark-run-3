f949f1de64caf8ec4487a43beae44d82102949b1
David S. Miller <davem@davemloft.net>
2010-11-17 10:22:56 -0800

    sparc: Do not export prom_nb{get,put}char().
    Never used outside of console_{32,64}.c
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/prom/console_32.c
+++ arch/sparc/prom/console_32.c
@@ -20,8 +20,7 @@ extern void restore_current(void);
 /* Non blocking get character from console input device, returns -1
  * if no input was taken.  This can be used for polling.
  */
-int
-prom_nbgetchar(void)
+static int prom_nbgetchar(void)
 {
 	static char inc;
 	int i = -1;
@@ -52,8 +51,7 @@ prom_nbgetchar(void)
 /* Non blocking put character to console device, returns -1 if
  * unsuccessful.
  */
-int
-prom_nbputchar(char c)
+static int prom_nbputchar(char c)
 {
 	static char outc;
 	unsigned long flags;
