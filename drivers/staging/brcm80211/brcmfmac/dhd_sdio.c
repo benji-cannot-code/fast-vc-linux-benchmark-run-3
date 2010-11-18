f94079309f9fcd80a8b22bfdbf1dc460827c0578
Arend van Spriel <arend@broadcom.com>
2010-11-18 20:46:44 +0100

    staging: brcm80211: replaced PKTSETLEN macro by native __skb_trim call
    - removed PKTSETLEN macro and used __skb_trim call instead.
    - removed unused macros PKTSETPOOL and PKTPOOL
    
    Reviewed-by: Henry Ptasinski <henryp@broadcom.com>
    Reviewed-by: Brett Rudley <brudley@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/dhd_sdio.c
+++ drivers/staging/brcm80211/brcmfmac/dhd_sdio.c
@@ -367,7 +367,7 @@ extern void bcmsdh_enable_hw_oob_intr(void *sdh, bool enable);
 		ASSERT(PKTLEN((p)) >= ((len) + datalign));	\
 		if (datalign)						\
 			skb_pull((p), datalign);			\
-		PKTSETLEN((p), (len));				\
+		__skb_trim((p), (len));				\
 	} while (0)
 
 /* Limit on rounding up frames */
@@ -3522,7 +3522,7 @@ static u8 dhdsdio_rxglom(dhd_bus_t *bus, u8 rxseq)
 				prhex("Rx Subframe Data", dptr, dlen);
 #endif
 
-			PKTSETLEN(pfirst, sublen);
+			__skb_trim(pfirst, sublen);
 			skb_pull(pfirst, doff);
 
 			if (PKTLEN(pfirst) == 0) {
@@ -4152,7 +4152,7 @@ deliver:
 					prhex("Glom Data", PKTDATA(pkt), len);
 				}
 #endif
-				PKTSETLEN(pkt, len);
+				__skb_trim(pkt, len);
 				ASSERT(doff == SDPCM_HDRLEN);
 				skb_pull(pkt, SDPCM_HDRLEN);
 				bus->glomd = pkt;
@@ -4165,7 +4165,7 @@ deliver:
 		}
 
 		/* Fill in packet len and prio, deliver upward */
-		PKTSETLEN(pkt, len);
+		__skb_trim(pkt, len);
 		skb_pull(pkt, doff);
 
 #ifdef SDTEST
