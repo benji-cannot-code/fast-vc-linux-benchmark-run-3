1fa156676e5472ab4ed99d314c636dedc1da7a68
Bryan Wu <bryan.wu@canonical.com>
2010-11-18 09:45:59 +0800

    OMAP: DSS2: Add back authors of panel-generic.c based drivers
    Signed-off-by: Bryan Wu <bryan.wu@canonical.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>

--- drivers/video/omap2/displays/panel-generic-dpi.c
+++ drivers/video/omap2/displays/panel-generic-dpi.c
@@ -5,6 +5,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2010 Canonical Ltd.
  * Author: Bryan Wu <bryan.wu@canonical.com>
  *
+ * LCD panel driver for Sharp LQ043T1DG01
+ *
+ * Copyright (C) 2009 Texas Instruments Inc
+ * Author: Vaibhav Hiremath <hvaibhav@ti.com>
+ *
+ * LCD panel driver for Toppoly TDO35S
+ *
+ * Copyright (C) 2009 CompuLab, Ltd.
+ * Author: Mike Rapoport <mike@compulab.co.il>
+ *
  * Copyright (C) 2008 Nokia Corporation
  * Author: Tomi Valkeinen <tomi.valkeinen@nokia.com>
  *
