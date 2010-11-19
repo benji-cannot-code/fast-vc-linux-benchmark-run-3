cafffaa50490e0a84a205f8f2ed6930855d187d6
Michael Hennerich <michael.hennerich@analog.com>
2010-11-19 15:16:43 +0100

    staging: iio: dac: Update drivers to use new attribute naming
    Remove depreciated macro from header
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Acked-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/dac/dac.h
+++ drivers/staging/iio/dac/dac.h
@@ -3,9 +3,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * dac.h - sysfs attributes associated with DACs
  */
 
-/* Deprecated */
-#define IIO_DEV_ATTR_DAC(_num, _store, _addr)			\
-	IIO_DEVICE_ATTR(dac_##_num, S_IWUSR, NULL, _store, _addr)
-
 #define IIO_DEV_ATTR_OUT_RAW(_num, _store, _addr)				\
 	IIO_DEVICE_ATTR(out##_num##_raw, S_IWUSR, NULL, _store, _addr)
