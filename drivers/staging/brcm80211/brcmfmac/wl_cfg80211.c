e1a6d4fcac9062e06971f25d278c44fbf79a210b
Huang Weiyi <weiyi.huang@gmail.com>
2010-11-20 20:06:18 +0800

    staging: brcm80211: remove duplicated #include
    Remove duplicated #include('s) in
      drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
+++ drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <dhdioctl.h>
 #include <wlioctl.h>
 
-#include <proto/ethernet.h>
-#include <dngl_stats.h>
-#include <dhd.h>
-
 #include <linux/kthread.h>
 #include <linux/netdevice.h>
 #include <linux/sched.h>
