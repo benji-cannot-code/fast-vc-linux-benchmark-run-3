146e8216fc9487bade1fc3809e8f265cc2d3dda3
Jarkko Nikula <jhnikula@gmail.com>
2010-11-21 19:48:47 +0200

    ASoC: Remove cyclic dependency between soc.h and soc-dapm.h/soc-dai.h
    There is no need anymore to include soc.h in soc-dapm.h and soc-dai.h as
    drivers are converted to include only soc.h.
    
    Thanks to Lars-Peter Clausen <lars@metafoo.de> for pointing out the issue.
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/sound/soc-dapm.h
+++ include/sound/soc-dapm.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/types.h>
 #include <sound/control.h>
-#include <sound/soc.h>
 
 /* widget has no PM register bit */
 #define SND_SOC_NOPM	-1
