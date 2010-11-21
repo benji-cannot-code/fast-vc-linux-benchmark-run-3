4a547be0c5934f0c37dd002545e9f83451164fb9
Jarkko Nikula <jhnikula@gmail.com>
2010-11-21 19:48:44 +0200

    ASoC: Fix compile breakage in jz4740.c and smartq_wm8987.c
    Commit ce6120c require that soc-dapm.h cannot be included before soc.h but
    these two drivers were not checked. Fix them by including only soc.h as it
    includes soc-dapm.h.
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/jz4740.c
+++ sound/soc/codecs/jz4740.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/initval.h>
-#include <sound/soc-dapm.h>
 #include <sound/soc.h>
 
 #define JZ4740_REG_CODEC_1 0x0
