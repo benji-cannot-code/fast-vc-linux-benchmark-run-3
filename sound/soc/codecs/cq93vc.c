b1ab21f8684b61f52d0e35089f31d3e945f4431b
Jarkko Nikula <jhnikula@gmail.com>
2010-11-21 19:48:46 +0200

    ASoC: Do not include soc-dai.h
    There is no need to include soc-dai.h since soc.h includes it. Convert
    drivers to include only soc.h.
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/cq93vc.c
+++ sound/soc/codecs/cq93vc.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
-#include <sound/soc-dai.h>
 #include <sound/initval.h>
 
 #include <mach/dm365.h>
