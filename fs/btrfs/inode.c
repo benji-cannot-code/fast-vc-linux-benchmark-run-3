5aacbe0e5dcdc8c5e0782bc5977903842bbac693
Miao Xie <miaox@cn.fujitsu.com>
2010-11-22 03:01:39 +0000

    btrfs: fix free dip and dip->csums twice
    bio_endio() will free dip and dip->csums, so dip and dip->csums twice will
    be freed twice. Fix it.
    
    Signed-off-by: Miao Xie <miaox@cn.fujitsu.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -5732,7 +5732,7 @@ static void btrfs_submit_direct(int rw, struct bio *bio, struct inode *inode,
 
 	ret = btrfs_bio_wq_end_io(root->fs_info, bio, 0);
 	if (ret)
-		goto out_err;
+		goto free_ordered;
 
 	if (write && !skip_sum) {
 		ret = btrfs_wq_submit_bio(BTRFS_I(inode)->root->fs_info,
@@ -5741,7 +5741,7 @@ static void btrfs_submit_direct(int rw, struct bio *bio, struct inode *inode,
 				   __btrfs_submit_bio_start_direct_io,
 				   __btrfs_submit_bio_done);
 		if (ret)
-			goto out_err;
+			goto free_ordered;
 		return;
 	} else if (!skip_sum)
 		btrfs_lookup_bio_sums_dio(root, inode, bio,
@@ -5749,11 +5749,8 @@ static void btrfs_submit_direct(int rw, struct bio *bio, struct inode *inode,
 
 	ret = btrfs_map_bio(root, rw, bio, 0, 1);
 	if (ret)
-		goto out_err;
+		goto free_ordered;
 	return;
-out_err:
-	kfree(dip->csums);
-	kfree(dip);
 free_ordered:
 	/*
 	 * If this is a write, we need to clean up the reserved space and kill
