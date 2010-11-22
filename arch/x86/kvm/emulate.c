49d797652f4bb8302106e227e4e348409df83f9e
Avi Kivity <avi@redhat.com>
2010-11-22 17:53:23 +0200

    KVM: x86 emulator: drop dead pf injection in emulate_popf()
    If rc == X86EMUL_PROPAGATE_FAULT, we would have returned earlier.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -480,11 +480,6 @@ static void emulate_gp(struct x86_emulate_ctxt *ctxt, int err)
 	emulate_exception(ctxt, GP_VECTOR, err, true);
 }
 
-static void emulate_pf(struct x86_emulate_ctxt *ctxt)
-{
-	emulate_exception(ctxt, PF_VECTOR, 0, true);
-}
-
 static void emulate_ud(struct x86_emulate_ctxt *ctxt)
 {
 	emulate_exception(ctxt, UD_VECTOR, 0, false);
@@ -1184,9 +1179,6 @@ static int emulate_popf(struct x86_emulate_ctxt *ctxt,
 
 	*(unsigned long *)dest =
 		(ctxt->eflags & ~change_mask) | (val & change_mask);
-
-	if (rc == X86EMUL_PROPAGATE_FAULT)
-		emulate_pf(ctxt);
 
 	return rc;
 }
