a52bf3ee14dd7d3568dacae2050a9efabaaa0112
Jassi Brar <jassi.brar@samsung.com>
2010-11-22 15:36:44 +0900

    ASoC: Samsung: Rename PCM driver
    Rename Samsung PCM Controller driver
    	s3c-pcm.[c/h] -> pcm.[c/h]
    
    Signed-off-by: Jassi Brar <jassi.brar@samsung.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/s3c24xx/pcm.c
+++ sound/soc/s3c24xx/pcm.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* sound/soc/s3c24xx/s3c-pcm.c
+/* sound/soc/s3c24xx/pcm.c
  *
  * ALSA SoC Audio Layer - S3C PCM-Controller driver
  *
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/dma.h>
 
 #include "dma.h"
-#include "s3c-pcm.h"
+#include "pcm.h"
 
 static struct s3c2410_dma_client s3c_pcm_dma_client_out = {
 	.name		= "PCM Stereo out"
