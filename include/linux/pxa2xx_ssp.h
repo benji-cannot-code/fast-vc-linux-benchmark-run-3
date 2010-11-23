3200cf8ed02e13c14b4c1c095406e9ac90f4c8d3
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2010-11-22 17:12:17 -0800

    spi/pxa2xx: Add chipselect support for Sodaville
    The SPI core on Sodaville supports chip selects. Its configuration
    moved into the SSSR register at bit 0 and 1. Thus Sodaville can be hooked
    up with up to 4 devices.
    This patch ensures that the bits which are otherwiese reserved are only
    touched on Sodaville and not on any other PXAs. Also it makes sure that
    the status register does not lose the CS information while clearing the
    ROR bit.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Dirk Brandewie <dirk.brandewie@gmail.com>

--- include/linux/pxa2xx_ssp.h
+++ include/linux/pxa2xx_ssp.h
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SSCR1_SPH	(1 << 4)	/* Motorola SPI SSPSCLK phase setting */
 #define SSCR1_MWDS	(1 << 5)	/* Microwire Transmit Data Size */
 
+#define SSSR_ALT_FRM_MASK	3	/* Masks the SFRM signal number */
 #define SSSR_TNF	(1 << 2)	/* Transmit FIFO Not Full */
 #define SSSR_RNE	(1 << 3)	/* Receive FIFO Not Empty */
 #define SSSR_BSY	(1 << 4)	/* SSP Busy */
@@ -161,6 +162,7 @@ enum pxa_ssp_type {
 	PXA25x_NSSP, /* pxa 255, 26x (including ASSP) */
 	PXA27x_SSP,
 	PXA168_SSP,
+	CE4100_SSP,
 };
 
 struct ssp_device {
