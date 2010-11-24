7099d1d29cfdfac20bfaa2c9ef8b763bb4f612bc
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2010-11-24 02:37:58 +0000

    stmmac: tidy-up stmmac_priv structure
    This patch tidies-up the stmmac_priv structure
    that had many fileds alredy defined in the
    plat_stmmacenet_data structure.
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/stmmac/stmmac_ethtool.c
+++ drivers/net/stmmac/stmmac_ethtool.c
@@ -95,7 +95,7 @@ static void stmmac_ethtool_getdrvinfo(struct net_device *dev,
 {
 	struct stmmac_priv *priv = netdev_priv(dev);
 
-	if (!priv->is_gmac)
+	if (!priv->plat->has_gmac)
 		strcpy(info->driver, MAC100_ETHTOOL_NAME);
 	else
 		strcpy(info->driver, GMAC_ETHTOOL_NAME);
@@ -177,7 +177,7 @@ static void stmmac_ethtool_gregs(struct net_device *dev,
 
 	memset(reg_space, 0x0, REG_SPACE_SIZE);
 
-	if (!priv->is_gmac) {
+	if (!priv->plat->has_gmac) {
 		/* MAC registers */
 		for (i = 0; i < 12; i++)
 			reg_space[i] = readl(priv->ioaddr + (i * 4));
