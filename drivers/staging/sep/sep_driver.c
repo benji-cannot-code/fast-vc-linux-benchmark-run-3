948b610aaabd943f8a69e4b9a93ff1e537cffeb4
Alan Cox <alan@linux.intel.com>
2010-11-24 19:33:43 +0000

    Staging: sep: handle the rar definition stuff in the header
    SEP isn't the only driver that may need to handle both cases easily
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/sep/sep_driver.c
+++ drivers/staging/sep/sep_driver.c
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cacheflush.h>
 #include <linux/sched.h>
 #include <linux/delay.h>
+#include <linux/rar_register.h>
 
 #include <linux/netlink.h>
 #include <linux/connector.h>
