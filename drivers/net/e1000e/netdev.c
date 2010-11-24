25c5eefb37d9cc26c44da6125922f267ff519e4d
Bruce Allan <bruce.w.allan@intel.com>
2010-11-24 07:42:43 +0000

    e1000e: prevent null ptr dereference in e1000_tx_queue()
    tx_desc can be dereferenced as a null pointer when count is passed in
    as 0.
    
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Tested-by: Emil Tantilov <emil.s.tantilov@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/e1000e/netdev.c
+++ drivers/net/e1000e/netdev.c
@@ -4630,7 +4630,7 @@ static void e1000_tx_queue(struct e1000_adapter *adapter,
 
 	i = tx_ring->next_to_use;
 
-	while (count--) {
+	do {
 		buffer_info = &tx_ring->buffer_info[i];
 		tx_desc = E1000_TX_DESC(*tx_ring, i);
 		tx_desc->buffer_addr = cpu_to_le64(buffer_info->dma);
@@ -4641,7 +4641,7 @@ static void e1000_tx_queue(struct e1000_adapter *adapter,
 		i++;
 		if (i == tx_ring->count)
 			i = 0;
-	}
+	} while (--count > 0);
 
 	tx_desc->lower.data |= cpu_to_le32(adapter->txd_cmd);
 
