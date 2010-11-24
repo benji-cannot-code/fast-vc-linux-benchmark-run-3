9bea4fca5bd40bcf7af6c5d23945938967d40587
Paul Walmsley <paul@pwsan.com>
2010-11-24 16:49:05 -0700

    OMAP2+: PM/serial: hold console semaphore while OMAP UARTs are disabled
    The console semaphore must be held while the OMAP UART devices are
    disabled, lest a console write cause an ARM abort (and a kernel crash)
    when the underlying console device is inaccessible.  These crashes
    only occur when the console is on one of the OMAP internal serial
    ports.
    
    While this problem has been latent in the PM idle loop for some time,
    the crash was not triggerable with an unmodified kernel until commit
    6f251e9db1093c187addc309b5f2f7fe3efd2995 ("OMAP: UART: omap_device
    conversions, remove implicit 8520 assumptions").  After this patch, a
    console write often occurs after the console UART has been disabled in
    the idle loop, crashing the system.  Several users have encountered
    this bug:
    
        http://www.mail-archive.com/linux-omap@vger.kernel.org/msg38396.html
    
        http://www.mail-archive.com/linux-omap@vger.kernel.org/msg36602.html
    
    The same commit also introduced new code that disabled the UARTs
    during init, in omap_serial_init_port().  The kernel will also crash
    in this code when earlyconsole and extra debugging is enabled:
    
        http://www.mail-archive.com/linux-omap@vger.kernel.org/msg36411.html
    
    The minimal fix for the -rc series is to hold the console semaphore
    while the OMAP UARTs are disabled.  This is a somewhat overbroad fix,
    since the console may not be located on an OMAP UART, as is the case
    with the GPMC UART on Zoom3.  While it is technically possible to
    determine which devices the console or earlyconsole is actually
    running on, it is not a trivial problem to solve, and the code to do
    so is not really appropriate for the -rc series.
    
    The right long-term fix is to ensure that no code outside of the OMAP
    serial driver can disable an OMAP UART.  As I understand it, code to
    implement this is under development by TI.
    
    This patch is a collaboration between Paul Walmsley <paul@pwsan.com>
    and Tony Lindgren <tony@atomide.com>.  Thanks to Ming Lei
    <tom.leiming@gmail.com> and Pramod <pramod.gurav@ti.com> for their
    feedback on earlier versions of this patch.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Acked-by: Kevin Hilman <khilman@deeprootsystems.com>
    Cc: Ming Lei <tom.leiming@gmail.com>
    Cc: Pramod <pramod.gurav@ti.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Jean Pihet <jean.pihet@newoldbits.com>
    Cc: Govindraj.R <govindraj.raja@ti.com>

--- arch/arm/mach-omap2/serial.c
+++ arch/arm/mach-omap2/serial.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/serial_8250.h>
 #include <linux/pm_runtime.h>
+#include <linux/console.h>
 
 #ifdef CONFIG_SERIAL_OMAP
 #include <plat/omap-serial.h>
@@ -808,6 +809,8 @@ void __init omap_serial_init_port(int port)
 
 	oh->dev_attr = uart;
 
+	acquire_console_sem(); /* in case the earlycon is on the UART */
+
 	/*
 	 * Because of early UART probing, UART did not get idled
 	 * on init.  Now that omap_device is ready, ensure full idle
@@ -831,6 +834,8 @@ void __init omap_serial_init_port(int port)
 		uart->timeout = (30 * HZ);
 	omap_uart_block_sleep(uart);
 	uart->timeout = DEFAULT_TIMEOUT;
+
+	release_console_sem();
 
 	if ((cpu_is_omap34xx() && uart->padconf) ||
 	    (uart->wk_en && uart->wk_mask)) {
