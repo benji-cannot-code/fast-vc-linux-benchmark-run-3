e44592796e99b8fd0c3a64284e51c3b9f1740457
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2010-11-24 02:38:05 +0000

    stmmac: add init/exit callback in plat_stmmacenet_data struct
    This patch adds in the plat_stmmacenet_data
    the init and exit callbacks that can be used
    for invoking specific platform functions.
    For example, on ST targets, these call the
    PAD manager functions to set PIO lines and
    syscfg registers.
    The patch removes the stmmac_claim_resource
    only used on STM Kernels as well.
    
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/stmmac/stmmac_main.c
+++ drivers/net/stmmac/stmmac_main.c
@@ -1644,7 +1644,7 @@ static int stmmac_dvr_probe(struct platform_device *pdev)
 	struct resource *res;
 	void __iomem *addr = NULL;
 	struct net_device *ndev = NULL;
-	struct stmmac_priv *priv;
+	struct stmmac_priv *priv = NULL;
 	struct plat_stmmacenet_data *plat_dat;
 
 	pr_info("STMMAC driver:\n\tplatform registration... ");
@@ -1709,10 +1709,12 @@ static int stmmac_dvr_probe(struct platform_device *pdev)
 	/* Set the I/O base addr */
 	ndev->base_addr = (unsigned long)addr;
 
-	/* Verify embedded resource for the platform */
-	ret = stmmac_claim_resource(pdev);
-	if (ret < 0)
-		goto out;
+	/* Custom initialisation */
+	if (priv->plat->init) {
+		ret = priv->plat->init(pdev);
+		if (unlikely(ret))
+			goto out;
+	}
 
 	/* MAC HW revice detection */
 	ret = stmmac_mac_device_setup(ndev);
@@ -1746,6 +1748,9 @@ static int stmmac_dvr_probe(struct platform_device *pdev)
 
 out:
 	if (ret < 0) {
+		if (priv->plat->exit)
+			priv->plat->exit(pdev);
+
 		platform_set_drvdata(pdev, NULL);
 		release_mem_region(res->start, resource_size(res));
 		if (addr != NULL)
@@ -1778,6 +1783,9 @@ static int stmmac_dvr_remove(struct platform_device *pdev)
 	netif_carrier_off(ndev);
 
 	stmmac_mdio_unregister(ndev);
+
+	if (priv->plat->exit)
+		priv->plat->exit(pdev);
 
 	platform_set_drvdata(pdev, NULL);
 	unregister_netdev(ndev);
