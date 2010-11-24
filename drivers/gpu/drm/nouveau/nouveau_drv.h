a202982901be85258b9ab7f43788e30ac83d7be4
Ben Skeggs <bskeggs@redhat.com>
2010-11-24 10:47:15 +1000

    drm/nvc0: implement pfifo engine hooks
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_drv.h
+++ drivers/gpu/drm/nouveau/nouveau_drv.h
@@ -241,6 +241,7 @@ struct nouveau_channel {
 	/* PFIFO context */
 	struct nouveau_gpuobj *ramfc;
 	struct nouveau_gpuobj *cache;
+	void *fifo_priv;
 
 	/* PGRAPH context */
 	/* XXX may be merge 2 pointers as private data ??? */
@@ -338,6 +339,7 @@ struct nouveau_fb_engine {
 };
 
 struct nouveau_fifo_engine {
+	void *priv;
 	int  channels;
 
 	struct nouveau_gpuobj *playlist[2];
