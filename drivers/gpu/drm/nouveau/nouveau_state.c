214ee9424a5160d1585982c1fb9a27d1fc6c7587
Ben Skeggs <bskeggs@redhat.com>
2010-11-24 10:18:28 +1000

    drm/nvc0: skip dma object creation for drm channel
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_state.c
+++ drivers/gpu/drm/nouveau/nouveau_state.c
@@ -553,6 +553,10 @@ nouveau_card_init_channel(struct drm_device *dev)
 	if (ret)
 		return ret;
 
+	/* no dma objects on fermi... */
+	if (dev_priv->card_type >= NV_C0)
+		goto out_done;
+
 	ret = nouveau_gpuobj_dma_new(dev_priv->channel, NV_CLASS_DMA_IN_MEMORY,
 				     0, dev_priv->vram_size,
 				     NV_MEM_ACCESS_RW, NV_MEM_TARGET_VRAM,
@@ -577,6 +581,7 @@ nouveau_card_init_channel(struct drm_device *dev)
 	if (ret)
 		goto out_err;
 
+out_done:
 	mutex_unlock(&dev_priv->channel->mutex);
 	return 0;
 
