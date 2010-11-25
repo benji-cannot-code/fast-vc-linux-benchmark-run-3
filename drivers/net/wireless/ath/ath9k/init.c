8465fc15b1e5b7a7727e750af67b918b7c87519f
Gabor Juhos <juhosg@openwrt.org>
2010-11-25 18:26:07 +0100

    ath9k: use per-device struct for pm_qos_* operations
    The ath9k driver uses a shared pm_qos_request_list structure for all
    devices. This causes the following warning if more than one device is
    present in the system:
    
    WARNING: at kernel/pm_qos_params.c:234 ath9k_init_device+0x5e8/0x6b0()
    pm_qos_add_request() called for already added request
    Modules linked in:
    Call Trace:
    [<802b1cdc>] dump_stack+0x8/0x34
    [<8007dd90>] warn_slowpath_common+0x78/0xa4
    [<8007de44>] warn_slowpath_fmt+0x2c/0x38
    [<801b0828>] ath9k_init_device+0x5e8/0x6b0
    [<801bc508>] ath_pci_probe+0x2dc/0x39c
    [<80176254>] pci_device_probe+0x64/0xa4
    [<8019471c>] driver_probe_device+0xbc/0x188
    [<80194854>] __driver_attach+0x6c/0xa4
    [<80193e20>] bus_for_each_dev+0x60/0xb0
    [<80193580>] bus_add_driver+0xcc/0x268
    [<80194c08>] driver_register+0xe0/0x198
    [<801764e0>] __pci_register_driver+0x50/0xe0
    [<80365f48>] ath9k_init+0x3c/0x6c
    [<8006050c>] do_one_initcall+0xfc/0x1d8
    [<80355340>] kernel_init+0xd4/0x174
    [<800639a4>] kernel_thread_helper+0x10/0x18
    
    ---[ end trace 5345fc6f870564a6 ]---
    
    This patch fixes that warning by using a separate pm_qos_request_list
    sructure for each device.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/init.c
+++ drivers/net/wireless/ath/ath9k/init.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/slab.h>
-#include <linux/pm_qos_params.h>
 
 #include "ath9k.h"
 
@@ -180,8 +179,6 @@ static const struct ath_ops ath9k_common_ops = {
 	.read = ath9k_ioread32,
 	.write = ath9k_iowrite32,
 };
-
-struct pm_qos_request_list ath9k_pm_qos_req;
 
 /**************************/
 /*     Initialization     */
@@ -760,7 +757,7 @@ int ath9k_init_device(u16 devid, struct ath_softc *sc, u16 subsysid,
 	ath_init_leds(sc);
 	ath_start_rfkill_poll(sc);
 
-	pm_qos_add_request(&ath9k_pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+	pm_qos_add_request(&sc->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
 			   PM_QOS_DEFAULT_VALUE);
 
 	return 0;
@@ -831,7 +828,7 @@ void ath9k_deinit_device(struct ath_softc *sc)
 	}
 
 	ieee80211_unregister_hw(hw);
-	pm_qos_remove_request(&ath9k_pm_qos_req);
+	pm_qos_remove_request(&sc->pm_qos_req);
 	ath_rx_cleanup(sc);
 	ath_tx_cleanup(sc);
 	ath9k_deinit_softc(sc);
--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/nl80211.h>
-#include <linux/pm_qos_params.h>
 #include "ath9k.h"
 #include "btcoex.h"
 
@@ -1245,7 +1244,7 @@ static int ath9k_start(struct ieee80211_hw *hw)
 			ath9k_btcoex_timer_resume(sc);
 	}
 
-	pm_qos_update_request(&ath9k_pm_qos_req, 55);
+	pm_qos_update_request(&sc->pm_qos_req, 55);
 
 mutex_unlock:
 	mutex_unlock(&sc->mutex);
@@ -1424,7 +1423,7 @@ static void ath9k_stop(struct ieee80211_hw *hw)
 
 	sc->sc_flags |= SC_OP_INVALID;
 
-	pm_qos_update_request(&ath9k_pm_qos_req, PM_QOS_DEFAULT_VALUE);
+	pm_qos_update_request(&sc->pm_qos_req, PM_QOS_DEFAULT_VALUE);
 
 	mutex_unlock(&sc->mutex);
 
