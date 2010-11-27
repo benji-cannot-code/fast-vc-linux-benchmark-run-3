037fb58beb685fb512f0976b604beb4450f61d3b
Szymon Janc <szymon@janc.net.pl>
2010-11-27 08:39:45 +0000

    forcedeth: include <linux/io.h> and <linux/uaccess.h> instead of <asm/io.h> and <asm/uaccess.h> as suggested by checkpatch
    Signed-off-by: Szymon Janc <szymon@janc.net.pl>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/forcedeth.c
+++ drivers/net/forcedeth.c
@@ -61,10 +61,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_vlan.h>
 #include <linux/dma-mapping.h>
 #include <linux/slab.h>
+#include <linux/uaccess.h>
+#include  <linux/io.h>
 
 #include <asm/irq.h>
-#include <asm/io.h>
-#include <asm/uaccess.h>
 #include <asm/system.h>
 
 #if 0
