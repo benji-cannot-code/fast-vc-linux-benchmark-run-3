65bea8da947dd0fa0fba5d01666247f9a9c60d55
Pekka Enberg <penberg@kernel.org>
2010-11-28 23:00:07 +0200

    Staging: w35und: Remove unused fields from struct wb_usb
    This patch removes two unused fields from struct wb_usb:
    
      - DetectCount which is always zero
    
      - IsUsb20 which is a write-only struct member
    
    Cc: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/winbond/wbusb_s.h
+++ drivers/staging/winbond/wbusb_s.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 struct wb_usb {
-	u32	IsUsb20;
 	struct	usb_device *udev;
-	u32	DetectCount;
 };
 #endif
