db28afae420176b2b555280199374fbc3f169a27
Mimi Zohar <zohar@linux.vnet.ibm.com>
2010-11-29 16:20:04 -0500

    keys: add missing include file for trusted and encrypted keys
    This patch fixes the linux-next powerpc build errors as reported by
    Stephen Rothwell.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Mimi Zohar <zohar@us.ibm.com>
    Tested-by: Rajiv Andrade <srajiv@linux.vnet.ibm.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/keys/trusted_defined.c
+++ security/keys/trusted_defined.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/parser.h>
 #include <linux/string.h>
+#include <linux/err.h>
 #include <keys/user-type.h>
 #include <keys/trusted-type.h>
 #include <linux/key-type.h>
