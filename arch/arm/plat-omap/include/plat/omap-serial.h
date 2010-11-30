c0d842577a6a62ac3b12d5556773a25af6f6d8fd
Andrei Emeltchenko <andrei.emeltchenko@nokia.com>
2010-11-30 14:11:49 -0800

    omap: Serial: Define register access modes in LCR
    Access to some registers depends on register access mode
    Three different modes are available for OMAP (at least)
    • Operational mode     LCR_REG[7] = 0x0
    • Configuration mode A LCR_REG[7] = 0x1 and LCR_REG[7:0]! = 0xBF
    • Configuration mode B LCR_REG[7] = 0x1 and LCR_REG[7:0]  = 0xBF
    
    Define access modes and remove redefinitions and magic numbers
    in serial drivers (and later in bluetooth driver).
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@nokia.com>
    Acked-by: Govindraj.R <govindraj.raja@ti.com>
    Acked-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/omap-serial.h
+++ arch/arm/plat-omap/include/plat/omap-serial.h
@@ -34,15 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define OMAP_MODE13X_SPEED	230400
 
-/*
- * LCR = 0XBF: Switch to Configuration Mode B.
- * In configuration mode b allow access
- * to EFR,DLL,DLH.
- * Reference OMAP TRM Chapter 17
- * Section: 1.4.3 Mode Selection
- */
-#define OMAP_UART_LCR_CONF_MDB	0XBF
-
 /* WER = 0x7F
  * Enable module level wakeup in WER reg
  */
--- include/linux/serial_reg.h
+++ include/linux/serial_reg.h
@@ -100,6 +100,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define UART_LCR_WLEN7		0x02 /* Wordlength: 7 bits */
 #define UART_LCR_WLEN8		0x03 /* Wordlength: 8 bits */
 
+/*
+ * Access to some registers depends on register access / configuration
+ * mode.
+ */
+#define UART_LCR_CONF_MODE_A	UART_LCR_DLAB	/* Configutation mode A */
+#define UART_LCR_CONF_MODE_B	0xBF		/* Configutation mode B */
+
 #define UART_MCR	4	/* Out: Modem Control Register */
 #define UART_MCR_CLKSEL		0x80 /* Divide clock by 4 (TI16C752, EFR[4]=1) */
 #define UART_MCR_TCRTLR		0x40 /* Access TCR/TLR (TI16C752, EFR[4]=1) */
