8b170a7e76be41b254a1e1744d9e7548cf7873c8
Allan Stephens <Allan.Stephens@windriver.com>
2010-11-30 12:00:57 +0000

    tipc: Delete useless function prototypes
    Removes several function declarations that aren't used anywhere,
    either because they reference routines that no longer exist or
    because all users of the function reference it after it has already
    been defined.
    
    Signed-off-by: Allan Stephens <Allan.Stephens@windriver.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/node.c
+++ net/tipc/node.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "port.h"
 #include "name_distr.h"
 
-void node_print(struct print_buf *buf, struct tipc_node *n_ptr, char *str);
 static void node_lost_contact(struct tipc_node *n_ptr);
 static void node_established_contact(struct tipc_node *n_ptr);
 
