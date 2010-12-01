f0d4bb283b41111dbd5c460ef41fb7854fdbe636
Felipe Balbi <balbi@ti.com>
2010-12-01 14:23:48 +0200

    usb: musb: add Kconfig options for each glue layer
    This will make things simpler when choosing which
    glue layer to compile. It avoids a lot of magic
    around the "default" Kconfig option and lets the
    user choose what exactly s/he wants to compile.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- arch/arm/mach-omap2/usb-musb.c
+++ arch/arm/mach-omap2/usb-musb.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/am35xx.h>
 #include <plat/usb.h>
 
-#ifdef CONFIG_USB_MUSB_SOC
+#if defined(CONFIG_USB_MUSB_OMAP2PLUS) || defined (CONFIG_USB_MUSB_AM35X)
 
 static struct resource musb_resources[] = {
 	[0] = { /* start and end set dynamically */
