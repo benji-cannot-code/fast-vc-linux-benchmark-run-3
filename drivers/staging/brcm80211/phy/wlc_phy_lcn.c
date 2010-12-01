8282db5afd18233dd4676c0991a9e15a26be4c83
Randy Dunlap <randy.dunlap@oracle.com>
2010-12-01 11:16:35 -0800

    staging: brcm80211 needs lots of delay.h
    Fix lots of errors like:
    
    drivers/staging/brcm80211/phy/wlc_phy_n.c:17613: error: implicit declaration of function 'mdelay'
    drivers/staging/brcm80211/util/nicpci.c:246: error: implicit declaration of function 'udelay'
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Henry Ptasinski <henryp@broadcom.com>
    Cc: Dowan Kim <dowan@broadcom.com>
    Cc: Roland Vossen <rvossen@broadcom.com>
    Cc: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/phy/wlc_phy_lcn.c
+++ drivers/staging/brcm80211/phy/wlc_phy_lcn.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/string.h>
 #include <linux/bitops.h>
+#include <linux/delay.h>
 #include <wlc_cfg.h>
 #include <qmath.h>
 #include <osl.h>
--- drivers/staging/brcm80211/phy/wlc_phy_n.c
+++ drivers/staging/brcm80211/phy/wlc_phy_n.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <bcmdefs.h>
 #include <wlc_cfg.h>
+#include <linux/delay.h>
 #include <linux/pci.h>
 #include <osl.h>
 #include <siutils.h>
