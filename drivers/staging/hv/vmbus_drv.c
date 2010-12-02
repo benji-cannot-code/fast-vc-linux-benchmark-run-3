33fae0dca74de44fb370bd9f731abc5267379d97
Greg Kroah-Hartman <gregkh@suse.de>
2010-12-02 12:11:25 -0800

    Staging: hv: make some vmbus_drv functions static
    Now that vmbus_drv.c is merged with vmbus.c, some of the newly global
    functions can now be marked static.
    
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/vmbus_drv.c
+++ drivers/staging/hv/vmbus_drv.c
@@ -207,7 +207,7 @@ static void VmbusOnCleanup(struct hv_driver *drv)
 /*
  * vmbus_on_msg_dpc - DPC routine to handle messages from the hypervisior
  */
-void vmbus_on_msg_dpc(struct hv_driver *drv)
+static void vmbus_on_msg_dpc(struct hv_driver *drv)
 {
 	int cpu = smp_processor_id();
 	void *page_addr = hv_context.synic_message_page[cpu];
@@ -254,7 +254,7 @@ void vmbus_on_msg_dpc(struct hv_driver *drv)
 /*
  * vmbus_on_event_dpc - DPC routine to handle events from the hypervisior
  */
-void vmbus_on_event_dpc(struct hv_driver *drv)
+static void vmbus_on_event_dpc(struct hv_driver *drv)
 {
 	/* TODO: Process any events */
 	VmbusOnEvents();
@@ -263,7 +263,7 @@ void vmbus_on_event_dpc(struct hv_driver *drv)
 /*
  * vmbus_on_isr - ISR routine
  */
-int vmbus_on_isr(struct hv_driver *drv)
+static int vmbus_on_isr(struct hv_driver *drv)
 {
 	int ret = 0;
 	int cpu = smp_processor_id();
