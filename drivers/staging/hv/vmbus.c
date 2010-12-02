947db05f467dba6bb9744720bfa504aab309ceba
Greg Kroah-Hartman <gregkh@suse.de>
2010-12-02 11:28:28 -0800

    Staging: hv: rename the vmbus to "hyperv"
    This changes the name of the bus in sysfs from "vmbus" to "hyperv" which
    is the name it should show (vmbus is way to generic).
    
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/vmbus.c
+++ drivers/staging/hv/vmbus.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "version_info.h"
 #include "vmbus_private.h"
 
-static const char *gDriverName = "vmbus";
+static const char *gDriverName = "hyperv";
 
 /*
  * Windows vmbus does not defined this.
