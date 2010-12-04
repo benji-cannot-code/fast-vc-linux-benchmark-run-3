75f62c9717ec7ed99f7d4383d05b5e44a4a245b6
Russell King <rmk+kernel@arm.linux.org.uk>
2010-12-04 16:13:29 +0000

    ARM: GIC: consolidate gic_cpu_base_addr to common GIC code
    Every architecture using the GIC has a gic_cpu_base_addr pointer for
    GIC 0 for their entry assembly code to use to decode the cause of the
    current interrupt.  Move this into the common GIC code.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Tested-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/hardware/gic.h
+++ arch/arm/include/asm/hardware/gic.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GIC_DIST_SOFTINT		0xf00
 
 #ifndef __ASSEMBLY__
+extern void __iomem *gic_cpu_base_addr;
+
 void gic_init(unsigned int, unsigned int, void __iomem *, void __iomem *);
 void gic_secondary_init(unsigned int);
 void gic_cascade_irq(unsigned int gic_nr, unsigned int irq);
