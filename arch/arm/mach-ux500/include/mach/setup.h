758bb44d3d2752ae7d6b8d09cd30ab98b65f50e7
Per Forlin <per.forlin@linaro.org>
2010-12-05 12:49:03 +0100

    ux500: Add eMMC support in U5500.
    U5500 now boots from sdi0 (onboard eMMC).
    Change machine type to U5500.
    Adjust uart and sdi0 clock rates for u5500.
    All necessary clocks must be enabled before Linux starts because
    there is no clock tree support in u5500 yet.
    
    Signed-off-by: Per Forlin <per.forlin@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>

--- arch/arm/mach-ux500/include/mach/setup.h
+++ arch/arm/mach-ux500/include/mach/setup.h
@@ -23,6 +23,9 @@ extern void __init u5500_init_devices(void);
 extern void __init u8500_init_devices(void);
 
 extern void __init ux500_init_irq(void);
+
+extern void __init u5500_sdi_init(void);
+
 /* We re-use nomadik_timer for this platform */
 extern void nmdk_timer_init(void);
 
