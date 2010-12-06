ff3e75c578da7cf9758d8ddfc6145f0a4e9ae1d7
Hank Janssen <hjanssen@microsoft.com>
2010-12-06 12:26:46 -0800

    staging: hv: Convert camel case functions in storvsc_api.h to lowercase
    Convert camel case functions in storvsc_api.h to lowercase
    
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/blkvsc_drv.c
+++ drivers/staging/hv/blkvsc_drv.c
@@ -1488,7 +1488,7 @@ static int __init blkvsc_init(void)
 
 	DPRINT_INFO(BLKVSC_DRV, "Blkvsc initializing....");
 
-	ret = blkvsc_drv_init(BlkVscInitialize);
+	ret = blkvsc_drv_init(blk_vsc_initialize);
 
 	return ret;
 }
