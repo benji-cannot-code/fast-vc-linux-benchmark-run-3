73f96b0081db8d6d59c659d046e21c6c895057bb
Roland Vossen <rvossen@broadcom.com>
2010-12-07 17:45:42 +0100

    staging: brcm80211: replaced typedef wl_if_t by struct wl_if
    Code cleanup.
    
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/sys/wlc_types.h
+++ drivers/staging/brcm80211/sys/wlc_types.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct wlc_info;
 struct wlc_hw_info;
 struct wlc_if;
-typedef struct wl_if wl_if_t;
+struct wl_if;
 struct ampdu_info;
 struct wlc_ap_info;
 struct antsel_info;
