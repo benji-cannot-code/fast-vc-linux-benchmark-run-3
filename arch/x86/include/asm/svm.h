246777d050ea7da599dae4ca13708126c2c3004d
Joerg Roedel <joerg.roedel@amd.com>
2010-12-07 17:15:06 +0100

    KVM: SVM: Add xsetbv intercept
    This patch implements the xsetbv intercept to the AMD part
    of KVM. This makes AVX usable in a save way for the guest on
    AVX capable AMD hardware.
    
    The patch is tested by using AVX in the guest and host in
    parallel and checking for data corruption. I also used the
    KVM xsave unit-tests and they all pass.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/include/asm/svm.h
+++ arch/x86/include/asm/svm.h
@@ -48,6 +48,7 @@ enum {
 	INTERCEPT_MONITOR,
 	INTERCEPT_MWAIT,
 	INTERCEPT_MWAIT_COND,
+	INTERCEPT_XSETBV,
 };
 
 
@@ -330,6 +331,7 @@ struct __attribute__ ((__packed__)) vmcb {
 #define SVM_EXIT_MONITOR	0x08a
 #define SVM_EXIT_MWAIT		0x08b
 #define SVM_EXIT_MWAIT_COND	0x08c
+#define SVM_EXIT_XSETBV		0x08d
 #define SVM_EXIT_NPF  		0x400
 
 #define SVM_EXIT_ERR		-1
