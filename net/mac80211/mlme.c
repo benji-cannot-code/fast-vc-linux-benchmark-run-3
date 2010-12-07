54bc07db8a8236c543a46f30b50fe5ab722d6c6f
Felix Fietkau <nbd@openwrt.org>
2010-12-07 04:40:18 +0100

    mac80211: fix a compiler warning
    net/mac80211/mlme.c: In function 'ieee80211_sta_work':
    net/mac80211/mlme.c:1981: warning: too many arguments for format
    
    Introduced by commit 04ac3c0ee2c773c321ec472d892635a20556f34d
    ("mac80211: speed up AP probing using nullfunc frames").
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -1983,8 +1983,7 @@ void ieee80211_sta_work(struct ieee80211_sub_if_data *sdata)
 				wiphy_debug(local->hw.wiphy,
 					    "%s: No ack for nullfunc frame to"
 					    " AP %pM, disconnecting.\n",
-					    sdata->name, bssid,
-					    ifmgd->probe_send_count);
+					    sdata->name, bssid);
 #endif
 				ieee80211_sta_connection_lost(sdata, bssid);
 			}
