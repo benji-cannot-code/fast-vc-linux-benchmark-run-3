ed44a520578b5590f355f584bf573b5b41e28977
Pavankumar Kondeti <pkondeti@codeaurora.org>
2010-12-07 17:54:03 +0530

    USB: gadget: Add USB controller driver for MSM SoC
    MSM SoC has chipidea USB controller. So use ci13xxx_udc core.
    This driver depends on transceiver driver for clock control,
    PHY initialization, VBUS detection.  Register for notify_event
    callback to perform MSM specific quirks after controller is reset
    and stopped.
    
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -149,6 +149,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	gadget_is_pch(g)	0
 #endif
 
+#ifdef CONFIG_USB_GADGET_CI13XXX_MSM
+#define gadget_is_ci13xxx_msm(g)	(!strcmp("ci13xxx_msm", (g)->name))
+#else
+#define gadget_is_ci13xxx_msm(g)	0
+#endif
+
 /**
  * usb_gadget_controller_number - support bcdDevice id convention
  * @gadget: the controller being driven
@@ -208,6 +214,8 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x26;
 	else if (gadget_is_pch(gadget))
 		return 0x27;
+	else if (gadget_is_ci13xxx_msm(gadget))
+		return 0x28;
 	return -ENOENT;
 }
 
