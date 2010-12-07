8918b2eef9e14285328043af9fa207b9192fc56b
Wu Fengguang <fengguang.wu@intel.com>
2010-12-06 22:34:29 -0600

    writeback: trace global_dirty_state
    Add trace event balance_dirty_state for showing the global dirty page
    counts and thresholds at each global_dirty_limits() invocation.  This
    will cover the callers throttle_vm_writeout(), over_bground_thresh()
    and each balance_dirty_pages() loop.
    
    Signed-off-by: Wu Fengguang <fengguang.wu@intel.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -448,6 +448,7 @@ void global_dirty_limits(unsigned long *pbackground, unsigned long *pdirty)
 	}
 	*pbackground = background;
 	*pdirty = dirty;
+	trace_global_dirty_state(background, dirty);
 }
 
 /**
