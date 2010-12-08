25fe6e1029324b13d62e6b4a41d9147f1c010eb3
Sahitya Tummala <stummala@codeaurora.org>
2010-12-08 15:03:03 +0530

    mmc: msm_sdcc: Fix possible circular locking dependency warning
    In the context of request processing thread, data mover lock is
    acquired after the host lock.  In another context, in the completion
    handler of data mover the locks are acquired in the reverse order,
    resulting in possible circular lock dependency warning. Hence,
    schedule a tasklet to process the dma completion so as to avoid
    nested locks.
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.h
+++ drivers/mmc/host/msm_sdcc.h
@@ -173,6 +173,8 @@ struct msmsdcc_dma_data {
 	struct msmsdcc_host		*host;
 	int				busy; /* Set if DM is busy */
 	int				active;
+	unsigned int			result;
+	struct msm_dmov_errdata		err;
 };
 
 struct msmsdcc_pio_data {
@@ -236,6 +238,7 @@ struct msmsdcc_host {
 	int			cmdpoll;
 	struct msmsdcc_stats	stats;
 
+	struct tasklet_struct	dma_tlet;
 	/* Command parameters */
 	unsigned int		cmd_timeout;
 	unsigned int		cmd_pio_irqmask;
