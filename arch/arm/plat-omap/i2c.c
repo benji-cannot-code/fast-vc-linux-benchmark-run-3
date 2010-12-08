97f1431f791baf9d6f8cd1be9c6fe0b860f9ef54
Tony Lindgren <tony@atomide.com>
2010-12-07 16:25:40 -0800

    omap: Fix undefined reference to omap2_i2c_mux_pins
    In some cases we can get error function `omap2_i2c_add_bus':
    arch/arm/plat-omap/i2c.c:136: undefined reference to `omap2_i2c_mux_pins'
    arch/arm/plat-omap/i2c.c:141: undefined reference to `omap_hwmod_lookup'
    arch/arm/plat-omap/i2c.c:157: undefined reference to `omap_device_build'
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/i2c.c
+++ arch/arm/plat-omap/i2c.c
@@ -131,6 +131,7 @@ static struct omap_device_pm_latency omap_i2c_latency[] = {
 	},
 };
 
+#ifdef CONFIG_ARCH_OMAP2PLUS
 static inline int omap2_i2c_add_bus(int bus_id)
 {
 	int l;
@@ -167,6 +168,12 @@ static inline int omap2_i2c_add_bus(int bus_id)
 
 	return PTR_ERR(od);
 }
+#else
+static inline int omap2_i2c_add_bus(int bus_id)
+{
+	return 0;
+}
+#endif
 
 static int __init omap_i2c_add_bus(int bus_id)
 {
