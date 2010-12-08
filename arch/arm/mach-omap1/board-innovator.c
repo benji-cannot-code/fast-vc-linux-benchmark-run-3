d9f196b2d0964326389c9aef351e2e6f718bc3da
Tony Lindgren <tony@atomide.com>
2010-12-07 16:26:55 -0800

    omap: Fix gpio_request calls to happen as arch_initcall
    Looks like some boards are calling gpio_request from init_irq.
    This will make the request_irq fail, as GPIO will be initialized
    as postcore_initcall.
    
    Reported-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/board-innovator.c
+++ arch/arm/mach-omap1/board-innovator.c
@@ -297,7 +297,6 @@ static void __init innovator_init_irq(void)
 		omap1510_fpga_init_irq();
 	}
 #endif
-	innovator_init_smc91x();
 }
 
 #ifdef CONFIG_ARCH_OMAP15XX
@@ -388,6 +387,8 @@ static struct omap_board_config_kernel innovator_config[] = {
 
 static void __init innovator_init(void)
 {
+	innovator_init_smc91x();
+
 #ifdef CONFIG_ARCH_OMAP15XX
 	if (cpu_is_omap1510()) {
 		unsigned char reg;
