dce2ace1dd337956f98606160985f9dda72839ea
Roopa Prabhu <roprabhu@cisco.com>
2010-12-08 13:53:58 +0000

    enic: Add ndo_set_vf_mac support for enic dynamic devices
    This patch implements the ndo_set_vf_mac netdev operation for enic
    dynamic devices. It treats the mac address set by IFLA_VF_MAC as a
    special case to use it in the port profile provisioning data.
    
    Signed-off-by: Roopa Prabhu <roprabhu@cisco.com>
    Signed-off-by: David Wang <dwang2@cisco.com>
    Signed-off-by: Christian Benvenuti <benve@cisco.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/enic/enic.h
+++ drivers/net/enic/enic.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME		"enic"
 #define DRV_DESCRIPTION		"Cisco VIC Ethernet NIC Driver"
-#define DRV_VERSION		"1.4.1.8"
+#define DRV_VERSION		"1.4.1.9"
 #define DRV_COPYRIGHT		"Copyright 2008-2010 Cisco Systems, Inc"
 
 #define ENIC_BARS_MAX		6
@@ -62,6 +62,7 @@ struct enic_port_profile {
 	char name[PORT_PROFILE_MAX];
 	u8 instance_uuid[PORT_UUID_MAX];
 	u8 host_uuid[PORT_UUID_MAX];
+	u8 vf_mac[ETH_ALEN];
 };
 
 /* Per-instance private data structure */
