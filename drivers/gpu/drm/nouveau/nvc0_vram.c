de5732a8b769aa69943f316c85acd85f1ee2c119
Ben Skeggs <bskeggs@redhat.com>
2010-12-08 11:19:30 +1000

    drm/nvc0: initial support for tiled buffer objects
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvc0_vram.c
+++ drivers/gpu/drm/nouveau/nvc0_vram.c
@@ -30,8 +30,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 bool
 nvc0_vram_flags_valid(struct drm_device *dev, u32 tile_flags)
 {
-	if (likely(!(tile_flags & NOUVEAU_GEM_TILE_LAYOUT_MASK)))
+	switch (tile_flags & NOUVEAU_GEM_TILE_LAYOUT_MASK) {
+	case 0x0000:
+	case 0xfe00:
+	case 0xdb00:
+	case 0x1100:
 		return true;
+	default:
+		break;
+	}
+
 	return false;
 }
 
