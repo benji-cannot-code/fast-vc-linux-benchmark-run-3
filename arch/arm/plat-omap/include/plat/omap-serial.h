2ccacf6ec25bc20a35265615a6c909550e8114ce
Benoit Cousson <b-cousson@ti.com>
2010-12-09 14:24:17 +0000

    omap: serial: Change device name: omap-hsuart -> omap_uart
    The naming convention for omap_device is omap_XXX.
    
    Rename the device and driver name in order to stick
    to this naming convention.
    Remove the hs prefix that is implicit for every OMAP uarts.
    
    Signed-off-by: Benoit Cousson <b-cousson@ti.com>
    Acked-by: Govindraj Raja <govindraj.raja@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>
    Cc: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/omap-serial.h
+++ arch/arm/plat-omap/include/plat/omap-serial.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat/mux.h>
 
-#define DRIVER_NAME	"omap-hsuart"
+#define DRIVER_NAME	"omap_uart"
 
 /*
  * Use tty device name as ttyO, [O -> OMAP]
