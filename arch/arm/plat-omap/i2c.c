8547030994bf017ed54ec116838efd5e1398263d
Benoit Cousson <b-cousson@ti.com>
2010-12-09 14:24:16 +0000

    I2C: i2c-omap: Change device name: i2c_omap -> omap_i2c
    The convention for omap device naming is omap_XXX.
    
    Rename the device and driver name in order to stick
    to this naming convention.
    
    Change device name in clock nodes as well.
    
    Signed-off-by: Benoit Cousson <b-cousson@ti.com>
    Cc: Kevin Hilman <khilman@deeprootsystems.com>
    Cc: Rajendra Nayak <rnayak@ti.com>
    Cc: Ben Dooks <ben-i2c@fluff.org>
    Acked-by: Paul Walmsley <paul@pwsan.com>
    Acked-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/i2c.c
+++ arch/arm/plat-omap/i2c.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP_I2C_SIZE		0x3f
 #define OMAP1_I2C_BASE		0xfffb3800
 
-static const char name[] = "i2c_omap";
+static const char name[] = "omap_i2c";
 
 #define I2C_RESOURCE_BUILDER(base, irq)			\
 	{						\
