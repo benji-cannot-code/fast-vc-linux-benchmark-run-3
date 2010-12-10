5a04f1620f90bb86894eb3fd5a4048a4e7b5ac7d
Jon Mason <jon.mason@exar.com>
2010-12-10 15:40:02 +0000

    s2io: make strings at tables const
    Put immutable data in read/only section.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: Jon Mason <jon.mason@exar.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/s2io.c
+++ drivers/net/s2io.c
@@ -92,11 +92,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRV_VERSION "2.0.26.27"
 
 /* S2io Driver name & version. */
-static char s2io_driver_name[] = "Neterion";
-static char s2io_driver_version[] = DRV_VERSION;
+static const char s2io_driver_name[] = "Neterion";
+static const char s2io_driver_version[] = DRV_VERSION;
 
-static int rxd_size[2] = {32, 48};
-static int rxd_count[2] = {127, 85};
+static const int rxd_size[2] = {32, 48};
+static const int rxd_count[2] = {127, 85};
 
 static inline int RXD_IS_UP2DT(struct RxD_t *rxdp)
 {
