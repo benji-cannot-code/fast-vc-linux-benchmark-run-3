6760fd88d4d39977058a649608e88abd081abd76
Chris Mason <chris.mason@oracle.com>
2010-12-13 14:56:23 -0500

    Btrfs: account for missing devices in RAID allocation profiles
    When we mount in RAID degraded mode without adding a new device to
    replace the failed one, we can end up using the wrong RAID flags for
    allocations.
    
    This results in strange combinations of block groups (raid1 in a raid10
    filesystem) and corruptions when we try to allocate blocks from single
    spindle chunks on drives that are actually missing.
    
    The first device has two small 4MB chunks in it that mkfs creates and
    these are usually unused in a raid1 or raid10 setup.  But, in -o degraded,
    the allocator will fall back to these because the mask of desired raid groups
    isn't correct.
    
    The fix here is to count the missing devices as we build up the list
    of devices in the system.  This count is used when picking the
    raid level to make sure we continue using the same levels that were
    in place before we lost a drive.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/volumes.h
+++ fs/btrfs/volumes.h
@@ -46,6 +46,7 @@ struct btrfs_device {
 	int barriers;
 	int writeable;
 	int in_fs_metadata;
+	int missing;
 
 	spinlock_t io_lock;
 
@@ -95,6 +96,7 @@ struct btrfs_fs_devices {
 	u64 num_devices;
 	u64 open_devices;
 	u64 rw_devices;
+	u64 missing_devices;
 	u64 total_rw_bytes;
 	struct block_device *latest_bdev;
 
