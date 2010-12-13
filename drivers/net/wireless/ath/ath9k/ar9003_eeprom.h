2be0db797d7f9c7726cc5984b3c0c820b327237c
Felix Fietkau <nbd@openwrt.org>
2010-12-13 08:40:53 +0100

    ath9k_hw: fix the PA predistortion rate mask
    The EEPROM PAPRD rate mask fields only contain mask values for actual
    rates in the low 25 bits. The upper bits are reserved for tx power
    scale values. Add the proper mask definitions and use them before
    writing the values to the register.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/ar9003_eeprom.h
+++ drivers/net/wireless/ath/ath9k/ar9003_eeprom.h
@@ -32,6 +32,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AR9300_ANT_16S               25
 #define AR9300_FUTURE_MODAL_SZ       6
 
+#define AR9300_PAPRD_RATE_MASK		0x01ffffff
+#define AR9300_PAPRD_SCALE_1		0x0e000000
+#define AR9300_PAPRD_SCALE_1_S		25
+#define AR9300_PAPRD_SCALE_2		0x70000000
+#define AR9300_PAPRD_SCALE_2_S		28
+
 /* Delta from which to start power to pdadc table */
 /* This offset is used in both open loop and closed loop power control
  * schemes. In open loop power control, it is not really needed, but for
