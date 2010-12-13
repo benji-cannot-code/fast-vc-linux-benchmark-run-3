084ebb5f74ac677f23babc19c03d6d8ec0181107
Don Zickus <dzickus@redhat.com>
2010-12-13 10:31:58 -0500

    x86, watchdog: Compile fix when CONFIG_LOCAL_APIC not enabled
    When adjusting the code to handle removing the old nmi watchdog,
    I forgot to consider the compile case when the local apic is not
    enabled.
    
    This change fixes the following build error:
    
      arch/x86/kernel/apic/hw_nmi.c:28:6: error: redefinition of ‘touch_nmi_watchdog’
    
    Signed-off-by: Don Zickus <dzickus@redhat.com>
    Acked-by: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Randy Dunlap <randy.dunlap@oracle.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Rakib Mullick <rakib.mullick@gmail.com>
    LKML-Reference: <20101213153719.GD18577@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic/hw_nmi.c
+++ arch/x86/kernel/apic/hw_nmi.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nmi.h>
 #include <linux/module.h>
 
+#ifdef ARCH_HAS_NMI_WATCHDOG
 #ifdef CONFIG_HARDLOCKUP_DETECTOR
 u64 hw_nmi_get_sample_period(void)
 {
@@ -32,6 +33,8 @@ void touch_nmi_watchdog(void)
 }
 EXPORT_SYMBOL(touch_nmi_watchdog);
 #endif
+#endif
+
 #ifdef arch_trigger_all_cpu_backtrace
 /* For reliability, we're prepared to waste bits here. */
 static DECLARE_BITMAP(backtrace_mask, NR_CPUS) __read_mostly;
