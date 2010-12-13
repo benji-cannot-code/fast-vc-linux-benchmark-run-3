0fa2daff18e6fece031b16d12bf39bd7a8fcf76d
Sundar Iyer <sundar.iyer@stericsson.com>
2010-12-13 09:33:12 +0530

    mfd/tc35892: rename tc35892 header to tc3589x
    Rename the header file to include further variants within
    the same mfd core driver
    
    Acked-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Sundar Iyer <sundar.iyer@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>

--- drivers/mfd/tc35892.c
+++ drivers/mfd/tc35892.c
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/i2c.h>
 #include <linux/mfd/core.h>
-#include <linux/mfd/tc35892.h>
+#include <linux/mfd/tc3589x.h>
 
 /**
  * tc35892_reg_read() - read a single TC35892 register
