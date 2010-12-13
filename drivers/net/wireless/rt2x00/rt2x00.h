0c54ff99bcdfd56a0efceacb028713ee8ae3ef35
Johannes Stezenbach <js@sig21.net>
2010-12-13 12:38:49 +0100

    rt2x00: remove stray semicolon
    The stray semicolon after DEBUG_PRINTK_MSG causes things
    like "if (...) WARNING(...); else {}" to fail with syntax error.
    
    Signed-off-by: Johannes Stezenbach <js@sig21.net>
    Acked-by: Gertjan van Wingerde <gwingerde@gmail.com>
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00.h
+++ drivers/net/wireless/rt2x00/rt2x00.h
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_RT2X00_DEBUG
 #define DEBUG_PRINTK(__dev, __kernlvl, __lvl, __msg, __args...)	\
-	DEBUG_PRINTK_MSG(__dev, __kernlvl, __lvl, __msg, ##__args);
+	DEBUG_PRINTK_MSG(__dev, __kernlvl, __lvl, __msg, ##__args)
 #else
 #define DEBUG_PRINTK(__dev, __kernlvl, __lvl, __msg, __args...)	\
 	do { } while (0)
