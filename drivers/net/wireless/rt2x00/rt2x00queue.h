49c3e6bd34f7de6107c21d327796e117891608c5
Ivo van Doorn <ivdoorn@gmail.com>
2010-12-13 12:36:38 +0100

    rt2x00: Fix WMM Queue naming
    The Queue names were incorrectly copied from the legacy drivers,
    as a result the queue names were inversed to what was expected.
    
    This renames the queues using this mapping:
    	QID_AC_BK -> QID_AC_VO (priority 0)
    	QID_AC_BE -> QID_AC_VI (priority 1)
    	QID_AC_VI -> QID_AC_BE (priority 2)
    	QID_AC_VO -> QID_AC_BK (priority 3)
    
    Note that this was a naming problem only, which didn't affect
    the assignment of frames to their respective queues.
    
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2x00queue.h
+++ drivers/net/wireless/rt2x00/rt2x00queue.h
@@ -46,10 +46,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**
  * enum data_queue_qid: Queue identification
  *
+ * @QID_AC_VO: AC VO queue
+ * @QID_AC_VI: AC VI queue
  * @QID_AC_BE: AC BE queue
  * @QID_AC_BK: AC BK queue
- * @QID_AC_VI: AC VI queue
- * @QID_AC_VO: AC VO queue
  * @QID_HCCA: HCCA queue
  * @QID_MGMT: MGMT queue (prio queue)
  * @QID_RX: RX queue
@@ -58,10 +58,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @QID_ATIM: Atim queue (value unspeficied, don't send it to device)
  */
 enum data_queue_qid {
-	QID_AC_BE = 0,
-	QID_AC_BK = 1,
-	QID_AC_VI = 2,
-	QID_AC_VO = 3,
+	QID_AC_VO = 0,
+	QID_AC_VI = 1,
+	QID_AC_BE = 2,
+	QID_AC_BK = 3,
 	QID_HCCA = 4,
 	QID_MGMT = 13,
 	QID_RX = 14,
