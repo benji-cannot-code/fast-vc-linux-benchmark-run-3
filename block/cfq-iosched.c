2485ae6e7ba8dcba3412ff2d28fc02db2cb59a9b
Shaohua Li writes <shaohua.li@intel.com>
2010-12-13 14:32:22 +0100

    block cfq: select new workload if priority changed
    If priority is changed, continuing to check workload_expires and service tree
    count of the previous workload does not make sense. We should always choose
    the workload with lowest key of new priority in such case.
    
    Signed-off-by: Shaohua Li <shaohua.li@intel.com>
    Reviewed-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -2102,6 +2102,7 @@ static void choose_service_tree(struct cfq_data *cfqd, struct cfq_group *cfqg)
 	unsigned count;
 	struct cfq_rb_root *st;
 	unsigned group_slice;
+	enum wl_prio_t original_prio = cfqd->serving_prio;
 
 	if (!cfqg) {
 		cfqd->serving_prio = IDLE_WORKLOAD;
@@ -2120,6 +2121,9 @@ static void choose_service_tree(struct cfq_data *cfqd, struct cfq_group *cfqg)
 		return;
 	}
 
+	if (original_prio != cfqd->serving_prio)
+		goto new_workload;
+
 	/*
 	 * For RT and BE, we have to choose also the type
 	 * (SYNC, SYNC_NOIDLE, ASYNC), and to compute a workload
@@ -2134,6 +2138,7 @@ static void choose_service_tree(struct cfq_data *cfqd, struct cfq_group *cfqg)
 	if (count && !time_after(jiffies, cfqd->workload_expires))
 		return;
 
+new_workload:
 	/* otherwise select new workload type */
 	cfqd->serving_type =
 		cfq_choose_wl(cfqd, cfqg, cfqd->serving_prio);
