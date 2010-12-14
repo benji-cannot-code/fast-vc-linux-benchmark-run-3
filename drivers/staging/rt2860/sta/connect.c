2bb27dd84704702e705d8bd72fbd1baa2e78e6a4
L. Alberto Giménez <agimenez@sysvalve.es>
2010-12-14 02:01:55 +0100

    Staging: rt2860: Sanitize DBGPRINT_ERR macro
    Cleaner implementation. Avoids the need of the double parenthesis to call the
    macro.
    
    Signed-off-by: L. Alberto Giménez <agimenez@sysvalve.es>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rt2860/sta/connect.c
+++ drivers/staging/rt2860/sta/connect.c
@@ -216,8 +216,7 @@ void MlmeCntlMachinePerformAction(struct rt_rtmp_adapter *pAd,
 		break;
 #endif /* RTMP_MAC_USB // */
 	default:
-		DBGPRINT_ERR(("ERROR! CNTL - Illegal message type(=%ld)",
-			      Elem->MsgType));
+		DBGPRINT_ERR("ERROR! CNTL - Illegal message type(=%ld)", Elem->MsgType);
 		break;
 	}
 }
