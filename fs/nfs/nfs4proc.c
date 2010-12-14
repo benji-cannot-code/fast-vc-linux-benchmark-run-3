03f418807d8d38671b83b2bd598d50b67986f9bb
J. Bruce Fields <bfields@redhat.com>
2010-12-13 19:05:46 -0500

    nfs4: fix units bug causing hang on recovery
    Note that cl_lease_time is in jiffies.  This can cause a very long wait
    in the NFS4ERR_CLID_INUSE case.
    
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -3518,7 +3518,7 @@ int nfs4_proc_setclientid(struct nfs_client *clp, u32 program,
 		if (signalled())
 			break;
 		if (loop++ & 1)
-			ssleep(clp->cl_lease_time + 1);
+			ssleep(clp->cl_lease_time / HZ + 1);
 		else
 			if (++clp->cl_id_uniquifier == 0)
 				break;
