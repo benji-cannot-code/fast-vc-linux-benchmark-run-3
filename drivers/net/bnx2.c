b2ecaa08f501ab5296d1c41a3d5de127e45403d7
Tejun Heo <tj@kernel.org>
2010-12-14 06:09:59 +0000

    bnx2: remove cancel_work_sync() from remove_one
    Michael pointed out that bnx2_close() already cancels bp->reset_task
    and thus it is guaranteed to be idle when bnx2_remove_one() is called.
    Remove the unnecessary cancel_work_sync() in remove_one.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -8394,8 +8394,6 @@ bnx2_remove_one(struct pci_dev *pdev)
 	struct net_device *dev = pci_get_drvdata(pdev);
 	struct bnx2 *bp = netdev_priv(dev);
 
-	cancel_work_sync(&bp->reset_task);
-
 	unregister_netdev(dev);
 
 	if (bp->mips_firmware)
