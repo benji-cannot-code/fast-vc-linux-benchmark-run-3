50621bd4e12413e5bdc574078ede84957c48eb86
Timo Teräs <timo.teras@iki.fi>
2010-12-15 20:48:08 +0200

    staging: hv: convert vmbus_on_msg_dpc to not call osd_schedule_callback
    The additional abstraction is unneeded. This also fixes a sleeping
    while atomic issue as osd_schedule_callback can sleep which is
    not allowed for vmbus_on_msg_dpc running in a tasklet.
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=16701
    
    Reviewed-By: Hank Janssen <hjanssen@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Timo Teräs <timo.teras@iki.fi>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/channel_mgmt.c
+++ drivers/staging/hv/channel_mgmt.c
@@ -754,7 +754,6 @@ void vmbus_onmessage(void *context)
 			   hdr->msgtype, size);
 		print_hex_dump_bytes("", DUMP_PREFIX_NONE,
 				     (unsigned char *)msg->u.payload, size);
-		kfree(msg);
 		return;
 	}
 
@@ -763,9 +762,6 @@ void vmbus_onmessage(void *context)
 	else
 		DPRINT_ERR(VMBUS, "Unhandled channel message type %d",
 			   hdr->msgtype);
-
-	/* Free the msg that was allocated in VmbusOnMsgDPC() */
-	kfree(msg);
 }
 
 /*
