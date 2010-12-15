9479a6397e174c1c1b15e66fe0bd8af47b4ad4e1
Chris Wilson <chris@chris-wilson.co.uk>
2010-12-15 09:56:50 +0000

    Revert "drm/i915: Avoid using PIPE_CONTROL on Ironlake"
    Restore PIPE_CONTROL once again just for Ironlake, as it appears that
    MI_USER_INTERRUPT does not have the same coherency guarantees, that is
    on Ironlake the interrupt following a GPU write is not guaranteed to
    arrive after the write is coherent from the CPU, as it does on the
    other generations.
    
    Reported-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Reported-by: Shuang He <shuang.he@intel.com>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=32402
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -350,7 +350,7 @@ static irqreturn_t ironlake_irq_handler(struct drm_device *dev)
 				READ_BREADCRUMB(dev_priv);
 	}
 
-	if (gt_iir & GT_USER_INTERRUPT)
+	if (gt_iir & (GT_USER_INTERRUPT | GT_PIPE_NOTIFY))
 		notify_ring(dev, &dev_priv->ring[RCS]);
 	if (gt_iir & bsd_usr_interrupt)
 		notify_ring(dev, &dev_priv->ring[VCS]);
@@ -1559,6 +1559,7 @@ static int ironlake_irq_postinstall(struct drm_device *dev)
 	else
 		render_irqs =
 			GT_USER_INTERRUPT |
+			GT_PIPE_NOTIFY |
 			GT_BSD_USER_INTERRUPT;
 	I915_WRITE(GTIER, render_irqs);
 	POSTING_READ(GTIER);
