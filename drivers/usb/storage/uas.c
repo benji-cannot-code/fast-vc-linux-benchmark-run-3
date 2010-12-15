77a69ec29c66e4afda851a8e1805462ab96a53b1
Matthew Wilcox <matthew@wil.cx>
2010-12-15 15:44:03 -0500

    USB: uas: Use kzalloc instead of kmalloc
    The IUs are not being fully initialised by the driver (due to the reserved
    space).  Since we should be zeroing reserved fields, use kzalloc to do
    it for us.
    
    Reported-by: Luben Tuikov <ltuikov@yahoo.com>
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/storage/uas.c
+++ drivers/usb/storage/uas.c
@@ -298,7 +298,7 @@ static struct urb *uas_alloc_sense_urb(struct uas_dev_info *devinfo, gfp_t gfp,
 	if (!urb)
 		goto out;
 
-	iu = kmalloc(sizeof(*iu), gfp);
+	iu = kzalloc(sizeof(*iu), gfp);
 	if (!iu)
 		goto free;
 
@@ -329,7 +329,7 @@ static struct urb *uas_alloc_cmd_urb(struct uas_dev_info *devinfo, gfp_t gfp,
 	if (len < 0)
 		len = 0;
 	len = ALIGN(len, 4);
-	iu = kmalloc(sizeof(*iu) + len, gfp);
+	iu = kzalloc(sizeof(*iu) + len, gfp);
 	if (!iu)
 		goto free;
 
