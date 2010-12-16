e594b37b0382a6c47b86383a62cb50ed4b082188
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2010-12-16 13:39:18 -0800

    trace/xen: add skeleton for Xen trace events
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

---
+++ include/trace/events/xen.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM xen
+
+#if !defined(_TRACE_XEN_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_XEN_H
+
+#include <linux/tracepoint.h>
+#include <asm/paravirt_types.h>
+
+#endif /*  _TRACE_XEN_H */
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>
