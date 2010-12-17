6f26b9a1eeb388adc0f5492b0e88ab67f4f8116d
Linus Walleij <linus.walleij@stericsson.com>
2010-12-17 21:16:23 +0100

    ux500: rename modem IRQ and MBOX files
    Suffix the U5500 modem IRQ and MBOX files with *-db5500* so that
    we clearly know the SoC they belong to, in line with the rest of
    the files in mach-ux500.
    
    Cc: Stefan Nilsson <stefan.xk.nilsson@stericsson.com>
    Cc: Martin Persson <martin.persson@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>

--- arch/arm/mach-ux500/mbox-db5500.c
+++ arch/arm/mach-ux500/mbox-db5500.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
 #include <linux/completion.h>
-#include <mach/mbox.h>
+#include <mach/mbox-db5500.h>
 
 #define MBOX_NAME "mbox"
 
