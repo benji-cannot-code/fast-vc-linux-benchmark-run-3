d2b168100c5c8e914dd317f4d22377b87a46673b
Jerome Marchand <jmarchan@redhat.com>
2010-12-17 16:59:33 +0100

    Staging: zram: make ZRAM depends on SYSFS
    We can not configure zram device without sysfs anyway, so make zram
    depends on it.
    
    Signed-off-by: Jerome Marchand <jmarchan@redhat.com>
    Acked-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "zram_drv.h"
 
-#ifdef CONFIG_SYSFS
-
 static u64 zram_stat64_read(struct zram *zram, u64 *v)
 {
 	u64 val;
@@ -221,5 +219,3 @@ static struct attribute *zram_disk_attrs[] = {
 struct attribute_group zram_disk_attr_group = {
 	.attrs = zram_disk_attrs,
 };
-
-#endif	/* CONFIG_SYSFS */
