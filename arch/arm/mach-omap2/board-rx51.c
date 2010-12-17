c7b24f769e3fc7f6782957f971799785dc09efff
Aaro Koskinen <aaro.koskinen@nokia.com>
2010-12-17 15:13:44 -0800

    arm: omap: add sdram-nokia.h
    Add a header file for Nokia SDRAM functions. Based on patches by Juha
    Keski-Saari.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@nokia.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-rx51.c
+++ arch/arm/mach-omap2/board-rx51.c
@@ -33,10 +33,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "mux.h"
 #include "pm.h"
+#include "sdram-nokia.h"
 
 #define RX51_GPIO_SLEEP_IND 162
 
-struct omap_sdrc_params *nokia_get_sdram_timings(void);
 extern void rx51_video_mem_init(void);
 
 static struct gpio_led gpio_leds[] = {
--- arch/arm/mach-omap2/sdram-nokia.c
+++ arch/arm/mach-omap2/sdram-nokia.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/clock.h>
 #include <plat/sdrc.h>
 
+#include "sdram-nokia.h"
 
 /* In picoseconds, except for tREF (ns), tXP, tCKE, tWTR (clks) */
 struct sdram_timings {
