b47380b19805af116e453de5a49996d01a15aeaa
Arnd Hannemann <arnd@arndnet.de>
2010-12-19 21:16:07 +0000

    mmc: tmio_mmc: silence compiler warnings
    with "mmc: tmio: implement a bounce buffer for unaligned DMA"
    gcc generates the following warnings:
    
    drivers/mmc/host/tmio_mmc.c:654:6: warning: 'ret' may be used uninitialized in this function
    drivers/mmc/host/tmio_mmc.c:730:6: warning: 'ret' may be used uninitialized in this function
    
    This patch fixes these by setting ret to -EINVAL in the affected code paths.
    
    Signed-off-by: Arnd Hannemann <arnd@arndnet.de>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/tmio_mmc.c
+++ drivers/mmc/host/tmio_mmc.c
@@ -674,8 +674,10 @@ static void tmio_mmc_start_dma_rx(struct tmio_mmc_host *host)
 	}
 
 	if ((!aligned && (host->sg_len > 1 || sg->length > PAGE_CACHE_SIZE ||
-			  align >= MAX_ALIGN)) || !multiple)
+			  align >= MAX_ALIGN)) || !multiple) {
+		ret = -EINVAL;
 		goto pio;
+	}
 
 	/* The only sg element can be unaligned, use our bounce buffer then */
 	if (!aligned) {
@@ -749,8 +751,10 @@ static void tmio_mmc_start_dma_tx(struct tmio_mmc_host *host)
 	}
 
 	if ((!aligned && (host->sg_len > 1 || sg->length > PAGE_CACHE_SIZE ||
-			  align >= MAX_ALIGN)) || !multiple)
+			  align >= MAX_ALIGN)) || !multiple) {
+		ret = -EINVAL;
 		goto pio;
+	}
 
 	/* The only sg element can be unaligned, use our bounce buffer then */
 	if (!aligned) {
