d948aecd9069bb8c7fbcd4936ddeab2c9e9c0141
Shan Wei <shanwei@cn.fujitsu.com>
2010-12-19 21:59:35 -0800

    net: kill unused macros
    These macros never be used, so remove them.
    
    Signed-off-by: Shan Wei <shanwei@cn.fujitsu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/netpoll.c
+++ net/core/netpoll.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_UDP_CHUNK 1460
 #define MAX_SKBS 32
-#define MAX_QUEUE_DEPTH (MAX_SKBS / 2)
 
 static struct sk_buff_head skb_pool;
 
