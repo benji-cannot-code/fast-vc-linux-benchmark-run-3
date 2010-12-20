e6897435ef54ff608b0ccf38d71f6ea6b37a7300
John W. Linville <linville@tuxdriver.com>
2010-12-20 15:16:53 -0500

    rtl818x: move rtl8180 and rtl8187 to separate subdirectories
    These drivers share one header file, but nothing else.  Worse, both
    drivers use the rtl8225 part with different register settings.  The
    results has been some ugly naming -- let's simplify that.
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Acked-by: Hin-Tak Leung <htl10@users.sourceforge.net>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Acked-by: Herton Ronaldo Krzesinski <herton@mandriva.com.br>

--- drivers/net/wireless/rtl818x/rtl8180/sa2400.c
+++ drivers/net/wireless/rtl818x/rtl8180/sa2400.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/mac80211.h>
 
 #include "rtl8180.h"
-#include "rtl8180_sa2400.h"
+#include "sa2400.h"
 
 static const u32 sa2400_chan[] = {
 	0x00096c, /* ch1 */
