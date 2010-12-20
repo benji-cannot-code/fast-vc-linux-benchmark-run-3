f3dd3dbfac2e134895406f69d12efbc6f11ad1b4
Aaro Koskinen <aaro.koskinen@iki.fi>
2010-12-20 23:50:11 +0200

    sisfb: delete HAVE_CONFIG_H checks
    Delete unneeded HAVE_CONFIG_H checks.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Cc: Thomas Winischhofer <thomas@winischhofer.net>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/video/sis/init.c
+++ drivers/video/sis/init.c
@@ -57,10 +57,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Used by permission.
  */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #include "init.h"
 
 #ifdef CONFIG_FB_SIS_300
--- drivers/video/sis/init301.c
+++ drivers/video/sis/init301.c
@@ -58,10 +58,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
 #if 1
 #define SET_EMI		/* 302LV/ELV: Set EMI values */
 #endif
