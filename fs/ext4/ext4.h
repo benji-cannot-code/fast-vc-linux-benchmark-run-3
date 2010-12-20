6ba40006d84ce84a06eec2b85e3bacaad4e74686
Eric Sandeen <sandeen@redhat.com>
2010-12-19 22:10:31 -0500

    ext4: zero out nanosecond timestamps for small inodes
    When nanosecond timestamp resolution isn't supported on an ext4
    partition (inode size = 128), stat() appears to be returning
    uninitialized garbage in the nanosecond component of timestamps.
    
    EXT4_INODE_GET_XTIME should zero out tv_nsec when EXT4_FITS_IN_INODE
    evaluates to false.
    
    Reported-by: Jordan Russell <jr-list-2010@quo.to>
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -694,6 +694,8 @@ do {									       \
 	if (EXT4_FITS_IN_INODE(raw_inode, EXT4_I(inode), xtime ## _extra))     \
 		ext4_decode_extra_time(&(inode)->xtime,			       \
 				       raw_inode->xtime ## _extra);	       \
+	else								       \
+		(inode)->xtime.tv_nsec = 0;				       \
 } while (0)
 
 #define EXT4_EINODE_GET_XTIME(xtime, einode, raw_inode)			       \
@@ -704,6 +706,8 @@ do {									       \
 	if (EXT4_FITS_IN_INODE(raw_inode, einode, xtime ## _extra))	       \
 		ext4_decode_extra_time(&(einode)->xtime,		       \
 				       raw_inode->xtime ## _extra);	       \
+	else								       \
+		(einode)->xtime.tv_nsec = 0;				       \
 } while (0)
 
 #define i_disk_version osd1.linux1.l_i_version
