77f0cf6a765c08cd4b6ecf9b8564f9ed5497c2a9
Jarkko Nikula <jhnikula@gmail.com>
2010-12-21 17:46:01 +0000

    omap: rx51: Remove tvout code that plays with gpio 40
    Commit 60d24ee "Added video data to support tvout on rx51" added code that
    tries to assign gpio 40 as OMAP DSS reset_gpio for tvout. This is wrong
    since this gpio has nothing to do with OMAP DSS but it is used to control
    one switch that selects is the audio jack connected to tvout or audio
    circuitry.
    
    This switch is already supported by the RX51 ASoC driver so there is no need
    to control it elsewhere. Switch is contolled with ALSA control
    'Jack Function' and tvout can be selected with following example:
    
    	amixer -D hw:0 set 'Jack Function' 'TV-OUT'
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Cc: Srikar <ext-srikar.1.bhavanarayana@nokia.com>
    Acked-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-rx51-video.c
+++ arch/arm/mach-omap2/board-rx51-video.c
@@ -25,9 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mux.h"
 
 #define RX51_LCD_RESET_GPIO	90
-/* REVISIT  to verify with rx51.c at sound/soc/omap */
-#define RX51_TVOUT_SEL_GPIO	40
-
 
 #if defined(CONFIG_FB_OMAP2) || defined(CONFIG_FB_OMAP2_MODULE)
 
@@ -38,17 +35,6 @@ static int rx51_lcd_enable(struct omap_dss_device *dssdev)
 }
 
 static void rx51_lcd_disable(struct omap_dss_device *dssdev)
-{
-	gpio_set_value(dssdev->reset_gpio, 0);
-}
-
-static int rx51_tvout_enable(struct omap_dss_device *dssdev)
-{
-	gpio_set_value(dssdev->reset_gpio, 1);
-	return 0;
-}
-
-static void rx51_tvout_disable(struct omap_dss_device *dssdev)
 {
 	gpio_set_value(dssdev->reset_gpio, 0);
 }
@@ -68,9 +54,6 @@ static struct omap_dss_device  rx51_tv_device = {
 	.type			= OMAP_DISPLAY_TYPE_VENC,
 	.driver_name		= "venc",
 	.phy.venc.type	        = OMAP_DSS_VENC_TYPE_COMPOSITE,
-	.reset_gpio	        = RX51_TVOUT_SEL_GPIO,
-	.platform_enable        = rx51_tvout_enable,
-	.platform_disable       = rx51_tvout_disable,
 };
 
 static struct omap_dss_device *rx51_dss_devices[] = {
@@ -112,9 +95,6 @@ static int __init rx51_video_init(void)
 	}
 
 	gpio_direction_output(RX51_LCD_RESET_GPIO, 1);
-
-	/* REVISIT  to verify with rx51.c at sound/soc/omap */
-	gpio_direction_output(RX51_TVOUT_SEL_GPIO, 1);
 
 	platform_add_devices(rx51_video_devices,
 				ARRAY_SIZE(rx51_video_devices));
