a5e5be1b13280c13ad46f61e081aa5563a4504bd
Jarkko Nikula <jhnikula@gmail.com>
2010-12-20 18:48:15 -0800

    omap: rx51: Remove extra tlv320aic3x.h inclusion
    Remove second tlv320aic3x.h inclusion that came along the commit f0fba2a
    "ASoC: multi-component - ASoC Multi-Component Support".
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Cc: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-rx51-peripherals.c
+++ arch/arm/mach-omap2/board-rx51-peripherals.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/gpio_keys.h>
 #include <linux/mmc/host.h>
-#include <sound/tlv320aic3x.h>
 
 #include <plat/mcspi.h>
 #include <plat/board.h>
