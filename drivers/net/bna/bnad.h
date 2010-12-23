d943b7d6706bee0d086cf7d4cad58a107c47027d
Rasesh Mody <rmody@brocade.com>
2010-12-23 21:45:05 +0000

    bna: Fix for TX queue
    Change Details:
    	- Call netif_wake_queue() if we have freed up sufficient elements
    	at the end of completion processing
    	- Add netif_queue_stopped counter back to bnad_drv_stats {}
    	- Get netif_queue_stopped value from stack
    	- Remove BUG_ON() on value returned by pci_unmap_addr()
    
    Signed-off-by: Debashis Dutt <ddutt@brocade.com>
    Signed-off-by: Rasesh Mody <rmody@brocade.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bna/bnad.h
+++ drivers/net/bna/bnad.h
@@ -127,6 +127,7 @@ struct bnad_completion {
 struct bnad_drv_stats {
 	u64 		netif_queue_stop;
 	u64		netif_queue_wakeup;
+	u64		netif_queue_stopped;
 	u64		tso4;
 	u64		tso6;
 	u64		tso_err;
@@ -309,8 +310,10 @@ extern void bnad_cleanup_rx(struct bnad *bnad, uint rx_id);
 extern void bnad_dim_timer_start(struct bnad *bnad);
 
 /* Statistics */
-extern void bnad_netdev_qstats_fill(struct bnad *bnad, struct rtnl_link_stats64 *stats);
-extern void bnad_netdev_hwstats_fill(struct bnad *bnad, struct rtnl_link_stats64 *stats);
+extern void bnad_netdev_qstats_fill(struct bnad *bnad,
+		struct rtnl_link_stats64 *stats);
+extern void bnad_netdev_hwstats_fill(struct bnad *bnad,
+		struct rtnl_link_stats64 *stats);
 
 /**
  * MACROS
