3ce10bd1bd39d62dbbc536c9818ce42ef9d8c2ef
Mauro Carvalho Chehab <mchehab@redhat.com>
2010-12-27 11:47:17 -0300

    [media] lirc_zilog: Fix a warning
    drivers/staging/lirc/lirc_zilog.c: In function ‘send_code’:
    drivers/staging/lirc/lirc_zilog.c:886:1: warning: label ‘done’ defined but not used
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/lirc/lirc_zilog.c
+++ drivers/staging/lirc/lirc_zilog.c
@@ -884,7 +884,6 @@ static int send_code(struct IR *ir, unsigned int code, unsigned int key)
 		return -EFAULT;
 	}
 
-done:
 	/* Oh good, it worked */
 	dprintk("sent code %u, key %u\n", code, key);
 	return 0;
