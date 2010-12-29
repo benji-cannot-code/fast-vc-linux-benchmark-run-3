10d897f7324864797d4a61a016664a1488377e53
Hans Verkuil <hverkuil@xs4all.nl>
2010-12-29 14:29:55 -0300

    [media] v4l: fix handling of v4l2_input.capabilities
    The v4l core sets the v4l2_input.capabilities field based on the supplied
    v4l2_ioctl_ops. However, several drivers do a memset or memcpy of the v4l2_input
    struct, thus overwriting that field incorrectly.
    
    Either remove the memset (which is already done by the v4l core), or add the
    proper capabilities field in case of a memcpy.
    
    The same is also true for v4l2_output, but that only affected the ivtv driver.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/ivtv/ivtv-cards.c
+++ drivers/media/video/ivtv/ivtv-cards.c
@@ -1314,7 +1314,6 @@ int ivtv_get_input(struct ivtv *itv, u16 index, struct v4l2_input *input)
 		"Composite 3"
 	};
 
-	memset(input, 0, sizeof(*input));
 	if (index >= itv->nof_inputs)
 		return -EINVAL;
 	input->index = index;
@@ -1332,7 +1331,6 @@ int ivtv_get_output(struct ivtv *itv, u16 index, struct v4l2_output *output)
 {
 	const struct ivtv_card_output *card_output = itv->card->video_outputs + index;
 
-	memset(output, 0, sizeof(*output));
 	if (index >= itv->card->nof_outputs)
 		return -EINVAL;
 	output->index = index;
