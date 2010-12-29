939299c084ffe6081cfc16e4a3d7d4c9250f2029
Ben Hutchings <ben@decadent.org.uk>
2010-12-29 14:55:03 +0000

    btrfs: Require CAP_SYS_ADMIN for filesystem rebalance
    Filesystem rebalancing (BTRFS_IOC_BALANCE) affects the entire
    filesystem and may run uninterruptibly for a long time.  This does not
    seem to be something that an unprivileged user should be able to do.
    
    Reported-by: Aron Xu <happyaron.xu@gmail.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/blkdev.h>
 #include <linux/random.h>
 #include <linux/iocontext.h>
+#include <linux/capability.h>
 #include <asm/div64.h>
 #include "compat.h"
 #include "ctree.h"
@@ -2024,6 +2025,9 @@ int btrfs_balance(struct btrfs_root *dev_root)
 
 	if (dev_root->fs_info->sb->s_flags & MS_RDONLY)
 		return -EROFS;
+
+	if (!capable(CAP_SYS_ADMIN))
+		return -EPERM;
 
 	mutex_lock(&dev_root->fs_info->volume_mutex);
 	dev_root = dev_root->fs_info->dev_root;
