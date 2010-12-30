8ff26100dd4bd0e4db972dae5aa64fc7dbb511cf
John Fastabend <john.r.fastabend@intel.com>
2010-12-30 09:26:31 +0000

    dcbnl: add appliction tlv handlers
    This patch adds application tlv handlers. Networking stacks
    may use the application priority to set the skb priority of
    their stack using the negoatiated dcbx priority.
    
    This patch provides the dcb_{get|set}app() routines for the
    stack to query these parameters. Notice lower layer drivers
    can use the dcbnl_ops routines if additional handling is
    needed. Perhaps in the firmware case for example
    
    Signed-off-by: John Fastabend <john.r.fastabend@intel.com>
    Signed-off-by: Shmulik Ravid <shmulikr@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/dcbnl.h
+++ include/net/dcbnl.h
@@ -23,6 +23,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/dcbnl.h>
 
+struct dcb_app_type {
+	char		  name[IFNAMSIZ];
+	struct dcb_app	  app;
+	struct list_head  list;
+};
+
+u8 dcb_setapp(struct net_device *, struct dcb_app *);
+u8 dcb_getapp(struct net_device *, struct dcb_app *);
+
 /*
  * Ops struct for the netlink callbacks.  Used by DCB-enabled drivers through
  * the netdevice struct.
