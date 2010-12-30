23071a7aa9a6493bb2d3c702e6736e1508561960
Richard Zhao <richard.zhao@freescale.com>
2010-12-30 19:25:02 +0800

    arm: mx51: define mx51's own MXC_GPIO_IRQS
    mx5 SoCs have different GPIO port count.
    
    Signed-off-by: Richard Zhao <richard.zhao@freescale.com>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/plat-mxc/include/mach/irqs.h
+++ arch/arm/plat-mxc/include/mach/irqs.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MXC_GPIO_IRQS		(32 * 4)
 #elif defined CONFIG_ARCH_MX25
 #define MXC_GPIO_IRQS		(32 * 4)
-#elif defined CONFIG_ARCH_MX5
+#elif defined CONFIG_ARCH_MX51
 #define MXC_GPIO_IRQS		(32 * 4)
 #elif defined CONFIG_ARCH_MXC91231
 #define MXC_GPIO_IRQS		(32 * 4)
