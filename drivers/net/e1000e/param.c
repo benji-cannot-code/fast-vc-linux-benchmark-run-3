8b1c5aaaf53b6cf4a279216ee3a3e279eb608715
Bruce Allan <bruce.w.allan@intel.com>
2010-12-31 06:10:01 +0000

    e1000e: consistent use of Rx/Tx vs. RX/TX/rx/tx in comments/logs
    Some minor comment errors and whitespace issues discovered while looking
    into this are also addressed.
    
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Tested-by: Jeff Pieper <jeffrey.e.pieper@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/e1000e/param.c
+++ drivers/net/e1000e/param.c
@@ -63,10 +63,9 @@ MODULE_PARM_DESC(copybreak,
 	module_param_array_named(X, X, int, &num_##X, 0);	\
 	MODULE_PARM_DESC(X, desc);
 
-
 /*
  * Transmit Interrupt Delay in units of 1.024 microseconds
- * Tx interrupt delay needs to typically be set to something non zero
+ * Tx interrupt delay needs to typically be set to something non-zero
  *
  * Valid Range: 0-65535
  */
@@ -113,6 +112,7 @@ E1000_PARAM(InterruptThrottleRate, "Interrupt Throttling Rate");
 #define DEFAULT_ITR 3
 #define MAX_ITR 100000
 #define MIN_ITR 100
+
 /* IntMode (Interrupt Mode)
  *
  * Valid Range: 0 - 2
