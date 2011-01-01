21d49430f6756ed822e66b2553097ee239295541
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-01-01 14:56:18 -0800

    tipc: update log.h re-include protection to reflect new name
    The tipc/dbg.h file was recently renamed to tipc/log.h,
    but the re-include define was not updated accordingly.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/log.h
+++ net/tipc/log.h
@@ -35,8 +35,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _TIPC_DBG_H
-#define _TIPC_DBG_H
+#ifndef _TIPC_LOG_H
+#define _TIPC_LOG_H
 
 /**
  * struct print_buf - TIPC print buffer structure
