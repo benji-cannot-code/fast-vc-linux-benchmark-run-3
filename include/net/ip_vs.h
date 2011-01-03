cd86c59f73ef36d6bedf6c6cdd5e1c807574bdd7
Hans Schillstrom <hans.schillstrom@ericsson.com>
2011-01-03 14:45:00 +0100

    IPVS: netns, trash handling
    trash list per namspace,
    and reordering of some params in dst struct.
    
    [ horms@verge.net.au: Use cancel_delayed_work_sync() instead of
    	              cancel_rearming_delayed_work(). Found during
    		      merge conflict resoliution ]
    Signed-off-by: Hans Schillstrom <hans.schillstrom@ericsson.com>
    Acked-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- include/net/ip_vs.h
+++ include/net/ip_vs.h
@@ -663,8 +663,8 @@ struct ip_vs_dest {
 	struct list_head	d_list;   /* for table with all the dests */
 
 	u16			af;		/* address family */
-	union nf_inet_addr	addr;		/* IP address of the server */
 	__be16			port;		/* port number of the server */
+	union nf_inet_addr	addr;		/* IP address of the server */
 	volatile unsigned	flags;		/* dest status flags */
 	atomic_t		conn_flags;	/* flags to copy to conn */
 	atomic_t		weight;		/* server weight */
@@ -691,8 +691,8 @@ struct ip_vs_dest {
 	/* for virtual service */
 	struct ip_vs_service	*svc;		/* service it belongs to */
 	__u16			protocol;	/* which protocol (TCP/UDP) */
-	union nf_inet_addr	vaddr;		/* virtual IP address */
 	__be16			vport;		/* virtual port number */
+	union nf_inet_addr	vaddr;		/* virtual IP address */
 	__u32			vfwmark;	/* firewall mark of service */
 };
 
