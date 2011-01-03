df6000cf01dcc433bf0c5f2dbb720ae4d2b0de70
Nishanth Menon <nm@ti.com>
2011-01-03 12:58:30 -0600

    OMAP2+: TWL: include pm header for init protos
    twl_init functions are declared in pm.h and used in pm.c
    pm.h header defining the protos need to be included to
    ensure that omap_twl.c has consistent function definition.
    This fixes sparse warning:
    arch/arm/mach-omap2/omap_twl.c:237:12: warning: symbol 'omap4_twl_init' was not declared. Should it be static?
    arch/arm/mach-omap2/omap_twl.c:256:12: warning: symbol 'omap3_twl_init' was not declared. Should it be static?
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- arch/arm/mach-omap2/omap_twl.c
+++ arch/arm/mach-omap2/omap_twl.c
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat/voltage.h>
 
+#include "pm.h"
+
 #define OMAP3_SRI2C_SLAVE_ADDR		0x12
 #define OMAP3_VDD_MPU_SR_CONTROL_REG	0x00
 #define OMAP3_VDD_CORE_SR_CONTROL_REG	0x01
