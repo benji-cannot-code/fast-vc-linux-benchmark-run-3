077915111b9364370bc4f2c7efa58e0b88044566
Stephane Eranian <eranian@google.com>
2011-01-04 16:30:01 +0200

    perf tools: Fix perf_event.h header usage
    This patch fixes the usage of the perf_event.h header file
    between command modules and the supporting code in util.
    
    It is necessary to ensure that ALL files use the SAME
    perf_event.h header from the kernel source tree.
    
    There were a couple of #include <linux/perf_event.h> mixed
    with #include "../../perf_event.h".
    
    This caused issues on some distros because of mismatch
    in the layout of struct perf_event_attr. That eventually
    led perf stat to segfault.
    
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Robert Richter <robert.richter@amd.com>
    Cc: Stephane Eranian <eranian@gmail.com>
    LKML-Reference: <4d233cf0.2308e30a.7b00.ffffc187@mx.google.com>
    Signed-off-by: Stephane Eranian <eranian@google.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/parse-events.h
+++ tools/perf/util/parse-events.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Parse symbolic events/counts passed in as options:
  */
 
-#include <linux/perf_event.h>
+#include "../../../include/linux/perf_event.h"
 
 struct list_head;
 struct perf_evsel;
