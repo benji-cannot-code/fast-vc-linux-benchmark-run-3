2825762a55c675a284e7e047508dc806236aeec0
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-01-05 12:31:15 -0300

    [media] cx25821: Fix compilation breakage due to BKL dependency
    drivers/staging/cx25821/cx25821-video.c: In function ‘video_open’:
    drivers/staging/cx25821/cx25821-video.c:817:8: error: implicit declaration of function ‘lock_kernel’
    drivers/staging/cx25821/cx25821-video.c:834:9: error: implicit declaration of function ‘unlock_kernel’
    
    This patch just adds a header with BKL function. It should be enough
    to avoid compilation breakage, but, as BKL will be removed, we need
    to properly fix it on a latter patch.
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Reported-by: Anca Emanuel <anca.emanuel@gmail.com>
    Cc: Palash Bandyopadhyay <Palash.Bandyopadhyay@conexant.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/cx25821/cx25821-video.c
+++ drivers/staging/cx25821/cx25821-video.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include "cx25821-video.h"
+#include <linux/smp_lock.h>
 
 MODULE_DESCRIPTION("v4l2 driver module for cx25821 based TV cards");
 MODULE_AUTHOR("Hiep Huynh <hiep.huynh@conexant.com>");
