c2558d6eb2dd4b3fe30058c80f3667be7005a364
Rafael J. Wysocki <rjw@sisk.pl>
2011-01-07 01:45:58 +0100

    ACPI / PM: Make suspend_nvs_save() use acpi_os_map_memory()
    It turns out that the NVS memory region that suspend_nvs_save()
    attempts to map has been already mapped by acpi_os_map_memory(), so
    suspend_nvs_save() should better use acpi_os_map_memory() for mapping
    memory to avoid conflicts.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/nvs.c
+++ drivers/acpi/nvs.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/slab.h>
 #include <linux/acpi.h>
+#include <acpi/acpiosxf.h>
 
 /*
  * Platforms, like ACPI, may want us to save some memory used by them during
@@ -80,7 +81,7 @@ void suspend_nvs_free(void)
 			free_page((unsigned long)entry->data);
 			entry->data = NULL;
 			if (entry->kaddr) {
-				iounmap(entry->kaddr);
+				acpi_os_unmap_memory(entry->kaddr, entry->size);
 				entry->kaddr = NULL;
 			}
 		}
@@ -114,7 +115,8 @@ int suspend_nvs_save(void)
 
 	list_for_each_entry(entry, &nvs_list, node)
 		if (entry->data) {
-			entry->kaddr = ioremap(entry->phys_start, entry->size);
+			entry->kaddr = acpi_os_map_memory(entry->phys_start,
+							  entry->size);
 			if (!entry->kaddr) {
 				suspend_nvs_free();
 				return -ENOMEM;
