7a869a1376c024b2c9a2e0b7a2d1d2aea49f5a4e
Jason Liu <r64343@freescale.com>
2011-01-11 05:44:29 +0800

    ARM i.MX50: Rename devices-mx50.h
    There are devices-imx51.h and devices-imx53.h under
    arch/arm/mach-mx5 directory. So, had better rename
    devices-mx50.h to devices-imx50.h to follow the same
    naming convention with imx51 and imx53 part.
    
    Signed-off-by: Jason Liu <r64343@freescale.com>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-mx5/board-mx50_rdp.c
+++ arch/arm/mach-mx5/board-mx50_rdp.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/mach/time.h>
 
-#include "devices-mx50.h"
+#include "devices-imx50.h"
 
 static iomux_v3_cfg_t mx50_rdp_pads[] __initdata = {
 	/* SD1 */
