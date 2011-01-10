291a8e44942cbf55be2bd23e9f491a4de3d38c95
Clemens Ladisch <clemens@ladisch.de>
2011-01-10 16:16:08 +0100

    ALSA: virtuoso: reduce MCLK in double rate modes
    For the CSxxxx and AKxxxx DAC/ADC chips, the MCLK factor in double rate
    modes (64-96 kHz) can be reduced to 128x without reducing sound quality.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/oxygen/xonar_pcm179x.c
+++ sound/pci/oxygen/xonar_pcm179x.c
@@ -1033,7 +1033,7 @@ static const struct oxygen_model model_xonar_d2 = {
 	.function_flags = OXYGEN_FUNCTION_SPI |
 			  OXYGEN_FUNCTION_ENABLE_SPI_4_5,
 	.dac_mclks = OXYGEN_MCLKS(512, 128, 128),
-	.adc_mclks = OXYGEN_MCLKS(256, 256, 128),
+	.adc_mclks = OXYGEN_MCLKS(256, 128, 128),
 	.dac_i2s_format = OXYGEN_I2S_FORMAT_I2S,
 	.adc_i2s_format = OXYGEN_I2S_FORMAT_LJUST,
 };
@@ -1067,7 +1067,7 @@ static const struct oxygen_model model_xonar_hdav = {
 	.misc_flags = OXYGEN_MISC_MIDI,
 	.function_flags = OXYGEN_FUNCTION_2WIRE,
 	.dac_mclks = OXYGEN_MCLKS(512, 128, 128),
-	.adc_mclks = OXYGEN_MCLKS(256, 256, 128),
+	.adc_mclks = OXYGEN_MCLKS(256, 128, 128),
 	.dac_i2s_format = OXYGEN_I2S_FORMAT_I2S,
 	.adc_i2s_format = OXYGEN_I2S_FORMAT_LJUST,
 };
@@ -1098,7 +1098,7 @@ static const struct oxygen_model model_xonar_st = {
 	.dac_volume_max = 255,
 	.function_flags = OXYGEN_FUNCTION_2WIRE,
 	.dac_mclks = OXYGEN_MCLKS(512, 128, 128),
-	.adc_mclks = OXYGEN_MCLKS(256, 256, 128),
+	.adc_mclks = OXYGEN_MCLKS(256, 128, 128),
 	.dac_i2s_format = OXYGEN_I2S_FORMAT_I2S,
 	.adc_i2s_format = OXYGEN_I2S_FORMAT_LJUST,
 };
