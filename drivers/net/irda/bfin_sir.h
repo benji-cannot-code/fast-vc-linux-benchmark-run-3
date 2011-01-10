04b2fc345186973c2a821310488628f06ba87753
Mike Frysinger <vapier@gentoo.org>
2011-01-10 02:45:57 -0500

    net/irda: bfin_sir: back out transitional defines
    Now that the common header is sane, we can drop the transitional cruft.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- drivers/net/irda/bfin_sir.h
+++ drivers/net/irda/bfin_sir.h
@@ -27,9 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cacheflush.h>
 #include <asm/dma.h>
 #include <asm/portmux.h>
-/* Some transitional glue */
-#include <linux/serial_core.h>
-#include <linux/circ_buf.h>
 #include <mach/bfin_serial_5xx.h>
 #undef DRIVER_NAME
 
