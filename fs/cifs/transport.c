56673364ed8e91feb874941206d2ed5229fa4742
Jeff Layton <jlayton@redhat.com>
2011-01-11 07:24:21 -0500

    cifs: add ability to send an echo request
    Reviewed-by: Suresh Jayaraman <sjayaraman@suse.de>
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/transport.c
+++ fs/cifs/transport.c
@@ -79,7 +79,7 @@ AllocMidQEntry(const struct smb_hdr *smb_buffer, struct TCP_Server_Info *server)
 	return temp;
 }
 
-static void
+void
 DeleteMidQEntry(struct mid_q_entry *midEntry)
 {
 #ifdef CONFIG_CIFS_STATS2
