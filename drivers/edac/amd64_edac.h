e948f25442b7ba4fd8a26367f1eae0c5a83b2e1c
Borislav Petkov <borislav.petkov@amd.com>
2011-01-11 22:08:07 +0100

    amd64_edac: Add support for interleaved region swapping
    On revC3 and revE Fam10h machines and later, non-interleaved graphics
    framebuffer memory under the 16G mark can be swapped with a region
    located at the bottom of memory so that the GPU can use the interleaved
    region and thus two channels. Add support for that.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>

--- drivers/edac/amd64_edac.h
+++ drivers/edac/amd64_edac.h
@@ -240,6 +240,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define dct_dram_enabled(pvt)		((pvt)->dct_sel_lo & BIT(8))
 #define dct_memory_cleared(pvt)		((pvt)->dct_sel_lo & BIT(10))
 
+#define SWAP_INTLV_REG			0x10c
+
 #define DCT_SEL_HI			0x114
 
 /*
