c594957a5097ce07779de00f80f9ac51d41b5ead
Takashi Iwai <tiwai@suse.de>
2011-01-11 17:58:26 +0100

    ASoC: Fix section mismatch in wm8995.c
    __devinitconst can't be used for data referred in driver struct.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/soc/codecs/wm8995.c
+++ sound/soc/codecs/wm8995.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "wm8995.h"
 
-static const u16 wm8995_reg_defs[WM8995_MAX_REGISTER + 1] __devinitconst = {
+static const u16 wm8995_reg_defs[WM8995_MAX_REGISTER + 1] = {
 	[0]     = 0x8995, [5]     = 0x0100, [16]    = 0x000b, [17]    = 0x000b,
 	[24]    = 0x02c0, [25]    = 0x02c0, [26]    = 0x02c0, [27]    = 0x02c0,
 	[28]    = 0x000f, [32]    = 0x0005, [33]    = 0x0005, [40]    = 0x0003,
