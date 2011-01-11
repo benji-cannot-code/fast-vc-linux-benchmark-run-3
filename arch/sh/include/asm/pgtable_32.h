473b2e3eb9a1ae3e20b5408d79778b0b52f956e2
Paul Mundt <lethal@linux-sh.org>
2011-01-11 15:02:59 +0900

    sh: Fix up legacy PTEA space attribute mapping.
    When p3_ioremap() was converted to ioremap_prot() there was some breakage
    introduced where the 29-bit segmentation logic would trap the area range
    and return an identity mapping without having allowed the area
    specification to force mapping through page tables. This wires up a PCC
    mask for pgprot verification to work out whether to short-circuit the
    identity mapping on legacy parts, restoring the previous behaviour.
    
    Reported-by: Nobuhiro Iwamatsu <iwamatsu@nigauri.org>
    Cc: stable@kernel.org
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/pgtable_32.h
+++ arch/sh/include/asm/pgtable_32.h
@@ -77,6 +77,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Wrapper for extended mode pgprot twiddling */
 #define _PAGE_EXT(x)		((unsigned long long)(x) << 32)
 
+#ifdef CONFIG_X2TLB
+#define _PAGE_PCC_MASK	0x00000000	/* No legacy PTEA support */
+#else
+
 /* software: moves to PTEA.TC (Timing Control) */
 #define _PAGE_PCC_AREA5	0x00000000	/* use BSC registers for area5 */
 #define _PAGE_PCC_AREA6	0x80000000	/* use BSC registers for area6 */
@@ -90,7 +94,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PAGE_PCC_ATR8	0x60000000	/* Attribute Memory space, 8 bit bus */
 #define _PAGE_PCC_ATR16	0x60000001	/* Attribute Memory space, 6 bit bus */
 
-#ifndef CONFIG_X2TLB
+#define _PAGE_PCC_MASK	0xe0000001
+
 /* copy the ptea attributes */
 static inline unsigned long copy_ptea_attributes(unsigned long x)
 {
