0fd0fe4782d89048937eeb72bd882816880a8ffa
Stefan Schmidt <stefan@datenfreihafen.org>
2011-01-12 10:30:42 +0100

    fs/btrfs: Fix build of ctree
    Fix the build failure in some configurations:
    
         CC [M]  fs/btrfs/ctree.o
      In file included from fs/btrfs/ctree.c:21:0:
      fs/btrfs/ctree.h:1003:17: error: field 'super_kobj' has incomplete type
      fs/btrfs/ctree.h:1074:17: error: field 'root_kobj' has incomplete type
      make[2]: *** [fs/btrfs/ctree.o] Error 1
      make[1]: *** [fs/btrfs] Error 2
      make: *** [fs] Error 2
    
    caused by commit 57cc7215b708 ("headers: kobject.h redux")
    
    We need to include kobject.h here.
    
    Reported-by: Jeff Garzik <jeff@garzik.org>
    Fix-suggested-by: Li Zefan <lizf@cn.fujitsu.com>
    Signed-off-by: Stefan Schmidt <stefan@datenfreihafen.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/backing-dev.h>
 #include <linux/wait.h>
 #include <linux/slab.h>
+#include <linux/kobject.h>
 #include <asm/kmap_types.h>
 #include "extent_io.h"
 #include "extent_map.h"
