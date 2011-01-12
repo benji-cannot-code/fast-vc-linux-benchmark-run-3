5e7f940ee66645eb92ea900815d4e6420bee7ce9
Nicolas Ferre <nicolas.ferre@atmel.com>
2011-01-12 15:39:09 +0100

    dmaengine: at_hdmac: flags located in first descriptor
    Place flags on first descriptor of chain instead of last.
    This is the one used by atc_chain_complete() function while unmapping.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/dma/at_hdmac.c
+++ drivers/dma/at_hdmac.c
@@ -604,7 +604,7 @@ atc_prep_dma_memcpy(struct dma_chan *chan, dma_addr_t dest, dma_addr_t src,
 	/* set end-of-link to the last link descriptor of list*/
 	set_desc_eol(desc);
 
-	desc->txd.flags = flags; /* client is in control of this ack */
+	first->txd.flags = flags; /* client is in control of this ack */
 
 	return &first->txd;
 
@@ -749,8 +749,8 @@ atc_prep_slave_sg(struct dma_chan *chan, struct scatterlist *sgl,
 	first->txd.cookie = -EBUSY;
 	first->len = total_len;
 
-	/* last link descriptor of list is responsible of flags */
-	prev->txd.flags = flags; /* client is in control of this ack */
+	/* first link descriptor of list is responsible of flags */
+	first->txd.flags = flags; /* client is in control of this ack */
 
 	return &first->txd;
 
