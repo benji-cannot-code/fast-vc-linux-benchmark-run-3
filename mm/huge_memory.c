8df218b99dd5793685e1907dcb11878395fbc86b
Andrea Arcangeli <aarcange@redhat.com>
2011-01-13 15:47:05 -0800

    thp: enable direct defrag
    With memory compaction in, and lumpy-reclaim disabled, it seems safe
    enough to defrag memory during the (synchronous) transparent hugepage page
    faults (TRANSPARENT_HUGEPAGE_DEFRAG_FLAG) and not only during khugepaged
    (async) hugepage allocations that was already enabled even before memory
    compaction was in (TRANSPARENT_HUGEPAGE_DEFRAG_KHUGEPAGED_FLAG).
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Acked-by: Mel Gorman <mel@csn.ul.ie>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 unsigned long transparent_hugepage_flags __read_mostly =
 	(1<<TRANSPARENT_HUGEPAGE_FLAG)|
+	(1<<TRANSPARENT_HUGEPAGE_DEFRAG_FLAG)|
 	(1<<TRANSPARENT_HUGEPAGE_DEFRAG_KHUGEPAGED_FLAG);
 
 /* default scan 8*512 pte (or vmas) every 30 second */
