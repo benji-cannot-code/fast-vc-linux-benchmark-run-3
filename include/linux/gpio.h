201061512d347913723af9eeac61d627c93215ce
Wolfram Sang <w.sang@pengutronix.de>
2011-01-12 17:00:23 -0800

    gpiolib: annotate gpio-intialization with __must_check
    Because GPIOs can have crucial functions especially in embedded systems,
    we are better safe than sorry regarding their configuration.  For
    gpio_request, the documentation is simply enforced: <quote>"The return
    value of gpio_request() must be checked."</quote> For gpio_direction_* and
    gpio_request_*, we now act accordingly.
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Cc: David Brownell <dbrownell@users.sourceforge.net>
    Cc: Greg KH <gregkh@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/gpio.h
+++ include/linux/gpio.h
@@ -30,7 +30,7 @@ static inline int gpio_is_valid(int number)
 	return 0;
 }
 
-static inline int gpio_request(unsigned gpio, const char *label)
+static inline int __must_check gpio_request(unsigned gpio, const char *label)
 {
 	return -ENOSYS;
 }
@@ -43,12 +43,12 @@ static inline void gpio_free(unsigned gpio)
 	WARN_ON(1);
 }
 
-static inline int gpio_direction_input(unsigned gpio)
+static inline int __must_check gpio_direction_input(unsigned gpio)
 {
 	return -ENOSYS;
 }
 
-static inline int gpio_direction_output(unsigned gpio, int value)
+static inline int __must_check gpio_direction_output(unsigned gpio, int value)
 {
 	return -ENOSYS;
 }
