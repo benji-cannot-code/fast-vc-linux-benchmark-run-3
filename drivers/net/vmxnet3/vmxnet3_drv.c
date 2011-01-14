e5f21e8afb6d44afb525fdfcfdef40048fe5c6b8
Shreyas Bhatewara <sbhatewara@vmware.com>
2011-01-14 14:59:52 +0000

    vmxnet3: Disable napi in suspend, reenable in resume.
    There is a small possibility of a race where the suspend routine gets
    called, while a napi callback is still pending and when that comes up,
    it enables interrupts which just got disabled in the suspend routine.
    This change adds napi disable call in suspend and enable in resume to
    avoid race.
    
    Signed-off-by: Shreyas N Bhatewara <sbhatewara@vmware.com>
    Acked-by: Dmitry Torokhov <dtor@vmware.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vmxnet3/vmxnet3_drv.c
+++ drivers/net/vmxnet3/vmxnet3_drv.c
@@ -3102,6 +3102,9 @@ vmxnet3_suspend(struct device *device)
 	if (!netif_running(netdev))
 		return 0;
 
+	for (i = 0; i < adapter->num_rx_queues; i++)
+		napi_disable(&adapter->rx_queue[i].napi);
+
 	vmxnet3_disable_all_intrs(adapter);
 	vmxnet3_free_irqs(adapter);
 	vmxnet3_free_intr_resources(adapter);
@@ -3193,7 +3196,7 @@ skip_arp:
 static int
 vmxnet3_resume(struct device *device)
 {
-	int err;
+	int err, i = 0;
 	struct pci_dev *pdev = to_pci_dev(device);
 	struct net_device *netdev = pci_get_drvdata(pdev);
 	struct vmxnet3_adapter *adapter = netdev_priv(netdev);
@@ -3225,6 +3228,8 @@ vmxnet3_resume(struct device *device)
 			       VMXNET3_CMD_UPDATE_PMCFG);
 	vmxnet3_alloc_intr_resources(adapter);
 	vmxnet3_request_irqs(adapter);
+	for (i = 0; i < adapter->num_rx_queues; i++)
+		napi_enable(&adapter->rx_queue[i].napi);
 	vmxnet3_enable_all_intrs(adapter);
 
 	return 0;
