ab49b9d82999d6c0a924d26847e5cb58906bd769
Jianbin Kang <kjbmail@gmail.com>
2011-01-14 20:07:05 +0800

    kbuild: add GNU GLOBAL tags generation
    GNU GLOBAL (http://www.gnu.org/software/global/) is a source code tagging system
    It is really cheap to support it in kbuild system.
    
    Signed-off-by: Jianbin Kang <kjbmail@gmail.com>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/tags.sh
+++ scripts/tags.sh
@@ -115,6 +115,11 @@ docscope()
 	cscope -b -f cscope.out
 }
 
+dogtags()
+{
+	all_sources | gtags -f -
+}
+
 exuberant()
 {
 	all_sources | xargs $1 -a                               \
@@ -184,6 +189,10 @@ fi
 case "$1" in
 	"cscope")
 		docscope
+		;;
+
+	"gtags")
+		dogtags
 		;;
 
 	"tags")
