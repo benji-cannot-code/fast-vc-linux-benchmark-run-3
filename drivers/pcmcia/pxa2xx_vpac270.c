8e8f91105cc9abc0598126f6037b58a9c1955ee7
Marek Vasut <marek.vasut@gmail.com>
2011-01-15 19:22:19 +0100

    ARM: pxa: Update copyright notices for Palm devices
    Signed-off-by: Marek Vasut <marek.vasut@gmail.com>

--- drivers/pcmcia/pxa2xx_vpac270.c
+++ drivers/pcmcia/pxa2xx_vpac270.c
@@ -4,8 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Driver for Voipac PXA270 PCMCIA and CF sockets
  *
- * Copyright (C) 2010
- * Marek Vasut <marek.vasut@gmail.com>
+ * Copyright (C) 2010-2011 Marek Vasut <marek.vasut@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
