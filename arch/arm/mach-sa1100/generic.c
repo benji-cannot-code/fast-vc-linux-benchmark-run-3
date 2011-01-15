9b7293232a6eb34f4f03bb23b09f7ce461c4d559
Russell King <rmk+kernel@arm.linux.org.uk>
2011-01-15 00:14:25 +0000

    ARM: Fix build regression on SA11x0, PXA, and H720x targets
    Build errors similar this appeared in todays kautobuild for the above
    targets:
    
    In file included from arch/arm/include/asm/pgtable.h:461,
                     from arch/arm/mach-pxa/generic.c:26:
    include/asm-generic/pgtable.h: In function 'ptep_test_and_clear_young':
    include/asm-generic/pgtable.h:29: error: dereferencing pointer to incomplete type
    
    None of the .c files including asm/pgtable.h with this error is using
    this header, so simply remove the include.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-sa1100/generic.c
+++ arch/arm/mach-sa1100/generic.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/div64.h>
 #include <mach/hardware.h>
 #include <asm/system.h>
-#include <asm/pgtable.h>
 #include <asm/mach/map.h>
 #include <asm/mach/flash.h>
 #include <asm/irq.h>
