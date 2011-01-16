8d459c2a0403c16992c30dc7db3951c62bd49299
Jesper Juhl <jj@chaosbits.net>
2011-01-16 20:37:52 +0100

    ACPICA: Fix memory leak in acpi_ev_asynch_execute_gpe_method().
    We will leak the memory allocated to 'local_gpe_event_info' if
    'acpi_ut_acquire_mutex()' fails or if 'acpi_ev_valid_gpe_event()' fails in
    drivers/acpi/acpica/evgpe.c::acpi_ev_asynch_execute_gpe_method().
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Reviewed-by: Rafael Wysocki <rjw@sisk.pl>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/acpica/evgpe.c
+++ drivers/acpi/acpica/evgpe.c
@@ -472,6 +472,7 @@ static void ACPI_SYSTEM_XFACE acpi_ev_asynch_execute_gpe_method(void *context)
 
 	status = acpi_ut_acquire_mutex(ACPI_MTX_EVENTS);
 	if (ACPI_FAILURE(status)) {
+		ACPI_FREE(local_gpe_event_info);
 		return_VOID;
 	}
 
@@ -479,6 +480,7 @@ static void ACPI_SYSTEM_XFACE acpi_ev_asynch_execute_gpe_method(void *context)
 
 	if (!acpi_ev_valid_gpe_event(gpe_event_info)) {
 		status = acpi_ut_release_mutex(ACPI_MTX_EVENTS);
+		ACPI_FREE(local_gpe_event_info);
 		return_VOID;
 	}
 
