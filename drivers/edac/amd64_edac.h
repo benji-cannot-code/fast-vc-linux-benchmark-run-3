6383089c6acfcb0aac871c1f06a22444653d7730
Borislav Petkov <borislav.petkov@amd.com>
2011-01-17 17:52:57 +0100

    amd64_edac: Sanitize ->read_dram_ctl_register
    This function is relevant for F10h and higher, and it has only one
    callsite so drop its function pointer from the low_ops struct.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>

--- drivers/edac/amd64_edac.h
+++ drivers/edac/amd64_edac.h
@@ -237,7 +237,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define dct_ganging_enabled(pvt)	((boot_cpu_data.x86 == 0x10) && ((pvt)->dct_sel_lo & BIT(4)))
 
 #define dct_data_intlv_enabled(pvt)	((pvt)->dct_sel_lo & BIT(5))
-#define dct_dram_enabled(pvt)		((pvt)->dct_sel_lo & BIT(8))
 #define dct_memory_cleared(pvt)		((pvt)->dct_sel_lo & BIT(10))
 
 #define SWAP_INTLV_REG			0x10c
@@ -445,7 +444,6 @@ extern struct mcidev_sysfs_attribute amd64_dbg_attrs[NUM_DBG_ATTRS],
  */
 struct low_ops {
 	int (*early_channel_count)	(struct amd64_pvt *pvt);
-	void (*read_dram_ctl_register)	(struct amd64_pvt *pvt);
 	void (*map_sysaddr_to_csrow)	(struct mem_ctl_info *mci, u64 sys_addr,
 					 u16 syndrome);
 	int (*dbam_to_cs)		(struct amd64_pvt *pvt, int cs_mode);
