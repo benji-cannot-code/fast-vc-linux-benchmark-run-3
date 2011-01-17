693b8f7b244b93ee927e5e8dae1514e306494a99
Al Viro <viro@zeniv.linux.org.uk>
2011-01-17 00:47:38 -0500

    autofs4: keep symlink body in inode->i_private
    gets rid of all ->free()/->u.symlink machinery in autofs; we simply
    keep symlink bodies in inode->i_private and free them in ->evict_inode().
    
    Acked-by: Ian Kent <raven@themaw.net>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/autofs4/root.c
+++ fs/autofs4/root.c
@@ -562,6 +562,7 @@ static int autofs4_dir_symlink(struct inode *dir,
 			kfree(ino);
 		return -ENOMEM;
 	}
+	inode->i_private = cp;
 	d_add(dentry, inode);
 
 	dentry->d_fsdata = ino;
@@ -571,7 +572,6 @@ static int autofs4_dir_symlink(struct inode *dir,
 	if (p_ino && dentry->d_parent != dentry)
 		atomic_inc(&p_ino->count);
 
-	ino->u.symlink = cp;
 	dir->i_mtime = CURRENT_TIME;
 
 	return 0;
