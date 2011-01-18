855a0edeb0b08ebbcdf0995eb389f69e3750b10b
Allan Stephens <Allan.Stephens@windriver.com>
2011-01-18 13:58:27 -0500

    tipc: Eliminate unnecessary locking when starting topology service
    Modifies the initialization code for TIPC's topology service to
    avoid taking the spinlock protecting the subscriber list, since
    there is no need to do this.
    
    Signed-off-by: Allan Stephens <Allan.Stephens@windriver.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- net/tipc/subscr.c
+++ net/tipc/subscr.c
@@ -543,7 +543,6 @@ int tipc_subscr_start(void)
 	spin_lock_init(&topsrv.lock);
 	INIT_LIST_HEAD(&topsrv.subscriber_list);
 
-	spin_lock_bh(&topsrv.lock);
 	res = tipc_createport(NULL,
 			      TIPC_CRITICAL_IMPORTANCE,
 			      NULL,
@@ -564,12 +563,10 @@ int tipc_subscr_start(void)
 		goto failed;
 	}
 
-	spin_unlock_bh(&topsrv.lock);
 	return 0;
 
 failed:
 	err("Failed to create subscription service\n");
-	spin_unlock_bh(&topsrv.lock);
 	return res;
 }
 
