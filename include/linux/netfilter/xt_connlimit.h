1f706596898f5d6cc1639ef99ea185f9ac0f27a1
Jan Engelhardt <jengelh@medozas.de>
2011-01-18 17:32:40 +0100

    netfilter: xtables: connlimit revision 1
    This adds destination address-based selection. The old "inverse"
    member is overloaded (memory-wise) with a new "flags" variable,
    similar to how J.Park did it with xt_string rev 1. Since revision 0
    userspace only sets flag 0x1, no great changes are made to explicitly
    test for different revisions.
    
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>

--- include/linux/netfilter/xt_connlimit.h
+++ include/linux/netfilter/xt_connlimit.h
@@ -4,6 +4,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct xt_connlimit_data;
 
+enum {
+	XT_CONNLIMIT_INVERT = 1 << 0,
+	XT_CONNLIMIT_DADDR  = 1 << 1,
+};
+
 struct xt_connlimit_info {
 	union {
 		union nf_inet_addr mask;
@@ -15,6 +20,13 @@ struct xt_connlimit_info {
 #endif
 	};
 	unsigned int limit, inverse;
+	union {
+		/* revision 0 */
+		unsigned int inverse;
+
+		/* revision 1 */
+		__u32 flags;
+	};
 
 	/* Used internally by the kernel */
 	struct xt_connlimit_data *data __attribute__((aligned(8)));
