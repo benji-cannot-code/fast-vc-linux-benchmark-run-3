061f76263cca4ce2787456ce2c52114cfb051590
Mimi Zohar <zohar@linux.vnet.ibm.com>
2011-01-18 09:07:12 -0500

    encrypted-keys: rename encrypted_defined files to encrypted
    Rename encrypted_defined.c and encrypted_defined.h files to encrypted.c and
    encrypted.h, respectively. Based on request from David Howells.
    
    Signed-off-by: Mimi Zohar <zohar@us.ibm.com>
    Acked-by: David Howells <dhowells@redhat.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/keys/encrypted.c
+++ security/keys/encrypted.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/sha.h>
 #include <crypto/aes.h>
 
-#include "encrypted_defined.h"
+#include "encrypted.h"
 
 static const char KEY_TRUSTED_PREFIX[] = "trusted:";
 static const char KEY_USER_PREFIX[] = "user:";
@@ -889,6 +889,7 @@ static int __init init_encrypted(void)
 out:
 	encrypted_shash_release();
 	return ret;
+
 }
 
 static void __exit cleanup_encrypted(void)
