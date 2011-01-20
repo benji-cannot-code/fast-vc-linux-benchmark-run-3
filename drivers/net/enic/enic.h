77eeb72fd0d0989662c1e3e5cb996ee0d98f5022
Roopa Prabhu <roprabhu@cisco.com>
2011-01-20 14:35:54 +0000

    enic: Bug Fix: Dont reset ENIC_SET_APPLIED flag on port profile disassociate
    enic_get_vf_port returns port profile operation status only if ENIC_SET_APPLIED
    flag is set. A recent rework of enic_set_port_profile added code to reset this
    flag on disassociate. As a result of which a client calling enic_get_vf_port
    to get the status of port profile disassociate will always get a return value
    of ENODATA. This patch renames ENIC_SET_APPLIED to more appropriate
    ENIC_PORT_REQUEST_APPLIED and reverts back the recent change so that the
    flag is set both at associate and disassociate of a port profile.
    
    Signed-off-by: Roopa Prabhu <roprabhu@cisco.com>
    Signed-off-by: David Wang <dwang2@cisco.com>
    Signed-off-by: Christian Benvenuti <benve@cisco.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/enic/enic.h
+++ drivers/net/enic/enic.h
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME		"enic"
 #define DRV_DESCRIPTION		"Cisco VIC Ethernet NIC Driver"
-#define DRV_VERSION		"1.4.1.10"
-#define DRV_COPYRIGHT		"Copyright 2008-2010 Cisco Systems, Inc"
+#define DRV_VERSION		"2.1.1.2"
+#define DRV_COPYRIGHT		"Copyright 2008-2011 Cisco Systems, Inc"
 
 #define ENIC_BARS_MAX		6
 
@@ -50,7 +50,7 @@ struct enic_msix_entry {
 	void *devid;
 };
 
-#define ENIC_SET_APPLIED		(1 << 0)
+#define ENIC_PORT_REQUEST_APPLIED	(1 << 0)
 #define ENIC_SET_REQUEST		(1 << 1)
 #define ENIC_SET_NAME			(1 << 2)
 #define ENIC_SET_INSTANCE		(1 << 3)
