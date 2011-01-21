9410374207c23a6a698b17b97fa375e499ff5ab8
Arend van Spriel <arend@broadcom.com>
2011-01-21 10:54:49 +0100

    staging: brcm80211: cleanup on the brcm80211 include directory
    moved several files to specific source directory as these do not
    need to be shared between drivers. Also removed some unused include
    files from the include directory.
    
    Reviewed-by: Brett Rudley <brudley@broadcom.com>
    Reviewed-by: Henry Ptasinski <henryp@broadcom.com>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/sys/wlc_event.c
+++ drivers/staging/brcm80211/brcmsmac/sys/wlc_event.c
@@ -34,9 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <d11.h>
 #include <wlc_rate.h>
 #include <wlc_mac80211.h>
-#ifdef MSGTRACE
-#include <msgtrace.h>
-#endif
 #include <wl_dbg.h>
 
 /* Local prototypes */
