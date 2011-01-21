e5a8e975d5801b173813be102e63d593956e9158
Mian Yousaf Kaukab <mian-yousaf.kaukab@linaro.org>
2011-01-21 18:24:19 +0100

    mach-ux500: add MUSB to db5500 devices
    - DMA tx and rx maps for usb channels are set to be configured at
      runtime
    - MUSB is enabled with soc specific base address, irq and dma
      configurations
    
    Signed-off-by: Mian Yousaf Kaukab <mian-yousaf.kaukab@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/devices-db5500.h
+++ arch/arm/mach-ux500/devices-db5500.h
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define db5500_add_rtc() \
 	dbx500_add_rtc(U5500_RTC_BASE, IRQ_DB5500_RTC);
 
+#define db5500_add_usb(rx_cfg, tx_cfg) \
+	ux500_add_usb(U5500_USBOTG_BASE, IRQ_DB5500_USBOTG, rx_cfg, tx_cfg)
+
 #define db5500_add_sdi0(pdata) \
 	dbx500_add_sdi("sdi0", U5500_SDI0_BASE, IRQ_DB5500_SDMMC0, pdata)
 #define db5500_add_sdi1(pdata) \
