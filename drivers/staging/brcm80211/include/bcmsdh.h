8cd4a061520f6d4aa5651ffd11895d46d4cfbaad
Arend van Spriel <arend@broadcom.com>
2011-01-21 10:54:48 +0100

    staging: brcm80211: moved code around for cleanup
    Restructured code to have more consistent directory tree for the
    two drivers.
    
    Reviewed-by: Brett Rudley <brudley@broadcom.com>
    Reviewed-by: Henry Ptasinski <henryp@broadcom.com>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/include/bcmsdh.h
+++ drivers/staging/brcm80211/include/bcmsdh.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	_bcmsdh_h_
 #define	_bcmsdh_h_
 
+#include <linux/skbuff.h>
 #define BCMSDH_ERROR_VAL	0x0001	/* Error */
 #define BCMSDH_INFO_VAL		0x0002	/* Info */
 extern const uint bcmsdh_msglevel;
--- drivers/staging/brcm80211/util/aiutils.c
+++ drivers/staging/brcm80211/util/aiutils.c
@@ -19,9 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/string.h>
 #include <bcmdefs.h>
-#ifdef BRCM_FULLMAC
-#include <linux/netdevice.h>
-#endif
 #include <osl.h>
 #include <linux/module.h>
 #include <linux/pci.h>
