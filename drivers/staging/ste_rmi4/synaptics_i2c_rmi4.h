4b3c63895b49851173a0d68ad74d98072e678359
Naveen Kumar Gaddipati <naveen.gaddipati@stericsson.com>
2011-01-21 15:50:16 +0530

    staging: synaptics: Update with the kernel object name of touch device
    Update with the kernel object name of touch device for getting
    the regulator of the synaptics rmi4 touch device.
    
    Signed-off-by: Naveen Kumar Gaddipati <naveen.gaddipati@stericsson.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ste_rmi4/synaptics_i2c_rmi4.h
+++ drivers/staging/ste_rmi4/synaptics_i2c_rmi4.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This structure gives platform data for rmi4.
  */
 struct synaptics_rmi4_platform_data {
-	const char *name;
 	int irq_number;
 	int irq_type;
 	bool x_flip;
