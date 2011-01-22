c720118c9df83ca1e176840df4dfcd91392a7095
Tomas Winkler <tomas.winkler@intel.com>
2011-01-23 01:13:54 +0200

    staging: easycap: remove redunant headers
    place all globals to easycap.h, which is included
    by all c-files
    easycap_standard: fix declaration vs. definiton conflict
    
    Cc: Mike Thomas <rmthomas@sciolus.org>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/easycap/easycap_main.c
+++ drivers/staging/easycap/easycap_main.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*****************************************************************************/
 
 #include "easycap.h"
-#include "easycap_main.h"
 
 int easycap_debug;
 static int easycap_bars = 1;
