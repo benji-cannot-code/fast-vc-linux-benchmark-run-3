8abedd22f2aaee16c6c348a20f77405e4bf202f4
Tejun Heo <tj@kernel.org>
2011-01-23 14:37:29 +0100

    x86: Make default_send_IPI_mask_sequence/allbutself_logical() 32bit only
    Both functions are used only in 32bit.  Put them inside
    CONFIG_X86_32. This is to prepare for logical apicid handling
    update.
    
    - Cyrill Gorcunov spotted that I forgot to move declarations in
    ipi.h   under CONFIG_X86_32.  Fixed.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Pekka Enberg <penberg@kernel.org>
    Reviewed-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Acked-by: Yinghai Lu <yinghai@kernel.org>
    Cc: eric.dumazet@gmail.com
    Cc: brgerst@gmail.com
    Cc: shaohui.zheng@intel.com
    Cc: rientjes@google.com
    LKML-Reference: <1295789862-25482-4-git-send-email-tj@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic/ipi.c
+++ arch/x86/kernel/apic/ipi.c
@@ -57,6 +57,8 @@ void default_send_IPI_mask_allbutself_phys(const struct cpumask *mask,
 	local_irq_restore(flags);
 }
 
+#ifdef CONFIG_X86_32
+
 void default_send_IPI_mask_sequence_logical(const struct cpumask *mask,
 						 int vector)
 {
@@ -96,8 +98,6 @@ void default_send_IPI_mask_allbutself_logical(const struct cpumask *mask,
 		}
 	local_irq_restore(flags);
 }
-
-#ifdef CONFIG_X86_32
 
 /*
  * This is only used on smaller machines.
