53ff8914f7de41c733576fb4c40ab085520624ef
Tejun Heo <tj@kernel.org>
2011-01-23 14:37:28 +0100

    x86: Drop x86_32 MAX_APICID
    Commit 56d91f13 (x86, acpi: Add MAX_LOCAL_APIC for 32bit) added
    MAX_LOCAL_APIC for x86_32 but didn't replace MAX_APICID users
    with it. Convert MAX_APICID users to MAX_LOCAL_APIC and drop
    MAX_APICID.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Pekka Enberg <penberg@kernel.org>
    Acked-by: Yinghai Lu <yinghai@kernel.org>
    Cc: eric.dumazet@gmail.com
    Cc: yinghai@kernel.org
    Cc: brgerst@gmail.com
    Cc: gorcunov@gmail.com
    Cc: shaohui.zheng@intel.com
    Cc: rientjes@google.com
    LKML-Reference: <1295789862-25482-3-git-send-email-tj@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -73,7 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/i8259.h>
 
 #ifdef CONFIG_X86_32
-u8 apicid_2_node[MAX_APICID];
+u8 apicid_2_node[MAX_LOCAL_APIC];
 #endif
 
 /* State of each CPU */
