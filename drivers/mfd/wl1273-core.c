ae190455c5cdbeb8e462cc19a13eca4e0045201d
Wolfram Sang <w.sang@pengutronix.de>
2011-01-24 11:44:27 +0100

    mfd: Remove obsolete cleanup for wl1273 clientdata
    A few new i2c-drivers came into the kernel which clear the clientdata-pointer
    on exit or error. This is obsolete meanwhile, the core will do it.
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/wl1273-core.c
+++ drivers/mfd/wl1273-core.c
@@ -39,7 +39,6 @@ static int wl1273_core_remove(struct i2c_client *client)
 	dev_dbg(&client->dev, "%s\n", __func__);
 
 	mfd_remove_devices(&client->dev);
-	i2c_set_clientdata(client, NULL);
 	kfree(core);
 
 	return 0;
@@ -105,7 +104,6 @@ static int __devinit wl1273_core_probe(struct i2c_client *client,
 	return 0;
 
 err:
-	i2c_set_clientdata(client, NULL);
 	pdata->free_resources();
 	kfree(core);
 
