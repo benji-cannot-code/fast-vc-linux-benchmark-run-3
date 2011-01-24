31cecaf051e428ea5df2ae77043eea2eca97290e
Tsutomu Itoh <t-itoh@jp.fujitsu.com>
2011-01-24 00:57:10 +0000

    btrfs: check return value of btrfs_start_ioctl_transaction() properly
    btrfs_start_ioctl_transaction() returns ERR_PTR(), not NULL.
    So, it is necessary to use IS_ERR() to check the return value.
    
    Signed-off-by: Tsutomu Itoh <t-itoh@jp.fujitsu.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -2086,7 +2086,7 @@ static long btrfs_ioctl_trans_start(struct file *file)
 
 	ret = -ENOMEM;
 	trans = btrfs_start_ioctl_transaction(root, 0);
-	if (!trans)
+	if (IS_ERR(trans))
 		goto out_drop;
 
 	file->private_data = trans;
