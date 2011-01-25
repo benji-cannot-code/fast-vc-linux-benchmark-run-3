f35382b000388af9015962cb1ca331381ecb4647
Dan Carpenter <error27@gmail.com>
2011-01-25 14:15:11 +0000

    serial: mrst_max3110: make buffer larger
    This is used to store the spi_device ->modalias so they have to be the same
    size.  SPI_NAME_SIZE is 32.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Cc: stable@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/tty/serial/mrst_max3110.c
+++ drivers/tty/serial/mrst_max3110.c
@@ -52,7 +52,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct uart_max3110 {
 	struct uart_port port;
 	struct spi_device *spi;
-	char name[24];
+	char name[SPI_NAME_SIZE];
 
 	wait_queue_head_t wq;
 	struct task_struct *main_thread;
