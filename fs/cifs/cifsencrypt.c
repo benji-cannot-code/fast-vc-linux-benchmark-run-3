e57027fd8e788ed544fef888bb30637214373367
Steve French <sfrench@us.ibm.com>
2011-01-25 19:28:43 +0000

    [CIFS] Replace cifs md5 hashing functions with kernel crypto APIs
    Replace remaining use of md5 hash functions local to cifs module
    with kernel crypto APIs.
    Remove header and source file containing those local functions.
    
    Signed-off-by: Shirish Pargaonkar <shirishpargaonkar@gmail.com>
    Reviewed-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifsencrypt.c
+++ fs/cifs/cifsencrypt.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cifspdu.h"
 #include "cifsglob.h"
 #include "cifs_debug.h"
-#include "md5.h"
 #include "cifs_unicode.h"
 #include "cifsproto.h"
 #include "ntlmssp.h"
--- fs/cifs/smbencrypt.c
+++ fs/cifs/smbencrypt.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cifs_unicode.h"
 #include "cifspdu.h"
 #include "cifsglob.h"
-#include "md5.h"
 #include "cifs_debug.h"
 #include "cifsencrypt.h"
 
