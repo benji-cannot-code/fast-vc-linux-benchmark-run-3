05c3f96851ba0525db7578117c0f061594be553b
Arnd Bergmann <arnd@arndb.de>
2011-01-25 23:17:27 +0100

    appletalk: move to staging
    For all I know, Appletalk is dead, the only reasonable
    use right now would be nostalgia, and that can be served
    well enough by old kernels. The code is largely not
    in a bad shape, but it still uses the big kernel lock,
    and nobody seems motivated to change that.
    
    FWIW, the last release of MacOS that supported Appletalk
    was MacOS X 10.5, made in 2007, and it has been abandoned
    by Apple with 10.6. Using TCP/IP instead of Appletalk has
    been supported since MacOS 7.6, which was released in
    1997 and is able to run on most of the legacy hardware.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
    Cc: netdev@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/appletalk/aarp.c
+++ drivers/staging/appletalk/aarp.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <net/datalink.h>
 #include <net/psnap.h>
-#include <linux/atalk.h>
+#include "atalk.h"
 #include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
--- drivers/staging/appletalk/ipddp.c
+++ drivers/staging/appletalk/ipddp.c
@@ -30,12 +30,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 #include <linux/ip.h>
-#include <linux/atalk.h>
 #include <linux/if_arp.h>
 #include <linux/slab.h>
 #include <net/route.h>
 #include <asm/uaccess.h>
 
+#include "atalk.h"
 #include "ipddp.h"		/* Our stuff */
 
 static const char version[] = KERN_INFO "ipddp.c:v0.01 8/28/97 Bradford W. Johnson <johns393@maroon.tc.umn.edu>\n";
