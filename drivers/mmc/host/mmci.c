2e3d8dabeeb1b8867b157ea9a90d1e8a78e9d2db
Russell King <rmk+kernel@arm.linux.org.uk>
2011-01-27 10:56:52 +0000

    ARM: mmci: no need for separate host->data_xfered
    We don't need to store the number of bytes transferred in our host
    structure - we can store this directly in data->bytes_xfered.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/mmc/host/mmci.c
+++ drivers/mmc/host/mmci.c
@@ -143,9 +143,6 @@ mmci_request_end(struct mmci_host *host, struct mmc_request *mrq)
 	host->mrq = NULL;
 	host->cmd = NULL;
 
-	if (mrq->data)
-		mrq->data->bytes_xfered = host->data_xfered;
-
 	/*
 	 * Need to drop the host lock here; mmc_request_done may call
 	 * back into the driver...
@@ -203,7 +200,7 @@ static void mmci_start_data(struct mmci_host *host, struct mmc_data *data)
 
 	host->data = data;
 	host->size = data->blksz * data->blocks;
-	host->data_xfered = 0;
+	data->bytes_xfered = 0;
 
 	mmci_init_sg(host, data);
 
@@ -312,7 +309,7 @@ mmci_data_irq(struct mmci_host *host, struct mmc_data *data,
 				success = 0;
 			data->error = -EIO;
 		}
-		host->data_xfered = round_down(success, data->blksz);
+		data->bytes_xfered = round_down(success, data->blksz);
 	}
 
 	if (status & MCI_DATABLOCKEND)
@@ -323,7 +320,7 @@ mmci_data_irq(struct mmci_host *host, struct mmc_data *data,
 
 		if (!data->error)
 			/* The error clause is handled above, success! */
-			host->data_xfered += data->blksz * data->blocks;
+			data->bytes_xfered = data->blksz * data->blocks;
 
 		if (!data->stop) {
 			mmci_request_end(host, data->mrq);
