479bae85c58366becfd9a43b5924bd66c7dd778b
Torben Hohn <torbenh@gmx.de>
2011-01-27 15:59:51 +0100

    h8300: Switch do_timer() to xtime_update()
    xtime_update() takes the xtime_lock itself.
    
    Signed-off-by: Torben Hohn <torbenh@gmx.de>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: johnstul@us.ibm.com
    Cc: hch@infradead.org
    Cc: yong.zhang0@gmail.com
    LKML-Reference: <20110127145951.23248.92727.stgit@localhost>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/h8300/kernel/timer/timer8.c
+++ arch/h8300/kernel/timer/timer8.c
@@ -62,7 +62,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * timer_interrupt() needs to keep up the real-time clock,
- * as well as call the "do_timer()" routine every clocktick
+ * as well as call the "xtime_update()" routine every clocktick
  */
 
 static irqreturn_t timer_interrupt(int irq, void *dev_id)
