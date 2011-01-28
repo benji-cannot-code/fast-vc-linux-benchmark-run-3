2710048258fded14e010b069c4412706c1ad13f1
Joe Perches <joe@perches.com>
2011-01-27 20:04:32 -0800

    staging: ath6kl: Convert tspecCompliance from A_BOOL to int
    Make the use in wmi_verify_tspec_params match the declaration
    of the variable.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Acked-by: Vipin Mehta <vipin.mehta@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/wmi/wmi.c
+++ drivers/staging/ath6kl/wmi/wmi.c
@@ -4651,7 +4651,7 @@ wmi_get_power_mode_cmd(struct wmi_t *wmip)
 }
 
 int
-wmi_verify_tspec_params(WMI_CREATE_PSTREAM_CMD *pCmd, A_BOOL tspecCompliance)
+wmi_verify_tspec_params(WMI_CREATE_PSTREAM_CMD *pCmd, int tspecCompliance)
 {
     int ret = A_OK;
 
