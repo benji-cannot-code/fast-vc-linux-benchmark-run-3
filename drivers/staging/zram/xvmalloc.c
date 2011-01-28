ab9d0b012e19a68953f0c99114c37c072cdc9153
Robert Jennings <rcj@linux.vnet.ibm.com>
2011-01-28 08:59:26 -0600

    zram/xvmalloc: create CONFIG_ZRAM_DEBUG for debug code
    Add a debug config flag to enable debug printk output and future
    debug code.
    
    Signed-off-by: Robert Jennings <rcj@linux.vnet.ibm.com>
    Reviewed-by: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/zram/xvmalloc.c
+++ drivers/staging/zram/xvmalloc.c
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Released under the terms of GNU General Public License Version 2.0
  */
 
+#ifdef CONFIG_ZRAM_DEBUG
+#define DEBUG
+#endif
+
 #include <linux/bitops.h>
 #include <linux/errno.h>
 #include <linux/highmem.h>
