e13f5a4888dba57b01d6c1fc1271ddb3b022e733
Joe Perches <joe@perches.com>
2011-01-27 20:04:27 -0800

    staging: ath6kl: Convert 0 to FALSE
    Convert a set of an A_BOOL from 0 to FALSE.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Acked-by: Vipin Mehta <vipin.mehta@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/os/linux/ar6000_android.c
+++ drivers/staging/ath6kl/os/linux/ar6000_android.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/earlysuspend.h>
 #endif
 
-A_BOOL enable_mmc_host_detect_change = 0;
+A_BOOL enable_mmc_host_detect_change = FALSE;
 static void ar6000_enable_mmchost_detect_change(int enable);
 
 
