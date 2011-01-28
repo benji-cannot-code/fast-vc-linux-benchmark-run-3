af2ed2b29ca5419d9d1c77461fbc33149fec15ff
Sergei Shtylyov <sshtylyov@ru.mvista.com>
2011-01-28 21:58:54 +0300

    sata_dwc_460ex: fix return value of dma_dwc_xfer_setup()
    The caller expects this function to return the DMA channel number on success,
    while it returns 0...
    
    Signed-off-by: Sergei Shtylyov <sshtylyov@ru.mvista.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/sata_dwc_460ex.c
+++ drivers/ata/sata_dwc_460ex.c
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #undef	DRV_NAME
 #undef	DRV_VERSION
 #define DRV_NAME        "sata-dwc"
-#define DRV_VERSION     "1.1"
+#define DRV_VERSION     "1.2"
 
 /* SATA DMA driver Globals */
 #define DMA_NUM_CHANS		1
@@ -719,7 +719,7 @@ static int dma_dwc_xfer_setup(struct scatterlist *sg, int num_elems,
 	/* Program the CTL register with src enable / dst enable */
 	out_le32(&(host_pvt.sata_dma_regs->chan_regs[dma_ch].ctl.low),
 		 DMA_CTL_LLP_SRCEN | DMA_CTL_LLP_DSTEN);
-	return 0;
+	return dma_ch;
 }
 
 /*
