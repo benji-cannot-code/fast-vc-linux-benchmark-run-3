9ea485c6bb2cb3cc02f7625fa7947de0ac439a50
Felipe Balbi <balbi@ti.com>
2011-01-27 16:39:42 -0800

    arm: omap1: fix a bunch of section mismatches
    Fix the following section mismatches:
    WARNING: arch/arm/mach-omap1/built-in.o(.data+0x491c): Section mismatch
    in reference from the variable fsample_config to the (unknown reference)
    .init.data:(unknown)
    The variable fsample_config references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    WARNING: arch/arm/mach-omap1/built-in.o(.data+0x8f0c): Section mismatch
    in reference from the variable ams_delta_config to the (unknown
    reference) .init.data:(unknown)
    The variable ams_delta_config references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    WARNING: arch/arm/mach-omap1/built-in.o(.data+0x93ac): Section mismatch
    in reference from the variable ams_delta_camera_device to the (unknown
    reference) .init.data:(unknown)
    The variable ams_delta_camera_device references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    WARNING: vmlinux.o(.data+0x5e94): Section mismatch in reference from the
    variable fsample_config to the (unknown reference) .init.data:(unknown)
    The variable fsample_config references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    WARNING: vmlinux.o(.data+0xa484): Section mismatch in reference from the
    variable ams_delta_config to the (unknown reference)
    .init.data:(unknown)
    The variable ams_delta_config references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    WARNING: vmlinux.o(.data+0xa924): Section mismatch in reference from the
    variable ams_delta_camera_device to the (unknown reference)
    .init.data:(unknown)
    The variable ams_delta_camera_device references
    the (unknown reference) __initdata (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console,
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/board-fsample.c
+++ arch/arm/mach-omap1/board-fsample.c
@@ -288,7 +288,7 @@ static struct platform_device *devices[] __initdata = {
 	&lcd_device,
 };
 
-static struct omap_lcd_config fsample_lcd_config __initdata = {
+static struct omap_lcd_config fsample_lcd_config = {
 	.ctrl_name	= "internal",
 };
 
