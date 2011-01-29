908e76761e00bff14c858fb46325d24a26b7914f
Yi Zou <yi.zou@intel.com>
2011-01-28 16:04:55 -0800

    [SCSI] libfcoe: add implementation to support fcoe transport
    Add the new fcoe_transport.c file that implements basic fcoe transport
    interface. Eventually, the sysfs entries to create/destroy/enable/disable
    an FCoE instance will be coming to the fcoe transport layer, who does a
    look-up to find the corresponding transport provide and pass the corresponding
    action over to the identified provider.
    
    The fcoe.ko will become the default fcoe transport provider that can support
    FCoE on any given netdev interfaces, as the Open-FCoE.org's default software
    FCoE HBA solution. Any vendor specific FCoE HBA driver that is built on top
    of Open-FCoE's kernel stack of libfc & libfcoe as well as the user land tool
    of fcoe-utils can easily plug-in and start running FCoE on their network
    interfaces. The fcoe.ko will be converted to act as the default provider if
    no vendor specific transport provider is found, as it is always added to the
    very end of the list of attached transports.
    
    Signed-off-by: Yi Zou <yi.zou@intel.com>
    Signed-off-by: Bhanu Prakash Gollapudi <bprakash@broadcom.com>
    Signed-off-by: Robert Love <robert.w.love@intel.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/fcoe/libfcoe.h
+++ drivers/scsi/fcoe/libfcoe.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern unsigned int libfcoe_debug_logging;
 #define LIBFCOE_LOGGING	    0x01 /* General logging, not categorized */
 #define LIBFCOE_FIP_LOGGING 0x02 /* FIP logging */
+#define LIBFCOE_TRANSPORT_LOGGING	0x04 /* FCoE transport logging */
 
 #define LIBFCOE_CHECK_LOGGING(LEVEL, CMD)		\
 do {							\
@@ -22,5 +23,10 @@ do {							\
 	LIBFCOE_CHECK_LOGGING(LIBFCOE_FIP_LOGGING,			\
 			      printk(KERN_INFO "host%d: fip: " fmt,	\
 				     (fip)->lp->host->host_no, ##args);)
+
+#define LIBFCOE_TRANSPORT_DBG(fmt, args...)				\
+	LIBFCOE_CHECK_LOGGING(LIBFCOE_TRANSPORT_LOGGING,		\
+			      printk(KERN_INFO "%s: " fmt,		\
+				     __func__, ##args);)
 
 #endif /* _FCOE_LIBFCOE_H_ */
