03b26d4347a927879ae36a690498398e116475d3
Stephen Warren <swarren@nvidia.com>
2011-01-31 15:37:15 -0700

    ARM: tegra: Add to struct harmony_audio_platform_data
    Add fields to describe all the audio-related GPIOs on Harmony, except for
    the codec's GPIO IRQ, which will be passed in its i2c_board_info.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- arch/arm/mach-tegra/include/mach/harmony_audio.h
+++ arch/arm/mach-tegra/include/mach/harmony_audio.h
@@ -17,4 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct harmony_audio_platform_data {
 	int gpio_spkr_en;
+	int gpio_hp_det;
+	int gpio_int_mic_en;
+	int gpio_ext_mic_en;
 };
