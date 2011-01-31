5588af9dc5977391b3e7be8bb46a2ac3aacff79e
Luciano Coelho <coelho@ti.com>
2011-01-31 09:41:52 +0200

    wl12xx: fix warning due to missing arg in ampdu_action
    Commit 0b01f030d38e00650e2db42da083d8647aad40a5 added a new argument
    to the ampdu_action operation.  The ampdu_action operation in the
    wl12xx driver currently doesn't have that argument and this generates
    a warning.  This happened during merging of the latest mac80211
    patches with the wl12xx BA patches.
    
      CC [M]  drivers/net/wireless/wl12xx/main.o
    drivers/net/wireless/wl12xx/main.c:3035: warning: initialization from incompatible pointer type
    
    The wl12xx driver doesn't need to do anything about the buf_size
    argument since the AMPDU TX is fully handled by the firmware.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl12xx/main.c
+++ drivers/net/wireless/wl12xx/main.c
@@ -2725,8 +2725,9 @@ out:
 }
 
 int wl1271_op_ampdu_action(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
-			    enum ieee80211_ampdu_mlme_action action,
-			    struct ieee80211_sta *sta, u16 tid, u16 *ssn)
+			   enum ieee80211_ampdu_mlme_action action,
+			   struct ieee80211_sta *sta, u16 tid, u16 *ssn,
+			   u8 buf_size)
 {
 	struct wl1271 *wl = hw->priv;
 	int ret;
