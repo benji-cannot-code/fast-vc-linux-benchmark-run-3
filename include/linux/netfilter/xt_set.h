ebe8bca5b814cc3e116e1b53d991976c4ae3c1f5
Patrick McHardy <kaber@trash.net>
2011-02-01 18:52:42 +0100

    netfilter: ipset: install ipset related header files
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- include/linux/netfilter/xt_set.h
+++ include/linux/netfilter/xt_set.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _XT_SET_H
 #define _XT_SET_H
 
+#include <linux/types.h>
 #include <linux/netfilter/ipset/ip_set.h>
 
 /* Revision 0 interface: backward compatible with netfilter/iptables */
