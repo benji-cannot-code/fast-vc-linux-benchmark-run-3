bfff6fc2ab747230bbd41cbc792693d990821f4f
Amit Shah <amit.shah@redhat.com>
2011-02-01 09:31:25 +0530

    virtio: console: Move file back to drivers/char/
    Commit 728674a7e466628df2aeec6d11a2ae1ef968fb67 moved virtio_console.c
    to drivers/tty/hvc/ under the perception of this being an hvc driver.
    It was such once, but these days it has generic communication
    capabilities as well, so move it to drivers/char/.
    
    In the future, the hvc part from this file can be split off and moved
    under drivers/tty/hvc/.
    
    Signed-off-by: Amit Shah <amit.shah@redhat.com>
    CC: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/char/virtio_console.c
+++ drivers/char/virtio_console.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/virtio_console.h>
 #include <linux/wait.h>
 #include <linux/workqueue.h>
-#include "hvc_console.h"
+#include "../tty/hvc/hvc_console.h"
 
 /*
  * This is a global struct for storing common data for all the devices
