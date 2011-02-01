f13c7b3dd8757188b3fb07c3cea90ee3ee563199
Rik van Riel <riel@redhat.com>
2011-02-01 09:52:41 -0500

    KVM: keep track of which task is running a KVM vcpu
    Keep track of which task is running a KVM vcpu.  This helps us
    figure out later what task to wake up if we want to boost a
    vcpu that got preempted.
    
    Unfortunately there are no guarantees that the same task
    always keeps the same vcpu, so we can only track the task
    across a single "run" of the vcpu.
    
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -123,6 +123,7 @@ struct kvm_vcpu {
 	int fpu_active;
 	int guest_fpu_loaded, guest_xcr0_loaded;
 	wait_queue_head_t wq;
+	struct pid *pid;
 	int sigset_active;
 	sigset_t sigset;
 	struct kvm_vcpu_stat stat;
