bb8d55b7c113590b0f06182976edc19b0274b6ac
Thomas Gleixner <tglx@linutronix.de>
2011-02-01 13:51:48 +0000

    posix-timers: Convert clock_settime to clockid_to_kclock()
    Use the new kclock decoding function in clock_settime and cleanup all
    kclocks which use the default functions. Rename the misnomed
    common_clock_set() to posix_clock_realtime_set().
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: John Stultz <johnstul@us.ibm.com>
    Tested-by: Richard Cochran <richard.cochran@omicron.at>
    LKML-Reference: <20110201134418.518851246@linutronix.de>

--- kernel/posix-cpu-timers.c
+++ kernel/posix-cpu-timers.c
@@ -1605,7 +1605,6 @@ static __init int init_posix_cpu_timers(void)
 	struct k_clock process = {
 		.clock_getres	= process_cpu_clock_getres,
 		.clock_get	= process_cpu_clock_get,
-		.clock_set	= do_posix_clock_nosettime,
 		.timer_create	= process_cpu_timer_create,
 		.nsleep		= process_cpu_nsleep,
 		.nsleep_restart	= process_cpu_nsleep_restart,
@@ -1613,7 +1612,6 @@ static __init int init_posix_cpu_timers(void)
 	struct k_clock thread = {
 		.clock_getres	= thread_cpu_clock_getres,
 		.clock_get	= thread_cpu_clock_get,
-		.clock_set	= do_posix_clock_nosettime,
 		.timer_create	= thread_cpu_timer_create,
 	};
 	struct timespec ts;
