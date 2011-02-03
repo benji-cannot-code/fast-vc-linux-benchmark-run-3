0d40c4ae523fe1e32dfda9534fcd6ea1de67bfca
Greg Kroah-Hartman <gregkh@suse.de>
2011-02-03 15:59:58 -0800

    dynamic_debug: add #include <linux/sched.h>
    This fixes a build breakage caused by
    8ba6ebf583f12da32036fc0f003ab4043e54692e "Dynamic debug: Add more flags"
    
    Cc: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/jump_label.h>
 #include <linux/hardirq.h>
+#include <linux/sched.h>
 
 extern struct _ddebug __start___verbose[];
 extern struct _ddebug __stop___verbose[];
