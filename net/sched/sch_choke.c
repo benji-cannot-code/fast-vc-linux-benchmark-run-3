48617582c8f06f6e2c7c46d4eb0a5588d6e5b083
David S. Miller <davem@davemloft.net>
2011-02-02 23:06:31 -0800

    sch_choke: Need linux/vmalloc.h
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_choke.c
+++ net/sched/sch_choke.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/skbuff.h>
 #include <linux/reciprocal_div.h>
+#include <linux/vmalloc.h>
 #include <net/pkt_sched.h>
 #include <net/inet_ecn.h>
 #include <net/red.h>
