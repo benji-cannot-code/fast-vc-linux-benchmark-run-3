efb9338fd3099933837be434617ce9abf85572e9
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2011-02-02 23:21:58 -0800

    Input: qt602240_ts - rename to atmel_mxt_ts
    Since the driver will be supporting whole range of Atmels mXT touchscreen
    controllers we better rename it to atmel_mxt_ts.
    
    Acked-by: Iiro Valkonen <iiro.valkonen@atmel.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- arch/arm/mach-s5pv210/mach-goni.c
+++ arch/arm/mach-s5pv210/mach-goni.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fb.h>
 #include <linux/i2c.h>
 #include <linux/i2c-gpio.h>
-#include <linux/i2c/qt602240_ts.h>
+#include <linux/i2c/atmel_mxt_ts.h>
 #include <linux/mfd/max8998.h>
 #include <linux/mfd/wm8994/pdata.h>
 #include <linux/regulator/fixed.h>
--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/firmware.h>
 #include <linux/i2c.h>
-#include <linux/i2c/qt602240_ts.h>
+#include <linux/i2c/atmel_mxt_ts.h>
 #include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/slab.h>
