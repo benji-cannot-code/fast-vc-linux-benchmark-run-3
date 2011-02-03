4688b49eec507fce74bf9e698f2b83198a3b23fd
Thomas Gleixner <tglx@linutronix.de>
2011-02-03 12:27:44 +0100

    genirq: Consolidate disable/enable
    Create irq_disable/enable and use them to keep the flags consistent.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -201,7 +201,7 @@ int irq_startup(struct irq_desc *desc)
 	if (desc->irq_data.chip->irq_startup)
 		return desc->irq_data.chip->irq_startup(&desc->irq_data);
 
-	desc->irq_data.chip->irq_enable(&desc->irq_data);
+	irq_enable(desc);
 	return 0;
 }
 
@@ -210,6 +210,16 @@ void irq_shutdown(struct irq_desc *desc)
 	desc->status |= IRQ_MASKED | IRQ_DISABLED;
 	desc->depth = 1;
 	desc->irq_data.chip->irq_shutdown(&desc->irq_data);
+}
+
+void irq_enable(struct irq_desc *desc)
+{
+	desc->irq_data.chip->irq_enable(&desc->irq_data);
+}
+
+void irq_disable(struct irq_desc *desc)
+{
+	desc->irq_data.chip->irq_disable(&desc->irq_data);
 }
 
 /*
