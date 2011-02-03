0924f7716339721a1a7b0c62e8073b4a382f9394
Ben Skeggs <bskeggs@redhat.com>
2011-02-03 16:07:44 +1000

    drm/nv50-nvc0: include nv50_display in evo debugging
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nv50_display.c
+++ drivers/gpu/drm/nouveau/nv50_display.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#define NOUVEAU_DMA_DEBUG (nouveau_reg_debug & NOUVEAU_REG_DEBUG_EVO)
 #include "nv50_display.h"
 #include "nouveau_crtc.h"
 #include "nouveau_encoder.h"
