c68117c3b21c786615f10282341295d955361f02
Ben Skeggs <bskeggs@redhat.com>
2011-02-03 15:46:14 +1000

    drm/nv50-nvc0: request and wait on notification of modeset completion
    This should prevent a number of races from occuring, the most obvious of
    which will be exposed when we start making use of the "display sync" evo
    channel for page flipping.  The DS channel will reject any command stream
    that doesn't completely agree with the current "master" state.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nv50_display.h
+++ drivers/gpu/drm/nouveau/nv50_display.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nv50_display {
 	struct nouveau_channel *master;
+	struct nouveau_gpuobj *ntfy;
 
 	struct tasklet_struct tasklet;
 	struct {
