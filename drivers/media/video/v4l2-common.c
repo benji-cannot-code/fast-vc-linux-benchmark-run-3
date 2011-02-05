4324811fcc429c77ef098b1472f03c1ecac242f0
Hans Verkuil <hverkuil@xs4all.nl>
2011-02-05 10:10:38 -0300

    [media] v4l: removal of old, obsolete ioctls
    Some ioctl's were defined wrong on 2.6.2 and 2.6.6, using the wrong
    type of R/W arguments. They were fixed, but the old ioctl names are
    still there, maintained to avoid breaking binary compatibility:
    
    There's no sense on preserving those forever, as it is very doubtful
    that someone would try to use a such old binary with a modern kernel.
    Removing them will allow us to remove some magic done at the V4L ioctl
    handler.
    
    Note that any application compiled with a videodev2.h from 2.6.7 or later
    will be using the correct ioctls.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/v4l2-common.c
+++ drivers/media/video/v4l2-common.c
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/io.h>
 #include <asm/div64.h>
-#define __OLD_VIDIOC_ /* To allow fixing old calls*/
 #include <media/v4l2-common.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ctrls.h>
