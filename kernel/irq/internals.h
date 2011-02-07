59b2ca4cecc846c5a6d4d57306a80fc162f49a40
Thomas Gleixner <tglx@linutronix.de>
2011-02-07 01:55:43 +0100

    genirq: Move irq thread flags to core
    Soleley used in core code.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/irq/internals.h
+++ kernel/irq/internals.h
@@ -12,6 +12,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int noirqdebug;
 
+/*
+ * Bits used by threaded handlers:
+ * IRQTF_RUNTHREAD - signals that the interrupt handler thread should run
+ * IRQTF_DIED      - handler thread died
+ * IRQTF_WARNED    - warning "IRQ_WAKE_THREAD w/o thread_fn" has been printed
+ * IRQTF_AFFINITY  - irq thread is requested to adjust affinity
+ */
+enum {
+	IRQTF_RUNTHREAD,
+	IRQTF_DIED,
+	IRQTF_WARNED,
+	IRQTF_AFFINITY,
+};
+
 #define irq_data_to_desc(data)	container_of(data, struct irq_desc, irq_data)
 
 /* Set default functions for irq_chip structures: */
