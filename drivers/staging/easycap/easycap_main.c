cf00518ff6ad77df24f0415b19eacdac38e35e14
Tomas Winkler <tomas.winkler@intel.com>
2011-02-09 01:12:46 +0200

    staging/easycap: remove EASYCAP_SILENT option
    This has simulated a fault condition of probing for audio capability
    
    Cc: Mike Thomas <rmthomas@sciolus.org>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/easycap/easycap_main.c
+++ drivers/staging/easycap/easycap_main.c
@@ -4189,9 +4189,6 @@ static int easycap_usb_probe(struct usb_interface *pusb_interface,
  */
 /*--------------------------------------------------------------------------*/
 	case 1: {
-#ifdef EASYCAP_SILENT
-		return -ENOENT;
-#endif /*EASYCAP_SILENT*/
 		if (!peasycap) {
 			SAM("MISTAKE: peasycap is NULL\n");
 			return -EFAULT;
@@ -4208,9 +4205,6 @@ static int easycap_usb_probe(struct usb_interface *pusb_interface,
 	}
 	/*--------------------------------------------------------------------------*/
 	case 2: {
-#ifdef EASYCAP_SILENT
-		return -ENOENT;
-#endif /*EASYCAP_SILENT*/
 		if (!peasycap) {
 			SAM("MISTAKE: peasycap is NULL\n");
 			return -EFAULT;
