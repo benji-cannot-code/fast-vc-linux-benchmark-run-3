7cecd27a5d886d65a99294b9aae1da477e913d9f
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2011-02-09 21:40:09 +0100

    ARM: omap: move omap_board_config_kernel to .init.data
    This variable is only assigned in __init functions and never used later.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/common.c
+++ arch/arm/plat-omap/common.c
@@ -25,10 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NO_LENGTH_CHECK 0xffffffff
 
-struct omap_board_config_kernel *omap_board_config;
+struct omap_board_config_kernel *omap_board_config __initdata;
 int omap_board_config_size;
 
-static const void *get_config(u16 tag, size_t len, int skip, size_t *len_out)
+static const void *__init get_config(u16 tag, size_t len,
+		int skip, size_t *len_out)
 {
 	struct omap_board_config_kernel *kinfo = NULL;
 	int i;
