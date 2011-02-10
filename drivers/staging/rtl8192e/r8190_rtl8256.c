40074b0a25f85ffd688dbe3e2a90829c1a351a2c
Mike McCormack <mikem@ring3k.org>
2011-02-11 08:45:03 +0900

    staging: rtl8192e: rf_ps_lock doesn't need to be IRQ safe
    Signed-off-by: Mike McCormack <mikem@ring3k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192e/r8190_rtl8256.c
+++ drivers/staging/rtl8192e/r8190_rtl8256.c
@@ -717,7 +717,6 @@ MgntActSet_RF_State(
 	bool 			bConnectBySSID = false;
 	RT_RF_POWER_STATE	rtState;
 	u16					RFWaitCounter = 0;
-	unsigned long flag;
 	RT_TRACE(COMP_POWER, "===>MgntActSet_RF_State(): StateToSet(%d)\n",StateToSet);
 
 	//1//
@@ -727,10 +726,10 @@ MgntActSet_RF_State(
 
 	while(true)
 	{
-		spin_lock_irqsave(&priv->rf_ps_lock,flag);
+		spin_lock(&priv->rf_ps_lock);
 		if(priv->RFChangeInProgress)
 		{
-			spin_unlock_irqrestore(&priv->rf_ps_lock,flag);
+			spin_unlock(&priv->rf_ps_lock);
 			RT_TRACE(COMP_POWER, "MgntActSet_RF_State(): RF Change in progress! Wait to set..StateToSet(%d).\n", StateToSet);
 
 			// Set RF after the previous action is done.
@@ -752,7 +751,7 @@ MgntActSet_RF_State(
 		else
 		{
 			priv->RFChangeInProgress = true;
-			spin_unlock_irqrestore(&priv->rf_ps_lock,flag);
+			spin_unlock(&priv->rf_ps_lock);
 			break;
 		}
 	}
@@ -810,9 +809,9 @@ MgntActSet_RF_State(
 	}
 
 	// Release RF spinlock
-	spin_lock_irqsave(&priv->rf_ps_lock,flag);
+	spin_lock(&priv->rf_ps_lock);
 	priv->RFChangeInProgress = false;
-	spin_unlock_irqrestore(&priv->rf_ps_lock,flag);
+	spin_unlock(&priv->rf_ps_lock);
 
 	RT_TRACE(COMP_POWER, "<===MgntActSet_RF_State()\n");
 	return bActionAllowed;
