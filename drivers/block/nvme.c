eefcfabf12b5ea9686cf35b623259188f48ffbcc
Matthew Wilcox <matthew.r.wilcox@intel.com>
2011-02-10 12:01:09 -0500

    NVMe: Check for DMA mapping failure
    If dma_map_sg returns 0 (failure), we need to fail the I/O.
    
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- drivers/block/nvme.c
+++ drivers/block/nvme.c
@@ -452,7 +452,8 @@ static int nvme_submit_bio_queue(struct nvme_queue *nvmeq, struct nvme_ns *ns,
 		dma_dir = DMA_FROM_DEVICE;
 	}
 
-	nvme_map_bio(nvmeq->q_dmadev, nbio, bio, dma_dir, psegs);
+	if (nvme_map_bio(nvmeq->q_dmadev, nbio, bio, dma_dir, psegs) == 0)
+		goto mapping_failed;
 
 	cmnd->rw.flags = 1;
 	cmnd->rw.command_id = cmdid;
@@ -470,6 +471,11 @@ static int nvme_submit_bio_queue(struct nvme_queue *nvmeq, struct nvme_ns *ns,
 
 	spin_unlock_irqrestore(&nvmeq->q_lock, flags);
 
+	return 0;
+
+ mapping_failed:
+	free_nbio(nvmeq, nbio);
+	bio_endio(bio, -ENOMEM);
 	return 0;
 
  free_nbio:
