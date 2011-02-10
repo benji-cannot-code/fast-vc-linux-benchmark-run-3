ed3f14adc9e71748efa915aab9ae68afde79ab54
Stephen Warren <swarren@nvidia.com>
2011-02-10 15:37:19 -0700

    ASoC: Tegra: Add MODULE_ALIAS
    With the appropriate MODULE_ALIAS in place, the audio modules will be
    automatically loaded; there is no longer a need for manual modprobes.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/tegra/tegra_pcm.c
+++ sound/soc/tegra/tegra_pcm.c
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "tegra_pcm.h"
 
+#define DRV_NAME "tegra-pcm-audio"
+
 static const struct snd_pcm_hardware tegra_pcm_hardware = {
 	.info			= SNDRV_PCM_INFO_MMAP |
 				  SNDRV_PCM_INFO_MMAP_VALID |
@@ -378,7 +380,7 @@ static int __devexit tegra_pcm_platform_remove(struct platform_device *pdev)
 
 static struct platform_driver tegra_pcm_driver = {
 	.driver = {
-		.name = "tegra-pcm-audio",
+		.name = DRV_NAME,
 		.owner = THIS_MODULE,
 	},
 	.probe = tegra_pcm_platform_probe,
@@ -400,3 +402,4 @@ module_exit(snd_tegra_pcm_exit);
 MODULE_AUTHOR("Stephen Warren <swarren@nvidia.com>");
 MODULE_DESCRIPTION("Tegra PCM ASoC driver");
 MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:" DRV_NAME);
