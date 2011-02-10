7be9abba3e58c87d76a8109bb798a4935039b22b
Eliot Blennerhassett <eblennerhassett@audioscience.com>
2011-02-10 17:26:17 +1300

    ALSA: asihpi - Minor define updates
    HPI version 4.05.32
    Tweak HPI error code for backward compatibility.
    Add BUILD to build-related defines.
    
    Signed-off-by: Eliot Blennerhassett <eblennerhassett@audioscience.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/asihpi/hpios.h
+++ sound/pci/asihpi/hpios.h
@@ -28,9 +28,7 @@ HPI Operating System Specific macros for Linux Kernel driver
 #define HPI_OS_LINUX_KERNEL
 
 #define HPI_OS_DEFINED
-#define HPI_KERNEL_MODE
-
-#define HPI_REASSIGN_DUPLICATE_ADAPTER_IDX
+#define HPI_BUILD_KERNEL_MODE
 
 #include <linux/io.h>
 #include <asm/system.h>
@@ -143,7 +141,7 @@ static inline void cond_unlock(struct hpios_spinlock *l)
 #define hpios_dsplock_unlock(obj)  cond_unlock(&(obj)->dsp_lock)
 
 #ifdef CONFIG_SND_DEBUG
-#define HPI_DEBUG
+#define HPI_BUILD_DEBUG
 #endif
 
 #define HPI_ALIST_LOCKING
