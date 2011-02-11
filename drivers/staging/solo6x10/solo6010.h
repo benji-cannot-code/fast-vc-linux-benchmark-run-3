405f9ccc334b4b30f3f8fc14c1d86d73972b966c
Krzysztof Hałasa <khalasa@piap.pl>
2011-02-11 13:32:11 +0100

    staging: Solo6x10: Add support for SOLO6110 chip.
    Signed-off-by: Krzysztof Hałasa <khalasa@piap.pl>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/solo6x10/solo6010.h
+++ drivers/staging/solo6x10/solo6010.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef PCI_VENDOR_ID_SOFTLOGIC
 #define PCI_VENDOR_ID_SOFTLOGIC		0x9413
 #define PCI_DEVICE_ID_SOLO6010		0x6010
+#define PCI_DEVICE_ID_SOLO6110		0x6110
 #endif
 
 #ifndef PCI_VENDOR_ID_BLUECHERRY
@@ -70,6 +71,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SOLO6010_VER_SUB		0
 #define SOLO6010_VER_NUM \
 	KERNEL_VERSION(SOLO6010_VER_MAJOR, SOLO6010_VER_MINOR, SOLO6010_VER_SUB)
+
+#define FLAGS_6110			1
 
 /*
  * The SOLO6010 actually has 8 i2c channels, but we only use 2.
@@ -184,6 +187,7 @@ struct solo6010_dev {
 	u8 __iomem		*reg_base;
 	int			nr_chans;
 	int			nr_ext;
+	u32			flags;
 	u32			irq_mask;
 	u32			motion_mask;
 	spinlock_t		reg_io_lock;
