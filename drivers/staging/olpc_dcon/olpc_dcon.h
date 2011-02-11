1d25b21c8204d0031a74849f3dc363ab8ef3de05
Andres Salomon <dilinger@queued.net>
2011-02-10 17:56:27 -0800

    staging: olpc_dcon: clean up backlight handling
     - Move bl_val and bl_dev into dcon_priv struct....
     - The only time we ever read the backlight val from the dcon is
       at probe time.  Rather than calling dcon_get_backlight for that, just
       read from the register.
     - Drop dcon_get_backlight; it's just returning dcon->bl_val.
     - Rename dcon_set_backlight_hw to dcon_set_backlight, and drop the
       old dcon_set_backlight function.  Move contents of old dcon_set_backlight
       function into dconbl_set.
     - Shuffle backlight_ops callbacks around to be closer to struct, and
       rename them.
     - Make use of new backlight_properties arg to backlight_device_register,
       drop old code that set this manually.
    
    Signed-off-by: Andres Salomon <dilinger@queued.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/olpc_dcon/olpc_dcon.h
+++ drivers/staging/olpc_dcon/olpc_dcon.h
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct dcon_priv {
 	struct i2c_client *client;
 	struct fb_info *fbinfo;
+	struct backlight_device *bl_dev;
 
 	struct work_struct switch_source;
 	struct notifier_block reboot_nb;
@@ -58,6 +59,9 @@ struct dcon_priv {
 
 	/* Shadow register for the DCON_REG_MODE register */
 	u8 disp_mode;
+
+	/* The current backlight value - this saves us some smbus traffic */
+	u8 bl_val;
 
 	/* Current source, initialized at probe time */
 	int curr_src;
