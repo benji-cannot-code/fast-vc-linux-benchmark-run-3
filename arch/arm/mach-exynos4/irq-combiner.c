03a866ee5e648f29e9117a04e740927119c07bbb
Kukjin Kim <kgene.kim@samsung.com>
2011-02-14 16:10:55 +0900

    ARM: EXYNOS4: Update IRQ part
    This patch updates IRQ part of EXYNOS4 according to the change of
    ARCH name, EXYNOS4.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos4/irq-combiner.c
+++ arch/arm/mach-exynos4/irq-combiner.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/mach-s5pv310/irq-combiner.c
+/* linux/arch/arm/mach-exynos4/irq-combiner.c
  *
- * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2010-2011 Samsung Electronics Co., Ltd.
  *		http://www.samsung.com
  *
  * Based on arch/arm/common/gic.c
