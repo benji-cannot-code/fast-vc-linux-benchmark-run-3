eb2c083be5fcc2254096e5ec46499a3dcb805ff2
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-02-13 21:52:02 -0300

    [media] cx88: use unlocked_ioctl for cx88-video.
    cx88-video has locks. don't use the locked ioctl version, as
    it is not needed.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx88/cx88-video.c
+++ drivers/media/video/cx88/cx88-video.c
@@ -1673,7 +1673,7 @@ static const struct v4l2_file_operations video_fops =
 	.read	       = video_read,
 	.poll          = video_poll,
 	.mmap	       = video_mmap,
-	.ioctl	       = video_ioctl2,
+	.unlocked_ioctl = video_ioctl2,
 };
 
 static const struct v4l2_ioctl_ops video_ioctl_ops = {
@@ -1723,7 +1723,7 @@ static const struct v4l2_file_operations radio_fops =
 	.owner         = THIS_MODULE,
 	.open          = video_open,
 	.release       = video_release,
-	.ioctl         = video_ioctl2,
+	.unlocked_ioctl = video_ioctl2,
 };
 
 static const struct v4l2_ioctl_ops radio_ioctl_ops = {
