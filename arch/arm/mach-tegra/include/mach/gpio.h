970dec17340ca54d2ae7e956fe07bbbdecc999f4
Olof Johansson <olof@lixom.net>
2011-02-13 19:12:27 -0800

    ARM: tegra: add tegra_gpio_table and tegra_gpio_config
    To give one place to setup the pins that are used as GPIOs instead
    of as their pinmuxed functions. Specifying enabled as false explicitly
    disables the gpio mode of that pin (if left on by firmware).
    
    This should remove the need for calling these from specific drivers and
    thus reduce tegra-specific code from them.
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Acked-by: Erik Gilling <konkers@android.com>

--- arch/arm/mach-tegra/include/mach/gpio.h
+++ arch/arm/mach-tegra/include/mach/gpio.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MACH_TEGRA_GPIO_H
 #define __MACH_TEGRA_GPIO_H
 
+#include <linux/init.h>
 #include <mach/irqs.h>
 
 #define TEGRA_NR_GPIOS		INT_GPIO_NR
@@ -48,6 +49,12 @@ static inline int irq_to_gpio(unsigned int irq)
 	return -EINVAL;
 }
 
+struct tegra_gpio_table {
+	int	gpio;	/* GPIO number */
+	bool	enable;	/* Enable for GPIO at init? */
+};
+
+void tegra_gpio_config(struct tegra_gpio_table *table, int num);
 void tegra_gpio_enable(int gpio);
 void tegra_gpio_disable(int gpio);
 
