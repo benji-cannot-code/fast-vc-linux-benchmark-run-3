14017f70aea0ae53bb69cf916fdf90823b2c6bdd
Jesper Juhl <jj@chaosbits.net>
2011-02-14 23:38:18 +0100

    batman-adv: Remove two duplicate includes.
    Remove duplicate inclusion of "send.h" and "routing.h" from
    net/batman-adv/soft-interface.c
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- net/batman-adv/soft-interface.c
+++ net/batman-adv/soft-interface.c
@@ -30,14 +30,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hash.h"
 #include "gateway_common.h"
 #include "gateway_client.h"
-#include "send.h"
 #include "bat_sysfs.h"
 #include <linux/slab.h>
 #include <linux/ethtool.h>
 #include <linux/etherdevice.h>
 #include <linux/if_vlan.h>
 #include "unicast.h"
-#include "routing.h"
 
 
 static int bat_get_settings(struct net_device *dev, struct ethtool_cmd *cmd);
