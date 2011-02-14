648b8e172ce1bb41815df0ae0e20319a7d062124
Jarkko Nikula <jhnikula@gmail.com>
2011-02-14 17:20:22 +0200

    ASoC: omap: rx51: Report headset insertion instead of video out cable
    It is more usefull to report headset instead of video out cable in response
    to jack insertion as this is more usual use-case and because now the headset
    feature is supported. Automatic accessory detection is not possible at the
    moment so most sensible static accessory type have to be used.
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- sound/soc/omap/rx51.c
+++ sound/soc/omap/rx51.c
@@ -241,7 +241,7 @@ static struct snd_soc_jack_gpio rx51_av_jack_gpios[] = {
 	{
 		.gpio = RX51_JACK_DETECT_GPIO,
 		.name = "avdet-gpio",
-		.report = SND_JACK_VIDEOOUT,
+		.report = SND_JACK_HEADSET,
 		.invert = 1,
 		.debounce_time = 200,
 	},
@@ -332,7 +332,8 @@ static int rx51_aic34_init(struct snd_soc_pcm_runtime *rtd)
 
 	/* AV jack detection */
 	err = snd_soc_jack_new(codec, "AV Jack",
-			       SND_JACK_VIDEOOUT, &rx51_av_jack);
+			       SND_JACK_HEADSET | SND_JACK_VIDEOOUT,
+			       &rx51_av_jack);
 	if (err)
 		return err;
 	err = snd_soc_jack_add_gpios(&rx51_av_jack,
