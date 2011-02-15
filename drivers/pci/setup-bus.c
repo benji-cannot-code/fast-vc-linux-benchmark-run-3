bb3038ce487c3a283f5eb18fc542ba1821192626
Ram Pai <linuxram@us.ibm.com>
2011-02-14 17:43:18 -0800

    PCI: data structure agnostic free list function
    Replace free_failed_list() with a free_list() call. free_list() can
    handle 'resource_list_x', 'resource_list' and any linked list linked
    through ->next
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -37,6 +37,16 @@ struct resource_list_x {
 	unsigned long flags;
 };
 
+#define free_list(type, head) do {                      \
+	struct type *list, *tmp;			\
+	for (list = (head)->next; list;) {		\
+		tmp = list;				\
+		list = list->next;			\
+		kfree(tmp);				\
+	}						\
+	(head)->next = NULL;				\
+} while (0)
+
 static void add_to_failed_list(struct resource_list_x *head,
 				 struct pci_dev *dev, struct resource *res)
 {
@@ -57,19 +67,6 @@ static void add_to_failed_list(struct resource_list_x *head,
 	tmp->end = res->end;
 	tmp->flags = res->flags;
 	list->next = tmp;
-}
-
-static void free_failed_list(struct resource_list_x *head)
-{
-	struct resource_list_x *list, *tmp;
-
-	for (list = head->next; list;) {
-		tmp = list;
-		list = list->next;
-		kfree(tmp);
-	}
-
-	head->next = NULL;
 }
 
 static void __dev_sort_resources(struct pci_dev *dev,
@@ -901,7 +898,7 @@ again:
 
 	if (tried_times >= 2) {
 		/* still fail, don't need to try more */
-		free_failed_list(&head);
+		free_list(resource_list_x, &head);
 		goto enable_all;
 	}
 
@@ -932,7 +929,7 @@ again:
 
 		list = list->next;
 	}
-	free_failed_list(&head);
+	free_list(resource_list_x, &head);
 
 	goto again;
 
