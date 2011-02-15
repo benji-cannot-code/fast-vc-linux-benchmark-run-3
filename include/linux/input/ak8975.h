2ffb5c12cb241412dfd4a7a9d674d8c67b77cf3d
Tony SIM <chinyeow.sim.xt@renesas.com>
2011-02-15 19:10:27 +0900

    staging: iio: ak8975: add platform data.
    As some of the platform not support irq_to_gpio, we pass gpio port
    by platform data.
    
    Signed-off-by: Tony SIM <chinyeow.sim.xt@renesas.com>
    Signed-off-by: Andrew Chew <achew@nvidia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

---
+++ include/linux/input/ak8975.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * ak8975 platform support
+ *
+ * Copyright (C) 2010 Renesas Solutions Corp.
+ *
+ * Author: Tony SIM <chinyeow.sim.xt@renesas.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _AK8975_H
+#define _AK8975_H
+
+struct ak8975_platform_data {
+	int gpio;
+};
+
+#endif
