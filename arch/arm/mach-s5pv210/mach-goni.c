ff5d9f1648e8d9b7bc1068129d634dac5da9ae89
Iiro Valkonen <iiro.valkonen@atmel.com>
2011-02-15 13:36:52 -0800

    Input: atmel_mxt_ts - allow board code to specify IRQ flags
    Different board have different requirements/setups so let's be more
    flexible.
    
    Signed-off-by: Iiro Valkonen <iiro.valkonen@atmel.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- arch/arm/mach-s5pv210/mach-goni.c
+++ arch/arm/mach-s5pv210/mach-goni.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
 #include <linux/gpio.h>
+#include <linux/interrupt.h>
 
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -232,6 +233,7 @@ static struct mxt_platform_data qt602240_platform_data = {
 	.threshold	= 0x28,
 	.voltage	= 2800000,              /* 2.8V */
 	.orient		= MXT_DIAGONAL,
+	.irqflags	= IRQF_TRIGGER_FALLING,
 };
 
 static struct s3c2410_platform_i2c i2c2_data __initdata = {
