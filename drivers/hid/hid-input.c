07bd04cf7b739f0bd50439473fb538edc2c56082
Benjamin Tissoires <benjamin.tissoires@enac.fr>
2011-02-15 15:41:10 +0100

    HID: kernel oops in out_cleanup in function hidinput_connect
    Goto out_cleanup infers a kernel oops: hidinput_disconnect calls
    input_unregister_driver to all members of hid->inputs.
    However, hidinput already has been added to hid->inputs even
    though input_register_device was not called.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@enac.fr>
    Reviewed-by: Dmitry Torokhov <dtor@mail.ru>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-input.c
+++ drivers/hid/hid-input.c
@@ -929,6 +929,7 @@ int hidinput_connect(struct hid_device *hid, unsigned int force)
 	return 0;
 
 out_cleanup:
+	list_del(&hidinput->list);
 	input_free_device(hidinput->input);
 	kfree(hidinput);
 out_unwind:
