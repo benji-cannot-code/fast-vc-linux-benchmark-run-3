e84d6ffeeca6c39018e67755ac080c62093daddb
viresh kumar <viresh.kumar@st.com>
2011-02-16 07:40:39 +0100

    ARM: 6703/1: SPEAr: update clk API support
    - Add support for divisor per parent clock
    - Add ENABLED_ON_INIT feature in clk
    - Add clk_set_rate(), round_rate_index & clk_round_rate()
    - Simplify clk_recalc functions
    - Add/update clock definitions
    
    Reviewed-by: Stanley Miao <stanley.miao@windriver.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>
    Signed-off-by: shiraz hashim <shiraz.hashim@st.com>
    Signed-off-by: Rajeev Kumar <rajeev-dlh.kumar@st.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-spear3xx/include/mach/misc_regs.h
+++ arch/arm/mach-spear3xx/include/mach/misc_regs.h
@@ -64,8 +64,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GPT1_CLK_SHIFT		11
 #define GPT2_CLK_SHIFT		12
 #define GPT_CLK_MASK		0x1
-#define AUX_CLK_PLL3_MASK	0
-#define AUX_CLK_PLL1_MASK	1
+#define AUX_CLK_PLL3_VAL	0
+#define AUX_CLK_PLL1_VAL	1
 
 #define PERIP1_CLK_ENB		(MISC_BASE + 0x02C)
 /* PERIP1_CLK_ENB register masks */
@@ -114,6 +114,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RAS3_CLK_SYNT		(MISC_BASE + 0x074)
 #define RAS4_CLK_SYNT		(MISC_BASE + 0x078)
 /* aux clk synthesiser register masks for irda to ras4 */
+#define AUX_SYNT_ENB		31
 #define AUX_EQ_SEL_SHIFT	30
 #define AUX_EQ_SEL_MASK		1
 #define AUX_EQ1_SEL		0
