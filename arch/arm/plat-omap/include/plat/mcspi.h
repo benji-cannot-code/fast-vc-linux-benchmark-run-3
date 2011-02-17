2dc3e163c427a435de7e60ebd7107aa751083026
Charulatha V <charu@ti.com>
2011-02-17 09:53:09 -0800

    OMAP2420: hwmod data: Add McSPI
    Update the omap2420 hwmod data with the McSPI info.
    Add a device attribute structure which will be used
    for passing number of chipselects from hwmod data.
    Add revision macros to be passed from rev field from
    hwmod.
    
    Signed-off-by: Charulatha V <charu@ti.com>
    Signed-off-by: Govindraj.R <govindraj.raja@ti.com>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/mcspi.h
+++ arch/arm/plat-omap/include/plat/mcspi.h
@@ -2,8 +2,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _OMAP2_MCSPI_H
 #define _OMAP2_MCSPI_H
 
+#define OMAP2_MCSPI_REV 0
+#define OMAP3_MCSPI_REV 1
+#define OMAP4_MCSPI_REV 2
+
 struct omap2_mcspi_platform_config {
 	unsigned short	num_cs;
+};
+
+struct omap2_mcspi_dev_attr {
+	unsigned short num_chipselect;
 };
 
 struct omap2_mcspi_device_config {
