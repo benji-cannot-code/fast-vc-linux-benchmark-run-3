c9603a83c2ca937aed29540a703bc8f0c2f0e50f
Vipin Mehta <vmehta@atheros.com>
2011-02-18 13:13:14 -0800

    staging: ath6kl: Adding state in driver to track the sme state
    Adding state in driver to track the sme state. The connect/disconnect
    events from the driver were messing up the state maintained within the
    cfg80211 module.
    
    Signed-off-by: Vipin Mehta <vmehta@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/os/linux/include/ar6000_drv.h
+++ drivers/staging/ath6kl/os/linux/include/ar6000_drv.h
@@ -394,6 +394,12 @@ struct ar_key {
     u8 seq_len;
     u32 cipher;
 };
+
+enum {
+    SME_DISCONNECTED,
+    SME_CONNECTING,
+    SME_CONNECTED
+};
 #endif /* ATH6K_CONFIG_CFG80211 */
 
 
@@ -596,6 +602,7 @@ typedef struct ar6_softc {
     struct wireless_dev *wdev;
     struct cfg80211_scan_request    *scan_request;
     struct ar_key   keys[WMI_MAX_KEY_INDEX + 1];
+    u32 smeState;
 #endif /* ATH6K_CONFIG_CFG80211 */
     u16 arWlanPowerState;
     bool                  arWlanOff;
