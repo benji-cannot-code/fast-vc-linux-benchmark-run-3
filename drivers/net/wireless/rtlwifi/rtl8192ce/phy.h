fba24c0d9d884716e5237b6660378d8fc8d865a3
Larry Finger <Larry.Finger@lwfinger.net>
2011-02-19 16:29:07 -0600

    rtlwifi: Modify wifi.h for rtl8192cu
    Further merge of parameters needed for rtl8192cu. In addition,
    some changes needed for rtl8192se and rtl8192de are included
    and additional Hungarian notation is removed.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/rtl8192ce/phy.h
+++ drivers/net/wireless/rtlwifi/rtl8192ce/phy.h
@@ -58,8 +58,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IQK_MAC_REG_NUM			4
 
 #define RF90_PATH_MAX			2
-#define CHANNEL_MAX_NUMBER		14
-#define CHANNEL_GROUP_MAX		3
 
 #define CT_OFFSET_MAC_ADDR		0X16
 
@@ -79,8 +77,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CT_OFFSET_CUSTOMER_ID		0x7F
 
 #define RTL92C_MAX_PATH_NUM		2
-#define CHANNEL_MAX_NUMBER		14
-#define CHANNEL_GROUP_MAX		3
 #define LLT_LAST_ENTRY_OF_TX_PKT_BUFFER	255
 enum swchnlcmd_id {
 	CMDID_END,
