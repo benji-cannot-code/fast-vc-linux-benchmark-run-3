f498e0d997f0599382883178597a9998576de888
Russell King <rmk+kernel@arm.linux.org.uk>
2011-02-20 12:27:49 +0000

    ARM: tlb: move noMMU tlb_flush() to asm/tlb.h
    There's no need to noMMU to put tlb_flush() in asm/tlbflush.h - it's
    part of the tlb shootdown interface.  Move it to asm/tlb.h instead, as
    per x86.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/tlb.h
+++ arch/arm/include/asm/tlb.h
@@ -23,6 +23,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef CONFIG_MMU
 
 #include <linux/pagemap.h>
+
+#define tlb_flush(tlb)	((void) tlb)
+
 #include <asm-generic/tlb.h>
 
 #else /* !CONFIG_MMU */
