9476c91b39d2f3ab076d0b4b9b0b1cc3a542a912
Javier Martinez Canillas <martinez.javier@gmail.com>
2011-02-21 18:16:43 +0100

    Staging: xgifb: Remove all the references to XGIfb_accel
    xgifb framebuffer driver has an option to use an accelerator engine that never
    get used (XGIfb_accel is always 0).
    
    An earlier patchset remove the code that depends on the accelerator being
    activated. This patch removes all the references to XGIfb_accel.
    
    Signed-off-by: Javier Martinez Canillas <martinez.javier@gmail.com>
    Acked-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/XGI_main_26.c
+++ drivers/staging/xgifb/XGI_main_26.c
@@ -44,8 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "XGI_main.h"
 #include "vb_util.h"
 
-int XGIfb_accel = 0;
-
 #define Index_CR_GPIO_Reg1 0x48
 #define Index_CR_GPIO_Reg2 0x49
 #define Index_CR_GPIO_Reg3 0x4a
@@ -1191,10 +1189,6 @@ static int XGIfb_do_set_var(struct fb_var_screeninfo *var, int isactive,
 		xgi_video_info.video_linelength = info->var.xres_virtual
 				* (xgi_video_info.video_bpp >> 3);
 		xgi_video_info.accel = 0;
-		if (XGIfb_accel) {
-			xgi_video_info.accel = (var->accel_flags
-					& FB_ACCELF_TEXT) ? -1 : 0;
-		}
 		switch (xgi_video_info.video_bpp) {
 		case 8:
 			xgi_video_info.DstColor = 0x0000;
@@ -2857,8 +2851,6 @@ XGIINITSTATIC int __init XGIfb_setup(char *options)
 				printk(KERN_INFO "XGIfb: Illegal pdc parameter\n");
 				XGIfb_pdc = 0;
 			}
-		} else if (!strncmp(this_opt, "noaccel", 7)) {
-			XGIfb_accel = 0;
 		} else if (!strncmp(this_opt, "noypan", 6)) {
 			XGIfb_ypan = 0;
 		} else if (!strncmp(this_opt, "userom:", 7)) {
@@ -2873,11 +2865,9 @@ XGIINITSTATIC int __init XGIfb_setup(char *options)
 		/* TW: Acceleration only with MMIO mode */
 		if ((XGIfb_queuemode != -1) && (XGIfb_queuemode != MMIO_CMD)) {
 			XGIfb_ypan = 0;
-			XGIfb_accel = 0;
 		}
 		/* TW: Panning only with acceleration */
-		if (XGIfb_accel == 0)
-			XGIfb_ypan = 0;
+		XGIfb_ypan = 0;
 
 	}
 	printk("\nxgifb: outa xgifb_setup 3450");
@@ -3371,11 +3361,6 @@ static int __devinit xgifb_probe(struct pci_dev *pdev,
 		}
 
 		xgi_video_info.accel = 0;
-		if (XGIfb_accel) {
-			xgi_video_info.accel = -1;
-			default_var.accel_flags |= FB_ACCELF_TEXT;
-			XGIfb_initaccel();
-		}
 
 		fb_info->flags = FBINFO_FLAG_DEFAULT;
 		fb_info->var = default_var;
