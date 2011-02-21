a243020c2f29712bcd4ccab59081c93dd4860cd6
Sujith Manoharan <Sujith.Manoharan@atheros.com>
2011-02-21 07:48:53 +0530

    ath9k_htc: Make sequence number calculation per-VIF
    Signed-off-by: Sujith Manoharan <Sujith.Manoharan@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/htc.h
+++ drivers/net/wireless/ath/ath9k/htc.h
@@ -209,6 +209,7 @@ struct ath9k_htc_target_stats {
 
 struct ath9k_htc_vif {
 	u8 index;
+	u16 seq_no;
 };
 
 struct ath9k_vif_iter_data {
@@ -373,7 +374,6 @@ struct ath9k_htc_priv {
 	u16 txpowlimit;
 	u16 nvifs;
 	u16 nstations;
-	u16 seq_no;
 	u32 bmiss_cnt;
 	bool rearm_ani;
 	bool reconfig_beacon;
