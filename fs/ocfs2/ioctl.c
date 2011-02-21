c5f87894c077751ab6d2cbb0a40147ff06c7895e
Tao Ma <boyu.mt@taobao.com>
2011-02-21 11:18:23 +0800

    ocfs2: remove INODE from unused files.
    As there are no such debug information in fs/ocfs2/ioctl.c,
    fs/ocfs2/locks.c and fs/ocfs2/sysfile.c, ML_INODE are also
    removed.
    
    Signed-off-by: Tao Ma <boyu.mt@taobao.com>

--- fs/ocfs2/ioctl.c
+++ fs/ocfs2/ioctl.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mount.h>
 #include <linux/compat.h>
 
-#define MLOG_MASK_PREFIX ML_INODE
 #include <cluster/masklog.h>
 
 #include "ocfs2.h"
