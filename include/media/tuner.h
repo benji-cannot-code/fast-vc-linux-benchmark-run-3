35cbf61a2e3b1f73a0d4ab01761298a48150a55a
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-02-21 21:03:59 -0300

    [media] tuner: Remove remaining usages of T_DIGITAL_TV
    A few places used T_DIGITAL_TV internally. Remove the usage of this
    obsolete mode mask.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -162,7 +162,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum tuner_mode {
 	T_RADIO		= 1 << V4L2_TUNER_RADIO,
 	T_ANALOG_TV     = 1 << V4L2_TUNER_ANALOG_TV,
-	T_DIGITAL_TV    = 1 << V4L2_TUNER_DIGITAL_TV,
+	/* Don't need to map V4L2_TUNER_DIGITAL_TV, as tuner-core won't use it */
 };
 
 /* Older boards only had a single tuner device. Nowadays multiple tuner
