14da2460844c126a6d072c86a722d111769ade75
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2011-02-22 21:07:42 +0100

    x86: dtb: Add support for PCI devices backed by dtb nodes
    x86_of_pci_init() does two things:
    
    - it provides a generic irq enable and disable function. enable queries
      the device tree for the interrupt information, calls ->xlate on the
      irq host and updates the pci->irq information for the device.
    
    - it walks through PCI bus(es) in the device tree and adds its children
      (device) nodes to appropriate pci_dev nodes in kernel. So the dtb
      node information is available at probe time of the PCI device.
    
    Adding a PCI bus based on the information in the device tree is
    currently not supported. Right now direct access via ioports is used.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Tested-by: Dirk Brandewie <dirk.brandewie@gmail.com>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    Cc: sodaville@linutronix.de
    Cc: devicetree-discuss@lists.ozlabs.org
    LKML-Reference: <1298405266-1624-8-git-send-email-bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/prom.h
+++ arch/x86/include/asm/prom.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/of.h>
 #include <linux/types.h>
+#include <linux/pci.h>
 
 #include <asm/irq.h>
 #include <asm/atomic.h>
@@ -30,8 +31,21 @@ extern void add_dtb(u64 data);
 void x86_dtb_find_config(void);
 void x86_dtb_get_config(unsigned int unused);
 void add_interrupt_host(struct irq_domain *ih);
+void __cpuinit x86_of_pci_init(void);
+
+static inline struct device_node *pci_device_to_OF_node(struct pci_dev *pdev)
+{
+	return pdev ? pdev->dev.of_node : NULL;
+}
+
+static inline struct device_node *pci_bus_to_OF_node(struct pci_bus *bus)
+{
+	return pci_device_to_OF_node(bus->self);
+}
+
 #else
 static inline void add_dtb(u64 data) { }
+static inline void x86_of_pci_init(void) { }
 #define x86_dtb_find_config x86_init_noop
 #define x86_dtb_get_config x86_init_uint_noop
 #define of_ioapic 0
