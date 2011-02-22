319b9bdff237dbcda7a3a80481564272fca2649d
Tao Ma <boyu.mt@taobao.com>
2011-02-22 22:27:33 +0800

    ocfs2: Remove masklog ML_EXTENT_MAP.
    Remove mlog(0) from fs/ocfs2/extent_map.c and the masklog EXTENT_MAP.
    
    Signed-off-by: Tao Ma <boyu.mt@taobao.com>

--- fs/ocfs2/extent_map.c
+++ fs/ocfs2/extent_map.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/fiemap.h>
 
-#define MLOG_MASK_PREFIX ML_EXTENT_MAP
 #include <cluster/masklog.h>
 
 #include "ocfs2.h"
@@ -40,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "inode.h"
 #include "super.h"
 #include "symlink.h"
+#include "ocfs2_trace.h"
 
 #include "buffer_head_io.h"
 
@@ -842,8 +842,7 @@ int ocfs2_read_virt_blocks(struct inode *inode, u64 v_block, int nr,
 	u64 p_block, p_count;
 	int i, count, done = 0;
 
-	mlog(0, "(inode = %p, v_block = %llu, nr = %d, bhs = %p, "
-	     "flags = %x, validate = %p)\n",
+	trace_ocfs2_read_virt_blocks(
 	     inode, (unsigned long long)v_block, nr, bhs, flags,
 	     validate);
 
