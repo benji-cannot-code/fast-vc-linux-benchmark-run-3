47e29798be1422bf5d9a660a3fe38cbd364e46aa
Tao Ma <boyu.mt@taobao.com>
2011-02-23 22:45:26 +0800

    ocfs2: Remove masklog ML_NAMEI.
    Remove mlog(0) from fs/ocfs2/namei.c and the masklog NAMEI finally.
    
    Signed-off-by: Tao Ma <boyu.mt@taobao.com>

--- fs/ocfs2/cluster/masklog.h
+++ fs/ocfs2/cluster/masklog.h
@@ -99,7 +99,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ML_DLM_GLUE	0x0000000000080000ULL /* ocfs2 dlm glue layer */
 #define ML_BH_IO	0x0000000000100000ULL /* ocfs2 buffer I/O */
 #define ML_UPTODATE	0x0000000000200000ULL /* ocfs2 caching sequence #'s */
-#define ML_NAMEI	0x0000000000400000ULL /* ocfs2 directory / namespace */
 #define ML_VOTE		0x0000000001000000ULL /* ocfs2 node messaging  */
 #define ML_DCACHE	0x0000000002000000ULL /* ocfs2 dcache operations */
 #define ML_CONN		0x0000000004000000ULL /* net connection management */
