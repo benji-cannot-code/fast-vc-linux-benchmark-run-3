c55e02f184bd464c155205fb05d1e8549f538e6d
Thomas Gleixner <tglx@linutronix.de>
2011-02-23 19:54:53 +0100

    x86: ioapic: Move trigger defines to io_apic.h
    Required for devicetree based io_apic configuration.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/apic/io_apic.c
+++ arch/x86/kernel/apic/io_apic.c
@@ -1217,10 +1217,6 @@ void __setup_vector_irq(int cpu)
 static struct irq_chip ioapic_chip;
 static struct irq_chip ir_ioapic_chip;
 
-#define IOAPIC_AUTO     -1
-#define IOAPIC_EDGE     0
-#define IOAPIC_LEVEL    1
-
 #ifdef CONFIG_X86_32
 static inline int IO_APIC_irq_trigger(int irq)
 {
