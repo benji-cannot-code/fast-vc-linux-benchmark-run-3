fe460034fe766d12d40333516d35bc84e5f7885b
Wolfram Sang <w.sang@pengutronix.de>
2011-02-23 11:11:35 +0100

    i2c-eg20t: include slab.h for memory allocations
    Fixes (with v2.6.38-rc3/parisc/parisc-allmodconfig):
      src/drivers/i2c/busses/i2c-eg20t.c:720: error: implicit declaration of function 'kzalloc'
      src/drivers/i2c/busses/i2c-eg20t.c:790: error: implicit declaration of function 'kfree'
    
    Reported-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Cc: Tomoya MORINAGA <tomoya-linux@dsn.okisemi.com>
    Cc: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- drivers/i2c/busses/i2c-eg20t.c
+++ drivers/i2c/busses/i2c-eg20t.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/mutex.h>
 #include <linux/ktime.h>
+#include <linux/slab.h>
 
 #define PCH_EVENT_SET	0	/* I2C Interrupt Event Set Status */
 #define PCH_EVENT_NONE	1	/* I2C Interrupt Event Clear Status */
