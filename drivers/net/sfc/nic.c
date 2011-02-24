ea1297b8b11669291bf94815bde86a20bb9ebbcd
Ben Hutchings <bhutchings@solarflare.com>
2011-02-24 19:30:41 +0000

    sfc: Remove configurable FIFO thresholds for pause frame generation
    In Falcon we can configure the fill levels of the RX data FIFO which
    trigger the generation of pause frames (if enabled), and we have
    module parameters for this.
    
    Siena does not allow the levels to be configured (or, if it does, this
    is done by the MC firmware and is not configurable by drivers).
    
    So far as I can tell, the module parameters are not used by our
    internal scripts and have not been documented (with the exception of
    the short parameter descriptions).  Therefore, remove them and always
    initialise Falcon with the default values.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>

--- drivers/net/sfc/nic.c
+++ drivers/net/sfc/nic.c
@@ -42,26 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RX_DC_ENTRIES 64
 #define RX_DC_ENTRIES_ORDER 3
 
-/* RX FIFO XOFF watermark
- *
- * When the amount of the RX FIFO increases used increases past this
- * watermark send XOFF. Only used if RX flow control is enabled (ethtool -A)
- * This also has an effect on RX/TX arbitration
- */
-int efx_nic_rx_xoff_thresh = -1;
-module_param_named(rx_xoff_thresh_bytes, efx_nic_rx_xoff_thresh, int, 0644);
-MODULE_PARM_DESC(rx_xoff_thresh_bytes, "RX fifo XOFF threshold");
-
-/* RX FIFO XON watermark
- *
- * When the amount of the RX FIFO used decreases below this
- * watermark send XON. Only used if TX flow control is enabled (ethtool -A)
- * This also has an effect on RX/TX arbitration
- */
-int efx_nic_rx_xon_thresh = -1;
-module_param_named(rx_xon_thresh_bytes, efx_nic_rx_xon_thresh, int, 0644);
-MODULE_PARM_DESC(rx_xon_thresh_bytes, "RX fifo XON threshold");
-
 /* If EFX_MAX_INT_ERRORS internal errors occur within
  * EFX_INT_ERROR_EXPIRE seconds, we consider the NIC broken and
  * disable it.
