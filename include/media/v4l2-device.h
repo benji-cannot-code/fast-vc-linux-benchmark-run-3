427e7e36cdd07d90b813c4cc6d0a5003bf6c2fdc
Hans Verkuil <hverkuil@xs4all.nl>
2011-02-24 10:42:24 -0300

    [media] v4l2: add v4l2_prio_state to v4l2_device and video_device
    Integrate the v4l2_prio_state into the core, ready for use.
    
    One struct v4l2_prio_state is added to v4l2_device and a pointer
    to a prio state is added to video_device.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/v4l2-device.h
+++ include/media/v4l2-device.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/media-device.h>
 #include <media/v4l2-subdev.h>
+#include <media/v4l2-dev.h>
 
 /* Each instance of a V4L2 device should create the v4l2_device struct,
    either stand-alone or embedded in a larger struct.
@@ -56,6 +57,8 @@ struct v4l2_device {
 			unsigned int notification, void *arg);
 	/* The control handler. May be NULL. */
 	struct v4l2_ctrl_handler *ctrl_handler;
+	/* Device's priority state */
+	struct v4l2_prio_state prio;
 	/* BKL replacement mutex. Temporary solution only. */
 	struct mutex ioctl_lock;
 };
