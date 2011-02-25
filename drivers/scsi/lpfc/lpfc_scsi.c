54f2b8e0ac29240b86016dc3ad4f26e3ffc9971b
Mike Christie <michaelc@cs.wisc.edu>
2011-02-25 14:04:28 -0600

    [SCSI] lpfc: block target when port queueing limit is hit
    Instead of blocking the entire host when the port's
    queueing limit is hit, we should only block the port's
    target. This will allow IO to other ports to execute.
    
    Signed-off-by: Mike Christie <michaelc@cs.wisc.edu>
    Acked-by: James Smart  <james.smart@emulex.com>
    Signed-off-by: James Bottomley <James.Bottomley@suse.de>

--- drivers/scsi/lpfc/lpfc_scsi.c
+++ drivers/scsi/lpfc/lpfc_scsi.c
@@ -3063,7 +3063,7 @@ lpfc_queuecommand_lck(struct scsi_cmnd *cmnd, void (*done) (struct scsi_cmnd *))
 		goto out_fail_command;
 	}
 	if (atomic_read(&ndlp->cmd_pending) >= ndlp->cmd_qdepth)
-		goto out_host_busy;
+		goto out_tgt_busy;
 
 	lpfc_cmd = lpfc_get_scsi_buf(phba, ndlp);
 	if (lpfc_cmd == NULL) {
@@ -3179,6 +3179,9 @@ lpfc_queuecommand_lck(struct scsi_cmnd *cmnd, void (*done) (struct scsi_cmnd *))
 	lpfc_release_scsi_buf(phba, lpfc_cmd);
  out_host_busy:
 	return SCSI_MLQUEUE_HOST_BUSY;
+
+ out_tgt_busy:
+	return SCSI_MLQUEUE_TARGET_BUSY;
 
  out_fail_command:
 	done(cmnd);
