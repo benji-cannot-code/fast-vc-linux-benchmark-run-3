4235dfd093995f28d9285863fea4fabbb61d9923
David S. Miller <davem@davemloft.net>
2011-02-24 22:50:30 -0800

    rtlwifi: Need to include vmalloc.h
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/rtlwifi/wifi.h
+++ drivers/net/wireless/rtlwifi/wifi.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 #include <linux/version.h>
 #include <linux/etherdevice.h>
+#include <linux/vmalloc.h>
 #include <linux/usb.h>
 #include <net/mac80211.h>
 #include "debug.h"
