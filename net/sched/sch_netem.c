e4b03d1dfb4f456c59a4d3c803109c97c8da1d21
David S. Miller <davem@davemloft.net>
2011-02-24 22:48:13 -0800

    sch_netem: Need to include vmalloc.h
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_netem.c
+++ net/sched/sch_netem.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/skbuff.h>
+#include <linux/vmalloc.h>
 #include <linux/rtnetlink.h>
 
 #include <net/netlink.h>
