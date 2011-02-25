d39107c97cb4ef5884fe21e7c82c4e7b1f980daf
Arend van Spriel <arend@broadcom.com>
2011-02-25 16:39:23 +0100

    staging: brcm80211: remove NULL pointer checks before calling kfree
    kfree function can handle NULL pointer as passed parameter so there
    is no need for the calling function to check for this before calling
    kfree.
    
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Brett Rudley <brudley@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/wlc_bmac.c
+++ drivers/staging/brcm80211/brcmsmac/wlc_bmac.c
@@ -1045,10 +1045,8 @@ int wlc_bmac_detach(struct wlc_info *wlc)
 	wlc_phy_shim_detach(wlc_hw->physhim);
 
 	/* free vars */
-	if (wlc_hw->vars) {
-		kfree(wlc_hw->vars);
-		wlc_hw->vars = NULL;
-	}
+	kfree(wlc_hw->vars);
+	wlc_hw->vars = NULL;
 
 	if (wlc_hw->sih) {
 		si_detach(wlc_hw->sih);
