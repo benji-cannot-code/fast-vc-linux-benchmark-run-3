c055d92f94ebbce23d36bf08beeeab4a701e807f
Arvid Brodin <arvid.brodin@enea.com>
2011-02-26 22:05:26 +0100

    usb/isp1760: Remove redundant "data_buffer" member from struct inter_packet_info
    Signed-off-by: Arvid Brodin <arvid.brodin@enea.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/isp1760-hcd.c
+++ drivers/usb/host/isp1760-hcd.c
@@ -838,7 +838,6 @@ static void enqueue_one_atl_qtd(struct isp1760_hcd *priv,
 
 	priv->atl_ints[slot].qh = qh;
 	priv->atl_ints[slot].qtd = qtd;
-	priv->atl_ints[slot].data_buffer = qtd->data_buffer;
 	qtd->status |= URB_ENQUEUED;
 	qtd->status |= slot << 16;
 }
@@ -857,7 +856,6 @@ static void enqueue_one_int_qtd(struct isp1760_hcd *priv,
 
 	priv->int_ints[slot].qh = qh;
 	priv->int_ints[slot].qtd = qtd;
-	priv->int_ints[slot].data_buffer = qtd->data_buffer;
 	qtd->status |= URB_ENQUEUED;
 	qtd->status |= slot << 16;
 }
@@ -1148,8 +1146,7 @@ static void do_atl_int(struct usb_hcd *hcd)
 			switch (DW1_GET_PID(ptd.dw1)) {
 			case IN_PID:
 				mem_reads8(hcd->regs, qtd->payload_addr,
-					priv->atl_ints[queue_entry].data_buffer,
-					length);
+						qtd->data_buffer, length);
 
 			case OUT_PID:
 
@@ -1160,7 +1157,6 @@ static void do_atl_int(struct usb_hcd *hcd)
 			}
 		}
 
-		priv->atl_ints[queue_entry].data_buffer = NULL;
 		priv->atl_ints[queue_entry].qtd = NULL;
 		priv->atl_ints[queue_entry].qh = NULL;
 
@@ -1284,8 +1280,7 @@ static void do_intl_int(struct usb_hcd *hcd)
 			switch (DW1_GET_PID(ptd.dw1)) {
 			case IN_PID:
 				mem_reads8(hcd->regs, qtd->payload_addr,
-					priv->int_ints[queue_entry].data_buffer,
-					length);
+						qtd->data_buffer, length);
 			case OUT_PID:
 
 				urb->actual_length += length;
@@ -1295,7 +1290,6 @@ static void do_intl_int(struct usb_hcd *hcd)
 			}
 		}
 
-		priv->int_ints[queue_entry].data_buffer = NULL;
 		priv->int_ints[queue_entry].qtd = NULL;
 		priv->int_ints[queue_entry].qh = NULL;
 
@@ -1701,7 +1695,6 @@ static int isp1760_urb_dequeue(struct usb_hcd *hcd, struct urb *urb,
 
 			ints->qh = NULL;
 			ints->qtd = NULL;
-			ints->data_buffer = NULL;
 
 			isp1760_urb_done(priv, urb, status);
 			if (qtd)
