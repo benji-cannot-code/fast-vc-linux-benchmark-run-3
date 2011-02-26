53084d87cd6741e4983bc12dc932a947c805c63f
Manohar Vanga <manohar.vanga@cern.ch>
2011-02-27 00:15:35 +0100

    staging: vme: remove unreachable code
    Remove some more unreachable code found in bridges/vme_ca91cx42.c
    and bridges/vme_tsi148.c
    
    Signed-off-by: Manohar Vanga <manohar.vanga@cern.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vme/bridges/vme_ca91cx42.c
+++ drivers/staging/vme/bridges/vme_ca91cx42.c
@@ -561,8 +561,6 @@ static int ca91cx42_alloc_resource(struct vme_master_resource *image,
 
 	return 0;
 
-	iounmap(image->kern_base);
-	image->kern_base = NULL;
 err_remap:
 	release_resource(&image->bus_resource);
 err_resource:
@@ -1783,7 +1781,6 @@ static int ca91cx42_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 
 	return 0;
 
-	vme_unregister_bridge(ca91cx42_bridge);
 err_reg:
 	ca91cx42_crcsr_exit(ca91cx42_bridge, pdev);
 err_lm:
