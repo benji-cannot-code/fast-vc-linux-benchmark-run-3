755f2c2ddbfa899f7fac1496d7b9752bd2cfaaad
Justin P. Mattock <justinmattock@gmail.com>
2011-02-26 20:34:02 -0800

    drivers:staging:bcm:CmHost.c Remove one to many n's in a word.
    The Patch below removes one to many "n's" in a word..
    
    Signed-off-by: Justin P. Mattock <justinmattock@gmail.com>
    CC: Greg Kroah-Hartman <gregkh@suse.de>
    CC: Stephen Hemminger <shemminger@vyatta.com>
    CC: devel@driverdev.osuosl.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/bcm/CmHost.c
+++ drivers/staging/bcm/CmHost.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /************************************************************
 *			CMHOST.C
-*	This file contains the routines for handling Connnection
+*	This file contains the routines for handling Connection
 *	Management.
 ************************************************************/
 
