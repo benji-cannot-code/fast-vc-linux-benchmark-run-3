94f106ea989481cbbd6b785058850c4be29aa7c2
Zeng Zhaoming <b32542@freescale.com>
2011-02-28 03:45:21 +0800

    ASoC: remove unnecessary header including in SGTL5000 codec driver
    Remove unnecessary headers:
     - mach/hardware.h in sgtl5000.c
     - linux/i2c.h in sgtl5000.h
    
    Signed-off-by: Zeng Zhaoming <b32542@freescale.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/sgtl5000.c
+++ sound/soc/codecs/sgtl5000.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
 #include <sound/initval.h>
-#include <mach/hardware.h>
 
 #include "sgtl5000.h"
 
--- sound/soc/codecs/sgtl5000.h
+++ sound/soc/codecs/sgtl5000.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SGTL5000_H
 #define _SGTL5000_H
 
-#include <linux/i2c.h>
-
 /*
  * Register values.
  */
