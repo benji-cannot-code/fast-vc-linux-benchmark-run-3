aff4a231aff4837471b617a11a3d5e30bc4eb3a6
Shmulik Ravid <shmulikr@broadcom.com>
2011-02-28 12:19:55 -0800

    bnx2x: use dcb_setapp to manage negotiated application tlvs
    With this patch the bnx2x uses the generic dcbnl application tlv list
    instead of implementing its own get-app handler. When the driver is
    alerted to a change in the DCB negotiated parameters, it calls
    dcb_setapp to update the dcbnl application tlvs list making it available
    to user mode applications and registered notifiers.   
    
    Signed-off-by: Shmulik Ravid <shmulikr@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x/bnx2x.h
+++ drivers/net/bnx2x/bnx2x.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BNX2X_NEW_NAPI
 
 #if defined(CONFIG_DCB)
-#define BCM_DCB
+#define BCM_DCBNL
 #endif
 #if defined(CONFIG_CNIC) || defined(CONFIG_CNIC_MODULE)
 #define BCM_CNIC 1
