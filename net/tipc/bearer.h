064340dbafe3f416c9195c20250492925a300791
Allan Stephens <Allan.Stephens@windriver.com>
2011-02-28 15:03:22 -0500

    tipc: Remove unused field in bearer structure
    Eliminates a field in TIPC's bearer objects that is set, but never
    referenced.
    
    Signed-off-by: Allan Stephens <Allan.Stephens@windriver.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- net/tipc/bearer.h
+++ net/tipc/bearer.h
@@ -106,7 +106,6 @@ struct media {
  * @name: bearer name (format = media:interface)
  * @media: ptr to media structure associated with bearer
  * @priority: default link priority for bearer
- * @detect_scope: network address mask used during automatic link creation
  * @identity: array index of this bearer within TIPC bearer array
  * @link_req: ptr to (optional) structure making periodic link setup requests
  * @links: list of non-congested links associated with bearer
@@ -129,7 +128,6 @@ struct tipc_bearer {
 	spinlock_t lock;
 	struct media *media;
 	u32 priority;
-	u32 detect_scope;
 	u32 identity;
 	struct link_req *link_req;
 	struct list_head links;
