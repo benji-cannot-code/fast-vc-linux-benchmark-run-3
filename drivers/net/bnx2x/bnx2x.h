12b5a2482e1c443f311839f9fb2507c53321133e
Dmitry Kravkov <dmitry@broadcom.com>
2011-02-28 03:37:09 +0000

    bnx2x: update driver version to 1.62.00-6
    Signed-off-by: Dmitry Kravkov <dmitry@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x/bnx2x.h
+++ drivers/net/bnx2x/bnx2x.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * (you will need to reboot afterwards) */
 /* #define BNX2X_STOP_ON_ERROR */
 
-#define DRV_MODULE_VERSION      "1.62.00-5"
+#define DRV_MODULE_VERSION      "1.62.00-6"
 #define DRV_MODULE_RELDATE      "2011/01/30"
 #define BNX2X_BC_VER            0x040200
 
