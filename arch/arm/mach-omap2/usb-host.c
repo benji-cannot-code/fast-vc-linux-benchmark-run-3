0f45d64e0b7861c5e5fff39b2f15d07532e6f75c
Felipe Balbi <balbi@ti.com>
2011-03-01 20:08:15 +0530

    arm: omap2: usb: rename usb-ehci.c to usb-host.c
    We already have both EHCI and OHCI there, so let's
    rename to be sure everybody will understand the entire
    USB HOST functionality is setup on this file.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- arch/arm/mach-omap2/usb-host.c
+++ arch/arm/mach-omap2/usb-host.c
@@ -1,15 +1,15 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * linux/arch/arm/mach-omap2/usb-ehci.c
+ * usb-host.c - OMAP USB Host
  *
  * This file will contain the board specific details for the
- * Synopsys EHCI host controller on OMAP3430
+ * Synopsys EHCI/OHCI host controller on OMAP3430 and onwards
  *
- * Copyright (C) 2007 Texas Instruments
+ * Copyright (C) 2007-2011 Texas Instruments
  * Author: Vikram Pandita <vikram.pandita@ti.com>
  *
  * Generalization by:
- * Felipe Balbi <felipe.balbi@nokia.com>
+ * Felipe Balbi <balbi@ti.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
