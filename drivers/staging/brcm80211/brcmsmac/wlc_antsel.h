3c028bb943fd93da5f4452cf4e8935c7dfeeb8dd
Arend van Spriel <arend@broadcom.com>
2011-03-01 10:56:56 +0100

    staging: brcm80211: change prototype for wlc_antsel_attach
    wlc_antsel_attach was called with four parameters but actually
    three parameters were already provided in the first parameter.
    
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Brett Rudley <brudley@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/wlc_antsel.h
+++ drivers/staging/brcm80211/brcmsmac/wlc_antsel.h
@@ -17,10 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef _wlc_antsel_h_
 #define _wlc_antsel_h_
-extern struct antsel_info *wlc_antsel_attach(struct wlc_info *wlc,
-					struct osl_info *osh,
-					struct wlc_pub *pub,
-					struct wlc_hw_info *wlc_hw);
+
+extern struct antsel_info *wlc_antsel_attach(struct wlc_info *wlc);
 extern void wlc_antsel_detach(struct antsel_info *asi);
 extern void wlc_antsel_init(struct antsel_info *asi);
 extern void wlc_antsel_antcfg_get(struct antsel_info *asi, bool usedef,
@@ -28,4 +26,5 @@ extern void wlc_antsel_antcfg_get(struct antsel_info *asi, bool usedef,
 				  u8 id, u8 fbid, u8 *antcfg,
 				  u8 *fbantcfg);
 extern u8 wlc_antsel_antsel2id(struct antsel_info *asi, u16 antsel);
-#endif				/* _wlc_antsel_h_ */
+
+#endif /* _wlc_antsel_h_ */
