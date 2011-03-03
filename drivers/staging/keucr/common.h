3a94ebbb50158fc6b45340a8de6d6061e19b2a9b
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2011-03-03 14:25:07 +0100

    staging: keucr: remove unused typedef VOID
    Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/keucr/common.h
+++ drivers/staging/keucr/common.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef COMMON_INCD
 #define COMMON_INCD
 
-typedef void VOID;
 typedef u8 BOOLEAN;
 typedef u8 BYTE;
 typedef u8 *PBYTE;
