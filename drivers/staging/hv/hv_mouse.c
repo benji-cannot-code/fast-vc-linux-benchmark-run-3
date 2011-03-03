a7ab4a609392129c62720139ef3bc65f0336367b
Greg Kroah-Hartman <gregkh@suse.de>
2011-03-02 19:08:05 -0800

    Staging: hv: hv_mouse: fix build warning
    The trans_id variable (u64) was being incorrectly cast to a unsigned
    long * when it should have just been unsigned long.
    
    Fun with pointers, what a fricken mess, we need some real type safety
    for these types of fields somehow...
    
    Cc: Hank Janssen <hjanssen@microsoft.com>
    Cc: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/hv_mouse.c
+++ drivers/staging/hv/hv_mouse.c
@@ -331,7 +331,7 @@ static void MousevscOnSendCompletion(struct hv_device *Device, struct vmpacket_d
 		return;
 	}
 
-	request = (void *)(unsigned long *)Packet->trans_id;
+	request = (void *)(unsigned long)Packet->trans_id;
 
 	if (request == &inputDevice->ProtocolReq) {
 		/* FIXME */
