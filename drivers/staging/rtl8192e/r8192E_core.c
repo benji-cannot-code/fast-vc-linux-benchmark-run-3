27ef7686caa6acad4482011588d9872ec645b27c
Mike McCormack <mikem@ring3k.org>
2011-03-03 22:46:17 +0900

    staging: rtl8192e: Pass r8192_priv to MgntActSet_RF_State
    Signed-off-by: Mike McCormack <mikem@ring3k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8192e/r8192E_core.c
+++ drivers/staging/rtl8192e/r8192E_core.c
@@ -1828,12 +1828,14 @@ static short rtl8192_is_tx_queue_empty(struct net_device *dev)
 
 static void rtl8192_hw_sleep_down(struct net_device *dev)
 {
-	MgntActSet_RF_State(dev, eRfSleep, RF_CHANGE_BY_PS);
+	struct r8192_priv *priv = ieee80211_priv(dev);
+	MgntActSet_RF_State(priv, eRfSleep, RF_CHANGE_BY_PS);
 }
 
 static void rtl8192_hw_wakeup(struct net_device* dev)
 {
-	MgntActSet_RF_State(dev, eRfOn, RF_CHANGE_BY_PS);
+	struct r8192_priv *priv = ieee80211_priv(dev);
+	MgntActSet_RF_State(priv, eRfOn, RF_CHANGE_BY_PS);
 }
 
 static void rtl8192_hw_wakeup_wq (struct work_struct *work)
@@ -2810,12 +2812,12 @@ static RT_STATUS rtl8192_adapter_start(struct r8192_priv *priv)
 	if(priv->RfOffReason > RF_CHANGE_BY_PS)
 	{ // H/W or S/W RF OFF before sleep.
 		RT_TRACE((COMP_INIT|COMP_RF|COMP_POWER), "%s(): Turn off RF for RfOffReason(%d)\n", __FUNCTION__,priv->RfOffReason);
-		MgntActSet_RF_State(dev, eRfOff, priv->RfOffReason);
+		MgntActSet_RF_State(priv, eRfOff, priv->RfOffReason);
 	}
 	else if(priv->RfOffReason >= RF_CHANGE_BY_IPS)
 	{ // H/W or S/W RF OFF before sleep.
 		RT_TRACE((COMP_INIT|COMP_RF|COMP_POWER), "%s(): Turn off RF for RfOffReason(%d)\n",  __FUNCTION__, priv->RfOffReason);
-		MgntActSet_RF_State(dev, eRfOff, priv->RfOffReason);
+		MgntActSet_RF_State(priv, eRfOff, priv->RfOffReason);
 	}
 	else
 	{
@@ -3060,8 +3062,6 @@ rtl819x_ifcheck_resetornot(struct r8192_priv *priv)
 #ifdef ENABLE_IPS
 static void InactivePsWorkItemCallback(struct r8192_priv *priv)
 {
-	struct net_device *dev = priv->ieee80211->dev;
-
 	PRT_POWER_SAVE_CONTROL pPSC = &priv->PowerSaveControl;
 
 	RT_TRACE(COMP_POWER, "InactivePsWorkItemCallback() --------->\n");
@@ -3079,7 +3079,7 @@ static void InactivePsWorkItemCallback(struct r8192_priv *priv)
 			pPSC->eInactivePowerState == eRfOff?"OFF":"ON");
 
 
-	MgntActSet_RF_State(dev, pPSC->eInactivePowerState, RF_CHANGE_BY_IPS);
+	MgntActSet_RF_State(priv, pPSC->eInactivePowerState, RF_CHANGE_BY_IPS);
 
 	//
 	// To solve CAM values miss in RF OFF, rewrite CAM values after RF ON. By Bruce, 2007-09-20.
@@ -3452,7 +3452,7 @@ static int _rtl8192_up(struct r8192_priv *priv)
 	RT_TRACE(COMP_INIT, "start adapter finished\n");
 
 	if (priv->eRFPowerState != eRfOn)
-		MgntActSet_RF_State(dev, eRfOn, priv->RfOffReason);
+		MgntActSet_RF_State(priv, eRfOn, priv->RfOffReason);
 
 	if(priv->ieee80211->state != IEEE80211_LINKED)
 	ieee80211_softmac_start_protocol(priv->ieee80211);
