ecfb00b8449048e04aaf29fbbee10ae2ff18699c
omar ramirez <omar.ramirez@ti.com>
2011-03-04 13:32:44 -0700

    OMAP2+: hwmod: use status bit info for reset line
    On OMAP2 and OMAP3 the reset ctrl shift doesn't match the
    status bit, as it does on OMAP4, when handling the reset lines.
    
    This patch adds a new member in the reset info structure, so now it
    can be added as part of hwmod data, and checked accordingly for
    OMAP2 or 3; otherwise, there could be cases when the shift masks
    doesn't match both of the registers, and a successful reset might
    throw an error message or vice versa.
    
    Signed-off-by: Omar Ramirez Luna <omar.ramirez@ti.com>
    [paul@pwsan.com: added a warning if st_shift used on OMAP4; renamed 'r'
     variable; improved some documentation]
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/plat-omap/include/plat/omap_hwmod.h
+++ arch/arm/plat-omap/include/plat/omap_hwmod.h
@@ -125,6 +125,7 @@ struct omap_hwmod_dma_info {
  * struct omap_hwmod_rst_info - IPs reset lines use by hwmod
  * @name: name of the reset line (module local name)
  * @rst_shift: Offset of the reset bit
+ * @st_shift: Offset of the reset status bit (OMAP2/3 only)
  *
  * @name should be something short, e.g., "cpu0" or "rst". It is defined
  * locally to the hwmod.
@@ -132,6 +133,7 @@ struct omap_hwmod_dma_info {
 struct omap_hwmod_rst_info {
 	const char	*name;
 	u8		rst_shift;
+	u8		st_shift;
 };
 
 /**
