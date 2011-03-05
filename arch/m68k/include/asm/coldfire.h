66b9daa0c60dfc2a65f29e077b9725db1c353d94
Greg Ungerer <gerg@uclinux.org>
2011-03-05 22:48:52 +1000

    m68knommu: remove bogus definition of MBAR for ColdFire 532x family
    Remove the bogus definition of the MBAR register for the ColdFire 532x
    family. It doesn't have an MBAR register, its peripheral registers are
    at fixed addresses and are not relative to a settable base.
    
    All the code that relyed on this definition existing has been cleaned
    up. The register address definitions now include the base as required.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/coldfire.h
+++ arch/m68k/include/asm/coldfire.h
@@ -42,9 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
     defined(CONFIG_M520x)
 #undef MCF_MBAR
 #define	MCF_MBAR	MCF_IPSBAR
-#elif defined(CONFIG_M532x)
-#undef MCF_MBAR
-#define MCF_MBAR	0x00000000
 #endif
 
 /****************************************************************************/
