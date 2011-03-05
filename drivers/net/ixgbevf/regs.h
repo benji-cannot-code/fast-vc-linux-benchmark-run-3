9da53d12c051b740d6ed61d09d887637d7132585
Emil Tantilov <emil.s.tantilov@intel.com>
2011-03-05 08:02:18 +0000

    ixgbe: correct typo in define name
    VF Free Running Timer register name missing an F.
    
    Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>
    Acked-by: Greg Rose <gregory.v.rose@intel.com>
    Tested-by: Evan Swanson <evan.swanson@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ixgbevf/regs.h
+++ drivers/net/ixgbevf/regs.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IXGBE_VFCTRL           0x00000
 #define IXGBE_VFSTATUS         0x00008
 #define IXGBE_VFLINKS          0x00010
-#define IXGBE_VFRTIMER         0x00048
+#define IXGBE_VFFRTIMER        0x00048
 #define IXGBE_VFRXMEMWRAP      0x03190
 #define IXGBE_VTEICR           0x00100
 #define IXGBE_VTEICS           0x00104
