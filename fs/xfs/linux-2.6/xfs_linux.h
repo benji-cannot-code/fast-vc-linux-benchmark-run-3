12ba7da60375db8e40f38e6548b983da77a8d3c7
Dave Chinner <dchinner@redhat.com>
2011-03-07 10:09:35 +1100

    xfs: kill support/debug.[ch]
    The remaining functionality in debug.[ch] is effectively just assert
    handling, conditional debug definitions and hex dumping. The hex
    dumping and assert function can be moved into the new printk module,
    while the rest can be moved into top-level header files. This allows
    fs/xfs/support/debug.[ch] to be completely removed from the
    codebase.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Alex Elder <aelder@sgi.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/linux-2.6/xfs_linux.h
+++ fs/xfs/linux-2.6/xfs_linux.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mrlock.h>
 #include <time.h>
 
-#include <support/debug.h>
 #include <support/uuid.h>
 
 #include <linux/semaphore.h>
@@ -281,5 +280,26 @@ static inline __uint64_t howmany_64(__uint64_t x, __uint32_t y)
 #else
 #define __arch_pack
 #endif
+
+#define ASSERT_ALWAYS(expr)	\
+	(unlikely(expr) ? (void)0 : assfail(#expr, __FILE__, __LINE__))
+
+#ifndef DEBUG
+#define ASSERT(expr)	((void)0)
+
+#ifndef STATIC
+# define STATIC static noinline
+#endif
+
+#else /* DEBUG */
+
+#define ASSERT(expr)	\
+	(unlikely(expr) ? (void)0 : assfail(#expr, __FILE__, __LINE__))
+
+#ifndef STATIC
+# define STATIC noinline
+#endif
+
+#endif /* DEBUG */
 
 #endif /* __XFS_LINUX__ */
