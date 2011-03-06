f8c8be2733156664b8dddb116710941f13e1d969
Shahar Levi <shahar_levi@ti.com>
2011-03-06 16:32:08 +0200

    wl12xx: 1281/1283 support - Add acx commands
    New acx command that sets: Rx fifo enable reduced bus transactions
    in RX path. Tx bus transactions padding to SDIO block size that
    improve preference in Tx and essential for working with SDIO HS (48Mhz).
    The max SDIO block size is 256 when working with Tx bus transactions
    padding to SDIO block.
    
    Add new ops to SDIO & SPI that handles the win size change in case of
    transactions padding (relevant only for SDIO).
    
    [Fix endianess issues; simplify sdio-specific block_size handling;
    minor changes in comments; use "aligned_len" in one calculation
    instead of "pad" to avoid confusion -- Luca]
    
    Signed-off-by: Shahar Levi <shahar_levi@ti.com>
    Reviewed-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/io.c
+++ drivers/net/wireless/wl12xx/io.c
@@ -44,6 +44,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OCP_STATUS_REQ_FAILED 0x20000
 #define OCP_STATUS_RESP_ERROR 0x30000
 
+bool wl1271_set_block_size(struct wl1271 *wl)
+{
+	if (wl->if_ops->set_block_size) {
+		wl->if_ops->set_block_size(wl);
+		return true;
+	}
+
+	return false;
+}
+
 void wl1271_disable_interrupts(struct wl1271 *wl)
 {
 	wl->if_ops->disable_irq(wl);
