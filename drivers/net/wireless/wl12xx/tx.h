eb674cfd139da141954a00f8d0dd78fccc4c3629
Shahar Levi <shahar_levi@ti.com>
2011-03-06 16:32:14 +0200

    wl12xx: 1281/1283 support - Add dummy packet support
    Support sending dummy packet to wl128x FW as results of
    dummy packet event. That is part of dynamic TX mem blocks mechanism.
    
    Only send dummy packet when not in AP mode.
    
    [Even though the DUMMY_PACKET_EVENT_ID and the
    STA_REMOVE_COMPLETE_EVENT_ID events are defined to the same value, we
    need to treat them separately in the code.  Keep the check and enable
    STA_REMOVE_COMPLETE_EVENT_ID for AP mode and DUMMY_PACKET_EVENT_ID for
    STA.  Moved one warning to a cleaner place.  Use WL1271_TID_MGMT for
    dummy packets -- Luca]
    
    Signed-off-by: Shahar Levi <shahar_levi@ti.com>
    Reviewed-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/tx.h
+++ drivers/net/wireless/wl12xx/tx.h
@@ -42,6 +42,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 					  BIT(8) | BIT(9))
 #define TX_HW_ATTR_LAST_WORD_PAD         (BIT(10) | BIT(11))
 #define TX_HW_ATTR_TX_CMPLT_REQ          BIT(12)
+#define TX_HW_ATTR_TX_DUMMY_REQ          BIT(13)
+
+#define TX_PKT_TYPE_DUMMY_REQ            5
 
 #define TX_HW_ATTR_OFST_SAVE_RETRIES     0
 #define TX_HW_ATTR_OFST_HEADER_PAD       1
@@ -55,6 +58,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define WL1271_TX_ALIGN_TO 4
 #define WL1271_TKIP_IV_SPACE 4
+
+/* Used for management frames and dummy packets */
+#define WL1271_TID_MGMT 7
 
 struct wl127x_tx_mem {
 	/*
