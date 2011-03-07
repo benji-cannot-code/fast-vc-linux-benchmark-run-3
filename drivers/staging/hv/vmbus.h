0ec7720624b20beaeaaf36809009414c151f80ed
K. Y. Srinivasan <kys@microsoft.com>
2011-03-07 13:34:48 -0800

    Staging: hv: Remove probe_error from vm_device
    In preparation for consolidating all device related state into
    struct hv_device, move probe_error from vm_device to
    hv_device.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/vmbus.h
+++ drivers/staging/hv/vmbus.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct vm_device {
 	struct hv_guid class_id;
 	struct hv_guid device_id;
-	int probe_error;
 	struct hv_device device_obj;
 	struct device device;
 };
