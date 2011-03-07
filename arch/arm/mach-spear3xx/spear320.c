716224ec8789b2a77b2c93c0c25ec611fa1eabd2
viresh kumar <viresh.kumar@st.com>
2011-03-07 05:57:05 +0100

    ARM: 6791/1: SPEAr3xx: Declare device structures after shirq code
    Order of declarations should be: pmx_devs, shirq support, amba_devices,
    plat_devices, routines. This patch moves gpio_device below shirq support.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-spear3xx/spear320.c
+++ arch/arm/mach-spear3xx/spear320.c
@@ -385,8 +385,6 @@ struct pmx_driver pmx_driver = {
 	.mux_reg = {.offset = PAD_MUX_CONFIG_REG, .mask = 0x00007fff},
 };
 
-/* Add spear320 specific devices here */
-
 /* spear3xx shared irq */
 struct shirq_dev_config shirq_ras1_config[] = {
 	{
@@ -510,6 +508,8 @@ struct spear_shirq shirq_intrcomm_ras = {
 		.reset_to_clear = 1,
 	},
 };
+
+/* Add spear320 specific devices here */
 
 /* spear320 routines */
 void __init spear320_init(void)
