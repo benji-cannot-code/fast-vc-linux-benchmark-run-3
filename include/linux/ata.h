cdf28384c426e9dcd2ddea8aa8f8eaccdae88a06
Hannes Reinecke <hare@suse.de>
2011-03-07 08:56:44 +0100

    libata: Include WWN ID in inquiry VPD emulation
    As per SAT-3 the WWN ID should be included in the VPD page 0x83
    (device identification) emulation.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- include/linux/ata.h
+++ include/linux/ata.h
@@ -90,6 +90,7 @@ enum {
 	ATA_ID_SPG		= 98,
 	ATA_ID_LBA_CAPACITY_2	= 100,
 	ATA_ID_SECTOR_SIZE	= 106,
+	ATA_ID_WWN		= 108,
 	ATA_ID_LOGICAL_SECTOR_SIZE	= 117,	/* and 118 */
 	ATA_ID_LAST_LUN		= 126,
 	ATA_ID_DLF		= 128,
@@ -104,6 +105,7 @@ enum {
 	ATA_ID_SERNO_LEN	= 20,
 	ATA_ID_FW_REV_LEN	= 8,
 	ATA_ID_PROD_LEN		= 40,
+	ATA_ID_WWN_LEN		= 8,
 
 	ATA_PCI_CTL_OFS		= 2,
 
@@ -814,6 +816,11 @@ static inline int ata_id_has_unload(const u16 *id)
 	    id[ATA_ID_CFSSE] & (1 << 13))
 		return 1;
 	return 0;
+}
+
+static inline bool ata_id_has_wwn(const u16 *id)
+{
+	return (id[ATA_ID_CSF_DEFAULT] & 0xC100) == 0x4100;
 }
 
 static inline int ata_id_form_factor(const u16 *id)
