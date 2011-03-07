41b608cb4f89fe74016f70c16d31c368b8e0d8c2
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2011-03-07 21:59:45 +0000

    net: sh_eth: modify the PHY_INTERFACE_MODE
    The previous code had hardcoded the PHY_INTERFACE_MODE_MII of phy_connect.
    So some Gigabit PHYs will not behave correctly.
    The patch adds the phy_interface in sh_eth_plat_data, so we can select
    the phy interface.
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sh/include/asm/sh_eth.h
+++ arch/sh/include/asm/sh_eth.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SH_ETH_H__
 #define __ASM_SH_ETH_H__
 
+#include <linux/phy.h>
+
 enum {EDMAC_LITTLE_ENDIAN, EDMAC_BIG_ENDIAN};
 enum {
 	SH_ETH_REG_GIGABIT,
@@ -13,6 +15,7 @@ struct sh_eth_plat_data {
 	int phy;
 	int edmac_endian;
 	int register_type;
+	phy_interface_t phy_interface;
 
 	unsigned char mac_addr[6];
 	unsigned no_ether_link:1;
