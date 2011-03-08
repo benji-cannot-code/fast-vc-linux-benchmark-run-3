7d1be7279436007ed3e98c7d29a2a69c59f3ef4b
Paul Walmsley <paul@pwsan.com>
2011-03-07 19:28:15 -0700

    OMAP2/3: WKUP powerdomain: mark as being always on
    Mark the WKUP powerdomain as being always on -- at least, as long as the
    chip has power.  This will be used to enable the powerdomain code to
    determine whether a given powerdomain is ever able to power off.  While
    here, update the file copyright.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/powerdomains2xxx_3xxx_data.c
+++ arch/arm/mach-omap2/powerdomains2xxx_3xxx_data.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OMAP2/3 common powerdomain definitions
  *
  * Copyright (C) 2007-2008 Texas Instruments, Inc.
- * Copyright (C) 2007-2010 Nokia Corporation
+ * Copyright (C) 2007-2011 Nokia Corporation
  *
  * Paul Walmsley, Jouni Högander
  *
@@ -77,4 +77,5 @@ struct powerdomain wkup_omap2_pwrdm = {
 	.name		= "wkup_pwrdm",
 	.prcm_offs	= WKUP_MOD,
 	.omap_chip	= OMAP_CHIP_INIT(CHIP_IS_OMAP24XX | CHIP_IS_OMAP3430),
+	.pwrsts		= PWRSTS_ON,
 };
