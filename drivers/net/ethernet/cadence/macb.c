5d02923eec652e7579680c8a4516f2ca87274313
Jamie Iles <jamie@jamieiles.com>
2011-03-08 20:17:06 +0000

    macb: unify at91 and avr32 platform data
    Both at91 and avr32 defines its own platform data structure for
    the macb driver and both share common structures though at91
    includes a currently unused phy_irq_pin.  Create a common
    macb_platform_data for macb that both at91 and avr32 can use.  In
    future we can use this to support other architectures that use the
    same IP block with the macb driver.
    
    v2: rename eth_platform_data to macb_platform_data and allow at91_ether
    to share the platform data with macb.
    
    Signed-off-by: Jamie Iles <jamie@jamieiles.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Tested-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- drivers/net/ethernet/cadence/macb.c
+++ drivers/net/ethernet/cadence/macb.c
@@ -20,11 +20,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 #include <linux/dma-mapping.h>
+#include <linux/platform_data/macb.h>
 #include <linux/platform_device.h>
 #include <linux/phy.h>
-
-#include <mach/board.h>
-#include <mach/cpu.h>
 
 #include "macb.h"
 
@@ -192,7 +190,7 @@ static int macb_mii_probe(struct net_device *dev)
 {
 	struct macb *bp = netdev_priv(dev);
 	struct phy_device *phydev;
-	struct eth_platform_data *pdata;
+	struct macb_platform_data *pdata;
 	int ret;
 
 	phydev = phy_find_first(bp->mii_bus);
@@ -229,7 +227,7 @@ static int macb_mii_probe(struct net_device *dev)
 
 static int macb_mii_init(struct macb *bp)
 {
-	struct eth_platform_data *pdata;
+	struct macb_platform_data *pdata;
 	int err = -ENXIO, i;
 
 	/* Enable management port */
@@ -1120,7 +1118,7 @@ static const struct net_device_ops macb_netdev_ops = {
 
 static int __init macb_probe(struct platform_device *pdev)
 {
-	struct eth_platform_data *pdata;
+	struct macb_platform_data *pdata;
 	struct resource *regs;
 	struct net_device *dev;
 	struct macb *bp;
