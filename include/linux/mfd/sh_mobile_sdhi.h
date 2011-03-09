e56c131421774ee833158624d4d2f3992f0ebe4a
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2011-03-09 11:33:35 +0100

    mfd: remove now redundant sh_mobile_sdhi.h header
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Acked-by: Paul Mundt <lethal@linux-sh.org>
    Acked-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mfd/sh_mobile_sdhi.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef MFD_SH_MOBILE_SDHI_H
-#define MFD_SH_MOBILE_SDHI_H
-
-/* Compatibility header - will disappear once all platforms are converted */
-#include <linux/mmc/sh_mobile_sdhi.h>
-
-#endif /* MFD_SH_MOBILE_SDHI_H */
