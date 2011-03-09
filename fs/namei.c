f0852ef2ebdd636a42bfccfc525e74730d967823
Al Viro <viro@zeniv.linux.org.uk>
2011-03-08 21:16:28 -0500

    nd->inode is not set on the second attempt in path_walk()
    We leave it at whatever it had been pointing to after the
    first link_path_walk() had failed with -ESTALE.  Things
    do not work well after that...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -1547,6 +1547,7 @@ static int path_walk(const char *name, struct nameidata *nd)
 		/* nd->path had been dropped */
 		current->total_link_count = 0;
 		nd->path = save;
+		nd->inode = save.dentry->d_inode;
 		path_get(&nd->path);
 		nd->flags |= LOOKUP_REVAL;
 		result = link_path_walk(name, nd);
