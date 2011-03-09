aad1decfa843b0536653ccbeec013527e62dde07
Mike Rapoport <mike@compulab.co.il>
2011-03-09 16:31:18 +0200

    ARM: tegra: harmony: initialize the TPS65862 PMIC
    Initialize the PMIC voltage regulators and provide the supply map for
    PCI-e clock supply. The rest of the supplies should be added together
    with the drivers that use them.
    
    Signed-off-by: Mike Rapoport <mike@compulab.co.il>
    CC: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-tegra/board-harmony.h
+++ arch/arm/mach-tegra/board-harmony.h
@@ -33,5 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TEGRA_GPIO_EXT_MIC_EN		TEGRA_GPIO_PX1
 
 void harmony_pinmux_init(void);
+int harmony_regulator_init(void);
 
 #endif
