e0d33ebd78692cee0f2689d35185a0337a6f07c6
Oleg Drokin <green@linuxhacker.ru>
2011-03-11 04:17:27 +0300

    firewire: ohci: Misleading kfree in ohci.c::pci_probe/remove
    It seems drivers/firewire/ohci.c is making some optimistic assumptions
    about struct fw_ohci and that member "card" will always remain the first
    member of the struct.
    Plus it's probably going to confuse a lot of static code analyzers too.
    
    So I wonder if there is a good reason not to free the ohci struct just
    like it was allocated instead of the tricky &ohci->card way?
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    
    It is perhaps just a rudiment from before mainline submission of the
    driver.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/ohci.c
+++ drivers/firewire/ohci.c
@@ -3310,7 +3310,7 @@ static int __devinit pci_probe(struct pci_dev *dev,
  fail_disable:
 	pci_disable_device(dev);
  fail_free:
-	kfree(&ohci->card);
+	kfree(ohci);
 	pmac_ohci_off(dev);
  fail:
 	if (err == -ENOMEM)
@@ -3354,7 +3354,7 @@ static void pci_remove(struct pci_dev *dev)
 	pci_iounmap(dev, ohci->registers);
 	pci_release_region(dev, 0);
 	pci_disable_device(dev);
-	kfree(&ohci->card);
+	kfree(ohci);
 	pmac_ohci_off(dev);
 
 	fw_notify("Removed fw-ohci device.\n");
