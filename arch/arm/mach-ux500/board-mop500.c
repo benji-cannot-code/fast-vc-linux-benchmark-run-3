160926363af856017de00b2060ef981f7fc5c2bd
Bengt Jonsson <bengt.g.jonsson@stericsson.com>
2011-03-11 11:54:46 +0100

    regulator: initialization for ab8500 regulators
    The regulators on the AB8500 have a lot of custom
    hardware control settings pertaining to 8 external
    signals, settings which are board-specific and need
    be provided from the platform at startup.
    
    Initialization added for regulators Vana, VextSupply1,
    VextSupply2, VextSupply3, Vaux1, Vaux2, Vaux3, VTVout,
    Vintcore12, Vaudio, Vdmic, Vamic1, Vamic2, VrefDDR.
    
    Signed-off-by: Bengt Jonsson <bengt.g.jonsson@stericsson.com>
    Reviewed-by: Rickard Andersson <rickard.andersson@stericsson.com>
    Reviewed-by: Jonas Aberg <jonas.aberg@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/amba/serial.h>
 #include <linux/spi/spi.h>
 #include <linux/mfd/ab8500.h>
+#include <linux/regulator/ab8500.h>
 #include <linux/mfd/tc3589x.h>
 #include <linux/leds-lp5521.h>
 #include <linux/input.h>
