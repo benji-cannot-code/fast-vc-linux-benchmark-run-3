7fa93b2cd33f0afac6f0cb5cd63a77f9cf14c632
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-03-13 12:26:27 +0200

    staging: xgifb: clean up register access types
    Make type usage consistent. Use u8 for HW registers and unsigned for
    bitmasks.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_util.h
+++ drivers/staging/xgifb/vb_util.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _VBUTIL_
 #define _VBUTIL_
-extern void xgifb_reg_set(unsigned long, unsigned short, unsigned short);
-extern unsigned char xgifb_reg_get(unsigned long, unsigned short);
-extern void xgifb_reg_or(unsigned long, unsigned short, unsigned short);
-extern void xgifb_reg_and(unsigned long, unsigned short, unsigned short);
-extern void xgifb_reg_and_or(unsigned long, unsigned short, unsigned short, unsigned short);
+extern void xgifb_reg_set(unsigned long, u8, u8);
+extern u8 xgifb_reg_get(unsigned long, u8);
+extern void xgifb_reg_or(unsigned long, u8, unsigned);
+extern void xgifb_reg_and(unsigned long, u8, unsigned);
+extern void xgifb_reg_and_or(unsigned long, u8, unsigned, unsigned);
 #endif
 
