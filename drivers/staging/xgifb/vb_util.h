47b51ca682498d7c8f1f79ad3a949cdf1cbe1026
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-03-13 12:26:18 +0200

    staging: xgifb: replace XGINew_GetReg2() with inb()
    Replace XGINew_GetReg2() with inb().
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_util.h
+++ drivers/staging/xgifb/vb_util.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern   void     XGINew_SetReg1(unsigned long, unsigned short, unsigned short);
 extern   void     XGINew_SetReg3(unsigned long, unsigned short);
 extern    unsigned char     XGINew_GetReg1(unsigned long, unsigned short);
-extern    unsigned char     XGINew_GetReg2(unsigned long);
 extern   void     XGINew_SetReg4(unsigned long, unsigned long);
 extern   unsigned long    XGINew_GetReg3(unsigned long);
 extern   void     XGINew_SetRegOR(unsigned long Port,unsigned short Index,unsigned short DataOR);
