f20aff9b458380fc0211d0634b0212a9f6136fa6
Luis R. Rodriguez <lrodriguez@atheros.com>
2011-03-14 10:59:08 -0700

    ath6kl: remove-typedef ST_PS_DATA_FORMAT
    remove-typedef -s ST_PS_DATA_FORMAT \
    	"struct st_ps_data_format" drivers/staging/ath6kl/
    
    Tested-by: Naveen Singh <nsingh@atheros.com>
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/miscdrv/ar3kps/ar3kpsparser.c
+++ drivers/staging/ath6kl/miscdrv/ar3kps/ar3kpsparser.c
@@ -101,10 +101,10 @@ typedef struct tRamPatch
 
 
 
-typedef struct ST_PS_DATA_FORMAT {
+struct st_ps_data_format {
    enum eType   eDataType;
    bool    bIsArray;
-}ST_PS_DATA_FORMAT;
+};
 
 typedef struct ST_READ_STATUS {
     unsigned uTagID;
@@ -147,7 +147,7 @@ char AthReadChar(u8 *buffer, u32 len,u32 *pos)
     }
 }
 /* PS parser helper function */
-unsigned int uGetInputDataFormat(char *pCharLine, ST_PS_DATA_FORMAT *pstFormat)
+unsigned int uGetInputDataFormat(char *pCharLine, struct st_ps_data_format *pstFormat)
 {
     if(pCharLine[0] != '[') {
         pstFormat->eDataType = eHex;
@@ -287,7 +287,7 @@ unsigned int uGetInputDataFormat(char *pCharLine, ST_PS_DATA_FORMAT *pstFormat)
     }
 }
 
-unsigned int uReadDataInSection(char *pCharLine, ST_PS_DATA_FORMAT stPS_DataFormat)
+unsigned int uReadDataInSection(char *pCharLine, struct st_ps_data_format stPS_DataFormat)
 {
     char *pTokenPtr = pCharLine;
 
@@ -328,7 +328,7 @@ int AthParseFilesUnified(u8 *srcbuffer,u32 srclen, int FileFormat)
 
 
    int uReadCount;
-   ST_PS_DATA_FORMAT stPS_DataFormat;
+   struct st_ps_data_format stPS_DataFormat;
    ST_READ_STATUS   stReadStatus = {0, 0, 0,0};
    pos = 0;
    Buffer = NULL;
