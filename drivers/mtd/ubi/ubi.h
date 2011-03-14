dc03e28b0df156db0758868277592be93a12bf6c
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2011-03-14 17:06:52 +0200

    UBI: allocate write checking buffer on demand
    Instead of using pre-allocated 'ubi->dbg_peb_buf' buffer in
    'ubi_dbg_check_write()', dynamically allocate it when needed. The
    intend is to get rid of the pre-allocated 'ubi->dbg_peb_buf' buffer
    completely. And the need for this arises because we want to change
    to dynamic debugging control instead of compile-time control, i.e.,
    we are going to kill the CONFIG_MTD_UBI_DEBUG_PARANOID Kconfig
    option, which would mean that 'ubi->dbg_peb_buf' is always allocated,
    which would be wasteful.
    
    Thus, we are getting rid of 'ubi->dbg_peb_buf', and this is a
    preparation for that.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/notifier.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/ubi.h>
+#include <asm/pgtable.h>
 
 #include "ubi-media.h"
 #include "scan.h"
