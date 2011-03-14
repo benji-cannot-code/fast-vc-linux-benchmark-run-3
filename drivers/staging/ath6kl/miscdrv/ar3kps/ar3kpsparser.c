17a5ec9146b90402187c321b3ac2bd5ea5ba3541
Luis R. Rodriguez <lrodriguez@atheros.com>
2011-03-14 10:59:09 -0700

    ath6kl: remove-typedef ST_READ_STATUS
    remove-typedef -s ST_READ_STATUS \
    	"struct st_read_status" drivers/staging/ath6kl/
    
    Tested-by: Naveen Singh <nsingh@atheros.com>
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/miscdrv/ar3kps/ar3kpsparser.c
+++ drivers/staging/ath6kl/miscdrv/ar3kps/ar3kpsparser.c
@@ -106,13 +106,13 @@ struct st_ps_data_format {
    bool    bIsArray;
 };
 
-typedef struct ST_READ_STATUS {
+struct st_read_status {
     unsigned uTagID;
     unsigned uSection;
     unsigned uLineCount;
     unsigned uCharCount;
     unsigned uByteCount;
-}ST_READ_STATUS;
+};
 
 
 /* Stores the number of PS Tags */
@@ -329,7 +329,7 @@ int AthParseFilesUnified(u8 *srcbuffer,u32 srclen, int FileFormat)
 
    int uReadCount;
    struct st_ps_data_format stPS_DataFormat;
-   ST_READ_STATUS   stReadStatus = {0, 0, 0,0};
+   struct st_read_status   stReadStatus = {0, 0, 0,0};
    pos = 0;
    Buffer = NULL;
 
