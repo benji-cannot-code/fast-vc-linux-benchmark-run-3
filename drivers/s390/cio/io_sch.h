ded50729e6f4253dfe7e0ef145ea78e4a0a64f23
Sebastian Ott <sebott@linux.vnet.ibm.com>
2011-03-15 17:08:25 +0100

    [S390] cio: move options to io_sch_private
    Move the options member from struct subchannel to
    io_subchannel_private since it's I/O subchannel specific.
    Also remove the unused cio_get_options function.
    
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/cio/io_sch.h
+++ drivers/s390/cio/io_sch.h
@@ -11,7 +11,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct io_subchannel_private {
 	union orb orb;		/* operation request block */
 	struct ccw1 sense_ccw;	/* static ccw for sense command */
-} __attribute__ ((aligned(8)));
+	struct {
+		unsigned int suspend:1;	/* allow suspend */
+		unsigned int prefetch:1;/* deny prefetch */
+		unsigned int inter:1;	/* suppress intermediate interrupts */
+	} __packed options;
+} __aligned(8);
 
 #define to_io_private(n) ((struct io_subchannel_private *)n->private)
 #define sch_get_cdev(n) (dev_get_drvdata(&n->dev))
