b573d49356e876e1b52603b9a9d07844d35d424e
Clemens Ladisch <clemens@ladisch.de>
2011-03-15 07:57:24 +0100

    ALSA: firewire-lib, firewire-speakers: handle packet queueing errors
    Add an AMDTP stream error state that occurs when we fail to queue
    another packet.  In this case, the stream is stopped, and the error can
    be reported when the application tries to restart the PCM stream.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/amdtp.h
+++ sound/firewire/amdtp.h
@@ -57,7 +57,7 @@ struct amdtp_out_stream {
 
 	struct snd_pcm_substream *pcm;
 
-	unsigned int packet_counter;
+	int packet_index;
 	unsigned int data_block_counter;
 
 	unsigned int data_block_state;
@@ -109,6 +109,18 @@ static inline void amdtp_out_stream_set_midi(struct amdtp_out_stream *s,
 					     unsigned int midi_ports)
 {
 	s->midi_ports = midi_ports;
+}
+
+/**
+ * amdtp_out_streaming_error - check for streaming error
+ * @s: the AMDTP output stream
+ *
+ * If this function returns true, the stream's packet queue has stopped due to
+ * an asynchronous error.
+ */
+static inline bool amdtp_out_streaming_error(struct amdtp_out_stream *s)
+{
+	return s->packet_index < 0;
 }
 
 /**
