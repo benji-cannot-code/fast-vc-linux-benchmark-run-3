3e170c85974980fc96873a2d068181aa202e7994
Stephen Rothwell <sfr@canb.auug.org.au>
2011-03-17 15:02:51 +0100

    drbd: need include for bitops functions declarations
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>

--- drivers/block/drbd/drbd_bitmap.c
+++ drivers/block/drbd/drbd_bitmap.c
@@ -29,6 +29,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/drbd.h>
 #include <linux/slab.h>
 #include <asm/kmap_types.h>
+
+#include <asm-generic/bitops/le.h>
+
 #include "drbd_int.h"
 
 
