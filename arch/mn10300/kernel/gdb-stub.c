68292f468923b6dd49e71b6a5725e7fc89f4df07
David Howells <dhowells@redhat.com>
2011-03-18 16:54:29 +0000

    MN10300: gdbstub: Restrict single-stepping to non-preemptable non-SMP configs
    Restrict single-stepping through the kernel using gdbstub to non-preemptable
    non-SMP configs as gdbstub has to do software single-stepping by means of
    temporary breakpoints.  Hardware single-stepping is unavailable as Panasonic
    have not sufficiently documented the interface to it.
    
    Software single-stepping through preemptable or SMP kernels runs into problems
    as it makes it much more likely that the wrong thread will hit the temporary
    breakpoints.  It seems impractical to work around the problem for the most
    part.  It could be possible to make a UP preemptable kernel switch temporary
    breakpoints in and out in switch_to().
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/mn10300/kernel/gdb-stub.c
+++ arch/mn10300/kernel/gdb-stub.c
@@ -406,6 +406,7 @@ static int hexToInt(char **ptr, int *intValue)
 	return (numChars);
 }
 
+#ifdef CONFIG_GDBSTUB_ALLOW_SINGLE_STEP
 /*
  * We single-step by setting breakpoints. When an exception
  * is handled, we need to restore the instructions hoisted
@@ -730,6 +731,7 @@ static int gdbstub_single_step(struct pt_regs *regs)
 	__gdbstub_restore_bp();
 	return -EFAULT;
 }
+#endif /* CONFIG_GDBSTUB_ALLOW_SINGLE_STEP */
 
 #ifdef CONFIG_GDBSTUB_CONSOLE
 
@@ -1209,11 +1211,13 @@ static int gdbstub(struct pt_regs *regs, enum exception_code excep)
 	/* if we were single stepping, restore the opcodes hoisted for the
 	 * breakpoint[s] */
 	broke = 0;
+#ifdef CONFIG_GDBSTUB_ALLOW_SINGLE_STEP
 	if ((step_bp[0].addr && step_bp[0].addr == (u8 *) regs->pc) ||
 	    (step_bp[1].addr && step_bp[1].addr == (u8 *) regs->pc))
 		broke = 1;
 
 	__gdbstub_restore_bp();
+#endif
 
 	if (gdbstub_rx_unget) {
 		sigval = SIGINT;
@@ -1549,17 +1553,21 @@ packet_waiting:
 			 * Step to next instruction
 			 */
 		case 's':
-			/*
-			 * using the T flag doesn't seem to perform single
+			/* Using the T flag doesn't seem to perform single
 			 * stepping (it seems to wind up being caught by the
 			 * JTAG unit), so we have to use breakpoints and
 			 * continue instead.
 			 */
+#ifdef CONFIG_GDBSTUB_ALLOW_SINGLE_STEP
 			if (gdbstub_single_step(regs) < 0)
 				/* ignore any fault error for now */
 				gdbstub_printk("unable to set single-step"
 					       " bp\n");
 			goto done;
+#else
+			gdbstub_strcpy(output_buffer, "E01");
+			break;
+#endif
 
 			/*
 			 * Set baud rate (bBB)
