df35307fe4439ae1b18d01eade2e8eae2787ee9a
Grant Grundler <grundler@google.com>
2011-03-18 15:56:57 -0700

    STAGING: brcm80211 v2 keep power on in suspend state
    Keep WIFI power on during suspend.
    
    Consumes 10s of milliwatts but avoids having to reload firmware
    on resume. Tested on tegra2_seaboard.
    
    Signed-off-by: Venkat Rao <vrao@broadcom.com>
    Tested-by: Grant Grundler <grundler@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
+++ drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 #include <wl_cfg80211.h>
 
+void sdioh_sdio_set_host_pm_flags(int flag);
+
 static struct sdio_func *cfg80211_sdio_func;
 static struct wl_dev *wl_cfg80211_dev;
 static const u8 ether_bcast[ETH_ALEN] = {255, 255, 255, 255, 255, 255};
@@ -1988,6 +1990,8 @@ static s32 wl_cfg80211_suspend(struct wiphy *wiphy)
 	}
 	clear_bit(WL_STATUS_SCANNING, &wl->status);
 	clear_bit(WL_STATUS_SCAN_ABORTING, &wl->status);
+
+	sdioh_sdio_set_host_pm_flags(MMC_PM_KEEP_POWER);
 
 	return err;
 }
