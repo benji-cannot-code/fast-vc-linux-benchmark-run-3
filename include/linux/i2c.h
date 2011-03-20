5bb4ce9b8e52d43956c32845ca06c0b71b71dc24
Jean Delvare <khali@linux-fr.org>
2011-03-20 14:50:52 +0100

    i2c: Get rid of <linux/i2c-id.h>
    The last remaining ID in <linux/i2c-id.h> is no longer used anywhere,
    so we can finally get rid of it.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #ifdef __KERNEL__
 #include <linux/module.h>
-#include <linux/i2c-id.h>
 #include <linux/mod_devicetable.h>
 #include <linux/device.h>	/* for struct device */
 #include <linux/sched.h>	/* for completion */
