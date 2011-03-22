496a8162f06bc1f160ef67c5986c714795d5a5d9
Felix Fietkau <nbd@openwrt.org>
2011-03-22 21:54:19 +0100

    ath9k: fix stuck beacon detection
    Stuck beacon detection is supposed to trigger when 9 consecutive beacons
    could not be sent by the hardware. When the driver runs only one active
    AP mode interface, it still configures the hardware beacon timer for
    4 (ATH_BCBUF) beacon slots slots, which causes stuck beacon detection
    to be reset if ath9k_hw_stoptxdma clears the stuck frames between
    SWBA intervals.
    Fix this by not resetting the missed beacon count for empty slots and
    multiplying the threshold not by the maximum number of beacon slots
    but by the configured number of beacon interfaces.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/beacon.c
+++ drivers/net/wireless/ath/ath9k/beacon.c
@@ -364,7 +364,7 @@ void ath_beacon_tasklet(unsigned long data)
 	if (ath9k_hw_numtxpending(ah, sc->beacon.beaconq) != 0) {
 		sc->beacon.bmisscnt++;
 
-		if (sc->beacon.bmisscnt < BSTUCK_THRESH) {
+		if (sc->beacon.bmisscnt < BSTUCK_THRESH * sc->nbcnvifs) {
 			ath_dbg(common, ATH_DBG_BSTUCK,
 				"missed %u consecutive beacons\n",
 				sc->beacon.bmisscnt);
@@ -379,13 +379,6 @@ void ath_beacon_tasklet(unsigned long data)
 		}
 
 		return;
-	}
-
-	if (sc->beacon.bmisscnt != 0) {
-		ath_dbg(common, ATH_DBG_BSTUCK,
-			"resume beacon xmit after %u misses\n",
-			sc->beacon.bmisscnt);
-		sc->beacon.bmisscnt = 0;
 	}
 
 	/*
@@ -420,6 +413,13 @@ void ath_beacon_tasklet(unsigned long data)
 		if (bf != NULL) {
 			bfaddr = bf->bf_daddr;
 			bc = 1;
+		}
+
+		if (sc->beacon.bmisscnt != 0) {
+			ath_dbg(common, ATH_DBG_BSTUCK,
+				"resume beacon xmit after %u misses\n",
+				sc->beacon.bmisscnt);
+			sc->beacon.bmisscnt = 0;
 		}
 	}
 
