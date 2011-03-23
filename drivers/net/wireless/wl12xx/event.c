3ce6b8143032bf39562a97197c307cdc412ccf64
Eliad Peller <eliad@wizery.com>
2011-03-23 22:22:15 +0200

    wl12xx: rearrange some ELP wake_up/sleep calls
    ELP (Extremely/Enhanced Low Power, or something like that ;)) refers to
    the powerstate of the 12xx chip, in which very low power is consumed,
    and no commands (from the host) can be issued until the chip is woken up.
    
    Wakeup/sleep commands must be protected by a wl->mutex, so it's generally
    a good idea to call wakeup/sleep along with the mutex lock/unlock (where
    needed). However, in some places the wl12xx driver calls wakeup/sleep in
    some "inner" functions. This result in some "nested" wakeup/sleep calls
    which might end up letting the chip go to sleep prematurely (e.g. during
    event handling).
    
    Fix it by rearranging the elp calls to come along with mutex_lock/unlock.
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>
    Signed-off-by: Ido Yariv <ido@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/event.c
+++ drivers/net/wireless/wl12xx/event.c
@@ -34,6 +34,7 @@ void wl1271_pspoll_work(struct work_struct *work)
 {
 	struct delayed_work *dwork;
 	struct wl1271 *wl;
+	int ret;
 
 	dwork = container_of(work, struct delayed_work, work);
 	wl = container_of(dwork, struct wl1271, pspoll_work);
@@ -56,8 +57,13 @@ void wl1271_pspoll_work(struct work_struct *work)
 	 * delivery failure occurred, and no-one changed state since, so
 	 * we should go back to powersave.
 	 */
+	ret = wl1271_ps_elp_wakeup(wl);
+	if (ret < 0)
+		goto out;
+
 	wl1271_ps_set_mode(wl, STATION_POWER_SAVE_MODE, wl->basic_rate, true);
 
+	wl1271_ps_elp_sleep(wl);
 out:
 	mutex_unlock(&wl->mutex);
 };
@@ -130,11 +136,6 @@ static int wl1271_event_ps_report(struct wl1271 *wl,
 
 		/* enable beacon early termination */
 		ret = wl1271_acx_bet_enable(wl, true);
-		if (ret < 0)
-			break;
-
-		/* go to extremely low power mode */
-		wl1271_ps_elp_sleep(wl);
 		break;
 	default:
 		break;
