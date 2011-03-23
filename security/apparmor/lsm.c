7c6c3cf89cd34523ec7633c3b49993f86f9948d0
Serge E. Hallyn <serge@hallyn.com>
2011-03-23 16:43:17 -0700

    userns: security: make capabilities relative to the user namespace
    - Introduce ns_capable to test for a capability in a non-default
      user namespace.
    - Teach cap_capable to handle capabilities in a non-default
      user namespace.
    
    The motivation is to get to the unprivileged creation of new
    namespaces.  It looks like this gets us 90% of the way there, with
    only potential uid confusion issues left.
    
    I still need to handle getting all caps after creation but otherwise I
    think I have a good starter patch that achieves all of your goals.
    
    Changelog:
    	11/05/2010: [serge] add apparmor
    	12/14/2010: [serge] fix capabilities to created user namespaces
    	Without this, if user serge creates a user_ns, he won't have
    	capabilities to the user_ns he created.  THis is because we
    	were first checking whether his effective caps had the caps
    	he needed and returning -EPERM if not, and THEN checking whether
    	he was the creator.  Reverse those checks.
    	12/16/2010: [serge] security_real_capable needs ns argument in !security case
    	01/11/2011: [serge] add task_ns_capable helper
    	01/11/2011: [serge] add nsown_capable() helper per Bastian Blank suggestion
    	02/16/2011: [serge] fix a logic bug: the root user is always creator of
    		    init_user_ns, but should not always have capabilities to
    		    it!  Fix the check in cap_capable().
    	02/21/2011: Add the required user_ns parameter to security_capable,
    		    fixing a compile failure.
    	02/23/2011: Convert some macros to functions as per akpm comments.  Some
    		    couldn't be converted because we can't easily forward-declare
    		    them (they are inline if !SECURITY, extern if SECURITY).  Add
    		    a current_user_ns function so we can use it in capability.h
    		    without #including cred.h.  Move all forward declarations
    		    together to the top of the #ifdef __KERNEL__ section, and use
    		    kernel-doc format.
    	02/23/2011: Per dhowells, clean up comment in cap_capable().
    	02/23/2011: Per akpm, remove unreachable 'return -EPERM' in cap_capable.
    
    (Original written and signed off by Eric;  latest, modified version
    acked by him)
    
    [akpm@linux-foundation.org: fix build]
    [akpm@linux-foundation.org: export current_user_ns() for ecryptfs]
    [serge.hallyn@canonical.com: remove unneeded extra argument in selinux's task_has_capability]
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Serge E. Hallyn <serge.hallyn@canonical.com>
    Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Acked-by: Daniel Lezcano <daniel.lezcano@free.fr>
    Acked-by: David Howells <dhowells@redhat.com>
    Cc: James Morris <jmorris@namei.org>
    Signed-off-by: Serge E. Hallyn <serge.hallyn@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/sysctl.h>
 #include <linux/audit.h>
+#include <linux/user_namespace.h>
 #include <net/sock.h>
 
 #include "include/apparmor.h"
@@ -137,11 +138,11 @@ static int apparmor_capget(struct task_struct *target, kernel_cap_t *effective,
 }
 
 static int apparmor_capable(struct task_struct *task, const struct cred *cred,
-			    int cap, int audit)
+			    struct user_namespace *ns, int cap, int audit)
 {
 	struct aa_profile *profile;
 	/* cap_capable returns 0 on success, else -EPERM */
-	int error = cap_capable(task, cred, cap, audit);
+	int error = cap_capable(task, cred, ns, cap, audit);
 	if (!error) {
 		profile = aa_cred_profile(cred);
 		if (!unconfined(profile))
