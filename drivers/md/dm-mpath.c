9660286d007e11c5a3775f8a09850777c7e9390d
Mike Snitzer <snitzer@redhat.com>
2011-03-24 13:54:31 +0000

    dm mpath: fail message ioctl if specified path is not valid
    Fail the reinstate_path and fail_path message ioctl if the specified
    path is not valid.
    
    The message ioctl would succeed for the 'reinistate_path' and
    'fail_path' messages even if action was not taken because the
    specified device was not a valid path of the multipath device.
    
    Before, when /dev/vdb is not a path of mpathb:
    $ dmsetup message mpathb 0 reinstate_path /dev/vdb
    $ echo $?
    0
    
    After:
    $ dmsetup message mpathb 0 reinstate_path /dev/vdb
    device-mapper: message ioctl failed: Invalid argument
    Command failed
    $ echo $?
    1
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-mpath.c
+++ drivers/md/dm-mpath.c
@@ -1066,7 +1066,7 @@ out:
 static int action_dev(struct multipath *m, struct dm_dev *dev,
 		      action_fn action)
 {
-	int r = 0;
+	int r = -EINVAL;
 	struct pgpath *pgpath;
 	struct priority_group *pg;
 
@@ -1670,7 +1670,7 @@ out:
  *---------------------------------------------------------------*/
 static struct target_type multipath_target = {
 	.name = "multipath",
-	.version = {1, 2, 0},
+	.version = {1, 3, 0},
 	.module = THIS_MODULE,
 	.ctr = multipath_ctr,
 	.dtr = multipath_dtr,
