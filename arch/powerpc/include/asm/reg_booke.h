9140dc904a0523d61c2ffe896176415c0e0d88af
Varun Sethi <Varun.Sethi@freescale.com>
2011-03-24 11:50:26 +0000

    powerpc/booke: Correct the SPRN_MAS5 definition.
    339 is the SPR number for MAS5 documented by Power ISA 2.06, and
    implemented by e500mc.  It is not yet used anywhere in the kernel,
    so nothing should be relying on the wrong number.
    
    Signed-off-by: Varun Sethi <Varun.Sethi@freescale.com>
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/reg_booke.h
+++ arch/powerpc/include/asm/reg_booke.h
@@ -111,7 +111,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SPRN_MAS2	0x272	/* MMU Assist Register 2 */
 #define SPRN_MAS3	0x273	/* MMU Assist Register 3 */
 #define SPRN_MAS4	0x274	/* MMU Assist Register 4 */
-#define SPRN_MAS5	0x275	/* MMU Assist Register 5 */
+#define SPRN_MAS5	0x153	/* MMU Assist Register 5 */
 #define SPRN_MAS6	0x276	/* MMU Assist Register 6 */
 #define SPRN_PID1	0x279	/* Process ID Register 1 */
 #define SPRN_PID2	0x27A	/* Process ID Register 2 */
