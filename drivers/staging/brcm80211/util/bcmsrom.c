78228406f4c101572ae99cd393d4162bf2918ee1
Arend van Spriel <arend@broadcom.com>
2011-03-25 11:25:59 +0100

    staging: brcm80211: remove unneccessary include from bcmsrom.c
    Include statement of if_ether.h turned out to be unneccessary so
    this is removed.
    
    Reviewed-by: Henry Ptasinski <henryp@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/util/bcmsrom.c
+++ drivers/staging/brcm80211/util/bcmsrom.c
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sbsdpcmdev.h>
 #endif
 
-#include <linux/if_ether.h>
-
 #define SROM_OFFSET(sih) ((sih->ccrev > 31) ? \
 	(((sih->cccaps & CC_CAP_SROM) == 0) ? NULL : \
 	 ((u8 *)curmap + PCI_16KB0_CCREGS_OFFSET + CC_SROM_OTP)) : \
