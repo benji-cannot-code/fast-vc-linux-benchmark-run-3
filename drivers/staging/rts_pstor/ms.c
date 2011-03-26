2f935d89ff311555397bbe7e7e385926c5c921b6
Alexander Beregalov <a.beregalov@gmail.com>
2011-03-26 20:18:13 +0300

    Staging: rts_pstor: redundant null check before kfree()
    Signed-off-by: Alexander Beregalov <a.beregalov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rts_pstor/ms.c
+++ drivers/staging/rts_pstor/ms.c
@@ -3817,9 +3817,7 @@ int mg_get_local_EKB(struct scsi_cmnd *srb, struct rtsx_chip *chip)
 	rtsx_stor_set_xfer_buf(buf, bufflen, srb);
 
 GetEKBFinish:
-	if (buf) {
-		kfree(buf);
-	}
+	kfree(buf);
 	return retval;
 }
 
@@ -4055,9 +4053,7 @@ int mg_get_ICV(struct scsi_cmnd *srb, struct rtsx_chip *chip)
 	rtsx_stor_set_xfer_buf(buf, bufflen, srb);
 
 GetICVFinish:
-	if (buf) {
-		kfree(buf);
-	}
+	kfree(buf);
 	return retval;
 }
 
@@ -4156,9 +4152,7 @@ int mg_set_ICV(struct scsi_cmnd *srb, struct rtsx_chip *chip)
 #endif
 
 SetICVFinish:
-	if (buf) {
-		kfree(buf);
-	}
+	kfree(buf);
 	return retval;
 }
 
