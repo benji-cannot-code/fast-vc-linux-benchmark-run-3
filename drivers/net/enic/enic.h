526d911626fe7cc74efd287d6cded80edb4c66f7
Roopa Prabhu <roprabhu@cisco.com>
2011-03-29 20:36:07 +0000

    enic: Add support for PORT_REQUEST_PREASSOCIATE_RR
    Current enic code only supports ASSOCIATE and DISASSOCIATE port profile
    operations. This patch adds enic support for port profile
    PORT_REQUEST_PREASSOCIATE_RR operation. The VIC adapter (8021qbh) is capable
    of handling port profile requests done in two steps namely PREASSOCIATE_RR
    and ASSOCIATE today. The motivation to support PREASSOCIATE_RR comes mainly
    from its use as an optimization during VM migration ie, to do resource
    reservation on destination host before resources on source host are released.
    
    PREASSOCIATE_RR is a VDP operation and according to the latest at IEEE,
    8021qbh will also need to support VDP commands.
    
    In addition to handling the new PORT_REQUEST_PREASSOCIATE_RR operation
    this patch also does the below:
    - Introduces handlers for PORT_REQUEST operations
    - Moves most of the port profile handling code to new files enic_pp.[ch]
    - Uses new fw devcmds for port profile operations
    
    Signed-off-by: Roopa Prabhu <roprabhu@cisco.com>
    Signed-off-by: David Wang <dwang2@cisco.com>
    Signed-off-by: Christian Benvenuti <benve@cisco.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/enic/enic.h
+++ drivers/net/enic/enic.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME		"enic"
 #define DRV_DESCRIPTION		"Cisco VIC Ethernet NIC Driver"
-#define DRV_VERSION		"2.1.1.12"
+#define DRV_VERSION		"2.1.1.13"
 #define DRV_COPYRIGHT		"Copyright 2008-2011 Cisco Systems, Inc"
 
 #define ENIC_BARS_MAX		6
@@ -120,5 +120,7 @@ static inline struct device *enic_get_dev(struct enic *enic)
 {
 	return &(enic->pdev->dev);
 }
+
+void enic_reset_addr_lists(struct enic *enic);
 
 #endif /* _ENIC_H_ */
