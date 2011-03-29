c4575595d323afd924124682b76d38b6db4697e6
Greg Ungerer <gerg@uclinux.org>
2011-03-29 14:03:26 +1000

    m68knommu: move EXPORT of dump_fpu to function definition
    The EXPORT_SYMBOL(dump_fpu) belongs at the definition of the function,
    not in some other random code file. So move it there.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/kernel/m68k_ksyms_no.c
+++ arch/m68k/kernel/m68k_ksyms_no.c
@@ -17,11 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/checksum.h>
 #include <asm/current.h>
 
-extern int dump_fpu(struct pt_regs *, elf_fpregset_t *);
-
 /* platform dependent support */
-
-EXPORT_SYMBOL(dump_fpu);
 
 EXPORT_SYMBOL(ip_fast_csum);
 
