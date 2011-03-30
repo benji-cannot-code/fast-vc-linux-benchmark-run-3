eb4a501c4816b2a750b72f25a1a986218dd45eb3
Luis R. Rodriguez <lrodriguez@atheros.com>
2011-03-29 17:56:28 -0700

    ath6kl: rename the pm ops to something more sensible
    This has no functional changes.
    
    Cc: Naveen Singh <nsingh@atheros.com>
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/hif/sdio/linux_sdio/src/hif.c
+++ drivers/staging/ath6kl/hif/sdio/linux_sdio/src/hif.c
@@ -165,14 +165,14 @@ static void ath6kl_hifdev_remove(struct sdio_func *func)
 }
 
 #if defined(CONFIG_PM)
-static int hifDeviceSuspend(struct device *dev)
+static int ath6kl_hifdev_suspend(struct device *dev)
 {
 	struct sdio_func *func = dev_to_sdio_func(dev);
 	int status = 0;
 	struct hif_device *device;
 
 	device = ath6kl_get_hifdev(func);
-	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: +hifDeviceSuspend\n"));
+	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: +ath6kl_hifdev_suspend\n"));
 
 	if (device && device->claimedContext &&
 	    osdrvCallbacks.deviceSuspendHandler) {
@@ -185,7 +185,7 @@ static int hifDeviceSuspend(struct device *dev)
 	}
 
 	CleanupHIFScatterResources(device);
-	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: -hifDeviceSuspend\n"));
+	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: -ath6kl_hifdev_suspend\n"));
 
 	switch (status) {
 	case 0:
@@ -198,14 +198,14 @@ static int hifDeviceSuspend(struct device *dev)
 	}
 }
 
-static int hifDeviceResume(struct device *dev)
+static int ath6kl_hifdev_resume(struct device *dev)
 {
 	struct sdio_func *func = dev_to_sdio_func(dev);
 	int status = 0;
 	struct hif_device *device;
 
 	device = ath6kl_get_hifdev(func);
-	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: +hifDeviceResume\n"));
+	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: +ath6kl_hifdev_resume\n"));
 	if (device && device->claimedContext &&
 	    osdrvCallbacks.deviceSuspendHandler) {
 		status = osdrvCallbacks.
@@ -213,14 +213,14 @@ static int hifDeviceResume(struct device *dev)
 		if (status == 0)
 			device->is_suspend = false;
 	}
-	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: -hifDeviceResume\n"));
+	AR_DEBUG_PRINTF(ATH_DEBUG_TRACE, ("AR6000: -ath6kl_hifdev_resume\n"));
 
 	return status;
 }
 
-static const struct dev_pm_ops ar6k_device_pm_ops = {
-	.suspend = hifDeviceSuspend,
-	.resume = hifDeviceResume,
+static const struct dev_pm_ops ath6kl_hifdev_pmops = {
+	.suspend = ath6kl_hifdev_suspend,
+	.resume = ath6kl_hifdev_resume,
 };
 #endif /* CONFIG_PM */
 
@@ -231,7 +231,7 @@ static struct sdio_driver ath6kl_hifdev_driver = {
 	.remove = ath6kl_hifdev_remove,
 #if defined(CONFIG_PM)
 	.drv = {
-		.pm = &ar6k_device_pm_ops,
+		.pm = &ath6kl_hifdev_pmops,
 	},
 #endif
 };
