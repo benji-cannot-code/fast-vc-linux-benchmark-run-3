b3f34682a5eb1b4bbfa39fb60774e4459e622905
Colin Cross <ccross@android.com>
2011-03-30 00:24:43 -0700

    ARM: tegra: gpio: Fix unused variable warnings
    Change b0f18edaf6ee4e6fac89cae63a90bd38ad2a3418
    (arm: tegra: Remove unused bogus irq enable/disable magic)
    introduces warnings:
    arch/arm/mach-tegra/gpio.c: In function 'tegra_gpio_resume':
    arch/arm/mach-tegra/gpio.c:260: warning: unused variable 'i'
    arch/arm/mach-tegra/gpio.c: In function 'tegra_gpio_suspend':
    arch/arm/mach-tegra/gpio.c:283: warning: unused variable 'i'
    
    Fix them, and fix a coding style issue on the same lines.
    
    Signed-off-by: Colin Cross <ccross@android.com>
    Acked-by: Erik Gilling <konkers@konkers.net>

--- arch/arm/mach-tegra/gpio.c
+++ arch/arm/mach-tegra/gpio.c
@@ -258,7 +258,8 @@ static void tegra_gpio_irq_handler(unsigned int irq, struct irq_desc *desc)
 void tegra_gpio_resume(void)
 {
 	unsigned long flags;
-	int b, p, i;
+	int b;
+	int p;
 
 	local_irq_save(flags);
 
@@ -281,7 +282,8 @@ void tegra_gpio_resume(void)
 void tegra_gpio_suspend(void)
 {
 	unsigned long flags;
-	int b, p, i;
+	int b;
+	int p;
 
 	local_irq_save(flags);
 	for (b = 0; b < ARRAY_SIZE(tegra_gpio_banks); b++) {
