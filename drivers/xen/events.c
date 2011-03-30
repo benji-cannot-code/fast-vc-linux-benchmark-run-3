171ff7f85ffcb24d3fe7aeca195e56f46db4c147
Nicolas Kaiser <nikai@nikai.net>
2011-03-30 21:14:26 +0200

    xen: events: fix error checks in bind_*_to_irqhandler()
    Checking 'irq < 0' doesn't work when 'irq' is unsigned.
    
    The assigned bind_evtchn_to_irq() and bind_virq_to_irq()
    return int, so using int appears appropriate.
    
    Signed-off-by: Nicolas Kaiser <nikai@nikai.net>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/events.c
+++ drivers/xen/events.c
@@ -913,8 +913,7 @@ int bind_evtchn_to_irqhandler(unsigned int evtchn,
 			      unsigned long irqflags,
 			      const char *devname, void *dev_id)
 {
-	unsigned int irq;
-	int retval;
+	int irq, retval;
 
 	irq = bind_evtchn_to_irq(evtchn);
 	if (irq < 0)
@@ -956,8 +955,7 @@ int bind_virq_to_irqhandler(unsigned int virq, unsigned int cpu,
 			    irq_handler_t handler,
 			    unsigned long irqflags, const char *devname, void *dev_id)
 {
-	unsigned int irq;
-	int retval;
+	int irq, retval;
 
 	irq = bind_virq_to_irq(virq, cpu);
 	if (irq < 0)
