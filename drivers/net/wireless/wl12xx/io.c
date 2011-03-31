d805daae3937ddcc3d50acfa3d434f672c901281
Ido Yariv <ido@wizery.com>
2011-03-31 10:06:58 +0200

    wl12xx: Clean up the block size alignment code
    Simplify and clean up the block size alignment code:
    1. Set the block size according to the padding field type, as it cannot
       exceed the maximum value this field can hold.
    2. Move the alignment code into a function instead of duplicating it in
       multiple places.
    3. In the current implementation, the block_size member can be
       misleading because a zero value actually means that there's no need to
       align. Declare a block size alignment quirk instead.
    
    Signed-off-by: Ido Yariv <ido@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/io.c
+++ drivers/net/wireless/wl12xx/io.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wl12xx.h"
 #include "wl12xx_80211.h"
 #include "io.h"
+#include "tx.h"
 
 #define OCP_CMD_LOOP  32
 
@@ -47,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 bool wl1271_set_block_size(struct wl1271 *wl)
 {
 	if (wl->if_ops->set_block_size) {
-		wl->if_ops->set_block_size(wl);
+		wl->if_ops->set_block_size(wl, WL12XX_BUS_BLOCK_SIZE);
 		return true;
 	}
 
