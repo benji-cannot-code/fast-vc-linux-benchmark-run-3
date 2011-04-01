04bbb7ab742d0c101ef085d26795114d51913117
Rajesh Borundia <Rajesh.Borundia@qlogic.com>
2011-04-01 14:28:31 +0000

    qlcnic: Fix LRO disable
    o In dev->open LRO was enabled by default, enable it depending
      upon netdev->features , kernel may have disabled it.
    o Configure LRO when interface is up.
    
    Signed-off-by: Rajesh Borundia <Rajesh.Borundia@qlogic.com>
    Signed-off-by: Anirban Chakraborty <Anirban.Chakraborty@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlcnic/qlcnic_hw.c
+++ drivers/net/qlcnic/qlcnic_hw.c
@@ -567,6 +567,9 @@ int qlcnic_config_hw_lro(struct qlcnic_adapter *adapter, int enable)
 	u64 word;
 	int rv;
 
+	if (!test_bit(__QLCNIC_FW_ATTACHED, &adapter->state))
+		return 0;
+
 	memset(&req, 0, sizeof(struct qlcnic_nic_req));
 
 	req.qhdr = cpu_to_le64(QLCNIC_HOST_REQUEST << 23);
@@ -711,6 +714,9 @@ int qlcnic_send_lro_cleanup(struct qlcnic_adapter *adapter)
 	struct qlcnic_nic_req req;
 	u64 word;
 	int rv;
+
+	if (!test_bit(__QLCNIC_FW_ATTACHED, &adapter->state))
+		return 0;
 
 	memset(&req, 0, sizeof(struct qlcnic_nic_req));
 	req.qhdr = cpu_to_le64(QLCNIC_HOST_REQUEST << 23);
