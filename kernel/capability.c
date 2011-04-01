21e4db65f939a7f4ce3a4ddbbacc3115e45aa7e3
Eric Paris <eparis@redhat.com>
2011-04-01 17:08:34 -0400

    capabilities: do not drop CAP_SETPCAP from the initial task
    In olden' days of yore CAP_SETPCAP had special meaning for the init task.
    We actually have code to make sure that CAP_SETPCAP wasn't in pE of things
    using the init_cred.  But CAP_SETPCAP isn't so special any more and we
    don't have a reason to special case dropping it for init or kthreads....
    
    Signed-off-by: Eric Paris <eparis@redhat.com>
    Acked-by: Andrew G. Morgan <morgan@kernel.org>
    Signed-off-by: James Morris <jmorris@namei.org>

--- kernel/capability.c
+++ kernel/capability.c
@@ -24,11 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 const kernel_cap_t __cap_empty_set = CAP_EMPTY_SET;
 const kernel_cap_t __cap_full_set = CAP_FULL_SET;
-const kernel_cap_t __cap_init_eff_set = CAP_INIT_EFF_SET;
 
 EXPORT_SYMBOL(__cap_empty_set);
 EXPORT_SYMBOL(__cap_full_set);
-EXPORT_SYMBOL(__cap_init_eff_set);
 
 int file_caps_enabled = 1;
 
