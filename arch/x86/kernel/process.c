07061260342adb2392a5bc2115a49fca310d3b50
Len Brown <len.brown@intel.com>
2011-04-01 15:28:09 -0400

    x86 idle: EXPORT_SYMBOL(default_idle, pm_idle) only when APM demands it
    In the long run, we don't want default_idle() or (pm_idle)() to
    be exported outside of process.c.  Start by not exporting them
    to modules, unless the APM build demands it.
    
    cc: x86@kernel.org
    cc: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -341,7 +341,9 @@ EXPORT_SYMBOL(boot_option_idle_override);
  * Powermanagement idle function, if any..
  */
 void (*pm_idle)(void);
+#if defined(CONFIG_APM_MODULE) && defined(CONFIG_APM_CPU_IDLE)
 EXPORT_SYMBOL(pm_idle);
+#endif
 
 #ifdef CONFIG_X86_32
 /*
@@ -401,7 +403,7 @@ void default_idle(void)
 		cpu_relax();
 	}
 }
-#ifdef CONFIG_APM_MODULE
+#if defined(CONFIG_APM_MODULE) && defined(CONFIG_APM_CPU_IDLE)
 EXPORT_SYMBOL(default_idle);
 #endif
 
