1c94024aabb529a105c60e3a1b7507375a1b232f
Justin P. Mattock <justinmattock@gmail.com>
2011-04-08 19:49:08 -0700

    treewide: remove extra semicolons
    Signed-off-by: Justin P. Mattock <justinmattock@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/net/wireless/ath/ath9k/phy.h
+++ drivers/net/wireless/ath/ath9k/phy.h
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RF_BANK_SETUP(_bank, _iniarray, _col) do {			\
 		int i;							\
 		for (i = 0; i < (_iniarray)->ia_rows; i++)		\
-			(_bank)[i] = INI_RA((_iniarray), i, _col);;	\
+			(_bank)[i] = INI_RA((_iniarray), i, _col);	\
 	} while (0)
 
 #define	AR_PHY_TIMING11_SPUR_FREQ_SD		0x3FF00000
