e9a65b40415abb9a0be4263b386d067f8ef4de9c
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-04-10 14:10:05 +0800

    atmel_serial: keep the platform_device unchanged
    specify the port num via platform_data this will allow to match the clock
    with the plaform_dev staticaly
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Acked-by: Alan Cox <alan@linux.intel.com>
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>
    Cc: Patrice Vilchez <patrice.vilchez@atmel.com>
    Cc: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
    Cc: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/arm/mach-at91/at91cap9_devices.c
+++ arch/arm/mach-at91/at91cap9_devices.c
@@ -1200,6 +1200,7 @@ struct platform_device *atmel_default_console_device;	/* the serial console devi
 void __init at91_register_uart(unsigned id, unsigned portnr, unsigned pins)
 {
 	struct platform_device *pdev;
+	struct atmel_uart_data *pdata;
 
 	switch (id) {
 		case 0:		/* DBGU */
@@ -1225,7 +1226,8 @@ void __init at91_register_uart(unsigned id, unsigned portnr, unsigned pins)
 		default:
 			return;
 	}
-	pdev->id = portnr;		/* update to mapped ID */
+	pdata = pdev->dev.platform_data;
+	pdata->num = portnr;		/* update to mapped ID */
 
 	if (portnr < ATMEL_MAX_UART)
 		at91_uarts[portnr] = pdev;
