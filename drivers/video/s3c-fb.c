fbb4ff832887fa27fd15ccae149b372a6516024f
Jingoo Han <jg1.han@samsung.com>
2011-04-11 07:22:58 +0000

    video: s3c-fb: make runtime pm functions static
    This patch makes runtime pm functions static.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/video/s3c-fb.c
+++ drivers/video/s3c-fb.c
@@ -1550,7 +1550,7 @@ static int s3c_fb_resume(struct device *dev)
 	return 0;
 }
 
-int s3c_fb_runtime_suspend(struct device *dev)
+static int s3c_fb_runtime_suspend(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct s3c_fb *sfb = platform_get_drvdata(pdev);
@@ -1570,7 +1570,7 @@ int s3c_fb_runtime_suspend(struct device *dev)
 	return 0;
 }
 
-int s3c_fb_runtime_resume(struct device *dev)
+static int s3c_fb_runtime_resume(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct s3c_fb *sfb = platform_get_drvdata(pdev);
