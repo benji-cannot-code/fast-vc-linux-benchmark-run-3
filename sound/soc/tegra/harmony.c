1abc72b13eadf3e259d72f5b7df492f48735859b
Stephen Warren <swarren@nvidia.com>
2011-04-12 11:29:00 -0600

    ASoC: Tegra: Rename pdev tegra-snd-harmony to tegra-snd-wm8903
    Soon, this machine driver will be updated to handle a number of Tegra boards
    using the WM8903 codec. Rename the platform device in advance to reflect this.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/tegra/harmony.c
+++ sound/soc/tegra/harmony.c
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tegra_pcm.h"
 #include "tegra_asoc_utils.h"
 
-#define DRV_NAME "tegra-snd-harmony"
+#define DRV_NAME "tegra-snd-wm8903"
 
 #define GPIO_SPKR_EN    BIT(0)
 #define GPIO_INT_MIC_EN BIT(1)
