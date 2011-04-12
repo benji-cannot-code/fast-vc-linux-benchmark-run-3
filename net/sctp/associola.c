4cfa73eb60f4f4e1ce385c74f030b305c167b75d
Vlad Yasevich <vladislav.yasevich@hp.com>
2011-04-12 15:20:48 +0000

    sctp: fix oops when updating retransmit path with DEBUG on
    commit fbdf501c9374966a56829ecca3a7f25d2b49a305
      sctp: Do no select unconfirmed transports for retransmissions
    
    Introduced the initial falt.
    
    commit d598b166ced20d9b9281ea3527c0e18405ddb803
      sctp: Make sure we always return valid retransmit path
    
    Solved the problem, but forgot to change the DEBUG statement.
    Thus it was still possible to dereference a NULL pointer.
    
    Signed-off-by: Wei Yongjun <yjwei@cn.fujitsu.com>
    Signed-off-by: Vlad Yasevich <vladislav.yasevich@hp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/associola.c
+++ net/sctp/associola.c
@@ -1324,6 +1324,8 @@ void sctp_assoc_update_retran_path(struct sctp_association *asoc)
 
 	if (t)
 		asoc->peer.retran_path = t;
+	else
+		t = asoc->peer.retran_path;
 
 	SCTP_DEBUG_PRINTK_IPADDR("sctp_assoc_update_retran_path:association"
 				 " %p addr: ",
