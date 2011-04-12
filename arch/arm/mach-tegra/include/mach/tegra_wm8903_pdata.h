a574ca07a1500b5bc4a727d42e0709c049434232
Stephen Warren <swarren@nvidia.com>
2011-04-12 11:29:01 -0600

    ARM: Tegra: Add to tegra_wm8903_platform_data
    Seaboard derivate Kaen has a GPIO to mute the headphone output. Add a field
    to tegra_wm8903_platform_data so the board files can pass the GPIO number
    for that to the ASoC machine driver.
    
    Also, initialize this new field to a "not present" value for Harmony.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- arch/arm/mach-tegra/include/mach/tegra_wm8903_pdata.h
+++ arch/arm/mach-tegra/include/mach/tegra_wm8903_pdata.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct tegra_wm8903_platform_data {
 	int gpio_spkr_en;
 	int gpio_hp_det;
+	int gpio_hp_mute;
 	int gpio_int_mic_en;
 	int gpio_ext_mic_en;
 };
