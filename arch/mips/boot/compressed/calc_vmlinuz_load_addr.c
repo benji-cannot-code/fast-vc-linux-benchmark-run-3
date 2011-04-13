db15d9086bb5f7d0c344fedfbf7968b38952190c
Ralf Baechle <ralf@linux-mips.org>
2011-04-13 21:49:54 +0200

    MIPS: Fix calc_vmlinuz_load_addr build warnings.
      HOSTCC  arch/mips/boot/compressed/calc_vmlinuz_load_addr
    arch/mips/boot/compressed/calc_vmlinuz_load_addr.c: In function 'main':
    arch/mips/boot/compressed/calc_vmlinuz_load_addr.c:35:2: warning: format '%llx' expects type 'long long unsigned int *', but argument 3 has type 'uint64_t *'
    arch/mips/boot/compressed/calc_vmlinuz_load_addr.c:54:2: warning: format '%llx' expects type 'long long unsigned int', but argument 2 has type 'uint64_t'
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/boot/compressed/calc_vmlinuz_load_addr.c
+++ arch/mips/boot/compressed/calc_vmlinuz_load_addr.c
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int main(int argc, char *argv[])
 {
+	unsigned long long vmlinux_size, vmlinux_load_addr, vmlinuz_load_addr;
 	struct stat sb;
-	uint64_t vmlinux_size, vmlinux_load_addr, vmlinuz_load_addr;
 
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s <pathname> <vmlinux_load_addr>\n",
