65bf901c6340584e791c197099737cf804a90fdd
Michal Simek <monstr@monstr.eu>
2011-04-14 11:48:43 +0200

    microblaze: Simplify early console binding from DT
    Recognize early Linux console from chosen - linux,stdout-path
    instead of detecting the first console with appropriate
    compatible strings.
    This patch solved the problem on system with multiple
    consoles.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/prom.h
+++ arch/microblaze/include/asm/prom.h
@@ -27,8 +27,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HAVE_ARCH_DEVTREE_FIXUPS
 
 /* Other Prototypes */
-extern int early_uartlite_console(void);
-extern int early_uart16550_console(void);
+enum early_consoles {
+	UARTLITE = 1,
+	UART16550 = 2,
+};
+
+extern int of_early_console(void *version);
 
 #ifdef CONFIG_PCI
 /*
