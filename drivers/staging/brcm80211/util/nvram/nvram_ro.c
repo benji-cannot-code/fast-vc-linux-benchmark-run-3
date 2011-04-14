019f075482dc3ce51a566d10d3e2ed65fe98195c
Arend van Spriel <arend@broadcom.com>
2011-04-14 16:53:29 +0200

    staging: brcm80211: remove unnecessary include from nvram_ro.c
    Source file included bcmotp.h but did not use any declared item
    from that include file so it can safely be removed.
    
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Henry Ptasinski <henryp@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/util/nvram/nvram_ro.c
+++ drivers/staging/brcm80211/util/nvram/nvram_ro.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmnvram.h>
 #include <sbchipc.h>
 #include <bcmsrom.h>
-#include <bcmotp.h>
 #include <bcmdevs.h>
 #include <hndsoc.h>
 
