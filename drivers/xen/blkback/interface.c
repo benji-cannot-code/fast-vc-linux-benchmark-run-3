ca73e526a182214975a4ead473aada1c69f6e2b6
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-04-14 17:27:29 -0400

    xen/blkback: Fix interface.c checkpatch warnings .. except
    +               sring_x86_64 = (struct blkif_x86_64_sring *)blkif->blk_ring_area->addr;
    
    WARNING: line over 80 characters
    +               BACK_RING_INIT(&blkif->blk_rings.x86_64, sring_x86_64, PAGE_SIZE);
    
    as breaking them up really does not help that much.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/blkback/interface.c
+++ drivers/xen/blkback/interface.c
@@ -87,7 +87,8 @@ static void unmap_frontend_page(struct blkif_st *blkif)
 		BUG();
 }
 
-int blkif_map(struct blkif_st *blkif, unsigned long shared_page, unsigned int evtchn)
+int blkif_map(struct blkif_st *blkif, unsigned long shared_page,
+	      unsigned int evtchn)
 {
 	int err;
 
@@ -95,7 +96,8 @@ int blkif_map(struct blkif_st *blkif, unsigned long shared_page, unsigned int ev
 	if (blkif->irq)
 		return 0;
 
-	if ( (blkif->blk_ring_area = alloc_vm_area(PAGE_SIZE)) == NULL )
+	blkif->blk_ring_area = alloc_vm_area(PAGE_SIZE);
+	if (!blkif->blk_ring_area)
 		return -ENOMEM;
 
 	err = map_frontend_page(blkif, shared_page);
@@ -132,8 +134,7 @@ int blkif_map(struct blkif_st *blkif, unsigned long shared_page, unsigned int ev
 
 	err = bind_interdomain_evtchn_to_irqhandler(
 		blkif->domid, evtchn, blkif_be_int, 0, "blkif-backend", blkif);
-	if (err < 0)
-	{
+	if (err < 0) {
 		unmap_frontend_page(blkif);
 		free_vm_area(blkif->blk_ring_area);
 		blkif->blk_rings.common.sring = NULL;
