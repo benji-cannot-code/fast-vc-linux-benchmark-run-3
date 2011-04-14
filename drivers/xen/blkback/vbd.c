89167c6455ec3ff83d7ca3e4952b497f4df4d1d9
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-04-14 17:05:23 -0400

    xen/blkback: Add some comments.
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/blkback/vbd.c
+++ drivers/xen/blkback/vbd.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /******************************************************************************
- * blkback/vbd.c
- *
  * Routines for managing virtual block devices (VBDs).
  *
  * Copyright (c) 2003-2005, Keir Fraser & Steve Hand
--- drivers/xen/blkback/xenbus.c
+++ drivers/xen/blkback/xenbus.c
@@ -108,7 +108,7 @@ static void update_blkif_status(blkif_t *blkif)
 }
 
 
-/****************************************************************
+/*
  *  sysfs interface for VBD I/O requests
  */
 
