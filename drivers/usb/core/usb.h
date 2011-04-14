6854832958053d985af6ff25a25e6c044994c0ac
Michal Nazarewicz <mina86@mina86.com>
2011-04-14 17:47:09 +0200

    usb: core: Change usb_create_sysfs_intf_files()' return type to void
    The usb_create_sysfs_intf_files() function always returned zero even
    if it failed to create sysfs fails.  Since this is a desired behaviour
    there is no need to return return code at all.  This commit changes
    function's return type (form int) to void.
    
    Signed-off-by: Michal Nazarewicz <mina86@mina86.com>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/usb.h
+++ drivers/usb/core/usb.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int usb_create_sysfs_dev_files(struct usb_device *dev);
 extern void usb_remove_sysfs_dev_files(struct usb_device *dev);
-extern int usb_create_sysfs_intf_files(struct usb_interface *intf);
+extern void usb_create_sysfs_intf_files(struct usb_interface *intf);
 extern void usb_remove_sysfs_intf_files(struct usb_interface *intf);
 extern int usb_create_ep_devs(struct device *parent,
 				struct usb_host_endpoint *endpoint,
