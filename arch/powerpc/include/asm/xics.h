3b9f101107e1338ca02e1f00036fdf69cd05b18b
Michael Ellerman <michael@ellerman.id.au>
2011-04-14 22:31:58 +0000

    powerpc/xics: xics.h relies on linux/interrupt.h
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/xics.h
+++ arch/powerpc/include/asm/xics.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _XICS_H
 #define _XICS_H
 
+#include <linux/interrupt.h>
+
 #define XICS_IPI		2
 #define XICS_IRQ_SPURIOUS	0
 
