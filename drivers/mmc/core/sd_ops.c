7f3f0d4f973c125e2d4d972222707b0edb16c849
Chris Ball <cjb@laptop.org>
2011-04-13 23:49:45 -0400

    mmc: initialize struct mmc_request at declaration time
    Converts from:
    	struct mmc_request mrq;
    	memset(&mrq, 0, sizeof(struct mmc_request));
    
    to:
    	struct mmc_request mrq = {0};
    
    because it's shorter, as performant, and easier to work out whether
    initialization has happened.
    
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/sd_ops.c
+++ drivers/mmc/core/sd_ops.c
@@ -67,7 +67,7 @@ static int mmc_app_cmd(struct mmc_host *host, struct mmc_card *card)
 int mmc_wait_for_app_cmd(struct mmc_host *host, struct mmc_card *card,
 	struct mmc_command *cmd, int retries)
 {
-	struct mmc_request mrq;
+	struct mmc_request mrq = {0};
 
 	int i, err;
 
@@ -244,7 +244,7 @@ int mmc_send_relative_addr(struct mmc_host *host, unsigned int *rca)
 int mmc_app_send_scr(struct mmc_card *card, u32 *scr)
 {
 	int err;
-	struct mmc_request mrq;
+	struct mmc_request mrq = {0};
 	struct mmc_command cmd = {0};
 	struct mmc_data data = {0};
 	struct scatterlist sg;
@@ -266,8 +266,6 @@ int mmc_app_send_scr(struct mmc_card *card, u32 *scr)
 	data_buf = kmalloc(sizeof(card->raw_scr), GFP_KERNEL);
 	if (data_buf == NULL)
 		return -ENOMEM;
-
-	memset(&mrq, 0, sizeof(struct mmc_request));
 
 	mrq.cmd = &cmd;
 	mrq.data = &data;
@@ -305,7 +303,7 @@ int mmc_app_send_scr(struct mmc_card *card, u32 *scr)
 int mmc_sd_switch(struct mmc_card *card, int mode, int group,
 	u8 value, u8 *resp)
 {
-	struct mmc_request mrq;
+	struct mmc_request mrq = {0};
 	struct mmc_command cmd = {0};
 	struct mmc_data data = {0};
 	struct scatterlist sg;
@@ -317,8 +315,6 @@ int mmc_sd_switch(struct mmc_card *card, int mode, int group,
 
 	mode = !!mode;
 	value &= 0xF;
-
-	memset(&mrq, 0, sizeof(struct mmc_request));
 
 	mrq.cmd = &cmd;
 	mrq.data = &data;
@@ -352,7 +348,7 @@ int mmc_sd_switch(struct mmc_card *card, int mode, int group,
 int mmc_app_sd_status(struct mmc_card *card, void *ssr)
 {
 	int err;
-	struct mmc_request mrq;
+	struct mmc_request mrq = {0};
 	struct mmc_command cmd = {0};
 	struct mmc_data data = {0};
 	struct scatterlist sg;
@@ -366,8 +362,6 @@ int mmc_app_sd_status(struct mmc_card *card, void *ssr)
 	err = mmc_app_cmd(card->host, card);
 	if (err)
 		return err;
-
-	memset(&mrq, 0, sizeof(struct mmc_request));
 
 	mrq.cmd = &cmd;
 	mrq.data = &data;
