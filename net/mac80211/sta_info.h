b50b94e08c3b8cb2971828cdecd86634501f6efa
Felix Fietkau <nbd@openwrt.org>
2011-04-17 17:45:00 +0200

    mac80211: add a function for setting the TIM bit for a specific station
    This allows a driver to buffer frames for a PS station and tell mac80211
    to wake it up even though mac80211 does not have any buffered frames for
    it.
    This is necessary for properly handling aggregation related buffering,
    in ath9k, because the driver needs to keep its frames in order to keep
    track of the Block-ACK window.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/sta_info.h
+++ net/mac80211/sta_info.h
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	be in the queues
  * @WLAN_STA_PSPOLL: Station sent PS-poll while driver was keeping
  *	station in power-save mode, reply when the driver unblocks.
+ * @WLAN_STA_PS_DRIVER_BUF: Station has frames pending in driver internal
+ *	buffers. Automatically cleared on station wake-up.
  */
 enum ieee80211_sta_info_flags {
 	WLAN_STA_AUTH		= 1<<0,
@@ -59,6 +61,7 @@ enum ieee80211_sta_info_flags {
 	WLAN_STA_BLOCK_BA	= 1<<11,
 	WLAN_STA_PS_DRIVER	= 1<<12,
 	WLAN_STA_PSPOLL		= 1<<13,
+	WLAN_STA_PS_DRIVER_BUF	= 1<<14,
 };
 
 #define STA_TID_NUM 16
