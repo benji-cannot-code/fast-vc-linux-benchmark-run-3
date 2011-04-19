f330b1b06b2f2e6c01110a193276ddf68aa86222
Dominik Brodowski <linux@dominikbrodowski.net>
2011-04-19 19:58:59 +0200

    cpupowerutils: idle_monitor - ConfigStyle bugfixes
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- tools/power/cpupower/utils/idle_monitor/amd_fam14h_idle.c
+++ tools/power/cpupower/utils/idle_monitor/amd_fam14h_idle.c
@@ -105,7 +105,7 @@ static int amd_fam14h_get_pci_info(struct cstate *state,
 				   unsigned int *enable_bit,
 				   unsigned int cpu)
 {
-	switch(state->id) {
+	switch (state->id) {
 	case NON_PC0:
 		*enable_bit = PCI_NON_PC0_ENABLE_BIT;
 		*pci_offset = PCI_NON_PC0_OFFSET;
@@ -178,7 +178,7 @@ static int amd_fam14h_disable(cstate_t *state, unsigned int cpu)
 		/* was the bit whether NBP1 got entered set? */
 		nbp1_entered = (val & (1 << PCI_NBP1_ACTIVE_BIT)) |
 			(val & (1 << PCI_NBP1_ENTERED_BIT));
-			
+
 		dprint("NBP1 was %sentered - 0x%x - enable_bit: "
 		       "%d - pci_offset: 0x%x\n",
 		       nbp1_entered ? "" : "not ",
@@ -215,7 +215,7 @@ static int fam14h_get_count_percent(unsigned int id, double *percent,
 				    unsigned int cpu)
 {
 	unsigned long diff;
-	
+
 	if (id >= AMD_FAM14H_STATE_NUM)
 		return -1;
 	/* residency count in 80ns -> divide through 12.5 to get us residency */
@@ -237,9 +237,8 @@ static int amd_fam14h_start(void)
 	int num, cpu;
 	clock_gettime(CLOCK_REALTIME, &start_time);
 	for (num = 0; num < AMD_FAM14H_STATE_NUM; num++) {
-		for (cpu = 0; cpu < cpu_count; cpu++) {
+		for (cpu = 0; cpu < cpu_count; cpu++)
 			amd_fam14h_init(&amd_fam14h_cstates[num], cpu);
-		}
 	}
 #ifdef DEBUG
 	clock_gettime(CLOCK_REALTIME, &dbg_time);
@@ -258,9 +257,8 @@ static int amd_fam14h_stop(void)
 	clock_gettime(CLOCK_REALTIME, &end_time);
 
 	for (num = 0; num < AMD_FAM14H_STATE_NUM; num++) {
-		for (cpu = 0; cpu < cpu_count; cpu++) {
+		for (cpu = 0; cpu < cpu_count; cpu++)
 			amd_fam14h_disable(&amd_fam14h_cstates[num], cpu);
-		}
 	}
 #ifdef DEBUG
 	clock_gettime(CLOCK_REALTIME, &dbg_time);
@@ -282,8 +280,8 @@ static int is_nbp1_capable(void)
 	return val & (1 << 31);
 }
 
-struct cpuidle_monitor* amd_fam14h_register(void) {
-
+struct cpuidle_monitor *amd_fam14h_register(void)
+{
 	int num;
 
 	if (cpupower_cpu_info.vendor != X86_VENDOR_AMD)
@@ -300,9 +298,9 @@ struct cpuidle_monitor* amd_fam14h_register(void) {
 
 	/* We do not alloc for nbp1 machine wide counter */
 	for (num = 0; num < AMD_FAM14H_STATE_NUM - 1; num++) {
-		previous_count[num] = calloc (cpu_count,
+		previous_count[num] = calloc(cpu_count,
 					      sizeof(unsigned long long));
-		current_count[num]  = calloc (cpu_count,
+		current_count[num]  = calloc(cpu_count,
 					      sizeof(unsigned long long));
 	}
 
