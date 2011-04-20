5a82691c18cdabf5d3ba4e12b822f445ae8a4456
Avi Kivity <avi@redhat.com>
2011-04-20 15:47:13 +0300

    KVM: x86 emulator: add ->fix_hypercall() callback
    Artificial, but needed to remove direct calls to KVM.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -4026,7 +4026,7 @@ twobyte_insn:
 			if (c->modrm_mod != 3 || c->modrm_rm != 1)
 				goto cannot_emulate;
 
-			rc = kvm_fix_hypercall(ctxt->vcpu);
+			rc = ctxt->ops->fix_hypercall(ctxt);
 			if (rc != X86EMUL_CONTINUE)
 				goto done;
 
@@ -4049,7 +4049,7 @@ twobyte_insn:
 			if (c->modrm_mod == 3) {
 				switch (c->modrm_rm) {
 				case 1:
-					rc = kvm_fix_hypercall(ctxt->vcpu);
+					rc = ctxt->ops->fix_hypercall(ctxt);
 					break;
 				default:
 					goto cannot_emulate;
