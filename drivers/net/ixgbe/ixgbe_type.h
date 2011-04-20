29248516eb03bdb93169f41dd08a361b598d8c75
Emil Tantilov <emil.s.tantilov@intel.com>
2011-04-20 08:49:06 +0000

    ixgbe: improve EEPROM read/write operations
    Introduce buffered read/writes which greatly improves performance on
    parts with large EEPROMs.
    
    Previously reading/writing a word requires taking/releasing of synchronization
    semaphores which adds 10ms to each operation. The optimization is to
    read/write in buffers, but make sure the semaphore is not held for >500ms
    according to the datasheet.
    
    Since we can't read the EEPROM page size ixgbe_detect_eeprom_page_size() is
    used to discover the EEPROM size when needed and keeps the result in
    word_page_size for the rest of the run time.
    
    Use buffered reads for ethtool -e.
    
    Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>
    Tested-by: Evan Swanson <evan.swanson@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ixgbe/ixgbe_type.h
+++ drivers/net/ixgbe/ixgbe_type.h
@@ -1669,6 +1669,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define IXGBE_ETH_LENGTH_OF_ADDRESS   6
 
+#define IXGBE_EEPROM_PAGE_SIZE_MAX       128
+#define IXGBE_EEPROM_RD_BUFFER_MAX_COUNT 512 /* EEPROM words # read in burst */
+#define IXGBE_EEPROM_WR_BUFFER_MAX_COUNT 256 /* EEPROM words # wr in burst */
+
 #ifndef IXGBE_EEPROM_GRANT_ATTEMPTS
 #define IXGBE_EEPROM_GRANT_ATTEMPTS 1000 /* EEPROM # attempts to gain grant */
 #endif
@@ -2564,7 +2568,9 @@ typedef u8* (*ixgbe_mc_addr_itr) (struct ixgbe_hw *hw, u8 **mc_addr_ptr,
 struct ixgbe_eeprom_operations {
 	s32 (*init_params)(struct ixgbe_hw *);
 	s32 (*read)(struct ixgbe_hw *, u16, u16 *);
+	s32 (*read_buffer)(struct ixgbe_hw *, u16, u16, u16 *);
 	s32 (*write)(struct ixgbe_hw *, u16, u16);
+	s32 (*write_buffer)(struct ixgbe_hw *, u16, u16, u16 *);
 	s32 (*validate_checksum)(struct ixgbe_hw *, u16 *);
 	s32 (*update_checksum)(struct ixgbe_hw *);
 	u16 (*calc_checksum)(struct ixgbe_hw *);
@@ -2650,6 +2656,7 @@ struct ixgbe_eeprom_info {
 	u32                             semaphore_delay;
 	u16                             word_size;
 	u16                             address_bits;
+	u16                             word_page_size;
 };
 
 #define IXGBE_FLAGS_DOUBLE_RESET_REQUIRED	0x01
