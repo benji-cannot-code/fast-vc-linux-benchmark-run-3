5be62c95b6b8b2e0da8042abbc9287126423e72b
Xiao Guangrong <xiaoguangrong@cn.fujitsu.com>
2011-04-20 10:07:30 +0000

    Btrfs: fix the race between reading and updating devices
    On btrfs_congested_fn and __unplug_io_fn paths, we should hold
    device_list_mutex to avoid remove/add device path to
    update fs_devices->devices
    
    On __btrfs_close_devices and btrfs_prepare_sprout paths, the devices in
    fs_devices->devices or fs_devices->devices is updated, so we should hold
    the mutex to avoid the reader side to reach them
    
    Signed-off-by: Xiao Guangrong <xiaoguangrong@cn.fujitsu.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -1411,6 +1411,7 @@ static int btrfs_congested_fn(void *congested_data, int bdi_bits)
 	struct btrfs_device *device;
 	struct backing_dev_info *bdi;
 
+	mutex_lock(&info->fs_devices->device_list_mutex);
 	list_for_each_entry(device, &info->fs_devices->devices, dev_list) {
 		if (!device->bdev)
 			continue;
@@ -1420,6 +1421,7 @@ static int btrfs_congested_fn(void *congested_data, int bdi_bits)
 			break;
 		}
 	}
+	mutex_unlock(&info->fs_devices->device_list_mutex);
 	return ret;
 }
 
