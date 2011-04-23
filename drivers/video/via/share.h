f49f1beaa16fecba12b52e8ef61b9e97fc73fc43
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2011-04-23 22:24:52 +0000

    viafb: reduce OLPC refresh a bit
    When allowing some PLL calculation we get a frequency that seems to
    be a bit higher than what the OLPC DCON likes resulting in a still
    readable but not so good image. We don't really know whether this is
    a problem with the calculation formula or the OLPC but as other
    displays seem to be happy with the other modes adjusting the OLPC
    refresh looks like the better thing. This patch prevents a
    regression when dynamic PLL calculation is allowed.
    
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>

--- drivers/video/via/share.h
+++ drivers/video/via/share.h
@@ -291,6 +291,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HW_LAYOUT_LCD_EXTERNAL_LCD2 0x10
 
 /* Definition Refresh Rate */
+#define REFRESH_49      49
 #define REFRESH_50      50
 #define REFRESH_60      60
 #define REFRESH_75      75
