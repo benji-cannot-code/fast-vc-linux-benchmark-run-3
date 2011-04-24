2a1fe508019895cf2c55275f88c2b8897cae3bdf
Christoph Hellwig <hch@infradead.org>
2011-04-24 19:02:58 +0000

    xfs: fix compiler warning in xfs_trace.h
    xfs_fsblock_t may be a 32-bit type on if XFS_BIG_BLKNOS is not set,
    make sure to cast a value of this type to an unsigned long long
    before using the ll printk qualifier.
    
    Reported-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Alex Elder <aelder@sgi.com>

--- fs/xfs/linux-2.6/xfs_trace.h
+++ fs/xfs/linux-2.6/xfs_trace.h
@@ -1392,7 +1392,7 @@ DECLARE_EVENT_CLASS(xfs_alloc_class,
 		  __entry->wasfromfl,
 		  __entry->isfl,
 		  __entry->userdata,
-		  __entry->firstblock)
+		  (unsigned long long)__entry->firstblock)
 )
 
 #define DEFINE_ALLOC_EVENT(name) \
