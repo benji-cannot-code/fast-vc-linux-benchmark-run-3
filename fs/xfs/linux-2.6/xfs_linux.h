0b265399caed14fb433e5957009d6369e7485e5c
Christoph Hellwig <hch@infradead.org>
2011-04-24 19:06:17 +0000

    xfs: reduce the number of pagb_lock roundtrips in xfs_alloc_clear_busy
    Instead of finding the per-ag and then taking and releasing the pagb_lock
    for every single busy extent completed sort the list of busy extents and
    only switch betweens AGs where nessecary.  This becomes especially important
    with the online discard support which will hit this lock more often.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Alex Elder <aelder@sgi.com>

--- fs/xfs/linux-2.6/xfs_linux.h
+++ fs/xfs/linux-2.6/xfs_linux.h
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/writeback.h>
 #include <linux/capability.h>
+#include <linux/list_sort.h>
 
 #include <asm/page.h>
 #include <asm/div64.h>
