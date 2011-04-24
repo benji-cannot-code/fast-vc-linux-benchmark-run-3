6ecbe1add49c6d2eec26c03da108995b75f03e11
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-04-24 18:15:34 +0800

    at91: move register clocks to soc generic init
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>
    Cc: Patrice Vilchez <patrice.vilchez@atmel.com>

--- arch/arm/mach-at91/soc.h
+++ arch/arm/mach-at91/soc.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct at91_init_soc {
 	unsigned int *default_irq_priority;
 	void (*map_io)(void);
+	void (*register_clocks)(void);
 	void (*init)(void);
 };
 
