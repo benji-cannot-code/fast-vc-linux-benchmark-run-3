302c18edfddca6a5ab039b50345d7a8a6a421251
Mike Frysinger <vapier@gentoo.org>
2011-04-25 12:33:15 +0200

    Freezer: Use SMP barriers
    The freezer processes are dealing with multiple threads running
    simultaneously, and on a UP system, the memory reads/writes do
    not need barriers to keep things in sync.  These are only needed
    on SMP systems, so use SMP barriers instead.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- kernel/freezer.c
+++ kernel/freezer.c
@@ -18,7 +18,7 @@ static inline void frozen_process(void)
 {
 	if (!unlikely(current->flags & PF_NOFREEZE)) {
 		current->flags |= PF_FROZEN;
-		wmb();
+		smp_wmb();
 	}
 	clear_freeze_flag(current);
 }
@@ -94,7 +94,7 @@ bool freeze_task(struct task_struct *p, bool sig_only)
 	 * the task as frozen and next clears its TIF_FREEZE.
 	 */
 	if (!freezing(p)) {
-		rmb();
+		smp_rmb();
 		if (frozen(p))
 			return false;
 
