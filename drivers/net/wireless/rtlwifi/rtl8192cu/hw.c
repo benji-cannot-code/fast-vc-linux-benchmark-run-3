d9d0f07212ee30ec985619e74c92961b79f5d19e
Chaoming_Li <chaoming_li@realsil.com.cn>
2011-04-25 12:53:50 -0500

    rtlwifi: rtl8192ce: Change hw routine for addition of rtl8192se and rtl8192de
    Change rtl8192ce hw routine for addition of RTL8192SE and RTL8192DE.
    
    Signed-off-by: Chaoming_Li <chaoming_li@realsil.com.cn>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/rtl8192cu/hw.c
+++ drivers/net/wireless/rtlwifi/rtl8192cu/hw.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "trx.h"
 #include "led.h"
 #include "table.h"
+#include "../rtl8192ce/hw.h"
 
 static void _rtl92cu_phy_param_tab_init(struct ieee80211_hw *hw)
 {
