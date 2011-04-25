6822219c8516ed4fd926fbe176832d636f727d50
Chaoming_Li <chaoming_li@realsil.com.cn>
2011-04-25 13:23:10 -0500

    rtlwifi: Convert core routines for addition of rtl8192se and rtl8192de
    Convert core routines for addition of RTL8192SE and RTL8192DE code.
    
    Additional files are changed to allow compilation.
    
    Signed-off-by: Chaoming_Li <chaoming_li@realsil.com.cn>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/core.h
+++ drivers/net/wireless/rtlwifi/core.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Hsinchu 300, Taiwan.
  *
  * Larry Finger <Larry.Finger@lwfinger.net>
+ *
  *****************************************************************************/
 
 #ifndef __RTL_CORE_H__
