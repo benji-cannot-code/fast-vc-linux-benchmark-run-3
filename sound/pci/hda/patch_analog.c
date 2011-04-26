d0dd5b3c4d3d754e757d3c451845a51b5e749147
Takashi Iwai <tiwai@suse.de>
2011-04-26 15:18:33 +0200

    ALSA: hda - Move EAPD power-down into shutup callback for AD codecs
    EAPD power-down should be called also for normal shutup cases.
    Let's move to there.   This also fixes the compile warnings when
    CONFIG_PM isn't set automatically.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_analog.c
+++ sound/pci/hda/patch_analog.c
@@ -490,11 +490,6 @@ static int ad198x_build_pcms(struct hda_codec *codec)
 	return 0;
 }
 
-static inline void ad198x_shutup(struct hda_codec *codec)
-{
-	snd_hda_shutup_pins(codec);
-}
-
 static void ad198x_free_kctls(struct hda_codec *codec)
 {
 	struct ad198x_spec *spec = codec->spec;
@@ -548,6 +543,12 @@ static void ad198x_power_eapd(struct hda_codec *codec)
 	}
 }
 
+static void ad198x_shutup(struct hda_codec *codec)
+{
+	snd_hda_shutup_pins(codec);
+	ad198x_power_eapd(codec);
+}
+
 static void ad198x_free(struct hda_codec *codec)
 {
 	struct ad198x_spec *spec = codec->spec;
@@ -565,7 +566,6 @@ static void ad198x_free(struct hda_codec *codec)
 static int ad198x_suspend(struct hda_codec *codec, pm_message_t state)
 {
 	ad198x_shutup(codec);
-	ad198x_power_eapd(codec);
 	return 0;
 }
 #endif
