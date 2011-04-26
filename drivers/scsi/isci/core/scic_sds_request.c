c56dc36a0288cc1f7f06c668e30425656d72a1e2
Dave Jiang <dave.jiang@intel.com>
2011-04-26 12:31:37 -0700

    isci: removing non-working ATAPI code
    Removing not used / bit-rotten ATAPI code. This needs to go back
    and debugged at a later date.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    [reflow against devel, delete dead sati headers]
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/scsi/isci/core/scic_sds_request.c
+++ drivers/scsi/isci/core/scic_sds_request.c
@@ -74,10 +74,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "scu_constants.h"
 #include "scu_task_context.h"
 
-#if !defined(DISABLE_ATAPI)
-#include "scic_sds_stp_packet_request.h"
-#endif
-
 /*
  * ****************************************************************************
  * * SCIC SDS IO REQUEST CONSTANTS
@@ -670,16 +666,6 @@ static enum sci_status scic_io_request_construct_sata(struct scic_sds_request *s
 	case SAT_PROTOCOL_FPDMA:
 		status = scic_sds_stp_ncq_request_construct(sci_req, len, dir);
 		break;
-
-#if !defined(DISABLE_ATAPI)
-	case SAT_PROTOCOL_PACKET_NON_DATA:
-	case SAT_PROTOCOL_PACKET_DMA_DATA_IN:
-	case SAT_PROTOCOL_PACKET_DMA_DATA_OUT:
-	case SAT_PROTOCOL_PACKET_PIO_DATA_IN:
-	case SAT_PROTOCOL_PACKET_PIO_DATA_OUT:
-		status = scic_sds_stp_packet_request_construct(sci_req);
-		break;
-#endif
 
 	case SAT_PROTOCOL_DMA_QUEUED:
 	case SAT_PROTOCOL_DMA:
