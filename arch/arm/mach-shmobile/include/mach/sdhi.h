5aa44935db31fd2b87a03e0ec0bab0e231f66537
Simon Horman <horms@verge.net.au>
2011-04-26 06:29:53 +0100

    ARM: 6886/1: mmc, Add zboot from eSD support for SuperH Mobile ARM
    This allows a ROM-able zImage to be written to eSD and for SuperH Mobile
    ARM to boot directly from the SDHI hardware block.
    
    This is achieved by the MaskROM loading the first portion of the image into
    MERAM and then jumping to it.  This portion contains loader code which
    copies the entire image to SDRAM and jumps to it. From there the zImage
    boot code proceeds as normal, uncompressing the image into its final
    location and then jumping to it.
    
    Cc: Paul Mundt <lethal@linux-sh.org>
    Acked-by: Magnus Damm <magnus.damm@gmail.com>
    Acked-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/mach-shmobile/include/mach/sdhi.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef SDHI_H
+#define SDHI_H
+
+/**************************************************
+ *
+ *		CPU specific settings
+ *
+ **************************************************/
+
+#ifdef CONFIG_ARCH_SH7372
+#include "mach/sdhi-sh7372.h"
+#else
+#error "unsupported CPU."
+#endif
+
+#endif /* SDHI_H */
