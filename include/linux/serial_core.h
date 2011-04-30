da4e90afa11f08cd50932cc057454ef05c8050aa
John Linn <john.linn@xilinx.com>
2011-04-30 00:07:43 -0400

    tty/serial: add support for Xilinx PS UART
    The Xilinx PS Uart is used on the new ARM based SoC. This
    UART is not compatible with others such that a seperate
    driver is required.
    
    Signed-off-by: John Linn <john.linn@xilinx.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -203,6 +203,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* VIA VT8500 SoC */
 #define PORT_VT8500	97
 
+/* Xilinx PSS UART */
+#define PORT_XUARTPS	98
+
 #ifdef __KERNEL__
 
 #include <linux/compiler.h>
