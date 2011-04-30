134765a47880556c7322ef841e4d8cdf08f89dc9
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-04-30 12:13:14 +0300

    OMAP: DSS2: OMAPFB: Add struct to store per-display data
    Create a new struct omapfb_display_data to contain omapfb's private
    per-display data. Move the bpp override there.
    
    This struct will be used to hold auto/manual update state of a display
    in the following patches.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/omapfb/omapfb.h
+++ drivers/video/omap2/omapfb/omapfb.h
@@ -74,6 +74,11 @@ struct omapfb_info {
 	bool mirror;
 };
 
+struct omapfb_display_data {
+	struct omap_dss_device *dssdev;
+	u8 bpp_override;
+};
+
 struct omapfb2_device {
 	struct device *dev;
 	struct mutex  mtx;
@@ -87,17 +92,11 @@ struct omapfb2_device {
 	struct omapfb2_mem_region regions[10];
 
 	unsigned num_displays;
-	struct omap_dss_device *displays[10];
+	struct omapfb_display_data displays[10];
 	unsigned num_overlays;
 	struct omap_overlay *overlays[10];
 	unsigned num_managers;
 	struct omap_overlay_manager *managers[10];
-
-	unsigned num_bpp_overrides;
-	struct {
-		struct omap_dss_device *dssdev;
-		u8 bpp;
-	} bpp_overrides[10];
 };
 
 struct omapfb_colormode {
@@ -142,6 +141,19 @@ static inline struct omap_dss_device *fb2display(struct fb_info *fbi)
 	}
 
 	return NULL;
+}
+
+static inline struct omapfb_display_data *get_display_data(
+		struct omapfb2_device *fbdev, struct omap_dss_device *dssdev)
+{
+	int i;
+
+	for (i = 0; i < fbdev->num_displays; ++i)
+		if (fbdev->displays[i].dssdev == dssdev)
+			return &fbdev->displays[i];
+
+	/* This should never happen */
+	BUG();
 }
 
 static inline void omapfb_lock(struct omapfb2_device *fbdev)
