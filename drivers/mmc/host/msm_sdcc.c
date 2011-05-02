b3eeb3eb12417df8423551da9c9d522c95fdc8a6
Sahitya Tummala <stummala@codeaurora.org>
2011-05-02 18:10:01 +0530

    mmc: msm_sdcc: Change initialization order of busclk_timer in probe
    Intialize busclk_timer before it is accessed in probe.
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -1219,6 +1219,10 @@ msmsdcc_probe(struct platform_device *pdev)
 	host->plat = plat;
 	host->mmc = mmc;
 	host->curr.cmd = NULL;
+	init_timer(&host->busclk_timer);
+	host->busclk_timer.data = (unsigned long) host;
+	host->busclk_timer.function = msmsdcc_busclk_expired;
+
 
 	host->cmdpoll = 1;
 
@@ -1335,10 +1339,6 @@ msmsdcc_probe(struct platform_device *pdev)
 		host->oldstat = host->plat->status(mmc_dev(host->mmc));
 		host->eject = !host->oldstat;
 	}
-
-	init_timer(&host->busclk_timer);
-	host->busclk_timer.data = (unsigned long) host;
-	host->busclk_timer.function = msmsdcc_busclk_expired;
 
 	ret = request_irq(cmd_irqres->start, msmsdcc_irq, IRQF_SHARED,
 			  DRIVER_NAME " (cmd)", host);
