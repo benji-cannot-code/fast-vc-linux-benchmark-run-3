dbbe9a04fb5de9238f6c9eebf93b709045c88e22
Luciano Coelho <coelho@ti.com>
2011-05-02 12:37:33 +0300

    Revert "wl12xx: support FW TX inactivity triggers"
    This reverts commit 47684808fd89d6809c0886e06f8ac324252499d8.
    
    Conflicts:
    
    	drivers/net/wireless/wl12xx/conf.h
    	drivers/net/wireless/wl12xx/main.c

--- drivers/net/wireless/wl12xx/acx.h
+++ drivers/net/wireless/wl12xx/acx.h
@@ -1154,7 +1154,7 @@ struct wl1271_acx_fw_tsf_information {
 	u8 padding[3];
 } __packed;
 
-struct wl1271_acx_ap_max_tx_retry {
+struct wl1271_acx_max_tx_retry {
 	struct acx_header header;
 
 	/*
@@ -1163,13 +1163,6 @@ struct wl1271_acx_ap_max_tx_retry {
 	 */
 	__le16 max_tx_retry;
 	u8 padding_1[2];
-} __packed;
-
-struct wl1271_acx_sta_max_tx_retry {
-	struct acx_header header;
-
-	u8 max_tx_retry;
-	u8 padding_1[3];
 } __packed;
 
 struct wl1271_acx_config_ps {
@@ -1392,8 +1385,7 @@ int wl1271_acx_set_ba_session(struct wl1271 *wl,
 int wl1271_acx_set_ba_receiver_session(struct wl1271 *wl, u8 tid_index, u16 ssn,
 				       bool enable);
 int wl1271_acx_tsf_info(struct wl1271 *wl, u64 *mactime);
-int wl1271_acx_ap_max_tx_retry(struct wl1271 *wl);
-int wl1271_acx_sta_max_tx_retry(struct wl1271 *wl);
+int wl1271_acx_max_tx_retry(struct wl1271 *wl);
 int wl1271_acx_config_ps(struct wl1271 *wl);
 int wl1271_acx_set_inconnection_sta(struct wl1271 *wl, u8 *addr);
 int wl1271_acx_set_ap_beacon_filter(struct wl1271 *wl, bool enable);
