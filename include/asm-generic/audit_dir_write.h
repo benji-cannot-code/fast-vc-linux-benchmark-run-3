b666292c830f57143115e1f3413ade2333019425
Chris Metcalf <cmetcalf@tilera.com>
2011-05-02 16:52:19 -0400

    audit: support the "standard" <asm-generic/unistd.h>
    Many of the syscalls mentioned in the audit code are not present
    for architectures that implement only the "standard" set of
    Linux syscalls (e.g. openat, but not open, etc.).  This change
    adds proper #ifdefs for all those syscalls.
    
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- include/asm-generic/audit_dir_write.h
+++ include/asm-generic/audit_dir_write.h
@@ -1,14 +1,28 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifdef __NR_rename
 __NR_rename,
+#endif
+#ifdef __NR_mkdir
 __NR_mkdir,
+#endif
+#ifdef __NR_rmdir
 __NR_rmdir,
+#endif
 #ifdef __NR_creat
 __NR_creat,
 #endif
+#ifdef __NR_link
 __NR_link,
+#endif
+#ifdef __NR_unlink
 __NR_unlink,
+#endif
+#ifdef __NR_symlink
 __NR_symlink,
+#endif
+#ifdef __NR_mknod
 __NR_mknod,
+#endif
 #ifdef __NR_mkdirat
 __NR_mkdirat,
 __NR_mknodat,
