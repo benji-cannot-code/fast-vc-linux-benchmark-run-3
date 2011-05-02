eb2884b5b91ab63c2dbb3b6bd3ebac241bc9f112
Vasanthakumar Thiagarajan <vasanth@atheros.com>
2011-05-02 11:22:42 +0530

    ath6kl: Remove duplicate si_reg.h
    Signed-off-by: Vasanthakumar Thiagarajan <vasanth@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/os/linux/eeprom.c
+++ drivers/staging/ath6kl/os/linux/eeprom.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 
 #include "AR6002/hw2.0/hw/gpio_reg.h"
-#include "AR6002/hw2.0/hw/si_reg.h"
+#include "hw/si_reg.h"
 
 //
 // defines
