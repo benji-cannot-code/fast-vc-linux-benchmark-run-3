49962de13b19534d1432383a50ed7e3e6157ed99
Vasanthakumar Thiagarajan <vasanth@atheros.com>
2011-05-02 11:22:41 +0530

    ath6kl: Remove duplicate rtc_reg.h
    Also remove unused rtc register definition from rtc_reg.h.
    
    Signed-off-by: Vasanthakumar Thiagarajan <vasanth@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/miscdrv/common_drv.c
+++ drivers/staging/ath6kl/miscdrv/common_drv.c
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hw/mbox_host_reg.h"
 #include "AR6002/hw2.0/hw/si_reg.h"
 #include "AR6002/hw2.0/hw/gpio_reg.h"
-#include "AR6002/hw2.0/hw/rtc_reg.h"
 #include "AR6002/hw2.0/hw/vmc_reg.h"
+#include "hw/rtc_reg.h"
 #include "hw/mbox_reg.h"
 #include "hw/apb_map.h"
 
