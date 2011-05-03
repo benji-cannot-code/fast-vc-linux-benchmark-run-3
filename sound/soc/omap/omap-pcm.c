44e75458bf17c131002e53950978926519acdda1
Peter Ujfalusi <peter.ujfalusi@ti.com>
2011-05-03 18:14:43 +0300

    ASoC: omap-pcm: Update e-mail address
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Jarkko Nikula <jhnikula@gmail.com>
    Signed-off-by: Liam Girdwood <lrg@ti.com>

--- sound/soc/omap/omap-pcm.c
+++ sound/soc/omap/omap-pcm.c
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2008 Nokia Corporation
  *
  * Contact: Jarkko Nikula <jhnikula@gmail.com>
- *          Peter Ujfalusi <peter.ujfalusi@nokia.com>
+ *          Peter Ujfalusi <peter.ujfalusi@ti.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
