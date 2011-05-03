49527d6151b8ba42b3b6e264b2a218b24b117a40
Vinod Koul <vinod.koul@intel.com>
2011-05-03 17:32:01 +0100

    intel_sst: MSIC codec power optimisation
    This patch adds power optimization for the msic codec and ensure
    codec is completely powered off when codec is idle.
    
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Ramesh Babu K V <ramesh.babu@intel.com>
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/intel_sst/intelmid_v2_control.c
+++ drivers/staging/intel_sst/intelmid_v2_control.c
@@ -316,7 +316,7 @@ static int nc_power_down(void)
 	return nc_enable_audiodac(UNMUTE);
 }
 
-static int nc_power_down_pb(void)
+static int nc_power_down_pb(unsigned int device)
 {
 
 	int retval = 0;
@@ -360,7 +360,7 @@ static int nc_power_down_pb(void)
 
 }
 
-static int nc_power_down_cp(void)
+static int nc_power_down_cp(unsigned int device)
 {
 	struct sc_reg_access sc_access[] = {
 		{POWERCTRL1, 0x00, 0xBE},
