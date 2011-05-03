97fd71b704ac4dcc407330a1655eb37a4863c36d
Peter Ujfalusi <peter.ujfalusi@ti.com>
2011-05-03 18:15:10 +0300

    ASoC: RX51: Update e-mail address
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Jarkko Nikula <jhnikula@gmail.com>
    Signed-off-by: Liam Girdwood <lrg@ti.com>

--- sound/soc/omap/rx51.c
+++ sound/soc/omap/rx51.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2008 - 2009 Nokia Corporation
  *
- * Contact: Peter Ujfalusi <peter.ujfalusi@nokia.com>
+ * Contact: Peter Ujfalusi <peter.ujfalusi@ti.com>
  *          Eduardo Valentin <eduardo.valentin@nokia.com>
  *          Jarkko Nikula <jhnikula@gmail.com>
  *
