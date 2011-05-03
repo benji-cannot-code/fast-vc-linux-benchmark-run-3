20aa74053a63ff6c2a4f38bd6f38b8261fc2cc64
Takashi Iwai <tiwai@suse.de>
2011-05-03 16:41:02 +0200

    ALSA: lola - Use a single BDL
    Use a single BDL for both buffers instead of allocating for each.
    
    Also a few tune-up to avoid the stream stalls in the PCM code and
    the prelimianry work for SG-buffer support are added, too.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/lola/lola.h
+++ sound/pci/lola/lola.h
@@ -300,7 +300,6 @@ struct lola_stream {
 	unsigned int bufsize;
 	unsigned int period_bytes;
 	unsigned int frags;
-	struct snd_dma_buffer bdl; /* BDL buffer */
 
 	/* format + channel setup */
 	unsigned int format_verb;
@@ -315,6 +314,7 @@ struct lola_stream {
 
 struct lola_pcm {
 	unsigned int num_streams;
+	struct snd_dma_buffer bdl; /* BDL buffer */
 	struct lola_stream streams[MAX_STREAM_COUNT];
 };
 
