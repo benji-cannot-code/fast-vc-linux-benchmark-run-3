9f68a13cefccde7334297172ea50c8b591456f99
Vasanthakumar Thiagarajan <vasanth@atheros.com>
2011-05-04 14:23:54 +0530

    ath6kl: Remove unused eeprom.c
    Also delete si_reg.h which is only used in eeprom.c
    
    Signed-off-by: Vasanthakumar Thiagarajan <vasanth@atheros.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ath6kl/miscdrv/common_drv.c
+++ drivers/staging/ath6kl/miscdrv/common_drv.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "athdefs.h"
 
 #include "hw/mbox_host_reg.h"
-#include "hw/si_reg.h"
 #include "AR6002/hw2.0/hw/gpio_reg.h"
 #include "hw/rtc_reg.h"
 #include "hw/mbox_reg.h"
