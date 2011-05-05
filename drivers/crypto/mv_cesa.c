0f573097ee51c2a9192ee2cd7e2e5f80d5434cfa
Phil Sutter <phil.sutter@viprinet.com>
2011-05-05 15:28:59 +0200

    crypto: mv_cesa - drop this call to mv_hash_final from mv_hash_finup
    The code in mv_hash_final is actually a superset of mv_hash_finup's
    body. Since the driver works fine without, drop it.
    
    Signed-off-by: Phil Sutter <phil.sutter@viprinet.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/mv_cesa.c
+++ drivers/crypto/mv_cesa.c
@@ -729,9 +729,6 @@ static int mv_hash_final(struct ahash_request *req)
 
 static int mv_hash_finup(struct ahash_request *req)
 {
-	if (!req->nbytes)
-		return mv_hash_final(req);
-
 	mv_update_hash_req_ctx(ahash_request_ctx(req), 1, req->nbytes);
 	return mv_handle_req(&req->base);
 }
