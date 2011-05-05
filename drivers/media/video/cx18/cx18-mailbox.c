2c9cea6657165b67356a50828c397ef6b6b3d152
Simon Farnsworth <simon.farnsworth@onelan.co.uk>
2011-05-05 09:42:36 -0300

    [media] cx18: Fix warnings introduced during cleanup
    I misused the ktime API, and failed to remove some traces of the
    in-kernel format conversion. Fix these, so the the driver builds
    without warnings.
    
    Signed-off-by: Simon Farnsworth <simon.farnsworth@onelan.co.uk>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx18/cx18-mailbox.c
+++ drivers/media/video/cx18/cx18-mailbox.c
@@ -165,10 +165,9 @@ static void cx18_mdl_send_to_videobuf(struct cx18_stream *s,
 {
 	struct cx18_videobuf_buffer *vb_buf;
 	struct cx18_buffer *buf;
-	u8 *p, u;
+	u8 *p;
 	u32 offset = 0;
 	int dispatch = 0;
-	int i;
 
 	if (mdl->bytesused == 0)
 		return;
@@ -204,7 +203,7 @@ static void cx18_mdl_send_to_videobuf(struct cx18_stream *s,
 	}
 
 	if (dispatch) {
-		ktime_get_ts(&vb_buf->vb.ts);
+		vb_buf->vb.ts = ktime_to_timeval(ktime_get());
 		list_del(&vb_buf->vb.queue);
 		vb_buf->vb.state = VIDEOBUF_DONE;
 		wake_up(&vb_buf->vb.done);
