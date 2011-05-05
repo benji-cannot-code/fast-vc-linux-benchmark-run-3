199e5be38e0fb555c56f2c1538733cd9ade80132
Benny Halevy <bhalevy@panasas.com>
2011-05-05 08:28:46 +0300

    pnfs: resolve header dependency in pnfs.h
    Some definitions in the header file depend on nfs_fs.h so pnfs.h can't
    be included independently.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.h
+++ fs/nfs/pnfs.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef FS_NFS_PNFS_H
 #define FS_NFS_PNFS_H
 
+#include <linux/nfs_fs.h>
 #include <linux/nfs_page.h>
 
 enum {
