ab98a8c5b0aa4719a6e173ce6493d8899b65547e
matt mooney <mfm@muteddisk.com>
2011-05-06 03:47:49 -0700

    staging: usbip: vhci.h: coding style cleanup
    Fix a few alignment issues and remove extraneous lines.
    
    Signed-off-by: matt mooney <mfm@muteddisk.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/usbip/vhci.h
+++ drivers/staging/usbip/vhci.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/usb/hcd.h>
 
-
 struct vhci_device {
 	struct usb_device *udev;
 
@@ -34,11 +33,10 @@ struct vhci_device {
 	/* speed of a remote device */
 	enum usb_device_speed speed;
 
-	/*  vhci root-hub port to which this device is attached  */
+	/* vhci root-hub port to which this device is attached */
 	__u32 rhport;
 
 	struct usbip_device ud;
-
 
 	/* lock for the below link lists */
 	spinlock_t priv_lock;
@@ -55,7 +53,6 @@ struct vhci_device {
 	wait_queue_head_t waitq_tx;
 };
 
-
 /* urb->hcpriv, use container_of() */
 struct vhci_priv {
 	unsigned long seqnum;
@@ -64,7 +61,6 @@ struct vhci_priv {
 	struct vhci_device *vdev;
 	struct urb *urb;
 };
-
 
 struct vhci_unlink {
 	/* seqnum of this request */
@@ -86,12 +82,12 @@ struct vhci_unlink {
 
 /* for usb_bus.hcpriv */
 struct vhci_hcd {
-	spinlock_t	lock;
+	spinlock_t lock;
 
-	u32	port_status[VHCI_NPORTS];
+	u32 port_status[VHCI_NPORTS];
 
-	unsigned	resuming:1;
-	unsigned long	re_timeout;
+	unsigned resuming:1;
+	unsigned long re_timeout;
 
 	atomic_t seqnum;
 
@@ -103,10 +99,8 @@ struct vhci_hcd {
 	struct vhci_device vdev[VHCI_NPORTS];
 };
 
-
 extern struct vhci_hcd *the_controller;
 extern struct attribute_group dev_attr_group;
-
 
 /*-------------------------------------------------------------------------*/
 /* prototype declaration */
@@ -117,10 +111,9 @@ void rh_port_disconnect(int rhport);
 int vhci_rx_loop(void *data);
 int vhci_tx_loop(void *data);
 
-struct urb *pickup_urb_and_free_priv(struct vhci_device *vdev,
-					    __u32 seqnum);
+struct urb *pickup_urb_and_free_priv(struct vhci_device *vdev, __u32 seqnum);
 
-#define hardware		(&the_controller->pdev.dev)
+#define hardware (&the_controller->pdev.dev)
 
 static inline struct vhci_device *port_to_vdev(__u32 port)
 {
