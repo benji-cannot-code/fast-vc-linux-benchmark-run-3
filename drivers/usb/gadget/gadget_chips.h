ffe97a97be57f3e12a01bff834e72972eb5df367
Thomas Abraham <thomas.ab@samsung.com>
2011-05-07 22:28:04 +0200

    USB: Gadget: Add Samsung S3C24XX USB High-Speed controller driver
    The Samsung's S3C2416, S3C2443 and S3C2450 includes a USB High-Speed
    device controller module. This driver enables support for USB high-speed
    gadget functionality for the Samsung S3C24xx SoC's that include this
    controller.
    
    Signed-off-by: Thomas Abraham <thomas.ab@samsung.com>
    Signed-off-by: Sangbeom Kim <sbkim73@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Alexander Neumann <alexander@bumpern.de>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -137,6 +137,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define gadget_is_s3c_hsotg(g)    0
 #endif
 
+#ifdef CONFIG_USB_S3C_HSUDC
+#define gadget_is_s3c_hsudc(g) (!strcmp("s3c-hsudc", (g)->name))
+#else
+#define gadget_is_s3c_hsudc(g) 0
+#endif
+
 #ifdef CONFIG_USB_GADGET_EG20T
 #define	gadget_is_pch(g)	(!strcmp("pch_udc", (g)->name))
 #else
@@ -216,6 +222,8 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x28;
 	else if (gadget_is_renesas_usbhs(gadget))
 		return 0x29;
+	else if (gadget_is_s3c_hsudc(gadget))
+		return 0x30;
 
 	return -ENOENT;
 }
