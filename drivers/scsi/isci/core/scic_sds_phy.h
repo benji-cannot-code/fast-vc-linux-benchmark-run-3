236c5e62e412acae6bfc7832408f56a581862ea9
Dan Williams <dan.j.williams@intel.com>
2011-05-06 17:36:38 -0700

    isci: unify phy data structures
    Make scic_sds_phy a member of isci_phy and merge their lifetimes which
    means removing the phy table from scic_sds_controller in favor of the
    one at that isci_host level.
    
    Reported-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/scsi/isci/core/scic_sds_phy.h
+++ drivers/scsi/isci/core/scic_sds_phy.h
@@ -218,7 +218,6 @@ enum scic_sds_phy_protocol {
 	SCIC_SDS_MAX_PHY_PROTOCOLS
 };
 
-struct isci_phy;
 /**
  * struct scic_sds_phy - This structure  contains or references all of the data
  *    necessary to represent the core phy object and SCU harware protocol
@@ -227,11 +226,6 @@ struct isci_phy;
  *
  */
 struct scic_sds_phy {
-	/**
-	 * This field depicts the peer object for the phy.
-	 */
-	struct isci_phy *iphy;
-
 	/**
 	 * This field contains the information for the base phy state machine.
 	 */
--- drivers/scsi/isci/phy.h
+++ drivers/scsi/isci/phy.h
@@ -55,24 +55,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 
-#if !defined(_ISCI_PHY_H_)
+#ifndef _ISCI_PHY_H_
 #define _ISCI_PHY_H_
 
-#include "port.h"
-#include "host.h"
 #include <scsi/sas.h>
 #include <scsi/libsas.h>
-
-
-/**
- * struct isci_phy - This class implements the ISCI specific representation of
- *    the phy object.
- *
- *
- */
+#include "scic_sds_phy.h"
+#include "port.h"
+#include "host.h"
 
 struct isci_phy {
-	struct scic_sds_phy *sci_phy_handle;
+	struct scic_sds_phy sci;
 	struct asd_sas_phy sas_phy;
 	struct isci_port *isci_port;
 	u8 sas_addr[SAS_ADDR_SIZE];
@@ -83,17 +76,21 @@ struct isci_phy {
 	} frame_rcvd;
 };
 
-#define to_isci_phy(p)	\
-	container_of(p, struct isci_phy, sas_phy);
+static inline struct isci_phy *to_isci_phy(struct asd_sas_phy *sas_phy)
+{
+	struct isci_phy *iphy = container_of(sas_phy, typeof(*iphy), sas_phy);
 
-void isci_phy_init(
-	struct isci_phy *phy,
-	struct isci_host *isci_host,
-	int index);
+	return iphy;
+}
 
-int isci_phy_control(
-	struct asd_sas_phy *phy,
-	enum phy_func func,
-	void *buf);
+static inline struct isci_phy *sci_phy_to_iphy(struct scic_sds_phy *sci_phy)
+{
+	struct isci_phy *iphy = container_of(sci_phy, typeof(*iphy), sci);
+
+	return iphy;
+}
+
+void isci_phy_init(struct isci_phy *iphy, struct isci_host *ihost, int index);
+int isci_phy_control(struct asd_sas_phy *phy, enum phy_func func, void *buf);
 
 #endif /* !defined(_ISCI_PHY_H_) */
