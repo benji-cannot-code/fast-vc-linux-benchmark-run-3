d54eb5a40717a4670a822994e8a9675b0cf31622
Arend van Spriel <arend@broadcom.com>
2011-05-09 16:33:21 +0200

    staging: brcm80211: local used function made static
    In wlc_alloc.c function for setting ID field in struct wlc_bsscfg
    was only used locally. It has been made static and removed from
    header file.
    
    Cc: devel@linuxdriverproject.org
    Cc: linux-wireless@vger.kernel.org
    Reviewed-by: Roland Vossen <rvossen@broadcoom.com>
    Reviewed-by: Henry Ptasinski <henryp@broadcom.com>
    Reviewed-by: Brett Rudley <brudley@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/wlc_alloc.c
+++ drivers/staging/brcm80211/brcmsmac/wlc_alloc.c
@@ -44,7 +44,7 @@ static struct wlc_pub *wlc_pub_malloc(uint unit,
 static void wlc_pub_mfree(struct wlc_pub *pub);
 static void wlc_tunables_init(wlc_tunables_t *tunables, uint devid);
 
-void wlc_tunables_init(wlc_tunables_t *tunables, uint devid)
+static void wlc_tunables_init(wlc_tunables_t *tunables, uint devid)
 {
 	tunables->ntxd = NTXD;
 	tunables->nrxd = NRXD;
@@ -138,7 +138,7 @@ static void wlc_bsscfg_mfree(struct wlc_bsscfg *cfg)
 	kfree(cfg);
 }
 
-void wlc_bsscfg_ID_assign(struct wlc_info *wlc, struct wlc_bsscfg *bsscfg)
+static void wlc_bsscfg_ID_assign(struct wlc_info *wlc, struct wlc_bsscfg *bsscfg)
 {
 	bsscfg->ID = wlc->next_bsscfg_ID;
 	wlc->next_bsscfg_ID++;
