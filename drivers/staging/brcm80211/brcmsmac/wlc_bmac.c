c53e057680157ddb08443551eac5ad7e28317078
Henry Ptasinski <henryp@broadcom.com>
2011-05-09 16:33:09 +0200

    staging: brcm80211: move functions only used by brcmsmac out of utils
    The functions gitvar() and getintvar() are only used by brcmsmac, so move them
    out of the bcmutils.c file, which is shared by both brcmsmac and brcmfmac.
    
    Cc: devel@linuxdriverproject.org
    Cc: linux-wireless@vger.kernel.org
    Reviewed-by: Brett Rudley <brudley@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/wlc_bmac.c
+++ drivers/staging/brcm80211/brcmsmac/wlc_bmac.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmsrom.h>
 #include <bcmotp.h>
 #include <bcmutils.h>
+#include <bcmnvram.h>
 #include <wlioctl.h>
 #include <sbconfig.h>
 #include <sbchipc.h>
--- drivers/staging/brcm80211/brcmsmac/wlc_pmu.c
+++ drivers/staging/brcm80211/brcmsmac/wlc_pmu.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmdevs.h>
 #include <sbchipc.h>
 #include <bcmutils.h>
+#include <bcmnvram.h>
 #include "wlc_pmu.h"
 
 /*
