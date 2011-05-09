19cbdf550582dfd3aa4c8ac0179c977261da5ece
Stefan Ringel <stefan.ringel@arcor.de>
2011-05-09 16:53:54 -0300

    [media] tm6000: add eeprom
    Signed-off-by: Stefan Ringel <stefan.ringel@arcor.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/tm6000/tm6000-cards.c
+++ drivers/staging/tm6000/tm6000-cards.c
@@ -85,6 +85,7 @@ struct tm6000_board {
 
 	struct tm6000_input	vinput[3];
 	struct tm6000_input	rinput;
+
 	char		*ir_codes;
 };
 
@@ -92,7 +93,8 @@ struct tm6000_board tm6000_boards[] = {
 	[TM6000_BOARD_UNKNOWN] = {
 		.name         = "Unknown tm6000 video grabber",
 		.caps = {
-			.has_tuner    = 1,
+			.has_tuner	= 1,
+			.has_eeprom	= 1,
 		},
 		.gpio = {
 			.tuner_reset	= TM6000_GPIO_1,
@@ -119,6 +121,7 @@ struct tm6000_board tm6000_boards[] = {
 		.tuner_addr   = 0xc2 >> 1,
 		.caps = {
 			.has_tuner	= 1,
+			.has_eeprom	= 1,
 		},
 		.gpio = {
 			.tuner_reset	= TM6000_GPIO_1,
@@ -144,7 +147,7 @@ struct tm6000_board tm6000_boards[] = {
 		.tuner_addr   = 0xc2 >> 1,
 		.caps = {
 			.has_tuner	= 1,
-			.has_dvb	= 1,
+			.has_eeprom	= 1,
 		},
 		.gpio = {
 			.tuner_reset	= TM6000_GPIO_1,
