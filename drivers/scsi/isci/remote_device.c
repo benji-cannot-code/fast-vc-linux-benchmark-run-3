785ba79a078e59eaf29f61cd494aec2a914a9ead
Piotr Sawicki <piotr.sawicki@intel.com>
2011-05-10 23:50:32 +0000

    isci: unify request start handlers
    Unify the implementation in scic_sds_request_start and kill the state
    handler.
    
    Reported-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Piotr Sawicki <piotr.sawicki@intel.com>
    [remove scic_sds_request_constructed_state_start_handler]
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/scsi/isci/remote_device.c
+++ drivers/scsi/isci/remote_device.c
@@ -516,7 +516,7 @@ enum sci_status scic_sds_remote_device_start_io(struct scic_sds_controller *scic
 		if (status != SCI_SUCCESS)
 			break;
 
-		status = sci_req->state_handlers->start_handler(sci_req);
+		status = scic_sds_request_start(sci_req);
 		if (status != SCI_SUCCESS)
 			break;
 
@@ -541,7 +541,7 @@ enum sci_status scic_sds_remote_device_start_io(struct scic_sds_controller *scic
 			if (status != SCI_SUCCESS)
 				break;
 
-			status = sci_req->state_handlers->start_handler(sci_req);
+			status = scic_sds_request_start(sci_req);
 		} else
 			return SCI_FAILURE_INVALID_STATE;
 		break;
@@ -710,7 +710,7 @@ enum sci_status scic_sds_remote_device_start_task(struct scic_sds_controller *sc
 		if (status != SCI_SUCCESS)
 			goto out;
 
-		status = sci_req->state_handlers->start_handler(sci_req);
+		status = scic_sds_request_start(sci_req);
 		if (status != SCI_SUCCESS)
 			goto out;
 
