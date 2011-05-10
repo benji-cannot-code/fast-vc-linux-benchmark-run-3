787cb6a1336c2b3ee18f7ae748eca9588b782616
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-05-10 19:48:10 +0300

    OMAP: DSS2: Move panel-generic-dpi.h to include/video/
    arch/arm/plat-omap/include/plat/panel-generic-dpi.h is an include for
    the OMAP DSS panel driver for generic DPI displays. A more logical place
    for it is in include/video.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- arch/arm/mach-omap2/board-omap3stalker.c
+++ arch/arm/mach-omap2/board-omap3stalker.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/nand.h>
 #include <plat/usb.h>
 #include <video/omapdss.h>
-#include <plat/panel-generic-dpi.h>
+#include <video/omap-panel-generic-dpi.h>
 
 #include <plat/mcspi.h>
 #include <linux/input/matrix_keypad.h>
--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/common.h>
 #include <plat/usb.h>
 #include <plat/mmc.h>
-#include <plat/panel-generic-dpi.h>
+#include <video/omap-panel-generic-dpi.h>
 #include "timer-gp.h"
 
 #include "hsmmc.h"
