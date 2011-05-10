9a14b2d8a13ed791bd218f19d6bb47d5547b1401
K. Y. Srinivasan <kys@microsoft.com>
2011-05-10 07:55:52 -0700

    Staging: hv: blkvsc_drv: Move the assignment of driver name
    In preperation to eliminating blk_vsc_initialize(), move the assignment
    of driver name.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/blkvsc_drv.c
+++ drivers/staging/hv/blkvsc_drv.c
@@ -175,7 +175,6 @@ static int blk_vsc_initialize(struct hv_driver *driver)
 
 	stor_driver = hvdr_to_stordr(driver);
 
-	driver->name = drv_name;
 	memcpy(&driver->dev_type, &dev_type, sizeof(struct hv_guid));
 
 	return ret;
@@ -843,6 +842,7 @@ static int blkvsc_drv_init(void)
 	/* Callback to client driver to complete the initialization */
 	blk_vsc_initialize(&storvsc_drv->base);
 
+	drv->name = drv_name;
 	drv->driver.name = drv_name;
 
 	/* The driver belongs to vmbus */
