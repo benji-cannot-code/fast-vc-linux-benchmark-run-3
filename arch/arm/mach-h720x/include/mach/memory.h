f67023ba43ccd76308ec12cce2e421d9a7ece79f
Russell King <rmk+kernel@arm.linux.org.uk>
2011-05-11 16:06:29 +0100

    ARM: Replace platform definition of ISA_DMA_THRESHOLD/MAX_DMA_ADDRESS
    The values of ISA_DMA_THRESHOLD and MAX_DMA_ADDRESS are related; one is
    the physical/bus address, the other is the virtual address.  Both need
    to be kept in step, so rather than having platforms define both, allow
    them to define a single macro which sets both of these macros
    appropraitely.
    
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-h720x/include/mach/memory.h
+++ arch/arm/mach-h720x/include/mach/memory.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * There should not be more than (0xd0000000 - 0xc0000000)
  * bytes of RAM.
  */
-#define ISA_DMA_THRESHOLD	(PHYS_OFFSET + SZ_256M - 1)
-#define MAX_DMA_ADDRESS		(PAGE_OFFSET + SZ_256M)
+#define ARM_DMA_ZONE_SIZE	SZ_256M
 
 #endif
