37c120eb7440c65f4e56e9a9f2d3e1da8b0b47a0
Sage Weil <sage@newdream.net>
2011-05-12 16:13:54 -0700

    rbd: fix leak of ops struct
    The ops vector must be freed by the rbd_do_request caller.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- drivers/block/rbd.c
+++ drivers/block/rbd.c
@@ -997,6 +997,8 @@ static int rbd_do_op(struct request *rq,
 			     ops,
 			     num_reply,
 			     rbd_req_cb, 0, NULL);
+
+	rbd_destroy_ops(ops);
 done:
 	kfree(seg_name);
 	return ret;
@@ -1064,7 +1066,9 @@ static int rbd_req_sync_notify_ack(struct rbd_device *dev,
 {
 	struct ceph_osd_req_op *ops;
 	struct page **pages = NULL;
-	int ret = rbd_create_rw_ops(&ops, 1, CEPH_OSD_OP_NOTIFY_ACK, 0);
+	int ret;
+
+	ret = rbd_create_rw_ops(&ops, 1, CEPH_OSD_OP_NOTIFY_ACK, 0);
 	if (ret < 0)
 		return ret;
 
