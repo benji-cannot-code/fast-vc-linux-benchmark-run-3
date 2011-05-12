9cc0ed6254150a169d488a5ea13a7edca0f67351
Ondrej Zary <linux@rainbow-software.org>
2011-05-12 22:18:09 +0200

    ALSA: tea575x: remove unused card from struct
    struct snd_card *card is present in struct snd_tea575x but never used.
    Remove it.
    
    Signed-off-by: Ondrej Zary <linux@rainbow-software.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/tea575x-tuner.h
+++ include/sound/tea575x-tuner.h
@@ -43,7 +43,6 @@ struct snd_tea575x_ops {
 };
 
 struct snd_tea575x {
-	struct snd_card *card;
 	struct video_device *vd;	/* video device */
 	bool tea5759;			/* 5759 chip is present */
 	bool mute;			/* Device is muted? */
