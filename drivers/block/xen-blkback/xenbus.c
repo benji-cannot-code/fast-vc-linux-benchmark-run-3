ca6f1b9d9c4cdd2b4f001326a250881e00dc4421
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-05-12 18:02:28 -0400

    xen/blkback: Align the tabs on the structure.
    The recent changes caused this field of the structure to be offset a bit.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/block/xen-blkback/xenbus.c
+++ drivers/block/xen-blkback/xenbus.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct backend_info {
 	struct xenbus_device	*dev;
-	struct xen_blkif		*blkif;
+	struct xen_blkif	*blkif;
 	struct xenbus_watch	backend_watch;
 	unsigned		major;
 	unsigned		minor;
