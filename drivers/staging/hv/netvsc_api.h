190fbfd01cab8c8049db20bce081e9b0b43be9b3
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:25 -0700

    Staging: hv: Move the contents of vmbus.h to hyperv.h
    Move the contents of vmbus.h to hyperv.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/netvsc_api.h
+++ drivers/staging/hv/netvsc_api.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _NETVSC_API_H_
 
 #include "hyperv.h"
-#include "vmbus.h"
 
 /* Fwd declaration */
 struct hv_netvsc_packet;
