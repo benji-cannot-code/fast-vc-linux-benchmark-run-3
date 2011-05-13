87cfbb741e610389c22efa7c6384f85ec3afb6f8
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:22 -0700

    Staging: hv: Move the contents of channel.h to hyperv.h
    Move the contents of channel.h to hyperv.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/storvsc_drv.c
+++ drivers/staging/hv/storvsc_drv.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vmbus.h"
 #include "storvsc_api.h"
 #include "vstorage.h"
-#include "channel.h"
 
 static int storvsc_ringbuffer_size = STORVSC_RING_BUFFER_SIZE;
 
--- drivers/staging/hv/vmbus_drv.c
+++ drivers/staging/hv/vmbus_drv.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hv_api.h"
 #include "logging.h"
 #include "vmbus.h"
-#include "channel.h"
 #include "vmbus_private.h"
 
 
