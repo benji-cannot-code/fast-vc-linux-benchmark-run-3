1102c0238a3627ac7cb632417e67ae1eb249dabe
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:30 -0700

    Staging: hv: vmbus_drv: Move the content of hv.h to hyperv_vmbus.h
    Move the content of hv.h to hyperv_vmbus.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/vmbus_private.h
+++ drivers/staging/hv/vmbus_private.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _VMBUS_PRIVATE_H_
 #define _VMBUS_PRIVATE_H_
 
-#include "hv.h"
 #include "hyperv.h"
 #include "ring_buffer.h"
 #include <linux/list.h>
