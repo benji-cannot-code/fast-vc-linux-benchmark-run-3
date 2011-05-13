9fb928e24e7d55ee22c9ea2f0759715e9deab14c
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:18 -0700

    Staging: hv: Move the contents of the file version_info.h to hyperv.h
    Move the contents of the file version_info.h to hyperv.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/netvsc_drv.c
+++ drivers/staging/hv/netvsc_drv.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hyperv.h"
 #include "hv_api.h"
 #include "logging.h"
-#include "version_info.h"
 #include "vmbus.h"
 #include "netvsc_api.h"
 
