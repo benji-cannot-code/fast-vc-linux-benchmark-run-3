c0e1da5c77eafd2ed5aae9f67e1b8c2901e6bf49
Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>
2011-05-13 12:30:42 +0200

    i2c-nomadik: remove the unnecessary delay
    The delay in the driver seems to be not needed, so remove it.
    
    Signed-off-by: Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>
    Reviewed-by: Markus Grape <markus.grape@stericsson.com>
    Tested-by: Per Persson <per.xb.persson@stericsson.com>
    Tested-by: Chethan Krishna N <chethan.krishna@stericsson.com>
    Reviewed-by: Srinidhi Kasagar <srinidhi.kasagar@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>

--- drivers/i2c/busses/i2c-nomadik.c
+++ drivers/i2c/busses/i2c-nomadik.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
-#include <linux/delay.h>
 #include <linux/slab.h>
 #include <linux/interrupt.h>
 #include <linux/i2c.h>
@@ -105,9 +104,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* maximum threshold value */
 #define MAX_I2C_FIFO_THRESHOLD	15
-
-/* per-transfer delay, required for the hardware to stabilize */
-#define I2C_DELAY		150
 
 enum i2c_status {
 	I2C_NOP,
@@ -270,12 +266,6 @@ static int init_hw(struct nmk_i2c_dev *dev)
 	dev->cli.operation = I2C_NO_OPERATION;
 
 exit:
-	/*
-	 * TODO: What is this delay for?
-	 * Must be pretty pointless since the hw block
-	 * is frozen. Or?
-	 */
-	udelay(I2C_DELAY);
 	return stat;
 }
 
@@ -653,7 +643,6 @@ static int nmk_i2c_xfer(struct i2c_adapter *i2c_adap,
 
 				break;
 			}
-			udelay(I2C_DELAY);
 		}
 		if (status == 0)
 			break;
@@ -779,13 +768,8 @@ static irqreturn_t i2c_irq_handler(int irq, void *arg)
 			}
 		}
 
-		i2c_set_bit(dev->virtbase + I2C_ICR, I2C_IT_MTD);
-		i2c_set_bit(dev->virtbase + I2C_ICR, I2C_IT_MTDWS);
-
-		disable_interrupts(dev,
-				(I2C_IT_TXFNE | I2C_IT_TXFE | I2C_IT_TXFF
-					| I2C_IT_TXFOVR | I2C_IT_RXFNF
-					| I2C_IT_RXFF | I2C_IT_RXFE));
+		disable_all_interrupts(dev);
+		clear_all_interrupts(dev);
 
 		if (dev->cli.count) {
 			dev->result = -EIO;
