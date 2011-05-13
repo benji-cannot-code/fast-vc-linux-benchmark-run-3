b3bb2f36c6b9fa5c766ec0532f36c474b30cbd0b
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:35:15 -0700

    Staging: hv: netvsc_drv: Move the dmi_check code to netvsc_drv_init()
    In preparation to eliminating netvsc_init(), move the dmi_check code
    to netvsc_drv_init().
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/netvsc_drv.c
+++ drivers/staging/hv/netvsc_drv.c
@@ -459,6 +459,12 @@ static int netvsc_drv_init(void)
 	struct hv_driver *drv = &netvsc_drv;
 	int ret;
 
+	pr_info("initializing....");
+
+	if (!dmi_check_system(hv_netvsc_dmi_table))
+		return -ENODEV;
+
+
 	/* Callback to client driver to complete the initialization */
 	netvsc_initialize(drv);
 
@@ -472,11 +478,6 @@ static int netvsc_drv_init(void)
 
 static int __init netvsc_init(void)
 {
-	pr_info("initializing....");
-
-	if (!dmi_check_system(hv_netvsc_dmi_table))
-		return -ENODEV;
-
 	return netvsc_drv_init();
 }
 
