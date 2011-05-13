7973a172b363bba102c1d6a2fa3f41e1a1d9e266
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:34 -0700

    Staging: hv: stor: Create a common header file for storage drivers
    Create a common header file for storage drivers.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/storvsc.c
+++ drivers/staging/hv/storvsc.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 
 #include "hyperv.h"
+#include "hyperv_storage.h"
 #include "storvsc_api.h"
 #include "vstorage.h"
 
--- drivers/staging/hv/storvsc_drv.c
+++ drivers/staging/hv/storvsc_drv.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/scsi_dbg.h>
 
 #include "hyperv.h"
+#include "hyperv_storage.h"
 #include "storvsc_api.h"
 #include "vstorage.h"
 
