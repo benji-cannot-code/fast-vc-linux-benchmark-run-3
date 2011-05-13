001e339c80288e64181b6f2612740d8b936fc7f5
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:31 -0700

    Staging: hv: vmbus_drv: Move the contents of ring_buffer.h to hyperv_vmbus.h
    Move the contents of ring_buffer.h to hyperv_vmbus.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/ring_buffer.c
+++ drivers/staging/hv/ring_buffer.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "hyperv.h"
 #include "hyperv_vmbus.h"
-#include "ring_buffer.h"
 
 
 /* #defines */
--- drivers/staging/hv/vmbus_private.h
+++ drivers/staging/hv/vmbus_private.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _VMBUS_PRIVATE_H_
 
 #include "hyperv.h"
-#include "ring_buffer.h"
 #include <linux/list.h>
 #include <asm/sync_bitops.h>
 
