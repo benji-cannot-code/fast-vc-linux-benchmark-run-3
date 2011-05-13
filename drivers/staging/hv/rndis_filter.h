87923c6af3401255fec534c3d2d918936bdacc5b
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:40 -0700

    Staging: hv: netvsc: Move the contents of rndis.h to hyperv_net.h
    Move the contents of rndis.h to hyperv_net.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/rndis_filter.h
+++ drivers/staging/hv/rndis_filter.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __struct_bcount(x)
 
 
-#include "rndis.h"
 
 #define RNDIS_HEADER_SIZE	(sizeof(struct rndis_message) - \
 				 sizeof(union rndis_message_container))
