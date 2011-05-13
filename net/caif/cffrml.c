a8c721def66a9e79d1ac98cc639a7b225c750f70
sjur.brandeland@stericsson.com <sjur.brandeland@stericsson.com>
2011-05-13 02:43:59 +0000

    caif: Use rcu_read_lock in CAIF mux layer.
    Replace spin_lock with rcu_read_lock when accessing lists to layers
    and cache. While packets are in flight rcu_read_lock should not be held,
    instead ref-counters are used in combination with RCU.
    
    Signed-off-by: Sjur Brændeland <sjur.brandeland@stericsson.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/caif/cffrml.c
+++ net/caif/cffrml.c
@@ -146,3 +146,11 @@ static void cffrml_ctrlcmd(struct cflayer *layr, enum caif_ctrlcmd ctrl,
 	if (layr->up->ctrlcmd)
 		layr->up->ctrlcmd(layr->up, ctrl, layr->id);
 }
+
+void cffrml_put(struct cflayer *layr)
+{
+}
+
+void cffrml_hold(struct cflayer *layr)
+{
+}
