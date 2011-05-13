2222aa737a7998ebd0bec6d3dafa7a26923d90ea
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:19 -0700

    Staging: hv: Move the contents of vmbus_channel_interface.h to hyperv.h
    Move the contents of vmbus_channel_interface.h to hyperv.h
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/netvsc.h
+++ drivers/staging/hv/netvsc.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/list.h>
 #include "vmbus_packet_format.h"
-#include "vmbus_channel_interface.h"
+#include "hyperv.h"
 #include "netvsc_api.h"
 
 
