f4ecdd78bd9c23da4886654c8753bfcc043a68c0
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:29 -0700

    Staging: hv: vmbus_drv: Include the contents of hv_api.h in hyperv_vmbus.h
    Include the contents of hv_api.h in hyperv_vmbus.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/hv.h
+++ drivers/staging/hv/hv.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __HV_H__
 #define __HV_H__
 
-#include "hv_api.h"
+#include "hyperv_vmbus.h"
 
 enum {
 	VMBUS_MESSAGE_CONNECTION_ID	= 1,
--- drivers/staging/hv/netvsc_drv.c
+++ drivers/staging/hv/netvsc_drv.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/pkt_sched.h>
 
 #include "hyperv.h"
-#include "hv_api.h"
 #include "netvsc_api.h"
 
 struct net_device_context {
