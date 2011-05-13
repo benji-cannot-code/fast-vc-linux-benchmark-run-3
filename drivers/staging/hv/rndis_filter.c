0eb393b9066687d8fd2d555565b863dee05bb78f
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:35:09 -0700

    Staging: hv: netvsc_drv: Get rid of some unnecessary/dead code from rndis_filter.c
    After the cleanup that has been done, some code in rndis_filter.c
    is no longer needed; get rid of it.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/rndis_filter.c
+++ drivers/staging/hv/rndis_filter.c
@@ -31,11 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hyperv.h"
 #include "hyperv_net.h"
 
-/* Data types */
-struct rndis_filter_driver_object {
-	/* The original driver */
-	struct netvsc_driver inner_drv;
-};
 
 enum rndis_device_state {
 	RNDIS_DEV_UNINITIALIZED = 0,
@@ -80,8 +75,6 @@ static void rndis_filter_send_completion(void *ctx);
 static void rndis_filter_send_request_completion(void *ctx);
 
 
-/* The one and only */
-static struct rndis_filter_driver_object rndis_filter;
 
 static struct rndis_device *get_rndis_device(void)
 {
@@ -578,15 +571,6 @@ Exit:
 
 int rndis_filter_init(struct netvsc_driver *drv)
 {
-
-	/* Driver->Context = rndisDriver; */
-
-	memset(&rndis_filter, 0, sizeof(struct rndis_filter_driver_object));
-
-	/*rndisDriver->Driver = Driver;
-
-	ASSERT(Driver->OnLinkStatusChanged);
-	rndisDriver->OnLinkStatusChanged = Driver->OnLinkStatusChanged;*/
 
 	return 0;
 }
