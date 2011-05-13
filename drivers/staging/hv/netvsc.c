13ebb083d8b5b114150bb9079d5920e90cf10b22
K. Y. Srinivasan <kys@microsoft.com>
2011-05-12 19:34:23 -0700

    Staging: hv: Move the contents of logging.h to hyperv.h
    Move the contents of logging.h to hyperv.h.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Hank Janssen <hjanssen@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/netvsc.c
+++ drivers/staging/hv/netvsc.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "hyperv.h"
 #include "hv_api.h"
-#include "logging.h"
 #include "netvsc.h"
 #include "rndis_filter.h"
 
--- drivers/staging/hv/vmbus_drv.c
+++ drivers/staging/hv/vmbus_drv.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "hyperv.h"
 #include "hv_api.h"
-#include "logging.h"
 #include "vmbus.h"
 #include "vmbus_private.h"
 
