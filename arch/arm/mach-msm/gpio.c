134a63cab7fc8cf073949a0f742aafb7a107dc85
David Brown <davidb@codeaurora.org>
2011-05-16 13:32:15 -0700

    msm: gpiomux: Move public API to public header
    The gpiomux.h header contains some SOC ifdefs.  However, the API that
    is actually used by the GPIO driver only uses two functions that are
    general.  Move these general definitions into a public header file.
    
    Change-Id: Ia5df8af87dba268225598d56908e523bcfc24ef6
    Signed-off-by: David Brown <davidb@codeaurora.org>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/mach-msm/gpio.c
+++ arch/arm/mach-msm/gpio.c
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/module.h>
 #include <mach/cpu.h>
+#include <mach/msm_gpiomux.h>
 #include "gpio_hw.h"
-#include "gpiomux.h"
 
 #define FIRST_GPIO_IRQ MSM_GPIO_TO_INT(0)
 
