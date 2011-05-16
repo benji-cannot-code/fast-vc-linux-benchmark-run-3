b7a379f280eae8659deb91fb400c5ac6f928a08e
Andy Lutomirski <luto@mit.edu>
2011-05-16 15:12:47 +1000

    crypto: aesni-intel - Merge with fpu.ko
    Loading fpu without aesni-intel does nothing.  Loading aesni-intel
    without fpu causes modes like xts to fail.  (Unloading
    aesni-intel will restore those modes.)
    
    One solution would be to make aesni-intel depend on fpu, but it
    seems cleaner to just combine the modules.
    
    This is probably responsible for bugs like:
    https://bugzilla.redhat.com/show_bug.cgi?id=589390
    
    Signed-off-by: Andy Lutomirski <luto@mit.edu>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/x86/crypto/aesni-intel_glue.c
+++ arch/x86/crypto/aesni-intel_glue.c
@@ -141,6 +141,9 @@ asmlinkage void aesni_gcm_dec(void *ctx, u8 *out,
 			u8 *hash_subkey, const u8 *aad, unsigned long aad_len,
 			u8 *auth_tag, unsigned long auth_tag_len);
 
+int crypto_fpu_init(void);
+void crypto_fpu_exit(void);
+
 static inline struct
 aesni_rfc4106_gcm_ctx *aesni_rfc4106_gcm_ctx_get(struct crypto_aead *tfm)
 {
@@ -1258,6 +1261,8 @@ static int __init aesni_init(void)
 		return -ENODEV;
 	}
 
+	if ((err = crypto_fpu_init()))
+		goto fpu_err;
 	if ((err = crypto_register_alg(&aesni_alg)))
 		goto aes_err;
 	if ((err = crypto_register_alg(&__aesni_alg)))
@@ -1335,6 +1340,7 @@ blk_ecb_err:
 __aes_err:
 	crypto_unregister_alg(&aesni_alg);
 aes_err:
+fpu_err:
 	return err;
 }
 
@@ -1364,6 +1370,8 @@ static void __exit aesni_exit(void)
 	crypto_unregister_alg(&blk_ecb_alg);
 	crypto_unregister_alg(&__aesni_alg);
 	crypto_unregister_alg(&aesni_alg);
+
+	crypto_fpu_exit();
 }
 
 module_init(aesni_init);
