2e9c5a853659d0e5d5d44f42fd037e2ea6cb4bba
David Brown <davidb@codeaurora.org>
2011-05-16 13:57:39 -0700

    gpio_msm: Move Qualcomm v6 MSM driver into drivers
    Migrate the driver for the v6-based MSM chips into drivers/gpio.  The
    driver is unchanged, only moved.
    
    Change-Id: I03ba597b95b4d62b42da112a8efac88d67aa40f9
    Signed-off-by: David Brown <davidb@codeaurora.org>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- drivers/gpio/gpio-msm-v1.c
+++ drivers/gpio/gpio-msm-v1.c
@@ -1,8 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/mach-msm/gpio.c
- *
+/*
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
