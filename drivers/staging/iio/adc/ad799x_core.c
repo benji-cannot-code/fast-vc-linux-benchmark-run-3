739e0ec5e7a776f34ccab37fba3fe47c71ee5ba9
Jonathan Cameron <jic23@cam.ac.uk>
2011-05-18 14:41:54 +0100

    staging:iio:adc:ad799x removed unused headers.
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/ad799x_core.c
+++ drivers/staging/iio/adc/ad799x_core.c
@@ -24,11 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/interrupt.h>
-#include <linux/workqueue.h>
 #include <linux/device.h>
 #include <linux/kernel.h>
 #include <linux/sysfs.h>
-#include <linux/list.h>
 #include <linux/i2c.h>
 #include <linux/regulator/consumer.h>
 #include <linux/slab.h>
