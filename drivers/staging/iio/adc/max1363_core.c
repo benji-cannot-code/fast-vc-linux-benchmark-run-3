3d56709a4e131e6f735c99278b0c9a6fe94377e5
Jonathan Cameron <jic23@cam.ac.uk>
2011-05-18 14:41:23 +0100

    staging:iio:adc:max1363 move to irqchip based threaded irq triggering
    V2: Fixed missing free of pollfunc->name as pointed out by
    Michael Hennerich.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/max1363_core.c
+++ drivers/staging/iio/adc/max1363_core.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
   */
 
 #include <linux/interrupt.h>
-#include <linux/workqueue.h>
 #include <linux/device.h>
 #include <linux/kernel.h>
 #include <linux/sysfs.h>
@@ -1171,8 +1170,6 @@ static int __devinit max1363_probe(struct i2c_client *client,
 	st->reg = reg;
 	/* this is only used for device removal purposes */
 	i2c_set_clientdata(client, indio_dev);
-
-	atomic_set(&st->protect_ring, 0);
 
 	st->chip_info = &max1363_chip_info_tbl[id->driver_data];
 	st->client = client;
