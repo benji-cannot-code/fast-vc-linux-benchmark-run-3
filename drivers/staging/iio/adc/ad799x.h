6485349715ed162ec03d04a021f48dba3cacce7e
Michael Hennerich <michael.hennerich@analog.com>
2011-05-18 14:41:53 +0100

    staging:iio:adc:ad799x: Use private data space from iio_allocate_device
    Use private data space from iio_allocate_device
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Acked-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/ad799x.h
+++ drivers/staging/iio/adc/ad799x.h
@@ -104,7 +104,6 @@ struct ad799x_chip_info {
 };
 
 struct ad799x_state {
-	struct iio_dev			*indio_dev;
 	struct i2c_client		*client;
 	const struct ad799x_chip_info	*chip_info;
 	size_t				d_size;
