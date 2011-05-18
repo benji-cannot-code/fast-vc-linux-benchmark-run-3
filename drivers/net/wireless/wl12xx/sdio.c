1ff4cdf4bce3c2f18e492bb5600f3ee9f104f839
Eliad Peller <eliad@wizery.com>
2011-05-18 11:24:16 +0300

    wl12xx: remove unused crc7 references
    crc7 is used only in wl12xx_spi.
    
    Remove redundant crc7.h includes, and update Kconfig to select CRC7
    only if WL12XX_SPI is being selected.
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/sdio.c
+++ drivers/net/wireless/wl12xx/sdio.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/irq.h>
 #include <linux/module.h>
-#include <linux/crc7.h>
 #include <linux/vmalloc.h>
 #include <linux/mmc/sdio_func.h>
 #include <linux/mmc/sdio_ids.h>
