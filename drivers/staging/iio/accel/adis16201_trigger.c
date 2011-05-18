1ccf45aa29c3f9dc03eb654a7b4dca19c6e3a83f
Jonathan Cameron <jic23@cam.ac.uk>
2011-05-18 14:42:22 +0100

    staging:iio:trigger handle name attr in core, remove old alloc and register any control_attrs via struct device
    As the majority of triggers don't actually have any other control_attrs lets use the fact
    that struct device has a groups element when we do need to have these attributes registered.
    A vargs function is used to cut down on lots of building strings in every single driver
    just in order to pass them into the allocate.
    
    Also iio_allocate_trigger_named -> iio_allocate_trigger as there is no
    unamed version any more, so that is now just confusing.
    
    Blackfin tested and fixed by Michael Hennerich.
    
    V2: Elements from Michael Hennerich's patches for the ade7758
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/accel/adis16201_trigger.c
+++ drivers/staging/iio/accel/adis16201_trigger.c
@@ -12,17 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../trigger.h"
 #include "adis16201.h"
 
-static DEVICE_ATTR(name, S_IRUGO, iio_trigger_read_name, NULL);
-
-static struct attribute *adis16201_trigger_attrs[] = {
-	&dev_attr_name.attr,
-	NULL,
-};
-
-static const struct attribute_group adis16201_trigger_attr_group = {
-	.attrs = adis16201_trigger_attrs,
-};
-
 /**
  * adis16201_data_rdy_trigger_set_state() set datardy interrupt state
  **/
@@ -40,19 +29,11 @@ int adis16201_probe_trigger(struct iio_dev *indio_dev)
 {
 	int ret;
 	struct adis16201_state *st = indio_dev->dev_data;
-	char *name;
 
-	name = kasprintf(GFP_KERNEL,
-			 "adis16201-dev%d",
-			 indio_dev->id);
-	if (name == NULL) {
-		ret = -ENOMEM;
-		goto error_ret;
-	}
-	st->trig = iio_allocate_trigger_named(name);
+	st->trig = iio_allocate_trigger("adis16201-dev%d", indio_dev->id);
 	if (st->trig == NULL) {
 		ret = -ENOMEM;
-		goto error_free_name;
+		goto error_ret;
 	}
 	ret = request_irq(st->us->irq,
 			  &iio_trigger_generic_data_rdy_poll,
@@ -65,7 +46,6 @@ int adis16201_probe_trigger(struct iio_dev *indio_dev)
 	st->trig->owner = THIS_MODULE;
 	st->trig->private_data = st;
 	st->trig->set_trigger_state = &adis16201_data_rdy_trigger_set_state;
-	st->trig->control_attrs = &adis16201_trigger_attr_group;
 	ret = iio_trigger_register(st->trig);
 
 	/* select default trigger */
@@ -79,8 +59,6 @@ error_free_irq:
 	free_irq(st->us->irq, st->trig);
 error_free_trig:
 	iio_free_trigger(st->trig);
-error_free_name:
-	kfree(name);
 error_ret:
 	return ret;
 }
@@ -90,7 +68,6 @@ void adis16201_remove_trigger(struct iio_dev *indio_dev)
 	struct adis16201_state *state = indio_dev->dev_data;
 
 	iio_trigger_unregister(state->trig);
-	kfree(state->trig->name);
 	free_irq(state->us->irq, state->trig);
 	iio_free_trigger(state->trig);
 }
--- drivers/staging/iio/accel/adis16204_trigger.c
+++ drivers/staging/iio/accel/adis16204_trigger.c
@@ -13,17 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../trigger.h"
 #include "adis16204.h"
 
-static DEVICE_ATTR(name, S_IRUGO, iio_trigger_read_name, NULL);
-
-static struct attribute *adis16204_trigger_attrs[] = {
-	&dev_attr_name.attr,
-	NULL,
-};
-
-static const struct attribute_group adis16204_trigger_attr_group = {
-	.attrs = adis16204_trigger_attrs,
-};
-
 /**
  * adis16204_data_rdy_trigger_set_state() set datardy interrupt state
  **/
@@ -41,20 +30,11 @@ int adis16204_probe_trigger(struct iio_dev *indio_dev)
 {
 	int ret;
 	struct adis16204_state *st = indio_dev->dev_data;
-	char *name;
 
-	name = kasprintf(GFP_KERNEL,
-			 "adis16204-dev%d",
-			 indio_dev->id);
-	if (name == NULL) {
-		ret = -ENOMEM;
-		goto error_ret;
-	}
-
-	st->trig = iio_allocate_trigger_named(name);
+	st->trig = iio_allocate_trigger("adis16204-dev%d", indio_dev->id);
 	if (st->trig == NULL) {
 		ret = -ENOMEM;
-		goto error_free_name;
+		goto error_ret;
 	}
 
 	ret = request_irq(st->us->irq,
@@ -69,7 +49,6 @@ int adis16204_probe_trigger(struct iio_dev *indio_dev)
 	st->trig->owner = THIS_MODULE;
 	st->trig->private_data = st;
 	st->trig->set_trigger_state = &adis16204_data_rdy_trigger_set_state;
-	st->trig->control_attrs = &adis16204_trigger_attr_group;
 	ret = iio_trigger_register(st->trig);
 
 	/* select default trigger */
@@ -83,8 +62,6 @@ error_free_irq:
 	free_irq(st->us->irq, st->trig);
 error_free_trig:
 	iio_free_trigger(st->trig);
-error_free_name:
-	kfree(name);
 error_ret:
 	return ret;
 }
@@ -94,7 +71,6 @@ void adis16204_remove_trigger(struct iio_dev *indio_dev)
 	struct adis16204_state *state = indio_dev->dev_data;
 
 	iio_trigger_unregister(state->trig);
-	kfree(state->trig->name);
 	free_irq(state->us->irq, state->trig);
 	iio_free_trigger(state->trig);
 }
