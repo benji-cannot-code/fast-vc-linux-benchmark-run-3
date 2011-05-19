55935e713d1decceb43e6c0811d56dc5a2d3f3bd
Eric Dumazet <eric.dumazet@gmail.com>
2011-05-19 12:24:16 +0000

    macvlan: remove one synchronize_rcu() call
    When one macvlan device is dismantled, we can avoid one
    synchronize_rcu() call done after deletion from hash list, since caller
    will perform a synchronize_net() call after its ndo_stop() call.
    
    Add a new netdev->dismantle field to signal this dismantle intent.
    
    Reduces RTNL hold time.
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    CC: Patrick McHardy <kaber@trash.net>
    CC: Ben Greear <greearb@candelatech.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5127,7 +5127,7 @@ static void rollback_registered_many(struct list_head *head)
 			list_del(&dev->unreg_list);
 			continue;
 		}
-
+		dev->dismantle = true;
 		BUG_ON(dev->reg_state != NETREG_REGISTERED);
 	}
 
