80aca9fbae9251887ed87b02d238cc4d36a2fdaa
anish kumar <anish198519851985@gmail.com>
2011-05-19 20:58:42 +0530

    Staging: sm7xx: removing extra white spaces,redundant code and using macros
    This patch is to remove extra spaces,redundant code and using
    ARRAY_SIZE macros.
    
    Signed-off-by: anish kumar <anish198519851985@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/sm7xx/smtcfb.c
+++ drivers/staging/sm7xx/smtcfb.c
@@ -129,7 +129,7 @@ u16 smtc_ChipIDs[] = {
 	0x720
 };
 
-#define numSMTCchipIDs (sizeof(smtc_ChipIDs) / sizeof(u16))
+#define numSMTCchipIDs ARRAY_SIZE(smtc_ChipIDs)
 
 static struct fb_var_screeninfo smtcfb_var = {
 	.xres           = 1024,
@@ -843,7 +843,7 @@ static int __init sm712vga_setup(char *options)
 	smdbg("\nsm712vga_setup = %s\n", options);
 
 	for (index = 0;
-	     index < (sizeof(vesa_mode) / sizeof(struct vesa_mode_table));
+	     index < ARRAY_SIZE(vesa_mode);
 	     index++) {
 		if (strstr(options, vesa_mode[index].mode_index)) {
 			smtc_screen_info.lfb_width = vesa_mode[index].lfb_width;
@@ -877,7 +877,6 @@ static int __devinit smtcfb_pci_probe(struct pci_dev *pdev,
 	err = pci_enable_device(pdev);	/* enable SMTC chip */
 	if (err)
 		return err;
-	err = -ENOMEM;
 
 	hw.chipID = ent->device;
 	sprintf(name, "sm%Xfb", hw.chipID);
@@ -1007,7 +1006,7 @@ static int __devinit smtcfb_pci_probe(struct pci_dev *pdev,
 
 	return 0;
 
- failed:
+failed:
 	printk(KERN_INFO "Silicon Motion, Inc.  primary display init fail\n");
 
 	smtc_unmap_smem(sfb);
