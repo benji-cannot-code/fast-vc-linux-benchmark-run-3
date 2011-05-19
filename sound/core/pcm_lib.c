2c138766d21298035e52dd8422556aa1c9178d5a
Ben Gardiner <bengardiner@nanometrics.ca>
2011-05-18 23:52:38 -0400

    ALSA: sound, core, pcm_lib: fix xrun_log
    The xrun_log function was augmented with the in_interrupt parameter whereas the
    empty macro definition used when xrun logging is disabled was not.
    
    Add a third parameter to the empty macro definition so as to not cause compiler
    errors when xrun logging (CONFIG_SND_PCM_XRUN_DEBUG) is disabled.
    
    Signed-off-by: Ben Gardiner <bengardiner@nanometrics.ca>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/pcm_lib.c
+++ sound/core/pcm_lib.c
@@ -267,7 +267,7 @@ static void xrun_log_show(struct snd_pcm_substream *substream)
 #else /* ! CONFIG_SND_PCM_XRUN_DEBUG */
 
 #define hw_ptr_error(substream, fmt, args...) do { } while (0)
-#define xrun_log(substream, pos)	do { } while (0)
+#define xrun_log(substream, pos, in_interrupt)	do { } while (0)
 #define xrun_log_show(substream)	do { } while (0)
 
 #endif
