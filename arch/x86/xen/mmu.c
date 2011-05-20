feb0291efc70633ad884dda0b06a2a0466ef0fb3
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2011-05-20 16:34:44 -0700

    xen: fix compile without CONFIG_XEN_DEBUG_FS
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>

--- arch/x86/xen/mmu.c
+++ arch/x86/xen/mmu.c
@@ -2303,6 +2303,7 @@ out:
 }
 EXPORT_SYMBOL_GPL(xen_remap_domain_mfn_range);
 
+#ifdef CONFIG_XEN_DEBUG_FS
 static int p2m_dump_open(struct inode *inode, struct file *filp)
 {
 	return single_open(filp, p2m_dump_show, NULL);
@@ -2314,3 +2315,4 @@ static const struct file_operations p2m_dump_fops = {
 	.llseek		= seq_lseek,
 	.release	= single_release,
 };
+#endif /* CONFIG_XEN_DEBUG_FS */
