10ef14c729131e0abc5ce7ff1bf6ba3d2206d77d
Stephen Rothwell <sfr@canb.auug.org.au>
2011-05-20 11:11:53 +1000

    signal.h need a definition of struct task_struct
    This fixes these build errors on powerpc:
    
      In file included from arch/powerpc/mm/fault.c:18:
      include/linux/signal.h:239: error: 'struct task_struct' declared inside parameter list
      include/linux/signal.h:239: error: its scope is only this definition or declaration, which is probably not what you want
      include/linux/signal.h:240: error: 'struct task_struct' declared inside parameter list
      ..
    
    Exposed by commit e66eed651fd1 ("list: remove prefetching from regular
    list iterators"), which removed the include of <linux/prefetch.h> from
    <linux/list.h>.
    
    Without that, linux/signal.h no longer accidentally got the declaration
    of 'struct task_struct'.
    
    Fix by properly declaring the struct, rather than introducing any new
    header file dependency.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/signal.h
+++ include/linux/signal.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 #include <linux/list.h>
 
+struct task_struct;
+
 /* for sysctl */
 extern int print_fatal_signals;
 /*
