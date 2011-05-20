618443840bf179b78504213b8cb134cfb1e19be3
Sergei Trofimovich <slyfox@gentoo.org>
2011-05-20 20:20:30 +0000

    btrfs: don't spin in shrink_delalloc if there is nothing to free
    Observed as a large delay when --mixed filesystem is filled up.
    Test example:
    1. create tiny --mixed FS:
       $ dd if=/dev/zero of=2G.img seek=$((2048 * 1024 * 1024 - 1)) count=1 bs=1
       $ mkfs.btrfs --mixed 2G.img
       $ mount -oloop 2G.img /mnt/ut/
    2. Try to fill it up:
       $ dd if=/dev/urandom of=10M.file bs=10240 count=1024
       $ seq 1 256 | while read file_no; do echo $file_no; time cp 10M.file ${file_no}.copy; done
    
    Up to '200.copy' it goes fast, but when disk fills-up each -ENOSPC
    message takes 3 seconds to pop-up _every_ ENOSPC (and in usermode linux
    it's even more: 30-60 seconds!). (Maybe, time depends on kernel's timer resolution).
    
    No IO, no CPU load, just rescheduling. Some debugging revealed busy spinning
    in shrink_delalloc.
    
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>
    Reviewed-by: Josef Bacik <josef@redhat.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -3426,6 +3426,10 @@ static int shrink_delalloc(struct btrfs_trans_handle *trans,
 	if (reserved == 0)
 		return 0;
 
+	/* nothing to shrink - nothing to reclaim */
+	if (root->fs_info->delalloc_bytes == 0)
+		return 0;
+
 	max_reclaim = min(reserved, to_reclaim);
 
 	while (loops < 1024) {
