8f45d5387c9b59e9df0816b1e0241eecb8e1e6d5
Stefan Ringel <stefan.ringel@arcor.de>
2011-05-21 03:05:38 -0300

    [media] tm6000: fix uninitialized field, change prink to dprintk
    fix uninitialized field, change prink to dprintk
    
    Signed-off-by: Stefan Ringel <stefan.ringel@arcor.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/tm6000/tm6000-video.c
+++ drivers/staging/tm6000/tm6000-video.c
@@ -335,6 +335,7 @@ static int copy_streams(u8 *data, unsigned long len,
 			size = dev->isoc_ctl.size;
 			pos = dev->isoc_ctl.pos;
 			pktsize = dev->isoc_ctl.pktsize;
+			field = dev->isoc_ctl.field;
 		}
 		cpysize = (endp - ptr > size) ? size : endp - ptr;
 		if (cpysize) {
@@ -360,7 +361,8 @@ static int copy_streams(u8 *data, unsigned long len,
 				/* Need some code to copy pts */
 				u32 pts;
 				pts = *(u32 *)ptr;
-				printk(KERN_INFO "%s: field %d, PTS %x", dev->name, field, pts);
+				dprintk(dev, V4L2_DEBUG_ISOC, "field %d, PTS %x",
+					field, pts);
 				break;
 			}
 			}
@@ -372,6 +374,7 @@ static int copy_streams(u8 *data, unsigned long len,
 			dev->isoc_ctl.pos = pos + cpysize;
 			dev->isoc_ctl.size = size - cpysize;
 			dev->isoc_ctl.cmd = cmd;
+			dev->isoc_ctl.field = field;
 			dev->isoc_ctl.pktsize = pktsize - (endp - ptr);
 			ptr += endp - ptr;
 		} else {
