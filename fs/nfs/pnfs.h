98ff96169fea951c9fd11ee1c064bd518cc04452
Benny Halevy <bhalevy@panasas.com>
2011-05-22 19:53:48 +0300

    pnfs: encode_layoutcommit
    Add a layout driver method to encode the layout type specific
    opaque part of layout commit in-line in the xdr stream.
    
    Currently, the pnfs-objects layout driver uses it to encode metadata hints
    to the MDS and the blocks layout driver to commit provisionally allocated
    extents to the file.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.h
+++ fs/nfs/pnfs.h
@@ -109,6 +109,10 @@ struct pnfs_layoutdriver_type {
 	void (*encode_layoutreturn) (struct pnfs_layout_hdr *layoutid,
 				     struct xdr_stream *xdr,
 				     const struct nfs4_layoutreturn_args *args);
+
+	void (*encode_layoutcommit) (struct pnfs_layout_hdr *layoutid,
+				     struct xdr_stream *xdr,
+				     const struct nfs4_layoutcommit_args *args);
 };
 
 struct pnfs_layout_hdr {
