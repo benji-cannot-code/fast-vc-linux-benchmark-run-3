46ea1fd2b4144d4ed7e4a1505aa85289ca59f24a
Jamie Iles <jamie@jamieiles.com>
2011-05-23 17:15:46 +0100

    mtd: introduce mtd_device_(un)register()
    To prepare for the removal of add_mtd_device and add_mtd_partitions(),
    introduce mtd_device_register().  This will create partitions if they
    are supplied or register the whole device if there are no partitions.
    
    Once all drivers are converted to use mtd_device_register(),
    add_mtd_device() and add_mtd_partitions() will be made internal only.
    
    v2: move kerneldoc to implementation file and fixup some kerneldoc
    warnings.
    
    Artem: tweak comments: remove junk tabs, use dots consistently.
    
    Signed-off-by: Jamie Iles <jamie@jamieiles.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- include/linux/mtd/partitions.h
+++ include/linux/mtd/partitions.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Partition definition structure:
  *
  * An array of struct partition is passed along with a MTD object to
- * add_mtd_partitions() to create them.
+ * mtd_device_register() to create them.
  *
  * For each partition, these fields are available:
  * name: string that will be used to label the partition's MTD device.
