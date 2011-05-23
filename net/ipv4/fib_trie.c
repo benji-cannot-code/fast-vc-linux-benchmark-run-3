a224ce2ec1cc9a5c6598f6790ecd4733d350f1a6
David S. Miller <davem@davemloft.net>
2011-05-22 20:53:43 -0400

    ipv4: Include linux/prefetch.h in fib_trie.c
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/fib_trie.c
+++ net/ipv4/fib_trie.c
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 #include <linux/proc_fs.h>
 #include <linux/rcupdate.h>
+#include <linux/prefetch.h>
 #include <linux/skbuff.h>
 #include <linux/netlink.h>
 #include <linux/init.h>
