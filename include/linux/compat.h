8d569b283ade24d4d7cc0dd5636bd4bb3b9427e5
Stephen Rothwell <sfr@canb.auug.org.au>
2011-05-24 13:28:54 +1000

    compat: include aio_abi.h for aio_context_t
    fixes this build error on sparc64 (at least):
    
    In file included from arch/sparc/include/asm/siginfo.h:19,
                     from include/linux/signal.h:5,
                     from include/linux/sched.h:73,
                     from arch/sparc/kernel/asm-offsets.c:13:
    include/linux/compat.h:401: error: expected ')' before 'ctx_id'
    include/linux/compat.h:406: error: expected ')' before 'ctx_id'
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- include/linux/compat.h
+++ include/linux/compat.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/socket.h>
 #include <linux/if.h>
 #include <linux/fs.h>
+#include <linux/aio_abi.h>	/* for aio_context_t */
 
 #include <asm/compat.h>
 #include <asm/siginfo.h>
