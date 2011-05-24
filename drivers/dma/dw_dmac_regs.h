32521b61a98411ab891088b6714ae9736ed40835
Viresh Kumar <viresh.kumar@st.com>
2011-05-24 14:04:09 +0530

    dmaengine/dw_dmac: Update maintainer-ship
    Nobody is currently maintaining dw_dmac. We are using dw_dmac for SPEAr13xx and
    are currently maintaining it. After discussing with Vinod, sending this patch to
    update maintainer-ship of dw_dmac.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>
    Acked-by: Havard Skinnemoen <hskinnemoen@gmail.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/dw_dmac_regs.h
+++ drivers/dma/dw_dmac_regs.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Driver for the Synopsys DesignWare AHB DMA Controller
  *
  * Copyright (C) 2005-2007 Atmel Corporation
+ * Copyright (C) 2010-2011 ST Microelectronics
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
