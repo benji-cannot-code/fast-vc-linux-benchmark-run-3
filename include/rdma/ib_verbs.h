7ce22efb3c2c92f17ad815ff05d04335673731bf
Sean Hefty <sean.hefty@intel.com>
2011-05-23 19:42:29 -0700

    RDMA/core: Add XRC SRQ type
    XRC ("eXtended reliable connected") is an IB transport that provides
    better scalability by allowing senders to specify which shared receive
    queue (SRQ) should be used to receive a message, which essentially
    allows one transport context (QP connection) to serve multiple
    destinations (as long as they share an adapter, of course).
    
    XRC defines SRQs that are specifically used by XRC connections.  Expand
    the SRQ code to support XRC SRQs.  An XRC SRQ is currently restricted to
    only XRC use according to the IB XRC Annex.
    
    Portions of this patch were derived from work by
    Jack Morgenstein <jackm@dev.mellanox.co.il>.
    
    Signed-off-by: Sean Hefty <sean.hefty@intel.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -525,7 +525,8 @@ enum ib_cq_notify_flags {
 };
 
 enum ib_srq_type {
-	IB_SRQT_BASIC
+	IB_SRQT_BASIC,
+	IB_SRQT_XRC
 };
 
 enum ib_srq_attr_mask {
@@ -544,6 +545,13 @@ struct ib_srq_init_attr {
 	void		       *srq_context;
 	struct ib_srq_attr	attr;
 	enum ib_srq_type	srq_type;
+
+	union {
+		struct {
+			struct ib_xrcd *xrcd;
+			struct ib_cq   *cq;
+		} xrc;
+	} ext;
 };
 
 struct ib_qp_cap {
@@ -896,6 +904,14 @@ struct ib_srq {
 	void		       *srq_context;
 	enum ib_srq_type	srq_type;
 	atomic_t		usecnt;
+
+	union {
+		struct {
+			struct ib_xrcd *xrcd;
+			struct ib_cq   *cq;
+			u32		srq_num;
+		} xrc;
+	} ext;
 };
 
 struct ib_qp {
