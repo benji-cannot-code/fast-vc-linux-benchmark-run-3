bf1d24c02b905058cee859310095ca4ad141aa8d
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2011-05-24 11:39:02 +0300

    iwlagn: add a iwl_pci.[ch] files that will contain all PCI specific code
    Move some PCI functionality to the new iwl_pci.[ch] files:
    * the PCI_DEVICE_TABLE
    * the pci_driver struct definition
    * the PCI probe / remove functions
    * the PCI suspend / resume functions
    
    All these functions are now split: the trigger comes from the PCI layer which
    calls to the bus generic code located in the other files.
    
    This is the beginning only. There are still a lot of PCI related code needs
    to be gathered.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-dev.h
+++ drivers/net/wireless/iwlwifi/iwl-dev.h
@@ -49,6 +49,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-agn-rs.h"
 #include "iwl-agn-tt.h"
 
+#define DRV_NAME        "iwlagn"
+
 struct iwl_tx_queue;
 
 /* CT-KILL constants */
