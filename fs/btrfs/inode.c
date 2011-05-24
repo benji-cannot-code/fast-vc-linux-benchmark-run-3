0a1fdabbbbd09ee406939b89a6628445c23d7ff5
Sage Weil <sage@newdream.net>
2011-05-24 13:06:15 -0700

    btrfs: remove unnecessary dentry_unhash in rmdir/rename_dir
    Btrfs has no problems with lingering references to unlinked directory
    inodes.
    
    CC: Chris Mason <chris.mason@oracle.com>
    CC: linux-btrfs@vger.kernel.org
    Signed-off-by: Sage Weil <sage@newdream.net>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -3063,8 +3063,6 @@ static int btrfs_rmdir(struct inode *dir, struct dentry *dentry)
 	    inode->i_ino == BTRFS_FIRST_FREE_OBJECTID)
 		return -ENOTEMPTY;
 
-	dentry_unhash(dentry);
-
 	trans = __unlink_start_trans(dir, dentry);
 	if (IS_ERR(trans))
 		return PTR_ERR(trans);
@@ -6994,9 +6992,6 @@ static int btrfs_rename(struct inode *old_dir, struct dentry *old_dentry,
 	u64 index = 0;
 	u64 root_objectid;
 	int ret;
-
-	if (new_inode && S_ISDIR(new_dentry->d_inode->i_mode))
-		dentry_unhash(new_dentry);
 
 	if (new_dir->i_ino == BTRFS_EMPTY_SUBVOL_DIR_OBJECTID)
 		return -EPERM;
