308b1b202400223999f62e37a0af5f43a3430738
Amitkumar Karwar <akarwar@marvell.com>
2011-05-23 18:00:17 -0700

    mwifiex: remove support for old chipsets W0/W1
    For SD8787 A0/A1 chipsets we use the default firmware image
    file 'mrvl/sd8787_uapsta.bin'. So the chip rev id variable
    is removed.
    
    The global variable fw_name is moved to adapter structure
    so that we can support a different interface, such as PCIe,
    in future.
    
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwifiex/sdio.h
+++ drivers/net/wireless/mwifiex/sdio.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "main.h"
 
+#define SD8787_DEFAULT_FW_NAME "mrvl/sd8787_uapsta.bin"
+
 #define BLOCK_MODE	1
 #define BYTE_MODE	0
 
