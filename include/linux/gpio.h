890c283eaf29712b3e2548b32da3300ac4d93890
Lars-Peter Clausen <lars@metafoo.de>
2011-05-25 16:20:31 -0700

    gpio: make gpio_{request,free}_array gpio array parameter const
    gpio_{request,free}_array should not (and do not) modify the passed gpio
    array, so make the parameter const.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Eric Miao <eric.y.miao@gmail.com>
    Acked-by: Wolfram Sang <w.sang@pengutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/gpio.h
+++ include/linux/gpio.h
@@ -42,7 +42,7 @@ static inline int gpio_request_one(unsigned gpio,
 	return -ENOSYS;
 }
 
-static inline int gpio_request_array(struct gpio *array, size_t num)
+static inline int gpio_request_array(const struct gpio *array, size_t num)
 {
 	return -ENOSYS;
 }
@@ -55,7 +55,7 @@ static inline void gpio_free(unsigned gpio)
 	WARN_ON(1);
 }
 
-static inline void gpio_free_array(struct gpio *array, size_t num)
+static inline void gpio_free_array(const struct gpio *array, size_t num)
 {
 	might_sleep();
 
