4f60176e656585e6ddf2cfa3e1b81f9379c2fa17
Tim Gardner <tim.gardner@canonical.com>
2011-05-24 17:13:05 -0700

    ulimit: raise default hard ulimit on number of files to 4096
    Apps are increasingly using more than 1024 file descriptors.  See
    discussion in several distro bug trackers, e.g.  BugLink:
    http://bugs.launchpad.net/bugs/663090
    https://issues.rpath.com/browse/RPL-2054
    
    You don't want to raise the default soft limit, since that might break
    apps that use select(), but it's safe to raise the default hard limit;
    that way, apps that know they need lots of file descriptors can raise
    their soft limit without needing root, and without user intervention.
    
    Ubuntu is doing this with a kernel change because they have a policy of
    not changing kernel defaults in userland.
    
    While 4096 might not be enough for *all* apps, it seems to be plenty for
    the apps I've seen lately that are unhappy with 1024.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Cc: Dan Kegel <dank@kegel.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -24,7 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Fixed constants first: */
 #undef NR_OPEN
-#define INR_OPEN 1024		/* Initial setting for nfile rlimits */
+#define INR_OPEN_CUR 1024	/* Initial setting for nfile rlimits */
+#define INR_OPEN_MAX 4096	/* Hard limit for nfile rlimits */
 
 #define BLOCK_SIZE_BITS 10
 #define BLOCK_SIZE (1<<BLOCK_SIZE_BITS)
