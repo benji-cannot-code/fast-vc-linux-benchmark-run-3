6110289242573bb8a2eaf437de96f2568026726f
Paul Mundt <lethal@linux-sh.org>
2011-05-25 11:22:58 +0900

    ARM: mach-shmobile: Tidy up after SH7372 pm changes.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/arm/mach-shmobile/pm-sh7372.c
+++ arch/arm/mach-shmobile/pm-sh7372.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SBAR 0xe6180020
 #define APARMBAREA 0xe6f10020
 
-void sh7372_enter_core_standby(void)
+static void sh7372_enter_core_standby(void)
 {
 	void __iomem *smfram = (void __iomem *)SMFRAM;
 
