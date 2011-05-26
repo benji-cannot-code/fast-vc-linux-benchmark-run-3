77e69431345ef1c66209e621c536f394b8f658f8
Linus Torvalds <torvalds@linux-foundation.org>
2011-05-26 12:03:50 -0700

    Fix build with !HUGETLBFS
    I stupidly broke the case of CONFIG_HUGETLBFS=n when doing the
    conversion to vm_flags_t in commit ca16d140af91 ("mm: don't access
    vm_flags as 'int'").  And my 'allyesconfig' build didn't find it, for
    obvious reasons..
    
    Include <linux/mm_types.h> in <linux/hugetlb.h>.  The problem could have
    been avoided by just turning the hugetlb_file_setup() error wrapper into
    a macro, but mm_types.h is a reasonable include in this file.
    
    Reported-by: Richard -rw- Weinberger <richard.weinberger@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_HUGETLB_H
 #define _LINUX_HUGETLB_H
 
+#include <linux/mm_types.h>
 #include <linux/fs.h>
 #include <linux/hugetlb_inline.h>
 
