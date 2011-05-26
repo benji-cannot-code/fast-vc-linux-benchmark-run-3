32705c4b0d3e93e1550f021bf96836becbb40f9e
Carolyn Wyborny <carolyn.wyborny@intel.com>
2011-05-26 03:02:26 +0000

    igb: Change version to remove number after -k in kernel versions.
    This patch changes the way versioning is done for igb in the kernel by
    removing the number after the "k."  It has been determined that just the
    "k" is sufficient to identify a kernel version and the following number
    was used in an inconsistent manner.
    
    Signed-off-by: Carolyn Wyborny <carolyn.wyborny@intel.com>
    Tested-by: Jeff Pieper <jeffrey.e.pieper@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/igb/igb_main.c
+++ drivers/net/igb/igb_main.c
@@ -55,9 +55,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAJ 3
 #define MIN 0
 #define BUILD 6
-#define KFIX 2
 #define DRV_VERSION __stringify(MAJ) "." __stringify(MIN) "." \
-__stringify(BUILD) "-k" __stringify(KFIX)
+__stringify(BUILD) "-k"
 char igb_driver_name[] = "igb";
 char igb_driver_version[] = DRV_VERSION;
 static const char igb_driver_string[] =
