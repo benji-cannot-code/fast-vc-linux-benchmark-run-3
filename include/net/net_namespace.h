2c6d37e77b5deea37ca3662337b503173f7b3267
David S. Miller <davem@davemloft.net>
2011-05-26 16:40:37 -0400

    net: Add linux/sysctl.h includes where needed.
    Several networking headers were depending upon the implicit
    linux/sysctl.h include they get when including linux/net.h
    
    Add explicit includes.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/atomic.h>
 #include <linux/workqueue.h>
 #include <linux/list.h>
+#include <linux/sysctl.h>
 
 #include <net/netns/core.h>
 #include <net/netns/mib.h>
