311ba3c566a801669713e5a70565225dde6e429f
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-26 13:36:45 -0400

    net: inet_timewait_sock doesnt need <linux/module.h>
    There is nothing module specific in this header, and removing
    it doesn't seem to uncover any implicit dependencies either.
    Must be simply a vestige of an ancient legacy.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/net/inet_timewait_sock.h
+++ include/net/inet_timewait_sock.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kmemcheck.h>
 #include <linux/list.h>
-#include <linux/module.h>
 #include <linux/timer.h>
 #include <linux/types.h>
 #include <linux/workqueue.h>
