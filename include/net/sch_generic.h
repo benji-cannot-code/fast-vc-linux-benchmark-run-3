a55637be3b54e6233c809beeb06f4583a7af866c
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-26 13:38:59 -0400

    net: sch_generic remove redundant use of <linux/module.h>
    This file has modular references, but they are limited to
    those which are covered by the simple "struct module;"
    declaration used in dozens of other places.  In fact that
    declaration is already there (just outside of the context
    of this commit) so simply remove the include line.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/types.h>
 #include <linux/rcupdate.h>
-#include <linux/module.h>
 #include <linux/pkt_sched.h>
 #include <linux/pkt_cls.h>
 #include <net/gen_stats.h>
