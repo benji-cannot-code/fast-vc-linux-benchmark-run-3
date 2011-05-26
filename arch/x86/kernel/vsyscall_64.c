498302d8b03a9411980a769ce0c603eba2612787
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-26 12:33:18 -0400

    x86: fix implicit include of <linux/topology.h> in vsyscall_64
    In removing the presence of <linux/module.h> from some of the
    more common <linux/something.h> files, this implict include
    of <linux/topology.h> was uncovered.
    
      CC      arch/x86/kernel/vsyscall_64.o
      arch/x86/kernel/vsyscall_64.c: In function ‘vsyscall_set_cpu’:
      arch/x86/kernel/vsyscall_64.c:259: error: implicit declaration of function ‘cpu_to_node’
    
    Explicitly call it out so the cleanup can take place.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/x86/kernel/vsyscall_64.c
+++ arch/x86/kernel/vsyscall_64.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seqlock.h>
 #include <linux/jiffies.h>
 #include <linux/sysctl.h>
+#include <linux/topology.h>
 #include <linux/clocksource.h>
 #include <linux/getcpu.h>
 #include <linux/cpu.h>
