807642642cddc7e62f24f2f90c75d4cf057d1f53
Stephen Rothwell <sfr@canb.auug.org.au>
2011-05-26 11:09:10 +1000

    video: mb862xx: udelay need linux/delay.h
    Fix this:
    
      drivers/video/mb862xx/mb862xx-i2c.c: In function 'mb862xx_i2c_wait_event':
      drivers/video/mb862xx/mb862xx-i2c.c:25: error: implicit declaration of function 'udelay'
    
    caused by commit f8a6b1f44833 ("video: mb862xx: add support for
    controller's I2C bus adapter").
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/mb862xx/mb862xx-i2c.c
+++ drivers/video/mb862xx/mb862xx-i2c.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fb.h>
 #include <linux/i2c.h>
 #include <linux/io.h>
+#include <linux/delay.h>
 
 #include "mb862xxfb.h"
 #include "mb862xx_reg.h"
