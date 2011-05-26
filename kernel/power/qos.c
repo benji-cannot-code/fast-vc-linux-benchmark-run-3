182ca353f59ffe4d319b57b42fcfa5f2c3cd9a49
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-26 16:00:52 -0400

    kernel: Fix files explicitly needing EXPORT_SYMBOL infrastructure
    These files were getting <linux/module.h> via an implicit non-obvious
    path, but we want to crush those out of existence since they cost
    time during compiles of processing thousands of lines of headers
    for no reason.  Give them the lightweight header that just contains
    the EXPORT_SYMBOL infrastructure.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 
 #include <linux/uaccess.h>
+#include <linux/export.h>
 
 /*
  * locking rule: all changes to constraints or notifiers lists
