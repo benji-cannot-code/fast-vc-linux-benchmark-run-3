04cedb4a7b9538f6455a8f838ab83ada599976f8
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-27 13:39:01 -0400

    powerpc: io-workarounds.c was implicitly getting init_mm
    It was coming in via device.h --> module.h etc. but we want to
    clean that up.  So explicitly include the header where init_mm
    is being declared.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/powerpc/kernel/io-workarounds.c
+++ arch/powerpc/kernel/io-workarounds.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #undef DEBUG
 
 #include <linux/kernel.h>
+#include <linux/sched.h>	/* for init_mm */
 
 #include <asm/io.h>
 #include <asm/machdep.h>
