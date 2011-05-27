1f5d6ac4a5b08833bb1fbd0d88f2ffd28433b5e1
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-27 16:14:23 -0400

    tty: Add module.h to drivers/tty users who just expect it there.
    We are cleaning up the issue that means module.h is omnipresent.
    These tty users are the people who implictly are relying on that.
    Fix up the real users to call out the include that they really need.
    
    In the case of jsm_driver.c file, it had moduleparam.h but that
    isn't enough and it needs the full module.h
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/tty/serial/max3107.c
+++ drivers/tty/serial/max3107.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/spi/spi.h>
 #include <linux/freezer.h>
+#include <linux/module.h>
 #include "max3107.h"
 
 static const struct baud_table brg26_ext[] = {
