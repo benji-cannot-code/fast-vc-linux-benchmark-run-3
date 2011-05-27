d6ff86041011b4612ef5c1798df2bc1be554ed1f
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-27 13:27:45 -0400

    powerpc: Fix up implicit stat.h users
    They get it via module.h (via device.h) but we want to clean that up.
    When we do, we'll get things like:
    
    ibmebus.c:314: error: 'S_IWUSR' undeclared here (not in a function)
    vio.c:972: error: 'S_IWUSR' undeclared here (not in a function)
    
    so add in the stat header it is using explicitly in advance.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/powerpc/platforms/pseries/eeh_sysfs.c
+++ arch/powerpc/platforms/pseries/eeh_sysfs.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Send comments and feedback to Linas Vepstas <linas@austin.ibm.com>
  */
 #include <linux/pci.h>
+#include <linux/stat.h>
 #include <asm/ppc-pci.h>
 #include <asm/pci-bridge.h>
 
