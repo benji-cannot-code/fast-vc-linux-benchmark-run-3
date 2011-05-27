244c579e4eaf4bb03a94ee08e8046a6f11eea3a8
Bhanu Prakash Gollapudi <bprakash@broadcom.com>
2011-05-27 11:47:28 -0700

    [SCSI] bnx2fc: Bumped version to 1.0.2
    Signed-off-by: Bhanu Prakash Gollapudi <bprakash@broadcom.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/bnx2fc/bnx2fc.h
+++ drivers/scsi/bnx2fc/bnx2fc.h
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bnx2fc_constants.h"
 
 #define BNX2FC_NAME		"bnx2fc"
-#define BNX2FC_VERSION		"1.0.1"
+#define BNX2FC_VERSION		"1.0.2"
 
 #define PFX			"bnx2fc: "
 
