fd4ede1eef29f4cb1d6386c1ba350bc0c2a7a895
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-27 09:57:25 -0400

    rtc: Add module.h to implicit users in drivers/rtc
    The module.h was implicitly everywhere, but when we clean
    that up, the implicit users will compile fail; fix them up
    in advance.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/rtc/rtc-pcf2123.c
+++ drivers/rtc/rtc-pcf2123.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/rtc.h>
 #include <linux/spi/spi.h>
+#include <linux/module.h>
 
 #define DRV_VERSION "0.6"
 
