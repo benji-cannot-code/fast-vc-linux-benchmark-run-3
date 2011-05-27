e93a20773694b35a38a49a06160a88b419d54b04
Mattias Wallin <mattias.wallin@stericsson.com>
2011-05-27 10:30:34 +0200

    ARM: ux500: add support for clocksource DBX500 PRCMU
    This patch adds support for the DBX500 PRCMU clocksource
    to ux500 platforms.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Mattias Wallin <mattias.wallin@stericsson.co>
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu.c
+++ arch/arm/mach-ux500/cpu.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/mfd/db8500-prcmu.h>
 #include <linux/mfd/db5500-prcmu.h>
+#include <linux/clksrc-dbx500-prcmu.h>
 
 #include <asm/cacheflush.h>
 #include <asm/hardware/cache-l2x0.h>
@@ -141,7 +142,15 @@ static void __init ux500_timer_init(void)
 	else
 		ux500_unknown_soc();
 
+	if (cpu_is_u8500())
+		clksrc_dbx500_timer_base = __io_address(U8500_PRCMU_TIMER_4_BASE);
+	else if (cpu_is_u5500())
+		clksrc_dbx500_timer_base = __io_address(U5500_PRCMU_TIMER_3_BASE);
+	else
+		ux500_unknown_soc();
+
 	nmdk_timer_init();
+	clksrc_dbx500_prcmu_init();
 }
 
 struct sys_timer ux500_timer = {
