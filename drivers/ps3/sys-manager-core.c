96223596b1ad72a409ccdd1a08ea0401e8a74c4b
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-27 10:46:24 -0400

    powerpc: add export.h to files making use of EXPORT_SYMBOL
    With module.h being implicitly everywhere via device.h, the absence
    of explicitly including something for EXPORT_SYMBOL went unnoticed.
    Since we are heading to fix things up and clean module.h from the
    device.h file, we need to explicitly include these files now.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/ps3/sys-manager-core.c
+++ drivers/ps3/sys-manager-core.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/kernel.h>
+#include <linux/export.h>
 #include <asm/lv1call.h>
 #include <asm/ps3.h>
 
