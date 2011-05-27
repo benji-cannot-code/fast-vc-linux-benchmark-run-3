ef3c181afb1b896063dbbd1ac6798bb6f6d558bc
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-27 13:23:32 -0400

    powerpc: Fix up modules that should be including module.h
    So that we can clean up the header files and not be relying
    on implicit includes from device.h ---> module.h
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/powerpc/platforms/86xx/gef_gpio.c
+++ arch/powerpc/platforms/86xx/gef_gpio.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_gpio.h>
 #include <linux/gpio.h>
 #include <linux/slab.h>
+#include <linux/module.h>
 
 #define GEF_GPIO_DIRECT		0x00
 #define GEF_GPIO_IN		0x04
