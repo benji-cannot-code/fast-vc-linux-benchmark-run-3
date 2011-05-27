09efc0f5f00de07df949ca18324fee3172bb1b5c
David S. Miller <davem@davemloft.net>
2011-05-27 13:41:33 -0400

    net: Kill ratelimit.h dependency in linux/net.h
    Ingo Molnar noticed that we have this unnecessary ratelimit.h
    dependency in linux/net.h, which hid compilation problems from
    people doing builds only with CONFIG_NET enabled.
    
    Move this stuff out to a seperate net/net_ratelimit.h file and
    include that in the only two places where this thing is needed.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Acked-by: Ingo Molnar <mingo@elte.hu>

--- net/core/utils.c
+++ net/core/utils.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ratelimit.h>
 
 #include <net/sock.h>
+#include <net/net_ratelimit.h>
 
 #include <asm/byteorder.h>
 #include <asm/system.h>
