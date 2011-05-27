b51202941a301a772c307e9ae04e750e96d1c08a
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-26 20:52:10 -0400

    selinux: Add export.h to files using EXPORT_SYMBOL/THIS_MODULE
    The pervasive, but implicit presence of <linux/module.h> meant
    that things like this file would happily compile as-is.  But
    with the desire to phase out the module.h being included everywhere,
    point this file at export.h which will give it THIS_MODULE and
    the EXPORT_SYMBOL variants.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/posix-timers.h>
 #include <linux/syslog.h>
 #include <linux/user_namespace.h>
+#include <linux/export.h>
 
 #include "avc.h"
 #include "objsec.h"
