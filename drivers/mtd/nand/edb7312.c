268ab14f38e0e57e11638574a01a3db88b26a240
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2011-05-29 20:16:55 +0400

    mtd: edb7312 don't specify default parsing options
    Since 'cmdline, NULL' is now a default for parse_mtd_partitions, don't specify
    this in every driver, instead pass NULL to force parse_mtd_partitions
    to use default.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/nand/edb7312.c
+++ drivers/mtd/nand/edb7312.c
@@ -99,8 +99,6 @@ static int ep7312_device_ready(struct mtd_info *mtd)
 	return 1;
 }
 
-const char *part_probes[] = { "cmdlinepart", NULL };
-
 /*
  * Main initialization routine
  */
@@ -159,7 +157,7 @@ static int __init ep7312_init(void)
 		return -ENXIO;
 	}
 	ep7312_mtd->name = "edb7312-nand";
-	mtd_parts_nb = parse_mtd_partitions(ep7312_mtd, part_probes, &mtd_parts, 0);
+	mtd_parts_nb = parse_mtd_partitions(ep7312_mtd, NULL, &mtd_parts, 0);
 	if (mtd_parts_nb > 0)
 		part_type = "command line";
 	else
