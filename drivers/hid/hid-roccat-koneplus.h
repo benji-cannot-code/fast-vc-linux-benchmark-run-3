b6e207927aca9d42643862e6d57579cbbec000b1
Stefan Achatz <erazor_de@users.sourceforge.net>
2011-05-29 19:32:57 +0200

    HID: roccat: Add "Roccat Talk" support for koneplus
    Added binary sysfs attribute to support new functionality the manufacturer
    added to koneplus.
    
    Signed-off-by: Stefan Achatz <erazor_de@users.sourceforge.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-roccat-koneplus.h
+++ drivers/hid/hid-roccat-koneplus.h
@@ -15,6 +15,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
+struct koneplus_talk {
+	uint8_t command; /* KONEPLUS_COMMAND_TALK */
+	uint8_t size; /* always 0x10 */
+	uint8_t data[14];
+} __packed;
+
 /*
  * case 1: writes request 80 and reads value 1
  *
@@ -140,6 +146,7 @@ enum koneplus_commands {
 	KONEPLUS_COMMAND_INFO = 0x9,
 	KONEPLUS_COMMAND_E = 0xe,
 	KONEPLUS_COMMAND_SENSOR = 0xf,
+	KONEPLUS_COMMAND_TALK = 0x10,
 	KONEPLUS_COMMAND_FIRMWARE_WRITE = 0x1b,
 	KONEPLUS_COMMAND_FIRMWARE_WRITE_CONTROL = 0x1c,
 };
@@ -154,6 +161,7 @@ enum koneplus_usb_commands {
 	KONEPLUS_USB_COMMAND_TCU = 0x30c,
 	KONEPLUS_USB_COMMAND_E = 0x30e,
 	KONEPLUS_USB_COMMAND_SENSOR = 0x30f,
+	KONEPLUS_USB_COMMAND_TALK = 0x310,
 	KONEPLUS_USB_COMMAND_FIRMWARE_WRITE = 0x31b,
 	KONEPLUS_USB_COMMAND_FIRMWARE_WRITE_CONTROL = 0x31c,
 };
@@ -194,6 +202,7 @@ enum koneplus_mouse_report_button_types {
 	 * data2 = action
 	 */
 	KONEPLUS_MOUSE_REPORT_BUTTON_TYPE_MULTIMEDIA = 0xf0,
+	KONEPLUS_MOUSE_REPORT_TALK = 0xff,
 };
 
 enum koneplus_mouse_report_button_action {
