60778f277c2a3b718f69fffb61d298071f04c136
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2011-05-29 20:16:49 +0400

    mtd: bcm_umi_nand don't specify default parsing options
    Since 'cmdline, NULL' is now a default for parse_mtd_partitions, don't specify
    this in every driver, instead pass NULL to force parse_mtd_partitions
    to use default.
    
    Artem: tweaked the patch
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/nand/bcm_umi_nand.c
+++ drivers/mtd/nand/bcm_umi_nand.c
@@ -53,8 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const __devinitconst char gBanner[] = KERN_INFO \
 	"BCM UMI MTD NAND Driver: 1.00\n";
 
-const char *part_probes[] = { "cmdlinepart", NULL };
-
 #if NAND_ECC_BCH
 static uint8_t scan_ff_pattern[] = { 0xff };
 
@@ -497,9 +495,8 @@ static int __devinit bcm_umi_nand_probe(struct platform_device *pdev)
 		struct mtd_partition *partition_info;
 
 		board_mtd->name = "bcm_umi-nand";
-		nr_partitions =
-		    parse_mtd_partitions(board_mtd, part_probes,
-					 &partition_info, 0);
+		nr_partitions = parse_mtd_partitions(board_mtd, NULL,
+						     &partition_info, 0);
 
 		if (nr_partitions <= 0) {
 			printk(KERN_ERR "BCM UMI NAND: Too few partitions - %d\n",
