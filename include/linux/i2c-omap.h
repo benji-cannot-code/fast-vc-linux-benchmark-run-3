7123ad17f1d3a505b12f5ce8d248835715215460
Andy Green <andy@warmcat.com>
2011-05-30 07:43:08 -0700

    I2C: OMAP1/OMAP2+: add flags field to omap i2c platform data
    OMAP I2C driver can access the configuration flags through
    its platform data.
    
    Cc: patches@linaro.org
    Reported-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Andy Green <andy.green@linaro.org>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Acked-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- include/linux/i2c-omap.h
+++ include/linux/i2c-omap.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct omap_i2c_bus_platform_data {
 	u32		clkrate;
 	u32		rev;
+	u32		flags;
 	void		(*set_mpu_wkup_lat)(struct device *dev, long set);
 	int		(*device_enable) (struct platform_device *pdev);
 	int		(*device_shutdown) (struct platform_device *pdev);
