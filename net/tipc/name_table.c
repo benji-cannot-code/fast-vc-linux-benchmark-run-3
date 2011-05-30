c59d24cfe8aba2e763d6d5eb7026e187cfd23f90
Allan Stephens <allan.stephens@windriver.com>
2011-05-30 11:27:50 -0400

    tipc: Eliminate checks for empty zone list during name translation
    Gets rid of a pair of checks to see if a name sequence entry in
    TIPC's name table has an empty zone list. These checks are pointless
    since the zone list can never be empty (i.e. as soon as the list
    becomes empty the associated name sequence entry is deleted).
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- net/tipc/name_table.c
+++ net/tipc/name_table.c
@@ -575,14 +575,13 @@ u32 tipc_nametbl_translate(u32 type, u32 instance, u32 *destnode)
 						cluster_list);
 			list_move_tail(&publ->cluster_list,
 				       &info->cluster_list);
-		} else if (!list_empty(&info->zone_list)) {
+		} else {
 			publ = list_first_entry(&info->zone_list,
 						struct publication,
 						zone_list);
 			list_move_tail(&publ->zone_list,
 				       &info->zone_list);
-		} else
-			goto no_match;
+		}
 	}
 
 	/* Round-Robin Algorithm: */
@@ -599,8 +598,6 @@ u32 tipc_nametbl_translate(u32 type, u32 instance, u32 *destnode)
 					cluster_list);
 		list_move_tail(&publ->cluster_list, &info->cluster_list);
 	} else {
-		if (list_empty(&info->zone_list))
-			goto no_match;
 		publ = list_first_entry(&info->zone_list, struct publication,
 					zone_list);
 		list_move_tail(&publ->zone_list, &info->zone_list);
