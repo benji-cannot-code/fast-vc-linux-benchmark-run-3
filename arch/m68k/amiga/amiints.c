5c3c00347dc9bbfd886dcbead5294a353d9c552e
Geert Uytterhoeven <geert@linux-m68k.org>
2011-05-31 22:08:28 +0200

    m68k/amiga: Convert Amiga to genirq
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/amiga/amiints.c
+++ arch/m68k/amiga/amiints.c
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/errno.h>
+#ifdef CONFIG_GENERIC_HARDIRQS
+#include <linux/irq.h>
+#endif
 
 #include <asm/irq.h>
 #include <asm/traps.h>
