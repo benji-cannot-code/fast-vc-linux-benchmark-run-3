0cfa917f6f69a9a9f0fd11018236adc56bf58805
Axel Lin <axel.lin@gmail.com>
2011-05-31 20:55:44 +0800

    ARM: OMAP2: Add missing include of linux/gpio.h
    I got some build error like below while executing "make omap2plus_defconfig".
    
      CC      arch/arm/mach-omap2/board-2430sdp.o
    arch/arm/mach-omap2/board-2430sdp.c: In function 'omap_2430sdp_init':
    arch/arm/mach-omap2/board-2430sdp.c:247: error: 'GPIOF_OUT_INIT_LOW' undeclared (first use in this function)
    arch/arm/mach-omap2/board-2430sdp.c:247: error: (Each undeclared identifier is reported only once
    arch/arm/mach-omap2/board-2430sdp.c:247: error: for each function it appears in.)
    
    This patch fixes the build error by include linux/gpio.h instead of mach/gpio.h.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Cc: Syed Mohammed Khasim <x0khasim@ti.com>
    Cc: Grazvydas Ignotas <notasas@gmail.com>
    Cc: Steve Sakoman <steve@sakoman.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-overo.c
+++ arch/arm/mach-omap2/board-overo.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/init.h>
 #include <linux/io.h>
+#include <linux/gpio.h>
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/i2c/twl.h>
@@ -46,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/common.h>
 #include <video/omapdss.h>
 #include <video/omap-panel-generic-dpi.h>
-#include <mach/gpio.h>
 #include <plat/gpmc.h>
 #include <mach/hardware.h>
 #include <plat/nand.h>
