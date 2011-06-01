e764775f6968df757bb7f60eaa762875636fc314
Roland Vossen <rvossen@broadcom.com>
2011-06-01 13:45:06 +0200

    staging: brcm80211: added support for more bcm43224 based boards
    Patch created by Gottfried Haider. Add support for BCM943224HMB devices
    as found in recent Lenovo ThinkPads.
    
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/include/bcmdevs.h
+++ drivers/staging/brcm80211/include/bcmdevs.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BCM4319_D11N5G_ID	0x4339	/* 4319 802.11n 5G device */
 
 #define BCM43224_D11N_ID	0x4353	/* 43224 802.11n dualband device */
+#define BCM43224_D11N_ID_VEN1	0x0576	/* Vendor specific 43224 802.11n db */
 
 #define BCM43225_D11N2G_ID	0x4357	/* 43225 802.11n 2.4GHz device */
 
