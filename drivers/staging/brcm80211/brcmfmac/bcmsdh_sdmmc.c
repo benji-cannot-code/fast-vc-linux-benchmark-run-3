c37676cd3ca13adfd1ba5cedd897e2725e534f92
Franky Lin <frankyl@broadcom.com>
2011-06-01 13:45:41 +0200

    staging: brcm80211: remove sdioh.h from fullmac
    Remove unused head file
    
    Signed-off-by: Franky Lin <frankyl@broadcom.com>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/bcmsdh_sdmmc.c
+++ drivers/staging/brcm80211/brcmfmac/bcmsdh_sdmmc.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <bcmdevs.h>
 #include <bcmutils.h>
 #include <bcmwifi.h>
-#include <sdioh.h>		/* SDIO Host Controller Specification */
 #include <bcmsdbus.h>		/* bcmsdh to/from specific controller APIs */
 #include <sdiovar.h>		/* ioctl/iovars */
 
