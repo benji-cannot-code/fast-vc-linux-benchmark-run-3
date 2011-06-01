d8a3e5bb6679a20a817e6cb4725b23b56032602e
Kukjin Kim <kgene.kim@samsung.com>
2011-06-01 14:18:22 -0700

    [CPUFREQ] Move ARM Samsung cpufreq drivers to drivers/cpufreq/
    According to discussion of the ARM arch subsystem migration,
    ARM cpufreq drivers move to drivers/cpufreq. So this patch
    adds Kconfig.arm for ARM like x86 and adds Samsung S5PV210
    and EXYNOS4210 cpufreq driver compile in there.
    As a note, otherw will be moved.
    
    Cc: Dave Jones <davej@redhat.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/cpufreq/s5pv210-cpufreq.c
+++ drivers/cpufreq/s5pv210-cpufreq.c
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/mach-s5pv210/cpufreq.c
- *
+/*
  * Copyright (c) 2010 Samsung Electronics Co., Ltd.
  *		http://www.samsung.com
  *
