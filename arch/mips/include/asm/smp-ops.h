ff431e681ce041126d22683c51d855c589a722e4
Ralf Baechle <ralf@linux-mips.org>
2011-06-01 14:20:09 +0100

    MIPS: ARC: Fix build of firmware library on uniprocessor.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/smp-ops.h
+++ arch/mips/include/asm/smp-ops.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SMP_OPS_H
 #define __ASM_SMP_OPS_H
 
+#include <linux/errno.h>
+
 #ifdef CONFIG_SMP
 
 #include <linux/cpumask.h>
