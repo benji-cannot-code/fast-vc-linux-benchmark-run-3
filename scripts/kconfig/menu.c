0c3db4eeb22cd9c098587e65de34c69c4ee45da7
Arnaud Lacombe <lacombar@gmail.com>
2011-06-01 16:00:46 -0400

    kconfig: add missing <stdarg.h> inclusion
    This header is needed when using va_{start,end,copy}(3) functions family.
    
    Signed-off-by: Arnaud Lacombe <lacombar@gmail.com>

--- scripts/kconfig/menu.c
+++ scripts/kconfig/menu.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Released under the terms of the GNU GPL v2.0.
  */
 
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 
