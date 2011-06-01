3b92452ecfbe27be23b8dcc4a8e43b8c257db2cd
Arend van Spriel <arend@broadcom.com>
2011-06-01 13:45:24 +0200

    staging: brcm80211: remove functions from nicpci.h
    Couple of functions in the header file are actually only used by
    nicpci.c itself and as such made static and removed from the header
    file.
    
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/nicpci.h
+++ drivers/staging/brcm80211/brcmsmac/nicpci.h
@@ -57,17 +57,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bar0 + 12K accesses chipc core registers */
 #define PCI_16KB0_CCREGS_OFFSET	(12 * 1024)
 
-struct sbpcieregs;
 struct si_pub;
 
 extern u8 pcicore_find_pci_capability(void *dev, u8 req_cap_id,
 					 unsigned char *buf, u32 *buflen);
-extern uint pcie_readreg(struct sbpcieregs *pcieregs,
-			 uint addrtype, uint offset);
-extern uint pcie_writereg(struct sbpcieregs *pcieregs,
-			  uint addrtype, uint offset, uint val);
 
-extern u8 pcie_clkreq(void *pch, u32 mask, u32 val);
 extern u32 pcie_lcreg(void *pch, u32 mask, u32 val);
 
 extern void *pcicore_init(struct si_pub *sih, void *pdev, void *regs);
