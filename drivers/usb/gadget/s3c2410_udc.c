4d3b4f020116942b5088e97d1f91b83f0a1799ee
Bryan Wu <bryan.wu@canonical.com>
2011-06-02 12:51:29 +0800

    usb: gadget: include <linux/prefetch.h> to fix compiling error
    drivers/usb/gadget/at91_udc.c: In function 'write_fifo':
    drivers/usb/gadget/at91_udc.c:421:2: error: implicit declaration of function 'prefetch'
    make[3]: *** [drivers/usb/gadget/at91_udc.o] Error 1
    make[2]: *** [drivers/usb/gadget] Error 2
    make[2]: *** Waiting for unfinished jobs....
    
    Signed-off-by: Bryan Wu <bryan.wu@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/s3c2410_udc.c
+++ drivers/usb/gadget/s3c2410_udc.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/gpio.h>
+#include <linux/prefetch.h>
 
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
