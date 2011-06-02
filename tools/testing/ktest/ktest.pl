0fb6dcf2478b1f01f260dbac07d7badc79b05572
Steven Rostedt <srostedt@redhat.com>
2011-06-01 23:27:19 -0400

    ktest: Ignore unset values of the minconfig in config_bisect
    By ignoring the unset values of the minconfig in deciding
    what to test in the config_bisect can cause the problem
    config from being tested too.
    
    Just do not test the configs that are set in the minconfig.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -1481,7 +1481,7 @@ sub process_config_ignore {
 	or dodie "Failed to read $config";
 
     while (<IN>) {
-	if (/^(.*?(CONFIG\S*)(=.*| is not set))/) {
+	if (/^((CONFIG\S*)=.*)/) {
 	    $config_ignore{$2} = $1;
 	}
     }
