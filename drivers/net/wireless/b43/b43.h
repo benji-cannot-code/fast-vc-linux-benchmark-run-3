5c42ad4bfe030dfc9a9787728974b28f85c10cb8
Rafał Miłecki <zajec5@gmail.com>
2011-06-02 09:56:04 +0200

    b43: add an option to register BROKEN bcma driver
    It does nothing useful yet, so it is matched as BROKEN.
    For now this is just an option for b43, in future we may want to make
    b43 support SSB or BCMA (note: or, not xor).
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/b43.h
+++ drivers/net/wireless/b43/b43.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
 #include <linux/hw_random.h>
+#include <linux/bcma/bcma.h>
 #include <linux/ssb/ssb.h>
 #include <net/mac80211.h>
 
