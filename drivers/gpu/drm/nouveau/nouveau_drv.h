bc4c01021b64eadb7c6ad2a4d583eec5fdb3c9ab
Ben Skeggs <bskeggs@redhat.com>
2011-06-03 10:07:08 +1000

    drm/nv50-nvc0/vm: take client reference on shared channel vm
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_drv.h
+++ drivers/gpu/drm/nouveau/nouveau_drv.h
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct nouveau_fpriv {
 	spinlock_t lock;
 	struct list_head channels;
+	struct nouveau_vm *vm;
 };
 
 static inline struct nouveau_fpriv *
