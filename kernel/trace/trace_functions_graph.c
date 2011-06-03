a327e0a4ea04b9c06ae6343678fc782a079f1e9b
Jiri Olsa <jolsa@redhat.com>
2011-06-03 16:58:50 +0200

    tracing, function_graph: Remove lock-depth from latency trace
    The lock_depth was removed in commit
    e6e1e25 tracing: Remove lock_depth from event entry
    
    Removing the lock_depth info from function_graph latency header.
    
    With following commands:
    	# echo function_graph > ./current_tracer
    	# echo 1 > options/latency-format
    	# cat trace
    
    This is what it looked like before:
    # tracer: function_graph
    #
    # function_graph latency trace v1.1.5 on 3.0.0-rc1-tip+
    # --------------------------------------------------------------------
    # latency: 0 us, #59756/311298, CPU#0 | (M:preempt VP:0, KP:0, SP:0 HP:0 #P:2)
    #    -----------------
    #    | task: -0 (uid:0 nice:0 policy:0 rt_prio:0)
    #    -----------------
    #
    #      _-----=> irqs-off
    #     / _----=> need-resched
    #    | / _---=> hardirq/softirq
    #    || / _--=> preempt-depth
    #    ||| / _-=> lock-depth
    #    |||| /
    # CPU|||||  DURATION                  FUNCTION CALLS
    # |  |||||   |   |                     |   |   |   |
     0)  ....  0.068 us    |    } /* __rcu_read_unlock */
    ...
    
    This is what it looks like now:
    # tracer: function_graph
    #
    # function_graph latency trace v1.1.5 on 3.0.0-rc1-tip+
    # --------------------------------------------------------------------
    # latency: 0 us, #59747/1744610, CPU#0 | (M:preempt VP:0, KP:0, SP:0 HP:0 #P:2)
    #    -----------------
    #    | task: -0 (uid:0 nice:0 policy:0 rt_prio:0)
    #    -----------------
    #
    #      _-----=> irqs-off
    #     / _----=> need-resched
    #    | / _---=> hardirq/softirq
    #    || / _--=> preempt-depth
    #    ||| /
    # CPU||||  DURATION                  FUNCTION CALLS
    # |  ||||   |   |                     |   |   |   |
     0)  ..s.  1.641 us    |  } /* __rcu_process_callbacks */
    ...
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/r/1307113131-10045-5-git-send-email-jolsa@redhat.com
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/trace_functions_graph.c
+++ kernel/trace/trace_functions_graph.c
@@ -1299,8 +1299,7 @@ static void print_lat_header(struct seq_file *s, u32 flags)
 	seq_printf(s, "#%.*s / _----=> need-resched    \n", size, spaces);
 	seq_printf(s, "#%.*s| / _---=> hardirq/softirq \n", size, spaces);
 	seq_printf(s, "#%.*s|| / _--=> preempt-depth   \n", size, spaces);
-	seq_printf(s, "#%.*s||| / _-=> lock-depth      \n", size, spaces);
-	seq_printf(s, "#%.*s|||| /                     \n", size, spaces);
+	seq_printf(s, "#%.*s||| /                      \n", size, spaces);
 }
 
 static void __print_graph_headers_flags(struct seq_file *s, u32 flags)
@@ -1319,7 +1318,7 @@ static void __print_graph_headers_flags(struct seq_file *s, u32 flags)
 	if (flags & TRACE_GRAPH_PRINT_PROC)
 		seq_printf(s, "  TASK/PID       ");
 	if (lat)
-		seq_printf(s, "|||||");
+		seq_printf(s, "||||");
 	if (flags & TRACE_GRAPH_PRINT_DURATION)
 		seq_printf(s, "  DURATION   ");
 	seq_printf(s, "               FUNCTION CALLS\n");
@@ -1333,7 +1332,7 @@ static void __print_graph_headers_flags(struct seq_file *s, u32 flags)
 	if (flags & TRACE_GRAPH_PRINT_PROC)
 		seq_printf(s, "   |    |        ");
 	if (lat)
-		seq_printf(s, "|||||");
+		seq_printf(s, "||||");
 	if (flags & TRACE_GRAPH_PRINT_DURATION)
 		seq_printf(s, "   |   |      ");
 	seq_printf(s, "               |   |   |   |\n");
