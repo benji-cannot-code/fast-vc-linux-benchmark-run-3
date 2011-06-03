6b975939b961a011e1600a12774d59a087412342
Joe Perches <joe@perches.com>
2011-06-03 11:51:19 +0000

    net: Remove unnecessary semicolons
    Semicolons are not necessary after switch/while/for/if braces
    so remove them.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1520,7 +1520,7 @@ int hci_recv_fragment(struct hci_dev *hdev, int type, void *data, int count)
 
 		data += (count - rem);
 		count = rem;
-	};
+	}
 
 	return rem;
 }
@@ -1555,7 +1555,7 @@ int hci_recv_stream_fragment(struct hci_dev *hdev, void *data, int count)
 
 		data += (count - rem);
 		count = rem;
-	};
+	}
 
 	return rem;
 }
