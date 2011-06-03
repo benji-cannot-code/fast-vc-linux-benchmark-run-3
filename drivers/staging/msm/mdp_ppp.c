9827c464f8f12f1542fed888a324d4abb8947686
Joe Perches <joe@perches.com>
2011-06-03 02:28:50 -0700

    staging: msm: Use angle brackets for system includes
    Use the normal include style.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/msm/mdp_ppp.c
+++ drivers/staging/msm/mdp_ppp.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/file.h>
 #include <linux/major.h>
 
-#include "linux/proc_fs.h"
+#include <linux/proc_fs.h>
 
 #include <mach/hardware.h>
 #include <linux/io.h>
--- drivers/staging/msm/mdp_ppp_v20.c
+++ drivers/staging/msm/mdp_ppp_v20.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/fb.h>
-#include "linux/proc_fs.h"
+#include <linux/proc_fs.h>
 
 #include <mach/hardware.h>
 #include <linux/io.h>
