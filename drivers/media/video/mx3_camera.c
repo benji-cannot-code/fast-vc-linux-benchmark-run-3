1b3d5f046be004cebecdb2031ff7bdbce17f7f2f
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2011-06-04 14:11:10 -0300

    [media] V4L: mx3_camera: remove redundant calculations
    soc_camera core now performs the standard .bytesperline and .sizeimage
    calculations internally, no need to duplicate in drivers.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/mx3_camera.c
+++ drivers/media/video/mx3_camera.c
@@ -913,12 +913,6 @@ static int mx3_camera_set_fmt(struct soc_camera_device *icd,
 	pix->colorspace		= mf.colorspace;
 	icd->current_fmt	= xlate;
 
-	pix->bytesperline = soc_mbus_bytes_per_line(pix->width,
-						    xlate->host_fmt);
-	if (pix->bytesperline < 0)
-		return pix->bytesperline;
-	pix->sizeimage = pix->height * pix->bytesperline;
-
 	dev_dbg(icd->dev.parent, "Sensor set %dx%d\n", pix->width, pix->height);
 
 	return ret;
@@ -945,12 +939,6 @@ static int mx3_camera_try_fmt(struct soc_camera_device *icd,
 		pix->height = 4096;
 	if (pix->width > 4096)
 		pix->width = 4096;
-
-	pix->bytesperline = soc_mbus_bytes_per_line(pix->width,
-						    xlate->host_fmt);
-	if (pix->bytesperline < 0)
-		return pix->bytesperline;
-	pix->sizeimage = pix->height * pix->bytesperline;
 
 	/* limit to sensor capabilities */
 	mf.width	= pix->width;
