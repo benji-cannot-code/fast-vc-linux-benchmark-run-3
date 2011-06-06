ca3c5f3b8c5922ec4b7585ce7e0dba1e4a8d98c3
Mike McCormack <mikem@ring3k.org>
2011-06-06 23:12:53 +0900

    rtlwifi: Free skb in one place
    Signed-off-by: Mike McCormack <mikem@ring3k.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/pci.c
+++ drivers/net/wireless/rtlwifi/pci.c
@@ -728,11 +728,9 @@ static void _rtl_pci_rx_interrupt(struct ieee80211_hw *hw)
 		    (rtlpriv->rtlhal.current_bandtype == BAND_ON_2_4G) &&
 		     (ieee80211_is_beacon(fc) ||
 		     ieee80211_is_probe_resp(fc))) {
-			dev_kfree_skb_any(skb);
+			;
 		} else {
-			if (unlikely(!rtl_action_proc(hw, skb, false))) {
-				dev_kfree_skb_any(skb);
-			} else {
+			if (likely(rtl_action_proc(hw, skb, false))) {
 				struct sk_buff *uskb = NULL;
 				u8 *pdata;
 				uskb = dev_alloc_skb(skb->len + 128);
@@ -740,7 +738,6 @@ static void _rtl_pci_rx_interrupt(struct ieee80211_hw *hw)
 				       &rx_status, sizeof(rx_status));
 				pdata = (u8 *)skb_put(uskb, skb->len);
 				memcpy(pdata, skb->data, skb->len);
-				dev_kfree_skb_any(skb);
 
 				ieee80211_rx_irqsafe(hw, uskb);
 			}
@@ -752,6 +749,7 @@ static void _rtl_pci_rx_interrupt(struct ieee80211_hw *hw)
 			tasklet_schedule(&rtlpriv->works.ips_leave_tasklet);
 		}
 
+		dev_kfree_skb_any(skb);
 		skb = new_skb;
 
 		rtlpci->rx_ring[rx_queue_idx].rx_buf[index] = skb;
