feea2502a649721b36f715e60e68d25f462ccbdd
Santosh Shilimkar <santosh.shilimkar@ti.com>
2011-06-06 20:28:23 +0530

    ARM: OMAP: PM: Add support to allocate the memory for secure RAM
    Allocate the memory to save secure ram context which needs
    to be done when MPU is hitting OFF mode.
    
    The ROM code expects a physical address to this memory
    and hence use memblock APIs to reserve this memory as part
    of .reserve() callback. Maximum size as per secure RAM requirements
    is allocated.
    
    To keep omap1 build working, omap-secure.h file is created
    under plat-omap directory.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Acked-by: Jean Pihet <j-pihet@ti.com>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Tested-by: Vishwanath BS <vishwanath.bs@ti.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- arch/arm/mach-omap2/omap-secure.c
+++ arch/arm/mach-omap2/omap-secure.c
@@ -14,10 +14,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/io.h>
+#include <linux/memblock.h>
 
 #include <asm/cacheflush.h>
 
 #include <mach/omap-secure.h>
+
+static phys_addr_t omap_secure_memblock_base;
 
 /**
  * omap_sec_dispatcher: Routine to dispatch low power secure
@@ -50,4 +53,30 @@ u32 omap_secure_dispatcher(u32 idx, u32 flag, u32 nargs, u32 arg1, u32 arg2,
 	ret = omap_smc2(idx, flag, __pa(param));
 
 	return ret;
+}
+
+/* Allocate the memory to save secure ram */
+int __init omap_secure_ram_reserve_memblock(void)
+{
+	phys_addr_t paddr;
+	u32 size = OMAP_SECURE_RAM_STORAGE;
+
+	size = ALIGN(size, SZ_1M);
+	paddr = memblock_alloc(size, SZ_1M);
+	if (!paddr) {
+		pr_err("%s: failed to reserve %x bytes\n",
+				__func__, size);
+		return -ENOMEM;
+	}
+	memblock_free(paddr, size);
+	memblock_remove(paddr, size);
+
+	omap_secure_memblock_base = paddr;
+
+	return 0;
+}
+
+phys_addr_t omap_secure_ram_mempool_base(void)
+{
+	return omap_secure_memblock_base;
 }
