f9fef39ecf0c04e5a064b64de15b5f203720115c
Grant Likely <grant.likely@secretlab.ca>
2011-06-06 09:37:58 -0600

    gpio/mxs: Move Freescale mxs gpio driver to drivers/gpio
    GPIO drivers are getting moved to drivers/gpio for cleanup and
    consolidation.  This patch moves the mxs driver.  Follow up patches
    will clean it up and make it a fine upstanding example of a gpio
    driver.
    
    v2: Removed header file entirely and put struct definition directly
        into driver.  The struct isn't used anywhere else in the kernel.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Tested-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/gpio/gpio-mxs.c
+++ drivers/gpio/gpio-mxs.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/mx28.h>
 #include <asm-generic/bug.h>
 
-#include "gpio.h"
-
 static struct mxs_gpio_port *mxs_gpio_ports;
 static int gpio_table_size;
 
@@ -50,6 +48,15 @@ static int gpio_table_size;
 #define GPIO_INT_HIGH_LEV	0x3
 #define GPIO_INT_LEV_MASK	(1 << 0)
 #define GPIO_INT_POL_MASK	(1 << 1)
+
+struct mxs_gpio_port {
+	void __iomem *base;
+	int id;
+	int irq;
+	int irq_high;
+	int virtual_irq_start;
+	struct gpio_chip chip;
+};
 
 /* Note: This driver assumes 32 GPIOs are handled in one register */
 
