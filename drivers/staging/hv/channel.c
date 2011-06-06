28f8cc4e0d4aa92dbd435ae9c40f6e2271ef6b01
K. Y. Srinivasan <kys@microsoft.com>
2011-06-06 15:49:45 -0700

    Staging: hv: vmbus: Get rid of the timer based handling of channel events
    Get rid of the timer based handling of channel events.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/channel.c
+++ drivers/staging/hv/channel.c
@@ -890,7 +890,6 @@ void vmbus_onchannel_event(struct vmbus_channel *channel)
 
 	channel->onchannel_callback(channel->channel_callback_context);
 
-	mod_timer(&channel->poll_timer, jiffies + usecs_to_jiffies(100));
 }
 
 /*
