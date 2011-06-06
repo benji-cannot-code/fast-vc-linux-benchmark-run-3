0374c4370549c6e7d80f52f08f2a1628939a87d0
K. Y. Srinivasan <kys@microsoft.com>
2011-06-06 15:49:46 -0700

    Staging: hv: vmbus: Get rid of the call to dump channel state in channel event handler
    Get rid of the call to dump channel state in channel event handler.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Abhishek Kane <v-abkane@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/channel.c
+++ drivers/staging/hv/channel.c
@@ -886,7 +886,6 @@ EXPORT_SYMBOL_GPL(vmbus_recvpacket_raw);
  */
 void vmbus_onchannel_event(struct vmbus_channel *channel)
 {
-	dump_vmbus_channel(channel);
 
 	channel->onchannel_callback(channel->channel_callback_context);
 
