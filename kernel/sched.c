078bab682ed7c8e05356c8baeb0256f85ce5a820
Frederic Weisbecker <fweisbec@gmail.com>
2011-06-07 22:53:39 +0200

    sched: Remove pointless in_atomic() definition check
    It's really supposed to be defined here. If it's not then
    we actually want the build to crash so that we know it,
    and not keep it silent.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- kernel/sched.c
+++ kernel/sched.c
@@ -8029,7 +8029,6 @@ static inline int preempt_count_equals(int preempt_offset)
 
 void __might_sleep(const char *file, int line, int preempt_offset)
 {
-#ifdef in_atomic
 	static unsigned long prev_jiffy;	/* ratelimiting */
 
 	if ((preempt_count_equals(preempt_offset) && !irqs_disabled()) ||
@@ -8051,7 +8050,6 @@ void __might_sleep(const char *file, int line, int preempt_offset)
 	if (irqs_disabled())
 		print_irqtrace_events(current);
 	dump_stack();
-#endif
 }
 EXPORT_SYMBOL(__might_sleep);
 #endif
