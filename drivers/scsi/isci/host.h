fdae6414d9b8a6ec0733135ea4c3894ec63692a9
Dan Williams <dan.j.williams@intel.com>
2011-06-07 18:50:55 -0700

    isci: cleanup/optimize pool implementation
    The circ_buf macros are ~6% faster, as measured by perf, because they take
    advantage of power-of-two math assumptions i.e. no test and branch for
    rollover. Their semantics are clearer than the hidden side effects in pool.h
    (like sci_pool_get() which hides an assignment).
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/scsi/isci/host.h
+++ drivers/scsi/isci/host.h
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "remote_device.h"
 #include "phy.h"
-#include "pool.h"
 #include "isci.h"
 #include "remote_node_table.h"
 #include "registers.h"
@@ -179,11 +178,6 @@ struct scic_sds_controller {
 	 * This field is the free RNi data structure
 	 */
 	struct scic_remote_node_table available_remote_nodes;
-
-	/**
-	 * This field is the TCi pool used to manage the task context index.
-	 */
-	SCI_POOL_CREATE(tci_pool, u16, SCI_MAX_IO_REQUESTS);
 
 	/**
 	 * This filed is the struct scic_power_control data used to controll when direct
@@ -311,6 +305,10 @@ struct scic_sds_controller {
 
 struct isci_host {
 	struct scic_sds_controller sci;
+	u16 tci_head;
+	u16 tci_tail;
+	u16 tci_pool[SCI_MAX_IO_REQUESTS];
+
 	union scic_oem_parameters oem_parameters;
 
 	int id; /* unique within a given pci device */
@@ -423,8 +421,6 @@ enum scic_sds_controller_states {
 	 */
 	SCIC_FAILED,
 };
-
-
 
 /**
  * struct isci_pci_info - This class represents the pci function containing the
