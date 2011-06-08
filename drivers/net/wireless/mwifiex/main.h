cd9c25d8e5028352a57fcbc36b694b4023313cd1
Amitkumar Karwar <akarwar@marvell.com>
2011-06-08 20:39:20 +0530

    mwifiex: get rid of global adapter pointer
    1) Avoid global adapter pointer usage
       a) in sdio.c by moving some code from mwifiex_cleanup_module() to
          mwifiex_sdio_remove().
       b) in main.c by passing an extra parameter to few functions.
    2) Add new variable "user_rmmod" to identify if mwifiex_sdio_remove()
       callback function is called in card removal or rmmod context. These
       code changes are adapted from Libertas driver.
    3) Remove unnecessary NULL pointer check for "func" pointer in
       mwifiex_sdio_remove().
    
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Yogesh Ashok Powar <yogeshp@marvell.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwifiex/main.h
+++ drivers/net/wireless/mwifiex/main.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "fw.h"
 
 extern const char driver_version[];
-extern struct mwifiex_adapter *g_adapter;
 
 enum {
 	MWIFIEX_ASYNC_CMD,
