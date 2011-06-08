36e1d19170e622633025deea915f713ec8eda3e2
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2011-06-08 10:47:25 +0200

    avr32/at32ap: fix mapping of platform device id for USART
    This patch will fix the mapping of the platform device id when mapping USART
    peripheral ID to UART platform device id. Not setting the platform device id
    will in most cases (when you map USART > 0 to UART 0) make the console not
    available.
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- arch/avr32/mach-at32ap/at32ap700x.c
+++ arch/avr32/mach-at32ap/at32ap700x.c
@@ -1044,6 +1044,7 @@ void __init at32_map_usart(unsigned int hw_id, unsigned int line, int flags)
 		data->regs = (void __iomem *)pdev->resource[0].start;
 	}
 
+	pdev->id = line;
 	pdata = pdev->dev.platform_data;
 	pdata->num = line;
 	at32_usarts[line] = pdev;
