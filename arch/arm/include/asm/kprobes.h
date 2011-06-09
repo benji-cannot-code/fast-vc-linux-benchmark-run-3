93564f68b11a616f0270c89ac2983b43665927be
Jon Medhurst <tixy@yxit.co.uk>
2011-06-09 14:05:51 +0100

    ARM: kprobes: Extend arch_specific_insn to add pointer to emulated instruction
    When we come to emulating Thumb instructions then, to interwork
    correctly, the code on in the instruction slot must be invoked with a
    function pointer which has the least significant bit set. Rather that
    set this by hand in every Thumb emulation function we will add a new
    field for this purpose to arch_specific_insn, called insn_fn.
    
    This also enables us to seamlessly share emulation functions between ARM
    and Thumb code.
    
    Signed-off-by: Jon Medhurst <tixy@yxit.co.uk>
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/include/asm/kprobes.h
+++ arch/arm/include/asm/kprobes.h
@@ -35,6 +35,7 @@ struct kprobe;
 typedef void (kprobe_insn_handler_t)(struct kprobe *, struct pt_regs *);
 typedef unsigned long (kprobe_check_cc)(unsigned long);
 typedef void (kprobe_insn_singlestep_t)(struct kprobe *, struct pt_regs *);
+typedef void (kprobe_insn_fn_t)(void);
 
 /* Architecture specific copy of original instruction. */
 struct arch_specific_insn {
@@ -42,6 +43,7 @@ struct arch_specific_insn {
 	kprobe_insn_handler_t		*insn_handler;
 	kprobe_check_cc			*insn_check_cc;
 	kprobe_insn_singlestep_t	*insn_singlestep;
+	kprobe_insn_fn_t		*insn_fn;
 };
 
 struct prev_kprobe {
