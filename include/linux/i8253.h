1ee281f9bc8191a170661251e6fa3c01b41ec9d9
Thomas Gleixner <tglx@linutronix.de>
2011-06-09 13:08:30 +0000

    i8253: Cleanup outb/inb magic
    Remove the hysterical outb/inb_pit defines and use outb_p/inb_p in the
    code.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: John Stultz <john.stultz@linaro.org>
    Link: http://lkml.kernel.org/r/20110609130622.348437125@linutronix.de

--- include/linux/i8253.h
+++ include/linux/i8253.h
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PIT_LATCH	((PIT_TICK_RATE + HZ/2) / HZ)
 
-#define inb_pit         inb_p
-#define outb_pit        outb_p
-
 extern raw_spinlock_t i8253_lock;
 extern struct clock_event_device i8253_clockevent;
 extern void clockevent_i8253_init(bool oneshot);
