b07fa41e12c1e0907bcb572b439430783b50a92a
Larry Finger <Larry.Finger@lwfinger.net>
2011-06-10 15:11:55 -0500

    rtlwifi: Fix build problems introduced by merging rtl8192de
    These patches allow compilation of rtlwifi, rtl8192c_common,
    rtl8192ce, rtl8192cu and rtl8192se to compile after rtl8192de
    was added.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/rtl8192ce/reg.h
+++ drivers/net/wireless/rtlwifi/rtl8192ce/reg.h
@@ -1204,7 +1204,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EPROM_CMD_CONFIG			0x3
 #define EPROM_CMD_LOAD				1
 
+#define HWSET_MAX_SIZE				128
 #define	HWSET_MAX_SIZE_92S			HWSET_MAX_SIZE
+#define EFUSE_MAX_SECTION			16
 
 #define	WL_HWPDN_EN				BIT(0)
 
