e9cfaa4049475274104414af6fc92a089274379a
Michael Hennerich <michael.hennerich@analog.com>
2011-06-10 15:40:48 +0200

    iio: industrialio-core: Add IIO_OUT type
    Add IIO_OUT type for DAC like devices
    In case of IIO_OUT make sure the channel device attribute is writable
    
    Ideally we add a flag to iio_chan_spec that tells the core that
    the channel device attribute is writable...
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Acked-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/iio.h
+++ drivers/staging/iio/iio.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum iio_chan_type {
 	/* real channel types */
 	IIO_IN,
+	IIO_OUT,
 	IIO_CURRENT,
 	IIO_POWER,
 	IIO_ACCEL,
