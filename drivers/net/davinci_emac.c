19adccb393786f95c9eb71ffe4239e781043d51c
Richard Cochran <richardcochran@gmail.com>
2011-06-12 02:19:01 +0000

    davinci_emac: enable transmit time stamping.
    This patch enables software (and phy device) transmit time stamping
    for the DaVinci EMAC driver. Tested together with the dp83640 PHY.
    
    Cc: Anant Gole <anantgole@ti.com>
    Cc: Kevin Hilman <khilman@deeprootsystems.com>
    Signed-off-by: Richard Cochran <richard.cochran@omicron.at>
    Signed-off-by: David S. Miller <davem@conan.davemloft.net>

--- drivers/net/davinci_emac.c
+++ drivers/net/davinci_emac.c
@@ -1091,6 +1091,7 @@ static int emac_dev_xmit(struct sk_buff *skb, struct net_device *ndev)
 			dev_err(emac_dev, "DaVinci EMAC: desc submit failed");
 		goto fail_tx;
 	}
+	skb_tx_timestamp(skb);
 
 	return NETDEV_TX_OK;
 
