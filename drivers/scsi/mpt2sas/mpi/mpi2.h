5661db04060d569aa6936a921e6647fcac7d65da
Kashyap, Desai <kashyap.desai@lsi.com>
2011-06-14 10:55:45 +0530

    [SCSI] mpt2sas MPI next revision header update
    mpt2sas driver revision q header update:
    
    (1) Modified the descriptions of the LocalAddress bit in the
        Flags field of the MPI SGE Format description and the MPI
        Simple Element.
    (2) Modified Data Location Address Space bits in the Flags field
        of the IEEE Chain Element.
    (3) Added more detail to the description of the DataLength field
        for the SCSI IO Request and Target Assist Request. Removed
        restriction on using chained SGLs when using multicast or
        bidirectional support.
    (4) In Manufacturing Page 7, added ReceptacleID field to
        ConnectorInfo, and reworked how the Pinout field is used.
    (5) In IO Unit Page 7, added BoardTemperature and
        BoardTemperatureUnits fields.
    (6) In IOC Page 1, changed CoalescingTimeout to units of
        half-microsecond and updated descriptions.
    (7) Modified descriptions of SATASlumberTimeout and
        SASSlumberTimeout fields in SAS IO Unit Page 5 to indicate
        the timers start after partial mode is entered.
    (8) Added Extended Manufacturing configuration pages.
    
    Signed-off-by: Kashyap Desai <kashyap.desai@lsi.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/mpt2sas/mpi/mpi2.h
+++ drivers/scsi/mpt2sas/mpi/mpi2.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *                  scatter/gather formats.
  *  Creation Date:  June 21, 2006
  *
- *  mpi2.h Version:  02.00.17
+ *  mpi2.h Version:  02.00.18
  *
  *  Version History
  *  ---------------
@@ -65,6 +65,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  05-12-10  02.00.16  Bumped MPI2_HEADER_VERSION_UNIT.
  *                      Added alternative defines for the SGE Direction bit.
  *  08-11-10  02.00.17  Bumped MPI2_HEADER_VERSION_UNIT.
+ *  11-10-10  02.00.18  Bumped MPI2_HEADER_VERSION_UNIT.
+ *                      Added MPI2_IEEE_SGE_FLAGS_SYSTEMPLBCPI_ADDR define.
  *  --------------------------------------------------------------------------
  */
 
@@ -90,7 +92,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MPI2_VERSION_02_00                  (0x0200)
 
 /* versioning for this MPI header set */
-#define MPI2_HEADER_VERSION_UNIT            (0x11)
+#define MPI2_HEADER_VERSION_UNIT            (0x12)
 #define MPI2_HEADER_VERSION_DEV             (0x00)
 #define MPI2_HEADER_VERSION_UNIT_MASK       (0xFF00)
 #define MPI2_HEADER_VERSION_UNIT_SHIFT      (8)
@@ -1061,10 +1063,14 @@ typedef struct _MPI2_IEEE_SGE_UNION
 
 #define MPI2_IEEE_SGE_FLAGS_ADDR_MASK           (0x03)
 #define MPI2_IEEE_SGE_FLAGS_SYSTEM_ADDR         (0x00)
+						/* IEEE Simple Element only */
 #define MPI2_IEEE_SGE_FLAGS_IOCDDR_ADDR         (0x01)
+						/* IEEE Simple Element only */
 #define MPI2_IEEE_SGE_FLAGS_IOCPLB_ADDR         (0x02)
 #define MPI2_IEEE_SGE_FLAGS_IOCPLBNTA_ADDR      (0x03)
-
+						/* IEEE Simple Element only */
+#define MPI2_IEEE_SGE_FLAGS_SYSTEMPLBCPI_ADDR   (0x03)
+						/* IEEE Chain Element only */
 
 /****************************************************************************
 *  IEEE SGE operation Macros
