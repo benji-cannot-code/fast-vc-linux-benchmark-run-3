c0f27c896fa7d8e724d6f24c1b0d607b0d9ea098
Joerg Roedel <joerg.roedel@amd.com>
2011-06-14 16:44:25 +0200

    iommu/amd: Move missing parts to drivers/iommu
    A few parts of the driver were missing in drivers/iommu.
    Move them there to have the complete driver in that
    directory.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/amd_iommu.c
+++ drivers/iommu/amd_iommu.c
@@ -28,13 +28,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iommu-helper.h>
 #include <linux/iommu.h>
 #include <linux/delay.h>
+#include <linux/amd-iommu.h>
 #include <asm/proto.h>
 #include <asm/iommu.h>
 #include <asm/gart.h>
 #include <asm/dma.h>
-#include <asm/amd_iommu_proto.h>
-#include <asm/amd_iommu_types.h>
-#include <asm/amd_iommu.h>
+
+#include "amd_iommu_proto.h"
+#include "amd_iommu_types.h"
 
 #define CMD_SET_TYPE(cmd, t) ((cmd)->data[1] |= ((t) << 28))
 
