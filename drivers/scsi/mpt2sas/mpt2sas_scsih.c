95e47776bc790b0c7a20338d0ee2df3ff2230993
Kashyap, Desai <kashyap.desai@lsi.com>
2011-06-14 10:57:08 +0530

    [SCSI] mpt2sas: Added DID_NO_CONNECT return when driver remove and avoid shutdown call
    Driver should not call shutdown call from _scsih_remove otherwise,
    The scsi midlayer can be deadlocked when devices are removed from the driver
    pci_driver->shutdown handler.
    
    Signed-off-by: Kashyap Desai <kashyap.desai@lsi.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/mpt2sas/mpt2sas_scsih.c
+++ drivers/scsi/mpt2sas/mpt2sas_scsih.c
@@ -3764,7 +3764,7 @@ _scsih_qcmd_lck(struct scsi_cmnd *scmd, void (*done)(struct scsi_cmnd *))
 		return 0;
 	}
 
-	if (ioc->pci_error_recovery) {
+	if (ioc->pci_error_recovery || ioc->remove_host) {
 		scmd->result = DID_NO_CONNECT << 16;
 		scmd->scsi_done(scmd);
 		return 0;
@@ -7351,7 +7351,6 @@ _scsih_remove(struct pci_dev *pdev)
 	}
 
 	sas_remove_host(shost);
-	_scsih_shutdown(pdev);
 	list_del(&ioc->list);
 	scsi_remove_host(shost);
 	scsi_host_put(shost);
