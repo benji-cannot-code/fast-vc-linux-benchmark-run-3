98f4f7190e9c15107f75d713cff7398a8ad65c20
Rafał Miłecki <zajec5@gmail.com>
2011-06-14 08:18:59 +0200

    b43: add Kconfig option for compiling SSB driver
    It is always "y" for now, b43 will not compile without CONFIG_SSB yet.
    This is just a first step of dropping SSB dependency.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/bus.c
+++ drivers/net/wireless/b43/bus.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /* SSB */
-
+#ifdef CONFIG_B43_SSB
 static inline int b43_bus_ssb_bus_may_powerdown(struct b43_bus_dev *dev)
 {
 	return ssb_bus_may_powerdown(dev->sdev->bus);
@@ -125,3 +125,4 @@ struct b43_bus_dev *b43_bus_dev_ssb_init(struct ssb_device *sdev)
 
 	return dev;
 }
+#endif /* CONFIG_B43_SSB */
