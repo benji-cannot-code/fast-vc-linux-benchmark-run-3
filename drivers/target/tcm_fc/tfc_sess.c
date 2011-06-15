c645edf6cfe4b544c65586a6ff30287ca37669dc
Dan Carpenter <error27@gmail.com>
2011-06-15 09:41:33 -0700

    target: Fix ERR_PTR dereferencing bugs
    transport_init_session() and core_tmr_alloc_req() never return NULL,
    they only return ERR_PTRs on error.
    
    v2: Fix patch to return PTR_ERR(tl_nexus->se_sess) from Ankit Jain's
    feedback.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Ankit Jain <jankit@suse.de>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/target/tcm_fc/tfc_sess.c
+++ drivers/target/tcm_fc/tfc_sess.c
@@ -230,7 +230,7 @@ static struct ft_sess *ft_sess_create(struct ft_tport *tport, u32 port_id,
 		return NULL;
 
 	sess->se_sess = transport_init_session();
-	if (!sess->se_sess) {
+	if (IS_ERR(sess->se_sess)) {
 		kfree(sess);
 		return NULL;
 	}
