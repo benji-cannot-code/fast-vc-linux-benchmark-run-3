4d40f349d5d782e09b78c82f94f5958250fff57b
Kuninori Morimoto <morimoto.kuninori@renesas.com>
2011-06-15 06:16:35 +0000

    ARM: mach-shmobile: mackerel: tidyup usbhs driver settings
    - usb0 pipe is same as default. own pipe config is not needed
    - usb1 lost get_id function
    
    Signed-off-by: Kuninori Morimoto <morimoto.kuninori@renesas.com>
    Acked-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/arm/mach-shmobile/board-mackerel.c
+++ arch/arm/mach-shmobile/board-mackerel.c
@@ -630,19 +630,6 @@ static void usbhs0_hardware_exit(struct platform_device *pdev)
 	cancel_delayed_work_sync(&priv->work);
 }
 
-static u32 usbhs0_pipe_cfg[] = {
-	USB_ENDPOINT_XFER_CONTROL,
-	USB_ENDPOINT_XFER_ISOC,
-	USB_ENDPOINT_XFER_ISOC,
-	USB_ENDPOINT_XFER_BULK,
-	USB_ENDPOINT_XFER_BULK,
-	USB_ENDPOINT_XFER_BULK,
-	USB_ENDPOINT_XFER_INT,
-	USB_ENDPOINT_XFER_INT,
-	USB_ENDPOINT_XFER_INT,
-	USB_ENDPOINT_XFER_BULK,
-};
-
 static struct usbhs_private usbhs0_private = {
 	.usbcrcaddr	= 0xe605810c,		/* USBCR2 */
 	.info = {
@@ -655,8 +642,6 @@ static struct usbhs_private usbhs0_private = {
 		},
 		.driver_param = {
 			.buswait_bwait	= 4,
-			.pipe_type	= usbhs0_pipe_cfg,
-			.pipe_size	= ARRAY_SIZE(usbhs0_pipe_cfg),
 		},
 	},
 };
@@ -787,6 +772,11 @@ static void usbhs1_hardware_exit(struct platform_device *pdev)
 	free_irq(IRQ8, pdev);
 }
 
+static int usbhs1_get_id(struct platform_device *pdev)
+{
+	return USBHS_GADGET;
+}
+
 static u32 usbhs1_pipe_cfg[] = {
 	USB_ENDPOINT_XFER_CONTROL,
 	USB_ENDPOINT_XFER_ISOC,
@@ -813,6 +803,7 @@ static struct usbhs_private usbhs1_private = {
 		.platform_callback = {
 			.hardware_init	= usbhs1_hardware_init,
 			.hardware_exit	= usbhs1_hardware_exit,
+			.get_id		= usbhs1_get_id,
 			.phy_reset	= usbhs_phy_reset,
 			.get_vbus	= usbhs_get_vbus,
 		},
