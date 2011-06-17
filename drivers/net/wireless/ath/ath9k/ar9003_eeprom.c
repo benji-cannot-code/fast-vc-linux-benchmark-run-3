6c263b81335d8e26887081cbd52bf2dd060791e0
Mohammed Shafi Shajakhan <mshajakhan@atheros.com>
2011-06-17 11:07:32 +0530

    ath9k: Fix a corner case failure in sending PAPRD frames in HT40
    when the AP is configured with HT40/2.4GHz and when PAPRD is enabled in station
    we have PAPRD training frames never sent out because of the following
    failure "PAPRD target power delta out of range". This consistently happens
    in channel 10,11 in Over The Air testing and rarely in channel 6 under wired
    setup.
            the reason behind this issue is all the HT40 rates target power
    are 0 when we operate in channel 10/11 at 2.4GHz
    
    Signed-off-by: Mohammed Shafi Shajakhan <mshajakhan@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/ar9003_eeprom.c
+++ drivers/net/wireless/ath/ath9k/ar9003_eeprom.c
@@ -1462,7 +1462,7 @@ static const struct ar9300_eeprom ar9300_h112 = {
 		{ { CTL(60, 0), CTL(60, 1), CTL(60, 0), CTL(60, 0) } },
 		{ { CTL(60, 1), CTL(60, 0), CTL(60, 0), CTL(60, 1) } },
 
-		{ { CTL(60, 1), CTL(60, 0), CTL(0, 0), CTL(0, 0) } },
+		{ { CTL(60, 1), CTL(60, 0), CTL(60, 0), CTL(60, 0) } },
 		{ { CTL(60, 0), CTL(60, 1), CTL(60, 0), CTL(60, 0) } },
 		{ { CTL(60, 0), CTL(60, 1), CTL(60, 0), CTL(60, 0) } },
 
@@ -2617,7 +2617,7 @@ static const struct ar9300_eeprom ar9300_h116 = {
 		 { { CTL(60, 0), CTL(60, 1), CTL(60, 0), CTL(60, 0) } },
 		 { { CTL(60, 1), CTL(60, 0), CTL(60, 0), CTL(60, 1) } },
 
-		 { { CTL(60, 1), CTL(60, 0), CTL(0, 0), CTL(0, 0) } },
+		 { { CTL(60, 1), CTL(60, 0), CTL(60, 0), CTL(60, 0) } },
 		 { { CTL(60, 0), CTL(60, 1), CTL(60, 0), CTL(60, 0) } },
 		 { { CTL(60, 0), CTL(60, 1), CTL(60, 0), CTL(60, 0) } },
 
