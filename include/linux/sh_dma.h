3db1888e7daa735559038725b375b6b26b4c1383
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2011-06-17 08:21:05 +0000

    dmaengine: shdma: add .needs_tend_set / .no_dmars flags
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/linux/sh_dma.h
+++ include/linux/sh_dma.h
@@ -67,6 +67,8 @@ struct sh_dmae_pdata {
 	u32 chcr_ie_bit;
 
 	unsigned int dmaor_is_32bit:1;
+	unsigned int needs_tend_set:1;
+	unsigned int no_dmars:1;
 };
 
 /* DMA register */
@@ -75,6 +77,8 @@ struct sh_dmae_pdata {
 #define TCR	0x08
 #define CHCR	0x0C
 #define DMAOR	0x40
+
+#define TEND	0x18 /* USB-DMAC */
 
 /* DMAOR definitions */
 #define DMAOR_AE	0x00000004
