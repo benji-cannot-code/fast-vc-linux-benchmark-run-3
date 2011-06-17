34fd495ceb9a8b987ae413a48de4c5ff4da72b9b
Mike Frysinger <vapier@gentoo.org>
2011-06-17 04:16:58 -0400

    spi/bfin_spi: uninline fat queue funcs
    There's no need for these queue funcs to be inlined, so drop the
    markings.  This shaves off a few hundred duplicated bytes.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- drivers/spi/spi-bfin5xx.c
+++ drivers/spi/spi-bfin5xx.c
@@ -1187,7 +1187,7 @@ static void bfin_spi_cleanup(struct spi_device *spi)
 	spi_set_ctldata(spi, NULL);
 }
 
-static inline int bfin_spi_init_queue(struct bfin_spi_master_data *drv_data)
+static int bfin_spi_init_queue(struct bfin_spi_master_data *drv_data)
 {
 	INIT_LIST_HEAD(&drv_data->queue);
 	spin_lock_init(&drv_data->lock);
@@ -1209,7 +1209,7 @@ static inline int bfin_spi_init_queue(struct bfin_spi_master_data *drv_data)
 	return 0;
 }
 
-static inline int bfin_spi_start_queue(struct bfin_spi_master_data *drv_data)
+static int bfin_spi_start_queue(struct bfin_spi_master_data *drv_data)
 {
 	unsigned long flags;
 
@@ -1231,7 +1231,7 @@ static inline int bfin_spi_start_queue(struct bfin_spi_master_data *drv_data)
 	return 0;
 }
 
-static inline int bfin_spi_stop_queue(struct bfin_spi_master_data *drv_data)
+static int bfin_spi_stop_queue(struct bfin_spi_master_data *drv_data)
 {
 	unsigned long flags;
 	unsigned limit = 500;
@@ -1260,7 +1260,7 @@ static inline int bfin_spi_stop_queue(struct bfin_spi_master_data *drv_data)
 	return status;
 }
 
-static inline int bfin_spi_destroy_queue(struct bfin_spi_master_data *drv_data)
+static int bfin_spi_destroy_queue(struct bfin_spi_master_data *drv_data)
 {
 	int status;
 
