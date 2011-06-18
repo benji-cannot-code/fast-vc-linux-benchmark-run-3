d3659bfdfe55ae6867ed1dafe626b5f544861774
Dave Carroll <dcarroll@astekcorp.com>
2011-06-18 07:36:40 +0000

    powerpc: Add printk companion for ppc_md.progress
    This patch adds a printk companion to replace the udbg progress function
    when initmem is freed.
    
    Suggested-by: Milton Miller <miltonm@bga.com>
    Suggested-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Dave Carroll <dcarroll@astekcorp.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/setup.h
+++ arch/powerpc/include/asm/setup.h
@@ -4,4 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm-generic/setup.h>
 
+extern void ppc_printk_progress(char *s, unsigned short hex);
+
 #endif	/* _ASM_POWERPC_SETUP_H */
