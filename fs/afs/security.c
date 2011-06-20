2236ebebc8de83afe60c31ada81e4f85f12667cd
Al Viro <viro@zeniv.linux.org.uk>
2011-06-20 19:16:29 -0400

    ->permission() sanitizing: don't pass flags to generic_permission()
    redundant; all callers get it duplicated in mask & MAY_NOT_BLOCK and none of
    them removes that bit.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/afs/security.c
+++ fs/afs/security.c
@@ -351,7 +351,7 @@ int afs_permission(struct inode *inode, int mask, unsigned int flags)
 	}
 
 	key_put(key);
-	ret = generic_permission(inode, mask, flags);
+	ret = generic_permission(inode, mask);
 	_leave(" = %d", ret);
 	return ret;
 
