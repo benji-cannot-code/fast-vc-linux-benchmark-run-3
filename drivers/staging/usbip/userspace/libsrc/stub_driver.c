6a177a95911f36d32c55537991b4d35658f6d2d7
matt mooney <mfm@muteddisk.com>
2011-06-19 22:44:34 -0700

    staging: usbip: userspace: usbip_common.h: fixup header includes
    Remove unnecessary headers from the file, and add the now missing
    headers into the files that actually need them.
    
    Signed-off-by: matt mooney <mfm@muteddisk.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/usbip/userspace/libsrc/stub_driver.c
+++ drivers/staging/usbip/userspace/libsrc/stub_driver.c
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <sys/types.h>
 #include <sys/stat.h>
+
+#include <errno.h>
 #include <unistd.h>
 
 #include "usbip.h"
--- drivers/staging/usbip/userspace/libsrc/usbip_common.h
+++ drivers/staging/usbip/userspace/libsrc/usbip_common.h
@@ -3,20 +3,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2005-2007 Takahiro Hirofuchi
  */
 
-#ifndef _USBIP_COMMON_H
-#define _USBIP_COMMON_H
-
-#include <unistd.h>
-#include <stdint.h>
-#include <syslog.h>
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#ifndef __USBIP_COMMON_H
+#define __USBIP_COMMON_H
 
 #include <sysfs/libsysfs.h>
-#include <netdb.h>
-#include <sys/socket.h>
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <syslog.h>
+#include <unistd.h>
 
 #ifndef USBIDS_FILE
 #define USBIDS_FILE "/usr/share/hwdata/usb.ids"
@@ -147,4 +145,4 @@ void usbip_names_free(void);
 void usbip_names_get_product(char *buff, size_t size, uint16_t vendor, uint16_t product);
 void usbip_names_get_class(char *buff, size_t size, uint8_t class, uint8_t subclass, uint8_t protocol);
 
-#endif
+#endif /* __USBIP_COMMON_H */
