f3170ca6cb8391c53ff673227d22b94ccda5bf1d
Jesper Juhl <jj@chaosbits.net>
2011-06-20 12:13:10 +0000

    Remove redundant linux/version.h includes from net/
    It was suggested by "make versioncheck" that the follwing includes of
    linux/version.h are redundant:
    
      /home/jj/src/linux-2.6/net/caif/caif_dev.c: 14 linux/version.h not needed.
      /home/jj/src/linux-2.6/net/caif/chnl_net.c: 10 linux/version.h not needed.
      /home/jj/src/linux-2.6/net/ipv4/gre.c: 19 linux/version.h not needed.
      /home/jj/src/linux-2.6/net/netfilter/ipset/ip_set_core.c: 20 linux/version.h not needed.
      /home/jj/src/linux-2.6/net/netfilter/xt_set.c: 16 linux/version.h not needed.
    
    and it seems that it is right.
    
    Beyond manually inspecting the source files I also did a few build
    tests with various configs to confirm that including the header in
    those files is indeed not needed.
    
    Here's a patch to remove the pointless includes.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Acked-by: Jozsef Kadlecsik <kadlec@blackhole.kfki.hu>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/caif/chnl_net.c
+++ net/caif/chnl_net.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ":%s(): " fmt, __func__
 
-#include <linux/version.h>
 #include <linux/fs.h>
 #include <linux/hardirq.h>
 #include <linux/init.h>
--- net/netfilter/ipset/ip_set_core.c
+++ net/netfilter/ipset/ip_set_core.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/netlink.h>
 #include <linux/rculist.h>
-#include <linux/version.h>
 #include <net/netlink.h>
 
 #include <linux/netfilter.h>
