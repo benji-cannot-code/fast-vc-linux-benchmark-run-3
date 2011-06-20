00fcae5bef5174e1d43b722a93f1b04eac3919df
Michael Fiedler <michael.fiedler@informatik.stud.uni-erlangen.de>
2011-06-20 16:34:28 +0200

    staging: rtl8712: Use linux/atomic.h instead of asm/atomic.h
    Fixes warning from scripts/checkpatch.pl.
    
    Signed-off-by: Michael Fiedler <michael.fiedler@informatik.stud.uni-erlangen.de>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8712/osdep_service.h
+++ drivers/staging/rtl8712/osdep_service.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/circ_buf.h>
 #include <linux/uaccess.h>
 #include <asm/byteorder.h>
-#include <asm/atomic.h>
+#include <linux/atomic.h>
 #include <linux/wireless.h>
 #include <linux/rtnetlink.h>
 #include "ethernet.h"
