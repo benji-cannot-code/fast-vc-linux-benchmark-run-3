41d9b0faed0050c810171225ef0d79ed2e0c8f75
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-06-21 09:35:36 +0300

    OMAP: DSS2: Fix FIFO threshold and burst size for OMAP4
    The DMA FIFO threshold registers and burst size registers have changed
    for OMAP4.  The current code only handles OMAP2/3 case, and so the
    values are a bit off for OMAP4.  A summary of the differences between
    OMAP2/3 and OMAP4:
    
    Burst size:
    OMAP2/3: 4 x 32 bits / 8 x 32 bits / 16 x 32 bits
    OMAP4: 2 x 128 bits / 4 x 128 bits / 8 x 128 bits
    
    Threshold size:
    OMAP2/3: in bytes (8 bit units)
    OMAP4: in 128bit units
    
    This patch fixes the issue by creating two new helper functions in
    dss_features: dss_feat_get_buffer_size_unit() and
    dss_feat_get_burst_size_unit(). These return (in bytes) the unit size
    for threshold registers and unit size for burst size register,
    respectively, and are used to calculate correct values.
    
    For the threshold size the usage is straightforward. However, the burst
    size register has different multipliers for OMAP2/3 and OMAP4. This
    patch solves the problem by defining the multipliers for the burst size
    as 2x, 4x and 8x, which fit fine for the OMAP4 burst size definition
    (i.e. burst size unit for OMAP4 is 128bits), but requires a slight twist
    on OMAP2/3 by defining the burst size unit as 64bit.
    
    As the driver in practice always uses the maximum burst size, and no use
    case currently exists where we would want to use a smaller burst size,
    this patch changes the driver to hardcode the burst size when
    initializing DISPC. This makes the threshold configuration code somewhat
    simpler.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/manager.c
+++ drivers/video/omap2/dss/manager.c
@@ -508,7 +508,6 @@ struct overlay_cache_data {
 	bool replication;
 	bool ilace;
 
-	enum omap_burst_size burst_size;
 	u32 fifo_low;
 	u32 fifo_high;
 };
@@ -948,8 +947,7 @@ static int configure_overlay(enum omap_plane plane)
 
 	dispc_enable_replication(plane, c->replication);
 
-	dispc_set_burst_size(plane, c->burst_size);
-	dispc_setup_plane_fifo(plane, c->fifo_low, c->fifo_high);
+	dispc_set_fifo_threshold(plane, c->fifo_low, c->fifo_high);
 
 	dispc_enable_plane(plane, 1);
 
@@ -1418,7 +1416,7 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 	/* Configure overlay fifos */
 	for (i = 0; i < omap_dss_get_num_overlays(); ++i) {
 		struct omap_dss_device *dssdev;
-		u32 size;
+		u32 size, burst_size;
 
 		ovl = omap_dss_get_overlay(i);
 
@@ -1436,6 +1434,8 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 		if (use_fifomerge)
 			size *= 3;
 
+		burst_size = dispc_get_burst_size(ovl->id);
+
 		switch (dssdev->type) {
 		case OMAP_DISPLAY_TYPE_DPI:
 		case OMAP_DISPLAY_TYPE_DBI:
@@ -1443,13 +1443,13 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 		case OMAP_DISPLAY_TYPE_VENC:
 		case OMAP_DISPLAY_TYPE_HDMI:
 			default_get_overlay_fifo_thresholds(ovl->id, size,
-					&oc->burst_size, &oc->fifo_low,
+					burst_size, &oc->fifo_low,
 					&oc->fifo_high);
 			break;
 #ifdef CONFIG_OMAP2_DSS_DSI
 		case OMAP_DISPLAY_TYPE_DSI:
 			dsi_get_overlay_fifo_thresholds(ovl->id, size,
-					&oc->burst_size, &oc->fifo_low,
+					burst_size, &oc->fifo_low,
 					&oc->fifo_high);
 			break;
 #endif
