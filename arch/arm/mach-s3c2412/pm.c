7e6ef23236cfa88b11fbfdf582776875c2d03e6f
Russell King <rmk+kernel@arm.linux.org.uk>
2011-06-21 19:29:26 +0100

    ARM: pm: samsung: move cpu_suspend into C code
    Move the call to cpu_suspend into C code, and noticing that all the
    s3c_cpu_save implementations are now identical, we can move this
    into the common samsung code.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s3c2412/pm.c
+++ arch/arm/mach-s3c2412/pm.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern void s3c2412_sleep_enter(void);
 
-static void s3c2412_cpu_suspend(void)
+static void s3c2412_cpu_suspend(unsigned long arg)
 {
 	unsigned long tmp;
 
