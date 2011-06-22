6a820f72df044c9f6da516debcd28362df50ecd8
Mark Rutland <mark.rutland@arm.com>
2011-06-22 15:30:51 +0100

    ARM: 6974/1: pmu: refactor reservation
    Currently, PMU platform_device reservation relies on some minor abuse
    of the platform_device::id field for determining the type of PMU. This
    is problematic for device tree based probing, where the ID cannot be
    controlled.
    
    This patch removes reliance on the id field, and depends on each PMU's
    platform driver to figure out which type it is. As all PMUs handled by
    the current platform_driver name "arm-pmu" are CPU PMUs, this
    convention is hardcoded. New PMU types can be supported through the use
    of {of,platform}_device_id tables
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Jamie Iles <jamie@jamieiles.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Cc: Rob Herring <rob.herring@calxeda.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -436,7 +436,7 @@ armpmu_reserve_hardware(void)
 			if (irq >= 0)
 				free_irq(irq, NULL);
 		}
-		release_pmu(pmu_device);
+		release_pmu(ARM_PMU_DEVICE_CPU);
 		pmu_device = NULL;
 	}
 
@@ -455,7 +455,7 @@ armpmu_release_hardware(void)
 	}
 	armpmu->stop();
 
-	release_pmu(pmu_device);
+	release_pmu(ARM_PMU_DEVICE_CPU);
 	pmu_device = NULL;
 }
 
