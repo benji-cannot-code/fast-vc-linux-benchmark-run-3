79d30970529fe1753ea11d2fce981b6a811ddd7e
Jesper Juhl <jj@chaosbits.net>
2011-06-24 01:04:19 +0200

    Remove unneeded version.h includes (and add where needed) for drivers/tm6000/
    It was pointed out by 'make versioncheck' that linux/version.h was not
    always being included where needed and sometimes included needlessly
    in drivers/staging/tm6000/.
    This patch fixes up the includes.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tm6000/tm6000-cards.c
+++ drivers/staging/tm6000/tm6000-cards.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/usb.h>
-#include <linux/version.h>
 #include <linux/slab.h>
 #include <media/v4l2-common.h>
 #include <media/tuner.h>
--- drivers/staging/tm6000/tm6000-video.c
+++ drivers/staging/tm6000/tm6000-video.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/sched.h>
 #include <linux/random.h>
-#include <linux/version.h>
 #include <linux/usb.h>
 #include <linux/videodev2.h>
 #include <media/v4l2-ioctl.h>
