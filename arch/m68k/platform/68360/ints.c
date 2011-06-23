148232dc60b3d41142a17fcc4bc4d78b54f10342
Greg Ungerer <gerg@uclinux.org>
2011-06-23 15:52:25 +1000

    m68knommu: correctly use trap_init
    Currently trap_init() is an empty function for m68knommu. Instead
    the vectors are being setup as part of the IRQ initialization.
    This is inconsistent with m68k and other architectures.
    
    Change the local init_vectors() to be trap_init(), and init the
    vectors at the correct time during startup. This will help merge of
    m68k and m68knommu trap code in the furture.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/platform/68360/ints.c
+++ arch/m68k/platform/68360/ints.c
@@ -64,9 +64,8 @@ static struct irq_chip intc_irq_chip = {
  * This function should be called during kernel startup to initialize
  * the vector table.
  */
-void init_IRQ(void)
+void __init trap_init(void)
 {
-	int i;
 	int vba = (CPM_VECTOR_BASE<<4);
 
 	/* set up the vectors */
@@ -131,6 +130,11 @@ void init_IRQ(void)
 
 	/* turn off all CPM interrupts */
 	pquicc->intr_cimr = 0x00000000;
+}
+
+void init_IRQ(void)
+{
+	int i;
 
 	for (i = 0; (i < NR_IRQS); i++) {
 		irq_set_chip(i, &intc_irq_chip);
