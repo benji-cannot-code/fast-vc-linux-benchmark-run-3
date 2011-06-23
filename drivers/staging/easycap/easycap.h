cfca5d61147aa2fb0b4726276140577a75deaf5f
Jesper Juhl <jj@chaosbits.net>
2011-06-24 00:51:12 +0200

    Remove unneeded version.h includes (and add where needed) for drivers/staging/easycap/
    It was pointed out by 'make versioncheck' that linux/version.h was not
    always being included where needed and sometimes included needlessly
    in drivers/staging/easycap/.
    This patch fixes up the includes.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/easycap/easycap.h
+++ drivers/staging/easycap/easycap.h
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 
 #include <linux/i2c.h>
-#include <linux/version.h>
 #include <linux/workqueue.h>
 #include <linux/poll.h>
 #include <linux/mm.h>
