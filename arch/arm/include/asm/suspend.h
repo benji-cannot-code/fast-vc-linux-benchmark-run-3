8bd90baa2a5ec723dc957391b5157e002c370755
Russell King <rmk+kernel@arm.linux.org.uk>
2011-06-23 14:24:09 +0100

    ARM: pm: ensure our temporary page table entry is removed from the TLB
    Ensure that our temporary page table entry is flushed from the TLB
    before we resume normal operations.  This ensures that userspace
    won't trip over the stale TLB entry.
    
    Tested-by: Kevin Hilman <khilman@ti.com>
    Acked-by: Jean Pihet <j-pihet@ti.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/suspend.h
+++ arch/arm/include/asm/suspend.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARM_SUSPEND_H
 
 #include <asm/memory.h>
+#include <asm/tlbflush.h>
 
 extern void cpu_resume(void);
 
@@ -15,6 +16,7 @@ static inline void cpu_suspend(unsigned long arg, void (*fn)(unsigned long))
 	extern void __cpu_suspend(int, long, unsigned long,
 				  void (*)(unsigned long));
 	__cpu_suspend(0, PHYS_OFFSET - PAGE_OFFSET, arg, fn);
+	flush_tlb_all();
 }
 
 #endif
