67f4debddf24550bd1ee73534ffec91129660563
Stephen Rothwell <sfr@canb.auug.org.au>
2011-06-23 12:58:55 +1000

    Bluetooth: include scatterlist.h where needed
    net/bluetooth/smp.c: In function 'smp_e':
    net/bluetooth/smp.c:49:21: error: storage size of 'sg' isn't known
    net/bluetooth/smp.c:67:2: error: implicit declaration of function 'sg_init_one'
    net/bluetooth/smp.c:49:21: warning: unused variable 'sg'
    
    Caused by commit d22ef0bc83c5 ("Bluetooth: Add LE SMP Cryptoolbox
    functions").  Missing include file, presumably.  This batch has been in
    the bluetooth tree since June 14, so it may have been exposed by the
    removal of linux/mm.h from netdevice.h ...
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/bluetooth/smp.c
+++ net/bluetooth/smp.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/bluetooth/l2cap.h>
 #include <net/bluetooth/smp.h>
 #include <linux/crypto.h>
+#include <linux/scatterlist.h>
 #include <crypto/b128ops.h>
 
 #define SMP_TIMEOUT 30000 /* 30 seconds */
