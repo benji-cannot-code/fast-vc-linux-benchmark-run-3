ce272caf3a943b23c35c78ed0c0d25b43b450ac6
Jesper Juhl <jj@chaosbits.net>
2011-06-24 01:02:55 +0200

    Remove unneeded version.h include from drivers/staging/tidspbridge/include/dspbridge/host_os.h
    It was pointed out by 'make versioncheck' that include of
    linux/version.h is not needed in
    drivers/staging/tidspbridge/include/dspbridge/host_os.h
    This patch removes it.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/tidspbridge/include/dspbridge/host_os.h
+++ drivers/staging/tidspbridge/include/dspbridge/host_os.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/io.h>
 #include <linux/syscalls.h>
-#include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
 #include <linux/stddef.h>
