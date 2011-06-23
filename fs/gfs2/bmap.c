16011c2ae78098e99cc0488b2ce5defd8c52fe55
Eric Sandeen <sandeen@redhat.com>
2011-06-23 10:39:34 -0500

    GFS2: combine duplicated block freeing routines
    __gfs2_free_data and __gfs2_free_meta are almost identical, and
    can be trivially combined.
    
    [This is as per Eric's original patch minus gfs2_free_data() which had
     no callers left and plus the conversion of the bmap.c calls to these
     functions. All in all, a nice clean up]
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/bmap.c
+++ fs/gfs2/bmap.c
@@ -855,11 +855,7 @@ static int do_strip(struct gfs2_inode *ip, struct buffer_head *dibh,
 			blen++;
 		else {
 			if (bstart) {
-				if (metadata)
-					__gfs2_free_meta(ip, bstart, blen);
-				else
-					__gfs2_free_data(ip, bstart, blen);
-
+				__gfs2_free_blocks(ip, bstart, blen, metadata);
 				btotal += blen;
 			}
 
@@ -871,11 +867,7 @@ static int do_strip(struct gfs2_inode *ip, struct buffer_head *dibh,
 		gfs2_add_inode_blocks(&ip->i_inode, -1);
 	}
 	if (bstart) {
-		if (metadata)
-			__gfs2_free_meta(ip, bstart, blen);
-		else
-			__gfs2_free_data(ip, bstart, blen);
-
+		__gfs2_free_blocks(ip, bstart, blen, metadata);
 		btotal += blen;
 	}
 
