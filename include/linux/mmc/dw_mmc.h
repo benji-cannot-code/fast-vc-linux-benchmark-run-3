48439cad05d8e93d1260cd1fa3a60ed834ba6417
James Hogan <james.hogan@imgtec.com>
2011-06-24 13:55:55 +0100

    mmc: dw_mmc: convert card tasklet to workqueue
    Convert the card insert/remove tasklet to a workqueue, and call the
    setpower platform specific callback without the spinlock held. This
    means neither of the setpower or get_cd callbacks are called from atomic
    context which allows them to sleep.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Acked-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/dw_mmc.h
+++ include/linux/mmc/dw_mmc.h
@@ -132,7 +132,7 @@ struct dw_mci {
 	u32			stop_cmdr;
 	u32			dir_status;
 	struct tasklet_struct	tasklet;
-	struct tasklet_struct	card_tasklet;
+	struct work_struct	card_work;
 	unsigned long		pending_events;
 	unsigned long		completed_events;
 	enum dw_mci_state	state;
