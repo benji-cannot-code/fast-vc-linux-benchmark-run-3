1255ea1206c652c19051507e9c0a00a9b877a4cb
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-06-26 10:18:03 -0300

    [media] return -ENOTTY for unsupported ioctl's at legacy drivers
    Those drivers are not relying at the V4L2 core to handle the ioctl's.
    So, we need to manually patch them every time a change goes to the
    core.
    
    Acked-by: Hans Verkuil <hans.verkuil@cisco.com>
    Acked-By: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/pvrusb2/pvrusb2-v4l2.c
+++ drivers/media/video/pvrusb2/pvrusb2-v4l2.c
@@ -370,11 +370,6 @@ static long pvr2_v4l2_do_ioctl(struct file *file, unsigned int cmd, void *arg)
 		break;
 	}
 
-	case VIDIOC_S_AUDIO:
-	{
-		ret = -EINVAL;
-		break;
-	}
 	case VIDIOC_G_TUNER:
 	{
 		struct v4l2_tuner *vt = (struct v4l2_tuner *)arg;
@@ -851,7 +846,7 @@ static long pvr2_v4l2_do_ioctl(struct file *file, unsigned int cmd, void *arg)
 #endif
 
 	default :
-		ret = -EINVAL;
+		ret = -ENOTTY;
 		break;
 	}
 
