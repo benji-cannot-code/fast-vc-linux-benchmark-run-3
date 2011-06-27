6d83ca2aab185524e8fbf2df5f1a23feec45b74f
Jon Mason <jdmason@kudzu.us>
2011-06-27 12:50:14 -0500

    rtlwifi: remove unnecessary read of PCI_CAP_ID_EXP
    The PCIE capability offset is saved during PCI bus walking.  It will
    remove an unnecessary search in the PCI configuration space if this
    value is referenced instead of reacquiring it.
    
    Also, remove unnecessary and unused #defines for PCI.
    
    Signed-off-by: Jon Mason <jdmason@kudzu.us>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/pci.h
+++ drivers/net/wireless/rtlwifi/pci.h
@@ -76,11 +76,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCI_CONF_ADDRESS	0x0CF8	/*PCI Configuration Space Address */
 #define PCI_CONF_DATA		0x0CFC	/*PCI Configuration Space Data */
 
-#define PCI_CLASS_BRIDGE_DEV		0x06
-#define PCI_SUBCLASS_BR_PCI_TO_PCI	0x04
-#define PCI_CAPABILITY_ID_PCI_EXPRESS	0x10
-#define PCI_CAP_ID_EXP			0x10
-
 #define U1DONTCARE			0xFF
 #define U2DONTCARE			0xFFFF
 #define U4DONTCARE			0xFFFFFFFF
