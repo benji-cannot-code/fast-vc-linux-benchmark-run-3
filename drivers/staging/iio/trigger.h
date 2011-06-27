bd5487795c76876600b37c3c91824c0e6d1b4276
Michael Hennerich <michael.hennerich@analog.com>
2011-06-27 13:07:09 +0100

    iio: trigger: Add filter callback
    Allow devices to reject triggers and vice versa.
    
    Changes since V1:
    Added kernel-doc
    Moved callback into iio_info
    Changed function naming
    Revised return value passing
    Add forward declaration to avoid warnings
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Acked-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/trigger.h
+++ drivers/staging/iio/trigger.h
@@ -30,6 +30,8 @@ struct iio_subirq {
  * @set_trigger_state:	[DRIVER] switch on/off the trigger on demand
  * @try_reenable:	function to reenable the trigger when the
  *			use count is zero (may be NULL)
+ * @validate_device:	function to validate the device when the
+ *			current trigger gets changed.
  * @subirq_chip:	[INTERN] associate 'virtual' irq chip.
  * @subirq_base:	[INTERN] base number for irqs provided by trigger.
  * @subirqs:		[INTERN] information about the 'child' irqs.
@@ -49,6 +51,8 @@ struct iio_trigger {
 
 	int (*set_trigger_state)(struct iio_trigger *trig, bool state);
 	int (*try_reenable)(struct iio_trigger *trig);
+	int (*validate_device)(struct iio_trigger *trig,
+			       struct iio_dev *indio_dev);
 
 	struct irq_chip			subirq_chip;
 	int				subirq_base;
