ee4530e8da8fc18194f8443f5327682af90cb75e
Jonathan Cameron <jic23@cam.ac.uk>
2011-06-27 13:07:34 +0100

    staging:iio:dac:ad5791: allocate chip state with iio_dev and use iio_priv for access.
    Slightly fiddly case with two regulators - could reorder, but this is the approach with
    smallest likely impact.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Acked-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/dac/ad5791.h
+++ drivers/staging/iio/dac/ad5791.h
@@ -82,7 +82,6 @@ struct ad5791_chip_info {
 
 /**
  * struct ad5791_state - driver instance specific data
- * @indio_dev:		the industrial I/O device
  * @us:			spi_device
  * @reg_vdd:		positive supply regulator
  * @reg_vss:		negative supply regulator
@@ -92,7 +91,6 @@ struct ad5791_chip_info {
  */
 
 struct ad5791_state {
-	struct iio_dev			*indio_dev;
 	struct spi_device		*spi;
 	struct regulator		*reg_vdd;
 	struct regulator		*reg_vss;
