11780c80622e8152201c346114561ee60a66f071
Rafał Miłecki <zajec5@gmail.com>
2011-06-28 00:08:52 +0200

    b43: HT-PHY: replace radio routing magic numbers
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/radio_2059.h
+++ drivers/net/wireless/b43/radio_2059.h
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "phy_ht.h"
 
+#define R2059_SYN			0x000
+#define R2059_TXRX0			0x400
+#define R2059_RXRX1			0x800
+#define R2059_ALL			0xC00
+
 /* Values for various registers uploaded on channel switching */
 struct b43_phy_ht_channeltab_e_radio2059 {
 	/* The channel frequency in MHz */
