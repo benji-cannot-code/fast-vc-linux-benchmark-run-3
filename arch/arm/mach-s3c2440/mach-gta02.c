ce56ec7fd6a4b405e2abbb64c03dee69a62fe7b1
Peter Korsgaard <jacmet@sunsite.dk>
2011-06-28 14:49:15 +0200

    mach-gta2: remove unused spi-gpio.h include
    Not needed, and the file is going away.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>
    Acked-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/arm/mach-s3c2440/mach-gta02.c
+++ arch/arm/mach-s3c2440/mach-gta02.c
@@ -75,7 +75,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/fb.h>
 
 #include <mach/spi.h>
-#include <mach/spi-gpio.h>
 #include <plat/usb-control.h>
 #include <mach/regs-mem.h>
 #include <mach/hardware.h>
