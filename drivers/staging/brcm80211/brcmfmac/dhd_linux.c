a06c23d6346c808cc5c1de77d345cb0e8cb269ea
Arend van Spriel <arend@broadcom.com>
2011-06-29 16:47:40 -0700

    staging: brcm80211: use linux native ethertype iso ETH_P_BRCM
    The linux native ethertype ETH_P_LINK_CTL is matching ETH_P_BRCM so
    lets use that definition and get rid of ETH_P_BRCM.
    
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/dhd_linux.c
+++ drivers/staging/brcm80211/brcmfmac/dhd_linux.c
@@ -44,8 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dhd_dbg.h"
 #include "wl_cfg80211.h"
 
-#define ETH_P_BRCM			0x886c
-
 /* Global ASSERT type flag */
 u32 g_assert_type;
 
@@ -948,7 +946,7 @@ void brcmf_rx_frame(dhd_pub_t *dhdp, int ifidx, struct sk_buff *skb,
 		skb_pull(skb, ETH_HLEN);
 
 		/* Process special event packets and then discard them */
-		if (ntohs(skb->protocol) == ETH_P_BRCM)
+		if (ntohs(skb->protocol) == ETH_P_LINK_CTL)
 			brcmf_host_event(dhd, &ifidx,
 					  skb_mac_header(skb),
 					  &event, &data);
