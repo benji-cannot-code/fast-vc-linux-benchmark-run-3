6f0407d7313bd7fa0dc15ce6de3e17444a7c5947
Roland Vossen <rvossen@broadcom.com>
2011-06-29 16:46:45 -0700

    staging: brcm80211: parsed ADDBA response ack window parameter
    IEEE80211_AMPDU_TX_OPERATIONAL provides a BA window size parameter. Code
    is now using this parameter to restrict the amount of outstanding tx
    AMPDUs.
    
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/scb.h
+++ drivers/staging/brcm80211/brcmsmac/scb.h
@@ -26,11 +26,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AMPDU_TX_BA_MAX_WSIZE	64	/* max Tx ba window size (in pdu) */
 /* structure to store per-tid state for the ampdu initiator */
 struct scb_ampdu_tid_ini {
-	u32 magic;
 	u8 tx_in_transit;	/* number of pending mpdus in transit in driver */
 	u8 tid;		/* initiator tid for easy lookup */
 	u8 txretry[AMPDU_TX_BA_MAX_WSIZE];	/* tx retry count; indexed by seq modulo */
 	struct scb *scb;	/* backptr for easy lookup */
+	u8 ba_wsize;		/* negotiated ba window size (in pdu) */
 };
 
 #define AMPDU_MAX_SCB_TID	NUMPRIO
@@ -52,7 +52,6 @@ struct scb_ampdu {
 };
 
 #define SCB_MAGIC 	0xbeefcafe
-#define INI_MAGIC 	0xabcd1234
 
 /* station control block - one per remote MAC address */
 struct scb {
