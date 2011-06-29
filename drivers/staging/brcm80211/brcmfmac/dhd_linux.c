13db6fd1fd660423f3cd49e8c8b22caebbc3470c
Arend van Spriel <arend@broadcom.com>
2011-06-29 16:47:31 -0700

    staging: brcm80211: remove EMBEDDED_PLATFORM macro definition
    The macro is always intended to be defined so no need for it.
    
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/dhd_linux.c
+++ drivers/staging/brcm80211/brcmfmac/dhd_linux.c
@@ -1931,10 +1931,8 @@ int brcmf_bus_start(dhd_pub_t *dhdp)
 {
 	int ret = -1;
 	dhd_info_t *dhd = (dhd_info_t *) dhdp->info;
-#ifdef EMBEDDED_PLATFORM
 	char iovbuf[WL_EVENTING_MASK_LEN + 12];	/*  Room for "event_msgs" +
 						 '\0' + bitvec  */
-#endif				/* EMBEDDED_PLATFORM */
 
 	ASSERT(dhd);
 
@@ -1982,7 +1980,7 @@ int brcmf_bus_start(dhd_pub_t *dhdp)
 		DHD_ERROR(("%s failed bus is not ready\n", __func__));
 		return -ENODEV;
 	}
-#ifdef EMBEDDED_PLATFORM
+
 	brcmu_mkiovar("event_msgs", dhdp->eventmask, WL_EVENTING_MASK_LEN,
 		      iovbuf, sizeof(iovbuf));
 	brcmf_proto_cdc_query_ioctl(dhdp, 0, BRCMF_C_GET_VAR, iovbuf,
@@ -2016,7 +2014,6 @@ int brcmf_bus_start(dhd_pub_t *dhdp)
 	dhdp->pktfilter_count = 1;
 	/* Setup filter to allow only unicast */
 	dhdp->pktfilter[0] = "100 0 0 0 0x01 0x00";
-#endif				/* EMBEDDED_PLATFORM */
 
 	/* Bus is ready, do any protocol initialization */
 	ret = brcmf_proto_init(&dhd->pub);
