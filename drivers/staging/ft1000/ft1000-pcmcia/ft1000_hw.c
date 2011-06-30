fc551caf540f0739bd6f04cec918b42c6c3cbbe0
Ondrej Zary <linux@rainbow-software.org>
2011-07-01 00:03:36 +0200

    staging: ft1000-pcmcia: remove MEDIAMSG typedefs
    Remove MEDIAMSG typedefs.
    
    Signed-off-by: Ondrej Zary <linux@rainbow-software.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/ft1000/ft1000-pcmcia/ft1000_hw.c
+++ drivers/staging/ft1000/ft1000-pcmcia/ft1000_hw.c
@@ -1016,7 +1016,7 @@ void ft1000_proc_drvmsg(struct net_device *dev)
 	FT1000_INFO *info = netdev_priv(dev);
 	u16 msgtype;
 	u16 tempword;
-	PMEDIAMSG pmediamsg;
+	struct media_msg *pmediamsg;
 	PDSPINITMSG pdspinitmsg;
 	struct drv_msg *pdrvmsg;
 	u16 len;
@@ -1091,7 +1091,7 @@ void ft1000_proc_drvmsg(struct net_device *dev)
 			info->CardReady = 1;
 			break;
 		case MEDIA_STATE:
-			pmediamsg = (PMEDIAMSG) & cmdbuffer[0];
+			pmediamsg = (struct media_msg *) & cmdbuffer[0];
 			if (info->ProgConStat != 0xFF) {
 			if (pmediamsg->state) {
 				DEBUG(1, "Media is up\n");
