3b928ba212274dc4cb28868e782169594bb4ff07
Dan Carpenter <error27@gmail.com>
2011-06-30 12:02:05 +0300

    Staging: comedi: usbdux: release locks on error paths
    Smatch complains about a couple error paths where the semaphores
    were not released.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/comedi/drivers/usbdux.c
+++ drivers/staging/comedi/drivers/usbdux.c
@@ -1466,6 +1466,7 @@ static int usbdux_ao_inttrig(struct comedi_device *dev,
 		dev_err(&this_usbduxsub->interface->dev,
 			"comedi%d: usbdux_ao_inttrig: invalid trignum\n",
 			dev->minor);
+		up(&this_usbduxsub->sem);
 		return -EINVAL;
 	}
 	if (!(this_usbduxsub->ao_cmd_running)) {
@@ -2672,6 +2673,7 @@ static int usbdux_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	if (ret < 0) {
 		dev_err(&udev->interface->dev,
 			"comedi%d: error alloc space for subdev\n", dev->minor);
+		up(&udev->sem);
 		up(&start_stop_sem);
 		return ret;
 	}
