6855a812bcfced405d195671f792a28f82a7aaa6
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2011-07-01 13:00:21 +1000

    powerpc: Fix build problem with default ppc_md.progress commit
    a9c0f41b3a64955fd6f4e9d66ae1df1cbdee0cd0 breaks the build
    on some platforms. The extern declaration must be shielded
    against assembly.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/setup.h
+++ arch/powerpc/include/asm/setup.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm-generic/setup.h>
 
+#ifndef __ASSEMBLY__
 extern void ppc_printk_progress(char *s, unsigned short hex);
+#endif
 
 #endif	/* _ASM_POWERPC_SETUP_H */
