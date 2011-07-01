6051f80beb39a97f5995d520755c5d6ef4739a25
Greg Dietsche <Gregory.Dietsche@cuw.edu>
2011-07-01 16:16:19 -0700

    net: rds: fix const array syntax
    Correct the syntax so that both array and pointer are const.
    
    Signed-off-by: Greg Dietsche <Gregory.Dietsche@cuw.edu>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/rds/tcp_stats.c
+++ net/rds/tcp_stats.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 DEFINE_PER_CPU(struct rds_tcp_statistics, rds_tcp_stats)
 	____cacheline_aligned;
 
-static const char const *rds_tcp_stat_names[] = {
+static const char * const rds_tcp_stat_names[] = {
 	"tcp_data_ready_calls",
 	"tcp_write_space_calls",
 	"tcp_sndbuf_full",
