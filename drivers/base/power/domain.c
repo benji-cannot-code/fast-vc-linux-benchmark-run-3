8bb4f4d84b1c791669ffbb808fcbea7431bc117e
Rafael J. Wysocki <rjw@sisk.pl>
2011-07-01 22:13:29 +0200

    PM / Domains: Wakeup devices support for system sleep transitions
    There is the problem how to handle devices set up to wake up the
    system from sleep states during system-wide power transitions.
    In some cases, those devices can be turned off entirely, because the
    wakeup signals will be generated on their behalf anyway.  In some
    other cases, they will generate wakeup signals if their clocks are
    stopped, but only if power is not removed from them.  Finally, in
    some cases, they can only generate wakeup signals if power is not
    removed from them and their clocks are enabled.
    
    To allow platform-specific code to decide whether or not to put
    wakeup devices (and their PM domains) into low-power state during
    system-wide transitions, such as system suspend, introduce a new
    generic PM domain callback, .active_wakeup(), that will be used
    during the "noirq" phase of system suspend and hibernation (after
    image creation) to decide what to do with wakeup devices.
    Specifically, if this callback is present and returns "true", the
    generic PM domain code will not execute .stop_device() for the
    given wakeup device and its PM domain won't be powered off.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Kevin Hilman <khilman@ti.com>

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -451,6 +451,10 @@ static int pm_genpd_suspend_noirq(struct device *dev)
 	if (ret)
 		return ret;
 
+	if (device_may_wakeup(dev)
+	    && genpd->active_wakeup && genpd->active_wakeup(dev))
+		return 0;
+
 	if (genpd->stop_device)
 		genpd->stop_device(dev);
 
@@ -670,6 +674,10 @@ static int pm_genpd_dev_poweroff_noirq(struct device *dev)
 	ret = pm_generic_poweroff_noirq(dev);
 	if (ret)
 		return ret;
+
+	if (device_may_wakeup(dev)
+	    && genpd->active_wakeup && genpd->active_wakeup(dev))
+		return 0;
 
 	if (genpd->stop_device)
 		genpd->stop_device(dev);
