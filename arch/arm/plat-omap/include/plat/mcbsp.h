fd0dc11f9437d3e88a7382e68a17d7522a84da0f
Jarkko Nikula <jhnikula@gmail.com>
2011-07-01 08:52:27 +0000

    omap: mcbsp: Remove port number enums
    These McBSP port number enums are used only in two places in the McBSP code
    so we may remove then and just use numeric values like rest of the code does.
    
    Signed-off-by: Jarkko Nikula <jhnikula@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/mcbsp.h
+++ arch/arm/plat-omap/include/plat/mcbsp.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP_MCBSP_PLATFORM_DEVICE(port_nr)		\
 static struct platform_device omap_mcbsp##port_nr = {	\
 	.name	= "omap-mcbsp-dai",			\
-	.id	= OMAP_MCBSP##port_nr,			\
+	.id	= port_nr - 1,			\
 }
 
 #define MCBSP_CONFIG_TYPE2	0x2
@@ -331,14 +331,6 @@ struct omap_mcbsp_reg_cfg {
 	u16 xccr;
 	u16 rccr;
 };
-
-typedef enum {
-	OMAP_MCBSP1 = 0,
-	OMAP_MCBSP2,
-	OMAP_MCBSP3,
-	OMAP_MCBSP4,
-	OMAP_MCBSP5
-} omap_mcbsp_id;
 
 typedef enum {
 	OMAP_MCBSP_WORD_8 = 0,
