d08ae05b9b349fcf207ebf4b697fd5e52b43b422
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-03 16:20:57 -0400

    xen: Add module.h to modular drivers/xen users.
    Previously these drivers just got module.h implicitly, but we
    are cleaning that up and it will be no longer.  Call out the
    real users of it.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/xen/xenbus/xenbus_probe_frontend.c
+++ drivers/xen/xenbus/xenbus_probe_frontend.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kthread.h>
 #include <linux/mutex.h>
 #include <linux/io.h>
+#include <linux/module.h>
 
 #include <asm/page.h>
 #include <asm/pgtable.h>
