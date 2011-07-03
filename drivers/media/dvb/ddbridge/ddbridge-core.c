8ff4e1a8725b64f6ae61b41285a6fe399ac88aaa
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-07-03 19:00:32 -0300

    [media] ddbridge: use linux/io.h, instead of asm/io.h
    WARNING: Use #include <linux/io.h> instead of <asm/io.h>
    +#include <asm/io.h>
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/ddbridge/ddbridge-core.c
+++ drivers/media/dvb/ddbridge/ddbridge-core.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/slab.h>
 #include <linux/poll.h>
-#include <asm/io.h>
+#include <linux/io.h>
 #include <linux/pci.h>
 #include <linux/pci_ids.h>
 #include <linux/timer.h>
