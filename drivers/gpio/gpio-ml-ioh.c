71c718315d269d68bb3adb1a07ab5a60d4c7e1fc
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-03 13:38:09 -0400

    drivers/gpio: Fix drivers who are implicit users of module.h
    A pending cleanup will mean that module.h won't be implicitly
    everywhere anymore.  Make sure the modular drivers in gpio
    are actually calling out for <module.h> explicitly in advance.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/gpio/gpio-ml-ioh.c
+++ drivers/gpio/gpio-ml-ioh.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  */
+#include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/pci.h>
