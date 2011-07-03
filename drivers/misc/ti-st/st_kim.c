8f854e0ec8ca4888962037469acf6408d5f7ece9
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-03 15:14:56 -0400

    drivers/misc: Add module.h to files who are really modular.
    These files really need the full module.h header file present, but
    were just getting it implicitly before.  Fix it up in advance so we
    avoid build failures once the cleanup commit is present.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/misc/ti-st/st_kim.c
+++ drivers/misc/ti-st/st_kim.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/skbuff.h>
 #include <linux/ti_wilink_st.h>
+#include <linux/module.h>
 
 
 #define MAX_ST_DEVICES	3	/* Imagine 1 on each UART for now */
