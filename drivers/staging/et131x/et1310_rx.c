2186ef23745776d9e23518c236e020a70f9a8224
Mark Einon <mark.einon@gmail.com>
2011-07-03 18:08:23 +0100

    staging: et131x: Remove some unecessary packet accounting code
    The ce_stats ipackets and opackets members are only used to update
    the net_device_stats->[r,t]x_packets counters, so get rid of them.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/et131x/et1310_rx.c
+++ drivers/staging/et131x/et1310_rx.c
@@ -1132,7 +1132,7 @@ void et131x_handle_recv_interrupt(struct et131x_adapter *etdev)
 			continue;
 
 		/* Increment the number of packets we received */
-		etdev->stats.ipackets++;
+		etdev->net_stats.rx_packets++;
 
 		/* Set the status on the packet, either resources or success */
 		if (etdev->rx_ring.nReadyRecv < RFD_LOW_WATER_MARK) {
