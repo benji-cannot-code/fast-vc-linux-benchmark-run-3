0ccba66263d9f7d2cfc6d774a50e8f1af1da6643
Roland Vossen <rvossen@broadcom.com>
2011-07-05 22:06:07 +0200

    staging: brcm80211: removed unused code and definitions from fullmac
    Code cleanup.
    
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Reviewed-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/dhd_common.c
+++ drivers/staging/brcm80211/brcmfmac/dhd_common.c
@@ -415,20 +415,6 @@ int brcmf_c_ioctl(struct brcmf_pub *drvr, struct brcmf_c_ioctl *ioc, void *buf,
 			if (bcmerror != -ENOTSUPP)
 				break;
 
-			/* not in generic table, try protocol module */
-			if (ioc->cmd == BRCMF_GET_VAR)
-				bcmerror = brcmf_proto_iovar_op(drvr, buf,
-								arg, arglen,
-								buf, buflen,
-								IOV_GET);
-			else
-				bcmerror = brcmf_proto_iovar_op(drvr, buf,
-								NULL, 0, arg,
-								arglen,
-								IOV_SET);
-			if (bcmerror != -ENOTSUPP)
-				break;
-
 			/* if still not found, try bus module */
 			if (ioc->cmd == BRCMF_GET_VAR)
 				bcmerror = brcmf_sdbrcm_bus_iovar_op(drvr,
@@ -816,8 +802,6 @@ brcmf_c_host_event(struct brcmf_info *drvr_priv, int *ifidx, void *pktdata,
 		}
 		/* send up the if event: btamp user needs it */
 		*ifidx = brcmf_ifname2idx(drvr_priv, event->ifname);
-		/* push up to external supp/auth */
-		brcmf_event(drvr_priv, (char *)pvt_data, evlen, *ifidx);
 		break;
 
 		/* These are what external supplicant/authenticator wants */
@@ -830,8 +814,6 @@ brcmf_c_host_event(struct brcmf_info *drvr_priv, int *ifidx, void *pktdata,
 		/* Fall through: this should get _everything_  */
 
 		*ifidx = brcmf_ifname2idx(drvr_priv, event->ifname);
-		/* push up to external supp/auth */
-		brcmf_event(drvr_priv, (char *)pvt_data, evlen, *ifidx);
 		DHD_TRACE(("%s: MAC event %d, flags %x, status %x\n",
 			   __func__, type, flags, status));
 
