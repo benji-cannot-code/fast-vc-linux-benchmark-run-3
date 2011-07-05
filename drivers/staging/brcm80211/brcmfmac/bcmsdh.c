b4fb7b09080c9269bce54f18104dd32b085bc19e
Roland Vossen <rvossen@broadcom.com>
2011-07-06 00:02:51 +0200

    staging: brcm80211: moved fullmac definitions from .h to .c files
    And deleted unused definitions. Improves code since it makes clear that the
    definitions are only used by a specific .c file.
    
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/bcmsdh.c
+++ drivers/staging/brcm80211/brcmfmac/bcmsdh.c
@@ -59,6 +59,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* debugging macros */
 #define SDLX_MSG(x)
 
+#define SDIOH_CMD_TYPE_NORMAL   0	/* Normal command */
+#define SDIOH_CMD_TYPE_APPEND   1	/* Append command */
+#define SDIOH_CMD_TYPE_CUTTHRU  2	/* Cut-through command */
+
+#define SDIOH_DATA_PIO          0	/* PIO mode */
+#define SDIOH_DATA_DMA          1	/* DMA mode */
+
 struct brcmf_sdio_card {
 	bool init_success;	/* underlying driver successfully attached */
 	void *sdioh;		/* handler for sdioh */
