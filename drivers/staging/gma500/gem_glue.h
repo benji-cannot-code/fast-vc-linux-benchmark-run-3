d9ef5295b61e7b70c475f01f66e39d1a2d3509b3
Alan Cox <alan@linux.intel.com>
2011-07-05 15:35:55 +0100

    gma500: GEM glue
    Add this temporarily so we can keep making progress and also bundle all the
    GEM bits we need together in our staging driver while we get them into GEM
    itself.
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

---
+++ drivers/staging/gma500/gem_glue.h
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+extern void drm_gem_object_release_wrap(struct drm_gem_object *obj);
+extern int drm_gem_private_object_init(struct drm_device *dev,
+			struct drm_gem_object *obj, size_t size);
+
