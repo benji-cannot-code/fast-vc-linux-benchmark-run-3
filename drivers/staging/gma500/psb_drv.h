9dec37ccf5bb333375a1c571bc1b26d2b765b413
Alan Cox <alan@linux.intel.com>
2011-07-05 15:41:43 +0100

    gma500: the GEM and GTT code is device independant
    Rename the gem and gtt files accordingly.
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/gma500/psb_drv.h
+++ drivers/staging/gma500/psb_drv.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "psb_drm.h"
 #include "psb_reg.h"
 #include "psb_intel_drv.h"
-#include "psb_gtt.h"
+#include "gtt.h"
 #include "psb_powermgmt.h"
 #include "mrst.h"
 
