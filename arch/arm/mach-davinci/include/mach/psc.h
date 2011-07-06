0bd1481919634ed4d190ac21d3fa8676ab533224
Sekhar Nori <nsekhar@ti.com>
2011-07-06 06:01:21 +0000

    davinci: pass clock flags to davinci_psc_config()
    Enabling or disabling a PSC can take certain
    modifiers like "disable with reset", "force
    enable/disable" and "enable/disable with local
    reset" apart from the regular clock gating
    functionality.
    
    Pass a flags argument to davinci_psc_config()
    so these variations can be supported there.
    
    At this time only "disable with reset" is
    supported, but other functionality will be
    added in subsequent patches.
    
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/include/mach/psc.h
+++ arch/arm/mach-davinci/include/mach/psc.h
@@ -250,7 +250,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int davinci_psc_is_clk_active(unsigned int ctlr, unsigned int id);
 extern void davinci_psc_config(unsigned int domain, unsigned int ctlr,
-		unsigned int id, u32 next_state);
+		unsigned int id, bool enable, u32 flags);
 
 #endif
 
