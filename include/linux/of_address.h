5069bcde1a1db4d7c5faf98455c20425fae98efa
Grant Likely <grant.likely@secretlab.ca>
2011-07-05 23:42:37 -0600

    dt: include linux/errno.h in linux/of_address.h
    of_address.h makes reference to some of the error code #defines, so it
    needs to include errno.h.  If CONFIG_PCI is not selected, then some files
    will fail to compile.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/of_address.h
+++ include/linux/of_address.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __OF_ADDRESS_H
 #define __OF_ADDRESS_H
 #include <linux/ioport.h>
+#include <linux/errno.h>
 #include <linux/of.h>
 
 extern u64 of_translate_address(struct device_node *np, const __be32 *addr);
