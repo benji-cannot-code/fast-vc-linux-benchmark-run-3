e1fee96153ded760fa46dc507d7d842f037f4620
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-07-05 22:28:09 -0400

    ARM: mach-ixp4xx: move from ARM_DMA_ZONE_SIZE to mdesc->dma_zone_size
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/mach-ixp4xx/include/mach/memory.h
+++ arch/arm/mach-ixp4xx/include/mach/memory.h
@@ -15,8 +15,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define PLAT_PHYS_OFFSET	UL(0x00000000)
 
-#ifdef CONFIG_PCI
-#define ARM_DMA_ZONE_SIZE	SZ_64M
-#endif
-
 #endif
