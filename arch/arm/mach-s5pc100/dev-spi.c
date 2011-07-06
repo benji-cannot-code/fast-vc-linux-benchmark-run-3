59bc5a6d111961c44a9e0a6b4779c025626624b8
Padmavathi Venna <padma.v@samsung.com>
2011-07-06 15:37:08 +0900

    ARM: S5PC100: Fix for compilation error
    S5PC100 Compilation fails without this patch
    
    Signed-off-by: Padmavathi Venna <padma.v@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s5pc100/dev-spi.c
+++ arch/arm/mach-s5pc100/dev-spi.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/dma.h>
 #include <mach/map.h>
 #include <mach/spi-clocks.h>
+#include <mach/irqs.h>
 
 #include <plat/s3c64xx-spi.h>
 #include <plat/gpio-cfg.h>
