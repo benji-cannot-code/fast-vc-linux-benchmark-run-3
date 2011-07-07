4a2b3d298565e1dbea003f255c5f87ca4c58bd9a
Rafał Miłecki <zajec5@gmail.com>
2011-07-07 18:58:25 +0200

    b43: LCN-PHY: include new PHY in common code
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/phy_common.h
+++ drivers/net/wireless/b43/phy_common.h
@@ -196,6 +196,7 @@ struct b43_phy_g;
 struct b43_phy_n;
 struct b43_phy_lp;
 struct b43_phy_ht;
+struct b43_phy_lcn;
 
 struct b43_phy {
 	/* Hardware operation callbacks. */
@@ -220,6 +221,8 @@ struct b43_phy {
 		struct b43_phy_lp *lp;
 		/* HT-PHY specific information */
 		struct b43_phy_ht *ht;
+		/* LCN-PHY specific information */
+		struct b43_phy_lcn *lcn;
 	};
 
 	/* Band support flags. */
