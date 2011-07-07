f7703ebb47f8d768d01d1892990e7d3d14491160
Pekka Enberg <penberg@kernel.org>
2011-07-07 22:47:01 +0300

    SLUB: Fix missing <linux/stacktrace.h> include
    This fixes the following build breakage commit d6543e3 ("slub: Enable backtrace
    for create/delete points"):
    
      CC      mm/slub.o
    mm/slub.c: In function ‘set_track’:
    mm/slub.c:428: error: storage size of ‘trace’ isn’t known
    mm/slub.c:435: error: implicit declaration of function ‘save_stack_trace’
    mm/slub.c:428: warning: unused variable ‘trace’
    make[1]: *** [mm/slub.o] Error 1
    make: *** [mm/slub.o] Error 2
    
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- mm/slub.c
+++ mm/slub.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/memory.h>
 #include <linux/math64.h>
 #include <linux/fault-inject.h>
+#include <linux/stacktrace.h>
 
 #include <trace/events/kmem.h>
 
