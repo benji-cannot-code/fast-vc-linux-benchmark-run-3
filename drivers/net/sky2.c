b482a22f5cc7151034e1f905b84fd9892223869c
stephen hemminger <shemminger@vyatta.com>
2011-07-07 05:51:00 +0000

    sky2: version 1.29
    Since new hardware chip support was added bump version.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/sky2.c
+++ drivers/net/sky2.c
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sky2.h"
 
 #define DRV_NAME		"sky2"
-#define DRV_VERSION		"1.28"
+#define DRV_VERSION		"1.29"
 
 /*
  * The Yukon II chipset takes 64 bit command blocks (called list elements)
