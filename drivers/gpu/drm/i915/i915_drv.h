86ae86ccae58b29c2655b98525dde720d8fea5c5
Chris Wilson <chris@chris-wilson.co.uk>
2011-07-08 12:22:42 +0100

    drm/i915: Perform intel_enable_fbc() from a delayed task
    In order to accommodate the requirements of re-enabling FBC after
    page-flipping, but to avoid doing so and incurring the cost of a wait
    for vblank in the middle of a page-flip sequence, we defer the actual
    enablement by 50ms. If any request to disable FBC arrive within that
    interval, the enablement is cancelled and we are saved from blocking on
    the wait.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -267,6 +267,7 @@ enum intel_pch {
 #define QUIRK_PIPEA_FORCE (1<<0)
 
 struct intel_fbdev;
+struct intel_fbc_work;
 
 typedef struct drm_i915_private {
 	struct drm_device *dev;
@@ -336,6 +337,7 @@ typedef struct drm_i915_private {
 	int cfb_fence;
 	int cfb_plane;
 	int cfb_y;
+	struct intel_fbc_work *fbc_work;
 
 	struct intel_opregion opregion;
 
