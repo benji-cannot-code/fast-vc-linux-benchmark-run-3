c0d6e1222bdee0061411676397e62ebfbca3c7bf
Alan Cox <alan@linux.intel.com>
2011-07-08 09:43:45 +0100

    gma500: Fix symbol clash with i915
    Randy Dunlap reports:
    | when both CONFIG_DRM_I915=y and CONFIG_DRM_PSB=y:
    | drivers/staging/built-in.o: In function `intel_opregion_init':
    | (.text+0x47943): multiple definition of `intel_opregion_init'
    | drivers/gpu/built-in.o:(.text+0x17277a): first defined here
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/gma500/intel_opregion.c
+++ drivers/staging/gma500/intel_opregion.c
@@ -48,7 +48,7 @@ struct opregion_acpi {
 	/*FIXME: add it later*/
 } __packed;
 
-int intel_opregion_init(struct drm_device *dev)
+int gma_intel_opregion_init(struct drm_device *dev)
 {
 	struct drm_psb_private *dev_priv = dev->dev_private;
 	u32 opregion_phy;
@@ -72,7 +72,7 @@ int intel_opregion_init(struct drm_device *dev)
 	return 0;
 }
 
-int intel_opregion_exit(struct drm_device *dev)
+int gma_intel_opregion_exit(struct drm_device *dev)
 {
 	struct drm_psb_private *dev_priv = dev->dev_private;
 	if (dev_priv->lid_state)
