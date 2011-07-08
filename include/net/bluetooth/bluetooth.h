fa08fdf264e467069b14e907d4d32f1b1df71b2b
Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
2011-07-08 18:31:46 -0300

    Bluetooth: Add support for returning the encryption key size
    This will be useful when userspace wants to restrict some kinds of
    operations based on the length of the key size used to encrypt the
    link.
    
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- include/net/bluetooth/bluetooth.h
+++ include/net/bluetooth/bluetooth.h
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BT_SECURITY	4
 struct bt_security {
 	__u8 level;
+	__u8 key_size;
 };
 #define BT_SECURITY_SDP		0
 #define BT_SECURITY_LOW		1
