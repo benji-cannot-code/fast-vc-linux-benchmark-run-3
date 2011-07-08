a1ae9630107762ed033015b9fe5f06e15e59da37
Wolfram Sang <w.sang@pengutronix.de>
2011-07-08 14:33:02 +0900

    ARM: S3C24XX: drop return codes in void function of dma.c
    Commit bb072c3c (ARM / Samsung: Use struct syscore_ops for "core" power
    management) turned s3c2410_dma_resume_chan() from int to void. So, drop
    the actual return values, too. Fixes:
    
    arch/arm/plat-s3c24xx/dma.c: In function 's3c2410_dma_resume_chan':
    arch/arm/plat-s3c24xx/dma.c:1238:3: warning: 'return' with a value, in function returning void
    arch/arm/plat-s3c24xx/dma.c:1250:2: warning: 'return' with a value, in function returning void
    
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Acked-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-s3c24xx/dma.c
+++ arch/arm/plat-s3c24xx/dma.c
@@ -1232,7 +1232,7 @@ static void s3c2410_dma_resume_chan(struct s3c2410_dma_chan *cp)
 	/* restore channel's hardware configuration */
 
 	if (!cp->in_use)
-		return 0;
+		return;
 
 	printk(KERN_INFO "dma%d: restoring configuration\n", cp->number);
 
@@ -1243,8 +1243,6 @@ static void s3c2410_dma_resume_chan(struct s3c2410_dma_chan *cp)
 
 	if (cp->map != NULL)
 		dma_sel.select(cp, cp->map);
-
-	return 0;
 }
 
 static void s3c2410_dma_resume(void)
