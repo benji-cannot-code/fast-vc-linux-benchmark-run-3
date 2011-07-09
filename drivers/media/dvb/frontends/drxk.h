8a1e80216cef15702051939686f11bc9f43a200b
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-07-09 12:36:58 -0300

    [media] drxk: Move I2C address into a config structure
    Currently, the only parameter to be configured is the I2C
    address. However, Terratec H5 logs shows that it needs a different
    setting for some things, and it has its own firmware.
    
    So, move the addr into a config structure, in order to allow adding
    the required configuration bits.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/drxk.h
+++ drivers/media/dvb/frontends/drxk.h
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/i2c.h>
 
-extern struct dvb_frontend *drxk_attach(struct i2c_adapter *i2c,
-					u8 adr,
+struct drxk_config {
+	u8 adr;
+};
+
+extern struct dvb_frontend *drxk_attach(const struct drxk_config *config,
+					struct i2c_adapter *i2c,
 					struct dvb_frontend **fe_t);
 #endif
