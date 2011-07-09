20008c34aa7cfc16e11d66e8f0d1f113f6816b86
Raghavendra D Prabhu <rprabhu@wnohang.net>
2011-07-09 21:54:49 +0530

    nconfig: Avoid Wunused-but-set warning
    I am seeing Wunused-but-set warning while make nconfig.  Looks like
    active_menu is not used. Removing it fixes the warning.
    
    Signed-off-by: Raghavendra D Prabhu <rprabhu@wnohang.net>
    Acked-by: WANG Cong <xiyou.wangcong@gmail.com>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/nconf.c
+++ scripts/kconfig/nconf.c
@@ -1068,7 +1068,6 @@ static void conf(struct menu *menu)
 	struct menu *submenu = 0;
 	const char *prompt = menu_get_prompt(menu);
 	struct symbol *sym;
-	struct menu *active_menu = NULL;
 	int res;
 	int current_index = 0;
 	int last_top_row = 0;
@@ -1153,7 +1152,6 @@ static void conf(struct menu *menu)
 			continue;
 
 		submenu = (struct menu *) item_data();
-		active_menu = (struct menu *)item_data();
 		if (!submenu || !menu_is_visible(submenu))
 			continue;
 		if (submenu)
