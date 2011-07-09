dc05473de18071008165fa1f09c78784e4b4bc60
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-07-09 13:06:12 -0300

    [media] drxk: Convert an #ifdef logic as a new config parameter
    Instead of using #ifdef I2C_LONG_ADR for some devices, convert
    it into a parameter. Terratec H5 logs from the original driver
    seems to need this mode.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/drxk.h
+++ drivers/media/dvb/frontends/drxk.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct drxk_config {
 	u8 adr;
+	u32 single_master : 1;
 };
 
 extern struct dvb_frontend *drxk_attach(const struct drxk_config *config,
