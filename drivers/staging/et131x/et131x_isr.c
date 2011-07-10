8272386561f0f65602a6ed2976efa92ecf328cd0
Mark Einon <mark.einon@gmail.com>
2011-07-10 16:35:33 +0100

    staging: et131x: add et1310_ prefix to et1310_mac.c functions
    Making finger pointing that bit easier.
    Tested on an ET-131x device.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/et131x/et131x_isr.c
+++ drivers/staging/et131x/et131x_isr.c
@@ -462,7 +462,7 @@ void et131x_isr_handler(struct work_struct *work)
 			 * to maintain the top, software managed bits of the
 			 * counter(s).
 			 */
-			handle_macstat_interrupt(etdev);
+			et1310_handle_macstat_interrupt(etdev);
 		}
 
 		/* Handle SLV Timeout Interrupt */
