1d48fa8c1e9f3e804701959efcc499ca9c3b19b3
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-10 13:09:12 -0400

    staging: Add export.h for THIS_MODULE/EXPORT_SYMBOL to drivers/staging users.
    Lots of drivers have in the past expected the presence of basic things
    like THIS_MODULE and EXPORT_SYMBOL.  With the header cleanup, they wont
    have these.  Call out the include explicitly.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/staging/iio/accel/adis16240_trigger.c
+++ drivers/staging/iio/accel/adis16240_trigger.c
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/spi/spi.h>
+#include <linux/export.h>
 
 #include "../iio.h"
 #include "../trigger.h"
--- drivers/staging/iio/industrialio-buffer.c
+++ drivers/staging/iio/industrialio-buffer.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * - Alternative access techniques?
  */
 #include <linux/kernel.h>
+#include <linux/export.h>
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
