fab58e8ba685f6007b96ea5593d7663836465972
Steven Rostedt <srostedt@redhat.com>
2011-07-11 10:12:59 -0400

    ftrace: Fix warning when CONFIG_FUNCTION_TRACER is not defined
    The struct ftrace_hash was declared within CONFIG_FUNCTION_TRACER
    but was referenced outside of it.
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/ftrace.h>
 
+struct ftrace_hash;
+
 #ifdef CONFIG_FUNCTION_TRACER
 
 extern int ftrace_enabled;
@@ -29,8 +31,6 @@ ftrace_enable_sysctl(struct ctl_table *table, int write,
 		     loff_t *ppos);
 
 typedef void (*ftrace_func_t)(unsigned long ip, unsigned long parent_ip);
-
-struct ftrace_hash;
 
 enum {
 	FTRACE_OPS_FL_ENABLED		= 1 << 0,
