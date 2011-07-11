5599b388d8b4a09d22fb092f44c824ff535f310f
Alex Deucher <alexdeucher@gmail.com>
2011-07-11 20:22:33 +0000

    drm/radeon/kms: fix typo in read_disabled vbios code
    BUS_CNTL reg and bits moved between pre-PCIE and PCIE asics.
    
    Signed-off-by: Alex Deucher <alexdeucher@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/radeon_reg.h
+++ drivers/gpu/drm/radeon/radeon_reg.h
@@ -301,6 +301,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #       define RADEON_BUS_READ_BURST         (1 << 30)
 #define RADEON_BUS_CNTL1                    0x0034
 #       define RADEON_BUS_WAIT_ON_LOCK_EN    (1 << 4)
+#define RV370_BUS_CNTL                      0x004c
+#       define RV370_BUS_BIOS_DIS_ROM        (1 << 2)
 /* rv370/rv380, rv410, r423/r430/r480, r5xx */
 #define RADEON_MSI_REARM_EN		    0x0160
 #	define RV370_MSI_REARM_EN	     (1 << 0)
