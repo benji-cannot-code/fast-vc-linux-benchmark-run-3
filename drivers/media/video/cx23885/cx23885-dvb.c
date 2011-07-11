ab166a1c619f70f117d8022bbf9547f23a831194
istvan_v@mailbox.hu <istvan_v@mailbox.hu>
2011-07-11 10:58:35 -0300

    [media] cx23885: added support for card 107d:6f39
    This patch, based on code by Mirek Slugen, implements support for the
    Leadtek WinFast PxDVR3200 H card with XC4000 tuner (107d:6f39).
    
    Signed-off-by: Istvan Varga <istvan_v@mailbox.hu>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx23885/cx23885-dvb.c
+++ drivers/media/video/cx23885/cx23885-dvb.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tda8290.h"
 #include "tda18271.h"
 #include "lgdt330x.h"
+#include "xc4000.h"
 #include "xc5000.h"
 #include "max2165.h"
 #include "tda10048.h"
@@ -920,6 +921,26 @@ static int dvb_register(struct cx23885_tsport *port)
 				&cfg);
 			if (fe != NULL && fe->ops.tuner_ops.set_config != NULL)
 				fe->ops.tuner_ops.set_config(fe, &ctl);
+		}
+		break;
+	case CX23885_BOARD_LEADTEK_WINFAST_PXDVR3200_H_XC4000:
+		i2c_bus = &dev->i2c_bus[0];
+
+		fe0->dvb.frontend = dvb_attach(zl10353_attach,
+					       &dvico_fusionhdtv_xc3028,
+					       &i2c_bus->i2c_adap);
+		if (fe0->dvb.frontend != NULL) {
+			struct dvb_frontend	*fe;
+			struct xc4000_config	cfg = {
+				.i2c_address	  = 0x61,
+				.default_pm	  = 0,
+				.dvb_amplitude	  = 134,
+				.set_smoothedcvbs = 1,
+				.if_khz		  = 4560
+			};
+
+			fe = dvb_attach(xc4000_attach, fe0->dvb.frontend,
+					&dev->i2c_bus[1].i2c_adap, &cfg);
 		}
 		break;
 	case CX23885_BOARD_TBS_6920:
