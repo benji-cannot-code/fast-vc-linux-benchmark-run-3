8836798792eb9f16c08f785134854a6a3021183b
Johannes Berg <johannes.berg@intel.com>
2011-07-13 10:39:18 +0200

    mwifiex: add wext include
    In trying to remove the wext includes from mac80211
    and cfg80211 I found that mwifiex currently uses
    them. This is wrong, it shouldn't, but to not break
    it completely include wext there.
    
    Please remove this and fix all the resulting bugs.
    
    Cc: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Acked-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwifiex/ioctl.h
+++ drivers/net/wireless/mwifiex/ioctl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MWIFIEX_IOCTL_H_
 #define _MWIFIEX_IOCTL_H_
 
+#include <linux/wireless.h>
 #include <net/mac80211.h>
 
 enum {
