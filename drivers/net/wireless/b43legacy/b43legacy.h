424e9ab8758cef6b851f1427a196c091de0febcd
Johannes Berg <johannes.berg@intel.com>
2011-07-13 10:39:14 +0200

    b43legacy: remove wireless extensions inclusions
    linux/wireless.h and net/iw_handler.h headers are
    for wireless extensions only, so mac80211 drivers
    shouldn't be including them.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43legacy/b43legacy.h
+++ drivers/net/wireless/b43legacy/b43legacy.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ssb/ssb.h>
 #include <linux/ssb/ssb_driver_chipcommon.h>
 
-#include <linux/wireless.h>
 #include <net/mac80211.h>
 
 #include "debugfs.h"
--- drivers/net/wireless/b43legacy/main.c
+++ drivers/net/wireless/b43legacy/main.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 #include <linux/etherdevice.h>
 #include <linux/firmware.h>
-#include <linux/wireless.h>
 #include <linux/workqueue.h>
 #include <linux/sched.h>
 #include <linux/skbuff.h>
