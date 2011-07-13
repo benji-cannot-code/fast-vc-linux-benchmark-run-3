732c49139fe733d533730d453356089c7ec506e0
Chen Gong <gong.chen@linux.intel.com>
2011-07-13 13:14:14 +0800

    ACPI, APEI, ERST, Fix erst-dbg long record reading issue
    When we debug ERST table with erst-dbg, if the error record in ERST
    table is too long(>4K), it can't be read out.  So this patch increases
    the buffer size to 16K to ensure such error records can be read from
    ERST table.
    
    Signed-off-by: Chen Gong <gong.chen@linux.intel.com>
    Signed-off-by: Huang Ying <ying.huang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/apei/erst-dbg.c
+++ drivers/acpi/apei/erst-dbg.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ERST_DBG_PFX			"ERST DBG: "
 
-#define ERST_DBG_RECORD_LEN_MAX		4096
+#define ERST_DBG_RECORD_LEN_MAX		0x4000
 
 static void *erst_dbg_buf;
 static unsigned int erst_dbg_buf_len;
