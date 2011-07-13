cb29b65f754e448a009cfd502045bc62ae88721d
Johannes Berg <johannes.berg@intel.com>
2011-07-13 10:39:19 +0200

    mac80211: remove linux/wireless.h inclusion
    linux/wireless.h is for wireless extensions only, so
    mac80211 shouldn't include it since it uses cfg80211.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- include/net/mac80211.h
+++ include/net/mac80211.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/if_ether.h>
 #include <linux/skbuff.h>
-#include <linux/wireless.h>
 #include <linux/device.h>
 #include <linux/ieee80211.h>
 #include <net/cfg80211.h>
