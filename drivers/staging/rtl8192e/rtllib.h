7f6009f6f9835e963b8dbb18dd6f9ff385f7ed80
Larry Finger <Larry.Finger@lwfinger.net>
2011-07-13 18:47:44 -0500

    staging: rtl8192e: Remove dead code associated with TCP_CSUM_OFFLOAD_RX
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- drivers/staging/rtl8192e/rtllib.h
+++ drivers/staging/rtl8192e/rtllib.h
@@ -1096,9 +1096,6 @@ struct rtllib_rx_stats {
 	char   cck_adc_pwdb[4];
 	u16    Seq_Num;
 	u8     nTotalAggPkt;
-#ifdef TCP_CSUM_OFFLOAD_RX
-	u8	tcp_csum_valid;
-#endif
 #endif
 
 };
@@ -1399,9 +1396,6 @@ struct rtllib_rxb {
 	struct sk_buff *subframes[MAX_SUBFRAME_COUNT];
 	u8 dst[ETH_ALEN];
 	u8 src[ETH_ALEN];
-#ifdef TCP_CSUM_OFFLOAD_RX
-	u8 tcp_csum_valid;
-#endif
 }__attribute__((packed));
 
 typedef union _frameqos {
