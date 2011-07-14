f50ea3d536c9fc282a84a0f8c96c361c217b93f3
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2011-07-14 18:39:10 +0200

    mmc: tmio: fix a deadlock
    Currently the tmio-mmc driver contains a recursive runtime PM method
    invocation, which leads to a deadlock on a mutex. Avoid it by taking
    care not to request DMA too early.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/tmio_mmc_dma.c
+++ drivers/mmc/host/tmio_mmc_dma.c
@@ -23,8 +23,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define TMIO_MMC_MIN_DMA_LEN 8
 
-static void tmio_mmc_enable_dma(struct tmio_mmc_host *host, bool enable)
+void tmio_mmc_enable_dma(struct tmio_mmc_host *host, bool enable)
 {
+	if (!host->chan_tx || !host->chan_rx)
+		return;
+
 #if defined(CONFIG_SUPERH) || defined(CONFIG_ARCH_SHMOBILE)
 	/* Switch DMA mode on or off - SuperH specific? */
 	sd_ctrl_write16(host, CTL_DMA_ENABLE, enable ? 2 : 0);
