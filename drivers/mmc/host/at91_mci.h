3c13f5c78bb2c47fca92455eb2b797be9bc5b675
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2011-07-15 01:52:01 +0200

    mmc: at91_mci: move register header from include/ to drivers/
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/at91_mci.h
+++ drivers/mmc/host/at91_mci.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/arm/mach-at91/include/mach/at91_mci.h
+ * drivers/mmc/host/at91_mci.h
  *
  * Copyright (C) 2005 Ivan Kokshaysky
  * Copyright (C) SAN People
