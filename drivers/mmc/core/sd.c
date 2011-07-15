571055cb821007736ef631bab197076a82fec94b
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-15 12:01:27 -0400

    mmc: Fix implicit use of stat.h header in associated files
    Once the implicit use of module.h is prevented, these files will
    fail to find the stat.h header content.
    
    Fix up the implicit usage expectations in advance of the cleanup.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/err.h>
 #include <linux/slab.h>
+#include <linux/stat.h>
 
 #include <linux/mmc/host.h>
 #include <linux/mmc/card.h>
