e8362e2ddb1aa53da0d0fbc05ad4f8a20e74057f
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-17 16:50:39 -0400

    drivers/uwb: fix implicit use of stat.h
    The module.h (via device.h) was pulling handfuls of implicitly present
    header files -- including basic stuff like stat.h.  Fix up these stat.h
    users in advance so they don't break on the cleanup.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/uwb/lc-dev.c
+++ drivers/uwb/lc-dev.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/kdev_t.h>
 #include <linux/random.h>
+#include <linux/stat.h>
 #include "uwb-internal.h"
 
 /* We initialize addresses to 0xff (invalid, as it is bcast) */
