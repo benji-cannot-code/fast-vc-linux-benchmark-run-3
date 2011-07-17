3c1e54ccc573bde4742c45ec38e19d4c6d46eec3
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-17 16:24:35 -0400

    regulator: Fix implicit use of notifier.h by driver.h
    This was implicitly appearing by way of module.h -- but when
    we fix that, we'll get this:
    
    In file included from drivers/regulator/dummy.c:21:
    include/linux/regulator/driver.h:197: error: field 'notifier' has incomplete type
    make[3]: *** [drivers/regulator/dummy.o] Error 1
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/linux/regulator/driver.h
+++ include/linux/regulator/driver.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __LINUX_REGULATOR_DRIVER_H_
 
 #include <linux/device.h>
+#include <linux/notifier.h>
 #include <linux/regulator/consumer.h>
 
 struct regulator_dev;
