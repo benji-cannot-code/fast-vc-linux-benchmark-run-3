d22b378ecd17feccbbae508c320d97a086a12c2f
Borislav Petkov <bp@alien8.de>
2011-07-18 21:10:54 +0200

    x86, vdso: Drop now wrong comment
    Now that 1b3f2a72bbcfdf92e368a44448c45eb639b05b5e is in, it is very
    important that the below lying comment be removed! :-)
    
    Signed-off-by: Borislav Petkov <bp@alien8.de>
    Link: http://lkml.kernel.org/r/20110718191054.GA18359@liondog.tnic
    Acked-by: Andy Lutomirski <luto@mit.edu>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/vdso/vclock_gettime.c
+++ arch/x86/vdso/vclock_gettime.c
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * The code should have no internal unresolved relocations.
  * Check with readelf after changing.
- * Also alternative() doesn't work.
  */
 
 /* Disable profiling for userspace code: */
