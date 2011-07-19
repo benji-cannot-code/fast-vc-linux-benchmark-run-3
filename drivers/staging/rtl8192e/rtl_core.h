26f884e4e88591acbf23f819415078236e4dd595
Larry Finger <Larry.Finger@lwfinger.net>
2011-07-19 00:21:31 -0500

    staging: rtl8192e: Convert typedef Stats to struct rt_stats
    Remove typedef from struct.
    Rename struct.
    Rename uses.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- drivers/staging/rtl8192e/rtl_core.h
+++ drivers/staging/rtl8192e/rtl_core.h
@@ -377,8 +377,7 @@ struct rt_smooth_data_4rf {
 	u32	TotalVal[4];
 };//, *struct rt_smooth_data_4rf *;
 
-typedef struct Stats
-{
+struct rt_stats {
 	unsigned long txrdu;
 	unsigned long rxrdu;
 	unsigned long rxok;
@@ -471,7 +470,7 @@ typedef struct Stats
 	u32 Slide_Beacon_Total;
 	struct rt_smooth_data_4rf cck_adc_pwdb;
 	u32	CurrentShowTxate;
-} Stats;
+};
 
 struct channel_access_setting {
 	u16 SIFS_Timer;
@@ -633,7 +632,7 @@ typedef struct r8192_priv
 	struct semaphore			rf_sem;
 	struct mutex				mutex;
 
-	struct Stats				stats;
+	struct rt_stats stats;
 	struct iw_statistics			wstats;
 	struct proc_dir_entry		*dir_dev;
 
