18f85ac7a5c7c09454ea296053154ccd76d01432
Timur Tabi <timur@freescale.com>
2011-07-19 15:45:51 -0500

    drivers/virt: add missing linux/interrupt.h to fsl_hypervisor.c
    fsl_hypervisor.c calls request_irq() but does not include linux/interrupt.h.
    Normally, the driver will compile without error, but it can fail on some
    configurations.
    
    Signed-off-by: Timur Tabi <timur@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- drivers/virt/fsl_hypervisor.c
+++ drivers/virt/fsl_hypervisor.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/reboot.h>
 #include <linux/uaccess.h>
 #include <linux/notifier.h>
+#include <linux/interrupt.h>
 
 #include <linux/io.h>
 #include <asm/fsl_hcalls.h>
