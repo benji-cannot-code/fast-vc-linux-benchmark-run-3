c6c8c8096a389cd4fb542ed135f3155c11181bec
Larry Finger <Larry.Finger@lwfinger.net>
2011-07-19 00:04:08 -0500

    staging: rtl8192e: Convert typedef bandwidth_autoswitch to struct bandwidth_autoswitch
    Remove typedef from struct.
    Rename struct.
    Rename uses.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- drivers/staging/rtl8192e/rtllib.h
+++ drivers/staging/rtl8192e/rtllib.h
@@ -1780,13 +1780,12 @@ struct tx_pending {
 	struct rtllib_txb *txb;
 };//;
 
-typedef struct _bandwidth_autoswitch
-{
+struct bandwidth_autoswitch {
 	long threshold_20Mhzto40Mhz;
 	long	threshold_40Mhzto20Mhz;
 	bool bforced_tx20Mhz;
 	bool bautoswitch_enable;
-}bandwidth_autoswitch,*pbandwidth_autoswitch;
+};//,*pbandwidth_autoswitch;
 
 
 
@@ -2384,7 +2383,7 @@ struct rtllib_device {
 	u32	fsync_seconddiff_ratethreshold;
 	Fsync_State			fsync_state;
 	bool		bis_any_nonbepkts;
-	bandwidth_autoswitch bandwidth_auto_switch;
+	struct bandwidth_autoswitch bandwidth_auto_switch;
 	bool FwRWRF;
 
 	struct rt_link_detect LinkDetectInfo;
