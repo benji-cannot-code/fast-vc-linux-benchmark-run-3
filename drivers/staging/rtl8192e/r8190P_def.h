d09faa98062e919d341541238753269546edafc2
Larry Finger <Larry.Finger@lwfinger.net>
2011-07-19 12:54:24 -0500

    staging: rtl8192e: Convert typedef rtl819x_loopback_e to enum rtl819x_loopback
    Remove typedef from enum.
    Rename enum.
    Rename uses.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- drivers/staging/rtl8192e/r8190P_def.h
+++ drivers/staging/rtl8192e/r8190P_def.h
@@ -30,12 +30,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RX_CMD_QUEUE				1
 
 
-typedef enum _rtl819x_loopback_e{
+enum rtl819x_loopback {
 	RTL819X_NO_LOOPBACK = 0,
 	RTL819X_MAC_LOOPBACK = 1,
 	RTL819X_DMA_LOOPBACK = 2,
 	RTL819X_CCK_LOOPBACK = 3,
-} rtl819x_loopback_e;
+};
 
 
 #define RESET_DELAY_8185			20
