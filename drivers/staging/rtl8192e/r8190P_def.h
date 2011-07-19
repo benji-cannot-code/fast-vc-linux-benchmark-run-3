c6508433eabc4fd440378a15d97aba0731a14301
Larry Finger <Larry.Finger@lwfinger.net>
2011-07-18 22:56:46 -0500

    staging: rtl8192e: Convert typedef rx_fwinfo to struct rx_fwinfo
    Remove typedef from struct.
    Rename struct.
    Rename uses.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- drivers/staging/rtl8192e/r8190P_def.h
+++ drivers/staging/rtl8192e/r8190P_def.h
@@ -389,7 +389,7 @@ struct rx_desc {
 };//, *prx_desc;
 
 
-typedef struct _rx_fwinfo_819x_pci{
+struct rx_fwinfo {
 	u16			Reserved1:12;
 	u16			PartAggr:1;
 	u16			FirstAGGR:1;
@@ -408,6 +408,6 @@ typedef struct _rx_fwinfo_819x_pci{
 
 	u32			TSFL;
 
-}rx_fwinfo, *prx_fwinfo;
+};//, *prx_fwinfo;
 
 #endif
