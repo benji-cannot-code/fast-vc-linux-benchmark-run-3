b8188cd7b07bfa93b95ad4c9c42baa04d7264197
Hans de Goede <hdegoede@redhat.com>
2011-07-19 07:14:22 -0300

    [media] pwc: Use v4l2-device and v4l2-fh
    This is a preperation patch for adding support for control events. Actually
    enabling support for control events will be done in a separate patch, as that
    depends on the necessary poll changes going upstream
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/pwc/pwc.h
+++ drivers/media/video/pwc/pwc.h
@@ -36,8 +36,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/errno.h>
 #include <linux/videodev2.h>
 #include <media/v4l2-common.h>
+#include <media/v4l2-device.h>
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-ctrls.h>
+#include <media/v4l2-fh.h>
+#include <media/v4l2-event.h>
 #include <media/videobuf2-vmalloc.h>
 #ifdef CONFIG_USB_PWC_INPUT_EVDEV
 #include <linux/input.h>
@@ -199,6 +202,7 @@ struct pwc_frame_buf
 struct pwc_device
 {
 	struct video_device vdev;
+	struct v4l2_device v4l2_dev;
 	struct mutex modlock;
 
 	/* Pointer to our usb_device, may be NULL after unplug */
