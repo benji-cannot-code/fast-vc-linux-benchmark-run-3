a7810f6c760e5ed7feca1f2342913f1c777bcb19
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-07-20 19:52:49 -0300

    [media] mt2063: CodingStyle fixes
    convert to Unix eol, run Lindent and remove bad whitespaces
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/tuners/mt2063_cfg.h
+++ drivers/media/common/tuners/mt2063_cfg.h
@@ -1,13 +1,14 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
-
-static unsigned int mt2063_setTune(struct dvb_frontend *fe, UData_t f_in,UData_t bw_in, enum MTTune_atv_standard tv_type)
+static unsigned int mt2063_setTune(struct dvb_frontend *fe, UData_t f_in,
+				   UData_t bw_in,
+				   enum MTTune_atv_standard tv_type)
 {
 	//return (int)MT_Tune_atv(h, f_in, bw_in, tv_type);
 
-	struct dvb_frontend_ops	*frontend_ops = NULL;
-	struct dvb_tuner_ops	*tuner_ops = NULL;
-	struct tuner_state	t_state;
+	struct dvb_frontend_ops *frontend_ops = NULL;
+	struct dvb_tuner_ops *tuner_ops = NULL;
+	struct tuner_state t_state;
 	struct mt2063_state *mt2063State = fe->tuner_priv;
 	int err = 0;
 
@@ -19,20 +20,22 @@ static unsigned int mt2063_setTune(struct dvb_frontend *fe, UData_t f_in,UData_t
 	if (&frontend_ops->tuner_ops)
 		tuner_ops = &frontend_ops->tuner_ops;
 	if (tuner_ops->set_state) {
-		if ((err = tuner_ops->set_state(fe, DVBFE_TUNER_FREQUENCY, &t_state)) < 0) {
+		if ((err =
+		     tuner_ops->set_state(fe, DVBFE_TUNER_FREQUENCY,
+					  &t_state)) < 0) {
 			printk("%s: Invalid parameter\n", __func__);
 			return err;
 		}
 	}
-	
+
 	return err;
 }
 
 static unsigned int mt2063_lockStatus(struct dvb_frontend *fe)
 {
-	struct dvb_frontend_ops	*frontend_ops = &fe->ops;
-	struct dvb_tuner_ops	*tuner_ops = &frontend_ops->tuner_ops;
-	struct tuner_state	t_state;
+	struct dvb_frontend_ops *frontend_ops = &fe->ops;
+	struct dvb_tuner_ops *tuner_ops = &frontend_ops->tuner_ops;
+	struct tuner_state t_state;
 	int err = 0;
 
 	if (&fe->ops)
@@ -40,7 +43,9 @@ static unsigned int mt2063_lockStatus(struct dvb_frontend *fe)
 	if (&frontend_ops->tuner_ops)
 		tuner_ops = &frontend_ops->tuner_ops;
 	if (tuner_ops->get_state) {
-		if ((err = tuner_ops->get_state(fe, DVBFE_TUNER_REFCLOCK, &t_state)) < 0) {
+		if ((err =
+		     tuner_ops->get_state(fe, DVBFE_TUNER_REFCLOCK,
+					  &t_state)) < 0) {
 			printk("%s: Invalid parameter\n", __func__);
 			return err;
 		}
@@ -50,9 +55,9 @@ static unsigned int mt2063_lockStatus(struct dvb_frontend *fe)
 
 static unsigned int tuner_MT2063_Open(struct dvb_frontend *fe)
 {
-	struct dvb_frontend_ops	*frontend_ops = &fe->ops;
-	struct dvb_tuner_ops	*tuner_ops = &frontend_ops->tuner_ops;
-	struct tuner_state	t_state;
+	struct dvb_frontend_ops *frontend_ops = &fe->ops;
+	struct dvb_tuner_ops *tuner_ops = &frontend_ops->tuner_ops;
+	struct tuner_state t_state;
 	int err = 0;
 
 	if (&fe->ops)
@@ -60,20 +65,22 @@ static unsigned int tuner_MT2063_Open(struct dvb_frontend *fe)
 	if (&frontend_ops->tuner_ops)
 		tuner_ops = &frontend_ops->tuner_ops;
 	if (tuner_ops->set_state) {
-		if ((err = tuner_ops->set_state(fe, DVBFE_TUNER_OPEN, &t_state)) < 0) {
+		if ((err =
+		     tuner_ops->set_state(fe, DVBFE_TUNER_OPEN,
+					  &t_state)) < 0) {
 			printk("%s: Invalid parameter\n", __func__);
 			return err;
 		}
 	}
-	
+
 	return err;
 }
 
 static unsigned int tuner_MT2063_SoftwareShutdown(struct dvb_frontend *fe)
 {
-	struct dvb_frontend_ops	*frontend_ops = &fe->ops;
-	struct dvb_tuner_ops	*tuner_ops = &frontend_ops->tuner_ops;
-	struct tuner_state	t_state;
+	struct dvb_frontend_ops *frontend_ops = &fe->ops;
+	struct dvb_tuner_ops *tuner_ops = &frontend_ops->tuner_ops;
+	struct tuner_state t_state;
 	int err = 0;
 
 	if (&fe->ops)
@@ -81,20 +88,22 @@ static unsigned int tuner_MT2063_SoftwareShutdown(struct dvb_frontend *fe)
 	if (&frontend_ops->tuner_ops)
 		tuner_ops = &frontend_ops->tuner_ops;
 	if (tuner_ops->set_state) {
-		if ((err = tuner_ops->set_state(fe, DVBFE_TUNER_SOFTWARE_SHUTDOWN, &t_state)) < 0) {
+		if ((err =
+		     tuner_ops->set_state(fe, DVBFE_TUNER_SOFTWARE_SHUTDOWN,
+					  &t_state)) < 0) {
 			printk("%s: Invalid parameter\n", __func__);
 			return err;
 		}
 	}
-	
+
 	return err;
 }
 
 static unsigned int tuner_MT2063_ClearPowerMaskBits(struct dvb_frontend *fe)
 {
-	struct dvb_frontend_ops	*frontend_ops = &fe->ops;
-	struct dvb_tuner_ops	*tuner_ops = &frontend_ops->tuner_ops;
-	struct tuner_state	t_state;
+	struct dvb_frontend_ops *frontend_ops = &fe->ops;
+	struct dvb_tuner_ops *tuner_ops = &frontend_ops->tuner_ops;
+	struct tuner_state t_state;
 	int err = 0;
 
 	if (&fe->ops)
@@ -102,11 +111,13 @@ static unsigned int tuner_MT2063_ClearPowerMaskBits(struct dvb_frontend *fe)
 	if (&frontend_ops->tuner_ops)
 		tuner_ops = &frontend_ops->tuner_ops;
 	if (tuner_ops->set_state) {
-		if ((err = tuner_ops->set_state(fe, DVBFE_TUNER_CLEAR_POWER_MASKBITS, &t_state)) < 0) {
+		if ((err =
+		     tuner_ops->set_state(fe, DVBFE_TUNER_CLEAR_POWER_MASKBITS,
+					  &t_state)) < 0) {
 			printk("%s: Invalid parameter\n", __func__);
 			return err;
 		}
 	}
-	
+
 	return err;
 }
