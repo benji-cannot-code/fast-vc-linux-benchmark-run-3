604f5036ebfe6a740f8781e3215fdfd7a54b053e
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2011-07-20 00:05:21 +0000

    stmmac: update the version (V2)
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/stmmac/stmmac.h
+++ drivers/net/stmmac/stmmac.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
   Author: Giuseppe Cavallaro <peppe.cavallaro@st.com>
 *******************************************************************************/
 
-#define DRV_MODULE_VERSION	"Nov_2010"
+#define DRV_MODULE_VERSION	"July_2011"
 #include <linux/stmmac.h>
 
 #include "common.h"
