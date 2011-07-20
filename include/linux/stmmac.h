379bde4f3b0c5e937d71636c2e0a3aa5bb7a4dcb
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2011-07-20 00:05:23 +0000

    stmmac: unify MAC and PHY configuration parameters (V2)
    Prior to this change, most PHY configuration parameters were passed
    into the STMMAC device as a separate PHY device. As well as being
    unusual, this made it difficult to make changes to the MAC/PHY
    relationship.
    
    This patch moves all the PHY parameters into the MAC configuration
    structure, mainly as a separate structure. This allows us to completely
    ignore the MDIO bus attached to a stmmac if desired, and not create
    the PHY bus. It also allows the stmmac driver to use a different PHY
    from the one it is connected to, for example a fixed PHY or bit banging
    PHY.
    
    Also derive the stmmac/PHY connection type (MII/RMII etc) from the
    mode can be passed into <platf>_configure_ethernet.
    STLinux kernel at git://git.stlinux.com/stm/linux-sh4-2.6.32.y.git
    provides several examples how to use this new infrastructure (that
    actually is easier to maintain and clearer).
    
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/stmmac.h
+++ include/linux/stmmac.h
@@ -29,11 +29,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/platform_device.h>
 
-/* platform data for platform device structure's platform_data field */
+/* Platfrom data for platform device structure's platform_data field */
 
-/* Private data for the STM on-board ethernet driver */
+struct stmmac_mdio_bus_data {
+	int bus_id;
+	int (*phy_reset)(void *priv);
+	unsigned int phy_mask;
+	int *irqs;
+	int probed_phy_irq;
+};
+
 struct plat_stmmacenet_data {
 	int bus_id;
+	int phy_addr;
+	int interface;
+	struct stmmac_mdio_bus_data *mdio_bus_data;
 	int pbl;
 	int clk_csr;
 	int has_gmac;
@@ -49,14 +59,4 @@ struct plat_stmmacenet_data {
 	void *custom_cfg;
 	void *bsp_priv;
 };
-
-struct plat_stmmacphy_data {
-	int bus_id;
-	int phy_addr;
-	unsigned int phy_mask;
-	int interface;
-	int (*phy_reset)(void *priv);
-	void *priv;
-};
 #endif
-
