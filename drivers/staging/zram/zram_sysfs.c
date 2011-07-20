5967dbc982dc7befab7365ab904e7a61639565da
Noah Watkins <noahwatkins@gmail.com>
2011-07-20 17:06:08 -0600

    staging: zram: make global var "num_devices" use unique name
    The global variable "num_devices" is too general to be
    global. This patch switches the name to be "zram_num_devices".
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -35,7 +35,7 @@ static struct zram *dev_to_zram(struct device *dev)
 	int i;
 	struct zram *zram = NULL;
 
-	for (i = 0; i < num_devices; i++) {
+	for (i = 0; i < zram_num_devices; i++) {
 		zram = &zram_devices[i];
 		if (disk_to_dev(zram->disk) == dev)
 			break;
