2237189511967a87fcbaa2fbd77d901ccb331d72
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-07-20 22:56:53 +0100

    regmap: Provide register map dump via debugfs
    Copy over the read parts of the ASoC debugfs implementation into regmap,
    allowing users to see what the register values the device has are at
    runtime. The implementation, especially the support for seeking, is
    mostly due to Dimitris Papastamos' work in ASoC.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/base/regmap/regmap.c
+++ drivers/base/regmap/regmap.c
@@ -157,6 +157,8 @@ struct regmap *regmap_init(struct device *dev,
 		goto err_bus;
 	}
 
+	regmap_debugfs_init(map);
+
 	return map;
 
 err_bus:
@@ -173,6 +175,7 @@ EXPORT_SYMBOL_GPL(regmap_init);
  */
 void regmap_exit(struct regmap *map)
 {
+	regmap_debugfs_exit(map);
 	kfree(map->work_buf);
 	module_put(map->bus->owner);
 	kfree(map);
@@ -473,3 +476,11 @@ out:
 	return ret;
 }
 EXPORT_SYMBOL_GPL(regmap_update_bits);
+
+static int __init regmap_initcall(void)
+{
+	regmap_debugfs_initcall();
+
+	return 0;
+}
+postcore_initcall(regmap_initcall);
