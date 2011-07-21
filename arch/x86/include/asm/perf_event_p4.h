bc479ca3af52eef67d2372e7b2483079ff939b6a
Cyrill Gorcunov <gorcunov@gmail.com>
2011-07-21 20:06:25 +0400

    x86, perf: P4 PMU - Fix typos in comments and style cleanup
    This patch:
    
     - fixes typos in comments and clarifies the text
     - renames obscure p4_event_alias::original and ::alter members to
       ::original and ::alternative as appropriate
     - drops parenthesis from the return of p4_get_alias_event()
    
    No functional changes.
    
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Link: http://lkml.kernel.org/r/20110721160625.GX7492@sun
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/include/asm/perf_event_p4.h
+++ arch/x86/include/asm/perf_event_p4.h
@@ -134,7 +134,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * In case of event aliasing we need to preserve some
- * caller bits otherwise the mapping won't be complete.
+ * caller bits, otherwise the mapping won't be complete.
  */
 #define P4_CONFIG_EVENT_ALIAS_MASK			  \
 	(p4_config_pack_escr(P4_CONFIG_MASK_ESCR)	| \
