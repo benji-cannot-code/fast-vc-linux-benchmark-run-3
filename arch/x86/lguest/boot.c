85315770537adaf44451dbfd1ff52411b7d0778c
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-21 13:03:20 -0400

    lguest: add export.h to lguest files for THIS_MODULE/EXPORT_SYMBOL
    We need this in advance of the module.h cleanup, or we'll
    get compile errors like this:
    
      CC      drivers/lguest/lguest_device.o
    drivers/lguest/lguest_device.c: In function ‘lguest_devices_init’:
    drivers/lguest/lguest_device.c:490: error: ‘THIS_MODULE’ undeclared (first use in this function)
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/x86/lguest/boot.c
+++ arch/x86/lguest/boot.c
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/lguest_launcher.h>
 #include <linux/virtio_console.h>
 #include <linux/pm.h>
+#include <linux/export.h>
 #include <asm/apic.h>
 #include <asm/lguest.h>
 #include <asm/paravirt.h>
