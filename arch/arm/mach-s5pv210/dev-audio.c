09cc0097d3422c7db23ed00d99b865a05ff9b244
Sangbeom Kim <sbkim73@samsung.com>
2011-07-21 14:15:11 +0900

    ARM: SAMSUNG: Add IRQ_I2S0 definition
    To handle i2s0 interrupt and To fix compilation error
    adds IRQ_I2S0 for exynos4, s3c64xx, s5p64x0
    
    Signed-off-by: Sangbeom Kim <sbkim73@samsung.com>
    [kgene.kim@samsung.com: Fixed build failure due to inclusion]
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s5pv210/dev-audio.c
+++ arch/arm/mach-s5pv210/dev-audio.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/map.h>
 #include <mach/dma.h>
 #include <mach/irqs.h>
+#include <mach/regs-audss.h>
 
 static const char *rclksrc[] = {
 	[0] = "busclk",
