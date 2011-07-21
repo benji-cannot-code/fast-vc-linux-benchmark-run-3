cb1fdb517b65d03c763298b058bc6dfbe0d86d33
Shawn Guo <shawn.guo@linaro.org>
2011-07-21 22:24:10 +0800

    usb/ehci-mxc: add missing inclusion of mach/hardware.h
    As cpu_is_mx stuff is being used in the driver, header mach/hardware.h
    should be explicitly included.
    
    The missing of the header is causing today's linux-next build error
    as bleow.
    
      CC      drivers/usb/host/ehci-hcd.o
    In file included from linux-next/drivers/usb/host/ehci-hcd.c:1190:0:
    linux-next/drivers/usb/host/ehci-mxc.c: In function 'ehci_mxc_drv_probe':
    linux-next/drivers/usb/host/ehci-mxc.c:175:2: error: implicit declaration of function 'cpu_is_mx35'
    linux-next/drivers/usb/host/ehci-mxc.c:175:2: error: implicit declaration of function 'cpu_is_mx25'
    linux-next/drivers/usb/host/ehci-mxc.c:185:2: error: implicit declaration of function 'cpu_is_mx51'
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/ehci-mxc.c
+++ drivers/usb/host/ehci-mxc.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb/ulpi.h>
 #include <linux/slab.h>
 
+#include <mach/hardware.h>
 #include <mach/mxc_ehci.h>
 
 #include <asm/mach-types.h>
