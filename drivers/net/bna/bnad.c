46e16b582832cdf7b677de36469aeb55e1144bec
Rasesh Mody <rmody@brocade.com>
2011-07-22 08:07:40 +0000

    bna: Print Driver Version
    Change details:
     - Print the dirver version when module is loaded.
    
    Signed-off-by: Rasesh Mody <rmody@brocade.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bna/bnad.c
+++ drivers/net/bna/bnad.c
@@ -3233,7 +3233,8 @@ bnad_module_init(void)
 {
 	int err;
 
-	pr_info("Brocade 10G Ethernet driver\n");
+	pr_info("Brocade 10G Ethernet driver - version: %s\n",
+			BNAD_VERSION);
 
 	bfa_nw_ioc_auto_recover(bnad_ioc_auto_recover);
 
