d1b17cfe6422fa096193f06fad90b845336d2f20
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-22 13:52:47 -0400

    powerpc: cell/beat_wrapper.h is implicitly using memcpy functions
    This has been relying on the fact that the parent file would have
    module.h (and thus nearly everything) present.  But once we fix that,
    we'll get stuck with this failure:
    
    In file included from arch/powerpc/platforms/cell/beat_spu_priv1.c:26:
    arch/powerpc/platforms/cell/beat_wrapper.h: In function 'beat_eeprom_write':
    arch/powerpc/platforms/cell/beat_wrapper.h:160: error: implicit declaration of function 'memcpy'
    
    and many more instances of the same.  Fix it in advance.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/powerpc/platforms/cell/beat_wrapper.h
+++ arch/powerpc/platforms/cell/beat_wrapper.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #ifndef BEAT_HCALL
+#include <linux/string.h>
 #include "beat_syscall.h"
 
 /* defined in hvCall.S */
