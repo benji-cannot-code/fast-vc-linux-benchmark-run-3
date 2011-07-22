4e2decd1613b4227dc5a9019dfa20c6995c94385
Eliot Blennerhassett <eblennerhassett@audioscience.com>
2011-07-22 15:52:50 +1200

    ALSA: asihpi - Fix minor typos and spelling
    Signed-off-by: Eliot Blennerhassett <eblennerhassett@audioscience.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/asihpi/hpimsgx.c
+++ sound/pci/asihpi/hpimsgx.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-Extended Message Function With Response Cacheing
+Extended Message Function With Response Caching
 
 (C) Copyright AudioScience Inc. 2002
 *****************************************************************************/
