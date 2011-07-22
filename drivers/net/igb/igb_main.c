e14aa0583a56b386c8087e3c27dde3762bbcf048
Huang Weiyi <weiyi.huang@gmail.com>
2011-07-22 22:20:01 +0000

    igb: remove duplicated #include
    Remove duplicated #include('s) in
      drivers/net/igb/igb_main.c
    
    Signed-off-by: Huang Weiyi <weiyi.huang@gmail.com>
    Acked-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/igb/igb_main.c
+++ drivers/net/igb/igb_main.c
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_ether.h>
 #include <linux/aer.h>
 #include <linux/prefetch.h>
-#include <linux/if_vlan.h>
 #ifdef CONFIG_IGB_DCA
 #include <linux/dca.h>
 #endif
