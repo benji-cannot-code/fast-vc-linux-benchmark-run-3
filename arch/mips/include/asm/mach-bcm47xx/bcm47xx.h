08b9b67c24b42d66b5b7982226094e94956849e6
Hauke Mehrtens <hauke@hauke-m.de>
2011-07-23 01:20:12 +0200

    bcm47xx: prepare to support different buses
    Prepare bcm47xx to support different System buses. Before adding
    support for bcma it should be possible to build bcm47xx without the
    need of ssb. With this patch bcm47xx does not directly contain a
    ssb_bus, but a union contain all the supported system buses. As a SoC
    just uses one system bus a union is a good choice.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- arch/mips/include/asm/mach-bcm47xx/bcm47xx.h
+++ arch/mips/include/asm/mach-bcm47xx/bcm47xx.h
@@ -20,7 +20,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_BCM47XX_H
 #define __ASM_BCM47XX_H
 
-/* SSB bus */
-extern struct ssb_bus ssb_bcm47xx;
+#include <linux/ssb/ssb.h>
+
+enum bcm47xx_bus_type {
+	BCM47XX_BUS_TYPE_SSB,
+};
+
+union bcm47xx_bus {
+	struct ssb_bus ssb;
+};
+
+extern union bcm47xx_bus bcm47xx_bus;
+extern enum bcm47xx_bus_type bcm47xx_bus_type;
 
 #endif /* __ASM_BCM47XX_H */
