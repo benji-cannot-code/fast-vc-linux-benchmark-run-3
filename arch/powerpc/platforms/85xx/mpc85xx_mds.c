83721aeef22050227bc39bea9f719e9be82ad065
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-22 14:24:04 -0400

    powerpc: remove non-required uses of include <linux/module.h>
    None of the files touched here are modules, and they are not
    exporting any symbols either -- so there is no need to be including
    the module.h.  Builds of all the files remains successful.
    
    Even kernel/module.c does not need to include it, since it includes
    linux/moduleloader.h instead.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/powerpc/platforms/85xx/mpc85xx_mds.c
+++ arch/powerpc/platforms/85xx/mpc85xx_mds.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/seq_file.h>
 #include <linux/initrd.h>
-#include <linux/module.h>
 #include <linux/fsl_devices.h>
 #include <linux/of_platform.h>
 #include <linux/of_device.h>
