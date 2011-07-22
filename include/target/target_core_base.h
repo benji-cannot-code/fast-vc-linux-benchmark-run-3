32fb06cad49924ac2531821611d4fafca1eea209
Nicholas Bellinger <nab@linux-iscsi.org>
2011-07-22 08:24:22 +0000

    target: Bump version to v4.1.0-rc1-ml
    This patch bumps the target core version to v4.1.0-rc1 now that we are
    in sync with upstream lio-core-2.6.git/master
    
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- include/target/target_core_base.h
+++ include/target/target_core_base.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <net/tcp.h>
 
-#define TARGET_CORE_MOD_VERSION		"v4.0.0-rc7-ml"
+#define TARGET_CORE_MOD_VERSION		"v4.1.0-rc1-ml"
 #define SHUTDOWN_SIGS	(sigmask(SIGKILL)|sigmask(SIGINT)|sigmask(SIGABRT))
 
 /* Used by transport_generic_allocate_iovecs() */
