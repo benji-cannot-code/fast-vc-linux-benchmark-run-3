e46b4b5d1e164927ce631a8f0f3f328f6cfba122
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-23 16:55:17 -0400

    mips: remove needless include of module.h from core kernel files.
    None of these files are using modular infrastructure, and build
    tests reveal that none of these files are really relying on any
    implicit inclusions via. module.h either.  So delete them.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/mips/kernel/8250-platform.c
+++ arch/mips/kernel/8250-platform.c
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2007 Ralf Baechle (ralf@linux-mips.org)
  */
-#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/serial_8250.h>
 
--- arch/mips/kernel/unaligned.c
+++ arch/mips/kernel/unaligned.c
@@ -74,7 +74,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *       Undo the partial store in this case.
  */
 #include <linux/mm.h>
-#include <linux/module.h>
 #include <linux/signal.h>
 #include <linux/smp.h>
 #include <linux/sched.h>
