bcba8cec424835b00db08066e8108887b15bb539
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-07-22 21:22:29 -0300

    [media] mt2063: don't crash if device is not initialized
    Instead of crash, return -ENODEV, if the device is not poperly
    initialized.
    
    Also, give a second chance for it to initialize, at set_params
    calls.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/tuners/mt2063.c
+++ drivers/media/common/tuners/mt2063.c
@@ -221,6 +221,8 @@ enum MT2063_Register_Offsets {
 struct mt2063_state {
 	struct i2c_adapter *i2c;
 
+	bool init;
+
 	const struct mt2063_config *config;
 	struct dvb_tuner_ops ops;
 	struct dvb_frontend *frontend;
@@ -1975,6 +1977,8 @@ static int mt2063_init(struct dvb_frontend *fe)
 	if (status < 0)
 		return status;
 
+	state->init = true;
+
 	return 0;
 }
 
@@ -1984,6 +1988,9 @@ static int mt2063_get_status(struct dvb_frontend *fe, u32 *tuner_status)
 	int status;
 
 	dprintk(2, "\n");
+
+	if (!state->init)
+		return -ENODEV;
 
 	*tuner_status = 0;
 	status = mt2063_lockStatus(state);
@@ -2019,6 +2026,12 @@ static int mt2063_set_analog_params(struct dvb_frontend *fe,
 	int status;
 
 	dprintk(2, "\n");
+
+	if (!state->init) {
+		status = mt2063_init(fe);
+		if (status < 0)
+			return status;
+	}
 
 	switch (params->mode) {
 	case V4L2_TUNER_RADIO:
@@ -2083,6 +2096,12 @@ static int mt2063_set_params(struct dvb_frontend *fe)
 	s32 if_mid;
 	s32 rcvr_mode;
 
+	if (!state->init) {
+		status = mt2063_init(fe);
+		if (status < 0)
+			return status;
+	}
+
 	dprintk(2, "\n");
 
 	if (c->bandwidth_hz == 0)
@@ -2133,6 +2152,9 @@ static int mt2063_get_frequency(struct dvb_frontend *fe, u32 *freq)
 
 	dprintk(2, "\n");
 
+	if (!state->init)
+		return -ENODEV;
+
 	*freq = state->frequency;
 	return 0;
 }
@@ -2142,6 +2164,9 @@ static int mt2063_get_bandwidth(struct dvb_frontend *fe, u32 *bw)
 	struct mt2063_state *state = fe->tuner_priv;
 
 	dprintk(2, "\n");
+
+	if (!state->init)
+		return -ENODEV;
 
 	*bw = state->AS_Data.f_out_bw - 750000;
 	return 0;
