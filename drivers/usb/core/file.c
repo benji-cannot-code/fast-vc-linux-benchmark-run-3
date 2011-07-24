f0997251ae124f43cbbed7a8d6049735cc1cf594
Al Viro <viro@zeniv.linux.org.uk>
2011-07-23 20:24:48 -0400

    switch device_get_devnode() and ->devnode() to umode_t *
    both callers of device_get_devnode() are only interested in lower 16bits
    and nobody tries to return anything wider than 16bit anyway.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/usb/core/file.c
+++ drivers/usb/core/file.c
@@ -67,7 +67,7 @@ static struct usb_class {
 	struct class *class;
 } *usb_class;
 
-static char *usb_devnode(struct device *dev, mode_t *mode)
+static char *usb_devnode(struct device *dev, umode_t *mode)
 {
 	struct usb_class_driver *drv;
 
