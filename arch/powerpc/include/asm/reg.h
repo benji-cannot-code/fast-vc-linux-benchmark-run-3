cc1c668947d76433978ae1f6ce4d6b71cda978e4
Scott Wood <scottwood@freescale.com>
2011-07-25 11:02:11 +0000

    powerpc: mtspr/mtmsr should take an unsigned long
    Add a cast in case the caller passes in a different type, as it would
    if mtspr/mtmsr were functions.
    
    Previously, if a 64-bit type was passed in on 32-bit, GCC would bind the
    constraint to a pair of registers, and would substitute the first register
    in the pair in the asm code.  This corresponds to the upper half of the
    64-bit register, which is generally not the desired behavior.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -1025,13 +1025,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define mtmsrd(v)	__mtmsrd((v), 0)
 #define mtmsr(v)	mtmsrd(v)
 #else
-#define mtmsr(v)	asm volatile("mtmsr %0" : : "r" (v) : "memory")
+#define mtmsr(v)	asm volatile("mtmsr %0" : \
+				     : "r" ((unsigned long)(v)) \
+				     : "memory")
 #endif
 
 #define mfspr(rn)	({unsigned long rval; \
 			asm volatile("mfspr %0," __stringify(rn) \
 				: "=r" (rval)); rval;})
-#define mtspr(rn, v)	asm volatile("mtspr " __stringify(rn) ",%0" : : "r" (v)\
+#define mtspr(rn, v)	asm volatile("mtspr " __stringify(rn) ",%0" : \
+				     : "r" ((unsigned long)(v)) \
 				     : "memory")
 
 #ifdef __powerpc64__
