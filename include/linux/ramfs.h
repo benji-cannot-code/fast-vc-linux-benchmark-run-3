188226970787ae9cd3da4f9596c3c2716b458b52
Al Viro <viro@zeniv.linux.org.uk>
2011-07-26 03:16:55 -0400

    pohmelfs: propagate umode_t
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/ramfs.h
+++ include/linux/ramfs.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_RAMFS_H
 
 struct inode *ramfs_get_inode(struct super_block *sb, const struct inode *dir,
-	 int mode, dev_t dev);
+	 umode_t mode, dev_t dev);
 extern struct dentry *ramfs_mount(struct file_system_type *fs_type,
 	 int flags, const char *dev_name, void *data);
 
