853cb16c9f25e54a53f792141ccea8dd6b0ab7fe
Daniel Morsing <daniel.morsing@gmail.com>
2011-07-27 13:53:28 +0200

    staging: brcm80211: Fix double include introduced by bad merge
    A merge with Linus' tree added a double include of linux/interrupt.h.
    Fix by removing one of the includes.
    
    Signed-off-by: Daniel Morsing <daniel.morsing@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/staging/brcm80211/brcmsmac/mac80211_if.h
+++ drivers/staging/brcm80211/brcmsmac/mac80211_if.h
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BRCMS_SET_SHORTSLOT_OVERRIDE		146
 
 
-#include <linux/interrupt.h>
-
 /* BMAC Note: High-only driver is no longer working in softirq context as it needs to block and
  * sleep so perimeter lock has to be a semaphore instead of spinlock. This requires timers to be
  * submitted to workqueue instead of being on kernel timer
