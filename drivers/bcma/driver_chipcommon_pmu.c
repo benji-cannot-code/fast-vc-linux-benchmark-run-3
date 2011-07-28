74509220e1b2ec5c5f75b45f50f31a003038d89e
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-27 21:21:04 -0400

    bcma: fix implicit use of export.h contents
    Fix in advance, or we will get things like this:
    
    drivers/bcma/core.c:20: warning: data definition has no type or storage class
    drivers/bcma/core.c:20: warning: type defaults to 'int' in declaration of 'EXPORT_SYMBOL_GPL'
    drivers/bcma/core.c:20: warning: parameter names (without types) in function declaration
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/bcma/driver_chipcommon_pmu.c
+++ drivers/bcma/driver_chipcommon_pmu.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "bcma_private.h"
+#include <linux/export.h>
 #include <linux/bcma/bcma.h>
 
 static u32 bcma_chipco_pll_read(struct bcma_drv_cc *cc, u32 offset)
