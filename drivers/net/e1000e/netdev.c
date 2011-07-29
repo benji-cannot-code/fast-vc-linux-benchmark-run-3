7880d8b153a703a65a02f1546f99bba3a9a73c72
Bruce Allan <bruce.w.allan@intel.com>
2011-07-29 05:52:51 +0000

    e1000e: minor re-order of #include files
    The recent commit a6b7a407 when back-ported to the out-of-tree e1000e
    driver caused a compilation error on older kernels which required a
    re-ordering of the #include files.  This cosmetic patch syncs the two
    drivers for easier maintainability.
    
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Tested-by:  Aaron Brown  <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/e1000e/netdev.c
+++ drivers/net/e1000e/netdev.c
@@ -32,12 +32,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/init.h>
-#include <linux/interrupt.h>
 #include <linux/pci.h>
 #include <linux/vmalloc.h>
 #include <linux/pagemap.h>
 #include <linux/delay.h>
 #include <linux/netdevice.h>
+#include <linux/interrupt.h>
 #include <linux/tcp.h>
 #include <linux/ipv6.h>
 #include <linux/slab.h>
