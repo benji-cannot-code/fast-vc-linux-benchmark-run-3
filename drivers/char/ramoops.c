f654c6a08b384e317ffb94e278dfa1998c7f1964
James Bottomley <James.Bottomley@HansenPartnership.com>
2011-07-29 17:11:32 +0400

    ramoops: fix compile failure on parisc
    Fixes this:
    
      drivers/char/ramoops.c: In function 'ramoops_init':
      drivers/char/ramoops.c:221: error: implicit declaration of function 'IS_ERR'
      drivers/char/ramoops.c:222: error: implicit declaration of function 'PTR_ERR'
    
    If it actually builds on other platforms, it's probably getting
    linux/err.h via some other #include.
    
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/ramoops.c
+++ drivers/char/ramoops.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/kernel.h>
+#include <linux/err.h>
 #include <linux/module.h>
 #include <linux/kmsg_dump.h>
 #include <linux/time.h>
