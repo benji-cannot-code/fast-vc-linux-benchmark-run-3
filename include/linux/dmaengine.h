89751bbef2166e7caaebf4f3591939aa5b5529e3
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-29 16:55:11 +1000

    linux/dmaengine.h: fix implicit use of bitmap.h and asm/page.h
    The implicit presence of module.h and all its sub-includes was
    masking these implicit header usages:
    
    include/linux/dmaengine.h:684: warning: 'struct page' declared inside parameter list
    include/linux/dmaengine.h:684: warning: its scope is only this definition or declaration, which is probably not what you want
    include/linux/dmaengine.h:687: warning: 'struct page' declared inside parameter list
    include/linux/dmaengine.h:736:2: error: implicit declaration of function 'bitmap_zero'
    
    With input from Stephen Rothwell <sfr@canb.auug.org.au>
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/linux/dmaengine.h
+++ include/linux/dmaengine.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/uio.h>
 #include <linux/dma-direction.h>
+#include <linux/bitmap.h>
+#include <asm/page.h>
 
 struct scatterlist;
 
