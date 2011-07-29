d16902233c8f76908ce6f8fff9239c5681580754
Anirban Chakraborty <anirban.chakraborty@qlogic.com>
2011-07-29 13:30:26 +0000

    qlcnic: FW dump related changes
    o Added code to support FW reset without invoking the dump
    o Fixed the return value of the dump data size if dump is not available.
    
    Signed-off-by: Anirban Chakraborty <anirban.chakraborty@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/qlcnic/qlcnic.h
+++ drivers/net/qlcnic/qlcnic.h
@@ -1345,6 +1345,7 @@ enum op_codes {
 #define QLCNIC_FORCE_FW_DUMP_KEY	0xdeadfeed
 #define QLCNIC_ENABLE_FW_DUMP		0xaddfeed
 #define QLCNIC_DISABLE_FW_DUMP		0xbadfeed
+#define QLCNIC_FORCE_FW_RESET		0xdeaddead
 
 struct qlcnic_dump_operations {
 	enum op_codes opcode;
