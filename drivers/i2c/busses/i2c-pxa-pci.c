4b4179ef7b1275d0d94c4c19d0700dbf7e9d5462
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-29 21:14:30 -0700

    i2c: Add module.h to modular files prev. implicitly getting it
    These files use interfaces from linux/module.h, so they must
    include that file to avoid build errors when the implicit
    presence of module.h is removed.
    
    [with i2c-pxa-pci.c fix from Randy Dunlap <rdunlap@xenotime.net>]
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/i2c/busses/i2c-pxa-pci.c
+++ drivers/i2c/busses/i2c-pxa-pci.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * It does not support slave mode, the register slightly moved. This PCI
  * device provides three bars, every contains a single I2C controller.
  */
+#include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/platform_device.h>
 #include <linux/i2c/pxa-i2c.h>
