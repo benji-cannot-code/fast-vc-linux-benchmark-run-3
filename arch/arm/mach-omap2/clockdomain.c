acaa6cf75a1a4c9cca93c5b8bcdfcb6e8e998698
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-31 10:52:44 -0400

    arm: fix implicit memset/string.h usage in various arch/arm files
    To fix things like this:
    
    arch/arm/mach-omap2/usb-tusb6010.c:58: error: implicit declaration of function 'memset'
    arch/arm/kernel/leds.c:40: error: implicit declaration of function 'strcspn'
    arch/arm/kernel/leds.c:40: warning: incompatible implicit declaration of built-in function 'strcspn'
    arch/arm/kernel/leds.c:45: error: implicit declaration of function 'strncmp'
    arch/arm/kernel/leds.c:55: error: implicit declaration of function 'strlen'
    arch/arm/kernel/leds.c:55: warning: incompatible implicit declaration of built-in function 'strlen'
    arch/arm/mach-omap2/clockdomain.c:52: error: implicit declaration of function 'strcmp'
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/arm/mach-omap2/clockdomain.c
+++ arch/arm/mach-omap2/clockdomain.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/list.h>
 #include <linux/errno.h>
+#include <linux/string.h>
 #include <linux/delay.h>
 #include <linux/clk.h>
 #include <linux/limits.h>
