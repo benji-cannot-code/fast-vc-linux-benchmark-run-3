75fe534c5809b2043f5348e1806693ed43aa26fb
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-31 18:33:21 -0400

    ia64: Add export.h to arch/ia64 specific files as required
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/ia64/pci/pci.c
+++ arch/ia64/pci/pci.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/bootmem.h>
+#include <linux/export.h>
 
 #include <asm/machvec.h>
 #include <asm/page.h>
