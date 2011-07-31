a1a00ac9d8cd62f5c38a94d13f9dcd912ed32399
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-07-31 16:14:14 -0400

    arm: fix implicit module.h users by adding it to arch/arm as required.
    These files will fail to compile if we dont clean them up in advance
    and have them include the appropriate headers they need.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/arm/mach-pxa/corgi.c
+++ arch/arm/mach-pxa/corgi.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spi/pxa2xx_spi.h>
 #include <linux/mtd/sharpsl.h>
 #include <linux/input/matrix_keypad.h>
+#include <linux/module.h>
 #include <video/w100fb.h>
 
 #include <asm/setup.h>
