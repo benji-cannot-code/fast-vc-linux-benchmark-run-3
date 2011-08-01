44623fce3cad8cb0ba84a7ff6536985db90d4a9a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-08-01 13:02:17 +0900

    ASoC: Implement new DC servo readback mode for late WM8994 revisions
    Later WM8994 devices implement a new DC servo readback mode with the
    register used to access the offset moved to register 0x59. Implement
    support for this and enable it on the appropriate devices.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Liam Girdwood <lrg@ti.com>

--- include/linux/mfd/wm8994/registers.h
+++ include/linux/mfd/wm8994/registers.h
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WM8994_DC_SERVO_2                       0x55
 #define WM8994_DC_SERVO_4                       0x57
 #define WM8994_DC_SERVO_READBACK                0x58
+#define WM8994_DC_SERVO_4E			0x59
 #define WM8994_ANALOGUE_HP_1                    0x60
 #define WM8958_MIC_DETECT_1                     0xD0
 #define WM8958_MIC_DETECT_2                     0xD1
