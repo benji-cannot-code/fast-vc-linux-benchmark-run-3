5592bc73513c60cc2f3fb657827f4f410a0cc7c9
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-08-01 13:42:48 -0400

    sparc: fix implicit use of spitfire.h in pcr.c and of_device_64.c
    To resolve these on 64bit allnoconfig builds:
    
      CC      arch/sparc/kernel/pcr.o
    arch/sparc/kernel/pcr.c: In function 'register_perf_hsvc':
    arch/sparc/kernel/pcr.c:102: error: 'tlb_type' undeclared (first use in this function)
    
      CC      arch/sparc/kernel/of_device_64.o
    arch/sparc/kernel/of_device_64.c: In function 'build_device_resources':
    arch/sparc/kernel/of_device_64.c:406: error: 'tlb_type' undeclared (first use in this function)
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/sparc/kernel/of_device_64.c
+++ arch/sparc/kernel/of_device_64.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/of_device.h>
 #include <linux/of_platform.h>
+#include <asm/spitfire.h>
 
 #include "of_device_common.h"
 
--- arch/sparc/kernel/pcr.c
+++ arch/sparc/kernel/pcr.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pil.h>
 #include <asm/pcr.h>
 #include <asm/nmi.h>
+#include <asm/spitfire.h>
 
 /* This code is shared between various users of the performance
  * counters.  Users will be oprofile, pseudo-NMI watchdog, and the
