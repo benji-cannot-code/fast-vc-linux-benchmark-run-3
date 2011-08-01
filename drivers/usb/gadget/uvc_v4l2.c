3f0c6168e6fee1fcf44a656cce4e8b8d588e8682
Jesper Juhl <jj@chaosbits.net>
2011-08-01 23:33:29 +0200

    Remove unneeded version.h includes (and add where needed) for drivers/usb/
    It was pointed out by 'make versioncheck' that linux/version.h was not
    always being included where needed and sometimes included needlessly
    in drivers/usb/.
    This patch fixes up the includes.
    
    For the UVC gadget driver bits, this was ACK'ed by Laurent Pinchart.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/usb/gadget/uvc_v4l2.c
+++ drivers/usb/gadget/uvc_v4l2.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/list.h>
 #include <linux/mutex.h>
-#include <linux/version.h>
 #include <linux/videodev2.h>
 #include <linux/vmalloc.h>
 #include <linux/wait.h>
