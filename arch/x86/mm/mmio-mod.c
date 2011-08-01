cdcd5c5993090b726f7e6a1732a214e15dfbfb8d
Jesper Juhl <jj@chaosbits.net>
2011-08-01 23:07:51 +0200

    Remove unneeded version.h include from arch/x86/
    It was pointed out by 'make versioncheck' that the include of
    linux/version.h is not needed in arch/x86/mm/mmio-mod.c .
    This patch removes it.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- arch/x86/mm/mmio-mod.c
+++ arch/x86/mm/mmio-mod.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/io.h>
-#include <linux/version.h>
 #include <linux/kallsyms.h>
 #include <asm/pgtable.h>
 #include <linux/mmiotrace.h>
