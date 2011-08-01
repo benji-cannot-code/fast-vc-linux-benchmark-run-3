5eba5d1fce421d8a864a35facce43a06cd76e9d6
Jesper Juhl <jj@chaosbits.net>
2011-08-01 23:16:06 +0200

    Remove unneeded version.h includes from drivers/block/
    It was pointed out by 'make versioncheck' that some includes of
    linux/version.h are not needed in drivers/block/.
    This patch removes them.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/block/drbd/drbd_int.h
+++ drivers/block/drbd/drbd_int.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/compiler.h>
 #include <linux/types.h>
-#include <linux/version.h>
 #include <linux/list.h>
 #include <linux/sched.h>
 #include <linux/bitops.h>
