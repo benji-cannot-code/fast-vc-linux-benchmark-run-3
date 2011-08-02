2aa1be61e33a33f7444b698b2d616d268c21f588
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-08-02 12:22:48 -0400

    ARM: mach-clps711x: move special idle code out of line
    ... and hook it to arm_pm_idle.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/mach-clps711x/include/mach/system.h
+++ arch/arm/mach-clps711x/include/mach/system.h
@@ -27,10 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline void arch_idle(void)
 {
-	clps_writel(1, HALT);
-	__asm__ __volatile__(
-	"mov	r0, r0\n\
-	mov	r0, r0");
+	cpu_do_idle();
 }
 
 #endif
