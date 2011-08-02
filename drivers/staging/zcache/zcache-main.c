e25cf5d06b8623c8c6526f81726fd701682eba2c
Thadeu Lima de Souza Cascardo <cascardo@holoscopio.com>
2011-08-02 14:20:27 -0300

    staging: zcache: module is GPL
    This avoids tainting the kernel as if a proprietary module was loaded.
    The kernel will still be tainted because this is a staging driver.
    
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@holoscopio.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -54,6 +54,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_CLIENTS 16
 #define LOCAL_CLIENT ((uint16_t)-1)
+
+MODULE_LICENSE("GPL");
+
 struct zcache_client {
 	struct tmem_pool *tmem_pools[MAX_POOLS_PER_CLIENT];
 	struct xv_pool *xvpool;
