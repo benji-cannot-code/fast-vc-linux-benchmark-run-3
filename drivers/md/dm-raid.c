cfdf45143077aea05d20a32864bfa16942beb915
Alasdair G Kergon <agk@redhat.com>
2011-08-02 12:32:03 +0100

    dm raid: tidy includes
    A dm target only needs to use include/linux dm headers.
    
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-raid.c
+++ drivers/md/dm-raid.c
@@ -10,8 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "md.h"
 #include "raid5.h"
-#include "dm.h"
 #include "bitmap.h"
+
+#include <linux/device-mapper.h>
 
 #define DM_MSG_PREFIX "raid"
 
