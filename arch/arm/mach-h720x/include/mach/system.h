2bf893ccdf83c144f210a0bec6b6e4c34bf841f1
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-08-03 06:55:31 -0400

    ARM: mach-h720x: move special idle code out of line
    ... and hook it to arm_pm_idle.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/mach-h720x/include/mach/system.h
+++ arch/arm/mach-h720x/include/mach/system.h
@@ -17,12 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void arch_idle(void)
 {
-	CPU_REG (PMU_BASE, PMU_MODE) = PMU_MODE_IDLE;
-	nop();
-	nop();
-	CPU_REG (PMU_BASE, PMU_MODE) = PMU_MODE_RUN;
-	nop();
-	nop();
+	cpu_do_idle();
 }
 
 #endif
