bb5d0c554de74ce0be69a11f15171b58f4c2140b
Jon Medhurst <tixy@yxit.co.uk>
2011-08-04 10:46:46 +0100

    ARM: mach-s5p64x0: Setup consistent dma size at boot time
    Signed-off-by: Jon Medhurst <tixy@yxit.co.uk>
    CC: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s5p64x0/include/mach/memory.h
+++ arch/arm/mach-s5p64x0/include/mach/memory.h
@@ -15,6 +15,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARCH_MEMORY_H __FILE__
 
 #define PLAT_PHYS_OFFSET		UL(0x20000000)
-#define CONSISTENT_DMA_SIZE	SZ_8M
 
 #endif /* __ASM_ARCH_MEMORY_H */
