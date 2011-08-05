5223c1b1763ea4caf47d670f84ece9d6096c318e
Peter Hurley <peter@hurleysoftware.com>
2011-08-05 10:51:50 -0400

    Bluetooth: hidp: Only free input device if failed register
    When an hidp connection is added for a boot protocol input
    device, only free the allocated device if device registration fails.
    Subsequent failures should only unregister the device (the input
    device api documents that unregister will also free the allocated
    device).
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -843,6 +843,8 @@ static int hidp_setup_input(struct hidp_session *session,
 
 	err = input_register_device(input);
 	if (err < 0) {
+		input_free_device(input);
+		session->input = NULL;
 		hci_conn_put_device(session->conn);
 		return err;
 	}
@@ -1090,7 +1092,6 @@ purge:
 failed:
 	up_write(&hidp_session_sem);
 
-	input_free_device(session->input);
 	kfree(session);
 	return err;
 }
