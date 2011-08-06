562a74b378776ee78ded81de171006dabcc075a3
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2011-08-06 23:35:25 +0000

    viafb: eliminate viafb_get_pixclock
    As there is only one user we can just insert the formula where
    needed. Avoids one lookup in the modetable and
    viafb_fill_var_timing_info does no longer depend on the modetable.
    
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>

--- drivers/video/via/share.h
+++ drivers/video/via/share.h
@@ -637,10 +637,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define V_SYNC_SATRT_SHADOW_INDEX   18
 #define V_SYNC_END_SHADOW_INDEX     19
 
-/* Definition Video Mode Pixel Clock (picoseconds)
-*/
-#define RES_640X480_60HZ_PIXCLOCK    39722
-
 /* LCD display method
 */
 #define     LCD_EXPANDSION              0x00
