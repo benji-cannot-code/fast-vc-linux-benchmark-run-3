430a0a31d6f8aeea2ddb66b90f24f96131e4a5f2
Roland Vossen <rvossen@broadcom.com>
2011-08-08 15:58:22 +0200

    staging: brcm80211: fixed build issue for big endian platforms
    Driver now builds for big endian mips platform, possibly also for other
    big endian platforms. A change was made to the R_REG and W_REG macro's.
    These macro's perform an xor (^) operation for endianess swap purposes.
    Gcc complained because an xor operation is not allowed on a pointer type.
    Fixed this by casting the pointer to an unsigned long.
    
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/dma.h
+++ drivers/staging/brcm80211/brcmsmac/dma.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	_BRCM_DMA_H_
 #define	_BRCM_DMA_H_
 
+#include <linux/delay.h>
 #include "types.h"		/* forward structure declarations */
 
 /* DMA structure:
