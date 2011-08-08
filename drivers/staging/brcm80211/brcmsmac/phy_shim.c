211b5ab11b0ebaa910d395dc0a86fc9de6cf248c
Roland Vossen <rvossen@broadcom.com>
2011-08-08 15:57:47 +0200

    staging: brcm80211: merged bmac.c into main.c
    Future goal is to merge the bmac layer in the driver with the 'common' layer.
    A step towards this goal is to have one file containing both bmac and common
    code. Header files (bmac.h, main.h) were also merged.
    
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/phy_shim.c
+++ drivers/staging/brcm80211/brcmsmac/phy_shim.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <net/mac80211.h>
 
-#include "bmac.h"
 #include "main.h"
 #include "mac80211_if.h"
 #include "phy_shim.h"
