3eb46c903d7e05895454099452f0dc88c8d78580
John Stultz <john.stultz@linaro.org>
2011-08-10 10:37:59 -0700

    alarmtimers: Change alarmtimer functions to return alarmtimer_restart values
    In order to properly fix the denial of service issue with high freq
    periodic alarm timers, we need to push the re-arming logic into the
    alarm timer handler, much as the hrtimer code does.
    
    This patch introduces alarmtimer_restart enum and changes the
    alarmtimer handler declarations to use it as a return value. Further,
    to ease following changes, it extends the alarmtimer handler functions
    to also take the time at expiration. No logic is yet modified.
    
    CC: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- include/linux/alarmtimer.h
+++ include/linux/alarmtimer.h
@@ -14,6 +14,11 @@ enum alarmtimer_type {
 	ALARM_NUMTYPE,
 };
 
+enum alarmtimer_restart {
+	ALARMTIMER_NORESTART,
+	ALARMTIMER_RESTART,
+};
+
 /**
  * struct alarm - Alarm timer structure
  * @node:	timerqueue node for adding to the event list this value
@@ -27,14 +32,14 @@ enum alarmtimer_type {
 struct alarm {
 	struct timerqueue_node	node;
 	ktime_t			period;
-	void			(*function)(struct alarm *);
+	enum alarmtimer_restart	(*function)(struct alarm *, ktime_t now);
 	enum alarmtimer_type	type;
 	bool			enabled;
 	void			*data;
 };
 
 void alarm_init(struct alarm *alarm, enum alarmtimer_type type,
-		void (*function)(struct alarm *));
+		enum alarmtimer_restart (*function)(struct alarm *, ktime_t));
 void alarm_start(struct alarm *alarm, ktime_t start, ktime_t period);
 void alarm_cancel(struct alarm *alarm);
 
