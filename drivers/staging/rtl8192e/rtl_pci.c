9292392f426f65dd8a4cbf0fe19a1be5c14ed698
Larry Finger <Larry.Finger@lwfinger.net>
2011-08-11 18:16:43 -0500

    staging: rtl8192e: Cleanup checkpatch -f warnings and errors - Part XVII
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- drivers/staging/rtl8192e/rtl_pci.c
+++ drivers/staging/rtl8192e/rtl_pci.c
@@ -26,7 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rtl_pci.h"
 #include "rtl_core.h"
 
-static void rtl8192_parse_pci_configuration(struct pci_dev *pdev, struct net_device *dev)
+static void rtl8192_parse_pci_configuration(struct pci_dev *pdev,
+					    struct net_device *dev)
 {
 	struct r8192_priv *priv = (struct r8192_priv *)rtllib_priv(dev);
 
@@ -38,10 +39,11 @@ static void rtl8192_parse_pci_configuration(struct pci_dev *pdev, struct net_dev
 	pci_read_config_byte(priv->pdev, pos + PCI_EXP_LNKCTL, &LinkCtrlReg);
 	priv->NdisAdapter.LinkCtrlReg = LinkCtrlReg;
 
-	RT_TRACE(COMP_INIT, "Link Control Register =%x\n", priv->NdisAdapter.LinkCtrlReg);
+	RT_TRACE(COMP_INIT, "Link Control Register =%x\n",
+		 priv->NdisAdapter.LinkCtrlReg);
 
 	pci_read_config_byte(pdev, 0x98, &tmp);
-	tmp |=BIT4;
+	tmp |= BIT4;
 	pci_write_config_byte(pdev, 0x98, tmp);
 
 	tmp = 0x17;
@@ -66,24 +68,27 @@ bool rtl8192_pci_findadapter(struct pci_dev *pdev, struct net_device *dev)
 	if (DeviceID == 0x8172) {
 		switch (RevisionID) {
 		case HAL_HW_PCI_REVISION_ID_8192PCIE:
-			printk("Adapter(8192 PCI-E) is found - DeviceID=%x\n", DeviceID);
+			printk(KERN_INFO "Adapter(8192 PCI-E) is found - "
+			       "DeviceID=%x\n", DeviceID);
 			priv->card_8192 = NIC_8192E;
 			break;
 		case HAL_HW_PCI_REVISION_ID_8192SE:
-			printk("Adapter(8192SE) is found - DeviceID=%x\n", DeviceID);
+			printk(KERN_INFO "Adapter(8192SE) is found - "
+			       "DeviceID=%x\n", DeviceID);
 			priv->card_8192 = NIC_8192SE;
 			break;
 		default:
-			printk("UNKNOWN nic type(%4x:%4x)\n", pdev->vendor, pdev->device);
+			printk(KERN_INFO "UNKNOWN nic type(%4x:%4x)\n",
+			       pdev->vendor, pdev->device);
 			priv->card_8192 = NIC_UNKNOWN;
 			return false;
 		}
 	}
 
 	if (priv->ops->nic_type != priv->card_8192) {
-		printk("Detect info(%x) and hardware info(%x) not match!\n",
+		printk(KERN_INFO "Detect info(%x) and hardware info(%x) not match!\n",
 				priv->ops->nic_type, priv->card_8192);
-		printk("Please select proper driver before install!!!!\n");
+		printk(KERN_INFO "Please select proper driver before install!!!!\n");
 		return false;
 	}
 
