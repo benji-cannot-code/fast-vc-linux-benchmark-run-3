0a8c0f86d7d1d03e8e23a8755a1880ae8ef101a3
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-08-11 13:24:07 -0400

    parisc: add module.h to files really requiring it
    We want to clean up the implicit everywhere presence of module.h
    which means files like this that use module infrastructure need
    to explicitly call it out for inclusion.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/parisc/sba_iommu.c
+++ drivers/parisc/sba_iommu.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/module.h>
 
 #include <asm/ropes.h>
 #include <asm/mckinley.h>	/* for proc_mckinley_root */
