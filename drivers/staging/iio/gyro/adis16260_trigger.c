5932cd267617cfffdd4d7894df32ea35258dbcd4
Jonathan Cameron <jic23@cam.ac.uk>
2011-08-12 17:08:48 +0100

    staging:iio:gyro and meter: header housecleaning.
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/gyro/adis16260_trigger.c
+++ drivers/staging/iio/gyro/adis16260_trigger.c
@@ -1,15 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <linux/mutex.h>
-#include <linux/device.h>
 #include <linux/kernel.h>
-#include <linux/sysfs.h>
-#include <linux/list.h>
 #include <linux/spi/spi.h>
 
 #include "../iio.h"
-#include "../sysfs.h"
 #include "../trigger.h"
 #include "adis16260.h"
 
