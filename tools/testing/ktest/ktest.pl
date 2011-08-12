0930e2e038fe495e397f3a1395669bc8921bb86e
Andrew Jones <drjones@redhat.com>
2011-08-12 15:32:03 +0200

    ktest: Create outputdir if it does not exist
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Link: http://lkml.kernel.org/r/1313155932-20092-2-git-send-email-drjones@redhat.com
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -2851,9 +2851,11 @@ for (my $i = 1; $i <= $opt{"NUM_TESTS"}; $i++) {
 
     chdir $builddir || die "can't change directory to $builddir";
 
-    if (!-d $tmpdir) {
-	mkpath($tmpdir) or
-	    die "can't create $tmpdir";
+    foreach my $dir ($tmpdir, $outputdir) {
+	if (!-d $dir) {
+	    mkpath($dir) or
+		die "can't create $dir";
+	}
     }
 
     $ENV{"SSH_USER"} = $ssh_user;
