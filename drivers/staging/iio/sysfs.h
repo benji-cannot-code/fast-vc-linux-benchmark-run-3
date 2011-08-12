d123d3eaefd2be72bfa1fd90df0e45406a2cbc87
Jonathan Cameron <jic23@cam.ac.uk>
2011-08-12 16:55:31 +0100

    staging:iio: sysfs.h remove unused val2 and dead macro.
    Vivien pointed out that 99e5dc45b854b4b661044e807905152911ed3fdb
    removed the IIO_DEVICE_ATTR_2 macro but didn't clean up the other
    macros that used it.
    
    As it turns out, no one is using val2 in tree, so lets scrap that
    until it is needed.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Reported-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/sysfs.h
+++ drivers/staging/iio/sysfs.h
@@ -19,13 +19,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * struct iio_dev_attr - iio specific device attribute
  * @dev_attr:	underlying device attribute
  * @address:	associated register address
- * @val2:	secondary attribute value
  * @l:		list head for maintaining list of dynamically created attrs.
  */
 struct iio_dev_attr {
 	struct device_attribute dev_attr;
 	int address;
-	int val2;
 	struct list_head l;
 	struct iio_chan_spec const *c;
 };
@@ -64,10 +62,6 @@ struct iio_const_attr {
 #define IIO_DEVICE_ATTR_NAMED(_vname, _name, _mode, _show, _store, _addr) \
 	struct iio_dev_attr iio_dev_attr_##_vname			\
 	= IIO_ATTR(_name, _mode, _show, _store, _addr)
-
-#define IIO_DEVICE_ATTR_2(_name, _mode, _show, _store, _addr, _val2)	\
-	struct iio_dev_attr iio_dev_attr_##_name			\
-	= IIO_ATTR_2(_name, _mode, _show, _store, _addr, _val2)
 
 #define IIO_CONST_ATTR(_name, _string)					\
 	struct iio_const_attr iio_const_attr_##_name			\
