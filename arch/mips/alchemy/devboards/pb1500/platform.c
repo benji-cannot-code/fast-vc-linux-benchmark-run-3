0a68bd00d89361706f8a69459a0c86f116389dc4
Manuel Lauss <manuel.lauss@googlemail.com>
2011-08-12 11:39:45 +0200

    MIPS: Alchemy: remove all CONFIG_SOC_AU1??? defines
    Now that no driver any longer depends on the CONFIG_SOC_AU1???  symbols,
    it's time to get rid of them: Move some of the platform devices to the
    boards which can use them, Rename a few (unused) constants in the header,
    Replace them with MIPS_ALCHEMY in the various Kconfig files.  Finally
    delete them altogether from the Alchemy Kconfig file.
    
    Signed-off-by: Manuel Lauss <manuel.lauss@googlemail.com>
    To: Linux-MIPS <linux-mips@linux-mips.org>
    Patchwork: https://patchwork.linux-mips.org/patch/2707/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/alchemy/devboards/pb1500/platform.c
+++ arch/mips/alchemy/devboards/pb1500/platform.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <linux/dma-mapping.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <asm/mach-au1x00/au1000.h>
