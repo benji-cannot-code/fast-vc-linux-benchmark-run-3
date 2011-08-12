765fcd4933b95a5d2c98132bf4a5eab12651f26c
Jonathan Cameron <jic23@cam.ac.uk>
2011-08-12 17:08:39 +0100

    staging:iio:gyro:adis16260 remove some unused headers
    These should have gone with the iio_chan_spec conversion patch.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Acked-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/gyro/adis16260_core.c
+++ drivers/staging/iio/gyro/adis16260_core.c
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../iio.h"
 #include "../sysfs.h"
 #include "../ring_generic.h"
-#include "../adc/adc.h"
-#include "gyro.h"
 
 #include "adis16260.h"
 
