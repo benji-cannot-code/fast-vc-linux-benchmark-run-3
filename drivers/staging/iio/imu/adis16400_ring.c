6b47e618c24f6f11263f16254d15be4c719d39f2
Jonathan Cameron <jic23@cam.ac.uk>
2011-08-12 17:08:37 +0100

    staging:iio:imu:adis16400 remove now unused headers.
    These should really have gone away with the code that needed them.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Acked-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/imu/adis16400_ring.c
+++ drivers/staging/iio/imu/adis16400_ring.c
@@ -1,19 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <linux/gpio.h>
-#include <linux/workqueue.h>
 #include <linux/mutex.h>
-#include <linux/device.h>
 #include <linux/kernel.h>
 #include <linux/spi/spi.h>
 #include <linux/slab.h>
-#include <linux/sysfs.h>
-#include <linux/list.h>
 #include <linux/bitops.h>
 
 #include "../iio.h"
-#include "../sysfs.h"
 #include "../ring_sw.h"
 #include "../accel/accel.h"
 #include "../trigger.h"
