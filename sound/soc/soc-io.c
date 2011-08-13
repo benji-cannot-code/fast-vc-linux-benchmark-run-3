3cdf3c92ee642afd654b6fc41d783c2a099cbef4
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-08-13 11:50:48 +0900

    ASoC: Support !CONFIG_REGMAP builds
    Since we changed regmap to be selected and register per bus rather than
    via the core only we can't rely on it being enabled by the ASoC core.
    Support compiling it out.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Reported-by: Axel Lin <axel.lin@gmail.com>

--- sound/soc/soc-io.c
+++ sound/soc/soc-io.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <trace/events/asoc.h>
 
+#ifdef CONFIG_REGMAP
 static int hw_write(struct snd_soc_codec *codec, unsigned int reg,
 		    unsigned int value)
 {
@@ -151,4 +152,12 @@ int snd_soc_codec_set_cache_io(struct snd_soc_codec *codec,
 	return 0;
 }
 EXPORT_SYMBOL_GPL(snd_soc_codec_set_cache_io);
-
+#else
+int snd_soc_codec_set_cache_io(struct snd_soc_codec *codec,
+			       int addr_bits, int data_bits,
+			       enum snd_soc_control_type control)
+{
+	return -ENOTSUPP;
+}
+EXPORT_SYMBOL_GPL(snd_soc_codec_set_cache_io);
+#endif
