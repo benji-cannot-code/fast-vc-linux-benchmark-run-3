cc9ff7f818e94029729497c174239152d64bb75f
Stephen Rothwell <sfr@canb.auug.org.au>
2011-08-16 09:36:06 +1000

    regmap: using module facilities requires module.h
    Commit b33f9cbd67ba ("regmap: Specify a module license") added a
    MODULES_LICENSE to this file without adding an include of module.h.
    
    module.h should have been included anyway, since this file has
    EXPORT_SYMBOLs as well.  With the pending module.h split up, this would
    probably have caused build problems.
    
    Cc: Stephen Warren <swarren@nvidia.com>
    Cc: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/base/regmap/regmap-spi.c
+++ drivers/base/regmap/regmap-spi.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regmap.h>
 #include <linux/spi/spi.h>
 #include <linux/init.h>
+#include <linux/module.h>
 
 static int regmap_spi_write(struct device *dev, const void *data, size_t count)
 {
