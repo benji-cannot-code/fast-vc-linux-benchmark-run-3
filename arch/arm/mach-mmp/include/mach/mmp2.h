b29424f66ad472aa9934b04c356fb9b05ebfecd6
Leo Yan <leoy@marvell.com>
2011-08-15 11:09:53 +0800

    ARM: mmp: register audio sram bank
    MMP platform has sram for the audio island;
    Add the device structure for the audio sram,
    and register the audio sram device at init phase.
    
    Signed-off-by: Leo Yan <leoy@marvell.com>
    Acked-by: Haojian Zhuang <haojian.zhuang@gmail.com>
    Signed-off-by: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-mmp/include/mach/mmp2.h
+++ arch/arm/mach-mmp/include/mach/mmp2.h
@@ -14,6 +14,7 @@ extern void mmp2_clear_pmic_int(void);
 #include <linux/i2c.h>
 #include <linux/i2c/pxa-i2c.h>
 #include <mach/devices.h>
+#include <mach/sram.h>
 
 extern struct pxa_device_desc mmp2_device_uart1;
 extern struct pxa_device_desc mmp2_device_uart2;
@@ -29,6 +30,7 @@ extern struct pxa_device_desc mmp2_device_sdh0;
 extern struct pxa_device_desc mmp2_device_sdh1;
 extern struct pxa_device_desc mmp2_device_sdh2;
 extern struct pxa_device_desc mmp2_device_sdh3;
+extern struct pxa_device_desc mmp2_device_asram;
 
 static inline int mmp2_add_uart(int id)
 {
@@ -84,6 +86,11 @@ static inline int mmp2_add_sdhost(int id, struct sdhci_pxa_platdata *data)
 	}
 
 	return pxa_register_device(d, data, sizeof(*data));
+}
+
+static inline int mmp2_add_asram(struct sram_platdata *data)
+{
+	return pxa_register_device(&mmp2_device_asram, data, sizeof(*data));
 }
 
 #endif /* __ASM_MACH_MMP2_H */
