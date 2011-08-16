4c5039b7fe7ab370f4aa717e984daf7968acb560
Stanislaw Gruszka <sgruszka@redhat.com>
2011-08-16 14:17:04 +0200

    iwlegacy: rename iwlegacy to il
    More renaming to make code shorter.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

--- drivers/net/wireless/iwlegacy/iwl-debug.h
+++ drivers/net/wireless/iwlegacy/iwl-debug.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __il_debug_h__
 
 struct il_priv;
-extern u32 iwlegacy_debug_level;
+extern u32 il_debug_level;
 
 #define IL_ERR(p, f, a...) dev_err(&((p)->pci_dev->dev), f, ## a)
 #define IL_WARN(p, f, a...) dev_warn(&((p)->pci_dev->dev), f, ## a)
