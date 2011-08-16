da5131a1fdea70a02d7397f9c1873b4735e0e2a7
Ohad Ben-Cohen <ohad@wizery.com>
2011-08-16 15:19:10 +0300

    omap: iommu: PREFETCH_IOTLB cleanup
    Use PREFETCH_IOTLB to control the content of the called function,
    instead of inlining it in the code.
    
    This improves readability of the code, and also prevents an "unused
    function" warning to show up when PREFETCH_IOTLB isn't set.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Acked-by: Hiroshi DOYU <Hiroshi.DOYU@nokia.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/omap-iommu.c
+++ drivers/iommu/omap-iommu.c
@@ -254,6 +254,7 @@ static struct cr_regs __iotlb_read_cr(struct iommu *obj, int n)
  * @obj:	target iommu
  * @e:		an iommu tlb entry info
  **/
+#ifdef PREFETCH_IOTLB
 static int load_iotlb_entry(struct iommu *obj, struct iotlb_entry *e)
 {
 	int err = 0;
@@ -309,6 +310,20 @@ static int load_iotlb_entry(struct iommu *obj, struct iotlb_entry *e)
 out:
 	clk_disable(obj->clk);
 	return err;
+}
+
+#else /* !PREFETCH_IOTLB */
+
+static int load_iotlb_entry(struct iommu *obj, struct iotlb_entry *e)
+{
+	return 0;
+}
+
+#endif /* !PREFETCH_IOTLB */
+
+static int prefetch_iotlb_entry(struct iommu *obj, struct iotlb_entry *e)
+{
+	return load_iotlb_entry(obj, e);
 }
 
 /**
@@ -664,10 +679,8 @@ int iopgtable_store_entry(struct iommu *obj, struct iotlb_entry *e)
 
 	flush_iotlb_page(obj, e->da);
 	err = iopgtable_store_entry_core(obj, e);
-#ifdef PREFETCH_IOTLB
 	if (!err)
-		load_iotlb_entry(obj, e);
-#endif
+		prefetch_iotlb_entry(obj, e);
 	return err;
 }
 EXPORT_SYMBOL_GPL(iopgtable_store_entry);
