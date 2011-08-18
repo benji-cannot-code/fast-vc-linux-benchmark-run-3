bbcf7fbdfc11567ff5ce88c52e22d8dd98edc0b9
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:04:59 +0100

    um: take userland definition of barrier() to user.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/include/shared/process.h
+++ arch/um/include/shared/process.h
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <signal.h>
 
-/* Copied from linux/compiler-gcc.h since we can't include it directly */
-#define barrier() __asm__ __volatile__("": : :"memory")
-
 extern void sig_handler(int sig, struct sigcontext *sc);
 extern void alarm_handler(int sig, struct sigcontext *sc);
 
--- arch/um/os-Linux/signal.c
+++ arch/um/os-Linux/signal.c
@@ -17,9 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sysdep/barrier.h"
 #include "sysdep/sigcontext.h"
 
-/* Copied from linux/compiler-gcc.h since we can't include it directly */
-#define barrier() __asm__ __volatile__("": : :"memory")
-
 void (*sig_info[NSIG])(int, struct uml_pt_regs *) = {
 	[SIGTRAP]	= relay_signal,
 	[SIGFPE]	= relay_signal,
