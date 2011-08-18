a7e707ae78d991dce160350be8fdf14212f87ac0
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:07:59 +0100

    um: kill shared/tlb.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/kernel/tlb.c
+++ arch/um/kernel/tlb.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mem_user.h"
 #include "os.h"
 #include "skas.h"
-#include "tlb.h"
 
 struct host_vm_change {
 	struct host_vm_op {
