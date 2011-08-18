3a0c5e6272307f57af05575438c2a7d0f817753b
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:05:39 +0100

    um: sysdep/barrier.h is not used for anything
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/os-Linux/signal.c
+++ arch/um/os-Linux/signal.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "as-layout.h"
 #include "kern_util.h"
 #include "os.h"
-#include "sysdep/barrier.h"
 #include "sysdep/mcontext.h"
 
 void (*sig_info[NSIG])(int, struct uml_pt_regs *) = {
