b15edb1bab4b7857a3e9c9864b4113af0f72d359
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:08:09 +0100

    um: kill shared/mem_kern.h
    ... nothing declared there exists
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/ubd_kern.c
+++ arch/um/drivers/ubd_kern.c
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ubd_user.h"
 #include "os.h"
 #include "mem.h"
-#include "mem_kern.h"
 #include "cow.h"
 
 enum ubd_req { UBD_READ, UBD_WRITE };
