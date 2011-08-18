d864c2dc4e1292c1f860c6bcd9614d931546539a
Jochen Friedrich <jochen@scram.de>
2011-08-18 21:53:57 +0100

    ARM: 7027/1: simpad: Add ucb1x00 GPIO definitions and register GPIO
    Add ucb1x00 GPIO definitions to simpad.h and add gpio_base
    to ucb1x00 platform device so the pins are available using
    the GPIO API.
    
    Signed-off-by: Jochen Friedrich <jochen@scram.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-sa1100/simpad.c
+++ arch/arm/mach-sa1100/simpad.c
@@ -133,6 +133,7 @@ static struct resource simpad_flash_resources [] = {
 static struct mcp_plat_data simpad_mcp_data = {
 	.mccr0		= MCCR0_ADM,
 	.sclk_rate	= 11981000,
+	.gpio_base	= SIMPAD_UCB1X00_GPIO_BASE,
 };
 
 
