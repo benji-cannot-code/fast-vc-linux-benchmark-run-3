2b7970ff123e08efa27489c6d42f1c6829f9cf0f
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:01:49 +0100

    um: get rid of kern_constants.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/include/shared/user.h
+++ arch/um/include/shared/user.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __USER_H__
 #define __USER_H__
 
-#include "kern_constants.h"
+#include <generated/asm-offsets.h>
 
 /*
  * The usual definition - copied here because the kernel provides its own,
