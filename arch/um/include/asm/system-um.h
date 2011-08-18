7c2a71971026a64074691749331b1ae6029ba889
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:04:49 +0100

    um: take system.h out of shared/sysdep
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/include/asm/system-um.h
+++ arch/um/include/asm/system-um.h
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __UM_SYSTEM_GENERIC_H
 #define __UM_SYSTEM_GENERIC_H
 
-#include "sysdep/system.h"
-
 extern int get_signals(void);
 extern int set_signals(int enable);
 extern void block_signals(void);
--- arch/um/sys-x86/asm/system.h
+++ arch/um/sys-x86/asm/system.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpufeature.h>
 #include <asm/cmpxchg.h>
 #include <asm/nops.h>
+#include <asm/system-um.h>
 
 #include <linux/kernel.h>
 #include <linux/irqflags.h>
