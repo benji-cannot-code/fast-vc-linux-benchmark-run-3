c9ac6d164fe10cbb1e0660af5333799d38af9cf4
Geert Uytterhoeven <geert@linux-m68k.org>
2011-08-18 14:47:16 +0200

    m68k/q40: Convert Q40/Q60 to genirq
    q40_irq_handler() must be kept to translate ISA IRQs to the range 1-15.
    q40_probe_irq_o{ff,n}() become unused.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Richard Zidlicky <rz@linux-m68k.org>

--- arch/m68k/q40/q40ints.c
+++ arch/m68k/q40/q40ints.c
@@ -16,10 +16,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/interrupt.h>
+#ifdef CONFIG_GENERIC_HARDIRQS
+#include <linux/irq.h>
+#else
+#include <asm/irq.h>
+#endif
 
 #include <asm/ptrace.h>
 #include <asm/system.h>
-#include <asm/irq.h>
 #include <asm/traps.h>
 
 #include <asm/q40_master.h>
@@ -327,6 +331,7 @@ void q40_irq_disable(struct irq_data *data)
 	}
 }
 
+#ifndef CONFIG_GENERIC_HARDIRQS
 unsigned long q40_probe_irq_on(void)
 {
 	printk("irq probing not working - reconfigure the driver to avoid this\n");
@@ -336,3 +341,4 @@ int q40_probe_irq_off(unsigned long irqs)
 {
 	return -1;
 }
+#endif
