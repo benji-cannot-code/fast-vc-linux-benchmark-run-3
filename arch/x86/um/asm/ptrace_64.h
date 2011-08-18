9007fd2aae25b5edf4d88d5350bc2352b4cbf775
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:10:39 +0100

    um: merge signal_{32,64}.c
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/x86/um/asm/ptrace_64.h
+++ arch/x86/um/asm/ptrace_64.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PT_REGS_ORIG_RAX(r) UPT_ORIG_RAX(&(r)->regs)
 #define PT_REGS_RIP(r) UPT_IP(&(r)->regs)
-#define PT_REGS_RSP(r) UPT_SP(&(r)->regs)
+#define PT_REGS_SP(r) UPT_SP(&(r)->regs)
 
 #define PT_REGS_EFLAGS(r) UPT_EFLAGS(&(r)->regs)
 
