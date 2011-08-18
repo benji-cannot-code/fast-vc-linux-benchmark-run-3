c7c6c4551aa8f409aa9eae9d959d909ce372a19d
Al Viro <viro@ftp.linux.org.uk>
2011-08-18 20:10:19 +0100

    um: increase stack growth cushion in pagefault
    analog of [PATCH] i386: let usermode execute the "enter" instruction from
    circa 2006.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/x86/um/asm/processor.h
+++ arch/x86/um/asm/processor.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # include "processor_64.h"
 #endif
 
+#define ARCH_IS_STACKGROW(address) \
+       (address + 65536 + 32 * sizeof(unsigned long) >= UPT_SP(&current->thread.regs.regs))
+
 #include <asm/processor-generic.h>
 
 #endif
