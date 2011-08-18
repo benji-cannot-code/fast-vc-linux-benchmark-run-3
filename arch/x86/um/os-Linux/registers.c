cde1f66575fe08e4fefe9e3fcd92c8dff0907dc4
Richard Weinberger <richard@nod.at>
2011-08-18 21:58:07 +0200

    um: we need sys/user.h only on i386
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/x86/um/os-Linux/registers.c
+++ arch/x86/um/os-Linux/registers.c
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <errno.h>
 #include <sys/ptrace.h>
+#ifdef __i386__
 #include <sys/user.h>
+#endif
 #include "longjmp.h"
 #include "sysdep/ptrace_user.h"
 
