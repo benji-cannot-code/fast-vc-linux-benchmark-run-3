a3b8a5c36f6acf6a02cf85594912835e8803028d
Ralf Baechle <ralf@linux-mips.org>
2011-08-18 21:32:18 -0700

    NET: Korina: Don't include <asm/segment.h>
    Korina.c is a MIPS-specific SOC driver and <asm/segment> is empty on
    MIPS since 2.1.7 ...
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/korina.c
+++ drivers/net/ethernet/korina.c
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/system.h>
 #include <asm/bitops.h>
 #include <asm/pgtable.h>
-#include <asm/segment.h>
 #include <asm/io.h>
 #include <asm/dma.h>
 
