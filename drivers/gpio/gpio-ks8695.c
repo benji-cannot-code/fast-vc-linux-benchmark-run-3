f7d21d010634facf7d425b2f244b3f8c3be1ea3f
Linus Walleij <linus.walleij@linaro.org>
2011-08-22 08:37:38 +0100

    ARM: 7036/1: mach-ks8695: break out GPIO driver specifics
    The <mach/gpio.h> file is included from upper directories
    and deal with generic GPIO and gpiolib stuff. Break out the
    platform and driver specific defines and functions into its own
    header file.
    
    Cc: zeal <zealcook@gmail.com>
    Cc: Ben Dooks <ben-linux@fluff.org>
    Acked-by: Daniel Silverstone <dsilvers@simtec.co.uk>
    Acked-by: Simtec Linux Team <linux@simtec.co.uk>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/gpio/gpio-ks8695.c
+++ drivers/gpio/gpio-ks8695.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/irq.h>
 
 #include <mach/regs-gpio.h>
+#include <mach/gpio-ks8695.h>
 
 /*
  * Configure a GPIO line for either GPIO function, or its internal
