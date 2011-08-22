a22e09cd4bd73dab9901ef2818bdf157e66be3f2
Linus Walleij <linus.walleij@linaro.org>
2011-08-22 08:49:07 +0100

    ARM: 7049/1: mach-sa1100: move SA1100 GPIO driver to GPIO subsystem
    As per example from the other ARM boards, push the SA100
    GPIO driver down to the GPIO subsystem so it can be consolidated.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/gpio/gpio-sa1100.c
+++ drivers/gpio/gpio-sa1100.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 
 #include <mach/hardware.h>
-#include "generic.h"
 
 static int sa1100_gpio_get(struct gpio_chip *chip, unsigned offset)
 {
