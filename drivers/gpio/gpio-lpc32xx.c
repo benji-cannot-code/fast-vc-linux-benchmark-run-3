18a7cf368af9ff795ed0527541f9cd456e2e231f
Linus Walleij <linus.walleij@linaro.org>
2011-08-22 08:45:15 +0100

    ARM: 7045/1: mach-lpc32xx: break out GPIO driver specifics
    The <mach/gpio.h> file is included from upper directories
    and deal with generic GPIO and gpiolib stuff. Break out the
    platform and driver specific defines and functions into its own
    header file.
    
    Cc: Wolfram Sang <w.sang@pengutronix.de>
    Cc: Barry Song <bs14@csr.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/gpio/gpio-lpc32xx.c
+++ drivers/gpio/gpio-lpc32xx.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/hardware.h>
 #include <mach/platform.h>
+#include <mach/gpio-lpc32xx.h>
 
 #define LPC32XX_GPIO_P3_INP_STATE		_GPREG(0x000)
 #define LPC32XX_GPIO_P3_OUTP_SET		_GPREG(0x004)
