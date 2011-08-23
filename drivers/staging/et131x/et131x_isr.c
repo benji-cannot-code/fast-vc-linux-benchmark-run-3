465f54c98de46505c57a6d81a251aeb39e1e160b
Mark Einon <mark.einon@gmail.com>
2011-08-23 23:40:37 +0100

    staging: et131x: Rename var name 'etdev' to 'adapter' throughout module
    Used regex (from vim) :%s/\<etdev\>/adapter/g
    
    Changed because:
    * 'etdev' is easily confused as a misspelling of 'netdev'
    * 'adapter' is more widely used for this struct type in net drivers. This
    change made comparing code with other drivers much easier.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/et131x/et131x_isr.c
+++ drivers/staging/et131x/et131x_isr.c
@@ -243,10 +243,10 @@ out:
  */
 void et131x_isr_handler(struct work_struct *work)
 {
-	struct et131x_adapter *etdev =
+	struct et131x_adapter *adapter =
 		container_of(work, struct et131x_adapter, task);
-	u32 status = etdev->stats.interrupt_status;
-	struct address_map __iomem *iomem = etdev->regs;
+	u32 status = adapter->stats.interrupt_status;
+	struct address_map __iomem *iomem = adapter->regs;
 
 	/*
 	 * These first two are by far the most common.  Once handled, we clear
@@ -255,11 +255,11 @@ void et131x_isr_handler(struct work_struct *work)
 	 */
 	/* Handle all the completed Transmit interrupts */
 	if (status & ET_INTR_TXDMA_ISR)
-		et131x_handle_send_interrupt(etdev);
+		et131x_handle_send_interrupt(adapter);
 
 	/* Handle all the completed Receives interrupts */
 	if (status & ET_INTR_RXDMA_XFR_DONE)
-		et131x_handle_recv_interrupt(etdev);
+		et131x_handle_recv_interrupt(adapter);
 
 	status &= 0xffffffd7;
 
@@ -271,7 +271,7 @@ void et131x_isr_handler(struct work_struct *work)
 			/* Following read also clears the register (COR) */
 			txdma_err = readl(&iomem->txdma.tx_dma_error);
 
-			dev_warn(&etdev->pdev->dev,
+			dev_warn(&adapter->pdev->dev,
 				    "TXDMA_ERR interrupt, error = %d\n",
 				    txdma_err);
 		}
@@ -296,8 +296,8 @@ void et131x_isr_handler(struct work_struct *work)
 			/* If the user has flow control on, then we will
 			 * send a pause packet, otherwise just exit
 			 */
-			if (etdev->flowcontrol == FLOW_TXONLY ||
-			    etdev->flowcontrol == FLOW_BOTH) {
+			if (adapter->flowcontrol == FLOW_TXONLY ||
+			    adapter->flowcontrol == FLOW_BOTH) {
 				u32 pm_csr;
 
 				/* Tell the device to send a pause packet via
@@ -346,7 +346,7 @@ void et131x_isr_handler(struct work_struct *work)
 			 */
 			/* TRAP();*/
 
-			dev_warn(&etdev->pdev->dev,
+			dev_warn(&adapter->pdev->dev,
 				    "RxDMA_ERR interrupt, error %x\n",
 				    readl(&iomem->txmac.tx_test));
 		}
@@ -360,7 +360,7 @@ void et131x_isr_handler(struct work_struct *work)
 			 * message when we are in DBG mode, otherwise we
 			 * will ignore it.
 			 */
-			dev_err(&etdev->pdev->dev, "WAKE_ON_LAN interrupt\n");
+			dev_err(&adapter->pdev->dev, "WAKE_ON_LAN interrupt\n");
 		}
 
 		/* Handle the PHY interrupt */
@@ -380,26 +380,26 @@ void et131x_isr_handler(struct work_struct *work)
 				 * so, disable it because we will not be able
 				 * to read PHY values until we are out.
 				 */
-				et1310_disable_phy_coma(etdev);
+				et1310_disable_phy_coma(adapter);
 			}
 
 			/* Read the PHY ISR to clear the reason for the
 			 * interrupt.
 			 */
-			et131x_mii_read(etdev,
+			et131x_mii_read(adapter,
 					(uint8_t) offsetof(struct mi_regs, isr),
 					&myisr);
 
-			if (!etdev->replica_phy_loopbk) {
-				et131x_mii_read(etdev,
+			if (!adapter->replica_phy_loopbk) {
+				et131x_mii_read(adapter,
 				       (uint8_t) offsetof(struct mi_regs, bmsr),
 				       &bmsr_data);
 
-				bmsr_ints = etdev->bmsr ^ bmsr_data;
-				etdev->bmsr = bmsr_data;
+				bmsr_ints = adapter->bmsr ^ bmsr_data;
+				adapter->bmsr = bmsr_data;
 
 				/* Do all the cable in / cable out stuff */
-				et131x_mii_check(etdev, bmsr_data, bmsr_ints);
+				et131x_mii_check(adapter, bmsr_data, bmsr_ints);
 			}
 		}
 
@@ -417,7 +417,7 @@ void et131x_isr_handler(struct work_struct *work)
 			 * a nutshell, the whole Tx path will have to be reset
 			 * and re-configured afterwards.
 			 */
-			dev_warn(&etdev->pdev->dev,
+			dev_warn(&adapter->pdev->dev,
 				    "TXMAC interrupt, error 0x%08x\n",
 				    err);
 
@@ -435,14 +435,14 @@ void et131x_isr_handler(struct work_struct *work)
 			 * set the flag to cause us to reset so we can solve
 			 * this issue.
 			 */
-			/* MP_SET_FLAG( etdev,
+			/* MP_SET_FLAG( adapter,
 						fMP_ADAPTER_HARDWARE_ERROR); */
 
-			dev_warn(&etdev->pdev->dev,
+			dev_warn(&adapter->pdev->dev,
 			  "RXMAC interrupt, error 0x%08x.  Requesting reset\n",
 				    readl(&iomem->rxmac.err_reg));
 
-			dev_warn(&etdev->pdev->dev,
+			dev_warn(&adapter->pdev->dev,
 				    "Enable 0x%08x, Diag 0x%08x\n",
 				    readl(&iomem->rxmac.ctrl),
 				    readl(&iomem->rxmac.rxq_diag));
@@ -462,7 +462,7 @@ void et131x_isr_handler(struct work_struct *work)
 			 * to maintain the top, software managed bits of the
 			 * counter(s).
 			 */
-			et1310_handle_macstat_interrupt(etdev);
+			et1310_handle_macstat_interrupt(adapter);
 		}
 
 		/* Handle SLV Timeout Interrupt */
@@ -478,5 +478,5 @@ void et131x_isr_handler(struct work_struct *work)
 			 */
 		}
 	}
-	et131x_enable_interrupts(etdev);
+	et131x_enable_interrupts(adapter);
 }
