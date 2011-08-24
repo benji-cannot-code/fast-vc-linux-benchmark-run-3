7c6f013e361290328d7db299c242ab5e2e02842d
Jerome Glisse <jglisse@redhat.com>
2011-08-24 20:00:17 +0000

    drm/radeon/kms: evergreen & ni reset SPI block on CP resume
    For some reason SPI block is in broken state after module
    unloading. This lead to broken rendering after reloading
    module. Fix this by reseting SPI block in CP resume function
    
    Signed-off-by: Jerome Glisse <jglisse@redhat.com
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@kernel.org
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/ni.c
+++ drivers/gpu/drm/radeon/ni.c
@@ -1160,6 +1160,7 @@ int cayman_cp_resume(struct radeon_device *rdev)
 				 SOFT_RESET_PA |
 				 SOFT_RESET_SH |
 				 SOFT_RESET_VGT |
+				 SOFT_RESET_SPI |
 				 SOFT_RESET_SX));
 	RREG32(GRBM_SOFT_RESET);
 	mdelay(15);
