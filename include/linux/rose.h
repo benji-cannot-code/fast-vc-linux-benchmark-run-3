52d9ca733b0548bd34a941f282460ab335f09839
Ben Hutchings <ben@decadent.org.uk>
2011-08-24 18:43:18 +0000

    headers, ax25: Add missing #include to <linux/netrom.h>, <linux/rose.h>
    These headers use the ax25_address type defined in <linux/ax25.h>.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/rose.h
+++ include/linux/rose.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	ROSE_KERNEL_H
 #define	ROSE_KERNEL_H
 
+#include <linux/ax25.h>
+
 #define ROSE_MTU	251
 
 #define ROSE_MAX_DIGIS 6
