3e7d7a792543141dc572474411f434b78a1e12cd
Ben Hutchings <ben@decadent.org.uk>
2011-08-24 18:40:48 +0000

    headers, pppox: Add missing #include to <linux/if_pppox.h>
    <linux/if_ppox.h> uses ETH_ALEN, defined in <linux/if_ether.h>.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_pppox.h
+++ include/linux/if_pppox.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/byteorder.h>
 
-#ifdef  __KERNEL__
 #include <linux/if_ether.h>
+#ifdef  __KERNEL__
 #include <linux/if.h>
 #include <linux/netdevice.h>
 #include <linux/ppp_channel.h>
