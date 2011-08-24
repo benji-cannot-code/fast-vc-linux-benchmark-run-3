71a0a4d3b28c1b3f1f04ea9f30faabe27aeb2664
Daniel Kurtz <djkurtz@chromium.org>
2011-08-23 23:02:31 -0700

    Input: synaptics - decode AGM packet types
    A Synaptics image sensor tracks 5 fingers, but can only report 2.
    
    The algorithm for choosing which 2 fingers to report and in which packet:
      Touchpad maintains 5 slots, numbered 0 to 4
      Initially all slots are empty
      As new fingers are detected, assign them to the lowest available slots
      The touchpad always reports:
        SGM: lowest numbered non-empty slot
        AGM: highest numbered non-empty slot, if there is one
    
    In addition, these touchpads have a special AGM packet type which reports
    the number of fingers currently being tracked, and which finger is in
    each of the two slots.  Unfortunately, these "TYPE=2" packets are only used
    when more than 3 fingers are being tracked.  When less than 4 fingers
    are present, the 'w' value must be used to track how many fingers are
    present, and knowing which fingers are being reported is much more
    difficult, if not impossible.
    
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>
    Acked-by: Chase Douglas <chase.douglas@canonical.com>
    Acked-by: Henrik Rydberg <rydberg@euromail.se>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/mouse/synaptics.h
+++ drivers/input/mouse/synaptics.h
@@ -116,9 +116,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SYN_REDUCED_FILTER_FUZZ		8
 
 /*
+ * A structure to describe which internal touchpad finger slots are being
+ * reported in raw packets.
+ */
+struct synaptics_mt_state {
+	int count;			/* num fingers being tracked */
+	int sgm;			/* which slot is reported by sgm pkt */
+	int agm;			/* which slot is reported by agm pkt*/
+};
+
+/*
  * A structure to describe the state of the touchpad hardware (buttons and pad)
  */
-
 struct synaptics_hw_state {
 	int x;
 	int y;
@@ -131,6 +140,9 @@ struct synaptics_hw_state {
 	unsigned int down:1;
 	unsigned char ext_buttons;
 	signed char scroll;
+
+	/* As reported in last AGM-CONTACT packets */
+	struct synaptics_mt_state mt_state;
 };
 
 struct synaptics_data {
