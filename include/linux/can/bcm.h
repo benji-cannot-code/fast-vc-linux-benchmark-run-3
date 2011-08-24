768790e2d85566e0838b0948157a16d17871e822
Ben Hutchings <ben@decadent.org.uk>
2011-08-24 18:46:06 +0000

    headers, can: Add missing #include to <linux/can/bcm.h>
    <linux/can/bcm.h> uses type canid_t, defined in <linux/can.h>.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Acked-by: Oliver Hartkopp <socketcan@hartkopp.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/can/bcm.h
+++ include/linux/can/bcm.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CAN_BCM_H
 
 #include <linux/types.h>
+#include <linux/can.h>
 
 /**
  * struct bcm_msg_head - head of messages to/from the broadcast manager
