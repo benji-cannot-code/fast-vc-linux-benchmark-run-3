8110a787653f71a95045b14ba77ce93098d37ee6
Joe Perches <joe@perches.com>
2011-08-25 13:22:24 +0000

    caif: Remove OOM messages, use kzalloc
    Remove per site OOM messages because they duplicate
    the generic mm subsystem OOM message.
    
    Use kzalloc instead of kmalloc/memset
    when next to the OOM message removals.
    
    Reduces object size (allyesconfig ~2%)
    
    $ size -t drivers/net/caif/built-in.o.old net/caif/built-in.o.old
       text	   data	    bss	    dec	    hex	filename
      32297	    700	   8224	  41221	   a105	drivers/net/caif/built-in.o.old
      72159	   1317	  20552	  94028	  16f4c	net/caif/built-in.o.old
     104456	   2017	  28776	 135249	  21051	(TOTALS)
    $ size -t drivers/net/caif/built-in.o.new net/caif/built-in.o.new
       text	   data	    bss	    dec	    hex	filename
      31975	    700	   8184	  40859	   9f9b	drivers/net/caif/built-in.o.new
      70748	   1317	  20152	  92217	  16839	net/caif/built-in.o.new
     102723	   2017	  28336	 133076	  207d4	(TOTALS)
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/caif/cfcnfg.c
+++ net/caif/cfcnfg.c
@@ -79,10 +79,8 @@ struct cfcnfg *cfcnfg_create(void)
 
 	/* Initiate this layer */
 	this = kzalloc(sizeof(struct cfcnfg), GFP_ATOMIC);
-	if (!this) {
-		pr_warn("Out of memory\n");
+	if (!this)
 		return NULL;
-	}
 	this->mux = cfmuxl_create();
 	if (!this->mux)
 		goto out_of_mem;
@@ -109,8 +107,6 @@ struct cfcnfg *cfcnfg_create(void)
 
 	return this;
 out_of_mem:
-	pr_warn("Out of memory\n");
-
 	synchronize_rcu();
 
 	kfree(this->mux);
@@ -449,10 +445,8 @@ cfcnfg_linkup_rsp(struct cflayer *layer, u8 channel_id, enum cfctrl_srv serv,
 				"- unknown channel type\n");
 		goto unlock;
 	}
-	if (!servicel) {
-		pr_warn("Out of memory\n");
+	if (!servicel)
 		goto unlock;
-	}
 	layer_set_dn(servicel, cnfg->mux);
 	cfmuxl_set_uplayer(cnfg->mux, servicel, channel_id);
 	layer_set_up(servicel, adapt_layer);
@@ -498,10 +492,8 @@ got_phyid:
 	case CFPHYTYPE_FRAG:
 		phy_driver =
 		    cfserl_create(CFPHYTYPE_FRAG, phyid, stx);
-		if (!phy_driver) {
-			pr_warn("Out of memory\n");
+		if (!phy_driver)
 			goto out;
-		}
 		break;
 	case CFPHYTYPE_CAIF:
 		phy_driver = NULL;
@@ -522,7 +514,6 @@ got_phyid:
 	frml = cffrml_create(phyid, fcs);
 
 	if (!frml) {
-		pr_warn("Out of memory\n");
 		kfree(phyinfo);
 		goto out;
 	}
