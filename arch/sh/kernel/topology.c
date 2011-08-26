b4b4b3eff3dcd1debb7abd19b10b11c4bf22b6fc
Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
2011-08-26 05:05:41 +0000

    sh: Fix implicit declaration of function numa_node_id
    CC      arch/sh/kernel/topology.o
      arch/sh/kernel/topology.c: In function ‘topology_init’:
      arch/sh/kernel/topology.c:77: error: implicit declaration of function ‘numa_node_id’
    
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/topology.c
+++ arch/sh/kernel/topology.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpumask.h>
 #include <linux/init.h>
 #include <linux/percpu.h>
+#include <linux/topology.h>
 #include <linux/node.h>
 #include <linux/nodemask.h>
 
