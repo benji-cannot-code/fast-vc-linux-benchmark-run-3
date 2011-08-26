f93174de47b7354e467f6131ea590cd67096805f
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2011-08-25 23:10:33 -0700

    iwlagn: Rename iwlcore prefix
    There are number of functions with "iwlcore_" prefix which not feels right,
    rename those to "iwl_".
    
    No functional changes by making the renames.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-eeprom.c
+++ drivers/net/wireless/iwlwifi/iwl-eeprom.c
@@ -227,7 +227,7 @@ static void iwl_set_otp_access(struct iwl_priv *priv, enum iwl_access_mode mode)
 			    CSR_OTP_GP_REG_OTP_ACCESS_MODE);
 }
 
-static int iwlcore_get_nvm_type(struct iwl_priv *priv, u32 hw_rev)
+static int iwl_get_nvm_type(struct iwl_priv *priv, u32 hw_rev)
 {
 	u32 otpgp;
 	int nvm_type;
@@ -432,7 +432,7 @@ int iwl_eeprom_init(struct iwl_priv *priv, u32 hw_rev)
 	u16 validblockaddr = 0;
 	u16 cache_addr = 0;
 
-	priv->nvm_device_type = iwlcore_get_nvm_type(priv, hw_rev);
+	priv->nvm_device_type = iwl_get_nvm_type(priv, hw_rev);
 	if (priv->nvm_device_type == -ENOENT)
 		return -ENOENT;
 	/* allocate eeprom */
