de3fe408100b74dcde48bfcc96132a51f9e78039
Arnaldo Carvalho de Melo <acme@redhat.com>
2011-08-29 08:33:17 -0300

    perf buildid-list: Support showing the build id in an ELF file
    Try first reading the build id, validating that it is an ELF file, etc.
    Cheap as libelf will bail out as soon as the magic number check fails.
    
    Useful when investigating debuginfo packaging problems like this one:
    
    [root@emilia ~]# perf buildid-list -i /usr/lib/debug/lib/modules/`uname -r`/vmlinux
    77bb4ea591a602d455ace759a377c9adfff1aba3
    [root@emilia ~]# perf buildid-list -k
    07b0c016a2b30004e86132d0239945b1e88f5d75
    [root@emilia ~]#
    
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-4elot9oxwa0rr0d90dshca3a@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-buildid-list.c
+++ tools/perf/builtin-buildid-list.c
@@ -2,8 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * builtin-buildid-list.c
  *
- * Builtin buildid-list command: list buildids in perf.data or in the running
- * kernel.
+ * Builtin buildid-list command: list buildids in perf.data, in the running
+ * kernel and in ELF files.
  *
  * Copyright (C) 2009, Red Hat Inc.
  * Copyright (C) 2009, Arnaldo Carvalho de Melo <acme@redhat.com>
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "util/parse-options.h"
 #include "util/session.h"
 #include "util/symbol.h"
+
+#include <libelf.h>
 
 static char const *input_name = "perf.data";
 static bool force;
@@ -71,11 +73,30 @@ static int sysfs__fprintf_build_id(FILE *fp)
 	return 0;
 }
 
+static int filename__fprintf_build_id(const char *name, FILE *fp)
+{
+	u8 build_id[BUILD_ID_SIZE];
+	char sbuild_id[BUILD_ID_SIZE * 2 + 1];
+
+	if (filename__read_build_id(name, build_id,
+				    sizeof(build_id)) != sizeof(build_id))
+		return 0;
+
+	build_id__sprintf(build_id, sizeof(build_id), sbuild_id);
+	return fprintf(fp, "%s\n", sbuild_id);
+}
+
 static int __cmd_buildid_list(void)
 {
-
 	if (show_kernel)
 		return sysfs__fprintf_build_id(stdout);
+
+	elf_version(EV_CURRENT);
+	/*
+ 	 * See if this is an ELF file first:
+ 	 */
+	if (filename__fprintf_build_id(input_name, stdout))
+		return 0;
 
 	return perf_session__list_build_ids();
 }
