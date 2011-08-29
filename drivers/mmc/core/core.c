97c84c6ec13d5ceaa12cc053776622fe31a1fb6f
Adrian Hunter <adrian.hunter@intel.com>
2011-08-29 16:42:15 +0300

    mmc: block: add eMMC hardware reset support
    For cards that support hardware reset (just eMMC), try a reset and
    retry before returning an I/O error.  However this is not done for
    ECC errors and is never done twice for the same operation type
    (READ, WRITE, DISCARD, SECURE DISCARD) until that type of operation
    again succeeds.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1530,7 +1530,7 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 	if (err) {
 		printk(KERN_ERR "mmc_erase: group start error %d, "
 		       "status %#x\n", err, cmd.resp[0]);
-		err = -EINVAL;
+		err = -EIO;
 		goto out;
 	}
 
@@ -1545,7 +1545,7 @@ static int mmc_do_erase(struct mmc_card *card, unsigned int from,
 	if (err) {
 		printk(KERN_ERR "mmc_erase: group end error %d, status %#x\n",
 		       err, cmd.resp[0]);
-		err = -EINVAL;
+		err = -EIO;
 		goto out;
 	}
 
