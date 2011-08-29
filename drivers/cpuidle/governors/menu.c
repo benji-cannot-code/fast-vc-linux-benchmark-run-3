15882d953b66ab547b772fa0475b4848c0a9654e
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-08-29 17:52:39 -0400

    cpuidle: Add module.h to drivers/cpuidle files as required.
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tick.h>
 #include <linux/sched.h>
 #include <linux/math64.h>
+#include <linux/module.h>
 
 #define BUCKETS 12
 #define INTERVALS 8
