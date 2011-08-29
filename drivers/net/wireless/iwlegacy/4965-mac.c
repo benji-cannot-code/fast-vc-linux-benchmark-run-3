f828d30df0bfa42c1da6a2a57ceeb04c438b2181
Stanislaw Gruszka <sgruszka@redhat.com>
2011-08-29 16:59:15 +0200

    iwlegacy: merge iwl-4965-led.c into 4965.c
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

--- drivers/net/wireless/iwlegacy/4965-mac.c
+++ drivers/net/wireless/iwlegacy/4965-mac.c
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-sta.h"
 #include "iwl-4965-calib.h"
 #include "iwl-4965.h"
-#include "iwl-4965-led.h"
 
 
 /******************************************************************************
