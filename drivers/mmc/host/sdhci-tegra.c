e761a0c3e033497127b6cbf8e7098968151314c5
Stephen Warren <swarren@nvidia.com>
2011-08-30 13:17:16 -0600

    mmc: sdhci-tegra: Add 8-bit support to device tree binding.
    The previous patch which implemented a DT binding for sdhci-tegra did not
    allow all platform data fields to be initialized from DT. The following
    were missing:
    
    is_8bit: Implemented by this patch.
    
    pm_flags: Not implemented yet. There are no mainline users of this field.
      I'm not quite sure what it's for, and hence how to represent this
      in DT; the value ends up being assigned to host->mmc->pm_caps.
    
    While we're at it, fix the binding documentation to refer to "SD/MMC"
    instead of "eSDHC", since that's the correct name; "eSDHC" was cut/paste
    from the Freescale binding docs.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/io.h>
+#include <linux/of.h>
 #include <linux/of_gpio.h>
 #include <linux/gpio.h>
 #include <linux/mmc/card.h>
@@ -153,6 +154,8 @@ static struct tegra_sdhci_platform_data * __devinit sdhci_tegra_dt_parse_pdata(
 	plat->cd_gpio = of_get_named_gpio(np, "cd-gpios", 0);
 	plat->wp_gpio = of_get_named_gpio(np, "wp-gpios", 0);
 	plat->power_gpio = of_get_named_gpio(np, "power-gpios", 0);
+	if (of_find_property(np, "support-8bit", NULL))
+		plat->is_8bit = 1;
 
 	return plat;
 }
