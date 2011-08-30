4fc6f556a9bf2d0ff653bcf3c5d69006c751e39f
Peter Ujfalusi <peter.ujfalusi@ti.com>
2011-08-30 14:39:52 +0300

    ASoC: tpa6130a2: Model support cleanup
    Use the device name and driver_data to identify
    the TPA model supported by the driver.
    Board files should use either "tpa6130a2" or
    "tpa6140a2" as device name to specify the model
    in used on the specific board.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Tested-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Acked-by: Liam Girdwood <lrg@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/tpa6130a2.c
+++ sound/soc/codecs/tpa6130a2.c
@@ -384,7 +384,7 @@ static int __devinit tpa6130a2_probe(struct i2c_client *client,
 
 	pdata = client->dev.platform_data;
 	data->power_gpio = pdata->power_gpio;
-	data->id = pdata->id;
+	data->id = id->driver_data;
 
 	mutex_init(&data->mutex);
 
@@ -406,7 +406,7 @@ static int __devinit tpa6130a2_probe(struct i2c_client *client,
 	switch (data->id) {
 	default:
 		dev_warn(dev, "Unknown TPA model (%d). Assuming 6130A2\n",
-			 pdata->id);
+			 data->id);
 	case TPA6130A2:
 		regulator = "Vdd";
 		break;
@@ -470,7 +470,8 @@ static int __devexit tpa6130a2_remove(struct i2c_client *client)
 }
 
 static const struct i2c_device_id tpa6130a2_id[] = {
-	{ "tpa6130a2", 0 },
+	{ "tpa6130a2", TPA6130A2 },
+	{ "tpa6140a2", TPA6140A2 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, tpa6130a2_id);
