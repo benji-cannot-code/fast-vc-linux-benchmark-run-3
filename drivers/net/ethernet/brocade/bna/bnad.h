0298d08f60a5d6b9bb5cf74d2cb449ad63969b05
Rasesh Mody <rmody@brocade.com>
2011-08-30 15:27:39 +0000

    bna: Interrupt Polling and NAPI Init Changes
    Change details:
     - Remove unnecessary ccb check from bnad_poll_cq
     - Add bnad pointer to rx_ctrl structure, so that bnad can be accessed directly
       from rx_ctrl in the NAPI poll routines, even if ccb is NULL
     - Validate ccb before referencing to it in bnad_msix_rx and bnad_napi_poll_rx
     - Fix the order of NAPI init / uninit in Tx / Rx setup / teardown path:
       a. Kill bnad tx free tasklet ahead of call to bna_tx_destroy()
       b. Call NAPI disable only after call to Rx free_irq(). This makes sure Rx
          interrupt does not schedule a poll when NAPI is already disabled
     - NAPI poll runs before the h/w has completed configuration. This causes a
       crash. Delay enabling NAPI till after bna_rx_enable(). Split NAPI
       initialization into 2 steps, bnad_napi_init() & bnad_napi_enable().
    
    Signed-off-by: Gurunatha Karaje <gkaraje@brocade.com>
    Signed-off-by: Rasesh Mody <rmody@brocade.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/brocade/bna/bnad.h
+++ drivers/net/ethernet/brocade/bna/bnad.h
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct bnad_rx_ctrl {
 	struct bna_ccb *ccb;
+	struct bnad *bnad;
 	unsigned long  flags;
 	struct napi_struct	napi;
 };
