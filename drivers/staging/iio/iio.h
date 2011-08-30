af4d1188cbde264dd251ca4f9fc33d6bab7d99b6
Jonathan Cameron <jic23@cam.ac.uk>
2011-08-30 12:41:11 +0100

    staging:iio:trivial add a string array for IIO_RAW and IIO_PROCESSED.
    Introduce IIO_RAW and IIO_PROCESSED and an array for their strings.
    Also move one function to just above where it is called.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/iio.h
+++ drivers/staging/iio/iio.h
@@ -23,6 +23,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Currently assumes nano seconds.
  */
 
+enum iio_data_type {
+	IIO_RAW,
+	IIO_PROCESSED,
+};
+
 enum iio_chan_type {
 	/* real channel types */
 	IIO_IN,
