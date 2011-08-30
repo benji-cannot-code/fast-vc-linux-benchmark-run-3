424e02ea7570f0dd277ddfc4b21d9e336db72437
Alexey Khoroshilov <khoroshilov@ispras.ru>
2011-08-31 00:41:14 +0400

    staging: mei: unlock dev->device_lock mutex on error path in mei_open()
    mei_open() acquires dev->device_lock mutex and try to allocate mei_cl,
    but if the allocation fails it goes to return statement. As a result
    dev->device_lock left locked.
    
    The patch fixes goto to unlock dev->device_lock mutex on this path.
    
    Found by Linux Driver Verification project (linuxtesting.org).
    
    Signed-off-by: Alexey Khoroshilov <khoroshilov@ispras.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/mei/main.c
+++ drivers/staging/mei/main.c
@@ -414,7 +414,7 @@ static int mei_open(struct inode *inode, struct file *file)
 	err = -ENOMEM;
 	cl = mei_cl_allocate(dev);
 	if (!cl)
-		goto out;
+		goto out_unlock;
 
 	err = -ENODEV;
 	if (dev->mei_state != MEI_ENABLED) {
