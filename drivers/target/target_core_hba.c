fede66e4338a77ffc65cd8b67242145ab9213c1d
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-08-30 14:20:44 -0400

    drivers/target: Add module.h to drivers/target files as required.
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/target/target_core_hba.c
+++ drivers/target/target_core_hba.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/in.h>
+#include <linux/module.h>
 #include <net/sock.h>
 #include <net/tcp.h>
 
--- drivers/target/target_core_transport.c
+++ drivers/target/target_core_transport.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kthread.h>
 #include <linux/in.h>
 #include <linux/cdrom.h>
+#include <linux/module.h>
 #include <asm/unaligned.h>
 #include <net/sock.h>
 #include <net/tcp.h>
