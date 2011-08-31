a573c6f73744c2e0fb1a212bc4caa527542494e5
Stanislaw Gruszka <sgruszka@redhat.com>
2011-08-31 11:19:51 +0200

    iwlegacy: rename iwl-csr.h to csr.h
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

--- drivers/net/wireless/iwlegacy/3945.h
+++ drivers/net/wireless/iwlegacy/3945.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Hardware specific file defines the PCI IDs table for that hardware module */
 extern const struct pci_device_id il3945_hw_card_ids[];
 
-#include "iwl-csr.h"
 #include "iwl-prph.h"
 #include "iwl-debug.h"
 #include "iwl-power.h"
--- drivers/net/wireless/iwlegacy/iwl-dev.h
+++ drivers/net/wireless/iwlegacy/iwl-dev.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ieee80211_radiotap.h>
 
 #include "iwl-eeprom.h"
-#include "iwl-csr.h"
+#include "csr.h"
 #include "iwl-prph.h"
 #include "iwl-debug.h"
 #include "4965.h"
