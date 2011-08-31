ac55158d9dfbbfb8e87c49f20cd47c0f7630e26a
Stanislaw Gruszka <sgruszka@redhat.com>
2011-08-31 14:09:39 +0200

    iwlegacy: rename iwl-prph.h to prph.h
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

--- drivers/net/wireless/iwlegacy/3945.h
+++ drivers/net/wireless/iwlegacy/3945.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern const struct pci_device_id il3945_hw_card_ids[];
 
 #include "common.h"
-#include "iwl-prph.h"
 #include "iwl-debug.h"
 
 /* Highest firmware API version supported */
