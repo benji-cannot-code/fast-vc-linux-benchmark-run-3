c9d247fce046a6c22e06b94a0d88898ca6cc293a
Haiyang Zhang <haiyangz@microsoft.com>
2011-09-01 12:19:39 -0700

    staging: hv: remove unnecessary includes in netvsc
    hyperv.h is included by hyperv_net.h already, so no need to include it
    again in these C files.
    
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/hv/netvsc_drv.c
+++ drivers/staging/hv/netvsc_drv.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <net/pkt_sched.h>
 
-#include "hyperv.h"
 #include "hyperv_net.h"
 
 struct net_device_context {
