36cbfa75174629a243695f7ed4ef27a7ccda736f
Axel Lin <axel.lin@gmail.com>
2011-09-01 08:13:38 +0800

    video: nuc900fb: remove include of mach/clkdev.h
    Since commit aa3831cf9d29cfeaebd8c2169378b74111364487
    "ARM: Consolidate the clkdev header files",
    the header file arch/arm/mach-nuc93x/include/mach/clkdev.h is removed.
    
    This patch fixes below build error:
    
    drivers/video/nuc900fb.c:42:25: error: mach/clkdev.h: No such file or directory
    make[2]: *** [drivers/video/nuc900fb.o] Error 1
    make[1]: *** [drivers/video] Error 2
    make: *** [drivers] Error 2
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>

--- drivers/video/nuc900fb.c
+++ drivers/video/nuc900fb.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/regs-clock.h>
 #include <mach/regs-ldm.h>
 #include <mach/fb.h>
-#include <mach/clkdev.h>
 
 #include "nuc900fb.h"
 
