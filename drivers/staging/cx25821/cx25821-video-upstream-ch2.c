572cb73d98b4cd2e19980926b981b11df5580d22
Leonid V. Fedorenchik <leonidsbox@gmail.com>
2011-09-02 11:55:42 +0800

    Staging: cx25821: Fix labels' placement in cx25821-video-upstream-ch2.c
    Delete whitespace characters before labels.
    
    Signed-off-by: Leonid V. Fedorenchik <leonidsbox@gmail.com>
    Reviewed-By: Valdis Kletnieks <valdis.kletnieks@vt.edu>
    Reviewed-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/cx25821/cx25821-video-upstream-ch2.c
+++ drivers/staging/cx25821/cx25821-video-upstream-ch2.c
@@ -522,7 +522,7 @@ static int cx25821_upstream_buffer_prepare_ch2(struct cx25821_dev *dev,
 
 	return 0;
 
-	error:
+error:
 	return ret;
 }
 
@@ -721,7 +721,7 @@ int cx25821_start_video_dma_upstream_ch2(struct cx25821_dev *dev,
 
 	return 0;
 
-	fail_irq:
+fail_irq:
 	cx25821_dev_unregister(dev);
 	return err;
 }
@@ -831,7 +831,7 @@ int cx25821_vidupstream_init_ch2(struct cx25821_dev *dev, int channel_select,
 
 	return 0;
 
-	error:
+error:
 	cx25821_dev_unregister(dev);
 
 	return err;
