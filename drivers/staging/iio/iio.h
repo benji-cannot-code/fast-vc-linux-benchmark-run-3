3c684e392dc45c3b1907032b664181f1a55aa0d6
Jonathan Cameron <jic23@cam.ac.uk>
2011-09-02 17:14:34 +0100

    staging:iio: ABI rework - add in_ or out_ prefix to channnels
    Also involves changing current inX outX to in_voltageX and out_voltageX
    
    V2: squash users of the IIO_CHAN_OUT macro and get rid of it.
    There are very few of these, so it is easier to fix them.
    
    Signed-off-by: Jonathan Cameron <jic23@cam.ac.uk>
    Acked-by: Michael Hennerich <Michael.Hennerich@analog.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/iio.h
+++ drivers/staging/iio/iio.h
@@ -28,12 +28,11 @@ enum iio_data_type {
 
 enum iio_chan_type {
 	/* real channel types */
-	IIO_IN,
-	IIO_OUT,
+	IIO_VOLTAGE,
 	IIO_CURRENT,
 	IIO_POWER,
 	IIO_ACCEL,
-	IIO_IN_DIFF,
+	IIO_VOLTAGE_DIFF,
 	IIO_GYRO,
 	IIO_MAGN,
 	IIO_LIGHT,
@@ -45,6 +44,10 @@ enum iio_chan_type {
 	IIO_ANGL,
 	IIO_TIMESTAMP,
 };
+
+/* Nasty hack to avoid massive churn */
+#define	IIO_IN IIO_VOLTAGE
+#define	IIO_IN_DIFF IIO_VOLTAGE_DIFF
 
 #define IIO_MOD_X			0
 #define IIO_MOD_LIGHT_BOTH		0
@@ -128,14 +131,17 @@ struct iio_chan_spec {
 	unsigned		processed_val:1;
 	unsigned		modified:1;
 	unsigned		indexed:1;
+	unsigned		output:1;
 };
 
 #define IIO_ST(si, rb, sb, sh)						\
 	{ .sign = si, .realbits = rb, .storagebits = sb, .shift = sh }
 
-#define IIO_CHAN(_type, _mod, _indexed, _proc, _name, _chan, _chan2,	\
+/* Macro assumes input channels */
+#define IIO_CHAN(_type, _mod, _indexed, _proc, _name, _chan, _chan2, \
 		 _inf_mask, _address, _si, _stype, _event_mask)		\
 	{ .type = _type,						\
+	  .output = 0,							\
 	  .modified = _mod,						\
 	  .indexed = _indexed,						\
 	  .processed_val = _proc,					\
