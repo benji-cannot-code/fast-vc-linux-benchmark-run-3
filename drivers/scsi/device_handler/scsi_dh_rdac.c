68962bfcb0017095d67e23fdee374d607554bc4f
Moger, Babu <Babu.Moger@netapp.com>
2011-09-02 11:21:17 -0400

    [SCSI] scsi_dh_rdac: Adding couple more vendor product ids
    This patch adds couple more Vendor/Product IDs for RDAC.. There are no
    functional changes.
    
    Signed-off-by: Babu Moger <babu.moger@netapp.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/device_handler/scsi_dh_rdac.c
+++ drivers/scsi/device_handler/scsi_dh_rdac.c
@@ -796,6 +796,7 @@ static const struct scsi_dh_devlist rdac_dev_list[] = {
 	{"IBM", "3526"},
 	{"SGI", "TP9400"},
 	{"SGI", "TP9500"},
+	{"SGI", "TP9700"},
 	{"SGI", "IS"},
 	{"STK", "OPENstorage D280"},
 	{"SUN", "CSM200_R"},
@@ -815,6 +816,7 @@ static const struct scsi_dh_devlist rdac_dev_list[] = {
 	{"SUN", "CSM100_R_FC"},
 	{"SUN", "STK6580_6780"},
 	{"SUN", "SUN_6180"},
+	{"SUN", "ArrayStorage"},
 	{NULL, NULL},
 };
 
