75f43700839863d9e2cf3e9c4e815a3237a96171
Boojin Kim <boojin.kim@samsung.com>
2011-09-02 09:44:40 +0900

    ARM: SAMSUNG: Remove S3C-PL330-DMA driver
    Since DMA generic APIs can be used for Samsung DMA now so that
    the s3c-pl330 which includes Samsung specific DMA APIs can be
    removed.
    
    Signed-off-by: Boojin Kim <boojin.kim@samsung.com>
    Cc: Jassi Brar <jassisinghbrar@gmail.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- arch/arm/plat-samsung/include/plat/dma-pl330.h
+++ arch/arm/plat-samsung/include/plat/dma-pl330.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DMA_PL330_H_
 #define __DMA_PL330_H_ __FILE__
 
-#define S3C2410_DMAF_AUTOSTART		(1 << 0)
-#define S3C2410_DMAF_CIRCULAR		(1 << 1)
-
 /*
  * PL330 can assign any channel to communicate with
  * any of the peripherals attched to the DMAC.
@@ -89,6 +86,10 @@ enum dma_ch {
 	DMACH_MAX,
 };
 
+struct s3c2410_dma_client {
+	char	*name;
+};
+
 static inline bool s3c_dma_has_circular(void)
 {
 	return true;
@@ -98,6 +99,7 @@ static inline bool samsung_dma_is_dmadev(void)
 {
 	return true;
 }
-#include <plat/dma.h>
+
+#include <plat/dma-ops.h>
 
 #endif	/* __DMA_PL330_H_ */
