ae4827149dd1b7f57daa65b53a2c4ed96b3eeeaf
Boojin Kim <boojin.kim@samsung.com>
2011-09-02 09:44:35 +0900

    ARM: SAMSUNG: Add common DMA operations
    This patch adds common DMA operations which are used for Samsung DMA
    drivers. Currently there are two types of DMA driver for Samsung SoCs.
    The one is S3C-DMA for S3C SoCs and the other is PL330-DMA for S5P SoCs.
    This patch provides funcion pointers for common DMA operations to DMA
    client driver like SPI and Audio. It makes DMA client drivers support
    multi-platform.
    In addition, this common DMA operations implement the shared actions
    that are needed for DMA client driver. For example shared actions are
    filter() function for dma_request_channel() and parameter passing for
    device_prep_slave_sg().
    
    Signed-off-by: Boojin Kim <boojin.kim@samsung.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- arch/arm/mach-s3c2410/include/mach/dma.h
+++ arch/arm/mach-s3c2410/include/mach/dma.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_ARCH_DMA_H
 #define __ASM_ARCH_DMA_H __FILE__
 
-#include <plat/dma.h>
 #include <linux/sysdev.h>
 
 #define MAX_DMA_TRANSFER_SIZE   0x100000 /* Data Unit is half word  */
@@ -51,6 +50,13 @@ enum dma_ch {
 	DMACH_UART3_SRC2,
 	DMACH_MAX,		/* the end entry */
 };
+
+static inline bool samsung_dma_is_dmadev(void)
+{
+	return false;
+}
+
+#include <plat/dma.h>
 
 #define DMACH_LOW_LEVEL	(1<<28)	/* use this to specifiy hardware ch no */
 
