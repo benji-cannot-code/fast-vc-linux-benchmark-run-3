cb7e13a9e29b3654ce10ca893ff8059feee428d3
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-09-02 22:26:55 -0400

    ARM: switch from NO_MACH_MEMORY_H to NEED_MACH_MEMORY_H
    Given that we want the default to not have any <mach/memory.h> and given
    that there are now fewer cases where it is still provided than the cases
    where it is not at this point, this makes sense to invert the logic and
    just identify the exception cases.
    
    The word "need" instead of "have" was chosen to construct the config
    symbol so not to suggest that having a mach/memory.h file is actually
    a feature that one should aim for.
    
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/include/asm/memory.h
+++ arch/arm/include/asm/memory.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/sizes.h>
 
-#ifndef CONFIG_NO_MACH_MEMORY_H
+#ifdef CONFIG_NEED_MACH_MEMORY_H
 #include <mach/memory.h>
 #endif
 
