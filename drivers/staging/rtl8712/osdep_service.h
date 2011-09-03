99e9cb0640c39e367755474545900f81a5795174
Ali Bahar <ali@internetdog.org>
2011-09-04 03:14:08 +0800

    staging: r8712u: Merging Realtek's latest (v2.6.6). Removed _usb_submit_urb.
    Replaced (Realtek's) _usb_submit_urb by (linux's own) usb_submit_urb.
    
    Signed-off-by: Ali Bahar <ali@internetDog.org>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8712/osdep_service.h
+++ drivers/staging/rtl8712/osdep_service.h
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ethernet.h"
 #include <linux/if_arp.h>
 #include <linux/firmware.h>
-#define   _usb_submit_urb(x, y)     usb_submit_urb(x, y)
 
 struct	__queue	{
 	struct	list_head	queue;
