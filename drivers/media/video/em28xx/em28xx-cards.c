d30b8e611c3464286c30cbae6f55489972c7de12
Chris Rankin <rankincj@yahoo.com>
2011-09-04 15:26:21 -0300

    [media] em28xx: Fix em28xx_devused cleanup logic on error
    On 04/09/11 00:49, Mauro Carvalho Chehab wrote:
    > This is an automatic generated email to let you know that the following patch were queued at the
    > http://git.linuxtv.org/media_tree.git tree:
    >
    > Subject: [media] em28xx: use atomic bit operations for devices-in-use mask
    > Author:  Chris Rankin<rankincj@yahoo.com>
    > Date:    Sat Aug 20 08:21:03 2011 -0300
    >
    > Use atomic bit operations for the em28xx_devused mask, to prevent an
    > unlikely race condition should two adapters be plugged in
    > simultaneously. The operations also clearer than explicit bit
    > manipulation anyway.
    >
    > Signed-off-by: Chris Rankin<rankincj@yahoo.com>
    > Signed-off-by: Mauro Carvalho Chehab<mchehab@redhat.com>
    >
    >   drivers/media/video/em28xx/em28xx-cards.c |   33 ++++++++++++++---------------
    
    I think you missed this line in the merge.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx-cards.c
+++ drivers/media/video/em28xx/em28xx-cards.c
@@ -3115,7 +3115,6 @@ static int em28xx_usb_probe(struct usb_interface *interface,
 				em28xx_err(DRIVER_NAME " This is an anciliary "
 					"interface not used by the driver\n");
 
-				em28xx_devused &= ~(1<<nr);
 				retval = -ENODEV;
 				goto err;
 			}
