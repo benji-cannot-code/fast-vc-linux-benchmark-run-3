89671547a63136873699def2848c438e1c009214
Mark Einon <mark.einon@gmail.com>
2011-09-04 11:24:36 +0100

    staging: et131x: remove calls to netif_carrier_[on|off] from et131x_mii_check
    As et131x_adjust_link is called from the phydev in response to a netif_carrier_[on|off], and is the only caller of et131x_mii_check, we don't need to call netif_carrier_[on|off] again.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/et131x/et1310_phy.c
+++ drivers/staging/et131x/et1310_phy.c
@@ -453,7 +453,6 @@ void et131x_mii_check(struct et131x_adapter *adapter,
 	if (bmsr_ints & BMSR_LSTATUS) {
 		if (bmsr & BMSR_LSTATUS) {
 			adapter->boot_coma = 20;
-			netif_carrier_on(adapter->netdev);
 		} else {
 			dev_warn(&adapter->pdev->dev,
 			    "Link down - cable problem ?\n");
@@ -475,8 +474,6 @@ void et131x_mii_check(struct et131x_adapter *adapter,
 						 register18 | 511);
 				et131x_mii_write(adapter, 0x12, register18);
 			}
-
-			netif_carrier_off(adapter->netdev);
 
 			/* Free the packets being actively sent & stopped */
 			et131x_free_busy_send_packets(adapter);
