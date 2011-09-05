5a3a90b1376b05c52b84d4ced0045a50c12801e1
Ohad Ben-Cohen <ohad@wizery.com>
2011-09-05 16:42:36 +0300

    hwspinlock/core: simplify 'owner' handling
    Use struct device_driver's owner member instead of asking drivers to
    explicitly pass the owner again.
    
    This simplifies drivers and also save some memory, since there's no
    point now in maintaining a separate owner pointer per hwspinlock.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>

--- drivers/hwspinlock/omap_hwspinlock.c
+++ drivers/hwspinlock/omap_hwspinlock.c
@@ -144,7 +144,6 @@ static int __devinit omap_hwspinlock_probe(struct platform_device *pdev)
 		}
 
 		omap_lock->lock.dev = &pdev->dev;
-		omap_lock->lock.owner = THIS_MODULE;
 		omap_lock->lock.id = i;
 		omap_lock->lock.ops = &omap_hwspinlock_ops;
 		omap_lock->addr = io_base + LOCK_BASE_OFFSET + sizeof(u32) * i;
@@ -209,6 +208,7 @@ static struct platform_driver omap_hwspinlock_driver = {
 	.remove		= omap_hwspinlock_remove,
 	.driver		= {
 		.name	= "omap_hwspinlock",
+		.owner	= THIS_MODULE,
 	},
 };
 
