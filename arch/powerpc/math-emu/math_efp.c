c238876bf8a075214241c32ca248fc9599224dae
Liu Yu <yu.liu@freescale.com>
2011-09-05 17:01:23 +0800

    powerpc/math_emu/efp: Look for errata handler when type mismatches
    We already have cpu a005 errata handler when instruction cannot be
    recognized.  Before we lookup the inst, there's type checking, and we also
    need to handle it in errata handler when the type checking failed.
    
    Signed-off-by: Liu Yu <yu.liu@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/math-emu/math_efp.c
+++ arch/powerpc/math-emu/math_efp.c
@@ -172,10 +172,6 @@ static unsigned long insn_type(unsigned long speinsn)
 	case EFDNABS:	ret = XA;	break;
 	case EFDNEG:	ret = XA;	break;
 	case EFDSUB:	ret = AB;	break;
-
-	default:
-		printk(KERN_ERR "\nOoops! SPE instruction no type found.");
-		printk(KERN_ERR "\ninst code: %08lx\n", speinsn);
 	}
 
 	return ret;
@@ -196,7 +192,7 @@ int do_spe_mathemu(struct pt_regs *regs)
 
 	type = insn_type(speinsn);
 	if (type == NOTYPE)
-		return -ENOSYS;
+		goto illegal;
 
 	func = speinsn & 0x7ff;
 	fc = (speinsn >> 21) & 0x1f;
