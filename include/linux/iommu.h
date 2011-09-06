f2ed7a02bba0eb7803e39ec7ef5a6d3374575dae
Joerg Roedel <joerg.roedel@amd.com>
2011-09-06 16:48:40 +0200

    iommu/core: Define iommu_ops and register_iommu only with CONFIG_IOMMU_API
    This makes it impossible to compile an iommu driver into the
    kernel without selecting CONFIG_IOMMU_API.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -35,6 +35,8 @@ struct iommu_domain {
 #define IOMMU_CAP_CACHE_COHERENCY	0x1
 #define IOMMU_CAP_INTR_REMAP		0x2	/* isolates device intrs */
 
+#ifdef CONFIG_IOMMU_API
+
 struct iommu_ops {
 	int (*domain_init)(struct iommu_domain *domain);
 	void (*domain_destroy)(struct iommu_domain *domain);
@@ -49,8 +51,6 @@ struct iommu_ops {
 	int (*domain_has_cap)(struct iommu_domain *domain,
 			      unsigned long cap);
 };
-
-#ifdef CONFIG_IOMMU_API
 
 extern void register_iommu(struct iommu_ops *ops);
 extern bool iommu_found(void);
@@ -71,9 +71,7 @@ extern int iommu_domain_has_cap(struct iommu_domain *domain,
 
 #else /* CONFIG_IOMMU_API */
 
-static inline void register_iommu(struct iommu_ops *ops)
-{
-}
+struct iommu_ops {};
 
 static inline bool iommu_found(void)
 {
