59cfb5d9e6ba41d6316752ff99f45c8c2647603f
David Herrmann <dh.herrmann@googlemail.com>
2011-09-06 13:50:40 +0200

    HID: wiimote: Add MAINTAINERS entry
    Add entry to MAINTAINERS and also bump version level as the core driver is
    feature complete now.
    
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-wiimote.c
+++ drivers/hid/hid-wiimote.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include "hid-ids.h"
 
-#define WIIMOTE_VERSION "0.1"
+#define WIIMOTE_VERSION "0.2"
 #define WIIMOTE_NAME "Nintendo Wii Remote"
 #define WIIMOTE_BUFSIZE 32
 
