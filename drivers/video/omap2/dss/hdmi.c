d5b40860bafc668b35d87c14f719c7b8f0829ccb
Mythri P K <mythripk@ti.com>
2011-09-08 19:06:21 +0530

    OMAP4: DSS2: HDMI: Move HDMI IP independent generic header
    Some of the header file definitions that are there in the hdmi.h are generic
    and can be used across OMAP's, Thus moving generic definition to new file.
    
    Signed-off-by: Mythri P K <mythripk@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm_params.h>
 #endif
 
+#include "ti_hdmi.h"
 #include "dss.h"
 #include "hdmi.h"
 #include "dss_features.h"
