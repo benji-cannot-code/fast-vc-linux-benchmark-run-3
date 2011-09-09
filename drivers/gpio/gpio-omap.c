f4d7e1cb7edc6ef6e90dead56154e473ae7819e0
Nishanth Menon <nm@ti.com>
2011-09-09 19:08:58 +0530

    gpio/omap: save and restore debounce registers
    GPIO debounce registers need to be saved and restored for proper functioning
    of driver.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    tarun.kanti@ti.com: Debounce context save is moved to _set_gpio_debounce()
    as part of dynamic context save to remove overhead.
    Signed-off-by: Tarun Kanti DebBarma <tarun.kanti@ti.com>
    Reviewed-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- drivers/gpio/gpio-omap.c
+++ drivers/gpio/gpio-omap.c
@@ -45,6 +45,8 @@ struct gpio_regs {
 	u32 risingdetect;
 	u32 fallingdetect;
 	u32 dataout;
+	u32 debounce;
+	u32 debounce_en;
 };
 
 struct gpio_bank {
@@ -230,6 +232,10 @@ static void _set_gpio_debounce(struct gpio_bank *bank, unsigned gpio,
 	 * that point. Therefore we have to enable dbck here.
 	 */
 	_gpio_dbck_enable(bank);
+	if (bank->dbck_enable_mask) {
+		bank->context.debounce = debounce;
+		bank->context.debounce_en = val;
+	}
 }
 
 static inline void set_gpio_trigger(struct gpio_bank *bank, int gpio,
@@ -1364,6 +1370,12 @@ static void omap_gpio_restore_context(struct gpio_bank *bank)
 	__raw_writel(bank->context.fallingdetect,
 				bank->base + bank->regs->fallingdetect);
 	__raw_writel(bank->context.dataout, bank->base + bank->regs->dataout);
+	if (bank->dbck_enable_mask) {
+		__raw_writel(bank->context.debounce, bank->base +
+					bank->regs->debounce);
+		__raw_writel(bank->context.debounce_en,
+					bank->base + bank->regs->debounce_en);
+	}
 }
 #endif /* CONFIG_PM_RUNTIME */
 #else
