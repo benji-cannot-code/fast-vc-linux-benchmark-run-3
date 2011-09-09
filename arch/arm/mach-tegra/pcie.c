f2e672d99a5e90b9f3b4b8c5103aa5dfbdf16147
Olof Johansson <olof@lixom.net>
2011-09-08 18:11:24 -0700

    ARM: tegra: pcie: include board.h
    Fixes:
    
    arch/arm/mach-tegra/pcie.c:908:12: warning: symbol 'tegra_pcie_init' was not declared. Should it be static?
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-tegra/pcie.c
+++ arch/arm/mach-tegra/pcie.c
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/clk.h>
 #include <mach/powergate.h>
 
+#include "board.h"
+
 /* register definitions */
 #define AFI_OFFSET	0x3800
 #define PADS_OFFSET	0x3000
