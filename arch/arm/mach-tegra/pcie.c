7d23e5e821c4c61c476058b1f663ed452e90b9c3
Olof Johansson <olof@lixom.net>
2011-09-08 18:09:54 -0700

    ARM: tegra: pcie: don't cast __iomem pointers
    Fixes a lot of:
    
    arch/arm/mach-tegra/pcie.c:678:8: warning: cast removes address space of expression
    arch/arm/mach-tegra/pcie.c:678:8: warning: incorrect type in argument 1 (different base types)
    arch/arm/mach-tegra/pcie.c:678:8:    expected void const volatile [noderef] <asn:2>*<noident>
    arch/arm/mach-tegra/pcie.c:678:8:    got unsigned int
    arch/arm/mach-tegra/pcie.c:678:8: warning: cast removes address space of expression
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-tegra/pcie.c
+++ arch/arm/mach-tegra/pcie.c
@@ -151,9 +151,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static void __iomem *reg_pmc_base = IO_ADDRESS(TEGRA_PMC_BASE);
 
 #define pmc_writel(value, reg) \
-	__raw_writel(value, (u32)reg_pmc_base + (reg))
+	__raw_writel(value, reg_pmc_base + (reg))
 #define pmc_readl(reg) \
-	__raw_readl((u32)reg_pmc_base + (reg))
+	__raw_readl(reg_pmc_base + (reg))
 
 /*
  * Tegra2 defines 1GB in the AXI address map for PCIe.
