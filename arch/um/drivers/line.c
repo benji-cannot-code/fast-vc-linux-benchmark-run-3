9d5cf535b038ae6563555fc415a7155d22080944
Al Viro <viro@zeniv.linux.org.uk>
2011-09-10 08:17:04 -0400

    um: race fix: initialize delayed_work *before* registering IRQ
    ... since chan_interrupt() might schedule it if there's too much
    incoming data.  Kill task argument of chan_interrupt(), while
    we are at it - it's always &line->task.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/line.c
+++ arch/um/drivers/line.c
@@ -22,17 +22,8 @@ static irqreturn_t line_interrupt(int irq, void *data)
 	struct line *line = chan->line;
 
 	if (line)
-		chan_interrupt(line, &line->task, line->tty, irq);
+		chan_interrupt(line, line->tty, irq);
 	return IRQ_HANDLED;
-}
-
-static void line_timer_cb(struct work_struct *work)
-{
-	struct line *line = container_of(work, struct line, task.work);
-
-	if (!line->throttled)
-		chan_interrupt(line, &line->task, line->tty,
-			       line->driver->read_irq);
 }
 
 /*
@@ -328,8 +319,7 @@ void line_unthrottle(struct tty_struct *tty)
 	struct line *line = tty->driver_data;
 
 	line->throttled = 0;
-	chan_interrupt(line, &line->task, tty,
-		       line->driver->read_irq);
+	chan_interrupt(line, tty, line->driver->read_irq);
 
 	/*
 	 * Maybe there is enough stuff pending that calling the interrupt
@@ -424,8 +414,6 @@ int line_open(struct line *lines, struct tty_struct *tty)
 	err = enable_chan(line);
 	if (err) /* line_close() will be called by our caller */
 		goto out_unlock;
-
-	INIT_DELAYED_WORK(&line->task, line_timer_cb);
 
 	if (!line->sigio) {
 		chan_enable_winch(line->chan_out, tty);
