7252fc23c27c30021bb6ec96186f24aa57e16d70
Al Viro <viro@zeniv.linux.org.uk>
2011-09-09 20:20:52 -0400

    um: line->have_irq is never checked...
    looks like a half-arsed duplicate of line->enabled
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/line.h
+++ arch/um/drivers/line.h
@@ -57,7 +57,6 @@ struct line {
 	int sigio;
 	struct delayed_work task;
 	const struct line_driver *driver;
-	int have_irq;
 };
 
 extern void line_close(struct tty_struct *tty, struct file * filp);
