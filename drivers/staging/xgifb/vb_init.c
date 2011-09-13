ccb5651ed9c451e5acdf4eb2c1940ea660d22678
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-09-13 22:49:32 +0300

    staging: xgifb: unify #include order
    Include Linux headers before driver's own headers.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_init.c
+++ drivers/staging/xgifb/vb_init.c
@@ -1,8 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#include "vgatypes.h"
-
 #include <linux/types.h>
 #include <linux/delay.h> /* udelay */
+#include "vgatypes.h"
 #include "XGIfb.h"
 
 #include "vb_def.h"
