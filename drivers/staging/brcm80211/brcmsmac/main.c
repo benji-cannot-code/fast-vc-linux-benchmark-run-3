1950f022eeee82fdd200924e6a11583a3bb02f0e
Arend van Spriel <arend@broadcom.com>
2011-09-13 09:49:43 +0200

    staging: brcm80211: remove function ieee_set_channel()
    The function ieee_set_channel() provides very little functionality
    so it has been removed.
    
    Reported-by: Johannes Berg <johannes@sipsolutions.net>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Roland Vossen <rvossen@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/main.c
+++ drivers/staging/brcm80211/brcmsmac/main.c
@@ -6289,14 +6289,8 @@ u16 brcms_c_get_phy_type(struct brcms_c_info *wlc, int phyidx)
 	return wlc->band->phytype;
 }
 
-int brcms_c_set_shortslot_override(struct brcms_c_info *wlc, s8 sslot_override)
+void brcms_c_set_shortslot_override(struct brcms_c_info *wlc, s8 sslot_override)
 {
-	if (sslot_override != BRCMS_SHORTSLOT_AUTO &&
-	    sslot_override != BRCMS_SHORTSLOT_OFF &&
-	    sslot_override != BRCMS_SHORTSLOT_ON) {
-		return -EINVAL;
-	}
-
 	wlc->shortslot_override = sslot_override;
 
 	/*
@@ -6304,7 +6298,7 @@ int brcms_c_set_shortslot_override(struct brcms_c_info *wlc, s8 sslot_override)
 	 * currently on the 5G band
 	 */
 	if (wlc->band->bandtype == BRCM_BAND_5G)
-		return 0;
+		return;
 
 	if (wlc->pub->up && wlc->pub->associated) {
 		/* let watchdog or beacon processing update shortslot */
@@ -6321,7 +6315,6 @@ int brcms_c_set_shortslot_override(struct brcms_c_info *wlc, s8 sslot_override)
 			    (wlc->shortslot_override ==
 			     BRCMS_SHORTSLOT_ON);
 	}
-	return 0;
 }
 
 /*
