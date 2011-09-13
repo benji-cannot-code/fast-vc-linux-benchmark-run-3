ac192ff8d3c1bec0172547992618f30bac3bf98c
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-09-13 22:49:34 +0300

    staging: xgifb: add missing #include
    Add a missing include to eliminate the following sparse warning:
    
    drivers/staging/xgifb/vb_table.h:2572:28: warning: symbol 'XGI21_LCDCapList' was not declared. Should it be static?
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_setmode.c
+++ drivers/staging/xgifb/vb_setmode.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vb_def.h"
 #include "vgatypes.h"
 #include "vb_struct.h"
+#include "vb_init.h"
 #include "vb_util.h"
 #include "vb_table.h"
 #include "vb_setmode.h"
