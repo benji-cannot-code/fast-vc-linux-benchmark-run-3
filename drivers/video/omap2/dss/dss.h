4ea61dc64e8bb14f7ddeebfa492101d825597a5f
Archit Taneja <archit@ti.com>
2011-09-13 18:28:41 +0530

    OMAPDSS: DISPC: Get correct pixel clock for TV manager
    dispc_mgr_pclk_rate() is used to calculate minimum required functional clock for
    scaling in calc_fclk() and calc_fclk_five_taps(). This function returns the
    correct pixel clock for LCD and LCD2 managers, but not for TV manager. Extend
    this function so that it gets the correct pixel clock for TV manager.
    
    This also prevents the crash we get when we try to scale overlays connected to
    TV manager. The current code leads to a BUG() being executed if we call
    dispc_mgr_pclk_rate() for the TV manager.
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dss.h
+++ drivers/video/omap2/dss/dss.h
@@ -451,6 +451,7 @@ int venc_init_platform_driver(void);
 void venc_uninit_platform_driver(void);
 void venc_dump_regs(struct seq_file *s);
 int venc_init_display(struct omap_dss_device *display);
+unsigned long venc_get_pixel_clock(void);
 #else
 static inline int venc_init_platform_driver(void)
 {
@@ -459,6 +460,11 @@ static inline int venc_init_platform_driver(void)
 static inline void venc_uninit_platform_driver(void)
 {
 }
+static inline unsigned long venc_get_pixel_clock(void)
+{
+	WARN("%s: VENC not compiled in, returning pclk as 0\n", __func__);
+	return 0;
+}
 #endif
 
 /* HDMI */
@@ -466,6 +472,7 @@ static inline void venc_uninit_platform_driver(void)
 int hdmi_init_platform_driver(void);
 void hdmi_uninit_platform_driver(void);
 int hdmi_init_display(struct omap_dss_device *dssdev);
+unsigned long hdmi_get_pixel_clock(void);
 #else
 static inline int hdmi_init_display(struct omap_dss_device *dssdev)
 {
@@ -477,6 +484,11 @@ static inline int hdmi_init_platform_driver(void)
 }
 static inline void hdmi_uninit_platform_driver(void)
 {
+}
+static inline unsigned long hdmi_get_pixel_clock(void)
+{
+	WARN("%s: HDMI not compiled in, returning pclk as 0\n", __func__);
+	return 0;
 }
 #endif
 int omapdss_hdmi_display_enable(struct omap_dss_device *dssdev);
