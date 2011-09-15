b58410dd0c5e7b90582a615401591403aa85a64f
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2011-09-15 11:46:50 -0700

    iwlagn: add support for v2 of temperature offset calibration
    For 2000 series of NICs, version 2 of temperature offset calibration
    should be used.
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-5000-hw.h
+++ drivers/net/wireless/iwlwifi/iwl-5000-hw.h
@@ -75,8 +75,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static inline s32 iwl_temp_calib_to_offset(struct iwl_priv *priv)
 {
 	u16 temperature, voltage;
-	__le16 *temp_calib =
-		(__le16 *)iwl_eeprom_query_addr(priv, EEPROM_TEMPERATURE);
+	__le16 *temp_calib = (__le16 *)iwl_eeprom_query_addr(priv,
+				EEPROM_KELVIN_TEMPERATURE);
 
 	temperature = le16_to_cpu(temp_calib[0]);
 	voltage = le16_to_cpu(temp_calib[1]);
