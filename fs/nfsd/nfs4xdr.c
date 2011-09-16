865ed8540916fd9f2c3beba3370681fd9c0d127f
J. Bruce Fields <bfields@redhat.com>
2011-09-16 17:42:48 -0400

    nfsd4: match close replays on stateid, not open owner id
    Keep around an unhashed copy of the final stateid after the last close
    using an openowner, and when identifying a replay, match against that
    stateid instead of just against the open owner id.  Free it the next
    time the seqid is bumped or the stateowner is destroyed.
    
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4xdr.c
+++ fs/nfsd/nfs4xdr.c
@@ -1637,6 +1637,7 @@ static void encode_seqid_op_tail(struct nfsd4_compoundres *resp, __be32 *save, _
 			  (char *)resp->p - (char *)save;
 		memcpy(stateowner->so_replay.rp_buf, save,
 			stateowner->so_replay.rp_buflen);
+		nfsd4_purge_closed_stateid(stateowner);
 	}
 }
 
