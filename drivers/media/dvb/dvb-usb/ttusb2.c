0124fcaa2df03f24bf66ec3e27402023712e46bc
Michael Krufky <mkrufky@kernellabs.com>
2011-09-16 09:36:56 -0300

    [media] ttusb2: fix ERROR: space required after that ','
    Signed-off-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/ttusb2.c
+++ drivers/media/dvb/dvb-usb/ttusb2.c
@@ -473,7 +473,7 @@ static struct dvb_usb_device_properties ttusb2_properties_ct3650 = {
 					}
 				}
 			}
-		},{
+		}, {
 			.streaming_ctrl   = NULL,
 
 			.frontend_attach  = ttusb2_frontend_tda10023_attach,
