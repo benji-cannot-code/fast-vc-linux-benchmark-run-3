387d88eb14246a40ab9b064283b059a181e8a5a4
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-09-18 13:21:27 -0400

    net: add moduleparam.h for users of module_param/MODULE_PARM_DESC
    These files were getting access to these two via the implicit
    presence of module.h everywhere.  They aren't modules, so they
    don't need the full module.h inclusion though.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/nl80211.h>
 #include <linux/platform_device.h>
+#include <linux/moduleparam.h>
 #include <net/cfg80211.h>
 #include "core.h"
 #include "reg.h"
