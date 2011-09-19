1e559b7521a813f081f654f666ade6193b673120
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2011-09-19 17:44:52 +0000

    powerpc: Add skeleton PowerNV platform
    This adds a skeletton for the new Power "Non Virtualized"
    platform which will be used by machines supporting running
    without an hypervisor, for example in order to run KVM.
    
    These machines will be using a new firmware called OPAL
    for which the support will be provided by later patches.
    
    The PowerNV platform is intended to be also usable under
    the BML environment used internally for early CPU bringup
    which is why the code also supports using RTAS instead of
    OPAL in various places.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

---
+++ arch/powerpc/platforms/powernv/powernv.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _POWERNV_H
+#define _POWERNV_H
+
+#ifdef CONFIG_SMP
+extern void pnv_smp_init(void);
+#else
+static inline void pnv_smp_init(void) { }
+#endif
+
+#endif /* _POWERNV_H */
