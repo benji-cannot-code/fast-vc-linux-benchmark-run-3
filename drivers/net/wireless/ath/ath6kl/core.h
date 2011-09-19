32a2a76d3854b64bd99709f997d016af78f469bb
Jouni Malinen <jouni@qca.qualcomm.com>
2011-09-19 19:15:07 +0300

    ath6kl: Export beacon interval and DTIM period through STA info
    Now that we allow the first Beacon frame after each connection to
    be processed at the host, we can figure out the DTIM period and
    expose it with Beacon interval through STA info BSS parameters
    to user space.
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/core.h
+++ drivers/net/wireless/ath/ath6kl/core.h
@@ -396,6 +396,7 @@ struct ath6kl_req_key {
 #define WLAN_ENABLED	     12
 #define TESTMODE	     13
 #define CLEAR_BSSFILTER_ON_BEACON 14
+#define DTIM_PERIOD_AVAIL    15
 
 struct ath6kl {
 	struct device *dev;
@@ -512,6 +513,8 @@ struct ath6kl {
 	u16 next_chan;
 
 	bool p2p;
+	u16 assoc_bss_beacon_int;
+	u8 assoc_bss_dtim_period;
 
 #ifdef CONFIG_ATH6KL_DEBUG
 	struct {
