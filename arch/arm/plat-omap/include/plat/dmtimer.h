5a7c97d0baba0830c2471b49eec0dbbde16960e0
Tarun Kanti DebBarma <tarun.kanti@ti.com>
2011-09-20 17:00:19 +0530

    ARM: OMAP: dmtimer: platform driver
    Add dmtimer platform driver functions which include:
    (1) platform driver initialization
    (2) driver probe function
    (3) driver remove function
    
    Signed-off-by: Tarun Kanti DebBarma <tarun.kanti@ti.com>
    Signed-off-by: Thara Gopinath <thara@ti.com>
    Reviewed-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Acked-by: Cousson, Benoit <b-cousson@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/dmtimer.h
+++ arch/arm/plat-omap/include/plat/dmtimer.h
@@ -230,6 +230,7 @@ int omap_dm_timers_active(void);
 
 struct omap_dm_timer {
 	unsigned long phys_base;
+	int id;
 	int irq;
 #ifdef CONFIG_ARCH_OMAP2PLUS
 	struct clk *iclk, *fclk;
@@ -247,6 +248,7 @@ struct omap_dm_timer {
 	unsigned enabled:1;
 	unsigned posted:1;
 	struct platform_device *pdev;
+	struct list_head node;
 };
 
 extern u32 sys_timer_reserved;
