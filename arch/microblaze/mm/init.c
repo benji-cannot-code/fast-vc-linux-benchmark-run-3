6ebbf6479a5a2afed7b80f23467e77245394742a
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-09-22 11:22:55 -0400

    microblaze: Add export.h to arch/microblaze files as required
    For access to the EXPORT_SYMBOL variants and THIS_MODULE macros.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/microblaze/mm/init.c
+++ arch/microblaze/mm/init.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pfn.h>
 #include <linux/slab.h>
 #include <linux/swap.h>
+#include <linux/export.h>
 
 #include <asm/page.h>
 #include <asm/mmu_context.h>
