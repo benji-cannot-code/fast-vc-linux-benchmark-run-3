7ecc6a9538a79a434e3ca38a60e91bb9577a1aa8
Mythri P K <mythripk@ti.com>
2011-09-22 13:37:43 +0530

    OMAPDSS: HDMI: Replace hdmi_reg struct with u16
    Remove usage of hdmi_reg struct to use u16 instead in the HDMI IP header
    file. hdmi_reg struct is not really needed, and the same change was also
    made for dispc earlier.
    
    Signed-off-by: Mythri P K <mythripk@ti.com>
    [tomi.valkeinen@ti.com: updated the description]
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/ti_hdmi_4xxx_ip.c
+++ drivers/video/omap2/dss/ti_hdmi_4xxx_ip.c
@@ -33,15 +33,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dss.h"
 
 static inline void hdmi_write_reg(void __iomem *base_addr,
-				const struct hdmi_reg idx, u32 val)
+				const u16 idx, u32 val)
 {
-	__raw_writel(val, base_addr + idx.idx);
+	__raw_writel(val, base_addr + idx);
 }
 
 static inline u32 hdmi_read_reg(void __iomem *base_addr,
-				const struct hdmi_reg idx)
+				const u16 idx)
 {
-	return __raw_readl(base_addr + idx.idx);
+	return __raw_readl(base_addr + idx);
 }
 
 static inline void __iomem *hdmi_wp_base(struct hdmi_ip_data *ip_data)
@@ -70,7 +70,7 @@ static inline void __iomem *hdmi_core_sys_base(struct hdmi_ip_data *ip_data)
 }
 
 static inline int hdmi_wait_for_bit_change(void __iomem *base_addr,
-				const struct hdmi_reg idx,
+				const u16 idx,
 				int b2, int b1, u32 val)
 {
 	u32 t = 0;
