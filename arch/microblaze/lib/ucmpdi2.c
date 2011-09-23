faec68e10a5072066bd314a1ea4d9f8baff0023b
Michal Simek <monstr@monstr.eu>
2011-09-23 08:46:24 +0200

    microblaze: Add __ucmpdi2() helper function
    Add missing __ucmpdi2 helper function.
    
    Error log:
    kernel/built-in.o: In function `print_graph_duration':
    : undefined reference to `__ucmpdi2'
    kernel/built-in.o: In function `print_graph_duration':
    : undefined reference to `__ucmpdi2'
    
    Based on MIPS code.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

---
+++ arch/microblaze/lib/ucmpdi2.c
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <linux/module.h>
+
+#include "libgcc.h"
+
+word_type __ucmpdi2(unsigned long long a, unsigned long long b)
+{
+	const DWunion au = {.ll = a};
+	const DWunion bu = {.ll = b};
+
+	if ((unsigned int) au.s.high < (unsigned int) bu.s.high)
+		return 0;
+	else if ((unsigned int) au.s.high > (unsigned int) bu.s.high)
+		return 2;
+	if ((unsigned int) au.s.low < (unsigned int) bu.s.low)
+		return 0;
+	else if ((unsigned int) au.s.low > (unsigned int) bu.s.low)
+		return 2;
+	return 1;
+}
+EXPORT_SYMBOL(__ucmpdi2);
