1b5f9fff6240a7e578dc7bea05d92d942e7bfdc4
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-09-23 16:23:11 +0100

    ASoC: Add support for on-board analogue microphones on Speyside WM8962
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/samsung/speyside_wm8962.c
+++ sound/soc/samsung/speyside_wm8962.c
@@ -142,6 +142,7 @@ static struct snd_soc_dapm_widget widgets[] = {
 	SND_SOC_DAPM_MIC("Headset Mic", NULL),
 
 	SND_SOC_DAPM_MIC("DMIC", NULL),
+	SND_SOC_DAPM_MIC("AMIC", NULL),
 
 	SND_SOC_DAPM_SPK("Main Speaker", NULL),
 };
@@ -156,6 +157,10 @@ static struct snd_soc_dapm_route audio_paths[] = {
 	{ "Headset Mic", NULL, "MICBIAS" },
 	{ "IN4L", NULL, "Headset Mic" },
 	{ "IN4R", NULL, "Headset Mic" },
+
+	{ "AMIC", NULL, "MICBIAS" },
+	{ "IN1L", NULL, "AMIC" },
+	{ "IN1R", NULL, "AMIC" },
 
 	{ "DMIC", NULL, "MICBIAS" },
 	{ "DMICDAT", NULL, "DMIC" },
