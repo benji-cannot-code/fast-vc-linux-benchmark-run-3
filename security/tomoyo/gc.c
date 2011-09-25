63b1531573f1252688b41ca3296ae10335751a03
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2011-09-25 17:51:06 +0900

    TOMOYO: Remove tomoyo_policy_memory_lock spinlock.
    tomoyo_policy_lock mutex already protects it.
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/tomoyo/gc.c
+++ security/tomoyo/gc.c
@@ -9,6 +9,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kthread.h>
 #include <linux/slab.h>
 
+/**
+ * tomoyo_memory_free - Free memory for elements.
+ *
+ * @ptr:  Pointer to allocated memory.
+ *
+ * Returns nothing.
+ *
+ * Caller holds tomoyo_policy_lock mutex.
+ */
+static inline void tomoyo_memory_free(void *ptr)
+{
+	tomoyo_memory_used[TOMOYO_MEMORY_POLICY] -= ksize(ptr);
+	kfree(ptr);
+}
+
 /* The list for "struct tomoyo_io_buffer". */
 static LIST_HEAD(tomoyo_io_buffer_list);
 /* Lock for protecting tomoyo_io_buffer_list. */
@@ -216,6 +231,8 @@ static void tomoyo_del_acl(struct list_head *element)
  * @element: Pointer to "struct list_head".
  *
  * Returns nothing.
+ *
+ * Caller holds tomoyo_policy_lock mutex.
  */
 static inline void tomoyo_del_domain(struct list_head *element)
 {
@@ -417,12 +434,13 @@ static void tomoyo_try_to_gc(const enum tomoyo_policy_id type,
 				(element, typeof(struct tomoyo_domain_info),
 				 list)->users))
 			goto reinject;
-		tomoyo_del_domain(element);
 		break;
 	case TOMOYO_MAX_POLICY:
 		break;
 	}
 	mutex_lock(&tomoyo_policy_lock);
+	if (type == TOMOYO_ID_DOMAIN)
+		tomoyo_del_domain(element);
 	tomoyo_memory_free(element);
 	return;
 reinject:
