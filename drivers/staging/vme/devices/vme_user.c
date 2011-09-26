8a5e80635085dc7494148fcf068be1243b807214
Manohar Vanga <manohar.vanga@cern.ch>
2011-09-26 11:27:18 +0200

    staging: vme_user: rename USER_BUS_MAX to VME_USER_BUS_MAX
    Signed-off-by: Manohar Vanga <manohar.vanga@cern.ch>
    Cc: Martyn Welch <martyn.welch@ge.com>
    Reviewed-by: Emilio G. Cota <cota@braap.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/vme/devices/vme_user.c
+++ drivers/staging/vme/devices/vme_user.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static DEFINE_MUTEX(vme_user_mutex);
 static const char driver_name[] = "vme_user";
 
-static int bus[USER_BUS_MAX];
+static int bus[VME_USER_BUS_MAX];
 static unsigned int bus_num;
 
 /* Currently Documentation/devices.txt defines the following for VME:
@@ -644,10 +644,10 @@ static int __init vme_user_init(void)
 	/* Let's start by supporting one bus, we can support more than one
 	 * in future revisions if that ever becomes necessary.
 	 */
-	if (bus_num > USER_BUS_MAX) {
+	if (bus_num > VME_USER_BUS_MAX) {
 		printk(KERN_ERR "%s: Driver only able to handle %d buses\n",
-			driver_name, USER_BUS_MAX);
-		bus_num = USER_BUS_MAX;
+			driver_name, VME_USER_BUS_MAX);
+		bus_num = VME_USER_BUS_MAX;
 	}
 
 	/*
@@ -669,7 +669,7 @@ err_nocard:
 
 static int vme_user_match(struct vme_dev *vdev)
 {
-	if (vdev->num >= USER_BUS_MAX)
+	if (vdev->num >= VME_USER_BUS_MAX)
 		return 0;
 	return 1;
 }
