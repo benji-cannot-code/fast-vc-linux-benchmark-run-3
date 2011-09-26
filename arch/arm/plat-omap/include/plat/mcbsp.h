383de8fa5418c301a9bb7c8019775748ba5d8fa6
Jarkko Nikula <jarkko.nikula@bitmer.com>
2011-09-26 10:45:40 +0300

    ARM: OMAP: mcbsp: Make wakeup control generic
    Currently wakeup control code is compiled only when CONFIG_ARCH_OMAP3 is
    set even it should be available for CONFIG_ARCH_OMAP4 only builds also.
    
    Fix this by making wakeup control generic so that it is executed whenever
    new feature flag has_wakeup in platform data is set. Currently flag is set
    for McBSP config types 3 and 4.
    
    Remove also old comments about idle mode settings and HW bug workarounds
    that were not updated during hwmod conversion.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Acked-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Tested-by: Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/mcbsp.h
+++ arch/arm/plat-omap/include/plat/mcbsp.h
@@ -318,6 +318,9 @@ struct omap_mcbsp_platform_data {
 	u16 buffer_size;
 	u8 reg_size;
 	u8 reg_step;
+
+	/* McBSP platform and instance specific features */
+	bool has_wakeup; /* Wakeup capability */
 };
 
 struct omap_mcbsp_st_data {
