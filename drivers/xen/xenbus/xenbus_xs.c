7a061fdda91da9b8c4b36cbddd7d787ce2c6894a
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-09-26 13:13:42 -0400

    xen/pv-on-hvm:kexec: Fix implicit declaration of function 'xen_hvm_domain'
    Randy found a compile error when using make randconfig to trigger
    
    drivers/xen/xenbus/xenbus_xs.c:909:2: error: implicit declaration of function 'xen_hvm_domain'
    
    it is unclear which of the CONFIG options triggered this. This
    patch fixes the error.
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/xenbus/xenbus_xs.c
+++ drivers/xen/xenbus/xenbus_xs.c
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <xen/xenbus.h>
+#include <xen/xen.h>
 #include "xenbus_comms.h"
 
 struct xs_stored_msg {
