446c8adce1d0b73eb6ba58916d4407efeb94ab1a
Jarkko Nikula <jarkko.nikula@bitmer.com>
2011-09-26 10:45:41 +0300

    ARM: OMAP: mcbsp: Make tranceiver configuration control register access generic
    McBSP transmit and receive configuration control registers must be set up
    for OMAP2430 and later. Replace is_omap tests in generic code with a new
    feature flag has_ccr in platform data so that there is no need to change
    code for any upcoming OMAP version.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Acked-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Tested-by: Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/mcbsp.h
+++ arch/arm/plat-omap/include/plat/mcbsp.h
@@ -321,6 +321,7 @@ struct omap_mcbsp_platform_data {
 
 	/* McBSP platform and instance specific features */
 	bool has_wakeup; /* Wakeup capability */
+	bool has_ccr; /* Transceiver has configuration control registers */
 };
 
 struct omap_mcbsp_st_data {
