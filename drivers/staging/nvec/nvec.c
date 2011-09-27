723fe68bb3815f265cabe11f7be47b6eb00cf932
Julian Andres Klode <jak@jak-linux.org>
2011-09-27 19:00:55 +0200

    staging: nvec: Have nvec_write_async() return -ENOMEM on OOM
    Change nvec_write_async() to return an integer, 0 by default,
    a negative error on failure. Change nvec_write_sync() to
    check the return value and abort if it is negative.
    
    Signed-off-by: Julian Andres Klode <jak@jak-linux.org>
    Acked-by: Marc Dietrich <marvin24@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/nvec/nvec.c
+++ drivers/staging/nvec/nvec.c
@@ -155,13 +155,16 @@ static void nvec_gpio_set_value(struct nvec_chip *nvec, int value)
 	gpio_set_value(nvec->gpio, value);
 }
 
-void nvec_write_async(struct nvec_chip *nvec, const unsigned char *data,
+int nvec_write_async(struct nvec_chip *nvec, const unsigned char *data,
 			short size)
 {
 	struct nvec_msg *msg;
 	unsigned long flags;
 
 	msg = nvec_msg_alloc(nvec);
+	if (msg == NULL)
+		return -ENOMEM;
+
 	msg->data[0] = size;
 	memcpy(msg->data + 1, data, size);
 	msg->size = size + 1;
@@ -171,6 +174,8 @@ void nvec_write_async(struct nvec_chip *nvec, const unsigned char *data,
 	spin_unlock_irqrestore(&nvec->tx_lock, flags);
 
 	queue_work(nvec->wq, &nvec->tx_work);
+
+	return 0;
 }
 EXPORT_SYMBOL(nvec_write_async);
 
@@ -182,7 +187,9 @@ struct nvec_msg *nvec_write_sync(struct nvec_chip *nvec,
 	mutex_lock(&nvec->sync_write_mutex);
 
 	nvec->sync_write_pending = (data[1] << 8) + data[0];
-	nvec_write_async(nvec, data, size);
+
+	if (nvec_write_async(nvec, data, size) < 0)
+		return NULL;
 
 	dev_dbg(nvec->dev, "nvec_sync_write: 0x%04x\n",
 					nvec->sync_write_pending);
