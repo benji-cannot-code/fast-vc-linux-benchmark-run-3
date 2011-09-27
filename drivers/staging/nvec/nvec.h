965349ad1010dee5cfd8e803fd28b7a94ca6f391
Julian Andres Klode <jak@jak-linux.org>
2011-09-27 19:00:39 +0200

    staging: nvec: Include missing headers
    Those headers were apparently included by other headers
    previously, but are not anymore.
    
    Signed-off-by: Julian Andres Klode <jak@jak-linux.org>
    Acked-by: Marc Dietrich <marvin24@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/nvec/nvec.h
+++ drivers/staging/nvec/nvec.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_MFD_NVEC
 #define __LINUX_MFD_NVEC
 
+#include <linux/notifier.h>
 #include <linux/semaphore.h>
 
 typedef enum {
