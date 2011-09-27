f49192a1fd922f2e3e3e07b3e6cfc17003348d6e
Jamie Iles <jamie@jamieiles.com>
2011-09-27 20:41:01 +0100

    ARM: u300: convert to MULTI_IRQ_HANDLER
    Now that there is a generic IRQ handler for multiple VIC devices use it
    for u300 to help building multi platform kernels.
    
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Jamie Iles <jamie@jamieiles.com>

--- arch/arm/mach-u300/u300.c
+++ arch/arm/mach-u300/u300.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <mach/hardware.h>
 #include <mach/platform.h>
+#include <asm/hardware/vic.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/memory.h>
@@ -50,6 +51,7 @@ MACHINE_START(U300, MACH_U300_STRING)
 	.atag_offset	= BOOT_PARAMS_OFFSET,
 	.map_io		= u300_map_io,
 	.init_irq	= u300_init_irq,
+	.handle_irq	= vic_handle_irq,
 	.timer		= &u300_timer,
 	.init_machine	= u300_init_machine,
 MACHINE_END
