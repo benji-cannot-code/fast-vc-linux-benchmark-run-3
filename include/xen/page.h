e17f990d425be88606c37c048984c7b81898e51a
David Vrabel <david.vrabel@citrix.com>
2011-09-28 17:46:32 +0100

    xen/balloon: account for pages released during memory setup
    In xen_memory_setup() pages that occur in gaps in the memory map are
    released back to Xen.  This reduces the domain's current page count in
    the hypervisor.  The Xen balloon driver does not correctly decrease
    its initial current_pages count to reflect this.  If 'delta' pages are
    released and the target is adjusted the resulting reservation is
    always 'delta' less than the requested target.
    
    This affects dom0 if the initial allocation of pages overlaps the PCI
    memory region but won't affect most domU guests that have been setup
    with pseudo-physical memory maps that don't have gaps.
    
    Fix this by accouting for the released pages when starting the balloon
    driver.
    
    If the domain's targets are managed by xapi, the domain may eventually
    run out of memory and die because xapi currently gets its target
    calculations wrong and whenever it is restarted it always reduces the
    target by 'delta'.
    
    Signed-off-by: David Vrabel <david.vrabel@citrix.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/page.h
+++ include/xen/page.h
@@ -6,4 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern phys_addr_t xen_extra_mem_start, xen_extra_mem_size;
 
+extern unsigned long xen_released_pages;
+
 #endif	/* _XEN_PAGE_H */
