06e614d982d06545636de903c22252c24ea481f5
Rafael J. Wysocki <rjw@sisk.pl>
2011-09-28 21:44:36 +0200

    PCI / PM: Remove unnecessary error variable from acpi_dev_run_wake()
    The result returned by acpi_dev_run_wake() is always either -EINVAL
    or -ENODEV, while obviously it should return 0 on success.  The
    problem is that the leftover error variable, that's not really used
    in the function, is initialized with -ENODEV and then returned
    without modification.
    
    To fix this issue remove the error variable from acpi_dev_run_wake()
    and make the function return 0 on success as appropriate.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/pci-acpi.c
+++ drivers/pci/pci-acpi.c
@@ -286,7 +286,6 @@ static int acpi_dev_run_wake(struct device *phys_dev, bool enable)
 {
 	struct acpi_device *dev;
 	acpi_handle handle;
-	int error = -ENODEV;
 
 	if (!device_run_wake(phys_dev))
 		return -EINVAL;
@@ -306,7 +305,7 @@ static int acpi_dev_run_wake(struct device *phys_dev, bool enable)
 		acpi_disable_wakeup_device_power(dev);
 	}
 
-	return error;
+	return 0;
 }
 
 static void acpi_pci_propagate_run_wake(struct pci_bus *bus, bool enable)
