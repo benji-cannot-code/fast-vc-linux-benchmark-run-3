94046b0e63af8680ce2b5020475dc8d33ac1c13a
Stephen Rothwell <sfr@canb.auug.org.au>
2011-09-28 18:32:34 +1000

    wireless/ath6kl: use of module_param requires the inclusion of moduleparam.h
    Otheriwse the module.h split up fails like this:
    
    drivers/net/wireless/ath/ath6kl/init.c:27:26: error: expected ')' before 'uint'
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/init.c
+++ drivers/net/wireless/ath/ath6kl/init.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <linux/moduleparam.h>
 #include <linux/of.h>
 #include <linux/mmc/sdio_func.h>
 #include "core.h"
