69354f42eadd0372eb210dae0f6bfab4c7ae9dfe
Stephen Rothwell <sfr@canb.auug.org.au>
2011-09-28 17:42:14 +1000

    x86, amd: Include linux/elf.h since we use stuff from asm/elf.h
    After merging the moduleh tree, today's linux-next build (x86_64
    allmodconfig) failed like this:
    
      arch/x86/kernel/sys_x86_64.c:28:10: warning: 'enum align_flags' declared inside parameter list
      arch/x86/kernel/sys_x86_64.c:28:10: warning: its scope is only this definition or declaration, which is probably not what you
      want arch/x86/kernel/sys_x86_64.c:28:22: error: parameter 3 ('flags') has incomplete type
      [...]
    
    Presumably caused by the module.h split interacting with a
    new commit dfb09f9b7ab0 ("x86, amd: Avoid cache aliasing penalties
    on AMD family 15h") from the x8 tree.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Borislav Petkov <borislav.petkov@amd.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Paul Gortmaker <paul.gortmaker@windriver.com>
    Link: http://lkml.kernel.org/r/20110928174214.17a58be15d84d67c185930e1@canb.auug.org.au
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/sys_x86_64.c
+++ arch/x86/kernel/sys_x86_64.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/personality.h>
 #include <linux/random.h>
 #include <linux/uaccess.h>
+#include <linux/elf.h>
 
 #include <asm/ia32.h>
 #include <asm/syscalls.h>
