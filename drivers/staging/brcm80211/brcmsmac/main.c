dd1e17aa6fc6f4ad2be2e74c2bc7ee1303103291
Arend van Spriel <arend@broadcom.com>
2011-09-29 15:34:12 -0700

    staging: brcm80211: remove uncoditional code blocks from brcmsmac
    Using a block statement to scope function variables is not
    common in linux kernel development. Browsed through the brcmsmac
    to remove those.
    
    Reported-by: Johannes Berg <johannes@sipsolutions.net>
    Reviewed-by: Roland Vossen <rvossen@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmsmac/main.c
+++ drivers/staging/brcm80211/brcmsmac/main.c
@@ -5077,7 +5077,7 @@ brcms_c_attach(struct brcms_info *wl, u16 vendor, u16 device, uint unit,
 {
 	struct brcms_c_info *wlc;
 	uint err = 0;
-	uint j;
+	uint i, j;
 	struct brcms_pub *pub;
 	uint n_disabled;
 
@@ -5150,11 +5150,8 @@ brcms_c_attach(struct brcms_info *wl, u16 vendor, u16 device, uint unit,
 			       wlc->stf->hw_rxchain);
 
 	/* pull up some info resulting from the low attach */
-	{
-		int i;
-		for (i = 0; i < NFIFO; i++)
-			wlc->core->txavail[i] = wlc->hw->txavail[i];
-	}
+	for (i = 0; i < NFIFO; i++)
+		wlc->core->txavail[i] = wlc->hw->txavail[i];
 
 	brcms_b_hw_etheraddr(wlc->hw, wlc->perm_etheraddr);
 
