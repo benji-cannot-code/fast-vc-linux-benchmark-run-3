9054d21a3573e06f1cc0a4ef6351fbbf66c3d804
Roland Vossen <rvossen@broadcom.com>
2011-10-04 23:18:52 +0200

    staging: brcm80211: fullmac: fixed double #include
    Fix double include as detected by checkincludes script.
    
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
+++ drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/kthread.h>
 #include <linux/netdevice.h>
-#include <linux/sched.h>
 #include <linux/bitops.h>
 #include <linux/etherdevice.h>
 #include <linux/ieee80211.h>
