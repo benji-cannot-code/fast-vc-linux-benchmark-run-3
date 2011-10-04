2148dab159dfb19d4ca46a7693804d71835024f6
Stephen Rothwell <sfr@canb.auug.org.au>
2011-10-04 18:32:57 +1100

    staging: r8712u: include module.h where needed
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/rtl8712/usb_intf.c
+++ drivers/staging/rtl8712/usb_intf.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _HCI_INTF_C_
 
 #include <linux/usb.h>
+#include <linux/module.h>
 
 #include "osdep_service.h"
 #include "drv_types.h"
