f84acb128b193be2b9c14ca832a6b316e81a586e
Stephen Rothwell <sfr@canb.auug.org.au>
2011-10-04 18:21:11 +1100

    staging: iio: adc: add includes of module.h and stat.h
    fixes these build problems:
    
    drivers/staging/iio/adc/ad7746.c:345:8: error: 'S_IWUSR' undeclared here (not in a function)
    drivers/staging/iio/adc/ad7746.c:436:8: error: 'S_IRUGO' undeclared here (not in a function)
    drivers/staging/iio/adc/ad7746.c:681:19: error: 'THIS_MODULE' undeclared here (not in a function)
    drivers/staging/iio/adc/ad7746.c:778:1: warning: data definition has no type or storage class [enabled by default]
    drivers/staging/iio/adc/ad7746.c:778:1: warning: type defaults to 'int' in declaration of 'MODULE_DEVICE_TABLE' [-Wimplicit-int]
    drivers/staging/iio/adc/ad7746.c:778:1: warning: parameter names (without types) in function declaration [enabled by default]
    drivers/staging/iio/adc/ad7746.c:799:15: error: expected declaration specifiers or '...' before string constant
    drivers/staging/iio/adc/ad7746.c:800:20: error: expected declaration specifiers or '...' before string constant
    drivers/staging/iio/adc/ad7746.c:801:16: error: expected declaration specifiers or '...' before string constant
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Jonathan Cameron <jic23@cam.ac.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/iio/adc/ad7746.c
+++ drivers/staging/iio/adc/ad7746.c
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysfs.h>
 #include <linux/i2c.h>
 #include <linux/delay.h>
+#include <linux/module.h>
+#include <linux/stat.h>
 
 #include "../iio.h"
 #include "../sysfs.h"
