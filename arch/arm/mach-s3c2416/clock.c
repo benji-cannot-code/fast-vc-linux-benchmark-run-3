aec79cda6c726165dddb8d51a2de8081fd708e21
Kukjin Kim <kgene.kim@samsung.com>
2011-10-04 19:41:43 +0900

    ARM: SAMSUNG: Consolidate plat/pll.h
    Removed
    - arch/arm/plat-s3c24xx/include/plat/pll.h
    - arch/arm/mach-s3c64xx/include/mach/pll.h
    - arch/arm/plat-s5p/include/plat/pll.h
    - arch/arm/plat-samsung/include/plat/pll6553x.h
    
    And created
    - arch/arm/plat-samsung/include/plat/pll.h
    
    Cc: Ben Dooks <ben-linux@fluff.org>
    [kgene.kim@samsung.com: changed title]
    [kgene.kim@samsung.com: fixed conflicts in plat-s5p/include/pll.h]
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c2416/clock.c
+++ arch/arm/mach-s3c2416/clock.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/cpu.h>
 
 #include <plat/cpu-freq.h>
-#include <plat/pll6553x.h>
 #include <plat/pll.h>
 
 #include <asm/mach/map.h>
