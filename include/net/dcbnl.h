928e6fbef1aa85f5a6f399e012c29fd296a1ba55
Mark Rustad <mark.d.rustad@intel.com>
2011-10-06 08:52:33 +0000

    dcb: Use ifindex instead of ifname
    Use ifindex instead of ifname in the DCB app ring. This makes for a smaller
    data structure and faster comparisons. It also avoids possible issues when
    a net device is renamed.
    
    Signed-off-by: Mark Rustad <mark.d.rustad@intel.com>
    Signed-off-by: John Fastabend <john.r.fastabend@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/dcbnl.h
+++ include/net/dcbnl.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dcbnl.h>
 
 struct dcb_app_type {
-	char		  name[IFNAMSIZ];
+	int	ifindex;
 	struct dcb_app	  app;
 	struct list_head  list;
 };
