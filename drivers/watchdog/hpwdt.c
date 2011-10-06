8d2e8026234715e38749e8d897ff8f757353f06a
Ingo Molnar <mingo@elte.hu>
2011-10-06 14:20:27 +0200

    x86, nmi, drivers: Fix nmi splitup build bug
    nmi.c needs an #include <linux/mca.h>:
    
     arch/x86/kernel/nmi.c: In function ‘unknown_nmi_error’:
     arch/x86/kernel/nmi.c:286:6: error: ‘MCA_bus’ undeclared (first use in this function)
     arch/x86/kernel/nmi.c:286:6: note: each undeclared identifier is reported only once for each function it appears in
    
    Another one is the hpwdt driver:
    
     drivers/watchdog/hpwdt.c:507:9: error: ‘NMI_DONE’ undeclared (first use in this function)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/watchdog/hpwdt.c
+++ drivers/watchdog/hpwdt.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/notifier.h>
 #include <asm/cacheflush.h>
 #endif /* CONFIG_HPWDT_NMI_DECODING */
+#include <asm/nmi.h>
 
 #define HPWDT_VERSION			"1.3.0"
 #define SECS_TO_TICKS(secs)		((secs) * 1000 / 128)
