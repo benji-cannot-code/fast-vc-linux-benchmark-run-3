588cae9569e92d3442d46c7e1bc1a54e4577a924
Luciano Coelho <coelho@ti.com>
2011-10-07 11:02:42 +0300

    wl12xx: move debugging definitions to a separate file
    Separate the debugging macros and other definitions to a new debug.h
    file.  This is be needed because the sdio and spi modules don't need
    to depend on the wl12xx module anymore, but still need to include
    wl12xx.h.  Currently they do depend on it, because of the debugging
    global that wl12xx exports.  A future patch will remove this
    dependency.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/ps.c
+++ drivers/net/wireless/wl12xx/ps.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ps.h"
 #include "io.h"
 #include "tx.h"
+#include "debug.h"
 
 #define WL1271_WAKEUP_TIMEOUT 500
 
--- drivers/net/wireless/wl12xx/tx.c
+++ drivers/net/wireless/wl12xx/tx.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/etherdevice.h>
 
 #include "wl12xx.h"
+#include "debug.h"
 #include "io.h"
 #include "reg.h"
 #include "ps.h"
