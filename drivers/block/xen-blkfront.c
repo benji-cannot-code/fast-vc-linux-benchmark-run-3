aa3c47bb7ee5cb27db77a047663d9d56d0cec654
Laszlo Ersek <lersek@redhat.com>
2011-10-07 21:34:38 +0200

    xen-blkfront: plug device number leak in xlblk_init() error path
    ... though after a failed xenbus_register_frontend() all may be lost.
    
    Acked-by: Ian Campbell <ian.campbell@citrix.com>
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/block/xen-blkfront.c
+++ drivers/block/xen-blkfront.c
@@ -1451,6 +1451,8 @@ static struct xenbus_driver blkfront = {
 
 static int __init xlblk_init(void)
 {
+	int ret;
+
 	if (!xen_domain())
 		return -ENODEV;
 
@@ -1460,7 +1462,13 @@ static int __init xlblk_init(void)
 		return -ENODEV;
 	}
 
-	return xenbus_register_frontend(&blkfront);
+	ret = xenbus_register_frontend(&blkfront);
+	if (ret) {
+		unregister_blkdev(XENVBD_MAJOR, DEV_NAME);
+		return ret;
+	}
+
+	return 0;
 }
 module_init(xlblk_init);
 
