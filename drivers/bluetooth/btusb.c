2a2cd3c8cd3d4a0db3bc436aeb631e29c7153946
Paul Bolle <pebolle@tiscali.nl>
2011-10-09 12:12:22 +0200

    Bluetooth: btusb: hide more usb_submit_urb errors
    There are still three calls of usb_submit_urb() that will print errors
    if those calls return -EPERM or -ENODEV. I have never triggered these,
    so I'm not sure when these return values might be seen. It still makes
    sense to be silent if these occur (since "urb is being killed" and
    "device got disconnected" aren't things to worry about).
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -313,7 +313,8 @@ static int btusb_submit_intr_urb(struct hci_dev *hdev, gfp_t mem_flags)
 
 	err = usb_submit_urb(urb, mem_flags);
 	if (err < 0) {
-		BT_ERR("%s urb %p submission failed (%d)",
+		if (err != -EPERM && err != -ENODEV)
+			BT_ERR("%s urb %p submission failed (%d)",
 						hdev->name, urb, -err);
 		usb_unanchor_urb(urb);
 	}
@@ -398,7 +399,8 @@ static int btusb_submit_bulk_urb(struct hci_dev *hdev, gfp_t mem_flags)
 
 	err = usb_submit_urb(urb, mem_flags);
 	if (err < 0) {
-		BT_ERR("%s urb %p submission failed (%d)",
+		if (err != -EPERM && err != -ENODEV)
+			BT_ERR("%s urb %p submission failed (%d)",
 						hdev->name, urb, -err);
 		usb_unanchor_urb(urb);
 	}
@@ -521,7 +523,8 @@ static int btusb_submit_isoc_urb(struct hci_dev *hdev, gfp_t mem_flags)
 
 	err = usb_submit_urb(urb, mem_flags);
 	if (err < 0) {
-		BT_ERR("%s urb %p submission failed (%d)",
+		if (err != -EPERM && err != -ENODEV)
+			BT_ERR("%s urb %p submission failed (%d)",
 						hdev->name, urb, -err);
 		usb_unanchor_urb(urb);
 	}
