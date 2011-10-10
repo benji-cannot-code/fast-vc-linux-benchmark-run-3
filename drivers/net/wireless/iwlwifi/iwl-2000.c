8220760734e298cc6716fd84831d876b30bcad33
Johannes Berg <johannes.berg@intel.com>
2011-10-10 07:27:15 -0700

    iwlagn: remove 6000 hw header
    The constants can be moved together with
    the similar ones for other devices and we
    can then remove the file.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-2000.c
+++ drivers/net/wireless/iwlwifi/iwl-2000.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-agn.h"
 #include "iwl-helpers.h"
 #include "iwl-agn-hw.h"
-#include "iwl-6000-hw.h"
 #include "iwl-shared.h"
 #include "iwl-cfg.h"
 
