303b72b7cca93748f6fc97f4f691b0fd8e05882f
Tomas Winkler <tomas.winkler@intel.com>
2011-10-10 08:36:38 +0200

    staging/easycap: kill timeval members of easycap struct
    timeval[0-9] were not used or used in a ready only code
    so we can remove them safely and so the code
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/easycap/easycap_ioctl.c
+++ drivers/staging/easycap/easycap_ioctl.c
@@ -932,7 +932,6 @@ static int adjust_mute(struct easycap *peasycap, int value)
 			switch (peasycap->mute) {
 			case 1: {
 				peasycap->audio_idle = 1;
-				peasycap->timeval0.tv_sec = 0;
 				SAM("adjusting mute: %i=peasycap->audio_idle\n",
 				    peasycap->audio_idle);
 				return 0;
@@ -2334,7 +2333,6 @@ long easycap_unlocked_ioctl(struct file *file,
 
 		peasycap->video_idle = 1;
 		peasycap->audio_idle = 1;
-		peasycap->timeval0.tv_sec = 0;
 /*---------------------------------------------------------------------------*/
 /*
  *  IF THE WAIT QUEUES ARE NOT CLEARED IN RESPONSE TO THE STREAMOFF COMMAND
