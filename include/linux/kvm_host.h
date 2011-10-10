e3af724e6cc513f58ae5ef56fa4bd884a6358e33
Eric B Munson <emunson@mgebm.net>
2011-10-10 11:46:15 -0400

    KVM: Fix include dependency for mmu_notifier
    The kvm_host struct can include an mmu_notifier struct but mmu_notifier.h is
    not included directly.
    
    Signed-off-by: Eric B Munson <emunson@mgebm.net>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/signal.h>
 #include <linux/sched.h>
 #include <linux/mm.h>
+#include <linux/mmu_notifier.h>
 #include <linux/preempt.h>
 #include <linux/msi.h>
 #include <linux/slab.h>
