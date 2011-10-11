ee8810fa1535a1c04ab2801b86bb7eb1725b034d
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-10-11 21:47:17 +0300

    staging: xgifb: add a global variable for the default refresh rate
    Add a global variable for the default refresh rate. This is done to
    get rid of references to xgi_video_info before the probe routine, which
    should allocate the xgi_video_info dynamically in the future.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/XGI_main_26.c
+++ drivers/staging/xgifb/XGI_main_26.c
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static char *mode;
 static int vesa = -1;
+static unsigned int refresh_rate;
 
 /* -------------------- Macro definitions ---------------------------- */
 
@@ -1891,9 +1892,9 @@ static int __init XGIfb_setup(char *options)
 		} else if (!strncmp(this_opt, "vesa:", 5)) {
 			vesa = xgifb_optval(this_opt, 5);
 		} else if (!strncmp(this_opt, "vrate:", 6)) {
-			xgi_video_info.refresh_rate = xgifb_optval(this_opt, 6);
+			refresh_rate = xgifb_optval(this_opt, 6);
 		} else if (!strncmp(this_opt, "rate:", 5)) {
-			xgi_video_info.refresh_rate = xgifb_optval(this_opt, 5);
+			refresh_rate = xgifb_optval(this_opt, 5);
 		} else if (!strncmp(this_opt, "crt1off", 7)) {
 			XGIfb_crt1off = 1;
 		} else if (!strncmp(this_opt, "filter:", 7)) {
@@ -2277,6 +2278,7 @@ static int __devinit xgifb_probe(struct pci_dev *pdev,
 	XGIfb_mode_no = XGIbios_mode[xgifb_mode_idx].mode_no;
 
 	/* yilin set default refresh rate */
+	xgi_video_info.refresh_rate = refresh_rate;
 	if (xgi_video_info.refresh_rate == 0)
 		xgi_video_info.refresh_rate = 60;
 	if (XGIfb_search_refresh_rate(
