e15ec38ffc31a01766ba69d327fa8c0f1e32da3b
NeilBrown <neilb@suse.de>
2011-10-11 16:48:46 +1100

    md: remove typedefs: mirror_info_t -> struct mirror_info
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/raid1.h
+++ drivers/md/raid1.h
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _RAID1_H
 #define _RAID1_H
 
-typedef struct mirror_info mirror_info_t;
-
 struct mirror_info {
 	struct md_rdev	*rdev;
 	sector_t	head_position;
@@ -24,7 +22,7 @@ struct pool_info {
 
 struct r1_private_data_s {
 	struct mddev		*mddev;
-	mirror_info_t		*mirrors;
+	struct mirror_info		*mirrors;
 	int			raid_disks;
 
 	/* When choose the best device for a read (read_balance())
--- drivers/md/raid10.h
+++ drivers/md/raid10.h
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _RAID10_H
 #define _RAID10_H
 
-typedef struct mirror_info mirror_info_t;
-
 struct mirror_info {
 	struct md_rdev	*rdev;
 	sector_t	head_position;
@@ -16,7 +14,7 @@ struct mirror_info {
 
 struct r10_private_data_s {
 	struct mddev		*mddev;
-	mirror_info_t		*mirrors;
+	struct mirror_info	*mirrors;
 	int			raid_disks;
 	spinlock_t		device_lock;
 
