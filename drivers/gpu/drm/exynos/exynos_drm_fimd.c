83897d8981696398165f2ac2c629bb243b329cf6
Inki Dae <inki.dae@samsung.com>
2011-10-14 13:29:52 +0900

    drm/exynos: fixed build warnings and comments.
    Signed-off-by: Inki Dae <inki.dae@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/exynos/exynos_drm_fimd.c
+++ drivers/gpu/drm/exynos/exynos_drm_fimd.c
@@ -91,8 +91,6 @@ struct fimd_context {
 
 static bool fimd_display_is_connected(struct device *dev)
 {
-	struct fimd_context *ctx = get_fimd_context(dev);
-
 	DRM_DEBUG_KMS("%s\n", __FILE__);
 
 	/* TODO. */
@@ -111,8 +109,6 @@ static void *fimd_get_timing(struct device *dev)
 
 static int fimd_check_timing(struct device *dev, void *timing)
 {
-	struct fimd_context *ctx = get_fimd_context(dev);
-
 	DRM_DEBUG_KMS("%s\n", __FILE__);
 
 	/* TODO. */
@@ -122,8 +118,6 @@ static int fimd_check_timing(struct device *dev, void *timing)
 
 static int fimd_display_power_on(struct device *dev, int mode)
 {
-	struct fimd_context *ctx = get_fimd_context(dev);
-
 	DRM_DEBUG_KMS("%s\n", __FILE__);
 
 	/* TODO. */
@@ -500,7 +494,7 @@ static void fimd_finish_pageflip(struct drm_device *drm_dev, int crtc)
 
 	list_for_each_entry_safe(e, t, &dev_priv->pageflip_event_list,
 			base.link) {
-		/* if event's pipe isn't same as crtc then ignor it. */
+		/* if event's pipe isn't same as crtc then ignore it. */
 		if (crtc != e->pipe)
 			continue;
 
@@ -526,7 +520,6 @@ static irqreturn_t fimd_irq_handler(int irq, void *dev_id)
 	struct fimd_context *ctx = (struct fimd_context *)dev_id;
 	struct exynos_drm_subdrv *subdrv = &ctx->subdrv;
 	struct drm_device *drm_dev = subdrv->drm_dev;
-	struct device *dev = subdrv->manager.dev;
 	struct exynos_drm_manager *manager = &subdrv->manager;
 	u32 val;
 
@@ -544,8 +537,6 @@ static irqreturn_t fimd_irq_handler(int irq, void *dev_id)
 
 static int fimd_subdrv_probe(struct drm_device *drm_dev, struct device *dev)
 {
-	struct drm_driver *drm_driver = drm_dev->driver;
-
 	DRM_DEBUG_KMS("%s\n", __FILE__);
 
 	/*
@@ -570,8 +561,6 @@ static int fimd_subdrv_probe(struct drm_device *drm_dev, struct device *dev)
 
 static void fimd_subdrv_remove(struct drm_device *drm_dev)
 {
-	struct drm_driver *drm_driver = drm_dev->driver;
-
 	DRM_DEBUG_KMS("%s\n", __FILE__);
 
 	/* TODO. */
