4485485212d839496568923686c9a1c45f91608c
Inki Dae <inki.dae@samsung.com>
2011-10-14 13:29:47 +0900

    drm/exynos: fixed overlay updating time at page flip.
    buffer addess is set to shadow register and then applied to
    real register at vsync front porch time.
    
    Signed-off-by: Joonyoung Shim <jy0922.shim@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/exynos/exynos_drm_crtc.c
+++ drivers/gpu/drm/exynos/exynos_drm_crtc.c
@@ -79,7 +79,7 @@ struct exynos_drm_crtc {
 	unsigned int			pipe;
 };
 
-void exynos_drm_crtc_apply(struct drm_crtc *crtc)
+static void exynos_drm_crtc_apply(struct drm_crtc *crtc)
 {
 	struct exynos_drm_crtc *exynos_crtc = to_exynos_crtc(crtc);
 	struct exynos_drm_overlay *overlay = &exynos_crtc->overlay;
@@ -279,6 +279,8 @@ static int exynos_drm_crtc_page_flip(struct drm_crtc *crtc,
 
 			goto out;
 		}
+
+		exynos_drm_crtc_apply(crtc);
 
 		dev_priv->pageflip_event = true;
 	}
