c70f637a1d4fcc97527337db3bf26e78e26c390d
Greg Ungerer <gerg@uclinux.org>
2011-10-18 15:41:26 +1000

    m68k: use tracehook_report_syscall_entry/exit for ColdFire MMU ptrace path
    The existing ColdFire code (which is all non-mmu) for system call entry
    and exit uses the more modern tracehook_report_syscall_entry()/exit()
    into the ptrace code. Now that we are supporting ColdFire with MMU we
    need the same hooks for these.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Matt Waddel <mwaddel@yahoo.com>
    Acked-by: Kurt Mahan <kmahan@xmission.com>

--- arch/m68k/kernel/ptrace_mm.c
+++ arch/m68k/kernel/ptrace_mm.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ptrace.h>
 #include <linux/user.h>
 #include <linux/signal.h>
+#include <linux/tracehook.h>
 
 #include <asm/uaccess.h>
 #include <asm/page.h>
@@ -276,3 +277,20 @@ asmlinkage void syscall_trace(void)
 		current->exit_code = 0;
 	}
 }
+
+#ifdef CONFIG_COLDFIRE
+asmlinkage int syscall_trace_enter(void)
+{
+	int ret = 0;
+
+	if (test_thread_flag(TIF_SYSCALL_TRACE))
+		ret = tracehook_report_syscall_entry(task_pt_regs(current));
+	return ret;
+}
+
+asmlinkage void syscall_trace_leave(void)
+{
+	if (test_thread_flag(TIF_SYSCALL_TRACE))
+		tracehook_report_syscall_exit(task_pt_regs(current), 0);
+}
+#endif /* CONFIG_COLDFIRE */
