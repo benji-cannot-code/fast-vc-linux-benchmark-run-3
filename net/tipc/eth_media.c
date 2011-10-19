3ccab41d033c27a8afbff7c20f92174a22350e8c
Allan Stephens <allan.stephens@windriver.com>
2011-10-19 15:39:21 -0400

    tipc: Eliminate useless memset operations in Ethernet media support
    Gets rid of two pointless operations that zero out the array used to
    record information about TIPC's Ethernet bearers. There is no need to
    initialize the array on start up since it is a global variable that is
    already zero'd out, and there is no need to zero it out on exit because
    the array is never referenced again.
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- net/tipc/eth_media.c
+++ net/tipc/eth_media.c
@@ -372,8 +372,6 @@ int tipc_eth_media_start(void)
 	if (eth_started)
 		return -EINVAL;
 
-	memset(eth_bearers, 0, sizeof(eth_bearers));
-
 	res = tipc_register_media(&eth_media_info);
 	if (res)
 		return res;
@@ -397,6 +395,5 @@ void tipc_eth_media_stop(void)
 
 	flush_scheduled_work();
 	unregister_netdevice_notifier(&notifier);
-	memset(&eth_bearers, 0, sizeof(eth_bearers));
 	eth_started = 0;
 }
