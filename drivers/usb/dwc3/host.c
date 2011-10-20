fad24d3995645027ad53ea9f6127ace110ccbcb2
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2011-10-20 18:43:10 +0200

    usb: dwc3: host: remove unused includes
    None of these are required atm.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -36,23 +36,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/slab.h>
-#include <linux/spinlock.h>
 #include <linux/platform_device.h>
-#include <linux/pm_runtime.h>
-#include <linux/interrupt.h>
-#include <linux/ioport.h>
-#include <linux/io.h>
-#include <linux/list.h>
-#include <linux/delay.h>
-#include <linux/dma-mapping.h>
 
 #include "core.h"
-#include "io.h"
-
-#include "debug.h"
 
 static struct resource generic_resources[] = {
 	{
