2dde801498a7eea760900274c062af46d2a0874d
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2011-10-20 09:43:26 +0200

    perf symbols: Increase symbol KSYM_NAME_LEN size
    Fglrx propietary driver has symbol names over 128 chars (:S). This
    breaks the function kallsyms__parse.
    
    This fix increases the size of KSYM_NAME_LEN, so kallsyms__parse can
    work on such kernels.
    
    The only counterparty, is that such function requires 128 more bytes to
    work.
    
    Acked-by: Pekka Enberg <penberg@kernel.org>
    Cc: Anton Blanchard <anton@samba.org>
    Cc: David Ahern <daahern@cisco.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1319096606-11568-1-git-send-email-ricardo.ribalda@gmail.com
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/utsname.h>
 
 #ifndef KSYM_NAME_LEN
-#define KSYM_NAME_LEN 128
+#define KSYM_NAME_LEN 256
 #endif
 
 #ifndef NT_GNU_BUILD_ID
