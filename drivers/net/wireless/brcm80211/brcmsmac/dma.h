d471dcca8d2e81cfd294d577fca3642f977902bb
Arend van Spriel <arend@broadcom.com>
2011-10-21 16:16:31 +0200

    brcm80211: smac: use sk_buff list for handling frames in receive path
    In the receive path the frames are obtained from the dma using
    multiple sk_buff that were linked using the skb next pointer.
    This has been changed and it now used sk_buff lists and skb_queue
    functions instead.
    
    Reported-by: Johannes Berg <johannes@sipsolutions.net>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Alwin Beukers <alwin@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmsmac/dma.h
+++ drivers/net/wireless/brcm80211/brcmsmac/dma.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	_BRCM_DMA_H_
 
 #include <linux/delay.h>
+#include <linux/skbuff.h>
 #include "types.h"		/* forward structure declarations */
 
 /* map/unmap direction */
@@ -81,7 +82,7 @@ extern struct dma_pub *dma_attach(char *name, struct si_pub *sih,
 			    uint nrxpost, uint rxoffset, uint *msg_level);
 
 void dma_rxinit(struct dma_pub *pub);
-struct sk_buff *dma_rx(struct dma_pub *pub);
+int dma_rx(struct dma_pub *pub, struct sk_buff_head *skb_list);
 bool dma_rxfill(struct dma_pub *pub);
 bool dma_rxreset(struct dma_pub *pub);
 bool dma_txreset(struct dma_pub *pub);
