a9cde1aa107a5f61213499162accada619852acd
Peter Foley <pefoley2@verizon.net>
2011-10-22 10:48:49 -0400

    kconfig: use xfwrite wrapper function to silence warnings
    Use the xfwrite wrapper function defined in lkc.h to check the return value of
    fwrite and silence these warnings.
    
      HOSTCC  scripts/kconfig/zconf.tab.o
    scripts/kconfig/zconf.tab.c: In function 'header_print_comment':
    /usr/src/lto/scripts/kconfig/confdata.c:551:10: warning: ignoring return value of 'fwrite', declared with attribute warn_unused_result
    scripts/kconfig/zconf.tab.c: In function 'kconfig_print_comment':
    /usr/src/lto/scripts/kconfig/confdata.c:467:10: warning: ignoring return value of 'fwrite', declared with attribute warn_unused_result
    
    Signed-off-by: Peter Foley <pefoley2@verizon.net>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -465,7 +465,7 @@ kconfig_print_comment(FILE *fp, const char *value, void *arg)
 		fprintf(fp, "#");
 		if (l) {
 			fprintf(fp, " ");
-			fwrite(p, l, 1, fp);
+			xfwrite(p, l, 1, fp);
 			p += l;
 		}
 		fprintf(fp, "\n");
@@ -538,7 +538,7 @@ header_print_comment(FILE *fp, const char *value, void *arg)
 		fprintf(fp, " *");
 		if (l) {
 			fprintf(fp, " ");
-			fwrite(p, l, 1, fp);
+			xfwrite(p, l, 1, fp);
 			p += l;
 		}
 		fprintf(fp, "\n");
