5b30d16c52ea41e705150a4b1c47843b1e5b9cb4
Leonid V. Fedorenchik <leonidsbox@gmail.com>
2011-10-22 01:43:28 -0300

    [media] cx25821-audio.h: Line up defines
    Line up defines to the same column.
    
    Signed-off-by: Leonid V. Fedorenchik <leonidsbox@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx25821/cx25821-audio.h
+++ drivers/media/video/cx25821/cx25821-audio.h
@@ -24,12 +24,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __CX25821_AUDIO_H__
 #define __CX25821_AUDIO_H__
 
-#define USE_RISC_NOOP               1
-#define LINES_PER_BUFFER            15
-#define AUDIO_LINE_SIZE             128
+#define USE_RISC_NOOP		1
+#define LINES_PER_BUFFER	15
+#define AUDIO_LINE_SIZE		128
 
 /* Number of buffer programs to use at once. */
-#define NUMBER_OF_PROGRAMS  8
+#define NUMBER_OF_PROGRAMS	8
 
 /*
  * Max size of the RISC program for a buffer. - worst case is 2 writes per line
