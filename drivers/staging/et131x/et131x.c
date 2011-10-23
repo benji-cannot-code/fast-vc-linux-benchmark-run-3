41ec8f81ac8900ce66c2aa5e13af91bf2b610730
Mark Einon <mark.einon@gmail.com>
2011-10-23 10:22:54 +0100

    staging: et131x: Remove redundant check and return statement
    In nic_send_packet(), by the time 'frag' is checked to be zero, it never
    is - the for loop has been entered (as nr_frags is always > 0) and frag
    has been incremented at least once. Remove the check and associated
    error return.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/et131x/et131x.c
+++ drivers/staging/et131x/et131x.c
@@ -3413,9 +3413,6 @@ static int nic_send_packet(struct et131x_adapter *adapter, struct tcb *tcb)
 		}
 	}
 
-	if (frag == 0)
-		return -EIO;
-
 	if (phydev && phydev->speed == SPEED_1000) {
 		if (++adapter->tx_ring.since_irq == PARM_TX_NUM_BUFS_DEF) {
 			/* Last element & Interrupt flag */
