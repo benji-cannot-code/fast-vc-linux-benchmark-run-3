82bc991c064a59986a74f92811f647c24910c81a
Thomas Abraham <thomas.abraham@linaro.org>
2011-10-24 11:47:46 +0200

    serial: samsung: switch to clkdev based clock lookup
    Instead of using clock names supplied in platform data, use a generic
    clock name 'clk_uart_baud' to look up clocks. The platform code should
    register clocks with the name 'clk_uart_baud' which can be used by the
    baud rate generator. The clock lookup and selection of the best clock
    as baud rate clock is reworked.
    
    Platform code can specify the clocks that can be used as source for the
    baud clock (as supported previously by passing names of clocks). A new
    member is added to the platform data 'clk_sel' which holds a bit-field
    value with each bit representing a baud source clock. If a bit at any
    bit position is set, that clock is looked up to participate in the
    selection of the baud clock source.
    
    Cc: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Thomas Abraham <thomas.abraham@linaro.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-samsung/include/plat/regs-serial.h
+++ arch/arm/plat-samsung/include/plat/regs-serial.h
@@ -223,6 +223,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define S5PV210_UFSTAT_RXSHIFT	(0)
 
 #define NO_NEED_CHECK_CLKSRC	1
+#define S3C2410_UCON_CLKSEL0	(1 << 0)
+#define S3C2410_UCON_CLKSEL1	(1 << 1)
+#define S3C2410_UCON_CLKSEL2	(1 << 2)
+#define S3C2410_UCON_CLKSEL3	(1 << 3)
 
 #ifndef __ASSEMBLY__
 
@@ -258,6 +262,7 @@ struct s3c2410_uartcfg {
 	unsigned char	   unused;
 	unsigned short	   flags;
 	upf_t		   uart_flags;	 /* default uart flags */
+	unsigned int	   clk_sel;
 
 	unsigned int	   has_fracval;
 
