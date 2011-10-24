8032bd739056fd34612aab668b199294194fef19
Thomas Abraham <thomas.abraham@linaro.org>
2011-10-24 11:43:11 +0200

    DMA: PL330: Infer transfer direction from transfer request instead of platform data
    The transfer direction for a channel can be inferred from the transfer
    request and the need for specifying transfer direction in platfrom data
    can be eliminated. So the structure definition 'struct dma_pl330_peri'
    is no longer required.
    
    The channel's private data is set to point to a channel id specified in
    the platform data (instead of an instance of type 'struct dma_pl330_peri').
    The filter function is correspondingly modified to match the channel id.
    
    With the 'struct dma_pl330_peri' removed from platform data, the dma
    controller transfer capabilities cannot be inferred any more. Hence,
    the dma controller capabilities is specified using platform data.
    
    Acked-by: Jassi Brar <jassisinghbrar@gmail.com>
    Acked-by: Boojin Kim <boojin.kim@samsung.com>
    Signed-off-by: Thomas Abraham <thomas.abraham@linaro.org>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- include/linux/amba/pl330.h
+++ include/linux/amba/pl330.h
@@ -16,15 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmaengine.h>
 #include <asm/hardware/pl330.h>
 
-struct dma_pl330_peri {
-	/*
-	 * Peri_Req i/f of the DMAC that is
-	 * peripheral could be reached from.
-	 */
-	u8 peri_id; /* specific dma id */
-	enum pl330_reqtype rqtype;
-};
-
 struct dma_pl330_platdata {
 	/*
 	 * Number of valid peripherals connected to DMAC.
@@ -35,7 +26,9 @@ struct dma_pl330_platdata {
 	 */
 	u8 nr_valid_peri;
 	/* Array of valid peripherals */
-	struct dma_pl330_peri *peri;
+	u8 *peri_id;
+	/* Operational capabilities */
+	dma_cap_mask_t cap_mask;
 	/* Bytes to allocate for MC buffer */
 	unsigned mcbuf_sz;
 };
