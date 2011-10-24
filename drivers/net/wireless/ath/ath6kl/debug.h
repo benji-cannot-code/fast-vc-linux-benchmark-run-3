c07d56017f3933af10272ff9858f5a576f8ae5c8
Kalle Valo <kvalo@qca.qualcomm.com>
2011-10-24 12:17:59 +0300

    ath6kl: add debug messages for credit handling
    Also take few from htc debug level which are more suitable for credit.
    
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/debug.h
+++ drivers/net/wireless/ath/ath6kl/debug.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hif.h"
 
 enum ATH6K_DEBUG_MASK {
-	/* hole */
+	ATH6KL_DBG_CREDIT	= BIT(0),
 	/* hole */
 	ATH6KL_DBG_WLAN_TX      = BIT(2),     /* wlan tx */
 	ATH6KL_DBG_WLAN_RX      = BIT(3),     /* wlan rx */
