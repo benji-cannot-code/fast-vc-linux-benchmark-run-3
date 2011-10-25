bd18543f0799e3b6521d39afe186692ae127d508
Daniel Vetter <daniel.vetter@ffwll.ch>
2011-10-25 22:33:37 +0200

    drm/sman: kill user_hash_tab
    No longer used.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/drm_sman.h
+++ include/drm/drm_sman.h
@@ -88,7 +88,6 @@ struct drm_memblock_item {
 struct drm_sman {
 	struct drm_sman_mm *mm;
 	int num_managers;
-	struct drm_open_hash user_hash_tab;
 };
 
 /*
@@ -140,11 +139,7 @@ extern struct drm_memblock_item *drm_sman_alloc(struct drm_sman * sman,
 						unsigned long size,
 						unsigned alignment,
 						unsigned long owner);
-/*
- * Free a memory block identified by its user hash key.
- */
 
-extern int drm_sman_free_key(struct drm_sman * sman, unsigned int key);
 extern void drm_sman_free(struct drm_memblock_item *item);
 
 /*
