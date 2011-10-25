53c07c03b5c142790459535f25aafcc2bb8ec8aa
Vasanthakumar Thiagarajan <vthiagar@qca.qualcomm.com>
2011-10-25 19:34:13 +0530

    ath6kl: Take vif information from wmi event
    Interface index is passed in wmi command header from target.
    Use this index to get the appropriate vif.
    
    Signed-off-by: Vasanthakumar Thiagarajan <vthiagar@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/debug.c
+++ drivers/net/wireless/ath/ath6kl/debug.c
@@ -1250,6 +1250,8 @@ static ssize_t ath6kl_create_qos_write(struct file *file,
 {
 
 	struct ath6kl *ar = file->private_data;
+	/* TODO: Findout vif */
+	struct ath6kl_vif *vif = ar->vif;
 	char buf[100];
 	ssize_t len;
 	char *sptr, *token;
@@ -1404,7 +1406,7 @@ static ssize_t ath6kl_create_qos_write(struct file *file,
 		return -EINVAL;
 	pstream.medium_time = cpu_to_le32(val32);
 
-	ath6kl_wmi_create_pstream_cmd(ar->wmi, &pstream);
+	ath6kl_wmi_create_pstream_cmd(ar->wmi, vif->fw_vif_idx, &pstream);
 
 	return count;
 }
@@ -1422,6 +1424,8 @@ static ssize_t ath6kl_delete_qos_write(struct file *file,
 {
 
 	struct ath6kl *ar = file->private_data;
+	/* TODO: Findout vif */
+	struct ath6kl_vif *vif = ar->vif;
 	char buf[100];
 	ssize_t len;
 	char *sptr, *token;
@@ -1446,7 +1450,8 @@ static ssize_t ath6kl_delete_qos_write(struct file *file,
 	if (kstrtou8(token, 0, &tsid))
 		return -EINVAL;
 
-	ath6kl_wmi_delete_pstream_cmd(ar->wmi, traffic_class, tsid);
+	ath6kl_wmi_delete_pstream_cmd(ar->wmi, vif->fw_vif_idx,
+				      traffic_class, tsid);
 
 	return count;
 }
