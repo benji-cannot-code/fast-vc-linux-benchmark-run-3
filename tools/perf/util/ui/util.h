71feb5d644d0c4145327407ab1af55a278181793
Arnaldo Carvalho de Melo <acme@redhat.com>
2011-10-25 13:45:16 -0200

    perf ui: Improve handling sigwinch a bit
    No need to unblock it at each ui__getch() and also allow other users to
    check if a resize is needed, or force an refresh of terminal dimensions.
    
    The 'force' one shouldn't be needed, but its in a slow path, so leave it
    like that for now, I'll revisit this another day.
    
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-aujchu6yx3bfy64non1rky0w@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/ui/util.h
+++ tools/perf/util/ui/util.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <stdbool.h>
 
+int ui__getch(int delay_secs);
 int ui__popup_menu(int argc, char * const argv[]);
 int ui__help_window(const char *text);
 bool ui__dialog_yesno(const char *msg);
