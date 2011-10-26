4d9d348304b82060ef17c57e4290cdb9cd339b16
Matthew McClintock <msm@freescale.com>
2011-10-26 13:46:57 -0500

    powerpc/85xx: issue 15 EOI after core reset for FSL CoreNet devices
    This is listed as a requirement for Freescale CoreNet based devices  (e.g
    p4080ds with MPIC v4.x) after issuing a core reset to properly clear pending
    interrupts.
    
    Signed-off-by: Matthew McClintock <msm@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/sysdev/mpic.c
+++ arch/powerpc/sysdev/mpic.c
@@ -1749,6 +1749,7 @@ void mpic_reset_core(int cpu)
 	struct mpic *mpic = mpic_primary;
 	u32 pir;
 	int cpuid = get_hard_smp_processor_id(cpu);
+	int i;
 
 	/* Set target bit for core reset */
 	pir = mpic_read(mpic->gregs, MPIC_INFO(GREG_PROCESSOR_INIT));
@@ -1760,6 +1761,15 @@ void mpic_reset_core(int cpu)
 	pir &= ~(1 << cpuid);
 	mpic_write(mpic->gregs, MPIC_INFO(GREG_PROCESSOR_INIT), pir);
 	mpic_read(mpic->gregs, MPIC_INFO(GREG_PROCESSOR_INIT));
+
+	/* Perform 15 EOI on each reset core to clear pending interrupts.
+	 * This is required for FSL CoreNet based devices */
+	if (mpic->flags & MPIC_FSL) {
+		for (i = 0; i < 15; i++) {
+			_mpic_write(mpic->reg_type, &mpic->cpuregs[cpuid],
+				      MPIC_CPU_EOI, 0);
+		}
+	}
 }
 #endif /* CONFIG_SMP */
 
