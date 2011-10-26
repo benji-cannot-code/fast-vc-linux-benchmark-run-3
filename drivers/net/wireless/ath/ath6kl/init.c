3dbc70d4e876f4eecceb7d72ab36ec4cb26a3e3f
Sangwook Lee <sangwook.lee@linaro.org>
2011-10-26 16:28:38 +0100

    ath6kl: Fix compilation error from of.h
    When compiling ath6kl for ARM with device tree tree compilation fails
    with errors like:
    
    include/linux/of.h: In function 'of_property_read_u32_array':
    include/linux/of.h:249:10: error: 'ENOSYS' undeclared
    
    Workaround this by including errno.h from init.c.
    
    kvalo: improved commit log
    
    Signed-off-by: Sangwook Lee <sangwook.lee@linaro.org>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/init.c
+++ drivers/net/wireless/ath/ath6kl/init.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/moduleparam.h>
+#include <linux/errno.h>
 #include <linux/of.h>
 #include <linux/mmc/sdio_func.h>
 #include "core.h"
