78776438293c0db2cc927a06dc6e6a962671efcb
Aarthi Thiruvengadam <athiruve@qca.qualcomm.com>
2011-10-27 09:35:56 -0700

    ath6kl: fix missing copy of action frame contents
    The wpa_supplicant was receiving incorrect frame contents in the
    callback function that indicates the status of the frame transmitted.
    This patch fixes a missing copy of the frame contents to a local
    buffer. The local buffer keeps track of the last sent management frame.
    
    Signed-off-by: Aarthi Thiruvengadam <athiruve@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/wmi.c
+++ drivers/net/wireless/ath/ath6kl/wmi.c
@@ -2847,6 +2847,7 @@ int ath6kl_wmi_send_action_cmd(struct wmi *wmi, u8 if_idx, u32 id, u32 freq,
 	}
 
 	kfree(wmi->last_mgmt_tx_frame);
+	memcpy(buf, data, data_len);
 	wmi->last_mgmt_tx_frame = buf;
 	wmi->last_mgmt_tx_frame_len = data_len;
 
