af9bcbc7f58200ab9a77976413afc9f21be071dc
Allan Stephens <allan.stephens@windriver.com>
2011-10-27 16:31:26 -0400

    tipc: Prevent loss of fragmented messages over unicast links
    Modifies unicast link endpoint logic so an incoming fragmented message
    is not lost if reassembly cannot begin because there is no buffer big
    enough to hold the entire reassembled message. The link endpoint now
    ignores the first fragment completely, which causes the sending node to
    retransmit the first fragment so that reassembly can be re-attempted.
    
    Previously, the sender would have had no reason to retransmit the 1st
    fragment, so we would never have a chance to re-try the allocation.
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>

--- net/tipc/link.c
+++ net/tipc/link.c
@@ -1775,6 +1775,7 @@ protocol_check:
 					head = link_insert_deferred_queue(l_ptr,
 									  head);
 				if (likely(msg_is_dest(msg, tipc_own_addr))) {
+					int ret;
 deliver:
 					if (likely(msg_isdata(msg))) {
 						tipc_node_unlock(n_ptr);
@@ -1799,11 +1800,15 @@ deliver:
 						continue;
 					case MSG_FRAGMENTER:
 						l_ptr->stats.recv_fragments++;
-						if (tipc_link_recv_fragment(&l_ptr->defragm_buf,
-									    &buf, &msg)) {
+						ret = tipc_link_recv_fragment(
+							&l_ptr->defragm_buf,
+							&buf, &msg);
+						if (ret == 1) {
 							l_ptr->stats.recv_fragmented++;
 							goto deliver;
 						}
+						if (ret == -1)
+							l_ptr->next_in_no--;
 						break;
 					case CHANGEOVER_PROTOCOL:
 						type = msg_type(msg);
@@ -2633,7 +2638,9 @@ int tipc_link_recv_fragment(struct sk_buff **pending, struct sk_buff **fb,
 			set_fragm_size(pbuf, fragm_sz);
 			set_expected_frags(pbuf, exp_fragm_cnt - 1);
 		} else {
-			warn("Link unable to reassemble fragmented message\n");
+			dbg("Link unable to reassemble fragmented message\n");
+			buf_discard(fbuf);
+			return -1;
 		}
 		buf_discard(fbuf);
 		return 0;
