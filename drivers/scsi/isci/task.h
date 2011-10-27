48ac24abeb4a94274a3a96368859750ca6d5c9a0
Jeff Skirvin <jeffrey.d.skirvin@intel.com>
2011-10-27 15:05:06 -0700

    [SCSI] isci: Handle task request timeouts correctly.
    In the case where "task" requests timeout (note that this class of
    requests can also include SATA/STP soft reset FIS transmissions),
    handle the case where the task was being managed by some call to
    terminate the task request by completing both the tmf and the aborting
    process.
    
    Signed-off-by: Jeff Skirvin <jeffrey.d.skirvin@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/isci/task.h
+++ drivers/scsi/isci/task.h
@@ -59,6 +59,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/sas_ata.h>
 #include "host.h"
 
+#define ISCI_TERMINATION_TIMEOUT_MSEC 500
+
 struct isci_request;
 
 /**
