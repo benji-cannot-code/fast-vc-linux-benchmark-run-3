4e9a88d0adead8d71bab03fe30fd50961e31c58e
Martin Schwidefsky <schwidefsky@de.ibm.com>
2011-10-30 15:17:13 +0100

    [S390] sparse: fix sparse warnings about missing prototypes
    Add prototypes and includes for functions used in different modules.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/ipl.c
+++ arch/s390/kernel/ipl.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sclp.h>
 #include <asm/sigp.h>
 #include <asm/checksum.h>
+#include "entry.h"
 
 #define IPL_PARM_BLOCK_VERSION 0
 
