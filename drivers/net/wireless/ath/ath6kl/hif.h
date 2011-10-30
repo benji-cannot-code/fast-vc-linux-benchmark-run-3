fa785041fcebff4f903dfe1d450eb27a24fea987
Kalle Valo <kvalo@qca.qualcomm.com>
2011-10-30 21:15:57 +0200

    ath6kl: create ath6kl_hif_stop()
    This is to reset hif layer for powering down hw.
    
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/hif.h
+++ drivers/net/wireless/ath/ath6kl/hif.h
@@ -247,6 +247,7 @@ struct ath6kl_hif_ops {
 	int (*resume)(struct ath6kl *ar);
 	int (*power_on)(struct ath6kl *ar);
 	int (*power_off)(struct ath6kl *ar);
+	void (*stop)(struct ath6kl *ar);
 };
 
 int ath6kl_hif_setup(struct ath6kl_device *dev);
