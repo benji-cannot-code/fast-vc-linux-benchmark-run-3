132425d893c0f8e7db4b772cb795d4f2f57385bf
Stefan Haberland <stefan.haberland@de.ibm.com>
2011-10-30 15:17:09 +0100

    [S390] dasd: prevent path verification before resume
    Mark the device as suspended and delay execution of the path
    verification worker to prevent mix-up.
    
    Signed-off-by: Stefan Haberland <stefan.haberland@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/block/dasd.c
+++ drivers/s390/block/dasd.c
@@ -3290,6 +3290,9 @@ int dasd_generic_pm_freeze(struct ccw_device *cdev)
 	if (IS_ERR(device))
 		return PTR_ERR(device);
 
+	/* mark device as suspended */
+	set_bit(DASD_FLAG_SUSPENDED, &device->flags);
+
 	if (device->discipline->freeze)
 		rc = device->discipline->freeze(device);
 
@@ -3364,6 +3367,7 @@ int dasd_generic_restore_device(struct ccw_device *cdev)
 	if (device->block)
 		dasd_schedule_block_bh(device->block);
 
+	clear_bit(DASD_FLAG_SUSPENDED, &device->flags);
 	dasd_put_device(device);
 	return 0;
 }
