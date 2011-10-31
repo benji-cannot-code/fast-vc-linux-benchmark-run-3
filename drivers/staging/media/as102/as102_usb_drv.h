e02b80a876657bb75f0e06a988b876f50c85d4d2
Piotr Chmura <chmooreck@poczta.onet.pl>
2011-10-31 12:24:53 -0300

    [media] staging: as102: Add nBox Tuner Dongle support
    Add support for nBox Tuner Dongle based on the same chip.
    
    Cc: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Piotr Chmura <chmooreck@poczta.onet.pl>
    Signed-off-by: Sylwester Nawrocki <snjw23@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/media/as102/as102_usb_drv.h
+++ drivers/staging/media/as102/as102_usb_drv.h
@@ -43,6 +43,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ELGATO_EYETV_DTT_USB_VID	0x0fd9
 #define ELGATO_EYETV_DTT_USB_PID	0x002c
 
+/* nBox: nBox DVB-T Dongle */
+#define AS102_NBOX_DVBT_DONGLE_NAME	"nBox DVB-T Dongle"
+#define NBOX_DVBT_DONGLE_USB_VID	0x0b89
+#define NBOX_DVBT_DONGLE_USB_PID	0x0007
+
 #if (LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18))
 void as102_urb_stream_irq(struct urb *urb, struct pt_regs *regs);
 #else
