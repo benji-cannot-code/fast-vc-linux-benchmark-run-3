fc16b7c3a7c3f7296c7c942f2c0cb421eaa25365
Rajiv Andrade <srajiv@linux.vnet.ibm.com>
2011-11-01 17:00:52 -0200

    TPM: fix transmit_cmd error logic
    It's incorrect to assume that buffers returned by the TPM
    10 bytes long are always error reports. This patches
    parses the error field in its header instead. The error report
    is now being printed using dev_err() instead of dev_dbg(), making
    it easier for users to provide more detailed bug reports.
    
    Signed-off-by: Rajiv Andrade <srajiv@linux.vnet.ibm.com>

--- drivers/char/tpm/tpm_tis.c
+++ drivers/char/tpm/tpm_tis.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/freezer.h>
 #include "tpm.h"
 
-#define TPM_HEADER_SIZE 10
-
 enum tis_access {
 	TPM_ACCESS_VALID = 0x80,
 	TPM_ACCESS_ACTIVE_LOCALITY = 0x20,
