6fc70673bc0eb2dd9eca225ccc3317e1b420d0c2
Magnus Damm <damm@opensource.se>
2011-10-31 17:12:09 -0700

    drivers/leds/leds-renesas-tpu.c: move Renesas TPU LED driver platform data
    Use the platform_data include directory for the TPU LED driver, as
    suggested by Paul Mundt.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Cc: Paul Mundt <lethal@linux-sh.org>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/leds/leds-renesas-tpu.c
+++ drivers/leds/leds-renesas-tpu.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/clk.h>
 #include <linux/leds.h>
-#include <linux/leds-renesas-tpu.h>
+#include <linux/platform_data/leds-renesas-tpu.h>
 #include <linux/gpio.h>
 #include <linux/err.h>
 #include <linux/slab.h>
