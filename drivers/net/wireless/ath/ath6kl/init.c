92dc971d8b0a6058c9d92bcdb3593b74305877d7
Kalle Valo <kvalo@qca.qualcomm.com>
2011-11-03 12:18:31 +0200

    ath6kl: add suspend_cutpower module parameter
    This is to force ath6kl to power off hardware during suspend even if
    sdio support keep power. This is needed, for example, when sdio
    controller is buggy or maximum powersaving is desired.
    
    Usage:
    
    insmod ath6kl.ko suspend_cutpower=1
    
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/init.c
+++ drivers/net/wireless/ath/ath6kl/init.c
@@ -28,9 +28,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 unsigned int debug_mask;
 static unsigned int testmode;
+static bool suspend_cutpower;
 
 module_param(debug_mask, uint, 0644);
 module_param(testmode, uint, 0644);
+module_param(suspend_cutpower, bool, 0444);
 
 /*
  * Include definitions here that can be used to tune the WLAN module
@@ -1596,6 +1598,9 @@ int ath6kl_core_init(struct ath6kl *ar)
 
 	ar->conf_flags = ATH6KL_CONF_IGNORE_ERP_BARKER |
 			 ATH6KL_CONF_ENABLE_11N | ATH6KL_CONF_ENABLE_TX_BURST;
+
+	if (suspend_cutpower)
+		ar->conf_flags |= ATH6KL_CONF_SUSPEND_CUTPOWER;
 
 	ar->wiphy->flags |= WIPHY_FLAG_SUPPORTS_FW_ROAM |
 			    WIPHY_FLAG_HAVE_AP_SME;
