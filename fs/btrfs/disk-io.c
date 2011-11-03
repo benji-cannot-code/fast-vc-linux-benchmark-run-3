0c439eeba78b14d2b99e199e8fb1031265b440d2
Chris Mason <chris.mason@oracle.com>
2011-11-03 15:21:39 -0400

    Btrfs: stop the readahead threads on failed mount
    If we don't stop them, they linger around corrupting
    memory by using pointers to freed things.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -2445,6 +2445,7 @@ fail_tree_roots:
 
 fail_sb_buffer:
 	btrfs_stop_workers(&fs_info->generic_worker);
+	btrfs_stop_workers(&fs_info->readahead_workers);
 	btrfs_stop_workers(&fs_info->fixup_workers);
 	btrfs_stop_workers(&fs_info->delalloc_workers);
 	btrfs_stop_workers(&fs_info->workers);
