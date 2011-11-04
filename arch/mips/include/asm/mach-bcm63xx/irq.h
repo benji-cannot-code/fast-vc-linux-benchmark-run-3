4960ce6861ffbb97228249f4f0d675379041da71
Maxime Bizon <mbizon@freebox.fr>
2011-11-04 19:09:32 +0100

    MIPS: BCM63XX: Prepare irq code to handle different external irq hardware implementation.
    External irq only works for 6348, change code to prepare support of
    other CPUs.
    
    Signed-off-by: Maxime Bizon <mbizon@freebox.fr>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/2895/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

---
+++ arch/mips/include/asm/mach-bcm63xx/irq.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ASM_MACH_BCM63XX_IRQ_H
+#define __ASM_MACH_BCM63XX_IRQ_H
+
+#define NR_IRQS	128
+#define MIPS_CPU_IRQ_BASE 0
+
+#endif
