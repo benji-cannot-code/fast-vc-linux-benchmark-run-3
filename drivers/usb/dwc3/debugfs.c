de32ddc044cd0ba48c82a58a1c4b7147a35ab1f0
Felipe Balbi <balbi@ti.com>
2011-11-04 12:40:05 +0200

    usb: dwc3: fix sparse errors
    sparse caught three mistakes on this driver,
    fix them:
    
    drivers/usb/dwc3/ep0.c:806:29: warning: duplicate const
    drivers/usb/dwc3/debugfs.c:481:15: warning: symbol 'dwc3_debugfs_init' \
    	was not declared. Should it be static?
    drivers/usb/dwc3/debugfs.c:518:16: warning: symbol 'dwc3_debugfs_exit' \
    	was not declared. Should it be static?
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/debugfs.c
+++ drivers/usb/dwc3/debugfs.c
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "core.h"
 #include "gadget.h"
 #include "io.h"
+#include "debug.h"
 
 struct dwc3_register {
 	const char	*name;
