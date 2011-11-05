97250e583dde08b5af88bca50948f5b664a0a400
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-05 11:16:05 +0000

    ARM: restart: highbank: use new restart hook
    Hook these platforms restart code into the new restart hook rather
    than using arch_reset().
    
    Acked-by: Rob Herring <rob.herring@calxeda.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-highbank/system.c
+++ arch/arm/mach-highbank/system.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "core.h"
 #include "sysregs.h"
 
-void arch_reset(char mode, const char *cmd)
+void highbank_restart(char mode, const char *cmd)
 {
 	if (mode == 'h')
 		hignbank_set_pwr_hard_reset();
