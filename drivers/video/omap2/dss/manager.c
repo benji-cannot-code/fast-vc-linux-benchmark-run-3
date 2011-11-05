9d5cac27003b9f571d67bb20fcaf444b06eabfe2
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-11-05 10:44:48 +0200

    OMAPDSS: store managers in an array
    Overlay managers are stored in a linked list. There's no need for this
    list, as an array would do just as fine.
    
    This patch changes the code to use an array for overlay managers.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/manager.c
+++ drivers/video/omap2/dss/manager.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dss_features.h"
 
 static int num_managers;
-static struct list_head manager_list;
+static struct omap_overlay_manager *managers;
 
 static ssize_t manager_name_show(struct omap_overlay_manager *mgr, char *buf)
 {
@@ -586,25 +586,19 @@ static void omap_dss_mgr_get_info(struct omap_overlay_manager *mgr,
 	*info = mgr->info;
 }
 
-static void omap_dss_add_overlay_manager(struct omap_overlay_manager *manager)
-{
-	++num_managers;
-	list_add_tail(&manager->list, &manager_list);
-}
-
 int dss_init_overlay_managers(struct platform_device *pdev)
 {
 	int i, r;
 
-	INIT_LIST_HEAD(&manager_list);
+	num_managers = dss_feat_get_num_mgrs();
 
-	num_managers = 0;
+	managers = kzalloc(sizeof(struct omap_overlay_manager) * num_managers,
+			GFP_KERNEL);
 
-	for (i = 0; i < dss_feat_get_num_mgrs(); ++i) {
-		struct omap_overlay_manager *mgr;
-		mgr = kzalloc(sizeof(*mgr), GFP_KERNEL);
+	BUG_ON(managers == NULL);
 
-		BUG_ON(mgr == NULL);
+	for (i = 0; i < num_managers; ++i) {
+		struct omap_overlay_manager *mgr = &managers[i];
 
 		switch (i) {
 		case 0:
@@ -635,15 +629,11 @@ int dss_init_overlay_managers(struct platform_device *pdev)
 
 		dss_overlay_setup_dispc_manager(mgr);
 
-		omap_dss_add_overlay_manager(mgr);
-
 		r = kobject_init_and_add(&mgr->kobj, &manager_ktype,
 				&pdev->dev.kobj, "manager%d", i);
 
-		if (r) {
+		if (r)
 			DSSERR("failed to create sysfs file\n");
-			continue;
-		}
 	}
 
 	return 0;
@@ -651,17 +641,17 @@ int dss_init_overlay_managers(struct platform_device *pdev)
 
 void dss_uninit_overlay_managers(struct platform_device *pdev)
 {
-	struct omap_overlay_manager *mgr;
+	int i;
 
-	while (!list_empty(&manager_list)) {
-		mgr = list_first_entry(&manager_list,
-				struct omap_overlay_manager, list);
-		list_del(&mgr->list);
+	for (i = 0; i < num_managers; ++i) {
+		struct omap_overlay_manager *mgr = &managers[i];
+
 		kobject_del(&mgr->kobj);
 		kobject_put(&mgr->kobj);
-		kfree(mgr);
 	}
 
+	kfree(managers);
+	managers = NULL;
 	num_managers = 0;
 }
 
@@ -673,15 +663,10 @@ EXPORT_SYMBOL(omap_dss_get_num_overlay_managers);
 
 struct omap_overlay_manager *omap_dss_get_overlay_manager(int num)
 {
-	int i = 0;
-	struct omap_overlay_manager *mgr;
+	if (num >= num_managers)
+		return NULL;
 
-	list_for_each_entry(mgr, &manager_list, list) {
-		if (i++ == num)
-			return mgr;
-	}
-
-	return NULL;
+	return &managers[num];
 }
 EXPORT_SYMBOL(omap_dss_get_overlay_manager);
 
