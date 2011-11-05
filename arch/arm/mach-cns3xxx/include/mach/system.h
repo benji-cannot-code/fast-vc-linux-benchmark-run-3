1e9076b2d8daaad117f9e0a54b4cbd92fd61e9b8
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-05 15:32:23 +0000

    ARM: restart: cns3xxx: use new restart hook
    Hook these platforms restart code into the new restart hook rather
    than using arch_reset().
    
    Acked-by: Anton Vorontsov <cbouatmailru@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-cns3xxx/include/mach/system.h
+++ arch/arm/mach-cns3xxx/include/mach/system.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MACH_SYSTEM_H
 #define __MACH_SYSTEM_H
 
-#include <linux/io.h>
 #include <asm/proc-fns.h>
 
 static inline void arch_idle(void)
@@ -24,6 +23,8 @@ static inline void arch_idle(void)
 	cpu_do_idle();
 }
 
-void arch_reset(char mode, const char *cmd);
+static inline void arch_reset(char mode, const char *cmd)
+{
+}
 
 #endif
