00ef2296767c229a8a0dac573d52e7aff01f8001
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-05 11:26:32 +0000

    ARM: restart: iop3xx: use new restart hook
    Hook these platforms restart code into the arm_pm_restart hook rather
    than using arch_reset().
    
    In doing so, we split out the n2100 platform specific restart handler
    into the n2100 platform file.
    
    Acked-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-iop32x/include/mach/system.h
+++ arch/arm/mach-iop32x/include/mach/system.h
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#include <asm/mach-types.h>
-#include <asm/hardware/iop3xx.h>
-#include <mach/n2100.h>
-
 static inline void arch_idle(void)
 {
 	cpu_do_idle();
@@ -19,15 +15,4 @@ static inline void arch_idle(void)
 
 static inline void arch_reset(char mode, const char *cmd)
 {
-	if (machine_is_n2100()) {
-		gpio_line_set(N2100_HARDWARE_RESET, GPIO_LOW);
-		gpio_line_config(N2100_HARDWARE_RESET, GPIO_OUT);
-		while (1)
-			;
-	}
-
-	*IOP3XX_PCSR = 0x30;
-
-	/* Jump into ROM at address 0 */
-	soft_restart(0);
 }
--- arch/arm/plat-iop/restart.c
+++ arch/arm/plat-iop/restart.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/arm/mach-iop33x/include/mach/system.h
+ * restart.c
  *
  * Copyright (C) 2001 MontaVista Software, Inc.
  *
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-static inline void arch_idle(void)
-{
-	cpu_do_idle();
-}
+#include <asm/hardware/iop3xx.h>
+#include <mach/hardware.h>
 
-static inline void arch_reset(char mode, const char *cmd)
+void iop3xx_restart(char mode, const char *cmd)
 {
+	*IOP3XX_PCSR = 0x30;
+
+	/* Jump into ROM at address 0 */
+	soft_restart(0);
 }
