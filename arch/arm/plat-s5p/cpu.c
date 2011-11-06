f05339b527398861d1e8f85ef2a9476b5688a4d1
Kukjin Kim <kgene.kim@samsung.com>
2011-11-06 13:54:56 +0900

    ARM: EXYNOS: Add ARCH_EXYNOS and reorganize arch/arm/mach-exynos
    The arch/arm/mach-exynos4 directory (CONFIG_ARCH_EXYNOS4) has
    made for plaforms based on EXYNOS4 SoCs. But since upcoming
    Samsung's SoCs such as EXYNOS5 (ARM Cortex A15) can reuse most
    codes in current mach-exynos4, one mach-exynos directory will
    be used for them.
    
    This patch changes to CONFIG_ARCH_EXYNOS (arch/arm/mach-exynos)
    but keeps original CONFIG_ARCH_EXYNOS4 in mach-exynos/Kconfig to
    avoid changing in driver side.
    
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-s5p/cpu.c
+++ arch/arm/plat-s5p/cpu.c
@@ -76,7 +76,7 @@ static struct cpu_table cpu_ids[] __initdata = {
 		.map_io		= exynos4_map_io,
 		.init_clocks	= exynos4_init_clocks,
 		.init_uarts	= exynos4_init_uarts,
-		.init		= exynos4_init,
+		.init		= exynos_init,
 		.name		= name_exynos4210,
 	}, {
 		.idcode		= EXYNOS4212_CPU_ID,
@@ -84,7 +84,7 @@ static struct cpu_table cpu_ids[] __initdata = {
 		.map_io		= exynos4_map_io,
 		.init_clocks	= exynos4_init_clocks,
 		.init_uarts	= exynos4_init_uarts,
-		.init		= exynos4_init,
+		.init		= exynos_init,
 		.name		= name_exynos4212,
 	}, {
 		.idcode		= EXYNOS4412_CPU_ID,
@@ -92,7 +92,7 @@ static struct cpu_table cpu_ids[] __initdata = {
 		.map_io		= exynos4_map_io,
 		.init_clocks	= exynos4_init_clocks,
 		.init_uarts	= exynos4_init_uarts,
-		.init		= exynos4_init,
+		.init		= exynos_init,
 		.name		= name_exynos4412,
 	},
 };
