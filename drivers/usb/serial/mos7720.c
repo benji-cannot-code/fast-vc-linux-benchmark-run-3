a19e739dd8a23fc196a1f76bab5134f1a5506e34
Johan Hovold <jhovold@gmail.com>
2011-11-06 19:06:26 +0100

    USB: mos7720: remove incorrect read-urb check
    Remove incorrect and unnecessary check for port->read_urb which is not
    set to NULL, contrary to what seems to be assumed, when urb is killed.
    
    Compile only-tested.
    
    Signed-off-by: Johan Hovold <jhovold@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/mos7720.c
+++ drivers/usb/serial/mos7720.c
@@ -940,11 +940,6 @@ static void mos7720_bulk_in_callback(struct urb *urb)
 	}
 	tty_kref_put(tty);
 
-	if (!port->read_urb) {
-		dbg("URB KILLED !!!");
-		return;
-	}
-
 	if (port->read_urb->status != -EINPROGRESS) {
 		port->read_urb->dev = port->serial->dev;
 
@@ -1786,11 +1781,6 @@ static void mos7720_set_termios(struct tty_struct *tty,
 
 	/* change the port settings to the new ones specified */
 	change_port_settings(tty, mos7720_port, old_termios);
-
-	if (!port->read_urb) {
-		dbg("%s", "URB KILLED !!!!!");
-		return;
-	}
 
 	if (port->read_urb->status != -EINPROGRESS) {
 		port->read_urb->dev = serial->dev;
