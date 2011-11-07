91d3cea57ea8cb826ae0b6d64b38910e86863d21
Raja Mani <rmani@qca.qualcomm.com>
2011-11-07 22:52:45 +0200

    ath6kl: Add new state for WOW mode
    In addition to existing deep sleep and cut pwr mode, new state
    is added in ath6kl_cfg_suspend_mode as well as in ath6kl_state for WOW.
    
    Signed-off-by: Raja Mani <rmani@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/cfg80211.h
+++ drivers/net/wireless/ath/ath6kl/cfg80211.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum ath6kl_cfg_suspend_mode {
 	ATH6KL_CFG_SUSPEND_DEEPSLEEP,
 	ATH6KL_CFG_SUSPEND_CUTPOWER,
+	ATH6KL_CFG_SUSPEND_WOW
 };
 
 struct net_device *ath6kl_interface_add(struct ath6kl *ar, char *name,
