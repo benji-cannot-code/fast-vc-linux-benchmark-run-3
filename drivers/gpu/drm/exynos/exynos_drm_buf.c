5ddda497f51d10128365b174f8bd7eb66208cfc2
Inki Dae <inki.dae@samsung.com>
2011-11-09 16:50:30 +0900

    drm/exynos: removed unnecessary variable.
    Signed-off-by: Inki Dae <inki.dae@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_buf.c
+++ drivers/gpu/drm/exynos/exynos_drm_buf.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "exynos_drm_drv.h"
 #include "exynos_drm_buf.h"
 
-static DEFINE_MUTEX(exynos_drm_buf_lock);
-
 static int lowlevel_buffer_allocate(struct drm_device *dev,
 		struct exynos_drm_buf_entry *entry)
 {
