a67df50d0017b637fb01b3663e6d42f76ba17d4f
Tomas Winkler <tomas.winkler@intel.com>
2011-11-09 08:26:39 -0300

    [media] easycap: remove unused members of struct easycap
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/media/easycap/easycap.h
+++ drivers/staging/media/easycap/easycap.h
@@ -325,8 +325,6 @@ struct easycap {
 	int lost[INPUT_MANY];
 	int merit[180];
 
-	long long int dnbydt;
-
 	int    video_interface;
 	int    video_altsetting_on;
 	int    video_altsetting_off;
@@ -354,7 +352,6 @@ struct easycap {
 	u8 *pcache;
 	int video_mt;
 	int audio_mt;
-	long long audio_bytes;
 	u32 isequence;
 
 	int vma_many;
@@ -451,9 +448,6 @@ struct easycap {
  *  SOUND PROPERTIES
  */
 /*---------------------------------------------------------------------------*/
-
-	int audio_buffer_many;
-
 	int allocation_audio_urb;
 	int allocation_audio_page;
 	int allocation_audio_struct;
