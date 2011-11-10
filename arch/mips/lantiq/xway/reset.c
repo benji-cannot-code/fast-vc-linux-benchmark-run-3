d26ca5e8065d824e8a3a18705e9292c70f54ccc3
John Crispin <blogic@openwrt.org>
2011-11-10 21:33:07 +0100

    MIPS: lantiq: use export.h in favour of module.h
    The code located at arch/mips/lantiq/ included module.h to be able to use
    the EXPORT_SYMBOL* macros. These can now be directly included using
    export.h.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/2937/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/lantiq/xway/reset.c
+++ arch/mips/lantiq/xway/reset.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/ioport.h>
 #include <linux/pm.h>
-#include <linux/module.h>
+#include <linux/export.h>
 #include <asm/reboot.h>
 
 #include <lantiq_soc.h>
