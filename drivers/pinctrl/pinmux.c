3eaffc1c6c1b93bb3f1f37b67debedb20b92ca66
Linus Walleij <linus.walleij@linaro.org>
2011-11-10 09:27:41 +0100

    pinctrl: documentation update
    Update the docs removing an obsolete __refdata tag and document
    the mysterious return value of pin_free(). And fixes up some various
    confusions in the pinctrl documentation.
    
    Reported-by: Rajendra Nayak <rnayak@ti.com>
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Reported-by: Thomas Abraham <thomas.abraham@linaro.org>
    Reported-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinmux.c
+++ drivers/pinctrl/pinmux.c
@@ -175,6 +175,10 @@ out:
  * @pin: the pin to free
  * @gpio_range: the range matching the GPIO pin if this is a request for a
  *	single GPIO pin
+ *
+ * This function returns a pointer to the function name in use. This is used
+ * for callers that dynamically allocate a function name so it can be freed
+ * once the pin is free. This is done for GPIO request functions.
  */
 static const char *pin_free(struct pinctrl_dev *pctldev, int pin,
 			    struct pinctrl_gpio_range *gpio_range)
@@ -920,7 +924,7 @@ int pinmux_hog_maps(struct pinctrl_dev *pctldev)
 }
 
 /**
- * pinmux_hog_maps() - unhog specific map entries on controller device
+ * pinmux_unhog_maps() - unhog specific map entries on controller device
  * @pctldev: the pin control device to unhog entries on
  */
 void pinmux_unhog_maps(struct pinctrl_dev *pctldev)
