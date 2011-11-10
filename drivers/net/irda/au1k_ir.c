b2d5b730c6c3f3ddff8e5de0717853d4477ac9cd
Manuel Lauss <manuel.lauss@googlemail.com>
2011-11-10 12:06:22 +0000

    MIPS: Alchemy: one kernel for DB1000/DB1500/DB1100
    These 3 boards are very similar; with this patch a single kernel image
    which runs on all three can be built.
    
    Tested on DB1500 and DB1100.
    
    Signed-off-by: Manuel Lauss <manuel.lauss@googlemail.com>
    To: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/2872/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- drivers/net/irda/au1k_ir.c
+++ drivers/net/irda/au1k_ir.c
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq.h>
 #include <asm/io.h>
 #include <asm/au1000.h>
-#if defined(CONFIG_MIPS_DB1000) || defined(CONFIG_MIPS_DB1100)
+#if defined(CONFIG_MIPS_DB1000)
 #include <asm/mach-db1x00/bcsr.h>
 #else 
 #error au1k_ir: unsupported board
@@ -275,7 +275,7 @@ static int au1k_irda_net_init(struct net_device *dev)
 		aup->tx_db_inuse[i] = pDB;
 	}
 
-#if defined(CONFIG_MIPS_DB1000) || defined(CONFIG_MIPS_DB1100)
+#if defined(CONFIG_MIPS_DB1000)
 	/* power on */
 	bcsr_mod(BCSR_RESETS, BCSR_RESETS_IRDA_MODE_MASK,
 			      BCSR_RESETS_IRDA_MODE_FULL);
@@ -663,7 +663,7 @@ au1k_irda_set_speed(struct net_device *dev, int speed)
 	u32 control;
 	int ret = 0, timeout = 10, i;
 	volatile ring_dest_t *ptxd;
-#if defined(CONFIG_MIPS_DB1000) || defined(CONFIG_MIPS_DB1100)
+#if defined(CONFIG_MIPS_DB1000)
 	unsigned long irda_resets;
 #endif
 
@@ -712,14 +712,14 @@ au1k_irda_set_speed(struct net_device *dev, int speed)
 	}
 
 	if (speed == 4000000) {
-#if defined(CONFIG_MIPS_DB1000) || defined(CONFIG_MIPS_DB1100)
+#if defined(CONFIG_MIPS_DB1000)
 		bcsr_mod(BCSR_RESETS, 0, BCSR_RESETS_FIR_SEL);
 #else /* Pb1000 and Pb1100 */
 		writel(1<<13, CPLD_AUX1);
 #endif
 	}
 	else {
-#if defined(CONFIG_MIPS_DB1000) || defined(CONFIG_MIPS_DB1100)
+#if defined(CONFIG_MIPS_DB1000)
 		bcsr_mod(BCSR_RESETS, BCSR_RESETS_FIR_SEL, 0);
 #else /* Pb1000 and Pb1100 */
 		writel(readl(CPLD_AUX1) & ~(1<<13), CPLD_AUX1);
