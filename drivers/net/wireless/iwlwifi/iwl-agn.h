c96988bea5c3a78f32d52687895c81fee996ad51
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2011-11-10 06:55:11 -0800

    iwlwifi: move all mac80211 related functions to one place
    There are many mac80211 callback functions in the driver, as current
    implementation, those functions are scatter everywhere which is very difficult
    to find and maintain, move all the mac80211 callback functions into single
    file. There should be no functional changes
    
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn.h
+++ drivers/net/wireless/iwlwifi/iwl-agn.h
@@ -66,6 +66,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "iwl-dev.h"
 
+struct iwlagn_ucode_capabilities {
+	u32 max_probe_length;
+	u32 standard_phy_calibration_size;
+	u32 flags;
+};
+
 extern struct ieee80211_ops iwlagn_hw_ops;
 
 int iwl_reset_ict(struct iwl_trans *trans);
@@ -77,6 +83,15 @@ static inline void iwl_set_calib_hdr(struct iwl_calib_hdr *hdr, u8 cmd)
 	hdr->groups_num = 1;
 	hdr->data_valid = 1;
 }
+
+void __iwl_down(struct iwl_priv *priv);
+void iwl_down(struct iwl_priv *priv);
+void iwlagn_prepare_restart(struct iwl_priv *priv);
+
+/* MAC80211 */
+struct ieee80211_hw *iwl_alloc_all(void);
+int iwlagn_mac_setup_register(struct iwl_priv *priv,
+			      struct iwlagn_ucode_capabilities *capa);
 
 /* RXON */
 int iwlagn_set_pan_params(struct iwl_priv *priv);
