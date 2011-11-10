c67c0640fc6677a7756f910bb0170b58511da73e
Franky Lin <frankyl@broadcom.com>
2011-11-10 20:30:22 +0100

    brcm80211: fmac: add corerev function for bcm4330 chip
    This patch is part of the series adding new backplane support
    
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/sdio_chip.h
+++ drivers/net/wireless/brcm80211/brcmfmac/sdio_chip.h
@@ -61,6 +61,7 @@ struct chip_core_info {
 	u32 base;
 	u32 wrapbase;
 	u32 caps;
+	u32 cib;
 };
 
 struct chip_info {
@@ -75,6 +76,8 @@ struct chip_info {
 	u32 ramsize;
 
 	bool (*iscoreup)(struct brcmf_sdio_dev *sdiodev, struct chip_info *ci,
+			 u16 coreid);
+	u32 (*corerev)(struct brcmf_sdio_dev *sdiodev, struct chip_info *ci,
 			 u16 coreid);
 };
 
