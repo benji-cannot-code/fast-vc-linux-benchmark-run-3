4b7aad1bb1b882c70092405e59cad6fed4212939
Ohad Ben-Cohen <ohad@wizery.com>
2011-11-10 11:32:27 +0200

    iommu/omap: announce supported page sizes
    Let the IOMMU core know we support 4KiB, 64KiB, 1MiB and 16MiB page sizes.
    
    This way the IOMMU core can split any arbitrary-sized physically
    contiguous regions (that it needs to map) as needed.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Cc: Hiroshi DOYU <hdoyu@nvidia.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/omap-iommu.c
+++ drivers/iommu/omap-iommu.c
@@ -34,6 +34,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	     (__i < (n)) && (cr = __iotlb_read_cr((obj), __i), true);	\
 	     __i++)
 
+/* bitmap of the page sizes currently supported */
+#define OMAP_IOMMU_PGSIZES	(SZ_4K | SZ_64K | SZ_1M | SZ_16M)
+
 /**
  * struct omap_iommu_domain - omap iommu domain
  * @pgtable:	the page table
@@ -1208,6 +1211,7 @@ static struct iommu_ops omap_iommu_ops = {
 	.unmap		= omap_iommu_unmap,
 	.iova_to_phys	= omap_iommu_iova_to_phys,
 	.domain_has_cap	= omap_iommu_domain_has_cap,
+	.pgsize_bitmap	= OMAP_IOMMU_PGSIZES,
 };
 
 static int __init omap_iommu_init(void)
