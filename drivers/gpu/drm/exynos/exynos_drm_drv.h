96830b13b505fd8eaa868e83cc7379c11d6d422b
Inki Dae <inki.dae@samsung.com>
2011-11-12 16:57:42 +0900

    drm/exynos: include linux/module.h
    Signed-off-by: Inki Dae <inki.dae@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_drv.h
+++ drivers/gpu/drm/exynos/exynos_drm_drv.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _EXYNOS_DRM_DRV_H_
 #define _EXYNOS_DRM_DRV_H_
 
+#include <linux/module.h>
 #include "drm.h"
 
 #define MAX_CRTC	2
