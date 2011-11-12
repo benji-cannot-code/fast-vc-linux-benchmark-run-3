76169cd43436d6bfac2ce31b8421bc28fe606bd2
Manjunath Hadli <manjunath.hadli@ti.com>
2011-11-12 20:36:02 +0530

    ARM: davinci: vpif: move code to driver core header from platform
    Move vpif related definitions for capture and display drivers
    from dm646x platform header file to vpif_types.h inside
    the driver as these definitions are related to driver code
    rather than the platform or board.
    
    This enables reusing this IP across platforms.
    
    Signed-off-by: Manjunath Hadli <manjunath.hadli@ti.com>
    Acked-by: Mauro Carvalho Chehab <mchehab@redhat.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- drivers/media/video/davinci/vpif.h
+++ drivers/media/video/davinci/vpif.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev2.h>
 #include <mach/hardware.h>
 #include <mach/dm646x.h>
+#include <media/davinci/vpif_types.h>
 
 /* Maximum channel allowed */
 #define VPIF_NUM_CHANNELS		(4)
--- drivers/media/video/davinci/vpif_display.h
+++ drivers/media/video/davinci/vpif_display.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-device.h>
 #include <media/videobuf-core.h>
 #include <media/videobuf-dma-contig.h>
+#include <media/davinci/vpif_types.h>
 
 #include "vpif.h"
 
