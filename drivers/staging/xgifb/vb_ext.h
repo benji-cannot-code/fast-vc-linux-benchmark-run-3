351b82e2ecc048a87f75d146c0a26caa4bc747cc
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-11-13 00:09:25 +0200

    staging: xgifb: move XGINew_SenseLCD() to vb_init.c
    The function is used only in vb_init.c, so move it there and make
    it static.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_ext.h
+++ drivers/staging/xgifb/vb_ext.h
@@ -2,7 +2,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _VBEXT_
 #define _VBEXT_
 
-extern unsigned short XGINew_SenseLCD(struct xgi_hw_device_info *,
-				      struct vb_device_info *pVBInfo);
-
 #endif
