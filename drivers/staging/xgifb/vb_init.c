1824b7508f2c2b4fa15023a7a71e50152f88ae16
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-11-13 00:09:26 +0200

    staging: xgifb: delete vb_ext.{c,h}
    Delete empty files.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_init.c
+++ drivers/staging/xgifb/vb_init.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vb_util.h"
 #include "vb_setmode.h"
 #include "vb_init.h"
-#include "vb_ext.h"
 
 
 #include <linux/io.h>
