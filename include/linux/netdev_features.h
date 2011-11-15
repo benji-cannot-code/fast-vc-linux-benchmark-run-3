1a450b49faf4f918dc4ac22d05344d35524c2368
Michał Mirosław <mirq-linux@rere.qmqm.pl>
2011-11-15 15:29:55 +0000

    net: extend netdev_features_t to 64 bits
    Signed-off-by: Michał Mirosław <mirq-linux@rere.qmqm.pl>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdev_features.h
+++ include/linux/netdev_features.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-typedef u32 netdev_features_t;
+typedef u64 netdev_features_t;
 
 enum {
 	NETIF_F_SG_BIT,			/* Scatter/gather IO. */
