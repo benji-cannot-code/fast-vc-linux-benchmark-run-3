1e6dbf1a85e51848232915fed438ca671d2e8954
Grant Grundler <grundler@google.com>
2011-11-15 07:12:43 +0000

    net-next:asix: V2 Update VERSION
    Only update VERSION to reflect previous changes.
    
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/asix.c
+++ drivers/net/usb/asix.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb/usbnet.h>
 #include <linux/slab.h>
 
-#define DRIVER_VERSION "26-Sep-2011"
+#define DRIVER_VERSION "08-Nov-2011"
 #define DRIVER_NAME "asix"
 
 /* ASIX AX8817X based USB 2.0 Ethernet Devices */
