bdafb208bbb4bc709ebabf71ff9df3f02a22205c
Tomi Valkeinen <tomi.valkeinen@ti.com>
2011-11-15 16:37:53 +0200

    OMAPDSS: APPLY: rewrite overlay enable/disable
    Overlays are currently enabled and disabled with a boolean in the struct
    omap_overlay_info. The overlay info is set with ovl->set_overlay_info(),
    and made into use with mgr->apply().
    
    This doesn't work properly, as the enable/disable status may affect also
    other overlays, for example when using fifo-merge. Thus the enabling and
    disabling of the overlay needs to be done outside the normal overlay
    configuration.
    
    This patch achieves that by doing the following things:
    
    1) Add function pointers to struct omap_overlay: enable(), disable() and
    is_enabled(). These are used to do the obvious. The functions may block.
    
    2) Move the "enabled" field from struct omap_overlay to ovl_priv_data.
    
    3) Add a new route for settings to be applied to the HW, called
    "extra_info". The status of the normal info and extra_info are tracked
    separately.
    
    The point here is to allow the normal info to be changed and
    applied in non-blocking matter, whereas the extra_info can only be
    changed when holding the mutex. This makes it possible to, for example,
    set the overlay enable flag, apply it, and wait until the HW has taken
    the flag into use.
    
    This is not possible if the enable flag would be in the normal info, as
    a new value for the flag could be set at any time from the users of
    omapdss.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -2068,6 +2068,8 @@ static int omapfb_create_framebuffers(struct omapfb2_device *fbdev)
 		if (ofbi->num_overlays > 0) {
 			struct omap_overlay *ovl = ofbi->overlays[0];
 
+			ovl->manager->apply(ovl->manager);
+
 			r = omapfb_overlay_enable(ovl, 1);
 
 			if (r) {
