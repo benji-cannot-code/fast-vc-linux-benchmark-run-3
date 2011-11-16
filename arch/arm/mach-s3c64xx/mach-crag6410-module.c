ed4abe3e177bea0bb2c5005b19e8e78c42219af3
Kukjin Kim <kgene.kim@samsung.com>
2011-11-17 01:14:38 +0900

    ARM: SAMSUNG: inclusion export.h instead of module.h
    Only for THIS_MODULE, the <linux/export.h> file should be
    added instead of <linux/module.h>.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c64xx/mach-crag6410-module.c
+++ arch/arm/mach-s3c64xx/mach-crag6410-module.c
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
-#include <linux/module.h>
+#include <linux/export.h>
 #include <linux/interrupt.h>
 #include <linux/i2c.h>
 
