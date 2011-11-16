2f3d2641c6e3bd706066853dcdfb96c15b7c63b1
Leon Romanovsky <leon@leon.nu>
2011-11-16 12:06:58 +0200

    ASoC: Convert ALC5632 codec to use regmap API
    Signed-off-by: Leon Romanovsky <leon@leon.nu>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/alc5632.h
+++ sound/soc/codecs/alc5632.h
@@ -247,4 +247,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ALC5632_VENDOR_ID1			0x7C
 #define ALC5632_VENDOR_ID2			0x7E
 
+#define ALC5632_MAX_REGISTER        0x7E
+
 #endif
