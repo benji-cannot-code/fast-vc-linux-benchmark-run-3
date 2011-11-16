c8d6950558b175fd04fdaa2ba6f4c971b9403342
Linus Walleij <linus.walleij@linaro.org>
2011-11-16 21:58:10 +0100

    pinctrl: support pinconfig on the U300
    This adds pin configuration support for the U300 driver pair,
    we can now read out the biasing and drive mode in debugfs and
    configure it using the new configuration API.
    
    ChangeLog v1->v2:
    - Migrate to pin config and generic pin config changes.
    ChangeLog v2->v3:
    - Adjust to generic pin config changes in v7 patch set.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

---
+++ drivers/pinctrl/pinctrl-coh901.h
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+int u300_gpio_config_get(struct gpio_chip *chip,
+			 unsigned offset,
+			 unsigned long *config);
+int u300_gpio_config_set(struct gpio_chip *chip, unsigned offset,
+			 enum pin_config_param param);
