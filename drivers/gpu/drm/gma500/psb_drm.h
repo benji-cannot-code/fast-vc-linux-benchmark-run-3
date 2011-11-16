7a0e7172fc0501ba06d3a25bbc374ebd06871d4a
Alan Cox <alan@linux.intel.com>
2011-11-16 22:39:32 +0000

    gma500: kill off NUM_PIPE define
    We don't want this external in case someone adds more to the hardware. We
    want it out of the ABI.
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/gma500/psb_drm.h
+++ drivers/gpu/drm/gma500/psb_drm.h
@@ -23,9 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _PSB_DRM_H_
 #define _PSB_DRM_H_
 
-#define PSB_NUM_PIPE 3
-
-
 /*
  *	Manage the LUT for an output
  */
