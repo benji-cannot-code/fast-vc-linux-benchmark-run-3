c3001bd20c2ffcc6d76deaa6a975a0de2ad262fb
Al Viro <viro@zeniv.linux.org.uk>
2011-11-17 01:40:05 -0500

    btrfs: merge free_fs_info() calls on fill_super failures
    ... all the way up into btrfs_mount().
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -2428,7 +2428,6 @@ retry_root_backup:
 		up_read(&fs_info->cleanup_work_sem);
 		if (err) {
 			close_ctree(tree_root);
-			free_fs_info(fs_info);
 			return err;
 		}
 	}
@@ -2479,7 +2478,6 @@ fail_srcu:
 	cleanup_srcu_struct(&fs_info->subvol_srcu);
 fail:
 	btrfs_close_devices(fs_info->fs_devices);
-	free_fs_info(fs_info);
 	return err;
 
 recovery_tree_root:
