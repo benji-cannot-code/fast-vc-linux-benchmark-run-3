5e28a4f58ca290c171a6806fc17bdb4071b5c82d
Vasanthakumar Thiagarajan <vthiagar@qca.qualcomm.com>
2011-11-18 10:05:26 +0530

    ath6kl: Remove modparam multi_norm_if_support
    This modparam was introduced to enable non-p2p mode operation
    on two virtual interfaces. It does not seem to be necessary to
    have a separate module parameter to do that. Instead, this option
    can be enabled when any one of the interfaces is not going to be
    used for p2p (ath6kl_p2p).
    
    Signed-off-by: Vasanthakumar Thiagarajan <vthiagar@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/cfg80211.c
+++ drivers/net/wireless/ath/ath6kl/cfg80211.c
@@ -22,10 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "testmode.h"
 
 static unsigned int ath6kl_p2p;
-static unsigned int multi_norm_if_support;
 
 module_param(ath6kl_p2p, uint, 0644);
-module_param(multi_norm_if_support, uint, 0644);
 
 #define RATETAB_ENT(_rate, _rateid, _flags) {   \
 	.bitrate    = (_rate),                  \
@@ -2453,14 +2451,13 @@ struct ath6kl *ath6kl_core_alloc(struct device *dev)
 	}
 
 	ar = wiphy_priv(wiphy);
-	if (!multi_norm_if_support)
-		ar->p2p = !!ath6kl_p2p;
+	ar->p2p = !!ath6kl_p2p;
 	ar->wiphy = wiphy;
 	ar->dev = dev;
 
 	ar->vif_max = 1;
 
-	if (multi_norm_if_support)
+	if (!ar->p2p)
 		ar->max_norm_iface = 2;
 	else
 		ar->max_norm_iface = 1;
