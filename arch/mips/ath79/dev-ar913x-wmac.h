d405bffab1449464e61357f73d63f89bf89daa32
Gabor Juhos <juhosg@openwrt.org>
2011-11-18 00:17:53 +0000

    MIPS: ath79: Add AR933x specific WMAC setup code
    The wireless MAC of the AR933x SoCs uses different base address, and
    requires different setup code.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    Cc: Imre Kaloz <kaloz@openwrt.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/3030/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ath79/dev-ar913x-wmac.h
+++ arch/mips/ath79/dev-ar913x-wmac.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- *  Atheros AR913X SoC built-in WMAC device support
+ *  Atheros AR913X/AR933X SoC built-in WMAC device support
  *
- *  Copyright (C) 2008-2010 Gabor Juhos <juhosg@openwrt.org>
+ *  Copyright (C) 2008-2011 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2008 Imre Kaloz <kaloz@openwrt.org>
  *
  *  This program is free software; you can redistribute it and/or modify it
