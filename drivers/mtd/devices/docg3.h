0a2dcc33e96e9adb0e700ec1b63a74f8ee7fa699
Robert Jarzmik <robert.jarzmik@free.fr>
2011-11-19 16:02:46 +0100

    mtd: docg3: fix BCH registers
    BCH registers are contiguous, not on every byte. Fix the
    register definitions.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Reviewed-by: Ivan Djelic <ivan.djelic@parrot.com>
    Reviewed-by: Mike Dunn <mikedunn@newsguy.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/devices/docg3.h
+++ drivers/mtd/devices/docg3.h
@@ -106,7 +106,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DOC_ECCCONF1			0x1042
 #define DOC_ECCPRESET			0x1044
 #define DOC_HAMMINGPARITY		0x1046
-#define DOC_BCH_SYNDROM(idx)		(0x1048 + (idx << 1))
+#define DOC_BCH_SYNDROM(idx)		(0x1048 + (idx << 0))
 
 #define DOC_PROTECTION			0x1056
 #define DOC_DPS0_ADDRLOW		0x1060
