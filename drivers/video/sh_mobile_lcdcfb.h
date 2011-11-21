c47350cf06a71f82a3681d89c121ad096e7b09ba
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2011-11-22 00:56:58 +0100

    fbdev: sh_mobile_lcdc: Don't store copy of platform data
    Instead of copying the whole platform data structure to struct
    sh_mobile_lcdc_chan, store a const pointer to the channel platform data.
    
    MERAM configuration information needs to be changed at runtime, so copy
    it to struct sh_mobile_lcdc_chan.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/video/sh_mobile_lcdcfb.h
+++ drivers/video/sh_mobile_lcdcfb.h
@@ -55,7 +55,7 @@ struct sh_mobile_lcdc_entity {
 struct sh_mobile_lcdc_chan {
 	struct sh_mobile_lcdc_priv *lcdc;
 	struct sh_mobile_lcdc_entity *tx_dev;
-	struct sh_mobile_lcdc_chan_cfg cfg;
+	const struct sh_mobile_lcdc_chan_cfg *cfg;
 
 	unsigned long *reg_offs;
 	unsigned long ldmt1r_value;
@@ -67,6 +67,7 @@ struct sh_mobile_lcdc_chan {
 
 	void *fb_mem;
 	unsigned long fb_size;
+
 	dma_addr_t dma_handle;
 	unsigned long pan_offset;
 
