b0185da3e3a01b38f5bb5df681529f6ec14d9f8d
Joerg Roedel <joerg.roedel@amd.com>
2011-11-21 16:50:23 +0100

    iommu/amd: Implement functions to manage GCR3 table
    This patch adds functions necessary to set and clear the
    GCR3 values associated with a particular PASID in an IOMMUv2
    domain.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/amd_iommu_types.h
+++ drivers/iommu/amd_iommu_types.h
@@ -280,6 +280,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DTE_GCR3_SHIFT_B	16
 #define DTE_GCR3_SHIFT_C	43
 
+#define GCR3_VALID		0x01ULL
 
 #define IOMMU_PAGE_MASK (((1ULL << 52) - 1) & ~0xfffULL)
 #define IOMMU_PTE_PRESENT(pte) ((pte) & IOMMU_PTE_P)
