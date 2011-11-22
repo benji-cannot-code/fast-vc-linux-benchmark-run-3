966d4dbc265d451d4721c6b59c148ae66ad13c59
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-22 17:30:28 +0000

    ARM: pgtable: switch to use pgtable-nopud.h
    Nick Piggin noted upon introducing 4level-fixup.h:
    
    | Add a temporary "fallback" header so architectures can run with
    | the 4level pagetables patch without modification. All architectures
    | should be converted to use the folding headers (include/asm-generic/
    | pgtable-nop?d.h) as soon as possible, and the fallback header removed.
    
    This makes ARM compliant with this statement.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm/include/asm/pgalloc.h
+++ arch/arm/include/asm/pgalloc.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define pmd_alloc_one(mm,addr)		({ BUG(); ((pmd_t *)2); })
 #define pmd_free(mm, pmd)		do { } while (0)
-#define pgd_populate(mm,pmd,pte)	BUG()
+#define pud_populate(mm,pmd,pte)	BUG()
 
 extern pgd_t *pgd_alloc(struct mm_struct *mm);
 extern void pgd_free(struct mm_struct *mm, pgd_t *pgd);
