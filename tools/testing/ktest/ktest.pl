7af265a10d3e84dd810f158ccdf67496b51c05f7
Steven Rostedt <srostedt@redhat.com>
2011-11-21 20:12:04 -0500

    ktest: Add default for ssh-user, build-target and target-image
    When a user runs ktest without an argument, or the argument given
    is not a config file that exists, ktest will ask the user a few
    questions to create a simple ktest config file.
    
    A few of the questions should have a default value set, that if anything
    it will make it easier for the user to know what is suppose to
    be in that value.
    
    These new values are:
    
      SSH_USER, BUILD_TARGET and TARGET_IMAGE
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -23,7 +23,6 @@ my %default;
 
 #default opts
 $default{"NUM_TESTS"}		= 1;
-$default{"REBOOT_TYPE"}		= "grub";
 $default{"TEST_TYPE"}		= "test";
 $default{"BUILD_TYPE"}		= "randconfig";
 $default{"MAKE_CMD"}		= "make";
@@ -52,7 +51,14 @@ $default{"REBOOT"}		= "ssh \$SSH_USER\@\$MACHINE reboot";
 $default{"STOP_AFTER_SUCCESS"}	= 10;
 $default{"STOP_AFTER_FAILURE"}	= 60;
 $default{"STOP_TEST_AFTER"}	= 600;
+
+# required, and we will ask users if they don't have them but we keep the default
+# value something that is common.
+$default{"REBOOT_TYPE"}		= "grub";
 $default{"LOCALVERSION"}	= "-test";
+$default{"SSH_USER"}		= "root";
+$default{"BUILD_TARGET"} 	= "arch/x86/boot/bzImage";
+$default{"TARGET_IMAGE"}	= "/boot/vmlinuz-test";
 
 my $ktest_config;
 my $version;
