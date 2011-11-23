f4cbea5610c770ee2e408c241bb8cfa36450727a
Franky Lin <frankyl@broadcom.com>
2011-11-22 17:21:59 -0800

    brcm80211: fmac: change function bus_stop parameter
    Change parameter to device pointer for bus layer interface function
    brcmf_sdbrcm_bus_stop. This is part of the fullmac bus interface
    refactoring.
    
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/dhd_bus.h
+++ drivers/net/wireless/brcm80211/brcmfmac/dhd_bus.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 /* Stop bus module: clear pending frames, disable data flow */
-extern void brcmf_sdbrcm_bus_stop(struct brcmf_sdio *bus);
+extern void brcmf_sdbrcm_bus_stop(struct device *dev);
 
 /* Initialize bus module: prepare for communication w/dongle */
 extern int brcmf_sdbrcm_bus_init(struct device *dev);
