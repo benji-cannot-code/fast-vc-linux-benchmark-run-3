55281c76aad3c2d1b477e9d578915d54f9128f3d
Franky Lin <frankyl@broadcom.com>
2011-11-22 17:21:47 -0800

    brcm80211: fmac: move module init/exit to sdio layer
    This patch is part of the fullmac bus interface refactoring series.
    It moves the module init/exit code to bus layer.
    
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Alwin Beukers <alwin@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/dhd_bus.h
+++ drivers/net/wireless/brcm80211/brcmfmac/dhd_bus.h
@@ -28,10 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Exported from brcmf bus module (brcmf_usb, brcmf_sdio)
  */
 
-/* Indicate (dis)interest in finding dongles. */
-extern int brcmf_bus_register(void);
-extern void brcmf_bus_unregister(void);
-
 /* obtain linux device object providing bus function */
 extern struct device *brcmf_bus_get_device(struct brcmf_bus *bus);
 
