ea69c65a9d1bbec35eadbb4afe6f3e16f254b5c2
Stefan Hajnoczi <stefanha@gmail.com>
2011-11-23 08:20:42 +0000

    staging: line6: add Pod HD300 support
    The Pod HD device family uses new MIDI SysEx messages and therefore
    cannot reuse the existing Pod code.  Instead of hardcoding Pod HD MIDI
    messages into the driver, leave MIDI up to userspace.  This driver
    simply presents MIDI and pcm ALSA devices.
    
    This device is similar to the Pod except that it has 48 kHz audio and
    does not respond to Pod SysEx messages.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/line6/driver.c
+++ drivers/staging/line6/driver.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "midi.h"
 #include "playback.h"
 #include "pod.h"
+#include "podhd.h"
 #include "revision.h"
 #include "toneport.h"
 #include "usbdefs.h"
@@ -50,6 +51,7 @@ static const struct usb_device_id line6_id_table[] = {
 	{USB_DEVICE(LINE6_VENDOR_ID, LINE6_DEVID_TONEPORT_UX1)},
 	{USB_DEVICE(LINE6_VENDOR_ID, LINE6_DEVID_TONEPORT_UX2)},
 	{USB_DEVICE(LINE6_VENDOR_ID, LINE6_DEVID_VARIAX)},
+	{USB_DEVICE(LINE6_VENDOR_ID, LINE6_DEVID_PODHD300)},
 	{},
 };
 
@@ -73,7 +75,8 @@ static struct line6_properties line6_properties_table[] = {
 	{ "TonePortGX",    "TonePort GX",      LINE6_BIT_TONEPORT_GX,   LINE6_BIT_PCM               },
 	{ "TonePortUX1",   "TonePort UX1",     LINE6_BIT_TONEPORT_UX1,  LINE6_BIT_PCM               },
 	{ "TonePortUX2",   "TonePort UX2",     LINE6_BIT_TONEPORT_UX2,  LINE6_BIT_PCM               },
-	{ "Variax",        "Variax Workbench", LINE6_BIT_VARIAX,        LINE6_BIT_CONTROL           }
+	{ "Variax",        "Variax Workbench", LINE6_BIT_VARIAX,        LINE6_BIT_CONTROL           },
+	{ "PODHD300",      "POD HD300",        LINE6_BIT_PODHD300,      LINE6_BIT_CONTROL_PCM_HWMON },
 };
 /* *INDENT-ON* */
 
@@ -437,6 +440,9 @@ static void line6_data_received(struct urb *urb)
 			line6_pod_process_message((struct usb_line6_pod *)
 						  line6);
 			break;
+
+		case LINE6_DEVID_PODHD300:
+			break; /* let userspace handle MIDI */
 
 		case LINE6_DEVID_PODXTLIVE:
 			switch (line6->interface_number) {
@@ -813,6 +819,7 @@ static int line6_probe(struct usb_interface *interface,
 	case LINE6_DEVID_BASSPODXTPRO:
 	case LINE6_DEVID_PODXT:
 	case LINE6_DEVID_PODXTPRO:
+	case LINE6_DEVID_PODHD300:
 		alternate = 5;
 		break;
 
@@ -862,6 +869,12 @@ static int line6_probe(struct usb_interface *interface,
 	case LINE6_DEVID_PODXT:
 	case LINE6_DEVID_PODXTPRO:
 		size = sizeof(struct usb_line6_pod);
+		ep_read = 0x84;
+		ep_write = 0x03;
+		break;
+
+	case LINE6_DEVID_PODHD300:
+		size = sizeof(struct usb_line6_podhd);
 		ep_read = 0x84;
 		ep_write = 0x03;
 		break;
@@ -1018,6 +1031,11 @@ static int line6_probe(struct usb_interface *interface,
 		ret = line6_pod_init(interface, (struct usb_line6_pod *)line6);
 		break;
 
+	case LINE6_DEVID_PODHD300:
+		ret = line6_podhd_init(interface,
+				       (struct usb_line6_podhd *)line6);
+		break;
+
 	case LINE6_DEVID_PODXTLIVE:
 		switch (interface_number) {
 		case PODXTLIVE_INTERFACE_POD:
@@ -1138,6 +1156,10 @@ static void line6_disconnect(struct usb_interface *interface)
 		case LINE6_DEVID_PODXT:
 		case LINE6_DEVID_PODXTPRO:
 			line6_pod_disconnect(interface);
+			break;
+
+		case LINE6_DEVID_PODHD300:
+			line6_podhd_disconnect(interface);
 			break;
 
 		case LINE6_DEVID_PODXTLIVE:
