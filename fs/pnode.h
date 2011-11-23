ec9f73c123e773bcbf63d92debba7abed973b36b
Al Viro <viro@zeniv.linux.org.uk>
2011-11-23 12:27:01 -0500

    vfs: add missing parens in pnode.h macros
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/pnode.h
+++ fs/pnode.h
@@ -12,11 +12,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include "mount.h"
 
-#define IS_MNT_SHARED(mnt) (mnt->mnt_flags & MNT_SHARED)
-#define IS_MNT_SLAVE(mnt) (mnt->mnt_master)
-#define IS_MNT_NEW(mnt)  (!mnt->mnt_ns)
-#define CLEAR_MNT_SHARED(mnt) (mnt->mnt_flags &= ~MNT_SHARED)
-#define IS_MNT_UNBINDABLE(mnt) (mnt->mnt_flags & MNT_UNBINDABLE)
+#define IS_MNT_SHARED(mnt) ((mnt)->mnt_flags & MNT_SHARED)
+#define IS_MNT_SLAVE(mnt) ((mnt)->mnt_master)
+#define IS_MNT_NEW(mnt)  (!(mnt)->mnt_ns)
+#define CLEAR_MNT_SHARED(mnt) ((mnt)->mnt_flags &= ~MNT_SHARED)
+#define IS_MNT_UNBINDABLE(mnt) ((mnt)->mnt_flags & MNT_UNBINDABLE)
 
 #define CL_EXPIRE    		0x01
 #define CL_SLAVE     		0x02
