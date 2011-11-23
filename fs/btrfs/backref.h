07aca5285527b035733cb0b0ddd9de787c299edb
Jan Schmidt <list.btrfs@jan-o-sch.net>
2011-11-23 18:55:04 +0100

    Btrfs: added btrfs_find_all_roots()
    This function gets a byte number (a data extent), collects all the leafs
    pointing to it and walks up the trees to find all fs roots pointing to those
    leafs. It also returns the list of all leafs pointing to that extent.
    
    It does proper locking for the involved trees, can be used on busy file
    systems and honors delayed refs.
    
    Signed-off-by: Arne Jansen <sensille@gmx.net>
    Signed-off-by: Jan Schmidt <list.btrfs@jan-o-sch.net>

--- fs/btrfs/backref.h
+++ fs/btrfs/backref.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __BTRFS_BACKREF__
 
 #include "ioctl.h"
+#include "ulist.h"
 
 struct inode_fs_paths {
 	struct btrfs_path		*btrfs_path;
@@ -54,6 +55,10 @@ int iterate_inodes_from_logical(u64 logical, struct btrfs_fs_info *fs_info,
 				iterate_extent_inodes_t *iterate, void *ctx);
 
 int paths_from_inode(u64 inum, struct inode_fs_paths *ipath);
+
+int btrfs_find_all_roots(struct btrfs_trans_handle *trans,
+				struct btrfs_fs_info *fs_info, u64 bytenr,
+				u64 num_bytes, u64 seq, struct ulist **roots);
 
 struct btrfs_data_container *init_data_container(u32 total_bytes);
 struct inode_fs_paths *init_ipath(s32 total_bytes, struct btrfs_root *fs_root,
