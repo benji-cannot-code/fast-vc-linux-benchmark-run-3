0918d0460967c1a95ce05caa65ebc0782a7d2891
Hans Verkuil <hverkuil@xs4all.nl>
2011-11-24 08:47:51 -0300

    [media] ir-sanyo-decoder.c doesn't compile
    I get this error when compiling for_v3.3:
    
    There is a include <linux/module.h> missing.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/rc/ir-sanyo-decoder.c
+++ drivers/media/rc/ir-sanyo-decoder.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Information for this protocol is available at the Sanyo LC7461 datasheet.
  */
 
+#include <linux/module.h>
 #include <linux/bitrev.h>
 #include "rc-core-priv.h"
 
