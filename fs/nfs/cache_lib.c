537b7ac05e33b0c58242f4cdd96ebad070bc257b
Stanislav Kinsbursky <skinsbursky@parallels.com>
2011-11-25 17:12:48 +0300

    NFS: split cache creation and PipeFS registration
    This precursor patch splits NFS cache creation and PipeFS registartion.
    It's required for latter split of NFS DNS resolver cache creation per network
    namespace context and PipeFS registration/unregistration on MOUNT/UMOUNT
    events.
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/cache_lib.c
+++ fs/nfs/cache_lib.c
@@ -121,7 +121,6 @@ int nfs_cache_register(struct cache_detail *cd)
 	mnt = rpc_get_mount();
 	if (IS_ERR(mnt))
 		return PTR_ERR(mnt);
-	sunrpc_init_cache_detail(cd);
 	ret = vfs_path_lookup(mnt->mnt_root, mnt, "/cache", 0, &path);
 	if (ret)
 		goto err;
@@ -130,7 +129,6 @@ int nfs_cache_register(struct cache_detail *cd)
 	if (!ret)
 		return ret;
 err:
-	sunrpc_destroy_cache_detail(cd);
 	rpc_put_mount();
 	return ret;
 }
@@ -142,3 +140,12 @@ void nfs_cache_unregister(struct cache_detail *cd)
 	rpc_put_mount();
 }
 
+void nfs_cache_init(struct cache_detail *cd)
+{
+	sunrpc_init_cache_detail(cd);
+}
+
+void nfs_cache_destroy(struct cache_detail *cd)
+{
+	sunrpc_destroy_cache_detail(cd);
+}
