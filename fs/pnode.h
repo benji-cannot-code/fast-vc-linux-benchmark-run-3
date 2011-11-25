bc43cfb3f7f4943995d8a4ef7250651bd3079a92
Al Viro <viro@zeniv.linux.org.uk>
2011-11-24 20:43:10 -0500

    vfs: spread struct mount - change_mnt_propagation/set_mnt_shared
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/pnode.h
+++ fs/pnode.h
@@ -24,13 +24,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CL_MAKE_SHARED 		0x08
 #define CL_PRIVATE 		0x10
 
-static inline void set_mnt_shared(struct vfsmount *mnt)
+static inline void set_mnt_shared(struct mount *mnt)
 {
-	mnt->mnt_flags &= ~MNT_SHARED_MASK;
-	mnt->mnt_flags |= MNT_SHARED;
+	mnt->mnt.mnt_flags &= ~MNT_SHARED_MASK;
+	mnt->mnt.mnt_flags |= MNT_SHARED;
 }
 
-void change_mnt_propagation(struct vfsmount *, int);
+void change_mnt_propagation(struct mount *, int);
 int propagate_mnt(struct vfsmount *, struct dentry *, struct vfsmount *,
 		struct list_head *);
 int propagate_umount(struct list_head *);
