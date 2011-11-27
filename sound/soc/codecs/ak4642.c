47b76747236e28982c02e2c34cdb4befd2583878
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-11-27 12:11:46 +0000

    ASoC: Remove driver versioning from ak4642
    It's never been updated so it can't be that useful and it makes the
    driver needlessly chatty.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/ak4642.c
+++ sound/soc/codecs/ak4642.c
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/initval.h>
 #include <sound/tlv.h>
 
-#define AK4642_VERSION "0.0.1"
-
 #define PW_MGMT1	0x00
 #define PW_MGMT2	0x01
 #define SG_SL1		0x02
@@ -473,8 +471,6 @@ static int ak4642_probe(struct snd_soc_codec *codec)
 {
 	struct ak4642_priv *ak4642 = snd_soc_codec_get_drvdata(codec);
 	int ret;
-
-	dev_info(codec->dev, "AK4642 Audio Codec %s", AK4642_VERSION);
 
 	ret = snd_soc_codec_set_cache_io(codec, 8, 8, ak4642->control_type);
 	if (ret < 0) {
