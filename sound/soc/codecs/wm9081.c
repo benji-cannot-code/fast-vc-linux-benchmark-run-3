228fd72ad7f5d5598dd500bfabb5cb41606ea19b
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-11-27 15:57:44 +0000

    ASoC: Convert wm9081 driver to use devm_kzalloc()
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm9081.c
+++ sound/soc/codecs/wm9081.c
@@ -1362,7 +1362,8 @@ static __devinit int wm9081_i2c_probe(struct i2c_client *i2c,
 	unsigned int reg;
 	int ret;
 
-	wm9081 = kzalloc(sizeof(struct wm9081_priv), GFP_KERNEL);
+	wm9081 = devm_kzalloc(&i2c->dev, sizeof(struct wm9081_priv),
+			      GFP_KERNEL);
 	if (wm9081 == NULL)
 		return -ENOMEM;
 
@@ -1406,7 +1407,6 @@ static __devinit int wm9081_i2c_probe(struct i2c_client *i2c,
 err_regmap:
 	regmap_exit(wm9081->regmap);
 err:
-	kfree(wm9081);
 
 	return ret;
 }
@@ -1417,7 +1417,6 @@ static __devexit int wm9081_i2c_remove(struct i2c_client *client)
 
 	snd_soc_unregister_codec(&client->dev);
 	regmap_exit(wm9081->regmap);
-	kfree(i2c_get_clientdata(client));
 	return 0;
 }
 
