bb0443d9b858993997fb7569b9a49fec20342db6
Ben Hutchings <ben@decadent.org.uk>
2011-11-27 17:06:08 +0000

    dsa: Move all definitions needed by drivers into <net/dsa.h>
    Any headers included by drivers should be under include/, and
    any definitions they use are not really private to the core as
    the name "dsa_priv.h" suggests.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Acked-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dsa/mv88e6060.c
+++ net/dsa/mv88e6060.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/netdevice.h>
 #include <linux/phy.h>
-#include "dsa_priv.h"
+#include <net/dsa.h>
 
 #define REG_PORT(p)		(8 + (p))
 #define REG_GLOBAL		0x0f
