72d120ba465fd6fc086d5593c893cb71051aa951
Ralf Baechle <ralf@linux-mips.org>
2011-11-28 16:11:28 +0000

    MIPS: Fix Jazz 64-bit build error.
    Move add_wired_entry to its own header file from where it will be
    always included.  Patch up other users of add_wired_entry to also include
    the header as needed.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/jazz/setup.c
+++ arch/mips/jazz/setup.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/jazzdma.h>
 #include <asm/reboot.h>
 #include <asm/pgtable.h>
+#include <asm/tlbmisc.h>
 
 extern asmlinkage void jazz_handle_int(void);
 
