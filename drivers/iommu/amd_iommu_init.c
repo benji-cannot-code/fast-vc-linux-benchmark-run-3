d6af2e9ee30e1732546964f342a4b75bb913d829
Joerg Roedel <joerg.roedel@amd.com>
2011-11-28 15:11:02 +0100

    iommu/amd: Add iommuv2 flag to struct amd_iommu
    In mixed IOMMU setups this flag inidicates whether an IOMMU
    supports the v2 features or not. This patch also adds a
    global flag together with a function to query that flag from
    other code. The flag shows if at least one IOMMUv2 is in the
    system.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/amd_iommu_init.c
+++ drivers/iommu/amd_iommu_init.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/msi.h>
 #include <linux/amd-iommu.h>
+#include <linux/export.h>
 #include <asm/pci-direct.h>
 #include <asm/iommu.h>
 #include <asm/gart.h>
@@ -143,6 +144,8 @@ bool amd_iommu_np_cache __read_mostly;
 bool amd_iommu_iotlb_sup __read_mostly = true;
 
 u32 amd_iommu_max_pasids __read_mostly = ~0;
+
+bool amd_iommu_v2_present __read_mostly;
 
 /*
  * The ACPI table parsing functions set this variable on an error
@@ -759,6 +762,12 @@ static void __init init_iommu_from_pci(struct amd_iommu *iommu)
 		pasids  = (1 << shift);
 
 		amd_iommu_max_pasids = min(amd_iommu_max_pasids, pasids);
+	}
+
+	if (iommu_feature(iommu, FEATURE_GT) &&
+	    iommu_feature(iommu, FEATURE_PPR)) {
+		iommu->is_iommu_v2   = true;
+		amd_iommu_v2_present = true;
 	}
 
 	if (!is_rd890_iommu(iommu->dev))
@@ -1646,3 +1655,9 @@ IOMMU_INIT_FINISH(amd_iommu_detect,
 		  gart_iommu_hole_init,
 		  0,
 		  0);
+
+bool amd_iommu_v2_supported(void)
+{
+	return amd_iommu_v2_present;
+}
+EXPORT_SYMBOL(amd_iommu_v2_supported);
