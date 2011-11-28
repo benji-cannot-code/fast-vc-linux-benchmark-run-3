7d1a0243d33b6b19a8fef3a959cf340eb3ad0ca2
Greg Ungerer <gerg@uclinux.org>
2011-11-28 16:32:49 +1000

    m68k: common PCI support definitions and code
    Basic set of definitions and support code required to turn on CONFIG_PCI
    for the m68k architecture. Nothing specific to any PCI implementation in
    any m68k class CPU hardware yet.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/asm/pci.h
+++ arch/m68k/include/asm/pci.h
@@ -3,11 +3,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_M68K_PCI_H
 
 #include <asm-generic/pci-dma-compat.h>
+#include <asm-generic/pci.h>
 
 /* The PCI address space does equal the physical memory
  * address space.  The networking and block device layers use
  * this boolean for bounce buffer decisions.
  */
 #define PCI_DMA_BUS_IS_PHYS	(1)
+
+#define	pcibios_assign_all_busses()	1
+
+#define	PCIBIOS_MIN_IO		0x00000100
+#define	PCIBIOS_MIN_MEM		0x02000000
 
 #endif /* _ASM_M68K_PCI_H */
