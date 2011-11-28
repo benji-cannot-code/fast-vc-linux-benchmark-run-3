f1a2c78f39eee1a7749698b234d5619215cf0502
Lars-Peter Clausen <lars@metafoo.de>
2011-11-28 09:44:17 +0100

    ASoC: Move SigmaDSP firmware loader to ASoC
    It has been pointed out previously, that the firmware subsystem is not the right
    place for the SigmaDSP firmware loader. Furthermore the SigmaDSP is currently
    only used in audio products and we are aiming for better integration into the
    ASoC framework in the future, with support for ALSA controls for firmware
    parameters and support dynamic power management as well. So the natural choice
    for the SigmaDSP firmware loader is the ASoC subsystem.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/adau1701.c
+++ sound/soc/codecs/adau1701.c
@@ -13,13 +13,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/i2c.h>
 #include <linux/delay.h>
-#include <linux/sigma.h>
 #include <linux/slab.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
 
+#include "sigmadsp.h"
 #include "adau1701.h"
 
 #define ADAU1701_DSPCTRL	0x1c
--- sound/soc/codecs/sigmadsp.c
+++ sound/soc/codecs/sigmadsp.c
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/i2c.h>
 #include <linux/module.h>
-#include <linux/sigma.h>
+
+#include "sigmadsp.h"
 
 static size_t sigma_action_size(struct sigma_action *sa)
 {
