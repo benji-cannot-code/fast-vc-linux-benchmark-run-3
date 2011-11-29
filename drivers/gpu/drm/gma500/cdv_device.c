b50679938a24bb38fd8291bf11f255efb2561afd
Alan Cox <alan@linux.intel.com>
2011-11-29 22:21:03 +0000

    gma500: do a pass over the FIXME tags
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/gma500/cdv_device.c
+++ drivers/gpu/drm/gma500/cdv_device.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VGA_SR_INDEX		0x3c4
 #define VGA_SR_DATA		0x3c5
 
-/* FIXME: should check if we are the active VGA device ?? */
 static void cdv_disable_vga(struct drm_device *dev)
 {
 	u8 sr1;
