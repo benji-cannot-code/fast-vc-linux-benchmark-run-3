1b7db507b843bc56ba4899dd1be8f5e34a0319ff
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2011-12-01 00:05:31 +0100

    PM / Domains: fix compilation failure for CONFIG_PM_GENERIC_DOMAINS unset
    Fix the following compalitaion breakage:
    
    In file included from linux/drivers/sh/pm_runtime.c:15:
    linux/include/linux/pm_domain.h: In function 'dev_to_genpd':
    linux/include/linux/pm_domain.h:142: error: implicit declaration of function 'ERR_PTR'
    linux/include/linux/pm_domain.h:142: warning: return makes pointer from integer without a cast
    In file included from linux/include/linux/sh_clk.h:10,
                     from linux/drivers/sh/pm_runtime.c:19:
    linux/include/linux/err.h: At top level:
    linux/include/linux/err.h:22: error: conflicting types for 'ERR_PTR'
    linux/include/linux/pm_domain.h:142: note: previous implicit declaration of 'ERR_PTR' was here
    make[3]: *** [drivers/sh/pm_runtime.o] Error 1
    
    Reported-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.yj@renesas.com>
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_PM_DOMAIN_H
 
 #include <linux/device.h>
+#include <linux/err.h>
 
 enum gpd_status {
 	GPD_STATE_ACTIVE = 0,	/* PM domain is active */
