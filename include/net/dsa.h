6804144623d94afa665661ebb5854ef2e0ab4f4e
Axel Lin <axel.lin@gmail.com>
2011-11-30 22:07:18 +0000

    dsa: Include linux/if_ether.h to fix build error
    Include linux/if_ether.h to fix below build errors:
    
      CC      arch/arm/mach-kirkwood/common.o
    In file included from arch/arm/mach-kirkwood/common.c:19:
    include/net/dsa.h: In function 'dsa_uses_dsa_tags':
    include/net/dsa.h:192: error: 'ETH_P_DSA' undeclared (first use in this function)
    include/net/dsa.h:192: error: (Each undeclared identifier is reported only once
    include/net/dsa.h:192: error: for each function it appears in.)
    include/net/dsa.h: In function 'dsa_uses_trailer_tags':
    include/net/dsa.h:197: error: 'ETH_P_TRAILER' undeclared (first use in this function)
    make[1]: *** [arch/arm/mach-kirkwood/common.o] Error 1
    make: *** [arch/arm/mach-kirkwood] Error 2
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_NET_DSA_H
 #define __LINUX_NET_DSA_H
 
+#include <linux/if_ether.h>
 #include <linux/list.h>
 #include <linux/timer.h>
 #include <linux/workqueue.h>
