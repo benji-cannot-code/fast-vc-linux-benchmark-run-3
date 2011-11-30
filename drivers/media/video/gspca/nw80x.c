f922fae106ddbe003bf12a7b366cddfbc5c1814f
Jean-François Moine <moinejf@free.fr>
2011-11-30 05:12:54 -0300

    [media] gspca: Remove the useless variable 'reverse_alts'
    The alternate settings are now sorted in gspca main, so the flag
    'reverse_alts' is no more useful.
    
    Signed-off-by: Jean-François Moine <moinejf@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/nw80x.c
+++ drivers/media/video/gspca/nw80x.c
@@ -1764,7 +1764,6 @@ static int sd_config(struct gspca_dev *gspca_dev,
 	if ((unsigned) webcam >= NWEBCAMS)
 		webcam = 0;
 	sd->webcam = webcam;
-	gspca_dev->cam.reverse_alts = 1;
 	gspca_dev->cam.ctrls = sd->ctrls;
 	sd->ag_cnt = -1;
 
