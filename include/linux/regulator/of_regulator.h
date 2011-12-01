e60897e2517247d2addd40a4ed5ace903520512c
Shawn Guo <shawn.guo@linaro.org>
2011-12-01 17:21:06 +0800

    regulator: pass device_node to of_get_regulator_init_data()
    It's not always true that the device_node of regulator can be found
    at dev->of_node at the time when of_get_regulator_init_data() is being
    called, because in some cases the regulator nodes in device tree do
    not have 'struct device' behind them until regulator_dev gets created
    for it by core function regulator_register().
    
    The patch adds device_node as a new parameter to
    of_get_regulator_init_data(), so that caller can pass in the node of
    regulator directly.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Cc: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Cc: Liam Girdwood <lrg@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/regulator/of_regulator.h
+++ include/linux/regulator/of_regulator.h
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if defined(CONFIG_OF)
 extern struct regulator_init_data
-	*of_get_regulator_init_data(struct device *dev);
+	*of_get_regulator_init_data(struct device *dev,
+				    struct device_node *node);
 #else
 static inline struct regulator_init_data
-	*of_get_regulator_init_data(struct device *dev)
+	*of_get_regulator_init_data(struct device *dev,
+				    struct device_node *node)
 {
 	return NULL;
 }
