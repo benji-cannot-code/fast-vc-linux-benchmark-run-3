68fe1fdef41a527cb72ffa70308e2b69bad45e7e
Jan Schmidt <list.btrfs@jan-o-sch.net>
2011-12-01 14:35:19 +0100

    Btrfs: make sure we're not using obsolete code in btrfs_get_extent
    There's code in btrfs_get_extent that should never be used. This patch turns
    a WARN_ON(1) into a BUG(), hoping we can remove the transaction code from
    btrfs_get_extent soon.
    
    Signed-off-by: Jan Schmidt <list.btrfs@jan-o-sch.net>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -5023,7 +5023,7 @@ again:
 			}
 			flush_dcache_page(page);
 		} else if (create && PageUptodate(page)) {
-			WARN_ON(1);
+			BUG();
 			if (!trans) {
 				kunmap(page);
 				free_extent_map(em);
