f87b848e4ef9427f607beaf53d317f8e0c9f2fa5
Chris Metcalf <cmetcalf@tilera.com>
2011-12-01 11:37:20 -0500

    arch/tile: add a few #includes and an EXPORT to catch up with kernel changes.
    The empty_zero_page[] export is required for ZERO_PAGE() module references.
    The #includes are due to changes in implicit inclusion, and should of
    course have been in the sources from the beginning.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- arch/tile/kernel/sysfs.c
+++ arch/tile/kernel/sysfs.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/slab.h>
 #include <linux/smp.h>
+#include <linux/stat.h>
 #include <hv/hypervisor.h>
 
 /* Return a string queried from the hypervisor, truncated to page size. */
