d9040018916fb789c15295e9d1c93c798aaf67f7
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-12-01 13:53:18 +0000

    mfd: Add missing mutex.h inclusion to WM8994 core.h
    struct wm8994 includes a mutex so we need to include mutex.h before we
    declare it. All current users rely on this being done implicitly.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Acked-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/wm8994/core.h
+++ include/linux/mfd/wm8994/core.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MFD_WM8994_CORE_H__
 #define __MFD_WM8994_CORE_H__
 
+#include <linux/mutex.h>
 #include <linux/interrupt.h>
 
 enum wm8994_type {
