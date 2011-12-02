c7b4d94e25f0aab71ad7bad62edf0d1d99cfee80
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-12-02 14:17:54 +0900

    ARM: S3C64XX: Add linux/export.h to dev-spi.c
    This is required for the EXPORT_SYMBOL()s the code uses, previously the
    header was being included implicitly by another header.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c64xx/dev-spi.c
+++ arch/arm/mach-s3c64xx/dev-spi.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/string.h>
+#include <linux/export.h>
 #include <linux/platform_device.h>
 #include <linux/dma-mapping.h>
 #include <linux/gpio.h>
