fe33ec27b414a941477ea7d8895c53a927bb2e9f
HeungJun Kim <riverful.kim@samsung.com>
2011-12-03 11:47:40 -0300

    [media] m5mols: Add support for the system initialization interrupt
    The M-5MOLS internal controller's initialization time depends on the
    hardware and firmware revision. Currently the driver just waits for
    worst case time period, after applying the voltage supplies, for
    the device to be ready. The M-5MOLS supports "System initialization"
    interrupt which is triggered after the controller finished booting.
    So use this interrupt to optimize the initialization sequence.
    
    After the voltage supplies are applied the I2C communication will
    fail, until the internal controller initializes to Flash Writer
    state. For the period when the I2C is not accessible use the
    isp_ready flag to suppress the error logs.
    
    Signed-off-by: HeungJun Kim <riverful.kim@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/m5mols/m5mols_reg.h
+++ drivers/media/video/m5mols/m5mols_reg.h
@@ -356,6 +356,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Starts internal ARM core booting after power-up */
 #define FLASH_CAM_START		I2C_REG(CAT_FLASH, 0x12, 1)
-#define REG_START_ARM_BOOT	0x01
+#define REG_START_ARM_BOOT	0x01	/* write value */
+#define REG_IN_FLASH_MODE	0x00	/* read value */
 
 #endif	/* M5MOLS_REG_H */
