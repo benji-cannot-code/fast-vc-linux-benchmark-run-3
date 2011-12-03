a4e66bd372d2d63c72de0c8c30d12e3455b62f1e
Stas Sergeev <stsp@list.ru>
2011-12-03 16:40:45 -0300

    [media] [saa7134] do not change mute state for capturing audio
    The attached patch fixes the automute logic of saa7134.
    It avoids the white noise on the pulseaudio startup.
    (pulseaudio reads the saa7134 alsa device on startup)
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/saa7134/saa7134-video.c
+++ drivers/media/video/saa7134/saa7134-video.c
@@ -1463,6 +1463,8 @@ static int video_release(struct file *file)
 	struct saa6588_command cmd;
 	unsigned long flags;
 
+	saa7134_tvaudio_close(dev);
+
 	/* turn off overlay */
 	if (res_check(fh, RESOURCE_OVERLAY)) {
 		spin_lock_irqsave(&dev->slock,flags);
