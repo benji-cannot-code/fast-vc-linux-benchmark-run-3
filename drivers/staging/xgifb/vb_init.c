bc93427a595f6e7f01028aa6512e4c6ac73f1197
Aaro Koskinen <aaro.koskinen@iki.fi>
2011-12-06 00:10:42 +0200

    staging: xgifb: ReadVBIOSTablData(): rename to xgifb_read_vbios()
    Rename the function to xgifb_read_vbios().
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/xgifb/vb_init.c
+++ drivers/staging/xgifb/vb_init.c
@@ -1115,7 +1115,7 @@ done:
 	return rom_copy;
 }
 
-static void ReadVBIOSTablData(struct pci_dev *pdev,
+static void xgifb_read_vbios(struct pci_dev *pdev,
 			      struct vb_device_info *pVBInfo)
 {
 	struct xgifb_video_info *xgifb_info = pci_get_drvdata(pdev);
@@ -1524,8 +1524,7 @@ unsigned char XGIInitNew(struct pci_dev *pdev)
 
 	InitTo330Pointer(HwDeviceExtension->jChipType, pVBInfo);
 
-	/* ReadVBIOSData */
-	ReadVBIOSTablData(pdev, pVBInfo);
+	xgifb_read_vbios(pdev, pVBInfo);
 
 	/* 1.Openkey */
 	xgifb_reg_set(pVBInfo->P3c4, 0x05, 0x86);
