254c1e48198724df45e627e11865ca58d8b9c7ac
Al Viro <viro@zeniv.linux.org.uk>
2011-12-06 13:32:36 -0500

    switch mnt_namespace ->root to struct mount
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/mount.h
+++ fs/mount.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct mnt_namespace {
 	atomic_t		count;
-	struct vfsmount *	root;
+	struct mount *	root;
 	struct list_head	list;
 	wait_queue_head_t poll;
 	int event;
