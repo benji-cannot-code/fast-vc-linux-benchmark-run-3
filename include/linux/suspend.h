233ce58be74cf5f6cb9a866bea5d4591347ac732
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2011-12-06 23:18:12 +0100

    PM / Sleep: Use the freezer_count() functions in [un]lock_system_sleep() APIs
    Now that freezer_count() and freezer_do_not_count() don't have the restriction
    that they are effective only when called by userspace processes, use
    them in lock_system_sleep() and unlock_system_sleep() instead of open-coding
    their parts.
    
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- include/linux/suspend.h
+++ include/linux/suspend.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/pm.h>
 #include <linux/mm.h>
+#include <linux/freezer.h>
 #include <asm/errno.h>
 
 #ifdef CONFIG_VT
@@ -381,16 +382,14 @@ static inline void unlock_system_sleep(void) {}
 
 static inline void lock_system_sleep(void)
 {
-	/* simplified freezer_do_not_count() */
-	current->flags |= PF_FREEZER_SKIP;
+	freezer_do_not_count();
 	mutex_lock(&pm_mutex);
 }
 
 static inline void unlock_system_sleep(void)
 {
 	mutex_unlock(&pm_mutex);
-	/* simplified freezer_count() */
-	current->flags &= ~PF_FREEZER_SKIP;
+	freezer_count();
 }
 #endif
 
