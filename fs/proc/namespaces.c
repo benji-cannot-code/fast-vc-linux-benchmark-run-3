e2952c8c89934a956a80717439b5878b7401da58
Al Viro <viro@zeniv.linux.org.uk>
2011-12-07 13:06:11 -0500

    vfs: trim includes a bit
    [folded fix for missing magic.h from Tetsuo Handa]
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/proc/namespaces.c
+++ fs/proc/namespaces.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/file.h>
 #include <linux/utsname.h>
 #include <net/net_namespace.h>
-#include <linux/mnt_namespace.h>
 #include <linux/ipc_namespace.h>
 #include <linux/pid_namespace.h>
 #include "internal.h"
--- security/tomoyo/realpath.c
+++ security/tomoyo/realpath.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "common.h"
+#include <linux/magic.h>
 
 /**
  * tomoyo_encode2 - Encode binary string to ascii string.
