544ba765a8cb58efcd6fd3a71a7de66b9528b2d7
Manuel Lauss <manuel.lauss@googlemail.com>
2011-12-08 10:42:10 +0000

    MTD: nand: make au1550nd.c a platform_driver
    Transform the au1550nd.c driver into a platform_driver and hook it
    up in the PB1550 board (gen_nand works fine on the DB1550, but since
    I don't have a PB1550 to test this driver stays for now).
    
    Signed-off-by: Manuel Lauss <manuel.lauss@googlemail.com>
    Cc: linux-mtd@lists.infradead.org
    To: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/2875/
    Patchwork: https://patchwork.linux-mips.org/patch/3160/
    Acked-by: Artem Bityutskiy <dedekind1@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

---
+++ arch/mips/include/asm/mach-au1x00/au1550nd.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * platform data for the Au1550 NAND driver
+ */
+
+#ifndef _AU1550ND_H_
+#define _AU1550ND_H_
+
+#include <linux/mtd/partitions.h>
+
+struct au1550nd_platdata {
+	struct mtd_partition *parts;
+	int num_parts;
+	int devwidth;	/* 0 = 8bit device, 1 = 16bit device */
+};
+
+#endif
