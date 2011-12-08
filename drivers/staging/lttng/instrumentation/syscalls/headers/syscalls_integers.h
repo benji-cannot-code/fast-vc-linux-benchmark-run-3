8b0aa4d8d9de2f42e5f33091a3c7da23a412236e
Greg Kroah-Hartman <gregkh@suse.de>
2011-12-08 15:25:56 -0800

    Staging: lttng: remove from the drivers/staging/ tree
    The "proper" way to do this is to work with the existing in-kernel
    tracing subsystem and work to get the missing features that are in lttng
    into those subsystems.
    
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/lttng/instrumentation/syscalls/headers/syscalls_integers.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifdef CONFIG_X86_64
-#include "x86-64-syscalls-3.0.4_integers.h"
-#endif
-
-#ifdef CONFIG_X86_32
-#include "x86-32-syscalls-3.1.0-rc6_integers.h"
-#endif
--- drivers/staging/lttng/instrumentation/syscalls/headers/syscalls_pointers_override.h
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/*
- * This is a place-holder for override defines for system calls with
- * pointers (all architectures).
- */
