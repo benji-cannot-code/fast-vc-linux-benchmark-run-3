2040c88fd914ac9d2a404946883f02e3e61bb6a7
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-12-08 10:52:19 +0900

    ARM: S3C64XX: Hook up VDDINT on Cragganmore
    Basic hookup, we do have a GPIO to use to control the voltage but
    we don't currently use it.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c64xx/mach-crag6410.c
+++ arch/arm/mach-s3c64xx/mach-crag6410.c
@@ -381,6 +381,10 @@ static struct regulator_init_data vddarm __initdata = {
 	.driver_data = &vddarm_pdata,
 };
 
+static struct regulator_consumer_supply vddint_consumers[] __initdata = {
+	REGULATOR_SUPPLY("vddint", NULL),
+};
+
 static struct regulator_init_data vddint __initdata = {
 	.constraints = {
 		.name = "VDDINT",
@@ -389,6 +393,9 @@ static struct regulator_init_data vddint __initdata = {
 		.always_on = 1,
 		.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
 	},
+	.num_consumer_supplies = ARRAY_SIZE(vddint_consumers),
+	.consumer_supplies = vddint_consumers,
+	.supply_regulator = "WALLVDD",
 };
 
 static struct regulator_init_data vddmem __initdata = {
