58c23f29a4b39912d56279ba66816836932bc9aa
Michal Simek <monstr@monstr.eu>
2011-12-09 10:45:20 +0100

    microblaze: Change NO_IRQ to 0
    As has been discussed many times[1], Using NO_IRQ set to anything other
    than 0 is bug waiting to happen since many drivers follow the pattern
    "if (!irq)" for testing whether or not an irq has been set.
    
    This patch changes the Microblaze NO_IRQ setting from -1 to 0 to bring
    it in line with most of the rest of the kernel.  It also prepares for
    Microblaze eventually supporting multiple interrupt controllers by
    breaking the assumption that hwirq# == Linux IRQ#.  The Linux IRQ
    number is just a cookie with no guarantee of a direct relationship
    with the hardware irq arrangement.
    
    At this point, Microblaze interrupt handling only supports only one
    instance of one kind of interrupt controller (xilinx_intc).  This change
    shouldn't affect any architecture code outside of the interrupt
    controller driver and the irq_of mapping.
    
    Updated to 3.2 and to use irq_data.hwirq by Rob Herring.
    Tested and fixed by Michal Simek.
    
    [1] http://lkml.org/lkml/2005/11/21/221
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/irq.h
+++ arch/microblaze/include/asm/irq.h
@@ -10,7 +10,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_MICROBLAZE_IRQ_H
 #define _ASM_MICROBLAZE_IRQ_H
 
-#define NR_IRQS 32
+
+/*
+ * Linux IRQ# is currently offset by one to map to the hardware
+ * irq number. So hardware IRQ0 maps to Linux irq 1.
+ */
+#define NO_IRQ_OFFSET	1
+#define IRQ_OFFSET	NO_IRQ_OFFSET
+#define NR_IRQS		(32 + IRQ_OFFSET)
 #include <asm-generic/irq.h>
 
 /* This type is the placeholder for a hardware interrupt number. It has to
@@ -21,7 +28,7 @@ typedef unsigned long irq_hw_number_t;
 
 extern unsigned int nr_irq;
 
-#define NO_IRQ (-1)
+#define NO_IRQ 0
 
 struct pt_regs;
 extern void do_IRQ(struct pt_regs *regs);
