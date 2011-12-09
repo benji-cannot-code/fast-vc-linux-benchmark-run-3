85f45e34d7e6434d9ef3306c15a5de9eb2238102
Al Viro <viro@zeniv.linux.org.uk>
2011-12-08 23:01:06 -0500

    vfs: live vfsmounts never have NULL ->mnt_sb
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/usb/core/inode.c
+++ drivers/usb/core/inode.c
@@ -265,7 +265,7 @@ static int remount(struct super_block *sb, int *flags, char *data)
 		return -EINVAL;
 	}
 
-	if (usbfs_mount && usbfs_mount->mnt_sb)
+	if (usbfs_mount)
 		update_sb(usbfs_mount->mnt_sb);
 
 	return 0;
@@ -501,9 +501,8 @@ static int fs_create_by_name (const char *name, mode_t mode,
 	 * have around.
 	 */
 	if (!parent ) {
-		if (usbfs_mount && usbfs_mount->mnt_sb) {
+		if (usbfs_mount)
 			parent = usbfs_mount->mnt_root;
-		}
 	}
 
 	if (!parent) {
