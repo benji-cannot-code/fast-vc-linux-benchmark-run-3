6d7322c646d37e22a4039f805fe69350b56020f3
H. Peter Anvin <hpa@zytor.com>
2011-12-09 11:13:59 -0800

    x86, um: Fix typo in 32-bit system call modifications
    We override sys_iopl(), not stub_iopl(); the latter is a 64-bitism
    that doesn't apply to i386 in the first place.
    
    Reported-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/um/sys_call_table_32.c
+++ arch/x86/um/sys_call_table_32.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 /* Not going to be implemented by UML, since we have no hardware. */
-#define stub_iopl sys_ni_syscall
+#define sys_iopl sys_ni_syscall
 #define sys_ioperm sys_ni_syscall
 
 #define sys_vm86old sys_ni_syscall
