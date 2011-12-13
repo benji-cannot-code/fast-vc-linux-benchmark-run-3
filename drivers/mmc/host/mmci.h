616371cb8fabe2318ccd385e2f61ab5376253d11
Ulf Hansson <ulf.hansson@stericsson.com>
2011-12-13 16:52:00 +0100

    ARM: 7215/1: mmc: mmci: Increase max_segs from 16 to 128
    A significant increase (10-20%) in performance throughput for
    USB mass storage is the reason for incrementing the value.
    By some reason the USB driver allocates buffers which requires
    a scattergather list to contain a lot more than 16 elements to
    get optimal performance. This change sets the maximum elements
    to 128.
    
    Tests with large reads and large writes (100 MiB) show that the
    throughput increase is significant for write (10% for this test)
    but not for read. Tests are run on a Linux host with ext4 FS on
    the gadget mass storage device. The sg-len still exceeds 16 for
    the read tests but the performance gain is low or nothing.
    
    Tested-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Per Forlin <per.forlin@stericsson.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/mmc/host/mmci.h
+++ drivers/mmc/host/mmci.h
@@ -161,7 +161,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(MCI_RXFIFOHALFFULLMASK | MCI_RXDATAAVLBLMASK | \
 	 MCI_TXFIFOHALFEMPTYMASK)
 
-#define NR_SG		16
+#define NR_SG		128
 
 struct clk;
 struct variant_data;
