1c70587386cd0ea624244ea0c13925e9421f4e9b
Arik Nemtsov <arik@wizery.com>
2011-12-13 12:15:09 +0200

    wlcore/wl12xx: add hw op for vif init
    Add an op for family-specific vif initialization.  Currently unused,
    but will be needed when wl18xx support is implemented.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wlcore/init.c
+++ drivers/net/wireless/ti/wlcore/init.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cmd.h"
 #include "tx.h"
 #include "io.h"
+#include "hw_ops.h"
 
 int wl1271_init_templates_config(struct wl1271 *wl)
 {
@@ -636,6 +637,10 @@ int wl1271_init_vif_specific(struct wl1271 *wl, struct ieee80211_vif *vif)
 
 	/* Configure initiator BA sessions policies */
 	ret = wl1271_set_ba_policies(wl, wlvif);
+	if (ret < 0)
+		return ret;
+
+	ret = wlcore_hw_init_vif(wl, wlvif);
 	if (ret < 0)
 		return ret;
 
