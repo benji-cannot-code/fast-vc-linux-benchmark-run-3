68ba765dd178e8520edc8004e6e3ff0b86f6fc88
Greg Kroah-Hartman <gregkh@suse.de>
2011-12-13 11:05:24 -0800

    s390: tape_class.h: remove kobj_map.h inclusion
    This include file is not needed for any driver to be referencing, so
    remove it from the tape_class.h file.
    
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/s390/char/tape_class.h
+++ drivers/s390/char/tape_class.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/major.h>
-#include <linux/kobj_map.h>
 #include <linux/cdev.h>
 
 #include <linux/device.h>
