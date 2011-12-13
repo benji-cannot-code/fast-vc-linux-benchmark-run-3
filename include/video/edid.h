486480110da2279bdbbed48423e376317e84f3ef
David Howells <dhowells@redhat.com>
2011-12-13 09:26:45 +0000

    UAPI: Fix nested __KERNEL__ guards in video/edid.h
    Fix nested __KERNEL__ guards in video/edid.h to make parsing easier.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- include/video/edid.h
+++ include/video/edid.h
@@ -2,16 +2,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __linux_video_edid_h__
 #define __linux_video_edid_h__
 
-#if !defined(__KERNEL__) || defined(CONFIG_X86)
-
 struct edid_info {
 	unsigned char dummy[128];
 };
 
 #ifdef __KERNEL__
+#ifdef CONFIG_X86
 extern struct edid_info edid_info;
-#endif /* __KERNEL__ */
-
+#endif
 #endif
 
 #endif /* __linux_video_edid_h__ */
