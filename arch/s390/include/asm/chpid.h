658548fe0bd64a9eb20d35232f5e1d26fa9f8102
David Howells <dhowells@redhat.com>
2011-12-13 09:26:45 +0000

    UAPI: Alter the S390 asm include guards to be recognisable by the UAPI splitter
    Alter some of the S390 asm include guards to fit a pattern that the UAPI
    splitter recognises.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/s390/include/asm/chpid.h
+++ arch/s390/include/asm/chpid.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #ifndef _ASM_S390_CHPID_H
-#define _ASM_S390_CHPID_H _ASM_S390_CHPID_H
+#define _ASM_S390_CHPID_H
 
 #include <linux/string.h>
 #include <linux/types.h>
