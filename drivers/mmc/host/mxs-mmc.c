49336f22191ce2fd1f0e8ba95b0c2436492455aa
Shawn Guo <shawn.guo@linaro.org>
2011-12-13 23:48:04 +0800

    mmc: mxs-mmc: fix the dma_transfer_direction migration
    The commit 05f5799 (mmc-host: move to dma_transfer_direction) left out
    the DMA_NONE, in turn breaks the driver as below.
    
    [    0.650000] mxs-mmc mxs-mmc.0: initialized
    [    0.650000] mxs-mmc mxs-mmc.1: initialized
    [    0.690000] mxs-dma mxs-dma-apbh: maximum bytes for sg entry exceeded: -55906
    7475 > 65280
    [    0.690000] mxs-mmc mxs-mmc.0: mxs_mmc_ac: failed to prep dma
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- drivers/mmc/host/mxs-mmc.c
+++ drivers/mmc/host/mxs-mmc.c
@@ -358,6 +358,7 @@ static void mxs_mmc_bc(struct mxs_mmc_host *host)
 	host->ssp_pio_words[1] = cmd0;
 	host->ssp_pio_words[2] = cmd1;
 	host->dma_dir = DMA_NONE;
+	host->slave_dirn = DMA_TRANS_NONE;
 	desc = mxs_mmc_prep_dma(host, 0);
 	if (!desc)
 		goto out;
@@ -397,6 +398,7 @@ static void mxs_mmc_ac(struct mxs_mmc_host *host)
 	host->ssp_pio_words[1] = cmd0;
 	host->ssp_pio_words[2] = cmd1;
 	host->dma_dir = DMA_NONE;
+	host->slave_dirn = DMA_TRANS_NONE;
 	desc = mxs_mmc_prep_dma(host, 0);
 	if (!desc)
 		goto out;
@@ -515,6 +517,7 @@ static void mxs_mmc_adtc(struct mxs_mmc_host *host)
 	host->ssp_pio_words[1] = cmd0;
 	host->ssp_pio_words[2] = cmd1;
 	host->dma_dir = DMA_NONE;
+	host->slave_dirn = DMA_TRANS_NONE;
 	desc = mxs_mmc_prep_dma(host, 0);
 	if (!desc)
 		goto out;
