a31155d77ca372c0144f481b28de5812128ff632
Afzal Mohammed <afzal@ti.com>
2011-12-13 10:46:43 -0800

    ARM: OMAP: am33xx: Update common OMAP machine specific sources
    This patch updates the common machine specific source files for
    support for AM33XX/AM335x with cpu type, macros for identification of
    AM33XX/AM335X device.
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Tested-by: Kevin Hilman <khilman@ti.com>
    [tony@atomide.com: updated for map_io and common.h changes, dropped CK_AM33XX]
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/hardware.h
+++ arch/arm/plat-omap/include/plat/hardware.h
@@ -288,5 +288,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/omap34xx.h>
 #include <plat/omap44xx.h>
 #include <plat/ti816x.h>
+#include <plat/am33xx.h>
 
 #endif	/* __ASM_ARCH_OMAP_HARDWARE_H */
