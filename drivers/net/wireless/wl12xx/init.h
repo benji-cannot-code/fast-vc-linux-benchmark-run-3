66d0b4052e0a799c1ab72198ecc30647eae986d6
Luciano Coelho <coelho@ti.com>
2011-12-13 15:45:54 +0200

    wl12xx: remove deprecated packet detection threshold config
    The ACX_PD_THRESHOLD configuration command is deprecated and should
    not be used anymore.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/init.h
+++ drivers/net/wireless/wl12xx/init.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int wl1271_hw_init_power_auth(struct wl1271 *wl);
 int wl1271_init_templates_config(struct wl1271 *wl);
-int wl1271_init_phy_config(struct wl1271 *wl);
 int wl1271_init_pta(struct wl1271 *wl);
 int wl1271_init_energy_detection(struct wl1271 *wl);
 int wl1271_chip_specific_init(struct wl1271 *wl);
