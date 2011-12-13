1b407e3a18345303c1bfd96e769629b6e57ddf67
David Howells <dhowells@redhat.com>
2011-12-13 15:07:49 +0000

    UAPI: Don't have a #elif clause in a __KERNEL__ guard in linux/soundcard.h
    Don't have a #elif clause in a __KERNEL__ guard in linux/soundcard.h to make
    parsing easier.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- include/linux/soundcard.h
+++ include/linux/soundcard.h
@@ -199,7 +199,8 @@ typedef struct seq_event_rec {
 #  else
 #    error "could not determine byte order"
 #  endif
-#elif defined(__BYTE_ORDER)
+#else
+# if defined(__BYTE_ORDER)
 #  if __BYTE_ORDER == __BIG_ENDIAN
 #    define AFMT_S16_NE AFMT_S16_BE
 #  elif __BYTE_ORDER == __LITTLE_ENDIAN
@@ -207,6 +208,7 @@ typedef struct seq_event_rec {
 #  else
 #    error "could not determine byte order"
 #  endif
+# endif
 #endif
 
 /*
