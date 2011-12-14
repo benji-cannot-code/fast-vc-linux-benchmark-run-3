328bc28519794243a91156bc3517a90ac9486db0
Luciano Coelho <coelho@ti.com>
2011-12-14 14:57:58 +0200

    wl12xx: remove unused firmware version macros
    We don't use WL12XX_BA_SUPPORT_FW_COST_VER2_START nor
    WL12XX_BA_SUPPORT_FW_COST_VER2_END anymore.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/wl12xx.h
+++ drivers/net/wireless/wl12xx/wl12xx.h
@@ -36,13 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "conf.h"
 #include "ini.h"
 
-/*
- * FW versions support BA 11n
- * versions marks x.x.x.50-60.x
- */
-#define WL12XX_BA_SUPPORT_FW_COST_VER2_START    50
-#define WL12XX_BA_SUPPORT_FW_COST_VER2_END      60
-
 #define WL127X_FW_NAME "ti-connectivity/wl127x-fw-3.bin"
 #define WL128X_FW_NAME "ti-connectivity/wl128x-fw-3.bin"
 
