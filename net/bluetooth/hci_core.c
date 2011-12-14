52e29894fac54283602d7cb36fe13d34bf05c349
Gustavo F. Padovan <padovan@profusion.mobi>
2011-12-14 15:08:48 -0200

    Bluetooth: Use RCU to manipulate chan_list
    Instead of using tasklet_disable() to prevent acess to the channel use, we
    can use RCU and improve the performance of our code.
    
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -2136,7 +2136,9 @@ static inline struct hci_chan *hci_chan_sent(struct hci_dev *hdev, __u8 type,
 
 		conn_num++;
 
-		list_for_each_entry(tmp, &conn->chan_list, list) {
+		rcu_read_lock();
+
+		list_for_each_entry_rcu(tmp, &conn->chan_list, list) {
 			struct sk_buff *skb;
 
 			if (skb_queue_empty(&tmp->data_q))
@@ -2159,6 +2161,8 @@ static inline struct hci_chan *hci_chan_sent(struct hci_dev *hdev, __u8 type,
 				chan = tmp;
 			}
 		}
+
+		rcu_read_unlock();
 
 		if (hci_conn_num(hdev, type) == conn_num)
 			break;
@@ -2208,7 +2212,9 @@ static void hci_prio_recalculate(struct hci_dev *hdev, __u8 type)
 
 		num++;
 
-		list_for_each_entry(chan, &conn->chan_list, list) {
+		rcu_read_lock();
+
+		list_for_each_entry_rcu(chan, &conn->chan_list, list) {
 			struct sk_buff *skb;
 
 			if (chan->sent) {
@@ -2228,6 +2234,8 @@ static void hci_prio_recalculate(struct hci_dev *hdev, __u8 type)
 			BT_DBG("chan %p skb %p promoted to %d", chan, skb,
 								skb->priority);
 		}
+
+		rcu_read_unlock();
 
 		if (hci_conn_num(hdev, type) == num)
 			break;
