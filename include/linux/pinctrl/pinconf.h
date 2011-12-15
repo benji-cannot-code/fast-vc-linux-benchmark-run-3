99c84423b97d9c07b706e67bc6167ec7717bc138
Stephen Warren <swarren@nvidia.com>
2011-12-15 16:57:16 -0700

    pinctrl: add "struct seq_file;" to pinconf.h
    This allows one to include pinconf.h without having to include other
    headers first.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/pinctrl/pinconf.h
+++ include/linux/pinctrl/pinconf.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_PINCONF
 
 struct pinctrl_dev;
+struct seq_file;
 
 /**
  * struct pinconf_ops - pin config operations, to be implemented by
