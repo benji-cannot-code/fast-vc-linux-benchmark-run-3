7635bbc8fce323997f7af69497359ac2d2c04059
Andrew Lunn <andrew@lunn.ch>
2011-12-15 08:15:07 +0100

    ARM: Orion: Add clocks using the generic clk infrastructure.
    Add tclk as a fixed rate clock for all platforms. In addition, on
    kirkwood, add a gated clock for most of the clocks which can be gated.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Tested-by: Jamie Lentin <jm@lentin.co.uk>
    [mturquette@linaro.org: removed redundant CLKDEV_LOOKUP from Kconfig]
    [mturquette@linaro.org: removed redundant clk.h from mach-dove/common.c]
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- arch/arm/plat-orion/common.c
+++ arch/arm/plat-orion/common.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 #include <linux/serial_8250.h>
 #include <linux/ata_platform.h>
+#include <linux/clk.h>
+#include <linux/clkdev.h>
 #include <linux/mv643xx_eth.h>
 #include <linux/mv643xx_i2c.h>
 #include <net/dsa.h>
