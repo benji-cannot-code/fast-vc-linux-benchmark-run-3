20577de11d095236567e2340769900997cc84dee
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-12-16 23:13:28 -0500

    ARM: mach-exynos: use standard arch_idle()
    This is equivalent and more similar to existing architectures.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>

--- arch/arm/mach-exynos/include/mach/system.h
+++ arch/arm/mach-exynos/include/mach/system.h
@@ -16,6 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void arch_idle(void)
 {
-	/* nothing here yet */
+	cpu_do_idle();
 }
 #endif /* __ASM_ARCH_SYSTEM_H */
