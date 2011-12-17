638e11fcd2d2d6d65d46f633334d23dd6975782c
Nicolas Pitre <nicolas.pitre@linaro.org>
2011-12-16 23:13:28 -0500

    ARM: mach-s5pv210: use standard arch_idle()
    This is equivalent and more similar to existing architectures.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>

--- arch/arm/mach-s5pv210/include/mach/system.h
+++ arch/arm/mach-s5pv210/include/mach/system.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void arch_idle(void)
 {
-	/* nothing here yet */
+	cpu_do_idle();
 }
 
 #endif /* __ASM_ARCH_SYSTEM_H */
