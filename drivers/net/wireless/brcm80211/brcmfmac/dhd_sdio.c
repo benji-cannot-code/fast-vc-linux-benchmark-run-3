8ddc64071e3f10bfaf0a711153b093123ec717cc
Franky Lin <frankyl@broadcom.com>
2011-12-16 18:36:57 -0800

    brcm80211: fmac: change function txflowcontrol parameter
    Change parameter to device pointer for bus layer interface function
    brcmf_txflowcontrol. This is part of the fullmac bus interface
    refactoring.
    
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Reviewed-by: Alwin Beukers <alwin@broadcom.com>
    Signed-off-by: Franky Lin <frankyl@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/dhd_sdio.c
+++ drivers/net/wireless/brcm80211/brcmfmac/dhd_sdio.c
@@ -2290,7 +2290,7 @@ static uint brcmf_sdbrcm_sendfromq(struct brcmf_sdio *bus, uint maxframes)
 	/* Deflow-control stack if needed */
 	if (drvr->up && (drvr->bus_if->state == BRCMF_BUS_DATA) &&
 	    drvr->txoff && (pktq_len(&bus->txq) < TXLOW))
-		brcmf_txflowcontrol(drvr, 0, OFF);
+		brcmf_txflowcontrol(bus->sdiodev->dev, 0, OFF);
 
 	return cnt;
 }
@@ -2603,7 +2603,7 @@ int brcmf_sdbrcm_bus_txdata(struct device *dev, struct sk_buff *pkt)
 	spin_unlock_bh(&bus->txqlock);
 
 	if (pktq_len(&bus->txq) >= TXHI)
-		brcmf_txflowcontrol(bus->drvr, 0, ON);
+		brcmf_txflowcontrol(bus->sdiodev->dev, 0, ON);
 
 #ifdef BCMDBG
 	if (pktq_plen(&bus->txq, prec) > qcount[prec])
