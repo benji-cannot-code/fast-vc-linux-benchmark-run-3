bb307149e5fced7558ab0072a17faea9dc4d04af
Eric W. Biederman <ebiederm@xmission.com>
2011-12-18 20:09:31 -0800

    sysfs: Kill nlink counting.
    Tracking the number of subdirectories requires an extra field that increases
    the size of sysfs_dirent.  nlinks are not particularly interesting for sysfs
    and the nlink counts are wrong when network namespaces are involved so stop
    counting them, and always return nlink == 1.  Userspace already knows that
    directories with nlink == 1 have an nlink count they can't use to count
    subdirectories.
    
    This reduces the size of sysfs_dirent by 8 bytes on 64bit platforms.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/sysfs/sysfs.h
+++ fs/sysfs/sysfs.h
@@ -20,7 +20,6 @@ struct sysfs_open_dirent;
 struct sysfs_elem_dir {
 	struct kobject		*kobj;
 
-	unsigned long		subdirs;
 	/* children rbtree starts here and goes through sd->s_rb */
 	struct rb_root		children;
 };
