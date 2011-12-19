1822f11e3a4a91d1ede44c5d330e5edc60d65873
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-12-19 15:08:15 -0500

    xen/xenbus-frontend: Fix compile error with randconfig
    drivers/xen/xenbus/xenbus_dev_frontend.c: In function 'xenbus_init':
    drivers/xen/xenbus/xenbus_dev_frontend.c:609:2: error: implicit declaration of function 'xen_domain'
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/xenbus/xenbus_dev_frontend.c
+++ drivers/xen/xenbus/xenbus_dev_frontend.c
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xenbus_comms.h"
 
 #include <xen/xenbus.h>
+#include <xen/xen.h>
 #include <asm/xen/hypervisor.h>
 
 MODULE_LICENSE("GPL");
