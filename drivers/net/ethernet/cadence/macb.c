c1a80324a16db8ee965dc218a853406101b0582b
Olof Johansson <olof@lixom.net>
2011-12-20 13:13:07 -0800

    net: macb: fix build break with !CONFIG_OF
    This fixes the build of the macb driver that was broken by the DT changes:
    
    drivers/net/ethernet/cadence/macb.c:1503: error: implicit declaration of function 'of_match_ptr'
    drivers/net/ethernet/cadence/macb.c:1503: error: 'macb_dt_ids' undeclared here (not in a function)
    drivers/net/ethernet/cadence/macb.c:1503: error: initializer element is not constant
    drivers/net/ethernet/cadence/macb.c:1503: error: (near initialization for 'macb_driver.driver.of_match_table')
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- drivers/net/ethernet/cadence/macb.c
+++ drivers/net/ethernet/cadence/macb.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/macb.h>
 #include <linux/platform_device.h>
 #include <linux/phy.h>
+#include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_net.h>
 
