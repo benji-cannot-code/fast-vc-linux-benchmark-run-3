b463a532ec9f5b98afe8a6e951e962e9c089491e
Scott Wood <scottwood@freescale.com>
2011-12-20 15:34:31 +0000

    KVM: PPC: e500: merge <asm/kvm_e500.h> into arch/powerpc/kvm/e500.h
    Keeping two separate headers for e500-specific things was a
    pain, and wasn't even organized along any logical boundary.
    
    There was TLB stuff in <asm/kvm_e500.h> despite the existence of
    arch/powerpc/kvm/e500_tlb.h, and nothing in <asm/kvm_e500.h> needed
    to be referenced from outside arch/powerpc/kvm.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    [agraf: fix bisectability]
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/powerpc/kvm/e500_emulate.c
+++ arch/powerpc/kvm/e500_emulate.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/kvm_ppc.h>
 #include <asm/disassemble.h>
-#include <asm/kvm_e500.h>
 
 #include "booke.h"
 #include "e500.h"
--- arch/powerpc/kvm/e500_tlb.c
+++ arch/powerpc/kvm/e500_tlb.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 #include <linux/hugetlb.h>
 #include <asm/kvm_ppc.h>
-#include <asm/kvm_e500.h>
 
 #include "../mm/mmu_decl.h"
 #include "e500.h"
