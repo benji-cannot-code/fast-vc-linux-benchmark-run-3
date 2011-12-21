e7f6724017869c7fac86a87ef9f79038833751c6
Marek Szyprowski <m.szyprowski@samsung.com>
2011-12-21 16:55:57 +0100

    common: dma-mapping: remove old alloc_coherent and free_coherent methods
    Remove old, unused alloc_coherent and free_coherent methods from
    dma_map_ops structure.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Acked-by: Kyungmin Park <kyungmin.park@samsung.com>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/scatterlist.h>
 
 struct dma_map_ops {
-	void* (*alloc_coherent)(struct device *dev, size_t size,
-				dma_addr_t *dma_handle, gfp_t gfp);
-	void (*free_coherent)(struct device *dev, size_t size,
-			      void *vaddr, dma_addr_t dma_handle);
 	void* (*alloc)(struct device *dev, size_t size,
 				dma_addr_t *dma_handle, gfp_t gfp,
 				struct dma_attrs *attrs);
