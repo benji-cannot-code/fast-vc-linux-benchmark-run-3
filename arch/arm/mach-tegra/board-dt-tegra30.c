f96e5c87f186b07d224a98ada59f6dfeddfb14dc
Thierry Reding <thierry.reding@avionic-design.de>
2011-12-21 08:04:13 +0100

    pwm: tegra: Add device tree support
    Add auxdata to instantiate the PWFM controller from a device tree,
    include the corresponding nodes in the dtsi files for Tegra 20 and
    Tegra 30 and add binding documentation.
    
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>

--- arch/arm/mach-tegra/board-dt-tegra30.c
+++ arch/arm/mach-tegra/board-dt-tegra30.c
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/hardware/gic.h>
 
+#include <mach/iomap.h>
+
 #include "board.h"
 #include "clock.h"
 
@@ -53,6 +55,7 @@ struct of_dev_auxdata tegra30_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("nvidia,tegra20-i2c", 0x7000C700, "tegra-i2c.3", NULL),
 	OF_DEV_AUXDATA("nvidia,tegra20-i2c", 0x7000D000, "tegra-i2c.4", NULL),
 	OF_DEV_AUXDATA("nvidia,tegra30-ahub", 0x70080000, "tegra30-ahub", NULL),
+	OF_DEV_AUXDATA("nvidia,tegra30-pwm", TEGRA_PWFM_BASE, "tegra-pwm", NULL),
 	{}
 };
 
