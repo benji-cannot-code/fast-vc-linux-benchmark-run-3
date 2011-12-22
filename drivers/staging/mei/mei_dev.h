ee795a3e231ec9ac790ff22fd1f9380ff8f9631b
Tomas Winkler <tomas.winkler@intel.com>
2011-12-22 18:50:50 +0200

    staging/mei: add watchdog device registration wrappers
    add mei_watchdog_register/unregister wrappers for
    cleaner  encapsulation
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/mei/mei_dev.h
+++ drivers/staging/mei/mei_dev.h
@@ -36,11 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 extern struct pci_dev *mei_device;
 
-/*
- * AMT Watchdog Device
- */
-#define INTEL_AMT_WATCHDOG_ID "INTCAMT"
-extern struct watchdog_device amt_wd_dev;
 
 /*
  * AMTHI Client UUID
