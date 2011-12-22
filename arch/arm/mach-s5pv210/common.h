752e373761a947cf99bdba973cefe89e98b25e5f
Kukjin Kim <kgene.kim@samsung.com>
2011-12-22 23:32:07 +0100

    ARM: 7248/1: S5PV210: introduce arch/arm/mach-s5pv210/common.[ch]
    This patch introduces common.[ch] which are used only in the
    arch/arm/mach-s5pv210/ directory. The common.c file merges
    the cpu.c and init.c which are used commonly on S5PCV210/S5PC100
    SoC and the common.h local header file replaces with plat/s5pv210.h
    file.
    
    Cc: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s5pv210/common.h
+++ arch/arm/mach-s5pv210/common.h
@@ -1,30 +1,30 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/plat-samsung/include/plat/s5pv210.h
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com
  *
- * Copyright (c) 2010 Samsung Electronics Co., Ltd.
- *		http://www.samsung.com/
- *
- * Header file for s5pv210 cpu support
+ * Common Header for S5PV210 machines
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
-*/
+ */
 
-/* Common init code for S5PV210 related SoCs */
+#ifndef __ARCH_ARM_MACH_S5PV210_COMMON_H
+#define __ARCH_ARM_MACH_S5PV210_COMMON_H
 
-extern void s5pv210_common_init_uarts(struct s3c2410_uartcfg *cfg, int no);
-extern void s5pv210_register_clocks(void);
-extern void s5pv210_setup_clocks(void);
+void s5pv210_init_io(struct map_desc *mach_desc, int size);
+void s5pv210_init_irq(void);
+
+void s5pv210_register_clocks(void);
+void s5pv210_setup_clocks(void);
 
 #ifdef CONFIG_CPU_S5PV210
 
 extern  int s5pv210_init(void);
-extern void s5pv210_init_irq(void);
 extern void s5pv210_map_io(void);
 extern void s5pv210_init_clocks(int xtal);
-
-#define s5pv210_init_uarts s5pv210_common_init_uarts
+extern void s5pv210_init_uarts(struct s3c2410_uartcfg *cfg, int no);
 
 #else
 #define s5pv210_init_clocks NULL
@@ -32,3 +32,5 @@ extern void s5pv210_init_clocks(int xtal);
 #define s5pv210_map_io NULL
 #define s5pv210_init NULL
 #endif
+
+#endif /* __ARCH_ARM_MACH_S5PV210_COMMON_H */
--- arch/arm/mach-s5pv210/mach-smdkv210.c
+++ arch/arm/mach-s5pv210/mach-smdkv210.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/regs-serial.h>
 #include <plat/regs-srom.h>
 #include <plat/gpio-cfg.h>
-#include <plat/s5pv210.h>
 #include <plat/devs.h>
 #include <plat/cpu.h>
 #include <plat/adc.h>
@@ -47,6 +46,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/s5p-time.h>
 #include <plat/backlight.h>
 #include <plat/regs-fb-v4.h>
+
+#include "common.h"
 
 /* Following are default values for UCON, ULCON and UFCON UART registers */
 #define SMDKV210_UCON_DEFAULT	(S3C2410_UCON_TXILEVEL |	\
@@ -278,7 +279,7 @@ static struct platform_pwm_backlight_data smdkv210_bl_data = {
 
 static void __init smdkv210_map_io(void)
 {
-	s5p_init_io(NULL, 0, S5P_VA_CHIPID);
+	s5pv210_init_io(NULL, 0);
 	s3c24xx_init_clocks(24000000);
 	s3c24xx_init_uarts(smdkv210_uartcfgs, ARRAY_SIZE(smdkv210_uartcfgs));
 	s5p_set_timer_source(S5P_PWM2, S5P_PWM4);
