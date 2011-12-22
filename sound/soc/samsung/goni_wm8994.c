8b295ec6461b70adaa6eba1763d2fa659781f145
Axel Lin <axel.lin@gmail.com>
2011-12-22 10:53:15 +0800

    ASoC: samsung: Add .owner to struct snd_soc_card
    Missed .owner of struct snd_soc_card will prevent the module from being
    removed from underneath its users.
    
    Reported-by: Lothar Waßmann <LW@KARO-electronics.de>
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/samsung/goni_wm8994.c
+++ sound/soc/samsung/goni_wm8994.c
@@ -245,6 +245,7 @@ static struct snd_soc_dai_link goni_dai[] = {
 
 static struct snd_soc_card goni = {
 	.name = "goni",
+	.owner = THIS_MODULE,
 	.dai_link = goni_dai,
 	.num_links = ARRAY_SIZE(goni_dai),
 
