9587e630028a8235184043c3fff50eb10ff71faa
Rafał Miłecki <zajec5@gmail.com>
2011-12-23 20:36:06 +0100

    drm/radeon/kms: setup HDMI mode on Evergreen encoders
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/evergreen_reg.h
+++ drivers/gpu/drm/radeon/evergreen_reg.h
@@ -200,4 +200,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EVERGREEN_DC_GPIO_HPD_EN                        0x64b8
 #define EVERGREEN_DC_GPIO_HPD_Y                         0x64bc
 
+/* HDMI blocks at 0x7030, 0x7c30, 0x10830, 0x11430, 0x12030, 0x12c30 */
+#define EVERGREEN_HDMI_BASE				0x7030
+
+#define EVERGREEN_HDMI_CONFIG_OFFSET			0xf0
+
 #endif
