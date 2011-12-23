5eb1f55caa41512b5308b17e0dca7a5d0829e9fb
Magnus Damm <damm@opensource.se>
2011-12-23 01:23:07 +0100

    PM / shmobile: Add support for the sh7372 A4S power domain / sleep mode
    The sh7372 contains a power domain named A4S which in turn
    contains power domains for both I/O Devices and CPU cores.
    
    At this point only System wide Suspend-to-RAM is supported,
    but the the hardware can also support CPUIdle. With more
    efforts in the future CPUIdle can work with bot A4S and A3SM.
    
    Tested on the sh7372 Mackerel board.
    
    [rjw: Rebased on top of the current linux-pm tree.]
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- arch/arm/mach-shmobile/include/mach/sh7372.h
+++ arch/arm/mach-shmobile/include/mach/sh7372.h
@@ -500,6 +500,7 @@ extern struct sh7372_pm_domain sh7372_d4;
 extern struct sh7372_pm_domain sh7372_a4r;
 extern struct sh7372_pm_domain sh7372_a3rv;
 extern struct sh7372_pm_domain sh7372_a3ri;
+extern struct sh7372_pm_domain sh7372_a4s;
 extern struct sh7372_pm_domain sh7372_a3sp;
 extern struct sh7372_pm_domain sh7372_a3sg;
 
@@ -516,5 +517,7 @@ extern void sh7372_pm_add_subdomain(struct sh7372_pm_domain *sh7372_pd,
 
 extern void sh7372_intcs_suspend(void);
 extern void sh7372_intcs_resume(void);
+extern void sh7372_intca_suspend(void);
+extern void sh7372_intca_resume(void);
 
 #endif /* __ASM_SH7372_H__ */
