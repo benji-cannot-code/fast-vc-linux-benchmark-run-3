9b086f973a5041935ba7acd443ce73ec3acc9fed
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2011-12-23 17:10:15 +0200

    mtd: introduce mtd_get_unmapped_area interface
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/mtdchar.c
+++ drivers/mtd/mtdchar.c
@@ -1136,7 +1136,7 @@ static unsigned long mtdchar_get_unmapped_area(struct file *file,
 		if (offset > mtd->size - len)
 			return (unsigned long) -EINVAL;
 
-		return mtd->get_unmapped_area(mtd, len, offset, flags);
+		return mtd_get_unmapped_area(mtd, len, offset, flags);
 	}
 
 	/* can't map directly */
