bd231f19c15b14985d52f50c08da20ec64dcb8ed
Greg Ungerer <gerg@uclinux.org>
2011-12-24 13:00:02 +1000

    m68knommu: clean up init code in ColdFire 523x startup
    We can move the QSPI init call to the more general config_BSP() code on
    the 523x platorm setup code. Then we can remove the initcall code all
    together.
    
    We can also remove the un-needed include of mcfuart.h while we are
    cleaning up here too.
    
    Also I noticed that we are not calling the fec_init() code here, and we
    should be doing that. Put that back in too.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/platform/523x/config.c
+++ arch/m68k/platform/523x/config.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/machdep.h>
 #include <asm/coldfire.h>
 #include <asm/mcfsim.h>
-#include <asm/mcfuart.h>
 
 /***************************************************************************/
 
@@ -68,18 +67,10 @@ void __init config_BSP(char *commandp, int size)
 {
 	mach_reset = m523x_cpu_reset;
 	mach_sched_init = hw_timer_init;
-}
-
-/***************************************************************************/
-
-static int __init init_BSP(void)
-{
+	m523x_fec_init();
 #ifdef CONFIG_SPI_COLDFIRE_QSPI
 	m523x_qspi_init();
 #endif
-	return 0;
 }
-
-arch_initcall(init_BSP);
 
 /***************************************************************************/
