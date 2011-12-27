7677760de36c10949fa8bb38284a480c03d79d3c
Heiko Carsten <heiko.carstens@de.ibm.com>
2011-12-27 11:27:29 +0100

    [S390] sysctl: get rid of dead declaration
    Remove dead sysctl_userprocess_debug. The variable is
    gone since we map our old userprocess_debug sysctl to
    the common code show_unhandled_signals sysctl on s390.
    
    Signed-off-by: Heiko Carsten <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/system.h
+++ arch/s390/include/asm/system.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct task_struct;
 
-extern int sysctl_userprocess_debug;
-
 extern struct task_struct *__switch_to(void *, void *);
 extern void update_per_regs(struct task_struct *task);
 
