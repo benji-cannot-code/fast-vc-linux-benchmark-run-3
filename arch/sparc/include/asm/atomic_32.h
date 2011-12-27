c782c39828f0a5bbd6ce58da2b62cfe80530340a
Sam Ravnborg <sam@ravnborg.org>
2011-12-27 21:46:53 +0100

    sparc32: support atomic64_t
    There is no-one that really require atomic64_t support on sparc32.
    But several drivers fails to build without proper atomic64 support.
    And for an allyesconfig build for sparc32 this is annoying.
    
    Include the generic atomic64_t support for sparc32.
    This has a text footprint cost:
    
    $size vmlinux (before atomic64_t support)
       text    data     bss     dec     hex filename
    3578860  134260  108781 3821901  3a514d vmlinux
    
    $size vmlinux (after atomic64_t support)
       text    data     bss     dec     hex filename
    3579892  130684  108781 3819357  3a475d vmlinux
    
    text increase (3579892 - 3578860) = 1032 bytes
    
    data decreases - but I fail to explain why!
    I have rebuild twice to check my numbers.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/atomic_32.h
+++ arch/sparc/include/asm/atomic_32.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
+#include <asm-generic/atomic64.h>
+
 #include <asm/system.h>
 
 #define ATOMIC_INIT(i)  { (i) }
