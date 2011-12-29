7019e811cfe8b6d35e3cc651b0d57f9a4d4890c8
Hans de Goede <hdegoede@redhat.com>
2011-12-29 16:50:57 -0300

    [media] gscpa_vicam: Fix oops if unplugged while streaming
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/vicam.c
+++ drivers/media/video/gspca/vicam.c
@@ -325,7 +325,8 @@ static void sd_stop0(struct gspca_dev *gspca_dev)
 	dev->work_thread = NULL;
 	mutex_lock(&gspca_dev->usb_lock);
 
-	vicam_set_camera_power(gspca_dev, 0);
+	if (gspca_dev->present)
+		vicam_set_camera_power(gspca_dev, 0);
 }
 
 /* Table of supported USB devices */
