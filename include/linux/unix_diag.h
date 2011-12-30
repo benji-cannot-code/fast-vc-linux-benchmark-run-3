a73f1d5aa2696446b6d57cf6bd746bc965d947fb
Pavel Emelyanov <xemul@parallels.com>
2011-12-30 00:52:51 +0000

    unix_diag: Include unix_diag.h into header-y target
    The headers check complains it should include the linux/types.h
    withing, thus add this one.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/unix_diag.h
+++ include/linux/unix_diag.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __UNIX_DIAG_H__
 #define __UNIX_DIAG_H__
 
+#include <linux/types.h>
+
 struct unix_diag_req {
 	__u8	sdiag_family;
 	__u8	sdiag_protocol;
