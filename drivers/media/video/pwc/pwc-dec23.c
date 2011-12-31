501f2c7f8ac5ab9c90726183db9c08bb66408e3e
Hans de Goede <hdegoede@redhat.com>
2011-12-31 07:45:39 -0300

    [media] pwc: Remove driver specific ioctls
    This stems from the v4l1 era, with v4l2 everything can be done with
    standardized v4l2 API calls.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/pwc/pwc-dec23.c
+++ drivers/media/video/pwc/pwc-dec23.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "pwc-timon.h"
 #include "pwc-kiara.h"
 #include "pwc-dec23.h"
-#include <media/pwc-ioctl.h>
 
 #include <linux/string.h>
 #include <linux/slab.h>
--- drivers/media/video/pwc/pwc-timon.h
+++ drivers/media/video/pwc/pwc-timon.h
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef PWC_TIMON_H
 #define PWC_TIMON_H
 
-#include <media/pwc-ioctl.h>
+#include "pwc.h"
 
 #define PWC_FPS_MAX_TIMON 6
 
