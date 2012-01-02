aea8b91b34872c192ef0bfa448c7fc7bf9966556
Andre Guedes <andre.guedes@openbossa.org>
2012-01-02 16:50:53 -0300

    Bluetooth: Fix mgmt_stop_discovery_failed()
    Stop Discovery Command Status Event should use mgmt status code.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>
    Acked-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -2757,7 +2757,7 @@ int mgmt_stop_discovery_failed(struct hci_dev *hdev, u8 status)
 	if (!cmd)
 		return -ENOENT;
 
-	err = cmd_status(cmd->sk, hdev->id, cmd->opcode, status);
+	err = cmd_status(cmd->sk, hdev->id, cmd->opcode, mgmt_status(status));
 	mgmt_pending_remove(cmd);
 
 	return err;
