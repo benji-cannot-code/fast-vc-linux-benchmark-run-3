8329d5c6311a7f533bfb517adcf5bfeb867316a6
Fabio Estevam <festevam@gmail.com>
2012-01-02 14:19:03 -0200

    ARM: prom.h: Fix build error by removing unneeded header file
    Fix the following build error:
    
      CC [M]  fs/udf/balloc.o
    In file included from /home/fabio/next/linux-next/arch/arm/include/asm/prom.h:16,
                     from include/linux/of.h:140,
                     from include/asm-generic/gpio.h:7,
                     from arch/arm/plat-mxc/include/mach/irqs.h:14,
                     from /home/fabio/next/linux-next/arch/arm/include/asm/irq.h:4,
                     from /home/fabio/next/linux-next/arch/arm/include/asm/hardirq.h:6,
                     from include/linux/hardirq.h:7,
                     from include/linux/highmem.h:8,
                     from include/linux/pagemap.h:10,
                     from include/linux/buffer_head.h:13,
                     from fs/udf/udfdecl.h:11,
                     from fs/udf/balloc.c:22:
    /home/fabio/next/linux-next/arch/arm/include/asm/setup.h:146: error: redefinition of 'struct tag'
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    [grant.likely: fix build failure on drivers/of/fdt.c]
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- arch/arm/include/asm/prom.h
+++ arch/arm/include/asm/prom.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_OF
 
-#include <asm/setup.h>
 #include <asm/irq.h>
 
 extern struct machine_desc *setup_machine_fdt(unsigned int dt_phys);
--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/slab.h>
 
+#include <asm/setup.h>  /* for COMMAND_LINE_SIZE */
 #ifdef CONFIG_PPC
 #include <asm/machdep.h>
 #endif /* CONFIG_PPC */
