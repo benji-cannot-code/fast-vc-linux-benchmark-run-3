1d2aca10c2ce705385a2931664cf8f007051f8fe
Rob Herring <rob.herring@calxeda.com>
2012-01-03 16:57:33 -0600

    ARM: shmobile: remove NR_IRQS
    Remove NR_IRQS and explicitly include mach/irqs.h as needed. shmobile
    properly allocates irq_descs for each irqchip, so setting .nr_irqs for
    each machine is not needed.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>

--- arch/arm/mach-shmobile/include/mach/irqs.h
+++ arch/arm/mach-shmobile/include/mach/irqs.h
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/sh_intc.h>
 
-#define NR_IRQS         1024
-
 /* GIC */
 #define gic_spi(nr)		((nr) + 32)
 
