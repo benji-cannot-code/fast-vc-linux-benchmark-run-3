19684536bbec46c000e61bcba40a78625e804a71
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2012-01-04 14:10:32 -0500

    xen/pciback: Move the PCI_DEV_FLAGS_ASSIGNED ops to the "[un|]bind"
    operation instead of doing it per guest creation/disconnection. Without
    this we could have potentially unloaded the vf driver from the
    xen pciback control even if the driver was binded to the xen-pciback.
    This will hold on to it until the user "unbind"s the PCI device using
    SysFS.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/xen-pciback/pci_stub.c
+++ drivers/xen/xen-pciback/pci_stub.c
@@ -100,6 +100,7 @@ static void pcistub_device_release(struct kref *kref)
 	kfree(pci_get_drvdata(psdev->dev));
 	pci_set_drvdata(psdev->dev, NULL);
 
+	psdev->dev->dev_flags &= ~PCI_DEV_FLAGS_ASSIGNED;
 	pci_dev_put(psdev->dev);
 
 	kfree(psdev);
@@ -332,6 +333,7 @@ static int __devinit pcistub_init_device(struct pci_dev *dev)
 	dev_dbg(&dev->dev, "reset device\n");
 	xen_pcibk_reset_device(dev);
 
+	dev->dev_flags |= PCI_DEV_FLAGS_ASSIGNED;
 	return 0;
 
 config_release:
