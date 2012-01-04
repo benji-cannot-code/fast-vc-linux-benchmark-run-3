1df0735e468dcf273d6d3d1dd343c3c3034ffbcd
Frans Meulenbroeks <fransmeulenbroeks@gmail.com>
2012-01-04 23:27:19 +0100

    max1111.c: fix checkpatch warning
    Signed-off-by: Frans Meulenbroeks <fransmeulenbroeks@gmail.com>
    Signed-off-by: Guenter Roeck <guenter.roeck@ericsson.com>

--- drivers/hwmon/max1111.c
+++ drivers/hwmon/max1111.c
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2004-2005 Richard Purdie
  *
  * Copyright (C) 2008 Marvell International Ltd.
- * 	Eric Miao <eric.miao@marvell.com>
+ *	Eric Miao <eric.miao@marvell.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
