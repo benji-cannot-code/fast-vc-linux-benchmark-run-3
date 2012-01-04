a995d53a9f697ae237ee38899074bc677f20e28f
Florian Fainelli <florian@openwrt.org>
2012-01-04 08:59:35 +0000

    r6040: use MAC_RST bit definition with MCR1 read/writes
    MAC_RST bit is already defined, use it instead of 0x1 where applicable.
    
    Signed-off-by: Florian Fainelli <florian@openwrt.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/rdc/r6040.c
+++ drivers/net/ethernet/rdc/r6040.c
@@ -372,7 +372,7 @@ static void r6040_init_mac_regs(struct net_device *dev)
 	iowrite16(MAC_RST, ioaddr + MCR1);
 	while (limit--) {
 		cmd = ioread16(ioaddr + MCR1);
-		if (cmd & 0x1)
+		if (cmd & MAC_RST)
 			break;
 	}
 	/* Reset internal state machine */
@@ -454,7 +454,7 @@ static void r6040_down(struct net_device *dev)
 	iowrite16(MAC_RST, ioaddr + MCR1);	/* Reset RDC MAC */
 	while (limit--) {
 		cmd = ioread16(ioaddr + MCR1);
-		if (cmd & 0x1)
+		if (cmd & MAC_RST)
 			break;
 	}
 
@@ -736,7 +736,7 @@ static void r6040_mac_address(struct net_device *dev)
 	u16 *adrp;
 
 	/* MAC operation register */
-	iowrite16(0x01, ioaddr + MCR1); /* Reset MAC */
+	iowrite16(MAC_RST, ioaddr + MCR1); /* Reset MAC */
 	iowrite16(2, ioaddr + MAC_SM); /* Reset internal state machine */
 	iowrite16(0, ioaddr + MAC_SM);
 	mdelay(5);
