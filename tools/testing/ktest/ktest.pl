ef207738ee24565fa08875129b808b6d093f0c0c
Steven Rostedt <srostedt@redhat.com>
2012-01-04 21:51:59 -0500

    ktest: Add INGORE_ERRORS to ignore warnings in boot up
    When testing a kernel that has warnings, ktest.pl will fail the test
    when it sees the warning. If you need to test the the kernel and want
    to ignore the errors that are produced, the option IGNORE_ERRORS has
    been added. When IGNORE_ERRORS is set to something other than 0, it will
    ignore call traces due to WARN_ON().
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -105,6 +105,7 @@ my $start_minconfig;
 my $start_minconfig_defined;
 my $output_minconfig;
 my $ignore_config;
+my $ignore_errors;
 my $addconfig;
 my $in_bisect = 0;
 my $bisect_bad_commit = "";
@@ -219,6 +220,7 @@ my %option_map = (
     "BISECT_SLEEP_TIME"		=> \$bisect_sleep_time,
     "PATCHCHECK_SLEEP_TIME"	=> \$patchcheck_sleep_time,
     "IGNORE_WARNINGS"		=> \$ignore_warnings,
+    "IGNORE_ERRORS"		=> \$ignore_errors,
     "BISECT_MANUAL"		=> \$bisect_manual,
     "BISECT_SKIP"		=> \$bisect_skip,
     "CONFIG_BISECT_GOOD"	=> \$config_bisect_good,
@@ -1530,7 +1532,7 @@ sub monitor {
 	}
 
 	if ($full_line =~ /call trace:/i) {
-	    if (!$bug && !$skip_call_trace) {
+	    if (!$ignore_errors && !$bug && !$skip_call_trace) {
 		$bug = 1;
 		$failure_start = time;
 	    }
