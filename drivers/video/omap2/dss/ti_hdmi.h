00bbad867f5f13276bd98decdf9f062d6f0c3811
Mythri P K <mythripk@ti.com>
2012-01-06 17:52:08 +0530

    OMAPDSS: HDMI: update static timing table
    Add the vsync polarity, hsync polarity, interlace to hdmi_video_timings.
    Remove the now duplicate structure hdmi_timings.
    update the static table structure in HDMI with CEA/VESA code and mode.
    
    Signed-off-by: Mythri P K <mythripk@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/ti_hdmi.h
+++ drivers/video/omap2/dss/ti_hdmi.h
@@ -43,6 +43,7 @@ enum hdmi_clk_refsel {
 	HDMI_REFSEL_SYSCLK = 3
 };
 
+/* HDMI timing structure */
 struct hdmi_video_timings {
 	u16 x_res;
 	u16 y_res;
@@ -54,13 +55,9 @@ struct hdmi_video_timings {
 	u16 vsw;
 	u16 vfp;
 	u16 vbp;
-};
-
-/* HDMI timing structure */
-struct hdmi_timings {
-	struct hdmi_video_timings timings;
-	int vsync_pol;
-	int hsync_pol;
+	bool vsync_pol;
+	bool hsync_pol;
+	bool interlace;
 };
 
 struct hdmi_cm {
@@ -69,8 +66,7 @@ struct hdmi_cm {
 };
 
 struct hdmi_config {
-	struct hdmi_timings timings;
-	u16	interlace;
+	struct hdmi_video_timings timings;
 	struct hdmi_cm cm;
 };
 
