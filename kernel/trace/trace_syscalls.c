4076ae1822e517d490abd57e081d8afd22d92806
H. Peter Anvin <hpa@zytor.com>
2012-01-07 14:10:18 -0800

    x86: Move <asm/asm-offsets.h> from trace_syscalls.c to asm/syscall.h
    This reverts commit d5e553d6e0a4bdea43adae7373e3fa144b9a1aaa, which
    caused large numbers of build warnings on PowerPC.
    
    This moves the #include <asm/asm-offsets.h> to <asm/syscall.h>, which
    makes some kind of sense since NR_syscalls is syscalls related.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Link: http://lkml.kernel.org/r/20111214181545.6e13bc954cb7ddce9086e861@canb.auug.org.au

--- kernel/trace/trace_syscalls.c
+++ kernel/trace/trace_syscalls.c
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ftrace.h>
 #include <linux/perf_event.h>
 #include <asm/syscall.h>
-#include <asm/asm-offsets.h>
 
 #include "trace_output.h"
 #include "trace.h"
