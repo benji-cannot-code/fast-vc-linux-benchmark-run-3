e18a715b33b307b3275e289b33d3cd9abe6bd9c8
David Herrmann <dh.herrmann@googlemail.com>
2012-01-07 15:47:24 +0100

    Bluetooth: Remove __hci_dev_put/hold
    Since we remove the owner field of hci_dev hci_dev_put and __hci_dev_put
    do the same so we can merge them into one function. Same for
    hci_dev_hold and __hci_dev_hold.
    
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1655,7 +1655,7 @@ int hci_register_dev(struct hci_dev *hdev)
 	schedule_work(&hdev->power_on);
 
 	hci_notify(hdev, HCI_DEV_REG);
-	__hci_dev_hold(hdev);
+	hci_dev_hold(hdev);
 
 	return id;
 
@@ -1718,7 +1718,7 @@ void hci_unregister_dev(struct hci_dev *hdev)
 	hci_adv_entries_clear(hdev);
 	hci_dev_unlock(hdev);
 
-	__hci_dev_put(hdev);
+	hci_dev_put(hdev);
 }
 EXPORT_SYMBOL(hci_unregister_dev);
 
