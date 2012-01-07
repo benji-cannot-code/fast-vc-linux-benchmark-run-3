5f49f5793504a4a4b205b8288f4ade5a65ef2710
David Herrmann <dh.herrmann@googlemail.com>
2012-01-07 15:47:08 +0100

    Bluetooth: bluecard-cs: Remove empty destruct cb
    The destruct callback is optional and we provide an empty callback so
    remove it entirely to avoid unnecessary code.
    
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- drivers/bluetooth/bluecard_cs.c
+++ drivers/bluetooth/bluecard_cs.c
@@ -692,11 +692,6 @@ static int bluecard_hci_send_frame(struct sk_buff *skb)
 }
 
 
-static void bluecard_hci_destruct(struct hci_dev *hdev)
-{
-}
-
-
 static int bluecard_hci_ioctl(struct hci_dev *hdev, unsigned int cmd, unsigned long arg)
 {
 	return -ENOIOCTLCMD;
@@ -742,7 +737,6 @@ static int bluecard_open(bluecard_info_t *info)
 	hdev->close    = bluecard_hci_close;
 	hdev->flush    = bluecard_hci_flush;
 	hdev->send     = bluecard_hci_send_frame;
-	hdev->destruct = bluecard_hci_destruct;
 	hdev->ioctl    = bluecard_hci_ioctl;
 
 	hdev->owner = THIS_MODULE;
