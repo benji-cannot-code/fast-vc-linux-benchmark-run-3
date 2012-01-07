83487483236cdcd8305fe986d11184828da31ec3
Jaecheol Lee <jc.lee@samsung.com>
2012-01-07 20:18:39 +0900

    [CPUFREQ] EXYNOS: Removed useless headers and codes
    This patch removes no referencing header files and cleaned up
    useless code.
    
    Signed-off-by: Jaecheol Lee <jc.lee@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/cpufreq/exynos-cpufreq.c
+++ drivers/cpufreq/exynos-cpufreq.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
 */
 
-#include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/err.h>
 #include <linux/clk.h>
@@ -19,15 +18,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regulator/consumer.h>
 #include <linux/cpufreq.h>
 #include <linux/suspend.h>
-#include <linux/reboot.h>
 
-#include <mach/map.h>
-#include <mach/regs-clock.h>
-#include <mach/regs-mem.h>
 #include <mach/cpufreq.h>
 
-#include <plat/clock.h>
-#include <plat/pm.h>
+#include <plat/cpu.h>
 
 static struct exynos_dvfs_info *exynos_info;
 
