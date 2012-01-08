189ae3515a32e741e20742132bf32f5d3fe4f1dc
Russell King <rmk+kernel@arm.linux.org.uk>
2012-01-08 12:04:05 +0000

    NET: sa11x0-ir: fix documentation bug
    Spell the module parameter correctly in comments.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/net/irda/sa1100_ir.c
+++ drivers/net/irda/sa1100_ir.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  This driver takes one kernel command line parameter, sa1100ir=, with
  *  the following options:
  *	max_rate:baudrate	- set the maximum baud rate
- *	power_leve:level	- set the transmitter power level
+ *	power_level:level	- set the transmitter power level
  *	tx_lpm:0|1		- set transmit low power mode
  */
 #include <linux/module.h>
