79d6ef332d780283d1acb321b728fd8cddb451fa
Eugeni Dodonov <eugeni.dodonov@intel.com>
2012-01-07 23:40:34 -0200

    drm/i915: simplify pipe checking
    This is also handled by i915_reg.h, so just reuse this trick to reduce
    universe entropy.
    
    Signed-off-by: Eugeni Dodonov <eugeni.dodonov@intel.com>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Reviewed-by: Cyril Brulebois <kibi@debian.org>
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- drivers/gpu/drm/i915/i915_suspend.c
+++ drivers/gpu/drm/i915/i915_suspend.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "drm.h"
 #include "i915_drm.h"
 #include "intel_drv.h"
+#include "i915_reg.h"
 
 static bool i915_pipe_enabled(struct drm_device *dev, enum pipe pipe)
 {
@@ -36,7 +37,7 @@ static bool i915_pipe_enabled(struct drm_device *dev, enum pipe pipe)
 	u32	dpll_reg;
 
 	if (HAS_PCH_SPLIT(dev))
-		dpll_reg = (pipe == PIPE_A) ? _PCH_DPLL_A : _PCH_DPLL_B;
+		dpll_reg = PCH_DPLL(pipe);
 	else
 		dpll_reg = (pipe == PIPE_A) ? _DPLL_A : _DPLL_B;
 
