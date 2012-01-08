d659c254e5d51d03b896ac2370f8eac633cb52d9
Mark Salter <msalter@redhat.com>
2012-01-08 12:31:28 -0500

    C6X: deal with memblock API changes
    Recent memblock related commits require the following C6X changes:
    
      * commit 24aa07882b672fff2da2f5c955759f0bd13d32d5
        asm/memblock.h no longer required
    
      * commit 1440c4e2c918532f39131c3330fe2226e16be7b6
        memblock_analyze() no longer needed to update total size
    
      * commit fe091c208a40299fba40e62292a610fb91e44b4e
        memblock_init() no longer needed
    
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- arch/c6x/kernel/setup.c
+++ arch/c6x/kernel/setup.c
@@ -306,8 +306,6 @@ void __init setup_arch(char **cmdline_p)
 	/* Initialize command line */
 	*cmdline_p = c6x_command_line;
 
-	memblock_init();
-
 	memory_end = ram_end;
 	memory_end &= ~(PAGE_SIZE - 1);
 
@@ -367,7 +365,6 @@ void __init setup_arch(char **cmdline_p)
 					 memory_end >> PAGE_SHIFT);
 	memblock_reserve(memory_start, bootmap_size);
 
-	memblock_analyze();
 	unflatten_device_tree();
 
 	c6x_cache_init();
