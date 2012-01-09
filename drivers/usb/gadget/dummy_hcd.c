5490e6c9136e8ec816329511b981e2ed4e22e7bd
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-01-09 13:15:00 +0100

    usb: gadget: dummy_hcd: make alloc/free streams static
    There is no reason why dummy_alloc_streams() and dummy_free_streams()
    are global. Make them static instead.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/dummy_hcd.c
+++ drivers/usb/gadget/dummy_hcd.c
@@ -2287,7 +2287,7 @@ static int dummy_setup(struct usb_hcd *hcd)
 }
 
 /* Change a group of bulk endpoints to support multiple stream IDs */
-int dummy_alloc_streams(struct usb_hcd *hcd, struct usb_device *udev,
+static int dummy_alloc_streams(struct usb_hcd *hcd, struct usb_device *udev,
 	struct usb_host_endpoint **eps, unsigned int num_eps,
 	unsigned int num_streams, gfp_t mem_flags)
 {
@@ -2299,7 +2299,7 @@ int dummy_alloc_streams(struct usb_hcd *hcd, struct usb_device *udev,
 }
 
 /* Reverts a group of bulk endpoints back to not using stream IDs. */
-int dummy_free_streams(struct usb_hcd *hcd, struct usb_device *udev,
+static int dummy_free_streams(struct usb_hcd *hcd, struct usb_device *udev,
 	struct usb_host_endpoint **eps, unsigned int num_eps,
 	gfp_t mem_flags)
 {
