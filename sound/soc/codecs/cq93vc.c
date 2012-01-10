3b29ae257b399bad33a2d5a021a87adb32a6a0a0
Manjunath Hadli <manjunath.hadli@ti.com>
2012-01-10 16:57:43 +0530

    ASoC: CQ93VC: remove machine specific header file inclusion from codec driver
    remove unnecessary inclusion of machine specific header
    file mach/dm365.h from cq93vc.c voice codec driver
    which comes in the way of platform code consolidation.
    
    Signed-off-by: Manjunath Hadli <manjunath.hadli@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/cq93vc.c
+++ sound/soc/codecs/cq93vc.c
@@ -39,8 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 #include <sound/initval.h>
 
-#include <mach/dm365.h>
-
 static inline unsigned int cq93vc_read(struct snd_soc_codec *codec,
 						unsigned int reg)
 {
