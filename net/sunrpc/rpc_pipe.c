8ce3c825136c09371001f085a14a632ce1b7a41b
Stanislav Kinsbursky <skinsbursky@parallels.com>
2012-01-10 17:04:48 +0400

    SUNRPC: kernel PipeFS mount point creation routines removed
    This patch removes static rpc_mnt variable and its creation and destruction
    routines, because they are not used anymore.
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- net/sunrpc/rpc_pipe.c
+++ net/sunrpc/rpc_pipe.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 
 #include <asm/ioctls.h>
-#include <linux/fs.h>
 #include <linux/poll.h>
 #include <linux/wait.h>
 #include <linux/seq_file.h>
@@ -37,9 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RPCDBG_FACILITY RPCDBG_DEBUG
 
 #define NET_NAME(net)	((net == &init_net) ? " (init_net)" : "")
-
-static struct vfsmount *rpc_mnt __read_mostly;
-static int rpc_mount_count;
 
 static struct file_system_type rpc_pipe_fs_type;
 
@@ -449,23 +445,6 @@ struct rpc_filelist {
 	const struct file_operations *i_fop;
 	umode_t mode;
 };
-
-struct vfsmount *rpc_get_mount(void)
-{
-	int err;
-
-	err = simple_pin_fs(&rpc_pipe_fs_type, &rpc_mnt, &rpc_mount_count);
-	if (err != 0)
-		return ERR_PTR(err);
-	return rpc_mnt;
-}
-EXPORT_SYMBOL_GPL(rpc_get_mount);
-
-void rpc_put_mount(void)
-{
-	simple_release_fs(&rpc_mnt, &rpc_mount_count);
-}
-EXPORT_SYMBOL_GPL(rpc_put_mount);
 
 static int rpc_delete_dentry(const struct dentry *dentry)
 {
