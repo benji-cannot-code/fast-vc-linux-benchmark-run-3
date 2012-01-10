248148f8c557431a2ef5eb33be8eff3dadf97813
Darren Hart <dvhart@linux.intel.com>
2012-01-10 15:41:12 -0800

    merge_config.sh: whitespace cleanup
    Fix whitespace usage in the clean_up routine.
    
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>
    Acked-by: John Stultz <john.stultz@linaro.org>
    Cc: Michal Marek <mmarek@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/merge_config.sh
+++ scripts/kconfig/merge_config.sh
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  See the GNU General Public License for more details.
 
 clean_up() {
-       rm -f $TMP_FILE
-       exit
+	rm -f $TMP_FILE
+	exit
 }
 trap clean_up HUP INT TERM
 
