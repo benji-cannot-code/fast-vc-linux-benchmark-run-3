1ca3ae91dc920cd8854a0f7466a02f772a9b1f69
Antti Palosaari <crope@iki.fi>
2012-01-10 13:15:16 -0300

    [media] hd29l2: synch for latest DVB core changes
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/hd29l2.c
+++ drivers/media/dvb/frontends/hd29l2.c
@@ -334,8 +334,7 @@ static int hd29l2_read_ucblocks(struct dvb_frontend *fe, u32 *ucblocks)
 	return 0;
 }
 
-static enum dvbfe_search hd29l2_search(struct dvb_frontend *fe,
-	struct dvb_frontend_parameters *p)
+static enum dvbfe_search hd29l2_search(struct dvb_frontend *fe)
 {
 	int ret, i;
 	struct hd29l2_priv *priv = fe->demodulator_priv;
@@ -357,7 +356,7 @@ static enum dvbfe_search hd29l2_search(struct dvb_frontend *fe,
 
 	/* program tuner */
 	if (fe->ops.tuner_ops.set_params)
-		fe->ops.tuner_ops.set_params(fe, p);
+		fe->ops.tuner_ops.set_params(fe);
 
 	/* get and program IF */
 	if (fe->ops.tuner_ops.get_if_frequency)
@@ -553,8 +552,7 @@ static int hd29l2_get_frontend_algo(struct dvb_frontend *fe)
 	return DVBFE_ALGO_CUSTOM;
 }
 
-static int hd29l2_get_frontend(struct dvb_frontend *fe,
-	struct dvb_frontend_parameters *p)
+static int hd29l2_get_frontend(struct dvb_frontend *fe)
 {
 	int ret;
 	struct hd29l2_priv *priv = fe->demodulator_priv;
@@ -823,9 +821,9 @@ err:
 EXPORT_SYMBOL(hd29l2_attach);
 
 static struct dvb_frontend_ops hd29l2_ops = {
+	.delsys = { SYS_DVBT },
 	.info = {
 		.name = "HDIC HD29L2 DMB-TH",
-		.type = FE_OFDM,
 		.frequency_min = 474000000,
 		.frequency_max = 858000000,
 		.frequency_stepsize = 10000,
