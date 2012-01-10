f6842de66b3dbe51d4691a55117d15bad4dc2fc0
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-01-10 18:00:50 -0300

    [media] [PATCH] don't reset the delivery system on DTV_CLEAR
    As a DVBv3 application may be relying on the delivery system,
    don't reset it at DTV_CLEAR. For DVBv5 applications, the
    delivery system should be set anyway.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-core/dvb_frontend.c
+++ drivers/media/dvb/dvb-core/dvb_frontend.c
@@ -910,7 +910,6 @@ static int dvb_frontend_clear_cache(struct dvb_frontend *fe)
 
 	c->state = DTV_CLEAR;
 
-	c->delivery_system = fe->ops.delsys[0];
 	dprintk("%s() Clearing cache for delivery system %d\n", __func__,
 		c->delivery_system);
 
@@ -2378,6 +2377,8 @@ int dvb_register_frontend(struct dvb_adapter* dvb,
 	 * Initialize the cache to the proper values according with the
 	 * first supported delivery system (ops->delsys[0])
 	 */
+
+        fe->dtv_property_cache.delivery_system = fe->ops.delsys[0];
 	dvb_frontend_clear_cache(fe);
 
 	mutex_unlock(&frontend_mutex);
