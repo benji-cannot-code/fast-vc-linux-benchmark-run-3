6ff1b4ae677f0ace71db30b76c135095d0ddaae0
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-01-10 09:48:50 -0200

    [media] cx231xx: fix device disconnect checks
    The driver were using DEV_MISCONFIGURED on some places, and
    DEV_DISCONNECTED on others. In a matter of fact, DEV_MISCONFIGURED
    were set only during the usb disconnect callback, with
    was confusing.
    
    Also, the alsa driver never checks if the device is present,
    before doing some dangerous things.
    
    Remove DEV_MISCONFIGURED, replacing it by DEV_DISCONNECTED.
    
    Also, fixes the other usecases for DEV_DISCONNECTED.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/cx231xx/cx231xx-audio.c
+++ drivers/media/video/cx231xx/cx231xx-audio.c
@@ -112,6 +112,9 @@ static void cx231xx_audio_isocirq(struct urb *urb)
 	struct snd_pcm_substream *substream;
 	struct snd_pcm_runtime *runtime;
 
+	if (dev->state & DEV_DISCONNECTED)
+		return;
+
 	switch (urb->status) {
 	case 0:		/* success */
 	case -ETIMEDOUT:	/* NAK */
@@ -197,6 +200,9 @@ static void cx231xx_audio_bulkirq(struct urb *urb)
 	struct snd_pcm_substream *substream;
 	struct snd_pcm_runtime *runtime;
 
+	if (dev->state & DEV_DISCONNECTED)
+		return;
+
 	switch (urb->status) {
 	case 0:		/* success */
 	case -ETIMEDOUT:	/* NAK */
@@ -274,6 +280,9 @@ static int cx231xx_init_audio_isoc(struct cx231xx *dev)
 
 	cx231xx_info("%s: Starting ISO AUDIO transfers\n", __func__);
 
+	if (dev->state & DEV_DISCONNECTED)
+		return -ENODEV;
+
 	sb_size = CX231XX_ISO_NUM_AUDIO_PACKETS * dev->adev.max_pkt_size;
 
 	for (i = 0; i < CX231XX_AUDIO_BUFS; i++) {
@@ -331,6 +340,9 @@ static int cx231xx_init_audio_bulk(struct cx231xx *dev)
 	int sb_size;
 
 	cx231xx_info("%s: Starting BULK AUDIO transfers\n", __func__);
+
+	if (dev->state & DEV_DISCONNECTED)
+		return -ENODEV;
 
 	sb_size = CX231XX_NUM_AUDIO_PACKETS * dev->adev.max_pkt_size;
 
@@ -430,6 +442,11 @@ static int snd_cx231xx_capture_open(struct snd_pcm_substream *substream)
 	if (!dev) {
 		cx231xx_errdev("BUG: cx231xx can't find device struct."
 			       " Can't proceed with open\n");
+		return -ENODEV;
+	}
+
+	if (dev->state & DEV_DISCONNECTED) {
+		cx231xx_errdev("Can't open. the device was removed.\n");
 		return -ENODEV;
 	}
 
@@ -571,6 +588,9 @@ static int snd_cx231xx_capture_trigger(struct snd_pcm_substream *substream,
 {
 	struct cx231xx *dev = snd_pcm_substream_chip(substream);
 	int retval;
+
+	if (dev->state & DEV_DISCONNECTED)
+		return -ENODEV;
 
 	spin_lock(&dev->adev.slock);
 	switch (cmd) {
