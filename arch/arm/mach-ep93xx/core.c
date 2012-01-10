61bb7dcb6899e5d00333019b0b75da3cccb118fe
Ryan Mallon <rmallon@gmail.com>
2012-01-11 09:29:26 +1100

    ep93xx: Move peripheral defines to local SoC header
    Move the AHB/APB peripheral defines to local SoC header since they are
    only needed by the core SoC code. The UART defines are not moved
    because they are used by the mach/uncompress.h header.
    
    Signed-off-by: Ryan Mallon <rmallon@gmail.com>
    Reviewed-by: Mika Westerberg <mika.westerberg@iki.fi>
    Acked-by: Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ep93xx/core.c
+++ arch/arm/mach-ep93xx/core.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/hardware/vic.h>
 
+#include "soc.h"
 
 /*************************************************************************
  * Static I/O mappings that are needed for all EP93xx platforms
