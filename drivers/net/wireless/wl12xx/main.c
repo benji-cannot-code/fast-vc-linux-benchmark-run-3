449d1d1c3f97cf9971d7be2e984ec284f8d3e6f3
Ido Yariv <ido@wizery.com>
2012-01-11 09:42:40 +0200

    wl12xx: Power off after flushing work
    When stopping plt, the chip is powered off before all current work items
    are flushed and interrupts are disabled. This might introduce a race in
    which the driver tries to communicate with a powered off chip.
    
    Fix this by powering off the device only after interrupts are disabled
    and all work items are flushed.
    
    Signed-off-by: Ido Yariv <ido@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/wl12xx/main.c
+++ drivers/net/wireless/wl12xx/main.c
@@ -1407,8 +1407,6 @@ int wl1271_plt_stop(struct wl1271 *wl)
 		goto out;
 	}
 
-	wl1271_power_off(wl);
-
 	wl->state = WL1271_STATE_OFF;
 	wl->rx_counter = 0;
 
@@ -1418,6 +1416,11 @@ int wl1271_plt_stop(struct wl1271 *wl)
 	wl1271_flush_deferred_work(wl);
 	cancel_work_sync(&wl->netstack_work);
 	cancel_work_sync(&wl->recovery_work);
+
+	mutex_lock(&wl->mutex);
+	wl1271_power_off(wl);
+	mutex_unlock(&wl->mutex);
+
 out:
 	return ret;
 }
