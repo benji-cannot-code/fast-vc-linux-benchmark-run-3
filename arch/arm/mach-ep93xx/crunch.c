a6f1c4bfbff2403044b64ac62d61c3f91c6435ae
Ryan Mallon <rmallon@gmail.com>
2012-01-11 12:53:33 +1100

    ep93xx: Move EP93XX_SYSCON defines to SoC private header
    The EP93XX_SYSCON defines are now no longer needed outside of the
    EP93xx SoC core code, so they can be moved to a private header.
    
    Signed-off-by: Ryan Mallon <rmallon@gmail.com>
    Reviewed-by: Mika Westerberg <mika.westerberg@iki.fi>
    Acked-by: Hartley Sweeten <hsweeten@visionengravers.com>

--- arch/arm/mach-ep93xx/crunch.c
+++ arch/arm/mach-ep93xx/crunch.c
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/ep93xx-regs.h>
 #include <asm/thread_notify.h>
 
+#include "soc.h"
+
 struct crunch_state *crunch_owner;
 
 void crunch_task_release(struct thread_info *thread)
