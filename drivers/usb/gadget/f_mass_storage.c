1b4be72da9d587b07653fed35c74a399ec1b45c9
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-01-13 21:54:46 +0100

    usb: gadget: f_mass_storage: remove one FSG_NO_INTR_EP
    Remove one define of FSG_NO_INTR_EP and we still have that we can use.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -305,7 +305,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static const char fsg_string_interface[] = "Mass Storage";
 
-#define FSG_NO_INTR_EP 1
 #define FSG_NO_DEVICE_STRINGS    1
 #define FSG_NO_OTG               1
 #define FSG_NO_INTR_EP           1
