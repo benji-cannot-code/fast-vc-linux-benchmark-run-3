28c029ce26113619baf3d2c52e569ece40921f5b
Josef Bacik <josef@redhat.com>
2012-01-12 19:10:12 -0500

    Btrfs: release space on error in page_mkwrite
    If updating the inode gave us an ENOSPC we were just returning in page_mkwrite,
    which is a problem since we make our reservation right before trying to update
    the inode, so fix the out label so that we actually free our reservation.
    Thanks,
    
    Signed-off-by: Josef Bacik <josef@redhat.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6489,8 +6489,8 @@ out_unlock:
 	if (!ret)
 		return VM_FAULT_LOCKED;
 	unlock_page(page);
-	btrfs_delalloc_release_space(inode, PAGE_CACHE_SIZE);
 out:
+	btrfs_delalloc_release_space(inode, PAGE_CACHE_SIZE);
 	return ret;
 }
 
