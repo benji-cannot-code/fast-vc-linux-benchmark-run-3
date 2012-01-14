ee499a0b7e7c083a68fa1e1c50920343e7520453
Devendra Naga <devendra.aaru@gmail.com>
2012-01-14 08:16:21 +0000

    net: remove version.h includes in net/openvswitch/
    remove version.h includes in net/openswitch/ as reported by make versioncheck.
    
    Signed-off-by: Devendra Naga <devendra.aaru@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/openvswitch/datapath.c
+++ net/openvswitch/datapath.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rcupdate.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
-#include <linux/version.h>
 #include <linux/ethtool.h>
 #include <linux/wait.h>
 #include <asm/system.h>
