68bc18c9736057b3bf9efdcbd5a1d3af18e5ea5c
Russell King <rmk+kernel@arm.linux.org.uk>
2012-01-14 12:21:18 +0000

    FB: sa1100: remove assabet specific initialization
    Remove the assabet specific initialization for PAL output mode -
    we call the lcd_power function before we enable the LCD controller,
    which will disable the LCD panel to prevent it receiving incorrect
    timings.  Therefore, this setup here is redundant.
    
    Acked-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/video/sa1100fb.c
+++ drivers/video/sa1100fb.c
@@ -183,15 +183,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/hardware.h>
 #include <asm/mach-types.h>
-#include <mach/assabet.h>
 #include <mach/shannon.h>
 
 /*
  * Complain if VAR is out of range.
  */
 #define DEBUG_VAR 1
-
-#undef ASSABET_PAL_VIDEO
 
 #include "sa1100fb.h"
 
@@ -1229,11 +1226,6 @@ static int __devinit sa1100fb_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "request_irq failed: %d\n", ret);
 		goto failed;
 	}
-
-#ifdef ASSABET_PAL_VIDEO
-	if (machine_is_assabet())
-		ASSABET_BCR_clear(ASSABET_BCR_LCD_ON);
-#endif
 
 	/*
 	 * This makes sure that our colour bitfield
