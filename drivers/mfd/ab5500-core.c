480b8af52368cc2ffe59f2ad1dbbdbc83345e748
Jesper Juhl <jj@chaosbits.net>
2012-01-15 21:26:46 +0100

    ab5500-core.c: Don't needlessly include linux/version.h
    As 'make versioncheck' so nicely points out, there's no need for
    drivers/mfd/ab5500-core.c to #include the linux/version.h header.
    This patch removes the pointless include.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/mfd/ab5500-core.c
+++ drivers/mfd/ab5500-core.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/spinlock.h>
 #include <linux/mfd/core.h>
-#include <linux/version.h>
 #include <linux/mfd/db5500-prcmu.h>
 
 #include "ab5500-core.h"
