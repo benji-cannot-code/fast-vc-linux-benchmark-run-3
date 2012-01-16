12c027c97e191db2500a1663cecd0d81b727ca1e
Shan Wei <davidshan@tencent.com>
2012-01-16 18:34:24 +0000

    net: kill duplicate included header
    For net part, remove duplicate included header.
    
    Signed-off-by: Shan Wei <davidshan@tencent.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/caif/caif_usb.c
+++ net/caif/caif_usb.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <linux/slab.h>
-#include <linux/netdevice.h>
 #include <linux/mii.h>
 #include <linux/usb.h>
 #include <linux/usb/usbnet.h>
