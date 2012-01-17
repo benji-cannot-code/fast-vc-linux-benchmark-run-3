2b54381e06ded12140d0d9140ed38ee9a4fc671d
Rob Herring <rob.herring@calxeda.com>
2012-01-17 13:10:25 -0600

    sh: intc: remove dependency on NR_IRQS
    SH intc has a compile time dependency on NR_IRQS. Make this dependency a
    local define so that shmobile (and ARM in general) can have run-time
    NR_IRQS setting.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>

--- include/linux/sh_intc.h
+++ include/linux/sh_intc.h
@@ -4,6 +4,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/ioport.h>
 
+#ifdef CONFIG_SUPERH
+#define INTC_NR_IRQS	512
+#else
+#define INTC_NR_IRQS	1024
+#endif
+
 /*
  * Convert back and forth between INTEVT and IRQ values.
  */
