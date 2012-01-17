e883630a9642849c529c10f9e5f865f09a990540
Sachin Kamat <sachin.kamat@linaro.org>
2012-01-17 04:52:41 -0300

    [media] s5p-mfc: Remove linux/version.h include from s5p_mfc.c
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/s5p-mfc/s5p_mfc.c
+++ drivers/media/video/s5p-mfc/s5p_mfc.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
-#include <linux/version.h>
 #include <linux/videodev2.h>
 #include <linux/workqueue.h>
 #include <media/videobuf2-core.h>
