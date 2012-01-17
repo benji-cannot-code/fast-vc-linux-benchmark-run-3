7bc83fb16f9016ce499e6c6788dad380aff3f344
Alex Yang <xiaojuny@qca.qualcomm.com>
2012-01-17 15:32:29 +0200

    ath6kl: add testmode 2 for 6003 ART
    Add testmode 2 for 6003 ART. When you insmod ath6kl_sdio.ko testmode=2, ath6kl
    will load ART firmware utf.bin and testscript nullTestFlow.bin. These files
    should be put in the firmware folder.
    
    kvalo: add "ath6kl:" to the title, word wrap the commit log and remove
    extra line in the code
    
    Signed-off-by: Alex Yang <xiaojuny@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/core.h
+++ drivers/net/wireless/ath/ath6kl/core.h
@@ -126,6 +126,8 @@ struct ath6kl_fw_ie {
 #define AR6003_HW_2_1_1_OTP_FILE		"otp.bin"
 #define AR6003_HW_2_1_1_FIRMWARE_FILE		"athwlan.bin"
 #define AR6003_HW_2_1_1_TCMD_FIRMWARE_FILE	"athtcmd_ram.bin"
+#define AR6003_HW_2_1_1_UTF_FIRMWARE_FILE	"utf.bin"
+#define AR6003_HW_2_1_1_TESTSCRIPT_FILE	"nullTestFlow.bin"
 #define AR6003_HW_2_1_1_PATCH_FILE		"data.patch.bin"
 #define AR6003_HW_2_1_1_BOARD_DATA_FILE "ath6k/AR6003/hw2.1.1/bdata.bin"
 #define AR6003_HW_2_1_1_DEFAULT_BOARD_DATA_FILE	\
@@ -593,6 +595,7 @@ struct ath6kl {
 		u32 board_addr;
 		u32 refclk_hz;
 		u32 uarttx_pin;
+		u32 testscript_addr;
 
 		struct ath6kl_hw_fw {
 			const char *dir;
@@ -600,6 +603,8 @@ struct ath6kl {
 			const char *fw;
 			const char *tcmd;
 			const char *patch;
+			const char *utf;
+			const char *testscript;
 		} fw;
 
 		const char *fw_board;
@@ -624,6 +629,9 @@ struct ath6kl {
 
 	u8 *fw_patch;
 	size_t fw_patch_len;
+
+	u8 *fw_testscript;
+	size_t fw_testscript_len;
 
 	unsigned int fw_api;
 	unsigned long fw_capabilities[ATH6KL_CAPABILITY_LEN];
