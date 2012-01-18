a6cb32d4788c49c69eca27c5c4c4d70f2d0dd384
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2012-01-18 16:06:17 +0200

    UBIFS: make the dbg_lock spinlock static
    Remove the usage of the 'dbg_lock' spinlock from 'dbg_err()' and make
    it static.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- fs/ubifs/debug.c
+++ fs/ubifs/debug.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_UBIFS_FS_DEBUG
 
-DEFINE_SPINLOCK(dbg_lock);
+static DEFINE_SPINLOCK(dbg_lock);
 
 static const char *get_key_fmt(int fmt)
 {
