8d74afe8532b5a8458b663d065fad37aec0a93ea
Peter Naulls <peter@chocky.org>
2012-01-17 18:27:09 -0800

    USB: serial: ftdi additional IDs
    I tested this against 2.6.39 in the Ubuntu kernel, however I see the IDs
    are not in latest 3.2 git.
    
    This adds IDs for the FTDI controller in the Rainforest Automation
    Zigbee dongle.
    
    Signed-off-by: Peter Naulls <peter@chocky.org>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -1176,3 +1176,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 /* TagTracer MIFARE*/
 #define FTDI_ZEITCONTROL_TAGTRACE_MIFARE_PID	0xF7C0
+
+/*
+ * Rainforest Automation
+ */
+/* ZigBee controller */
+#define FTDI_RF_R106		0x8A28
