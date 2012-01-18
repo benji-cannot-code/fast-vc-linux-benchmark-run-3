d5a01384e3fc7fa01194a56f6e482a3e06db793d
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-01-18 14:04:29 +0100

    target: accept REQUEST_SENSE with 18bytes
    WindowsXP+BOT issues a MODE_SENSE request with page 0x1c which is not
    suppoerted by target. Target rejects that command with
    TCM_INVALID_CDB_FIELD, so far so good. On BOT I can't send the SENSE
    response back, instead I can only reply that an error occured. The next
    thing happens is a REQUEST_SENSE request with 18 bytes length. Since the
    check here is more than 18 bytes I have to NACK that request as well.
    This is not really required: We check for some additional room, but we
    never use it. The additional length is set to 0xa so the total length is
    0xa + 8 = 18 which is fine with my 18 bytes.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/target/target_core_cdb.c
+++ drivers/target/target_core_cdb.c
@@ -1059,11 +1059,8 @@ int target_emulate_request_sense(struct se_task *task)
 		 */
 		buf[0] = 0x70;
 		buf[SPC_SENSE_KEY_OFFSET] = UNIT_ATTENTION;
-		/*
-		 * Make sure request data length is enough for additional
-		 * sense data.
-		 */
-		if (cmd->data_length <= 18) {
+
+		if (cmd->data_length < 18) {
 			buf[7] = 0x00;
 			err = -EINVAL;
 			goto end;
@@ -1080,11 +1077,8 @@ int target_emulate_request_sense(struct se_task *task)
 		 */
 		buf[0] = 0x70;
 		buf[SPC_SENSE_KEY_OFFSET] = NO_SENSE;
-		/*
-		 * Make sure request data length is enough for additional
-		 * sense data.
-		 */
-		if (cmd->data_length <= 18) {
+
+		if (cmd->data_length < 18) {
 			buf[7] = 0x00;
 			err = -EINVAL;
 			goto end;
