aaf555ee0bbea6862f52606d68cb4865aafa3dec
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-01-17 22:04:24 -0500

    NFS: Remove unnecessary includes from linux/nfs_fs_i.h
    Also from linux/nfs_xdr.h.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/pagelist.c
+++ fs/nfs/pagelist.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/file.h>
 #include <linux/sched.h>
 #include <linux/sunrpc/clnt.h>
+#include <linux/nfs.h>
 #include <linux/nfs3.h>
 #include <linux/nfs4.h>
 #include <linux/nfs_page.h>
