6215a47d91748b063aacf64ba93f11b9f65be844
Eric Anholt <eric@anholt.net>
2012-01-19 10:50:06 -0800

    drm/i915: Correct the bit number for the MI_FLUSH_ENABLE.
    Older specs claimed this was bit 11, but newer specs and the actual
    simulator code say it was bit 12.  Regardless, we don't use MI_FLUSH,
    or try to enable it any more.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Ben Widawsky <ben@bwidawsk.net>
    [danvet: Anyone trying to use this bit, please read all the relevant
    discussions, it's epic.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -392,7 +392,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MI_MODE		0x0209c
 # define VS_TIMER_DISPATCH				(1 << 6)
-# define MI_FLUSH_ENABLE				(1 << 11)
+# define MI_FLUSH_ENABLE				(1 << 12)
 
 #define GFX_MODE	0x02520
 #define GFX_MODE_GEN7	0x0229c
