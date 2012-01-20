581f819a18ab7840686d47fe742d1f385062c5fe
Paul Gortmaker <paul.gortmaker@windriver.com>
2012-01-20 18:46:49 -0500

    lib: fix implicit users of kernel.h for TAINT_WARN
    A pending header cleanup will cause this to show up as:
    
    lib/average.c:38: error: 'TAINT_WARN' undeclared (first use in this function)
    lib/list_debug.c:24: error: 'TAINT_WARN' undeclared (first use in this function)
    
    and TAINT_WARN comes from include/linux/kernel.h file.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- lib/average.c
+++ lib/average.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/average.h>
+#include <linux/kernel.h>
 #include <linux/bug.h>
 #include <linux/log2.h>
 
