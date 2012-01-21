8534ada0423d488749c93fd8501d0ee4d73607d1
Yinghai Lu <yinghai@kernel.org>
2012-01-21 02:08:26 -0800

    PCI: Move struct resource_list to setup-bus.c
    No user outside of setup-bus.c now.  Later patches will convert
    resource_list to a regular list.
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -28,6 +28,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include "pci.h"
 
+struct resource_list {
+	struct resource_list *next;
+	struct resource *res;
+	struct pci_dev *dev;
+};
+
 struct resource_list_x {
 	struct resource_list_x *next;
 	struct resource *res;
