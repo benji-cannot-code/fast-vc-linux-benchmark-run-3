cadd7c5809e1a66691866903b0a276771c5157a5
Sam Hansen <solid.se7en@gmail.com>
2012-01-22 00:48:25 -0800

    staging: xgifb: pr_fmt kbuild macro
    Added the kbuild macro pr_fmt() to XGI_main_26.c
    
    Signed-off-by: Sam Hansen <solid.se7en@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/XGI_main_26.c
+++ drivers/staging/xgifb/XGI_main_26.c
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Base on TW's sis fbdev code.
  */
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 /* #include <linux/config.h> */
 #include <linux/module.h>
 #include <linux/moduleparam.h>
