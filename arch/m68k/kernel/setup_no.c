abdc56dd514420a559b2412a8a2c7c79c75b4f95
Greg Ungerer <gerg@uclinux.org>
2012-01-23 13:25:56 +1000

    m68knommu: make persistent clock code consistent with m68k
    The read_persistent_clock() code is different on m68knommu, for really no
    reason. With a few changes to support function names and some code
    re-organization the code can be made the same.
    
    This will make it easier to merge the arch/m68k/kernel/time.c for m68k and
    m68knommu in a future patch.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/kernel/setup_no.c
+++ arch/m68k/kernel/setup_no.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/initrd.h>
 #include <linux/root_dev.h>
+#include <linux/rtc.h>
 
 #include <asm/setup.h>
 #include <asm/irq.h>
@@ -49,6 +50,7 @@ char __initdata command_line[COMMAND_LINE_SIZE];
 
 /* machine dependent timer functions */
 int (*mach_set_clock_mmss)(unsigned long);
+int (*mach_hwclk) (int, struct rtc_time*);
 
 /* machine dependent reboot functions */
 void (*mach_reset)(void);
