82b0d552573ad4c49e41b676fefccd582a77e4a3
Paul Mundt <lethal@linux-sh.org>
2012-01-24 14:07:18 +0900

    sh: intc: Make global intc controller counter static.
    No need to expose this globally since it's only used for core accounting.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/sh/intc/core.c
+++ drivers/sh/intc/core.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 LIST_HEAD(intc_list);
 DEFINE_RAW_SPINLOCK(intc_big_lock);
-unsigned int nr_intc_controllers;
+static unsigned int nr_intc_controllers;
 
 /*
  * Default priority level
