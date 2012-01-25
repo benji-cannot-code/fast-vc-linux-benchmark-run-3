555e8679df798b9ffa6e53a83587777f12f0dc5a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-01-25 15:19:20 +0000

    ASoC: wm5100: Make sure we switch to button reporting mode
    When we have identified an accessory make sure we've flagged that we've
    done so in order to make sure we always report buttons and don't continue
    to polarity flip.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm5100.c
+++ sound/soc/codecs/wm5100.c
@@ -2184,6 +2184,7 @@ static void wm5100_micd_irq(struct snd_soc_codec *codec)
 		if (wm5100->jack_detecting) {
 			dev_dbg(codec->dev, "Microphone detected\n");
 			wm5100->jack_mic = true;
+			wm5100->jack_detecting = false;
 			snd_soc_jack_report(wm5100->jack,
 					    SND_JACK_HEADSET,
 					    SND_JACK_HEADSET | SND_JACK_BTN_0);
@@ -2222,6 +2223,7 @@ static void wm5100_micd_irq(struct snd_soc_codec *codec)
 					    SND_JACK_BTN_0);
 		} else if (wm5100->jack_detecting) {
 			dev_dbg(codec->dev, "Headphone detected\n");
+			wm5100->jack_detecting = false;
 			snd_soc_jack_report(wm5100->jack, SND_JACK_HEADPHONE,
 					    SND_JACK_HEADPHONE);
 
