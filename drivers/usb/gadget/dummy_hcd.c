1f820a52880525729d2d2d46aa32ad4f5d15b05b
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-01-25 11:51:19 +0100

    usb: gadget: dummy_hcd: don't assign ->desc on error case
    If the stream check fails then we leave ep->desc assigend but we return
    with an error code. The caller assumes the endpoint is not enabled
    (which is the case) but it can not enable it again due to this
    assigment.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/dummy_hcd.c
+++ drivers/usb/gadget/dummy_hcd.c
@@ -515,7 +515,6 @@ static int dummy_enable(struct usb_ep *_ep,
 	}
 
 	_ep->maxpacket = max;
-	ep->desc = desc;
 	if (usb_ss_max_streams(_ep->comp_desc)) {
 		if (!usb_endpoint_xfer_bulk(desc)) {
 			dev_err(udc_dev(dum), "Can't enable stream support on "
@@ -524,6 +523,7 @@ static int dummy_enable(struct usb_ep *_ep,
 		}
 		ep->stream_en = 1;
 	}
+	ep->desc = desc;
 
 	dev_dbg(udc_dev(dum), "enabled %s (ep%d%s-%s) maxpacket %d stream %s\n",
 		_ep->name,
