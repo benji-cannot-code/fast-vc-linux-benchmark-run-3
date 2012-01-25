fb02cfe468204b27226230073b47853e98176363
Pawel Moll <pawel.moll@arm.com>
2012-01-25 15:37:29 +0000

    ARM: vexpress: Get rid of MMIO_P2V
    This patch gets rid of the MMIO_P2V and __MMIO_P2V macros,
    defining constant virtual base for motherboard and tile
    peripherals instead.
    
    Additionally, in preparation for the new motherboard memory
    map, the motherboard peripherals are using base pointers
    calculated in runtime, instead of compile-time calculated
    values.
    
    Signed-off-by: Pawel Moll <pawel.moll@arm.com>

--- arch/arm/mach-vexpress/platsmp.c
+++ arch/arm/mach-vexpress/platsmp.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 
 #include <mach/motherboard.h>
-#define V2M_PA_CS7 0x10000000
 
 #include "core.h"
 
@@ -44,7 +43,5 @@ void __init platform_smp_prepare_cpus(unsigned int max_cpus)
 	 * until it receives a soft interrupt, and then the
 	 * secondary CPU branches to this address.
 	 */
-	writel(~0, MMIO_P2V(V2M_SYS_FLAGSCLR));
-	writel(virt_to_phys(versatile_secondary_startup),
-		MMIO_P2V(V2M_SYS_FLAGSSET));
+	v2m_flags_set(virt_to_phys(versatile_secondary_startup));
 }
