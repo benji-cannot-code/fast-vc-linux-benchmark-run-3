e9a2e8fad74144b6e26fd029c5e92b33c699a5fa
Joerg Roedel <joerg.roedel@amd.com>
2012-01-26 19:40:52 +0100

    iommu: Add domain-attribute handlers
    This patch introduces an extension to the iommu-api to get
    and set attributes for an iommu_domain. Two functions are
    introduced for this:
    
    	* iommu_domain_get_attr()
    	* iommu_domain_set_attr()
    
    These functions will be used to make the iommu-api suitable
    for GART-like IOMMUs and to implement hardware-specifc
    api-extensions.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -48,6 +48,10 @@ struct iommu_domain {
 #define IOMMU_CAP_CACHE_COHERENCY	0x1
 #define IOMMU_CAP_INTR_REMAP		0x2	/* isolates device intrs */
 
+enum iommu_attr {
+	DOMAIN_ATTR_MAX,
+};
+
 #ifdef CONFIG_IOMMU_API
 
 /**
@@ -60,7 +64,8 @@ struct iommu_domain {
  * @unmap: unmap a physically contiguous memory region from an iommu domain
  * @iova_to_phys: translate iova to physical address
  * @domain_has_cap: domain capabilities query
- * @commit: commit iommu domain
+ * @domain_get_attr: Query domain attributes
+ * @domain_set_attr: Change domain attributes
  * @pgsize_bitmap: bitmap of supported page sizes
  */
 struct iommu_ops {
@@ -77,6 +82,10 @@ struct iommu_ops {
 	int (*domain_has_cap)(struct iommu_domain *domain,
 			      unsigned long cap);
 	int (*device_group)(struct device *dev, unsigned int *groupid);
+	int (*domain_get_attr)(struct iommu_domain *domain,
+			       enum iommu_attr attr, void *data);
+	int (*domain_set_attr)(struct iommu_domain *domain,
+			       enum iommu_attr attr, void *data);
 	unsigned long pgsize_bitmap;
 };
 
@@ -99,6 +108,11 @@ extern int iommu_domain_has_cap(struct iommu_domain *domain,
 extern void iommu_set_fault_handler(struct iommu_domain *domain,
 			iommu_fault_handler_t handler, void *token);
 extern int iommu_device_group(struct device *dev, unsigned int *groupid);
+
+extern int iommu_domain_get_attr(struct iommu_domain *domain, enum iommu_attr,
+				 void *data);
+extern int iommu_domain_set_attr(struct iommu_domain *domain, enum iommu_attr,
+				 void *data);
 
 /**
  * report_iommu_fault() - report about an IOMMU fault to the IOMMU framework
@@ -201,6 +215,18 @@ static inline void iommu_set_fault_handler(struct iommu_domain *domain,
 static inline int iommu_device_group(struct device *dev, unsigned int *groupid)
 {
 	return -ENODEV;
+}
+
+static inline int iommu_domain_get_attr(struct iommu_domain *domain,
+					enum iommu_attr attr, void *data)
+{
+	return -EINVAL;
+}
+
+static inline int iommu_domain_set_attr(struct iommu_domain *domain,
+					enum iommu_attr attr, void *data)
+{
+	return -EINVAL;
 }
 
 #endif /* CONFIG_IOMMU_API */
