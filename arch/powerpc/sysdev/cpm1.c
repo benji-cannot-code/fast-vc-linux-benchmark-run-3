e2f0ece0c1fef25fe23ca8cb204c548b45c159f5
Grant Likely <grant.likely@secretlab.ca>
2012-01-26 12:24:34 -0700

    irq_domain/powerpc: constify irq_domain_ops
    Make all the irq_domain_ops structures in powerpc 'static const'
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Milton Miller <miltonm@bga.com>
    Tested-by: Olof Johansson <olof@lixom.net>

--- arch/powerpc/sysdev/cpm1.c
+++ arch/powerpc/sysdev/cpm1.c
@@ -124,7 +124,7 @@ static struct irqaction cpm_error_irqaction = {
 	.name = "error",
 };
 
-static struct irq_domain_ops cpm_pic_host_ops = {
+static const struct irq_domain_ops cpm_pic_host_ops = {
 	.map = cpm_pic_host_map,
 };
 
