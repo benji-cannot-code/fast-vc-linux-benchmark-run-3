a687251318eb15ff2e2e2e872986292153d8fc9e
Russell King <rmk+kernel@arm.linux.org.uk>
2012-01-26 10:37:46 +0000

    USB: sa1111: sparse and checkpatch cleanups
    Clean up the ohci-sa1111 driver formatting to be more compliant with
    current standards, and add 'static' to various function definitions
    to avoid sparse complaints about undeclared functions.  Remove the
    unnecessary local declaration of 'usb_disabled', which can be found
    instead in linux/usb.h.
    
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/usb/host/ohci-sa1111.c
+++ drivers/usb/host/ohci-sa1111.c
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_STATUS_NHCIMFCLR      (1 << 10)
 #define USB_STATUS_USBPWRSENSE    (1 << 11)
 
-extern int usb_disabled(void);
-
 /*-------------------------------------------------------------------------*/
 
 static int sa1111_start_hc(struct sa1111_dev *dev)
@@ -83,6 +81,7 @@ static int sa1111_start_hc(struct sa1111_dev *dev)
 static void sa1111_stop_hc(struct sa1111_dev *dev)
 {
 	unsigned int usb_rst;
+
 	printk(KERN_DEBUG "%s: stopping SA-1111 OHCI USB Controller\n",
 	       __FILE__);
 
@@ -107,7 +106,7 @@ static void dump_hci_status(struct usb_hcd *hcd, const char *label)
 {
 	unsigned long status = sa1111_readl(hcd->regs + USB_STATUS);
 
-	dbg ("%s USB_STATUS = { %s%s%s%s%s}", label,
+	dbg("%s USB_STATUS = { %s%s%s%s%s}", label,
 	     ((status & USB_STATUS_IRQHCIRMTWKUP) ? "IRQHCIRMTWKUP " : ""),
 	     ((status & USB_STATUS_IRQHCIBUFFACC) ? "IRQHCIBUFFACC " : ""),
 	     ((status & USB_STATUS_NIRQHCIM) ? "" : "IRQHCIM "),
@@ -132,15 +131,16 @@ static void dump_hci_status(struct usb_hcd *hcd, const char *label)
  *
  * Store this function in the HCD's struct pci_driver as probe().
  */
-int usb_hcd_sa1111_probe (const struct hc_driver *driver,
-			  struct sa1111_dev *dev)
+static int usb_hcd_sa1111_probe(const struct hc_driver *driver,
+	struct sa1111_dev *dev)
 {
 	struct usb_hcd *hcd;
 	int retval;
 
-	hcd = usb_create_hcd (driver, &dev->dev, "sa1111");
+	hcd = usb_create_hcd(driver, &dev->dev, "sa1111");
 	if (!hcd)
 		return -ENOMEM;
+
 	hcd->rsrc_start = dev->res.start;
 	hcd->rsrc_len = resource_size(&dev->res);
 
@@ -149,6 +149,7 @@ int usb_hcd_sa1111_probe (const struct hc_driver *driver,
 		retval = -EBUSY;
 		goto err1;
 	}
+
 	hcd->regs = dev->mapbase;
 
 	ret = sa1111_start_hc(dev);
@@ -181,9 +182,8 @@ int usb_hcd_sa1111_probe (const struct hc_driver *driver,
  * Reverses the effect of usb_hcd_sa1111_probe(), first invoking
  * the HCD's stop() method.  It is always called from a thread
  * context, normally "rmmod", "apmd", or something similar.
- *
  */
-void usb_hcd_sa1111_remove (struct usb_hcd *hcd, struct sa1111_dev *dev)
+static void usb_hcd_sa1111_remove(struct usb_hcd *hcd, struct sa1111_dev *dev)
 {
 	usb_remove_hcd(hcd);
 	sa1111_stop_hc(dev);
@@ -193,18 +193,19 @@ void usb_hcd_sa1111_remove (struct usb_hcd *hcd, struct sa1111_dev *dev)
 
 /*-------------------------------------------------------------------------*/
 
-static int __devinit
-ohci_sa1111_start (struct usb_hcd *hcd)
+static int __devinit ohci_sa1111_start(struct usb_hcd *hcd)
 {
-	struct ohci_hcd	*ohci = hcd_to_ohci (hcd);
-	int		ret;
+	struct ohci_hcd	*ohci = hcd_to_ohci(hcd);
+	int ret;
 
-	if ((ret = ohci_init(ohci)) < 0)
+	ret = ohci_init(ohci);
+	if (ret < 0)
 		return ret;
 
-	if ((ret = ohci_run (ohci)) < 0) {
-		err ("can't start %s", hcd->self.bus_name);
-		ohci_stop (hcd);
+	ret = ohci_run(ohci);
+	if (ret < 0) {
+		err("can't start %s", hcd->self.bus_name);
+		ohci_stop(hcd);
 		return ret;
 	}
 	return 0;
