5b0c7d39950c8eec36cb014f97161b1e11c2b001
Marek Olšák <maraeo@gmail.com>
2012-01-27 12:17:59 -0500

    drm/radeon/kms: add support for streamout v7
    v2: agd5f: add strmout CS checking, copy_dw register checking
    
    v3: agd5f: don't use cs_check_reg() for copy_dw checking as it
    will incorrectly patch the command stream for certain regs.
    
    v4: agd5f: add warning if safe reg check fails for copy_dw
    
    v5: agd5f: add stricter checking for 6xx/7xx
    
    v6: agd5f: add range checking for copy_dw on eg+,
    add sx_surface_sync to safe reg list for 7xx.
    
    v7: agd5f: add stricter checking for eg+
    
    Signed-off-by: Marek Olšák <maraeo@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/radeon/radeon_drv.c
+++ drivers/gpu/drm/radeon/radeon_drv.c
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   2.10.0 - fusion 2D tiling
  *   2.11.0 - backend map, initial compute support for the CS checker
  *   2.12.0 - RADEON_CS_KEEP_TILING_FLAGS
- *   2.13.0 - virtual memory support
+ *   2.13.0 - virtual memory support, streamout
  */
 #define KMS_DRIVER_MAJOR	2
 #define KMS_DRIVER_MINOR	13
