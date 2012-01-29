5dabd9ad0f20935d2e69863943a4f7a23631c426
Axel Lin <axel.lin@gmail.com>
2012-01-29 17:52:37 +0800

    regulator: Add devm_regulator_put in consumer.h
    Commit d5ad34f7cb "regulator: Implement devm_regulator_free()"
    actually implements devm_regulator_put.
    Thus rename devm_regulator_free to devm_regulator_put.
    
    Also add empty devm_regulator_put for !CONFIG_REGULATOR
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -138,7 +138,7 @@ struct regulator *__must_check devm_regulator_get(struct device *dev,
 struct regulator *__must_check regulator_get_exclusive(struct device *dev,
 						       const char *id);
 void regulator_put(struct regulator *regulator);
-void devm_regulator_free(struct regulator *regulator);
+void devm_regulator_put(struct regulator *regulator);
 
 /* regulator output control and status */
 int regulator_enable(struct regulator *regulator);
@@ -214,6 +214,10 @@ devm_regulator_get(struct device *dev, const char *id)
 }
 
 static inline void regulator_put(struct regulator *regulator)
+{
+}
+
+static inline void devm_regulator_put(struct regulator *regulator)
 {
 }
 
