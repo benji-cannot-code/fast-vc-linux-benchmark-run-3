f7bb4516e5e7c90ed86f7c578cb138ca5fbabca6
Johannes Berg <johannes.berg@intel.com>
2012-01-29 18:36:01 -0800

    iwlwifi: release IRQ in error path
    smatch correctly complains:
    iwl-trans-pcie.c +1528 iwl_trans_pcie_start_hw(50) warn: 'trans->irq' was not released on error
    
    Fix it.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi W Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-trans-pcie.c
+++ drivers/net/wireless/iwlwifi/iwl-trans-pcie.c
@@ -1505,7 +1505,7 @@ static int iwl_trans_pcie_start_hw(struct iwl_trans *trans)
 	err = iwl_prepare_card_hw(trans);
 	if (err) {
 		IWL_ERR(trans, "Error while preparing HW: %d", err);
-		goto error;
+		goto err_free_irq;
 	}
 
 	iwl_apm_init(trans);
@@ -1523,6 +1523,8 @@ static int iwl_trans_pcie_start_hw(struct iwl_trans *trans)
 
 	return err;
 
+err_free_irq:
+	free_irq(trans->irq, trans);
 error:
 	iwl_free_isr_ict(trans);
 	tasklet_kill(&trans_pcie->irq_tasklet);
