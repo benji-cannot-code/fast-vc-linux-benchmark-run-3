549c43d2ff4758a460f0aaa54f5c0163071ab86e
Ben Hutchings <bhutchings@solarflare.com>
2012-01-30 16:53:37 +0000

    sfc: MTD: Leave the DEBUG macro alone
    <linux/mtd/mtd.h> no longer defines DEBUG so we do not need to
    un-define it here.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>

--- drivers/net/ethernet/sfc/mtd.c
+++ drivers/net/ethernet/sfc/mtd.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/bitops.h>
 #include <linux/module.h>
-#undef DEBUG /* <linux/mtd/mtd.h> has its own use for DEBUG */
 #include <linux/mtd/mtd.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
