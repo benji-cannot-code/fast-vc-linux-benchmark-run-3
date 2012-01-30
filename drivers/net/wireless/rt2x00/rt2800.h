83d3475cebd4f773d152191fcb4989a9e1c86004
Stanislaw Gruszka <sgruszka@redhat.com>
2012-01-30 16:17:59 +0100

    rt2800: radio 3xxxx: channel switch RX/TX calibration fixes
    Synchronize code with Ralink driver:
    2011_0719_RT3070_RT3370_RT5370_RT5372_Linux_STA_V2.5.0.3_DPO
    Based on functions:
    RT30xx_ChipSwitchChannel
    RT33xx_ChipSwitchChannel
    NICInitRT3370RFRegisters
    and defines from:
    include/chip/rt33xx.h
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Acked-by: Gertjan van Wingerde <gwingerde@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2800.h
+++ drivers/net/wireless/rt2x00/rt2800.h
@@ -1873,6 +1873,13 @@ struct mac_iveiv_entry {
 #define RFCSR23_FREQ_OFFSET		FIELD8(0x7f)
 
 /*
+ * RFCSR 24:
+ */
+#define RFCSR24_TX_AGC_FC		FIELD8(0x1f)
+#define RFCSR24_TX_H20M			FIELD8(0x20)
+#define RFCSR24_TX_CALIB		FIELD8(0x7f)
+
+/*
  * RFCSR 27:
  */
 #define RFCSR27_R1			FIELD8(0x03)
@@ -1893,6 +1900,7 @@ struct mac_iveiv_entry {
  */
 #define RFCSR31_RX_AGC_FC		FIELD8(0x1f)
 #define RFCSR31_RX_H20M			FIELD8(0x20)
+#define RFCSR31_RX_CALIB		FIELD8(0x7f)
 
 /*
  * RFCSR 38:
