68d4e626485b5894e98b56eb411a44ef0c2d2c73
Jan Beulich <JBeulich@suse.com>
2012-02-03 15:09:04 +0000

    xen/tmem: cleanup
    Use 'bool' for boolean variables. Do proper section placement.
    Eliminate an unnecessary export.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/tmem.h
+++ include/xen/tmem.h
@@ -1,6 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _XEN_TMEM_H
 #define _XEN_TMEM_H
+
+#include <linux/types.h>
+
 /* defined in drivers/xen/tmem.c */
-extern int tmem_enabled;
+extern bool tmem_enabled;
+
 #endif /* _XEN_TMEM_H */
