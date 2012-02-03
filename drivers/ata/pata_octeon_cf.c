98a41f62fc12bf26d91e91a2decdadddee2dde30
David Daney <david.daney@cavium.com>
2012-02-03 09:36:57 -0800

    ata: pata_octeon_cf: Use correct byte order for DMA in when built little-endian.
    We need to set the 'endian' bit in this case.
    
    Acked-by: Jeff Garzik <jgarzik@redhat.com>
    Signed-off-by: David Daney <david.daney@cavium.com>

--- drivers/ata/pata_octeon_cf.c
+++ drivers/ata/pata_octeon_cf.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <scsi/scsi_host.h>
 
+#include <asm/byteorder.h>
 #include <asm/octeon/octeon.h>
 
 /*
@@ -590,6 +591,9 @@ static void octeon_cf_dma_start(struct ata_queued_cmd *qc)
 
 	/* Set the direction of the DMA */
 	mio_boot_dma_cfg.u64 = 0;
+#ifdef __LITTLE_ENDIAN
+	mio_boot_dma_cfg.s.endian = 1;
+#endif
 	mio_boot_dma_cfg.s.en = 1;
 	mio_boot_dma_cfg.s.rw = ((qc->tf.flags & ATA_TFLAG_WRITE) != 0);
 
