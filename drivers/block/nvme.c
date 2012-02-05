54a554692dbb17c69a68faa1fe5ff39bb5eb762a
Jesper Juhl <jj@chaosbits.net>
2012-02-05 20:11:27 +0100

    NVM Express: Remove unneeded include of linux/version.h from nvme.c
    There's no need for drivers/block/nvme.c to include linux/version.h,
    so remove the include.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/block/nvme.c
+++ drivers/block/nvme.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/types.h>
-#include <linux/version.h>
 
 #define NVME_Q_DEPTH 1024
 #define SQ_SIZE(depth)		(depth * sizeof(struct nvme_command))
