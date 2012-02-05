c9f38a70009e6317c97ea95f78e7a8c2bdcddea6
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-02-05 20:25:32 +0800

    ARM: at91: factorise duplicated at91sam9 idle
    Remove duplicated at91sam9xxxx_idle() functions introduced
    by commit c9dfafb "ARM: mach-at91: move special idle code out of line".
    Replace by a generic at91sam9_idle() function in setup.c common
    location.
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/clock.c
+++ arch/arm/mach-at91/clock.c
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/at91_pmc.h>
 #include <mach/cpu.h>
 
+#include <asm/proc-fns.h>
+
 #include "clock.h"
 #include "generic.h"
 
@@ -819,3 +821,9 @@ static int __init at91_clock_reset(void)
 	return 0;
 }
 late_initcall(at91_clock_reset);
+
+void at91sam9_idle(void)
+{
+	at91_sys_write(AT91_PMC_SCDR, AT91_PMC_PCK);
+	cpu_do_idle();
+}
