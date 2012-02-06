0e8c08e786295a7d498a16b77b6ca0d6e0c8ffcf
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2012-02-07 00:39:36 +0100

    staging: tidspbridge: remove dbc.h
    All the macros in dbc.h are removed now so this patch removes it completely.
    
    Signed-off-by: Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/tidspbridge/core/io_sm.c
+++ drivers/staging/tidspbridge/core/io_sm.c
@@ -34,9 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  ----------------------------------- DSP/BIOS Bridge */
 #include <dspbridge/dbdefs.h>
 
-/* Trace & Debug */
-#include <dspbridge/dbc.h>
-
 /* Services Layer */
 #include <dspbridge/ntfy.h>
 #include <dspbridge/sync.h>
--- drivers/staging/tidspbridge/rmgr/rmm.c
+++ drivers/staging/tidspbridge/rmgr/rmm.c
@@ -47,9 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  ----------------------------------- DSP/BIOS Bridge */
 #include <dspbridge/dbdefs.h>
 
-/*  ----------------------------------- Trace & Debug */
-#include <dspbridge/dbc.h>
-
 /*  ----------------------------------- This */
 #include <dspbridge/rmm.h>
 
