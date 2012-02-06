9d2084e7346a9cb58a264cb3a8a0774cb6c97037
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-02-06 18:50:39 +0000

    ASoC: wm8994: Bring WM8994 accessory detection up to date
    Make the mechanism used for WM8994 more like that for WM1811 and WM8958:
    provide the logic to distinguish between headphone and headset and hard
    code the reporting of sensible SND_JACK values. Should integration with
    other detection mechanisms be required we can add appropriate callbacks
    (though some integrations should be able to use the subsystem ones).
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8994.h
+++ sound/soc/codecs/wm8994.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 typedef void (*wm8958_micdet_cb)(u16 status, void *data);
 
 int wm8994_mic_detect(struct snd_soc_codec *codec, struct snd_soc_jack *jack,
-		      int micbias, int det, int shrt);
+		      int micbias);
 int wm8958_mic_detect(struct snd_soc_codec *codec, struct snd_soc_jack *jack,
 		      wm8958_micdet_cb cb, void *cb_data);
 
@@ -47,8 +47,7 @@ void wm8958_dsp2_init(struct snd_soc_codec *codec);
 
 struct wm8994_micdet {
 	struct snd_soc_jack *jack;
-	int det;
-	int shrt;
+	bool detecting;
 };
 
 /* codec private data */
