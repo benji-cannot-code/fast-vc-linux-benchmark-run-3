6a28fbbf42bd6700987850d9e31747129d0b9adc
Michael Krufky <mkrufky@kernellabs.com>
2012-02-07 02:39:36 -0300

    [media] xc5000: remove static dependencies on xc5000 created by previous changesets
    convert the firmware configuration attach-time parameter from
    a pointer to an integer so as to remove the static dependency
    created by the previous changesets.
    
    Signed-off-by: Michael Krufky <mkrufky@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/common/tuners/xc5000.h
+++ drivers/media/common/tuners/xc5000.h
@@ -28,23 +28,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct dvb_frontend;
 struct i2c_adapter;
 
-struct xc5000_fw_cfg {
-	char *name;
-	u16 size;
-};
+#define XC5000_FW_A_1_6_114            1
+#define XC5000_FW_C_41_024_5_31875     2
 
-extern struct xc5000_fw_cfg xc5000a_1_6_114;
-extern struct xc5000_fw_cfg xc5000c_41_024_5_31875;
-
-#define XC5000_DEFAULT_FIRMWARE  &xc5000a_1_6_114
-#define XC5000C_DEFAULT_FIRMWARE &xc5000c_41_024_5_31875
+#define XC5000_DEFAULT_FIRMWARE  XC5000_FW_A_1_6_114
+#define XC5000C_DEFAULT_FIRMWARE XC5000_FW_C_41_024_5_31875
 
 struct xc5000_config {
 	u8   i2c_address;
 	u32  if_khz;
 	u8   radio_input;
 
-	struct xc5000_fw_cfg *fw;
+	int fw;
 };
 
 /* xc5000 callback command */
