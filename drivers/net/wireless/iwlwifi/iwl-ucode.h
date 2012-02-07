e2cc719d6285c875f388c2d6d824b4ee63f8b826
Don Fry <donald.h.fry@intel.com>
2012-02-07 15:00:12 -0800

    iwlwifi: Move ucode pointers to iwl_fw
    The ucode image is a ucode related thing not a transport one.  Move them.
    
    Signed-off-by: Don Fry <donald.h.fry@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-ucode.h
+++ drivers/net/wireless/iwlwifi/iwl-ucode.h
@@ -64,8 +64,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_ucode_h__
 #define __iwl_ucode_h__
 
-#include "iwl-trans.h"
-
 #include <linux/netdevice.h>
 
 /* v1/v2 uCode file layout */
@@ -180,6 +178,18 @@ struct iwl_ucode_capabilities {
 	u32 max_probe_length;
 	u32 standard_phy_calibration_size;
 	u32 flags;
+};
+
+/* one for each uCode image (inst/data, boot/init/runtime) */
+struct fw_desc {
+	dma_addr_t p_addr;	/* hardware address */
+	void *v_addr;		/* software address */
+	u32 len;		/* size in bytes */
+};
+
+struct fw_img {
+	struct fw_desc code;	/* firmware code image */
+	struct fw_desc data;	/* firmware data image */
 };
 
 /**
