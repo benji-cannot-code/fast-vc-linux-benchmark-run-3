c42934d919ff786f03345cd3bddcb4d9dd089e8e
John Stultz <john.stultz@linaro.org>
2012-02-09 14:24:33 -0800

    staging: android-alarm: Fix include compile issues
    The file asm/mach/time.h doesn't exist on all arches,
    so include <linux/time.h>. Also linux/sysdev.h is gone
    so kill it.
    
    CC: Arve Hjønnevåg <arve@android.com>
    CC: Android Kernel Team <kernel-team@android.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/alarm.c
+++ drivers/staging/android/alarm.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#include <asm/mach/time.h>
+#include <linux/time.h>
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/miscdevice.h>
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rtc.h>
 #include <linux/sched.h>
 #include <linux/spinlock.h>
-#include <linux/sysdev.h>
 #include <linux/wakelock.h>
 #include "android_alarm.h"
 
