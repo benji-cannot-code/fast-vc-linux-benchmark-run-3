e187c3e8bd3449e9b75588d24930c3d86ded1cd2
Peter Huewe <peterhuewe@gmx.de>
2012-02-09 21:11:48 +0100

    staging/xgifb: Use TVCLKBASE_315 as a base address
    Since the defines TVVCLKDIV2, TVVCLK, HiTVVCLKDIV2, HiTVVCLK,
    HiTVSimuVCLK and HiTVTextVCLK are now defined as relative values, we
    have to use TVCLKBASE_315 (0x31) as a base address to get the same values
    as before the merge.
    
    The old and now duplicated defines were removed
    
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/vb_def.h
+++ drivers/staging/xgifb/vb_def.h
@@ -289,12 +289,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VCLK217_325          0x55
 #define XGI_YPbPr750pVCLK    0x57
 
-#define TVVCLKDIV2           0x3A
-#define TVVCLK               0x3B
-#define HiTVVCLKDIV2         0x3C
-#define HiTVVCLK             0x3D
-#define HiTVSimuVCLK         0x3E
-#define HiTVTextVCLK         0x3F
 #define VCLK39_77            0x40
 #define YPbPr525pVCLK           0x3A
 #define NTSC1024VCLK         0x41
