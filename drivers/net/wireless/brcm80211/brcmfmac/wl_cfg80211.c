1d17cb62ff447c569d52ec07ccdc031245f9bf9a
Arend van Spriel <arend@broadcom.com>
2012-02-09 21:09:06 +0100

    brcm80211: fmac: change allocation flag in brcmf_enq_event() function
    As the function is called from atomic context it should not do the
    kzalloc call with GFP_KERNEL.
    
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Kan Yan <kanyan@broadcom.com>
    Reviewed-by: Franky (Zhenhui) Lin <frankyl@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/wl_cfg80211.c
+++ drivers/net/wireless/brcm80211/brcmfmac/wl_cfg80211.c
@@ -3298,7 +3298,9 @@ static struct brcmf_cfg80211_event_q *brcmf_deq_event(
 }
 
 /*
-** push event to tail of the queue
+*	push event to tail of the queue
+*
+*	remark: this function may not sleep as it is called in atomic context.
 */
 
 static s32
@@ -3308,7 +3310,7 @@ brcmf_enq_event(struct brcmf_cfg80211_priv *cfg_priv, u32 event,
 	struct brcmf_cfg80211_event_q *e;
 	s32 err = 0;
 
-	e = kzalloc(sizeof(struct brcmf_cfg80211_event_q), GFP_KERNEL);
+	e = kzalloc(sizeof(struct brcmf_cfg80211_event_q), GFP_ATOMIC);
 	if (!e)
 		return -ENOMEM;
 
