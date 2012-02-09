d54e1d9f08d2c00354ff5d4d7338dba2199694a5
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-02-08 22:01:15 -0500

    SUNRPC: Ensure that we can trace waitqueues when !defined(CONFIG_SYSCTL)
    The tracepoint code relies on the queue->name being defined in order to
    be able to display the name of the waitqueue on which an RPC task is
    sleeping.
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Reported-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Acked-by: Randy Dunlap <rdunlap@xenotime.net>

--- include/linux/sunrpc/debug.h
+++ include/linux/sunrpc/debug.h
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_SYSCTL
 #define  RPC_DEBUG
 #endif
+#ifdef CONFIG_TRACEPOINTS
+#define RPC_TRACEPOINTS
+#endif
 /* #define  RPC_PROFILE */
 
 /*
