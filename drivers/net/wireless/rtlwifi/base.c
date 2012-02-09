1888d19652102e4913bad1f5cb8283e0a4d694e8
Tristan Pourcelot <tristan.pourcelot@gmail.com>
2012-02-09 14:48:09 +0100

    Correcting typos in rtlwifi/base.c
    This patch correct some typos in a comment.
    
    Signed-off-by: Tristan Pourcelot <tristan.pourcelot@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/base.c
+++ drivers/net/wireless/rtlwifi/base.c
@@ -40,10 +40,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 
 /*
- *NOTICE!!!: This file will be very big, we hsould
- *keep it clear under follwing roles:
+ *NOTICE!!!: This file will be very big, we should
+ *keep it clear under following roles:
  *
- *This file include follwing part, so, if you add new
+ *This file include following parts, so, if you add new
  *functions into this file, please check which part it
  *should includes. or check if you should add new part
  *for this file:
