475927dd128ce8b5a5c1b63a562b13fb104a6a43
Peter De Schrijver <pdeschrijver@nvidia.com>
2012-02-10 01:47:44 +0200

    ARM: tegra: functions to access the flowcontroller
    Introduce some functions to write to the flowcontroller registers. The
    flowcontroller controls CPU sleepstates and wakeup.
    
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Tested-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-tegra/flowctrl.h
+++ arch/arm/mach-tegra/flowctrl.h
@@ -35,4 +35,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FLOW_CTRL_HALT_CPU1_EVENTS	0x14
 #define FLOW_CTRL_CPU1_CSR		0x18
 
+#ifndef __ASSEMBLY__
+void flowctrl_write_cpu_csr(unsigned int cpuid, u32 value);
+void flowctrl_write_cpu_halt(unsigned int cpuid, u32 value);
+#endif
+
 #endif
