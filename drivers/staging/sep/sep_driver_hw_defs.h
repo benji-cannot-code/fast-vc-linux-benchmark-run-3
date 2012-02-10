2071b41fef55fedff227d10d7b0743ecc7f43eff
Mark A. Allyn <mark.a.allyn@intel.com>
2012-02-10 13:52:27 +0000

    staging: sep: Add new PCI identifier
    [This is picked out of the differences between the upstream driver and
     the staging driver. I'm resolving the differences as a series of updates -AC]
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sep/sep_driver_hw_defs.h
+++ drivers/staging/sep/sep_driver_hw_defs.h
@@ -34,9 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef SEP_DRIVER_HW_DEFS__H
 #define SEP_DRIVER_HW_DEFS__H
 
-/* PCI ID's */
-#define MFLD_PCI_DEVICE_ID 0x0826
-
 /*----------------------- */
 /* HW Registers Defines.  */
 /*                        */
