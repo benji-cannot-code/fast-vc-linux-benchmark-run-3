437c17cf0b2c6c81d23e5d4a60b9dd0baf5a0887
John Fastabend <john.r.fastabend@intel.com>
2012-02-10 03:19:41 +0000

    net: dcb: getnumtcs()/setnumtcs() should return an int
    {g|s}etnumtcs() today returns a u8 that is only used by the DCB code
    to verify no error occurred. Today the driver implementations return
    negative error codes which end up being non-zero so the logic works
    out but triggers some sparse warnings.
    
    To fix the sparse warnings convert the return value to an int.
    
    CC: Eilon Greenstein <eilong@broadcom.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: John Fastabend <john.r.fastabend@intel.com>
    Tested-by: Ross Brattain <ross.b.brattain@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_dcb_nl.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_dcb_nl.c
@@ -491,7 +491,7 @@ static u8 ixgbe_dcbnl_getcap(struct net_device *netdev, int capid, u8 *cap)
 	return 0;
 }
 
-static u8 ixgbe_dcbnl_getnumtcs(struct net_device *netdev, int tcid, u8 *num)
+static int ixgbe_dcbnl_getnumtcs(struct net_device *netdev, int tcid, u8 *num)
 {
 	struct ixgbe_adapter *adapter = netdev_priv(netdev);
 	u8 rval = 0;
@@ -515,7 +515,7 @@ static u8 ixgbe_dcbnl_getnumtcs(struct net_device *netdev, int tcid, u8 *num)
 	return rval;
 }
 
-static u8 ixgbe_dcbnl_setnumtcs(struct net_device *netdev, int tcid, u8 num)
+static int ixgbe_dcbnl_setnumtcs(struct net_device *netdev, int tcid, u8 num)
 {
 	return -EINVAL;
 }
