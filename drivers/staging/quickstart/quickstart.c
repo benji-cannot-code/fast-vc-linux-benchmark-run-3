00c3bc0faea279415bac26e2fe7b749dbcb5b2c8
Szymon Janc <szymon@janc.net.pl>
2012-02-10 21:05:44 +0100

    Staging: quickstart: Bump driver version to 1.04
    Signed-off-by: Szymon Janc <szymon@janc.net.pl>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/quickstart/quickstart.c
+++ drivers/staging/quickstart/quickstart.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#define QUICKSTART_VERSION "1.03"
+#define QUICKSTART_VERSION "1.04"
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
