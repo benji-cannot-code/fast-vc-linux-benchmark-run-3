4d43260c148e34903da18e0e893ba9730f11dd58
Gerard Cauvy <g-cauvy1@ti.com>
2012-02-10 12:21:18 +0200

    usb: dwc3: ep0: fix SetFeature(TEST)
    When host requests us to enter a test mode,
    we cannot directly enter the test mode before
    Status Phase is completed, otherwise the core
    will never be able to deliver the Status ZLP
    to host, because it has already entered the
    requested Test Mode.
    
    In order to fix the error, we move the actual
    start of Test Mode right after we receive
    Transfer Complete event of the status phase.
    
    Signed-off-by: Gerard Cauvy <g-cauvy1@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -685,6 +685,9 @@ struct dwc3 {
 
 	struct dwc3_hwparams	hwparams;
 	struct dentry		*root;
+
+	u8			test_mode;
+	u8			test_mode_nr;
 };
 
 /* -------------------------------------------------------------------------- */
