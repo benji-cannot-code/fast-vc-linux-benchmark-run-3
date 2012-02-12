fae602f1459b92ae074ddeaff85366f2365cfe60
Rob Herring <rob.herring@calxeda.com>
2012-02-12 15:40:10 -0600

    ARM: ep93xx: clean-up mach/io.h
    Move ep93xx specifics in mach/io.h to ep93xx-regs.h.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ryan Mallon <rmallon@gmail.com>

--- arch/arm/mach-ep93xx/include/mach/io.h
+++ arch/arm/mach-ep93xx/include/mach/io.h
@@ -11,13 +11,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __io(p)			__typesafe_io(p)
 #define __mem_pci(p)		(p)
 
-/*
- * A typesafe __io() variation for variable initialisers
- */
-#ifdef __ASSEMBLER__
-#define IOMEM(p)		p
-#else
-#define IOMEM(p)		((void __iomem __force *)(p))
-#endif
-
 #endif /* __ASM_MACH_IO_H */
