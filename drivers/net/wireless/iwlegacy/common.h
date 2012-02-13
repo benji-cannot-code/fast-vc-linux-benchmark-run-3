1e3132a30f1c189006b3b33729cc2486643a69c8
Stanislaw Gruszka <sgruszka@redhat.com>
2012-02-13 11:23:17 +0100

    iwlegacy: remove il_apm_ops
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlegacy/common.h
+++ drivers/net/wireless/iwlegacy/common.h
@@ -1567,11 +1567,6 @@ struct il_hcmd_utils_ops {
 	void (*post_scan) (struct il_priv *il);
 };
 
-struct il_apm_ops {
-	int (*init) (struct il_priv *il);
-	void (*config) (struct il_priv *il);
-};
-
 #ifdef CONFIG_IWLEGACY_DEBUGFS
 struct il_debugfs_ops {
 	ssize_t(*rx_stats_read) (struct file *file, char __user *user_buf,
@@ -1606,9 +1601,9 @@ struct il_lib_ops {
 	int (*set_channel_switch) (struct il_priv *il,
 				   struct ieee80211_channel_switch *ch_switch);
 	/* power management */
-	struct il_apm_ops apm_ops;
+	int (*apm_init) (struct il_priv *il);
 
-	/* power */
+	/* tx power */
 	int (*send_tx_power) (struct il_priv *il);
 	void (*update_chain_flags) (struct il_priv *il);
 
