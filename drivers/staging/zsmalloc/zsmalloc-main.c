d219859605b612845e4f7305856fdd04539d9251
Seth Jennings <sjenning@linux.vnet.ibm.com>
2012-02-13 08:47:49 -0600

    staging: fix powerpc linux-next break on zsmalloc
    linux/vmalloc.h added to zsmalloc-main.c to resolve implicit
    declaration errors.
    
    X86 dependency added to zsmalloc and dependent drivers zcache and zram.
    
    This X86 only requirement is not ideal.  Working to find portable
    functions for __flush_tlb_one and set_pte.
    
    Signed-off-by: Seth Jennings <sjenning@linux.vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/zsmalloc/zsmalloc-main.c
+++ drivers/staging/zsmalloc/zsmalloc-main.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <linux/cpumask.h>
 #include <linux/cpu.h>
+#include <linux/vmalloc.h>
 
 #include "zsmalloc.h"
 #include "zsmalloc_int.h"
