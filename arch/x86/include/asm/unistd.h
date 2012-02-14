63c4ac0dbb2a63ebcdae6bf810187dfb643425eb
H. Peter Anvin <hpa@zytor.com>
2012-02-14 14:46:23 -0800

    x32: Generate <asm/unistd_64_x32.h>
    Generate macros for the *kernel* code to use to refer to x32 system
    calls.  These have an __NR_x32_ prefix and do not include
    __X32_SYSCALL_BIT.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/include/asm/unistd.h
+++ arch/x86/include/asm/unistd.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # else
 
 #  include <asm/unistd_64.h>
+#  include <asm/unistd_64_x32.h>
 #  define __ARCH_WANT_COMPAT_SYS_TIME
 
 # endif
