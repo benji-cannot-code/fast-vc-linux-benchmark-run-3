c68e9c98f3ec7bb964227483c30c6324d593982d
Bob Moore <robert.moore@intel.com>
2012-02-14 13:11:07 +0800

    ACPICA: Update _REV return value to 5
    _REV returns the supported ACPI revision level, which is now 5.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lin Ming <ming.m.lin@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/acpica/acconfig.h
+++ drivers/acpi/acpica/acconfig.h
@@ -94,7 +94,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Version of ACPI supported */
 
-#define ACPI_CA_SUPPORT_LEVEL           3
+#define ACPI_CA_SUPPORT_LEVEL           5
 
 /* Maximum count for a semaphore object */
 
