959bf9b42031f7dce5036706785ef5663a2ed859
Nicolas Ferre <nicolas.ferre@atmel.com>
2012-02-14 18:08:14 +0100

    ARM: at91: AIC and GPIO IRQ device tree initialization
    Both AIC and GPIO controllers are now using the standard of_irq_init()
    function to initialize IRQs in case of DT use.
    The DT specific initialization functions are now separated from the
    non-DT case and are now using "linear" irq domains.
    The .map() irqdomain operation is responsible for positioning the IRQ
    handlers. In AIC case, the Linux IRQ number is directly programmed in
    the hardware to avoid an additional reverse mapping operation.
    The AIC position its irq domain as the "default" irq domain.
    
    For DT case, the priority is not yet filled in the SMR. It will be the
    subject of another patch.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/board-dt.c
+++ arch/arm/mach-at91/board-dt.c
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/gpio.h>
+#include <linux/of.h>
+#include <linux/of_irq.h>
 #include <linux/of_platform.h>
 
 #include <mach/hardware.h>
@@ -81,9 +83,16 @@ static void __init ek_add_device_nand(void)
 	at91_add_device_nand(&ek_nand_data);
 }
 
+static const struct of_device_id irq_of_match[] __initconst = {
+
+	{ .compatible = "atmel,at91rm9200-aic", .data = at91_aic_of_init },
+	{ .compatible = "atmel,at91rm9200-gpio", .data = at91_gpio_of_irq_setup },
+	{ /*sentinel*/ }
+};
+
 static void __init at91_dt_init_irq(void)
 {
-	at91_init_irq_default();
+	of_irq_init(irq_of_match);
 }
 
 static void __init at91_dt_device_init(void)
