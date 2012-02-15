bf4d0d2c769df76b6c20e6947a815824f0eced43
Mintz Yuval <yuvalmin@broadcom.com>
2012-02-15 02:10:23 +0000

    bnx2x: half duplex support added for several boards
    From: Yaniv Rosner <yaniv.rosner@broadcom.com>
    
    Several boards require an additional HW bit written in-order to enable
    half duplex.
    
    Signed-off-by: Yaniv Rosner <yaniv.rosner@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_reg.h
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_reg.h
@@ -4813,6 +4813,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    The fields are: [4:0] - tail pointer; 10:5] - Link List size; 15:11] -
    header pointer. */
 #define UCM_REG_XX_TABLE					 0xe0300
+#define UMAC_COMMAND_CONFIG_REG_HD_ENA				 (0x1<<10)
 #define UMAC_COMMAND_CONFIG_REG_IGNORE_TX_PAUSE			 (0x1<<28)
 #define UMAC_COMMAND_CONFIG_REG_LOOP_ENA			 (0x1<<15)
 #define UMAC_COMMAND_CONFIG_REG_NO_LGTH_CHECK			 (0x1<<24)
