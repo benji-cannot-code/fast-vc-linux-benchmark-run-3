c9d03fbf595db89c4bd2fcf33c3cab30e2831f72
Danny Kukawka <danny.kukawka@bisect.de>
2012-02-15 15:20:55 -0300

    [media] mt9p031.c included media/v4l2-subdev.h twice
    drivers/media/video/mt9p031.c included 'media/v4l2-subdev.h' twice,
    remove the duplicate.
    
    Signed-off-by: Danny Kukawka <danny.kukawka@bisect.de>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/mt9p031.c
+++ drivers/media/video/mt9p031.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/log2.h>
 #include <linux/pm.h>
 #include <linux/slab.h>
-#include <media/v4l2-subdev.h>
 #include <linux/videodev2.h>
 
 #include <media/mt9p031.h>
