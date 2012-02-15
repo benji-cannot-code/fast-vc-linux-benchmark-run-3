b651156dcd7ef741ac129f37735dc1be7b7ded17
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-02-15 18:35:40 +0800

    ARM: at91: uncompress Store UART address in a variable
    This will allow a future change to auto-detect which UART to use.
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/include/mach/uncompress.h
+++ arch/arm/mach-at91/include/mach/uncompress.h
@@ -44,6 +44,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define UART_OFFSET AT91_USART5
 #endif
 
+void __iomem *at91_uart;
+
+static inline void arch_decomp_setup(void)
+{
+#ifdef UART_OFFSET
+	at91_uart = (void __iomem *) UART_OFFSET;	/* physical address */
+#endif
+}
 /*
  * The following code assumes the serial port has already been
  * initialized by the bootloader.  If you didn't setup a port in
@@ -54,26 +62,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static void putc(int c)
 {
 #ifdef UART_OFFSET
-	void __iomem *sys = (void __iomem *) UART_OFFSET;	/* physical address */
-
-	while (!(__raw_readl(sys + ATMEL_US_CSR) & ATMEL_US_TXRDY))
+	while (!(__raw_readl(at91_uart + ATMEL_US_CSR) & ATMEL_US_TXRDY))
 		barrier();
-	__raw_writel(c, sys + ATMEL_US_THR);
+	__raw_writel(c, at91_uart + ATMEL_US_THR);
 #endif
 }
 
 static inline void flush(void)
 {
 #ifdef UART_OFFSET
-	void __iomem *sys = (void __iomem *) UART_OFFSET;	/* physical address */
-
 	/* wait for transmission to complete */
-	while (!(__raw_readl(sys + ATMEL_US_CSR) & ATMEL_US_TXEMPTY))
+	while (!(__raw_readl(at91_uart + ATMEL_US_CSR) & ATMEL_US_TXEMPTY))
 		barrier();
 #endif
 }
-
-#define arch_decomp_setup()
 
 #define arch_decomp_wdog()
 
