d191d4d5ad6dc62f97d239ce627e7f47abc29e14
Danny Kukawka <danny.kukawka@bisect.de>
2012-02-15 09:20:04 +0000

    arch/sparc/kernel/unaligned_64.c: included linux/bitops.h twice
    arch/sparc/kernel/unaligned_64.c included 'linux/bitops.h' twice,
    remove the duplicate.
    
    Signed-off-by: Danny Kukawka <danny.kukawka@bisect.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/unaligned_64.c
+++ arch/sparc/kernel/unaligned_64.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/perf_event.h>
 #include <linux/ratelimit.h>
-#include <linux/bitops.h>
 #include <asm/fpumacro.h>
 
 enum direction {
