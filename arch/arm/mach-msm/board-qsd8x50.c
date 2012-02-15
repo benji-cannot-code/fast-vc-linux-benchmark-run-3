aa35113ba6fe922c17ae9dc7f509283a5df032f4
Danny Kukawka <danny.kukawka@bisect.de>
2012-02-15 20:18:48 +0100

    arch/arm/mach-msm: linux/gpio.h included twice
    arch/arm/mach-msm/board-msm7x30.c and arch/arm/mach-msm/board-qsd8x50.c
    included 'linux/gpio.h' twice, remove the duplicates.
    
    Signed-off-by: Danny Kukawka <danny.kukawka@bisect.de>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- arch/arm/mach-msm/board-qsd8x50.c
+++ arch/arm/mach-msm/board-qsd8x50.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/kernel.h>
 #include <linux/irq.h>
-#include <linux/gpio.h>
 #include <linux/platform_device.h>
 #include <linux/delay.h>
 #include <linux/usb/msm_hsusb.h>
