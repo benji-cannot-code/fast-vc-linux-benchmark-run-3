53558e416e6341e7dcf60f480c145ee0a6693f2f
Liu Bo <liubo2009@cn.fujitsu.com>
2012-02-16 18:34:38 +0800

    Btrfs: kick out redundant stuff in convert_extent_bit
    clear_state_bit will do merge_state for us, so kick out the redundant one.
    
    Signed-off-by: Liu Bo <liubo2009@cn.fujitsu.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -967,8 +967,6 @@ hit_next:
 
 		set_state_bits(tree, state, &bits);
 		clear_state_bit(tree, state, &clear_bits, 0);
-
-		merge_state(tree, state);
 		if (last_end == (u64)-1)
 			goto out;
 
@@ -1013,7 +1011,6 @@ hit_next:
 		if (state->end <= end) {
 			set_state_bits(tree, state, &bits);
 			clear_state_bit(tree, state, &clear_bits, 0);
-			merge_state(tree, state);
 			if (last_end == (u64)-1)
 				goto out;
 			start = last_end + 1;
@@ -1074,8 +1071,6 @@ hit_next:
 
 		set_state_bits(tree, prealloc, &bits);
 		clear_state_bit(tree, prealloc, &clear_bits, 0);
-
-		merge_state(tree, prealloc);
 		prealloc = NULL;
 		goto out;
 	}
