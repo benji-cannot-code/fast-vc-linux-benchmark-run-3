092f967e2c87ac7df87a481d801dee0473e25a50
Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
2012-02-16 01:14:37 +0000

    fadump: Initialize elfcore header and add PT_LOAD program headers.
    Build the crash memory range list by traversing through system memory during
    the first kernel before we register for firmware-assisted dump. After the
    successful dump registration, initialize the elfcore header and populate
    PT_LOAD program headers with crash memory ranges. The elfcore header is
    saved in the scratch area within the reserved memory. The scratch area starts
    at the end of the memory reserved for saving RMR region contents. The
    scratch area contains fadump crash info structure that contains magic number
    for fadump validation and physical address where the eflcore header can be
    found. This structure will also be used to pass some important crash info
    data to the second kernel which will help second kernel to populate ELF core
    header with correct data before it gets exported through /proc/vmcore. Since
    the firmware preserves the entire partition memory at the time of crash the
    contents of the scratch area will be preserved till second kernel boot.
    
    Since the memory dump exported through /proc/vmcore is in ELF format similar
    to kdump, it will help us to reuse the kdump infrastructure for dump capture
    and filtering. Unlike phyp dump, userspace tool does not need to refer any
    sysfs interface while reading /proc/vmcore.
    
    NOTE: The current design implementation does not address a possibility of
    introducing additional fields (in future) to this structure without affecting
    compatibility. It's on TODO list to come up with better approach to
    address this.
    
    Reserved dump area start => +-------------------------------------+
                                |  CPU state dump data                |
                                +-------------------------------------+
                                |  HPTE region data                   |
                                +-------------------------------------+
                                |  RMR region data                    |
    Scratch area start       => +-------------------------------------+
                                |  fadump crash info structure {      |
                                |     magic nummber                   |
                         +------|---- elfcorehdr_addr                 |
                         |      |  }                                  |
                         +----> +-------------------------------------+
                                |  ELF core header                    |
    Reserved dump area end   => +-------------------------------------+
    
    Signed-off-by: Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/fadump.h
+++ arch/powerpc/include/asm/fadump.h
@@ -44,6 +44,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MIN_BOOT_MEM	(((RMA_END < (0x1UL << 28)) ? (0x1UL << 28) : RMA_END) \
 			+ (0x1UL << 26))
 
+#define memblock_num_regions(memblock_type)	(memblock.memblock_type.cnt)
+
+#ifndef ELF_CORE_EFLAGS
+#define ELF_CORE_EFLAGS 0
+#endif
+
 /* Firmware provided dump sections */
 #define FADUMP_CPU_STATE_DATA	0x0001
 #define FADUMP_HPTE_REGION	0x0002
@@ -56,6 +62,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FADUMP_REGISTER		1
 #define FADUMP_UNREGISTER	2
 #define FADUMP_INVALIDATE	3
+
+/* Dump status flag */
+#define FADUMP_ERROR_FLAG	0x2000
 
 /* Kernel Dump section info */
 struct fadump_section {
@@ -110,12 +119,46 @@ struct fw_dump {
 	/* cmd line option during boot */
 	unsigned long	reserve_bootvar;
 
+	unsigned long	fadumphdr_addr;
 	int		ibm_configure_kernel_dump;
 
 	unsigned long	fadump_enabled:1;
 	unsigned long	fadump_supported:1;
 	unsigned long	dump_active:1;
 	unsigned long	dump_registered:1;
+};
+
+/*
+ * Copy the ascii values for first 8 characters from a string into u64
+ * variable at their respective indexes.
+ * e.g.
+ *  The string "FADMPINF" will be converted into 0x4641444d50494e46
+ */
+static inline u64 str_to_u64(const char *str)
+{
+	u64 val = 0;
+	int i;
+
+	for (i = 0; i < sizeof(val); i++)
+		val = (*str) ? (val << 8) | *str++ : val << 8;
+	return val;
+}
+#define STR_TO_HEX(x)	str_to_u64(x)
+
+#define FADUMP_CRASH_INFO_MAGIC		STR_TO_HEX("FADMPINF")
+
+/* fadump crash info structure */
+struct fadump_crash_info_header {
+	u64		magic_number;
+	u64		elfcorehdr_addr;
+};
+
+/* Crash memory ranges */
+#define INIT_CRASHMEM_RANGES	(INIT_MEMBLOCK_REGIONS + 2)
+
+struct fad_crash_memory_ranges {
+	unsigned long long	base;
+	unsigned long long	size;
 };
 
 extern int early_init_dt_scan_fw_dump(unsigned long node,
