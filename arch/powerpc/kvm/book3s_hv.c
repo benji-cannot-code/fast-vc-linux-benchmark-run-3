3f402b9b5bc1413a03adbf02270fe28f2d8101c9
Danny Kukawka <danny.kukawka@bisect.de>
2012-02-16 14:55:54 +0100

    arch/powerpc/kvm/book3s_hv.c: included linux/sched.h twice
    arch/powerpc/kvm/book3s_hv.c: included 'linux/sched.h' twice,
    remove the duplicate.
    
    Signed-off-by: Danny Kukawka <danny.kukawka@bisect.de>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- arch/powerpc/kvm/book3s_hv.c
+++ arch/powerpc/kvm/book3s_hv.c
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/page.h>
 #include <asm/hvcall.h>
 #include <linux/gfp.h>
-#include <linux/sched.h>
 #include <linux/vmalloc.h>
 #include <linux/highmem.h>
 #include <linux/hugetlb.h>
