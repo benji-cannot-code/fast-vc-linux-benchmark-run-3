5833661f1713d215c9b33c4d67051dfe1cf2fec8
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2012-02-16 09:35:19 +0200

    iwlwifi: document the operational mode
    Also add a might_sleep to enforce the context requirements.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-trans.h
+++ drivers/net/wireless/iwlwifi/iwl-trans.h
@@ -107,12 +107,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	6) Eventually, the free function will be called.
  */
 
-/**
- * DOC: API needed by the transport layer from the op_mode
- *
- * TODO
- */
-
 struct iwl_priv;
 struct iwl_shared;
 struct iwl_op_mode;
