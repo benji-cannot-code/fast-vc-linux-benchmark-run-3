3851dc69f192701967b2e6effb3232a780e4af59
Danny Kukawka <danny.kukawka@bisect.de>
2012-02-16 15:46:38 +0100

    arch/arm/mach-shmobile/board-ag5evm.c: included linux/dma-mapping.h twice
    arch/arm/mach-shmobile/board-ag5evm.c: included 'linux/dma-mapping.h'
    twice, remove the duplicate.
    
    Signed-off-by: Danny Kukawka <danny.kukawka@bisect.de>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/arm/mach-shmobile/board-ag5evm.c
+++ arch/arm/mach-shmobile/board-ag5evm.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/sh_mobile_sdhi.h>
 #include <linux/mfd/tmio.h>
 #include <linux/sh_clk.h>
-#include <linux/dma-mapping.h>
 #include <video/sh_mobile_lcdc.h>
 #include <video/sh_mipi_dsi.h>
 #include <sound/sh_fsi.h>
