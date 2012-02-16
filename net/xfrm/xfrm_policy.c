e17073cf0beb10119015ae1da4f69d86d5a214d0
Eric Paris <eparis@redhat.com>
2012-02-16 15:08:39 -0500

    SELinux: include flow.h where used rather than get it indirectly
    We use flow_cache_genid in the selinux xfrm files.  This is declared in
    net/flow.h  However we do not include that file directly anywhere.  We have
    always just gotten it through a long chain of indirect .h file includes.
    
    on x86_64:
    
      CC      security/selinux/ss/services.o
    In file included from
    /next/linux-next-20120216/security/selinux/ss/services.c:69:0:
    /next/linux-next-20120216/security/selinux/include/xfrm.h: In function 'selinux_xfrm_notify_policyload':
    /next/linux-next-20120216/security/selinux/include/xfrm.h:51:14: error: 'flow_cache_genid' undeclared (first use in this function)
    /next/linux-next-20120216/security/selinux/include/xfrm.h:51:14: note: each undeclared identifier is reported only once for each function it appears in
    make[3]: *** [security/selinux/ss/services.o] Error 1
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- net/xfrm/xfrm_policy.c
+++ net/xfrm/xfrm_policy.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cache.h>
 #include <linux/audit.h>
 #include <net/dst.h>
+#include <net/flow.h>
 #include <net/xfrm.h>
 #include <net/ip.h>
 #ifdef CONFIG_XFRM_STATISTICS
