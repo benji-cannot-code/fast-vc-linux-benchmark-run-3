401ab49d01179bea355b7415a76d66b54f12ff55
Huang Shijie <b32955@freescale.com>
2012-02-17 11:22:37 +0800

    mtd: change the location of the ONFI detected log
    Some strange nand chip(such as Hynix H27UBG8T2A) can pass the `ONFI` signature
    check. So the log can be printed out even it is not an ONFI nand indeed.
    
    Change this log to the end of the function. Print out the log only when we
    really detect an ONFI nand.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>
    Acked-by: Florian Fainelli <ffainelli@freebox.fr>
    Acked-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -2854,7 +2854,6 @@ static int nand_flash_detect_onfi(struct mtd_info *mtd, struct nand_chip *chip,
 		chip->read_byte(mtd) != 'F' || chip->read_byte(mtd) != 'I')
 		return 0;
 
-	pr_info("ONFI flash detected\n");
 	chip->cmdfunc(mtd, NAND_CMD_PARAM, 0, -1);
 	for (i = 0; i < 3; i++) {
 		chip->read_buf(mtd, (uint8_t *)p, sizeof(*p));
@@ -2904,6 +2903,7 @@ static int nand_flash_detect_onfi(struct mtd_info *mtd, struct nand_chip *chip,
 	chip->options |= (NAND_NO_READRDY |
 			NAND_NO_AUTOINCR) & NAND_CHIPOPTIONS_MSK;
 
+	pr_info("ONFI flash detected\n");
 	return 1;
 }
 
