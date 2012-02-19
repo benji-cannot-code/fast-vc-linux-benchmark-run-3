89f443059812364e9933e550337878cc9c2cf99c
Greg Ungerer <gerg@uclinux.org>
2012-02-19 16:33:11 +1000

    m68knommu: make 527x CPU reset register addressing consistent
    If we make all MCF_RCR (CPU reset register) addressing consistent across all
    ColdFire CPU family members that use it then we will be able to remove the
    duplicated copies of the code that use it.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/m527xsim.h
+++ arch/m68k/include/asm/m527xsim.h
@@ -329,8 +329,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *  Reset Control Unit (relative to IPSBAR).
  */
-#define	MCF_RCR			0x110000
-#define	MCF_RSR			0x110001
+#define	MCF_RCR			(MCF_IPSBAR + 0x110000)
+#define	MCF_RSR			(MCF_IPSBAR + 0x110001)
 
 #define	MCF_RCR_SWRESET		0x80		/* Software reset bit */
 #define	MCF_RCR_FRCSTOUT	0x40		/* Force external reset */
