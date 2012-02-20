2608f2965b7d0ed164e671abf96533d87d33834c
Bhanu Prakash Gollapudi <bprakash@broadcom.com>
2012-02-20 09:59:09 +0000

    bnx2fc: HSI dependent changes for 7.2.xx FW
    with Tx only section for single cached SGEs.
    
    Signed-off-by: Bhanu Prakash Gollapudi <bprakash@broadcom.com>
    Signed-off-by: Dmitry Kravkov <dmitry@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/scsi/bnx2fc/bnx2fc_constants.h
+++ drivers/scsi/bnx2fc/bnx2fc_constants.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Current FCoE HSI version number composed of two fields (16 bit) */
 /* Implies on a change broken previous HSI */
-#define FCOE_HSI_MAJOR_VERSION (1)
+#define FCOE_HSI_MAJOR_VERSION (2)
 /* Implies on a change which does not broken previous HSI */
 #define FCOE_HSI_MINOR_VERSION (1)
 
