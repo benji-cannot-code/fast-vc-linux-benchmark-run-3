6b527eebb48a611e62da957760030e004a418ba1
Mike Isely <isely@pobox.com>
2012-02-20 02:03:39 -0300

    [media] pvrusb2: Stop statically initializing reserved struct fields to zero
    In any statically initialized data structure, the compiler is going to
    zero any part which is not already explicitly initialized.  While we
    can take that knowledge overboard and simply avoid initializing
    anything that needs to be zero, it's at least a good idea not to
    bother zeroing parts that we don't otherwise care about - like
    "reserved" fields which may change in the future.  Avoiding
    initialization of those fields now also avoids possible conflict down
    the road.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/pvrusb2/pvrusb2-v4l2.c
+++ drivers/media/video/pvrusb2/pvrusb2-v4l2.c
@@ -108,7 +108,6 @@ static struct v4l2_fmtdesc pvr_fmtdesc [] = {
 		// This should really be V4L2_PIX_FMT_MPEG, but xawtv
 		// breaks when I do that.
 		.pixelformat    = 0, // V4L2_PIX_FMT_MPEG,
-		.reserved       = { 0, 0, 0, 0 }
 	}
 };
 
@@ -146,7 +145,6 @@ static struct v4l2_format pvr_format [] = {
 				.start = { 0, 0 },
 				.count = { 0, 0 },
 				.flags = 0,
-				.reserved = { 0, 0 }
 			}
 		}
 	}
