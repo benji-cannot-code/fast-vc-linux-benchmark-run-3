853360560851296fe09da774876639e84d207368
Mike Isely <isely@pobox.com>
2012-02-20 02:28:56 -0300

    [media] pvrusb2: Transform video standard detection result into read-only control ID
    Other aspects of the pvrusb2 driver - including in particular those
    dealing with video standards - all use internal control IDs for
    uniform access by the interfaces.  By exporting the querystd result as
    another control ID, uniform access to it becomes available through the
    sysfs interface.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/pvrusb2/pvrusb2-hdw.h
+++ drivers/media/video/pvrusb2/pvrusb2-hdw.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PVR2_CID_CROPCAPBT 16
 #define PVR2_CID_CROPCAPBW 17
 #define PVR2_CID_CROPCAPBH 18
+#define PVR2_CID_STDDETECT 19
 
 /* Legal values for the INPUT state variable */
 #define PVR2_CVAL_INPUT_TV 0
@@ -214,9 +215,6 @@ struct pvr2_stream *pvr2_hdw_get_video_stream(struct pvr2_hdw *);
 /* Emit a video standard struct */
 int pvr2_hdw_get_stdenum_value(struct pvr2_hdw *hdw,struct v4l2_standard *std,
 			       unsigned int idx);
-
-/* Get the detected video standard */
-int pvr2_hdw_get_detected_std(struct pvr2_hdw *hdw, v4l2_std_id *std);
 
 /* Enable / disable retrieval of CPU firmware or prom contents.  This must
    be enabled before pvr2_hdw_cpufw_get() will function.  Note that doing
