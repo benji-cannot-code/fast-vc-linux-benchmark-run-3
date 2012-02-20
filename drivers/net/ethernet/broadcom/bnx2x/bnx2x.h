160827751531dd27d5579d639f4ed1d88c31d70f
Dmitry Kravkov <dmitry@broadcom.com>
2012-02-20 09:59:12 +0000

    bnx2x: update driver version to 1.72.10-0
    Signed-off-by: Dmitry Kravkov <dmitry@broadcom.com>
    Signed-off-by: Eilon Greenstein <eilong@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x.h
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x.h
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * (you will need to reboot afterwards) */
 /* #define BNX2X_STOP_ON_ERROR */
 
-#define DRV_MODULE_VERSION      "1.72.00-0"
-#define DRV_MODULE_RELDATE      "2012/01/26"
+#define DRV_MODULE_VERSION      "1.72.10-0"
+#define DRV_MODULE_RELDATE      "2012/02/20"
 #define BNX2X_BC_VER            0x040200
 
 #if defined(CONFIG_DCB)
