25929fda0f035a5b9ffb1da98337e4f20f9cd414
Johannes Berg <johannes.berg@intel.com>
2012-02-21 18:31:06 +0200

    iwlwifi: move uCode flags handling to op_mode
    The uCode flags modification is op_mode dependent
    since the P2P config is an op-mode config.
    
    This also fixes P2P enabling: due to the uCode
    loading code shuffle moving the SKU check before
    the EEPROM was read it was always false and would
    always disable PAN/P2P.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-agn.c
+++ drivers/net/wireless/iwlwifi/iwl-agn.c
@@ -1152,6 +1152,7 @@ int iwl_op_mode_dvm_start(struct iwl_bus *bus,
 	struct iwl_priv *priv;
 	struct ieee80211_hw *hw;
 	u16 num_mac;
+	u32 ucode_flags;
 
 	/************************
 	 * 1. Allocating HW data
@@ -1243,6 +1244,22 @@ int iwl_op_mode_dvm_start(struct iwl_bus *bus,
 	 ************************/
 	iwl_set_hw_params(priv);
 
+	ucode_flags = fw->ucode_capa.flags;
+
+#ifndef CONFIG_IWLWIFI_P2P
+	ucode_flags &= ~IWL_UCODE_TLV_FLAGS_PAN;
+#endif
+	if (!(hw_params(priv).sku & EEPROM_SKU_CAP_IPAN_ENABLE))
+		ucode_flags &= ~IWL_UCODE_TLV_FLAGS_PAN;
+
+	/*
+	 * if not PAN, then don't support P2P -- might be a uCode
+	 * packaging bug or due to the eeprom check above
+	 */
+	if (!(ucode_flags & IWL_UCODE_TLV_FLAGS_PAN))
+		ucode_flags &= ~IWL_UCODE_TLV_FLAGS_P2P;
+
+
 	/*******************
 	 * 6. Setup priv
 	 *******************/
@@ -1267,9 +1284,9 @@ int iwl_op_mode_dvm_start(struct iwl_bus *bus,
 		 "%s", fw->fw_version);
 
 	priv->new_scan_threshold_behaviour =
-		!!(fw->ucode_capa.flags & IWL_UCODE_TLV_FLAGS_NEWSCAN);
+		!!(ucode_flags & IWL_UCODE_TLV_FLAGS_NEWSCAN);
 
-	if (fw->ucode_capa.flags & IWL_UCODE_TLV_FLAGS_PAN) {
+	if (ucode_flags & IWL_UCODE_TLV_FLAGS_PAN) {
 		priv->sta_key_max_num = STA_KEY_MAX_NUM_PAN;
 		priv->shrd->cmd_queue = IWL_IPAN_CMD_QUEUE_NUM;
 	} else {
@@ -1283,7 +1300,7 @@ int iwl_op_mode_dvm_start(struct iwl_bus *bus,
 		fw->ucode_capa.standard_phy_calibration_size + 1;
 
 	/* initialize all valid contexts */
-	iwl_init_context(priv, fw->ucode_capa.flags);
+	iwl_init_context(priv, ucode_flags);
 
 	/**************************************************
 	 * This is still part of probe() in a sense...
