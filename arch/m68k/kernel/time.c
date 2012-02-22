ea221a0b6a69fe18b80ee345fd3c6e2f4e98c6e0
Greg Ungerer <gerg@uclinux.org>
2012-02-22 13:51:22 +1000

    m68k: remove dead timer code
    The set_rtc_mmss() function is defined "static inline" but is never used
    in this file. Remove it.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/kernel/time.c
+++ arch/m68k/kernel/time.c
@@ -29,13 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timex.h>
 #include <linux/profile.h>
 
-static inline int set_rtc_mmss(unsigned long nowtime)
-{
-  if (mach_set_clock_mmss)
-    return mach_set_clock_mmss (nowtime);
-  return -1;
-}
-
 /*
  * timer_interrupt() needs to keep up the real-time clock,
  * as well as call the "xtime_update()" routine every clocktick
