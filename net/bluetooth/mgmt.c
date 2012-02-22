1f1958e2d9c214352494fa3b483d3afb519c8ec1
Johan Hedberg <johan.hedberg@intel.com>
2012-02-22 22:06:38 +0200

    Bluetooth: mgmt: Check for HCI_UP in update_eir() and update_class()
    These functions should just silently fail when we're not powered on
    instead of trying to send HCI commands.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -533,6 +533,9 @@ static int update_eir(struct hci_dev *hdev)
 {
 	struct hci_cp_write_eir cp;
 
+	if (!test_bit(HCI_UP, &hdev->flags))
+		return 0;
+
 	if (!(hdev->features[6] & LMP_EXT_INQ))
 		return 0;
 
@@ -570,6 +573,9 @@ static int update_class(struct hci_dev *hdev)
 	u8 cod[3];
 
 	BT_DBG("%s", hdev->name);
+
+	if (!test_bit(HCI_UP, &hdev->flags))
+		return 0;
 
 	if (test_bit(HCI_SERVICE_CACHE, &hdev->dev_flags))
 		return 0;
