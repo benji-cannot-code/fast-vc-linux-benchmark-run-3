214f6b7a02f8ea1e1bad6eab64620afc1e52ff33
Guenter Roeck <linux@roeck-us.net>
2012-02-22 08:56:44 -0800

    hwmon: (pmbus) Simplify remove functions
    Since devm_kzalloc() is now used to allocate driver memory, the client
    driver remove function has no purpose other than to call pmbus_do_remove().
    This means we can get rid of it by redefining pmbus_do_remove() to use the
    same prototype, and pointing to it directly.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/pmbus/max34440.c
+++ drivers/hwmon/pmbus/max34440.c
@@ -225,12 +225,6 @@ static int max34440_probe(struct i2c_client *client,
 	return pmbus_do_probe(client, id, &max34440_info[id->driver_data]);
 }
 
-static int max34440_remove(struct i2c_client *client)
-{
-	pmbus_do_remove(client);
-	return 0;
-}
-
 static const struct i2c_device_id max34440_id[] = {
 	{"max34440", max34440},
 	{"max34441", max34441},
@@ -245,7 +239,7 @@ static struct i2c_driver max34440_driver = {
 		   .name = "max34440",
 		   },
 	.probe = max34440_probe,
-	.remove = max34440_remove,
+	.remove = pmbus_do_remove,
 	.id_table = max34440_id,
 };
 
