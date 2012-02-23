5f9d105f96b08d0e1e5e2d294001d96a919651f4
Johan Hedberg <johan.hedberg@intel.com>
2012-02-23 09:52:28 +0200

    Bluetooth: mgmt: Fix count parameter in get_connections reply
    This patch fixes the count parameter in the Get Connections reply
    message. We cannot know the right number until iterating through all
    connections so set the parameter value only after the loop.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -1726,8 +1726,8 @@ static int get_connections(struct sock *sk, u16 index)
 	struct hci_dev *hdev;
 	struct hci_conn *c;
 	size_t rp_len;
-	u16 count;
-	int i, err;
+	int err;
+	u16 i;
 
 	BT_DBG("");
 
@@ -1744,20 +1744,18 @@ static int get_connections(struct sock *sk, u16 index)
 		goto unlock;
 	}
 
-	count = 0;
+	i = 0;
 	list_for_each_entry(c, &hdev->conn_hash.list, list) {
 		if (test_bit(HCI_CONN_MGMT_CONNECTED, &c->flags))
-			count++;
+			i++;
 	}
 
-	rp_len = sizeof(*rp) + (count * sizeof(struct mgmt_addr_info));
+	rp_len = sizeof(*rp) + (i * sizeof(struct mgmt_addr_info));
 	rp = kmalloc(rp_len, GFP_ATOMIC);
 	if (!rp) {
 		err = -ENOMEM;
 		goto unlock;
 	}
-
-	put_unaligned_le16(count, &rp->conn_count);
 
 	i = 0;
 	list_for_each_entry(c, &hdev->conn_hash.list, list) {
@@ -1769,6 +1767,8 @@ static int get_connections(struct sock *sk, u16 index)
 			continue;
 		i++;
 	}
+
+	put_unaligned_le16(i, &rp->conn_count);
 
 	/* Recalculate length in case of filtered SCO connections, etc */
 	rp_len = sizeof(*rp) + (i * sizeof(struct mgmt_addr_info));
