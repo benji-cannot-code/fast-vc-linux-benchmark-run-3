3a8f765695c499d28591cf66f319dca109f29618
Bjorn Helgaas <bhelgaas@google.com>
2012-02-23 20:18:57 -0700

    mips/PCI: removed unused pci_probe configurability
    We never assign anything other than PCI_ASSIGN_ALL_BUSSES to pci_probe,
    so just remove the indirection.  If configurability is required in the
    future, please use the pci_flags/PCI_REASSIGN_ALL_BUS functionality
    as is done for powerpc.
    
    CC: Ralf Baechle <ralf@linux-mips.org>
    CC: linux-mips@linux-mips.org
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- arch/mips/pci/pci.c
+++ arch/mips/pci/pci.c
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * assignments.
  */
 
-#define PCI_ASSIGN_ALL_BUSSES	1
-
-unsigned int pci_probe = PCI_ASSIGN_ALL_BUSSES;
-
 /*
  * The PCI controller list.
  */
@@ -239,7 +235,7 @@ static int pcibios_enable_resources(struct pci_dev *dev, int mask)
 
 unsigned int pcibios_assign_all_busses(void)
 {
-	return (pci_probe & PCI_ASSIGN_ALL_BUSSES) ? 1 : 0;
+	return 1;
 }
 
 int pcibios_enable_device(struct pci_dev *dev, int mask)
