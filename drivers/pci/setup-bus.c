a3c7334a86bb13b3b1740d2ad64db77681bb42ad
Bjorn Helgaas <bhelgaas@google.com>
2012-02-23 20:18:59 -0700

    PCI: make pci_flags non-weak
    No architecture defines its own pci_flags, so the core symbol does not
    need to be weak.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm-generic/pci-bridge.h>
 #include "pci.h"
 
-unsigned int __weak pci_flags;
+unsigned int pci_flags;
 
 struct pci_dev_resource {
 	struct list_head list;
