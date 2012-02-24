b50583ed4217e77ca14cb7f923e4834d5daf3c06
Ezequiel García <elezegarcia@gmail.com>
2012-02-24 11:24:17 -0300

    [media] staging: easycap: Initialize 'ntsc' parameter before usage
    This parameter is now initialized at init_easycap(),
    this way we assure it won't be used uninitialized.
    
    Signed-off-by: Ezequiel Garcia <elezegarcia@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/media/easycap/easycap_main.c
+++ drivers/staging/media/easycap/easycap_main.c
@@ -2961,6 +2961,10 @@ static void init_easycap(struct easycap *peasycap,
 	peasycap->audio_isoc_buffer_size = -1;
 
 	peasycap->frame_buffer_many = FRAME_BUFFER_MANY;
+
+	peasycap->ntsc = easycap_ntsc;
+	JOM(8, "defaulting initially to %s\n",
+		easycap_ntsc ? "NTSC" : "PAL");
 }
 
 static int populate_inputset(struct easycap *peasycap)
@@ -2973,7 +2977,6 @@ static int populate_inputset(struct easycap *peasycap)
 
 	inputset = peasycap->inputset;
 
-	/* FIXME: peasycap->ntsc is not yet initialized */
 	fmtidx = peasycap->ntsc ? NTSC_M : PAL_BGHIN;
 
 	m = 0;
@@ -3651,9 +3654,6 @@ static int easycap_usb_probe(struct usb_interface *intf,
 		 * because some udev rules triggers easycap_open()
 		 * immediately after registration, causing a clash.
 		 */
-		peasycap->ntsc = easycap_ntsc;
-		JOM(8, "defaulting initially to %s\n",
-			easycap_ntsc ? "NTSC" : "PAL");
 		rc = reset(peasycap);
 		if (rc) {
 			SAM("ERROR: reset() rc = %i\n", rc);
