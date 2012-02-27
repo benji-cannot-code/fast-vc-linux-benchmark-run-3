7821878ad7d8eac259872a85e53a7bc1cb1cf91b
Lai Jiangshan <laijs@cn.fujitsu.com>
2012-02-27 09:29:09 -0800

    rcu: Implement a variant of Peter's SRCU algorithm
    This commit implements a variant of Peter's algorithm, which may be found
    at https://lkml.org/lkml/2012/2/1/119.
    
    o	Make the checking lock-free to enable parallel checking.
    	Parallel checking is required when (1) the original checking
    	task is preempted for a long time, (2) sychronize_srcu_expedited()
    	starts during an ongoing SRCU grace period, or (3) we wish to
    	avoid acquiring a lock.
    
    o	Since the checking is lock-free, we avoid a mutex in state machine
    	for call_srcu().
    
    o	Remove the SRCU_REF_MASK and remove the coupling with the flipping.
    	This might allow us to remove the preempt_disable() in future
    	versions, though such removal will need great care because it
    	rescinds the one-old-reader-per-CPU guarantee.
    
    o	Remove a smp_mb(), simplify the comments and make the smp_mb() pairs
    	more intuitive.
    
    Inspired-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/srcu.h
+++ include/linux/srcu.h
@@ -33,18 +33,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct srcu_struct_array {
 	unsigned long c[2];
+	unsigned long seq[2];
 };
-
-/* Bit definitions for field ->c above and ->snap below. */
-#define SRCU_USAGE_BITS		1
-#define SRCU_REF_MASK		(ULONG_MAX >> SRCU_USAGE_BITS)
-#define SRCU_USAGE_COUNT	(SRCU_REF_MASK + 1)
 
 struct srcu_struct {
 	unsigned completed;
 	struct srcu_struct_array __percpu *per_cpu_ref;
 	struct mutex mutex;
-	unsigned long snap[NR_CPUS];
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map dep_map;
 #endif /* #ifdef CONFIG_DEBUG_LOCK_ALLOC */
