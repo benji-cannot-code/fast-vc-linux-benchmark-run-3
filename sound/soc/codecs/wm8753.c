30faa3103650f0c599d4e4b88db787c16b08bdd3
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-02-27 15:24:10 +0000

    ASoC: wm8753: Convert to devm_kzalloc()
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8753.c
+++ sound/soc/codecs/wm8753.c
@@ -1558,7 +1558,8 @@ static int __devinit wm8753_spi_probe(struct spi_device *spi)
 	struct wm8753_priv *wm8753;
 	int ret;
 
-	wm8753 = kzalloc(sizeof(struct wm8753_priv), GFP_KERNEL);
+	wm8753 = devm_kzalloc(&spi->dev, sizeof(struct wm8753_priv),
+			      GFP_KERNEL);
 	if (wm8753 == NULL)
 		return -ENOMEM;
 
@@ -1581,7 +1582,6 @@ static int __devinit wm8753_spi_probe(struct spi_device *spi)
 err_regmap:
 	regmap_exit(wm8753->regmap);
 err:
-	kfree(wm8753);
 	return ret;
 }
 
@@ -1613,7 +1613,8 @@ static __devinit int wm8753_i2c_probe(struct i2c_client *i2c,
 	struct wm8753_priv *wm8753;
 	int ret;
 
-	wm8753 = kzalloc(sizeof(struct wm8753_priv), GFP_KERNEL);
+	wm8753 = devm_kzalloc(&i2c->dev, sizeof(struct wm8753_priv),
+			      GFP_KERNEL);
 	if (wm8753 == NULL)
 		return -ENOMEM;
 
@@ -1633,10 +1634,10 @@ static __devinit int wm8753_i2c_probe(struct i2c_client *i2c,
 		dev_err(&i2c->dev, "Failed to register CODEC: %d\n", ret);
 		goto err_regmap;
 	}
+
 err_regmap:
 	regmap_exit(wm8753->regmap);
 err:
-	kfree(wm8753);
 	return ret;
 }
 
@@ -1646,7 +1647,6 @@ static __devexit int wm8753_i2c_remove(struct i2c_client *client)
 
 	snd_soc_unregister_codec(&client->dev);
 	regmap_exit(wm8753->regmap);
-	kfree(wm8753);
 	return 0;
 }
 
