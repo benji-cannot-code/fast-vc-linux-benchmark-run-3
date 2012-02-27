1e834b6531d4d71bba174830f798e7b8dd9c6ce2
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-02-28 00:43:09 +0100

    drm/i915: merge gmbus and gpio i2c adpater into one
    ... and directly call the newly exported i2c bit-banging functions.
    
    The code is still pretty convoluted because we only set up the gpio
    i2c stuff when actually falling back, resulting in more complexity
    than necessary. This will be fixed up in the next patch.
    
    v2: Use exported i2c_bit_algo vtable instead of exported functions.
    
    Reviewed-by: Eugeni Dodonov <eugeni.dodonov@intel.com>
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "intel_ringbuffer.h"
 #include <linux/io-mapping.h>
 #include <linux/i2c.h>
+#include <linux/i2c-algo-bit.h>
 #include <drm/intel-gtt.h>
 #include <linux/backlight.h>
 
@@ -304,6 +305,7 @@ struct intel_gmbus {
 	struct i2c_adapter *force_bit;
 	u32 reg0;
 	u32 gpio_reg;
+	struct i2c_algo_bit_data bit_algo;
 	struct drm_i915_private *dev_priv;
 };
 
