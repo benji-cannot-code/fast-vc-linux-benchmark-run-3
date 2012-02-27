50d74a764d5b6da2d0df3237356797e802c66c92
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-02-27 15:18:56 -0800

    tty: keyboard.c: add uaccess.h to fix a build problem on sparc32
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/vt/keyboard.c
+++ drivers/tty/vt/keyboard.c
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/reboot.h>
 #include <linux/notifier.h>
 #include <linux/jiffies.h>
+#include <linux/uaccess.h>
 
 #include <asm/irq_regs.h>
 
