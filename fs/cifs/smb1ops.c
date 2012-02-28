e2ea75c897007352d51b7b6568e39687dfeb271e
Pavel Shilovsky <piastry@etersoft.ru>
2012-02-28 14:23:34 +0300

    CIFS: Separate protocol specific lock type handling
    Signed-off-by: Pavel Shilovsky <piastry@etersoft.ru>

--- fs/cifs/smb1ops.c
+++ fs/cifs/smb1ops.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cifsglob.h"
 #include "cifsproto.h"
 #include "cifs_debug.h"
+#include "cifspdu.h"
 
 /*
  * An NT cancel request header looks just like the original request except:
@@ -66,4 +67,8 @@ struct smb_version_operations smb1_operations = {
 
 struct smb_version_values smb1_values = {
 	.version_string = SMB1_VERSION_STRING,
+	.large_lock_type = LOCKING_ANDX_LARGE_FILES,
+	.exclusive_lock_type = 0,
+	.shared_lock_type = LOCKING_ANDX_SHARED_LOCK,
+	.unlock_lock_type = 0,
 };
