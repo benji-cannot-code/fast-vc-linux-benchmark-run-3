f07d5c545a8b3e30ffcfd5dbdd3a61fadc8249c8
Wim Van Sebroeck <wim@iguana.be>
2012-02-29 20:20:58 +0100

    watchdog: fix set_timeout operations
    Since we changed the behaviour of the set_timeout operation in the
    watchdog API, we need to change the allready converted drivers so
    that they update the timeout field at the end of the set_timeout
    operation.
    
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/s3c2410_wdt.c
+++ drivers/watchdog/s3c2410_wdt.c
@@ -202,6 +202,8 @@ static int s3c2410wdt_set_heartbeat(struct watchdog_device *wdd, unsigned timeou
 	writel(count, wdt_base + S3C2410_WTDAT);
 	writel(wtcon, wdt_base + S3C2410_WTCON);
 
+	wdd->timeout = timeout;
+
 	return 0;
 }
 
