e14437d97a2ffcd917eb035de8132862a1f75de0
Afzal Mohammed <afzal@ti.com>
2012-02-29 18:11:56 +0530

    ARM: OMAP2+: nand: unify init functions
    Helper function for updating nand platform data has been
    added the capability to take timing structure arguement.
    Usage of omap_nand_flash_init() has been replaced by modifed
    one, omap_nand_flash_init was doing things similar to
    board_nand_init except that NAND CS# were being acquired
    based on bootloader setting. As CS# is hardwired for a given
    board, acquiring gpmc CS# has been removed, and updated with
    the value on board.
    
    NAND CS# used in beagle board & omap3evm was found to be CS0.
    Thomas Weber <thomas.weber.linux@googlemail.com> reported
    that value of devkit8000 to be CS0. Overo board was found
    to be using CS0 based on u-boot, while google grep says
    omap3touchbook too has CS0.
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>
    Reviewed-by: Jon Hunter <jon-hunter@ti.com>
    Acked-by: Igor Grinberg <grinberg@compulab.co.il>

--- arch/arm/mach-omap2/board-omap3evm.c
+++ arch/arm/mach-omap2/board-omap3evm.c
@@ -57,6 +57,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sdram-micron-mt46h32m32lf-6.h"
 #include "hsmmc.h"
 #include "common-board-devices.h"
+#include "board-flash.h"
+
+#define	NAND_CS			0
 
 #define OMAP3_EVM_TS_GPIO	175
 #define OMAP3_EVM_EHCI_VBUS	22
@@ -732,8 +735,9 @@ static void __init omap3_evm_init(void)
 	}
 	usb_musb_init(&musb_board_data);
 	usbhs_init(&usbhs_bdata);
-	omap_nand_flash_init(NAND_BUSWIDTH_16, omap3evm_nand_partitions,
-			     ARRAY_SIZE(omap3evm_nand_partitions));
+	board_nand_init(omap3evm_nand_partitions,
+			ARRAY_SIZE(omap3evm_nand_partitions), NAND_CS,
+			NAND_BUSWIDTH_16, NULL);
 
 	omap_ads7846_init(1, OMAP3_EVM_TS_GPIO, 310, NULL);
 	omap3evm_init_smsc911x();
