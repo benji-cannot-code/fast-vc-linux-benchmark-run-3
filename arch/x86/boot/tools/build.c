cb9fdd68f5f69dde96174954d7ec7b690354b361
H. Peter Anvin <hpa@zytor.com>
2012-02-28 23:30:58 -0800

    x86, tools: Remove unneeded header files from tools/build.c
    We include <sys/sysmacros.h> and <asm/boot.h>, but none of those
    header files actually provide anything this file needs.  Furthermore,
    it breaks cross-compilation, so just remove them.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Reported-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Cc: Matt Fleming <matt@console-pimps.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Nick Bowler <nbowler@elliptictech.com>
    Link: http://lkml.kernel.org/r/20120229111322.9eb4b23ff1672e8853ad3b3b@canb.auug.org.au

--- arch/x86/boot/tools/build.c
+++ arch/x86/boot/tools/build.c
@@ -30,11 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdarg.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/sysmacros.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
-#include <asm/boot.h>
 #include <tools/le_byteshift.h>
 
 typedef unsigned char  u8;
