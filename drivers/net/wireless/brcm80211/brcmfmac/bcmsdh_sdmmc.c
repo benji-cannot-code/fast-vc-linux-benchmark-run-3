15671419f1bce6c1840aead6bc1b2a19740d0b4f
Arend van Spriel <arend@broadcom.com>
2012-03-02 22:55:48 +0100

    brcm80211: fmac: initialize host interface drivers regardless result
    The module init function of brcmfmac calls init functions for SDIO and
    USB doing driver registration. This patch removes terminating the module
    init when a driver registration for one host interface fails.
    
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Franky (Zhenhui) Lin <frankyl@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/bcmsdh_sdmmc.c
+++ drivers/net/wireless/brcm80211/brcmfmac/bcmsdh_sdmmc.c
@@ -605,7 +605,7 @@ void brcmf_sdio_exit(void)
 	sdio_unregister_driver(&brcmf_sdmmc_driver);
 }
 
-int brcmf_sdio_init(void)
+void brcmf_sdio_init(void)
 {
 	int ret;
 
@@ -615,6 +615,4 @@ int brcmf_sdio_init(void)
 
 	if (ret)
 		brcmf_dbg(ERROR, "sdio_register_driver failed: %d\n", ret);
-
-	return ret;
 }
