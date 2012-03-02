87c7498344e19d6029350703063f4bedf2e66ec7
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2012-03-01 17:10:17 -0800

    spi: Add SuperH HSPI prototype driver
    This patch adds SuperH HSPI driver.
    It is still prototype driver, but has enough function at this point.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

---
+++ include/linux/spi/sh_hspi.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (C) 2011 Kuninori Morimoto
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef SH_HSPI_H
+#define SH_HSPI_H
+
+/*
+ * flags
+ *
+ *
+ */
+#define SH_HSPI_CLK_DIVC(d)		(d & 0xFF)
+
+#define SH_HSPI_FBS		(1 << 8)
+#define SH_HSPI_CLKP_HIGH	(1 << 9)	/* default LOW */
+#define SH_HSPI_IDIV_DIV128	(1 << 10)	/* default div16 */
+struct sh_hspi_info {
+	u32	flags;
+};
+
+#endif
