c8518c90ea74607a6d6c435d7a4f24b9ef0753e0
Johan Hedberg <johan.hedberg@intel.com>
2012-03-02 03:13:19 +0200

    Bluetooth: mgmt: Add new error code for invalid index
    The index is part of the command header and not its parameters so it
    makes sense to distinguish this from the invalid parameters error.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>

--- include/net/bluetooth/mgmt.h
+++ include/net/bluetooth/mgmt.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MGMT_STATUS_DISCONNECTED	0x0e
 #define MGMT_STATUS_NOT_POWERED		0x0f
 #define MGMT_STATUS_CANCELLED		0x10
+#define MGMT_STATUS_INVALID_INDEX	0x11
 
 struct mgmt_hdr {
 	__le16 opcode;
