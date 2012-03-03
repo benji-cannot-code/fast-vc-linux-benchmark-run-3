5424ffd3f9c4f24b188e57d513768f92350d08f3
Ben Collins <bcollins@ubuntu.com>
2012-03-03 12:43:27 -0800

    udlfb: Make sure to get correct endian keys from vendor descriptor
    The driver was not using le16_to_cpu when reading keys from the vendor
    descriptor, causing incorrect parsing. Mainly, sku_pixel_limit was not
    being parsed on big-endian systems. This would result in a blank screen
    on big-endian CPUs where the DL chips's max mode was smaller than the
    monitor's native mode.
    
    Signed-off-by: Ben Collins <bcollins@ubuntu.com>
    Signed-off-by: Bernie Thompson <bernie@plugable.com>

--- drivers/video/udlfb.c
+++ drivers/video/udlfb.c
@@ -1542,7 +1542,7 @@ static int dlfb_parse_vendor_descriptor(struct dlfb_data *dev,
 			u8 length;
 			u16 key;
 
-			key = *((u16 *) desc);
+			key = le16_to_cpu(*((u16 *) desc));
 			desc += sizeof(u16);
 			length = *desc;
 			desc++;
