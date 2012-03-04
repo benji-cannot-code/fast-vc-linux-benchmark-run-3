9f245a370b63b8c4f2cfb489839f007652b880ef
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-03-04 18:13:56 -0500

    NFSv4.1: Fix matching of the stateids when returning a delegation
    nfs41_validate_delegation_stateid is broken if we supply a stateid with
    a non-zero sequence id. Instead of trying to match the sequence id,
    the function assumes that we always want to error. While this is
    true for a delegation callback, it is not true in general.
    
    Also fix a typo in nfs4_callback_recall.
    
    Reported-by: Andy Adamson <andros@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/callback_proc.c
+++ fs/nfs/callback_proc.c
@@ -88,8 +88,7 @@ __be32 nfs4_callback_recall(struct cb_recallargs *args, void *dummy,
 		res = 0;
 		break;
 	case -ENOENT:
-		if (res != 0)
-			res = htonl(NFS4ERR_BAD_STATEID);
+		res = htonl(NFS4ERR_BAD_STATEID);
 		break;
 	default:
 		res = htonl(NFS4ERR_RESOURCE);
@@ -326,10 +325,11 @@ int nfs41_validate_delegation_stateid(struct nfs_delegation *delegation, const n
 	if (delegation == NULL)
 		return 0;
 
-	if (stateid->stateid.seqid != 0)
+	if (stateid->stateid.seqid != 0 &&
+	    stateid->stateid.seqid != delegation->stateid.stateid.seqid)
 		return 0;
-	if (memcmp(&delegation->stateid.stateid.other,
-		   &stateid->stateid.other,
+	if (memcmp(delegation->stateid.stateid.other,
+		   stateid->stateid.other,
 		   NFS4_STATEID_OTHER_SIZE))
 		return 0;
 
