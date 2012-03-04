dddfec6fe03ae490d8041dcc71801304fded2119
Stefan Richter <stefanr@s5r6.in-berlin.de>
2012-03-04 14:24:31 +0100

    firewire: tone down some diagnostic log messages
    The "skipped bus generations" message was added together with the
    respective fw_device retaining/ reviving code in order to see how it all
    works out.  It did well, so don't spam the log anymore.
    
    The "register access failure" situation still needs an actual handler.
    But at this point it makes less sense to ask folks to send mails about
    it.  We now have a pretty good picture of what controllers emit this and
    when:
    
    Texas Instruments PCIxx21 FireWire + CardBus + flash memory card
    controller:
    https://bugzilla.redhat.com/show_bug.cgi?id=608544
    
    O2 Micro FireWire + flash memory card controller:
    https://bugs.launchpad.net/ubuntu/+source/linux/+bug/801719
    https://bugs.launchpad.net/ubuntu/+source/linux/+bug/881688
    http://marc.info/?l=linux1394-devel&m=132309283531423
    http://marc.info/?l=linux1394-devel&m=132368567907469
    http://marc.info/?l=linux1394-devel&m=132516165727468
    http://marc.info/?l=linux1394-devel&m=133006486927699
    
    Pinnacle Movieboard:
    commit 7f7e37115a8b6724f26d0637a04e1d35e3c59717
    http://marc.info/?l=linux1394-devel&m=130714243325962
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/ohci.c
+++ drivers/firewire/ohci.c
@@ -2077,8 +2077,7 @@ static irqreturn_t irq_handler(int irq, void *data)
 	}
 
 	if (unlikely(event & OHCI1394_regAccessFail))
-		dev_err(ohci->card.device,
-			"register access failure - please notify linux1394-devel@lists.sf.net\n");
+		dev_err(ohci->card.device, "register access failure\n");
 
 	if (unlikely(event & OHCI1394_postedWriteErr)) {
 		reg_read(ohci, OHCI1394_PostedWriteAddressHi);
