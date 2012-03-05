19ce519b52dd3600d93367e5521159748d247cfc
Jiri Slaby <jslaby@suse.cz>
2012-03-05 14:51:54 +0100

    TTY: remove tty driver re-set from tty_reopen
    This is from tty_reopen:
        struct tty_driver *driver = tty->driver;
        ...
        tty->driver = driver;
    and it doesn't make sense at all. The driver is intended to be set in
    initialize_tty_struct from tty_init_dev (initial open). So this set in
    tty_reopen is not needed.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -1349,7 +1349,6 @@ static int tty_reopen(struct tty_struct *tty)
 		tty->link->count++;
 	}
 	tty->count++;
-	tty->driver = driver; /* N.B. why do this every time?? */
 
 	mutex_lock(&tty->ldisc_mutex);
 	WARN_ON(!test_bit(TTY_LDISC, &tty->flags));
