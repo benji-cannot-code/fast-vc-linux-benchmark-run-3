531f6abcab74806d796d9d5b470c73620a1f79e1
Jiri Slaby <jslaby@suse.cz>
2012-03-05 14:51:50 +0100

    TTY: let alloc_tty_driver deduce the owner automatically
    Like the rest of the kernel, make a stub from alloc_tty_driver which
    calls __alloc_tty_driver with proper owner. This will save us one more
    assignment on the driver side.
    
    Also this fixes some drivers which didn't set the owner. This allowed
    user to remove the module from the system even though a tty from the
    driver is still open.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty_driver.h
+++ include/linux/tty_driver.h
@@ -235,6 +235,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	if provided (otherwise EINVAL will be returned).
  */
 
+#include <linux/export.h>
 #include <linux/fs.h>
 #include <linux/list.h>
 #include <linux/cdev.h>
@@ -325,13 +326,15 @@ struct tty_driver {
 
 extern struct list_head tty_drivers;
 
-extern struct tty_driver *alloc_tty_driver(int lines);
+extern struct tty_driver *__alloc_tty_driver(int lines, struct module *owner);
 extern void put_tty_driver(struct tty_driver *driver);
 extern void tty_set_operations(struct tty_driver *driver,
 			const struct tty_operations *op);
 extern struct tty_driver *tty_find_polling_driver(char *name, int *line);
 
 extern void tty_driver_kref_put(struct tty_driver *driver);
+
+#define alloc_tty_driver(lines) __alloc_tty_driver(lines, THIS_MODULE)
 
 static inline struct tty_driver *tty_driver_kref_get(struct tty_driver *d)
 {
