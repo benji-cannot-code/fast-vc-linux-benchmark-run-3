9166f8a15a2447fc5511fbbf9a85d88a1ee39307
Jiri Slaby <jslaby@suse.cz>
2012-03-05 14:52:44 +0100

    TTY: amiserial, stop using serial_state->{irq,type,line}
    * instead of line, use tty->index or iterator...
    * irq and type are left unset. So get rid of them.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/serialP.h
+++ include/linux/serialP.h
@@ -30,9 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct serial_state {
 	int	baud_base;
 	unsigned long	port;
-	int	irq;
-	int	type;
-	int	line;
 	int	xmit_fifo_size;
 	int	custom_divisor;
 	struct async_icount	icount;	
