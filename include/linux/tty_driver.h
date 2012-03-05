86a29e35e706d317f383ea72a3176b2d9b4a7ff8
Jiri Slaby <jslaby@suse.cz>
2012-03-05 14:51:56 +0100

    TTY: tty_driver, document tty->ops->shutdown limitation
    Note that tty->ops->shutdown is called from whatever context the user
    drops the last tty reference from. E.g. if one takes a reference in
    an ISR, tty close happens on other CPU and the final tty put is from
    the ISR, tty->ops->shutdown will be called from that hard irq context.
    
    We would have a problem in vt if we start using tty refcounting from
    other contexts than user there. It is because vt's shutdown uses
    mutexes. This is yet to be fixed.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Reported-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty_driver.h
+++ include/linux/tty_driver.h
@@ -51,6 +51,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	Note that tty_shutdown() is not called if ops->shutdown is defined.
  *	This means one is responsible to take care of calling ops->remove (e.g.
  *	via tty_driver_remove_tty) and releasing tty->termios.
+ *	Note that this hook may be called from *all* the contexts where one
+ *	uses tty refcounting (e.g. tty_port_tty_get).
  *
  *
  * void (*cleanup)(struct tty_struct * tty);
