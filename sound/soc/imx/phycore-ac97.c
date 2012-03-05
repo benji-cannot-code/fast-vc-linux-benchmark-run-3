f4e3e0c89ae02e4a80fb1d0950916ad94ab41366
Shawn Guo <shawn.guo@linaro.org>
2012-03-05 22:30:50 +0800

    ASoC: imx: move phycore audmux call into ASoC machine driver
    It moves phycore audmux configuration call from board file into ASoC
    machine driver phycore-ac97 to ease converting audmux into a platform
    driver later.
    It moves phycore audmux configuration call from board file into ASoC
    machine driver phycore-ac97, so that it gets aligned with wm1133-ev1
    and mx27vis-aic32x4, and more importantly it will ease the moving of
    audmux into sound/soc/imx as a platform driver later.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/imx/phycore-ac97.c
+++ sound/soc/imx/phycore-ac97.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm.h>
 #include <sound/soc.h>
 #include <asm/mach-types.h>
+#include <mach/audmux.h>
 
 static struct snd_soc_card imx_phycore;
 
@@ -51,9 +52,32 @@ static int __init imx_phycore_init(void)
 {
 	int ret;
 
-	if (!machine_is_pcm043() && !machine_is_pca100())
+	if (machine_is_pca100()) {
+		mxc_audmux_v1_configure_port(MX27_AUDMUX_HPCR1_SSI0,
+			MXC_AUDMUX_V1_PCR_SYN | /* 4wire mode */
+			MXC_AUDMUX_V1_PCR_TFCSEL(3) |
+			MXC_AUDMUX_V1_PCR_TCLKDIR | /* clock is output */
+			MXC_AUDMUX_V1_PCR_RXDSEL(3));
+		mxc_audmux_v1_configure_port(3,
+			MXC_AUDMUX_V1_PCR_SYN | /* 4wire mode */
+			MXC_AUDMUX_V1_PCR_TFCSEL(0) |
+			MXC_AUDMUX_V1_PCR_TFSDIR |
+			MXC_AUDMUX_V1_PCR_RXDSEL(0));
+	} else if (machine_is_pcm043()) {
+		mxc_audmux_v2_configure_port(3,
+			MXC_AUDMUX_V2_PTCR_SYN | /* 4wire mode */
+			MXC_AUDMUX_V2_PTCR_TFSEL(0) |
+			MXC_AUDMUX_V2_PTCR_TFSDIR,
+			MXC_AUDMUX_V2_PDCR_RXDSEL(0));
+		mxc_audmux_v2_configure_port(0,
+			MXC_AUDMUX_V2_PTCR_SYN | /* 4wire mode */
+			MXC_AUDMUX_V2_PTCR_TCSEL(3) |
+			MXC_AUDMUX_V2_PTCR_TCLKDIR, /* clock is output */
+			MXC_AUDMUX_V2_PDCR_RXDSEL(3));
+	} else {
 		/* return happy. We might run on a totally different machine */
 		return 0;
+	}
 
 	imx_phycore_snd_ac97_device = platform_device_alloc("soc-audio", -1);
 	if (!imx_phycore_snd_ac97_device)
