f7995a779ddcad6af0ac0af04023688b2870d79b
Johannes Berg <johannes.berg@intel.com>
2012-03-05 11:24:26 -0800

    iwlwifi: introduce statistics lock
    The statistics are currently only half-heartedly
    locked against concurrent reading & modification
    so introduce a lock to really protect them.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn-rx.c
+++ drivers/net/wireless/iwlwifi/iwl-agn-rx.c
@@ -267,6 +267,8 @@ static bool iwlagn_good_ack_health(struct iwl_priv *priv,
 	if (priv->agg_tids_count)
 		return true;
 
+	lockdep_assert_held(&priv->statistics.lock);
+
 	old = &priv->statistics.tx;
 
 	actual_delta = le32_to_cpu(cur->actual_ack_cnt) -
@@ -510,6 +512,8 @@ static int iwlagn_rx_statistics(struct iwl_priv *priv,
 	IWL_DEBUG_RX(priv, "Statistics notification received (%d bytes).\n",
 		     len);
 
+	spin_lock(&priv->statistics.lock);
+
 	if (len == sizeof(struct iwl_bt_notif_statistics)) {
 		struct iwl_bt_notif_statistics *stats;
 		stats = &pkt->u.stats_bt;
@@ -543,6 +547,7 @@ static int iwlagn_rx_statistics(struct iwl_priv *priv,
 		WARN_ONCE(1, "len %d doesn't match BT (%zu) or normal (%zu)\n",
 			  len, sizeof(struct iwl_bt_notif_statistics),
 			  sizeof(struct iwl_notif_statistics));
+		spin_unlock(&priv->statistics.lock);
 		return 0;
 	}
 
@@ -586,6 +591,9 @@ static int iwlagn_rx_statistics(struct iwl_priv *priv,
 	}
 	if (cfg(priv)->lib->temperature && change)
 		cfg(priv)->lib->temperature(priv);
+
+	spin_unlock(&priv->statistics.lock);
+
 	return 0;
 }
 
