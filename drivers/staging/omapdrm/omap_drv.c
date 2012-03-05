9f58f6de8e2b26ba80a361e8f4439dc5bac32af0
Andy Gross <andy.gross@ti.com>
2012-03-05 10:48:39 -0600

    staging: drm/omap: Get DMM resources from hwmod
    This patch splits the DMM off into a separate sub-device, in order to
    utilize the platform device information that was created as part of
    the OMAP hwmod entry for the DMM.
    
    The driver probe function queries the iomem resource and IRQ using
    standard platform_get functions.
    
    The OMAP DRM driver now calls the platform_driver_register() function
    for the subordinate DMM driver inside its probe function.  This
    guarantees the required order for the DMM and ensures the DMM resources
    are available for use by the DRM driver.
    
    Signed-off-by: Andy Gross <andy.gross@ti.com>
    Signed-off-by: Rob Clark <rob@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/omapdrm/omap_drv.c
+++ drivers/staging/omapdrm/omap_drv.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "drm_crtc_helper.h"
 #include "drm_fb_helper.h"
+#include "omap_dmm_tiler.h"
 
 #define DRIVER_NAME		MODULE_NAME
 #define DRIVER_DESC		"OMAP DRM"
@@ -803,6 +804,9 @@ static void pdev_shutdown(struct platform_device *device)
 static int pdev_probe(struct platform_device *device)
 {
 	DBG("%s", device->name);
+	if (platform_driver_register(&omap_dmm_driver))
+		dev_err(&device->dev, "DMM registration failed\n");
+
 	return drm_platform_init(&omap_drm_driver, device);
 }
 
@@ -810,6 +814,8 @@ static int pdev_remove(struct platform_device *device)
 {
 	DBG("");
 	drm_platform_exit(&omap_drm_driver, device);
+
+	platform_driver_unregister(&omap_dmm_driver);
 	return 0;
 }
 
