462424e41238b27845c53e5b1ed263f3c5bd1386
Jiri Slaby <jslaby@suse.cz>
2012-03-05 14:52:10 +0100

    TTY: remove serialP.h inclusion from some files
    All of them do not use the ugly interface defined in that header.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/xtensa/platforms/iss/console.c
+++ arch/xtensa/platforms/iss/console.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/param.h>
 #include <linux/seq_file.h>
 #include <linux/serial.h>
-#include <linux/serialP.h>
 
 #include <asm/uaccess.h>
 #include <asm/irq.h>
--- drivers/tty/serial/m32r_sio.c
+++ drivers/tty/serial/m32r_sio.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #include <linux/sysrq.h>
 #include <linux/serial.h>
-#include <linux/serialP.h>
 #include <linux/delay.h>
 
 #include <asm/m32r.h>
