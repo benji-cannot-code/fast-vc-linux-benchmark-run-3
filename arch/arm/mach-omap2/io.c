0b4e5895de1b503f9983276e88afffb7149fcbe0
Tony Lindgren <tony@atomide.com>
2012-03-06 11:49:22 -0800

    ARM: OMAP2+: Fix omap24xx_io_desc warning if SoC subtypes are not selected
    Otherwise we get the following warning:
    
    arch/arm/mach-omap2/io.c:53:24: warning:
    'omap24xx_io_desc' defined but not used [-Wunused-variable]
    
    Reported-by: Russell King <linux@arm.linux.org.uk>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * default mapping provided here.
  */
 
-#ifdef CONFIG_ARCH_OMAP2
+#if defined(CONFIG_SOC_OMAP2420) || defined(CONFIG_SOC_OMAP2430)
 static struct map_desc omap24xx_io_desc[] __initdata = {
 	{
 		.virtual	= L3_24XX_VIRT,
