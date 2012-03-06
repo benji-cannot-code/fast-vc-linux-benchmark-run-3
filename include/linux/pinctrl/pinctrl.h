8a4032033ddf6a31493ae9973b72bf8b80a13d98
Linus Walleij <linus.walleij@linaro.org>
2012-03-06 21:15:51 +0100

    pinctrl: split pincontrol states into its own header
    Move the pin control state defines into its own header file,
    since it is used both by machine.h which is facing the platform
    and by consumer.h which is facing the drivers, and pinctrl.h
    which is pinctrl-driver internal, let's not have each and every
    .h file include all others, then isolation is moot.
    
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/pinctrl/pinctrl.h
+++ include/linux/pinctrl/pinctrl.h
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/radix-tree.h>
 #include <linux/list.h>
 #include <linux/seq_file.h>
-
-#define PINCTRL_STATE_DEFAULT "default"
+#include "pinctrl-state.h"
 
 struct pinctrl_dev;
 struct pinmux_ops;
