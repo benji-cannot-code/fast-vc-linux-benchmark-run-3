dd0ea47d36f9b019ba61c3c587ac9d1032ff14dd
Jason Cooper <jason@lakedaemon.net>
2012-03-06 23:55:04 +0000

    ARM: kirkwood: use devicetree for rtc-mv
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-kirkwood/common.h
+++ arch/arm/mach-kirkwood/common.h
@@ -62,7 +62,6 @@ static inline void dreamplug_init(void) {};
 /* early init functions not converted to fdt yet */
 char *kirkwood_id(void);
 void kirkwood_l2_init(void);
-void kirkwood_rtc_init(void);
 void kirkwood_wdt_init(void);
 void kirkwood_xor0_init(void);
 void kirkwood_xor1_init(void);
