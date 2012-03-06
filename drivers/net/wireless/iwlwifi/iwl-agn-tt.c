a79e4a423d92f81f12325bdcd33415c6e0959480
Johannes Berg <johannes.berg@intel.com>
2012-03-06 13:30:47 -0800

    iwlwifi: move status check functions out of shared
    They are only used in the DVM op_mode.
    Also move the rfkill debug macros that
    depend on them.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn-tt.c
+++ drivers/net/wireless/iwlwifi/iwl-agn-tt.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <net/mac80211.h>
 
+#include "iwl-agn.h"
 #include "iwl-eeprom.h"
 #include "iwl-dev.h"
 #include "iwl-core.h"
