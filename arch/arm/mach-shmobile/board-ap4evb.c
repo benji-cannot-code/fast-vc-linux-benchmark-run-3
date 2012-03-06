71dee3555f60bd02781cc904c64f91bec79f889b
Magnus Damm <damm@opensource.se>
2012-03-06 16:31:55 +0900

    ARM: mach-shmobile: Move sh7372 AP4EVB external clk setup
    Move the board specific external clock setting for sh7372 AP4EVB
    from machine_desc->sys_timer->init() to machine_desc->init_machine().
    
    This is ground work for shmobile timer code consolidation.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- arch/arm/mach-shmobile/board-ap4evb.c
+++ arch/arm/mach-shmobile/board-ap4evb.c
@@ -1198,6 +1198,9 @@ static void __init ap4evb_init(void)
 	u32 srcr4;
 	struct clk *clk;
 
+	/* External clock source */
+	clk_set_rate(&sh7372_dv_clki_clk, 27000000);
+
 	sh7372_pinmux_init();
 
 	/* enable SCIFA0 */
@@ -1438,9 +1441,6 @@ static void __init ap4evb_timer_init(void)
 {
 	sh7372_clock_init();
 	shmobile_timer.init();
-
-	/* External clock source */
-	clk_set_rate(&sh7372_dv_clki_clk, 27000000);
 }
 
 static struct sys_timer ap4evb_timer = {
