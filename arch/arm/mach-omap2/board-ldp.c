e01a0741832322cb250b6515c863ca4531dbe6b7
Danny Kukawka <danny.kukawka@bisect.de>
2012-03-05 16:11:03 -0800

    ARM: OMAP2+: included some headers twice
    arch/arm/mach-omap2/: included some headers tiwce:
    - arch/arm/mach-omap2/board-ldp.c: 'linux/gpio.h'
    - arch/arm/mach-omap2/io.c: 'common.h'
    - arch/arm/mach-omap2/omap_hwmod_44xx_data.c: 'plat/i2c.h'
    
    Remove the duplicates.
    
    Signed-off-by: Danny Kukawka <danny.kukawka@bisect.de>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-ldp.c
+++ arch/arm/mach-omap2/board-ldp.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/smsc911x.h>
 #include <linux/mmc/host.h>
-#include <linux/gpio.h>
 
 #include <mach/hardware.h>
 #include <asm/mach-types.h>
--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "clockdomain.h"
 #include <plat/omap_hwmod.h>
 #include <plat/multi.h>
-#include "common.h"
 
 /*
  * The machine specific code may provide the extra mapping besides the
