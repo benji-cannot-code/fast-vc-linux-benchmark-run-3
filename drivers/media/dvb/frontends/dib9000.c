f022774d79c125daff085408a8826f383efbd278
Alexey Khoroshilov <khoroshilov@ispras.ru>
2012-03-06 17:08:16 -0300

    [media] dib9000: fix explicit lock mismatches
    There are several error paths, where &state->platform.risc.mem_mbx_lock
    is not unlocked. The patch fixes it.
    
    Found by Linux Driver Verification project (linuxtesting.org).
    
    Signed-off-by: Alexey Khoroshilov <khoroshilov@ispras.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/dib9000.c
+++ drivers/media/dvb/frontends/dib9000.c
@@ -2209,6 +2209,7 @@ static int dib9000_read_signal_strength(struct dvb_frontend *fe, u16 * strength)
 
 	DibAcquireLock(&state->platform.risc.mem_mbx_lock);
 	if (dib9000_fw_memmbx_sync(state, FE_SYNC_CHANNEL) < 0) {
+		DibReleaseLock(&state->platform.risc.mem_mbx_lock);
 		ret = -EIO;
 		goto error;
 	}
@@ -2234,8 +2235,10 @@ static u32 dib9000_get_snr(struct dvb_frontend *fe)
 	u16 val;
 
 	DibAcquireLock(&state->platform.risc.mem_mbx_lock);
-	if (dib9000_fw_memmbx_sync(state, FE_SYNC_CHANNEL) < 0)
+	if (dib9000_fw_memmbx_sync(state, FE_SYNC_CHANNEL) < 0) {
+		DibReleaseLock(&state->platform.risc.mem_mbx_lock);
 		return -EIO;
+	}
 	dib9000_risc_mem_read(state, FE_MM_R_FE_MONITOR, (u8 *) c, 16 * 2);
 	DibReleaseLock(&state->platform.risc.mem_mbx_lock);
 
@@ -2292,6 +2295,7 @@ static int dib9000_read_unc_blocks(struct dvb_frontend *fe, u32 * unc)
 	DibAcquireLock(&state->demod_lock);
 	DibAcquireLock(&state->platform.risc.mem_mbx_lock);
 	if (dib9000_fw_memmbx_sync(state, FE_SYNC_CHANNEL) < 0) {
+		DibReleaseLock(&state->platform.risc.mem_mbx_lock);
 		ret = -EIO;
 		goto error;
 	}
