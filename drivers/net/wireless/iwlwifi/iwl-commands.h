977654a5f12591771bab74cd8f805e7cbbd489d7
Johannes Berg <johannes.berg@intel.com>
2012-03-06 13:31:07 -0800

    iwlwifi: clean up iwl-commands.h
    Do some cleanups here:
     * remove an unused prototype
     * remove some unused constants
     * clean up includes
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-commands.h
+++ drivers/net/wireless/iwlwifi/iwl-commands.h
@@ -70,15 +70,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_commands_h__
 #define __iwl_commands_h__
 
-#include <linux/etherdevice.h>
 #include <linux/ieee80211.h>
+#include <linux/types.h>
 
-struct iwl_priv;
-
-/* Tx rates */
-#define IWL_CCK_RATES	4
-#define IWL_OFDM_RATES	8
-#define IWL_MAX_RATES	(IWL_CCK_RATES + IWL_OFDM_RATES)
 
 enum {
 	REPLY_ALIVE = 0x1,
@@ -3851,8 +3845,6 @@ struct iwlagn_wowlan_kek_kck_material_cmd {
 	__le16	kek_len;
 	__le64	replay_ctr;
 } __packed;
-
-int iwl_agn_check_rxon_cmd(struct iwl_priv *priv);
 
 /*
  * REPLY_WIPAN_PARAMS = 0xb2 (Commands and Notification)
