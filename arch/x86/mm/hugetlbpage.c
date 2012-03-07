50e3b7f5eb7e99388ae63e9087d923e4901a83f1
Linus Torvalds <torvalds@linux-foundation.org>
2012-03-06 18:48:13 -0800

    x86: fix typo in recent find_vma_prev purge
    It turns out that test-compiling this file on x86-64 doesn't really
    help, because much of it is x86-32-specific.  And so I hadn't noticed
    the slightly over-eager removal of the 'r' from 'addr' variable despite
    thinking I had tested it.
    
    Signed-off-by: Linus "oopsie" Torvalds <torvalds@linux-foundation.org>

--- arch/x86/mm/hugetlbpage.c
+++ arch/x86/mm/hugetlbpage.c
@@ -334,7 +334,7 @@ try_again:
 		 * Lookup failure means no vma is above this address,
 		 * i.e. return with success:
 		 */
-		vma = find_vma(mm, add);
+		vma = find_vma(mm, addr);
 		if (!vma)
 			return addr;
 
