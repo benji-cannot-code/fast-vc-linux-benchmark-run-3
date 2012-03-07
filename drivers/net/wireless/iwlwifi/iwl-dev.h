bb3e950bfbeaaad2535257a73892459f4be6ab33
Johannes Berg <johannes.berg@intel.com>
2012-03-07 09:52:16 -0800

    iwlwifi: redesign PASSIVE_NO_RX workaround
    The PASSIVE_NO_RX workaround currently crosses
    through the op_mode and transport layers, which
    is a bit odd. This also isn't necessary, if the
    transport simply reports when queues are full
    (or no longer full) the op_mode can keep track
    of this state, and report to mac80211 only what
    *it* thinks is appropriate. What is appropriate
    can then be based on whether queues should be
    stopped to wait for RX or not.
    
    This significantly simplifies the transport API,
    it no longer needs to expose anything to stop a
    queue, nor to wake "any" queue, this can all be
    handled in the upper layer completely.
    
    Also simplify the handling to not be dependent
    on the context, that makes little sense as the
    queues are shared and both contexts have to be
    on the same channel anyway.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-dev.h
+++ drivers/net/wireless/iwlwifi/iwl-dev.h
@@ -671,8 +671,6 @@ struct iwl_rxon_context {
 		bool enabled, is_40mhz;
 		u8 extension_chan_offset;
 	} ht;
-
-	bool last_tx_rejected;
 };
 
 enum iwl_scan_type {
@@ -720,6 +718,9 @@ struct iwl_priv {
 
 	spinlock_t sta_lock;
 	struct mutex mutex;
+
+	unsigned long transport_queue_stop;
+	bool passive_no_rx;
 
 	/* ieee device used by generic ieee processing code */
 	struct ieee80211_hw *hw;
