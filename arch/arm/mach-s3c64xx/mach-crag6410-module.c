933866337fd54e379f9da8b0445c5fe4f16f199a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-03-08 11:55:07 +0000

    ARM: S3C64XX: Hook up supply for WM8994 LDOs on Littlemill
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- arch/arm/mach-s3c64xx/mach-crag6410-module.c
+++ arch/arm/mach-s3c64xx/mach-crag6410-module.c
@@ -137,6 +137,14 @@ static const struct i2c_board_info wm1259_devs[] = {
 	},
 };
 
+static struct regulator_init_data wm8994_ldo1 = {
+	.supply_regulator = "WALLVDD",
+};
+
+static struct regulator_init_data wm8994_ldo2 = {
+	.supply_regulator = "WALLVDD",
+};
+
 static struct wm8994_pdata wm8994_pdata = {
 	.gpio_base = CODEC_GPIO_BASE,
 	.gpio_defaults = {
@@ -144,8 +152,8 @@ static struct wm8994_pdata wm8994_pdata = {
 	},
 	.irq_base = CODEC_IRQ_BASE,
 	.ldo = {
-		 { },
-		 { },
+		 { .init_data = &wm8994_ldo1, },
+		 { .init_data = &wm8994_ldo2, },
 	},
 };
 
