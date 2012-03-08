ff2ff3311e1e9f8a9a46d3f5096b1888a5d3a341
Stephen Boyd <sboyd@codeaurora.org>
2012-03-08 11:41:14 -0800

    staging: ram_console: Fix section mismatches
    WARNING: vmlinux.o(.text+0xfcf6e): Section mismatch in reference
    from the function ram_console_driver_probe() to the function
    .init.text:persistent_ram_init_ringbuffer()
    The function ram_console_driver_probe() references
    the function __init persistent_ram_init_ringbuffer().
    This is often because ram_console_driver_probe lacks a __init
    annotation or the annotation of persistent_ram_init_ringbuffer is
    wrong.
    
    Move this driver to platform_driver_probe() because ram console
    devices aren't going to be added and removed at runtime. Also
    shorten the probe function name since driver is redundant and
    makes the function name long.
    
    Cc: Android Kernel Team <kernel-team@android.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ram_console.c
+++ drivers/staging/android/ram_console.c
@@ -51,7 +51,7 @@ void ram_console_enable_console(int enabled)
 		ram_console.flags &= ~CON_ENABLED;
 }
 
-static int ram_console_driver_probe(struct platform_device *pdev)
+static int __init ram_console_probe(struct platform_device *pdev)
 {
 	struct ram_console_platform_data *pdata = pdev->dev.platform_data;
 	struct persistent_ram_zone *prz;
@@ -76,7 +76,6 @@ static int ram_console_driver_probe(struct platform_device *pdev)
 }
 
 static struct platform_driver ram_console_driver = {
-	.probe = ram_console_driver_probe,
 	.driver		= {
 		.name	= "ram_console",
 	},
@@ -84,9 +83,7 @@ static struct platform_driver ram_console_driver = {
 
 static int __init ram_console_module_init(void)
 {
-	int err;
-	err = platform_driver_register(&ram_console_driver);
-	return err;
+	return platform_driver_probe(&ram_console_driver, ram_console_probe);
 }
 
 static ssize_t ram_console_read_old(struct file *file, char __user *buf,
