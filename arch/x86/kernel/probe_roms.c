23034b4bb4ba3f1c304a2533b3a77e0ebd15c219
Jan Beulich <JBeulich@suse.com>
2012-03-08 09:24:57 +0000

    x86: Include probe_roms.h in probe_roms.c
    ... to ensure that declarations and definitions are in sync.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Link: http://lkml.kernel.org/r/4F5888F902000078000770F1@nat28.tlf.novell.com
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/probe_roms.c
+++ arch/x86/kernel/probe_roms.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/export.h>
 
+#include <asm/probe_roms.h>
 #include <asm/pci-direct.h>
 #include <asm/e820.h>
 #include <asm/mmzone.h>
