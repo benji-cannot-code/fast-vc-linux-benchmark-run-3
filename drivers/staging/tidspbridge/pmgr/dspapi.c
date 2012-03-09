dcdb44199f12730a3ef95d5422a2d9e9d45ce5ae
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2012-03-09 01:03:41 +0100

    staging: tidspbridge: remove strm_init() and strm_exit()
    The strm module has a strm_init() and a strm_exit() whose only purpose
    is to keep a reference counting which is not used at all.
    
    This patch removes these functions and the reference count variable.
    
    There is no functional changes.
    
    Signed-off-by: Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/tidspbridge/pmgr/dspapi.c
+++ drivers/staging/tidspbridge/pmgr/dspapi.c
@@ -272,7 +272,6 @@ void api_exit(void)
 		chnl_exit();
 		msg_exit();
 		io_exit();
-		strm_exit();
 		mgr_exit();
 		rmm_exit();
 	}
@@ -287,12 +286,11 @@ bool api_init(void)
 {
 	bool ret = true;
 	bool fdev, fchnl, fmsg, fio;
-	bool fmgr, fstrm, frmm;
+	bool fmgr, frmm;
 
 	if (api_c_refs == 0) {
 		/* initialize driver and other modules */
 		fmgr = mgr_init();
-		fstrm = strm_init();
 		frmm = rmm_init();
 		fchnl = chnl_init();
 		fmsg = msg_mod_init();
@@ -304,9 +302,6 @@ bool api_init(void)
 
 			if (fmgr)
 				mgr_exit();
-
-			if (fstrm)
-				strm_exit();
 
 			if (fchnl)
 				chnl_exit();
