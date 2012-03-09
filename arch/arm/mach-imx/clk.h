ff7e840dfb0ac83e7e4dae60f792d83396f7e12e
Sascha Hauer <s.hauer@pengutronix.de>
2012-03-09 09:11:55 +0100

    ARM i.MX: Add common clock support for pllv1
    The pllv1 is found on i.MX1, i.M25, i.MX27, i.MX31 and i.MX35.
    Currently only reading the rate is supported.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/clk.h
+++ arch/arm/mach-imx/clk.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk-provider.h>
 #include <mach/clock.h>
 
-struct clk *imx_clk_pllv1(const char *name, char *parent,
+struct clk *imx_clk_pllv1(const char *name, const char *parent,
 		void __iomem *base);
 
 static inline struct clk *imx_clk_fixed(const char *name, int rate)
