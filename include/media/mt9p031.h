4b46879c554fb8ae38bd4aaa2e0746bc8d5ccd84
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2012-03-09 10:59:41 -0300

    [media] mt9p031: Replace the reset board callback by a GPIO number
    Use the GPIO from the sensor driver instead of calling back to board
    code.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/mt9p031.h
+++ include/media/mt9p031.h
@@ -4,11 +4,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct v4l2_subdev;
 
+/*
+ * struct mt9p031_platform_data - MT9P031 platform data
+ * @set_xclk: Clock frequency set callback
+ * @reset: Chip reset GPIO (set to -1 if not used)
+ * @ext_freq: Input clock frequency
+ * @target_freq: Pixel clock frequency
+ */
 struct mt9p031_platform_data {
 	int (*set_xclk)(struct v4l2_subdev *subdev, int hz);
-	int (*reset)(struct v4l2_subdev *subdev, int active);
-	int ext_freq; /* input frequency to the mt9p031 for PLL dividers */
-	int target_freq; /* frequency target for the PLL */
+	int reset;
+	int ext_freq;
+	int target_freq;
 };
 
 #endif
