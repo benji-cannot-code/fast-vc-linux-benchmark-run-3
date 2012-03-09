134e56974b6c6437564ca241cdb4381e8b1763b7
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2012-03-09 01:03:45 +0100

    staging: tidspbridge: remove io_init() and io_exit()
    The io module has a io_init() and a io_exit() whose only purpose is to
    keep a reference counting which is not used at all.
    
    This patch removes these functions and the reference count variable.
    
    There is no functional changes.
    
    Signed-off-by: Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/tidspbridge/pmgr/io.c
+++ drivers/staging/tidspbridge/pmgr/io.c
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <ioobj.h>
 #include <dspbridge/io.h>
 
-/*  ----------------------------------- Globals */
-static u32 refs;
-
 /*
  *  ======== io_create ========
  *  Purpose:
@@ -94,29 +91,4 @@ int io_destroy(struct io_mgr *hio_mgr)
 	status = (*intf_fxns->io_destroy) (hio_mgr);
 
 	return status;
-}
-
-/*
- *  ======== io_exit ========
- *  Purpose:
- *      Discontinue usage of the IO module.
- */
-void io_exit(void)
-{
-	refs--;
-}
-
-/*
- *  ======== io_init ========
- *  Purpose:
- *      Initialize the IO module's private state.
- */
-bool io_init(void)
-{
-	bool ret = true;
-
-	if (ret)
-		refs++;
-
-	return ret;
 }
