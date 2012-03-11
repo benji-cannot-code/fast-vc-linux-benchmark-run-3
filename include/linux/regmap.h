b8a369273698453324a95c891d9966bbc9ea258e
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-03-11 11:49:17 +0000

    regmap: Rejig struct declarations for stubbed API
    Ensure we have a forward declaration of struct regmap that isn't just
    the return value of regmap_init() and make the definition of the
    register defaults available.
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/regmap.h
+++ include/linux/regmap.h
@@ -17,11 +17,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/list.h>
 
-#ifdef CONFIG_REGMAP
-
 struct module;
 struct i2c_client;
 struct spi_device;
+struct regmap;
 
 /* An enum of all the supported cache types */
 enum regcache_type {
@@ -42,6 +41,8 @@ struct reg_default {
 	unsigned int reg;
 	unsigned int def;
 };
+
+#ifdef CONFIG_REGMAP
 
 /**
  * Configuration for the register map of a device.
