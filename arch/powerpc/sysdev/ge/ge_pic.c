55d9895b510cfcd688a83906725f3244c02ee2ed
Martyn Welch <martyn.welch@ge.com>
2012-03-12 17:12:59 +0000

    powerpc: Move GE PIC drivers
    Move the GE PIC drivers to allow these to be used by non-86xx boards.
    
    Signed-off-by: Martyn Welch <martyn.welch@ge.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/sysdev/ge/ge_pic.c
+++ arch/powerpc/sysdev/ge/ge_pic.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/prom.h>
 #include <asm/irq.h>
 
-#include "gef_pic.h"
+#include "ge_pic.h"
 
 #define DEBUG
 #undef DEBUG
