32d0870a2a674fd770e9136e3b247fe4c099a2ff
Vinod Koul <vinod.koul@linux.intel.com>
2012-03-13 11:58:12 +0530

    dmaengine: fix for cookie changes and merge
    Fixed trivial issues in drivers:
    	drivers/dma/imx-sdma.c
    	drivers/dma/intel_mid_dma.c
    	drivers/dma/ioat/dma_v3.c
    	drivers/dma/iop-adma.c
    	drivers/dma/sirf-dma.c
    	drivers/dma/timb_dma.c
    
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- drivers/dma/sirf-dma.c
+++ drivers/dma/sirf-dma.c
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_platform.h>
 #include <linux/sirfsoc_dma.h>
 
+#include "dmaengine.h"
+
 #define SIRFSOC_DMA_DESCRIPTORS                 16
 #define SIRFSOC_DMA_CHANNELS                    16
 
