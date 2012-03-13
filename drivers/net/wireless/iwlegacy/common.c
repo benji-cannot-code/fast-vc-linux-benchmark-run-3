b3369d165eed1f509d792eebecf545392399247e
Stanislaw Gruszka <sgruszka@redhat.com>
2012-03-13 16:11:27 +0100

    iwlegacy: do not nulify il->vif on reset
    This il->vif is dereferenced in different part of iwlegacy code, so do
    not nullify it. This should fix random crashes observed in companion
    with microcode errors i.e. crash in il3945_config_ap().
    
    Additionally this should address also
    WARNING: at drivers/net/wireless/iwlegacy/common.c:4656 il_mac_remove_interface
    at least one of the possible reasons of that warning.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlegacy/common.c
+++ drivers/net/wireless/iwlegacy/common.c
@@ -4509,6 +4509,7 @@ il_mac_add_interface(struct ieee80211_hw *hw, struct ieee80211_vif *vif)
 {
 	struct il_priv *il = hw->priv;
 	int err;
+	bool reset;
 
 	mutex_lock(&il->mutex);
 	D_MAC80211("enter: type %d, addr %pM\n", vif->type, vif->addr);
@@ -4519,7 +4520,12 @@ il_mac_add_interface(struct ieee80211_hw *hw, struct ieee80211_vif *vif)
 		goto out;
 	}
 
-	if (il->vif) {
+	/*
+	 * We do not support multiple virtual interfaces, but on hardware reset
+	 * we have to add the same interface again.
+	 */
+	reset = (il->vif == vif);
+	if (il->vif && !reset) {
 		err = -EOPNOTSUPP;
 		goto out;
 	}
@@ -4529,8 +4535,11 @@ il_mac_add_interface(struct ieee80211_hw *hw, struct ieee80211_vif *vif)
 
 	err = il_set_mode(il);
 	if (err) {
-		il->vif = NULL;
-		il->iw_mode = NL80211_IFTYPE_STATION;
+		IL_WARN("Fail to set mode %d\n", vif->type);
+		if (!reset) {
+			il->vif = NULL;
+			il->iw_mode = NL80211_IFTYPE_STATION;
+		}
 	}
 
 out:
