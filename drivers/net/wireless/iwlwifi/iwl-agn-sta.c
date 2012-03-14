77e6dcb7e46343b60aad576555ba839505e74e18
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2012-03-14 15:59:15 -0700

    iwlwifi: move iwl_bcast_addr to iwl-agn-sta.c
    Move this as part of iwl-core.c cleanup.
    
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-agn-sta.c
+++ drivers/net/wireless/iwlwifi/iwl-agn-sta.c
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-agn.h"
 #include "iwl-trans.h"
 
+const u8 iwl_bcast_addr[ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
 static int iwl_sta_ucode_activate(struct iwl_priv *priv, u8 sta_id)
 {
 	lockdep_assert_held(&priv->sta_lock);
--- drivers/net/wireless/iwlwifi/iwl-core.c
+++ drivers/net/wireless/iwlwifi/iwl-core.c
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-agn.h"
 #include "iwl-trans.h"
 
-const u8 iwl_bcast_addr[ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-
 
 
 #ifdef CONFIG_IWLWIFI_DEBUGFS
