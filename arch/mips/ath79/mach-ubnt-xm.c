e90b4682ebc72588b984f38c7806dfab5b55fb59
Gabor Juhos <juhosg@openwrt.org>
2012-03-14 10:36:06 +0100

    MIPS: ath79: fix a wrong IRQ number
    The Ubiquiti XM board setup code uses an invalid
    IRQ number, because it if above of NR_IRQS. This
    leads to failed 'request_irq' calls:
    
      ath9k 0000:00:00.0: request_irq failed
      ath9k: probe of 0000:00:00.0 failed with error -22
    
    Preserve some IRQ numbers for the built-in IRQ
    controller of PCI host controllers in the
    AR71XX/AR724X SoCs, and use the correct IRQ
    number in the board setup code.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/3495/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ath79/mach-ubnt-xm.c
+++ arch/mips/ath79/mach-ubnt-xm.c
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ath9k_platform.h>
 #endif /* CONFIG_PCI */
 
+#include <asm/mach-ath79/irq.h>
+
 #include "machtypes.h"
 #include "dev-gpio-buttons.h"
 #include "dev-leds-gpio.h"
@@ -34,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define UBNT_XM_KEYS_POLL_INTERVAL	20
 #define UBNT_XM_KEYS_DEBOUNCE_INTERVAL	(3 * UBNT_XM_KEYS_POLL_INTERVAL)
 
-#define UBNT_XM_PCI_IRQ			48
 #define UBNT_XM_EEPROM_ADDR		(u8 *) KSEG1ADDR(0x1fff1000)
 
 static struct gpio_led ubnt_xm_leds_gpio[] __initdata = {
@@ -87,7 +88,7 @@ static struct ath9k_platform_data ubnt_xm_eeprom_data;
 
 static struct ar724x_pci_data ubnt_xm_pci_data[] = {
 	{
-		.irq	= UBNT_XM_PCI_IRQ,
+		.irq	= ATH79_PCI_IRQ(0),
 		.pdata	= &ubnt_xm_eeprom_data,
 	},
 };
