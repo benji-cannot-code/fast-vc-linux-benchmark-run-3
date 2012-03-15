8a2a05924be839eaf1cf51444501b317bf441ef3
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2012-03-15 22:11:51 +0200

    crypto: camellia-x86_64 - module init/exit functions should be static
    This caused conflict with twofish-x86_64-3way when compiled into kernel,
    same function names and not static.
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
    Acked-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/x86/crypto/camellia_glue.c
+++ arch/x86/crypto/camellia_glue.c
@@ -1926,7 +1926,7 @@ static int force;
 module_param(force, int, 0);
 MODULE_PARM_DESC(force, "Force module load, ignore CPU blacklist");
 
-int __init init(void)
+static int __init init(void)
 {
 	if (!force && is_blacklisted_cpu()) {
 		printk(KERN_INFO
@@ -1939,7 +1939,7 @@ int __init init(void)
 	return crypto_register_algs(camellia_algs, ARRAY_SIZE(camellia_algs));
 }
 
-void __exit fini(void)
+static void __exit fini(void)
 {
 	crypto_unregister_algs(camellia_algs, ARRAY_SIZE(camellia_algs));
 }
