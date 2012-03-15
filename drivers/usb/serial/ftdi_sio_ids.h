a494a42151934760bc9960112d8b457190746a4f
Michał Wróbel <michal.wrobel@flytronic.pl>
2012-03-15 17:24:04 +0100

    USB: ftdi_sio: new PID: LUMEL PD12
    Signed-off-by: Michał Wróbel <michal.wrobel@flytronic.pl>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*** third-party PIDs (using FTDI_VID) ***/
 
+#define FTDI_LUMEL_PD12_PID	0x6002
+
 /*
  * Marvell OpenRD Base, Client
  * http://www.open-rd.org
