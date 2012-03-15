2447bf02a532814d7fbd151305844a0a861ea298
Jiri Olsa <jolsa@redhat.com>
2012-03-15 20:09:17 +0100

    perf tools: Add perf pmu object to access pmu format definition
    Adding pmu object which provides interface to pmu's sysfs
    event format definition located at:
      ${sysfs_mount}/bus/event_source/devices/${pmu}/format
    
    Following interface is exported:
      struct perf_pmu* perf_pmu__find(char *name);
      - this function returns pmu object, which is then
        passed as a handle to other interface functions
    
      int perf_pmu__config(struct perf_pmu *pmu, struct perf_event_attr *attr,
                           struct list_head *head_terms);
      - this function configures perf_event_attr struct based
        on pmu's format definitions and config terms data,
        containined in head_terms list.
    
    Parser generator is used to retrive the pmu's format definition.
    The generated parser is part of the patch. Added makefile rule
    'pmu-parser' to generate the parser code out of the bison/flex
    sources.
    
    Added builtin test 'Test perf pmu format parsing', which could
    be run like:
    	perf test pmu
    
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/n/tip-errz96u1668gj9wlop1zhpht@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-test.c
+++ tools/perf/builtin-test.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "util/parse-events.h"
 #include "util/symbol.h"
 #include "util/thread_map.h"
+#include "util/pmu.h"
 #include "../../include/linux/hw_breakpoint.h"
 
 #include <sys/mman.h>
@@ -1485,6 +1486,11 @@ static int test__rdpmc(void)
 
 #endif
 
+static int test__perf_pmu(void)
+{
+	return perf_pmu__test();
+}
+
 static struct test {
 	const char *desc;
 	int (*func)(void);
@@ -1518,6 +1524,10 @@ static struct test {
 	{
 		.desc = "Validate PERF_RECORD_* events & perf_sample fields",
 		.func = test__PERF_RECORD,
+	},
+	{
+		.desc = "Test perf pmu format parsing",
+		.func = test__perf_pmu,
 	},
 	{
 		.func = NULL,
