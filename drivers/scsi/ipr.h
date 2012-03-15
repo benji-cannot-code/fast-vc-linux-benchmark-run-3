7977d3c537df1665d1a05c2a4dbfd9856c3acbfc
Brian King <brking@linux.vnet.ibm.com>
2012-03-14 21:20:08 -0500

    [SCSI] ipr: Remove unnecessary interrupt clearing on new adapters
    The latest ipr hardware no longer requires the driver to issue any MMIOs
    to clear the interrupt so remove this to optimize performance.
    
    Signed-off-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/ipr.h
+++ drivers/scsi/ipr.h
@@ -1307,6 +1307,7 @@ struct ipr_interrupts {
 struct ipr_chip_cfg_t {
 	u32 mailbox;
 	u8 cache_line_size;
+	u8 clear_isr;
 	struct ipr_interrupt_offsets regs;
 };
 
@@ -1389,6 +1390,7 @@ struct ipr_ioa_cfg {
 	u8 sis64:1;
 	u8 dump_timeout:1;
 	u8 cfg_locked:1;
+	u8 clear_isr:1;
 
 	u8 revid;
 
