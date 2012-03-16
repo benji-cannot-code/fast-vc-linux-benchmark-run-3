caabb8e3493aef8847923d327243a418a2dc51ce
Stephen Warren <swarren@wwwdotorg.org>
2012-03-16 14:54:23 -0600

    pinctrl: include <linux/bug.h> to prevent compile errors
    Macros in <linux/pinctrl/machine.h> call ARRAY_SIZE(), the definition of
    which eventually calls BUILD_BUG_ON_ZERO(), which is defined in
    <linux/bug.h>. Include that so that every .c file using the pinctrl macros
    doesn't have to do that itself.
    
    Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/pinctrl/machine.h
+++ include/linux/pinctrl/machine.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_PINCTRL_MACHINE_H
 #define __LINUX_PINCTRL_MACHINE_H
 
+#include <linux/bug.h>
+
 #include "pinctrl-state.h"
 
 enum pinctrl_map_type {
