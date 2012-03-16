6df2d0ac4cd4828c1b231801217155bf387e6700
Richard Cochran <richardcochran@gmail.com>
2012-03-16 10:55:32 +0000

    igb: add PTP Hardware Clock code
    This patch adds a source file implementing a PHC. Only the basic
    clock operations have been implemented, although the hardware
    would offer some ancillary functions. The code is fairly self
    contained and is not yet used in the main igb driver.
    
    Every timestamp and clock read operation must consult the overflow
    counter to form a correct time value. Access to the counter is
    protected by a spin lock, and the counter is implemented using the
    standard cyclecounter/timecounter code.
    
    Signed-off-by: Richard Cochran <richardcochran@gmail.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igb/igb.h
+++ drivers/net/ethernet/intel/igb/igb.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clocksource.h>
 #include <linux/timecompare.h>
 #include <linux/net_tstamp.h>
+#include <linux/ptp_clock_kernel.h>
 #include <linux/bitops.h>
 #include <linux/if_vlan.h>
 
@@ -365,6 +366,13 @@ struct igb_adapter {
 	u32 wvbr;
 	int node;
 	u32 *shadow_vfta;
+
+	struct ptp_clock *ptp_clock;
+	struct ptp_clock_info caps;
+	struct delayed_work overflow_work;
+	spinlock_t tmreg_lock;
+	struct cyclecounter cc;
+	struct timecounter tc;
 };
 
 #define IGB_FLAG_HAS_MSI           (1 << 0)
