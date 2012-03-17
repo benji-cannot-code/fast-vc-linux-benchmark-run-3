9c3f34c1c54704f634cc5b4f1dca0f5647d2eb49
Al Viro <viro@zeniv.linux.org.uk>
2012-03-17 02:17:32 -0400

    usbfs: kill racy detection of simple_pin_fs()
    can check MS_KERNMOUNT in flags now
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/usb/core/inode.c
+++ drivers/usb/core/inode.c
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const struct file_operations default_file_operations;
 static struct vfsmount *usbfs_mount;
 static int usbfs_mount_count;	/* = 0 */
-static int ignore_mount = 0;
 
 static struct dentry *devices_usbfs_dentry;
 static int num_buses;	/* = 0 */
@@ -257,7 +256,7 @@ static int remount(struct super_block *sb, int *flags, char *data)
 	 * i.e. it's a simple_pin_fs from create_special_files,
 	 * then ignore it.
 	 */
-	if (ignore_mount)
+	if (*flags & MS_KERNMOUNT)
 		return 0;
 
 	if (parse_options(sb, data)) {
@@ -583,19 +582,12 @@ static int create_special_files (void)
 	struct dentry *parent;
 	int retval;
 
-	/* the simple_pin_fs calls will call remount with no options
-	 * without this flag that would overwrite the real mount options (if any)
-	 */
-	ignore_mount = 1;
-
 	/* create the devices special file */
 	retval = simple_pin_fs(&usb_fs_type, &usbfs_mount, &usbfs_mount_count);
 	if (retval) {
 		printk(KERN_ERR "Unable to get usbfs mount\n");
 		goto exit;
 	}
-
-	ignore_mount = 0;
 
 	parent = usbfs_mount->mnt_root;
 	devices_usbfs_dentry = fs_create_file ("devices",
