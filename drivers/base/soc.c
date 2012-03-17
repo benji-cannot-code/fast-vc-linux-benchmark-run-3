427ef68c96d9c7e714b22c37e130907cebfb09b5
Lee Jones <lee.jones@linaro.org>
2012-03-17 09:17:49 +0000

    drivers/base: fix compiler warning in SoC export driver - idr should be ida
    This fixes:
      note: expected ‘struct ida *’ but argument is of type ‘struct idr *’
      warning: passing argument 1 of ‘ida_pre_get’ from incompatible pointer type
    
    Reported-by: Arnd Bergman <arnd@arndb.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/soc.c
+++ drivers/base/soc.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sys_soc.h>
 #include <linux/err.h>
 
-static DEFINE_IDR(soc_ida);
+static DEFINE_IDA(soc_ida);
 static DEFINE_SPINLOCK(soc_lock);
 
 static ssize_t soc_info_get(struct device *dev,
