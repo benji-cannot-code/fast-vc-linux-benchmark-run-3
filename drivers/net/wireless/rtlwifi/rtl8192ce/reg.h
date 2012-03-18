aa3947d0209ecacc458e96424e6b795b19bdd774
Joe Perches <joe@perches.com>
2012-03-18 17:37:59 +0000

    rtlwifi: Remove unused ETH_ADDR_LEN defines
    Just neatening.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/rtlwifi/rtl8192ce/reg.h
+++ drivers/net/wireless/rtlwifi/rtl8192ce/reg.h
@@ -1191,7 +1191,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define USB_AGG_EN				BIT(3)
 
-#define MAC_ADDR_LEN				6
 #define LAST_ENTRY_OF_TX_PKT_BUFFER		255
 
 #define POLLING_LLT_THRESHOLD			20
