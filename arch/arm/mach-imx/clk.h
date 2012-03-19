85f6f32d8dedcdb235ea4dd861bc312b3556041a
Sascha Hauer <s.hauer@pengutronix.de>
2012-03-19 12:36:10 +0100

    ARM i.MX: Add common clock support for pllv2
    This PLL is found on i.MX51 and i.MX53
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/clk.h
+++ arch/arm/mach-imx/clk.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct clk *imx_clk_pllv1(const char *name, const char *parent,
 		void __iomem *base);
 
+struct clk *imx_clk_pllv2(const char *name, const char *parent,
+		void __iomem *base);
+
 static inline struct clk *imx_clk_fixed(const char *name, int rate)
 {
 	return clk_register_fixed_rate(NULL, name, NULL, CLK_IS_ROOT, rate);
