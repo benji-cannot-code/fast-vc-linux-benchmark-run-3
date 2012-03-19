87aa82d5d093046e962c8fd4b1383f536d2d8909
Antti Palosaari <crope@iki.fi>
2012-03-19 11:48:19 -0300

    [media] em28xx: support for 2013:0251 PCTV QuatroStick nano (520e)
    Hardware is based of:
    Empia EM2884
    Micronas DRX 3926K
    NXP TDA18271HDC2
    
    ... + analog parts.
    Analog is not supported currently. Only DVB-T and DVB-C.
    
    There seems to be still	problems for locking DVB-C channels which have
    strong signal. Attenuator helps. I think it is demodulator IF/RF AGC
    issue. Lets fix it later. Patches are welcome.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/em28xx/em28xx.h
+++ drivers/media/video/em28xx/em28xx.h
@@ -127,6 +127,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2884_BOARD_CINERGY_HTC_STICK		  82
 #define EM2860_BOARD_HT_VIDBOX_NW03 		  83
 #define EM2874_BOARD_MAXMEDIA_UB425_TC            84
+#define EM2884_BOARD_PCTV_520E                    85
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
