61b03193d1ba756f1a4cf08011b0c153507741f6
David Rientjes <rientjes@google.com>
2012-03-20 09:53:06 +0100

    drivers, samsung-laptop: fix usage of isalnum
    linux/ctype.h is needed for isalnum() to avoid a build error:
    
    drivers/platform/x86/samsung-laptop.c: In function ‘samsung_sabi_diag’:
    drivers/platform/x86/samsung-laptop.c:1306: error: implicit declaration of function ‘isalnum’
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>

--- drivers/platform/x86/samsung-laptop.c
+++ drivers/platform/x86/samsung-laptop.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/acpi.h>
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
+#include <linux/ctype.h>
 
 /*
  * This driver is needed because a number of Samsung laptops do not hook
