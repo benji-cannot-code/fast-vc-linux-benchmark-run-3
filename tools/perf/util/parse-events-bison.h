6981f35b5121ca51fbd64a1be858a92ce8118037
Jiri Olsa <jolsa@redhat.com>
2012-03-20 19:15:40 +0100

    perf tools: Fix modifier to be applied on correct events
    The event modifier needs to be applied only on the event definition it
    is attached to.
    
    The current state is that in case of multiple events definition (in
    single '-e' option, separated by ',') all will get modifier of the last
    one.
    
    Fixing this by adding separated list for each event definition, so the
    modifier is applied only to proper event(s). Added automated test to
    catch this, plus some other modifier tests.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1332267341-26338-3-git-send-email-jolsa@redhat.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/parse-events-bison.h
+++ tools/perf/util/parse-events-bison.h
@@ -1,10 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* A Bison parser, made by GNU Bison 2.4.3.  */
+/* A Bison parser, made by GNU Bison 2.5.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
-   2009, 2010 Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -60,8 +59,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 typedef union YYSTYPE
 {
 
-/* Line 1685 of yacc.c  */
-#line 45 "util/parse-events.y"
+/* Line 2068 of yacc.c  */
+#line 46 "util/parse-events.y"
 
 	char *str;
 	unsigned long num;
@@ -70,8 +69,8 @@ typedef union YYSTYPE
 
 
 
-/* Line 1685 of yacc.c  */
-#line 74 "util/parse-events-bison.h"
+/* Line 2068 of yacc.c  */
+#line 73 "util/parse-events-bison.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
