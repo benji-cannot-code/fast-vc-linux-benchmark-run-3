9f6f45d269f90f876c1405ff36a4e5f782b7e041
Don Fry <donald.h.fry@intel.com>
2012-03-20 16:36:54 -0700

    iwlwifi: move HCMD_ACTIVE to trans
    The HCMD_ACTIVE bit is only used in trans.
    
    Signed-off-by: Don Fry <donald.h.fry@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-shared.h
+++ drivers/net/wireless/iwlwifi/iwl-shared.h
@@ -151,7 +151,6 @@ struct iwl_mod_params {
 /**
  * struct iwl_shared - shared fields for all the layers of the driver
  *
- * @status: STATUS_*
  * @wowlan: are we running wowlan uCode
  * @bus: pointer to the bus layer data
  * @cfg: see struct iwl_cfg
@@ -162,8 +161,6 @@ struct iwl_mod_params {
  * @eeprom: pointer to the eeprom/OTP image
  */
 struct iwl_shared {
-	unsigned long status;
-
 	const struct iwl_cfg *cfg;
 	struct iwl_trans *trans;
 	void *drv;
