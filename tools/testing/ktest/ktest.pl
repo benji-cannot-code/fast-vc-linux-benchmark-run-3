21c7233671568b9776af6d556abb894db019c396
Steven Rostedt <srostedt@redhat.com>
2012-03-21 08:18:35 -0400

    ktest: Add warning when bugs are ignored
    When IGNORE_ERRORS is set, ktest will not fail a test if a backtrace
    is detected. But this can be an issue if the user added it in the
    config but forgot to remove it. They may be left wondering why their
    test did not fail, or even worse, why their bisect gave the wrong
    commit.
    
    Add a warning in the output if IGNORE_WARNINGS is set, and ktest detects
    a kernel error.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -1461,6 +1461,7 @@ sub get_sha1 {
 sub monitor {
     my $booted = 0;
     my $bug = 0;
+    my $bug_ignored = 0;
     my $skip_call_trace = 0;
     my $loops;
 
@@ -1532,9 +1533,13 @@ sub monitor {
 	}
 
 	if ($full_line =~ /call trace:/i) {
-	    if (!$ignore_errors && !$bug && !$skip_call_trace) {
-		$bug = 1;
-		$failure_start = time;
+	    if (!$bug && !$skip_call_trace) {
+		if ($ignore_errors) {
+		    $bug_ignored = 1;
+		} else {
+		    $bug = 1;
+		    $failure_start = time;
+		}
 	    }
 	}
 
@@ -1594,6 +1599,10 @@ sub monitor {
     if (!$booted) {
 	return 0 if ($in_bisect);
 	fail "failed - never got a boot prompt." and return 0;
+    }
+
+    if ($bug_ignored) {
+	doprint "WARNING: Call Trace detected but ignored due to IGNORE_ERRORS=1\n";
     }
 
     return 1;
