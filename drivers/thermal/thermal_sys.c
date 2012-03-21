b455a27a462d84b1231db04945140bc150cb579c
Fabio Estevam <festevam@gmail.com>
2012-03-21 12:55:00 -0700

    drivers/thermal/thermal_sys.c: fix build warning
    With CONFIG_NET=n:
    
    drivers/thermal/thermal_sys.c:63: warning: 'thermal_event_seqnum' defined but not used
    
    Move 'thermal_event_seqnum' definition inside the '#ifdef CONFIG_NET'
    
    [akpm@linux-foundation.org: make thermal_event_seqnum local to generate_netlink_event()]
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Guenter Roeck <guenter.roeck@ericsson.com>
    Acked-by: Durgadoss R <durgadoss.r@intel.com>
    Cc: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/thermal/thermal_sys.c
+++ drivers/thermal/thermal_sys.c
@@ -61,8 +61,6 @@ static LIST_HEAD(thermal_tz_list);
 static LIST_HEAD(thermal_cdev_list);
 static DEFINE_MUTEX(thermal_list_lock);
 
-static unsigned int thermal_event_seqnum;
-
 static int get_idr(struct idr *idr, struct mutex *lock, int *id)
 {
 	int err;
@@ -1313,6 +1311,7 @@ int thermal_generate_netlink_event(u32 orig, enum events event)
 	void *msg_header;
 	int size;
 	int result;
+	static unsigned int thermal_event_seqnum;
 
 	/* allocate memory */
 	size = nla_total_size(sizeof(struct thermal_genl_event)) + \
