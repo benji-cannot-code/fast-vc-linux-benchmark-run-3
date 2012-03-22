1cd2f7ce7abb294a30065f1e3b50f07dfce49c53
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2012-03-22 23:59:52 +0200

    iwlwifi: don't use shared for the logger any more
    Each modules will hold a pointer to struct device instead.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/iwlwifi/iwl-drv.c
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @shrd: pointer to common shared structure
  * @op_mode: the running op_mode
  * @trans: transport layer
+ * @dev: for debug prints only
  * @cfg: configuration struct
  * @fw_index: firmware revision to try loading
  * @firmware_name: composite filename of ucode file to load
@@ -92,6 +93,7 @@ struct iwl_drv {
 	struct iwl_shared *shrd;
 	struct iwl_op_mode *op_mode;
 	struct iwl_trans *trans;
+	struct device *dev;
 	const struct iwl_cfg *cfg;
 
 	int fw_index;                   /* firmware we're trying to load */
@@ -899,6 +901,7 @@ struct iwl_drv *iwl_drv_start(struct iwl_shared *shrd,
 	/* For printing only - temporary until we change the logger */
 	drv->shrd = shrd;
 	drv->trans = trans;
+	drv->dev = trans->dev;
 	drv->cfg = cfg;
 
 	init_completion(&drv->request_firmware_complete);
