9f099e261414994420123d3ea235649c588e843e
Bob Pearson <rpearson@systemfabricworks.com>
2012-03-23 15:02:22 -0700

    crc32: miscellaneous cleanups
    Misc cleanup of lib/crc32.c and related files.
    
    - remove unnecessary header files.
    
    - straighten out some convoluted ifdef's
    
    - rewrite some references to 2 dimensional arrays as 1 dimensional
      arrays to make them correct.  I.e.  replace tab[i] with tab[0][i].
    
    - a few trivial whitespace changes
    
    - fix a warning in gen_crc32tables.c caused by a mismatch in the type of
      the pointer passed to output table.  Since the table is only used at
      kernel compile time, it is simpler to make the table big enough to hold
      the largest column size used.  One cannot make the column size smaller
      in output_table because it has to be used by both the le and be tables
      and they can have different column sizes.
    
    [djwong@us.ibm.com: Minor changelog tweaks]
    Signed-off-by: Bob Pearson <rpearson@systemfabricworks.com>
    Signed-off-by: Darrick J. Wong <djwong@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/gen_crc32table.c
+++ lib/gen_crc32table.c
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LE_TABLE_SIZE (1 << CRC_LE_BITS)
 #define BE_TABLE_SIZE (1 << CRC_BE_BITS)
 
-static uint32_t crc32table_le[4][LE_TABLE_SIZE];
-static uint32_t crc32table_be[4][BE_TABLE_SIZE];
+static uint32_t crc32table_le[4][256];
+static uint32_t crc32table_be[4][256];
 
 /**
  * crc32init_le() - allocate and initialize LE table data
@@ -63,7 +63,7 @@ static void crc32init_be(void)
 	}
 }
 
-static void output_table(uint32_t table[4][256], int len, char *trans)
+static void output_table(uint32_t (*table)[256], int len, char *trans)
 {
 	int i, j;
 
