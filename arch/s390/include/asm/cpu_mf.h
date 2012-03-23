bb9a8da1eef8f436d14f1eb4fe86b47e6efaee46
Jan Glauber <jang@linux.vnet.ibm.com>
2012-03-23 11:13:05 +0100

    [S390] oprofile: Allow multiple users of the measurement alert interrupt
    Prepare the measurement facility which is currently only used by oprofile
    for multiple users.  To achieve that the measurement alert interrupt control
    bit needs to be protected.  The measurement alert definitions are moved
    to a header file and an interrupt mask is added so that users can discard
    interrupts if they are for a different measurement subsystem.
    
    Reviewed-by: Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
    Signed-off-by: Jan Glauber <jang@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

---
+++ arch/s390/include/asm/cpu_mf.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _ASM_S390_CPU_MF_H
+#define _ASM_S390_CPU_MF_H
+
+#define CPU_MF_INT_SF_MASK	0xffc00000
+
+#define CPU_MF_INT_SF_IAE	(1 << 31)	/* invalid entry address */
+#define CPU_MF_INT_SF_ISE	(1 << 30)	/* incorrect SDBT entry */
+#define CPU_MF_INT_SF_PRA	(1 << 29)	/* program request alert */
+#define CPU_MF_INT_SF_SACA	(1 << 23)	/* sampler auth. change alert */
+#define CPU_MF_INT_SF_LSDA	(1 << 22)	/* loss of sample data alert */
+
+#endif
