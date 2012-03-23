8b2854924a2d18060dd986d9dbd074765bc4fdb8
Al Viro <viro@zeniv.linux.org.uk>
2012-03-23 16:45:51 -0400

    migrate ext2_fs.h guts to fs/ext2/ext2.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ext2/xattr_security.c
+++ fs/ext2/xattr_security.c
@@ -4,10 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Handler for storing security labels as extended attributes.
  */
 
-#include <linux/slab.h>
-#include <linux/string.h>
-#include <linux/fs.h>
-#include <linux/ext2_fs.h>
+#include "ext2.h"
 #include <linux/security.h>
 #include "xattr.h"
 
