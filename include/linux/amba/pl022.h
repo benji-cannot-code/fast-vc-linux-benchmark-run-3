4642b8639da76394a780d34c4271ded18619321c
Viresh Kumar <viresh.kumar@st.com>
2012-03-23 13:05:48 +0530

    spi/pL022: include types.h to remove compilation warnings
    linux/pl022.h uses definitions like, u8, u16, etc, which have dependency of
    types.h file, which isn't included in it. So, we get compilation warnings.
    
    This patch includes types.h there to fix these warnings.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@st.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- include/linux/amba/pl022.h
+++ include/linux/amba/pl022.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SSP_PL022_H
 #define _SSP_PL022_H
 
+#include <linux/types.h>
+
 /**
  * whether SSP is in loopback mode or not
  */
