f0b8c3bb14d01a171884daf950df5c97b5181a40
Lars Ellenberg <lars.ellenberg@linbit.com>
2012-03-26 17:06:29 +0200

    drbd: move put_ldev from __req_mod() to the endio callback
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>

--- drivers/block/drbd/drbd_req.c
+++ drivers/block/drbd/drbd_req.c
@@ -417,7 +417,6 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 
 		maybe_wakeup_conflicting_requests(req);
 		req_may_be_completed_not_susp(req, m);
-		put_ldev(mdev);
 		break;
 
 	case ABORT_DISK_IO:
@@ -435,7 +434,6 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		__drbd_chk_io_error(mdev, false);
 		maybe_wakeup_conflicting_requests(req);
 		req_may_be_completed_not_susp(req, m);
-		put_ldev(mdev);
 		break;
 
 	case READ_AHEAD_COMPLETED_WITH_ERROR:
@@ -443,7 +441,6 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		req->rq_state |= RQ_LOCAL_COMPLETED;
 		req->rq_state &= ~RQ_LOCAL_PENDING;
 		req_may_be_completed_not_susp(req, m);
-		put_ldev(mdev);
 		break;
 
 	case READ_COMPLETED_WITH_ERROR:
@@ -455,7 +452,6 @@ int __req_mod(struct drbd_request *req, enum drbd_req_event what,
 		D_ASSERT(!(req->rq_state & RQ_NET_MASK));
 
 		__drbd_chk_io_error(mdev, false);
-		put_ldev(mdev);
 
 	goto_queue_for_net_read:
 
