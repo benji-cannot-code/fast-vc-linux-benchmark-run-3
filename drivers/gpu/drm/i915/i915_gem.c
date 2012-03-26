98d0d6e07e9b2228573f24b840740b8fa1906f4b
Chris Wilson <chris@chris-wilson.co.uk>
2012-03-26 10:10:27 +0200

    drm/i915: Avoid using mappable space for relocation processing through the CPU
    We try to avoid writing the relocations through the uncached GTT, if the
    buffer is currently in the CPU write domain and so will be flushed out to
    main memory afterwards anyway. Also on SandyBridge we can safely write
    to the pages in cacheable memory, so long as the buffer is LLC mapped.
    In either of these cases, we therefore do not need to force the
    reallocation of the buffer into the mappable region of the GTT, reducing
    the aperture pressure.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -40,8 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static __must_check int i915_gem_object_flush_gpu_write_domain(struct drm_i915_gem_object *obj);
 static void i915_gem_object_flush_gtt_write_domain(struct drm_i915_gem_object *obj);
 static void i915_gem_object_flush_cpu_write_domain(struct drm_i915_gem_object *obj);
-static __must_check int i915_gem_object_set_to_cpu_domain(struct drm_i915_gem_object *obj,
-							  bool write);
 static __must_check int i915_gem_object_set_cpu_read_domain_range(struct drm_i915_gem_object *obj,
 								  uint64_t offset,
 								  uint64_t size);
@@ -3074,7 +3072,7 @@ i915_gem_object_finish_gpu(struct drm_i915_gem_object *obj)
  * This function returns when the move is complete, including waiting on
  * flushes to occur.
  */
-static int
+int
 i915_gem_object_set_to_cpu_domain(struct drm_i915_gem_object *obj, bool write)
 {
 	uint32_t old_write_domain, old_read_domains;
