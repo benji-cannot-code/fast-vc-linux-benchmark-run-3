a56c9a395ba34d122898c70acb564e31bc3626e7
Christoph Hellwig <hch@infradead.org>
2012-03-26 04:57:08 -0400

    target: don't limit transfer sizes for the ramdisk backend
    The ramdisk backend has not inherent limitations for handling requests,
    so don't artificially limits the transfer size.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/target/target_core_rd.h
+++ drivers/target/target_core_rd.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RD_DEVICE_QUEUE_DEPTH	32
 #define RD_MAX_DEVICE_QUEUE_DEPTH 128
 #define RD_BLOCKSIZE		512
-#define RD_MAX_SECTORS		1024
 
 /* Used in target_core_init_configfs() for virtual LUN 0 access */
 int __init rd_module_init(void);
