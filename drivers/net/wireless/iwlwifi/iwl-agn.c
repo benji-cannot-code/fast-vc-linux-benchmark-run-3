5f66d0fea9922f068ebc8dd0bae376ecda3fcec4
Joe Perches <joe@perches.com>
2012-03-26 08:27:40 -0700

    iwlwifi: Add pr_fmt
    Prefix dmesg output with "iwlwifi: " by
    adding #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn.c
+++ drivers/net/wireless/iwlwifi/iwl-agn.c
@@ -27,6 +27,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Intel Corporation, 5200 N.E. Elam Young Parkway, Hillsboro, OR 97124-6497
  *
  *****************************************************************************/
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
