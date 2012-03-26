3c0af67e4f74dfffd391707b9f1b35bdcbb3273d
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-03-26 21:47:18 +0200

    i2c-gpio: Use linux/gpio.h rather than asm/gpio.h
    Direct inclusion of asm/gpio.h has been deprecated for a while now due
    to the cross platform gpiolib.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- drivers/i2c/busses/i2c-gpio.c
+++ drivers/i2c/busses/i2c-gpio.c
@@ -15,8 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/platform_device.h>
-
-#include <asm/gpio.h>
+#include <linux/gpio.h>
 
 /* Toggle SDA by changing the direction of the pin */
 static void i2c_gpio_setsda_dir(void *data, int state)
