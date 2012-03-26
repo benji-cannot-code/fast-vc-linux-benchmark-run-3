0624796b948c319e1c9b4e1a5a82009d6d5fc212
Parav Pandit <parav.pandit@emulex.com>
2012-03-26 14:27:13 +0000

    be2net: Add functionality to support RoCE driver
    - Increase MSI-X vectors by 5 for RoCE traffic.
    - Add macro to check roce support on a device.
    - Add device-specific doorbell and MSI-X vector fields shared with NIC
      functionality.
    - Provide RoCE driver registration and deregistration functions.
    - Add support functions which will be invoked on adapter add/remove
      and port up/down events.
    - Traverse through the list of adapters to invoke callback functions.
    
    Signed-off-by: Parav Pandit <parav.pandit@emulex.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/net/ethernet/emulex/benet/be_hw.h
+++ drivers/net/ethernet/emulex/benet/be_hw.h
@@ -99,11 +99,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SLI_INTF_REV_SHIFT			4
 #define SLI_INTF_FT_MASK			0x00000001
 
+#define SLI_INTF_TYPE_2		2
+#define SLI_INTF_TYPE_3		3
 
 /* SLI family */
 #define BE_SLI_FAMILY		0x0
 #define LANCER_A0_SLI_FAMILY	0xA
-
+#define SKYHAWK_SLI_FAMILY      0x2
 
 /********* ISR0 Register offset **********/
 #define CEV_ISR0_OFFSET 			0xC18
