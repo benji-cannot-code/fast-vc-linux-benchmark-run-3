35921e1f00022e4527efd135ca58925cdd12ab1f
Zhenzhong Duan <zhenzhong.duan@oracle.com>
2012-03-28 14:42:47 -0700

    kexec: add further check to crashkernel
    When using crashkernel=2M-256M, the kernel doesn't give any warning.  This
    is misleading sometimes.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@oracle.com>
    Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/kexec.c
+++ kernel/kexec.c
@@ -1360,6 +1360,10 @@ static int __init parse_crashkernel_simple(char 		*cmdline,
 
 	if (*cur == '@')
 		*crash_base = memparse(cur+1, &cur);
+	else if (*cur != ' ' && *cur != '\0') {
+		pr_warning("crashkernel: unrecognized char\n");
+		return -EINVAL;
+	}
 
 	return 0;
 }
