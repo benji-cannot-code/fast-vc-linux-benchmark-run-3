515a8ea524a105f750d3b83474b17bc11ab94023
David Howells <dhowells@redhat.com>
2012-03-28 18:30:02 +0100

    Disintegrate asm/system.h for CRIS
    Disintegrate asm/system.h for CRIS.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Jesper Nilsson <jesper.nilsson@axis.com>
    cc: linux-cris-kernel@axis.com

--- arch/cris/arch-v10/kernel/debugport.c
+++ arch/cris/arch-v10/kernel/debugport.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/major.h>
 #include <linux/delay.h>
 #include <linux/tty.h>
-#include <asm/system.h>
 #include <arch/svinto.h>
 #include <asm/io.h>             /* Get SIMCOUT. */
 
