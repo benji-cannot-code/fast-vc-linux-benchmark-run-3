fee4c675653b7112470959cda9b68486a7ade21f
Steffen Klassert <steffen.klassert@secunet.com>
2012-03-28 08:42:34 +0200

    padata: Add a reference to the api documentation
    Add a reference to the padata api documentation at Documentation/padata.txt
    
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- kernel/padata.c
+++ kernel/padata.c
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * padata.c - generic interface to process data streams in parallel
  *
+ * See Documentation/padata.txt for an api documentation.
+ *
  * Copyright (C) 2008, 2009 secunet Security Networks AG
  * Copyright (C) 2008, 2009 Steffen Klassert <steffen.klassert@secunet.com>
  *
