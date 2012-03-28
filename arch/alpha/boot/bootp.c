5e6877afcd6049461d28e8339c913bfdfbdb1b48
David Howells <dhowells@redhat.com>
2012-03-28 18:11:12 +0100

    Disintegrate asm/system.h for Alpha
    Disintegrate asm/system.h for Alpha.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: linux-alpha@vger.kernel.org

--- arch/alpha/boot/bootp.c
+++ arch/alpha/boot/bootp.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <generated/utsrelease.h>
 #include <linux/mm.h>
 
-#include <asm/system.h>
 #include <asm/console.h>
 #include <asm/hwrpb.h>
 #include <asm/pgtable.h>
