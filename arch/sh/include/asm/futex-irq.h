3e5562541e718c640b8bda0309646255da186b56
David Howells <dhowells@redhat.com>
2012-03-28 18:30:03 +0100

    Disintegrate asm/system.h for SH
    Disintegrate asm/system.h for SH.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: linux-sh@vger.kernel.org

--- arch/sh/include/asm/futex-irq.h
+++ arch/sh/include/asm/futex-irq.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SH_FUTEX_IRQ_H
 #define __ASM_SH_FUTEX_IRQ_H
 
-#include <asm/system.h>
 
 static inline int atomic_futex_op_xchg_set(int oparg, u32 __user *uaddr,
 					   int *oldval)
