42435b98803539457cf9ec080dd7baae633b5b36
David Howells <dhowells@redhat.com>
2012-03-28 18:30:02 +0100

    Disintegrate asm/system.h for H8300
    Disintegrate asm/system.h for H8300.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Yoshinori Sato <ysato@users.sourceforge.jp>

--- arch/h8300/kernel/traps.c
+++ arch/h8300/kernel/traps.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/bug.h>
 
-#include <asm/system.h>
 #include <asm/irq.h>
 #include <asm/traps.h>
 #include <asm/page.h>
