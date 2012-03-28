469fbf5cb285d6802760a616ebb75c39323a1cdb
David Howells <dhowells@redhat.com>
2012-03-28 18:30:03 +0100

    Delete all instances of asm/system.h
    Delete all instances of asm/system.h as they should be redundant by this
    point.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/cris/include/asm/system.h
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* FILE TO BE DELETED. DO NOT ADD STUFF HERE! */
-#include <asm/barrier.h>
-#include <asm/cmpxchg.h>
-#include <asm/exec.h>
-#include <asm/switch_to.h>
--- arch/parisc/include/asm/system.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* FILE TO BE DELETED. DO NOT ADD STUFF HERE! */
-#include <asm/barrier.h>
-#include <asm/exec.h>
-#include <asm/ldcw.h>
-#include <asm/special_insns.h>
-#include <asm/switch_to.h>
