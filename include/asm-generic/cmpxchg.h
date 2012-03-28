6377f1455698101d77abb9bf8ebe2fb81600f7e6
David Howells <dhowells@redhat.com>
2012-03-28 18:30:03 +0100

    Make asm-generic/cmpxchg.h #include asm-generic/cmpxchg-local.h
    Make asm-generic/cmpxchg.h #include asm-generic/cmpxchg-local.h as all arch
    files that #include the former also #include the latter.  See:
    
    	grep -rl asm-generic/cmpxchg-local[.]h arch/ | sort > b
    	grep -rl asm-generic/cmpxchg[.]h arch/ | sort > a
    	comm a b
    
    This simplifies the disintegration of asm-generic/system.h for arches that
    don't have their own.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/cmpxchg.h
+++ include/asm-generic/cmpxchg.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #error "Cannot use generic cmpxchg on SMP"
 #endif
 
+#include <asm-generic/cmpxchg-local.h>
+
 /*
  * Atomic compare and exchange.
  *
