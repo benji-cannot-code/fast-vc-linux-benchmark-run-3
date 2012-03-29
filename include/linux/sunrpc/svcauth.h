4f16ac16fc5b3deb1e3509df9eca58eddd71e401
Simo Sorce <simo@redhat.com>
2012-03-29 19:18:19 -0400

    svcauth: remove unused define
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- include/linux/sunrpc/svcauth.h
+++ include/linux/sunrpc/svcauth.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sunrpc/cache.h>
 #include <linux/hash.h>
 
-#define SVC_CRED_NGROUPS	32
 struct svc_cred {
 	uid_t			cr_uid;
 	gid_t			cr_gid;
