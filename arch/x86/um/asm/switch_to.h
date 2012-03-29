aea4d84c15409d24d352fa6fa0ecf2887079a624
Richard Weinberger <richard@nod.at>
2012-03-29 18:47:46 +0200

    um: Disintegrate asm/system.h
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Reported-by: Toralf Förster <toralf.foerster@gmx.de>
    CC: dhowells@redhat.com

---
+++ arch/x86/um/asm/switch_to.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _ASM_UM_SWITCH_TO_H_
+#define _ASM_UM_SWITCH_TO_H_
+
+extern void *_switch_to(void *prev, void *next, void *last);
+#define switch_to(prev, next, last) prev = _switch_to(prev, next, last)
+
+#endif
