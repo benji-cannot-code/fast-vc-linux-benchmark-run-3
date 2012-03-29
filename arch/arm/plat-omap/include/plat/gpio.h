9504187f7a68db3949396eb392dcf3c51745afec
Tarun Kanti DebBarma <tarun.kanti@ti.com>
2012-03-29 08:41:02 -0700

    ARM: OMAP: Remove OMAP_GPIO_IRQ macro definition
    Since all references to OMAP_GPIO_IRQ macro are replaced now
    with gpio_to_irq(), this can be removed altogether.
    
    Signed-off-by: Tarun Kanti DebBarma <tarun.kanti@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/gpio.h
+++ arch/arm/plat-omap/include/plat/gpio.h
@@ -159,10 +159,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP_MPUIO(nr)		(OMAP_MAX_GPIO_LINES + (nr))
 #define OMAP_GPIO_IS_MPUIO(nr)	((nr) >= OMAP_MAX_GPIO_LINES)
 
-#define OMAP_GPIO_IRQ(nr)	(OMAP_GPIO_IS_MPUIO(nr) ? \
-				 IH_MPUIO_BASE + ((nr) & 0x0f) : \
-				 IH_GPIO_BASE + (nr))
-
 #define METHOD_MPUIO		0
 #define METHOD_GPIO_1510	1
 #define METHOD_GPIO_1610	2
