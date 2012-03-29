f90c9362cee9d546ccfec7aae4831142ca23e796
Dan Carpenter <dan.carpenter@oracle.com>
2012-03-29 20:57:08 +0200

    blkcg: change a spin_lock() to spin_lock_irq()
    Smatch complains that we re-enable IRQs twice.  It looks like we forgot
    to disable them here on the spin_trylock() failure path.  This was added
    in 9f13ef678e "blkcg: use double locking instead of RCU for blkg
    synchronization".
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>`
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -1602,7 +1602,7 @@ static int blkiocg_pre_destroy(struct cgroup_subsys *subsys,
 		} else {
 			spin_unlock_irq(&blkcg->lock);
 			cpu_relax();
-			spin_lock(&blkcg->lock);
+			spin_lock_irq(&blkcg->lock);
 		}
 	}
 
