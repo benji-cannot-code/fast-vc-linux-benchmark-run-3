5d75e4b1c7a88c912332ce4c3ca45ed2c0c116f8
Amit Shah <amit.shah@redhat.com>
2012-03-29 12:50:20 +0530

    virtio: drop thaw PM operation
    The thaw operation was used by the balloon driver, but after the last
    commit there's no reason to have separate thaw and restore callbacks.
    
    Signed-off-by: Amit Shah <amit.shah@redhat.com>

--- drivers/virtio/virtio_balloon.c
+++ drivers/virtio/virtio_balloon.c
@@ -399,11 +399,6 @@ static int restore_common(struct virtio_device *vdev)
 	return 0;
 }
 
-static int virtballoon_thaw(struct virtio_device *vdev)
-{
-	return restore_common(vdev);
-}
-
 static int virtballoon_restore(struct virtio_device *vdev)
 {
 	return restore_common(vdev);
@@ -427,7 +422,6 @@ static struct virtio_driver virtio_balloon_driver = {
 #ifdef CONFIG_PM
 	.freeze	=	virtballoon_freeze,
 	.restore =	virtballoon_restore,
-	.thaw =		virtballoon_thaw,
 #endif
 };
 
