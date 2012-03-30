f891516455504c3bdab0db6acd9b9b8c6f779778
Ben Widawsky <ben@bwidawsk.net>
2012-03-29 19:11:26 -0700

    drm/i915: ring irq cleanups
    - gen6 put/get only need one argument
        rflags and gflags are always the same (see above explanation)
    - remove a couple redundantly defined IRQs
    - reordered some lines to make things go in descending order
    
    Every ring has its own interrupts, enables, masks, and status bits that
    are fed into the main interrupt enable/mask/status registers. At one
    point in time it seemed like a good idea to make our functions support
    the notion that each interrupt may have a different bit position in the
    corresponding register (blitter parser error may be bit n in IMR, but
    bit m in blitter IMR). It turned out though that the HW designers did us
    a solid on Gen6+ and this unfortunate situation has been avoided. This
    allows our interrupt code to be cleaned up a bit.
    
    I jammed this into one commit because there should be no functional
    change with this commit, and staging it into multiple commits was
    unnecessarily artificial IMO.
    
    CC: Chris Wilson <chris@chris-wilson.co.uk>
    CC: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Ben Widawsky <benjamin.widawsky@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    [danvet:
    - fixed up merged conflict with vlv changes.
    - added GEN6 to GT blitter bit, we only use it on gen6+.
    - added a comment to both ring irq bits and GT irq bits that on gen6+
      these alias.
    - added comment that GT_BSD_USER_INTERRUPT is ilk-only.
    - I've got confused a bit that we still use GT_USER_INTERRUPT on ivb
      for the render ring - but this goes back to ilk where we have only
      gt interrupt bits and so we be equally confusing if changed.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -463,7 +463,7 @@ static irqreturn_t valleyview_irq_handler(DRM_IRQ_ARGS)
 			notify_ring(dev, &dev_priv->ring[RCS]);
 		if (gt_iir & GT_GEN6_BSD_USER_INTERRUPT)
 			notify_ring(dev, &dev_priv->ring[VCS]);
-		if (gt_iir & GT_BLT_USER_INTERRUPT)
+		if (gt_iir & GT_GEN6_BLT_USER_INTERRUPT)
 			notify_ring(dev, &dev_priv->ring[BCS]);
 
 		if (gt_iir & (GT_GEN6_BLT_CS_ERROR_INTERRUPT |
@@ -621,9 +621,9 @@ static irqreturn_t ivybridge_irq_handler(DRM_IRQ_ARGS)
 
 	if (gt_iir & (GT_USER_INTERRUPT | GT_PIPE_NOTIFY))
 		notify_ring(dev, &dev_priv->ring[RCS]);
-	if (gt_iir & GT_GEN6_BSD_USER_INTERRUPT)
+	if (gt_iir & GEN6_BSD_USER_INTERRUPT)
 		notify_ring(dev, &dev_priv->ring[VCS]);
-	if (gt_iir & GT_BLT_USER_INTERRUPT)
+	if (gt_iir & GEN6_BLITTER_USER_INTERRUPT)
 		notify_ring(dev, &dev_priv->ring[BCS]);
 
 	if (de_iir & DE_GSE_IVB)
@@ -689,7 +689,7 @@ static irqreturn_t ironlake_irq_handler(DRM_IRQ_ARGS)
 	atomic_inc(&dev_priv->irq_received);
 
 	if (IS_GEN6(dev))
-		bsd_usr_interrupt = GT_GEN6_BSD_USER_INTERRUPT;
+		bsd_usr_interrupt = GEN6_BSD_USER_INTERRUPT;
 
 	/* disable master interrupt before clearing iir  */
 	de_ier = I915_READ(DEIER);
@@ -723,7 +723,7 @@ static irqreturn_t ironlake_irq_handler(DRM_IRQ_ARGS)
 		notify_ring(dev, &dev_priv->ring[RCS]);
 	if (gt_iir & bsd_usr_interrupt)
 		notify_ring(dev, &dev_priv->ring[VCS]);
-	if (gt_iir & GT_BLT_USER_INTERRUPT)
+	if (gt_iir & GEN6_BLITTER_USER_INTERRUPT)
 		notify_ring(dev, &dev_priv->ring[BCS]);
 
 	if (de_iir & DE_GSE)
@@ -2082,8 +2082,8 @@ static int ironlake_irq_postinstall(struct drm_device *dev)
 	if (IS_GEN6(dev))
 		render_irqs =
 			GT_USER_INTERRUPT |
-			GT_GEN6_BSD_USER_INTERRUPT |
-			GT_BLT_USER_INTERRUPT;
+			GEN6_BSD_USER_INTERRUPT |
+			GEN6_BLITTER_USER_INTERRUPT;
 	else
 		render_irqs =
 			GT_USER_INTERRUPT |
@@ -2155,8 +2155,8 @@ static int ivybridge_irq_postinstall(struct drm_device *dev)
 	I915_WRITE(GTIIR, I915_READ(GTIIR));
 	I915_WRITE(GTIMR, dev_priv->gt_irq_mask);
 
-	render_irqs = GT_USER_INTERRUPT | GT_GEN6_BSD_USER_INTERRUPT |
-		GT_BLT_USER_INTERRUPT;
+	render_irqs = GT_USER_INTERRUPT | GEN6_BSD_USER_INTERRUPT |
+		GEN6_BLITTER_USER_INTERRUPT;
 	I915_WRITE(GTIER, render_irqs);
 	POSTING_READ(GTIER);
 
@@ -2219,7 +2219,7 @@ static int valleyview_irq_postinstall(struct drm_device *dev)
 
 	render_irqs = GT_GEN6_BLT_FLUSHDW_NOTIFY_INTERRUPT |
 		GT_GEN6_BLT_CS_ERROR_INTERRUPT |
-		GT_BLT_USER_INTERRUPT |
+		GT_GEN6_BLT_USER_INTERRUPT |
 		GT_GEN6_BSD_USER_INTERRUPT |
 		GT_GEN6_BSD_CS_ERROR_INTERRUPT |
 		GT_GEN7_L3_PARITY_ERROR_INTERRUPT |
