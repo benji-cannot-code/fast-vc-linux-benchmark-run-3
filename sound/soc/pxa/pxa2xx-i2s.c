7d47a6159502207812bdfd48ce67f135337cfdf7
Martin Jansa <martin.jansa@gmail.com>
2012-04-02 10:24:08 +0200

    ASoC: pxa: pxa2xx-i2s: add io.h for IOMEM macro
    * fixes
      sound/soc/pxa/pxa2xx-i2s.c:86:2: error: implicit declaration of function 'IOMEM' [-Werror=implicit-function-declaration]
      sound/soc/pxa/pxa2xx-i2s.c:86:2: error: initializer element is not constant
      after 23019a733bb83c8499f192fb428b7e6e81c95a34 removed IOMEM
      definition from arch/arm/mach-pxa/include/mach/hardware.h
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/pxa/pxa2xx-i2s.c
+++ sound/soc/pxa/pxa2xx-i2s.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/clk.h>
 #include <linux/platform_device.h>
+#include <linux/io.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/initval.h>
