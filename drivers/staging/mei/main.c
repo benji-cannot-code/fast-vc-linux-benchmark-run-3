b5b0306bd4742a753a21db3e1c0a3b48fa5f5228
Tomas Winkler <tomas.winkler@intel.com>
2012-04-02 20:32:37 +0300

    staging/mei: remove unused variable
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mei/main.c
+++ drivers/staging/mei/main.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "interface.h"
 
 
-#define MEI_READ_TIMEOUT 45
 #define MEI_DRIVER_NAME	"mei"
 #define MEI_DEV_NAME "mei"
 
