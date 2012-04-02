0fe3ab522036b4f617920c4a8ef0a576442ea4b7
Jiri Slaby <jslaby@suse.cz>
2012-04-02 13:54:38 +0200

    TTY: 68328serial, use ulong flags for interrupts status
    flags passed to local_irq_save/restore should be ulong. Switch tehem
    to that. Otherwise we get compilation warnings:
    .../68328serial.c:248:9: warning: comparison of distinct pointer types lacks a cast [enabled by default]
    .../68328serial.c:257:9: warning: comparison of distinct pointer types lacks a cast [enabled by default]
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: linux-m68k@lists.linux-m68k.org
    Acked-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/68328serial.c
+++ drivers/tty/serial/68328serial.c
@@ -163,7 +163,8 @@ static void rs_stop(struct tty_struct *tty)
 
 static int rs_put_char(char ch)
 {
-        int flags, loops = 0;
+	unsigned long flags;
+	int loops = 0;
 
         local_irq_save(flags);
 
@@ -1183,7 +1184,8 @@ static const struct tty_operations rs_ops = {
 static int __init
 rs68328_init(void)
 {
-	int flags, i;
+	unsigned long flags;
+	int i;
 	struct m68k_serial *info;
 
 	serial_driver = alloc_tty_driver(NR_PORTS);
