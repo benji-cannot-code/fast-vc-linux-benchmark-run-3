dc1ed3081801f5f6ab96839cd313f4911bd8315b
Jiri Slaby <jslaby@suse.cz>
2012-04-02 13:54:21 +0200

    TTY: HVC, add tty_port
    And use kref from that. This means we need tty_port->ops->destruct to
    properly free the structure. This is what destroy_hvc_struct used to
    do so we leverage that.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: linuxppc-dev@lists.ozlabs.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/hvc/hvc_console.h
+++ drivers/tty/hvc/hvc_console.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HVC_ALLOC_TTY_ADAPTERS	8
 
 struct hvc_struct {
+	struct tty_port port;
 	spinlock_t lock;
 	int index;
 	struct tty_struct *tty;
@@ -62,7 +63,6 @@ struct hvc_struct {
 	struct winsize ws;
 	struct work_struct tty_resize;
 	struct list_head next;
-	struct kref kref; /* ref count & hvc_struct lifetime */
 };
 
 /* implemented by a low level driver */
