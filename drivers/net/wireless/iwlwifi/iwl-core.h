1101b8c2c215cbd185cdad90739589d4bcdb7519
Johannes Berg <johannes.berg@intel.com>
2012-04-02 15:14:48 +0200

    iwlwifi: move TIME_UNIT
    We're getting rid of iwl-core.h, move TIME_UNIT out.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-core.h
+++ drivers/net/wireless/iwlwifi/iwl-core.h
@@ -73,8 +73,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct iwl_host_cmd;
 struct iwl_cmd;
 
-#define TIME_UNIT		1024
-
 /***************************
  *   L i b                 *
  ***************************/
