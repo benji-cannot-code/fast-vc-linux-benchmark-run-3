041f71891481a215ef99dcfb07703c916b361fc2
Jayamohan Kallickal <jayamohan.kallickal@emulex.com>
2012-04-03 23:41:45 -0500

    [SCSI] be2iscsi: Update external Branding to Emulex
    Change MODULE_AUTHOR, driver name and other external print strings from
    Serverengines to Emulex.
    
    Signed-off-by: Minh Tran <MinhDuc.Tran@Emulex.Com>
    Signed-off-by: Jayamohan Kallickal <jayamohan.kallickal@emulex.com>
    Signed-off-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/be2iscsi/be_cmds.h
+++ drivers/scsi/be2iscsi/be_cmds.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * firmware in the BE. These requests are communicated to the processor
  * using Work Request Blocks (WRBs) submitted to the MCC-WRB ring or via one
  * WRB inside a MAILBOX.
- * The commands are serviced by the ARM processor in the BladeEngine's MPU.
+ * The commands are serviced by the ARM processor in the OneConnect's MPU.
  */
 struct be_sge {
 	u32 pa_lo;
