ea633c161dde39eddef4ae81048e023d4fa3a9a6
Jingoo Han <jg1.han@samsung.com>
2012-04-04 15:57:44 +0900

    video: s3c-fb: add runtime_get/put to suspend/resume
    This patch adds runtime_get/put to suspend/resume, which should be
    necessary to prevent the problem to access the fimd register
    without block power on.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>

--- drivers/video/s3c-fb.c
+++ drivers/video/s3c-fb.c
@@ -1558,6 +1558,8 @@ static int s3c_fb_suspend(struct device *dev)
 	struct s3c_fb_win *win;
 	int win_no;
 
+	pm_runtime_get_sync(sfb->dev);
+
 	for (win_no = S3C_FB_MAX_WIN - 1; win_no >= 0; win_no--) {
 		win = sfb->windows[win_no];
 		if (!win)
@@ -1571,6 +1573,9 @@ static int s3c_fb_suspend(struct device *dev)
 		clk_disable(sfb->lcd_clk);
 
 	clk_disable(sfb->bus_clk);
+
+	pm_runtime_put_sync(sfb->dev);
+
 	return 0;
 }
 
@@ -1582,6 +1587,8 @@ static int s3c_fb_resume(struct device *dev)
 	struct s3c_fb_win *win;
 	int win_no;
 	u32 reg;
+
+	pm_runtime_get_sync(sfb->dev);
 
 	clk_enable(sfb->bus_clk);
 
@@ -1628,6 +1635,8 @@ static int s3c_fb_resume(struct device *dev)
 		dev_dbg(&pdev->dev, "resuming window %d\n", win_no);
 		s3c_fb_set_par(win->fbinfo);
 	}
+
+	pm_runtime_put_sync(sfb->dev);
 
 	return 0;
 }
