c8f4429bb769211730d79df051bacf808eaeb3f6
Deepak SIKRI <deepak.sikri@st.com>
2012-04-04 04:33:20 +0000

    stmmac: Define CSUM offload engine Types
    This patch explicitly defines the CSUM offload engine type which need
    (not mandatory) to be passed from the platform code.
    STMMAC core supports two check sum offload engine types- Type-1 & Type-2.
    Also, there are STMMAC cores that do not have the check sum offload
    capabilities.
    
    The behaviour of Type-1 & Type-2 cores related to provision of checksum
    increases the packet length for Type-1 cores by 2, as the checksum is appended
    at the end of data packet and the same is made accountable in the DMA status.
    The STMMAC cores beyond Version-3.5 provide HW interface registers which allows
    the user to read the HW capabilities, while to support the previous cores the
    information related to HW capabilities has to be provided from the platform
    code.
    
    The Type-1 cores which do not have the HW register interface need this
    information.
    
    This patch also updates the driver's doc.
    
    Signed-off-by: Deepak Sikri <deepak.sikri@st.com>
    Hacked-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/stmmac.h
+++ include/linux/stmmac.h
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/platform_device.h>
 
+#define STMMAC_RX_COE_NONE	0
+#define STMMAC_RX_COE_TYPE1	1
+#define STMMAC_RX_COE_TYPE2	2
+
 /* Platfrom data for platform device structure's platform_data field */
 
 struct stmmac_mdio_bus_data {
@@ -50,6 +54,7 @@ struct plat_stmmacenet_data {
 	int has_gmac;
 	int enh_desc;
 	int tx_coe;
+	int rx_coe;
 	int bugged_jumbo;
 	int pmt;
 	int force_sf_dma_mode;
