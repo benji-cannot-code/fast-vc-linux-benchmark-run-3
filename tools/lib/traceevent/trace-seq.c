1ca7f3ac3629f0cf564bb6c242e268c85eb3db72
Steven Rostedt <srostedt@redhat.com>
2012-04-06 00:47:55 +0200

    events: Update tools/lib/traceevent to work with perf
    Some of the util functions of libtraceevent.a conflict with perf,
    such as die(), warning() and others. Move them into event-util.h
    that is not included by the perf tools.
    
    Also, as perf compiles with 'bool' the filter_arg->bool needs to
    be renamed to filter_arg->boolean.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Arnaldo Carvalho de Melo <acme@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Arun Sharma <asharma@fb.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- tools/lib/traceevent/trace-seq.c
+++ tools/lib/traceevent/trace-seq.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdarg.h>
 
 #include "event-parse.h"
+#include "event-utils.h"
 
 /*
  * The TRACE_SEQ_POISON is to catch the use of using
