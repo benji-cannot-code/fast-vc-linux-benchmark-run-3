a40711be49d92708593001c478b88e49fa6f5351
Santosh Nayak <santoshprasadnayak@gmail.com>
2012-04-05 11:31:08 +0530

    agp: Remove 'break' after 'return' statement.
    'break' is unnecessary after 'return' statement.
    Remove all such 'break' as clean up.
    
    Signed-off-by: Santosh Nayak <santoshprasadnayak@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/char/agp/generic.c
+++ drivers/char/agp/generic.c
@@ -1011,7 +1011,6 @@ int agp_generic_free_gatt_table(struct agp_bridge_data *bridge)
 	case LVL2_APER_SIZE:
 		/* The generic routines can't deal with 2 level gatt's */
 		return -EINVAL;
-		break;
 	default:
 		page_order = 0;
 		break;
@@ -1078,7 +1077,6 @@ int agp_generic_insert_memory(struct agp_memory * mem, off_t pg_start, int type)
 	case LVL2_APER_SIZE:
 		/* The generic routines can't deal with 2 level gatt's */
 		return -EINVAL;
-		break;
 	default:
 		num_entries = 0;
 		break;
