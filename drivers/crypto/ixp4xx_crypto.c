400709ce876f8d06f63daee1190cc2c4fee26384
Michał Wróbel <michal.wrobel@flytronic.pl>
2012-04-05 20:34:21 +0800

    crypto: ixp4xx - include fix
    Before commit de47725421ad5627a5c905f4e40bb844ebc06d29 ("include: replace
    linux/module.h with "struct module" wherever possible") <linux/module.h> was
    implicitly included through <linux/platform_device.h> -> <linux/device.h>.
    
    Signed-off-by: Michał Wróbel <michal.wrobel@flytronic.pl>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ixp4xx_crypto.c
+++ drivers/crypto/ixp4xx_crypto.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/spinlock.h>
 #include <linux/gfp.h>
+#include <linux/module.h>
 
 #include <crypto/ctr.h>
 #include <crypto/des.h>
