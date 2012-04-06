8e76f211c876b5df0642797cc6ee9d7485ab423a
Jonas Bonn <jonas@southpole.se>
2012-04-06 13:04:12 +0200

    openrisc: remove unnecessary includes
    Signed-off-by: Jonas Bonn <jonas@southpole.se>

--- arch/openrisc/kernel/irq.c
+++ arch/openrisc/kernel/irq.c
@@ -15,15 +15,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *      2 of the License, or (at your option) any later version.
  */
 
-#include <linux/ptrace.h>
-#include <linux/errno.h>
 #include <linux/interrupt.h>
 #include <linux/init.h>
 #include <linux/of.h>
 #include <linux/ftrace.h>
 #include <linux/irq.h>
-#include <linux/seq_file.h>
-#include <linux/kernel_stat.h>
 #include <linux/export.h>
 #include <linux/irqdomain.h>
 #include <linux/irqflags.h>
