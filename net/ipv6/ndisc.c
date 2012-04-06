efff0364b652c81a0fae271ce43e18bf41a52b57
Alexey I. Froloff <raorn@raorn.name>
2012-04-06 05:50:58 +0000

    Treat ND option 31 as userland (DNSSL support)
    As specified in RFC6106, DNSSL option contains one or more domain names
    of DNS suffixes.  8-bit identifier of the DNSSL option type as assigned
    by the IANA is 31.  This option should also be treated as userland.
    
    Signed-off-by: Alexey I. Froloff <raorn@raorn.name>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ndisc.c
+++ net/ipv6/ndisc.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *	Changes:
  *
+ *	Alexey I. Froloff		:	RFC6106 (DNSSL) support
  *	Pierre Ynard			:	export userland ND options
  *						through netlink (RDNSS support)
  *	Lars Fenneberg			:	fixed MTU setting on receipt
@@ -229,7 +230,8 @@ static struct nd_opt_hdr *ndisc_next_option(struct nd_opt_hdr *cur,
 
 static inline int ndisc_is_useropt(struct nd_opt_hdr *opt)
 {
-	return opt->nd_opt_type == ND_OPT_RDNSS;
+	return opt->nd_opt_type == ND_OPT_RDNSS ||
+		opt->nd_opt_type == ND_OPT_DNSSL;
 }
 
 static struct nd_opt_hdr *ndisc_next_useropt(struct nd_opt_hdr *cur,
