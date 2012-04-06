3bde23894142e1bec3004d225396d2e91f87a851
Aaro Koskinen <aaro.koskinen@iki.fi>
2012-04-07 01:14:08 +0300

    staging: xgifb: delete VB_ExtTVFlickerIndex
    Delete VB_ExtTVFlickerIndex. It's 0 for all video modes.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/vb_setmode.c
+++ drivers/staging/xgifb/vb_setmode.c
@@ -5945,7 +5945,7 @@ static void XGI_SetAntiFlicker(unsigned short ModeNo,
 			       unsigned short ModeIdIndex,
 			       struct vb_device_info *pVBInfo)
 {
-	unsigned short tempbx, index;
+	unsigned short tempbx;
 
 	unsigned char tempah;
 
@@ -5954,8 +5954,6 @@ static void XGI_SetAntiFlicker(unsigned short ModeNo,
 
 	tempbx = XGI_GetTVPtrIndex(pVBInfo);
 	tempbx &= 0xFE;
-	index = pVBInfo->EModeIDTable[ModeIdIndex].VB_ExtTVFlickerIndex;
-	tempbx += index;
 	tempah = TVAntiFlickList[tempbx];
 	tempah = tempah << 4;
 
