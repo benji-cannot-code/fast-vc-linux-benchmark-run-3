8ecc97dfa0d29dbd1a7cfb2b92bfc1002c55cdaf
Aaro Koskinen <aaro.koskinen@iki.fi>
2012-04-07 01:13:51 +0300

    staging: xgifb: drop XG41 code
    XG_41 is not listed in xgifb_pci_table, so the code can be safely dropped.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/XGI_main.h
+++ drivers/staging/xgifb/XGI_main.h
@@ -13,9 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define XGIFAIL(x) do { printk(x "\n"); return -EINVAL; } while (0)
 
-#ifndef PCI_DEVICE_ID_XGI_41
-#define PCI_DEVICE_ID_XGI_41      0x041
-#endif
 #ifndef PCI_DEVICE_ID_XGI_42
 #define PCI_DEVICE_ID_XGI_42      0x042
 #endif
