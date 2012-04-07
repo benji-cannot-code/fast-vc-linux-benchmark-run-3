470be6572cc1a50be1ea74f2ff3528c59ab101ad
Grant Likely <grant.likely@secretlab.ca>
2012-04-07 14:16:53 -0600

    spi/devicetree: Move devicetree support code into spi directory
    The SPI device tree support code isn't shared by any other subsystem.  It can
    be moved into the core drivers/spi directory and the exported symbol can be
    removed.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Cc: Rob Herring <rob.herring@calxeda.com>

--- drivers/spi/spi-fsl-espi.c
+++ drivers/spi/spi-fsl-espi.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/of.h>
 #include <linux/of_platform.h>
-#include <linux/of_spi.h>
 #include <linux/interrupt.h>
 #include <linux/err.h>
 #include <sysdev/fsl_soc.h>
