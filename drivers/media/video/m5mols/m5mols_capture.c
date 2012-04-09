6182b63c07b1c86b66abd869eef475fcc60a6336
Sylwester Nawrocki <s.nawrocki@samsung.com>
2012-04-09 13:51:56 -0300

    [media] m5mols: Refactored controls handling
    This patch is a prerequisite for the new controls addition. It consolidates
    the control handling code, which is moved to m5mols_controls.c and
    staticized. The controls initialization is reordered to better reflect
    the control clusters and make the diffs smaller when new controls are added.
    To make the code easier to follow when more controls is added use separate
    set function for each control.
    
    Rewrite the image effect registers handling.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/m5mols/m5mols_capture.c
+++ drivers/media/video/m5mols/m5mols_capture.c
@@ -115,7 +115,7 @@ int m5mols_start_capture(struct m5mols_info *info)
 	 * format. The frame capture is initiated during switching from Monitor
 	 * to Capture mode.
 	 */
-	ret = m5mols_mode(info, REG_MONITOR);
+	ret = m5mols_set_mode(info, REG_MONITOR);
 	if (!ret)
 		ret = m5mols_restore_controls(info);
 	if (!ret)
@@ -125,7 +125,7 @@ int m5mols_start_capture(struct m5mols_info *info)
 	if (!ret)
 		ret = m5mols_lock_3a(info, true);
 	if (!ret)
-		ret = m5mols_mode(info, REG_CAPTURE);
+		ret = m5mols_set_mode(info, REG_CAPTURE);
 	if (!ret)
 		/* Wait until a frame is captured to ISP internal memory */
 		ret = m5mols_wait_interrupt(sd, REG_INT_CAPTURE, 2000);
