a4e1c2ca3dfad10b88b7f417fcf83ab6b625fe7e
Xi Wang <xi.wang@gmail.com>
2012-04-09 17:15:45 -0300

    [media] zoran: fix integer overflow in setup_window()
    `clipcount' is from userspace and thus needs validation.  Otherwise,
    a large `clipcount' could overflow the vmalloc() size, leading to
    out-of-bounds access.
    
    | setup_window()
    | zoran_s_fmt_vid_overlay()
    | __video_do_ioctl()
    | video_ioctl2()
    
    Use 2048 as the maximum `clipcount'.  Also change the corresponding
    parameter type to `unsigned int'.
    
    Signed-off-by: Xi Wang <xi.wang@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/zoran/zoran_driver.c
+++ drivers/media/video/zoran/zoran_driver.c
@@ -1132,8 +1132,14 @@ static int setup_fbuffer(struct zoran_fh *fh,
 }
 
 
-static int setup_window(struct zoran_fh *fh, int x, int y, int width, int height,
-	struct v4l2_clip __user *clips, int clipcount, void __user *bitmap)
+static int setup_window(struct zoran_fh *fh,
+			int x,
+			int y,
+			int width,
+			int height,
+			struct v4l2_clip __user *clips,
+			unsigned int clipcount,
+			void __user *bitmap)
 {
 	struct zoran *zr = fh->zr;
 	struct v4l2_clip *vcp = NULL;
@@ -1153,6 +1159,14 @@ static int setup_window(struct zoran_fh *fh, int x, int y, int width, int height
 			KERN_ERR
 			"%s: %s - no overlay format set\n",
 			ZR_DEVNAME(zr), __func__);
+		return -EINVAL;
+	}
+
+	if (clipcount > 2048) {
+		dprintk(1,
+			KERN_ERR
+			"%s: %s - invalid clipcount\n",
+			 ZR_DEVNAME(zr), __func__);
 		return -EINVAL;
 	}
 
@@ -1219,7 +1233,7 @@ static int setup_window(struct zoran_fh *fh, int x, int y, int width, int height
 				   (width * height + 7) / 8)) {
 			return -EFAULT;
 		}
-	} else if (clipcount > 0) {
+	} else if (clipcount) {
 		/* write our own bitmap from the clips */
 		vcp = vmalloc(sizeof(struct v4l2_clip) * (clipcount + 4));
 		if (vcp == NULL) {
