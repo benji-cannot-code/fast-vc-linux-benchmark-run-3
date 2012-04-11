bd756d8aeb50330e87247d464bf78313222afb79
Andrew Lunn <andrew@lunn.ch>
2012-04-11 21:07:45 +0200

    ARM: Kirkwood: Replace clock gating
    Add a varient of the basic clk-gate code. This variant calls a
    function before gating the clock off. This function is used to disable
    the SATA or PCIe PHY.
    
    Now that all the drivers prepare and enable there clk as needed, there
    is no need for the common code to keep track of which clocks need
    gating on. Let the common clock framework turn off clocks which are
    not used.
    
    Buy using the added clk varient, when the clk framework turns off SATA
    or PCIe clocks, we also disabled SATA and PCIe PHYs which were not
    needed.
    
    The function kirkwood_pcie_id() can now be called outside of __init
    code, so remove this property for it, and functions it calls.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Tested-by: Jamie Lentin <jm@lentin.co.uk>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- arch/arm/plat-orion/pcie.c
+++ arch/arm/plat-orion/pcie.c
@@ -53,12 +53,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  PCIE_DEBUG_SOFT_RESET		(1<<20)
 
 
-u32 __init orion_pcie_dev_id(void __iomem *base)
+u32 orion_pcie_dev_id(void __iomem *base)
 {
 	return readl(base + PCIE_DEV_ID_OFF) >> 16;
 }
 
-u32 __init orion_pcie_rev(void __iomem *base)
+u32 orion_pcie_rev(void __iomem *base)
 {
 	return readl(base + PCIE_DEV_REV_OFF) & 0xff;
 }
