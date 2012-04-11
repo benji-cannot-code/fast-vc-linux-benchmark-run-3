ce1412ab50d6d245861625f8cad0270bda86746b
Namhyung Kim <namhyung@gmail.com>
2012-04-11 17:04:59 -0300

    perf annotate: Fix a build error
        CC util/annotate.o
    util/annotate.c: In function symbol__annotate:
    util/annotate.c:87:16: error: parsed_line may be used uninitialized in this function [-Werror=maybe-uninitialized]
    util/annotate.c:211:22: note: parsed_line was declared here
    cc1: all warnings being treated as errors
    make: *** [util/annotate.o] Error 1
    make: *** Waiting for unfinished jobs....
    
    Signed-off-by: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: Ashay Rane <ashay.rane@tacc.utexas.edu>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/r/87ty0tlv4i.fsf@dasan.aot.lge.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/annotate.c
+++ tools/perf/util/annotate.c
@@ -227,6 +227,7 @@ static int symbol__parse_objdump_line(struct symbol *sym, struct map *map,
 		*c = 0;
 
 	line_ip = -1;
+	parsed_line = line;
 
 	/*
 	 * Strip leading spaces:
@@ -256,8 +257,7 @@ static int symbol__parse_objdump_line(struct symbol *sym, struct map *map,
 			offset = -1;
 		else
 			parsed_line = tmp2 + 1;
-	} else
-		parsed_line = line;
+	}
 
 	objdump_line = objdump_line__new(offset, parsed_line, privsize);
 	free(line);
