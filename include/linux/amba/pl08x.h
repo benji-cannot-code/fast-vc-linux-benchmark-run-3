353c4e86252439e25b8c7e3892e1619cb11d0c0d
Linus Walleij <linus.walleij@linaro.org>
2012-04-12 09:01:49 +0200

    dma/amba-pl08x: add support for the Nomadik variant
    The Nomadik PL080 variant has some extra protection bits that
    may be set, so we need to check these bits to see if the
    channels are actually available for the DMAengine to use.
    
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Alim Akhtar <alim.akhtar@gmail.com>
    Cc: Alessandro Rubini <rubini@gnudd.com>
    Reviewed-by: Viresh Kumar <viresh.kumar@st.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- include/linux/amba/pl08x.h
+++ include/linux/amba/pl08x.h
@@ -93,6 +93,8 @@ struct pl08x_bus_data {
  * right now
  * @serving: the virtual channel currently being served by this physical
  * channel
+ * @locked: channel unavailable for the system, e.g. dedicated to secure
+ * world
  */
 struct pl08x_phy_chan {
 	unsigned int id;
@@ -100,6 +102,7 @@ struct pl08x_phy_chan {
 	spinlock_t lock;
 	int signal;
 	struct pl08x_dma_chan *serving;
+	bool locked;
 };
 
 /**
