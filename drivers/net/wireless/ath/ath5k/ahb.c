a33f2259a0470e379049d33f7b86d8c39afaee9b
Jonathan Bither <jonbither@gmail.com>
2012-04-12 06:13:33 -0400

    ath5k: fix undefined 'THIS_MODULE'
    When cross compiling ath5k for a Mips machine with kernel 3.2.14
    the compilation fails with "/ath5k/ahb.c:231:12: error: 'THIS_MODULE' undeclared here (not in a function)"
    Fix the build by including <linux/export.h>
    
    Signed-off-by: Jonathan Bither <jonbither@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath5k/ahb.c
+++ drivers/net/wireless/ath/ath5k/ahb.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nl80211.h>
 #include <linux/platform_device.h>
 #include <linux/etherdevice.h>
+#include <linux/export.h>
 #include <ar231x_platform.h>
 #include "ath5k.h"
 #include "debug.h"
