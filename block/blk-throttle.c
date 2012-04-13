fd3fd22a44b2707bcb31f037188dd92c167de501
Tejun Heo <tj@kernel.org>
2012-04-13 13:11:34 -0700

    blkcg: drop stuff unused after per-queue policy activation update
    * All_q_list is unused.  Drop all_q_{mutex|list}.
    
    * @for_root of blkg_lookup_create() is always %false when called from
      outside blk-cgroup.c proper.  Factor out __blkg_lookup_create() so
      that it doesn't check whether @q is bypassing and use the
      underscored version for the @for_root callsite.
    
    * blkg_destroy_all() is used only from blkcg proper and @destroy_root
      is always %true.  Make it static and drop @destroy_root.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-throttle.c
+++ block/blk-throttle.c
@@ -286,7 +286,7 @@ static struct throtl_grp *throtl_lookup_create_tg(struct throtl_data *td,
 	} else {
 		struct blkio_group *blkg;
 
-		blkg = blkg_lookup_create(blkcg, q, false);
+		blkg = blkg_lookup_create(blkcg, q);
 
 		/* if %NULL and @q is alive, fall back to root_tg */
 		if (!IS_ERR(blkg))
