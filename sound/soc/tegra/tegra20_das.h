dede03742de8eb146a5f12b5a3ccd3b1df7f7dd9
Stephen Warren <swarren@nvidia.com>
2012-04-13 12:14:08 -0600

    ASoC: convert Tegra20 DAS driver to regmap
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/tegra/tegra20_das.h
+++ sound/soc/tegra/tegra20_das.h
@@ -86,8 +86,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct tegra20_das {
 	struct device *dev;
-	void __iomem *regs;
-	struct dentry *debug;
+	struct regmap *regmap;
 };
 
 /*
