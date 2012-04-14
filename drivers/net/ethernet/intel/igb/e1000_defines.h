86d61ba76026110a2867eea04f5f576eaa253646
Matthew Vick <matthew.vick@intel.com>
2012-04-14 05:20:32 +0000

    igb: Disable the BMC-to-OS Watchdog Enable bit for DMAC.
    Under certain scenarios, it's possible that bursty manageability traffic
    over the BMC-to-OS path may overrun the internal manageability receive
    buffer causing dropped manageability packets. Clearing this bit prevents
    this situation by interrupting coalescing to allow manageability traffic
    through.
    
    Signed-off-by: Matthew Vick <matthew.vick@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igb/e1000_defines.h
+++ drivers/net/ethernet/intel/igb/e1000_defines.h
@@ -302,6 +302,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 							* transactions */
 #define E1000_DMACR_DMAC_LX_SHIFT       28
 #define E1000_DMACR_DMAC_EN             0x80000000 /* Enable DMA Coalescing */
+/* DMA Coalescing BMC-to-OS Watchdog Enable */
+#define E1000_DMACR_DC_BMC2OSW_EN	0x00008000
 
 #define E1000_DMCTXTH_DMCTTHR_MASK      0x00000FFF /* DMA Coalescing Transmit
 							* Threshold */
