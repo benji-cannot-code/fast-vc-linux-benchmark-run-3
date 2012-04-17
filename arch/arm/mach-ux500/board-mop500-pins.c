660f6d81f648efa6161c206bf36883407d716115
Rabin Vincent <rabin.vincent@stericsson.com>
2012-04-17 13:35:31 +0200

    ARM: ux500: implement pin API
    Implement an API to allow a list of pincfgs to be remuxed to active
    and sleep modes, for power saving. This is not exported on purpose,
    because it is not to be called by drivers directly.
    
    Signed-off-by: Rabin Vincent <rabin.vincent@stericsson.com>
    Reviewed-by: Srinidhi Kasagar <srinidhi.kasagar@stericsson.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500-pins.c
+++ arch/arm/mach-ux500/board-mop500-pins.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/hardware.h>
 
 #include "pins-db8500.h"
+#include "pins.h"
 
 static pin_cfg_t mop500_pins_common[] = {
 	/* I2C */
