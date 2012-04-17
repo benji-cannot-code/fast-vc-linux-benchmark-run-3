f71b34b384832569ce00b1208af6b42ba390568c
Chris Wilson <chris@chris-wilson.co.uk>
2012-04-17 16:38:12 +0100

    drm/i915: Replace open coded MI_BATCH_GTT
    The (2<<6) virtual memory space selector harks back to gen3 and is
    mandatory given our use of GTT space for batchbuffers. On gen4+, use of
    the GTT became mandatory and bit6 marked reserved. However the code must
    now explicitly set (1<<7), which conveniently is also (2<<6).
    
    To clarify the meaning for future readers, replace the open coded (2<<6)
    with MI_BATCH_GTT.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -232,6 +232,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   MI_BATCH_NON_SECURE	(1)
 #define   MI_BATCH_NON_SECURE_I965 (1<<8)
 #define MI_BATCH_BUFFER_START	MI_INSTR(0x31, 0)
+#define   MI_BATCH_GTT		    (2<<6) /* aliased with (1<<7) on gen4 */
 #define MI_SEMAPHORE_MBOX	MI_INSTR(0x16, 1) /* gen6+ */
 #define  MI_SEMAPHORE_GLOBAL_GTT    (1<<22)
 #define  MI_SEMAPHORE_UPDATE	    (1<<21)
