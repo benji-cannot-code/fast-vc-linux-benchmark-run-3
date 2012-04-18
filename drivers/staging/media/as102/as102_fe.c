4699c29226bd57efea6c3eb6bf61df962be513f5
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2012-04-18 01:30:08 -0300

    drivers: staging: media: as102: as102fe.c: Remove include of version.h
    The output of "make versioncheck" told us that:
    
    drivers/staging/media/as102/as102_fe.c: 20 linux/version.h not needed.
    
    If we take a look at the code, we can agree to remove this include.
    
    Cc: Mauro Carvalho Chehab <mchehab@infradead.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: <linux-media@vger.kernel.org>
    Cc: <devel@driverdev.osuosl.org>
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/as102/as102_fe.c
+++ drivers/staging/media/as102/as102_fe.c
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-#include <linux/version.h>
-
 #include "as102_drv.h"
 #include "as10x_types.h"
 #include "as10x_cmd.h"
