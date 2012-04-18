e483699960ecf3c00b2d64993020ff55f544898c
Eugeni Dodonov <eugeni.dodonov@intel.com>
2012-04-18 11:51:14 -0300

    drm/i915: fix line breaks in intel_pm
    The previous patch had way too long lines, this fixes them to fit into a
    reasonable screen space.
    
    Signed-off-by: Eugeni Dodonov <eugeni.dodonov@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -29,11 +29,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_drv.h"
 #include "intel_drv.h"
 
-/* FBC, or Frame Buffer Compression, is a technique employed to compress the framebuffer contents in-memory, aiming at reducing the required bandwidth during in-memory transfers and, therefore, reduce the power packet.
+/* FBC, or Frame Buffer Compression, is a technique employed to compress the
+ * framebuffer contents in-memory, aiming at reducing the required bandwidth
+ * during in-memory transfers and, therefore, reduce the power packet.
  *
- * The benefits of FBC are mostly visible with solid backgrounds and variation-less patterns.
+ * The benefits of FBC are mostly visible with solid backgrounds and
+ * variation-less patterns.
  *
- * FBC-related functionality can be enabled by the means of the i915.i915_enable_fbc parameter
+ * FBC-related functionality can be enabled by the means of the
+ * i915.i915_enable_fbc parameter
  */
 
 void i8xx_disable_fbc(struct drm_device *dev)
