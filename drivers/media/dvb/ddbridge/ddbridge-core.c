96d39087485090c00106ca34e8de8587c3c3bac1
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2012-04-18 00:30:03 -0300

    [media] drivers: media: dvb: ddbridge: ddbridge-code: Remove unneeded include of version.h
    The output of "make versioncheck" told us that the file
    drivers/media/dvb/ddbridge/ddbridge-code.c has a incorrect include of
    version.h:
    
    linux/drivers/media/dvb/ddbridge/ddbridge-core.c: 34 linux/version.h not
    needed.
    
    After take a look in the code, we can agree to remove it.
    
    Cc: Mauro Carvalho Chehab <mchehab@infradead.org>
    Cc: <linux-media@vger.kernel.org>
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/ddbridge/ddbridge-core.c
+++ drivers/media/dvb/ddbridge/ddbridge-core.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/pci_ids.h>
 #include <linux/timer.h>
-#include <linux/version.h>
 #include <linux/i2c.h>
 #include <linux/swab.h>
 #include <linux/vmalloc.h>
