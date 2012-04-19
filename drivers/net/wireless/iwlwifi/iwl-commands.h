35080dffa5a958ec21a21230bacb9c09f5565bd7
Wey-Yi Guy <wey-yi.w.guy@intel.com>
2012-04-19 09:49:03 -0700

    iwlwifi: small define change
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-commands.h
+++ drivers/net/wireless/iwlwifi/iwl-commands.h
@@ -1904,6 +1904,8 @@ struct iwl_bt_cmd {
 					IWLAGN_BT_VALID_REDUCED_TX_PWR | \
 					IWLAGN_BT_VALID_3W_LUT)
 
+#define IWLAGN_BT_DECISION_LUT_SIZE	12
+
 struct iwl_basic_bt_cmd {
 	u8 flags;
 	u8 ledtime; /* unused */
@@ -1914,7 +1916,7 @@ struct iwl_basic_bt_cmd {
 	u8 bt3_prio_sample_time;
 	u8 bt3_timer_t2_value;
 	__le16 bt4_reaction_time; /* unused */
-	__le32 bt3_lookup_table[12];
+	__le32 bt3_lookup_table[IWLAGN_BT_DECISION_LUT_SIZE];
 	__le16 bt4_decision_time; /* unused */
 	__le16 valid;
 };
