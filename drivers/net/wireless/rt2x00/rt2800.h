bc2325ffb816fd6c3ae290d9313909fa417369d6
Anisse Astier <anisse@astier.eu>
2012-04-19 11:20:32 +0200

    rt2800: debugfs register access: BBP is 256 bytes big
    We're already using BBP for values > 128. Make that explicit and allow
    debugfs access.
    
    Signed-off-by: Anisse Astier <anisse@astier.eu>
    Acked-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2800.h
+++ drivers/net/wireless/rt2x00/rt2800.h
@@ -99,7 +99,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EEPROM_BASE			0x0000
 #define EEPROM_SIZE			0x0110
 #define BBP_BASE			0x0000
-#define BBP_SIZE			0x0080
+#define BBP_SIZE			0x00ff
 #define RF_BASE				0x0004
 #define RF_SIZE				0x0010
 
