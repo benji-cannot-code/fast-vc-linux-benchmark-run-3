e1e332a2434c5bb49aec088bd725504d251a12ae
Stanislaw Gruszka <sgruszka@redhat.com>
2012-04-19 09:51:24 -0700

    iwlwifi: add option to disable 5GHz band
    There are various problems happened on 5GHz band not observed on
    2.4 GHz (microcode errors, queue stuck, etc... ) . Also roaming
    between 5GHz AP and 2GHz does not work very well. To workaround
    the problems add option to disable 5GHz support. This will help
    on environments where APs are dual-band, and devices will not try
    to associate on band where issues happen.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-modparams.h
+++ drivers/net/wireless/iwlwifi/iwl-modparams.h
@@ -104,6 +104,7 @@ enum iwl_power_level {
  * @ant_coupling: antenna coupling in dB, default = 0
  * @bt_ch_announce: BT channel inhibition, default = enable
  * @auto_agg: enable agg. without check, default = true
+ * @disable_5ghz: disable 5GHz capability, default = false
  */
 struct iwl_mod_params {
 	int sw_crypto;
@@ -120,6 +121,7 @@ struct iwl_mod_params {
 	int ant_coupling;
 	bool bt_ch_announce;
 	bool auto_agg;
+	bool disable_5ghz;
 };
 
 #endif /* #__iwl_shared_h__ */
