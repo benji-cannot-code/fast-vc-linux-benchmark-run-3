77344c4a854ccf4842de09d921b2522bbe819797
Paul Mundt <lethal@linux-sh.org>
2012-04-19 17:52:20 +0900

    sh64: Kill off unused trap_no/error_code from thread_struct.
    While the trap number and error code are passed around for debugging
    purposes, this occurs wholly independently of the thread struct values.
    These values were never part of the sigcontext ABI and are thus never
    passed anywhere, so we can just kill them off across the board.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/tlbflush_64.c
+++ arch/sh/mm/tlbflush_64.c
@@ -214,7 +214,6 @@ bad_area:
 			panic("INIT had user mode bad_area\n");
 		}
 		tsk->thread.address = address;
-		tsk->thread.error_code = writeaccess;
 		info.si_signo = SIGSEGV;
 		info.si_errno = 0;
 		info.si_addr = (void *) address;
@@ -264,8 +263,6 @@ do_sigbus:
 	 * or user mode.
 	 */
 	tsk->thread.address = address;
-	tsk->thread.error_code = writeaccess;
-	tsk->thread.trap_no = 14;
 	force_sig(SIGBUS, tsk);
 
 	/* Kernel mode? Handle exceptions or die */
