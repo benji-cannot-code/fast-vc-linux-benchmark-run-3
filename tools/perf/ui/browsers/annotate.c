e2c1f0c22c7551f942a05c454799fafa79160f64
Arnaldo Carvalho de Melo <acme@redhat.com>
2012-04-20 16:26:14 -0300

    ui browser: Add method to write graphical characters
    To save typing on the switch char set slang stuff.
    
    It also helps in removing more slang direct calls, wrapping them at the
    ui_browser level, where at some point I'll try to implement those in
    terms of GTK+.
    
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Namhyung Kim <namhyung@gmail.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-63yhb2htv9g3g1olmojzptkd@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/ui/browsers/annotate.c
+++ tools/perf/ui/browsers/annotate.c
@@ -72,9 +72,7 @@ static void annotate_browser__write(struct ui_browser *self, void *entry, int ro
 		slsmg_write_nstring(" ", 9);
 	}
 
-	SLsmg_set_char_set(1);
-	SLsmg_write_char(SLSMG_VLINE_CHAR);
-	SLsmg_set_char_set(0);
+	ui_browser__write_graph(self, SLSMG_VLINE_CHAR);
 	SLsmg_write_char(' ');
 
 	/* The scroll bar isn't being used */
@@ -117,10 +115,8 @@ static void annotate_browser__write(struct ui_browser *self, void *entry, int ro
 			if (ins__is_jump(dl->ins)) {
 				bool fwd = dl->ops.target > (u64)dl->offset;
 
-				SLsmg_set_char_set(1);
-				SLsmg_write_char(fwd ? SLSMG_DARROW_CHAR :
-						       SLSMG_UARROW_CHAR);
-				SLsmg_set_char_set(0);
+				ui_browser__write_graph(self, fwd ? SLSMG_DARROW_CHAR :
+								    SLSMG_UARROW_CHAR);
 				SLsmg_write_char(' ');
 			} else {
 				slsmg_write_nstring(" ", 2);
@@ -132,9 +128,7 @@ static void annotate_browser__write(struct ui_browser *self, void *entry, int ro
 			if (strcmp(dl->name, "retq")) {
 				slsmg_write_nstring(" ", 2);
 			} else {
-				SLsmg_set_char_set(1);
-				SLsmg_write_char(SLSMG_LARROW_CHAR);
-				SLsmg_set_char_set(0);
+				ui_browser__write_graph(self, SLSMG_LARROW_CHAR);
 				SLsmg_write_char(' ');
 			}
 
