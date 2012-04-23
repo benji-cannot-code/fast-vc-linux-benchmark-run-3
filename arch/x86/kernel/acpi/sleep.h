aa500ec6edc861d3ff9e9c114ebe3bf452d91499
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2012-04-22 23:03:18 -0400

    x86, acpi: Call acpi_enter_sleep_state via an asmlinkage C function from assembler
    With commit a2ef5c4fd44ce3922435139393b89f2cce47f576
    "ACPI: Move module parameter gts and bfs to sleep.c" the
    wake_sleep_flags is required when calling acpi_enter_sleep_state.
    
    The assembler code in wakeup_*.S did not do that. One solution
    is to call it from assembler and stick the wake_sleep_flags on
    the stack (for 32-bit) or in %esi (for 64-bit). hpa and rafael
    both suggested however to create a wrapper function to call
    acpi_enter_sleep_state and call said wrapper function
    ("acpi_enter_s3") from assembler.
    
    For 32-bit, the acpi_enter_s3 ends up looking as so:
    
      push   %ebp
      mov    %esp,%ebp
      sub    $0x8,%esp
      movzbl 0xc1809314,%eax [wake_sleep_flags]
      movl   $0x3,(%esp)
      mov    %eax,0x4(%esp)
      call   0xc12d1fa0 <acpi_enter_sleep_state>
      leave
      ret
    
    And 64-bit:
    
      movzbl 0x9afde1(%rip),%esi        [wake_sleep_flags]
      push   %rbp
      mov    $0x3,%edi
      mov    %rsp,%rbp
      callq  0xffffffff812e9800 <acpi_enter_sleep_state>
      leaveq
      retq
    
    Reviewed-by: H. Peter Anvin <hpa@zytor.com>
    Suggested-by: H. Peter Anvin <hpa@zytor.com>
    [v2: Remove extra assembler operations, per hpa review]
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Link: http://lkml.kernel.org/r/1335150198-21899-3-git-send-email-konrad.wilk@oracle.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/acpi/sleep.h
+++ arch/x86/kernel/acpi/sleep.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <asm/trampoline.h>
+#include <linux/linkage.h>
 
 extern unsigned long saved_video_mode;
 extern long saved_magic;
@@ -11,6 +12,7 @@ extern long saved_magic;
 extern int wakeup_pmode_return;
 
 extern u8 wake_sleep_flags;
+extern asmlinkage void acpi_enter_s3(void);
 
 extern unsigned long acpi_copy_wakeup_routine(unsigned long);
 extern void wakeup_long64(void);
