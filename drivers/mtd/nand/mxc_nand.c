b1cc9ea601c43029aea39293761160d5f6e690a6
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2012-04-23 11:23:33 +0200

    mtd: mxc_nand: set owner field to prevent module unloading when in use
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/nand/mxc_nand.c
+++ drivers/mtd/nand/mxc_nand.c
@@ -1276,6 +1276,7 @@ static int __devexit mxcnd_remove(struct platform_device *pdev)
 static struct platform_driver mxcnd_driver = {
 	.driver = {
 		   .name = DRIVER_NAME,
+		   .owner = THIS_MODULE,
 	},
 	.remove = __devexit_p(mxcnd_remove),
 };
