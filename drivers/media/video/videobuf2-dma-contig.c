4d328f0e13596f7f528175a95c0e0fbe850f934b
H Hartley Sweeten <hartleys@visionengravers.com>
2012-04-24 19:08:12 -0300

    [media] media: videobuf2-dma-contig: include header for exported symbols
    Include the header to pickup the definitions of the exported symbols.
    
    Quiets the following sparse warnings:
    
    warning: symbol 'vb2_dma_contig_memops' was not declared. Should it be static?
    warning: symbol 'vb2_dma_contig_init_ctx' was not declared. Should it be static?
    warning: symbol 'vb2_dma_contig_cleanup_ctx' was not declared. Should it be static?
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Pawel Osciak <pawel@osciak.com>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: Mauro Carvalho Chehab <mchehab@infradead.org>
    Acked-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/videobuf2-dma-contig.c
+++ drivers/media/video/videobuf2-dma-contig.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 
 #include <media/videobuf2-core.h>
+#include <media/videobuf2-dma-contig.h>
 #include <media/videobuf2-memops.h>
 
 struct vb2_dc_conf {
