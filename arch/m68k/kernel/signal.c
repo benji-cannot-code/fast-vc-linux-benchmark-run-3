71036e4dc8888e3106e258a4c911e1af5d28e96f
Al Viro <viro@zeniv.linux.org.uk>
2012-04-24 01:46:23 -0400

    m68k: add TIF_NOTIFY_RESUME and handle it.
    TIF_NOTIFY_RESUME added (as bit 5).  That way nommu glue needs no changes at
    all; mmu one needs just to replace jmi do_signal_return to jne do_signal_return
    There we have flags shifted up, until bit 6 (SIGPENDING) is in MSBit; instead
    of checking that MSBit is set (jmi) we check that MSBit or something below it
    is set (jne); bits 0..4 are never set, so that's precisely "bit 6 or bit 5 is
    set".
    
    Usual handling of NOTIFY_RESUME/SIGPENDING is done in do_notify_resume(); glue
    calls it instead of do_signal().
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/m68k/kernel/signal.c
+++ arch/m68k/kernel/signal.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty.h>
 #include <linux/binfmts.h>
 #include <linux/module.h>
+#include <linux/tracehook.h>
 
 #include <asm/setup.h>
 #include <asm/uaccess.h>
@@ -1155,7 +1156,7 @@ handle_signal(int sig, struct k_sigaction *ka, siginfo_t *info,
  * want to handle. Thus you cannot kill init even with a SIGKILL even by
  * mistake.
  */
-asmlinkage void do_signal(struct pt_regs *regs)
+static void do_signal(struct pt_regs *regs)
 {
 	siginfo_t info;
 	struct k_sigaction ka;
@@ -1185,5 +1186,17 @@ asmlinkage void do_signal(struct pt_regs *regs)
 	if (test_thread_flag(TIF_RESTORE_SIGMASK)) {
 		clear_thread_flag(TIF_RESTORE_SIGMASK);
 		sigprocmask(SIG_SETMASK, &current->saved_sigmask, NULL);
+	}
+}
+
+void do_notify_resume(struct pt_regs *regs)
+{
+	if (test_thread_flag(TIF_SIGPENDING))
+		do_signal(regs);
+
+	if (test_and_clear_thread_flag(TIF_NOTIFY_RESUME)) {
+		tracehook_notify_resume(regs);
+		if (current->replacement_session_keyring)
+			key_replace_session_keyring();
 	}
 }
