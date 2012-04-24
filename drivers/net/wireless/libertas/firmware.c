690b69b9ac72b44197c5b7696921be343b06daf9
Luis R. Rodriguez <mcgrof@do-not-panic.com>
2012-04-23 19:55:48 -0700

    libertas: include sched.h on firmware.c
    Do not assume we have our subsystem including this for us,
    at least for older kernels this is not true. Lets just be
    explicit about this requirement for the usage of wake_up().
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/firmware.c
+++ drivers/net/wireless/libertas/firmware.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Firmware loading and handling functions.
  */
 
+#include <linux/sched.h>
 #include <linux/firmware.h>
 #include <linux/firmware.h>
 #include <linux/module.h>
