07dca8ae3721fc8395829ee3be9040a8dfb63288
Manish Rangankar <manish.rangankar@qlogic.com>
2012-04-23 22:32:33 -0700

    [SCSI] qla4xxx: Fix kernel panic during discovery logout.
    Update the session and connection parameter before sending
    connection logged in event to iscsiadm because in some
    scenario logout may come in just after we send the logged
    in event to user, which free up session, connection and ddb,
    but DPC is still updating session and connect parameter
    which can lead to panic.
    
    Signed-off-by: Manish Rangankar <manish.rangankar@qlogic.com>
    Signed-off-by: Vikas Chaudhary <vikas.chaudhary@qlogic.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/qla4xxx/ql4_init.c
+++ drivers/scsi/qla4xxx/ql4_init.c
@@ -885,8 +885,8 @@ int qla4xxx_ddb_change(struct scsi_qla_host *ha, uint32_t fw_ddb_index,
 		switch (state) {
 		case DDB_DS_SESSION_ACTIVE:
 		case DDB_DS_DISCOVERY:
-			ddb_entry->unblock_sess(ddb_entry->sess);
 			qla4xxx_update_session_conn_param(ha, ddb_entry);
+			ddb_entry->unblock_sess(ddb_entry->sess);
 			status = QLA_SUCCESS;
 			break;
 		case DDB_DS_SESSION_FAILED:
@@ -898,6 +898,7 @@ int qla4xxx_ddb_change(struct scsi_qla_host *ha, uint32_t fw_ddb_index,
 		}
 		break;
 	case DDB_DS_SESSION_ACTIVE:
+	case DDB_DS_DISCOVERY:
 		switch (state) {
 		case DDB_DS_SESSION_FAILED:
 			/*
