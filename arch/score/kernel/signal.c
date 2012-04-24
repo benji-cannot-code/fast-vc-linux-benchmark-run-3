5262315451793175fafdaed694b06d86234030a7
Al Viro <viro@zeniv.linux.org.uk>
2012-04-24 01:55:30 -0400

    score: add handling of NOTIFY_RESUME to do_notify_resume()
    It's already called if TIF_NOTIFY_RESUME is set, so we only
    need to add the actual work.  Note that checking for RESTORE_SIGMASK
    was not needed - set_restore_sigmask() also sets SIGPENDING, so
    we never RESTORE_SIGMASK without SIGPENDING.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/score/kernel/signal.c
+++ arch/score/kernel/signal.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ptrace.h>
 #include <linux/unistd.h>
 #include <linux/uaccess.h>
+#include <linux/tracehook.h>
 
 #include <asm/cacheflush.h>
 #include <asm/syscalls.h>
@@ -351,6 +352,12 @@ asmlinkage void do_notify_resume(struct pt_regs *regs, void *unused,
 				__u32 thread_info_flags)
 {
 	/* deal with pending signal delivery */
-	if (thread_info_flags & (_TIF_SIGPENDING | _TIF_RESTORE_SIGMASK))
+	if (thread_info_flags & _TIF_SIGPENDING)
 		do_signal(regs);
+	if (thread_info_flags & _TIF_NOTIFY_RESUME) {
+		clear_thread_flag(TIF_NOTIFY_RESUME);
+		tracehook_notify_resume(regs);
+		if (current->replacement_session_keyring)
+			key_replace_session_keyring();
+	}
 }
