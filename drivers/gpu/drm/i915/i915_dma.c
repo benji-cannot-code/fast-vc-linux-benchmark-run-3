cf37fa2b193868719b6b971c66e5c19fad8e244b
Chris Wilson <chris@chris-wilson.co.uk>
2012-04-24 22:59:43 +0100

    drm/i915: pending_flip_is_done is gen3, name it so
    And remove the cargo-culted copy from the valleyview irq handler.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1110,10 +1110,6 @@ static int i915_load_modeset_init(struct drm_device *dev)
 	if (ret)
 		goto cleanup_vga_client;
 
-	/* IIR "flip pending" bit means done if this bit is set */
-	if (IS_GEN3(dev) && (I915_READ(ECOSKPD) & ECO_FLIP_DONE))
-		dev_priv->flip_pending_is_done = true;
-
 	/* Initialise stolen first so that we may reserve preallocated
 	 * objects for the BIOS to KMS transition.
 	 */
