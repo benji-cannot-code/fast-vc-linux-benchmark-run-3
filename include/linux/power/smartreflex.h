65190cb49a134f35070d503a4dcd5a209da835d5
Jean Pihet <j-pihet@ti.com>
2012-04-24 10:56:40 +0530

    ARM: OMAP2+: SmartReflex: introduce a busy loop condition test macro
    Now that omap_test_timeout is only accessible from mach-omap2/,
    introduce a similar function for SR.
    
    This change makes the SmartReflex implementation ready for the move
    to drivers/.
    
    Signed-off-by: Jean Pihet <j-pihet@ti.com>
    Signed-off-by: J Keerthy <j-keerthy@ti.com>
    Reviewed-by: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- include/linux/power/smartreflex.h
+++ include/linux/power/smartreflex.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <linux/platform_device.h>
-
+#include <linux/delay.h>
 #include <plat/voltage.h>
 
 /*
@@ -167,6 +167,27 @@ struct omap_sr {
 	u32				senn_mod;
 	void __iomem			*base;
 };
+
+/**
+ * test_cond_timeout - busy-loop, testing a condition
+ * @cond: condition to test until it evaluates to true
+ * @timeout: maximum number of microseconds in the timeout
+ * @index: loop index (integer)
+ *
+ * Loop waiting for @cond to become true or until at least @timeout
+ * microseconds have passed.  To use, define some integer @index in the
+ * calling code.  After running, if @index == @timeout, then the loop has
+ * timed out.
+ *
+ * Copied from omap_test_timeout */
+#define sr_test_cond_timeout(cond, timeout, index)		\
+({								\
+	for (index = 0; index < timeout; index++) {		\
+		if (cond)					\
+			break;					\
+		udelay(1);					\
+	}							\
+})
 
 /**
  * struct omap_sr_pmic_data - Strucutre to be populated by pmic code to pass
