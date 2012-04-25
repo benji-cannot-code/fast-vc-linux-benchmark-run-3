fbe14e8054c1a78ece7d211e5808059a68251241
Masanari Iida <standby24x7@gmail.com>
2012-04-25 23:28:35 +0900

    staging,ozwpan: Fix typo in comments within staging/ozwpan
    Correct spelling in comments withon staging/ozwpan
    
    Signed-off-by: Masanari Iida <standby24x7@gmail.com>
    Acked-by: Chris Kelly <ckelly@ozmodevices.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ozwpan/ozhcd.c
+++ drivers/staging/ozwpan/ozhcd.c
@@ -1417,7 +1417,7 @@ static void oz_process_ep0_urb(struct oz_hcd *ozhcd, struct urb *urb,
 			oz_trace("USB_REQ_SET_CONFIGURATION - req\n");
 			break;
 		case USB_REQ_GET_CONFIGURATION:
-			/* We short curcuit this case and reply directly since
+			/* We short circuit this case and reply directly since
 			 * we have the selected configuration number cached.
 			 */
 			oz_event_log(OZ_EVT_CTRL_LOCAL, setup->bRequest, 0, 0,
@@ -1433,7 +1433,7 @@ static void oz_process_ep0_urb(struct oz_hcd *ozhcd, struct urb *urb,
 			}
 			break;
 		case USB_REQ_GET_INTERFACE:
-			/* We short curcuit this case and reply directly since
+			/* We short circuit this case and reply directly since
 			 * we have the selected interface alternative cached.
 			 */
 			oz_event_log(OZ_EVT_CTRL_LOCAL, setup->bRequest, 0, 0,
