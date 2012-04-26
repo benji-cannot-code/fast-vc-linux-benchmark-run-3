1c2305f32c32bbc98f839eb4bebce4e4f42b42d5
Javier M. Mellid <jmunhoz@igalia.com>
2012-04-26 20:45:54 +0200

    staging: sm7xx: remove driver version support
    This patch removes non used version support. Code won't show version
    message almost 4 years old.
    
    Tested with SM712
    
    Signed-off-by: Javier M. Mellid <jmunhoz@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm7xx/smtcfb.h
+++ drivers/staging/sm7xx/smtcfb.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  more details.
  */
 
-#define SMTC_LINUX_FB_VERSION	"version 0.11.2619.21.01 July 27, 2008"
-
 #define NR_PALETTE        256
 #define NR_RGB            2
 
