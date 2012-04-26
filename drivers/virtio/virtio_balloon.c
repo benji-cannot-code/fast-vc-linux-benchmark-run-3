d6d3ea2ecaf99002df440fc2c9893058e7977934
Amit Shah <amit.shah@redhat.com>
2012-04-27 00:45:55 +0530

    virtio: balloon: drop restore_common()
    restore_common() was used when there were different thaw and freeze PM
    callbacks implemented.  We removed thaw in commit
    f38f8387cbdc4138a492ce9f2a5f04fd3cd3cf33.
    
    restore_common() can be removed and virtballoon_restore() can itself do
    the restore ops.
    
    Signed-off-by: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/virtio/virtio_balloon.c
+++ drivers/virtio/virtio_balloon.c
@@ -420,7 +420,7 @@ static int virtballoon_freeze(struct virtio_device *vdev)
 	return 0;
 }
 
-static int restore_common(struct virtio_device *vdev)
+static int virtballoon_restore(struct virtio_device *vdev)
 {
 	struct virtio_balloon *vb = vdev->priv;
 	int ret;
@@ -432,11 +432,6 @@ static int restore_common(struct virtio_device *vdev)
 	fill_balloon(vb, towards_target(vb));
 	update_balloon_size(vb);
 	return 0;
-}
-
-static int virtballoon_restore(struct virtio_device *vdev)
-{
-	return restore_common(vdev);
 }
 #endif
 
