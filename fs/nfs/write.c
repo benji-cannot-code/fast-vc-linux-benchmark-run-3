08f3a33265689310954f0aa401a76599c89094c8
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-04-27 14:31:47 -0400

    NFS: Fix a use-before-initialised warning in fs/nfs/write.c and fs/nfs/pnfs.c
    If the allocation of nfs_write_header fails, the list of nfs_pages that
    needs to be cleaned up is still on desc->pg_list...
    
    Reported-by: Bryan Schumaker <bjschuma@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Acked-by: Fred Isaman <iisaman@netapp.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -1165,7 +1165,7 @@ static int nfs_generic_pg_writepages(struct nfs_pageio_descriptor *desc)
 
 	whdr = nfs_writehdr_alloc();
 	if (!whdr) {
-		desc->pg_completion_ops->error_cleanup(&hdr->pages);
+		desc->pg_completion_ops->error_cleanup(&desc->pg_list);
 		return -ENOMEM;
 	}
 	hdr = &whdr->header;
