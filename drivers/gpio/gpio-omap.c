6e3a6efc51d5bb4da4212c076611c5eb98fa090e
Tarun Kanti DebBarma <tarun.kanti@ti.com>
2012-04-27 19:43:38 +0530

    gpio/omap: fix missing check in *_runtime_suspend()
    We do checking for bank->enabled_non_wakeup_gpios in order
    to skip redundant operations. Somehow, the check got missed
    while doing the cleanup series.
    
    Just to make sure that we do context restore correctly in
    *_runtime_resume(), the bank->workaround_enabled check is
    moved after context restore. Otherwise, it would prevent
    context restore when bank->enabled_non_wakeup_gpios is 0.
    
    Cc: Kevin Hilman <khilman@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Cc: Cousson, Benoit <b-cousson@ti.com>
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Signed-off-by: Tarun Kanti DebBarma <tarun.kanti@ti.com>
    Reviewed-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Tested-by: Govindraj.R <govindraj.raja@ti.com>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- drivers/gpio/gpio-omap.c
+++ drivers/gpio/gpio-omap.c
@@ -1158,6 +1158,9 @@ static int omap_gpio_runtime_suspend(struct device *dev)
 
 	spin_lock_irqsave(&bank->lock, flags);
 
+	if (!bank->enabled_non_wakeup_gpios)
+		goto update_gpio_context_count;
+
 	/*
 	 * Only edges can generate a wakeup event to the PRCM.
 	 *
@@ -1233,11 +1236,6 @@ static int omap_gpio_runtime_resume(struct device *dev)
 	__raw_writel(bank->context.risingdetect,
 		     bank->base + bank->regs->risingdetect);
 
-	if (!bank->workaround_enabled) {
-		spin_unlock_irqrestore(&bank->lock, flags);
-		return 0;
-	}
-
 	if (bank->get_context_loss_count) {
 		context_lost_cnt_after =
 			bank->get_context_loss_count(bank->dev);
@@ -1248,6 +1246,11 @@ static int omap_gpio_runtime_resume(struct device *dev)
 			spin_unlock_irqrestore(&bank->lock, flags);
 			return 0;
 		}
+	}
+
+	if (!bank->workaround_enabled) {
+		spin_unlock_irqrestore(&bank->lock, flags);
+		return 0;
 	}
 
 	__raw_writel(bank->context.fallingdetect,
