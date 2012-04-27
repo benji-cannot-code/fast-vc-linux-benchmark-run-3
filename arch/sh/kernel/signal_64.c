9c3501764822fba940b94aa44bf3f5c281fdeed4
Al Viro <viro@zeniv.linux.org.uk>
2012-04-26 23:52:33 -0400

    sh64: failure to build sigframe != signal without handler
    it's actually "send me SIGSEGV"...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/sh/kernel/signal_64.c
+++ arch/sh/kernel/signal_64.c
@@ -114,8 +114,8 @@ static void do_signal(struct pt_regs *regs)
 
 			tracehook_signal_handler(signr, &info, &ka, regs,
 					test_thread_flag(TIF_SINGLESTEP));
-			return;
 		}
+		return;
 	}
 
 	/* Did we come from a system call? */
