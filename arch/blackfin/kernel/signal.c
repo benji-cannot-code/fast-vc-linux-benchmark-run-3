cc6c979b9e148869d80cc47640dabfea5101a018
Al Viro <viro@zeniv.linux.org.uk>
2012-04-28 02:04:15 -0400

    new helper: signal_delivered()
    Does block_sigmask() + tracehook_signal_handler();  called when
    sigframe has been successfully built.  All architectures converted
    to it; block_sigmask() itself is gone now (merged into this one).
    
    I'm still not too happy with the signature, but that's a separate
    story (IMO we need a structure that would contain signal number +
    siginfo + k_sigaction, so that get_signal_to_deliver() would fill one,
    signal_delivered(), handle_signal() and probably setup...frame() -
    take one).
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/blackfin/kernel/signal.c
+++ arch/blackfin/kernel/signal.c
@@ -261,8 +261,7 @@ handle_signal(int sig, siginfo_t *info, struct k_sigaction *ka,
 	if (ret)
 		return;
 
-	block_sigmask(ka, sig);
-	tracehook_signal_handler(sig, info, ka, regs,
+	signal_delivered(sig, info, ka, regs,
 			test_thread_flag(TIF_SINGLESTEP));
 }
 
