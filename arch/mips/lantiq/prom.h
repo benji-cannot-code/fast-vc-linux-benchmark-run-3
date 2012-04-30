8cefbbdb0b24e8e033de32918762f86d3dcfcaad
John Crispin <blogic@openwrt.org>
2012-04-30 11:33:08 +0200

    MIPS: lantiq: add xway soc ids
    Add the soc ids for additional xway socs. The patch also merges the amazon_se
    code with the other socs.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/3707/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/lantiq/prom.h
+++ arch/mips/lantiq/prom.h
@@ -11,16 +11,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LTQ_PROM_H__
 
 #define LTQ_SYS_TYPE_LEN	0x100
+#define LTQ_SYS_REV_LEN         0x10
 
 struct ltq_soc_info {
 	unsigned char *name;
 	unsigned int rev;
+	unsigned char rev_type[LTQ_SYS_REV_LEN];
+	unsigned int srev;
 	unsigned int partnum;
 	unsigned int type;
 	unsigned char sys_type[LTQ_SYS_TYPE_LEN];
+	unsigned char *compatible;
 };
 
 extern void ltq_soc_detect(struct ltq_soc_info *i);
 extern void ltq_soc_setup(void);
+extern void ltq_soc_init(void);
 
 #endif
