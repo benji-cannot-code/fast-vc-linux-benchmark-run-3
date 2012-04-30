ff64e481bdb02df3a178f016f07a798e48a5a1aa
David S. Miller <davem@davemloft.net>
2012-04-30 13:21:28 -0400

    l2tp: Add missing net/net/ip6_checksum.h include.
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/l2tp/l2tp_core.c
+++ net/l2tp/l2tp_core.c
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/inet6_connection_sock.h>
 #include <net/inet_ecn.h>
 #include <net/ip6_route.h>
+#include <net/ip6_checksum.h>
 
 #include <asm/byteorder.h>
 #include <linux/atomic.h>
