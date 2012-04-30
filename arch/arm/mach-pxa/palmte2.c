a84ce503865cbbb399106ee4413f43df36046c9a
Arnd Bergmann <arnd@arndb.de>
2012-04-30 16:05:28 +0000

    ARM: pxa: define palmte2_pxa_keys conditionally
    Gcc prints a harmless warning about palmte2_pxa_keys not being used
    when the gpio keyboard driver is disabled. The solution is to use
    the same #ifdef that is already present in the place where the
    symbol is used.
    
    Without this patch, building palmz72_defconfig results in:
    
    /home/arnd/linux-arm/arch/arm/mach-pxa/palmte2.c:128:31: warning: 'palmte2_pxa_keys' defined but not used [-Wunused-variable]
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Marek Vasut <marek.vasut@gmail.com>
    Cc: Carlos Eduardo Medaglia Dyonisio <cadu@nerdfeliz.com>
    Cc: Haojian Zhuang <haojian.zhuang@gmail.com>
    Cc: Eric Miao <eric.y.miao@gmail.com>

--- arch/arm/mach-pxa/palmte2.c
+++ arch/arm/mach-pxa/palmte2.c
@@ -106,6 +106,7 @@ static struct pxamci_platform_data palmte2_mci_platform_data = {
 	.gpio_power		= GPIO_NR_PALMTE2_SD_POWER,
 };
 
+#if defined(CONFIG_KEYBOARD_GPIO) || defined(CONFIG_KEYBOARD_GPIO_MODULE)
 /******************************************************************************
  * GPIO keys
  ******************************************************************************/
@@ -133,6 +134,7 @@ static struct platform_device palmte2_pxa_keys = {
 		.platform_data = &palmte2_pxa_keys_data,
 	},
 };
+#endif
 
 /******************************************************************************
  * Backlight
