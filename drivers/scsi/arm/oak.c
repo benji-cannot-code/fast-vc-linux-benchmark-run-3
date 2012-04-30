87331a230505347b49e1f111f70f72fe813424b6
Arnd Bergmann <arnd@arndb.de>
2012-04-30 16:26:31 +0000

    SCSI: ARM: ncr5380/oak uses no interrupts
    The ncr5380 driver is included by multiple board specific
    drivers, which may or may not use the interrupt handler.
    The oak variant doesn't, and should set the DONT_USE_INTR
    macro.
    
    Without this patch, building rpc_defconfig results in:
    
    drivers/scsi/arm/../NCR5380.c:1160:20: warning: 'oakscsi_intr' defined but not used [-Wunused-function]
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Cc: "James E.J. Bottomley" <JBottomley@parallels.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-scsi@vger.kernel.org

--- drivers/scsi/arm/oak.c
+++ drivers/scsi/arm/oak.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*#define PSEUDO_DMA*/
 
 #define OAKSCSI_PUBLIC_RELEASE 1
+#define DONT_USE_INTR
 
 #define priv(host)			((struct NCR5380_hostdata *)(host)->hostdata)
 #define NCR5380_local_declare()		void __iomem *_base
