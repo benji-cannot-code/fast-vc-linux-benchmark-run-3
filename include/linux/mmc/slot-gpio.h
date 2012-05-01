fb8554d6b21b014e72a7ed2e9e42ac6fbc5991b9
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2012-05-01 16:27:25 +0200

    mmc: add CD GPIO polling support to slot functions
    A simple extension of mmc slot functions add support for CD GPIO polling
    for cases where the GPIO cannot produce interrupts, or where this is not
    desired for some reason.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/slot-gpio.h
+++ include/linux/mmc/slot-gpio.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MMC_SLOT_GPIO_H
 
 struct mmc_host;
+
+int mmc_gpio_get_cd(struct mmc_host *host);
 int mmc_gpio_request_cd(struct mmc_host *host, unsigned int gpio);
 void mmc_gpio_free_cd(struct mmc_host *host);
 
