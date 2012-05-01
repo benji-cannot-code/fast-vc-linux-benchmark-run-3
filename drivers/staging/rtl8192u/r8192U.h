33420c701c321a376a27e23a7fb956dd85f458bd
Justin P. Mattock <justinmattock@gmail.com>
2012-05-01 08:34:14 -0700

    staging: "rtl8192e/r8192u" typo change ContiuneDiffCount.
    As mentioned by jesper juhl, we should probably change ContiuneDiffCount
    to ContinueDiffCount. Below you will find the changes to do so.  I have
    compile tested this and everything builds with the changes, as for
    testing on the hardware I am unable to do.
    
    Signed-off-by: Justin P. Mattock <justinmattock@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8192U.h
+++ drivers/staging/rtl8192u/r8192U.h
@@ -1117,7 +1117,7 @@ typedef struct r8192_priv
 	bool bfsync_processing;	// 500ms Fsync timer is active or not
 	u32 	rate_record;
 	u32 	rateCountDiffRecord;
-	u32	ContiuneDiffCount;
+	u32	ContinueDiffCount;
 	bool bswitch_fsync;
 
 	u8	framesync;
