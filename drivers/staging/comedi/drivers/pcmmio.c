1a5202bb7987eae036192cac282fd4cfe19bc100
Ravishankar Karkala Mallikarjunayya <ravishankarkm32@gmail.com>
2012-05-01 11:55:07 +0530

    Staging: comedi: fix printk issue in pcmmio.c
    This is a patch to the pcmmio.c file that fixes up a printk warning
    found by the checkpatch.pl tool.
    
    Signed-off-by: Ravishankar Karkala Mallikarjunayya <ravishankar.km@greenturtles.in>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/pcmmio.c
+++ drivers/staging/comedi/drivers/pcmmio.c
@@ -338,7 +338,7 @@ static int pcmmio_dio_insn_bits(struct comedi_device *dev,
 		}
 #ifdef DAMMIT_ITS_BROKEN
 		/* DEBUG */
-		printk("data_out_byte %02x\n", (unsigned)byte);
+		printk(KERN_DEBUG "data_out_byte %02x\n", (unsigned)byte);
 #endif
 		/* save the digital input lines for this byte.. */
 		s->state |= ((unsigned int)byte) << offset;
