3c9beded8e42a766ecf819a4038ce62a794d602e
Alexander Duyck <alexander.h.duyck@intel.com>
2012-05-02 18:18:42 +0000

    net: Stop decapitating clones that have a head_frag
    This change is meant ot prevent stealing the skb->head to use as a page in
    the event that the skb->head was cloned.  This allows the other clones to
    track each other via shinfo->dataref.
    
    Without this we break down to two methods for tracking the reference count,
    one being dataref, the other being the page count.  As a result it becomes
    difficult to track how many references there are to skb->head.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4590,7 +4590,7 @@ copyfrags:
 		to->data_len += len;
 		goto merge;
 	}
-	if (from->head_frag) {
+	if (from->head_frag && !skb_cloned(from)) {
 		struct page *page;
 		unsigned int offset;
 
@@ -4600,12 +4600,7 @@ copyfrags:
 		offset = from->data - (unsigned char *)page_address(page);
 		skb_fill_page_desc(to, skb_shinfo(to)->nr_frags,
 				   page, offset, skb_headlen(from));
-
-		if (skb_cloned(from))
-			get_page(page);
-		else
-			*fragstolen = true;
-
+		*fragstolen = true;
 		delta = len; /* we dont know real truesize... */
 		goto copyfrags;
 	}
