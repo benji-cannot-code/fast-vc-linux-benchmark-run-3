b5935b57dbfb1e4b39ccd3a2a0c66226c5155bd8
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-05-01 20:56:46 -0400

    USB: input: yealink.c: fix up dev_err() usage
    We should always reference the input device for dev_err(), not the USB
    device.  Fix up the places where I got this wrong.
    
    Reported-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    CC: Henk Vergonet <Henk.Vergonet@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/input/misc/yealink.c
+++ drivers/input/misc/yealink.c
@@ -429,7 +429,7 @@ static void urb_irq_callback(struct urb *urb)
 	int ret, status = urb->status;
 
 	if (status)
-		dev_err(&yld->udev->dev, "%s - urb status %d\n",
+		dev_err(&yld->idev->dev, "%s - urb status %d\n",
 			__func__, status);
 
 	switch (yld->irq_data->cmd) {
@@ -445,7 +445,7 @@ static void urb_irq_callback(struct urb *urb)
 		break;
 
 	default:
-		dev_err(&yld->udev->dev, "unexpected response %x\n",
+		dev_err(&yld->idev->dev, "unexpected response %x\n",
 			yld->irq_data->cmd);
 	}
 
@@ -454,7 +454,7 @@ static void urb_irq_callback(struct urb *urb)
 	if (!yld->shutdown) {
 		ret = usb_submit_urb(yld->urb_ctl, GFP_ATOMIC);
 		if (ret && ret != -EPERM)
-			dev_err(&yld->udev->dev,
+			dev_err(&yld->idev->dev,
 				"%s - usb_submit_urb failed %d\n",
 				__func__, ret);
 	}
@@ -466,7 +466,7 @@ static void urb_ctl_callback(struct urb *urb)
 	int ret = 0, status = urb->status;
 
 	if (status)
-		dev_err(&yld->udev->dev, "%s - urb status %d\n",
+		dev_err(&yld->idev->dev, "%s - urb status %d\n",
 			__func__, status);
 
 	switch (yld->ctl_data->cmd) {
@@ -485,7 +485,7 @@ static void urb_ctl_callback(struct urb *urb)
 	}
 
 	if (ret && ret != -EPERM)
-		dev_err(&yld->udev->dev, "%s - usb_submit_urb failed %d\n",
+		dev_err(&yld->idev->dev, "%s - usb_submit_urb failed %d\n",
 			__func__, ret);
 }
 
