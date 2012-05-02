74c6faa85bdd4ab97bed9748ed857435cf697c5d
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-05-01 21:33:05 -0700

    USB: bcm5974.c: remove dbg() usage
    dbg() was a very old USB-specific macro that should no longer
    be used. This patch removes it from being used in the driver
    and uses dev_dbg() instead.
    
    CC: Henrik Rydberg <rydberg@euromail.se>
    CC: Alessandro Rubini <rubini@ipvvis.unipv.it>
    CC: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/input/mouse/bcm5974.c
+++ drivers/input/mouse/bcm5974.c
@@ -632,6 +632,7 @@ static int bcm5974_wellspring_mode(struct bcm5974 *dev, bool on)
 static void bcm5974_irq_button(struct urb *urb)
 {
 	struct bcm5974 *dev = urb->context;
+	struct input_dev *idev = dev->input;
 	int error;
 
 	switch (urb->status) {
@@ -641,10 +642,11 @@ static void bcm5974_irq_button(struct urb *urb)
 	case -ECONNRESET:
 	case -ENOENT:
 	case -ESHUTDOWN:
-		dbg("bcm5974: button urb shutting down: %d", urb->status);
+		dev_dbg(&idev->dev, "button urb shutting down: %d\n",
+			urb->status);
 		return;
 	default:
-		dbg("bcm5974: button urb status: %d", urb->status);
+		dev_dbg(&idev->dev, "button urb status: %d\n", urb->status);
 		goto exit;
 	}
 
@@ -655,12 +657,13 @@ static void bcm5974_irq_button(struct urb *urb)
 exit:
 	error = usb_submit_urb(dev->bt_urb, GFP_ATOMIC);
 	if (error)
-		dev_err(&dev->input->dev, "button urb failed: %d\n", error);
+		dev_err(&idev->dev, "button urb failed: %d\n", error);
 }
 
 static void bcm5974_irq_trackpad(struct urb *urb)
 {
 	struct bcm5974 *dev = urb->context;
+	struct input_dev *idev = dev->input;
 	int error;
 
 	switch (urb->status) {
@@ -670,10 +673,11 @@ static void bcm5974_irq_trackpad(struct urb *urb)
 	case -ECONNRESET:
 	case -ENOENT:
 	case -ESHUTDOWN:
-		dbg("bcm5974: trackpad urb shutting down: %d", urb->status);
+		dev_dbg(&idev->dev, "trackpad urb shutting down: %d\n",
+			urb->status);
 		return;
 	default:
-		dbg("bcm5974: trackpad urb status: %d", urb->status);
+		dev_dbg(&idev->dev, "trackpad urb status: %d\n", urb->status);
 		goto exit;
 	}
 
@@ -688,7 +692,7 @@ static void bcm5974_irq_trackpad(struct urb *urb)
 exit:
 	error = usb_submit_urb(dev->tp_urb, GFP_ATOMIC);
 	if (error)
-		dev_err(&dev->input->dev, "trackpad urb failed: %d\n", error);
+		dev_err(&idev->dev, "trackpad urb failed: %d\n", error);
 }
 
 /*
