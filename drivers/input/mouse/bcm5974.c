bb433d6af4752044decf67ed4c1155894edbc45d
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-05-01 20:56:47 -0400

    USB: input: bcm5974.c: fix up dev_err() usage
    We should always reference the input device for dev_err(), not the USB
    device.  Fix up the places where I got this wrong.
    
    Reported-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    CC: Henrik Rydberg <rydberg@euromail.se>
    CC: Alessandro Rubini <rubini@ipvvis.unipv.it>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/input/mouse/bcm5974.c
+++ drivers/input/mouse/bcm5974.c
@@ -585,7 +585,7 @@ static int bcm5974_wellspring_mode(struct bcm5974 *dev, bool on)
 	int retval = 0, size;
 
 	if (!data) {
-		dev_err(&dev->intf->dev, "out of memory\n");
+		dev_err(&dev->input->dev, "out of memory\n");
 		retval = -ENOMEM;
 		goto out;
 	}
@@ -598,7 +598,7 @@ static int bcm5974_wellspring_mode(struct bcm5974 *dev, bool on)
 			BCM5974_WELLSPRING_MODE_REQUEST_INDEX, data, 8, 5000);
 
 	if (size != 8) {
-		dev_err(&dev->intf->dev, "could not read from device\n");
+		dev_err(&dev->input->dev, "could not read from device\n");
 		retval = -EIO;
 		goto out;
 	}
@@ -616,7 +616,7 @@ static int bcm5974_wellspring_mode(struct bcm5974 *dev, bool on)
 			BCM5974_WELLSPRING_MODE_REQUEST_INDEX, data, 8, 5000);
 
 	if (size != 8) {
-		dev_err(&dev->intf->dev, "could not write to device\n");
+		dev_err(&dev->input->dev, "could not write to device\n");
 		retval = -EIO;
 		goto out;
 	}
@@ -655,7 +655,7 @@ static void bcm5974_irq_button(struct urb *urb)
 exit:
 	error = usb_submit_urb(dev->bt_urb, GFP_ATOMIC);
 	if (error)
-		dev_err(&dev->intf->dev, "button urb failed: %d\n", error);
+		dev_err(&dev->input->dev, "button urb failed: %d\n", error);
 }
 
 static void bcm5974_irq_trackpad(struct urb *urb)
@@ -688,7 +688,7 @@ static void bcm5974_irq_trackpad(struct urb *urb)
 exit:
 	error = usb_submit_urb(dev->tp_urb, GFP_ATOMIC);
 	if (error)
-		dev_err(&dev->intf->dev, "trackpad urb failed: %d\n", error);
+		dev_err(&dev->input->dev, "trackpad urb failed: %d\n", error);
 }
 
 /*
