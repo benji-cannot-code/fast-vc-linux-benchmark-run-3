e00de6bb4b94f1bc5b528f057fd85cefae10f58d
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-05-01 21:33:35 -0700

    USB: ehci-q.c: remove dbg() usage
    dbg() was a very old USB-specific macro that should no longer
    be used. This patch removes it from being used in the driver
    and uses dev_dbg() instead.
    
    CC: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-q.c
+++ drivers/usb/host/ehci-q.c
@@ -944,7 +944,8 @@ qh_make (
 		}
 		break;
 	default:
-		dbg ("bogus dev %p speed %d", urb->dev, urb->dev->speed);
+		ehci_dbg(ehci, "bogus dev %p speed %d\n", urb->dev,
+			urb->dev->speed);
 done:
 		qh_put (qh);
 		return NULL;
