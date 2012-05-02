866f3fcc9f61e2509dfb363e10816093ae6f5ffc
Jeff Layton <jlayton@redhat.com>
2012-05-02 14:02:40 -0400

    cifs: make sure we ignore the credentials= and cred= options
    Older mount.cifs programs passed this on to the kernel after parsing
    the file. Make sure the kernel ignores that option.
    
    Should fix:
    
        https://bugzilla.kernel.org/show_bug.cgi?id=43195
    
    Cc: Sachin Prabhu <sprabhu@redhat.com>
    Reported-by: Ronald <ronald645@gmail.com>
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -216,6 +216,8 @@ static const match_table_t cifs_mount_option_tokens = {
 
 	{ Opt_ignore, "cred" },
 	{ Opt_ignore, "credentials" },
+	{ Opt_ignore, "cred=%s" },
+	{ Opt_ignore, "credentials=%s" },
 	{ Opt_ignore, "guest" },
 	{ Opt_ignore, "rw" },
 	{ Opt_ignore, "ro" },
