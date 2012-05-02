aaad1d377d76625f7c3cf479719dbcc73ac110a9
Borislav Petkov <borislav.petkov@amd.com>
2012-05-02 16:43:02 +0200

    x86, MCE, AMD: Remove local_allocate_... wrapper
    It is unneeded now so drop it.
    
    Signed-off-by: Borislav Petkov <borislav.petkov@amd.com>

--- arch/x86/kernel/cpu/mcheck/mce_amd.c
+++ arch/x86/kernel/cpu/mcheck/mce_amd.c
@@ -62,6 +62,7 @@ struct threshold_bank {
 	struct kobject		*kobj;
 	struct threshold_block	*blocks;
 };
+
 static DEFINE_PER_CPU(struct threshold_bank * [NR_BANKS], threshold_banks);
 
 static unsigned char shared_bank[NR_BANKS] = {
@@ -546,13 +547,6 @@ out_free:
 	return err;
 }
 
-static __cpuinit long
-local_allocate_threshold_blocks(int cpu, unsigned int bank)
-{
-	return allocate_threshold_blocks(cpu, bank, 0,
-					 MSR_IA32_MC0_MISC + bank * 4);
-}
-
 static __cpuinit int threshold_create_bank(unsigned int cpu, unsigned int bank)
 {
 	struct device *dev = per_cpu(mce_device, cpu);
@@ -576,7 +570,8 @@ static __cpuinit int threshold_create_bank(unsigned int cpu, unsigned int bank)
 
 	per_cpu(threshold_banks, cpu)[bank] = b;
 
-	err = local_allocate_threshold_blocks(cpu, bank);
+	err = allocate_threshold_blocks(cpu, bank, 0,
+					MSR_IA32_MC0_MISC + bank * 4);
 	if (!err)
 		goto out;
 
