319993ee4a359db44aa42c348e32b2cc7fabcb00
Lee Jones <lee.jones@linaro.org>
2012-05-02 09:20:37 +0100

    ARM: ux500: Enable LED heartbeat functionality on Snowball
    This patch configures the user LED on Snowball to double flash
    every second or so, whilst it's still alive. This can give key
    indications as to what the board is doing in the case of no
    console output.
    
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct gpio_led snowball_led_array[] = {
 	{
 		.name = "user_led",
-		.default_trigger = "none",
+		.default_trigger = "heartbeat",
 		.gpio = 142,
 	},
 };
