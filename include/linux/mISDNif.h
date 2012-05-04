104ba8fc41f526ae0314e797f4993501396211d7
Karsten Keil <isdn@linux-pingi.de>
2012-05-04 04:15:32 +0000

    mISDN: L2 timeouts need to be queued as L2 event
    To be full preemptiv safe, we cannot handle a L2 timeout in the timer
    context itself, we should do all actions via the D-channel thread.
    
    Signed-off-by: Karsten Keil <kkeil@linux-pingi.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/mISDNif.h
+++ include/linux/mISDNif.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define	MISDN_MAJOR_VERSION	1
 #define	MISDN_MINOR_VERSION	1
-#define MISDN_RELEASE		21
+#define MISDN_RELEASE		26
 
 /* primitives for information exchange
  * generell format
@@ -115,6 +115,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MDL_STATUS_UI_IND	0x1E04
 #define MDL_ERROR_IND		0x1F04
 #define MDL_ERROR_RSP		0x5F04
+
+/* intern layer 2 */
+#define DL_TIMER200_IND		0x7004
+#define DL_TIMER203_IND		0x7304
+#define DL_INTERN_MSG		0x7804
 
 /* DL_INFORMATION_IND types */
 #define DL_INFO_L2_CONNECT	0x0001
