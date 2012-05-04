06d387aba0ae92085744f3090d7c1a004417411a
Samuel Ortiz <sameo@linux.intel.com>
2012-05-04 11:24:16 +0200

    NFC: Take a reference on the LLCP local pointer when creating a socket
    LLCP sockets point to their local LLCP service, so they need to take a
    reference on it.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- net/nfc/llcp/llcp.h
+++ net/nfc/llcp/llcp.h
@@ -45,6 +45,8 @@ struct nfc_llcp_local {
 	struct list_head list;
 	struct nfc_dev *dev;
 
+	struct kref ref;
+
 	struct mutex sdp_lock;
 	struct mutex socket_lock;
 
@@ -166,6 +168,8 @@ struct nfc_llcp_sock {
 
 
 struct nfc_llcp_local *nfc_llcp_find_local(struct nfc_dev *dev);
+struct nfc_llcp_local *nfc_llcp_local_get(struct nfc_llcp_local *local);
+int nfc_llcp_local_put(struct nfc_llcp_local *local);
 u8 nfc_llcp_get_sdp_ssap(struct nfc_llcp_local *local,
 			 struct nfc_llcp_sock *sock);
 u8 nfc_llcp_get_local_ssap(struct nfc_llcp_local *local);
