9042ee5d8509ccdfa01ec6027d62e6d2956d86f2
Alexander Shiyan <shc_work@mail.ru>
2012-05-06 09:21:57 +0400

    ARM: clps711x: Using a single definition for the PHYS and VIRT registers offset
    Using a single definition for the physical and virtual address register for all
    variants boards clps711x. This patch also includes the use of a single function
    clps_read/write in some units.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/include/asm/hardware/ep7212.h
+++ arch/arm/include/asm/hardware/ep7212.h
@@ -25,18 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_HARDWARE_EP7212_H
 
 /*
- * define EP7212_BASE to be the base address of the region
- * you want to access.
- */
-
-#define EP7212_PHYS_BASE	(0x80000000)
-
-#ifndef __ASSEMBLY__
-#define ep_readl(off)		__raw_readl(EP7212_BASE + (off))
-#define ep_writel(val,off)	__raw_writel(val, EP7212_BASE + (off))
-#endif
-
-/*
  * These registers are specific to the EP7212 only
  */
 #define DAIR			0x2000
