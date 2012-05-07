5d8977142fb8b6506550a5366a3a75cafdb33de3
Jingoo Han <jg1.han@samsung.com>
2012-05-07 09:20:09 +0900

    video: s3c-fb: use pr_debug instead of printk
    This patch uses pr_debug instead of printk to allow dynamic debugging.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>

--- drivers/video/s3c-fb.c
+++ drivers/video/s3c-fb.c
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_FB_S3C_DEBUG_REGWRITE
 #undef writel
 #define writel(v, r) do { \
-	printk(KERN_DEBUG "%s: %08x => %p\n", __func__, (unsigned int)v, r); \
+	pr_debug("%s: %08x => %p\n", __func__, (unsigned int)v, r); \
 	__raw_writel(v, r); \
 } while (0)
 #endif /* FB_S3C_DEBUG_REGWRITE */
