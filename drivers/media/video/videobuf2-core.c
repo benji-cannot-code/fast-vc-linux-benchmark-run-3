0a45b516ad6838ff85bcb2a69cc3712420d03189
Hans de Goede <hdegoede@redhat.com>
2012-05-08 14:47:39 -0300

    [media] videobuf2: Fix a bug in fileio emulation error handling
    Various error paths in fileio_init where not setting the request-count
    to 0 when unrequesting the buffers on error to init the fileio emulation.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/videobuf2-core.c
+++ drivers/media/video/videobuf2-core.c
@@ -1858,7 +1858,6 @@ static int __vb2_init_fileio(struct vb2_queue *q, int read)
 	 * (multiplane buffers are not supported).
 	 */
 	if (q->bufs[0]->num_planes != 1) {
-		fileio->req.count = 0;
 		ret = -EBUSY;
 		goto err_reqbufs;
 	}
@@ -1905,6 +1904,7 @@ static int __vb2_init_fileio(struct vb2_queue *q, int read)
 	return ret;
 
 err_reqbufs:
+	fileio->req.count = 0;
 	vb2_reqbufs(q, &fileio->req);
 
 err_kfree:
