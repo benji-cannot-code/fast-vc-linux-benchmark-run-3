da69fcd8b7e896376ad135250e39f9aa2d04d3e5
Jarkko Sakkinen <jarkko.sakkinen@intel.com>
2012-05-08 21:22:42 +0300

    x86, realmode: flattened rm hierachy
    Simplified hierarchy under rm directory to a flat
    directory because it is not anymore really justified
    to have own directory for wakeup code. It only adds
    more complexity.
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@intel.com>
    Link: http://lkml.kernel.org/r/1336501366-28617-20-git-send-email-jarkko.sakkinen@intel.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/acpi/sleep.c
+++ arch/x86/kernel/acpi/sleep.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cacheflush.h>
 #include <asm/realmode.h>
 
-#include "../../realmode/rm/wakeup/wakeup.h"
+#include "../../realmode/rm/wakeup.h"
 #include "sleep.h"
 
 unsigned long acpi_realmode_flags;
