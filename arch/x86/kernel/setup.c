3d4aefb2d0dc0e2b9bf7cce735d1cfe1a589caf6
Jarkko Sakkinen <jarkko.sakkinen@intel.com>
2012-05-08 21:22:26 +0300

    x86, realmode: Relocator for realmode code
    Implements relocator for real mode code that is called
    as part of setup_arch(). Processes segment relocations
    and linear relocations. Real-mode code is relocated to
    a free hole below 1 MB.
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@intel.com>
    Link: http://lkml.kernel.org/r/1336501366-28617-4-git-send-email-jarkko.sakkinen@intel.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mtrr.h>
 #include <asm/apic.h>
 #include <asm/trampoline.h>
+#include <asm/realmode.h>
 #include <asm/e820.h>
 #include <asm/mpspec.h>
 #include <asm/setup.h>
@@ -919,6 +920,7 @@ void __init setup_arch(char **cmdline_p)
 			max_pfn_mapped<<PAGE_SHIFT);
 
 	setup_trampolines();
+	setup_real_mode();
 
 	init_gbpages();
 
