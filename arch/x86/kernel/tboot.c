2298595fd4d23aa78a53d2d5a3256758740aeebd
Jarkko Sakkinen <jarkko.sakkinen@intel.com>
2012-05-08 21:22:45 +0300

    x86, realmode: fixes compilation issue in tboot.c
    Fixed include path of wakeup.h in tboot.c.
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@intel.com>
    Link: http://lkml.kernel.org/r/1336501366-28617-23-git-send-email-jarkko.sakkinen@intel.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/tboot.c
+++ arch/x86/kernel/tboot.c
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/e820.h>
 #include <asm/io.h>
 
-#include "acpi/realmode/wakeup.h"
+#include "../realmode/rm/wakeup.h"
 
 /* Global pointer to shared data; NULL means no measured launch. */
 struct tboot *tboot __read_mostly;
