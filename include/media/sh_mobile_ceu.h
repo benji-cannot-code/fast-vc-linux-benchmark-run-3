f4eacc144ba5eb2140030c6993a6fddfb7ff5ea1
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2012-05-08 00:00:07 -0300

    [media] V4L2: sh_mobile_ceu: manage lower 8bit bus
    CAMCR::DTIF feild controls camera bus as upper8bit/16bit/lower8bit.
    This patch manages unmanaged lower 8bit bus
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/sh_mobile_ceu.h
+++ include/media/sh_mobile_ceu.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SH_CEU_FLAG_USE_16BIT_BUS	(1 << 1) /* use 16bit bus width */
 #define SH_CEU_FLAG_HSYNC_LOW		(1 << 2) /* default High if possible */
 #define SH_CEU_FLAG_VSYNC_LOW		(1 << 3) /* default High if possible */
+#define SH_CEU_FLAG_LOWER_8BIT		(1 << 4) /* default upper 8bit */
 
 struct device;
 struct resource;
