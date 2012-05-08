c7382a42868f3cea9ef2de6641b3b9b56e2293d1
H. Peter Anvin <hpa@linux.intel.com>
2012-05-08 21:22:37 +0300

    x86, realmode: Replace open-coded ljmpw with a macro
    We cannot code an ljmpw to the real-mode segment directly, because gas
    refuses to assemble an ljmp with a symbolic segment.  Instead of
    open-coding it everywhere, define a macro and use it for this case.
    
    This is specifically an ljmpw from a 16-bit segment.  This is okay, as
    one should never enter real mode from a 32-bit segment: if one do, the
    CPU ends up in a bizarre (and useless) mode sometimes called "unreal
    mode" where segments behave like real mode but the default address and
    operand sizes is 32 bits.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    Link: http://lkml.kernel.org/r/1336501366-28617-15-git-send-email-jarkko.sakkinen@intel.com

---
+++ arch/x86/realmode/rm/realmode.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef ARCH_X86_REALMODE_RM_REALMODE_H
+#define ARCH_X86_REALMODE_RM_REALMODE_H
+
+#ifdef __ASSEMBLY__
+
+/*
+ * 16-bit ljmpw to the real_mode_seg
+ *
+ * This must be open-coded since gas will choke on using a
+ * relocatable symbol for the segment portion.
+ */
+#define LJMPW_RM(to)	.byte 0xea ; .word (to), real_mode_seg
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* ARCH_X86_REALMODE_RM_REALMODE_H */
