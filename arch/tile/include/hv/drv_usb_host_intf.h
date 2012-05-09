864bf5437cfc49ac55dda74f184f64f74b64511f
Chris Metcalf <cmetcalf@tilera.com>
2012-05-09 12:25:02 -0400

    arch/tile: provide kernel support for the tilegx USB shim
    This change adds support for accessing the USB shim from within the
    kernel.  Note that this change by itself does not allow the kernel
    to act as a host or as a device; it merely exposes the built-in on-chip
    hardware to the kernel.
    
    The <arch/usb_host.h> and <arch/usb_host_def.h> headers are empty at
    the moment because the kernel does not require any types or definitions
    specific to the tilegx USB shim; the generic USB core code is all we need.
    The headers are left in as stubs so that we don't need to modify the
    hypervisor header (drv_usb_host_intf.h) from upstream.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

---
+++ arch/tile/include/hv/drv_usb_host_intf.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright 2012 Tilera Corporation. All Rights Reserved.
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation, version 2.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
+ *   NON INFRINGEMENT.  See the GNU General Public License for
+ *   more details.
+ */
+
+/**
+ * Interface definitions for the USB host driver.
+ */
+
+#ifndef _SYS_HV_DRV_USB_HOST_INTF_H
+#define _SYS_HV_DRV_USB_HOST_INTF_H
+
+#include <arch/usb_host.h>
+
+
+/** Offset for the EHCI register MMIO region. */
+#define HV_USB_HOST_MMIO_OFFSET_EHCI ((uint64_t) USB_HOST_HCCAPBASE_REG)
+
+/** Offset for the OHCI register MMIO region. */
+#define HV_USB_HOST_MMIO_OFFSET_OHCI ((uint64_t) USB_HOST_OHCD_HC_REVISION_REG)
+
+/** Size of the register MMIO region.  This turns out to be the same for
+ *  both EHCI and OHCI. */
+#define HV_USB_HOST_MMIO_SIZE ((uint64_t) 0x1000)
+
+/** The number of service domains supported by the USB host shim. */
+#define HV_USB_HOST_NUM_SVC_DOM 1
+
+
+#endif /* _SYS_HV_DRV_USB_HOST_INTF_H */
