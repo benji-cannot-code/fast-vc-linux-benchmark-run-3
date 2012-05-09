48413a6f87abaa431fc2989bcef04ca9d13fecaf
Avinash Patil <patila@marvell.com>
2012-05-08 18:30:24 -0700

    mwifiex: add cfg80211 start_ap and stop_ap handlers
    1. add start AP and stop AP handlers
    2. support for parsing ssid, beacon period, DTIM period
    from beacon_parameters structure and setting it to FW.
    
    Signed-off-by: Avinash Patil <patila@marvell.com>
    Signed-off-by: Yogesh Ashok Powar <yogeshp@marvell.com>
    Signed-off-by: Kiran Divekar <dkiran@marvell.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwifiex/decl.h
+++ drivers/net/wireless/mwifiex/decl.h
@@ -56,6 +56,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MWIFIEX_RX_DATA_BUF_SIZE     (4 * 1024)
 #define MWIFIEX_RX_CMD_BUF_SIZE	     (2 * 1024)
 
+#define MAX_BEACON_PERIOD                  (4000)
+#define MIN_BEACON_PERIOD                  (50)
+#define MAX_DTIM_PERIOD                    (100)
+#define MIN_DTIM_PERIOD                    (1)
+
 #define MWIFIEX_RTS_MIN_VALUE              (0)
 #define MWIFIEX_RTS_MAX_VALUE              (2347)
 #define MWIFIEX_FRAG_MIN_VALUE             (256)
@@ -92,6 +97,11 @@ struct mwifiex_fw_image {
 	u32 helper_len;
 	u8 *fw_buf;
 	u32 fw_len;
+};
+
+struct mwifiex_802_11_ssid {
+	u32 ssid_len;
+	u8 ssid[IEEE80211_MAX_SSID_LEN];
 };
 
 struct mwifiex_wait_queue {
