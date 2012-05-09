fc0848b286e6e661c6e61b8ea431746ad5f7c48a
Chris Wilson <chris@chris-wilson.co.uk>
2012-05-09 11:56:28 +0100

    drm/i915: gen6_enable_rps() wants to be called after ring initialisation
    Currently we call gen6_enable_rps() (which writes into the per-ring
    register mmio space) from intel_modeset_init_hw() which is called before
    we initialise the rings. If we defer intel_modeset_init_hw() until
    afterwards (in the intel_modeset_gem_init() phase) all is well.
    
    v2: Rectify ordering of gem vs display HW init upon resume. (Daniel)
    
    v3: Fix up locking. (Paulo)
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    [danvet: Smash Paulo's locking fix onto Chris' patch.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -6659,6 +6659,7 @@ void intel_modeset_init_hw(struct drm_device *dev)
 
 	if (IS_IRONLAKE_M(dev)) {
 		ironlake_enable_drps(dev);
+		ironlake_enable_rc6(dev);
 		intel_init_emon(dev);
 	}
 
@@ -6720,8 +6721,6 @@ void intel_modeset_init(struct drm_device *dev)
 	i915_disable_vga(dev);
 	intel_setup_outputs(dev);
 
-	intel_modeset_init_hw(dev);
-
 	INIT_WORK(&dev_priv->idle_work, intel_idle_update);
 	setup_timer(&dev_priv->idle_timer, intel_gpu_idle_timer,
 		    (unsigned long)dev);
@@ -6729,8 +6728,7 @@ void intel_modeset_init(struct drm_device *dev)
 
 void intel_modeset_gem_init(struct drm_device *dev)
 {
-	if (IS_IRONLAKE_M(dev))
-		ironlake_enable_rc6(dev);
+	intel_modeset_init_hw(dev);
 
 	intel_setup_overlay(dev);
 }
