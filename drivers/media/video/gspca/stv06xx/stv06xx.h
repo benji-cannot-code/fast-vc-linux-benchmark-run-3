dc34acae23e8d79c796a5742e32d36a0f246f0c5
Hans de Goede <hdegoede@redhat.com>
2012-05-09 11:22:47 -0300

    [media] gscpa_stv06xx: Make sd_desc const
    Now that stv06xx is using the control framework it is no longer necessary
    to have a (non const) copy of sd_desc inside the sd specific data struct.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/gspca/stv06xx/stv06xx.h
+++ drivers/media/video/gspca/stv06xx/stv06xx.h
@@ -93,9 +93,6 @@ struct sd {
 	/* Control handler */
 	struct v4l2_ctrl_handler ctrl_handler;
 
-	/* A pointer to the sd_desc struct */
-	struct sd_desc desc;
-
 	/* Sensor private data */
 	void *sensor_priv;
 
