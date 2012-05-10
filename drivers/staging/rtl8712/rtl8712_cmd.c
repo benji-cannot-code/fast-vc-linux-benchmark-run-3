0f6ad5848cf69123bbcb38be0bdde9c2a3a9cc3b
Paul Gortmaker <paul.gortmaker@windriver.com>
2012-05-09 23:10:54 -0400

    staging: delete all duplicated endian crap from rtl8712 driver
    This driver had headers like big_endian.h, little_endian.h, swab.h
    and yet we can throw them all in the trash can and the thing
    still builds on x86-64 and ppc, just by deleting the references
    to the deleted files.
    
    Cc: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/staging/rtl8712/rtl8712_cmd.c
+++ drivers/staging/rtl8712/rtl8712_cmd.c
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "drv_types.h"
 #include "recv_osdep.h"
 #include "mlme_osdep.h"
-#include "rtl871x_byteorder.h"
 #include "rtl871x_ioctl_set.h"
 
 static void check_hw_pbc(struct _adapter *padapter)
--- drivers/staging/rtl8712/xmit_linux.c
+++ drivers/staging/rtl8712/xmit_linux.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "osdep_service.h"
 #include "drv_types.h"
 
-#include "rtl871x_byteorder.h"
 #include "wifi.h"
 #include "mlme_osdep.h"
 #include "xmit_osdep.h"
