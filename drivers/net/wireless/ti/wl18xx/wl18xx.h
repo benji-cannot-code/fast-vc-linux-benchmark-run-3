42850b29bfe04e9288d0876d2b93dc5dd9aaf0b9
Luciano Coelho <coelho@ti.com>
2012-05-10 12:13:13 +0300

    wl18xx: add trigger command and ack event operations
    Add the operations that allow wlcore to trigger commands to the
    firmware and acknowledge when an event has been fully received.
    
    Allocate a private buffer to hold the maximum sized cmd. Send the
    entire length of the buffer each time a command is sent to signal
    EOT. Remove the previous EOT mechanism.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

---
+++ drivers/net/wireless/ti/wl18xx/wl18xx.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * This file is part of wl18xx
+ *
+ * Copyright (C) 2011 Texas Instruments Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#ifndef __WL18XX_PRIV_H__
+#define __WL18XX_PRIV_H__
+
+
+#define WL18XX_CMD_MAX_SIZE          740
+
+struct wl18xx_priv {
+	/* buffer for sending commands to FW */
+	u8 cmd_buf[WL18XX_CMD_MAX_SIZE];
+};
+
+#endif /* __WL18XX_PRIV_H__ */
