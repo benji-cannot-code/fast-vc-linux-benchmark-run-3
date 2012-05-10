f0af918a317cba29cb19bcc69278920f43b4fcc6
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-05-10 11:15:14 +0300

    OMAPFB: fix parsing of vram parameter
    omapfb_parse_vram_param()'s check for end pointer returned from
    simple_strtoul() is wrong, causing the code to bug if the second or
    later vram parameters are non-parseable, for example
    "omapfb.vram=0:2M,:5M".
    
    However, even in that case the code will most likely bail out with
    -EINVAL in the following checks, so the bug is probably not a fatal one.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reported-by: Hein Tibosch <hein_tibosch@yahoo.es>

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -1503,7 +1503,7 @@ static int omapfb_parse_vram_param(const char *param, int max_entries,
 
 		fbnum = simple_strtoul(p, &p, 10);
 
-		if (p == param)
+		if (p == start)
 			return -EINVAL;
 
 		if (*p != ':')
