0108fa6ef81a89d04c329610f6075e867f7378fe
Arik Nemtsov <arik@wizery.com>
2012-05-10 12:13:27 +0300

    wlcore/wl18xx: add hw op for setting Tx HW checksum
    Some chip families are capable of checksumming certain classes of Tx
    packets in HW. Indicate this fact in the netdev features and perform the
    HW checksum by protocol type for the 18xx family.
    
    Fix the location of the skb network header when we move it so we can
    rely on it when setting the checksum.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wlcore/acx.h
+++ drivers/net/wireless/ti/wlcore/acx.h
@@ -1246,6 +1246,7 @@ enum {
 	ACX_CONFIG_HANGOVER              = 0x0042,
 	ACX_FEATURE_CFG                  = 0x0043,
 	ACX_PROTECTION_CFG               = 0x0044,
+	ACX_CHECKSUM_CONFIG              = 0x0045,
 };
 
 
