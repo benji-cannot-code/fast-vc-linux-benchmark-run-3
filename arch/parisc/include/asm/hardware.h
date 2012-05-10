0a033f899065d927aff02986aef5e16ec626b6a4
Rolf Eike Beer <eike-kernel@sf-tec.de>
2012-05-10 23:03:26 +0200

    parisc: drop include of asm/pdc.h from asm/hardware.h
    It seems none of the symbols defined by pdc.h is needed, but it introduces an
    include loop causing compile errors:
    
    In file included from arch/parisc/include/asm/spinlock.h:4:0,
                     from arch/parisc/include/asm/atomic.h:20,
                     from include/linux/atomic.h:4,
                     from arch/parisc/include/asm/bitops.h:56,
                     from include/linux/bitops.h:35,
                     from include/asm-generic/getorder.h:7,
                     from arch/parisc/include/asm/page.h:162,
                     from arch/parisc/include/asm/pdc.h:346,
                     from arch/parisc/include/asm/hardware.h:5,
                     from arch/parisc/kernel/hardware.c:30:
    arch/parisc/include/asm/processor.h:74:16: error: field ‘cpu_type’ has incomplete type
    arch/parisc/include/asm/processor.h:77:20: error: field ‘model’ has incomplete type
    arch/parisc/include/asm/processor.h: In function ‘parisc_requires_coherency’:
    arch/parisc/include/asm/processor.h:349:36: error: ‘mako’ undeclared (first use in this function)
    arch/parisc/include/asm/processor.h:349:36: note: each undeclared identifier is reported only once for each function it appears in
    arch/parisc/include/asm/processor.h:350:30: error: ‘mako2’ undeclared (first use in this function)
    
    Signed-off-by: Rolf Eike Beer <eike-kernel@sf-tec.de>
    Acked-by: Grant Grundler <grantgrundler@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/parisc/include/asm/hardware.h
+++ arch/parisc/include/asm/hardware.h
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PARISC_HARDWARE_H
 
 #include <linux/mod_devicetable.h>
-#include <asm/pdc.h>
 
 #define HWTYPE_ANY_ID		PA_HWTYPE_ANY_ID
 #define HVERSION_ANY_ID		PA_HVERSION_ANY_ID
