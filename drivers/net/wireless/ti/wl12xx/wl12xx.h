163f643c4250364a6a94a1262b53d72542044536
Luciano Coelho <coelho@ti.com>
2012-05-10 12:13:45 +0300

    wlcore/wl12xx: move rx_mem_pool_addr element to wl12xx
    Only wl127x chips use the rx_mem_pool_addr values, which need to be
    given to the firmware as part of the RX path.  Move this from core to
    the wl12xx driver.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- drivers/net/wireless/ti/wl12xx/wl12xx.h
+++ drivers/net/wireless/ti/wl12xx/wl12xx.h
@@ -25,6 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "conf.h"
 
+struct wl127x_rx_mem_pool_addr {
+	u32 addr;
+	u32 addr_extra;
+};
+
 struct wl12xx_priv {
 	struct wl12xx_priv_conf conf;
 
