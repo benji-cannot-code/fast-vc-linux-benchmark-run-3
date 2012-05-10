1edcb4cfc73720559a7ab26c8137b54fa99eb67b
Arik Nemtsov <arik@wizery.com>
2012-05-10 12:13:32 +0300

    wl18xx: ipmlement ap_rate_mask hw op
    Enable wide-chan or MIMO rates when appropriate.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wl18xx/tx.h
+++ drivers/net/wireless/ti/wl18xx/tx.h
@@ -32,6 +32,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WL18XX_TX_STATUS_DESC_ID_MASK    0x7F
 #define WL18XX_TX_STATUS_STAT_BIT_IDX    7
 
+/*
+ * The FW uses a special bit to indicate a wide channel should be used in
+ * the rate policy.
+ */
+#define CONF_TX_RATE_USE_WIDE_CHAN BIT(31)
+
 void wl18xx_tx_immediate_complete(struct wl1271 *wl);
 
 #endif /* __WL12XX_TX_H__ */
