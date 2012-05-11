9ffe9395e9ff6db54894ed8baceb05f344749cc8
John Crispin <blogic@openwrt.org>
2012-05-11 18:45:25 +0200

    MIPS: lantiq: drop ltq_gpio_request() and gpio_to_irq()
    As part of the conversion to OF we also implement pinctrl drivers. Previously
    we used ltq_gpio_request() to set pinmuxing. This is now obselete and we can
    hence drop the function.
    
    Additionally we remove gpio_to_irq() from the gpio driver and move it to a
    header file.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/3801/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-lantiq/xway/lantiq_soc.h
+++ arch/mips/include/asm/mach-lantiq/xway/lantiq_soc.h
@@ -154,8 +154,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LTQ_MPS_CHIPID		((u32 *)(LTQ_MPS_BASE_ADDR + 0x0344))
 
 /* request a non-gpio and set the PIO config */
-extern int  ltq_gpio_request(unsigned int pin, unsigned int alt0,
-	unsigned int alt1, unsigned int dir, const char *name);
 extern void ltq_pmu_enable(unsigned int module);
 extern void ltq_pmu_disable(unsigned int module);
 
