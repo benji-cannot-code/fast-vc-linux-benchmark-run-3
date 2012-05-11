b8fe0c5b885c3c92dd382773d285d030880850bd
Laxman Dewangan <ldewangan@nvidia.com>
2012-05-11 12:08:42 +0530

    regulator: tps62360: make init_data of platform data to pointer.
    Convert platform data member regulator_init_data to pointer type.
    This will avoid the copy of entire regualator init data into
    platform data member when adding dt support and it can be achieve
    by simple assignment:
    pdata->init_data = of_get_regulator_init_data(dev, dev->of_node);
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/regulator/tps62360.h
+++ include/linux/regulator/tps62360.h
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_REGULATOR_TPS62360_H
 #define __LINUX_REGULATOR_TPS62360_H
 
-#include <linux/regulator/machine.h>
-
 /*
  * struct tps62360_regulator_platform_data - tps62360 regulator platform data.
  *
@@ -45,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @vsel1_def_state: Default state of vsel1. 1 if it is high else 0.
  */
 struct tps62360_regulator_platform_data {
-	struct regulator_init_data reg_init_data;
+	struct regulator_init_data *reg_init_data;
 	bool en_force_pwm;
 	bool en_discharge;
 	bool en_internal_pulldn;
