fc15707d92b8a069fa2ae050eb5d6aac1163e96a
Heiko Stuebner <heiko@sntech.de>
2012-05-12 16:22:17 +0900

    ARM: S3C24XX: move plat-s3c24xx/cpu.c
    Following the model of s3c64xx cpu.c becomes common.c in mach-s3c24xx,
    to got more common code added later on.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c24xx/common.c
+++ arch/arm/mach-s3c24xx/common.c
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	http://www.simtec.co.uk/products/SWLINUX/
  *	Ben Dooks <ben@simtec.co.uk>
  *
- * S3C24XX CPU Support
+ * Common code for S3C24XX machines
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
