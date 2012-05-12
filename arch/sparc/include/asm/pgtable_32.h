85ab002cf099d10160e0d791041bcf3c3b994d48
David S. Miller <davem@davemloft.net>
2012-05-11 21:07:50 -0700

    sparc32: Kill asm/vac-ops.h
    All sun4/sun4c stuff and unused.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/pgtable_32.h
+++ arch/sparc/include/asm/pgtable_32.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/swap.h>
 #include <asm/types.h>
 #include <asm/pgtsrmmu.h>
-#include <asm/vac-ops.h>
 #include <asm/oplib.h>
 #include <asm/btfixup.h>
 #include <asm/cpu_type.h>
--- arch/sparc/mm/init_32.c
+++ arch/sparc/mm/init_32.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gfp.h>
 
 #include <asm/sections.h>
-#include <asm/vac-ops.h>
 #include <asm/page.h>
 #include <asm/pgtable.h>
 #include <asm/vaddrs.h>
