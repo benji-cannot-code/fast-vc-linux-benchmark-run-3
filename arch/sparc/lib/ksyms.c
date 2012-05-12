a59a1f780e43f616073cde29573b7c3d1eed544a
David S. Miller <davem@davemloft.net>
2012-05-11 19:53:29 -0700

    sparc32: Remove inline strncmp "optimization" for constant counts.
    Let the compiler do stuff like this.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/lib/ksyms.c
+++ arch/sparc/lib/ksyms.c
@@ -57,8 +57,6 @@ extern int __divdi3(int, int);
 extern void (*__copy_1page)(void *, const void *);
 extern void (*bzero_1page)(void *);
 
-extern int __strncmp(const char *, const char *, __kernel_size_t);
-
 extern void ___rw_read_enter(void);
 extern void ___rw_read_try(void);
 extern void ___rw_read_exit(void);
@@ -81,9 +79,6 @@ EXPORT_SYMBOL(__csum_partial_copy_sparc_generic);
 EXPORT_SYMBOL(__copy_1page);
 EXPORT_SYMBOL(__memmove);
 EXPORT_SYMBOL(bzero_1page);
-
-/* string functions */
-EXPORT_SYMBOL(__strncmp);
 
 /* Moving data to/from/in userspace. */
 EXPORT_SYMBOL(__copy_user);
