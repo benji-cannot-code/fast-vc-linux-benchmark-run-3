9d6692af93c52e703f35c81fd1b279ba5b739da6
Sam Ravnborg <sam@ravnborg.org>
2012-05-13 10:30:54 +0200

    sparc32: drop btfixup in mmu_context_32.h
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/srmmu.c
+++ arch/sparc/mm/srmmu.c
@@ -1223,7 +1223,7 @@ static void srmmu_update_mmu_cache(struct vm_area_struct * vma, unsigned long ad
 {
 }
 
-static void srmmu_destroy_context(struct mm_struct *mm)
+void destroy_context(struct mm_struct *mm)
 {
 
 	if(mm->context != NO_CONTEXT) {
@@ -1989,7 +1989,6 @@ void __init load_mmu(void)
 #endif
 
 	BTFIXUPSET_CALL(update_mmu_cache, srmmu_update_mmu_cache, BTFIXUPCALL_NOP);
-	BTFIXUPSET_CALL(destroy_context, srmmu_destroy_context, BTFIXUPCALL_NORM);
 
 	get_srmmu_type();
 
