cb6bb5d4e6fa03f2347285ed7d42ebebe8ef2c3e
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2012-05-14 19:49:35 +0300

    UBI: amend commentaries WRT dtype
    Richard removed the "dtype" hint, but few commentaries were left and this patch
    removes them. I've also added a better description about the "dtype" field in
    the ubi-user.h for people who may ever wonder what was that dtype thing about.
    
    This patch also adds an important note that it is better to use value "3" for
    the "dtype" field.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/ubi/wl.c
+++ drivers/mtd/ubi/wl.c
@@ -42,12 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * physical eraseblocks with low erase counter to free physical eraseblocks
  * with high erase counter.
  *
- * The 'ubi_wl_get_peb()' function accepts data type hints which help to pick
- * an "optimal" physical eraseblock. For example, when it is known that the
- * physical eraseblock will be "put" soon because it contains short-term data,
- * the WL sub-system may pick a free physical eraseblock with low erase
- * counter, and so forth.
- *
  * If the WL sub-system fails to erase a physical eraseblock, it marks it as
  * bad.
  *
@@ -71,8 +65,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *    to the user; instead, we first want to let users fill them up with data;
  *
  *  o there is a chance that the user will put the physical eraseblock very
- *    soon, so it makes sense not to move it for some time, but wait; this is
- *    especially important in case of "short term" physical eraseblocks.
+ *    soon, so it makes sense not to move it for some time, but wait.
  *
  * Physical eraseblocks stay protected only for limited time. But the "time" is
  * measured in erase cycles in this case. This is implemented with help of the
