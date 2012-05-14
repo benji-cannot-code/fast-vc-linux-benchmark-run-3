2d2eb2264db00af9733b7285fe822cd86533d2b8
Chanwoo Choi <cw00.choi@samsung.com>
2012-05-14 22:54:20 +0200

    mfd: Add MAX77693 irq handler
    This patch supports IRQ handling for MAX77693.
    
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>
    Signed-off-by: Myungjoo Ham <myungjoo.ham@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/max77693.h
+++ include/linux/mfd/max77693.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __LINUX_MFD_MAX77693_H
 
 struct max77693_platform_data {
-	/* IRQ */
 	int wakeup;
 };
 #endif	/* __LINUX_MFD_MAX77693_H */
