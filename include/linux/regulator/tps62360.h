063b61de0544d02e28b3890277150d765e9e4180
Laxman Dewangan <ldewangan@nvidia.com>
2012-05-14 17:46:51 +0530

    regulator: tps62360: support force PWM mode via regulator mode
    Change the mechanism of enabling the force PWM mode through
    regulator set mode. This can be dynamically configured now.
    In the REGULATOR_MODE_FAST the force PWM is enabled and in
    REGULATOR_MODE_NORMAL the force PWM is disabled.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/regulator/tps62360.h
+++ include/linux/regulator/tps62360.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * struct tps62360_regulator_platform_data - tps62360 regulator platform data.
  *
  * @reg_init_data: The regulator init data.
- * @en_force_pwm: Enable force pwm or not.
  * @en_discharge: Enable discharge the output capacitor via internal
  *                register.
  * @en_internal_pulldn: internal pull down enable or not.
@@ -44,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct tps62360_regulator_platform_data {
 	struct regulator_init_data *reg_init_data;
-	bool en_force_pwm;
 	bool en_discharge;
 	bool en_internal_pulldn;
 	int vsel0_gpio;
