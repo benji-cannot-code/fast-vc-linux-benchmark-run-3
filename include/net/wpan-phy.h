2434b69fb73762fefe6541cf02e247a6a2f70ac4
alex.bluesman.smirnov@gmail.com <alex.bluesman.smirnov@gmail.com>
2012-05-15 20:50:29 +0000

    mac802154: monitor device support
    Support for monitor device intended to capture all the network activity.
    This interface could be used by networks sniffers and is already
    supported by WireShark. That's a good test point to check that basic
    MAC support works.
    
    Signed-off-by: Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/wpan-phy.h
+++ include/net/wpan-phy.h
@@ -26,6 +26,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/bug.h>
 
+/* According to the IEEE 802.15.4 stadard the upper most significant bits of
+ * the 32-bit channel bitmaps shall be used as an integer value to specify 32
+ * possible channel pages. The lower 27 bits of the channel bit map shall be
+ * used as a bit mask to specify channel numbers within a channel page.
+ */
+#define WPAN_NUM_CHANNELS	27
+#define WPAN_NUM_PAGES		32
+
 struct wpan_phy {
 	struct mutex pib_lock;
 
