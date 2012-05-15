5ffc2cc2cbc20ef328696119e4668872e1601f69
Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
2012-05-15 11:02:48 -0300

    [media] smiapp: Remove smiapp-debug.h in favour of dynamic debug
    Remove smiapp-debug.h and let people use CONFIG_DYNAMIC_DEBUG instead. The
    option only affected to when debug information was being printed.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/smiapp-pll.c
+++ drivers/media/video/smiapp-pll.c
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#include "smiapp/smiapp-debug.h"
-
 #include <linux/gcd.h>
 #include <linux/lcm.h>
 #include <linux/module.h>
