cde623d9f5bf0f48dfdbdc570dd8a472cbba1015
Sonic Zhang <sonic.zhang@analog.com>
2012-05-16 18:10:27 +0800

    blackfin: twi: include linux/i2c.h
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- arch/blackfin/include/asm/bfin_twi.h
+++ arch/blackfin/include/asm/bfin_twi.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_BFIN_TWI_H__
 
 #include <linux/types.h>
+#include <linux/i2c.h>
 
 /*
  * All Blackfin system MMRs are padded to 32bits even if the register
