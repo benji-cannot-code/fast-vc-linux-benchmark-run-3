d6bfa01639be5b1905802b38ee1080e3305808e6
Suresh Siddha <suresh.b.siddha@intel.com>
2012-05-16 15:03:54 -0700

    x86, fpu: drop the fpu state during thread exit
    There is no need to save any active fpu state to the task structure
    memory if the task is dead. Just drop the state instead.
    
    For example, this saved some 1770 xsave's during the system boot
    of a two socket Xeon system.
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Link: http://lkml.kernel.org/r/1336692811-30576-4-git-send-email-suresh.b.siddha@intel.com
    Cc: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -88,6 +88,16 @@ void arch_task_cache_init(void)
 				  SLAB_PANIC | SLAB_NOTRACK, NULL);
 }
 
+static inline void drop_fpu(struct task_struct *tsk)
+{
+	/*
+	 * Forget coprocessor state..
+	 */
+	tsk->fpu_counter = 0;
+	clear_fpu(tsk);
+	clear_used_math();
+}
+
 /*
  * Free current thread data structures etc..
  */
@@ -110,6 +120,8 @@ void exit_thread(void)
 		put_cpu();
 		kfree(bp);
 	}
+
+	drop_fpu(me);
 }
 
 void show_regs(struct pt_regs *regs)
@@ -150,12 +162,7 @@ void flush_thread(void)
 
 	flush_ptrace_hw_breakpoint(tsk);
 	memset(tsk->thread.tls_array, 0, sizeof(tsk->thread.tls_array));
-	/*
-	 * Forget coprocessor state..
-	 */
-	tsk->fpu_counter = 0;
-	clear_fpu(tsk);
-	clear_used_math();
+	drop_fpu(tsk);
 }
 
 static void hard_disable_TSC(void)
