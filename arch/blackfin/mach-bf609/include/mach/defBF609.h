f4c59d75d58af6ee83756dd9b6cbb869a5c7a2fa
Sonic Zhang <sonic.zhang@analog.com>
2012-05-16 17:24:33 +0800

    blackfin: mach-bf609: add machine files
    Add machine files for bf60x including head files, Kconfig/Makefile and board
    file.
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

---
+++ arch/blackfin/mach-bf609/include/mach/defBF609.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright 2011 Analog Devices Inc.
+ *
+ * Licensed under the ADI BSD license or the GPL-2 (or later)
+ */
+
+#ifndef _DEF_BF609_H
+#define _DEF_BF609_H
+
+/* Include defBF60x_base.h for the set of #defines that are common to all ADSP-BF60x processors */
+#include "defBF60x_base.h"
+
+/* The following are the #defines needed by ADSP-BF609 that are not in the common header */
+
+#endif /* _DEF_BF609_H */
