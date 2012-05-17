9178d450d654cf17143cb86f008b6f3067c840b4
Dave Airlie <airlied@redhat.com>
2012-05-17 08:31:29 +0100

    x86/vga: fix build with efi disabled.
    Reported by sfr on -next merge.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- arch/x86/pci/fixup.c
+++ arch/x86/pci/fixup.c
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmi.h>
 #include <linux/pci.h>
 #include <linux/init.h>
+#include <linux/vgaarb.h>
 #include <asm/pci_x86.h>
-#include <asm/vga.h>
 
 static void __devinit pci_fixup_i450nx(struct pci_dev *d)
 {
