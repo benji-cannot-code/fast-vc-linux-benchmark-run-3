85c585900973b9c02d3009c6998f71bfb06cca22
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2012-05-17 15:41:12 +0300

    UBI: rename ubi_scan_destroy_ai
    The old name is not logical anymore - rename it to 'ubi_destroy_ai()'.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/ubi/build.c
+++ drivers/mtd/ubi/build.c
@@ -609,7 +609,7 @@ static int attach_by_scanning(struct ubi_device *ubi)
 	if (err)
 		goto out_wl;
 
-	ubi_scan_destroy_ai(ai);
+	ubi_destroy_ai(ai);
 	return 0;
 
 out_wl:
@@ -618,7 +618,7 @@ out_vtbl:
 	free_internal_volumes(ubi);
 	vfree(ubi->vtbl);
 out_ai:
-	ubi_scan_destroy_ai(ai);
+	ubi_destroy_ai(ai);
 	return err;
 }
 
