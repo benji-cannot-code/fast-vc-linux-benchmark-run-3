13697a2dfd5f2020c148eae1e67f8e7200888e47
H. Peter Anvin <hpa@linux.intel.com>
2012-05-17 10:03:02 -0700

    xen-acpi-processor: Add missing #include <xen/xen.h>
    This file depends on <xen/xen.h>, but the dependency was hidden due
    to: <asm/acpi.h> -> <asm/trampoline.h> -> <asm/io.h> -> <xen/xen.h>
    
    With the removal of <asm/trampoline.h>, this exposed the missing
    
    Reported-by: Ingo Molnar <mingo@kernel.org>
    Cc: Len Brown <lenb@kernel.org>
    Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Jeremy Fitzhardinge <jeremy@goop.org>
    Cc: Jarkko Sakkinen <jarkko.sakkinen@intel.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- drivers/xen/xen-acpi-processor.c
+++ drivers/xen/xen-acpi-processor.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <acpi/acpi_drivers.h>
 #include <acpi/processor.h>
 
+#include <xen/xen.h>
 #include <xen/interface/platform.h>
 #include <asm/xen/hypercall.h>
 
