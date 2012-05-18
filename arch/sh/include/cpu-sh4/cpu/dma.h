4d0893ec2e75facb3d6c49607d3e6b380394e5a4
Paul Mundt <lethal@linux-sh.org>
2012-05-18 17:27:28 +0900

    sh: cpu dma evt2irq migration.
    This migrates the cpu-family relative DMA IRQ definitions over to
    evt2irq() backed hwirq lookups.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/cpu-sh4/cpu/dma.h
+++ arch/sh/include/cpu-sh4/cpu/dma.h
@@ -9,13 +9,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <cpu/dma-sh4a.h>
 
 #else /* CONFIG_CPU_SH4A */
+
+#include <linux/sh_intc.h>
+
 /*
  * SH7750/SH7751/SH7760
  */
-#define DMTE0_IRQ	34
-#define DMTE4_IRQ	44
-#define DMTE6_IRQ	46
-#define DMAE0_IRQ	38
+#define DMTE0_IRQ	evt2irq(0x640)
+#define DMTE4_IRQ	evt2irq(0x780)
+#define DMTE6_IRQ	evt2irq(0x7c0)
+#define DMAE0_IRQ	evt2irq(0x6c0)
 
 #define SH_DMAC_BASE0	0xffa00000
 #define SH_DMAC_BASE1	0xffa00070
