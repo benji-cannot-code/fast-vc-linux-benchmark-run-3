225ec0cd31d31ce01b8eed8adb0ece98746a96dd
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2012-05-18 11:10:07 +0200

    Staging: ipack/bridges/tpci200: remove TPCI200_SHORTNAME constant
    Removed TPCI200_SHORTNAME. For the pr_* the name of the module is already
    included due to pr_fmt declaration.
    
    In other cases, KBUILD_MODNAME is used instead.
    
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ipack/bridges/tpci200.h
+++ drivers/staging/ipack/bridges/tpci200.h
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../ipack.h"
 
-#define TPCI200_SHORTNAME "TPCI200"
-
 #define TPCI200_NB_SLOT               0x4
 #define TPCI200_NB_BAR                0x6
 
