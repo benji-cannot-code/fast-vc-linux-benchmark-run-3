32d978dde2658cde843e011312ef1607d1dbd6c8
Paul Mundt <lethal@linux-sh.org>
2012-05-18 15:38:50 +0900

    sh: hp6xx evt2irq migration.
    Migrate hp6xx to evt2irq backed hwirq lookups.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/mach-hp6xx/setup.c
+++ arch/sh/boards/mach-hp6xx/setup.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/irq.h>
+#include <linux/sh_intc.h>
 #include <sound/sh_dac_audio.h>
 #include <asm/hd64461.h>
 #include <asm/io.h>
@@ -36,7 +37,7 @@ static struct resource cf_ide_resources[] = {
 		.flags = IORESOURCE_MEM,
 	},
 	[2] = {
-		.start = 77,
+		.start = evt2irq(0xba0),
 		.flags = IORESOURCE_IRQ,
 	},
 };
