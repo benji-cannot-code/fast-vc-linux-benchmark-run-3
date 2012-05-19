444bf524f538342c5103d2f5297d6fc47e3958ad
Paul Mundt <lethal@linux-sh.org>
2012-05-19 18:50:09 +0900

    sh: Kill off MAX_DMA_ADDRESS leftovers.
    We don't support the ISA DMA API, so this is only ever misused. The
    dma-sh case inadvertently broke the dreamcast case by testing the wrong
    variable for the total number of channels, so this fixes that up too.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/include/asm/dma-sh.h
+++ arch/sh/include/asm/dma-sh.h
@@ -33,21 +33,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 static int dmte_irq_map[] __maybe_unused = {
-#if (MAX_DMA_CHANNELS >= 4)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 4)
     DMTE0_IRQ,
     DMTE0_IRQ + 1,
     DMTE0_IRQ + 2,
     DMTE0_IRQ + 3,
 #endif
-#if (MAX_DMA_CHANNELS >= 6)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 6)
     DMTE4_IRQ,
     DMTE4_IRQ + 1,
 #endif
-#if (MAX_DMA_CHANNELS >= 8)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 8)
     DMTE6_IRQ,
     DMTE6_IRQ + 1,
 #endif
-#if (MAX_DMA_CHANNELS >= 12)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 12)
     DMTE8_IRQ,
     DMTE9_IRQ,
     DMTE10_IRQ,
@@ -63,21 +63,21 @@ static int dmte_irq_map[] __maybe_unused = {
 
 /* DMA base address */
 static u32 dma_base_addr[] __maybe_unused = {
-#if (MAX_DMA_CHANNELS >= 4)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 4)
 	SH_DMAC_BASE0 + 0x00,	/* channel 0 */
 	SH_DMAC_BASE0 + 0x10,
 	SH_DMAC_BASE0 + 0x20,
 	SH_DMAC_BASE0 + 0x30,
 #endif
-#if (MAX_DMA_CHANNELS >= 6)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 6)
 	SH_DMAC_BASE0 + 0x50,
 	SH_DMAC_BASE0 + 0x60,
 #endif
-#if (MAX_DMA_CHANNELS >= 8)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 8)
 	SH_DMAC_BASE1 + 0x00,
 	SH_DMAC_BASE1 + 0x10,
 #endif
-#if (MAX_DMA_CHANNELS >= 12)
+#if (CONFIG_NR_ONCHIP_DMA_CHANNELS >= 12)
 	SH_DMAC_BASE1 + 0x20,
 	SH_DMAC_BASE1 + 0x30,
 	SH_DMAC_BASE1 + 0x50,
--- arch/sh/include/asm/dma.h
+++ arch/sh/include/asm/dma.h
@@ -18,14 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <asm-generic/dma.h>
 
-#ifdef CONFIG_NR_DMA_CHANNELS
-#  define MAX_DMA_CHANNELS	(CONFIG_NR_DMA_CHANNELS)
-#elif defined(CONFIG_NR_ONCHIP_DMA_CHANNELS)
-#  define MAX_DMA_CHANNELS	(CONFIG_NR_ONCHIP_DMA_CHANNELS)
-#else
-#  define MAX_DMA_CHANNELS	0
-#endif
-
 /*
  * Read and write modes can mean drastically different things depending on the
  * channel configuration. Consult your DMAC documentation and module
