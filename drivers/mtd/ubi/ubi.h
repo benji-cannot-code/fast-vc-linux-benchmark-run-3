dc57e61ccd103868883743fda05c9b51c09f10e7
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2012-05-20 21:14:22 +0300

    UBI: introduce UBI_ALL constant
    Joel will use it in his 'ubi_flush()' extention to specify all eraseblocks.
    Also amend the comment for UBI_UNKNOWN - it is used beyond attaching info
    structure now.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -66,7 +66,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Background thread name pattern */
 #define UBI_BGT_NAME_PATTERN "ubi_bgt%dd"
 
-/* This marker in the EBA table means that the LEB is um-mapped */
+/*
+ * This marker in the EBA table means that the LEB is um-mapped.
+ * NOTE! It has to have the same value as %UBI_ALL.
+ */
 #define UBI_LEB_UNMAPPED -1
 
 /*
@@ -82,10 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define UBI_PROT_QUEUE_LEN 10
 
-/*
- * When a field of the attaching information has this value - its real value is
- * unknown.
- */
+/* The volume ID/LEB number/erase counter is unknown */
 #define UBI_UNKNOWN -1
 
 /*
