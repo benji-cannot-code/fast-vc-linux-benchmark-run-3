ccfc58f1edf1accf71ff3278189023c83b89c6f2
Wim Van Sebroeck <wim@iguana.be>
2012-05-21 15:31:06 +0200

    watchdog: correct the name of the watchdog_core inlude file
    The watchdog_core include file should have been named
    watchdog_core.h and not watchdog_dev.h . Correct this.
    
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/watchdog_core.c
+++ drivers/watchdog/watchdog_core.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/watchdog.h>	/* For watchdog specific items */
 #include <linux/init.h>		/* For __init/__exit/... */
 
-#include "watchdog_dev.h"	/* For watchdog_dev_register/... */
+#include "watchdog_core.h"	/* For watchdog_dev_register/... */
 
 /**
  * watchdog_register_device() - register a watchdog device
--- drivers/watchdog/watchdog_dev.c
+++ drivers/watchdog/watchdog_dev.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>		/* For __init/__exit/... */
 #include <linux/uaccess.h>	/* For copy_to_user/put_user/... */
 
-#include "watchdog_dev.h"
+#include "watchdog_core.h"
 
 /* make sure we only register one /dev/watchdog device */
 static unsigned long watchdog_dev_busy;
