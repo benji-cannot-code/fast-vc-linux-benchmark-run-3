4fed9dbdef9d0bf640ae198c14d505b7a5549211
Wim Van Sebroeck <wim@iguana.be>
2012-05-21 15:33:05 +0200

    watchdog: watchdog_core.h: make functions extern
    Make the functions in watchdog_core.h extern like it should.
    
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/watchdog_core.h
+++ drivers/watchdog/watchdog_core.h
@@ -30,5 +30,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *	Functions/procedures to be called by the core
  */
-int watchdog_dev_register(struct watchdog_device *);
-int watchdog_dev_unregister(struct watchdog_device *);
+extern int watchdog_dev_register(struct watchdog_device *);
+extern int watchdog_dev_unregister(struct watchdog_device *);
