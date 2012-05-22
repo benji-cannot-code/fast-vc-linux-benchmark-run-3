3f1fda4398071e1728663cb950da59cff4614b17
Stephen Warren <swarren@nvidia.com>
2012-05-22 16:09:52 -0600

    ASoC: tegra+alc5632: unconditionally free jack GPIOs in remove
    The headphone jack GPIOs are added/initialized in the DAI link's init()
    method, and hence in theory may not always have been added before remove()
    is called in some unusual cases. In order to prevent calling
    snd_soc_jack_free_gpios() if snd_soc_jack_add_gpios() had not been, the
    code kept track of the initialization state to avoid the free call when
    necessary.
    
    However, it appears that snd_soc_jack_free_gpios() is robust in the face
    of being called without snd_soc_jack_add_gpios() first succeeding, so
    there is little point manually tracking this information. Hence, remove
    the tracking code. All other machine drivers already operate this way.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/tegra/tegra_alc5632.c
+++ sound/soc/tegra/tegra_alc5632.c
@@ -34,11 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME "tegra-alc5632"
 
-#define GPIO_HP_DET     BIT(0)
-
 struct tegra_alc5632 {
 	struct tegra_asoc_utils_data util_data;
-	int gpio_requested;
 	int gpio_hp_det;
 };
 
@@ -124,7 +121,6 @@ static int tegra_alc5632_asoc_init(struct snd_soc_pcm_runtime *rtd)
 		snd_soc_jack_add_gpios(&tegra_alc5632_hs_jack,
 						1,
 						&tegra_alc5632_hp_jack_gpio);
-		machine->gpio_requested |= GPIO_HP_DET;
 	}
 
 	snd_soc_dapm_force_enable_pin(dapm, "MICBIAS1");
@@ -237,11 +233,8 @@ static int __devexit tegra_alc5632_remove(struct platform_device *pdev)
 	struct snd_soc_card *card = platform_get_drvdata(pdev);
 	struct tegra_alc5632 *machine = snd_soc_card_get_drvdata(card);
 
-	if (machine->gpio_requested & GPIO_HP_DET)
-		snd_soc_jack_free_gpios(&tegra_alc5632_hs_jack,
-					1,
-					&tegra_alc5632_hp_jack_gpio);
-	machine->gpio_requested = 0;
+	snd_soc_jack_free_gpios(&tegra_alc5632_hs_jack, 1,
+				&tegra_alc5632_hp_jack_gpio);
 
 	snd_soc_unregister_card(card);
 
