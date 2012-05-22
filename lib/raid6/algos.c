2ec13add9a09390e91a91f3a57c7f7df7bee8000
Jim Kukunas <james.t.kukunas@linux.intel.com>
2012-05-22 13:54:16 +1000

    lib/raid6: fix test program build
    <linux/module.h> drags in headers which are not visible to userspace,
    thus breaking the build for the test program.
    
    Signed-off-by: Jim Kukunas <james.t.kukunas@linux.intel.com>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- lib/raid6/algos.c
+++ lib/raid6/algos.c
@@ -18,11 +18,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/raid/pq.h>
-#include <linux/module.h>
 #ifndef __KERNEL__
 #include <sys/mman.h>
 #include <stdio.h>
 #else
+#include <linux/module.h>
 #include <linux/gfp.h>
 #if !RAID6_USE_EMPTY_ZERO_PAGE
 /* In .bss so it's zeroed */
