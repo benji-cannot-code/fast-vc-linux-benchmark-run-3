c19b441ea58d0172f526f558c66a70ce36d3db76
David S. Miller <davem@davemloft.net>
2012-05-23 19:56:06 -0700

    sparc: Increase portability of strncpy_from_user() implementation.
    Hide details of maximum user address calculation in a new
    asm/uaccess.h interface named user_addr_max().
    
    Provide little-endian implementation in find_zero(), which should work
    but can probably be improved.
    
    Abstrace alignment check behind IS_UNALIGNED() macro.
    
    Kill double-semicolon, noticed by David Howells.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/uaccess.h
+++ arch/sparc/include/asm/uaccess.h
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess_32.h>
 #endif
 
+#define user_addr_max() \
+	(segment_eq(get_fs(), USER_DS) ? STACK_TOP : ~0UL)
+
 extern long strncpy_from_user(char *dest, const char __user *src, long count);
 
 #endif
