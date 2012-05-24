2270907873e6e5dbe2861722b0ba983e8db2388c
David S. Miller <davem@davemloft.net>
2012-05-23 20:12:50 -0700

    kernel: Move REPEAT_BYTE definition into linux/kernel.h
    And make sure that everything using it explicitly includes
    that header file.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/lib/usercopy.c
+++ arch/sparc/lib/usercopy.c
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/uaccess.h>
+#include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/bug.h>
 
@@ -11,8 +12,6 @@ void copy_from_user_overflow(void)
 	WARN(1, "Buffer overflow detected!\n");
 }
 EXPORT_SYMBOL(copy_from_user_overflow);
-
-#define REPEAT_BYTE(x)	((~0ul / 0xff) * (x))
 
 static inline long find_zero(unsigned long mask)
 {
