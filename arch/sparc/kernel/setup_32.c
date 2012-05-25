d61e159e858a1f0fb97108e08eb62d3ced86de60
Sam Ravnborg <sam@ravnborg.org>
2012-05-25 21:20:08 +0000

    sparc32: introduce sparc32_start_kernel called from head_32.S
    This gives us a C hook before we call start_kernel()
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- arch/sparc/kernel/setup_32.c
+++ arch/sparc/kernel/setup_32.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/kdebug.h>
 #include <linux/export.h>
+#include <linux/start_kernel.h>
 
 #include <asm/io.h>
 #include <asm/processor.h>
@@ -244,6 +245,15 @@ EXPORT_SYMBOL(sparc_cpu_model);
 struct tt_entry *sparc_ttable;
 
 struct pt_regs fake_swapper_regs;
+
+/* Called from head_32.S - before we have setup anything
+ * in the kernel. Be very careful with what you do here.
+ */
+void __init sparc32_start_kernel(struct linux_romvec *rp)
+{
+	prom_init(rp);
+	start_kernel();
+}
 
 void __init setup_arch(char **cmdline_p)
 {
