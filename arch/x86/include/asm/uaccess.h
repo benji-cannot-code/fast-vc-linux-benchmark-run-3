513f940d4c8659094e41be90999ad247dad046f7
Linus Torvalds <torvalds@linux-foundation.org>
2012-05-26 10:14:39 -0700

    x86: use generic strncpy_from_user routine
    The generic strncpy_from_user() is not really optimal, since it is
    designed to work on both little-endian and big-endian.  And on
    little-endian you can simplify much of the logic to find the first zero
    byte, since little-endian arithmetic doesn't have to worry about the
    carry bit propagating into earlier bytes (only later bytes, which we
    don't care about).
    
    But I have patches to make the generic routines use the architecture-
    specific <asm/word-at-a-time.h> infrastructure, so that we can regain
    the little-endian optimizations.  But before we do that, switch over to
    the generic routines to make the patches each do just one well-defined
    thing.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/x86/include/asm/uaccess.h
+++ arch/x86/include/asm/uaccess.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define segment_eq(a, b)	((a).seg == (b).seg)
 
+#define user_addr_max() (current_thread_info()->addr_limit.seg)
 #define __addr_ok(addr)					\
 	((unsigned long __force)(addr) <		\
 	 (current_thread_info()->addr_limit.seg))
