fe0bd6729fdf7050355905a321b76f0e521f13d1
Heiko Carstens <heiko.carstens@de.ibm.com>
2012-05-29 07:33:59 +0200

    s390/uaccess: fix access_ok compile warnings
    On s390 access_ok is a macro which discards all parameters and always
    returns 1. This can result in compile warnings which warn about unused
    variables like this:
    
    fs/read_write.c: In function 'rw_copy_check_uvector':
    fs/read_write.c:684:16: warning: unused variable 'buf' [-Wunused-variable]
    
    Fix this by adding a __range_ok() function which consumes all parameters
    but still always returns 1.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/uaccess.h
+++ arch/s390/include/asm/uaccess.h
@@ -51,10 +51,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define segment_eq(a,b) ((a).ar4 == (b).ar4)
 
-#define __access_ok(addr, size)	\
-({				\
-	__chk_user_ptr(addr);	\
-	1;			\
+static inline int __range_ok(unsigned long addr, unsigned long size)
+{
+	return 1;
+}
+
+#define __access_ok(addr, size)				\
+({							\
+	__chk_user_ptr(addr);				\
+	__range_ok((unsigned long)(addr), (size));	\
 })
 
 #define access_ok(type, addr, size) __access_ok(addr, size)
