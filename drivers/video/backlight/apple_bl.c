49e6ad4701cba2e4010a3a6c83e8739a2e0e1f1f
H Hartley Sweeten <hartleys@visionengravers.com>
2012-05-29 15:07:14 -0700

    drivers/video/backlight/apple_bl.c: include header for exported symbol prototypes
    Include the header to pickup the exported symbol prototype.
    
    Quiets the sparse warning:
    
      warning: symbol 'apple_bl_register' was not declared. Should it be static?
      warning: symbol 'apple_bl_unregister' was not declared. Should it be static?
    
    [akpm@linux-foundation.org: fix resulting build error]
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/backlight/apple_bl.c
+++ drivers/video/backlight/apple_bl.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/acpi.h>
 #include <linux/atomic.h>
+#include <linux/apple_bl.h>
 
 static struct backlight_device *apple_backlight_device;
 
