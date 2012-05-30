7d891830bb6f062f8cadc791ad838b657f5763dc
Antti Palosaari <crope@iki.fi>
2012-05-29 22:20:24 -0300

    [media] dvb_usb_v2: get rid of (struct dvb_usb_adapter_fe_properties)
    Get rid of (struct dvb_usb_adapter_fe_properties) as we no longer
    need it. Frontends are now defined as a array of pointers inside
    adapter struct.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/dvb_usb.h
+++ drivers/media/dvb/dvb-usb/dvb_usb.h
@@ -114,9 +114,6 @@ struct usb_data_stream_properties {
  *  pll_desc and pll_init_buf of struct dvb_usb_device).
  * @stream: configuration of the USB streaming
  */
-struct dvb_usb_adapter_fe_properties {
-	int size_of_priv;
-};
 
 #define MAX_NO_OF_FE_PER_ADAP 3
 struct dvb_usb_adapter_properties {
@@ -140,7 +137,6 @@ struct dvb_usb_adapter_properties {
 			unsigned int, void *, unsigned int);
 
 	int num_frontends;
-	struct dvb_usb_adapter_fe_properties fe[MAX_NO_OF_FE_PER_ADAP];
 	struct usb_data_stream_properties stream;
 };
 
@@ -307,15 +303,6 @@ struct usb_data_stream {
  *
  * @stream: the usb data stream.
  */
-struct dvb_usb_fe_adapter {
-	struct dvb_frontend *fe;
-
-	int (*fe_init)  (struct dvb_frontend *);
-	int (*fe_sleep) (struct dvb_frontend *);
-
-	void *priv;
-};
-
 struct dvb_usb_adapter {
 #define DVB_USB_ADAP_STATE_INIT 0x000
 #define DVB_USB_ADAP_STATE_DVB  0x001
@@ -335,7 +322,10 @@ struct dvb_usb_adapter {
 	struct dvb_demux     demux;
 	struct dvb_net       dvb_net;
 
-	struct dvb_usb_fe_adapter fe_adap[MAX_NO_OF_FE_PER_ADAP];
+	struct dvb_frontend *fe[MAX_NO_OF_FE_PER_ADAP];
+	int (*fe_init[MAX_NO_OF_FE_PER_ADAP]) (struct dvb_frontend *);
+	int (*fe_sleep[MAX_NO_OF_FE_PER_ADAP]) (struct dvb_frontend *);
+
 	int active_fe;
 	int num_frontends_initialized;
 
