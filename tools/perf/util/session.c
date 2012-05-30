811e9b614f98a710dfa65dfa5629b3059af3b953
David Ahern <dsahern@gmail.com>
2012-05-29 22:58:26 -0600

    perf script: Fix regression in callchain dso name
    $ perf script -i /tmp/perf.data
    ...
    gcc 13623 544315.062858: context-switches:
        ffffffff815f65c9 __schedule ([kernel.kallsyms])
        ffffffff81087cea __cond_resched ([kernel.kallsyms])
        ffffffff815f6b92 _cond_resched ([kernel.kallsyms])
        ffffffff815fb87a do_page_fault ([kernel.kallsyms])
        ffffffff815f8465 page_fault ([kernel.kallsyms])
            2b7a71ea0303 _dl_lookup_symbol_x ([kernel.kallsyms])
            2b7a71ea1eb5 _dl_relocate_object ([kernel.kallsyms])
            2b7a71e99b2e dl_main ([kernel.kallsyms])
            2b7a71eab7f4 _dl_sysdep_start ([kernel.kallsyms])
    
    All DSO's in a callchain are printed as [kernel.kallsyms].
    
    git bisect chased it to:
    
    547a92e0aedb88129e7fbd804697a11949de2e5a is the first bad commit
    commit 547a92e0aedb88129e7fbd804697a11949de2e5a
    Author: Akihiro Nagai <akihiro.nagai.hw@hitachi.com>
    Date:   Mon Jan 30 13:42:57 2012 +0900
    
        perf script: Unify the expressions indicating "unknown"
    
        The perf script command uses various expressions to indicate "unknown".
    
        It is unfriendly for user scripts to parse it. So, this patch unifies
        the expressions to "[unknown]".
    
    Looks like a copy-paste in that the other references use al.map but this one
    should be node->map.
    
    With this patch you get:
    
    $ perf script -i /tmp/perf.data
    ...
    gcc 13623 544315.062858: context-switches:
        ffffffff815f65c9 __schedule ([kernel.kallsyms])
        ffffffff81087cea __cond_resched ([kernel.kallsyms])
        ffffffff815f6b92 _cond_resched ([kernel.kallsyms])
        ffffffff815fb87a do_page_fault ([kernel.kallsyms])
        ffffffff815f8465 page_fault ([kernel.kallsyms])
            2b7a71ea0303 _dl_lookup_symbol_x (/lib64/ld-2.14.90.so)
            2b7a71ea1eb5 _dl_relocate_object (/lib64/ld-2.14.90.so)
            2b7a71e99b2e dl_main (/lib64/ld-2.14.90.so)
            2b7a71eab7f4 _dl_sysdep_start (/lib64/ld-2.14.90.so)
    
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Cc: Akihiro Nagai <akihiro.nagai.hw@hitachi.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1338353906-60706-1-git-send-email-dsahern@gmail.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -1461,7 +1461,7 @@ void perf_event__print_ip(union perf_event *event, struct perf_sample *sample,
 			}
 			if (print_dso) {
 				printf(" (");
-				map__fprintf_dsoname(al.map, stdout);
+				map__fprintf_dsoname(node->map, stdout);
 				printf(")");
 			}
 			printf("\n");
