c1d1216b072a7b756f508a8fe590509503c71f6b
Anton Vorontsov <anton.vorontsov@linaro.org>
2012-05-31 16:26:25 -0700

    um: should hold tasklist_lock while traversing processes
    Traversing the tasks requires holding tasklist_lock, otherwise it is
    unsafe.
    
    p.s.  However, I'm not sure that calling os_kill_ptraced_process() in the
    atomic context is correct.  It seem to work, but please take a closer
    look.
    
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/kernel/reboot.c
+++ arch/um/kernel/reboot.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "linux/sched.h"
+#include "linux/spinlock.h"
 #include "linux/slab.h"
 #include "kern_util.h"
 #include "os.h"
@@ -23,6 +24,7 @@ static void kill_off_processes(void)
 		struct task_struct *p;
 		int pid;
 
+		read_lock(&tasklist_lock);
 		for_each_process(p) {
 			if (p->mm == NULL)
 				continue;
@@ -30,6 +32,7 @@ static void kill_off_processes(void)
 			pid = p->mm->context.id.u.pid;
 			os_kill_ptraced_process(pid, 1);
 		}
+		read_unlock(&tasklist_lock);
 	}
 }
 
