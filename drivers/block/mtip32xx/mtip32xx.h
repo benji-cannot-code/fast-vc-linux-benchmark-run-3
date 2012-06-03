c83456c46ea301460d389a431a06c820a3d0f6cc
Sachin Kamat <sachin.kamat@linaro.org>
2012-06-03 21:56:21 +0530

    mtip32xx: Remove version.h header file inclusion
    version.h header file inclusion is no longer required.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>

--- drivers/block/mtip32xx/mtip32xx.h
+++ drivers/block/mtip32xx/mtip32xx.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ata.h>
 #include <linux/interrupt.h>
 #include <linux/genhd.h>
-#include <linux/version.h>
 
 /* Offset of Subsystem Device ID in pci confoguration space */
 #define PCI_SUBSYSTEM_DEVICEID	0x2E
