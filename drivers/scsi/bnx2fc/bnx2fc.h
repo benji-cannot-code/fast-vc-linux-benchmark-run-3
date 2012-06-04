2ddf38bc6934c05bba5a9781798ef25de19e1717
Joe Perches <joe@perches.com>
2012-06-04 16:15:43 -0700

    [SCSI] bnx2fc: Reduce object size by consolidating formats
    Deduplication of formats and consolidating tests
    makes the object much smaller.
    
    Add bnx2fc_debug.c, add functions for a few logging
    functions (BNX2FC_IO_DBG, BNX2FC_TGT_DBG, BNX2FC_HBA_DBG).
    Use printf extension %pV.
    Add and use pr_fmt and pr_<level>.
    Move the debug #include below structure definitions.
    
    $ size drivers/scsi/bnx2fc/built-in.o*
       text	   data	    bss	    dec	    hex	filename
     101563	   1165	  24976	 127704	  1f2d8	drivers/scsi/bnx2fc/built-in.o.new
     138473	   1109	  33400	 172982	  2a3b6	drivers/scsi/bnx2fc/built-in.o.old
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Bhanu Prakash Gollapudi <bprakash@broadcom.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/bnx2fc/bnx2fc.h
+++ drivers/scsi/bnx2fc/bnx2fc.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Written by: Bhanu Prakash Gollapudi (bprakash@broadcom.com)
  */
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
@@ -58,7 +60,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/fc/fc_fcp.h>
 
 #include "57xx_hsi_bnx2fc.h"
-#include "bnx2fc_debug.h"
 #include "../../net/ethernet/broadcom/cnic_if.h"
 #include "bnx2fc_constants.h"
 
@@ -554,5 +555,8 @@ void bnx2fc_process_seq_cleanup_compl(struct bnx2fc_cmd *seq_clnup_req,
 				      u8 rx_state);
 int bnx2fc_initiate_seq_cleanup(struct bnx2fc_cmd *orig_io_req, u32 offset,
 				enum fc_rctl r_ctl);
+
+
+#include "bnx2fc_debug.h"
 
 #endif
