dfc2c6d57ec85d26a36b3d5cb96bfc927d08aeaf
Ben Widawsky <ben@bwidawsk.net>
2012-06-04 14:42:51 -0700

    drm/i915: use the default context
    With the code to do HW context switches in place have the driver load the
    default context for the render ring when the driver loads.
    
    The default context will be an ever present context that is available to
    switch to at any time for the given ring.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>

--- drivers/gpu/drm/i915/i915_gem_context.c
+++ drivers/gpu/drm/i915/i915_gem_context.c
@@ -98,6 +98,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct i915_hw_context *
 i915_gem_context_get(struct drm_i915_file_private *file_priv, u32 id);
+static int do_switch(struct drm_i915_gem_object *from_obj,
+		     struct i915_hw_context *to, u32 seqno);
 
 static int get_context_size(struct drm_device *dev)
 {
@@ -224,6 +226,14 @@ static int create_default_context(struct drm_i915_private *dev_priv)
 	if (ret) {
 		do_destroy(ctx);
 		return ret;
+	}
+
+	ret = do_switch(NULL, ctx, 0);
+	if (ret) {
+		i915_gem_object_unpin(ctx->obj);
+		do_destroy(ctx);
+	} else {
+		DRM_DEBUG_DRIVER("Default HW context loaded\n");
 	}
 
 	return ret;
