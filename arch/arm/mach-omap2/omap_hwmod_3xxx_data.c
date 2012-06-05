3e00586c239f7f7758f6b6bc1ac3409270d1c80d
Jon Hunter <jon-hunter@ti.com>
2012-06-05 12:34:54 -0500

    ARM: OMAP2+: Fix external clock support for dmtimers
    Currently, the dmtimer determines whether an timer can support an external
    clock source (sys_altclk) for driving the timer by the IP version. Only
    OMAP24xx devices can support an external clock source, but the IP version
    between OMAP24xx and OMAP3xxx is common and so this incorrectly indicates
    that OMAP3 devices can use an external clock source.
    
    Rather than use the IP version, just let the clock framework handle this.
    If the "alt_ck" does not exist for a timer then the clock framework will fail
    to find the clock and hence will return an error. By doing this we can eliminate
    the "timer_ip_version" variable passed as part of the platform data and simplify
    the code.
    
    We can also remove the timer IP version from the HWMOD data because the dmtimer
    driver uses the TIDR register to determine the IP version.
    
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
@@ -130,7 +130,6 @@ static struct omap_hwmod_class_sysconfig omap3xxx_timer_1ms_sysc = {
 static struct omap_hwmod_class omap3xxx_timer_1ms_hwmod_class = {
 	.name = "timer",
 	.sysc = &omap3xxx_timer_1ms_sysc,
-	.rev = OMAP_TIMER_IP_VERSION_1,
 };
 
 static struct omap_hwmod_class_sysconfig omap3xxx_timer_sysc = {
@@ -146,7 +145,6 @@ static struct omap_hwmod_class_sysconfig omap3xxx_timer_sysc = {
 static struct omap_hwmod_class omap3xxx_timer_hwmod_class = {
 	.name = "timer",
 	.sysc = &omap3xxx_timer_sysc,
-	.rev =  OMAP_TIMER_IP_VERSION_1,
 };
 
 /* secure timers dev attribute */
