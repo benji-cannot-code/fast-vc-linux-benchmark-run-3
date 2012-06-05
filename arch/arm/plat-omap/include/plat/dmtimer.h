c094927c6429094c1b9759ee5c0aa63f54031136
Jon Hunter <jon-hunter@ti.com>
2012-06-05 12:34:57 -0500

    ARM: OMAP: Add flag to indicate if a timer needs a manual reset
    For OMAP1 devices, it is necessary to perform a manual reset of the timer.
    Currently, this is indicating by setting the "needs_manual_reset" variable in
    the platform data. Instead of using an extra variable to indicate this add a new
    timer capabilities flag to indicate this and remove the "needs_manual_reset"
    member from the platform data.
    
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/dmtimer.h
+++ arch/arm/plat-omap/include/plat/dmtimer.h
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP_TIMER_SECURE				0x80000000
 #define OMAP_TIMER_ALWON				0x40000000
 #define OMAP_TIMER_HAS_PWM				0x20000000
+#define OMAP_TIMER_NEEDS_RESET				0x10000000
 
 struct omap_timer_capability_dev_attr {
 	u32 timer_capability;
@@ -91,7 +92,6 @@ struct timer_regs {
 
 struct dmtimer_platform_data {
 	int (*set_timer_src)(struct platform_device *pdev, int source);
-	u32 needs_manual_reset:1;
 	u32 timer_capability;
 };
 
