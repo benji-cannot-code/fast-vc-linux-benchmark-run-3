d662b8104a7f91502e3d47ae9c046b963cf9c791
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-06-05 10:07:08 +0200

    drm/i915: pnv has a backlight polarity control bit, too
    We already correctly ignore bit0 on gen < 4, now we also know why ;-)
    I've decided that losing that single bit of precision isn't worth the
    trouble to sprinkle IS_PINEVIEW checks all over the backlight control
    code - that code is way too fragile imo.
    
    Reviewed-by: Eugeni Dodonov <eugeni.dodonov@intel.com>
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -1829,6 +1829,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define   BACKLIGHT_DUTY_CYCLE_SHIFT		(0)
 #define   BACKLIGHT_DUTY_CYCLE_MASK		(0xffff)
+#define   BACKLIGHT_DUTY_CYCLE_MASK_PNV		(0xfffe)
+#define   BLM_POLARITY_PNV			(1 << 0) /* pnv only */
 
 #define BLC_HIST_CTL		0x61260
 
