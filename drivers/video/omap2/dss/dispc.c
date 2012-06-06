4e9a3b58fb09cd7fa1a47cf5ad0719ede3f35d9e
Archit Taneja <archit@ti.com>
2012-06-06 16:25:52 +0530

    OMAPDSS: DISPC/APPLY: Use interlace info in manager timings for dispc_ovl_setup()
    Currently the interlace parameter passed to dispc_ovl_setup() is configured by
    checking the display type, and set to true if the display type is VENC.
    
    This isn't correct as other panels can take interlaced content too. The
    omap_video_timings struct in manager's private data contains the info whether
    the panel is in interlaced mode or not.
    
    Signed-off-by: Archit Taneja <archit@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -2158,8 +2158,7 @@ static int dispc_ovl_calc_scaling(enum omap_plane plane,
 }
 
 int dispc_ovl_setup(enum omap_plane plane, struct omap_overlay_info *oi,
-		bool ilace, bool replication,
-		const struct omap_video_timings *mgr_timings)
+		bool replication, const struct omap_video_timings *mgr_timings)
 {
 	struct omap_overlay *ovl = omap_dss_get_overlay(plane);
 	bool five_taps = true;
@@ -2175,6 +2174,7 @@ int dispc_ovl_setup(enum omap_plane plane, struct omap_overlay_info *oi,
 	u16 out_width, out_height;
 	enum omap_channel channel;
 	int x_predecim = 1, y_predecim = 1;
+	bool ilace = mgr_timings->interlace;
 
 	channel = dispc_ovl_get_channel_out(plane);
 
