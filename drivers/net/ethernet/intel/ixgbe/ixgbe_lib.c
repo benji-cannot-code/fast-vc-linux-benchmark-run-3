079a6f701971ca0237e03e627cc856d21d710310
Alexander Duyck <alexander.h.duyck@intel.com>
2012-06-06 05:38:20 +0000

    ixgbe: Fix handling of FDIR_HASH flag
    This change makes it so that we can use the atr_sample_rate to determine if
    we are capable of supporting ATR. The advantage to this approach is that it
    allows us to now determine the setting of the IXGBE_FLAG_FDIR_HASH_CAPABLE
    based on the queueing scheme, instead of the queueing scheme being based on
    the flag.
    
    Using this approach there are essentially 5 conditions that must be checked
    prior to trying to enable ATR:
    1.  Is SR-IOV disabled?
    2.  Are the number of TCs <= 1?
    3.  Is RSS queueing limit greater than 1?
    4.  Is atr_sample_rate set?
    5.  Is Flow Director perfect filtering disabled?
    
    If any of these conditions are enabled they should disable ATR filtering.
    Note that in the case of conditions 1 through 4 being met we will set
    things up for ATR queueing, however if test 5 fails we will still leave the
    queues allocated for use by perfect filters.  The reason for this is to
    allow for us to switch back and forth between ntuple and ATR without
    needing to reallocate the descriptor rings.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Tested-by: Phil Schmitt <phillip.j.schmitt@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_lib.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_lib.c
@@ -371,6 +371,9 @@ static bool ixgbe_set_dcb_sriov_queues(struct ixgbe_adapter *adapter)
 	adapter->ring_feature[RING_F_RSS].indices = 1;
 	adapter->ring_feature[RING_F_RSS].mask = IXGBE_RSS_DISABLED_MASK;
 
+	/* disable ATR as it is not supported when VMDq is enabled */
+	adapter->flags &= ~IXGBE_FLAG_FDIR_HASH_CAPABLE;
+
 	adapter->num_rx_pools = vmdq_i;
 	adapter->num_rx_queues_per_pool = tcs;
 
@@ -450,6 +453,9 @@ static bool ixgbe_set_dcb_queues(struct ixgbe_adapter *adapter)
 	rss_i = min_t(int, rss_i, f->limit);
 	f->indices = rss_i;
 	f->mask = rss_m;
+
+	/* disable ATR as it is not supported when multiple TCs are enabled */
+	adapter->flags &= ~IXGBE_FLAG_FDIR_HASH_CAPABLE;
 
 #ifdef IXGBE_FCOE
 	/* FCoE enabled queues require special configuration indexed
@@ -607,16 +613,22 @@ static bool ixgbe_set_rss_queues(struct ixgbe_adapter *adapter)
 	f->indices = rss_i;
 	f->mask = IXGBE_RSS_16Q_MASK;
 
+	/* disable ATR by default, it will be configured below */
+	adapter->flags &= ~IXGBE_FLAG_FDIR_HASH_CAPABLE;
+
 	/*
 	 * Use Flow Director in addition to RSS to ensure the best
 	 * distribution of flows across cores, even when an FDIR flow
 	 * isn't matched.
 	 */
-	if (adapter->flags & IXGBE_FLAG_FDIR_HASH_CAPABLE) {
+	if (rss_i > 1 && adapter->atr_sample_rate) {
 		f = &adapter->ring_feature[RING_F_FDIR];
 
 		f->indices = min_t(u16, num_online_cpus(), f->limit);
 		rss_i = max_t(u16, rss_i, f->indices);
+
+		if (!(adapter->flags & IXGBE_FLAG_FDIR_PERFECT_CAPABLE))
+			adapter->flags |= IXGBE_FLAG_FDIR_HASH_CAPABLE;
 	}
 
 #ifdef IXGBE_FCOE
@@ -1055,13 +1067,7 @@ static void ixgbe_set_interrupt_capability(struct ixgbe_adapter *adapter)
 	}
 
 	adapter->flags &= ~IXGBE_FLAG_DCB_ENABLED;
-	if (adapter->flags & IXGBE_FLAG_FDIR_HASH_CAPABLE) {
-		e_err(probe,
-		      "ATR is not supported while multiple "
-		      "queues are disabled.  Disabling Flow Director\n");
-	}
-	adapter->flags &= ~IXGBE_FLAG_FDIR_HASH_CAPABLE;
-	adapter->atr_sample_rate = 0;
+
 	ixgbe_disable_sriov(adapter);
 
 	adapter->ring_feature[RING_F_RSS].limit = 1;
