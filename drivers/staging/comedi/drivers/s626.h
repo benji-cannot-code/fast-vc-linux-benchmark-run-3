82861453c792cac29b400da2356e02a0428072e1
Paul Bolle <pebolle@tiscali.nl>
2012-06-06 14:17:46 +0200

    treewide: Put a space between #include and FILE
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/staging/comedi/drivers/s626.h
+++ drivers/staging/comedi/drivers/s626.h
@@ -81,7 +81,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define INLINE static __inline
 #endif
 
-#include<linux/slab.h>
+#include <linux/slab.h>
 
 #define S626_SIZE 0x0200
 #define SIZEOF_ADDRESS_SPACE		0x0200
