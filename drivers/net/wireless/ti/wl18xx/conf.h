6c3c9daf73aa4829643b371836c57e1c5347cbcb
Luciano Coelho <coelho@ti.com>
2012-06-07 23:39:27 +0300

    wlcore/wl18xx: export conf struct in a debugfs file
    Add conf file header structure, magic and version values and export
    the entire conf struct in debugfs.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wl18xx/conf.h
+++ drivers/net/wireless/ti/wl18xx/conf.h
@@ -23,6 +23,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __WL18XX_CONF_H__
 #define __WL18XX_CONF_H__
 
+#define WL18XX_CONF_MAGIC	0x10e100ca
+#define WL18XX_CONF_VERSION	(WLCORE_CONF_VERSION | 0x0001)
+#define WL18XX_CONF_MASK	0x0000ffff
+#define WL18XX_CONF_SIZE	(WLCORE_CONF_SIZE + \
+				 sizeof(struct wl18xx_priv_conf))
+
 struct wl18xx_conf_phy {
 	u8 phy_standalone;
 	u8 rdl;
