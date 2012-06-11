4de072b2fbdfd4456d454a79a0af16740089c6d4
Eric Dumazet <edumazet@google.com>
2012-06-11 06:36:13 +0000

    net: keep name_hlist close to name
    __dev_get_by_name() is slow because pm_qos_req has been inserted between
    name[] and name_hlist, adding cache misses.
    
    pm_qos_req has nothing to do at the beginning of struct net_device
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1047,10 +1047,9 @@ struct net_device {
 	 */
 	char			name[IFNAMSIZ];
 
-	struct pm_qos_request	pm_qos_req;
-
-	/* device name hash chain */
+	/* device name hash chain, please keep it close to name[] */
 	struct hlist_node	name_hlist;
+
 	/* snmp alias */
 	char 			*ifalias;
 
@@ -1323,6 +1322,8 @@ struct net_device {
 
 	/* group the device belongs to */
 	int group;
+
+	struct pm_qos_request	pm_qos_req;
 };
 #define to_net_dev(d) container_of(d, struct net_device, dev)
 
