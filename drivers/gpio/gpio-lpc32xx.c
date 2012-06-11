7b2df59a93d24f2d33a32e3979404638ea0a5e21
Roland Stigge <stigge@antcom.de>
2012-06-11 10:12:40 +0200

    gpio: LPC32xx: Driver cleanup
    Since LPC32xx is now switched over to devicetree based GPIO, the unused
    lpc32xx_gpio_init() can be removed.
    
    Further, the driver title changed since it referred to the wrong file.
    
    Signed-off-by: Roland Stigge <stigge@antcom.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-lpc32xx.c
+++ drivers/gpio/gpio-lpc32xx.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/arm/mach-lpc32xx/gpiolib.c
+ * GPIO driver for LPC32xx SoC
  *
  * Author: Kevin Wells <kevin.wells@nxp.com>
  *
@@ -457,13 +457,6 @@ static struct lpc32xx_gpio_chip lpc32xx_gpiochip[] = {
 		.gpio_grp = &gpio_grp_regs_p3,
 	},
 };
-
-/* Empty now, can be removed later when mach-lpc32xx is finally switched over
- * to DT support
- */
-void __init lpc32xx_gpio_init(void)
-{
-}
 
 static int lpc32xx_of_xlate(struct gpio_chip *gc,
 			    const struct of_phandle_args *gpiospec, u32 *flags)
