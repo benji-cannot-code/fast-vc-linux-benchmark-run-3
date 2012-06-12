587568b90bd0d551210c0995d2b226b659d52326
Guenter Roeck <linux@roeck-us.net>
2012-06-12 08:27:49 -0700

    hwmon: (w83781d) Fix compile warning
    The following compile warning may be seen if the driver is compiled with
    -Wuninitialized:
    
    drivers/hwmon/w83781d.c: warning: 'sc_addr[1]' may be used uninitialized in this
    function [-Wuninitialized]
    
    While this is a false positive, it is annoying in nightly builds, and may help
    to conceal real problems. The current code is quite tricky, and and it is easy
    to rearrage the code to make the warning disappear. So fix it.
    
    Cc: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/w83781d.c
+++ drivers/hwmon/w83781d.c
@@ -868,6 +868,7 @@ w83781d_detect_subclients(struct i2c_client *new_client)
 	struct i2c_adapter *adapter = new_client->adapter;
 	struct w83781d_data *data = i2c_get_clientdata(new_client);
 	enum chips kind = data->type;
+	int num_sc = 1;
 
 	id = i2c_adapter_id(adapter);
 
@@ -892,6 +893,7 @@ w83781d_detect_subclients(struct i2c_client *new_client)
 	}
 
 	if (kind != w83783s) {
+		num_sc = 2;
 		if (force_subclients[0] == id &&
 		    force_subclients[1] == address) {
 			sc_addr[1] = force_subclients[3];
@@ -907,7 +909,7 @@ w83781d_detect_subclients(struct i2c_client *new_client)
 		}
 	}
 
-	for (i = 0; i <= 1; i++) {
+	for (i = 0; i < num_sc; i++) {
 		data->lm75[i] = i2c_new_dummy(adapter, sc_addr[i]);
 		if (!data->lm75[i]) {
 			dev_err(&new_client->dev, "Subclient %d "
@@ -918,8 +920,6 @@ w83781d_detect_subclients(struct i2c_client *new_client)
 				goto ERROR_SC_3;
 			goto ERROR_SC_2;
 		}
-		if (kind == w83783s)
-			break;
 	}
 
 	return 0;
