8923023e60710d34a48c43f5538788113f582163
Sonic Zhang <sonic.zhang@analog.com>
2012-06-13 16:22:45 +0800

    i2c:i2c-bfin-twi: include twi head file
    TWI bit mask macros are moved to twi head file.
    Depend on commit 61c16b5c7414b6d0511dc384e0ea994e250e6339
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>

--- drivers/i2c/busses/i2c-bfin-twi.c
+++ drivers/i2c/busses/i2c-bfin-twi.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/blackfin.h>
 #include <asm/portmux.h>
 #include <asm/irq.h>
+#include <asm/bfin_twi.h>
 
 /* SMBus mode*/
 #define TWI_I2C_MODE_STANDARD		1
