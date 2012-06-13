b826d3d5b12264825621724b2c9ec2af9bd20194
H Hartley Sweeten <hartleys@visionengravers.com>
2012-06-13 15:43:04 -0700

    staging: comedi: 8255: add namespace to 'do_config' function
    The name 'do_config' is pretty generic, and this function is the
    only one in this driver without namespace. Add namespace to it
    just to avoid any issues.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/8255.c
+++ drivers/staging/comedi/drivers/8255.c
@@ -170,7 +170,8 @@ static int subdev_8255_insn(struct comedi_device *dev,
 	return insn->n;
 }
 
-static void do_config(struct comedi_device *dev, struct comedi_subdevice *s)
+static void subdev_8255_do_config(struct comedi_device *dev,
+				  struct comedi_subdevice *s)
 {
 	struct subdev_8255_private *spriv = s->private;
 	unsigned long iobase = spriv->iobase;
@@ -222,7 +223,7 @@ static int subdev_8255_insn_config(struct comedi_device *dev,
 		return -EINVAL;
 	}
 
-	do_config(dev, s);
+	subdev_8255_do_config(dev, s);
 
 	return 1;
 }
@@ -345,7 +346,7 @@ int subdev_8255_init(struct comedi_device *dev, struct comedi_subdevice *s,
 	s->state	= 0;
 	s->io_bits	= 0;
 
-	do_config(dev, s);
+	subdev_8255_do_config(dev, s);
 
 	return 0;
 }
