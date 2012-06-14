4dcf2635bf62a33bd3092be79fc4960b65aed726
Steven Miao <realmz6@gmail.com>
2012-06-14 18:04:01 +0800

    bf60x: pm: add pint suspend and resume support
    save and restore pint sec CTL
    save and restore pint registers
    add pint suspend and resume when suspend to mem
    
    Signed-off-by: Steven Miao <realmz6@gmail.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- arch/blackfin/mach-common/pm.c
+++ arch/blackfin/mach-common/pm.c
@@ -173,6 +173,8 @@ int bfin_pm_suspend_mem_enter(void)
 
 	bfin_gpio_pm_hibernate_suspend();
 
+	bfin_pint_suspend();
+
 #if defined(CONFIG_BFIN_EXTMEM_WRITEBACK) || defined(CONFIG_BFIN_L2_WRITEBACK)
 	flushinv_all_dcache();
 #endif
@@ -190,6 +192,8 @@ int bfin_pm_suspend_mem_enter(void)
 
 	_enable_icplb();
 	_enable_dcplb();
+
+	bfin_pint_resume();
 
 	bfin_gpio_pm_hibernate_restore();
 	blackfin_dma_resume();
