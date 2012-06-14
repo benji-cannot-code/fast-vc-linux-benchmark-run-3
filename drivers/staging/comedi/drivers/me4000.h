ff0b9a8881873bd08cf8fdd062603a578f87360a
H Hartley Sweeten <hartleys@visionengravers.com>
2012-06-13 19:13:15 -0700

    staging: comedi: me4000: remove ISR_PDEBUG macro
    The ISR_PDEBUG macro is used to output trace messages in the isr
    routine. This shouldn't be in a mainline driver. Remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Frank Mori Hess <fmhess@users.sourceforge.net>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/me4000.h
+++ drivers/staging/comedi/drivers/me4000.h
@@ -29,15 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
   Debug section
   ===========================================================================*/
 
-#undef ME4000_ISR_DEBUG		/*  Debug the interrupt service routine */
 #undef ME4000_DEBUG		/*  General purpose debug masseges */
-
-#ifdef ME4000_ISR_DEBUG
-#undef ISR_PDEBUG
-#define ISR_PDEBUG(fmt, args...) printk(KERN_DEBUG"comedi%d: me4000: " fmt, dev->minor,  ##args)
-#else
-#define ISR_PDEBUG(fmt, args...)	/*  no debugging, do nothing */
-#endif
 
 #ifdef ME4000_DEBUG
 #undef PDEBUG
