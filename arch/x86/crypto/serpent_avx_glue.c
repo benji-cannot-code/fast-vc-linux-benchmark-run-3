bfeb0b66f2855ffabafecca9e839fffd755e1a49
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2012-06-14 10:09:03 +0800

    crypto: serpent-sse2/avx - allow both to be built into kernel
    Rename serpent-avx assembler functions so that they do not collide with
    serpent-sse2 assembler functions when linking both versions in to same
    kernel image.
    
    Reported-by: Randy Dunlap <rdunlap@xenotime.net>
    Cc: Johannes Goetzfried <Johannes.Goetzfried@informatik.stud.uni-erlangen.de>
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/x86/crypto/serpent_avx_glue.c
+++ arch/x86/crypto/serpent_avx_glue.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/i387.h>
 #include <asm/xcr.h>
 #include <asm/xsave.h>
-#include <asm/serpent.h>
+#include <asm/serpent-avx.h>
 #include <crypto/scatterwalk.h>
 #include <linux/workqueue.h>
 #include <linux/spinlock.h>
--- arch/x86/crypto/serpent_sse2_glue.c
+++ arch/x86/crypto/serpent_sse2_glue.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/lrw.h>
 #include <crypto/xts.h>
 #include <asm/i387.h>
-#include <asm/serpent.h>
+#include <asm/serpent-sse2.h>
 #include <crypto/scatterwalk.h>
 #include <linux/workqueue.h>
 #include <linux/spinlock.h>
