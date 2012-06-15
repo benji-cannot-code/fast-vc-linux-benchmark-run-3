5ad0f1d68308d487d0ce2a34ced6d91411b5c1b1
Jiri Olsa <jolsa@redhat.com>
2012-06-15 14:31:40 +0800

    perf/tool: Add support to reuse event grammar to parse out terms
    We want to reuse the event grammar for parsing aliased terms.
    The obvious reason is we dont need to add new code when there's
    already support for this in event grammar.
    
    Doing this by adding terms and event start entries into event
    parse grammar. The grammar forks on the begining based on the
    starting token, which is supplied via bison interface into the
    lexer.  The lexer then returns the starting token as the first
    token, thus making the grammar switch accordingly.
    
    Currently 2 starting tokens/grammars are supported:
    
    	PE_START_TERMS, PE_START_EVENTS
    
    The PE_START_TERMS related grammar uses 'event_config' part
    of the grammar for term parsing.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1339741902-8449-12-git-send-email-zheng.z.yan@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "header.h"
 #include "debugfs.h"
 #include "parse-events-bison.h"
+#define YY_EXTRA_TYPE int
 #include "parse-events-flex.h"
 #include "pmu.h"
 
@@ -789,13 +790,13 @@ int parse_events_modifier(struct list_head *list, char *str)
 	return 0;
 }
 
-static int parse_events__scanner(const char *str, void *data)
+static int parse_events__scanner(const char *str, void *data, int start_token)
 {
 	YY_BUFFER_STATE buffer;
 	void *scanner;
 	int ret;
 
-	ret = parse_events_lex_init(&scanner);
+	ret = parse_events_lex_init_extra(start_token, &scanner);
 	if (ret)
 		return ret;
 
@@ -812,6 +813,27 @@ static int parse_events__scanner(const char *str, void *data)
 	return ret;
 }
 
+/*
+ * parse event config string, return a list of event terms.
+ */
+int parse_events_terms(struct list_head *terms, const char *str)
+{
+	struct parse_events_data__terms data = {
+		.terms = NULL,
+	};
+	int ret;
+
+	ret = parse_events__scanner(str, &data, PE_START_TERMS);
+	if (!ret) {
+		list_splice(data.terms, terms);
+		free(data.terms);
+		return 0;
+	}
+
+	parse_events__free_terms(data.terms);
+	return ret;
+}
+
 int parse_events(struct perf_evlist *evlist, const char *str, int unset __used)
 {
 	struct parse_events_data__events data = {
@@ -820,7 +842,7 @@ int parse_events(struct perf_evlist *evlist, const char *str, int unset __used)
 	};
 	int ret;
 
-	ret = parse_events__scanner(str, &data);
+	ret = parse_events__scanner(str, &data, PE_START_EVENTS);
 	if (!ret) {
 		int entries = data.idx - evlist->nr_entries;
 		perf_evlist__splice_list_tail(evlist, &data.list, entries);
--- tools/perf/util/parse-events.h
+++ tools/perf/util/parse-events.h
@@ -34,6 +34,7 @@ extern int parse_events_option(const struct option *opt, const char *str,
 			       int unset);
 extern int parse_events(struct perf_evlist *evlist, const char *str,
 			int unset);
+extern int parse_events_terms(struct list_head *terms, const char *str);
 extern int parse_filter(const struct option *opt, const char *str, int unset);
 
 #define EVENTS_HELP_MAX (128*1024)
@@ -67,6 +68,10 @@ struct parse_events__term {
 struct parse_events_data__events {
 	struct list_head list;
 	int idx;
+};
+
+struct parse_events_data__terms {
+	struct list_head *terms;
 };
 
 int parse_events__is_hardcoded_term(struct parse_events__term *term);
