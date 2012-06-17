037d89d5295cfbe92996e1982b4721e14a6834c4
Steven King <sfking@fdwdc.com>
2012-06-17 00:34:00 -0700

    m68knommu: Add clk definitions for m520x.
    The 520x has individually controllable clocks for its peripherals.  Add clk
    definitions for these and add default initialization of either enabled or
    disabled for all of the clocks.
    
    Signed-off-by: Steven King <sfking@fdwdc.com>
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/m520xsim.h
+++ arch/m68k/include/asm/m520xsim.h
@@ -191,5 +191,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	MCF_RCR_SWRESET		0x80		/* Software reset bit */
 #define	MCF_RCR_FRCSTOUT	0x40		/* Force external reset */
 
+/*
+ *  Power Management.
+ */
+#define MCFPM_WCR		0xfc040013
+#define MCFPM_PPMSR0		0xfc04002c
+#define MCFPM_PPMCR0		0xfc04002d
+#define MCFPM_PPMHR0		0xfc040030
+#define MCFPM_PPMLR0		0xfc040034
+#define MCFPM_LPCR		0xfc0a0007
+
 /****************************************************************************/
 #endif  /* m520xsim_h */
