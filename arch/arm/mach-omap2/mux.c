72f71932153cfe25f38cf64db233dbd88900cce5
Paul Walmsley <paul@pwsan.com>
2012-06-17 12:00:58 -0600

    ARM: OMAP2+: mux: fix sparse warning
    Commit bbd707acee279a61177a604822db92e8164d00db ("ARM: omap2: use
    machine specific hook for late init") resulted in the addition of this
    sparse warning:
    
    arch/arm/mach-omap2/mux.c:791:12: warning: symbol 'omap_mux_late_init' was not declared. Should it be static?
    
    Fix by including the header file containing the prototype.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Cc: Shawn Guo <shawn.guo@linaro.org>
    Cc: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/mux.c
+++ arch/arm/mach-omap2/mux.c
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "control.h"
 #include "mux.h"
 #include "prm.h"
+#include "common.h"
 
 #define OMAP_MUX_BASE_OFFSET		0x30	/* Offset from CTRL_BASE */
 #define OMAP_MUX_BASE_SZ		0x5ca
