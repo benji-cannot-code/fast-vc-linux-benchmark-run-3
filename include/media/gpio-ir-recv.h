b8ff22b42c25df6d3607872445f1f9247dcd5f3a
Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
2012-06-18 15:02:44 -0300

    [media] media: gpio-ir-recv: add map name
    Make it possible for gpio-ir-recv users to choose a map name.
    
    Cc: Ravi Kumar V <kumarrav@codeaurora.org>
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/gpio-ir-recv.h
+++ include/media/gpio-ir-recv.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct gpio_ir_recv_platform_data {
 	int gpio_nr;
 	bool active_low;
+	const char *map_name;
 };
 
 #endif /* __GPIO_IR_RECV_H__ */
