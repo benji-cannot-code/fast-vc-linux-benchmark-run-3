3c47e31835e3e2b2acbd9fa3db1a6cebfe334af1
Miguel Gómez <magomez@igalia.com>
2012-06-18 13:12:03 +0200

    Staging: xgifb: Remove XGIFAIL() macro and its calls.
    XGIFAIL() prints a message and returns a value, but it's used only in one
    place. Better remove it and replace the call with the macro content.
    
    Signed-off-by: Miguel Gómez <magomez@igalia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/XGI_main.h
+++ drivers/staging/xgifb/XGI_main.h
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "XGIfb.h"
 #include "vb_def.h"
 
-#define XGIFAIL(x) do { printk(x "\n"); return -EINVAL; } while (0)
-
 #ifndef PCI_DEVICE_ID_XGI_42
 #define PCI_DEVICE_ID_XGI_42      0x042
 #endif
