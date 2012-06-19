f5b7b7a26e0375e9f6ffd8e50b6b8ed5b49d880c
Ian Abbott <abbotti@mev.co.uk>
2012-06-19 10:17:43 +0100

    staging: comedi: make class and file operations static
    A couple of global variables in "comedi_fops.c" are only referenced from
    that .c file and can be declared 'static'.  Also remove them from
    "comedi_fops.h" where they are declared 'extern'.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_fops.h
+++ drivers/staging/comedi/comedi_fops.h
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _COMEDI_FOPS_H
 #include <linux/types.h>
 
-extern struct class *comedi_class;
-extern const struct file_operations comedi_fops;
 extern bool comedi_autoconfig;
 extern struct comedi_driver *comedi_drivers;
 
