5cd99106a461f69c4a228ad38519068f9725a67e
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-06-19 19:31:48 +0100

    ALSA: Add missing include of pcm.h to pcm_params.h
    There's a dependency but no #include.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/pcm_params.h
+++ include/sound/pcm_params.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <sound/pcm.h>
+
 int snd_pcm_hw_param_first(struct snd_pcm_substream *pcm, 
 			   struct snd_pcm_hw_params *params,
 			   snd_pcm_hw_param_t var, int *dir);
