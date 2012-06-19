e3409aa4860ad1a156f5b5085b6fcb1ec2658b23
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-06-19 16:15:42 -0700

    Staging: add CSR wifi module
    This consists of two modules, the driver, and a "helper" module that is
    just a wrapper around common kernel functions.  The wrapper module will
    be removed soon, but for now it's needed.
    
    These files were based on the csr-linux-wifi-5.0.3-oss.tar.gz package
    provided by CSR and Blue Giga, and is covered under the license
    specified in the LICENSE.txt file (basically dual BSD and GPLv2).  The
    files were flattened out of the deep directory mess they were originally
    in, and a few EXPORT_SYMBOL_GPL() were added in order for everything to
    link properly with the helper module setup.
    
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Lauri Hintsala <Lauri.Hintsala@bluegiga.com>
    Cc: Riku Mettälä <riku.mettala@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

---
+++ drivers/staging/csr/csr_formatted_io.c
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*****************************************************************************
+
+            (c) Cambridge Silicon Radio Limited 2010
+            All rights reserved and confidential information of CSR
+
+            Refer to LICENSE.txt included with this source for details
+            on the license terms.
+
+*****************************************************************************/
+
+#include "csr_types.h"
+#include "csr_formatted_io.h"
+#include "csr_util.h"
+
+CsrInt32 CsrSnprintf(CsrCharString *dest, CsrSize n, const CsrCharString *fmt, ...)
+{
+    CsrInt32 r;
+    va_list args;
+    va_start(args, fmt);
+    r = CsrVsnprintf(dest, n, fmt, args);
+    va_end(args);
+
+    if (dest && (n > 0))
+    {
+        dest[n - 1] = '\0';
+    }
+
+    return r;
+}
