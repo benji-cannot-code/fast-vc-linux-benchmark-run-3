d12efa0de73400c80ee7f3dc5b9c4aa643cae602
Alexandre Pereira da Silva <aletes.xgr@gmail.com>
2012-06-20 09:37:57 -0300

    usb: gadget: lpc32xx_udc: fix build error with debugfs enabled
    If CONFIG_USB_GADGET_DEBUG_FILES is enabled, lpc32xx_udc breaks
    compilation because of a missing include file.
    
    Signed-off-by: Alexandre Pereira da Silva <aletes.xgr@gmail.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/lpc32xx_udc.c
+++ drivers/usb/gadget/lpc32xx_udc.c
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/irqs.h>
 #include <mach/board.h>
 #ifdef CONFIG_USB_GADGET_DEBUG_FILES
+#include <linux/debugfs.h>
 #include <linux/seq_file.h>
 #endif
 
