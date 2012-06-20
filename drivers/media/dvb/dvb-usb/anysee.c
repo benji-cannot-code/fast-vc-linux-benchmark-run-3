156e882c5634e0ac9fbb007fe6dc2c679d3d6ebb
Antti Palosaari <crope@iki.fi>
2012-06-20 00:32:53 -0300

    [media] dvb_usb_v2: multiple small tweaks around the code
    Naming, small code changes, debug writings, etc.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb/anysee.c
+++ drivers/media/dvb/dvb-usb/anysee.c
@@ -1265,7 +1265,7 @@ static int anysee_init(struct dvb_usb_device *d)
 	return 0;
 }
 
-static void anysee_disconnect(struct dvb_usb_device *d)
+static void anysee_exit(struct dvb_usb_device *d)
 {
 	return anysee_ci_release(d);
 }
@@ -1288,7 +1288,7 @@ static struct dvb_usb_device_properties anysee_props = {
 	.get_rc_config    = anysee_get_rc_config,
 	.frontend_ctrl    = anysee_frontend_ctrl,
 	.streaming_ctrl   = anysee_streaming_ctrl,
-	.disconnect       = anysee_disconnect,
+	.exit             = anysee_exit,
 
 	.num_adapters = 1,
 	.adapter = {
