ad0eb183f244547c06a36752e1df16190f75bb94
Lauri Hintsala <lauri.hintsala@bluegiga.com>
2012-06-21 13:29:12 +0300

    Staging: csr: add WEXT and AP mode support
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Lauri Hintsala <lauri.hintsala@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/sme_sys.c
+++ drivers/staging/csr/sme_sys.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "unifi_priv.h"
 #include "csr_wifi_hip_conversions.h"
 #ifdef CSR_SUPPORT_WEXT_AP
-#include "sme_csr/csr_wifi_sme_sef.h"
+#include "csr_wifi_sme_sef.h"
 #endif
 
 
