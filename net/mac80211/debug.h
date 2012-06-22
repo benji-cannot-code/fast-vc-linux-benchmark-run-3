7c6b81d86f9126efa0928c1990c62f3aef032da0
Johannes Berg <johannes.berg@intel.com>
2012-06-22 13:36:25 +0200

    mac80211: trace debug messages
    It can be very useful to have all debug messages
    available when debugging, but hard to correlate
    between different sources, so add a trace event
    for all mac80211 debug messages.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/debug.h
+++ net/mac80211/debug.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MAC80211_DEBUG_H
 #define __MAC80211_DEBUG_H
+#include <net/cfg80211.h>
 
 #ifdef CONFIG_MAC80211_IBSS_DEBUG
 #define MAC80211_IBSS_DEBUG 1
@@ -62,6 +63,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAC80211_MLME_DEBUG 0
 #endif
 
+#ifdef CONFIG_MAC80211_MESSAGE_TRACING
+void __sdata_info(const char *fmt, ...) __printf(1, 2);
+void __sdata_dbg(bool print, const char *fmt, ...) __printf(2, 3);
+void __sdata_err(const char *fmt, ...) __printf(1, 2);
+void __wiphy_dbg(struct wiphy *wiphy, bool print, const char *fmt, ...)
+	__printf(3, 4);
+
+#define _sdata_info(sdata, fmt, ...)					\
+	__sdata_info("%s: " fmt, (sdata)->name, ##__VA_ARGS__)
+#define _sdata_dbg(print, sdata, fmt, ...)				\
+	__sdata_dbg(print, "%s: " fmt, (sdata)->name, ##__VA_ARGS__)
+#define _sdata_err(sdata, fmt, ...)					\
+	__sdata_err("%s: " fmt, (sdata)->name, ##__VA_ARGS__)
+#define _wiphy_dbg(print, wiphy, fmt, ...)				\
+	__wiphy_dbg(wiphy, print, fmt, ##__VA_ARGS__)
+#else
 #define _sdata_info(sdata, fmt, ...)					\
 do {									\
 	pr_info("%s: " fmt,						\
@@ -86,6 +103,7 @@ do {									\
 	if (print)							\
 		wiphy_dbg((wiphy), fmt, ##__VA_ARGS__);			\
 } while (0)
+#endif
 
 #define sdata_info(sdata, fmt, ...)					\
 	_sdata_info(sdata, fmt, ##__VA_ARGS__)
