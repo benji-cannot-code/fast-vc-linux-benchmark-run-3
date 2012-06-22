1cfc1a8ab0278bbe10d5a144ba9fb59c33a1599c
Pablo Neira Ayuso <pablo@netfilter.org>
2012-06-22 11:25:33 +0200

    netfilter: nfnetlink_queue: fix sparse warning due to missing include
    This patch fixes a sparse warning due to missing include header file.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nfnetlink_queue_ct.c
+++ net/netfilter/nfnetlink_queue_ct.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netfilter/nfnetlink.h>
 #include <linux/netfilter/nfnetlink_queue.h>
 #include <net/netfilter/nf_conntrack.h>
+#include <net/netfilter/nfnetlink_queue.h>
 
 struct nf_conn *nfqnl_ct_get(struct sk_buff *entskb, size_t *size,
 			     enum ip_conntrack_info *ctinfo)
