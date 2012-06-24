f1c03d95d6f8ac0b946381d4b0ed2feb689f0f0f
Jesper Juhl <jj@chaosbits.net>
2012-06-25 01:56:35 +0200

    staging: vt6656: iwctl: remove redundant ';'
    else clauses don't need to be terminated with ';'.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/iwctl.c
+++ drivers/staging/vt6656/iwctl.c
@@ -1322,7 +1322,7 @@ int iwctl_giwsens(struct net_device *dev, struct iw_request_info *info,
 	}
 	else {
 		wrq->value = 0;
-	};
+	}
 	wrq->disabled = (wrq->value == 0);
 	wrq->fixed = 1;
 	return 0;
