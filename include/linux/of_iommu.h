dccbb294ceeff8d1bfd8b43c1b22ae7288b5c82d
Hiroshi Doyu <hdoyu@nvidia.com>
2012-06-25 14:23:54 +0300

    iommu: Add DMA window parser, of_get_dma_window()
    This code was based on:
        "arch/microblaze/kernel/prom_parse.c"
        "arch/powerpc/kernel/prom_parse.c"
    
    Can replace "of_parse_dma_window()" in the above. This supports
    different formats flexibly. "prefix" can be configured if any. "busno"
    and "index" are optionally specified. Set NULL and 0 if not used.
    
    Signed-off-by: Hiroshi DOYU <hdoyu@nvidia.com>
    Acked-by: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

---
+++ include/linux/of_iommu.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __OF_IOMMU_H
+#define __OF_IOMMU_H
+
+#ifdef CONFIG_OF_IOMMU
+
+extern int of_get_dma_window(struct device_node *dn, const char *prefix,
+			     int index, unsigned long *busno, dma_addr_t *addr,
+			     size_t *size);
+
+#else
+
+static inline int of_get_dma_window(struct device_node *dn, const char *prefix,
+			    int index, unsigned long *busno, dma_addr_t *addr,
+			    size_t *size)
+{
+	return -EINVAL;
+}
+
+#endif	/* CONFIG_OF_IOMMU */
+
+#endif /* __OF_IOMMU_H */
