c95f67342c151a32e6991183a0deab4d8eb1ec10
H. Peter Anvin <hpa@zytor.com>
2012-06-26 07:58:23 -0700

    x86, cpufeature: Remove stray %s, add -w to mkcapflags.pl
    There was a stray %s left from testing, remove it.
    
    Add -w to the #! line (which is parsed by Perl even if the Perl
    interpreter is invoked explicitly on the command line) to catch these
    kinds of errors in the future.
    
    Reported-by: Jean Delvare <khali@linux-fr.org>
    Link: http://lkml.kernel.org/r/20120626143246.0c9bf301@endymion.delvare
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/cpu/mkcapflags.pl
+++ arch/x86/kernel/cpu/mkcapflags.pl
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 #
 # Generate the x86_cap_flags[] array from include/asm-x86/cpufeature.h
 #
@@ -30,7 +30,7 @@ while (defined($line = <IN>)) {
 			print STDERR "$in: duplicate feature name: $feature\n";
 			$err++;
 		}
-		printf OUT "\t%-32s = \"%s\",%s\n", "[$macro]", $feature;
+		printf OUT "\t%-32s = \"%s\",\n", "[$macro]", $feature;
 	}
 }
 print OUT "};\n";
