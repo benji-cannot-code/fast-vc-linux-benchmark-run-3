f423ec4fa20974cc68a370001633f79b011f9096
Greg Ungerer <gerg@uclinux.org>
2012-06-26 21:02:54 +1000

    m68k: use simpler dma_alloc_coherent() for ColdFire CPUs
    On all ColdFire platforms (whether MMU enabled or not) we want to use the
    simple page based dma_alloc_coherent. We don't want the virtual mapping
    version that is used on classic m68k setups. So modify the conditionals
    to use the existing simpler dma_alloc_coherent on all ColdFire and non-MMU
    builds.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/kernel/dma.c
+++ arch/m68k/kernel/dma.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/pgalloc.h>
 
-#ifdef CONFIG_MMU
+#if defined(CONFIG_MMU) && !defined(CONFIG_COLDFIRE)
 
 void *dma_alloc_coherent(struct device *dev, size_t size,
 			 dma_addr_t *handle, gfp_t flag)
@@ -97,7 +97,7 @@ void dma_free_coherent(struct device *dev, size_t size,
 	free_pages((unsigned long)vaddr, get_order(size));
 }
 
-#endif /* CONFIG_MMU */
+#endif /* CONFIG_MMU && !CONFIG_COLDFIRE */
 
 EXPORT_SYMBOL(dma_alloc_coherent);
 EXPORT_SYMBOL(dma_free_coherent);
