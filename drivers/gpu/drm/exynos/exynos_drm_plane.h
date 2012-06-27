f4353ea9bcb66c8a99e39d4f42642d3a859a76ea
Joonyoung Shim <jy0922.shim@samsung.com>
2012-06-27 14:27:05 +0900

    drm/exynos: update overlay via plane from crtc
    There is no any reason to update overlay at crtc directly because the
    crtc uses plane. Move its code to plane and call proper functions of
    plane from crtc.
    
    Signed-off-by: Joonyoung Shim <jy0922.shim@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_plane.h
+++ drivers/gpu/drm/exynos/exynos_drm_plane.h
@@ -10,8 +10,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+int exynos_plane_mode_set(struct drm_plane *plane, struct drm_crtc *crtc,
+			  struct drm_framebuffer *fb, int crtc_x, int crtc_y,
+			  unsigned int crtc_w, unsigned int crtc_h,
+			  uint32_t src_x, uint32_t src_y,
+			  uint32_t src_w, uint32_t src_h);
+void exynos_plane_commit(struct drm_plane *plane);
 struct drm_plane *exynos_plane_init(struct drm_device *dev,
 				    unsigned int possible_crtcs, bool priv);
 int exynos_plane_set_zpos_ioctl(struct drm_device *dev, void *data,
 				struct drm_file *file_priv);
-struct exynos_drm_overlay *get_exynos_drm_overlay(struct drm_plane *plane);
