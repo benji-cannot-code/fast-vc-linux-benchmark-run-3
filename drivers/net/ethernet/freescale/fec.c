798fa5c20d6ec13bd35975c7301136705374aa49
Shawn Guo <shawn.guo@linaro.org>
2012-06-27 03:45:20 +0000

    net: fec: reset phy after pinctrl setup
    In case that bootloader or platform initialization does not set up
    fec pins, the fec_reset_phy will not be able to succeed, because
    fec_reset_phy is currently called before devm_pinctrl_get_select_default.
    Move fec_reset_phy call to the place between devm_pinctrl_get_select_default
    and fec_enet_init to have above case be taken care.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fec.c
+++ drivers/net/ethernet/freescale/fec.c
@@ -1594,8 +1594,6 @@ fec_probe(struct platform_device *pdev)
 		fep->phy_interface = ret;
 	}
 
-	fec_reset_phy(pdev);
-
 	for (i = 0; i < FEC_IRQ_NUM; i++) {
 		irq = platform_get_irq(pdev, i);
 		if (irq < 0) {
@@ -1634,6 +1632,8 @@ fec_probe(struct platform_device *pdev)
 
 	clk_prepare_enable(fep->clk_ahb);
 	clk_prepare_enable(fep->clk_ipg);
+
+	fec_reset_phy(pdev);
 
 	ret = fec_enet_init(ndev);
 	if (ret)
