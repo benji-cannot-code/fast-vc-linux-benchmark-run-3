18ddfb13d844acb265d074685ecca6a4676d9f7b
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-06-27 14:59:20 +0100

    ASoC: wm5102: Remove unused platform data header
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm5102.c
+++ sound/soc/codecs/wm5102.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/jack.h>
 #include <sound/initval.h>
 #include <sound/tlv.h>
-#include <sound/wm5102.h>
 
 #include <linux/mfd/arizona/core.h>
 #include <linux/mfd/arizona/registers.h>
