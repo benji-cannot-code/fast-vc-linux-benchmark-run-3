8a3e3117e4464f6d2215e3a225bb3c17a4538410
Jesper Juhl <jj@chaosbits.net>
2012-06-27 22:01:18 +0200

    staging: vt6656: int.c: Put comment about DEBUG print define on same line as code
    A small comment at the end of the line, mentioning the debug level, is
    enough - no need to repeat the entire line of code just for that.
    
    Signed-off-by: Jesper Juhl <jj@chaosbits.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/int.c
+++ drivers/staging/vt6656/int.c
@@ -42,8 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "usbpipe.h"
 
 /*---------------------  Static Definitions -------------------------*/
-/* static int msglevel = MSG_LEVEL_DEBUG; */
-static int msglevel = MSG_LEVEL_INFO;
+static int msglevel = MSG_LEVEL_INFO; /* MSG_LEVEL_DEBUG */
 
 /*---------------------  Static Classes  ----------------------------*/
 
