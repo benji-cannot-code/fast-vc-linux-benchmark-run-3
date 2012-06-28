de038b974805381fbb66641a251540dd78c0d3da
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-06-28 14:08:26 +0100

    i2c-s3c2410: Use plain pm_runtime_put()
    There's no point in using _sync() as we don't really care if the suspend
    has completed immediately.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Reviewed-by: Shubhrajyoti D <shubhrajyoti@ti.com>
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>

--- drivers/i2c/busses/i2c-s3c2410.c
+++ drivers/i2c/busses/i2c-s3c2410.c
@@ -610,7 +610,7 @@ static int s3c24xx_i2c_xfer(struct i2c_adapter *adap,
 
 		if (ret != -EAGAIN) {
 			clk_disable(i2c->clk);
-			pm_runtime_put_sync(&adap->dev);
+			pm_runtime_put(&adap->dev);
 			return ret;
 		}
 
@@ -620,7 +620,7 @@ static int s3c24xx_i2c_xfer(struct i2c_adapter *adap,
 	}
 
 	clk_disable(i2c->clk);
-	pm_runtime_put_sync(&adap->dev);
+	pm_runtime_put(&adap->dev);
 	return -EREMOTEIO;
 }
 
