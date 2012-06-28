6c037b604b65f79405e65d34d9b004bc6c6101cf
Felix Fietkau <nbd@openwrt.org>
2012-06-28 02:33:41 +0200

    ath9k: de-duplicate initvals
    The initvals tool from https://github.com/mcgrof/qca-swiss-army-knife has
    been modified to detect identical initval tables and replace them with
    macros. This patch contains the generated changes.
    
    On MIPS this reduces the binary size by 24 KB with no runtime changes.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/ar9462_2p0_initvals.h
+++ drivers/net/wireless/ath/ath9k/ar9462_2p0_initvals.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (c) 2010 Atheros Communications Inc.
+ * Copyright (c) 2010-2011 Atheros Communications Inc.
+ * Copyright (c) 2011-2012 Qualcomm Atheros Inc.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -1008,7 +1009,7 @@ static const u32 ar9462_2p0_radio_core[][2] = {
 
 static const u32 ar9462_2p0_soc_preamble[][2] = {
 	/* Addr      allmodes  */
-	{0x000040a4 ,0x00a0c1c9},
+	{0x000040a4, 0x00a0c1c9},
 	{0x00007020, 0x00000000},
 	{0x00007034, 0x00000002},
 	{0x00007038, 0x000004c2},
