fdcbbb4059de533559d0345c00ae4dafffee4fa0
Stephen Warren <swarren@nvidia.com>
2012-06-28 16:29:19 -0600

    ARM: tegra: use of_default_bus_match_table
    of_default_bus_match_table is a table of default bus types supported by
    of_platform_populate(). Since Tegra has no need to support any specific
    custom list of bus types, modify the DT board files to use this default
    list.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/board-dt-tegra30.c
+++ arch/arm/mach-tegra/board-dt-tegra30.c
@@ -37,11 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board.h"
 #include "clock.h"
 
-static struct of_device_id tegra_dt_match_table[] __initdata = {
-	{ .compatible = "simple-bus", },
-	{}
-};
-
 struct of_dev_auxdata tegra30_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("nvidia,tegra20-sdhci", 0x78000000, "sdhci-tegra.0", NULL),
 	OF_DEV_AUXDATA("nvidia,tegra20-sdhci", 0x78000200, "sdhci-tegra.1", NULL),
@@ -76,7 +71,7 @@ static void __init tegra30_dt_init(void)
 {
 	tegra_clk_init_from_table(tegra_dt_clk_init_table);
 
-	of_platform_populate(NULL, tegra_dt_match_table,
+	of_platform_populate(NULL, of_default_bus_match_table,
 				tegra30_auxdata_lookup, NULL);
 }
 
