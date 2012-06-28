69644cf6faa2408fde68a9393e64bb4a756921b4
Archit Taneja <archit@ti.com>
2012-06-28 11:15:51 +0530

    OMAPDSS: Add interlace parameter to omap_video_timings
    Add a parameter called interlace which tells whether the timings are in
    interlaced or progressive mode. This aligns the omap_video_timings struct with
    the Xorg modeline configuration.
    
    It also removes the hack needed to write to divide the manager height by 2 if
    the connected interface is VENC.
    
    Signed-off-by: Archit Taneja <archit@ti.com>

--- drivers/video/omap2/dss/venc.c
+++ drivers/video/omap2/dss/venc.c
@@ -273,6 +273,8 @@ const struct omap_video_timings omap_dss_pal_timings = {
 	.vsw		= 5,
 	.vfp		= 5,
 	.vbp		= 41,
+
+	.interlace	= true,
 };
 EXPORT_SYMBOL(omap_dss_pal_timings);
 
@@ -286,6 +288,8 @@ const struct omap_video_timings omap_dss_ntsc_timings = {
 	.vsw		= 6,
 	.vfp		= 6,
 	.vbp		= 31,
+
+	.interlace	= true,
 };
 EXPORT_SYMBOL(omap_dss_ntsc_timings);
 
