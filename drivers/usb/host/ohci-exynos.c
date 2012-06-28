23a7159a6b1aa3402de1dd45001894b8fc646c9b
Jingoo Han <jg1.han@samsung.com>
2012-06-28 16:49:42 +0900

    USB: ohci-exynos: add clock gating to suspend/resume
    This patch adds clock gating to suspend and resume functions.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ohci-exynos.c
+++ drivers/usb/host/ohci-exynos.c
@@ -226,6 +226,9 @@ static int exynos_ohci_suspend(struct device *dev)
 
 	if (pdata && pdata->phy_exit)
 		pdata->phy_exit(pdev, S5P_USB_PHY_HOST);
+
+	clk_disable(exynos_ohci->clk);
+
 fail:
 	spin_unlock_irqrestore(&ohci->lock, flags);
 
@@ -238,6 +241,8 @@ static int exynos_ohci_resume(struct device *dev)
 	struct usb_hcd *hcd = exynos_ohci->hcd;
 	struct platform_device *pdev = to_platform_device(dev);
 	struct exynos4_ohci_platdata *pdata = pdev->dev.platform_data;
+
+	clk_enable(exynos_ohci->clk);
 
 	if (pdata && pdata->phy_init)
 		pdata->phy_init(pdev, S5P_USB_PHY_HOST);
