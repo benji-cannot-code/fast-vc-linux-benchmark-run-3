d1c19efef2f94a49867cd10e4d2f1abbb2bc767f
Axel Lin <axel.lin@gmail.com>
2012-06-29 09:45:16 +0800

    regulator: tps65217: Convert LDO1 to use regulator_list_voltage_table
    Convert tps65217_pmic_ldo1_ops to use regulator_list_voltage_table.
    
    We have n_voltages and volt_table settings in regulator_desc,
    so we don't need the table and table_len fields in struct tps_info.
    Thus remove them from struct tps_info.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/mfd/tps65217.h
+++ include/linux/mfd/tps65217.h
@@ -228,8 +228,6 @@ struct tps65217_board {
  * @max_uV:		minimum micro volts
  * @vsel_to_uv:		Function pointer to get voltage from selector
  * @uv_to_vsel:		Function pointer to get selector from voltage
- * @table:		Table for non-uniform voltage step-size
- * @table_len:		Length of the voltage table
  *
  * This data is used to check the regualtor voltage limits while setting.
  */
@@ -239,8 +237,6 @@ struct tps_info {
 	int max_uV;
 	int (*vsel_to_uv)(unsigned int vsel);
 	int (*uv_to_vsel)(int uV, unsigned int *vsel);
-	const int *table;
-	unsigned int table_len;
 };
 
 /**
