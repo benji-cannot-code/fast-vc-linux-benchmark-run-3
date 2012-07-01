44110dd6f2cba8544397fdac9e99c01eb74152c0
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-07-01 23:24:36 +0200

    drm/i915: rip out encoder->disable/enable checks
    All encoders are now converted so there's no need for these checks any
    more.
    
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3268,10 +3268,8 @@ static void ironlake_crtc_enable(struct drm_crtc *crtc)
 	intel_crtc_update_cursor(crtc, true);
 
 encoders:
-	for_each_encoder_on_crtc(dev, crtc, encoder) {
-		if (encoder->enable)
-			encoder->enable(encoder);
-	}
+	for_each_encoder_on_crtc(dev, crtc, encoder)
+		encoder->enable(encoder);
 }
 
 static void ironlake_crtc_disable(struct drm_crtc *crtc)
@@ -3286,10 +3284,8 @@ static void ironlake_crtc_disable(struct drm_crtc *crtc)
 
 	/* XXX: For compatability with the crtc helper code, call the encoder's
 	 * disable function unconditionally for now. */
-	for_each_encoder_on_crtc(dev, crtc, encoder) {
-		if (encoder->disable)
-			encoder->disable(encoder);
-	}
+	for_each_encoder_on_crtc(dev, crtc, encoder)
+		encoder->disable(encoder);
 
 	if (!intel_crtc->active)
 		return;
@@ -3413,10 +3409,8 @@ static void i9xx_crtc_enable(struct drm_crtc *crtc)
 	intel_crtc_update_cursor(crtc, true);
 
 encoders:
-	for_each_encoder_on_crtc(dev, crtc, encoder) {
-		if (encoder->enable)
-			encoder->enable(encoder);
-	}
+	for_each_encoder_on_crtc(dev, crtc, encoder)
+		encoder->enable(encoder);
 }
 
 static void i9xx_crtc_disable(struct drm_crtc *crtc)
@@ -3430,10 +3424,8 @@ static void i9xx_crtc_disable(struct drm_crtc *crtc)
 
 	/* XXX: For compatability with the crtc helper code, call the encoder's
 	 * disable function unconditionally for now. */
-	for_each_encoder_on_crtc(dev, crtc, encoder) {
-		if (encoder->disable)
-			encoder->disable(encoder);
-	}
+	for_each_encoder_on_crtc(dev, crtc, encoder)
+		encoder->disable(encoder);
 
 	if (!intel_crtc->active)
 		return;
