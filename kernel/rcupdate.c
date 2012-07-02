cb37952262bcb6b7493903286f0a6e6d4622ce46
Paul E. McKenney <paul.mckenney@linaro.org>
2012-07-02 14:42:01 -0700

    rcu: Add PROVE_RCU_DELAY to provoke difficult races
    There have been some recent bugs that were triggered only when
    preemptible RCU's __rcu_read_unlock() was preempted just after setting
    ->rcu_read_lock_nesting to INT_MIN, which is a low-probability event.
    Therefore, reproducing those bugs (to say nothing of gaining confidence
    in alleged fixes) was quite difficult.  This commit therefore creates
    a new debug-only RCU kernel config option that forces a short delay
    in __rcu_read_unlock() to increase the probability of those sorts of
    bugs occurring.
    
    Signed-off-by: Paul E. McKenney <paul.mckenney@linaro.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- kernel/rcupdate.c
+++ kernel/rcupdate.c
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/export.h>
 #include <linux/hardirq.h>
+#include <linux/delay.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/rcu.h>
@@ -82,6 +83,9 @@ void __rcu_read_unlock(void)
 	} else {
 		barrier();  /* critical section before exit code. */
 		t->rcu_read_lock_nesting = INT_MIN;
+#ifdef CONFIG_PROVE_RCU_DELAY
+		udelay(10); /* Make preemption more probable. */
+#endif /* #ifdef CONFIG_PROVE_RCU_DELAY */
 		barrier();  /* assign before ->rcu_read_unlock_special load */
 		if (unlikely(ACCESS_ONCE(t->rcu_read_unlock_special)))
 			rcu_read_unlock_special(t);
