b6e450125d7c7dc1645ce923f32494590fb5738f
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-07-02 11:43:47 +0200

    drm/i915: WARN when trying to enabled an unused crtc
    This is the first tiny step towards cross-checking the entire modeset
    state machine with WARNs. A crtc can only be enabled when it's
    actually in use, i.e. crtc->active imlies crtc->enabled.
    
    Unfortunately we can't (yet) check this when disabling the crtc,
    because the crtc helpers are a bit slopy with updating state and
    unconditionally update crtc->enabled before changing the hw state.
    
    Fixing that requires quite some more work.
    
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3216,6 +3216,8 @@ static void ironlake_crtc_enable(struct drm_crtc *crtc)
 	u32 temp;
 	bool is_pch_port;
 
+	WARN_ON(!crtc->enabled);
+
 	/* XXX: For compatability with the crtc helper code, call the encoder's
 	 * enable function unconditionally for now. */
 	if (intel_crtc->active)
@@ -3391,6 +3393,8 @@ static void i9xx_crtc_enable(struct drm_crtc *crtc)
 	struct intel_encoder *encoder;
 	int pipe = intel_crtc->pipe;
 	int plane = intel_crtc->plane;
+
+	WARN_ON(!crtc->enabled);
 
 	/* XXX: For compatability with the crtc helper code, call the encoder's
 	 * enable function unconditionally for now. */
