8a387f8c5ba536c1723496e5b1eb631e037eb6f3
Mike Snitzer <snitzer@redhat.com>
2012-07-03 12:55:33 +0100

    dm persistent data: fix shadow_info_leak on dm_tm_destroy
    Cleanup the shadow table before destroying the transaction manager.
    
    Reference: leak was identified with kmemleak when running
    test_discard_random_sectors in the thinp-test-suite.
    
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/persistent-data/dm-transaction-manager.c
+++ drivers/md/persistent-data/dm-transaction-manager.c
@@ -139,6 +139,9 @@ EXPORT_SYMBOL_GPL(dm_tm_create_non_blocking_clone);
 
 void dm_tm_destroy(struct dm_transaction_manager *tm)
 {
+	if (!tm->is_clone)
+		wipe_shadow_table(tm);
+
 	kfree(tm);
 }
 EXPORT_SYMBOL_GPL(dm_tm_destroy);
