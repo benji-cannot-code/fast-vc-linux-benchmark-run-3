2fe9e69b33ac8771f440d7671e7d728bf1f9d21f
majianpeng <majianpeng@gmail.com>
2012-07-03 12:12:26 +1000

    md:Add blk_plug in sync_thread.
    Add blk_plug in sync_thread will increase the performance of sync.
    Because sync_thread did not blk_plug,so when raid sync, the bio merge
    not well.
    
    Testing environment:
    SATA controller: Intel Corporation 82801JI (ICH10 Family) SATA AHCI
    Controller.
    OS:Linux xxx 3.5.0-rc2+ #340 SMP Tue Jun 12 09:00:25 CST 2012
    x86_64 x86_64 x86_64 GNU/Linux.
    RAID5: four ST31000524NS disk.
    
    Without blk_plug:recovery speed about 63M/Sec;
    Add blk_plug:recovery speed about 120M/Sec.
    
    Using blktrace:
    blktrace -d /dev/sdb -w 60  -o -|blkparse -i -
    
    without blk_plug:
    Total (8,16):
     Reads Queued:      309811,     1239MiB	 Writes Queued:           0,        0KiB
     Read Dispatches:   283583,     1189MiB	 Write Dispatches:        0,        0KiB
     Reads Requeued:         0		 Writes Requeued:         0
     Reads Completed:   273351,     1149MiB	 Writes Completed:        0,        0KiB
     Read Merges:        23533,    94132KiB	 Write Merges:            0,        0KiB
     IO unplugs:             0        	 Timer unplugs:           0
    
    add blk_plug:
    Total (8,16):
     Reads Queued:      428697,     1714MiB	 Writes Queued:           0,        0KiB
     Read Dispatches:     3954,     1714MiB	 Write Dispatches:        0,        0KiB
     Reads Requeued:         0		 Writes Requeued:         0
     Reads Completed:     3956,     1715MiB	 Writes Completed:        0,        0KiB
     Read Merges:       424743,     1698MiB	 Write Merges:            0,        0KiB
     IO unplugs:             0        	 Timer unplugs:        3384
    
    The ratio of merge will be markedly increased.
    
    Signed-off-by: majianpeng <majianpeng@gmail.com>
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -7299,6 +7299,7 @@ void md_do_sync(struct mddev *mddev)
 	int skipped = 0;
 	struct md_rdev *rdev;
 	char *desc;
+	struct blk_plug plug;
 
 	/* just incase thread restarts... */
 	if (test_bit(MD_RECOVERY_DONE, &mddev->recovery))
@@ -7448,6 +7449,7 @@ void md_do_sync(struct mddev *mddev)
 	}
 	mddev->curr_resync_completed = j;
 
+	blk_start_plug(&plug);
 	while (j < max_sectors) {
 		sector_t sectors;
 
@@ -7553,6 +7555,7 @@ void md_do_sync(struct mddev *mddev)
 	 * this also signals 'finished resyncing' to md_stop
 	 */
  out:
+	blk_finish_plug(&plug);
 	wait_event(mddev->recovery_wait, !atomic_read(&mddev->recovery_active));
 
 	/* tell personality that we are finished */
