f2d5452940702a3678d1f01bad0ee8589d6ed3a4
Greg Ungerer <gerg@uclinux.org>
2012-07-04 13:49:59 +0000

    m68knommu: move the badly named mcfne.h to a better mcf8390.h
    The mcfne.h include contains definitions to support NS8390 eth based hardware
    on ColdFire based CPU boards. So change its name to reflect that better.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/m68k/include/asm/mcf8390.h
+++ arch/m68k/include/asm/mcf8390.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /****************************************************************************/
 
 /*
- *	mcfne.h -- NE2000 in ColdFire eval boards.
+ *	mcf8390.h -- NS8390 support for ColdFire eval boards.
  *
  *	(C) Copyright 1999-2000, Greg Ungerer (gerg@snapgear.com)
  *	(C) Copyright 2000,      Lineo (www.lineo.com)
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 /****************************************************************************/
-#ifndef	mcfne_h
-#define	mcfne_h
+#ifndef	mcf8390_h
+#define	mcf8390_h
 /****************************************************************************/
 
 
@@ -240,4 +240,4 @@ void ne2000_outsw(unsigned int addr, const void *vbuf, unsigned long len)
 #endif /* NE2000_OFFOFFSET */
 
 /****************************************************************************/
-#endif	/* mcfne_h */
+#endif	/* mcf8390_h */
