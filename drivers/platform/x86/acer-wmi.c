72ce82169427975d270d6e22078e18d471365edc
Marek Vasut <marex@denx.de>
2012-07-05 01:30:10 +0200

    ACER: Fix Smatch double-free issue
    The patch 6ae3a0876185: "ACER: Add support for accelerometer sensor"
    from Jun 1, 2012, leads to the following Smatch warning:
    drivers/platform/x86/acer-wmi.c:1886 acer_wmi_accel_destroy()
             error: don't call input_free_device() after input_unregister_device()
    
    drivers/platform/x86/acer-wmi.c
      1883  static void acer_wmi_accel_destroy(void)
      1884  {
      1885          input_unregister_device(acer_wmi_accel_dev);
      1886          input_free_device(acer_wmi_accel_dev);
      1887  }
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Matthew Garrett <mjg59@srcf.ucam.org>
    Cc: Fengguang Wu <wfg@linux.intel.com>
    Cc: joeyli <jlee@suse.com>
    Cc: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>

--- drivers/platform/x86/acer-wmi.c
+++ drivers/platform/x86/acer-wmi.c
@@ -1884,7 +1884,6 @@ err_free_dev:
 static void acer_wmi_accel_destroy(void)
 {
 	input_unregister_device(acer_wmi_accel_dev);
-	input_free_device(acer_wmi_accel_dev);
 }
 
 static int __init acer_wmi_input_setup(void)
