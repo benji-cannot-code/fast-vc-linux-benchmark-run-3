8f6959a4c826c68be3c4f00f6d77c0cf863ef37d
David Daney <david.daney@cavium.com>
2012-07-05 18:12:40 +0200

    staging: octeon_ethernet: Convert to use device tree.
    Get MAC address and PHY connection from the device tree.  The driver
    is converted to a platform driver.
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Acked-by: Grant Likely <grant.likely@secretlab.ca>
    Cc: linux-mips@linux-mips.org
    Cc: devicetree-discuss@lists.ozlabs.org
    Cc: Rob Herring <rob.herring@calxeda.com>
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/3940/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- drivers/staging/octeon/octeon-ethernet.h
+++ drivers/staging/octeon/octeon-ethernet.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef OCTEON_ETHERNET_H
 #define OCTEON_ETHERNET_H
 
+#include <linux/of.h>
+
 /**
  * This is the definition of the Ethernet driver's private
  * driver state stored in netdev_priv(dev).
@@ -60,6 +62,7 @@ struct octeon_ethernet {
 	void (*poll) (struct net_device *dev);
 	struct delayed_work	port_periodic_work;
 	struct work_struct	port_work;	/* may be unused. */
+	struct device_node	*of_node;
 };
 
 int cvm_oct_free_work(void *work_queue_entry);
