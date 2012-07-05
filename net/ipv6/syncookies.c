0e5cd97d0f4c94a46f250e595b5ebbd3dd76a739
Eric Dumazet <edumazet@google.com>
2012-07-05 20:34:35 +0000

    ipv6: remove redundant declarations
    remove redundant declarations, they belong in include/net/tcp.h
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/syncookies.c
+++ net/ipv6/syncookies.c
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ipv6.h>
 #include <net/tcp.h>
 
-extern int sysctl_tcp_syncookies;
-extern __u32 syncookie_secret[2][16-4+SHA_DIGEST_WORDS];
-
 #define COOKIEBITS 24	/* Upper bits store count */
 #define COOKIEMASK (((__u32)1 << COOKIEBITS) - 1)
 
