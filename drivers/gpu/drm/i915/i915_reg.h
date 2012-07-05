6702632ffa1f4f05f55cda20de9ed65c0dda9008
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-07-05 12:17:30 +0200

    drm/i915: adjust framebuffer base address on gen4+
    The tileoffset register only supports a limited offset in x/y of 4096,
    so for giant screen configuration with a shared fb we wrap around.
    
    Fix this by computing a linear offset in tiles (pages) and only use
    the tileoffset register to offset within the tile.
    
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -2987,7 +2987,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I915_LO_DISPBASE(val)	(val & ~DISP_BASEADDR_MASK)
 #define I915_HI_DISPBASE(val)	(val & DISP_BASEADDR_MASK)
 #define I915_MODIFY_DISPBASE(reg, gfx_addr) \
-		(I915_WRITE(reg, gfx_addr | I915_LO_DISPBASE(I915_READ(reg))))
+		(I915_WRITE((reg), (gfx_addr) | I915_LO_DISPBASE(I915_READ(reg))))
 
 /* VBIOS flags */
 #define SWF00			0x71410
