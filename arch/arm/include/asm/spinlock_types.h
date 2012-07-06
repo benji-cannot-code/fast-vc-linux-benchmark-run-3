e25b66cbc98e5376933c4bd7922f92336e642f4e
Will Deacon <will.deacon@arm.com>
2012-07-06 15:43:41 +0100

    ARM: 7446/1: spinlock: use ticket algorithm for ARMv6+ locking implementation
    Ticket spinlocks ensure locking fairness by introducing a FIFO-like
    nature to the granting of lock acquisitions and also reducing the
    thundering herd effect when spinning on a lock by allowing the cacheline
    to remain in a shared state amongst the waiting CPUs. This is especially
    important on systems where memory-access times are not necessarily
    uniform when accessing the lock structure (for example, on a
    multi-cluster platform where the lock is allocated into L1 when a CPU
    releases it).
    
    This patch implements the ticket spinlock algorithm for ARM, replacing
    the simpler implementation for ARMv6+ processors.
    
    Reviewed-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/spinlock_types.h
+++ arch/arm/include/asm/spinlock_types.h
@@ -6,11 +6,24 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # error "please don't include this file directly"
 #endif
 
+#define TICKET_SHIFT	16
+
 typedef struct {
-	volatile unsigned int lock;
+	union {
+		u32 slock;
+		struct __raw_tickets {
+#ifdef __ARMEB__
+			u16 next;
+			u16 owner;
+#else
+			u16 owner;
+			u16 next;
+#endif
+		} tickets;
+	};
 } arch_spinlock_t;
 
-#define __ARCH_SPIN_LOCK_UNLOCKED	{ 0 }
+#define __ARCH_SPIN_LOCK_UNLOCKED	{ { 0 } }
 
 typedef struct {
 	volatile unsigned int lock;
