80b4234e94cd20c340a87312fd2c6230af7fe2cc
Joerg Roedel <joerg.roedel@amd.com>
2012-07-06 13:28:37 +0200

    iommu/amd: Fix sparse warnings
    A few sparse warnings fire in drivers/iommu/amd_iommu_init.c.
    Fix most of them with this patch. Also fix the sparse
    warnings in drivers/iommu/irq_remapping.c while at it.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/irq_remapping.c
+++ drivers/iommu/irq_remapping.c
@@ -1,7 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/string.h>
+#include <linux/cpumask.h>
 #include <linux/errno.h>
+#include <linux/msi.h>
+
+#include <asm/hw_irq.h>
+#include <asm/irq_remapping.h>
 
 #include "irq_remapping.h"
 
