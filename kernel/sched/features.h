2ab1ab7493db8ba27b8bd7de6fe278c7d82b17bc
Vincent Guittot <vincent.guittot@linaro.org>
2012-07-09 11:27:06 +0200

    sched: cpu_power: enable ARCH_POWER
    Heteregeneous ARM platform uses arch_scale_freq_power function
    to reflect the relative capacity of each core
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1341826026-6504-6-git-send-email-vincent.guittot@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -35,7 +35,7 @@ SCHED_FEAT(CACHE_HOT_BUDDY, true)
 /*
  * Use arch dependent cpu power functions
  */
-SCHED_FEAT(ARCH_POWER, false)
+SCHED_FEAT(ARCH_POWER, true)
 
 SCHED_FEAT(HRTICK, false)
 SCHED_FEAT(DOUBLE_TICK, false)
