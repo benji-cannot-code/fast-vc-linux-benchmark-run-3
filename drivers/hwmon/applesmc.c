fb99f0f256d9d03c32b52f7257e120a1991bb548
Henrik Rydberg <rydberg@euromail.se>
2012-07-09 12:10:26 +0200

    hwmon: (applesmc) Shorten minimum wait time
    The 2012 series of MacBooks have a faster SMC, and the current driver
    timings do not work at all. Tests show that decreasing the minimum
    wait time, from 64 us to 16 us, works well. Since this is still larger
    than the original minimum of 10 us used before 2008, there is nothing
    inherently problematic with changing it. The fail frequency on older
    machines seems to increase slightly, but not enough to be noticeable.
    
    Tested on MBA11, MBA31, MBA5,2, MBP9,2.
    
    The patch was originally written by adamski99 (ubuntuforums.org) and
    later tested by janhouse (bbs.archlinux.org).
    
    Signed-off-by: Henrik Rydberg <rydberg@euromail.se>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/applesmc.c
+++ drivers/hwmon/applesmc.c
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define APPLESMC_MAX_DATA_LENGTH 32
 
 /* wait up to 32 ms for a status change. */
-#define APPLESMC_MIN_WAIT	0x0040
+#define APPLESMC_MIN_WAIT	0x0010
 #define APPLESMC_MAX_WAIT	0x8000
 
 #define APPLESMC_STATUS_MASK	0x0f
