ecba2eb3137d53b26f3afa077f5590d114db2065
Antti Palosaari <crope@iki.fi>
2012-07-09 14:59:28 -0300

    [media] dvb_usb_v2: move from dvb-usb to dvb-usb-v2
    Move to own directory.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/dvb-usb-v2/dvb_usb.h
+++ drivers/media/dvb/dvb-usb-v2/dvb_usb.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_demux.h"
 #include "dvb_net.h"
 #include "dmxdev.h"
-#include "dvb-usb-ids.h"
+#include "../dvb-usb/dvb-usb-ids.h"
 
 /*
  * device file: /dev/dvb/adapter[0-1]/frontend[0-2]
