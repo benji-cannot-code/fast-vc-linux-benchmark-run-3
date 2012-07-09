8665ef449810060e925ff85b2ea1c8737114a6e6
Varun Sethi <Varun.Sethi@freescale.com>
2012-07-09 18:28:21 +0530

    powerpc/booke: Merge the 32 bit e5500/e500mc cpu setup code.
    Merge the 32 bit cpu setup code for e500mc/e5500 and define the
    "cpu_restore" routine (for e5500/e6500) only for the 64 bit case. The
    cpu_restore routine is used in the 64 bit case for setting up the secondary
    cores.
    
    Signed-off-by: Varun Sethi <Varun.Sethi@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/kernel/cputable.c
+++ arch/powerpc/kernel/cputable.c
@@ -2017,7 +2017,9 @@ static struct cpu_spec __initdata cpu_specs[] = {
 		.oprofile_cpu_type	= "ppc/e500mc",
 		.oprofile_type		= PPC_OPROFILE_FSL_EMB,
 		.cpu_setup		= __setup_cpu_e5500,
+#ifndef CONFIG_PPC32
 		.cpu_restore		= __restore_cpu_e5500,
+#endif
 		.machine_check		= machine_check_e500mc,
 		.platform		= "ppce5500",
 	},
@@ -2035,7 +2037,9 @@ static struct cpu_spec __initdata cpu_specs[] = {
 		.oprofile_cpu_type	= "ppc/e6500",
 		.oprofile_type		= PPC_OPROFILE_FSL_EMB,
 		.cpu_setup		= __setup_cpu_e5500,
+#ifndef CONFIG_PPC32
 		.cpu_restore		= __restore_cpu_e5500,
+#endif
 		.machine_check		= machine_check_e500mc,
 		.platform		= "ppce6500",
 	},
