210b46751538dec85d33a7152f86f5958b962404
H Hartley Sweeten <hartleys@visionengravers.com>
2012-07-10 16:52:45 -0700

    staging: comedi: rtd520: remove RtdInterruptStatus macro
    This macro uses the 'devpriv' macro which relies on a local variable
    having a specific name. Plus it's just a wrapper around a simple
    'readw'. Remove the macro.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/rtd520.c
+++ drivers/staging/comedi/drivers/rtd520.c
@@ -407,10 +407,6 @@ struct rtdPrivate {
 
 /* Macros to access registers */
 
-/* Interrupt status */
-#define RtdInterruptStatus(dev) \
-	readw(devpriv->las0+LAS0_IT)
-
 /* Interrupt mask */
 #define RtdInterruptMask(dev, v) \
 	writew((devpriv->intMask = (v)), devpriv->las0+LAS0_IT)
@@ -1079,7 +1075,7 @@ static irqreturn_t rtd_interrupt(int irq,	/* interrupt number (ignored) */
 	/* Fall through and check for other interrupt sources */
 #endif /* USE_DMA */
 
-	status = RtdInterruptStatus(dev);
+	status = readw(devpriv->las0 + LAS0_IT);
 	/* if interrupt was not caused by our board, or handled above */
 	if (0 == status)
 		return IRQ_HANDLED;
@@ -1176,7 +1172,7 @@ transferDone:
 	comedi_event(dev, s);
 
 	/* clear the interrupt */
-	status = RtdInterruptStatus(dev);
+	status = readw(devpriv->las0 + LAS0_IT);
 	RtdInterruptClearMask(dev, status);
 	RtdInterruptClear(dev);
 
@@ -1628,7 +1624,7 @@ static int rtd_ai_cancel(struct comedi_device *dev, struct comedi_subdevice *s)
 		devpriv->flags &= ~DMA0_ACTIVE;
 	}
 #endif /* USE_DMA */
-	status = RtdInterruptStatus(dev);
+	status = readw(devpriv->las0 + LAS0_IT);
 	DPRINTK
 	    ("rtd520: Acquisition canceled. %ld ints, intStat=%x, overStat=%x\n",
 	     devpriv->intCount, status,
