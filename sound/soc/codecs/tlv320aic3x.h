8ab3949bfb035168cc0fa8796e7584b91b679f50
Jiri Prchal <jiri.prchal@aksignal.cz>
2012-07-10 14:36:58 +0200

    ASoC: tlv320aic3x: add input clock selection
    This patch adds input selection of main codec clock - from what pin.
    Both registers set same value since codec uses clock divider or pll at one time.
    
    Signed-off-by: Jiri Prchal <jiri.prchal@aksignal.cz>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/tlv320aic3x.h
+++ sound/soc/codecs/tlv320aic3x.h
@@ -196,6 +196,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PLL_CLKIN_SHIFT		4
 #define MCLK_SOURCE		0x0
 #define PLL_CLKDIV_SHIFT	0
+#define PLLCLK_IN_MASK		0x30
+#define PLLCLK_IN_SHIFT		4
+#define CLKDIV_IN_MASK		0xc0
+#define CLKDIV_IN_SHIFT		6
+/* clock in source */
+#define CLKIN_MCLK		0
+#define CLKIN_GPIO2		1
+#define CLKIN_BCLK		2
 
 /* Software reset register bits */
 #define SOFT_RESET		0x80
