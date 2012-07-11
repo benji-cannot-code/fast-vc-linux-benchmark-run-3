1513545eb949b6766d40e074c3c95f38c4095cdc
Pawel Moll <pawel.moll@arm.com>
2012-07-11 17:07:25 +0100

    ARM: vexpress: Config option for early printk console
    Versatile Express platform can be used in different configurations,
    the console UART used by early printk may be located at different
    addresses in the address space.
    
    This patch makes it possible to select the base address of a PL011
    UART to be used as a console output in the kernel configuration.
    The default behaviour is still the heuristic detecting memory map
    on Cortex-A core tiles.
    
    The zImage decompressor will use the same configuration values
    or print out nothing if DEBUG_LL is not enabled.
    
    Signed-off-by: Pawel Moll <pawel.moll@arm.com>

--- arch/arm/mach-vexpress/include/mach/uncompress.h
+++ arch/arm/mach-vexpress/include/mach/uncompress.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static unsigned long get_uart_base(void)
 {
+#if defined(CONFIG_DEBUG_VEXPRESS_UART0_DETECT)
 	unsigned long mpcore_periph;
 
 	/*
@@ -43,6 +44,13 @@ static unsigned long get_uart_base(void)
 		return UART_BASE;
 	else
 		return UART_BASE_RS1;
+#elif defined(CONFIG_DEBUG_VEXPRESS_UART0_CA9)
+	return UART_BASE;
+#elif defined(CONFIG_DEBUG_VEXPRESS_UART0_RS1)
+	return UART_BASE_RS1;
+#else
+	return 0;
+#endif
 }
 
 /*
@@ -51,6 +59,9 @@ static unsigned long get_uart_base(void)
 static inline void putc(int c)
 {
 	unsigned long base = get_uart_base();
+
+	if (!base)
+		return;
 
 	while (AMBA_UART_FR(base) & (1 << 5))
 		barrier();
@@ -61,6 +72,9 @@ static inline void putc(int c)
 static inline void flush(void)
 {
 	unsigned long base = get_uart_base();
+
+	if (!base)
+		return;
 
 	while (AMBA_UART_FR(base) & (1 << 3))
 		barrier();
