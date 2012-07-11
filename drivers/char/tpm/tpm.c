bf425f2ef8c0a783af323564e780bf15a88a1265
Kent Yoder <key@linux.vnet.ibm.com>
2012-07-11 10:08:12 -0500

    tpm: modularize event log collection
    Break ACPI-specific pieces of the event log handling into their own file
    and create tpm_eventlog.[ch] to store common event log handling code.
    This will be required to integrate future event log sources on platforms
    without ACPI tables.
    
    Signed-off-by: Kent Yoder <key@linux.vnet.ibm.com>

--- drivers/char/tpm/tpm.c
+++ drivers/char/tpm/tpm.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/freezer.h>
 
 #include "tpm.h"
+#include "tpm_eventlog.h"
 
 enum tpm_const {
 	TPM_MINOR = 224,	/* officially assigned */
