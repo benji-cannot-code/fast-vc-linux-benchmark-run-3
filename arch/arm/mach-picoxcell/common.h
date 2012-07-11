666efe8b1a481eacd16e3d98c4e2a95fb4e01955
Dinh Nguyen <dinguyen@altera.com>
2012-07-11 15:13:16 -0500

    clocksource: dw_apb_timer: Add common DTS glue for dw_apb_timer
    Make a common device tree glue for clocksource/dw_apb_timer.
    Move mach-picoxcell/time.c to be a generic device tree application
    of the dw_apb_timer.
    
    Configure mach-picoxcell to use the dw_apb_timer_of device tree
    implementation in drivers/clocksource.
    
    Signed-off-by: Pavel Machek <pavel@denx.de>
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    Acked-by: Jamie Iles <jamie@jamieiles.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-picoxcell/common.h
+++ arch/arm/mach-picoxcell/common.h
@@ -13,6 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach/time.h>
 
-extern struct sys_timer picoxcell_timer;
+extern struct sys_timer dw_apb_timer;
 
 #endif /* __PICOXCELL_COMMON_H__ */
