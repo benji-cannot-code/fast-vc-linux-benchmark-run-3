07ea7edc5ce22066a0bc511f1478f29f052d26bd
Andres Salomon <dilinger@queued.net>
2012-07-11 01:16:29 -0700

    drivers: OLPC: update various drivers to include olpc-ec.h
    Switch over to using olpc-ec.h in multiple steps, so as not to break builds.
    This covers every driver that calls olpc_ec_cmd().
    
    Signed-off-by: Andres Salomon <dilinger@queued.net>
    Acked-by: Paul Fox <pgf@laptop.org>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/olpc.h
+++ arch/x86/include/asm/olpc.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X86_OLPC_H
 
 #include <asm/geode.h>
-#include <linux/olpc-ec.h>
 
 struct olpc_platform_t {
 	int flags;
--- arch/x86/platform/olpc/olpc.c
+++ arch/x86/platform/olpc/olpc.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/syscore_ops.h>
 #include <linux/debugfs.h>
 #include <linux/mutex.h>
+#include <linux/olpc-ec.h>
 
 #include <asm/geode.h>
 #include <asm/setup.h>
