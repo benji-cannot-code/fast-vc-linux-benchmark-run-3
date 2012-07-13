a5b262e1dd001b1af4c5f02f814bb1ad2f0e339f
Bjorn Helgaas <bhelgaas@google.com>
2012-07-13 14:30:21 -0600

    PCI: Add Vendor-Specific Extended Capability header info
    This adds the fields in the Vendor-Specific Header: ID, Rev, and Length.
    There may be multiple Vendor-Specific capabilities, so drivers should use
    the VSEC ID to identify the one of interest.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- include/linux/pci_regs.h
+++ include/linux/pci_regs.h
@@ -678,6 +678,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  PCI_PWR_CAP_BUDGET(x)	((x) & 1)	/* Included in system budget */
 #define PCI_EXT_CAP_PWR_SIZEOF	16
 
+/* Vendor-Specific (VSEC, PCI_EXT_CAP_ID_VNDR) */
+#define PCI_VNDR_HEADER		4	/* Vendor-Specific Header */
+#define  PCI_VNDR_HEADER_ID(x)	((x) & 0xffff)
+#define  PCI_VNDR_HEADER_REV(x)	(((x) >> 16) & 0xf)
+#define  PCI_VNDR_HEADER_LEN(x)	(((x) >> 20) & 0xfff)
+
 /*
  * Hypertransport sub capability types
  *
