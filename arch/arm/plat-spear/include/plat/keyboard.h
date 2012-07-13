006f677733085fb7f2370e3280080261b74d6c32
Shiraz Hashim <shiraz.hashim@st.com>
2012-07-13 00:11:10 -0700

    Input: spear_keyboard - reconfigure operating frequency on suspend
    On some platform it may happen that the input clock to keyboard may
    change during suspend, thus impacting its wakeup capability.
    
    There is no means for keyboard driver to know this frequency before
    hand. Hence introduce a platform data 'suspended_rate' which indicates
    the frequency during suspend at which keyboard operates.
    
    Accordingly reprogram keyboard while going into suspend and restore
    original configuration at the time of resume.
    
    Signed-off-by: Shiraz Hashim <shiraz.hashim@st.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- arch/arm/plat-spear/include/plat/keyboard.h
+++ arch/arm/plat-spear/include/plat/keyboard.h
@@ -150,6 +150,7 @@ int _name[] = { \
  * keymap: pointer to keymap data (table and size)
  * rep: enables key autorepeat
  * mode: choose keyboard support(9x9, 6x6, 2x2)
+ * suspended_rate: rate at which keyboard would operate in suspended mode
  *
  * This structure is supposed to be used by platform code to supply
  * keymaps to drivers that implement keyboards.
@@ -158,6 +159,7 @@ struct kbd_platform_data {
 	const struct matrix_keymap_data *keymap;
 	bool rep;
 	unsigned int mode;
+	unsigned int suspended_rate;
 };
 
 #endif /* __PLAT_KEYBOARD_H */
