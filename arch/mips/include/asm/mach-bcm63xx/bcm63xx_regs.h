f5cf7ccce88a0713ca20279f84e71d5eb7952263
Maxime Bizon <mbizon@freebox.fr>
2012-07-13 07:46:03 +0000

    MIPS: BCM63XX: add external irq support for BCM6345
    Add the missing definitions for BCM6345.
    
    Signed-off-by: Maxime Bizon <mbizon@freebox.fr>
    Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>
    Patchwork: http://patchwork.linux-mips.org/patch/4091/
    Signed-off-by: John Crispin <blogic@openwrt.org>

--- arch/mips/include/asm/mach-bcm63xx/bcm63xx_regs.h
+++ arch/mips/include/asm/mach-bcm63xx/bcm63xx_regs.h
@@ -162,6 +162,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* External Interrupt Configuration register */
 #define PERF_EXTIRQ_CFG_REG_6328	0x18
 #define PERF_EXTIRQ_CFG_REG_6338	0x14
+#define PERF_EXTIRQ_CFG_REG_6345	0x14
 #define PERF_EXTIRQ_CFG_REG_6348	0x14
 #define PERF_EXTIRQ_CFG_REG_6358	0x14
 #define PERF_EXTIRQ_CFG_REG_6368	0x18
