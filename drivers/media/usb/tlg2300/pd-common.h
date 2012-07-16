b54818be725c2f625d07750bd2f3fcb4b528d584
Hans Verkuil <hans.verkuil@cisco.com>
2012-07-16 08:17:58 -0300

    [media] tlg2300: embed video_device
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Acked-by: Huang Shijie <shijie8@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/tlg2300/pd-common.h
+++ drivers/media/usb/tlg2300/pd-common.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TUNER_FREQ_MAX		(862000000)
 
 struct vbi_data {
-	struct video_device	*v_dev;
+	struct video_device	v_dev;
 	struct video_data	*video;
 	struct front_face	*front;
 
@@ -64,7 +64,7 @@ struct running_context {
 
 struct video_data {
 	/* v4l2 video device */
-	struct video_device	*v_dev;
+	struct video_device	v_dev;
 
 	/* the working context */
 	struct running_context	context;
@@ -235,7 +235,6 @@ void dvb_stop_streaming(struct pd_dvb_adapter *);
 /* FM */
 int poseidon_fm_init(struct poseidon *);
 int poseidon_fm_exit(struct poseidon *);
-struct video_device *vdev_init(struct poseidon *, struct video_device *);
 
 /* vendor command ops */
 int send_set_req(struct poseidon*, u8, s32, s32*);
@@ -251,7 +250,6 @@ void free_all_urb_generic(struct urb **urb_array, int num);
 
 /* misc */
 void poseidon_delete(struct kref *kref);
-void destroy_video_device(struct video_device **v_dev);
 extern int debug_mode;
 void set_debug_mode(struct video_device *vfd, int debug_mode);
 
