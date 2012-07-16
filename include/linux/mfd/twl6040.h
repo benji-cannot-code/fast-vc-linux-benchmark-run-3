2bc472b478a333fdc385cb60f5775dbd01d6d062
Peter Ujfalusi <peter.ujfalusi@ti.com>
2012-07-16 11:49:44 +0200

    mfd: Add support for twl6041
    The delta between twl6040 and twl6041 is small, the main difference is in
    the number of GPOs (3 on twl6040, 1 on twl6041).
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/twl6040.h
+++ include/linux/mfd/twl6040.h
@@ -164,6 +164,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TWL6040_REV_ES1_0		0x00
 #define TWL6040_REV_ES1_1		0x01 /* Rev ES1.1 and ES1.2 */
 #define TWL6040_REV_ES1_3		0x02
+#define TWL6041_REV_ES2_0		0x10
 
 #define TWL6040_IRQ_TH			0
 #define TWL6040_IRQ_PLUG		1
