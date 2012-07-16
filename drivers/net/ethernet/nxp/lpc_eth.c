756b0ab6c8a25e6ddce038ea2f191fff14b78ba0
Duan Jiong <djduanjiong@gmail.com>
2012-07-16 02:29:05 +0000

    lpc_eth: remove duplicated include
    Remove duplicated #include <linux/delay.h> in
    drivers/net/ethernet/nxp/lpc_eth.c
    
    Signed-off-by: Duan Jiong<djduanjiong@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/nxp/lpc_eth.c
+++ drivers/net/ethernet/nxp/lpc_eth.c
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_net.h>
 #include <linux/types.h>
 
-#include <linux/delay.h>
 #include <linux/io.h>
 #include <mach/board.h>
 #include <mach/platform.h>
