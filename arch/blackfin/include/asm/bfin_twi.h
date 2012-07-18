3322493eb614691cb7bc6d885ed777c7a0659870
Sonic Zhang <sonic.zhang@analog.com>
2012-07-18 18:22:45 +0800

    blackfin: twi: read twi mmr via bfin_read macro
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Bob Liu <lliubbo@gmail.com>

--- arch/blackfin/include/asm/bfin_twi.h
+++ arch/blackfin/include/asm/bfin_twi.h
@@ -95,7 +95,7 @@ static inline u16 read_RCV_DATA8(struct bfin_twi_iface *iface)
 	unsigned long flags;
 
 	flags = hard_local_irq_save();
-	ret = iface->regs_base->rcv_data8;
+	ret = bfin_read16(&iface->regs_base->rcv_data8);
 	hard_local_irq_restore(flags);
 
 	return ret;
@@ -107,7 +107,7 @@ static inline u16 read_RCV_DATA16(struct bfin_twi_iface *iface)
 	unsigned long flags;
 
 	flags = hard_local_irq_save();
-	ret = iface->regs_base->rcv_data16;
+	ret = bfin_read16(&iface->regs_base->rcv_data16);
 	hard_local_irq_restore(flags);
 
 	return ret;
