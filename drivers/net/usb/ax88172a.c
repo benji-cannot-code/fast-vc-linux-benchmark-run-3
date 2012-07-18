bc71541aa3b734ca3306aa2fc60faa57cd6ed4ac
Christian Riesch <christian.riesch@omicron.at>
2012-07-18 12:56:52 +0200

    asix: Fix return value in AX88172A driver bind function
    Return -ENOTSUPP if the initialization fails because the
    device is configured for a mode that is not supported by the driver.
    
    Signed-off-by: Christian Riesch <christian.riesch@omicron.at>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/ax88172a.c
+++ drivers/net/usb/ax88172a.c
@@ -275,6 +275,7 @@ static int ax88172a_bind(struct usbnet *dev, struct usb_interface *intf)
 		break;
 	default:
 		netdev_err(dev->net, "Interface mode not supported by driver\n");
+		ret = -ENOTSUPP;
 		goto free;
 	}
 
