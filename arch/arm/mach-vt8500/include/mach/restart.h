8e97c8476612a05a5ed8d50b8621ecd4a89aaec8
Tony Prisk <linux@prisktech.co.nz>
2012-07-19 03:42:19 +1200

    ARM:vt8500: Convert to use .restart and remove arch_reset()
    Removed system.h as it only contained an inline for arch_reset()
    Changed the existing board files to use .restart in there machine
    descriptions.
    
    Added device tree support for the restart controller.
    Device tree support for mach-vt8500 is still a work-in-progress.
    
    Signed-off-by: Tony Prisk <linux@prisktech.co.nz>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

---
+++ arch/arm/mach-vt8500/include/mach/restart.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* linux/arch/arm/mach-vt8500/restart.h
+ *
+ * Copyright (C) 2012 Tony Prisk <linux@prisktech.co.nz>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+void wmt_setup_restart(void);
+void wmt_restart(char mode, const char *cmd);
