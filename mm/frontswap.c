81a1a57d429fbc041b702585213ab855e6e82097
Sasha Levin <levinsasha928@gmail.com>
2012-07-19 18:51:22 -0400

    mm: frontswap: remove unneeded headers
    Signed-off-by: Sasha Levin <levinsasha928@gmail.com>
    [v1: Rebased with tracing removed]
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- mm/frontswap.c
+++ mm/frontswap.c
@@ -12,15 +12,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This work is licensed under the terms of the GNU GPL, version 2.
  */
 
-#include <linux/mm.h>
 #include <linux/mman.h>
 #include <linux/swap.h>
 #include <linux/swapops.h>
-#include <linux/proc_fs.h>
 #include <linux/security.h>
-#include <linux/capability.h>
 #include <linux/module.h>
-#include <linux/uaccess.h>
 #include <linux/debugfs.h>
 #include <linux/frontswap.h>
 #include <linux/swapfile.h>
