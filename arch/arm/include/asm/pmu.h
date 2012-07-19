e52eb38fbbd823d3712f1be09257772cf24a7764
Sudeep KarkadaNagesha <Sudeep.KarkadaNagesha@arm.com>
2012-07-19 09:50:21 +0100

    ARM: pmu: remove arm_pmu_type enumeration
    The arm_pmu_type enumeration was initially introduced to identify
    different PMU types in the system, the usual one being that on the CPU
    (ARM_PMU_DEVICE_CPU). With the removal of the PMU reservation code and
    the introduction of devicetree bindings for the CPU PMU, the enumeration
    is no longer required.
    
    This patch removes the enumeration and updates the various CPU PMU
    platform devices so that they no longer pass an .id field referring
    to identify the PMU type.
    
    Cc: Haojian Zhuang <haojian.zhuang@gmail.com>
    Cc: Olof Johansson <olof@lixom.net>
    Cc: Pawel Moll <pawel.moll@arm.com>
    Acked-by: Jon Hunter <jon-hunter@ti.com>
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Jiandong Zheng <jdzheng@broadcom.com>
    Signed-off-by: Sudeep KarkadaNagesha <Sudeep.KarkadaNagesha@arm.com>
    [will: cosmetic edits and actual removal of the enum type]
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm/include/asm/pmu.h
+++ arch/arm/include/asm/pmu.h
@@ -17,15 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/perf_event.h>
 
 /*
- * Types of PMUs that can be accessed directly and require mutual
- * exclusion between profiling tools.
- */
-enum arm_pmu_type {
-	ARM_PMU_DEVICE_CPU	= 0,
-	ARM_NUM_PMU_DEVICES,
-};
-
-/*
  * struct arm_pmu_platdata - ARM PMU platform data
  *
  * @handle_irq: an optional handler which will be called from the
@@ -74,7 +65,6 @@ struct pmu_hw_events {
 
 struct arm_pmu {
 	struct pmu	pmu;
-	enum arm_pmu_type type;
 	cpumask_t	active_irqs;
 	char		*name;
 	irqreturn_t	(*handle_irq)(int irq_num, void *dev);
