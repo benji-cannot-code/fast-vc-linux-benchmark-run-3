c5bf29c5de1712c5f700cc6dc08c7870cc1d36bc
Mark Salter <msalter@redhat.com>
2012-07-18 23:49:40 -0400

    C6X: remove dependence on legacy IRQs
    The core priority PIC code uses legacy irq support to facilitate direct
    mapping of core hw interrupt numbers to linux interrupt numbers. This
    patch removes the legacy irq usage and replaces it with a generic linear
    mapping.
    
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- arch/c6x/include/asm/irq.h
+++ arch/c6x/include/asm/irq.h
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define NR_PRIORITY_IRQS 16
 
-#define NR_IRQS_LEGACY	NR_PRIORITY_IRQS
-
 /* Total number of virq in the platform */
 #define NR_IRQS		256
 
