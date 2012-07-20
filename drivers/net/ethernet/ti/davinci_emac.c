18f277b908c84191bb9abf0419f7ca38b00f0b39
Mark A. Greer <mgreer@animalcreek.com>
2012-07-20 15:19:21 +0000

    net: ethernet: davinci_emac: Remove unnecessary #include
    The '#include <mach/mux.h>' line in davinci_emac.c
    causes a compile error because that header file
    isn't found.  It turns out that the #include isn't
    needed because the driver isn't (and shoudn't be)
    touching the mux anyway, so remove it.
    
    CC: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Mark A. Greer <mgreer@animalcreek.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ti/davinci_emac.c
+++ drivers/net/ethernet/ti/davinci_emac.c
@@ -64,8 +64,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/of_net.h>
 
-#include <mach/mux.h>
-
 #include <asm/irq.h>
 #include <asm/page.h>
 
