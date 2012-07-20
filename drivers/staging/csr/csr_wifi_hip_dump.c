1c4af6aa16769f26647348ce32e446a784ba2ab1
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-07-20 14:16:11 -0700

    staging: csr: remove CsrMemSet
    It's really just memset(), so call that instead.
    
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Lauri Hintsala <Lauri.Hintsala@bluegiga.com>
    Cc: Riku Mettälä <riku.mettala@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_wifi_hip_dump.c
+++ drivers/staging/csr/csr_wifi_hip_dump.c
@@ -672,7 +672,7 @@ coredump_buffer* new_coredump_node(void *ospriv, coredump_buffer *prevnode)
     {
         return NULL;
     }
-    CsrMemSet(newnode, 0, sizeof(coredump_buffer));
+    memset(newnode, 0, sizeof(coredump_buffer));
 
     /* Allocate chip memory zone capture buffers */
     for (i = 0; i < HIP_CDUMP_NUM_ZONES; i++)
@@ -682,7 +682,7 @@ coredump_buffer* new_coredump_node(void *ospriv, coredump_buffer *prevnode)
         newnode->zone[i] = newzone;
         if (newzone != NULL)
         {
-            CsrMemSet(newzone, 0, zone_size);
+            memset(newzone, 0, zone_size);
         }
         else
         {
--- drivers/staging/csr/unifi_sme.c
+++ drivers/staging/csr/unifi_sme.c
@@ -1211,7 +1211,7 @@ void uf_send_pkt_to_encrypt(struct work_struct *work)
 
         if (pktBulkDataLength > 0) {
 		    pktBulkData = (u8 *)CsrPmemAlloc(pktBulkDataLength);
-		    CsrMemSet(pktBulkData, 0, pktBulkDataLength);
+		    memset(pktBulkData, 0, pktBulkDataLength);
 	    } else {
 		    unifi_error(priv, "uf_send_pkt_to_encrypt() : invalid buffer\n");
 		    return;
