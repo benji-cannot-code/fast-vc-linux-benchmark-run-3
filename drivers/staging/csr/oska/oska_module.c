63d4c10985f94fb069cea826a3337997f9e3ce9b
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-07-19 17:37:48 -0700

    staging: csr: oska: remove refcount.c
    It's not called by anyone, so remove it and the .h file and don't export
    the functions as they are not around anymore.
    
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Lauri Hintsala <Lauri.Hintsala@bluegiga.com>
    Cc: Riku Mettälä <riku.mettala@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/oska/oska_module.c
+++ drivers/staging/csr/oska/oska_module.c
@@ -9,14 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/module.h>
 
-#include "all.h"
-#include "refcount.h"
-
-EXPORT_SYMBOL(os_refcount_init);
-EXPORT_SYMBOL(os_refcount_destroy);
-EXPORT_SYMBOL(os_refcount_get);
-EXPORT_SYMBOL(os_refcount_put);
-
 MODULE_DESCRIPTION("Operating System Kernel Abstraction");
 MODULE_AUTHOR("Cambridge Silicon Radio Ltd.");
 MODULE_LICENSE("GPL and additional rights");
