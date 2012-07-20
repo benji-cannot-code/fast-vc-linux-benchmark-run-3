8ea56a1feda08d5c82d4345ca79c7fdb5726715c
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-07-20 15:06:15 -0700

    stating: csr: remove unneeded #includes in csr_util.c
    This .c file only holds the MODULE_* stuff for the module, so remove the
    other #include calls as they are not needed anymore.
    
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Lauri Hintsala <Lauri.Hintsala@bluegiga.com>
    Cc: Riku Mettälä <riku.mettala@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_util.c
+++ drivers/staging/csr/csr_util.c
@@ -9,12 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 *****************************************************************************/
 
-#include <linux/kernel.h>
 #include <linux/module.h>
-#include <stdarg.h>
-
-#include "csr_pmem.h"
-#include "csr_util.h"
 
 MODULE_DESCRIPTION("CSR Operating System Kernel Abstraction");
 MODULE_AUTHOR("Cambridge Silicon Radio Ltd.");
