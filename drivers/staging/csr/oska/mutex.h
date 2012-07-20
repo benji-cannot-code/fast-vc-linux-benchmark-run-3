734000bc0003f16459fc7b30e2d7053108d88328
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-07-19 17:33:09 -0700

    Staging: csr: remove oska compat functions
    These functions were for older kernel versions, which we aren't
    supporting anymore now that this is in the kernel.  So remove the files,
    they are no longer needed.
    
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Lauri Hintsala <Lauri.Hintsala@bluegiga.com>
    Cc: Riku Mettälä <riku.mettala@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/oska/mutex.h
+++ drivers/staging/csr/oska/mutex.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/mutex.h>
+#include <linux/semaphore.h>
 
-#include "kernel-compat.h"
 
 /* Real mutexes were only added to 2.6.16 so use semaphores
    instead. */
