20b47b11a30f1a7a54ea6b5cb27fab1d47bb824a
Joe Millenbach <jmillenbach@gmail.com>
2012-07-19 18:04:41 -0700

    x86, boot: Exclude early_serial_console.c if can't use it.
    Removes early_serial_console.c code if we don't have the config option that
    enables it (EARLY_PRINTK). When disabling this code, make early_serial_base a
    constant 0 to allow the compiler to optimize away the code that checks for
    early_serial_base.
    
    Signed-off-by: Joe Millenbach <jmillenbach@gmail.com>
    Link: http://lkml.kernel.org/r/1342746282-28497-7-git-send-email-jmillenbach@gmail.com
    Signed-off-by: Gokul Caushik <caushik1@gmail.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/boot/compressed/early_serial_console.c
+++ arch/x86/boot/compressed/early_serial_console.c
@@ -1,6 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "misc.h"
 
+#ifdef CONFIG_EARLY_PRINTK
+
 int early_serial_base;
 
 #include "../early_serial_console.c"
+
+#endif
