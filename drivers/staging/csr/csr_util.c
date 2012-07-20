ea87f6ab41b492e509e80f2a61ff25ff3251265b
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-07-20 15:04:31 -0700

    staging: csr: make CsrUInt16ToHex static
    We do this by moving it from csr_util.c to csr_wifi_hip_card_sdio_intr.c
    as that's the only place that called it.
    
    Cc: Mikko Virkkilä <mikko.virkkila@bluegiga.com>
    Cc: Lauri Hintsala <Lauri.Hintsala@bluegiga.com>
    Cc: Riku Mettälä <riku.mettala@bluegiga.com>
    Cc: Veli-Pekka Peltola <veli-pekka.peltola@bluegiga.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_util.c
+++ drivers/staging/csr/csr_util.c
@@ -16,24 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "csr_pmem.h"
 #include "csr_util.h"
 
-/*------------------------------------------------------------------*/
-/* Base conversion */
-/*------------------------------------------------------------------*/
-/* Convert signed 32 bit (or less) integer to string */
-void CsrUInt16ToHex(u16 number, char *str)
-{
-    u16 index;
-    u16 currentValue;
-
-    for (index = 0; index < 4; index++)
-    {
-        currentValue = (u16) (number & 0x000F);
-        number >>= 4;
-        str[3 - index] = (char) (currentValue > 9 ? currentValue + 55 : currentValue + '0');
-    }
-    str[4] = '\0';
-}
-
 MODULE_DESCRIPTION("CSR Operating System Kernel Abstraction");
 MODULE_AUTHOR("Cambridge Silicon Radio Ltd.");
 MODULE_LICENSE("GPL and additional rights");
