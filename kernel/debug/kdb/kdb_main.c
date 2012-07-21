aecf0cf20f8e90eb2313d5a6cd45878f4eac92fe
Anton Vorontsov <anton.vorontsov@linaro.org>
2012-07-20 17:28:25 -0700

    kdb: Switch to nolock variants of kmsg_dump functions
    The locked variants are prone to deadlocks (suppose we got to the
    debugger w/ the logbuf lock held), so let's switch to nolock variants.
    
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/debug/kdb/kdb_main.c
+++ kernel/debug/kdb/kdb_main.c
@@ -2073,8 +2073,8 @@ static int kdb_dmesg(int argc, const char **argv)
 		kdb_set(2, setargs);
 	}
 
-	kmsg_dump_rewind(&dumper);
-	while (kmsg_dump_get_line(&dumper, 1, NULL, 0, NULL))
+	kmsg_dump_rewind_nolock(&dumper);
+	while (kmsg_dump_get_line_nolock(&dumper, 1, NULL, 0, NULL))
 		n++;
 
 	if (lines < 0) {
@@ -2106,8 +2106,8 @@ static int kdb_dmesg(int argc, const char **argv)
 	if (skip >= n || skip < 0)
 		return 0;
 
-	kmsg_dump_rewind(&dumper);
-	while (kmsg_dump_get_line(&dumper, 1, buf, sizeof(buf), &len)) {
+	kmsg_dump_rewind_nolock(&dumper);
+	while (kmsg_dump_get_line_nolock(&dumper, 1, buf, sizeof(buf), &len)) {
 		if (skip) {
 			skip--;
 			continue;
