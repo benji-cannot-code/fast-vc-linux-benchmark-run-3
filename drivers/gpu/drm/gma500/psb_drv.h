2c1afee5592b7851e4330f920fdfa93181780d60
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2012-07-24 16:47:34 +0100

    drm: gma500: Kill the GEM glue layer
    The private gem_create_mmap_offset() function is now implemented in the
    DRM core as drm_gem_create_mmap_offset(). Use it and kill the private
    copy.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/gma500/psb_drv.h
+++ drivers/gpu/drm/gma500/psb_drv.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <drm/drmP.h>
 #include "drm_global.h"
-#include "gem_glue.h"
 #include "gma_drm.h"
 #include "psb_reg.h"
 #include "psb_intel_drv.h"
