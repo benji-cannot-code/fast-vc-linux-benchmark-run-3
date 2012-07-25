aef64285f8cedc4bf69aeafae4e4c5e0a93fbf04
Felipe Balbi <balbi@ti.com>
2012-07-25 15:05:32 +0300

    w1: omap-hdq: drop ARCH dependency
    Let the driver compile everywhere while
    also removing unnecessary headers.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Acked-by: Evgeniy Polyakov <zbr@ioremap.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/w1/masters/omap_hdq.c
+++ drivers/w1/masters/omap_hdq.c
@@ -19,9 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/pm_runtime.h>
 
-#include <asm/irq.h>
-#include <mach/hardware.h>
-
 #include "../w1.h"
 #include "../w1_int.h"
 
