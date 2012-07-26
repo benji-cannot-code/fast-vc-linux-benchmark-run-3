7d0559d6610ff42dbb43424be9a5c62d0f0c8668
Sam Ravnborg <sam@ravnborg.org>
2012-07-26 11:02:26 +0000

    sparc32: move probe_memory() to srmmu.c
    Only one user so move it to the file using it.
    It had nothing to do in fault_32.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/fault_32.c
+++ arch/sparc/mm/fault_32.c
@@ -33,18 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int show_unhandled_signals = 1;
 
-/* Return how much physical memory we have.  */
-unsigned long probe_memory(void)
-{
-	unsigned long total = 0;
-	int i;
-
-	for (i = 0; sp_banks[i].num_bytes; i++)
-		total += sp_banks[i].num_bytes;
-
-	return total;
-}
-
 static void unhandled_fault(unsigned long, struct task_struct *,
 		struct pt_regs *) __attribute__ ((noreturn));
 
