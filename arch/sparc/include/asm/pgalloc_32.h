eb2e2b930981f433a38becc9988f122e6b4e27a8
Sam Ravnborg <sam@ravnborg.org>
2012-07-26 11:02:23 +0000

    sparc32: drop quicklist
    The quicklist stuff is not used anymore - drop it.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/pgalloc_32.h
+++ arch/sparc/include/asm/pgalloc_32.h
@@ -12,21 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct page;
 
-extern struct pgtable_cache_struct {
-	unsigned long *pgd_cache;
-	unsigned long *pte_cache;
-	unsigned long pgtable_cache_sz;
-	unsigned long pgd_cache_sz;
-} pgt_quicklists;
-
 void *srmmu_get_nocache(int size, int align);
 void srmmu_free_nocache(void *addr, int size);
-
-#define pgd_quicklist           (pgt_quicklists.pgd_cache)
-#define pmd_quicklist           ((unsigned long *)0)
-#define pte_quicklist           (pgt_quicklists.pte_cache)
-#define pgtable_cache_size      (pgt_quicklists.pgtable_cache_sz)
-#define pgd_cache_size		(pgt_quicklists.pgd_cache_sz)
 
 #define check_pgt_cache()	do { } while (0)
 
