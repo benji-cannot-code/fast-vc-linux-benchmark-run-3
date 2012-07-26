f151ca23a7fae85bac8f75ecaf8fab239ac82d0d
Avi Kivity <avi@redhat.com>
2012-07-26 18:01:50 +0300

    KVM: Remove internal timer abstraction
    kvm_timer_fn(), the sole inhabitant of timer.c, is only used by lapic.c. Move
    it there to make it easier to hack on it.
    
    struct kvm_timer is a thin wrapper around hrtimer, and only adds obfuscation.
    Move near its two users (with different names) to prepare for simplification.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- arch/x86/kvm/lapic.h
+++ arch/x86/kvm/lapic.h
@@ -3,9 +3,24 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __KVM_X86_LAPIC_H
 
 #include "iodev.h"
-#include "kvm_timer.h"
 
 #include <linux/kvm_host.h>
+
+struct kvm_timer {
+	struct hrtimer timer;
+	s64 period; 				/* unit: ns */
+	u32 timer_mode_mask;
+	u64 tscdeadline;
+	atomic_t pending;			/* accumulated triggered timers */
+	bool reinject;
+	struct kvm_timer_ops *t_ops;
+	struct kvm *kvm;
+	struct kvm_vcpu *vcpu;
+};
+
+struct kvm_timer_ops {
+	bool (*is_periodic)(struct kvm_timer *);
+};
 
 struct kvm_lapic {
 	unsigned long base_address;
