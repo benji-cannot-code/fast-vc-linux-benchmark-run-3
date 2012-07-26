5e93854c6e0ad3cfec1d87d3feb804ccffb4f974
Sam Ravnborg <sam@ravnborg.org>
2012-07-26 11:02:24 +0000

    sparc32: centralize all mmu context handling in srmmu.c
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/fault_32.c
+++ arch/sparc/mm/fault_32.c
@@ -33,12 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int show_unhandled_signals = 1;
 
-/* At boot time we determine these two values necessary for setting
- * up the segment maps and page table entries (pte's).
- */
-
-int num_contexts;
-
 /* Return how much physical memory we have.  */
 unsigned long probe_memory(void)
 {
