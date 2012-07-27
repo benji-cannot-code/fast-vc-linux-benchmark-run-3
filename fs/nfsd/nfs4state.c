7071a90092702ec2e18c0b6e4bcb5d316bdffea0
J. Bruce Fields <bfields@redhat.com>
2012-07-27 16:30:12 -0400

    nfsd4: fix missing fault_inject.h include
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xdr4.h"
 #include "vfs.h"
 #include "current_stateid.h"
+#include "fault_inject.h"
 
 #define NFSDDBG_FACILITY                NFSDDBG_PROC
 
