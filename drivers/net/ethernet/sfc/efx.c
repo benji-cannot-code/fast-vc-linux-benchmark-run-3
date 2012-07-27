1459a332b54be41551ebb710ad75038a95adf386
Ben Hutchings <bhutchings@solarflare.com>
2012-07-27 19:35:39 +0100

    sfc: Hold the RTNL lock for more of the suspend/resume cycle
    I don't think these PM functions can race with userland net device
    operations, but it's much easier to reason about locking if state is
    consistently guarded by the same lock.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>

--- drivers/net/ethernet/sfc/efx.c
+++ drivers/net/ethernet/sfc/efx.c
@@ -2728,6 +2728,8 @@ static int efx_pm_freeze(struct device *dev)
 {
 	struct efx_nic *efx = pci_get_drvdata(to_pci_dev(dev));
 
+	rtnl_lock();
+
 	efx->state = STATE_UNINIT;
 
 	netif_device_detach(efx->net_dev);
@@ -2735,12 +2737,16 @@ static int efx_pm_freeze(struct device *dev)
 	efx_stop_all(efx);
 	efx_stop_interrupts(efx, false);
 
+	rtnl_unlock();
+
 	return 0;
 }
 
 static int efx_pm_thaw(struct device *dev)
 {
 	struct efx_nic *efx = pci_get_drvdata(to_pci_dev(dev));
+
+	rtnl_lock();
 
 	efx_start_interrupts(efx, false);
 
@@ -2755,6 +2761,8 @@ static int efx_pm_thaw(struct device *dev)
 	efx->state = STATE_READY;
 
 	efx->type->resume_wol(efx);
+
+	rtnl_unlock();
 
 	/* Reschedule any quenched resets scheduled during efx_pm_freeze() */
 	queue_work(reset_workqueue, &efx->reset_work);
