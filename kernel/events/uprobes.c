aeb73a54a2967784c3f6e36ee7580bd39a25d989
Oleg Nesterov <oleg@redhat.com>
2012-07-29 20:22:18 +0200

    uprobes: Kill write_opcode()->lock_page(new_page)
    write_opcode() does lock_page(new_page) for no reason. Nobody
    can see this page until __replace_page() exposes it under ptl
    lock, and we do nothing with this page after pte_unmap_unlock().
    
    If nothing else, the similar code in do_wp_page() doesn't lock
    the new page for page_add_new_anon_rmap/set_pte_at_notify.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Srikar Dronamraju <srikar.vnet.ibm.com>
    Cc: Anton Arapov <anton@redhat.com>
    Cc: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/20120729182218.GA20315@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -240,9 +240,7 @@ retry:
 	if (ret)
 		goto unlock_out;
 
-	lock_page(new_page);
 	ret = __replace_page(vma, vaddr, old_page, new_page);
-	unlock_page(new_page);
 
 unlock_out:
 	unlock_page(old_page);
