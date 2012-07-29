0a4e5d631d4b58ee72629fc26056a78ecdcd543f
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2012-07-29 16:47:40 +0000

    da8xx-fb: fix compile issue due to missing include
    Signed-off-by: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>

--- drivers/video/da8xx-fb.c
+++ drivers/video/da8xx-fb.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #include <linux/spinlock.h>
 #include <linux/slab.h>
+#include <linux/delay.h>
 #include <linux/lcm.h>
 #include <video/da8xx-fb.h>
 #include <asm/div64.h>
