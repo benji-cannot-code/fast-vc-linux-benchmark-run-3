2fdb1c5df5d28ff5069cdf31ce3f8a45cfd40e07
Alexander Graf <agraf@suse.de>
2012-07-31 00:19:50 +0200

    KVM: PPC: E500: Implement MMU notifiers
    The e500 target has lived without mmu notifiers ever since it got
    introduced, but fails for the user space check on them with hugetlbfs.
    
    So in order to get that one working, implement mmu notifiers in a
    reasonably dumb fashion and be happy. On embedded hardware, we almost
    never end up with mmu notifier calls, since most people don't overcommit.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/include/asm/kvm_host.h
+++ arch/powerpc/include/asm/kvm_host.h
@@ -47,7 +47,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KVM_COALESCED_MMIO_PAGE_OFFSET 1
 #endif
 
-#ifdef CONFIG_KVM_BOOK3S_64_HV
+#if defined(CONFIG_KVM_BOOK3S_64_HV) || defined(CONFIG_KVM_E500V2) || \
+    defined(CONFIG_KVM_E500MC)
 #include <linux/mmu_notifier.h>
 
 #define KVM_ARCH_WANT_MMU_NOTIFIER
