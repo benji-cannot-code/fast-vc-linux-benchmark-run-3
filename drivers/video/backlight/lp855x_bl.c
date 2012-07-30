b250a26170776fce5abb0a70bbb0bcf2adef03f7
Kim, Milo <Milo.Kim@ti.com>
2012-07-30 14:40:50 -0700

    backlight: move register definitions from header to source
    ROM boundary definitions do not need to be exported because these are
    used only internally in the lp855x driver.
    
    And few code cosmetic changes
    
    Signed-off-by: Milo(Woogyom) Kim <milo.kim@ti.com>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Cc: Bryan Wu <bryan.wu@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/backlight/lp855x_bl.c
+++ drivers/video/backlight/lp855x_bl.c
@@ -18,8 +18,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/lp855x.h>
 
 /* Registers */
-#define BRIGHTNESS_CTRL		(0x00)
-#define DEVICE_CTRL		(0x01)
+#define BRIGHTNESS_CTRL		0x00
+#define DEVICE_CTRL		0x01
+#define EEPROM_START		0xA0
+#define EEPROM_END		0xA7
+#define EPROM_START		0xA0
+#define EPROM_END		0xAF
 
 #define BUF_SIZE		20
 #define DEFAULT_BL_NAME		"lcd-backlight"
