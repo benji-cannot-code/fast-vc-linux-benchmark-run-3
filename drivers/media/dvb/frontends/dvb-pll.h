23163f98416b0cc81beb1d5d90c0bc600504760c
Patrice Chotard <patrice.chotard@sfr.fr>
2012-07-31 15:31:20 -0300

    [media] dvb: add support for Thomson DTT7520X
    Add support for Thomson DTT7520X pll needed by
    ngene card Terratec Cynergy 2400i DT
    
    Signed-off-by: Patrice Chotard <patricechotard@free.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb/frontends/dvb-pll.h
+++ drivers/media/dvb/frontends/dvb-pll.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_SAMSUNG_TBDU18132      16
 #define DVB_PLL_SAMSUNG_TBMU24112      17
 #define DVB_PLL_TDEE4		       18
+#define DVB_PLL_THOMSON_DTT7520X       19
 
 /**
  * Attach a dvb-pll to the supplied frontend structure.
