c2472ab837c7c117f95931fd86525dbe170844b3
Takashi Iwai <tiwai@suse.de>
2012-07-31 10:16:59 +0200

    ALSA: hda - Fix polarity of mute LED on HP Mini 210
    The commit a3e199732b made the LED working again on HP Mini 210 but
    with a wrong polarity.  This patch fixes the polarity for this
    machine, and also introduce a new model string "hp-inv-led".
    
    Bugzilla: https://bugzilla.novell.com/show_bug.cgi?id=772923
    
    Cc: <stable@vger.kernel.org> [v3.3+]
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_sigmatel.c
+++ sound/pci/hda/patch_sigmatel.c
@@ -103,6 +103,7 @@ enum {
 	STAC_HP_DV7_4000,
 	STAC_HP_ZEPHYR,
 	STAC_92HD83XXX_HP_LED,
+	STAC_92HD83XXX_HP_INV_LED,
 	STAC_92HD83XXX_MODELS
 };
 
@@ -1678,6 +1679,7 @@ static const char * const stac92hd83xxx_models[STAC_92HD83XXX_MODELS] = {
 	[STAC_HP_DV7_4000] = "hp-dv7-4000",
 	[STAC_HP_ZEPHYR] = "hp-zephyr",
 	[STAC_92HD83XXX_HP_LED] = "hp-led",
+	[STAC_92HD83XXX_HP_INV_LED] = "hp-inv-led",
 };
 
 static const struct snd_pci_quirk stac92hd83xxx_cfg_tbl[] = {
@@ -5562,6 +5564,9 @@ again:
 		spec->init = stac92hd83xxx_hp_zephyr_init;
 		break;
 	case STAC_92HD83XXX_HP_LED:
+		default_polarity = 0;
+		break;
+	case STAC_92HD83XXX_HP_INV_LED:
 		default_polarity = 1;
 		break;
 	}
