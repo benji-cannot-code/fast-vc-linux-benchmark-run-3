2a2fdb7c2dfd1ec048b433f427e314200ec71f94
Michal Simek <monstr@monstr.eu>
2012-08-01 10:29:28 +0200

    microblaze: Support 4k/16k/64k pages
    Add support for page size which is supported by MMU.
    Remove 8k and 32k page size because they are not supported
    by MMU.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/include/asm/page.h
+++ arch/microblaze/include/asm/page.h
@@ -24,12 +24,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 
 /* PAGE_SHIFT determines the page size */
-#if defined(CONFIG_MICROBLAZE_32K_PAGES)
-#define PAGE_SHIFT		15
+#if defined(CONFIG_MICROBLAZE_64K_PAGES)
+#define PAGE_SHIFT		16
 #elif defined(CONFIG_MICROBLAZE_16K_PAGES)
 #define PAGE_SHIFT		14
-#elif defined(CONFIG_MICROBLAZE_8K_PAGES)
-#define PAGE_SHIFT		13
 #else
 #define PAGE_SHIFT		12
 #endif
