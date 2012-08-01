e9d835e6478c1e1d32c744a565d554e000e8272e
Alexander Shiyan <shc_work@mail.ru>
2012-08-01 12:00:20 +0400

    serial: sc26xx: Fix compile breakage
    This patch fixes the following compile breakage:
    
      CC      drivers/tty/serial/sc26xx.o
    drivers/tty/serial/sc26xx.c: In function 'read_sc_port':
    drivers/tty/serial/sc26xx.c:100: error: implicit declaration of function 'readb'
    drivers/tty/serial/sc26xx.c: In function 'write_sc_port':
    drivers/tty/serial/sc26xx.c:105: error: implicit declaration of function 'writeb'
    drivers/tty/serial/sc26xx.c: In function 'sc26xx_probe':
    drivers/tty/serial/sc26xx.c:652: error: implicit declaration of function 'ioremap_nocache'
    drivers/tty/serial/sc26xx.c:652: warning: assignment makes pointer from integer without a cast
    make[3]: *** [drivers/tty/serial/sc26xx.o] Error 1
    make[2]: *** [drivers/tty/serial] Error 2
    make[1]: *** [drivers/tty] Error 2
    make: *** [drivers] Error 2
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/sc26xx.c
+++ drivers/tty/serial/sc26xx.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/irq.h>
+#include <linux/io.h>
 
 #if defined(CONFIG_MAGIC_SYSRQ)
 #define SUPPORT_SYSRQ
