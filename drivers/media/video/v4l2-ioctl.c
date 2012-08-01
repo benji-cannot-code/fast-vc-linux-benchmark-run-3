54c2428f00fc402cd1c2f3e3b5199555af7fb455
Hans Verkuil <hverkuil@xs4all.nl>
2012-08-01 15:52:46 -0300

    [media] VIDIOC_ENUM_FREQ_BANDS fix
    When VIDIOC_ENUM_FREQ_BANDS is called for a driver that doesn't supply an
    enum_freq_bands op, then it will fall back to reporting a single freq band
    based on information from g_tuner or g_modulator.
    
    Due to a bug this is an infinite list since the index field wasn't tested.
    
    This patch fixes this and returns -EINVAL if index != 0.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/v4l2-ioctl.c
+++ drivers/media/video/v4l2-ioctl.c
@@ -1854,6 +1854,8 @@ static int v4l_enum_freq_bands(const struct v4l2_ioctl_ops *ops,
 			.type = type,
 		};
 
+		if (p->index)
+			return -EINVAL;
 		err = ops->vidioc_g_tuner(file, fh, &t);
 		if (err)
 			return err;
@@ -1870,6 +1872,8 @@ static int v4l_enum_freq_bands(const struct v4l2_ioctl_ops *ops,
 		};
 
 		if (type != V4L2_TUNER_RADIO)
+			return -EINVAL;
+		if (p->index)
 			return -EINVAL;
 		err = ops->vidioc_g_modulator(file, fh, &m);
 		if (err)
