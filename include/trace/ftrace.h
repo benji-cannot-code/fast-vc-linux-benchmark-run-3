7119fe1e416dec0592a0778f566a7384bb2cfac0
Steven Rostedt <srostedt@redhat.com>
2012-08-02 10:32:10 -0400

    tracing: Pass the ftrace_file to the buffer lock reserve code
    Pass the struct ftrace_event_file *ftrace_file to the
    trace_event_buffer_lock_reserve() (new function that replaces the
    trace_current_buffer_lock_reserver()).
    
    The ftrace_file holds a pointer to the trace_array that is in use.
    In the case of multiple buffers with different trace_arrays, this
    allows different events to be recorded into different buffers.
    
    Also fixed some of the stale comments in include/trace/ftrace.h
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/trace/ftrace.h
+++ include/trace/ftrace.h
@@ -415,7 +415,8 @@ static inline notrace int ftrace_get_offsets_##call(			\
  *
  * static void ftrace_raw_event_<call>(void *__data, proto)
  * {
- *	struct ftrace_event_call *event_call = __data;
+ *	struct ftrace_event_file *ftrace_file = __data;
+ *	struct ftrace_event_call *event_call = ftrace_file->event_call;
  *	struct ftrace_data_offsets_<call> __maybe_unused __data_offsets;
  *	struct ring_buffer_event *event;
  *	struct ftrace_raw_<call> *entry; <-- defined in stage 1
@@ -429,7 +430,7 @@ static inline notrace int ftrace_get_offsets_##call(			\
  *
  *	__data_size = ftrace_get_offsets_<call>(&__data_offsets, args);
  *
- *	event = trace_current_buffer_lock_reserve(&buffer,
+ *	event = trace_event_buffer_lock_reserve(&buffer, ftrace_file,
  *				  event_<call>->event.type,
  *				  sizeof(*entry) + __data_size,
  *				  irq_flags, pc);
@@ -441,7 +442,7 @@ static inline notrace int ftrace_get_offsets_##call(			\
  *			   __array macros.
  *
  *	if (!filter_current_check_discard(buffer, event_call, entry, event))
- *		trace_current_buffer_unlock_commit(buffer,
+ *		trace_nowake_buffer_unlock_commit(buffer,
  *						   event, irq_flags, pc);
  * }
  *
@@ -534,7 +535,7 @@ ftrace_raw_event_##call(void *__data, proto)				\
 									\
 	__data_size = ftrace_get_offsets_##call(&__data_offsets, args); \
 									\
-	event = trace_current_buffer_lock_reserve(&buffer,		\
+	event = trace_event_buffer_lock_reserve(&buffer, ftrace_file,	\
 				 event_call->event.type,		\
 				 sizeof(*entry) + __data_size,		\
 				 irq_flags, pc);			\
