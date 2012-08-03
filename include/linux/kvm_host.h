aa4a0742956b9565d4ebdacc8c71758cd6868acd
Xiao Guangrong <xiaoguangrong@linux.vnet.ibm.com>
2012-08-03 15:39:19 +0800

    KVM: introduce KVM_PFN_ERR_BAD
    Then, remove get_bad_pfn
    
    Signed-off-by: Xiao Guangrong <xiaoguangrong@linux.vnet.ibm.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define KVM_PFN_ERR_FAULT	(-EFAULT)
 #define KVM_PFN_ERR_HWPOISON	(-EHWPOISON)
+#define KVM_PFN_ERR_BAD		(-ENOENT)
 
 /*
  * vcpu->requests bit members
