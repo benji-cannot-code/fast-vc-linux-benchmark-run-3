2b1b4f41262c58a8d9e9e546b39fc1b0fb27f3b0
Tony Prisk <linux@prisktech.co.nz>
2012-08-03 21:00:06 +1200

    arm: vt8500: Update arch-vt8500 to devicetree support.
    Merged existing board files to a single dt-capable file.
    Converted irq and timer code to devicetree.
    Removed existing device files that are no longer required with
    devicetree support.
    All existing platform devices are converted to devicetree nodes
    except PWM.
    
    Removed restart.c and moved code into vt8500.c to remove
    duplicate PMC code.
    
    Signed-off-by: Tony Prisk <linux@prisktech.co.nz>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-vt8500/common.h
+++ arch/arm/mach-vt8500/common.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/mach-vt8500/restart.h
+/* linux/arch/arm/mach-vt8500/dt_common.h
  *
  * Copyright (C) 2012 Tony Prisk <linux@prisktech.co.nz>
  *
@@ -14,5 +14,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-void vt8500_setup_restart(void);
-void vt8500_restart(char mode, const char *cmd);
+#ifndef __ARCH_ARM_MACH_VT8500_DT_COMMON_H
+#define __ARCH_ARM_MACH_VT8500_DT_COMMON_H
+
+#include <linux/of.h>
+
+void __init vt8500_timer_init(void);
+int __init vt8500_irq_init(struct device_node *node,
+				struct device_node *parent);
+
+/* defined in drivers/clk/clk-vt8500.c */
+void __init vtwm_clk_init(void __iomem *pmc_base);
+
+#endif
