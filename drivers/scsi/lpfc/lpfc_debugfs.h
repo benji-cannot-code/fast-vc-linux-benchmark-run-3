97b94e31bae0d6a9fccf7ac6c3e76e36cea50670
James Smart <james.smart@emulex.com>
2012-08-03 12:35:13 -0400

    [SCSI] lpfc 8.3.33: Add debugfs interface to display SLI queue information
    Signed-off-by: James Smart <james.smart@emulex.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/lpfc/lpfc_debugfs.h
+++ drivers/scsi/lpfc/lpfc_debugfs.h
@@ -37,6 +37,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* dumpHostSlim output buffer size */
 #define LPFC_DUMPHOSTSLIM_SIZE 4096
 
+/* dumpSLIqinfo output buffer size */
+#define	LPFC_DUMPSLIQINFO_SIZE 4096
+
 /* hbqinfo output buffer size */
 #define LPFC_HBQINFO_SIZE 8192
 
