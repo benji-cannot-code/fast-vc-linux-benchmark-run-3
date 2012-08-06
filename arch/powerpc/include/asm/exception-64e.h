7579780ea54e0c66e1c8e61be48cdfcd8f6a0dd1
Mihai Caraman <mihai.caraman@freescale.com>
2012-08-06 03:27:04 +0000

    powerpc/booke64: Use GSRR registers in Guest Doorbell interrupts
    Guest Doorbell interrupts use guest save and restore registers. Add a new
    Guest Doorbell exception type to accommodate GSRR0/1 SPRs usage in exception
    prolog and fix the exception handler.
    
    Signed-off-by: Mihai Caraman <mihai.caraman@freescale.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/exception-64e.h
+++ arch/powerpc/include/asm/exception-64e.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * critical data
  */
 
+#define PACA_EXGDBELL PACA_EXGEN
 
 /* We are out of SPRGs so we save some things in the PACA. The normal
  * exception frame is smaller than the CRIT or MC one though
