5d121b43608101bec9f0461db0ddf66b3d0b5bd8
Prashant Gaikwad <pgaikwad@nvidia.com>
2012-08-06 11:57:43 +0530

    ARM: tegra: Port tegra to generic clock framework
    This patch converts tegra clock code to generic clock framework in following way:
     - Implement clk_ops as required by generic clk framework. (tegraXX_clocks.c)
     - Use platform specific struct clk_tegra in clk_ops implementation instead of struct clk.
     - Initialize all clock data statically. (tegraXX_clocks_data.c)
    
    Legacy framework did not have recalc_rate and is_enabled functions. Implemented these functions.
    Removed init function. It's functionality is splitted into recalc_rate and is_enabled.
    
    Static initialization is used since slab is not up in .init_early and clock
    is needed to be initialized before clockevent/clocksource initialization.
    Macros redefined for clk_tegra.
    
    Also, single struct clk_tegra is used for all type of clocks (PLL, peripheral etc.). This
    is to move quickly to generic common clock framework so that other dependent features will
    not be blocked (such as DT binding).
    
    Enabling COMMON_CLOCK config moved to ARCH_TEGRA since it is enabled for both Tegra20
    and Tegra30.
    
    Signed-off-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/tegra20_clocks.h
+++ arch/arm/mach-tegra/tegra20_clocks.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MACH_TEGRA20_CLOCK_H
 #define __MACH_TEGRA20_CLOCK_H
 
+extern struct clk_ops tegra_clk_32k_ops;
 extern struct clk_ops tegra_pll_ops;
 extern struct clk_ops tegra_clk_m_ops;
 extern struct clk_ops tegra_pll_div_ops;
@@ -34,5 +35,8 @@ extern struct clk_ops tegra_blink_clk_ops;
 extern struct clk_ops tegra_emc_clk_ops;
 extern struct clk_ops tegra_periph_clk_ops;
 extern struct clk_ops tegra_clk_shared_bus_ops;
+
+void tegra2_periph_clk_reset(struct clk_hw *hw, bool assert);
+void tegra2_cop_clk_reset(struct clk_hw *hw, bool assert);
 
 #endif
--- arch/arm/mach-tegra/tegra30_clocks.h
+++ arch/arm/mach-tegra/tegra30_clocks.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MACH_TEGRA30_CLOCK_H
 #define __MACH_TEGRA30_CLOCK_H
 
+extern struct clk_ops tegra_clk_32k_ops;
 extern struct clk_ops tegra30_clk_m_ops;
 extern struct clk_ops tegra_clk_m_div_ops;
 extern struct clk_ops tegra_pll_ref_ops;
@@ -28,17 +29,26 @@ extern struct clk_ops tegra30_plle_ops;
 extern struct clk_ops tegra_cml_clk_ops;
 extern struct clk_ops tegra_pciex_clk_ops;
 extern struct clk_ops tegra_sync_source_ops;
-extern struct clk_ops tegra30_audio_sync_clk_ops;
+extern struct clk_ops tegra_audio_sync_clk_ops;
 extern struct clk_ops tegra30_clk_double_ops;
 extern struct clk_ops tegra_clk_out_ops;
 extern struct clk_ops tegra30_super_ops;
 extern struct clk_ops tegra30_blink_clk_ops;
 extern struct clk_ops tegra30_twd_ops;
 extern struct clk_ops tegra30_periph_clk_ops;
-extern struct clk_ops tegra_dsib_clk_ops;
+extern struct clk_ops tegra30_dsib_clk_ops;
 extern struct clk_ops tegra_nand_clk_ops;
 extern struct clk_ops tegra_vi_clk_ops;
 extern struct clk_ops tegra_dtv_clk_ops;
 extern struct clk_ops tegra_clk_shared_bus_ops;
 
+int tegra30_plld_clk_cfg_ex(struct clk_hw *hw,
+				enum tegra_clk_ex_param p, u32 setting);
+void tegra30_periph_clk_reset(struct clk_hw *hw, bool assert);
+int tegra30_vi_clk_cfg_ex(struct clk_hw *hw,
+				enum tegra_clk_ex_param p, u32 setting);
+int tegra30_nand_clk_cfg_ex(struct clk_hw *hw,
+				enum tegra_clk_ex_param p, u32 setting);
+int tegra30_dtv_clk_cfg_ex(struct clk_hw *hw,
+				enum tegra_clk_ex_param p, u32 setting);
 #endif
