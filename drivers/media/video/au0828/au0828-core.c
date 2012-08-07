5f5c450e0c4f1aea231686db6dc93ca714845a54
Devin Heitmueller <dheitmueller@kernellabs.com>
2012-08-06 22:46:58 -0300

    [media] au0828: fix race condition that causes xc5000 to not bind for digital
    In some cases users would see the xc5000_attach() call failing for the
    digital side of the tuner on initialization.  This is because of udev
    running v4l-id while the digital side of the board is still coming up.
    
    This is the exact same race condition which was present in em28xx (not
    surprising since I copied all the locking logic from that driver when I
    added analog support).  Reproduce Mauro's fix from the em28xx driver in
    au0828.
    
    Reported-by: Rick Harding <rharding@mitechie.com>
    Signed-off-by: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/au0828/au0828-core.c
+++ drivers/media/video/au0828/au0828-core.c
@@ -206,6 +206,8 @@ static int au0828_usb_probe(struct usb_interface *interface,
 		return -ENOMEM;
 	}
 
+	mutex_init(&dev->lock);
+	mutex_lock(&dev->lock);
 	mutex_init(&dev->mutex);
 	mutex_init(&dev->dvb.lock);
 	dev->usbdev = usbdev;
@@ -216,6 +218,7 @@ static int au0828_usb_probe(struct usb_interface *interface,
 	if (retval) {
 		printk(KERN_ERR "%s() v4l2_device_register failed\n",
 		       __func__);
+		mutex_unlock(&dev->lock);
 		kfree(dev);
 		return -EIO;
 	}
@@ -245,6 +248,8 @@ static int au0828_usb_probe(struct usb_interface *interface,
 
 	printk(KERN_INFO "Registered device AU0828 [%s]\n",
 		dev->board.name == NULL ? "Unset" : dev->board.name);
+
+	mutex_unlock(&dev->lock);
 
 	return 0;
 }
