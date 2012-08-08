ad7118d5fd0ea320717d940c84235e6898ed5520
Archit Taneja <archit@ti.com>
2012-08-08 16:56:06 +0530

    OMAPDSS: SDI: Maintain our own timings field in driver data
    The SDI driver currently relies on the timings in omap_dss_device struct to
    configure the DISPC accordingly. This makes the SDI interface driver dependent
    on the omap_dss_device struct.
    
    Make the SDI driver data maintain it's own timings field. The panel driver is
    expected to call omapdss_sdi_set_timings() to set these timings before the panel
    is enabled.
    
    Make the SDI panel driver configure the new timings is the omap_dss_device
    struct(dssdev->panel.timings). The SDI driver is responsible for maintaining
    only it's own copy of timings.
    
    Signed-off-by: Archit Taneja <archit@ti.com>

--- drivers/video/omap2/displays/panel-acx565akm.c
+++ drivers/video/omap2/displays/panel-acx565akm.c
@@ -601,6 +601,8 @@ static int acx_panel_power_on(struct omap_dss_device *dssdev)
 
 	mutex_lock(&md->mutex);
 
+	omapdss_sdi_set_timings(dssdev, &dssdev->panel.timings);
+
 	r = omapdss_sdi_display_enable(dssdev);
 	if (r) {
 		pr_err("%s sdi enable failed\n", __func__);
@@ -733,6 +735,8 @@ static void acx_panel_set_timings(struct omap_dss_device *dssdev,
 		struct omap_video_timings *timings)
 {
 	omapdss_sdi_set_timings(dssdev, timings);
+
+	dssdev->panel.timings = *timings;
 }
 
 static int acx_panel_check_timings(struct omap_dss_device *dssdev,
