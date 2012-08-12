25abb3ee5a3a366c266c6f9a0ce020e3fbb84eda
Shawn Guo <shawn.guo@linaro.org>
2012-08-12 22:33:43 +0800

    ARM: imx53: build in pinctrl support
    As all imx53 boards booting from device tree have pinctrl set up in dts,
    it's time to remove the dummy pinctrl state and build in the real imx53
    pinctrl support.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/imx53-dt.c
+++ arch/arm/mach-imx/imx53-dt.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
-#include <linux/pinctrl/machine.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/time.h>
 #include <mach/common.h>
@@ -78,8 +77,6 @@ static void __init imx53_dt_init(void)
 	struct device_node *node;
 	const struct of_device_id *of_id;
 	void (*func)(void);
-
-	pinctrl_provide_dummies();
 
 	node = of_find_matching_node(NULL, imx53_iomuxc_of_match);
 	if (node) {
