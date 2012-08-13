118e6c130c94ba4628ebb7085961b9e76dc94a71
Linus Walleij <linus.walleij@linaro.org>
2012-08-13 13:49:45 +0200

    ARM: u300: convert to sparse IRQs
    This converts the U300 to use sparse IRQs, which is simple now
    that the number of machines are reduced.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-u300/core.c
+++ arch/arm/mach-u300/core.c
@@ -39,13 +39,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/memory.h>
 #include <asm/hardware/vic.h>
 #include <asm/mach/map.h>
-#include <asm/mach/irq.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 
 #include <mach/coh901318.h>
 #include <mach/hardware.h>
 #include <mach/syscon.h>
+#include <mach/irqs.h>
 
 #include "timer.h"
 #include "spi.h"
@@ -1808,6 +1808,7 @@ MACHINE_START(U300, "Ericsson AB U335 S335/B335 Prototype Board")
 	/* Maintainer: Linus Walleij <linus.walleij@stericsson.com> */
 	.atag_offset	= 0x100,
 	.map_io		= u300_map_io,
+	.nr_irqs	= NR_IRQS_U300,
 	.init_irq	= u300_init_irq,
 	.handle_irq	= vic_handle_irq,
 	.timer		= &u300_timer,
