bd24f26c12462213251887c56c080e59413f1895
Linus Walleij <linus.walleij@linaro.org>
2012-08-13 10:11:15 +0200

    ARM: u300: retire ancient platforms
    This retires the B26/S26, B330/S330 and B365/S365 boards from
    the U300 platform. The only board really used anywhere today
    is the S335 so let's concentrate any efforts on that one board.
    Also the board variants are selected at compile-time, which is
    strictly a no-no these days, if multi-board support is to be
    brought back it need to happen using run-time and device tree.
    
    My work on ARM Linux started on the B26/S26 so it's a bit sad
    to see it go, but there is a time to move on with all things
    in life.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-u300/include/mach/dma_channels.h
+++ arch/arm/mach-u300/include/mach/dma_channels.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * arch/arm/mach-u300/include/mach/dma_channels.h
  *
  *
- * Copyright (C) 2007-2009 ST-Ericsson
+ * Copyright (C) 2007-2012 ST-Ericsson
  * License terms: GNU General Public License (GPL) version 2
  * Map file for the U300 dma driver.
  * Author: Per Friden <per.friden@stericsson.com>
@@ -51,19 +51,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define U300_DMA_GENERAL_PURPOSE_6    35
 #define U300_DMA_GENERAL_PURPOSE_7    36
 #define U300_DMA_GENERAL_PURPOSE_8    37
-#ifdef CONFIG_MACH_U300_BS335
 #define U300_DMA_UART1_TX             38
 #define U300_DMA_UART1_RX             39
-#else
-#define U300_DMA_GENERAL_PURPOSE_9    38
-#define U300_DMA_GENERAL_PURPOSE_10   39
-#endif
 
-#ifdef CONFIG_MACH_U300_BS335
 #define U300_DMA_DEVICE_CHANNELS      32
-#else
-#define U300_DMA_DEVICE_CHANNELS      30
-#endif
 #define U300_DMA_CHANNELS             40
 
 
