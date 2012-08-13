4b094cddbbcc2bee2a3eddfd0a5b453b86f509cf
Pavel Emelyanov <xemul@parallels.com>
2012-08-13 05:53:28 +0000

    packet: Diag core and basic socket info dumping
    The diag module can be built independently from the af_packet.ko one,
    just like it's done in unix sockets.
    
    The core dumping message carries the info available at socket creation
    time, i.e. family, type and protocol (in the same byte order as shown in
    the proc file).
    
    The socket inode number and cookie is reserved for future per-socket info
    retrieving. The per-protocol filtering is also reserved for future by
    requiring the sdiag_protocol to be zero.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/linux/packet_diag.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __PACKET_DIAG_H__
+#define __PACKET_DIAG_H__
+
+#include <linux/types.h>
+
+struct packet_diag_req {
+	__u8	sdiag_family;
+	__u8	sdiag_protocol;
+	__u16	pad;
+	__u32	pdiag_ino;
+	__u32	pdiag_show;
+	__u32	pdiag_cookie[2];
+};
+
+struct packet_diag_msg {
+	__u8	pdiag_family;
+	__u8	pdiag_type;
+	__u16	pdiag_num;
+
+	__u32	pdiag_ino;
+	__u32	pdiag_cookie[2];
+};
+
+#endif
