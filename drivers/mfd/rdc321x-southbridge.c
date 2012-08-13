291f62801a8df9811050db1f225d76a7114a71f4
Masanari Iida <standby24x7@gmail.com>
2012-08-13 21:00:25 +0900

    mfd: printk/comment fixes
    Correct spelling typo in drivers/mfd
    
    Signed-off-by: Masanari Iida <standby24x7@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/mfd/rdc321x-southbridge.c
+++ drivers/mfd/rdc321x-southbridge.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * RDC321x MFD southbrige driver
+ * RDC321x MFD southbridge driver
  *
  * Copyright (C) 2007-2010 Florian Fainelli <florian@openwrt.org>
  * Copyright (C) 2010 Bernhard Loos <bernhardloos@googlemail.com>
