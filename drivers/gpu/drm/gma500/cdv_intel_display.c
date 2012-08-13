2c630787dccb141471522b8a40677a93d4bc5bad
Forest Bond <forest.bond@rapidrollout.com>
2012-08-13 16:29:04 +0000

    gma500: psb_intel_crtc: Drop crtc_enable flag.
    This is set when setting DPMS on and off, but it isn't checked anywhere,
    so just remove it.
    
    Signed-off-by: Forest Bond <forest.bond@rapidrollout.com>
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/gma500/cdv_intel_display.c
+++ drivers/gpu/drm/gma500/cdv_intel_display.c
@@ -931,7 +931,6 @@ static void cdv_intel_crtc_dpms(struct drm_crtc *crtc, int mode)
 		/* Give the overlay scaler a chance to enable
 		 * if it's on this pipe */
 		/* psb_intel_crtc_dpms_video(crtc, true); TODO */
-		psb_intel_crtc->crtc_enable = true;
 		break;
 	case DRM_MODE_DPMS_OFF:
 		if (!psb_intel_crtc->active)
@@ -982,7 +981,6 @@ static void cdv_intel_crtc_dpms(struct drm_crtc *crtc, int mode)
 
 		/* Wait for the clocks to turn off. */
 		udelay(150);
-		psb_intel_crtc->crtc_enable = false;
 		break;
 	}
 	cdv_intel_update_watermark(dev, crtc);
