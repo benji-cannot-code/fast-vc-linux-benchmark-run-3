7954003821e10a9a51246c0be691221332fb18c5
Lad, Prabhakar <prabhakar.lad@ti.com>
2012-08-14 01:23:09 -0300

    [media] media: davinci: fix section mismatch warnings
    This patch fixes section mismatch warnings for
    davinci video drivers.
    
    [mchehab@redhat.com: applied it as a fixup over the previous (and wrong) changeset c6afbf2]
    Signed-off-by: Lad, Prabhakar <prabhakar.lad@ti.com>
    Signed-off-by: Manjunath Hadli <manjunath.hadli@ti.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/video/davinci/vpif.c
+++ drivers/media/video/davinci/vpif.c
@@ -418,7 +418,7 @@ int vpif_channel_getfid(u8 channel_id)
 }
 EXPORT_SYMBOL(vpif_channel_getfid);
 
-static int __init vpif_probe(struct platform_device *pdev)
+static int __devinit vpif_probe(struct platform_device *pdev)
 {
 	int status = 0;
 
@@ -491,7 +491,7 @@ static const struct dev_pm_ops vpif_pm = {
 #define vpif_pm_ops NULL
 #endif
 
-static __refdata struct platform_driver vpif_driver = {
+static struct platform_driver vpif_driver = {
 	.driver = {
 		.name	= "vpif",
 		.owner = THIS_MODULE,
