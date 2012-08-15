cf253ae98218df6c2cfd0befb42dd7f1ebca52f9
H Hartley Sweeten <hartleys@visionengravers.com>
2012-08-14 18:25:59 -0700

    staging: comedi: dnya_pci10xx: remove unused DRV_NAME
    This define is not used in the driver. Remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/dyna_pci10xx.c
+++ drivers/staging/comedi/drivers/dyna_pci10xx.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 
 #define PCI_VENDOR_ID_DYNALOG		0x10b5
-#define DRV_NAME			"dyna_pci10xx"
 
 #define READ_TIMEOUT 50
 
