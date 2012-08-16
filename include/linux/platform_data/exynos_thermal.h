3f3b8a110ee191cc23aaa8292a9fea4499e62e24
Amit Daniel Kachhap <amit.kachhap@linaro.org>
2012-08-16 17:11:43 +0530

    thermal: exynos: register the tmu sensor with the kernel thermal layer
    This code added creates a link between temperature sensors, linux thermal
    framework and cooling devices for samsung exynos platform.  This layer
    monitors the temperature from the sensor and informs the generic thermal
    layer to take the necessary cooling action.
    
    [akpm@linux-foundation.org: fix comment layout]
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@linaro.org>
    Acked-by: Guenter Roeck <guenter.roeck@ericsson.com>
    Cc: SangWook Ju <sw.ju@samsung.com>
    Cc: Durgadoss <durgadoss.r@intel.com>
    Cc: Len Brown <lenb@kernel.org>
    Cc: Jean Delvare <khali@linux-fr.org>
    Cc: Kyungmin Park <kmpark@infradead.org>
    Cc: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Amit Daniel Kachhap <amit.daniel@samsung.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- include/linux/platform_data/exynos_thermal.h
+++ include/linux/platform_data/exynos_thermal.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef _LINUX_EXYNOS_THERMAL_H
 #define _LINUX_EXYNOS_THERMAL_H
+#include <linux/cpu_cooling.h>
 
 enum calibration_type {
 	TYPE_ONE_POINT_TRIMMING,
@@ -33,6 +34,22 @@ enum soc_type {
 	SOC_ARCH_EXYNOS4210 = 1,
 	SOC_ARCH_EXYNOS,
 };
+/**
+ * struct freq_clip_table
+ * @freq_clip_max: maximum frequency allowed for this cooling state.
+ * @temp_level: Temperature level at which the temperature clipping will
+ *	happen.
+ * @mask_val: cpumask of the allowed cpu's where the clipping will take place.
+ *
+ * This structure is required to be filled and passed to the
+ * cpufreq_cooling_unregister function.
+ */
+struct freq_clip_table {
+	unsigned int freq_clip_max;
+	unsigned int temp_level;
+	const struct cpumask *mask_val;
+};
+
 /**
  * struct exynos_tmu_platform_data
  * @threshold: basic temperature for generating interrupt
@@ -73,6 +90,9 @@ enum soc_type {
  * @type: determines the type of SOC
  * @efuse_value: platform defined fuse value
  * @cal_type: calibration type for temperature
+ * @freq_clip_table: Table representing frequency reduction percentage.
+ * @freq_tab_count: Count of the above table as frequency reduction may
+ *	applicable to only some of the trigger levels.
  *
  * This structure is required for configuration of exynos_tmu driver.
  */
@@ -91,5 +111,7 @@ struct exynos_tmu_platform_data {
 
 	enum calibration_type cal_type;
 	enum soc_type type;
+	struct freq_clip_table freq_tab[4];
+	unsigned int freq_tab_count;
 };
 #endif /* _LINUX_EXYNOS_THERMAL_H */
