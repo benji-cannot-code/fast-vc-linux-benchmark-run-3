35d33c27164cd28ce76e531fc76b88c21b24f638
Dave Airlie <airlied@redhat.com>
2012-08-16 10:15:34 +1000

    drm/i915: implement dma buf begin_cpu_access (v2)
    In order for udl vmap to work properly, we need to push the object
    into the CPU domain before we start copying the data to the USB device.
    
    This along with the udl change avoids userspace explicit mapping to
    be used.
    
    v2: add a flag for userspace to query to know if Intel kernel driver can
    deal with the vmap flushing properly. In theory udl would need a flag also,
    but I intend to push the patches very close to each other and other drivers
    should do the right thing from the start.
    
    I've added a test to my intel-gpu-tools prime branch, however testing
    this is a bit messy since the only way to get udl to vmap is to rendering
    something. I've tested this with real code as well to make sure it works.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    [danvet: resolved conflict, which required reallocating the PARAM
    number to 21.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1013,6 +1013,9 @@ static int i915_getparam(struct drm_device *dev, void *data,
 	case I915_PARAM_HAS_SEMAPHORES:
 		value = i915_semaphore_is_enabled(dev);
 		break;
+	case I915_PARAM_HAS_PRIME_VMAP_FLUSH:
+		value = 1;
+		break;
 	default:
 		DRM_DEBUG_DRIVER("Unknown parameter %d\n",
 				 param->param);
