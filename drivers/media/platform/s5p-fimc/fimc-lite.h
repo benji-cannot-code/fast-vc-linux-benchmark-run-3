d592f136fa508fd8cae808b04920768921c0d39a
Sachin Kamat <sachin.kamat@linaro.org>
2012-08-17 03:28:26 -0300

    [media] s5p-fimc: Replace asm/* headers with linux/*
    Silences the following warning:
    WARNING: Use #include <linux/sizes.h> instead of <asm/sizes.h>
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/s5p-fimc/fimc-lite.h
+++ drivers/media/platform/s5p-fimc/fimc-lite.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef FIMC_LITE_H_
 #define FIMC_LITE_H_
 
-#include <asm/sizes.h>
+#include <linux/sizes.h>
 #include <linux/io.h>
 #include <linux/irqreturn.h>
 #include <linux/platform_device.h>
