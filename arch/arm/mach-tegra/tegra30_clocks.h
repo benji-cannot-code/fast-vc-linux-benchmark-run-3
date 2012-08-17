7b9ab5773064ff78ca0d05316b31216677f4f998
Joseph Lo <josephl@nvidia.com>
2012-08-17 14:51:21 +0800

    ARM: tegra: clocks: separate tegra_clk_32k_ops from Tegra20 and Tegra30
    Currently the tegra20 and tegra30 share the same symbol for
    tegra_clk_32k_ops. This will cause a compile error when building
    a tegra20-only kernel image. Add tegra_clk_32k_ops for tegra20 and
    modify tegra30_clk_32k_ops for tegra30.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/tegra30_clocks.h
+++ arch/arm/mach-tegra/tegra30_clocks.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MACH_TEGRA30_CLOCK_H
 #define __MACH_TEGRA30_CLOCK_H
 
-extern struct clk_ops tegra_clk_32k_ops;
+extern struct clk_ops tegra30_clk_32k_ops;
 extern struct clk_ops tegra30_clk_m_ops;
 extern struct clk_ops tegra_clk_m_div_ops;
 extern struct clk_ops tegra_pll_ref_ops;
