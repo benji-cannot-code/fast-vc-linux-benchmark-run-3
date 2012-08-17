3c0aaeeb65c2e473a4f8d902ebc1d62de2f23237
Lars-Peter Clausen <lars@metafoo.de>
2012-08-17 16:57:00 +0100

    iio:trigger_consumer.h: Add missing includes and forward declarations
    Add includes and forward declarations for types used in this file. This avoids
    compile errors if the other files have not been included before.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- include/linux/iio/trigger_consumer.h
+++ include/linux/iio/trigger_consumer.h
@@ -11,6 +11,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_IIO_TRIGGER_CONSUMER_H__
 #define __LINUX_IIO_TRIGGER_CONSUMER_H__
 
+#include <linux/interrupt.h>
+#include <linux/types.h>
+
+struct iio_dev;
+struct iio_trigger;
+
 /**
  * struct iio_poll_func - poll function pair
  *
