4edcc25f418700d16e345b801bcd9693f0011964
Shaun Ruffell <sruffell@digium.com>
2012-08-19 01:11:24 -0300

    edac: edac_mc no longer deals with kobjects directly
    There are no more embedded kobjects in struct mem_ctl_info. Remove a header and
    a comment that does not reflect the code anymore.
    
    Signed-off-by: Shaun Ruffell <sruffell@digium.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/linux/edac.h
+++ include/linux/edac.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/atomic.h>
 #include <linux/device.h>
-#include <linux/kobject.h>
 #include <linux/completion.h>
 #include <linux/workqueue.h>
 #include <linux/debugfs.h>
