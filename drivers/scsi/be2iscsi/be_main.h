f0f52bffbc517686577b069f3d6626af4de362c2
John Soni Jose <sony.john-n@emulex.com>
2012-08-20 23:00:08 +0530

    [SCSI] be2iscsi: Issue MBX Cmd for login to boot target in crashdump mode
    When the driver comes up in crashdump mode, it has to explicitly
    issue command to FW for logging to the boot target. This fix issues
    MBX Cmd to login to boot target in crashdump mode.
    
    Signed-off-by: John Soni Jose <sony.john-n@emulex.com>
    Signed-off-by: Jayamohan Kallickal <jayamohan.kallickal@emulex.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/be2iscsi/be_main.h
+++ drivers/scsi/be2iscsi/be_main.h
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_CMD_SZ			65536
 #define IIOC_SCSI_DATA                  0x05	/* Write Operation */
 
+#define INVALID_SESS_HANDLE	0xFFFFFFFF
 #define DBG_LVL				0x00000001
 #define DBG_LVL_1			0x00000001
 #define DBG_LVL_2			0x00000002
