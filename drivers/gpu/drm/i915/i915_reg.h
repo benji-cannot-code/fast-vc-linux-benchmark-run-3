6b77d567aa27f5ae35b2b38c01b8697293ce7c05
Ben Widawsky <ben@bwidawsk.net>
2012-08-20 16:15:14 -0700

    drm/i915: Find unclaimed MMIO writes.
    ERR_INT on HSW will display unclaimed MMIO accesses. This can be either
    the result of a driver bug writing to an invalid addresses, or the
    result of RC6.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Tested-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Antti Koskipaa <antti.koskipaa@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -502,6 +502,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ERROR_GEN6	0x040a0
 #define GEN7_ERR_INT	0x44040
+#define   ERR_INT_MMIO_UNCLAIMED (1<<13)
 
 /* GM45+ chicken bits -- debug workaround bits that may be required
  * for various sorts of correct behavior.  The top 16 bits of each are
