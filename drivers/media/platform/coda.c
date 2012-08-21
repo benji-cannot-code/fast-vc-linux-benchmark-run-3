b85128c86411c9184952c66bd6f5e3b4818513fd
Richard Zhao <richard.zhao@freescale.com>
2012-08-21 02:47:42 -0300

    [media] media: coda: remove duplicated call of fh_to_ctx in vidioc_s_fmt_vid_out
    Signed-off-by: Richard Zhao <richard.zhao@freescale.com>
    Acked-by: Javier Martin <javier.martin@vista-silicon.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/coda.c
+++ drivers/media/platform/coda.c
@@ -502,7 +502,7 @@ static int vidioc_s_fmt_vid_out(struct file *file, void *priv,
 	if (ret)
 		return ret;
 
-	ret = vidioc_s_fmt(fh_to_ctx(priv), f);
+	ret = vidioc_s_fmt(ctx, f);
 	if (ret)
 		ctx->colorspace = f->fmt.pix.colorspace;
 
