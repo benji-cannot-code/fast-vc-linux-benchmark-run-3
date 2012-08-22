db1306742c459b83ac381bc08ed8288a5a83f5e8
Henrik Rydberg <rydberg@euromail.se>
2012-08-22 20:43:22 +0200

    Input: MT - Handle frame synchronization in core
    Most MT drivers perform the same actions on frame synchronization.
    Some actions, like dropping unseen contacts, are also unnecessarily
    complex. Collect common frame synchronization tasks in a new function,
    input_mt_sync_frame(). Depending on the flags set, it drops unseen
    contacts and performs pointer emulation.
    
    With init flags and frame synchronization in place, most MT drivers
    can be simplified. First out are the bcm5974 and hid-multitouch
    drivers, following this patch.
    
    Reviewed-and-tested-by: Benjamin Tissoires <benjamin.tissoires@enac.fr>
    Tested-by: Ping Cheng <pingc@wacom.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Henrik Rydberg <rydberg@euromail.se>

--- include/linux/input/mt.h
+++ include/linux/input/mt.h
@@ -16,12 +16,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define TRKID_MAX	0xffff
 
+#define INPUT_MT_POINTER	0x0001	/* pointer device, e.g. trackpad */
+#define INPUT_MT_DIRECT		0x0002	/* direct device, e.g. touchscreen */
+#define INPUT_MT_DROP_UNUSED	0x0004	/* drop contacts not seen in frame */
 /**
  * struct input_mt_slot - represents the state of an input MT slot
  * @abs: holds current values of ABS_MT axes for this slot
+ * @frame: last frame at which input_mt_report_slot_state() was called
  */
 struct input_mt_slot {
 	int abs[ABS_MT_LAST - ABS_MT_FIRST + 1];
+	unsigned int frame;
 };
 
 /**
@@ -30,6 +35,7 @@ struct input_mt_slot {
  * @num_slots: number of MT slots the device uses
  * @slot: MT slot currently being transmitted
  * @flags: input_mt operation flags
+ * @frame: increases every time input_mt_sync_frame() is called
  * @slots: array of slots holding current values of tracked contacts
  */
 struct input_mt {
@@ -37,6 +43,7 @@ struct input_mt {
 	int num_slots;
 	int slot;
 	unsigned int flags;
+	unsigned int frame;
 	struct input_mt_slot slots[];
 };
 
@@ -81,5 +88,7 @@ void input_mt_report_slot_state(struct input_dev *dev,
 
 void input_mt_report_finger_count(struct input_dev *dev, int count);
 void input_mt_report_pointer_emulation(struct input_dev *dev, bool use_count);
+
+void input_mt_sync_frame(struct input_dev *dev);
 
 #endif
