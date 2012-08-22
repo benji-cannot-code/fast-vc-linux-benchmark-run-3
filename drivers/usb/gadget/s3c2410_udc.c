449952e1e47adb9807fc5747fd39f6d7e3363215
Sachin Kamat <sachin.kamat@linaro.org>
2012-08-22 15:48:58 +0530

    usb: gadget: s3c2410_udc: Replace <asm/io.h> with <linux/io.h>
    Fixes the following warning:
    WARNING: Use #include <linux/io.h> instead of <asm/io.h>
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/s3c2410_udc.c
+++ drivers/usb/gadget/s3c2410_udc.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/gpio.h>
 #include <linux/prefetch.h>
+#include <linux/io.h>
 
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
@@ -36,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb/gadget.h>
 
 #include <asm/byteorder.h>
-#include <asm/io.h>
 #include <asm/irq.h>
 #include <asm/unaligned.h>
 #include <mach/irqs.h>
