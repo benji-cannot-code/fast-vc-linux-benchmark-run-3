06271fe1632a91b36ed14339dcf40624671dc6a3
Antti Palosaari <crope@iki.fi>
2012-08-22 19:42:00 -0300

    [media] dvb_usb_v2: use dvb_usb_dbg_usb_control_msg()
    Convert drivers: au6610, ce6230, ec168, rtl28xxu for
    dvb_usb_dbg_usb_control_msg() macro.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/dvb-usb-v2/ce6230.h
+++ drivers/media/usb/dvb-usb-v2/ce6230.h
@@ -27,17 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "zl10353.h"
 #include "mxl5005s.h"
 
-#define ce6230_debug_dump(r, t, v, i, b, l) { \
-	char *direction; \
-	if (t == (USB_TYPE_VENDOR | USB_DIR_OUT)) \
-		direction = ">>>"; \
-	else \
-		direction = "<<<"; \
-	pr_debug("%s: %02x %02x %02x %02x %02x %02x %02x %02x %s [%d bytes]\n", \
-			 __func__, t, r, v & 0xff, v >> 8, i & 0xff, i >> 8, \
-			l & 0xff, l >> 8, direction, l); \
-}
-
 #define CE6230_USB_TIMEOUT 1000
 
 struct usb_req {
