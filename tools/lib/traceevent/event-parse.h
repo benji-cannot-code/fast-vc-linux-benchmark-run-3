60f38358ae3c1c269159b2eeeaaa601b616cbb6f
Steven Rostedt <rostedt@goodmis.org>
2012-08-23 11:22:01 -0400

    tools lib traceevent: Modify header to work in C++ programs
    Replace keyword "private" to "priv" in event-parse.h to allow it to be
    used in C++ programs.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1345735321.5069.62.camel@gandalf.local.home
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/lib/traceevent/event-parse.h
+++ tools/lib/traceevent/event-parse.h
@@ -50,7 +50,7 @@ struct pevent_record {
 	int			cpu;
 	int			ref_count;
 	int			locked;		/* Do not free, even if ref_count is zero */
-	void			*private;
+	void			*priv;
 #if DEBUG_RECORD
 	struct pevent_record	*prev;
 	struct pevent_record	*next;
@@ -107,7 +107,7 @@ struct plugin_option {
 	char				*plugin_alias;
 	char				*description;
 	char				*value;
-	void				*private;
+	void				*priv;
 	int				set;
 };
 
