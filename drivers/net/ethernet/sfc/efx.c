2a19133ba7f8c2fa8cb54b2c04ce0f7db65725ec
Ben Hutchings <bhutchings@solarflare.com>
2012-08-24 18:04:38 +0100

    sfc: Fix the initial device operstate
    Following commit 8f4cccb ('net: Set device operstate at registration
    time') it is now correct and preferable to set the carrier off before
    registering a device.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>

--- drivers/net/ethernet/sfc/efx.c
+++ drivers/net/ethernet/sfc/efx.c
@@ -2121,6 +2121,9 @@ static int efx_register_netdev(struct efx_nic *efx)
 		goto fail_locked;
 	efx_update_name(efx);
 
+	/* Always start with carrier off; PHY events will detect the link */
+	netif_carrier_off(net_dev);
+
 	rc = register_netdevice(net_dev);
 	if (rc)
 		goto fail_locked;
@@ -2130,9 +2133,6 @@ static int efx_register_netdev(struct efx_nic *efx)
 		efx_for_each_channel_tx_queue(tx_queue, channel)
 			efx_init_tx_queue_core_txq(tx_queue);
 	}
-
-	/* Always start with carrier off; PHY events will detect the link */
-	netif_carrier_off(net_dev);
 
 	rtnl_unlock();
 
