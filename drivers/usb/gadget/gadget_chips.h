c7852b9a96370b849587810fbe789d6889b25559
Kevin Cernekee <cernekee@gmail.com>
2012-08-25 12:38:52 -0700

    usb: gadget: bcm63xx UDC driver
    Driver for the "USB20D" / "USBD" block on BCM6328, BCM6368, BCM6816,
    BCM6362, BCM3383, and others.
    
    The hardware block was designed to support networking applications
    (direct connection of a home router to a PC), and the endpoint
    configuration is fixed.
    
    [ balbi@ti.com : dropped USB_GADGET_DUALSPEED from Kconfig ]
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>
    Acked-by: Sebastian Andrzej Siewior <sebastian@breakpoint.cc>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define gadget_is_amd5536udc(g)		(!strcmp("amd5536udc", (g)->name))
 #define gadget_is_at91(g)		(!strcmp("at91_udc", (g)->name))
 #define gadget_is_atmel_usba(g)		(!strcmp("atmel_usba_udc", (g)->name))
+#define gadget_is_bcm63xx(g)		(!strcmp("bcm63xx_udc", (g)->name))
 #define gadget_is_ci13xxx_msm(g)	(!strcmp("ci13xxx_msm", (g)->name))
 #define gadget_is_ci13xxx_pci(g)	(!strcmp("ci13xxx_pci", (g)->name))
 #define gadget_is_dummy(g)		(!strcmp("dummy_udc", (g)->name))
@@ -122,6 +123,8 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x32;
 	else if (gadget_is_lpc32xx(gadget))
 		return 0x33;
+	else if (gadget_is_bcm63xx(gadget))
+		return 0x34;
 
 	return -ENOENT;
 }
