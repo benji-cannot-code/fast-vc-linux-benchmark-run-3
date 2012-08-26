d5802bc2f7a14b0309402e46c4f6d8bddaab2f08
Patrick McHardy <kaber@trash.net>
2012-08-26 19:14:31 +0200

    netfilter: ip6tables: add stateless IPv6-to-IPv6 Network Prefix Translation target
    Signed-off-by: Patrick McHardy <kaber@trash.net>

---
+++ include/linux/netfilter_ipv6/ip6t_NPT.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __NETFILTER_IP6T_NPT
+#define __NETFILTER_IP6T_NPT
+
+#include <linux/types.h>
+#include <linux/netfilter.h>
+
+struct ip6t_npt_tginfo {
+	union nf_inet_addr	src_pfx;
+	union nf_inet_addr	dst_pfx;
+	__u8			src_pfx_len;
+	__u8			dst_pfx_len;
+	/* Used internally by the kernel */
+	__sum16			adjustment;
+};
+
+#endif /* __NETFILTER_IP6T_NPT */
