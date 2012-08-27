24554bae85ed1d8ce13e60890317a76cf14ea96b
Kees Cook <keescook@chromium.org>
2012-08-27 13:32:15 -0700

    debugfs: more tightly restrict default mount mode
    Since the debugfs is mostly only used by root, make the default mount
    mode 0700. Most system owners do not need a more permissive value,
    but they can choose to weaken the restrictions via their fstab.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/debugfs/inode.c
+++ fs/debugfs/inode.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/magic.h>
 #include <linux/slab.h>
 
-#define DEBUGFS_DEFAULT_MODE	0755
+#define DEBUGFS_DEFAULT_MODE	0700
 
 static struct vfsmount *debugfs_mount;
 static int debugfs_mount_count;
