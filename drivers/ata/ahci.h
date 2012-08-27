8e0728993268263a72494bd566b9172571ca00c5
Viresh Kumar <viresh.kumar@linaro.com>
2012-08-27 10:37:19 +0530

    ata/ahci_platform: Add clock framework support
    On many architectures, drivers are supposed to prepare/unprepare &
    enable/disable functional clock of device. This patch adds clock support for
    ahci_platform.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/ahci.h
+++ drivers/ata/ahci.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _AHCI_H
 #define _AHCI_H
 
+#include <linux/clk.h>
 #include <linux/libata.h>
 
 /* Enclosure Management Control */
@@ -317,6 +318,7 @@ struct ahci_host_priv {
 	u32 			em_loc; /* enclosure management location */
 	u32			em_buf_sz;	/* EM buffer size in byte */
 	u32			em_msg_type;	/* EM message type */
+	struct clk		*clk;		/* Only for platforms supporting clk */
 };
 
 extern int ahci_ignore_sss;
