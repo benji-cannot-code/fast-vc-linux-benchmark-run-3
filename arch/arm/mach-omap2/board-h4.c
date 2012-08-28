5865dca701970ff02c99f86f01a63d3b99f3d0a5
Igor Grinberg <grinberg@compulab.co.il>
2012-08-29 02:18:56 +0300

    ARM: OMAP: move debug_card_init() function
    debug_card_init() function resides in the plat/board.h file.
    Move it to a separate header file under plat/ so the board.h file can be
    removed.
    
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-h4.c
+++ arch/arm/mach-omap2/board-h4.c
@@ -33,11 +33,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 
-#include <plat/board.h>
 #include "common.h"
 #include <plat/menelaus.h>
 #include <plat/dma.h>
 #include <plat/gpmc.h>
+#include <plat/debug-devices.h>
 
 #include <video/omapdss.h>
 #include <video/omap-panel-generic-dpi.h>
