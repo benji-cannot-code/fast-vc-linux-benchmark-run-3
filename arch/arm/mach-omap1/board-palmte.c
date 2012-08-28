a7198eb059fb0baa2aa124884888d66a8c01afde
Igor Grinberg <grinberg@compulab.co.il>
2012-08-29 02:18:54 +0300

    ARM: OMAP1: move omap1_bl pdata out of arch/arm/*
    omap1 backlight platform data resides inside plat/board.h while it
    should be inside include/linux/...
    Move the omap1 backlight platform data to
    include/linux/platform_data/.
    
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Cc: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Cc: linux-fbdev@vger.kernel.org
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
    Acked-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/board-palmte.c
+++ arch/arm/mach-omap1/board-palmte.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/apm-emulation.h>
 #include <linux/omapfb.h>
+#include <linux/platform_data/omap1_bl.h>
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
--- arch/arm/mach-omap1/board-palmz71.c
+++ arch/arm/mach-omap1/board-palmz71.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/omapfb.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/ads7846.h>
+#include <linux/platform_data/omap1_bl.h>
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
