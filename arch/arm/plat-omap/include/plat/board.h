50f2416e83967a606b0710ce3ea6cb35b956d04a
Igor Grinberg <grinberg@compulab.co.il>
2012-08-29 02:18:55 +0300

    ARM: OMAP1: move lcd pdata out of arch/arm/*
    omap1 lcd platform data resides inside plat/board.h while it
    should be inside include/linux/...
    Move the omap1 lcd platform data to include/linux/omapfb.h.
    
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
    Acked-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/board.h
+++ arch/arm/plat-omap/include/plat/board.h
@@ -13,13 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-struct omap_lcd_config {
-	char panel_name[16];
-	char ctrl_name[16];
-	s16  nreset_gpio;
-	u8   data_lines;
-};
-
 /* for TI reference platforms sharing the same debug card */
 extern int debug_card_init(u32 addr, unsigned gpio);
 
