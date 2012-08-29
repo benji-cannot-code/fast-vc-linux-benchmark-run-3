159980ddf765a6fb2705c95743854a5db770b6ae
Rob Herring <rob.herring@calxeda.com>
2012-08-29 10:16:55 -0500

    ARM: orion: move custom gpio functions to orion-gpio.h
    Move custom orion platforms gpio code to orion-gpio to remove the
    dependency on mach/gpio.h.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Jason Cooper <jason@lakedaemon.net>
    Cc: Andrew Lunn <andrew@lunn.ch>

--- arch/arm/plat-orion/include/plat/orion-gpio.h
+++ arch/arm/plat-orion/include/plat/orion-gpio.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/arm/plat-orion/include/plat/gpio.h
+ * arch/arm/plat-orion/include/plat/orion-gpio.h
  *
  * Marvell Orion SoC GPIO handling.
  *
