62d3e8e379e120347fddf735812d4c7e18cbada8
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-08-30 10:54:12 +0300

    OMAPFB1: remove unnecessary includes
    Remove unnecessary includes from the old omapfb driver.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap/lcd_palmte.c
+++ drivers/video/omap/lcd_palmte.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/io.h>
 
-#include <plat/fpga.h>
 #include "omapfb.h"
 
 static int palmte_panel_init(struct lcd_panel *panel,
