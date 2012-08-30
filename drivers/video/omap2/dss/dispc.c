dfb2f19fe47a54c3fec1e781dd3032031876a8e8
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-08-30 10:52:47 +0300

    OMAPDSS: remove unnecessary includes
    Remove unnecessary includes from omapdss.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 
-#include <plat/clock.h>
-
 #include <video/omapdss.h>
 
 #include "dss.h"
