3c5b243d37d83867f64d0f21e7e0d310e2205351
Al Viro <viro@zeniv.linux.org.uk>
2012-08-31 15:48:05 -0400

    powerpc: switch to generic sys_execve()/kernel_execve()
    the only non-obvious part is that current_pt_regs() is really needed
    here - task_pt_regs() is NULL for kernel threads; it's OK for ptrace
    uses (the thing task_pt_regs() is intended for), but not for us.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/powerpc/include/asm/unistd.h
+++ arch/powerpc/include/asm/unistd.h
@@ -421,6 +421,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_WANT_COMPAT_SYS_RT_SIGSUSPEND
 #define __ARCH_WANT_SYS_NEWFSTATAT
 #endif
+#define __ARCH_WANT_SYS_EXECVE
+#define __ARCH_WANT_KERNEL_EXECVE
 
 /*
  * "Conditional" syscalls
