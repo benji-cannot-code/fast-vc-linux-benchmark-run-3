6adf6b81387104aab37e3ee81f84828bcb974ba5
Ian Abbott <abbotti@mev.co.uk>
2012-08-31 20:41:31 +0100

    staging: comedi: das08: Remove pc104 bustype value
    Remove the `pc104` value from `enum das08_bustype` and replace
    occurrences of it in the code and data with the `isa` value.  The code
    treats them as equivalent anyway with expressions like
    `(thisboard->bustype == isa || thisboard->bustype == pc104)`.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/das08.h
+++ drivers/staging/comedi/drivers/das08.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _DAS08_H
 #define _DAS08_H
 
-enum das08_bustype { isa, pci, pcmcia, pc104 };
+enum das08_bustype { isa, pci, pcmcia };
 /* different ways ai data is encoded in first two registers */
 enum das08_ai_encoding { das08_encode12, das08_encode16, das08_pcm_encode12 };
 enum das08_lrange { das08_pg_none, das08_bipolar5, das08_pgh, das08_pgl,
