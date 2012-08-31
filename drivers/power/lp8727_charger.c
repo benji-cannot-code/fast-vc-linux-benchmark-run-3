b1dc84165d4e84e991ea3cab56a8784161bfba3f
Kim, Milo <Milo.Kim@ti.com>
2012-08-31 09:23:25 +0000

    lp8727_charger: Add configurable debouce timer
    Debounce time is configurable in the platform side. If it is not defined,
    the default value is 270ms.
    
    Platform data is msec unit, and this time is converted to jiffies
    internally. The workqueue uses this jiffies time in the interrupt
    handling. So debounce_jiffies is added in the private data.
    
    Signed-off-by: Milo(Woogyom) Kim <milo.kim@ti.com>
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>

--- drivers/power/lp8727_charger.c
+++ drivers/power/lp8727_charger.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/power_supply.h>
 #include <linux/platform_data/lp8727.h>
 
-#define DEBOUNCE_MSEC	270
+#define DEFAULT_DEBOUNCE_MSEC	270
 
 /* Registers */
 #define CTRL1		0x1
@@ -91,6 +91,7 @@ struct lp8727_chg {
 	struct lp8727_psy *psy;
 	struct lp8727_chg_param *chg_parm;
 	enum lp8727_dev_id devid;
+	unsigned long debounce_jiffies;
 };
 
 static int lp8727_read_bytes(struct lp8727_chg *pchg, u8 reg, u8 *data, u8 len)
@@ -237,15 +238,18 @@ static void lp8727_delayed_func(struct work_struct *_work)
 static irqreturn_t lp8727_isr_func(int irq, void *ptr)
 {
 	struct lp8727_chg *pchg = ptr;
-	unsigned long delay = msecs_to_jiffies(DEBOUNCE_MSEC);
 
-	queue_delayed_work(pchg->irqthread, &pchg->work, delay);
+	queue_delayed_work(pchg->irqthread, &pchg->work,
+					pchg->debounce_jiffies);
 
 	return IRQ_HANDLED;
 }
 
 static int lp8727_intr_config(struct lp8727_chg *pchg)
 {
+	unsigned delay_msec = pchg->pdata ? pchg->pdata->debounce_msec :
+						DEFAULT_DEBOUNCE_MSEC;
+
 	INIT_DELAYED_WORK(&pchg->work, lp8727_delayed_func);
 
 	pchg->irqthread = create_singlethread_workqueue("lp8727-irqthd");
@@ -253,6 +257,8 @@ static int lp8727_intr_config(struct lp8727_chg *pchg)
 		dev_err(pchg->dev, "can not create thread for lp8727\n");
 		return -ENOMEM;
 	}
+
+	pchg->debounce_jiffies = msecs_to_jiffies(delay_msec);
 
 	return request_threaded_irq(pchg->client->irq,
 				NULL,
