f404f839afca30a824cbd8b3584702536de7273e
Tony Lindgren <tony@atomide.com>
2012-08-31 16:08:07 -0700

    ARM: OMAP2+ Move SoC specific headers to be local to mach-omap2
    These can now be moved to be local headers in mach-omap2.
    
    Note that this patch removes arch/arm/plat-omap/devices.c as it
    will get removed anyways with Paul Walmsley's patch
    "ARM: OMAP: split OMAP1, OMAP2+ RNG device registration".
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/soc.h
+++ arch/arm/mach-omap2/soc.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/cpu.h>
-#include <plat/omap24xx.h>
-#include <plat/omap34xx.h>
-#include <plat/omap44xx.h>
-#include <plat/ti81xx.h>
-#include <plat/am33xx.h>
-#include <plat/omap54xx.h>
+#include "omap24xx.h"
+#include "omap34xx.h"
+#include "omap44xx.h"
+#include "ti81xx.h"
+#include "am33xx.h"
+#include "omap54xx.h"
