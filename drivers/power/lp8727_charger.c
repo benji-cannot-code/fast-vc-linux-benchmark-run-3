0772098d243da82955ebb32334f456bb0a07f861
Kim, Milo <Milo.Kim@ti.com>
2012-08-31 09:27:22 +0000

    lp8727_charger: More pure cosmetic improvements
    This is really minor, but it improves the readability.
    
    Signed-off-by: Milo(Woogyom) Kim <milo.kim@ti.com>
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>

--- drivers/power/lp8727_charger.c
+++ drivers/power/lp8727_charger.c
@@ -150,7 +150,6 @@ static int lp8727_init_device(struct lp8727_chg *pchg)
 	if (ret)
 		return ret;
 
-
 	val = LP8727_ID200_EN | LP8727_ADC_EN | LP8727_CP_EN;
 	ret = lp8727_write_byte(pchg, LP8727_CTRL1, val);
 	if (ret)
@@ -163,6 +162,7 @@ static int lp8727_init_device(struct lp8727_chg *pchg)
 static int lp8727_is_dedicated_charger(struct lp8727_chg *pchg)
 {
 	u8 val;
+
 	lp8727_read_byte(pchg, LP8727_STATUS1, &val);
 	return val & LP8727_DCPORT;
 }
@@ -170,6 +170,7 @@ static int lp8727_is_dedicated_charger(struct lp8727_chg *pchg)
 static int lp8727_is_usb_charger(struct lp8727_chg *pchg)
 {
 	u8 val;
+
 	lp8727_read_byte(pchg, LP8727_STATUS1, &val);
 	return val & LP8727_CHPORT;
 }
