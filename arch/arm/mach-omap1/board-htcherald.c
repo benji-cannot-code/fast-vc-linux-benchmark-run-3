6aef1322aaf05b15365f5894bc87211e8d50d07d
Tony Lindgren <tony@atomide.com>
2012-08-31 17:04:35 -0700

    ARM: OMAP1: Move SoC specific headers from plat to mach for omap1
    There's no need to have these in plat-omap any longer. Note that these
    could eventually be made local to mach-omap1 instead of being in mach.
    
    But to do that, at least various driver access using omap7xxx.h registers
    needs to be fixed first.
    
    Cc: spi-devel-general@lists.sourceforge.net
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/board-htcherald.c
+++ arch/arm/mach-omap1/board-htcherald.c
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 
-#include <plat/omap7xx.h>
+#include <mach/omap7xx.h>
 #include <plat/keypad.h>
 #include <plat/mmc.h>
 
--- arch/arm/mach-omap1/devices.c
+++ arch/arm/mach-omap1/devices.c
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/mux.h>
 #include <plat/dma.h>
 #include <plat/mmc.h>
-#include <plat/omap7xx.h>
 
+#include <mach/omap7xx.h>
 #include <mach/camera.h>
 #include <mach/hardware.h>
 
