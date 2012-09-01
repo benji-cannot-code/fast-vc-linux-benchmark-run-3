2a8dc1405bf0a42364191fb6b1422ce07890385d
Krzysztof Hałasa <khc@pm.waw.pl>
2012-09-01 18:28:14 +0200

    IXP4xx: Fix Goramo MultiLink platform compilation.
    Signed-off-by: Krzysztof Hałasa <khc@pm.waw.pl>

--- arch/arm/mach-ixp4xx/goramo_mlr.c
+++ arch/arm/mach-ixp4xx/goramo_mlr.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/mach/flash.h>
 #include <asm/mach/pci.h>
+#include <asm/system_info.h>
 
 #define SLOT_ETHA		0x0B	/* IDSEL = AD21 */
 #define SLOT_ETHB		0x0C	/* IDSEL = AD20 */
