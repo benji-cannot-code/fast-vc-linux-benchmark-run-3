95f7c39a151c7af2fff13c7ab99084d72fc6f716
Heiko Carstens <heiko.carstens@de.ibm.com>
2012-09-03 14:11:32 +0200

    s390/topology: remove sysinfo header include, add forward declaration instead
    Any change to sysinfo.h causes a whole kernel recompile since sysinfo.h is
    included by topology.h, which again is used nearly everywhere.
    So remove that include and add a forward declaration instead.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/topology.h
+++ arch/s390/include/asm/topology.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_S390_TOPOLOGY_H
 
 #include <linux/cpumask.h>
-#include <asm/sysinfo.h>
 
+struct sysinfo_15_1_x;
 struct cpu;
 
 #ifdef CONFIG_SCHED_BOOK
--- arch/s390/kernel/topology.c
+++ arch/s390/kernel/topology.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/smp.h>
 #include <linux/mm.h>
+#include <asm/sysinfo.h>
 
 #define PTF_HORIZONTAL	(0UL)
 #define PTF_VERTICAL	(1UL)
