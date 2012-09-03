5cf7b124e40cb5ccb66a20d1576eb093471a70eb
Akinobu Mita <akinobu.mita@gmail.com>
2012-09-03 21:59:58 +0900

    mtd: mtd_nandecctest: remove unnecessary include
    Including linux/jiffies.h was required for calling srandom32(jiffies)
    that has already been removed.
    
    Signed-off-by: Akinobu Mita <akinobu.mita@gmail.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/tests/mtd_nandecctest.c
+++ drivers/mtd/tests/mtd_nandecctest.c
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/random.h>
 #include <linux/string.h>
 #include <linux/bitops.h>
-#include <linux/jiffies.h>
 #include <linux/mtd/nand_ecc.h>
 
 #if defined(CONFIG_MTD_NAND) || defined(CONFIG_MTD_NAND_MODULE)
