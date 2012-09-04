c56e4c3c1a1e604e377c20466dd5e597dff8983c
Hans Verkuil <hans.verkuil@cisco.com>
2012-09-04 12:08:47 -0300

    [media] v4l2: make vidioc_s_modulator const
    Write-only ioctls should have a const argument in the ioctl op.
    Do this conversion for vidioc_s_modulator.
    Adding const for write-only ioctls was decided during the 2012 Media Workshop.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/radio/radio-wl1273.c
+++ drivers/media/radio/radio-wl1273.c
@@ -1716,7 +1716,7 @@ out:
 }
 
 static int wl1273_fm_vidioc_s_modulator(struct file *file, void *priv,
-					struct v4l2_modulator *modulator)
+					const struct v4l2_modulator *modulator)
 {
 	struct wl1273_device *radio = video_get_drvdata(video_devdata(file));
 	struct wl1273_core *core = radio->core;
