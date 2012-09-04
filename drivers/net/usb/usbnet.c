731972d7b597ca8e41acfb5148db1f2a543d29a3
Oliver Neukum <oliver@neukum.org>
2012-09-04 04:21:54 +0000

    usbnet: drop unneeded check for NULL
    usbnet_start_xmit() is always called with a valid skb
    
    Signed-off-by: Oliver Neukum <oneukum@suse.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -1093,8 +1093,7 @@ netdev_tx_t usbnet_start_xmit (struct sk_buff *skb,
 	unsigned long		flags;
 	int retval;
 
-	if (skb)
-		skb_tx_timestamp(skb);
+	skb_tx_timestamp(skb);
 
 	// some devices want funky USB-level framing, for
 	// win32 driver (usually) and/or hardware quirks
