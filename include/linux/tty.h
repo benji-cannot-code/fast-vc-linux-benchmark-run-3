bfcb8480a663c77fdac796817dfb52de386588be
Alan Cox <alan@linux.intel.com>
2012-09-04 16:35:08 +0100

    tty: move the async flags from the serial code into the tty includes
    These are used with the tty_port flags which are tty generic so move the
    flags into a more sensible place. This then makes it possible to add
    helpers such as those suggested by Huang Shijie.
    
    Signed-off-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty_driver.h>
 #include <linux/tty_ldisc.h>
 #include <linux/mutex.h>
+#include <linux/tty_flags.h>
 
 
 
