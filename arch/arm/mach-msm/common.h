881956d74ad2545d94d84126345b7353a3fe6878
Stephen Boyd <sboyd@codeaurora.org>
2012-09-05 12:28:59 -0700

    ARM: msm: Remove non-DT targets from 8960
    Remove the non-DT targets supported by 8960. This makes 8960 a
    device tree only target.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- arch/arm/mach-msm/common.h
+++ arch/arm/mach-msm/common.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern struct sys_timer msm7x01_timer;
 extern struct sys_timer msm7x30_timer;
-extern struct sys_timer msm8960_timer;
 extern struct sys_timer msm_dt_timer;
 extern struct sys_timer qsd8x50_timer;
 
--- arch/arm/mach-msm/timer.c
+++ arch/arm/mach-msm/timer.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TIMER_ENABLE_CLR_ON_MATCH_EN    BIT(1)
 #define TIMER_ENABLE_EN                 BIT(0)
 #define TIMER_CLEAR             0x000C
-#define DGT_CLK_CTL             0x0030
 #define DGT_CLK_CTL_DIV_4	0x3
 
 #define GPT_HZ 32768
@@ -345,18 +344,6 @@ static void __init msm7x30_timer_init(void)
 
 struct sys_timer msm7x30_timer = {
 	.init = msm7x30_timer_init
-};
-
-static void __init msm8960_timer_init(void)
-{
-	if (msm_timer_map(0x0200A004, 0x0208A024))
-		return;
-	writel_relaxed(DGT_CLK_CTL_DIV_4, event_base + DGT_CLK_CTL);
-	msm_timer_init(27000000 / 4, 32, 17, true);
-}
-
-struct sys_timer msm8960_timer = {
-	.init = msm8960_timer_init
 };
 
 static void __init qsd8x50_timer_init(void)
