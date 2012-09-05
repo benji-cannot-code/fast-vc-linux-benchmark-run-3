0bdd88e2f0d2f548259bc65047a926e0a7783f01
Hans Verkuil <hans.verkuil@cisco.com>
2012-09-05 05:33:21 -0300

    [media] v4l2-dev: add new VFL_DIR_ defines
    These will be used by v4l2-dev.c to improve ioctl checking.
    I.e. ioctls for capture should return -ENOTTY when called for
    an output device.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/media/v4l2-dev.h
+++ include/media/v4l2-dev.h
@@ -27,6 +27,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VFL_TYPE_SUBDEV		3
 #define VFL_TYPE_MAX		4
 
+/* Is this a receiver, transmitter or mem-to-mem? */
+/* Ignored for VFL_TYPE_SUBDEV. */
+#define VFL_DIR_RX		0
+#define VFL_DIR_TX		1
+#define VFL_DIR_M2M		2
+
 struct v4l2_ioctl_callbacks;
 struct video_device;
 struct v4l2_device;
@@ -106,7 +112,8 @@ struct video_device
 
 	/* device info */
 	char name[32];
-	int vfl_type;
+	int vfl_type;	/* device type */
+	int vfl_dir;	/* receiver, transmitter or m2m */
 	/* 'minor' is set to -1 if the registration failed */
 	int minor;
 	u16 num;
