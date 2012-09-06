128bcca6c1b9a0fd0f0c16b17d4fde7eb59941e2
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-09-06 20:11:10 +0200

    usb: gadget: libcomposite: move config.c into libcomposite
    This patch moves config.c into libcomposite and updates all gadgets.
    
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/tcm_usb_gadget.c
+++ drivers/usb/gadget/tcm_usb_gadget.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/unaligned.h>
 
 #include "epautoconf.c"
-#include "config.c"
 #include "composite.c"
 
 #include "tcm_usb_gadget.h"
