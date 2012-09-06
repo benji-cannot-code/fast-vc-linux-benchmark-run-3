ec74379a6835eaf5923b3b450efb23a9be1f1760
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2012-09-06 15:05:43 +0300

    Bluetooth: trivial: Make hci_chan_del return void
    Return code is not needed in hci_chan_del
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -936,7 +936,7 @@ struct hci_chan *hci_chan_create(struct hci_conn *conn)
 	return chan;
 }
 
-int hci_chan_del(struct hci_chan *chan)
+void hci_chan_del(struct hci_chan *chan)
 {
 	struct hci_conn *conn = chan->conn;
 	struct hci_dev *hdev = conn->hdev;
@@ -949,8 +949,6 @@ int hci_chan_del(struct hci_chan *chan)
 
 	skb_queue_purge(&chan->data_q);
 	kfree(chan);
-
-	return 0;
 }
 
 void hci_chan_list_flush(struct hci_conn *conn)
