7c3ce090b6c5936f860606b2825aad62e4e03188
Devendra Naga <devendra.aaru@gmail.com>
2012-09-07 00:08:05 +0530

    staging: vt6655: return ENOMEM rather than ENODEV when alloc_etherdev fail
    when alloc_etherdev fails we should be returning ENOMEM, not ENODEV
    
    Signed-off-by: Devendra Naga <devendra.aaru@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/device_main.c
+++ drivers/staging/vt6655/device_main.c
@@ -923,7 +923,7 @@ vt6655_probe(struct pci_dev *pcid, const struct pci_device_id *ent)
 
     if (dev == NULL) {
         printk(KERN_ERR DEVICE_NAME ": allocate net device failed \n");
-        return -ENODEV;
+        return -ENOMEM;
     }
 
     // Chain it all together
