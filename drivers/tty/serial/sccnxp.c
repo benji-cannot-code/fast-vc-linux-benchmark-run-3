23f0eb6bb1517b7fdf80c7fc6d3b989650f4190a
Stephen Rothwell <sfr@canb.auug.org.au>
2012-09-06 15:05:04 +1000

    serial: serial_core.h needs console.h included first
    Fixes these build errors:
    
    In file included from drivers/tty/serial/sccnxp.c:20:0:
    include/linux/serial_core.h: In function 'uart_handle_break':
    include/linux/serial_core.h:543:30: error: dereferencing pointer to incomplete type
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/sccnxp.c
+++ drivers/tty/serial/sccnxp.c
@@ -18,12 +18,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/device.h>
+#include <linux/console.h>
 #include <linux/serial_core.h>
 #include <linux/serial.h>
 #include <linux/io.h>
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
-#include <linux/console.h>
 #include <linux/platform_device.h>
 #include <linux/platform_data/sccnxp.h>
 
