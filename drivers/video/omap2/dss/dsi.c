48aa655f3f9e6e7a56f423f4d71c8d9bf325855f
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-09-06 13:57:39 +0300

    OMAPDSS: omap_dss_register_device() doesn't take display index
    We used to have all the displays of the board in one list, and we made a
    "displayX" directory in the sysfs, where X was the index of the display
    in the list.
    
    This doesn't work anymore with device tree, as there's no single list to
    get the number from, and it doesn't work very well even with non-DT as
    we need to do some tricks to get the index nowadays.
    
    This patch changes omap_dss_register_device() so that it doesn't take
    disp_num as a parameter anymore, but uses a private increasing counter
    for the display number.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dsi.c
+++ drivers/video/omap2/dss/dsi.c
@@ -5028,7 +5028,7 @@ static void __init dsi_probe_pdata(struct platform_device *dsidev)
 			continue;
 		}
 
-		r = omap_dss_register_device(dssdev, &dsidev->dev, i);
+		r = omap_dss_register_device(dssdev, &dsidev->dev);
 		if (r)
 			DSSERR("device %s register failed: %d\n",
 					dssdev->name, r);
