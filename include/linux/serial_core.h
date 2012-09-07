caf526ab4f7488bf6fd2c570a09ef1f79f552f80
Sean Young <sean@mess.org>
2012-09-07 19:06:24 +0100

    8250: blacklist Winbond CIR port
    The legacy serial driver will detect the Winbond CIR device as a serial
    port, since it looks exactly like a serial port unless you know what
    it is from the PNP ID.
    
    Here we track this port as a special PORT_8250_CIR type, preventing the
    legacy serial driver from probing it.
    
    Signed-off-by: Sean Young <sean@mess.org>
    Acked-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -49,7 +49,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PORT_TEGRA	20	/* NVIDIA Tegra internal UART */
 #define PORT_XR17D15X	21	/* Exar XR17D15x UART */
 #define PORT_LPC3220	22	/* NXP LPC32xx SoC "Standard" UART */
-#define PORT_MAX_8250	22	/* max port ID */
+#define PORT_8250_CIR	23	/* CIR infrared port, has its own driver */
+#define PORT_MAX_8250	23	/* max port ID */
 
 /*
  * ARM specific type numbers.  These are not currently guaranteed
