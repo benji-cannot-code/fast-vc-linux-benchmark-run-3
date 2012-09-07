4cf6a79cf873c550fa14d1054e3f0b44cc215d8e
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-09-07 14:31:45 +0200

    usb/core: use bin2bcd() for bcdDevice in RH
    The kernel's version number is used as decimal in the bcdDevice field of
    the RH descriptor. For kernel version v3.12 we would see 3.0c in lsusb.
    I am not sure how important it is to stick with bcd values since this is
    this way since we started git history and nobody complained (however back
    then we reported only 2.6).
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hcd.c
+++ drivers/usb/core/hcd.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include <linux/bcd.h>
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/kernel.h>
@@ -124,9 +125,8 @@ static inline int is_root_hub(struct usb_device *udev)
  */
 
 /*-------------------------------------------------------------------------*/
-
-#define KERNEL_REL	((LINUX_VERSION_CODE >> 16) & 0x0ff)
-#define KERNEL_VER	((LINUX_VERSION_CODE >> 8) & 0x0ff)
+#define KERNEL_REL	bin2bcd(((LINUX_VERSION_CODE >> 16) & 0x0ff))
+#define KERNEL_VER	bin2bcd(((LINUX_VERSION_CODE >> 8) & 0x0ff))
 
 /* usb 3.0 root hub device descriptor */
 static const u8 usb3_rh_dev_descriptor[18] = {
