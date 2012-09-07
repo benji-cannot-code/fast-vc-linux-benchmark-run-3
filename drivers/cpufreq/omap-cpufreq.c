aab66d35633ca9b9690c8cb586416b07b86e38ef
Paul Walmsley <paul@pwsan.com>
2012-09-07 18:16:35 +0000

    cpufreq: OMAP: fix clock usage to be SoC independent, remove plat/ includes
    OMAP core code now has SoC-independent clock alias for the scalable
    CPU clock.  Using it means driver is SoC independent and will work for
    AM3xxx SoCs as well as OMAP1/3/4.
    
    While here, remove some unnecessary plat/ includes that are
    interfering with multi-subarch ARM kernels.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    [tony@atomide.com: updated already changed clock aliases]
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    [khilman@ti.com: minor shortlog/changelog updates]
    Acked-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Kevin Hilman <khilman@ti.com>

--- drivers/cpufreq/omap-cpufreq.c
+++ drivers/cpufreq/omap-cpufreq.c
@@ -31,11 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/smp_plat.h>
 #include <asm/cpu.h>
 
-#include <plat/clock.h>
-#include <plat/common.h>
 #include <plat/omap_device.h>
-
-#include <mach/hardware.h>
 
 /* OPP tolerance in percentage */
 #define	OPP_TOLERANCE	4
@@ -43,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct cpufreq_frequency_table *freq_table;
 static atomic_t freq_table_users = ATOMIC_INIT(0);
 static struct clk *mpu_clk;
-static char *mpu_clk_name;
 static struct device *mpu_dev;
 static struct regulator *mpu_reg;
 
@@ -180,7 +175,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 {
 	int result = 0;
 
-	mpu_clk = clk_get(NULL, mpu_clk_name);
+	mpu_clk = clk_get(NULL, "cpufreq_ck");
 	if (IS_ERR(mpu_clk))
 		return PTR_ERR(mpu_clk);
 
@@ -261,18 +256,6 @@ static struct cpufreq_driver omap_driver = {
 
 static int __init omap_cpufreq_init(void)
 {
-	if (cpu_is_omap24xx())
-		mpu_clk_name = "virt_prcm_set";
-	else if (cpu_is_omap34xx())
-		mpu_clk_name = "dpll1_ck";
-	else if (cpu_is_omap44xx())
-		mpu_clk_name = "dpll_mpu_ck";
-
-	if (!mpu_clk_name) {
-		pr_err("%s: unsupported Silicon?\n", __func__);
-		return -EINVAL;
-	}
-
 	mpu_dev = omap_device_get_by_hwmod_name("mpu");
 	if (IS_ERR(mpu_dev)) {
 		pr_warning("%s: unable to get the mpu device\n", __func__);
