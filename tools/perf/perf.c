19cf1efb80f6e3c7d07a53ea88162c6d6a1051db
Irina Tirdea <irina.tirdea@intel.com>
2012-09-08 03:43:19 +0300

    perf tools: include missing pthread.h header
    pthread variables are used in some files without explicitely including
    pthread.h. This leads to compile errors on Android. e.g.: in annotate.h,
    error: unknown type name 'pthread_mutex_t'
    
    Including pthread.h explicitely in files that use it to have all definitions
    included.
    
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Link: http://lkml.kernel.org/r/1347065004-15306-8-git-send-email-irina.tirdea@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/perf.c
+++ tools/perf/perf.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "util/run-command.h"
 #include "util/parse-events.h"
 #include "util/debugfs.h"
+#include <pthread.h>
 
 const char perf_usage_string[] =
 	"perf [--version] [--help] COMMAND [ARGS]";
