662d8551ef3ad5078ea8060ef984d93a5cace438
Irina Tirdea <irina.tirdea@intel.com>
2012-09-08 08:35:51 +0300

    perf bench: fix assert when NDEBUG is defined
    When NDEBUG is defined, the assert macro will be expanded to nothing.
    Some assert calls used in perf are also including some functionality
    (e.g. system calls), not only validity checks. Therefore, if NDEBUG is
    defined, this functionality will be removed along with the assert.  Perf
    also defines BUG_ON based on assert, so it has the same problem.
    
    Define BUG_ON so that the condition will be executed when NDEBUG is
    defined.  Replace the assert statements that have these side effects
    with BUG_ON.
    
    For defining BUG_ON, use "if (cond) {}" insted of "if (cond) ;" because
    in the latter case build fails with "error: suggest braces around empty
    body in an ‘if’ statement [-Werror=empty-body]"
    
    Suggested-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Reviewed-by: Namhyung Kim <namhyung@kernel.org>
    Reviewed-by: Pekka Enberg <penberg@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Link: http://lkml.kernel.org/r/1347082551-2394-1-git-send-email-irina.tirdea@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/include/linux/kernel.h
+++ tools/perf/util/include/linux/kernel.h
@@ -48,7 +48,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #ifndef BUG_ON
+#ifdef NDEBUG
+#define BUG_ON(cond) do { if (cond) {} } while (0)
+#else
 #define BUG_ON(cond) assert(!(cond))
+#endif
 #endif
 
 /*
