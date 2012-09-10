eb03507a9324d0c3078c640afcb3acc1fd740b49
Irina Tirdea <irina.tirdea@gmail.com>
2012-09-11 01:14:58 +0300

    perf tools: include wrapper for magic.h
    perf is currently including magic.h directly from the kernel. If the
    glibc magic.h is also included, this leads to warnings that the
    constants are redefined. This happens on some systems (e.g. Android).
    
    Redefinition errors on Android:
    In file included from util/util.h:79:0,
                     from util/cache.h:5,
                     from util/abspath.c:1:
    util/../../../include/linux/magic.h:5:0:
    error: "AFFS_SUPER_MAGIC" redefined [-Werror]
    bionic/libc/include/sys/vfs.h:53:0:
    note: this is the location of the previous definition
    util/../../../include/linux/magic.h:19:0:
    error: "EFS_SUPER_MAGIC" redefined [-Werror]
    bionic/libc/include/sys/vfs.h:61:0:
    note: this is the location of the previous definition
    util/../../../include/linux/magic.h:26:0:
    error: "HPFS_SUPER_MAGIC" redefined [-Werror]
    bionic/libc/include/sys/vfs.h:67:0:
    note: this is the location of the previous definition
    
    Only two constants from magic.h are used by perf (DEBUGFS_MAGIC and
    SYSFS_MAGIC). This fix provides a wrapper for magic.h that includes only
    these constants instead of including the kernel header file directly.
    
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Acked-by: Pekka Enberg <penberg@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Irina Tirdea <irina.tirdea@intel.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Link: http://lkml.kernel.org/r/1347315303-29906-2-git-send-email-irina.tirdea@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -71,7 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <inttypes.h>
-#include "../../../include/linux/magic.h"
+#include <linux/magic.h>
 #include "types.h"
 #include <sys/ttydefaults.h>
 
