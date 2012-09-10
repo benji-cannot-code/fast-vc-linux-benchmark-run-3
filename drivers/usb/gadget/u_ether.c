b24095af0b8128bae4168021f9e9a49c07955352
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-09-10 16:30:50 +0200

    usb: gadget: add module.h to u_ether.c
    Spotted by Fengguang Wu:
    |In file included from drivers/usb/gadget/ether.c:110:0:
    |drivers/usb/gadget/u_ether.c:87:21: error: expected ')' before 'uint'
    |drivers/usb/gadget/u_ether.c:88:25: error: expected ')' before string constant
    
    This is because u_ether.c relied on having module.h included somewhere.
    This isn't the case since composite.c is no longer included.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/u_ether.c
+++ drivers/usb/gadget/u_ether.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* #define VERBOSE_DEBUG */
 
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/gfp.h>
 #include <linux/device.h>
 #include <linux/ctype.h>
