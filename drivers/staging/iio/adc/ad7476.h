11cf7e946476eb7aa044b61a1ac6602dea53b664
Lars-Peter Clausen <lars@metafoo.de>
2012-09-10 09:34:00 +0100

    staging:iio:ad7476: Rework reference voltage handling
    Slightly rework the reference voltage handling for the ad7476 driver. Now the only
    way to specify a external reference voltage is to use the regulator API,
    previously it was possible to use either platform_data or the regulator API. The
    new way is more consistent with what other drivers do.
    
    Also do not ignore errors when requesting the regulator, since this will cope
    very poorly with e.g. deferred probing.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/adc/ad7476.h
+++ drivers/staging/iio/adc/ad7476.h
@@ -11,16 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define RES_MASK(bits)	((1 << (bits)) - 1)
 
-/*
- * TODO: struct ad7476_platform_data needs to go into include/linux/iio
- */
-
-struct ad7476_platform_data {
-	u16				vref_mv;
-};
-
 struct ad7476_chip_info {
-	u16				int_vref_mv;
+	unsigned int			int_vref_uv;
 	struct iio_chan_spec		channel[2];
 };
 
@@ -28,7 +20,6 @@ struct ad7476_state {
 	struct spi_device		*spi;
 	const struct ad7476_chip_info	*chip_info;
 	struct regulator		*reg;
-	u16				int_vref_mv;
 	struct spi_transfer		xfer;
 	struct spi_message		msg;
 	/*
