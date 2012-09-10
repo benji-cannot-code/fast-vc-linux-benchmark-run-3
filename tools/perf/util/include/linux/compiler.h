b5e1ff32a680daa8f626b1c87dd660b6b322bc3f
Irina Tirdea <irina.tirdea@gmail.com>
2012-09-11 01:14:59 +0300

    perf tools: Update types definitions for Android
    Some type definitions are missing from Android or are already defined in
    bionic and lead to redefinition errors.
    
    Android defines in types.h __le32. Since perf is wrapping <linux/types.h> with a
    local version, we need to define this constant in the local version too.
    Error in Android:
    In file included from bionic/libc/include/unistd.h:36:0,
                     from external/perf/tools/perf/util/util.h:46,
                     from external/perf/tools/perf/util/cache.h:5,
                     from external/perf/tools/perf/util/abspath.c:1:
    bionic/libc/kernel/common/linux/capability.h:60:2:
    error: unknown type name '__le32'
    
    roundup() definition is missing:
    util/symbol.c: In function 'symbols__fixup_end':
    util/symbol.c:106: warning: implicit declaration of function 'roundup'
    util/symbol.c:106: warning: nested extern declaration of 'roundup'
    
    __force macro defined in perf is also defined in libc which leads to
    redefinition errors. In order to avoid these, we guard these definition
    with
    
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Acked-by: Pekka Enberg <penberg@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Irina Tirdea <irina.tirdea@intel.com>
    Cc: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Link: http://lkml.kernel.org/r/1347315303-29906-3-git-send-email-irina.tirdea@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/include/linux/compiler.h
+++ tools/perf/util/include/linux/compiler.h
@@ -13,4 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __used		__attribute__((__unused__))
 #define __packed	__attribute__((__packed__))
 
+#ifndef __force
+#define __force
+#endif
+
 #endif
--- tools/perf/util/include/linux/kernel.h
+++ tools/perf/util/include/linux/kernel.h
@@ -47,6 +47,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	_min1 < _min2 ? _min1 : _min2; })
 #endif
 
+#ifndef roundup
+#define roundup(x, y) (                                \
+{                                                      \
+	const typeof(y) __y = y;		       \
+	(((x) + (__y - 1)) / __y) * __y;	       \
+}                                                      \
+)
+#endif
+
 #ifndef BUG_ON
 #ifdef NDEBUG
 #define BUG_ON(cond) do { if (cond) {} } while (0)
