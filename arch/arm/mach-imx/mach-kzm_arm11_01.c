a35b47f0b66457fdae75a20e81d60c9c84dfa605
Sascha Hauer <s.hauer@pengutronix.de>
2012-09-11 08:50:00 +0200

    ARM: i.MX remove last leftovers from legacy clock support
    This also removes mach/clock.h along the way
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Reviewed-by: Mike Turquette <mturquette@linaro.org>

--- arch/arm/mach-imx/mach-kzm_arm11_01.c
+++ arch/arm/mach-imx/mach-kzm_arm11_01.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/map.h>
 #include <asm/mach/time.h>
 
-#include <mach/clock.h>
 #include <mach/common.h>
 #include <mach/hardware.h>
 #include <mach/iomux-mx3.h>
