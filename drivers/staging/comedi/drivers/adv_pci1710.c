e84fb5218514b69ef23d3f05f505110596aef6ac
H Hartley Sweeten <hartleys@visionengravers.com>
2012-09-11 15:08:52 -0700

    staging: comedi: adv_pci1710: remove "dummy" boardinfo entry
    The legacy attach used the "dummy" boardinfo entry to allow
    matching a boardinfo to a pci card based on the "name" passed
    by the comedi core. This driver now uses the PCI auto config
    mechanism which always matches to the PCI vendor/device ids.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adv_pci1710.c
+++ drivers/staging/comedi/drivers/adv_pci1710.c
@@ -53,8 +53,6 @@ Configuration options:
 				 * correct channel number on every 12 bit
 				 * sample */
 
-#define DRV_NAME "adv_pci1710"
-
 #define PCI_VENDOR_ID_ADVANTECH		0x13fe
 
 /* hardware types of the cards */
@@ -294,9 +292,6 @@ static const struct boardtype boardtypes[] = {
 		.rangecode_ai	= range_codes_pci17x1,
 		.ai_ns_min	= 10000,
 		.fifo_half_size	= 512,
-	}, {
-		/* dummy entry corresponding to driver name */
-		.name		= DRV_NAME,
 	},
 };
 
