6cd0dfa3421ecb349d5e4042d8f72455f04bf0bc
Antti Palosaari <crope@iki.fi>
2012-09-12 11:37:27 -0300

    [media] ec168: declare MODULE_FIRMWARE
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/dvb-usb-v2/ec168.h
+++ drivers/media/usb/dvb-usb-v2/ec168.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_usb.h"
 
 #define EC168_USB_TIMEOUT 1000
+#define EC168_FIRMWARE "dvb-usb-ec168.fw"
 
 struct ec168_req {
 	u8  cmd;       /* [1] */
