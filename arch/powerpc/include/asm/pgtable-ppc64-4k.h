048086c0cd47ba6e99a46b1eee86533dd26a040d
Scott Wood <scottwood@freescale.com>
2012-09-12 13:00:09 +0000

    powerpc/mm: Fix typo in PTRS_PER_PUD
    PTRS_PER_PUD should be based on PUD_INDEX_SIZE, not PMD_INDEX_SIZE.  We
    got away with it because PUD and PMD had the same index size, but this is
    no longer true with Aneesh's patchset to support a 46-bit user effective
    address space.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/pgtable-ppc64-4k.h
+++ arch/powerpc/include/asm/pgtable-ppc64-4k.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PTRS_PER_PTE	(1 << PTE_INDEX_SIZE)
 #define PTRS_PER_PMD	(1 << PMD_INDEX_SIZE)
-#define PTRS_PER_PUD	(1 << PMD_INDEX_SIZE)
+#define PTRS_PER_PUD	(1 << PUD_INDEX_SIZE)
 #define PTRS_PER_PGD	(1 << PGD_INDEX_SIZE)
 
 /* PMD_SHIFT determines what a second-level page table entry can map */
