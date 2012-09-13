189535873c64f4ecf34c6934aba79a263788e09c
Alexander Duyck <alexander.h.duyck@intel.com>
2012-09-13 06:27:55 +0000

    igb: Remove logic that was doing NUMA pseudo-aware allocations
    This change removes the code that was doing the NUMA allocations for the
    q_vectors, rings, and ring resources.  The problem is the logic used assumed
    that the NUMA nodes were always interleved and that is not always the case.
    
    At some point I hope to add this functionality back in a more controlled
    manner in the future.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igb/igb.h
+++ drivers/net/ethernet/intel/igb/igb.h
@@ -214,7 +214,6 @@ struct igb_q_vector {
 	struct igb_ring_container rx, tx;
 
 	struct napi_struct napi;
-	int numa_node;
 
 	u16 itr_val;
 	u8 set_itr;
@@ -259,7 +258,6 @@ struct igb_ring {
 	};
 	/* Items past this point are only used during ring alloc / free */
 	dma_addr_t dma;                /* phys address of the ring */
-	int numa_node;                  /* node to alloc ring memory on */
 };
 
 enum e1000_ring_flags_t {
@@ -374,7 +372,6 @@ struct igb_adapter {
 	int vf_rate_link_speed;
 	u32 rss_queues;
 	u32 wvbr;
-	int node;
 	u32 *shadow_vfta;
 
 #ifdef CONFIG_IGB_PTP
