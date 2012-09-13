0471bfd33de4481703aff6bcfdab55113d067f06
Shawn Guo <shawn.guo@linaro.org>
2012-09-13 21:18:39 +0800

    ARM: imx: include iim.h rather than mach/iim.h
    Rename mach-imx/include/mach/iim.h to mach-imx/iim.h, and update users
    to include iim.h rather than mach/iim.h.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-imx/cpu-imx31.c
+++ arch/arm/mach-imx/cpu-imx31.c
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/io.h>
 #include <mach/hardware.h>
-#include <mach/iim.h>
 
 #include "common.h"
+#include "iim.h"
 
 static int mx31_cpu_rev = -1;
 
