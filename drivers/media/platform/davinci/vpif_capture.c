cc45b7df264c65bd7465949e4be9e486bb494ae0
Lad, Prabhakar <prabhakar.lad@ti.com>
2012-09-14 10:22:24 -0300

    [media] davinci: vpif: capture/display: fix race condition
    channel_first_int[][] variable is used as a flag for the ISR,
    This flag was being set after enabling the interrupts, There
    where situations when the isr occurred even before the flag was set
    dues to which it was causing the application hang.
    This patch sets  channel_first_int[][] flag just before enabling the
    interrupt.
    
    Reported-by: David Oleszkiewicz <doleszki@adsyscontrols.com>
    Signed-off-by: Lad, Prabhakar <prabhakar.lad@ti.com>
    Signed-off-by: Manjunath Hadli <manjunath.hadli@ti.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/davinci/vpif_capture.c
+++ drivers/media/platform/davinci/vpif_capture.c
@@ -340,6 +340,7 @@ static int vpif_start_streaming(struct vb2_queue *vq, unsigned int count)
 	 * Set interrupt for both the fields in VPIF Register enable channel in
 	 * VPIF register
 	 */
+	channel_first_int[VPIF_VIDEO_INDEX][ch->channel_id] = 1;
 	if ((VPIF_CHANNEL0_VIDEO == ch->channel_id)) {
 		channel0_intr_assert();
 		channel0_intr_enable(1);
@@ -351,7 +352,6 @@ static int vpif_start_streaming(struct vb2_queue *vq, unsigned int count)
 		channel1_intr_enable(1);
 		enable_channel1(1);
 	}
-	channel_first_int[VPIF_VIDEO_INDEX][ch->channel_id] = 1;
 
 	return 0;
 }
