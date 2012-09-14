c85d726e52f25a8c9b1ad8bbf0ebd9a836c7b417
Heiko Carstens <heiko.carstens@de.ibm.com>
2012-09-14 13:45:24 +0200

    s390/process: add missing header include
    Add appropriate header file:
    
    arch/s390/kernel/process.c:327:15: warning: symbol 'arch_align_stack' was not declared.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/process.c
+++ arch/s390/kernel/process.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/processor.h>
 #include <asm/vtimer.h>
+#include <asm/exec.h>
 #include <asm/irq.h>
 #include <asm/nmi.h>
 #include <asm/smp.h>
