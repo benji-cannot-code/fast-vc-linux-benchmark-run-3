a76d22e98e89d278b7f2a01ff503ca7eeca5247c
Ian Abbott <abbotti@mev.co.uk>
2012-09-14 17:34:24 +0100

    staging: comedi: mite: replace printk() calls
    Replace the `printk()` calls in this module with `dev_...()` calls where
    possible or `pr_...()` calls otherwise.
    
    Rework the normally ifdefed out (by the `DEBUG_MITE` macro)
    `mite_dump_regs()` to dump register offsets instead of remapped
    addresses.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/mite.h
+++ drivers/staging/comedi/drivers/mite.h
@@ -33,9 +33,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCIMIO_COMPAT
 
 #ifdef DEBUG_MITE
-#define MDPRINTK(format, args...)	printk(format , ## args)
+#define MDPRINTK(format, args...)	pr_debug(format , ## args)
 #else
-#define MDPRINTK(format, args...)
+#define MDPRINTK(format, args...)	do { } while (0)
 #endif
 
 #define MAX_MITE_DMA_CHANNELS 8
