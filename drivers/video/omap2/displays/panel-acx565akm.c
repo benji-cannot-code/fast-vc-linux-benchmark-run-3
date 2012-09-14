4c94c2e6632c586f05decb4221f3b3978b5e7e75
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-09-14 13:20:22 +0300

    OMAPDSS: acx565akm: remove platform backlight calls
    acx565akm has support to call set_backlight/get_backlight in platform
    code. They are not used by any board, and thus can be removed from the
    driver.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Archit Taneja <archit@ti.com>

--- drivers/video/omap2/displays/panel-acx565akm.c
+++ drivers/video/omap2/displays/panel-acx565akm.c
@@ -337,8 +337,6 @@ static int acx565akm_bl_update_status(struct backlight_device *dev)
 	r = 0;
 	if (md->has_bc)
 		acx565akm_set_brightness(md, level);
-	else if (md->dssdev->set_backlight)
-		r = md->dssdev->set_backlight(md->dssdev, level);
 	else
 		r = -ENODEV;
 
@@ -353,7 +351,7 @@ static int acx565akm_bl_get_intensity(struct backlight_device *dev)
 
 	dev_dbg(&dev->dev, "%s\n", __func__);
 
-	if (!md->has_bc && md->dssdev->set_backlight == NULL)
+	if (!md->has_bc)
 		return -ENODEV;
 
 	if (dev->props.fb_blank == FB_BLANK_UNBLANK &&
@@ -565,8 +563,6 @@ static int acx_panel_probe(struct omap_dss_device *dssdev)
 
 	if (md->has_bc)
 		brightness = acx565akm_get_actual_brightness(md);
-	else if (dssdev->get_backlight)
-		brightness = dssdev->get_backlight(dssdev);
 	else
 		brightness = 0;
 
