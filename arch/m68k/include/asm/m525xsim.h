1345af0cc3c6282664fa85d3d6849651a9a4bd86
Greg Ungerer <gerg@uclinux.org>
2012-09-14 16:09:59 +1000

    m68knommu: make ColdFire Park and Assignment register definitions absolute addresses
    Make all definitions of the ColdFire MPARK and IRQ Assignment registers
    absolute addresses. Currently some are relative to the MBAR peripheral
    region.
    
    The various ColdFire parts use different methods to address the internal
    registers, some are absolute, some are relative to peripheral regions
    which can be mapped at different address ranges (such as the MBAR and IPSBAR
    registers). We don't want to deal with this in the code when we are
    accessing these registers, so make all register definitions the absolute
    address - factoring out whether it is an offset into a peripheral region.
    
    This makes them all consistently defined, and reduces the occasional bugs
    caused by inconsistent definition of the register addresses.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/asm/m525xsim.h
+++ arch/m68k/include/asm/m525xsim.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MCFSIM_SYPCR		(MCF_MBAR + 0x01)	/* System Protection */
 #define MCFSIM_SWIVR		(MCF_MBAR + 0x02)	/* SW Watchdog intr */
 #define MCFSIM_SWSR		(MCF_MBAR + 0x03)	/* SW Watchdog srv */
-#define MCFSIM_MPARK		0x0C		/* BUS Master Control Reg*/
+#define MCFSIM_MPARK		(MCF_MBAR + 0x0C)	/* BUS Master Ctrl */
 #define MCFSIM_IPR		(MCF_MBAR + 0x40)	/* Interrupt Pending */
 #define MCFSIM_IMR		(MCF_MBAR + 0x44)	/* Interrupt Mask */
 #define MCFSIM_ICR0		(MCF_MBAR + 0x4c)	/* Intr Ctrl reg 0 */
