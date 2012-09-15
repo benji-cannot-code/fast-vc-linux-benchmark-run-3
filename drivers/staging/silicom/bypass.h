a817e9b2cd9a656462b53715ff3b4f7a1d2a375d
Daniel Cotey <puff65537@bansheeslibrary.com>
2012-09-15 06:08:07 -0700

    Staging: silicom: bypass.h: checkpatch whitespace
    Remove trailing spaces, first chunk
    
    Signed-off-by: Daniel Cotey <puff65537@bansheeslibrary.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/silicom/bypass.h
+++ drivers/staging/silicom/bypass.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CMND_EN_MASK           0x02	/* BIT_1 */
 #define DIS_BYPASS_CAP_MASK    0x04	/* BIT_2    Bypass Cap is disable*/
 #define DFLT_PWRON_MASK        0x08	/* BIT_3 */
-#define BYPASS_OFF_MASK        0x10	/* BIT_4 */ 
+#define BYPASS_OFF_MASK        0x10	/* BIT_4 */
 #define BYPASS_FLAG_MASK       0x20	/* BIT_5 */
 #define STD_NIC_MASK           (DIS_BYPASS_CAP_MASK | BYPASS_OFF_MASK | DFLT_PWRON_MASK)
 #define WD_EXP_FLAG_MASK       0x40	/* BIT_6 */
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define STATUS_DISC_REG_ADDR    13
 #define WDTE_DISC_BPN_MASK      0x01	/* BIT_0    1 when wdt expired -> TAP, 0 - Bypass */
-#define STD_NIC_ON_MASK         0x02	/* BIT_1    */ 
+#define STD_NIC_ON_MASK         0x02	/* BIT_1    */
 #define DIS_DISC_CAP_MASK       0x04	/* BIT_2    TAP Cap is disable*/
 #define DFLT_PWRON_DISC_MASK    0x08	/* BIT_3    */
 #define DISC_OFF_MASK           0x10	/* BIT_4    */
