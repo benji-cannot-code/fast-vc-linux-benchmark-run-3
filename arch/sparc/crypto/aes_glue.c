c576b1a06ea955f205724fa7b90b13c69d17bb7f
David S. Miller <davem@davemloft.net>
2012-09-15 09:17:10 -0700

    sparc64: Add missing pr_fmt define to crypto opcode drivers.
    The hashes and crc32c had it, only the AES/DES/CAMELLIA drivers were
    missing it.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/crypto/aes_glue.c
+++ arch/sparc/crypto/aes_glue.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *    Copyright (c) 2010, Intel Corporation.
  */
 
+#define pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
+
 #include <linux/crypto.h>
 #include <linux/init.h>
 #include <linux/module.h>
--- arch/sparc/crypto/des_glue.c
+++ arch/sparc/crypto/des_glue.c
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2012 David S. Miller <davem@davemloft.net>
  */
 
+#define pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
+
 #include <linux/crypto.h>
 #include <linux/init.h>
 #include <linux/module.h>
