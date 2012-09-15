a3fc1e393145615fc208279797591bd0264efc7e
Shawn Guo <shawn.guo@linaro.org>
2012-09-15 14:31:14 +0800

    video: mx3fb: remove unneeded mach/hardware.h inclusion
    The inclusion of mach/hardware.h is not used by the driver at all.
    Remove it.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Cc: linux-fbdev@vger.kernel.org

--- drivers/video/mx3fb.c
+++ drivers/video/mx3fb.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma/ipu-dma.h>
 
 #include <linux/platform_data/dma-imx.h>
-#include <mach/hardware.h>
 #include <linux/platform_data/video-mx3fb.h>
 
 #include <asm/io.h>
