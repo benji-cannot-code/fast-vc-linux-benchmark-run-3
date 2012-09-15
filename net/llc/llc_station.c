74f5740d19e9b8652c74c69cc7058f5d8b051217
Ben Hutchings <ben@decadent.org.uk>
2012-09-15 17:11:40 +0000

    llc2: Remove explicit indexing of state action arrays
    These arrays are accessed by iteration in
    llc_exec_station_trans_actions().  There must not be any zero-filled
    gaps in them, so the explicit indices are pointless.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/llc/llc_station.c
+++ net/llc/llc_station.c
@@ -112,8 +112,8 @@ free:
 
 /* state transition for LLC_STATION_EV_RX_NULL_DSAP_XID_C event */
 static llc_station_action_t llc_stat_up_state_actions_2[] = {
-	[0] = llc_station_ac_send_xid_r,
-	[1] = NULL,
+	llc_station_ac_send_xid_r,
+	NULL,
 };
 
 static struct llc_station_state_trans llc_stat_up_state_trans_2 = {
@@ -123,8 +123,8 @@ static struct llc_station_state_trans llc_stat_up_state_trans_2 = {
 
 /* state transition for LLC_STATION_EV_RX_NULL_DSAP_TEST_C event */
 static llc_station_action_t llc_stat_up_state_actions_3[] = {
-	[0] = llc_station_ac_send_test_r,
-	[1] = NULL,
+	llc_station_ac_send_test_r,
+	NULL,
 };
 
 static struct llc_station_state_trans llc_stat_up_state_trans_3 = {
