d4a08200c25c579f90c17714bfe28b42cb2aa4b3
H Hartley Sweeten <hartleys@visionengravers.com>
2012-09-17 13:17:19 -0700

    staging: comedi: jr3_pci: quiet "Using plain interger as NULL pointer" noise
    Quiet some sparse warnings about:
    
    warning: Using plain integer as NULL pointer
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/jr3_pci.c
+++ drivers/staging/comedi/drivers/jr3_pci.c
@@ -367,7 +367,7 @@ static int read_idm_word(const u8 *data, size_t size, int *pos,
 			 unsigned int *val)
 {
 	int result = 0;
-	if (pos != 0 && val != 0) {
+	if (pos && val) {
 		/*  Skip over non hex */
 		for (; *pos < size && !isxdigit(data[*pos]); (*pos)++) {
 		}
@@ -877,7 +877,7 @@ static int jr3_pci_attach(struct comedi_device *dev,
 			p->maxdata_list[56] = 0xffff;
 			p->maxdata_list[57] = 0xffff;
 			/*  Channel specific range and maxdata */
-			dev->subdevices[i].range_table = 0;
+			dev->subdevices[i].range_table = NULL;
 			dev->subdevices[i].range_table_list =
 			    p->range_table_list;
 			dev->subdevices[i].maxdata = 0;
