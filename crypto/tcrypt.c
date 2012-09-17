21237dd4cf97eb44bd59baf657a5b7a4b83cc430
Suresh Siddha <suresh.b.siddha@intel.com>
2012-09-17 10:37:26 -0700

    crypto, tcrypt: remove local_bh_disable/enable() around local_irq_disable/enable()
    Ran into this while looking at some new crypto code using FPU
    hitting a WARN_ON_ONCE(!irq_fpu_usable()) in the kernel_fpu_begin()
    on a x86 kernel that uses the new eagerfpu model. In short, current eagerfpu
    changes return 0 for interrupted_kernel_fpu_idle() and the in_interrupt()
    thinks it is in the interrupt context because of the local_bh_disable().
    Thus resulting in the WARN_ON().
    
    Remove the local_bh_disable/enable() calls around the existing
    local_irq_disable/enable() calls. local_irq_disable/enable() already
    disables the BH.
    
     [ If there are any other legitimate users calling kernel_fpu_begin() from
       the process context but with BH disabled, then we can look into fixing the
       irq_fpu_usable() in future. ]
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Cc: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/tcrypt.c
+++ crypto/tcrypt.c
@@ -98,7 +98,6 @@ static int test_cipher_cycles(struct blkcipher_desc *desc, int enc,
 	int ret = 0;
 	int i;
 
-	local_bh_disable();
 	local_irq_disable();
 
 	/* Warm-up run. */
@@ -131,7 +130,6 @@ static int test_cipher_cycles(struct blkcipher_desc *desc, int enc,
 
 out:
 	local_irq_enable();
-	local_bh_enable();
 
 	if (ret == 0)
 		printk("1 operation in %lu cycles (%d bytes)\n",
@@ -301,7 +299,6 @@ static int test_hash_cycles_digest(struct hash_desc *desc,
 	int i;
 	int ret;
 
-	local_bh_disable();
 	local_irq_disable();
 
 	/* Warm-up run. */
@@ -328,7 +325,6 @@ static int test_hash_cycles_digest(struct hash_desc *desc,
 
 out:
 	local_irq_enable();
-	local_bh_enable();
 
 	if (ret)
 		return ret;
@@ -349,7 +345,6 @@ static int test_hash_cycles(struct hash_desc *desc, struct scatterlist *sg,
 	if (plen == blen)
 		return test_hash_cycles_digest(desc, sg, blen, out);
 
-	local_bh_disable();
 	local_irq_disable();
 
 	/* Warm-up run. */
@@ -392,7 +387,6 @@ static int test_hash_cycles(struct hash_desc *desc, struct scatterlist *sg,
 
 out:
 	local_irq_enable();
-	local_bh_enable();
 
 	if (ret)
 		return ret;
