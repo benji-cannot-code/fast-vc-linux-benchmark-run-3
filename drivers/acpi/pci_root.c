9f573d9b435bb5ed03372dddb6a3cb422397de9f
Taku Izumi <izumi.taku@jp.fujitsu.com>
2012-09-18 15:22:35 +0900

    PCI/ACPI: Pass acpi_pci_root to acpi_pci_drivers' add/remove interface
    This patch changes .add/.remove interfaces of acpi_pci_driver.
    In the current implementation acpi_handle is passed as a parameter
    of .add/.remove interface.  However, the acpi_pci_root structure
    contains more useful information than just the acpi_handle.  This
    enables us to avoid some useless lookups in each acpi_pci_driver.
    
    Note: This changes interfaces used by acpi_pci_register_driver(), an
    exported symbol.  This patch updates all the in-kernel users, but any
    out-of-kernel acpi_pci_register_driver() users will need updates.
    
    [bhelgaas: changelog]
    Signed-off-by: Taku Izumi <izumi.taku@jp.fujitsu.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/acpi/pci_root.c
+++ drivers/acpi/pci_root.c
@@ -88,7 +88,7 @@ int acpi_pci_register_driver(struct acpi_pci_driver *driver)
 	list_add_tail(&driver->node, &acpi_pci_drivers);
 	if (driver->add)
 		list_for_each_entry(root, &acpi_pci_roots, node) {
-			driver->add(root->device->handle);
+			driver->add(root);
 			n++;
 		}
 	mutex_unlock(&acpi_pci_root_lock);
@@ -105,7 +105,7 @@ void acpi_pci_unregister_driver(struct acpi_pci_driver *driver)
 	list_del(&driver->node);
 	if (driver->remove)
 		list_for_each_entry(root, &acpi_pci_roots, node)
-			driver->remove(root->device->handle);
+			driver->remove(root);
 	mutex_unlock(&acpi_pci_root_lock);
 }
 EXPORT_SYMBOL(acpi_pci_unregister_driver);
@@ -630,7 +630,7 @@ static int acpi_pci_root_start(struct acpi_device *device)
 	mutex_lock(&acpi_pci_root_lock);
 	list_for_each_entry(driver, &acpi_pci_drivers, node)
 		if (driver->add)
-			driver->add(device->handle);
+			driver->add(root);
 	mutex_unlock(&acpi_pci_root_lock);
 
 	pci_bus_add_devices(root->bus);
@@ -646,7 +646,7 @@ static int acpi_pci_root_remove(struct acpi_device *device, int type)
 	mutex_lock(&acpi_pci_root_lock);
 	list_for_each_entry(driver, &acpi_pci_drivers, node)
 		if (driver->remove)
-			driver->remove(root->device->handle);
+			driver->remove(root);
 	mutex_unlock(&acpi_pci_root_lock);
 
 	device_set_run_wake(root->bus->bridge, false);
