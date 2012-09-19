c041a6132639220fb2abb4c8d45685f09231dfd9
Stephen Warren <swarren@nvidia.com>
2012-09-19 14:19:02 -0600

    ARM: tegra: select SPARSE_IRQ
    SPARSE_IRQ is required for single zImage support.
    
    With this enabled, we can delete <mach/irqs.h>. This requires removing
    one unnecessary include of that file, and hard-coding the PCIe IRQ into
    the PCIe driver. This is a hack that will be dealt with as part of
    converting the PCIe driver into a true DT-supporting driver.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/pcie.c
+++ arch/arm/mach-tegra/pcie.c
@@ -44,6 +44,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board.h"
 #include "iomap.h"
 
+/* Hack - need to parse this from DT */
+#define INT_PCIE_INTR 130
+
 /* register definitions */
 #define AFI_OFFSET	0x3800
 #define PADS_OFFSET	0x3000
