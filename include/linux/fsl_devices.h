12ba3e5730c8412362d1971e0ce794158b8a023f
Ramneek Mehresh <ramneek.mehresh@freescale.com>
2012-09-19 14:18:45 +0530

    fsl/usb: Add support for USB controller version 2.4
    Add support for USB controller version 2.4
    
    Signed-off-by: Ramneek Mehresh <ramneek.mehresh@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/fsl_devices.h
+++ include/linux/fsl_devices.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FSL_USB_VER_OLD		0
 #define FSL_USB_VER_1_6		1
 #define FSL_USB_VER_2_2		2
+#define FSL_USB_VER_2_4		3
 
 #include <linux/types.h>
 
