6e53506d5a411a02cf1ae0465053e3785755b08b
Axel Lin <[mailto:axel.lin@gmail.com]>
2012-09-19 16:30:00 +0100

    HID: hid-sensor-hub: Fix sensor_hub_probe error handling
    Fix below issues:
    1. In the case of goto err_close, hid_hw_stop(hdev) is called twice. Fix it.
    2. If fails to allocate MFD device name, we also need to free all
       successfully allocated names in previous iterations.
    3. In sensor_hub_remove(), Call hid_hw_close() before hid_hw_stop().
    4. Adjust unnecessary change lines for hid_err.
    
    Signed-off-by: Axel Lin <axel.lin@gmail.com>
    Acked-by: Jiri Kosina <jkosina@suse.cz>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/hid/hid-sensor-hub.c
+++ drivers/hid/hid-sensor-hub.c
@@ -556,8 +556,7 @@ static int sensor_hub_probe(struct hid_device *hdev,
 						sizeof(struct mfd_cell),
 						GFP_KERNEL);
 	if (sd->hid_sensor_hub_client_devs == NULL) {
-		hid_err(hdev,
-			"Failed to allocate memory for mfd cells\n");
+		hid_err(hdev, "Failed to allocate memory for mfd cells\n");
 			ret = -ENOMEM;
 			goto err_close;
 	}
@@ -569,10 +568,9 @@ static int sensor_hub_probe(struct hid_device *hdev,
 			name = kasprintf(GFP_KERNEL, "HID-SENSOR-%x",
 						field->physical);
 			if (name  == NULL) {
-				hid_err(hdev,
-					"Failed MFD device name\n");
+				hid_err(hdev, "Failed MFD device name\n");
 					ret = -ENOMEM;
-					goto err_free_cells;
+					goto err_free_names;
 			}
 			sd->hid_sensor_hub_client_devs[
 				sd->hid_sensor_client_cnt].name = name;
@@ -596,10 +594,8 @@ static int sensor_hub_probe(struct hid_device *hdev,
 err_free_names:
 	for (i = 0; i < sd->hid_sensor_client_cnt ; ++i)
 		kfree(sd->hid_sensor_hub_client_devs[i].name);
-err_free_cells:
 	kfree(sd->hid_sensor_hub_client_devs);
 err_close:
-	hid_hw_stop(hdev);
 	hid_hw_close(hdev);
 err_stop_hw:
 	hid_hw_stop(hdev);
@@ -618,8 +614,8 @@ static void sensor_hub_remove(struct hid_device *hdev)
 	int i;
 
 	hid_dbg(hdev, " hardware removed\n");
-	hid_hw_stop(hdev);
 	hid_hw_close(hdev);
+	hid_hw_stop(hdev);
 	spin_lock_irqsave(&data->lock, flags);
 	if (data->pending.status)
 		complete(&data->pending.ready);
