7f9104c2139ebc2462e4cb255019c9a43809b96e
Richard Kuo <rkuo@codeaurora.org>
2012-09-19 16:22:02 -0500

    Hexagon: Copyright marking changes
    Code Aurora Forum (CAF) is becoming a part of Linux Foundation Labs.
    
    Signed-off-by: Richard Kuo <rkuo@codeaurora.org>

--- arch/hexagon/include/asm/atomic.h
+++ arch/hexagon/include/asm/atomic.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Atomic operations for the Hexagon architecture
  *
- * Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2010-2011, The Linux Foundation. All rights reserved.
  *
  *
  * This program is free software; you can redistribute it and/or modify
