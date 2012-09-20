6ab43cf9694320bb442be15009ed3fd788588de2
Tony Lindgren <tony@atomide.com>
2012-09-20 11:42:10 -0700

    ARM: OMAP2+: Make l4_3xxx.h local
    This can be local to mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_3xxx_data.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/dma.h>
 #include <plat/serial.h>
 #include "l3_3xxx.h"
-#include <plat/l4_3xxx.h>
+#include "l4_3xxx.h"
 #include <plat/i2c.h>
 #include <plat/mmc.h>
 #include <linux/platform_data/asoc-ti-mcbsp.h>
