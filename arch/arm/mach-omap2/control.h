cf05e57ab1aba17016a1a7e8fd12af765f0be8ae
Tony Lindgren <tony@atomide.com>
2012-09-20 11:41:04 -0700

    ARM: OMAP2+: Make ctrl_module_pad_wkup_44xx.h local
    This can be local to mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/control.h
+++ arch/arm/mach-omap2/control.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ctrl_module_core_44xx.h"
 #include <mach/ctrl_module_wkup_44xx.h>
 #include "ctrl_module_pad_core_44xx.h"
-#include <mach/ctrl_module_pad_wkup_44xx.h>
+#include "ctrl_module_pad_wkup_44xx.h"
 
 #include "am33xx.h"
 
