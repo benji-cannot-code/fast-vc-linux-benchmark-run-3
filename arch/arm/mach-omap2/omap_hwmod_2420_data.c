fa38ab7d19e99beceb973bdc13aec4ccbe3a109b
Tony Lindgren <tony@atomide.com>
2012-09-20 11:42:02 -0700

    ARM: OMAP2+: Make l3_2xxx.h local
    This can be local to mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap_hwmod_2420_data.c
+++ arch/arm/mach-omap2/omap_hwmod_2420_data.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/serial.h>
 #include <plat/i2c.h>
 #include <plat/dmtimer.h>
-#include <plat/l3_2xxx.h>
+#include "l3_2xxx.h"
 #include <plat/l4_2xxx.h>
 #include <plat/mmc.h>
 
--- arch/arm/mach-omap2/omap_hwmod_2430_data.c
+++ arch/arm/mach-omap2/omap_hwmod_2430_data.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/i2c.h>
 #include <plat/dmtimer.h>
 #include <plat/mmc.h>
-#include <plat/l3_2xxx.h>
+#include "l3_2xxx.h"
 
 #include "soc.h"
 #include "omap_hwmod_common_data.h"
