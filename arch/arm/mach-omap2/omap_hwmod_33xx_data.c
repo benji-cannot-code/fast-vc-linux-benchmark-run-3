a75bfc3e9ad9fcadfd8eb0e3d4b25c5978196ee6
Kevin Hilman <khilman@deeprootsystems.com>
2012-09-20 09:38:14 -0700

    ARM: OMAP: AM33xx hwmod: fixup SPI after platform_data move
    AM33xx hwmod data includes "mcspi.h" which has now been moved after
    the platform_data move.  Fix it.
    
    Signed-off-by: Kevin Hilman <khilman@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-omap2/omap_hwmod_33xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_33xx_data.c
@@ -18,9 +18,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/omap_hwmod.h>
 #include <plat/cpu.h>
 #include <linux/platform_data/gpio-omap.h>
+#include <linux/platform_data/spi-omap2-mcspi.h>
 #include <plat/dma.h>
 #include <plat/mmc.h>
-#include <plat/mcspi.h>
 #include <plat/i2c.h>
 
 #include "omap_hwmod_common_data.h"
