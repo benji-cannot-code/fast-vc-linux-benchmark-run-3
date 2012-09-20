912d9b00613eda0891517166a8ca4e92b6999969
Tomasz Figa <t.figa@samsung.com>
2012-09-21 07:33:52 +0900

    pinctrl: exynos: Fix typos in gpio/wkup _irq_mask
    To mask GPIO/wakeup IRQ, the corresponding bit in mask register has to
    be set.
    
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- drivers/pinctrl/pinctrl-exynos.c
+++ drivers/pinctrl/pinctrl-exynos.c
@@ -59,7 +59,7 @@ static void exynos_gpio_irq_mask(struct irq_data *irqd)
 	unsigned long mask;
 
 	mask = readl(d->virt_base + reg_mask);
-	mask |= ~(1 << edata->pin);
+	mask |= 1 << edata->pin;
 	writel(mask, d->virt_base + reg_mask);
 }
 
@@ -291,7 +291,7 @@ static void exynos_wkup_irq_mask(struct irq_data *irqd)
 	unsigned long mask;
 
 	mask = readl(d->virt_base + reg_mask);
-	mask &= ~(1 << pin);
+	mask |= 1 << pin;
 	writel(mask, d->virt_base + reg_mask);
 }
 
