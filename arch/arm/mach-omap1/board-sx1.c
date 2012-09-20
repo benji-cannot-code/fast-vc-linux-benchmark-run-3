b5933ad69b96e29131cf925babb68c132621e0d0
Tony Lindgren <tony@atomide.com>
2012-09-20 11:41:22 -0700

    ARM: OMAP1: Move board-sx1.h from plat to mach
    This is only used by omap1.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/board-sx1.c
+++ arch/arm/mach-omap1/board-sx1.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/dma.h>
 #include <plat/irda.h>
 #include <plat/tc.h>
-#include <plat/board-sx1.h>
+#include <mach/board-sx1.h>
 
 #include <mach/hardware.h>
 #include <mach/usb.h>
