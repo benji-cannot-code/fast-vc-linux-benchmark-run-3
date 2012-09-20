777163c87816b2e8065837b24bbb9f4d3a8614df
Tony Lindgren <tony@atomide.com>
2012-09-20 11:40:56 -0700

    ARM: OMAP2+: Make board-rx51.h local
    This can be local to mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-rx51-peripherals.c
+++ arch/arm/mach-omap2/board-rx51-peripherals.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/gpmc.h>
 #include <plat/gpmc-smc91x.h>
 
-#include <mach/board-rx51.h>
+#include "board-rx51.h"
 
 #include <sound/tlv320aic3x.h>
 #include <sound/tpa6130a2-plat.h>
