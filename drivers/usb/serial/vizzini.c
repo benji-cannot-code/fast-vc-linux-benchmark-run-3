1eec9ac4fa29a705bdb116b96258ed987172261f
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-09-21 16:46:38 +0100

    USB: serial: vizzini: remove outdated comment
    I fixed up all of the #ifdef stuff, so remove the comment saying it
    still needs to be removed.
    
    Reported-by: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Cc: Rob Duncan <rob.duncan@exar.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/serial/vizzini.c
+++ drivers/usb/serial/vizzini.c
@@ -38,15 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-/* This version of the Linux driver source contains a number of
-   abominable conditional compilation sections to manage the API
-   changes between kernel versions 2.6.18, 2.6.25, and the latest
-   (currently 2.6.27).  At some point we'll hand a version of this
-   driver off to the mainline Linux source tree, and we'll strip all
-   these sections out.  For now it makes it much easier to keep it all
-   in sync while the driver is being developed. */
-
-
 #define DRIVER_VERSION "v.0.76"
 #define DRIVER_AUTHOR "Rob Duncan <rob.duncan@exar.com>"
 #define DRIVER_DESC "USB Driver for Vizzini USB serial port"
