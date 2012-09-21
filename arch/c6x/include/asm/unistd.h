f84f42ea8fa6894c60a61b2b7a59197449d6e567
Mark Salter <msalter@redhat.com>
2012-09-21 12:26:38 -0400

    c6x: switch to generic kernel_execve
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/c6x/include/asm/unistd.h
+++ arch/c6x/include/asm/unistd.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #if !defined(_ASM_C6X_UNISTD_H) || defined(__SYSCALL)
 #define _ASM_C6X_UNISTD_H
 
+#define __ARCH_WANT_KERNEL_EXECVE
+
 /* Use the standard ABI for syscalls. */
 #include <asm-generic/unistd.h>
 
