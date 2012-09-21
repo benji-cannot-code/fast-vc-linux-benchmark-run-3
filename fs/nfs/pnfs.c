332a97fcaaff6d16bb9cdd3d301437a4fdba4bea
Trond Myklebust <Trond.Myklebust@netapp.com>
2012-09-21 16:37:02 -0400

    NFSv4.1: Remove the NFS_LAYOUT_RETURNED state
    It serves no purpose that the test for whether or not we have valid
    layout segments doesn't already serve.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -693,7 +693,7 @@ _pnfs_return_layout(struct inode *ino)
 
 	spin_lock(&ino->i_lock);
 	lo = nfsi->layout;
-	if (!lo || pnfs_test_layout_returned(lo)) {
+	if (!lo) {
 		spin_unlock(&ino->i_lock);
 		dprintk("NFS: %s no layout to return\n", __func__);
 		goto out;
@@ -711,7 +711,6 @@ _pnfs_return_layout(struct inode *ino)
 		goto out;
 	}
 	lo->plh_block_lgets++;
-	pnfs_mark_layout_returned(lo);
 	spin_unlock(&ino->i_lock);
 	pnfs_free_lseg_list(&tmp_list);
 
@@ -722,7 +721,6 @@ _pnfs_return_layout(struct inode *ino)
 		status = -ENOMEM;
 		pnfs_layout_io_set_failed(lo, IOMODE_RW);
 		pnfs_layout_io_set_failed(lo, IOMODE_READ);
-		pnfs_clear_layout_returned(lo);
 		pnfs_put_layout_hdr(lo);
 		goto out;
 	}
@@ -1111,9 +1109,6 @@ pnfs_update_layout(struct inode *ino,
 
 	if (list_empty(&lo->plh_segs))
 		first = true;
-
-	/* Enable LAYOUTRETURNs */
-	pnfs_clear_layout_returned(lo);
 
 	spin_unlock(&ino->i_lock);
 	if (first) {
