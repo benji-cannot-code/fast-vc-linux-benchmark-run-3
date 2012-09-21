bd7b8c7d23c92b9bcd68437329c001296b9f2bae
Ian Minett <ian_minett@creativelabs.com>
2012-09-20 20:29:14 -0700

    ALSA: hda - Add CA0132 register definitions file
    Signed-off-by: Ian Minett <ian_minett@creativelabs.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_ca0132.c
+++ sound/pci/hda/patch_ca0132.c
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hda_local.h"
 #include "hda_auto_parser.h"
 
+#include "ca0132_regs.h"
+
 #define WIDGET_CHIP_CTRL      0x15
 #define WIDGET_DSP_CTRL       0x16
 
