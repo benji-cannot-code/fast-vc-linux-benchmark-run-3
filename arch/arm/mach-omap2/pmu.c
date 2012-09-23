ac8f1f63ccc53b101b10a124d6f678d0098e89f0
Jon Hunter <jon-hunter@ti.com>
2012-09-23 17:28:30 -0600

    ARM: OMAP2+: PMU: Add runtime PM support
    The original implementation of this patch was done by Ming Lei for PMU on OMAP4
    [1]. Since then the PM runtime calls have been moved into the ARM PMU code and
    this greatly simplifies the changes.
    
    The another differnce since the original version, is that it is no longer
    necessary to call pm_runtime_get/put during the PMU initialisation was we are no
    longer accessing the hardware at this stage.
    
    By adding runtime PM support, we can ensure that the appropriate power and clock
    domains are kept on while PMU is being used.
    
    [1] http://lists.infradead.org/pipermail/linux-arm-kernel/2011-November/074153.html
    
    Cc: Ming Lei <ming.lei@canonical.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Benoit Cousson <b-cousson@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>
    Cc: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/pmu.c
+++ arch/arm/mach-omap2/pmu.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  */
+#include <linux/pm_runtime.h>
 
 #include <asm/pmu.h>
 
@@ -54,7 +55,12 @@ static int __init omap2_init_pmu(unsigned oh_num, char *oh_names[])
 	WARN(IS_ERR(omap_pmu_dev), "Can't build omap_device for %s.\n",
 	     dev_name);
 
-	return IS_ERR(omap_pmu_dev) ? PTR_ERR(omap_pmu_dev) : 0;
+	if (IS_ERR(omap_pmu_dev))
+		return PTR_ERR(omap_pmu_dev);
+
+	pm_runtime_enable(&omap_pmu_dev->dev);
+
+	return 0;
 }
 
 static int __init omap_init_pmu(void)
