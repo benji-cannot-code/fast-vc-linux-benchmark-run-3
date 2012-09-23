fa14cd3a60092edff6d333eb147c70022bac90c2
Afzal Mohammed <afzal@ti.com>
2012-09-23 17:28:24 -0600

    ARM: OMAP2+: gpmc: Adapt to HWMOD
    Create API for platforms to adapt GPMC to HWMOD
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>
    Reviewed-by: Jon Hunter <jon-hunter@ti.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/gpmc.c
+++ arch/arm/mach-omap2/gpmc.c
@@ -32,9 +32,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/cpu.h>
 #include <plat/gpmc.h>
 #include <plat/sdrc.h>
+#include <plat/omap_device.h>
 
 #include "soc.h"
 #include "common.h"
+
+#define	DEVICE_NAME		"omap-gpmc"
 
 /* GPMC register offsets */
 #define GPMC_REVISION		0x00
@@ -898,6 +901,25 @@ static int __init gpmc_init(void)
 	return ret;
 }
 postcore_initcall(gpmc_init);
+
+static int __init omap_gpmc_init(void)
+{
+	struct omap_hwmod *oh;
+	struct platform_device *pdev;
+	char *oh_name = "gpmc";
+
+	oh = omap_hwmod_lookup(oh_name);
+	if (!oh) {
+		pr_err("Could not look up %s\n", oh_name);
+		return -ENODEV;
+	}
+
+	pdev = omap_device_build(DEVICE_NAME, -1, oh, NULL, 0, NULL, 0, 0);
+	WARN(IS_ERR(pdev), "could not build omap_device for %s\n", oh_name);
+
+	return IS_ERR(pdev) ? PTR_ERR(pdev) : 0;
+}
+postcore_initcall(omap_gpmc_init);
 
 static irqreturn_t gpmc_handle_irq(int irq, void *dev)
 {
