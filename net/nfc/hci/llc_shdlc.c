eccf1598ababea343c4e25f75cd10c970249c843
Waldemar Rymarkiewicz <waldemar.rymarkiewicz@tieto.com>
2012-09-24 08:07:07 +0200

    NFC: Fix typo negociating -> negotiating
    Signed-off-by: Waldemar Rymarkiewicz <waldemar.rymarkiewicz@tieto.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- net/nfc/hci/llc_shdlc.c
+++ net/nfc/hci/llc_shdlc.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum shdlc_state {
 	SHDLC_DISCONNECTED = 0,
 	SHDLC_CONNECTING = 1,
-	SHDLC_NEGOCIATING = 2,
+	SHDLC_NEGOTIATING = 2,
 	SHDLC_HALF_CONNECTED = 3,
 	SHDLC_CONNECTED = 4
 };
@@ -417,7 +417,7 @@ static void llc_shdlc_rcv_u_frame(struct llc_shdlc *shdlc,
 	switch (u_frame_modifier) {
 	case U_FRAME_RSET:
 		switch (shdlc->state) {
-		case SHDLC_NEGOCIATING:
+		case SHDLC_NEGOTIATING:
 		case SHDLC_CONNECTING:
 			/*
 			 * We sent RSET, but chip wants to negociate or we
@@ -458,7 +458,7 @@ static void llc_shdlc_rcv_u_frame(struct llc_shdlc *shdlc,
 	case U_FRAME_UA:
 		if ((shdlc->state == SHDLC_CONNECTING &&
 		     shdlc->connect_tries > 0) ||
-		    (shdlc->state == SHDLC_NEGOCIATING)) {
+		    (shdlc->state == SHDLC_NEGOTIATING)) {
 			llc_shdlc_connect_complete(shdlc, 0);
 			shdlc->state = SHDLC_CONNECTED;
 		}
@@ -641,10 +641,10 @@ static void llc_shdlc_sm_work(struct work_struct *work)
 			mod_timer(&shdlc->connect_timer, jiffies +
 				  msecs_to_jiffies(SHDLC_CONNECT_VALUE_MS));
 
-			shdlc->state = SHDLC_NEGOCIATING;
+			shdlc->state = SHDLC_NEGOTIATING;
 		}
 		break;
-	case SHDLC_NEGOCIATING:
+	case SHDLC_NEGOTIATING:
 		if (timer_pending(&shdlc->connect_timer) == 0) {
 			shdlc->state = SHDLC_CONNECTING;
 			queue_work(system_nrt_wq, &shdlc->sm_work);
