6e569bebe6a86cd492ed08dfa6e74861de000388
Kevin McKinney <klmckinney1@gmail.com>
2012-09-23 23:07:17 -0400

    Staging: bcm: Remove typedef for _stCPacketClassificationRuleSI and call directly.
    This patch removes typedef for
    _stCPacketClassificationRuleSI, changes the
    name of the struct to bcm_packet_class_rules,
    and updates the comments appropriately . In
    addition, any calls to typedefs
    "CCPacketClassificationRuleSI,
    stCPacketClassificationRuleSI,
    or *pstCPacketClassificationRuleSI" are changed
    to call the struct directly.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Acked-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/cntrl_SignalingInterface.h
+++ drivers/staging/bcm/cntrl_SignalingInterface.h
@@ -35,12 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NUM_ETHERTYPE_BYTES		3
 #define NUM_IPV6_FLOWLABLE_BYTES	3
 
-/*
- * structure Definitions
- *
- * brief class cCPacketClassificationRule
- */
-struct _stCPacketClassificationRuleSI {
+struct bcm_packet_class_rules {
 	/* 16bit UserPriority Of The Service Flow */
 	B_UINT16 u16UserPriority;
 	/* 16bit VLANID Of The Service Flow */
@@ -97,7 +92,6 @@ struct _stCPacketClassificationRuleSI {
 	B_UINT8 u8ClassifierActionRule;
 	B_UINT16 u16ValidityBitMap;
 };
-typedef struct _stCPacketClassificationRuleSI CCPacketClassificationRuleSI, stCPacketClassificationRuleSI, *pstCPacketClassificationRuleSI;
 
 struct bcm_phs_rules {
 	/* 8bit PHS Index Of The Service Flow */
@@ -128,8 +122,8 @@ struct bcm_convergence_types {
 	B_UINT8 u8PhsDSCAction;
 	/* 16bit Padding */
 	B_UINT8 u8Padding[2];
-	/* brief class cCPacketClassificationRule */
-	stCPacketClassificationRuleSI cCPacketClassificationRule;
+	/* Packet classification rules structure */
+	struct bcm_packet_class_rules cCPacketClassificationRule;
 	/* Payload header suppression rules structure */
 	struct bcm_phs_rules cPhsRule;
 };
