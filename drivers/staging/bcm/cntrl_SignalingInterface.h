059edfc9dd2e00f61eb6dea66b6d335c207d3359
Kevin McKinney <klmckinney1@gmail.com>
2012-09-23 23:07:13 -0400

    Staging: bcm: Remove typedef for _stLocalSFAddRequest and call directly.
    This patch removes typedef for _stLocalSFAddRequest,
    and changes the name of the struct to
    bcm_add_request. In addition, any
    calls to typedefs "stLocalSFAddRequest or
    *pstLocalSFAddRequest" are changed to
    call the struct directly.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Acked-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/cntrl_SignalingInterface.h
+++ drivers/staging/bcm/cntrl_SignalingInterface.h
@@ -261,10 +261,7 @@ typedef struct _stServiceFlowParamSI {
 } stServiceFlowParamSI, *pstServiceFlowParamSI;
 typedef stServiceFlowParamSI CServiceFlowParamSI;
 
-/*
- * structure stLocalSFAddRequest
- */
-typedef struct _stLocalSFAddRequest {
+struct bcm_add_request {
 	B_UINT8 u8Type;	/* < Type */
 	B_UINT8 eConnectionDir; /* < Connection direction */
 	/* brief 16 bit TID */
@@ -275,8 +272,7 @@ typedef struct _stLocalSFAddRequest {
 	B_UINT16 u16VCID; /* < 16bit VCID */
 	/* brief structure ParameterSet */
 	stServiceFlowParamSI *psfParameterSet; /* < structure ParameterSet */
-
-} stLocalSFAddRequest, *pstLocalSFAddRequest;
+};
 
 struct bcm_add_indication {
 	B_UINT8 u8Type;	/* < Type */
