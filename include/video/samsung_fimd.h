da283ba409b0e5ce5784d18087e7fe68cfdfffda
Jingoo Han <jg1.han@samsung.com>
2012-09-25 16:43:16 +0900

    video: s3c-fb: move the address definition for VIDOSD register
    The address definition for VIDTCON VIDOSD is moved to right
    place.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>

--- include/video/samsung_fimd.h
+++ include/video/samsung_fimd.h
@@ -238,6 +238,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Local input channels (windows 0-2) */
 #define SHADOWCON_CHx_LOCAL_ENABLE(_win)	(1 << (5 + (_win)))
 
+/* VIDOSDx */
+
+#define VIDOSD_BASE				(0x40)
 #define VIDOSDxA_TOPLEFT_X_E(_x)		((((_x) & 0x800) >> 11) << 23)
 #define VIDOSDxA_TOPLEFT_X_MASK			(0x7ff << 11)
 #define VIDOSDxA_TOPLEFT_X_SHIFT		(11)
@@ -408,10 +411,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define S3C_FB_MAX_WIN (5)  /* number of hardware windows available. */
 #define VIDCON1_FSTATUS_EVEN	(1 << 15)
-
-/* OSD1 and OSD4 do not have register D */
-
-#define VIDOSD_BASE				(0x40)
 
 #define VIDINTCON0				(0x130)
 
