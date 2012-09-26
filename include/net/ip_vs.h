4fcf9838e821e2b2b52c295295aaf4898858d634
Jesper Dangaard Brouer <brouer@redhat.com>
2012-09-26 14:06:20 +0200

    ipvs: Use config macro IS_ENABLED()
    Cleanup patch.
    
    Use the IS_ENABLED macro, instead of having to check
    both the build and the module CONFIG_ option.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- include/net/ip_vs.h
+++ include/net/ip_vs.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ip.h>
 #include <linux/ipv6.h>			/* for struct ipv6hdr */
 #include <net/ipv6.h>
-#if defined(CONFIG_NF_CONNTRACK) || defined(CONFIG_NF_CONNTRACK_MODULE)
+#if IS_ENABLED(CONFIG_NF_CONNTRACK)
 #include <net/netfilter/nf_conntrack.h>
 #endif
 #include <net/net_namespace.h>		/* Netw namespace */
