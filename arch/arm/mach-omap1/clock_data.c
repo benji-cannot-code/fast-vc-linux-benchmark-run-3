14f3afad74580b5944d285bfa9ad8433fa42ccaa
Paul Walmsley <paul@pwsan.com>
2012-09-27 10:33:35 -0600

    ARM: OMAP: clock: split plat/clkdev_omap.h into OMAP1/2 files
    To facilitate the ARM single image work, split
    arch/arm/plat-omap/include/plat/clkdev_omap.h into the
    arch/arm/mach-omap1/clock.h and arch/arm/mach-omap2/clock.h files.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/clock_data.c
+++ arch/arm/mach-omap1/clock_data.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>  /* for machine_is_* */
 
 #include <plat/cpu.h>
-#include <plat/clkdev_omap.h>
 
 #include <mach/hardware.h>
 #include <mach/usb.h>   /* for OTG_BASE */
--- arch/arm/mach-omap2/clock2430_data.c
+++ arch/arm/mach-omap2/clock2430_data.c
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/list.h>
 
-#include <plat/clkdev_omap.h>
-
 #include "soc.h"
 #include "iomap.h"
 #include "clock.h"
