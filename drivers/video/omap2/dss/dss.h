dbe37db9542254c7b0c6a910edf54c8805016c44
Chandrabhanu Mahapatra <cmahapatra@ti.com>
2012-09-27 16:52:17 +0530

    OMAPDSS: Move definition of DEBUG flag to Makefile
    In OMAPDSS the DEBUG flag is set only after the OMAPDSS module is called, for
    which the debugging capabilities are available only after its proper
    initialization. As a result of which tracking of bugs prior to or during initial
    process becomes difficult. So, the definition of DEBUG is being moved to the
    corresponding Makefile.
    
    Signed-off-by: Chandrabhanu Mahapatra <cmahapatra@ti.com>
    Reviewed-by: Sumit Semwal <sumit.semwal@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dss.h
+++ drivers/video/omap2/dss/dss.h
@@ -24,10 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __OMAP2_DSS_H
 #define __OMAP2_DSS_H
 
-#ifdef CONFIG_OMAP2_DSS_DEBUG_SUPPORT
-#define DEBUG
-#endif
-
 #ifdef DEBUG
 extern bool dss_debug;
 #ifdef DSS_SUBSYS_NAME
