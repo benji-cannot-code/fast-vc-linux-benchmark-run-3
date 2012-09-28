6448a16db9e1d6452ea2b653387c714a0800b9a4
James Bottomley <JBottomley@Parallels.com>
2012-09-28 11:54:48 +0100

    [PARISC] asm: redo generic includes
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- arch/parisc/include/asm/user.h
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* This file should not exist, but lots of generic code still includes
-   it. It's a hangover from old a.out days and the traditional core
-   dump format.  We are ELF-only, and so are our core dumps.  If we
-   need to support HP/UX core format then we'll do it here
-   eventually. */
--- arch/parisc/include/asm/vga.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef __ASM_PARISC_VGA_H__
-#define __ASM_PARISC_VGA_H__
-
-/* nothing */
-
-#endif /* __ASM_PARISC_VGA_H__ */
