38991f57cb8772241c42c2265d647bc42cd03b77
Jon Hunter <jon-hunter@ti.com>
2012-09-28 11:34:49 -0500

    ARM: OMAP: Add platform data header for DMTIMERs
    Move definition of dmtimer platform data structure in to its own header
    under <linux/platform_data>.
    
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>

--- arch/arm/mach-omap2/timer.c
+++ arch/arm/mach-omap2/timer.c
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
+#include <linux/platform_device.h>
+#include <linux/platform_data/dmtimer-omap.h>
 
 #include <asm/mach/time.h>
 #include <asm/smp_twd.h>
