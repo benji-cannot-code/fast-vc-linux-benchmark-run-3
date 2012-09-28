d1c8b91e1bb66f1f7c8561aa603b12507d5bcdbb
Steve Glendinning <steve.glendinning@shawell.net>
2012-09-28 00:07:12 +0000

    smsc95xx: add wol magic packet support
    This patch enables wake from system suspend on magic packet.
    
    Patch updated to replace BUG_ON with WARN_ON_ONCE and return.
    
    Signed-off-by: Steve Glendinning <steve.glendinning@shawell.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/smsc95xx.h
+++ drivers/net/usb/smsc95xx.h
@@ -206,6 +206,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WUFF				(0x128)
 
 #define WUCSR				(0x12C)
+#define WUCSR_GUE_			(0x00000200)
+#define WUCSR_WUFR_			(0x00000040)
+#define WUCSR_MPR_			(0x00000020)
+#define WUCSR_WAKE_EN_			(0x00000004)
+#define WUCSR_MPEN_			(0x00000002)
 
 #define COE_CR				(0x130)
 #define Tx_COE_EN_			(0x00010000)
