2d960b0f04c8886aba55d82396e95262f5081316
Michael Krufky <mkrufky@linuxtv.org>
2012-09-29 16:06:23 -0300

    [media] tda18271: make 'low-power standby mode after attach' multi-instance safe
    Ensure that unnecessary features are powered down at the end of the
    attach() function on attach of the tuner's first instance. If the
    configuration requires the loop thru or xtout features, they will
    remain enabled.
    This must *only* be done after attaching the first instance of the tuner.
    If there are multiple instances of the tuner, the bridge driver will need
    to maintain power managament by itself.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/tuners/tda18271-fe.c
+++ drivers/media/tuners/tda18271-fe.c
@@ -1286,6 +1286,10 @@ struct dvb_frontend *tda18271_attach(struct dvb_frontend *fe, u8 addr,
 		    (priv->id == TDA18271HDC2))
 			tda18271c2_rf_cal_init(fe);
 
+		/* enter standby mode, with required output features enabled */
+		ret = tda18271_toggle_output(fe, 1);
+		tda_fail(ret);
+
 		mutex_unlock(&priv->lock);
 		break;
 	default:
@@ -1323,9 +1327,6 @@ struct dvb_frontend *tda18271_attach(struct dvb_frontend *fe, u8 addr,
 
 	if (tda18271_debug & (DBG_MAP | DBG_ADV))
 		tda18271_dump_std_map(fe);
-
-	ret = tda18271_sleep(fe);
-	tda_fail(ret);
 
 	return fe;
 fail:
