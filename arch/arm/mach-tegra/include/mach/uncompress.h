b426aca5f793da08fdf442c60a2131d5ab4641f2
Stephen Warren <swarren@nvidia.com>
2012-10-01 15:33:20 -0600

    ARM: tegra: decouple uncompress.h and debug-macro.S
    Prior to this change, Tegra's debug-macro.S relied on uncompress.h having
    determined which UART to use, and whether it was safe to use the UART
    (i.e. is it not in reset, and is clocked). This determination was
    communicated from uncompress.h to debug-macro.S using a few bytes of
    Tegra's IRAM (an on-SoC RAM). This had the disadvantage that uncompress.h
    was a required part of the kernel boot process; booting a non-compressed
    kernel would not allow earlyprintk to operate.
    
    This change duplicates the UART selection and validation logic into
    debug-macro.S so that the reliance on uncompress.h is removed.
    
    This also helps out with single-zImage work, since there is currently no
    support for using any uncompress.h with single-zImage.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/include/mach/uncompress.h
+++ arch/arm/mach-tegra/include/mach/uncompress.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial_reg.h>
 
 #include "../../iomap.h"
-#include "../../irammap.h"
 
 #define BIT(x) (1 << (x))
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
@@ -51,17 +50,6 @@ static void putc(int c)
 
 static inline void flush(void)
 {
-}
-
-static inline void save_uart_address(void)
-{
-	u32 *buf = (u32 *)(TEGRA_IRAM_BASE + TEGRA_IRAM_DEBUG_UART_OFFSET);
-
-	if (uart) {
-		buf[0] = TEGRA_IRAM_DEBUG_UART_COOKIE;
-		buf[1] = (u32)uart;
-	} else
-		buf[0] = 0;
 }
 
 static const struct {
@@ -170,7 +158,6 @@ static inline void arch_decomp_setup(void)
 	else
 		uart = (volatile u8 *)uarts[uart_id].base;
 
-	save_uart_address();
 	if (uart == NULL)
 		return;
 
