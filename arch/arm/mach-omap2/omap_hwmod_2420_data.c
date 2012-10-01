d1d5bd04529d5c9e10c4c9f604a5ded92a1c7bdb
Jon Hunter <jon-hunter@ti.com>
2012-10-01 15:12:29 -0500

    ARM: OMAP: Remove unnecessary inclusion of dmtimer.h
    Some source files are including dmtimer.h but not actually using any dmtimer
    definitions or functions. Therefore, remove the inclusion dmtimer.h from these
    source files.
    
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>
    Acked-by: Omar Ramirez Luna <omar.luna@linaro.org>

--- arch/arm/mach-omap2/omap_hwmod_2420_data.c
+++ arch/arm/mach-omap2/omap_hwmod_2420_data.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/spi-omap2-mcspi.h>
 
 #include <plat-omap/dma-omap.h>
-#include <plat/dmtimer.h>
 
 #include "omap_hwmod.h"
 #include "l3_2xxx.h"
