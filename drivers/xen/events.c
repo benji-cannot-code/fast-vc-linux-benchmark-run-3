d3b2c4101b0a64ed7157c170312d4bf87ed8906f
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2012-10-02 15:57:57 +0100

    xen: mark xen_init_IRQ __init
    xen_init_IRQ should be marked __init because it calls other functions
    marked __init and is always called by functions marked __init (on both
    x86 and arm).
    
    Also remove the unused EXPORT_SYMBOL_GPL(xen_init_IRQ).
    
    Both changes were introduced by "xen/arm: receive Xen events on ARM".
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/events.c
+++ drivers/xen/events.c
@@ -1803,7 +1803,7 @@ void xen_callback_vector(void)
 void xen_callback_vector(void) {}
 #endif
 
-void xen_init_IRQ(void)
+void __init xen_init_IRQ(void)
 {
 	int i;
 
@@ -1847,4 +1847,3 @@ void xen_init_IRQ(void)
 	}
 #endif
 }
-EXPORT_SYMBOL_GPL(xen_init_IRQ);
