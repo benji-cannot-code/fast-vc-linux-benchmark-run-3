b67997aefc7655a40cd76f7a9f019cee4696e2b1
Tony Breeds <tony@bakeyournoodle.com>
2012-10-02 15:52:41 +0000

    powerpc: Add asm/debug.h to get powerpc_debugfs_root
    Since the "Disintegrate asm/system.h for PowerPC"
    (ae3a197e3d0bfe3f4bf1693723e82dc018c096f3) This has been failing when
    DEBUG is #defined.
    
    Signed-off-by: Tony Breeds <tony@bakeyournoodle.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kernel/prom.c
+++ arch/powerpc/kernel/prom.c
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/kexec.h>
 #include <asm/opal.h>
 #include <asm/fadump.h>
+#include <asm/debug.h>
 
 #include <mm/mmu_decl.h>
 
