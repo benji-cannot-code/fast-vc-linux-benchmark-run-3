6d0d7ab568ac248406b79b218f040e262a2e65de
Tatyana Nikolova <Tatyana.E.Nikolova@intel.com>
2012-10-02 20:32:15 +0000

    RDMA/nes: Bump the version number of nes driver
    Bumpthe version of the nes driver to reflect recent fixes.
    
    Signed-off-by: Tatyana Nikolova <Tatyana.E.Nikolova@intel.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/hw/nes/nes.h
+++ drivers/infiniband/hw/nes/nes.h
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define QUEUE_DISCONNECTS
 
 #define DRV_NAME    "iw_nes"
-#define DRV_VERSION "1.5.0.0"
+#define DRV_VERSION "1.5.0.1"
 #define PFX         DRV_NAME ": "
 
 /*
