0954cd1fc9b7691129198cf969e775b209bf4f08
Axel Lin <axel.lin@ingics.com>
2012-10-02 09:16:53 +0900

    extcon: adc-jack: Add missing MODULE_LICENSE
    This driver can be built as a module, add MODULE_LICENSE for it.
    For completeness, also adds MODULE_AUTHOR and MODULE_DESCRIPTION.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/extcon-adc-jack.c
+++ drivers/extcon/extcon-adc-jack.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/device.h>
 #include <linux/platform_device.h>
@@ -196,3 +197,7 @@ static struct platform_driver adc_jack_driver = {
 };
 
 module_platform_driver(adc_jack_driver);
+
+MODULE_AUTHOR("MyungJoo Ham <myungjoo.ham@samsung.com>");
+MODULE_DESCRIPTION("ADC Jack extcon driver");
+MODULE_LICENSE("GPL v2");
