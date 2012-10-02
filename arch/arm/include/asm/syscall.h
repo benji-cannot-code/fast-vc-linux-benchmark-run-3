03db760f82ab81c894c3cba983ec966faea58d3d
Wade Farnsworth <wade_farnsworth@mentor.com>
2012-10-02 17:08:30 +0100

    ARM: 7548/1: include linux/sched.h in syscall.h
    The syscall tracing patch introduces a compile bug in lttng-modules
    when the latter calls syscall_get_nr(), similar to the following:
    
    <path-to-linux>/arch/arm/include/asm/syscall.h:21:2: error: implicit declaration of function 'task_thread_info' [-Werror=implicit-function-declaration]
    
    The issue is that we are using task_thread_info() in the
    syscall_get_nr() function in asm/syscall.h, but not explicitly
    including sched.h from this file, so we can expect this bug might
    surface any time that syscall_get_nr() is called.
    
    Explicitly including sched.h solves the problem.
    
    Cc: <stable@vger.kernel.org> [3.5, 3.6]
    Signed-off-by: Wade Farnsworth <wade_farnsworth@mentor.com>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/syscall.h
+++ arch/arm/include/asm/syscall.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_ARM_SYSCALL_H
 
 #include <linux/err.h>
+#include <linux/sched.h>
 
 extern const unsigned long sys_call_table[];
 
