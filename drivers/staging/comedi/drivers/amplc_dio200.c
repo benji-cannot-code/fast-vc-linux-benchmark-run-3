aaf293d2f6bd02de4ece2fdd49df514cbbbd7620
Ian Abbott <abbotti@mev.co.uk>
2012-10-03 16:25:16 +0100

    staging: comedi: amplc_dio200: fix possible NULL deref during detach
    `dio200_detach()` is called by the comedi core to clean up if either
    `dio200_attach()` or `dio200_attach_pci()` return an error.  It assigns
    `thisboard` to the return value of `comedi_board(dev)` and assumes it is
    non-null.  In the case of a previous call to `dio200_attach()` it won't
    be `NULL` because the comedi core will have pointed it to one of the
    elements of `dio200_boards[]`, but in the case of a previous call to
    `dio200_attach_pci()` it could be `NULL`, leading to a null pointer
    dereference.
    
    Check that `thisboard` is valid at the top of `dio200_detach()` and
    return early if it is `NULL`.  This is okay because the only other thing
    that could have been allocated is `dev->private` and that is freed by
    the comedi core, not by this function.
    
    Cc: <stable@vger.kernel.org> # 3.6.x
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_dio200.c
+++ drivers/staging/comedi/drivers/amplc_dio200.c
@@ -1411,6 +1411,8 @@ static void dio200_detach(struct comedi_device *dev)
 	const struct dio200_layout_struct *layout;
 	unsigned n;
 
+	if (!thisboard)
+		return;
 	if (dev->irq)
 		free_irq(dev->irq, dev);
 	if (dev->subdevices) {
