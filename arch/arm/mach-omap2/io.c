b8090fd2efcc391c35dde3275681e2b02d6046f3
Tony Lindgren <tony@atomide.com>
2012-10-03 11:23:43 -0700

    ARM: OMAP: Merge plat/multi.h into plat/cpu.h
    This is private to cpu.h and no other places should
    need to include it and we can drop the include
    in mach-omap2/io.c.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -28,12 +28,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat/serial.h>
 #include <plat/omap-pm.h>
-#include "omap_hwmod.h"
-#include <plat/multi.h>
 #include <plat-omap/dma-omap.h>
 
 #include "../plat-omap/sram.h"
 
+#include "omap_hwmod.h"
 #include "soc.h"
 #include "iomap.h"
 #include "voltage.h"
