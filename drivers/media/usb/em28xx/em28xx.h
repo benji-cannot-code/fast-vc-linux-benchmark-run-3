f3f7a0f1e4fcb09c354b158efb46eb5314b6974f
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2012-10-04 14:22:54 -0300

    [media] em28xx: Better support for the Terratec Cinergy HTC USB XS
    This intializes the card just like the windows driver does
    
    - the "HTC USB XS HD" uses the same firmware as the "HTC Stick HD";
    - both have different GPIO and reg init sequences;
    
    Tested only with DVB-C.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/em28xx/em28xx.h
+++ drivers/media/usb/em28xx/em28xx.h
@@ -129,6 +129,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2874_BOARD_MAXMEDIA_UB425_TC            84
 #define EM2884_BOARD_PCTV_510E                    85
 #define EM2884_BOARD_PCTV_520E                    86
+#define EM2884_BOARD_TERRATEC_HTC_USB_XS	  87
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
