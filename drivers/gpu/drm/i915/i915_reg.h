fecae400534931ed960a30b4a0770fd1970aed0c
Paulo Zanoni <paulo.r.zanoni@intel.com>
2012-10-05 12:05:53 -0300

    drm/i915: enable and disable DDI_FUNC_CTL at the right time
    And the right time is exactly after/before changing PIPE_CONF. See the
    documentation about the mode set sequence.
    
    This code is not inside any encoder-specific callback because
    DDI_FUNC_CTL is part of the pipe, so it is used by all encoders.
    
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -4395,6 +4395,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Those bits are ignored by pipe EDP since it can only connect to DDI A */
 #define  PIPE_DDI_PORT_MASK		(7<<28)
 #define  PIPE_DDI_SELECT_PORT(x)	((x)<<28)
+#define  PIPE_DDI_PORT_NONE		(0<<28)
 #define  PIPE_DDI_MODE_SELECT_MASK	(7<<24)
 #define  PIPE_DDI_MODE_SELECT_HDMI	(0<<24)
 #define  PIPE_DDI_MODE_SELECT_DVI	(1<<24)
