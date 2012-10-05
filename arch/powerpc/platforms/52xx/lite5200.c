7310391b58bfd0c533b2bae33d091989e6488996
Andi Kleen <ak@linux.intel.com>
2012-10-04 17:11:37 -0700

    sections: fix section conflicts in arch/powerpc
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/powerpc/platforms/52xx/lite5200.c
+++ arch/powerpc/platforms/52xx/lite5200.c
@@ -173,7 +173,7 @@ static void __init lite5200_setup_arch(void)
 	mpc52xx_setup_pci();
 }
 
-static const char *board[] __initdata = {
+static const char * const board[] __initconst = {
 	"fsl,lite5200",
 	"fsl,lite5200b",
 	NULL,
