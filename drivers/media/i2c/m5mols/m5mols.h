3c14af49f992a42ea8ef6f798e2a4069ec7933dc
Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
2012-10-06 15:04:40 -0300

    [media] m5mols: Add missing #include <linux/sizes.h>
    Include <linux/sizes.h> header that is missing after commit ab7ef22419927
    "[media] m5mols: Implement .get_frame_desc subdev callback".
    It prevents possible build errors due to undefined SZ_1M.
    
    This header is currently included only when m5mols is compiled
    on arm; if build on other archs, the compilation will break.
    
    Reported-by: Jan Hoogenraad <jan-conceptronic@hoogenraad.net>
    Signed-off-by: Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/i2c/m5mols/m5mols.h
+++ drivers/media/i2c/m5mols/m5mols.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef M5MOLS_H
 #define M5MOLS_H
 
+#include <linux/sizes.h>
 #include <media/v4l2-subdev.h>
 #include "m5mols_reg.h"
 
