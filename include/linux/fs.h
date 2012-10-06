a8347b250e56ddecb4d6f22606a228c80db60964
Marco Stornelli <marco.stornelli@gmail.com>
2012-10-06 12:43:09 +0200

    vfs: drop lock/unlock super
    Removed s_lock from super_block and removed lock/unlock super.
    
    Signed-off-by: Marco Stornelli <marco.stornelli@gmail.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1508,7 +1508,6 @@ struct super_block {
 	unsigned long		s_magic;
 	struct dentry		*s_root;
 	struct rw_semaphore	s_umount;
-	struct mutex		s_lock;
 	int			s_count;
 	atomic_t		s_active;
 #ifdef CONFIG_SECURITY
