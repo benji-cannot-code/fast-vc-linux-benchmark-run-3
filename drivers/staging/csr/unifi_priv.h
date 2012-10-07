e2fe55f73ff30ff829220ac078f6eb25a402b100
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2012-10-07 21:43:26 +0800

    staging: csr: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    dpatch engine is used to auto generate this patch.
    (https://github.com/weiyj/dpatch)
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/unifi_priv.h
+++ drivers/staging/csr/unifi_priv.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_UNIFI_PRIV_H__
 #define __LINUX_UNIFI_PRIV_H__ 1
 
-#include <linux/version.h>
 #include <linux/module.h>
 #include <linux/string.h>
 #include <linux/errno.h>
