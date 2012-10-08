7bd0b0d93a08a8c226c14ec36cb02e0752328cf4
Tony Lindgren <tony@atomide.com>
2012-10-08 09:11:22 -0700

    ARM: OMAP2+: Make board-zoom.h local
    This can be local to mach-omap2.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-ldp.c
+++ arch/arm/mach-omap2/board-ldp.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "common.h"
 #include "gpmc.h"
-#include <mach/board-zoom.h>
+#include "board-zoom.h"
 #include <plat/usb.h>
 #include "gpmc-smsc911x.h"
 
