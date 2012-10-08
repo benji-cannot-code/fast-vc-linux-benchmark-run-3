74c779a53e5d5b4cdbf20f119c919a83f4490ad5
Minchan Kim <minchan@kernel.org>
2012-10-08 16:33:51 -0700

    cma: decrease cc.nr_migratepages after reclaiming pagelist
    reclaim_clean_pages_from_list() reclaims clean pages before migration so
    cc.nr_migratepages should be updated.  Currently, there is no problem but
    it can be wrong if we try to use the value in future.
    
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Cc: Michal Nazarewicz <mina86@mina86.com>
    Cc: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5675,7 +5675,7 @@ static int __alloc_contig_migrate_range(struct compact_control *cc,
 					unsigned long start, unsigned long end)
 {
 	/* This function is based on compact_zone() from compaction.c. */
-
+	unsigned long nr_reclaimed;
 	unsigned long pfn = start;
 	unsigned int tries = 0;
 	int ret = 0;
@@ -5702,7 +5702,9 @@ static int __alloc_contig_migrate_range(struct compact_control *cc,
 			break;
 		}
 
-		reclaim_clean_pages_from_list(cc->zone, &cc->migratepages);
+		nr_reclaimed = reclaim_clean_pages_from_list(cc->zone,
+							&cc->migratepages);
+		cc->nr_migratepages -= nr_reclaimed;
 
 		ret = migrate_pages(&cc->migratepages,
 				    alloc_migrate_target,
