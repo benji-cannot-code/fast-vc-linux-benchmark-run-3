86ce319c6266689dadbc059912ac6d525e700e44
David Howells <dhowells@redhat.com>
2012-10-09 09:47:14 +0100

    UAPI: (Scripted) Disintegrate arch/mips/include/asm
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Michael Kerrisk <mtk.manpages@gmail.com>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: Dave Jones <davej@redhat.com>

--- arch/mips/include/uapi/asm/types.h
+++ arch/mips/include/uapi/asm/types.h
@@ -9,26 +9,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   written by Ralf Baechle
  * Copyright (C) 1999 Silicon Graphics, Inc.
  */
-#ifndef _ASM_TYPES_H
-#define _ASM_TYPES_H
-
-# include <asm-generic/int-ll64.h>
-#include <uapi/asm/types.h>
+#ifndef _UAPI_ASM_TYPES_H
+#define _UAPI_ASM_TYPES_H
 
 /*
- * These aren't exported outside the kernel to avoid name space clashes
+ * We don't use int-l64.h for the kernel anymore but still use it for
+ * userspace to avoid code changes.
  */
-#ifndef __ASSEMBLY__
-
-/*
- * Don't use phys_t.  You've been warned.
- */
-#ifdef CONFIG_64BIT_PHYS_ADDR
-typedef unsigned long long phys_t;
-#else
-typedef unsigned long phys_t;
+#ifndef __KERNEL__
+# if _MIPS_SZLONG == 64
+#  include <asm-generic/int-l64.h>
+# else
+#  include <asm-generic/int-ll64.h>
+# endif
 #endif
 
-#endif /* __ASSEMBLY__ */
 
-#endif /* _ASM_TYPES_H */
+#endif /* _UAPI_ASM_TYPES_H */
