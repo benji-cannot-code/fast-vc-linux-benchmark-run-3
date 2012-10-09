5a22a66d4f21bcb156e69447d588d8032576284f
Eric Paris <eparis@redhat.com>
2012-10-09 10:56:25 -0400

    SELinux: rename SE_SBLABELSUPP to SBLABEL_MNT
    Just a flag rename as we prepare to make it not so special.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Non-mount related flags */
 #define SE_SBINITIALIZED	0x10
 #define SE_SBPROC		0x20
-#define SE_SBLABELSUPP	0x40
+#define SBLABEL_MNT	0x40
 
 #define CONTEXT_STR	"context="
 #define FSCONTEXT_STR	"fscontext="
