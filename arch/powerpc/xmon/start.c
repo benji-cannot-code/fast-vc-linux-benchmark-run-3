7e02c32f4899aef2c3ffb7f8cb4b897ce57d948d
Michael Ellerman <michael@ellerman.id.au>
2012-10-09 04:20:30 +0000

    powerpc/xmon: Remove empty xmon_map_scc()
    This has been empty since 2005, commit 51d3082 "Unify udbg (#2)".
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/xmon/start.c
+++ arch/powerpc/xmon/start.c
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/udbg.h>
 #include "nonstdio.h"
 
-void xmon_map_scc(void)
-{
-}
-
 int xmon_write(const void *ptr, int nb)
 {
 	return udbg_write(ptr, nb);
