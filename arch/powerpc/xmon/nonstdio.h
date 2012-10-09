eff516d4644b790669efe30b97897b81a81d0da7
Michael Ellerman <michael@ellerman.id.au>
2012-10-09 04:20:31 +0000

    powerpc/xmon: Make xmon_getchar() static
    xmon_getchar() is only called from within nonstdio.c, so make it static.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/xmon/nonstdio.h
+++ arch/powerpc/xmon/nonstdio.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define putchar	xmon_putchar
 
 extern int xmon_putchar(int c);
-extern int xmon_getchar(void);
 extern void xmon_puts(const char *);
 extern char *xmon_gets(char *, int);
 extern void xmon_printf(const char *, ...);
