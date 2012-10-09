5afe943f9382395706827aa8eb0f3f974e814727
James Hogan <james.hogan@imgtec.com>
2012-10-09 10:54:27 +0100

    metag: Signal handling
    Add signal handling code for metag.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>

---
+++ arch/metag/include/uapi/asm/siginfo.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _METAG_SIGINFO_H
+#define _METAG_SIGINFO_H
+
+#define __ARCH_SI_TRAPNO
+
+#include <asm-generic/siginfo.h>
+
+#endif
