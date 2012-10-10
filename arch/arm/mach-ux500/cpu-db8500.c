9357792fb0b196c747cd6ac2cf8d72e4b830f711
Linus Walleij <linus.walleij@linaro.org>
2012-10-10 14:27:58 +0200

    pinctrl/nomadik: move the platform data header
    This moves the platform data header for the Nomadik pin controller
    to <linux/platform_data/pinctrl-nomadik.h>.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -18,14 +18,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/io.h>
 #include <linux/mfd/abx500/ab8500.h>
+#include <linux/platform_data/usb-musb-ux500.h>
+#include <linux/platform_data/pinctrl-nomadik.h>
 
 #include <asm/pmu.h>
 #include <asm/mach/map.h>
-#include <plat/gpio-nomadik.h>
 #include <mach/hardware.h>
 #include <mach/setup.h>
 #include <mach/devices.h>
-#include <linux/platform_data/usb-musb-ux500.h>
 #include <mach/db8500-regs.h>
 
 #include "devices-db8500.h"
