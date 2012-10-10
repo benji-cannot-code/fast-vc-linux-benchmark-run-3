9ef5351d0fa55f957885691152ea7a85541ebc4e
Alexander Shiyan <shc_work@mail.ru>
2012-10-10 19:45:28 +0400

    ARM: clps711x: p720t: remove missing #include
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-clps711x/p720t.c
+++ arch/arm/mach-clps711x/p720t.c
@@ -37,8 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/map.h>
 #include <mach/syspld.h>
 
-#include <asm/hardware/clps7111.h>
-
 #include "common.h"
 
 /*
