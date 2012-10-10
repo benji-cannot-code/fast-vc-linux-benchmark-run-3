3bb39a2efb3544657c84abe99ca2902e003fcb74
Linus Walleij <linus.walleij@linaro.org>
2012-10-10 14:35:17 +0200

    pinctrl/nomadik: merge old pincfg header
    This merges the old <plat/pincfg.h> header into
    <linux/platform_data/pinctrl-nomadik.h> and rids us of
    yet one more <plat/*> include.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-nomadik/board-nhk8815.c
+++ arch/arm/mach-nomadik/board-nhk8815.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/time.h>
 
 #include <plat/mtu.h>
-#include <plat/pincfg.h>
 
 #include <linux/platform_data/mtd-nomadik-nand.h>
 #include <mach/fsmc.h>
