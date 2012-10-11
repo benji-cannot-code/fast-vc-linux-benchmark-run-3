289b0f19284e253aeec0a2dd8e01ada25839039d
Josef Bacik <jbacik@fusionio.com>
2012-10-11 15:53:56 -0400

    Btrfs: only log the inode item if we can get away with it
    Currently we copy all the file information into the log, inode item, the
    refs, xattrs etc.  Except most of this doesn't change from fsync to fsync,
    just the inode item changes.  So set a flag if an xattr changes or a link is
    added, and otherwise only log the inode item.  Thanks,
    
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    Signed-off-by: Chris Mason <chris.mason@fusionio.com>

--- fs/btrfs/btrfs_inode.h
+++ fs/btrfs/btrfs_inode.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BTRFS_INODE_HAS_ORPHAN_ITEM		5
 #define BTRFS_INODE_HAS_ASYNC_EXTENT		6
 #define BTRFS_INODE_NEEDS_FULL_SYNC		7
+#define BTRFS_INODE_COPY_EVERYTHING		8
 
 /* in memory btrfs inode */
 struct btrfs_inode {
