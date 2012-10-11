63db2a1fb73160ccc0471d86baf5ce106f250f67
Andy Whitcroft <apw@canonical.com>
2012-10-11 11:32:21 +0100

    efivarfs: efivarfs_fill_super() ensure we clean up correctly on error
    Ensure we free both the name and inode on error when building the
    individual variables.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Matthew Garrett <mjg@redhat.com>
    Acked-by: Jeremy Kerr <jeremy.kerr@canonical.com>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- drivers/firmware/efivars.c
+++ drivers/firmware/efivars.c
@@ -949,6 +949,7 @@ int efivarfs_fill_super(struct super_block *sb, void *data, int silent)
 	struct dentry *root;
 	struct efivar_entry *entry, *n;
 	struct efivars *efivars = &__efivars;
+	char *name;
 
 	efivarfs_sb = sb;
 
@@ -970,16 +971,18 @@ int efivarfs_fill_super(struct super_block *sb, void *data, int silent)
 		return -ENOMEM;
 
 	list_for_each_entry_safe(entry, n, &efivars->list, list) {
-		struct inode *inode;
 		struct dentry *dentry, *root = efivarfs_sb->s_root;
-		char *name;
 		unsigned long size = 0;
 		int len, i;
+
+		inode = NULL;
 
 		len = utf16_strlen(entry->var.VariableName);
 
 		/* GUID plus trailing NULL */
 		name = kmalloc(len + 38, GFP_ATOMIC);
+		if (!name)
+			goto fail;
 
 		for (i = 0; i < len; i++)
 			name[i] = entry->var.VariableName[i] & 0xFF;
@@ -992,7 +995,13 @@ int efivarfs_fill_super(struct super_block *sb, void *data, int silent)
 
 		inode = efivarfs_get_inode(efivarfs_sb, root->d_inode,
 					  S_IFREG | 0644, 0);
+		if (!inode)
+			goto fail_name;
+
 		dentry = d_alloc_name(root, name);
+		if (!dentry)
+			goto fail_inode;
+
 		/* copied by the above to local storage in the dentry. */
 		kfree(name);
 
@@ -1010,6 +1019,13 @@ int efivarfs_fill_super(struct super_block *sb, void *data, int silent)
 	}
 
 	return 0;
+
+fail_inode:
+	iput(inode);
+fail_name:
+	kfree(name);
+fail:
+	return -ENOMEM;
 }
 
 static struct dentry *efivarfs_mount(struct file_system_type *fs_type,
