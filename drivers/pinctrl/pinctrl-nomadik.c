f32799364bf60a7262429ba7d6e098b95d61784e
Linus Walleij <linus.walleij@linaro.org>
2012-10-11 14:33:42 +0200

    pinctrl/nomadik: provide stubs for legacy Nomadik
    The compilation of the pinctrl driver failed on the legacy
    Nomadik NHK8815 platform because it was not providing the PRCMU
    interfaces needed to support the extended alternate functions
    used by the ux500 series.
    
    Solve this by providing some stubs for the legacy platform, to
    avoid too much #ifdefs in the code per se. Theoretically this
    actually allows the Nomadik and Ux500 to have a single kernel
    image with support for the PRCM registers on the Ux500 (though
    they have incompatible archs, but the spirit is there).
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-nomadik.c
+++ drivers/pinctrl/pinctrl-nomadik.c
@@ -31,7 +31,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pinctrl/pinconf.h>
 /* Since we request GPIOs from ourself */
 #include <linux/pinctrl/consumer.h>
+/*
+ * For the U8500 archs, use the PRCMU register interface, for the older
+ * Nomadik, provide some stubs. The functions using these will only be
+ * called on the U8500 series.
+ */
+#ifdef CONFIG_ARCH_U8500
 #include <linux/mfd/dbx500-prcmu.h>
+#else
+static inline u32 prcmu_read(unsigned int reg) {
+	return 0;
+}
+static inline void prcmu_write(unsigned int reg, u32 value) {}
+static inline void prcmu_write_masked(unsigned int reg, u32 mask, u32 value) {}
+#endif
 
 #include <asm/mach/irq.h>
 
