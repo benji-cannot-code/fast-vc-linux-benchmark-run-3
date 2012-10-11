c62599b7bc2bd3b604e7730e2cf0c6a5b1a267d6
Mihai Caraman <mihai.caraman@freescale.com>
2012-10-11 06:13:19 +0000

    KVM: PPC: booke: Fix get_tb() compile error on 64-bit
    Include header file for get_tb() declaration.
    
    Signed-off-by: Mihai Caraman <mihai.caraman@freescale.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/kvm/booke.c
+++ arch/powerpc/kvm/booke.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/dbell.h>
 #include <asm/hw_irq.h>
 #include <asm/irq.h>
+#include <asm/time.h>
 
 #include "timing.h"
 #include "booke.h"
