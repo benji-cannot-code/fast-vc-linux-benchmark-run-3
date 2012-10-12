6a20f9f5cbc8c2c82981826143ebc663e916e7fc
Lai Jiangshan <laijs@cn.fujitsu.com>
2012-10-13 01:14:14 +0800

    srcu: Credit Lai Jiangshan with SRCU rewrite
    Lai Jiangshan rewrote SRCU, so this commit ensures that he gets his
    proper share of blame^Wcredit.
    
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/srcu.h
+++ include/linux/srcu.h
@@ -17,8 +17,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
  * Copyright (C) IBM Corporation, 2006
+ * Copyright (C) Fujitsu, 2012
  *
  * Author: Paul McKenney <paulmck@us.ibm.com>
+ *	   Lai Jiangshan <laijs@cn.fujitsu.com>
  *
  * For detailed explanation of Read-Copy Update mechanism see -
  * 		Documentation/RCU/ *.txt
