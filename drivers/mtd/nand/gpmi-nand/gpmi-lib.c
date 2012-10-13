57d4aee9e0fbb92ae24c2ba6d1199976078a94a7
Huang Shijie <shijie8@gmail.com>
2012-10-13 13:03:29 -0400

    mtd: gpmi: remove unneccessary header
    The whole gpmi-nand driver has turned to pure devicetree supported.
    So the linux/mtd/gpmi-nand.h is not neccessary now. Just remove it,
    and move some macros to the gpmi-nand driver itself.
    
    Signed-off-by: Huang Shijie <shijie8@gmail.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/nand/gpmi-nand/gpmi-lib.c
+++ drivers/mtd/nand/gpmi-nand/gpmi-lib.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-#include <linux/mtd/gpmi-nand.h>
 #include <linux/delay.h>
 #include <linux/clk.h>
 
