0831924ada67604acafa0535f7637fd6011d6878
David Howells <dhowells@redhat.com>
2012-10-15 03:55:40 +0400

    UAPI: (Scripted) Disintegrate arch/xtensa/include/asm
    UAPI: (Scripted) Disintegrate arch/xtensa/include/asm
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Michael Kerrisk <mtk.manpages@gmail.com>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- arch/xtensa/include/uapi/asm/types.h
+++ arch/xtensa/include/uapi/asm/types.h
@@ -8,17 +8,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2001 - 2005 Tensilica Inc.
  */
-#ifndef _XTENSA_TYPES_H
-#define _XTENSA_TYPES_H
 
-#include <uapi/asm/types.h>
+#ifndef _UAPI_XTENSA_TYPES_H
+#define _UAPI_XTENSA_TYPES_H
+
+#include <asm-generic/int-ll64.h>
+
+#ifdef __ASSEMBLY__
+# define __XTENSA_UL(x)		(x)
+# define __XTENSA_UL_CONST(x)	x
+#else
+# define __XTENSA_UL(x)		((unsigned long)(x))
+# define __XTENSA_UL_CONST(x)	x##UL
+#endif
 
 #ifndef __ASSEMBLY__
-/*
- * These aren't exported outside the kernel to avoid name space clashes
- */
-
-#define BITS_PER_LONG 32
 
 #endif
-#endif	/* _XTENSA_TYPES_H */
+
+#endif /* _UAPI_XTENSA_TYPES_H */
