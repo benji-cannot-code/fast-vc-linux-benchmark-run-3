0aaba0c424e0bbc61b4ec67c5b249460f3ea1143
Marek Vasut <marex@denx.de>
2012-10-14 04:32:55 +0200

    spi: mxs: Assign message status after transfer finished
    In the current code implementing the MXS SPI driver, every transferred
    message had assigned status = 0, which is not correct. Properly assign
    status returned from the I/O functions.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/spi/spi-mxs.c
+++ drivers/spi/spi-mxs.c
@@ -481,7 +481,7 @@ static int mxs_spi_transfer_one(struct spi_master *master,
 		first = last = 0;
 	}
 
-	m->status = 0;
+	m->status = status;
 	spi_finalize_current_message(master);
 
 	return status;
