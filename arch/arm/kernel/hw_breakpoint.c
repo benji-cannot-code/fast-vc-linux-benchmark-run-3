32a5e9e886241e2b2fb24efeb06287870635fcb5
Dietmar Eggemann <dietmar.eggemann@arm.com>
2012-10-14 20:23:04 +0100

    ARM: coresight: common definition for (OS) Lock Access Register key value
    Coresight components and debug are using a common lock control mechansim.
    Writing 0xC5ACCE55 to the Lock Access Register (LAR) in case of a coresight
    components enables further access to the coresight device registers. Writing
    any other value to it removes the write access.
    Writing 0xC5ACCE55 to the OS Lock Access Register (OSLAR) in case of debug
    locks the debug register for further access to the debug registers. Writing
    any other value to it unlocks the debug registers.
    
    Unfortunately, the existing coresight code uses the terms lock and unlock the
    other way around. Unlocking stands for enabling write access and locking for
    removing write access.
    
    That is why the definition of the LAR and OSLAR key value has been changed to
    CS_LAR_KEY.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm/kernel/hw_breakpoint.c
+++ arch/arm/kernel/hw_breakpoint.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/hw_breakpoint.h>
 #include <asm/kdebug.h>
 #include <asm/traps.h>
+#include <asm/hardware/coresight.h>
 
 /* Breakpoint currently in use for each BRP. */
 static DEFINE_PER_CPU(struct perf_event *, bp_on_reg[ARM_MAX_BRP]);
@@ -956,9 +957,9 @@ static void reset_ctrl_regs(void *unused)
 
 	/*
 	 * Unconditionally clear the OS lock by writing a value
-	 * other than 0xC5ACCE55 to the access register.
+	 * other than CS_LAR_KEY to the access register.
 	 */
-	ARM_DBG_WRITE(c1, c0, 4, 0);
+	ARM_DBG_WRITE(c1, c0, 4, ~CS_LAR_KEY);
 	isb();
 
 	/*
