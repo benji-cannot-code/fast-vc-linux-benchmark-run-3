bd027deb83f7d732d39bcb91788537809d0b08ec
Al Viro <viro@zeniv.linux.org.uk>
2012-10-15 16:37:12 -0400

    m32r: switch to generic sys_execve()
    ... and get rid of the horrors in fork()/vfork()/clone() prototypes.
    It's fscking faster to calculate pt_regs in question than to fetch
    the pointer from stack...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/m32r/include/asm/unistd.h
+++ arch/m32r/include/asm/unistd.h
@@ -353,6 +353,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_WANT_SYS_OLDUMOUNT
 #define __ARCH_WANT_SYS_RT_SIGACTION
 #define __ARCH_WANT_SYS_RT_SIGSUSPEND
+#define __ARCH_WANT_SYS_EXECVE
 
 #define __IGNORE_lchown
 #define __IGNORE_setuid
