50b43cd6204450866f117321f1f434732ab6e627
Greg Ungerer <gerg@uclinux.org>
2012-10-15 14:56:09 +1000

    m68k: remove unused asm/dbg.h
    The contents of the m68k asm/dbg.h are never used, remove the file and
    remove the one reference to it.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- drivers/tty/serial/68328serial.c
+++ drivers/tty/serial/68328serial.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 2.4/2.5 port                 David McCullough
  */
 
-#include <asm/dbg.h>
 #include <linux/module.h>
 #include <linux/errno.h>
 #include <linux/serial.h>
