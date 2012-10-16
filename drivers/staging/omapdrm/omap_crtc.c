60ce555f6600b45acc5e62fc61afdc3d249ab88b
Andy Gross <andy.gross@ti.com>
2012-10-16 00:17:40 -0500

    staging: drm/omap: Fix include error during make
    Fixed include error for drm_mode.h
    
    Signed-off-by: Andy Gross <andy.gross@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/omapdrm/omap_crtc.c
+++ drivers/staging/omapdrm/omap_crtc.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "omap_drv.h"
 
-#include "drm_mode.h"
+#include <drm/drm_mode.h>
 #include "drm_crtc.h"
 #include "drm_crtc_helper.h"
 
