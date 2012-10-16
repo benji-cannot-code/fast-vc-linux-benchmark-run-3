2fa8b9d42b30660187d62a21cfc8662803784c55
Manjunathappa, Prakash <prakash.pm@ti.com>
2012-10-16 10:23:16 +0530

    da8xx-fb: cleanup LCDC configurations
    Configure below LCDC configurations to optimal values, also have an
    option configure these optional parameters for platform.
    1) AC bias configuration: Required only for passive panels
    2) Dma_burst_size:
    3) FIFO_DMA_DELAY:
    4) FIFO threshold: Does not apply for da830 LCDC.
    
    Patch is verified for 16bpp and 24bpp configurations on da830, da850 and
    am335x EVMs.
    
    Signed-off-by: Manjunathappa, Prakash <prakash.pm@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- include/video/da8xx-fb.h
+++ include/video/da8xx-fb.h
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DA8XX_FB_H
 #define DA8XX_FB_H
 
-enum panel_type {
-	QVGA = 0
-};
-
 enum panel_shade {
 	MONOCHROME = 0,
 	COLOR_ACTIVE,
@@ -28,13 +24,6 @@ enum raster_load_mode {
 	LOAD_PALETTE,
 };
 
-struct display_panel {
-	enum panel_type panel_type; /* QVGA */
-	int max_bpp;
-	int min_bpp;
-	enum panel_shade panel_shade;
-};
-
 struct da8xx_lcdc_platform_data {
 	const char manu_name[10];
 	void *controller_data;
@@ -43,7 +32,7 @@ struct da8xx_lcdc_platform_data {
 };
 
 struct lcd_ctrl_config {
-	const struct display_panel *p_disp_panel;
+	enum panel_shade panel_shade;
 
 	/* AC Bias Pin Frequency */
 	int ac_bias;
@@ -69,17 +58,8 @@ struct lcd_ctrl_config {
 	/* Mono 8-bit Mode: 1=D0-D7 or 0=D0-D3 */
 	unsigned char mono_8bit_mode;
 
-	/* Invert line clock */
-	unsigned char invert_line_clock;
-
-	/* Invert frame clock  */
-	unsigned char invert_frm_clock;
-
 	/* Horizontal and Vertical Sync Edge: 0=rising 1=falling */
 	unsigned char sync_edge;
-
-	/* Horizontal and Vertical Sync: Control: 0=ignore */
-	unsigned char sync_ctrl;
 
 	/* Raster Data Order Select: 1=Most-to-least 0=Least-to-most */
 	unsigned char raster_order;
