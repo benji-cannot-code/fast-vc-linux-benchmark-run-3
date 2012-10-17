a4c1d9f9b2c5a5bd77665c9745c7d089f1afb31c
Linus Walleij <linus.walleij@linaro.org>
2012-10-17 18:31:20 +0200

    pinctrl/coh901: use irqdomain, allocate irqdescs
    This switches the COH 901 pinctrl driver to allocate its GPIO
    IRQs dynamically, and start to use a linear irqdomain to map
    from the hardware IRQs.
    
    This way we can cut away the complex allocation of IRQ numbers
    from the <mach/irqs.h> file.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-u300/core.c
+++ arch/arm/mach-u300/core.c
@@ -1446,7 +1446,6 @@ static struct platform_device pinctrl_device = {
 static struct u300_gpio_platform u300_gpio_plat = {
 	.ports = 7,
 	.gpio_base = 0,
-	.gpio_irq_base = IRQ_U300_GPIO_BASE,
 	.pinctrl_device = &pinctrl_device,
 };
 
