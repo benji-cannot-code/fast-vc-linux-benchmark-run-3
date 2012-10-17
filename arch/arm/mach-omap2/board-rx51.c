b822b6c4562f20583605e0b363bdce1fb67e43e7
Pavel Machek <pavel@ucw.cz>
2012-10-17 23:03:00 +0200

    ARM: OMAP2+: Nokia N9/N900/N950 -- mention product names
    This adds product names (that most users know) to Kconfig and board
    comments.
    
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Reviewed-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-rx51.c
+++ arch/arm/mach-omap2/board-rx51.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * linux/arch/arm/mach-omap2/board-rx51.c
+ * Board support file for Nokia N900 (aka RX-51).
  *
  * Copyright (C) 2007, 2008 Nokia
  *
