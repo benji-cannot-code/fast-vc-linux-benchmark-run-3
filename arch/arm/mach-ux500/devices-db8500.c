e2c43908b6a966ac7e825d455534cf99bf6eb6f2
Linus Walleij <linus.walleij@linaro.org>
2012-10-18 11:08:05 +0200

    ARM: plat-nomadik: convert platforms to SPARSE_IRQ
    This converts the Nomadik and Ux500 platforms to use SPARSE_IRQ.
    
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/devices-db8500.c
+++ arch/arm/mach-ux500/devices-db8500.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/hardware.h>
 #include <mach/setup.h>
+#include <mach/irqs.h>
 
 #include "ste-dma40-db8500.h"
 
