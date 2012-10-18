e344001eac7460795302d00fe3a0fd887e86b8ea
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-10-18 13:46:29 +0300

    OMAPDSS: get the dss version from core pdev
    The output drivers get the omapdss hw version from the platform data for
    their respective output device. This doesn't work with DT, as there's no
    platform data for them.
    
    Add a new function, omapdss_get_version(), which returns the dss version
    from the core device, which will have platform data on DT also. The
    function is exported so that users of omapdss can also use it.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -4099,7 +4099,6 @@ static const struct dispc_features omap44xx_dispc_feats __initconst = {
 
 static int __init dispc_init_features(struct platform_device *pdev)
 {
-	struct omap_dss_board_info *pdata = pdev->dev.platform_data;
 	const struct dispc_features *src;
 	struct dispc_features *dst;
 
@@ -4109,7 +4108,7 @@ static int __init dispc_init_features(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
-	switch (pdata->version) {
+	switch (omapdss_get_version()) {
 	case OMAPDSS_VER_OMAP24xx:
 		src = &omap24xx_dispc_feats;
 		break;
