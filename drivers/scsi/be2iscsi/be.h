88e211d525cc0b9c61b21c1224ae773ec64752bc
John Soni Jose <sony.john-n@emulex.com>
2012-10-20 04:43:20 +0530

    [SCSI] be2iscsi: Fix driver support for Skyhawk-R adapter.
    Fix support for Skyhawk-R adapter by populating the pci_id_table
    
    Signed-off-by: John Soni Jose <sony.john-n@emulex.com>
    Signed-off-by: Jayamohan Kallickal <jayamohan.kallickal@emulex.com>
    Reviewed-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/be2iscsi/be.h
+++ drivers/scsi/be2iscsi/be.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* BladeEngine Generation numbers */
 #define BE_GEN2 2
 #define BE_GEN3 3
-
+#define BE_GEN4	4
 struct be_dma_mem {
 	void *va;
 	dma_addr_t dma;
