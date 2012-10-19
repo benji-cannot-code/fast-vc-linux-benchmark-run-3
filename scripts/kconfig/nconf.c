705e7efdc65b4b21af8ed4796af70df9cc6e6a61
Yann E. MORIN <yann.morin.1998@free.fr>
2012-10-20 01:06:25 +0200

    kconfig: get CONFIG_ prefix from the environment
    Currently, the CONFIG_ prefix is hard-coded in the kconfig frontends
    executables. This means that two projects that use kconfig with
    different prefixes can not share the same kconfig frontends.
    
    Instead of hard-coding the prefix in the frontends, get it from the
    environment, and revert back to hard-coded value if not found.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/nconf.c
+++ scripts/kconfig/nconf.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define _GNU_SOURCE
 #include <string.h>
+#include <stdlib.h>
 
 #include "lkc.h"
 #include "nconf.h"
