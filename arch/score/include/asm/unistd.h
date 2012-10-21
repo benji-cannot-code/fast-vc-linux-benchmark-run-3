70e80623af99efa7fa08a0cb45d4adeeb090a75c
Al Viro <viro@zeniv.linux.org.uk>
2012-10-21 16:44:57 -0400

    score: switch to generic fork/vfork/clone
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/score/include/asm/unistd.h
+++ arch/score/include/asm/unistd.h
@@ -6,5 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_WANT_SYSCALL_OFF_T
 #define __ARCH_WANT_SYSCALL_DEPRECATED
 #define __ARCH_WANT_SYS_EXECVE
+#define __ARCH_WANT_SYS_CLONE
+#define __ARCH_WANT_SYS_FORK
+#define __ARCH_WANT_SYS_VFORK
 
 #include <asm-generic/unistd.h>
