f17d427c3825e862f418cf926e4836c10983d24b
Josh Cartwright <josh.cartwright@ni.com>
2012-10-21 21:15:37 -0500

    zynq: move static peripheral mappings
    Shifting them up into the vmalloc region prevents the following warning,
    when booting a zynq qemu target with more than 512mb of RAM:
    
      BUG: mapping for 0xe0000000 at 0xe0000000 out of vmalloc space
    
    In addition, it allows for reuse of these mappings when the proper
    drivers issue requests via ioremap().
    
    There are currently unknown issues with the early uart mapping.  For
    now, the uart will be mapped to a known working address.
    
    Signed-off-by: Josh Cartwright <josh.cartwright@ni.com>
    Cc: John Linn <john.linn@xilinx.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Tested-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/common.c
+++ arch/arm/mach-zynq/common.c
@@ -74,12 +74,12 @@ static struct map_desc io_desc[] __initdata = {
 	{
 		.virtual	= TTC0_VIRT,
 		.pfn		= __phys_to_pfn(TTC0_PHYS),
-		.length		= SZ_4K,
+		.length		= TTC0_SIZE,
 		.type		= MT_DEVICE,
 	}, {
 		.virtual	= SCU_PERIPH_VIRT,
 		.pfn		= __phys_to_pfn(SCU_PERIPH_PHYS),
-		.length		= SZ_8K,
+		.length		= SCU_PERIPH_SIZE,
 		.type		= MT_DEVICE,
 	},
 
@@ -87,7 +87,7 @@ static struct map_desc io_desc[] __initdata = {
 	{
 		.virtual	= UART0_VIRT,
 		.pfn		= __phys_to_pfn(UART0_PHYS),
-		.length		= SZ_4K,
+		.length		= UART0_SIZE,
 		.type		= MT_DEVICE,
 	},
 #endif
