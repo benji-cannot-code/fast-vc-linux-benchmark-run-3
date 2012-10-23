d28faf05b836ea9d5a3bfc3f986ee7499ca2f2c6
Al Viro <viro@zeniv.linux.org.uk>
2012-10-22 23:10:08 -0400

    flagday: kill pt_regs argument of do_fork()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/mips/kernel/syscall.c
+++ arch/mips/kernel/syscall.c
@@ -93,7 +93,7 @@ save_static_function(sys_fork);
 static int __used noinline
 _sys_fork(nabi_no_regargs struct pt_regs regs)
 {
-	return do_fork(SIGCHLD, regs.regs[29], &regs, 0, NULL, NULL);
+	return do_fork(SIGCHLD, regs.regs[29], 0, NULL, NULL);
 }
 
 save_static_function(sys_clone);
@@ -124,7 +124,7 @@ _sys_clone(nabi_no_regargs struct pt_regs regs)
 #else
 	child_tidptr = (int __user *) regs.regs[8];
 #endif
-	return do_fork(clone_flags, newsp, &regs, 0,
+	return do_fork(clone_flags, newsp, 0,
 	               parent_tidptr, child_tidptr);
 }
 
