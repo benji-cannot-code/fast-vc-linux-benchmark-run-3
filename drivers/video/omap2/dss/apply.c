281ef31bd0c8fdcef2295a37adde1d22d3bd50ac
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-10-23 13:46:12 +0300

    OMAPDSS: move ovl & ovl-mgr init to apply.c
    Overlay and overlay_manager structs will only be needed in the compat
    mode.
    
    This patch moves initialization of overlay and overlay_manager structs
    to apply.c, so that they are handled in omapdss_compat_init().
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/apply.c
+++ drivers/video/omap2/dss/apply.c
@@ -1470,12 +1470,17 @@ static DEFINE_MUTEX(compat_init_lock);
 
 int omapdss_compat_init(void)
 {
+	struct platform_device *pdev = dss_get_core_pdev();
+
 	mutex_lock(&compat_init_lock);
 
 	if (compat_refcnt++ > 0)
 		goto out;
 
 	apply_init_priv();
+
+	dss_init_overlay_managers(pdev);
+	dss_init_overlays(pdev);
 
 out:
 	mutex_unlock(&compat_init_lock);
@@ -1486,11 +1491,15 @@ EXPORT_SYMBOL(omapdss_compat_init);
 
 void omapdss_compat_uninit(void)
 {
+	struct platform_device *pdev = dss_get_core_pdev();
+
 	mutex_lock(&compat_init_lock);
 
 	if (--compat_refcnt > 0)
 		goto out;
 
+	dss_uninit_overlay_managers(pdev);
+	dss_uninit_overlays(pdev);
 out:
 	mutex_unlock(&compat_init_lock);
 }
