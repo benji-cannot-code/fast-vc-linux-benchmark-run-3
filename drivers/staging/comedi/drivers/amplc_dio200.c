afb843e2c5e5c2b20f6bc243a554a6111f455378
Ian Abbott <abbotti@mev.co.uk>
2012-10-24 16:47:48 +0100

    staging: comedi: amplc_dio200: no need to set hw_dev
    The call to `comedi_set_hw_dev()` from `dio200_pci_common_attach()` is
    now unnecessary since `dio200_pci_common_attach()` is now only called
    from this driver's `attach_pci` hook `dio200_attach_pci()` and the
    comedi core now calls `comedi_set_hw_dev()` before calling that.  Remove
    the unnecessary call.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_dio200.c
+++ drivers/staging/comedi/drivers/amplc_dio200.c
@@ -1260,8 +1260,6 @@ static int dio200_pci_common_attach(struct comedi_device *dev,
 	unsigned long iobase;
 	int ret;
 
-	comedi_set_hw_dev(dev, &pci_dev->dev);
-
 	ret = comedi_pci_enable(pci_dev, DIO200_DRIVER_NAME);
 	if (ret < 0) {
 		dev_err(dev->class_dev,
