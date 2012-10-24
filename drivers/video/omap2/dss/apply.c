c2d7ab82d7d8e808397bac7723f52d56173ca67a
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-10-24 12:39:53 +0300

    OMAPDSS: move blocking mgr enable/disable to compat layer
    dispc_mgr_enable_sync and dispc_mgr_disable_sync are only used with the
    compat mode. Non-compat will use the simpler enable and disable
    functions.
    
    This patch moves the synchronous enable/disable code to the compat
    layer. A new file is created, dispc-compat.c, which contains low level
    dispc compat code (versus apply.c, which contains slightly higher level
    compat code).
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/apply.c
+++ drivers/video/omap2/dss/apply.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dss.h"
 #include "dss_features.h"
+#include "dispc-compat.h"
 
 /*
  * We have 4 levels of cache for the dispc settings. First two are in SW and
