79fd8c13e42f87a322e969f622f84c08849da461
Peter Zijlstra <a.p.zijlstra@chello.nl>
2012-10-25 14:16:43 +0200

    mm: numa: Add fault driven placement and migration
    NOTE: This patch is based on "sched, numa, mm: Add fault driven
    	placement and migration policy" but as it throws away all the policy
    	to just leave a basic foundation I had to drop the signed-offs-by.
    
    This patch creates a bare-bones method for setting PTEs pte_numa in the
    context of the scheduler that when faulted later will be faulted onto the
    node the CPU is running on.  In itself this does nothing useful but any
    placement policy will fundamentally depend on receiving hints on placement
    from fault context and doing something intelligent about it.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Acked-by: Rik van Riel <riel@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -1047,6 +1047,7 @@ int do_huge_pmd_numa_page(struct mm_struct *mm, struct vm_area_struct *vma,
 	 */
 	split_huge_page(page);
 	put_page(page);
+
 	return 0;
 
 clear_pmdnuma:
@@ -1061,8 +1062,10 @@ clear_pmdnuma:
 
 out_unlock:
 	spin_unlock(&mm->page_table_lock);
-	if (page)
+	if (page) {
 		put_page(page);
+		task_numa_fault(numa_node_id(), HPAGE_PMD_NR);
+	}
 	return 0;
 }
 
