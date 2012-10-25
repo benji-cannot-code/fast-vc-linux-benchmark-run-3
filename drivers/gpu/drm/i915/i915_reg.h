e61fd0901fe4ff159986b7e7e5ae2221a5416bff
Jesse Barnes <jbarnes@virtuousgeek.org>
2012-10-25 12:15:44 -0700

    drm/i915: implement WaDisableVLVClockGating_VBIIssue on VLV
    This allows us to get the right vblank interrupt frequency.
    
    v2: pull in register definition
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Reviewed-by: Antti Koskipää <antti.koskipaa@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -557,6 +557,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IIR		0x020a4
 #define IMR		0x020a8
 #define ISR		0x020ac
+#define VLV_GUNIT_CLOCK_GATE	0x182060
+#define   GCFG_DIS		(1<<8)
 #define VLV_IIR_RW	0x182084
 #define VLV_IER		0x1820a0
 #define VLV_IIR		0x1820a4
