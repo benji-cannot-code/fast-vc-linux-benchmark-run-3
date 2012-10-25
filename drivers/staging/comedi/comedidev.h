f688279396e914268218a2ee20c4577384d1d7f6
H Hartley Sweeten <hartleys@visionengravers.com>
2012-10-24 18:12:34 -0700

    staging: comedi: comedidev.h: add PCI_VENDOR_ID_DT
    Add a define for the Data Translation, Inc. PCI vendor id. Remove
    the duplicates in the drivers.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedidev.h
+++ drivers/staging/comedi/comedidev.h
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * PCI Vendor IDs not in <linux/pci_ids.h>
  */
 #define PCI_VENDOR_ID_ICP		0x104c
+#define PCI_VENDOR_ID_DT		0x1116
 #define PCI_VENDOR_ID_CONTEC		0x1221
 #define PCI_VENDOR_ID_CB		0x1307	/* Measurement Computing */
 #define PCI_VENDOR_ID_ADVANTECH		0x13fe
