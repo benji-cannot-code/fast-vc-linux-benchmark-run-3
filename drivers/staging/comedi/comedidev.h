d7b8dbaf6afc49e92745a84180be2c309e3a912d
H Hartley Sweeten <hartleys@visionengravers.com>
2012-10-24 18:12:50 -0700

    staging: comedi: comedidev.h: add PCI_VENDOR_ID_KOLTER
    Add a define for the Kolter Electronic PCI vendor id. Remove the
    duplicates in the drivers.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedidev.h
+++ drivers/staging/comedi/comedidev.h
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * PCI Vendor IDs not in <linux/pci_ids.h>
  */
+#define PCI_VENDOR_ID_KOLTER		0x1001
 #define PCI_VENDOR_ID_ICP		0x104c
 #define PCI_VENDOR_ID_DT		0x1116
 #define PCI_VENDOR_ID_CONTEC		0x1221
--- drivers/staging/comedi/drivers/ke_counter.c
+++ drivers/staging/comedi/drivers/ke_counter.c
@@ -37,7 +37,6 @@ Kolter Electronic PCI Counter Card.
 
 #include "../comedidev.h"
 
-#define PCI_VENDOR_ID_KOLTER    0x1001
 #define CNT_CARD_DEVICE_ID      0x0014
 
 /*-- counter write ----------------------------------------------------------*/
