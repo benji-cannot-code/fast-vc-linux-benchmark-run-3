ed40b31755d7a8e419ece438e49f2c0acbe58ef8
David Howells <dhowells@redhat.com>
2012-10-25 13:37:57 -0700

    UAPI: fix tools/vm/page-types.c
    Fix tools/vm/page-types.c to use the UAPI variant of linux/kernel-page-flags.h
    lest the following error appear:
    
      In file included from page-types.c:38:0:
        ../../include/linux/kernel-page-flags.h:4:42: fatal error:
        uapi/linux/kernel-page-flags.h: No such file or directory
    
    Reported-by: Daniel Hazelton <dshadowwolf@gmail.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-by: Fengguang Wu <fengguang.wu@intel.com>
    Tested-by: Daniel Hazelton <dshadowwolf@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/vm/page-types.c
+++ tools/vm/page-types.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/mount.h>
 #include <sys/statfs.h>
 #include "../../include/uapi/linux/magic.h"
-#include "../../include/linux/kernel-page-flags.h"
+#include "../../include/uapi/linux/kernel-page-flags.h"
 
 
 #ifndef MAX_PATH
