5b7d76e99841b9b0b7a3c2d9aacd54f14fd67973
Flavio Leitner <fbl@redhat.com>
2012-10-26 14:39:53 +0000

    netxen: explicity handle pause autoneg parameter
    The hardware doesn't support controlling pause frames autoneg, so
    report that back correctly to userspace.
    
    Signed-off-by: Flavio Leitner <fbl@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qlogic/netxen/netxen_nic_ethtool.c
+++ drivers/net/ethernet/qlogic/netxen/netxen_nic_ethtool.c
@@ -489,6 +489,8 @@ netxen_nic_get_pauseparam(struct net_device *dev,
 	__u32 val;
 	int port = adapter->physical_port;
 
+	pause->autoneg = 0;
+
 	if (adapter->ahw.port_type == NETXEN_NIC_GBE) {
 		if ((port < 0) || (port >= NETXEN_NIU_MAX_GBE_PORTS))
 			return;
@@ -533,6 +535,11 @@ netxen_nic_set_pauseparam(struct net_device *dev,
 	struct netxen_adapter *adapter = netdev_priv(dev);
 	__u32 val;
 	int port = adapter->physical_port;
+
+	/* not supported */
+	if (pause->autoneg)
+		return -EINVAL;
+
 	/* read mode */
 	if (adapter->ahw.port_type == NETXEN_NIC_GBE) {
 		if ((port < 0) || (port >= NETXEN_NIU_MAX_GBE_PORTS))
