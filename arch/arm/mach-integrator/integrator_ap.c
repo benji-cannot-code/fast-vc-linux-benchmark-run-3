108af8bba284995ea3f38aa4b93bd6c158076af8
Linus Walleij <linus.walleij@linaro.org>
2012-10-27 01:24:29 +0200

    ARM: integrator: get rid of preallocated irq descriptors
    The Integrators were using the .nr_irqs field of the machine
    descriptor to pre-allocate a number of descriptors at boot. This
    is not right: the irq chip implementations should allocate
    their descriptors themselves, and as a result the simple
    irqdomain code warns about it.
    
    Get rid of this by just deleting the .nr_irq field from the
    machine descriptors but take care: doing so makes the default
    implementation hog the first 16 IRQ numbers, so these cannot
    be used by the still static IRQ number assignments in the
    ATAG boot case. So for these, bump the IRQ numbers to begin
    at 64 and upward.
    
    Introduce an offset to offset all IRQ numbers if need be,
    though we don't expect to do that again as device tree comes
    along.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-integrator/integrator_ap.c
+++ arch/arm/mach-integrator/integrator_ap.c
@@ -500,7 +500,6 @@ static const char * ap_dt_board_compat[] = {
 DT_MACHINE_START(INTEGRATOR_AP_DT, "ARM Integrator/AP (Device Tree)")
 	.reserve	= integrator_reserve,
 	.map_io		= ap_map_io,
-	.nr_irqs	= NR_IRQS_INTEGRATOR_AP,
 	.init_early	= ap_init_early,
 	.init_irq	= ap_init_irq_of,
 	.handle_irq	= fpga_handle_irq,
@@ -610,7 +609,6 @@ MACHINE_START(INTEGRATOR, "ARM-Integrator")
 	.atag_offset	= 0x100,
 	.reserve	= integrator_reserve,
 	.map_io		= ap_map_io,
-	.nr_irqs	= NR_IRQS_INTEGRATOR_AP,
 	.init_early	= ap_init_early,
 	.init_irq	= ap_init_irq,
 	.handle_irq	= fpga_handle_irq,
