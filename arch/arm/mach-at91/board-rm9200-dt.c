223ba42ab7edb60fcae7f831b6bab8fb35cb67ba
Joachim Eastwood <manabian@gmail.com>
2012-10-28 18:31:11 +0000

    ARM: AT91: Add AT91RM9200EK board device tree
    Signed-off-by: Joachim Eastwood <manabian@gmail.com>
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- arch/arm/mach-at91/board-rm9200-dt.c
+++ arch/arm/mach-at91/board-rm9200-dt.c
@@ -17,15 +17,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
 
-#include <mach/board.h>
-#include <mach/at91_aic.h>
-
 #include <asm/setup.h>
 #include <asm/irq.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 #include <asm/mach/irq.h>
 
+#include "at91_aic.h"
 #include "generic.h"
 
 
