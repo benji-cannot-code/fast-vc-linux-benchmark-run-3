988118e7744f6dc1798bd3d1154df4c41e558113
Tony Lindgren <tony@atomide.com>
2012-10-29 09:35:35 -0700

    ARM: OMAP: Move omap2+ specific parts of sram.c to mach-omap2
    Let's make the omap2+ specific parts private to mach-omap2.
    
    This leaves just a minimal shared code into plat-omap like
    it should be.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hsmmc.h"
 #include "omap4-sar-layout.h"
 #include "omap-secure.h"
+#include "sram.h"
 
 #ifdef CONFIG_CACHE_L2X0
 static void __iomem *l2cache_base;
