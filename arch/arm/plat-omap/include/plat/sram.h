d935277ff7b1a563728b80522e4d07b138d5600f
Tony Lindgren <tony@atomide.com>
2012-10-29 14:36:25 -0700

    ARM: OMAP: Introduce common omap_map_sram() and omap_sram_reset()
    This will allow us to separate out omap1 and omap2+ specific
    code in the later patches.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/include/plat/sram.h
+++ arch/arm/plat-omap/include/plat/sram.h
@@ -1,6 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int omap_sram_init(void);
 
+void omap_map_sram(unsigned long start, unsigned long size,
+			unsigned long skip, int cached);
+void omap_sram_reset(void);
+
 extern void *omap_sram_push_address(unsigned long size);
 
 /* Macro to push a function to the internal SRAM, using the fncpy API */
