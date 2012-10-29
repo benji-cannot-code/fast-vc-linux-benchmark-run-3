55d0192e41f1a9b988b63d1dc9c993b3cab59e5b
H Hartley Sweeten <hartleys@visionengravers.com>
2012-10-29 16:53:53 -0700

    staging: comedi: hwdrv_apci1032: remove forward declarations
    None of the functions in this file are exported. Make all of them
    static and remove the unnecessary forward declarations.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi-data/hwdrv_apci1032.c
+++ drivers/staging/comedi/drivers/addi-data/hwdrv_apci1032.c
@@ -85,8 +85,10 @@ static unsigned int ui_InterruptStatus;
 +----------------------------------------------------------------------------+
 */
 
-int i_APCI1032_ConfigDigitalInput(struct comedi_device *dev, struct comedi_subdevice *s,
-	struct comedi_insn *insn, unsigned int *data)
+static int i_APCI1032_ConfigDigitalInput(struct comedi_device *dev,
+					 struct comedi_subdevice *s,
+					 struct comedi_insn *insn,
+					 unsigned int *data)
 {
 	struct addi_private *devpriv = dev->private;
 	unsigned int ui_TmpValue;
@@ -146,8 +148,10 @@ int i_APCI1032_ConfigDigitalInput(struct comedi_device *dev, struct comedi_subde
 |			                                                         |
 +----------------------------------------------------------------------------+
 */
-int i_APCI1032_Read1DigitalInput(struct comedi_device *dev, struct comedi_subdevice *s,
-	struct comedi_insn *insn, unsigned int *data)
+static int i_APCI1032_Read1DigitalInput(struct comedi_device *dev,
+					struct comedi_subdevice *s,
+					struct comedi_insn *insn,
+					unsigned int *data)
 {
 	struct addi_private *devpriv = dev->private;
 	unsigned int ui_TmpValue = 0;
@@ -189,8 +193,10 @@ int i_APCI1032_Read1DigitalInput(struct comedi_device *dev, struct comedi_subdev
 +----------------------------------------------------------------------------+
 */
 
-int i_APCI1032_ReadMoreDigitalInput(struct comedi_device *dev, struct comedi_subdevice *s,
-	struct comedi_insn *insn, unsigned int *data)
+static int i_APCI1032_ReadMoreDigitalInput(struct comedi_device *dev,
+					   struct comedi_subdevice *s,
+					   struct comedi_insn *insn,
+					   unsigned int *data)
 {
 	struct addi_private *devpriv = dev->private;
 	unsigned int ui_PortValue = data[0];
@@ -283,7 +289,7 @@ static void v_APCI1032_Interrupt(int irq, void *d)
 +----------------------------------------------------------------------------+
 */
 
-int i_APCI1032_Reset(struct comedi_device *dev)
+static int i_APCI1032_Reset(struct comedi_device *dev)
 {
 	struct addi_private *devpriv = dev->private;
 
