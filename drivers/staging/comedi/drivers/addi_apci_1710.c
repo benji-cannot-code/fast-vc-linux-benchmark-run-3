c10f837f91dd4ea475eaa36c6c0a0b0e0a441510
H Hartley Sweeten <hartleys@visionengravers.com>
2012-10-29 16:51:33 -0700

    staging: comedi: addi-data: move the main #include's to the drivers
    The addi-data drivers are all built by the main driver files including
    addi-data/addi_common.c. That file then includes other files depending
    on what driver is being compiled. This is makes the code quite messy
    and hard to follow.
    
    Start cleaning it up by removing the unneeded #include's in addi_common.c
    and moving the some of the comedi #include's into the individual driver
    files.
    
    This is the first step in getting rid of the #ifdef'ery in addi_common.c.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi_apci_1710.c
+++ drivers/staging/comedi/drivers/addi_apci_1710.c
@@ -1,4 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <asm/i387.h>
+
+#include "../comedidev.h"
+#include "comedi_fc.h"
+
+#include "addi-data/addi_common.h"
+#include "addi-data/addi_amcc_s5933.h"
+
 #define CONFIG_APCI_1710 1
 
 #define ADDIDATA_DRIVER_NAME	"addi_apci_1710"
