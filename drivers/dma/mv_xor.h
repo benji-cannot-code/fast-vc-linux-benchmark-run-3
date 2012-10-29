80635f099a45482a83521d613420e9bcead01fa1
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-10-29 16:27:34 +0100

    dma: mv_xor: do not use pool_size from platform_data within the driver
    The driver currently pokes into the platform_data structure during its
    normal operation to get the pool_size value. Poking into the
    platform_data structure is not nice when moving to the Device Tree, so
    this commit adds a new pool_size field in the mv_xor_device structure,
    which gets initialized at ->probe() time. The driver then uses this
    field instead of the platform_data.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- drivers/dma/mv_xor.h
+++ drivers/dma/mv_xor.h
@@ -73,6 +73,7 @@ struct mv_xor_device {
 	int				id;
 	dma_addr_t			dma_desc_pool;
 	void				*dma_desc_pool_virt;
+	size_t                          pool_size;
 	struct dma_device		common;
 	struct mv_xor_shared_private	*shared;
 };
