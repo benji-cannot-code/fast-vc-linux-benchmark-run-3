15403ac5b86edee9d7e1dd53fbd7e859ed4c8852
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-10-30 11:54:34 +0100

    dma: mv_xor: remove sub-driver 'mv_xor'
    Now that XOR channels are directly registered by the main
    'mv_xor_shared' device ->probe() function and all users of the
    'mv_xor' device have been removed, we can get rid of the latter.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- include/linux/platform_data/dma-mv_xor.h
+++ include/linux/platform_data/dma-mv_xor.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mbus.h>
 
 #define MV_XOR_SHARED_NAME	"mv_xor_shared"
-#define MV_XOR_NAME		"mv_xor"
 
 struct mv_xor_platform_data {
 	struct platform_device		*shared;
