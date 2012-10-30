7cb414058a5c9f7d02f9c435c594f75eb7f86790
Tony Lindgren <tony@atomide.com>
2012-10-30 10:33:24 -0700

    ARM: OMAP: Fix relative includes for shared i2c.h file
    As discussed on linux-arm-kernel, we want to avoid
    relative includes for the arch/arm/*omap* shared code:
    
    http://www.spinics.net/lists/linux-omap/msg80520.html
    
    To fix this for the shared i2c.h, let's re-introduce
    a minimal plat/i2c.h.
    
    Note that drivers must not use this header as it will
    break build for omap2+ CONFIG_MULTIPLATFORM builds.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/common.h
+++ arch/arm/mach-omap1/common.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/mtd.h>
 #include <linux/i2c-omap.h>
 
-#include "../plat-omap/i2c.h"
+#include <plat/i2c.h>
 
 #if defined(CONFIG_ARCH_OMAP730) || defined(CONFIG_ARCH_OMAP850)
 void omap7xx_map_io(void);
--- arch/arm/plat-omap/i2c.c
+++ arch/arm/plat-omap/i2c.c
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/clk.h>
 
-#include <mach/irqs.h>
+#include <plat/i2c.h>
 
-#include "i2c.h"
+#include <mach/irqs.h>
 
 #define OMAP_I2C_MAX_CONTROLLERS 4
 static struct omap_i2c_bus_platform_data i2c_pdata[OMAP_I2C_MAX_CONTROLLERS];
