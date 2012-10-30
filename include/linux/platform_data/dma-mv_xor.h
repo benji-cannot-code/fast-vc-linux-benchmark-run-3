a12a5018d20f5a7c0c2b6c678ef5aa2ed494f933
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-10-30 11:59:42 +0100

    dma: mv_xor: change the driver name to 'mv_xor'
    Since we got rid of the per-XOR channel 'mv_xor' driver, now the
    per-XOR engine driver that used to be called 'mv_xor_shared' can
    simply be named 'mv_xor'.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- include/linux/platform_data/dma-mv_xor.h
+++ include/linux/platform_data/dma-mv_xor.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmaengine.h>
 #include <linux/mbus.h>
 
-#define MV_XOR_SHARED_NAME	"mv_xor_shared"
+#define MV_XOR_NAME	"mv_xor"
 
 struct mv_xor_channel_data {
 	int				hw_id;
