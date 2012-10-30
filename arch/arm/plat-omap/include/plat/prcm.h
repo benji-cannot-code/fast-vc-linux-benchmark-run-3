bfb215e015acbee212c180693a715a375b71f131
Paul Walmsley <paul@pwsan.com>
2012-10-29 20:49:45 -0600

    ARM: OMAP2+: PRCM: remove omap_prcm_get_reset_sources()
    omap_prcm_get_reset_sources() is now unused; so, remove it.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Tested-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- arch/arm/plat-omap/include/plat/prcm.h
+++ arch/arm/plat-omap/include/plat/prcm.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_ARM_ARCH_OMAP_PRCM_H
 #define __ASM_ARM_ARCH_OMAP_PRCM_H
 
-u32 omap_prcm_get_reset_sources(void);
 int omap2_cm_wait_idlest(void __iomem *reg, u32 mask, u8 idlest,
 			 const char *name);
 
