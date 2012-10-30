1804a2bdb917f4563966f4cf4a95df46ff9ae632
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-10-30 08:11:24 +0800

    arm: at91: move reset controller header to arm/arm/mach-at91
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/at91sam9261.c
+++ arch/arm/mach-at91/at91sam9261.c
@@ -21,9 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/cpu.h>
 #include <mach/at91sam9261.h>
 #include <mach/at91_pmc.h>
-#include <mach/at91_rstc.h>
 
 #include "at91_aic.h"
+#include "at91_rstc.h"
 #include "soc.h"
 #include "generic.h"
 #include "clock.h"
--- arch/arm/mach-at91/pm.c
+++ arch/arm/mach-at91/pm.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Show the reason for the previous system reset.
  */
 
-#include <mach/at91_rstc.h>
+#include "at91_rstc.h"
 #include "at91_shdwc.h"
 
 static void __init show_reset_status(void)
