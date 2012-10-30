72b13442604b8cfe4c602cebbd13ee290ecbce0c
Paul Walmsley <paul@pwsan.com>
2012-10-29 20:57:31 -0600

    ARM: OMAP2+: PRCM: remove omap2_cm_wait_idlest()
    Now that all users of mach-omap2/omap2_cm_wait_idlest() have been removed,
    delete the function and its supporting macros and prototypes.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Tested-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- arch/arm/plat-omap/include/plat/prcm.h
+++ arch/arm/plat-omap/include/plat/prcm.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/io.h>
 
-int omap2_cm_wait_idlest(void __iomem *reg, u32 mask, u8 idlest,
-			 const char *name);
 void __init omap2_set_globals_prcm(void __iomem *prm, void __iomem *cm,
 				   void __iomem *cm2, void __iomem *prcm_mpu);
 
