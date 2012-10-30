3a40f9bc70f721c1d0de8898fc93c5d3ea4b7a7a
H Hartley Sweeten <hartleys@visionengravers.com>
2012-10-29 17:03:54 -0700

    staging: comedi: addi_eeprom: remove the last forward declarations
    None of the functions in this file are exported. Make them static and
    remove the unnecessary forward declarations.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi-data/addi_eeprom.c
+++ drivers/staging/comedi/drivers/addi-data/addi_eeprom.c
@@ -108,12 +108,6 @@ struct str_AnalogInputHeader {
 	unsigned char b_Resolution;
 };
 
-int i_EepromReadMainHeader(unsigned short w_PCIBoardEepromAddress,
-	char *pc_PCIChipInformation, struct comedi_device *dev);
-
-unsigned short w_EepromReadWord(unsigned short w_PCIBoardEepromAddress, char *pc_PCIChipInformation,
-	unsigned short w_EepromStartAddress);
-
 static void v_EepromClock76(unsigned int dw_Address,
 			    unsigned int dw_RegisterValue)
 {
@@ -214,8 +208,9 @@ static void v_EepromWaitBusy(unsigned short w_PCIBoardEepromAddress)
 	} while (b_EepromBusy == 0x80);
 }
 
-unsigned short w_EepromReadWord(unsigned short w_PCIBoardEepromAddress, char *pc_PCIChipInformation,
-	unsigned short w_EepromStartAddress)
+static unsigned short w_EepromReadWord(unsigned short w_PCIBoardEepromAddress,
+				       char *pc_PCIChipInformation,
+				       unsigned short w_EepromStartAddress)
 {
 	unsigned char b_Counter = 0;
 	unsigned char b_ReadByte = 0;
@@ -438,8 +433,9 @@ static int i_EepromReadAnlogInputHeader(unsigned short w_PCIBoardEepromAddress,
 	return 0;
 }
 
-int i_EepromReadMainHeader(unsigned short w_PCIBoardEepromAddress,
-	char *pc_PCIChipInformation, struct comedi_device *dev)
+static int i_EepromReadMainHeader(unsigned short w_PCIBoardEepromAddress,
+				  char *pc_PCIChipInformation,
+				  struct comedi_device *dev)
 {
 	const struct addi_board *this_board = comedi_board(dev);
 	struct addi_private *devpriv = dev->private;
