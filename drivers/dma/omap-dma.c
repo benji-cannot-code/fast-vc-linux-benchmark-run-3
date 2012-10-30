fb6a2ea64397d2f77c3d86c174fd542165bcc21f
Tony Lindgren <tony@atomide.com>
2012-10-30 11:03:22 -0700

    ARM: OMAP: Remove cpu_is_omap usage from plat-omap/dma.c
    This code will be eventually in drivers, and for the
    code in the drivers we don't want to have any cpu_is_omap
    usage. Those macros should be private to arch/arm/mach-omap1
    and arch/arm/mach-omap2.
    
    To fix this, let's move the define for dma_omap2plus()
    to dma-omap.h, and use the existing dma_attr passed in
    the platform_data as the revision registers are what they
    are.
    
    Note that we can now also remove the relative includes
    introduced by the recent clean-up patches.
    
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Vinod Koul <vinod.koul@intel.com>
    Cc: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/dma/omap-dma.c
+++ drivers/dma/omap-dma.c
@@ -22,12 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <plat-omap/dma-omap.h>
 
-#ifdef CONFIG_ARCH_OMAP2PLUS
-#define dma_omap2plus()	1
-#else
-#define dma_omap2plus()	0
-#endif
-
 struct omap_dmadev {
 	struct dma_device ddev;
 	spinlock_t lock;
