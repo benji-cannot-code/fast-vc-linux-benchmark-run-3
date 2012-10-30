645f28a9829d2cbc2d465c42889224d21b0cbd9d
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2012-10-30 08:07:11 +0800

    arm: at91: move at91_shdwc.h to arch/arm/mach-at91
    This is only used by old boards style or via core code.
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/pm.c
+++ arch/arm/mach-at91/pm.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <mach/at91_rstc.h>
-#include <mach/at91_shdwc.h>
+#include "at91_shdwc.h"
 
 static void __init show_reset_status(void)
 {
--- arch/arm/mach-at91/setup.c
+++ arch/arm/mach-at91/setup.c
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/cpu.h>
 #include <mach/at91_dbgu.h>
 #include <mach/at91_pmc.h>
-#include <mach/at91_shdwc.h>
 
+#include "at91_shdwc.h"
 #include "soc.h"
 #include "generic.h"
 
