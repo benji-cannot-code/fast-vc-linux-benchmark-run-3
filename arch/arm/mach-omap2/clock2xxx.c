cbd66758573bc636c6af2ce5d7b77762c4d2b9de
Paul Walmsley <paul@pwsan.com>
2012-10-29 20:55:53 -0600

    ARM: OMAP2xxx: clock: remove global 'dclk' variable
    Remove the global 'dclk' variable, instead replacing it with a
    variable local to the dpllcore clock type C file.  This removes some
    of the special-case code surrounding the OMAP2xxx clock init.
    
    This patch is a prerequisite for the removal of the
    omap_prcm_restart() code from arch/arm/mach-omap2/prcm.c.  It also
    cleans up some special-case OMAP2xxx clock code in the process.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Tested-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- arch/arm/mach-omap2/clock2xxx.c
+++ arch/arm/mach-omap2/clock2xxx.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cm.h"
 #include "cm-regbits-24xx.h"
 
-struct clk *vclk, *sclk, *dclk;
+struct clk *vclk, *sclk;
 
 /*
  * Omap24xx specific clock functions
