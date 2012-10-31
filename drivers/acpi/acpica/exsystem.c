d0aa345c9269a6849e9b7da7f81707be67991ce0
Bob Moore <robert.moore@intel.com>
2012-10-31 02:26:11 +0000

    ACPICA: ACPICA core: Cleanup empty lines at file start and end
    Maintenance for source code consistency.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/exsystem.c
+++ drivers/acpi/acpica/exsystem.c
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-
 /******************************************************************************
  *
  * Module Name: exsystem - Interface to OS services
