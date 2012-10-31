8b601ea048c8da66a1dfb52e86443a1457eced6b
Kevin McKinney <klmckinney1@gmail.com>
2012-10-30 21:50:44 -0400

    Staging: bcm: Remove typedef stLocalSFAddRequestAlt.
    This patch removes typedef
    stLocalSFAddRequestAlt in CmHost.h.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/CmHost.h
+++ drivers/staging/bcm/CmHost.h
@@ -24,19 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DSX_MESSAGE_EXCHANGE_BUFFER        0xBF60AC84 /* This contains the pointer */
 #define DSX_MESSAGE_EXCHANGE_BUFFER_SIZE   72000      /* 24 K Bytes */
 
-/* brief structure stLocalSFAddRequest */
-typedef struct stLocalSFAddRequestAlt {
-	u8	u8Type;
-	u8	u8Direction;
-	u16	u16TID;
-	/* brief 16bitCID */
-	u16	u16CID;
-	/* brief 16bitVCID */
-	u16	u16VCID;
-	struct bcm_connect_mgr_params sfParameterSet;
-	/* USE_MEMORY_MANAGER(); */
-} stLocalSFAddRequestAlt;
-
 struct bcm_add_indication_alt {
 	u8	u8Type;
 	u8	u8Direction;
