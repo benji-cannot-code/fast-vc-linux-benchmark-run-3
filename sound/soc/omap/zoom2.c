3249b5e0e89b78a6bc3128d3b776b3fc19ed477a
Tony Lindgren <tony@atomide.com>
2012-10-31 09:54:56 -0700

    ARM: OMAP: Remove NEED_MACH_GPIO_H
    Omap no longer needs this option, mach/gpio.h is
    empty.
    
    Also remove mach/irqs.h from gpio-omap.h and
    include it directly from the related omap1
    gpio init files.
    
    Otherwise omap2+ build fails for MULTI_PLATFORM.
    
    Cc: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Cc: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Cc: Liam Girdwood <lrg@ti.com>
    Cc: alsa-devel@alsa-project.org
    Acked-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- sound/soc/omap/zoom2.c
+++ sound/soc/omap/zoom2.c
@@ -27,9 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 
 #include <asm/mach-types.h>
-#include <mach/hardware.h>
-#include <mach/gpio.h>
-#include <mach/board-zoom.h>
 #include <linux/platform_data/asoc-ti-mcbsp.h>
 
 /* Register descriptions for twl4030 codec part */
