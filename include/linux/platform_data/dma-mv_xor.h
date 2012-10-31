2c01278ba228225a0636bb0f6cbf8dec9ce127e4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-10-31 13:24:41 +0100

    dma: mv_xor: remove 'shared' from mv_xor_platform_data
    This member of the platform_data structure is no longer used, so get
    rid of it.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- include/linux/platform_data/dma-mv_xor.h
+++ include/linux/platform_data/dma-mv_xor.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MV_XOR_SHARED_NAME	"mv_xor_shared"
 
 struct mv_xor_platform_data {
-	struct platform_device		*shared;
 	int				hw_id;
 	dma_cap_mask_t			cap_mask;
 	size_t				pool_size;
