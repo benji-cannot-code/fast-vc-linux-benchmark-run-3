4f9637807a0a747f89530900cbf342e8ff6ea197
Richard Cochran <richardcochran@gmail.com>
2012-10-31 06:27:23 +0000

    bfin_mac: replace sys time stamps with raw ones instead.
    This patch replaces the sys time stamps and timecompare code with simple
    raw hardware time stamps in nanosecond resolution. The only tricky bit is
    to find a PTP Hardware Clock period slower than the input clock period
    and a power of two.
    
    Compile tested only.
    
    Signed-off-by: Richard Cochran <richardcochran@gmail.com>
    Tested-by: Bob Liu <lliubbo@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/adi/bfin_mac.h
+++ drivers/net/ethernet/adi/bfin_mac.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _BFIN_MAC_H_
 
 #include <linux/net_tstamp.h>
-#include <linux/clocksource.h>
-#include <linux/timecompare.h>
 #include <linux/timer.h>
 #include <linux/etherdevice.h>
 #include <linux/bfin_mac.h>
@@ -95,9 +93,8 @@ struct bfin_mac_local {
 	struct mii_bus *mii_bus;
 
 #if defined(CONFIG_BFIN_MAC_USE_HWSTAMP)
-	struct cyclecounter cycles;
-	struct timecounter clock;
-	struct timecompare compare;
+	u32 addend;
+	unsigned int shift;
 	struct hwtstamp_config stamp_cfg;
 #endif
 };
