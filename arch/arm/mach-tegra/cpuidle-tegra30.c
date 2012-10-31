12f7985a67c3185df31c11d02ca234b07d91d3d1
Joseph Lo <josephl@nvidia.com>
2012-10-31 17:41:15 +0800

    ARM: tegra: cpuidle: separate cpuidle driver for different chips
    The different Tegra chips may have different CPU idle states and data.
    Individual CPU idle driver make it more easy to maintain.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/cpuidle-tegra30.c
+++ arch/arm/mach-tegra/cpuidle-tegra30.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/arm/mach-tegra/cpuidle.c
- *
  * CPU idle driver for Tegra CPUs
  *
  * Copyright (c) 2010-2012, NVIDIA Corporation.
@@ -24,26 +22,46 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/cpuidle.h>
 
-#include "fuse.h"
-#include "cpuidle.h"
+#include <asm/cpuidle.h>
 
-static int __init tegra_cpuidle_init(void)
+static struct cpuidle_driver tegra_idle_driver = {
+	.name = "tegra_idle",
+	.owner = THIS_MODULE,
+	.en_core_tk_irqen = 1,
+	.state_count = 1,
+	.states = {
+		[0] = ARM_CPUIDLE_WFI_STATE_PWR(600),
+	},
+};
+
+static DEFINE_PER_CPU(struct cpuidle_device, tegra_idle_device);
+
+int __init tegra30_cpuidle_init(void)
 {
 	int ret;
+	unsigned int cpu;
+	struct cpuidle_device *dev;
+	struct cpuidle_driver *drv = &tegra_idle_driver;
 
-	switch (tegra_chip_id) {
-	case TEGRA20:
-		ret = tegra20_cpuidle_init();
-		break;
-	case TEGRA30:
-		ret = tegra30_cpuidle_init();
-		break;
-	default:
-		ret = -ENODEV;
-		break;
+	ret = cpuidle_register_driver(&tegra_idle_driver);
+	if (ret) {
+		pr_err("CPUidle driver registration failed\n");
+		return ret;
 	}
 
-	return ret;
+	for_each_possible_cpu(cpu) {
+		dev = &per_cpu(tegra_idle_device, cpu);
+		dev->cpu = cpu;
+
+		dev->state_count = drv->state_count;
+		ret = cpuidle_register_device(dev);
+		if (ret) {
+			pr_err("CPU%u: CPUidle device registration failed\n",
+				cpu);
+			return ret;
+		}
+	}
+	return 0;
 }
-device_initcall(tegra_cpuidle_init);
