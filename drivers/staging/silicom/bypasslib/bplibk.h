6825eea797d842264840276f0e342f6ff253cfd5
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2012-11-01 13:47:19 +0800

    Staging: silicom: bypasslib: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    dpatch engine is used to auto generate this patch.
    (https://github.com/weiyj/dpatch)
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/silicom/bypasslib/bplibk.h
+++ drivers/staging/silicom/bypasslib/bplibk.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "bp_ioctl.h"
 #include "libbp_sd.h"
-#include <linux/version.h>
 
 #define IF_NAME            "eth"
 #define SILICOM_VID        0x1374
