c7afa7104446bec1df8852b71ebb6671ff03430f
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2012-11-01 13:47:45 +0800

    Staging: silicom: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    dpatch engine is used to auto generate this patch.
    (https://github.com/weiyj/dpatch)
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/silicom/bp_mod.c
+++ drivers/staging/silicom/bp_mod.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*                                                                            */
 /*                                                                            */
 /******************************************************************************/
-#include <linux/version.h>
 
 #include <linux/kernel.h>	/* We're doing kernel work */
 #include <linux/module.h>	/* Specifically, a module */
