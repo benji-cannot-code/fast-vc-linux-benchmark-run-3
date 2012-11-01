79376bf77521ded67fdb8cd83de9cf824e5ceb0c
Liu Bo <bo.li.liu@oracle.com>
2012-11-01 06:38:48 +0000

    Btrfs: get right arguments for btrfs_wait_ordered_range
    btrfs_wait_ordered_range expects for 'len' instead of 'end'.
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
    Signed-off-by: Chris Mason <chris.mason@fusionio.com>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -1563,7 +1563,7 @@ int btrfs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 	 * range being left.
 	 */
 	atomic_inc(&root->log_batch);
-	btrfs_wait_ordered_range(inode, start, end);
+	btrfs_wait_ordered_range(inode, start, end - start + 1);
 	atomic_inc(&root->log_batch);
 
 	/*
