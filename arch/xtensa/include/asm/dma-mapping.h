6839737c2c17d3c9373d1f5f6f2e18bceb0c5a67
Max Filippov <jcmvbkbc@gmail.com>
2012-11-01 18:38:27 +0400

    xtensa: provide DMA_ERROR_CODE definition
    This fixes the following allmodconfig build error:
    
    drivers/uio/uio_dmem_genirq.c:95:18: error: 'DMA_ERROR_CODE' undeclared (first use in this function)
    drivers/uio/uio_dmem_genirq.c:238:18: error: 'DMA_ERROR_CODE' undeclared (first use in this function)
    make[3]: *** [drivers/uio/uio_dmem_genirq.o] Error 1
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- arch/xtensa/include/asm/dma-mapping.h
+++ arch/xtensa/include/asm/dma-mapping.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/scatterlist.h>
 
+#define DMA_ERROR_CODE		(~(dma_addr_t)0x0)
+
 /*
  * DMA-consistent mapping functions.
  */
