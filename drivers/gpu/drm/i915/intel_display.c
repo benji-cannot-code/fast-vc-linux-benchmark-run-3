ddcaff3677c20a2b0d79b72c219b981843b2775e
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-11-01 09:15:30 +0100

    drm/i915: CPT+ pch transcoder workaround
    We need to set the timing override chicken bit after fdi link training
    has completed and before we enable the transcoder. We also have to
    clear that bit again after disabling the pch transcoder.
    
    See "Graphics BSpec: vol4g North Display Engine Registers [IVB],
    Display Mode Set Sequence" and "Graphics BSpec: vol4h South Display
    Engine Registers [CPT, PPT], South Display Engine Transcoder and FDI
    Control, Transcoder Debug and DFT, TRANS_CHICKEN_2" bit 31:
    
    "Workaround : Enable the override prior to enabling the transcoder.
    Disable the override after disabling the transcoder."
    
    While at it, use the _PIPE macro for the other TRANS_DP register.
    
    v2: Keep the w/a as-is, but kill the original (but wrongly placed)
    workaround introduced in
    
    commit 3bcf603f6d5d18bd9d076dc280de71f48add4101
    Author: Jesse Barnes <jbarnes@virtuousgeek.org>
    Date:   Wed Jul 27 11:51:40 2011 -0700
    
        drm/i915: apply timing generator bug workaround on CPT and PPT
    
    and
    
    commit d4270e57efe9e2536798c59e1ed2fd0a1e5cdfcf
    Author: Jesse Barnes <jbarnes@virtuousgeek.org>
    Date:   Tue Oct 11 10:43:02 2011 -0700
    
        drm/i915: export a CPT mode set verification function
    
    Note that this old code has unconditionally set the w/a, which might
    explain why fdi link training sometimes silently fails, and especially
    why the auto-train did not seem to work properly.
    
    v3: Paulo Zanoni pointed out that this workaround is also required on
    the LPT PCH. And Arthur Ranyan confirmed that this workaround is
    requierd for all ports on the pch, not just DP: The important part
    is that the bit is set whenever the pch transcoder is enabled, and
    that it is _not_ set while the fdi link is trained. It is also
    important that the pch transcoder is fully disabled, i.e. we have to
    wait for bit 30 to clear before clearing the w/a bit.
    
    Hence move to workaround into enable/disable_transcoder, where the pch
    transcoder gets enabled/disabled.
    
    v4: Whitespace changes dropped.
    
    v5: Don't run the w/a on IBX, we only need it on CPT/PPT and LPT.
    
    v6:
    - resolve conflicts with Paulo's big hsw vga rework
    - s/!IBX/CPT since hsw paths are now all separate, and Paulo's patch
      to implement the equivalent w/a for LPT is already merged.
    
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
    Cc: Paulo Zanoni <przanoni@gmail.com>
    Cc: Arthur Ranyan <arthur.j.runyan@intel.com>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com> (v5)
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org> (v5)
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -1674,9 +1674,9 @@ static void intel_disable_pch_pll(struct intel_crtc *intel_crtc)
 static void ironlake_enable_pch_transcoder(struct drm_i915_private *dev_priv,
 					   enum pipe pipe)
 {
-	int reg;
-	u32 val, pipeconf_val;
+	struct drm_device *dev = dev_priv->dev;
 	struct drm_crtc *crtc = dev_priv->pipe_to_crtc_mapping[pipe];
+	uint32_t reg, val, pipeconf_val;
 
 	/* PCH only available on ILK+ */
 	BUG_ON(dev_priv->info->gen < 5);
@@ -1689,6 +1689,15 @@ static void ironlake_enable_pch_transcoder(struct drm_i915_private *dev_priv,
 	/* FDI must be feeding us bits for PCH ports */
 	assert_fdi_tx_enabled(dev_priv, pipe);
 	assert_fdi_rx_enabled(dev_priv, pipe);
+
+	if (HAS_PCH_CPT(dev)) {
+		/* Workaround: Set the timing override bit before enabling the
+		 * pch transcoder. */
+		reg = TRANS_CHICKEN2(pipe);
+		val = I915_READ(reg);
+		val |= TRANS_CHICKEN2_TIMING_OVERRIDE;
+		I915_WRITE(reg, val);
+	}
 
 	reg = TRANSCONF(pipe);
 	val = I915_READ(reg);
@@ -1732,7 +1741,7 @@ static void lpt_enable_pch_transcoder(struct drm_i915_private *dev_priv,
 
 	/* Workaround: set timing override bit. */
 	val = I915_READ(_TRANSA_CHICKEN2);
-	val |= TRANS_AUTOTRAIN_GEN_STALL_DIS;
+	val |= TRANS_CHICKEN2_TIMING_OVERRIDE;
 	I915_WRITE(_TRANSA_CHICKEN2, val);
 
 	val = TRANS_ENABLE;
@@ -1752,8 +1761,8 @@ static void lpt_enable_pch_transcoder(struct drm_i915_private *dev_priv,
 static void ironlake_disable_pch_transcoder(struct drm_i915_private *dev_priv,
 					    enum pipe pipe)
 {
-	int reg;
-	u32 val;
+	struct drm_device *dev = dev_priv->dev;
+	uint32_t reg, val;
 
 	/* FDI relies on the transcoder */
 	assert_fdi_tx_disabled(dev_priv, pipe);
@@ -1769,6 +1778,14 @@ static void ironlake_disable_pch_transcoder(struct drm_i915_private *dev_priv,
 	/* wait for PCH transcoder off, transcoder state */
 	if (wait_for((I915_READ(reg) & TRANS_STATE_ENABLE) == 0, 50))
 		DRM_ERROR("failed to disable transcoder %d\n", pipe);
+
+	if (!HAS_PCH_IBX(dev)) {
+		/* Workaround: Clear the timing override chicken bit again. */
+		reg = TRANS_CHICKEN2(pipe);
+		val = I915_READ(reg);
+		val &= ~TRANS_CHICKEN2_TIMING_OVERRIDE;
+		I915_WRITE(reg, val);
+	}
 }
 
 static void lpt_disable_pch_transcoder(struct drm_i915_private *dev_priv)
@@ -1784,7 +1801,7 @@ static void lpt_disable_pch_transcoder(struct drm_i915_private *dev_priv)
 
 	/* Workaround: clear timing override bit. */
 	val = I915_READ(_TRANSA_CHICKEN2);
-	val &= ~TRANS_AUTOTRAIN_GEN_STALL_DIS;
+	val &= ~TRANS_CHICKEN2_TIMING_OVERRIDE;
 	I915_WRITE(_TRANSA_CHICKEN2, val);
 }
 
@@ -3328,16 +3345,12 @@ prepare: /* separate function? */
 void intel_cpt_verify_modeset(struct drm_device *dev, int pipe)
 {
 	struct drm_i915_private *dev_priv = dev->dev_private;
-	int dslreg = PIPEDSL(pipe), tc2reg = TRANS_CHICKEN2(pipe);
+	int dslreg = PIPEDSL(pipe);
 	u32 temp;
 
 	temp = I915_READ(dslreg);
 	udelay(500);
 	if (wait_for(I915_READ(dslreg) != temp, 5)) {
-		/* Without this, mode sets may fail silently on FDI */
-		I915_WRITE(tc2reg, TRANS_AUTOTRAIN_GEN_STALL_DIS);
-		udelay(250);
-		I915_WRITE(tc2reg, 0);
 		if (wait_for(I915_READ(dslreg) != temp, 5))
 			DRM_ERROR("mode set failed: pipe %d stuck\n", pipe);
 	}
