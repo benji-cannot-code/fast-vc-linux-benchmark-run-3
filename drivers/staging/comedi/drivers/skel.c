5b2c13121c2178ff8964c24562c866d177fc0448
Ian Abbott <abbotti@mev.co.uk>
2012-11-01 14:02:14 +0000

    staging: comedi: skel: replace pr_...() with dev_...()
    Use the dev_info() etc. instead of pr_info() etc.  Remove a couple of
    them that seemed a bit pointless.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/skel.c
+++ drivers/staging/comedi/drivers/skel.c
@@ -206,8 +206,6 @@ static int skel_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	struct comedi_subdevice *s;
 	int ret;
 
-	pr_info("comedi%d: skel: ", dev->minor);
-
 /*
  * If you can probe the device to determine what device in a series
  * it is, this is the place to do it.  Otherwise, dev->board_ptr
@@ -273,7 +271,7 @@ static int skel_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 		s->type = COMEDI_SUBD_UNUSED;
 	}
 
-	pr_info("attached\n");
+	dev_info(dev->class_dev, "skel: attached\n");
 
 	return 0;
 }
@@ -323,9 +321,7 @@ static int skel_ai_rinsn(struct comedi_device *dev, struct comedi_subdevice *s,
 				break;
 		}
 		if (i == TIMEOUT) {
-			/* printk() should be used instead of printk()
-			 * whenever the code can be called from real-time. */
-			pr_info("timeout\n");
+			dev_warn(dev->class_dev, "ai timeout\n");
 			return -ETIMEDOUT;
 		}
 
@@ -500,7 +496,6 @@ static int skel_ao_winsn(struct comedi_device *dev, struct comedi_subdevice *s,
 	int i;
 	int chan = CR_CHAN(insn->chanspec);
 
-	pr_info("skel_ao_winsn\n");
 	/* Writing a list of values to an AO channel is probably not
 	 * very useful, but that's how the interface is defined. */
 	for (i = 0; i < insn->n; i++) {
