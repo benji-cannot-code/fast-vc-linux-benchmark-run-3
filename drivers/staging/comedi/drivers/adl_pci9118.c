39265c0d917e490a6b870962d4549b30185455bc
Ian Abbott <abbotti@mev.co.uk>
2012-11-02 13:52:31 +0000

    staging: comedi: adl_pci9118: cards have same vendor ID
    Don't bother recording the PCI vendor ID of each board in `boardtypes[]`
    as they are all the same and the code doesn't use it anyway.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adl_pci9118.c
+++ drivers/staging/comedi/drivers/adl_pci9118.c
@@ -209,8 +209,7 @@ static const struct comedi_lrange range_pci9118hg = { 8, {
 
 struct boardtype {
 	const char *name;		/* board name */
-	int vendor_id;			/* PCI vendor a device ID of card */
-	int device_id;
+	int device_id;			/* PCI device ID of card */
 	int iorange_amcc;		/* iorange for own S5933 region */
 	int iorange_9118;		/* pass thru card region size */
 	int n_aichan;			/* num of A/D chans */
@@ -2132,7 +2131,6 @@ static void pci9118_detach(struct comedi_device *dev)
 static const struct boardtype boardtypes[] = {
 	{
 		.name		= "pci9118dg",
-		.vendor_id	= PCI_VENDOR_ID_AMCC,
 		.device_id	= 0x80d9,
 		.iorange_amcc	= AMCC_OP_REG_SIZE,
 		.iorange_9118	= IORANGE_9118,
@@ -2150,7 +2148,6 @@ static const struct boardtype boardtypes[] = {
 		.half_fifo_size	= 512,
 	}, {
 		.name		= "pci9118hg",
-		.vendor_id	= PCI_VENDOR_ID_AMCC,
 		.device_id	= 0x80d9,
 		.iorange_amcc	= AMCC_OP_REG_SIZE,
 		.iorange_9118	= IORANGE_9118,
@@ -2168,7 +2165,6 @@ static const struct boardtype boardtypes[] = {
 		.half_fifo_size	= 512,
 	}, {
 		.name		= "pci9118hr",
-		.vendor_id	= PCI_VENDOR_ID_AMCC,
 		.device_id	= 0x80d9,
 		.iorange_amcc	= AMCC_OP_REG_SIZE,
 		.iorange_9118	= IORANGE_9118,
