903852dfc206ceef7142da0e9bc7aaa0a0ffeb22
Jani Nikula <jani.nikula@intel.com>
2012-11-02 10:19:55 +0200

    drm/i915: pass adjusted_mode to intel_choose_pipe_bpp_dither(), again
    Daniel's backmerge
    
    commit c2fb7916927e989ea424e61ce5fe617e54878827
    Merge: 29de6ce 6f0c058
    Author: Daniel Vetter <daniel.vetter@ffwll.ch>
    Date:   Mon Oct 22 14:34:51 2012 +0200
    
        Merge tag 'v3.7-rc2' into drm-intel-next-queued
    
    to solve conflicts blew up (either git or Daniel was trying to be too
    clever for their own good; it's usually convenient to blame tools ;) and
    caused the changes of
    
    commit 0c96c65b48fba3ffe9822a554cbc0cd610765cd5
    Author: Jani Nikula <jani.nikula@intel.com>
    Date:   Wed Sep 26 18:43:10 2012 +0300
    
        drm/i915: use adjusted_mode instead of mode for checking the 6bpc force flag
    
    in ironlake_crtc_mode_set() to be dropped.
    
    Fix the call in ironlake_crtc_mode_set() again, and while at it, also fix
    the new, copy-pasted haswell_crtc_mode_set() to use adjusted_mode.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -5230,7 +5230,8 @@ static int ironlake_crtc_mode_set(struct drm_crtc *crtc,
 	intel_crtc_update_cursor(crtc, true);
 
 	/* determine panel color depth */
-	dither = intel_choose_pipe_bpp_dither(crtc, fb, &intel_crtc->bpp, mode);
+	dither = intel_choose_pipe_bpp_dither(crtc, fb, &intel_crtc->bpp,
+					      adjusted_mode);
 	if (is_lvds && dev_priv->lvds_dither)
 		dither = true;
 
@@ -5428,7 +5429,8 @@ static int haswell_crtc_mode_set(struct drm_crtc *crtc,
 	intel_crtc_update_cursor(crtc, true);
 
 	/* determine panel color depth */
-	dither = intel_choose_pipe_bpp_dither(crtc, fb, &intel_crtc->bpp, mode);
+	dither = intel_choose_pipe_bpp_dither(crtc, fb, &intel_crtc->bpp,
+					      adjusted_mode);
 	if (is_lvds && dev_priv->lvds_dither)
 		dither = true;
 
