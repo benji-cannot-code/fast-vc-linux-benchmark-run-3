da6b464db310d839d5a290e34bafc753e044f6b4
Aaro Koskinen <aaro.koskinen@iki.fi>
2012-11-04 21:14:44 +0200

    staging: xgifb: XGI_GetLcdPtr: use real pointers for data tables
    Use real pointers for LCD data tables to simplify the code.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/vb_struct.h
+++ drivers/staging/xgifb/vb_struct.h
@@ -57,7 +57,7 @@ struct XGI330_LCDDataTablStruct {
 	unsigned char  PANELID;
 	unsigned short MASK;
 	unsigned short CAP;
-	unsigned short DATAPTR;
+	void *DATAPTR;
 };
 
 struct XGI330_TVDataTablStruct {
