d66eef546515fcfdd9e5e5eb71ba836cd0593b82
Aaro Koskinen <aaro.koskinen@iki.fi>
2012-11-04 21:14:54 +0200

    staging: xgifb: eliminate pVBInfo->XGINEWUB_CRT1Table
    Access XGI_CRT1Table directly and make it const.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/vb_def.h
+++ drivers/staging/xgifb/vb_def.h
@@ -262,5 +262,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern const struct XGI_ExtStruct XGI330_EModeIDTable[];
 extern const struct XGI_Ext2Struct XGI330_RefIndex[];
+extern const struct XGI_CRT1TableStruct XGI_CRT1Table[];
 
 #endif
