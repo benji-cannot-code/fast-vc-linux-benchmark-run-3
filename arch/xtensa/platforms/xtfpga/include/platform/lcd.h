875dcad0c74c4273506e99788855761212f9d2a3
Max Filippov <jcmvbkbc@gmail.com>
2012-11-05 07:37:14 +0400

    xtensa: add support for the XTFPGA boards
    The Avnet LX60/LX110/LX200 board is an FPGA board that can be configured with
    an Xtensa processor and an OpenCores Ethernet device.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Chris Zankel <chris@zankel.net>

---
+++ arch/xtensa/platforms/xtfpga/include/platform/lcd.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * arch/xtensa/platform/xtavnet/include/platform/lcd.h
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2001, 2006 Tensilica Inc.
+ */
+
+#ifndef __XTENSA_XTAVNET_LCD_H
+#define __XTENSA_XTAVNET_LCD_H
+
+/* Display string STR at position POS on the LCD. */
+void lcd_disp_at_pos(char *str, unsigned char pos);
+
+/* Shift the contents of the LCD display left or right. */
+void lcd_shiftleft(void);
+void lcd_shiftright(void);
+#endif
