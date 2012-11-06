7ce4e647668116fe5805ffd3cf98c91b0e7ee11f
Johannes Berg <johannes.berg@intel.com>
2012-11-06 16:36:21 +0100

    iwlwifi: return commands with error on FW error
    When a firmware error occurs, don't just abort synchronous
    commands but also return an error (-EIO) and block any new
    commands as well. Currently, an error is only returned if
    WANT_SKB was set which is confusing and can lead to issues.
    
    Blocking is done until a new firmware image is loaded.
    
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwlwifi/pcie/trans.c
@@ -1027,6 +1027,7 @@ static int iwl_load_given_ucode(struct iwl_trans *trans,
 static int iwl_trans_pcie_start_fw(struct iwl_trans *trans,
 				   const struct fw_img *fw)
 {
+	struct iwl_trans_pcie *trans_pcie = IWL_TRANS_GET_PCIE_TRANS(trans);
 	int ret;
 	bool hw_rfkill;
 
@@ -1035,6 +1036,8 @@ static int iwl_trans_pcie_start_fw(struct iwl_trans *trans,
 		IWL_WARN(trans, "Exit HW not ready\n");
 		return -EIO;
 	}
+
+	clear_bit(STATUS_FW_ERROR, &trans_pcie->status);
 
 	iwl_enable_rfkill_int(trans);
 
