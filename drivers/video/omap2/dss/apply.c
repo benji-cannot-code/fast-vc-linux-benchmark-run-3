25461f9e5b12d599df5782f46b421dd7d301aba3
Archit Taneja <archit@ti.com>
2012-11-07 14:47:24 +0530

    OMAPDSS: APPLY: Remove unnecessary call to mg_clear_shadow_dirty
    When doing a manual update in dss_mgr_start_update, we clear the shadow dirty
    flags. Although there isn't any harm in clearing them. The need to clear them
    out here should never arrive.
    
    When applying configurations for a manual update manager, we never do any
    register writes, i.e, calls to dss_mgr_write_regs and dss_mgr_write_regs_extra
    never happen while applying. We do all these writes only when we call
    dss_mgr_start_update. Hence, there is never a time when the shadow registers
    are dirty.
    
    Remove the call to mg_clear_shadow_dirty.
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/apply.c
+++ drivers/video/omap2/dss/apply.c
@@ -771,8 +771,6 @@ void dss_mgr_start_update(struct omap_overlay_manager *mgr)
 
 	dispc_mgr_enable_sync(mgr->id);
 
-	mgr_clear_shadow_dirty(mgr);
-
 	spin_unlock_irqrestore(&data_lock, flags);
 }
 
