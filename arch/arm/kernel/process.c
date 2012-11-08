fd81d1e2be3d2e998f01c90f254fb5994b77dc96
Nicolas Pitre <nicolas.pitre@linaro.org>
2012-11-08 19:54:11 +0100

    ARM: 7574/1: kernel/process.c: include idmap.h instead of redeclaring setup_mm_for_reboot()
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/leds.h>
 
 #include <asm/cacheflush.h>
+#include <asm/idmap.h>
 #include <asm/processor.h>
 #include <asm/thread_notify.h>
 #include <asm/stacktrace.h>
@@ -56,8 +57,6 @@ static const char *processor_modes[] = {
 static const char *isa_modes[] = {
   "ARM" , "Thumb" , "Jazelle", "ThumbEE"
 };
-
-extern void setup_mm_for_reboot(void);
 
 static volatile int hlt_counter;
 
