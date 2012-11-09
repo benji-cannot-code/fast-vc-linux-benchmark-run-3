1c750115314e859ec989c032bcf9f29fa99e71b7
Davide Ciminaghi <ciminaghi@gnudd.com>
2012-11-09 15:20:01 +0100

    mfd: sta2x11-mfd: Add myself to copyright
    Signed-off-by: Davide Ciminaghi <ciminaghi@gnudd.com>
    Acked-by: Alessandro Rubini <rubini@gnudd.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/sta2x11-mfd.c
+++ drivers/mfd/sta2x11-mfd.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright (c) 2009-2011 Wind River Systems, Inc.
- * Copyright (c) 2011 ST Microelectronics (Alessandro Rubini)
+ * Copyright (c) 2011 ST Microelectronics (Alessandro Rubini, Davide Ciminaghi)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
