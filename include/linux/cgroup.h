60f979d804afe66239464be222e46a42794466fe
Tejun Heo <tj@kernel.org>
2012-11-09 09:12:29 -0800

    cgroup: use rculist ops for cgroup->children
    Use RCU safe list operations for cgroup->children.  This will be used
    to implement cgroup children / descendant walking which can be used by
    controllers.
    
    Note that cgroup_create() now puts a new cgroup at the end of the
    ->children list instead of head.  This isn't strictly necessary but is
    done so that the iteration order is more conventional.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Michal Hocko <mhocko@suse.cz>
    Reviewed-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Acked-by: Li Zefan <lizefan@huawei.com>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpumask.h>
 #include <linux/nodemask.h>
 #include <linux/rcupdate.h>
+#include <linux/rculist.h>
 #include <linux/cgroupstats.h>
 #include <linux/prio_heap.h>
 #include <linux/rwsem.h>
