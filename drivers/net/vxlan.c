82500eb7145707711dae3afa7f1aaaa98ffcb382
Alexander Duyck <alexander.h.duyck@intel.com>
2012-11-09 13:35:24 +0000

    vxlan: Fix error that was resulting in VXLAN MTU size being 10 bytes too large
    This change fixes an issue I found where VXLAN frames were fragmented when
    they were up to the VXLAN MTU size.  I root caused the issue to the fact that
    the headroom was 4 + 20 + 8 + 8.  This math doesn't appear to be correct
    because we are not inserting a VLAN header, but instead a 2nd Ethernet header.
    As such the math for the overhead should be 20 + 8 + 8 + 14 to account for the
    extra headers that are inserted for VXLAN.
    
    Signed-off-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -51,8 +51,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VXLAN_N_VID	(1u << 24)
 #define VXLAN_VID_MASK	(VXLAN_N_VID - 1)
-/* VLAN + IP header + UDP + VXLAN */
-#define VXLAN_HEADROOM (4 + 20 + 8 + 8)
+/* IP header + UDP + VXLAN + Ethernet header */
+#define VXLAN_HEADROOM (20 + 8 + 8 + 14)
 
 #define VXLAN_FLAGS 0x08000000	/* struct vxlanhdr.vx_flags required value. */
 
