d4c1747a5b17f9c4c9b77fd3635765f360a17448
Rami Rosen <ramirose@gmail.com>
2012-11-13 13:29:15 +0000

    vxlan: fix a typo.
    Use eXtensible and not eXtensiable in the comment on top.
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Acked-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * VXLAN: Virtual eXtensiable Local Area Network
+ * VXLAN: Virtual eXtensible Local Area Network
  *
  * Copyright (c) 2012 Vyatta Inc.
  *
