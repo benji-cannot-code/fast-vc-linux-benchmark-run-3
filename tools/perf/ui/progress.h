7455becb72300cf569a9e846d266ac898c994905
Namhyung Kim <namhyung.kim@lge.com>
2012-11-13 22:30:34 +0900

    perf ui: Add ui_progress__finish()
    Sometimes we need to know when the progress bar should disappear.
    
    Checking curr >= total wasn't enough since there're cases not met that
    condition for the last call.
    
    So add a new ->finish callback to identify this explicitly.  Currently
    only GTK frontend needs it.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1352813436-14173-4-git-send-email-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/ui/progress.h
+++ tools/perf/ui/progress.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct ui_progress {
 	void (*update)(u64, u64, const char *);
+	void (*finish)(void);
 };
 
 extern struct ui_progress *progress_fns;
@@ -13,5 +14,6 @@ extern struct ui_progress *progress_fns;
 void ui_progress__init(void);
 
 void ui_progress__update(u64 curr, u64 total, const char *title);
+void ui_progress__finish(void);
 
 #endif
