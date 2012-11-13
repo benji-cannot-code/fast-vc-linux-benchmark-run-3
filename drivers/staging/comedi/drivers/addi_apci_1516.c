bb87c83657755962487b084b61f60202d8b27516
H Hartley Sweeten <hartleys@visionengravers.com>
2012-11-13 13:40:02 -0700

    staging: comedi: addi_apci_1516: remove unused define
    The COMEDI_SUBD_TTLIO define is not used by this driver. Remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi_apci_1516.c
+++ drivers/staging/comedi/drivers/addi_apci_1516.c
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "addi-data/addi_common.h"
 
-#ifndef COMEDI_SUBD_TTLIO
-#define COMEDI_SUBD_TTLIO   11	/* Digital Input Output But TTL */
-#endif
-
 #include "addi-data/hwdrv_apci1516.c"
 
 static const struct addi_board apci1516_boardtypes[] = {
