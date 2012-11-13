732a93c4d1b47c71045f7a12973b453b1244b754
Joseph Lo <josephl@nvidia.com>
2012-11-13 10:04:48 +0800

    ARM: tegra: retain L2 content over CPU suspend/resume
    The L2 RAM is in different power domain from the CPU cluster. So the
    L2 content can be retained over CPU suspend/resume. To do that, we
    need to disable L2 after the MMU is disabled, and enable L2 before
    the MMU is enabled. But the L2 controller is in the same power domain
    with the CPU cluster. We need to restore it's settings and re-enable
    it after the power be resumed.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Acked-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/pm.h
+++ arch/arm/mach-tegra/pm.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MACH_TEGRA_PM_H_
 #define _MACH_TEGRA_PM_H_
 
+extern unsigned long l2x0_saved_regs_addr;
+
 void save_cpu_arch_register(void);
 void restore_cpu_arch_register(void);
 
