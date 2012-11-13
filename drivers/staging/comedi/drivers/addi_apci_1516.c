b7c0f59da32f4bec9060fe7e702e98940882103c
H Hartley Sweeten <hartleys@visionengravers.com>
2012-11-13 13:40:18 -0700

    staging: comedi: addi_apci_1516: remove unnecessary include
    This include is not needed.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi_apci_1516.c
+++ drivers/staging/comedi/drivers/addi_apci_1516.c
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../comedidev.h"
 #include "comedi_fc.h"
-#include "amcc_s5933.h"
 
 #include "addi-data/addi_common.h"
 
