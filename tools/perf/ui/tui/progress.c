165986404b155b7d440d8749d10c4ab334c27219
Namhyung Kim <namhyung.kim@lge.com>
2012-11-13 22:30:31 +0900

    perf ui tui: Move progress.c under ui/tui directory
    Current ui_progress functions are implemented for TUI only.  So move the
    file under the tui directory.  This is needed for providing an UI-
    agnostic wrapper.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1352813436-14173-1-git-send-email-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/ui/tui/progress.c
+++ tools/perf/ui/tui/progress.c
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../cache.h"
-#include "progress.h"
-#include "libslang.h"
-#include "ui.h"
-#include "browser.h"
+#include "../progress.h"
+#include "../libslang.h"
+#include "../ui.h"
+#include "../browser.h"
 
 void ui_progress__update(u64 curr, u64 total, const char *title)
 {
