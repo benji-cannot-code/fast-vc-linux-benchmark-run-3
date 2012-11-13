5f519dfc8d7d9d4135531969a041ba63fbbab786
Akeem G. Abodunrin <akeem.g.abodunrin@intel.com>
2012-11-13 04:03:25 +0000

    igb: Ethtool support to enable and disable EEE
    This patch allows users to enable and disable EEE using Ethtool.
    It also allows users to get EEE settings, as supported by the device.
    
    Signed-off-by: Akeem G Abodunrin <akeem.g.abodunrin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/igb/e1000_defines.h
+++ drivers/net/ethernet/intel/igb/e1000_defines.h
@@ -875,6 +875,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define E1000_EEER_FRC_AN            0x10000000  /* Enable EEE in loopback */
 #define E1000_EEER_LPI_FC            0x00040000  /* EEE Enable on FC */
 #define E1000_EEE_SU_LPI_CLK_STP     0X00800000  /* EEE LPI Clock Stop */
+#define E1000_EEER_EEE_NEG           0x20000000  /* EEE capability nego */
 
 /* SerDes Control */
 #define E1000_GEN_CTL_READY             0x80000000
