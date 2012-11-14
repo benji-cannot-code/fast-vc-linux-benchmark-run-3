9ed44bb92185332cfcacdd0577a3fe9adcd7e8cf
Al Viro <viro@zeniv.linux.org.uk>
2012-11-13 23:47:37 -0500

    bfin: switch to generic vfork, get rid of pointless wrappers
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/blackfin/include/asm/unistd.h
+++ arch/blackfin/include/asm/unistd.h
@@ -448,6 +448,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_WANT_SYS_RT_SIGACTION
 #define __ARCH_WANT_SYS_RT_SIGSUSPEND
 #define __ARCH_WANT_SYS_EXECVE
+#define __ARCH_WANT_SYS_VFORK
 
 /*
  * "Conditional" syscalls
