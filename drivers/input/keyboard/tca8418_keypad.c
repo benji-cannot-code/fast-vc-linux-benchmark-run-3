8e25cfb8578e32444402bfdbbc53a119817416fa
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2012-11-14 08:16:15 -0800

    Input: tca8418_keypad - move device ID tables closer to where they are used
    This matches structure of most other input drivers.
    
    Reviewed-by: Alban Bedel <alban.bedel@avionic-design.de>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/keyboard/tca8418_keypad.c
+++ drivers/input/keyboard/tca8418_keypad.c
@@ -111,21 +111,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KEY_EVENT_CODE		0x7f
 #define KEY_EVENT_VALUE		0x80
 
-
-static const struct i2c_device_id tca8418_id[] = {
-	{ TCA8418_NAME, 8418, },
-	{ }
-};
-MODULE_DEVICE_TABLE(i2c, tca8418_id);
-
-#ifdef CONFIG_OF
-static const struct of_device_id tca8418_dt_ids[] __devinitconst = {
-	{ .compatible = "ti,tca8418", },
-	{ }
-};
-MODULE_DEVICE_TABLE(of, tca8418_dt_ids);
-#endif
-
 struct tca8418_keypad {
 	unsigned int irq;
 	unsigned int row_shift;
@@ -419,6 +404,20 @@ static int tca8418_keypad_remove(struct i2c_client *client)
 
 	return 0;
 }
+
+static const struct i2c_device_id tca8418_id[] = {
+	{ TCA8418_NAME, 8418, },
+	{ }
+};
+MODULE_DEVICE_TABLE(i2c, tca8418_id);
+
+#ifdef CONFIG_OF
+static const struct of_device_id tca8418_dt_ids[] __devinitconst = {
+	{ .compatible = "ti,tca8418", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, tca8418_dt_ids);
+#endif
 
 static struct i2c_driver tca8418_keypad_driver = {
 	.driver = {
