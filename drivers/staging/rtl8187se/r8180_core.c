4b5867fb0762189b00e8ab8fc65dba0d289657aa
Kumar Amit Mehta <gmate.amit@gmail.com>
2012-11-14 15:30:57 +0530

    staging: rtl8187se: r8180_core.c: Inspect the return value of register_netdev()
    Inspect the return value of register_netdev() in the driver probe routine and
    return -ENODEV in case of error.
    
    Signed-off-by: Kumar Amit Mehta <gmate.amit@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8187se/r8180_core.c
+++ drivers/staging/rtl8187se/r8180_core.c
@@ -3259,7 +3259,8 @@ static int __devinit rtl8180_pci_probe(struct pci_dev *pdev,
 
 	netif_carrier_off(dev);
 
-	register_netdev(dev);
+	if (register_netdev(dev))
+		goto fail1;
 
 	rtl8180_proc_init_one(dev);
 
