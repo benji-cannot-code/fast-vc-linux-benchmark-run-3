4273e124f5c92e111eef977aca3766086b1f9fde
Daniel Mack <zonque@gmail.com>
2012-11-14 11:14:26 +0800

    spi/omap: fix D0/D1 direction confusion
    0384e90b8 ("spi/mcspi: allow configuration of pin directions") did what
    it claimed to do the wrong way around. D0/D1 is configured as output by
    *clearing* the bits in the conf registers, hence also breaking the
    former default behaviour.
    
    Fix this before that change is merged to mainline.
    
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/linux/platform_data/spi-omap2-mcspi.h
+++ include/linux/platform_data/spi-omap2-mcspi.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define OMAP4_MCSPI_REG_OFFSET 0x100
 
-#define MCSPI_PINDIR_D0_OUT_D1_IN	0
-#define MCSPI_PINDIR_D0_IN_D1_OUT	1
+#define MCSPI_PINDIR_D0_IN_D1_OUT	0
+#define MCSPI_PINDIR_D0_OUT_D1_IN	1
 
 struct omap2_mcspi_platform_config {
 	unsigned short	num_cs;
