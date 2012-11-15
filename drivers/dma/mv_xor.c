b5d422951c4943b353aec6cabe7c3f1d7da8fe60
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-11-15 16:11:18 +0100

    dma: mv_xor: add missing free_irq() call
    Even though the driver cannot be unloaded at the moment, it is still
    good to properly free the IRQ handlers in the channel removal function.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- drivers/dma/mv_xor.c
+++ drivers/dma/mv_xor.c
@@ -1083,6 +1083,8 @@ static int mv_xor_channel_remove(struct mv_xor_chan *mv_chan)
 		list_del(&chan->device_node);
 	}
 
+	free_irq(mv_chan->irq, mv_chan);
+
 	return 0;
 }
 
@@ -1102,6 +1104,7 @@ mv_xor_channel_add(struct mv_xor_device *xordev,
 	}
 
 	mv_chan->idx = idx;
+	mv_chan->irq = irq;
 
 	dma_dev = &mv_chan->dmadev;
 
@@ -1148,7 +1151,7 @@ mv_xor_channel_add(struct mv_xor_device *xordev,
 	/* clear errors before enabling interrupts */
 	mv_xor_device_clear_err_status(mv_chan);
 
-	ret = devm_request_irq(&pdev->dev, irq,
+	ret = devm_request_irq(&pdev->dev, mv_chan->irq,
 			       mv_xor_interrupt_handler,
 			       0, dev_name(&pdev->dev), mv_chan);
 	if (ret)
