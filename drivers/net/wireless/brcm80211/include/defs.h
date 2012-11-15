d9ac9950f6928763f3beef8e2966fa4bc4530e0f
Seth Forshee <seth.forshee@canonical.com>
2012-11-15 08:08:07 -0600

    brcmsmac: Add brcms_dbg_dma() debug macro
    Also convert relevant messages to use this macro.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Tested-by: Daniel Wagner <wagi@monom.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/include/defs.h
+++ drivers/net/wireless/brcm80211/include/defs.h
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BRCM_DL_RX		0x00000004
 #define BRCM_DL_TX		0x00000008
 #define BRCM_DL_INT		0x00000010
+#define BRCM_DL_DMA		0x00000020
 
 #define PM_OFF	0
 #define PM_MAX	1
