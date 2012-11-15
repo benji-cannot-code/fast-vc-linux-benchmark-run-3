f425bc009f315867226392f9f252e97a76937937
Seth Forshee <seth.forshee@canonical.com>
2012-11-15 08:07:55 -0600

    brcmsmac: Use IEEE 802.11 AC levels for pktq precedence levels
    The mac80211 tx queues and brcmsmac DMA fifos both map directly to AC
    levels. Therefore it's much more straightforward to queue tx frames and
    choose the tx fifo based on the mac80211 queue instead of mapping 802.1D
    priority tags to precedence levels then back to AC levels. mac80211
    already maps the 802.1D levels to the appropriate AC levels and queues
    management frames at the maximum priority, so the results should be
    identical.
    
    One functional change resulting from this patch is that AMPDU retries no
    longer get a priority boost to queue them ahead of packets with the same
    priority already in the tx queue. This behavior will be restored (in
    effect at least) in a later patch when the tx queue is removed.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Reviewed-by: Arend van Spriel <arend@broadcom.com>
    Tested-by: Daniel Wagner <wagi@monom.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmsmac/main.h
+++ drivers/net/wireless/brcm80211/brcmsmac/main.h
@@ -102,9 +102,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DATA_BLOCK_TX_SUPR	(1 << 4)
 
-/* 802.1D Priority to TX FIFO number for wme */
-extern const u8 prio2fifo[];
-
 /* Ucode MCTL_WAKE override bits */
 #define BRCMS_WAKE_OVERRIDE_CLKCTL	0x01
 #define BRCMS_WAKE_OVERRIDE_PHYREG	0x02
@@ -535,7 +532,6 @@ struct brcms_c_info {
 
 	u16 wme_retries[IEEE80211_NUM_ACS];
 	u16 tx_prec_map;
-	u16 fifo2prec_map[NFIFO];
 
 	struct brcms_bss_cfg *bsscfg;
 
@@ -642,7 +638,7 @@ extern void brcms_c_txfifo(struct brcms_c_info *wlc, uint fifo,
 			   struct sk_buff *p, bool commit);
 extern void brcms_c_txfifo_complete(struct brcms_c_info *wlc, uint fifo);
 extern void brcms_c_txq_enq(struct brcms_c_info *wlc, struct scb *scb,
-			    struct sk_buff *sdu, uint prec);
+			    struct sk_buff *sdu);
 extern void brcms_c_print_txstatus(struct tx_status *txs);
 extern int brcms_b_xmtfifo_sz_get(struct brcms_hardware *wlc_hw, uint fifo,
 		   uint *blocks);
