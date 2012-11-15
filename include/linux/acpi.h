ebb3765d328d0552c293e12feffcd3319271b8dd
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2012-11-15 13:15:37 +0100

    ACPI / PM: Fix build problem when CONFIG_ACPI or CONFIG_PM is not set
    Commit e5cc8ef (ACPI / PM: Provide ACPI PM callback routines for
    subsystems) introduced a build problem occuring if CONFIG_ACPI is
    unset or CONFIG_PM is unset and errno.h is not included before
    acpi.h, because in that case ENODEV used in acpi.h is undefined.
    
    Fix the issue by making acpi.h include errno.h.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_ACPI_H
 #define _LINUX_ACPI_H
 
+#include <linux/errno.h>
 #include <linux/ioport.h>	/* for struct resource */
 
 #ifdef	CONFIG_ACPI
