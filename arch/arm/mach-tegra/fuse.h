e0e1dcb5f246fbbbdf10cb0beb18239f68c30773
Danny Huang <dahuang@nvidia.com>
2012-11-15 15:42:32 +0800

    ARM: tegra: flexible spare fuse read function
    Change the spare fuse base from a definition to a variable.
    It provides flexibilty to read spare fuse on different chip.
    
    Signed-off-by: Danny Huang <dahuang@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/fuse.h
+++ arch/arm/mach-tegra/fuse.h
@@ -49,5 +49,7 @@ extern int tegra_bct_strapping;
 
 unsigned long long tegra_chip_uid(void);
 void tegra_init_fuse(void);
+bool tegra_spare_fuse(int bit);
+u32 tegra_fuse_readl(unsigned long offset);
 
 #endif
