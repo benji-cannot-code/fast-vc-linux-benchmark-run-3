fc09a7576b36fe50416f480ef76c31bd00ec416a
Vlad Yasevich <vyasevic@redhat.com>
2012-11-15 08:49:11 +0000

    net: Switch to using the new packet offload infrustructure
    Convert to using the new GSO/GRO registration mechanism and new
    packet offload structure.
    
    Signed-off-by: Vlad Yasevich <vyasevic@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -1663,6 +1663,10 @@ static int ipv4_proc_init(void);
 static struct packet_type ip_packet_type __read_mostly = {
 	.type = cpu_to_be16(ETH_P_IP),
 	.func = ip_rcv,
+};
+
+static struct packet_offload ip_packet_offload __read_mostly = {
+	.type = cpu_to_be16(ETH_P_IP),
 	.gso_send_check = inet_gso_send_check,
 	.gso_segment = inet_gso_segment,
 	.gro_receive = inet_gro_receive,
@@ -1782,6 +1786,7 @@ static int __init inet_init(void)
 
 	ipfrag_init();
 
+	dev_add_offload(&ip_packet_offload);
 	dev_add_pack(&ip_packet_type);
 
 	rc = 0;
