63dfcff9970050fc9c4c6106c1ca94c241ed335c
Christoph Hellwig <hch@lst.de>
2012-11-16 09:20:37 +1100

    xfs: add CRC infrastructure
     - add a mount feature bit for CRC enabled filesystems
     - add some helpers for generating and verifying the CRCs
     - add a copy_uuid helper
    
    The checksumming helpers are loosely based on similar ones in sctp,
    all other bits come from Dave Chinner.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Ben Myers <bpm@sgi.com>

--- fs/xfs/xfs_linux.h
+++ fs/xfs/xfs_linux.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/blkdev.h>
 #include <linux/slab.h>
+#include <linux/crc32c.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/file.h>
