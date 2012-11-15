6acf73874bc99ff913f7fd3053d784b97f3f1ecb
Lars-Peter Clausen <lars@metafoo.de>
2012-11-15 13:15:00 +0000

    staging:iio:ad7298: Rework regulator handling
    Rework the regulator handling of the driver to match more closely what we do in
    other drivers. Make the regulator non-optional if a external reference is used.
    Also dispose the option of specifying the reference voltage via platform data.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/adc/ad7298.h
+++ drivers/staging/iio/adc/ad7298.h
@@ -27,19 +27,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define RES_MASK(bits)	((1 << (bits)) - 1)
 
-/*
- * TODO: struct ad7298_platform_data needs to go into include/linux/iio
- */
-
+/**
+ * struct ad7298_platform_data - Platform data for the ad7298 ADC driver
+ * @ext_ref: Whether to use an external reference voltage.
+ **/
 struct ad7298_platform_data {
-	/* External Vref voltage applied */
-	u16				vref_mv;
+	bool ext_ref;
 };
 
 struct ad7298_state {
 	struct spi_device		*spi;
 	struct regulator		*reg;
-	u16				int_vref_mv;
 	unsigned			ext_ref;
 	struct spi_transfer		ring_xfer[10];
 	struct spi_transfer		scan_single_xfer[3];
