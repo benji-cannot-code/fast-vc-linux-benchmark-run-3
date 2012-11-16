0216d63dac914859cf2b25b392dfebcf561d36df
Johannes Berg <johannes.berg@intel.com>
2012-11-16 10:59:21 +0000

    net: remove unnecessary wireless includes
    The wireless and wext includes in net-sysfs.c aren't
    needed, so remove them.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -19,11 +19,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <net/net_namespace.h>
 #include <linux/rtnetlink.h>
-#include <linux/wireless.h>
 #include <linux/vmalloc.h>
 #include <linux/export.h>
 #include <linux/jiffies.h>
-#include <net/wext.h>
 
 #include "net-sysfs.h"
 
