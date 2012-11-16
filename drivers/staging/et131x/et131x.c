72fe9cfa4e4bf18594870219f24f732f26fae7a5
Mark Einon <mark.einon@gmail.com>
2012-11-16 10:47:38 +0000

    staging: et131x: Remove alignment offset padding on DMA buffer allocation/free
    This padding was used to align buffers to a 4k boundary when returned
    from dma_alloc_coherent(). As the buffers are already 4k aligned, and
    the alignment no longer performed, the padding is not needed.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/et131x/et131x.c
+++ drivers/staging/et131x/et131x.c
@@ -2292,8 +2292,7 @@ static int et131x_rx_dma_memory_alloc(struct et131x_adapter *adapter)
 
 	for (id = 0; id < NUM_FBRS; id++) {
 		/* Allocate an area of memory for Free Buffer Ring */
-		bufsize = (sizeof(struct fbr_desc) *
-				rx_ring->fbr[id]->num_entries) + 0xfff;
+		bufsize = (sizeof(struct fbr_desc) * rx_ring->fbr[id]->num_entries);
 		rx_ring->fbr[id]->ring_virtaddr =
 				dma_alloc_coherent(&adapter->pdev->dev,
 					bufsize,
@@ -2464,8 +2463,7 @@ static void et131x_rx_dma_memory_free(struct et131x_adapter *adapter)
 			}
 
 			bufsize =
-			    (sizeof(struct fbr_desc) * rx_ring->fbr[id]->num_entries) +
-										0xfff;
+			    sizeof(struct fbr_desc) * rx_ring->fbr[id]->num_entries;
 
 			dma_free_coherent(&adapter->pdev->dev, bufsize,
 					    rx_ring->fbr[id]->ring_virtaddr,
