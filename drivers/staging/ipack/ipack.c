b389627969bd582f99e91ef8db42c86c17fa6f41
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2012-11-15 17:50:14 -0800

    Staging: ipack: CONFIG_HOTPLUG is always enabled
    as CONFIG_HOTPLUG is always enabled now, just remove the #ifdef logic in
    the ipack core for it if was disabled.
    
    Cc:Samuel Iglesias Gonsalvez <siglesias@igalia.com>
    Cc: Jens Taprogge <jens.taprogge@taprogge.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ipack/ipack.c
+++ drivers/staging/ipack/ipack.c
@@ -86,8 +86,6 @@ static int ipack_bus_remove(struct device *device)
 	return 0;
 }
 
-#ifdef CONFIG_HOTPLUG
-
 static int ipack_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
 	struct ipack_device *idev;
@@ -104,12 +102,6 @@ static int ipack_uevent(struct device *dev, struct kobj_uevent_env *env)
 
 	return 0;
 }
-
-#else /* !CONFIG_HOTPLUG */
-
-#define ipack_uevent NULL
-
-#endif /* !CONFIG_HOTPLUG */
 
 #define ipack_device_attr(field, format_string)				\
 static ssize_t								\
