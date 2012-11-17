79cc7399ee5d3d21bc1db1bb54ab280968d3edfe
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2012-11-17 15:22:26 +0100

    clk: mvebu: add clock gating control provider for DT
    This driver allows to provide DT clocks for clock gates found on
    Marvell Dove and Kirkwood SoCs. The clock gates are referenced by
    the phandle index of the corresponding bit in the clock gating control
    register to ease lookup in the datasheet.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>

--- drivers/clk/mvebu/clk.c
+++ drivers/clk/mvebu/clk.c
@@ -18,9 +18,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include "clk-core.h"
 #include "clk-cpu.h"
+#include "clk-gating-ctrl.h"
 
 void __init mvebu_clocks_init(void)
 {
 	mvebu_core_clk_init();
+	mvebu_gating_clk_init();
 	mvebu_cpu_clk_init();
 }
