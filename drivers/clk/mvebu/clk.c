12217deb8525e7353279b72adae96b72f168f4b5
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2012-11-17 15:22:22 +0100

    clk: mvebu: add mvebu core clocks.
    This driver allows to provide DT clocks for core clocks found on
    Marvell Kirkwood, Dove & 370/XP SoCs. The core clock frequencies and
    ratios are determined by decoding the Sample-At-Reset registers.
    
    Although technically correct, using a divider of 0 will lead to
    div_by_zero panic. Let's use a ratio of 0/1 instead to fail later
    with a zero clock.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Tested-by Gregory CLEMENT <gregory.clement@free-electrons.com>

---
+++ drivers/clk/mvebu/clk.c
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Marvell EBU SoC clock handling.
+ *
+ * Copyright (C) 2012 Marvell
+ *
+ * Gregory CLEMENT <gregory.clement@free-electrons.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+#include <linux/kernel.h>
+#include <linux/clk.h>
+#include <linux/clk-provider.h>
+#include <linux/of_address.h>
+#include <linux/clk/mvebu.h>
+#include <linux/of.h>
+#include "clk-core.h"
+
+void __init mvebu_clocks_init(void)
+{
+	mvebu_core_clk_init();
+}
