e98089170b0991bea3142c451fa77cd4f143358e
Yinghai Lu <yinghai@kernel.org>
2012-11-16 19:39:19 -0800

    x86, mm: kill numa_free_all_bootmem()
    Now NO_BOOTMEM version free_all_bootmem_node() does not really
    do free_bootmem at all, and it only call register_page_bootmem_info_node
    instead.
    
    That is confusing, try to kill that free_all_bootmem_node().
    
    Before that, this patch will remove numa_free_all_bootmem().
    
    That function could be replaced with register_page_bootmem_info() and
    free_all_bootmem();
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Link: http://lkml.kernel.org/r/1353123563-3103-43-git-send-email-yinghai@kernel.org
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/numa_64.h
+++ arch/x86/include/asm/numa_64.h
@@ -2,6 +2,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_NUMA_64_H
 #define _ASM_X86_NUMA_64_H
 
-extern unsigned long numa_free_all_bootmem(void);
-
 #endif /* _ASM_X86_NUMA_64_H */
