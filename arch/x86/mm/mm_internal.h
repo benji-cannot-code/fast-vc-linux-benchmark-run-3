708fa31e09e68e1ab32d16e2998d9373f1385f20
Yinghai Lu <yinghai@kernel.org>
2012-11-16 19:39:04 -0800

    x86, mm: Add alloc_low_pages(num)
    32bit kmap mapping needs pages to be used for low to high.
    At this point those pages are still from pgt_buf_* from BRK, so it is
    ok now.
    But we want to move early_ioremap_page_table_range_init() out of
    init_memory_mapping() and only call it one time later, that will
    make page_table_range_init/page_table_kmap_check/alloc_low_page to
    use memblock to get page.
    
    memblock allocation for pages are from high to low.
    So will get panic from page_table_kmap_check() that has BUG_ON to do
    ordering checking.
    
    This patch add alloc_low_pages to make it possible to allocate serveral
    pages at first, and hand out pages one by one from low to high.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Link: http://lkml.kernel.org/r/1353123563-3103-28-git-send-email-yinghai@kernel.org
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/mm/mm_internal.h
+++ arch/x86/mm/mm_internal.h
@@ -2,6 +2,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __X86_MM_INTERNAL_H
 #define __X86_MM_INTERNAL_H
 
-void *alloc_low_page(void);
+void *alloc_low_pages(unsigned int num);
+static inline void *alloc_low_page(void)
+{
+	return alloc_low_pages(1);
+}
 
 #endif	/* __X86_MM_INTERNAL_H */
