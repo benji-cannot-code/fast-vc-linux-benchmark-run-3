a67ec35fd9af6807b21bf043758e56a80e53c2d8
Andrew Lunn <andrew@lunn.ch>
2012-11-17 15:46:13 +0100

    ARM: Kirkwood: Convert GoFlexNet to use regulators
    Control the power to USB using a fixed regulator.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Tested-by: Josh Coombs <josh.coombs@gmail.com>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-goflexnet.c
+++ arch/arm/mach-kirkwood/board-goflexnet.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_fdt.h>
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
-#include <linux/gpio.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -62,10 +61,6 @@ void __init goflexnet_init(void)
 	 * Basic setup. Needs to be called early.
 	 */
 	kirkwood_mpp_conf(goflexnet_mpp_config);
-
-	if (gpio_request(29, "USB Power Enable") != 0 ||
-	    gpio_direction_output(29, 1) != 0)
-		pr_err("can't setup GPIO 29 (USB Power Enable)\n");
 
 	kirkwood_ge00_init(&goflexnet_ge00_data);
 }
