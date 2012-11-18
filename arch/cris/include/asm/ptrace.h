25ffe8bc5803420a230d4218adb6f1a3bf70db62
Al Viro <viro@zeniv.linux.org.uk>
2012-11-18 12:50:10 -0500

    new helper: current_user_stack_pointer()
    	Cross-architecture equivalent of rdusp(); default is
    user_stack_pointer(current_pt_regs()) - that works for almost all
    platforms that have usp saved in pt_regs.  The only exception from
    that is ia64 - we want memory stack, not the backing store for
    register one.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/cris/include/asm/ptrace.h
+++ arch/cris/include/asm/ptrace.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PTRACE_SETREGS            13
 
 #define profile_pc(regs) instruction_pointer(regs)
+#define current_user_stack_pointer() rdusp()
 
 #endif /* __KERNEL__ */
 
