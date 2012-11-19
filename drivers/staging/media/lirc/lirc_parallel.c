ade292b960ee505aa47bcf39d44ce58a38a96277
Bill Pemberton <wfp5p@virginia.edu>
2012-11-19 13:27:10 -0500

    staging: lirc: remove use of __devexit
    CONFIG_HOTPLUG is going away as an option so __devexit is no
    longer needed.
    
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Cc: Mauro Carvalho Chehab <mchehab@infradead.org>
    Cc: linux-media@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/lirc/lirc_parallel.c
+++ drivers/staging/media/lirc/lirc_parallel.c
@@ -589,7 +589,7 @@ static int lirc_parallel_probe(struct platform_device *dev)
 	return 0;
 }
 
-static int __devexit lirc_parallel_remove(struct platform_device *dev)
+static int lirc_parallel_remove(struct platform_device *dev)
 {
 	return 0;
 }
