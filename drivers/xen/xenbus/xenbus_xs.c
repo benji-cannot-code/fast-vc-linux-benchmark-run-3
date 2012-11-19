9580a11efe5826375437a3d07350f2c2cd69bdaf
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-19 16:52:30 +0530

    xen/xenbus: Remove duplicate inclusion of asm/xen/hypervisor.h
    asm/xen/hypervisor.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/xenbus/xenbus_xs.c
+++ drivers/xen/xenbus/xenbus_xs.c
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <xen/xenbus.h>
 #include <xen/xen.h>
 #include "xenbus_comms.h"
-#include <asm/xen/hypervisor.h>
 
 struct xs_stored_msg {
 	struct list_head list;
