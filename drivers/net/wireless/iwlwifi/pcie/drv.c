30f52df1b81ccaa52c3b9a6adae1efb6880df564
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-19 17:15:40 +0530

    iwlwifi: Remove duplicate inclusion of iwl-trans.h in pcie/drv.c
    iwl-trans.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/iwlwifi/pcie/drv.c
@@ -70,7 +70,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "iwl-trans.h"
 #include "iwl-drv.h"
-#include "iwl-trans.h"
 
 #include "cfg.h"
 #include "internal.h"
