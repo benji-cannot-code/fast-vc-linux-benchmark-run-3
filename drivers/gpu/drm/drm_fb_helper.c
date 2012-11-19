b923def2b59fa2c03aa2f13186d5c3d45e9123a2
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-19 09:44:58 +0000

    drm/fb_helper: Fix potential NULL pointer dereference
    kcalloc returns NULL on failure. Hence check for the return value
    and exit on error to avoid NULL pointer dereference.
    
    Fixes the following smatch errors:
    drivers/gpu/drm/drm_fb_helper.c:1271 drm_setup_crtcs() error:
    potential null dereference 'modes'.  (kcalloc returns null)
    drivers/gpu/drm/drm_fb_helper.c:1272 drm_setup_crtcs() error:
    potential null dereference 'crtcs'.  (kcalloc returns null)
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_fb_helper.c
+++ drivers/gpu/drm/drm_fb_helper.c
@@ -1252,6 +1252,11 @@ static void drm_setup_crtcs(struct drm_fb_helper *fb_helper)
 			sizeof(struct drm_display_mode *), GFP_KERNEL);
 	enabled = kcalloc(dev->mode_config.num_connector,
 			  sizeof(bool), GFP_KERNEL);
+	if (!crtcs || !modes || !enabled) {
+		DRM_ERROR("Memory allocation failed\n");
+		goto out;
+	}
+
 
 	drm_enable_connectors(fb_helper, enabled);
 
@@ -1290,6 +1295,7 @@ static void drm_setup_crtcs(struct drm_fb_helper *fb_helper)
 		}
 	}
 
+out:
 	kfree(crtcs);
 	kfree(modes);
 	kfree(enabled);
