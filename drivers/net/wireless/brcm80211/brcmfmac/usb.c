59214939d52957112bca54d766464ac792205dc8
John W. Linville <linville@tuxdriver.com>
2012-11-19 11:04:28 -0500

    brcmfmac: include linux/vmalloc.h from usb.c
    This avoids build failures on some architectures...
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Acked-by: Arend van Spriel <arend@broadcom.com>

--- drivers/net/wireless/brcm80211/brcmfmac/usb.c
+++ drivers/net/wireless/brcm80211/brcmfmac/usb.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/firmware.h>
 #include <linux/usb.h>
+#include <linux/vmalloc.h>
 
 #include <brcmu_utils.h>
 #include <brcmu_wifi.h>
