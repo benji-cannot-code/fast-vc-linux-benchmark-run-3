a67e02d3810d38bd0f4c368254e37380df59c056
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-19 17:03:15 +0530

    usb: musb: dsps: Remove duplicate inclusion of linux/of.h
    linux/of.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_dsps.c
+++ drivers/usb/musb/musb_dsps.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/init.h>
 #include <linux/io.h>
-#include <linux/of.h>
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/dma-mapping.h>
