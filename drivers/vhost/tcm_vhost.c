2f9868d207cfba8702cb532c6bc2465e5d65c7ec
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-19 01:28:28 +0000

    vhost: Remove duplicate inclusion of linux/vhost.h
    linux/vhost.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/vhost/tcm_vhost.c
+++ drivers/vhost/tcm_vhost.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/compat.h>
 #include <linux/eventfd.h>
-#include <linux/vhost.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
 #include <asm/unaligned.h>
