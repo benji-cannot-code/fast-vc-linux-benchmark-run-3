4f77b8d6086dd3988a6d3748f2142716adf7f06b
Bill Pemberton <wfp5p@virginia.edu>
2012-11-19 13:21:57 -0500

    staging: iio: remove use of __devinit
    CONFIG_HOTPLUG is going away as an option so __devinit is no longer
    needed.
    
    Signed-off-by: Bill Pemberton <wfp5p@virginia.edu>
    Cc: Jonathan Cameron <jic23@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/iio/addac/adt7316.c
+++ drivers/staging/iio/addac/adt7316.c
@@ -2126,7 +2126,7 @@ static const struct iio_info adt7516_info = {
 /*
  * device probe and remove
  */
-int __devinit adt7316_probe(struct device *dev, struct adt7316_bus *bus,
+int adt7316_probe(struct device *dev, struct adt7316_bus *bus,
 		const char *name)
 {
 	struct adt7316_chip_info *chip;
