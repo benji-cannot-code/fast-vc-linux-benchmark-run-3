ec7b8ed817867dae96a49505253b60901b2561bd
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-19 17:07:52 +0530

    spi: omap2-mcspi: remove duplicate inclusion of linux/err.h
    linux/err.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/spi/spi-omap2-mcspi.c
+++ drivers/spi/spi-omap2-mcspi.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/pinctrl/consumer.h>
-#include <linux/err.h>
 
 #include <linux/spi/spi.h>
 
