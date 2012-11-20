f2c104d7b5400644ba87d37333abc05617f32de8
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2012-11-20 13:46:19 +0900

    mfd: arizona: Use correct array for ARRAY_SIZE in mfd_add_devices call
    wm5102_devs array was used for ARRAY_SIZE whilst adding the wm5110
    devices. This change corrects this to get the size from the wm5110_devs
    array. As both arrays are the same size no issues should have been
    caused by this bug.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/arizona-core.c
+++ drivers/mfd/arizona-core.c
@@ -521,7 +521,7 @@ int __devinit arizona_dev_init(struct arizona *arizona)
 		break;
 	case WM5110:
 		ret = mfd_add_devices(arizona->dev, -1, wm5110_devs,
-				      ARRAY_SIZE(wm5102_devs), NULL, 0, NULL);
+				      ARRAY_SIZE(wm5110_devs), NULL, 0, NULL);
 		break;
 	}
 
