acf541819f8899187cb2acaddcf0a1da1fc71d2d
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-20 14:07:16 +0530

    staging: drm/imx: Remove duplicate inclusion of linux/videodev2.h
    linux/videodev2.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/mach-omap2/board-overo.c
+++ arch/arm/mach-omap2/board-overo.c
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/flash.h>
 #include <asm/mach/map.h>
 
-#include "common.h"
 #include <video/omapdss.h>
 #include <video/omap-panel-generic-dpi.h>
 #include <video/omap-panel-tfp410.h>
--- drivers/staging/imx-drm/ipu-v3/imx-ipu-v3.h
+++ drivers/staging/imx-drm/ipu-v3/imx-ipu-v3.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev2.h>
 #include <linux/bitmap.h>
 #include <linux/fb.h>
-#include <linux/videodev2.h>
 
 struct ipu_soc;
 
