421c4c6b4b597e47f9d7756e68a58eaf6652edcb
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-20 11:19:15 +0530

    mfd: twl6040: Remove duplicate inclusion of linux/err.h
    linux/err.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/twl6040.c
+++ drivers/mfd/twl6040.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/regmap.h>
-#include <linux/err.h>
 #include <linux/mfd/core.h>
 #include <linux/mfd/twl6040.h>
 #include <linux/regulator/consumer.h>
