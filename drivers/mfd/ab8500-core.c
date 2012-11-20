1cc7c2184e1e7c0455555eb8464dea88251e0ef2
Ulf Hansson <ulf.hansson@stericsson.com>
2012-11-20 14:57:25 +0100

    mfd: ab8500-core: Add abx500-clk as an mfd child device
    Hierarchically, the abx500-clk shall be considered as a child of the
    ab8500 core. The abx500-clk is intiated at arch init and thus the clks
    will be available when clients needs them.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@stericsson.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Reviewed-by: Philippe LANGLAIS <philippe.langlais@stericsson.com>
    Reviewed-by: Patrice CHOTARD <patrice.chotard@stericsson.com>
    Reviewed-by: Gabriel FERNANDEZ <gabriel.fernandez@stericsson.com>
    Reviewed-by: Philippe BEGNIC <philippe.begnic@stericsson.com>
    Acked-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/ab8500-core.c
+++ drivers/mfd/ab8500-core.c
@@ -1190,6 +1190,10 @@ static struct mfd_cell ab9540_devs[] = {
 		.name = "ab8500-regulator",
 	},
 	{
+		.name = "abx500-clk",
+		.of_compatible = "stericsson,abx500-clk",
+	},
+	{
 		.name = "ab8500-gpadc",
 		.of_compatible = "stericsson,ab8500-gpadc",
 		.num_resources = ARRAY_SIZE(ab8500_gpadc_resources),
@@ -1257,6 +1261,10 @@ static struct mfd_cell ab8505_devs[] = {
 		.name = "ab8500-regulator",
 	},
 	{
+		.name = "abx500-clk",
+		.of_compatible = "stericsson,abx500-clk",
+	},
+	{
 		.name = "ab8500-gpadc",
 		.num_resources = ARRAY_SIZE(ab8505_gpadc_resources),
 		.resources = ab8505_gpadc_resources,
@@ -1314,6 +1322,10 @@ static struct mfd_cell ab8540_devs[] = {
 	},
 	{
 		.name = "ab8500-regulator",
+	},
+	{
+		.name = "abx500-clk",
+		.of_compatible = "stericsson,abx500-clk",
 	},
 	{
 		.name = "ab8500-gpadc",
