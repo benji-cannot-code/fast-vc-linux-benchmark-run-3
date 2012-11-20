3a9855b1072be89f36701ae73339b9f434d07afc
Al Viro <viro@zeniv.linux.org.uk>
2012-11-19 22:00:52 -0500

    x86: get rid of pt_regs argument of iopl(2)
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/x86/include/asm/syscalls.h
+++ arch/x86/include/asm/syscalls.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Common in X86_32 and X86_64 */
 /* kernel/ioport.c */
 asmlinkage long sys_ioperm(unsigned long, unsigned long, int);
-long sys_iopl(unsigned int, struct pt_regs *);
+asmlinkage long sys_iopl(unsigned int);
 
 /* kernel/ldt.c */
 asmlinkage int sys_modify_ldt(int, void __user *, unsigned long);
