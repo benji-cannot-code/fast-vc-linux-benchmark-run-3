8d1d6ac35a4336a072fd5de7651868313eb5d969
Johan Meiring <johanmeiring@gmail.com>
2012-11-20 19:28:44 +0200

    staging: cxt1e1: musycc.h: fixes coding style issues
    This commit fixes instances of spaces being used for indentation
    instead of tabs.
    
    Signed-off-by: Johan Meiring <johanmeiring@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/cxt1e1/musycc.h
+++ drivers/staging/cxt1e1/musycc.h
@@ -49,8 +49,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define INT_QUEUE_SIZE    MUSYCC_NIQD
 
 /* RAM image of MUSYCC registers laid out as a C structure */
-    struct musycc_groupr
-    {
+struct musycc_groupr
+{
 	VINT32      thp[32];    /* Transmit Head Pointer [5-29]           */
 	VINT32      tmp[32];    /* Transmit Message Pointer [5-30]        */
 	VINT32      rhp[32];    /* Receive Head Pointer [5-29]            */
@@ -68,11 +68,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	VINT32      mpd;        /* Memory Protection Descriptor [5-18]    */
 	VINT32      mld;        /* Message Length Descriptor [5-20]       */
 	VINT32      pcd;        /* Port Configuration Descriptor [5-19]   */
-    };
+};
 
 /* hardware MUSYCC registers laid out as a C structure */
-    struct musycc_globalr
-    {
+struct musycc_globalr
+{
 	VINT32      gbp;        /* Group Base Pointer                     */
 	VINT32      dacbp;      /* Dual Address Cycle Base Pointer        */
 	VINT32      srd;        /* Service Request Descriptor             */
@@ -101,7 +101,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	VINT32      pcd;        /* Port Configuration Descriptor [5-19]   */
 	VINT32      rbist;      /* Receive BIST status [5-4]              */
 	VINT32      tbist;      /* Receive BIST status [5-4]              */
-    };
+};
 
 /* Global Config Descriptor bit macros */
 #define MUSYCC_GCD_ECLK_ENABLE  0x00000800      /* EBUS clock enable */
