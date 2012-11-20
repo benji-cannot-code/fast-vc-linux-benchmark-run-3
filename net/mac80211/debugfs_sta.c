51c3be7009bcbcedf93edcad85da7067b2d283cc
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2012-11-20 08:46:02 +0530

    mac80211: Add debugfs callbacks for station addition/removal
    Provide drivers with hooks to create debugfs files when
    a new station is added. This would help drivers to take
    advantage of mac80211's station list infrastructure and not maintain
    tedious station management code internally.
    
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    [ifdef inline wrapper functions]
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/debugfs_sta.c
+++ net/mac80211/debugfs_sta.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "debugfs.h"
 #include "debugfs_sta.h"
 #include "sta_info.h"
+#include "driver-ops.h"
 
 /* sta attributtes */
 
@@ -335,6 +336,8 @@ STA_OPS(ht_capa);
 
 void ieee80211_sta_debugfs_add(struct sta_info *sta)
 {
+	struct ieee80211_local *local = sta->local;
+	struct ieee80211_sub_if_data *sdata = sta->sdata;
 	struct dentry *stations_dir = sta->sdata->debugfs.subdir_stations;
 	u8 mac[3*ETH_ALEN];
 
@@ -380,10 +383,16 @@ void ieee80211_sta_debugfs_add(struct sta_info *sta)
 	DEBUGFS_ADD_COUNTER(tx_retry_failed, tx_retry_failed);
 	DEBUGFS_ADD_COUNTER(tx_retry_count, tx_retry_count);
 	DEBUGFS_ADD_COUNTER(wep_weak_iv_count, wep_weak_iv_count);
+
+	drv_sta_add_debugfs(local, sdata, &sta->sta, sta->debugfs.dir);
 }
 
 void ieee80211_sta_debugfs_remove(struct sta_info *sta)
 {
+	struct ieee80211_local *local = sta->local;
+	struct ieee80211_sub_if_data *sdata = sta->sdata;
+
+	drv_sta_remove_debugfs(local, sdata, &sta->sta, sta->debugfs.dir);
 	debugfs_remove_recursive(sta->debugfs.dir);
 	sta->debugfs.dir = NULL;
 }
