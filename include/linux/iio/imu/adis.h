7ccb1df77f4eaa8ccdc03d5c39b0f81600cf7add
Lars-Peter Clausen <lars@metafoo.de>
2012-11-20 13:36:00 +0000

    iio:imu: Add support for the ADIS16480 and similar IMUs
    This patch adds support for the ADIS16375, ADIS16480, ADIS16485, ADIS16488 6
    degree to 10 degree of freedom IMUs.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- include/linux/iio/imu/adis.h
+++ include/linux/iio/imu/adis.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ADIS_PAGE_SIZE 0x80
 #define ADIS_REG_PAGE_ID 0x00
 
+struct adis;
+
 /**
  * struct adis_data - ADIS chip variant specific data
  * @read_delay: SPI delay for read operations in us
@@ -44,6 +46,8 @@ struct adis_data {
 
 	const char * const *status_error_msgs;
 	unsigned int status_error_mask;
+
+	int (*enable_irq)(struct adis *adis, bool enable);
 
 	bool has_paging;
 };
