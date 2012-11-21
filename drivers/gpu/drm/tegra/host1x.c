37182331bf7c6d81dd3d59ece2e455cdcd2dbf1d
Thierry Reding <thierry.reding@avionic-design.de>
2012-11-21 09:50:41 +0100

    drm: tegra: Add Tegra30 support
    Add support for host1x, the display controllers and HDMI on the Tegra30
    SoC.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>
    Tested-by: Stephen Warren <swarren@nvidia.com>
    Tested-by: Mark Zhang <markz@nvidia.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/tegra/host1x.c
+++ drivers/gpu/drm/tegra/host1x.c
@@ -69,6 +69,8 @@ static int host1x_parse_dt(struct host1x *host1x)
 	static const char * const compat[] = {
 		"nvidia,tegra20-dc",
 		"nvidia,tegra20-hdmi",
+		"nvidia,tegra30-dc",
+		"nvidia,tegra30-hdmi",
 	};
 	unsigned int i;
 	int err;
@@ -269,6 +271,7 @@ int host1x_unregister_client(struct host1x *host1x,
 }
 
 static struct of_device_id tegra_host1x_of_match[] = {
+	{ .compatible = "nvidia,tegra30-host1x", },
 	{ .compatible = "nvidia,tegra20-host1x", },
 	{ },
 };
