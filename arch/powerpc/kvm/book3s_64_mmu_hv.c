93cca43ea0991973b3e5cafd7f3fbf7fa977afd6
Paul Mackerras <paulus@samba.org>
2012-11-21 23:29:12 +0000

    KVM: PPC: Book3S HV: Report correct HPT entry index when reading HPT
    This fixes a bug in the code which allows userspace to read out the
    contents of the guest's hashed page table (HPT).  On the second and
    subsequent passes through the HPT, when we are reporting only those
    entries that have changed, we were incorrectly initializing the index
    field of the header with the index of the first entry we skipped
    rather than the first changed entry.  This fixes it.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/kvm/book3s_64_mmu_hv.c
+++ arch/powerpc/kvm/book3s_64_mmu_hv.c
@@ -1283,7 +1283,6 @@ static ssize_t kvm_htab_read(struct file *file, char __user *buf,
 	while (nb + sizeof(hdr) + HPTE_SIZE < count) {
 		/* Initialize header */
 		hptr = (struct kvm_get_htab_header __user *)buf;
-		hdr.index = i;
 		hdr.n_valid = 0;
 		hdr.n_invalid = 0;
 		nw = nb;
@@ -1299,6 +1298,7 @@ static ssize_t kvm_htab_read(struct file *file, char __user *buf,
 				++revp;
 			}
 		}
+		hdr.index = i;
 
 		/* Grab a series of valid entries */
 		while (i < kvm->arch.hpt_npte &&
