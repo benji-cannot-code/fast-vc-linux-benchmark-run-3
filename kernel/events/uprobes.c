f64a97e9a88c7baecfda6520f0f09aa6db6d484f
Oleg Nesterov <oleg@redhat.com>
2012-11-21 18:01:43 +0100

    uprobes: Kill the pointless inode/uc checks in register/unregister
    register/unregister verifies that inode/uc != NULL. For what?
    This really looks like "hide the potential problem", the caller
    should pass the valid data.
    
    register() also checks uc->next == NULL, probably to prevent the
    double-register but the caller can do other stupid/wrong things.
    If we do this check, then we should document that uc->next should
    be cleared before register() and add BUG_ON().
    
    Also add the small comment about the i_size_read() check.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -845,9 +845,7 @@ int uprobe_register(struct inode *inode, loff_t offset, struct uprobe_consumer *
 	struct uprobe *uprobe;
 	int ret;
 
-	if (!inode || !uc || uc->next)
-		return -EINVAL;
-
+	/* Racy, just to catch the obvious mistakes */
 	if (offset > i_size_read(inode))
 		return -EINVAL;
 
@@ -883,9 +881,6 @@ int uprobe_register(struct inode *inode, loff_t offset, struct uprobe_consumer *
 void uprobe_unregister(struct inode *inode, loff_t offset, struct uprobe_consumer *uc)
 {
 	struct uprobe *uprobe;
-
-	if (!inode || !uc)
-		return;
 
 	uprobe = find_uprobe(inode, offset);
 	if (!uprobe)
