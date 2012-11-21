46a9248a87d3b2470377ebf5cb76a02ea7c13dcf
Arnaldo Carvalho de Melo <acme@redhat.com>
2012-11-21 18:58:50 -0300

    perf top: Add missing newline on pr_err call
    The perf_event__process_sample function, when not finding a machine
    associated with a sample, was calling pr_err without a newline,
    garbling the screen on TUI mode due to a problem introduced by a
    recent ui_helpline patch.
    
    On --stdio it would just concatenate the messages for each sample with
    no machine associated, fix it by adding the newline.
    
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Namhyung Kim <namhyung@gmail.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-vuz88welqvp15c2uybd9osnz@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-top.c
+++ tools/perf/builtin-top.c
@@ -728,7 +728,7 @@ static void perf_event__process_sample(struct perf_tool *tool,
 	}
 
 	if (!machine) {
-		pr_err("%u unprocessable samples recorded.",
+		pr_err("%u unprocessable samples recorded.\n",
 		       top->session->hists.stats.nr_unprocessable_samples++);
 		return;
 	}
