d54d9a56b0fa77dd3fa0d05a3be87e9d33d81c77
Ben Skeggs <bskeggs@redhat.com>
2012-11-22 15:42:23 +1000

    drm/nve0/bsp: implement initial support for engine
    Will allow use of the engine if firmware (nvXX_fuc084) provided.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/device/nve0.c
+++ drivers/gpu/drm/nouveau/core/subdev/device/nve0.c
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <engine/graph.h>
 #include <engine/disp.h>
 #include <engine/copy.h>
+#include <engine/bsp.h>
 
 int
 nve0_identify(struct nouveau_device *device)
@@ -75,6 +76,7 @@ nve0_identify(struct nouveau_device *device)
 		device->oclass[NVDEV_ENGINE_DISP   ] = &nve0_disp_oclass;
 		device->oclass[NVDEV_ENGINE_COPY0  ] = &nve0_copy0_oclass;
 		device->oclass[NVDEV_ENGINE_COPY1  ] = &nve0_copy1_oclass;
+		device->oclass[NVDEV_ENGINE_BSP    ] = &nve0_bsp_oclass;
 		break;
 	case 0xe7:
 		device->cname = "GK107";
@@ -100,6 +102,7 @@ nve0_identify(struct nouveau_device *device)
 		device->oclass[NVDEV_ENGINE_DISP   ] = &nve0_disp_oclass;
 		device->oclass[NVDEV_ENGINE_COPY0  ] = &nve0_copy0_oclass;
 		device->oclass[NVDEV_ENGINE_COPY1  ] = &nve0_copy1_oclass;
+		device->oclass[NVDEV_ENGINE_BSP    ] = &nve0_bsp_oclass;
 		break;
 	default:
 		nv_fatal(device, "unknown Kepler chipset\n");
