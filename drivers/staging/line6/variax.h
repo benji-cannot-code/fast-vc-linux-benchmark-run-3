fcf868a95c9969ab3bcf083231a96b2dd1307eae
Stefan Hajnoczi <stefanha@gmail.com>
2012-11-22 20:49:10 +0100

    staging: line6: drop variax tone sysfs attr
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/line6/variax.h
+++ drivers/staging/line6/variax.h
@@ -94,11 +94,6 @@ struct usb_line6_variax {
 	unsigned char bank[18];
 
 	/**
-		Position of tone control dial.
-	*/
-	int tone;
-
-	/**
 		Handler for device initializaton.
 	*/
 	struct work_struct startup_work;
