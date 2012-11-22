20b0f9043eeb14e1c9894d1913e2a66becb3bf2e
Takashi Iwai <tiwai@suse.de>
2012-11-22 16:23:22 +0100

    ALSA: ymfpci: Use kmalloc for register buffer for PM
    Th buffer to save registers for PM is enough small for kmalloc(), not
    necessary to use vmalloc().
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ymfpci/ymfpci_main.c
+++ sound/pci/ymfpci/ymfpci_main.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
-#include <linux/vmalloc.h>
 #include <linux/mutex.h>
 #include <linux/module.h>
 
@@ -2262,7 +2261,7 @@ static int snd_ymfpci_free(struct snd_ymfpci *chip)
 #endif
 
 #ifdef CONFIG_PM_SLEEP
-	vfree(chip->saved_regs);
+	kfree(chip->saved_regs);
 #endif
 	if (chip->irq >= 0)
 		free_irq(chip->irq, chip);
@@ -2472,7 +2471,8 @@ int __devinit snd_ymfpci_create(struct snd_card *card,
 	}
 
 #ifdef CONFIG_PM_SLEEP
-	chip->saved_regs = vmalloc(YDSXGR_NUM_SAVED_REGS * sizeof(u32));
+	chip->saved_regs = kmalloc(YDSXGR_NUM_SAVED_REGS * sizeof(u32),
+				   GFP_KERNEL);
 	if (chip->saved_regs == NULL) {
 		snd_ymfpci_free(chip);
 		return -ENOMEM;
