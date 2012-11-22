d7544892c87977ee7716b885577dce9a96b68124
Sylwester Nawrocki <s.nawrocki@samsung.com>
2012-11-22 06:17:19 -0300

    [media] s5p-fimc: Prevent race conditions during subdevs registration
    Make sure when fimc and fimc-lite capture video node is registered
    it has valid pipeline_ops assigned to it. Otherwise when a video
    node is opened right after is was registered there, might be an
    attempt to use ops that are just being assigned, after function
    v4l2_device_register_subdev() returns.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/s5p-fimc/fimc-mdevice.c
+++ drivers/media/platform/s5p-fimc/fimc-mdevice.c
@@ -353,6 +353,7 @@ static int fimc_register_callback(struct device *dev, void *p)
 
 	sd = &fimc->vid_cap.subdev;
 	sd->grp_id = FIMC_GROUP_ID;
+	v4l2_set_subdev_hostdata(sd, (void *)&fimc_pipeline_ops);
 
 	ret = v4l2_device_register_subdev(&fmd->v4l2_dev, sd);
 	if (ret) {
@@ -361,7 +362,6 @@ static int fimc_register_callback(struct device *dev, void *p)
 		return ret;
 	}
 
-	fimc->pipeline_ops = &fimc_pipeline_ops;
 	fmd->fimc[fimc->id] = fimc;
 	return 0;
 }
@@ -376,6 +376,7 @@ static int fimc_lite_register_callback(struct device *dev, void *p)
 		return 0;
 
 	fimc->subdev.grp_id = FLITE_GROUP_ID;
+	v4l2_set_subdev_hostdata(&fimc->subdev, (void *)&fimc_pipeline_ops);
 
 	ret = v4l2_device_register_subdev(&fmd->v4l2_dev, &fimc->subdev);
 	if (ret) {
@@ -385,7 +386,6 @@ static int fimc_lite_register_callback(struct device *dev, void *p)
 		return ret;
 	}
 
-	fimc->pipeline_ops = &fimc_pipeline_ops;
 	fmd->fimc_lite[fimc->index] = fimc;
 	return 0;
 }
