1f99a20b6a7b9e089146f74524e44b9cfedd59ed
Ben Skeggs <bskeggs@redhat.com>
2012-11-22 13:02:06 +1000

    drm/nvc0/copy: share interrupt handler with nva3
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/include/engine/copy.h
+++ drivers/gpu/drm/nouveau/core/include/engine/copy.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NOUVEAU_COPY_H__
 #define __NOUVEAU_COPY_H__
 
+void nva3_copy_intr(struct nouveau_subdev *);
+
 extern struct nouveau_oclass nva3_copy_oclass;
 extern struct nouveau_oclass nvc0_copy0_oclass;
 extern struct nouveau_oclass nvc0_copy1_oclass;
