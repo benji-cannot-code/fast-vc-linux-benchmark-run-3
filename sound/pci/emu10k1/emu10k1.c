d8424fb1c44c428ff133b809462bf4fe1914e00e
Takashi Iwai <tiwai@suse.de>
2012-11-22 17:18:49 +0100

    ALSA: emu10k1: don't update firmware during suspend/resume
    Add a flag to suppress the update in emu1010_firmware_thread() during
    suspend/resume.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/emu10k1/emu10k1.c
+++ sound/pci/emu10k1/emu10k1.c
@@ -216,6 +216,8 @@ static int snd_emu10k1_suspend(struct device *dev)
 
 	snd_power_change_state(card, SNDRV_CTL_POWER_D3hot);
 
+	emu->suspend = 1;
+
 	snd_pcm_suspend_all(emu->pcm);
 	snd_pcm_suspend_all(emu->pcm_mic);
 	snd_pcm_suspend_all(emu->pcm_efx);
@@ -260,6 +262,8 @@ static int snd_emu10k1_resume(struct device *dev)
 
 	if (emu->card_capabilities->ca0151_chip)
 		snd_p16v_resume(emu);
+
+	emu->suspend = 0;
 
 	snd_power_change_state(card, SNDRV_CTL_POWER_D0);
 	return 0;
