b6f15271f53271f82267743b5bccfaa4d646178d
Scott Ling <sl@opensource.wolfsonmicro.com>
2012-11-23 12:37:35 +0000

    ASoC: wm0010: Add checking for .dfw info record version.
    The info record at the start of the dsp firmware file has been
    expanded to incorporate additional version information. We need
    to check the version to make sure we understand the layout of
    the information in the record. The srec2image tool is currently
    used to create this record during creation of the .dfw file.
    
    Signed-off-by: Scott Ling <sl@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm0010.c
+++ sound/soc/codecs/wm0010.c
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DEVICE_ID_WM0010	10
 
+/* We only support v1 of the .dfw INFO record */
+#define INFO_VERSION		1
+
 enum dfw_cmd {
 	DFW_CMD_FUSE = 0x01,
 	DFW_CMD_CODE_HDR,
@@ -46,6 +49,13 @@ struct dfw_binrec {
 	u32 address;
 	uint8_t data[0];
 } __packed;
+
+struct dfw_inforec {
+	u8 info_version;
+	u8 tool_major_version;
+	u8 tool_minor_version;
+	u8 dsp_target;
+};
 
 struct dfw_pllrec {
 	u8 command;
@@ -343,6 +353,7 @@ static int wm0010_firmware_load(char *name, struct snd_soc_codec *codec)
 	struct completion done;
 	const struct firmware *fw;
 	const struct dfw_binrec *rec;
+	const struct dfw_inforec *inforec;
 	u64 *img;
 	u8 *out, dsp;
 	u32 len, offset;
@@ -357,8 +368,9 @@ static int wm0010_firmware_load(char *name, struct snd_soc_codec *codec)
 	}
 
 	rec = (const struct dfw_binrec *)fw->data;
+	inforec = (const struct dfw_inforec *)rec->data;
 	offset = 0;
-	dsp = rec->data[0];
+	dsp = inforec->dsp_target;
 	wm0010->boot_failed = false;
 	BUG_ON(!list_empty(&xfer_list));
 	init_completion(&done);
@@ -369,6 +381,17 @@ static int wm0010_firmware_load(char *name, struct snd_soc_codec *codec)
 		ret = -EINVAL;
 		goto abort;
 	}
+
+	if (inforec->info_version != INFO_VERSION) {
+		dev_err(codec->dev,
+			"Unsupported version (%02d) of INFO record\r\n",
+			inforec->info_version);
+		ret = -EINVAL;
+		goto abort;
+	}
+
+	dev_dbg(codec->dev, "Version v%02d INFO record found\r\n",
+		inforec->info_version);
 
 	/* Check it's a DSP file */
 	if (dsp != DEVICE_ID_WM0010) {
