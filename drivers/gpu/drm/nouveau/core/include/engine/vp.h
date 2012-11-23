0edcbc519aae0f5123ddfa0359f863c52dffc516
Maarten Lankhorst <maarten.lankhorst@canonical.com>
2012-11-23 11:10:45 +1000

    nvc0/vp: initial implementation of engine
    Will allow use of the engine if firmware (nvXX_fuc085) provided.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@canonical.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/include/engine/vp.h
+++ drivers/gpu/drm/nouveau/core/include/engine/vp.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NOUVEAU_VP_H__
 
 extern struct nouveau_oclass nv84_vp_oclass;
+extern struct nouveau_oclass nvc0_vp_oclass;
 extern struct nouveau_oclass nve0_vp_oclass;
 
 #endif
