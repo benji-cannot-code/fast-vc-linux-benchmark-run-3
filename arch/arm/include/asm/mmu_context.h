775a93871e4bb683e51f69148634ada33f0689b3
Nicolas Pitre <nicolas.pitre@linaro.org>
2012-11-25 03:24:32 +0100

    ARM: 7582/2: rename kvm_seq to vmalloc_seq so to avoid confusion with KVM
    The kvm_seq value has nothing to do what so ever with this other KVM.
    Given that KVM support on ARM is imminent, it's best to rename kvm_seq
    into something else to clearly identify what it is about i.e. a sequence
    number for vmalloc section mappings.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/mmu_context.h
+++ arch/arm/include/asm/mmu_context.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/proc-fns.h>
 #include <asm-generic/mm_hooks.h>
 
-void __check_kvm_seq(struct mm_struct *mm);
+void __check_vmalloc_seq(struct mm_struct *mm);
 
 #ifdef CONFIG_CPU_HAS_ASID
 
@@ -35,8 +35,8 @@ void check_and_switch_context(struct mm_struct *mm, struct task_struct *tsk);
 static inline void check_and_switch_context(struct mm_struct *mm,
 					    struct task_struct *tsk)
 {
-	if (unlikely(mm->context.kvm_seq != init_mm.context.kvm_seq))
-		__check_kvm_seq(mm);
+	if (unlikely(mm->context.vmalloc_seq != init_mm.context.vmalloc_seq))
+		__check_vmalloc_seq(mm);
 
 	if (irqs_disabled())
 		/*
