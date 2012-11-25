ab891b3d496edae7387346121797ccffb91c184c
Joe Perches <joe@perches.com>
2012-11-25 18:26:34 +0000

    ip6mr: Add sizeof verification to MRT6_ASSERT and MT6_PIM
    Verify the length of the user-space arguments.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ip6mr.c
+++ net/ipv6/ip6mr.c
@@ -1647,6 +1647,9 @@ int ip6_mroute_setsockopt(struct sock *sk, int optname, char __user *optval, uns
 	case MRT6_ASSERT:
 	{
 		int v;
+
+		if (optlen != sizeof(v))
+			return -EINVAL;
 		if (get_user(v, (int __user *)optval))
 			return -EFAULT;
 		mrt->mroute_do_assert = v;
@@ -1657,6 +1660,9 @@ int ip6_mroute_setsockopt(struct sock *sk, int optname, char __user *optval, uns
 	case MRT6_PIM:
 	{
 		int v;
+
+		if (optlen != sizeof(v))
+			return -EINVAL;
 		if (get_user(v, (int __user *)optval))
 			return -EFAULT;
 		v = !!v;
