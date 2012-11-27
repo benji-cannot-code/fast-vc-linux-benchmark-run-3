dafc0351eddaf31ee9812e63745ef4c205c6289a
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2012-11-27 11:39:40 -0500

    xen/acpi: Move the xen_running_on_version_or_later function.
    As on ia64 builds we get:
    include/xen/interface/version.h: In function 'xen_running_on_version_or_later':
    include/xen/interface/version.h:76: error: implicit declaration of function 'HYPERVISOR_xen_version'
    
    We can later on make this function exportable if there are
    modules using part of it. For right now the only two users are
    built-in.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/xen-acpi-pad.c
+++ drivers/xen/xen-acpi-pad.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <acpi/acpi_drivers.h>
 #include <asm/xen/hypercall.h>
 #include <xen/interface/version.h>
+#include <xen/xen-ops.h>
 
 #define ACPI_PROCESSOR_AGGREGATOR_CLASS	"acpi_pad"
 #define ACPI_PROCESSOR_AGGREGATOR_DEVICE_NAME "Processor Aggregator"
