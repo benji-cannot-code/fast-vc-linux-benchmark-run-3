b6fb85f91e0327ddb4726fdff574429d5fc8e813
Sachin Kamat <sachin.kamat@linaro.org>
2012-11-27 16:02:16 +0530

    f2fs: remove unneeded version.h header file from f2fs.h
    Including <linux/version.h> is not necessary.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/page-flags.h>
 #include <linux/buffer_head.h>
-#include <linux/version.h>
 #include <linux/slab.h>
 #include <linux/crc32.h>
 #include <linux/magic.h>
