c0735369686e7da561d2faaf68e08677d4d45e63
Chris Wilson <chris@chris-wilson.co.uk>
2012-11-27 16:22:52 +0000

    drm/i915: Preallocate next seqno before touching the ring
    Based on the work by Mika Kuoppala, we realised that we need to handle
    seqno wraparound prior to committing our changes to the ring. The most
    obvious point then is to grab the seqno inside intel_ring_begin(), and
    then to reuse that seqno for all ring operations until the next request.
    As intel_ring_begin() can fail, the callers must already be prepared to
    handle such failure and so we can safely add further checks.
    
    This patch looks like it should be split up into the interface
    changes and the tweaks to move seqno wrapping from the execbuffer into
    the core seqno increment. However, I found no easy way to break it into
    incremental steps without introducing further broken behaviour.
    
    v2: Mika found a silly mistake and a subtle error in the existing code;
    inside i915_gem_retire_requests() we were resetting the sync_seqno of
    the target ring based on the seqno from this ring - which are only
    related by the order of their allocation, not retirement. Hence we were
    applying the optimisation that the rings were synchronised too early,
    fortunately the only real casualty there is the handling of seqno
    wrapping.
    
    v3: Do not forget to reset the sync_seqno upon module reinitialisation,
    ala resume.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@intel.com>
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=863861
    Reviewed-by: Mika Kuoppala <mika.kuoppala@intel.com> [v2]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_ringbuffer.h
+++ drivers/gpu/drm/i915/intel_ringbuffer.h
@@ -71,8 +71,7 @@ struct  intel_ring_buffer {
 	int __must_check (*flush)(struct intel_ring_buffer *ring,
 				  u32	invalidate_domains,
 				  u32	flush_domains);
-	int		(*add_request)(struct intel_ring_buffer *ring,
-				       u32 *seqno);
+	int		(*add_request)(struct intel_ring_buffer *ring);
 	/* Some chipsets are not quite as coherent as advertised and need
 	 * an expensive kick to force a true read of the up-to-date seqno.
 	 * However, the up-to-date seqno is not always required and the last
@@ -206,7 +205,6 @@ static inline void intel_ring_emit(struct intel_ring_buffer *ring,
 
 void intel_ring_advance(struct intel_ring_buffer *ring);
 
-u32 intel_ring_get_seqno(struct intel_ring_buffer *ring);
 int intel_ring_flush_all_caches(struct intel_ring_buffer *ring);
 int intel_ring_invalidate_all_caches(struct intel_ring_buffer *ring);
 
@@ -220,6 +218,12 @@ void intel_ring_setup_status_page(struct intel_ring_buffer *ring);
 static inline u32 intel_ring_get_tail(struct intel_ring_buffer *ring)
 {
 	return ring->tail;
+}
+
+static inline u32 intel_ring_get_seqno(struct intel_ring_buffer *ring)
+{
+	BUG_ON(ring->outstanding_lazy_request == 0);
+	return ring->outstanding_lazy_request;
 }
 
 static inline void i915_trace_irq_get(struct intel_ring_buffer *ring, u32 seqno)
