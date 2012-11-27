39914fadd05bb4ab263542d0c8fd373d8a5d9f47
Luciano Coelho <coelho@ti.com>
2012-11-27 15:51:56 +0200

    wlcore/wl12xx: move wl12xx chip ID defines to the lower driver
    Move wl12xx-specific chip ID macros to the wl12xx driver and rename
    them to 127X and 128X for clarity since both the "1" (2.4GHz) and the
    "3" (2.4GHz and 5GHz) variants use the same chip ID.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- drivers/net/wireless/ti/wl12xx/wl12xx.h
+++ drivers/net/wireless/ti/wl12xx/wl12xx.h
@@ -25,6 +25,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "conf.h"
 
+/* WiLink 6/7 chip IDs */
+#define CHIP_ID_127X_PG10              (0x04030101)
+#define CHIP_ID_127X_PG20              (0x04030111)
+#define CHIP_ID_128X_PG10              (0x05030101)
+#define CHIP_ID_128X_PG20              (0x05030111)
+
 /* minimum FW required for driver for wl127x */
 #define WL127X_CHIP_VER		6
 #define WL127X_IFTYPE_VER	3
