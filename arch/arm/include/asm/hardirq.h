81a421eb5dd00294996b11187fb2b26f351c6feb
Nicolas Pitre <nicolas.pitre@linaro.org>
2012-11-27 21:54:41 -0500

    ARM: SMP: basic IPI triggered completion support
    We need a mechanism to let an inbound CPU signal that it is alive before
    even getting into the kernel environment i.e. from early assembly code.
    Using an IPI is the simplest way to achieve that.
    
    This adds some basic infrastructure to register a struct completion
    pointer to be "completed" when the dedicated IPI for this task is
    received.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>

--- arch/arm/include/asm/hardirq.h
+++ arch/arm/include/asm/hardirq.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/threads.h>
 #include <asm/irq.h>
 
-#define NR_IPI	6
+#define NR_IPI	7
 
 typedef struct {
 	unsigned int __softirq_pending;
