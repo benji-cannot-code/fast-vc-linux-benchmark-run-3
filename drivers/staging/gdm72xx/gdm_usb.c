81bb85b400981bc010704bfceb64e599398c1340
Dan Carpenter <dan.carpenter@oracle.com>
2012-11-29 17:17:25 +0300

    staging: gdm72xx: unlock on error in init_usb()
    We recently added locking here and there was an error path which is
    missing an unlock.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm72xx/gdm_usb.c
+++ drivers/staging/gdm72xx/gdm_usb.c
@@ -206,6 +206,7 @@ static int init_usb(struct usbwm_dev *udev)
 	for (i = 0; i < MAX_NR_SDU_BUF; i++) {
 		t = alloc_tx_struct(tx);
 		if (t == NULL) {
+			spin_unlock_irqrestore(&tx->lock, flags);
 			ret = -ENOMEM;
 			goto fail;
 		}
