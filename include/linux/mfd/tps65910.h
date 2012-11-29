a5047ed9743aed1182d762e1421ce0a90b26e879
Kim, Milo <Milo.Kim@ti.com>
2012-11-29 08:48:26 +0000

    mfd: tps65910: Remove unused data
    The 'io_mutex' is not used anywhere.
    The regmap API supports the mutex internally, so no additional mutex
    is required.
    
    And 'domain' private data is unnecessary because the irq domain is
    already registered by using regmap_add_irq_chip().
    
    Signed-off-by: Milo(Woogyom) Kim <milo.kim@ti.com>
    Acked-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/tps65910.h
+++ include/linux/mfd/tps65910.h
@@ -894,7 +894,6 @@ struct tps65910 {
 	struct device *dev;
 	struct i2c_client *i2c_client;
 	struct regmap *regmap;
-	struct mutex io_mutex;
 	unsigned int id;
 
 	/* Client devices */
@@ -908,7 +907,6 @@ struct tps65910 {
 	/* IRQ Handling */
 	int chip_irq;
 	struct regmap_irq_chip_data *irq_data;
-	struct irq_domain *domain;
 };
 
 struct tps65910_platform_data {
