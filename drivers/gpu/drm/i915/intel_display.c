2818242b1405f75727fb205ef32a83a8d73ef59c
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-11-29 22:18:51 +0100

    drm/i915: fixup sparse warnings
    - __iomem where there is none (I love how we mix these things up).
    - Use gfp_t instead of an other plain type.
    - Unconfuse one place about enum pipe vs enum transcoder - for the pch
      transcoder we actually use the pipe enum. Fixup the other cases
      where we assign the pipe to the cpu transcoder with explicit casts.
    - Declare the mch_lock properly in a header.
    
    There is still a decent mess in intel_bios.c about __iomem, but heck,
    this is x86 and we're allowed to do that.
    
    Makes-sparse-happy: Chris Wilson <chris@chris-wilson.co.uk>
    [danvet: Use a space after the cast consistently and fix up the
    newly-added cast in i915_irq.c to properly use __iomem.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -1672,7 +1672,7 @@ static void lpt_enable_pch_transcoder(struct drm_i915_private *dev_priv,
 	BUG_ON(dev_priv->info->gen < 5);
 
 	/* FDI must be feeding us bits for PCH ports */
-	assert_fdi_tx_enabled(dev_priv, cpu_transcoder);
+	assert_fdi_tx_enabled(dev_priv, (enum pipe) cpu_transcoder);
 	assert_fdi_rx_enabled(dev_priv, TRANSCODER_A);
 
 	/* Workaround: set timing override bit. */
@@ -1760,7 +1760,7 @@ static void intel_enable_pipe(struct drm_i915_private *dev_priv, enum pipe pipe,
 {
 	enum transcoder cpu_transcoder = intel_pipe_to_cpu_transcoder(dev_priv,
 								      pipe);
-	enum transcoder pch_transcoder;
+	enum pipe pch_transcoder;
 	int reg;
 	u32 val;
 
@@ -1780,7 +1780,8 @@ static void intel_enable_pipe(struct drm_i915_private *dev_priv, enum pipe pipe,
 		if (pch_port) {
 			/* if driving the PCH, we need FDI enabled */
 			assert_fdi_rx_pll_enabled(dev_priv, pch_transcoder);
-			assert_fdi_tx_pll_enabled(dev_priv, cpu_transcoder);
+			assert_fdi_tx_pll_enabled(dev_priv,
+						  (enum pipe) cpu_transcoder);
 		}
 		/* FIXME: assert CPU port conditions for SNB+ */
 	}
@@ -3599,7 +3600,7 @@ static void haswell_crtc_off(struct drm_crtc *crtc)
 
 	/* Stop saying we're using TRANSCODER_EDP because some other CRTC might
 	 * start using it. */
-	intel_crtc->cpu_transcoder = intel_crtc->pipe;
+	intel_crtc->cpu_transcoder = (enum transcoder) intel_crtc->pipe;
 
 	intel_ddi_put_crtc_pll(crtc);
 }
