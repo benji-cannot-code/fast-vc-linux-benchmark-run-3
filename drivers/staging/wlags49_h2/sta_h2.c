29db48c2993be5c2b530b79112146bad1939310f
Johan Meiring <johanmeiring@gmail.com>
2012-11-30 18:58:43 +0200

    staging: wlags49_h2: sta_h2: fixes spaces around parentheses
    This commit fixes an instance where the spacing around parentheses
    was incorrect.
    
    Signed-off-by: Johan Meiring <johanmeiring@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlags49_h2/sta_h2.c
+++ drivers/staging/wlags49_h2/sta_h2.c
@@ -4382,7 +4382,7 @@ static const hcf_8 fw_image_4_data[] = {
 
 static const CFG_IDENTITY_STRCT fw_image_infoidentity[] = {
 	{
-		sizeof( CFG_IDENTITY_STRCT ) / sizeof(hcf_16) - 1,
+		sizeof(CFG_IDENTITY_STRCT) / sizeof(hcf_16) - 1,
 		CFG_FW_IDENTITY,
 		COMP_ID_FW_STA,
 		3,							//Variant
