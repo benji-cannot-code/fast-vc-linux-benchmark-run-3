6334170bacdf4976ec53c53cfa5def0da00cb909
Johan Meiring <johanmeiring@gmail.com>
2012-11-30 18:58:44 +0200

    staging: wlags49_h2: sta_h2: fixes casting style issue
    This commit cleans up a styling issue when casting to "hcf_8 *".
    
    Signed-off-by: Johan Meiring <johanmeiring@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlags49_h2/sta_h2.c
+++ drivers/staging/wlags49_h2/sta_h2.c
@@ -4400,7 +4400,7 @@ static const CFG_PROG_STRCT fw_image_code[] = {
 		0x0186,		// sizeof(fw_image_1_data),
 		0x00000060,					// Target address in NIC Memory
 		0x0000,						// CRC: yes/no	TYPE: primary/station/tertiary
-		(hcf_8 FAR   *) fw_image_1_data
+		(hcf_8 *)fw_image_1_data
 	},
 	{
 		8,
@@ -4409,7 +4409,7 @@ static const CFG_PROG_STRCT fw_image_code[] = {
 		0x2518,		// sizeof(fw_image_2_data),
 		0x00000C16,					// Target address in NIC Memory
 		0x0000,						// CRC: yes/no	TYPE: primary/station/tertiary
-		(hcf_8 FAR   *) fw_image_2_data
+		(hcf_8 *)fw_image_2_data
 	},
 	{
 		8,
@@ -4418,7 +4418,7 @@ static const CFG_PROG_STRCT fw_image_code[] = {
 		0x3daa,		// sizeof(fw_image_3_data),
 		0x001E312E,					// Target address in NIC Memory
 		0x0000,						// CRC: yes/no	TYPE: primary/station/tertiary
-		(hcf_8 FAR   *) fw_image_3_data
+		(hcf_8 *)fw_image_3_data
 	},
 	{
 		8,
@@ -4427,7 +4427,7 @@ static const CFG_PROG_STRCT fw_image_code[] = {
 		0xaa66,		// sizeof(fw_image_4_data),
 		0x001F4000,					// Target address in NIC Memory
 		0x0000,						// CRC: yes/no	TYPE: primary/station/tertiary
-		(hcf_8 FAR   *) fw_image_4_data
+		(hcf_8 *)fw_image_4_data
 	},
 	{
 		5,
