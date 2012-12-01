a8e48f13c0bd98e69fe5bc1672be3f2e27698c4f
K. Y. Srinivasan <kys@microsoft.com>
2012-12-01 06:46:37 -0800

    Drivers: hv: Get rid of hv_get_ringbuffer_interrupt_mask()
    This function is no longer used; get rid of it.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Reviewed-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hv/hyperv_vmbus.h
+++ drivers/hv/hyperv_vmbus.h
@@ -566,7 +566,6 @@ int hv_ringbuffer_read(struct hv_ring_buffer_info *ring_info,
 		   u32 buflen,
 		   u32 offset);
 
-u32 hv_get_ringbuffer_interrupt_mask(struct hv_ring_buffer_info *ring_info);
 
 void hv_ringbuffer_get_debuginfo(struct hv_ring_buffer_info *ring_info,
 			    struct hv_ring_buffer_debug_info *debug_info);
