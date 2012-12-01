3d50735c3f1ce354944b44ec3e306f404e165ebb
Lars-Peter Clausen <lars@metafoo.de>
2012-12-01 14:24:40 +0100

    asm-generic/mmu.h: Remove unused vmlist field from mm_context_t
    Nothing is using the vmlist field in mm_context_t anymore. It has been removed
    from the non-generic versions over 3 years ago 8feae1311 ("NOMMU: Make VMAs per
    MM as for MMU-mode linux").
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/asm-generic/mmu.h
+++ include/asm-generic/mmu.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifndef __ASSEMBLY__
 typedef struct {
-	struct vm_list_struct	*vmlist;
 	unsigned long		end_brk;
 } mm_context_t;
 #endif
