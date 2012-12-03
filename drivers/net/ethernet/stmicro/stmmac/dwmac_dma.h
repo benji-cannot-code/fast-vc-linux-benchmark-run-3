294cdb5200dde834ce66ed0da12023c52d6bc2d1
walimis <walimisdev@gmail.com>
2012-12-03 02:19:44 +0000

    stmmac: remove two repeated macros
    Two macros have been defined twice, remove them.
    
    Signed-off-by: Liming Wang <walimisdev@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwmac_dma.h
+++ drivers/net/ethernet/stmicro/stmmac/dwmac_dma.h
@@ -81,8 +81,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DMA_STATUS_GPI		0x10000000	/* PMT interrupt */
 #define DMA_STATUS_GMI		0x08000000	/* MMC interrupt */
 #define DMA_STATUS_GLI		0x04000000	/* GMAC Line interface int */
-#define DMA_STATUS_GMI		0x08000000
-#define DMA_STATUS_GLI		0x04000000
 #define DMA_STATUS_EB_MASK	0x00380000	/* Error Bits Mask */
 #define DMA_STATUS_EB_TX_ABORT	0x00080000	/* Error Bits - TX Abort */
 #define DMA_STATUS_EB_RX_ABORT	0x00100000	/* Error Bits - RX Abort */
