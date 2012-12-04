26dceab61d37f0d6fa11c284e59e31476ed21288
Luis Alves <ljalvs@gmail.com>
2012-12-04 23:04:49 +0000

    m68knommu: platform code merge for 68000 core cpus
    This patch merges all 68000 core cpus into one directory.
    There is a lot of common code in the 68328, 68EZ328 and 68VZ328 directories.
    
    This will also facilitate easy development of support for original stand
    alone MC68000 CPU machines.
    
    Signed-off-by: Luis Alves <ljalvs@gmail.com>
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/platform/68000/m68VZ328.c
+++ arch/m68k/platform/68000/m68VZ328.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /***************************************************************************/
 
 /*
- *  linux/arch/m68knommu/platform/68VZ328/config.c
+ *  m68VZ328.c - 68VZ328 specific config
  *
  *  Copyright (C) 1993 Hamish Macdonald
  *  Copyright (C) 1999 D. Jeff Dionne
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/bootstd.h>
 
 #ifdef CONFIG_INIT_LCD
-#include "bootlogo.h"
+#include "bootlogo-vz.h"
 #endif
 
 /***************************************************************************/
