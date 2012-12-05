ea8c88df6a8e6af5a6aaa41be1db458ff3ab9a9d
Arend van Spriel <arend@broadcom.com>
2012-12-05 15:26:03 +0100

    brcmfmac: remove WL_SCAN() macro
    Get rid of WL_SCAN(...) macro in favor of brcmf_dbg(SCAN,...)
    
    Reviewed-by: Hante Meuleman <meuleman@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/wl_cfg80211.h
+++ drivers/net/wireless/brcm80211/brcmfmac/wl_cfg80211.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dhd_dbg.h"
 
-#define	WL_SCAN(fmt, ...)	brcmf_dbg(SCAN, fmt, ##__VA_ARGS__)
 #define	WL_CONN(fmt, ...)	brcmf_dbg(CONN, fmt, ##__VA_ARGS__)
 
 #define WL_NUM_SCAN_MAX			10
