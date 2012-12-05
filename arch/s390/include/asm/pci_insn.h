706ba37e2d9cd5233283b6c72d4855bedba3ac57
Jan Glauber <jang@linux.vnet.ibm.com>
2012-12-05 14:43:25 +0100

    s390/pci: no msleep in potential IRQ context
    The PCI instructions may be used in IRQ context so scheduling is forbidden.
    Use udelay and shorten the delay since we are now polling.
    
    Signed-off-by: Jan Glauber <jang@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/pci_insn.h
+++ arch/s390/include/asm/pci_insn.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/delay.h>
 
-#define ZPCI_INSN_BUSY_DELAY	1	/* 1 millisecond */
+#define ZPCI_INSN_BUSY_DELAY	1	/* 1 microsecond */
 
 /* Load/Store status codes */
 #define ZPCI_PCI_ST_FUNC_NOT_ENABLED		4
@@ -139,7 +139,7 @@ static inline int rpcit_instr(u64 fn, u64 addr, u64 range)
 	do {
 		cc = __rpcit(fn, addr, range, &status);
 		if (cc == 2)
-			msleep(ZPCI_INSN_BUSY_DELAY);
+			udelay(ZPCI_INSN_BUSY_DELAY);
 	} while (cc == 2);
 
 	if (cc)
@@ -199,7 +199,7 @@ static inline int pcilg_instr(u64 *data, u64 req, u64 offset)
 	do {
 		cc = __pcilg(data, req, offset, &status);
 		if (cc == 2)
-			msleep(ZPCI_INSN_BUSY_DELAY);
+			udelay(ZPCI_INSN_BUSY_DELAY);
 	} while (cc == 2);
 
 	if (cc) {
@@ -237,7 +237,7 @@ static inline int pcistg_instr(u64 data, u64 req, u64 offset)
 	do {
 		cc = __pcistg(data, req, offset, &status);
 		if (cc == 2)
-			msleep(ZPCI_INSN_BUSY_DELAY);
+			udelay(ZPCI_INSN_BUSY_DELAY);
 	} while (cc == 2);
 
 	if (cc)
@@ -269,7 +269,7 @@ static inline int pcistb_instr(const u64 *data, u64 req, u64 offset)
 	do {
 		cc = __pcistb(data, req, offset, &status);
 		if (cc == 2)
-			msleep(ZPCI_INSN_BUSY_DELAY);
+			udelay(ZPCI_INSN_BUSY_DELAY);
 	} while (cc == 2);
 
 	if (cc)
