2e87b23342c6ce2cb708d6713062797f92f55f0b
Haren Myneni <haren@linux.vnet.ibm.com>
2012-12-06 21:48:26 +0000

    powerpc: Increase exceptions arrays in paca struct to save PPR
    [PATCH 3/6] powerpc: Increase exceptions arrays in paca struct to save PPR
    
    Using paca to save user defined PPR value in the first level exception vector.
    
    Signed-off-by: Haren Myneni <haren@us.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/exception-64s.h
+++ arch/powerpc/include/asm/exception-64s.h
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EX_R3		64
 #define EX_LR		72
 #define EX_CFAR		80
+#define EX_PPR		88	/* SMT thread status register (priority) */
 
 #ifdef CONFIG_RELOCATABLE
 #define EXCEPTION_RELON_PROLOG_PSERIES_1(label, h)			\
