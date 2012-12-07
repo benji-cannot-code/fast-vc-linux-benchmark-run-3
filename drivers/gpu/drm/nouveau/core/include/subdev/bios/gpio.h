f73e6027c2655986b833dfab338fd6e84f555596
Ben Skeggs <bskeggs@redhat.com>
2012-12-07 10:31:09 +1000

    drm/nouveau/bios: add support for parsing xpio table data
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/include/subdev/bios/gpio.h
+++ drivers/gpu/drm/nouveau/core/include/subdev/bios/gpio.h
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NVBIOS_GPIO_H__
 #define __NVBIOS_GPIO_H__
 
-struct nouveau_bios;
-
 enum dcb_gpio_func_name {
 	DCB_GPIO_PANEL_POWER = 0x01,
 	DCB_GPIO_TVDAC0 = 0x0c,
