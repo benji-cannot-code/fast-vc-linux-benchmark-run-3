eece0fd63404455788baf4f5e25f66faaf5d4e47
Sachin Kamat <sachin.kamat@linaro.org>
2012-12-07 14:18:29 +0530

    drm/i915: Remove duplicate inclusion of drm/drm_edid.h
    drm/drm_edid.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_modes.c
+++ drivers/gpu/drm/i915/intel_modes.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fb.h>
 #include <drm/drm_edid.h>
 #include <drm/drmP.h>
-#include <drm/drm_edid.h>
 #include "intel_drv.h"
 #include "i915_drv.h"
 
