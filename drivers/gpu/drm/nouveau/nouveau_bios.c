e91958aebc752a525b5982fea7c1ce5f0ae08ee5
Emil Velikov <emil.l.velikov@gmail.com>
2012-12-07 17:26:44 +0000

    drm/nouveau: set legacy bios data before parsing the structure
    Commit 767baf82 drm/nouveau: remove some more unnecessary legacy bios code
    
    has introduced a regression my misplacing the code that sets the major/chip
    versions, which are used whist parsing the bmp/bit structure in vbios
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_bios.c
+++ drivers/gpu/drm/nouveau/nouveau_bios.c
@@ -2009,6 +2009,8 @@ static bool NVInitVBIOS(struct drm_device *dev)
 
 	legacy->data = bios->data;
 	legacy->length = bios->size;
+	legacy->major_version = bios->version.major;
+	legacy->chip_version = bios->version.chip;
 	if (bios->bit_offset) {
 		legacy->type = NVBIOS_BIT;
 		legacy->offset = bios->bit_offset;
@@ -2020,8 +2022,6 @@ static bool NVInitVBIOS(struct drm_device *dev)
 		return !parse_bmp_structure(dev, legacy, legacy->offset);
 	}
 
-	legacy->major_version = bios->version.major;
-	legacy->chip_version = bios->version.chip;
 	return false;
 }
 
