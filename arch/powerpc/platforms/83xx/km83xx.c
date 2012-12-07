7961e70ea3536ab92bd770b9f622ffe2d157cf7c
Holger Brunck <holger.brunck@keymile.com>
2012-12-07 16:09:13 +0100

    powerpc/83xx: fix checkpatch warnings for km83xx.c
    Signed-off-by: Holger Brunck <holger.brunck@keymile.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/platforms/83xx/km83xx.c
+++ arch/powerpc/platforms/83xx/km83xx.c
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_device.h>
 
 #include <linux/atomic.h>
-#include <asm/time.h>
-#include <asm/io.h>
+#include <linux/time.h>
+#include <linux/io.h>
 #include <asm/machdep.h>
 #include <asm/ipic.h>
 #include <asm/irq.h>
@@ -89,15 +89,13 @@ static void __init mpc83xx_km_setup_arch(void)
 
 		np_par = of_find_node_by_name(NULL, "par_io");
 		if (np_par == NULL) {
-			printk(KERN_WARNING "%s couldn;t find par_io node\n",
-				__func__);
+			pr_warn("%s couldn;t find par_io node\n", __func__);
 			return;
 		}
 		/* Map Parallel I/O ports registers */
 		ret = of_address_to_resource(np_par, 0, &res);
 		if (ret) {
-			printk(KERN_WARNING "%s couldn;t map par_io registers\n",
-				__func__);
+			pr_warn("%s couldn;t map par_io registers\n", __func__);
 			return;
 		}
 
