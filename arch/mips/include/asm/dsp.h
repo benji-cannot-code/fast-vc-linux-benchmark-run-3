e5fe59a5b222cc779b9cea755759285fc706af96
Steven J. Hill <sjhill@mips.com>
2012-12-06 22:52:03 -0600

    MIPS: DSP: Fix DSP mask for registers.
    The DSP bit mask for the RDDSP and WRDSP instructions was wrong.
    
    [ralf@linux-mips.org: The mask field of the RDDSP and WRDSP instructions
    is 10 bits long.  DSP_MASK had all these fields which according to the
    architecture specification may result in UNPREDICTABLE operation.]
    
    Signed-off-by: Steven J. Hill <sjhill@mips.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/4683/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/dsp.h
+++ arch/mips/include/asm/dsp.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mipsregs.h>
 
 #define DSP_DEFAULT	0x00000000
-#define DSP_MASK	0x3ff
+#define DSP_MASK	0x3f
 
 #define __enable_dsp_hazard()						\
 do {									\
