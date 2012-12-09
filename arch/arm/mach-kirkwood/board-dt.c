01ae65eaf19108cf974ab053242bf5813200e67e
Willy Tarreau <w@1wt.eu>
2012-12-09 19:40:04 +0100

    ARM: kirkwood: convert Guruplug Server Plus to use the device tree
    Add a device tree entry for the Guruplug Server Plus board. This port
    was based both on the work done on the dreamplug and the dockstar.
    
    It builds, boots and works on my Guruplug Server Plus.
    
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-dt.c
+++ arch/arm/mach-kirkwood/board-dt.c
@@ -99,6 +99,9 @@ static void __init kirkwood_dt_init(void)
 	if (of_machine_is_compatible("globalscale,dreamplug"))
 		dreamplug_init();
 
+	if (of_machine_is_compatible("globalscale,guruplug"))
+		guruplug_dt_init();
+
 	if (of_machine_is_compatible("dlink,dns-kirkwood"))
 		dnskw_init();
 
@@ -150,6 +153,7 @@ static void __init kirkwood_dt_init(void)
 
 static const char * const kirkwood_dt_board_compat[] = {
 	"globalscale,dreamplug",
+	"globalscale,guruplug",
 	"dlink,dns-320",
 	"dlink,dns-325",
 	"iom,iconnect",
