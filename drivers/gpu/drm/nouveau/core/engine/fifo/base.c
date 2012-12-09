2514e6a85deff95035d0bc24d0f53dde7f0fbb97
Marcin Slusarz <marcin.slusarz@gmail.com>
2012-12-09 15:45:21 +0100

    drm/nouveau: prepare for reporting channel owner
    - record channel owner process name
    - add some helpers for accessing this information
    - let nouveau_enum hold additional value (will be needed in the next patch)
    
    Signed-off-by: Marcin Slusarz <marcin.slusarz@gmail.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/fifo/base.c
+++ drivers/gpu/drm/nouveau/core/engine/fifo/base.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Authors: Ben Skeggs
  */
 
+#include <core/client.h>
 #include <core/object.h>
 #include <core/handle.h>
 #include <core/class.h>
@@ -145,6 +146,20 @@ nouveau_fifo_chid(struct nouveau_fifo *priv, struct nouveau_object *object)
 	}
 
 	return -1;
+}
+
+const char *
+nouveau_client_name_for_fifo_chid(struct nouveau_fifo *fifo, u32 chid)
+{
+	struct nouveau_fifo_chan *chan = NULL;
+	unsigned long flags;
+
+	spin_lock_irqsave(&fifo->lock, flags);
+	if (chid >= fifo->min && chid <= fifo->max)
+		chan = (void *)fifo->channel[chid];
+	spin_unlock_irqrestore(&fifo->lock, flags);
+
+	return nouveau_client_name(chan);
 }
 
 void
