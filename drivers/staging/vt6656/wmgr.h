dbbb3d734f4fc3cac538c30dfa13042f07f07b3e
Malcolm Priestley <tvboxspy@gmail.com>
2012-12-10 21:55:59 +0000

    staging: vt6656: create new structure names to remove typedefs.
    This patch and the next 8 patches remove the old structure names and
    replace;
    
    struct vnt_private for DEVICE_INFO and PSDevice
    struct vnt_manager for SMgmtObject
    struct vnt_rx_mgmt for SRxMgmtPacket and PSRxMgmtPacket
    struct vnt_tx_mgmt for STxMgmtPacket and PSTxMgmtPacket
    
    and
    
    struct vnt_manager vnt_mgmt
    to replace
    SMgmtObject sMgmtObj;
    
    Applied after patch staging: vt6656: dpc.c :RXbBulkInProcessData clean up RSC
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/wmgr.h
+++ drivers/staging/vt6656/wmgr.h
@@ -219,8 +219,8 @@ typedef enum tagWMAC_POWER_MODE {
 
 
 
-// Tx Management Packet descriptor
-typedef struct tagSTxMgmtPacket {
+/* Tx Management Packet descriptor */
+typedef struct vnt_tx_mgmt {
 
     PUWLAN_80211HDR     p80211Header;
     unsigned int                cbMPDULen;
@@ -229,8 +229,8 @@ typedef struct tagSTxMgmtPacket {
 } STxMgmtPacket, *PSTxMgmtPacket;
 
 
-// Rx Management Packet descriptor
-typedef struct tagSRxMgmtPacket {
+/* Rx Management Packet descriptor */
+typedef struct vnt_rx_mgmt {
 
     PUWLAN_80211HDR     p80211Header;
 	u64 qwLocalTSF;
@@ -245,7 +245,7 @@ typedef struct tagSRxMgmtPacket {
 
 
 
-typedef struct tagSMgmtObject
+typedef struct vnt_manager
 {
 	void *pAdapter;
     // MAC address
