3ad07f5a8735289f53ddb47565bb11e98a50c0bd
Kevin McKinney <klmckinney1@gmail.com>
2012-12-10 21:54:56 -0500

    Staging: bcm: Properly format braces in IPv6ProtocolHdr.h
    This patch properly formats braces in IPv6ProtocolHdr.h
    as reported by checkpath.pl
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/IPv6ProtocolHdr.h
+++ drivers/staging/bcm/IPv6ProtocolHdr.h
@@ -22,8 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define IPV6_DESTOPTS_HDR_OPTIONSIZE 0x8
 
-typedef struct IPV6HeaderFormatTag
-{
+typedef struct IPV6HeaderFormatTag {
 	UCHAR  ucVersionPrio;
 	UCHAR  aucFlowLabel[3];
 	USHORT usPayloadLength;
@@ -33,8 +32,7 @@ typedef struct IPV6HeaderFormatTag
 	ULONG  ulDestIpAddress[4];
 } IPV6Header;
 
-typedef struct IPV6RoutingHeaderFormatTag
-{
+typedef struct IPV6RoutingHeaderFormatTag {
 	UCHAR ucNextHeader;
 	UCHAR ucRoutingType;
 	UCHAR ucNumAddresses;
@@ -42,45 +40,39 @@ typedef struct IPV6RoutingHeaderFormatTag
 	ULONG ulReserved;
 } IPV6RoutingHeader;
 
-typedef struct IPV6FragmentHeaderFormatTag
-{
+typedef struct IPV6FragmentHeaderFormatTag {
 	UCHAR  ucNextHeader;
 	UCHAR  ucReserved;
 	USHORT usFragmentOffset;
 	ULONG  ulIdentification;
 } IPV6FragmentHeader;
 
-typedef struct IPV6DestOptionsHeaderFormatTag
-{
+typedef struct IPV6DestOptionsHeaderFormatTag {
 	UCHAR ucNextHeader;
 	UCHAR ucHdrExtLen;
 	UCHAR ucDestOptions[6];
 } IPV6DestOptionsHeader;
 
-typedef struct IPV6HopByHopOptionsHeaderFormatTag
-{
+typedef struct IPV6HopByHopOptionsHeaderFormatTag {
 	UCHAR ucNextHeader;
 	UCHAR ucMisc[3];
 	ULONG ulJumboPayloadLen;
 } IPV6HopByHopOptionsHeader;
 
-typedef struct IPV6AuthenticationHeaderFormatTag
-{
+typedef struct IPV6AuthenticationHeaderFormatTag {
 	UCHAR  ucNextHeader;
 	UCHAR  ucLength;
 	USHORT usReserved;
 	ULONG  ulSecurityParametersIndex;
 } IPV6AuthenticationHeader;
 
-typedef struct IPV6IcmpHeaderFormatTag
-{
+typedef struct IPV6IcmpHeaderFormatTag {
 	UCHAR  ucType;
 	UCHAR  ucCode;
 	USHORT usChecksum;
 } IPV6IcmpHeader;
 
-typedef enum _E_IPADDR_CONTEXT
-{
+typedef enum _E_IPADDR_CONTEXT {
 	eSrcIpAddress,
 	eDestIpAddress
 } E_IPADDR_CONTEXT;
