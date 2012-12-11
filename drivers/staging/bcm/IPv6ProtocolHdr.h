ee6285142f3d4bf1529bea583968998af2fd7ed5
Kevin McKinney <klmckinney1@gmail.com>
2012-12-10 21:54:57 -0500

    Staging: bcm: Replace USHORT with unsigned short in IPv6ProtocolHdr.h
    This patch replace "USHORT" with "unsigned
    short" in IPv6ProtocolHdr.h
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/IPv6ProtocolHdr.h
+++ drivers/staging/bcm/IPv6ProtocolHdr.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 typedef struct IPV6HeaderFormatTag {
 	UCHAR  ucVersionPrio;
 	UCHAR  aucFlowLabel[3];
-	USHORT usPayloadLength;
+	unsigned short usPayloadLength;
 	UCHAR  ucNextHeader;
 	UCHAR  ucHopLimit;
 	ULONG  ulSrcIpAddress[4];
@@ -43,7 +43,7 @@ typedef struct IPV6RoutingHeaderFormatTag {
 typedef struct IPV6FragmentHeaderFormatTag {
 	UCHAR  ucNextHeader;
 	UCHAR  ucReserved;
-	USHORT usFragmentOffset;
+	unsigned short usFragmentOffset;
 	ULONG  ulIdentification;
 } IPV6FragmentHeader;
 
@@ -62,14 +62,14 @@ typedef struct IPV6HopByHopOptionsHeaderFormatTag {
 typedef struct IPV6AuthenticationHeaderFormatTag {
 	UCHAR  ucNextHeader;
 	UCHAR  ucLength;
-	USHORT usReserved;
+	unsigned short usReserved;
 	ULONG  ulSecurityParametersIndex;
 } IPV6AuthenticationHeader;
 
 typedef struct IPV6IcmpHeaderFormatTag {
 	UCHAR  ucType;
 	UCHAR  ucCode;
-	USHORT usChecksum;
+	unsigned short usChecksum;
 } IPV6IcmpHeader;
 
 typedef enum _E_IPADDR_CONTEXT {
@@ -79,14 +79,14 @@ typedef enum _E_IPADDR_CONTEXT {
 
 /* Function Prototypes */
 
-USHORT IpVersion6(struct bcm_mini_adapter *Adapter, /* < Pointer to the driver control structure */
+unsigned short IpVersion6(struct bcm_mini_adapter *Adapter, /* < Pointer to the driver control structure */
 					PVOID pcIpHeader, /* <Pointer to the IP Hdr of the packet */
 					struct bcm_classifier_rule *pstClassifierRule);
 
 VOID DumpIpv6Address(ULONG *puIpv6Address);
 
-extern BOOLEAN MatchSrcPort(struct bcm_classifier_rule *pstClassifierRule, USHORT ushSrcPort);
-extern BOOLEAN MatchDestPort(struct bcm_classifier_rule *pstClassifierRule, USHORT ushSrcPort);
+extern BOOLEAN MatchSrcPort(struct bcm_classifier_rule *pstClassifierRule, unsigned short ushSrcPort);
+extern BOOLEAN MatchDestPort(struct bcm_classifier_rule *pstClassifierRule, unsigned short ushSrcPort);
 extern BOOLEAN MatchProtocol(struct bcm_classifier_rule *pstClassifierRule, UCHAR ucProtocol);
 
 #endif
