2194d97ce1b3e86d99348ee3d23e33861a5a6c4c
Kevin McKinney <klmckinney1@gmail.com>
2012-12-10 21:54:58 -0500

    Staging: bcm: Replace ULONG with unsigned long in IPv6ProtocolHdr.h
    This patch replaces "ULONG" with "unsigned long" in
    IPv6ProtocolHdr.h.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/IPv6ProtocolHdr.h
+++ drivers/staging/bcm/IPv6ProtocolHdr.h
@@ -28,8 +28,8 @@ typedef struct IPV6HeaderFormatTag {
 	unsigned short usPayloadLength;
 	UCHAR  ucNextHeader;
 	UCHAR  ucHopLimit;
-	ULONG  ulSrcIpAddress[4];
-	ULONG  ulDestIpAddress[4];
+	unsigned long  ulSrcIpAddress[4];
+	unsigned long  ulDestIpAddress[4];
 } IPV6Header;
 
 typedef struct IPV6RoutingHeaderFormatTag {
@@ -37,14 +37,14 @@ typedef struct IPV6RoutingHeaderFormatTag {
 	UCHAR ucRoutingType;
 	UCHAR ucNumAddresses;
 	UCHAR ucNextAddress;
-	ULONG ulReserved;
+	unsigned long ulReserved;
 } IPV6RoutingHeader;
 
 typedef struct IPV6FragmentHeaderFormatTag {
 	UCHAR  ucNextHeader;
 	UCHAR  ucReserved;
 	unsigned short usFragmentOffset;
-	ULONG  ulIdentification;
+	unsigned long  ulIdentification;
 } IPV6FragmentHeader;
 
 typedef struct IPV6DestOptionsHeaderFormatTag {
@@ -56,14 +56,14 @@ typedef struct IPV6DestOptionsHeaderFormatTag {
 typedef struct IPV6HopByHopOptionsHeaderFormatTag {
 	UCHAR ucNextHeader;
 	UCHAR ucMisc[3];
-	ULONG ulJumboPayloadLen;
+	unsigned long ulJumboPayloadLen;
 } IPV6HopByHopOptionsHeader;
 
 typedef struct IPV6AuthenticationHeaderFormatTag {
 	UCHAR  ucNextHeader;
 	UCHAR  ucLength;
 	unsigned short usReserved;
-	ULONG  ulSecurityParametersIndex;
+	unsigned long  ulSecurityParametersIndex;
 } IPV6AuthenticationHeader;
 
 typedef struct IPV6IcmpHeaderFormatTag {
@@ -83,7 +83,7 @@ unsigned short IpVersion6(struct bcm_mini_adapter *Adapter, /* < Pointer to the 
 					PVOID pcIpHeader, /* <Pointer to the IP Hdr of the packet */
 					struct bcm_classifier_rule *pstClassifierRule);
 
-VOID DumpIpv6Address(ULONG *puIpv6Address);
+VOID DumpIpv6Address(unsigned long *puIpv6Address);
 
 extern BOOLEAN MatchSrcPort(struct bcm_classifier_rule *pstClassifierRule, unsigned short ushSrcPort);
 extern BOOLEAN MatchDestPort(struct bcm_classifier_rule *pstClassifierRule, unsigned short ushSrcPort);
