f107f53a47e82830c35777ac48a3092fff08fb69
David Howells <dhowells@redhat.com>
2012-12-12 15:36:40 +0000

    MN10300: ASB2305 PCI code needs linux/irq.h
    ASB2305 PCI code needs to #include linux/irq.h for XIRQ1 so that it can set
    the CPU interrupt priority level on the PCI interrupt.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- arch/mn10300/unit-asb2305/pci.c
+++ arch/mn10300/unit-asb2305/pci.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/delay.h>
+#include <linux/irq.h>
 #include <asm/io.h>
 #include "pci-asb2305.h"
 
