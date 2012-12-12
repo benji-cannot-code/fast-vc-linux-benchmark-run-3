16714bdac4a5ba1b0075d55f9cb7fa9f82dc6b45
Zhang Rui <rui.zhang@intel.com>
2012-12-12 15:31:37 +0800

    Thermal: Fix DEFAULT_THERMAL_GOVERNOR
    Fix DEFAULT_THERMAL_GOVERNOR to be consistant with the
    default governor selected in kernel config file.
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -47,8 +47,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define THERMAL_GENL_VERSION                    0x01
 #define THERMAL_GENL_MCAST_GROUP_NAME           "thermal_mc_group"
 
-/* Default Thermal Governor: Does Linear Throttling */
-#define DEFAULT_THERMAL_GOVERNOR	"step_wise"
+/* Default Thermal Governor */
+#if defined(CONFIG_THERMAL_DEFAULT_GOV_STEP_WISE)
+#define DEFAULT_THERMAL_GOVERNOR       "step_wise"
+#elif defined(CONFIG_THERMAL_DEFAULT_GOV_FAIR_SHARE)
+#define DEFAULT_THERMAL_GOVERNOR       "fair_share"
+#elif defined(CONFIG_THERMAL_DEFAULT_GOV_USER_SPACE)
+#define DEFAULT_THERMAL_GOVERNOR       "user_space"
+#endif
 
 struct thermal_zone_device;
 struct thermal_cooling_device;
