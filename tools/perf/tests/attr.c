d42dc31f4e8429bad216cb6df3703132a861128d
Arnaldo Carvalho de Melo <acme@redhat.com>
2012-12-13 16:43:04 -0300

    perf python: Fix breakage introduced by the test_attr infrastructure
    The test_attr infrastructure hooks on the sys_perf_event_open call,
    checking if a variable is set and if so calling a function to intercept
    calls and do the checking.
    
    But both the variable and the function aren't on objects that are
    linked on the python binding, breaking it:
    
      # perf test -v 15
      15: Try 'use perf' in python, checking link problems       :
      --- start ---
      Traceback (most recent call last):
        File "<stdin>", line 1, in <module>
      ImportError: /home/acme/git/build/perf//python/perf.so: undefined symbol: test_attr__enabled
      ---- end ----
      Try 'use perf' in python, checking link problems: FAILED!
      #
    
    Fix it by moving the variable to one of the linked object files and
    providing a stub for the function in the python.o object, that is only
    linked in the python binding.
    
    Now 'perf test' is happy again:
    
      # perf test 15
      15: Try 'use perf' in python, checking link problems       : Ok
      #
    
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Namhyung Kim <namhyung@gmail.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/n/tip-0rsca2kn44b38rgdpr3tz6n5@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/attr.c
+++ tools/perf/tests/attr.c
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int verbose;
 
-bool test_attr__enabled;
-
 static char *dir;
 
 void test_attr__init(void)
--- tools/perf/util/util.c
+++ tools/perf/util/util.c
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 unsigned int page_size;
 
+bool test_attr__enabled;
+
 bool perf_host  = true;
 bool perf_guest = false;
 
