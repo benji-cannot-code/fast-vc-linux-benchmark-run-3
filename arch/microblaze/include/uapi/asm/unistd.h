d22b24469b601f018a9e0c3552992d42c41aa39d
Michal Simek <michal.simek@xilinx.com>
2012-12-13 17:26:23 +0100

    microblaze: Wire-up new system call kcmp
    Wire-up kcmp syscall.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/uapi/asm/unistd.h
+++ arch/microblaze/include/uapi/asm/unistd.h
@@ -395,7 +395,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_sendmmsg		376
 #define __NR_process_vm_readv	377
 #define __NR_process_vm_writev	378
+#define __NR_kcmp		379
 
-#define __NR_syscalls		379
+#define __NR_syscalls		380
 
 #endif /* _UAPI_ASM_MICROBLAZE_UNISTD_H */
