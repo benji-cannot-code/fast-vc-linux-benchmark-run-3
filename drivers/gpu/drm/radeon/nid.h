51a2cf616298b84614b1dc98f0a00dfde451ee22
Alex Deucher <alexander.deucher@amd.com>
2012-12-14 11:57:36 -0500

    drm/radeon: add support for thermal sensor on tn
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/nid.h
+++ drivers/gpu/drm/radeon/nid.h
@@ -490,6 +490,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #       define CACHE_FLUSH_AND_INV_EVENT_TS                     (0x14 << 0)
 #       define CACHE_FLUSH_AND_INV_EVENT                        (0x16 << 0)
 
+/* TN SMU registers */
+#define	TN_CURRENT_GNB_TEMP				0x1F390
+
 /*
  * UVD
  */
