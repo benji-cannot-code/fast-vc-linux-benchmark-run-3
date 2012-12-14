82539080a805bfaee5df5b3bec86c96a3379c11f
Linus Walleij <linus.walleij@linaro.org>
2012-12-14 16:26:28 +0100

    ARM: ux500: fix missing include
    Commit db298da2c31e1dcf7c9b7c9cef6217ad3449ebb7
    "ARM: nomadik: move platform_data definition"
    moved the Ux500-specific musb header from <mach/usb.h>
    to <linux/platform_data/usb-musb-ux500.h>
    
    However that makes some board code fail to compile
    now:
    
    CC      arch/arm/mach-ux500/board-mop500.o
    linux/arch/arm/mach-ux500/cpu-db8500.c: In function 'u8500_init_devices':
    linux/arch/arm/mach-ux500/cpu-db8500.c:226:2: error: implicit declaration of function 'ux500_add_usb' [-Werror=implicit-function-declaration]
    cc1: some warnings being treated as errors
    make[3]: *** [arch/arm/mach-ux500/cpu-db8500.o] Error 1
    make[3]: *** Waiting for unfinished jobs....
    make[2]: *** [arch/arm/mach-ux500] Error 2
    make[2]: *** Waiting for unfinished jobs....
    
    So fix this up by adding the proper include file that
    appears to have been brought in implicitly one or another
    way in the past.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-ux500/devices-db8500.h
+++ arch/arm/mach-ux500/devices-db8500.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DEVICES_DB8500_H
 #define __DEVICES_DB8500_H
 
+#include <linux/platform_data/usb-musb-ux500.h>
 #include <mach/irqs.h>
 #include "devices-common.h"
 
