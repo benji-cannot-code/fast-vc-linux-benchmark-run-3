54b8493cd647455c996c09c88ba74bcda3ea49ab
Kevin McKinney <klmckinney1@gmail.com>
2012-12-14 19:26:46 -0500

    Staging: bcm: Fix warning: "do {} while (0) macros should not be semicolon terminated" in led_control.h
    This patch properly fixes the warning "do {} while
    (0) macros should not be semicolon terminated" as
    reported by checkpath.pl
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/led_control.h
+++ drivers/staging/bcm/led_control.h
@@ -23,14 +23,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		(Adapter->LEDInfo.LEDState[index].BitPolarity == 1) ?	\
 			wrmaltWithLock(Adapter, BCM_GPIO_OUTPUT_SET_REG, &gpio_val, sizeof(gpio_val)) : \
 			wrmaltWithLock(Adapter, BCM_GPIO_OUTPUT_CLR_REG, &gpio_val, sizeof(gpio_val)); \
-	} while (0);
+	} while (0)
 
 #define TURN_OFF_LED(GPIO, index)  do {					\
 		UINT gpio_val = GPIO;					\
 		(Adapter->LEDInfo.LEDState[index].BitPolarity == 1) ?	\
 			wrmaltWithLock(Adapter, BCM_GPIO_OUTPUT_CLR_REG, &gpio_val, sizeof(gpio_val)) : \
 			wrmaltWithLock(Adapter, BCM_GPIO_OUTPUT_SET_REG, &gpio_val, sizeof(gpio_val)); \
-	} while (0);
+	} while (0)
 
 #define B_ULONG32 unsigned long
 
