c56d188bb317ef5506c21beb0d75ab5868bd739c
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2012-12-15 23:51:11 +0100

    sh: sh7724: Register PFC platform device
    Add arch code to register the PFC platform device instead of calling the
    driver directly. Platform device registration in the sh-pfc driver will
    be removed.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/sh/kernel/cpu/sh4a/pinmux-sh7724.c
+++ arch/sh/kernel/cpu/sh4a/pinmux-sh7724.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>
+#include <cpu/pfc.h>
 #include <cpu/sh7724.h>
 
 enum {
@@ -2226,6 +2227,6 @@ static struct pinmux_info sh7724_pinmux_info = {
 
 static int __init plat_pinmux_setup(void)
 {
-	return register_pinmux(&sh7724_pinmux_info);
+	return sh_pfc_register_info(NULL, NULL, 0, &sh7724_pinmux_info);
 }
 arch_initcall(plat_pinmux_setup);
