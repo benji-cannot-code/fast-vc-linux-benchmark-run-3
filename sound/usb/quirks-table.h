2ebadb092e981d1ef482318e5c2d98e3b5424352
Eldad Zack <eldad@fogrefinery.com>
2012-12-15 05:30:33 +0100

    ALSA: usb-audio: ignore-quirk for HP Wireless Audio
    As Joe Cooper <swelljoe@gmail.com> reported, "On most HP Envy laptops
    the snd-usb-audio module causes the system to become unresponsive and
    Gnome Shell 3 to crash.".
    See also:
     http://mailman.alsa-project.org/pipermail/alsa-devel/2012-December/057729.html
    
    Add a quirk to ignore this device (for now) to solve the instability
    issue and allow other USB audio devices to be used.
    
    Reported-by: Joe Cooper <swelljoe@gmail.com>
    Tested-by: Isaac Smith <hunternet93@gmail.com>
    Signed-off-by: Eldad Zack <eldad@fogrefinery.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/usb/quirks-table.h
+++ sound/usb/quirks-table.h
@@ -100,6 +100,42 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 },
 
 /*
+ * HP Wireless Audio
+ * When not ignored, causes instability issues for some users, forcing them to
+ * blacklist the entire module.
+ */
+{
+	USB_DEVICE(0x0424, 0xb832),
+	.driver_info = (unsigned long) &(const struct snd_usb_audio_quirk) {
+		.vendor_name = "Standard Microsystems Corp.",
+		.product_name = "HP Wireless Audio",
+		.ifnum = QUIRK_ANY_INTERFACE,
+		.type = QUIRK_COMPOSITE,
+		.data = (const struct snd_usb_audio_quirk[]) {
+			/* Mixer */
+			{
+				.ifnum = 0,
+				.type = QUIRK_IGNORE_INTERFACE,
+			},
+			/* Playback */
+			{
+				.ifnum = 1,
+				.type = QUIRK_IGNORE_INTERFACE,
+			},
+			/* Capture */
+			{
+				.ifnum = 2,
+				.type = QUIRK_IGNORE_INTERFACE,
+			},
+			/* HID Device, .ifnum = 3 */
+			{
+				.ifnum = -1,
+			}
+		}
+	}
+},
+
+/*
  * Logitech QuickCam: bDeviceClass is vendor-specific, so generic interface
  * class matches do not take effect without an explicit ID match.
  */
