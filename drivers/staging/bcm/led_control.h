be96bde5536b4dbcf36a1da23e828d80b3c99a1a
Kevin McKinney <klmckinney1@gmail.com>
2012-12-14 19:26:53 -0500

    Staging: bcm: Remove typedef for _LEDColors and call directly.
    This patch removes typedef for _LEDColors, and changes
    the name of the enum to bcm_led_colors. In addition,
    any calls to LEDColors are changed to call directly.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/led_control.h
+++ drivers/staging/bcm/led_control.h
@@ -34,12 +34,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define B_ULONG32 unsigned long
 
-typedef enum _LEDColors {
+enum bcm_led_colors {
 	RED_LED		= 1,
 	BLUE_LED	= 2,
 	YELLOW_LED	= 3,
 	GREEN_LED	= 4
-} LEDColors; /* Enumerated values of different LED types */
+};
 
 enum bcm_led_events {
 	SHUTDOWN_EXIT		= 0x00,
