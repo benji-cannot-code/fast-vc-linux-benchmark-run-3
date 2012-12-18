5cf0b83b44499e83618fce93d7d78e824afdf3f1
Ed Cashin <ecashin@coraid.com>
2012-12-17 16:04:15 -0800

    aoe: allow comma separator in aoe_iflist value
    By default, the aoe driver uses any ethernet interface for AoE, but the
    aoe_iflist module parameter provides a convenient way to limit AoE
    traffic to a specific list of local network interfaces.
    
    This change allows a list to be specified using the comma character as a
    separator.  For example,
    
      modprobe aoe aoe_iflist=eth2,eth3
    
    Before, it was inconvenient to get the quoting right in shell scripts
    when setting aoe_iflist to have more than one network interface.
    
    Signed-off-by: Ed Cashin <ecashin@coraid.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/aoe/aoe.h
+++ drivers/block/aoe/aoe.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AOE_PARTITIONS (16)
 #endif
 
-#define WHITESPACE " \t\v\f\n"
+#define WHITESPACE " \t\v\f\n,"
 
 enum {
 	AOECMD_ATA,
