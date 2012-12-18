4e3d334b1b5945a1015322a2c03655fa5e9c472c
Jeff Layton <jlayton@redhat.com>
2012-12-18 06:35:10 -0500

    cifs: eliminate cifsERROR variable
    It's always set to "1" and there's no way to change it to anything else.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 int cifsFYI = 0;
-int cifsERROR = 1;
 int traceSMB = 0;
 bool enable_oplocks = true;
 unsigned int linuxExtEnabled = 1;
