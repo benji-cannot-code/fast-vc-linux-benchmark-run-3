34fa552d312a18a53d6ccbec5a191da270358fd6
Ed Cashin <ecashin@coraid.com>
2012-12-17 16:04:02 -0800

    aoe: remove call to request handler from I/O completion
    There is no need to call the request handler function in the I/O
    completion routine.  The user impact of not doing it is a more "nice" aoe
    driver that is less susceptible to causing soft lockups.
    
    Signed-off-by: Ed Cashin <ecashin@coraid.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/aoe/aoecmd.c
+++ drivers/block/aoe/aoecmd.c
@@ -1140,8 +1140,6 @@ badrsp:
 	if (buf && --buf->nframesout == 0 && buf->resid == 0)
 		aoe_end_buf(d, buf);
 
-	aoecmd_work(d);
-
 	spin_unlock_irq(&d->lock);
 	aoedev_put(d);
 	dev_kfree_skb(skb);
