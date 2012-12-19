122a00a6929aec9320cfd330b4e18deda0075cc3
Jiri Olsa <jolsa@redhat.com>
2012-12-19 11:33:39 -0300

    perf tests: Add return states enum for tests
    Test can currently return one of 3 states: ok, fail, skip.
    
    The ok and fail states are self-explanatory. The skip state means that
    some of the conditions for running the test was not met, making it
    impossible to even run the test. For instance, if the hardware doesn't
    support the 'precise' level required by a test, it will be skipped.
    
    Signed-off-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Link: http://lkml.kernel.org/n/tip-04vnsdndarctfb1eii5c9hcy@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -2,6 +2,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef TESTS_H
 #define TESTS_H
 
+enum {
+	TEST_OK   =  0,
+	TEST_FAIL = -1,
+	TEST_SKIP = -2,
+};
+
 /* Tests */
 int test__vmlinux_matches_kallsyms(void);
 int test__open_syscall_event(void);
