95f15df1cf29627bb426dc728588557d10584e1f
Soren Brinkmann <soren.brinkmann@xilinx.com>
2012-12-19 10:18:40 -0800

    arm: zynq: timer: Remove redundant #includes
    Some #includes are implicitly included through others, some are
    just not needed.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Tested-by: Josh Cartwright <josh.cartwright@ni.com>

--- arch/arm/mach-zynq/timer.c
+++ arch/arm/mach-zynq/timer.c
@@ -16,20 +16,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
-#include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <linux/types.h>
-#include <linux/clocksource.h>
 #include <linux/clockchips.h>
-#include <linux/io.h>
-#include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/slab.h>
 #include <linux/clk-provider.h>
-
 #include "common.h"
 
 /*
