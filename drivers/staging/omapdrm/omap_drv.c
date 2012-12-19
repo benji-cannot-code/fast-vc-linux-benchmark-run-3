496dcdc28e084f3e3d8bf1c852e8952a31b8e86d
Andy Gross <andy.gross@ti.com>
2012-12-19 14:53:37 -0600

    drm/omap: Add PM capabilities
    Added power management capabilities into the omapdrm and DMM drivers.
    During suspend, we don't need to do anything to maintain the state of
    the LUT.  We have all the necessary information to recreate the mappings
    of the GEM object list maintained by the omapdrm driver.
    
    On resume, the DMM resume handler will first reprogram the LUT to point
    to the dummy page.  The subsequent resume handler in the omapdrm will call
    into the DMM and reprogram each of the buffer objects.  This will ensure
    that all of the necessary objects will be pinned into the DMM properly.
    
    Order of suspend/resume handlers is done by device creation.  We create
    the DMM device before the omapdrm, so the correct order is maintained.
    
    Signed-off-by: Andy Gross <andy.gross@ti.com>
    Signed-off-by: Rob Clark <rob@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/omapdrm/omap_drv.c
+++ drivers/staging/omapdrm/omap_drv.c
@@ -369,6 +369,9 @@ static int dev_load(struct drm_device *dev, unsigned long flags)
 		/* well, limp along without an fbdev.. maybe X11 will work? */
 	}
 
+	/* store off drm_device for use in pm ops */
+	dev_set_drvdata(dev->dev, dev);
+
 	drm_kms_helper_poll_init(dev);
 
 	return 0;
@@ -393,6 +396,8 @@ static int dev_unload(struct drm_device *dev)
 
 	kfree(dev->dev_private);
 	dev->dev_private = NULL;
+
+	dev_set_drvdata(dev->dev, NULL);
 
 	return 0;
 }
@@ -559,10 +564,19 @@ static int pdev_remove(struct platform_device *device)
 	return 0;
 }
 
+#ifdef CONFIG_PM
+static const struct dev_pm_ops omapdrm_pm_ops = {
+	.resume = omap_gem_resume,
+};
+#endif
+
 struct platform_driver pdev = {
 		.driver = {
 			.name = DRIVER_NAME,
 			.owner = THIS_MODULE,
+#ifdef CONFIG_PM
+			.pm = &omapdrm_pm_ops,
+#endif
 		},
 		.probe = pdev_probe,
 		.remove = pdev_remove,
