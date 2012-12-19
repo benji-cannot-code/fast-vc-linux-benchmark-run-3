ae99368b4eebe093d0a26459b4cabd30375dedea
Peter Ujfalusi <peter.ujfalusi@ti.com>
2012-12-19 10:50:09 +0100

    ARM: OMAP2+: Fix compillation error in mach-omap2/timer.c
    prom_add_property() has been renamed to of_add_property()
    This patch fixes the following comilation error:
    
    arch/arm/mach-omap2/timer.c: In function ‘omap_get_timer_dt’:
    arch/arm/mach-omap2/timer.c:178:3: error: implicit declaration of function ‘prom_add_property’ [-Werror=implicit-function-declaration]
    cc1: some warnings being treated as errors
    make[1]: *** [arch/arm/mach-omap2/timer.o] Error 1
    make[1]: *** Waiting for unfinished jobs....
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-omap2/timer.c
+++ arch/arm/mach-omap2/timer.c
@@ -176,7 +176,7 @@ static struct device_node * __init omap_get_timer_dt(struct of_device_id *match,
 			continue;
 		}
 
-		prom_add_property(np, &device_disabled);
+		of_add_property(np, &device_disabled);
 		return np;
 	}
 
