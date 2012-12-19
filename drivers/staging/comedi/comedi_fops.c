ed21fb5ed93abd16474faf074df85ab9eb6e4433
H Hartley Sweeten <hsweeten@visionengravers.com>
2012-12-19 15:43:40 -0700

    staging: comedi: comedi_fops: don't export comedi_get_subdevice_runflags()
    The subdevice runflags are protected with a spin_lock. Only the comedi
    core should be accessing them directly.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_fops.c
+++ drivers/staging/comedi/comedi_fops.c
@@ -364,7 +364,7 @@ static void comedi_set_subdevice_runflags(struct comedi_subdevice *s,
 	spin_unlock_irqrestore(&s->spin_lock, flags);
 }
 
-unsigned comedi_get_subdevice_runflags(struct comedi_subdevice *s)
+static unsigned comedi_get_subdevice_runflags(struct comedi_subdevice *s)
 {
 	unsigned long flags;
 	unsigned runflags;
@@ -374,7 +374,6 @@ unsigned comedi_get_subdevice_runflags(struct comedi_subdevice *s)
 	spin_unlock_irqrestore(&s->spin_lock, flags);
 	return runflags;
 }
-EXPORT_SYMBOL(comedi_get_subdevice_runflags);
 
 bool comedi_is_subdevice_running(struct comedi_subdevice *s)
 {
