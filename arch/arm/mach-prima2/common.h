16e8a9bd6e2311593c2338d93298769342438ef7
Barry Song <Baohua.Song@csr.com>
2012-12-20 19:11:18 +0800

    ARM: PRIMA2: irq: make prima2 irq can work even we enable GIC for Marco
    in Marco, we will use GIC. this patch prepares the handle_irq for prima2
    to avoid the compiling errors since we want only one defconfig and zImage
    for both prima2 and marco that means we will need handle_irq for both.
    
    Signed-off-by: Baohua Song <Baohua.Song@csr.com>

--- arch/arm/mach-prima2/common.h
+++ arch/arm/mach-prima2/common.h
@@ -12,12 +12,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/init.h>
 #include <asm/mach/time.h>
+#include <asm/exception.h>
 
 extern void sirfsoc_prima2_timer_init(void);
 
 extern void __init sirfsoc_of_irq_init(void);
 extern void __init sirfsoc_of_clk_init(void);
 extern void sirfsoc_restart(char, const char *);
+extern asmlinkage void __exception_irq_entry sirfsoc_handle_irq(struct pt_regs *regs);
 
 #ifndef CONFIG_DEBUG_LL
 static inline void sirfsoc_map_lluart(void)  {}
