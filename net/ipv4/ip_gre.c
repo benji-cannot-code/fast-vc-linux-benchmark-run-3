5db3d0e8e4da8aa33b32c2c12e583758cfe3a3cd
Eric Dumazet <edumazet@google.com>
2012-12-20 16:00:27 +0000

    ip_gre: fix possible use after free
    Once skb_realloc_headroom() is called, tiph might point to freed memory.
    
    Cache tiph->ttl value before the reallocation, to avoid unexpected
    behavior.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Isaku Yamahata <yamahata@valinux.co.jp>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_gre.c
+++ net/ipv4/ip_gre.c
@@ -751,6 +751,7 @@ static netdev_tx_t ipgre_tunnel_xmit(struct sk_buff *skb, struct net_device *dev
 	int    gre_hlen;
 	__be32 dst;
 	int    mtu;
+	u8     ttl;
 
 	if (skb->ip_summed == CHECKSUM_PARTIAL &&
 	    skb_checksum_help(skb))
@@ -816,6 +817,7 @@ static netdev_tx_t ipgre_tunnel_xmit(struct sk_buff *skb, struct net_device *dev
 			goto tx_error;
 	}
 
+	ttl = tiph->ttl;
 	tos = tiph->tos;
 	if (tos == 1) {
 		tos = 0;
@@ -908,6 +910,7 @@ static netdev_tx_t ipgre_tunnel_xmit(struct sk_buff *skb, struct net_device *dev
 		dev_kfree_skb(skb);
 		skb = new_skb;
 		old_iph = ip_hdr(skb);
+		/* Warning : tiph value might point to freed memory */
 	}
 
 	skb_reset_transport_header(skb);
@@ -931,8 +934,9 @@ static netdev_tx_t ipgre_tunnel_xmit(struct sk_buff *skb, struct net_device *dev
 	iph->tos		=	ipgre_ecn_encapsulate(tos, old_iph, skb);
 	iph->daddr		=	fl4.daddr;
 	iph->saddr		=	fl4.saddr;
+	iph->ttl		=	ttl;
 
-	if ((iph->ttl = tiph->ttl) == 0) {
+	if (ttl == 0) {
 		if (skb->protocol == htons(ETH_P_IP))
 			iph->ttl = old_iph->ttl;
 #if IS_ENABLED(CONFIG_IPV6)
