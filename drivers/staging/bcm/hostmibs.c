cf7c0a1eb3ce03eac663810054c6ab1998c0326e
Kevin McKinney <klmckinney1@gmail.com>
2012-12-20 00:31:30 -0500

    Staging: bcm: Remove typedef for _S_SERVICEFLOW_ENTRY and call directly.
    This patch removes typedef for _S_SERVICEFLOW_ENTRY, and
    changes the name of the struct to bcm_phs_entry. In
    addition, any calls to struct "_S_SERVICEFLOW_ENTRY" are
    changed to call directly.
    
    Signed-off-by: Kevin McKinney <klmckinney1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/hostmibs.c
+++ drivers/staging/bcm/hostmibs.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 INT ProcessGetHostMibs(struct bcm_mini_adapter *Adapter, struct bcm_host_stats_mibs *pstHostMibs)
 {
-	S_SERVICEFLOW_ENTRY *pstServiceFlowEntry = NULL;
+	struct bcm_phs_entry *pstServiceFlowEntry = NULL;
 	S_PHS_RULE *pstPhsRule = NULL;
 	S_CLASSIFIER_TABLE *pstClassifierTable = NULL;
 	S_CLASSIFIER_ENTRY *pstClassifierRule = NULL;
