086d209398821a1ecc14eed9c02b73b71165e6a1
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2012-12-23 21:10:12 +0100

    usb: gadget: move the global the_dev variable to their users
    the u_ether.c file has a global variable named the_dev which keeps a
    pointer to the network device after it has been created via
    gether_setup_name(). It is only used internally by u_ether. This patches
    moves the variable to its users and passes it via the port.ioport where
    it is saved later anyway.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/cdc2.c
+++ drivers/usb/gadget/cdc2.c
@@ -104,7 +104,7 @@ static struct usb_gadget_strings *dev_strings[] = {
 };
 
 static u8 hostaddr[ETH_ALEN];
-
+static struct eth_dev *the_dev;
 /*-------------------------------------------------------------------------*/
 static struct usb_function *f_acm;
 static struct usb_function_instance *fi_serial;
@@ -123,7 +123,7 @@ static int __init cdc_do_config(struct usb_configuration *c)
 		c->bmAttributes |= USB_CONFIG_ATT_WAKEUP;
 	}
 
-	status = ecm_bind_config(c, hostaddr);
+	status = ecm_bind_config(c, hostaddr, the_dev);
 	if (status < 0)
 		return status;
 
@@ -170,9 +170,9 @@ static int __init cdc_bind(struct usb_composite_dev *cdev)
 	}
 
 	/* set up network link layer */
-	status = gether_setup(cdev->gadget, hostaddr);
-	if (status < 0)
-		return status;
+	the_dev = gether_setup(cdev->gadget, hostaddr);
+	if (IS_ERR(the_dev))
+		return PTR_ERR(the_dev);
 
 	/* set up serial link layer */
 	status = gserial_alloc_line(&tty_line);
@@ -203,7 +203,7 @@ static int __init cdc_bind(struct usb_composite_dev *cdev)
 fail1:
 	gserial_free_line(tty_line);
 fail0:
-	gether_cleanup();
+	gether_cleanup(the_dev);
 	return status;
 }
 
@@ -212,7 +212,7 @@ static int __exit cdc_unbind(struct usb_composite_dev *cdev)
 	usb_put_function(f_acm);
 	usb_put_function_instance(fi_serial);
 	gserial_free_line(tty_line);
-	gether_cleanup();
+	gether_cleanup(the_dev);
 	return 0;
 }
 
