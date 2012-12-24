fe3071e5117e195ac4c165457d0fab4a963ea16f
Sachin Kamat <sachin.kamat@linaro.org>
2012-12-24 14:03:49 +0530

    drm/exynos: Remove explicit freeing using devm_* APIs in exynos_drm_gsc.c
    devm_* APIs are device managed and get freed automatically when the
    device detaches. Thus explicit freeing is not needed. This saves some
    code.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_gsc.c
+++ drivers/gpu/drm/exynos/exynos_drm_gsc.c
@@ -1688,8 +1688,7 @@ static int __devinit gsc_probe(struct platform_device *pdev)
 	ctx->gsc_clk = clk_get(dev, "gscl");
 	if (IS_ERR(ctx->gsc_clk)) {
 		dev_err(dev, "failed to get gsc clock.\n");
-		ret = PTR_ERR(ctx->gsc_clk);
-		goto err_ctx;
+		return PTR_ERR(ctx->gsc_clk);
 	}
 
 	/* resource memory */
@@ -1712,7 +1711,7 @@ static int __devinit gsc_probe(struct platform_device *pdev)
 	if (!res) {
 		dev_err(dev, "failed to request irq resource.\n");
 		ret = -ENOENT;
-		goto err_get_regs;
+		goto err_clk;
 	}
 
 	ctx->irq = res->start;
@@ -1720,7 +1719,7 @@ static int __devinit gsc_probe(struct platform_device *pdev)
 		IRQF_ONESHOT, "drm_gsc", ctx);
 	if (ret < 0) {
 		dev_err(dev, "failed to request irq.\n");
-		goto err_get_regs;
+		goto err_clk;
 	}
 
 	/* context initailization */
@@ -1764,12 +1763,8 @@ err_ippdrv_register:
 	pm_runtime_disable(dev);
 err_get_irq:
 	free_irq(ctx->irq, ctx);
-err_get_regs:
-	devm_iounmap(dev, ctx->regs);
 err_clk:
 	clk_put(ctx->gsc_clk);
-err_ctx:
-	devm_kfree(dev, ctx);
 	return ret;
 }
 
@@ -1787,11 +1782,7 @@ static int __devexit gsc_remove(struct platform_device *pdev)
 	pm_runtime_disable(dev);
 
 	free_irq(ctx->irq, ctx);
-	devm_iounmap(dev, ctx->regs);
-
 	clk_put(ctx->gsc_clk);
-
-	devm_kfree(dev, ctx);
 
 	return 0;
 }
