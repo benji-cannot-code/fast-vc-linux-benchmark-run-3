56c127bbd7691d980bee4131c1a7f363a53fa009
Stephen Warren <swarren@wwwdotorg.org>
2012-12-24 11:14:58 -0700

    namei.h: include errno.h
    This solves:
    
    In file included from fs/ext3/symlink.c:20:0:
    include/linux/namei.h: In function 'retry_estale':
    include/linux/namei.h:114:19: error: 'ESTALE' undeclared (first use in this function)
    
    Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/namei.h
+++ include/linux/namei.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_NAMEI_H
 
 #include <linux/dcache.h>
+#include <linux/errno.h>
 #include <linux/linkage.h>
 #include <linux/path.h>
 
