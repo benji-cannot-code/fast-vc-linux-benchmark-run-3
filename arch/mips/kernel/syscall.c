21b5c4f87cb97a53b4ff04db17015126d48a0d7e
Al Viro <viro@zeniv.linux.org.uk>
2012-12-23 20:18:28 -0500

    mips: no magic arguments for sysm_pipe()
    current_pt_regs() works just fine
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/mips/kernel/syscall.c
+++ arch/mips/kernel/syscall.c
@@ -47,20 +47,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * argument.  Historically that used to be expensive in Linux.  These days
  * the performance advantage is negligible.
  */
-asmlinkage int sysm_pipe(nabi_no_regargs volatile struct pt_regs regs)
+asmlinkage int sysm_pipe(void)
 {
 	int fd[2];
-	int error, res;
-
-	error = do_pipe_flags(fd, 0);
-	if (error) {
-		res = error;
-		goto out;
-	}
-	regs.regs[3] = fd[1];
-	res = fd[0];
-out:
-	return res;
+	int error = do_pipe_flags(fd, 0);
+	if (error)
+		return error;
+	current_pt_regs()->regs[3] = fd[1];
+	return fd[0];
 }
 
 SYSCALL_DEFINE6(mips_mmap, unsigned long, addr, unsigned long, len,
