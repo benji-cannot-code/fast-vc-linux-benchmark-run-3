00aed1aaff4ee4bce434d8afc1f4c9bbf2f0d223
Sachin Kamat <sachin.kamat@linaro.org>
2012-12-24 14:03:42 +0530

    drm/exynos: Use devm_kzalloc in exynos_drm_ipp.c
    devm_kzalloc makes the code simpler by eliminating the need for
    explicit freeing.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_ipp.c
+++ drivers/gpu/drm/exynos/exynos_drm_ipp.c
@@ -1891,7 +1891,7 @@ static int __devinit ipp_probe(struct platform_device *pdev)
 	struct exynos_drm_subdrv *subdrv;
 	int ret;
 
-	ctx = kzalloc(sizeof(*ctx), GFP_KERNEL);
+	ctx = devm_kzalloc(&pdev->dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
 		return -ENOMEM;
 
@@ -1912,8 +1912,7 @@ static int __devinit ipp_probe(struct platform_device *pdev)
 	ctx->event_workq = create_singlethread_workqueue("ipp_event");
 	if (!ctx->event_workq) {
 		dev_err(dev, "failed to create event workqueue\n");
-		ret = -EINVAL;
-		goto err_clear;
+		return -EINVAL;
 	}
 
 	/*
@@ -1954,8 +1953,6 @@ err_cmd_workq:
 	destroy_workqueue(ctx->cmd_workq);
 err_event_workq:
 	destroy_workqueue(ctx->event_workq);
-err_clear:
-	kfree(ctx);
 	return ret;
 }
 
@@ -1980,8 +1977,6 @@ static int __devexit ipp_remove(struct platform_device *pdev)
 	/* destroy command, event work queue */
 	destroy_workqueue(ctx->cmd_workq);
 	destroy_workqueue(ctx->event_workq);
-
-	kfree(ctx);
 
 	return 0;
 }
