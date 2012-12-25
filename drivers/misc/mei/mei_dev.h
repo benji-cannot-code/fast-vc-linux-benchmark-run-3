51579a32823ed807a0a9abeb43b4abd2673fbd50
Tomas Winkler <tomas.winkler@intel.com>
2012-12-25 19:06:02 +0200

    mei: extract device dependent constants into hw-me.h
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/mei_dev.h
+++ drivers/misc/mei/mei_dev.h
@@ -22,7 +22,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/watchdog.h>
 #include <linux/poll.h>
 #include <linux/mei.h>
+
 #include "hw.h"
+#include "hw-me.h"
 
 /*
  * watch dog definition
