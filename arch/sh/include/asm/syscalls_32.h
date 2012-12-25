b744368a7e7faf74a8352afddb0cd716305c0893
Al Viro <viro@zeniv.linux.org.uk>
2012-12-25 16:20:46 -0500

    sh: switch to generic old sigsuspend()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/sh/include/asm/syscalls_32.h
+++ arch/sh/include/asm/syscalls_32.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct pt_regs;
 
-asmlinkage int sys_sigsuspend(old_sigset_t mask);
 asmlinkage int sys_sigaction(int sig, const struct old_sigaction __user *act,
 			     struct old_sigaction __user *oact);
 asmlinkage int sys_sigreturn(unsigned long r4, unsigned long r5,
