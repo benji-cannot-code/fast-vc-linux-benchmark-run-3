6bcf92747bf174527c5a5bed7f704769f58cab07
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2012-12-24 20:03:56 -0800

    ARM: shmobile: add sample amixer settings for ap4evb
    FSI2A-AK4643 needs amixer setting to use it.
    This patch adds quick setting guide
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-ap4evb.c
+++ arch/arm/mach-shmobile/board-ap4evb.c
@@ -144,6 +144,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * SW41	:  ON : SH-Mobile AP4 Audio Mode
  *	: OFF : Bluetooth Audio Mode
+ *
+ * it needs amixer settings for playing
+ *
+ * amixer set "Headphone Enable" on
  */
 
 /*
