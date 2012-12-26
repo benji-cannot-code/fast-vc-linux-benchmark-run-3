f047c20b70774f0f40e22e917d10cb51a14d5131
Al Viro <viro@zeniv.linux.org.uk>
2012-12-26 14:06:24 -0500

    powerpc: trim the crap from syscalls.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/powerpc/include/asm/syscalls.h
+++ arch/powerpc/include/asm/syscalls.h
@@ -6,9 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/linkage.h>
 #include <linux/types.h>
-#include <asm/signal.h>
 
-struct pt_regs;
 struct rtas_args;
 
 asmlinkage unsigned long sys_mmap(unsigned long addr, size_t len,
@@ -17,11 +15,8 @@ asmlinkage unsigned long sys_mmap(unsigned long addr, size_t len,
 asmlinkage unsigned long sys_mmap2(unsigned long addr, size_t len,
 		unsigned long prot, unsigned long flags,
 		unsigned long fd, unsigned long pgoff);
-asmlinkage long sys_pipe(int __user *fildes);
-asmlinkage long sys_pipe2(int __user *fildes, int flags);
 asmlinkage long ppc64_personality(unsigned long personality);
 asmlinkage int ppc_rtas(struct rtas_args __user *uargs);
-asmlinkage time_t sys64_time(time_t __user * tloc);
 
 #endif /* __KERNEL__ */
 #endif /* __ASM_POWERPC_SYSCALLS_H */
