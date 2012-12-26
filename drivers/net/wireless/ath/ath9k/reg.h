abf05424050c21c4d8c8317ae213f830e23eee23
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2012-12-26 12:27:38 +0530

    ath9k_hw: Remove AR9485 1.0 macro
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/reg.h
+++ drivers/net/wireless/ath/ath9k/reg.h
@@ -885,9 +885,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define AR_SREV_9485(_ah) \
 	(((_ah)->hw_version.macVersion == AR_SREV_VERSION_9485))
-#define AR_SREV_9485_10(_ah) \
-	(AR_SREV_9485(_ah) && \
-	 ((_ah)->hw_version.macRev == AR_SREV_REVISION_9485_10))
 #define AR_SREV_9485_11(_ah) \
 	(AR_SREV_9485(_ah) && \
 	 ((_ah)->hw_version.macRev == AR_SREV_REVISION_9485_11))
