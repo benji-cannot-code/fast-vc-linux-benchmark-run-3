aaed1e2fe8ca9cc86b79b12e743253411f83ec57
Chen Gang <gang.chen@asianux.com>
2012-12-26 18:06:39 +0800

    serial: avoid double free after call ioc4_serial_remove_one
      before goto out5,  soft, control, serial are all assigned to idd
      after finish call ioc4_serial_remove_one, all resources are released
      we need return instead of go on, or double free
    
    Signed-off-by: Chen Gang <gang.chen@asianux.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/ioc4_serial.c
+++ drivers/tty/serial/ioc4_serial.c
@@ -2884,6 +2884,7 @@ ioc4_serial_attach_one(struct ioc4_driver_data *idd)
 	/* error exits that give back resources */
 out5:
 	ioc4_serial_remove_one(idd);
+	return ret;
 out4:
 	kfree(soft);
 out3:
