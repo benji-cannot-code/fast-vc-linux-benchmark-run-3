39746d32e79ae1e6002571d22e78fe1faa9daf24
Al Viro <viro@zeniv.linux.org.uk>
2012-12-27 12:11:46 -0500

    mips: switch to generic sys_fork() and sys_clone()
    we still need the wrappers to store callee-saved registers in
    pt_regs, but once that done we can jump to kernel/fork.c variants.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/mips/include/asm/unistd.h
+++ arch/mips/include/asm/unistd.h
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # ifdef CONFIG_MIPS32_O32
 #  define __ARCH_WANT_COMPAT_SYS_TIME
 # endif
+#define __ARCH_WANT_SYS_FORK
+#define __ARCH_WANT_SYS_CLONE
 
 /* whitelists for checksyscalls */
 #define __IGNORE_select
