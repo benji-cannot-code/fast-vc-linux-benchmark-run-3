64e69fc68a027c52614925715ebdd3122cd681de
Prathyush K <prathyush.k@samsung.com>
2012-12-27 06:40:13 -0500

    drm/exynos: fimd: modify condition in fimd resume
    If fimd is runtime suspended (by DPMS OFF), fimd_suspend does not
    call fimd_activate(false) and just returns. Similarily the check in
    fimd_resume should not resume if previously runtime_suspended.
    Instead the existing check does the opposite. So if fimd was not
    runtime suspended, suspend will turn off fimd but resume will not turn
    it on.  This patch fixes this issue by reversing the condition.
    
    Signed-off-by: Prathyush K <prathyush.k@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_fimd.c
+++ drivers/gpu/drm/exynos/exynos_drm_fimd.c
@@ -1047,7 +1047,7 @@ static int fimd_resume(struct device *dev)
 	 * of pm runtime would still be 1 so in this case, fimd driver
 	 * should be on directly not drawing on pm runtime interface.
 	 */
-	if (pm_runtime_suspended(dev)) {
+	if (!pm_runtime_suspended(dev)) {
 		int ret;
 
 		ret = fimd_activate(ctx, true);
