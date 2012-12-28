44fdb023faa2f1b237709c4d8b315b3d96af9ad5
Andrew Lunn <andrew@lunn.ch>
2012-12-28 15:08:48 +0100

    ARM: Kirkwood: Convert NSA310 to DT based regulators.
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-nsa310.c
+++ arch/arm/mach-kirkwood/board-nsa310.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "common.h"
 #include "mpp.h"
 
-#define NSA310_GPIO_USB_POWER_OFF	21
 #define NSA310_GPIO_POWER_OFF		48
 
 static unsigned int nsa310_mpp_config[] __initdata = {
@@ -71,9 +70,6 @@ static void __init nsa310_gpio_init(void)
 				  "Power Off");
 	if (!err)
 		pm_power_off = nsa310_power_off;
-
-	nsa310_gpio_request(NSA310_GPIO_USB_POWER_OFF, GPIOF_OUT_INIT_LOW,
-			    "USB Power Off");
 }
 
 void __init nsa310_init(void)
