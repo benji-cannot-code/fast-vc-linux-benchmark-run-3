f9c1c9e966bb0fa88939e479d72256050cdf5d7e
Kukjin Kim <kgene.kim@samsung.com>
2012-12-28 16:29:10 -0800

    cpufreq: exynos: cleanup exynos-cpufreq header
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- drivers/cpufreq/exynos5250-cpufreq.c
+++ drivers/cpufreq/exynos5250-cpufreq.c
@@ -20,7 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/map.h>
 #include <mach/regs-clock.h>
-#include <mach/cpufreq.h>
+
+#include "exynos-cpufreq.h"
 
 #define CPUFREQ_LEVEL_END	(L15 + 1)
 
