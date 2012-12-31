00a62dbbf8e7bd4fb9e7680c05980cf4055daa00
Peter Ujfalusi <peter.ujfalusi@ti.com>
2012-12-31 11:51:47 +0100

    ASoC: sdp3430: No need to configure pin mux for extmute
    The codec driver takes care of this.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/omap/sdp3430.c
+++ sound/soc/omap/sdp3430.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk.h>
 #include <linux/platform_device.h>
-#include <linux/i2c/twl.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/soc.h>
@@ -33,19 +32,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach-types.h>
 #include <linux/platform_data/gpio-omap.h>
-#include <linux/platform_data/asoc-ti-mcbsp.h>
 
-/* Register descriptions for twl4030 codec part */
-#include <linux/mfd/twl4030-audio.h>
 #include <linux/module.h>
 
 #include "omap-mcbsp.h"
 #include "omap-pcm.h"
-
-/* TWL4030 PMBR1 Register */
-#define TWL4030_INTBR_PMBR1		0x0D
-/* TWL4030 PMBR1 Register GPIO6 mux bit */
-#define TWL4030_GPIO6_PWM0_MUTE(value)	(value << 2)
 
 static struct snd_soc_card snd_soc_sdp3430;
 
@@ -213,7 +204,6 @@ static struct platform_device *sdp3430_snd_device;
 static int __init sdp3430_soc_init(void)
 {
 	int ret;
-	u8 pin_mux;
 
 	if (!machine_is_omap_3430sdp())
 		return -ENODEV;
@@ -226,14 +216,6 @@ static int __init sdp3430_soc_init(void)
 	}
 
 	platform_set_drvdata(sdp3430_snd_device, &snd_soc_sdp3430);
-
-	/* Set TWL4030 GPIO6 as EXTMUTE signal */
-	twl_i2c_read_u8(TWL4030_MODULE_INTBR, &pin_mux,
-						TWL4030_INTBR_PMBR1);
-	pin_mux &= ~TWL4030_GPIO6_PWM0_MUTE(0x03);
-	pin_mux |= TWL4030_GPIO6_PWM0_MUTE(0x02);
-	twl_i2c_write_u8(TWL4030_MODULE_INTBR, pin_mux,
-						TWL4030_INTBR_PMBR1);
 
 	ret = platform_device_add(sdp3430_snd_device);
 	if (ret)
