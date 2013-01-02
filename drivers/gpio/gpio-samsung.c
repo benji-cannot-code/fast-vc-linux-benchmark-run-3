445990d6adf1519788c2ea6e376e20a9ea88f5f2
Kukjin Kim <kgene.kim@samsung.com>
2013-01-02 14:23:38 -0800

    gpio: samsung: remove inclusion <mach/regs-clock.h>
    The <mach/regs-clock.h> is not needed for gpio-samsung.
    
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-samsung.c
+++ drivers/gpio/gpio-samsung.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/hardware.h>
 #include <mach/map.h>
-#include <mach/regs-clock.h>
 #include <mach/regs-gpio.h>
 
 #include <plat/cpu.h>
