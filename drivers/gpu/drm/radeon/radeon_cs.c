759860194da877f393eb35d6c7ff6cf819ec3a4d
Ilija Hadzic <ihadzic@research.bell-labs.com>
2013-01-02 18:27:38 -0500

    drm/radeon: remove unused prototype from radeon_cs
    Signed-off-by: Ilija Hadzic <ihadzic@research.bell-labs.com>
    Reviewed-by: Marek Olšák <maraeo@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_cs.c
+++ drivers/gpu/drm/radeon/radeon_cs.c
@@ -30,9 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "radeon_reg.h"
 #include "radeon.h"
 
-void r100_cs_dump_packet(struct radeon_cs_parser *p,
-			 struct radeon_cs_packet *pkt);
-
 static int radeon_cs_parser_relocs(struct radeon_cs_parser *p)
 {
 	struct drm_device *ddev = p->rdev->ddev;
