b566e86b83e265d110bab385102a842e85b375e0
Kukjin Kim <kgene.kim@samsung.com>
2013-01-02 10:27:36 -0800

    ARM: S3C24XX: make osiris-cpld.h and osiris-map.h local
    This makes the headers local in mach-s3c24xx/.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c24xx/mach-rx1950.c
+++ arch/arm/mach-s3c24xx/mach-rx1950.c
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/clock.h>
 #include <plat/cpu.h>
 #include <plat/devs.h>
-#include <plat/irq.h>
 #include <plat/pm.h>
 #include <plat/regs-iic.h>
 #include <plat/regs-serial.h>
