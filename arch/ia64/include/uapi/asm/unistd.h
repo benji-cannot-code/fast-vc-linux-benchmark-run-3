f24e2bc6f3eb1d6f9e7502240772b3cd23503e80
Luck, Tony <tony.luck@intel.com>
2013-01-03 10:33:48 -0800

    Wire up finit_module syscall
    Linux was granted a new system call to load modules by file descriptor
    in commit 34e1169d996a ("module: add syscall to load module from fd").
    
    Wire it up for ia64 (ready for the Chrome port :-)
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/ia64/include/uapi/asm/unistd.h
+++ arch/ia64/include/uapi/asm/unistd.h
@@ -325,5 +325,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_process_vm_readv		1332
 #define __NR_process_vm_writev		1333
 #define __NR_accept4			1334
+#define __NR_finit_module		1335
 
 #endif /* _UAPI_ASM_IA64_UNISTD_H */
