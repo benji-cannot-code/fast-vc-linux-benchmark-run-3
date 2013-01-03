28217d0534a6527c103a254754963f1d140a5f4a
Heiko Carstens <heiko.carstens@de.ibm.com>
2013-01-03 14:31:53 +0100

    s390/3215: partially revert tty close handling fix
    Partially revert ae289dc1f "s390/3215: fix tty close handling", since this
    leads sometimes to hanging agetty processes and therefore systems that get
    stuck while starting.
    
    This was magically fixed (bisected) by a common code patch from Alan Cox:
    36b3c070 "tty: Move the handling of the tty release logic", however it was
    unrelated.
    
    Since the removed code worked for a decade, nobody knows anymore why it was
    in there in the first place and debugging the observed hang is non-trivial
    (at least for me :) ), let's just re-add the removed code before we see
    other side effects.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- drivers/s390/char/con3215.c
+++ drivers/s390/char/con3215.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RAW3215_NR_CCWS	    3
 #define RAW3215_TIMEOUT	    HZ/10     /* time for delayed output */
 
+#define RAW3215_FIXED	    1	      /* 3215 console device is not be freed */
 #define RAW3215_WORKING	    4	      /* set if a request is being worked on */
 #define RAW3215_THROTTLED   8	      /* set if reading is disabled */
 #define RAW3215_STOPPED	    16	      /* set if writing is disabled */
@@ -631,7 +632,8 @@ static void raw3215_shutdown(struct raw3215_info *raw)
 	DECLARE_WAITQUEUE(wait, current);
 	unsigned long flags;
 
-	if (!(raw->port.flags & ASYNC_INITIALIZED))
+	if (!(raw->port.flags & ASYNC_INITIALIZED) ||
+	    (raw->flags & RAW3215_FIXED))
 		return;
 	/* Wait for outstanding requests, then free irq */
 	spin_lock_irqsave(get_ccwdev_lock(raw->cdev), flags);
@@ -927,6 +929,8 @@ static int __init con3215_init(void)
 	raw->cdev = cdev;
 	dev_set_drvdata(&cdev->dev, raw);
 	cdev->handler = raw3215_irq;
+
+	raw->flags |= RAW3215_FIXED;
 
 	/* Request the console irq */
 	if (raw3215_startup(raw) != 0) {
