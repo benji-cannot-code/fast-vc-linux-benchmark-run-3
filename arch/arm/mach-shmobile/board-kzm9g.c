f27f1d9fd38e85846aa899d3ce6deaf91bae392b
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-01-03 12:54:28 +0100

    ARM: shmobile: kzm9g: Register pinctrl mappings for I2C
    Replace the GPIO-based I2C pinmux configuration by pinctrl mappings.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- arch/arm/mach-shmobile/board-kzm9g.c
+++ arch/arm/mach-shmobile/board-kzm9g.c
@@ -602,6 +602,9 @@ static struct platform_device *kzm_devices[] __initdata = {
 };
 
 static const struct pinctrl_map kzm_pinctrl_map[] = {
+	/* I2C3 */
+	PIN_MAP_MUX_GROUP_DEFAULT("i2c-sh_mobile.3", "pfc-sh73a0",
+				  "i2c3_1", "i2c3"),
 	/* LCD */
 	PIN_MAP_MUX_GROUP_DEFAULT("sh_mobile_lcdc_fb.0", "pfc-sh73a0",
 				  "lcd_data24", "lcd"),
@@ -724,10 +727,6 @@ static void __init kzm_init(void)
 	gpio_request(GPIO_FN_SDHICMD2,		NULL);
 	gpio_request(GPIO_FN_SDHICLK2,		NULL);
 	gpio_request_one(14, GPIOF_OUT_INIT_HIGH, NULL); /* power */
-
-	/* I2C 3 */
-	gpio_request(GPIO_FN_PORT27_I2C_SCL3, NULL);
-	gpio_request(GPIO_FN_PORT28_I2C_SDA3, NULL);
 
 	/* enable FSI2 port A (ak4648) */
 	gpio_request(GPIO_FN_FSIACK,	NULL);
