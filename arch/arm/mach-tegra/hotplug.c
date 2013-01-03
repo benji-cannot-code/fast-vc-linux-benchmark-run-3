bc60d837e67d2258f7c4407b05a71182f3803da3
Joseph Lo <josephl@nvidia.com>
2013-01-03 14:43:00 +0800

    ARM: tegra: moving the clock gating procedure to tegra_cpu_kill
    The tegra_cpu_die was be executed by the CPU itslf. So the clock gating
    procedure won't be executed after the CPU hardware shutdown code. Moving
    the clock gating procedure to tegra_cpu_kill that will be run by another
    CPU after the CPU died.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Acked-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/hotplug.c
+++ arch/arm/mach-tegra/hotplug.c
@@ -20,6 +20,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void (*tegra_hotplug_shutdown)(void);
 
+int tegra_cpu_kill(unsigned cpu)
+{
+	cpu = cpu_logical_map(cpu);
+
+	/* Clock gate the CPU */
+	tegra_wait_cpu_in_reset(cpu);
+	tegra_disable_cpu_clock(cpu);
+
+	return 1;
+}
+
 /*
  * platform-specific code to shutdown a CPU
  *
@@ -27,17 +38,11 @@ static void (*tegra_hotplug_shutdown)(void);
  */
 void __ref tegra_cpu_die(unsigned int cpu)
 {
-	cpu = cpu_logical_map(cpu);
-
 	/* Clean L1 data cache */
 	tegra_disable_clean_inv_dcache();
 
 	/* Shut down the current CPU. */
 	tegra_hotplug_shutdown();
-
-	/* Clock gate the CPU */
-	tegra_wait_cpu_in_reset(cpu);
-	tegra_disable_cpu_clock(cpu);
 
 	/* Should never return here. */
 	BUG();
