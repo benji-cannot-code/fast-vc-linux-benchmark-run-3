9ed76e490fb27afc1e78f3d5acd280e7118e1789
Linus Walleij <linus.walleij@linaro.org>
2013-01-04 14:07:51 +0100

    dma: coh901318: push definitions into driver
    Move everything in the header file that is not used in both
    the main driver and the LLI portions into the driver, including
    register definitions and such.
    
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/dma/coh901318_lli.c
+++ drivers/dma/coh901318_lli.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/memory.h>
 #include <linux/gfp.h>
 #include <linux/dmapool.h>
+#include <linux/dmaengine.h>
 
 #include "coh901318.h"
 #include "coh901318_lli.h"
