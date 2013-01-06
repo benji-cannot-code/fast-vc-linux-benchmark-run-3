49d9faa34dc195058193160b499b98fdbd869267
Linus Walleij <linus.walleij@linaro.org>
2013-01-06 02:25:20 +0100

    ARM: nomadik: delete old board files
    The Device Tree support on Nomadik can do everything the old board
    files could do, so delete the old board files and make the nomadik
    select CONFIG_OF.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-nomadik/cpu-8815.h
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-extern void cpu8815_map_io(void);
-extern void cpu8815_platform_init(void);
-extern void cpu8815_init_irq(void);
-extern void cpu8815_restart(char, const char *);
-extern struct sys_timer cpu8815_timer;
