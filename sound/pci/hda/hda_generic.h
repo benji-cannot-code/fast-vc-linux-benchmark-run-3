663e5fd71ce281189d7189c8103b51e96493a65d
Takashi Iwai <tiwai@suse.de>
2013-01-07 12:42:48 +0100

    ALSA: hda - Add snd_hda_gen_free() and snd_hda_gen_check_power_status()
    Just to remove duplicated codes.
    Also fixed EXPORT_SYMBOL() in hda_generic.c.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_generic.h
+++ sound/pci/hda/hda_generic.h
@@ -219,6 +219,7 @@ int snd_hda_gen_spec_init(struct hda_gen_spec *spec);
 void snd_hda_gen_spec_free(struct hda_gen_spec *spec);
 
 int snd_hda_gen_init(struct hda_codec *codec);
+void snd_hda_gen_free(struct hda_codec *codec);
 
 struct nid_path *snd_hda_get_nid_path(struct hda_codec *codec,
 				      hda_nid_t from_nid, hda_nid_t to_nid);
@@ -257,5 +258,9 @@ void snd_hda_gen_line_automute(struct hda_codec *codec,
 void snd_hda_gen_mic_autoswitch(struct hda_codec *codec,
 				struct hda_jack_tbl *jack);
 void snd_hda_gen_update_outputs(struct hda_codec *codec);
+
+#ifdef CONFIG_PM
+int snd_hda_gen_check_power_status(struct hda_codec *codec, hda_nid_t nid);
+#endif
 
 #endif /* __SOUND_HDA_GENERIC_H */
