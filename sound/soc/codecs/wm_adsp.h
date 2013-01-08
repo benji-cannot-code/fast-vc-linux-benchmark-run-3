3e0fa0c1744f6d8131ffa24d8a6d56af418dbed2
Mark Brown <broonie@opensource.wolfsonmicro.com>
2013-01-08 16:09:31 +0000

    ASoC: wm_adsp: Implement support for algorithm-specific coefficient blocks
    WMDR coefficient files can specify coefficients in terms of algorithm
    specific data regions. Record the start addresses of these regions while
    parsing the algorithms and then use them to handle coefficients with
    these formats.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm_adsp.h
+++ sound/soc/codecs/wm_adsp.h
@@ -26,6 +26,13 @@ struct wm_adsp_region {
 	unsigned int base;
 };
 
+struct wm_adsp_alg_region {
+	struct list_head list;
+	unsigned int alg;
+	int type;
+	unsigned int base;
+};
+
 struct wm_adsp {
 	const char *part;
 	int num;
@@ -34,6 +41,8 @@ struct wm_adsp {
 	struct regmap *regmap;
 
 	int base;
+
+	struct list_head alg_regions;
 
 	const struct wm_adsp_region *mem;
 	int num_mems;
