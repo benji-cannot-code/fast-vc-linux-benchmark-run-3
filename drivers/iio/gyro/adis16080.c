20184bf971333c3dd3f93ac85243d99088c19837
Lars-Peter Clausen <lars@metafoo.de>
2013-01-09 14:01:00 +0000

    staging:iio:adis16080: Move out of staging
    The driver is rather simple and in a good shape. It follows the IIO ABI and the
    standard codechecker tools do not report any issues, so move it out of staging.
    
    While moving it also remove one outdated 'fixme' comment.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/gyro/adis16080.c
+++ drivers/iio/gyro/adis16080.c
@@ -229,7 +229,6 @@ error_ret:
 	return ret;
 }
 
-/* fixme, confirm ordering in this function */
 static int adis16080_remove(struct spi_device *spi)
 {
 	iio_device_unregister(spi_get_drvdata(spi));
