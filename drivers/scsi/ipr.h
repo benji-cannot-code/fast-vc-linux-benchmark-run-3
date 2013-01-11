145b36ee631eba12e0a006cedabc827bfa86409b
wenxiong@linux.vnet.ibm.com <wenxiong@linux.vnet.ibm.com>
2013-01-11 17:43:53 -0600

    [SCSI] ipr: Driver version 2.6.0
    Signed-off-by: Wen Xiong <wenxiong@linux.vnet.ibm.com>
    Acked-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/ipr.h
+++ drivers/scsi/ipr.h
@@ -40,8 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Literals
  */
-#define IPR_DRIVER_VERSION "2.5.4"
-#define IPR_DRIVER_DATE "(July 11, 2012)"
+#define IPR_DRIVER_VERSION "2.6.0"
+#define IPR_DRIVER_DATE "(November 16, 2012)"
 
 /*
  * IPR_MAX_CMD_PER_LUN: This defines the maximum number of outstanding
