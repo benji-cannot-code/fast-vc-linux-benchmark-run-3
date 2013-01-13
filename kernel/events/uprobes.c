d0831376d2d71a1ecce5dd2c885041a08d6abfbd
Josh Stone <jistone@redhat.com>
2013-01-13 19:03:34 +0100

    uprobes: Add exports for module use
    The original pull message for uprobes (commit 654443e2) noted:
    
      This tree includes uprobes support in 'perf probe' - but SystemTap
      (and other tools) can take advantage of user probe points as well.
    
    In order to actually be usable in module-based tools like SystemTap, the
    interface needs to be exported.  This patch first adds the obvious
    exports for uprobe_register and uprobe_unregister.  Then it also adds
    one for task_user_regset_view, which is necessary to get the correct
    state of userspace registers.
    
    Signed-off-by: Josh Stone <jistone@redhat.com>
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pagemap.h>	/* read_mapping_page */
 #include <linux/slab.h>
 #include <linux/sched.h>
+#include <linux/export.h>
 #include <linux/rmap.h>		/* anon_vma_prepare */
 #include <linux/mmu_notifier.h>	/* set_pte_at_notify */
 #include <linux/swap.h>		/* try_to_free_swap */
@@ -852,6 +853,7 @@ int uprobe_register(struct inode *inode, loff_t offset, struct uprobe_consumer *
 		goto retry;
 	return ret;
 }
+EXPORT_SYMBOL_GPL(uprobe_register);
 
 /*
  * uprobe_unregister - unregister a already registered probe.
@@ -872,6 +874,7 @@ void uprobe_unregister(struct inode *inode, loff_t offset, struct uprobe_consume
 	up_write(&uprobe->register_rwsem);
 	put_uprobe(uprobe);
 }
+EXPORT_SYMBOL_GPL(uprobe_unregister);
 
 static int unapply_uprobe(struct uprobe *uprobe, struct mm_struct *mm)
 {
