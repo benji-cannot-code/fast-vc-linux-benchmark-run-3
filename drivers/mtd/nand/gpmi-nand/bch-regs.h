2d7b90866fcc645c10e0b3887873d25bc2c78ac4
Huang Shijie <b32955@freescale.com>
2013-01-14 15:19:29 +0800

    mtd: gpmi: dump the BCH registers
    Dump the BCH registers in gpmi_dump_info().
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- drivers/mtd/nand/gpmi-nand/bch-regs.h
+++ drivers/mtd/nand/gpmi-nand/bch-regs.h
@@ -104,4 +104,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		? (((v) >> 2) & MX6Q_BM_BCH_FLASH0LAYOUT1_DATAN_SIZE)	\
 		: ((v) & BM_BCH_FLASH0LAYOUT1_DATAN_SIZE)		\
 	)
+
+#define HW_BCH_VERSION				0x00000160
 #endif
