0f1a2a31263ab181de89b856789ba24132be252f
Lee Jones <lee.jones@linaro.org>
2013-01-15 16:37:56 +0000

    ARM: ux500: remove irq_base property from platform_data
    AB8500 GPIO no longer handles its GPIO IRQs. Instead, the AB8500
    core driver has taken back the responsibility. Prior to this
    happening, the AB8500 GPIO driver provided a set of virtual IRQs
    which were used as a pass-through. These virtual IRQs had a base
    of MOP500_AB8500_VIR_GPIO_IRQ_BASE, which was passed though pdata.
    We don't need to do this anymore, so we're pulling out the
    property from the structure.
    
    Cc: arm@kernel.org
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/mfd/abx500/ab8500-gpio.h
+++ include/linux/mfd/abx500/ab8500-gpio.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct abx500_gpio_platform_data {
 	int gpio_base;
-	u32 irq_base;
 };
 
 enum abx500_gpio_pull_updown {
