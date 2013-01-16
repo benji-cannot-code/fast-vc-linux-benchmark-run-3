469eb6f90025abe103ea9108f2ccf0fd262cb677
David Henningsson <david.henningsson@canonical.com>
2013-01-16 15:58:45 +0100

    ALSA: hda - Make sure fill_all_dac_nids is called for digital only codecs
    Otherwise no PCM will be built for codecs without analog I/O.
    
    Signed-off-by: David Henningsson <david.henningsson@canonical.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_generic.c
+++ sound/pci/hda/hda_generic.c
@@ -1517,8 +1517,6 @@ static int parse_output_paths(struct hda_codec *codec)
 	bool best_wired = true, best_mio = true;
 	bool hp_spk_swapped = false;
 
-	fill_all_dac_nids(codec);
-
 	best_cfg = kmalloc(sizeof(*best_cfg), GFP_KERNEL);
 	if (!best_cfg)
 		return -ENOMEM;
@@ -3428,6 +3426,8 @@ int snd_hda_gen_parse_auto_config(struct hda_codec *codec,
 		spec->autocfg = *cfg;
 		cfg = &spec->autocfg;
 	}
+
+	fill_all_dac_nids(codec);
 
 	if (!cfg->line_outs) {
 		if (cfg->dig_outs || cfg->dig_in_pin) {
