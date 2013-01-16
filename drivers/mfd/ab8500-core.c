fa285473af14eed7323d7e6c72f1b8ea6ad26729
Lee Jones <lee.jones@linaro.org>
2013-01-16 15:09:36 +0000

    mfd: ab8500: allow AB9540 based devices to use ABX500 pinctrl
    The old AB8500 GPIO driver has been un-BROKEN and converted into a
    multi-platform pinctrl driver. If any AB9540 based devices wish to
    request any GPIO pins that it offers, they can after this patch.
    
    Cc: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/mfd/ab8500-core.c
+++ drivers/mfd/ab8500-core.c
@@ -1115,7 +1115,8 @@ static struct mfd_cell ab8500_devs[] = {
 
 static struct mfd_cell ab9540_devs[] = {
 	{
-		.name = "ab8500-gpio",
+		.name = "pinctrl-ab9540",
+		.of_compatible = "stericsson,ab9540-gpio",
 	},
 	{
 		.name = "ab9540-usb",
