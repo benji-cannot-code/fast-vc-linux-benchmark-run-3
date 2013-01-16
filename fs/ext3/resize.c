1cd7a85bb79bd0457cbb1e15cc65a141255d8a0a
Wang Shilong <wangsl-fnst@cn.fujitsu.com>
2013-01-15 21:20:01 -0800

    Ext3: return ENOMEM rather than EIO if sb_getblk fails
    It will be better to use ENOMEM rather than EIO, because the only
    reason that sb_getblk fails is that allocation fails.
    
    Signed-off-by: Wang Shilong <wangsl-fnst@cn.fujitsu.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/ext3/resize.c
+++ fs/ext3/resize.c
@@ -118,7 +118,7 @@ static struct buffer_head *bclean(handle_t *handle, struct super_block *sb,
 
 	bh = sb_getblk(sb, blk);
 	if (unlikely(!bh))
-		return ERR_PTR(-EIO);
+		return ERR_PTR(-ENOMEM);
 	if ((err = ext3_journal_get_write_access(handle, bh))) {
 		brelse(bh);
 		bh = ERR_PTR(err);
@@ -236,7 +236,7 @@ static int setup_new_group_blocks(struct super_block *sb,
 
 		gdb = sb_getblk(sb, block);
 		if (unlikely(!gdb)) {
-			err = -EIO;
+			err = -ENOMEM;
 			goto exit_bh;
 		}
 		if ((err = ext3_journal_get_write_access(handle, gdb))) {
@@ -724,7 +724,7 @@ static void update_backups(struct super_block *sb,
 
 		bh = sb_getblk(sb, group * bpg + blk_off);
 		if (unlikely(!bh)) {
-			err = -EIO;
+			err = -ENOMEM;
 			break;
 		}
 		ext3_debug("update metadata backup %#04lx\n",
