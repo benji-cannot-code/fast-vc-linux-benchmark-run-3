90bc93a1967b18b8d156b72621ff14d80c6181be
Ben Widawsky <ben@bwidawsk.net>
2013-01-17 12:45:17 -0800

    drm/i915: Remove use of gtt_mappable_entries
    Mappable_end, ie. size is almost always what you want as opposed to the
    number of entries. Since we already have that information, we can scrap
    the number of entries and only calculate it when needed.
    
    If gtt_start is !0, this will have slightly different behavior. This
    difference can only occur in DRI1, and exists when we try to kick out
    the firmware fb. The new code seems like a bugfix to me.
    
    The other case where we've changed the behavior is during init we check
    the mappable region against our current known upper and lower limits
    (64MB, and 512MB). This now matches the comment, and makes things more
    convenient after removing gtt_mappable_entries.
    
    Also worth noting is the setting of mappable_end is taken out of setup
    because we do it earlier now in the DRI2 case and therefore need to add
    that tiny hunk to support the DRI1 IOCTL.
    
    v2: Move up mappable end to before legacy AGP init
    
    v3: Add the dev_priv inclusion here from previous rebase error in patch
    5
    
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@gmail.com> (v2)
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    [danvet: squash in fix for a printk format flag mismatch warning.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -150,6 +150,7 @@ int
 i915_gem_init_ioctl(struct drm_device *dev, void *data,
 		    struct drm_file *file)
 {
+	struct drm_i915_private *dev_priv = dev->dev_private;
 	struct drm_i915_gem_init *args = data;
 
 	if (drm_core_check_feature(dev, DRIVER_MODESET))
@@ -166,6 +167,7 @@ i915_gem_init_ioctl(struct drm_device *dev, void *data,
 	mutex_lock(&dev->struct_mutex);
 	i915_gem_setup_global_gtt(dev, args->gtt_start, args->gtt_end,
 				  args->gtt_end);
+	dev_priv->gtt.mappable_end = args->gtt_end;
 	mutex_unlock(&dev->struct_mutex);
 
 	return 0;
