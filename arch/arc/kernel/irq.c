a32df923c3e9b19e7c3802c63ef3a43447421709
Vineet Gupta <vgupta@synopsys.com>
2013-01-18 15:12:16 +0530

    ARC: irqflags - Interrupt enabling/disabling at in-core intc
    ARC700 has an in-core intc which provides 2 priorities (a.k.a.) "levels"
    of interrupts (per IRQ) hencforth referred to as L1/L2 interrupts.
    
    CPU flags register STATUS32 has Interrupt Enable bits per level (E1/E2)
    to globally enable (or disable) all IRQs at a level. Hence the
    implementation of arch_local_irq_{save,restore,enable,disable}( )
    
    The STATUS32 reg can be r/w only using the AUX Interface of ARC, hence
    the use of LR/SR instructions. Further, E1/E2 bits in there can only be
    updated using the FLAG insn.
    
    The intc supports 32 interrupts - and per IRQ enabling is controlled by
    a bit in the AUX_IENABLE register, hence the implmentation of
    arch_{,un}mask_irq( ) routines.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>

---
+++ arch/arc/kernel/irq.c
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (C) 2011-12 Synopsys, Inc. (www.synopsys.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#include <linux/interrupt.h>
+#include <linux/module.h>
+#include <asm/irqflags.h>
+#include <asm/arcregs.h>
+
+void arch_local_irq_enable(void)
+{
+	unsigned long flags;
+
+	/*
+	 * ARC IDE Drivers tries to re-enable interrupts from hard-isr
+	 * context which is simply wrong
+	 */
+	if (in_irq()) {
+		WARN_ONCE(1, "IRQ enabled from hard-isr");
+		return;
+	}
+
+	flags = arch_local_save_flags();
+	flags |= (STATUS_E1_MASK | STATUS_E2_MASK);
+	arch_local_irq_restore(flags);
+}
+EXPORT_SYMBOL(arch_local_irq_enable);
