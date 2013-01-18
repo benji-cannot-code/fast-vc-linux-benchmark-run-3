329e4091ce96cf02c7f893ef867a0679122dd1ec
Ben Widawsky <ben@bwidawsk.net>
2013-01-18 12:30:31 -0800

    drm/i915: Cut out the infamous ILK w/a from AGP layer
    And, move it to where the rest of the logic is.
    
    There is some slight functionality changes. There was extra paranoid
    checks in AGP code making sure we never do idle maps on gen2 parts. That
    was not duplicated as the simple PCI id check should do the right thing.
    
    v2: use IS_GEN5 && IS_MOBILE check instead. For now, this is the same as
    IS_IRONLAKE_M but is more future proof. The workaround docs hint that
    more than one platform may be effected, but we've never seen such a
    platform in the wild. (Rodrigo, Daniel)
    
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@gmail.com> (v1)
    Cc: Dave Airlie <airlied@redhat.com>
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/char/agp/intel-gtt.c
+++ drivers/char/agp/intel-gtt.c
@@ -841,9 +841,6 @@ static int intel_fake_agp_insert_entries(struct agp_memory *mem,
 {
 	int ret = -EINVAL;
 
-	if (intel_private.base.do_idle_maps)
-		return -ENODEV;
-
 	if (intel_private.clear_fake_agp) {
 		int start = intel_private.base.stolen_size / PAGE_SIZE;
 		int end = intel_private.base.gtt_mappable_entries;
@@ -907,9 +904,6 @@ static int intel_fake_agp_remove_entries(struct agp_memory *mem,
 {
 	if (mem->page_count == 0)
 		return 0;
-
-	if (intel_private.base.do_idle_maps)
-		return -ENODEV;
 
 	intel_gtt_clear_range(pg_start, mem->page_count);
 
@@ -1070,24 +1064,6 @@ static void i965_write_entry(dma_addr_t addr,
 	writel(addr | pte_flags, intel_private.gtt + entry);
 }
 
-/* Certain Gen5 chipsets require require idling the GPU before
- * unmapping anything from the GTT when VT-d is enabled.
- */
-static inline int needs_idle_maps(void)
-{
-#ifdef CONFIG_INTEL_IOMMU
-	const unsigned short gpu_devid = intel_private.pcidev->device;
-
-	/* Query intel_iommu to see if we need the workaround. Presumably that
-	 * was loaded first.
-	 */
-	if ((gpu_devid == PCI_DEVICE_ID_INTEL_IRONLAKE_M_HB ||
-	     gpu_devid == PCI_DEVICE_ID_INTEL_IRONLAKE_M_IG) &&
-	     intel_iommu_gfx_mapped)
-		return 1;
-#endif
-	return 0;
-}
 
 static int i9xx_setup(void)
 {
@@ -1115,9 +1091,6 @@ static int i9xx_setup(void)
 		intel_private.gtt_bus_addr = reg_addr + KB(512);
 		break;
 	}
-
-	if (needs_idle_maps())
-		intel_private.base.do_idle_maps = 1;
 
 	intel_i9xx_setup_flush();
 
