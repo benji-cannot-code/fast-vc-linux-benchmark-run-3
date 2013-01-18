89004c4c73483b29c3c15a16c39eb42652857f95
Felipe Balbi <balbi@ti.com>
2013-01-18 13:18:44 +0200

    usb: gadget: fix two sparse warnings
    drivers/usb/gadget/u_serial.c:1291:5: sparse: symbol \
    	'userial_init' was not declared. Should it be static?
    drivers/usb/gadget/zero.c:66:25: sparse: symbol \
    	'gzero_options' was not declared. Should it be static?
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/u_serial.c
+++ drivers/usb/gadget/u_serial.c
@@ -1289,7 +1289,7 @@ void gserial_disconnect(struct gserial *gser)
 }
 EXPORT_SYMBOL_GPL(gserial_disconnect);
 
-int userial_init(void)
+static int userial_init(void)
 {
 	unsigned			i;
 	int				status;
