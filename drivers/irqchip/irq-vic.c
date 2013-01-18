73b0d2cacdccc260ddd11d8aa3bde068ad7569fd
Catalin Marinas <catalin.marinas@arm.com>
2013-01-18 15:20:06 +0000

    arm: Move the set_handle_irq and handle_arch_irq declarations to asm/irq.h
    This patch prepares the removal of <asm/mach/irq.h> include in the
    GIC and VIC irqchip drivers.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Tested-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Rob Herring <rob.herring@calxeda.com>

--- drivers/irqchip/irq-vic.c
+++ drivers/irqchip/irq-vic.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irqchip/arm-vic.h>
 
 #include <asm/exception.h>
-#include <asm/mach/irq.h>
+#include <asm/irq.h>
 
 #include "irqchip.h"
 
