36c093a65a98c608fea69355d4d5b6c27428e0e2
Alexander Graf <agraf@suse.de>
2013-01-18 15:13:19 +0100

    KVM: PPC: E500: Make clear_tlb_refs and clear_tlb1_bitmap static
    Host shadow TLB flushing is logic that the guest TLB code should have
    no insight about. Declare the internal clear_tlb_refs and clear_tlb1_bitmap
    functions static to the host TLB handling file.
    
    Instead of these, we can use the already exported kvmppc_core_flush_tlb().
    This gives us a common API across the board to say "please flush any
    pending host shadow translation".
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/kvm/e500_mmu_host.h
+++ arch/powerpc/kvm/e500_mmu_host.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void inval_gtlbe_on_host(struct kvmppc_vcpu_e500 *vcpu_e500, int tlbsel,
 			 int esel);
 
-void clear_tlb1_bitmap(struct kvmppc_vcpu_e500 *vcpu_e500);
-void clear_tlb_refs(struct kvmppc_vcpu_e500 *vcpu_e500);
 int e500_mmu_host_init(struct kvmppc_vcpu_e500 *vcpu_e500);
 void e500_mmu_host_uninit(struct kvmppc_vcpu_e500 *vcpu_e500);
 
