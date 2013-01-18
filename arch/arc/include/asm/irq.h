99004f8d3611087f99ec54ced054ff67cef3d617
Vineet Gupta <vgupta@synopsys.com>
2013-01-18 15:12:20 +0530

    ARC: [plat-arcfpga] Static platform device for CONFIG_SERIAL_ARC
    N.B. This is old style of hardcoding platform device specific info
    in code and it's instantiation thererof using platform_add_devices().
    Subsequent patches replace this with DeviceTree based runtime probe.
    
    This patch has been retained just as an example of "don't-do-this" for
    newer kernel ports.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
    Cc: Arnd Bergmann <arnd@arndb.de>

--- arch/arc/include/asm/irq.h
+++ arch/arc/include/asm/irq.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TIMER0_IRQ      3
 #define TIMER1_IRQ      4
 
+#include <plat/irq.h>		/* Board Specific IRQ assignments */
 #include <asm-generic/irq.h>
 
 extern void __init arc_init_IRQ(void);
