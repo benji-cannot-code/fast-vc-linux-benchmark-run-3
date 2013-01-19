7b284dabd03d40f49fec40c41452108467ecc399
Stefan Hajnoczi <stefanha@gmail.com>
2013-01-19 09:59:00 +0100

    staging: line6: drop CONFIG_LINE6_USB_DUMP_PCM
    The CONFIG_LINE6_USB_DUMP_PCM config option prints a hexdump of PCM
    audio data as URBs are sent and received.  The usbmon feature should be
    used instead of manually dumping PCM URBs.  There are a few advantages
    to using usbmon:
    
     * Can be turned on/off at runtime
     * Provides full USB-level traffic
     * tcpdump and wireshark support for powerful analysis
     * No driver-specific code is required
    
    This is the last user of line6_write_hexdump() so we drop it too.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/line6/driver.h
+++ drivers/staging/line6/driver.h
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRIVER_NAME "line6usb"
 
-#if defined(CONFIG_LINE6_USB_DUMP_PCM)
-#define CONFIG_LINE6_USB_DUMP_ANY
-#endif
-
 #define LINE6_TIMEOUT 1
 #define LINE6_BUFSIZE_LISTEN 32
 #define LINE6_MESSAGE_MAXLEN 256
@@ -219,10 +215,5 @@ extern int line6_transmit_parameter(struct usb_line6 *line6, int param,
 extern int line6_version_request_async(struct usb_line6 *line6);
 extern int line6_write_data(struct usb_line6 *line6, int address, void *data,
 			    size_t datalen);
-
-#ifdef CONFIG_LINE6_USB_DUMP_ANY
-extern void line6_write_hexdump(struct usb_line6 *line6, char dir,
-				const unsigned char *buffer, int size);
-#endif
 
 #endif
