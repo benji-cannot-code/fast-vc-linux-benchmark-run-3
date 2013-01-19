3203d7e50fcc5841247f6091dffd975168ce45cc
Jon Mason <jon.mason@intel.com>
2013-01-19 02:02:35 -0700

    ntb_netdev: Update Version
    Update NTB netdev version to 0.7
    
    Signed-off-by: Jon Mason <jon.mason@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ntb_netdev.c
+++ drivers/net/ntb_netdev.c
@@ -52,7 +52,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/ntb.h>
 
-#define NTB_NETDEV_VER	"0.6"
+#define NTB_NETDEV_VER	"0.7"
 
 MODULE_DESCRIPTION(KBUILD_MODNAME);
 MODULE_VERSION(NTB_NETDEV_VER);
