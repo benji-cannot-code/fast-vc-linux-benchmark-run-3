fc32a11200eead20cf576bd1bc9bb8140371e1fb
Peter Senna Tschudin <peter.senna@gmail.com>
2013-01-19 19:41:30 -0300

    [media] [V2,23/24] usb/hdpvr/hdpvr-i2c.c: use IS_ENABLED() macro
    replace:
     #if defined(CONFIG_I2C) || \
         defined(CONFIG_I2C_MODULE)
    with:
     #if IS_ENABLED(CONFIG_I2C)
    This change was made for: CONFIG_I2C
    
    Reported-by: Mauro Carvalho Chehab <mchehab@redhat.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/hdpvr/hdpvr-i2c.c
+++ drivers/media/usb/hdpvr/hdpvr-i2c.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#if defined(CONFIG_I2C) || defined(CONFIG_I2C_MODULE)
+#if IS_ENABLED(CONFIG_I2C)
 
 #include <linux/i2c.h>
 #include <linux/slab.h>
