c94cc8ce7da7fbb335d80cc3a27569eb81ffc663
Peter Senna Tschudin <peter.senna@gmail.com>
2013-01-19 19:41:09 -0300

    [media] [V2,02/24] pci/saa7134/saa7134.h: use IS_ENABLED() macro
    replace:
     #if defined(CONFIG_VIDEO_SAA7134_DVB) || \
         defined(CONFIG_VIDEO_SAA7134_DVB_MODULE)
    with:
     #if IS_ENABLED(CONFIG_VIDEO_SAA7134_DVB)
    This change was made for: CONFIG_VIDEO_SAA7134_DVB
    
    Reported-by: Mauro Carvalho Chehab <mchehab@redhat.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/pci/saa7134/saa7134.h
+++ drivers/media/pci/saa7134/saa7134.h
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/videobuf-dma-sg.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
-#if defined(CONFIG_VIDEO_SAA7134_DVB) || defined(CONFIG_VIDEO_SAA7134_DVB_MODULE)
+#if IS_ENABLED(CONFIG_VIDEO_SAA7134_DVB)
 #include <media/videobuf-dvb.h>
 #endif
 
@@ -645,7 +645,7 @@ struct saa7134_dev {
 	struct work_struct         empress_workqueue;
 	int                        empress_started;
 
-#if defined(CONFIG_VIDEO_SAA7134_DVB) || defined(CONFIG_VIDEO_SAA7134_DVB_MODULE)
+#if IS_ENABLED(CONFIG_VIDEO_SAA7134_DVB)
 	/* SAA7134_MPEG_DVB only */
 	struct videobuf_dvb_frontends frontends;
 	int (*original_demod_sleep)(struct dvb_frontend *fe);
