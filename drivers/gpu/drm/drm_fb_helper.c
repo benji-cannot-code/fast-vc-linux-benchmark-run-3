f4852397942e3893f116d79286fd608e12f75bfc
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-01-20 18:18:07 +0100

    drm/fb-helper: unexport drm_fb_helper_panic
    It doesn't even show up in any header files and only used iternally.
    Originally it was (ab)used to restore the fbcon on lastclose, but that
    died with
    
    commit e8e7a2b8ccfdae0d4cb6bd25824bbedcd42da316
    Author: Dave Airlie <airlied@redhat.com>
    Date:   Thu Apr 21 22:18:32 2011 +0100
    
        drm/i915: restore only the mode of this driver on lastclose (v2)
    
    Reviewed-by: Rob Clark <robdclark@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/drm_fb_helper.c
+++ drivers/gpu/drm/drm_fb_helper.c
@@ -288,7 +288,7 @@ static bool drm_fb_helper_force_kernel_mode(void)
 	return error;
 }
 
-int drm_fb_helper_panic(struct notifier_block *n, unsigned long ununsed,
+static int drm_fb_helper_panic(struct notifier_block *n, unsigned long ununsed,
 			void *panic_str)
 {
 	/*
@@ -301,7 +301,6 @@ int drm_fb_helper_panic(struct notifier_block *n, unsigned long ununsed,
 	pr_err("panic occurred, switching back to text console\n");
 	return drm_fb_helper_force_kernel_mode();
 }
-EXPORT_SYMBOL(drm_fb_helper_panic);
 
 static struct notifier_block paniced = {
 	.notifier_call = drm_fb_helper_panic,
