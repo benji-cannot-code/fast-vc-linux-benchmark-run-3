79f3e2e004d94076a46febc5f4564f238dc0d43b
Christian König <christian.koenig@amd.com>
2013-01-21 13:58:46 +0100

    drm/radeon: Deprecate UMS support v2
    KMS support is out and stable for a couple of years now and
    the userspace code has deprecated or abandoned the old UMS interface.
    
    So make the KMS interface the default and deprecate the UMS interface
    in the kernel as well.
    
    v2: rebased on alex/drm-next-3.9-wip
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/r600_cs.c
+++ drivers/gpu/drm/radeon/r600_cs.c
@@ -2314,17 +2314,7 @@ int r600_cs_parse(struct radeon_cs_parser *p)
 	return 0;
 }
 
-static int r600_cs_parser_relocs_legacy(struct radeon_cs_parser *p)
-{
-	if (p->chunk_relocs_idx == -1) {
-		return 0;
-	}
-	p->relocs = kzalloc(sizeof(struct radeon_cs_reloc), GFP_KERNEL);
-	if (p->relocs == NULL) {
-		return -ENOMEM;
-	}
-	return 0;
-}
+#ifdef CONFIG_DRM_RADEON_UMS
 
 /**
  * cs_parser_fini() - clean parser states
@@ -2348,6 +2338,18 @@ static void r600_cs_parser_fini(struct radeon_cs_parser *parser, int error)
 	}
 	kfree(parser->chunks);
 	kfree(parser->chunks_array);
+}
+
+static int r600_cs_parser_relocs_legacy(struct radeon_cs_parser *p)
+{
+	if (p->chunk_relocs_idx == -1) {
+		return 0;
+	}
+	p->relocs = kzalloc(sizeof(struct radeon_cs_reloc), GFP_KERNEL);
+	if (p->relocs == NULL) {
+		return -ENOMEM;
+	}
+	return 0;
 }
 
 int r600_cs_legacy(struct drm_device *dev, void *data, struct drm_file *filp,
@@ -2410,6 +2412,8 @@ void r600_cs_legacy_init(void)
 {
 	r600_nomm = 1;
 }
+
+#endif
 
 /*
  *  DMA
