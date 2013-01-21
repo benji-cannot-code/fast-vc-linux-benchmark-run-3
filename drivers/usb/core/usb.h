04269b099e4a80dbaa6dad01dd3205ee410ae85a
Lan Tianyu <tianyu.lan@intel.com>
2013-01-21 22:18:00 +0800

    USB: Set usb port's DeviceRemovable according acpi information
    ACPI provide "_PLD" and "_UPC" aml methods to describe usb port
    visibility and connectability. This patch is to add usb_hub_adjust_DeviceRemovable()
    to adjust usb hub port's DeviceRemovable according ACPI information and invoke it in
    the rh_call_control(). When hub descriptor request is issued at first time,
    usb port device isn't created and usb port is not bound with acpi. So first
    hub descriptor request is not changed based on ACPI information. After usb
    port devices being created, call usb_hub_adjust_DeviceRemovable in the hub_configure()
    and then set hub port's DeviceRemovable according ACPI information and this also works
    for non-root hub.
    
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Lan Tianyu <tianyu.lan@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/usb.h
+++ drivers/usb/core/usb.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm.h>
 #include <linux/acpi.h>
 
+struct usb_hub_descriptor;
 struct dev_state;
 
 /* Functions local to drivers/usb/core/ */
@@ -183,6 +184,8 @@ extern enum usb_port_connect_type
 	usb_get_hub_port_connect_type(struct usb_device *hdev, int port1);
 extern void usb_set_hub_port_connect_type(struct usb_device *hdev, int port1,
 	enum usb_port_connect_type type);
+extern void usb_hub_adjust_deviceremovable(struct usb_device *hdev,
+		struct usb_hub_descriptor *desc);
 
 #ifdef CONFIG_ACPI
 extern int usb_acpi_register(void);
