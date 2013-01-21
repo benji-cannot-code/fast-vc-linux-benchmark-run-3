bd836fe6c7990d3af0c5a0f4fe0dc8f4c05d2590
Takashi Iwai <tiwai@suse.de>
2013-01-21 16:10:56 +0100

    ALSA: hda - Remove superfluous header inclusions
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_sigmatel.c
+++ sound/pci/hda/patch_sigmatel.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmi.h>
 #include <linux/module.h>
 #include <sound/core.h>
-#include <sound/asoundef.h>
 #include <sound/jack.h>
 #include <sound/tlv.h>
 #include "hda_codec.h"
