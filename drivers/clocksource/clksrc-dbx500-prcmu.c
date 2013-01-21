ee23d8db3d2563437202e175baf603ed21356f30
Fabio Baltieri <fabio.baltieri@linaro.org>
2013-01-21 13:09:32 +0100

    clocksource: dbx500-prcmu: comment cleanup
    Remove references to the deprecated DB5500 platform.
    
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Fabio Baltieri <fabio.baltieri@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/clocksource/clksrc-dbx500-prcmu.c
+++ drivers/clocksource/clksrc-dbx500-prcmu.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * DBx500-PRCMU Timer
  * The PRCMU has 5 timers which are available in a always-on
  * power domain.  We use the Timer 4 for our always-on clock
- * source on DB8500 and Timer 3 on DB5500.
+ * source on DB8500.
  */
 #include <linux/clockchips.h>
 #include <linux/clksrc-dbx500-prcmu.h>
