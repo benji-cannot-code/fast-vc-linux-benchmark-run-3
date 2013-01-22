dbb4be8f2ec274ff2ed65c784307637bdfcd415f
Dave Hansen <dave@linux.vnet.ibm.com>
2013-01-22 13:24:30 -0800

    x86, mm: Make DEBUG_VIRTUAL work earlier in boot
    The KVM code has some repeated bugs in it around use of __pa() on
    per-cpu data.  Those data are not in an area on which using
    __pa() is valid.  However, they are also called early enough in
    boot that __vmalloc_start_set is not set, and thus the
    CONFIG_DEBUG_VIRTUAL debugging does not catch them.
    
    This adds a check to also verify __pa() calls against max_low_pfn,
    which we can use earler in boot than is_vmalloc_addr().  However,
    if we are super-early in boot, max_low_pfn=0 and this will trip
    on every call, so also make sure that max_low_pfn is set before
    we try to use it.
    
    With this patch applied, CONFIG_DEBUG_VIRTUAL will actually
    catch the bug I was chasing (and fix later in this series).
    
    I'd love to find a generic way so that any __pa() call on percpu
    areas could do a BUG_ON(), but there don't appear to be any nice
    and easy ways to check if an address is a percpu one.  Anybody
    have ideas on a way to do this?
    
    Signed-off-by: Dave Hansen <dave@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/20130122212430.F46F8159@kernel.stglabs.ibm.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/mm/physaddr.c
+++ arch/x86/mm/physaddr.c
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <linux/bootmem.h>
 #include <linux/mmdebug.h>
 #include <linux/module.h>
 #include <linux/mm.h>
@@ -69,10 +70,16 @@ EXPORT_SYMBOL(__virt_addr_valid);
 #ifdef CONFIG_DEBUG_VIRTUAL
 unsigned long __phys_addr(unsigned long x)
 {
+	unsigned long phys_addr = x - PAGE_OFFSET;
 	/* VMALLOC_* aren't constants  */
 	VIRTUAL_BUG_ON(x < PAGE_OFFSET);
 	VIRTUAL_BUG_ON(__vmalloc_start_set && is_vmalloc_addr((void *) x));
-	return x - PAGE_OFFSET;
+	/* max_low_pfn is set early, but not _that_ early */
+	if (max_low_pfn) {
+		VIRTUAL_BUG_ON((phys_addr >> PAGE_SHIFT) > max_low_pfn);
+		BUG_ON(slow_virt_to_phys((void *)x) != phys_addr);
+	}
+	return phys_addr;
 }
 EXPORT_SYMBOL(__phys_addr);
 #endif
