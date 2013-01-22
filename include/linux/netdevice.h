b70c1eccfe5c6be1dbdbf6f7d650afca1acc1a3d
Cong Wang <amwang@redhat.com>
2013-01-22 21:29:39 +0000

    netpoll: add RCU annotation to npinfo field
    dev->npinfo is protected by RCU.
    
    This fixes the following sparse warnings:
    
    net/core/netpoll.c:177:48: error: incompatible types in comparison expression (different address spaces)
    net/core/netpoll.c:200:35: error: incompatible types in comparison expression (different address spaces)
    net/core/netpoll.c:221:35: error: incompatible types in comparison expression (different address spaces)
    net/core/netpoll.c:327:18: error: incompatible types in comparison expression (different address spaces)
    
    Cc: Eric Dumazet <eric.dumazet@gmail.com>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: Cong Wang <amwang@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1273,7 +1273,7 @@ struct net_device {
 	void (*destructor)(struct net_device *dev);
 
 #ifdef CONFIG_NETPOLL
-	struct netpoll_info	*npinfo;
+	struct netpoll_info __rcu	*npinfo;
 #endif
 
 #ifdef CONFIG_NET_NS
