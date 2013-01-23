2edaed41a65374f0fb9138d277aed0e3e0c09edf
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2013-01-23 00:22:37 +0000

    net: phy: icplus: fix broken INTR pin settings
    This patch fixes the setting of the INTR pin that is
    valid for IP101 A/G device and not for the IP1001.
    
    Reported-by: Anunay Saxena <anunay.saxena@st.com>
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/icplus.c
+++ drivers/net/phy/icplus.c
@@ -140,11 +140,6 @@ static int ip1001_config_init(struct phy_device *phydev)
 	if (c < 0)
 		return c;
 
-	/* INTR pin used: speed/link/duplex will cause an interrupt */
-	c = phy_write(phydev, IP101A_G_IRQ_CONF_STATUS, IP101A_G_IRQ_DEFAULT);
-	if (c < 0)
-		return c;
-
 	if ((phydev->interface == PHY_INTERFACE_MODE_RGMII) ||
 	    (phydev->interface == PHY_INTERFACE_MODE_RGMII_ID) ||
 	    (phydev->interface == PHY_INTERFACE_MODE_RGMII_RXID) ||
@@ -176,6 +171,11 @@ static int ip101a_g_config_init(struct phy_device *phydev)
 	int c;
 
 	c = ip1xx_reset(phydev);
+	if (c < 0)
+		return c;
+
+	/* INTR pin used: speed/link/duplex will cause an interrupt */
+	c = phy_write(phydev, IP101A_G_IRQ_CONF_STATUS, IP101A_G_IRQ_DEFAULT);
 	if (c < 0)
 		return c;
 
