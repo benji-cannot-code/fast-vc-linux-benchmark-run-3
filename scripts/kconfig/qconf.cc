fc2723aea17c9365b07b19c27640990bf09db849
Tiana Rakotovao Andriamahefa <rkmahefa@gmail.com>
2013-01-24 16:20:39 +0000

    kconfig: fix a compiliation error when using make xconfig
    When using make xconfig, the following compilation error appears :
       /usr/include/qt3/qvaluelist.h:427:13: error: ‘ptrdiff_t’ does not name a type
    Including stddef.h in scripts/kconfig/qconf.cc permits to avoid this error.
    
    Signed-off-by: Tiana Rakotovao Andriamahefa <rkmahefa@gmail.com>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/qconf.cc
+++ scripts/kconfig/qconf.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <qglobal.h>
 
 #if QT_VERSION < 0x040000
+#include <stddef.h>
 #include <qmainwindow.h>
 #include <qvbox.h>
 #include <qvaluelist.h>
