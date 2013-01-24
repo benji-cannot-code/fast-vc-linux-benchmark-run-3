d4e1951920d0cac02603d7c5a3562f3fe1f0f416
Yinghai Lu <yinghai@kernel.org>
2013-01-24 12:19:48 -0800

    x86, 64bit, mm: Add generic kernel/ident mapping helper
    It is simple version for kernel_physical_mapping_init.
    it will work to build one page table that will be used later.
    
    Use mapping_info to control
            1. alloc_pg_page method
            2. if PMD is EXEC,
            3. if pgd is with kernel low mapping or ident mapping.
    
    Will use to replace some local versions in kexec, hibernation and etc.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Link: http://lkml.kernel.org/r/1359058816-7615-8-git-send-email-yinghai@kernel.org
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/init.h
+++ arch/x86/include/asm/init.h
@@ -2,5 +2,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_INIT_H
 #define _ASM_X86_INIT_H
 
+struct x86_mapping_info {
+	void *(*alloc_pgt_page)(void *); /* allocate buf for page table */
+	void *context;			 /* context for alloc_pgt_page */
+	unsigned long pmd_flag;		 /* page flag for PMD entry */
+	bool kernel_mapping;		 /* kernel mapping or ident mapping */
+};
+
+int kernel_ident_mapping_init(struct x86_mapping_info *info, pgd_t *pgd_page,
+				unsigned long addr, unsigned long end);
 
 #endif /* _ASM_X86_INIT_H */
