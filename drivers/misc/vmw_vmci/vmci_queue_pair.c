8d9b3224aac151930ef92b19566bc3a85aca362d
David Rientjes <rientjes@google.com>
2013-01-24 14:49:31 -0800

    drivers, vmci: Fix build error
    We can't rely on vmalloc.h being included by other included files because
    under some configs it is possible for the build to fail:
    
    drivers/misc/vmw_vmci/vmci_queue_pair.c: In function 'qp_free_queue':
    drivers/misc/vmw_vmci/vmci_queue_pair.c:270: error: implicit declaration of function 'vunmap'
    drivers/misc/vmw_vmci/vmci_queue_pair.c:277: error: implicit declaration of function 'vfree'
    drivers/misc/vmw_vmci/vmci_queue_pair.c: In function 'qp_alloc_queue':
    drivers/misc/vmw_vmci/vmci_queue_pair.c:302: error: implicit declaration of function 'vmalloc'
    drivers/misc/vmw_vmci/vmci_queue_pair.c:302: warning: assignment makes pointer from integer without a cast
    drivers/misc/vmw_vmci/vmci_queue_pair.c:324: error: implicit declaration of function 'vmap'
    drivers/misc/vmw_vmci/vmci_queue_pair.c:324: error: 'VM_MAP' undeclared (first use in this function)
    drivers/misc/vmw_vmci/vmci_queue_pair.c:324: error: (Each undeclared identifier is reported only once
    drivers/misc/vmw_vmci/vmci_queue_pair.c:324: error: for each function it appears in.)
    drivers/misc/vmw_vmci/vmci_queue_pair.c: In function 'qp_host_map_queues':
    drivers/misc/vmw_vmci/vmci_queue_pair.c:843: error: 'VM_MAP' undeclared (first use in this function)
    
    Fix the build by directly including vmalloc.h.
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Cc: George Zhang <georgezhang@vmware.com>
    Cc: Andy King <acking@vmware.com>
    Cc: Dmitry Torokhov <dtor@vmware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/vmw_vmci/vmci_queue_pair.c
+++ drivers/misc/vmw_vmci/vmci_queue_pair.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/socket.h>
 #include <linux/wait.h>
+#include <linux/vmalloc.h>
 
 #include "vmci_handle_array.h"
 #include "vmci_queue_pair.h"
