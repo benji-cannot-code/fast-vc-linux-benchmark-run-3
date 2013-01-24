7e7b49ea93b85419276831128b13bb33718676a4
Shirish S <s.shirish@samsung.com>
2013-01-23 22:03:18 -0500

    drm/exynos: add check for the device power status
    V2: Add mutex protection, while read.
    
    The hdmi and mixer win_commit calls currently are
    not checking the status of IP before updating the
    respective registers, this patch adds this check.
    
    Signed-off-by: Shirish S <s.shirish@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_hdmi.c
+++ drivers/gpu/drm/exynos/exynos_hdmi.c
@@ -2164,6 +2164,13 @@ static void hdmi_commit(void *ctx)
 
 	DRM_DEBUG_KMS("[%d] %s\n", __LINE__, __func__);
 
+	mutex_lock(&hdata->hdmi_mutex);
+	if (!hdata->powered) {
+		mutex_unlock(&hdata->hdmi_mutex);
+		return;
+	}
+	mutex_unlock(&hdata->hdmi_mutex);
+
 	hdmi_conf_apply(hdata);
 }
 
