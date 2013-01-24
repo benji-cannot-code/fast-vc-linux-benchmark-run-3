5fee876c68ad35866a1798373082d6144c420edf
Felipe Balbi <balbi@ti.com>
2013-01-24 16:38:19 +0200

    usb: gadget: mv_u3d_core: fix a compile warning
    Fix the following compile warning:
    
    mv_u3d_core.c:1766:12: warning: 'mv_u3d_remove' \
    	defined but not used [-Wunused-function]
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/mv_u3d_core.c
+++ drivers/usb/gadget/mv_u3d_core.c
@@ -2073,7 +2073,7 @@ static void mv_u3d_shutdown(struct platform_device *dev)
 
 static struct platform_driver mv_u3d_driver = {
 	.probe		= mv_u3d_probe,
-	.remove		= __exit_p(mv_u3d_remove),
+	.remove		= mv_u3d_remove,
 	.shutdown	= mv_u3d_shutdown,
 	.driver		= {
 		.owner	= THIS_MODULE,
