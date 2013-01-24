d7b05659bc1619655e525929f44463ed3f06f8b4
Mauro Carvalho Chehab <mchehab@redhat.com>
2013-01-24 13:16:17 -0300

    [media] mb86a20s: remove global BER/PER counters if per-layer counters vanish
    If, for any reason, all per-layers counters stop, remove the
    corresponding global counter.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/dvb-frontends/mb86a20s.c
+++ drivers/media/dvb-frontends/mb86a20s.c
@@ -1605,7 +1605,6 @@ static int mb86a20s_get_stats(struct dvb_frontend *fe)
 					"%s: Can't get BER for layer %c (error %d).\n",
 					__func__, 'A' + i, rc);
 			}
-
 			if (c->block_error.stat[1 + i].scale != FE_SCALE_NOT_AVAILABLE)
 				pre_ber_layers++;
 
@@ -1628,7 +1627,6 @@ static int mb86a20s_get_stats(struct dvb_frontend *fe)
 					"%s: Can't get BER for layer %c (error %d).\n",
 					__func__, 'A' + i, rc);
 			}
-
 			if (c->block_error.stat[1 + i].scale != FE_SCALE_NOT_AVAILABLE)
 				post_ber_layers++;
 
@@ -1653,7 +1651,6 @@ static int mb86a20s_get_stats(struct dvb_frontend *fe)
 					__func__, 'A' + i, rc);
 
 			}
-
 			if (c->block_error.stat[1 + i].scale != FE_SCALE_NOT_AVAILABLE)
 				per_layers++;
 
@@ -1687,6 +1684,9 @@ static int mb86a20s_get_stats(struct dvb_frontend *fe)
 		c->pre_bit_error.stat[0].uvalue = t_pre_bit_error;
 		c->pre_bit_count.stat[0].scale = FE_SCALE_COUNTER;
 		c->pre_bit_count.stat[0].uvalue = t_pre_bit_count;
+	} else {
+		c->pre_bit_error.stat[0].scale = FE_SCALE_NOT_AVAILABLE;
+		c->pre_bit_count.stat[0].scale = FE_SCALE_COUNTER;
 	}
 
 	/*
@@ -1705,6 +1705,9 @@ static int mb86a20s_get_stats(struct dvb_frontend *fe)
 		c->post_bit_error.stat[0].uvalue = t_post_bit_error;
 		c->post_bit_count.stat[0].scale = FE_SCALE_COUNTER;
 		c->post_bit_count.stat[0].uvalue = t_post_bit_count;
+	} else {
+		c->post_bit_error.stat[0].scale = FE_SCALE_NOT_AVAILABLE;
+		c->post_bit_count.stat[0].scale = FE_SCALE_COUNTER;
 	}
 
 	if (per_layers) {
@@ -1719,6 +1722,9 @@ static int mb86a20s_get_stats(struct dvb_frontend *fe)
 		c->block_error.stat[0].uvalue = t_block_error;
 		c->block_count.stat[0].scale = FE_SCALE_COUNTER;
 		c->block_count.stat[0].uvalue = t_block_count;
+	} else {
+		c->block_error.stat[0].scale = FE_SCALE_NOT_AVAILABLE;
+		c->block_count.stat[0].scale = FE_SCALE_COUNTER;
 	}
 
 	return rc;
