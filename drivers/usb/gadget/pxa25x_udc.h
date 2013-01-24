2a2750d8d166a3afe3c5072e9d057bcc83868b9e
Felipe Balbi <balbi@ti.com>
2013-01-24 17:11:44 +0200

    usb: gadget: pxa25x_udc: convert to udc_start/udc_stop
    Mechanical change making use of the new (can we
    still call it new ?) interface for registering
    UDC drivers.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/pxa25x_udc.h
+++ drivers/usb/gadget/pxa25x_udc.h
@@ -127,6 +127,7 @@ struct pxa25x_udc {
 	struct dentry				*debugfs_udc;
 #endif
 };
+#define to_pxa25x(g)	(container_of((g), struct pxa25x_udc, gadget))
 
 /*-------------------------------------------------------------------------*/
 
