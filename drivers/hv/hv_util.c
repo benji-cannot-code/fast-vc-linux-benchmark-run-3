ed9741512f0c5a6c7fad1196d23ef963325ef6f2
K. Y. Srinivasan <kys@microsoft.com>
2013-01-23 17:42:45 -0800

    Drivers: hv: Execute shutdown in a thread context
    Execute the shutdown code in a thread context. With recent changes made to the
    shutdown code, shutdown code cannot be invoked from an interrupt context.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Reviewed-by: Haiyang Zhang <haiyangz@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hv/hv_util.c
+++ drivers/hv/hv_util.c
@@ -50,6 +50,16 @@ static struct hv_util_service util_kvp = {
 	.util_deinit = hv_kvp_deinit,
 };
 
+static void perform_shutdown(struct work_struct *dummy)
+{
+	orderly_poweroff(true);
+}
+
+/*
+ * Perform the shutdown operation in a thread context.
+ */
+static DECLARE_WORK(shutdown_work, perform_shutdown);
+
 static void shutdown_onchannelcallback(void *context)
 {
 	struct vmbus_channel *channel = context;
@@ -107,7 +117,7 @@ static void shutdown_onchannelcallback(void *context)
 	}
 
 	if (execute_shutdown == true)
-		orderly_poweroff(true);
+		schedule_work(&shutdown_work);
 }
 
 /*
