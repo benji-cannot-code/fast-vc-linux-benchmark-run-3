7596b1384b8e9b64afd9f1d8c2645ff74ce5067d
Johannes Berg <johannes.berg@intel.com>
2013-01-24 16:43:48 +0100

    iwlwifi: include debugfs.h
    The iwl-op-mode.h file uses a struct dentry for debugfs
    so should include debugfs.h.
    
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-op-mode.h
+++ drivers/net/wireless/iwlwifi/iwl-op-mode.h
@@ -64,6 +64,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __iwl_op_mode_h__
 #define __iwl_op_mode_h__
 
+#include <linux/debugfs.h>
+
 struct iwl_op_mode;
 struct iwl_trans;
 struct sk_buff;
