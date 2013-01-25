7deb0046decef136a5e3f6a9f11f6e43dfd76425
Cong Ding <dinggnu@gmail.com>
2013-01-25 17:48:59 -0600

    libceph: fix undefined behavior when using snprintf()
    The variable "str" is used as both the source and destination in
    function snprintf(), which is undefined behavior based on C11. The
    original description in C11 is:
    	"If copying takes place between objects that
    	overlap, the behavior is undefined."
    
    And, the function of ceph_osdmap_state_str() is to return the osdmap
    state, so it should return "doesn't exist" when all the conditions
    are not satisfied. I fix it in this patch.
    
    [elder@inktank.com: shortened the commit message]
    
    Signed-off-by: Cong Ding <dinggnu@gmail.com>
    Reviewed-by: Alex Elder <elder@inktank.com>

--- net/ceph/osdmap.c
+++ net/ceph/osdmap.c
@@ -14,26 +14,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 char *ceph_osdmap_state_str(char *str, int len, int state)
 {
-	int flag = 0;
-
 	if (!len)
-		goto done;
+		return str;
 
-	*str = '\0';
-	if (state) {
-		if (state & CEPH_OSD_EXISTS) {
-			snprintf(str, len, "exists");
-			flag = 1;
-		}
-		if (state & CEPH_OSD_UP) {
-			snprintf(str, len, "%s%s%s", str, (flag ? ", " : ""),
-				 "up");
-			flag = 1;
-		}
-	} else {
+	if ((state & CEPH_OSD_EXISTS) && (state & CEPH_OSD_UP))
+		snprintf(str, len, "exists, up");
+	else if (state & CEPH_OSD_EXISTS)
+		snprintf(str, len, "exists");
+	else if (state & CEPH_OSD_UP)
+		snprintf(str, len, "up");
+	else
 		snprintf(str, len, "doesn't exist");
-	}
-done:
+
 	return str;
 }
 
