9382d5c3b41fb97da715eb16dc53912f94b8e135
H Hartley Sweeten <hartleys@visionengravers.com>
2013-01-25 15:00:22 -0700

    staging: comedi: quatech_daqp_cs: remove casts of void *
    Casting a void * is not necessary.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/quatech_daqp_cs.c
+++ drivers/staging/comedi/drivers/quatech_daqp_cs.c
@@ -211,7 +211,7 @@ static void hex_dump(char *str, void *ptr, int len)
 
 static int daqp_ai_cancel(struct comedi_device *dev, struct comedi_subdevice *s)
 {
-	struct local_info_t *local = (struct local_info_t *)s->private;
+	struct local_info_t *local = s->private;
 
 	if (local->stop)
 		return -EIO;
@@ -238,7 +238,7 @@ static int daqp_ai_cancel(struct comedi_device *dev, struct comedi_subdevice *s)
  */
 static enum irqreturn daqp_interrupt(int irq, void *dev_id)
 {
-	struct local_info_t *local = (struct local_info_t *)dev_id;
+	struct local_info_t *local = dev_id;
 	struct comedi_device *dev;
 	struct comedi_subdevice *s;
 	int loop_limit = 10000;
@@ -266,7 +266,7 @@ static enum irqreturn daqp_interrupt(int irq, void *dev_id)
 		return IRQ_NONE;
 	}
 
-	if ((struct local_info_t *)s->private != local) {
+	if (s->private != local) {
 		pr_warn("invalid comedi_subdevice.\n");
 		return IRQ_NONE;
 	}
@@ -336,7 +336,7 @@ static int daqp_ai_insn_read(struct comedi_device *dev,
 			     struct comedi_subdevice *s,
 			     struct comedi_insn *insn, unsigned int *data)
 {
-	struct local_info_t *local = (struct local_info_t *)s->private;
+	struct local_info_t *local = s->private;
 	int i;
 	int v;
 	int counter = 10000;
@@ -529,7 +529,7 @@ static int daqp_ai_cmdtest(struct comedi_device *dev,
 
 static int daqp_ai_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 {
-	struct local_info_t *local = (struct local_info_t *)s->private;
+	struct local_info_t *local = s->private;
 	struct comedi_cmd *cmd = &s->async->cmd;
 	int counter;
 	int scanlist_start_on_every_entry;
@@ -731,7 +731,7 @@ static int daqp_ao_insn_write(struct comedi_device *dev,
 			      struct comedi_subdevice *s,
 			      struct comedi_insn *insn, unsigned int *data)
 {
-	struct local_info_t *local = (struct local_info_t *)s->private;
+	struct local_info_t *local = s->private;
 	int d;
 	unsigned int chan;
 
@@ -758,7 +758,7 @@ static int daqp_di_insn_read(struct comedi_device *dev,
 			     struct comedi_subdevice *s,
 			     struct comedi_insn *insn, unsigned int *data)
 {
-	struct local_info_t *local = (struct local_info_t *)s->private;
+	struct local_info_t *local = s->private;
 
 	if (local->stop)
 		return -EIO;
@@ -774,7 +774,7 @@ static int daqp_do_insn_write(struct comedi_device *dev,
 			      struct comedi_subdevice *s,
 			      struct comedi_insn *insn, unsigned int *data)
 {
-	struct local_info_t *local = (struct local_info_t *)s->private;
+	struct local_info_t *local = s->private;
 
 	if (local->stop)
 		return -EIO;
