309864a018560765788ed37974f9242cce743795
Arnd Bergmann <arnd@arndb.de>
2013-01-25 22:20:40 +0000

    ARM: sa1100: don't warn about mach/ide.h
    This warning has existed since before the start of (git) history.
    Apparently nobody has bothered to fix it in a long time, and
    this is unlikely to change. Note that the file that the warning
    refers to has moved to a different location and was subsequently
    deleted in 2008.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Russell King <linux@arm.linux.org.uk>

--- arch/arm/mach-sa1100/lart.c
+++ arch/arm/mach-sa1100/lart.c
@@ -25,9 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "generic.h"
 
-
-#warning "include/asm/arch-sa1100/ide.h needs fixing for lart"
-
 static struct mcp_plat_data lart_mcp_data = {
 	.mccr0		= MCCR0_ADM,
 	.sclk_rate	= 11981000,
