c3dca8f8d33d29c48c9a44ac8dab91f36879f2e9
H Hartley Sweeten <hartleys@visionengravers.com>
2013-01-25 17:33:17 -0700

    staging: comedi: ni_mio_cs: remove setting dev->driver
    The comedi core sets the dev->driver pointer before calling the
    comedi_driver attach function.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_mio_cs.c
+++ drivers/staging/comedi/drivers/ni_mio_cs.c
@@ -336,7 +336,6 @@ static int mio_cs_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	if (!link)
 		return -EIO;
 
-	dev->driver = &driver_ni_mio_cs;
 	dev->iobase = link->resource[0]->start;
 
 	irq = link->irq;
