5496571a70688e32b017d59fba0ea0b82e5f9b25
H Hartley Sweeten <hartleys@visionengravers.com>
2013-01-25 18:37:06 -0700

    staging: comedi: ni_labpc_cs: remove pcmcia_device private data
    The private data, struct local_info_t, is not being used in the
    driver. Remove it as well as the kzalloc/kfree.
    
    Also, don't set the 'pcmcia_cur_dev' variable unless the pcmcia
    configuration is successful.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc_cs.c
+++ drivers/staging/comedi/drivers/ni_labpc_cs.c
@@ -155,11 +155,6 @@ static int labpc_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	return labpc_common_attach(dev, iobase, irq, 0);
 }
 
-struct local_info_t {
-	struct pcmcia_device *link;
-	struct bus_operations *bus;
-};
-
 static int labpc_pcmcia_config_loop(struct pcmcia_device *p_dev,
 				void *priv_data)
 {
@@ -171,16 +166,7 @@ static int labpc_pcmcia_config_loop(struct pcmcia_device *p_dev,
 
 static int labpc_cs_attach(struct pcmcia_device *link)
 {
-	struct local_info_t *local;
 	int ret;
-
-	local = kzalloc(sizeof(*local), GFP_KERNEL);
-	if (!local)
-		return -ENOMEM;
-	local->link = link;
-	link->priv = local;
-
-	pcmcia_cur_dev = link;
 
 	link->config_flags |= CONF_ENABLE_IRQ | CONF_ENABLE_PULSE_IRQ |
 		CONF_AUTO_AUDIO | CONF_AUTO_SET_IO;
@@ -198,6 +184,8 @@ static int labpc_cs_attach(struct pcmcia_device *link)
 	if (ret)
 		goto failed;
 
+	pcmcia_cur_dev = link;
+
 	return 0;
 
 failed:
@@ -208,9 +196,6 @@ failed:
 static void labpc_cs_detach(struct pcmcia_device *link)
 {
 	pcmcia_disable_device(link);
-
-	/* This points to the parent local_info_t struct (may be null) */
-	kfree(link->priv);
 }
 
 static const struct pcmcia_device_id labpc_cs_ids[] = {
