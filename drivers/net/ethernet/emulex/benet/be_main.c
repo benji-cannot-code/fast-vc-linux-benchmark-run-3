445b131b4f9e593f7fec986871ed6f6f5665a0e6
Sarveshwar Bandi <sarveshwar.bandi@emulex.com>
2013-01-28 04:17:01 +0000

    be2net: Updating Module Author string and log message string to "Emulex Corporation"
    Signed-off-by: Sarveshwar Bandi <sarveshwar.bandi@emulex.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/emulex/benet/be_main.c
+++ drivers/net/ethernet/emulex/benet/be_main.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 MODULE_VERSION(DRV_VER);
 MODULE_DEVICE_TABLE(pci, be_dev_ids);
 MODULE_DESCRIPTION(DRV_DESC " " DRV_VER);
-MODULE_AUTHOR("ServerEngines Corporation");
+MODULE_AUTHOR("Emulex Corporation");
 MODULE_LICENSE("GPL");
 
 static unsigned int num_vfs;
