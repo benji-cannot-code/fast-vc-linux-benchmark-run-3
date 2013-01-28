e543d9a82dc883e7ea641374fead2e6a42be5ef8
Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
2013-01-28 18:31:07 +0200

    wil6210: checkpatch warnings
    Signed-off-by: Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/wil6210/interrupt.c
+++ drivers/net/wireless/ath/wil6210/interrupt.c
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline void wil_icr_clear(u32 x, void __iomem *addr)
 {
-
 }
 #else /* defined(CONFIG_WIL6210_ISR_COR) */
 /* configure to Write-1-to-Clear mode */
