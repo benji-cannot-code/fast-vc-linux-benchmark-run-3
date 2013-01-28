9f6a5bd61a4514676c54da9acb591c491e0a024b
Michal Simek <michal.simek@xilinx.com>
2013-01-28 13:22:27 +0100

    arm: zynq: Add missing irqchip.h to common.c
    The patch: "ARM: use common irqchip_init for GIC init"
    (sha1: 0529e315bbda5d502c93df2cfafba9bb337fbdf4)
    should also add linux/irqchip.h header.
    
    Error message:
    arch/arm/mach-zynq/common.c:99:14: error: 'irqchip_init'
      undeclared here (not in a function)
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/common.c
+++ arch/arm/mach-zynq/common.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
 #include <linux/of.h>
+#include <linux/irqchip.h>
 
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
