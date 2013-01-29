91a73585023b206bbf061ddd9afe3d55ff4b0e72
Peter Hurley <peter@hurleysoftware.com>
2013-01-28 22:34:40 -0500

    staging/fwserial: Factor unstable stats/debug/status info to debugfs
    Add the following file hierarchy to debugfs:
    
      <debugfs>-+
                +- firewire_serial -+- <unit> -+- peers
                                    |          +- stats
                                    |
                                    +- <unit> -+- peers
                                               +- stats
    
    The 'peers' file (read-only) contains status and configuration
    info for attached peers for the given fwserial unit.
    
    The 'stats' file (read-only) contains statistics and data profiling
    information for each tty port for the given fwserial unit.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fwserial/fwserial.h
+++ drivers/staging/fwserial/fwserial.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial_reg.h>
 #include <linux/module.h>
 #include <linux/seq_file.h>
+#include <linux/debugfs.h>
 
 #include "dma_fifo.h"
 
@@ -339,6 +340,7 @@ struct fw_serial {
 	struct fw_card	  *card;
 	struct kref	  kref;
 
+	struct dentry	  *debugfs;
 	struct fwtty_peer *self;
 
 	struct list_head  list;
