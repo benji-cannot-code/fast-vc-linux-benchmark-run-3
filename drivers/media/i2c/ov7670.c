433840f25b9dce69fa17a9d0f7ca26e1d4f29e94
Javier Martin <javier.martin@vista-silicon.com>
2013-01-29 07:26:38 -0300

    [media] media: ov7670: Add possibility to disable pixclk during hblank
    Some bridge drivers capture pixels during blanking periods if
    pixclk is enabled. In order to avoid capturing bogus data we need to
    disable pixclk in the sensor during those blanking periods.
    
    Acked-by: Jonathan Corbet <corbet@lwn.net>
    Signed-off-by: Javier Martin <javier.martin@vista-silicon.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/i2c/ov7670.c
+++ drivers/media/i2c/ov7670.c
@@ -232,6 +232,7 @@ struct ov7670_info {
 	u8 clkrc;			/* Clock divider value */
 	bool use_smbus;			/* Use smbus I/O instead of I2C */
 	bool pll_bypass;
+	bool pclk_hb_disable;
 	const struct ov7670_devtype *devtype; /* Device specifics */
 };
 
@@ -1713,6 +1714,9 @@ static int ov7670_probe(struct i2c_client *client,
 		 */
 		if (config->pll_bypass && id->driver_data != MODEL_OV7670)
 			info->pll_bypass = true;
+
+		if (config->pclk_hb_disable)
+			info->pclk_hb_disable = true;
 	}
 
 	/* Make sure it's an ov7670 */
@@ -1736,6 +1740,9 @@ static int ov7670_probe(struct i2c_client *client,
 	tpf.numerator = 1;
 	tpf.denominator = 30;
 	info->devtype->set_framerate(sd, &tpf);
+
+	if (info->pclk_hb_disable)
+		ov7670_write(sd, REG_COM10, COM10_PCLK_HB);
 
 	return 0;
 }
