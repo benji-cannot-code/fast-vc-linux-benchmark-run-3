60fbd40d5b10b1297e5f7862141f922d11ab5b58
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-01-30 15:25:06 -0700

    staging: comedi: don't expose comedi_device_{attach,detach}
    These functions are only used by the comedi core. Move the
    prototypes to comedi_internal.h so they are not exposed to
    the comedi drivers.
    
    Tidy up comedi_internal.h a bit so that all the internal stuff
    in drivers.c is grouped.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_internal.h
+++ drivers/staging/comedi/comedi_internal.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 int do_rangeinfo_ioctl(struct comedi_device *dev,
 		       struct comedi_rangeinfo __user *arg);
-int insn_inval(struct comedi_device *dev, struct comedi_subdevice *s,
-	       struct comedi_insn *insn, unsigned int *data);
 int comedi_alloc_board_minor(struct device *hardware_device);
 void comedi_free_board_minor(unsigned minor);
 int comedi_find_board_minor(struct device *hardware_device);
@@ -23,6 +21,15 @@ unsigned int comedi_buf_write_n_allocated(struct comedi_async *async);
 extern unsigned int comedi_default_buf_size_kb;
 extern unsigned int comedi_default_buf_maxsize_kb;
 extern bool comedi_autoconfig;
+
+/* drivers.c */
+
 extern struct comedi_driver *comedi_drivers;
+
+int insn_inval(struct comedi_device *, struct comedi_subdevice *,
+	       struct comedi_insn *, unsigned int *);
+
+void comedi_device_detach(struct comedi_device *);
+int comedi_device_attach(struct comedi_device *, struct comedi_devconfig *);
 
 #endif /* _COMEDI_INTERNAL_H */
