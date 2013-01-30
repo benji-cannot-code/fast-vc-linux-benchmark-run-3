c175aef92eea6ac391d9962706b1c636edc495ad
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-01-30 16:44:25 -0700

    staging: comedi: ni_daq_dio24: remove pcmcia_device private data
    The private data, struct local_info_t, is not being used in the
    driver. Remove it as well as the kzalloc/kfree.
    
    Also, don't set the 'pcmcia_cur_dev' variable unless the pcmcia
    probe is successful.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_daq_dio24.c
+++ drivers/staging/comedi/drivers/ni_daq_dio24.c
@@ -185,11 +185,6 @@ static void dio24_detach(struct comedi_device *dev)
 		free_irq(dev->irq, dev);
 };
 
-struct local_info_t {
-	struct pcmcia_device *link;
-	struct bus_operations *bus;
-};
-
 static int dio24_pcmcia_config_loop(struct pcmcia_device *p_dev,
 				    void *priv_data)
 {
@@ -201,16 +196,7 @@ static int dio24_pcmcia_config_loop(struct pcmcia_device *p_dev,
 
 static int dio24_cs_attach(struct pcmcia_device *link)
 {
-	struct local_info_t *local;
 	int ret;
-
-	local = kzalloc(sizeof(*local), GFP_KERNEL);
-	if (!local)
-		return -ENOMEM;
-	local->link = link;
-	link->priv = local;
-
-	pcmcia_cur_dev = link;
 
 	link->config_flags |= CONF_ENABLE_IRQ | CONF_AUTO_AUDIO |
 		CONF_AUTO_SET_IO;
@@ -228,6 +214,8 @@ static int dio24_cs_attach(struct pcmcia_device *link)
 	if (ret)
 		goto failed;
 
+	pcmcia_cur_dev = link;
+
 	return 0;
 
 failed:
@@ -238,9 +226,6 @@ failed:
 static void dio24_cs_detach(struct pcmcia_device *link)
 {
 	pcmcia_disable_device(link);
-
-	/* This points to the parent local_info_t struct */
-	kfree(link->priv);
 }
 
 static const struct pcmcia_device_id dio24_cs_ids[] = {
