0ecfa79d9bf7ed0d3f8dc2a56f48ec6b8a82e751
Lars-Peter Clausen <lars@metafoo.de>
2013-01-31 14:27:00 +0000

    staging:iio:adxrs450: Reflow overlong lines
    Reflow two lines to meet the 80 characters per line limit.
    
    Fixes the following checkpatch warnings:
    	WARNING: line over 80 characters
    	#29: FILE: staging/iio/gyro/adxrs450_core.c:29:
    	+ * @reg_address: the address of the lower of the two registers,which should be an even address,
    
    	WARNING: line over 80 characters
    	#81: FILE: staging/iio/gyro/adxrs450_core.c:81:
    	+ * @reg_address: the address of the lower of the two registers,which should be an even address,
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/gyro/adxrs450_core.c
+++ drivers/staging/iio/gyro/adxrs450_core.c
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**
  * adxrs450_spi_read_reg_16() - read 2 bytes from a register pair
  * @dev: device associated with child of actual iio_dev
- * @reg_address: the address of the lower of the two registers,which should be an even address,
- * Second register's address is reg_address + 1.
+ * @reg_address: the address of the lower of the two registers, which should be
+ *	an even address, the second register's address is reg_address + 1.
  * @val: somewhere to pass back the value read
  **/
 static int adxrs450_spi_read_reg_16(struct iio_dev *indio_dev,
@@ -79,8 +79,8 @@ error_ret:
 /**
  * adxrs450_spi_write_reg_16() - write 2 bytes data to a register pair
  * @dev: device associated with child of actual actual iio_dev
- * @reg_address: the address of the lower of the two registers,which should be an even address,
- * Second register's address is reg_address + 1.
+ * @reg_address: the address of the lower of the two registers,which should be
+ *	an even address, the second register's address is reg_address + 1.
  * @val: value to be written.
  **/
 static int adxrs450_spi_write_reg_16(struct iio_dev *indio_dev,
