b6c84365c3cab9553baa07f1686e877f1fa2e84f
Kukjin Kim <kgene.kim@samsung.com>
2013-01-31 16:54:38 -0800

    ARM: S3C24XX: Remove plat-s3c24xx directory in arch/arm/
    This patch is for just moving plat-s3c24xx/*.c into mach-s3c24xx/, so
    that we could remove plat-s3c24xx directory. But since the PLAT_S3C24XX
    is used in drivers, the statement is not deleted and it will be sorted
    out next time.
    
    Cc: Ben Dooks <ben-linux@fluff.org>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c24xx/iotiming-s3c2412.c
+++ arch/arm/mach-s3c24xx/iotiming-s3c2412.c
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* linux/arch/arm/plat-s3c24xx/s3c2412-iotiming.c
- *
+/*
  * Copyright (c) 2006-2008 Simtec Electronics
  *	http://armlinux.simtec.co.uk/
  *	Ben Dooks <ben@simtec.co.uk>
