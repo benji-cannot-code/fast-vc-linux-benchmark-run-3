7a25e57802cb2716c455d766c28229575897f6b6
Linus Walleij <linus.walleij@linaro.org>
2013-02-02 23:16:57 +0100

    ARM: integrator: convert PCIv3 bridge to platform device
    This converts the PCIv3 driver to a platform device driver, and
    registers the device only on the Integrator/AP instead of bailing
    out in the initcall if the platform is not correct.
    
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-integrator/integrator_ap.c
+++ arch/arm/mach-integrator/integrator_ap.c
@@ -495,6 +495,15 @@ static struct of_dev_auxdata ap_auxdata_lookup[] __initdata = {
 	{ /* sentinel */ },
 };
 
+/*
+ * This is a placeholder that will get deleted when we move the PCI
+ * device over to the device tree.
+ */
+static struct platform_device pci_v3_device_of = {
+	.name		= "pci-v3",
+	.id		= 0,
+};
+
 static void __init ap_init_of(void)
 {
 	unsigned long sc_dec;
@@ -548,6 +557,8 @@ static void __init ap_init_of(void)
 
 	of_platform_populate(root, of_default_bus_match_table,
 			ap_auxdata_lookup, parent);
+
+	platform_device_register(&pci_v3_device_of);
 
 	sc_dec = readl(ap_syscon_base + INTEGRATOR_SC_DEC_OFFSET);
 	for (i = 0; i < 4; i++) {
@@ -616,6 +627,11 @@ static void __init ap_map_io_atag(void)
  * for eventual deletion.
  */
 
+static struct platform_device pci_v3_device = {
+	.name		= "pci-v3",
+	.id		= 0,
+};
+
 static struct resource cfi_flash_resource = {
 	.start		= INTEGRATOR_FLASH_BASE,
 	.end		= INTEGRATOR_FLASH_BASE + INTEGRATOR_FLASH_SIZE - 1,
@@ -673,6 +689,7 @@ static void __init ap_init(void)
 	unsigned long sc_dec;
 	int i;
 
+	platform_device_register(&pci_v3_device);
 	platform_device_register(&cfi_flash_device);
 
 	ap_syscon_base = __io_address(INTEGRATOR_SC_BASE);
