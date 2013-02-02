95c88ad83ef9df35a31ee5255733594ea1e6ece8
Kukjin Kim <kgene.kim@samsung.com>
2013-02-01 17:13:37 -0800

    ARM: S3C24XX: header mach/regs-s3c2412-mem.h local
    Since header mach/regs-s3c2412-mem.h is used only into mach-s3c24xx/,
    this patch moves the header file in local.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-s3c24xx/iotiming-s3c2412.c
+++ arch/arm/mach-s3c24xx/iotiming-s3c2412.c
@@ -28,11 +28,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 
-#include <mach/regs-s3c2412-mem.h>
-
 #include <plat/cpu.h>
 #include <plat/cpu-freq-core.h>
 #include <plat/clock.h>
+
+#include "s3c2412.h"
 
 #define print_ns(x) ((x) / 10), ((x) % 10)
 
