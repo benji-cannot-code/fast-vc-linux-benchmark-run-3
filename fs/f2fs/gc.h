0c06b39471350b48c136552ed7d11870d841cc18
Namjae Jeon <namjae.jeon@samsung.com>
2013-02-02 23:52:28 +0900

    f2fs: name gc task as per the block device
    Currently GC task is started for each f2fs formatted/mounted device.
    But, when we check the task list, using 'ps', there is no distinguishing
    factor between the tasks. So, name the task as per the block device just
    like the flusher threads.
    Also, remove the macro GC_THREAD_NAME and instead use the name: f2fs_gc
    to avoid name length truncation, as the command length is 16
    -> TASK_COMM_LEN 16 and example name like:
    f2fs_gc_task:8:16 -> this exceeds name length
    
    Before Patch for 2 F2FS formatted partitions:
    root  28061  0.0  0.0  0 0 ? S 10:31   0:00 [f2fs_gc_task]
    root  28087  0.0  0.0  0 0 ? S 10:32   0:00 [f2fs_gc_task]
    
    After Patch:
    root  16756  0.0  0.0  0  0 ?  S  14:57   0:00 [f2fs_gc-8:18]
    root  16765  0.0  0.0  0  0 ?  S  14:57   0:00 [f2fs_gc-8:19]
    
    Signed-off-by: Namjae Jeon <namjae.jeon@samsung.com>
    Signed-off-by: Amit Sahrawat <a.sahrawat@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- fs/f2fs/gc.h
+++ fs/f2fs/gc.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#define GC_THREAD_NAME	"f2fs_gc_task"
 #define GC_THREAD_MIN_WB_PAGES		1	/*
 						 * a threshold to determine
 						 * whether IO subsystem is idle
