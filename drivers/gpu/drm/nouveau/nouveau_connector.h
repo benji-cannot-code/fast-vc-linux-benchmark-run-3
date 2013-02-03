822470bc731432b176be26967173c7e15b64fecc
Ben Skeggs <bskeggs@redhat.com>
2013-02-03 20:29:53 +1000

    drm/nouveau/drm: store full dcb gpio function data in connector
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_connector.h
+++ drivers/gpu/drm/nouveau/nouveau_connector.h
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_edid.h>
 #include "nouveau_crtc.h"
 
+#include <subdev/bios.h>
+#include <subdev/bios/gpio.h>
+
 struct nouveau_i2c_port;
 
 enum nouveau_underscan_type {
@@ -60,9 +63,9 @@ enum nouveau_dithering_depth {
 struct nouveau_connector {
 	struct drm_connector base;
 	enum dcb_connector_type type;
+	struct dcb_gpio_func hpd;
 	u8 index;
 	u8 *dcb;
-	u8 hpd;
 
 	int dithering_mode;
 	int dithering_depth;
