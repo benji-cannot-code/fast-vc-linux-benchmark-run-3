7b5b43eb5d34dceefea6bc2705c2fae8d9e21b66
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-02-04 15:35:26 -0800

    kgdb: remove #include <linux/serial_8250.h> from kgdb.h
    There's no reason kgdb.h itself needs to include the 8250 serial port
    header file.  So push it down to the _very_ limited number of individual
    drivers that need the values in that file, and fix up the places where
    people really wanted serial_core.h and platform_device.h.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/kgdb_nmi.c
+++ drivers/tty/serial/kgdb_nmi.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty.h>
 #include <linux/tty_driver.h>
 #include <linux/tty_flip.h>
+#include <linux/serial_core.h>
 #include <linux/interrupt.h>
 #include <linux/hrtimer.h>
 #include <linux/tick.h>
