15e4e590734e080661deaa5c282d72b072f26afc
Ryota Ozaki <ozaki.ryota@gmail.com>
2013-02-04 10:35:30 +1030

    virtio-mmio: fix wrong comment about register offset
    The register offset of InterruptACK is 0x064, not 0x060.
    Fix it.
    
    Signed-off-by: Ryota Ozaki <ozaki.ryota@gmail.com>
    Reviewed-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>
    Acked-by: Pawel Moll <pawel.moll@arm.com>
    CC: Jiri Kosina <trivial@kernel.org>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/virtio/virtio_mmio.c
+++ drivers/virtio/virtio_mmio.c
@@ -76,7 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * 0x050  W  QueueNotify      Queue notifier
  * 0x060  R  InterruptStatus  Interrupt status register
- * 0x060  W  InterruptACK     Interrupt acknowledge register
+ * 0x064  W  InterruptACK     Interrupt acknowledge register
  * 0x070  RW Status           Device status register
  *
  * 0x100+ RW                  Device-specific configuration space
