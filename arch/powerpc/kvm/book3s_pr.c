cf281491ba4732bced379d125900a57e391771c9
Paul Mackerras <paulus@samba.org>
2013-02-04 18:11:44 +0000

    powerpc/kvm/book3s_pr: Fix compilation on 32-bit machines
    Commit a413f474a0 ("powerpc: Disable relocation on exceptions whenever
    PR KVM is active") added calls to pSeries_disable_reloc_on_exc() and
    pSeries_enable_reloc_on_exc() to book3s_pr.c, and added declarations
    of those functions to <asm/hvcall.h>, but didn't add an include of
    <asm/hvcall.h> to book3s_pr.c.  64-bit kernels seem to get hvcall.h
    included via some other path, but 32-bit kernels fail to compile with:
    
    arch/powerpc/kvm/book3s_pr.c: In function ‘kvmppc_core_init_vm’:
    arch/powerpc/kvm/book3s_pr.c:1300:4: error: implicit declaration of function ‘pSeries_disable_reloc_on_exc’ [-Werror=implicit-function-declaration]
    arch/powerpc/kvm/book3s_pr.c: In function ‘kvmppc_core_destroy_vm’:
    arch/powerpc/kvm/book3s_pr.c:1316:4: error: implicit declaration of function ‘pSeries_enable_reloc_on_exc’ [-Werror=implicit-function-declaration]
    cc1: all warnings being treated as errors
    make[2]: *** [arch/powerpc/kvm/book3s_pr.o] Error 1
    make[1]: *** [arch/powerpc/kvm] Error 2
    make: *** [sub-make] Error 2
    
    This fixes it by adding an include of hvcall.h.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kvm/book3s_pr.c
+++ arch/powerpc/kvm/book3s_pr.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mmu_context.h>
 #include <asm/switch_to.h>
 #include <asm/firmware.h>
+#include <asm/hvcall.h>
 #include <linux/gfp.h>
 #include <linux/sched.h>
 #include <linux/vmalloc.h>
