0eae875fe5e96024e189921320c17d0a5854a1d0
Roger Tseng <rogerable@realtek.com>
2013-02-04 15:45:59 +0800

    mfd: rtsx: Support RTS5227
    Support new model RTS5227.
    
    Signed-off-by: Roger Tseng <rogerable@realtek.com>
    Reviewed-by: Wei WANG <wei_wang@realsil.com.cn>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/rtsx_pcr.h
+++ drivers/mfd/rtsx_pcr.h
@@ -32,5 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void rts5209_init_params(struct rtsx_pcr *pcr);
 void rts5229_init_params(struct rtsx_pcr *pcr);
 void rtl8411_init_params(struct rtsx_pcr *pcr);
+void rts5227_init_params(struct rtsx_pcr *pcr);
 
 #endif
