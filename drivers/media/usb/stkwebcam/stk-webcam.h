bfdea945e9891700a08f6f992c5e96a726d13f60
Hans Verkuil <hans.verkuil@cisco.com>
2013-02-04 08:53:11 -0300

    [media] stk-webcam: convert to the control framework
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Tested-by: Arvydas Sidorenko <asido4@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/stkwebcam/stk-webcam.h
+++ drivers/media/usb/stkwebcam/stk-webcam.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/usb.h>
 #include <media/v4l2-device.h>
+#include <media/v4l2-ctrls.h>
 #include <media/v4l2-common.h>
 
 #define DRIVER_VERSION		"v0.0.1"
@@ -61,7 +62,6 @@ enum stk_mode {MODE_VGA, MODE_SXGA, MODE_CIF, MODE_QVGA, MODE_QCIF};
 
 struct stk_video {
 	enum stk_mode mode;
-	int brightness;
 	__u32 palette;
 	int hflip;
 	int vflip;
@@ -94,6 +94,7 @@ struct regval {
 
 struct stk_camera {
 	struct v4l2_device v4l2_dev;
+	struct v4l2_ctrl_handler hdl;
 	struct video_device vdev;
 	struct usb_device *udev;
 	struct usb_interface *interface;
