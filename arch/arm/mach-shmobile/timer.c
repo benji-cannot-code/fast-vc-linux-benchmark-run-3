7f24e59886332a0ccc4441086601ac19fedaf2a7
Magnus Damm <damm@opensource.se>
2013-02-05 16:30:49 +0900

    ARM: shmobile: Register ARM architected timer
    Register ARM architected timer by default for all mach-shmobile
    systems using late timer.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/timer.c
+++ arch/arm/mach-shmobile/timer.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/platform_device.h>
 #include <linux/delay.h>
+#include <asm/arch_timer.h>
 #include <asm/mach/time.h>
 #include <asm/smp_twd.h>
 
@@ -63,4 +64,6 @@ void __init shmobile_earlytimer_init(void)
 
 void __init shmobile_timer_init(void)
 {
+	arch_timer_of_register();
+	arch_timer_sched_clock_init();
 }
