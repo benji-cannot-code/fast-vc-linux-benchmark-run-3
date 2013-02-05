cae4679e42ea22169ef292a44480686e781fc002
Milo(Woogyom) Kim <milo.kim@ti.com>
2013-02-05 19:08:40 +0900

    leds-lp55xx: use lp55xx_set_brightness()
     lp5521_set_brightness() and lp5523_set_brightness() are replaced with
     common function, lp55xx_set_brightness().
     This function is invoked when the brightness of each LED channel is updated.
     LP5521 and LP5523 have different register address for the brightness control,
     so this work is done by chip specific brightness_work_fn().
    
     lp5521/5523_led_brightness_work():
     use lp55xx_led and lp55xx_chip data structure.
     use lp55xx write function.
    
    Signed-off-by: Milo(Woogyom) Kim <milo.kim@ti.com>
    Signed-off-by: Bryan Wu <cooloney@gmail.com>

--- drivers/leds/leds-lp55xx-common.c
+++ drivers/leds/leds-lp55xx-common.c
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "leds-lp55xx-common.h"
 
+static struct lp55xx_led *cdev_to_lp55xx_led(struct led_classdev *cdev)
+{
+	return container_of(cdev, struct lp55xx_led, cdev);
+}
+
 static void lp55xx_reset_device(struct lp55xx_chip *chip)
 {
 	struct lp55xx_device_config *cfg = chip->cfg;
@@ -75,6 +80,10 @@ static struct attribute_group lp55xx_led_attr_group = {
 static void lp55xx_set_brightness(struct led_classdev *cdev,
 			     enum led_brightness brightness)
 {
+	struct lp55xx_led *led = cdev_to_lp55xx_led(cdev);
+
+	led->brightness = (u8)brightness;
+	schedule_work(&led->brightness_work);
 }
 
 static int lp55xx_init_led(struct lp55xx_led *led,
