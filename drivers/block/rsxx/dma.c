6bb3b65fde0c6b4ca060e6af4a85be40d659937c
Jens Axboe <axboe@kernel.dk>
2013-02-07 10:04:21 +0100

    rsxx: add slab.h include to dma.c
    kbuild test robot says:
    
    tree:   git://git.kernel.dk/linux-block.git for-3.9/drivers
    head:   1262e24a59a052f9a98383e47e7c903712490d5c
    commit: 8722ff8cdbfac9c1b20e67bb067b455c48cb8e93 [6/8] block: IBM RamSan 70/80 device driver
    config: make ARCH=alpha allyesconfig
    
    All error/warnings:
    
       drivers/block/rsxx/dma.c: In function 'rsxx_complete_dma':
    >> drivers/block/rsxx/dma.c:251:2: error: implicit declaration of function 'kmem_cache_free' [-Werror=implicit-function-declaration]
       drivers/block/rsxx/dma.c: In function 'rsxx_queue_discard':
    >> drivers/block/rsxx/dma.c:567:2: error: implicit declaration of function 'kmem_cache_alloc' [-Werror=implicit-function-declaration]
    >> drivers/block/rsxx/dma.c:567:6: warning: assignment makes pointer from integer without a cast [enabled by default]
       drivers/block/rsxx/dma.c: In function 'rsxx_queue_dma':
    >> drivers/block/rsxx/dma.c:601:6: warning: assignment makes pointer from integer without a cast [enabled by default]
       drivers/block/rsxx/dma.c: In function 'rsxx_dma_init':
    >> drivers/block/rsxx/dma.c:985:2: error: implicit declaration of function 'KMEM_CACHE' [-Werror=implicit-function-declaration]
    >> drivers/block/rsxx/dma.c:985:29: error: 'rsxx_dma' undeclared (first use in this function)
       drivers/block/rsxx/dma.c:985:29: note: each undeclared identifier is reported only once for each function it appears in
    >> drivers/block/rsxx/dma.c:985:39: error: 'SLAB_HWCACHE_ALIGN' undeclared (first use in this function)
       drivers/block/rsxx/dma.c: In function 'rsxx_dma_cleanup':
    >> drivers/block/rsxx/dma.c:995:2: error: implicit declaration of function 'kmem_cache_destroy' [-Werror=implicit-function-declaration]
       cc1: some warnings being treated as errors
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/rsxx/dma.c
+++ drivers/block/rsxx/dma.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+#include <linux/slab.h>
 #include "rsxx_priv.h"
 
 struct rsxx_dma {
