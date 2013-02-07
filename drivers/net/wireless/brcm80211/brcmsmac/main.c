2e18448931627a1612f95827c9f54604635bcf87
Tim Gardner <tim.gardner@canonical.com>
2013-02-07 12:35:35 -0700

    brcmsmac: fix u16 overflow warning
    DOT11_MIN_BEACON_PERIOD and DOT11_MAX_BEACON_PERIOD are
    superfluous. The only invalid beacon period is 0. Comparing
    a 16 bit quantity to 0xffff also causes a compile warning:
    
    drivers/net/wireless/brcm80211/brcmsmac/main.c:5560 brcms_c_set_beacon_period()
     warn: impossible condition '(period > 65535) => (0-65535 > 65535)'
    
    Observed from smatch analysis.
    
    Cc: Brett Rudley <brudley@broadcom.com>
    Cc: Arend van Spriel <arend@broadcom.com>
    Cc: "Franky (Zhenhui) Lin" <frankyl@broadcom.com>
    Cc: Hante Meuleman <meuleman@broadcom.com>
    Cc: "John W. Linville" <linville@tuxdriver.com>
    Cc: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Cc: Seth Forshee <seth.forshee@canonical.com>
    Cc: Hauke Mehrtens <hauke@hauke-m.de>
    Cc: linux-wireless@vger.kernel.org
    Cc: brcm80211-dev-list@broadcom.com
    Cc: netdev@vger.kernel.org
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmsmac/main.c
+++ drivers/net/wireless/brcm80211/brcmsmac/main.c
@@ -102,8 +102,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DOT11_RTS_LEN			16
 #define DOT11_CTS_LEN			10
 #define DOT11_BA_BITMAP_LEN		128
-#define DOT11_MIN_BEACON_PERIOD		1
-#define DOT11_MAX_BEACON_PERIOD		0xFFFF
 #define DOT11_MAXNUMFRAGS		16
 #define DOT11_MAX_FRAG_LEN		2346
 
@@ -5556,8 +5554,7 @@ int brcms_c_set_rateset(struct brcms_c_info *wlc, struct brcm_rateset *rs)
 
 int brcms_c_set_beacon_period(struct brcms_c_info *wlc, u16 period)
 {
-	if (period < DOT11_MIN_BEACON_PERIOD ||
-	    period > DOT11_MAX_BEACON_PERIOD)
+	if (period == 0)
 		return -EINVAL;
 
 	wlc->default_bss->beacon_period = period;
