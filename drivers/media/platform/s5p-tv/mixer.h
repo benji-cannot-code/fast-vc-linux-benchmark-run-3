8cff2089bfe774f7461c6dd68aa5551fb05073ba
Sachin Kamat <sachin.kamat@linaro.org>
2013-02-07 02:55:54 -0300

    [media] s5p-tv: Include missing irqreturn.h header
    Without this patch we get the following compilation errors:
    drivers/media/platform/s5p-tv/mixer.h:345:13: error: Expected ; at end of declaration
    drivers/media/platform/s5p-tv/mixer.h:345:13: error: got mxr_irq_handler
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/s5p-tv/mixer.h
+++ drivers/media/platform/s5p-tv/mixer.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #include <linux/fb.h>
+#include <linux/irqreturn.h>
 #include <linux/kernel.h>
 #include <linux/spinlock.h>
 #include <linux/wait.h>
