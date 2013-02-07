8b9d0cec91fb07768eb62b293a6d16f4f0c3350e
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-02-07 13:27:10 -0700

    staging: comedi: ni_daq_dio24: add back missing 'if (ret)'
    The 'if (ret)' after calling comedi_pcmcia_enable() was accidentally
    removed in:
    
    Commit: 573a964882065f9b2cb71db8a225ba067b4b5b7a
    staging: comedi: ni_daq_dio24: use comedi_pcmcia_{enable,disable}
    
    Put if back so that dio24_auto_attach() can finish attaching to
    the board after enabling the pcmcia device.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_daq_dio24.c
+++ drivers/staging/comedi/drivers/ni_daq_dio24.c
@@ -57,6 +57,7 @@ static int dio24_auto_attach(struct comedi_device *dev,
 
 	link->config_flags |= CONF_AUTO_SET_IO;
 	ret = comedi_pcmcia_enable(dev, NULL);
+	if (ret)
 		return ret;
 	dev->iobase = link->resource[0]->start;
 
