c1e6b239f3bf4bc5c161b02b0ae586ea7dde2ac1
Takashi Iwai <tiwai@suse.de>
2013-02-07 09:58:11 +0100

    ALSA: hda - Enable loopback accounts for CONFIG_PM=n, too
    The loopback list is referred by the VIA codec driver no matter
    whether CONFIG_PM is set or not, thus we need to enable it always.
    Otherwise it gets compile errors.
    
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_generic.c
+++ sound/pci/hda/hda_generic.c
@@ -2484,7 +2484,6 @@ static int create_in_jack_mode(struct hda_codec *codec, hda_nid_t pin)
  * Parse input paths
  */
 
-#ifdef CONFIG_PM
 /* add the powersave loopback-list entry */
 static void add_loopback_list(struct hda_gen_spec *spec, hda_nid_t mix, int idx)
 {
@@ -2499,9 +2498,6 @@ static void add_loopback_list(struct hda_gen_spec *spec, hda_nid_t mix, int idx)
 	spec->num_loopbacks++;
 	spec->loopback.amplist = spec->loopback_list;
 }
-#else
-#define add_loopback_list(spec, mix, idx) /* NOP */
-#endif
 
 /* create input playback/capture controls for the given pin */
 static int new_analog_input(struct hda_codec *codec, int input_idx,
