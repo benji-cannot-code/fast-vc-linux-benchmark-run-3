86c3f217d4116dc4a5b42ae33929bc361ba8bbc5
Paul Bolle <pebolle@tiscali.nl>
2013-02-08 01:57:55 -0500

    [SCSI] qla2xxx: silence two GCC warnings.
    Compiling qla_gs.o (part of the qla2xxx module) triggers two GCC
    warnings:
        drivers/scsi/qla2xxx/qla_gs.c: In function ‘qla2x00_fdmi_rhba’:
        drivers/scsi/qla2xxx/qla_gs.c:1339:7: warning: array subscript is above array bounds [-Warray-bounds]
        drivers/scsi/qla2xxx/qla_gs.c: In function ‘qla2x00_fdmi_register’:
        drivers/scsi/qla2xxx/qla_gs.c:1663:15: warning: array subscript is above array bounds [-Warray-bounds]
    
    It seems that the sequence of a strcpy followed by a strlen confuses GCC
    when it is keeping track of array bounds here. (It is not clear to me
    which array triggers this warning and by how much GCC thinks the
    subscript is above its bounds. Neither is it clear to me why comparable
    code in these two functions doesn't trigger this warning.)
    
    An easy way to silence these warnings is to use preprocessor macros and
    strncpy, as that apparently gives GCC enough information to keep track
    of array bounds.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Saurav Kashyap <saurav.kashyap@qlogic.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "qla_nx.h"
 #define QLA2XXX_DRIVER_NAME	"qla2xxx"
 #define QLA2XXX_APIDEV		"ql2xapidev"
+#define QLA2XXX_MANUFACTURER	"QLogic Corporation"
 
 /*
  * We have MAILBOX_REGISTER_COUNT sized arrays in a few places,
