a6c09539375aa79a552825c98808e9cced60564f
Hante Meuleman <meuleman@broadcom.com>
2013-02-08 15:53:49 +0100

    brcmfmac: P2P action frame tx.
    With this patch it is possible to send action frames.
    
    Reviewed-by: Arend Van Spriel <arend@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Hante Meuleman <meuleman@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/fwil_types.h
+++ drivers/net/wireless/brcm80211/brcmfmac/fwil_types.h
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/if_ether.h>
 
+
+#define BRCMF_FIL_ACTION_FRAME_SIZE	1800
+
+
 enum brcmf_fil_p2p_if_types {
 	BRCMF_FIL_P2P_IF_CLIENT,
 	BRCMF_FIL_P2P_IF_GO,
@@ -38,6 +42,21 @@ struct brcmf_fil_chan_info_le {
 	__le32 hw_channel;
 	__le32 target_channel;
 	__le32 scan_channel;
+};
+
+struct brcmf_fil_action_frame_le {
+	u8	da[ETH_ALEN];
+	__le16	len;
+	__le32	packet_id;
+	u8	data[BRCMF_FIL_ACTION_FRAME_SIZE];
+};
+
+struct brcmf_fil_af_params_le {
+	__le32					channel;
+	__le32					dwell_time;
+	u8					bssid[ETH_ALEN];
+	u8					pad[2];
+	struct brcmf_fil_action_frame_le	action_frame;
 };
 
 struct brcmf_fil_bss_enable_le {
