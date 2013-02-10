3612c7d86f63edc7ebd2456732a97d279719528e
Tony Lindgren <tony@atomide.com>
2013-02-09 19:20:57 -0800

    ARM: OMAP2+: Make sure files with omap initcalls include soc.h
    Looks like there are few more places that I missed that can cause
    compiler warnings. After grepping for omap initcall, all files
    needing soc.h should now have it.
    
    Reported-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-omap2/smartreflex-class3.c
+++ arch/arm/mach-omap2/smartreflex-class3.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/power/smartreflex.h>
+#include "soc.h"
 #include "voltage.h"
 
 static int sr_class3_enable(struct omap_sr *sr)
