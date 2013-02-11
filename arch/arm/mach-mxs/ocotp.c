8d57f3e7c7635f43c1a21309cf219290e0adb106
Fabio Estevam <fabio.estevam@freescale.com>
2013-02-11 12:01:48 -0200

    ARM: mxs: ocotp: Fix sparse warning
    Include <mach/common.h> header to fix the following sparse warning:
    
    arch/arm/mach-mxs/ocotp.c:33:11: warning: symbol 'mxs_get_ocotp' was not declared. Should it be static?
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-mxs/ocotp.c
+++ arch/arm/mach-mxs/ocotp.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>	/* for cpu_relax() */
 
 #include <mach/mxs.h>
+#include <mach/common.h>
 
 #define OCOTP_WORD_OFFSET		0x20
 #define OCOTP_WORD_COUNT		0x20
