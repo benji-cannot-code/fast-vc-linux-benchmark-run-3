5d1da67a21219ec242eefb7890d637ccd4a6c4bf
Tomi Valkeinen <tomi.valkeinen@ti.com>
2013-02-12 16:45:23 +0530

    OMAPDSS: n8x0: remove platform backlight calls
    The n8x0 panel contains support to call platform backlight functions.
    These are not used by any board, and can be removed.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Archit Taneja <archit@ti.com>

--- include/video/omap-panel-data.h
+++ include/video/omap-panel-data.h
@@ -57,8 +57,6 @@ struct panel_n8x0_data {
 	void (*platform_disable)(struct omap_dss_device *dssdev);
 	int panel_reset;
 	int ctrl_pwrdown;
-
-	int (*set_backlight)(struct omap_dss_device *dssdev, int level);
 };
 
 /**
