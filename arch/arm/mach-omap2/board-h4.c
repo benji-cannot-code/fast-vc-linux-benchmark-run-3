edebd0e1a0f74a08dc3d6593cfd6e8c7a7424629
Archit Taneja <archit@ti.com>
2013-02-12 16:46:46 +0530

    OMAPDSS: panels: keep platform data of all panels in a single header
    Structs for platform data of omapdss panels are found in headers in the
    'include/video/' path. Board files populate these structs with platform
    specific values, and the panel driver uses these to configure the panel.
    
    Currently, each panel has it's own header in the above path. Move all the
    omapdss panel platform data structs to a single header omap-panel-data.h.
    This is useful because:
    
    - All other omapdss panel drivers will be modified to use platform data. This
      would lead to a lot of panel headers usable only by omapdss. A lot of these
      platform data structs are trivial, and don't really need a separate header.
    - Platform data would be eventually removed, and platform information would be
      passed via device tree. Therefore, omapdss panel platform data structs are
      temporary, and will be easier to remove if they are all in the same header.
    - All board files will have to include the same header to configure a panel's
      platform data, that makes the board files more consistent.
    
    Signed-off-by: Archit Taneja <archit@ti.com>

--- arch/arm/mach-omap2/board-h4.c
+++ arch/arm/mach-omap2/board-h4.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/map.h>
 
 #include <video/omapdss.h>
-#include <video/omap-panel-generic-dpi.h>
+#include <video/omap-panel-data.h>
 
 #include "common.h"
 #include "mux.h"
--- drivers/video/omap2/displays/panel-taal.c
+++ drivers/video/omap2/displays/panel-taal.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 
 #include <video/omapdss.h>
-#include <video/omap-panel-nokia-dsi.h>
+#include <video/omap-panel-data.h>
 #include <video/mipi_display.h>
 
 /* DSI Virtual channel. Hardcoded for now. */
