bff1c2920592d675e1108fa8d055e7cd69be34ed
Hiral Patel <hiralpat@cisco.com>
2013-02-12 17:00:59 -0800

    [SCSI] fnic: Fix SGEs limit
    Driver allows IOs with more SGEs than max SGEs supported by Palo. The current
    max SGEs supported by the fnic driver is 1024. The current register settings
    on Palo supports a max of 256 only. Palo would return any IO with more than
    256 SGEs with an error indicating INVALID_SGLS. Fnic driver should limit the
    max supported SGLs in the driver to 256 to avoid this error.
    
    Signed-off-by: Sesidhar Baddela <sebaddel@cisco.com>
    Signed-off-by: Hiral Patel <hiralpat@cisco.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/fnic/fnic_io.h
+++ drivers/scsi/fnic/fnic_io.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/fc/fc_fcp.h>
 
 #define FNIC_DFLT_SG_DESC_CNT  32
-#define FNIC_MAX_SG_DESC_CNT        1024    /* Maximum descriptors per sgl */
+#define FNIC_MAX_SG_DESC_CNT        256     /* Maximum descriptors per sgl */
 #define FNIC_SG_DESC_ALIGN          16      /* Descriptor address alignment */
 
 struct host_sg_desc {
