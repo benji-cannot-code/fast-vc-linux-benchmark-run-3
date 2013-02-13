bb5e2fcc846080ad96228f8935d995ddb86cc934
Lee Jones <lee.jones@linaro.org>
2013-02-13 11:39:19 +0000

    ab8500-bm: Charge only mode fixes for the ab9540
    Fix for charging not getting enabled in charge only mode by
    external charger.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/power/abx500_chargalg.c
+++ drivers/power/abx500_chargalg.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mfd/abx500.h>
 #include <linux/mfd/abx500/ux500_chargalg.h>
 #include <linux/mfd/abx500/ab8500-bm.h>
+#include <linux/notifier.h>
 
 /* Watchdog kick interval */
 #define CHG_WD_INTERVAL			(6 * HZ)
@@ -243,6 +244,9 @@ struct abx500_chargalg {
 	struct timer_list maintenance_timer;
 	struct kobject chargalg_kobject;
 };
+
+/*External charger prepare notifier*/
+BLOCKING_NOTIFIER_HEAD(charger_notifier_list);
 
 /* Main battery properties */
 static enum power_supply_property abx500_chargalg_props[] = {
@@ -504,6 +508,8 @@ static int abx500_chargalg_kick_watchdog(struct abx500_chargalg *di)
 static int abx500_chargalg_ac_en(struct abx500_chargalg *di, int enable,
 	int vset, int iset)
 {
+	static int abx500_chargalg_ex_ac_enable_toggle;
+
 	if (!di->ac_chg || !di->ac_chg->ops.enable)
 		return -ENXIO;
 
@@ -515,6 +521,14 @@ static int abx500_chargalg_ac_en(struct abx500_chargalg *di, int enable,
 
 	di->chg_info.ac_iset = iset;
 	di->chg_info.ac_vset = vset;
+
+	/* Enable external charger */
+	if (enable && di->ac_chg->external &&
+	    !abx500_chargalg_ex_ac_enable_toggle) {
+		blocking_notifier_call_chain(&charger_notifier_list,
+					     0, di->dev);
+		abx500_chargalg_ex_ac_enable_toggle++;
+	}
 
 	return di->ac_chg->ops.enable(di->ac_chg, enable, vset, iset);
 }
--- include/linux/mfd/abx500/ux500_chargalg.h
+++ include/linux/mfd/abx500/ux500_chargalg.h
@@ -42,4 +42,6 @@ struct ux500_charger {
 	bool external;
 };
 
+extern struct blocking_notifier_head charger_notifier_list;
+
 #endif
