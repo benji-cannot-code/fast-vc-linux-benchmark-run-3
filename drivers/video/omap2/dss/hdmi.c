8398e5277c0237566f02e6023803ba0d5a5729b9
Tomi Valkeinen <tomi.valkeinen@ti.com>
2013-02-13 12:17:43 +0200

    OMAPDSS: HDMI: init output earlier
    Move hdmi driver's output initialization a bit earlier, so that it
    happens before hdmi panel init. In the future the hdmi panel will depend
    on the output being ready.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Archit Taneja <archit@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -1093,6 +1093,8 @@ static int __init omapdss_hdmihw_probe(struct platform_device *pdev)
 	hdmi.ip_data.pll_offset = HDMI_PLLCTRL;
 	hdmi.ip_data.phy_offset = HDMI_PHY;
 
+	hdmi_init_output(pdev);
+
 	r = hdmi_panel_init();
 	if (r) {
 		DSSERR("can't init panel\n");
@@ -1100,8 +1102,6 @@ static int __init omapdss_hdmihw_probe(struct platform_device *pdev)
 	}
 
 	dss_debugfs_create_file("hdmi", hdmi_dump_regs);
-
-	hdmi_init_output(pdev);
 
 	hdmi_probe_pdata(pdev);
 
