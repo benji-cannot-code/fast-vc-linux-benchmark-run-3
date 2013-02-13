33fbf478282f4f1f43f8d6db75c5ec5e26605ea0
Michael Neuling <mikey@neuling.org>
2013-02-13 16:21:30 +0000

    powerpc: Add new instructions for transactional memory
    Here we define the new instructions we need for transactional memory in the
    kernel.  This is so we can support compiling with binutils that don't support
    the new transactional memory instructions.
    
    Transactional memory results in two sets of architected state (GPRs/VSRs
    etc).
    
    treclaim allows us to read the checkpointed state (from the tbegin) so that we
    can store it away on a context switch.  It does this by overwriting the exiting
    architected state, so you have to save that away before you treclaim.  treclaim
    will also abort a transaction, so you can give a register value which contains
    an abort reason.
    
    trecheckpoint allows us to inject into the checkpointed state as if it were at
    the tbegin.  It does this by copying the current architected state into the
    checkpointed state.
    
    Signed-off-by: Matt Evans <matt@ozlabs.org>
    Signed-off-by: Michael Neuling <mikey@neuling.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/ppc-opcode.h
+++ arch/powerpc/include/asm/ppc-opcode.h
@@ -130,6 +130,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PPC_INST_TLBSRX_DOT		0x7c0006a5
 #define PPC_INST_XXLOR			0xf0000510
 #define PPC_INST_XVCPSGNDP		0xf0000780
+#define PPC_INST_TRECHKPT		0x7c0007dd
+#define PPC_INST_TRECLAIM		0x7c00075d
+#define PPC_INST_TABORT			0x7c00071d
 
 #define PPC_INST_NAP			0x4c000364
 #define PPC_INST_SLEEP			0x4c0003a4
@@ -294,5 +297,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PPC_NAP			stringify_in_c(.long PPC_INST_NAP)
 #define PPC_SLEEP		stringify_in_c(.long PPC_INST_SLEEP)
+
+/* Transactional memory instructions */
+#define TRECHKPT		stringify_in_c(.long PPC_INST_TRECHKPT)
+#define TRECLAIM(r)		stringify_in_c(.long PPC_INST_TRECLAIM \
+					       | __PPC_RA(r))
+#define TABORT(r)		stringify_in_c(.long PPC_INST_TABORT \
+					       | __PPC_RA(r))
 
 #endif /* _ASM_POWERPC_PPC_OPCODE_H */
