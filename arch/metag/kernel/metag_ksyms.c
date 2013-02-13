ee95fafe3e9cc5d03e14d06935da8bb4e4a9e799
James Hogan <james.hogan@imgtec.com>
2013-02-13 13:30:15 +0000

    metag: cleanup metag_ksyms.c includes
    Minimise metag_ksyms.c includes to directly include the <asm/*.h> files
    that declare a particular symbol, and not include any unnecessary ones.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>

--- arch/metag/kernel/metag_ksyms.c
+++ arch/metag/kernel/metag_ksyms.c
@@ -1,13 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/export.h>
-#include <linux/linkage.h>
-#include <linux/string.h>
-#include <linux/mm.h>
-#include <linux/interrupt.h>
-#include <linux/hardirq.h>
 
-#include <asm/checksum.h>
+#include <asm/div64.h>
 #include <asm/ftrace.h>
+#include <asm/page.h>
+#include <asm/string.h>
 #include <asm/tbx.h>
 
 EXPORT_SYMBOL(clear_page);
