b8489b153d51d3eb615ad7038d62e20c7053f39d
Peter Huewe <peterhuewe@gmx.de>
2013-02-15 21:17:22 +0100

    staging/ozwpan: Mark local functions as static (fix sparse warnings)
    sparse complains about the following functions
    
    ozhcd.c:330:20: warning: symbol 'oz_uncancel_urb' was not declared.  Should it be static?
    ozhcd.c:420:6: warning: symbol 'oz_complete_buffered_urb' was not declared. Should it be static?
    ozmain.c:25:6: warning: symbol 'g_net_dev' was not declared. Should it be static?
    ozevent.c:95:5: warning: symbol 'oz_events_open' was not declared.  Should it be static?
    ozevent.c:110:5: warning: symbol 'oz_events_release' was not declared.  Should it be static?
    ozevent.c:121:9: warning: symbol 'oz_events_read' was not declared.  Should it be static?
    ozevent.c:160:30: warning: symbol 'oz_events_fops' was not declared.  Should it be static?
    ozcdev.c:47:14: warning: symbol 'g_oz_class' was not declared. Should it be static?
    ozcdev.c:74:5: warning: symbol 'oz_cdev_open' was not declared. Should it be static?
    ozcdev.c:86:5: warning: symbol 'oz_cdev_release' was not declared.  Should it be static?
    ozcdev.c:94:9: warning: symbol 'oz_cdev_read' was not declared. Should it be static?
    ozcdev.c:146:9: warning: symbol 'oz_cdev_write' was not declared. Should it be static?
    ozcdev.c:236:6: warning: symbol 'oz_cdev_ioctl' was not declared. Should it be static?
    ozcdev.c:300:14: warning: symbol 'oz_cdev_poll' was not declared. Should it be static?
    ozcdev.c:321:30: warning: symbol 'oz_fops' was not declared. Should it be static?
    ozproto.c:958:6: warning: context imbalance in 'oz_polling_lock_bh' - wrong count at exit
    ozproto.c:964:6: warning: context imbalance in 'oz_polling_unlock_bh' - unexpected unlock
    ozusbsvc1.c:308:6: warning: symbol 'oz_usb_handle_ep_data' was not declared. Should it be static?
    ozpd.c:410:6: warning: symbol 'oz_set_more_bit' was not declared. Should it be static?
    ozpd.c:418:6: warning: symbol 'oz_set_last_pkt_nb' was not declared.  Should it be static?
    
    -> add static keyword to silence the warning and make sparse happy.
    
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ozwpan/ozcdev.c
+++ drivers/staging/ozwpan/ozcdev.c
@@ -45,7 +45,7 @@ struct oz_serial_ctx {
 /*------------------------------------------------------------------------------
  */
 static struct oz_cdev g_cdev;
-struct class *g_oz_class;
+static struct class *g_oz_class;
 /*------------------------------------------------------------------------------
  * Context: process and softirq
  */
@@ -72,7 +72,7 @@ static void oz_cdev_release_ctx(struct oz_serial_ctx *ctx)
 /*------------------------------------------------------------------------------
  * Context: process
  */
-int oz_cdev_open(struct inode *inode, struct file *filp)
+static int oz_cdev_open(struct inode *inode, struct file *filp)
 {
 	struct oz_cdev *dev;
 	oz_trace("oz_cdev_open()\n");
@@ -84,7 +84,7 @@ int oz_cdev_open(struct inode *inode, struct file *filp)
 /*------------------------------------------------------------------------------
  * Context: process
  */
-int oz_cdev_release(struct inode *inode, struct file *filp)
+static int oz_cdev_release(struct inode *inode, struct file *filp)
 {
 	oz_trace("oz_cdev_release()\n");
 	return 0;
@@ -92,7 +92,7 @@ int oz_cdev_release(struct inode *inode, struct file *filp)
 /*------------------------------------------------------------------------------
  * Context: process
  */
-ssize_t oz_cdev_read(struct file *filp, char __user *buf, size_t count,
+static ssize_t oz_cdev_read(struct file *filp, char __user *buf, size_t count,
 		loff_t *fpos)
 {
 	int n;
@@ -144,8 +144,8 @@ out2:
 /*------------------------------------------------------------------------------
  * Context: process
  */
-ssize_t oz_cdev_write(struct file *filp, const char __user *buf, size_t count,
-		loff_t *fpos)
+static ssize_t oz_cdev_write(struct file *filp, const char __user *buf,
+		size_t count, loff_t *fpos)
 {
 	struct oz_pd *pd;
 	struct oz_elt_buf *eb;
@@ -234,7 +234,8 @@ static int oz_set_active_pd(u8 *addr)
 /*------------------------------------------------------------------------------
  * Context: process
  */
-long oz_cdev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+static long oz_cdev_ioctl(struct file *filp, unsigned int cmd,
+			  unsigned long arg)
 {
 	int rc = 0;
 	if (_IOC_TYPE(cmd) != OZ_IOCTL_MAGIC)
@@ -298,7 +299,7 @@ long oz_cdev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 /*------------------------------------------------------------------------------
  * Context: process
  */
-unsigned int oz_cdev_poll(struct file *filp, poll_table *wait)
+static unsigned int oz_cdev_poll(struct file *filp, poll_table *wait)
 {
 	unsigned int ret = 0;
 	struct oz_cdev *dev = filp->private_data;
@@ -319,7 +320,7 @@ unsigned int oz_cdev_poll(struct file *filp, poll_table *wait)
 }
 /*------------------------------------------------------------------------------
  */
-const struct file_operations oz_fops = {
+static const struct file_operations oz_fops = {
 	.owner =	THIS_MODULE,
 	.open =		oz_cdev_open,
 	.release =	oz_cdev_release,
