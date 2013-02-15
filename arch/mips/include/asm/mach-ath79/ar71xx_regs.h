0f35bbe6051000e071717ae6c91ac24a7d5a2c25
Gabor Juhos <juhosg@openwrt.org>
2013-02-15 13:38:19 +0000

    MIPS: ath79: add GPIO setup code for the QCA955X SoCs
    The existing code can handle the GPIO controller of
    the QCA955x SoCs. Add a minimal glue code to make it
    working.
    
    Cc: Rodriguez, Luis <rodrigue@qca.qualcomm.com>
    Cc: Giori, Kathy <kgiori@qca.qualcomm.com>
    Cc: QCA Linux Team <qca-linux-team@qca.qualcomm.com>
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/4947/
    Signed-off-by: John Crispin <blogic@openwrt.org>

--- arch/mips/include/asm/mach-ath79/ar71xx_regs.h
+++ arch/mips/include/asm/mach-ath79/ar71xx_regs.h
@@ -511,6 +511,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AR913X_GPIO_COUNT		22
 #define AR933X_GPIO_COUNT		30
 #define AR934X_GPIO_COUNT		23
+#define QCA955X_GPIO_COUNT		24
 
 /*
  * SRIF block
