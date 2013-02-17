f2d1d4674cb9d11ba038a455d393f725be263249
Frank Schaefer <fschaefer.oss@googlemail.com>
2013-02-17 09:41:29 -0300

    [media] bttv: move fini_bttv_i2c() from bttv-input.c to bttv-i2c.c
    Like init_bttv_i2c(), fini_bttv_i2c() belongs to bttv-i2c.c.
    
    Signed-off-by: Frank Schäfer <fschaefer.oss@googlemail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/pci/bt8xx/bttvp.h
+++ drivers/media/pci/bt8xx/bttvp.h
@@ -301,6 +301,10 @@ extern int no_overlay;
 /* bttv-input.c                                               */
 
 extern void init_bttv_i2c_ir(struct bttv *btv);
+
+/* ---------------------------------------------------------- */
+/* bttv-i2c.c                                                 */
+extern int init_bttv_i2c(struct bttv *btv);
 extern int fini_bttv_i2c(struct bttv *btv);
 
 /* ---------------------------------------------------------- */
@@ -311,7 +315,6 @@ extern unsigned int bttv_verbose;
 extern unsigned int bttv_debug;
 extern unsigned int bttv_gpio;
 extern void bttv_gpio_tracking(struct bttv *btv, char *comment);
-extern int init_bttv_i2c(struct bttv *btv);
 
 #define dprintk(fmt, ...)			\
 do {						\
