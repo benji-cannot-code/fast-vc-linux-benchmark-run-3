a197dc576f029aba85afc56a87954dac4c6d8826
Andy King <acking@vmware.com>
2013-02-18 06:04:13 +0000

    VSOCK: Don't reject PF_VSOCK protocol
    Allow our own family as the protocol value for socket creation.
    
    Reported-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Andy King <acking@vmware.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/vmw_vsock/af_vsock.c
+++ net/vmw_vsock/af_vsock.c
@@ -1861,7 +1861,7 @@ static int vsock_create(struct net *net, struct socket *sock,
 	if (!sock)
 		return -EINVAL;
 
-	if (protocol)
+	if (protocol && protocol != PF_VSOCK)
 		return -EPROTONOSUPPORT;
 
 	switch (sock->type) {
