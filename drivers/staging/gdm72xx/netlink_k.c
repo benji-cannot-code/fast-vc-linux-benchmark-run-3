b1a1be6bb237c06170d8076eccbea56a057c0591
Peter Huewe <peterhuewe@gmx.de>
2013-02-19 18:50:18 +0100

    staging/gdm72xx: Include corresponding header file (fix sparse warning)
    sdio_boot.c and netlink_k.c both have a corresponding header file with
    their function prototypes but fail to include them, which leads to the
    following sparse warnings:
    sdio_boot.c:135:5: warning: symbol 'sdio_boot' was not declared. Should it be static?
    netlink_k.c:89:13: warning: symbol 'netlink_init' was not declared. Should it be static?
    netlink_k.c:109:6: warning: symbol 'netlink_exit' was not declared. Should it be static?
    netlink_k.c:114:5: warning: symbol 'netlink_send' was not declared. Should it be static?
    
    -> Add the include files and silence the warning
    
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm72xx/netlink_k.c
+++ drivers/staging/gdm72xx/netlink_k.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netlink.h>
 #include <asm/byteorder.h>
 #include <net/sock.h>
+#include "netlink_k.h"
 
 #if !defined(NLMSG_HDRLEN)
 #define NLMSG_HDRLEN	 ((int) NLMSG_ALIGN(sizeof(struct nlmsghdr)))
