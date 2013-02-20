bfafb8ba3ce12b310c0baadeefd4d1d0ca75c48c
James Hogan <james.hogan@imgtec.com>
2013-02-20 13:59:13 +0000

    metag: copy devicetree to non-init memory
    Make a copy of the device tree blob in non-init memory. It is required
    when using built-in device tree files that the platform code copies the
    blob to non-init memory prior to calling unflatten_device_tree(),
    otherwise the strings that the device tree refer to will get poisoned
    and potentially reused, breaking later reading of the device tree
    post-init (such as compatible matching in modules, debugfs, and the
    procfs interface).
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Reviewed-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/metag/include/asm/prom.h
+++ arch/metag/include/asm/prom.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HAVE_ARCH_DEVTREE_FIXUPS
 
 extern struct machine_desc *setup_machine_fdt(void *dt);
+extern void copy_fdt(void);
 extern void metag_dt_memblock_reserve(void);
 
 #endif /* __ASM_METAG_PROM_H */
