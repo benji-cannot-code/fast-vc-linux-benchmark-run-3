8b14dd33df4758ad45d4b4255e06efab8b22af9e
Miao Xie <miaox@cn.fujitsu.com>
2013-02-20 09:13:32 +0000

    Btrfs: fix the qgroup reserved space is released prematurely
    In start_transactio(), we will try to join the transaction again after
    the current transaction is committed, so we should not release the
    reserved space of the qgroup. Fix it.
    
    Cc: Arne Jansen <sensille@gmx.net>
    Signed-off-by: Miao Xie <miaox@cn.fujitsu.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -384,7 +384,7 @@ again:
 	h->block_rsv = NULL;
 	h->orig_rsv = NULL;
 	h->aborted = 0;
-	h->qgroup_reserved = qgroup_reserved;
+	h->qgroup_reserved = 0;
 	h->delayed_ref_elem.seq = 0;
 	h->type = type;
 	h->allocating_chunk = false;
@@ -403,6 +403,7 @@ again:
 		h->block_rsv = &root->fs_info->trans_block_rsv;
 		h->bytes_reserved = num_bytes;
 	}
+	h->qgroup_reserved = qgroup_reserved;
 
 got_it:
 	btrfs_record_root_in_trans(h, root);
