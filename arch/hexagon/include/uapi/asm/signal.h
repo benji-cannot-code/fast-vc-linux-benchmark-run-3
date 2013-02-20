34f6226018e54761919d2c588d77150bcab034a2
Richard Kuo <rkuo@codeaurora.org>
2013-02-19 19:25:48 -0600

    Hexagon: fix signal.c compile error
    Signed-off-by: Richard Kuo <rkuo@codeaurora.org>

--- arch/hexagon/include/uapi/asm/signal.h
+++ arch/hexagon/include/uapi/asm/signal.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_SIGNAL_H
 #define _ASM_SIGNAL_H
 
+#include <uapi/asm/registers.h>
+
 extern unsigned long __rt_sigtramp_template[2];
 
 void do_signal(struct pt_regs *regs);
