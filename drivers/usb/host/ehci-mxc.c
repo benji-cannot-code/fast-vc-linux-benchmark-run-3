40770e1d8bdc53ae8db11234f799caa9b5b8ac50
Fabio Estevam <fabio.estevam@freescale.com>
2013-02-20 16:52:25 -0300

    usb: host: ehci-mxc: Remove unneeded header file
    Since commit c0304996b (USB: ehci-mxc: remove Efika MX-specific CHRGVBUS hack)
    there is no need to include <asm/mach-types.h>, so remove it.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-mxc.c
+++ drivers/usb/host/ehci-mxc.c
@@ -29,11 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/usb.h>
 #include <linux/usb/hcd.h>
-
 #include <linux/platform_data/usb-ehci-mxc.h>
-
-#include <asm/mach-types.h>
-
 #include "ehci.h"
 
 #define DRIVER_DESC "Freescale On-Chip EHCI Host driver"
