bb455e65e62c8b513adee0de5d4a3fa2727858b2
Stephen Boyd <sboyd@codeaurora.org>
2013-02-21 15:04:08 -0800

    clocksource: time-armada-370-xp: Fix sparse warning
    drivers/clocksource/time-armada-370-xp.c:217:13: warning: symbol
    'armada_370_xp_timer_init' was not declared. Should it be static?
    
    Also remove the __init marking in the prototype as it's
    unnecessary and drop the init.h file.
    
    Acked-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clocksource/time-armada-370-xp.c
+++ drivers/clocksource/time-armada-370-xp.c
@@ -29,9 +29,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/module.h>
 #include <linux/sched_clock.h>
+#include <linux/percpu.h>
+#include <linux/time-armada-370-xp.h>
 
 #include <asm/localtimer.h>
-#include <linux/percpu.h>
 /*
  * Timer block registers.
  */
