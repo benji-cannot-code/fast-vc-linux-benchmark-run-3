b0a652b9f4a85c69ae620020107aca48cfdf63ef
Thierry Reding <thierry.reding@avionic-design.de>
2013-02-21 08:11:57 +0100

    drm/tegra: Fix color expansion
    bpp stores the number of bytes per pixel, but color expansion needs to
    be enabled for less than 24 bits per pixel.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>

--- drivers/gpu/drm/tegra/dc.c
+++ drivers/gpu/drm/tegra/dc.c
@@ -501,7 +501,7 @@ int tegra_dc_setup_window(struct tegra_dc *dc, unsigned int index,
 		tegra_dc_writel(dc, 0x0000, DC_WIN_CSC_KVB);
 
 		value |= CSC_ENABLE;
-	} else if (bpp < 24) {
+	} else if (window->bits_per_pixel < 24) {
 		value |= COLOR_EXPAND;
 	}
 
