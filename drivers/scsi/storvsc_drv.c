083436fd190eae637b1e75d8031445edab882051
Olaf Hering <olaf@aepfle.de>
2013-02-21 12:04:51 -0800

    [SCSI] storvsc: avoid usage of WRITE_SAME
    Set scsi_device->no_write_same because the host does not support it.
    Also blacklist WRITE_SAME to avoid (and log) accident usage.
    
    If the guest uses the ext4 filesystem, storvsc hangs while it prints
    these messages in an endless loop:
    ...
    [  161.459523] hv_storvsc vmbus_0_1: cmd 0x41 scsi status 0x2 srb status 0x6
    [  161.462157] sd 2:0:0:0: [sda]
    [  161.463135] Sense Key : No Sense [current]
    [  161.464983] sd 2:0:0:0: [sda]
    [  161.465899] Add. Sense: No additional sense information
    [  161.468211] hv_storvsc vmbus_0_1: cmd 0x41 scsi status 0x2 srb status 0x6
    [  161.475766] sd 2:0:0:0: [sda]
    [  161.476728] Sense Key : No Sense [current]
    [  161.478284] sd 2:0:0:0: [sda]
    [  161.479441] Add. Sense: No additional sense information
    ...
    
    This happens with a guest running on Windows Server 2012, but happens to
    work while running on Windows Server 2008. WRITE_SAME isnt really
    supported by both versions, so disable the command usage globally.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/storvsc_drv.c
+++ drivers/scsi/storvsc_drv.c
@@ -1157,6 +1157,8 @@ static int storvsc_device_configure(struct scsi_device *sdevice)
 
 	blk_queue_bounce_limit(sdevice->request_queue, BLK_BOUNCE_ANY);
 
+	sdevice->no_write_same = 1;
+
 	return 0;
 }
 
@@ -1239,6 +1241,8 @@ static bool storvsc_scsi_cmd_ok(struct scsi_cmnd *scmnd)
 	u8 scsi_op = scmnd->cmnd[0];
 
 	switch (scsi_op) {
+	/* the host does not handle WRITE_SAME, log accident usage */
+	case WRITE_SAME:
 	/*
 	 * smartd sends this command and the host does not handle
 	 * this. So, don't send it.
