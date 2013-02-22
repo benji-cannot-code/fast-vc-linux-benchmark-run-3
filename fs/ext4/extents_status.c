13221920bdd61651078d217848dfdce36b0c3cc8
Eryu Guan <guaneryu@gmail.com>
2013-02-22 15:27:47 -0500

    ext4: no need to remove extent if len is 0 in ext4_es_remove_extent()
    len is 0 means no extent needs to be removed, so return immediately.
    Otherwise it could trigger the following BUG_ON() in
    ext4_es_remove_extent()
    
    	end = lblk + len - 1;
    	BUG_ON(end < lblk);
    
    This could be reproduced by a simple truncate(1) command by an
    unprivileged user
    
    	truncate -s $(($((2**32 - 1)) * 4096)) /mnt/ext4/testfile
    
    The same is true for __es_insert_extent().
    
    Patched kernel passed xfstests regression test.
    
    Signed-off-by: Eryu Guan <guaneryu@gmail.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Reviewed-by: Zheng Liu <wenqing.lz@taobao.com>

--- fs/ext4/extents_status.c
+++ fs/ext4/extents_status.c
@@ -457,6 +457,9 @@ int ext4_es_insert_extent(struct inode *inode, ext4_lblk_t lblk,
 	es_debug("add [%u/%u) %llu %llx to extent status tree of inode %lu\n",
 		 lblk, len, pblk, status, inode->i_ino);
 
+	if (!len)
+		return 0;
+
 	BUG_ON(end < lblk);
 
 	newes.es_lblk = lblk;
@@ -649,6 +652,9 @@ int ext4_es_remove_extent(struct inode *inode, ext4_lblk_t lblk,
 	trace_ext4_es_remove_extent(inode, lblk, len);
 	es_debug("remove [%u/%u) from extent status tree of inode %lu\n",
 		 lblk, len, inode->i_ino);
+
+	if (!len)
+		return err;
 
 	end = lblk + len - 1;
 	BUG_ON(end < lblk);
