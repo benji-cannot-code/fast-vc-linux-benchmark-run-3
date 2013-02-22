5bfa29300368d9ffcd1a9fa92a443b35222ff676
Kurt Kanzenbach <ly80toro@cip.cs.fau.de>
2013-02-22 12:13:26 +0100

    staging: usbip: userspace: libsrc: do not init static/globals to 0
    This patch fixes the following checkpatch errors:
    -ERROR: do not initialise statics to 0 or NULL
    -ERROR: do not initialise globals to 0 or NULL
    
    Signed-off-by: Kurt Kanzenbach <ly80toro@cip.cs.fau.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/usbip/userspace/libsrc/names.c
+++ drivers/staging/usbip/userspace/libsrc/names.c
@@ -247,7 +247,7 @@ struct pool {
 	void *mem;
 };
 
-static struct pool *pool_head = NULL;
+static struct pool *pool_head;
 
 static void *my_malloc(size_t size)
 {
--- drivers/staging/usbip/userspace/libsrc/usbip_common.c
+++ drivers/staging/usbip/userspace/libsrc/usbip_common.c
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #undef  PROGNAME
 #define PROGNAME "libusbip"
 
-int usbip_use_syslog = 0;
-int usbip_use_stderr = 0;
-int usbip_use_debug  = 0;
+int usbip_use_syslog;
+int usbip_use_stderr;
+int usbip_use_debug;
 
 struct speed_string {
 	int num;
