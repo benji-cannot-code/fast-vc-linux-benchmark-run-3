79881f1f984738f40a0f163dca6137521f186d33
Philip J Kelleher <pjk1939@linux.vnet.ibm.com>
2013-02-25 12:27:46 -0600

    block: IBM RamSan 70/80 trivial changes.
    This patch includes trivial changes that were recommended by
    different members of the Linux Community.
    
    Changes include:
    o Removing the redundant wmb().
    o Formatting
    o Various other little things.
    
    Signed-off-by: Philip J Kelleher <pjk1939@linux.vnet.ibm.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/rsxx/config.c
+++ drivers/block/rsxx/config.c
@@ -30,10 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rsxx_priv.h"
 #include "rsxx_cfg.h"
 
-static void initialize_config(void *config)
+static void initialize_config(struct rsxx_card_cfg *cfg)
 {
-	struct rsxx_card_cfg *cfg = config;
-
 	cfg->hdr.version = RSXX_CFG_VERSION;
 
 	cfg->data.block_size        = RSXX_HW_BLK_SIZE;
@@ -182,7 +180,7 @@ int rsxx_load_config(struct rsxx_cardinfo *card)
 	} else {
 		dev_info(CARD_TO_DEV(card),
 			"Initializing card configuration.\n");
-		initialize_config(card);
+		initialize_config(&card->config);
 		st = rsxx_save_config(card);
 		if (st)
 			return st;
