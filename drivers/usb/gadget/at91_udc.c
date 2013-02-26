bdcd88deb337d6fa83360cfd780c777744ad5e44
Felipe Balbi <balbi@ti.com>
2013-02-26 14:16:21 +0200

    usb: gadget: at91_udc: don't touch gadget.dev.driver
    udc-core now handles that for us, which means
    we can remove it from our driver.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/at91_udc.c
+++ drivers/usb/gadget/at91_udc.c
@@ -1632,7 +1632,6 @@ static int at91_start(struct usb_gadget *gadget,
 
 	udc = container_of(gadget, struct at91_udc, gadget);
 	udc->driver = driver;
-	udc->gadget.dev.driver = &driver->driver;
 	udc->gadget.dev.of_node = udc->pdev->dev.of_node;
 	udc->enabled = 1;
 	udc->selfpowered = 1;
@@ -1653,7 +1652,6 @@ static int at91_stop(struct usb_gadget *gadget,
 	at91_udp_write(udc, AT91_UDP_IDR, ~0);
 	spin_unlock_irqrestore(&udc->lock, flags);
 
-	udc->gadget.dev.driver = NULL;
 	udc->driver = NULL;
 
 	DBG("unbound from %s\n", driver->driver.name);
