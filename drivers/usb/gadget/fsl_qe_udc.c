4544f079495b43601ede5c76421c8a5835ef4f66
Felipe Balbi <balbi@ti.com>
2013-02-26 14:35:06 +0200

    usb: gadget: fsl_qe_udc: don't touch gadget.dev.driver
    udc-core now handles that for us, which means
    we can remove it from our driver.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/fsl_qe_udc.c
+++ drivers/usb/gadget/fsl_qe_udc.c
@@ -2297,7 +2297,6 @@ static int fsl_qe_start(struct usb_gadget *gadget,
 	driver->driver.bus = NULL;
 	/* hook up the driver */
 	udc->driver = driver;
-	udc->gadget.dev.driver = &driver->driver;
 	udc->gadget.speed = driver->max_speed;
 
 	/* Enable IRQ reg and Set usbcmd reg EN bit */
@@ -2339,7 +2338,6 @@ static int fsl_qe_stop(struct usb_gadget *gadget,
 		nuke(loop_ep, -ESHUTDOWN);
 	spin_unlock_irqrestore(&udc->lock, flags);
 
-	udc->gadget.dev.driver = NULL;
 	udc->driver = NULL;
 
 	dev_info(udc->dev, "unregistered gadget driver '%s'\r\n",
