7948f08d7a664e7fc5b6c852151308f820856406
Vineet Gupta <vgupta@synopsys.com>
2013-02-27 18:23:27 +0530

    ARC: Fixup the current ABI version
    The upstream kernel ABI (v3) is different from current out-of-tree (v2):
    * no-legacy-syscalls
    * user_regs_struct layout has changed
    
    So we rev up the ABI version
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/elf.h
+++ arch/arc/include/asm/elf.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Machine specific ELF Hdr flags */
 #define EF_ARC_OSABI_MSK	0x00000f00
 #define EF_ARC_OSABI_ORIG	0x00000000   /* MUST be zero for back-compat */
-#define EF_ARC_OSABI_V2		0x00000200
+#define EF_ARC_OSABI_CURRENT	0x00000300   /* v3 (no legacy syscalls) */
 
 /* ARC Relocations (kernel Modules only) */
 #define  R_ARC_32		0x4
