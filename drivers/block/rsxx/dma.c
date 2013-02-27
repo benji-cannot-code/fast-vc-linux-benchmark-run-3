ae52ed8ce9832dbe14f833a71b77f7c3cf66c5d7
Philip J Kelleher <pjk1939@linux.vnet.ibm.com>
2013-02-27 09:24:59 -0600

    block: IBM RamSan 70/80 branding changes.
    This patch includes changing the hardware branding name from
    IBM RamSan to IBM FlashSystem.
    
    v2 Changes include:
    o Removing the unnecessary IBM Vendor ID #define
    
    v1 Changes include:
    o Changed all references of RamSan to FlashSystem.
    o Changed the vendor/device IDs for the product.
    o Changed driver version number.
    o Updated the MAINTAINERS file.
    o Various other little things.
    
    Signed-off-by: Philip J Kelleher <pjk1939@linux.vnet.ibm.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/rsxx/dma.c
+++ drivers/block/rsxx/dma.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct rsxx_dma {
 	struct list_head	 list;
 	u8			 cmd;
-	unsigned int		 laddr;     /* Logical address on the ramsan */
+	unsigned int		 laddr;     /* Logical address */
 	struct {
 		u32		 off;
 		u32		 cnt;
