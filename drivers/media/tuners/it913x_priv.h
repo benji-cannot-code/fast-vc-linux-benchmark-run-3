1caa892c508f5d45b9b1bcf99d2c6fbbb724da86
Antti Palosaari <crope@iki.fi>
2013-02-28 20:14:38 -0300

    [media] it913x: include tuner IDs from af9033.h
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/tuners/it913x_priv.h
+++ drivers/media/tuners/it913x_priv.h
@@ -25,15 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IT913X_PRIV_H
 
 #include "it913x.h"
-
-/* Build in tuner types */
-#define IT9137 0x38
-#define IT9135_38 0x38
-#define IT9135_51 0x51
-#define IT9135_52 0x52
-#define IT9135_60 0x60
-#define IT9135_61 0x61
-#define IT9135_62 0x62
+#include "af9033.h"
 
 #define PRO_LINK		0x0
 #define PRO_DMOD		0x1
