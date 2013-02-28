5c19a527b1c13b527e1e4a1c6b1eb047b2c19d6d
Tejun Heo <tj@kernel.org>
2013-02-27 17:05:07 -0800

    idr: add idr_layer->prefix
    Add a field which carries the prefix of ID the idr_layer covers.  This
    will be used to implement lookup hint.
    
    This patch doesn't make use of the new field and doesn't introduce any
    behavior difference.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/idr.h
+++ include/linux/idr.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IDR_MASK ((1 << IDR_BITS)-1)
 
 struct idr_layer {
+	int			prefix;	/* the ID prefix of this idr_layer */
 	DECLARE_BITMAP(bitmap, IDR_SIZE); /* A zero bit means "space here" */
 	struct idr_layer __rcu	*ary[1<<IDR_BITS];
 	int			count;	/* When zero, we can release it */
