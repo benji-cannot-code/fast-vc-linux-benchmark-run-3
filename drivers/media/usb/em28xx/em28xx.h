fe3e4608e18f8dada63c9c253818ae0b27ad4a98
Mauro Carvalho Chehab <mchehab@redhat.com>
2013-02-28 10:35:56 -0300

    [media] em28xx: Add ISDB support for c3tech Digital duo
    This is an hybrid board. However, for analog, it requires
    a new driver for saa7136. So, for now, let's just add
    support for Digital TV.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/em28xx/em28xx.h
+++ drivers/media/usb/em28xx/em28xx.h
@@ -130,6 +130,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2884_BOARD_PCTV_510E			  85
 #define EM2884_BOARD_PCTV_520E			  86
 #define EM2884_BOARD_TERRATEC_HTC_USB_XS	  87
+#define EM2884_BOARD_C3TECH_DIGITAL_DUO		  88
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
