b88c8e22657f2e84635a697578719a7a66511508
Jeremy Kerr <jk@ozlabs.org>
2013-02-27 17:05:55 -0800

    selftests/efivarfs: add create-read test
    Test that reads from a newly-created efivarfs file (with no data written)
    will return EOF.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
    Cc: Matt Fleming <matt.fleming@intel.com>
    Cc: Lingzhu Xiang <lxiang@redhat.com>
    Cc: Dave Young <dyoung@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/efivarfs/efivarfs.sh
+++ tools/testing/selftests/efivarfs/efivarfs.sh
@@ -71,6 +71,12 @@ test_create_empty()
 	fi
 }
 
+test_create_read()
+{
+	local file=$efivarfs_mount/$FUNCNAME-$test_guid
+	./create-read $file
+}
+
 test_delete()
 {
 	local attrs='\x07\x00\x00\x00'
@@ -126,6 +132,7 @@ rc=0
 
 run_test test_create
 run_test test_create_empty
+run_test test_create_read
 run_test test_delete
 run_test test_zero_size_delete
 run_test test_open_unlink
