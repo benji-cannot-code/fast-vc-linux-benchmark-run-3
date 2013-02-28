30cc4f1714e86c03b91744a46368c429570e9384
Ming Lei <ming.lei@canonical.com>
2013-02-27 17:05:19 -0800

    block/partitions: optimize memory allocation in check_partition()
    Currently, sizeof(struct parsed_partitions) may be 64KB in 32bit arch, so
    it is easy to trigger page allocation failure by check_partition,
    especially in hotplug block device situation(such as, USB mass storage,
    MMC card, ...), and Felipe Balbi has observed the failure.
    
    This patch does below optimizations on the allocation of struct
    parsed_partitions to try to address the issue:
    
    - make parsed_partitions.parts as pointer so that the pointed memory can
      fit in 32KB buffer, then approximate 32KB memory can be saved
    
    - vmalloc the buffer pointed by parsed_partitions.parts because 32KB is
      still a bit big for kmalloc
    
    - given that many devices have the partition count limit, so only
      allocate disk_max_parts() partitions instead of 256 partitions always
    
    Signed-off-by: Ming Lei <ming.lei@canonical.com>
    Reported-by: Felipe Balbi <balbi@ti.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Reviewed-by: Yasuaki Ishimatsu <isimatu.yasuaki@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- block/partition-generic.c
+++ block/partition-generic.c
@@ -419,7 +419,7 @@ int rescan_partitions(struct gendisk *disk, struct block_device *bdev)
 	int p, highest, res;
 rescan:
 	if (state && !IS_ERR(state)) {
-		kfree(state);
+		free_partitions(state);
 		state = NULL;
 	}
 
@@ -526,7 +526,7 @@ rescan:
 			md_autodetect_dev(part_to_dev(part)->devt);
 #endif
 	}
-	kfree(state);
+	free_partitions(state);
 	return 0;
 }
 
