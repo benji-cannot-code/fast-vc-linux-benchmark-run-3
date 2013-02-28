a119331f32a820948c8b0c27f578a9b8858f6725
Shuah Khan <shuah.khan@hp.com>
2013-02-27 17:06:45 -0700

    PCI: Add PCI_BUS_NUM() and PCI_DEVID() interfaces
    PCI defines PCI_DEVFN(), PCI_SLOT(), and PCI_FUNC() interfaces; however,
    it doesn't have interfaces to return PCI bus and PCI device id. Drivers
    (AMD IOMMU, and AER) implement module specific definitions for PCI_BUS()
    and AMD_IOMMU driver also has a module specific interface to calculate PCI
    device id from bus number and devfn.
    
    Add PCI_BUS_NUM and PCI_DEVID interfaces to return PCI bus number and PCI
    device id respectively to avoid the need for duplicate definitions in other
    modules. AER driver code and AMD IOMMU driver define PCI_BUS. AMD IOMMU
    driver defines an interface to calculate device id from bus number, and
    devfn pair.
    
    PCI_DEVFN(), PCI_SLOT(), and PCI_FUNC() interfaces are exported to
    user-space via uapi/linux/pci.h. However, in the interest to keep the new
    interfaces as kernel only and not export them to user-space unnecessarily,
    added them to linux/pci.h instead.
    
    Signed-off-by: Shuah Khan <shuah.khan@hp.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Joerg Roedel <joro@8bytes.org>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -36,6 +36,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Include the ID list */
 #include <linux/pci_ids.h>
 
+/*
+ * The PCI interface treats multi-function devices as independent
+ * devices.  The slot/function address of each device is encoded
+ * in a single byte as follows:
+ *
+ *	7:3 = slot
+ *	2:0 = function
+ * PCI_DEVFN(), PCI_SLOT(), and PCI_FUNC() are defined uapi/linux/pci.h
+ * In the interest of not exposing interfaces to user-space unnecessarily,
+ * the following kernel only defines are being added here.
+ */
+#define PCI_DEVID(bus, devfn)  ((((u16)bus) << 8) | devfn)
+/* return bus from PCI devid = ((u16)bus_number) << 8) | devfn */
+#define PCI_BUS_NUM(x) (((x) >> 8) & 0xff)
+
 /* pci_slot represents a physical slot */
 struct pci_slot {
 	struct pci_bus *bus;		/* The bus this slot is on */
