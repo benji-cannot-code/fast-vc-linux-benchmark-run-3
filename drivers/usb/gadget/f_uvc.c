11c7d44d86481f0e6593efcc263969df1ad1e7fb
Bhupesh Sharma <bhupesh.sharma@st.com>
2013-03-01 20:46:28 +0100

    usb: gadget: uvc: Add fix for UVC compliance test suite assertion 6.3.90 failure
    As per UVC compliance test specification's assertion number 6.3.90
    related to 'Standard VS Isochronous Video Data Endpoint Descriptor Assertions',
    the bits D3..2 of 'bmAttributes' field of Standard VS Isochronous Video Data
    Endpoint Descriptor should be 01 (binary) to indicate that the
    synchronization type is ASYNCHRONOUS.
    
    This mandatory requirement has been captured in section 3.10.1.1 of the UVC
    Video Class Specification version 1.1
    
    This patch adds a fix for the same.
    
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@st.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Tested-by: Bhupesh Sharma <bhupesh.sharma@st.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/f_uvc.c
+++ drivers/usb/gadget/f_uvc.c
@@ -154,7 +154,8 @@ static struct usb_endpoint_descriptor uvc_fs_streaming_ep __initdata = {
 	.bLength		= USB_DT_ENDPOINT_SIZE,
 	.bDescriptorType	= USB_DT_ENDPOINT,
 	.bEndpointAddress	= USB_DIR_IN,
-	.bmAttributes		= USB_ENDPOINT_XFER_ISOC,
+	.bmAttributes		= USB_ENDPOINT_SYNC_ASYNC
+				| USB_ENDPOINT_XFER_ISOC,
 	/* The wMaxPacketSize and bInterval values will be initialized from
 	 * module parameters.
 	 */
@@ -166,7 +167,8 @@ static struct usb_endpoint_descriptor uvc_hs_streaming_ep __initdata = {
 	.bLength		= USB_DT_ENDPOINT_SIZE,
 	.bDescriptorType	= USB_DT_ENDPOINT,
 	.bEndpointAddress	= USB_DIR_IN,
-	.bmAttributes		= USB_ENDPOINT_XFER_ISOC,
+	.bmAttributes		= USB_ENDPOINT_SYNC_ASYNC
+				| USB_ENDPOINT_XFER_ISOC,
 	/* The wMaxPacketSize and bInterval values will be initialized from
 	 * module parameters.
 	 */
@@ -179,7 +181,8 @@ static struct usb_endpoint_descriptor uvc_ss_streaming_ep __initdata = {
 	.bDescriptorType	= USB_DT_ENDPOINT,
 
 	.bEndpointAddress	= USB_DIR_IN,
-	.bmAttributes		= USB_ENDPOINT_XFER_ISOC,
+	.bmAttributes		= USB_ENDPOINT_SYNC_ASYNC
+				| USB_ENDPOINT_XFER_ISOC,
 	/* The wMaxPacketSize and bInterval values will be initialized from
 	 * module parameters.
 	 */
