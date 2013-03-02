2f3db08d2bdeee35a373ed8489f038a5ea799a89
Vineet Gupta <vgupta@synopsys.com>
2013-03-02 12:31:39 +0530

    ARC: Remove SET_PERSONALITY (tracks cross-arch change)
    Tracks commit e72837e3e7b "default SET_PERSONALITY() in linux/elf.h"
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/elf.h
+++ arch/arc/include/asm/elf.h
@@ -73,7 +73,4 @@ extern int elf_check_arch(const struct elf32_hdr *);
  */
 #define ELF_PLATFORM	(NULL)
 
-#define SET_PERSONALITY(ex) \
-	set_personality(PER_LINUX | (current->personality & (~PER_MASK)))
-
 #endif
