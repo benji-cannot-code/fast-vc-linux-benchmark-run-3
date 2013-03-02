eeacef995f413ac1d0b25d26e2b42e322ace726e
SeongJae Park <sj38.park@gmail.com>
2013-03-02 10:45:17 +0900

    staging: csr: csr_time.c: Fix coding style
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/csr/csr_time.c
+++ drivers/staging/csr/csr_time.c
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*****************************************************************************
 
-            (c) Cambridge Silicon Radio Limited 2010
-            All rights reserved and confidential information of CSR
+	(c) Cambridge Silicon Radio Limited 2010
+	All rights reserved and confidential information of CSR
 
-            Refer to LICENSE.txt included with this source for details
-            on the license terms.
+	Refer to LICENSE.txt included with this source for details
+	on the license terms.
 
 *****************************************************************************/
 
