65a704dd57d8fe295ff0d3210ff4e205360d7146
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2013-03-03 23:08:45 +0100

    ACPI / container: Use hotplug profile user space interface
    Make the ACPI container driver register its ACPI scan handler object
    using acpi_scan_add_handler_with_hotplug() to allow user space to
    manipulate its hotplug profile attributes.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Toshi Kani <toshi.kani@hp.com>
    Tested-by: Toshi Kani <toshi.kani@hp.com>

--- drivers/acpi/container.c
+++ drivers/acpi/container.c
@@ -50,7 +50,7 @@ static int container_device_attach(struct acpi_device *device,
 	return 1;
 }
 
-static struct acpi_scan_handler container_device_handler = {
+static struct acpi_scan_handler container_handler = {
 	.ids = container_device_ids,
 	.attach = container_device_attach,
 	.hotplug = {
@@ -61,5 +61,5 @@ static struct acpi_scan_handler container_device_handler = {
 
 void __init acpi_container_init(void)
 {
-	acpi_scan_add_handler(&container_device_handler);
+	acpi_scan_add_handler_with_hotplug(&container_handler, "container");
 }
