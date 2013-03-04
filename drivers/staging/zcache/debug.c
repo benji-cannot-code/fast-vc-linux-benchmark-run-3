a4538022f117401f78a298478c6b334f7c6b5360
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2013-03-04 13:18:20 -0500

    zcache/debug: Coalesce all debug under CONFIG_ZCACHE_DEBUG
    and also define this extra attribute in the Kconfig entry.
    
    Acked-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/zcache/debug.c
+++ drivers/staging/zcache/debug.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/atomic.h>
 #include "debug.h"
 
-#ifdef CONFIG_DEBUG_FS
+#ifdef CONFIG_ZCACHE_DEBUG
 #include <linux/debugfs.h>
 
 #define ATTR(x)  { .name = #x, .val = &zcache_##x, }
