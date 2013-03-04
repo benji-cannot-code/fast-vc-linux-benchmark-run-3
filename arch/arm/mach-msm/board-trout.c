8403228ba3ccdd9db665e9c893e13406fb8a7f58
Stephen Boyd <sboyd@codeaurora.org>
2013-03-04 15:08:27 -0800

    gpio: Make gpio-msm-v1 into a platform driver
    Doing this removes the dependence of this driver on the
    msm_iomap.h and cpu.h mach include headers provied by MSM. This
    is necessary to support single zImage work in the future and
    allows us to remove cpu.h entirely and brings us closer to
    removing msm_iomap.h.
    
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Tested-by: Rohit Vaswani <rvaswani@codeaurora.org>
    Acked-by: Rohit Vaswani <rvaswani@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- arch/arm/mach-msm/board-trout.c
+++ arch/arm/mach-msm/board-trout.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern int trout_init_mmc(unsigned int);
 
 static struct platform_device *devices[] __initdata = {
+	&msm_device_gpio_7201,
 	&msm_device_uart3,
 	&msm_device_smd,
 	&msm_device_nand,
