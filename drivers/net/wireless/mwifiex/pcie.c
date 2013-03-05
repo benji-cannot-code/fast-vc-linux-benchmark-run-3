690c218f75bf561ceab80c78ad1cf58ab455314d
Bing Zhao <bzhao@marvell.com>
2013-03-04 16:27:54 -0800

    mwifiex: fix [-Wunused-function] warnings on pcie suspend/resume
    drivers/net/wireless/mwifiex/pcie.c:204:12: warning:
      'mwifiex_pcie_resume' defined but not used [-Wunused-function]
    drivers/net/wireless/mwifiex/pcie.c:166:12: warning:
      'mwifiex_pcie_suspend' defined but not used [-Wunused-function]
    
    The suspend/resume handlers ought to be under CONFIG_PM directive.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwifiex/pcie.c
+++ drivers/net/wireless/mwifiex/pcie.c
@@ -77,6 +77,7 @@ static bool mwifiex_pcie_ok_to_access_hw(struct mwifiex_adapter *adapter)
 	return false;
 }
 
+#ifdef CONFIG_PM
 /*
  * Kernel needs to suspend all functions separately. Therefore all
  * registered functions must have drivers with suspend and resume
@@ -150,6 +151,7 @@ static int mwifiex_pcie_resume(struct pci_dev *pdev)
 
 	return 0;
 }
+#endif
 
 /*
  * This function probes an mwifiex device and registers it. It allocates
