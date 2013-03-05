104ec9a826507361d23d8f8f36efa502f9c3bf0d
Aaron Lu <aaron.lu@intel.com>
2013-03-05 12:12:04 +0800

    libata-acpi: remove redundent code for power resource handling
    With commit:
    bc9b6407bd6df3ab7189e5622816bbc11ae9d2d8
    ACPI / PM: Rework the handling of devices depending on power resources
    
    The ACPI core now takes care of the power resources an acpi device
    depends on in that when the power resources are turned on, any devices
    that are bound to or in the dependent list of this acpi device will be
    runtime resumed. So there is no need for ata acpi code to duplicate this
    effort, and thus, the ata_acpi_(un)register_power_resource functions are
    no longer needed.
    
    The above commit thinks the scsi device is not bound to the acpi device,
    so needs to be added to the dependent list. But actually, it is. So
    there is no need to add it to the dependent list, or it will be runtime
    resumed twice(though this wouldn't cause any problem).
    
    This patch fixes it, and as a result, the
    ata_acpi_(un)register_power_resource and ata_acpi_(un)bind functions
    are removed.
    
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/libata-acpi.c
+++ drivers/ata/libata-acpi.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/slab.h>
 #include <linux/pm_runtime.h>
-#include <linux/pm_qos.h>
 #include <scsi/scsi_device.h>
 #include "libata.h"
 
@@ -1019,38 +1018,6 @@ int ata_acpi_on_devcfg(struct ata_device *dev)
 void ata_acpi_on_disable(struct ata_device *dev)
 {
 	ata_acpi_clear_gtf(dev);
-}
-
-static void ata_acpi_register_power_resource(struct ata_device *dev)
-{
-	struct scsi_device *sdev = dev->sdev;
-	acpi_handle handle;
-
-	handle = ata_dev_acpi_handle(dev);
-	if (handle)
-		acpi_dev_pm_add_dependent(handle, &sdev->sdev_gendev);
-}
-
-static void ata_acpi_unregister_power_resource(struct ata_device *dev)
-{
-	struct scsi_device *sdev = dev->sdev;
-	acpi_handle handle;
-
-	handle = ata_dev_acpi_handle(dev);
-	if (handle)
-		acpi_dev_pm_remove_dependent(handle, &sdev->sdev_gendev);
-}
-
-void ata_acpi_bind(struct ata_device *dev)
-{
-	ata_acpi_register_power_resource(dev);
-	if (zpodd_dev_enabled(dev))
-		dev_pm_qos_expose_flags(&dev->sdev->sdev_gendev, 0);
-}
-
-void ata_acpi_unbind(struct ata_device *dev)
-{
-	ata_acpi_unregister_power_resource(dev);
 }
 
 static int compat_pci_ata(struct ata_port *ap)
