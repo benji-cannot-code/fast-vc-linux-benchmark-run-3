f6e9fe9f95a24096f296eafb7903aa899b306379
Alexander Bondar <alexander.bondar@intel.com>
2013-03-05 13:47:04 +0200

    iwlwifi: mvm: Rename some power management definitions and methods
    Replace SLEEP_OVER_DTIM by SKIP_OVER_DTIM.
    Add iwl_mvm prefix to a function name.
    
    Signed-off-by: Alexander Bondar <alexander.bondar@intel.com>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/mvm/fw-api-power.h
+++ drivers/net/wireless/iwlwifi/mvm/fw-api-power.h
@@ -73,7 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *		receiver and transmitter. '0' - does not allow.
  * @POWER_FLAGS_POWER_MANAGEMENT_ENA_MSK: '0' Driver disables power management,
  *		'1' Driver enables PM (use rest of parameters)
- * @POWER_FLAGS_SLEEP_OVER_DTIM_MSK: '0' PM have to walk up every DTIM,
+ * @POWER_FLAGS_SKIP_OVER_DTIM_MSK: '0' PM have to walk up every DTIM,
  *		'1' PM could sleep over DTIM till listen Interval.
  * @POWER_FLAGS_ADVANCE_PM_ENA_MSK: Advanced PM (uAPSD) enable mask
  * @POWER_FLAGS_LPRX_ENA_MSK: Low Power RX enable.
@@ -81,7 +81,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum iwl_power_flags {
 	POWER_FLAGS_POWER_SAVE_ENA_MSK		= BIT(0),
 	POWER_FLAGS_POWER_MANAGEMENT_ENA_MSK	= BIT(1),
-	POWER_FLAGS_SLEEP_OVER_DTIM_MSK		= BIT(2),
+	POWER_FLAGS_SKIP_OVER_DTIM_MSK		= BIT(2),
 	POWER_FLAGS_ADVANCE_PM_ENA_MSK		= BIT(9),
 	POWER_FLAGS_LPRX_ENA_MSK		= BIT(11),
 };
