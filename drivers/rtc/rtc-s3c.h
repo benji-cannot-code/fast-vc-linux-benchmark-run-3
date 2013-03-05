0359f1d15438b834667cde0bd5c509c221e8eabf
Arnd Bergmann <arnd@arndb.de>
2013-03-05 12:04:37 +0100

    rtc: s3c: make header file local
    Nothing outside of the rtc driver includes plat/regs-rtc.h,
    so we can simply move the file into the same directory,
    which allows us to build the file as platform-independent
    code.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: rtc-linux@googlegroups.com
    Cc: Alessandro Zummo <a.zummo@towertech.it>

--- drivers/rtc/rtc-s3c.h
+++ drivers/rtc/rtc-s3c.h
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* arch/arm/mach-s3c2410/include/mach/regs-rtc.h
- *
+/*
  * Copyright (c) 2003 Simtec Electronics <linux@simtec.co.uk>
  *		      http://www.simtec.co.uk/products/SWLINUX/
  *
