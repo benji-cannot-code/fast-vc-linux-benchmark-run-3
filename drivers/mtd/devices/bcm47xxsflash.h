2aa8152a8c7168dafd4d9414c28a5d5962c8fba3
Rafał Miłecki <zajec5@gmail.com>
2013-03-06 12:33:17 +0100

    mtd: bcm47xxsflash: keep a reference to the BCMA
    To implement erase and write support we need to "talk" with ChipCommon
    BCMA core which serial flash it attached to.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/devices/bcm47xxsflash.h
+++ drivers/mtd/devices/bcm47xxsflash.h
@@ -4,7 +4,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/mtd/mtd.h>
 
+struct bcma_drv_cc;
+
 struct bcm47xxsflash {
+	struct bcma_drv_cc *bcma_cc;
+
 	u32 window;
 	u32 blocksize;
 	u16 numblocks;
