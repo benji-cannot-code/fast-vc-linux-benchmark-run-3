756f36b01b038ff8bd4b2044a84fac439b33acbd
Sachin Kamat <sachin.kamat@linaro.org>
2013-03-06 14:27:54 +0530

    tpm_i2c_stm_st33: Remove duplicate inclusion of header files
    module.h and sched.h were included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Kent Yoder <key@linux.vnet.ibm.com>

--- drivers/char/tpm/tpm_i2c_stm_st33.c
+++ drivers/char/tpm/tpm_i2c_stm_st33.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
-#include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/init.h>
@@ -51,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/io.h>
 #include <linux/slab.h>
-#include <linux/sched.h>
 
 #include "tpm.h"
 #include "tpm_i2c_stm_st33.h"
