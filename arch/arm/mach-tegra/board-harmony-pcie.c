d102ae0312832b49bfde3287894469c6c626dd06
Stephen Warren <swarren@nvidia.com>
2013-03-06 17:16:25 -0700

    ARM: tegra: fix ignored return value of regulator_enable
    This fixes:
    
    arch/arm/mach-tegra/board-harmony-pcie.c: In function ‘harmony_pcie_init’:
    arch/arm/mach-tegra/board-harmony-pcie.c:65:18: warning: ignoring return
        value of ‘regulator_enable’, declared with attribute
        warn_unused_result [-Wunused-result]
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/board-harmony-pcie.c
+++ arch/arm/mach-tegra/board-harmony-pcie.c
@@ -63,7 +63,11 @@ int __init harmony_pcie_init(void)
 		goto err_reg;
 	}
 
-	regulator_enable(regulator);
+	err = regulator_enable(regulator);
+	if (err) {
+		pr_err("%s: regulator_enable failed: %d\n", __func__, err);
+		goto err_en;
+	}
 
 	err = tegra_pcie_init(true, true);
 	if (err) {
@@ -75,6 +79,7 @@ int __init harmony_pcie_init(void)
 
 err_pcie:
 	regulator_disable(regulator);
+err_en:
 	regulator_put(regulator);
 err_reg:
 	gpio_free(en_vdd_1v05);
