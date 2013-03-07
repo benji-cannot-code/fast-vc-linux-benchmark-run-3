7cd66981cffb871a5675dc69b4dd3b6e0a0a6c35
Felipe Balbi <balbi@ti.com>
2013-03-07 17:37:59 +0200

    usb: phy: rename all phy drivers to phy-$name-usb.c
    this will make sure that we have sensible names
    for all phy drivers. Current situation was already
    quite bad with too generic names being used.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/phy/phy-fsl-usb.c
+++ drivers/usb/phy/phy-fsl-usb.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/unaligned.h>
 
-#include "fsl_otg.h"
+#include "phy-fsl-usb.h"
 
 #define DRIVER_VERSION "Rev. 1.55"
 #define DRIVER_AUTHOR "Jerry Huang/Li Yang"
--- drivers/usb/phy/phy-mv-usb.c
+++ drivers/usb/phy/phy-mv-usb.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb/hcd.h>
 #include <linux/platform_data/mv_usb.h>
 
-#include "mv_otg.h"
+#include "phy-mv-usb.h"
 
 #define	DRIVER_DESC	"Marvell USB OTG transceiver driver"
 #define	DRIVER_VERSION	"Jan 20, 2010"
