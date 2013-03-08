aeb1a4fcb873c8a51aec7e5fad733efbdb3bb86a
Tomasz Figa <tomasz.figa@gmail.com>
2013-03-08 20:35:16 +0100

    ARM: SAMSUNG: Remove remaining uses of plat/regs-timer.h header
    This patch removes remaining inclusions of plat/regs-timer.h as
    a preparation to remove the header.
    
    As a part of this, things like save and restore of PWM registers are
    removed from SoC-specific code, because it is handled in appropriate
    drivers now.
    
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>
    Reviewed-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Tested-by: Heiko Stuebner <heiko@sntech.de>
    Tested-by: Mark Brown <broonie@linaro.org>
    Tested-by: Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/plat-samsung/s5p-irq.c
+++ arch/arm/plat-samsung/s5p-irq.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/irqs.h>
 #include <mach/map.h>
-#include <plat/regs-timer.h>
 #include <plat/cpu.h>
 
 void __init s5p_init_irq(u32 *vic, u32 num_vic)
