6febec131be24b43973800de6b0468c3b5f2fb11
Mark Brown <broonie@opensource.wolfsonmicro.com>
2013-03-08 15:22:29 +0800

    ASoC: arizona: Provide defines for the clock rates
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/arizona.h
+++ sound/soc/codecs/arizona.h
@@ -50,6 +50,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARIZONA_MIXER_VOL_SHIFT                 1
 #define ARIZONA_MIXER_VOL_WIDTH                 7
 
+#define ARIZONA_CLK_6MHZ   0
+#define ARIZONA_CLK_12MHZ  1
+#define ARIZONA_CLK_24MHZ  2
+#define ARIZONA_CLK_49MHZ  3
+#define ARIZONA_CLK_73MHZ  4
+#define ARIZONA_CLK_98MHZ  5
+#define ARIZONA_CLK_147MHZ 6
+
 #define ARIZONA_MAX_DAI  4
 #define ARIZONA_MAX_ADSP 4
 
