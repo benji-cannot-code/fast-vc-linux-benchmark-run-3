5856760f10809de6fc81443adc04c117903035ee
Sylwester Nawrocki <s.nawrocki@samsung.com>
2013-03-08 12:59:33 -0300

    [media] s5p-fimc: Add device tree support for the media device driver
    This patch adds changes required for the main camera media device
    driver corresponding to the top level 'camera' device node.
    The drivers of devices corresponding to child nodes of the 'camera'
    node are looked up and and registered as sub-devices to the top
    level driver. The main driver's probing is deferred if any of the
    sub-device drivers is not yet initialized and ready.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Andrzej Hajda <a.hajda@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/s5p-fimc/fimc-mdevice.h
+++ drivers/media/platform/s5p-fimc/fimc-mdevice.h
@@ -22,6 +22,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "fimc-lite.h"
 #include "mipi-csis.h"
 
+#define FIMC_OF_NODE_NAME	"fimc"
+#define FIMC_LITE_OF_NODE_NAME	"fimc-lite"
+#define FIMC_IS_OF_NODE_NAME	"fimc-is"
+#define CSIS_OF_NODE_NAME	"csis"
+
 /* Group IDs of sensor, MIPI-CSIS, FIMC-LITE and the writeback subdevs. */
 #define GRP_ID_SENSOR		(1 << 8)
 #define GRP_ID_FIMC_IS_SENSOR	(1 << 9)
