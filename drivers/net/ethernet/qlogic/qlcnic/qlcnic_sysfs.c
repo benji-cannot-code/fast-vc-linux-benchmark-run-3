a2782230c18a997866808a407e7f4cc2d7000687
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-03-11 04:29:50 +0000

    qlcnic: remove duplicated include from qlcnic_sysfs.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qlogic/qlcnic/qlcnic_sysfs.c
+++ drivers/net/ethernet/qlogic/qlcnic/qlcnic_sysfs.c
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/aer.h>
 #include <linux/log2.h>
 
-#include <linux/sysfs.h>
-
 #define QLC_STATUS_UNSUPPORTED_CMD	-2
 
 int qlcnicvf_config_bridged_mode(struct qlcnic_adapter *adapter, u32 enable)
