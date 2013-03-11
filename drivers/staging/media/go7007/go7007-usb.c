092f177239aaadc337de1c441e94f4e9974f5d1d
Hans Verkuil <hans.verkuil@cisco.com>
2013-03-11 06:45:14 -0300

    [media] go7007-loader: add support for the other devices and move fw files
    Add support for the other devices that need to load the boot firmware.
    All firmware files are now placed in a single go7007 directory.
    Also remove the device_extension_s stuff: this is clearly a left-over from
    the olden days.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/staging/media/go7007/go7007-usb.c
+++ drivers/staging/media/go7007/go7007-usb.c
@@ -77,7 +77,7 @@ struct go7007_usb {
 static struct go7007_usb_board board_matrix_ii = {
 	.flags		= GO7007_USB_EZUSB,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = GO7007_BOARD_HAS_AUDIO |
 					GO7007_BOARD_USE_ONBOARD_I2C,
 		.audio_flags	 = GO7007_AUDIO_I2S_MODE_1 |
@@ -118,7 +118,7 @@ static struct go7007_usb_board board_matrix_ii = {
 static struct go7007_usb_board board_matrix_reload = {
 	.flags		= GO7007_USB_EZUSB,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = GO7007_BOARD_HAS_AUDIO |
 					GO7007_BOARD_USE_ONBOARD_I2C,
 		.audio_flags	 = GO7007_AUDIO_I2S_MODE_1 |
@@ -156,7 +156,7 @@ static struct go7007_usb_board board_matrix_reload = {
 static struct go7007_usb_board board_star_trek = {
 	.flags		= GO7007_USB_EZUSB | GO7007_USB_EZUSB_I2C,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = GO7007_BOARD_HAS_AUDIO, /* |
 					GO7007_BOARD_HAS_TUNER, */
 		.sensor_flags	 = GO7007_SENSOR_656 |
@@ -204,7 +204,7 @@ static struct go7007_usb_board board_star_trek = {
 static struct go7007_usb_board board_px_tv402u = {
 	.flags		= GO7007_USB_EZUSB | GO7007_USB_EZUSB_I2C,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = GO7007_BOARD_HAS_AUDIO |
 					GO7007_BOARD_HAS_TUNER,
 		.sensor_flags	 = GO7007_SENSOR_656 |
@@ -279,7 +279,7 @@ static struct go7007_usb_board board_px_tv402u = {
 static struct go7007_usb_board board_xmen = {
 	.flags		= 0,
 	.main_info	= {
-		.firmware	  = "go7007tv.bin",
+		.firmware	  = "go7007/go7007tv.bin",
 		.flags		  = GO7007_BOARD_USE_ONBOARD_I2C,
 		.hpi_buffer_cap   = 0,
 		.sensor_flags	  = GO7007_SENSOR_VREF_POLAR,
@@ -314,7 +314,7 @@ static struct go7007_usb_board board_xmen = {
 static struct go7007_usb_board board_matrix_revolution = {
 	.flags		= GO7007_USB_EZUSB,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = GO7007_BOARD_HAS_AUDIO |
 					GO7007_BOARD_USE_ONBOARD_I2C,
 		.audio_flags	 = GO7007_AUDIO_I2S_MODE_1 |
@@ -352,7 +352,7 @@ static struct go7007_usb_board board_matrix_revolution = {
 static struct go7007_usb_board board_lifeview_lr192 = {
 	.flags		= GO7007_USB_EZUSB,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = GO7007_BOARD_HAS_AUDIO |
 					GO7007_BOARD_USE_ONBOARD_I2C,
 		.audio_flags	 = GO7007_AUDIO_I2S_MODE_1 |
@@ -380,7 +380,7 @@ static struct go7007_usb_board board_lifeview_lr192 = {
 static struct go7007_usb_board board_endura = {
 	.flags		= 0,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = 0,
 		.audio_flags	 = GO7007_AUDIO_I2S_MODE_1 |
 					GO7007_AUDIO_I2S_MASTER |
@@ -405,7 +405,7 @@ static struct go7007_usb_board board_endura = {
 static struct go7007_usb_board board_adlink_mpg24 = {
 	.flags		= 0,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.flags		 = GO7007_BOARD_USE_ONBOARD_I2C,
 		.audio_flags	 = GO7007_AUDIO_I2S_MODE_1 |
 					GO7007_AUDIO_I2S_MASTER |
@@ -438,7 +438,7 @@ static struct go7007_usb_board board_adlink_mpg24 = {
 static struct go7007_usb_board board_sensoray_2250 = {
 	.flags		= GO7007_USB_EZUSB | GO7007_USB_EZUSB_I2C,
 	.main_info	= {
-		.firmware	 = "go7007tv.bin",
+		.firmware	 = "go7007/go7007tv.bin",
 		.audio_flags	 = GO7007_AUDIO_I2S_MODE_1 |
 					GO7007_AUDIO_I2S_MASTER |
 					GO7007_AUDIO_WORD_16,
@@ -487,7 +487,7 @@ static struct go7007_usb_board board_sensoray_2250 = {
 	},
 };
 
-MODULE_FIRMWARE("go7007tv.bin");
+MODULE_FIRMWARE("go7007/go7007tv.bin");
 
 static const struct usb_device_id go7007_usb_id_table[] = {
 	{
