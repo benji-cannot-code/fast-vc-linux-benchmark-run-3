72ae5c2679de05eff3ecedfa3d3deda6b1d29126
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-03-12 11:09:34 +0800

    wil6210: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Acked-by: Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/wil6210/cfg80211.c
+++ drivers/net/wireless/ath/wil6210/cfg80211.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/wireless.h>
 #include <linux/ieee80211.h>
 #include <linux/slab.h>
-#include <linux/version.h>
 #include <net/cfg80211.h>
 
 #include "wil6210.h"
