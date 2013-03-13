c48208b5b802e32fd33331bdf0776884c2ff5fd0
Lars-Peter Clausen <lars@metafoo.de>
2013-03-13 19:26:04 +0100

    mmc: sdhci-pltfm: Constify the ops field of sdhci_pltfm_data struct
    All users of the sdhci_ops struct in the sdhci core already treat it as
    const.  The sdhci-pltfm code itself never actually looks at the ops field
    of the sdhci_pltfm_data struct and merely passes it on to the sdhci core,
    so make we can make it const in the sdhci_pltfm_data struct as well.
    This allows us to declare sdhci_ops structs as const in drivers using
    the sdhci-pltfm helper code.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-pltfm.h
+++ drivers/mmc/host/sdhci-pltfm.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sdhci.h"
 
 struct sdhci_pltfm_data {
-	struct sdhci_ops *ops;
+	const struct sdhci_ops *ops;
 	unsigned int quirks;
 };
 
