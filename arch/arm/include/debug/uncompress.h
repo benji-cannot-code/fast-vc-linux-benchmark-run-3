b79c7ad42e90d8cb50f36416757fa279f0bd08ba
Shawn Guo <shawn.guo@linaro.org>
2013-03-14 08:47:27 +0100

    ARM: 7671/1: use Kconfig to select uncompress.h
    Following the approach handling DEBUG_LL inclusion, the patch creates
    a Kconfig symbol CONFIG_UNCOMPRESS_INCLUDE for choosing the correct
    uncompress header.  For traditional build, mach/uncompress.h will be
    included in arch/arm/boot/compressed/misc.c.  For multiplatform build,
    debug/uncompress.h which contains a suite of empty functions will be
    used.  In this way, a platform with particular uncompress.h
    implementation could choose its own uncompress.h with this Kconfig
    option.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/include/debug/uncompress.h
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+static inline void putc(int c) {}
+static inline void flush(void) {}
+static inline void arch_decomp_setup(void) {}
