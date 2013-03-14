b18f5192a5051690ef11328b23690d47189125fd
Felipe Balbi <balbi@ti.com>
2013-03-14 10:49:13 +0200

    usb: renesas: remove unused DMA_ADDR_INVALID
    DMA_ADDR_INVALID isn't used anymore, it's safe
    to remove it.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/renesas_usbhs/fifo.c
+++ drivers/usb/renesas_usbhs/fifo.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 void usbhs_pkt_init(struct usbhs_pkt *pkt)
 {
-	pkt->dma = DMA_ADDR_INVALID;
 	INIT_LIST_HEAD(&pkt->node);
 }
 
