12f1fca0d2c0a00bc715ade85e963f2e224110f3
Ian Abbott <abbotti@mev.co.uk>
2013-03-15 13:15:36 +0000

    staging: comedi: remove unneeded settings of `dev->iobase`
    Some PCI drivers use the "spare" `iobase` member of `struct
    comedi_device` as a flag to indicate that the call to
    `comedi_pci_enable()` was successful.  This is no longer necessary now
    that `comedi_pci_enable()` and `comedi_pci_disable()` use the
    `ioenabled` member of `struct comedi_device` themselves to keep track of
    what needs to be done.
    
    Remove the unnecessary assignments to the `iobase` member in the
    relevant drivers.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/gsc_hpdi.c
+++ drivers/staging/comedi/drivers/gsc_hpdi.c
@@ -503,7 +503,6 @@ static int hpdi_auto_attach(struct comedi_device *dev,
 	retval = comedi_pci_enable(dev);
 	if (retval)
 		return retval;
-	dev->iobase = 1;	/* the "detach" needs this */
 	pci_set_master(pcidev);
 
 	devpriv->plx9080_iobase =
--- drivers/staging/comedi/drivers/ni_pcidio.c
+++ drivers/staging/comedi/drivers/ni_pcidio.c
@@ -1116,7 +1116,6 @@ static int nidio_auto_attach(struct comedi_device *dev,
 	ret = comedi_pci_enable(dev);
 	if (ret)
 		return ret;
-	dev->iobase = 1;
 
 	devpriv = kzalloc(sizeof(*devpriv), GFP_KERNEL);
 	if (!devpriv)
