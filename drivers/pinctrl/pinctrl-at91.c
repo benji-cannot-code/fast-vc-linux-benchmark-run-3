eadefebfed09682cbd52266572bd50f60391f6a9
Sachin Kamat <sachin.kamat@linaro.org>
2013-03-15 10:07:02 +0530

    pinctrl: at91: Remove duplicate const
    const declared twice. Fixes the following sparse warning:
    drivers/pinctrl/pinctrl-at91.c:815:21: warning: duplicate const
    drivers/pinctrl/pinctrl-at91.c:849:21: warning: duplicate const
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-at91.c
+++ drivers/pinctrl/pinctrl-at91.c
@@ -813,7 +813,7 @@ static int at91_pinctrl_mux_mask(struct at91_pinctrl *info,
 {
 	int ret = 0;
 	int size;
-	const const __be32 *list;
+	const __be32 *list;
 
 	list = of_get_property(np, "atmel,mux-mask", &size);
 	if (!list) {
@@ -847,7 +847,7 @@ static int at91_pinctrl_parse_groups(struct device_node *np,
 {
 	struct at91_pmx_pin *pin;
 	int size;
-	const const __be32 *list;
+	const __be32 *list;
 	int i, j;
 
 	dev_dbg(info->dev, "group(%d): %s\n", index, np->name);
