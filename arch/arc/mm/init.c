64fe22553dd8e7e7a09b5fb92e08bf3b312c5263
Paul Bolle <pebolle@tiscali.nl>
2013-03-15 17:16:17 +0100

    ARC: remove #ifdef-ed out include of dead header
    There's no (Kconfig) macro CONFIG_BLOCK_DEV_RAM. (CONFIG_BLK_DEV_RAM
    does exist though.) But linux/blk.h got killed in 2005 anyway (in a
    patch titled "kill blk.h"), so these three lines can be removed.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/mm/init.c
+++ arch/arc/mm/init.c
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/bootmem.h>
 #include <linux/memblock.h>
-#ifdef CONFIG_BLOCK_DEV_RAM
-#include <linux/blk.h>
-#endif
 #include <linux/swap.h>
 #include <linux/module.h>
 #include <asm/page.h>
