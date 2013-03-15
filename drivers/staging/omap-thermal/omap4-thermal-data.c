20d7da90133a4b57c2220bef37dde7f9a3cb540d
Eduardo Valentin <eduardo.valentin@ti.com>
2013-03-15 08:59:51 -0400

    staging: omap-thermal: create header for register, bitfields and definitions
    In order to have a better code readability and organization, this patch
    splits omap-bandgap.h into three headers.
    . omap-bandgap.h will contain only the driver related data structures
    definitions and macros
    . omap4xxx-bandgap.h will contain only defines and bitfields
    related to OMAP4 based devices
    . omap5xxx-bandgap.h will contain only defines and bitfields
    related to OMAP5 based devices
    
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/omap-thermal/omap4-thermal-data.c
+++ drivers/staging/omap-thermal/omap4-thermal-data.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "omap-thermal.h"
 #include "omap-bandgap.h"
+#include "omap4xxx-bandgap.h"
 
 /*
  * OMAP4430 has one instance of thermal sensor for MPU
