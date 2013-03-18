fead0f064331199d9fcaf2ee1e6bcd8279efdbdb
Alan Stern <stern@rowland.harvard.edu>
2013-03-18 12:04:54 -0400

    USB: EHCI: decrease schedule-status poll timeout
    This patch (as1657) decreases the timeout used by ehci-hcd for polling
    the async and periodic schedule statuses.  The timeout is currently
    set to 20 ms, which is much too high.  Controllers should always
    update the schedule status within one or two ms of being told to do
    so; if they don't then something is wrong.
    
    Furthermore, bug reports have shown that sometimes controllers
    (particularly those made by VIA) don't update the status bit at all,
    even when the schedule does change state.  When this happens, polling
    for 20 ms would cause an unnecessarily long delay.
    
    The delay is reduced to somewhere between 2 and 4 ms, depending on the
    slop allowed by the kernel's high-res timers.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-timer.c
+++ drivers/usb/host/ehci-timer.c
@@ -114,8 +114,8 @@ static void ehci_poll_ASS(struct ehci_hcd *ehci)
 
 	if (want != actual) {
 
-		/* Poll again later, but give up after about 20 ms */
-		if (ehci->ASS_poll_count++ < 20) {
+		/* Poll again later, but give up after about 2-4 ms */
+		if (ehci->ASS_poll_count++ < 2) {
 			ehci_enable_event(ehci, EHCI_HRTIMER_POLL_ASS, true);
 			return;
 		}
@@ -160,8 +160,8 @@ static void ehci_poll_PSS(struct ehci_hcd *ehci)
 
 	if (want != actual) {
 
-		/* Poll again later, but give up after about 20 ms */
-		if (ehci->PSS_poll_count++ < 20) {
+		/* Poll again later, but give up after about 2-4 ms */
+		if (ehci->PSS_poll_count++ < 2) {
 			ehci_enable_event(ehci, EHCI_HRTIMER_POLL_PSS, true);
 			return;
 		}
