18ea9a162f287fa56bae31e292272f761333431c
Kalle Valo <kvalo@qca.qualcomm.com>
2013-03-18 13:42:20 +0200

    ath6kl: add tracing points for sdio transfers
    Add tracing points for sdio transfers, just dump the address, flags and the
    buffer.
    
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath6kl/trace.c
+++ drivers/net/wireless/ath/ath6kl/trace.c
@@ -15,5 +15,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <linux/module.h>
+
 #define CREATE_TRACE_POINTS
 #include "trace.h"
+
+EXPORT_TRACEPOINT_SYMBOL(ath6kl_sdio);
+EXPORT_TRACEPOINT_SYMBOL(ath6kl_sdio_scat);
