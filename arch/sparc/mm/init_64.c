215fc08350d93b90fd3b33fb9e06a3bdefa72a49
Tkhai Kirill <tkhai@yandex.ru>
2013-03-19 15:12:08 +0000

    sparc64: Do not change num_physpages during initmem freeing
    Common hibernation code looks at num_physpages during suspend and restore.
    Restore is able to be called from initcall, which is before initmem freeing.
    This case leads to restore fail.
    
    Signed-off-by: Kirill Tkhai <tkhai@yandex.ru>
    CC: David Miller <davem@davemloft.net>
    CC: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/init_64.c
+++ arch/sparc/mm/init_64.c
@@ -2126,7 +2126,6 @@ void free_initmem(void)
 			ClearPageReserved(p);
 			init_page_count(p);
 			__free_page(p);
-			num_physpages++;
 			totalram_pages++;
 		}
 	}
@@ -2143,7 +2142,6 @@ void free_initrd_mem(unsigned long start, unsigned long end)
 		ClearPageReserved(p);
 		init_page_count(p);
 		__free_page(p);
-		num_physpages++;
 		totalram_pages++;
 	}
 }
