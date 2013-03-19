2ee98b1c5cbd190af8cfcb5aafb819fb7802d0e7
Eduardo Valentin <eduardo.valentin@ti.com>
2013-03-19 10:54:20 -0400

    staging: ti-soc-thermal: rename Kconfig options
    This patch renames the Kconfig options to cope with
    the new naming convention.
    
    Cc: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Cc: Benoit <b-cousson@ti.com>
    Cc: Nishanth Menon <nm@ti.com>
    Cc: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ti-soc-thermal/ti-thermal.h
+++ drivers/staging/ti-soc-thermal/ti-thermal.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 02110-1301 USA
  *
  */
-#ifndef __OMAP_THERMAL_H
-#define __OMAP_THERMAL_H
+#ifndef __TI_THERMAL_H
+#define __TI_THERMAL_H
 
 #include "ti-bandgap.h"
 
@@ -74,7 +74,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define omap_thermal_is_valid_trip(trip)				\
 	((trip) >= 0 && (trip) < OMAP_TRIP_NUMBER)
 
-#ifdef CONFIG_OMAP_THERMAL
+#ifdef CONFIG_TI_THERMAL
 int omap_thermal_expose_sensor(struct omap_bandgap *bgp, int id,
 			       char *domain);
 int omap_thermal_remove_sensor(struct omap_bandgap *bgp, int id);
