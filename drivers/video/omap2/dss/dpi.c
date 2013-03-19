88410c26194d0da5ef6e9b2792ab5e2adfd19b1e
Tomi Valkeinen <tomi.valkeinen@ti.com>
2013-03-19 11:33:52 +0200

    OMAPDSS: DPI: fix regulators for DT
    On some platforms DPI requires a regulator to be enabled to power up the
    output pins. This regulator is, for some reason, currently attached to
    the virtual omapdss device, instead of the DPI device. This does not
    work for DT, as the regulator mappings need to be described in the DT
    data, and the virtual omapdss device is not present there.
    
    Fix the issue by acquiring the regulator in the DPI device. To retain
    compatibility with the current board files, the old method of getting
    the regulator is kept. The old method can be removed when the board
    files have been changed to pass the regulator to DPI.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dpi.c
+++ drivers/video/omap2/dss/dpi.c
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dss_features.h"
 
 static struct {
+	struct platform_device *pdev;
+
 	struct regulator *vdds_dsi_reg;
 	struct platform_device *dsidev;
 
@@ -556,8 +558,11 @@ static int dpi_init_regulator(void)
 	vdds_dsi = dss_get_vdds_dsi();
 
 	if (IS_ERR(vdds_dsi)) {
-		DSSERR("can't get VDDS_DSI regulator\n");
-		return PTR_ERR(vdds_dsi);
+		vdds_dsi = devm_regulator_get(&dpi.pdev->dev, "vdds_dsi");
+		if (IS_ERR(vdds_dsi)) {
+			DSSERR("can't get VDDS_DSI regulator\n");
+			return PTR_ERR(vdds_dsi);
+		}
 	}
 
 	dpi.vdds_dsi_reg = vdds_dsi;
@@ -707,6 +712,8 @@ static void __exit dpi_uninit_output(struct platform_device *pdev)
 static int omap_dpi_probe(struct platform_device *pdev)
 {
 	int r;
+
+	dpi.pdev = pdev;
 
 	mutex_init(&dpi.lock);
 
