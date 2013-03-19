1fc4fe5276252d32bd939a415dfbd36117d582e2
Jingoo Han <jg1.han@samsung.com>
2013-03-19 01:28:56 +0000

    usb: gadget: mv_u3d: make mv_u3d_pm_ops static
    Make mv_u3d_pm_ops static because mv_u3d_pm_ops is not exported.
    Also, CONFIG_PM_SLEEP is used to remove unnecessary ifdefs.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/mv_u3d_core.c
+++ drivers/usb/gadget/mv_u3d_core.c
@@ -2003,7 +2003,7 @@ err_pdata:
 	return retval;
 }
 
-#ifdef CONFIG_PM
+#ifdef CONFIG_PM_SLEEP
 static int mv_u3d_suspend(struct device *dev)
 {
 	struct mv_u3d *u3d = dev_get_drvdata(dev);
@@ -2046,9 +2046,9 @@ static int mv_u3d_resume(struct device *dev)
 
 	return 0;
 }
-
-SIMPLE_DEV_PM_OPS(mv_u3d_pm_ops, mv_u3d_suspend, mv_u3d_resume);
 #endif
+
+static SIMPLE_DEV_PM_OPS(mv_u3d_pm_ops, mv_u3d_suspend, mv_u3d_resume);
 
 static void mv_u3d_shutdown(struct platform_device *dev)
 {
@@ -2067,9 +2067,7 @@ static struct platform_driver mv_u3d_driver = {
 	.driver		= {
 		.owner	= THIS_MODULE,
 		.name	= "mv-u3d",
-#ifdef CONFIG_PM
 		.pm	= &mv_u3d_pm_ops,
-#endif
 	},
 };
 
