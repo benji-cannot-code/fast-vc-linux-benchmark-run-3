43d03dc2377ca7a51f282bacace86d68633549a3
Arnd Bergmann <arnd@arndb.de>
2013-03-19 15:31:08 +0100

    ARM: sirf: fix prima2 interrupt lookup
    We must not read the interrupts property manually but instead
    use irq_of_parse_and_map() to guarantee that we get the correct
    interrupt number once we stop using the legacy IRQ domain.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Tested-by: Barry Song <Baohua.Song@csr.com>

--- arch/arm/mach-prima2/timer-prima2.c
+++ arch/arm/mach-prima2/timer-prima2.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/slab.h>
 #include <linux/of.h>
+#include <linux/of_irq.h>
 #include <linux/of_address.h>
 #include <mach/map.h>
 #include <asm/sched_clock.h>
@@ -224,7 +225,6 @@ static struct of_device_id timer_ids[] = {
 static void __init sirfsoc_of_timer_map(void)
 {
 	struct device_node *np;
-	const unsigned int *intspec;
 
 	np = of_find_matching_node(NULL, timer_ids);
 	if (!np)
@@ -234,9 +234,7 @@ static void __init sirfsoc_of_timer_map(void)
 		panic("unable to map timer cpu registers\n");
 
 	/* Get the interrupts property */
-	intspec = of_get_property(np, "interrupts", NULL);
-	BUG_ON(!intspec);
-	sirfsoc_timer_irq.irq = be32_to_cpup(intspec);
+	sirfsoc_timer_irq.irq = irq_of_parse_and_map(np, 0);
 
 	of_node_put(np);
 }
