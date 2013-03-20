6999193c71ccb49b1276eb85c3faf43d014f0a6b
David S. Miller <davem@davemloft.net>
2013-03-20 12:09:01 -0700

    sparc64: Kill __ARCH_WANT_UNLOCKED_CTXSW
    As Peter Z. explained at:
    
    https://lkml.org/lkml/2011/12/12/268
    
    it's not needed at all and I even tested it back then.
    This patch just got lost in the shuffle for some reason.
    
    Reported-by: Kirill Tkhai <tkhai@yandex.ru>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/processor_64.h
+++ arch/sparc/include/asm/processor_64.h
@@ -19,9 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ptrace.h>
 #include <asm/page.h>
 
-/* Don't hold the runqueue lock over context switch */
-#define __ARCH_WANT_UNLOCKED_CTXSW
-
 /* The sparc has no problems with write protection */
 #define wp_works_ok 1
 #define wp_works_ok__is_a_macro /* for versions in ksyms.c */
