6b83bae483eb87943e585b57d7a028a0fdea2b28
Michal Simek <michal.simek@xilinx.com>
2013-03-20 11:11:43 +0100

    arm: zynq: Load scu baseaddress at run time
    Use Cortex a9 cp15 to read scu baseaddress.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/common.h
+++ arch/arm/mach-zynq/common.h
@@ -18,4 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MACH_ZYNQ_COMMON_H__
 #define __MACH_ZYNQ_COMMON_H__
 
+extern void __iomem *zynq_scu_base;
+
 #endif
