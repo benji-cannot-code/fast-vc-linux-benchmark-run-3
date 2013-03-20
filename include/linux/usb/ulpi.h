d1fbf838b9ab5bcefdbffd93552532bc7b237721
Fabio Estevam <fabio.estevam@freescale.com>
2013-03-20 10:35:44 -0300

    usb: ulpi: Define a *otg_ulpi_create no-op
    Building a kernel for imx_v4_v5_defconfig with CONFIG_USB_ULPI disabled, results
    in the following error:
    
    arch/arm/mach-imx/built-in.o: In function 'pca100_init':
    platform-mx2-emma.c:(.init.text+0x6788): undefined reference to 'otg_ulpi_create'
    platform-mx2-emma.c:(.init.text+0x682c): undefined reference to 'mxc_ulpi_access_ops'
    
    Fix this by providing a no-op definition of *otg_ulpi_create for the case when
    CONFIG_USB_ULPI is not defined.
    
    Acked-by: Igor Grinberg <grinberg@compulab.co.il>
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- include/linux/usb/ulpi.h
+++ include/linux/usb/ulpi.h
@@ -182,8 +182,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*-------------------------------------------------------------------------*/
 
+#if IS_ENABLED(CONFIG_USB_ULPI)
 struct usb_phy *otg_ulpi_create(struct usb_phy_io_ops *ops,
 					unsigned int flags);
+#else
+static inline struct usb_phy *otg_ulpi_create(struct usb_phy_io_ops *ops,
+					      unsigned int flags)
+{
+	return NULL;
+}
+#endif
 
 #ifdef CONFIG_USB_ULPI_VIEWPORT
 /* access ops for controllers with a viewport register */
