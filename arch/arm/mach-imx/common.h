11bdb0a9a2b823e2387746f901c0e20bf45f3cbf
Anson Huang <b20788@freescale.com>
2013-03-20 19:39:42 -0400

    ARM: imx: enable anatop suspend/resume
    Anatop module have sereval configurations for user
    to reduce the power consumption in suspend, provide
    suspend/resume interface for further use and enable
    fet_odrive to reduce CORE LDO leakage during suspend.
    
    As we have a common anatop file, remove all the operations
    of anatop module in other files, use anatop interfaces to
    do that.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/common.h
+++ arch/arm/mach-imx/common.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright 2004-2007 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2004-2013 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -129,6 +129,11 @@ extern void imx_src_prepare_restart(void);
 extern void imx_gpc_init(void);
 extern void imx_gpc_pre_suspend(void);
 extern void imx_gpc_post_resume(void);
+extern void imx_anatop_init(void);
+extern void imx_anatop_pre_suspend(void);
+extern void imx_anatop_post_resume(void);
+extern void imx_anatop_usb_chrg_detect_disable(void);
+extern u32 imx_anatop_get_digprog(void);
 extern int imx6q_set_lpm(enum mxc_cpu_pwr_mode mode);
 extern void imx6q_set_chicken_bit(void);
 
