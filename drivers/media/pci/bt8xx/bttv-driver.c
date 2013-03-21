c3b1bebdb4c71a169d74accbd19d14fb5f4b8f8b
Frank Schaefer <fschaefer.oss@googlemail.com>
2013-03-21 13:51:20 -0300

    [media] bttv: apply mute settings on open
    Previously, this has been done implicitly for video device nodes by calling
    set_input() (which calls audio_input() and also modified the mute
    setting).
    Since input and mute setting are now untangled (as much as possible), we need to
    apply the mute setting with an explicit call to audio_mute().
    Also apply the mute setting when the radio device node gets opened.
    
    Signed-off-by: Frank Schäfer <fschaefer.oss@googlemail.com>
    Acked-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/pci/bt8xx/bttv-driver.c
+++ drivers/media/pci/bt8xx/bttv-driver.c
@@ -3066,7 +3066,7 @@ static int bttv_open(struct file *file)
 			    fh, &btv->lock);
 	set_tvnorm(btv,btv->tvnorm);
 	set_input(btv, btv->input, btv->tvnorm);
-
+	audio_mute(btv, btv->mute);
 
 	/* The V4L2 spec requires one global set of cropping parameters
 	   which only change on request. These are stored in btv->crop[1].
@@ -3231,6 +3231,7 @@ static int radio_open(struct file *file)
 	v4l2_fh_init(&fh->fh, vdev);
 
 	btv->radio_user++;
+	audio_mute(btv, btv->mute);
 
 	v4l2_fh_add(&fh->fh);
 
