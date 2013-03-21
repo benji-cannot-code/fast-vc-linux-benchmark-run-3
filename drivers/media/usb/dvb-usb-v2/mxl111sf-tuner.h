63a018f98b5ff591156abbd3a84c12ad15ed9cbc
Mauro Carvalho Chehab <mchehab@redhat.com>
2013-03-21 16:29:36 -0300

    [media] dvb-usb/dvb-usb-v2: use IS_ENABLED
    Instead of checking everywhere there for 3 symbols, use instead
    IS_ENABLED macro.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/dvb-usb-v2/mxl111sf-tuner.h
+++ drivers/media/usb/dvb-usb-v2/mxl111sf-tuner.h
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MXL111SF_TUNER_H__
 #define __MXL111SF_TUNER_H__
 
+#include <linux/kconfig.h>
 #include "dvb_frontend.h"
-
 #include "mxl111sf.h"
 
 enum mxl_if_freq {
@@ -61,8 +61,7 @@ struct mxl111sf_tuner_config {
 
 /* ------------------------------------------------------------------------ */
 
-#if defined(CONFIG_DVB_USB_MXL111SF) || \
-	(defined(CONFIG_DVB_USB_MXL111SF_MODULE) && defined(MODULE))
+#if IS_ENABLED(CONFIG_DVB_USB_MXL111SF)
 extern
 struct dvb_frontend *mxl111sf_tuner_attach(struct dvb_frontend *fe,
 					   struct mxl111sf_state *mxl_state,
