a00a6212f1484073e200880e45e7b5025e66a4e7
Stephen Warren <swarren@nvidia.com>
2013-03-21 08:08:46 +0000

    i2c: tegra: assume CONFIG_OF, remove platform data
    Tegra only supports, and always enables, device tree. Remove all ifdefs
    and runtime checks for DT support from the driver. Platform data is
    therefore no longer required. Delete the header that defines it.
    
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- arch/arm/mach-tegra/board-dt-tegra20.c
+++ arch/arm/mach-tegra/board-dt-tegra20.c
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pda_power.h>
 #include <linux/platform_data/tegra_usb.h>
 #include <linux/io.h>
-#include <linux/i2c.h>
-#include <linux/i2c-tegra.h>
 #include <linux/usb/tegra_usb_phy.h>
 
 #include <asm/mach-types.h>
