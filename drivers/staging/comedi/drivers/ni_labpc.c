2ccbc882b7c5ee8ae989531fc0e05b315def4061
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-03-22 09:45:41 -0700

    staging: comedi: ni_labpc: move declaration of local var 'dma_flags'
    In labpc_common_attach(), move the declaration of the local variable
    'dma_flags' to remove the need for the extra #ifdef/#endif.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc.c
+++ drivers/staging/comedi/drivers/ni_labpc.c
@@ -1669,12 +1669,9 @@ int labpc_common_attach(struct comedi_device *dev, unsigned long iobase,
 	const struct labpc_boardinfo *board = comedi_board(dev);
 	struct labpc_private *devpriv = dev->private;
 	struct comedi_subdevice *s;
-	int i;
 	unsigned long isr_flags;
-#ifdef CONFIG_ISA_DMA_API
-	unsigned long dma_flags;
-#endif
 	int ret;
+	int i;
 
 	dev_info(dev->class_dev, "ni_labpc: %s\n", board->name);
 	if (iobase == 0) {
@@ -1729,6 +1726,8 @@ int labpc_common_attach(struct comedi_device *dev, unsigned long iobase,
 		dev_err(dev->class_dev, "invalid dma channel %u\n", dma_chan);
 		return -EINVAL;
 	} else if (dma_chan) {
+		unsigned long dma_flags;
+
 		/* allocate dma buffer */
 		devpriv->dma_buffer = kmalloc(dma_buffer_size,
 					      GFP_KERNEL | GFP_DMA);
