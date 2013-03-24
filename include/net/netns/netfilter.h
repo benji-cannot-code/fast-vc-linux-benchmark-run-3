eb4e30eefa998bc69c4035ceb1e950c108b844e8
Gao feng <gaofeng@cn.fujitsu.com>
2013-03-24 23:50:39 +0000

    netfilter: make /proc/net/netfilter pernet
    This patch makes this proc dentry pernet. So far only init_net
    had a /proc/net/netfilter directory.
    
    Signed-off-by: Gao feng <gaofeng@cn.fujitsu.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

---
+++ include/net/netns/netfilter.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __NETNS_NETFILTER_H
+#define __NETNS_NETFILTER_H
+
+#include <linux/proc_fs.h>
+
+struct netns_nf {
+#if defined CONFIG_PROC_FS
+	struct proc_dir_entry *proc_netfilter;
+#endif
+};
+#endif
