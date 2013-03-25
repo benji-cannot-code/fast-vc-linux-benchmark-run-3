8084e04d2b5f4162a48d116cb4925d100f3d5100
Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
2013-03-25 12:09:02 -0500

    MIPS: microMIPS: Floating point support.
    Add logic needed to do floating point emulation in microMIPS mode.
    
    Signed-off-by: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Signed-off-by: Steven J. Hill <Steven. Hill@imgtec.com>

--- arch/mips/include/asm/inst.h
+++ arch/mips/include/asm/inst.h
@@ -74,4 +74,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 typedef unsigned int mips_instruction;
 
+/* microMIPS instruction decode structure. Do NOT export!!! */
+struct mm_decoded_insn {
+	mips_instruction insn;
+	mips_instruction next_insn;
+	int pc_inc;
+	int next_pc_inc;
+	int micro_mips_mode;
+};
+
 #endif /* _ASM_INST_H */
