bab5dabe3d17e9c5a29b265627c5407fdb19264d
Heiko Carstens <heiko.carstens@de.ibm.com>
2013-03-25 17:22:55 +0100

    KVM: s390: fix compile with !CONFIG_COMPAT
    arch/s390/kvm/priv.c should include both
    linux/compat.h and asm/compat.h.
    Fixes this one:
    
    In file included from arch/s390/kvm/priv.c:23:0:
    arch/s390/include/asm/compat.h: In function ‘arch_compat_alloc_user_space’:
    arch/s390/include/asm/compat.h:258:2: error: implicit declaration of function ‘is_compat_task’
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Gleb Natapov <gleb@redhat.com>

--- arch/s390/kvm/priv.c
+++ arch/s390/kvm/priv.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kvm.h>
 #include <linux/gfp.h>
 #include <linux/errno.h>
+#include <linux/compat.h>
 #include <asm/asm-offsets.h>
 #include <asm/current.h>
 #include <asm/debug.h>
