55de52d771dd67293869ea6ec6cccde63ba816c2
Christian Lamparter <chunkeey@googlemail.com>
2013-03-25 17:15:17 +0100

    carl9170: remove fast channel change feature
    Marco Fonseca reported a issue with his carl9170 device:
    "I'm seeing a problem with the carl driver. If I change channels
    repeatedly on the 2.4ghz band, monitoring (e.g. tcpdump) will
    eventually halt.  I've seen this on various versions of the carl
    driver/firmware (both from 1.9.4 to 1.9.7)"
    <http://marc.info/?l=linux-wireless&m=136381302428113>
    
    The culprit was identified as "fast channel change feature" which
    according to Adrian Chadd is: "... notoriously unreliable and
    really only fully debugged on some very later chips."
    <http://marc.info/?l=linux-wireless&m=136416984531380>
    
    Therefore, this patch removes the fast channel change feature.
    The phy will now always have to go through a cold reset when
    changing channels, but it should no longer become deaf.
    
    Cc: Marco Fonseca <marco@tampabay.rr.com>
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/carl9170/carl9170.h
+++ drivers/net/wireless/ath/carl9170/carl9170.h
@@ -71,12 +71,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static const u8 ar9170_qmap[__AR9170_NUM_TXQ] = { 3, 2, 1, 0 };
 
-enum carl9170_rf_init_mode {
-	CARL9170_RFI_NONE,
-	CARL9170_RFI_WARM,
-	CARL9170_RFI_COLD,
-};
-
 #define CARL9170_MAX_RX_BUFFER_SIZE		8192
 
 enum carl9170_device_state {
@@ -600,7 +594,7 @@ int carl9170_led_set_state(struct ar9170 *ar, const u32 led_state);
 
 /* PHY / RF */
 int carl9170_set_channel(struct ar9170 *ar, struct ieee80211_channel *channel,
-	enum nl80211_channel_type bw, enum carl9170_rf_init_mode rfi);
+			 enum nl80211_channel_type bw);
 int carl9170_get_noisefloor(struct ar9170 *ar);
 
 /* FW */
