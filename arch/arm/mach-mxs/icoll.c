db1ab047c3f6de3039d99d7a9200de5e7e979883
Shawn Guo <shawn.guo@linaro.org>
2013-03-25 21:13:22 +0800

    ARM: mxs: get icoll base address from device tree
    Rather than using the static definition, it gets icoll base address
    with mapping from device tree.
    
    As the result, <mach/mxs.h> inclusion can be removed from the driver
    now.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-mxs/icoll.c
+++ arch/arm/mach-mxs/icoll.c
@@ -23,9 +23,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irqdomain.h>
 #include <linux/io.h>
 #include <linux/of.h>
+#include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <asm/exception.h>
-#include <mach/mxs.h>
 #include <mach/common.h>
 
 #define HW_ICOLL_VECTOR				0x0000
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ICOLL_NUM_IRQS		128
 
-static void __iomem *icoll_base = MXS_IO_ADDRESS(MXS_ICOLL_BASE_ADDR);
+static void __iomem *icoll_base;
 static struct irq_domain *icoll_domain;
 
 static void icoll_ack_irq(struct irq_data *d)
@@ -104,6 +104,9 @@ static struct irq_domain_ops icoll_irq_domain_ops = {
 static void __init icoll_of_init(struct device_node *np,
 			  struct device_node *interrupt_parent)
 {
+	icoll_base = of_iomap(np, 0);
+	WARN_ON(!icoll_base);
+
 	/*
 	 * Interrupt Collector reset, which initializes the priority
 	 * for each irq to level 0.
