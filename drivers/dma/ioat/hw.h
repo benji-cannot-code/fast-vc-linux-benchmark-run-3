4da531d5e8e64242496be70ceee46d5ce87c75f0
Dave Jiang <dave.jiang@intel.com>
2013-03-26 15:42:35 -0700

    ioatdma: Adding PCI IDs for Intel Atom S1200 product family ioatdma devices
    These should be good for the IOAT DMA devices on the Intel Atom S1269,
    S1279, and S1289 platforms. We are also adding IOAT v3.3 definition for
    the new DMA engine.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Acked-by: Dan Williams <djbw@fb.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/ioat/hw.h
+++ drivers/dma/ioat/hw.h
@@ -53,10 +53,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCI_DEVICE_ID_INTEL_IOAT_HSW8	0x2f2e
 #define PCI_DEVICE_ID_INTEL_IOAT_HSW9	0x2f2f
 
+#define PCI_DEVICE_ID_INTEL_IOAT_BWD0	0x0C50
+#define PCI_DEVICE_ID_INTEL_IOAT_BWD1	0x0C51
+#define PCI_DEVICE_ID_INTEL_IOAT_BWD2	0x0C52
+#define PCI_DEVICE_ID_INTEL_IOAT_BWD3	0x0C53
+
 #define IOAT_VER_1_2            0x12    /* Version 1.2 */
 #define IOAT_VER_2_0            0x20    /* Version 2.0 */
 #define IOAT_VER_3_0            0x30    /* Version 3.0 */
 #define IOAT_VER_3_2            0x32    /* Version 3.2 */
+#define IOAT_VER_3_3            0x33    /* Version 3.3 */
 
 
 int system_has_dca_enabled(struct pci_dev *pdev);
