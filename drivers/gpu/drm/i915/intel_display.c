8524e38cf3642b80c39e4686c3ff89de674ab59b
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-03-27 00:44:51 +0100

    drm/i915: compute pipe_config earlier
    To make decent modeset state checking possible (e.g. for the check
    mode with atomic modesetting) we want to have the full pipe
    configuration and state checks done before we touch the hw.
    
    To ensure that all the little bits&pieces that are now moved to the
    pipe_config handle this correctly, move its computation to the right
    spot now, before we touch the hw in the disable_pipes step.
    
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -7858,12 +7858,6 @@ int intel_set_mode(struct drm_crtc *crtc,
 	intel_modeset_affected_pipes(crtc, &modeset_pipes,
 				     &prepare_pipes, &disable_pipes);
 
-	DRM_DEBUG_KMS("set mode pipe masks: modeset: %x, prepare: %x, disable: %x\n",
-		      modeset_pipes, prepare_pipes, disable_pipes);
-
-	for_each_intel_crtc_masked(dev, disable_pipes, intel_crtc)
-		intel_crtc_disable(&intel_crtc->base);
-
 	*saved_hwmode = crtc->hwmode;
 	*saved_mode = crtc->mode;
 
@@ -7881,6 +7875,12 @@ int intel_set_mode(struct drm_crtc *crtc,
 			goto out;
 		}
 	}
+
+	DRM_DEBUG_KMS("set mode pipe masks: modeset: %x, prepare: %x, disable: %x\n",
+		      modeset_pipes, prepare_pipes, disable_pipes);
+
+	for_each_intel_crtc_masked(dev, disable_pipes, intel_crtc)
+		intel_crtc_disable(&intel_crtc->base);
 
 	for_each_intel_crtc_masked(dev, prepare_pipes, intel_crtc) {
 		if (intel_crtc->base.enabled)
