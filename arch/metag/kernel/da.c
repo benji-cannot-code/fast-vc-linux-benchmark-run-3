8bbf3248ea1bae75d9a047a0a38074c2522505c5
James Hogan <james.hogan@imgtec.com>
2013-03-26 14:10:42 +0000

    metag: export _metag_da_present and cpu_2_hwthread_id
    Export the symbols _metag_da_present and cpu_2_hwthread_id to modules
    (GPL only) to allow the imgdafs file system to be built as a module.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>

--- arch/metag/kernel/da.c
+++ arch/metag/kernel/da.c
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 
+#include <linux/export.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
 #include <asm/da.h>
 #include <asm/metag_mem.h>
 
 bool _metag_da_present;
+EXPORT_SYMBOL_GPL(_metag_da_present);
 
 int __init metag_da_probe(void)
 {
