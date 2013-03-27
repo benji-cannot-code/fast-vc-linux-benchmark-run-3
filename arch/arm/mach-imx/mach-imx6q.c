fc612034d18531ef211bd4e83464afccc50c4734
Philipp Zabel <p.zabel@pengutronix.de>
2013-03-27 18:30:37 +0100

    ARM i.MX6q: export imx6q_revision
    So it can be used in clk-imx6q.c for revision dependent clock tree setup.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cpuidle.h"
 #include "hardware.h"
 
-static int imx6q_revision(void)
+int imx6q_revision(void)
 {
 	static u32 rev;
 
