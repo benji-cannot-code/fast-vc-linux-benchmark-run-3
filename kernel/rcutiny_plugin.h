eba6f1588b2d2c9b24b83fa12f53544d581c434a
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-03-27 09:02:40 -0700

    rcu: Remove rcu_preempt_check_callbacks()
    With the removal of CONFIG_TINY_PREEMPT_RCU, rcu_preempt_check_callbacks()
    is now an empty function.  This commit therefore eliminates it by
    inlining it.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- kernel/rcutiny_plugin.h
+++ kernel/rcutiny_plugin.h
@@ -105,14 +105,6 @@ static void check_cpu_stalls(void)
 
 /*
  * Because preemptible RCU does not exist, it never has any callbacks
- * to check.
- */
-static void rcu_preempt_check_callbacks(void)
-{
-}
-
-/*
- * Because preemptible RCU does not exist, it never has any callbacks
  * to remove.
  */
 static void rcu_preempt_remove_callbacks(struct rcu_ctrlblk *rcp)
