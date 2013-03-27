dd7a4f76ff038693ffba307c187962f34a7e57e8
Aaro Koskinen <aaro.koskinen@iki.fi>
2013-03-27 23:53:08 +0200

    staging: xgifb: delete IF_DEF_TRUMPION and IF_DEF_DSTN
    Delete some unused flags.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/vb_struct.h
+++ drivers/staging/xgifb/vb_struct.h
@@ -146,7 +146,7 @@ struct vb_device_info {
 	unsigned short   LCDHRS, LCDVRS, LCDHDES, LCDVDES;
 
 	unsigned short   ModeType;
-	unsigned short   IF_DEF_LVDS, IF_DEF_TRUMPION, IF_DEF_DSTN;
+	unsigned short   IF_DEF_LVDS;
 	unsigned short   IF_DEF_CRT2Monitor;
 	unsigned short   IF_DEF_YPbPr;
 	unsigned short   IF_DEF_HiVision;
