bb517c7a6f22a4e263a88f22775410076f7f460f
Emil Goode <emilgoode@gmail.com>
2013-03-27 17:47:49 +0100

    staging: comedi: amplc_dio200: fix C99 array initializer warnings
    This patch fixes the following sparse warnings about use of
    obsolete array initializer:
    
    drivers/staging/comedi/drivers/amplc_dio200_pci.c:256:24: warning:
    	obsolete array initializer, use C99 syntax
    drivers/staging/comedi/drivers/amplc_dio200_pci.c:269:24: warning:
    	obsolete array initializer, use C99 syntax
    drivers/staging/comedi/drivers/amplc_dio200_pci.c:281:25: warning:
    	obsolete array initializer, use C99 syntax
    drivers/staging/comedi/drivers/amplc_dio200_pci.c:298:25: warning:
    	obsolete array initializer, use C99 syntax
    drivers/staging/comedi/drivers/amplc_dio200_pci.c:315:25: warning:
    	obsolete array initializer, use C99 syntax
    
    Signed-off-by: Emil Goode <emilgoode@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_dio200_pci.c
+++ drivers/staging/comedi/drivers/amplc_dio200_pci.c
@@ -254,7 +254,7 @@ enum dio200_pci_model {
 };
 
 static const struct dio200_board dio200_pci_boards[] = {
-	[pci215_model] {
+	[pci215_model] = {
 		.name = "pci215",
 		.bustype = pci_bustype,
 		.mainbar = 2,
@@ -267,7 +267,7 @@ static const struct dio200_board dio200_pci_boards[] = {
 			.has_clk_gat_sce = true,
 		},
 	},
-	[pci272_model] {
+	[pci272_model] = {
 		.name = "pci272",
 		.bustype = pci_bustype,
 		.mainbar = 2,
@@ -279,7 +279,7 @@ static const struct dio200_board dio200_pci_boards[] = {
 			.has_int_sce = true,
 		},
 	},
-	[pcie215_model] {
+	[pcie215_model] = {
 		.name = "pcie215",
 		.bustype = pci_bustype,
 		.mainbar = 1,
@@ -296,7 +296,7 @@ static const struct dio200_board dio200_pci_boards[] = {
 			.has_enhancements = true,
 		},
 	},
-	[pcie236_model] {
+	[pcie236_model] = {
 		.name = "pcie236",
 		.bustype = pci_bustype,
 		.mainbar = 1,
@@ -313,7 +313,7 @@ static const struct dio200_board dio200_pci_boards[] = {
 			.has_enhancements = true,
 		},
 	},
-	[pcie296_model] {
+	[pcie296_model] = {
 		.name = "pcie296",
 		.bustype = pci_bustype,
 		.mainbar = 1,
