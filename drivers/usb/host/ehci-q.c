e43ee1a5a719873a5f424904c8565155a3315186
Arnd Bergmann <arnd@arndb.de>
2013-03-28 21:09:46 +0000

    usb: ehci: mark unlink_empty_async_suspended() as __maybe_unused
    Patch 4d053fdac3 "usb: ehci: unlink_empty_async_suspended() only used
    with CONFIG_PM" tried to hide the unlink_empty_async_suspended function
    inside of an #ifdef to work around an unused function warning.
    
    Unfortunately that had the effect of introducing a new warning:
    
    drivers/usb/host/ehci-q.c:1297:13: warning: 'unlink_empty_async_suspended'
    	declared 'static' but never defined [-Wunused-function]
    
    While we could add another #ifdef around the function declaration to avoid
    this, a nicer solution is to mark it as __maybe_unused, which will let
    gcc silently drop the function definition when it is not needed.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-q.c
+++ drivers/usb/host/ehci-q.c
@@ -1294,9 +1294,8 @@ static void unlink_empty_async(struct ehci_hcd *ehci)
 	}
 }
 
-#ifdef CONFIG_PM
 /* The root hub is suspended; unlink all the async QHs */
-static void unlink_empty_async_suspended(struct ehci_hcd *ehci)
+static void __maybe_unused unlink_empty_async_suspended(struct ehci_hcd *ehci)
 {
 	struct ehci_qh		*qh;
 
@@ -1307,7 +1306,6 @@ static void unlink_empty_async_suspended(struct ehci_hcd *ehci)
 	}
 	start_iaa_cycle(ehci);
 }
-#endif
 
 /* makes sure the async qh will become idle */
 /* caller must own ehci->lock */
