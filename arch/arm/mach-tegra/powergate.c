7bed006d76e8685e458e99e136f0fa0cca8fa183
Thierry Reding <thierry.reding@avionic-design.de>
2013-03-28 21:35:03 +0100

    ARM: tegra: Export tegra_powergate_sequence_power_up()
    This function can be used by drivers to enable power to the hardware
    blocks that they drive. Most of the drivers can be built as a module
    and therefore require this function to be exported.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/powergate.c
+++ arch/arm/mach-tegra/powergate.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include <linux/delay.h>
 #include <linux/err.h>
+#include <linux/export.h>
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/seq_file.h>
@@ -169,6 +170,7 @@ err_clk:
 err_power:
 	return ret;
 }
+EXPORT_SYMBOL(tegra_powergate_sequence_power_up);
 
 int tegra_cpu_powergate_id(int cpuid)
 {
