7cdb16ddd8fa6d1c2dcf4b6f92b9e83b775d08ed
Gabor Juhos <juhosg@openwrt.org>
2013-03-29 15:52:27 +0100

    rt2x00: rt2x00pci: fix build error on Ralink RT3x5x SoCs
    The rt2800pci driver supports the built-in wireless
    MAC of the Ralink RT3x5x SoCs. However building the
    driver for these SoCs leads to the following error:
    
        LD      init/built-in.o
      drivers/built-in.o: In function `rt2800pci_rxdone_tasklet':
      <...>/drivers/net/wireless/rt2x00/rt2800pci.c:1012: undefined reference to `rt2x00pci_rxdone'
      drivers/built-in.o:(.rodata+0x4780): undefined reference to `rt2x00pci_initialize'
      drivers/built-in.o:(.rodata+0x4784): undefined reference to `rt2x00pci_uninitialize'
      drivers/built-in.o:(.rodata+0x47bc): undefined reference to `rt2x00pci_flush_queue'
      drivers/built-in.o:(.rodata+0x4818): undefined reference to `rt2x00pci_regbusy_read'
      make[5]: *** [vmlinux] Error 1
    
    The missing functions are provided by the rt2x00pci
    module. This module is only selected by the rt2800pci
    driver if PCI support is enabled in the kernel, because
    some parts of the rt2x00pci code depends on PCI support.
    
    PCI support is not available on the RT3x5x SoCs because
    those have no PCI host controller at all.
    
    Move the non PCI specific code from rt2x00pci into a
    separate module. This makes it possible to use that
    code even if PCI support is disabled. The affected
    functions are used by all of the rt2x00 PCI drivers
    so select the new module for those drivers.
    
    Signed-off-by: Gabor Juhos <juhosg@openwrt.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2400pci.c
+++ drivers/net/wireless/rt2x00/rt2400pci.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 
 #include "rt2x00.h"
+#include "rt2x00mmio.h"
 #include "rt2x00pci.h"
 #include "rt2400pci.h"
 
