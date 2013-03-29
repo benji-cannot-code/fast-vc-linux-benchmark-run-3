0a5e5a9838371cda0f4fc0fe30843bad9239b241
Shawn Guo <shawn.guo@linaro.org>
2013-03-29 09:45:31 +0800

    ARM: mxs: remove system.c
    There is no user of function mxs_reset_block() now.  Let's move
    mxs_restart() into mach-mxs.c as a static function and remove system.c
    completely.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-mxs/include/mach/common.h
+++ arch/arm/mach-mxs/include/mach/common.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __MACH_MXS_COMMON_H__
 
 extern const u32 *mxs_get_ocotp(void);
-extern int mxs_reset_block(void __iomem *);
-extern void mxs_restart(char, const char *);
 extern int mxs_saif_clkmux_select(unsigned int clkmux);
 
 extern int mx23_clocks_init(void);
