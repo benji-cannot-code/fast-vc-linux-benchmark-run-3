e182184d2db3ccddd1f435e3e42ba53c4c0944d4
Michael Grzeschik <m.grzeschik@pengutronix.de>
2013-03-30 12:54:10 +0200

    usb: chipidea: udc: add the define TD_PAGE_COUNT and fix all users
    A static count of transfer descriptors was used everywhere in the driver
    with the fixed number 5. This patch adds a define, named TD_PAGE_COUNT,
    and replaces all users of this value. This way its possible to have only
    one parameter to change and limit the amount of buffer pointers per TD.
    
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Reviewed-by: Peter Chen <peter.chen@freescale.com>
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/chipidea/ci.h
+++ drivers/usb/chipidea/ci.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /******************************************************************************
  * DEFINE
  *****************************************************************************/
+#define TD_PAGE_COUNT      5
 #define CI13XXX_PAGE_SIZE  4096ul /* page size for TD's */
 #define ENDPT_MAX          32
 
