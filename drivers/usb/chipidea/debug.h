ee5a6085f17838c419f75ac3b1d00881319bd5a6
Alexander Shishkin <alexander.shishkin@linux.intel.com>
2013-03-30 12:53:51 +0200

    usb: chipidea: convert debug entries in sysfs to debugfs
    Currently, we have a bunch of files in sysfs that display all sorts of
    debugging information for the device controller, so they have to move to
    debugfs where they belong. The "registers" interface have been removed,
    since it doesn't fit into the current driver design as is and it's hardly
    a good idea to touch the registers from userspace anyway.
    
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/chipidea/debug.h
+++ drivers/usb/chipidea/debug.h
@@ -15,17 +15,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __DRIVERS_USB_CHIPIDEA_DEBUG_H
 
 #ifdef CONFIG_USB_CHIPIDEA_DEBUG
-int dbg_create_files(struct device *dev);
-int dbg_remove_files(struct device *dev);
+int dbg_create_files(struct ci13xxx *ci);
+void dbg_remove_files(struct ci13xxx *ci);
 #else
-static inline int dbg_create_files(struct device *dev)
+static inline int dbg_create_files(struct ci13xxx *ci)
 {
 	return 0;
 }
 
-static inline int dbg_remove_files(struct device *dev)
+static inline void dbg_remove_files(struct ci13xxx *ci)
 {
-	return 0;
 }
 #endif
 
