2e03eb3fe995cc314656b83f764a91b11e7aac39
Peter Chen <peter.chen@freescale.com>
2013-03-30 02:46:43 +0200

    Revert "USB: chipidea: add vbus detect for udc"
    There are several problems with this patch:
      + in introduces a sparse warning for a condition that's always negative,
      + and because of that, it actually doesn't do anything useful,
      + and vbus detection belongs to otg, not device function anyway.
    
    This reverts commit 8c4fc031954b4eb72daf13d3c907a985a3eee208.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>
    [Alex: amended the above text]
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/chipidea/ci.h
+++ drivers/usb/chipidea/ci.h
@@ -140,7 +140,6 @@ struct ci13xxx {
 	enum ci_role			role;
 	bool				is_otg;
 	struct work_struct		work;
-	struct work_struct		vbus_work;
 	struct workqueue_struct		*wq;
 
 	struct dma_pool			*qh_pool;
