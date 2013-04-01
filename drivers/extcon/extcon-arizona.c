8d40e68fe5a527695a82877131e09390fc810100
Mark Brown <broonie@opensource.wolfsonmicro.com>
2013-04-01 19:09:45 +0100

    extcon: arizona: Retry HPDET identification for high impedance
    Sometimes we can trigger measurements early if contacts are shorted during
    a slow insertion. As well as debouncing add further robustness by retrying
    if we get a high impedance measurement for headphones as this can indicate
    that the headphones were not yet connected.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/extcon/extcon-arizona.c
+++ drivers/extcon/extcon-arizona.c
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARIZONA_ACCDET_MODE_HPL 1
 #define ARIZONA_ACCDET_MODE_HPR 2
 
+#define ARIZONA_HPDET_MAX 10000
+
 #define HPDET_DEBOUNCE 500
 
 struct arizona_extcon_info {
@@ -65,6 +67,7 @@ struct arizona_extcon_info {
 
 	bool hpdet_active;
 	bool hpdet_done;
+	bool hpdet_retried;
 
 	int num_hpdet_res;
 	unsigned int hpdet_res[3];
@@ -112,6 +115,8 @@ static const char *arizona_cable[] = {
 	"Line-out",
 	NULL,
 };
+
+static void arizona_start_hpdet_acc_id(struct arizona_extcon_info *info);
 
 static void arizona_extcon_do_magic(struct arizona_extcon_info *info,
 				    unsigned int magic)
@@ -394,7 +399,7 @@ static int arizona_hpdet_read(struct arizona_extcon_info *info)
 		/* If we go out of range report top of range */
 		if (val < 100 || val > 0x3fb) {
 			dev_dbg(arizona->dev, "Measurement out of range\n");
-			return 10000;
+			return ARIZONA_HPDET_MAX;
 		}
 
 		dev_dbg(arizona->dev, "HPDET read %d in range %d\n",
@@ -518,6 +523,16 @@ static int arizona_hpdet_do_id(struct arizona_extcon_info *info, int *reading)
 
 		/* Take the headphone impedance for the main report */
 		*reading = info->hpdet_res[0];
+
+		/* Sometimes we get false readings due to slow insert */
+		if (*reading >= ARIZONA_HPDET_MAX && !info->hpdet_retried) {
+			dev_dbg(arizona->dev, "Retrying high impedance\n");
+			info->num_hpdet_res = 0;
+			info->hpdet_retried = true;
+			arizona_start_hpdet_acc_id(info);
+			pm_runtime_put(info->dev);
+			return -EAGAIN;
+		}
 
 		/*
 		 * Either the two grounds measure differently or we
@@ -954,6 +969,7 @@ static irqreturn_t arizona_jackdet(int irq, void *data)
 			info->hpdet_res[i] = 0;
 		info->mic = false;
 		info->hpdet_done = false;
+		info->hpdet_retried = false;
 
 		for (i = 0; i < info->num_micd_ranges; i++)
 			input_report_key(info->input,
