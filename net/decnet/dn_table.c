392b43ead71b8600b7d1ffa999d16653e7ed2e8d
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-04-03 15:33:07 +0000

    decnet: remove duplicated include from dn_table.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/decnet/dn_table.c
+++ net/decnet/dn_table.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sockios.h>
 #include <linux/init.h>
 #include <linux/skbuff.h>
-#include <net/netlink.h>
 #include <linux/rtnetlink.h>
 #include <linux/proc_fs.h>
 #include <linux/netdevice.h>
