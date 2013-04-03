83d5c12c9501f0ee5b965569fe8984e4e2f47de1
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-04-03 13:39:14 -0700

    staging: comedi: ni_pcimio: use comedi provided range_bipolar10
    Remove the private range, range_ni_M_622x_ao, in this driver and use
    the comedi provided range_bipolar10 instead.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_pcimio.c
+++ drivers/staging/comedi/drivers/ni_pcimio.c
@@ -158,11 +158,6 @@ static const struct comedi_lrange range_ni_M_625x_ao = { 3, {
 							     }
 };
 
-static const struct comedi_lrange range_ni_M_622x_ao = { 1, {
-							     RANGE(-10, 10),
-							     }
-};
-
 enum ni_pcimio_boardid {
 	BOARD_PCIMIO_16XE_50,
 	BOARD_PCIMIO_16XE_10,
@@ -761,7 +756,7 @@ static const struct ni_board_struct ni_boards[] = {
 		.n_aochan	= 2,
 		.aobits		= 16,
 		.ao_fifo_depth	= 8191,
-		.ao_range_table	= &range_ni_M_622x_ao,
+		.ao_range_table	= &range_bipolar10,
 		.reg_type	= ni_reg_622x,
 		.ao_speed	= 1200,
 		.num_p0_dio_channels = 8,
@@ -777,7 +772,7 @@ static const struct ni_board_struct ni_boards[] = {
 		.n_aochan	= 2,
 		.aobits		= 16,
 		.ao_fifo_depth	= 8191,
-		.ao_range_table	= &range_ni_M_622x_ao,
+		.ao_range_table	= &range_bipolar10,
 		.reg_type	= ni_reg_622x,
 		.ao_speed	= 1200,
 		.num_p0_dio_channels = 8,
@@ -815,7 +810,7 @@ static const struct ni_board_struct ni_boards[] = {
 		.n_aochan	= 2,
 		.aobits		= 16,
 		.ao_fifo_depth	= 8191,
-		.ao_range_table	= &range_ni_M_622x_ao,
+		.ao_range_table	= &range_bipolar10,
 		.reg_type	= ni_reg_622x,
 		.ao_speed	= 1200,
 		.num_p0_dio_channels = 32,
@@ -831,7 +826,7 @@ static const struct ni_board_struct ni_boards[] = {
 		.n_aochan	= 2,
 		.aobits		= 16,
 		.ao_fifo_depth	= 8191,
-		.ao_range_table	= &range_ni_M_622x_ao,
+		.ao_range_table	= &range_bipolar10,
 		.reg_type	= ni_reg_622x,
 		.ao_speed	= 1200,
 		.num_p0_dio_channels = 32,
@@ -847,7 +842,7 @@ static const struct ni_board_struct ni_boards[] = {
 		.n_aochan	= 4,
 		.aobits		= 16,
 		.ao_fifo_depth	= 8191,
-		.ao_range_table	= &range_ni_M_622x_ao,
+		.ao_range_table	= &range_bipolar10,
 		.reg_type	= ni_reg_622x,
 		.ao_speed	= 1200,
 		.num_p0_dio_channels = 32,
