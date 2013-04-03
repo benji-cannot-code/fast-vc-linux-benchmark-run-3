840072a75cf4b09e0695fb50efe22b227657a4c4
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-04-04 01:15:45 +0200

    Revert "drm/i915: fix DP get_hw_state return value"
    This reverts commit deb18211a110c102d32b3e9ed866bd7d25e0f8d5.
    
    It completely breaks the logic, since when we fall through to the end
    of the function we actually _have_ figured out the correct pipe.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -1329,7 +1329,7 @@ static bool intel_dp_get_hw_state(struct intel_encoder *encoder,
 			      intel_dp->output_reg);
 	}
 
-	return false;
+	return true;
 }
 
 static void intel_disable_dp(struct intel_encoder *encoder)
