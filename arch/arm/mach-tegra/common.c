e07f2050d09ead2e135950f77bf56691ae432f3d
Joseph Lo <josephl@nvidia.com>
2013-04-03 19:31:45 +0800

    ARM: tegra: irq: add wake up handling
    Add the wake up handling for legacy irq controller, and using
    IRQCHIP_MASK_ON_SUSPEND for wake irq handling.
    
    Based on the work by:
    Varun Wadekar <vwadekar@nvidia.com>
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "common.h"
 #include "fuse.h"
 #include "iomap.h"
+#include "irq.h"
 #include "pmc.h"
 #include "apbio.h"
 #include "sleep.h"
@@ -65,6 +66,7 @@ void __init tegra_dt_init_irq(void)
 	tegra_pmc_init();
 	tegra_init_irq();
 	irqchip_init();
+	tegra_legacy_irq_syscore_init();
 }
 #endif
 
