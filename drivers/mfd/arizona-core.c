84eeee8fa8c234c614e0faeb628711cf5af658e8
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2013-04-03 12:53:37 +0100

    mfd: arizona: Wait for internal clocks to startup after reset
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/arizona-core.c
+++ drivers/mfd/arizona-core.c
@@ -530,8 +530,10 @@ int arizona_dev_init(struct arizona *arizona)
 		goto err_enable;
 	}
 
-	if (arizona->pdata.reset)
+	if (arizona->pdata.reset) {
 		gpio_set_value_cansleep(arizona->pdata.reset, 1);
+		msleep(1);
+	}
 
 	regcache_cache_only(arizona->regmap, false);
 
@@ -589,6 +591,8 @@ int arizona_dev_init(struct arizona *arizona)
 			dev_err(dev, "Failed to reset device: %d\n", ret);
 			goto err_reset;
 		}
+
+		msleep(1);
 
 		ret = regcache_sync(arizona->regmap);
 		if (ret != 0) {
