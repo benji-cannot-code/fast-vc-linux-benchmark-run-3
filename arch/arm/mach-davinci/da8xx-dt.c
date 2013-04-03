5e81ad1bc1157056296ad7a2efd8925a8c5d9c90
Manjunathappa, Prakash <prakash.pm@ti.com>
2013-04-03 19:39:09 +0530

    ARM: davinci: da850: override SPI DT node device name
    Populate OF_DEV_AUXDATA with desired device name expected by spi-davinci
    driver. Without this clk_get of spi-davinci DT driver fails.
    
    Signed-off-by: Manjunathappa, Prakash <prakash.pm@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/da8xx-dt.c
+++ arch/arm/mach-davinci/da8xx-dt.c
@@ -47,6 +47,7 @@ struct of_dev_auxdata da850_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("ti,da850-ecap", 0x01f06000, "ecap", NULL),
 	OF_DEV_AUXDATA("ti,da850-ecap", 0x01f07000, "ecap", NULL),
 	OF_DEV_AUXDATA("ti,da850-ecap", 0x01f08000, "ecap", NULL),
+	OF_DEV_AUXDATA("ti,da830-spi", 0x01f0e000, "spi_davinci.1", NULL),
 	{}
 };
 
