5bb9524cc276657fe24e9d6a18bee793edc2205d
Ian Abbott <abbotti@mev.co.uk>
2013-04-04 14:58:51 +0100

    staging: comedi: pre-lock mutex on creation of comedi device
    Return from `comedi_alloc_board_minor()` with the mutex of the created
    `struct comedi_device` pre-locked.  This allows further initialization
    by the caller without the worry of something getting in there first.
    
    `comedi_auto_config()` no longer needs to check if the device is already
    "attached" since whatever was trying to attach the device would need to
    have locked the mutex first.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_fops.c
+++ drivers/staging/comedi/comedi_fops.c
@@ -2275,6 +2275,7 @@ static void comedi_device_cleanup(struct comedi_device *dev)
 	mutex_destroy(&dev->mutex);
 }
 
+/* Note: the ->mutex is pre-locked on successful return */
 struct comedi_device *comedi_alloc_board_minor(struct device *hardware_device)
 {
 	struct comedi_file_info *info;
@@ -2293,6 +2294,7 @@ struct comedi_device *comedi_alloc_board_minor(struct device *hardware_device)
 	info->device = dev;
 	info->hardware_device = hardware_device;
 	comedi_device_init(dev);
+	mutex_lock(&dev->mutex);
 	spin_lock(&comedi_file_info_table_lock);
 	for (i = 0; i < COMEDI_NUM_BOARD_MINORS; ++i) {
 		if (comedi_file_info_table[i] == NULL) {
@@ -2302,6 +2304,7 @@ struct comedi_device *comedi_alloc_board_minor(struct device *hardware_device)
 	}
 	spin_unlock(&comedi_file_info_table_lock);
 	if (i == COMEDI_NUM_BOARD_MINORS) {
+		mutex_unlock(&dev->mutex);
 		comedi_device_cleanup(dev);
 		kfree(dev);
 		kfree(info);
@@ -2315,6 +2318,7 @@ struct comedi_device *comedi_alloc_board_minor(struct device *hardware_device)
 		dev->class_dev = csdev;
 	dev_set_drvdata(csdev, info);
 
+	/* Note: dev->mutex needs to be unlocked by the caller. */
 	return dev;
 }
 
@@ -2486,6 +2490,9 @@ static int __init comedi_init(void)
 			unregister_chrdev_region(MKDEV(COMEDI_MAJOR, 0),
 						 COMEDI_NUM_MINORS);
 			return PTR_ERR(dev);
+		} else {
+			/* comedi_alloc_board_minor() locked the mutex */
+			mutex_unlock(&dev->mutex);
 		}
 	}
 
