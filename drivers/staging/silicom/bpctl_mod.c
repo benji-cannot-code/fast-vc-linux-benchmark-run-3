8894051e9be46a1be712e3f7e61e5b5f420f4600
David Howells <dhowells@redhat.com>
2013-04-04 20:36:00 +0100

    silicom-bypass: Remove device_open/close()
    Remove device_open/close() functions as they don't really do anything and
    remove Device_Open as it isn't counted atomically and the value isn't used.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- drivers/staging/silicom/bpctl_mod.c
+++ drivers/staging/silicom/bpctl_mod.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BP_MOD_DESCR "Silicom Bypass-SD Control driver"
 #define BP_SYNC_FLAG 1
 
-static int Device_Open = 0;
 static int major_num = 0;
 
 MODULE_AUTHOR("Anna Lukin, annal@silicom.co.il");
@@ -289,27 +288,6 @@ static int bp_device_event(struct notifier_block *unused,
 static struct notifier_block bp_notifier_block = {
 	.notifier_call = bp_device_event,
 };
-
-static int device_open(struct inode *inode, struct file *file)
-{
-#ifdef DEBUG
-	printk("device_open(%p)\n", file);
-#endif
-	Device_Open++;
-/*
-* Initialize the message
-*/
-	return SUCCESS;
-}
-
-static int device_release(struct inode *inode, struct file *file)
-{
-#ifdef DEBUG
-	printk("device_release(%p,%p)\n", inode, file);
-#endif
-	Device_Open--;
-	return SUCCESS;
-}
 
 int is_bypass_fn(bpctl_dev_t *pbpctl_dev);
 int wdt_time_left(bpctl_dev_t *pbpctl_dev);
@@ -5807,8 +5785,6 @@ static long device_ioctl(struct file *file,	/* see include/linux/fs.h */
 static const struct file_operations Fops = {
 	.owner = THIS_MODULE,
 	.unlocked_ioctl = device_ioctl,
-	.open = device_open,
-	.release = device_release,	/* a.k.a. close */
 };
 
 #ifndef PCI_DEVICE
