010538cdb0e956d0fdccb1faabc3740a2c2da52e
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-04 12:54:11 +0000

    cpufreq: davinci: move cpufreq driver to drivers/cpufreq
    This patch moves cpufreq driver of ARM based davinci platform to
    drivers/cpufreq.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Sekhar Nori <nsekhar@ti.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/davinci-cpufreq.c
+++ drivers/cpufreq/davinci-cpufreq.c
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/cpufreq.h>
 #include <mach/common.h>
 
-#include "clock.h"
-
 struct davinci_cpufreq {
 	struct device *dev;
 	struct clk *armclk;
