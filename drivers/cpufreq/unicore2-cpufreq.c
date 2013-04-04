36d12ece07b4493f01b0de4b3d79f8f477849e72
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-04 12:54:23 +0000

    cpufreq: unicore2: move cpufreq driver to drivers/cpufreq
    This patch moves cpufreq driver of UNICORE-2 architecture to drivers/cpufreq.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Guan Xuetao <gxt@mprc.pku.edu.cn>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/unicore2-cpufreq.c
+++ drivers/cpufreq/unicore2-cpufreq.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * linux/arch/unicore32/kernel/cpu-ucv2.c: clock scaling for the UniCore-II
+ * clock scaling for the UniCore-II
  *
  * Code specific to PKUnity SoC and UniCore ISA
  *
