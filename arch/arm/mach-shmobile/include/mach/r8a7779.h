27f3404cd9b0af2750cf1cea587a18a95acb96f3
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2013-04-04 18:53:50 +0000

    ARM: shmobile: R8A7779: add Ether support
    Add Ether clock and platform device for R8A7779 SoC; add a function to register
    this device with board-specific platform data.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/r8a7779.h
+++ arch/arm/mach-shmobile/include/mach/r8a7779.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/sh_clk.h>
 #include <linux/pm_domain.h>
+#include <linux/sh_eth.h>
 
 /* Pin Function Controller:
  * GPIO_FN_xx - GPIO used to select pin function
@@ -353,6 +354,7 @@ extern void r8a7779_earlytimer_init(void);
 extern void r8a7779_add_early_devices(void);
 extern void r8a7779_add_standard_devices(void);
 extern void r8a7779_add_standard_devices_dt(void);
+extern void r8a7779_add_ether_device(struct sh_eth_plat_data *pdata);
 extern void r8a7779_clock_init(void);
 extern void r8a7779_pinmux_init(void);
 extern void r8a7779_pm_init(void);
