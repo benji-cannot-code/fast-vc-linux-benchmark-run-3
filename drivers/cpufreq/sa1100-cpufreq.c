84b188d79c908aebc285590a112b40d740853460
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-04 12:54:16 +0000

    cpufreq: sa11x0: move cpufreq driver to drivers/cpufreq
    This patch moves cpufreq driver of ARM based sa11x0 platform to drivers/cpufreq.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/sa1100-cpufreq.c
+++ drivers/cpufreq/sa1100-cpufreq.c
@@ -92,9 +92,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/cputype.h>
 
+#include <mach/generic.h>
 #include <mach/hardware.h>
-
-#include "generic.h"
 
 struct sa1100_dram_regs {
 	int speed;
--- drivers/cpufreq/sa1110-cpufreq.c
+++ drivers/cpufreq/sa1110-cpufreq.c
@@ -28,9 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cputype.h>
 #include <asm/mach-types.h>
 
+#include <mach/generic.h>
 #include <mach/hardware.h>
-
-#include "generic.h"
 
 #undef DEBUG
 
