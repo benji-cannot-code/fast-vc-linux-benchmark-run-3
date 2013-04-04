9c71b3e1eed7e21f0d93d5cb9e41b4f178077132
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-04 12:54:14 +0000

    cpufreq: integrator: move cpufreq driver to drivers/cpufreq
    This patch moves cpufreq driver of ARM based integrator platform to
    drivers/cpufreq.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/integrator-cpufreq.c
+++ drivers/cpufreq/integrator-cpufreq.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- *  linux/arch/arm/mach-integrator/cpu.c
- *
  *  Copyright (C) 2001-2002 Deep Blue Solutions Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
