40ae4d98f46a1ff051f842672286ae0627d8d0f1
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-04 12:54:20 +0000

    cpufreq: ia64: move cpufreq driver to drivers/cpufreq
    This patch moves cpufreq driver of IA64 architecture to drivers/cpufreq.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/ia64-acpi-cpufreq.c
+++ drivers/cpufreq/ia64-acpi-cpufreq.c
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/ia64/kernel/cpufreq/acpi-cpufreq.c
  * This file provides the ACPI based P-state support. This
  * module works with generic cpufreq infrastructure. Most of
  * the code is based on i386 version
