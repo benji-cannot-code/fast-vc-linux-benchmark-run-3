d8adf5f8ca6e4aacd1459a07907430a1e77a3d40
Ian Abbott <abbotti@mev.co.uk>
2013-04-04 14:58:47 +0100

    staging: comedi: add comedi_release_hardware_device()
    Add `comedi_release_hardware_device()` as a replacement for the call
    sequence `comedi_find_board_minor()`, `comedi_free_board_minor()`.  This
    is slightly safer as we can make sure nothing funny happens to the found
    `comedi_file_info_table[]` entry in the middle of the sequence.  Change
    `comedi_auto_unconfig()` to call the new function instead of the old
    sequence.  Remove `comedi_find_board_minor()` as it has no other
    callers.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_fops.c
+++ drivers/staging/comedi/comedi_fops.c
@@ -2349,7 +2349,7 @@ void comedi_free_board_minor(unsigned minor)
 	comedi_free_board_file_info(comedi_clear_minor(minor));
 }
 
-int comedi_find_board_minor(struct device *hardware_device)
+void comedi_release_hardware_device(struct device *hardware_device)
 {
 	int minor;
 	struct comedi_file_info *info;
@@ -2358,12 +2358,13 @@ int comedi_find_board_minor(struct device *hardware_device)
 		spin_lock(&comedi_file_info_table_lock);
 		info = comedi_file_info_table[minor];
 		if (info && info->hardware_device == hardware_device) {
+			comedi_file_info_table[minor] = NULL;
 			spin_unlock(&comedi_file_info_table_lock);
-			return minor;
+			comedi_free_board_file_info(info);
+			break;
 		}
 		spin_unlock(&comedi_file_info_table_lock);
 	}
-	return -ENODEV;
 }
 
 int comedi_alloc_subdevice_minor(struct comedi_subdevice *s)
