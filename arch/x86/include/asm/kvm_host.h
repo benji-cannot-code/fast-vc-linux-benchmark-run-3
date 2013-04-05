30bf3a56536628ff72bd0648000e8bf68930430c
Geoff Levand <geoff@infradead.org>
2013-04-05 19:20:30 +0000

    KVM: Move vm_list kvm_lock declarations out of x86
    The variables vm_list and kvm_lock are common to all architectures, so
    move the declarations from arch/x86/include/asm/kvm_host.h to
    include/linux/kvm_host.h.
    
    Fixes sparse warnings like these when building for arm64:
    
      virt/kvm/kvm_main.c: warning: symbol 'kvm_lock' was not declared. Should it be static?
      virt/kvm/kvm_main.c: warning: symbol 'vm_list' was not declared. Should it be static?
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>
    Signed-off-by: Gleb Natapov <gleb@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -95,9 +95,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ASYNC_PF_PER_VCPU 64
 
-extern raw_spinlock_t kvm_lock;
-extern struct list_head vm_list;
-
 struct kvm_vcpu;
 struct kvm;
 struct kvm_async_pf;
