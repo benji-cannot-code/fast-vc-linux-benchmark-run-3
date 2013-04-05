01491f81b819f2d6e500353bf97c1691f157301f
Linus Walleij <linus.walleij@linaro.org>
2013-04-05 22:45:11 +0200

    ARM: u300: enable delaytimer on the U300
    The U300 can support timer-based delays, so implement this.
    Skips past jiffy calibration.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-u300/timer.c
+++ arch/arm/mach-u300/timer.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/irq.h>
+#include <linux/delay.h>
 
 #include <mach/hardware.h>
 #include <mach/irqs.h>
@@ -346,6 +347,12 @@ static u32 notrace u300_read_sched_clock(void)
 	return readl(U300_TIMER_APP_VBASE + U300_TIMER_APP_GPT2CC);
 }
 
+static unsigned long u300_read_current_timer(void)
+{
+	return readl(U300_TIMER_APP_VBASE + U300_TIMER_APP_GPT2CC);
+}
+
+static struct delay_timer u300_delay_timer;
 
 /*
  * This sets up the system timers, clock source and clock event.
@@ -362,6 +369,10 @@ void __init u300_timer_init(void)
 	rate = clk_get_rate(clk);
 
 	setup_sched_clock(u300_read_sched_clock, 32, rate);
+
+	u300_delay_timer.read_current_timer = &u300_read_current_timer;
+	u300_delay_timer.freq = rate;
+	register_current_timer_delay(&u300_delay_timer);
 
 	/*
 	 * Disable the "OS" and "DD" timers - these are designed for Symbian!
