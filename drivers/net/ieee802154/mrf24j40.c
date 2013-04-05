b4a1cbf79dc2d92b7baec5699980cf1c8626be0f
Alan Ott <alan@signal11.us>
2013-04-05 10:34:51 +0000

    mrf24j40: Enable link-layer acknowledgement and retry
    On the MRF24J40, link-layer acknowledgment request and retry must be
    turned on explicitly for each packet.  Turn this on in the hardware based
    on the FC_ACK_REQ bit being set in the packet.
    
    Also, now that failure to receive an ACK will cause the hardware to report
    failure of transmission, change the log level for this failure to debug
    level.
    
    Signed-off-by: Alan Ott <alan@signal11.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ieee802154/mrf24j40.c
+++ drivers/net/ieee802154/mrf24j40.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pinctrl/consumer.h>
 #include <net/wpan-phy.h>
 #include <net/mac802154.h>
+#include <net/ieee802154.h>
 
 /* MRF24J40 Short Address Registers */
 #define REG_RXMCR    0x00  /* Receive MAC control */
@@ -350,7 +351,9 @@ static int mrf24j40_tx(struct ieee802154_dev *dev, struct sk_buff *skb)
 	if (ret)
 		goto err;
 	val |= 0x1;
-	val &= ~0x4;
+	/* Set TXNACKREQ if the ACK bit is set in the packet. */
+	if (skb->data[0] & IEEE802154_FC_ACK_REQ)
+		val |= 0x4;
 	write_short_reg(devrec, REG_TXNCON, val);
 
 	INIT_COMPLETION(devrec->tx_complete);
@@ -372,7 +375,7 @@ static int mrf24j40_tx(struct ieee802154_dev *dev, struct sk_buff *skb)
 	if (ret)
 		goto err;
 	if (val & 0x1) {
-		dev_err(printdev(devrec), "Error Sending. Retry count exceeded\n");
+		dev_dbg(printdev(devrec), "Error Sending. Retry count exceeded\n");
 		ret = -ECOMM; /* TODO: Better error code ? */
 	} else
 		dev_dbg(printdev(devrec), "Packet Sent\n");
