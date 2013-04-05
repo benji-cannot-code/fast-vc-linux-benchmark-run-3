15423f2d80fd53d347497fdafc0536fcba18d6b8
Arend van Spriel <arend@broadcom.com>
2013-04-05 10:57:44 +0200

    brcmfmac: add hexadecimal trace of message payload
    Adds a trace function used in brcmf_dbg_hex_dump() which adds the
    raw binary data to the trace. It requires trace-cmd plugin to see
    this data.
    
    Reviewed-by: Piotr Haber <phaber@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/dhd_dbg.h
+++ drivers/net/wireless/brcm80211/brcmfmac/dhd_dbg.h
@@ -94,6 +94,7 @@ do {								\
 
 #define brcmf_dbg_hex_dump(test, data, len, fmt, ...)			\
 do {									\
+	trace_brcmf_hexdump((void *)data, len);				\
 	if (test)							\
 		brcmu_dbg_hex_dump(data, len, fmt, ##__VA_ARGS__);	\
 } while (0)
--- drivers/net/wireless/brcm80211/brcmfmac/wl_cfg80211.c
+++ drivers/net/wireless/brcm80211/brcmfmac/wl_cfg80211.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <brcmu_wifi.h>
 #include "dhd.h"
 #include "dhd_dbg.h"
+#include "tracepoint.h"
 #include "fwil_types.h"
 #include "p2p.h"
 #include "wl_cfg80211.h"
