5f7b39e496f6d9afd26d2a830eb8920ff86a3a37
Simon Guinot <simon.guinot@sequanux.org>
2013-04-09 00:41:28 +0200

    ARM: Kirkwood: DT board setup for CloudBox
    This patch adds DT board setup for the LaCie NAS CloudBox. The CloudBox
    is a low cost NAS based on the Network Space v2.
    
    Chipset list:
    - CPU MARVELL 88F6702 1Ghz
    - SDRAM memory: 256MB DDR2-800 (2x128MB x8) 400Mhz
    - 1 Ethernet Gigabit port (PHY MARVELL 88E1318)
    - SPI flash, NOR 512KB
    - 1 push button
    - 2 LEDs (red and blue)
    
    There is no EEPROM and no USB ports embedded.
    
    Note that this board must not be confused with the Network Space Mini v2
    which is embedded in a previous LaCie product also named CloudBox.
    
    Signed-off-by: Simon Guinot <simon.guinot@sequanux.org>
    Acked-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-dt.c
+++ arch/arm/mach-kirkwood/board-dt.c
@@ -140,11 +140,12 @@ static void __init kirkwood_dt_init(void)
 	if (of_machine_is_compatible("keymile,km_kirkwood"))
 		km_kirkwood_init();
 
-	if (of_machine_is_compatible("lacie,inetspace_v2") ||
-	    of_machine_is_compatible("lacie,netspace_v2") ||
-	    of_machine_is_compatible("lacie,netspace_max_v2") ||
+	if (of_machine_is_compatible("lacie,cloudbox") ||
+	    of_machine_is_compatible("lacie,inetspace_v2") ||
 	    of_machine_is_compatible("lacie,netspace_lite_v2") ||
-	    of_machine_is_compatible("lacie,netspace_mini_v2"))
+	    of_machine_is_compatible("lacie,netspace_max_v2") ||
+	    of_machine_is_compatible("lacie,netspace_mini_v2") ||
+	    of_machine_is_compatible("lacie,netspace_v2"))
 		ns2_init();
 
 	if (of_machine_is_compatible("mpl,cec4"))
@@ -175,11 +176,12 @@ static const char * const kirkwood_dt_board_compat[] = {
 	"buffalo,lsxl",
 	"iom,ix2-200",
 	"keymile,km_kirkwood",
+	"lacie,cloudbox",
 	"lacie,inetspace_v2",
-	"lacie,netspace_max_v2",
-	"lacie,netspace_v2",
 	"lacie,netspace_lite_v2",
+	"lacie,netspace_max_v2",
 	"lacie,netspace_mini_v2",
+	"lacie,netspace_v2",
 	"mpl,cec4",
 	"netgear,readynas-duo-v2",
 	"plathome,openblocks-a6",
