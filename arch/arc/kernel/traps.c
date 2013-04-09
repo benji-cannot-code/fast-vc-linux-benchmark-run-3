1fe7eecc35f74f07a2ecfedb8472cbbdc7ec0f88
Christian Ruppert <christian.ruppert@abilis.com>
2013-04-09 11:50:45 +0200

    ARC: [build] Build failure with !KPROBES
    arch/arc/kernel/traps.c no longer compiles without explicitly including
    asm/kprobes.h
    
    Signed-off-by: Christian Ruppert <christian.ruppert@abilis.com>
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/traps.c
+++ arch/arc/kernel/traps.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kgdb.h>
 #include <asm/setup.h>
 #include <asm/unaligned.h>
+#include <asm/kprobes.h>
 
 void __init trap_init(void)
 {
