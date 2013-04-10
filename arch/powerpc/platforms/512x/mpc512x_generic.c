fb16813a1be4b93fda652c5ded9d046182cd2c56
Anatolij Gustschin <agust@denx.de>
2013-04-10 20:46:27 +0200

    powerpc/512x: add ifm ac14xx board
    Add dts file for ac14xx board and its board compatible
    string to the generic mpc512x board match list.
    Also add phandle to the dma DT node since there is a change
    (for MPC5121 SDHC DMA support) merged via linux-mmc tree
    with reference to the dma controller node in the sdhc node.
    
    Signed-off-by: Anatolij Gustschin <agust@denx.de>

--- arch/powerpc/platforms/512x/mpc512x_generic.c
+++ arch/powerpc/platforms/512x/mpc512x_generic.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const char * const board[] __initconst = {
 	"prt,prtlvt",
 	"fsl,mpc5125ads",
+	"ifm,ac14xx",
 	NULL
 };
 
