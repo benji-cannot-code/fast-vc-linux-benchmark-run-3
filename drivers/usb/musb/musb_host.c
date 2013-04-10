fa63d29472923ef9922b1b6e687c50d30e044ef1
Daniel Mack <zonque@gmail.com>
2013-04-10 21:55:42 +0200

    usb: musb: factor some host-specific functions
    In particular, this introduces musb_host_resume_root_hub()and
    musb_host_poke_root_hub() which will be stubbed out later.
    
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Acked-by: Peter Korsgaard <jacmet@sunsite.dk>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "musb_core.h"
 #include "musb_host.h"
 
-
 /* MUSB HOST status 22-mar-2006
  *
  * - There's still lots of partial code duplication for fault paths, so
@@ -2609,3 +2608,17 @@ const struct hc_driver musb_hc_driver = {
 	/* .start_port_reset	= NULL, */
 	/* .hub_irq_enable	= NULL, */
 };
+
+void musb_host_resume_root_hub(struct musb *musb)
+{
+	usb_hcd_resume_root_hub(musb_to_hcd(musb));
+}
+
+void musb_host_poke_root_hub(struct musb *musb)
+{
+	MUSB_HST_MODE(musb);
+	if (musb_to_hcd(musb)->status_urb)
+		usb_hcd_poll_rh_status(musb_to_hcd(musb));
+	else
+		usb_hcd_resume_root_hub(musb_to_hcd(musb));
+}
