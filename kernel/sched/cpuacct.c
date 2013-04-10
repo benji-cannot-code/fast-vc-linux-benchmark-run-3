13c39096906576a0a93ce411fac13e3e8fbeef5f
Ingo Molnar <mingo@kernel.org>
2013-04-10 15:10:50 +0200

    sched/cpuacct/UML: Fix header file dependency bug on the UML build
    The cpuacct split caused this build failure on UML:
    
      kernel/sched/cpuacct.c:94:2: error: implicit declaration of  function 'ERR_PTR'
    
    Cc: Li Zefan <lizefan@huawei.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/cpuacct.c
+++ kernel/sched/cpuacct.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/rcupdate.h>
 #include <linux/kernel_stat.h>
+#include <linux/err.h>
 
 #include "sched.h"
 
