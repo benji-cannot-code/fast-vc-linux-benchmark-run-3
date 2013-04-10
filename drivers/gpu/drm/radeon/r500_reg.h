5304cf53a1e612b295d659d6d463574e211f6435
Alex Deucher <alexander.deucher@amd.com>
2013-04-10 09:47:05 -0400

    drm/radeon: properly lock disp in mc_stop/resume for r5xx-r7xx
    Need to wait for the new addresses to take affect before
    re-enabling the MC.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org

--- drivers/gpu/drm/radeon/r500_reg.h
+++ drivers/gpu/drm/radeon/r500_reg.h
@@ -359,6 +359,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AVIVO_D1CRTC_STATUS_HV_COUNT                            0x60ac
 #define AVIVO_D1CRTC_STEREO_CONTROL                             0x60c4
 
+#define AVIVO_D1MODE_MASTER_UPDATE_LOCK                         0x60e0
 #define AVIVO_D1MODE_MASTER_UPDATE_MODE                         0x60e4
 
 /* master controls */
