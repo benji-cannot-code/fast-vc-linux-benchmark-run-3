0e938105b43d1de88639637f419c84c1df94f7c1
Arnd Bergmann <arnd@arndb.de>
2013-04-11 22:49:39 +0200

    usb: exynos: do not include plat/usb-phy.h
    The definitions have moved to include/linux/usb/samsung-usb-phy.h,
    and plat/usb-phy.h is unavailable from drivers in a multiplatform
    configuration.
    
    Also fix up the plat/usb-phy.h header file to use the definitions
    from the new header instead of providing a separate copy.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-s5p.c
+++ drivers/usb/host/ehci-s5p.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/usb-ehci-s5p.h>
 #include <linux/usb/phy.h>
 #include <linux/usb/samsung_usb_phy.h>
-#include <plat/usb-phy.h>
 #include <linux/usb.h>
 #include <linux/usb/hcd.h>
 #include <linux/usb/otg.h>
--- drivers/usb/host/ohci-exynos.c
+++ drivers/usb/host/ohci-exynos.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/usb-exynos.h>
 #include <linux/usb/phy.h>
 #include <linux/usb/samsung_usb_phy.h>
-#include <plat/usb-phy.h>
 
 struct exynos_ohci_hcd {
 	struct device *dev;
