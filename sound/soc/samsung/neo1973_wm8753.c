d0af5188f7edc63db35573f8d86597e937c38eb1
Heiko Stübner <heiko@sntech.de>
2013-04-11 21:02:43 +0200

    ASoC: samsung: fix neo1973-wm8753 compilation
    Commit b2ca78717cea (ARM: S3C24XX: make gta02.h local) already replaced
    the GTA02_GPIO_* constants in neo1973-wm8753.c but forgot to remove the
    inclusion of mach/gta02.h before moving the file out of mach/.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/samsung/neo1973_wm8753.c
+++ sound/soc/samsung/neo1973_wm8753.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach-types.h>
 #include "regs-iis.h"
-#include <mach/gta02.h>
 
 #include "../codecs/wm8753.h"
 #include "s3c24xx-i2s.h"
