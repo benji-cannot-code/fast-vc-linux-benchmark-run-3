acf74eb0ac0c2d98c4029be1f747b14ee8567995
David Howells <dhowells@redhat.com>
2013-04-11 23:55:54 +0100

    Include missing linux/magic.h inclusions
    Include missing linux/magic.h inclusions where the source file is currently
    expecting to get magic numbers through linux/proc_fs.h.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: linux-efi@vger.kernel.org
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/firmware/efivars.c
+++ drivers/firmware/efivars.c
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/pstore.h>
 #include <linux/ctype.h>
+#include <linux/magic.h>
 
 #include <linux/fs.h>
 #include <linux/ramfs.h>
