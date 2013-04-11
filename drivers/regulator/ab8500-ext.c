ea1c0b427d2a2251f1b643ba307f2695ad9e8d05
Axel Lin <axel.lin@ingics.com>
2013-04-11 12:04:18 +0800

    regulator: ab8500-ext: Make the return type of ab8500_ext_regulator_exit() void
    ab8500_ext_regulator_exit() never fails.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: Bengt Jonsson <bengt.g.jonsson@stericsson.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/regulator/ab8500-ext.c
+++ drivers/regulator/ab8500-ext.c
@@ -395,7 +395,7 @@ int ab8500_ext_regulator_init(struct platform_device *pdev)
 	return 0;
 }
 
-int ab8500_ext_regulator_exit(struct platform_device *pdev)
+void ab8500_ext_regulator_exit(struct platform_device *pdev)
 {
 	int i;
 
@@ -408,8 +408,6 @@ int ab8500_ext_regulator_exit(struct platform_device *pdev)
 
 		regulator_unregister(info->rdev);
 	}
-
-	return 0;
 }
 
 MODULE_LICENSE("GPL v2");
