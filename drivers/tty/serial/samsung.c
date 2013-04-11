57d9f997259b8fae5e50c06c99db25030b3adebc
Arnd Bergmann <arnd@arndb.de>
2013-04-11 02:04:48 +0200

    tty: serial/samsung: make register definitions global
    The registers for the Samsung S3C serial port are currently defined in
    the platform specific arch/arm/plat-samsung/include/plat/regs-serial.h
    file, which is not visible to multiplatform capable drivers.
    
    Unfortunately, it is not possible to move the file into a more local
    place as we should normally try to, because the same registers
    may be used in one of four places:
    
    * In the driver itself
    * In platform-independent ARM code for early debug output
    * In platform_data definitions
    * In the Samsung platform power management code
    
    I have also found no way to logically split out a platform_data
    file, other than possibly move everything into
    include/linux/platform_data, which also felt wrong. The only
    part of this file that makes sense to keep specific to the s3c24xx
    platform are the virtual and physical addresses defined here,
    which are needed in no other location.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty_flip.h>
 #include <linux/serial_core.h>
 #include <linux/serial.h>
+#include <linux/serial_s3c.h>
 #include <linux/delay.h>
 #include <linux/clk.h>
 #include <linux/cpufreq.h>
@@ -47,10 +48,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/irq.h>
 
-#include <mach/hardware.h>
-
-#include <plat/regs-serial.h>
+#ifdef CONFIG_SAMSUNG_CLOCK
 #include <plat/clock.h>
+#endif
 
 #include "samsung.h"
 
