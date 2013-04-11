c7dc4540d282483beaf2ebbb0b655b45311a6e04
John Crispin <blogic@openwrt.org>
2013-04-11 05:34:59 +0000

    MIPS: move mips_{set,get}_machine_name() to a more generic place
    Previously this functionality was only available to users of the mips_machine
    api. Moving the code to prom.c allows us to also add a OF wrapper.
    
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/5164/

--- arch/mips/kernel/proc.c
+++ arch/mips/kernel/proc.c
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpu-features.h>
 #include <asm/mipsregs.h>
 #include <asm/processor.h>
-#include <asm/mips_machine.h>
+#include <asm/prom.h>
 
 unsigned int vced_count, vcei_count;
 
