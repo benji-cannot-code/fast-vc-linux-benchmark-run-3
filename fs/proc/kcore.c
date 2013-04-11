65ad704f3c4c4dd1168a4aa50110a10042f0c579
David Howells <dhowells@redhat.com>
2013-04-11 13:34:43 +0100

    proc: Move non-public stuff from linux/proc_fs.h to fs/proc/internal.h
    Move non-public declarations and definitions from linux/proc_fs.h to
    fs/proc/internal.h.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/proc/kcore.c
+++ fs/proc/kcore.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioport.h>
 #include <linux/memory.h>
 #include <asm/sections.h>
+#include "internal.h"
 
 #define CORE_STR "CORE"
 
