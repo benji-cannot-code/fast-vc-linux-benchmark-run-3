9967d12ebc3ea685e912f9fc6bf975955a1fec59
Paulo Zanoni <paulo.r.zanoni@intel.com>
2013-04-12 17:57:57 -0300

    drm/i915: report Gen5+ CPU and PCH FIFO underruns
    In this commit we enable both CPU and PCH FIFO underrun reporting and
    start reporting them. We follow a few rules:
      - after we receive one of these errors, we mask the interrupt, so
        we won't get an "interrupt storm" and we also won't flood dmesg;
      - at each mode set we enable the interrupts again, so we'll see each
        message at most once per mode set;
      - in the specific places where we need to ignore the errors, we
        completely mask the interrupts.
    
    The downside of this patch is that since we're completely disabling
    (masking) the interrupts instead of just not printing error messages,
    we will mask more than just what we want on IVB/HSW CPU interrupts
    (due to GEN7_ERR_INT) and on CPT/PPT/LPT PCHs (due to SERR_INT). So
    when we decide to mask PCH FIFO underruns for pipe A on CPT, we'll
    also be masking PCH FIFO underruns for pipe B, because both are
    reported by SERR_INT, which has to be either completely enabled or
    completely disabled (in othe words, there's no way to disable/enable
    specific bits of GEN7_ERR_INT and SERR_INT).
    
    V2: Rename some functions and variables, downgrade messages to
    DRM_DEBUG_DRIVER and rebase.
    
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3347,6 +3347,10 @@ static void ironlake_crtc_enable(struct drm_crtc *crtc)
 		return;
 
 	intel_crtc->active = true;
+
+	intel_set_cpu_fifo_underrun_reporting(dev, pipe, true);
+	intel_set_pch_fifo_underrun_reporting(dev, pipe, true);
+
 	intel_update_watermarks(dev);
 
 	if (intel_pipe_has_type(crtc, INTEL_OUTPUT_LVDS)) {
@@ -3438,6 +3442,11 @@ static void haswell_crtc_enable(struct drm_crtc *crtc)
 		return;
 
 	intel_crtc->active = true;
+
+	intel_set_cpu_fifo_underrun_reporting(dev, pipe, true);
+	if (intel_crtc->config.has_pch_encoder)
+		intel_set_pch_fifo_underrun_reporting(dev, TRANSCODER_A, true);
+
 	intel_update_watermarks(dev);
 
 	if (intel_crtc->config.has_pch_encoder)
@@ -3524,6 +3533,7 @@ static void ironlake_crtc_disable(struct drm_crtc *crtc)
 	if (dev_priv->cfb_plane == plane)
 		intel_disable_fbc(dev);
 
+	intel_set_pch_fifo_underrun_reporting(dev, pipe, false);
 	intel_disable_pipe(dev_priv, pipe);
 
 	/* Disable PF */
@@ -3537,6 +3547,7 @@ static void ironlake_crtc_disable(struct drm_crtc *crtc)
 	ironlake_fdi_disable(crtc);
 
 	ironlake_disable_pch_transcoder(dev_priv, pipe);
+	intel_set_pch_fifo_underrun_reporting(dev, pipe, true);
 
 	if (HAS_PCH_CPT(dev)) {
 		/* disable TRANS_DP_CTL */
@@ -3603,6 +3614,8 @@ static void haswell_crtc_disable(struct drm_crtc *crtc)
 	if (dev_priv->cfb_plane == plane)
 		intel_disable_fbc(dev);
 
+	if (intel_crtc->config.has_pch_encoder)
+		intel_set_pch_fifo_underrun_reporting(dev, TRANSCODER_A, false);
 	intel_disable_pipe(dev_priv, pipe);
 
 	intel_ddi_disable_transcoder_func(dev_priv, cpu_transcoder);
@@ -3623,6 +3636,7 @@ static void haswell_crtc_disable(struct drm_crtc *crtc)
 
 	if (intel_crtc->config.has_pch_encoder) {
 		lpt_disable_pch_transcoder(dev_priv);
+		intel_set_pch_fifo_underrun_reporting(dev, TRANSCODER_A, true);
 		intel_ddi_fdi_disable(crtc);
 	}
 
