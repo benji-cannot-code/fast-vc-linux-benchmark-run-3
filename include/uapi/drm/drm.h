14338a68a460e6a460e78ac00747f85c4e74271c
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2013-04-12 18:28:26 -0500

    drm.h: Fix DRM compilation with bare-metal toolchain.
    An ifdef in drm.h expects to be compiled with full-fledged Linux
    toolchain, but it's common to compile kernel with just bare-metal
    toolchain which doesn't define __linux__. So, also add __KERNEL__
    check.
    
    [nm@ti.com: port forward to 3.9-rc6 and post to dri devel for feedback as RFC]
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/uapi/drm/drm.h
+++ include/uapi/drm/drm.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _DRM_H_
 #define _DRM_H_
 
-#if defined(__linux__)
+#if defined(__KERNEL__) || defined(__linux__)
 
 #include <linux/types.h>
 #include <asm/ioctl.h>
