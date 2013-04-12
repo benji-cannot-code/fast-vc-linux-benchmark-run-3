1464628172c5f447cd836d1a7ef45e442c53d29d
Jiang Liu <liuj97@gmail.com>
2013-04-12 05:44:25 +0000

    PCI: acpiphp: Convert acpiphp to be builtin only, not modular
    Convert acpiphp to be builtin only, with no module option.
    
    Previously, when HOTPLUG_PCI_ACPI=m, users could disable acpiphp by
    removing the module or preventing it from loading.  That can't be done
    if acpiphp is builtin statically, so this adds an "acpiphp.disable"
    kernel parameter.  If a user needs to use this parameter, it is a bug,
    and we want to hear about it.
    
    [bhelgaas: fold in acpiphp.disable here, remove documentation]
    Signed-off-by: Jiang Liu <jiang.liu@huawei.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/hotplug/acpiphp_core.c
+++ drivers/pci/hotplug/acpiphp_core.c
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SLOT_NAME_SIZE  21              /* {_SUN} */
 
 bool acpiphp_debug;
+bool acpiphp_disabled;
 
 /* local variables */
 static struct acpiphp_attention_info *attention_info;
@@ -61,7 +62,9 @@ MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
 MODULE_PARM_DESC(debug, "Debugging mode enabled or not");
+MODULE_PARM_DESC(disable, "disable acpiphp driver");
 module_param_named(debug, acpiphp_debug, bool, 0644);
+module_param_named(disable, acpiphp_disabled, bool, 0444);
 
 /* export the attention callback registration methods */
 EXPORT_SYMBOL_GPL(acpiphp_register_attention);
@@ -354,9 +357,11 @@ void acpiphp_unregister_hotplug_slot(struct acpiphp_slot *acpiphp_slot)
 
 static int __init acpiphp_init(void)
 {
-	info(DRIVER_DESC " version: " DRIVER_VERSION "\n");
+	info(DRIVER_DESC " version: " DRIVER_VERSION "%s\n",
+		acpiphp_disabled ? ", disabled by user; please report a bug"
+				 : "");
 
-	if (acpi_pci_disabled)
+	if (acpi_pci_disabled || acpiphp_disabled)
 		return 0;
 
 	/* read all the ACPI info from the system */
@@ -365,14 +370,4 @@ static int __init acpiphp_init(void)
 }
 
 
-static void __exit acpiphp_exit(void)
-{
-	if (acpi_pci_disabled)
-		return;
-
-	/* deallocate internal data structures etc. */
-	acpiphp_glue_exit();
-}
-
 module_init(acpiphp_init);
-module_exit(acpiphp_exit);
