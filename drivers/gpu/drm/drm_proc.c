cc67fe100b0b9fa76b490b4dd1424948d131a677
David Howells <dhowells@redhat.com>
2013-04-12 15:23:25 +0100

    drm: Constify drm_proc_list[]
    Constify drm_proc_list[] and related pointers.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/gpu/drm/drm_proc.c
+++ drivers/gpu/drm/drm_proc.c
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**
  * Proc file list.
  */
-static struct drm_info_list drm_proc_list[] = {
+static const struct drm_info_list drm_proc_list[] = {
 	{"name", drm_name_info, 0},
 	{"vm", drm_vm_info, 0},
 	{"clients", drm_clients_info, 0},
@@ -90,7 +90,7 @@ static const struct file_operations drm_proc_fops = {
  * Create a given set of proc files represented by an array of
  * gdm_proc_lists in the given root directory.
  */
-static int drm_proc_create_files(struct drm_info_list *files, int count,
+static int drm_proc_create_files(const struct drm_info_list *files, int count,
 			  struct proc_dir_entry *root, struct drm_minor *minor)
 {
 	struct drm_device *dev = minor->dev;
@@ -173,7 +173,7 @@ int drm_proc_init(struct drm_minor *minor, int minor_id,
 	return 0;
 }
 
-static int drm_proc_remove_files(struct drm_info_list *files, int count,
+static int drm_proc_remove_files(const struct drm_info_list *files, int count,
 			  struct drm_minor *minor)
 {
 	struct list_head *pos, *q;
