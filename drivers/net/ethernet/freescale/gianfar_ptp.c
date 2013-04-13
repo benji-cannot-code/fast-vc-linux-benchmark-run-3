41a484636de52446d8bda9f578d27408266146a5
Joe Perches <joe@perches.com>
2013-04-13 19:03:18 +0000

    gianfar: Use netdev_<level> when possible
    Use a more current logging style.
    
    Convert pr_<level> to netdev_<level> when a struct net_device is
    available.  Add pr_fmt and neaten other formats too.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/gianfar_ptp.c
+++ drivers/net/ethernet/freescale/gianfar_ptp.c
@@ -18,6 +18,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/device.h>
 #include <linux/hrtimer.h>
 #include <linux/init.h>
