0a5dc6046f07f83accec988a1aea72eaed0f734d
Wanpeng Li <liwanp@linux.vnet.ibm.com>
2013-04-13 21:01:30 +0800

    staging: ramster/debug: Add CONFIG_RAMSTER_DEBUG Kconfig entry
    Add CONFIG_RAMSTER_DEBUG Kconfig entry.
    
    Acked-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    Signed-off-by: Wanpeng Li <liwanp@linux.vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/zcache/ramster/debug.h
+++ drivers/staging/zcache/ramster/debug.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bug.h>
 
-#ifdef CONFIG_RAMSTER
+#ifdef CONFIG_RAMSTER_DEBUG
 
 extern long ramster_flnodes;
 static atomic_t ramster_flnodes_atomic = ATOMIC_INIT(0);
