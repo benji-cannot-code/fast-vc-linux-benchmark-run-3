99c98c00baf394aa6fe60fc79ed9c4170a70d8f4
Joe Perches <joe@perches.com>
2013-04-13 19:03:17 +0000

    fec: Convert printks to netdev_<level>
    Use a more current logging message style.
    
    Convert the printks where a struct net_device is available to
    netdev_<level>.  Convert the other printks to pr_<level> and
    add pr_fmt where appropriate.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fec_ptp.c
+++ drivers/net/ethernet/freescale/fec_ptp.c
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
