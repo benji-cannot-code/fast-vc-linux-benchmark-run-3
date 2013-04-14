9f77d670cb2d4118087f69a6b09be1e86361a099
Daniel Borkmann <dborkman@redhat.com>
2013-04-14 08:29:12 +0000

    net: tcp_memcontrol: minor: remove unused variable
    Commit 10b96f7306e5 (``tcp_memcontrol: remove a redundant statement
    in tcp_destroy_cgroup()'') says ``We read the value but make no use
    of it.'', but forgot to remove the variable declaration as well. This
    was a follow-up commit of 3f1346193 (``memcg: decrement static keys
    at real destroy time'') that removed the read of variable 'val'.
    
    This fixes therefore:
    
      CC      net/ipv4/tcp_memcontrol.o
    net/ipv4/tcp_memcontrol.c: In function ‘tcp_destroy_cgroup’:
    net/ipv4/tcp_memcontrol.c:67:6: warning: unused variable ‘val’ [-Wunused-variable]
    
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_memcontrol.c
+++ net/ipv4/tcp_memcontrol.c
@@ -65,7 +65,6 @@ void tcp_destroy_cgroup(struct mem_cgroup *memcg)
 {
 	struct cg_proto *cg_proto;
 	struct tcp_memcontrol *tcp;
-	u64 val;
 
 	cg_proto = tcp_prot.proto_cgroup(memcg);
 	if (!cg_proto)
