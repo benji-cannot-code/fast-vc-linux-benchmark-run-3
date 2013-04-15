c3bd1b7614d9d5772ce1d20770588490b0b241cc
Alex Deucher <alexander.deucher@amd.com>
2013-04-15 17:13:29 -0400

    drm/radeon/cik: restructure rlc setup
    Restructure rlc setup to handle clock and power
    gating.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/cikd.h
+++ drivers/gpu/drm/radeon/cikd.h
@@ -906,6 +906,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RLC_LB_CNTR_MAX                                   0xC348
 
 #define RLC_LB_CNTL                                       0xC364
+#       define LOAD_BALANCE_ENABLE                        (1 << 0)
 
 #define RLC_LB_CNTR_INIT                                  0xC36C
 
