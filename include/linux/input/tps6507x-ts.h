e67e1fb416d4e438c22ff2ab535823021081da8c
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2013-04-15 11:05:54 -0700

    Input: tps6507x-ts - remove vref from platform data
    Although defined in platform data, vref is not used anywhere.
    
    Also remove model, irq, and clear_penirq as they are not used either.
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/linux/input/tps6507x-ts.h
+++ include/linux/input/tps6507x-ts.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Board specific touch screen initial values */
 struct touchscreen_init_data {
 	int	poll_period;	/* ms */
-	int	vref;		/* non-zero to leave vref on */
 	__u16	min_pressure;	/* min reading to be treated as a touch */
 	__u16	vendor;
 	__u16	product;
