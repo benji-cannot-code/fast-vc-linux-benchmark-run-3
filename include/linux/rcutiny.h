3aa8c00fa7730b6adfac92da2c54c691ffb98a4d
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-04-15 08:25:27 -0700

    rcu: Remove the CONFIG_TINY_RCU ifdefs in rcutiny.h
    Now that CONFIG_TINY_PREEMPT_RCU is no more, this commit removes
    the CONFIG_TINY_RCU ifdefs from include/linux/rcutiny.h in favor of
    unconditionally compiling the CONFIG_TINY_RCU legs of those ifdefs.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    [ paulmck: Moved removal of #else to "Remove TINY_PREEMPT_RCU" as
      suggested by Josh Triplett. ]
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- include/linux/rcutiny.h
+++ include/linux/rcutiny.h
@@ -38,8 +38,6 @@ static inline void rcu_barrier_sched(void)
 	wait_rcu_gp(call_rcu_sched);
 }
 
-#ifdef CONFIG_TINY_RCU
-
 static inline void synchronize_rcu_expedited(void)
 {
 	synchronize_sched();	/* Only one CPU, so pretty fast anyway!!! */
@@ -49,8 +47,6 @@ static inline void rcu_barrier(void)
 {
 	rcu_barrier_sched();  /* Only one CPU, so only one list of callbacks! */
 }
-
-#endif /* #ifdef CONFIG_TINY_RCU */
 
 static inline void synchronize_rcu_bh(void)
 {
@@ -73,8 +69,6 @@ static inline void kfree_call_rcu(struct rcu_head *head,
 	call_rcu(head, func);
 }
 
-#ifdef CONFIG_TINY_RCU
-
 static inline void rcu_preempt_note_context_switch(void)
 {
 }
@@ -84,8 +78,6 @@ static inline int rcu_needs_cpu(int cpu, unsigned long *delta_jiffies)
 	*delta_jiffies = ULONG_MAX;
 	return 0;
 }
-
-#endif /* #ifdef CONFIG_TINY_RCU */
 
 static inline void rcu_note_context_switch(int cpu)
 {
