c90ce61a4944dc6a29b6b76daf645e7c2a5dc696
Pravin B Shelar <pshelar@nicira.com>
2013-04-15 13:30:37 -0700

    openvswitch: Use generic struct pcpu_tstats.
    Rather than defining ovs specific stats struct (vport_percpu_stats),
    we can use existing pcpu_tstats to achieve exactly same functionality.
    
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- net/openvswitch/vport.h
+++ net/openvswitch/vport.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef VPORT_H
 #define VPORT_H 1
 
+#include <linux/if_tunnel.h>
 #include <linux/list.h>
 #include <linux/netlink.h>
 #include <linux/openvswitch.h>
@@ -50,14 +51,6 @@ int ovs_vport_get_options(const struct vport *, struct sk_buff *);
 int ovs_vport_send(struct vport *, struct sk_buff *);
 
 /* The following definitions are for implementers of vport devices: */
-
-struct vport_percpu_stats {
-	u64 rx_bytes;
-	u64 rx_packets;
-	u64 tx_bytes;
-	u64 tx_packets;
-	struct u64_stats_sync sync;
-};
 
 struct vport_err_stats {
 	u64 rx_dropped;
@@ -90,7 +83,7 @@ struct vport {
 	struct hlist_node dp_hash_node;
 	const struct vport_ops *ops;
 
-	struct vport_percpu_stats __percpu *percpu_stats;
+	struct pcpu_tstats __percpu *percpu_stats;
 
 	spinlock_t stats_lock;
 	struct vport_err_stats err_stats;
