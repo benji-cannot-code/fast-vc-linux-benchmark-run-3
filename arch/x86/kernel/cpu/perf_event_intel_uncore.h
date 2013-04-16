ec08f94cf3aef556df461f67d6b65cea3403120a
Yan, Zheng <zheng.z.yan@intel.com>
2013-04-16 19:51:06 +0800

    perf/x86/intel: Fix SNB-EP CBO and PCU uncore PMU filter management
    The existing code assumes all Cbox and PCU events are using
    filter, but actually the filter is event specific. Furthermore
    the filter is sub-divided into multiple fields which are used
    by different events.
    
    Signed-off-by: Yan, Zheng <zheng.z.yan@intel.com>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: peterz@infradead.org
    Cc: ak@linux.intel.com
    Link: http://lkml.kernel.org/r/1366113067-3262-3-git-send-email-zheng.z.yan@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reported-by: Stephane Eranian <eranian@google.com>

--- arch/x86/kernel/cpu/perf_event_intel_uncore.h
+++ arch/x86/kernel/cpu/perf_event_intel_uncore.h
@@ -149,8 +149,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SNBEP_C0_MSR_PMON_CTL0			0xd10
 #define SNBEP_C0_MSR_PMON_BOX_CTL		0xd04
 #define SNBEP_C0_MSR_PMON_BOX_FILTER		0xd14
-#define SNBEP_CB0_MSR_PMON_BOX_FILTER_MASK	0xfffffc1f
 #define SNBEP_CBO_MSR_OFFSET			0x20
+
+#define SNBEP_CB0_MSR_PMON_BOX_FILTER_TID	0x1f
+#define SNBEP_CB0_MSR_PMON_BOX_FILTER_NID	0x3fc00
+#define SNBEP_CB0_MSR_PMON_BOX_FILTER_STATE	0x7c0000
+#define SNBEP_CB0_MSR_PMON_BOX_FILTER_OPC	0xff800000
+
+#define SNBEP_CBO_EVENT_EXTRA_REG(e, m, i) {	\
+	.event = (e),				\
+	.msr = SNBEP_C0_MSR_PMON_BOX_FILTER,	\
+	.config_mask = (m),			\
+	.idx = (i)				\
+}
 
 /* SNB-EP PCU register */
 #define SNBEP_PCU_MSR_PMON_CTR0			0xc36
