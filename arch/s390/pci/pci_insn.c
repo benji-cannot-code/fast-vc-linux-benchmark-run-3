58cb5c233aaa6a4ed0f6a104349cf155fa8c34ca
Sebastian Ott <sebott@linux.vnet.ibm.com>
2013-04-16 14:16:14 +0200

    s390/pci: add exception table to load/store instructions
    Don't let pci_load and friends crash the kernel when called with
    e.g. an invalid offset. Return -ENXIO instead.
    
    Reviewed-by: Gerald Schaefer <gerald.schaefer@de.ibm.com>
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/pci/pci_insn.c
+++ arch/s390/pci/pci_insn.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/delay.h>
 #include <asm/pci_insn.h>
+#include <asm/processor.h>
 
 #define ZPCI_INSN_BUSY_DELAY	1	/* 1 microsecond */
 
@@ -86,18 +87,20 @@ void set_irq_ctrl(u16 ctl, char *unused, u8 isc)
 }
 
 /* PCI Load */
-static inline u8 __pcilg(u64 *data, u64 req, u64 offset, u8 *status)
+static inline int __pcilg(u64 *data, u64 req, u64 offset, u8 *status)
 {
 	register u64 __req asm("2") = req;
 	register u64 __offset asm("3") = offset;
+	int cc = -ENXIO;
 	u64 __data;
-	u8 cc;
 
 	asm volatile (
 		"	.insn	rre,0xb9d20000,%[data],%[req]\n"
-		"	ipm	%[cc]\n"
+		"0:	ipm	%[cc]\n"
 		"	srl	%[cc],28\n"
-		: [cc] "=d" (cc), [data] "=d" (__data), [req] "+d" (__req)
+		"1:\n"
+		EX_TABLE(0b, 1b)
+		: [cc] "+d" (cc), [data] "=d" (__data), [req] "+d" (__req)
 		:  "d" (__offset)
 		: "cc");
 	*status = __req >> 24 & 0xff;
@@ -107,7 +110,8 @@ static inline u8 __pcilg(u64 *data, u64 req, u64 offset, u8 *status)
 
 int s390pci_load(u64 *data, u64 req, u64 offset)
 {
-	u8 cc, status;
+	u8 status;
+	int cc;
 
 	do {
 		cc = __pcilg(data, req, offset, &status);
@@ -115,29 +119,27 @@ int s390pci_load(u64 *data, u64 req, u64 offset)
 			udelay(ZPCI_INSN_BUSY_DELAY);
 	} while (cc == 2);
 
-	if (cc) {
+	if (cc)
 		printk_once(KERN_ERR "%s: error cc: %d  status: %d  req: %Lx  offset: %Lx\n",
 			    __func__, cc, status, req, offset);
-		/* TODO: on IO errors set data to 0xff...
-		 * here or in users of pcilg (le conversion)?
-		 */
-	}
-	return (cc) ? -EIO : 0;
+	return (cc > 0) ? -EIO : cc;
 }
 EXPORT_SYMBOL_GPL(s390pci_load);
 
 /* PCI Store */
-static inline u8 __pcistg(u64 data, u64 req, u64 offset, u8 *status)
+static inline int __pcistg(u64 data, u64 req, u64 offset, u8 *status)
 {
 	register u64 __req asm("2") = req;
 	register u64 __offset asm("3") = offset;
-	u8 cc;
+	int cc = -ENXIO;
 
 	asm volatile (
 		"	.insn	rre,0xb9d00000,%[data],%[req]\n"
-		"	ipm	%[cc]\n"
+		"0:	ipm	%[cc]\n"
 		"	srl	%[cc],28\n"
-		: [cc] "=d" (cc), [req] "+d" (__req)
+		"1:\n"
+		EX_TABLE(0b, 1b)
+		: [cc] "+d" (cc), [req] "+d" (__req)
 		: "d" (__offset), [data] "d" (data)
 		: "cc");
 	*status = __req >> 24 & 0xff;
@@ -146,7 +148,8 @@ static inline u8 __pcistg(u64 data, u64 req, u64 offset, u8 *status)
 
 int s390pci_store(u64 data, u64 req, u64 offset)
 {
-	u8 cc, status;
+	u8 status;
+	int cc;
 
 	do {
 		cc = __pcistg(data, req, offset, &status);
@@ -157,20 +160,22 @@ int s390pci_store(u64 data, u64 req, u64 offset)
 	if (cc)
 		printk_once(KERN_ERR "%s: error cc: %d  status: %d  req: %Lx  offset: %Lx\n",
 			__func__, cc, status, req, offset);
-	return (cc) ? -EIO : 0;
+	return (cc > 0) ? -EIO : cc;
 }
 EXPORT_SYMBOL_GPL(s390pci_store);
 
 /* PCI Store Block */
-static inline u8 __pcistb(const u64 *data, u64 req, u64 offset, u8 *status)
+static inline int __pcistb(const u64 *data, u64 req, u64 offset, u8 *status)
 {
-	u8 cc;
+	int cc = -ENXIO;
 
 	asm volatile (
 		"	.insn	rsy,0xeb00000000d0,%[req],%[offset],%[data]\n"
-		"	ipm	%[cc]\n"
+		"0:	ipm	%[cc]\n"
 		"	srl	%[cc],28\n"
-		: [cc] "=d" (cc), [req] "+d" (req)
+		"1:\n"
+		EX_TABLE(0b, 1b)
+		: [cc] "+d" (cc), [req] "+d" (req)
 		: [offset] "d" (offset), [data] "Q" (*data)
 		: "cc");
 	*status = req >> 24 & 0xff;
@@ -179,7 +184,8 @@ static inline u8 __pcistb(const u64 *data, u64 req, u64 offset, u8 *status)
 
 int s390pci_store_block(const u64 *data, u64 req, u64 offset)
 {
-	u8 cc, status;
+	u8 status;
+	int cc;
 
 	do {
 		cc = __pcistb(data, req, offset, &status);
@@ -190,6 +196,6 @@ int s390pci_store_block(const u64 *data, u64 req, u64 offset)
 	if (cc)
 		printk_once(KERN_ERR "%s: error cc: %d  status: %d  req: %Lx  offset: %Lx\n",
 			    __func__, cc, status, req, offset);
-	return (cc) ? -EIO : 0;
+	return (cc > 0) ? -EIO : cc;
 }
 EXPORT_SYMBOL_GPL(s390pci_store_block);
