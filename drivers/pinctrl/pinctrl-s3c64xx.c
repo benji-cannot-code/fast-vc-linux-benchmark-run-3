51bad0de3de0e75096a34bc03481735afcf3dd72
Tomasz Figa <tomasz.figa@gmail.com>
2013-04-17 00:23:59 +0200

    pinctrl: s3c64xx: Fix build error caused by undefined chained_irq_enter
    Commit de88cbb7 moved definitions of chained_irq_enter and
    chained_irq_exit to a generic header, which must be now included instead
    of the legacy arch-specific one.
    
    This patch fixes build error caused by including the legacy header
    leading to undefined chained_irq_enter and chained_irq_exit symbols.
    
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-s3c64xx.c
+++ drivers/pinctrl/pinctrl-s3c64xx.c
@@ -23,10 +23,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/of_irq.h>
 #include <linux/io.h>
+#include <linux/irqchip/chained_irq.h>
 #include <linux/slab.h>
 #include <linux/err.h>
-
-#include <asm/mach/irq.h>
 
 #include "pinctrl-samsung.h"
 
