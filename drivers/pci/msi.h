44595d6515bbde538c3278bf2189e53a45e8b55b
Bjorn Helgaas <bhelgaas@google.com>
2013-04-17 17:39:57 -0600

    PCI: Drop msi_lower_address_reg() and msi_upper_address_reg() macros
    msi_lower_address_reg() and msi_upper_address_reg() don't provide any
    useful abstraction, so drop them.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/msi.h
+++ drivers/pci/msi.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef MSI_H
 #define MSI_H
 
-#define msi_lower_address_reg(base)	(base + PCI_MSI_ADDRESS_LO)
-#define msi_upper_address_reg(base)	(base + PCI_MSI_ADDRESS_HI)
 #define msi_data_reg(base, is64bit)	\
 	(base + ((is64bit == 1) ? PCI_MSI_DATA_64 : PCI_MSI_DATA_32))
 #define msi_mask_reg(base, is64bit)	\
