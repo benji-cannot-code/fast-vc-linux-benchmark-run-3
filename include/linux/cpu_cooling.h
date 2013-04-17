752c030cfe731da2180fd1406fab430e219bb2f7
Eduardo Valentin <eduardo.valentin@ti.com>
2013-04-17 17:12:22 +0000

    thermal: cpu_cooling: remove unused symbols on cpu_cooling.h
    Remove defines that are not in used.
    
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Acked-by: Amit Daniel Kachhap <amit.daniel@samsung.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- include/linux/cpu_cooling.h
+++ include/linux/cpu_cooling.h
@@ -28,9 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/thermal.h>
 #include <linux/cpumask.h>
 
-#define CPUFREQ_COOLING_START		0
-#define CPUFREQ_COOLING_STOP		1
-
 #ifdef CONFIG_CPU_THERMAL
 /**
  * cpufreq_cooling_register - function to create cpufreq cooling device.
