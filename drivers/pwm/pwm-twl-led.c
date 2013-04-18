43e2beec0f4d8e317dc5dc0bcea9fbfaf7978013
Thierry Reding <thierry.reding@avionic-design.de>
2013-04-18 10:04:14 +0200

    pwm: Constify OF match tables
    A few drivers already annotate this properly. Make the same change for
    all other OF supporting drivers.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Alexandre Pereira da Silva <aletes.xgr@gmail.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/pwm/pwm-twl-led.c
+++ drivers/pwm/pwm-twl-led.c
@@ -324,7 +324,7 @@ static int twl_pwmled_remove(struct platform_device *pdev)
 }
 
 #ifdef CONFIG_OF
-static struct of_device_id twl_pwmled_of_match[] = {
+static const struct of_device_id twl_pwmled_of_match[] = {
 	{ .compatible = "ti,twl4030-pwmled" },
 	{ .compatible = "ti,twl6030-pwmled" },
 	{ },
