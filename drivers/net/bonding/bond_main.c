0f31e8508873e46a811b94d0556b52845c9db180
nikolay@redhat.com <nikolay@redhat.com>
2013-04-18 07:33:37 +0000

    bonding: disable netpoll on enslave failure
    slave_disable_netpoll() is not called upon enslave failure which would
    lead to a memory leak. Call slave_disable_netpoll() after err_detach as
    that's the first error path after enabling netpoll on that slave.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -1921,6 +1921,7 @@ err_detach:
 		write_unlock_bh(&bond->curr_slave_lock);
 		read_unlock(&bond->lock);
 	}
+	slave_disable_netpoll(new_slave);
 
 err_close:
 	slave_dev->priv_flags &= ~IFF_BONDING;
