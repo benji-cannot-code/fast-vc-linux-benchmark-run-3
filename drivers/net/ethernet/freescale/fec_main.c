66c71a16a29fd1dd102b6374a43ffc135dd814ae
Fabio Estevam <fabio.estevam@freescale.com>
2013-04-18 02:54:39 +0000

    fec: Remove unneeded asm header files
    There is nothing in the driver that requires <asm/coldfire.h> and
    <asm/mcfsim.h>.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -53,11 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/cacheflush.h>
 
-#ifndef CONFIG_ARM
-#include <asm/coldfire.h>
-#include <asm/mcfsim.h>
-#endif
-
 #include "fec.h"
 
 #if defined(CONFIG_ARM)
