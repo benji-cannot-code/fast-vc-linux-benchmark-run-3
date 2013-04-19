b6eeac68e6db5f733eea784051f27e1923ee814e
Florian Westphal <fw@strlen.de>
2013-04-19 04:58:26 +0000

    netfilter: nfnetlink_queue: add skb info attribute
    Once we allow userspace to receive gso/gro packets, userspace
    needs to be able to determine when checksums appear to be
    broken, but are not.
    
    NFQA_SKB_CSUMNOTREADY means 'checksums will be fixed in kernel
    later, pretend they are ok'.
    
    NFQA_SKB_GSO could be used for statistics, or to determine when
    packet size exceeds mtu.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/uapi/linux/netfilter/nfnetlink_queue.h
+++ include/uapi/linux/netfilter/nfnetlink_queue.h
@@ -46,6 +46,7 @@ enum nfqnl_attr_type {
 	NFQA_CT,			/* nf_conntrack_netlink.h */
 	NFQA_CT_INFO,			/* enum ip_conntrack_info */
 	NFQA_CAP_LEN,			/* __u32 length of captured packet */
+	NFQA_SKB_INFO,			/* __u32 skb meta information */
 
 	__NFQA_MAX
 };
@@ -98,5 +99,11 @@ enum nfqnl_attr_config {
 #define NFQA_CFG_F_FAIL_OPEN			(1 << 0)
 #define NFQA_CFG_F_CONNTRACK			(1 << 1)
 #define NFQA_CFG_F_MAX				(1 << 2)
+
+/* flags for NFQA_SKB_INFO */
+/* packet appears to have wrong checksums, but they are ok */
+#define NFQA_SKB_CSUMNOTREADY (1 << 0)
+/* packet is GSO (i.e., exceeds device mtu) */
+#define NFQA_SKB_GSO (1 << 1)
 
 #endif /* _NFNETLINK_QUEUE_H */
