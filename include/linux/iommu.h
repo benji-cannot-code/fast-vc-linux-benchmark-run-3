13fbfe98d07aa5e4770186227165742b4d230d2c
Wang YanQing <udknight@gmail.com>
2013-04-19 09:38:04 +0800

    iommu: Include linux/err.h
    The linux/iommu.h header uses ERR_PTR defined
    in linux/err.h but doesn't include it.
    
    Cc:joro@8bytes.org
    Reviewed-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Wang YanQing <udknight@gmail.com>
    Signed-off-by: Joerg Roedel <joro@8bytes.org>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __LINUX_IOMMU_H
 
 #include <linux/errno.h>
+#include <linux/err.h>
 #include <linux/types.h>
 
 #define IOMMU_READ	(1)
