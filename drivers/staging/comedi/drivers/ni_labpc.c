6a5c9119a2112c72805bb217e7feac31851ffcfb
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-04-22 12:33:53 -0700

    staging: comedi: ni_labpc: pass the isr_flags to labpc_common_attach()
    The PCI and PCMCIA LabPC boards use shared interrupts and need the
    IRQF_SHARED flag set when requesting the irq, the ISA boards do not.
    
    Instead of checking the 'bustype' in labpc_common_attach() in order
    to determine the isr_flags, just pass the flags as a parameter to the
    function.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc.c
+++ drivers/staging/comedi/drivers/ni_labpc.c
@@ -1589,12 +1589,11 @@ static int labpc_eeprom_insn_read(struct comedi_device *dev,
 }
 
 int labpc_common_attach(struct comedi_device *dev,
-			unsigned int irq)
+			unsigned int irq, unsigned long isr_flags)
 {
 	const struct labpc_boardinfo *board = comedi_board(dev);
 	struct labpc_private *devpriv = dev->private;
 	struct comedi_subdevice *s;
-	unsigned long isr_flags;
 	int ret;
 	int i;
 
@@ -1617,10 +1616,6 @@ int labpc_common_attach(struct comedi_device *dev,
 	}
 
 	if (irq) {
-		isr_flags = 0;
-		if (board->bustype == pci_bustype ||
-		    board->bustype == pcmcia_bustype)
-			isr_flags |= IRQF_SHARED;
 		ret = request_irq(irq, labpc_interrupt, isr_flags,
 				  dev->board_name, dev);
 		if (ret == 0)
@@ -1739,7 +1734,7 @@ static int labpc_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	if (ret)
 		return ret;
 
-	ret = labpc_common_attach(dev, irq);
+	ret = labpc_common_attach(dev, irq, 0);
 	if (ret)
 		return ret;
 
