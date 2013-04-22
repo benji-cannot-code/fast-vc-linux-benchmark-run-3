79954a78d9f6cc5cba58d79805406eec94b114fe
sjur.brandeland@stericsson.com <sjur.brandeland@stericsson.com>
2013-04-22 23:57:03 +0000

    caif: Update Dmitry's email address.
    Dmitry's address will start bouncing in a few days,
    update to his new mail address.
    
    Cc: Dmitry Tarnyagin <dmitry.tarnyagin@stericsson.com>
    Cc: Dmitry Tarnyagin <dmitry.tarnyagin@lockless.no>
    Signed-off-by: Sjur Brændeland <sjur.brandeland@stericsson.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/caif/caif_hsi.c
+++ drivers/net/caif/caif_hsi.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright (C) ST-Ericsson AB 2010
  * Author:  Daniel Martensson
- *	    Dmitry.Tarnyagin  / dmitry.tarnyagin@stericsson.com
+ *	    Dmitry.Tarnyagin  / dmitry.tarnyagin@lockless.no
  * License terms: GNU General Public License (GPL) version 2.
  */
 
