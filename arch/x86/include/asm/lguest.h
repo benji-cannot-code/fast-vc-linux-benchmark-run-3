6844777065fd265b8d74ca7721fdd0bdc5f3d63d
Rusty Russell <rusty@rustcorp.com.au>
2013-04-22 14:10:42 +0930

    lguest: map Switcher below fixmap.
    Now we've adjusted all the code, we can simply set switcher_addr to
    wherever it needs to go below the fixmaps, rather than asserting that
    it should be so.
    
    With large NR_CPUS and PAE, people were hitting the "mapping switcher
    would thwack fixmap" message.
    
    Reported-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- arch/x86/include/asm/lguest.h
+++ arch/x86/include/asm/lguest.h
@@ -15,12 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Page for Switcher text itself, then two pages per cpu */
 #define TOTAL_SWITCHER_PAGES (1 + 2 * nr_cpu_ids)
 
-/* We map at -4M (-2M for PAE) for ease of mapping (one PTE page). */
-#ifdef CONFIG_X86_PAE
-#define SWITCHER_ADDR 0xFFE00000
-#else
-#define SWITCHER_ADDR 0xFFC00000
-#endif
 /* Where we map the Switcher, in both Host and Guest. */
 extern unsigned long switcher_addr;
 
