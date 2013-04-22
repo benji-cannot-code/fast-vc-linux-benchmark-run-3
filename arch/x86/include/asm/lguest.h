1972a6854460c47f96d2ba88ba16606a9c06c6db
Rusty Russell <rusty@rustcorp.com.au>
2013-04-22 14:10:37 +0930

    lguest: prepare to make SWITCHER_ADDR a variable.
    We currently use the whole top PGD entry for the switcher, but that's
    hitting the fixmap in some configurations (mainly, large NR_CPUS).
    Introduce a variable, currently set to the constant.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- arch/x86/include/asm/lguest.h
+++ arch/x86/include/asm/lguest.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #else
 #define SWITCHER_ADDR 0xFFC00000
 #endif
+/* Where we map the Switcher, in both Host and Guest. */
+extern unsigned long switcher_addr;
 
 /* Found in switcher.S */
 extern unsigned long default_idt_entries[];
