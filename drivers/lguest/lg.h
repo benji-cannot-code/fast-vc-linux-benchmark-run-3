9045d6b80af83e83e4a079520e3ba4ffb0962031
Rusty Russell <rusty@rustcorp.com.au>
2013-04-22 14:10:40 +0930

    lguest: don't share Switcher PTE pages between guests.
    We currently use the whole top PGD entry for the switcher, so we
    simply share a fixed page of PTEs between all guests (actually, it's
    one per Host CPU, to ensure isolation between guests).
    
    Changes to a scheme where every guest has its own mappings.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/lguest/lg.h
+++ drivers/lguest/lg.h
@@ -15,9 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/lguest.h>
 
-void free_pagetables(void);
-int init_pagetables(struct page **switcher_pages);
-
 struct pgdir {
 	unsigned long gpgdir;
 	pgd_t *pgdir;
