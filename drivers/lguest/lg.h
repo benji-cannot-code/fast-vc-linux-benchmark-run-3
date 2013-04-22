aedc921e44b8d222674b97f3676290c849e10e7a
Rusty Russell <rusty@rustcorp.com.au>
2013-04-22 14:10:41 +0930

    lguest: cache last cpu we ran on.
    This optimizes the frobbing of our Switcher map.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/lguest/lg.h
+++ drivers/lguest/lg.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct pgdir {
 	unsigned long gpgdir;
 	bool switcher_mapped;
+	int last_host_cpu;
 	pgd_t *pgdir;
 };
 
