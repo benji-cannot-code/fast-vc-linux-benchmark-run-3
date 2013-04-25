d3117cc7637d32df29dbbe8168851a449e203391
Arnd Bergmann <arnd@arndb.de>
2013-04-25 17:10:04 +0200

    ARM: orion5x: include linux/cpu.h
    The linux/cpu.h header is no longer implictly included in this
    file, so we need to an #include statement to avoid this build
    warning:
    
    arch/arm/mach-orion5x/common.c:339:3: error: implicit declaration of function 'cpu_idle_poll_ctrl' [-Werror=implicit-function-declaration]
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Jason Cooper <jason@lakedaemon.net>
    Cc: Andrew Lunn <andrew@lunn.ch>

--- arch/arm/mach-orion5x/common.c
+++ arch/arm/mach-orion5x/common.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ata_platform.h>
 #include <linux/delay.h>
 #include <linux/clk-provider.h>
+#include <linux/cpu.h>
 #include <net/dsa.h>
 #include <asm/page.h>
 #include <asm/setup.h>
