550a1818d9b296f91a505268503b574a8260a764
Jesse Barnes <jbarnes@virtuousgeek.org>
2013-04-25 12:55:03 -0700

    drm/i915: warn about invalid pfit modes
    We prevent invalid ones from getting here in the first place, but it
    doesn't hurt to have an extra sanity check.
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_panel.c
+++ drivers/gpu/drm/i915/intel_panel.c
@@ -59,7 +59,6 @@ intel_pch_panel_fitting(struct intel_crtc *intel_crtc,
 			struct intel_crtc_config *pipe_config,
 			int fitting_mode)
 {
-	struct drm_i915_private *dev_priv = intel_crtc->base.dev->dev_private;
 	struct drm_display_mode *mode, *adjusted_mode;
 	int x, y, width, height;
 
@@ -108,12 +107,15 @@ intel_pch_panel_fitting(struct intel_crtc *intel_crtc,
 		}
 		break;
 
-	default:
 	case DRM_MODE_SCALE_FULLSCREEN:
 		x = y = 0;
 		width = adjusted_mode->hdisplay;
 		height = adjusted_mode->vdisplay;
 		break;
+
+	default:
+		WARN(1, "bad panel fit mode: %d\n", fitting_mode);
+		return;
 	}
 
 done:
@@ -268,7 +270,6 @@ void intel_gmch_panel_fitting(struct intel_crtc *intel_crtc,
 			}
 		}
 		break;
-	default:
 	case DRM_MODE_SCALE_FULLSCREEN:
 		/*
 		 * Full scaling, even if it changes the aspect ratio.
@@ -286,6 +287,9 @@ void intel_gmch_panel_fitting(struct intel_crtc *intel_crtc,
 						 HORIZ_INTERP_BILINEAR);
 		}
 		break;
+	default:
+		WARN(1, "bad panel fit mode: %d\n", fitting_mode);
+		return;
 	}
 
 	/* 965+ wants fuzzy fitting */
