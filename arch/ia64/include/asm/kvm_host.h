352c439722b0b177fbfa9957d2c18e87af317ee4
Alexander Graf <agraf@suse.de>
2013-04-25 11:50:03 +0200

    KVM: IA64: Carry non-ia64 changes into ia64
    We changed a few things in non-ia64 code paths. This patch blindly applies
    the changes to the ia64 code as well, hoping it proves useful in case anyone
    revives the ia64 kvm code.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/ia64/include/asm/kvm_host.h
+++ arch/ia64/include/asm/kvm_host.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KVM_USER_MEM_SLOTS 32
 
 #define KVM_COALESCED_MMIO_PAGE_OFFSET 1
+#define KVM_IRQCHIP_NUM_PINS  KVM_IOAPIC_NUM_PINS
 
 /* define exit reasons from vmm to kvm*/
 #define EXIT_REASON_VM_PANIC		0
