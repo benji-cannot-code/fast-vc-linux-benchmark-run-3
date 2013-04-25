abe979087f658bd5bbf66bb40ca7d5f1aa5bf53f
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2013-04-25 13:24:33 +0900

    f2fs: add a tracepoint on f2fs_new_inode
    This can help when debugging the free nid allocation flows.
    
    Reviewed-by: Namjae Jeon <namjae.jeon@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -64,7 +64,7 @@ static struct inode *f2fs_new_inode(struct inode *dir, umode_t mode)
 		nid_free = true;
 		goto out;
 	}
-
+	trace_f2fs_new_inode(inode, 0);
 	mark_inode_dirty(inode);
 	return inode;
 
@@ -72,6 +72,7 @@ out:
 	clear_nlink(inode);
 	unlock_new_inode(inode);
 fail:
+	trace_f2fs_new_inode(inode, err);
 	iput(inode);
 	if (nid_free)
 		alloc_nid_failed(sbi, ino);
