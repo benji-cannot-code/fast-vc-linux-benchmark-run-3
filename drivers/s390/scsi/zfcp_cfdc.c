4e0b6e08cb87138a561610cdb7c4dec0700a25ed
Sebastian Ott <sebott@linux.vnet.ibm.com>
2013-04-26 16:13:47 +0200

    [SCSI] zfcp: cfdc fops add owner
    Set the owner member of zfcp_cfdc_fops, to ensure that the
    caller of these functions holds a module reference.
    
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Steffen Maier <maier@linux.vnet.ibm.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/s390/scsi/zfcp_cfdc.c
+++ drivers/s390/scsi/zfcp_cfdc.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compat.h>
 #include <linux/slab.h>
 #include <linux/types.h>
+#include <linux/module.h>
 #include <linux/miscdevice.h>
 #include <asm/compat.h>
 #include <asm/ccwdev.h>
@@ -251,6 +252,7 @@ static long zfcp_cfdc_dev_ioctl(struct file *file, unsigned int command,
 }
 
 static const struct file_operations zfcp_cfdc_fops = {
+	.owner = THIS_MODULE,
 	.open = nonseekable_open,
 	.unlocked_ioctl = zfcp_cfdc_dev_ioctl,
 #ifdef CONFIG_COMPAT
