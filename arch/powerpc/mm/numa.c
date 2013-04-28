943157d727a829a19fbc202e9544527b5e586eff
Stephen Rothwell <sfr@canb.auug.org.au>
2013-04-28 18:04:33 +0000

    powerpc: Fix build failure after merge of the cgroup tree
    After merging the cgroup tree, today's linux-next build (powerpc
    ppc64_defconfig) failed like this:
    
    arch/powerpc/mm/numa.c: In function 'arch_update_cpu_topology':
    arch/powerpc/mm/numa.c:1465:2: error: implicit declaration of function 'kzalloc' [-Werror=implicit-function-declaration]
    arch/powerpc/mm/numa.c:1465:10: error: assignment makes pointer from integer without a cast [-Werror]
    arch/powerpc/mm/numa.c:1497:2: error: implicit declaration of function 'kfree' [-Werror=implicit-function-declaration]
    
    Caused by commit 30c05350c39d ("powerpc/pseries: Use stop machine to
    update cpu maps") from the powerpc tree interacting with (probably)
    commit ff794dea52ea ("cpuset: remove include of cgroup.h from cpuset.h")
    from the cgroup tree.  Removing includes from header files is fraught
    with danger ...
    
    The former should have added an include of linux/slab.h to
    arch/powerpc/mm/numa.c.
    
    I have added the following merge fix patch for today (but it should be
    applied to the powerpc tree ASAP).
    
    From: Stephen Rothwell <sfr@canb.auug.org.au>
    Date: Mon, 29 Apr 2013 14:01:44 +1000
    Subject: [PATCH] powerpc: numa.c: using kzalloc/kfree requires including
     slab.h
    
    fixes these build errors:
    
    arch/powerpc/mm/numa.c: In function 'arch_update_cpu_topology':
    arch/powerpc/mm/numa.c:1465:2: error: implicit declaration of function 'kzalloc' [-Werror=implicit-function-declaration]
    arch/powerpc/mm/numa.c:1465:10: error: assignment makes pointer from integer without a cast [-Werror]
    arch/powerpc/mm/numa.c:1497:2: error: implicit declaration of function 'kfree' [-Werror=implicit-function-declaration]
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/mm/numa.c
+++ arch/powerpc/mm/numa.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 #include <asm/sparsemem.h>
 #include <asm/prom.h>
 #include <asm/smp.h>
