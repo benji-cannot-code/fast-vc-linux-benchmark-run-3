c94a16c6d6dc8f07b1a6a8d4280b2e93d1e4c30b
Chegu Vinod <chegu_vinod@hp.com>
2013-04-27 18:31:04 -0700

    KVM: x86: Increase the "hard" max VCPU limit
    KVM guests today use 8bit APIC ids allowing for 256 ID's. Reserving one
    ID for Broadcast interrupts should leave 255 ID's. In case of KVM there
    is no need for reserving another ID for IO-APIC so the hard max limit for
    VCPUS can be increased from 254 to 255. (This was confirmed by Gleb Natapov
    http://article.gmane.org/gmane.comp.emulators.kvm.devel/99713  )
    
    Signed-off-by: Chegu Vinod <chegu_vinod@hp.com>
    Signed-off-by: Gleb Natapov <gleb@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/msr-index.h>
 #include <asm/asm.h>
 
-#define KVM_MAX_VCPUS 254
+#define KVM_MAX_VCPUS 255
 #define KVM_SOFT_MAX_VCPUS 160
 #define KVM_USER_MEM_SLOTS 125
 /* memory slots that are not exposed to userspace */
