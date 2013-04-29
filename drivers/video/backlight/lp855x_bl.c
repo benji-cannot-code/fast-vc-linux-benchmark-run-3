fa7a279cb93f03f32b53cd9f60f4046506cac183
Devendra Naga <devendra.aaru@gmail.com>
2013-04-29 16:17:30 -0700

    drivers/video/backlight/lp855x_bl.c: fix compiler warning in lp855x_probe
    While doing with make W=1 gcc (gcc (GCC) 4.7.2 20121109 (Red Hat
    4.7.2-8)) I found
    
      drivers/video/backlight/lp855x_bl.c: In function `lp855x_probe':
      drivers/video/backlight/lp855x_bl.c:342:35: warning: variable `mode' set but not used [-Wunused-but-set-variable]
    
    fixed by removing it as since its not used anywhere
    
    Signed-off-by: Devendra Naga <devendra.aaru@gmail.com>
    Acked-by: Milo Kim <milo.kim@ti.com>
    Cc: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/backlight/lp855x_bl.c
+++ drivers/video/backlight/lp855x_bl.c
@@ -340,7 +340,6 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 {
 	struct lp855x *lp;
 	struct lp855x_platform_data *pdata = cl->dev.platform_data;
-	enum lp855x_brightness_ctrl_mode mode;
 	int ret;
 
 	if (!pdata) {
@@ -355,7 +354,6 @@ static int lp855x_probe(struct i2c_client *cl, const struct i2c_device_id *id)
 	if (!lp)
 		return -ENOMEM;
 
-	mode = pdata->mode;
 	lp->client = cl;
 	lp->dev = &cl->dev;
 	lp->pdata = pdata;
