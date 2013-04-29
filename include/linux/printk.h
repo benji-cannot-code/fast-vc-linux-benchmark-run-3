9bc9f6a9d14ea421d9279d4c7ff89bc284663573
Andrew Morton <akpm@linux-foundation.org>
2013-04-29 16:17:19 -0700

    include/linux/printk.h: include stdarg.h
    printk.h uses va_list but doesn't include stdarg.h.  Hence printk.h is
    unusable unless its includer has already included kernel.h (which includes
    stdarg.h).
    
    Remove the dependency by including stdarg.h in printk.h
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __KERNEL_PRINTK__
 #define __KERNEL_PRINTK__
 
+#include <stdarg.h>
 #include <linux/init.h>
 #include <linux/kern_levels.h>
 
