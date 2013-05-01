e8574e7f7d14e6d4782991232731ee5861585062
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-05-01 19:17:31 +0000

    kvm/ppc/mpic: fix missing unlock in set_base_addr()
    Add the missing unlock before return from function set_base_addr()
    when disables the mapping.
    
    Introduced by commit 5df554ad5b7522ea62b0ff9d5be35183494efc21
    (kvm/ppc/mpic: in-kernel MPIC emulation)
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/kvm/mpic.c
+++ arch/powerpc/kvm/mpic.c
@@ -1487,8 +1487,8 @@ static int set_base_addr(struct openpic *opp, struct kvm_device_attr *attr)
 
 	map_mmio(opp);
 
-	mutex_unlock(&opp->kvm->slots_lock);
 out:
+	mutex_unlock(&opp->kvm->slots_lock);
 	return 0;
 }
 
