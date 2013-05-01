a498ff2c2d1dab09875b4d15a194f2e334b27f38
Tim Chen <tim.c.chen@linux.intel.com>
2013-05-01 12:52:48 -0700

    crypto: crct10dif - Wrap crc_t10dif function all to use crypto transform framework
    When CRC T10 DIF is calculated using the crypto transform framework, we
    wrap the crc_t10dif function call to utilize it.  This allows us to
    take advantage of any accelerated CRC T10 DIF transform that is
    plugged into the crypto framework.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/linux/crc-t10dif.h
+++ include/linux/crc-t10dif.h
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
+#define CRC_T10DIF_DIGEST_SIZE 2
+#define CRC_T10DIF_BLOCK_SIZE 1
+
+__u16 crc_t10dif_generic(__u16 crc, const unsigned char *buffer, size_t len);
 __u16 crc_t10dif(unsigned char const *, size_t);
 
 #endif
