b5412e811968247605b6059d890a920341bd606b
Arjan van de Ven <arjan@linux.intel.com>
2013-05-01 16:38:23 -0400

    NVMe: Set TASK_INTERRUPTIBLE before processing queues
    The kthread has two tasks; handling timeouts (for which it runs once per
    second), and submitting queued BIOs.  If a BIO happens to be queued after
    the thread has processed the queue but before it calls schedule_timeout(),
    the thread will sleep for a second before submitting it, which can cause
    performance problems in some rare cases (that will become more common in
    a subsequent patch).
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Tested-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -1292,7 +1292,7 @@ static int nvme_kthread(void *data)
 	struct nvme_dev *dev;
 
 	while (!kthread_should_stop()) {
-		__set_current_state(TASK_RUNNING);
+		set_current_state(TASK_INTERRUPTIBLE);
 		spin_lock(&dev_list_lock);
 		list_for_each_entry(dev, &dev_list, node) {
 			int i;
@@ -1309,7 +1309,6 @@ static int nvme_kthread(void *data)
 			}
 		}
 		spin_unlock(&dev_list_lock);
-		set_current_state(TASK_INTERRUPTIBLE);
 		schedule_timeout(round_jiffies_relative(HZ));
 	}
 	return 0;
