7aef4534f1634b41bad279f897e7fa892ffdba25
Linus Walleij <linus.walleij@linaro.org>
2013-05-02 17:05:05 +0200

    ARM: u300: delete <mach/irqs.h>
    All IRQs are now obtained from the device tree, and this file
    is unused, so delete it.
    
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-u300/timer.c
+++ arch/arm/mach-u300/timer.c
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 
-#include <mach/irqs.h>
-
 /* Generic stuff */
 #include <asm/sched_clock.h>
 #include <asm/mach/map.h>
