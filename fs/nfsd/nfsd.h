b7bc473af60a52a0a5b947c486fc4412178eca9c
Steve Dickson <steved@redhat.com>
2013-05-02 13:18:57 -0400

    NFSDv4.2: Add NFS v4.2 support to the NFS server
    This enables NFSv4.2 support for the server. To enable this
    code do the following:
      echo "+4.2" >/proc/fs/nfsd/versions
    
    after the nfsd kernel module is loaded.
    
    On its own this does nothing except allow the server to respond to
    compounds with minorversion set to 2.  All the new NFSv4.2 features are
    optional, so this is perfectly legal.
    
    Signed-off-by: Steve Dickson <steved@redhat.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfsd.h
+++ fs/nfsd/nfsd.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * nfsd version
  */
-#define NFSD_SUPPORTED_MINOR_VERSION	1
+#define NFSD_SUPPORTED_MINOR_VERSION	2
 /*
  * Maximum blocksizes supported by daemon under various circumstances.
  */
