d848515c183896661a312c6563b0be0b24647e65
Axel Lin <axel.lin@ingics.com>
2013-05-03 20:46:10 +0800

    regulator: Remove all platform_set_drvdata(pdev, NULL) in drivers
    Since 0998d06310 "device-core: Ensure drvdata = NULL when no driver is bound",
    this is done by driver core after device_release or on probe failure.
    Thus we can remove all platform_set_drvdata(pdev, NULL) in drivers.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- drivers/regulator/wm8400-regulator.c
+++ drivers/regulator/wm8400-regulator.c
@@ -251,7 +251,6 @@ static int wm8400_regulator_remove(struct platform_device *pdev)
 {
 	struct regulator_dev *rdev = platform_get_drvdata(pdev);
 
-	platform_set_drvdata(pdev, NULL);
 	regulator_unregister(rdev);
 
 	return 0;
