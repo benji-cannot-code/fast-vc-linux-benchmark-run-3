01a9a9454d19c3bc892a3d0b4722308117527be9
Lisa Nguyen <lisa@xenapiadmin.com>
2013-05-06 13:04:30 -0700

    vme/devices: Fixed camel-case variable names
    Fixed camel-case variable names in vme_user.c and vme_user.h.
    
    Signed-off-by: Lisa Nguyen <lisa@xenapiadmin.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vme/devices/vme_user.c
+++ drivers/staging/vme/devices/vme_user.c
@@ -110,7 +110,7 @@ struct driver_stats {
 	unsigned long ioctls;
 	unsigned long irqs;
 	unsigned long berrs;
-	unsigned long dmaErrors;
+	unsigned long dmaerrors;
 	unsigned long timeouts;
 	unsigned long external;
 };
@@ -161,7 +161,7 @@ static void reset_counters(void)
 	statistics.ioctls = 0;
 	statistics.irqs = 0;
 	statistics.berrs = 0;
-	statistics.dmaErrors = 0;
+	statistics.dmaerrors = 0;
 	statistics.timeouts = 0;
 }
 
