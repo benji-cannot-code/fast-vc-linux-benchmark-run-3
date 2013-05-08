10ac2f901294be41df4d9388d8526207cd58a6b7
Dimitris Papastamos <dp@opensource.wolfsonmicro.com>
2013-05-08 14:15:35 +0100

    ASoC: wm_adsp: Expose coefficient blocks as ALSA binary controls
    Add initial support for runtime tuning for the ADSP cores.  This
    is achieved by exposing the coefficient configuration blocks as
    ALSA binary controls.
    The current code assumes that no controls on the DSP are volatile.
    
    Signed-off-by: Dimitris Papastamos <dp@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm_adsp.h
+++ sound/soc/codecs/wm_adsp.h
@@ -31,6 +31,7 @@ struct wm_adsp_alg_region {
 	unsigned int alg;
 	int type;
 	unsigned int base;
+	size_t len;
 };
 
 struct wm_adsp {
@@ -56,6 +57,8 @@ struct wm_adsp {
 	bool running;
 
 	struct regulator *dvfs;
+
+	struct wm_coeff *wm_coeff;
 };
 
 #define WM_ADSP1(wname, num) \
