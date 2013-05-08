b2dc0181876be3c651904b985fcd4c04e0fadb56
Fabio Estevam <fabio.estevam@freescale.com>
2013-05-08 21:05:55 +0800

    video: mxsfb: Adapt to new videomode API
    commit 6cd2c7db4 (videomode: videomode_from_timing work) changed the name of
    the function from videomode_from_timing() to videomode_from_timings().
    
    commit 32ed6ef1 (videomode: create enum for videomode's display flags) changed
    the 'data_flags' field in videomode structure to 'flags'
    
    Adapt to these changes in order to fix the following errors:
    
    drivers/video/mxsfb.c:761:3: error: too many arguments to function 'videomode_from_timing'
    drivers/video/mxsfb.c:761:7: error: void value not ignored as it ought to be
    drivers/video/mxsfb.c:768:9: error: 'struct videomode' has no member named 'data_flags'
    drivers/video/mxsfb.c:770:9: error: 'struct videomode' has no member named 'data_flags'
    
    Also, select VIDEOMODE_HELPER instead of OF_VIDEOMODE, as this one is
    deprecated.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- drivers/video/mxsfb.c
+++ drivers/video/mxsfb.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/of_device.h>
-#include <video/of_display_timing.h>
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/dma-mapping.h>
@@ -51,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pinctrl/consumer.h>
 #include <linux/fb.h>
 #include <linux/regulator/consumer.h>
+#include <video/of_display_timing.h>
 #include <video/videomode.h>
 
 #define REG_SET	4
@@ -778,16 +778,16 @@ static int mxsfb_init_fbinfo_dt(struct mxsfb_info *host)
 		struct videomode vm;
 		struct fb_videomode fb_vm;
 
-		ret = videomode_from_timing(timings, &vm, i);
+		ret = videomode_from_timings(timings, &vm, i);
 		if (ret < 0)
 			goto put_timings_node;
 		ret = fb_videomode_from_videomode(&vm, &fb_vm);
 		if (ret < 0)
 			goto put_timings_node;
 
-		if (vm.data_flags & DISPLAY_FLAGS_DE_HIGH)
+		if (vm.flags & DISPLAY_FLAGS_DE_HIGH)
 			host->sync |= MXSFB_SYNC_DATA_ENABLE_HIGH_ACT;
-		if (vm.data_flags & DISPLAY_FLAGS_PIXDATA_NEGEDGE)
+		if (vm.flags & DISPLAY_FLAGS_PIXDATA_NEGEDGE)
 			host->sync |= MXSFB_SYNC_DOTCLK_FALLING_ACT;
 		fb_add_videomode(&fb_vm, &fb_info->modelist);
 	}
