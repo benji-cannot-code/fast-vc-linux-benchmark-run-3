85a21c672ae5c487c0eb38220c5e124d4bc2766a
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-05-09 17:41:59 +0200

    ARM: shmobile: armadillo-reference: Move pinctrl mappings to device tree
    Replace the pinctrl mappings in board code by device tree mappings.
    For devices that are still instantiated from board code reference the
    mappings as the default pin controller state to apply them at boot time.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-armadillo800eva-reference.c
+++ arch/arm/mach-shmobile/board-armadillo800eva-reference.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/gpio.h>
 #include <linux/io.h>
-#include <linux/pinctrl/machine.h>
 #include <mach/common.h>
 #include <mach/r8a7740.h>
 #include <asm/mach/arch.h>
@@ -120,12 +119,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	usbhsf_power_ctrl()
  */
 
-static const struct pinctrl_map eva_pinctrl_map[] = {
-	/* SCIFA1 */
-	PIN_MAP_MUX_GROUP_DEFAULT("sh-sci.1", "e6050000.pfc",
-				  "scifa1_data", "scifa1"),
-};
-
 static void __init eva_clock_init(void)
 {
 	struct clk *system	= clk_get(NULL, "system_clk");
@@ -169,8 +162,6 @@ static void __init eva_init(void)
 
 	r8a7740_clock_init(MD_CK0 | MD_CK2);
 	eva_clock_init();
-
-	pinctrl_register_mappings(eva_pinctrl_map, ARRAY_SIZE(eva_pinctrl_map));
 
 	r8a7740_meram_workaround();
 
