d41813a9479a3c416c275e15d56e4108a7968e1e
Libo Chen <libo.chen@huawei.com>
2013-05-09 02:08:33 -0400

    usb: tilegx: fix memleak when create hcd fail
    When usb_create_hcd fail, we should call gxio_usb_host_destroy
    
    Signed-off-by: Libo Chen <libo.chen@huawei.com>
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com> [extended to EHCI]

--- drivers/usb/host/ohci-tilegx.c
+++ drivers/usb/host/ohci-tilegx.c
@@ -113,8 +113,10 @@ static int ohci_hcd_tilegx_drv_probe(struct platform_device *pdev)
 
 	hcd = usb_create_hcd(&ohci_tilegx_hc_driver, &pdev->dev,
 			     dev_name(&pdev->dev));
-	if (!hcd)
-		return -ENOMEM;
+	if (!hcd) {
+		ret = -ENOMEM;
+		goto err_hcd;
+	}
 
 	/*
 	 * We don't use rsrc_start to map in our registers, but seems like
@@ -166,6 +168,7 @@ err_have_irq:
 err_no_irq:
 	tilegx_stop_ohc();
 	usb_put_hcd(hcd);
+err_hcd:
 	gxio_usb_host_destroy(&pdata->usb_ctx);
 	return ret;
 }
