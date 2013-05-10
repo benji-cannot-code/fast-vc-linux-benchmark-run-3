cca9679453bb2d87a86c4cc9aad9070813b32e6c
Lespiau, Damien <damien.lespiau@intel.com>
2013-05-10 12:36:42 +0000

    drm: Make the HPD status updates debug logs more readable
    Instead of just printing "status updated from 1 to 2", make those enum
    numbers immediately readable.
    
    v2: Also patch output_poll_execute() (Daniel Vetter)
    v3: Use drm_get_connector_status_name (Ville Syrjälä)
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org> (for v1)
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_crtc_helper.c
+++ drivers/gpu/drm/drm_crtc_helper.c
@@ -1008,10 +1008,11 @@ static void output_poll_execute(struct work_struct *work)
 			continue;
 
 		connector->status = connector->funcs->detect(connector, false);
-		DRM_DEBUG_KMS("[CONNECTOR:%d:%s] status updated from %d to %d\n",
+		DRM_DEBUG_KMS("[CONNECTOR:%d:%s] status updated from %s to %s\n",
 			      connector->base.id,
 			      drm_get_connector_name(connector),
-			      old_status, connector->status);
+			      drm_get_connector_status_name(old_status),
+			      drm_get_connector_status_name(connector->status));
 		if (old_status != connector->status)
 			changed = true;
 	}
@@ -1086,10 +1087,11 @@ void drm_helper_hpd_irq_event(struct drm_device *dev)
 		old_status = connector->status;
 
 		connector->status = connector->funcs->detect(connector, false);
-		DRM_DEBUG_KMS("[CONNECTOR:%d:%s] status updated from %d to %d\n",
+		DRM_DEBUG_KMS("[CONNECTOR:%d:%s] status updated from %s to %s\n",
 			      connector->base.id,
 			      drm_get_connector_name(connector),
-			      old_status, connector->status);
+			      drm_get_connector_status_name(old_status),
+			      drm_get_connector_status_name(connector->status));
 		if (old_status != connector->status)
 			changed = true;
 	}
