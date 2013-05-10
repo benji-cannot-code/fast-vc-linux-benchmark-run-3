629c2ce86b4a4783c307b55140ab08ac9707a527
Joe Thornber <ejt@redhat.com>
2013-05-10 14:37:21 +0100

    dm thin: generate event when metadata threshold passed
    Generate a dm event when the amount of remaining thin pool metadata
    space falls below a certain level.
    
    The threshold is taken to be a quarter of the size of the metadata
    device with a minimum threshold of 4MB.
    
    Signed-off-by: Joe Thornber <ejt@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-thin-metadata.h
+++ drivers/md/dm-thin-metadata.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DM_THIN_METADATA_H
 
 #include "persistent-data/dm-block-manager.h"
+#include "persistent-data/dm-space-map.h"
 
 #define THIN_METADATA_BLOCK_SIZE 4096
 
@@ -193,6 +194,11 @@ int dm_pool_resize_metadata_dev(struct dm_pool_metadata *pmd, dm_block_t new_siz
  * that nothing is changing.
  */
 void dm_pool_metadata_read_only(struct dm_pool_metadata *pmd);
+
+int dm_pool_register_metadata_threshold(struct dm_pool_metadata *pmd,
+					dm_block_t threshold,
+					dm_sm_threshold_fn fn,
+					void *context);
 
 /*----------------------------------------------------------------*/
 
