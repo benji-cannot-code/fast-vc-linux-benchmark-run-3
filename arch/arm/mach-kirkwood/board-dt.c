439d1159b02e9add87211696eca6d3a5cb145ef6
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2013-05-11 03:08:08 +0200

    ARM: kirkwood: move DT boards to SoC-centric clock init
    SoC centric clock init for Kirkwood can be used by calling of_clk_init.
    Use it and get rid of mvebu_clocks_init.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Tested-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Mike Turquette <mturquette@linaro.org>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-dt.c
+++ arch/arm/mach-kirkwood/board-dt.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/clk-provider.h>
-#include <linux/clk/mvebu.h>
 #include <linux/kexec.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -78,7 +77,7 @@ static void __init kirkwood_legacy_clk_init(void)
 
 static void __init kirkwood_of_clk_init(void)
 {
-	mvebu_clocks_init();
+	of_clk_init(NULL);
 	kirkwood_legacy_clk_init();
 }
 
