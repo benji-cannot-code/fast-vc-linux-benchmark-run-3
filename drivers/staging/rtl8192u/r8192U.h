b9b8deba988723c36105e297858f5766f42003bf
Xenia Ragiadakou <burzalodowa@gmail.com>
2013-05-13 20:15:51 +0300

    rtl8192u: fix whitespace around if statements in r8192U.h
    This patch fixes a part of the following checkpatch error:
    ERROR: space required before the open parenthesis '('
    by adding space after if
    
    Signed-off-by: Xenia Ragiadakou <burzalodowa@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8192U.h
+++ drivers/staging/rtl8192u/r8192U.h
@@ -89,7 +89,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DMESGE(x,a...)
 extern u32 rt_global_debug_component;
 #define RT_TRACE(component, x, args...) \
-do { if(rt_global_debug_component & component) \
+do { if (rt_global_debug_component & component) \
 	printk(KERN_DEBUG RTL819xU_MODULE_NAME ":" x "\n" , \
 	       ##args);\
 }while(0);
