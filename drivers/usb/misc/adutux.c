e0dc8334627bf7e136b7643cac6a42c57d16c809
Lisa Nguyen <lisa@xenapiadmin.com>
2013-05-13 12:40:47 -0700

    usb: misc: Replaced deprecated preprocessor in adutux.c
    Replaced asm/uaccess.h with linux/uaccess.h in adutux.c
    
    Signed-off-by: Lisa Nguyen <lisa@xenapiadmin.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/misc/adutux.c
+++ drivers/usb/misc/adutux.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/usb.h>
 #include <linux/mutex.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #ifdef CONFIG_USB_DEBUG
 static int debug = 5;
