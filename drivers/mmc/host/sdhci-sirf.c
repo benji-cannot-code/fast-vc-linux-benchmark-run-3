60e7a0175db0d09038901f6a3a79a07d55d2277e
Barry Song <Baohua.Song@csr.com>
2013-05-14 13:49:09 +0800

    mmc: sdhci-sirf: let device core setup the default pin configuration
    With device core now able to setup the default pin configuration, the
    call to devm_pinctrl_get_select_default can be removed. And the pin
    configuration code based on the deprecated Samsung specific gpio
    bindings is also removed.
    
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Barry Song <Baohua.Song@csr.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-sirf.c
+++ drivers/mmc/host/sdhci-sirf.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_gpio.h>
 #include <linux/mmc/slot-gpio.h>
-#include <linux/pinctrl/consumer.h>
 #include "sdhci-pltfm.h"
 
 struct sdhci_sirf_priv {
@@ -47,14 +46,7 @@ static int sdhci_sirf_probe(struct platform_device *pdev)
 	struct sdhci_host *host;
 	struct sdhci_pltfm_host *pltfm_host;
 	struct sdhci_sirf_priv *priv;
-	struct pinctrl *pinctrl;
 	int ret;
-
-	pinctrl = devm_pinctrl_get_select_default(&pdev->dev);
-	if (IS_ERR(pinctrl)) {
-		dev_err(&pdev->dev, "unable to get pinmux");
-		return PTR_ERR(pinctrl);
-	}
 
 	priv = devm_kzalloc(&pdev->dev, sizeof(struct sdhci_sirf_priv),
 		GFP_KERNEL);
