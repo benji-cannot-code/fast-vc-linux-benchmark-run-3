d63825dbe21625e7019060f123648209cdc1ab0e
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2013-05-14 17:02:11 +1000

    powerpc: Set show_unhandled_signals to 1 by default
    Just like other architectures
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/kernel/signal.c
+++ arch/powerpc/kernel/signal.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * through debug.exception-trace sysctl.
  */
 
-int show_unhandled_signals = 0;
+int show_unhandled_signals = 1;
 
 /*
  * Allocate space for the signal frame
