57e338eb9e1baaa9c6fd76f6cd3b5fa1be4b7729
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-05-14 13:37:07 -0700

    staging: comedi: usbduxfast: remove extra dev_err() messages
    A dev_err() message is output if send_dux_commands() fails. Remove
    the extra dev_err() messages output by the callers.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/usbduxfast.c
+++ drivers/staging/comedi/drivers/usbduxfast.c
@@ -998,8 +998,6 @@ static int usbduxfast_ai_cmd(struct comedi_device *dev,
 	/* 0 means that the AD commands are sent */
 	result = send_dux_commands(dev, SENDADCOMMANDS);
 	if (result < 0) {
-		dev_err(dev->class_dev,
-			"adc command could not be submitted, aborting\n");
 		up(&devpriv->sem);
 		return result;
 	}
@@ -1120,8 +1118,6 @@ static int usbduxfast_ai_insn_read(struct comedi_device *dev,
 	/* 0 means that the AD commands are sent */
 	err = send_dux_commands(dev, SENDADCOMMANDS);
 	if (err < 0) {
-		dev_err(dev->class_dev,
-			"adc command could not be submitted, aborting\n");
 		up(&devpriv->sem);
 		return err;
 	}
