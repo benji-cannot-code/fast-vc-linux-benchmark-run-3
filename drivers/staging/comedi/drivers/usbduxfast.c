c947fa85d4d80ff1c03b085788f093fa009f777a
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-05-14 13:34:54 -0700

    staging: comedi: usbduxfast: remove 'intf' from the private data
    The usb_interface pointer is already saved in the comedi_device
    by the comedi core.
    
    Remove the pointer from the private data and use comedi_to_usb_interface()
    to get the usb_interface when needed.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/usbduxfast.c
+++ drivers/staging/comedi/drivers/usbduxfast.c
@@ -153,7 +153,6 @@ struct usbduxfast_private {
 	struct urb *urbIn;	/* BULK-transfer handling: urb */
 	int8_t *transfer_buffer;
 	int16_t *insnBuffer;	/* input buffer for single insn */
-	struct usb_interface *intf;	/* interface structure */
 	short int ai_cmd_running;	/* asynchronous command is running */
 	short int ai_continous;	/* continous acquisition */
 	long int ai_sample_count;	/* number of samples to acquire */
@@ -1240,8 +1239,7 @@ static int usbduxfast_attach_common(struct comedi_device *dev)
 
 static int usbduxfast_request_firmware(struct comedi_device *dev)
 {
-	struct usbduxfast_private *devpriv = dev->private;
-	struct usb_interface *intf = devpriv->intf;
+	struct usb_interface *intf = comedi_to_usb_interface(dev);
 	struct usb_device *usb = interface_to_usbdev(intf);
 	const struct firmware *fw;
 	int ret;
@@ -1277,7 +1275,6 @@ static int usbduxfast_auto_attach(struct comedi_device *dev,
 
 	sema_init(&devpriv->sem, 1);
 	devpriv->usb = usb;
-	devpriv->intf = intf;
 	usb_set_intfdata(intf, devpriv);
 
 	devpriv->dux_commands = kmalloc(SIZEOFDUXBUFFER, GFP_KERNEL);
@@ -1322,6 +1319,7 @@ static int usbduxfast_auto_attach(struct comedi_device *dev,
 
 static void usbduxfast_detach(struct comedi_device *dev)
 {
+	struct usb_interface *intf = comedi_to_usb_interface(dev);
 	struct usbduxfast_private *devpriv = dev->private;
 
 	if (!devpriv)
@@ -1329,8 +1327,7 @@ static void usbduxfast_detach(struct comedi_device *dev)
 
 	down(&devpriv->sem);
 
-	if (devpriv->intf)
-		usb_set_intfdata(devpriv->intf, NULL);
+	usb_set_intfdata(intf, NULL);
 
 	if (devpriv->urbIn) {
 		/* waits until a running transfer is over */
