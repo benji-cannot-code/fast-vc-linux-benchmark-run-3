4a1042c53bdbcc45f0a625af092e428238b46494
Elena Ufimtseva <ufimtseva@gmail.com>
2013-05-15 12:57:16 -0400

    Staging: ced1401: Fixes C pointer format warnings
    Fixes checkpatch warnings about C pointer format.
    
    Signed-off-by: Elena Ufimtseva <ufimtseva@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ced1401/machine.h
+++ drivers/staging/ced1401/machine.h
@@ -81,15 +81,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
     #define FAR
 
     typedef int BOOL;       /*  To match Windows */
-    typedef char * LPSTR;
-    typedef const char * LPCSTR;
+    typedef char *LPSTR;
+    typedef const char *LPCSTR;
     typedef unsigned short WORD;
     typedef unsigned int  DWORD;
     typedef unsigned char  BYTE;
     typedef BYTE  BOOLEAN;
     typedef unsigned char UCHAR;
     #define __packed __attribute__((packed))
-    typedef BYTE * LPBYTE;
+    typedef BYTE *LPBYTE;
     #define HIWORD(x) (WORD)(((x)>>16) & 0xffff)
     #define LOWORD(x) (WORD)((x) & 0xffff)
 #endif
