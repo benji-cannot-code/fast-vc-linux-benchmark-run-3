413f7f77049bd9f13e76bfbc0e016e6401188273
Tülin İzer <tulinizer@gmail.com>
2013-05-15 05:33:48 +0300

    Staging: silicom: Fixed error 'space required after that ',''.
    This patch fixes error 'space required after that ','' found by
    checkpatch in driver silicom.
    
    Signed-off-by: Tülin İzer <tulinizer@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/silicom/bpctl_mod.c
+++ drivers/staging/silicom/bpctl_mod.c
@@ -5788,7 +5788,7 @@ static const struct file_operations Fops = {
 };
 
 #ifndef PCI_DEVICE
-#define PCI_DEVICE(vend,dev) \
+#define PCI_DEVICE(vend, dev) \
 	.vendor = (vend), .device = (dev), \
 	.subvendor = PCI_ANY_ID, .subdevice = PCI_ANY_ID
 #endif
