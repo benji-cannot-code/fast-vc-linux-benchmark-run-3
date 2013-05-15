0af3c2f3de450766a94f696068f8edf18d4260b1
Sukadev Bhattiprolu <sukadev@linux.vnet.ibm.com>
2013-05-14 22:56:51 -0700

    perf top: Add --objdump option
    perf: Add objdump option to 'perf top'
    
    Like with 'perf annotate' add the --objdump option to perf top so users
    can specify an alternate path to the /usr/bin/objdump binary.
    
    Reported-by: David A. Gilbert <DavidAGilbert@uk.ibm.com>
    Signed-off-by: Sukadev Bhattiprolu <sukadev@linux.vnet.ibm.com>
    Cc: DavidAGilbert@uk.ibm.com
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Maynard Johnson <mpjohn@us.ibm.com>
    Link: http://lkml.kernel.org/r/20130515055651.GA9985@us.ibm.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-top.c
+++ tools/perf/builtin-top.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "util/xyarray.h"
 #include "util/sort.h"
 #include "util/intlist.h"
+#include "arch/common.h"
 
 #include "util/debug.h"
 
@@ -940,6 +941,12 @@ static int __cmd_top(struct perf_top *top)
 	if (top->session == NULL)
 		return -ENOMEM;
 
+	if (!objdump_path) {
+		ret = perf_session_env__lookup_objdump(&top->session->header.env);
+		if (ret)
+			goto out_delete;
+	}
+
 	ret = perf_top__setup_sample_type(top);
 	if (ret)
 		goto out_delete;
@@ -1115,6 +1122,8 @@ int cmd_top(int argc, const char **argv, const char *prefix __maybe_unused)
 		    "Interleave source code with assembly code (default)"),
 	OPT_BOOLEAN(0, "asm-raw", &symbol_conf.annotate_asm_raw,
 		    "Display raw encoding of assembly instructions (default)"),
+	OPT_STRING(0, "objdump", &objdump_path, "path",
+		    "objdump binary to use for disassembly and annotations"),
 	OPT_STRING('M', "disassembler-style", &disassembler_style, "disassembler style",
 		   "Specify disassembler style (e.g. -M intel for intel syntax)"),
 	OPT_STRING('u', "uid", &target->uid_str, "user", "user to profile"),
