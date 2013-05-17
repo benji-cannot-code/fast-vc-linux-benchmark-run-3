5dc32e682e2e775042e3d0168814ad1e86e4c0d8
Amarjargal Gundjalam <amarjargal16@gmail.com>
2013-05-17 01:06:36 -0700

    keucr: fixes trailing whitespace error
    This patch fixes the following checkpatch error,
    ERROR: trailing whitespace
    
    Signed-off-by: Amarjargal Gundjalam <amarjargal16@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/keucr/usb.h
+++ drivers/staging/keucr/usb.h
@@ -198,7 +198,7 @@ struct us_data {
 	BYTE        MMC_SPEC_VER;
 	BYTE        MMC_BusWidth;
 	BYTE        MMC_HIGH_CAPACITY;
-	
+
 	/* ----- MS Control Data ---------------- */
 	BOOLEAN             MS_SWWP;
 	DWORD               MSP_TotalBlock;
@@ -214,7 +214,7 @@ struct us_data {
 	BYTE		BIN_FLAG;
 	DWORD		bl_num;
 	int		SrbStatus;
-	
+
 	/* ------Power Managerment --------------- */
 	BOOLEAN         Power_IsResum;	
 };
