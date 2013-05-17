3c60df35881a053ffd704d8281d972ebf595b82b
Johannes Berg <johannes.berg@intel.com>
2013-05-17 09:58:27 +0200

    iwlwifi: dvm: rename iwl_lib_ops to iwl_dvm_cfg
    The next patches will move some more configuration
    data that isn't needed by mvm into this struct, so
    rename it now since it won't just be ops.
    
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/dvm/agn.h
+++ drivers/net/wireless/iwlwifi/dvm/agn.h
@@ -77,13 +77,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IWL_INVALID_STATION	255
 
 /* device operations */
-extern struct iwl_lib_ops iwl1000_lib;
-extern struct iwl_lib_ops iwl2000_lib;
-extern struct iwl_lib_ops iwl2030_lib;
-extern struct iwl_lib_ops iwl5000_lib;
-extern struct iwl_lib_ops iwl5150_lib;
-extern struct iwl_lib_ops iwl6000_lib;
-extern struct iwl_lib_ops iwl6030_lib;
+extern struct iwl_dvm_cfg iwl_dvm_1000_cfg;
+extern struct iwl_dvm_cfg iwl_dvm_2000_cfg;
+extern struct iwl_dvm_cfg iwl_dvm_2030_cfg;
+extern struct iwl_dvm_cfg iwl_dvm_5000_cfg;
+extern struct iwl_dvm_cfg iwl_dvm_5150_cfg;
+extern struct iwl_dvm_cfg iwl_dvm_6000_cfg;
+extern struct iwl_dvm_cfg iwl_dvm_6030_cfg;
 
 
 #define TIME_UNIT		1024
