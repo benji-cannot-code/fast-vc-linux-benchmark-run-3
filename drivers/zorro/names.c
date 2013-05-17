bdf78ffa2a608f9dc30a563bbd188ff447f90194
Geert Uytterhoeven <geert@linux-m68k.org>
2013-05-17 14:01:24 +0200

    zorro: Refactor conditional handling of Zorro device name database
    Using an empty static inline function in the CONFIG_ZORRO_NAMES=n case
    allows to drop compilation of names.c.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- drivers/zorro/names.c
+++ drivers/zorro/names.c
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/zorro.h>
 
 
-#ifdef CONFIG_ZORRO_NAMES
-
 struct zorro_prod_info {
 	__u16 prod;
 	unsigned short seen;
@@ -98,11 +96,3 @@ void __init zorro_name_device(struct zorro_dev *dev)
 		}
 	}
 }
-
-#else
-
-void __init zorro_name_device(struct zorro_dev *dev)
-{
-}
-
-#endif
