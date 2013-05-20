2cbaec215d60a93c72e77ccae772bb53b0b50496
Viresh Kumar <viresh.kumar@linaro.org>
2013-05-20 09:57:17 +0530

    cpufreq: arm_big_little_dt: Instantiate as platform_driver
    As multiplatform build is being adopted by more and more ARM platforms, initcall
    function should be used very carefully. For example, when both arm_big_little_dt
    and cpufreq-cpu0 drivers are compiled in, arm_big_little_dt driver may try to
    register even if we had platform device for cpufreq-cpu0 registered.
    
    To eliminate this undesired the effect, the patch changes arm_big_little_dt
    driver to have it instantiated as a platform_driver. Then it will only run on
    platforms that create the platform_device "arm-bL-cpufreq-dt".
    
    Reported-and-tested-by: Rob Herring <robherring2@gmail.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/arm_big_little_dt.c
+++ drivers/cpufreq/arm_big_little_dt.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/opp.h>
+#include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/types.h>
 #include "arm_big_little.h"
@@ -96,7 +97,7 @@ static struct cpufreq_arm_bL_ops dt_bL_ops = {
 	.init_opp_table = dt_init_opp_table,
 };
 
-static int generic_bL_init(void)
+static int generic_bL_probe(struct platform_device *pdev)
 {
 	struct device_node *np;
 
@@ -107,13 +108,22 @@ static int generic_bL_init(void)
 	of_node_put(np);
 	return bL_cpufreq_register(&dt_bL_ops);
 }
-module_init(generic_bL_init);
 
-static void generic_bL_exit(void)
+static int generic_bL_remove(struct platform_device *pdev)
 {
-	return bL_cpufreq_unregister(&dt_bL_ops);
+	bL_cpufreq_unregister(&dt_bL_ops);
+	return 0;
 }
-module_exit(generic_bL_exit);
+
+static struct platform_driver generic_bL_platdrv = {
+	.driver = {
+		.name	= "arm-bL-cpufreq-dt",
+		.owner	= THIS_MODULE,
+	},
+	.probe		= generic_bL_probe,
+	.remove		= generic_bL_remove,
+};
+module_platform_driver(generic_bL_platdrv);
 
 MODULE_AUTHOR("Viresh Kumar <viresh.kumar@linaro.org>");
 MODULE_DESCRIPTION("Generic ARM big LITTLE cpufreq driver via DT");
