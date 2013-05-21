272d89cc7e05e2370b6310bb4048fcf76b88d526
Fabio Baltieri <fabio.baltieri@linaro.org>
2013-05-21 12:04:08 +0200

    ASoC: ab8500-codec: Set tx dai slots from tx_mask
    Replace hard-coded tx slot numbers from ab8500_codec_set_dai_tdm_slot
    using the ones requested by the machine driver in tx_mask instead.
    
    Signed-off-by: Fabio Baltieri <fabio.baltieri@linaro.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/ab8500-codec.h
+++ sound/soc/codecs/ab8500-codec.h
@@ -25,6 +25,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AB8500_SUPPORTED_RATE			(SNDRV_PCM_RATE_48000)
 #define AB8500_SUPPORTED_FMT			(SNDRV_PCM_FMTBIT_S16_LE)
 
+/* AB8500 interface slot offset definitions */
+
+#define AB8500_AD_DATA0_OFFSET	0
+#define AB8500_DA_DATA0_OFFSET	8
+#define AB8500_AD_DATA1_OFFSET	16
+#define AB8500_DA_DATA1_OFFSET	24
+
 /* AB8500 audio bank (0x0d) register definitions */
 
 #define AB8500_POWERUP				0x00
