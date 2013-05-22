ff31a54402560e4d1919d87ffd6b6912ee49ba27
Dave Martin <dave.martin@linaro.org>
2013-05-22 19:08:16 +0100

    ARM: bL_switcher: Add switch completion callback for bL_switch_request()
    There is no explicit way to know when a switch started via
    bL_switch_request() is complete.  This can lead to unpredictable
    behaviour when the switcher is controlled by a subsystem which
    makes dynamic decisions (such as cpufreq).
    
    The CPU PM notifier is not really suitable for signalling
    completion, because the CPU could get suspended and resumed for
    other, independent reasons while a switch request is in flight.
    Adding a whole new notifier for this seems excessive, and may tempt
    people to put heavyweight code on this path.
    
    This patch implements a new bL_switch_request_cb() function that
    allows for a per-request lightweight callback, private between the
    switcher and the caller of bL_switch_request_cb().
    
    Overlapping switches on a single CPU are considered incorrect if
    they are requested via bL_switch_request_cb() with a callback (they
    will lead to an unpredictable final state without explicit external
    synchronisation to force the requests into a particular order).
    Queuing requests robustly would be overkill because only one
    subsystem should be attempting to control the switcher at any time.
    
    Overlapping requests of this kind will be failed with -EBUSY to
    indicate that the second request won't take effect and the
    completer will never be called for it.
    
    bL_switch_request() is retained as a wrapper round the new function,
    with the old, fire-and-forget semantics.  In this case the last request
    will always win. The request may still be denied if a previous request
    with a completer is still pending.
    
    Signed-off-by: Dave Martin <dave.martin@linaro.org>
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>

--- arch/arm/include/asm/bL_switcher.h
+++ arch/arm/include/asm/bL_switcher.h
@@ -16,7 +16,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/types.h>
 
-int bL_switch_request(unsigned int cpu, unsigned int new_cluster_id);
+typedef void (*bL_switch_completion_handler)(void *cookie);
+
+int bL_switch_request_cb(unsigned int cpu, unsigned int new_cluster_id,
+			 bL_switch_completion_handler completer,
+			 void *completer_cookie);
+static inline int bL_switch_request(unsigned int cpu, unsigned int new_cluster_id)
+{
+	return bL_switch_request_cb(cpu, new_cluster_id, NULL, NULL);
+}
 
 /*
  * Register here to be notified about runtime enabling/disabling of
