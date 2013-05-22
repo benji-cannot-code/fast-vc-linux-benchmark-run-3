4a4497db2bb79ebe1a1793e530c22cdb05e98a85
Seung-Woo Kim <sw0312.kim@samsung.com>
2013-05-22 21:14:15 +0900

    drm/exynos: fix build warnings from ipp fimc
    Becuase of order of headers, there are build warnings and they are
    fixed with this patch.
    
    Signed-off-by: Seung-Woo Kim <sw0312.kim@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_fimc.c
+++ drivers/gpu/drm/exynos/exynos_drm_fimc.c
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 #include <linux/kernel.h>
-#include <linux/mfd/syscon.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/mfd/syscon.h>
 #include <linux/regmap.h>
 #include <linux/clk.h>
 #include <linux/pm_runtime.h>
