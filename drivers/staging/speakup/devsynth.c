4751fc1f0761188766cf61c4789a4c2f0fc232c5
Emil Goode <emilgoode@gmail.com>
2013-05-22 20:53:09 +0200

    staging: speakup: fix warnings by adding __user annotations
    This patch fixes the following sparse warnings by adding
    __user annotations.
    
    drivers/staging/speakup/speakup_soft.c:248:34: warning:
            incorrect type in argument 1 (different address spaces)
            drivers/staging/speakup/speakup_soft.c:248:34:
            expected void [noderef] <asn:1>*dst
            drivers/staging/speakup/speakup_soft.c:248:34:
            got char *[assigned] cp
    drivers/staging/speakup/speakup_soft.c:272:40: warning:
            incorrect type in argument 1 (different address spaces)
            drivers/staging/speakup/speakup_soft.c:272:40:
            expected char const [noderef] <asn:1>*s
            drivers/staging/speakup/speakup_soft.c:272:40:
            got char const *buf
    drivers/staging/speakup/speakup_soft.c:306:17: warning:
            incorrect type in initializer (incompatible argument 2
            (different address spaces))
            drivers/staging/speakup/speakup_soft.c:306:17:
            expected long ( *read )( ... )
            drivers/staging/speakup/speakup_soft.c:306:17:
            got long ( static [toplevel] *<noident> )( ... )
    drivers/staging/speakup/speakup_soft.c:307:18: warning:
            incorrect type in initializer (incompatible argument 2
            (different address spaces))
            drivers/staging/speakup/speakup_soft.c:307:18:
            expected long ( *write )( ... )
            drivers/staging/speakup/speakup_soft.c:307:18:
            got long ( static [toplevel] *<nfile_operationsoident> )( ... )
    
    drivers/staging/speakup/devsynth.c:29:41: warning:
            incorrect type in argument 2 (different address spaces)
            drivers/staging/speakup/devsynth.c:29:41:
            expected void const [noderef] <asn:1>*from
            drivers/staging/speakup/devsynth.c:29:41:    got char const *ptr
    drivers/staging/speakup/devsynth.c:62:17: warning:
            incorrect type in initializer (incompatible argument 2
            (different address spaces))
            drivers/staging/speakup/devsynth.c:62:17:
            expected long ( *read )( ... )
            drivers/staging/speakup/devsynth.c:62:17:
            got long ( static [toplevel] *<noident> )( ... )
    drivers/staging/speakup/devsynth.c:63:18: warning:
            incorrect type in initializer (incompatible argument 2
            (different address spaces))
            drivers/staging/speakup/devsynth.c:63:18:
            expected long ( *write )( ... )
            drivers/staging/speakup/devsynth.c:63:18:
            got long ( static [toplevel] *<noident> )( ... )
    
    Signed-off-by: Emil Goode <emilgoode@gmail.com>
    Acked-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/speakup/devsynth.c
+++ drivers/staging/speakup/devsynth.c
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static int misc_registered;
 static int dev_opened;
 
-static ssize_t speakup_file_write(struct file *fp, const char *buffer,
-		   size_t nbytes, loff_t *ppos)
+static ssize_t speakup_file_write(struct file *fp, const char __user *buffer,
+				  size_t nbytes, loff_t *ppos)
 {
 	size_t count = nbytes;
-	const char *ptr = buffer;
+	const char __user *ptr = buffer;
 	size_t bytes;
 	unsigned long flags;
 	u_char buf[256];
@@ -38,8 +38,8 @@ static ssize_t speakup_file_write(struct file *fp, const char *buffer,
 	return (ssize_t) nbytes;
 }
 
-static ssize_t speakup_file_read(struct file *fp, char *buf, size_t nbytes,
-	loff_t *ppos)
+static ssize_t speakup_file_read(struct file *fp, char __user *buf,
+				 size_t nbytes, loff_t *ppos)
 {
 	return 0;
 }
