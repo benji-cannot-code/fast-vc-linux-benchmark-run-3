f9fff4d9f1633f37b4a2e4be74be52fd6b519653
Jani Nikula <jani.nikula@intel.com>
2013-05-22 15:36:18 +0300

    drm/i915: drop redundant warnings on not holding dpio_lock
    The lower level sideband read/write functions already do this.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_hdmi.c
+++ drivers/gpu/drm/i915/intel_hdmi.c
@@ -1019,8 +1019,6 @@ static void intel_hdmi_pre_enable(struct intel_encoder *encoder)
 	if (!IS_VALLEYVIEW(dev))
 		return;
 
-	WARN_ON(!mutex_is_locked(&dev_priv->dpio_lock));
-
 	/* Enable clock channels for this port */
 	val = intel_dpio_read(dev_priv, DPIO_DATA_LANE_A(port));
 	val = 0;
@@ -1063,8 +1061,6 @@ static void intel_hdmi_pre_pll_enable(struct intel_encoder *encoder)
 
 	if (!IS_VALLEYVIEW(dev))
 		return;
-
-	WARN_ON(!mutex_is_locked(&dev_priv->dpio_lock));
 
 	/* Program Tx lane resets to default */
 	intel_dpio_write(dev_priv, DPIO_PCS_TX(port),
