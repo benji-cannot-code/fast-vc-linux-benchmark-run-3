4ea110a17d3fe567325cc6849c4aaa2080d917ca
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2013-05-23 10:32:03 +0200

    usb: gadget: f_ecm: convert to new function interface with backward compatibility
    Converting ecm to the new function interface requires converting
    the USB ecm's function code and its users.
    
    This patch converts the f_ecm.c to the new function interface.
    
    The file is now compiled into a separate usb_f_ecm.ko module.
    
    The old function interface is provided by means of a preprocessor
    conditional directives. After all users are converted, the old interface
    can be removed.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/g_ffs.c
+++ drivers/usb/gadget/g_ffs.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #    define USB_ETH_RNDIS y
 #  endif
 
+#define USBF_ECM_INCLUDED
 #  include "f_ecm.c"
 #  include "f_subset.c"
 #  ifdef USB_ETH_RNDIS
