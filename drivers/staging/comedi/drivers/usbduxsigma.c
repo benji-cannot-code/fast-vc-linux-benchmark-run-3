a514f4c52f7cbeec9124d853087d11de77d029f1
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-05-23 12:47:19 -0700

    staging: comedi: usbduxsigma: remove 'probed' checks in comedi functions
    The 'probed' flag in the private data is used by the usb_driver (*probe)
    function to find a free entry in the static usbduxsub array to use for
    the comedi_device private data. The (*probe) sets this flag before it
    hands off the probe to the comedi_driver (*auto_attach). The 'probed'
    flag is cleared in tidy_up() are part of the usb_driver (*disconnect),
    which calls the comedi_driver (*detach).
    
    The 'probed' flag will always be set if the comedi_driver (*auto_attach)
    completes successfully. The extra sanity checks in the comedi functions
    are not needed. Remove them.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/usbduxsigma.c
+++ drivers/staging/comedi/drivers/usbduxsigma.c
@@ -660,9 +660,6 @@ static int usbdux_ai_cmdtest(struct comedi_device *dev,
 	int err = 0, i;
 	unsigned int tmpTimer;
 
-	if (!(this_usbduxsub->probed))
-		return -ENODEV;
-
 	/* Step 1 : check if triggers are trivially valid */
 
 	err |= cfc_check_trigger_src(&cmd->start_src, TRIG_NOW | TRIG_INT);
@@ -824,10 +821,6 @@ static int usbdux_ai_inttrig(struct comedi_device *dev,
 		return -EFAULT;
 
 	down(&this_usbduxsub->sem);
-	if (!(this_usbduxsub->probed)) {
-		up(&this_usbduxsub->sem);
-		return -ENODEV;
-	}
 	if (trignum != 0) {
 		dev_err(&this_usbduxsub->interface->dev,
 			"comedi%d: usbdux_ai_inttrig: invalid trignum\n",
@@ -870,11 +863,6 @@ static int usbdux_ai_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 
 	/* block other CPUs from starting an ai_cmd */
 	down(&this_usbduxsub->sem);
-
-	if (!(this_usbduxsub->probed)) {
-		up(&this_usbduxsub->sem);
-		return -ENODEV;
-	}
 	if (this_usbduxsub->ai_cmd_running) {
 		dev_err(&this_usbduxsub->interface->dev, "comedi%d: "
 			"ai_cmd not possible. Another ai_cmd is running.\n",
@@ -992,10 +980,6 @@ static int usbdux_ai_insn_read(struct comedi_device *dev,
 		return 0;
 
 	down(&this_usbduxsub->sem);
-	if (!(this_usbduxsub->probed)) {
-		up(&this_usbduxsub->sem);
-		return -ENODEV;
-	}
 	if (this_usbduxsub->ai_cmd_running) {
 		dev_err(&this_usbduxsub->interface->dev,
 			"comedi%d: ai_insn_read not possible. "
@@ -1135,10 +1119,6 @@ static int usbdux_ao_insn_read(struct comedi_device *dev,
 		return -EFAULT;
 
 	down(&this_usbduxsub->sem);
-	if (!(this_usbduxsub->probed)) {
-		up(&this_usbduxsub->sem);
-		return -ENODEV;
-	}
 	for (i = 0; i < insn->n; i++)
 		data[i] = this_usbduxsub->outBuffer[chan];
 
@@ -1158,10 +1138,6 @@ static int usbdux_ao_insn_write(struct comedi_device *dev,
 		return -EFAULT;
 
 	down(&this_usbduxsub->sem);
-	if (!(this_usbduxsub->probed)) {
-		up(&this_usbduxsub->sem);
-		return -ENODEV;
-	}
 	if (this_usbduxsub->ao_cmd_running) {
 		dev_err(&this_usbduxsub->interface->dev,
 			"comedi%d: ao_insn_write: "
@@ -1202,11 +1178,6 @@ static int usbdux_ao_inttrig(struct comedi_device *dev,
 		return -EFAULT;
 
 	down(&this_usbduxsub->sem);
-
-	if (!(this_usbduxsub->probed)) {
-		ret = -ENODEV;
-		goto out;
-	}
 	if (trignum != 0) {
 		dev_err(&this_usbduxsub->interface->dev,
 			"comedi%d: usbdux_ao_inttrig: invalid trignum\n",
@@ -1243,9 +1214,6 @@ static int usbdux_ao_cmdtest(struct comedi_device *dev,
 
 	if (!this_usbduxsub)
 		return -EFAULT;
-
-	if (!(this_usbduxsub->probed))
-		return -ENODEV;
 
 	/* Step 1 : check if triggers are trivially valid */
 
@@ -1321,11 +1289,6 @@ static int usbdux_ao_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 		return -EFAULT;
 
 	down(&this_usbduxsub->sem);
-	if (!(this_usbduxsub->probed)) {
-		up(&this_usbduxsub->sem);
-		return -ENODEV;
-	}
-
 	/* set current channel of the running acquisition to zero */
 	s->async->cur_chan = 0;
 	for (i = 0; i < cmd->chanlist_len; ++i) {
@@ -1460,11 +1423,6 @@ static int usbdux_dio_insn_bits(struct comedi_device *dev,
 		return -EFAULT;
 
 	down(&this_usbduxsub->sem);
-
-	if (!(this_usbduxsub->probed)) {
-		up(&this_usbduxsub->sem);
-		return -ENODEV;
-	}
 
 	/* The insn data is a mask in data[0] and the new data
 	 * in data[1], each channel cooresponding to a bit. */
@@ -1992,7 +1950,7 @@ static int usbduxsigma_auto_attach(struct comedi_device *dev,
 	dev->private = NULL;
 
 	down(&start_stop_sem);
-	if (!uds || !uds->probed) {
+	if (!uds) {
 		dev_err(dev->class_dev,
 			"usbduxsigma: error: auto_attach failed, not connected\n");
 		ret = -ENODEV;
