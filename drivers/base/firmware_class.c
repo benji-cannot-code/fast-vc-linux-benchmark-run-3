0fc9eca1876c48e44c729b62d4adf7f51b981c20
Daniel Mack <zonque@gmail.com>
2013-05-23 22:17:18 +0200

    firmware: move EXPORT_SYMBOL annotations
    Move EXPORT_SYMBOL annotations so they follow immediately after the
    closing function brace line.
    
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Acked-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -1108,6 +1108,7 @@ request_firmware(const struct firmware **firmware_p, const char *name,
 {
 	return _request_firmware(firmware_p, name, device, true, false);
 }
+EXPORT_SYMBOL(request_firmware);
 
 /**
  * release_firmware: - release the resource associated with a firmware image
@@ -1121,6 +1122,7 @@ void release_firmware(const struct firmware *fw)
 		kfree(fw);
 	}
 }
+EXPORT_SYMBOL(release_firmware);
 
 /* Async support */
 struct firmware_work {
@@ -1201,6 +1203,7 @@ request_firmware_nowait(
 	schedule_work(&fw_work->work);
 	return 0;
 }
+EXPORT_SYMBOL(request_firmware_nowait);
 
 /**
  * cache_firmware - cache one firmware image in kernel memory space
@@ -1231,6 +1234,7 @@ int cache_firmware(const char *fw_name)
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(cache_firmware);
 
 /**
  * uncache_firmware - remove one cached firmware image
@@ -1261,6 +1265,7 @@ int uncache_firmware(const char *fw_name)
 
 	return -EINVAL;
 }
+EXPORT_SYMBOL_GPL(uncache_firmware);
 
 #ifdef CONFIG_PM_SLEEP
 static ASYNC_DOMAIN_EXCLUSIVE(fw_cache_domain);
@@ -1567,9 +1572,3 @@ static void __exit firmware_class_exit(void)
 
 fs_initcall(firmware_class_init);
 module_exit(firmware_class_exit);
-
-EXPORT_SYMBOL(release_firmware);
-EXPORT_SYMBOL(request_firmware);
-EXPORT_SYMBOL(request_firmware_nowait);
-EXPORT_SYMBOL_GPL(cache_firmware);
-EXPORT_SYMBOL_GPL(uncache_firmware);
