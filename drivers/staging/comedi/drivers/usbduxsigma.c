898f9ed72f81643d5cb8fbe3b69324c9033a2bdf
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-05-23 13:06:55 -0700

    staging: comedi: usbduxsigma: rename the (*cancel) functions
    Rename these functions so they have namespace associated with the
    driver.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/usbduxsigma.c
+++ drivers/staging/comedi/drivers/usbduxsigma.c
@@ -228,8 +228,8 @@ static void usbdux_ai_stop(struct usbduxsigma_private *devpriv, int do_unlink)
 	devpriv->ai_cmd_running = 0;
 }
 
-static int usbdux_ai_cancel(struct comedi_device *dev,
-			    struct comedi_subdevice *s)
+static int usbduxsigma_ai_cancel(struct comedi_device *dev,
+				 struct comedi_subdevice *s)
 {
 	struct usbduxsigma_private *devpriv = dev->private;
 
@@ -369,8 +369,8 @@ static void usbdux_ao_stop(struct usbduxsigma_private *devpriv, int do_unlink)
 	devpriv->ao_cmd_running = 0;
 }
 
-static int usbdux_ao_cancel(struct comedi_device *dev,
-			    struct comedi_subdevice *s)
+static int usbduxsigma_ao_cancel(struct comedi_device *dev,
+				 struct comedi_subdevice *s)
 {
 	struct usbduxsigma_private *devpriv = dev->private;
 
@@ -1434,7 +1434,7 @@ static int usbduxsigma_attach_common(struct comedi_device *dev)
 	s->insn_read	= usbduxsigma_ai_insn_read;
 	s->do_cmdtest	= usbduxsigma_ai_cmdtest;
 	s->do_cmd	= usbduxsigma_ai_cmd;
-	s->cancel	= usbdux_ai_cancel;
+	s->cancel	= usbduxsigma_ai_cancel;
 
 	/* Analog Output subdevice */
 	s = &dev->subdevices[1];
@@ -1449,7 +1449,7 @@ static int usbduxsigma_attach_common(struct comedi_device *dev)
 	s->insn_read	= usbduxsigma_ao_insn_read;
 	s->do_cmdtest	= usbduxsigma_ao_cmdtest;
 	s->do_cmd	= usbduxsigma_ao_cmd;
-	s->cancel	= usbdux_ao_cancel;
+	s->cancel	= usbduxsigma_ao_cancel;
 
 	/* Digital I/O subdevice */
 	s = &dev->subdevices[2];
