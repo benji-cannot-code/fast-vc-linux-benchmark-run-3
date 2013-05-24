0e611112db1e8296d75c71fcb682f6a58b8280ab
Tomi Valkeinen <tomi.valkeinen@ti.com>
2013-05-24 14:21:56 +0300

    OMAPDSS: Add new simple DPI panel driver
    Add simple DPI Panel driver which uses the new DSS device model and DSS
    ops. A "simple" panel means one that does not require any special setup.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- include/video/omap-panel-data.h
+++ include/video/omap-panel-data.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __OMAP_PANEL_DATA_H
 #define __OMAP_PANEL_DATA_H
 
+#include <video/display_timing.h>
+
 struct omap_dss_device;
 
 /**
@@ -212,6 +214,27 @@ struct connector_atv_platform_data {
 
 	enum omap_dss_venc_type connector_type;
 	bool invert_polarity;
+};
+
+/**
+ * panel_dpi platform data
+ * @name: name for this display entity
+ * @source: name of the display entity used as a video source
+ * @data_lines: number of DPI datalines
+ * @display_timing: timings for this panel
+ * @backlight_gpio: gpio to enable/disable the backlight (or -1)
+ * @enable_gpio: gpio to enable/disable the panel (or -1)
+ */
+struct panel_dpi_platform_data {
+	const char *name;
+	const char *source;
+
+	int data_lines;
+
+	const struct display_timing *display_timing;
+
+	int backlight_gpio;
+	int enable_gpio;
 };
 
 #endif /* __OMAP_PANEL_DATA_H */
