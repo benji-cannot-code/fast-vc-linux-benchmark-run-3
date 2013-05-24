2ff0b423ecdef47c5e82e83292dea1f8271a51e1
Fabio Baltieri <fabio.baltieri@linaro.org>
2013-05-24 12:39:19 +0200

    ASoC: ux500: Add missing mop500_ab8500.h include
    Add a missing include that was resulting in some sparse warning for
    non-static structure without forward declaration.
    
    Signed-off-by: Fabio Baltieri <fabio.baltieri@linaro.org>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/ux500/mop500_ab8500.c
+++ sound/soc/ux500/mop500_ab8500.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ux500_pcm.h"
 #include "ux500_msp_dai.h"
+#include "mop500_ab8500.h"
 #include "../codecs/ab8500-codec.h"
 
 #define TX_SLOT_MONO	0x0008
