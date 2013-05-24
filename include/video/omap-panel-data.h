eca22f2c74478ea48282806d19758fe1b1692f3a
Tomi Valkeinen <tomi.valkeinen@ti.com>
2013-05-24 14:22:20 +0300

    OMAPDSS: Add new DSI Command Mode panel  driver
    Add DSI Command Mode panel driver which uses the new DSS device model
    and DSS ops. This driver only supports a very basic set of features
    which should be common to all DSI command mode panels.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- include/video/omap-panel-data.h
+++ include/video/omap-panel-data.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __OMAP_PANEL_DATA_H
 #define __OMAP_PANEL_DATA_H
 
+#include <video/omapdss.h>
 #include <video/display_timing.h>
 
 struct omap_dss_device;
@@ -235,6 +236,33 @@ struct panel_dpi_platform_data {
 
 	int backlight_gpio;
 	int enable_gpio;
+};
+
+/**
+ * panel_dsicm platform data
+ * @name: name for this display entity
+ * @source: name of the display entity used as a video source
+ * @reset_gpio: gpio to reset the panel (or -1)
+ * @use_ext_te: use external TE GPIO
+ * @ext_te_gpio: external TE GPIO
+ * @ulps_timeout: time to wait before entering ULPS, 0 = disabled (ms)
+ * @use_dsi_backlight: true if panel uses DSI command to control backlight
+ * @pin_config: DSI pin configuration
+ */
+struct panel_dsicm_platform_data {
+	const char *name;
+	const char *source;
+
+	int reset_gpio;
+
+	bool use_ext_te;
+	int ext_te_gpio;
+
+	unsigned ulps_timeout;
+
+	bool use_dsi_backlight;
+
+	struct omap_dsi_pin_config pin_config;
 };
 
 #endif /* __OMAP_PANEL_DATA_H */
