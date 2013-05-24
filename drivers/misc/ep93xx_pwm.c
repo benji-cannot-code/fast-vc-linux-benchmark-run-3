e9214c9644e9f03b2c092d792233966ed6dbdd8e
H Hartley Sweeten <hartleys@visionengravers.com>
2013-05-24 16:24:51 -0700

    misc/ep93xx_pwm: remove ep93xx_pwm_disable() inline function
    This is a simple wrapper around writel(), remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Ryan Mallon <rmallon@gmail.com>
    Cc: Matthieu Crapet <mcrapet@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/ep93xx_pwm.c
+++ drivers/misc/ep93xx_pwm.c
@@ -45,11 +45,6 @@ static inline u16 ep93xx_pwm_read_tc(struct ep93xx_pwm *pwm)
 	return readl(pwm->mmio_base + EP93XX_PWMx_TERM_COUNT);
 }
 
-static inline void ep93xx_pwm_disable(struct ep93xx_pwm *pwm)
-{
-	writel(0x0, pwm->mmio_base + EP93XX_PWMx_ENABLE);
-}
-
 static inline int ep93xx_pwm_is_enabled(struct ep93xx_pwm *pwm)
 {
 	return readl(pwm->mmio_base + EP93XX_PWMx_ENABLE) & 0x1;
@@ -128,7 +123,7 @@ static ssize_t ep93xx_pwm_set_freq(struct device *dev,
 		return -EINVAL;
 
 	if (val == 0) {
-		ep93xx_pwm_disable(pwm);
+		writel(0x0, pwm->mmio_base + EP93XX_PWMx_ENABLE);
 	} else if (val <= (clk_get_rate(pwm->clk) / 2)) {
 		u32 term, duty;
 
@@ -277,7 +272,7 @@ static int __init ep93xx_pwm_probe(struct platform_device *pdev)
 	pwm->duty_percent = 50;
 
 	/* disable pwm at startup. Avoids zero value. */
-	ep93xx_pwm_disable(pwm);
+	writel(0x0, pwm->mmio_base + EP93XX_PWMx_ENABLE);
 	writel(EP93XX_PWM_MAX_COUNT, pwm->mmio_base + EP93XX_PWMx_TERM_COUNT);
 	writel(EP93XX_PWM_MAX_COUNT/2, pwm->mmio_base + EP93XX_PWMx_DUTY_CYCLE);
 
@@ -291,7 +286,7 @@ static int __exit ep93xx_pwm_remove(struct platform_device *pdev)
 {
 	struct ep93xx_pwm *pwm = platform_get_drvdata(pdev);
 
-	ep93xx_pwm_disable(pwm);
+	writel(0x0, pwm->mmio_base + EP93XX_PWMx_ENABLE);
 	clk_disable(pwm->clk);
 	sysfs_remove_group(&pdev->dev.kobj, &ep93xx_pwm_sysfs_files);
 	ep93xx_pwm_release_gpio(pdev);
