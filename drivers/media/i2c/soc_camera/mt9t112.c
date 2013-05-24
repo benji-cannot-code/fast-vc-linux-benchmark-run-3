34204918076eec9b3864c445ccfc578c13254300
Sachin Kamat <sachin.kamat@linaro.org>
2013-05-24 08:25:06 -0300

    [media] soc_camera: mt9t112: Remove empty function
    After the switch to devm_* functions, the 'remove' function does
    not do anything. Delete it.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: Kuninori Morimoto <morimoto.kuninori@renesas.com>
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/i2c/soc_camera/mt9t112.c
+++ drivers/media/i2c/soc_camera/mt9t112.c
@@ -1103,11 +1103,6 @@ static int mt9t112_probe(struct i2c_client *client,
 	return ret;
 }
 
-static int mt9t112_remove(struct i2c_client *client)
-{
-	return 0;
-}
-
 static const struct i2c_device_id mt9t112_id[] = {
 	{ "mt9t112", 0 },
 	{ }
@@ -1119,7 +1114,6 @@ static struct i2c_driver mt9t112_i2c_driver = {
 		.name = "mt9t112",
 	},
 	.probe    = mt9t112_probe,
-	.remove   = mt9t112_remove,
 	.id_table = mt9t112_id,
 };
 
