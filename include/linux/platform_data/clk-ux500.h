2a47b6b5eb1e394db052c399d593cb0dec04fb79
Philippe Begnic <philippe.begnic@st.com>
2013-05-27 14:41:29 +0200

    clk: ux500: Pass clock base adresses in initcall for u8540 and u9540
    Align on u8500 version, pass clock base address in clk_init functions
    for u8540 and u9540.
    
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Philippe Begnic <philippe.begnic@st.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- include/linux/platform_data/clk-ux500.h
+++ include/linux/platform_data/clk-ux500.h
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void u8500_clk_init(u32 clkrst1_base, u32 clkrst2_base, u32 clkrst3_base,
 		    u32 clkrst5_base, u32 clkrst6_base);
-void u9540_clk_init(void);
-void u8540_clk_init(void);
+void u9540_clk_init(u32 clkrst1_base, u32 clkrst2_base, u32 clkrst3_base,
+		    u32 clkrst5_base, u32 clkrst6_base);
+void u8540_clk_init(u32 clkrst1_base, u32 clkrst2_base, u32 clkrst3_base,
+		    u32 clkrst5_base, u32 clkrst6_base);
 
 #endif /* __CLK_UX500_H */
