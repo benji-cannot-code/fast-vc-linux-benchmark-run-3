d831db4cd40626664f1e33fb44339cf2d31be09d
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2013-05-27 11:56:26 +0200

    drm/exynos: fix tests for valid FIMD window number
    Valid values for FIMD windows are from 0 to WINDOWS_NR-1
    inclusive (5 windows in total). The WINDOWS_NR is also
    a size of fimd_context.win_data array.
    However, early-return tests for wrong values of windows
    accepted a value of WINDOWS_NR which is out of bound
    for fimd_context.win_data.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_fimd.c
+++ drivers/gpu/drm/exynos/exynos_drm_fimd.c
@@ -382,7 +382,7 @@ static void fimd_win_mode_set(struct device *dev,
 	if (win == DEFAULT_ZPOS)
 		win = ctx->default_win;
 
-	if (win < 0 || win > WINDOWS_NR)
+	if (win < 0 || win >= WINDOWS_NR)
 		return;
 
 	offset = overlay->fb_x * (overlay->bpp >> 3);
@@ -507,7 +507,7 @@ static void fimd_win_commit(struct device *dev, int zpos)
 	if (win == DEFAULT_ZPOS)
 		win = ctx->default_win;
 
-	if (win < 0 || win > WINDOWS_NR)
+	if (win < 0 || win >= WINDOWS_NR)
 		return;
 
 	win_data = &ctx->win_data[win];
@@ -623,7 +623,7 @@ static void fimd_win_disable(struct device *dev, int zpos)
 	if (win == DEFAULT_ZPOS)
 		win = ctx->default_win;
 
-	if (win < 0 || win > WINDOWS_NR)
+	if (win < 0 || win >= WINDOWS_NR)
 		return;
 
 	win_data = &ctx->win_data[win];
