f908b1762e9b2ce5287995e0654727ab8d642d49
Andrzej Hajda <a.hajda@samsung.com>
2013-05-28 03:26:15 -0300

    [media] s5p-mfc: v4l2 controls setup routine moved to initialization code
    Callback .start_streaming is called once for every queue,
    so v4l2_ctrl_handler_setup was called twice during stream start.
    Moving v4l2_ctrl_handler_setup to context initialization
    reduces numbers of calls and seems to be more consistent with API.
    
    Signed-off-by: Andrzej Hajda <a.hajda@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Kamil Debski <k.debski@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
@@ -1790,7 +1790,6 @@ static int s5p_mfc_start_streaming(struct vb2_queue *q, unsigned int count)
 		}
 	}
 
-	v4l2_ctrl_handler_setup(&ctx->ctrl_handler);
 	/* If context is ready then dev = work->data;schedule it to run */
 	if (s5p_mfc_ctx_ready(ctx))
 		set_work_bit_irqsave(ctx);
@@ -1951,6 +1950,7 @@ int s5p_mfc_enc_ctrls_setup(struct s5p_mfc_ctx *ctx)
 		if (controls[i].is_volatile && ctx->ctrls[i])
 			ctx->ctrls[i]->flags |= V4L2_CTRL_FLAG_VOLATILE;
 	}
+	v4l2_ctrl_handler_setup(&ctx->ctrl_handler);
 	return 0;
 }
 
