26bcc03efb3db1036f8da5b9369544687832f412
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2013-05-28 09:15:57 +0200

    usb: gadget: f_rndis: convert to new function interface with backward compatibility
    Converting rndis to the new function interface requires converting
    the USB rndis' function code and its users.
    
    This patch converts the f_rndis.c to the new function interface.
    
    The file is now compiled into a separate usb_f_rndis.ko module.
    
    The old function interface is provided by means of a preprocessor
    conditional directives. After all users are converted, the old interface
    can be removed.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/g_ffs.c
+++ drivers/usb/gadget/g_ffs.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_FSUBSET_INCLUDED
 #  include "f_subset.c"
 #  ifdef USB_ETH_RNDIS
+#    define USB_FRNDIS_INCLUDED
 #    include "f_rndis.c"
 #    include "rndis.h"
 #  endif
