179b6d81269dcfbcf8a403c9fe15dd3b9559ccff
Aaro Koskinen <aaro.koskinen@iki.fi>
2013-05-29 23:59:07 +0300

    staging: xgifb: SetFlag: delete DisableChB
    This flag is never set, so checks can be removed and code behind it
    deleted.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/vb_def.h
+++ drivers/staging/xgifb/vb_def.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SetCRT2ToDualEdge   0x8000
 
 #define ReserveTVOption     0x0008
-#define DisableChB          0x1000
 #define EnableChB           0x2000
 #define DisableChA          0x4000
 #define EnableChA           0x8000
