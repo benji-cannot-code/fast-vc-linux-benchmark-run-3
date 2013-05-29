95a2cffb1a043356f3228bf012d6d8da6d86c032
Hans Verkuil <hans.verkuil@cisco.com>
2013-05-29 06:59:43 -0300

    [media] cx231xx: remove g_chip_ident
    Remove g_chip_ident and replace it with g_chip_info.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/cx231xx/cx231xx-avcore.c
+++ drivers/media/usb/cx231xx/cx231xx-avcore.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/v4l2-common.h>
 #include <media/v4l2-ioctl.h>
-#include <media/v4l2-chip-ident.h>
 
 #include "cx231xx.h"
 #include "cx231xx-dif.h"
--- drivers/media/usb/cx231xx/cx231xx-vbi.c
+++ drivers/media/usb/cx231xx/cx231xx-vbi.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/v4l2-common.h>
 #include <media/v4l2-ioctl.h>
-#include <media/v4l2-chip-ident.h>
 #include <media/msp3400.h>
 #include <media/tuner.h>
 
