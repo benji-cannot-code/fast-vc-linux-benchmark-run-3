67c8e6733f4cf7890d3652ce382cfc09afd800c6
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-05-30 19:43:13 +0800

    cw1200: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/cw1200/cw1200.h
+++ drivers/net/wireless/cw1200/cw1200.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CW1200_H
 
 #include <linux/wait.h>
-#include <linux/version.h>
 #include <linux/mutex.h>
 #include <linux/workqueue.h>
 #include <net/mac80211.h>
