a6d0dfe3e58b2d04f19a8096a2fa7671285128e7
Arnd Bergmann <arnd@arndb.de>
2013-05-31 19:22:50 -0300

    [media] omap3isp: include linux/mm_types.h
    The ispqueue.h file uses vm_flags_t, which is defined in
    linux/mm_types.h, so we must include that header in order
    to build in all configurations.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/omap3isp/ispqueue.h
+++ drivers/media/platform/omap3isp/ispqueue.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/list.h>
+#include <linux/mm_types.h>
 #include <linux/mutex.h>
 #include <linux/videodev2.h>
 #include <linux/wait.h>
