4b57a1cdbbcfe713174ea44c54cabd836acc30c2
Sylwester Nawrocki <s.nawrocki@samsung.com>
2013-05-31 13:47:03 -0300

    [media] exynos4-is: Move __fimc_videoc_querycap() function to the common module
    Move __fimc_videoc_querycap() function to the common exynos4-is-common.ko
    module so it don't need to be reimplemented in multiple video node drivers
    of the exynos4-is.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/exynos4-is/common.h
+++ drivers/media/platform/exynos4-is/common.h
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
+#include <linux/device.h>
+#include <linux/videodev2.h>
 #include <media/media-entity.h>
 #include <media/v4l2-subdev.h>
 
 struct v4l2_subdev *fimc_find_remote_sensor(struct media_entity *entity);
+void __fimc_vidioc_querycap(struct device *dev, struct v4l2_capability *cap,
+			    unsigned int caps);
