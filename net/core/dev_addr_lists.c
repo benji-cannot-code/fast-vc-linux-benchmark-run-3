e738ff008b435f144ed30d02f4ccfe68fa02e78c
Jay Vosburgh <fubar@us.ibm.com>
2013-05-31 11:57:28 +0000

    net/core: __hw_addr_sync_one / _multiple broken
    Currently, __hw_addr_sync_one is called in a loop by
    __hw_addr_sync_multiple to sync each of a "from" device's hw addresses
    to a "to" device.  __hw_addr_sync_one calls __hw_addr_add_ex to attempt
    to add each address.  __hw_addr_add_ex is called with global=false, and
    sync=true.
    
    	__hw_addr_add_ex checks to see if the new address matches an
    address already on the list.  If so, it tests global and sync.  In this
    case, sync=true, and it then checks if the address is already synced,
    and if so, returns 0.
    
    	This 0 return causes __hw_addr_sync_one to increment the sync_cnt
    and refcount for the "from" list's address entry, even though the address
    is already synced and has a reference and sync_cnt.  This will cause
    the sync_cnt and refcount to increment without bound every time an
    addresses is added to the "from" device and synced to the "to" device.
    
    	The fix here has two parts:
    
    	First, when __hw_addr_add_ex finds the address already exists
    and is synced, return -EEXIST instead of 0.
    
    	Second, __hw_addr_sync_one checks the error return for -EEXIST,
    and if so, it (a) does not add a refcount/sync_cnt, and (b) returns 0
    itself so that __hw_addr_sync_multiple will not return an error.
    
    Signed-off-by: Jay Vosburgh <fubar@us.ibm.com>
    Reviewed-by: Vlad Yasevich <vyasevic@redhat.com>
    Tested-by: Shawn Bohrer <sbohrer@rgmadvisors.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev_addr_lists.c
+++ net/core/dev_addr_lists.c
@@ -68,7 +68,7 @@ static int __hw_addr_add_ex(struct netdev_hw_addr_list *list,
 			}
 			if (sync) {
 				if (ha->synced)
-					return 0;
+					return -EEXIST;
 				else
 					ha->synced = true;
 			}
@@ -141,10 +141,13 @@ static int __hw_addr_sync_one(struct netdev_hw_addr_list *to_list,
 
 	err = __hw_addr_add_ex(to_list, ha->addr, addr_len, ha->type,
 			       false, true);
-	if (err)
+	if (err && err != -EEXIST)
 		return err;
-	ha->sync_cnt++;
-	ha->refcount++;
+
+	if (!err) {
+		ha->sync_cnt++;
+		ha->refcount++;
+	}
 
 	return 0;
 }
