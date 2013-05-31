f76e8ab80969b5a510a53ee797c21e574b395ad3
Paulo Zanoni <paulo.r.zanoni@intel.com>
2013-05-31 16:33:22 -0300

    drm/i915: implement IPS feature
    Intermediate Pixel Storage is a feature that should reduce the number
    of times the display engine wakes up memory to read pixels, so it
    should allow deeper PC states. IPS can only be enabled on ULT pipe A
    with 8:8:8 pipe pixel formats.
    
    With eDP 1920x1080 and correct watermarks but without FBC this moves
    my PC7 residency from 2.5% to around 38%.
    
    v2: - It's tied to pipe A, not port A
        - Add pipe_config support (Chris)
        - Add some assertions (Chris)
        - Rebase against latest dinq
    v3: - Don't ever set ips_enabled to false (Daniel)
        - Only check for ips_enabled at hsw_disable_ips (Daniel)
    v4: - Add hsw_compute_ips_config (Daniel)
        - Use the new dump_pipe_config (Daniel)
    
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -1024,6 +1024,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Framebuffer compression for Ivybridge */
 #define IVB_FBC_RT_BASE			0x7020
 
+#define IPS_CTL		0x43408
+#define   IPS_ENABLE	(1 << 31)
 
 #define _HSW_PIPE_SLICE_CHICKEN_1_A	0x420B0
 #define _HSW_PIPE_SLICE_CHICKEN_1_B	0x420B4
@@ -3666,6 +3668,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LGC_PALETTE_A           0x4a000
 #define _LGC_PALETTE_B           0x4a800
 #define LGC_PALETTE(pipe) _PIPE(pipe, _LGC_PALETTE_A, _LGC_PALETTE_B)
+
+#define _GAMMA_MODE_A		0x4a480
+#define _GAMMA_MODE_B		0x4ac80
+#define GAMMA_MODE(pipe) _PIPE(pipe, _GAMMA_MODE_A, _GAMMA_MODE_B)
+#define GAMMA_MODE_MODE_MASK	(3 << 0)
+#define GAMMA_MODE_MODE_8bit	(0 << 0)
+#define GAMMA_MODE_MODE_10bit	(1 << 0)
+#define GAMMA_MODE_MODE_12bit	(2 << 0)
+#define GAMMA_MODE_MODE_SPLIT	(3 << 0)
 
 /* interrupts */
 #define DE_MASTER_IRQ_CONTROL   (1 << 31)
