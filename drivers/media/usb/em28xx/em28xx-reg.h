4ced107fe770cea7a141365d86d5f1fbfa7b237e
Frank Schaefer <fschaefer.oss@googlemail.com>
2013-06-03 14:12:04 -0300

    [media] em28xx: move snapshot button bit definition for reg 0x0C from em28xx-input.c to em28xx.h
    Signed-off-by: Frank Schäfer <fschaefer.oss@googlemail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/usb/em28xx/em28xx-reg.h
+++ drivers/media/usb/em28xx/em28xx-reg.h
@@ -69,7 +69,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #define EM28XX_R0A_CHIPID	0x0a
-#define EM28XX_R0C_USBSUSP	0x0c	/* */
+#define EM28XX_R0C_USBSUSP	0x0c
+#define   EM28XX_R0C_USBSUSP_SNAPSHOT	0x20 /* 1=button pressed, needs reset */
 
 #define EM28XX_R0E_AUDIOSRC	0x0e
 #define EM28XX_R0F_XCLK	0x0f
