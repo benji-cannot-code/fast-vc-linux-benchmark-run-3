5729c786a76c1b9a435cfe4b1808567de1b8fe15
Peng Tao <bergwolf@gmail.com>
2013-06-03 21:58:17 +0800

    staging/lustre: clean up and remove libcfs/linux/linux-fs.c
    filp_user_write() is open coded in libcfs_kkuc_msg_put(). All
    other functions/macros have no user at all. So we can remove
    the file.
    
    Signed-off-by: Peng Tao <tao.peng@emc.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/linux-fs.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/linux-fs.h
@@ -79,9 +79,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define flock_end(fl)			((fl)->fl_end)
 #define flock_set_end(fl, end)		do { (fl)->fl_end = (end); } while (0)
 
-ssize_t filp_user_write(struct file *filp, const void *buf, size_t count,
-			loff_t *offset);
-
 #ifndef IFSHIFT
 #define IFSHIFT			12
 #endif
