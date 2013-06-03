a5596a0bc27cefa40dc5b659d7ecb5006c17b3af
Arnd Bergmann <arnd@arndb.de>
2013-06-03 23:09:14 +0100

    ARM: 7745/1: psci: fix building without HOTPLUG_CPU
    The cpu_die field in smp_operations is not valid with CONFIG_HOTPLUG_CPU,
    so we must enclose it in #ifdef, but at least that lets us remove
    two other lines.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/psci_smp.c
+++ arch/arm/kernel/psci_smp.c
@@ -69,8 +69,6 @@ void __ref psci_cpu_die(unsigned int cpu)
        /* We should never return */
        panic("psci: cpu %d failed to shutdown\n", cpu);
 }
-#else
-#define psci_cpu_die NULL
 #endif
 
 bool __init psci_smp_available(void)
@@ -81,5 +79,7 @@ bool __init psci_smp_available(void)
 
 struct smp_operations __initdata psci_smp_ops = {
 	.smp_boot_secondary	= psci_boot_secondary,
+#ifdef CONFIG_HOTPLUG_CPU
 	.cpu_die		= psci_cpu_die,
+#endif
 };
