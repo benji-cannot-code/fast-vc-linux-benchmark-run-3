ed2d2dfd447917fb1d1ad8402d96339b3f33fc22
Michal Simek <michal.simek@xilinx.com>
2013-06-03 12:13:19 +0200

    video: xilinxfb: Use drvdata->regs_phys instead of physaddr
    physaddr will be remove in the next patch.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/xilinxfb.c
+++ drivers/video/xilinxfb.c
@@ -326,7 +326,7 @@ static int xilinxfb_assign(struct device *dev,
 
 	if (drvdata->flags & BUS_ACCESS_FLAG) {
 		/* Put a banner in the log (for DEBUG) */
-		dev_dbg(dev, "regs: phys=%lx, virt=%p\n", physaddr,
+		dev_dbg(dev, "regs: phys=%x, virt=%p\n", drvdata->regs_phys,
 					drvdata->regs);
 	}
 	/* Put a banner in the log (for DEBUG) */
@@ -354,7 +354,7 @@ err_fbmem:
 
 err_map:
 	if (drvdata->flags & BUS_ACCESS_FLAG)
-		release_mem_region(physaddr, 8);
+		release_mem_region(drvdata->regs_phys, 8);
 
 err_region:
 	kfree(drvdata);
