6cac4adb4d7896b66e0719571feb269a00a1e942
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-06-03 18:02:34 -0700

    staging: comedi: pcmuio: remove 'CALC_N_SUBDEVS' macro
    This macro is only used once in the driver, just remove it.
    
    This also fixes a > 80 char line checkpatch.pl warning.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/pcmuio.c
+++ drivers/staging/comedi/drivers/pcmuio.c
@@ -91,7 +91,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define INTR_PORTS_PER_SUBDEV (INTR_CHANS_PER_ASIC/CHANS_PER_PORT)
 #define MAX_DIO_CHANS   (PORTS_PER_ASIC*2*CHANS_PER_PORT)
 #define MAX_ASICS       (MAX_DIO_CHANS/CHANS_PER_ASIC)
-#define CALC_N_SUBDEVS(nchans) ((nchans)/MAX_CHANS_PER_SUBDEV + (!!((nchans)%MAX_CHANS_PER_SUBDEV)) /*+ (nchans > INTR_CHANS_PER_ASIC ? 2 : 1)*/)
 /* IO Memory sizes */
 #define ASIC_IOSIZE (0x10)
 #define PCMUIO48_IOSIZE ASIC_IOSIZE
@@ -810,7 +809,8 @@ static int pcmuio_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	}
 
 	chans_left = CHANS_PER_ASIC * board->num_asics;
-	n_subdevs = CALC_N_SUBDEVS(chans_left);
+	n_subdevs = (chans_left / MAX_CHANS_PER_SUBDEV) +
+		    (!!(chans_left % MAX_CHANS_PER_SUBDEV));
 	devpriv->sprivs = kcalloc(n_subdevs,
 				  sizeof(struct pcmuio_subdev_private),
 				  GFP_KERNEL);
