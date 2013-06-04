adc6a5bc7f2d21832e52a947fd957fcd95cc1668
Joseph Lo <josephl@nvidia.com>
2013-06-04 18:47:32 +0800

    ARM: tegra: cpuidle: move the init function behind the suspend init function
    One of the state of CPUidle on Tegra can power gate the CPU and the
    vdd_cpu rail. But it depends on some configurations from DT and a common
    hook function for different Tegra SoCs to power gate the CPU rail. And
    these stuffs are initialized after common Tegra suspend init function. So
    we move the CPUidle init behind the suspend init function. And making the
    CPUidle driver more generic.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/cpuidle.c
+++ arch/arm/mach-tegra/cpuidle.c
@@ -28,25 +28,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "fuse.h"
 #include "cpuidle.h"
 
-static int __init tegra_cpuidle_init(void)
+void __init tegra_cpuidle_init(void)
 {
-	int ret;
-
 	switch (tegra_chip_id) {
 	case TEGRA20:
-		ret = tegra20_cpuidle_init();
+		tegra20_cpuidle_init();
 		break;
 	case TEGRA30:
-		ret = tegra30_cpuidle_init();
+		tegra30_cpuidle_init();
 		break;
 	case TEGRA114:
-		ret = tegra114_cpuidle_init();
+		tegra114_cpuidle_init();
 		break;
 	default:
-		ret = -ENODEV;
 		break;
 	}
-
-	return ret;
 }
-device_initcall(tegra_cpuidle_init);
