62d0ef9bc1f547849a1f35f69a84d720935124a6
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-06-05 15:39:16 -0700

    staging: comedi: pcmad: remove unnecessary includes
    This driver does not use interrupts and the comedi core handles the
    allocation of the io resource so these includes are not required.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/pcmad.c
+++ drivers/staging/comedi/drivers/pcmad.c
@@ -39,10 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	   1 = two's complement (+-10V input range)
  */
 
-#include <linux/interrupt.h>
 #include "../comedidev.h"
-
-#include <linux/ioport.h>
 
 #define PCMAD_STATUS		0
 #define PCMAD_LSB		1
