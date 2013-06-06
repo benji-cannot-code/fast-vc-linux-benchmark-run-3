27933a5c4d0b2fe403ba3b6b90006dfa49112f50
Joachim Eastwood <manabian@gmail.com>
2013-06-06 10:24:17 +0200

    pcmcia: at91_cf: clean up header includes
    Use includes from linux/ instead of asm/ and remove a
    unnecessary mach/ include.
    
    Signed-off-by: Joachim Eastwood <manabian@gmail.com>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Acked-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pcmcia/at91_cf.c
+++ drivers/pcmcia/at91_cf.c
@@ -19,12 +19,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/gpio.h>
 #include <linux/platform_data/atmel.h>
+#include <linux/io.h>
+#include <linux/sizes.h>
 
 #include <pcmcia/ss.h>
-
-#include <mach/hardware.h>
-#include <asm/io.h>
-#include <asm/sizes.h>
 
 #include <mach/at91rm9200_mc.h>
 #include <mach/at91_ramc.h>
