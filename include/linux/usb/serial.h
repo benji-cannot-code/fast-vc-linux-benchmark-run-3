80757edf519b799a262b86faa1bf2028ff98b3ee
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-06-06 10:31:35 -0700

    USB: serial: increase the number of devices we support
    We had the limit of 255 USB to serial devices on one system for almost
    15 years, with no complaints.  But now it's time to move on from these
    tiny "baby" systems, and bump the number up to 512, which should last
    us a few more years:
    	"512 is a nice number" -- Tobias Winter
    
    Note, this is still a static value, and uses up tty core memory with
    this many tty devices allocated.  Converting the driver to use
    TTY_DRIVER_DYNAMIC_DEV is the next thing to do in order to remove this
    limitation.
    
    Reported-by: Tobias Winter <tobias@linuxdingsda.de>
    Tested-by: Tobias Winter <tobias@linuxdingsda.de>
    Reviewed-by: Johan Hovold <jhovold@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/usb/serial.h
+++ include/linux/usb/serial.h
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysrq.h>
 #include <linux/kfifo.h>
 
-#define SERIAL_TTY_MAJOR	188	/* Nice legal number now */
-#define SERIAL_TTY_MINORS	254	/* loads of devices :) */
-
 /* The maximum number of ports one device can grab at once */
 #define MAX_NUM_PORTS		8
 
