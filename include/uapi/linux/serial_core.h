dc374357f83c8969b608e79d8a21fd881d26fe0d
Jingchang Lu <b35083@freescale.com>
2013-06-07 09:20:40 +0800

    tty: serial: add Freescale lpuart driver support
    Add Freescale lpuart driver support. The lpuart device
    can be found on Vybrid VF610 and Layerscape LS-1 SoCs.
    
    Signed-off-by: Jingchang Lu <b35083@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -227,4 +227,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Rocketport EXPRESS/INFINITY */
 #define PORT_RP2	102
 
+/* Freescale lpuart */
+#define PORT_LPUART	103
+
 #endif /* _UAPILINUX_SERIAL_CORE_H */
