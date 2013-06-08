b510e462781c6a908b39889e4e704ad1268bf29c
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2013-06-09 00:34:36 +0400

    phy-rcar-usb: add R8A7778 support
    The driver currently only supports R8A7779 SoC. Compared to it, R8A7778 USB-PHY
    has extra register range containing two high-speed signal quality characteristic
    control registers which should be set up  during USB-PHY  startup depending on
    whether a ferrite bead is in use or not.  So, we now handle an optional second
    memory range in the driver's probe method, add the 'ferrite_bead' field to the
    driver's platform data, and add an extra (optional) step to the USB-PHY startup
    routine which sets up the extended registers.
    
    Also mark in the driver's Kconfig section  that R8A7778 is now supported and
    generally clarify that section, uppercasing the word "phy" and also changing
    the module name that got lost in the big driver rename, while at it...
    
    The patch has been tested on the Marzen and BOCK-W boards.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- include/linux/platform_data/usb-rcar-phy.h
+++ include/linux/platform_data/usb-rcar-phy.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 struct rcar_phy_platform_data {
+	bool ferrite_bead:1;	/* (R8A7778 only)			*/
+
 	bool port1_func:1;	/* true: port 1 used by function, false: host */
 	unsigned penc1:1;	/* Output of the PENC1 pin in function mode */
 	struct {		/* Overcurrent pin control for ports 0..2 */
@@ -21,7 +23,7 @@ struct rcar_phy_platform_data {
 				/* Set to false on port 1 in function mode */
 		bool active_high:1; /* true: active  high, false: active low */
 				/* Set to true  on port 1 in function mode */
-	} ovc_pin[3];
+	} ovc_pin[3];		/* (R8A7778 only has 2 ports)		*/
 };
 
 #endif /* __USB_RCAR_PHY_H */
