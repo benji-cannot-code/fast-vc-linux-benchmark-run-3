aa05e56e909e3fa385a207e519b9a53c6d213f45
Ben Hutchings <bhutchings@solarflare.com>
2013-06-10 18:03:17 +0100

    sfc: Store port number in private data, not net_device::dev_id
    We should not use net_device::dev_id to indicate the port number, as
    this affects the way the local part of IPv6 addresses is normally
    generated.
    
    This field was intended for use where multiple devices may share a
    single assigned MAC address and need to have different IPv6 addresses.
    Siena's two ports each have their own MAC addresses.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/net_driver.h
+++ drivers/net/ethernet/sfc/net_driver.h
@@ -785,6 +785,7 @@ struct efx_nic {
 
 	char name[IFNAMSIZ];
 	struct pci_dev *pci_dev;
+	unsigned int port_num;
 	const struct efx_nic_type *type;
 	int legacy_irq;
 	bool legacy_irq_enabled;
@@ -917,7 +918,7 @@ static inline int efx_dev_registered(struct efx_nic *efx)
 
 static inline unsigned int efx_port_num(struct efx_nic *efx)
 {
-	return efx->net_dev->dev_id;
+	return efx->port_num;
 }
 
 /**
