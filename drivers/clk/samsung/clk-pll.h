5072e3ca6779af58960e198d0d8491448d73b6ea
Yadwinder Singh Brar <yadi.brar@samsung.com>
2013-06-11 15:01:07 +0530

    clk: samsung: Define a common samsung_clk_register_pll()
    This patch defines a common samsung_clk_register_pll()
    Since pll2550 & pll35xx and pll2650 & pll36xx have exactly same clk ops
    implementation, added pll2550 and pll2650 also.
    
    Signed-off-by: Yadwinder Singh Brar <yadi.brar@samsung.com>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- drivers/clk/samsung/clk-pll.h
+++ drivers/clk/samsung/clk-pll.h
@@ -13,6 +13,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SAMSUNG_CLK_PLL_H
 #define __SAMSUNG_CLK_PLL_H
 
+enum samsung_pll_type {
+	pll_35xx,
+	pll_36xx,
+	pll_2550,
+	pll_2650,
+};
+
 enum pll45xx_type {
 	pll_4500,
 	pll_4502,
