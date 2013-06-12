ddc48331eaee46437dafd0062c7b6bd571587627
Dinh Nguyen <dinguyen@altera.com>
2013-06-12 10:18:51 -0500

    mmc: dw_mmc: Add support DW SD/MMC driver on SOCFPGA
    Add platform specific functionality for the DW SD/MMC driver for
    SoCFPGA. Move SDMMC_CMD_USE_HOLD_REG to dw_mmc.h so other platforms
    can use this define.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    Reviewed-by: Pavel Machek <pavel@denx.de>
    Acked-by: Jaehoon Chung <jh80.chung@samsung.com>
    Acked-by: Olof Johansson <olof@lixom.net>
    Acked-by: Seungwon Jeon <tgih.jun@samsung.com
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/dw_mmc.h
+++ drivers/mmc/host/dw_mmc.h
@@ -112,6 +112,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SDMMC_INT_ERROR			0xbfc2
 /* Command register defines */
 #define SDMMC_CMD_START			BIT(31)
+#define SDMMC_CMD_USE_HOLD_REG	BIT(29)
 #define SDMMC_CMD_CCS_EXP		BIT(23)
 #define SDMMC_CMD_CEATA_RD		BIT(22)
 #define SDMMC_CMD_UPD_CLK		BIT(21)
