fd8658158c57731975cc1fcbd41b4aa35e05fd89
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-06-12 16:15:46 -0700

    staging: comedi: addi_apci_3xxx: remove unused define
    This define is not used by the driver. Remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi-data/hwdrv_apci3xxx.c
+++ drivers/staging/comedi/drivers/addi-data/hwdrv_apci3xxx.c
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define APCI3XXX_DIFF				1
 #define APCI3XXX_CONFIGURATION			0
 
-#define APCI3XXX_TTL_INIT_DIRECTION_PORT2	0
-
 /*
 +----------------------------------------------------------------------------+
 |                         ANALOG INPUT FUNCTIONS                             |
