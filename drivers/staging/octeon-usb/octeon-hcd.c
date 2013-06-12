9adbf65662be911bd9de84fa90daa22bed965969
Aaro Koskinen <aaro.koskinen@iki.fi>
2013-06-13 01:00:35 +0300

    staging: octeon-usb: octeon-hcd: fix some includes
    Avoid including from "asm/".
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/octeon-usb/octeon-hcd.c
+++ drivers/staging/octeon-usb/octeon-hcd.c
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/usb.h>
 
-#include <asm/time.h>
-#include <asm/delay.h>
+#include <linux/time.h>
+#include <linux/delay.h>
 
 #include <asm/octeon/cvmx.h>
 #include "cvmx-usb.h"
