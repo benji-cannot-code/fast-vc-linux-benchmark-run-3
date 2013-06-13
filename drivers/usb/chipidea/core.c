2ddc29aed8ce004e4a5aec66c2829e1c59951abf
Michael Grzeschik <mgr@pengutronix.de>
2013-06-13 17:59:47 +0300

    usb: chipidea: udc: configure iso endpoints
    This patch adds iso endpoint support to the device controller.
    It makes use of the multiplication bits in the maxpacket field
    of the endpoint and calculates the multiplier bits for each
    transfer description on every request.
    
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Reviewed-by: Peter Chen <peter.chen@freescale.com>
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/chipidea/core.c
+++ drivers/usb/chipidea/core.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * TODO List
  * - OTG
- * - Isochronous & Interrupt Traffic
+ * - Interrupt Traffic
  * - Handle requests which spawns into several TDs
  * - GET_STATUS(device) - always reports 0
  * - Gadget API (majority of optional features)
