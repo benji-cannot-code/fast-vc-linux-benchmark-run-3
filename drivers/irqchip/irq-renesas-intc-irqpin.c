e31b4d0a2d16bf7c574e402b205122b87dd2195a
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2013-06-13 11:23:38 +0200

    irqchip: renesas-intc-irqpin: DT binding for sense bitfield width
    Most Renesas irqpin controllers have 4-bit sense fields, however, some
    have different widths. This patch adds a DT binding to optionally
    specify such non-standard values.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski+renesas@gmail.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/irqchip/irq-renesas-intc-irqpin.c
+++ drivers/irqchip/irq-renesas-intc-irqpin.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/init.h>
+#include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
@@ -350,6 +351,9 @@ static int intc_irqpin_probe(struct platform_device *pdev)
 	/* deal with driver instance configuration */
 	if (pdata)
 		memcpy(&p->config, pdata, sizeof(*pdata));
+	else
+		of_property_read_u32(pdev->dev.of_node, "sense-bitfield-width",
+				     &p->config.sense_bitfield_width);
 	if (!p->config.sense_bitfield_width)
 		p->config.sense_bitfield_width = 4; /* default to 4 bits */
 
