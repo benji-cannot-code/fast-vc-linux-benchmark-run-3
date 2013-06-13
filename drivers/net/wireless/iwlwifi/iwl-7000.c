515965b1bb81a7de488d54c39039a7fde61f32f3
Johannes Berg <johannes.berg@intel.com>
2013-06-13 16:06:08 +0200

    iwlwifi: bump required firmware API version for 3160/7260
    As the firmware API has changed significantly and we don't
    have support code for the old APIs, bump the version to be
    able to release the version 7 API firmware. Unfortunately
    this means that the driver in 3.9 and 3.10 can't work, but
    that's still better than crashing the device/driver there.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-7000.c
+++ drivers/net/wireless/iwlwifi/iwl-7000.c
@@ -68,16 +68,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-agn-hw.h"
 
 /* Highest firmware API version supported */
-#define IWL7260_UCODE_API_MAX	6
-#define IWL3160_UCODE_API_MAX	6
+#define IWL7260_UCODE_API_MAX	7
+#define IWL3160_UCODE_API_MAX	7
 
 /* Oldest version we won't warn about */
-#define IWL7260_UCODE_API_OK	6
-#define IWL3160_UCODE_API_OK	6
+#define IWL7260_UCODE_API_OK	7
+#define IWL3160_UCODE_API_OK	7
 
 /* Lowest firmware API version supported */
-#define IWL7260_UCODE_API_MIN	6
-#define IWL3160_UCODE_API_MIN	6
+#define IWL7260_UCODE_API_MIN	7
+#define IWL3160_UCODE_API_MIN	7
 
 /* NVM versions */
 #define IWL7260_NVM_VERSION		0x0a1d
