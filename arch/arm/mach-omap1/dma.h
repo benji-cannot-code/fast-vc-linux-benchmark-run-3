dd436875936fedcb474a61490ea0da697bf6fbc0
Jarkko Nikula <jarkko.nikula@bitmer.com>
2013-06-15 11:31:08 +0300

    ARM: OMAP1: Remove legacy irda.h and irda setup from board files
    omap-ir.c driver has never been upstream and was also removed from
    linux-omap.git four years ago (See linux-omap.git commit efd1e3f
    ("REMOVE OMAP LEGACY CODE: Reset drivers/net/irda to mainline")).
    
    Therefore remove needless device registration from a few board files
    and delete thus to be unused arch/arm/mach-omap1/include/mach/irda.h
    and unused OMAP_DMA_UART3_* definitions from dma.h.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/dma.h
+++ arch/arm/mach-omap1/dma.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __OMAP1_DMA_CHANNEL_H
 
 /* DMA channels for omap1 */
-#define OMAP_DMA_UART3_TX		18
-#define OMAP_DMA_UART3_RX		19
 
 /* These are only for 1610 */
 
