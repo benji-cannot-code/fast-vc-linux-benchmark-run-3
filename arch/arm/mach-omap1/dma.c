6c3d850776b5042d276b0d7554d83d5a00d94642
Jarkko Nikula <jarkko.nikula@bitmer.com>
2013-06-15 11:31:09 +0300

    ARM: OMAP1: Remove dma.h
    Add definitions in arch/arm/mach-omap1/dma.h are now removed so remove
    the file and include statements from dma.c and lcd_dma.c.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/dma.c
+++ arch/arm/mach-omap1/dma.c
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/irqs.h>
 
-#include "dma.h"
-
 #define OMAP1_DMA_BASE			(0xfffed800)
 #define OMAP1_LOGICAL_DMA_CH_COUNT	17
 #define OMAP1_DMA_STRIDE		0x40
