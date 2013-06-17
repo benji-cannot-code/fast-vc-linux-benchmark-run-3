15cef582720faefbd5bad190a002c9eecac33bb1
Tomasz Figa <tomasz.figa@gmail.com>
2013-06-17 23:45:33 +0900

    ARM: SAMSUNG: Add watchdog reset driver
    This patch adds a watchdog reset driver that can be used on Samsung SoCs
    that do not provide dedicated reset method. It replaces the legacy
    helper function that relies on static IO mapping.
    
    Signed-off-by: Tomasz Figa <tomasz.figa@gmail.com>
    Tested-by: Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-samsung/include/plat/watchdog-reset.h
+++ arch/arm/plat-samsung/include/plat/watchdog-reset.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
 */
 
+#ifndef __PLAT_SAMSUNG_WATCHDOG_RESET_H
+#define __PLAT_SAMSUNG_WATCHDOG_RESET_H
+
 #include <plat/clock.h>
 #include <plat/regs-watchdog.h>
 #include <mach/map.h>
@@ -45,3 +48,9 @@ static inline void arch_wdt_reset(void)
 	/* delay to allow the serial port to show the message */
 	mdelay(50);
 }
+
+extern void samsung_wdt_reset(void);
+extern void samsung_wdt_reset_of_init(void);
+extern void samsung_wdt_reset_init(void __iomem *base);
+
+#endif /* __PLAT_SAMSUNG_WATCHDOG_RESET_H */
