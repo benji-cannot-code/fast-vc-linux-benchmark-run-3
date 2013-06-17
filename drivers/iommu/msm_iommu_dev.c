0a7c433b2fd4389518cc91e25e982631977ca3c1
Stephen Boyd <sboyd@codeaurora.org>
2013-06-17 10:43:13 -0700

    msm: iommu: Use clk_set_rate() instead of clk_set_min_rate()
    Calling clk_set_min_rate() is no better than just calling
    clk_set_rate() because MSM clock code already takes care of
    calling the min_rate ops if the clock really needs
    clk_set_min_rate() called on it.
    
    Cc: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Acked-by: Joerg Roedel <joro@8bytes.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- drivers/iommu/msm_iommu_dev.c
+++ drivers/iommu/msm_iommu_dev.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/iommu_hw-8xxx.h>
 #include <mach/iommu.h>
-#include <mach/clk.h>
 
 struct iommu_ctx_iter_data {
 	/* input */
@@ -169,7 +168,7 @@ static int msm_iommu_probe(struct platform_device *pdev)
 
 	if (!IS_ERR(iommu_clk))	{
 		if (clk_get_rate(iommu_clk) == 0)
-			clk_set_min_rate(iommu_clk, 1);
+			clk_set_rate(iommu_clk, 1);
 
 		ret = clk_prepare_enable(iommu_clk);
 		if (ret) {
