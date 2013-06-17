e1097330f60c3e4c82a303361c3c076c1914170e
Markos Chandras <markos.chandras@imgtec.com>
2013-06-17 13:00:37 +0000

    MIPS: Sibyte: Add missing sched.h header
    It's needed for the TASK_INTERRUPTIBLE definition.
    
    Fixes the following build problem:
    arch/mips/sibyte/common/sb_tbprof.c:235:4: error: 'TASK_INTERRUPTIBLE'
    undeclared (first use in this function)
    
    [ralf@linux-mips.org: Ideally sched.h should be included into the actual
    user of TASK_INTERRUPTIBLE, <linux/wait.h> but that seems way too risky
    that close to a release.]
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Acked-by: Steven J. Hill <Steven.Hill@imgtec.com>
    Cc: sibyte-users@bitmover.com
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/5479/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/sibyte/common/sb_tbprof.c
+++ arch/mips/sibyte/common/sb_tbprof.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
+#include <linux/sched.h>
 #include <linux/vmalloc.h>
 #include <linux/fs.h>
 #include <linux/errno.h>
