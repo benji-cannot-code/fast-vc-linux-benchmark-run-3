c46381c0ea4325ca9d0bfd1e1f12704532569412
Vineet Gupta <vgupta@synopsys.com>
2013-06-17 18:27:23 +0530

    ARC: Setup Vector Table Base in early boot
    Otherwise early boot exceptions such as instructions errors due to
    configuration mismatch between kernel and hardware go off to la-la land,
    as opposed to hitting the handler and panic()'ing properly.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/irq.c
+++ arch/arc/kernel/irq.c
@@ -33,8 +33,6 @@ void __cpuinit arc_init_IRQ(void)
 {
 	int level_mask = 0;
 
-	write_aux_reg(AUX_INTR_VEC_BASE, _int_vec_base_lds);
-
 	/* Disable all IRQs: enable them as devices request */
 	write_aux_reg(AUX_IENABLE, 0);
 
