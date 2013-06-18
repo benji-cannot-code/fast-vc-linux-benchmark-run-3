af1bb02b37e3868e03550f034219c47a41c0a569
John Stultz <john.stultz@linaro.org>
2013-06-17 19:34:57 -0700

    Revert "dw_apb_timer_of.c: Remove parts that were picoxcell-specific"
    This reverts commit 55a68c23e0a675b2b8ac2656fd6edbf98b78e4c6.
    
    In order to avoid a collision with dw_apb_timer changes in
    the arm-soc tree, revert this change.
    
    I'm leaving it to the arm-soc folks to sort out if they want
    to keep the other side of the collision or if they're just going
    to back it all out and try again during the next release cycle.
    
    Reported-by: Dinh Nguyen <dinguyen@altera.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- include/linux/dw_apb_timer.h
+++ include/linux/dw_apb_timer.h
@@ -18,12 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clocksource.h>
 #include <linux/interrupt.h>
 
-#define APBTMR_N_LOAD_COUNT		0x00
-#define APBTMR_N_CURRENT_VALUE		0x04
-#define APBTMR_N_CONTROL		0x08
-#define APBTMR_N_EOI			0x0c
-#define APBTMR_N_INT_STATUS		0x10
-
 #define APBTMRS_REG_SIZE       0x14
 
 struct dw_apb_timer {
