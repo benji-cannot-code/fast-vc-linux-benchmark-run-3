76c0ddc027837026f6d4eb1920d11b4e326e153a
Heiko Stuebner <heiko@sntech.de>
2013-06-19 02:41:14 +0900

    pinctrl: s3c24xx: use correct header for chained_irq functions
    chained_irq_enter and chained_irq_exit moved to a real header
    under linux/pinctrl instead of asm/.
    
    Update the pinctrl driver to use the correct header and fix the
    build error.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>`
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- drivers/pinctrl/pinctrl-s3c24xx.c
+++ drivers/pinctrl/pinctrl-s3c24xx.c
@@ -20,11 +20,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irqdomain.h>
 #include <linux/irq.h>
 #include <linux/of_irq.h>
+#include <linux/irqchip/chained_irq.h>
 #include <linux/io.h>
 #include <linux/slab.h>
 #include <linux/err.h>
-
-#include <asm/mach/irq.h>
 
 #include "pinctrl-samsung.h"
 
