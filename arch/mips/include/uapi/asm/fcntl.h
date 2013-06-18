dec4f8f69b9e6b1f762bcc1bc8ad8cad6ff7b047
Ralf Baechle <ralf@linux-mips.org>
2013-06-18 13:39:12 +0200

    MIPS: fcntl.h: Use __kernel_off_t, not off_t.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/uapi/asm/fcntl.h
+++ arch/mips/include/uapi/asm/fcntl.h
@@ -62,8 +62,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct flock {
 	short	l_type;
 	short	l_whence;
-	off_t	l_start;
-	off_t	l_len;
+	__kernel_off_t	l_start;
+	__kernel_off_t	l_len;
 	long	l_sysid;
 	__kernel_pid_t l_pid;
 	long	pad[4];
