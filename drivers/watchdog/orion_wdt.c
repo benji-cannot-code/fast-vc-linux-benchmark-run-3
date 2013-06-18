85d83f81de6aed9d75017eff19d94f6f18b292d0
Russell King <rmk+kernel@arm.linux.org.uk>
2013-06-18 17:19:32 +0100

    Watchdog: allow orion_wdt to be built for Dove
    The watchdog infrastructure in Dove is no different from that in
    Orion5x or Kirkwood, so let's enable it for Dove.  The only things
    missing are a few register settings in Dove's bridge-regs.h.
    
    Rather than duplicating the same register bit masks for the RSTOUTn_MASK
    and BRIDGE_CAUSE registers, move the definitions into the watchdog
    driver itself.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Acked-by: Jason Cooper <jason@lakedaemon.net>
    Tested-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/orion_wdt.c
+++ drivers/watchdog/orion_wdt.c
@@ -39,6 +39,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WDT_IN_USE		0
 #define WDT_OK_TO_CLOSE		1
 
+#define WDT_RESET_OUT_EN	BIT(1)
+#define WDT_INT_REQ		BIT(3)
+
 static bool nowayout = WATCHDOG_NOWAYOUT;
 static int heartbeat = -1;		/* module parameter (seconds) */
 static unsigned int wdt_max_duration;	/* (seconds) */
