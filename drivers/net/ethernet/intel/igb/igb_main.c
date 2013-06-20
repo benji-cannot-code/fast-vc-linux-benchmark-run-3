7e3b3f3519447fd72976583e4ea1595e35098a7d
Mitch A Williams <mitch.a.williams@intel.com>
2013-06-20 06:03:36 +0000

    igb: don't allow SR-IOV without MSI-X
    MSI-X interrupts are required for SR-IOV operation. Check to make sure
    they're enabled before allowing the user to turn on VFs.
    
    Signed-off-by: Mitch Williams <mitch.a.williams@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -2437,6 +2437,11 @@ static int igb_enable_sriov(struct pci_dev *pdev, int num_vfs)
 	int err = 0;
 	int i;
 
+	if (!adapter->msix_entries) {
+		err = -EPERM;
+		goto out;
+	}
+
 	if (!num_vfs)
 		goto out;
 	else if (old_vfs && old_vfs == num_vfs)
