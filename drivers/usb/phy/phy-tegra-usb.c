8998868a8b0868bc041de4a78f19c28e38dc37c9
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-06-20 08:04:33 +0800

    usb: phy: tegra: remove duplicated include from phy-tegra-usb.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/phy/phy-tegra-usb.c
+++ drivers/usb/phy/phy-tegra-usb.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>
 #include <linux/usb/ehci_def.h>
 #include <linux/usb/tegra_usb_phy.h>
-#include <linux/module.h>
 
 #define ULPI_VIEWPORT		0x170
 
