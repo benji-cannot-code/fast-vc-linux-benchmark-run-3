c33906d7acaf72440d0aaa3ca265f88492d93732
Hans Verkuil <hans.verkuil@cisco.com>
2013-06-20 16:28:15 -0300

    [media] bfin_capture: fix compiler warning
    media-git/drivers/media/platform/blackfin/bfin_capture.c: In function ‘bcap_probe’:
    media-git/drivers/media/platform/blackfin/bfin_capture.c:1007:16: warning: ignoring return value of ‘vb2_queue_init’, declared with attribute warn_unused_result [-Wunused-result]
      vb2_queue_init(q);
                      ^
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Acked-by: Scott Jiang <scott.jiang.linux@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/media/platform/blackfin/bfin_capture.c
+++ drivers/media/platform/blackfin/bfin_capture.c
@@ -1005,7 +1005,9 @@ static int bcap_probe(struct platform_device *pdev)
 	q->mem_ops = &vb2_dma_contig_memops;
 	q->timestamp_type = V4L2_BUF_FLAG_TIMESTAMP_MONOTONIC;
 
-	vb2_queue_init(q);
+	ret = vb2_queue_init(q);
+	if (ret)
+		goto err_free_handler;
 
 	mutex_init(&bcap_dev->mutex);
 	init_completion(&bcap_dev->comp);
