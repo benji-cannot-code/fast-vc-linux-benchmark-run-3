eb4b120dd3df3f5c77f3dd6cdcdbbf5b0c5396be
Santosh Shilimkar <santosh.shilimkar@ti.com>
2013-06-21 18:54:47 -0400

    ARM: keystone: Move CPU bringup code to dedicated asm file
    Because of inline asm usage in platsmp.c, smc instruction
    creates build failure for ARM V6+V7 build where as using instruction
    encoding for smc breaks the thumb2 build.
    
    So move the code snippet to separate asm file and mark
    it with 'armv7-a$(plus_sec)' to avoid any build issues.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-keystone/keystone.h
+++ arch/arm/mach-keystone/keystone.h
@@ -12,7 +12,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __KEYSTONE_H__
 #define __KEYSTONE_H__
 
+#define KEYSTONE_MON_CPU_UP_IDX		0x00
+
+#ifndef __ASSEMBLER__
+
 extern struct smp_operations keystone_smp_ops;
 extern void secondary_startup(void);
+extern u32 keystone_cpu_smc(u32 command, u32 cpu, u32 addr);
 
+#endif /* __ASSEMBLER__ */
 #endif /* __KEYSTONE_H__ */
