164de5bc4f7dfff78e60e45150b24d7070b143a0
Mauro Carvalho Chehab <mchehab@redhat.com>
2013-06-21 11:19:10 -0300

    [media] pvrusb2: Remove unused variable
    drivers/media/usb/pvrusb2/pvrusb2-v4l2.c: In function 'pvr2_v4l2_dev_init':
    drivers/media/usb/pvrusb2/pvrusb2-v4l2.c:1268:21: warning: variable 'usbdev' set but not used [-Wunused-but-set-variable]
    
    This warning is due to changeset a28fbd04facb, with removed the
    usage of usbdev inside pvr2_v4l2_dev_init().
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>
    Cc: Hans Verkuil <hans.verkuil@cisco.com>
    Cc: Mike Isely <isely@pobox.com>

--- drivers/media/usb/pvrusb2/pvrusb2-v4l2.c
+++ drivers/media/usb/pvrusb2/pvrusb2-v4l2.c
@@ -1266,7 +1266,6 @@ static void pvr2_v4l2_dev_init(struct pvr2_v4l2_dev *dip,
 			       struct pvr2_v4l2 *vp,
 			       int v4l_type)
 {
-	struct usb_device *usbdev;
 	int mindevnum;
 	int unit_number;
 	struct pvr2_hdw *hdw;
@@ -1274,7 +1273,6 @@ static void pvr2_v4l2_dev_init(struct pvr2_v4l2_dev *dip,
 	dip->v4lp = vp;
 
 	hdw = vp->channel.mc_head->hdw;
-	usbdev = pvr2_hdw_get_dev(hdw);
 	dip->v4l_type = v4l_type;
 	switch (v4l_type) {
 	case VFL_TYPE_GRABBER:
