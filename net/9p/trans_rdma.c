62cfa6422012d0200206faec0b6b835c058cc301
Simon Derr <simon.derr@bull.net>
2013-06-21 15:32:37 +0200

    9P/RDMA: increase P9_RDMA_MAXSIZE to 1MB
    The current value is too low to get good performance.
    
    Signed-off-by: Simon Derr <simon.derr@bull.net>
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

--- net/9p/trans_rdma.c
+++ net/9p/trans_rdma.c
@@ -58,9 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define P9_RDMA_IRD		0
 #define P9_RDMA_ORD		0
 #define P9_RDMA_TIMEOUT		30000		/* 30 seconds */
-#define P9_RDMA_MAXSIZE		(4*4096)	/* Min SGE is 4, so we can
-						 * safely advertise a maxsize
-						 * of 64k */
+#define P9_RDMA_MAXSIZE		(1024*1024)	/* 1MB */
 
 /**
  * struct p9_trans_rdma - RDMA transport instance
