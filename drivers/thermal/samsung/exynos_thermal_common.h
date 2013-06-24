8328b1b2935ce1464da936db190597e6f30ca54e
Amit Daniel Kachhap <amit.daniel@samsung.com>
2013-06-24 16:20:37 +0530

    thermal: exynos: Make the zone handling use trip information
    This code simplifies the zone handling to use the trip information passed
    by the TMU driver and not the hardcoded macros. This also helps in adding
    more zone support.
    
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Acked-by: Jonghwa Lee <jonghwa3.lee@samsung.com>
    Signed-off-by: Amit Daniel Kachhap <amit.daniel@samsung.com>
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>

--- drivers/thermal/samsung/exynos_thermal_common.h
+++ drivers/thermal/samsung/exynos_thermal_common.h
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GET_ZONE(trip) (trip + 2)
 #define GET_TRIP(zone) (zone - 2)
 
-#define EXYNOS_ZONE_COUNT	3
-
 enum trigger_type {
 	THROTTLE_ACTIVE = 1,
 	THROTTLE_PASSIVE,
@@ -70,6 +68,7 @@ struct freq_clip_table {
 
 struct	thermal_trip_point_conf {
 	int trip_val[MAX_TRIP_COUNT];
+	int trip_type[MAX_TRIP_COUNT];
 	int trip_count;
 	unsigned char trigger_falling;
 };
