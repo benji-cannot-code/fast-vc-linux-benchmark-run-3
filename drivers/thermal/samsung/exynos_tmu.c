e408b4d479136d7ad79d8dac40ffa9e92383f577
Amit Daniel Kachhap <amit.daniel@samsung.com>
2013-06-24 16:20:26 +0530

    thermal: exynos: Rename exynos_thermal.c to exynos_tmu.c
    This patch renames exynos_thermal.c to exynos_tmu.c. This change is needed as
    this file now just contains exynos tmu driver related codes and thermal zone
    or cpufreq cooling registration related changes are not there anymore.
    
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Acked-by: Jonghwa Lee <jonghwa3.lee@samsung.com>
    Acked-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Amit Daniel Kachhap <amit.daniel@samsung.com>
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>

--- drivers/thermal/samsung/exynos_tmu.c
+++ drivers/thermal/samsung/exynos_tmu.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * exynos_thermal.c - Samsung EXYNOS TMU (Thermal Management Unit)
+ * exynos_tmu.c - Samsung EXYNOS TMU (Thermal Management Unit)
  *
  *  Copyright (C) 2011 Samsung Electronics
  *  Donggeun Kim <dg77.kim@samsung.com>
