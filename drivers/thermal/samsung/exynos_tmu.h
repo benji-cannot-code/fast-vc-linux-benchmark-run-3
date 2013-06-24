78a4643bc6fbb31f61c251716796848027ce1886
Amit Daniel Kachhap <amit.daniel@samsung.com>
2013-06-24 16:20:43 +0530

    thermal: exynos: Add driver support for exynos5440 TMU sensor
    This patch modifies TMU controller to add changes needed to work with
    exynos5440 platform. This sensor registers 3 instance of the tmu controller
    with the thermal zone and hence reports 3 temperature output. This controller
    supports upto five trip points. For critical threshold the driver uses the
    core driver thermal framework for shutdown.
    
    Acked-by: Jonghwa Lee <jonghwa3.lee@samsung.com>
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Jungseok Lee <jays.lee@samsung.com>
    Signed-off-by: Amit Daniel Kachhap <amit.daniel@samsung.com>
    Acked-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>

--- drivers/thermal/samsung/exynos_tmu.h
+++ drivers/thermal/samsung/exynos_tmu.h
@@ -41,6 +41,7 @@ enum calibration_mode {
 enum soc_type {
 	SOC_ARCH_EXYNOS4210 = 1,
 	SOC_ARCH_EXYNOS,
+	SOC_ARCH_EXYNOS5440,
 };
 
 /**
@@ -132,6 +133,8 @@ enum soc_type {
  * @emul_temp_shift: shift bits of emulation temperature.
  * @emul_time_shift: shift bits of emulation time.
  * @emul_time_mask: mask bits of emulation time.
+ * @tmu_irqstatus: register to find which TMU generated interrupts.
+ * @tmu_pmin: register to get/set the Pmin value.
  */
 struct exynos_tmu_registers {
 	u32	triminfo_data;
@@ -199,6 +202,9 @@ struct exynos_tmu_registers {
 	u32	emul_temp_shift;
 	u32	emul_time_shift;
 	u32	emul_time_mask;
+
+	u32	tmu_irqstatus;
+	u32	tmu_pmin;
 };
 
 /**
