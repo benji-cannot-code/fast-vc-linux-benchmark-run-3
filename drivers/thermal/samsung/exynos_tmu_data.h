5a97a899dd7721a8585b3c9e5ddf480aa9f4757e
Amit Daniel Kachhap <amit.daniel@samsung.com>
2013-06-24 16:20:32 +0530

    thermal: exynos: Support thermal tripping
    TMU urgently sends active-high signal (thermal trip) to PMU, and thermal
    tripping by hardware logic. Thermal tripping means that PMU cuts off the
    whole power of SoC by controlling external voltage regulator.
    
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Acked-by: Jonghwa Lee <jonghwa3.lee@samsung.com>
    Acked-by: Eduardo Valentin <eduardo.valentin@ti.com>
    Signed-off-by: Jonghwan Choi <jhbird.choi@samsung.com>
    Signed-off-by: Amit Daniel Kachhap <amit.daniel@samsung.com>
    Signed-off-by: Eduardo Valentin <eduardo.valentin@ti.com>

--- drivers/thermal/samsung/exynos_tmu_data.h
+++ drivers/thermal/samsung/exynos_tmu_data.h
@@ -92,6 +92,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EXYNOS_EMUL_DATA_MASK	0xFF
 #define EXYNOS_EMUL_ENABLE	0x1
 
+#define EXYNOS_MAX_TRIGGER_PER_REG	4
+
 #if defined(CONFIG_CPU_EXYNOS4210)
 extern struct exynos_tmu_platform_data const exynos4210_default_tmu_data;
 #define EXYNOS4210_TMU_DRV_DATA (&exynos4210_default_tmu_data)
