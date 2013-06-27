eb387ce4645700acdbe540524b77eb3ada55428b
Dave Airlie <airlied@redhat.com>
2013-06-27 10:07:39 +1000

    vgacon: fix missing include.
    This fixes the build error.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/video/console/vgacon.c
+++ drivers/video/console/vgacon.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kd.h>
 #include <linux/slab.h>
 #include <linux/vt_kern.h>
+#include <linux/sched.h>
 #include <linux/selection.h>
 #include <linux/spinlock.h>
 #include <linux/ioport.h>
