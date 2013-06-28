69b5ba578119575b8e76778c671f6e0bd82073c3
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-06-28 11:33:01 -0700

    USB: isp1362: remove CONFIG_USB_DEBUG dependency
    Now that the debugging macros are cleaned up, just rely on the dynamic
    debug code in the kernel to do the debug messages for the driver.
    
    This lets debugging be enabled without having to rebuild the driver, an
    important thing for users that can not do it.
    
    Acked-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/isp1362-hcd.c
+++ drivers/usb/host/isp1362-hcd.c
@@ -38,11 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * recovery time (MSCx = 0x7f8c) with a memory clock of 99.53 MHz.
  */
 
-#ifdef CONFIG_USB_DEBUG
-# define ISP1362_DEBUG
-#else
-# undef ISP1362_DEBUG
-#endif
+#undef ISP1362_DEBUG
 
 /*
  * The PXA255 UDC apparently doesn't handle GET_STATUS, GET_CONFIG and
