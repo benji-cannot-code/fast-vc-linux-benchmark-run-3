10274bb62a96462cda2016294d8d99a3da340e6a
David Disseldorp <ddiss@suse.de>
2013-06-28 11:47:33 +0200

    cifs: fill TRANS2_QUERY_FILE_INFO ByteCount fields
    Currently the trans2 ByteCount field is incorrectly left zero in
    TRANS2_QUERY_FILE_INFO info_level=SMB_QUERY_FILE_ALL_INFO and
    info_level=SMB_QUERY_FILE_UNIX_BASIC requests. The field should properly
    reflect the FID, information_level and padding bytes carried in these
    requests.
    
    Leaving this field zero causes such requests to fail against Novell CIFS
    servers. Other SMB servers (e.g. Samba) use the parameter count fields
    for data length calculations instead, so do not suffer the same fate.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>
    Acked-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Steve French <smfrench@gmail.com>

--- fs/cifs/cifssmb.c
+++ fs/cifs/cifssmb.c
@@ -3931,6 +3931,7 @@ QFileInfoRetry:
 	pSMB->Pad = 0;
 	pSMB->Fid = netfid;
 	inc_rfc1001_len(pSMB, byte_count);
+	pSMB->t2.ByteCount = cpu_to_le16(byte_count);
 
 	rc = SendReceive(xid, tcon->ses, (struct smb_hdr *) pSMB,
 			 (struct smb_hdr *) pSMBr, &bytes_returned, 0);
@@ -4099,6 +4100,7 @@ UnixQFileInfoRetry:
 	pSMB->Pad = 0;
 	pSMB->Fid = netfid;
 	inc_rfc1001_len(pSMB, byte_count);
+	pSMB->t2.ByteCount = cpu_to_le16(byte_count);
 
 	rc = SendReceive(xid, tcon->ses, (struct smb_hdr *) pSMB,
 			 (struct smb_hdr *) pSMBr, &bytes_returned, 0);
