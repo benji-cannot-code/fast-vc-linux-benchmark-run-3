664ae24606b5895fdfa8d88370ea5393f62a002c
Pravin B Shelar <pshelar@nicira.com>
2013-06-28 16:07:40 -0700

    openvswitch: Add Kconfig dependency on GRE-DEMUX.
    Openvswitch uses function from NET_IPGRE_DEMUX module.
    Add Kconfig dependency to fix following compilation errors:
    http://marc.info/?l=linux-netdev&m=137244035226634
    
    CC: Jesse Gross <jesse@nicira.com>
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Pravin Shelar <pshelar@nicira.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>
    Acked-by: Jesse Gross <jesse@nicira.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/openvswitch/vport-gre.c
+++ net/openvswitch/vport-gre.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 02110-1301, USA
  */
 
-#if IS_ENABLED(CONFIG_NET_IPGRE_DEMUX)
+#ifdef CONFIG_OPENVSWITCH_GRE
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/if.h>
@@ -272,4 +272,5 @@ const struct vport_ops ovs_gre_vport_ops = {
 	.get_name	= gre_get_name,
 	.send		= gre_tnl_send,
 };
-#endif
+
+#endif /* OPENVSWITCH_GRE */
