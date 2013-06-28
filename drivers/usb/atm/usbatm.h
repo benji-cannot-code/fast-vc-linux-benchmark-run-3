9e5b4f3d459e976b20208767a521a99dc9ec4fe5
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-06-28 11:32:52 -0700

    USB: usbatm: don't rely on CONFIG_USB_DEBUG
    Enable the USB atm drivers to use the dynamic debugging subsystem, and
    not rely on if CONFIG_USB_DEBUG is enabled or not for debugging
    messages. This also provides a saner debug message, pointing out the
    exact device the message is coming from.
    
    This also means the drivers do not have to be rebuilt to get debugging
    messages, important for getting information from users who can not
    rebuild their kernels.
    
    Cc: Duncan Sands <duncan.sands@free.fr>
    Acked-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/atm/usbatm.h
+++ drivers/usb/atm/usbatm.h
@@ -46,13 +46,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	dev_info(&(instance)->usb_intf->dev , format , ## arg)
 #define usb_warn(instance, format, arg...)	\
 	dev_warn(&(instance)->usb_intf->dev , format , ## arg)
-#ifdef DEBUG
 #define usb_dbg(instance, format, arg...)	\
-	dev_printk(KERN_DEBUG , &(instance)->usb_intf->dev , format , ## arg)
-#else
-#define usb_dbg(instance, format, arg...)	\
-	do {} while (0)
-#endif
+	dev_dbg(&(instance)->usb_intf->dev , format , ## arg)
 
 /* FIXME: move to dev_* once ATM is driver model aware */
 #define atm_printk(level, instance, format, arg...)	\
