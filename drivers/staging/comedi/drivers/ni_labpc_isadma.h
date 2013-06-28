2f0e6ecab65356c35ec2feb09008855d738cf68e
Ian Abbott <abbotti@mev.co.uk>
2013-06-28 17:09:20 +0100

    staging: comedi: ni_labpc: migrate DMA channel init & free
    Migrate the code for requesting an ISA DMA channel and a DMA buffer, and
    the code for freeing them into two new functions in the
    "ni_labpc_isadma" module: `labpc_init_dma_chan()` and
    `labpc_free_dma_chan()`.  Dummy inline functions are provided in
    "ni_labpc_isadma.h" if the "ni_labpc_isadma" module is not being built.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc_isadma.h
+++ drivers/staging/comedi/drivers/ni_labpc_isadma.h
@@ -10,7 +10,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if NI_LABPC_HAVE_ISA_DMA
 
+int labpc_init_dma_chan(struct comedi_device *dev, unsigned int dma_chan);
+void labpc_free_dma_chan(struct comedi_device *dev);
+
 #else
+
+static inline int labpc_init_dma_chan(struct comedi_device *dev,
+				      unsigned int dma_chan)
+{
+	return -ENOTSUPP;
+}
+
+static inline void labpc_free_dma_chan(struct comedi_device *dev)
+{
+}
 
 #endif
 
