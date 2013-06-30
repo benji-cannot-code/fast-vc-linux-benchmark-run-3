3f6a3f0fa8045b2516d03d101d154ec007348e27
Felipe Balbi <balbi@ti.com>
2013-06-30 14:19:33 +0300

    usb: dwc3: let non-DT platforms pass tx-fifo-resize flag;
    in case we're not in a DT boot, we should
    still be able to tell the driver how to behave.
    
    In order to be able to pass flags to the driver,
    we introduce platform_data structure which the
    core driver should use.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

---
+++ drivers/usb/dwc3/platform_data.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/**
+ * platform_data.h - USB DWC3 Platform Data Support
+ *
+ * Copyright (C) 2013 Texas Instruments Incorporated - http://www.ti.com
+ * Author: Felipe Balbi <balbi@ti.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2  of
+ * the License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+struct dwc3_platform_data {
+	bool tx_fifo_resize;
+};
