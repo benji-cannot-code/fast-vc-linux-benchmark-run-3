a612ef0f6cb3156ceff7c38cfed2d7d21aa4110f
Alex Deucher <alexander.deucher@amd.com>
2013-07-01 16:07:18 -0400

    drm/radeon/dpm: re-enable state transitions for Cayman
    Was disabled due to stability issues on certain boards
    caused by the a bug in the parsing of the atom mc reg tables.
    That's fixed now so re-enable.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/ni_dpm.c
+++ drivers/gpu/drm/radeon/ni_dpm.c
@@ -1037,7 +1037,6 @@ static int ni_restrict_performance_levels_before_switch(struct radeon_device *rd
 		0 : -EINVAL;
 }
 
-#if 0
 static int ni_unrestrict_performance_levels_after_switch(struct radeon_device *rdev)
 {
 	if (ni_send_msg_to_smc_with_parameter(rdev, PPSMC_MSG_SetForcedLevels, 0) != PPSMC_Result_OK)
@@ -1046,7 +1045,6 @@ static int ni_unrestrict_performance_levels_after_switch(struct radeon_device *r
 	return (ni_send_msg_to_smc_with_parameter(rdev, PPSMC_MSG_SetEnabledLevels, 0) == PPSMC_Result_OK) ?
 		0 : -EINVAL;
 }
-#endif
 
 static void ni_stop_smc(struct radeon_device *rdev)
 {
@@ -3833,14 +3831,11 @@ int ni_dpm_set_power_state(struct radeon_device *rdev)
 		return ret;
 	}
 
-#if 0
-	/* XXX */
 	ret = ni_unrestrict_performance_levels_after_switch(rdev);
 	if (ret) {
 		DRM_ERROR("ni_unrestrict_performance_levels_after_switch failed\n");
 		return ret;
 	}
-#endif
 
 	return 0;
 }
