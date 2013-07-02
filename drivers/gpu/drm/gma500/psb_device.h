fe6aea7ef5aa900e5fbc33d1234907ec59ef2c3f
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2013-07-02 17:07:59 +0200

    drm/gma500/psb: Make use of generic clock code
    Add chip specific callbacks for the generic and non-generic clock
    calculation code. Also remove as much dupilicated code as possible.
    
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- drivers/gpu/drm/gma500/psb_device.h
+++ drivers/gpu/drm/gma500/psb_device.h
@@ -1,5 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* copyright (c) 2008, Intel Corporation
+/*
+ * Copyright © 2013 Patrik Jakobsson
+ * Copyright © 2011 Intel Corporation
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -13,12 +15,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Authors:
- * Eric Anholt <eric@anholt.net>
  */
 
-#ifndef _INTEL_DISPLAY_H_
-#define _INTEL_DISPLAY_H_
+#ifndef _PSB_DEVICE_H_
+#define _PSB_DEVICE_H_
+
+extern const struct gma_clock_funcs psb_clock_funcs;
 
 #endif
