529ce8b9374a843d171618f268f79a7f48127fd5
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-03 15:07:22 -0700

    drivers/rtc/rtc-isl12022.c: remove empty function
    After the switch to devm_* functions and the removal of
    rtc_device_unregister(), the 'remove' function does not do anything.
    Delete it.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: Roman Fietze <roman.fietze@telemotive.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-isl12022.c
+++ drivers/rtc/rtc-isl12022.c
@@ -274,11 +274,6 @@ static int isl12022_probe(struct i2c_client *client,
 	return 0;
 }
 
-static int isl12022_remove(struct i2c_client *client)
-{
-	return 0;
-}
-
 static const struct i2c_device_id isl12022_id[] = {
 	{ "isl12022", 0 },
 	{ }
@@ -290,7 +285,6 @@ static struct i2c_driver isl12022_driver = {
 		.name	= "rtc-isl12022",
 	},
 	.probe		= isl12022_probe,
-	.remove		= isl12022_remove,
 	.id_table	= isl12022_id,
 };
 
