45d267bef60d0fbb33d190c2e2c7181b42db38cf
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-03 15:05:44 -0700

    drivers/rtc/rtc-at91rm9200.c: include <linux/uaccess.h>
    Silences the following checkpatch warning:
      WARNING: Use #include <linux/uaccess.h> instead of <asm/uaccess.h>
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: Andrew Victor <linux@maxim.org.za>
    Cc: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-at91rm9200.c
+++ drivers/rtc/rtc-at91rm9200.c
@@ -32,8 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #include "rtc-at91rm9200.h"
 
