849f03eb30fb73a45194e413f023201b556a0cab
Chen Gang <gang.chen@asianux.com>
2013-07-03 15:00:42 -0700

    arch: c6x: mm: include "asm/uaccess.h" to pass compiling
    Need include "asm/uaccess.h" to pass compiling.
    
    The related error (with allmodconfig):
    
      arch/c6x/mm/init.c: In function `paging_init':
      arch/c6x/mm/init.c:46:2: error: implicit declaration of function `set_fs' [-Werror=implicit-function-declaration]
      arch/c6x/mm/init.c:46:9: error: `KERNEL_DS' undeclared (first use in this function)
      arch/c6x/mm/init.c:46:9: note: each undeclared identifier is reported only once for each function it appears in
    
    Signed-off-by: Chen Gang <gang.chen@asianux.com>
    Cc: <stable@vger.kernel.org>	[3.10.x]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/c6x/mm/init.c
+++ arch/c6x/mm/init.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/initrd.h>
 
 #include <asm/sections.h>
+#include <asm/uaccess.h>
 
 /*
  * ZERO_PAGE is a special page that is used for zero-initialized
