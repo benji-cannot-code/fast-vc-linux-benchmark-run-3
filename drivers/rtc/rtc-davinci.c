662a3ce8fb8ad16c7bd4536da7e119bfb6ced6b3
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-03 15:05:46 -0700

    drivers/rtc/rtc-davinci.c: fix whitespace warning
    Silences the following warning:
      WARNING: please, no space before tabs
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-davinci.c
+++ drivers/rtc/rtc-davinci.c
@@ -118,7 +118,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static DEFINE_SPINLOCK(davinci_rtc_lock);
 
 struct davinci_rtc {
-	struct rtc_device 		*rtc;
+	struct rtc_device		*rtc;
 	void __iomem			*base;
 	resource_size_t			pbase;
 	size_t				base_size;
