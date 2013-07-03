bc375a3d977ef137238a787a0d9c8530544e3b0f
Dan Carpenter <dan.carpenter@oracle.com>
2013-07-02 20:32:39 -0700

    Input: cyttsp4 - silence shift wrap warning
    "*max" is a size_t (long) type but "1" is an int so static checkers
    complain that the shift could wrap.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/cyttsp4_core.c
+++ drivers/input/touchscreen/cyttsp4_core.c
@@ -154,7 +154,7 @@ static int cyttsp4_hw_reset(struct cyttsp4 *cd)
  */
 static int cyttsp4_bits_2_bytes(unsigned int nbits, size_t *max)
 {
-	*max = 1 << nbits;
+	*max = 1UL << nbits;
 	return (nbits + 7) / 8;
 }
 
