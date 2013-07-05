8d2ca83b018b821ea0a8654abab30b8cf0a5c54d
Ben Skeggs <bskeggs@redhat.com>
2013-07-05 10:26:20 +1000

    drm/nvc0-/gr: factor out yet more unknown magic into versioned functions
    NVC1/NVD9 are the only chipsets that should have anything different
    happen on them after this.  We previously weren't doing these
    register modifications, and NVIDIA do.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/graph/ctxnvc0.c
+++ drivers/gpu/drm/nouveau/core/engine/graph/ctxnvc0.c
@@ -902,6 +902,11 @@ nvc0_grctx_generate_mods(struct nvc0_graph_priv *priv, struct nvc0_grctx *info)
 }
 
 void
+nvc0_grctx_generate_unkn(struct nvc0_graph_priv *priv)
+{
+}
+
+void
 nvc0_grctx_generate_tpcid(struct nvc0_graph_priv *priv)
 {
 	int gpc, tpc, id;
@@ -1061,6 +1066,7 @@ nvc0_grctx_generate_main(struct nvc0_graph_priv *priv, struct nvc0_grctx *info)
 	nv_wr32(priv, 0x404154, 0x00000000);
 
 	oclass->mods(priv, info);
+	oclass->unkn(priv);
 
 	nvc0_grctx_generate_tpcid(priv);
 	nvc0_grctx_generate_r406028(priv);
@@ -1236,6 +1242,7 @@ nvc0_grctx_oclass = &(struct nvc0_grctx_oclass) {
 	},
 	.main = nvc0_grctx_generate_main,
 	.mods = nvc0_grctx_generate_mods,
+	.unkn = nvc0_grctx_generate_unkn,
 	.hub  = nvc0_grctx_init_hub,
 	.gpc  = nvc0_grctx_init_gpc,
 	.icmd = nvc0_grctx_init_icmd,
