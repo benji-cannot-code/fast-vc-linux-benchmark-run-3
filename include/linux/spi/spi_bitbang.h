4dec21535375e5bceb60f1da236988e6bf55136a
Mark Brown <broonie@linaro.org>
2013-07-05 20:07:27 +0100

    spi/bitbang: Use core message pump
    Convert drivers using bitbang to use the core mesasge pump infrastructure,
    saving some code and meaning that these drivers get to take advantage of
    work done on improving the core implementation.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- include/linux/spi/spi_bitbang.h
+++ include/linux/spi/spi_bitbang.h
@@ -5,11 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/workqueue.h>
 
 struct spi_bitbang {
-	struct workqueue_struct	*workqueue;
-	struct work_struct	work;
-
 	spinlock_t		lock;
-	struct list_head	queue;
 	u8			busy;
 	u8			use_dma;
 	u8			flags;		/* extra spi->mode support */
