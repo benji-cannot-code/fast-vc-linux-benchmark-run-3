d94c0df83f15ceffcb7e18e50729804e31b1479d
Michal Simek <michal.simek@xilinx.com>
2013-07-08 15:29:14 +0200

    spi/xilinx: Remove CONFIG_OF from the driver
    dev.of_node is in struct device all the time.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-xilinx.c
+++ drivers/spi/spi-xilinx.c
@@ -357,7 +357,6 @@ static int xilinx_spi_probe(struct platform_device *pdev)
 		bits_per_word = pdata->bits_per_word;
 	}
 
-#ifdef CONFIG_OF
 	if (pdev->dev.of_node) {
 		const __be32 *prop;
 		int len;
@@ -368,7 +367,6 @@ static int xilinx_spi_probe(struct platform_device *pdev)
 		if (prop && len >= sizeof(*prop))
 			num_cs = __be32_to_cpup(prop);
 	}
-#endif
 
 	if (!num_cs) {
 		dev_err(&pdev->dev,
