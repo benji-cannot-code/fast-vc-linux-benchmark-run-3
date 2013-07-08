08e482c506477a337b213dcd7698a6e856e41a97
Soren Brinkmann <soren.brinkmann@xilinx.com>
2013-07-08 09:51:37 -0700

    clocksource: cadence_ttc: Remove unused header
    The clk-provider.h header is not required by this driver.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/cadence_ttc_timer.c
+++ drivers/clocksource/cadence_ttc_timer.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/slab.h>
-#include <linux/clk-provider.h>
 
 /*
  * This driver configures the 2 16-bit count-up timers as follows:
