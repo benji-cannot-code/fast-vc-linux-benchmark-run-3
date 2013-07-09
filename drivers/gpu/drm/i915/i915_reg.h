b8eebea97f83ade3d52353fb7e04a9c0393c9c00
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-07-09 22:59:16 +0200

    drm/i915: improve GEN7_ERR_INT clearing for fifo underrun reporting
    Same treatment as for SERR_INT: If we clear only the bit for the pipe
    we're enabling (but unconditionally) then we can always check for
    possible underruns after having disabled the interrupt. That way pipe
    underruns won't be lost, but at worst only get reported in a delayed
    fashion.
    
    v2: The same logic bug as in the SERR handling change also existed
    here. The same bugfix of only reporting missed underruns when the
    error interrupt was masked applies, too.
    
    v3: Do the same fixes as for the SERR handling that Paulo suggested in
    his review:
    - s/%i/%c/ fix in the debug output
    - move the DE_ERR_INT_IVB read into the respective if block
    
    Cc: Paulo Zanoni <przanoni@gmail.com>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    [danvet: Fix up the checkpatch bikeshed Paulo noticed.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -682,6 +682,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   ERR_INT_FIFO_UNDERRUN_C	(1<<6)
 #define   ERR_INT_FIFO_UNDERRUN_B	(1<<3)
 #define   ERR_INT_FIFO_UNDERRUN_A	(1<<0)
+#define   ERR_INT_FIFO_UNDERRUN(pipe)	(1<<(pipe*3))
 
 #define FPGA_DBG		0x42300
 #define   FPGA_DBG_RM_NOCLAIM	(1<<31)
