273310889813bb2aafd8538839ffeaa7463ea546
Joe Perches <joe@perches.com>
2013-07-09 10:13:03 -0700

    [IA64] dmi.h: Make dmi_alloc use kzalloc
    x86/ia64 have a slight mismatch in dmi_alloc as
    x86 does a memset(0), and ia64 just does kmalloc.
    
    Make the ia64 dmi_alloc match the x86 style.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/include/asm/dmi.h
+++ arch/ia64/include/asm/dmi.h
@@ -8,6 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Use normal IO mappings for DMI */
 #define dmi_ioremap ioremap
 #define dmi_iounmap(x,l) iounmap(x)
-#define dmi_alloc(l) kmalloc(l, GFP_ATOMIC)
+#define dmi_alloc(l) kzalloc(l, GFP_ATOMIC)
 
 #endif
