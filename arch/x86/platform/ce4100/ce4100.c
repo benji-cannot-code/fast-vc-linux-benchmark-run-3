a7b5b704cca7ee01e39e9bb692ce8be233ab7711
Xiong Zhou <jencce.kernel@gmail.com>
2013-07-09 11:06:27 +0800

    x86/platform/ce4100: Add header file for reboot type
    Add header file for reboot type to fix this build failure:
    
     error: 'reboot_type' undeclared (first use in this function)
     error: 'BOOT_KBD' undeclared (first use in this function)
    
    Signed-off-by: Xiong Zhou <jencce.kernel@gmail.com>
    Cc: rui.zhang@intel.com
    Cc: alan@linux.intel.com
    Cc: ffainelli@freebox.fr
    Cc: mbizon@freebox.fr
    Cc: matthew.garrett@nebula.com
    Link: http://lkml.kernel.org/r/alpine.DEB.2.02.1307091053280.28371@M2420
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/platform/ce4100/ce4100.c
+++ arch/x86/platform/ce4100/ce4100.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/serial_reg.h>
 #include <linux/serial_8250.h>
+#include <linux/reboot.h>
 
 #include <asm/ce4100.h>
 #include <asm/prom.h>
