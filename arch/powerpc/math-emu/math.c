ba1eda842deb6cef0c69f156b6534ae1c044bea9
Kevin Hao <haokexin@gmail.com>
2013-07-10 09:43:43 +0800

    powerpc/math-emu: Remove the unneeded check for CONFIG_MATH_EMULATION in math.c
    The math.c is only built when CONFIG_MATH_EMULATION is enabled.
    So the #ifdef check for CONFIG_MATH_EMULATION in it seems redundant.
    Drop all of them.
    
    Signed-off-by: Kevin Hao <haokexin@gmail.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/math-emu/math.c
+++ arch/powerpc/math-emu/math.c
@@ -155,7 +155,6 @@ FLOATFUNC(fsqrts);
 #define XEU	15
 #define XFLB	10
 
-#ifdef CONFIG_MATH_EMULATION
 static int
 record_exception(struct pt_regs *regs, int eflag)
 {
@@ -213,7 +212,6 @@ record_exception(struct pt_regs *regs, int eflag)
 
 	return (fpscr & FPSCR_FEX) ? 1 : 0;
 }
-#endif /* CONFIG_MATH_EMULATION */
 
 int
 do_mathemu(struct pt_regs *regs)
@@ -223,11 +221,9 @@ do_mathemu(struct pt_regs *regs)
 	signed short sdisp;
 	u32 insn = 0;
 	int idx = 0;
-#ifdef CONFIG_MATH_EMULATION
 	int (*func)(void *, void *, void *, void *);
 	int type = 0;
 	int eflag, trap;
-#endif
 
 	if (get_user(insn, (u32 *)pc))
 		return -EFAULT;
