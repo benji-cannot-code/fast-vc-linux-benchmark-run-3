8b38c49cdea51cb1a38fd6ebdba4fba62ab847ce
Alexander Holler <holler@ahsoftware.de>
2013-07-10 09:32:00 +0100

    iio: hid-sensor-magn-3d: add module alias for autoload
    Add a MODULE_DEVICE_TABLE in order to let hotplug mechanisms automatically
    load the driver.
    
    This makes it also possible to use the usual driver name instead of
    HID-SENSOR-2000xx which isn't very descriptive in kernel messages.
    
    Signed-off-by: Alexander Holler <holler@ahsoftware.de>
    Acked-by:Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/magnetometer/hid-sensor-magn-3d.c
+++ drivers/iio/magnetometer/hid-sensor-magn-3d.c
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iio/triggered_buffer.h>
 #include "../common/hid-sensors/hid-sensor-trigger.h"
 
-/*Format: HID-SENSOR-usage_id_in_hex*/
-/*Usage ID from spec for Magnetometer-3D: 0x200083*/
-#define DRIVER_NAME "HID-SENSOR-200083"
-
 enum magn_3d_channel {
 	CHANNEL_SCAN_INDEX_X,
 	CHANNEL_SCAN_INDEX_Y,
@@ -391,9 +387,19 @@ static int hid_magn_3d_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static struct platform_device_id hid_magn_3d_ids[] = {
+	{
+		/* Format: HID-SENSOR-usage_id_in_hex_lowercase */
+		.name = "HID-SENSOR-200083",
+	},
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(platform, hid_magn_3d_ids);
+
 static struct platform_driver hid_magn_3d_platform_driver = {
+	.id_table = hid_magn_3d_ids,
 	.driver = {
-		.name	= DRIVER_NAME,
+		.name	= KBUILD_MODNAME,
 		.owner	= THIS_MODULE,
 	},
 	.probe		= hid_magn_3d_probe,
