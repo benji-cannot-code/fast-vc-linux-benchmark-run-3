89a5b7d23ae723891edaa335cd7cc6599bd0d4a5
Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
2013-07-10 18:12:11 +0200

    drm/gma500/cdv: Convert to gma_crtc_dpms()
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>

--- drivers/gpu/drm/gma500/gma_display.c
+++ drivers/gpu/drm/gma500/gma_display.c
@@ -211,10 +211,8 @@ void gma_crtc_dpms(struct drm_crtc *crtc, int mode)
 	 * and on, we should map those modes to DRM_MODE_DPMS_OFF in the CRTC.
 	 */
 
-	/* FIXME: Uncomment this when we move cdv to generic dpms
 	if (IS_CDV(dev))
 		cdv_intel_disable_self_refresh(dev);
-	*/
 
 	switch (mode) {
 	case DRM_MODE_DPMS_ON:
@@ -323,10 +321,8 @@ void gma_crtc_dpms(struct drm_crtc *crtc, int mode)
 		break;
 	}
 
-	/* FIXME: Uncomment this when we move cdv to generic dpms
 	if (IS_CDV(dev))
 		cdv_intel_update_watermark(dev, crtc);
-	*/
 
 	/* Set FIFO watermarks */
 	REG_WRITE(DSPARB, 0x3F3E);
