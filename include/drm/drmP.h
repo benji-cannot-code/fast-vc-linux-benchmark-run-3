3d8857fd5e00c3fc1efb7e1e497b63935ea90ccb
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-07-10 14:11:35 +0200

    drm: remove drm_modctx ioctl and use drm_noop instead
    It doesn't do anything, so kill the code.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@gmail.com>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1336,8 +1336,6 @@ extern int drm_resctx(struct drm_device *dev, void *data,
 		      struct drm_file *file_priv);
 extern int drm_addctx(struct drm_device *dev, void *data,
 		      struct drm_file *file_priv);
-extern int drm_modctx(struct drm_device *dev, void *data,
-		      struct drm_file *file_priv);
 extern int drm_getctx(struct drm_device *dev, void *data,
 		      struct drm_file *file_priv);
 extern int drm_switchctx(struct drm_device *dev, void *data,
