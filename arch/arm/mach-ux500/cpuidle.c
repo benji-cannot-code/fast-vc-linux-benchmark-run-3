a9317a98156227f3a315938092f55cceea9fb4d2
Linus Walleij <linus.walleij@linaro.org>
2013-07-10 15:52:01 +0200

    ARM: ux500: cpuidle: Remove pointless include
    The db8500-regs.h file does not need to be included by the cpuidle
    driver.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- arch/arm/mach-ux500/cpuidle.c
+++ arch/arm/mach-ux500/cpuidle.c
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpuidle.h>
 #include <asm/proc-fns.h>
 
-#include "db8500-regs.h"
-
 static atomic_t master = ATOMIC_INIT(0);
 static DEFINE_SPINLOCK(master_lock);
 
