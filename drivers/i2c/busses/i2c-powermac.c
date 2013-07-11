57d9063c5cb1fec2cb52bae8d7c8b01d4b8c40dd
Wolfram Sang <wsa@the-dreams.de>
2013-07-11 22:02:03 +0100

    i2c: powermac: fix return path on error
    We want to bail out immediately if i2c_add_adapter failed and not try to
    register child nodes with a nilled adapter structure.
    
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-powermac.c
+++ drivers/i2c/busses/i2c-powermac.c
@@ -447,6 +447,7 @@ static int i2c_powermac_probe(struct platform_device *dev)
 		printk(KERN_ERR "i2c-powermac: Adapter %s registration "
 		       "failed\n", adapter->name);
 		memset(adapter, 0, sizeof(*adapter));
+		return rc;
 	}
 
 	printk(KERN_INFO "PowerMac i2c bus %s registered\n", adapter->name);
@@ -456,7 +457,7 @@ static int i2c_powermac_probe(struct platform_device *dev)
 	 */
 	i2c_powermac_register_devices(adapter, bus);
 
-	return rc;
+	return 0;
 }
 
 static struct platform_driver i2c_powermac_driver = {
