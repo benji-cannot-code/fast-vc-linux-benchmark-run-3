e02eb8574ae5b558671b6a0d2e4fc885f4d5864e
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-07-14 16:05:55 -0700

    sysfs: add support for binary attributes in groups
    groups should be able to support binary attributes, just like it
    supports "normal" attributes.  This lets us only handle one type of
    structure, groups, throughout the driver core and subsystems, making
    binary attributes a "full fledged" part of the driver model, and not
    something just "tacked on".
    
    Reported-by: Oliver Schinagl <oliver@schinagl.nl>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct kobject;
 struct module;
+struct bin_attribute;
 enum kobj_ns_type;
 
 struct attribute {
@@ -60,9 +61,8 @@ struct attribute_group {
 	umode_t			(*is_visible)(struct kobject *,
 					      struct attribute *, int);
 	struct attribute	**attrs;
+	struct bin_attribute	**bin_attrs;
 };
-
-
 
 /**
  * Use these macros to make defining attributes easier. See include/linux/device.h
