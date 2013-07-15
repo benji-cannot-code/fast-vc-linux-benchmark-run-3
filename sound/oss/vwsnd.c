a5aff72435b00b0d579bc36a99576266a4248b3a
Takashi Iwai <tiwai@suse.de>
2013-07-15 11:58:55 +0200

    sound: oss/vwsnd: Add missing inclusion of linux/delay.h
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/oss/vwsnd.c
+++ sound/oss/vwsnd.c
@@ -150,6 +150,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
+#include <linux/delay.h>
 
 #include <asm/visws/cobalt.h>
 
