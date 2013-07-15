154b75de189986302fd767ca61d2102c244a3cd2
Baruch Siach <baruch@tkos.co.il>
2013-07-15 08:24:22 +0300

    xtensa: remove CCOUNT_PER_JIFFY
    Use ccount_freq directly to make the code a little more readable.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- arch/xtensa/include/asm/timex.h
+++ arch/xtensa/include/asm/timex.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 extern unsigned long ccount_freq;
-#define CCOUNT_PER_JIFFY (ccount_freq / HZ)
 
 typedef unsigned long long cycles_t;
 
