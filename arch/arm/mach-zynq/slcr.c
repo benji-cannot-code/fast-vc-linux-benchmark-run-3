7585cbdb48db6a0b13e19e6c747768fc48953fae
Soren Brinkmann <soren.brinkmann@xilinx.com>
2013-07-17 10:10:13 -0700

    arm: zynq: slcr: Remove redundant header #includes
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/arm/mach-zynq/slcr.c
+++ arch/arm/mach-zynq/slcr.c
@@ -15,18 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 02139, USA.
  */
 
-#include <linux/export.h>
 #include <linux/io.h>
-#include <linux/fs.h>
-#include <linux/interrupt.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
 #include <linux/of_address.h>
-#include <linux/uaccess.h>
-#include <linux/platform_device.h>
-#include <linux/slab.h>
-#include <linux/string.h>
 #include <linux/clk/zynq.h>
 #include "common.h"
 
