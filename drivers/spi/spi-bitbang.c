bb14064a60d5e2e38eb972bb1b7ffbd7280a5a4c
Fabio Estevam <fabio.estevam@freescale.com>
2013-07-17 15:49:54 -0300

    spi: spi-bitbang: Fix conversion of spi_bitbang_transfer_one()
    Since commit 2025172e3 (spi/bitbang: Use core message pump), the following
    kernel crash is seen:
    
    Unable to handle kernel NULL pointer dereference at virtual address 0000000d
    pgd = 80004000
    [0000000d] *pgd=00000000
    Internal error: Oops: 5 [#1] SMP ARM
    Modules linked in:
    CPU: 1 PID: 48 Comm: spi32766 Not tainted 3.11.0-rc1+ #4
    task: bfa3e580 ti: bfb90000 task.ti: bfb90000
    PC is at spi_bitbang_transfer_one+0x50/0x248
    LR is at spi_bitbang_transfer_one+0x20/0x248
    ...
    
    ,and also the following build warning:
    
    drivers/spi/spi-bitbang.c: In function 'spi_bitbang_start':
    drivers/spi/spi-bitbang.c:436:31: warning: assignment from incompatible pointer type [enabled by default]
    
    In order to fix it, we need to change the first parameter of
    spi_bitbang_transfer_one() to 'struct spi_master *master'.
    
    Tested on a mx6qsabrelite by succesfully probing a SPI NOR flash.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-bitbang.c
+++ drivers/spi/spi-bitbang.c
@@ -271,7 +271,7 @@ static int spi_bitbang_prepare_hardware(struct spi_master *spi)
 	return 0;
 }
 
-static int spi_bitbang_transfer_one(struct spi_device *spi,
+static int spi_bitbang_transfer_one(struct spi_master *master,
 				    struct spi_message *m)
 {
 	struct spi_bitbang 	*bitbang;
@@ -281,8 +281,9 @@ static int spi_bitbang_transfer_one(struct spi_device *spi,
 	unsigned		cs_change;
 	int			status;
 	int			do_setup = -1;
+	struct spi_device	*spi = m->spi;
 
-	bitbang = spi_master_get_devdata(spi->master);
+	bitbang = spi_master_get_devdata(master);
 
 	/* FIXME this is made-up ... the correct value is known to
 	 * word-at-a-time bitbang code, and presumably chipselect()
@@ -373,7 +374,7 @@ static int spi_bitbang_transfer_one(struct spi_device *spi,
 		ndelay(nsecs);
 	}
 
-	spi_finalize_current_message(spi->master);
+	spi_finalize_current_message(master);
 
 	return status;
 }
