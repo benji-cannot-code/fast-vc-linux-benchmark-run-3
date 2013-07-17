ab444800ed5b4d8c5cfdb9eb66aa22bfeb911a07
Lars-Peter Clausen <lars@metafoo.de>
2013-07-17 15:44:00 +0100

    staging:iio:adis16260: Remove support for orientation mapping
    The adis16260 driver implements a unique feature in that it allows to change the
    orientation of the gyroscope channel by specifying the orientation in platform
    data. This feature is as far as I can see unused though and makes the driver
    unnecessarily complex. So this patch removes the support for it. If it turns
    out we need this, the cleanest approach to implement it is by adding support for
    orientation mapping inside the IIO core so it is available to all drivers.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/gyro/adis16260.h
+++ drivers/staging/iio/gyro/adis16260.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef SPI_ADIS16260_H_
 #define SPI_ADIS16260_H_
 
-#include "adis16260_platform_data.h"
 #include <linux/iio/imu/adis.h>
 
 #define ADIS16260_STARTUP_DELAY	220 /* ms */
