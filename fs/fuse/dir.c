53b1ff508226bc529cee75b4dc6172caf325fee9
Miklos Szeredi <mszeredi@suse.cz>
2013-07-17 14:53:54 +0200

    fuse: readdirplus: cleanup
    Niels noted that we don't need the 'dentry = NULL' line.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    CC: Niels de Vos <ndevos@redhat.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1264,7 +1264,6 @@ static int fuse_direntplus_link(struct file *file,
 			goto found;
 		}
 		dput(dentry);
-		dentry = NULL;
 	}
 
 	dentry = d_alloc(parent, &name);
@@ -1300,8 +1299,7 @@ found:
 
 	err = 0;
 out:
-	if (dentry)
-		dput(dentry);
+	dput(dentry);
 	return err;
 }
 
