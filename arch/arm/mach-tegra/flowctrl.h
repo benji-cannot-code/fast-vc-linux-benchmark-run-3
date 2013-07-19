ff758da5029e23ae51bb4f9dd708f123fb0c938d
Joseph Lo <josephl@nvidia.com>
2013-07-19 17:25:25 +0800

    ARM: tegra114: add low level support for CPU idle powered-down mode
    The flow controller would take care the power sequence when CPU idle in
    powered-down mode. It powered gate the CPU when CPU runs into WFI
    instruction. And wake up the CPU when event be triggered.
    
    The sequence is below.
    * setting wfi bitmap for the CPU as the halt event in the
      FLOW_CTRL_CPU_HALT_REG to monitor the CPU running into WFI,then power
      gate it
    * setting IRQ and FIQ as wake up event to wake up CPU when event triggered
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/flowctrl.h
+++ arch/arm/mach-tegra/flowctrl.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FLOW_CTRL_SCLK_RESUME		(1 << 27)
 #define FLOW_CTRL_HALT_CPU_IRQ		(1 << 10)
 #define	FLOW_CTRL_HALT_CPU_FIQ		(1 << 8)
+#define FLOW_CTRL_HALT_GIC_IRQ		(1 << 9)
+#define FLOW_CTRL_HALT_GIC_FIQ		(1 << 8)
 #define FLOW_CTRL_CPU0_CSR		0x8
 #define	FLOW_CTRL_CSR_INTR_FLAG		(1 << 15)
 #define FLOW_CTRL_CSR_EVENT_FLAG	(1 << 14)
