9623734b90e1c2aeee4776531425f154c465be2f
Al Viro <viro@zeniv.linux.org.uk>
2013-07-19 16:13:30 +0400

    oprofile: get rid of pointless forward declarations of struct super_block
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/oprofile.h
+++ include/linux/oprofile.h
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IBS_FETCH_CODE			13
 #define IBS_OP_CODE			14
 
-struct super_block;
 struct dentry;
 struct file_operations;
 struct pt_regs;
