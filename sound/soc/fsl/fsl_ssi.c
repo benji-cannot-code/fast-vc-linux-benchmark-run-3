fc882ddf22f3fb5a2856b67e821ceb5fc1cc63b8
Mark Brown <broonie@linaro.org>
2013-07-19 11:40:13 +0100

    ASoC: fsl_ssi: Provide register I/O functions by default
    Use the ARM version by default as that's the more generally portable
    one, it doesn't matter if they work well on random platforms when the
    goal is only build coverage.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Acked-by: Timur Tabi <timur@tabi.org>

--- sound/soc/fsl/fsl_ssi.c
+++ sound/soc/fsl/fsl_ssi.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define read_ssi(addr)			 in_be32(addr)
 #define write_ssi(val, addr)		 out_be32(addr, val)
 #define write_ssi_mask(addr, clear, set) clrsetbits_be32(addr, clear, set)
-#elif defined ARM
+#else
 #define read_ssi(addr)			 readl(addr)
 #define write_ssi(val, addr)		 writel(val, addr)
 /*
