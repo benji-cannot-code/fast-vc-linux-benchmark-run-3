70e7fdbe5a48331aa80ebe7f22dc059b7c210906
Patil, Rachna <rachna@ti.com>
2013-07-20 17:27:35 +0100

    MFD: ti_tscadc: ADC Clock check not required
    ADC is ideally expected to work at a frequency of 3MHz.
    The present code had a check, which returned error if the frequency
    went below the threshold  value. But since AM335x supports various
    working frequencies, this check is not required.
    Now the code just uses the internal ADC clock divider to set the ADC
    frequency w.r.t the sys clock.
    
    Signed-off-by: Patil, Rachna <rachna@ti.com>
    Signed-off-by: Zubair Lutfullah <zubair.lutfullah@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/mfd/ti_am335x_tscadc.h
+++ include/linux/mfd/ti_am335x_tscadc.h
@@ -115,7 +115,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CNTRLREG_TSCENB		BIT(7)
 
 #define ADC_CLK			3000000
-#define	MAX_CLK_DIV		7
 #define TOTAL_STEPS		16
 #define TOTAL_CHANNELS		8
 
