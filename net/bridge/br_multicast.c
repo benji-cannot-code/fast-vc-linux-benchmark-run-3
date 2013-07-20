eb149c98e57a529c2ee7352b11c2e065d6dfa531
Eric Dumazet <edumazet@google.com>
2013-07-19 20:07:16 -0700

    bridge: do not call setup_timer() multiple times
    commit 9f00b2e7cf24 ("bridge: only expire the mdb entry when query is
    received") added a nasty bug as an active timer can be reinitialized.
    
    setup_timer() must be done once, no matter how many time mod_timer()
    is called. br_multicast_new_group() is the right place to do this.
    
    Reported-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Diagnosed-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Tested-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Cc: Cong Wang <amwang@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -620,6 +620,9 @@ rehash:
 	mp->br = br;
 	mp->addr = *group;
 
+	setup_timer(&mp->timer, br_multicast_group_expired,
+		    (unsigned long)mp);
+
 	hlist_add_head_rcu(&mp->hlist[mdb->ver], &mdb->mhash[hash]);
 	mdb->size++;
 
@@ -1127,7 +1130,6 @@ static int br_ip4_multicast_query(struct net_bridge *br,
 	if (!mp)
 		goto out;
 
-	setup_timer(&mp->timer, br_multicast_group_expired, (unsigned long)mp);
 	mod_timer(&mp->timer, now + br->multicast_membership_interval);
 	mp->timer_armed = true;
 
@@ -1205,7 +1207,6 @@ static int br_ip6_multicast_query(struct net_bridge *br,
 	if (!mp)
 		goto out;
 
-	setup_timer(&mp->timer, br_multicast_group_expired, (unsigned long)mp);
 	mod_timer(&mp->timer, now + br->multicast_membership_interval);
 	mp->timer_armed = true;
 
