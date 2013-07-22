11617024ddd8edeadaff87ab520093330f3ef483
Elen Song <elen.song@atmel.com>
2013-07-22 16:30:29 +0800

    serial: at91: distinguish usart and uart
    Distinguish usart and uart by read ip name register,
    The usart read name is "USAR",
    The uart and dbgu read name is "DBGU".
    
    Signed-off-by: Elen Song <elen.song@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/atmel_serial.h
+++ include/linux/atmel_serial.h
@@ -125,4 +125,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ATMEL_US_NER		0x44			/* Number of Errors Register */
 #define ATMEL_US_IF		0x4c			/* IrDA Filter Register */
 
+#define ATMEL_US_NAME		0xf0			/* Ip Name */
+
 #endif
