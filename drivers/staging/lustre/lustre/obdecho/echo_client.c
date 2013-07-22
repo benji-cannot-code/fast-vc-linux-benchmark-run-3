5f147b4983734fc932a20b884ab7bc740718d0c5
Liu Xuezhao <xuezhao.liu@emc.com>
2013-07-23 00:06:54 +0800

    staging/lustre/fld: prepare FLD module for client server split
    Split FLD server from client, fld_{handler,index}.c are not compliled
    unless server support is enabled.  Do not include dt_object.h or
    lustre_mdt.h in lustre_fld.h and fix the minor breakages caused by
    this elsewhere.  Generally cleanup includes in lustre/fld.
    
    Signed-off-by: Liu Xuezhao <xuezhao.liu@emc.com>
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-1330
    Lustre-change: http://review.whamcloud.com/2675
    Reviewed-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Peng Tao <tao.peng@emc.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdecho/echo_client.c
+++ drivers/staging/lustre/lustre/obdecho/echo_client.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <lustre_debug.h>
 #include <lprocfs_status.h>
 #include <cl_object.h>
+#include <md_object.h>
 #include <lustre_fid.h>
 #include <lustre_acl.h>
 #include <lustre_net.h>
