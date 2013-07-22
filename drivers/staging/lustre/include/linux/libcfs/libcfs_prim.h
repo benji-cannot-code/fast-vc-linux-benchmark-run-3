f4892deb78f4deff1bb8d0c9e7d2eb6a6fa31d49
Peng Tao <bergwolf@gmail.com>
2013-07-23 00:06:22 +0800

    staging/lustre: remove bogus ifndef EXPORT_SYMBOL
    Signed-off-by: Peng Tao <tao.peng@emc.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/libcfs_prim.h
+++ drivers/staging/lustre/include/linux/libcfs/libcfs_prim.h
@@ -41,10 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LIBCFS_PRIM_H__
 #define __LIBCFS_PRIM_H__
 
-#ifndef EXPORT_SYMBOL
-# define EXPORT_SYMBOL(s)
-#endif
-
 /*
  * Schedule
  */
