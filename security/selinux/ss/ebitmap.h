993522888b0cdf39e47633a232ec8e9d1fa1e445
Waiman Long <Waiman.Long@hp.com>
2013-07-23 17:38:41 -0400

    SELinux: Increase ebitmap_node size for 64-bit configuration
    Currently, the ebitmap_node structure has a fixed size of 32 bytes. On
    a 32-bit system, the overhead is 8 bytes, leaving 24 bytes for being
    used as bitmaps. The overhead ratio is 1/4.
    
    On a 64-bit system, the overhead is 16 bytes. Therefore, only 16 bytes
    are left for bitmap purpose and the overhead ratio is 1/2. With a
    3.8.2 kernel, a boot-up operation will cause the ebitmap_get_bit()
    function to be called about 9 million times. The average number of
    ebitmap_node traversal is about 3.7.
    
    This patch increases the size of the ebitmap_node structure to 64
    bytes for 64-bit system to keep the overhead ratio at 1/4. This may
    also improve performance a little bit by making node to node traversal
    less frequent (< 2) as more bits are available in each node.
    
    Signed-off-by: Waiman Long <Waiman.Long@hp.com>
    Acked-by:  Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <pmoore@redhat.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- security/selinux/ss/ebitmap.h
+++ security/selinux/ss/ebitmap.h
@@ -17,7 +17,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <net/netlabel.h>
 
-#define EBITMAP_UNIT_NUMS	((32 - sizeof(void *) - sizeof(u32))	\
+#ifdef CONFIG_64BIT
+#define	EBITMAP_NODE_SIZE	64
+#else
+#define	EBITMAP_NODE_SIZE	32
+#endif
+
+#define EBITMAP_UNIT_NUMS	((EBITMAP_NODE_SIZE-sizeof(void *)-sizeof(u32))\
 					/ sizeof(unsigned long))
 #define EBITMAP_UNIT_SIZE	BITS_PER_LONG
 #define EBITMAP_SIZE		(EBITMAP_UNIT_NUMS * EBITMAP_UNIT_SIZE)
