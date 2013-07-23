b92825133326048a6b4c67ba29d38f15cd60c03b
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-07-23 23:08:10 +0200

    sh-pfc: Remove unneeded mach/<soc>.h includes
    The SoC-specific mach/<soc>.h headers are included needlesly. Don't
    include them.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Acked-by: Magnus Damm <damm@opensource.se>

--- drivers/pinctrl/sh-pfc/pfc-sh7372.c
+++ drivers/pinctrl/sh-pfc/pfc-sh7372.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pinctrl/pinconf-generic.h>
 
 #include <mach/irqs.h>
-#include <mach/sh7372.h>
 
 #include "core.h"
 #include "sh_pfc.h"
