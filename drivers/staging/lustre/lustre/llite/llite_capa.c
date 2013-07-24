7506051f0d1dfde62e9fd7132714832902f0aef3
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:33 +0530

    staging: lustre: Remove version.h header inclusion in llite_capa.c
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_capa.c
+++ drivers/staging/lustre/lustre/llite/llite_capa.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUG_SUBSYSTEM S_LLITE
 
 #include <linux/fs.h>
-#include <linux/version.h>
 #include <asm/uaccess.h>
 #include <linux/file.h>
 #include <linux/kmod.h>
