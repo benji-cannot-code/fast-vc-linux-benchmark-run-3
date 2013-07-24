af079439ca8ee688f6923cb07ee05bd1ba1ca92c
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:47 +0530

    staging: lustre: Remove version.h header inclusion in linux-sysctl.c
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/linux/linux-sysctl.c
+++ drivers/staging/lustre/lustre/obdclass/linux/linux-sysctl.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/mm.h>
 #include <linux/sysctl.h>
-#include <linux/version.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/stat.h>
