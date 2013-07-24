73624ff99fa9b45ce44ee83578cec30649a24a13
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:34 +0530

    staging: lustre: Remove version.h header inclusion in llite_lib.c
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_lib.c
+++ drivers/staging/lustre/lustre/llite/llite_lib.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/types.h>
-#include <linux/version.h>
 #include <linux/mm.h>
 
 #include <lustre_lite.h>
