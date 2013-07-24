175803decfcf5fbd33cd29b6072c50e43717dd2d
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:46 +0530

    staging: lustre: Remove version.h header inclusion in capa.c
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/capa.c
+++ drivers/staging/lustre/lustre/obdclass/capa.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DEBUG_SUBSYSTEM S_SEC
 
-#include <linux/version.h>
 #include <linux/fs.h>
 #include <asm/unistd.h>
 #include <linux/slab.h>
