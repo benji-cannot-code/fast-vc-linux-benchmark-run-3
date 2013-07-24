005364ab1eead37dc6e94be574189cd7c0a7698c
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:32 +0530

    staging: lustre: Remove version.h header inclusion in dir.c
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/dir.c
+++ drivers/staging/lustre/lustre/llite/dir.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/pagemap.h>
 #include <linux/mm.h>
-#include <linux/version.h>
 #include <asm/uaccess.h>
 #include <linux/buffer_head.h>   // for wait_on_buffer
 #include <linux/pagevec.h>
