4ce60e6e519c5d07dadd698e3a884000fd734d63
Stephen Boyd <sboyd@codeaurora.org>
2013-07-24 13:54:27 -0700

    ARM: msm: Don't compile __msm_ioremap_caller() unless used
    This function is only used on MSM7x00A so only compile the
    function if the build includes support for that platform.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- arch/arm/mach-msm/io.c
+++ arch/arm/mach-msm/io.c
@@ -169,6 +169,7 @@ void __init msm_map_msm7x30_io(void)
 }
 #endif /* CONFIG_ARCH_MSM7X30 */
 
+#ifdef CONFIG_ARCH_MSM7X00A
 void __iomem *__msm_ioremap_caller(phys_addr_t phys_addr, size_t size,
 				   unsigned int mtype, void *caller)
 {
@@ -183,3 +184,4 @@ void __iomem *__msm_ioremap_caller(phys_addr_t phys_addr, size_t size,
 
 	return __arm_ioremap_caller(phys_addr, size, mtype, caller);
 }
+#endif
