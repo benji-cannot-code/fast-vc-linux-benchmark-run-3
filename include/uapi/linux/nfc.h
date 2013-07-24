8ae3db530b556dd99fc3b8669351ba3bdb398a72
Samuel Ortiz <sameo@linux.intel.com>
2013-07-24 18:10:50 +0200

    NFC: Add a GET_SE netlink API
    In order to fetch the discovered secure elements from an NFC controller,
    we need to send a netlink command that will dump the list of available
    SEs from NFC.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/uapi/linux/nfc.h
+++ include/uapi/linux/nfc.h
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @NFC_EVENT_SE_TRANSACTION: This event is sent when an application running on
  *	a specific SE notifies us about the end of a transaction. The parameter
  *	for this event is the application ID (AID).
+ * @NFC_CMD_GET_SE: Dump all discovered secure elements from an NFC controller.
  */
 enum nfc_commands {
 	NFC_CMD_UNSPEC,
@@ -113,6 +114,7 @@ enum nfc_commands {
 	NFC_EVENT_SE_REMOVED,
 	NFC_EVENT_SE_CONNECTIVITY,
 	NFC_EVENT_SE_TRANSACTION,
+	NFC_CMD_GET_SE,
 /* private: internal use only */
 	__NFC_CMD_AFTER_LAST
 };
