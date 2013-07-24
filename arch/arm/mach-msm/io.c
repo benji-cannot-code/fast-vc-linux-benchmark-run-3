1928a99fad6eb08a31900d1b60f5fb929ddf421f
Stephen Boyd <sboyd@codeaurora.org>
2013-07-24 13:54:31 -0700

    ARM: msm: Move mach/board.h contents to common.h
    The contents of mach/board.h are only used by files within
    mach-msm so there is no need to export this file outside of the
    mach-msm directory. Move the contents of the file to common.h to
    allow us to compile MSM in the multi-platform kernel.
    
    Cc: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Acked-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- arch/arm/mach-msm/io.c
+++ arch/arm/mach-msm/io.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/kernel.h>
+#include <linux/bug.h>
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/export.h>
@@ -27,8 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/page.h>
 #include <mach/msm_iomap.h>
 #include <asm/mach/map.h>
-
-#include <mach/board.h>
 
 #include "common.h"
 
