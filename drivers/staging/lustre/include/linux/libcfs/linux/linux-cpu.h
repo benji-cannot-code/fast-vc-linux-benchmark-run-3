9f16496c7bdbe48a7279eb3e60f6203d25b56cd5
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:21 +0530

    staging: lustre: Remove version.h header inclusion in linux-cpu.h
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/linux-cpu.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/linux-cpu.h
@@ -47,8 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/cpuset.h>
 #include <linux/topology.h>
-#include <linux/version.h>
-
 
 #ifdef CONFIG_SMP
 
