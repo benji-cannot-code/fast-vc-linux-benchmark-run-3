7e9a526e05e19bcfb6ce0f0c452907bc98026762
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:37 +0530

    staging: lustre: Remove version.h header inclusion in remote_perm.c
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/remote_perm.c
+++ drivers/staging/lustre/lustre/llite/remote_perm.c
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/types.h>
-#include <linux/version.h>
 
 #include <lustre_lite.h>
 #include <lustre_ha.h>
