a8e62c226450e6a3f1ba4bcdb1e182007b5327aa
Magnus Damm <damm@opensource.se>
2013-07-24 12:38:20 +0900

    ARM: shmobile: Remove Marzen use of <mach/hardware.h>
    Remove unused <mach/hardware.h> on Marzen.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-marzen.c
+++ arch/arm/mach-shmobile/board-marzen.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/host.h>
 #include <linux/mmc/sh_mobile_sdhi.h>
 #include <linux/mfd/tmio.h>
-#include <mach/hardware.h>
 #include <mach/r8a7779.h>
 #include <mach/common.h>
 #include <mach/irqs.h>
