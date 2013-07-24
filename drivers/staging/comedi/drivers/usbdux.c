39ea40ae0acdb71fe22c5848ea501ea4ff869c31
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-07-24 14:06:58 -0700

    staging: comedi: usbdux: remove the usb_driver (*probe) noise
    The dev_dbg() during the usb_driver (*probe) is just added noise.
    
    The dev_err() when a usb_alloc_urb() fails is not necessary. The
    allocation failure will have already output a message.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/usbdux.c
+++ drivers/staging/comedi/drivers/usbdux.c
@@ -2300,9 +2300,6 @@ static int usbdux_usb_probe(struct usb_interface *uinterf,
 	int i;
 	int index;
 
-	dev_dbg(dev, "comedi_: usbdux_: "
-		"finding a free structure for the usb-device\n");
-
 	down(&start_stop_sem);
 	/* look for a free place in the usbdux array */
 	index = -1;
@@ -2319,8 +2316,6 @@ static int usbdux_usb_probe(struct usb_interface *uinterf,
 		up(&start_stop_sem);
 		return -EMFILE;
 	}
-	dev_dbg(dev, "comedi_: usbdux: "
-		"usbduxsub[%d] is ready to connect to comedi.\n", index);
 
 	sema_init(&(usbduxsub[index].sem), 1);
 	/* save a pointer to the usb device */
@@ -2333,8 +2328,6 @@ static int usbdux_usb_probe(struct usb_interface *uinterf,
 	/* hand the private data over to the usb subsystem */
 	/* will be needed for disconnect */
 	usb_set_intfdata(uinterf, &(usbduxsub[index]));
-
-	dev_dbg(dev, "comedi_: usbdux: ifnum=%d\n", usbduxsub[index].ifnum);
 
 	/* test if it is high speed (USB 2.0) */
 	usbduxsub[index].high_speed =
@@ -2403,8 +2396,6 @@ static int usbdux_usb_probe(struct usb_interface *uinterf,
 		/* one frame: 1ms */
 		usbduxsub[index].urb_in[i] = usb_alloc_urb(1, GFP_KERNEL);
 		if (usbduxsub[index].urb_in[i] == NULL) {
-			dev_err(dev, "comedi_: usbdux%d: "
-				"Could not alloc. urb(%d)\n", index, i);
 			tidy_up(&(usbduxsub[index]));
 			up(&start_stop_sem);
 			return -ENOMEM;
@@ -2448,8 +2439,6 @@ static int usbdux_usb_probe(struct usb_interface *uinterf,
 		/* one frame: 1ms */
 		usbduxsub[index].urb_out[i] = usb_alloc_urb(1, GFP_KERNEL);
 		if (usbduxsub[index].urb_out[i] == NULL) {
-			dev_err(dev, "comedi_: usbdux%d: "
-				"Could not alloc. urb(%d)\n", index, i);
 			tidy_up(&(usbduxsub[index]));
 			up(&start_stop_sem);
 			return -ENOMEM;
@@ -2489,8 +2478,6 @@ static int usbdux_usb_probe(struct usb_interface *uinterf,
 		usbduxsub[index].size_pwm_buf = 512;
 		usbduxsub[index].urb_pwm = usb_alloc_urb(0, GFP_KERNEL);
 		if (usbduxsub[index].urb_pwm == NULL) {
-			dev_err(dev, "comedi_: usbdux%d: "
-				"Could not alloc. pwm urb\n", index);
 			tidy_up(&(usbduxsub[index]));
 			up(&start_stop_sem);
 			return -ENOMEM;
