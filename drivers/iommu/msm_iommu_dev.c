972fa086a44fef7bfd00496ec0fa01e35f21199b
Stephen Boyd <sboyd@codeaurora.org>
2013-07-24 13:54:33 -0700

    iommu/msm: Move mach includes to iommu directory
    Two header files exist in mach-msm's include/mach directory that
    are only used by the MSM iommu driver. Move these files to the
    iommu driver directory and prefix them with "msm_". This allows
    us to compile the MSM iommu driver on multi-platform kernels.
    
    Acked-by: Joerg Roedel <joro@8bytes.org>
    Cc: Stepan Moskovchenko <stepanm@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- drivers/iommu/msm_iommu_dev.c
+++ drivers/iommu/msm_iommu_dev.c
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/slab.h>
 
-#include <mach/iommu_hw-8xxx.h>
-#include <mach/iommu.h>
+#include "msm_iommu_hw-8xxx.h"
+#include "msm_iommu.h"
 
 struct iommu_ctx_iter_data {
 	/* input */
