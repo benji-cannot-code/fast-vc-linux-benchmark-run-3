d031d3e26ace508d25df74f0d9332ed4fc795619
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-24 12:06:20 +0530

    staging: lustre: Remove version.h header inclusion in kp30.h
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/kp30.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/kp30.h
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/highmem.h>
 #include <linux/module.h>
-#include <linux/version.h>
 #include <asm/atomic.h>
 #include <asm/uaccess.h>
 #include <linux/rwsem.h>
