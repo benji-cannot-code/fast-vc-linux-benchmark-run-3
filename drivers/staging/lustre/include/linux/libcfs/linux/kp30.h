544b4f5508fac2cc52177d3edda4a60b0f2345a2
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-07-24 10:11:47 -0700

    staging: lustre: don't use time() or CFS_CURRENT_TIME
    It's just "CURRENT_TIME", don't redefine a macro for something so simple
    as that...
    
    Cc: Peng Tao <tao.peng@emc.com>
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/kp30.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/kp30.h
@@ -83,10 +83,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CFS_SYSFS_MODULE_PARM  1 /* module parameters accessible via sysfs */
 
 /******************************************************************************/
-
-# define time(a) CURRENT_TIME
-
-/******************************************************************************/
 /* Light-weight trace
  * Support for temporary event tracing with minimal Heisenberg effect. */
 #define LWT_SUPPORT  0
