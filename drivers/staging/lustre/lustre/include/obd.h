675fc4aa1c09256b22cac78bd3d25ca8b01ed143
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 12:27:14 +0530

    staging: lustre: obd: Remove duplicate inclusion of header file
    Removed the header file included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/obd.h
+++ drivers/staging/lustre/lustre/include/obd.h
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <lustre_lib.h>
 #include <linux/libcfs/bitmap.h>
 #include <lu_ref.h>
-#include <lustre_lib.h>
 #include <lustre_export.h>
 #include <lustre_fid.h>
 #include <lustre_fld.h>
