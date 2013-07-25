e5b3dc3dfdf99b2e4a6669226f66698199322100
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 12:27:16 +0530

    staging: lustre: libcfs: Remove duplicate inclusion of header file
    Removed the header files included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/libcfs/linux/linux-proc.c
+++ drivers/staging/lustre/lustre/libcfs/linux/linux-proc.c
@@ -55,9 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/fs.h>
 #include <linux/file.h>
-#include <linux/stat.h>
 #include <linux/list.h>
-#include <asm/uaccess.h>
 
 #include <linux/proc_fs.h>
 #include <linux/sysctl.h>
