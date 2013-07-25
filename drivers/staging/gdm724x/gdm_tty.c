fc118d23dec86240a830bca9ed1b766ae6278671
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 11:36:52 +0530

    staging: gdm724x: Remove version.h header inclusion in gdm_tty.c
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm724x/gdm_tty.c
+++ drivers/staging/gdm724x/gdm_tty.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/init.h>
