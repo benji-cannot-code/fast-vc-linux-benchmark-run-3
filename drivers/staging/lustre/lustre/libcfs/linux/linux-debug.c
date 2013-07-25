51452ffa8637029fffdec363d1453a5ca6479fa7
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 12:27:15 +0530

    staging: lustre: linux-debug: Remove duplicate inclusion of header file
    Removed the header files included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/libcfs/linux/linux-debug.c
+++ drivers/staging/lustre/lustre/libcfs/linux/linux-debug.c
@@ -49,11 +49,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/unistd.h>
 #include <linux/interrupt.h>
-#include <asm/uaccess.h>
 #include <linux/completion.h>
-
 #include <linux/fs.h>
-#include <linux/stat.h>
 #include <asm/uaccess.h>
 #include <linux/miscdevice.h>
 
