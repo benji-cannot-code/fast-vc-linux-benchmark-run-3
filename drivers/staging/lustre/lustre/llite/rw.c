712bc7ddcbb184eb5cd85966439c2e6afa656c21
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 12:27:21 +0530

    staging: lustre: Remove duplicate header file inclusion in rw.c
    Removed the header files included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/rw.c
+++ drivers/staging/lustre/lustre/llite/rw.c
@@ -49,9 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 
 #include <linux/fs.h>
-#include <linux/stat.h>
-#include <asm/uaccess.h>
-#include <linux/mm.h>
 #include <linux/pagemap.h>
 /* current_is_kswapd() */
 #include <linux/swap.h>
