b2e47dcd90c88235d4f97989254f65604999fa4f
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 12:27:19 +0530

    staging: lustre: Remove duplicate header file inclusion in llite_mmap.c
    Removed the header files included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_mmap.c
+++ drivers/staging/lustre/lustre/llite/llite_mmap.c
@@ -44,9 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 
 #include <linux/fs.h>
-#include <linux/stat.h>
-#include <asm/uaccess.h>
-#include <linux/mm.h>
 #include <linux/pagemap.h>
 
 #define DEBUG_SUBSYSTEM S_LLITE
