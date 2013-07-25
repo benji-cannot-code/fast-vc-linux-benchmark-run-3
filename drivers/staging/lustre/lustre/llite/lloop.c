f97cc826a4fefe571d733980b72415d86943f470
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 12:27:20 +0530

    staging: lustre: Remove duplicate header file inclusion in lloop.c
    Removed the header files included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/lloop.c
+++ drivers/staging/lustre/lustre/llite/lloop.c
@@ -100,7 +100,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/completion.h>
 #include <linux/highmem.h>
 #include <linux/gfp.h>
-#include <linux/swap.h>
 #include <linux/pagevec.h>
 
 #include <asm/uaccess.h>
