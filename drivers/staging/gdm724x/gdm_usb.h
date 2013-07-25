aa638b23c47874738b299398928cdf042532aa5f
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 11:36:55 +0530

    staging: gdm724x: Remove version.h header inclusion in gdm_usb.h
    version.h header inclusion is not necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm724x/gdm_usb.h
+++ drivers/staging/gdm724x/gdm_usb.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _GDM_USB_H_
 #define _GDM_USB_H_
 
-#include <linux/version.h>
 #include <linux/types.h>
 #include <linux/usb.h>
 #include <linux/list.h>
