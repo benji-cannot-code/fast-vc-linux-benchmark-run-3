9e07b187563676ef399691ac9335892c9dda4adf
Santosh Shilimkar <santosh.shilimkar@ti.com>
2013-07-25 16:59:25 -0400

    i2c: davinci: remove useless mach/hardware include
    This driver no longer uses definitions from mach/hardware.h.
    On the other hand, including this header breaks this driver
    on non-davinci platforms which don't have such a header.
    
    Acked-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-davinci.c
+++ drivers/i2c/busses/i2c-davinci.c
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/of_i2c.h>
 #include <linux/of_device.h>
-
-#include <mach/hardware.h>
 #include <linux/platform_data/i2c-davinci.h>
 
 /* ----- global defines ----------------------------------------------- */
