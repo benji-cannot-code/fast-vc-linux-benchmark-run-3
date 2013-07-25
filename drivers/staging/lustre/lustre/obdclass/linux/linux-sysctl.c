aef59df45bbfb1f2926bcb610a0c8afffccebfeb
Sachin Kamat <sachin.kamat@linaro.org>
2013-07-25 12:27:27 +0530

    staging: lustre: obdclass: Remove duplicate header file inclusion
    Removed the header file included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/linux/linux-sysctl.c
+++ drivers/staging/lustre/lustre/obdclass/linux/linux-sysctl.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysctl.h>
 #include <linux/sched.h>
 #include <linux/mm.h>
-#include <linux/sysctl.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/stat.h>
