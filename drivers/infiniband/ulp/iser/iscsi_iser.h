46e38de60cad7c2bce19a259f37e9934ff1c964e
Or Gerlitz <ogerlitz@mellanox.com>
2013-07-28 12:35:36 +0300

    IB/iser: Use proper debug level value for info prints
    Commit 4f363882612 ("IB/iser: Move informational messages from error
    to info level") set info prints to be emitted at a lower debug level
    than warning prints, which is a bit odd.  Fix that.
    
    Also move the prints on unaligned SG from warning to debug level.
    
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/ulp/iser/iscsi_iser.h
+++ drivers/infiniband/ulp/iser/iscsi_iser.h
@@ -79,14 +79,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define iser_warn(fmt, arg...)				\
 	do {						\
-		if (iser_debug_level > 1)		\
+		if (iser_debug_level > 0)		\
 			pr_warn(PFX "%s:" fmt,          \
 				__func__ , ## arg);	\
 	} while (0)
 
 #define iser_info(fmt, arg...)				\
 	do {						\
-		if (iser_debug_level > 0)		\
+		if (iser_debug_level > 1)		\
 			pr_info(PFX "%s:" fmt,          \
 				__func__ , ## arg);	\
 	} while (0)
