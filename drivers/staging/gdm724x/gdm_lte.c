53afe5819abfbac449e43fcdceda97c80b113565
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-07-29 12:33:24 +0800

    staging: gdm724x: remove duplicated include from gdm_lte.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm724x/gdm_lte.c
+++ drivers/staging/gdm724x/gdm_lte.c
@@ -22,9 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 #include <linux/if_ether.h>
 #include <linux/if_vlan.h>
-#include <linux/in.h>
 #include <linux/in6.h>
-#include <linux/udp.h>
 #include <linux/tcp.h>
 #include <linux/icmp.h>
 #include <linux/icmpv6.h>
