ab00944261b68963d65342d6ebab28660512f8ea
Jon Mason <jon.mason@intel.com>
2013-07-29 16:31:18 -0700

    NTB: client event cleanup
    Provide a better event interface between the client and transport
    
    Signed-off-by: Jon Mason <jon.mason@intel.com>

--- drivers/ntb/ntb_transport.c
+++ drivers/ntb/ntb_transport.c
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/slab.h>
 #include <linux/types.h>
-#include <linux/ntb.h>
 #include "ntb_hw.h"
 
 #define NTB_TRANSPORT_VERSION	3
