b2b34821f843d954445f8c8f0a3360c39a10d288
Arron Wang <arron.wang@intel.com>
2013-07-30 14:35:35 +0200

    NFC: Fix secure element state check
    Another typo from the initial commit where we check for the secure
    element type field instead of its state when enabling or disabling it.
    
    Signed-off-by: Arron Wang <arron.wang@intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- net/nfc/core.c
+++ net/nfc/core.c
@@ -584,7 +584,7 @@ int nfc_enable_se(struct nfc_dev *dev, u32 se_idx)
 		goto error;
 	}
 
-	if (se->type == NFC_SE_ENABLED) {
+	if (se->state == NFC_SE_ENABLED) {
 		rc = -EALREADY;
 		goto error;
 	}
@@ -627,7 +627,7 @@ int nfc_disable_se(struct nfc_dev *dev, u32 se_idx)
 		goto error;
 	}
 
-	if (se->type == NFC_SE_DISABLED) {
+	if (se->state == NFC_SE_DISABLED) {
 		rc = -EALREADY;
 		goto error;
 	}
