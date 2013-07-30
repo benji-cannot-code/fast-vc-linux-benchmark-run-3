708f9dfa85adc045d88dcf121bf2f594a2e05e3a
Chen Gang <gang.chen@asianux.com>
2013-07-30 15:30:05 +0800

    include/linux/coda.h: remove useless '#else'
    '#else' is useless, need remove.
    
    Signed-off-by: Chen Gang <gang.chen@asianux.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- include/linux/coda.h
+++ include/linux/coda.h
@@ -61,7 +61,6 @@ Mellon the rights to redistribute these changes without encumbrance.
 
 #if defined(__linux__)
 typedef unsigned long long u_quad_t;
-#else
 #endif
 #include <uapi/linux/coda.h>
 #endif 
