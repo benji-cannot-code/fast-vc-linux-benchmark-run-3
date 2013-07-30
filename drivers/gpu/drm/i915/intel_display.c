06dd2114dd2e9093b1b837702677677476bc0e7c
Jani Nikula <jani.nikula@intel.com>
2013-07-30 12:20:32 +0300

    drm/i915: move encoder->enable callback later in VLV crtc enable
    VLV wants encoder enabling before the pipe is up. With the previously
    rearranged VLV DP and HDMI ->pre_enable and ->enable callbacks in place,
    this no longer depends on the early ->enable hook call. Move the
    ->enable call at the end of the sequence, similar to the crtc enable on
    other platforms. This will be needed e.g. for moving the eDP backlight
    enabling to the right place in the sequence, currently done too early on
    VLV.
    
    There should be no functional changes.
    
    v2: Rebase.
    
    v3: Explain why this is needed in the commit message (Chris).
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3663,10 +3663,6 @@ static void valleyview_crtc_enable(struct drm_crtc *crtc)
 		if (encoder->pre_enable)
 			encoder->pre_enable(encoder);
 
-	/* VLV wants encoder enabling _before_ the pipe is up. */
-	for_each_encoder_on_crtc(dev, crtc, encoder)
-		encoder->enable(encoder);
-
 	i9xx_pfit_enable(intel_crtc);
 
 	intel_crtc_load_lut(crtc);
@@ -3677,6 +3673,9 @@ static void valleyview_crtc_enable(struct drm_crtc *crtc)
 	intel_crtc_update_cursor(crtc, true);
 
 	intel_update_fbc(dev);
+
+	for_each_encoder_on_crtc(dev, crtc, encoder)
+		encoder->enable(encoder);
 }
 
 static void i9xx_crtc_enable(struct drm_crtc *crtc)
