b68bf94f8c95268303eb6c2d251b62d4d204a187
hayeswang <hayeswang@realtek.com>
2013-07-31 17:21:24 +0800

    net/usb/r815x: change the return value for bind functions
    Replace 0 with the result from usbnet_cdc_bind().
    
    Signed-off-by: Hayes Wang <hayeswang@realtek.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/r815x.c
+++ drivers/net/usb/r815x.c
@@ -173,7 +173,7 @@ static int r8153_bind(struct usbnet *dev, struct usb_interface *intf)
 	dev->mii.phy_id = R815x_PHY_ID;
 	dev->mii.supports_gmii = 1;
 
-	return 0;
+	return status;
 }
 
 static int r8152_bind(struct usbnet *dev, struct usb_interface *intf)
@@ -192,7 +192,7 @@ static int r8152_bind(struct usbnet *dev, struct usb_interface *intf)
 	dev->mii.phy_id = R815x_PHY_ID;
 	dev->mii.supports_gmii = 0;
 
-	return 0;
+	return status;
 }
 
 static const struct driver_info r8152_info = {
