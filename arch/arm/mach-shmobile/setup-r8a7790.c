e39c2d2bfd674885100c23b06ef3d8133055a75c
Magnus Damm <damm@opensource.se>
2013-08-01 04:03:00 +0900

    ARM: shmobile: Use clocksource_of_init() on r8a7790
    Replace the call to shmobile_timer_init() with
    clocksource_of_init(). This will allow us to
    get rid of shmobile_timer_init().
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    [horms+renesas@verge.net.au: include linux/clocksource.h]
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7790.c
+++ arch/arm/mach-shmobile/setup-r8a7790.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <linux/clocksource.h>
 #include <linux/irq.h>
 #include <linux/kernel.h>
 #include <linux/of_platform.h>
@@ -264,7 +265,7 @@ void __init r8a7790_timer_init(void)
 	iounmap(base);
 #endif /* CONFIG_ARM_ARCH_TIMER */
 
-	shmobile_timer_init();
+	clocksource_of_init();
 }
 
 void __init r8a7790_init_delay(void)
