94324614df0f26dd27a91c1cfd784dac3452c3cf
Chris Metcalf <cmetcalf@tilera.com>
2013-08-01 15:52:17 -0400

    tile: optimize and clean up string functions
    This change cleans up the string code in a number of ways:
    
    - For memcpy(), fix bug in prefetch and increase distance to 3 lines;
      optimize for unaligned data; do all loads before wh64 to make memcpy
      safe for forward-overlapping calls; etc.  Performance is improved.
    
    - Use new copy_byte() function on tilegx to spread a single byte value
      out into a full word using the shufflebytes instruction.
    
    - Clean up header include ordering to be more canonical, and remove
      spurious #undefs of function names.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- arch/tile/lib/memset_32.c
+++ arch/tile/lib/memset_32.c
@@ -13,13 +13,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   more details.
  */
 
-#include <arch/chip.h>
-
 #include <linux/types.h>
 #include <linux/string.h>
 #include <linux/module.h>
-
-#undef memset
+#include <arch/chip.h>
 
 void *memset(void *s, int c, size_t n)
 {
--- arch/tile/lib/strlen_32.c
+++ arch/tile/lib/strlen_32.c
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/module.h>
 
-#undef strlen
-
 size_t strlen(const char *s)
 {
 	/* Get an aligned pointer. */
