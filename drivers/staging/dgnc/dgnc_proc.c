d483e4db11dafe38aa1149c0c8189561c472c375
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-02 14:45:26 +0530

    staging: dgnc: Remove duplicate header file inclusion
    sched.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_proc.c
+++ drivers/staging/dgnc/dgnc_proc.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/proc_fs.h>
 #include <linux/serial_reg.h>
-#include <linux/sched.h>		/* For in_egroup_p() */
 #include <linux/string.h>
 #include <asm/uaccess.h>		/* For copy_from_user/copy_to_user */
 
