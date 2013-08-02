2312c33806e4378e428196a87b8e968ab989cf3c
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-02 15:25:19 +0800

    Revert "serial: sccnxp: Add DT support"
    This reverts commit 85c996907473e4ef824774b97b26499adf66521f.
    
    Alexander wishes to remove this patch as it is incorrect.
    
    Reported-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/platform_data/serial-sccnxp.h
+++ include/linux/platform_data/serial-sccnxp.h
@@ -79,11 +79,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SCCNXP platform data structure */
 struct sccnxp_pdata {
 	/* Shift for A0 line */
-	u32	reg_shift;
+	const u8		reg_shift;
 	/* Modem control lines configuration */
-	u32	mctrl_cfg[SCCNXP_MAX_UARTS];
+	const u32		mctrl_cfg[SCCNXP_MAX_UARTS];
 	/* Timer value for polling mode (usecs) */
-	u32	poll_time_us;
+	const unsigned int	poll_time_us;
 };
 
 #endif
