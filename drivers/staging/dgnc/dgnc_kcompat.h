c9554a757e198419cf9cb04013837233ab0eb6a9
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-02 14:40:14 +0530

    staging: dgnc: Include version.h header in dgnc_kcompat.h
    version.h header inclusion is necessary as detected by
    versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_kcompat.h
+++ drivers/staging/dgnc/dgnc_kcompat.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DGNC_KCOMPAT_H
 #define __DGNC_KCOMPAT_H
 
+#include <linux/version.h>
+
 # ifndef KERNEL_VERSION
 #  define KERNEL_VERSION(a,b,c)  (((a) << 16) + ((b) << 8) + (c))
 # endif
