4fc516ba2f6c9310eff8e904fdf49c1317000252
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-03 14:00:50 +0800

    staging: lustre: remove cfs_register_sysctl_table macro
    It was a wrapper around register_sysctl_table, so just remove it.
    
    Cc: Peng Tao <tao.peng@emc.com>
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/linux-prim.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/linux-prim.h
@@ -80,8 +80,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 typedef struct ctl_table		ctl_table_t;
 typedef struct ctl_table_header		ctl_table_header_t;
 
-#define cfs_register_sysctl_table(t, a) register_sysctl_table(t)
-
 #define DECLARE_PROC_HANDLER(name)		      \
 static int					      \
 LL_PROC_PROTO(name)				     \
