8056f8e793ce3939816e80f4fd0ad8438449506a
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-04 07:59:19 +0800

    staging: lustre: remove typedef for group_info_t
    Use "struct group_info" like the rest of the kernel, not a typedef.
    
    Cc: Peng Tao <tao.peng@emc.com>
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/idmap.c
+++ drivers/staging/lustre/lustre/obdclass/idmap.c
@@ -60,8 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * groups_search() is copied from linux kernel!
  * A simple bsearch.
  */
-static int lustre_groups_search(group_info_t *group_info,
-				gid_t grp)
+static int lustre_groups_search(const struct group_info *group_info, gid_t grp)
 {
 	int left, right;
 
@@ -85,7 +84,7 @@ static int lustre_groups_search(group_info_t *group_info,
 	return 0;
 }
 
-void lustre_groups_from_list(group_info_t *ginfo, gid_t *glist)
+void lustre_groups_from_list(struct group_info *ginfo, gid_t *glist)
 {
 	int i;
 	int count = ginfo->ngroups;
@@ -104,7 +103,7 @@ EXPORT_SYMBOL(lustre_groups_from_list);
 
 /* groups_sort() is copied from linux kernel! */
 /* a simple shell-metzner sort */
-void lustre_groups_sort(group_info_t *group_info)
+void lustre_groups_sort(struct group_info *group_info)
 {
 	int base, max, stride;
 	int gidsetsize = group_info->ngroups;
@@ -142,7 +141,7 @@ int lustre_in_group_p(struct lu_ucred *mu, gid_t grp)
 	int rc = 1;
 
 	if (grp != mu->uc_fsgid) {
-		group_info_t *group_info = NULL;
+		struct group_info *group_info = NULL;
 
 		if (mu->uc_ginfo || !mu->uc_identity ||
 		    mu->uc_valid == UCRED_OLD)
