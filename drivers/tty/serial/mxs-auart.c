fe263d1b3fb4dc593d12552d11445a0bc5fe67d7
Huang Shijie <b32955@freescale.com>
2013-08-03 10:09:14 -0400

    serial: mxs: enable the DMA only when the RTS/CTS is valid
    The original DMA support works only when RTS/CTS is enabled.
    (see the "e800163 serial: mxs-auart: add the DMA support for mx28")
    But after several patches, DMA support has lost this limit.
    (see the "bcc20f9 serial: mxs-auart: move to use generic DMA helper")
    
    So an UART without the RTS/CTS lines may also enables the DMA support,
    in which case the UART may gets unpredictable results.
    
    This patch adds an optional property for the UART DT node
    which indicates the UART has RTS and CTS lines, and it also means you
    enable the DMA support for this UART.
    
    This patch also adds a macro MXS_AUART_RTSCTS, and uses it to check
    RTS/CTS before we enable the DMA for the UART.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>
    Signed-off-by: Huang Shijie <shijie8@gmail.com>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/mxs-auart.c
+++ drivers/tty/serial/mxs-auart.c
@@ -138,6 +138,7 @@ struct mxs_auart_port {
 #define MXS_AUART_DMA_ENABLED	0x2
 #define MXS_AUART_DMA_TX_SYNC	2  /* bit 2 */
 #define MXS_AUART_DMA_RX_READY	3  /* bit 3 */
+#define MXS_AUART_RTSCTS	4  /* bit 4 */
 	unsigned long flags;
 	unsigned int ctrl;
 	enum mxs_auart_type devtype;
@@ -640,7 +641,8 @@ static void mxs_auart_settermios(struct uart_port *u,
 		 * we can only implement the DMA support for auart
 		 * in mx28.
 		 */
-		if (is_imx28_auart(s) && (s->flags & MXS_AUART_DMA_CONFIG)) {
+		if (is_imx28_auart(s) && (s->flags & MXS_AUART_DMA_CONFIG)
+				&& test_bit(MXS_AUART_RTSCTS, &s->flags)) {
 			if (!mxs_auart_dma_init(s))
 				/* enable DMA tranfer */
 				ctrl2 |= AUART_CTRL2_TXDMAE | AUART_CTRL2_RXDMAE
@@ -1009,6 +1011,9 @@ static int serial_mxs_probe_dt(struct mxs_auart_port *s,
 	s->port.line = ret;
 
 	s->flags |= MXS_AUART_DMA_CONFIG;
+
+	if (of_get_property(np, "fsl,uart-has-rtscts", NULL))
+		set_bit(MXS_AUART_RTSCTS, &s->flags);
 
 	return 0;
 }
