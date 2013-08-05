430f2e891b478a23ac98772117eb8ca410f71ccf
Josh Wu <josh.wu@atmel.com>
2013-08-05 19:14:38 +0800

    mtd: ofpart: add compatible check for child nodes
    In case that the nand device will support some features like Nand Flash
    Controller, we want to make the sub feature as a sub node of nand device.
    
    Use such organization it is easy to enable/disable feature, also it is back
    compatible and more readable.
    
    If the sub-node has a compatible property then it is a driver not partition.
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>
    Acked-by: Brian Norris <computersforpeace@gmail.com>
    Acked-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    [ added a missing newline -Brian ]
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- drivers/mtd/ofpart.c
+++ drivers/mtd/ofpart.c
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/mtd/partitions.h>
 
+static bool node_has_compatible(struct device_node *pp)
+{
+	return of_get_property(pp, "compatible", NULL);
+}
+
 static int parse_ofpart_partitions(struct mtd_info *master,
 				   struct mtd_partition **pparts,
 				   struct mtd_part_parser_data *data)
@@ -41,8 +46,12 @@ static int parse_ofpart_partitions(struct mtd_info *master,
 	/* First count the subnodes */
 	pp = NULL;
 	nr_parts = 0;
-	while ((pp = of_get_next_child(node, pp)))
+	while ((pp = of_get_next_child(node, pp))) {
+		if (node_has_compatible(pp))
+			continue;
+
 		nr_parts++;
+	}
 
 	if (nr_parts == 0)
 		return 0;
@@ -57,6 +66,9 @@ static int parse_ofpart_partitions(struct mtd_info *master,
 		const __be32 *reg;
 		int len;
 		int a_cells, s_cells;
+
+		if (node_has_compatible(pp))
+			continue;
 
 		reg = of_get_property(pp, "reg", &len);
 		if (!reg) {
