307635ee0d8d2a196753406ed06f32f4e90c1001
Andi Kleen <ak@linux.intel.com>
2013-08-05 15:02:44 -0700

    x86, asmlinkage: Make syscall tables visible
    They are referenced from entry*.S.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Link: http://lkml.kernel.org/r/1375740170-7446-11-git-send-email-andi@firstfloor.org
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/syscall_64.c
+++ arch/x86/kernel/syscall_64.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern void sys_ni_syscall(void);
 
-const sys_call_ptr_t sys_call_table[__NR_syscall_max+1] = {
+asmlinkage const sys_call_ptr_t sys_call_table[__NR_syscall_max+1] = {
 	/*
 	 * Smells like a compiler bug -- it doesn't work
 	 * when the & below is removed.
