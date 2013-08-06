e34c512523950528c292e6d5adbfce21ec829650
Yann E. MORIN <yann.morin.1998@free.fr>
2013-08-06 18:45:07 +0200

    kconfig: silence warning when parsing auto.conf when a symbol has changed type
    When a symbol changes type from tristate to bool, and was previously set to
    'm', a subsequent silentoldconfig would warn about inconsistency, such as:
    
        include/config/auto.conf:3014:warning: symbol value 'm' invalid for
        HOTPLUG_PCI_PCIE
    
    Seen by Linus with the merge in aa8032b (sequence to reproduce by Michal):
        git checkout 1fe0135
        make mrproper
        make allmodconfig
        make silentoldconfig
        git checkout aa8032b
        make allmodconfig
        make silentoldconfig
    
    Since HOTPLUG_PCI_PCIE changed from tristate to bool in aa8032b, it was
    previously set to 'm' in auto.conf by the first allmodconfig+silentoldconfig,
    but then was set to 'y' by the second allmodconfig. Then the second
    silentoldconfig prints the warning.
    
    The warning in this case is a spurious warning, which happens at the time
    kconfig tries to detect symbols that have changed, to touch the empty
    header files in include/config used for dependency-tracking by make.
    
    Silence the warning when we read the old auto.conf file, since it is
    perfectly legit that a symbol changed type since the previous call.
    
    Thread in:
        http://marc.info/?l=linux-pci&m=137569198904000&w=2
    
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Michal Marek <mmarek@suse.cz>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Yinghai Lu <yinghai@kernel.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -141,7 +141,9 @@ static int conf_set_sym_val(struct symbol *sym, int def, int def_flags, char *p)
 			sym->flags |= def_flags;
 			break;
 		}
-		conf_warning("symbol value '%s' invalid for %s", p, sym->name);
+		if (def != S_DEF_AUTO)
+			conf_warning("symbol value '%s' invalid for %s",
+				     p, sym->name);
 		return 1;
 	case S_OTHER:
 		if (*p != '"') {
@@ -162,7 +164,8 @@ static int conf_set_sym_val(struct symbol *sym, int def, int def_flags, char *p)
 			memmove(p2, p2 + 1, strlen(p2));
 		}
 		if (!p2) {
-			conf_warning("invalid string found");
+			if (def != S_DEF_AUTO)
+				conf_warning("invalid string found");
 			return 1;
 		}
 		/* fall through */
@@ -173,7 +176,9 @@ static int conf_set_sym_val(struct symbol *sym, int def, int def_flags, char *p)
 			sym->def[def].val = strdup(p);
 			sym->flags |= def_flags;
 		} else {
-			conf_warning("symbol value '%s' invalid for %s", p, sym->name);
+			if (def != S_DEF_AUTO)
+				conf_warning("symbol value '%s' invalid for %s",
+					     p, sym->name);
 			return 1;
 		}
 		break;
