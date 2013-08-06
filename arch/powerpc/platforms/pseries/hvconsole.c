ab952a48ea7f2007ccd7e83e0901e3f1bd2f1639
Anton Blanchard <anton@samba.org>
2013-08-07 02:01:23 +1000

    powerpc/pseries: Simplify H_GET_TERM_CHAR
    plpar_get_term_char is only used once and just adds a layer
    of complexity to H_GET_TERM_CHAR. plpar_put_term_char isn't
    used at all so we can remove it.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/platforms/pseries/hvconsole.c
+++ arch/powerpc/platforms/pseries/hvconsole.c
@@ -41,10 +41,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 int hvc_get_chars(uint32_t vtermno, char *buf, int count)
 {
-	unsigned long got;
+	long ret;
+	unsigned long retbuf[PLPAR_HCALL_BUFSIZE];
+	unsigned long *lbuf = (unsigned long *)buf;
 
-	if (plpar_get_term_char(vtermno, &got, buf) == H_SUCCESS)
-		return got;
+	ret = plpar_hcall(H_GET_TERM_CHAR, retbuf, vtermno);
+	lbuf[0] = retbuf[1];
+	lbuf[1] = retbuf[2];
+
+	if (ret == H_SUCCESS)
+		return retbuf[0];
 
 	return 0;
 }
