b935533740647f15ddda857c9a4333cb7e11f545
Magnus Damm <damm@opensource.se>
2013-08-08 07:13:49 +0900

    ARM: shmobile: Remove unused shmobile_smp_init_cpus()
    Remove shmobile_smp_init_cpus() since all SMP platforms in
    mach-shmobile now rely on DT for CPU core description instead
    of for instance determining number of cores from the SCU.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/platsmp.c
+++ arch/arm/mach-shmobile/platsmp.c
@@ -12,24 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 #include <linux/init.h>
-#include <linux/smp.h>
 #include <asm/cacheflush.h>
 #include <asm/smp_plat.h>
 #include <mach/common.h>
-
-void __init shmobile_smp_init_cpus(unsigned int ncores)
-{
-	unsigned int i;
-
-	if (ncores > nr_cpu_ids) {
-		pr_warn("SMP: %u cores greater than maximum (%u), clipping\n",
-			ncores, nr_cpu_ids);
-		ncores = nr_cpu_ids;
-	}
-
-	for (i = 0; i < ncores; i++)
-		set_cpu_possible(i, true);
-}
 
 extern unsigned long shmobile_smp_fn[];
 extern unsigned long shmobile_smp_arg[];
