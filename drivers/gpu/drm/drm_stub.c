647bc175ca0b2005290b51fdaa7bb678ed1e2d44
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-08-08 15:41:34 +0200

    drm: remove procfs code, take 2
    So almost two years ago I've tried to nuke the procfs code already
    once before:
    
    http://lists.freedesktop.org/archives/dri-devel/2011-October/015707.html
    
    The conclusion was that userspace drivers (specifically libdrm device
    node detection) stopped relying on procfs in 2001. But after some
    digging it turned out that the drmstat tool in libdrm is still using
    those files (but only when certain options are set). So we've decided
    to keep profcs.
    
    But I when I've started to dig around again what exactly this tool
    does I've noticed that it tries to read the "mem", "vm", and "vma"
    files from procfs. Now as far my git history digging shows "mem" never
    did anything useful (at least in the version that first showed up in
    upstream history in 2004) and the file was remove in
    
    commit 955b12def42e83287c1bdb1411d99451753c1391
    Author: Ben Gamari <bgamari@gmail.com>
    Date:   Tue Feb 17 20:08:49 2009 -0500
    
        drm: Convert proc files to seq_file and introduce debugfs
    
    Which means that for over 4 years drmstat has been broken, and no one
    cared. In my opinion that's proof enough that no one is actually using
    drmstat, and so that we can savely nuke the procfs support from drm.
    
    While at it fix up the error case cleanup for debugfs in drm_get_minor.
    
    v2: Fix dates, libdrm stopped relying on procfs for drm node detection
    in 2001.
    
    v3: fixup compilation warning for !CONFIG_DEBUG_FS, reported by
    Fengguang Wu.
    
    Cc: kbuild test robot <fengguang.wu@intel.com>
    Cc: Dave Airlie <airlied@linux.ie>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_stub.c
+++ drivers/gpu/drm/drm_stub.c
@@ -69,7 +69,6 @@ module_param_named(timestamp_monotonic, drm_timestamp_monotonic, int, 0600);
 struct idr drm_minors_idr;
 
 struct class *drm_class;
-struct proc_dir_entry *drm_proc_root;
 struct dentry *drm_debugfs_root;
 
 int drm_err(const char *func, const char *format, ...)
@@ -316,9 +315,8 @@ EXPORT_SYMBOL(drm_fill_in_dev);
  * \param sec-minor structure to hold the assigned minor
  * \return negative number on failure.
  *
- * Search an empty entry and initialize it to the given parameters, and
- * create the proc init entry via proc_init(). This routines assigns
- * minor numbers to secondary heads of multi-headed cards
+ * Search an empty entry and initialize it to the given parameters. This
+ * routines assigns minor numbers to secondary heads of multi-headed cards
  */
 int drm_get_minor(struct drm_device *dev, struct drm_minor **minor, int type)
 {
@@ -346,20 +344,11 @@ int drm_get_minor(struct drm_device *dev, struct drm_minor **minor, int type)
 
 	idr_replace(&drm_minors_idr, new_minor, minor_id);
 
-	if (type == DRM_MINOR_LEGACY) {
-		ret = drm_proc_init(new_minor, drm_proc_root);
-		if (ret) {
-			DRM_ERROR("DRM: Failed to initialize /proc/dri.\n");
-			goto err_mem;
-		}
-	} else
-		new_minor->proc_root = NULL;
-
 #if defined(CONFIG_DEBUG_FS)
 	ret = drm_debugfs_init(new_minor, minor_id, drm_debugfs_root);
 	if (ret) {
 		DRM_ERROR("DRM: Failed to initialize /sys/kernel/debug/dri.\n");
-		goto err_g2;
+		goto err_mem;
 	}
 #endif
 
@@ -367,7 +356,7 @@ int drm_get_minor(struct drm_device *dev, struct drm_minor **minor, int type)
 	if (ret) {
 		printk(KERN_ERR
 		       "DRM: Error sysfs_device_add.\n");
-		goto err_g2;
+		goto err_debugfs;
 	}
 	*minor = new_minor;
 
@@ -375,10 +364,11 @@ int drm_get_minor(struct drm_device *dev, struct drm_minor **minor, int type)
 	return 0;
 
 
-err_g2:
-	if (new_minor->type == DRM_MINOR_LEGACY)
-		drm_proc_cleanup(new_minor, drm_proc_root);
+err_debugfs:
+#if defined(CONFIG_DEBUG_FS)
+	drm_debugfs_cleanup(new_minor);
 err_mem:
+#endif
 	kfree(new_minor);
 err_idr:
 	idr_remove(&drm_minors_idr, minor_id);
@@ -392,10 +382,6 @@ EXPORT_SYMBOL(drm_get_minor);
  *
  * \param sec_minor - structure to be released
  * \return always zero
- *
- * Cleans up the proc resources. Not legal for this to be the
- * last minor released.
- *
  */
 int drm_put_minor(struct drm_minor **minor_p)
 {
@@ -403,8 +389,6 @@ int drm_put_minor(struct drm_minor **minor_p)
 
 	DRM_DEBUG("release secondary minor %d\n", minor->index);
 
-	if (minor->type == DRM_MINOR_LEGACY)
-		drm_proc_cleanup(minor, drm_proc_root);
 #if defined(CONFIG_DEBUG_FS)
 	drm_debugfs_cleanup(minor);
 #endif
