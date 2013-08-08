10ca68919079baceb1ae547712aec88cd4b0bc28
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-08-08 15:41:30 +0200

    drm/memory: don't export agp helpers
    They're only used by the agpgart support code in drm_agpgart.c,
    not by any drivers.
    
    I think long-term we should create a drm_internal.h include file with
    all the various functions only used by the drm core and not exported
    to drivers, and remove them from drmP.h. Oh, and someone should kill
    that upper-case P sometimes ;-) But that's all stuff for future patch
    bombs.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_memory.c
+++ drivers/gpu/drm/drm_memory.c
@@ -87,7 +87,6 @@ void drm_free_agp(DRM_AGP_MEM * handle, int pages)
 {
 	agp_free_memory(handle);
 }
-EXPORT_SYMBOL(drm_free_agp);
 
 /** Wrapper around agp_bind_memory() */
 int drm_bind_agp(DRM_AGP_MEM * handle, unsigned int start)
@@ -100,7 +99,6 @@ int drm_unbind_agp(DRM_AGP_MEM * handle)
 {
 	return agp_unbind_memory(handle);
 }
-EXPORT_SYMBOL(drm_unbind_agp);
 
 #else  /*  __OS_HAS_AGP  */
 static inline void *agp_remap(unsigned long offset, unsigned long size,
