2f46eb7b7e661ea2cdbf4a1793d8738987c3bc31
Michael Brunner <BrunnerMichael@t-online.de>
2013-08-09 17:33:43 +0200

    mfd: Add support for COMe-bHL6 and COMe-cTH6 to Kontron PLD driver
    This patch adds DMI system IDs for the new Kontron modules COMe-bHL6 and
    COMe-cTH6 to the Kontron PLD driver.
    
    Acked-by: Kevin Strasser <strassek@engr.orst.edu>
    Signed-off-by: Michael Brunner <michael.brunner@kontron.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/kempld-core.c
+++ drivers/mfd/kempld-core.c
@@ -414,6 +414,15 @@ static struct platform_driver kempld_driver = {
 
 static struct dmi_system_id __initdata kempld_dmi_table[] = {
 	{
+		.ident = "BHL6",
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Kontron"),
+			DMI_MATCH(DMI_BOARD_NAME, "COMe-bHL6"),
+		},
+		.driver_data = (void *)&kempld_platform_data_generic,
+		.callback = kempld_create_platform_device,
+	},
+	{
 		.ident = "CCR2",
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "Kontron"),
@@ -593,6 +602,15 @@ static struct dmi_system_id __initdata kempld_dmi_table[] = {
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "Kontron"),
 			DMI_MATCH(DMI_BOARD_NAME, "COMe-cCT6"),
+		},
+		.driver_data = (void *)&kempld_platform_data_generic,
+		.callback = kempld_create_platform_device,
+	},
+	{
+		.ident = "UTH6",
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Kontron"),
+			DMI_MATCH(DMI_BOARD_NAME, "COMe-cTH6"),
 		},
 		.driver_data = (void *)&kempld_platform_data_generic,
 		.callback = kempld_create_platform_device,
