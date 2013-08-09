bc4001524447e7e72cddd90b6238743b685686fd
Mark Brown <broonie@linaro.org>
2013-08-09 18:31:21 +0100

    net: asix: Staticise non-exported symbols
    Make functions that are only referenced from ops structures static, they
    do not need to be in the global namespace and sparse complains about this.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/ax88172a.c
+++ drivers/net/usb/ax88172a.c
@@ -162,7 +162,8 @@ static const struct net_device_ops ax88172a_netdev_ops = {
 	.ndo_set_rx_mode        = asix_set_multicast,
 };
 
-int ax88172a_get_settings(struct net_device *net, struct ethtool_cmd *cmd)
+static int ax88172a_get_settings(struct net_device *net,
+				 struct ethtool_cmd *cmd)
 {
 	if (!net->phydev)
 		return -ENODEV;
@@ -170,7 +171,8 @@ int ax88172a_get_settings(struct net_device *net, struct ethtool_cmd *cmd)
 	return phy_ethtool_gset(net->phydev, cmd);
 }
 
-int ax88172a_set_settings(struct net_device *net, struct ethtool_cmd *cmd)
+static int ax88172a_set_settings(struct net_device *net,
+				 struct ethtool_cmd *cmd)
 {
 	if (!net->phydev)
 		return -ENODEV;
@@ -178,7 +180,7 @@ int ax88172a_set_settings(struct net_device *net, struct ethtool_cmd *cmd)
 	return phy_ethtool_sset(net->phydev, cmd);
 }
 
-int ax88172a_nway_reset(struct net_device *net)
+static int ax88172a_nway_reset(struct net_device *net)
 {
 	if (!net->phydev)
 		return -ENODEV;
