f5896b8145422a071be6f310a45b1a3a5ae0fae4
Axel Lin <axel.lin@ingics.com>
2013-08-09 15:35:16 +0800

    spi: Use dev_get_drvdata at appropriate places
    Use dev_get_drvdata() instead of platform_get_drvdata(to_platform_device(dev)).
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-coldfire-qspi.c
+++ drivers/spi/spi-coldfire-qspi.c
@@ -559,7 +559,7 @@ static int mcfqspi_resume(struct device *dev)
 #ifdef CONFIG_PM_RUNTIME
 static int mcfqspi_runtime_suspend(struct device *dev)
 {
-	struct mcfqspi *mcfqspi = platform_get_drvdata(to_platform_device(dev));
+	struct mcfqspi *mcfqspi = dev_get_drvdata(dev);
 
 	clk_disable(mcfqspi->clk);
 
@@ -568,7 +568,7 @@ static int mcfqspi_runtime_suspend(struct device *dev)
 
 static int mcfqspi_runtime_resume(struct device *dev)
 {
-	struct mcfqspi *mcfqspi = platform_get_drvdata(to_platform_device(dev));
+	struct mcfqspi *mcfqspi = dev_get_drvdata(dev);
 
 	clk_enable(mcfqspi->clk);
 
