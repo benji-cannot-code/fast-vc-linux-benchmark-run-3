8e5e32c53eb3327bf296f90c863ea789c18e23af
Mark Brown <broonie@linaro.org>
2013-08-11 15:26:04 +0100

    usb: phy: nop: Defer clock prepare until PHY init
    Since we only enable the PHY clock on init and the PHY init and shutdown
    does not occur in atomitc context there is no need to prepare the clock
    before it is enabled.  Move the clk_prepare() operations to go along
    with the enables, allowing the clock to be fully idle when not in use.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/phy/phy-generic.c
+++ drivers/usb/phy/phy-generic.c
@@ -75,7 +75,7 @@ int usb_gen_phy_init(struct usb_phy *phy)
 	}
 
 	if (!IS_ERR(nop->clk))
-		clk_enable(nop->clk);
+		clk_prepare_enable(nop->clk);
 
 	if (!IS_ERR(nop->reset)) {
 		/* De-assert RESET */
@@ -98,7 +98,7 @@ void usb_gen_phy_shutdown(struct usb_phy *phy)
 	}
 
 	if (!IS_ERR(nop->clk))
-		clk_disable(nop->clk);
+		clk_disable_unprepare(nop->clk);
 
 	if (!IS_ERR(nop->vcc)) {
 		if (regulator_disable(nop->vcc))
@@ -161,14 +161,6 @@ int usb_phy_gen_create_phy(struct device *dev, struct usb_phy_gen_xceiv *nop,
 		}
 	}
 
-	if (!IS_ERR(nop->clk)) {
-		err = clk_prepare(nop->clk);
-		if (err) {
-			dev_err(dev, "Error preparing clock\n");
-			return err;
-		}
-	}
-
 	nop->vcc = devm_regulator_get(dev, "vcc");
 	if (IS_ERR(nop->vcc)) {
 		dev_dbg(dev, "Error getting vcc regulator: %ld\n",
@@ -200,13 +192,6 @@ int usb_phy_gen_create_phy(struct device *dev, struct usb_phy_gen_xceiv *nop,
 	return 0;
 }
 EXPORT_SYMBOL_GPL(usb_phy_gen_create_phy);
-
-void usb_phy_gen_cleanup_phy(struct usb_phy_gen_xceiv *nop)
-{
-	if (!IS_ERR(nop->clk))
-		clk_unprepare(nop->clk);
-}
-EXPORT_SYMBOL_GPL(usb_phy_gen_cleanup_phy);
 
 static int usb_phy_gen_xceiv_probe(struct platform_device *pdev)
 {
@@ -253,15 +238,13 @@ static int usb_phy_gen_xceiv_probe(struct platform_device *pdev)
 	if (err) {
 		dev_err(&pdev->dev, "can't register transceiver, err: %d\n",
 			err);
-		goto err_add;
+		return err;
 	}
 
 	platform_set_drvdata(pdev, nop);
 
 	return 0;
 
-err_add:
-	usb_phy_gen_cleanup_phy(nop);
 	return err;
 }
 
@@ -269,7 +252,6 @@ static int usb_phy_gen_xceiv_remove(struct platform_device *pdev)
 {
 	struct usb_phy_gen_xceiv *nop = platform_get_drvdata(pdev);
 
-	usb_phy_gen_cleanup_phy(nop);
 	usb_remove_phy(&nop->phy);
 
 	return 0;
