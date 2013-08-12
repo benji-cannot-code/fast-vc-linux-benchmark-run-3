2d9091e64135e326571b2346c42a8e031ae43e84
Dave Chinner <dchinner@redhat.com>
2013-08-12 20:49:34 +1000

    xfs: move unrelated definitions out of xfs_inode.h
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Ben Myers <bpm@sgi.com>

--- fs/xfs/xfs_ialloc.c
+++ fs/xfs/xfs_ialloc.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xfs_cksum.h"
 #include "xfs_buf_item.h"
 #include "xfs_icreate_item.h"
+#include "xfs_icache.h"
 
 
 /*
