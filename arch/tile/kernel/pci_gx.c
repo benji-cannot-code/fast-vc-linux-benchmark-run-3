e3640e7b5b249bddcae269c6b1f5a4522deabcb1
Chris Metcalf <cmetcalf@tilera.com>
2013-08-13 15:17:38 -0400

    tile: mark pcibios_init() as __init
    It was bombed away because it was previously marked as __devinit,
    but it should be an __init function.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- arch/tile/kernel/pci_gx.c
+++ arch/tile/kernel/pci_gx.c
@@ -1061,7 +1061,7 @@ void pcibios_fixup_bus(struct pci_bus *bus)
 }
 
 /* Process any "pci=" kernel boot arguments. */
-char *pcibios_setup(char *str)
+char *__init pcibios_setup(char *str)
 {
 	if (!strcmp(str, "off")) {
 		pci_probe = 0;
