ff4a8761caceb06514b2a86f6069b4fa7f111501
Li Zefan <lizefan@huawei.com>
2013-08-13 09:17:33 +0800

    cpuset: remove an unncessary forward declaration
    Signed-off-by: Li Zefan <lizefan@huawei.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -69,9 +69,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 int number_of_cpusets __read_mostly;
 
-/* Forward declare cgroup structures */
-struct cgroup_subsys cpuset_subsys;
-
 /* See "Frequency meter" comments, below. */
 
 struct fmeter {
