5fe457101514af612a1bc1f0e872a53db0ea741e
Zhiwu Song <Zhiwu.Song@csr.com>
2013-08-13 17:11:29 +0800

    i2c: sirf: fix the typo for setting bitrate to less than 100k
    there is a typo before, it makes the final bitrate wrong, this patch fixes
    it.
    
    Signed-off-by: Zhiwu Song <Zhiwu.Song@csr.com>
    Signed-off-by: Barry Song <Baohua.Song@csr.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-sirf.c
+++ drivers/i2c/busses/i2c-sirf.c
@@ -357,7 +357,7 @@ static int i2c_sirfsoc_probe(struct platform_device *pdev)
 
 	if (bitrate < 100000)
 		regval =
-			(2 * ctrl_speed) / (2 * bitrate * 11);
+			(2 * ctrl_speed) / (bitrate * 11);
 	else
 		regval = ctrl_speed / (bitrate * 5);
 
