1881dde1c09f1dbcb3a3dd7a591399caee5f4758
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-08-14 10:01:32 +0200

    drm/i915: clarify error paths in create_stolen_for_preallocated
    Use the standard inversely ordered goto label stack for everything.
    Spotted while reviewing place where we might need to to call
    vma_destroy but failed to do so.
    
    Cc: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem_stolen.c
+++ drivers/gpu/drm/i915/i915_gem_stolen.c
@@ -393,8 +393,7 @@ i915_gem_object_create_stolen_for_preallocated(struct drm_device *dev,
 		ret = drm_mm_reserve_node(&ggtt->mm, &vma->node);
 		if (ret) {
 			DRM_DEBUG_KMS("failed to allocate stolen GTT space\n");
-			i915_gem_vma_destroy(vma);
-			goto err_out;
+			goto err_vma;
 		}
 	}
 
@@ -405,6 +404,8 @@ i915_gem_object_create_stolen_for_preallocated(struct drm_device *dev,
 
 	return obj;
 
+err_vma:
+	i915_gem_vma_destroy(vma);
 err_out:
 	drm_mm_put_block(stolen);
 	drm_gem_object_unreference(&obj->base);
