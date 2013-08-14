0629b2a02b83d78079f6317aaf5b3adc96dcd1c2
Tuukka Tikkanen <tuukka.tikkanen@linaro.org>
2013-08-14 19:02:40 +0300

    cpuidle: Add a comment warning about possible overflow
    The menu governor has a number of tunable constants that may be changed
    in the source. If certain combination of values are chosen, an overflow
    is possible when the correction_factor is being recalculated.
    
    This patch adds a warning regarding this possibility and describes the
    change needed for fixing the issue. The change should not be permanently
    enabled, as it will hurt performance when it is not needed.
    
    Signed-off-by: Tuukka Tikkanen <tuukka.tikkanen@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -22,6 +22,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/math64.h>
 #include <linux/module.h>
 
+/*
+ * Please note when changing the tuning values:
+ * If (MAX_INTERESTING-1) * RESOLUTION > UINT_MAX, the result of
+ * a scaling operation multiplication may overflow on 32 bit platforms.
+ * In that case, #define RESOLUTION as ULL to get 64 bit result:
+ * #define RESOLUTION 1024ULL
+ *
+ * The default values do not overflow.
+ */
 #define BUCKETS 12
 #define INTERVALS 8
 #define RESOLUTION 1024
