0afda1499ebb776f7b0175882094ce7adf5eb5b3
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-14 16:38:01 +0530

    drm/exynos: Add missing of.h header include
    Add of.h explicitly for of_* APIs.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_fimc.c
+++ drivers/gpu/drm/exynos/exynos_drm_fimc.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regmap.h>
 #include <linux/clk.h>
 #include <linux/pm_runtime.h>
+#include <linux/of.h>
 
 #include <drm/drmP.h>
 #include <drm/exynos_drm.h>
