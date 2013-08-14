241d176faabb9d7a0ec846133c92f1ae571ea4a6
Trond Myklebust <Trond.Myklebust@netapp.com>
2013-08-14 17:58:28 -0400

    NFSv4.1: Add tracepoints for debugging slot table operations
    Add tracepoints to nfs41_setup_sequence and nfs41_sequence_done
    to track session and slot table state changes.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/nfs4trace.c
+++ fs/nfs/nfs4trace.c
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nfs_fs.h>
 #include "nfs4_fs.h"
 #include "internal.h"
+#include "nfs4session.h"
+#include "callback.h"
 
 #define CREATE_TRACE_POINTS
 #include "nfs4trace.h"
