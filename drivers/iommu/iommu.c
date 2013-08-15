2e6456b2750109d36ae0f6681bb5f67e7dd23ac9
Shuah Khan <shuah.kh@samsung.com>
2013-08-15 11:59:23 -0600

    iommu: Add event tracing feature to iommu
    Add tracing feature to iommu to report various iommu events. Classes
    iommu_group, iommu_device, and iommu_map_unmap are defined.
    
    iommu_group class events can be enabled to trigger when devices get added
    to and removed from an iommu group. Trace information includes iommu group
    id and device name.
    
    iommu:add_device_to_group
    iommu:remove_device_from_group
    
    iommu_device class events can be enabled to trigger when devices are attached
    to and detached from a domain. Trace information includes device name.
    
    iommu:attach_device_to_domain
    iommu:detach_device_from_domain
    
    iommu_map_unmap class events can be enabled to trigger when iommu map and
    unmap iommu ops. Trace information includes iova, physical address (map event
    only), and size.
    
    iommu:map
    iommu:unmap
    
    Signed-off-by: Shuah Khan <shuah.kh@samsung.com>
    Signed-off-by: Joerg Roedel <joro@8bytes.org>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/idr.h>
 #include <linux/notifier.h>
 #include <linux/err.h>
+#include <trace/events/iommu.h>
 
 static struct kset *iommu_group_kset;
 static struct ida iommu_group_ida;
