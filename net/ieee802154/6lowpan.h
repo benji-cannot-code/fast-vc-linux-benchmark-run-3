8029759fcee8dfbd46a641b578f1296c76c2bb52
Alexander Aring <alex.aring@gmail.com>
2013-08-16 21:59:56 +0200

    6lowpan: introduce lowpan_fetch_skb function
    This patch adds a helper function to parse the ipv6 header to a
    6lowpan header in stream.
    
    This function checks first if we can pull data with a specific
    length from a skb. If this seems to be okay, we copy skb data to
    a destination pointer and run skb_pull.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Reviewed-by: Werner Almesberger <werner@almesberger.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ieee802154/6lowpan.h
+++ net/ieee802154/6lowpan.h
@@ -231,4 +231,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 					dest = 16 bit inline */
 #define LOWPAN_NHC_UDP_CS_P_11	0xF3 /* source & dest = 0xF0B + 4bit inline */
 
+static inline bool lowpan_fetch_skb(struct sk_buff *skb,
+		void *data, const unsigned int len)
+{
+	if (unlikely(!pskb_may_pull(skb, len)))
+		return true;
+
+	skb_copy_from_linear_data(skb, data, len);
+	skb_pull(skb, len);
+
+	return false;
+}
+
 #endif /* __6LOWPAN_H__ */
