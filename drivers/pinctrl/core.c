6b720f4d0dc464b983fb89e484caf474a347a220
Axel Lin <axel.lin@ingics.com>
2013-08-18 20:34:22 +0800

    pinctrl: core: Hold pinctrldev_list_mutex mutex while traversing pinctrldev_list
    This one is missed in commit 44d5f7bb "pinctrl: sink pinctrldev_list_mutex".
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/core.c
+++ drivers/pinctrl/core.c
@@ -393,6 +393,8 @@ static int pinctrl_get_device_gpio_range(unsigned gpio,
 {
 	struct pinctrl_dev *pctldev = NULL;
 
+	mutex_lock(&pinctrldev_list_mutex);
+
 	/* Loop over the pin controllers */
 	list_for_each_entry(pctldev, &pinctrldev_list, node) {
 		struct pinctrl_gpio_range *range;
@@ -401,9 +403,12 @@ static int pinctrl_get_device_gpio_range(unsigned gpio,
 		if (range != NULL) {
 			*outdev = pctldev;
 			*outrange = range;
+			mutex_unlock(&pinctrldev_list_mutex);
 			return 0;
 		}
 	}
+
+	mutex_unlock(&pinctrldev_list_mutex);
 
 	return -EPROBE_DEFER;
 }
