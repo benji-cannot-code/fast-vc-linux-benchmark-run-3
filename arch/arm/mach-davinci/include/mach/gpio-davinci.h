f9e995a4582a672ca04932d4d25b7a57ef95d1f1
Philip Avinash <avinashphilip@ti.com>
2013-08-18 10:49:00 +0530

    ARM: davinci: support gpio platform device
    DaVinci GPIO driver now uses platform device model.
    Add a GPIO platform register API to convert the
    traditional DaVinci  SoCs to use the new model.
    
    While at it, also group related include files together
    wherever that was not the case.
    
    Signed-off-by: Philip Avinash <avinashphilip@ti.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Lad, Prabhakar <prabhakar.csengg@gmail.com>
    [nsekhar@ti.com: move function declaration to local header,
    		 simplify commit message, merge SoC specific
    		 portions from other patches into this patch]
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/include/mach/gpio-davinci.h
+++ arch/arm/mach-davinci/include/mach/gpio-davinci.h
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/irqs.h>
 #include <mach/common.h>
 
-#define DAVINCI_GPIO_BASE 0x01C67000
-
 enum davinci_gpio_type {
 	GPIO_TYPE_DAVINCI = 0,
 	GPIO_TYPE_TNETV107X,
