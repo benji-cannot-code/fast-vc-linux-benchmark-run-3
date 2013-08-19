b4e19d7c208572aca5d746cd52fa5ced9efe5118
Mark Brown <broonie@linaro.org>
2013-08-19 12:17:36 +0100

    ASoC: hdmi: Provide stub DAPM integration
    Ensure continued operation with DAPM being mandatory.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/hdmi.c
+++ sound/soc/codecs/hdmi.c
@@ -24,11 +24,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME "hdmi-audio-codec"
 
-static struct snd_soc_codec_driver hdmi_codec;
+static const struct snd_soc_dapm_widget hdmi_widgets[] = {
+	SND_SOC_DAPM_INPUT("RX"),
+	SND_SOC_DAPM_OUTPUT("TX"),
+};
+
+static const struct snd_soc_dapm_route hdmi_routes[] = {
+	{ "Capture", NULL, "RX" },
+	{ "TX", NULL, "Playback" },
+};
 
 static struct snd_soc_dai_driver hdmi_codec_dai = {
 	.name = "hdmi-hifi",
 	.playback = {
+		.stream_name = "Playback",
 		.channels_min = 2,
 		.channels_max = 8,
 		.rates = SNDRV_PCM_RATE_32000 |
@@ -39,6 +48,7 @@ static struct snd_soc_dai_driver hdmi_codec_dai = {
 			SNDRV_PCM_FMTBIT_S24_LE,
 	},
 	.capture = {
+		.stream_name = "Capture",
 		.channels_min = 2,
 		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_32000 |
@@ -49,6 +59,13 @@ static struct snd_soc_dai_driver hdmi_codec_dai = {
 			SNDRV_PCM_FMTBIT_S24_LE,
 	},
 
+};
+
+static struct snd_soc_codec_driver hdmi_codec = {
+	.dapm_widgets = hdmi_widgets,
+	.num_dapm_widgets = ARRAY_SIZE(hdmi_widgets),
+	.dapm_routes = hdmi_routes,
+	.num_dapm_routes = ARRAY_SIZE(hdmi_routes),
 };
 
 static int hdmi_codec_probe(struct platform_device *pdev)
