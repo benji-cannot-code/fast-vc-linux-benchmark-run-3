ac7b7ae5f21800246280c557a04f997076aac9d7
Asias He <asias@redhat.com>
2013-08-19 09:23:19 +0800

    vhost: Include linux/uio.h instead of linux/socket.h
    memcpy_fromiovec is moved from net/core/iovec.c to lib/iovec.c.
    linux/uio.h provides the declaration for memcpy_fromiovec.
    
    Include linux/uio.h instead of inux/socket.h for it.
    
    Signed-off-by: Asias He <asias@redhat.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/vhost/vhost.c
+++ drivers/vhost/vhost.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/eventfd.h>
 #include <linux/vhost.h>
-#include <linux/socket.h> /* memcpy_fromiovec */
+#include <linux/uio.h>
 #include <linux/mm.h>
 #include <linux/mmu_context.h>
 #include <linux/miscdevice.h>
