889a306a49d7217b88d93e93eb202b3b98beba9c
Lidza Louina <lidza.louina@gmail.com>
2013-08-20 14:15:36 -0400

    staging: dgnc: removes ifdef HAVE_UNLOCKED_IOCTL conditionals
    This patch removes the HAVE_UNLOCKED_IOCTL conditional
    statements from driver.c, mgmt.c and mgmt.h. This was
    used to support older kernels. It isn't needed now.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_mgmt.h
+++ drivers/staging/dgnc/dgnc_mgmt.h
@@ -27,12 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int dgnc_mgmt_open(struct inode *inode, struct file *file);
 int dgnc_mgmt_close(struct inode *inode, struct file *file);
-
-#ifdef HAVE_UNLOCKED_IOCTL
 long dgnc_mgmt_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
-#else
-int dgnc_mgmt_ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg);
-#endif
-
 #endif
 
