512d586cfd76f2138dcb4ef5e57d1e9bc9dedd98
Wei WANG <wei_wang@realsil.com.cn>
2013-08-20 14:18:56 +0800

    mfd: rtsx: Copyright modifications
    Update copyright date, remove author address and add Roger Tseng.
    
    Signed-off-by: Wei WANG <wei_wang@realsil.com.cn>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/rts5249.c
+++ drivers/mfd/rts5249.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Author:
  *   Wei WANG <wei_wang@realsil.com.cn>
- *   No. 128, West Shenhu Road, Suzhou Industry Park, Suzhou, China
  */
 
 #include <linux/module.h>
