5c9adc67bf0a0b29099dbe1eb2de8a92aae45e28
Linus Walleij <linus.walleij@linaro.org>
2013-08-20 11:19:24 +0200

    ARM: ux500: delete oldschool pin defines
    The pin definition file dates back to a time before the pin
    control subsystem. The functions called inside that pin control
    driver are now deleted. Let's get rid of this file.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500-pins.c
+++ arch/arm/mach-ux500/board-mop500-pins.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach-types.h>
 
-#include "pins-db8500.h"
 #include "board-mop500.h"
 
 enum custom_pin_cfg_t {
