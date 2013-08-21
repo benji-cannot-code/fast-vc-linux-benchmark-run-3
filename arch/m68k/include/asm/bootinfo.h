5f7f2cd2fd35094719cd93104b3856104b4acd06
Geert Uytterhoeven <geert@linux-m68k.org>
2013-08-21 22:36:32 +0200

    m68k: Add support to export bootinfo in procfs
    Add optional support to export the bootinfo used to boot the kernel in a
    "bootinfo" file in procfs.  This is useful with kexec.
    
    This is based on the similar feature for ATAGS on ARM.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/include/asm/bootinfo.h
+++ arch/m68k/include/asm/bootinfo.h
@@ -14,4 +14,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <uapi/asm/bootinfo.h>
 
+
+#ifndef __ASSEMBLY__
+
+#ifdef CONFIG_BOOTINFO_PROC
+extern void save_bootinfo(const struct bi_record *bi);
+#else
+static inline void save_bootinfo(const struct bi_record *bi) {}
+#endif
+
+#endif /* __ASSEMBLY__ */
+
+
 #endif /* _M68K_BOOTINFO_H */
