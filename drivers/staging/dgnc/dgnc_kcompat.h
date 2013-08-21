718b8e4ee9503a72d90bba289bc305058ed7474c
Lidza Louina <lidza.louina@gmail.com>
2013-08-21 11:08:04 -0400

    staging: dgnc: kcompat.h: removes trailing whitespace
    This patch removes trailing whitespace in
    the kcompat.h file.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_kcompat.h
+++ drivers/staging/dgnc/dgnc_kcompat.h
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # endif
 
 
-#if !defined(TTY_FLIPBUF_SIZE)        
-# define TTY_FLIPBUF_SIZE 512        
+#if !defined(TTY_FLIPBUF_SIZE)
+# define TTY_FLIPBUF_SIZE 512
 #endif
 
 
