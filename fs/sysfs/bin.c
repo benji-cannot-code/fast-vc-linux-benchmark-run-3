596dd73469a0ffc129fb3e4142ac099b4af27cbe
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-21 16:28:26 -0700

    sysfs: fix up space coding style issues
    This fixes up all of the space-related coding style issues for the sysfs
    code.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/sysfs/bin.c
+++ fs/sysfs/bin.c
@@ -392,7 +392,7 @@ out_unlock:
 	return rc;
 }
 
-static int open(struct inode * inode, struct file * file)
+static int open(struct inode *inode, struct file *file)
 {
 	struct sysfs_dirent *attr_sd = file->f_path.dentry->d_fsdata;
 	struct bin_attribute *attr = attr_sd->s_bin_attr.bin_attr;
@@ -436,7 +436,7 @@ static int open(struct inode * inode, struct file * file)
 	return error;
 }
 
-static int release(struct inode * inode, struct file * file)
+static int release(struct inode *inode, struct file *file)
 {
 	struct bin_buffer *bb = file->private_data;
 
