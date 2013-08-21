e1bf2522a2195d91c53ba1f226712e253a6b91d8
Lidza Louina <lidza.louina@gmail.com>
2013-08-21 11:08:13 -0400

    staging: dgnc: dpacompat.h: removes trailing whitespace
    This patch removes trailing whitespace in
    the dpacompat.h file.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dpacompat.h
+++ drivers/staging/dgnc/dpacompat.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 
-/* 
- * This structure holds data needed for the intelligent <--> nonintelligent 
+/*
+ * This structure holds data needed for the intelligent <--> nonintelligent
  * DPA translation
  */
  struct ni_info {
