de1dcb4f45090156ed6f61e1bfc07cac9711fd8f
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-22 13:36:57 +0530

    staging: xillybus: Remove duplicate inclusion of list.h
    list.h header file was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xillybus/xillybus.h
+++ drivers/staging/xillybus/xillybus.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cdev.h>
 #include <linux/spinlock.h>
 #include <linux/mutex.h>
-#include <linux/list.h>
 #include <linux/workqueue.h>
 
 struct xilly_endpoint_hardware;
