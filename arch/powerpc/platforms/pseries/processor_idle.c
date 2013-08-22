1e4712b2a412b48d439993770e7d9b879dc0ad23
Deepthi Dharwar <deepthi@linux.vnet.ibm.com>
2013-08-22 15:23:42 +0530

    pseries/cpuidle: Remove dependency of pseries.h file
    As a part of pseries_idle cleanup to make the backend driver
    code common to both pseries and powernv.
    Remove non-essential smt_snooze_delay declaration in pseries.h
    header file and pseries.h file inclusion in
    pseries/processor_idle.c
    
    Signed-off-by: Deepthi Dharwar <deepthi@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/platforms/pseries/processor_idle.c
+++ arch/powerpc/platforms/pseries/processor_idle.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/runlatch.h>
 
 #include "plpar_wrappers.h"
-#include "pseries.h"
 
 struct cpuidle_driver pseries_idle_driver = {
 	.name             = "pseries_idle",
