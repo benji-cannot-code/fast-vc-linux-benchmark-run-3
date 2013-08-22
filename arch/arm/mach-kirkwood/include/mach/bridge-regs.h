65315fb6aaa1eeba04fbfa1b779e646a2b7bffcd
Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
2013-08-22 12:49:07 -0300

    ARM: kirkwood: Add standby support
    Implements standby support for Kirkwood SoC. When the SoC enters
    standby state the memory PM units are disabled, the DDR is set
    in self-refresh mode, and the CPU is set in WFI.
    
    At this point there's no clock gating, as that is considered each
    driver's task.
    
    Signed-off-by: Simon Guinot <sguinot@lacie.com>
    Signed-off-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/include/mach/bridge-regs.h
+++ arch/arm/mach-kirkwood/include/mach/bridge-regs.h
@@ -79,4 +79,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CGC_TDM			(1 << 20)
 #define CGC_RESERVED		(0x6 << 21)
 
+#define MEMORY_PM_CTRL		(BRIDGE_VIRT_BASE + 0x118)
+
 #endif
