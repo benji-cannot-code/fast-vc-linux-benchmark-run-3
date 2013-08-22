af4139d2ff3141b6f4de70925997be1ddd3b48c6
Lee Jones <lee.jones@linaro.org>
2013-08-22 15:41:30 +0100

    ARM: ux500: Stop initialising the pinmaps when booting with ATAGs
    It's time to remove all ATAG support from ux500 and rely solely on
    Device Tree booting. This patch is part of that endeavour.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -154,7 +154,6 @@ static void __init mop500_init_machine(void)
 
 	platform_device_register(&db8500_prcmu_device);
 
-	mop500_pinmaps_init();
 	parent = u8500_init_devices();
 
 	mop500_uart_init(parent);
@@ -170,7 +169,6 @@ static void __init snowball_init_machine(void)
 
 	platform_device_register(&db8500_prcmu_device);
 
-	snowball_pinmaps_init();
 	parent = u8500_init_devices();
 
 	mop500_uart_init(parent);
@@ -185,7 +183,6 @@ static void __init hrefv60_init_machine(void)
 
 	platform_device_register(&db8500_prcmu_device);
 
-	hrefv60_pinmaps_init();
 	parent = u8500_init_devices();
 
 	mop500_uart_init(parent);
