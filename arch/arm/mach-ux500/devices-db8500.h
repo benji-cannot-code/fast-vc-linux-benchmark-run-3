97b3cc7e27c0b6eb3b35224ba37ea9a3e8198125
Lee Jones <lee.jones@linaro.org>
2013-08-22 12:10:48 +0100

    ARM: ux500: Rip out keypad initialisation which is no longer used
    This is part of the ux500 conversion over to Device Tree only
    booting, but this stuff haven't been used for years.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/devices-db8500.h
+++ arch/arm/mach-ux500/devices-db8500.h
@@ -14,27 +14,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "db8500-regs.h"
 #include "devices-common.h"
 
-struct ske_keypad_platform_data;
 struct pl022_ssp_controller;
 struct platform_device;
 
 extern struct ab8500_platform_data ab8500_platdata;
 extern struct prcmu_pdata db8500_prcmu_pdata;
 extern struct platform_device db8500_prcmu_device;
-
-static inline struct platform_device *
-db8500_add_ske_keypad(struct device *parent,
-		      struct ske_keypad_platform_data *pdata,
-		      size_t size)
-{
-	struct resource resources[] = {
-		DEFINE_RES_MEM(U8500_SKE_BASE, SZ_4K),
-		DEFINE_RES_IRQ(IRQ_DB8500_KB),
-	};
-
-	return platform_device_register_resndata(parent, "nmk-ske-keypad", -1,
-						 resources, 2, pdata, size);
-}
 
 static inline struct amba_device *
 db8500_add_ssp(struct device *parent, const char *name, resource_size_t base,
