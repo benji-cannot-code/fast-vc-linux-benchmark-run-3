4a07ba1907ed329fd6f7fa29d908f94d98855576
Paulo Zanoni <paulo.r.zanoni@intel.com>
2013-08-23 19:51:28 -0300

    drm/i915: enable trickle feed on Haswell
    We shouldn't disable the trickle feed bits on Haswell. Our
    documentation explicitly says the trickle feed bits of PRI_CTL and
    CUR_CTL should not be programmed to 1, and the hardware engineer also
    asked us to not program the SPR_CTL field to 1. Leaving the bits as 1
    could cause underflows.
    
    Reported-by: Arthur Runyan <arthur.j.runyan@intel.com>
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -3314,6 +3314,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   MCURSOR_PIPE_A	0x00
 #define   MCURSOR_PIPE_B	(1 << 28)
 #define   MCURSOR_GAMMA_ENABLE  (1 << 26)
+#define   CURSOR_TRICKLE_FEED_DISABLE	(1 << 14)
 #define _CURABASE		(dev_priv->info->display_mmio_offset + 0x70084)
 #define _CURAPOS		(dev_priv->info->display_mmio_offset + 0x70088)
 #define   CURSOR_POS_MASK       0x007FF
