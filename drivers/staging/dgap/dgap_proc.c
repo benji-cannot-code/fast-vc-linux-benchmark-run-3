6da6b75ae3bff23d6e71061f68d5d4fe7494fea8
Lidza Louina <lidza.louina@gmail.com>
2013-08-22 21:35:19 -0400

    staging: dgap: removes mgmt.h
    This patch removes mgmt.h and the headers that include
    it of files in dgap. This file isn't needed for the
    driver to work with the kernel.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_proc.c
+++ drivers/staging/dgap/dgap_proc.c
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dgap_driver.h"
 #include "dgap_proc.h"
-#include "dgap_mgmt.h"
 #include "dgap_conf.h"
 #include "dgap_parse.h"
 #include "dgap_fep5.h"
