f610ccda33a93bc2cc99e0304f4a5a2e90989829
Lee Jones <lee.jones@linaro.org>
2013-08-23 09:56:47 +0100

    ARM: ux500: Take out STUIB support when not booting with Device Tree
    It's time to remove all ATAG support from ux500 and rely solely on
    Device Tree booting. This patch is part of that endeavour.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500-uib.c
+++ arch/arm/mach-ux500/board-mop500-uib.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "id.h"
 
 enum mop500_uib {
-	STUIB,
 };
 
 struct uib {
@@ -26,11 +25,6 @@ struct uib {
 };
 
 static struct uib __initdata mop500_uibs[] = {
-	[STUIB] = {
-		.name	= "ST-UIB",
-		.option	= "stuib",
-		.init	= mop500_stuib_init,
-	},
 };
 
 static struct uib *mop500_uib;
@@ -99,8 +93,6 @@ int __init mop500_uib_init(void)
 		__mop500_uib_init(uib, "from uib= boot argument");
 		return 0;
 	}
-
-	__mop500_uib_init(&mop500_uibs[STUIB], "detected");
 
 	return 0;
 }
