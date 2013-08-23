76cf0465498bb652b4bdf91df13aabf2e4203ecd
Lidza Louina <lidza.louina@gmail.com>
2013-08-22 22:17:10 -0400

    staging: dgap: removes references to proc code
    This patch removes references to proc functions and
    structs in the code. The kernel uses sysfs instead
    of proc now.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_fep5.c
+++ drivers/staging/dgap/dgap_fep5.c
@@ -134,8 +134,6 @@ int dgap_after_config_loaded(void)
 		 */
 		dgap_Board[i]->flipbuf = dgap_driver_kzmalloc(MYFLIPLEN, GFP_ATOMIC);
 		dgap_Board[i]->flipflagbuf = dgap_driver_kzmalloc(MYFLIPLEN, GFP_ATOMIC);
-
-		//dgap_proc_register_basic_postscan(i);
 	}
 
 	return (rc);
