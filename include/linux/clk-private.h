f1b6932363792cc817c47b6fdd7c44cfe9994f9c
Sylwester Nawrocki <s.nawrocki@samsung.com>
2013-08-24 15:00:10 +0200

    clk: Implement clk_unregister
    clk_unregister() is currently not implemented and it is required when
    a clock provider module needs to be unloaded.
    
    Normally the clock supplier module is prevented to be unloaded by
    taking reference on the module in clk_get().
    
    For cases when the clock supplier module deinitializes despite the
    consumers of its clocks holding a reference on the module, e.g. when
    the driver is unbound through "unbind" sysfs attribute, there are
    empty clock ops added. These ops are assigned temporarily to struct
    clk and used until all consumers release the clock, to avoid invoking
    callbacks from the module which just got removed.
    
    Signed-off-by: Jiada Wang <jiada_wang@mentor.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- include/linux/clk-private.h
+++ include/linux/clk-private.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __LINUX_CLK_PRIVATE_H
 
 #include <linux/clk-provider.h>
+#include <linux/kref.h>
 #include <linux/list.h>
 
 /*
@@ -51,6 +52,7 @@ struct clk {
 #ifdef CONFIG_COMMON_CLK_DEBUG
 	struct dentry		*dentry;
 #endif
+	struct kref		ref;
 };
 
 /*
