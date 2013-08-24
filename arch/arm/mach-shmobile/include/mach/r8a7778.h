6e3adb78d4955fed9fbf79a4f081be2e5cad7e57
Max Filippov <max.filippov@cogentembedded.com>
2013-08-25 01:35:13 +0400

    ARM: shmobile: r8a7778: add HPB-DMAC support
    Add HPB-DMAC platform device on R8A7778 SoC along with its slave and channel
    configurations (only for SDHI0 so far).
    
    Signed-off-by: Max Filippov <max.filippov@cogentembedded.com>
    [Sergei: moved *enum* declaring HPB-DMAC slave IDs from now removed <mach/dma.h>
    to <mach/r8a7778.h>, removed #include <mach/dma.h> from setup-r8a7778.c, removed
    SSI-related *enum* values and SSI-related data from hpb_dmae_slaves[] and
    hpb_dmae_channels[], moved the comments after the element initializers of
    hpb_dmae_channels[].]
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/r8a7778.h
+++ arch/arm/mach-shmobile/include/mach/r8a7778.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright (C) 2013  Renesas Solutions Corp.
  * Copyright (C) 2013  Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+ * Copyright (C) 2013  Cogent Embedded, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,6 +22,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/sh_eth.h>
 #include <linux/platform_data/camera-rcar.h>
+
+/* HPB-DMA slave IDs */
+enum {
+	HPBDMA_SLAVE_DUMMY,
+	HPBDMA_SLAVE_SDHI0_TX,
+	HPBDMA_SLAVE_SDHI0_RX,
+};
 
 extern void r8a7778_add_standard_devices(void);
 extern void r8a7778_add_standard_devices_dt(void);
