7ac22f19a7420b77cf015e94ce2f53a2af1b4c89
Rob Herring <rob.herring@calxeda.com>
2013-08-26 10:20:26 -0500

    mips: use early_init_dt_scan
    Convert mips to use new early_init_dt_scan function.
    
    Remove early_init_dt_scan_memory_arch
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: linux-mips@linux-mips.org
    Acked-by: John Crispin <blogic@openwrt.org>

--- arch/mips/include/asm/prom.h
+++ arch/mips/include/asm/prom.h
@@ -18,9 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/bootinfo.h>
 
-extern int early_init_dt_scan_memory_arch(unsigned long node,
-	const char *uname, int depth, void *data);
-
 extern void device_tree_init(void);
 
 static inline unsigned long pci_address_to_pio(phys_addr_t address)
