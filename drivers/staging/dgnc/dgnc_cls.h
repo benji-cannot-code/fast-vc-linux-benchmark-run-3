76e1a9be9cfe07362599ff0c2ae24a1bc60233ce
Lidza Louina <lidza.louina@gmail.com>
2013-08-26 10:53:09 -0400

    staging: dgnc: cls_uart_struct: adds marker and changes vars' types for sparse
    This patch removes these sparse warnings found in the cls.c file:
    
    warning: incorrect type in argument 1 (different address spaces)
      expected void const volatile [noderef] <asn:2>*addr
      got unsigned char volatile *<noident>
    warning: incorrect type in argument 2 (different address spaces)
      expected void volatile [noderef] <asn:2>*addr
      got unsigned char volatile *<noident>
    
    The variables passed to readb and writeb need to
    be of type u8 with a __iomem marker. These warnings
    were popping up everytime the readb and writeb
    functions were called with a cls_uart_struct variable.
    
    The change made to the driver.h file adds the marker
    to the cls_uart_struct and the changes in cls.h
    changes its variables' types.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_cls.h
+++ drivers/staging/dgnc/dgnc_cls.h
@@ -37,14 +37,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  ************************************************************************/
 
 struct cls_uart_struct {
-	volatile uchar txrx;		/* WR  RHR/THR - Holding Reg */
-	volatile uchar ier;		/* WR  IER - Interrupt Enable Reg */
-	volatile uchar isr_fcr;		/* WR  ISR/FCR - Interrupt Status Reg/Fifo Control Reg */
-	volatile uchar lcr;		/* WR  LCR - Line Control Reg */
-	volatile uchar mcr;		/* WR  MCR - Modem Control Reg */
-	volatile uchar lsr;		/* WR  LSR - Line Status Reg */
-	volatile uchar msr;		/* WR  MSR - Modem Status Reg */
-	volatile uchar spr;		/* WR  SPR - Scratch Pad Reg */
+	u8 txrx;		/* WR  RHR/THR - Holding Reg */
+	u8 ier;		/* WR  IER - Interrupt Enable Reg */
+	u8 isr_fcr;		/* WR  ISR/FCR - Interrupt Status Reg/Fifo Control Reg */
+	u8 lcr;		/* WR  LCR - Line Control Reg */
+	u8 mcr;		/* WR  MCR - Modem Control Reg */
+	u8 lsr;		/* WR  LSR - Line Status Reg */
+	u8 msr;		/* WR  MSR - Modem Status Reg */
+	u8 spr;		/* WR  SPR - Scratch Pad Reg */
 };
 
 /* Where to read the interrupt register (8bits) */
