322a9a6723cb225a2f69e7c23bb8a5be63b66c03
Stratos Karafotis <stratosk@semaphore.gr>
2013-08-26 21:37:28 +0300

    cpufreq: governors: Remove duplicate check of target freq in supported range
    Function __cpufreq_driver_target() checks if target_freq is within
    policy->min and policy->max range. generic_powersave_bias_target() also
    checks if target_freq is valid via a cpufreq_frequency_table_target()
    call. So, drop the unnecessary duplicate check in *_check_cpu().
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -68,8 +68,6 @@ static void cs_check_cpu(int cpu, unsigned int load)
 			return;
 
 		dbs_info->requested_freq += get_freq_target(cs_tuners, policy);
-		if (dbs_info->requested_freq > policy->max)
-			dbs_info->requested_freq = policy->max;
 
 		__cpufreq_driver_target(policy, dbs_info->requested_freq,
 			CPUFREQ_RELATION_H);
@@ -90,8 +88,6 @@ static void cs_check_cpu(int cpu, unsigned int load)
 			return;
 
 		dbs_info->requested_freq -= get_freq_target(cs_tuners, policy);
-		if (dbs_info->requested_freq < policy->min)
-			dbs_info->requested_freq = policy->min;
 
 		__cpufreq_driver_target(policy, dbs_info->requested_freq,
 				CPUFREQ_RELATION_L);
