650654a2b294edb07682930fefc353cdccf308b3
Jingoo Han <jg1.han@samsung.com>
2013-08-26 09:32:05 +0900

    dma: sh: remove unnecessary platform_set_drvdata()
    The driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Acked-by: Guennadi Liakhovetski <g.liakhovetski+renesas@gmail.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/sh/shdmac.c
+++ drivers/dma/sh/shdmac.c
@@ -891,7 +891,6 @@ rst_err:
 	pm_runtime_put(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 
-	platform_set_drvdata(pdev, NULL);
 	shdma_cleanup(&shdev->shdma_dev);
 eshdma:
 	synchronize_rcu();
@@ -919,8 +918,6 @@ static int sh_dmae_remove(struct platform_device *pdev)
 
 	sh_dmae_chan_remove(shdev);
 	shdma_cleanup(&shdev->shdma_dev);
-
-	platform_set_drvdata(pdev, NULL);
 
 	synchronize_rcu();
 
--- drivers/dma/sh/sudmac.c
+++ drivers/dma/sh/sudmac.c
@@ -392,7 +392,6 @@ static int sudmac_probe(struct platform_device *pdev)
 chan_probe_err:
 	sudmac_chan_remove(su_dev);
 
-	platform_set_drvdata(pdev, NULL);
 	shdma_cleanup(&su_dev->shdma_dev);
 
 	return err;
@@ -406,7 +405,6 @@ static int sudmac_remove(struct platform_device *pdev)
 	dma_async_device_unregister(dma_dev);
 	sudmac_chan_remove(su_dev);
 	shdma_cleanup(&su_dev->shdma_dev);
-	platform_set_drvdata(pdev, NULL);
 
 	return 0;
 }
