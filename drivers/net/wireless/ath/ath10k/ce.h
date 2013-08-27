28fdc1194592f94b313ae76a9a16405d9df8b9f7
Michal Kazior <michal.kazior@tieto.com>
2013-08-27 13:08:00 +0200

    ath10k: remove unused ce_attr parameters
    Some parameters were unused and are not required.
    They have no representation in firmware. Clean
    them up.
    
    Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/ce.h
+++ drivers/net/wireless/ath/ath10k/ce.h
@@ -315,9 +315,6 @@ struct ce_attr {
 	/* CE_ATTR_* values */
 	unsigned int flags;
 
-	/* currently not in use */
-	unsigned int priority;
-
 	/* #entries in source ring - Must be a power of 2 */
 	unsigned int src_nentries;
 
@@ -329,9 +326,6 @@ struct ce_attr {
 
 	/* #entries in destination ring - Must be a power of 2 */
 	unsigned int dest_nentries;
-
-	/* Future use */
-	void *reserved;
 };
 
 /*
