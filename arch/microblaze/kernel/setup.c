e6ee40ac2d266b4bf75a90571b9a035a0abb1e62
Michal Simek <michal.simek@xilinx.com>
2013-08-27 11:13:29 +0200

    microblaze: timer: Use CLKSRC_OF initialization
    Simplify timer initialization and prepare the driver
    for moving to drivers/clocksource folder.
    Also remove system-timer property from binding because
    the name is too generic.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/kernel/setup.c
+++ arch/microblaze/kernel/setup.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/init.h>
+#include <linux/clocksource.h>
 #include <linux/string.h>
 #include <linux/seq_file.h>
 #include <linux/cpu.h>
@@ -191,6 +192,11 @@ void __init machine_early_init(const char *cmdline, unsigned int ram,
 	/* Initialize global data */
 	per_cpu(KM, 0) = 0x1;	/* We start in kernel mode */
 	per_cpu(CURRENT_SAVE, 0) = (unsigned long)current;
+}
+
+void __init time_init(void)
+{
+	clocksource_of_init();
 }
 
 #ifdef CONFIG_DEBUG_FS
