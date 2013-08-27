e31fb9a7a848ad1933dfec32ff66218dd2c293a1
Peter Hurley <peter@hurleysoftware.com>
2013-08-27 16:12:54 -0400

    drm/nouveau/core: Add priv field for event handlers
    Provide private field for event handlers exclusive use.
    Convert nouveau_fence_wait_uevent() and
    nouveau_fence_wait_uevent_handler(); drop struct nouveau_fence_uevent.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/include/core/event.h
+++ drivers/gpu/drm/nouveau/core/include/core/event.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nouveau_eventh {
 	struct list_head head;
+	void *priv;
 	int (*func)(struct nouveau_eventh *, int index);
 };
 
