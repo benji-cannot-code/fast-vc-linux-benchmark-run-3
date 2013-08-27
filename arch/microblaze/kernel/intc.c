a6dc24cfe81f0e73fd6413a116d326e0b4d570a2
Michal Simek <michal.simek@xilinx.com>
2013-08-27 10:48:29 +0200

    microblaze: intc: Update header
    Update dates in header and add Xilinx to it.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/kernel/intc.c
+++ arch/microblaze/kernel/intc.c
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (C) 2007-2009 Michal Simek <monstr@monstr.eu>
+ * Copyright (C) 2007-2013 Michal Simek <monstr@monstr.eu>
+ * Copyright (C) 2012-2013 Xilinx, Inc.
  * Copyright (C) 2007-2009 PetaLogix
  * Copyright (C) 2006 Atmark Techno, Inc.
  *
