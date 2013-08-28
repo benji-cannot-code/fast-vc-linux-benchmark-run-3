297072812aaeeaae0458f5305bff30659b45c1e0
Laxman Dewangan <ldewangan@nvidia.com>
2013-08-28 17:26:22 +0530

    pinctrl: utils: include export.h to avoid warnings
    Include "linux/export.h" to avoid following warnings during compilation:
    
    /***
    pinctrl/pinctrl-utils.c:53:1: warning: data definition has no type or storage class [enabled by default]
    pinctrl/pinctrl-utils.c:53:1: warning: type defaults to 'int' in declaration of 'EXPORT_SYMBOL_GPL' [-Wimplicit-int]
    pinctrl/pinctrl-utils.c:53:1: warning: parameter names (without types) in function declaration [enabled by default]
    pinctrl/pinctrl-utils.c:70:1: warning: data definition has no type or storage class [enabled by default]
    pinctrl/pinctrl-utils.c:70:1: warning: type defaults to 'int' in declaration of 'EXPORT_SYMBOL_GPL' [-Wimplicit-int]
    pinctrl/pinctrl-utils.c:70:1: warning: parameter names (without types) in function declaration [enabled by default]
    pinctrl/pinctrl-utils.c:98:1: warning: data definition has no type or storage class [enabled by default]
    pinctrl/pinctrl-utils.c:98:1: warning: type defaults to 'int' in declaration of 'EXPORT_SYMBOL_GPL' [-Wimplicit-int]
    pinctrl/pinctrl-utils.c:98:1: warning: parameter names (without types) in function declaration [enabled by default]
    pinctrl/pinctrl-utils.c:122:1: warning: data definition has no type or storage class [enabled by default]
    pinctrl/pinctrl-utils.c:122:1: warning: type defaults to 'int' in declaration of 'EXPORT_SYMBOL_GPL' [-Wimplicit-int]
    pinctrl/pinctrl-utils.c:122:1: warning: parameter names (without types) in function declaration [enabled by default]
    pinctrl/pinctrl-utils.c:135:1: warning: data definition has no type or storage class [enabled by default]
    pinctrl/pinctrl-utils.c:135:1: warning: type defaults to 'int' in declaration of 'EXPORT_SYMBOL_GPL' [-Wimplicit-int]
    pinctrl/pinctrl-utils.c:135:1: warning: parameter names (without types) in function declaration [enabled by default]
    **/
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-utils.c
+++ drivers/pinctrl/pinctrl-utils.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 02111-1307, USA
  */
 #include <linux/device.h>
+#include <linux/export.h>
 #include <linux/kernel.h>
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/of.h>
