be1885cc427deaacdc45507582d8a70153f1c19a
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-28 11:05:21 +0530

    staging: rtl8188eu: Include version.h header in xmit_linux.c
    Include version.h header file as detected by versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/xmit_linux.c
+++ drivers/staging/rtl8188eu/os_dep/xmit_linux.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  ******************************************************************************/
 #define _XMIT_OSDEP_C_
 
+#include <linux/version.h>
 #include <osdep_service.h>
 #include <drv_types.h>
 
