ad502d4a87176d54776f19a989d643aa773c804a
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-28 11:05:20 +0530

    staging: rtl8188eu: Include version.h in usb_ops.h
    Include version.h header file as detected by versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/usb_ops.h
+++ drivers/staging/rtl8188eu/include/usb_ops.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __USB_OPS_H_
 #define __USB_OPS_H_
 
+#include <linux/version.h>
 #include <osdep_service.h>
 #include <drv_types.h>
 #include <osdep_intf.h>
