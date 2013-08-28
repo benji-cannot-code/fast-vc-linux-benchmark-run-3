9f2d782658cfda2129d296a141f2e91b28e831ed
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-28 11:25:02 +0530

    staging: rtl8188eu: Remove duplicate header inclusion in ioctl_linux.c
    Removed the header files included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/ioctl_linux.c
+++ drivers/staging/rtl8188eu/os_dep/ioctl_linux.c
@@ -30,13 +30,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <rtw_ioctl.h>
 #include <rtw_ioctl_set.h>
 #include <rtw_mp_ioctl.h>
-#include <rtw_mp_ioctl.h>
 #include <usb_ops.h>
 #include <rtw_version.h>
 #include <rtl8188e_hal.h>
 
 #include <rtw_mp.h>
-#include <rtl8188e_hal.h>
 #include <rtw_iol.h>
 
 #define RTL_IOCTL_WPA_SUPPLICANT	(SIOCIWFIRSTPRIV + 30)
