516abb6c423325c99b5c71d553afff0abc6af15c
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-28 11:25:01 +0530

    staging: rtl8188eu: Remove duplicate incusion in drv_types.h
    wlan_bssdef.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/drv_types.h
+++ drivers/staging/rtl8188eu/include/drv_types.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drv_types_linux.h>
 #include <rtw_ht.h>
 #include <rtw_cmd.h>
-#include <wlan_bssdef.h>
 #include <rtw_xmit.h>
 #include <rtw_recv.h>
 #include <hal_intf.h>
