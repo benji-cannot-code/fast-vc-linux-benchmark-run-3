f0f02fa81fd8cf54978eaa1f2e3ac64ef1e8f851
Geert Uytterhoeven <geert@linux-m68k.org>
2013-08-29 22:53:25 +0200

    staging: dgap: Add missing #include <linux/slab.h>
    drivers/staging/dgap/dgap_driver.c: In function ‘dgap_cleanup_module’:
    drivers/staging/dgap/dgap_driver.c:423: error: implicit declaration of function ‘kfree’
    drivers/staging/dgap/dgap_driver.c: In function ‘dgap_driver_kzmalloc’:
    drivers/staging/dgap/dgap_driver.c:940: error: implicit declaration of function ‘kmalloc’
    drivers/staging/dgap/dgap_driver.c:940: warning: initialization makes pointer from integer without a cast
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_driver.c
+++ drivers/staging/dgap/dgap_driver.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/delay.h>	/* For udelay */
+#include <linux/slab.h>
 #include <asm/uaccess.h>	/* For copy_from_user/copy_to_user */
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39)
