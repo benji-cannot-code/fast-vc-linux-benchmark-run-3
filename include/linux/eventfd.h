2cdd0ee7b48ed9cd6d3bd82e1e1db5d464e6a668
Al Viro <viro@zeniv.linux.org.uk>
2013-08-30 12:29:49 -0400

    Kill indirect include of file.h from eventfd.h, use fdget() in cgroup.c
    kernel/cgroup.c is the only place in the tree that relies on eventfd.h
    pulling file.h; move that include there.  Switch from eventfd_fget()/fput()
    to fdget()/fdput(), while we are at it - eventfd_ctx_fileget() will fail
    on non-eventfd descriptors just fine, no need to do that check twice...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/eventfd.h
+++ include/linux/eventfd.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_EVENTFD_H
 
 #include <linux/fcntl.h>
-#include <linux/file.h>
 #include <linux/wait.h>
 
 /*
@@ -26,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define EFD_SHARED_FCNTL_FLAGS (O_CLOEXEC | O_NONBLOCK)
 #define EFD_FLAGS_SET (EFD_SHARED_FCNTL_FLAGS | EFD_SEMAPHORE)
+
+struct file;
 
 #ifdef CONFIG_EVENTFD
 
