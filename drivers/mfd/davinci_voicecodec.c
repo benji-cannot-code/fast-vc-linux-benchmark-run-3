fac804a0cff3c3871f5ed33ed0ae16eadc5f1d38
Mark Brown <broonie@linaro.org>
2013-08-31 14:08:55 +0100

    mfd: davinci_voicecodec: Remove unused read and write functions
    These functions are not referenced anywhere, nor prototyped, so just
    remove them.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/davinci_voicecodec.c
+++ drivers/mfd/davinci_voicecodec.c
@@ -33,17 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/mfd/davinci_voicecodec.h>
 
-u32 davinci_vc_read(struct davinci_vc *davinci_vc, int reg)
-{
-	return __raw_readl(davinci_vc->base + reg);
-}
-
-void davinci_vc_write(struct davinci_vc *davinci_vc,
-					   int reg, u32 val)
-{
-	__raw_writel(val, davinci_vc->base + reg);
-}
-
 static int __init davinci_vc_probe(struct platform_device *pdev)
 {
 	struct davinci_vc *davinci_vc;
