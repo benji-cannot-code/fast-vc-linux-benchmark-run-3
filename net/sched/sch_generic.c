b29076f73ab11290a029eef7dba65f52fb655379
stephen hemminger <stephen@networkplumber.org>
2013-08-31 10:15:33 -0700

    qdisc: fix build with !CONFIG_NET_SCHED
    Multiqueue scheduler refers to default_qdisc_ops; therefore the
    variable definition needs to be moved to handle case where net
    scheduler API is not available.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/pkt_sched.h>
 #include <net/dst.h>
 
+/* Qdisc to use by default */
+const struct Qdisc_ops *default_qdisc_ops = &pfifo_fast_ops;
+EXPORT_SYMBOL(default_qdisc_ops);
+
 /* Main transmission queue. */
 
 /* Modifications to data participating in scheduling must be protected with
