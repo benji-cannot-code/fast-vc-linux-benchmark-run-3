8d4b286750bfa6c3e87f838627fae63083756273
Mark Brown <broonie@linaro.org>
2013-08-31 14:08:56 +0100

    mfd: davinci_voicecodec: Provide a regmap for register I/O
    This will be used to support refactoring of the ASoC CODEC driver to use
    a regmap.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/mfd/davinci_voicecodec.c
+++ drivers/mfd/davinci_voicecodec.c
@@ -28,10 +28,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/clk.h>
+#include <linux/regmap.h>
 
 #include <sound/pcm.h>
 
 #include <linux/mfd/davinci_voicecodec.h>
+
+static struct regmap_config davinci_vc_regmap = {
+	.reg_bits = 32,
+	.val_bits = 32,
+};
 
 static int __init davinci_vc_probe(struct platform_device *pdev)
 {
@@ -61,6 +67,14 @@ static int __init davinci_vc_probe(struct platform_device *pdev)
 	davinci_vc->base = devm_ioremap_resource(&pdev->dev, res);
 	if (IS_ERR(davinci_vc->base)) {
 		ret = PTR_ERR(davinci_vc->base);
+		goto fail;
+	}
+
+	davinci_vc->regmap = devm_regmap_init_mmio(&pdev->dev,
+						   davinci_vc->base,
+						   &davinci_vc_regmap);
+	if (IS_ERR(davinci_vc->regmap)) {
+		ret = PTR_ERR(davinci_vc->regmap);
 		goto fail;
 	}
 
