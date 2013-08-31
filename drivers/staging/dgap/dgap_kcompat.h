599853b57e4d5537c60904b53ef232d174de0ef2
Sachin Kamat <sachin.kamat@linaro.org>
2013-08-31 10:58:23 +0530

    staging: dgap: Remove version check in dgap_kcompat.h
    Code should be for the kernel version it is merged in.
    Version check is not necessary.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_kcompat.h
+++ drivers/staging/dgap/dgap_kcompat.h
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DGAP_KCOMPAT_H
 #define __DGAP_KCOMPAT_H
 
-# ifndef KERNEL_VERSION
-#  define KERNEL_VERSION(a,b,c)  (((a) << 16) + ((b) << 8) + (c))
-# endif
-
-
 #if !defined(TTY_FLIPBUF_SIZE)
 # define TTY_FLIPBUF_SIZE 512
 #endif
@@ -66,29 +61,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		ulong dgap_##VAR; \
 		module_param(VAR, long, PERM); \
 		MODULE_PARM_DESC(VAR, DESC);
-
-
-
-
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,27)
-
-
-
-
-/* NOTHING YET */
-
-
-
-
-# else
-
-
-
-# error "this driver does not support anything below the 2.6.27 kernel series."
-
-
-
-# endif
 
 #endif /* ! __DGAP_KCOMPAT_H */
