4f3be22bd3edcae24b65cf5e8b50669aa2c35886
Steven Rostedt <rostedt@goodmis.org>
2013-09-01 22:56:21 +0200

    avr32: use Kbuild infrastructure to handle the asm-generic headers
    Use kbuild to add asm-generic headers that do nothing, also remove the arch
    specific wrapper headers.
    
    This only affects headers that do nothing but include the generic
    equivalent. It does not touch any header that does a little more.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Hans-Christian Egtvedt <egtvedt@samfundet.no>

--- arch/avr32/include/asm/div64.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef __ASM_AVR32_DIV64_H
-#define __ASM_AVR32_DIV64_H
-
-#include <asm-generic/div64.h>
-
-#endif /* __ASM_AVR32_DIV64_H */
