40f0f2824911e0df18c6cbd7c01af9ad2f510cb8
Alexandru Juncu <alexj@rosedu.org>
2013-09-04 11:41:34 -0300

    [media] dm1105: remove unneeded not-null test
    i2c_adap is a field of a struct and will always be allocated so
    its address will never be null.
    Suggested by coccinelle, manually verified.
    
    Signed-off-by: Alexandru Juncu <alexj@rosedu.org>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/pci/dm1105/dm1105.c
+++ drivers/media/pci/dm1105/dm1105.c
@@ -1202,8 +1202,7 @@ static void dm1105_remove(struct pci_dev *pdev)
 	dvb_dmxdev_release(&dev->dmxdev);
 	dvb_dmx_release(dvbdemux);
 	dvb_unregister_adapter(dvb_adapter);
-	if (&dev->i2c_adap)
-		i2c_del_adapter(&dev->i2c_adap);
+	i2c_del_adapter(&dev->i2c_adap);
 
 	dm1105_hw_exit(dev);
 	synchronize_irq(pdev->irq);
