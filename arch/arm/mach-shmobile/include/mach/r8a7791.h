1740cf20e33357292d020cfab4042d74953db0d0
Hisashi Nakamura <hisashi.nakamura.ak@renesas.com>
2013-09-04 12:45:57 +0900

    ARM: shmobile: Initial r8a7791 SoC support
    Add initial support for the r8a7791 SoC including:
     - Single Cortex-A15 CPU Core
     - GIC
    
    No static virtual mappings are used, all the components
    make use of ioremap(). DT_MACHINE_START is still wrapped
    in CONFIG_USE_OF to match other mach-shmobile code.
    
    Signed-off-by: Hisashi Nakamura <hisashi.nakamura.ak@renesas.com>
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>
    [damm@opensource.se: Forward ported to upstream, dropped not-yet-ready code]
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

---
+++ arch/arm/mach-shmobile/include/mach/r8a7791.h
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ASM_R8A7791_H__
+#define __ASM_R8A7791_H__
+
+void r8a7791_clock_init(void);
+
+#endif /* __ASM_R8A7791_H__ */
