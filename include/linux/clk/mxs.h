c038eda70f0fa027ad1fb18a097dbe0cae1a0c6b
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2013-09-04 13:16:01 +0200

    ARM: mxs: remove custom .init_time hook
    This patch converts clk-imx2[38] clocksource_of_init compatible init
    associated with fsl,imx2[38]-clkctrl. With arch/arm calling
    of_clk_init(NULL) from time_init(), we can now also remove custom
    .init_time hooks.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Acked-by: Mike Turquette <mturquette@linaro.org>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>

--- include/linux/clk/mxs.h
+++ include/linux/clk/mxs.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_CLK_MXS_H
 #define __LINUX_CLK_MXS_H
 
-int mx23_clocks_init(void);
-int mx28_clocks_init(void);
 int mxs_saif_clkmux_select(unsigned int clkmux);
 
 #endif
