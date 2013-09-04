1369a6d5543340bf50fd1e21f2dc5956c34a7c1f
govindarajulu.v <govindarajulu90@gmail.com>
2013-09-04 11:17:14 +0530

    driver/net: enic: Add multi tx support for enic
    The following patch adds multi tx support for enic.
    
    Signed-off-by: Nishank Trivedi <nistrive@cisco.com>
    Signed-off-by: Christian Benvenuti <benve@cisco.com>
    Signed-off-by: Govindarajulu Varadarajan <govindarajulu90@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cisco/enic/enic.h
+++ drivers/net/ethernet/cisco/enic/enic.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ENIC_BARS_MAX		6
 
-#define ENIC_WQ_MAX		1
+#define ENIC_WQ_MAX		8
 #define ENIC_RQ_MAX		8
 #define ENIC_CQ_MAX		(ENIC_WQ_MAX + ENIC_RQ_MAX)
 #define ENIC_INTR_MAX		(ENIC_CQ_MAX + 2)
