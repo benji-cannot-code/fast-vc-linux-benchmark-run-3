4621f6ce95629b23a02d47866a2326f08cfbfb98
Sachin Kamat <sachin.kamat@linaro.org>
2013-09-05 09:22:03 +0530

    regulator: Remove redundant NULL assignment
    NULL assignment corrupts the error pointer and is not
    necessary.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/regulator/max8997.c
+++ drivers/regulator/max8997.c
@@ -1210,7 +1210,6 @@ static int max8997_pmic_probe(struct platform_device *pdev)
 		if (IS_ERR(rdev[i])) {
 			dev_err(max8997->dev, "regulator init failed for %d\n",
 					id);
-			rdev[i] = NULL;
 			return PTR_ERR(rdev[i]);
 		}
 	}
