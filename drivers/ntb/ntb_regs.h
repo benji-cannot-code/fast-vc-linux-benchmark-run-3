5edac80446c13537afda027314bdb5cd5fea497b
Jon Mason <jon.mason@intel.com>
2013-09-06 16:51:16 -0700

    NTB: Xeon Doorbell errata workaround
    Modifications to the 14th bit of the B2BDOORBELL register will not be
    mirrored to the remote system due to a hardware issue.  To get around
    the issue, shrink the number of available doorbell bits by 1.  The max
    number of doorbells was being used as a way to referencing the Link
    Doorbell bit.  Since this would no longer work, the driver must now
    explicitly reference that bit.
    
    This does not affect the xeon_errata_workaround case, as it is not using
    the b2bdoorbell register.
    
    Signed-off-by: Jon Mason <jon.mason@intel.com>

--- drivers/ntb/ntb_regs.h
+++ drivers/ntb/ntb_regs.h
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SNB_MAX_COMPAT_SPADS	16
 /* Reserve the uppermost bit for link interrupt */
 #define SNB_MAX_DB_BITS		15
+#define SNB_LINK_DB		15
 #define SNB_DB_BITS_PER_VEC	5
 #define SNB_MAX_MW		2
 #define SNB_ERRATA_MAX_MW	1
