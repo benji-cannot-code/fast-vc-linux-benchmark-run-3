0dfedcbfe9420b823099510c2e9bddb1b0ecfda6
Sachin Kamat <sachin.kamat@linaro.org>
2013-09-06 13:31:09 +0530

    ab8500-charger: Check return value of regulator_enable
    Check the return value of regulator_enable to silence the following
    type of warnings:
    drivers/power/ab8500_charger.c:1390:20: warning: ignoring return value
    of ‘regulator_enable’, declared with attribute warn_unused_result
    [-Wunused-result]
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Anton Vorontsov <anton@enomsg.org>

--- drivers/power/ab8500_charger.c
+++ drivers/power/ab8500_charger.c
@@ -1388,8 +1388,12 @@ static int ab8500_charger_ac_en(struct ux500_charger *charger,
 		 * the GPADC module independant of the AB8500 chargers
 		 */
 		if (!di->vddadc_en_ac) {
-			regulator_enable(di->regu);
-			di->vddadc_en_ac = true;
+			ret = regulator_enable(di->regu);
+			if (ret)
+				dev_warn(di->dev,
+					"Failed to enable regulator\n");
+			else
+				di->vddadc_en_ac = true;
 		}
 
 		/* Check if the requested voltage or current is valid */
@@ -1557,8 +1561,12 @@ static int ab8500_charger_usb_en(struct ux500_charger *charger,
 		 * the GPADC module independant of the AB8500 chargers
 		 */
 		if (!di->vddadc_en_usb) {
-			regulator_enable(di->regu);
-			di->vddadc_en_usb = true;
+			ret = regulator_enable(di->regu);
+			if (ret)
+				dev_warn(di->dev,
+					"Failed to enable regulator\n");
+			else
+				di->vddadc_en_usb = true;
 		}
 
 		/* Enable USB charging */
