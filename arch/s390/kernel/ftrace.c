9b50518dc76550e9c0eec21107950e296e4297a2
Heiko Carstens <heiko.carstens@de.ibm.com>
2013-09-06 19:16:14 +0200

    s390/ftrace: avoid pointer arithmetics with function pointers
    Pointer arithmetics with function pointers is not really defined, but
    seems to do the right thing. Let's cast to a void pointer to have a
    defined behaviour, at least when using gcc.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/kernel/ftrace.c
+++ arch/s390/kernel/ftrace.c
@@ -179,7 +179,7 @@ int ftrace_enable_ftrace_graph_caller(void)
 
 	offset = ((void *) prepare_ftrace_return -
 		  (void *) ftrace_graph_caller) / 2;
-	return probe_kernel_write(ftrace_graph_caller + 2,
+	return probe_kernel_write((void *) ftrace_graph_caller + 2,
 				  &offset, sizeof(offset));
 }
 
@@ -187,7 +187,7 @@ int ftrace_disable_ftrace_graph_caller(void)
 {
 	static unsigned short offset = 0x0002;
 
-	return probe_kernel_write(ftrace_graph_caller + 2,
+	return probe_kernel_write((void *) ftrace_graph_caller + 2,
 				  &offset, sizeof(offset));
 }
 
