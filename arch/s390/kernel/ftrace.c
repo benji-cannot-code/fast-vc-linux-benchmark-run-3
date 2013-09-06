e39e4a2a32c447f88ff9dc8fcb85823670137e28
Heiko Carstens <heiko.carstens@de.ibm.com>
2013-09-06 19:10:48 +0200

    s390: make various functions static, add declarations to header files
    Make various functions static, add declarations to header files to
    fix a couple of sparse findings.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/s390/kernel/ftrace.c
+++ arch/s390/kernel/ftrace.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kprobes.h>
 #include <trace/syscall.h>
 #include <asm/asm-offsets.h>
+#include "entry.h"
 
 #ifdef CONFIG_DYNAMIC_FTRACE
 
