e4985076e74c853796a87a271a1d542ea98da0f2
Yinghai Lu <yinghai@kernel.org>
2013-09-06 19:08:00 -0700

    ACPI / x86: Increase override tables number limit
    Current ACPI tables in initrd is limited to 10, that is too small.
    64 should be good enough as we have 35 sigs and could have several
    SSDT.
    
    Two problems in current code prevent us from increasing limit:
     1. The cpio file info array is put in stack, as every element is 32
        bytes, could run out of stack if we have that array size to 64.
        We can move it out from stack, make it global and put it into the
        __initdata section.
     2. early_ioremap() only can remap 256k one time. Current code maps
        10 tables at a time. If we increased that limit, the whole size
        could be more than 256k, so early_ioremap() would fail with that.
        We can map chunks one by one during copying, instead of mapping
        all of them together.
    
    Signed-off-by: Yinghai <yinghai@kernel.org>
    Acked-by: Tejun Heo <tj@kernel.org>
    Tested-by: Thomas Renninger <trenn@suse.de>
    Reviewed-by: Tang Chen <tangchen@cn.fujitsu.com>
    Tested-by: Tang Chen <tangchen@cn.fujitsu.com>
    Acked-by: Toshi Kani <toshi.kani@hp.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- arch/x86/include/asm/acpi.h
+++ arch/x86/include/asm/acpi.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <acpi/pdc_intel.h>
 
 #include <asm/numa.h>
+#include <asm/fixmap.h>
 #include <asm/processor.h>
 #include <asm/mmu.h>
 #include <asm/mpspec.h>
