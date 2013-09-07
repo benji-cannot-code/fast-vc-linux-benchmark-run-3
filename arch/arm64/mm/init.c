db21a8462acfa47df6351984bc7e56cf23d6f649
Rob Herring <rob.herring@calxeda.com>
2013-09-07 14:57:42 -0500

    arm64: remove unnecessary prom.h include
    Remove unnecessary prom.h include in preparation to make prom.h optional.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Grant Likely <grant.likely@linaro.org>
    Cc: Will Deacon <will.deacon@arm.com>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sort.h>
 #include <linux/of_fdt.h>
 
-#include <asm/prom.h>
 #include <asm/sections.h>
 #include <asm/setup.h>
 #include <asm/sizes.h>
