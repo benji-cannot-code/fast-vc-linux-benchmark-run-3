f8fcf5500b109a9299c8d2d0617f76cbe6cfd3e6
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2013-09-07 09:07:13 +0200

    ARM: davinci: remove deprecated IRQF_DISABLED
    This patch proposes to remove the IRQF_DISABLED flag from Davinci code ;)
    It's a NOOP since 2.6.35, and will be removed one day
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-davinci/time.c
+++ arch/arm/mach-davinci/time.c
@@ -182,7 +182,7 @@ static struct timer_s timers[] = {
 		.name      = "clockevent",
 		.opts      = TIMER_OPTS_DISABLED,
 		.irqaction = {
-			.flags   = IRQF_DISABLED | IRQF_TIMER,
+			.flags   = IRQF_TIMER,
 			.handler = timer_interrupt,
 		}
 	},
@@ -191,7 +191,7 @@ static struct timer_s timers[] = {
 		.period     = ~0,
 		.opts       = TIMER_OPTS_PERIODIC,
 		.irqaction = {
-			.flags   = IRQF_DISABLED | IRQF_TIMER,
+			.flags   = IRQF_TIMER,
 			.handler = freerun_interrupt,
 		}
 	},
