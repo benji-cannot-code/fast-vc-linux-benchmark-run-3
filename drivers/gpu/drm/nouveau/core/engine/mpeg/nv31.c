9885823f25d8c6b3335c90b0e765922c48de92ec
Ilia Mirkin <imirkin@alum.mit.edu>
2013-09-07 21:04:10 -0400

    drm/nv40/mpeg: use the nv31-provided classes
    Since nv40 only covers pre-nv44 now, it can use the nv31-provided
    functions.
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/mpeg/nv31.c
+++ drivers/gpu/drm/nouveau/core/engine/mpeg/nv31.c
@@ -35,15 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <engine/fifo.h>
 #include <engine/mpeg.h>
-
-struct nv31_mpeg_priv {
-	struct nouveau_mpeg base;
-	atomic_t refcount;
-};
-
-struct nv31_mpeg_chan {
-	struct nouveau_object base;
-};
+#include <engine/mpeg/nv31.h>
 
 /*******************************************************************************
  * MPEG object classes
@@ -168,7 +160,7 @@ nv31_mpeg_context_dtor(struct nouveau_object *object)
 	nouveau_object_destroy(&chan->base);
 }
 
-static struct nouveau_oclass
+struct nouveau_oclass
 nv31_mpeg_cclass = {
 	.handle = NV_ENGCTX(MPEG, 0x31),
 	.ofuncs = &(struct nouveau_ofuncs) {
