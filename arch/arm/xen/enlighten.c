baba595c2da032a439e0a56f1b67a339838a85ec
Julien Grall <julien.grall@linaro.org>
2013-09-09 11:35:26 +0000

    xen/arm: disable cpuidle and cpufreq when linux is running as dom0
    When linux is running as dom0, Xen doesn't show the physical cpu but a
    virtual CPU.
    On some ARM SOC (for instance the exynos 5250), linux registers callbacks
    for cpuidle and cpufreq. When these callbacks are called, they will modify
    directly the physical cpu not the virtual one. It can impact the whole board
    instead of only dom0.
    
    Signed-off-by: Julien Grall <julien.grall@linaro.org>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- arch/arm/xen/enlighten.c
+++ arch/arm/xen/enlighten.c
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_irq.h>
 #include <linux/of_address.h>
+#include <linux/cpuidle.h>
+#include <linux/cpufreq.h>
 
 #include <linux/mm.h>
 
@@ -266,6 +268,13 @@ static int __init xen_guest_init(void)
 	gnttab_init();
 	if (!xen_initial_domain())
 		xenbus_probe(NULL);
+
+	/*
+	 * Making sure board specific code will not set up ops for
+	 * cpu idle and cpu freq.
+	 */
+	disable_cpuidle();
+	disable_cpufreq();
 
 	return 0;
 }
