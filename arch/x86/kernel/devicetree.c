60e1dc1623db61d8458ee27fcadb95fba21163cd
Rob Herring <rob.herring@calxeda.com>
2013-09-09 18:13:22 -0500

    x86: add necessary includes for prom.h
    Once prom.h is no longer implicitly included, we need to include setup.h
    to get COMMAND_LINE_SIZE.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Grant Likely <grant.likely@linaro.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: x86@kernel.org

--- arch/x86/kernel/devicetree.c
+++ arch/x86/kernel/devicetree.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/hpet.h>
 #include <asm/apic.h>
 #include <asm/pci_x86.h>
+#include <asm/setup.h>
 
 __initdata u64 initial_dtb;
 char __initdata cmd_line[COMMAND_LINE_SIZE];
