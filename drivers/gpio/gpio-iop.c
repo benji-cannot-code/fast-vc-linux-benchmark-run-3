38ba63aeaec9c7007de28a28f10abb35d47a2bf7
Linus Walleij <linus.walleij@linaro.org>
2013-09-09 15:00:40 +0200

    gpio: decouple the IOP GPIO driver from platform
    This removes the only dependence between the IOP GPIO
    driver and the GENERIC_GPIO header in <mach/gpio.h>
    and its common implementation in the <asm/*> namespace
    by copying the one constant it is using into the driver
    file.
    
    Cc: Lennert Buytenhek <kernel@wantstofly.org>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Mikael Pettersson <mikpe@it.uu.se>
    Tested-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-iop.c
+++ drivers/gpio/gpio-iop.c
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/gpio.h>
 #include <linux/export.h>
-#include <asm/hardware/iop3xx.h>
-#include <mach/gpio.h>
+
+#define IOP3XX_N_GPIOS	8
 
 void gpio_line_config(int line, int direction)
 {
