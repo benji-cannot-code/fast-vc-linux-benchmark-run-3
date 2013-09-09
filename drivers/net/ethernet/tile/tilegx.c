845f05d7b92b0973293121e0bc375258819c1732
Chris Metcalf <cmetcalf@tilera.com>
2013-09-09 14:11:54 -0400

    net: tilegx driver: avoid compiler warning
    The "id" variable was being incremented in common code, but only
    initialized and used in IPv4 code.  We move the increment to the IPv4
    code too, and then legitimately use the uninitialized_var() macro to
    avoid the gcc 4.6 warning that 'id' may be used uninitialized.
    Note that gcc 4.7 does not warn.
    
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/tile/tilegx.c
+++ drivers/net/ethernet/tile/tilegx.c
@@ -1735,7 +1735,8 @@ static void tso_headers_prepare(struct sk_buff *skb, unsigned char *headers,
 	unsigned int data_len = skb->len - sh_len;
 	unsigned char *data = skb->data;
 	unsigned int ih_off, th_off, p_len;
-	unsigned int isum_seed, tsum_seed, id, seq;
+	unsigned int isum_seed, tsum_seed, seq;
+	unsigned int uninitialized_var(id);
 	int is_ipv6;
 	long f_id = -1;    /* id of the current fragment */
 	long f_size = skb_headlen(skb) - sh_len;  /* current fragment size */
@@ -1782,7 +1783,7 @@ static void tso_headers_prepare(struct sk_buff *skb, unsigned char *headers,
 		} else {
 			ih = (struct iphdr *)(buf + ih_off);
 			ih->tot_len = htons(sh_len + p_len - ih_off);
-			ih->id = htons(id);
+			ih->id = htons(id++);
 			ih->check = csum_long(isum_seed + ih->tot_len +
 					      ih->id) ^ 0xffff;
 		}
@@ -1819,7 +1820,6 @@ static void tso_headers_prepare(struct sk_buff *skb, unsigned char *headers,
 			slot++;
 		}
 
-		id++;
 		seq += p_len;
 
 		/* The last segment may be less than gso_size. */
