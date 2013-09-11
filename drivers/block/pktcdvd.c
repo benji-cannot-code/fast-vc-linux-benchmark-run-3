1b8e8e18345c268013ed81734abf6be854fdbb17
Joe Perches <joe@perches.com>
2013-09-11 14:25:59 -0700

    pktcdvd: fix defective misuses of pkt_<level>
    Fix thinkos where pkt_<level> needs a valid pktcdvd_device * and the
    pointer is known to be NULL.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com> (go smatch!)
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/pktcdvd.c
+++ drivers/block/pktcdvd.c
@@ -2361,8 +2361,8 @@ static void pkt_make_request(struct request_queue *q, struct bio *bio)
 
 	pd = q->queuedata;
 	if (!pd) {
-		pkt_err(pd, "%s incorrect request queue\n",
-			bdevname(bio->bi_bdev, b));
+		pr_err("%s incorrect request queue\n",
+		       bdevname(bio->bi_bdev, b));
 		goto end_io;
 	}
 
@@ -2842,7 +2842,7 @@ static int pkt_remove_dev(dev_t pkt_dev)
 			break;
 	}
 	if (idx == MAX_WRITERS) {
-		pkt_dbg(1, pd, "dev not setup\n");
+		pr_debug("dev not setup\n");
 		ret = -ENXIO;
 		goto out;
 	}
