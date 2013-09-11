62d00099e2beed7f9d00a10f9ec2e13fca77433d
Mark Einon <mark.einon@gmail.com>
2013-09-11 14:14:43 +0100

    staging: et131x: Remove unused spinlock
    phy_lock is no longer used in any useful code, it's all been moved into
    a phy_device. Remove the lock definition and init.
    
    Signed-off-by: Mark Einon <mark.einon@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/et131x/et131x.c
+++ drivers/staging/et131x/et131x.c
@@ -497,8 +497,6 @@ struct et131x_adapter {
 	spinlock_t rcv_pend_lock;
 	spinlock_t fbr_lock;
 
-	spinlock_t phy_lock;
-
 	/* Packet Filter and look ahead size */
 	u32 packet_filter;
 
@@ -3929,7 +3927,6 @@ static struct et131x_adapter *et131x_adapter_init(struct net_device *netdev,
 	spin_lock_init(&adapter->rcv_lock);
 	spin_lock_init(&adapter->rcv_pend_lock);
 	spin_lock_init(&adapter->fbr_lock);
-	spin_lock_init(&adapter->phy_lock);
 
 	adapter->registry_jumbo_packet = 1514;	/* 1514-9216 */
 
