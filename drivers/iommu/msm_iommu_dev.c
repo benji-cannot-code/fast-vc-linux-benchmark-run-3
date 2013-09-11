3300be5587a336f05a8932459bd15b0fcef20ac4
Jingoo Han <jg1.han@samsung.com>
2013-09-11 14:19:43 -0700

    drivers/iommu: remove unnecessary platform_set_drvdata()
    The driver core clears the driver data to NULL after device_release or
    on probe failure.  Thus, it is not needed to manually clear the device
    driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Cc: David Brown <davidb@codeaurora.org>
    Cc: Stephen Boyd <sboyd@codeaurora.org>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Suman Anna <s-anna@ti.com>
    Acked-by: Libo Chen <libo.chen@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/iommu/msm_iommu_dev.c
+++ drivers/iommu/msm_iommu_dev.c
@@ -283,7 +283,6 @@ static int msm_iommu_remove(struct platform_device *pdev)
 		clk_put(drv->pclk);
 		memset(drv, 0, sizeof(*drv));
 		kfree(drv);
-		platform_set_drvdata(pdev, NULL);
 	}
 	return 0;
 }
@@ -367,7 +366,6 @@ static int msm_iommu_ctx_remove(struct platform_device *pdev)
 	if (drv) {
 		memset(drv, 0, sizeof(struct msm_iommu_ctx_drvdata));
 		kfree(drv);
-		platform_set_drvdata(pdev, NULL);
 	}
 	return 0;
 }
