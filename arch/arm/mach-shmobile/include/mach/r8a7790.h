180f0f4c83b110d5b97551671833471de233d324
Magnus Damm <damm@opensource.se>
2013-09-12 09:32:49 +0900

    ARM: shmobile: Break out R-Car Gen2 setup code
    Move arch timer workaround code and boot mode pin
    handling from setup-r8a7790.c to setup-rcar-gen2.c.
    
    With this in place the same code can be used on
    other R-Car Generation 2 devices such as r8a7791.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/r8a7790.h
+++ arch/arm/mach-shmobile/include/mach/r8a7790.h
@@ -2,15 +2,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_R8A7790_H__
 #define __ASM_R8A7790_H__
 
+#include <mach/rcar-gen2.h>
+
 void r8a7790_add_standard_devices(void);
 void r8a7790_add_dt_devices(void);
 void r8a7790_clock_init(void);
 void r8a7790_pinmux_init(void);
 void r8a7790_init_early(void);
-void r8a7790_timer_init(void);
 extern struct smp_operations r8a7790_smp_ops;
-
-#define MD(nr) BIT(nr)
-u32 r8a7790_read_mode_pins(void);
 
 #endif /* __ASM_R8A7790_H__ */
