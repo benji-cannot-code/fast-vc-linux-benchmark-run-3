80291f3d65a0deeb54d30f2de02711593aae712f
Jingoo Han <jg1.han@samsung.com>
2013-09-12 15:33:48 +0900

    serial: 8250_pci: remove unnecessary pci_set_drvdata()
    The driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_pci.c
+++ drivers/tty/serial/8250/8250_pci.c
@@ -3521,8 +3521,6 @@ static void pciserial_remove_one(struct pci_dev *dev)
 {
 	struct serial_private *priv = pci_get_drvdata(dev);
 
-	pci_set_drvdata(dev, NULL);
-
 	pciserial_remove_ports(priv);
 
 	pci_disable_device(dev);
