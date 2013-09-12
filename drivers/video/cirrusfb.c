89f76307beb01bbec2c28cbd614b66c9bc29ba39
Jingoo Han <jg1.han@samsung.com>
2013-09-12 15:51:57 +0900

    video: cirrusfb: remove unnecessary pci_set_drvdata()
    The driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/cirrusfb.c
+++ drivers/video/cirrusfb.c
@@ -2160,7 +2160,6 @@ static int cirrusfb_pci_register(struct pci_dev *pdev,
 	if (!ret)
 		return 0;
 
-	pci_set_drvdata(pdev, NULL);
 	iounmap(info->screen_base);
 err_release_legacy:
 	if (release_io_ports)
