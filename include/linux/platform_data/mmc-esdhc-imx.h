7805fb3e12eb6d975c58c35bd360c4a6f12e7b5e
Dong Aisheng <b29396@freescale.com>
2013-09-13 19:11:35 +0800

    mmc: sdhci-esdhc-imx: change pinctrl state according to uhs mode
    Without proper pinctrl state, the card may not be able to work
    on high speed stablely. e.g. SDR104.
    
    This patch add pinctrl state switch code according to different
    uhs mode include 100mhz sate, 200mhz sate and normal state
    (50Mhz and below).
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/platform_data/mmc-esdhc-imx.h
+++ include/linux/platform_data/mmc-esdhc-imx.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_ARCH_IMX_ESDHC_H
 #define __ASM_ARCH_IMX_ESDHC_H
 
+#include <linux/types.h>
+
 enum wp_types {
 	ESDHC_WP_NONE,		/* no WP, neither controller nor gpio */
 	ESDHC_WP_CONTROLLER,	/* mmc controller internal WP */
@@ -33,6 +35,7 @@ enum cd_types {
  * @cd_gpio:	gpio for card_detect interrupt
  * @wp_type:	type of write_protect method (see wp_types enum above)
  * @cd_type:	type of card_detect method (see cd_types enum above)
+ * @support_vsel:  indicate it supports 1.8v switching
  */
 
 struct esdhc_platform_data {
@@ -42,5 +45,6 @@ struct esdhc_platform_data {
 	enum cd_types cd_type;
 	int max_bus_width;
 	unsigned int f_max;
+	bool support_vsel;
 };
 #endif /* __ASM_ARCH_IMX_ESDHC_H */
