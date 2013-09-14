22832a1bdba5706c499f630c12def84202544474
Willy Tarreau <w@1wt.eu>
2013-09-14 10:32:59 +0200

    perf symbols: Add new option --ignore-vmlinux for perf top
    Running "perf top" on a machine with possibly invalid or non-matching
    vmlinux at the various places results in no symbol resolving despite
    /proc/kallsyms being present and valid.
    
    Add a new option --ignore-vmlinux to explicitly indicate that we do not
    want to use these kernels and just use what we have (kallsyms).
    
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Cc: Ingo Molnar <mingo@redhat.com>
    Link: http://lkml.kernel.org/r/20130914083259.GA3418@1wt.eu
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -1216,7 +1216,7 @@ static int dso__load_kernel_sym(struct dso *dso, struct map *map,
 		goto do_kallsyms;
 	}
 
-	if (symbol_conf.vmlinux_name != NULL) {
+	if (!symbol_conf.ignore_vmlinux && symbol_conf.vmlinux_name != NULL) {
 		err = dso__load_vmlinux(dso, map,
 					symbol_conf.vmlinux_name, filter);
 		if (err > 0) {
@@ -1228,7 +1228,7 @@ static int dso__load_kernel_sym(struct dso *dso, struct map *map,
 		return err;
 	}
 
-	if (vmlinux_path != NULL) {
+	if (!symbol_conf.ignore_vmlinux && vmlinux_path != NULL) {
 		err = dso__load_vmlinux_path(dso, map, filter);
 		if (err > 0)
 			return err;
