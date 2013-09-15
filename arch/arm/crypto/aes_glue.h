36ee5dd5319e11030842afda539854940ded3a7d
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2013-09-15 17:10:43 +0200

    ARM: move AES typedefs and function prototypes to separate header
    Put the struct definitions for AES keys and the asm function prototypes in a
    separate header and export the asm functions from the module.
    This allows other drivers to use them directly.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>

---
+++ arch/arm/crypto/aes_glue.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+
+#define AES_MAXNR 14
+
+struct AES_KEY {
+	unsigned int rd_key[4 * (AES_MAXNR + 1)];
+	int rounds;
+};
+
+struct AES_CTX {
+	struct AES_KEY enc_key;
+	struct AES_KEY dec_key;
+};
+
+asmlinkage void AES_encrypt(const u8 *in, u8 *out, struct AES_KEY *ctx);
+asmlinkage void AES_decrypt(const u8 *in, u8 *out, struct AES_KEY *ctx);
+asmlinkage int private_AES_set_decrypt_key(const unsigned char *userKey,
+					   const int bits, struct AES_KEY *key);
+asmlinkage int private_AES_set_encrypt_key(const unsigned char *userKey,
+					   const int bits, struct AES_KEY *key);
