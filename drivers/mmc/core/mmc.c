e56175f27e696d628534911a4ae01c4910c14db2
Ulf Hansson <ulf.hansson@linaro.org>
2013-09-16 11:28:42 +0200

    mmc: core: Prevent violation of specs while initializing cards
    According to eMMC/SD/SDIO specs, the VDD (VCC) voltage level must be
    maintained during the initialization sequence. If we want/need to tune
    the voltage level, a complete power cycle of the card must be executed.
    
    Most host drivers conforms to the specifications by only allowing to
    change VDD voltage level at the MMC_POWER_UP state, but some also cares
    about MMC_POWER_ON state, which they should'nt. This patch will not
    break those drivers, but they could clean up code to better reflect
    what is expected from the protocol layer.
    
    A big re-work of the mmc_select_voltage function is done to only change
    VDD voltage level if the host supports MMC_CAP2_FULL_PWR_CYCLE.
    Otherwise only validation of the host and card ocr mask will be done.
    
    A very nice side-effect of this patch is that we now don't need to
    reset the negotiated ocr mask at the mmc_power_off function, since now
    it will actually reflect the present voltage level, which safely can be
    used at the next power up and re-initialization. Moreover, we then only
    need to execute mmc_select_voltage from the attach sequence.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1536,7 +1536,6 @@ static int mmc_resume(struct mmc_host *host)
 
 	mmc_claim_host(host);
 	mmc_power_up(host, host->card->ocr);
-	mmc_select_voltage(host, host->card->ocr);
 	err = mmc_init_card(host, host->card->ocr, host->card);
 	mmc_release_host(host);
 
