c9bd5e8a134b512f908ebf10cdf79b14c5a7a352
Rob Herring <rob.herring@calxeda.com>
2013-09-16 21:05:05 -0500

    of: move of_translate_dma_address to of_address.h
    of_translate_dma_address is implemented in common code, so move the
    declaration there too.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Grant Likely <grant.likely@linaro.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: linuxppc-dev@lists.ozlabs.org

--- arch/powerpc/include/asm/prom.h
+++ arch/powerpc/include/asm/prom.h
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OF address retreival & translation
  */
 
-/* Translate a DMA address from device space to CPU space */
-extern u64 of_translate_dma_address(struct device_node *dev,
-				    const __be32 *in_addr);
-
 /* Parse the ibm,dma-window property of an OF node into the busno, phys and
  * size parameters.
  */
