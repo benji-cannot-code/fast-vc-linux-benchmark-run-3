f049644df9f70a2fe33d4b59608c400282829ee9
Magnus Damm <damm@opensource.se>
2013-09-19 05:11:11 +0900

    ARM: shmobile: Initial r7s72100 SoC support
    Add initial support for the r7272100 SoC including:
     - Single Cortex-A9 CPU Core
     - GIC
    
    No static virtual mappings are used, all the components
    make use of ioremap(). DT_MACHINE_START is still wrapped
    in CONFIG_USE_OF to match other mach-shmobile code.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

---
+++ arch/arm/mach-shmobile/include/mach/r7s72100.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ASM_R7S72100_H__
+#define __ASM_R7S72100_H__
+
+void r7s72100_clock_init(void);
+void r7s72100_init_early(void);
+
+#endif /* __ASM_R7S72100_H__ */
