d5a11c5835348ed77f48efda44e4ae7a97bda5c5
Hauke Mehrtens <hauke@hauke-m.de>
2013-09-18 13:33:00 +0200

    MIPS: BCM47XX: Fix detected clock on Asus WL520GC and WL520GU
    The Asus WL520GC and WL520GU are based on the BCM5354 and clocked at
    200MHz, but they do not have a clkfreq nvram variable set to the
    correct value. This adds a workaround for these devices.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/5843/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/bcm47xx/time.c
+++ arch/mips/bcm47xx/time.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/time.h>
 #include <bcm47xx.h>
 #include <bcm47xx_nvram.h>
+#include <bcm47xx_board.h>
 
 void __init plat_time_init(void)
 {
@@ -36,6 +37,7 @@ void __init plat_time_init(void)
 	u16 chip_id = 0;
 	char buf[10];
 	int len;
+	enum bcm47xx_board board = bcm47xx_board_get();
 
 	/*
 	 * Use deterministic values for initial counter interrupt
@@ -63,6 +65,15 @@ void __init plat_time_init(void)
 		len = bcm47xx_nvram_getenv("clkfreq", buf, sizeof(buf));
 		if (len >= 0 && !strncmp(buf, "200", 4))
 			hz = 100000000;
+	}
+
+	switch (board) {
+	case BCM47XX_BOARD_ASUS_WL520GC:
+	case BCM47XX_BOARD_ASUS_WL520GU:
+		hz = 100000000;
+		break;
+	default:
+		break;
 	}
 
 	if (!hz)
