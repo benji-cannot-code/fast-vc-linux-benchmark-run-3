b3ce8f5fe24927351bcbc554b34e898b8a8d8963
Lee Jones <lee.jones@linaro.org>
2013-09-18 14:53:06 +0100

    ARM: ux500: Purge UIB framework when booting with ATAGs
    It's time to remove all ATAG support from ux500 and rely solely on
    Device Tree booting. This patch is part of that endeavour.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -106,8 +106,8 @@ MACHINE_START(U8500, "ST-Ericsson MOP500 platform")
 	/* we re-use nomadik timer here */
 	.init_time	= ux500_timer_init,
 	.init_machine	= mop500_init_machine,
-	.init_late	= ux500_init_late,
 	.restart        = ux500_restart,
+	.init_late	= NULL,
 MACHINE_END
 
 MACHINE_START(U8520, "ST-Ericsson U8520 Platform HREFP520")
@@ -116,8 +116,8 @@ MACHINE_START(U8520, "ST-Ericsson U8520 Platform HREFP520")
 	.init_irq	= ux500_init_irq,
 	.init_time	= ux500_timer_init,
 	.init_machine	= mop500_init_machine,
-	.init_late	= ux500_init_late,
 	.restart        = ux500_restart,
+	.init_late	= NULL,
 MACHINE_END
 
 MACHINE_START(HREFV60, "ST-Ericsson U8500 Platform HREFv60+")
@@ -127,8 +127,8 @@ MACHINE_START(HREFV60, "ST-Ericsson U8500 Platform HREFv60+")
 	.init_irq	= ux500_init_irq,
 	.init_time	= ux500_timer_init,
 	.init_machine	= hrefv60_init_machine,
-	.init_late	= ux500_init_late,
 	.restart        = ux500_restart,
+	.init_late	= NULL,
 MACHINE_END
 
 MACHINE_START(SNOWBALL, "Calao Systems Snowball platform")
@@ -139,6 +139,6 @@ MACHINE_START(SNOWBALL, "Calao Systems Snowball platform")
 	/* we re-use nomadik timer here */
 	.init_time	= ux500_timer_init,
 	.init_machine	= snowball_init_machine,
-	.init_late	= NULL,
 	.restart        = ux500_restart,
+	.init_late	= NULL,
 MACHINE_END
