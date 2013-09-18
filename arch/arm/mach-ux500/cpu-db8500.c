302c139bd5619ad764842139beda99ea7bf02879
Lee Jones <lee.jones@linaro.org>
2013-09-18 14:50:59 +0100

    ARM: ux500: Deactivate enablement of DMA40 during ATAG booting
    It's time to remove all ATAG support from ux500 and rely solely on
    Device Tree booting. This patch is part of that endeavour.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "irqs.h"
 
 #include "devices-db8500.h"
-#include "ste-dma40-db8500.h"
 #include "db8500-regs.h"
 #include "board-mop500.h"
 #include "id.h"
@@ -135,7 +134,6 @@ static struct platform_device db8500_pmu_device = {
 };
 
 static struct platform_device *platform_devs[] __initdata = {
-	&u8500_dma40_device,
 	&db8500_pmu_device,
 };
 
