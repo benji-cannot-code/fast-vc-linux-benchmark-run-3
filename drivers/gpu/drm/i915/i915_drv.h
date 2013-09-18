4e9563574ab9e9d4201595b31d80043289651f18
Ben Widawsky <benjamin.widawsky@intel.com>
2013-09-17 21:12:45 -0700

    drm/i915: Keep a list of all contexts
    I have implemented this patch before without creating a separate list
    (I'm having trouble finding the links, but the messages ids are:
    <1364942743-6041-2-git-send-email-ben@bwidawsk.net>
    <1365118914-15753-9-git-send-email-ben@bwidawsk.net>)
    
    However, the code is much simpler to just use a list and it makes the
    code from the next patch a lot more pretty.
    
    As you'll see in the next patch, the reason for this is to be able to
    specify when a context needs to get L3 remapping. More details there.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -606,6 +606,8 @@ struct i915_hw_context {
 	struct intel_ring_buffer *ring;
 	struct drm_i915_gem_object *obj;
 	struct i915_ctx_hang_stats hang_stats;
+
+	struct list_head link;
 };
 
 struct i915_fbc {
@@ -1344,6 +1346,7 @@ typedef struct drm_i915_private {
 
 	bool hw_contexts_disabled;
 	uint32_t hw_context_size;
+	struct list_head context_list;
 
 	u32 fdi_rx_config;
 
