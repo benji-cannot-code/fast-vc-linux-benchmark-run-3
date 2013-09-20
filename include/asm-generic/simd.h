f278d122ba91d1930a611ca7a8e694b2e49ee1b0
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2013-09-20 09:55:40 +0200

    crypto: create generic version of ablk_helper
    Create a generic version of ablk_helper so it can be reused
    by other architectures.
    
    Acked-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

---
+++ include/asm-generic/simd.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+
+#include <linux/hardirq.h>
+
+/*
+ * may_use_simd - whether it is allowable at this time to issue SIMD
+ *                instructions or access the SIMD register file
+ *
+ * As architectures typically don't preserve the SIMD register file when
+ * taking an interrupt, !in_interrupt() should be a reasonable default.
+ */
+static __must_check inline bool may_use_simd(void)
+{
+	return !in_interrupt();
+}
