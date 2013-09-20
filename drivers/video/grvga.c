89de2a3f8b46618fe3dff0eff9327aae386ce2cf
Sachin Kamat <sachin.kamat@linaro.org>
2013-09-20 12:02:11 +0530

    video: grvga: Remove redundant dev_set_drvdata
    Driver core sets driver data to NULL upon failure or remove.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Cc: Kristoffer Glembo <kristoffer@gaisler.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/grvga.c
+++ drivers/video/grvga.c
@@ -497,7 +497,6 @@ static int grvga_probe(struct platform_device *dev)
 	return 0;
 
 free_mem:
-	dev_set_drvdata(&dev->dev, NULL);
 	if (grvga_fix_addr)
 		iounmap((void *)virtual_start);
 	else
@@ -531,7 +530,6 @@ static int grvga_remove(struct platform_device *device)
 			kfree((void *)info->screen_base);
 
 		framebuffer_release(info);
-		dev_set_drvdata(&device->dev, NULL);
 	}
 
 	return 0;
