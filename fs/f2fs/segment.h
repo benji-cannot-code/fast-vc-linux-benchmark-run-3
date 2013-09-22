104acec997a3602891eab8c6fcdfc2e7d79025e0
Chao Yu <chao2.yu@samsung.com>
2013-09-22 15:50:50 +0800

    f2fs: avoid allocating failure in bio_alloc
    This patch add macro MAX_BIO_BLOCKS to limit value of npages in
    f2fs_bio_alloc, it can avoid allocating failure in bio_alloc caused by
    npages is larger than BIO_MAX_PAGES.
    
    Signed-off-by: Yu Chao <chao2.yu@samsung.com>
    Reviewed-by: Gu Zheng <guz.fnst@cn.fujitsu.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -91,6 +91,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(blk_addr << ((sbi)->log_blocksize - F2FS_LOG_SECTOR_SIZE))
 #define SECTOR_TO_BLOCK(sbi, sectors)					\
 	(sectors >> ((sbi)->log_blocksize - F2FS_LOG_SECTOR_SIZE))
+#define MAX_BIO_BLOCKS(max_hw_blocks)					\
+	(min((int)max_hw_blocks, BIO_MAX_PAGES))
 
 /* during checkpoint, bio_private is used to synchronize the last bio */
 struct bio_private {
