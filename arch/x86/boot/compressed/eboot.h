b48035c2d92c7738ee32aa5f4412ebee86117489
Roy Franz <roy.franz@linaro.org>
2013-09-22 15:45:27 -0700

    efi: Move common EFI stub code from x86 arch code to common location
    No code changes made, just moving functions and #define from x86 arch
    directory to common location.  Code is shared using #include, similar
    to how decompression code is shared among architectures.
    
    Signed-off-by: Roy Franz <roy.franz@linaro.org>
    Acked-by: Mark Salter <msalter@redhat.com>
    Reviewed-by: Grant Likely <grant.likely@linaro.org>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- arch/x86/boot/compressed/eboot.h
+++ arch/x86/boot/compressed/eboot.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SEG_GRANULARITY_4KB	(1 << 0)
 
 #define DESC_TYPE_CODE_DATA	(1 << 0)
-#define EFI_READ_CHUNK_SIZE	(1024 * 1024)
 
 #define EFI_CONSOLE_OUT_DEVICE_GUID    \
 	EFI_GUID(0xd3b36f2c, 0xd551, 0x11d4, 0x9a, 0x46, 0x0, 0x90, 0x27, \
