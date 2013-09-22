4cce25ddd9763f18b678f6537bc0a36236782c25
Roy Franz <roy.franz@linaro.org>
2013-09-22 15:45:29 -0700

    efi: Rename memory allocation/free functions
    Rename them to be more similar, as low_free() could be used to free
    memory allocated by both high_alloc() and low_alloc().
    high_alloc() -> efi_high_alloc()
    low_alloc()  -> efi_low_alloc()
    low_free()   -> efi_free()
    
    Signed-off-by: Roy Franz <roy.franz@linaro.org>
    Acked-by: Mark Salter <msalter@redhat.com>
    Reviewed-by: Grant Likely <grant.likely@linaro.org>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- drivers/firmware/efi/efi-stub-helper.c
+++ drivers/firmware/efi/efi-stub-helper.c
@@ -87,7 +87,7 @@ fail:
 /*
  * Allocate at the highest possible address that is not above 'max'.
  */
-static efi_status_t high_alloc(efi_system_table_t *sys_table_arg,
+static efi_status_t efi_high_alloc(efi_system_table_t *sys_table_arg,
 			       unsigned long size, unsigned long align,
 			       unsigned long *addr, unsigned long max)
 {
@@ -166,8 +166,8 @@ fail:
 /*
  * Allocate at the lowest possible address.
  */
-static efi_status_t low_alloc(efi_system_table_t *sys_table_arg,
-		unsigned long size, unsigned long align,
+static efi_status_t efi_low_alloc(efi_system_table_t *sys_table_arg,
+			      unsigned long size, unsigned long align,
 			      unsigned long *addr)
 {
 	unsigned long map_size, desc_size;
@@ -227,7 +227,7 @@ fail:
 	return status;
 }
 
-static void low_free(efi_system_table_t *sys_table_arg, unsigned long size,
+static void efi_free(efi_system_table_t *sys_table_arg, unsigned long size,
 		     unsigned long addr)
 {
 	unsigned long nr_pages;
@@ -408,8 +408,8 @@ grow:
 		 * addresses in memory, so allocate enough memory for
 		 * all the initrd's.
 		 */
-		status = high_alloc(sys_table_arg, initrd_total, 0x1000,
-				   &initrd_addr, hdr->initrd_addr_max);
+		status = efi_high_alloc(sys_table_arg, initrd_total, 0x1000,
+				    &initrd_addr, hdr->initrd_addr_max);
 		if (status != EFI_SUCCESS) {
 			efi_printk(sys_table_arg, "Failed to alloc highmem for initrds\n");
 			goto close_handles;
@@ -457,7 +457,7 @@ grow:
 	return status;
 
 free_initrd_total:
-	low_free(sys_table_arg, initrd_total, initrd_addr);
+	efi_free(sys_table_arg, initrd_total, initrd_addr);
 
 close_handles:
 	for (k = j; k < i; k++)
