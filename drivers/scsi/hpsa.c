5c3289bbaca79b68b0966bc34b4dad101768a1ff
Stephen M. Cameron <scameron@beardog.cce.hp.com>
2013-09-23 13:33:46 -0500

    [SCSI] hpsa: remove unneeded include of seq_file.h
    Signed-off-by: Scott Teel <scott.teel@hp.com>
    Acked-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/fs.h>
 #include <linux/timer.h>
-#include <linux/seq_file.h>
 #include <linux/init.h>
 #include <linux/spinlock.h>
 #include <linux/compat.h>
