ea6d973405c5179b5dc1ed84abcb710a24867683
Lidza Louina <lidza.louina@gmail.com>
2013-09-23 18:47:14 -0400

    staging: dgap: tty.c: removes smatch warning "ignoring unreachable code"
    This patch removes this smatch warning:
    info: ignoring unreachable code.
    
    There were instances where there was extra code after
    the default action in switch statements. These default
    actions ended with a break so the code wasn't being run
    at anytime. This patch removes that extra code.
    
    Signed-off-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_tty.c
+++ drivers/staging/dgap/dgap_tty.c
@@ -3514,10 +3514,6 @@ static int dgap_tty_ioctl(struct tty_struct *tty, unsigned int cmd,
 			return(-EINVAL);
 		}
 
-		DGAP_UNLOCK(ch->ch_lock, lock_flags2);
-		DGAP_UNLOCK(bd->bd_lock, lock_flags);
-		return(-ENOIOCTLCMD);
-
 	case DIGI_GETA:
 		/* get information for ditty */
 		DGAP_UNLOCK(ch->ch_lock, lock_flags2);
@@ -3587,12 +3583,4 @@ static int dgap_tty_ioctl(struct tty_struct *tty, unsigned int cmd,
 
 		return(-ENOIOCTLCMD);
 	}
-
-	DGAP_UNLOCK(ch->ch_lock, lock_flags2);
-	DGAP_UNLOCK(bd->bd_lock, lock_flags);
-
-	DPR_IOCTL(("dgap_tty_ioctl end - cmd %s (%x), arg %lx\n", 
-		dgap_ioctl_name(cmd), cmd, arg));
-                        
-	return(0);
 }
