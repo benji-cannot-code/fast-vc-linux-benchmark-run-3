0962355c5db8c8c7143e1932019d564a42792269
Andrew Bresticker <abrestic@chromium.org>
2013-09-25 14:12:49 -0700

    clk: exynos5250: add clock ID for div_pcm0
    There is no gate for the PCM clock input to the AudioSS block, so
    the parent of sclk_pcm is div_pcm0.  Add a clock ID for it so that
    we can reference it in device trees.
    
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-by: Tomasz Figa <t.figa@samsung.com>
    Acked-by: Mike Turquette <mturquette@linaro.org>
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>

--- include/dt-bindings/clock/exynos5250.h
+++ include/dt-bindings/clock/exynos5250.h
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_DIV_I2S1		157
 #define CLK_DIV_I2S2		158
 #define CLK_SCLK_HDMIPHY	159
+#define CLK_DIV_PCM0		160
 
 /* gate clocks */
 #define CLK_GSCL0		256
