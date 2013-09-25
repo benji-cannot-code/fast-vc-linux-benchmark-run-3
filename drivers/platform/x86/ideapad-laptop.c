b27502fa6b4549d0e881e01096f18fa1a1b7d484
Zhang Rui <rui.zhang@intel.com>
2013-09-25 20:39:46 +0800

    ideapad_laptop: introduce #ifdef CONFIG_PM_SLEEP for PM specific code
    ideapad_acpi_resume() and ideapad_pm is meaningful
    only if CONFIG_PM_SLEEP is set.
    Thus introduce #ifdef for this piece of code.
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    CC: Matthew Garrett <matthew.garrett@nebula.com>
    CC: Ike Panhc <ike.pan@canonical.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/platform/x86/ideapad-laptop.c
+++ drivers/platform/x86/ideapad-laptop.c
@@ -902,6 +902,7 @@ static void ideapad_acpi_notify(struct acpi_device *adevice, u32 event)
 	}
 }
 
+#ifdef CONFIG_PM_SLEEP
 static int ideapad_acpi_resume(struct device *device)
 {
 	ideapad_sync_rfk_state(ideapad_priv);
@@ -910,6 +911,7 @@ static int ideapad_acpi_resume(struct device *device)
 }
 
 static SIMPLE_DEV_PM_OPS(ideapad_pm, NULL, ideapad_acpi_resume);
+#endif
 
 static struct acpi_driver ideapad_acpi_driver = {
 	.name = "ideapad_acpi",
@@ -918,7 +920,9 @@ static struct acpi_driver ideapad_acpi_driver = {
 	.ops.add = ideapad_acpi_add,
 	.ops.remove = ideapad_acpi_remove,
 	.ops.notify = ideapad_acpi_notify,
+#ifdef CONFIG_PM_SLEEP
 	.drv.pm = &ideapad_pm,
+#endif
 	.owner = THIS_MODULE,
 };
 module_acpi_driver(ideapad_acpi_driver);
