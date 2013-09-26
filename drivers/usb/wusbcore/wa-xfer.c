c636bf6a849d83d96b11224ecba1d3851f610d55
Thomas Pugliese <thomas.pugliese@gmail.com>
2013-09-26 14:08:16 -0500

    usb: wusbcore: set pointers to NULL after freeing in error cases
    This patch fixes two cases where error handling code was freeing memory
    but not setting the pointer to NULL.  This could lead to a double free
    in the HWA shutdown code.
    
    Signed-off-by: Thomas Pugliese <thomas.pugliese@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/wusbcore/wa-xfer.c
+++ drivers/usb/wusbcore/wa-xfer.c
@@ -1561,6 +1561,7 @@ error_submit_buf_in:
 			xfer, seg_idx, result);
 	seg->result = result;
 	kfree(wa->buf_in_urb->sg);
+	wa->buf_in_urb->sg = NULL;
 error_sg_alloc:
 	__wa_xfer_abort(xfer);
 error_complete:
@@ -1860,6 +1861,7 @@ out:
 
 error_dti_urb_submit:
 	usb_put_urb(wa->buf_in_urb);
+	wa->buf_in_urb = NULL;
 error_buf_in_urb_alloc:
 	usb_put_urb(wa->dti_urb);
 	wa->dti_urb = NULL;
