c598758758e6b02e0daa0fd1e04d7d3a3e240215
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2013-09-26 14:38:15 +0200

    usb: gadget: configfs: add a method to unregister the gadget
    Add a method to unregister the gadget using its config_item.
    
    There can be functions (e.g. mass storage), which in some circumstances
    need the gadget stopped. Add a method of stopping the gadget.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

---
+++ drivers/usb/gadget/configfs.h
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef USB__GADGET__CONFIGFS__H
+#define USB__GADGET__CONFIGFS__H
+
+void unregister_gadget_item(struct config_item *item);
+
+#endif /*  USB__GADGET__CONFIGFS__H */
