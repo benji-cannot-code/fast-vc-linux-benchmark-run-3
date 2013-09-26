276de7dab545cfed7c278b456b19f697dd49e9cc
Masanari Iida <standby24x7@gmail.com>
2013-09-27 00:11:44 +0900

    staging: rtl8188eu: Fix typo in rtl8188eu/core
    Correct spelling typo in rtl8188eu/core
    
    Signed-off-by: Masanari Iida <standby24x7@gmail.com>
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    # On branch rtl8188eu-typo2
    # Changes to be committed:
    #	modified:   drivers/staging/rtl8188eu/core/rtw_ap.c
    #	modified:   drivers/staging/rtl8188eu/core/rtw_br_ext.c
    #	modified:   drivers/staging/rtl8188eu/core/rtw_cmd.c
    #	modified:   drivers/staging/rtl8188eu/core/rtw_efuse.c
    #	modified:   drivers/staging/rtl8188eu/core/rtw_ieee80211.c
    #	modified:   drivers/staging/rtl8188eu/core/rtw_mlme.c
    #	modified:   drivers/staging/rtl8188eu/core/rtw_mlme_ext.c
    #	modified:   drivers/staging/rtl8188eu/core/rtw_security.c
    #
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_efuse.c
+++ drivers/staging/rtl8188eu/core/rtw_efuse.c
@@ -160,7 +160,7 @@ Efuse_CalculateWordCnts(u8 word_en)
 /*  */
 /* 	Description: */
 /* 		Execute E-Fuse read byte operation. */
-/* 		Refered from SD1 Richard. */
+/* 		Referred from SD1 Richard. */
 /*  */
 /* 	Assumption: */
 /* 		1. Boot from E-Fuse and successfully auto-load. */
@@ -215,7 +215,7 @@ ReadEFuseByte(
 /* 	Description: */
 /* 		1. Execute E-Fuse read byte operation according as map offset and */
 /* 		    save to E-Fuse table. */
-/* 		2. Refered from SD1 Richard. */
+/* 		2. Referred from SD1 Richard. */
 /*  */
 /* 	Assumption: */
 /* 		1. Boot from E-Fuse and successfully auto-load. */
