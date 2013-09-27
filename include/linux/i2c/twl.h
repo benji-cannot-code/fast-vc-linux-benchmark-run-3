87b787406ae2fdb36d7f1a1ab1f2ad84e98b2d04
Kishon Vijay Abraham I <kishon@ti.com>
2013-09-27 11:53:27 +0530

    usb: phy: twl4030: use the new generic PHY framework
    Used the generic PHY framework API to create the PHY. For powering on
    and powering off the PHY, power_on and power_off ops are used. Once the
    MUSB OMAP glue is adapted to the new framework, the suspend and resume
    ops of usb phy library will be removed. Also twl4030-usb driver is moved
    to drivers/phy/.
    
    However using the old usb phy library cannot be completely removed
    because otg is intertwined with phy and moving to the new
    framework completely will break otg. Once we have a separate otg state machine,
    we can get rid of the usb phy library.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Acked-by: Felipe Balbi <balbi@ti.com>
    Reviewed-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/i2c/twl.h
+++ include/linux/i2c/twl.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __TWL_H_
 
 #include <linux/types.h>
+#include <linux/phy/phy.h>
 #include <linux/input/matrix_keypad.h>
 
 /*
@@ -616,6 +617,7 @@ enum twl4030_usb_mode {
 struct twl4030_usb_data {
 	enum twl4030_usb_mode	usb_mode;
 	unsigned long		features;
+	struct phy_init_data	*init_data;
 
 	int		(*phy_init)(struct device *dev);
 	int		(*phy_exit)(struct device *dev);
