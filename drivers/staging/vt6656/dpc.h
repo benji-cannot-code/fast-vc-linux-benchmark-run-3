c52f05db8665f0d075e7b8c8ad72a5f24b8f77e2
Malcolm Priestley <tvboxspy@gmail.com>
2013-09-27 16:53:55 +0100

    staging: vt6656: convert RXvWorkItem to work queue
    Tasklet to workqueue.
    ReadWorkItem -> read_work_item
    
    Reduce atomic area of driver.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/dpc.h
+++ drivers/staging/vt6656/dpc.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "device.h"
 #include "wcmd.h"
 
-void RXvWorkItem(void *Context);
+void RXvWorkItem(struct work_struct *work);
 
 void RXvMngWorkItem(void *Context);
 
