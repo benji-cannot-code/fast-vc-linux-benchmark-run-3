2691b550e5b168341672673b2765d3ab447f6b46
Sudeep Dutt <sudeep.dutt@intel.com>
2013-09-27 09:50:06 -0700

    misc: mic: fix a warning in the IOCTL header file.
    The following warning from mic_ioctl.h is fixed via this patch:
    found __[us]{8,16,32,64} type without #include <linux/types.h>
    
    Reported-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ashutosh Dixit <ashutosh.dixit@intel.com>
    Signed-off-by: Dasaratharaman Chandramouli <dasaratharaman.chandramouli@intel.com>
    Signed-off-by: Nikhil Rao <nikhil.rao@intel.com>
    Signed-off-by: Harshavardhan R Kharche <harshavardhan.r.kharche@intel.com>
    Signed-off-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/mic_ioctl.h
+++ include/uapi/linux/mic_ioctl.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MIC_IOCTL_H_
 #define _MIC_IOCTL_H_
 
+#include <linux/types.h>
+
 /*
  * mic_copy - MIC virtio descriptor copy.
  *
