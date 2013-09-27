3729aa35fff66559b33b7a2accad22e67695b1bb
Mark Brown <broonie@linaro.org>
2013-09-27 20:10:26 +0100

    spi/s3c64xx: Flush FIFOs prior to cleaning up transfer
    Ensure that the FIFOs are fully drained before we deassert /CS or do any
    delays that have been requested in order to ensure that the behaviour
    visible on the bus matches that which was requested by the caller.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-s3c64xx.c
+++ drivers/spi/spi-s3c64xx.c
@@ -961,6 +961,8 @@ static int s3c64xx_spi_transfer_one_message(struct spi_master *master,
 			goto out;
 		}
 
+		flush_fifo(sdd);
+
 		if (xfer->delay_usecs)
 			udelay(xfer->delay_usecs);
 
@@ -973,8 +975,6 @@ static int s3c64xx_spi_transfer_one_message(struct spi_master *master,
 		}
 
 		msg->actual_length += xfer->len;
-
-		flush_fifo(sdd);
 	}
 
 out:
