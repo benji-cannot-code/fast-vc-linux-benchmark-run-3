8816fb72ae635c7121e60d58135b8cb96a84e0de
Malcolm Priestley <tvboxspy@gmail.com>
2013-09-27 16:55:45 +0100

    staging: vt6656: covert RXvMngWorkItem to work queue
    Tasklet to workqueue.
    RxMngWorkItem -> rx_mng_work_item
    
    Reduce atomic area of driver and dependency on system timer.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/dpc.h
+++ drivers/staging/vt6656/dpc.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void RXvWorkItem(struct work_struct *work);
 
-void RXvMngWorkItem(void *Context);
+void RXvMngWorkItem(struct work_struct *work);
 
 void RXvFreeRCB(struct vnt_rcb *pRCB, int bReAllocSkb);
 
