72e16cf5f3db9dd5a8b4b27e63dfdd4564547087
Sachin Kamat <sachin.kamat@linaro.org>
2013-09-27 15:20:26 +0530

    staging: dgnc: Remove KERNEL_VERSION check
    This check is not required.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Lidza Louina <lidza.louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_kcompat.h
+++ drivers/staging/dgnc/dgnc_kcompat.h
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DGNC_KCOMPAT_H
 #define __DGNC_KCOMPAT_H
 
-# ifndef KERNEL_VERSION
-#  define KERNEL_VERSION(a,b,c)  (((a) << 16) + ((b) << 8) + (c))
-# endif
-
-
 #if !defined(TTY_FLIPBUF_SIZE)
 # define TTY_FLIPBUF_SIZE 512
 #endif
