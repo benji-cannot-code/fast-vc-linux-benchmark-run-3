eb2e9afac8f526cda6a8445aa9c46d3d92177f32
Chanwoo Choi <cw00.choi@samsung.com>
2013-09-27 09:19:40 +0900

    extcon: Change field type of 'dev' in extcon_dev structure
    The extcon device must always need 'struct device' so this patch change
    field type of 'dev' instead of allocating memory for 'struct device' on
    extcon_dev_register() function.
    
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>
    Signed-off-by: Myungjoo Ham <cw00.choi@samsung.com>

--- include/linux/extcon.h
+++ include/linux/extcon.h
@@ -94,7 +94,7 @@ struct extcon_cable;
  *			name of the extcon device.
  * @print_state:	An optional callback to override the method to print the
  *			status of the extcon device.
- * @dev:		Device of this extcon. Do not provide at register-time.
+ * @dev:		Device of this extcon.
  * @state:		Attach/detach state of this extcon. Do not provide at
  *			register-time.
  * @nh:			Notifier for the state change events from this extcon
@@ -122,7 +122,7 @@ struct extcon_dev {
 	ssize_t	(*print_state)(struct extcon_dev *edev, char *buf);
 
 	/* Internal data. Please do not set. */
-	struct device *dev;
+	struct device dev;
 	struct raw_notifier_head nh;
 	struct list_head entry;
 	int max_supported;
