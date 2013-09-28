85be537aeda562406331ad81d7c6dd663bc1f54f
Myron Stowe <myron.stowe@redhat.com>
2013-09-28 11:55:49 -0600

    cris/PCI: Remove unused pci_mem_start
    Remove unused 'pci_mem_start' variable.
    
    Signed-off-by: Myron Stowe <myron.stowe@redhat.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    CC: Mikael Starvik <starvik@axis.com>
    CC: Jesper Nilsson <jesper.nilsson@axis.com>

--- arch/cris/include/asm/pci.h
+++ arch/cris/include/asm/pci.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pcibios_assign_all_busses(void) 1
 
-extern unsigned long pci_mem_start;
 #define PCIBIOS_MIN_IO		0x1000
 #define PCIBIOS_MIN_MEM		0x10000000
 
