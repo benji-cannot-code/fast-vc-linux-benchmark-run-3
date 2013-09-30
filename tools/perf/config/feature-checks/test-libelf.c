9379a1ad1b46fe303e543db7e64116881f5163e0
Ingo Molnar <mingo@kernel.org>
2013-09-30 14:20:25 +0200

    tools/perf/build: Split out feature check: 'libelf'
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/n/tip-qznhihaasbysfqO7ffvRsf9q@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

---
+++ tools/perf/config/feature-checks/test-libelf.c
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <libelf.h>
+
+int main(void)
+{
+	Elf *elf = elf_begin(0, ELF_C_READ, 0);
+	return (long)elf;
+}
