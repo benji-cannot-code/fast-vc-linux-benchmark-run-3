5a83a70dca1ad73028cdf63dbe9229f122c8884e
Borislav Petkov <bp@alien8.de>
2013-09-30 11:56:24 +0200

    x86/boot: Further compress CPUs bootup message
    Turn it into (for example):
    
    [    0.073380] x86: Booting SMP configuration:
    [    0.074005] .... node   #0, CPUs:          #1   #2   #3   #4   #5   #6   #7
    [    0.603005] .... node   #1, CPUs:     #8   #9  #10  #11  #12  #13  #14  #15
    [    1.200005] .... node   #2, CPUs:    #16  #17  #18  #19  #20  #21  #22  #23
    [    1.796005] .... node   #3, CPUs:    #24  #25  #26  #27  #28  #29  #30  #31
    [    2.393005] .... node   #4, CPUs:    #32  #33  #34  #35  #36  #37  #38  #39
    [    2.996005] .... node   #5, CPUs:    #40  #41  #42  #43  #44  #45  #46  #47
    [    3.600005] .... node   #6, CPUs:    #48  #49  #50  #51  #52  #53  #54  #55
    [    4.202005] .... node   #7, CPUs:    #56  #57  #58  #59  #60  #61  #62  #63
    [    4.811005] .... node   #8, CPUs:    #64  #65  #66  #67  #68  #69  #70  #71
    [    5.421006] .... node   #9, CPUs:    #72  #73  #74  #75  #76  #77  #78  #79
    [    6.032005] .... node  #10, CPUs:    #80  #81  #82  #83  #84  #85  #86  #87
    [    6.648006] .... node  #11, CPUs:    #88  #89  #90  #91  #92  #93  #94  #95
    [    7.262005] .... node  #12, CPUs:    #96  #97  #98  #99 #100 #101 #102 #103
    [    7.865005] .... node  #13, CPUs:   #104 #105 #106 #107 #108 #109 #110 #111
    [    8.466005] .... node  #14, CPUs:   #112 #113 #114 #115 #116 #117 #118 #119
    [    9.073006] .... node  #15, CPUs:   #120 #121 #122 #123 #124 #125 #126 #127
    [    9.679901] x86: Booted up 16 nodes, 128 CPUs
    
    and drop useless elements.
    
    Change num_digits() to hpa's division-avoiding, cell-phone-typed
    version which he went at great lengths and pains to submit on a
    Saturday evening.
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: huawei.libin@huawei.com
    Cc: wangyijing@huawei.com
    Cc: fenghua.yu@intel.com
    Cc: guohanjun@huawei.com
    Cc: paul.gortmaker@windriver.com
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20130930095624.GB16383@pd.tnic
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/lib/misc.c
+++ arch/x86/lib/misc.c
@@ -1,12 +1,22 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Count the digits of @val including a possible sign.
+ *
+ * (Typed on and submitted from hpa's mobile phone.)
+ */
 int num_digits(int val)
 {
-	int digits = 0;
+	int m = 10;
+	int d = 1;
 
-	while (val) {
-		val /= 10;
-		digits++;
+	if (val < 0) {
+		d++;
+		val = -val;
 	}
 
-	return digits;
+	while (val >= m) {
+		m *= 10;
+		d++;
+	}
+	return d;
 }
