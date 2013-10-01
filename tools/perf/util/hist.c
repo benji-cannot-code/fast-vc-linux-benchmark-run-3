34e333449067960c5c84047fed67a7ca5d47d542
Andi Kleen <ak@linux.intel.com>
2013-10-01 07:22:15 -0700

    tools/perf: Fix sorting for 64bit entries
    Some of the node comparisons in hist.c dropped the upper
    32bit by using an int variable to store the compare
    result. This broke various 64bit fields, causing
    incorrect collapsing (found for the TSX transaction field)
    
    Just use int64_t always.
    
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1380637335-30110-1-git-send-email-andi@firstfloor.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -347,7 +347,7 @@ static struct hist_entry *add_hist_entry(struct hists *hists,
 	struct rb_node **p;
 	struct rb_node *parent = NULL;
 	struct hist_entry *he;
-	int cmp;
+	int64_t cmp;
 
 	p = &hists->entries_in->rb_node;
 
@@ -885,7 +885,7 @@ static struct hist_entry *hists__add_dummy_entry(struct hists *hists,
 	struct rb_node **p;
 	struct rb_node *parent = NULL;
 	struct hist_entry *he;
-	int cmp;
+	int64_t cmp;
 
 	if (sort__need_collapse)
 		root = &hists->entries_collapsed;
