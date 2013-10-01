11bc614a2e52b3f9abf0d93bcffbb10a898df828
Andreas Herrmann <andreas.herrmann@calxeda.com>
2013-10-01 13:39:08 +0100

    iommu/arm-smmu: Print context fault information
    Print context fault information when the fault was not handled by
    report_iommu_fault.
    
    Signed-off-by: Andreas Herrmann <andreas.herrmann@calxeda.com>
    [will: fixed string formatting]
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -591,6 +591,9 @@ static irqreturn_t arm_smmu_context_fault(int irq, void *dev)
 		ret = IRQ_HANDLED;
 		resume = RESUME_RETRY;
 	} else {
+		dev_err_ratelimited(smmu->dev,
+		    "Unhandled context fault: iova=0x%08lx, fsynr=0x%x, cb=%d\n",
+		    iova, fsynr, root_cfg->cbndx);
 		ret = IRQ_NONE;
 		resume = RESUME_TERMINATE;
 	}
