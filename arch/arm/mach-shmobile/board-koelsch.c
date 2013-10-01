b0ea79b9a52b4f24d3d77b8993e6e099f7f03050
Magnus Damm <damm@opensource.se>
2013-10-01 17:12:02 +0900

    ARM: shmobile: Use r8a7791_add_standard_devices() on Koelsch
    Use r8a7791_add_standard_devices() on Koelsch to let
    the C version of the board code add on-chip devices.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-koelsch.c
+++ arch/arm/mach-shmobile/board-koelsch.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static void __init koelsch_add_standard_devices(void)
 {
 	r8a7791_clock_init();
-	r8a7791_add_dt_devices();
+	r8a7791_add_standard_devices();
 }
 
 static const char * const koelsch_boards_compat_dt[] __initconst = {
