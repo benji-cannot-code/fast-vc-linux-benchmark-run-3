12c17dac56a54c10d803ffc2e951a97be1c010c6
Paolo Bonzini <pbonzini@redhat.com>
2013-10-02 16:56:12 +0200

    KVM: mmu: unify destroy_kvm_mmu with kvm_mmu_unload
    They do the same thing, and destroy_kvm_mmu can be confused with
    kvm_mmu_destroy.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Gleb Natapov <gleb@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -3862,18 +3862,11 @@ static int init_kvm_mmu(struct kvm_vcpu *vcpu)
 		return init_kvm_softmmu(vcpu);
 }
 
-static void destroy_kvm_mmu(struct kvm_vcpu *vcpu)
-{
-	ASSERT(vcpu);
-	if (VALID_PAGE(vcpu->arch.mmu.root_hpa)) {
-		mmu_free_roots(vcpu);
-		WARN_ON(VALID_PAGE(vcpu->arch.mmu.root_hpa));
-	}
-}
-
 int kvm_mmu_reset_context(struct kvm_vcpu *vcpu)
 {
-	destroy_kvm_mmu(vcpu);
+	ASSERT(vcpu);
+
+	kvm_mmu_unload(vcpu);
 	return init_kvm_mmu(vcpu);
 }
 EXPORT_SYMBOL_GPL(kvm_mmu_reset_context);
@@ -3899,6 +3892,7 @@ EXPORT_SYMBOL_GPL(kvm_mmu_load);
 void kvm_mmu_unload(struct kvm_vcpu *vcpu)
 {
 	mmu_free_roots(vcpu);
+	WARN_ON(VALID_PAGE(vcpu->arch.mmu.root_hpa));
 }
 EXPORT_SYMBOL_GPL(kvm_mmu_unload);
 
@@ -4549,7 +4543,7 @@ void kvm_mmu_destroy(struct kvm_vcpu *vcpu)
 {
 	ASSERT(vcpu);
 
-	destroy_kvm_mmu(vcpu);
+	kvm_mmu_unload(vcpu);
 	free_mmu_pages(vcpu);
 	mmu_free_memory_caches(vcpu);
 }
