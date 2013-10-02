ef3f74e3e2d33e53243c4eef4a634c24b31d883f
Ingo Molnar <mingo@kernel.org>
2013-10-02 15:15:09 +0200

    tools/perf/build: Split out feature checks: 'liberty', 'liberty-z', 'cplus-demangle'
    Note that these are rarely executed tests, so we call feature_check() explicitly
    and don't have them in CORE_FEATURE_CHECKS.
    
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/n/tip-pvumlx6mbtfxffgrlwO2mRcx@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

---
+++ tools/perf/config/feature-checks/test-cplus-demangle.c
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+
+extern char *cplus_demangle(const char *, int);
+
+int main(void)
+{
+	cplus_demangle(0, 0);
+
+	return 0;
+}
+
