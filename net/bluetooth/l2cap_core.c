4061d07cd5fec36033671592e1c0028035617fb5
Marcel Holtmann <marcel@holtmann.org>
2013-10-02 23:46:54 -0700

    Bluetooth: Fix switch statement order for L2CAP fixed channels
    The switch statement for the various L2CAP fixed channel handlers
    is not really ordered.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -6467,9 +6467,6 @@ static void l2cap_recv_frame(struct l2cap_conn *conn, struct sk_buff *skb)
 	BT_DBG("len %d, cid 0x%4.4x", len, cid);
 
 	switch (cid) {
-	case L2CAP_CID_LE_SIGNALING:
-		l2cap_le_sig_channel(conn, skb);
-		break;
 	case L2CAP_CID_SIGNALING:
 		l2cap_sig_channel(conn, skb);
 		break;
@@ -6482,6 +6479,10 @@ static void l2cap_recv_frame(struct l2cap_conn *conn, struct sk_buff *skb)
 
 	case L2CAP_CID_ATT:
 		l2cap_att_channel(conn, skb);
+		break;
+
+	case L2CAP_CID_LE_SIGNALING:
+		l2cap_le_sig_channel(conn, skb);
 		break;
 
 	case L2CAP_CID_SMP:
