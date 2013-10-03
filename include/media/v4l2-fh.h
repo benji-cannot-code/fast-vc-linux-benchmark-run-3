3d701c05a06604713d675d49d687819b460d7938
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2013-10-03 18:55:52 -0300

    [media] v4l2-fh: Include linux/fs.h for struct file definition
    v4l2-fh.h dereferences struct file, the structure must thus be defined.
    Pull in its definition by including linux/fs.h.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- include/media/v4l2-fh.h
+++ include/media/v4l2-fh.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef V4L2_FH_H
 #define V4L2_FH_H
 
+#include <linux/fs.h>
 #include <linux/list.h>
 
 struct video_device;
