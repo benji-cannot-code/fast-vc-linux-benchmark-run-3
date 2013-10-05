7bd835c96ca08af7a717d95d3b06e1ae2909eb04
Geert Uytterhoeven <geert@linux-m68k.org>
2013-10-05 21:14:22 +0200

    m68k: Remove superfluous inclusions of <asm/bootinfo.h>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/mac/iop.c
+++ arch/m68k/mac/iop.c
@@ -112,7 +112,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/interrupt.h>
 
-#include <asm/bootinfo.h>
 #include <asm/macintosh.h>
 #include <asm/macints.h>
 #include <asm/mac_iop.h>
