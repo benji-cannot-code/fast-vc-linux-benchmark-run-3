dbf238f40ce2a8815ee48513e69667e9159692f1
Marcel Holtmann <marcel@holtmann.org>
2013-10-05 11:47:44 -0700

    Bluetooth: Use explicit AMP controller id value for BR/EDR
    The special AMP controller id 0 is reserved for the BR/EDR controller
    that has the main link. It is a fixed value and so use a constant for
    this throughout the code to make it more visible when the handling is
    for the BR/EDR channel or when it is for the AMP channel.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -65,7 +65,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HCI_AMP		0x01
 
 /* First BR/EDR Controller shall have ID = 0 */
-#define HCI_BREDR_ID	0
+#define AMP_ID_BREDR	0x00
 
 /* AMP controller types */
 #define AMP_TYPE_BREDR	0x00
