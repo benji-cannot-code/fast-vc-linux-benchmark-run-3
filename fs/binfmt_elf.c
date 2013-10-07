42d0866529d3f0af0b25711bcaea106d7628213c
Al Viro <viro@zeniv.linux.org.uk>
2013-10-07 07:23:45 -0400

    binfmt_elf: count notes towards coredump limit
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -2035,7 +2035,6 @@ static int elf_core_dump(struct coredump_params *cprm)
 	int has_dumped = 0;
 	mm_segment_t fs;
 	int segs;
-	size_t size = 0;
 	struct vm_area_struct *vma, *gate_vma;
 	struct elfhdr *elf = NULL;
 	loff_t offset = 0, dataoff;
@@ -2156,7 +2155,6 @@ static int elf_core_dump(struct coredump_params *cprm)
 	if (!elf_core_write_extra_phdrs(cprm, offset))
 		goto end_coredump;
 
-	size = cprm->written;
  	/* write out the notes section */
 	if (!write_note_info(&info, cprm))
 		goto end_coredump;
@@ -2168,7 +2166,6 @@ static int elf_core_dump(struct coredump_params *cprm)
 	if (!dump_seek(cprm->file, dataoff - cprm->written))
 		goto end_coredump;
 
-	cprm->written = size;
 	for (vma = first_vma(current, gate_vma); vma != NULL;
 			vma = next_vma(vma, gate_vma)) {
 		unsigned long addr;
