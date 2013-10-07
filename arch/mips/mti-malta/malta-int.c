a09f917af10413590669b30b86cb9d0cbb8f094e
Deng-Cheng Zhu <dengcheng.zhu@imgtec.com>
2013-10-07 09:45:05 -0700

    MIPS: Remove unused defines in piix4.h
    The PIIX4_ICTLR* and PIIX4_OCW* defines are not used by any other files.
    Remove them.
    
    The only file (other than fixup-malta.c which includes piix4.h in patch #1)
    containing "#include <asm/mips-boards/piix4.h>" is
    arch/mips/mti-malta/malta-int.c whose first version is actually
    "1da177e4c3:arch/mips/mips-boards/malta/malta_int.c". In that version, in
    the function get_int(), things in piix4.h are used. But now malta-int.c no
    longer needs those stuff.
    
    Signed-off-by: Deng-Cheng Zhu <dengcheng.zhu@imgtec.com>
    Cc: Steven J. Hill <Steven.Hill@imgtec.com>
    Reviewed-by: James Hogan <james.hogan@imgtec.com>
    Reviewed-by: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/6032/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mti-malta/malta-int.c
+++ arch/mips/mti-malta/malta-int.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq_regs.h>
 #include <asm/mips-boards/malta.h>
 #include <asm/mips-boards/maltaint.h>
-#include <asm/mips-boards/piix4.h>
 #include <asm/gt64120.h>
 #include <asm/mips-boards/generic.h>
 #include <asm/mips-boards/msc01_pci.h>
