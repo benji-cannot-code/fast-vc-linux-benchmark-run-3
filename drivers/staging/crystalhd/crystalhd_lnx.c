7f8013683b8751f779754a1b2bb98d2c4bcdcefc
Valentina Manea <valentina.manea.m@gmail.com>
2013-10-07 23:12:14 +0300

    Staging: crystalhd: Fix sparse warnings regarding static functions
    This fixes the following sparse warnings:
    * drivers/staging/crystalhd/crystalhd_lnx.c:78:29: warning:
    symbol 'chd_dec_alloc_iodata' was not declared. Should it be static?
    * drivers/staging/crystalhd/crystalhd_lnx.c:99:6: warning:
    symbol 'chd_dec_free_iodata' was not declared. Should it be static?
    * drivers/staging/crystalhd/crystalhd_lnx.c:630:5: warning:
    symbol 'chd_dec_pci_suspend' was not declared. Should it be static?
    * drivers/staging/crystalhd/crystalhd_lnx.c:664:5: warning:
    symbol 'chd_dec_pci_resume' was not declared. Should it be static?
    
    Signed-off-by: Valentina Manea <valentina.manea.m@gmail.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/crystalhd/crystalhd_lnx.c
+++ drivers/staging/crystalhd/crystalhd_lnx.c
@@ -76,8 +76,9 @@ static int chd_dec_disable_int(struct crystalhd_adp *adp)
 	return 0;
 }
 
-struct crystalhd_ioctl_data *chd_dec_alloc_iodata(struct crystalhd_adp *adp,
-					 bool isr)
+static struct
+crystalhd_ioctl_data *chd_dec_alloc_iodata(struct crystalhd_adp *adp,
+					   bool isr)
 {
 	unsigned long flags = 0;
 	struct crystalhd_ioctl_data *temp;
@@ -97,8 +98,8 @@ struct crystalhd_ioctl_data *chd_dec_alloc_iodata(struct crystalhd_adp *adp,
 	return temp;
 }
 
-void chd_dec_free_iodata(struct crystalhd_adp *adp,
-			 struct crystalhd_ioctl_data *iodata, bool isr)
+static void chd_dec_free_iodata(struct crystalhd_adp *adp,
+				struct crystalhd_ioctl_data *iodata, bool isr)
 {
 	unsigned long flags = 0;
 
@@ -628,7 +629,7 @@ err:
 }
 
 #ifdef CONFIG_PM
-int chd_dec_pci_suspend(struct pci_dev *pdev, pm_message_t state)
+static int chd_dec_pci_suspend(struct pci_dev *pdev, pm_message_t state)
 {
 	struct crystalhd_adp *adp;
 	struct crystalhd_ioctl_data *temp;
@@ -662,7 +663,7 @@ int chd_dec_pci_suspend(struct pci_dev *pdev, pm_message_t state)
 	return 0;
 }
 
-int chd_dec_pci_resume(struct pci_dev *pdev)
+static int chd_dec_pci_resume(struct pci_dev *pdev)
 {
 	struct crystalhd_adp *adp;
 	enum BC_STATUS sts = BC_STS_SUCCESS;
