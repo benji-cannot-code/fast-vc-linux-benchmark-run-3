f422fa3a6bbd6997d9610d7c3256048d752f227f
Shawn Bohrer <sbohrer@rgmadvisors.com>
2013-10-07 11:01:38 -0500

    udp: Only allow busy read/poll on connected sockets
    UDP sockets can receive packets from multiple endpoints and thus may be
    received on multiple receive queues.  Since packets packets can arrive
    on multiple receive queues we should not mark the napi_id for all
    packets.  This makes busy read/poll only work for connected UDP sockets.
    
    This additionally enables busy read/poll for UDP multicast packets as
    long as the socket is connected by moving the check into
    __udp_queue_rcv_skb().
    
    Signed-off-by: Shawn Bohrer <sbohrer@rgmadvisors.com>
    Suggested-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -1406,8 +1406,10 @@ static int __udp_queue_rcv_skb(struct sock *sk, struct sk_buff *skb)
 {
 	int rc;
 
-	if (inet_sk(sk)->inet_daddr)
+	if (inet_sk(sk)->inet_daddr) {
 		sock_rps_save_rxhash(sk, skb);
+		sk_mark_napi_id(sk, skb);
+	}
 
 	rc = sock_queue_rcv_skb(sk, skb);
 	if (rc < 0) {
@@ -1717,7 +1719,6 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 	if (sk != NULL) {
 		int ret;
 
-		sk_mark_napi_id(sk, skb);
 		ret = udp_queue_rcv_skb(sk, skb);
 		sock_put(sk);
 
