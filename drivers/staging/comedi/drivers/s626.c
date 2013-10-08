5a93d488445de86ae1a677a0ecf9f6bc02407c50
Ian Abbott <abbotti@mev.co.uk>
2013-10-08 19:06:39 +0100

    staging: comedi: s626: rename struct buffer_dma
    Re-tag `struct buffer_dma` to `struct s626_buffer_dma` to avoid
    potential namespace clashes.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/s626.c
+++ drivers/staging/comedi/drivers/s626.c
@@ -72,7 +72,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "comedi_fc.h"
 #include "s626.h"
 
-struct buffer_dma {
+struct s626_buffer_dma {
 	dma_addr_t physical_base;
 	void *logical_base;
 };
@@ -90,9 +90,9 @@ struct s626_private {
 	uint16_t counter_int_enabs;	/* counter interrupt enable mask
 					 * for MISC2 register */
 	uint8_t adc_items;		/* number of items in ADC poll list */
-	struct buffer_dma rps_buf;	/* DMA buffer used to hold ADC (RPS1)
+	struct s626_buffer_dma rps_buf;	/* DMA buffer used to hold ADC (RPS1)
 					 * program */
-	struct buffer_dma ana_buf;	/* DMA buffer used to receive ADC data
+	struct s626_buffer_dma ana_buf;	/* DMA buffer used to receive ADC data
 					 * and hold DAC data */
 	uint32_t *dac_wbuf;		/* pointer to logical adrs of DMA buffer
 					 * used to hold DAC data */
@@ -2450,7 +2450,7 @@ static void write_misc2(struct comedi_device *dev, uint16_t new_image)
 	debi_write(dev, LP_MISC1, MISC1_WDISABLE); /* Disable writes to MISC2 */
 }
 
-static void close_dma_b(struct comedi_device *dev, struct buffer_dma *pdma,
+static void close_dma_b(struct comedi_device *dev, struct s626_buffer_dma *pdma,
 			size_t bsize)
 {
 	struct pci_dev *pcidev = comedi_to_pci_dev(dev);
