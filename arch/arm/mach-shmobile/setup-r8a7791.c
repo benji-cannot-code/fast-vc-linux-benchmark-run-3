e158e3268f0a825db7922f814cc478631a3a8b6d
Magnus Damm <damm@opensource.se>
2013-10-08 12:39:10 +0900

    ARM: shmobile: r8a7791 PFC platform device support
    Add a platform device for the r8a7791 PFC device.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7791.c
+++ arch/arm/mach-shmobile/setup-r8a7791.c
@@ -32,6 +32,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/rcar-gen2.h>
 #include <asm/mach/arch.h>
 
+static const struct resource pfc_resources[] __initconst = {
+	DEFINE_RES_MEM(0xe6060000, 0x250),
+};
+
+#define r8a7791_register_pfc()						\
+	platform_device_register_simple("pfc-r8a7791", -1, pfc_resources, \
+					ARRAY_SIZE(pfc_resources))
+
+void __init r8a7791_pinmux_init(void)
+{
+	r8a7791_register_pfc();
+}
+
 #define SCIF_COMMON(scif_type, baseaddr, irq)			\
 	.type		= scif_type,				\
 	.mapbase	= baseaddr,				\
