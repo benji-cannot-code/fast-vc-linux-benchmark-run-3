608f1cdeace7b5b7d435173453af4d8643ec6812
Ebru Akagunduz <ebru.akagunduz@gmail.com>
2013-10-08 23:31:52 +0300

    Staging: lustre: fix space prohibited between function name and open parenthesis '(' in module.c
    Fix checkpatch.pl issues with space prohibited between
    function name and open parenthesis '(' in module.c
    
    Signed-off-by: Ebru Akagunduz <ebru.akagunduz@gmail.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/lnet/module.c
+++ drivers/staging/lustre/lnet/lnet/module.c
@@ -45,7 +45,7 @@ CFS_MODULE_PARM(config_on_load, "i", int, 0444,
 static struct mutex lnet_config_mutex;
 
 int
-lnet_configure (void *arg)
+lnet_configure(void *arg)
 {
 	/* 'arg' only there so I can be passed to cfs_create_thread() */
 	int    rc = 0;
@@ -65,7 +65,7 @@ lnet_configure (void *arg)
 }
 
 int
-lnet_unconfigure (void)
+lnet_unconfigure(void)
 {
 	int   refcount;
 
@@ -125,7 +125,7 @@ init_lnet(void)
 	}
 
 	rc = libcfs_register_ioctl(&lnet_ioctl_handler);
-	LASSERT (rc == 0);
+	LASSERT(rc == 0);
 
 	if (config_on_load) {
 		/* Have to schedule a separate thread to avoid deadlocking
@@ -142,7 +142,7 @@ fini_lnet(void)
 	int rc;
 
 	rc = libcfs_deregister_ioctl(&lnet_ioctl_handler);
-	LASSERT (rc == 0);
+	LASSERT(rc == 0);
 
 	LNetFini();
 }
