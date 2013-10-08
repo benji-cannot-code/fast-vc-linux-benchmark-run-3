f1bf3ce6d70f1fd252cc8b3ce01f765ba0dd2229
Joseph Lo <josephl@nvidia.com>
2013-10-08 12:50:03 +0800

    ARM: tegra: add Tegra124 SoC support
    Add Tegra124 SoC support that base on CortexA15MP Core. And enable the
    SMP function that can re-use the same procedure with Tegra114.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/fuse.h
+++ arch/arm/mach-tegra/fuse.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TEGRA20		0x20
 #define TEGRA30		0x30
 #define TEGRA114	0x35
+#define TEGRA124	0x40
 
 #ifndef __ASSEMBLY__
 enum tegra_revision {
