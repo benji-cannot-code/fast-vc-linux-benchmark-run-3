df70bf4798c74a950e010d24937c94c1db0890e7
Ian Abbott <abbotti@mev.co.uk>
2013-10-08 19:06:19 +0100

    staging: comedi: s626: remove unused DMAHandle
    The `DMAHandle` member of `struct bufferDMA` in "s626.h" is unused, so
    remove it.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/s626.h
+++ drivers/staging/comedi/drivers/s626.h
@@ -632,7 +632,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct bufferDMA {
 	dma_addr_t PhysicalBase;
 	void *LogicalBase;
-	uint32_t DMAHandle;
 };
 
 #endif
