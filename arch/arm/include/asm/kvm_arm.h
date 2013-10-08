2aa95acb1f7a05cfee44383dc89785aaa5a0994f
Marc Zyngier <marc.zyngier@arm.com>
2013-10-08 18:38:13 +0100

    ARM: KVM: Yield CPU when vcpu executes a WFE
    On an (even slightly) oversubscribed system, spinlocks are quickly
    becoming a bottleneck, as some vcpus are spinning, waiting for a
    lock to be released, while the vcpu holding the lock may not be
    running at all.
    
    This creates contention, and the observed slowdown is 40x for
    hackbench. No, this isn't a typo.
    
    The solution is to trap blocking WFEs and tell KVM that we're
    now spinning. This ensures that other vpus will get a scheduling
    boost, allowing the lock to be released more quickly. Also, using
    CONFIG_HAVE_KVM_CPU_RELAX_INTERCEPT slightly improves the performance
    when the VM is severely overcommited.
    
    Quick test to estimate the performance: hackbench 1 process 1000
    
    2xA15 host (baseline):	1.843s
    
    2xA15 guest w/o patch:	2.083s
    4xA15 guest w/o patch:	80.212s
    8xA15 guest w/o patch:	Could not be bothered to find out
    
    2xA15 guest w/ patch:	2.102s
    4xA15 guest w/ patch:	3.205s
    8xA15 guest w/ patch:	6.887s
    
    So we go from a 40x degradation to 1.5x in the 2x overcommit case,
    which is vaguely more acceptable.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/arm/include/asm/kvm_arm.h
+++ arch/arm/include/asm/kvm_arm.h
@@ -68,7 +68,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define HCR_GUEST_MASK (HCR_TSC | HCR_TSW | HCR_TWI | HCR_VM | HCR_BSU_IS | \
 			HCR_FB | HCR_TAC | HCR_AMO | HCR_IMO | HCR_FMO | \
-			HCR_SWIO | HCR_TIDCP)
+			HCR_TWE | HCR_SWIO | HCR_TIDCP)
 #define HCR_VIRT_EXCP_MASK (HCR_VA | HCR_VI | HCR_VF)
 
 /* System Control Register (SCTLR) bits */
@@ -208,6 +208,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HSR_EC_IABT_HYP	(0x21)
 #define HSR_EC_DABT	(0x24)
 #define HSR_EC_DABT_HYP	(0x25)
+
+#define HSR_WFI_IS_WFE		(1U << 0)
 
 #define HSR_HVC_IMM_MASK	((1UL << 16) - 1)
 
