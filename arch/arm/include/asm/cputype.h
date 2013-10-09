bb7a28ce21977b36f8d0612dfbf3e6c04cfacd17
Rob Herring <rob.herring@calxeda.com>
2013-10-09 17:26:44 +0100

    ARM: 7855/1: Add check for Cortex-A15 errata 798181 ECO
    The work-around for A15 errata 798181 is not needed if appropriate ECO
    fixes have been applied to r3p2 and earlier core revisions. This can be
    checked by reading REVIDR register bits 4 and 9. If only bit 4 is set,
    then the IPI broadcast can be skipped.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/cputype.h
+++ arch/arm/include/asm/cputype.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CPUID_TLBTYPE	3
 #define CPUID_MPUIR	4
 #define CPUID_MPIDR	5
+#define CPUID_REVIDR	6
 
 #ifdef CONFIG_CPU_V7M
 #define CPUID_EXT_PFR0	0x40
