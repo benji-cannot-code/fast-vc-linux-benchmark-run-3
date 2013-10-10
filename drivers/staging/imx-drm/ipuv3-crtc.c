5d70ed9213abddc6f7f6d05dee40b1c47d2024a8
Philipp Zabel <p.zabel@pengutronix.de>
2013-10-10 16:18:43 +0200

    staging: drm/imx: ipuv3-crtc: remove unused struct ipu_framebuffer
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/imx-drm/ipuv3-crtc.c
+++ drivers/staging/imx-drm/ipuv3-crtc.c
@@ -34,13 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRIVER_DESC		"i.MX IPUv3 Graphics"
 
-struct ipu_framebuffer {
-	struct drm_framebuffer	base;
-	void			*virt;
-	dma_addr_t		phys;
-	size_t			len;
-};
-
 struct ipu_crtc {
 	struct device		*dev;
 	struct drm_crtc		base;
