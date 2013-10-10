3bfe7c55ffd52c04e9e257f1ccc473d34aa32d62
Patrick McHardy <kaber@trash.net>
2013-10-10 23:35:40 +0200

    netfilter: nf_tables: add optimized data comparison for small values
    Add an optimized version of nft_data_cmp() that only handles values of to
    4 bytes length.
    
    This patch includes original Patrick McHardy's patch entitled (nf_tables:
    inline nft_cmp_fast_eval() into main evaluation loop).
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_core.c
+++ net/netfilter/nf_tables_core.c
@@ -21,6 +21,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/netfilter/nf_tables_core.h>
 #include <net/netfilter/nf_tables.h>
 
+static void nft_cmp_fast_eval(const struct nft_expr *expr,
+			      struct nft_data data[NFT_REG_MAX + 1])
+{
+	const struct nft_cmp_fast_expr *priv = nft_expr_priv(expr);
+	u32 mask;
+
+	mask = ~0U >> (sizeof(priv->data) * BITS_PER_BYTE - priv->len);
+	if ((data[priv->sreg].data[0] & mask) == priv->data)
+		return;
+	data[NFT_REG_VERDICT].verdict = NFT_BREAK;
+}
+
 unsigned int nft_do_chain(const struct nf_hook_ops *ops,
 			  struct sk_buff *skb,
 			  const struct net_device *in,
@@ -49,7 +61,11 @@ next_rule:
 	data[NFT_REG_VERDICT].verdict = NFT_CONTINUE;
 	list_for_each_entry_continue_rcu(rule, &chain->rules, list) {
 		nft_rule_for_each_expr(expr, last, rule) {
-			expr->ops->eval(expr, data, &pkt);
+			if (expr->ops == &nft_cmp_fast_ops)
+				nft_cmp_fast_eval(expr, data);
+			else
+				expr->ops->eval(expr, data, &pkt);
+
 			if (data[NFT_REG_VERDICT].verdict != NFT_CONTINUE)
 				break;
 		}
