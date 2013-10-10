6c48315cc0abf25a406239dc0d9b3bddc3f2a4d9
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-10-10 14:52:07 -0700

    rcutorture: Add --buildonly dry-run capability
    This commit adds --buildonly, which does the builds specified by the
    --configs argument, but does not boot or test the resulting kernels.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Greg KH <gregkh@linuxfoundation.org>

--- tools/testing/selftests/rcutorture/bin/kvm.sh
+++ tools/testing/selftests/rcutorture/bin/kvm.sh
@@ -42,6 +42,7 @@ kversion=""
 usage () {
 	echo "Usage: $scriptname optional arguments:"
 	echo "       --builddir absolute-pathname"
+	echo "       --buildonly"
 	echo "       --configs \"config-file list\""
 	echo "       --datestamp string"
 	echo "       --duration minutes"
@@ -83,6 +84,9 @@ do
 		builddir=$2
 		gotbuilddir=1
 		shift
+		;;
+	--buildonly)
+		RCU_BUILDONLY=1; export RCU_BUILDONLY
 		;;
 	--configs)
 		checkarg --configs "(list of config files)" "$#" "$2" '^[^/]*$' '^--'
