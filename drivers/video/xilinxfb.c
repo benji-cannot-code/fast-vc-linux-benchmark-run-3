b8566ea47ec73d3592f93e7bc690c6e394f33de7
Michal Simek <michal.simek@xilinx.com>
2013-10-10 08:30:21 +0200

    video: xilinxfb: Use devm_kzalloc instead of kzalloc
    Simplify driver probe and release function.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Reviewed-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/xilinxfb.c
+++ drivers/video/xilinxfb.c
@@ -369,8 +369,6 @@ err_fbmem:
 		devm_iounmap(dev, drvdata->regs);
 
 err_region:
-	kfree(drvdata);
-
 	return rc;
 }
 
@@ -403,8 +401,6 @@ static int xilinxfb_release(struct device *dev)
 		dcr_unmap(drvdata->dcr_host, drvdata->dcr_len);
 #endif
 
-	kfree(drvdata);
-
 	return 0;
 }
 
@@ -424,11 +420,9 @@ static int xilinxfb_of_probe(struct platform_device *pdev)
 	pdata = xilinx_fb_default_pdata;
 
 	/* Allocate the driver data region */
-	drvdata = kzalloc(sizeof(*drvdata), GFP_KERNEL);
-	if (!drvdata) {
-		dev_err(&pdev->dev, "Couldn't allocate device private record\n");
+	drvdata = devm_kzalloc(&pdev->dev, sizeof(*drvdata), GFP_KERNEL);
+	if (!drvdata)
 		return -ENOMEM;
-	}
 
 	/*
 	 * To check whether the core is connected directly to DCR or BUS
@@ -452,7 +446,6 @@ static int xilinxfb_of_probe(struct platform_device *pdev)
 		drvdata->dcr_host = dcr_map(op->dev.of_node, start, drvdata->dcr_len);
 		if (!DCR_MAP_OK(drvdata->dcr_host)) {
 			dev_err(&op->dev, "invalid DCR address\n");
-			kfree(drvdata);
 			return -ENODEV;
 		}
 	}
