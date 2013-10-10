265f8ec5860b352a51543c0c727d27bd1441d68d
Linus Walleij <linus.walleij@linaro.org>
2013-10-10 14:11:18 +0200

    ARM: integrator: get the CM control register by proxy
    The CM_CTRL register was accessed directly from the LED driver,
    which does not work now that we get the base for the register
    from the device tree. Add an accessor function to do this and
    make the LED driver compile again.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-integrator/include/mach/cm.h
+++ arch/arm/mach-integrator/include/mach/cm.h
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * update the core module control register.
+ * access the core module control register.
  */
+u32 cm_get(void);
 void cm_control(u32, u32);
 
 #define CM_CTRL	__io_address(INTEGRATOR_HDR_CTRL)
