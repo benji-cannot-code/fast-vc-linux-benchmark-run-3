5e02eb6dd37d827af02ad2d9435e28ef6cb2c006
Ulf Hansson <ulf.hansson@linaro.org>
2013-10-10 17:22:23 +0200

    mmc: core: Add MMC_CAP_RUNTIME_RESUME to resume at runtime_resume
    In some environments it is to prefer to postpone the resume of the card
    device until runtime_resume is being carried out, since it will mean a
    signficant decrease of the total system resume time.
    
    The reason of the decreased resume time is simply because of the actual
    re-initalization of the card, which typically takes hundreds of
    milliseconds, is performed outside the resume sequence and wont thus
    affect it.
    
    For removable card, the detect work tries to re-detect the card to make
    sure it is still present, as a part of that sequence the card will also
    be runtime_resumed and thus also fully resumed.
    
    For a non-removable card, typically a mmc blk request will trigger a
    runtime_resume and thus fully resume the card. This also means the
    first request will likely suffer from an inital latency since the
    re-initialization of the card needs to be performed.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1578,11 +1578,13 @@ static int mmc_shutdown(struct mmc_host *host)
  */
 static int mmc_resume(struct mmc_host *host)
 {
-	int err;
+	int err = 0;
 
-	err = _mmc_resume(host);
-	pm_runtime_set_active(&host->card->dev);
-	pm_runtime_mark_last_busy(&host->card->dev);
+	if (!(host->caps & MMC_CAP_RUNTIME_RESUME)) {
+		err = _mmc_resume(host);
+		pm_runtime_set_active(&host->card->dev);
+		pm_runtime_mark_last_busy(&host->card->dev);
+	}
 	pm_runtime_enable(&host->card->dev);
 
 	return err;
@@ -1613,7 +1615,7 @@ static int mmc_runtime_resume(struct mmc_host *host)
 {
 	int err;
 
-	if (!(host->caps & MMC_CAP_AGGRESSIVE_PM))
+	if (!(host->caps & (MMC_CAP_AGGRESSIVE_PM | MMC_CAP_RUNTIME_RESUME)))
 		return 0;
 
 	err = _mmc_resume(host);
