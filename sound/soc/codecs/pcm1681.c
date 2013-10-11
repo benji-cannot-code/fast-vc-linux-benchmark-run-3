5e0fc41fda6c0c30c1d5663cbbf96e35e0f1704b
Sachin Kamat <sachin.kamat@linaro.org>
2013-10-11 17:23:57 +0530

    ASoC: pcm1681: Include linux/of.h header
    'of_match_ptr' is defined in linux/of.h. Include it explicitly.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/pcm1681.c
+++ sound/soc/codecs/pcm1681.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/i2c.h>
 #include <linux/regmap.h>
+#include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <sound/pcm.h>
