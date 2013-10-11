f9686e169d4c640c1363d764f2dfb86001421812
David Rientjes <rientjes@google.com>
2013-10-11 16:07:31 -0700

    x86, hyperv: Fix build error due to missing <asm/apic.h> include
    9e7827b5ea4c ("x86, hyperv: Get the local APIC timer frequency from the
    hypervisor") breaks the build with some configs because apic.h isn't
    directly included:
    
    arch/x86/kernel/cpu/mshyperv.c: In function 'ms_hyperv_init_platform':
    arch/x86/kernel/cpu/mshyperv.c:90:3: error: 'lapic_timer_frequency' undeclared (first use in this function)
    arch/x86/kernel/cpu/mshyperv.c:90:3: note: each undeclared identifier is reported only once for each function it appears in
    
    Fix it by including asm/apic.h.
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Link: http://lkml.kernel.org/r/alpine.DEB.2.02.1310111604160.31170@chino.kir.corp.google.com
    Acked-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/kernel/cpu/mshyperv.c
+++ arch/x86/kernel/cpu/mshyperv.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/idle.h>
 #include <asm/irq_regs.h>
 #include <asm/i8259.h>
+#include <asm/apic.h>
 
 struct ms_hyperv_info ms_hyperv;
 EXPORT_SYMBOL_GPL(ms_hyperv);
