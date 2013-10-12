63fd7ec94c420e916c3814bec04a0486bb411bb2
Jovi Zhangwei <jovi.zhangwei@gmail.com>
2013-10-11 23:11:57 -0400

    staging: ktap: remove unused <asm/syscall.h> header file
    Fengguang Wu reported that ktap compile failed in mips,
    the reason is it cannot found <asm/syscall.h> in mips arch.
    
    Due to the code serviced by <asm/syscall.h> already removed,
    so just simply remove the according header file to avoid
    compiler failure.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Jovi Zhangwei <jovi.zhangwei@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ktap/interpreter/library/kdebug.c
+++ drivers/staging/ktap/interpreter/library/kdebug.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/version.h>
 #include <linux/ftrace_event.h>
-#include <asm/syscall.h> //syscall_set_return_value defined here
 #include "../../include/ktap.h"
 
 static void ktap_call_probe_closure(ktap_state *mainthread, ktap_closure *cl,
