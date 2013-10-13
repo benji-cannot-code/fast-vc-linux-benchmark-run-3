d80903e6c762a24321c187f3fea4d946168eae83
Marcel Holtmann <marcel@holtmann.org>
2013-10-13 13:09:02 -0700

    Bluetooth: Increase minor version of core module
    There have been a lot of changes in the core Bluetooth handling
    lately. So it is a good idea to increase the module version.
    
    The module version is not used anywhere, but it makes debugging
    a little bit simpler if versions can be distinguished.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/bluetooth/bluetooth.h>
 #include <linux/proc_fs.h>
 
-#define VERSION "2.16"
+#define VERSION "2.17"
 
 /* Bluetooth sockets */
 #define BT_MAX_PROTO	8
