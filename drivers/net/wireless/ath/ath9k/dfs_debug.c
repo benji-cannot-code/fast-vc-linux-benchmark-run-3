0fd4be889a22589c7202c996661fef87e8f36a61
Janusz Dziedzic <janusz.dziedzic@tieto.com>
2013-10-14 11:06:06 +0200

    ath9k/ath: move dfs pattern detector to ath
    Move the DFS pattern detector code to the ath module so
    the other Atheros drivers can make us of it. This makes
    no functional changes.
    
    Signed-off-by: Janusz Dziedzic <janusz.dziedzic@tieto.com>
    Reviewed-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/dfs_debug.c
+++ drivers/net/wireless/ath/ath9k/dfs_debug.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ath9k.h"
 #include "dfs_debug.h"
-#include "dfs_pattern_detector.h"
+#include "../dfs_pattern_detector.h"
 
 static struct ath_dfs_pool_stats dfs_pool_stats = { 0 };
 
--- drivers/net/wireless/ath/dfs_pattern_detector.c
+++ drivers/net/wireless/ath/dfs_pattern_detector.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dfs_pattern_detector.h"
 #include "dfs_pri_detector.h"
-#include "../ath.h"
+#include "ath.h"
 
 /*
  * tolerated deviation of radar time stamp in usecs on both sides
