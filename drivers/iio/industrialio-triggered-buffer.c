26e39736f5ad50095ead2583a5d701e0d65babf4
Lars-Peter Clausen <lars@metafoo.de>
2013-10-14 17:49:00 +0100

    iio:triggered-buffer: Remove redundant call to iio_sw_buffer_preenable().
    The equivalent of iio_sw_buffer_preenable() is now done in the IIO buffer core,
    so there is no need to do this from the driver anymore.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/industrialio-triggered-buffer.c
+++ drivers/iio/industrialio-triggered-buffer.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iio/trigger_consumer.h>
 
 static const struct iio_buffer_setup_ops iio_triggered_buffer_setup_ops = {
-	.preenable = &iio_sw_buffer_preenable,
 	.postenable = &iio_triggered_buffer_postenable,
 	.predisable = &iio_triggered_buffer_predisable,
 };
