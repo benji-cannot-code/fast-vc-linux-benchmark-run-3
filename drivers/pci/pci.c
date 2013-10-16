171e50f808c39a4906f8aa2a17b0a7446b48d4de
Bjorn Helgaas <bhelgaas@google.com>
2013-10-16 12:32:53 -0600

    PCI: Report pci_pme_active() kmalloc failure
    Previously, if kmalloc() failed, we claimed "PME# enabled" in dmesg,
    even though we didn't add the device to the pci_pme_list.  This prints
    a more correct warning.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -1639,8 +1639,10 @@ void pci_pme_active(struct pci_dev *dev, bool enable)
 		if (enable) {
 			pme_dev = kmalloc(sizeof(struct pci_pme_device),
 					  GFP_KERNEL);
-			if (!pme_dev)
-				goto out;
+			if (!pme_dev) {
+				dev_warn(&dev->dev, "can't enable PME#\n");
+				return;
+			}
 			pme_dev->dev = dev;
 			mutex_lock(&pci_pme_list_mutex);
 			list_add(&pme_dev->list, &pci_pme_list);
@@ -1661,7 +1663,6 @@ void pci_pme_active(struct pci_dev *dev, bool enable)
 		}
 	}
 
-out:
 	dev_dbg(&dev->dev, "PME# %s\n", enable ? "enabled" : "disabled");
 }
 
