70f3403a2f95911bef67f0a707f67c7c3a70dbcb
Sachin Kamat <sachin.kamat@linaro.org>
2013-10-16 14:26:55 +0530

    mfd: tps65910: Include linux/of.h header
    'of_match_ptr' is defined in linux/of.h. Include it explicitly to
    avoid breakage in the future.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/tps65910.c
+++ drivers/mfd/tps65910.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mfd/core.h>
 #include <linux/regmap.h>
 #include <linux/mfd/tps65910.h>
+#include <linux/of.h>
 #include <linux/of_device.h>
 
 static struct resource rtc_resources[] = {
