18b0e53d3292bfb8cdc5eae91837872c35c21856
Max Filippov <jcmvbkbc@gmail.com>
2013-10-17 02:42:12 +0400

    xtensa: remove NO_IRQ definitions
    These definitions (-1) were correct when used as a hardware IRQ number,
    but are incorrect as a mapped IRQ number.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- arch/xtensa/platforms/xtfpga/include/platform/hardware.h
+++ arch/xtensa/platforms/xtfpga/include/platform/hardware.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __XTENSA_XTAVNET_HARDWARE_H
 #define __XTENSA_XTAVNET_HARDWARE_H
 
-/* By default NO_IRQ is defined to 0 in Linux, but we use the
-   interrupt 0 for UART... */
-#define NO_IRQ                 -1
-
 /* Memory configuration. */
 
 #define PLATFORM_DEFAULT_MEM_START 0x00000000
