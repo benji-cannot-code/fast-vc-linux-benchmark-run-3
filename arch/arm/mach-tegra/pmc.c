02d4a529f0c775ee4454a580e429a9c8c5f6e24e
Thierry Reding <thierry.reding@gmail.com>
2013-10-16 19:19:00 +0200

    ARM: tegra: Remove duplicate powergate defines
    Instead of duplicating powergate defines, reuse the ones from the
    include/linux/tegra-powergate.h header file.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/pmc.c
+++ arch/arm/mach-tegra/pmc.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/tegra-powergate.h>
 
 #include "flowctrl.h"
 #include "fuse.h"
@@ -43,12 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PMC_CPUPWRGOOD_TIMER	0xc8
 #define PMC_CPUPWROFF_TIMER	0xcc
-
-#define TEGRA_POWERGATE_PCIE	3
-#define TEGRA_POWERGATE_VDEC	4
-#define TEGRA_POWERGATE_CPU1	9
-#define TEGRA_POWERGATE_CPU2	10
-#define TEGRA_POWERGATE_CPU3	11
 
 static u8 tegra_cpu_domains[] = {
 	0xFF,			/* not available for CPU0 */
