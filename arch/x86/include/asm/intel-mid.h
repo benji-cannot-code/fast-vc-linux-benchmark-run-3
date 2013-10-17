4e9be92acdbc68385232b5edbd9feae58d71163b
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2013-10-17 15:35:33 -0700

    intel_mid: Moved SFI related code to sfi.c
    Moved SFI specific parsing/handling code to sfi.c. This will enable us
    to reuse our intel-mid code for platforms that supports firmware
    interfaces other than SFI (like ACPI).
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Link: http://lkml.kernel.org/r/1382049336-21316-10-git-send-email-david.a.cohen@linux.intel.com
    Signed-off-by: David Cohen <david.a.cohen@linux.intel.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/include/asm/intel-mid.h
+++ arch/x86/include/asm/intel-mid.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int intel_mid_pci_init(void);
 extern int __init sfi_parse_mrtc(struct sfi_table_header *table);
+extern int __init sfi_parse_mtmr(struct sfi_table_header *table);
 extern int sfi_mrtc_num;
 extern struct sfi_rtc_table_entry sfi_mrtc_array[];
 
