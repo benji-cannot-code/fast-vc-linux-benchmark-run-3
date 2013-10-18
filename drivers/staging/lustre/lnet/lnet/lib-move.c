730ed0413e832f63e28161bb5b2b77b73cbc6d30
Lisa Nguyen <lisa@xenapiadmin.com>
2013-10-18 02:12:11 -0700

    staging/lustre/lnet: Remove () from return statements in lib-move.c
    Remove unnecessary parentheses from return statements to
    eliminate errors generated by checkpatch.pl and meet kernel
    coding standards.
    
    Signed-off-by: Lisa Nguyen <lisa@xenapiadmin.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/lnet/lib-move.c
+++ drivers/staging/lustre/lnet/lnet/lib-move.c
@@ -155,7 +155,7 @@ fail_peer(lnet_nid_t nid, int outgoing)
 		LIBCFS_FREE(tp, sizeof(*tp));
 	}
 
-	return (fail);
+	return fail;
 }
 
 unsigned int
@@ -166,7 +166,7 @@ lnet_iov_nob(unsigned int niov, struct iovec *iov)
 	while (niov-- > 0)
 		nob += (iov++)->iov_len;
 
-	return (nob);
+	return nob;
 }
 EXPORT_SYMBOL(lnet_iov_nob);
 
@@ -241,7 +241,7 @@ lnet_extract_iov(int dst_niov, struct iovec *dst,
 	unsigned int    niov;
 
 	if (len == 0)			   /* no data => */
-		return (0);		     /* no frags */
+		return 0;		     /* no frags */
 
 	LASSERT(src_niov > 0);
 	while (offset >= src->iov_len) {      /* skip initial frags */
@@ -261,7 +261,7 @@ lnet_extract_iov(int dst_niov, struct iovec *dst,
 
 		if (len <= frag_len) {
 			dst->iov_len = len;
-			return (niov);
+			return niov;
 		}
 
 		dst->iov_len = frag_len;
@@ -285,7 +285,7 @@ lnet_kiov_nob(unsigned int niov, lnet_kiov_t *kiov)
 	while (niov-- > 0)
 		nob += (kiov++)->kiov_len;
 
-	return (nob);
+	return nob;
 }
 EXPORT_SYMBOL(lnet_kiov_nob);
 
@@ -523,7 +523,7 @@ lnet_extract_kiov(int dst_niov, lnet_kiov_t *dst,
 	unsigned int    niov;
 
 	if (len == 0)			   /* no data => */
-		return (0);		     /* no frags */
+		return 0;		     /* no frags */
 
 	LASSERT(src_niov > 0);
 	while (offset >= src->kiov_len) {      /* skip initial frags */
@@ -546,7 +546,7 @@ lnet_extract_kiov(int dst_niov, lnet_kiov_t *dst,
 			dst->kiov_len = len;
 			LASSERT(dst->kiov_offset + dst->kiov_len
 					     <= PAGE_CACHE_SIZE);
-			return (niov);
+			return niov;
 		}
 
 		dst->kiov_len = frag_len;
@@ -1620,17 +1620,17 @@ lnet_msgtyp2str(int type)
 {
 	switch (type) {
 	case LNET_MSG_ACK:
-		return ("ACK");
+		return "ACK";
 	case LNET_MSG_PUT:
-		return ("PUT");
+		return "PUT";
 	case LNET_MSG_GET:
-		return ("GET");
+		return "GET";
 	case LNET_MSG_REPLY:
-		return ("REPLY");
+		return "REPLY";
 	case LNET_MSG_HELLO:
-		return ("HELLO");
+		return "HELLO";
 	default:
-		return ("<UNKNOWN>");
+		return "<UNKNOWN>";
 	}
 }
 EXPORT_SYMBOL(lnet_msgtyp2str);
