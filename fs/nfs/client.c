d19594d1c4a8dbf9ec67d31783578f10d03efb78
Weston Andros Adamson <dros@netapp.com>
2013-10-18 15:15:17 -0400

    NFS: cache parsed auth_info in nfs_server
    Cache the auth_info structure in nfs_server and pass these values to submounts.
    
    This lays the groundwork for supporting multiple sec= options.
    
    Signed-off-by: Weston Andros Adamson <dros@netapp.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/client.c
+++ fs/nfs/client.c
@@ -787,6 +787,7 @@ static int nfs_init_server(struct nfs_server *server,
 		goto error;
 
 	server->port = data->nfs_server.port;
+	server->auth_info = data->auth_info;
 
 	error = nfs_init_server_rpcclient(server, &timeparms,
 					  data->selected_flavor);
@@ -930,6 +931,7 @@ void nfs_server_copy_userdata(struct nfs_server *target, struct nfs_server *sour
 	target->acdirmax = source->acdirmax;
 	target->caps = source->caps;
 	target->options = source->options;
+	target->auth_info = source->auth_info;
 }
 EXPORT_SYMBOL_GPL(nfs_server_copy_userdata);
 
