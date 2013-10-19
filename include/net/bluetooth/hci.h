ce107b692356ea4f041f802c101a4df10f72c63d
Marcel Holtmann <marcel@holtmann.org>
2013-10-19 07:09:12 -0700

    Bluetooth: Add support for setting DUT mode
    The Device Under Test (DUT) mode is useful for doing certification
    testing and so expose this as debugfs option.
    
    This mode is actually special since you can only enter it. Restoring
    normal operation means that a HCI Reset is required. The current mode
    value gets tracked as a new device flag and when disabling it, the
    correct command to reset the controller is sent.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -116,6 +116,7 @@ enum {
 	HCI_PAIRABLE,
 	HCI_SERVICE_CACHE,
 	HCI_DEBUG_KEYS,
+	HCI_DUT_MODE,
 	HCI_UNREGISTER,
 	HCI_USER_CHANNEL,
 
@@ -1043,6 +1044,8 @@ struct hci_rp_write_remote_amp_assoc {
 	__u8     status;
 	__u8     phy_handle;
 } __packed;
+
+#define HCI_OP_ENABLE_DUT_MODE		0x1803
 
 #define HCI_OP_WRITE_SSP_DEBUG_MODE	0x1804
 
