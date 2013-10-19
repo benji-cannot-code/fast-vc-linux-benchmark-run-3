28f406847472d501decae684ddf69289f5ec3fdf
Teodora Baluta <teobaluta@gmail.com>
2013-10-19 21:09:48 +0300

    staging: rtl8187se: checkpatch cleanups
    Fix the following checkpatch.pl errors:
    
    drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c:890: "foo* bar" should be "foo *bar"
    drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c:944: "foo* bar" should be "foo *bar"
    
    Signed-off-by: Teodora Baluta <teobaluta@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c
+++ drivers/staging/rtl8187se/ieee80211/ieee80211_softmac.c
@@ -888,7 +888,7 @@ static struct sk_buff* ieee80211_probe_resp(struct ieee80211_device *ieee, u8 *d
 	return skb;
 }
 
-static struct sk_buff* ieee80211_assoc_resp(struct ieee80211_device *ieee,
+static struct sk_buff *ieee80211_assoc_resp(struct ieee80211_device *ieee,
 					    u8 *dest)
 {
 	struct sk_buff *skb;
@@ -942,7 +942,7 @@ static struct sk_buff* ieee80211_assoc_resp(struct ieee80211_device *ieee,
 	return skb;
 }
 
-static struct sk_buff* ieee80211_auth_resp(struct ieee80211_device *ieee,
+static struct sk_buff *ieee80211_auth_resp(struct ieee80211_device *ieee,
 					   int status, u8 *dest)
 {
 	struct sk_buff *skb;
