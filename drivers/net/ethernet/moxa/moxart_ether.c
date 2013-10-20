3f7e35e090859174769dcceff76102cdecfee6ef
Michael Opdenacker <michael.opdenacker@free-electrons.com>
2013-10-20 07:13:56 +0200

    ethernet: moxa: remove duplicate includes
    Reported by "make includecheck"
    
    Tested that drivers/net/ethernet/moxa/moxart_ether.c still compiles
    well on ARM
    
    Signed-off-by: Michael Opdenacker <michael.opdenacker@free-electrons.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/moxa/moxart_ether.c
+++ drivers/net/ethernet/moxa/moxart_ether.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/crc32.h>
 #include <linux/crc32c.h>
-#include <linux/dma-mapping.h>
 
 #include "moxart_ether.h"
 
