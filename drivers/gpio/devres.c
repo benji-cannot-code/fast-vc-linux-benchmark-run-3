3716c3c850ed4b03f24bff6913d58ada8a69f874
Alexandre Courbot <acourbot@nvidia.com>
2013-10-20 15:14:58 -0700

    gpiolib: devres: add missing headers
    Add missing headers for drivers/gpiolib/devres.c.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/devres.c
+++ drivers/gpio/devres.c
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/module.h>
+#include <linux/err.h>
 #include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/device.h>
 #include <linux/gfp.h>
 
