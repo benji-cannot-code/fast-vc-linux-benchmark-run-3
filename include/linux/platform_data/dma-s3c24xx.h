cd6b04e2c7c1d2621db7a8ae3d0cec4506d45c2c
Heiko Stuebner <heiko@sntech.de>
2013-10-21 05:32:48 +0900

    ARM: S3C24XX: add dma pdata for s3c2410, s3c2440 and s3c2442
    s3c2410 and s3c2442 share the same dma channels while s3c2440 has
    slight differences. But on all three the reachable sources per dma
    channel has constraints attached and thus encodes the usable
    combinations using the S3C24XX_DMA_CHANREQ macro.
    
    This also fixes the warning about s3c2410_dma_resource being unused
    as reported by Olof Johansson.
    
    Reported-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Reviewed-by: Tomasz Figa <t.figa@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- include/linux/platform_data/dma-s3c24xx.h
+++ include/linux/platform_data/dma-s3c24xx.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * any later version.
  */
 
+/* Helper to encode the source selection constraints for early s3c socs. */
+#define S3C24XX_DMA_CHANREQ(src, chan)	((BIT(3) | src) << chan * 4)
+
 enum s3c24xx_dma_bus {
 	S3C24XX_DMA_APB,
 	S3C24XX_DMA_AHB,
