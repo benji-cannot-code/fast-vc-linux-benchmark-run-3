7754ca87eba3dff4b4647f3bd5197696441ae565
Catalin Marinas <catalin.marinas@arm.com>
2013-10-25 15:48:33 +0100

    arm64: Fix the endianness of arch_spinlock_t
    The owner and next members of the arch_spinlock_t structure need to be
    swapped when compiling for big endian.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Reported-by: Matthew Leach <matthew.leach@arm.com>
    Acked-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/spinlock_types.h
+++ arch/arm64/include/asm/spinlock_types.h
@@ -24,8 +24,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TICKET_SHIFT	16
 
 typedef struct {
+#ifdef __AARCH64EB__
+	u16 next;
+	u16 owner;
+#else
 	u16 owner;
 	u16 next;
+#endif
 } __aligned(4) arch_spinlock_t;
 
 #define __ARCH_SPIN_LOCK_UNLOCKED	{ 0 , 0 }
