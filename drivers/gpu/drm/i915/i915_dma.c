e9f5bd378c1e86fcf2049a0cfcf89612fd50807e
Imre Deak <imre.deak@intel.com>
2013-10-28 17:20:35 +0200

    drm/i915: rename i915_init_power_well to init_power_domains_init
    Similarly rename the other related functions in the power domain
    interface.
    
    Higher level driver code calling these functions knows only about power
    domains, not the underlying power wells which may be different on
    different platforms. Also these functions really init/cleanup/resume
    power domains and only through that all related power wells, so rename
    them accordingly.
    
    Note that I left i915_{request,release}_power_well as is, since that
    really changes the state only of a single power well (and is HSW
    specific). It should also get a better name once we make it more
    generic by controlling things through a new audio power domain.
    
    v4:
    - use intel prefix instead of i915 everywhere (Paulo)
    - use a $prefix_$block_$action format (Daniel)
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1315,7 +1315,7 @@ static int i915_load_modeset_init(struct drm_device *dev)
 	if (ret)
 		goto cleanup_gem_stolen;
 
-	intel_init_power_well(dev);
+	intel_power_domains_init_hw(dev);
 
 	/* Keep VGA alive until i915_disable_vga_mem() */
 	intel_display_power_get(dev, POWER_DOMAIN_VGA);
@@ -1654,7 +1654,7 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 	}
 
 	if (HAS_POWER_WELL(dev))
-		i915_init_power_well(dev);
+		intel_power_domains_init(dev);
 
 	if (drm_core_check_feature(dev, DRIVER_MODESET)) {
 		ret = i915_load_modeset_init(dev);
@@ -1682,7 +1682,7 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 
 out_power_well:
 	if (HAS_POWER_WELL(dev))
-		i915_remove_power_well(dev);
+		intel_power_domains_remove(dev);
 	drm_vblank_cleanup(dev);
 out_gem_unload:
 	if (dev_priv->mm.inactive_shrinker.scan_objects)
@@ -1725,7 +1725,7 @@ int i915_driver_unload(struct drm_device *dev)
 		 * the power well is not enabled, so just enable it in case
 		 * we're going to unload/reload. */
 		intel_display_set_init_power(dev, true);
-		i915_remove_power_well(dev);
+		intel_power_domains_remove(dev);
 	}
 
 	i915_teardown_sysfs(dev);
