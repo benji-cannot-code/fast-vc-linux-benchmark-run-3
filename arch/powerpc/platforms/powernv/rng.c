e68780e7a8defc8b6de72215881512e3a864f96e
Stephen Rothwell <sfr@canb.auug.org.au>
2013-10-28 19:34:41 +1100

    powerpc: Add includes to fix powernv/rng.c build
    Caused by commit a4da0d50b2a0 ("powerpc: Implement
    arch_get_random_long/int() for powernv") from the powerpc tree
    interacting with commit b5b4bb3f6a11 ("of: only include prom.h on sparc")
    from the dt-rh tree.
    
    I added this merge fix patch (which will need to be sent to Linus when
    these two trees get merged, or could be applied now to the powerpc tree):
    
    [ Also add linux/smp.h to get cpu_to_chip_id -- BenH ]
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Rob Herring <rob.herring@calxeda.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/platforms/powernv/rng.c
+++ arch/powerpc/platforms/powernv/rng.c
@@ -12,10 +12,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/of.h>
+#include <linux/of_address.h>
 #include <linux/of_platform.h>
 #include <linux/slab.h>
+#include <linux/smp.h>
 #include <asm/archrandom.h>
 #include <asm/io.h>
+#include <asm/prom.h>
 #include <asm/machdep.h>
 
 
