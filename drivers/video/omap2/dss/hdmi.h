dc4435b991fe7462d023b78eb9153cf9f84adf28
Tomi Valkeinen <tomi.valkeinen@ti.com>
2013-10-28 11:47:28 +0200

    OMAPDSS: HDMI: add missing core irq
    HDMI_IRQ_CORE was not defined in the hdmi.h.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/hdmi.h
+++ drivers/video/omap2/dss/hdmi.h
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HDMI_WP_AUDIO_DATA			0x8C
 
 /* HDMI WP IRQ flags */
-
+#define HDMI_IRQ_CORE				(1 << 0)
 #define HDMI_IRQ_OCP_TIMEOUT			(1 << 4)
 #define HDMI_IRQ_AUDIO_FIFO_UNDERFLOW		(1 << 8)
 #define HDMI_IRQ_AUDIO_FIFO_OVERFLOW		(1 << 9)
