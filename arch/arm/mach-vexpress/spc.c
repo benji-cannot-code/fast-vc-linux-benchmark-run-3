f8d61a8926e11e1d6df25a697b37fe2842770db0
Sudeep KarkadaNagesha <sudeep.karkadanagesha@arm.com>
2013-10-29 12:18:40 +0000

    ARM: vexpress/TC2: register vexpress-spc cpufreq device
    This patch adds vexpress-spc platform device to enables the vexpress
    SPC cpufreq interface driver.
    
    Signed-off-by: Sudeep KarkadaNagesha <sudeep.karkadanagesha@arm.com>
    Acked-by: Pawel Moll <Pawel.Moll@arm.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- arch/arm/mach-vexpress/spc.c
+++ arch/arm/mach-vexpress/spc.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
+#include <linux/platform_device.h>
 #include <linux/pm_opp.h>
 #include <linux/slab.h>
 #include <linux/semaphore.h>
@@ -538,6 +539,7 @@ static int __init ve_spc_clk_init(void)
 			pr_warn("failed to initialise cpu%d opp table\n", cpu);
 	}
 
+	platform_device_register_simple("vexpress-spc-cpufreq", -1, NULL, 0);
 	return 0;
 }
 module_init(ve_spc_clk_init);
