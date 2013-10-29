377c60631ba3bb111590a0b1f043e4eab00f3270
Bob Moore <robert.moore@intel.com>
2013-10-29 09:29:09 +0800

    ACPICA: Add history/line-editing for Unix/Linux systems.
    The original upstream ACPICA change added full history and limited
    line editing to the debugger:
    
     This change adds full history and limited line editing for Unix and
     Linux systems. It does not use readline() because of portability issues.
     Instead it uses the POSIX termio interface to put the terminal in raw
     input mode so that the various special keys can be trapped (such as
     up-arrow and down-arrow for history support).
    
    Since the debugger is not shipped in the kernel, it only is necessary
    to update one header file to keep the kernel source in sync with the
    upstream.
    
    [rjw: Changelog]
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/acdebug.h
+++ drivers/acpi/acpica/acdebug.h
@@ -244,6 +244,8 @@ void acpi_db_display_history(void);
 
 char *acpi_db_get_from_history(char *command_num_arg);
 
+char *acpi_db_get_history_by_index(u32 commandd_num);
+
 /*
  * dbinput - user front-end to the AML debugger
  */
