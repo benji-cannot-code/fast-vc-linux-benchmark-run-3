8ca00a2f03fffa8800409083e70a88c2954d4c5c
Viresh Kumar <viresh.kumar@linaro.org>
2013-10-29 18:56:06 +0530

    cpufreq: distinguish drivers that do asynchronous notifications
    There are few special cases like exynos5440 which doesn't send POSTCHANGE
    notification from their ->target() routine and call some kind of bottom halves
    for doing this work, work/tasklet/etc.. From which they finally send POSTCHANGE
    notification.
    
    Its better if we distinguish them from other cpufreq drivers in some way so that
    core can handle them specially. So this patch introduces another flag:
    CPUFREQ_ASYNC_NOTIFICATION, which will be set by such drivers.
    
    This also changes exynos5440-cpufreq.c and powernow-k8 in order to set this
    flag.
    
    Acked-by: Amit Daniel Kachhap <amit.daniel@samsung.com>
    Acked-by: Kukjin Kim <kgene.kim@samsung.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/powernow-k8.c
+++ drivers/cpufreq/powernow-k8.c
@@ -1205,6 +1205,7 @@ out:
 }
 
 static struct cpufreq_driver cpufreq_amd64_driver = {
+	.flags		= CPUFREQ_ASYNC_NOTIFICATION,
 	.verify		= cpufreq_generic_frequency_table_verify,
 	.target_index	= powernowk8_target,
 	.bios_limit	= acpi_processor_get_bios_limit,
