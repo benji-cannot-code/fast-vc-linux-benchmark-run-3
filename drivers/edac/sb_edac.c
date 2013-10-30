424257acbaecae4230c145c5c47c766730df715d
Aristeu Rozanski <arozansk@redhat.com>
2013-10-30 13:27:05 -0300

    sb_edac: avoid decoding the same error multiple times
    Whenever the extended error reporting is active, multiple MCEs will be
    generated for the same event, which will lead to multiple repeated
    errors to be reported. So check ADDRV and only decode the error if the
    MCE address is valid.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/edac/sb_edac.c
+++ drivers/edac/sb_edac.c
@@ -1428,6 +1428,10 @@ static void sbridge_mce_output_error(struct mem_ctl_info *mci,
 		}
 	}
 
+	/* Only decode errors with an valid address (ADDRV) */
+	if (!GET_BITFIELD(m->status, 58, 58))
+		return;
+
 	rc = get_memory_error_data(mci, m->addr, &socket,
 				   &channel_mask, &rank, &area_type, msg);
 	if (rc < 0)
