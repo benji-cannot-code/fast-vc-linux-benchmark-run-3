e8b515d1643f3565223d94ccaee4b1e2dfd02ad0
Peter Zijlstra <peterz@infradead.org>
2013-10-30 11:42:46 +0100

    tools/perf: Add required memory barriers
    To match patch bf378d341e48 ("perf: Fix perf ring buffer memory
    ordering") change userspace to also adhere to the ordering outlined.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Michael Neuling <mikey@neuling.org>
    Cc: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    Cc: james.hogan@imgtec.com
    Cc: Vince Weaver <vince@deater.net>
    Cc: Victor Kaplansky <VICTORK@il.ibm.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Anton Blanchard <anton@samba.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    Cc: Michael Ellerman <michael@ellerman.id.au>
    Link: http://lkml.kernel.org/r/20131030104246.GH16117@laptop.programming.kicks-ass.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/perf/tests/rdpmc.c
+++ tools/perf/tests/rdpmc.c
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if defined(__x86_64__) || defined(__i386__)
 
-#define barrier() asm volatile("" ::: "memory")
-
 static u64 rdpmc(unsigned int counter)
 {
 	unsigned int low, high;
