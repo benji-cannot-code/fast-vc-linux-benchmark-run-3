4ccf3adc12addddd0ec81c21566e048e1e05a427
Seiji Aguchi <seiji.aguchi@hds.com>
2013-10-30 16:39:03 -0400

    x86, trace: Add page fault tracepoints
    This patch introduces page fault tracepoints to x86 architecture
    by switching IDT.
    
      Two events, for user and kernel spaces, are introduced at the beginning
      of page fault handler for tracing.
    
      - User space event
        There is a request of page fault event for user space as below.
    
        https://lkml.kernel.org/r/1368079520-11015-2-git-send-email-fdeslaur+()+gmail+!+com
        https://lkml.kernel.org/r/1368079520-11015-1-git-send-email-fdeslaur+()+gmail+!+com
    
      - Kernel space event:
        When we measure an overhead in kernel space for investigating performance
        issues, we can check if it comes from the page fault events.
    
    Signed-off-by: Seiji Aguchi <seiji.aguchi@hds.com>
    Link: http://lkml.kernel.org/r/52716E67.6090705@hds.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/kmemcheck.h>		/* kmemcheck_*(), ...		*/
 #include <asm/fixmap.h>			/* VSYSCALL_START		*/
 
+#define CREATE_TRACE_POINTS
+#include <asm/trace/exceptions.h>
+
 /*
  * Page fault error code bits:
  *
@@ -1233,12 +1236,22 @@ do_page_fault(struct pt_regs *regs, unsigned long error_code)
 	exception_exit(prev_state);
 }
 
+static void trace_page_fault_entries(struct pt_regs *regs,
+				     unsigned long error_code)
+{
+	if (user_mode(regs))
+		trace_user_page_fault(read_cr2(), regs, error_code);
+	else
+		trace_kernel_page_fault(read_cr2(), regs, error_code);
+}
+
 dotraplinkage void __kprobes
 trace_do_page_fault(struct pt_regs *regs, unsigned long error_code)
 {
 	enum ctx_state prev_state;
 
 	prev_state = exception_enter();
+	trace_page_fault_entries(regs, error_code);
 	__do_page_fault(regs, error_code);
 	exception_exit(prev_state);
 }
