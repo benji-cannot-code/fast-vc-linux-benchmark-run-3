2be1e9b9782ade5303cf249c424fa8c3e4f3d48e
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-10-30 13:32:15 +0800

    SUNRPC: remove duplicated include from clnt.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/in.h>
 #include <linux/in6.h>
 #include <linux/un.h>
-#include <linux/rcupdate.h>
 
 #include <linux/sunrpc/clnt.h>
 #include <linux/sunrpc/addr.h>
