e65d2fe69ddd116c5bf7a9420be73d36042a8f96
Chris Wilson <chris@chris-wilson.co.uk>
2013-10-30 09:28:22 +0000

    drm/i915: Capture batchbuffer state upon GPU hang
    The bbstate contains useful bits of debugging information such as
    whether the batch is being read from GTT or PPGTT, or whether it is
    allowed to execute privileged instructions.
    
    v2: Only record BB_STATE for gen4+
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -719,6 +719,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NOPID		0x02094
 #define HWSTAM		0x02098
 #define DMA_FADD_I8XX	0x020d0
+#define RING_BBSTATE(base)	((base)+0x110)
 
 #define ERROR_GEN6	0x040a0
 #define GEN7_ERR_INT	0x44040
