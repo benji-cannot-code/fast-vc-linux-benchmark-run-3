f364d63cb39669dc645f9ed116ef50d3b7dc950c
Thierry Reding <treding@nvidia.com>
2013-10-31 13:28:50 +0100

    drm/tegra: Make legacy fbdev support optional
    A lot of the modern userspace is capable of working without the legacy
    fbdev support. kmscon can be used as a replacement for the framebuffer
    console, and KMS X drivers create their own framebuffers.
    
    Most people don't have a system where all of this works yet, though, so
    leave support enabled by default.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/drm.h
+++ drivers/gpu/drm/tegra/drm.h
@@ -28,10 +28,12 @@ struct tegra_fb {
 	unsigned int num_planes;
 };
 
+#ifdef CONFIG_DRM_TEGRA_FBDEV
 struct tegra_fbdev {
 	struct drm_fb_helper base;
 	struct tegra_fb *fb;
 };
+#endif
 
 struct tegra_drm {
 	struct drm_device *drm;
@@ -39,7 +41,9 @@ struct tegra_drm {
 	struct mutex clients_lock;
 	struct list_head clients;
 
+#ifdef CONFIG_DRM_TEGRA_FBDEV
 	struct tegra_fbdev *fbdev;
+#endif
 };
 
 struct tegra_drm_client;
@@ -266,7 +270,9 @@ bool tegra_fb_is_bottom_up(struct drm_framebuffer *framebuffer);
 bool tegra_fb_is_tiled(struct drm_framebuffer *framebuffer);
 extern int tegra_drm_fb_init(struct drm_device *drm);
 extern void tegra_drm_fb_exit(struct drm_device *drm);
+#ifdef CONFIG_DRM_TEGRA_FBDEV
 extern void tegra_fbdev_restore_mode(struct tegra_fbdev *fbdev);
+#endif
 
 extern struct platform_driver tegra_dc_driver;
 extern struct platform_driver tegra_dsi_driver;
