13577ca2e53de639cab57072254ca82d7f721c43
Stephen Boyd <sboyd@codeaurora.org>
2013-10-30 18:21:09 -0700

    ARM: Introduce CPU_METHOD_OF_DECLARE() for cpu hotplug/smp
    The goal of multi-platform kernels is to remove the need for mach
    directories and machine descriptors. To further that goal,
    introduce CPU_METHOD_OF_DECLARE() to allow cpu hotplug/smp
    support to be separated from the machine descriptors.
    Implementers should specify an enable-method property in their
    cpus node and then implement a matching set of smp_ops in their
    hotplug/smp code, wiring it up with the CPU_METHOD_OF_DECLARE()
    macro. When the kernel is compiled we'll collect all the
    enable-method smp_ops into one section for use at boot.
    
    At boot time we'll look for an enable-method in each cpu node and
    try to match that against all known CPU enable methods in the
    kernel. If there are no enable-methods in the cpu nodes we
    fallback to the cpus node and try to use any enable-method found
    there. If that doesn't work we fall back to the old way of using
    the machine descriptor.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: <devicetree@vger.kernel.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Kumar Gala <galak@codeaurora.org>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -168,6 +168,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_OF_TABLES()
 #endif
 
+#ifdef CONFIG_SMP
+#define CPU_METHOD_OF_TABLES() . = ALIGN(8);				    \
+			   VMLINUX_SYMBOL(__cpu_method_of_table_begin) = .; \
+			   *(__cpu_method_of_table)			    \
+			   VMLINUX_SYMBOL(__cpu_method_of_table_end) = .;
+#else
+#define CPU_METHOD_OF_TABLES()
+#endif
+
 #define KERNEL_DTB()							\
 	STRUCT_ALIGN();							\
 	VMLINUX_SYMBOL(__dtb_start) = .;				\
@@ -492,6 +501,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	MEM_DISCARD(init.rodata)					\
 	CLK_OF_TABLES()							\
 	CLKSRC_OF_TABLES()						\
+	CPU_METHOD_OF_TABLES()						\
 	KERNEL_DTB()							\
 	IRQCHIP_OF_MATCH_TABLE()
 
