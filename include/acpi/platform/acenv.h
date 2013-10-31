550081e071c6b5a02575f4727bfa55dc1efd2e3f
Lv Zheng <lv.zheng@intel.com>
2013-10-31 09:31:12 +0800

    ACPICA: Update acpidump related header file changes.
    This patch updates header files used by acpidump to reduce the
    source code differences between Linux and ACPICA upstream.
    
    This patch does not affect the generation of the Linux kernel binary.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/platform/acenv.h
+++ include/acpi/platform/acenv.h
@@ -97,10 +97,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 /*
- * acpi_bin/acpi_help/acpi_src configuration. All single threaded, with
- * no debug output.
+ * acpi_bin/acpi_dump/acpi_src/acpi_xtract configuration. All single
+ * threaded, with no debug output.
  */
 #if (defined ACPI_BIN_APP)   || \
+	(defined ACPI_DUMP_APP)  || \
 	(defined ACPI_SRC_APP)   || \
 	(defined ACPI_XTRACT_APP)
 #define ACPI_APPLICATION
