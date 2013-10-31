2c207ab55698ac464bba7d2f1224046e22e3cfff
Tushar Behera <tushar.behera@linaro.org>
2013-10-31 06:46:14 +0100

    ARM: 7874/2: bL_switcher: Remove cpu_hotplug_driver_{lock,unlock}()
    Commit 6dedcca610c6 ("hotplug, powerpc, x86: Remove
    cpu_hotplug_driver_lock())" removes the the definition of
    cpu_hotplug_driver_{lock,unlock} APIs, thereby causing a build error.
    
    Replace these calls with {lock,unlock}_device_hotplug().
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/common/bL_switcher.c
+++ arch/arm/common/bL_switcher.c
@@ -578,9 +578,9 @@ static int bL_switcher_enable(void)
 	int cpu, ret;
 
 	mutex_lock(&bL_switcher_activation_lock);
-	cpu_hotplug_driver_lock();
+	lock_device_hotplug();
 	if (bL_switcher_active) {
-		cpu_hotplug_driver_unlock();
+		unlock_device_hotplug();
 		mutex_unlock(&bL_switcher_activation_lock);
 		return 0;
 	}
@@ -616,7 +616,7 @@ error:
 	bL_activation_notify(BL_NOTIFY_POST_DISABLE);
 
 out:
-	cpu_hotplug_driver_unlock();
+	unlock_device_hotplug();
 	mutex_unlock(&bL_switcher_activation_lock);
 	return ret;
 }
@@ -630,7 +630,7 @@ static void bL_switcher_disable(void)
 	struct task_struct *task;
 
 	mutex_lock(&bL_switcher_activation_lock);
-	cpu_hotplug_driver_lock();
+	lock_device_hotplug();
 
 	if (!bL_switcher_active)
 		goto out;
@@ -686,7 +686,7 @@ static void bL_switcher_disable(void)
 	bL_activation_notify(BL_NOTIFY_POST_DISABLE);
 
 out:
-	cpu_hotplug_driver_unlock();
+	unlock_device_hotplug();
 	mutex_unlock(&bL_switcher_activation_lock);
 }
 
