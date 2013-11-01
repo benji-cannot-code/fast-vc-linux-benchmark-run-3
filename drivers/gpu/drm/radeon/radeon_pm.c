724363c1830feb1e69f6c8ef85e3751c16cd3b85
Alex Deucher <alexander.deucher@amd.com>
2013-11-01 15:16:02 -0400

    drm/radeon: enable DPM by default on r7xx asics
    Seems to be stable on them.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_pm.c
+++ drivers/gpu/drm/radeon/radeon_pm.c
@@ -1241,10 +1241,6 @@ int radeon_pm_init(struct radeon_device *rdev)
 	case CHIP_RV670:
 	case CHIP_RS780:
 	case CHIP_RS880:
-	case CHIP_RV770:
-	case CHIP_RV730:
-	case CHIP_RV710:
-	case CHIP_RV740:
 	case CHIP_CAYMAN:
 	case CHIP_ARUBA:
 	case CHIP_BONAIRE:
@@ -1262,6 +1258,10 @@ int radeon_pm_init(struct radeon_device *rdev)
 		else
 			rdev->pm.pm_method = PM_METHOD_PROFILE;
 		break;
+	case CHIP_RV770:
+	case CHIP_RV730:
+	case CHIP_RV710:
+	case CHIP_RV740:
 	case CHIP_CEDAR:
 	case CHIP_REDWOOD:
 	case CHIP_JUNIPER:
