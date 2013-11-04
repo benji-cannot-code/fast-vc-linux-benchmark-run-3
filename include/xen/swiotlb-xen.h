918cdf607d4301bc6ffeb96f2f98a1a605be591a
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-11-04 17:54:27 +0000

    swiotlb-xen: missing include dma-direction.h
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Acked-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/swiotlb-xen.h
+++ include/xen/swiotlb-xen.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_SWIOTLB_XEN_H
 #define __LINUX_SWIOTLB_XEN_H
 
+#include <linux/dma-direction.h>
 #include <linux/swiotlb.h>
 
 extern int xen_swiotlb_init(int verbose, bool early);
