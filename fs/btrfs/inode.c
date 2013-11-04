7c5753377ff53fec991fa5ba4e87170ce0689327
Valentina Giusti <valentina.giusti@microon.de>
2013-11-04 22:34:22 +0100

    btrfs: remove unused variable from btrfs_new_inode
    Variable owner in btrfs_new_inode is unused since commit
    d82a6f1d7e8b61ed5996334d0db66651bb43641d
    (Btrfs: kill BTRFS_I(inode)->block_group)
    
    Signed-off-by: Valentina Giusti <valentina.giusti@microon.de>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -5373,7 +5373,6 @@ static struct inode *btrfs_new_inode(struct btrfs_trans_handle *trans,
 	u32 sizes[2];
 	unsigned long ptr;
 	int ret;
-	int owner;
 
 	path = btrfs_alloc_path();
 	if (!path)
@@ -5418,11 +5417,6 @@ static struct inode *btrfs_new_inode(struct btrfs_trans_handle *trans,
 	 * old info in the log.
 	 */
 	set_bit(BTRFS_INODE_NEEDS_FULL_SYNC, &BTRFS_I(inode)->runtime_flags);
-
-	if (S_ISDIR(mode))
-		owner = 0;
-	else
-		owner = 1;
 
 	key[0].objectid = objectid;
 	btrfs_set_key_type(&key[0], BTRFS_INODE_ITEM_KEY);
