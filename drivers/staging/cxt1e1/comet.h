6e75d361d595094baa2a387e3f6f553518489c47
Sima Baymani <sima.baymani@gmail.com>
2013-11-05 21:22:56 +0100

    staging: cxt1e1: comet,h - fix placement of braces
    Fix bad placement of braces per checkpatch warning.
    
    Signed-off-by: Sima Baymani <sima.baymani@gmail.com>
    Reviewed-by: Lisa Nguyen <lisa@xenapiadmin.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/cxt1e1/comet.h
+++ drivers/staging/cxt1e1/comet.h
@@ -26,8 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VINT32  volatile u_int32_t
 
-struct s_comet_reg
-{
+struct s_comet_reg {
 	VINT32 gbl_cfg;      /* 00  Global Cfg */
 	VINT32 clkmon;       /* 01  Clk Monitor */
 	VINT32 rx_opt;       /* 02  RX Options */
