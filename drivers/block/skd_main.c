9b259a94bd8b7f0f09d5c5778435d8a55edf6553
Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
2013-11-05 12:37:03 +0100

    skd: remove SCSI subsystem specific includes
    This is not a SCSI host driver so remove SCSI subsystem specific
    includes.
    
    Cc: Akhil Bhansali <abhansali@stec-inc.com>
    Cc: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/skd_main.c
+++ drivers/block/skd_main.c
@@ -43,9 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/wait.h>
 #include <linux/uio.h>
 #include <scsi/scsi.h>
-#include <scsi/scsi_host.h>
-#include <scsi/scsi_tcq.h>
-#include <scsi/scsi_cmnd.h>
 #include <scsi/sg.h>
 #include <linux/io.h>
 #include <linux/uaccess.h>
