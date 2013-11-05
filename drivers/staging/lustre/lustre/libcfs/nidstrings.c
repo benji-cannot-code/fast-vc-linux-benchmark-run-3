3b65feaa0a1cbbdb76791120cdc54176066731b6
Teodora Baluta <teobaluta@gmail.com>
2013-11-05 18:36:47 +0200

    staging: lustre: remove initialisation of static variable
    Fix the following checkpatch.pl error:
    
    ERROR: do not initialise statics to 0 or NULL
    +static int       libcfs_nidstring_idx = 0;
    
    Signed-off-by: Teodora Baluta <teobaluta@gmail.com>
    Reviewed-by: Lisa Nguyen <lisa@xenapiadmin.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/libcfs/nidstrings.c
+++ drivers/staging/lustre/lustre/libcfs/nidstrings.c
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 static char      libcfs_nidstrings[LNET_NIDSTR_COUNT][LNET_NIDSTR_SIZE];
-static int       libcfs_nidstring_idx = 0;
+static int       libcfs_nidstring_idx;
 
 static spinlock_t libcfs_nidstring_lock;
 
