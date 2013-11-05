646d8d09022daa279aa91da77133c2aae6734544
Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
2013-11-05 12:37:04 +0100

    skd: use <asm/unaligned.h>
    Use <asm/unaligned.h> instead of <asm-generic/unaligned.h>.
    
    Cc: Akhil Bhansali <abhansali@stec-inc.com>
    Cc: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/skd_main.c
+++ drivers/block/skd_main.c
@@ -46,7 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/sg.h>
 #include <linux/io.h>
 #include <linux/uaccess.h>
-#include <asm-generic/unaligned.h>
+#include <asm/unaligned.h>
 
 #include "skd_s1120.h"
 
