a8e40d174cd1f40e1ceb8c083a97d2d7fc3debb8
Linus Walleij <linus.walleij@linaro.org>
2013-11-05 10:30:14 +0100

    pinctrl: at91: copy define to driver
    The #define for the maximum number of GPIO blocks was retrieved
    into pinctrl-at91.c by implicit inclusion of <mach/gpio.h>
    from <linux/gpio.h> creating a dependency on machine-local
    <mach/gpio.h>. Break the depenency by copying this single
    define into the driver.
    
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-at91.c
+++ drivers/pinctrl/pinctrl-at91.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "core.h"
 
+#define MAX_GPIO_BANKS		5
 #define MAX_NB_GPIO_PER_BANK	32
 
 struct at91_pinctrl_mux_ops;
