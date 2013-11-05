41d3013e21d697c8b92306f2fd696d7e71674d1f
Luis R. Rodriguez <mcgrof@do-not-panic.com>
2013-11-05 09:18:01 -0800

    cfg80211: check regulatory request alpha2 early
    Currently nl80211 allows userspace to send the kernel
    a bogus regulatory domain with at most 32 rules set
    and it won't reject it until after its allocated
    memory. Let's be smart about it and take advantage
    that the last_request is now available under RTNL
    and check if the alpha2 matches an expected request
    and reject any bogus userspace requests prior to
    hitting the memory allocator.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/reg.h
+++ net/wireless/reg.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern const struct ieee80211_regdomain __rcu *cfg80211_regdomain;
 
+bool reg_is_valid_request(const char *alpha2);
 bool is_world_regdom(const char *alpha2);
 bool reg_supported_dfs_region(u8 dfs_region);
 
