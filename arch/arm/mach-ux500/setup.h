dbf6327d7adf8f3a866f4839b7653b705ffeaee7
Lee Jones <lee.jones@linaro.org>
2013-11-06 10:05:47 +0000

    ARM: ux500: Clean-up legacy extern prototype
    Decomission io_mapping() and device init calls from
    non-DT boot path.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/setup.h
+++ arch/arm/mach-ux500/setup.h
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void ux500_restart(enum reboot_mode mode, const char *cmd);
 
 void __init ux500_map_io(void);
-extern void __init u8500_map_io(void);
-
-extern struct device * __init u8500_init_devices(void);
 
 extern void __init ux500_init_irq(void);
 
