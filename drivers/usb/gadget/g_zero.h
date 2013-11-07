c1a0edc767b7af79b332e3e5bcfba0108e4fba9f
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2013-11-07 08:41:27 +0100

    usb: gadget: f_loopback: add configfs support
    Add support for using the loopback USB function in gadgets composed with
    configfs.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/g_zero.h
+++ drivers/usb/gadget/g_zero.h
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __G_ZERO_H
 #define __G_ZERO_H
 
+#define GZERO_BULK_BUFLEN	4096
+#define GZERO_QLEN		32
+
 struct usb_zero_options {
 	unsigned pattern;
 	unsigned isoc_interval;
@@ -31,6 +34,15 @@ struct f_lb_opts {
 	struct usb_function_instance func_inst;
 	unsigned bulk_buflen;
 	unsigned qlen;
+
+	/*
+	 * Read/write access to configfs attributes is handled by configfs.
+	 *
+	 * This is to protect the data from concurrent access by read/write
+	 * and create symlink/remove symlink.
+	 */
+	struct mutex			lock;
+	int				refcnt;
 };
 
 void lb_modexit(void);
