f34cb09e525d45b8d196a268bc57da4a923939dd
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2013-11-07 01:40:50 +0100

    ACPI: Remove acpi_pci_slot_init() headers from internal.h
    Since acpi_pci_slot_init() is now called from acpi_pci_init()
    and pci-acpi.h contains its header, remove that header (and the
    empty definition of that function for CONFIG_ACPI_PCI_SLOT unset)
    from internal.h as it doesn't have to be there any more.  That also
    avoids a build warning about duplicate function definitions for
    CONFIG_ACPI_PCI_SLOT unset.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/internal.h
+++ drivers/acpi/internal.h
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 acpi_status acpi_os_initialize1(void);
 int init_acpi_device_notify(void);
 int acpi_scan_init(void);
-#ifdef	CONFIG_ACPI_PCI_SLOT
-void acpi_pci_slot_init(void);
-#else
-static inline void acpi_pci_slot_init(void) { }
-#endif
 void acpi_pci_root_init(void);
 void acpi_pci_link_init(void);
 void acpi_pci_root_hp_init(void);
