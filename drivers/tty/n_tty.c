ccc49800a12e6dd0c7893fbf252ec9318902822b
Peter Hurley <peter@hurleysoftware.com>
2013-11-07 13:59:46 -0500

    n_tty: Ensure reader restarts worker for next reader
    A departing reader must restart a flush_to_ldisc() worker _before_
    the next reader enters the read loop; this is to avoid the new reader
    concluding no more i/o is available and prematurely exiting, when the
    old reader simply hasn't re-started the worker yet.
    
    Cc: stable <stable@vger.kernel.org> # 3.12
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/n_tty.c
+++ drivers/tty/n_tty.c
@@ -2244,6 +2244,9 @@ static ssize_t n_tty_read(struct tty_struct *tty, struct file *file,
 		if (time)
 			timeout = time;
 	}
+	n_tty_set_room(tty);
+	up_read(&tty->termios_rwsem);
+
 	mutex_unlock(&ldata->atomic_read_lock);
 	remove_wait_queue(&tty->read_wait, &wait);
 
@@ -2254,8 +2257,6 @@ static ssize_t n_tty_read(struct tty_struct *tty, struct file *file,
 	if (b - buf)
 		retval = b - buf;
 
-	n_tty_set_room(tty);
-	up_read(&tty->termios_rwsem);
 	return retval;
 }
 
