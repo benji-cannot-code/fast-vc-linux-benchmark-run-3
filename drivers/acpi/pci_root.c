9f2b97ceca20c3644b012ecbcc237a337a136382
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2013-11-07 01:41:48 +0100

    ACPI / hotplug: Make acpi_bus_hot_remove_device() internal
    Notice that handle_root_bridge_removal() is the only user of
    acpi_bus_hot_remove_device(), so it doesn't have to be exported
    any more and can be made internal to the ACPI core.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Toshi Kani <toshi.kani@hp.com>

--- drivers/acpi/pci_root.c
+++ drivers/acpi/pci_root.c
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <acpi/acpi_drivers.h>
 #include <acpi/apei.h>
 
+#include "internal.h"
+
 #define PREFIX "ACPI: "
 
 #define _COMPONENT		ACPI_PCI_COMPONENT
