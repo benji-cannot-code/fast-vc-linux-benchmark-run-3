c378ffa15644b6564f87a7df779d61854d2827dc
Peter Zijlstra <peterz@infradead.org>
2013-11-08 12:01:59 +0100

    arch: Move smp_mb__{before,after}_atomic_{inc,dec}.h into asm/atomic.h
    Move the barriers functions that depend on the atomic implementation
    into the atomic implementation.
    
    Reviewed-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Vineet Gupta <vgupta@synopsys.com> [for arch/arc bits]
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/20131213150640.786183683@infradead.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/arc/include/asm/barrier.h
+++ arch/arc/include/asm/barrier.h
@@ -31,11 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define smp_wmb()       barrier()
 #endif
 
-#define smp_mb__before_atomic_dec()	barrier()
-#define smp_mb__after_atomic_dec()	barrier()
-#define smp_mb__before_atomic_inc()	barrier()
-#define smp_mb__after_atomic_inc()	barrier()
-
 #define smp_read_barrier_depends()      do { } while (0)
 
 #endif
