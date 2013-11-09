55e5f47ff82a08c1b682f735ce6bd1c051086207
Michael Heimpold <mhei@heimpold.de>
2013-11-09 12:14:16 +0100

    ARM: mxs: add support for I2SE's duckbill series
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-mxs/mach-mxs.c
+++ arch/arm/mach-mxs/mach-mxs.c
@@ -158,6 +158,7 @@ enum mac_oui {
 	OUI_FSL,
 	OUI_DENX,
 	OUI_CRYSTALFONTZ,
+	OUI_I2SE,
 };
 
 static void __init update_fec_mac_prop(enum mac_oui oui)
@@ -211,6 +212,11 @@ static void __init update_fec_mac_prop(enum mac_oui oui)
 			macaddr[0] = 0x58;
 			macaddr[1] = 0xb9;
 			macaddr[2] = 0xe1;
+			break;
+		case OUI_I2SE:
+			macaddr[0] = 0x00;
+			macaddr[1] = 0x01;
+			macaddr[2] = 0x87;
 			break;
 		}
 		val = ocotp[i];
@@ -329,6 +335,11 @@ static void __init tx28_post_init(void)
 static void __init crystalfontz_init(void)
 {
 	update_fec_mac_prop(OUI_CRYSTALFONTZ);
+}
+
+static void __init duckbill_init(void)
+{
+	update_fec_mac_prop(OUI_I2SE);
 }
 
 static void __init m28cu3_init(void)
@@ -463,6 +474,8 @@ static void __init mxs_machine_init(void)
 		apx4devkit_init();
 	else if (of_machine_is_compatible("crystalfontz,cfa10036"))
 		crystalfontz_init();
+	else if (of_machine_is_compatible("i2se,duckbill"))
+		duckbill_init();
 	else if (of_machine_is_compatible("msr,m28cu3"))
 		m28cu3_init();
 
