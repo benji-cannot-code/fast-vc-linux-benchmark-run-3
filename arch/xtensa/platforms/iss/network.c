50d3074beff17bab52d4db29781165d87430ee88
Max Filippov <jcmvbkbc@gmail.com>
2013-11-10 16:05:45 +0400

    xtensa: ISS: fix command line parameter name
    Now 'ethX=...' syntax can actually be used in kernel command line to
    specify network interfaces.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- arch/xtensa/platforms/iss/network.c
+++ arch/xtensa/platforms/iss/network.c
@@ -682,7 +682,7 @@ static int __init iss_net_setup(char *str)
 
 #undef ERR
 
-__setup("eth=", iss_net_setup);
+__setup("eth", iss_net_setup);
 
 /*
  * Initialize all ISS Ethernet devices previously registered in iss_net_setup.
