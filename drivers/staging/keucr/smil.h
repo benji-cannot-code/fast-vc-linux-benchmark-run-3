8f5af870a250f83300aec279ac26567352e014d4
Rashika Kheria <rashika.kheria@gmail.com>
2013-11-11 20:39:34 +0530

    Staging: keucr: Move the declaration of variable ErrCode in smilmain.c
    This patch moves the declaration of variable ErrCode to file smilmain.c
    since this is the only file which uses it. Hence, it also removes extern
    declaration from the header file smil.h.
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Reviewed-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/keucr/smil.h
+++ drivers/staging/keucr/smil.h
@@ -194,7 +194,6 @@ struct keucr_media_area {
 extern BYTE IsSSFDCCompliance;
 extern BYTE IsXDCompliance;
 
-extern DWORD	ErrCode;
 extern WORD	ReadBlock;
 extern WORD	WriteBlock;
 extern DWORD	MediaChange;
