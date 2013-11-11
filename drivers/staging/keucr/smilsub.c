57003d7786310e848ca3a2f38c874676294ef84e
Rashika Kheria <rashika.kheria@gmail.com>
2013-11-11 20:38:20 +0530

    Staging: keucr: Move the declaration of variable ErrXDCode in smilsub.c
    This patch moves the declaration of variable ErrXDCode to file smilsub.c
    since this is the only file which uses it. Hence, it also removes extern
    declaration from the header file smil.h and unnecessary declaration in
    smilmain.c
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Reviewed-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/keucr/smilsub.c
+++ drivers/staging/keucr/smilsub.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "smil.h"
 
 static BYTE   _Check_D_DevCode(BYTE);
+static DWORD	ErrXDCode;
 
 struct keucr_media_info         Ssfdc;
 struct keucr_media_address      Media;
