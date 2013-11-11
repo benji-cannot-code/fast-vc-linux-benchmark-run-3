7e3c3a45ede766ef11258e0b84ad920bd816fa26
Michal Marek <mmarek@suse.cz>
2013-11-11 14:23:08 +0100

    kallsyms: Revert back to 128 max symbol length
    This reverts commits
    f3462aa (Kbuild: Handle longer symbols in kallsyms.c) and
    eea0e9c (kbuild: Increase kallsyms max symbol length)
    except for the added overflow check. The reason is a regression caused
    by increasing the buffer:
    http://marc.info/?l=linux-kernel&m=138387700415675.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Joe Mario <jmario@redhat.com>
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kallsyms.c
+++ scripts/kallsyms.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 #endif
 
-#define KSYM_NAME_LEN		255
+#define KSYM_NAME_LEN		128
 
 struct sym_entry {
 	unsigned long long addr;
