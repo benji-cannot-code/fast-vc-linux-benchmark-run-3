a5511169018489a98f299816bfca0f887f75ea23
Ingo Molnar <mingo@kernel.org>
2013-11-11 19:53:42 +0100

    Revert "x86/UV: Add uvtrace support"
    This reverts commit 8eba18428ac926f436064ac281e76d36d51bd631.
    
    uv_trace() is not used by anything, nor is uv_trace_nmi_func, nor
    uv_trace_func.
    
    That's not how we do instrumentation code in the kernel: we add
    tracepoints, printk()s, etc. so that everyone not just those with
    magic kernel modules can debug a system.
    
    So remove this unused (and misguied) piece of code.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Mike Travis <travis@sgi.com>
    Cc: Dimitri Sivanich <sivanich@sgi.com>
    Cc: Hedi Berriche <hedi@sgi.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
    Cc: Jason Wessel <jason.wessel@windriver.com>
    Link: http://lkml.kernel.org/n/tip-tumfBffmr4jmnt8Gyxanoblg@git.kernel.org

--- arch/x86/platform/uv/uv_nmi.c
+++ arch/x86/platform/uv/uv_nmi.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * SGI NMI/TRACE support routines
+ * SGI NMI support routines
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -39,13 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uv/uv.h>
 #include <asm/uv/uv_hub.h>
 #include <asm/uv/uv_mmrs.h>
-
-void (*uv_trace_func)(const char *f, const int l, const char *fmt, ...);
-EXPORT_SYMBOL(uv_trace_func);
-
-void (*uv_trace_nmi_func)(unsigned int reason, struct pt_regs *regs);
-EXPORT_SYMBOL(uv_trace_nmi_func);
-
 
 /*
  * UV handler for NMI
@@ -599,10 +592,6 @@ int uv_handle_nmi(unsigned int reason, struct pt_regs *regs)
 		local_irq_restore(flags);
 		return NMI_DONE;
 	}
-
-	/* Call possible NMI trace function */
-	if (unlikely(uv_trace_nmi_func))
-		(uv_trace_nmi_func)(reason, regs);
 
 	/* Indicate we are the first CPU into the NMI handler */
 	master = (atomic_read(&uv_nmi_cpu) == cpu);
