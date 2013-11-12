c1696e5eda860712cb14b191721d5152602bd633
Johan Hovold <jhovold@gmail.com>
2013-11-12 15:09:38 -0800

    backlight: atmel-pwm-bl: fix reported brightness
    The driver supports 16-bit brightness values, but the value returned
    from get_brightness was truncated to eight bits.
    
    Signed-off-by: Johan Hovold <jhovold@gmail.com>
    Cc: Jingoo Han <jg1.han@samsung.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/backlight/atmel-pwm-bl.c
+++ drivers/video/backlight/atmel-pwm-bl.c
@@ -71,7 +71,7 @@ static int atmel_pwm_bl_set_intensity(struct backlight_device *bd)
 static int atmel_pwm_bl_get_intensity(struct backlight_device *bd)
 {
 	struct atmel_pwm_bl *pwmbl = bl_get_data(bd);
-	u8 intensity;
+	u32 intensity;
 
 	if (pwmbl->pdata->pwm_active_low) {
 		intensity = pwm_channel_readl(&pwmbl->pwmc, PWM_CDTY) -
@@ -81,7 +81,7 @@ static int atmel_pwm_bl_get_intensity(struct backlight_device *bd)
 			pwm_channel_readl(&pwmbl->pwmc, PWM_CDTY);
 	}
 
-	return intensity;
+	return intensity & 0xffff;
 }
 
 static int atmel_pwm_bl_init_pwm(struct atmel_pwm_bl *pwmbl)
