a173fa5e40e531cf5abc37d5d4996235414f5850
Geert Uytterhoeven <geert@linux-m68k.org>
2013-11-12 20:56:20 +0100

    openrisc: Use the declarations provided by <asm/sections.h>
    Openrisc's private vmlinux.h duplicates a few definitions that are already
    provided by asm-generic/sections.h. The former is used by setup.c only,
    while the latter is already used everywhere else.
    
    Convert setup.c to use the generic version:
      - Include <asm/sections.h>,
      - Remove the (slightly different) extern declarations,
      - Remove the no longer needed address-of ('&') operators.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Jonas Bonn <jonas@southpole.se>

--- arch/openrisc/kernel/setup.c
+++ arch/openrisc/kernel/setup.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/of_platform.h>
 
+#include <asm/sections.h>
 #include <asm/segment.h>
 #include <asm/pgtable.h>
 #include <asm/types.h>
@@ -78,7 +79,7 @@ static unsigned long __init setup_memory(void)
 
 	ram_start_pfn = PFN_UP(memory_start);
 	/* free_ram_start_pfn is first page after kernel */
-	free_ram_start_pfn = PFN_UP(__pa(&_end));
+	free_ram_start_pfn = PFN_UP(__pa(_end));
 	ram_end_pfn = PFN_DOWN(memblock_end_of_DRAM());
 
 	max_pfn = ram_end_pfn;
@@ -291,10 +292,10 @@ void __init setup_arch(char **cmdline_p)
 	setup_cpuinfo();
 
 	/* process 1's initial memory region is the kernel code/data */
-	init_mm.start_code = (unsigned long)&_stext;
-	init_mm.end_code = (unsigned long)&_etext;
-	init_mm.end_data = (unsigned long)&_edata;
-	init_mm.brk = (unsigned long)&_end;
+	init_mm.start_code = (unsigned long)_stext;
+	init_mm.end_code = (unsigned long)_etext;
+	init_mm.end_data = (unsigned long)_edata;
+	init_mm.brk = (unsigned long)_end;
 
 #ifdef CONFIG_BLK_DEV_INITRD
 	initrd_start = (unsigned long)&__initrd_start;
