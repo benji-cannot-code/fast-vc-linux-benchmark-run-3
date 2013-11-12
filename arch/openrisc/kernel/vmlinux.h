276bd5525d0074820fdd1b48a2af5c0da34e9ef3
Geert Uytterhoeven <geert@linux-m68k.org>
2013-11-12 20:56:21 +0100

    openrisc: Remove unused declaration of __initramfs_start
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Jonas Bonn <jonas@southpole.se>

--- arch/openrisc/kernel/vmlinux.h
+++ arch/openrisc/kernel/vmlinux.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_BLK_DEV_INITRD
 extern char __initrd_start, __initrd_end;
-extern char __initramfs_start;
 #endif
 
 extern u32 __dtb_start[];
