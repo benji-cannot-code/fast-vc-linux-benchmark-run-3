4645dc27f0760facdf87c1f1e8881d704a9bcec8
Michael Holzheu <holzheu@linux.vnet.ibm.com>
2013-11-13 10:38:27 +0100

    s390/sclp: Consolidate early sclp init calls to sclp_early_detect()
    The new function calls the old ones. The sclp_event_mask_early() is removed
    and replaced by one invocation of sclp_set_event_mask(0, 0).
    
    Reviewed-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Michael Holzheu <holzheu@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/char/sclp_early.c
+++ drivers/s390/char/sclp_early.c
@@ -94,20 +94,7 @@ static void __init sclp_read_info_early(void)
 	}
 }
 
-static void __init sclp_event_mask_early(void)
-{
-	struct init_sccb *sccb = &early_event_mask_sccb;
-	int rc;
-
-	do {
-		memset(sccb, 0, sizeof(*sccb));
-		sccb->header.length = sizeof(*sccb);
-		sccb->mask_length = sizeof(sccb_mask_t);
-		rc = sclp_cmd_sync_early(SCLP_CMDW_WRITE_EVENT_MASK, sccb);
-	} while (rc == -EBUSY);
-}
-
-void __init sclp_facilities_detect(void)
+static void __init sclp_facilities_detect(void)
 {
 	struct read_info_sccb *sccb;
 
@@ -123,8 +110,6 @@ void __init sclp_facilities_detect(void)
 	sclp_rnmax = sccb->rnmax ? sccb->rnmax : sccb->rnmax2;
 	sclp_rzm = sccb->rnsize ? sccb->rnsize : sccb->rnsize2;
 	sclp_rzm <<= 20;
-
-	sclp_event_mask_early();
 }
 
 bool __init sclp_has_linemode(void)
@@ -247,7 +232,7 @@ unsigned long sclp_get_hsa_size(void)
 	return sclp_hsa_size;
 }
 
-void __init sclp_hsa_size_detect(void)
+static void __init sclp_hsa_size_detect(void)
 {
 	long size;
 
@@ -269,6 +254,12 @@ void __init sclp_hsa_size_detect(void)
 	if (size < 0)
 		return;
 out:
-	sclp_set_event_mask(0, 0);
 	sclp_hsa_size = size;
+}
+
+void __init sclp_early_detect(void)
+{
+	sclp_facilities_detect();
+	sclp_hsa_size_detect();
+	sclp_set_event_mask(0, 0);
 }
