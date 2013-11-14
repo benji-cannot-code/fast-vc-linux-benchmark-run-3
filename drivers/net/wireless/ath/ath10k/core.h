d23892f9f8329f937cc253db97fed16262a9bca9
Bartosz Markowski <bartosz.markowski@tieto.com>
2013-11-14 09:01:15 +0100

    ath10k: expand the wmi mgmt queue limit to 128
    It was obseverd many times the short queue limit can be
    easily exceeded in case of AP multiple BSSID scenarios.
    
    "ath10k: wmi mgmt_tx queue limit reached."
    
    This was leading to TX packets drops.
    
    This patch expands the queue limit to 128. After many stress
    experiments (high traffic and lot of management frames in
    the air) the new value seems to fix the problem on platforms
    with limited resources.
    
    Reported-By: Tomasz Skapski <tomasz.skapski@tieto.com>
    Signed-off-by: Bartosz Markowski <bartosz.markowski@tieto.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Antenna noise floor */
 #define ATH10K_DEFAULT_NOISE_FLOOR -95
 
-#define ATH10K_MAX_NUM_MGMT_PENDING 16
+#define ATH10K_MAX_NUM_MGMT_PENDING 128
 
 struct ath10k;
 
