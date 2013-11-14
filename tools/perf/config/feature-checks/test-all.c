1c5d660f32d3403bba5579cb68a8732e4f1524ab
Ingo Molnar <mingo@kernel.org>
2013-11-14 08:25:24 +0100

    tools/perf/build: Fix feature-libunwind-debug-frame handling
    Set feature-libunwind-debug-frame. We don't want it in
    CORE_FEATURE_TESTS because it's not the generic case, but we
    need to set it in the !feature-libunwind case.
    
    Also, because x86 distributions typically don't have
    dwarf_find_debug_frame() unwinding method:
    
      test-libunwind-debug-frame.c:(.text+0x31): undefined reference to `_Ux86_64_dwarf_find_debug_frame'
    
    Restrict this new API to ARM for the time being.
    
    With this patch test-all.c works again, so repeat perf builds
    are  fast again:
    
      comet:~/tip> perf stat --null --repeat 5 make -C tools/perf/
      [...]
    
           0,452899660 seconds time elapsed                                          ( +-  0,11% )
    
    While with before it was:
    
      comet:~/tip> perf stat --null --repeat 5 make -C tools/perf/
      [...]
    
           1,674001829 seconds time elapsed                                          ( +-  0,16% )
    
    [ Includes fix to config/feature-checks/Makefile from Will Deacon. ]
    
    Tested-by: Will Deacon <will.deacon@arm.com>
    Tested-by: Jean Pihet <jean.pihet@linaro.org>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Arnaldo Carvalho de Melo <acme@infradead.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/n/tip-scsoctqzmou3rpkixCHezy9e@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/perf/config/feature-checks/test-all.c
+++ tools/perf/config/feature-checks/test-all.c
@@ -50,10 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # include "test-libunwind.c"
 #undef main
 
-#define main main_test_libunwind_debug_frame
-# include "test-libunwind-debug-frame.c"
-#undef main
-
 #define main main_test_libaudit
 # include "test-libaudit.c"
 #undef main
