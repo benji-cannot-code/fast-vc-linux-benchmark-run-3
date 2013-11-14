de7a3f1aead44bbfb7cf5a867bcb72aba85c7886
James Simmons <uja.ornl@gmail.com>
2013-11-15 00:13:16 +0800

    staging/lustre/autoconf: remove LIBCFS_HAVE_IS_COMPAT_TASK test
    is_compat_task has been defined on all arches since v2.6.29.
    We can remove the test and dead code.
    
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-2800
    Lustre-change: http://review.whamcloud.com/5393
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Signed-off-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Peng Tao <bergwolf@gmail.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_internal.h
+++ drivers/staging/lustre/lustre/llite/llite_internal.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <lclient.h>
 #include <lustre_mdc.h>
 #include <linux/lustre_intent.h>
+#include <linux/compat.h>
 
 #ifndef FMODE_EXEC
 #define FMODE_EXEC 0
@@ -644,7 +645,7 @@ static inline int ll_need_32bit_api(struct ll_sb_info *sbi)
 #if BITS_PER_LONG == 32
 	return 1;
 #else
-	return unlikely(current_is_32bit() || (sbi->ll_flags & LL_SBI_32BIT_API));
+	return unlikely(is_compat_task() || (sbi->ll_flags & LL_SBI_32BIT_API));
 #endif
 }
 
