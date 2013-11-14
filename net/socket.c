25f9758b91f9cc38987a4bece36b8b8dceba233e
Ben Hutchings <bhutchings@solarflare.com>
2013-11-14 01:19:29 +0000

    net_tstamp: Add SIOCGHWTSTAMP ioctl to match SIOCSHWTSTAMP
    SIOCSHWTSTAMP returns the real configuration to the application
    using it, but there is currently no way for any other
    application to find out the configuration non-destructively.
    Add a new ioctl for this, making it unprivileged.
    
    Signed-off-by: Ben Hutchings <bhutchings@solarflare.com>

--- net/socket.c
+++ net/socket.c
@@ -3232,6 +3232,7 @@ static int compat_sock_ioctl_trans(struct file *file, struct socket *sock,
 	case SIOCBONDSLAVEINFOQUERY:
 	case SIOCBONDINFOQUERY:
 	case SIOCSHWTSTAMP:
+	case SIOCGHWTSTAMP:
 		return compat_ifr_data_ioctl(net, cmd, argp);
 
 	case FIOSETOWN:
