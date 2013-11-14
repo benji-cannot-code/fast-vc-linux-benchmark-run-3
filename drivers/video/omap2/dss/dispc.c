ca98a4057e089992c3286c0c67c48131c94abae5
Tomi Valkeinen <tomi.valkeinen@ti.com>
2013-11-14 11:38:25 +0200

    OMAPDSS: DISPC: Add MFLAG defines
    OMAP5 has MFLAG feature in DISPC. Add the register definition and dump
    it. The register is not used yet, though.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -3214,6 +3214,8 @@ static void dispc_dump_regs(struct seq_file *s)
 		DUMPREG(DISPC_CONTROL3);
 		DUMPREG(DISPC_CONFIG3);
 	}
+	if (dss_has_feature(FEAT_MFLAG))
+		DUMPREG(DISPC_GLOBAL_MFLAG_ATTRIBUTE);
 
 #undef DUMPREG
 
@@ -3288,6 +3290,8 @@ static void dispc_dump_regs(struct seq_file *s)
 			DUMPREG(i, DISPC_OVL_ATTRIBUTES2);
 		if (dss_has_feature(FEAT_PRELOAD))
 			DUMPREG(i, DISPC_OVL_PRELOAD);
+		if (dss_has_feature(FEAT_MFLAG))
+			DUMPREG(i, DISPC_OVL_MFLAG_THRESHOLD);
 	}
 
 #undef DISPC_REG
