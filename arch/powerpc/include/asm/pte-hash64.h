8c71e29e93d20ce6febea075dffe883c6f5a398a
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2013-11-18 14:58:10 +0530

    powerpc/mm: Free up _PAGE_COHERENCE for numa fault use later
    Set  memory coherence always on hash64 config. If
    a platform cannot have memory coherence always set they
    can infer that from _PAGE_NO_CACHE and _PAGE_WRITETHRU
    like in lpar. So we dont' really need a separate bit
    for tracking _PAGE_COHERENCE.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Acked-by: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/pte-hash64.h
+++ arch/powerpc/include/asm/pte-hash64.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PAGE_FILE		0x0002 /* (!present only) software: pte holds file offset */
 #define _PAGE_EXEC		0x0004 /* No execute on POWER4 and newer (we invert) */
 #define _PAGE_GUARDED		0x0008
-#define _PAGE_COHERENT		0x0010 /* M: enforce memory coherence (SMP systems) */
+/* We can derive Memory coherence from _PAGE_NO_CACHE */
 #define _PAGE_NO_CACHE		0x0020 /* I: cache inhibit */
 #define _PAGE_WRITETHRU		0x0040 /* W: cache write-through */
 #define _PAGE_DIRTY		0x0080 /* C: page changed */
