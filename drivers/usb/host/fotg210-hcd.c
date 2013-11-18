1263a83751af3c288d6aa0a9a56615be88954395
Oliver Neukum <oneukum@suse.de>
2013-11-18 13:23:03 +0100

    fotg210: remove conditional compilation
    Always compile in the debugfs support
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/fotg210-hcd.c
+++ drivers/usb/host/fotg210-hcd.c
@@ -353,13 +353,6 @@ dbg_port_buf(char *buf, unsigned len, const char *label, int port, u32 status)
 
 /*-------------------------------------------------------------------------*/
 
-#ifdef STUB_DEBUG_FILES
-
-static inline void create_debug_files(struct fotg210_hcd *bus) { }
-static inline void remove_debug_files(struct fotg210_hcd *bus) { }
-
-#else
-
 /* troubleshooting help: expose state in debugfs */
 
 static int debug_async_open(struct inode *, struct file *);
@@ -955,7 +948,6 @@ static inline void remove_debug_files(struct fotg210_hcd *fotg210)
 	debugfs_remove_recursive(fotg210->debug_dir);
 }
 
-#endif /* STUB_DEBUG_FILES */
 /*-------------------------------------------------------------------------*/
 
 /*
