bdfd0684585fc926d989561aaff001801e4f05e4
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2013-11-18 14:50:18 +0530

    powerpc: booke: Fix build failures
    arch/powerpc/platforms/wsp/wsp.c: In function ‘wsp_probe_devices’:
    arch/powerpc/platforms/wsp/wsp.c:76:3: error: implicit declaration of function ‘of_address_to_resource’ [-Werror=implicit-function-declaration]
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/platforms/wsp/psr2.c
+++ arch/powerpc/platforms/wsp/psr2.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/smp.h>
 #include <linux/time.h>
+#include <linux/of_fdt.h>
 
 #include <asm/machdep.h>
 #include <asm/udbg.h>
