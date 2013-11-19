67e11ef3288470e98ae739f9d50220e7d9e02303
Amir Shehata <amir.shehata@intel.com>
2013-11-19 21:23:46 +0800

    staging/lustre/lnet: constify name argument of lstcon_group_find/lstcon_batch_find
    This is part of original Lustre commit in external tree.
    
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-3093
    Lustre-change: http://review.whamcloud.com/6092
    Signed-off-by: Amir Shehata <amir.shehata@intel.com>
    Reviewed-by: Isaac Huang <he.huang@intel.com>
    Reviewed-by: Liang Zhen <liang.zhen@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Peng Tao <bergwolf@gmail.com>
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/selftest/console.c
+++ drivers/staging/lustre/lnet/selftest/console.c
@@ -266,7 +266,7 @@ lstcon_group_decref(lstcon_group_t *grp)
 }
 
 static int
-lstcon_group_find(char *name, lstcon_group_t **grpp)
+lstcon_group_find(const char *name, lstcon_group_t **grpp)
 {
 	lstcon_group_t   *grp;
 
@@ -832,7 +832,7 @@ lstcon_group_info(char *name, lstcon_ndlist_ent_t *gents_p,
 }
 
 int
-lstcon_batch_find(char *name, lstcon_batch_t **batpp)
+lstcon_batch_find(const char *name, lstcon_batch_t **batpp)
 {
 	lstcon_batch_t   *bat;
 
