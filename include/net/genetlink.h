fe82f87020cbdf052f7ff65200ce4eb2a580d291
Johannes Berg <johannes.berg@intel.com>
2013-11-19 15:19:36 +0100

    genetlink: remove family pointer from genl_multicast_group
    There's no reason to have the family pointer there since it
    can just be passed internally where needed, so remove it.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/genetlink.h
+++ include/net/genetlink.h
@@ -14,10 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @id: multicast group ID, assigned by the core, to use with
  *      genlmsg_multicast().
  * @list: list entry for linking
- * @family: pointer to family, need not be set before registering
  */
 struct genl_multicast_group {
-	struct genl_family	*family;	/* private */
 	struct list_head	list;		/* private */
 	char			name[GENL_NAMSIZ];
 	u32			id;
