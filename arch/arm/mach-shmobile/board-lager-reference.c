a6358263d1642d7f71342b6e8c7b8195e63250fa
Magnus Damm <damm@opensource.se>
2013-11-20 09:02:31 +0900

    ARM: shmobile: Use ->init_late() on Lager
    Hook in shmobile_init_late() on Lager V2. This enables some PM
    related things like CPUIdle and Suspend-to-RAM.
    
    With this patch applied it is possible to use Suspend-to-RAM:
    # echo enabled > /sys/class/tty/ttySC6/power/wakeup
    # echo mem > /sys/power/state
    (wake by sending a character on the serial console)
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-lager-reference.c
+++ arch/arm/mach-shmobile/board-lager-reference.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/init.h>
 #include <linux/of_platform.h>
+#include <mach/common.h>
 #include <mach/rcar-gen2.h>
 #include <mach/r8a7790.h>
 #include <asm/mach/arch.h>
@@ -42,5 +43,6 @@ DT_MACHINE_START(LAGER_DT, "lager")
 	.init_early	= r8a7790_init_early,
 	.init_time	= rcar_gen2_timer_init,
 	.init_machine	= lager_add_standard_devices,
+	.init_late	= shmobile_init_late,
 	.dt_compat	= lager_boards_compat_dt,
 MACHINE_END
