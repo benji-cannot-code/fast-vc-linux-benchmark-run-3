51e63b8306f7a64eea728591cb40604d312c883d
Linus Walleij <linus.walleij@linaro.org>
2013-11-21 13:55:14 +0100

    ARM: lpc32xx: move custom GPIO header
    Move <mach/gpio-lpc32xx.h> to <linux/platform_data/gpio-lpc32xx.h>.
    
    Acked-by: Roland Stigge <stigge@antcom.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-lpc32xx.c
+++ drivers/gpio/gpio-lpc32xx.c
@@ -26,10 +26,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_gpio.h>
 #include <linux/platform_device.h>
 #include <linux/module.h>
+#include <linux/platform_data/gpio-lpc32xx.h>
 
 #include <mach/hardware.h>
 #include <mach/platform.h>
-#include <mach/gpio-lpc32xx.h>
 #include <mach/irqs.h>
 
 #define LPC32XX_GPIO_P3_INP_STATE		_GPREG(0x000)
