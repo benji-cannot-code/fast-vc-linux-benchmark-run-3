6d158c4be12f975f9defe27fa7cc5eca7e0daa1e
Peng Tao <bergwolf@gmail.com>
2013-11-21 22:28:24 +0800

    staging/lustre/libcfs: remove filp_read
    no users.
    
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Peng Tao <bergwolf@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/linux-fs.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/linux-fs.h
@@ -53,9 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/backing-dev.h>
 #include <linux/posix_acl_xattr.h>
 
-#define filp_read(fp, buf, size, pos)			\
-	((fp)->f_op->read((fp), (buf), (size), pos))
-
 #define filp_write(fp, buf, size, pos)			\
 	((fp)->f_op->write((fp), (buf), (size), pos))
 
