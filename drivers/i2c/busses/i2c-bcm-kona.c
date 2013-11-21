d59a71aeba706eb04c3f1324fb546b7ac24c370d
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-11-21 10:58:46 +0800

    i2c: bcm-kona: remove duplicated include
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Reviewed-by: Markus Mayer <markus.mayer@linaro.org>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-bcm-kona.c
+++ drivers/i2c/busses/i2c-bcm-kona.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/io.h>
-#include <linux/clk.h>
 #include <linux/slab.h>
 
 /* Hardware register offsets and field defintions */
