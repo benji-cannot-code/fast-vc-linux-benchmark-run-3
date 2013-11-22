702b5f7deb6a5f38728badbf4cbcb460a6f425b0
Eyal Shapira <eyal@wizery.com>
2013-11-22 09:41:38 +0200

    iwlwifi: mvm: rs: increase stay in column timeout
    Remain in the same Tx modulation (i.e. column) for a longer
    time before starting a search cycle for a better modulation.
    This has been shown to give better results.
    Also change the name of the timeout define to better match its
    description.
    
    Signed-off-by: Eyal Shapira <eyal@wizery.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/mvm/rs.c
+++ drivers/net/wireless/iwlwifi/mvm/rs.c
@@ -53,8 +53,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* max allowed rate miss before sync LQ cmd */
 #define IWL_MISSED_RATE_MAX		15
-/* max time to accum history 2 seconds */
-#define IWL_RATE_SCALE_FLUSH_INTVL   (3*HZ)
+#define RS_STAY_IN_COLUMN_TIMEOUT       (5*HZ)
+
 
 static u8 rs_ht_to_legacy[] = {
 	[IWL_RATE_1M_INDEX] = IWL_RATE_6M_INDEX,
@@ -1600,7 +1600,7 @@ static void rs_stay_in_table(struct iwl_lq_sta *lq_sta, bool force_search)
 			flush_interval_passed =
 				time_after(jiffies,
 					   (unsigned long)(lq_sta->flush_timer +
-						IWL_RATE_SCALE_FLUSH_INTVL));
+						RS_STAY_IN_COLUMN_TIMEOUT));
 
 		/*
 		 * Check if we should allow search for new modulation mode.
