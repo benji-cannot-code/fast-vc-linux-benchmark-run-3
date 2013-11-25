46fe3e9632e2e6616629434457b63d85684060e6
Daniel Mack <zonque@gmail.com>
2013-11-25 22:26:41 +0100

    usb: musb: call musb_port_suspend from musb_bus_suspend
    Make musb_port_suspend() externally available, and call it when to host
    goes into suspend. This allows the core to go into suspend while a
    device is connected.
    
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_virthub.c
+++ drivers/usb/musb/musb_virthub.c
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "musb_core.h"
 
-static void musb_port_suspend(struct musb *musb, bool do_suspend)
+void musb_port_suspend(struct musb *musb, bool do_suspend)
 {
 	struct usb_otg	*otg = musb->xceiv->otg;
 	u8		power;
