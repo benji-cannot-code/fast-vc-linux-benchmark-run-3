a1253a56e54fcad3406f1d21e61eff09d3536fbe
Tony Lindgren <tony@atomide.com>
2013-11-25 15:17:10 -0800

    ARM: OMAP2+: Add quirks support for n8x0
    This allows us to keep things working when booted with
    device tree. Note that we still need to initialize most
    things with platform data as the drivers are lacking
    support for device tree.
    
    Tested-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/pdata-quirks.c
+++ arch/arm/mach-omap2/pdata-quirks.c
@@ -27,6 +27,8 @@ struct pdata_init {
 	void (*fn)(void);
 };
 
+struct of_dev_auxdata omap_auxdata_lookup[];
+
 /*
  * Create alias for USB host PHY clock.
  * Remove this when clock phandle can be provided via DT
@@ -67,6 +69,15 @@ static inline void legacy_init_wl12xx(unsigned ref_clock,
 				      int gpio)
 {
 }
+#endif
+
+#ifdef CONFIG_MACH_NOKIA_N8X0
+static void __init omap2420_n8x0_legacy_init(void)
+{
+	omap_auxdata_lookup[0].platform_data = n8x0_legacy_init();
+}
+#else
+#define omap2420_n8x0_legacy_init	NULL
 #endif
 
 #ifdef CONFIG_ARCH_OMAP3
@@ -131,6 +142,11 @@ void omap_pcs_legacy_init(int irq, void (*rearm)(void))
  * the dev entries in of_platform_populate().
  */
 static struct pdata_init auxdata_quirks[] __initdata = {
+#ifdef CONFIG_SOC_OMAP2420
+	{ "nokia,n800", omap2420_n8x0_legacy_init, },
+	{ "nokia,n810", omap2420_n8x0_legacy_init, },
+	{ "nokia,n810-wimax", omap2420_n8x0_legacy_init, },
+#endif
 	{ /* sentinel */ },
 };
 
