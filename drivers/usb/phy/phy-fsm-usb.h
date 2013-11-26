1b11cd66a0f415f87aa9e0a3b5da8c21977ee6ff
Felipe Balbi <balbi@ti.com>
2013-11-26 10:06:00 -0600

    usb: phy: fsm: don't depend on indirect includes
    this header uses spinlocks and errno values, so
    we must include <linux/spinlock.h> and <linux/errno.h>
    to avoid build errors.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/phy/phy-fsm-usb.h
+++ drivers/usb/phy/phy-fsm-usb.h
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include <linux/spinlock.h>
+#include <linux/errno.h>
+
 #undef VERBOSE
 
 #ifdef VERBOSE
