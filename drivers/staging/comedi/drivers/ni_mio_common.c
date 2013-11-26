5227bb6aa568cdaddf7592735895bf46e3de6822
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-11-26 10:21:34 -0700

    staging: comedi: ni_mio_common: remove DEBUG_STATUS_A messages
    Defining DEBUG_STATUS_A enables dumping of a status register during the
    interrupt. These messages are just added noise. Remove them.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_mio_common.c
+++ drivers/staging/comedi/drivers/ni_mio_common.c
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 
 /* #define DEBUG_INTERRUPT */
-/* #define DEBUG_STATUS_A */
 
 #include <linux/interrupt.h>
 #include <linux/sched.h>
@@ -261,12 +260,6 @@ static int ni_rtsi_insn_config(struct comedi_device *dev,
 
 static void caldac_setup(struct comedi_device *dev, struct comedi_subdevice *s);
 static int ni_read_eeprom(struct comedi_device *dev, int addr);
-
-#ifdef DEBUG_STATUS_A
-static void ni_mio_print_status_a(int status);
-#else
-#define ni_mio_print_status_a(a)
-#endif
 
 static int ni_ai_reset(struct comedi_device *dev, struct comedi_subdevice *s);
 #ifndef PCIDMA
@@ -1041,7 +1034,6 @@ static void handle_a_interrupt(struct comedi_device *dev, unsigned short status,
 	printk
 	    ("ni_mio_common: interrupt: a_status=%04x ai_mite_status=%08x\n",
 	     status, ai_mite_status);
-	ni_mio_print_status_a(status);
 #endif
 #ifdef PCIDMA
 	if (ai_mite_status & CHSR_LINKC) {
@@ -1078,7 +1070,6 @@ static void handle_a_interrupt(struct comedi_device *dev, unsigned short status,
 			      AI_SC_TC_Error_St)) {
 			printk("ni_mio_common: ai error a_status=%04x\n",
 			       status);
-			ni_mio_print_status_a(status);
 
 			shutdown_ai_command(dev);
 
@@ -1218,28 +1209,6 @@ static void handle_b_interrupt(struct comedi_device *dev,
 
 	ni_event(dev, s);
 }
-
-#ifdef DEBUG_STATUS_A
-static const char *const status_a_strings[] = {
-	"passthru0", "fifo", "G0_gate", "G0_TC",
-	"stop", "start", "sc_tc", "start1",
-	"start2", "sc_tc_error", "overflow", "overrun",
-	"fifo_empty", "fifo_half_full", "fifo_full", "interrupt_a"
-};
-
-static void ni_mio_print_status_a(int status)
-{
-	int i;
-
-	printk("A status:");
-	for (i = 15; i >= 0; i--) {
-		if (status & (1 << i)) {
-			printk(" %s", status_a_strings[i]);
-		}
-	}
-	printk("\n");
-}
-#endif
 
 #ifndef PCIDMA
 
