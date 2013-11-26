cbb48d4fee1de4bd5ee3b30f5caa2973808cf61b
Richard Guy Briggs <rgb@redhat.com>
2013-11-26 18:01:36 -0500

    audit: fix incorrect type of sessionid
    The type of task->sessionid is unsigned int, the return
    type of audit_get_sessionid should be consistent with it.
    
    Signed-off-by: Gao feng <gaofeng@cn.fujitsu.com>
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- include/linux/audit.h
+++ include/linux/audit.h
@@ -203,7 +203,7 @@ static inline kuid_t audit_get_loginuid(struct task_struct *tsk)
 	return tsk->loginuid;
 }
 
-static inline int audit_get_sessionid(struct task_struct *tsk)
+static inline unsigned int audit_get_sessionid(struct task_struct *tsk)
 {
 	return tsk->sessionid;
 }
@@ -360,7 +360,7 @@ static inline kuid_t audit_get_loginuid(struct task_struct *tsk)
 {
 	return INVALID_UID;
 }
-static inline int audit_get_sessionid(struct task_struct *tsk)
+static inline unsigned int audit_get_sessionid(struct task_struct *tsk)
 {
 	return -1;
 }
