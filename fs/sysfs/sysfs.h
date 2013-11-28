2870d612e3cc760139ea437f8e333c0005537dd1
Tejun Heo <tj@kernel.org>
2013-11-28 14:54:37 -0500

    sysfs, kernfs: make sysfs_super_info->ns const
    Add const qualifier to sysfs_super_info->ns so that it's consistent
    with other namespace tag usages in sysfs.  Because kobject doesn't use
    const qualifier for namespace tags, this ends up requiring an explicit
    cast to drop const qualifier in free_sysfs_super_info().
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/sysfs/sysfs.h
+++ fs/sysfs/sysfs.h
@@ -19,14 +19,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * mount.c
  */
 
-/*
- * Each sb is associated with one namespace tag, currently the network
- * namespace of the task which mounted this sysfs instance.  If multiple
- * tags become necessary, make the following an array and compare
- * sysfs_dirent tag against every entry.
- */
 struct sysfs_super_info {
-	void *ns;
+	/*
+	 * Each sb is associated with one namespace tag, currently the network
+	 * namespace of the task which mounted this sysfs instance.  If multiple
+	 * tags become necessary, make the following an array and compare
+	 * sysfs_dirent tag against every entry.
+	 */
+	const void		*ns;
 };
 #define sysfs_info(SB) ((struct sysfs_super_info *)(SB->s_fs_info))
 extern struct sysfs_dirent sysfs_root;
