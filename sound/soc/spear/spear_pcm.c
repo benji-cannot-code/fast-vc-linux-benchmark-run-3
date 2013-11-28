6236b5217a9ba7122e83da93f656854bca754d02
Lars-Peter Clausen <lars@metafoo.de>
2013-11-28 08:50:36 +0100

    ASoC: spear: Use devm_snd_dmaengine_pcm_register
    Makes the code slightly shorter.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Rajeev Kumar <rajeev-dlh.kumar@st.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/spear/spear_pcm.c
+++ sound/soc/spear/spear_pcm.c
@@ -50,16 +50,10 @@ static const struct snd_dmaengine_pcm_config spear_dmaengine_pcm_config = {
 
 static int spear_soc_platform_probe(struct platform_device *pdev)
 {
-	return snd_dmaengine_pcm_register(&pdev->dev,
+	return devm_snd_dmaengine_pcm_register(&pdev->dev,
 		&spear_dmaengine_pcm_config,
 		SND_DMAENGINE_PCM_FLAG_NO_DT |
 		SND_DMAENGINE_PCM_FLAG_COMPAT);
-}
-
-static int spear_soc_platform_remove(struct platform_device *pdev)
-{
-	snd_dmaengine_pcm_unregister(&pdev->dev);
-	return 0;
 }
 
 static struct platform_driver spear_pcm_driver = {
@@ -69,7 +63,6 @@ static struct platform_driver spear_pcm_driver = {
 	},
 
 	.probe = spear_soc_platform_probe,
-	.remove = spear_soc_platform_remove,
 };
 
 module_platform_driver(spear_pcm_driver);
