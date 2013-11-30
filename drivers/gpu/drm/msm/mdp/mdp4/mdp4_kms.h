a5c9842c67a67707b104ec6edd6f385a5db7813c
Rob Clark <robdclark@gmail.com>
2013-11-30 16:12:10 -0500

    drm/msm: split out msm_kms.h
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/mdp/mdp4/mdp4_kms.h
+++ drivers/gpu/drm/msm/mdp/mdp4/mdp4_kms.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __MDP4_KMS_H__
 
 #include "msm_drv.h"
+#include "msm_kms.h"
 #include "mdp/mdp_kms.h"
 #include "mdp4.xml.h"
 
