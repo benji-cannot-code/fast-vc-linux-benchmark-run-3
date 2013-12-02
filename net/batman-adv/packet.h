a71da2db90c1f288e2acb0b9d5d065b1243a1f1c
Simon Wunderlich <sw@simonwunderlich.de>
2013-12-02 20:38:30 +0100

    batman-adv: fix alignment for batadv_coded_packet
    The compiler may decide to pad the structure, and then it does not
    have the expected size of 46 byte. Fix this by moving it in the
    pragma pack(2) part of the code.
    
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
    Signed-off-by: Antonio Quartulli <antonio@meshcoding.com>

--- net/batman-adv/packet.h
+++ net/batman-adv/packet.h
@@ -316,8 +316,6 @@ struct batadv_bcast_packet {
 	 */
 };
 
-#pragma pack()
-
 /**
  * struct batadv_coded_packet - network coded packet
  * @header: common batman packet header and ttl of first included packet
@@ -349,6 +347,8 @@ struct batadv_coded_packet {
 	__be32   second_crc;
 	__be16   coded_len;
 };
+
+#pragma pack()
 
 /**
  * struct batadv_unicast_tvlv - generic unicast packet with tvlv payload
