99041e9096988290049d75815a8edf7814aa6654
Eran Harary <eran.harary@intel.com>
2013-12-02 12:18:10 +0200

    iwlwifi: pcie: change CSR reset in family 8000
    This register is not present in 8000 family devices.
    There is prph register instead.
    
    Signed-off-by: Eran Harary <eran.harary@intel.com>
    Reviewed-by: Dor Shaish <dor.shaish@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-prph.h
+++ drivers/net/wireless/iwlwifi/iwl-prph.h
@@ -106,6 +106,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Device NMI register */
 #define DEVICE_SET_NMI_REG 0x00a01c30
 
+/*
+ * Device reset for family 8000
+ * write to bit 24 in order to reset the CPU
+*/
+#define RELEASE_CPU_RESET		(0x300C)
+#define RELEASE_CPU_RESET_BIT		BIT(24)
+
 /*****************************************************************************
  *                        7000/3000 series SHR DTS addresses                 *
  *****************************************************************************/
