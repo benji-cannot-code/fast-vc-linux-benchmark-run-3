5d08b9e26cbb2e4e927674ec5c1a9bb1bd8a7ab8
Chao Yu <chao2.yu@samsung.com>
2013-12-05 09:54:56 +0800

    f2fs: avoid unneeded page release for correct _count of page
    In find_fsync_dnodes() and recover_data(), our flow is like this:
    
    ->f2fs_submit_page_bio()
    	-> f2fs_put_page()
    		-> page_cache_release()	---- page->_count declined to zero.
    ->__free_pages()
    	-> put_page_testzero() ---- page->_count will be declined again.
    
    We will get a segment fault in put_page_testzero when CONFIG_DEBUG_VM
    is on, or return MM with a bad page with wrong _count num.
    
    So let's just release this page.
    
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -146,7 +146,7 @@ static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head)
 
 		err = f2fs_submit_page_bio(sbi, page, blkaddr, READ_SYNC);
 		if (err)
-			goto out;
+			return err;
 
 		lock_page(page);
 
@@ -192,9 +192,10 @@ next:
 		/* check next segment */
 		blkaddr = next_blkaddr_of_node(page);
 	}
+
 	unlock_page(page);
-out:
 	__free_pages(page, 0);
+
 	return err;
 }
 
@@ -389,7 +390,7 @@ static int recover_data(struct f2fs_sb_info *sbi,
 
 		err = f2fs_submit_page_bio(sbi, page, blkaddr, READ_SYNC);
 		if (err)
-			goto out;
+			return err;
 
 		lock_page(page);
 
@@ -413,8 +414,8 @@ next:
 		/* check next segment */
 		blkaddr = next_blkaddr_of_node(page);
 	}
+
 	unlock_page(page);
-out:
 	__free_pages(page, 0);
 
 	if (!err)
