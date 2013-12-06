3b29f9ff8502f09ca07d14592bede3749c5419ff
Qais Yousef <qais.yousef@imgtec.com>
2013-12-06 11:00:45 +0000

    MIPS: sead3: use unflatten_and_copy_device_tree()
    we want the device tree to be unflattened into non init memory so it can be
    accessed later by, for example, a probing function of a driver module.
    
    Signed-off-by: Qais Yousef <qais.yousef@imgtec.com>
    Reviewed-by: Paul Burton <paul.burton@imgtec.com>
    Reviewed-by: James Hogan <james.hogan@imgtec.com>
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/6210/

--- arch/mips/mti-sead3/sead3-setup.c
+++ arch/mips/mti-sead3/sead3-setup.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/libfdt.h>
 #include <linux/of_platform.h>
 #include <linux/of_fdt.h>
-#include <linux/bootmem.h>
 
 #include <asm/prom.h>
 #include <asm/fw/fw.h>
@@ -99,18 +98,10 @@ void __init plat_mem_setup(void)
 
 void __init device_tree_init(void)
 {
-	unsigned long base, size;
-
 	if (!initial_boot_params)
 		return;
 
-	base = virt_to_phys((void *)initial_boot_params);
-	size = be32_to_cpu(initial_boot_params->totalsize);
-
-	/* Before we do anything, lets reserve the dt blob */
-	reserve_bootmem(base, size, BOOTMEM_DEFAULT);
-
-	unflatten_device_tree();
+	unflatten_and_copy_device_tree();
 }
 
 static int __init customize_machine(void)
