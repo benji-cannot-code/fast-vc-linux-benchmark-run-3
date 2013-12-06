04bceb5a65ddef37ec109f31b008ce6065ac0766
Lv Zheng <lv.zheng@intel.com>
2013-12-06 16:51:59 +0800

    SFI / ACPI: Fix warnings reported during builds with W=1
    The following warnings can be seen in W=1 builds, because the original
    sfi_acpi.[ch] header inclusions are incorrect:
    
    include/linux/sfi_acpi.h:72:2: error: implicit declaration of function 'acpi_table_parse' [-Werror=implicit-function-declaration]
    drivers/sfi/sfi_acpi.c:154:5: warning: no previous prototype for 'sfi_acpi_table_parse' [-Wmissing-prototypes]
    
    Fix linux/sfi_acpi.h and modify drivers/sfi/sfi_acpi.c accordingly.
    
    Reported-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    [rjw: Subject and changelog]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/sfi_acpi.h
+++ include/linux/sfi_acpi.h
@@ -60,6 +60,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SFI_ACPI_H
 #define _LINUX_SFI_ACPI_H
 
+#include <linux/acpi.h>
+#include <linux/sfi.h>
+
 #ifdef CONFIG_SFI
 #include <acpi/acpi.h>	/* FIXME: inclusion should be removed */
 
