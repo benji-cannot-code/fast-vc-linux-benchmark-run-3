49c578ada221b8e475c277d917cb0ea49e2f700a
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2013-12-05 18:09:30 -0800

    ARM: shmobile: r8a7778: camera-rcar header cleanup
    <linux/platform_data/camera-rcar.h> is needed on BockW,
    not setup-r8a7778.c
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/r8a7778.h
+++ arch/arm/mach-shmobile/include/mach/r8a7778.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_R8A7778_H__
 
 #include <linux/sh_eth.h>
-#include <linux/platform_data/camera-rcar.h>
 
 /* HPB-DMA slave IDs */
 enum {
