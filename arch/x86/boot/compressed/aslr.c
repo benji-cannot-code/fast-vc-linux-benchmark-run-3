a1ba4c6f8bfdb024f49b84f3993f239016f8780b
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-12-07 21:02:36 +0800

    x86, kaslr: Remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Link: http://lkml.kernel.org/r/CAPgLHd-Fjx1RybjWFAu1vHRfTvhWwMLL3x46BouC5uNxHPjy1A@mail.gmail.com
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/boot/compressed/aslr.c
+++ arch/x86/boot/compressed/aslr.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uts.h>
 #include <linux/utsname.h>
 #include <generated/utsrelease.h>
-#include <linux/version.h>
 
 /* Simplified build-specific string for starting entropy. */
 static const char build_str[] = UTS_RELEASE " (" LINUX_COMPILE_BY "@"
