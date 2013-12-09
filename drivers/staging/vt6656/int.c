efecc4b8c834b14206fb48764f076bbeddad4eeb
Malcolm Priestley <tvboxspy@gmail.com>
2013-12-09 22:30:14 +0000

    staging: vt6656: Replace pStatistic->abyTxPktInfo with struct vnt_tx_pkt_info
    Change s_vSaveTxPktInfo and BSSvUpdateNodeTxCounter to use vnt_tx_pkt_info
    relayed to BSSvUpdateNodeTxCounter via INTnsProcessData.
    
    pStatistic->abyTxPktInfo[byPktNum].byBroadMultiUni is unused and discarded.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/int.c
+++ drivers/staging/vt6656/int.c
@@ -93,7 +93,6 @@ void INTnsProcessData(struct vnt_private *pDevice)
 			pStats->tx_packets++;
 
 		BSSvUpdateNodeTxCounter(pDevice,
-					&(pDevice->scStatistic),
 					pINTData->byTSR0,
 					pINTData->byPkt0);
 		/*DBG_PRN_GRP01(("TSR0 %02x\n", pINTData->byTSR0));*/
@@ -106,7 +105,6 @@ void INTnsProcessData(struct vnt_private *pDevice)
 
 
 		BSSvUpdateNodeTxCounter(pDevice,
-					&(pDevice->scStatistic),
 					pINTData->byTSR1,
 					pINTData->byPkt1);
 		/*DBG_PRN_GRP01(("TSR1 %02x\n", pINTData->byTSR1));*/
@@ -118,7 +116,6 @@ void INTnsProcessData(struct vnt_private *pDevice)
 			pStats->tx_packets++;
 
 		BSSvUpdateNodeTxCounter(pDevice,
-					&(pDevice->scStatistic),
 					pINTData->byTSR2,
 					pINTData->byPkt2);
 		/*DBG_PRN_GRP01(("TSR2 %02x\n", pINTData->byTSR2));*/
@@ -130,7 +127,6 @@ void INTnsProcessData(struct vnt_private *pDevice)
 			pStats->tx_packets++;
 
 		BSSvUpdateNodeTxCounter(pDevice,
-					&(pDevice->scStatistic),
 					pINTData->byTSR3,
 					pINTData->byPkt3);
 		/*DBG_PRN_GRP01(("TSR3 %02x\n", pINTData->byTSR3));*/
