7e808fb57b20e5243752059821f4a55397c79790
Kinglong Mee <kinglongmee@gmail.com>
2013-12-09 19:04:23 +0800

    nfsd: using nfsd4_encode_noop for encoding destroy_session/free_stateid
    Get rid of the extra code, using nfsd4_encode_noop for encoding destroy_session and free_stateid.
    And, delete unused argument (fr_status) int nfsd4_free_stateid.
    
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/xdr4.h
+++ fs/nfsd/xdr4.h
@@ -375,7 +375,6 @@ struct nfsd4_test_stateid {
 
 struct nfsd4_free_stateid {
 	stateid_t	fr_stateid;         /* request */
-	__be32		fr_status;          /* response */
 };
 
 /* also used for NVERIFY */
