387ef5cb5dfff7f9285f29ad41414dc40cb476cc
Qais Yousef <qais.yousef@imgtec.com>
2013-12-09 09:49:45 +0000

    MIPS: include linux/types.h
    The file uses u16 type but doesn't include its definition explicitly
    
    I was getting this error when including this header in my driver:
    
      arch/mips/include/asm/mipsregs.h:644:33: error: unknown type name ‘u16’
    
    Signed-off-by: Qais Yousef <qais.yousef@imgtec.com>
    Reviewed-by: Steven J. Hill <Steven.Hill@imgtec.com>
    Acked-by: David Daney <david.daney@cavium.com>
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/6212/

--- arch/mips/include/asm/mipsregs.h
+++ arch/mips/include/asm/mipsregs.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_MIPSREGS_H
 
 #include <linux/linkage.h>
+#include <linux/types.h>
 #include <asm/hazards.h>
 #include <asm/war.h>
 
