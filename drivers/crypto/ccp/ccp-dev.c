480b61426b914ae3ae05549633ce38f4adc2278a
Fengguang Wu <fengguang.wu@intel.com>
2013-12-09 20:08:19 +0800

    crytpo: ccp - fix coccinelle warnings
    drivers/crypto/ccp/ccp-crypto-aes.c:344:1-7: Replace memcpy with struct assignment
    drivers/crypto/ccp/ccp-crypto-sha.c:398:1-7: Replace memcpy with struct assignment
    drivers/crypto/ccp/ccp-dev.c:578:2-3: Unneeded semicolon
    /c/kernel-tests/src/cocci/drivers/crypto/ccp/ccp-dev.c:565:2-3: Unneeded semicolon
    
    Generated by: coccinelle/misc/memcpy-assign.cocci
    
    CC: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccp/ccp-dev.c
+++ drivers/crypto/ccp/ccp-dev.c
@@ -563,7 +563,7 @@ static int __init ccp_mod_init(void)
 			return -ENODEV;
 		return ccp_pci_init();
 		break;
-	};
+	}
 
 	return -ENODEV;
 }
@@ -576,7 +576,7 @@ static void __exit ccp_mod_exit(void)
 	case 22:
 		ccp_pci_exit();
 		break;
-	};
+	}
 }
 
 module_init(ccp_mod_init);
