3c7b2353ae618a08939971e9248a5c07a72338fa
Adrian Hunter <adrian.hunter@intel.com>
2013-12-09 15:18:39 +0200

    perf evlist: Fix mmap pages rounding to power of 2
    'next_pow2()' only works for 'unsigned int' but the argument is
    'unsigned long'.  Checking for values less than (1 << 31) ensures that
    'next_pow2()' is not passed a value out of range but lets anything else
    go through unvalidated.
    
    As a result mmap_pages of zero is used e.g.
    
      perf record -v -m2147483649 uname
      mmap size 0B
      failed to mmap with 22 (Invalid argument)
    
    Fixed:
    
      perf record -m2147483649 uname
      rounding mmap pages size to 17592186044416 bytes (4294967296 pages)
      Invalid argument for --mmap_pages/-m
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Namhyung Kim <namhyung@gmail.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/r/1386595120-22978-4-git-send-email-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/ttydefaults.h>
 #include <lk/debugfs.h>
 #include <termios.h>
+#include <linux/bitops.h>
 
 extern const char *graph_line;
 extern const char *graph_dotted_line;
@@ -280,6 +281,17 @@ static inline unsigned next_pow2(unsigned x)
 	if (!x)
 		return 1;
 	return 1ULL << (32 - __builtin_clz(x - 1));
+}
+
+static inline unsigned long next_pow2_l(unsigned long x)
+{
+#if BITS_PER_LONG == 64
+	if (x <= (1UL << 31))
+		return next_pow2(x);
+	return (unsigned long)next_pow2(x >> 32) << 32;
+#else
+	return next_pow2(x);
+#endif
 }
 
 size_t hex_width(u64 v);
