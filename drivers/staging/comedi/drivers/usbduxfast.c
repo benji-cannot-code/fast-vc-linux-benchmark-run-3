affb33a032238f39fc7fbab26870a24446ca7f9b
H Hartley Sweeten <hsweeten@visionengravers.com>
2013-12-09 17:31:25 -0700

    staging: comedi: usbduxfast: tidy up comedi_lrange tables
    Tidy up the whitespace in the comedi_lrange tables.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/usbduxfast.c
+++ drivers/staging/comedi/drivers/usbduxfast.c
@@ -139,7 +139,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * comedi constants
  */
 static const struct comedi_lrange range_usbduxfast_ai_range = {
-	2, {BIP_RANGE(0.75), BIP_RANGE(0.5)}
+	2, {
+		BIP_RANGE(0.75),
+		BIP_RANGE(0.5)
+	}
 };
 
 /*
