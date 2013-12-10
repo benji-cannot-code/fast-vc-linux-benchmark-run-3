2081aedc736ce1e053502381b655a6bd5dab52d0
Jingoo Han <jg1.han@samsung.com>
2013-12-10 12:50:42 +0900

    net: phy: spi_ks8995: remove unnecessary spi_set_drvdata()
    The driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/spi_ks8995.c
+++ drivers/net/phy/spi_ks8995.c
@@ -326,7 +326,6 @@ static int ks8995_probe(struct spi_device *spi)
 	return 0;
 
 err_drvdata:
-	spi_set_drvdata(spi, NULL);
 	kfree(ks);
 	return err;
 }
@@ -338,7 +337,6 @@ static int ks8995_remove(struct spi_device *spi)
 	ks8995 = spi_get_drvdata(spi);
 	sysfs_remove_bin_file(&spi->dev.kobj, &ks8995_registers_attr);
 
-	spi_set_drvdata(spi, NULL);
 	kfree(ks8995);
 
 	return 0;
