91de7b75807d85f961e3d3b52ef0d29a9a0e3bad
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-12-11 15:13:59 +0100

    ARM: shmobile: Let Koelsch multiplatform boot with Koelsch DTB
    Let the multiplatform Koelsch support boot with the legacy DTS for
    Koelsch as well as the Koelsch reference DTS.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-koelsch-reference.c
+++ arch/arm/mach-shmobile/board-koelsch-reference.c
@@ -65,6 +65,7 @@ static void __init koelsch_add_standard_devices(void)
 }
 
 static const char * const koelsch_boards_compat_dt[] __initconst = {
+	"renesas,koelsch",
 	"renesas,koelsch-reference",
 	NULL,
 };
