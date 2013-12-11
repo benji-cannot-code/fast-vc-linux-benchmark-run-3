766d773537e2ea7b34521c6cbb7fe03ea7da88da
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-12-11 11:34:31 +0100

    drm: restrict the device list for shadow attached drivers
    There's really no need for the drm core to keep a list of all
    devices of a given driver - the linux device model keeps perfect
    track of this already for us.
    
    The exception is old legacy ums drivers using pci shadow attaching.
    So rename the lists to make the use case clearer and rip out everything
    else.
    
    v2: Rebase on top of David Herrmann's drm device register changes.
    Also drop the bogus dev_set_drvdata for platform drivers that somehow
    crept into the original version - drivers really should be in full
    control of that field.
    
    v3: Initialize driver->legacy_dev_list outside of the loop, spotted by
    David Herrmann.
    
    v4: Rebase on top of the newly created host1x drm_bus for tegra.
    
    Cc: David Herrmann <dh.herrmann@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1013,8 +1013,8 @@ struct drm_driver {
 	} kdriver;
 	struct drm_bus *bus;
 
-	/* List of devices hanging off this driver */
-	struct list_head device_list;
+	/* List of devices hanging off this driver with stealth attach. */
+	struct list_head legacy_dev_list;
 };
 
 #define DRM_MINOR_UNASSIGNED 0
@@ -1103,7 +1103,7 @@ struct drm_vblank_crtc {
  * may contain multiple heads.
  */
 struct drm_device {
-	struct list_head driver_item;	/**< list of devices per driver */
+	struct list_head legacy_dev_list;/**< list of devices per driver for stealth attach cleanup */
 	char *devname;			/**< For /proc/interrupts */
 	int if_version;			/**< Highest interface version set */
 
