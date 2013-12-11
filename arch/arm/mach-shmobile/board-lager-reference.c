36988190de55fec00dfbcb9da76ea2f44d4ef664
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2013-12-11 15:13:52 +0100

    ARM: shmobile: lager-reference: Enable multiplaform kernel support
    Enable multiplaform ARM architecture support for the Lager reference
    board. Common clock framework initialization will be handled by the
    rcar_gen2_init_timer() call, we just need to remove the legacy clock
    code initialization.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-lager-reference.c
+++ arch/arm/mach-shmobile/board-lager-reference.c
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void __init lager_add_standard_devices(void)
 {
+#ifndef CONFIG_COMMON_CLK
 	r8a7790_clock_init();
+#endif
 	r8a7790_add_dt_devices();
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 }
