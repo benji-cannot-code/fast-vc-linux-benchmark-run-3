eb6e955a1e0abe3906602e9a22ab6f5bbfd60902
Hans Verkuil <hans.verkuil@cisco.com>
2013-12-12 09:04:44 -0300

    [media] omap24xx/tcm825x: move to staging for future removal
    The omap24xx driver and the tcm825x sensor driver are the only two
    remaining drivers to still use the old deprecated v4l2-int-device API.
    
    Nobody maintains these drivers anymore. But unfortunately the v4l2-int-device
    API is used by out-of-tree drivers (MXC platform). This is a very bad situation
    since as long as this deprecated API stays in the kernel there is no reason for
    those out-of-tree drivers to convert.
    
    This patch moves v4l2-int-device and the two drivers that depend on it to
    staging in preparation for their removal.
    
    If someone would be interested in getting these drivers to work, then start with
    this since it's not very far from the state where they used to work:
    
    <URL:http://vihersipuli.retiisi.org.uk/cgi-bin/gitweb.cgi?p=~sailus/linux-omap/.git;a=summary>
    
    The branch is n800-cam. Porting to up-to-date APIs can then be done. David
    might have done some work in that area, so check with him first.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Cc: Sakari Ailus <sakari.ailus@iki.fi>
    Cc: David Cohen <dacohen@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/staging/media/omap24xx/omap24xxcam.h
+++ drivers/staging/media/omap24xx/omap24xxcam.h
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP24XXCAM_H
 
 #include <media/videobuf-dma-sg.h>
-#include <media/v4l2-int-device.h>
 #include <media/v4l2-device.h>
+#include "v4l2-int-device.h"
 
 /*
  *
--- drivers/staging/media/omap24xx/tcm825x.c
+++ drivers/staging/media/omap24xx/tcm825x.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/i2c.h>
 #include <linux/module.h>
-#include <media/v4l2-int-device.h>
+#include "v4l2-int-device.h"
 
 #include "tcm825x.h"
 
