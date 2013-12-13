9db3c341deab9ff6148b3376e9a3d2131bd3a073
Ben Widawsky <benjamin.widawsky@intel.com>
2013-12-12 17:26:03 -0800

    drm/i915/bdw: Force all Data Cache Data Port access to be Non-Coherent
    I stumbled on to some unimplemented errata. To be honest, I am not
    really sure of the impact, just that the docs say to do.
    
    No w/a name for this one.
    
    v2: v1 was a stale thing which should have never seen the light of day.
    (Haihao)
    
    Cc: Kenneth Graunke <kenneth@whitecape.org>
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -4168,6 +4168,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GEN7_L3SQCREG4				0xb034
 #define  L3SQ_URB_READ_CAM_MATCH_DISABLE	(1<<27)
 
+/* GEN8 chicken */
+#define HDC_CHICKEN0				0x7300
+#define  HDC_FORCE_NON_COHERENT			(1<<4)
+
 /* WaCatErrorRejectionIssue */
 #define GEN7_SQ_CHICKEN_MBCUNIT_CONFIG		0x9030
 #define  GEN7_SQ_CHICKEN_MBCUNIT_SQINTMOB	(1<<11)
