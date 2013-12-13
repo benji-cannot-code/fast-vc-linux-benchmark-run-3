211cf6d21cd23548c25adf015e11fb496e9f7a20
Colin Cross <ccross@android.com>
2013-12-13 14:24:57 -0800

    ion: move userspace api into uapi/ion.h
    Split the userspace api out of drivers/staging/android/ion/ion.h
    into drivers/staging/android/uapi/ion.h
    
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/compat_ion.c
+++ drivers/staging/android/ion/compat_ion.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ion.h"
 #include "compat_ion.h"
 
-/* See drivers/staging/android/ion/ion.h for the definition of these structs */
+/* See drivers/staging/android/uapi/ion.h for the definition of these structs */
 struct compat_ion_allocation_data {
 	compat_size_t len;
 	compat_size_t align;
