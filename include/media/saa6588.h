59d7903d62f0a903d9ce14d49be5e099be70d7c6
Hans Verkuil <hans.verkuil@cisco.com>
2013-12-14 08:28:36 -0300

    [media] saa6588: add support for non-blocking mode
    saa6588 always blocked while waiting for data, even if the filehandle
    was in non-blocking mode.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- include/media/saa6588.h
+++ include/media/saa6588.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct saa6588_command {
 	unsigned int  block_count;
+	bool          nonblocking;
 	int           result;
 	unsigned char __user *buffer;
 	struct file   *instance;
