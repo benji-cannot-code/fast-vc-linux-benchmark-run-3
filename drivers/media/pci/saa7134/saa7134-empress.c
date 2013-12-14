bcb13aaef24be53012bfafe99812c139bb279d31
Hans Verkuil <hans.verkuil@cisco.com>
2013-12-14 08:28:32 -0300

    [media] saa6752hs.h: drop empty header
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/pci/saa7134/saa7134-empress.c
+++ drivers/media/pci/saa7134/saa7134-empress.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/delay.h>
 
-#include <media/saa6752hs.h>
 #include <media/v4l2-common.h>
 #include <media/v4l2-event.h>
 
