f3fa86db931b86ce7b43816fa35672d481fbe2b6
Tej Parkash <tej.parkash@qlogic.com>
2013-12-16 06:49:43 -0500

    [SCSI] qla4xxx: Fix pending IO completion in reset path before initiating chip reset
    Issue:
    Pending IO wait does not complete after triggering Graceful reset,
    causing ack timeout and call traces.
    
    Fix:
    1. Reducing the IO command wait timeout before triggering reset,
       as logically also timeout should be less than reset timeout (10sec).
    2. Moving the abort IO after chip reset, because only after
       chip reset, driver owns the IO otherwise it is with firmware and can
       still revert back with response.
    
    Signed-off-by: Tej Parkash <tej.parkash@qlogic.com>
    Signed-off-by: Vikas Chaudhary <vikas.chaudhary@qlogic.com>
    Reviewed-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/qla4xxx/ql4_def.h
+++ drivers/scsi/qla4xxx/ql4_def.h
@@ -195,7 +195,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ADAPTER_INIT_TOV		30
 #define ADAPTER_RESET_TOV		180
 #define EXTEND_CMD_TOV			60
-#define WAIT_CMD_TOV			30
+#define WAIT_CMD_TOV			5
 #define EH_WAIT_CMD_TOV			120
 #define FIRMWARE_UP_TOV			60
 #define RESET_FIRMWARE_TOV		30
