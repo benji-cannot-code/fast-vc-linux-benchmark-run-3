a9cd8c7de57025c505a83d8689df262e974cf283
Jesper Dangaard Brouer <brouer@redhat.com>
2013-12-16 17:09:47 +0100

    ipvs: correct usage/allocation of seqadj ext in ipvs
    The IPVS FTP helper ip_vs_ftp could trigger an OOPS in nf_ct_seqadj_set,
    after commit 41d73ec053d2 (netfilter: nf_conntrack: make sequence number
    adjustments usuable without NAT).
    
    This is because, the seqadj ext is now allocated dynamically, and the
    IPVS code didn't handle this situation.  Fix this in the IPVS nfct
    code by invoking the alloc function nfct_seqadj_ext_add().
    
    Fixes: 41d73ec053d2 (netfilter: nf_conntrack: make sequence number adjustments usuable without NAT)
    Suggested-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- net/netfilter/ipvs/ip_vs_nfct.c
+++ net/netfilter/ipvs/ip_vs_nfct.c
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ip_vs.h>
 #include <net/netfilter/nf_conntrack_core.h>
 #include <net/netfilter/nf_conntrack_expect.h>
+#include <net/netfilter/nf_conntrack_seqadj.h>
 #include <net/netfilter/nf_conntrack_helper.h>
 #include <net/netfilter/nf_conntrack_zones.h>
 
@@ -96,6 +97,11 @@ ip_vs_update_conntrack(struct sk_buff *skb, struct ip_vs_conn *cp, int outin)
 
 	/* Alter reply only in original direction */
 	if (CTINFO2DIR(ctinfo) != IP_CT_DIR_ORIGINAL)
+		return;
+
+	/* Applications may adjust TCP seqs */
+	if (cp->app && nf_ct_protonum(ct) == IPPROTO_TCP &&
+	    !nfct_seqadj(ct) && !nfct_seqadj_ext_add(ct))
 		return;
 
 	/*
