8f797bbfd97813eb1ca10442dfc55a18f973f5a3
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2013-12-16 14:06:15 +0800

    openvswitch: remove duplicated include from flow_table.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Jesse Gross <jesse@nicira.com>

--- net/openvswitch/flow_table.c
+++ net/openvswitch/flow_table.c
@@ -45,8 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ipv6.h>
 #include <net/ndisc.h>
 
-#include "datapath.h"
-
 #define TBL_MIN_BUCKETS		1024
 #define REHASH_INTERVAL		(10 * 60 * HZ)
 
