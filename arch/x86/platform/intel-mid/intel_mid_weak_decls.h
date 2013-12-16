d88cfb763d5c3bc98f263e01cd1137d15d720dbd
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2013-12-16 12:07:37 -0800

    x86, intel-mid: Add Clovertrail platform support
    This patch adds Clovertrail support on intel-mid and makes it more
    flexible to support other SoCs.
    
    Signed-off-by: David Cohen <david.a.cohen@linux.intel.com>
    Link: http://lkml.kernel.org/r/1387224459-25746-3-git-send-email-david.a.cohen@linux.intel.com
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: Fei Yang <fei.yang@intel.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/platform/intel-mid/intel_mid_weak_decls.h
+++ arch/x86/platform/intel-mid/intel_mid_weak_decls.h
@@ -12,5 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /* __attribute__((weak)) makes these declarations overridable */
-extern void intel_mid_power_off(void) __attribute__((weak));
-extern unsigned long __init intel_mid_calibrate_tsc(void) __attribute__((weak));
+/* For every CPU addition a new get_<cpuname>_ops interface needs
+ * to be added.
+ */
+extern void * __cpuinit get_penwell_ops(void) __attribute__((weak));
+extern void * __cpuinit get_cloverview_ops(void) __attribute__((weak));
