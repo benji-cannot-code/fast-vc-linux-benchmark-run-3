6051905296d8aab5418f04ccb6081e72eac556c2
Rashika <rashika.kheria@gmail.com>
2013-12-17 14:45:00 +0530

    ACPI / NVS: Include appropriate header file in nvs.c
    Include header file internal.h in nvs.c because functions
    suspend_nvs_free(), suspend_nvs_alloc(), suspend_nvs_save() and
    suspend_nvs_restore() have their prototype declaration in internal.h.
    
    This eliminates the following warnings in nvs.c:
    drivers/acpi/nvs.c:128:6: warning: no previous prototype for ‘suspend_nvs_free’ [-Wmissing-prototypes]
    drivers/acpi/nvs.c:152:5: warning: no previous prototype for ‘suspend_nvs_alloc’ [-Wmissing-prototypes]
    drivers/acpi/nvs.c:169:5: warning: no previous prototype for ‘suspend_nvs_save’ [-Wmissing-prototypes]
    drivers/acpi/nvs.c:201:6: warning: no previous prototype for ‘suspend_nvs_restore’ [-Wmissing-prototypes]
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/nvs.c
+++ drivers/acpi/nvs.c
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/acpi.h>
 
+#include "internal.h"
+
 /* ACPI NVS regions, APEI may use it */
 
 struct nvs_region {
