e5c4c1be599876d22d25bda006416f0763fcb640
Rashika <rashika.kheria@gmail.com>
2013-12-17 15:04:17 +0530

    ACPI / proc: Include appropriate header file in proc.c
    Include appropriate header file internal.h in proc.c because function
    acpi_sleep_proc_init() has its prototype declaration in internal.h.
    
    This eliminates the following warning in proc.c:
    drivers/acpi/proc.c:148:12: warning: no previous prototype for ‘acpi_sleep_proc_init’ [-Wmissing-prototypes]
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/proc.c
+++ drivers/acpi/proc.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 
 #include "sleep.h"
+#include "internal.h"
 
 #define _COMPONENT		ACPI_SYSTEM_COMPONENT
 
