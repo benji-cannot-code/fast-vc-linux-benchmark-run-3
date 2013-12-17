0e40cd152da1c1ff2139f6173fbde52c815e836d
Archit Taneja <archit@ti.com>
2013-12-17 16:40:21 +0530

    OMAPDSS: DISPC: Preload more data in pipeline DMAs for OMAP4+ SoCs
    DISPC pipeline DMAs preload some bytes of pixel data in the vertical blanking
    region before the start of each frame. The preload ensures the pipeline doesn't
    underflow when the active region of the display starts.
    
    DISPC_GFX/VIDp_PRELOAD registers allow us to program how many bytes of data
    should be preloaded for each pipeline. Calculating a precise preload value
    would be a complex function of the pixel clock of the connected display, the
    vertical blanking duration and the interconnect traffic at that instance. If
    the register is left untouched, a default value is preloaded.
    
    We observe underflows for OMAP4+ SoCs for certain bandwidth intensive use cases
    with many other initiators active, and in situations where memory access isn't
    very efficient(like accessing Tiler mapped buffers and EMIF configured in
    non-interleaved more). The cause of the underflow is because the default preload
    value isn't sufficient for the DMA to reach a steady state. We configure the
    PRELOAD register such that the pipelines preload data up to the high threshold
    of the FIFO.
    
    Preloading lot of data for older SoCs can have a negative impact. Due to slower
    interconnects, it's possible that the DISPC DMA cannot preload up to the high
    threshold within the vertical blanking region of the panel. We leave the PRELOAD
    registers to their reset values since we haven't faced underflows with these
    SoCs because of low value of PRELOAD.
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -91,6 +91,8 @@ struct dispc_features {
 
 	/* revert to the OMAP4 mechanism of DISPC Smart Standby operation */
 	bool mstandby_workaround:1;
+
+	bool set_max_preload:1;
 };
 
 #define DISPC_MAX_NR_FIFOS 5
@@ -1201,6 +1203,15 @@ void dispc_ovl_set_fifo_threshold(enum omap_plane plane, u32 low, u32 high)
 	dispc_write_reg(DISPC_OVL_FIFO_THRESHOLD(plane),
 			FLD_VAL(high, hi_start, hi_end) |
 			FLD_VAL(low, lo_start, lo_end));
+
+	/*
+	 * configure the preload to the pipeline's high threhold, if HT it's too
+	 * large for the preload field, set the threshold to the maximum value
+	 * that can be held by the preload register
+	 */
+	if (dss_has_feature(FEAT_PRELOAD) && dispc.feat->set_max_preload &&
+			plane != OMAP_DSS_WB)
+		dispc_write_reg(DISPC_OVL_PRELOAD(plane), min(high, 0xfffu));
 }
 EXPORT_SYMBOL(dispc_ovl_set_fifo_threshold);
 
@@ -3527,6 +3538,7 @@ static const struct dispc_features omap24xx_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_24xx,
 	.num_fifos		=	3,
 	.no_framedone_tv	=	true,
+	.set_max_preload	=	false,
 };
 
 static const struct dispc_features omap34xx_rev1_0_dispc_feats __initconst = {
@@ -3546,6 +3558,7 @@ static const struct dispc_features omap34xx_rev1_0_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_34xx,
 	.num_fifos		=	3,
 	.no_framedone_tv	=	true,
+	.set_max_preload	=	false,
 };
 
 static const struct dispc_features omap34xx_rev3_0_dispc_feats __initconst = {
@@ -3565,6 +3578,7 @@ static const struct dispc_features omap34xx_rev3_0_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_34xx,
 	.num_fifos		=	3,
 	.no_framedone_tv	=	true,
+	.set_max_preload	=	false,
 };
 
 static const struct dispc_features omap44xx_dispc_feats __initconst = {
@@ -3584,6 +3598,7 @@ static const struct dispc_features omap44xx_dispc_feats __initconst = {
 	.calc_core_clk		=	calc_core_clk_44xx,
 	.num_fifos		=	5,
 	.gfx_fifo_workaround	=	true,
+	.set_max_preload	=	true,
 };
 
 static const struct dispc_features omap54xx_dispc_feats __initconst = {
@@ -3604,6 +3619,7 @@ static const struct dispc_features omap54xx_dispc_feats __initconst = {
 	.num_fifos		=	5,
 	.gfx_fifo_workaround	=	true,
 	.mstandby_workaround	=	true,
+	.set_max_preload	=	true,
 };
 
 static int __init dispc_init_features(struct platform_device *pdev)
