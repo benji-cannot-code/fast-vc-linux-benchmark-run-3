7556fc40cdafec6f63d412931591c659f31066c0
Rashika <rashika.kheria@gmail.com>
2013-12-17 14:58:31 +0530

    ACPI / PCI: Include appropriate header file in pci_link.c
    Includes appropriate header file internal.h in pci_link.c
    because function acpi_pci_link_init() has its prototype declaration in
    internal.h.
    
    This eliminates the following warning in pci_link.c:
    drivers/acpi/pci_link.c:874:13: warning: no previous prototype for ‘acpi_pci_link_init’ [-Wmissing-prototypes]
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/pci_link.c
+++ drivers/acpi/pci_link.c
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/acpi.h>
 
+#include "internal.h"
+
 #define PREFIX "ACPI: "
 
 #define _COMPONENT			ACPI_PCI_COMPONENT
