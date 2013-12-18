c5811019af95414dc0c67643e792ceddf7483825
Kukjin Kim <kgene.kim@samsung.com>
2013-12-19 04:23:13 +0900

    PM / devfreq: use inclusion <mach/map.h> instead of <plat/map-s5p.h>
    This fixes follwoing warning:
    In file included from drivers/devfreq/exynos/exynos4_bus.h:15:0,
                     from drivers/devfreq/exynos/exynos4_bus.c:35:
    arch/arm/mach-exynos/include/mach/map.h:22:0: warning: "S3C_UART_OFFSET" redefined [enabled by default]
    In file included from drivers/devfreq/exynos/exynos4_bus.c:33:0:
    arch/arm/plat-samsung/include/plat/map-s5p.h:57:0: note: this is the location of the previous definition
    
    Acked-by: MyungJoo Ham <myungjoo.ham@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- drivers/devfreq/exynos/exynos4_bus.c
+++ drivers/devfreq/exynos/exynos4_bus.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern unsigned int exynos_result_of_asv;
 #endif
 
-#include <plat/map-s5p.h>
+#include <mach/map.h>
 
 #include "exynos4_bus.h"
 
