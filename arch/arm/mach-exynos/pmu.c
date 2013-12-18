5b3af3a2b3430b8233b146d05ed6ba0d5522d171
Kukjin Kim <kgene.kim@samsung.com>
2013-12-19 04:22:40 +0900

    ARM: EXYNOS: remove <mach/regs-clock.h> for exynos
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/pmu.c
+++ arch/arm/mach-exynos/pmu.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/bug.h>
 
-#include <mach/regs-clock.h>
+#include <plat/cpu.h>
 
 #include "common.h"
 #include "regs-pmu.h"
