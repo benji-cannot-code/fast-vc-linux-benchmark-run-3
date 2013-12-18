2a68aa9efc3cd24f7068a1263e8bee43ecc72656
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2013-12-18 09:53:25 +0530

    ath9k: Add a delay between RTC reset/clear for AR9003
    The small delay that is present between a RTC reset/clear
    operation is required for the chip to settle and this is
    needed for all chips, not just the AR9002 family.
    
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/hw.c
+++ drivers/net/wireless/ath/ath9k/hw.c
@@ -1400,8 +1400,7 @@ static bool ath9k_hw_set_reset_power_on(struct ath_hw *ah)
 
 	REGWRITE_BUFFER_FLUSH(ah);
 
-	if (!AR_SREV_9300_20_OR_LATER(ah))
-		udelay(2);
+	udelay(2);
 
 	if (!AR_SREV_9100(ah) && !AR_SREV_9300_20_OR_LATER(ah))
 		REG_WRITE(ah, AR_RC, 0);
