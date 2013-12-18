b65d683b06920b4fe01917930095bac49f496b7b
Micky Ching <micky_ching@realsil.com.cn>
2013-12-18 10:03:13 +0800

    mfd: rtsx: Add support for card reader rtl8402
    rtl8402 is much like rtl8411, so just add it to rtl8411.c
    
    Signed-off-by: Micky Ching <micky_ching@realsil.com.cn>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/rtsx_pcr.h
+++ drivers/mfd/rtsx_pcr.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void rts5209_init_params(struct rtsx_pcr *pcr);
 void rts5229_init_params(struct rtsx_pcr *pcr);
 void rtl8411_init_params(struct rtsx_pcr *pcr);
+void rtl8402_init_params(struct rtsx_pcr *pcr);
 void rts5227_init_params(struct rtsx_pcr *pcr);
 void rts5249_init_params(struct rtsx_pcr *pcr);
 void rtl8411b_init_params(struct rtsx_pcr *pcr);
