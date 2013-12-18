ed6333edfb78d30571de19c909da28ef524a31bd
Kumar Sanghvi <kumaras@chelsio.com>
2013-12-18 16:38:20 +0530

    cxgb4: Include TCP as protocol when creating server filters
    We were creating LE Workaround Server Filters without specifying
    IPPROTO_TCP (6) in the filters (when F_PROTOCOL is set in TP_VLAN_PRI_MAP).
    This meant that UDP packets with matching IP Addresses/Ports would get
    caught up in the filter and be delivered to ULDs like iw_cxgb4.
    So, include the protocol information in the server filter properly.
    
    Based on original work by Casey Leedom <leedom@chelsio.com>
    
    Signed-off-by: Kumar Sanghvi <kumaras@chelsio.com>
    Signed-off-by: Hariprasad Shenai <hariprasad@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/cxgb4/t4_regs.h
+++ drivers/net/ethernet/chelsio/cxgb4/t4_regs.h
@@ -1172,6 +1172,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define A_TP_TX_SCHED_PCMD 0x25
 
+#define S_PROTOCOL    5
+#define V_PROTOCOL(x) ((x) << S_PROTOCOL)
+#define F_PROTOCOL    V_PROTOCOL(1U)
+
 #define S_PORT    1
 #define V_PORT(x) ((x) << S_PORT)
 #define F_PORT    V_PORT(1U)
