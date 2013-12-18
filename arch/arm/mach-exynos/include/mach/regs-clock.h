9014b7815364994b6c9fda095ecfd39599b5f3b7
Kukjin Kim <kgene.kim@samsung.com>
2013-12-19 04:01:24 +0900

    ARM: EXYNOS: remove inclusion <mach/regs-pmu.h> into another headers
    This is needed to remove dependency of headers.
    
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/include/mach/regs-clock.h
+++ arch/arm/mach-exynos/include/mach/regs-clock.h
@@ -366,8 +366,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Compatibility defines and inclusion */
 
-#include <mach/regs-pmu.h>
-
 #define S5P_EPLL_CON				EXYNOS4_EPLL_CON0
 
 #endif /* __ASM_ARCH_REGS_CLOCK_H */
