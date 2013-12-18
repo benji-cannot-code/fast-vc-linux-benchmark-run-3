666d7a9d31fe9a0615042e828687660924b8d019
Kukjin Kim <kgene.kim@samsung.com>
2013-12-19 04:21:34 +0900

    cpufreq: exynos: move definitions for exynos-cpufreq into drivers/cpufreq/
    This moves regarding exynos-cpufreq definitions into drivers/cpufreq/
    exynos-cpufreq.h because they are used only for the cpufreq driver.
    
    Cc: Rafael J. Wysocki <rjw@rjwysocki.net>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- drivers/cpufreq/exynos5250-cpufreq.c
+++ drivers/cpufreq/exynos5250-cpufreq.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpufreq.h>
 
 #include <mach/map.h>
-#include <mach/regs-clock.h>
 
 #include "exynos-cpufreq.h"
 
