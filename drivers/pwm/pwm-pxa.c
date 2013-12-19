32525a5a656e56d7ead913fceaa61f3d624bca7d
Jingoo Han <jg1.han@samsung.com>
2013-12-19 13:34:05 +0900

    pwm: pxa: remove unnecessary space before tabs
    Remove unnecessary space before tabs in order to fix the following
    checkpatch warning.
    
      WARNING: please, no space before tabs
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-pxa.c
+++ drivers/pwm/pwm-pxa.c
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  *
  * 2008-02-13	initial version
- * 		eric miao <eric.miao@marvell.com>
+ *		eric miao <eric.miao@marvell.com>
  */
 
 #include <linux/module.h>
