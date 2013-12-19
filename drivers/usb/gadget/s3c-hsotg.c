d7f3652ff2ebfddf5f9cc9b1845b3189ca944a82
Matt Porter <mporter@linaro.org>
2013-12-19 09:23:04 -0500

    usb: gadget: s3c-hsotg: enable build for other platforms
    Remove unused Samsung-specific machine include and Kconfig
    dependency on S3C.
    
    Signed-off-by: Matt Porter <mporter@linaro.org>
    Reviewed-by: Markus Mayer <markus.mayer@linaro.org>
    Reviewed-by: Tim Kryger <tim.kryger@linaro.org>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/s3c-hsotg.c
+++ drivers/usb/gadget/s3c-hsotg.c
@@ -37,8 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb/phy.h>
 #include <linux/platform_data/s3c-hsotg.h>
 
-#include <mach/map.h>
-
 #include "s3c-hsotg.h"
 
 static const char * const s3c_hsotg_supply_names[] = {
