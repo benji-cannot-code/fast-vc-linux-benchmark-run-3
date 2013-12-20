d957c316f013c19cdc7816482b86acc82de5d52f
Haojian Zhuang <haojian.zhuang@linaro.org>
2013-12-20 10:52:56 +0800

    ARM: hisi: rename hi3xxx to hisi
    Since some new Hisilicon SoCs are not named as hi3xxx, rename mach-hi3xxx
    to mach-hisi instead. And the pronounciation of "hisi" is similar to the
    chinese pronounciation of Hisilicon. So Hisilicon guys like this name.
    
    ARCH_HI3xxx will be renamed later since other drivers are using it and
    they are still in linux-next git tree. So rename ARCH_HI3xxx later.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>
    Signed-off-by: Kevin Hilman <khilman@linaro.org>

--- arch/arm/mach-hisi/hisilicon.c
+++ arch/arm/mach-hisi/hisilicon.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * (Hisilicon's Hi36xx/Hi37xx SoC based) flattened device tree enabled machine
+ * (Hisilicon's SoC based) flattened device tree enabled machine
  *
  * Copyright (c) 2012-2013 Hisilicon Ltd.
  * Copyright (c) 2012-2013 Linaro Ltd.
