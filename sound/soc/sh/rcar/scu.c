d7a34e95947e983b96c8b8ae2c0d995b442c92b8
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2013-12-19 19:27:03 -0800

    ASoC: rsnd: tidyup register naming
    Use correct register name which appears in the datasheet
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/sh/rcar/scu.c
+++ sound/soc/sh/rcar/scu.c
@@ -116,7 +116,7 @@ static int rsnd_scu_set_mode(struct rsnd_priv *priv,
 
 	if (rsnd_is_gen1(priv)) {
 		val = (1 << id);
-		rsnd_mod_bset(mod, SRC_CTRL, val, val);
+		rsnd_mod_bset(mod, SRC_ROUTE_CTRL, val, val);
 	}
 
 	return 0;
@@ -142,7 +142,7 @@ static int rsnd_scu_set_hpbif(struct rsnd_priv *priv,
 	}
 
 	rsnd_mod_write(mod, BUSIF_MODE, 1);
-	rsnd_mod_write(mod, BUSIF_ADINR, adinr);
+	rsnd_mod_write(mod, SRC_ADINR, adinr);
 
 	return 0;
 }
