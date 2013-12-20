2a7befd0b6752102a06fef5f087e4c417705ed16
Hans Verkuil <hans.verkuil@cisco.com>
2013-12-20 09:04:44 -0300

    [media] davinci-vpfe: fix compile error
    The include for delay.h was missing.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Cc: Lad, Prabhakar <prabhakar.csengg@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/staging/media/davinci_vpfe/dm365_isif.c
+++ drivers/staging/media/davinci_vpfe/dm365_isif.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *      Prabhakar Lad <prabhakar.lad@ti.com>
  */
 
+#include <linux/delay.h>
 #include "dm365_isif.h"
 #include "vpfe_mc_capture.h"
 
