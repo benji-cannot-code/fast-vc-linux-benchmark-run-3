4694ae3cae81542de66cd577a99e4747c89187a3
Jayachandran C <jchandra@broadcom.com>
2013-12-21 16:52:20 +0530

    MIPS: Netlogic: Identify XLP 9XX chip
    Adds processor ID of XLP 9XX to asm/cpu.h.  Update netlogic/xlp-hal/xlp.h
    to add cpu_is_xlp9xx() and to update cpu_is_xlpii() to support XLP 9XX.
    
    Signed-off-by: Jayachandran C <jchandra@broadcom.com>
    Signed-off-by: John Crispin <blogic@openwrt.org>
    Patchwork: http://patchwork.linux-mips.org/patch/6274/

--- arch/mips/include/asm/cpu.h
+++ arch/mips/include/asm/cpu.h
@@ -199,6 +199,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PRID_IMP_NETLOGIC_XLP8XX	0x1000
 #define PRID_IMP_NETLOGIC_XLP3XX	0x1100
 #define PRID_IMP_NETLOGIC_XLP2XX	0x1200
+#define PRID_IMP_NETLOGIC_XLP9XX	0x1500
 
 /*
  * Particular Revision values for bits 7:0 of the PRId register.
