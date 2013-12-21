779d0b70282c907eac4ce6cc58f243d89618241d
Rashika Kheria <rashika.kheria@gmail.com>
2013-12-21 15:52:38 +0530

    drivers: dgnc: Include appropriate header file in dgnc_trace.c
    Include appropriate header file dgnc/dgnc_trace.h in dgnc_trace.c
    because function dgnc_tracer_free() has its prototype declaration in the
    header file.
    
    This eliminates the following warning in dgnc_trace.c:
    drivers/staging/dgnc/dgnc_trace.c:180:6: warning: no previous prototype for ‘dgnc_tracer_free’ [-Wmissing-prototypes]
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_trace.c
+++ drivers/staging/dgnc/dgnc_trace.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 
 #include "dgnc_driver.h"
+#include "dgnc_trace.h"
 
 #define TRC_TO_CONSOLE 1
 
