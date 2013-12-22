3b6eefc578af8dcd7a0fb4905ad24a8938f7fc21
David S. Miller <davem@davemloft.net>
2013-12-21 22:00:39 -0500

    Revert "sctp: fix missing include file"
    This reverts commit ac0917f250c61ae1e395844ebd924db0e07d1f87.
    
    Better version of this fix forthcoming.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/input.c
+++ net/sctp/input.c
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/time.h> /* For struct timeval */
 #include <linux/slab.h>
 #include <net/ip.h>
-#include <net/ip6_route.h>
 #include <net/icmp.h>
 #include <net/snmp.h>
 #include <net/sock.h>
