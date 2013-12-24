f8ec9821356ac996bcc0993127bee7b7168ae3e3
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2013-12-24 10:44:22 +0530

    ath9k: Fix baseband watchdog interrupts
    Program the required baseband watchdog interrupt
    mask to ensure that the correct watchdog interrupts
    are raised when the BB is hung for some reason.
    
    Also, use the capability HW_BB_WATCHDOG instead of
    relying on other flags.
    
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/reg.h
+++ drivers/net/wireless/ath/ath9k/reg.h
@@ -305,6 +305,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AR_IMR_S2              0x00ac
 #define AR_IMR_S2_QCU_TXURN    0x000003FF
 #define AR_IMR_S2_QCU_TXURN_S  0
+#define AR_IMR_S2_BB_WATCHDOG  0x00010000
 #define AR_IMR_S2_CST          0x00400000
 #define AR_IMR_S2_GTT          0x00800000
 #define AR_IMR_S2_TIM          0x01000000
