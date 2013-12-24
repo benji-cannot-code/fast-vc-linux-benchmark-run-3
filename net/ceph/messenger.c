016d6bcbaf9e598d1559195b9985ad9eb7fadb7d
Ilya Dryomov <ilya.dryomov@inktank.com>
2013-12-24 21:19:24 +0200

    libceph: update ceph_features.h
    This updates ceph_features.h so that it has all feature bits defined in
    ceph.git.  In the interim since the last update, ceph.git crossed the
    "32 feature bits" point, and, the addition of the 33rd bit wasn't
    handled correctly.  The work-around is squashed into this commit and
    reflects ceph.git commit 053659d05e0349053ef703b414f44965f368b9f0.
    
    Signed-off-by: Ilya Dryomov <ilya.dryomov@inktank.com>
    Reviewed-by: Sage Weil <sage@inktank.com>

--- net/ceph/messenger.c
+++ net/ceph/messenger.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dns_resolver.h>
 #include <net/tcp.h>
 
+#include <linux/ceph/ceph_features.h>
 #include <linux/ceph/libceph.h>
 #include <linux/ceph/messenger.h>
 #include <linux/ceph/decode.h>
@@ -1946,7 +1947,8 @@ static int process_connect(struct ceph_connection *con)
 {
 	u64 sup_feat = con->msgr->supported_features;
 	u64 req_feat = con->msgr->required_features;
-	u64 server_feat = le64_to_cpu(con->in_reply.features);
+	u64 server_feat = ceph_sanitize_features(
+				le64_to_cpu(con->in_reply.features));
 	int ret;
 
 	dout("process_connect on %p tag %d\n", con, (int)con->in_tag);
