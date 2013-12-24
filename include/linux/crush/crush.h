ca65abae64cfd448de66da82e9f9f53e96ac348d
Ilya Dryomov <ilya.dryomov@inktank.com>
2013-12-24 21:19:24 +0200

    crush: eliminate CRUSH_MAX_SET result size limitation
    This is only present to size the temporary scratch arrays that we put on
    the stack.  Let the caller allocate them as they wish and remove the
    limitation.
    
    Reflects ceph.git commit 1cfe140bf2dab99517589a82a916f4c75b9492d1.
    
    Signed-off-by: Ilya Dryomov <ilya.dryomov@inktank.com>
    Reviewed-by: Sage Weil <sage@inktank.com>

--- include/linux/crush/crush.h
+++ include/linux/crush/crush.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #define CRUSH_MAX_DEPTH 10  /* max crush hierarchy depth */
-#define CRUSH_MAX_SET   10  /* max size of a mapping result */
 
 
 /*
