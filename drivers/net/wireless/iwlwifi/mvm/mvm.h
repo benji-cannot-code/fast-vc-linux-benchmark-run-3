112fc56d12a9b1abafd83bb89eae15850cd3e956
Ilan Peer <ilan.peer@intel.com>
2013-12-24 22:08:14 +0200

    iwlwifi: mvm: Add a missed beacons threshold
    Instead of always calling ieee80211_beacon_loss() on every missed
    beacons notification, call this function only if the number of
    consecutive missed beacons from last rx is higher than a predefined
    threshold.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/iwlwifi/mvm/mvm.h
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IWL_MVM_MAX_ADDRESSES		5
 /* RSSI offset for WkP */
 #define IWL_RSSI_OFFSET 50
+#define IWL_MVM_MISSED_BEACONS_THRESHOLD 8
 
 enum iwl_mvm_tx_fifo {
 	IWL_MVM_TX_FIFO_BK = 0,
