70c080fc29a0ff3d6df5a4ac278758d67912419b
Joseph Gasparakis <joseph.gasparakis@intel.com>
2013-12-28 05:27:57 +0000

    i40e: Rx checksum offload for VXLAN
    This implements receive offload for VXLAN for i40e.  The hardware
    supports checksum offload/verification of the inner/outer header.
    
    Change-Id: I450db300af6713f2044fef1191a0d1d294c13369
    Signed-off-by: Joseph Gasparakis <joseph.gasparakis@intel.com>
    Signed-off-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e.h
+++ drivers/net/ethernet/intel/i40e/i40e.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _I40E_H_
 
 #include <net/tcp.h>
+#include <net/udp.h>
 #include <linux/init.h>
 #include <linux/types.h>
 #include <linux/errno.h>
