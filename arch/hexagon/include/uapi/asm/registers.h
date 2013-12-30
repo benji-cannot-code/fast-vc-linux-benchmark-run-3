56ccb200341fcd21c35ab362da1c5efa158f3eb5
Richard Kuo <rkuo@codeaurora.org>
2013-12-30 14:21:12 -0600

    Hexagon: remove SP macro
    The SP/r29 macro wasn't used anywhere else and was causing conflicts
    with another module, so just remove it.
    
    Signed-off-by: Richard Kuo <rkuo@codeaurora.org>

--- arch/hexagon/include/uapi/asm/registers.h
+++ arch/hexagon/include/uapi/asm/registers.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_REGISTERS_H
 #define _ASM_REGISTERS_H
 
-#define SP r29
-
 #ifndef __ASSEMBLY__
 
 /*  See kernel/entry.S for further documentation.  */
@@ -216,7 +214,7 @@ struct pt_regs {
 #define pt_clr_singlestep(regs) ((regs)->hvmer.vmest &= ~(1<<HVM_VMEST_SS_SFT))
 
 #define pt_set_rte_sp(regs, sp) do {\
-	pt_psp(regs) = (regs)->SP = (sp);\
+	pt_psp(regs) = (regs)->r29 = (sp);\
 	} while (0)
 
 #define pt_set_kmode(regs) \
