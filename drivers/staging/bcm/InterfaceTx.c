2498a03c80aeb66fc8379ef44afa9fa55d5b3d13
Ralph Mueck <linux-kernel@rmueck.de>
2013-12-30 12:44:22 +0100

    beeceem: Add spaces before opening parentheses in if-conditionals in InterfaceTx.c
    This patch adds spaces before the opening parentheses of if-conditionals to comply with offical code styling guidelines.
    
    Signed-off-by: Ralph Mueck <linux-kernel@rmueck.de>
    Signed-off-by: Matthias Oefelein <ma.oefelein@arcor.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/InterfaceTx.c
+++ drivers/staging/bcm/InterfaceTx.c
@@ -14,9 +14,9 @@ static void write_bulk_callback(struct urb *urb/*, struct pt_regs *regs*/)
 	if (unlikely(netif_msg_tx_done(Adapter)))
 		pr_info(PFX "%s: transmit status %d\n", Adapter->dev->name, urb->status);
 
-	if(urb->status != STATUS_SUCCESS)
+	if (urb->status != STATUS_SUCCESS)
 	{
-		if(urb->status == -EPIPE)
+		if (urb->status == -EPIPE)
 		{
 			psIntfAdapter->psAdapter->bEndPointHalted = TRUE;
 			wake_up(&psIntfAdapter->psAdapter->tx_packet_wait_queue);
@@ -32,16 +32,16 @@ static void write_bulk_callback(struct urb *urb/*, struct pt_regs *regs*/)
 
 
 
-	if(TRUE == psAdapter->bPreparingForLowPowerMode)
+	if (TRUE == psAdapter->bPreparingForLowPowerMode)
 	{
 
-		if(((pControlMsg->szData[0] == GO_TO_IDLE_MODE_PAYLOAD) &&
+		if (((pControlMsg->szData[0] == GO_TO_IDLE_MODE_PAYLOAD) &&
 			(pControlMsg->szData[1] == TARGET_CAN_GO_TO_IDLE_MODE)))
 
 		{
 			bpowerDownMsg = TRUE;
 			//This covers the bus err while Idle Request msg sent down.
-			if(urb->status != STATUS_SUCCESS)
+			if (urb->status != STATUS_SUCCESS)
 			{
 				psAdapter->bPreparingForLowPowerMode = false;
 				BCM_DEBUG_PRINT(Adapter, DBG_TYPE_TX, NEXT_SEND, DBG_LVL_ALL, "Idle Mode Request msg failed to reach to Modem");
@@ -51,7 +51,7 @@ static void write_bulk_callback(struct urb *urb/*, struct pt_regs *regs*/)
 				goto err_exit;
 			}
 
-			if(psAdapter->bDoSuspend == false)
+			if (psAdapter->bDoSuspend == false)
 			{
 				psAdapter->IdleMode = TRUE;
 				//since going in Idle mode completed hence making this var false;
@@ -63,13 +63,13 @@ static void write_bulk_callback(struct urb *urb/*, struct pt_regs *regs*/)
 			}
 
 		}
-		else if((pControlMsg->Leader.Status == LINK_UP_CONTROL_REQ) &&
+		else if ((pControlMsg->Leader.Status == LINK_UP_CONTROL_REQ) &&
 			(pControlMsg->szData[0] == LINK_UP_ACK) &&
 			(pControlMsg->szData[1] == LINK_SHUTDOWN_REQ_FROM_FIRMWARE)  &&
 			(pControlMsg->szData[2] == SHUTDOWN_ACK_FROM_DRIVER))
 		{
 			//This covers the bus err while shutdown Request msg sent down.
-			if(urb->status != STATUS_SUCCESS)
+			if (urb->status != STATUS_SUCCESS)
 			{
 				psAdapter->bPreparingForLowPowerMode = false;
 				BCM_DEBUG_PRINT(Adapter, DBG_TYPE_TX, NEXT_SEND, DBG_LVL_ALL, "Shutdown Request Msg failed to reach to Modem");
@@ -80,7 +80,7 @@ static void write_bulk_callback(struct urb *urb/*, struct pt_regs *regs*/)
 			}
 
 			bpowerDownMsg = TRUE;
-			if(psAdapter->bDoSuspend == false)
+			if (psAdapter->bDoSuspend == false)
 			{
 				psAdapter->bShutStatus = TRUE;
 				//since going in shutdown mode completed hence making this var false;
@@ -91,7 +91,7 @@ static void write_bulk_callback(struct urb *urb/*, struct pt_regs *regs*/)
 			}
 		}
 
-		if(psAdapter->bDoSuspend && bpowerDownMsg)
+		if (psAdapter->bDoSuspend && bpowerDownMsg)
 		{
 			//issuing bus suspend request
 			BCM_DEBUG_PRINT(Adapter, DBG_TYPE_TX, NEXT_SEND, DBG_LVL_ALL, "Issuing the Bus suspend request to USB stack");
@@ -113,7 +113,7 @@ static struct bcm_usb_tcb *GetBulkOutTcb(struct bcm_interface_adapter *psIntfAda
 	struct bcm_usb_tcb *pTcb = NULL;
 	UINT index = 0;
 
-	if((atomic_read(&psIntfAdapter->uNumTcbUsed) < MAXIMUM_USB_TCB) &&
+	if ((atomic_read(&psIntfAdapter->uNumTcbUsed) < MAXIMUM_USB_TCB) &&
 		(psIntfAdapter->psAdapter->StopAllXaction == false))
 	{
 		index = atomic_read(&psIntfAdapter->uCurrTcb);
@@ -147,7 +147,7 @@ static int TransmitTcb(struct bcm_interface_adapter *psIntfAdapter, struct bcm_u
 
 	BCM_DEBUG_PRINT(psIntfAdapter->psAdapter, DBG_TYPE_TX, NEXT_SEND, DBG_LVL_ALL, "Sending Bulk out packet\n");
 	//For T3B,INT OUT end point will be used as bulk out end point
-	if((psIntfAdapter->psAdapter->chip_id == T3B) && (psIntfAdapter->bHighSpeedDevice == TRUE))
+	if ((psIntfAdapter->psAdapter->chip_id == T3B) && (psIntfAdapter->bHighSpeedDevice == TRUE))
 	{
 		usb_fill_int_urb(urb, psIntfAdapter->udev,
 			psIntfAdapter->sBulkOut.bulk_out_pipe,
@@ -162,7 +162,7 @@ static int TransmitTcb(struct bcm_interface_adapter *psIntfAdapter, struct bcm_u
 	}
 	urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP; /* For DMA transfer */
 
-	if(false == psIntfAdapter->psAdapter->device_removed &&
+	if (false == psIntfAdapter->psAdapter->device_removed &&
 	   false == psIntfAdapter->psAdapter->bEndPointHalted &&
 	   false == psIntfAdapter->bSuspended &&
 	   false == psIntfAdapter->bPreparingForBusSuspend)
@@ -171,7 +171,7 @@ static int TransmitTcb(struct bcm_interface_adapter *psIntfAdapter, struct bcm_u
 		if (retval)
 		{
 			BCM_DEBUG_PRINT(psIntfAdapter->psAdapter, DBG_TYPE_TX, NEXT_SEND, DBG_LVL_ALL, "failed submitting write urb, error %d", retval);
-			if(retval == -EPIPE)
+			if (retval == -EPIPE)
 			{
 				psIntfAdapter->psAdapter->bEndPointHalted = TRUE;
 				wake_up(&psIntfAdapter->psAdapter->tx_packet_wait_queue);
@@ -187,7 +187,7 @@ int InterfaceTransmitPacket(PVOID arg, PVOID data, UINT len)
 
 	struct bcm_interface_adapter *psIntfAdapter = arg;
 	pTcb = GetBulkOutTcb(psIntfAdapter);
-	if(pTcb == NULL)
+	if (pTcb == NULL)
 	{
 		BCM_DEBUG_PRINT(psIntfAdapter->psAdapter, DBG_TYPE_PRINTK, 0, 0, "No URB to transmit packet, dropping packet");
 		return -EFAULT;
