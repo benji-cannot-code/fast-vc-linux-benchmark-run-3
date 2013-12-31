86243940ca35ae9bcfaa66c8c754491f8d50e1fc
Vincent Stehlé <vincent.stehle@laposte.net>
2013-12-31 11:55:08 +0100

    staging: dgap: fix missing header inclusion
    Include slab.h to fix the following compilation error:
    
      drivers/staging/dgap/dgap_fep5.c: In function ‘dgap_do_config_load’:
      drivers/staging/dgap/dgap_fep5.c:78:2: error: implicit declaration of function ‘kzalloc’ [-Werror=implicit-function-declaration]
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@laposte.net>
    Cc: Lidza Louina <lidza.louina@gmail.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Scott H Kilau <Scott_Kilau@digi.com>
    Cc: Eng.Linux@digi.com
    Cc: trivial@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_fep5.c
+++ drivers/staging/dgap/dgap_fep5.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>	/* For copy_from_user/copy_to_user */
 #include <linux/tty.h>
 #include <linux/tty_flip.h>	/* For tty_schedule_flip */
+#include <linux/slab.h>
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39)
 #include <linux/sched.h>
