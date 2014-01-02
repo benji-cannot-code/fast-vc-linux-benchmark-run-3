e02eb4c4c3855b3540a92e5ba0613646008687b4
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2014-01-02 10:27:12 -0300

    [media] omap3isp: stat: Merge dma_addr and iommu_addr fields
    The fields store buffer addresses as seen from the device. The first one
    is used with an external DMA engine while the second one is used with
    the ISP DMA engine. As they're never used together, merge them.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Sakari Ailus <sakari.ailus@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/omap3isp/ispstat.h
+++ drivers/media/platform/omap3isp/ispstat.h
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ispstat;
 
 struct ispstat_buffer {
-	unsigned long iommu_addr;
 	struct iovm_struct *iovm;
 	void *virt_addr;
 	dma_addr_t dma_addr;
