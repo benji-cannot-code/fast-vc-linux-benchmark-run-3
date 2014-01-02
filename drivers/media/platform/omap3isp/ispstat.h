eeea75a538756ea13935d34528f19b84f21ec269
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2014-01-02 20:06:08 -0300

    [media] omap3isp: stat: Use the DMA API
    Replace the OMAP-specific IOMMU API usage by the DMA API. All buffers
    are now allocated using dma_alloc_coherent() and the related sg table is
    retrieved using dma_get_sgtable() for sync operations.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Sakari Ailus <sakari.ailus@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/omap3isp/ispstat.h
+++ drivers/media/platform/omap3isp/ispstat.h
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ispstat;
 
 struct ispstat_buffer {
-	const struct sg_table *sgt;
+	struct sg_table sgt;
 	void *virt_addr;
 	dma_addr_t dma_addr;
 	struct timespec ts;
