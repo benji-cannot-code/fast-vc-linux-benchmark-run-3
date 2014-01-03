d0d50a3962cf0bb307a7d7d8c273f5eb8e3fd7de
Dave Young <dyoung@redhat.com>
2014-01-03 11:54:31 +0800

    x86: ksysfs.c build fix
    kbuild test robot report below error for randconfig:
    
      arch/x86/kernel/ksysfs.c: In function 'get_setup_data_paddr':
      arch/x86/kernel/ksysfs.c:81:3: error: implicit declaration of function 'ioremap_cache' [-Werror=implicit-function-declaration]
      arch/x86/kernel/ksysfs.c:86:3: error: implicit declaration of function 'iounmap' [-Werror=implicit-function-declaration]
    
    Fix it by including <asm/io.h> in ksysfs.c
    
    Signed-off-by: Dave Young <dyoung@redhat.com>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- arch/x86/kernel/ksysfs.c
+++ arch/x86/kernel/ksysfs.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/mm.h>
 
+#include <asm/io.h>
 #include <asm/setup.h>
 
 static ssize_t version_show(struct kobject *kobj,
