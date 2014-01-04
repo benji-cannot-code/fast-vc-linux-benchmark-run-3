8d538a0968d5f6731e6c2258b6885aa645865655
Bjorn Helgaas <bhelgaas@google.com>
2014-01-03 18:29:00 -0700

    agp/intel: Use pci_bus_address() to get GTTADR bus address
    Per the Intel 915G/915GV/... Chipset spec (document number 301467-005),
    GTTADR is a standard PCI BAR.
    
    The PCI core reads GTTADR at enumeration-time.  Use pci_bus_address()
    instead of reading it again in the driver.  This works correctly for both
    32-bit and 64-bit BARs.  The spec above only mentions 32-bit GTTADR, but we
    should still use the standard interface.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/char/agp/intel-agp.h
+++ drivers/char/agp/intel-agp.h
@@ -116,7 +116,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* intel 915G registers */
 #define I915_GMADR_BAR	2
 #define I915_MMADR_BAR	0
-#define I915_PTEADDR	0x1C
+#define I915_PTE_BAR	3
 #define I915_GMCH_GMS_STOLEN_48M	(0x6 << 4)
 #define I915_GMCH_GMS_STOLEN_64M	(0x7 << 4)
 #define G33_GMCH_GMS_STOLEN_128M	(0x8 << 4)
