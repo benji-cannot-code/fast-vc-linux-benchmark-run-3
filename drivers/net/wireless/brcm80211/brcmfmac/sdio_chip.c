a2f0440ab72d62f77fb4466096541813512a6482
Arend van Spriel <arend@broadcom.com>
2014-01-06 12:40:50 +0100

    mmc: add SDIO identifiers for Broadcom WLAN devices
    The SDIO identifier for Broadcom WLAN devices were defined in the
    brcmfmac SDIO driver. Moving the definitions in MMC header file
    seems common sense.
    
    Reviewed-by: Hante Meuleman <meuleman@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/sdio_chip.c
+++ drivers/net/wireless/brcm80211/brcmfmac/sdio_chip.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/mmc/card.h>
 #include <linux/mmc/sdio_func.h>
+#include <linux/mmc/sdio_ids.h>
 #include <linux/ssb/ssb_regs.h>
 #include <linux/bcma/bcma.h>
 
