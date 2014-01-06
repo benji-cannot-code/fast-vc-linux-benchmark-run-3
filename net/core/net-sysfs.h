57dece2f807c081d43ad88380a276d3d15f484bd
Daniel Borkmann <dborkman@redhat.com>
2014-01-06 01:20:11 +0100

    net: netdev_kobject_init: annotate with __init
    netdev_kobject_init() is only being called from __init context,
    that is, net_dev_init(), so annotate it with __init as well, thus
    the kernel can take this as a hint that the function is used only
    during the initialization phase and free up used memory resources
    after its invocation.
    
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net-sysfs.h
+++ net/core/net-sysfs.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NET_SYSFS_H__
 #define __NET_SYSFS_H__
 
-int netdev_kobject_init(void);
+int __init netdev_kobject_init(void);
 int netdev_register_kobject(struct net_device *);
 void netdev_unregister_kobject(struct net_device *);
 int net_rx_queue_update_kobjects(struct net_device *, int old_num, int new_num);
