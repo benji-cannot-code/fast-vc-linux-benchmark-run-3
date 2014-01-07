37a7ebf955d6a9d15934cec9215a57bad09b882f
Philipp Zabel <p.zabel@pengutronix.de>
2014-01-07 12:34:11 +0100

    gpio: make gpiod_direction_output take a logical value
    The documentation was not clear about whether
    gpio_direction_output should take a logical value or the physical
    level on the output line, i.e. whether the ACTIVE_LOW status
    would be taken into account.
    
    This converts gpiod_direction_output to use the logical level
    and adds a new gpiod_direction_output_raw for the raw value.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Reviewed-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/gpio/consumer.h
+++ include/linux/gpio/consumer.h
@@ -37,6 +37,7 @@ void devm_gpiod_put(struct device *dev, struct gpio_desc *desc);
 int gpiod_get_direction(const struct gpio_desc *desc);
 int gpiod_direction_input(struct gpio_desc *desc);
 int gpiod_direction_output(struct gpio_desc *desc, int value);
+int gpiod_direction_output_raw(struct gpio_desc *desc, int value);
 
 /* Value get/set from non-sleeping context */
 int gpiod_get_value(const struct gpio_desc *desc);
@@ -117,6 +118,12 @@ static inline int gpiod_direction_input(struct gpio_desc *desc)
 	return -ENOSYS;
 }
 static inline int gpiod_direction_output(struct gpio_desc *desc, int value)
+{
+	/* GPIO can never have been requested */
+	WARN_ON(1);
+	return -ENOSYS;
+}
+static inline int gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 {
 	/* GPIO can never have been requested */
 	WARN_ON(1);
