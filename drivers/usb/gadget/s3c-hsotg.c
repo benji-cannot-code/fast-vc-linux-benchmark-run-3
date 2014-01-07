e564ee141f34891247ff2bd4a3c6994ae511ff37
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2014-01-07 21:41:15 +0800

    usb: gadget: s3c-hsotg: remove duplicated include from s3c-hsotg.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/s3c-hsotg.c
+++ drivers/usb/gadget/s3c-hsotg.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regulator/consumer.h>
 #include <linux/of_platform.h>
 #include <linux/phy/phy.h>
-#include <linux/usb/phy.h>
 
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
