1ac509b1d24c7bbd3d6b074f65d47dc8ff1526b9
Frank Haverkamp <haver@linux.vnet.ibm.com>
2014-01-07 15:41:24 +0100

    GenWQE: Fix compile problems for Alpha
    The header which contained the declaration for kcalloc() was not
    inlcuded.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Frank Haverkamp <haver@linux.vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/genwqe/card_base.h
+++ drivers/misc/genwqe/card_base.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/version.h>
 #include <linux/debugfs.h>
+#include <linux/slab.h>
 
 #include <linux/genwqe/genwqe_card.h>
 #include "genwqe_driver.h"
