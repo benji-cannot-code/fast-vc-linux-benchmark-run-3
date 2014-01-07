d3fb9336bb3e26bae7e3756d469e46e2de1772d2
Linus Walleij <linus.walleij@linaro.org>
2014-01-07 19:21:57 +0100

    ARM: s3c24xx: fix build error
    When enabling device tree on the S3C an additional build
    bug appears in the Osiris DVS board file:
    
    CC  arch/arm/mach-s3c24xx/mach-osiris-dvs.o
    archh/arm/mach-s3c24xx/mach-osiris-dvs.c:
    In function ‘osiris_dvs_notify’:
    arch/arm/mach-s3c24xx/mach-osiris-dvs.c:77:4:
    error: implicit declaration of function ‘S3C2410_GPB’
    [-Werror=implicit-function-declaration]
        gpio_set_value(OSIRIS_GPIO_DVS, 1);
        ^
    
    Fix this by explicitly including
    <linux/platform_data/gpio-samsung-s3c24xx.h>
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Kukjin Kim <kgene.kim@samsung.com>
    Cc: Tomasz Figa <tomasz.figa@gmail.com>
    Cc: Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
    Cc: Ben Dooks <ben-linux@fluff.org>
    Cc: linux-samsung-soc@vger.kernel.org
    Acked-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-s3c24xx/mach-osiris-dvs.c
+++ arch/arm/mach-s3c24xx/mach-osiris-dvs.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/cpufreq.h>
 #include <linux/gpio.h>
+#include <linux/platform_data/gpio-samsung-s3c24xx.h>
 
 #include <linux/i2c/tps65010.h>
 
