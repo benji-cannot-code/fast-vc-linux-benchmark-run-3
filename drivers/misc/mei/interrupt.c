219ea835577a73e4a2de4ecc26a16afb5a0b9e97
Alexander Usyskin <alexander.usyskin@intel.com>
2014-01-08 22:31:46 +0200

    mei: fix syntax in comments and debug output
    Fix syntax errors in comments and debug strings
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/interrupt.c
+++ drivers/misc/mei/interrupt.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /**
- * mei_irq_compl_handler - dispatch complete handelers
+ * mei_irq_compl_handler - dispatch complete handlers
  *	for the completed callbacks
  *
  * @dev - mei device
@@ -339,7 +339,7 @@ int mei_irq_read_handler(struct mei_device *dev,
 		goto reset_slots;
 	}
 
-	/* find recepient cl */
+	/* find recipient cl */
 	list_for_each_entry(cl, &dev->file_list, link) {
 		if (mei_cl_hbm_equal(cl, mei_hdr)) {
 			cl_dbg(dev, cl, "got a message\n");
@@ -347,7 +347,7 @@ int mei_irq_read_handler(struct mei_device *dev,
 		}
 	}
 
-	/* if no recepient cl was found we assume corrupted header\n */
+	/* if no recipient cl was found we assume corrupted header */
 	if (&cl->link == &dev->file_list) {
 		dev_err(&dev->pdev->dev, "no destination client found 0x%08X\n",
 				dev->rd_msg_hdr);
