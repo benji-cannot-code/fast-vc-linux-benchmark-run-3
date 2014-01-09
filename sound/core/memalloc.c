1924950003992f15bd8ccffad8fd4cd87935c061
Takashi Iwai <tiwai@suse.de>
2014-01-09 07:44:11 +0100

    ALSA: Remove superfluous header inclusions in memalloc.c
    After cutting off the proc and page reservation codes, we don't need
    many headers any longer.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/memalloc.c
+++ sound/core/memalloc.c
@@ -23,17 +23,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/module.h>
-#include <linux/proc_fs.h>
 #include <linux/init.h>
-#include <linux/pci.h>
 #include <linux/slab.h>
 #include <linux/mm.h>
-#include <linux/seq_file.h>
 #include <asm/uaccess.h>
 #include <linux/dma-mapping.h>
 #include <linux/genalloc.h>
 #include <linux/moduleparam.h>
-#include <linux/mutex.h>
 #include <sound/memalloc.h>
 
 
