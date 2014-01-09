8dcca7e0bacd9014eb2d929252fb31a5fce549ba
Markos Chandras <markos.chandras@imgtec.com>
2014-01-09 16:01:29 +0000

    MIPS: asm: cpu: Add cpu flag for Enhanced Virtual Addressing
    The MIPS *Aptiv family uses bit 28 in Config5 CP0 register to
    indicate whether the core supports EVA or not.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef cpu_has_segments
 #define cpu_has_segments	(cpu_data[0].options & MIPS_CPU_SEGMENTS)
 #endif
-
+#ifndef cpu_has_eva
+#define cpu_has_eva		(cpu_data[0].options & MIPS_CPU_EVA)
+#endif
 
 /*
  * For the moment we don't consider R6000 and R8000 so we can assume that
