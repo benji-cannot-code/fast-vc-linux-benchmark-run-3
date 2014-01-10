84851368e9316c98df423f28c658abb25f7abd76
Marek Roszko <mark.roszko@gmail.com>
2014-01-10 10:33:11 +0100

    tty/serial: at91: disable uart timer at start of shutdown
    The uart timer will schedule a tasklet when it fires. It is possible that it
    can fire inside _shutdown before it is killed in the dma and pdc cleanup
    routines. This causes a tasklet that exists after the port is shutdown, so when
    the kernel finally executes it, it panics as the tty port is NULL.
    
    This is a somewhat rare condition but its possible if a program keeps on
    opening/closing the port. It has been observed in particular with systemd
    boot messages that were causing a kernel panic because of this behavior.
    
    Moving the timer deletion to the beginning of the function stops a tasklet from
    being scheduled unexpectedly.
    
    Signed-off-by: Marek Roszko <mark.roszko@gmail.com>
    Cc: stable <stable@vger.kernel.org> # v3.12
    [nicolas.ferre@atmel.com: modify commit message, call setup_timer() in any case]
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/atmel_serial.c
+++ drivers/tty/serial/atmel_serial.c
@@ -826,9 +826,6 @@ static void atmel_release_rx_dma(struct uart_port *port)
 	atmel_port->desc_rx = NULL;
 	atmel_port->chan_rx = NULL;
 	atmel_port->cookie_rx = -EINVAL;
-
-	if (!atmel_port->is_usart)
-		del_timer_sync(&atmel_port->uart_timer);
 }
 
 static void atmel_rx_from_dma(struct uart_port *port)
@@ -1230,9 +1227,6 @@ static void atmel_release_rx_pdc(struct uart_port *port)
 				 DMA_FROM_DEVICE);
 		kfree(pdc->buf);
 	}
-
-	if (!atmel_port->is_usart)
-		del_timer_sync(&atmel_port->uart_timer);
 }
 
 static void atmel_rx_from_pdc(struct uart_port *port)
@@ -1605,12 +1599,13 @@ static int atmel_startup(struct uart_port *port)
 	/* enable xmit & rcvr */
 	UART_PUT_CR(port, ATMEL_US_TXEN | ATMEL_US_RXEN);
 
+	setup_timer(&atmel_port->uart_timer,
+			atmel_uart_timer_callback,
+			(unsigned long)port);
+
 	if (atmel_use_pdc_rx(port)) {
 		/* set UART timeout */
 		if (!atmel_port->is_usart) {
-			setup_timer(&atmel_port->uart_timer,
-					atmel_uart_timer_callback,
-					(unsigned long)port);
 			mod_timer(&atmel_port->uart_timer,
 					jiffies + uart_poll_timeout(port));
 		/* set USART timeout */
@@ -1625,9 +1620,6 @@ static int atmel_startup(struct uart_port *port)
 	} else if (atmel_use_dma_rx(port)) {
 		/* set UART timeout */
 		if (!atmel_port->is_usart) {
-			setup_timer(&atmel_port->uart_timer,
-					atmel_uart_timer_callback,
-					(unsigned long)port);
 			mod_timer(&atmel_port->uart_timer,
 					jiffies + uart_poll_timeout(port));
 		/* set USART timeout */
@@ -1651,6 +1643,12 @@ static int atmel_startup(struct uart_port *port)
 static void atmel_shutdown(struct uart_port *port)
 {
 	struct atmel_uart_port *atmel_port = to_atmel_uart_port(port);
+
+	/*
+	 * Prevent any tasklets being scheduled during
+	 * cleanup
+	 */
+	del_timer_sync(&atmel_port->uart_timer);
 
 	/*
 	 * Clear out any scheduled tasklets before
