6a6dae6d520e90eeaaf372b43e10c8a35418f16a
Prarit Bhargava <prarit@redhat.com>
2014-01-13 08:40:20 -0500

    x86/irq: Fix kbuild warning in smp_irq_move_cleanup_interrupt()
    Fengguang Wu's 0day kernel build service reported the following build warning:
    
      arch/x86/kernel/apic/io_apic.c:2211
      smp_irq_move_cleanup_interrupt() warn: always true condition '(irq <= -1) => (0-u32max <= (-1))'
    
    because irq is defined as an unsigned int instead of an int.
    Fix this trivial error by redefining irq as a signed int.  The
    remaining consumers of the int are okay.
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    Cc: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Sebastian Andrzej Siewior <sebastian@breakpoint.cc>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Link: http://lkml.kernel.org/r/1389620420-7110-1-git-send-email-prarit@redhat.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/apic/io_apic.c
+++ arch/x86/kernel/apic/io_apic.c
@@ -2203,7 +2203,7 @@ asmlinkage void smp_irq_move_cleanup_interrupt(void)
 
 	me = smp_processor_id();
 	for (vector = FIRST_EXTERNAL_VECTOR; vector < NR_VECTORS; vector++) {
-		unsigned int irq;
+		int irq;
 		unsigned int irr;
 		struct irq_desc *desc;
 		struct irq_cfg *cfg;
