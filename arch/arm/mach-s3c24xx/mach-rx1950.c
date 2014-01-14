84fd2236748b38ad9498ccf78f901a76e474eb98
Linus Walleij <linus.walleij@linaro.org>
2014-01-14 15:05:22 +0100

    ARM: s3c24xx: explicit dependency on <plat/gpio-cfg.h>
    Previously the custom GPIO header for the S3C24xx would in turn
    bring in the custom pin control implementation from
    <plat/gpio-cfg.h>. This is not good as it mixes up two
    subsystems and makes the dependencies hard to track. Make
    the dependency explicit by explicitly including the pin
    control header where needed.
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Tomasz Figa <tomasz.figa@gmail.com>
    Cc: Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
    Cc: Ben Dooks <ben-linux@fluff.org>
    Cc: Kukjin Kim <kgene.kim@samsung.com>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: linux-samsung-soc@vger.kernel.org
    Acked-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-s3c24xx/mach-rx1950.c
+++ arch/arm/mach-s3c24xx/mach-rx1950.c
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/pm.h>
 #include <plat/regs-serial.h>
 #include <plat/samsung-time.h>
+#include <plat/gpio-cfg.h>
 
 #include "common.h"
 #include "h1940.h"
--- drivers/mmc/host/s3cmci.c
+++ drivers/mmc/host/s3cmci.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/io.h>
 
+#include <plat/gpio-cfg.h>
 #include <mach/dma.h>
 #include <mach/gpio-samsung.h>
 
