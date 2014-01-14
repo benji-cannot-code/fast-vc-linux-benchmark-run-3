20461502cb3a83703bfe51bfa47e0a6c058d52f8
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-01-13 16:19:00 -0800

    lustre: delete linux/lustre_debug.h
    There was only one macro in here, LL_CDEBUG_PAGE().  So open code it in
    the 2 places it was used so that the developer seeing it can see just
    how horrid it really is, and they can remove it as I doubt it really is
    ever needed anymore.  But for now, keep the same functionality and
    remove a whole file (with a duplicate name of other files in the lustre
    repo, another confusion...)
    
    Cc: Peng Tao <bergwolf@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre_debug.h
+++ drivers/staging/lustre/lustre/include/lustre_debug.h
@@ -46,8 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <lustre_net.h>
 #include <obd.h>
 
-#include <linux/lustre_debug.h>
-
 /* lib/debug.c */
 void dump_lniobuf(struct niobuf_local *lnb);
 int dump_req(struct ptlrpc_request *req);
