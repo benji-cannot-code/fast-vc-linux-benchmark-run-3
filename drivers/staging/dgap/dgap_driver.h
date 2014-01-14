6f4edecb3d56bb3819e19767d8f9cfb9408e0e90
Masanari Iida <standby24x7@gmail.com>
2014-01-15 00:40:46 +0900

    staging: dgap: Fix trailing whitespace in dgap_driver.h
    This patch fixed trailing whitespace found by checkpatch.pl
    in staging/dgap/dgap_driver.h
    
    Signed-off-by: Masanari Iida <standby24x7@gmail.com>
    Acked-by: Lidza Louina <Lidza.Louina@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_driver.h
+++ drivers/staging/dgap/dgap_driver.h
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define	PROCSTR		"dgap"			/* /proc entries	 */
 #define	DEVSTR		"/dev/dg/dgap"		/* /dev entries		 */
-#define	DRVSTR		"dgap"			/* Driver name string 
+#define	DRVSTR		"dgap"			/* Driver name string
 						 * displayed by APR	 */
 #define	APR(args)	do { PRINTF_TO_KMEM(args); printk(DRVSTR": "); printk args; \
 			   } while (0)
@@ -113,7 +113,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #if defined TRC_TO_KMEM
-#define PRINTF_TO_KMEM(args) dgap_tracef args 
+#define PRINTF_TO_KMEM(args) dgap_tracef args
 #else //!defined TRC_TO_KMEM
 #define PRINTF_TO_KMEM(args)
 #endif
@@ -193,7 +193,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Our major for the mgmt devices.
  *
  * We can use 22, because Digi was allocated 22 and 23 for the epca driver.
- * 22 has now become obsolete now that the "cu" devices have 
+ * 22 has now become obsolete now that the "cu" devices have
  * been removed from 2.6.
  * Also, this *IS* the epca driver, just PCI only now.
  */
@@ -291,7 +291,7 @@ extern char *dgap_state_text[];
 extern char *dgap_driver_state_text[];
 
 
-/* 
+/*
  * Modem line constants are defined as macros because DSR and
  * DCD are swapable using the ditty altpin option.
  */
@@ -323,7 +323,7 @@ struct macounter
 };
 
 
-/************************************************************************ 
+/************************************************************************
  * Device flag definitions for bd_flags.
  ************************************************************************/
 #define	BD_FEP5PLUS	0x0001          /* Supports FEP5 Plus commands */
@@ -341,7 +341,7 @@ struct board_t
 
 	int		type;		/* Type of board */
 	char		*name;		/* Product Name */
-	struct pci_dev	*pdev;		/* Pointer to the pci_dev struct */ 
+	struct pci_dev	*pdev;		/* Pointer to the pci_dev struct */
 	u16		vendor;		/* PCI vendor ID */
 	u16		device;		/* PCI device ID */
 	u16		subvendor;	/* PCI subsystem vendor ID */
@@ -420,7 +420,7 @@ struct board_t
 
 
 
-/************************************************************************ 
+/************************************************************************
  * Unit flag definitions for un_flags.
  ************************************************************************/
 #define UN_ISOPEN	0x0001		/* Device is open		*/
@@ -440,7 +440,7 @@ struct board_t
 struct device;
 
 /************************************************************************
- * Structure for terminal or printer unit. 
+ * Structure for terminal or printer unit.
  ************************************************************************/
 struct un_t {
 	int	magic;		/* Unit Magic Number.			*/
@@ -458,7 +458,7 @@ struct un_t {
 };
 
 
-/************************************************************************ 
+/************************************************************************
  * Device flag definitions for ch_flags.
  ************************************************************************/
 #define CH_PRON         0x0001          /* Printer on string                */
@@ -485,7 +485,7 @@ struct un_t {
 #define SNIFF_WAIT_SPACE 0x4
 
 
-/************************************************************************ 
+/************************************************************************
  * Channel information structure.
  ************************************************************************/
 struct channel_t {
@@ -609,7 +609,7 @@ extern int		dgap_registerttyswithsysfs; /* Should we register the	*/
  * Global functions declared in dgap_fep5.c, but must be hidden from
  * user space programs.
  */
-extern void	dgap_poll_tasklet(unsigned long data); 
+extern void	dgap_poll_tasklet(unsigned long data);
 extern void	dgap_cmdb(struct channel_t *ch, uchar cmd, uchar byte1, uchar byte2, uint ncmds);
 extern void	dgap_cmdw(struct channel_t *ch, uchar cmd, u16 word, uint ncmds);
 extern void	dgap_wmove(struct channel_t *ch, char *buf, uint cnt);
