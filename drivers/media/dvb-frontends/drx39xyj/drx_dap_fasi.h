416aa08e32565a3e355a52375897a28b33b16f9c
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-01-16 11:08:15 -0300

    [media] drx-j: Use checkpatch --fix to solve several issues
    Instead of manually fixing the issues, use the --fix experimental
    checkpatch. That solves a bunch of checkpatch issues.
    
    Acked-by: Devin Heitmueller <dheitmueller@kernellabs.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/dvb-frontends/drx39xyj/drx_dap_fasi.h
+++ drivers/media/dvb-frontends/drx39xyj/drx_dap_fasi.h
@@ -84,18 +84,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 
 /* set default */
-#if !defined( DRXDAPFASI_LONG_ADDR_ALLOWED )
+#if !defined(DRXDAPFASI_LONG_ADDR_ALLOWED)
 #define  DRXDAPFASI_LONG_ADDR_ALLOWED 1
 #endif
 
 /* set default */
-#if !defined( DRXDAPFASI_SHORT_ADDR_ALLOWED )
+#if !defined(DRXDAPFASI_SHORT_ADDR_ALLOWED)
 #define  DRXDAPFASI_SHORT_ADDR_ALLOWED 1
 #endif
 
 /* check */
-#if ( ( DRXDAPFASI_LONG_ADDR_ALLOWED==0 ) && \
-      ( DRXDAPFASI_SHORT_ADDR_ALLOWED==0 ) )
+#if (( DRXDAPFASI_LONG_ADDR_ALLOWED == 0) && \
+      (DRXDAPFASI_SHORT_ADDR_ALLOWED == 0) )
 #error  At least one of short- or long-addressing format must be allowed.
 *;				/* illegal statement to force compiler error */
 #endif
@@ -177,12 +177,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 
 /* set default */
-#if !defined( DRXDAP_MAX_WCHUNKSIZE)
+#if !defined(DRXDAP_MAX_WCHUNKSIZE)
 #define  DRXDAP_MAX_WCHUNKSIZE 254
 #endif
 
 /* check */
-#if ( (DRXDAPFASI_LONG_ADDR_ALLOWED==0)&&(DRXDAPFASI_SHORT_ADDR_ALLOWED==1) )
+#if ((DRXDAPFASI_LONG_ADDR_ALLOWED == 0) && (DRXDAPFASI_SHORT_ADDR_ALLOWED == 1))
 #if DRXDAP_SINGLE_MASTER
 #define  DRXDAP_MAX_WCHUNKSIZE_MIN 3
 #else
@@ -197,7 +197,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #if  DRXDAP_MAX_WCHUNKSIZE <  DRXDAP_MAX_WCHUNKSIZE_MIN
-#if ( (DRXDAPFASI_LONG_ADDR_ALLOWED==0)&&(DRXDAPFASI_SHORT_ADDR_ALLOWED==1) )
+#if ((DRXDAPFASI_LONG_ADDR_ALLOWED == 0) && (DRXDAPFASI_SHORT_ADDR_ALLOWED == 1))
 #if DRXDAP_SINGLE_MASTER
 #error  DRXDAP_MAX_WCHUNKSIZE must be at least 3 in single master mode
 *;				/* illegal statement to force compiler error */
@@ -217,7 +217,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 /* set default */
-#if !defined( DRXDAP_MAX_RCHUNKSIZE)
+#if !defined(DRXDAP_MAX_RCHUNKSIZE)
 #define  DRXDAP_MAX_RCHUNKSIZE 254
 #endif
 
@@ -250,13 +250,13 @@ extern "C" {
 #define DRXDAP_FASI_MODEFLAGS     0xC0000000
 #define DRXDAP_FASI_FLAGS         0xF0000000
 
-#define DRXDAP_FASI_ADDR2BLOCK( addr )  (((addr)>>22)&0x3F)
-#define DRXDAP_FASI_ADDR2BANK( addr )   (((addr)>>16)&0x3F)
-#define DRXDAP_FASI_ADDR2OFFSET( addr ) ((addr)&0x7FFF)
+#define DRXDAP_FASI_ADDR2BLOCK(addr)  (((addr)>>22)&0x3F)
+#define DRXDAP_FASI_ADDR2BANK(addr)   (((addr)>>16)&0x3F)
+#define DRXDAP_FASI_ADDR2OFFSET(addr) ((addr)&0x7FFF)
 
-#define DRXDAP_FASI_SHORT_FORMAT( addr )     (((addr)& 0xFC30FF80)==0)
-#define DRXDAP_FASI_LONG_FORMAT( addr )      (((addr)& 0xFC30FF80)!=0)
-#define DRXDAP_FASI_OFFSET_TOO_LARGE( addr ) (((addr)& 0x00008000)!=0)
+#define DRXDAP_FASI_SHORT_FORMAT(addr)     (((addr) & 0xFC30FF80) == 0)
+#define DRXDAP_FASI_LONG_FORMAT(addr)      (((addr) & 0xFC30FF80) != 0)
+#define DRXDAP_FASI_OFFSET_TOO_LARGE(addr) (((addr) & 0x00008000) != 0)
 
 #ifdef __cplusplus
 }
