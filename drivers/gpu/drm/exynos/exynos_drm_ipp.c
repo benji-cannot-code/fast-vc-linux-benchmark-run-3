80a135f327af9695d4341b146fa5b2c196207bc3
Tushar Behera <tushar.behera@linaro.org>
2014-01-16 11:57:57 +0530

    drm/exynos: Fix multiplatform breakage for ipp/gsc
    There is no need to include "plat/map-base.h" in ipp driver. Remove
    this and enable this driver for multi-platform.
    
    However gsc driver is not multiplatform compliant yet, so make the
    compilation conditional upon !ARCH_MULTIPLATFORM.
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_ipp.c
+++ drivers/gpu/drm/exynos/exynos_drm_ipp.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/clk.h>
 #include <linux/pm_runtime.h>
-#include <plat/map-base.h>
 
 #include <drm/drmP.h>
 #include <drm/exynos_drm.h>
