53fe20d480dd11b2d4c068221ff425749a6b7ed0
Tejun Heo <tj@kernel.org>
2014-01-17 09:57:49 -0500

    kernfs: add struct dentry declaration in kernfs.h
    Hello, Greg.
    
    Two misc fixes for kernfs.
    
    Thanks.
    ------- 8< -------
    struct dentry is used in kernfs.h but its declaration was missing,
    leading to compilation errors unless its declaration gets pulled in in
    some other way.  Add the declaration.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/kernfs.h
+++ include/linux/kernfs.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/completion.h>
 
 struct file;
+struct dentry;
 struct iattr;
 struct seq_file;
 struct vm_area_struct;
