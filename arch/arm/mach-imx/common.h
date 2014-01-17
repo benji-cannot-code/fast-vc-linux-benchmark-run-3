d2a74d25ffa4f390e029904f4cf6b214b7e4e912
Anson Huang <b20788@freescale.com>
2014-01-17 11:39:05 +0800

    ARM: imx: add suspend in ocram support for i.mx6q
    When system enter suspend, we can set the DDR IO to
    high-Z state to save DDR IOs' power consumption, this
    operation can save many power(from ~26mA@1.5V to ~15mA@1.5V,
    measured on i.MX6Q SabreSD board, R25) of DDR IOs. To
    achieve that, we need to copy the suspend code to ocram
    and run the low level hardware related code(set DDR IOs
    to high-Z state) in ocram.
    
    If there is no ocram space available, then system will
    still do suspend in external DDR, hence no DDR IOs will
    be set to high-Z.
    
    The OCRAM usage layout is as below,
    
    ocram suspend region(4K currently):
    ======================== high address ======================
                                  .
                                  .
                                  .
                                  ^
                                  ^
                                  ^
                          imx6_suspend code
                 PM_INFO structure(imx6_cpu_pm_info)
    ======================== low address =======================
    
    Reviewed-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Anson Huang <b20788@freescale.com>
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- arch/arm/mach-imx/common.h
+++ arch/arm/mach-imx/common.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright 2004-2013 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2004-2014 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -146,8 +146,12 @@ void imx6sl_set_wait_clk(bool enter);
 void imx_cpu_die(unsigned int cpu);
 int imx_cpu_kill(unsigned int cpu);
 
+void imx6_suspend(void __iomem *ocram_vbase);
 void imx6q_pm_init(void);
+void imx6dl_pm_init(void);
+void imx6sl_pm_init(void);
 void imx6q_pm_set_ccm_base(void __iomem *base);
+
 #ifdef CONFIG_PM
 void imx5_pm_init(void);
 #else
