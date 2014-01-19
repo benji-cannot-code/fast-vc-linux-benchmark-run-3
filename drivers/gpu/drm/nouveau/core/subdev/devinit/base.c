22efe8eafebfba0db86e1a1fae88730ef67f0b1c
Ilia Mirkin <imirkin@alum.mit.edu>
2014-01-19 04:18:15 -0500

    drm/nouveau/devinit: lock/unlock crtc regs for all devices, not just pre-nv50
    Also make nv_lockvgac work for nv50+ devices. This should fix
    IO_CONDITION and related VBIOS opcodes that read/write the crtc regs.
    
    See https://bugs.freedesktop.org/show_bug.cgi?id=60680
    
    Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/devinit/base.c
+++ drivers/gpu/drm/nouveau/core/subdev/devinit/base.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <subdev/bios.h>
 #include <subdev/bios/init.h>
+#include <subdev/vga.h>
 
 #include "priv.h"
 
@@ -38,6 +39,9 @@ _nouveau_devinit_fini(struct nouveau_object *object, bool suspend)
 	/* force full reinit on resume */
 	if (suspend)
 		devinit->post = true;
+
+	/* unlock the extended vga crtc regs */
+	nv_lockvgac(devinit, false);
 
 	return nouveau_subdev_fini(&devinit->base, suspend);
 }
@@ -60,6 +64,17 @@ _nouveau_devinit_init(struct nouveau_object *object)
 	if (impl->disable)
 		nv_device(devinit)->disable_mask |= impl->disable(devinit);
 	return 0;
+}
+
+void
+_nouveau_devinit_dtor(struct nouveau_object *object)
+{
+	struct nouveau_devinit *devinit = (void *)object;
+
+	/* lock crtc regs */
+	nv_lockvgac(devinit, true);
+
+	nouveau_subdev_destroy(&devinit->base);
 }
 
 int
