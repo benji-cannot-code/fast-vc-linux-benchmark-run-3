14ca4a1b3891dcd25f917e95c7ef887104bda4aa
Kumar Gala <galak@codeaurora.org>
2014-01-21 17:14:10 -0600

    ARM: qcom: Split Qualcomm support into legacy and multiplatform
    Introduce a new mach-qcom that will support SoCs that intend to be
    multiplatform compatible while keeping mach-msm to legacy SoC/board
    support that will not transition over to multiplatform.
    
    As part of this, we move support for MSM8X60, MSM8960 and MSM8974 over
    to mach-qcom.
    
    Signed-off-by: Kumar Gala <galak@codeaurora.org>

--- arch/arm/mach-qcom/platsmp.c
+++ arch/arm/mach-qcom/platsmp.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Copyright (C) 2002 ARM Ltd.
  *  All Rights Reserved
  *  Copyright (c) 2010, Code Aurora Forum. All rights reserved.
+ *  Copyright (c) 2014 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -20,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/smp_plat.h>
 
 #include "scm-boot.h"
-#include "common.h"
 
 #define VDD_SC1_ARRAY_CLAMP_GFS_CTL 0x15A0
 #define SCSS_CPU1CORE_RESET 0xD80
