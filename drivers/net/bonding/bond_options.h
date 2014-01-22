24dbed9788eb4ed4ced9c1d086f503e4ac4ec562
Nikolay Aleksandrov <nikolay@redhat.com>
2014-01-22 14:53:24 +0100

    bonding: convert arp_ip_target to use the new option API
    This patch adds the necessary changes so arp_ip_target would use
    the new bonding option API. This option is an exception because of
    the way it's currently implemented that's why its netlink code is
    a bit different from the other options to keep the functionality as
    before and at the same time to have a single set function.
    
    This patch also fixes a few stylistic errors.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_options.h
+++ drivers/net/bonding/bond_options.h
@@ -46,6 +46,7 @@ enum {
 	BOND_OPT_ARP_ALL_TARGETS,
 	BOND_OPT_FAIL_OVER_MAC,
 	BOND_OPT_ARP_INTERVAL,
+	BOND_OPT_ARP_TARGETS,
 	BOND_OPT_LAST
 };
 
@@ -118,4 +119,7 @@ int bond_option_fail_over_mac_set(struct bonding *bond,
 				  struct bond_opt_value *newval);
 int bond_option_arp_interval_set(struct bonding *bond,
 				 struct bond_opt_value *newval);
+int bond_option_arp_ip_targets_set(struct bonding *bond,
+				   struct bond_opt_value *newval);
+void bond_option_arp_ip_targets_clear(struct bonding *bond);
 #endif /* _BOND_OPTIONS_H */
