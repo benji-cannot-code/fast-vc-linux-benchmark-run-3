b76afa75d57c3c60fd4e6779d5236023ca8300bc
Nikolay Aleksandrov <nikolay@redhat.com>
2014-01-22 14:53:29 +0100

    bonding: convert ad_select to use the new option API
    This patch adds the necessary changes so ad_select would use
    the new bonding option API.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_options.h
+++ drivers/net/bonding/bond_options.h
@@ -51,6 +51,7 @@ enum {
 	BOND_OPT_UPDELAY,
 	BOND_OPT_LACP_RATE,
 	BOND_OPT_MINLINKS,
+	BOND_OPT_AD_SELECT,
 	BOND_OPT_LAST
 };
 
@@ -133,5 +134,7 @@ int bond_option_updelay_set(struct bonding *bond,
 int bond_option_lacp_rate_set(struct bonding *bond,
 			      struct bond_opt_value *newval);
 int bond_option_min_links_set(struct bonding *bond,
+			      struct bond_opt_value *newval);
+int bond_option_ad_select_set(struct bonding *bond,
 			      struct bond_opt_value *newval);
 #endif /* _BOND_OPTIONS_H */
