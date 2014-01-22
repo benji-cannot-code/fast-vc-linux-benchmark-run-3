88ebff89be8042289c03509976b3f9b5999dc37e
Nikolay Aleksandrov <nikolay@redhat.com>
2014-01-22 14:53:30 +0100

    bonding: convert num_peer_notif to use the new option API
    This patch adds the necessary changes so num_peer_notif would use
    the new bonding option API.
    When the auto-sysfs generation is done an alias should be added for
    this option as there're currently 2 entries in sysfs for it.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bond_options.h
+++ drivers/net/bonding/bond_options.h
@@ -52,6 +52,7 @@ enum {
 	BOND_OPT_LACP_RATE,
 	BOND_OPT_MINLINKS,
 	BOND_OPT_AD_SELECT,
+	BOND_OPT_NUM_PEER_NOTIF,
 	BOND_OPT_LAST
 };
 
@@ -137,4 +138,6 @@ int bond_option_min_links_set(struct bonding *bond,
 			      struct bond_opt_value *newval);
 int bond_option_ad_select_set(struct bonding *bond,
 			      struct bond_opt_value *newval);
+int bond_option_num_peer_notif_set(struct bonding *bond,
+				   struct bond_opt_value *newval);
 #endif /* _BOND_OPTIONS_H */
