c2157ad522bc202baad43329ae5f01ebacfc3425
Jamie Liu <jamieliu@google.com>
2014-01-23 15:53:40 -0800

    mm/swapfile.c: do not skip lowest_bit in scan_swap_map() scan loop
    In the second half of scan_swap_map()'s scan loop, offset is set to
    si->lowest_bit and then incremented before entering the loop for the
    first time, causing si->swap_map[si->lowest_bit] to be skipped.
    
    Signed-off-by: Jamie Liu <jamieliu@google.com>
    Cc: Shaohua Li <shli@fusionio.com>
    Acked-by: Hugh Dickins <hughd@google.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Akinobu Mita <akinobu.mita@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -617,7 +617,7 @@ scan:
 		}
 	}
 	offset = si->lowest_bit;
-	while (++offset < scan_base) {
+	while (offset < scan_base) {
 		if (!si->swap_map[offset]) {
 			spin_lock(&si->lock);
 			goto checks;
@@ -630,6 +630,7 @@ scan:
 			cond_resched();
 			latency_ration = LATENCY_LIMIT;
 		}
+		offset++;
 	}
 	spin_lock(&si->lock);
 
