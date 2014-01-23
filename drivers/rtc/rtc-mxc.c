9aa83d2279f2d12fc30de645f2f8c364d0b3ab8b
Fabio Estevam <fabio.estevam@freescale.com>
2014-01-23 15:55:04 -0800

    drivers/rtc/rtc-mxc.c: remove unneeded label
    There is no need to jump to the 'exit_free_pdata' label when
    devm_clk_get() fails, as we can directly return the error and simplify the
    code a bit.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-mxc.c
+++ drivers/rtc/rtc-mxc.c
@@ -392,8 +392,7 @@ static int mxc_rtc_probe(struct platform_device *pdev)
 	pdata->clk = devm_clk_get(&pdev->dev, NULL);
 	if (IS_ERR(pdata->clk)) {
 		dev_err(&pdev->dev, "unable to get clock!\n");
-		ret = PTR_ERR(pdata->clk);
-		goto exit_free_pdata;
+		return PTR_ERR(pdata->clk);
 	}
 
 	clk_prepare_enable(pdata->clk);
@@ -447,8 +446,6 @@ static int mxc_rtc_probe(struct platform_device *pdev)
 
 exit_put_clk:
 	clk_disable_unprepare(pdata->clk);
-
-exit_free_pdata:
 
 	return ret;
 }
