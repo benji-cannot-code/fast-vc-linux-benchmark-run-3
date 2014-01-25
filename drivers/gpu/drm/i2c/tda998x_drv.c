ba5b02ba6163a1ddc79ef4d881e28dabd40dc95b
Jean-Francois Moine <moinejf@free.fr>
2014-01-25 18:14:39 +0100

    drm/i2c: tda998x: use ALSA IEC958 definitions and update audio frequency
    This patch sets the frequency as 'not indicated' instead of '48kHz'
    and uses the asound values in the channel status definition.
    
    Tested-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Jean-Francois Moine <moinejf@free.fr>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/gpu/drm/i2c/tda998x_drv.c
+++ drivers/gpu/drm/i2c/tda998x_drv.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/hdmi.h>
 #include <linux/module.h>
+#include <sound/asoundef.h>
 
 #include <drm/drmP.h>
 #include <drm/drm_crtc_helper.h>
@@ -650,10 +651,11 @@ tda998x_configure_audio(struct drm_encoder *encoder,
 	reg_clear(encoder, REG_AIP_CNTRL_0, AIP_CNTRL_0_RST_CTS);
 
 	/* Write the channel status */
-	buf[0] = 0x04;
+	buf[0] = IEC958_AES0_CON_NOT_COPYRIGHT;
 	buf[1] = 0x00;
-	buf[2] = 0x00;
-	buf[3] = 0xf1;
+	buf[2] = IEC958_AES3_CON_FS_NOTID;
+	buf[3] = IEC958_AES4_CON_ORIGFS_NOTID |
+			IEC958_AES4_CON_MAX_WORDLEN_24;
 	reg_write_range(encoder, REG_CH_STAT_B(0), buf, 4);
 
 	tda998x_audio_mute(encoder, true);
