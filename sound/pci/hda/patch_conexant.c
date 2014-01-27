4b001e06cfca4f250b4b385fcd2458e229d58de6
Takashi Iwai <tiwai@suse.de>
2014-01-27 18:12:55 +0100

    ALSA: hda/conexant - Disable static quirks
    Now all weird setups have been converted to fixups for the generic
    parser, and we can disable the static quirks.  This commit just turns
    the build off.  The bulky static quirk code still remains for a while,
    in case we get an overlooked regression.  It'll be removed at the next
    kernel version.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_conexant.c
+++ sound/pci/hda/patch_conexant.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hda_jack.h"
 #include "hda_generic.h"
 
-#define ENABLE_CXT_STATIC_QUIRKS
+#undef ENABLE_CXT_STATIC_QUIRKS
 
 #define CXT_PIN_DIR_IN              0x00
 #define CXT_PIN_DIR_OUT             0x01
