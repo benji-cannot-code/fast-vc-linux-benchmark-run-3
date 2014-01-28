c48062e62ed1de37b5c2521ea4dedb55933079ad
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2014-01-28 13:18:28 +0100

    mfd: max14577: Add of_compatible to extcon mfd_cell
    Add of_compatible ("maxim,max14577-muic") to the mfd_cell for extcon
    driver. If entry with such compatible is present in the DTS, the extcon
    driver will have of_node set.
    
    This may be useful for extcon consumers and it is documented in
    bindings documentation.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/max14577.c
+++ drivers/mfd/max14577.c
@@ -26,7 +26,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mfd/max14577-private.h>
 
 static struct mfd_cell max14577_devs[] = {
-	{ .name = "max14577-muic", },
+	{
+		.name = "max14577-muic",
+		.of_compatible = "maxim,max14577-muic",
+	},
 	{
 		.name = "max14577-regulator",
 		.of_compatible = "maxim,max14577-regulator",
