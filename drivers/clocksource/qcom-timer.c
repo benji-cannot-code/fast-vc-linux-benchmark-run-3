1119fc0df946276c5b60cfcafb1e56c1a9505344
Kumar Gala <galak@codeaurora.org>
2014-01-29 16:17:30 -0600

    clocksource: qcom: Move clocksource code out of mach-msm
    We intend to share the clocksource code for MSM platforms between legacy
    and multiplatform supported qcom SoCs.
    
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Kumar Gala <galak@codeaurora.org>

--- drivers/clocksource/qcom-timer.c
+++ drivers/clocksource/qcom-timer.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2009-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2012,2014, The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/sched_clock.h>
-
-#include <asm/mach/time.h>
-
-#include "common.h"
 
 #define TIMER_MATCH_VAL			0x0000
 #define TIMER_COUNT_VAL			0x0004
