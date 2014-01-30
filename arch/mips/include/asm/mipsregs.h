f441dbe9426ad375031ce104af48fff3301c2a58
Markos Chandras <markos.chandras@imgtec.com>
2014-01-30 17:21:29 +0000

    MIPS: mm: c-r4k: Detect instruction cache aliases
    The *Aptiv cores can use the CONF7/IAR bit to detect if the core
    has hardware support to remove instruction cache aliasing.
    
    This also defines the CONF7/AR bit in order to avoid using
    the '16' magic number.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/6499/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mipsregs.h
+++ arch/mips/include/asm/mipsregs.h
@@ -654,6 +654,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MIPS_CONF7_RPS		(_ULCAST_(1) << 2)
 
+#define MIPS_CONF7_IAR		(_ULCAST_(1) << 10)
+#define MIPS_CONF7_AR		(_ULCAST_(1) << 16)
+
 /*  EntryHI bit definition */
 #define MIPS_ENTRYHI_EHINV	(_ULCAST_(1) << 10)
 
