3f1c42af7e9966dc3a2dd4667a63c08d00f07198
Minchan Kim <minchan@kernel.org>
2014-01-30 15:45:50 -0800

    zsmalloc: move it under mm
    This patch moves zsmalloc under mm directory.
    
    Before that, description will explain why we have needed custom
    allocator.
    
    Zsmalloc is a new slab-based memory allocator for storing compressed
    pages.  It is designed for low fragmentation and high allocation success
    rate on large object, but <= PAGE_SIZE allocations.
    
    zsmalloc differs from the kernel slab allocator in two primary ways to
    achieve these design goals.
    
    zsmalloc never requires high order page allocations to back slabs, or
    "size classes" in zsmalloc terms.  Instead it allows multiple
    single-order pages to be stitched together into a "zspage" which backs
    the slab.  This allows for higher allocation success rate under memory
    pressure.
    
    Also, zsmalloc allows objects to span page boundaries within the zspage.
    This allows for lower fragmentation than could be had with the kernel
    slab allocator for objects between PAGE_SIZE/2 and PAGE_SIZE.  With the
    kernel slab allocator, if a page compresses to 60% of it original size,
    the memory savings gained through compression is lost in fragmentation
    because another object of the same size can't be stored in the leftover
    space.
    
    This ability to span pages results in zsmalloc allocations not being
    directly addressable by the user.  The user is given an
    non-dereferencable handle in response to an allocation request.  That
    handle must be mapped, using zs_map_object(), which returns a pointer to
    the mapped region that can be used.  The mapping is necessary since the
    object data may reside in two different noncontigious pages.
    
    The zsmalloc fulfills the allocation needs for zram perfectly
    
    [sjenning@linux.vnet.ibm.com: borrow Seth's quote]
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Nitin Gupta <ngupta@vflare.org>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Bob Liu <bob.liu@oracle.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Luigi Semenzato <semenzato@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Seth Jennings <sjenning@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -91,8 +91,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/hardirq.h>
 #include <linux/spinlock.h>
 #include <linux/types.h>
-
-#include "zsmalloc.h"
+#include <linux/zsmalloc.h>
 
 /*
  * This must be power of 2 and greater than of equal to sizeof(link_free).
