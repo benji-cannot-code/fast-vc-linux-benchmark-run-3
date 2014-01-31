3304fe3f34599f54a880ca60645ab8f3df5df903
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2014-01-31 01:32:42 +0100

    pinctrl: mvebu: kirkwood: provide generic mpp callbacks
    We want to get rid of passing register addresses to common pinctrl
    driver, so provide set/get callbacks that use generic mpp pins helper
    and will be used later.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Tested-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/pinctrl/mvebu/pinctrl-kirkwood.c
+++ drivers/pinctrl/mvebu/pinctrl-kirkwood.c
@@ -22,6 +22,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "pinctrl-mvebu.h"
 
+static void __iomem *mpp_base;
+
+static int kirkwood_mpp_ctrl_get(unsigned pid, unsigned long *config)
+{
+	return default_mpp_ctrl_get(mpp_base, pid, config);
+}
+
+static int kirkwood_mpp_ctrl_set(unsigned pid, unsigned long config)
+{
+	return default_mpp_ctrl_set(mpp_base, pid, config);
+}
+
 #define V(f6180, f6190, f6192, f6281, f6282, dx4122)	\
 	((f6180 << 0) | (f6190 << 1) | (f6192 << 2) |	\
 	 (f6281 << 3) | (f6282 << 4) | (dx4122 << 5))
