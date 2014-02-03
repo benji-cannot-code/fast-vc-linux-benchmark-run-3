ea41e07f0d89e0c320c836606528dc2a01a4ba7a
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2014-02-04 00:40:25 +0100

    ACPI / hotplug / PCI: Drop crit_sect locking
    After recent PCI core changes related to the rescan/remove locking,
    the code sections under crit_sect mutexes from ACPIPHP slot objects
    are always executed under the general PCI rescan/remove lock.
    For this reason, the crit_sect mutexes are simply redundant, so drop
    them.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Tested-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- drivers/pci/hotplug/acpiphp.h
+++ drivers/pci/hotplug/acpiphp.h
@@ -94,7 +94,6 @@ struct acpiphp_slot {
 	struct list_head funcs;		/* one slot may have different
 					   objects (i.e. for each function) */
 	struct slot *slot;
-	struct mutex crit_sect;
 
 	u8		device;		/* pci device# */
 	u32		flags;		/* see below */
