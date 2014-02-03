056ea03c0cc21d25ce071f54f42a6f1e9bdd51c7
Hauke Mehrtens <hauke@hauke-m.de>
2014-02-04 00:01:43 +0100

    ARM: BCM5301X: initial support for the BCM5301X/BCM470X SoCs with ARM CPU
    This patch adds support for the BCM5301X/BCM470X SoCs with an ARM CPUs.
    Currently just booting to a shell is working and nothing else, no
    Ethernet, wifi, flash, ...
    I have some pending patches to make Ethernet work for this device.
    Mostly device tree support for bcma is missing.
    
    This SoC is used in small office and home router with Broadcom SoCs
    it's internal name is Northstar. This code should support the BCM4707,
    BCM4708, BCM4709, BCM53010, BCM53011 and BCM53012 SoC. It uses one or
    two ARM Cortex A9 Cores, some highlights are 2 PCIe 2.0 controllers,
    4 Gigabit Ethernet MACs and a USB 3.0 host controller.
    
    This SoC uses a dual core CPU, but this is currently not implemented.
    More information about this SoC can be found here:
    http://www.anandtech.com/show/5925/broadcom-announces-bcm4708x-and-bcm5301x-socs-for-80211ac-routers
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Christian Daudt <bcm@fixthebug.org>
    Signed-off-by: Matt Porter <mporter@linaro.org>

---
+++ arch/arm/mach-bcm/bcm_5301x.c
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Broadcom BCM470X / BCM5301X ARM platform code.
+ *
+ * Copyright 2013 Hauke Mehrtens <hauke@hauke-m.de>
+ *
+ * Licensed under the GNU/GPL. See COPYING for details.
+ */
+#include <linux/of_platform.h>
+#include <asm/hardware/cache-l2x0.h>
+
+#include <asm/mach/arch.h>
+
+
+static void __init bcm5301x_dt_init(void)
+{
+	l2x0_of_init(0, ~0UL);
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+}
+
+static const char __initconst *bcm5301x_dt_compat[] = {
+	"brcm,bcm4708",
+	NULL,
+};
+
+DT_MACHINE_START(BCM5301X, "BCM5301X")
+	.init_machine	= bcm5301x_dt_init,
+	.dt_compat	= bcm5301x_dt_compat,
+MACHINE_END
