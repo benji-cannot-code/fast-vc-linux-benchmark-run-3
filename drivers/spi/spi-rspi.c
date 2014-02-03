d96001e10e60375a22486866f4e48398dcec9970
Shimoda, Yoshihiro <yoshihiro.shimoda.uh@renesas.com>
2014-02-03 10:43:46 +0900

    spi: rspi: fix build error when CONFIG_OF is not set
    This patch fixes an issue that the following build error happens when
    the CONFIG_OF is not set:
    
    drivers/spi/spi-rspi.c: In function 'rspi_probe':
    drivers/spi/spi-rspi.c:1203:26: error: 'rspi_of_match' undeclared (first use in this function)
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-rspi.c
+++ drivers/spi/spi-rspi.c
@@ -1165,6 +1165,7 @@ static int rspi_parse_dt(struct device *dev, struct spi_master *master)
 	return 0;
 }
 #else
+#define rspi_of_match	NULL
 static inline int rspi_parse_dt(struct device *dev, struct spi_master *master)
 {
 	return -EINVAL;
