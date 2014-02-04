682368d226f27e8198010fd3572c1bcebf951e45
Devesh Sharma <devesh.sharma@emulex.com>
2014-02-04 11:57:01 +0530

    RDMA/ocrdma: Increment abi version count
    Increment the ABI version count for driver/library interface.
    
    Signed-off-by: Devesh Sharma <devesh.sharma@emulex.com>
    Signed-off-by: Selvin Xavier <selvin.xavier@emulex.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/hw/ocrdma/ocrdma_abi.h
+++ drivers/infiniband/hw/ocrdma/ocrdma_abi.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __OCRDMA_ABI_H__
 #define __OCRDMA_ABI_H__
 
-#define OCRDMA_ABI_VERSION 1
+#define OCRDMA_ABI_VERSION 2
 #define OCRDMA_BE_ROCE_ABI_VERSION 1
 /* user kernel communication data structures. */
 
