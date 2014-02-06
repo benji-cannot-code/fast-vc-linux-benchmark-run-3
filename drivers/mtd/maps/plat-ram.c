2b4bb140d3b3059975deaa637cfa12b5619e81c2
Jingoo Han <jg1.han@samsung.com>
2014-02-06 15:12:15 +0900

    mtd: plat-ram: Remove unnecessary OOM messages
    The site-specific OOM messages are unnecessary, because they
    duplicate the MM subsystem generic OOM message.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/maps/plat-ram.c
+++ drivers/mtd/maps/plat-ram.c
@@ -138,7 +138,6 @@ static int platram_probe(struct platform_device *pdev)
 
 	info = kzalloc(sizeof(*info), GFP_KERNEL);
 	if (info == NULL) {
-		dev_err(&pdev->dev, "no memory for flash info\n");
 		err = -ENOMEM;
 		goto exit_error;
 	}
