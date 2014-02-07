c708fa1f63de3531d8c01b23a44a10a827c25b4a
Larry Finger <Larry.Finger@lwfinger.net>
2014-02-06 20:45:44 -0600

    staging: r8188eu: Remove ODM_AllocateMemory
    This wrapper for vzalloc() is not used.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/odm_interface.c
+++ drivers/staging/rtl8188eu/hal/odm_interface.c
@@ -21,12 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "odm_precomp.h"
 
-/*  ODM Memory relative API. */
-void ODM_AllocateMemory(struct odm_dm_struct *pDM_Odm, void **pPtr, u32 length)
-{
-	*pPtr = vzalloc(length);
-}
-
 s32 ODM_CompareMemory(struct odm_dm_struct *pDM_Odm, void *pBuf1, void *pBuf2, u32 length)
 {
 	return _rtw_memcmp(pBuf1, pBuf2, length);
