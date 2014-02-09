bf51ab81f458f788e4525414a3f8b7d0bb1121c5
navin patidar <navin.patidar@gmail.com>
2014-02-09 14:10:45 +0530

    staging: rtl8188eu: remove header file ip.h
    "ip.h" is included in four files but not being used, so
    remove "include/ip.h" header file and inclusion of this header file.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_recv.c
+++ drivers/staging/rtl8188eu/core/rtw_recv.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drv_types.h>
 #include <recv_osdep.h>
 #include <mlme_osdep.h>
-#include <ip.h>
 #include <ethernet.h>
 #include <usb_ops.h>
 #include <wifi.h>
--- drivers/staging/rtl8188eu/core/rtw_xmit.c
+++ drivers/staging/rtl8188eu/core/rtw_xmit.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drv_types.h>
 #include <wifi.h>
 #include <osdep_intf.h>
-#include <ip.h>
 #include <usb_ops.h>
 #include <usb_osintf.h>
 #include <linux/vmalloc.h>
