91ba9ffd85a5160e080c995ba6c708ad44f9c826
Peter Hurley <peter@hurleysoftware.com>
2014-02-09 20:59:17 -0500

    Bluetooth: Rename __rfcomm_dev_get() to __rfcomm_dev_lookup()
    Functions which search lists for matching id's are more
    commonly named *_lookup, which is the convention in the
    bluetooth core as well.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Tested-By: Alexander Holler <holler@ahsoftware.de>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/rfcomm/tty.c
+++ net/bluetooth/rfcomm/tty.c
@@ -141,7 +141,7 @@ static const struct tty_port_operations rfcomm_port_ops = {
 	.carrier_raised = rfcomm_dev_carrier_raised,
 };
 
-static struct rfcomm_dev *__rfcomm_dev_get(int id)
+static struct rfcomm_dev *__rfcomm_dev_lookup(int id)
 {
 	struct rfcomm_dev *dev;
 
@@ -158,7 +158,7 @@ static struct rfcomm_dev *rfcomm_dev_get(int id)
 
 	spin_lock(&rfcomm_dev_lock);
 
-	dev = __rfcomm_dev_get(id);
+	dev = __rfcomm_dev_lookup(id);
 
 	if (dev && !tty_port_get(&dev->port))
 		dev = NULL;
