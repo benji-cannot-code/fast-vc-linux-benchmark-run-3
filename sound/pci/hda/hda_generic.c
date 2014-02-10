ba8fdf6ac8f8e05f8452c0e10d1f45f3cfb34ef2
Takashi Iwai <tiwai@suse.de>
2014-02-10 18:23:57 +0100

    ALSA: hda - Make snd_hda_gen_spec_free() static
    The last user of snd_hda_gen_spec_free() is patch_via.c, and we can
    rewrite it safely with snd_hda_gen_free(), so that
    snd_hda_gen_spec_free() can be a local function in hda_generic.c.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_generic.c
+++ sound/pci/hda/hda_generic.c
@@ -80,7 +80,7 @@ static void free_kctls(struct hda_gen_spec *spec)
 	snd_array_free(&spec->kctls);
 }
 
-void snd_hda_gen_spec_free(struct hda_gen_spec *spec)
+static void snd_hda_gen_spec_free(struct hda_gen_spec *spec)
 {
 	if (!spec)
 		return;
@@ -88,7 +88,6 @@ void snd_hda_gen_spec_free(struct hda_gen_spec *spec)
 	snd_array_free(&spec->paths);
 	snd_array_free(&spec->loopback_list);
 }
-EXPORT_SYMBOL_GPL(snd_hda_gen_spec_free);
 
 /*
  * store user hints
