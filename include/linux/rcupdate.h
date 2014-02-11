fedc6a18504cbf55bdefc13c061ef1ed2b7eec12
Paul Gortmaker <paul.gortmaker@windriver.com>
2014-02-11 16:10:12 -0500

    rcu: Fix sparse warning for rcu_expedited from kernel/ksysfs.c
    This commit fixes the follwoing warning:
    
    kernel/ksysfs.c:143:5: warning: symbol 'rcu_expedited' was not declared. Should it be static?
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    [ paulmck: Moved the declaration to include/linux/rcupdate.h to avoid
    	   including the RCU-internal rcu.h file outside of RCU. ]
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <asm/barrier.h>
 
+extern int rcu_expedited; /* for sysctl */
 #ifdef CONFIG_RCU_TORTURE_TEST
 extern int rcutorture_runnable; /* for sysctl */
 #endif /* #ifdef CONFIG_RCU_TORTURE_TEST */
