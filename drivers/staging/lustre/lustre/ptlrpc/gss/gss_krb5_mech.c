d73d4c327cb62ce0c061d01f6543f39b95f6340c
Sachin Kamat <sachin.kamat@linaro.org>
2014-02-12 15:38:41 +0530

    staging: lustre: Remove duplicate inclusion of crypto.h
    crypto.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/gss/gss_krb5_mech.c
+++ drivers/staging/lustre/lustre/ptlrpc/gss/gss_krb5_mech.c
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/crypto.h>
 #include <linux/mutex.h>
-#include <linux/crypto.h>
 
 #include <obd.h>
 #include <obd_class.h>
