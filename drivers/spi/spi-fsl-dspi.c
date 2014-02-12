76384e73faf1149ac9c43385c868b0ac77992742
Chao Fu <B44548@freescale.com>
2014-02-12 15:29:06 +0800

    spi/fsl-dspi: Remove some coding sytle not in standard
    Remove some coding sytle not in standard in former code.
    
    Signed-off-by: Chao Fu <b44548@freescale.com>
    Reviewed-by: Xiubo Li <Li.Xiubo@freescale.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-fsl-dspi.c
+++ drivers/spi/spi-fsl-dspi.c
@@ -112,9 +112,9 @@ struct fsl_dspi {
 
 	struct regmap		*regmap;
 	int			irq;
-	struct clk 		*clk;
+	struct clk		*clk;
 
-	struct spi_transfer 	*cur_transfer;
+	struct spi_transfer	*cur_transfer;
 	struct chip_data	*cur_chip;
 	size_t			len;
 	void			*tx;
@@ -125,8 +125,8 @@ struct fsl_dspi {
 	u8			cs;
 	u16			void_write_data;
 
-	wait_queue_head_t 	waitq;
-	u32 			waitflags;
+	wait_queue_head_t	waitq;
+	u32			waitflags;
 };
 
 static inline int is_double_byte_mode(struct fsl_dspi *dspi)
