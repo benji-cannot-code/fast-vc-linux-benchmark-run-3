92a6b57271df0453d828e46dab203f0978738bb4
Magnus Damm <damm@opensource.se>
2014-02-13 17:26:08 +0900

    ARM: shmobile: Remove legacy EMEV2 SoC support
    Get rid of legacy EMEV2 SoC code including the legacy clock
    framework implementation. The multiplatform implementation
    together with DT board support shall be used instead.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/emev2.h
+++ arch/arm/mach-shmobile/include/mach/emev2.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern void emev2_map_io(void);
 extern void emev2_init_delay(void);
-extern void emev2_clock_init(void);
 extern struct smp_operations emev2_smp_ops;
 
 #endif /* __ASM_EMEV2_H__ */
