975d1ebe782490122303e2d804e5539ba6f8ef37
Joel Stanley <joel@jms.id.au>
2014-02-13 15:03:46 +1030

    tools/virtio: fix missing kmemleak_ignore symbol
    In commit bb478d8b167 virtio_ring: plug kmemleak false positive,
    kmemleak_ignore was introduced. This broke compilation of virtio_test:
    
      cc -g -O2 -Wall -I. -I ../../usr/include/ -Wno-pointer-sign
        -fno-strict-overflow -fno-strict-aliasing -fno-common -MMD
        -U_FORTIFY_SOURCE   -c -o virtio_ring.o ../../drivers/virtio/virtio_ring.c
      ../../drivers/virtio/virtio_ring.c: In function ‘vring_add_indirect’:
      ../../drivers/virtio/virtio_ring.c:177:2: warning: implicit declaration
      of function ‘kmemleak_ignore’ [-Wimplicit-function-declaration]
        kmemleak_ignore(desc);
        ^
      cc   virtio_test.o virtio_ring.o   -o virtio_test
      virtio_ring.o: In function `vring_add_indirect':
      tools/virtio/../../drivers/virtio/virtio_ring.c:177:
      undefined reference to `kmemleak_ignore'
    
    Add a dummy header for tools/virtio, and add #incldue <linux/kmemleak.h>
    to drivers/virtio/virtio_ring.c so it is picked up by the userspace
    tools.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/hrtimer.h>
+#include <linux/kmemleak.h>
 
 #ifdef DEBUG
 /* For development, we want to crash whenever the ring is screwed. */
