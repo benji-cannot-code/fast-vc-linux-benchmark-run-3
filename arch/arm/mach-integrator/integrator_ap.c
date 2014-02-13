a9ff3350f7b56b056a4351659a0e536d63bf3e98
Linus Walleij <linus.walleij@linaro.org>
2014-02-13 21:20:57 +0100

    ARM: integrator: merge platform.h to hardware.h
    There is no need to have the two separate headers <mach/platform.h>
    and <mach/hardware.h>, especially since we are now going to make them
    local files. There is not one single driver outside the mach-integrator
    folder referencing any of the files.
    
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Jonathan Austin <jonathan.austin@arm.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-integrator/integrator_ap.c
+++ arch/arm/mach-integrator/integrator_ap.c
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk-provider.h>
 
 #include <mach/hardware.h>
-#include <mach/platform.h>
 #include <asm/hardware/arm_timer.h>
 #include <asm/setup.h>
 #include <asm/param.h>		/* HZ */
--- arch/arm/mach-integrator/pci_v3.c
+++ arch/arm/mach-integrator/pci_v3.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <video/vga.h>
 
 #include <mach/hardware.h>
-#include <mach/platform.h>
 
 #include <asm/mach/map.h>
 #include <asm/signal.h>
