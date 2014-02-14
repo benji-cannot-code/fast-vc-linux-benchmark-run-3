cb22c0b80189c5c5e7b7cb972999d675debd4f7c
Roland Dreier <roland@purestorage.com>
2014-02-13 20:45:17 -0800

    mlx5: Add include of <linux/slab.h> because of kzalloc()/kfree() use
    On some architectures (for example, arm), we don't end up indirectly
    pulling in the declaration of kzalloc() and kfree(), and so building
    anything that includes <linux/mlx5/driver.h> breaks.  Fix this by adding
    an explicit include to get the declaration.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -39,8 +39,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/spinlock_types.h>
 #include <linux/semaphore.h>
+#include <linux/slab.h>
 #include <linux/vmalloc.h>
 #include <linux/radix-tree.h>
+
 #include <linux/mlx5/device.h>
 #include <linux/mlx5/doorbell.h>
 
