73ba996db40e2e46d5f2bf20f001993d9f7a5894
Florian Fainelli <f.fainelli@gmail.com>
2014-02-13 16:08:42 -0800

    net: phy: add MoCA PHY type
    Some Ethernet MACs are connected to a MoCA PHY which will handle the
    low-level job of sending Ethernet frames on the coaxial cable, these
    Ethernet MACs need to know about it to be properly configured.
    Add a new PHY mode "moca" and update the Device Tree parsing logic to
    look for it.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -75,6 +75,7 @@ typedef enum {
 	PHY_INTERFACE_MODE_RTBI,
 	PHY_INTERFACE_MODE_SMII,
 	PHY_INTERFACE_MODE_XGMII,
+	PHY_INTERFACE_MODE_MOCA,
 	PHY_INTERFACE_MODE_MAX,
 } phy_interface_t;
 
@@ -114,6 +115,8 @@ static inline const char *phy_modes(phy_interface_t interface)
 		return "smii";
 	case PHY_INTERFACE_MODE_XGMII:
 		return "xgmii";
+	case PHY_INTERFACE_MODE_MOCA:
+		return "moca";
 	default:
 		return "unknown";
 	}
