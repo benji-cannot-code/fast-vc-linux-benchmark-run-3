53a2bdb566a287e0229b9a7cba6f5c2525e6d701
Sebastian Reichel <sre@debian.org>
2014-02-15 00:05:53 +0100

    wl1251: move power GPIO handling into the driver
    Move the power GPIO handling from the board code into
    the driver. This is a dependency for device tree support.
    
    Signed-off-by: Sebastian Reichel <sre@debian.org>
    Reviewed-by: Pavel Machek <pavel@ucw.cz>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- include/linux/wl12xx.h
+++ include/linux/wl12xx.h
@@ -50,7 +50,7 @@ enum {
 };
 
 struct wl1251_platform_data {
-	void (*set_power)(bool enable);
+	int power_gpio;
 	/* SDIO only: IRQ number if WLAN_IRQ line is used, 0 for SDIO IRQs */
 	int irq;
 	bool use_eeprom;
