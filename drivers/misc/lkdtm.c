9b8087e37c0daf603dea2051eadafa1b2fe24be7
Kees Cook <keescook@chromium.org>
2014-02-14 15:58:50 -0800

    lkdtm: include cacheflush.h
    Some architectures need cacheflush.h explicitly included (mips) for
    use of flush_icache_range():
    
        config: make ARCH=mips allmodconfig
        All error/warnings:
        >> ERROR: "flush_icache_range" undefined!
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/lkdtm.c
+++ drivers/misc/lkdtm.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include <linux/vmalloc.h>
 #include <linux/mman.h>
+#include <asm/cacheflush.h>
 
 #ifdef CONFIG_IDE
 #include <linux/ide.h>
