e9bf0adb6d3d4299e1782d8f38d36f0a542c6cfe
Alexander Shiyan <shc_work@mail.ru>
2014-02-15 13:21:03 +0400

    watchdog: bcm63xx_wdt: Remove unused field from bcm63xx_wdt_device
    Field "default_ticks" is not used anywhere in the driver code.
    This patch removes this field.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/bcm63xx_wdt.c
+++ drivers/watchdog/bcm63xx_wdt.c
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct {
 	void __iomem *regs;
 	struct timer_list timer;
-	int default_ticks;
 	unsigned long inuse;
 	atomic_t ticks;
 } bcm63xx_wdt_device;
