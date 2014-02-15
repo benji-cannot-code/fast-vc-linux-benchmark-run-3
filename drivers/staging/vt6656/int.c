4b317fd51891e5725959b61c1304fcfbc85b46b7
Malcolm Priestley <tvboxspy@gmail.com>
2014-02-15 21:56:20 +0000

    staging: vt6656: Replace typedef enum _CARD_OP_MODE
    Replace typedef enum _CARD_OP_MODE eOPMode with op_mode enum nl80211_iftype
    
    enum changes
    OP_MODE_INFRASTRUCTURE -> NL80211_IFTYPE_STATION
    OP_MODE_AP -> NL80211_IFTYPE_AP
    OP_MODE_DEF -> NL80211_IFTYPE_UNSPECIFIED
    OP_MODE_ADHOC -> NL80211_IFTYPE_ADHOC
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/int.c
+++ drivers/staging/vt6656/int.c
@@ -132,7 +132,7 @@ void INTnsProcessData(struct vnt_private *pDevice)
 	}
 	if (pINTData->byISR0 != 0) {
 		if (pINTData->byISR0 & ISR_BNTX) {
-			if (pDevice->eOPMode == OP_MODE_AP) {
+			if (pDevice->op_mode == NL80211_IFTYPE_AP) {
 				if (pMgmt->byDTIMCount > 0) {
 					pMgmt->byDTIMCount--;
 					pMgmt->sNodeDBTable[0].bRxPSPoll =
@@ -150,7 +150,7 @@ void INTnsProcessData(struct vnt_private *pDevice)
 				bScheduleCommand((void *) pDevice,
 						WLAN_CMD_BECON_SEND,
 						NULL);
-			} /* if (pDevice->eOPMode == OP_MODE_AP) */
+			}
 		pDevice->bBeaconSent = true;
 		} else {
 			pDevice->bBeaconSent = false;
