b9051625c7caedf037556096cd004329d6ca9fd2
Naveen Krishna Chatradhi <ch.naveen@samsung.com>
2014-02-17 15:14:31 +0530

    clk: samsung: exynos5250/5420: Add gate clock for SSS module
    This patch adds gating clock for SSS(Security SubSystem)
    module on Exynos5250/5420.
    
    Signed-off-by: Naveen Krishna Chatradhi <ch.naveen@samsung.com>
    [t.figa: Fixed sort order and group name.]
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>

--- include/dt-bindings/clock/exynos5250.h
+++ include/dt-bindings/clock/exynos5250.h
@@ -151,6 +151,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_G2D			345
 #define CLK_MDMA0		346
 #define CLK_SMMU_MDMA0		347
+#define CLK_SSS			348
 
 /* mux clocks */
 #define CLK_MOUT_HDMI		1024
