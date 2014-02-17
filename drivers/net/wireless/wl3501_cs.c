9be54cfa1d59f28327575b932c9a3c924362fef0
Sachin Kamat <sachin.kamat@linaro.org>
2014-02-17 15:25:01 +0530

    net: wireless: wl3501_cs: Remove duplicate include
    linux/etherdevice.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wl3501_cs.c
+++ drivers/net/wireless/wl3501_cs.c
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/wireless.h>
 #include <linux/ieee80211.h>
-#include <linux/etherdevice.h>
 
 #include <net/iw_handler.h>
 
