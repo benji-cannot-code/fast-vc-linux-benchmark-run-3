3ad2abd6db9a8daa019c9ae8b90a685270554b0d
Sachin Kamat <sachin.kamat@linaro.org>
2014-02-17 15:29:21 +0530

    net: i40evf: Remove duplicate include
    linux/sctp.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/i40evf/i40evf.h
+++ drivers/net/ethernet/intel/i40evf/i40evf.h
@@ -39,8 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ipv6.h>
 #include <net/ip6_checksum.h>
 #include <net/udp.h>
-#include <linux/sctp.h>
-
 
 #include "i40e_type.h"
 #include "i40e_virtchnl.h"
