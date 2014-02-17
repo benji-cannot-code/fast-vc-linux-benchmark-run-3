768ad6643575fed7bb2c0c8bc17bb59b836ac720
Micky Ching <micky_ching@realsil.com.cn>
2014-02-17 16:45:46 +0800

    mmc: rtsx: fix card poweroff bug
    If the host driver removed while card in the slot, the host will not
    power off card power correctly. This bug is produced because host
    eject flag set before the last mmc_set_ios callback, we should set the
    eject flag after power off.
    
    Signed-off-by: Micky Ching <micky_ching@realsil.com.cn>
    Signed-off-by: Chris Ball <chris@printf.net>

--- drivers/mmc/host/rtsx_pci_sdmmc.c
+++ drivers/mmc/host/rtsx_pci_sdmmc.c
@@ -1298,7 +1298,6 @@ static int rtsx_pci_sdmmc_drv_remove(struct platform_device *pdev)
 	pcr->slots[RTSX_SD_CARD].p_dev = NULL;
 	pcr->slots[RTSX_SD_CARD].card_event = NULL;
 	mmc = host->mmc;
-	host->eject = true;
 
 	mutex_lock(&host->host_mutex);
 	if (host->mrq) {
@@ -1316,6 +1315,8 @@ static int rtsx_pci_sdmmc_drv_remove(struct platform_device *pdev)
 	mutex_unlock(&host->host_mutex);
 
 	mmc_remove_host(mmc);
+	host->eject = true;
+
 	mmc_free_host(mmc);
 
 	dev_dbg(&(pdev->dev),
