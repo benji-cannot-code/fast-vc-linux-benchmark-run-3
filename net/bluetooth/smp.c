fc6843fb92c68736d3f445baade9410aa256479f
Johan Hedberg <johan.hedberg@intel.com>
2014-02-18 10:19:37 +0200

    Bluetooth: Fix properly ignoring unexpected SMP PDUs
    If we didn't request certain pieces of information during the key
    distribution negotiation we should properly ignore those PDUs if the
    peer incorrectly sends them. This includes the Encryption Information
    and Master Identification PDUs if the EncKey bit was not set, and the
    Identity Information and Identity Address Information PDUs if the IdKey
    bit was not set.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/smp.c
+++ net/bluetooth/smp.c
@@ -892,6 +892,10 @@ static int smp_cmd_encrypt_info(struct l2cap_conn *conn, struct sk_buff *skb)
 	if (skb->len < sizeof(*rp))
 		return SMP_UNSPECIFIED;
 
+	/* Ignore this PDU if it wasn't requested */
+	if (!(smp->remote_key_dist & SMP_DIST_ENC_KEY))
+		return 0;
+
 	skb_pull(skb, sizeof(*rp));
 
 	memcpy(smp->tk, rp->ltk, sizeof(smp->tk));
@@ -911,6 +915,10 @@ static int smp_cmd_master_ident(struct l2cap_conn *conn, struct sk_buff *skb)
 
 	if (skb->len < sizeof(*rp))
 		return SMP_UNSPECIFIED;
+
+	/* Ignore this PDU if it wasn't requested */
+	if (!(smp->remote_key_dist & SMP_DIST_ENC_KEY))
+		return 0;
 
 	skb_pull(skb, sizeof(*rp));
 
@@ -936,6 +944,10 @@ static int smp_cmd_ident_info(struct l2cap_conn *conn, struct sk_buff *skb)
 	if (skb->len < sizeof(*info))
 		return SMP_UNSPECIFIED;
 
+	/* Ignore this PDU if it wasn't requested */
+	if (!(smp->remote_key_dist & SMP_DIST_ID_KEY))
+		return 0;
+
 	skb_pull(skb, sizeof(*info));
 
 	memcpy(smp->irk, info->irk, 16);
@@ -955,6 +967,10 @@ static int smp_cmd_ident_addr_info(struct l2cap_conn *conn,
 
 	if (skb->len < sizeof(*info))
 		return SMP_UNSPECIFIED;
+
+	/* Ignore this PDU if it wasn't requested */
+	if (!(smp->remote_key_dist & SMP_DIST_ID_KEY))
+		return 0;
 
 	skb_pull(skb, sizeof(*info));
 
