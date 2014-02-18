2345a7132635df92a7c7a188fd91cf755a6da8c7
Brad Volkin <bradley.d.volkin@intel.com>
2014-02-18 10:15:48 -0800

    drm/i915: Reject privileged commands
    The spec defines most of these commands as privileged. A few others,
    like the semaphore mbox command and some display commands, are also
    reserved for the driver's use. Subsequent patches relax some of
    these restrictions.
    
    v2: Rebased
    
    Signed-off-by: Brad Volkin <bradley.d.volkin@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -359,6 +359,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MI_PREDICATE            MI_INSTR(0x0C, 0)
 #define MI_RS_CONTEXT           MI_INSTR(0x0F, 0)
 #define MI_TOPOLOGY_FILTER      MI_INSTR(0x0D, 0)
+#define MI_LOAD_SCAN_LINES_EXCL MI_INSTR(0x13, 0)
 #define MI_URB_CLEAR            MI_INSTR(0x19, 0)
 #define MI_UPDATE_GTT           MI_INSTR(0x23, 0)
 #define MI_CLFLUSH              MI_INSTR(0x27, 0)
