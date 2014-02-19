2705990fd2be701784ec44d5fa38402fa93e14a4
Mark Hounschell <markh@compro.net>
2014-02-19 13:12:06 -0500

    staging: dgap: Merge dgap_fep5.h into dgap_driver.h
    There is a lot of cleanup work to do on these digi drivers and merging as
    much as is possible will make it easier. I also notice that many merged
    drivers are single source and header.
    
    Signed-off-by: Mark Hounschell <markh@compro.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap_driver.c
+++ drivers/staging/dgap/dgap_driver.c
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dgap_driver.h"
 #include "dgap_pci.h"
-#include "dgap_fep5.h"
 #include "dgap_conf.h"
 #include "dgap_parse.h"
 #include "dgap_types.h"
