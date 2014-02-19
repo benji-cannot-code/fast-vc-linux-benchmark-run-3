e4339490a28873f2a6a14217c3340eefaac8433f
Balaji T K <balajitk@ti.com>
2014-02-19 20:26:40 +0530

    mmc: omap_hsmmc: use devm_regulator API
    Use devm_regulator API, while at it use
    devm_regulator_get_optional for optional vmmc_aux supply
    
    Signed-off-by: Balaji T K <balajitk@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Tested-by: Florian Vaussard <florian.vaussard@epfl.ch>
    Tested-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Chris Ball <chris@printf.net>

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -322,7 +322,7 @@ static int omap_hsmmc_reg_get(struct omap_hsmmc_host *host)
 	struct regulator *reg;
 	int ocr_value = 0;
 
-	reg = regulator_get(host->dev, "vmmc");
+	reg = devm_regulator_get(host->dev, "vmmc");
 	if (IS_ERR(reg)) {
 		dev_err(host->dev, "vmmc regulator missing\n");
 		return PTR_ERR(reg);
@@ -342,7 +342,7 @@ static int omap_hsmmc_reg_get(struct omap_hsmmc_host *host)
 		}
 
 		/* Allow an aux regulator */
-		reg = regulator_get(host->dev, "vmmc_aux");
+		reg = devm_regulator_get_optional(host->dev, "vmmc_aux");
 		host->vcc_aux = IS_ERR(reg) ? NULL : reg;
 
 		/* For eMMC do not power off when not in sleep state */
@@ -372,8 +372,6 @@ static int omap_hsmmc_reg_get(struct omap_hsmmc_host *host)
 
 static void omap_hsmmc_reg_put(struct omap_hsmmc_host *host)
 {
-	regulator_put(host->vcc);
-	regulator_put(host->vcc_aux);
 	mmc_slot(host).set_power = NULL;
 }
 
