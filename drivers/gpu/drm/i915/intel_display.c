eafa0545e05f27fa6e732940122008d6e58a6df0
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-02-19 21:29:49 +0200

    drm: Add drm_vblank_on()
    drm_vblank_off() will turn off vblank interrupts, but as long as the
    refcount is elevated drm_vblank_get() will not re-enable them. This
    is a problem is someone is holding a vblank reference while a modeset is
    happening, and the driver requires vblank interrupt to work during that
    time.
    
    Add drm_vblank_on() as a counterpart to drm_vblank_off() which will
    re-enabled vblank interrupts if the refcount is already elevated. This
    will allow drivers to choose the specific places in the modeset sequence
    at which vblank interrupts get disabled and enabled.
    
    Testcase: igt/kms_flip/*-vs-suspend
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    [danvet: Add Testcase tag for the igt I've written.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -3738,6 +3738,8 @@ static void ironlake_crtc_enable(struct drm_crtc *crtc)
 	 * happening.
 	 */
 	intel_wait_for_vblank(dev, intel_crtc->pipe);
+
+	drm_vblank_on(dev, pipe);
 }
 
 /* IPS only exists on ULT machines and is tied to pipe A. */
@@ -3829,6 +3831,8 @@ static void haswell_crtc_enable(struct drm_crtc *crtc)
 	 * to change the workaround. */
 	haswell_mode_set_planes_workaround(intel_crtc);
 	ilk_crtc_enable_planes(crtc);
+
+	drm_vblank_on(dev, pipe);
 }
 
 static void ironlake_pfit_disable(struct intel_crtc *crtc)
@@ -4352,6 +4356,8 @@ static void valleyview_crtc_enable(struct drm_crtc *crtc)
 
 	for_each_encoder_on_crtc(dev, crtc, encoder)
 		encoder->enable(encoder);
+
+	drm_vblank_on(dev, pipe);
 }
 
 static void i9xx_crtc_enable(struct drm_crtc *crtc)
@@ -4399,6 +4405,8 @@ static void i9xx_crtc_enable(struct drm_crtc *crtc)
 
 	for_each_encoder_on_crtc(dev, crtc, encoder)
 		encoder->enable(encoder);
+
+	drm_vblank_on(dev, pipe);
 }
 
 static void i9xx_pfit_disable(struct intel_crtc *crtc)
