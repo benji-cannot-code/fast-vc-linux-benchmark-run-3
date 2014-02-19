342c9767f9a0833d4ff9648e40dbccba042d9de7
Mark Hounschell <markh@compro.net>
2014-02-19 13:12:14 -0500

    staging: dgap: Rename driver
    Renames driver file dgap_driver.c and dgap_driver.h to
    dgap.c and dgap.h because we are now single source and
    include file and better fits kernel naming conventions.
    
    Signed-off-by: Mark Hounschell <markh@compro.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap.c
+++ drivers/staging/dgap/dgap.c
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/kdev_t.h>
 
-#include "dgap_driver.h"
+#include "dgap.h"
 
 #define init_MUTEX(sem)         sema_init(sem, 1)
 #define DECLARE_MUTEX(name)     \
