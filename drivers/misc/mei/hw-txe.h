ac70fb88f8e44c7483a6df9be2ca9d5b381309bc
Stephen Rothwell <sfr@canb.auug.org.au>
2014-02-21 16:38:28 +1100

    mei: txe: include irqreturn.h for irqreturn_t etc
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/hw-txe.h
+++ drivers/misc/mei/hw-txe.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MEI_HW_TXE_H_
 #define _MEI_HW_TXE_H_
 
+#include <linux/irqreturn.h>
+
 #include "hw.h"
 #include "hw-txe-regs.h"
 
