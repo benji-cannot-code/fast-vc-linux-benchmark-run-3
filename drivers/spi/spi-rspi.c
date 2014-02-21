1e8132c53258d1cfcf6f2f5679529fc08a11d8a0
Geert Uytterhoeven <geert+renesas@linux-m68k.org>
2014-02-21 17:29:17 +0100

    spi: rspi: Remove empty rspi_cleanup()
    If spi_master.cleanup() is not needed, it can be left unimplemented.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@linux-m68k.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-rspi.c
+++ drivers/spi/spi-rspi.c
@@ -894,10 +894,6 @@ static int rspi_setup(struct spi_device *spi)
 	return 0;
 }
 
-static void rspi_cleanup(struct spi_device *spi)
-{
-}
-
 static u16 qspi_transfer_mode(const struct spi_transfer *xfer)
 {
 	if (xfer->tx_buf)
@@ -1256,7 +1252,6 @@ static int rspi_probe(struct platform_device *pdev)
 	master->bus_num = pdev->id;
 	master->setup = rspi_setup;
 	master->transfer_one = ops->transfer_one;
-	master->cleanup = rspi_cleanup;
 	master->prepare_message = rspi_prepare_message;
 	master->unprepare_message = rspi_unprepare_message;
 	master->mode_bits = ops->mode_bits;
