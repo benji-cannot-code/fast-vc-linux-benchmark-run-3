dcb70c49cf297d92223bb70df42b7c0446f5f6f9
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2014-02-21 01:10:09 +0100

    ACPI / dock: Use ACPI device object pointers instead of ACPI handles
    Rework the ACPI dock station driver to store ACPI device object
    pointers instead of ACPI handles in its internal data structures.
    
    The purpose is moslty to make subsequent simplifications possible,
    but also this allows the overall code size to be reduced slightly.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpi_drivers.h
+++ include/acpi/acpi_drivers.h
@@ -117,7 +117,7 @@ struct acpi_dock_ops {
 };
 
 #ifdef CONFIG_ACPI_DOCK
-extern int is_dock_device(acpi_handle handle);
+extern int is_dock_device(struct acpi_device *adev);
 extern int register_hotplug_dock_device(acpi_handle handle,
 					const struct acpi_dock_ops *ops,
 					void *context,
@@ -125,7 +125,7 @@ extern int register_hotplug_dock_device(acpi_handle handle,
 					void (*release)(void *));
 extern void unregister_hotplug_dock_device(acpi_handle handle);
 #else
-static inline int is_dock_device(acpi_handle handle)
+static inline int is_dock_device(struct acpi_device *adev)
 {
 	return 0;
 }
