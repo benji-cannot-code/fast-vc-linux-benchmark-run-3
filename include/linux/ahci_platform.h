f3bab344a01453571cc234e3df2c613f634ac413
Hans de Goede <hdegoede@redhat.com>
2014-02-22 16:53:34 +0100

    ahci-platform: "Library-ise" ahci_probe functionality
    ahci_probe consists of 3 steps:
    1) Get resources (get mmio, clks, regulator)
    2) Enable resources, handled by ahci_platform_enable_resouces
    3) The more or less standard ahci-host controller init sequence
    
    This commit refactors step 1 and 3 into separate functions, so the platform
    drivers for AHCI implementations which need a specific order in step 2,
    and / or need to do some custom register poking at some time, can re-use
    ahci-platform.c code without needing to copy and paste it.
    
    Note that ahci_platform_init_host's prototype takes the 3 non function
    members of ahci_platform_data as arguments, the idea is that drivers using
    the new exported utility functions will not use ahci_platform_data at all,
    and hopefully in the future ahci_platform_data can go away entirely.
    
    tj: Minor comment formatting updates.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/ahci_platform.h
+++ include/linux/ahci_platform.h
@@ -21,7 +21,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct device;
 struct ata_port_info;
 struct ahci_host_priv;
+struct platform_device;
 
+/*
+ * Note ahci_platform_data is deprecated, it is only kept around for use
+ * by the old da850 and spear13xx ahci code.
+ * New drivers should instead declare their own platform_driver struct, and
+ * use ahci_platform* functions in their own probe, suspend and resume methods.
+ */
 struct ahci_platform_data {
 	int (*init)(struct device *dev, void __iomem *addr);
 	void (*exit)(struct device *dev);
@@ -36,5 +43,12 @@ int ahci_platform_enable_clks(struct ahci_host_priv *hpriv);
 void ahci_platform_disable_clks(struct ahci_host_priv *hpriv);
 int ahci_platform_enable_resources(struct ahci_host_priv *hpriv);
 void ahci_platform_disable_resources(struct ahci_host_priv *hpriv);
+struct ahci_host_priv *ahci_platform_get_resources(
+	struct platform_device *pdev);
+int ahci_platform_init_host(struct platform_device *pdev,
+			    struct ahci_host_priv *hpriv,
+			    const struct ata_port_info *pi_template,
+			    unsigned int force_port_map,
+			    unsigned int mask_port_map);
 
 #endif /* _AHCI_PLATFORM_H */
