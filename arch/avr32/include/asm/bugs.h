badbd9103e329b61f0c9abe698eaee5815110564
Wolfram Sang <wsa@the-dreams.de>
2014-02-22 09:28:27 +0100

    avr32: remove cpu_data macro to fix compiles
    Having cpu_data as a parameterless macro can easily cause build failures
    because it can be a variable name like in linux/pm_domain.h [1]. So,
    remove the macro and convert its only user. Because this architecture
    cannot do SMP, remove the whole SMP block, too. Only compile tested due
    to no hardware.
    
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    Acked-by: Hans-Christian Egtvedt <egtvedt@samfundet.no>
    
    [1] https://lists.01.org/pipermail/kbuild-all/2014-February/003252.html

--- arch/avr32/include/asm/bugs.h
+++ arch/avr32/include/asm/bugs.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void __init check_bugs(void)
 {
-	cpu_data->loops_per_jiffy = loops_per_jiffy;
+	boot_cpu_data.loops_per_jiffy = loops_per_jiffy;
 }
 
 #endif /* __ASM_AVR32_BUGS_H */
