2cf8f67f2dbbb7658f88b398e621f0d28afad33f
Sagi Grimberg <sagig@mellanox.com>
2014-02-23 14:19:07 +0200

    IB/mlx5: Initialize mlx5_ib_qp signature-related members
    If user requested signature enable we initialize relevant mlx5_ib_qp
    members.  We mark the qp as sig_enable and we increase the effective
    SQ size, but still limit the user max_send_wr to original size
    computed.  We also allow the create_qp routine to accept sig_enable
    create flag.
    
    Signed-off-by: Sagi Grimberg <sagig@mellanox.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- include/linux/mlx5/qp.h
+++ include/linux/mlx5/qp.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mlx5/driver.h>
 
 #define MLX5_INVALID_LKEY	0x100
+#define MLX5_SIG_WQE_SIZE	(MLX5_SEND_WQE_BB * 5)
 
 enum mlx5_qp_optpar {
 	MLX5_QP_OPTPAR_ALT_ADDR_PATH		= 1 << 0,
