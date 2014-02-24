d21dd2ce21f2e22f02108793c87529b19c946fe4
Wolfram Sang <wsa@sang-engineering.com>
2014-02-24 20:57:11 +0100

    clk: shmobile: div6: use proper description in kernel doc
    These variable clocks have nothing to do with MSTP gating, probably a
    copy&paste leftover.
    
    Signed-off-by: Wolfram Sang <wsa@sang-engineering.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- drivers/clk/shmobile/clk-div6.c
+++ drivers/clk/shmobile/clk-div6.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CPG_DIV6_DIV_MASK	0x3f
 
 /**
- * struct div6_clock - MSTP gating clock
+ * struct div6_clock - CPG 6 bit divider clock
  * @hw: handle between common and hardware-specific interfaces
  * @reg: IO-remapped register
  * @div: divisor value (1-64)
