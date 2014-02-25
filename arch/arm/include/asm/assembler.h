9411459dd00a992d5defde737e3d97977a304472
Victor Kamensky <victor.kamensky@linaro.org>
2014-02-25 08:41:09 +0100

    ARM: 7990/1: asm: rename logical shift macros push pull into lspush lspull
    Renames logical shift macros, 'push' and 'pull', defined in
    arch/arm/include/asm/assembler.h, into 'lspush' and 'lspull'.
    That eliminates name conflict between 'push' logical shift macro
    and 'push' instruction mnemonic. That allows assembler.h to be
    included in .S files that use 'push' instruction.
    
    Suggested-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Victor Kamensky <victor.kamensky@linaro.org>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/assembler.h
+++ arch/arm/include/asm/assembler.h
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Endian independent macros for shifting bytes within registers.
  */
 #ifndef __ARMEB__
-#define pull            lsr
-#define push            lsl
+#define lspull          lsr
+#define lspush          lsl
 #define get_byte_0      lsl #0
 #define get_byte_1	lsr #8
 #define get_byte_2	lsr #16
@@ -42,8 +42,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define put_byte_2	lsl #16
 #define put_byte_3	lsl #24
 #else
-#define pull            lsl
-#define push            lsr
+#define lspull          lsl
+#define lspush          lsr
 #define get_byte_0	lsr #24
 #define get_byte_1	lsr #16
 #define get_byte_2	lsr #8
