beca773da922e7051a8a5e0ca58671629c4d163d
Markus Mayer <markus.mayer@linaro.org>
2014-02-25 14:17:45 -0800

    ARM: bcm281xx: Re-order hearder files
    Re-order header files alphabetically.
    
    Signed-off-by: Markus Mayer <markus.mayer@linaro.org>
    Signed-off-by: Matt Porter <mporter@linaro.org>

--- arch/arm/mach-bcm/board_bcm281xx.c
+++ arch/arm/mach-bcm/board_bcm281xx.c
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
-#include <linux/of_platform.h>
-#include <linux/of_address.h>
 #include <linux/clocksource.h>
+#include <linux/of_address.h>
+#include <linux/of_platform.h>
 
 #include <asm/mach/arch.h>
 
