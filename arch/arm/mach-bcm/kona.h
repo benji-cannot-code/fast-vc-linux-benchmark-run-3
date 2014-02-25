6761d9a0cf0e0706f55b74abe2cbf49034a784d8
Markus Mayer <markus.mayer@linaro.org>
2014-02-25 14:17:43 -0800

    ARM: bcm281xx: Move kona_l2_cache_init() so it can be shared
    In preparation for future SoCs, move kona_l2_cache_init() from board
    specific board_bcm281xx.c to shared kona.c, so multiple SoC families
    can make use of it. Also change the return type to "void", since we
    never look at the return code anyway.
    
    Signed-off-by: Markus Mayer <markus.mayer@linaro.org>
    Signed-off-by: Matt Porter <mporter@linaro.org>

--- arch/arm/mach-bcm/kona.h
+++ arch/arm/mach-bcm/kona.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (C) 2013 Broadcom Corporation
+ * Copyright (C) 2012-2014 Broadcom Corporation
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -14,5 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/reboot.h>
 
+void __init kona_l2_cache_init(void);
 void bcm_kona_setup_restart(void);
 void bcm_kona_restart(enum reboot_mode mode, const char *cmd);
