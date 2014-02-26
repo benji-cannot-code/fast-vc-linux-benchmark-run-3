5c0ac658c451a5dfb4788accef3af42b1d73530c
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2014-02-26 15:31:46 +0200

    ASoC: Intel: Baytrail: Fix implicit declaration of function 'memcpy_fromio'
    Some kernel configurations can cause following build error:
    
    sound/soc/intel/sst-baytrail-ipc.c: In function ‘sst_byt_get_dsp_position’:
    sound/soc/intel/sst-baytrail-ipc.c:744:2: error: implicit declaration of function ‘memcpy_fromio’ [-Werror=implicit-function-declaration]
      memcpy_fromio(&fw_tstamp,
      ^
    cc1: some warnings being treated as errors
    
    Fix this by including <linux/io.h> explicitly.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/intel/sst-baytrail-ipc.c
+++ sound/soc/intel/sst-baytrail-ipc.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/kthread.h>
 #include <linux/firmware.h>
+#include <linux/io.h>
 #include <asm/div64.h>
 
 #include "sst-baytrail-ipc.h"
