53fb1f2f1223a2ec2d39ffdaf9cafdc53067f898
Johan Hedberg <johan.hedberg@intel.com>
2014-02-26 23:33:43 +0200

    Bluetooth: Track not yet received keys in SMP
    To make is easier to track which keys we've received and which ones
    we're still waiting for simply clear the corresponding key bits from
    smp->remote_key_dist as they get received. This will allow us to
    simplify the code for checking for SMP completion in subsequent patches.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/smp.c
+++ net/bluetooth/smp.c
@@ -949,6 +949,9 @@ static int smp_cmd_master_ident(struct l2cap_conn *conn, struct sk_buff *skb)
 	if (!(smp->remote_key_dist & SMP_DIST_ENC_KEY))
 		return 0;
 
+	/* Mark the information as received */
+	smp->remote_key_dist &= ~SMP_DIST_ENC_KEY;
+
 	skb_pull(skb, sizeof(*rp));
 
 	hci_dev_lock(hdev);
@@ -1001,6 +1004,9 @@ static int smp_cmd_ident_addr_info(struct l2cap_conn *conn,
 	/* Ignore this PDU if it wasn't requested */
 	if (!(smp->remote_key_dist & SMP_DIST_ID_KEY))
 		return 0;
+
+	/* Mark the information as received */
+	smp->remote_key_dist &= ~SMP_DIST_ID_KEY;
 
 	skb_pull(skb, sizeof(*info));
 
