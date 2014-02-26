ed921dd7deadc59865c7f0ea6e1aa8f9f47daf0c
Bjorn Helgaas <bhelgaas@google.com>
2014-02-26 11:26:00 -0700

    PCI: Add "weak" generic pcibios_enable_device() implementation
    Many architectures implement pcibios_enable_device() the same way, so
    provide a default implementation in the core.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -1186,6 +1186,11 @@ int pci_load_and_free_saved_state(struct pci_dev *dev,
 }
 EXPORT_SYMBOL_GPL(pci_load_and_free_saved_state);
 
+int __weak pcibios_enable_device(struct pci_dev *dev, int bars)
+{
+	return pci_enable_resources(dev, bars);
+}
+
 static int do_pci_enable_device(struct pci_dev *dev, int bars)
 {
 	int err;
