ba7ce3ebcd07086fc94abb224cfa2ae9c5997b2b
Jan Beulich <JBeulich@suse.com>
2014-02-27 08:47:02 +0000

    x86, hash: Fix build failure with older binutils
    Just like for other ISA extension instruction uses we should check
    whether the assembler actually supports them. The fallback here simply
    is to encode an instruction  with fixed operands (%eax and %ecx).
    
    [ hpa: tagging for -stable as a build fix ]
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Link: http://lkml.kernel.org/r/530F0996020000780011FBE7@nat28.tlf.novell.com
    Cc: Francesco Fusco <ffusco@redhat.com>
    Cc: Thomas Graf <tgraf@redhat.com>
    Cc: David S. Miller <davem@davemloft.net>
    Acked-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    Cc: <stable@vger.kernel.org> # v3.14

--- arch/x86/lib/hash.c
+++ arch/x86/lib/hash.c
@@ -40,7 +40,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline u32 crc32_u32(u32 crc, u32 val)
 {
+#ifdef CONFIG_AS_CRC32
 	asm ("crc32l %1,%0\n" : "+r" (crc) : "rm" (val));
+#else
+	asm (".byte 0xf2, 0x0f, 0x38, 0xf1, 0xc1" : "+a" (crc) : "c" (val));
+#endif
 	return crc;
 }
 
