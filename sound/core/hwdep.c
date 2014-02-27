b3fab7569356f6f98d20c6cc5427fb73540b8ade
Takashi Iwai <tiwai@suse.de>
2014-02-27 16:00:17 +0100

    ALSA: Move EXPORT_SYMBOL() in appropriate places
    Just a cleanup to follow the standard coding style.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/hwdep.c
+++ sound/core/hwdep.c
@@ -396,6 +396,7 @@ int snd_hwdep_new(struct snd_card *card, char *id, int device,
 		*rhwdep = hwdep;
 	return 0;
 }
+EXPORT_SYMBOL(snd_hwdep_new);
 
 static int snd_hwdep_free(struct snd_hwdep *hwdep)
 {
@@ -568,5 +569,3 @@ static void __exit alsa_hwdep_exit(void)
 
 module_init(alsa_hwdep_init)
 module_exit(alsa_hwdep_exit)
-
-EXPORT_SYMBOL(snd_hwdep_new);
