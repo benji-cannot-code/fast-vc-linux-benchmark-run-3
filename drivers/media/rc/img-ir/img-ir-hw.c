e20e9744d6ef3ce31e93ea079472646df65628e0
James Hogan <james.hogan@imgtec.com>
2014-02-28 20:28:59 -0300

    [media] rc: img-ir: add Sharp decoder module
    Add an img-ir module for decoding the Sharp infrared protocol.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/rc/img-ir/img-ir-hw.c
+++ drivers/media/rc/img-ir/img-ir-hw.c
@@ -24,6 +24,7 @@ static DEFINE_SPINLOCK(img_ir_decoders_lock);
 extern struct img_ir_decoder img_ir_nec;
 extern struct img_ir_decoder img_ir_jvc;
 extern struct img_ir_decoder img_ir_sony;
+extern struct img_ir_decoder img_ir_sharp;
 
 static bool img_ir_decoders_preprocessed;
 static struct img_ir_decoder *img_ir_decoders[] = {
@@ -35,6 +36,9 @@ static struct img_ir_decoder *img_ir_decoders[] = {
 #endif
 #ifdef CONFIG_IR_IMG_SONY
 	&img_ir_sony,
+#endif
+#ifdef CONFIG_IR_IMG_SHARP
+	&img_ir_sharp,
 #endif
 	NULL
 };
