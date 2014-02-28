b8af2a4c5177af9725abcf47a95f2be398e00571
Alexander Aring <alex.aring@gmail.com>
2014-02-28 07:32:50 +0100

    6lowpan: handling 6lowpan fragmentation via inet_frag api
    This patch drops the current way of 6lowpan fragmentation on receiving
    side and replace it with a implementation which use the inet_frag api.
    The old fragmentation handling has some race conditions and isn't
    rfc4944 compatible. Also adding support to match fragments on
    destination address, source address, tag value and datagram_size
    which is missing in the current implementation.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/ieee802154_6lowpan.h
+++ include/net/netns/ieee802154_6lowpan.h
@@ -8,7 +8,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NETNS_IEEE802154_6LOWPAN_H__
 #define __NETNS_IEEE802154_6LOWPAN_H__
 
+struct netns_sysctl_lowpan {
+#ifdef CONFIG_SYSCTL
+	struct ctl_table_header *frags_hdr;
+#endif
+};
+
 struct netns_ieee802154_lowpan {
+	struct netns_sysctl_lowpan sysctl;
+	struct netns_frags	frags;
+	u16			max_dsize;
 };
 
 #endif
