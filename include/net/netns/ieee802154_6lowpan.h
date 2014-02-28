976727eead75a32895389b222367fcbef1d5ab66
Alexander Aring <alex.aring@gmail.com>
2014-02-28 07:32:49 +0100

    net: ns: add ieee802154_6lowpan namespace
    This patch adds necessary ieee802154 6lowpan namespace to provide the
    inet_frag information. This is a initial support for handling 6lowpan
    fragmentation with the inet_frag api.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/net/netns/ieee802154_6lowpan.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * ieee802154 6lowpan in net namespaces
+ */
+
+#include <net/inet_frag.h>
+
+#ifndef __NETNS_IEEE802154_6LOWPAN_H__
+#define __NETNS_IEEE802154_6LOWPAN_H__
+
+struct netns_ieee802154_lowpan {
+};
+
+#endif
