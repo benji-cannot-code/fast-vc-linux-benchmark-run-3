ca4087dd0ffd29f9876e4fa0983a7c7f07e06aff
James Hogan <james.hogan@imgtec.com>
2014-02-28 20:28:57 -0300

    [media] rc: img-ir: add JVC decoder module
    Add an img-ir module for decoding the JVC infrared protocol.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/rc/img-ir/img-ir-hw.c
+++ drivers/media/rc/img-ir/img-ir-hw.c
@@ -22,11 +22,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static DEFINE_SPINLOCK(img_ir_decoders_lock);
 
 extern struct img_ir_decoder img_ir_nec;
+extern struct img_ir_decoder img_ir_jvc;
 
 static bool img_ir_decoders_preprocessed;
 static struct img_ir_decoder *img_ir_decoders[] = {
 #ifdef CONFIG_IR_IMG_NEC
 	&img_ir_nec,
+#endif
+#ifdef CONFIG_IR_IMG_JVC
+	&img_ir_jvc,
 #endif
 	NULL
 };
