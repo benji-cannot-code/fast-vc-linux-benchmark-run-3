c38af928941cf461223e604d9a93d2ebde864e03
Aaro Koskinen <aaro.koskinen@iki.fi>
2014-03-02 00:09:08 +0200

    staging: octeon-ethernet: make global_register_lock static
    Make global_register_lock static to eliminate a sparse warning.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/octeon/ethernet-rgmii.c
+++ drivers/staging/octeon/ethernet-rgmii.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/octeon/cvmx-npi-defs.h>
 #include <asm/octeon/cvmx-gmxx-defs.h>
 
-DEFINE_SPINLOCK(global_register_lock);
+static DEFINE_SPINLOCK(global_register_lock);
 
 static int number_rgmii_ports;
 
