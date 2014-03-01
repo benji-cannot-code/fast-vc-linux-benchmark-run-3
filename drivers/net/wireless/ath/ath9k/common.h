a610f680fa9f1fa24b97778cbb4ac0481a09106e
Oleksij Rempel <linux@rempel-privat.de>
2014-03-01 21:15:56 +0100

    ath9k: move ath9k_beacon_config_sta to common-beacon
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/common.h
+++ drivers/net/wireless/ath/ath9k/common.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hw-ops.h"
 
 #include "common-init.h"
+#include "common-beacon.h"
 
 /* Common header for Atheros 802.11n base driver cores */
 
