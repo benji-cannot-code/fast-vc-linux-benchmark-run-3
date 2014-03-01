5b5ee8dff14e54a8f9392746dca2482eb44554dd
Aaro Koskinen <aaro.koskinen@iki.fi>
2014-03-02 00:09:09 +0200

    staging: octeon-ethernet: add missing include
    ethernet-mem was missing its own include file, causing sparse to complain
    about undeclared global functions.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/octeon/ethernet-mem.c
+++ drivers/staging/octeon/ethernet-mem.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/octeon/octeon.h>
 
+#include "ethernet-mem.h"
 #include "ethernet-defines.h"
 
 #include <asm/octeon/cvmx-fpa.h>
