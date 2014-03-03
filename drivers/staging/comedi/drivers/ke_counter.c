3a8b86f5d8bed2538ae2783e3325406b73ff7134
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-03-03 16:47:40 -0700

    staging: comedi: ke_counter: rename cnt_auto_attach()
    For aesthetics, rename this function so ist has namespace associated
    with the driver.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ke_counter.c
+++ drivers/staging/comedi/drivers/ke_counter.c
@@ -96,8 +96,8 @@ static int ke_counter_insn_read(struct comedi_device *dev,
 	return insn->n;
 }
 
-static int cnt_auto_attach(struct comedi_device *dev,
-				     unsigned long context_unused)
+static int ke_counter_auto_attach(struct comedi_device *dev,
+				  unsigned long context_unused)
 {
 	struct pci_dev *pcidev = comedi_to_pci_dev(dev);
 	struct comedi_subdevice *s;
@@ -133,7 +133,7 @@ static int cnt_auto_attach(struct comedi_device *dev,
 static struct comedi_driver ke_counter_driver = {
 	.driver_name	= "ke_counter",
 	.module		= THIS_MODULE,
-	.auto_attach	= cnt_auto_attach,
+	.auto_attach	= ke_counter_auto_attach,
 	.detach		= comedi_pci_disable,
 };
 
