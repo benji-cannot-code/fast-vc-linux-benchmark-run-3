e196c4566a3399e76d95240c21bdefdced63c421
Kristina Martšenko <kristina.martsenko@gmail.com>
2014-03-03 23:05:42 +0200

    staging: sbe-2t3e3: remove t3e3_rx_desc_t typedef
    Kernel coding style does not recommend using typedefs for structures, so
    remove the t3e3_rx_desc_t typedef.
    
    Fix the following checkpatch warning:
    drivers/staging/sbe-2t3e3/2t3e3.h:616: WARNING: do not add new typedefs
    
    Signed-off-by: Kristina Martšenko <kristina.martsenko@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sbe-2t3e3/2t3e3.h
+++ drivers/staging/sbe-2t3e3/2t3e3.h
@@ -614,12 +614,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * descriptor list and data buffer
  *
  **********************************************************************/
-typedef struct {
+struct t3e3_rx_desc {
 	u32 rdes0;
 	u32 rdes1;
 	u32 rdes2;
 	u32 rdes3;
-} t3e3_rx_desc_t;
+};
 
 #define SBE_2T3E3_RX_DESC_RING_SIZE			64
 
@@ -724,7 +724,7 @@ struct channel {
 		u32 interrupt_enable_mask;
 
 		/* receive chain/ring */
-		t3e3_rx_desc_t *rx_ring;
+		struct t3e3_rx_desc *rx_ring;
 		struct sk_buff *rx_data[SBE_2T3E3_RX_DESC_RING_SIZE];
 		u32 rx_ring_current_read;
 
