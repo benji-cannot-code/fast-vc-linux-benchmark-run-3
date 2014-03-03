aa58a0b48a79ab390d30ad2069e5c2b342158dfc
Kishon Vijay Abraham I <kishon@ti.com>
2014-03-03 17:08:14 +0530

    phy: omap-usb2: move omap_usb.h from linux/usb/ to linux/phy/
    No functional change. Moved omap_usb.h from linux/usb/ to linux/phy/.
    Also removed the unused members of struct omap_usb (after phy-omap-pipe3
    started using it's own header file)
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/phy-omap-usb2.c
+++ drivers/phy/phy-omap-usb2.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/of.h>
 #include <linux/io.h>
-#include <linux/usb/omap_usb.h>
+#include <linux/phy/omap_usb.h>
 #include <linux/usb/phy_companion.h>
 #include <linux/clk.h>
 #include <linux/err.h>
