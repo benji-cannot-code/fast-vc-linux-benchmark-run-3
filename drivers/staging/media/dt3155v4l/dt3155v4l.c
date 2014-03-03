de254ef2aa81e52bc247a9fe3baa37105113a0a9
Jon Mason <jdmason@kudzu.us>
2014-03-03 14:00:38 -0300

    [media] staging/dt3155v4l: use PCI_VENDOR_ID_INTEL
    Use PCI_VENDOR_ID_INTEL instead of creating its own vendor ID #define.
    
    Signed-off-by: Jon Mason <jdmason@kudzu.us>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/staging/media/dt3155v4l/dt3155v4l.c
+++ drivers/staging/media/dt3155v4l/dt3155v4l.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dt3155v4l.h"
 
-#define DT3155_VENDOR_ID 0x8086
 #define DT3155_DEVICE_ID 0x1223
 
 /* DT3155_CHUNK_SIZE is 4M (2^22) 8 full size buffers */
@@ -976,7 +975,7 @@ dt3155_remove(struct pci_dev *pdev)
 }
 
 static const struct pci_device_id pci_ids[] = {
-	{ PCI_DEVICE(DT3155_VENDOR_ID, DT3155_DEVICE_ID) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, DT3155_DEVICE_ID) },
 	{ 0, /* zero marks the end */ },
 };
 MODULE_DEVICE_TABLE(pci, pci_ids);
