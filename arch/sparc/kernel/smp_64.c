fdff1adf480aaabe3c5c4863db2b2a90ba9dc7b1
Bjorn Helgaas <bhelgaas@google.com>
2014-03-04 14:07:44 -0700

    sparc64, sched: Remove unused sparc64_multi_core
    Remove sparc64_multi_core because it's not used any more.
    
    It was added by a2f9f6bbb30e ("Fix {mc,smt}_capable()"), and the last uses
    were removed by e637d96bf462 ("sched: Remove unused mc_capable() and
    smt_capable()").
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: David S. Miller <davem@davemloft.net>
    Link: http://lkml.kernel.org/r/20140304210744.16893.75929.stgit@bhelgaas-glaptop.roam.corp.google.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/sparc/kernel/smp_64.c
+++ arch/sparc/kernel/smp_64.c
@@ -54,8 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "cpumap.h"
 
-int sparc64_multi_core __read_mostly;
-
 DEFINE_PER_CPU(cpumask_t, cpu_sibling_map) = CPU_MASK_NONE;
 cpumask_t cpu_core_map[NR_CPUS] __read_mostly =
 	{ [0 ... NR_CPUS-1] = CPU_MASK_NONE };
