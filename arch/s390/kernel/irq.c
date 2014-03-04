3ad4afb2bd56eb988a1575decf516c89391880ec
Thomas Gleixner <tglx@linutronix.de>
2014-03-04 23:57:29 +0100

    s390: Do not rely on magic indirect includes
    commit: 8f945a33 (genirq: Move kstat_incr_irqs_this_cpu() to core)
    unearthed the following:
    
       arch/s390/kernel/irq.c: In function 'init_IRQ':
    >> arch/s390/kernel/irq.c:93:2: error: implicit declaration of function 'irq_reserve_irqs'
    [-Werror=implicit-function-declaration]
    ....
       cc1: some warnings being treated as errors
    --
       drivers/s390/cio/cio.c: In function 'init_cio_interrupts':
    >> drivers/s390/cio/cio.c:594:2: error: implicit declaration of function
    'irq_set_chip_and_handler' [-Werror=implicit-function-declaration]
    [-Werror=implicit-function-declaration]
    ....
       cc1: some warnings being treated as errors
    
    The reason is that those files require linux/irq.h and magically
    pulled that in via linux/kernel_stat.h
    
    The commit above got rid of the pointless include of linux/irq.h in
    linux/kernel_stat.h and therefor broke the build.
    
    Include linux/irq.h
    
    Reported-by: fengguang.wu@intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: s390 <linux-s390@vger.kernel.org>

--- arch/s390/kernel/irq.c
+++ arch/s390/kernel/irq.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/slab.h>
 #include <linux/cpu.h>
+#include <linux/irq.h>
 #include <asm/irq_regs.h>
 #include <asm/cputime.h>
 #include <asm/lowcore.h>
--- drivers/s390/cio/cio.c
+++ drivers/s390/cio/cio.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/kernel_stat.h>
 #include <linux/interrupt.h>
+#include <linux/irq.h>
 #include <asm/cio.h>
 #include <asm/delay.h>
 #include <asm/irq.h>
