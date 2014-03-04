1c4617d874b5e729e3181632fc0ddceb2ace63f7
Thomas Gleixner <tglx@linutronix.de>
2014-03-04 23:39:58 +0100

    x86: hyperv: Make it build with CONFIG_HYPERV=m again
    Commit 1aec16967 (x86: Hyperv: Cleanup the irq mess) removed the
    ability to build the hyperv stuff as a module. Bring it back.
    
    Reported-by: fengguang.wu@intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: linuxdrivers <devel@linuxdriverproject.org>
    Cc: x86 <x86@kernel.org>

--- arch/x86/kernel/cpu/mshyperv.c
+++ arch/x86/kernel/cpu/mshyperv.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ms_hyperv_info ms_hyperv;
 EXPORT_SYMBOL_GPL(ms_hyperv);
 
-#ifdef CONFIG_HYPERV
+#if IS_ENABLED(CONFIG_HYPERV)
 static irq_handler_t *vmbus_handler;
 
 void hyperv_vector_handler(struct pt_regs *regs)
