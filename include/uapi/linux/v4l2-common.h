e23a3d137cb4a3ffc85360b03e188a6f1372018c
Hans Verkuil <hans.verkuil@cisco.com>
2014-03-04 07:46:47 -0300

    [media] v4l2: allow v4l2_subdev_edid to be used with video nodes
    Struct v4l2_subdev_edid and the VIDIOC_SUBDEV_G/S_EDID ioctls were
    specific for subdevices, but for hardware with a simple video pipeline
    you do not need/want to create subdevice nodes to just get/set the EDID.
    
    Move the v4l2_subdev_edid struct to v4l2-common.h and rename as
    v4l2_edid. Add the same ioctls to videodev2.h as well, thus allowing
    this API to be used with both video nodes and v4l-subdev nodes.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- include/uapi/linux/v4l2-common.h
+++ include/uapi/linux/v4l2-common.h
@@ -69,4 +69,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define V4L2_SUBDEV_SEL_FLAG_SIZE_LE	V4L2_SEL_FLAG_LE
 #define V4L2_SUBDEV_SEL_FLAG_KEEP_CONFIG V4L2_SEL_FLAG_KEEP_CONFIG
 
+struct v4l2_edid {
+	__u32 pad;
+	__u32 start_block;
+	__u32 blocks;
+	__u32 reserved[5];
+	__u8 __user *edid;
+};
+
 #endif /* __V4L2_COMMON__ */
