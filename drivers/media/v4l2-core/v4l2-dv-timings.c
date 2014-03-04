0c8304e454446e978dd473e7157877df809811ad
Hans Verkuil <hans.verkuil@cisco.com>
2014-03-04 04:51:50 -0300

    [media] v4l2-dv-timings: add module name, description, license
    I completely forgot to add them when I made this module. Loading this module
    without it will taint the kernel, which is not intended.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Cc: stable@vger.kernel.org      # for v3.12 and up
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/v4l2-core/v4l2-dv-timings.c
+++ drivers/media/v4l2-core/v4l2-dv-timings.c
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/v4l2-dv-timings.h>
 #include <media/v4l2-dv-timings.h>
 
+MODULE_AUTHOR("Hans Verkuil");
+MODULE_DESCRIPTION("V4L2 DV Timings Helper Functions");
+MODULE_LICENSE("GPL");
+
 const struct v4l2_dv_timings v4l2_dv_timings_presets[] = {
 	V4L2_DV_BT_CEA_640X480P59_94,
 	V4L2_DV_BT_CEA_720X480I59_94,
