fd3c0ab7a391676ebff57f8a1017dd6373911754
Heiko Carstens <heiko.carstens@de.ibm.com>
2014-03-05 10:43:51 +0100

    fs/compat: optional preadv64/pwrite64 compat system calls
    The preadv64/pwrite64 have been implemented for the x32 ABI, in order
    to allow passing 64 bit arguments from user space without splitting
    them into two 32 bit parameters, like it would be necessary for usual
    compat tasks.
    Howevert these two system calls are only being used for the x32 ABI,
    so add __ARCH_WANT_COMPAT defines for these two compat syscalls and
    make these two only visible for x86.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>

--- arch/x86/include/asm/unistd.h
+++ arch/x86/include/asm/unistd.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  include <asm/unistd_64_x32.h>
 #  define __ARCH_WANT_COMPAT_SYS_TIME
 #  define __ARCH_WANT_COMPAT_SYS_GETDENTS64
+#  define __ARCH_WANT_COMPAT_SYS_PREADV64
+#  define __ARCH_WANT_COMPAT_SYS_PWRITEV64
 
 # endif
 
