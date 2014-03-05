bb2f5baa6b86a074563d3dfb48a79e630157ad26
Frederic Weisbecker <fweisbec@gmail.com>
2014-03-05 16:22:37 +0100

    cputime: Bring cputime -> nsecs conversion
    We already have nsecs_to_cputime(). Now we need to be able to convert
    the other way around in order to fix a bug on steal time accounting.
    
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>

--- include/linux/cputime.h
+++ include/linux/cputime.h
@@ -4,6 +4,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/cputime.h>
 
+#ifndef cputime_to_nsecs
+# define cputime_to_nsecs(__ct)	\
+	(cputime_to_usecs(__ct) * NSEC_PER_USEC)
+#endif
+
 #ifndef nsecs_to_cputime
 # define nsecs_to_cputime(__nsecs)	\
 	usecs_to_cputime((__nsecs) / NSEC_PER_USEC)
