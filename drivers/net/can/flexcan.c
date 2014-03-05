41c4ac6aa621110ce03e55dff2688a7f21a4bd28
Marc Kleine-Budde <mkl@pengutronix.de>
2014-03-05 19:10:44 +0100

    can: flexcan: Remove #ifdef CONFIG_PM_SLEEP
    This patch removes #ifdef CONFIG_PM_SLEEP to improve compile coverage.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1202,8 +1202,7 @@ static int flexcan_remove(struct platform_device *pdev)
 	return 0;
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int flexcan_suspend(struct device *device)
+static int __maybe_unused flexcan_suspend(struct device *device)
 {
 	struct net_device *dev = dev_get_drvdata(device);
 	struct flexcan_priv *priv = netdev_priv(dev);
@@ -1222,7 +1221,7 @@ static int flexcan_suspend(struct device *device)
 	return 0;
 }
 
-static int flexcan_resume(struct device *device)
+static int __maybe_unused flexcan_resume(struct device *device)
 {
 	struct net_device *dev = dev_get_drvdata(device);
 	struct flexcan_priv *priv = netdev_priv(dev);
@@ -1234,7 +1233,6 @@ static int flexcan_resume(struct device *device)
 	}
 	return flexcan_chip_enable(priv);
 }
-#endif /* CONFIG_PM_SLEEP */
 
 static SIMPLE_DEV_PM_OPS(flexcan_pm_ops, flexcan_suspend, flexcan_resume);
 
