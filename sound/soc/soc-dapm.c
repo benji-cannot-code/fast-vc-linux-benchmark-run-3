a48fdc0de103e8df6f261c11f77661ddf4baad66
Mark Brown <broonie@linaro.org>
2014-03-06 16:15:48 +0800

    ASoC: dapm: Staticise dapm_mark_dirty()
    The function is not called outside soc-dapm.c so there is no need for it
    to be exported.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -147,7 +147,7 @@ static bool dapm_dirty_widget(struct snd_soc_dapm_widget *w)
 	return !list_empty(&w->dirty);
 }
 
-void dapm_mark_dirty(struct snd_soc_dapm_widget *w, const char *reason)
+static void dapm_mark_dirty(struct snd_soc_dapm_widget *w, const char *reason)
 {
 	if (!dapm_dirty_widget(w)) {
 		dev_vdbg(w->dapm->dev, "Marking %s dirty due to %s\n",
@@ -155,7 +155,6 @@ void dapm_mark_dirty(struct snd_soc_dapm_widget *w, const char *reason)
 		list_add_tail(&w->dirty, &w->dapm->card->dapm_dirty);
 	}
 }
-EXPORT_SYMBOL_GPL(dapm_mark_dirty);
 
 void dapm_mark_io_dirty(struct snd_soc_dapm_context *dapm)
 {
