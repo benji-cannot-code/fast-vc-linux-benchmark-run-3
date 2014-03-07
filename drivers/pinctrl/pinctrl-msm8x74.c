012d161e24697f1ca0f3cee0808a617b2644480a
Stephen Boyd <sboyd@codeaurora.org>
2014-03-06 22:44:42 -0800

    pinctrl: msm: Drop unused includes
    These includes are unused or can be handled via forward
    declarations. Remove them.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Acked-by: Bjorn Andersson <bjorn.andersson@sonymobile.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-msm8x74.c
+++ drivers/pinctrl/pinctrl-msm8x74.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/pinctrl/pinctrl.h>
-#include <linux/pinctrl/pinmux.h>
 
 #include "pinctrl-msm.h"
 
