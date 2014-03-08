64632763b90cf08adade5c6bf8f7a6f8598d1a8c
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-03-07 17:31:41 -0700

    staging: comedi: ni_660x: use cfc_handle_events()
    Use the comedi_fc helper function instead of duplicating the code.
    
    In the Kconfig, COMEDI_NI_660X selects COMEDI_FC indirectly by selecting
    COMEDI_NI_TIOCMD which selects COMEDI_MITE which selects COMEDI_FC.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_660x.c
+++ drivers/staging/comedi/drivers/ni_660x.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../comedidev.h"
 
+#include "comedi_fc.h"
 #include "mite.h"
 #include "ni_tio.h"
 
@@ -790,13 +791,7 @@ static void ni_660x_handle_gpct_interrupt(struct comedi_device *dev,
 	struct ni_gpct *counter = s->private;
 
 	ni_tio_handle_interrupt(counter, s);
-	if (s->async->events) {
-		if (s->async->events & (COMEDI_CB_EOA | COMEDI_CB_ERROR |
-					COMEDI_CB_OVERFLOW)) {
-			ni_660x_cancel(dev, s);
-		}
-		comedi_event(dev, s);
-	}
+	cfc_handle_events(dev, s);
 }
 
 static irqreturn_t ni_660x_interrupt(int irq, void *d)
