389036409ed4aaebfbdea6f4ce021235cdc48480
Archit Taneja <archit@ti.com>
2014-03-10 04:19:02 -0300

    [media] v4l: ti-vpe: zero out reserved fields in try_fmt
    Zero out the reserved formats in v4l2_pix_format_mplane and
    v4l2_plane_pix_format members of the returned v4l2_format pointer when passed
    through TRY_FMT ioctl.
    
    This ensures that the user doesn't interpret the non-zero fields as some data
    passed by the driver, and ensures compliance.
    
    Reviewed-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/ti-vpe/vpe.c
+++ drivers/media/platform/ti-vpe/vpe.c
@@ -1481,6 +1481,7 @@ static int __vpe_try_fmt(struct vpe_ctx *ctx, struct v4l2_format *f,
 		}
 	}
 
+	memset(pix->reserved, 0, sizeof(pix->reserved));
 	for (i = 0; i < pix->num_planes; i++) {
 		plane_fmt = &pix->plane_fmt[i];
 		depth = fmt->vpdma_fmt[i]->depth;
@@ -1492,6 +1493,8 @@ static int __vpe_try_fmt(struct vpe_ctx *ctx, struct v4l2_format *f,
 
 		plane_fmt->sizeimage =
 				(pix->height * pix->width * depth) >> 3;
+
+		memset(plane_fmt->reserved, 0, sizeof(plane_fmt->reserved));
 	}
 
 	return 0;
