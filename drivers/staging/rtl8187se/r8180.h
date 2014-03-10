27009fe5a00f3c24991e140583301bc07160b053
Ana Rey <anarey@gmail.com>
2014-03-10 18:56:44 +0100

    staging: rtl8187se: Convert _WIRELESS_MODE into a enum.
    The Documentation/CodingStyle doesn't recommend the use of typedef,
    convert this to enum.
    
    While at it, I have also renamed the variable names that were used in
    this typedef not to use Hungarian notation.
    
    Signed-off-by: Ana Rey <anarey@gmail.com>
    Signed-off-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>

--- drivers/staging/rtl8187se/r8180.h
+++ drivers/staging/rtl8187se/r8180.h
@@ -69,13 +69,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* +by amy 080312. */
 #define RATE_ADAPTIVE_TIMER_PERIOD 300
 
-typedef enum _WIRELESS_MODE {
+enum wireless_mode {
 	WIRELESS_MODE_UNKNOWN = 0x00,
 	WIRELESS_MODE_A = 0x01,
 	WIRELESS_MODE_B = 0x02,
 	WIRELESS_MODE_G = 0x04,
 	WIRELESS_MODE_AUTO = 0x08,
-} WIRELESS_MODE;
+};
 
 struct chnl_access_setting {
 	u16 sifs_timer;
