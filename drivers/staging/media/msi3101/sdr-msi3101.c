4005a97109a0502c57bd07372121ab36a41a550f
Antti Palosaari <crope@iki.fi>
2014-03-10 16:30:44 -0300

    [media] msi3101: fix v4l2-compliance issues
    Fix msi3101 driver v4l2-compliance issues.
    
    Cc: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/staging/media/msi3101/sdr-msi3101.c
+++ drivers/staging/media/msi3101/sdr-msi3101.c
@@ -1135,6 +1135,7 @@ static int msi3101_g_fmt_sdr_cap(struct file *file, void *priv,
 	dev_dbg(&s->udev->dev, "%s: pixelformat fourcc %4.4s\n", __func__,
 			(char *)&s->pixelformat);
 
+	memset(f->fmt.sdr.reserved, 0, sizeof(f->fmt.sdr.reserved));
 	f->fmt.sdr.pixelformat = s->pixelformat;
 
 	return 0;
@@ -1152,6 +1153,7 @@ static int msi3101_s_fmt_sdr_cap(struct file *file, void *priv,
 	if (vb2_is_busy(q))
 		return -EBUSY;
 
+	memset(f->fmt.sdr.reserved, 0, sizeof(f->fmt.sdr.reserved));
 	for (i = 0; i < NUM_FORMATS; i++) {
 		if (formats[i].pixelformat == f->fmt.sdr.pixelformat) {
 			s->pixelformat = f->fmt.sdr.pixelformat;
@@ -1173,6 +1175,7 @@ static int msi3101_try_fmt_sdr_cap(struct file *file, void *priv,
 	dev_dbg(&s->udev->dev, "%s: pixelformat fourcc %4.4s\n", __func__,
 			(char *)&f->fmt.sdr.pixelformat);
 
+	memset(f->fmt.sdr.reserved, 0, sizeof(f->fmt.sdr.reserved));
 	for (i = 0; i < NUM_FORMATS; i++) {
 		if (formats[i].pixelformat == f->fmt.sdr.pixelformat)
 			return 0;
@@ -1234,6 +1237,7 @@ static int msi3101_g_frequency(struct file *file, void *priv,
 		f->frequency = s->f_adc;
 		ret = 0;
 	} else if (f->tuner == 1) {
+		f->type = V4L2_TUNER_RF;
 		ret = v4l2_subdev_call(s->v4l2_subdev, tuner, g_frequency, f);
 	} else {
 		ret = -EINVAL;
