f9292b36e87def495aef64fd55feb1005d1f54b6
Hans Verkuil <hans.verkuil@cisco.com>
2014-03-10 12:23:13 -0300

    [media] vb2: move __qbuf_mmap before __qbuf_userptr
    __qbuf_mmap was sort of hidden in between the much larger __qbuf_userptr
    and __qbuf_dmabuf functions. Move it before __qbuf_userptr which is
    also conform the usual order these memory models are implemented: first
    mmap, then userptr, then dmabuf.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Acked-by: Pawel Osciak <pawel@osciak.com>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/v4l2-core/videobuf2-core.c
+++ drivers/media/v4l2-core/videobuf2-core.c
@@ -1306,6 +1306,15 @@ static void __fill_vb2_buffer(struct vb2_buffer *vb, const struct v4l2_buffer *b
 }
 
 /**
+ * __qbuf_mmap() - handle qbuf of an MMAP buffer
+ */
+static int __qbuf_mmap(struct vb2_buffer *vb, const struct v4l2_buffer *b)
+{
+	__fill_vb2_buffer(vb, b, vb->v4l2_planes);
+	return call_vb_qop(vb, buf_prepare, vb);
+}
+
+/**
  * __qbuf_userptr() - handle qbuf of a USERPTR buffer
  */
 static int __qbuf_userptr(struct vb2_buffer *vb, const struct v4l2_buffer *b)
@@ -1406,15 +1415,6 @@ err:
 	}
 
 	return ret;
-}
-
-/**
- * __qbuf_mmap() - handle qbuf of an MMAP buffer
- */
-static int __qbuf_mmap(struct vb2_buffer *vb, const struct v4l2_buffer *b)
-{
-	__fill_vb2_buffer(vb, b, vb->v4l2_planes);
-	return call_vb_qop(vb, buf_prepare, vb);
 }
 
 /**
