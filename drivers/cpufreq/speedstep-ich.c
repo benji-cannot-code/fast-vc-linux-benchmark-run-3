835b5c6cc0fdd7bdda853f3703ee570a7f497850
Viresh Kumar <viresh.kumar@linaro.org>
2014-03-10 14:53:34 +0530

    cpufreq: Remove cpufreq_generic_exit()
    cpufreq_generic_exit() is empty now and can be deleted.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/speedstep-ich.c
+++ drivers/cpufreq/speedstep-ich.c
@@ -312,7 +312,6 @@ static struct cpufreq_driver speedstep_driver = {
 	.verify	= cpufreq_generic_frequency_table_verify,
 	.target_index = speedstep_target,
 	.init	= speedstep_cpu_init,
-	.exit	= cpufreq_generic_exit,
 	.get	= speedstep_get,
 	.attr	= cpufreq_generic_attr,
 };
