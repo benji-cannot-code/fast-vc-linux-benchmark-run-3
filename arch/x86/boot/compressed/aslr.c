24f7d608012e8593f79f8ac516afae00aa4a8ea0
Josh Triplett <josh@joshtriplett.org>
2014-03-10 13:11:26 -0700

    x86, boot: Don't compile aslr.c when !CONFIG_RANDOMIZE_BASE
    All the code in aslr.c gets compiled out if !CONFIG_RANDOMIZE_BASE, but
    aslr.o itself still gets compiled in.  Eliminate it from the compile
    entirely in that case.
    
    This does not change the generated code at all, in either case.
    
    Signed-off-by: Josh Triplett <josh@joshtriplett.org>

--- arch/x86/boot/compressed/aslr.c
+++ arch/x86/boot/compressed/aslr.c
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "misc.h"
 
-#ifdef CONFIG_RANDOMIZE_BASE
 #include <asm/msr.h>
 #include <asm/archrandom.h>
 #include <asm/e820.h>
@@ -321,5 +320,3 @@ unsigned char *choose_kernel_location(unsigned char *input,
 out:
 	return (unsigned char *)choice;
 }
-
-#endif /* CONFIG_RANDOMIZE_BASE */
