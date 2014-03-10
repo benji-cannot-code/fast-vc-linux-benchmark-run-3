bf32f4f348907fb192eb0e4b9ee675c8462ca73e
Scott Wood <scottwood@freescale.com>
2014-03-10 17:29:38 -0500

    powerpc/booke64: Use SPRG_TLB_EXFRAME on bolted handlers
    While bolted handlers (including e6500) do not need to deal with a TLB
    miss recursively causing another TLB miss, nested TLB misses can still
    happen with crit/mc/debug exceptions -- so we still need to honor
    SPRG_TLB_EXFRAME.
    
    We don't need to spend time modifying it in the TLB miss fastpath,
    though -- the special level exception will handle that.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Cc: Mihai Caraman <mihai.caraman@freescale.com>
    Cc: kvm-ppc@vger.kernel.org

--- arch/powerpc/include/asm/kvm_booke_hv_asm.h
+++ arch/powerpc/include/asm/kvm_booke_hv_asm.h
@@ -46,10 +46,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Expected inputs (TLB exception type):
  *  r10 = saved CR
+ *  r12 = extlb pointer
  *  r13 = PACA_POINTER
- *  *(r13 + PACA_EX##type + EX_TLB_R10) = saved r10
- *  *(r13 + PACA_EX##type + EX_TLB_R11) = saved r11
- *  SPRN_SPRG_GEN_SCRATCH = saved r13
+ *  *(r12 + EX_TLB_R10) = saved r10
+ *  *(r12 + EX_TLB_R11) = saved r11
+ *  *(r12 + EX_TLB_R13) = saved r13
+ *  SPRN_SPRG_GEN_SCRATCH = saved r12
  *
  * Only the bolted version of TLB miss exception handlers is supported now.
  */
