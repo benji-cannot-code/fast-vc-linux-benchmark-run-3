5bcb42f9fea8b469297036256f40e3a149a1120d
Matt Porter <mporter@linaro.org>
2014-03-11 19:33:54 -0400

    mfd: Add bcm590xx pmu driver
    Add a driver for the BCM590xx PMU multi-function devices. The driver
    initially supports regmap initialization and instantiation of the
    voltage regulator device function of the PMU.
    
    Signed-off-by: Matt Porter <mporter@linaro.org>
    Reviewed-by: Tim Kryger <tim.kryger@linaro.org>
    Reviewed-by: Markus Mayer <markus.mayer@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

---
+++ include/linux/mfd/bcm590xx.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Broadcom BCM590xx PMU
+ *
+ * Copyright 2014 Linaro Limited
+ * Author: Matt Porter <mporter@linaro.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under  the terms of the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ */
+
+#ifndef __LINUX_MFD_BCM590XX_H
+#define __LINUX_MFD_BCM590XX_H
+
+#include <linux/device.h>
+#include <linux/i2c.h>
+#include <linux/regmap.h>
+
+/* max register address */
+#define BCM590XX_MAX_REGISTER	0xe7
+
+struct bcm590xx {
+	struct device *dev;
+	struct i2c_client *i2c_client;
+	struct regmap *regmap;
+	unsigned int id;
+};
+
+#endif /*  __LINUX_MFD_BCM590XX_H */
