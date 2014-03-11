cacbf2abe9f060ac58feb2fc0e1adbbcfb40a466
Stephen Boyd <sboyd@codeaurora.org>
2014-03-11 13:51:49 -0700

    irqchip: Remove unused include
    The "irqchip.h" include here is not needed as the only thing in
    irqchip.h is IRQCHIP_DECLARE which this file doesn't use. Drop
    it.
    
    Reported-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Link: http://lkml.kernel.org/r/531F7765.40207@codeaurora.org
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/irqchip/irqchip.c
+++ drivers/irqchip/irqchip.c
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/irqchip.h>
 
-#include "irqchip.h"
-
 /*
  * This special of_device_id is the sentinel at the end of the
  * of_device_id[] array of all irqchips. It is automatically placed at
