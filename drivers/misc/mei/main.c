2f3bce61968b9ac54f5d6e008f2fd8b1bd87d490
Tomas Winkler <tomas.winkler@intel.com>
2014-03-11 14:49:24 +0200

    mei: drop pr_fmt macros
    we are using dev_ logging facility so we don't need pr_fmt macros
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Reviewed-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/main.c
+++ drivers/misc/mei/main.c
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * more details.
  *
  */
-
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
