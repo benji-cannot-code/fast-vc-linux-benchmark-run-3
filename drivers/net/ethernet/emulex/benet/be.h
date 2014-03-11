b9cd2935aa2758c392f0ea2e88b0c9e767baf9a8
Sathya Perla <sathya.perla@emulex.com>
2014-03-11 18:53:09 +0530

    be2net: update driver version to 10.2
    Signed-off-by: Sathya Perla <sathya.perla@emulex.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/emulex/benet/be.h
+++ drivers/net/ethernet/emulex/benet/be.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "be_hw.h"
 #include "be_roce.h"
 
-#define DRV_VER			"10.0.600.0u"
+#define DRV_VER			"10.2u"
 #define DRV_NAME		"be2net"
 #define BE_NAME			"Emulex BladeEngine2"
 #define BE3_NAME		"Emulex BladeEngine3"
