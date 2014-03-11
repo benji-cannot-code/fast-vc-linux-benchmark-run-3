d28490765523da63c1bc238ce5e5e86c1e89a73e
Mark Hounschell <markh@compro.net>
2014-03-11 10:11:43 -0400

    staging: dgap: Replace/remove DGAP_SPINLOCK_INIT macro
    This patch gets rid of the DGAP_SPINLOCK_INIT macro
    
    Signed-off-by: Mark Hounschell <markh@compro.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap.c
+++ drivers/staging/dgap/dgap.c
@@ -755,7 +755,7 @@ static int dgap_found_board(struct pci_dev *pdev, int id)
 	brd->dpastatus = BD_NOFEP;
 	init_waitqueue_head(&brd->state_wait);
 
-	DGAP_SPINLOCK_INIT(brd->bd_lock);
+	spin_lock_init(&brd->bd_lock);
 
 	brd->state		= BOARD_FOUND;
 	brd->runwait		= 0;
@@ -1423,7 +1423,7 @@ static int dgap_tty_init(struct board_t *brd)
 		if (!brd->channels[i])
 			continue;
 
-		DGAP_SPINLOCK_INIT(ch->ch_lock);
+		spin_lock_init(&ch->ch_lock);
 
 		/* Store all our magic numbers */
 		ch->magic = DGAP_CHANNEL_MAGIC;
