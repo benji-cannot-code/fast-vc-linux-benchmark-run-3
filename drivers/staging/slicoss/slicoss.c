986d1126a203cd75cd4d040f4bb495309707f821
Gulsah Kose <gulsah.1004@gmail.com>
2014-03-13 00:34:53 +0200

    staging: slicoss: Fix unnecessary line continuations
    Removed unnecessary line continuations in slicoss.c
    
    Signed-off-by: Gulsah Kose <gulsah.1004@gmail.com>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>

--- drivers/staging/slicoss/slicoss.c
+++ drivers/staging/slicoss/slicoss.c
@@ -101,11 +101,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "slic.h"
 
 static uint slic_first_init = 1;
-static char *slic_banner = "Alacritech SLIC Technology(tm) Server "\
+static char *slic_banner = "Alacritech SLIC Technology(tm) Server "
 		"and Storage Accelerator (Non-Accelerated)";
 
 static char *slic_proc_version = "2.0.351  2006/07/14 12:26:00";
-static char *slic_product_name = "SLIC Technology(tm) Server "\
+static char *slic_product_name = "SLIC Technology(tm) Server "
 		"and Storage Accelerator (Non-Accelerated)";
 static char *slic_vendor = "Alacritech, Inc.";
 
