a09ad4b0705c374b59c34e02bf544f89586ae8a1
Michal Simek <michal.simek@xilinx.com>
2014-03-12 10:18:30 +0100

    microblaze: Wire-up new system calls sched_setattr/getattr
    Wire-up sched_setattr/getattr syscalls.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/uapi/asm/unistd.h
+++ arch/microblaze/include/uapi/asm/unistd.h
@@ -397,5 +397,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_process_vm_writev	378
 #define __NR_kcmp		379
 #define __NR_finit_module	380
+#define __NR_sched_setattr	381
+#define __NR_sched_getattr	382
 
 #endif /* _UAPI_ASM_MICROBLAZE_UNISTD_H */
