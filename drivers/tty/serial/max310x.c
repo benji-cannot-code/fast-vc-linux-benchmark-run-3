606b5136a2df50f5ff1f65a4153aadf300f3bbe5
Geert Uytterhoeven <geert@linux-m68k.org>
2014-03-12 15:01:54 +0100

    serial: max310x: Add missing #include <linux/uaccess.h>
    cris allmodconfig:
    
    drivers/tty/serial/max310x.c: In function 'max310x_ioctl':
    drivers/tty/serial/max310x.c:885:3: error: implicit declaration of function 'copy_from_user' [-Werror=implicit-function-declaration]
    drivers/tty/serial/max310x.c:916:3: error: implicit declaration of function 'copy_to_user' [-Werror=implicit-function-declaration]
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/max310x.c
+++ drivers/tty/serial/max310x.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
 #include <linux/spi/spi.h>
+#include <linux/uaccess.h>
 
 #define MAX310X_NAME			"max310x"
 #define MAX310X_MAJOR			204
