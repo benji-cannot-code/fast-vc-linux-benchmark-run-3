cfd59205da8b5f2e46085c1321c7a04d2a814fc5
Andreea-Cristina Bernat <bernat.ada@gmail.com>
2014-03-12 23:00:04 +0200

    staging: rtl8712: Delete space before semicolon
    This patch solves the warning: "space prohibited before semicolon".
    
    Signed-off-by: Andreea-Cristina Bernat <bernat.ada@gmail.com>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Peter P Waskiewicz Jr <peter.p.waskiewicz.jr@intel.com>

--- drivers/staging/rtl8712/osdep_service.h
+++ drivers/staging/rtl8712/osdep_service.h
@@ -165,7 +165,7 @@ static inline void sleep_schedulable(int ms)
 		delta = 1;/* 1 ms */
 	set_current_state(TASK_INTERRUPTIBLE);
 	if (schedule_timeout(delta) != 0)
-		return ;
+		return;
 }
 
 static inline u8 *_malloc(u32 sz)
