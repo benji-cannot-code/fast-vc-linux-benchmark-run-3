cdba14f8d079f7b598224c7921436150149510e8
Hanjun Guo <hanjun.guo@linaro.org>
2014-03-13 12:47:39 +0800

    ACPI: Remove duplicate definitions of PREFIX
    We already have a macro for PREFIX of "ACPI: " in
    drivers/acpi/internal.h, so remove the duplicate ones
    in ACPI drivers when internal.h is included.
    
    Signed-off-by: Hanjun Guo <hanjun.guo@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpi_cmos_rtc.c
+++ drivers/acpi/acpi_cmos_rtc.c
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "internal.h"
 
-#define PREFIX "ACPI: "
-
 ACPI_MODULE_NAME("cmos rtc");
 
 static const struct acpi_device_id acpi_cmos_rtc_ids[] = {
--- drivers/acpi/processor_core.c
+++ drivers/acpi/processor_core.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "internal.h"
 
-#define PREFIX			"ACPI: "
 #define _COMPONENT		ACPI_PROCESSOR_COMPONENT
 ACPI_MODULE_NAME("processor_core");
 
