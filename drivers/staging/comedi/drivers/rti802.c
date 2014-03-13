1b7e017f75a4c8698995641b80275788c401f323
H Hartley Sweeten <hartleys@visionengravers.com>
2014-03-13 15:48:37 -0700

    staging: comedi: rti802: remove RTI802_SIZE define
    This define is only used in the attach to specify the I/O region size
    passed to comedi_request_region(). Remove the define and just open
    code the value.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/rti802.c
+++ drivers/staging/comedi/drivers/rti802.c
@@ -36,8 +36,6 @@ Configuration Options:
 #include <linux/module.h>
 #include "../comedidev.h"
 
-#define RTI802_SIZE 4
-
 #define RTI802_SELECT 0
 #define RTI802_DATALOW 1
 #define RTI802_DATAHIGH 2
@@ -89,7 +87,7 @@ static int rti802_attach(struct comedi_device *dev, struct comedi_devconfig *it)
 	int i;
 	int ret;
 
-	ret = comedi_request_region(dev, it->options[0], RTI802_SIZE);
+	ret = comedi_request_region(dev, it->options[0], 0x04);
 	if (ret)
 		return ret;
 
