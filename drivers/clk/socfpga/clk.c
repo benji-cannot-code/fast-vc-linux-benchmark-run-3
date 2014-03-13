3dfd952f8fca943a532d7245c1c644c9927e840f
Dinh Nguyen <dinguyen@altera.com>
2014-03-13 16:08:01 -0500

    clk: socfpga: Fix section mismatch warning
    WARNING: drivers/clk/socfpga/built-in.o(.data+0xc0): Section mismatch in
    reference from the variable socfpga_child_clocks to the function
    .init.text:socfpga_pll_init()
    The variable socfpga_child_clocks references
    the function __init socfpga_pll_init()
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console
    
    WARNING: drivers/clk/socfpga/built-in.o(.data+0x184): Section mismatch in
    reference from the variable socfpga_child_clocks to the function
    .init.text:socfpga_periph_init()
    The variable socfpga_child_clocks references
    the function __init socfpga_periph_init()
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console
    
    WARNING: drivers/clk/socfpga/built-in.o(.data+0x248): Section mismatch in
    reference from the variable socfpga_child_clocks to the function
    .init.text:socfpga_gate_init()
    The variable socfpga_child_clocks references
    the function __init socfpga_gate_init()
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console
    
    Reported-by: Mike Turquette <mturquette@linaro.org>
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- drivers/clk/socfpga/clk.c
+++ drivers/clk/socfpga/clk.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void __iomem *clk_mgr_base_addr;
 
-static struct of_device_id socfpga_child_clocks[] = {
+static const struct of_device_id socfpga_child_clocks[] __initconst = {
 	{ .compatible = "altr,socfpga-pll-clock", socfpga_pll_init, },
 	{ .compatible = "altr,socfpga-perip-clk", socfpga_periph_init, },
 	{ .compatible = "altr,socfpga-gate-clk", socfpga_gate_init, },
