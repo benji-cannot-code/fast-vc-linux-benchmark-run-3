bb8123c9fb03c62c51c43822a30717e5c1fedd0e
Cody P Schafer <cody@linux.vnet.ibm.com>
2014-03-14 16:00:40 +1100

    powerpc/perf: Add macros for defining event fields & formats
    Add two macros which generate functions to extract the relevent bits
    from event->attr.config{,1,2}.
    
    EVENT_DEFINE_RANGE() defines an accessor for a range of bits in the
    event, as well as a "max" function that gives the maximum value of the
    field based on the bit width.
    
    EVENT_DEFINE_RANGE_FORMAT() defines the accessor & max routine and also
    a format attribute for use in the PMU's attr_groups.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>
    [mpe: move to powerpc, ugly but descriptive macro names]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/perf/hv-common.h
+++ arch/powerpc/perf/hv-common.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef LINUX_POWERPC_PERF_HV_COMMON_H_
 #define LINUX_POWERPC_PERF_HV_COMMON_H_
 
+#include <linux/perf_event.h>
 #include <linux/types.h>
 
 struct hv_perf_caps {
@@ -14,5 +15,23 @@ struct hv_perf_caps {
 };
 
 unsigned long hv_perf_caps_get(struct hv_perf_caps *caps);
+
+
+#define EVENT_DEFINE_RANGE_FORMAT(name, attr_var, bit_start, bit_end)	\
+PMU_FORMAT_ATTR(name, #attr_var ":" #bit_start "-" #bit_end);		\
+EVENT_DEFINE_RANGE(name, attr_var, bit_start, bit_end)
+
+#define EVENT_DEFINE_RANGE(name, attr_var, bit_start, bit_end)	\
+static u64 event_get_##name##_max(void)					\
+{									\
+	BUILD_BUG_ON((bit_start > bit_end)				\
+		    || (bit_end >= (sizeof(1ull) * 8)));		\
+	return (((1ull << (bit_end - bit_start)) - 1) << 1) + 1;	\
+}									\
+static u64 event_get_##name(struct perf_event *event)			\
+{									\
+	return (event->attr.attr_var >> (bit_start)) &			\
+		event_get_##name##_max();				\
+}
 
 #endif
