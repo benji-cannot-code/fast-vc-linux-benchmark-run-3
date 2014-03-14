81a3f25a9a25dfb8b1685260d5e3d7e2e033c770
Michael Ellerman <mpe@ellerman.id.au>
2014-03-14 16:00:34 +1100

    powerpc/perf: Enable BHRB access for EBB events
    The previous commit added constraint and register handling to allow
    processes using EBB (Event Based Branches) to request access to the BHRB
    (Branch History Rolling Buffer).
    
    With that in place we can allow processes using EBB to access the BHRB.
    This is achieved by setting BHRBA in MMCR0 when we enable EBB access. We
    must also clear BHRBA when we are disabling.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -665,6 +665,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   MMCR0_PMXE	0x04000000UL /* performance monitor exception enable */
 #define   MMCR0_FCECE	0x02000000UL /* freeze ctrs on enabled cond or event */
 #define   MMCR0_TBEE	0x00400000UL /* time base exception enable */
+#define   MMCR0_BHRBA	0x00200000UL /* BHRB Access allowed in userspace */
 #define   MMCR0_EBE	0x00100000UL /* Event based branch enable */
 #define   MMCR0_PMCC	0x000c0000UL /* PMC control */
 #define   MMCR0_PMCC_U6	0x00080000UL /* PMC1-6 are R/W by user (PR) */
