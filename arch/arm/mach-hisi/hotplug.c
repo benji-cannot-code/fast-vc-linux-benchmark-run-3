13d848f3d019b228ddbea254eba1eb9a61180d98
Arnd Bergmann <arnd@arndb.de>
2014-03-15 11:47:14 +0100

    ARM: hisi: fix building without CONFIG_HOTPLUG_CPU
    The hisi SMP code always uses the hi3xxx_set_cpu() function
    defined in the hotplug.c file, so we cannot build without
    this when CONFIG_SMP is enabled. This patch slightly restructures
    the code so we always build the parts of hotplug.c that we need
    but just leave out the CPU disable logic if CONFIG_HOTPLUG_CPU
    is turned off.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- arch/arm/mach-hisi/hotplug.c
+++ arch/arm/mach-hisi/hotplug.c
@@ -179,6 +179,7 @@ static inline void cpu_enter_lowpower(void)
 	  : "cc");
 }
 
+#ifdef CONFIG_HOTPLUG_CPU
 void hi3xxx_cpu_die(unsigned int cpu)
 {
 	cpu_enter_lowpower();
@@ -199,3 +200,4 @@ int hi3xxx_cpu_kill(unsigned int cpu)
 	hi3xxx_set_cpu(cpu, false);
 	return 1;
 }
+#endif
