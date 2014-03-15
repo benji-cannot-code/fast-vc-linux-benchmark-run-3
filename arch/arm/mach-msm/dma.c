e1fcca81b37575f5ec37c8b18c7dfd8549e271ac
Arnd Bergmann <arnd@arndb.de>
2014-03-15 11:39:30 +0100

    ARM: msm: export legacy DMA interfaces
    The msm_sdcc MMC driver and the msm_serial_hs uart driver both
    use the pre-dmaengine interfaces provided by the MSM platform.
    
    Since these drivers can be loadable modules, we should export
    the functions used in the drivers.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: David Brown <davidb@codeaurora.org>
    Cc: Daniel Walker <dwalker@fifo99.com>
    Cc: Bryan Huntsman <bryanh@codeaurora.org>

--- arch/arm/mach-msm/dma.c
+++ arch/arm/mach-msm/dma.c
@@ -79,6 +79,7 @@ void msm_dmov_stop_cmd(unsigned id, struct msm_dmov_cmd *cmd, int graceful)
 {
 	writel((graceful << 31), DMOV_FLUSH0(id));
 }
+EXPORT_SYMBOL_GPL(msm_dmov_stop_cmd);
 
 void msm_dmov_enqueue_cmd(unsigned id, struct msm_dmov_cmd *cmd)
 {
@@ -117,6 +118,7 @@ void msm_dmov_enqueue_cmd(unsigned id, struct msm_dmov_cmd *cmd)
 	}
 	spin_unlock_irqrestore(&msm_dmov_lock, irq_flags);
 }
+EXPORT_SYMBOL_GPL(msm_dmov_enqueue_cmd);
 
 struct msm_dmov_exec_cmdptr_cmd {
 	struct msm_dmov_cmd dmov_cmd;
