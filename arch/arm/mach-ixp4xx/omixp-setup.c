c3e8b5363487cd00c851b5370a85478959f87774
Arnd Bergmann <arnd@arndb.de>
2014-03-15 11:52:49 +0100

    ARM: ixp4xx/omixp: always include linux/leds.h
    The omixp board code unconditionally defines a gpio-led
    device, but for some reason includes the header file
    for this only if CONFIG_LEDS_CLASS is enabled, causing
    a build error otherwise.
    
    Removing the #ifdef fixes the build error with no downsides
    whatsoever.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Imre Kaloz <kaloz@openwrt.org>
    Cc: Krzysztof Halasa <khc@pm.waw.pl>

--- arch/arm/mach-ixp4xx/omixp-setup.c
+++ arch/arm/mach-ixp4xx/omixp-setup.c
@@ -18,9 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial_8250.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
-#ifdef CONFIG_LEDS_CLASS
 #include <linux/leds.h>
-#endif
 
 #include <asm/setup.h>
 #include <asm/memory.h>
