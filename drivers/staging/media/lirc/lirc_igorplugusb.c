83a32d5f6e900e788aaa308cba152f12e4af861d
Andreea-Cristina Bernat <bernat.ada@gmail.com>
2014-03-16 22:21:41 +0200

    Staging: lirc: Fix warning: assignment of bool to 0/1
    This patch solves the warning "Assignment of bool to 0/1"
    
    Signed-off-by: Andreea-Cristina Bernat <bernat.ada@gmail.com>
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/lirc/lirc_igorplugusb.c
+++ drivers/staging/media/lirc/lirc_igorplugusb.c
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* debugging support */
 #ifdef CONFIG_USB_DEBUG
-static bool debug = 1;
+static bool debug = true;
 #else
 static bool debug;
 #endif
