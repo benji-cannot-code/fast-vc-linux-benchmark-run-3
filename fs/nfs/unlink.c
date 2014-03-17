11c720a5fcfce605452debdd50559d494aeeb9f2
Jeff Layton <jlayton@redhat.com>
2014-03-17 07:06:58 -0400

    nfs: emit a fsnotify_nameremove call in sillyrename codepath
    If a file is sillyrenamed, then the generic vfs_unlink code will skip
    emitting fsnotify events for it.
    
    This patch has the sillyrename code do that instead.
    
    In truth this is a little bit odd since we aren't actually removing the
    dentry per-se, but renaming it. Still, this is probably the right thing
    to do since it's what userland apps expect to see when an unlink()
    occurs or some file is renamed on top of the dentry.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Tested-by: Anna Schumaker <Anna.Schumaker@netapp.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/unlink.c
+++ fs/nfs/unlink.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/wait.h>
 #include <linux/namei.h>
+#include <linux/fsnotify.h>
 
 #include "internal.h"
 #include "nfs4_fs.h"
@@ -466,8 +467,18 @@ nfs_async_rename(struct inode *old_dir, struct inode *new_dir,
 static void
 nfs_complete_sillyrename(struct rpc_task *task, struct nfs_renamedata *data)
 {
-	if (task->tk_status != 0)
-		nfs_cancel_async_unlink(data->old_dentry);
+	struct dentry *dentry = data->old_dentry;
+
+	if (task->tk_status != 0) {
+		nfs_cancel_async_unlink(dentry);
+		return;
+	}
+
+	/*
+	 * vfs_unlink and the like do not issue this when a file is
+	 * sillyrenamed, so do it here.
+	 */
+	fsnotify_nameremove(dentry, 0);
 }
 
 #define SILLYNAME_PREFIX ".nfs"
