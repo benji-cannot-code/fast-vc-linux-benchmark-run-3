6efccda6be7c434ac538ba1816989f4e685582f4
Vivek Goyal <vgoyal@redhat.com>
2014-03-18 15:26:36 -0400

    x86, boot: Undef memcmp before providing a new definition
    With CONFIG_X86_32=y, string_32.h gets pulled in compressed/string.c by
    "misch.h". string_32.h defines a macro to map memcmp to __builtin_memcmp().
    And that macro in turn changes the name of memcmp() defined here and
    converts it to __builtin_memcmp().
    
    I thought that's not the intention though. We probably want to provide
    our own optimized definition of memcmp(). If yes, then undef the memcmp
    before we define a new memcmp.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Link: http://lkml.kernel.org/r/1395170800-11059-2-git-send-email-vgoyal@redhat.com
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/boot/compressed/string.c
+++ arch/x86/boot/compressed/string.c
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "misc.h"
 
+/* Avoid intereference from any defines in string_32.h */
+#undef memcmp
 int memcmp(const void *s1, const void *s2, size_t len)
 {
 	u8 diff;
