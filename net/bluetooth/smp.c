57b22f095fb19af2cde5b75da4f94903566b7cd3
Johan Hedberg <johan.hedberg@intel.com>
2014-03-18 12:58:23 +0200

    Bluetooth: Fix const declaration for swap function src parameter
    To make it possible to (correctly) pass data declared as const as the
    src parameter to the swap56 and swap128 functions declare this parameter
    also as const.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/smp.c
+++ net/bluetooth/smp.c
@@ -36,14 +36,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define AUTH_REQ_MASK   0x07
 
-static inline void swap128(u8 src[16], u8 dst[16])
+static inline void swap128(const u8 src[16], u8 dst[16])
 {
 	int i;
 	for (i = 0; i < 16; i++)
 		dst[15 - i] = src[i];
 }
 
-static inline void swap56(u8 src[7], u8 dst[7])
+static inline void swap56(const u8 src[7], u8 dst[7])
 {
 	int i;
 	for (i = 0; i < 7; i++)
