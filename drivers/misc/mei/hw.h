449f68681a163e993834ddb7bb0ef6b884747165
Tomas Winkler <tomas.winkler@intel.com>
2014-03-18 22:52:00 +0200

    mei: me: add pg exit and entry flow commands
    For power gating entry we write hbm pg entry request command and
    then we set pg register
    For power gating exit we clear pg register and wait for exit request
    hbm command.
    
    Exit power gating request might also be initiated by the firmware
    w/o explicit driver request
    
    The power gating state is tracked by pg_state member of me_hw
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/hw.h
+++ drivers/misc/mei/hw.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MEI_IAMTHIF_STALL_TIMER    12  /* HPS */
 #define MEI_IAMTHIF_READ_TIMER     10  /* HPS */
 
+#define MEI_PGI_TIMEOUT            1  /* PG Isolation time response 1 sec */
 #define MEI_HBM_TIMEOUT            1   /* 1 second */
 
 /*
