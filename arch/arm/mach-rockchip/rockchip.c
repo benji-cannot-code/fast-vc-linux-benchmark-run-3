34e240af7b5af14e7786364ea87e857f58e62427
Russell King <rmk+kernel@arm.linux.org.uk>
2014-03-19 12:50:53 +0000

    ARM: l2c: remove unnecessary UL-suffix to mask values
    They're u32, they're not unsigned long.  The UL suffix is not required
    here.
    
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-rockchip/rockchip.c
+++ arch/arm/mach-rockchip/rockchip.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void __init rockchip_dt_init(void)
 {
-	l2x0_of_init(0, ~0UL);
+	l2x0_of_init(0, ~0);
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 }
 
