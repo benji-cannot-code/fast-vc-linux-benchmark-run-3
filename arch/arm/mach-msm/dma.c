af1f86980bdfbc7da81108eececbf869cff5d6df
Arnd Bergmann <arnd@arndb.de>
2014-03-19 18:29:36 +0100

    ARM: msm: add missing include of linux/module.h
    After the restructuring of the module.h and init.h headers,
    we now need to include this explicitly here.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: David Brown <davidb@codeaurora.org>
    Cc: Daniel Walker <dwalker@fifo99.com>
    Cc: Bryan Huntsman <bryanh@codeaurora.org>

--- arch/arm/mach-msm/dma.c
+++ arch/arm/mach-msm/dma.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/interrupt.h>
 #include <linux/completion.h>
+#include <linux/module.h>
 #include <mach/dma.h>
 #include <mach/msm_iomap.h>
 
