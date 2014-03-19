f14ad4c0f9e0554121c50eeb073fbdfce2d0ee5c
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2014-03-19 14:17:21 +0900

    f2fs: change reclaim rate in percentage
    It is more reasonable to determine the reclaiming rate of prefree segments
    according to the volume size, which is set to 5% by default.
    For example, if the volume is 128GB, the prefree segments are reclaimed
    when the number reaches to 6.4GB.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NULL_SEGNO			((unsigned int)(~0))
 #define NULL_SECNO			((unsigned int)(~0))
 
-#define DEF_RECLAIM_PREFREE_SEGMENTS	100	/* 200MB of prefree segments */
+#define DEF_RECLAIM_PREFREE_SEGMENTS	5	/* 5% over total segments */
 
 /* L: Logical segment # in volume, R: Relative segment # in main area */
 #define GET_L2R_SEGNO(free_i, segno)	(segno - free_i->start_segno)
