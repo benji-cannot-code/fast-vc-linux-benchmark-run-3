c8fa97d828ae7f142e08177ebefc9e41e3c7546e
Michael Welling <mwelling@ieee.org>
2014-03-18 23:24:31 -0500

    Staging: comedi: proc: title block update
    Follow up patch for "STAGING: comedi: style and checkpatch fixes".
    Removes the file name in the comment block per suggestions.
    
    Signed-off-by: Michael Welling <mwelling@ieee.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/proc.c
+++ drivers/staging/comedi/proc.c
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * module/proc.c
  * /proc interface for comedi
  *
  * COMEDI - Linux Control and Measurement Device Interface
