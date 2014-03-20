afc134065b51927a39ef5b45c3c97423100d3972
Lee Jones <lee.jones@linaro.org>
2014-03-20 09:20:39 +0000

    mtd: devices: Provide header for shared OPCODEs and SFDP commands
    JEDEC have helped to standardise a great deal of the commands which
    can be issued to a Serial Flash devices. Many of the Serial Flash
    Discoverable Parameters (SFDP) commands are generic across devices.
    This patch provides a shared point where these commands can be
    defined.
    
    Suggested-by: Mark Brown <broonie@kernel.org>
    Acked-by Angus Clark <angus.clark@st.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/devices/st_spi_fsm.c
+++ drivers/mtd/devices/st_spi_fsm.c
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/of.h>
 
+#include "serial_flash_cmds.h"
+
 /*
  * FSM SPI Controller Registers
  */
