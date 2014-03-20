b18c553a5c527d6f47f68d5f5f1d39ba99ebd7c4
Tushar Behera <tushar.behera@linaro.org>
2014-03-21 01:49:24 +0900

    ARM: SAMSUNG: Reorganize calls to reserve memory for MFC
    Reorganize code so that "plat/mfc.h" is no more referred
    from mach-exynos directory.
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/mach-exynos5-dt.c
+++ arch/arm/mach-exynos/mach-exynos5-dt.c
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 
 #include <asm/mach/arch.h>
-#include <plat/mfc.h>
 
 #include "common.h"
+#include "mfc.h"
 #include "regs-pmu.h"
 
 static void __init exynos5_dt_machine_init(void)
@@ -60,13 +60,7 @@ static char const *exynos5_dt_compat[] __initdata = {
 static void __init exynos5_reserve(void)
 {
 #ifdef CONFIG_S5P_DEV_MFC
-	struct s5p_mfc_dt_meminfo mfc_mem;
-
-	/* Reserve memory for MFC only if it's available */
-	mfc_mem.compatible = "samsung,mfc-v6";
-	if (of_scan_flat_dt(s5p_fdt_find_mfc_mem, &mfc_mem))
-		s5p_mfc_reserve_mem(mfc_mem.roff, mfc_mem.rsize, mfc_mem.loff,
-				mfc_mem.lsize);
+	of_scan_flat_dt(s5p_fdt_alloc_mfc_mem, "samsung,mfc-v6");
 #endif
 }
 
