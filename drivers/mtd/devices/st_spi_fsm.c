ab6427f86efd25682af0403d1706c95fc5f09b47
Lee Jones <lee.jones@linaro.org>
2014-03-20 11:11:45 +0000

    mtd: st_spi_fsm: Correct vendor name spelling issue - missing "M"
    Reported-by: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/devices/st_spi_fsm.c
+++ drivers/mtd/devices/st_spi_fsm.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Author: Angus Clark <angus.clark@st.com>
  *
- * Copyright (C) 2010-2014 STicroelectronics Limited
+ * Copyright (C) 2010-2014 STMicroelectronics Limited
  *
  * JEDEC probe based on drivers/mtd/devices/m25p80.c
  *
