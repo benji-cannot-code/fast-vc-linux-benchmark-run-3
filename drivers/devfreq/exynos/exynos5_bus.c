ec48fe9a2b7def85e7e87517488ea72ec59b5f91
Chanwoo Choi <cw00.choi@samsung.com>
2014-03-20 11:59:12 +0900

    PM / devfreq: exynos5: Use SIMPLE_DEV_PM_OPS macro
    This patch use SIMPLE_DEV_PM_OPS macro instead of legacy method.
    
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>
    Reviewed-by: Tomasz Figa <t.figa@samsung.com>
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

--- drivers/devfreq/exynos/exynos5_bus.c
+++ drivers/devfreq/exynos/exynos5_bus.c
@@ -430,6 +430,7 @@ static int exynos5_busfreq_int_remove(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_PM_SLEEP
 static int exynos5_busfreq_int_resume(struct device *dev)
 {
 	struct platform_device *pdev = container_of(dev, struct platform_device,
@@ -439,10 +440,12 @@ static int exynos5_busfreq_int_resume(struct device *dev)
 	busfreq_mon_reset(data);
 	return 0;
 }
-
 static const struct dev_pm_ops exynos5_busfreq_int_pm = {
 	.resume	= exynos5_busfreq_int_resume,
 };
+#endif
+static SIMPLE_DEV_PM_OPS(exynos5_busfreq_int_pm_ops, NULL,
+			 exynos5_busfreq_int_resume);
 
 /* platform device pointer for exynos5 devfreq device. */
 static struct platform_device *exynos5_devfreq_pdev;
@@ -453,7 +456,7 @@ static struct platform_driver exynos5_busfreq_int_driver = {
 	.driver		= {
 		.name		= "exynos5-bus-int",
 		.owner		= THIS_MODULE,
-		.pm		= &exynos5_busfreq_int_pm,
+		.pm		= &exynos5_busfreq_int_pm_ops,
 	},
 };
 
