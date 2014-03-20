dd02547ea0e0936f69ed68a93ec772987e7643b7
Lee Jones <lee.jones@linaro.org>
2014-03-20 09:21:07 +0000

    mtd: st_spi_fsm: Convert ST SPI FSM (NOR) Flash driver to new DT partitions
    The old API expected a "partitions" property provided a phandle to a
    separate partitions node, which itself contained yet more nodes each
    representing one partition. The new API rids the requirement for the
    superfluous intermediary partitions node. This patch provides the
    added information required for automatic parsing by the core.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/devices/st_spi_fsm.c
+++ drivers/mtd/devices/st_spi_fsm.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/mfd/syscon.h>
 #include <linux/mtd/mtd.h>
+#include <linux/mtd/partitions.h>
 #include <linux/sched.h>
 #include <linux/delay.h>
 #include <linux/io.h>
@@ -1998,6 +1999,7 @@ boot_device_fail:
 static int stfsm_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
+	struct mtd_part_parser_data ppdata;
 	struct flash_info *info;
 	struct resource *res;
 	struct stfsm *fsm;
@@ -2007,6 +2009,7 @@ static int stfsm_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "No DT found\n");
 		return -EINVAL;
 	}
+	ppdata.of_node = np;
 
 	fsm = devm_kzalloc(&pdev->dev, sizeof(*fsm), GFP_KERNEL);
 	if (!fsm)
@@ -2063,6 +2066,7 @@ static int stfsm_probe(struct platform_device *pdev)
 			return ret;
 	}
 
+	fsm->mtd.name		= info->name;
 	fsm->mtd.dev.parent	= &pdev->dev;
 	fsm->mtd.type		= MTD_NORFLASH;
 	fsm->mtd.writesize	= 4;
@@ -2082,7 +2086,7 @@ static int stfsm_probe(struct platform_device *pdev)
 		(long long)fsm->mtd.size, (long long)(fsm->mtd.size >> 20),
 		fsm->mtd.erasesize, (fsm->mtd.erasesize >> 10));
 
-	return mtd_device_parse_register(&fsm->mtd, NULL, NULL, NULL, 0);
+	return mtd_device_parse_register(&fsm->mtd, NULL, &ppdata, NULL, 0);
 }
 
 static int stfsm_remove(struct platform_device *pdev)
