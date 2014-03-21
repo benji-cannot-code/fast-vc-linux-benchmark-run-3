a15ab85abd9518e7d1be630084611c86df6e93f7
Max Filippov <jcmvbkbc@gmail.com>
2014-03-21 21:05:16 +0400

    xtensa: export __{invalidate,flush}_dcache_range
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/kernel/xtensa_ksyms.c
+++ arch/xtensa/kernel/xtensa_ksyms.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/in6.h>
 
 #include <asm/uaccess.h>
+#include <asm/cacheflush.h>
 #include <asm/checksum.h>
 #include <asm/dma.h>
 #include <asm/io.h>
@@ -128,4 +129,9 @@ EXPORT_SYMBOL(common_exception_return);
 
 #ifdef CONFIG_FUNCTION_TRACER
 EXPORT_SYMBOL(_mcount);
+#endif
+
+EXPORT_SYMBOL(__invalidate_dcache_range);
+#if XCHAL_DCACHE_IS_WRITEBACK
+EXPORT_SYMBOL(__flush_dcache_range);
 #endif
