5719552026baec52322a1efe23287c35fc6824fc
Max Filippov <jcmvbkbc@gmail.com>
2014-03-23 03:26:46 +0400

    xtensa: dump sysmem from the bootmem_init
    Debug dump of physical memory configuration. Useful for inspection of
    resulting memory map, esp. in the presence of memmap= kernel option.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/mm/init.c
+++ arch/xtensa/mm/init.c
@@ -33,6 +33,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct sysmem_info sysmem __initdata;
 
+static void __init sysmem_dump(void)
+{
+	unsigned i;
+
+	pr_debug("Sysmem:\n");
+	for (i = 0; i < sysmem.nr_banks; ++i)
+		pr_debug("  0x%08lx - 0x%08lx (%ldK)\n",
+			 sysmem.bank[i].start, sysmem.bank[i].end,
+			 (sysmem.bank[i].end - sysmem.bank[i].start) >> 10);
+}
+
 /*
  * Find bank with maximal .start such that bank.start <= start
  */
@@ -228,6 +239,7 @@ void __init bootmem_init(void)
 	unsigned long bootmap_start, bootmap_size;
 	int i;
 
+	sysmem_dump();
 	max_low_pfn = max_pfn = 0;
 	min_low_pfn = ~0;
 
