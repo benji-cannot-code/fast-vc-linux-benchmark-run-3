a6e15cdf4c5041c018ff410795bfad0dea93ef7c
Max Filippov <jcmvbkbc@gmail.com>
2014-03-23 03:17:43 +0400

    xtensa: split bootparam and kernel meminfo
    Bootparam meminfo is a bootloader ABI, kernel meminfo is for the kernel
    bookkeeping, keep them separate. Kernel doesn't care of memory region
    types, so drop the type field and don't pass it to add_sysmem_bank.
    Move kernel sysmem structures and prototypes to asm/sysmem.h and sysmem
    variable and add_sysmem_bank to mm/init.c
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

---
+++ arch/xtensa/include/asm/sysmem.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * sysmem-related prototypes.
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2014 Cadence Design Systems Inc.
+ */
+
+#ifndef _XTENSA_SYSMEM_H
+#define _XTENSA_SYSMEM_H
+
+#define SYSMEM_BANKS_MAX 31
+
+struct meminfo {
+	unsigned long start;
+	unsigned long end;
+};
+
+struct sysmem_info {
+	int nr_banks;
+	struct meminfo bank[SYSMEM_BANKS_MAX];
+};
+
+extern struct sysmem_info sysmem;
+
+int add_sysmem_bank(unsigned long start, unsigned long end);
+int mem_reserve(unsigned long, unsigned long, int);
+void bootmem_init(void);
+void zones_init(void);
+
+#endif /* _XTENSA_SYSMEM_H */
