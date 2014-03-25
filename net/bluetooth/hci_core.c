c97d260b2a5fc9914b4756a7fc213ef2deed08a5
Johan Hedberg <johan.hedberg@intel.com>
2014-03-25 12:06:18 +0200

    Bluetooth: Remove redundant NULL check
    All callers of hci_inquiry_cache_update() pass a non-NULL ssp pointer to
    it and even the function itself assumes in another place that the
    pointer is non-NULL. Therefore, remove the redundant check.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -2029,8 +2029,7 @@ bool hci_inquiry_cache_update(struct hci_dev *hdev, struct inquiry_data *data,
 
 	hci_remove_remote_oob_data(hdev, &data->bdaddr);
 
-	if (ssp)
-		*ssp = data->ssp_mode;
+	*ssp = data->ssp_mode;
 
 	ie = hci_inquiry_cache_lookup(hdev, &data->bdaddr);
 	if (ie) {
