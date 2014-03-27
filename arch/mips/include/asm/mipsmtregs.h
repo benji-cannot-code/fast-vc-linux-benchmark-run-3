b568238494edede82331ee92d3082fc24fb21299
Paul Burton <paul.burton@imgtec.com>
2014-03-27 10:57:01 +0000

    MIPS: MT: core_nvpes function to retrieve VPE count
    This function simply returns the number of VPEs present in the current
    core, or 1 if the core does not implement the MT ASE. In SMP kernels
    this will typically equal smp_num_siblings, however it will also be
    usable in UP kernels and helps prepare for the possibility of a
    heterogenous system where the VPE count is not the same across all
    cores.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/6665/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mipsmtregs.h
+++ arch/mips/include/asm/mipsmtregs.h
@@ -177,6 +177,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
+static inline unsigned core_nvpes(void)
+{
+	unsigned conf0;
+
+	if (!cpu_has_mipsmt)
+		return 1;
+
+	conf0 = read_c0_mvpconf0();
+	return ((conf0 & MVPCONF0_PVPE) >> MVPCONF0_PVPE_SHIFT) + 1;
+}
+
 static inline unsigned int dvpe(void)
 {
 	int res = 0;
