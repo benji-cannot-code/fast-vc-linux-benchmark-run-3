a26438ae8f4156d7c1c81502d57c4eea30306419
Linus Walleij <linus.walleij@linaro.org>
2014-03-27 14:18:51 +0100

    ARM: at91: localize GPIO header
    This moves the <mach/gpio.h> header in the AT91 platform down
    into the machine directory and removes the reliance on
    MACH_NEED_GPIO_H from the AT91.
    
    This does not move the platform to GENERIC_GPIO but localize
    the remaining work to be done for this to the mach-at91
    folder.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    [nicolas.ferre@atmel.com: adapt to newer kernel, add rsi-ews board]
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/at91sam9g45_devices.c
+++ arch/arm/mach-at91/at91sam9g45_devices.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board.h"
 #include "generic.h"
 #include "clock.h"
+#include "gpio.h"
 
 
 /* --------------------------------------------------------------------
