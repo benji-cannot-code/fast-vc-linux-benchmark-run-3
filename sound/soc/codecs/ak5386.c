e650a143272ba89315c134950762565127a2439d
Daniel Mack <zonque@gmail.com>
2014-03-28 19:05:04 +0100

    ASoC: ak5386: Add regulators to documentation and fix sparse warning
    Document the newly added regulators to the DT binding document.
    
    Also, "static const char const *x" is not identical to "static const
    char * const x", which sparse now complains about. Fix it.
    
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/ak5386.c
+++ sound/soc/codecs/ak5386.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm.h>
 #include <sound/initval.h>
 
-static const char const *supply_names[] = {
+static const char * const supply_names[] = {
 	"va", "vd"
 };
 
