c6db3a8e772fcbce188a43b898afece714386542
Daniel Thompson <daniel.thompson@linaro.org>
2014-03-28 11:41:38 +0000

    serial: mux: Align SUPPORT_SYSRQ behaviour with other drivers.
    The mux driver is anomalous among all the serial drivers that can
    define SUPPORT_SYSRQ because it can, with some configs, set
    SUPPORT_SYSRQ when SERIAL_CORE_CONSOLE is not set.
    
    Not only does this impose a pointless (but tiny) runtime overhead for
    such configs but, more significantly, it adds needless complexity when
    doing a code review to check for unexpected side effects of any
    changes to the serial core.
    
    This is (cross-)compile tested only because I do not have any PA-RISC
    hardware.
    
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/mux.c
+++ drivers/tty/serial/mux.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq.h>
 #include <asm/parisc-device.h>
 
-#ifdef CONFIG_MAGIC_SYSRQ
+#if defined(CONFIG_SERIAL_MUX_CONSOLE) && defined(CONFIG_MAGIC_SYSRQ)
 #include <linux/sysrq.h>
 #define SUPPORT_SYSRQ
 #endif
