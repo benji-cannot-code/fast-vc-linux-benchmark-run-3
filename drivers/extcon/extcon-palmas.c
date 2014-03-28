04a675ded4124e47835a201528367442d9188b13
Kishon Vijay Abraham I <kishon@ti.com>
2014-03-28 12:21:02 +0530

    extcon: palmas: explicitly set edev name as node name
    commit ca488 (extcon: of: Remove unnecessary function call by using the
    name of device_node) started using node name instead of device name
    to get the extcon device. This breaks dwc3-omap since it's not able to
    get the extcon device anymore. Fixed it by setting edev name of
    palmas to be the same as its node name.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Reported-by: Felipe Balbi <balbi@ti.com>
    Reviewed-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/extcon-palmas.c
+++ drivers/extcon/extcon-palmas.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
 #include <linux/err.h>
 #include <linux/mfd/palmas.h>
 #include <linux/of.h>
@@ -189,11 +190,13 @@ static int palmas_usb_probe(struct platform_device *pdev)
 
 	palmas_usb->edev.supported_cable = palmas_extcon_cable;
 	palmas_usb->edev.dev.parent = palmas_usb->dev;
+	palmas_usb->edev.name = kstrdup(node->name, GFP_KERNEL);
 	palmas_usb->edev.mutually_exclusive = mutually_exclusive;
 
 	status = extcon_dev_register(&palmas_usb->edev);
 	if (status) {
 		dev_err(&pdev->dev, "failed to register extcon device\n");
+		kfree(palmas_usb->edev.name);
 		return status;
 	}
 
@@ -231,6 +234,7 @@ static int palmas_usb_probe(struct platform_device *pdev)
 
 fail_extcon:
 	extcon_dev_unregister(&palmas_usb->edev);
+	kfree(palmas_usb->edev.name);
 
 	return status;
 }
@@ -240,6 +244,7 @@ static int palmas_usb_remove(struct platform_device *pdev)
 	struct palmas_usb *palmas_usb = platform_get_drvdata(pdev);
 
 	extcon_dev_unregister(&palmas_usb->edev);
+	kfree(palmas_usb->edev.name);
 
 	return 0;
 }
