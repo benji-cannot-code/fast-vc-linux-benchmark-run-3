b4822f320fa4b5a81ba97bb559b630f81d869af7
Goldwyn Rodrigues <rgoldwyn@suse.com>
2014-03-30 00:42:49 -0500

    Add node recovery callbacks
    DLM offers callbacks when a node fails and the lock remastery
    is performed:
    
    1. recover_prep: called when DLM discovers a node is down
    2. recover_slot: called when DLM identifies the node and recovery
    		can start
    3. recover_done: called when all nodes have completed recover_slot
    
    recover_slot() and recover_done() are also called when the node joins
    initially in order to inform the node with its slot number. These slot
    numbers start from one, so we deduct one to make it start with zero
    which the cluster-md code uses.
    
    Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.com>

--- drivers/md/md-cluster.h
+++ drivers/md/md-cluster.h
@@ -9,8 +9,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct mddev;
 
 struct md_cluster_operations {
-	int (*join)(struct mddev *mddev);
+	int (*join)(struct mddev *mddev, int nodes);
 	int (*leave)(struct mddev *mddev);
+	int (*slot_number)(struct mddev *mddev);
 };
 
 #endif /* _MD_CLUSTER_H */
