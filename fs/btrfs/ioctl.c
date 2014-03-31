1be57425f7a73d059acfdaef9afde63c81105410
Gui Hecheng <guihc.fnst@cn.fujitsu.com>
2014-03-31 18:03:25 +0800

    btrfs: filter invalid arg for btrfs resize
    Originally following cmds will work:
    	# btrfs fi resize -10A  <mnt>
    	# btrfs fi resize -10Gaha <mnt>
    Filter the arg by checking the return pointer of memparse.
    
    Signed-off-by: Gui Hecheng <guihc.fnst@cn.fujitsu.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -1473,6 +1473,7 @@ static noinline int btrfs_ioctl_resize(struct file *file,
 	struct btrfs_trans_handle *trans;
 	struct btrfs_device *device = NULL;
 	char *sizestr;
+	char *retptr;
 	char *devstr = NULL;
 	int ret = 0;
 	int mod = 0;
@@ -1540,8 +1541,8 @@ static noinline int btrfs_ioctl_resize(struct file *file,
 			mod = 1;
 			sizestr++;
 		}
-		new_size = memparse(sizestr, NULL);
-		if (new_size == 0) {
+		new_size = memparse(sizestr, &retptr);
+		if (*retptr != '\0' || new_size == 0) {
 			ret = -EINVAL;
 			goto out_free;
 		}
