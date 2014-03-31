081d113c40656761dbc9a9c1b386bb9b93e639e0
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-03-31 21:29:41 +0300

    drm/i915: Kill crtc->plane checks from the primary plane update hooks
    These were apparently meant to protect the SAREA which only has
    room for two pipes, but things clearly went a bit wonky when
    first the .update_plane() hooks were split up and then pipe C
    got introduced.
    
    The checks actually protecting the SAREA live in
    intel_crtc_update_sarea() these days, so the checks in the primary
    plane update hooks are just historical leftovers which are to be
    eliminated.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -2167,15 +2167,6 @@ static int i9xx_update_primary_plane(struct drm_crtc *crtc,
 	u32 dspcntr;
 	u32 reg;
 
-	switch (plane) {
-	case 0:
-	case 1:
-		break;
-	default:
-		DRM_ERROR("Can't update plane %c in SAREA\n", plane_name(plane));
-		return -EINVAL;
-	}
-
 	intel_fb = to_intel_framebuffer(fb);
 	obj = intel_fb->obj;
 
@@ -2267,16 +2258,6 @@ static int ironlake_update_primary_plane(struct drm_crtc *crtc,
 	unsigned long linear_offset;
 	u32 dspcntr;
 	u32 reg;
-
-	switch (plane) {
-	case 0:
-	case 1:
-	case 2:
-		break;
-	default:
-		DRM_ERROR("Can't update plane %c in SAREA\n", plane_name(plane));
-		return -EINVAL;
-	}
 
 	intel_fb = to_intel_framebuffer(fb);
 	obj = intel_fb->obj;
