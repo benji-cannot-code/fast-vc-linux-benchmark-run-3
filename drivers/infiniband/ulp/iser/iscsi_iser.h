8a2e46b10e13169bfdad991cd866e535c072d85f
Or Gerlitz <ogerlitz@mellanox.com>
2014-04-01 16:28:42 +0300

    IB/iser: Bump driver version to 1.3
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/infiniband/ulp/iser/iscsi_iser.h
+++ drivers/infiniband/ulp/iser/iscsi_iser.h
@@ -70,7 +70,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME	"iser"
 #define PFX		DRV_NAME ": "
-#define DRV_VER		"1.1"
+#define DRV_VER		"1.3"
 
 #define iser_dbg(fmt, arg...)				\
 	do {						\
