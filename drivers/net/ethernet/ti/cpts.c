98d13d2b1edbd41085e193fb44d97992b40eb4d7
Alexei Starovoitov <ast@plumgrid.com>
2014-04-01 18:26:48 -0700

    net: ti: fix CPTS driver build on arm
    fix build errors:
    drivers/net/ethernet/ti/cpts.c:266:12: error: 'ETH_HLEN' undeclared (first use in this function)
    drivers/net/ethernet/ti/cpts.c:276:23: error: 'VLAN_HLEN' undeclared (first use in this function)
    
    Fixes: 408eccce3204 ("net: ptp: move PTP classifier in its own file")
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Alexei Starovoitov <ast@plumgrid.com>
    Suggested-by: Daniel Borkmann <dborkman@redhat.com>
    Cc: Richard Cochran <richardcochran@gmail.com>
    Acked-by: Daniel Borkmann <dborkman@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ti/cpts.c
+++ drivers/net/ethernet/ti/cpts.c
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/time.h>
 #include <linux/uaccess.h>
 #include <linux/workqueue.h>
+#include <linux/if_ether.h>
+#include <linux/if_vlan.h>
 
 #include "cpts.h"
 
