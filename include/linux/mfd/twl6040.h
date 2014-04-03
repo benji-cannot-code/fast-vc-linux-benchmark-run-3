82ff8d8ce553934e53a00fb255ebb73c32f9ff94
Peter Ujfalusi <peter.ujfalusi@ti.com>
2014-04-03 13:54:41 +0300

    mfd: twl6040: Optional clk32k clock handling
    In certain boards the source for the clk32k clock can be gated. In these
    boards the clk32k clock can be provided to the driver and it is going to be
    enabled/disabled when it is needed.
    If the clk32k clock is not provided the driver will assume that it is always
    running.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/twl6040.h
+++ include/linux/mfd/twl6040.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/mfd/core.h>
 #include <linux/regulator/consumer.h>
+#include <linux/clk.h>
 
 #define TWL6040_REG_ASICID		0x01
 #define TWL6040_REG_ASICREV		0x02
@@ -224,6 +225,7 @@ struct twl6040 {
 	struct regmap *regmap;
 	struct regmap_irq_chip_data *irq_data;
 	struct regulator_bulk_data supplies[2]; /* supplies for vio, v2v1 */
+	struct clk *clk32k;
 	struct mutex mutex;
 	struct mutex irq_mutex;
 	struct mfd_cell cells[TWL6040_CELLS];
