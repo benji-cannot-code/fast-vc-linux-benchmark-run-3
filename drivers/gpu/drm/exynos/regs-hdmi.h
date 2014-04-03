2fe8b51a968fccbab4eedbd09b5f4cb1cff9c4f7
Shirish S <s.shirish@samsung.com>
2014-04-03 20:41:02 +0530

    drm/exynos: add hdmiphy power on/off sequence
    This patch implements the power on/off sequence
    of HDMI PHY in exynos5420 and exynos5250 as provided
    by the hardware team.
    
    This has been verified for mulitple iterations of
    S2R.
    
    Signed-off-by: Shirish S <s.shirish@samsung.com>
    Signed-off-by: Rahul Sharma <Rahul.Sharma@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/regs-hdmi.h
+++ drivers/gpu/drm/exynos/regs-hdmi.h
@@ -580,7 +580,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HDMI_TG_3D			HDMI_TG_BASE(0x00F0)
 
 /* HDMI PHY Registers Offsets*/
-#define HDMIPHY_MODE_SET_DONE		(0x7C >> 2)
+#define HDMIPHY_POWER		(0x74 >> 2)
+#define HDMIPHY_MODE_SET_DONE		(0x7c >> 2)
+
+/* HDMI PHY Values */
+#define HDMI_PHY_POWER_ON              0x80
+#define HDMI_PHY_POWER_OFF             0xff
 
 /* HDMI PHY Values */
 #define HDMI_PHY_DISABLE_MODE_SET	0x80
