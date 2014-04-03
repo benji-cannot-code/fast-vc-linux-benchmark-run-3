d3fd4a14cd37497262581070d0d34964762fadef
Sachin Kamat <sachin.kamat@linaro.org>
2014-04-03 14:49:51 -0700

    drivers/rtc/rtc-isl12057.c: remove duplicate include
    linux/rtc.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-isl12057.c
+++ drivers/rtc/rtc-isl12057.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rtc.h>
 #include <linux/i2c.h>
 #include <linux/bcd.h>
-#include <linux/rtc.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/regmap.h>
