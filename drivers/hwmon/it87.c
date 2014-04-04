c3110ff9fbbb794c9f487cdf090734b02a339fcb
Rudolf Marek <r.marek@assembler.cz>
2014-04-04 18:01:35 +0200

    hwmon: (it87) Add support for IT8623E
    Add support for the IT8623E found on Asus motherboards. It has same
    hardware monitoring block as IT8603E.
    
    Signed-off-by: Rudolf Marek <r.marek@assembler.cz>
    Signed-off-by: Jean Delvare <jdelvare@suse.de>

--- drivers/hwmon/it87.c
+++ drivers/hwmon/it87.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  similar parts.  The other devices are supported by different drivers.
  *
  *  Supports: IT8603E  Super I/O chip w/LPC interface
+ *            IT8623E  Super I/O chip w/LPC interface
  *            IT8705F  Super I/O chip w/LPC interface
  *            IT8712F  Super I/O chip w/LPC interface
  *            IT8716F  Super I/O chip w/LPC interface
@@ -149,6 +150,7 @@ static inline void superio_exit(void)
 #define IT8782F_DEVID 0x8782
 #define IT8783E_DEVID 0x8783
 #define IT8603E_DEVID 0x8603
+#define IT8623E_DEVID 0x8623
 #define IT87_ACT_REG  0x30
 #define IT87_BASE_REG 0x60
 
@@ -1768,6 +1770,7 @@ static int __init it87_find(unsigned short *address,
 		sio_data->type = it8783;
 		break;
 	case IT8603E_DEVID:
+	case IT8623E_DEVID:
 		sio_data->type = it8603;
 		break;
 	case 0xffff:	/* No device at all */
