41a7edc6636fc07104d59e6b126f605088e9c89f
Tomasz Stanislawski <t.stanislaws@samsung.com>
2014-04-04 16:53:19 +0200

    clk: samsung: exynos4: export sclk_hdmiphy clock
    Export sclk_hdmiphy clock to be usable from DT.
    
    Signed-off-by: Tomasz Stanislawski <t.stanislaws@samsung.com>
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>

--- include/dt-bindings/clock/exynos4.h
+++ include/dt-bindings/clock/exynos4.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_MOUT_MPLL_USER_C	18 /* Exynos4x12 only */
 #define CLK_MOUT_CORE		19
 #define CLK_MOUT_APLL		20
+#define CLK_SCLK_HDMIPHY	22
 
 /* gate for special clocks (sclk) */
 #define CLK_SCLK_FIMC0		128
