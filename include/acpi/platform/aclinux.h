5fd56ff3ed3af74fad6843ec0bb38945da904293
Lv Zheng <lv.zheng@intel.com>
2014-04-04 12:37:59 +0800

    ACPICA: Clean up comment divergences in aclinux.h
    When the following commmit is back ported to ACPICA, comments have been
    updated:
      Subject: ACPICA: Linux-specific header: Update support for Linux/acpi
               applications.
    
    This patch back ports the differences between the ACPICA upstream and
    Linux.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    [rjw: Subject]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/platform/aclinux.h
+++ include/acpi/platform/aclinux.h
@@ -92,7 +92,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <ctype.h>
 #include <unistd.h>
 
-/* Disable kernel specific declarators */
+/* Define/disable kernel-specific declarators */
 
 #ifndef __init
 #define __init
