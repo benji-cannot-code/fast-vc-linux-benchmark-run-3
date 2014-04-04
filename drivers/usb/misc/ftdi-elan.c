ab062cd46a94acc91fba653dd146bc8392d45d7f
Joe Perches <joe@perches.com>
2014-04-04 15:16:07 -0700

    usb: ftdi-elan: Use pr_<level>
    Use a more current logging style.
    
    Add pr_fmt to prefix messages appropriately.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/misc/ftdi-elan.c
+++ drivers/usb/misc/ftdi-elan.c
@@ -36,6 +36,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * via an ELAN U132 adapter.
  *
  */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/init.h>
@@ -624,7 +627,7 @@ static int ftdi_elan_open(struct inode *inode, struct file *file)
 	interface = usb_find_interface(&ftdi_elan_driver, subminor);
 
 	if (!interface) {
-		printk(KERN_ERR "can't find device for minor %d\n", subminor);
+		pr_err("can't find device for minor %d\n", subminor);
 		return -ENODEV;
 	} else {
 		struct usb_ftdi *ftdi = usb_get_intfdata(interface);
@@ -2723,10 +2726,8 @@ static int ftdi_elan_probe(struct usb_interface *interface,
 	struct usb_ftdi *ftdi;
 
 	ftdi = kzalloc(sizeof(struct usb_ftdi), GFP_KERNEL);
-	if (!ftdi) {
-		printk(KERN_ERR "Out of memory\n");
+	if (!ftdi)
 		return -ENOMEM;
-	}
 
 	mutex_lock(&ftdi_module_lock);
 	list_add_tail(&ftdi->ftdi_list, &ftdi_static_list);
@@ -2850,7 +2851,7 @@ static struct usb_driver ftdi_elan_driver = {
 static int __init ftdi_elan_init(void)
 {
 	int result;
-	printk(KERN_INFO "driver %s\n", ftdi_elan_driver.name);
+	pr_info("driver %s\n", ftdi_elan_driver.name);
 	mutex_init(&ftdi_module_lock);
 	INIT_LIST_HEAD(&ftdi_static_list);
 	status_queue = create_singlethread_workqueue("ftdi-status-control");
@@ -2867,8 +2868,7 @@ static int __init ftdi_elan_init(void)
 		destroy_workqueue(status_queue);
 		destroy_workqueue(command_queue);
 		destroy_workqueue(respond_queue);
-		printk(KERN_ERR "usb_register failed. Error number %d\n",
-		       result);
+		pr_err("usb_register failed. Error number %d\n", result);
 	}
 	return result;
 
@@ -2877,7 +2877,7 @@ err_respond_queue:
 err_command_queue:
 	destroy_workqueue(status_queue);
 err_status_queue:
-	printk(KERN_ERR "%s couldn't create workqueue\n", ftdi_elan_driver.name);
+	pr_err("%s couldn't create workqueue\n", ftdi_elan_driver.name);
 	return -ENOMEM;
 }
 
@@ -2886,7 +2886,7 @@ static void __exit ftdi_elan_exit(void)
 	struct usb_ftdi *ftdi;
 	struct usb_ftdi *temp;
 	usb_deregister(&ftdi_elan_driver);
-	printk(KERN_INFO "ftdi_u132 driver deregistered\n");
+	pr_info("ftdi_u132 driver deregistered\n");
 	list_for_each_entry_safe(ftdi, temp, &ftdi_static_list, ftdi_list) {
 		ftdi_status_cancel_work(ftdi);
 		ftdi_command_cancel_work(ftdi);
