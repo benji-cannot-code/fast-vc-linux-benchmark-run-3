f6c782473eda2304045f7e1a147f4f6048a0b0b8
Jean Delvare <jdelvare@suse.de>
2014-04-04 18:01:33 +0200

    hwmon: (lm63) Avoid initializing the same field twice
    data is kzalloc'd, so data->valid is already 0.
    
    Signed-off-by: Jean Delvare <jdelvare@suse.de>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/lm63.c
+++ drivers/hwmon/lm63.c
@@ -1122,7 +1122,6 @@ static int lm63_probe(struct i2c_client *client,
 		return -ENOMEM;
 
 	data->client = client;
-	data->valid = 0;
 	mutex_init(&data->update_lock);
 
 	/* Set the device type */
