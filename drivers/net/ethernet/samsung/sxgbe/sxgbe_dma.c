1f4e166d89804df755badb54b1d5b8ff74f88517
Jean Sacren <sakiwit@gmail.com>
2014-04-05 00:29:00 -0600

    sxgbe: fix duplicate #include headers
    The commit 1edb9ca69e8a ("net: sxgbe: add basic framework for
    Samsung 10Gb ethernet driver") added support for Samsung 10Gb
    ethernet driver(sxgbe) with a minor issue of including linux/io.h
    header twice in sxgbe_dma.c file. Fix the duplicate #include by
    deleting the top one so that all the rest good #include headers
    would be preserved in the alphabetical order.
    
    Signed-off-by: Jean Sacren <sakiwit@gmail.com>
    Cc: Byungho An <bh74.an@samsung.com>
    Cc: Girish K S <ks.giri@samsung.com>
    Cc: Siva Reddy Kallam <siva.kallam@samsung.com>
    Cc: Vipul Pandya <vipul.pandya@samsung.com>
    Acked-by: Byungho An <bh74.an@samsung.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/samsung/sxgbe/sxgbe_dma.c
+++ drivers/net/ethernet/samsung/sxgbe/sxgbe_dma.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#include <linux/io.h>
 #include <linux/delay.h>
 #include <linux/export.h>
 #include <linux/io.h>
