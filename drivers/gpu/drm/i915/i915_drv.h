1aa02623ea3866197932a8837e7412cf37daa5a4
Pradeep Bhat <pradeep.bhat@intel.com>
2014-04-05 12:13:28 +0530

    drm/i915: Add support for DRRS to switch RR
    This patch computes and stored 2nd M/N/TU for switching to different
    refresh rate dynamically. PIPECONF_EDP_RR_MODE_SWITCH bit helps toggle
    between alternate refresh rates programmed in 2nd M/N/TU registers.
    
    v2: Daniel's review comments
    Computing M2/N2 in compute_config and storing it in crtc_config
    
    v3: Modified reference to edp_downclock and edp_downclock_avail based on the
    changes made to move them from dev_private to intel_panel.
    
    v4: Modified references to is_drrs_supported based on the changes made to
    rename it to drrs_support.
    
    v5: Jani's review comments
    Removed superfluous return statements. Changed support for Gen 7 and above.
    Corrected indentation. Re-structured the code which finds crtc and connector
    from encoder. Changed some logs to be less verbose.
    
    v6: Modifying i915_drrs to include only intel connector as intel_dp can be
    derived from intel connector when required.
    
    v7: As per internal review comments, acquiring mutex just before accessing
    drrs RR. As per Chris's review comments, added documentation about the use
    of locking in the function.
    
    v8: Incorporated Jani's review comments.
    Removed reference to edp_downclock.
    
    v9: Jani's review comments. Modified comment in set_drrs. Changed index to
    type edp_drrs_refresh_rate_type. Check if PSR is enabled before setting
    registers fo DRRS.
    
    Signed-off-by: Pradeep Bhat <pradeep.bhat@intel.com>
    Signed-off-by: Vandana Kannan <vandana.kannan@intel.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -634,6 +634,10 @@ struct i915_fbc {
 	} no_fbc_reason;
 };
 
+struct i915_drrs {
+	struct intel_connector *connector;
+};
+
 struct i915_psr {
 	bool sink_support;
 	bool source_ok;
@@ -1325,6 +1329,7 @@ struct drm_i915_private {
 	struct timer_list hotplug_reenable_timer;
 
 	struct i915_fbc fbc;
+	struct i915_drrs drrs;
 	struct intel_opregion opregion;
 	struct intel_vbt_data vbt;
 
