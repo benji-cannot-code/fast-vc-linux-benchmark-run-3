f23fc65632bd3071bb6df53e5f262bc01f4536cc
Mark Salter <msalter@redhat.com>
2014-04-05 15:25:49 +0100

    arm64: fix !CONFIG_COMPAT build failures
    Recent arm64 builds using CONFIG_ARM64_64K_PAGES are failing with:
    
      arch/arm64/kernel/perf_regs.c: In function ‘perf_reg_abi’:
      arch/arm64/kernel/perf_regs.c:41:2: error: implicit declaration of function ‘is_compat_thread’
    
      arch/arm64/kernel/perf_event.c:1398:2: error: unknown type name ‘compat_uptr_t’
    
    This is due to some recent arm64 perf commits with compat support:
    
      commit 23c7d70d55c6d9:
        ARM64: perf: add support for frame pointer unwinding in compat mode
    
      commit 2ee0d7fd36a3f8:
        ARM64: perf: add support for perf registers API
    
    Those patches make the arm64 kernel unbuildable if CONFIG_COMPAT is not
    defined and CONFIG_ARM64_64K_PAGES depends on !CONFIG_COMPAT. This patch
    allows the arm64 kernel to build with and without CONFIG_COMPAT.
    
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/perf_regs.c
+++ arch/arm64/kernel/perf_regs.c
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/perf_event.h>
 #include <linux/bug.h>
+
+#include <asm/compat.h>
 #include <asm/perf_regs.h>
 #include <asm/ptrace.h>
 
