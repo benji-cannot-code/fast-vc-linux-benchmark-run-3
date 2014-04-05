e1be03aa8658cb0eb3b57c06f49225981ec87803
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-04-05 10:25:18 +0200

    drm/udl: Initialize ret in udl_driver_load
    We need to set it to -ENODEV when we don't recognize the device.
    Otherwise we return/print stack garbage.
    
    Spotted by coverity.
    
    Reviewed-by: David Herrmann <dh.herrmann@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/udl/udl_main.c
+++ drivers/gpu/drm/udl/udl_main.c
@@ -295,6 +295,7 @@ int udl_driver_load(struct drm_device *dev, unsigned long flags)
 	dev->dev_private = udl;
 
 	if (!udl_parse_vendor_descriptor(dev, dev->usbdev)) {
+		ret = -ENODEV;
 		DRM_ERROR("firmware not recognized. Assume incompatible device\n");
 		goto err;
 	}
