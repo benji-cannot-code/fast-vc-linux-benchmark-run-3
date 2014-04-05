6ebf56977474c862b932bf0bc6e881754ec238ba
Soren Brinkmann <soren.brinkmann@xilinx.com>
2014-04-04 17:23:43 -0700

    tty: xuartps: Rebrand driver as Cadence UART
    Zynq's UART is Cadence IP. Make this visible in the prompt in kconfig
    and additional comments in the driver.
    This also renames functions and symbols, as far as possible without
    breaking user space API, to reflect the Cadence origin. This is achieved
    through simple search and replace:
     - s/XUARTPS/CDNS_UART/g
     - s/xuartps/cdns_uart/g
    The only exceptions are PORT_XUARTPS and the driver name, which stay as is,
    due to their exposure to user space. As well as the - no legacy -
    compatibility string 'xlnx,xuartps'
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Tested-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -212,7 +212,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* VIA VT8500 SoC */
 #define PORT_VT8500	97
 
-/* Xilinx PSS UART */
+/* Cadence (Xilinx Zynq) UART */
 #define PORT_XUARTPS	98
 
 /* Atheros AR933X SoC */
