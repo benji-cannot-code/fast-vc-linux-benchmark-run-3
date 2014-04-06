eaddb477fb2fa712ef2e8cdd7eddf17c1ce02724
Russell King <rmk+kernel@arm.linux.org.uk>
2014-04-06 16:17:39 +0100

    ARM: add missing system_misc.h include to process.c
    arm_pm_restart(), arm_pm_idle() and soft_restart() are all declared in
    system_misc.h, but this file is not included in process.c.  Add this
    missing include.  Found via sparse:
    
    arch/arm/kernel/process.c:98:6: warning: symbol 'soft_restart' was not declared. Should it be static?
    arch/arm/kernel/process.c:127:6: warning: symbol 'arm_pm_restart' was not declared. Should it be static?
    arch/arm/kernel/process.c:134:6: warning: symbol 'arm_pm_idle' was not declared. Should it be static?
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 #include <asm/thread_notify.h>
 #include <asm/stacktrace.h>
+#include <asm/system_misc.h>
 #include <asm/mach/time.h>
 #include <asm/tls.h>
 
