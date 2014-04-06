191eebddd30d7a269bbaeba3b2805577157fa54e
Jonas Hahnfeld <hahnjo@hahnjo.de>
2014-04-06 17:46:04 +0200

    staging: rtl8187se: Fix warning symbol should be static
    This patch solves some sparse warnings about "symbol [...] was not
    declared. Should it be static?" by including the correct header files.
    
    Signed-off-by: Jonas Hahnfeld <hahnjo@hahnjo.de>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8187se/ieee80211/ieee80211_crypt_ccmp.c
+++ drivers/staging/rtl8187se/ieee80211/ieee80211_crypt_ccmp.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/wireless.h>
 
+#include "../r8180.h"
 #include "ieee80211.h"
 
 #include <linux/crypto.h>
--- drivers/staging/rtl8187se/r8180_wx.c
+++ drivers/staging/rtl8187se/r8180_wx.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "r8180.h"
 #include "r8180_hw.h"
+#include "r8180_wx.h"
 
 #include <net/iw_handler.h>
 #include "ieee80211/dot11d.h"
