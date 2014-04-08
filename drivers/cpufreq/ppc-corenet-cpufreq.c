302f8ae3405826c3cbaed617a217628c743e2208
Sachin Kamat <sachin.kamat@linaro.org>
2014-04-08 10:27:58 +0530

    cpufreq: ppc: Remove duplicate inclusion of fsl_soc.h
    fsl_soc.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/ppc-corenet-cpufreq.c
+++ drivers/cpufreq/ppc-corenet-cpufreq.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/cpufreq.h>
 #include <linux/errno.h>
-#include <sysdev/fsl_soc.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
