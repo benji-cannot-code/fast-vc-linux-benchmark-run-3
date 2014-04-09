f993f5072b02055dc3babe880e91564e1c9f15a6
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-04-09 23:21:14 +0200

    staging: rtl8723au: Remove unnecessary forward struct declarations from xmit_osdep.h
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/include/xmit_osdep.h
+++ drivers/staging/rtl8723au/include/xmit_osdep.h
@@ -22,12 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NR_XMITFRAME	256
 
-struct xmit_priv;
-struct pkt_attrib;
-struct sta_xmit_priv;
-struct xmit_frame;
-struct xmit_buf;
-
 int rtw_xmit23a_entry23a(struct sk_buff *pkt, struct net_device *pnetdev);
 
 void rtw_os_xmit_schedule23a(struct rtw_adapter *padapter);
