939d862f9e916a82fcbb896e3db6889eb0b96b4d
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-04-09 09:24:43 -0400

    tracepoint: Fix sparse warnings in tracepoint.c
    Fix the following sparse warnings:
    
      CHECK   kernel/tracepoint.c
    kernel/tracepoint.c:184:18: warning: incorrect type in assignment (different address spaces)
    kernel/tracepoint.c:184:18:    expected struct tracepoint_func *tp_funcs
    kernel/tracepoint.c:184:18:    got struct tracepoint_func [noderef] <asn:4>*funcs
    kernel/tracepoint.c:216:18: warning: incorrect type in assignment (different address spaces)
    kernel/tracepoint.c:216:18:    expected struct tracepoint_func *tp_funcs
    kernel/tracepoint.c:216:18:    got struct tracepoint_func [noderef] <asn:4>*funcs
    kernel/tracepoint.c:392:24: error: return expression in void function
      CC      kernel/tracepoint.o
    kernel/tracepoint.c: In function tracepoint_module_going:
    kernel/tracepoint.c:491:6: warning: symbol 'syscall_regfunc' was not declared. Should it be static?
    kernel/tracepoint.c:508:6: warning: symbol 'syscall_unregfunc' was not declared. Should it be static?
    
    Link: http://lkml.kernel.org/r/1397049883-28692-1-git-send-email-mathieu.desnoyers@efficios.com
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/tracepoint.c
+++ kernel/tracepoint.c
@@ -182,7 +182,8 @@ static int tracepoint_add_func(struct tracepoint *tp,
 	if (tp->regfunc && !static_key_enabled(&tp->key))
 		tp->regfunc();
 
-	tp_funcs = tp->funcs;
+	tp_funcs = rcu_dereference_protected(tp->funcs,
+			lockdep_is_held(&tracepoints_mutex));
 	old = func_add(&tp_funcs, func);
 	if (IS_ERR(old)) {
 		WARN_ON_ONCE(1);
@@ -214,7 +215,8 @@ static int tracepoint_remove_func(struct tracepoint *tp,
 {
 	struct tracepoint_func *old, *tp_funcs;
 
-	tp_funcs = tp->funcs;
+	tp_funcs = rcu_dereference_protected(tp->funcs,
+			lockdep_is_held(&tracepoints_mutex));
 	old = func_remove(&tp_funcs, func);
 	if (IS_ERR(old)) {
 		WARN_ON_ONCE(1);
