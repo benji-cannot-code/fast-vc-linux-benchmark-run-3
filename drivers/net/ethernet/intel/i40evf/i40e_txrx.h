fc76c1e0e0ab7b76f091f49f51b5aba7285bdc38
Jesse Brandeburg <jesse.brandeburg@intel.com>
2014-04-09 05:59:02 +0000

    i40e/i40evf: remove storm control
    The storm control features are not part of the hardware
    and mistakenly were left in the code.  Remove them as
    they are not needed any more.
    
    Change-ID: I6e9277c8da2c52e69348a657bae25271449c2099
    Signed-off-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Tested-by: Kavindya Deegala <kavindya.s.deegala@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40evf/i40e_txrx.h
+++ drivers/net/ethernet/intel/i40evf/i40e_txrx.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _I40E_TXRX_H_
 #define _I40E_TXRX_H_
 
-/* Interrupt Throttling and Rate Limiting (storm control) Goodies */
+/* Interrupt Throttling and Rate Limiting Goodies */
 
 #define I40E_MAX_ITR               0x0FF0  /* reg uses 2 usec resolution */
 #define I40E_MIN_ITR               0x0004  /* reg uses 2 usec resolution */
