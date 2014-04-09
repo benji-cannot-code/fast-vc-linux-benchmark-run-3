695372287946277708921e9e6eb39e3b7639da59
Brian Norris <computersforpeace@gmail.com>
2014-04-08 18:22:57 -0700

    mtd: spi-nor: EXPORT symbols which could be used by module drivers
    Fix errors like this:
    
        ERROR: "spi_nor_ids" [drivers/mtd/devices/m25p80.ko] undefined!
        ERROR: "spi_nor_scan" [drivers/mtd/devices/m25p80.ko] undefined!
        make[1]: *** [__modpost] Error 1
        make: *** [modules] Error 2
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -585,6 +585,7 @@ const struct spi_device_id spi_nor_ids[] = {
 	{ "cat25128", CAT25_INFO(2048, 8, 64, 2, SPI_NOR_NO_ERASE | SPI_NOR_NO_FR) },
 	{ },
 };
+EXPORT_SYMBOL_GPL(spi_nor_ids);
 
 static const struct spi_device_id *spi_nor_read_id(struct spi_nor *nor)
 {
@@ -1082,6 +1083,7 @@ int spi_nor_scan(struct spi_nor *nor, const struct spi_device_id *id,
 				mtd->eraseregions[i].numblocks);
 	return 0;
 }
+EXPORT_SYMBOL_GPL(spi_nor_scan);
 
 const struct spi_device_id *spi_nor_match_id(char *name)
 {
@@ -1094,6 +1096,7 @@ const struct spi_device_id *spi_nor_match_id(char *name)
 	}
 	return NULL;
 }
+EXPORT_SYMBOL_GPL(spi_nor_match_id);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Huang Shijie <shijie8@gmail.com>");
