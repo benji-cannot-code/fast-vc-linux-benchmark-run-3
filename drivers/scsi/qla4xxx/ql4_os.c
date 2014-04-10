744ccc5b43ad6d383e65268283a7324e043e2d38
Adheer Chandravanshi <adheer.chandravanshi@qlogic.com>
2014-04-09 21:12:28 -0400

    qla4xxx: Fix smatch warning in func qla4xxx_conn_get_param
    Signed-off-by: Vikas Chaudhary <vikas.chaudhary@qlogic.com>
    Reviewed-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/scsi/qla4xxx/ql4_os.c
+++ drivers/scsi/qla4xxx/ql4_os.c
@@ -2880,7 +2880,6 @@ static int qla4xxx_conn_get_param(struct iscsi_cls_conn *cls_conn,
 	struct iscsi_conn *conn;
 	struct qla_conn *qla_conn;
 	struct sockaddr *dst_addr;
-	int len = 0;
 
 	conn = cls_conn->dd_data;
 	qla_conn = conn->dd_data;
@@ -2894,9 +2893,6 @@ static int qla4xxx_conn_get_param(struct iscsi_cls_conn *cls_conn,
 	default:
 		return iscsi_conn_get_param(cls_conn, param, buf);
 	}
-
-	return len;
-
 }
 
 int qla4xxx_get_ddb_index(struct scsi_qla_host *ha, uint16_t *ddb_index)
