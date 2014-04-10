dfd516175c3686ff47168dce5c84b42d8b62122d
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2014-04-10 20:19:33 +0200

    ARM: at91: prepare common clk transition for sam9g45
    Enclose the sam9g45 old clk registration in "#if defined(CONFIG_OLD_CLK_AT91)
     #endif"
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/at91sam9g45.c
+++ arch/arm/mach-at91/at91sam9g45.c
@@ -26,10 +26,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "at91_aic.h"
 #include "soc.h"
 #include "generic.h"
-#include "clock.h"
 #include "sam9_smc.h"
 #include "pm.h"
 
+#if defined(CONFIG_OLD_CLK_AT91)
+#include "clock.h"
 /* --------------------------------------------------------------------
  *  Clocks
  * -------------------------------------------------------------------- */
@@ -332,6 +333,9 @@ static void __init at91sam9g45_register_clocks(void)
 	clk_register(&pck0);
 	clk_register(&pck1);
 }
+#else
+#define at91sam9g45_register_clocks NULL
+#endif
 
 /* --------------------------------------------------------------------
  *  GPIO
