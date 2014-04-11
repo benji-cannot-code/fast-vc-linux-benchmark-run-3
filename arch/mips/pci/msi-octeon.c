03bc7b169558cd1c9628d25ec2d5868c32e600db
Eunbong Song <eunb.song@samsung.com>
2014-04-11 08:32:54 +0000

    MIPS: Octeon: Add PCIe2 support in arch_setup_msi_irq()
    In arch_setup_msi_irq(), there is no case for PCIe2. So board which have PCIe2 functionality
    fails to boot with "Kernel panic - not syncing: arch_setup_msi_irq: Invalid octeon_dma_bar_type"
    message. This patch solve this problem.
    
    Signed-off-by: Eunbong Song <eunb.song@samsung.com>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/6747/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/pci/msi-octeon.c
+++ arch/mips/pci/msi-octeon.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/octeon/cvmx-npi-defs.h>
 #include <asm/octeon/cvmx-pci-defs.h>
 #include <asm/octeon/cvmx-npei-defs.h>
+#include <asm/octeon/cvmx-sli-defs.h>
 #include <asm/octeon/cvmx-pexp-defs.h>
 #include <asm/octeon/pci-octeon.h>
 
@@ -162,6 +163,11 @@ msi_irq_allocated:
 		/* FIXME CVMX_NPEI_MSI_RCV* other than 0? */
 		msg.address_lo = (0 + CVMX_NPEI_PCIE_MSI_RCV) & 0xffffffff;
 		msg.address_hi = (0 + CVMX_NPEI_PCIE_MSI_RCV) >> 32;
+		break;
+	case OCTEON_DMA_BAR_TYPE_PCIE2:
+		/* When using PCIe2, Bar 0 is based at 0 */
+		msg.address_lo = (0 + CVMX_SLI_PCIE_MSI_RCV) & 0xffffffff;
+		msg.address_hi = (0 + CVMX_SLI_PCIE_MSI_RCV) >> 32;
 		break;
 	default:
 		panic("arch_setup_msi_irq: Invalid octeon_dma_bar_type");
