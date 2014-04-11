c3430f9eeaf9ea20ee5e483333ea478cb00d6afb
Thomas Gleixner <tglx@linutronix.de>
2014-04-11 08:13:22 +0000

    can: c_can: Remove tx locking
    Mark suggested to use one IF for the softirq and the other for the
    xmit function to avoid the xmit lock.
    
    That requires to write the frame into the interface first, then handle
    the echo skb and store the dlc before committing the TX request to the
    message ram.
    
    We use an atomic to handle the active buffers instead of reading the
    MSGVAL register as thats way faster especially on PCH/x86.
    
    Suggested-by: Mark <mark5@del-llc.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Alexander Stein <alexander.stein@systec-electronic.com>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/c_can/c_can.h
+++ drivers/net/can/c_can/c_can.h
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define C_CAN_MSG_OBJ_RX_SPLIT	9
 #define C_CAN_MSG_RX_LOW_LAST	(C_CAN_MSG_OBJ_RX_SPLIT - 1)
-
-#define C_CAN_NEXT_MSG_OBJ_MASK	(C_CAN_MSG_OBJ_TX_NUM - 1)
 #define RECEIVE_OBJECT_BITS	0x0000ffff
 
 enum reg {
@@ -176,16 +174,12 @@ struct c_can_priv {
 	struct napi_struct napi;
 	struct net_device *dev;
 	struct device *device;
-	spinlock_t xmit_lock;
-	int tx_object;
+	atomic_t tx_active;
 	int last_status;
 	u16 (*read_reg) (struct c_can_priv *priv, enum reg index);
 	void (*write_reg) (struct c_can_priv *priv, enum reg index, u16 val);
 	void __iomem *base;
 	const u16 *regs;
-	unsigned long irq_flags; /* for request_irq() */
-	unsigned int tx_next;
-	unsigned int tx_echo;
 	void *priv;		/* for board-specific data */
 	enum c_can_dev_id type;
 	u32 __iomem *raminit_ctrlreg;
