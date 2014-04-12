6eef9eba538aafc9d20aa9f83aff37b68a492e82
Dominique van den Broeck <domdevlin@free.fr>
2014-04-12 15:18:14 +0200

    fwserial: (coding style) useless "extern" & space
    Style-only modifications to make checkpatch.pl --file --strict a bit happier.
    Removed useless "extern" in dma_fifo.h ;
    Removed one supernumerary space.
    
    Signed-off-by: Dominique van den Broeck <domdevlin@free.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fwserial/fwserial.c
+++ drivers/staging/fwserial/fwserial.c
@@ -1821,7 +1821,7 @@ static void fwserial_release_port(struct fwtty_port *port, bool reset)
 
 static void fwserial_plug_timeout(unsigned long data)
 {
-	struct fwtty_peer *peer = (struct fwtty_peer *) data;
+	struct fwtty_peer *peer = (struct fwtty_peer *)data;
 	struct fwtty_port *port;
 
 	spin_lock_bh(&peer->lock);
