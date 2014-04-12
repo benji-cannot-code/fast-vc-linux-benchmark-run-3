f69336507b78ae765647070917807b72fad69b75
Rui Miguel Silva <rmfrfs@gmail.com>
2014-04-12 01:33:11 +0100

    staging: keucr: fix sparse warning
    fix sparse warning:
    drivers/staging/keucr/smilmain.c:163:5: warning: symbol 'Check_D_MediaFmt' was not declared. Should it be static?
    
    by declaring it in the right header. And remove duplication of definition of
    SM_INIT
    
    Signed-off-by: Rui Miguel Silva <rmfrfs@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/keucr/init.c
+++ drivers/staging/keucr/init.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "usb.h"
 #include "scsiglue.h"
 #include "transport.h"
+#include "smil.h"
 #include "init.h"
 
 /*
--- drivers/staging/keucr/init.h
+++ drivers/staging/keucr/init.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "common.h"
 
 extern u32 MediaChange;
-extern int Check_D_MediaFmt(struct us_data *);
 
 
 
