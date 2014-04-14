d7711fb33df3d2dab030d2d9b9508daa23656744
Paul Burton <paul.burton@imgtec.com>
2014-04-14 12:04:27 +0100

    MIPS: smp-cps: rework core/VPE initialisation
    When hotplug and/or a powered down idle state are supported cases will
    arise where a non-zero VPE must be brought online without VPE 0, and it
    where multiple VPEs must be onlined simultaneously. This patch prepares
    for that by:
    
      - Splitting struct boot_config into core & VPE boot config structures,
        allocated one per core or VPE respectively. This allows for multiple
        VPEs to be onlined simultaneously without clobbering each others
        configuration.
    
      - Indicating which VPEs should be online within a core at any given
        time using a bitmap. This allows multiple VPEs to be brought online
        simultaneously and also indicates to VPE 0 whether it should halt
        after starting any non-zero VPEs that should be online within the
        core. For example if all VPEs within a core are offlined via hotplug
        and the user onlines the second VPE within that core:
    
          1) The core will be powered up.
    
          2) VPE 0 will run from the BEV (ie. mips_cps_core_entry) to
             initialise the core.
    
          3) VPE 0 will start VPE 1 because its bit is set in the cores
             bitmap.
    
          4) VPE 0 will halt itself because its bit is clear in the cores
             bitmap.
    
      - Moving the core & VPE initialisation to assembly code which does not
        make any use of the stack. This is because if a non-zero VPE is to
        be brought online in a powered down core then when VPE 0 of that
        core runs it may not have a valid stack, and even if it did then
        it's messy to run through parts of generic kernel code on VPE 0
        before starting the correct VPE.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- arch/mips/kernel/mips-cpc.c
+++ arch/mips/kernel/mips-cpc.c
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/errno.h>
+#include <linux/percpu.h>
+#include <linux/spinlock.h>
 
 #include <asm/mips-cm.h>
 #include <asm/mips-cpc.h>
