704bdbb5d720e315b01512cf58df1dd120634edd
Nicolas Ferre <nicolas.ferre@atmel.com>
2014-04-14 10:51:15 +0200

    ASoC: sam9g20_wm8731: remove useless mach/gpio.h
    This include file is about to disapear. In addition it is
    useless for this code. So it is time to remove it.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Acked-by: Mark Brown <broonie@linaro.org>

--- sound/soc/atmel/sam9g20_wm8731.c
+++ sound/soc/atmel/sam9g20_wm8731.c
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach-types.h>
 #include <mach/hardware.h>
-#include <mach/gpio.h>
 
 #include "../codecs/wm8731.h"
 #include "atmel-pcm.h"
