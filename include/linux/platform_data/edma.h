1eacfa37a6d0f8f346f090535f084388ebc5dd5d
Peter Ujfalusi <peter.ujfalusi@ti.com>
2014-04-14 14:41:56 +0300

    platform_data: edma: Be precise with the paRAM struct
    The edmacc_param struct should follow the layout of the paRAM area in the
    HW. Be explicit on the size of the fields (u32) and also mark the struct
    as packed to avoid any padding on non 32bit architectures.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Acked-by: Joel Fernandes <joelf@ti.com>
    Reviewed-and-Tested-by: Joel Fernandes <joelf@ti.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- include/linux/platform_data/edma.h
+++ include/linux/platform_data/edma.h
@@ -44,15 +44,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* PaRAM slots are laid out like this */
 struct edmacc_param {
-	unsigned int opt;
-	unsigned int src;
-	unsigned int a_b_cnt;
-	unsigned int dst;
-	unsigned int src_dst_bidx;
-	unsigned int link_bcntrld;
-	unsigned int src_dst_cidx;
-	unsigned int ccnt;
-};
+	u32 opt;
+	u32 src;
+	u32 a_b_cnt;
+	u32 dst;
+	u32 src_dst_bidx;
+	u32 link_bcntrld;
+	u32 src_dst_cidx;
+	u32 ccnt;
+} __packed;
 
 /* fields in edmacc_param.opt */
 #define SAM		BIT(0)
