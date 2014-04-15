cafab95fef376ed68b24335ad383739c1ac9446c
David Sterba <dsterba@suse.cz>
2014-04-15 16:42:03 +0200

    btrfs: assert that send is not in progres before root deletion
    CC: Miao Xie <miaox@cn.fujitsu.com>
    CC: Wang Shilong <wangsl.fnst@cn.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.cz>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -2425,6 +2425,7 @@ out_unlock:
 		shrink_dcache_sb(root->fs_info->sb);
 		btrfs_invalidate_inodes(dest);
 		d_delete(dentry);
+		ASSERT(dest->send_in_progress == 0);
 
 		/* the last ref */
 		if (dest->cache_inode) {
