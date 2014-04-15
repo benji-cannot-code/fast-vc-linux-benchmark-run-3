e1d23f173b56e32b58055399b6b7de99f05ea6c6
Arnd Bergmann <arnd@arndb.de>
2014-04-15 20:33:14 +0200

    ARM: mmp: make plat-pxa build standalone
    The ssp.c driver in plat-pxa is the only remaining file including a
    mach/*.h header from mmp. It actually doesn't need mach/hardware.h at
    all, so we can remove the #include.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/plat-pxa/ssp.c
+++ arch/arm/plat-pxa/ssp.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_device.h>
 
 #include <asm/irq.h>
-#include <mach/hardware.h>
 
 static DEFINE_MUTEX(ssp_lock);
 static LIST_HEAD(ssp_list);
