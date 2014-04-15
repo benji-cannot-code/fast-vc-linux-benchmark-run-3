e69d2c61345bbd1efb5db75ab36d7527b421c98d
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2014-04-15 12:27:57 +0200

    ARM: at91: sam9260: remove unused platform_data
    num_channels and registers are not used anymore since they are defined inside
    the at91_adc driver and assigned by matching the id_table.
    
    Also, remove the mach/at91_adc.h include that is not necessary anymore.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Acked-by: Jonathan Cameron <jic23@kernel.org>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/at91sam9260_devices.c
+++ arch/arm/mach-at91/at91sam9260_devices.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/at91sam9260_matrix.h>
 #include <mach/at91_matrix.h>
 #include <mach/at91sam9_smc.h>
-#include <mach/at91_adc.h>
 #include <mach/hardware.h>
 
 #include "board.h"
@@ -1326,13 +1325,6 @@ static struct at91_adc_trigger at91_adc_triggers[] = {
 	},
 };
 
-static struct at91_adc_reg_desc at91_adc_register_g20 = {
-	.channel_base = AT91_ADC_CHR(0),
-	.drdy_mask = AT91_ADC_DRDY,
-	.status_register = AT91_ADC_SR,
-	.trigger_register = AT91_ADC_MR,
-};
-
 void __init at91_add_device_adc(struct at91_adc_data *data)
 {
 	if (!data)
@@ -1350,9 +1342,7 @@ void __init at91_add_device_adc(struct at91_adc_data *data)
 	if (data->use_external_triggers)
 		at91_set_A_periph(AT91_PIN_PA22, 0);
 
-	data->num_channels = 4;
 	data->startup_time = 10;
-	data->registers = &at91_adc_register_g20;
 	data->trigger_number = 4;
 	data->trigger_list = at91_adc_triggers;
 
