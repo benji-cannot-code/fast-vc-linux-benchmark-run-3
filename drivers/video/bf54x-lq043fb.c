a19dfc81e69952b88d401b494db192b27168fbb9
Steven Miao <realmz6@gmail.com>
2014-04-15 15:17:19 +0800

    video: bf54x-lq043fb: fix build error
    Fix build error by including linux/gpio.h. Also drop asm/gpio.h which is
    not needed.
    
    Signed-off-by: Steven Miao <realmz6@gmail.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/bf54x-lq043fb.c
+++ drivers/video/bf54x-lq043fb.c
@@ -50,13 +50,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/dma-mapping.h>
 #include <linux/platform_device.h>
+#include <linux/gpio.h>
 
 #include <asm/blackfin.h>
 #include <asm/irq.h>
 #include <asm/dpmc.h>
 #include <asm/dma-mapping.h>
 #include <asm/dma.h>
-#include <asm/gpio.h>
 #include <asm/portmux.h>
 
 #include <mach/bf54x-lq043.h>
