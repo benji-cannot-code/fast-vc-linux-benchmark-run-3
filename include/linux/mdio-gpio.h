113d80ae8cd3038e17652e7b73d3ccd279b7ad3d
Guenter Roeck <linux@roeck-us.net>
2014-04-15 19:16:41 -0700

    net: mdio-gpio: Add support for active low gpio pins
    Some systems using mdio-gpio may use active-low gpio pins
    (eg with inverters or FETs connected to all or some of the
    gpio pins).
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/mdio-gpio.h
+++ include/linux/mdio-gpio.h
@@ -19,6 +19,9 @@ struct mdio_gpio_platform_data {
 	unsigned int mdc;
 	unsigned int mdio;
 
+	bool mdc_active_low;
+	bool mdio_active_low;
+
 	unsigned int phy_mask;
 	int irqs[PHY_MAX_ADDR];
 	/* reset callback */
