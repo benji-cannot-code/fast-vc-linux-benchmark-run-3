7b373169e1eb35b3d2a3797ebe982c686ae3f033
Hans Verkuil <hans.verkuil@cisco.com>
2014-04-17 06:06:06 -0300

    [media] saa7134: move saa7134_pgtable to saa7134_dmaqueue
    All dmaqueue's use saa7134_pgtable, so move it into struct saa7134_dmaqueue.
    The videobuf_queue priv_data field now points to the dmaqueue struct.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/pci/saa7134/saa7134.h
+++ drivers/media/pci/saa7134/saa7134.h
@@ -461,9 +461,6 @@ struct saa7134_buf {
 	int (*activate)(struct saa7134_dev *dev,
 			struct saa7134_buf *buf,
 			struct saa7134_buf *next);
-
-	/* page tables */
-	struct saa7134_pgtable  *pt;
 };
 
 struct saa7134_dmaqueue {
@@ -472,6 +469,7 @@ struct saa7134_dmaqueue {
 	struct list_head           queue;
 	struct timer_list          timeout;
 	unsigned int               need_two;
+	struct saa7134_pgtable     pt;
 };
 
 /* video filehandle status */
@@ -518,7 +516,6 @@ struct saa7134_dmasound {
 /* ts/mpeg status */
 struct saa7134_ts {
 	/* TS capture */
-	struct saa7134_pgtable     pt_ts;
 	int                        nr_packets;
 	int                        nr_bufs;
 };
@@ -591,10 +588,8 @@ struct saa7134_dev {
 
 	/* video+ts+vbi capture */
 	struct saa7134_dmaqueue    video_q;
-	struct saa7134_pgtable     pt_cap;
 	struct videobuf_queue      video_vbq;
 	struct saa7134_dmaqueue    vbi_q;
-	struct saa7134_pgtable     pt_vbi;
 	struct videobuf_queue      vbi_vbq;
 	unsigned int               video_fieldcount;
 	unsigned int               vbi_fieldcount;
