a16dd81951109fab7548c9236f42d70569ad0c3c
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2014-04-21 01:26:58 +0200

    sched/idle: Make cpuidle_idle_call() void
    The only value ever returned by cpuidle_idle_call() is 0 and its
    only caller ignores that value anyway, so make it void.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/4717784.WmVEpDoliM@vostro.rjw.lan
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -68,9 +68,8 @@ void __weak arch_cpu_idle(void)
  * cpuidle_idle_call - the main idle function
  *
  * NOTE: no locks or semaphores should be used here
- * return non-zero on failure
  */
-static int cpuidle_idle_call(void)
+static void cpuidle_idle_call(void)
 {
 	struct cpuidle_device *dev = __this_cpu_read(cpuidle_devices);
 	struct cpuidle_driver *drv = cpuidle_get_cpu_driver(dev);
@@ -83,7 +82,7 @@ static int cpuidle_idle_call(void)
 	 */
 	if (need_resched()) {
 		local_irq_enable();
-		return 0;
+		return;
 	}
 
 	/*
@@ -178,8 +177,6 @@ exit_idle:
 
 	rcu_idle_exit();
 	start_critical_timings();
-
-	return 0;
 }
 
 /*
