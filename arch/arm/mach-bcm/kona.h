f6af98045ef2a4d74645e80bc5d493fdedaf57ec
Alex Elder <elder@linaro.org>
2014-04-21 16:53:07 -0500

    ARM: bcm: config option for l2 cache support
    Add a new config option ARCH_BCM_MOBILE_L2_CACHE that allows support
    for level-2 cache to be enabled or disabled at build time for
    BCM218XX and BCM21664 family SoCs.
    
    Build support for SMC only if it's required (currently it's only
    required for to support level 2 cache control).
    
    If arch/arm/mach-bcm/kona.c gets compiled, ARCH_BCM_MOBILE_L2_CACHE
    must have been selected, which implies CONFIG_CACHE_L2X0 is set.
    There is therefore no need to check CONFIG_CACHE_L2X0 at the top
    of kona_l2_cache_init(), so get rid of that check.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Reviewed-by: Tim Kryger <tim.kryger@linaro.org>
    Reviewed-by: Markus Mayer <markus.mayer@linaro.org>
    Reviewed-by: Matt Porter <mporter@linaro.org>
    Signed-off-by: Matt Porter <mporter@linaro.org>

--- arch/arm/mach-bcm/kona.h
+++ arch/arm/mach-bcm/kona.h
@@ -12,4 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
+#ifdef CONFIG_ARCH_BCM_MOBILE_L2_CACHE
+
 void __init kona_l2_cache_init(void);
+#else
+#define kona_l2_cache_init() ((void)0)
+#endif
