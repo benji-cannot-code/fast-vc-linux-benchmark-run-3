1e7da89d0faf02c745deb0fcbb7c0437cd57180f
Sam Ravnborg <sam@ravnborg.org>
2014-04-21 21:39:31 +0200

    sparc32: fix sparse warning in devices.c
    Fix following warning:
    devices.c:114:13: warning: symbol 'device_scan' was not declared. Should it be static?
    
    Add prototype to asm/setup.h
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/devices.c
+++ arch/sparc/kernel/devices.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/smp.h>
 #include <asm/cpudata.h>
 #include <asm/cpu_type.h>
+#include <asm/setup.h>
 
 extern void clock_stop_probe(void); /* tadpole.c */
 
