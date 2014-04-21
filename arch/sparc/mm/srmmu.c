46f9d66d579825bc50980ce1581d3e1c268314c4
Sam Ravnborg <sam@ravnborg.org>
2014-04-21 21:39:16 +0200

    sparc32: rename mm/srmmu.h to mm/mm_32.h
    This file will be used for more than just srmmu stuff, so the old name was misleading.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/srmmu.c
+++ arch/sparc/mm/srmmu.c
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mxcc.h>
 #include <asm/ross.h>
 
-#include "srmmu.h"
+#include "mm_32.h"
 
 enum mbus_module srmmu_modtype;
 static unsigned int hwbug_bitmask;
