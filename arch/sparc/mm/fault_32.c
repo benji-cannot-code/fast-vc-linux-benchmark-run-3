12ae004dd16c44e0b25755a7b8a2ba0e95a9a217
Sam Ravnborg <sam@ravnborg.org>
2014-04-21 21:39:17 +0200

    sparc32: fix sparse warning in fault_32.c
    Fix following warning:
    fault_32.c:38:24: error: symbol 'unhandled_fault' redeclared with different type - different modifiers
    
    When this warning was fixed several new warnings popped up - fix them too.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/fault_32.c
+++ arch/sparc/mm/fault_32.c
@@ -31,10 +31,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/traps.h>
 #include <asm/uaccess.h>
 
-int show_unhandled_signals = 1;
+#include "mm_32.h"
 
-static void unhandled_fault(unsigned long, struct task_struct *,
-		struct pt_regs *) __attribute__ ((noreturn));
+int show_unhandled_signals = 1;
 
 static void __noreturn unhandled_fault(unsigned long address,
 				       struct task_struct *tsk,
--- arch/sparc/mm/mm_32.h
+++ arch/sparc/mm/mm_32.h
@@ -1,4 +1,14 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* fault_32.c - visible as they are called from assembler */
+asmlinkage int lookup_fault(unsigned long pc, unsigned long ret_pc,
+                            unsigned long address);
+asmlinkage void do_sparc_fault(struct pt_regs *regs, int text_fault, int write,
+                               unsigned long address);
+
+void window_overflow_fault(void);
+void window_underflow_fault(unsigned long sp);
+void window_ret_fault(struct pt_regs *regs);
+
 /* srmmu.c */
 extern char *srmmu_name;
 
