8d775e89b00255f052e8115072097a62e329e071
Ying Xue <ying.xue@windriver.com>
2014-04-21 10:55:42 +0800

    tipc: replace config_mutex lock with RTNL lock
    There have two paths where we can configure or change bearer status:
    one is that bearer is configured from user space with tipc-config
    tool; another one is that bearer is changed by notification events
    from its attached interface. On the first path, one dedicated
    config_mutex lock is guarded; on the latter path, RTNL lock has been
    placed to serialize the process of dealing with interface events.
    So, if RTNL lock is also used to protect the first path, this will
    not only extremely help us simplify current locking policy, but also
    config_mutex lock can be deleted as well.
    
    Signed-off-by: Ying Xue <ying.xue@windriver.com>
    Reviewed-by: Jon Maloy <jon.maloy@ericsson.com>
    Reviewed-by: Erik Hugne <erik.hugne@ericsson.com>
    Tested-by: Erik Hugne <erik.hugne@ericsson.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/config.c
+++ net/tipc/config.c
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define REPLY_TRUNCATED "<truncated>\n"
 
-static DEFINE_MUTEX(config_mutex);
-
 static const void *req_tlv_area;	/* request message TLV area */
 static int req_tlv_space;		/* request message TLV area size */
 static int rep_headroom;		/* reply message headroom to use */
@@ -224,7 +222,7 @@ struct sk_buff *tipc_cfg_do_cmd(u32 orig_node, u16 cmd, const void *request_area
 {
 	struct sk_buff *rep_tlv_buf;
 
-	mutex_lock(&config_mutex);
+	rtnl_lock();
 
 	/* Save request and reply details in a well-known location */
 	req_tlv_area = request_area;
@@ -338,6 +336,6 @@ struct sk_buff *tipc_cfg_do_cmd(u32 orig_node, u16 cmd, const void *request_area
 
 	/* Return reply buffer */
 exit:
-	mutex_unlock(&config_mutex);
+	rtnl_unlock();
 	return rep_tlv_buf;
 }
