1695c47cd43281b60b62b6a0195966177662128f
Alex Elder <elder@linaro.org>
2014-04-21 16:53:09 -0500

    ARM: bcm: use inline assembly for "smc" request
    Move the code that implements the "smc" call into a C function that
    uses inline assembly.  This allows us to make that function private,
    and enables us to get rid of "arch/arm/mach-bcm/bcm_kona_smc_asm.S".
    Rename what had been the "buffer_addr" argument to be "buffer_phys"
    so it's consistent with other usage in this file.
    
    Since it's now easy to do, verify that r12 contains SEC_EXIT_NORMAL
    upon completion of the SMC.  There really isn't a good way to handle
    the abnormal completion of a secure monitor request.
    
    Since "bcm_kona_smc.h" is now only included from C files, eliminate
    the #ifndef __ASSEMBLY__.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Reviewed-by: Tim Kryger <tim.kryger@linaro.org>
    Reviewed-by: Markus Mayer <markus.mayer@linaro.org>
    Reviewed-by: Matt Porter <mporter@linaro.org>
    Signed-off-by: Matt Porter <mporter@linaro.org>

--- arch/arm/mach-bcm/bcm_kona_smc.h
+++ arch/arm/mach-bcm/bcm_kona_smc.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SEC_ROM_RET_OK			0x00000001
 #define SEC_EXIT_NORMAL			0x1
 
-#ifndef	__ASSEMBLY__
 extern int __init bcm_kona_smc_init(void);
 
 extern unsigned bcm_kona_smc(unsigned service_id,
@@ -30,10 +29,5 @@ extern unsigned bcm_kona_smc(unsigned service_id,
 			     unsigned arg1,
 			     unsigned arg2,
 			     unsigned arg3);
-
-extern int bcm_kona_smc_asm(u32 service_id,
-			    u32 buffer_addr);
-
-#endif	/* __ASSEMBLY__ */
 
 #endif /* BCM_KONA_SMC_H */
