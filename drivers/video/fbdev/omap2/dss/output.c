1e97bcd4ada3160e0b2f07a8214d7288c5bb9237
Archit Taneja <archit@ti.com>
2014-04-22 17:43:48 +0530

    OMAPDSS: DT: Get source endpoint by matching reg-id
    In omapdss_of_find_source_for_first_ep, we retrieve a source endpoint's DT node,
    and then see what omapdss output has the matching device_node pointer in
    omap_dss_find_output_by_node.
    
    For all DPI and SDI outputs, the device_node pointer is set as the parent's DSS
    device_node pointer. If the source is one of these outputs, the above method
    won't work.
    
    To get the correct output for ports within DSS(and in other cases in the future,
    where multiple ports might be under one device), we require additional
    information which is exclusive to the output port.
    
    We create a new field in omap_dss_device called 'port_num', this provides port
    number of the output port corresponding to this device. When searching for the
    source endpoint in DT, we extract the 'reg' property from the port corresponding
    to the endpoint source. From the list of registered outputs, we pick out that
    output which has both dev->of_node and port_num matching with the device_node
    pointer and 'reg' of the source endpoint node from DT.
    
    For encoder blocks(the ones which have both an input and output port), we need
    to set the port_num as the 'reg' property for the output port as defined in the
    DT bindings. We set port_num to 1 in the tfp410 and tpd12s015 encoder drivers.
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/fbdev/omap2/dss/output.c
+++ drivers/video/fbdev/omap2/dss/output.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
+#include <linux/of.h>
 
 #include <video/omapdss.h>
 
@@ -132,18 +133,30 @@ struct omap_dss_device *omap_dss_find_output(const char *name)
 }
 EXPORT_SYMBOL(omap_dss_find_output);
 
-struct omap_dss_device *omap_dss_find_output_by_node(struct device_node *node)
+struct omap_dss_device *omap_dss_find_output_by_port_node(struct device_node *port)
 {
+	struct device_node *src_node;
 	struct omap_dss_device *out;
+	u32 reg;
+
+	src_node = dss_of_port_get_parent_device(port);
+	if (!src_node)
+		return NULL;
+
+	reg = dss_of_port_get_port_number(port);
 
 	list_for_each_entry(out, &output_list, list) {
-		if (out->dev->of_node == node)
+		if (out->dev->of_node == src_node && out->port_num == reg) {
+			of_node_put(src_node);
 			return omap_dss_get_device(out);
+		}
 	}
+
+	of_node_put(src_node);
 
 	return NULL;
 }
-EXPORT_SYMBOL(omap_dss_find_output_by_node);
+EXPORT_SYMBOL(omap_dss_find_output_by_port_node);
 
 struct omap_dss_device *omapdss_find_output_from_display(struct omap_dss_device *dssdev)
 {
