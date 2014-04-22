01f1d5bb2d16e61b3669161b4bf7df78a9439a93
Eric Paris <eparis@redhat.com>
2014-04-22 12:07:30 -0400

    sparc: simplify syscall_get_arch()
    Include linux/thread_info.h so we can use is_32_bit_task() cleanly.
    Then just simplify syscall_get_arch() since is_32_bit_task() works for
    all configuration options.
    
    Suggested-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- arch/powerpc/include/asm/syscall.h
+++ arch/powerpc/include/asm/syscall.h
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_SYSCALL_H	1
 
 #include <uapi/linux/audit.h>
-#include <linux/compat.h>
 #include <linux/sched.h>
+#include <linux/thread_info.h>
 
 /* ftrace syscalls requires exporting the sys_call_table */
 #ifdef CONFIG_FTRACE_SYSCALLS
@@ -91,12 +91,6 @@ static inline void syscall_set_arguments(struct task_struct *task,
 
 static inline int syscall_get_arch(void)
 {
-	int arch = AUDIT_ARCH_PPC;
-
-#ifdef CONFIG_PPC64
-	if (!is_32bit_task())
-		arch = AUDIT_ARCH_PPC64;
-#endif
-	return arch;
+	return is_32bit_task() ? AUDIT_ARCH_PPC : AUDIT_ARCH_PPC64;
 }
 #endif	/* _ASM_SYSCALL_H */
