13a62860be47b6e8416338df0c2e05fb34c288d6
Geert Uytterhoeven <geert@linux-m68k.org>
2014-04-22 12:51:13 +0200

    mips: Update the email address of Geert Uytterhoeven
    All my Sony addresses are defunct.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/6817/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/paravirt/paravirt-smp.c
+++ arch/mips/paravirt/paravirt-smp.c
@@ -100,10 +100,6 @@ static void paravirt_smp_finish(void)
 	local_irq_enable();
 }
 
-static void paravirt_cpus_done(void)
-{
-}
-
 static void paravirt_boot_secondary(int cpu, struct task_struct *idle)
 {
 	paravirt_smp_gp[cpu] = (unsigned long)task_thread_info(idle);
@@ -142,7 +138,6 @@ struct plat_smp_ops paravirt_smp_ops = {
 	.send_ipi_mask		= paravirt_send_ipi_mask,
 	.init_secondary		= paravirt_init_secondary,
 	.smp_finish		= paravirt_smp_finish,
-	.cpus_done		= paravirt_cpus_done,
 	.boot_secondary		= paravirt_boot_secondary,
 	.smp_setup		= paravirt_smp_setup,
 	.prepare_cpus		= paravirt_prepare_cpus,
