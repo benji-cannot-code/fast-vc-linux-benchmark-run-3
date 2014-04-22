423b757d20370411571226b8930e07008c964169
Eric Paris <eparis@redhat.com>
2014-04-22 11:46:16 -0400

    audit: fix build error when asm/syscall.h does not exist
    avr32 does not have an asm/syscall.h file.  We need the
    syscall_get_arch() definition from that file for all arch's which
    support CONFIG_AUDITSYSCALL.  Obviously avr32 is not one of those
    arch's.  Move the include inside the CONFIG_AUDITSYSCALL such that we
    only do the include if we need the results.
    
    When the syscall_get_arch() call is moved inside __audit_syscall_entry()
    this include can be dropped entirely.  But that is going to require some
    assembly changes on x86* in a patch that is not ready for the tree...
    
    Reported-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- include/linux/audit.h
+++ include/linux/audit.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/ptrace.h>
 #include <uapi/linux/audit.h>
-#include <asm/syscall.h>
 
 struct audit_sig_info {
 	uid_t		uid;
@@ -111,6 +110,8 @@ extern void audit_log_session_info(struct audit_buffer *ab);
 #endif
 
 #ifdef CONFIG_AUDITSYSCALL
+#include <asm/syscall.h> /* for syscall_get_arch() */
+
 /* These are defined in auditsc.c */
 				/* Public API */
 extern int  audit_alloc(struct task_struct *task);
