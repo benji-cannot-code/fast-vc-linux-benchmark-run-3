b4683b69d40d05619bd5082e8599e34a9d83242c
Sebastian Capella <sebastian.capella@linaro.org>
2014-04-21 17:30:46 -0700

    PM / hibernate: no kernel_power_off when pm_power_off NULL
    Reboot logic in kernel/reboot will avoid calling kernel_power_off
    when pm_power_off is null, and instead uses kernel_halt.  Change
    hibernate's power_down to follow the behavior in the reboot call.
    
    Calling the notifier twice (once for SYS_POWER_OFF and again for
    SYS_HALT) causes a panic during hibernation on Kirkwood
    Openblocks A6 board.
    
    Signed-off-by: Sebastian Capella <sebastian.capella@linaro.org>
    Reported-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
    Reviewed-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -600,7 +600,8 @@ static void power_down(void)
 	case HIBERNATION_PLATFORM:
 		hibernation_platform_enter();
 	case HIBERNATION_SHUTDOWN:
-		kernel_power_off();
+		if (pm_power_off)
+			kernel_power_off();
 		break;
 #ifdef CONFIG_SUSPEND
 	case HIBERNATION_SUSPEND:
@@ -628,7 +629,8 @@ static void power_down(void)
 	 * corruption after resume.
 	 */
 	printk(KERN_CRIT "PM: Please power down manually\n");
-	while(1);
+	while (1)
+		cpu_relax();
 }
 
 /**
