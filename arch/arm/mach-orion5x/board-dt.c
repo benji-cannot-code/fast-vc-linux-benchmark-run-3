2c9e40459af9c88898af15d09bae561603fb55b8
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-04-22 23:26:17 +0200

    ARM: orion5x: convert DT to use the mvebu-mbus driver
    This commit switches the Orion5x Device Tree files to use the DT
    representation and probing for the mvebu-mbus driver. The changes are
    mainly:
    
     * Re-organize the DT to follow the same organization as the one used
       on Armada 370/XP, which is needed for mvebu-mbus to work: a
       top-level soc { ... } node, which corresponds to the MBus bus, and
       a sub-node internal-regs { ... } for all peripherals whose register
       sit only in the "Internal Register Window". This change re-indents
       by one level the definition of all nodes in the Device Tree, which
       explains the large change.
    
     * Use custom functions orion5x_dt_init_early() and
       orion5x_dt_init_time() instead of orion5x_init_early() and
       orion5x_timer_init() as we now want the MBus driver to be probed
       from the Device Tree. We still use the old-style timer
       initialization, but that will be changed in a followup commit.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Link: https://lkml.kernel.org/r/1398202002-28530-14-git-send-email-thomas.petazzoni@free-electrons.com
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-orion5x/board-dt.c
+++ arch/arm/mach-orion5x/board-dt.c
@@ -16,10 +16,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/cpu.h>
+#include <linux/mbus.h>
 #include <asm/system_misc.h>
 #include <asm/mach/arch.h>
+#include <asm/mach/map.h>
 #include <mach/orion5x.h>
+#include <mach/bridge-regs.h>
 #include <plat/irq.h>
+#include <plat/time.h>
 #include "common.h"
 
 static struct of_dev_auxdata orion5x_auxdata_lookup[] __initdata = {
@@ -32,6 +36,16 @@ static struct of_dev_auxdata orion5x_auxdata_lookup[] __initdata = {
 	{},
 };
 
+static void orion5x_dt_init_early(void)
+{
+	orion_time_set_base(TIMER_VIRT_BASE);
+}
+
+static void orion5x_dt_init_time(void)
+{
+	orion5x_timer_init();
+}
+
 static void __init orion5x_dt_init(void)
 {
 	char *dev_name;
@@ -39,6 +53,8 @@ static void __init orion5x_dt_init(void)
 
 	orion5x_id(&dev, &rev, &dev_name);
 	printk(KERN_INFO "Orion ID: %s. TCLK=%d.\n", dev_name, orion5x_tclk);
+
+	BUG_ON(mvebu_mbus_dt_init());
 
 	/*
 	 * Setup Orion address map
@@ -72,9 +88,9 @@ static const char *orion5x_dt_compat[] = {
 DT_MACHINE_START(ORION5X_DT, "Marvell Orion5x (Flattened Device Tree)")
 	/* Maintainer: Thomas Petazzoni <thomas.petazzoni@free-electrons.com> */
 	.map_io		= orion5x_map_io,
-	.init_early	= orion5x_init_early,
+	.init_early	= orion5x_dt_init_early,
 	.init_irq	= orion_dt_init_irq,
-	.init_time	= orion5x_timer_init,
+	.init_time	= orion5x_dt_init_time,
 	.init_machine	= orion5x_dt_init,
 	.restart	= orion5x_restart,
 	.dt_compat	= orion5x_dt_compat,
