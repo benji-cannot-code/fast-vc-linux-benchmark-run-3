3d2e3ab964ffd85456f42727a882d3be59d06cd7
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-04-22 23:26:24 +0200

    ARM: orion5x: switch to use the clock driver for DT platforms
    This commit moves the Orion5x platforms using the Device Tree to use
    the recently introduced clock driver for Orion5x. To achieve that, it:
    
     * Adds the necessary DT description of the clock.
    
     * Selects ORION_CLK to enable the compilation of the clock driver.
    
     * Call of_clk_init() instead of the Orion5x-specific clock
       initialization function.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Link: https://lkml.kernel.org/r/1398202002-28530-21-git-send-email-thomas.petazzoni@free-electrons.com
    Acked-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-orion5x/board-dt.c
+++ arch/arm/mach-orion5x/board-dt.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_platform.h>
 #include <linux/cpu.h>
 #include <linux/mbus.h>
+#include <linux/clk-provider.h>
 #include <asm/system_misc.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -44,6 +45,7 @@ static void orion5x_dt_init_early(void)
 static void orion5x_dt_init_time(void)
 {
 	orion5x_timer_init();
+	of_clk_init(NULL);
 }
 
 static void __init orion5x_dt_init(void)
@@ -60,9 +62,6 @@ static void __init orion5x_dt_init(void)
 	 * Setup Orion address map
 	 */
 	orion5x_setup_wins();
-
-	/* Setup root of clk tree */
-	clk_init();
 
 	/*
 	 * Don't issue "Wait for Interrupt" instruction if we are
