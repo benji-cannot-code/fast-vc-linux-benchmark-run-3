48dba969371766be4927268cd59b6c3c916db750
Valentin Longchamp <valentin.longchamp@keymile.com>
2014-04-22 11:30:31 +0200

    powerpc/mpc85xx: add support for Keymile's kmcoge4 board
    This patch introduces the support for Keymile's kmcoge4 board which is
    the internal reference design for boards based on Freescale's
    P2040/P2041 SoCs. This internal reference design is named kmp204x.
    
    The peripherals used on this board are:
    - SPI NOR Flash as bootloader medium
    - NAND Flash with a ubi partition
    - 2 PCIe busses (hosts 1 and 3)
    - 3 FMAN Ethernet devices (FMAN1 DTSEC1/2/5)
    - 4 Local Bus windows, with one dedicated to the QRIO reset/power mgmt
      CPLD
    - 2 I2C busses
    - last but not least, the mandatory serial port
    
    The patch also adds a defconfig file for this reference design that is
    necessary because of the lowmem option that must be set higher due to
    the number of PCIe devices with big ioremapped mem ranges on the boad.
    
    Signed-off-by: Valentin Longchamp <valentin.longchamp@keymile.com>
    Signed-off-by: Scott Wood <scottwood@freescale.com>

--- arch/powerpc/platforms/85xx/corenet_generic.c
+++ arch/powerpc/platforms/85xx/corenet_generic.c
@@ -68,7 +68,7 @@ void __init corenet_gen_setup_arch(void)
 
 	swiotlb_detect_4g();
 
-	pr_info("%s board from Freescale Semiconductor\n", ppc_md.name);
+	pr_info("%s board\n", ppc_md.name);
 
 	mpc85xx_qe_init();
 }
@@ -123,6 +123,7 @@ static const char * const boards[] __initconst = {
 	"fsl,B4860QDS",
 	"fsl,B4420QDS",
 	"fsl,B4220QDS",
+	"keymile,kmcoge4",
 	NULL
 };
 
