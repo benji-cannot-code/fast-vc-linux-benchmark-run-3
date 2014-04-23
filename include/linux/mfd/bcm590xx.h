c1741b364c294f5d10c7b1497a55b5fbfadf415c
Matt Porter <mporter@linaro.org>
2014-04-23 19:21:31 -0400

    mfd: bcm590xx: Add support for secondary I2C slave address
    BCM590xx utilizes a secondary I2C slave address to access additional
    register space. Add support for the secondary address space by
    instantiating a dummy I2C device with the appropriate secondary
    I2C slave address. Also expose a secondary regmap register space so
    that MFD drivers can access this secondary i2c slave address space.
    
    Signed-off-by: Matt Porter <mporter@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/bcm590xx.h
+++ include/linux/mfd/bcm590xx.h
@@ -20,12 +20,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regmap.h>
 
 /* max register address */
-#define BCM590XX_MAX_REGISTER	0xe7
+#define BCM590XX_MAX_REGISTER_PRI	0xe7
+#define BCM590XX_MAX_REGISTER_SEC	0xf0
 
 struct bcm590xx {
 	struct device *dev;
-	struct i2c_client *i2c_client;
-	struct regmap *regmap;
+	struct i2c_client *i2c_pri;
+	struct i2c_client *i2c_sec;
+	struct regmap *regmap_pri;
+	struct regmap *regmap_sec;
 	unsigned int id;
 };
 
