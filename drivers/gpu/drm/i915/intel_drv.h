84656f292c7064618863e9ce408e2681a9c82b43
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-04-24 23:54:47 +0200

    drm/i915: Track hdmi mode in the pipe config
    Also add state readout and cross-check support. The only invasive change
    is wiring up the new flag to the ->set_infoframes callbacks.
    
    Reviewed-by: Naresh Kumar Kachhi <naresh.kumar.kachhi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -274,6 +274,9 @@ struct intel_crtc_config {
 	 * accordingly. */
 	bool has_dp_encoder;
 
+	/* Whether we should send NULL infoframes. Required for audio. */
+	bool has_hdmi_sink;
+
 	/*
 	 * Enable dithering, used when the selected pipe bpp doesn't match the
 	 * plane bpp.
@@ -487,6 +490,7 @@ struct intel_hdmi {
 				enum hdmi_infoframe_type type,
 				const void *frame, ssize_t len);
 	void (*set_infoframes)(struct drm_encoder *encoder,
+			       bool enable,
 			       struct drm_display_mode *adjusted_mode);
 };
 
