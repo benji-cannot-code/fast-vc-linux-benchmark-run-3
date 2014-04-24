d4d1ea255025400a5a7289fbc5ba9d1295856e9f
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-04-24 23:54:56 +0200

    drm/i915/hdmi: Remove ->mode_set callback
    Similar to dp the only thing we do is call intel_write_eld and prepare
    a bit of state for the enable hooks. The only difference is that we
    write that to the hardware instead of keeping track of it somewhere in
    software.
    
    Still we can just move all this to the very first enable hook.
    
    Reviewed-by: Naresh Kumar Kachhi <naresh.kumar.kachhi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_hdmi.c
+++ drivers/gpu/drm/i915/intel_hdmi.c
@@ -634,7 +634,7 @@ static void hsw_set_infoframes(struct drm_encoder *encoder,
 	intel_hdmi_set_hdmi_infoframe(encoder, adjusted_mode);
 }
 
-static void intel_hdmi_mode_set(struct intel_encoder *encoder)
+static void intel_hdmi_prepare(struct intel_encoder *encoder)
 {
 	struct drm_device *dev = encoder->base.dev;
 	struct drm_i915_private *dev_priv = dev->dev_private;
@@ -1139,6 +1139,8 @@ static void intel_hdmi_pre_enable(struct intel_encoder *encoder)
 	struct drm_display_mode *adjusted_mode =
 		&intel_crtc->config.adjusted_mode;
 
+	intel_hdmi_prepare(encoder);
+
 	intel_hdmi->set_infoframes(&encoder->base,
 				   intel_crtc->config.has_hdmi_sink,
 				   adjusted_mode);
@@ -1202,6 +1204,8 @@ static void vlv_hdmi_pre_pll_enable(struct intel_encoder *encoder)
 		to_intel_crtc(encoder->base.crtc);
 	enum dpio_channel port = vlv_dport_to_channel(dport);
 	int pipe = intel_crtc->pipe;
+
+	intel_hdmi_prepare(encoder);
 
 	/* Program Tx lane resets to default */
 	mutex_lock(&dev_priv->dpio_lock);
@@ -1449,7 +1453,6 @@ void intel_hdmi_init(struct drm_device *dev, int hdmi_reg, enum port port)
 			 DRM_MODE_ENCODER_TMDS);
 
 	intel_encoder->compute_config = intel_hdmi_compute_config;
-	intel_encoder->mode_set = intel_hdmi_mode_set;
 	intel_encoder->disable = intel_disable_hdmi;
 	intel_encoder->get_hw_state = intel_hdmi_get_hw_state;
 	intel_encoder->get_config = intel_hdmi_get_config;
