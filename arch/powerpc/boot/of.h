8a289f946a1f1f1115e75e477cdc8eabed46514e
Cédric Le Goater <clg@fr.ibm.com>
2014-04-24 09:23:33 +0200

    powerpc/boot: Define byteswapping routines for little endian
    These are not the most efficient versions of swab but the wrapper does
    not do much byte swapping. On a big endian cpu, these routines are
    a no-op.
    
    Signed-off-by: Cédric Le Goater <clg@fr.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/boot/of.h
+++ arch/powerpc/boot/of.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _PPC_BOOT_OF_H_
 #define _PPC_BOOT_OF_H_
 
+#include "swab.h"
+
 typedef void *phandle;
 typedef u32 ihandle;
 
@@ -22,8 +24,13 @@ void of_console_init(void);
 
 typedef u32			__be32;
 
+#ifdef __LITTLE_ENDIAN__
+#define cpu_to_be32(x) swab32(x)
+#define be32_to_cpu(x) swab32(x)
+#else
 #define cpu_to_be32(x) (x)
 #define be32_to_cpu(x) (x)
+#endif
 
 #define PROM_ERROR (-1u)
 
