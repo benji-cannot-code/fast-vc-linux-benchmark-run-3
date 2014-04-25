e17a6fe77e36f070029adbc0b6f1a37362c0fbcd
Andy Gross <agross@codeaurora.org>
2014-04-25 15:41:55 -0500

    pinctrl: qcom: Correct name for pin 0
    Fix copy/paste error in pinctrl_pin_desc for pin 0.
    
    Signed-off-by: Andy Gross <agross@codeaurora.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-ipq8064.c
+++ drivers/pinctrl/pinctrl-ipq8064.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "pinctrl-msm.h"
 
 static const struct pinctrl_pin_desc ipq8064_pins[] = {
-	PINCTRL_PIN(0, "GPIO_1"),
+	PINCTRL_PIN(0, "GPIO_0"),
 	PINCTRL_PIN(1, "GPIO_1"),
 	PINCTRL_PIN(2, "GPIO_2"),
 	PINCTRL_PIN(3, "GPIO_3"),
