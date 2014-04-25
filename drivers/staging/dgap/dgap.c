667905461a66012e64fc9270d066a9030144f358
Mark Hounschell <markh@compro.net>
2014-04-25 13:10:20 -0400

    staging: dgap: Add check for MAXBOARDS in .probe (dgap_init_one)
    Insure we don't allow configuring more than MAXBOARDS (32).
    
    Signed-off-by: Mark Hounschell <markh@compro.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap.c
+++ drivers/staging/dgap/dgap.c
@@ -573,6 +573,9 @@ static int dgap_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
 	int rc;
 
+	if (dgap_NumBoards >= MAXBOARDS)
+		return -EPERM;
+
 	/* wake up and enable device */
 	rc = pci_enable_device(pdev);
 
