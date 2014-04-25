0075f7369e3dff0b749d365a584e9bae1d782f75
Jon Ringle <jringle@gridpoint.com>
2014-04-25 15:53:10 -0400

    serial: sc16is7xx: fix implicit decl of func copy_{to,from}_user
    Fix by including linux/uaccess.h:
    
       drivers/tty/serial/sc16is7xx.c: In function 'sc16is7xx_ioctl':
    >> drivers/tty/serial/sc16is7xx.c:861:3: error: implicit declaration of function 'copy_from_user' [-Werror=implicit-function-declaration]
    >> drivers/tty/serial/sc16is7xx.c:867:3: error: implicit declaration of function 'copy_to_user' [-Werror=implicit-function-declaration]
    
    Signed-off-by: Jon Ringle <jringle@gridpoint.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/sc16is7xx.c
+++ drivers/tty/serial/sc16is7xx.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial.h>
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
+#include <linux/uaccess.h>
 
 #define SC16IS7XX_NAME			"sc16is7xx"
 
