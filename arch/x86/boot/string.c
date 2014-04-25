0b244221d8c22e6ce67b156df01a82ef55df61e8
Vivek Goyal <vgoyal@redhat.com>
2014-04-25 13:46:11 -0400

    x86, boot: Do not include boot.h in string.c
    string.c does not require whole of boot.h. Just inclusion of linux/types.h
    and ctypes.h seems to be sufficient.
    
    Keep list of stuff being included in string.c to bare minimal so that
    string.c can be included in other places easily.
    
    For example, Currently boot/compressed/string.c includes boot/string.c
    but looks like it does not want boot/boot.h. Hence there is a define
    in boot/compressed/misc.h "define BOOT_BOOT_H" which prevents inclusion
    of boot.h in compressed/string.c. And compressed/string.c is forced to
    include misc.h just for that reason.
    
    So by removing inclusion of boot.h, we can also get rid of inclusion of
    misch.h in compressed/misc.c.
    
    This also enables including of boot/string.c in purgatory/ code relatively
    easily.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Link: http://lkml.kernel.org/r/1398447972-27896-2-git-send-email-vgoyal@redhat.com
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/x86/boot/string.c
+++ arch/x86/boot/string.c
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Very basic string functions
  */
 
-#include "boot.h"
+#include <linux/types.h>
+#include "ctype.h"
 
 /*
  * This file gets included in compressed/string.c which might pull in
