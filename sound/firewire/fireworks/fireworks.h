fc1790cf0868247f56758d843515a0bb13f3fba2
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-04-25 22:45:08 +0900

    ALSA: fireworks: Add proc interface for debugging purpose
    This commit adds proc interface to output infomation for debugging.
     - firmware information
     - sampling rate and clock source
     - physical metering (linear value)
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/fireworks/fireworks.h
+++ sound/firewire/fireworks/fireworks.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/core.h>
 #include <sound/initval.h>
 #include <sound/pcm.h>
+#include <sound/info.h>
 
 #include "../packets-buffer.h"
 #include "../iso-resources.h"
@@ -80,6 +81,14 @@ struct snd_efw {
 	struct cmp_connection in_conn;
 	atomic_t capture_substreams;
 	atomic_t playback_substreams;
+
+	/* hardware metering parameters */
+	unsigned int phys_out;
+	unsigned int phys_in;
+	unsigned int phys_out_grp_count;
+	unsigned int phys_in_grp_count;
+	struct snd_efw_phys_grp phys_out_grps[HWINFO_MAX_CAPS_GROUPS];
+	struct snd_efw_phys_grp phys_in_grps[HWINFO_MAX_CAPS_GROUPS];
 };
 
 struct snd_efw_transaction {
@@ -187,6 +196,8 @@ int snd_efw_stream_start_duplex(struct snd_efw *efw, int sampling_rate);
 void snd_efw_stream_stop_duplex(struct snd_efw *efw);
 void snd_efw_stream_update_duplex(struct snd_efw *efw);
 void snd_efw_stream_destroy_duplex(struct snd_efw *efw);
+
+void snd_efw_proc_init(struct snd_efw *efw);
 
 #define SND_EFW_DEV_ENTRY(vendor, model) \
 { \
