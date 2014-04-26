03ca16f78b83c09d6342dada73523dad24a61c1a
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-04-26 18:55:02 +0200

    staging: rtl8723au: Remove obsolete header file cmd_osdep.h
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_cmd.c
+++ drivers/staging/rtl8723au/core/rtw_cmd.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <osdep_service.h>
 #include <drv_types.h>
 #include <recv_osdep.h>
-#include <cmd_osdep.h>
 #include <mlme_osdep.h>
 #include <rtl8723a_cmd.h>
 
