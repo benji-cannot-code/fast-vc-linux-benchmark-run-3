ff326a4bd6f24fb368b4e60ef767b6ace900f9e1
Lars-Peter Clausen <lars@metafoo.de>
2014-04-26 13:02:29 +0200

    ASoC: Remove name_prefix unset during DAI link init hack again
    This was initially removed in commit 6479f15ad ("ASoC: Remove name_prefix unset
    during DAI link init hack"), but was brought back in commit 503ae5e0 ("ASoC:
    core: Add helpers for dai link and aux dev init") by accident. This patch
    removes it again.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1275,13 +1275,9 @@ static int soc_aux_dev_init(struct snd_soc_card *card,
 {
 	struct snd_soc_aux_dev *aux_dev = &card->aux_dev[num];
 	struct snd_soc_pcm_runtime *rtd = &card->rtd_aux[num];
-	const char *temp;
 	int ret;
 
 	rtd->card = card;
-
-	temp = codec->name_prefix;
-	codec->name_prefix = NULL;
 
 	/* do machine specific initialization */
 	if (aux_dev->init) {
@@ -1289,8 +1285,6 @@ static int soc_aux_dev_init(struct snd_soc_card *card,
 		if (ret < 0)
 			return ret;
 	}
-
-	codec->name_prefix = temp;
 
 	rtd->codec = codec;
 
@@ -1303,14 +1297,9 @@ static int soc_dai_link_init(struct snd_soc_card *card,
 {
 	struct snd_soc_dai_link *dai_link =  &card->dai_link[num];
 	struct snd_soc_pcm_runtime *rtd = &card->rtd[num];
-	const char *temp;
 	int ret;
 
 	rtd->card = card;
-
-	/* machine controls, routes and widgets are not prefixed */
-	temp = codec->name_prefix;
-	codec->name_prefix = NULL;
 
 	/* do machine specific initialization */
 	if (dai_link->init) {
@@ -1318,8 +1307,6 @@ static int soc_dai_link_init(struct snd_soc_card *card,
 		if (ret < 0)
 			return ret;
 	}
-
-	codec->name_prefix = temp;
 
 	rtd->codec = codec;
 
