32ffe7d4e1b7dee4aaee518f01423ef09bdbcddc
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-04-26 18:54:59 +0200

    staging: rtl8723au: Fold _rtw_free_cmd_priv23a() into rtw_free_cmd_priv23a()
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/include/cmd_osdep.h
+++ drivers/staging/rtl8723au/include/cmd_osdep.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int _rtw_init_evt_priv23a(struct evt_priv *pevtpriv);
 void _rtw_free_evt_priv23a(struct	evt_priv *pevtpriv);
-void _rtw_free_cmd_priv23a(struct	cmd_priv *pcmdpriv);
 int _rtw_enqueue_cmd23a(struct rtw_queue *queue, struct cmd_obj *obj);
 
 #endif
