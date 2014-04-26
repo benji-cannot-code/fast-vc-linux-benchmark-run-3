49901c90af34f4f1e2539311e918c9462f7e9d31
Sebastian Reichel <sre@kernel.org>
2014-04-25 21:50:13 -0700

    Input: tsc2005 - use dev_err for error messages
    Change some dev_dbg() invocations to dev_err() ones, because they
    are supposed to output error messages.
    
    Signed-off-by: Sebastian Reichel <sre@kernel.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/tsc2005.c
+++ drivers/input/touchscreen/tsc2005.c
@@ -580,7 +580,7 @@ static int tsc2005_probe(struct spi_device *spi)
 	int error;
 
 	if (!pdata) {
-		dev_dbg(&spi->dev, "no platform data\n");
+		dev_err(&spi->dev, "no platform data\n");
 		return -ENODEV;
 	}
 
@@ -592,7 +592,7 @@ static int tsc2005_probe(struct spi_device *spi)
 	max_p	= pdata->ts_pressure_max   ? : MAX_12BIT;
 
 	if (spi->irq <= 0) {
-		dev_dbg(&spi->dev, "no irq\n");
+		dev_err(&spi->dev, "no irq\n");
 		return -ENODEV;
 	}
 
