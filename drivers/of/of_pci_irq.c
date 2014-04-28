ed25476f43981d5b5e0949ddc795fe09880241f4
Ian Molton <ian.molton@codethink.co.uk>
2014-04-28 15:32:21 +0100

    pci/of: Remove dead code
    	Commit 98d9f30c820d509145757e6ecbc36013aa02f7bc
    "pci/of: Match PCI devices to OF nodes dynamically" introduced a lot of code
    derived from the PPC PCI code, including some likes which were redundant.
    
    Remove these lines.
    
    Reviewed-by: William Towle <william.towle@codethink.co.uk>
    Signed-off-by: Ian Molton <ian.molton@codethink.co.uk>
    Signed-off-by: Grant Likely <grant.likely@linaro.org>

--- drivers/of/of_pci_irq.c
+++ drivers/of/of_pci_irq.c
@@ -20,7 +20,6 @@ int of_irq_parse_pci(const struct pci_dev *pdev, struct of_phandle_args *out_irq
 	struct device_node *dn, *ppnode;
 	struct pci_dev *ppdev;
 	u32 lspec;
-	__be32 lspec_be;
 	__be32 laddr[3];
 	u8 pin;
 	int rc;
@@ -88,7 +87,6 @@ int of_irq_parse_pci(const struct pci_dev *pdev, struct of_phandle_args *out_irq
 	out_irq->np = ppnode;
 	out_irq->args_count = 1;
 	out_irq->args[0] = lspec;
-	lspec_be = cpu_to_be32(lspec);
 	laddr[0] = cpu_to_be32((pdev->bus->number << 16) | (pdev->devfn << 8));
 	laddr[1] = laddr[2] = cpu_to_be32(0);
 	return of_irq_parse_raw(laddr, out_irq);
