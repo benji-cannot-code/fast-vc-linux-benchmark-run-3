ae7eec5adc820098240d13460229b3a5c34341e1
Tim Bird <tbird20d@gmail.com>
2014-04-28 16:34:19 +0300

    usb: phy: msm: Fix PTS definitions for MSM USB controller
    Fix the value used for Parallel Transceiver Select (PTS) for the MSM USB
    controller.  This is a standard chipidea PORTSC definition, where
    a PHY_TYPE of 10b (<<30) is ULPI and 11b (<<30) is SERIAL.
    Fix the definitions and use them correctly in the driver code.
    
    Signed-off-by: Tim Bird <tim.bird@sonymobile.com>
    Signed-off-by: Ivan T. Ivanov <iivanov@mm-sol.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- include/linux/usb/msm_hsusb_hw.h
+++ include/linux/usb/msm_hsusb_hw.h
@@ -32,8 +32,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_USBINTR          (MSM_USB_BASE + 0x0148)
 
 #define PORTSC_PHCD            (1 << 23) /* phy suspend mode */
-#define PORTSC_PTS_MASK         (3 << 30)
-#define PORTSC_PTS_ULPI         (3 << 30)
+#define PORTSC_PTS_MASK        (3 << 30)
+#define PORTSC_PTS_ULPI        (2 << 30)
+#define PORTSC_PTS_SERIAL      (3 << 30)
 
 #define USB_ULPI_VIEWPORT    (MSM_USB_BASE + 0x0170)
 #define ULPI_RUN              (1 << 30)
