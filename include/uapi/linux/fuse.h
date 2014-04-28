e28997cde28dc0939251092bbb8c9ca5cded524f
Miklos Szeredi <mszeredi@suse.cz>
2014-04-28 16:43:44 +0200

    fuse: add renameat2 support
    Support RENAME_EXCHANGE and RENAME_NOREPLACE flags on the userspace ABI.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -101,6 +101,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  - add reserved space to fuse_init_out
  *  - add FATTR_CTIME
  *  - add ctime and ctimensec to fuse_setattr_in
+ *  - add FUSE_RENAME2 request
  */
 
 #ifndef _LINUX_FUSE_H
@@ -354,6 +355,7 @@ enum fuse_opcode {
 	FUSE_BATCH_FORGET  = 42,
 	FUSE_FALLOCATE     = 43,
 	FUSE_READDIRPLUS   = 44,
+	FUSE_RENAME2       = 45,
 
 	/* CUSE specific operations */
 	CUSE_INIT          = 4096,
@@ -430,6 +432,12 @@ struct fuse_mkdir_in {
 
 struct fuse_rename_in {
 	uint64_t	newdir;
+};
+
+struct fuse_rename2_in {
+	uint64_t	newdir;
+	uint32_t	flags;
+	uint32_t	padding;
 };
 
 struct fuse_link_in {
