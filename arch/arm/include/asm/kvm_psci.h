ca3ce7562d205eadb4185414579fbef9f1f9af0d
Anup Patel <anup.patel@linaro.org>
2014-04-29 11:24:18 +0530

    ARM/ARM64: KVM: Make kvm_psci_call() return convention more flexible
    Currently, the kvm_psci_call() returns 'true' or 'false' based on whether
    the PSCI function call was handled successfully or not. This does not help
    us emulate system-level PSCI functions where the actual emulation work will
    be done by user space (QEMU or KVMTOOL). Examples of such system-level PSCI
    functions are: PSCI v0.2 SYSTEM_OFF and SYSTEM_RESET.
    
    This patch updates kvm_psci_call() to return three types of values:
    1) > 0 (success)
    2) = 0 (success but exit to user space)
    3) < 0 (errors)
    
    Signed-off-by: Anup Patel <anup.patel@linaro.org>
    Signed-off-by: Pranavkumar Sawargaonkar <pranavkumar@linaro.org>
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/arm/include/asm/kvm_psci.h
+++ arch/arm/include/asm/kvm_psci.h
@@ -23,6 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KVM_ARM_PSCI_0_2	2
 
 int kvm_psci_version(struct kvm_vcpu *vcpu);
-bool kvm_psci_call(struct kvm_vcpu *vcpu);
+int kvm_psci_call(struct kvm_vcpu *vcpu);
 
 #endif /* __ARM_KVM_PSCI_H__ */
