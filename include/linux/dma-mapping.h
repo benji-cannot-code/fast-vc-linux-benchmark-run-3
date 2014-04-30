fac91ab774dad9959c7aac76399bae5b29ffeed7
Bjorn Helgaas <bhelgaas@google.com>
2014-04-30 11:20:53 -0600

    DMA-API: Clarify physical/bus address distinction
    The DMA-API documentation sometimes refers to "physical addresses" when it
    really means "bus addresses."  Sometimes these are identical, but they may
    be different if the bridge leading to the bus performs address translation.
    Update the documentation to use "bus address" when appropriate.
    
    Also, consistently capitalize "DMA", use parens with function names, use
    dev_printk() in examples, and reword a few sections for clarity.
    
    No functional change; documentation changes only.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: James Bottomley <jbottomley@Parallels.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -9,6 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-direction.h>
 #include <linux/scatterlist.h>
 
+/*
+ * A dma_addr_t can hold any valid DMA or bus address for the platform.
+ * It can be given to a device to use as a DMA source or target.  A CPU cannot
+ * reference a dma_addr_t directly because there may be translation between
+ * its physical address space and the bus address space.
+ */
 struct dma_map_ops {
 	void* (*alloc)(struct device *dev, size_t size,
 				dma_addr_t *dma_handle, gfp_t gfp,
