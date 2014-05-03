d46dd0d21e51183336c0864f52ebc924aa16a5ec
navin patidar <navin.patidar@gmail.com>
2014-05-03 17:15:30 +0530

    staging: rtl8188eu: Remove 'u32 ref_cnt' from struct recv_buf
    Driver isn't making any use of value stored in variable ref_cnt.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/rtl8188eu_recv.c
+++ drivers/staging/rtl8188eu/hal/rtl8188eu_recv.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void rtl8188eu_init_recvbuf(struct adapter *padapter, struct recv_buf *precvbuf)
 {
-	precvbuf->ref_cnt = 0;
 }
 
 int	rtl8188eu_init_recv_priv(struct adapter *padapter)
@@ -64,7 +63,6 @@ int	rtl8188eu_init_recv_priv(struct adapter *padapter)
 		res = rtw_os_recvbuf_resource_alloc(padapter, precvbuf);
 		if (res == _FAIL)
 			break;
-		precvbuf->ref_cnt = 0;
 		precvbuf->adapter = padapter;
 		precvbuf++;
 	}
--- drivers/staging/rtl8188eu/include/rtw_recv.h
+++ drivers/staging/rtl8188eu/include/rtw_recv.h
@@ -236,7 +236,6 @@ struct sta_recv_priv {
 };
 
 struct recv_buf {
-	u32	ref_cnt;
 	struct adapter *adapter;
 	u8	*pbuf;
 	struct urb *purb;
