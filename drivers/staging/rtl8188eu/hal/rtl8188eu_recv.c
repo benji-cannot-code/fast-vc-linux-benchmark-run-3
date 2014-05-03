b42a5e016134bd120cecb80d0bc16d30299bb5b4
navin patidar <navin.patidar@gmail.com>
2014-05-03 17:15:29 +0530

    staging: rtl8188eu: Remove 'u32 len' from struct recv_buf
    Remove unused variable 'u32 len'.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/rtl8188eu_recv.c
+++ drivers/staging/rtl8188eu/hal/rtl8188eu_recv.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void rtl8188eu_init_recvbuf(struct adapter *padapter, struct recv_buf *precvbuf)
 {
-	precvbuf->len = 0;
 	precvbuf->ref_cnt = 0;
 }
 
