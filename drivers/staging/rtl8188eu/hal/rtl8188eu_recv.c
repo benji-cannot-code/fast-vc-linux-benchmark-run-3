b7a89a1d4f6deba053151e81ab990051378667a3
navin patidar <navin.patidar@gmail.com>
2014-05-03 17:15:25 +0530

    staging: rtl8188eu: Remove pdata, phead, ptail and pend from struct recv_buf
    Driver is not making use of value stored in removed variables.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/rtl8188eu_recv.c
+++ drivers/staging/rtl8188eu/hal/rtl8188eu_recv.c
@@ -32,15 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void rtl8188eu_init_recvbuf(struct adapter *padapter, struct recv_buf *precvbuf)
 {
 	precvbuf->len = 0;
-
 	precvbuf->ref_cnt = 0;
-
-	if (precvbuf->pbuf) {
-		precvbuf->pdata = precvbuf->pbuf;
-		precvbuf->phead = precvbuf->pbuf;
-		precvbuf->ptail = precvbuf->pbuf;
-		precvbuf->pend = precvbuf->pdata + MAX_RECVBUF_SZ;
-	}
 }
 
 int	rtl8188eu_init_recv_priv(struct adapter *padapter)
