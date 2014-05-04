f64d591b22e59942fdecf693d3554441a2243713
Alessandro Miceli <angelofsky1980@gmail.com>
2014-05-04 07:50:31 -0300

    [media] rtl28xxu: add [1b80:d3af] Sveon STV27
    Added support for Sveon STV27 device (rtl2832u + FC0013 tuner)
    
    Signed-off-by: Alessandro Miceli <angelofsky1980@gmail.com>
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Cc: stable@vger.kernel.org
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/dvb-core/dvb-usb-ids.h
+++ drivers/media/dvb-core/dvb-usb-ids.h
@@ -377,4 +377,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_PID_CTVDIGDUAL_V2				0xe410
 #define USB_PID_PCTV_2002E                              0x025c
 #define USB_PID_PCTV_2002E_SE                           0x025d
+#define USB_PID_SVEON_STV27                             0xd3af
 #endif
