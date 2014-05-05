1ccc04c5b6d457a176e3a8ce10d8b33efa0af1f1
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2014-05-05 08:39:44 +0530

    KVM: PPC: BOOK3S: PR: Enable Little Endian PR guest
    This patch make sure we inherit the LE bit correctly in different case
    so that we can run Little Endian distro in PR mode
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/kvm/book3s_64_mmu.c
+++ arch/powerpc/kvm/book3s_64_mmu.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void kvmppc_mmu_book3s_64_reset_msr(struct kvm_vcpu *vcpu)
 {
-	kvmppc_set_msr(vcpu, MSR_SF);
+	kvmppc_set_msr(vcpu, vcpu->arch.intr_msr);
 }
 
 static struct kvmppc_slb *kvmppc_mmu_book3s_64_find_slbe(
