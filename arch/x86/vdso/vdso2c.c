42394d0b4ef2827bb90bcdbeb9df3939f57f363d
Andy Lutomirski <luto@amacapital.net>
2014-05-05 12:19:35 -0700

    x86, vdso: Move the 32-bit vdso special pages after the text
    This unifies the vdso mapping code and teaches it how to map special
    pages at addresses corresponding to symbols in the vdso image.  The
    new code is used for all vdso variants, but so far only the 32-bit
    variants use the new vvar page position.
    
    Signed-off-by: Andy Lutomirski <luto@amacapital.net>
    Link: http://lkml.kernel.org/r/b6d7858ad7b5ac3fd3c29cab6d6d769bc45d195e.1399317206.git.luto@amacapital.net
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- arch/x86/vdso/vdso2c.c
+++ arch/x86/vdso/vdso2c.c
@@ -16,7 +16,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 /* Symbols that we need in vdso2c. */
+enum {
+	sym_vvar_page,
+	sym_hpet_page,
+	sym_end_mapping,
+};
+
+const int special_pages[] = {
+	sym_vvar_page,
+	sym_hpet_page,
+};
+
 char const * const required_syms[] = {
+	[sym_vvar_page] = "vvar_page",
+	[sym_hpet_page] = "hpet_page",
+	[sym_end_mapping] = "end_mapping",
 	"VDSO32_NOTE_MASK",
 	"VDSO32_SYSENTER_RETURN",
 	"__kernel_vsyscall",
