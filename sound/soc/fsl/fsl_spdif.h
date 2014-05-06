dbe084ee413684fd043a27ba38d7c68c50114050
Nicolin Chen <Guangyu.Chen@freescale.com>
2014-05-06 16:42:25 +0800

    ASoC: fsl_spdif: Improve coding style
    1) Apply better indentations
    2) Drop braces for single statement.
    3) Use simpler ternary to reduce code.
    
    Signed-off-by: Nicolin Chen <Guangyu.Chen@freescale.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/fsl/fsl_spdif.h
+++ sound/soc/fsl/fsl_spdif.h
@@ -94,6 +94,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SCR_USRC_SEL_RECV		(0x1 << SCR_USRC_SEL_OFFSET)
 #define SCR_USRC_SEL_CHIP		(0x3 << SCR_USRC_SEL_OFFSET)
 
+#define SCR_DMA_xX_EN(tx)		(tx ? SCR_DMA_TX_EN : SCR_DMA_RX_EN)
+
 /* SPDIF CDText control */
 #define SRCD_CD_USER_OFFSET		1
 #define SRCD_CD_USER			(1 << SRCD_CD_USER_OFFSET)
