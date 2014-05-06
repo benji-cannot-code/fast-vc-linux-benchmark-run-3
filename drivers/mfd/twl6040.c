d3a8f868e8e3e425caabec6351dbae634c636a41
Peter Ujfalusi <peter.ujfalusi@ti.com>
2014-05-06 11:46:10 +0300

    mfd: twl6040: Correct HPPLL configuration for 19.2 and 38.4 MHz mclk
    When the MCLK is 19.2 or 38.4 MHz the HPPLL need to be enabled and can be
    put in bypass mode.
    This will fix HPPLL use on boards with 19.2MHz mclk.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/twl6040.c
+++ drivers/mfd/twl6040.c
@@ -442,12 +442,9 @@ int twl6040_set_pll(struct twl6040 *twl6040, int pll_id,
 					    TWL6040_HPLLENA;
 				break;
 			case 19200000:
-				/*
-				* PLL disabled
-				* (enable PLL if MCLK jitter quality
-				*  doesn't meet specification)
-				*/
-				hppllctl |= TWL6040_MCLK_19200KHZ;
+				/* PLL enabled, bypass mode */
+				hppllctl |= TWL6040_MCLK_19200KHZ |
+					    TWL6040_HPLLBP | TWL6040_HPLLENA;
 				break;
 			case 26000000:
 				/* PLL enabled, active mode */
@@ -455,9 +452,9 @@ int twl6040_set_pll(struct twl6040 *twl6040, int pll_id,
 					    TWL6040_HPLLENA;
 				break;
 			case 38400000:
-				/* PLL enabled, active mode */
+				/* PLL enabled, bypass mode */
 				hppllctl |= TWL6040_MCLK_38400KHZ |
-					    TWL6040_HPLLENA;
+					    TWL6040_HPLLBP | TWL6040_HPLLENA;
 				break;
 			default:
 				dev_err(twl6040->dev,
