8afc6c2e2ef432128f53d6ee357f0eaeed2413c6
Thomas Gleixner <tglx@linutronix.de>
2014-05-07 15:44:11 +0000

    iommu: dmar: Provide arch specific irq allocation
    ia64 and x86 share this driver. x86 is moving to a different irq
    allocation and ia64 keeps its private irq_create/destroy stuff.
    
    Use macros to redirect to one or the other. Yes, macros to avoid
    include hell.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Grant Likely <grant.likely@linaro.org>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Acked-by: Joerg Roedel <joro@8bytes.org>
    Cc: x86@kernel.org
    Cc: linux-ia64@vger.kernel.org
    Cc: iommu@lists.linux-foundation.org
    Link: http://lkml.kernel.org/r/20140507154336.372289825@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/ia64/include/asm/irq_remapping.h
+++ arch/ia64/include/asm/irq_remapping.h
@@ -2,4 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __IA64_INTR_REMAPPING_H
 #define __IA64_INTR_REMAPPING_H
 #define irq_remapping_enabled 0
+#define dmar_alloc_hwirq	create_irq
+#define dmar_free_hwirq		destroy_irq
 #endif
