c3db1f8e9c47d60c8a6b2a8314b0e29ec23f0d46
Shaik Ameer Basha <shaik.ameer@samsung.com>
2014-05-08 16:57:56 +0530

    clk: samsung: exynos5420: update clocks for PERIC block
    This patch includes,
        1] renaming of the HSI2C clocks
        2] renaming of spi clocks according to the datasheet
        3] fixes for child-parent relationships
        4] adding of more clocks related to PERIC block
        5] use GATE_IP_* offsets instead of GATE_BUS_*
    
    Signed-off-by: Rahul Sharma <rahul.sharma@samsung.com>
    Signed-off-by: Shaik Ameer Basha <shaik.ameer@samsung.com>
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>

--- include/dt-bindings/clock/exynos5420.h
+++ include/dt-bindings/clock/exynos5420.h
@@ -70,10 +70,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_I2C1		262
 #define CLK_I2C2		263
 #define CLK_I2C3		264
-#define CLK_I2C4		265
-#define CLK_I2C5		266
-#define CLK_I2C6		267
-#define CLK_I2C7		268
+#define CLK_USI0		265
+#define CLK_USI1		266
+#define CLK_USI2		267
+#define CLK_USI3		268
 #define CLK_I2C_HDMI		269
 #define CLK_TSADC		270
 #define CLK_SPI0		271
@@ -86,9 +86,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_PCM2		278
 #define CLK_PWM			279
 #define CLK_SPDIF		280
-#define CLK_I2C8		281
-#define CLK_I2C9		282
-#define CLK_I2C10		283
+#define CLK_USI4		281
+#define CLK_USI5		282
+#define CLK_USI6		283
 #define CLK_ACLK66_PSGEN	300
 #define CLK_CHIPID		301
 #define CLK_SYSREG		302
