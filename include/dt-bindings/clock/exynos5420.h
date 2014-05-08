9963610a00dffe43f9ac7c8127f72843523a0feb
Shaik Ameer Basha <shaik.ameer@samsung.com>
2014-05-08 16:58:03 +0530

    clk: samsung: exynos5420: add misc clocks
    This patch adds some missing miscellaneous clocks specific
    to exynos5420.
    
    Signed-off-by: Rahul Sharma <rahul.sharma@samsung.com>
    Signed-off-by: Shaik Ameer Basha <shaik.ameer@samsung.com>
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>

--- include/dt-bindings/clock/exynos5420.h
+++ include/dt-bindings/clock/exynos5420.h
@@ -60,6 +60,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_SCLK_GSCL_WB	157
 #define CLK_SCLK_HDMIPHY	158
 #define CLK_MAU_EPLL		159
+#define CLK_SCLK_HSIC_12M	160
+#define CLK_SCLK_MPHY_IXTAL24	161
 
 /* gate clocks */
 #define CLK_ACLK66_PERIC	256
