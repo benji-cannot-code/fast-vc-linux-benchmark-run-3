5d1f71b2483c61a3b11febe9de17d1259e908e86
Shaik Ameer Basha <shaik.ameer@samsung.com>
2014-05-08 16:57:52 +0530

    clk: samsung: exynos5420: update clocks for GSCL and MSCL blocks
    This patch adds the missing GSCL and MSCL block clocks
    and corrects some wrong parent-child relationships.
    
    Signed-off-by: Rahul Sharma <rahul.sharma@samsung.com>
    Signed-off-by: Shaik Ameer Basha <shaik.ameer@samsung.com>
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>

--- include/dt-bindings/clock/exynos5420.h
+++ include/dt-bindings/clock/exynos5420.h
@@ -160,7 +160,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_GSCL_WB		464
 #define CLK_GSCL0		465
 #define CLK_GSCL1		466
-#define CLK_CLK_3AA		467
+#define CLK_FIMC_3AA		467
 #define CLK_ACLK266_G2D		470
 #define CLK_SSS			471
 #define CLK_SLIM_SSS		472
@@ -173,6 +173,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_SMMU_FIMCL1		493
 #define CLK_SMMU_FIMCL3		494
 #define CLK_FIMC_LITE3		495
+#define CLK_FIMC_LITE0		496
+#define CLK_FIMC_LITE1		497
 #define CLK_ACLK_G3D		500
 #define CLK_G3D			501
 #define CLK_SMMU_MIXER		502
