286987a36ebbd345876fb660b50a244fcd681ca9
Arnd Bergmann <arnd@arndb.de>
2014-05-09 13:35:39 +0200

    crypto: s5p-sss - fix multiplatform build
    As we are preparing to enable multiplatform support on EXYNOS,
    we can no longer include mach/*.h or plat/*.h headers from device
    drivers.
    
    The s5p-sss driver was just enabled for EXYNOS when it used to
    be used only on s5pv210, and it includes two samsung platform
    specific header files for historic reasons. Fortunately, it no
    longer actually needs them, so we can remove the #includes and
    avoid the problem
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Naveen Krishna Chatradhi <ch.naveen@samsung.com>
    Cc: Tomasz Figa <t.figa@samsung.com>
    Cc: <linux-samsung-soc@vger.kernel.org>
    Cc: Vladimir Zapolskiy <vz@mleia.com>
    Reviewed-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/s5p-sss.c
+++ drivers/crypto/s5p-sss.c
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/aes.h>
 #include <crypto/ctr.h>
 
-#include <plat/cpu.h>
-#include <mach/dma.h>
-
 #define _SBF(s, v)                      ((v) << (s))
 #define _BIT(b)                         _SBF(b, 1)
 
