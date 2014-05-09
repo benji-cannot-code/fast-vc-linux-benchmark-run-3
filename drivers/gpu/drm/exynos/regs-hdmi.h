2602c38f0c02807abe26af518d1dfa04ec4ffe72
Rahul Sharma <Rahul.Sharma@samsung.com>
2014-05-09 15:34:18 +0900

    drm/exynos: add support for apb mapped phys in hdmi driver
    Previous SoCs have hdmi phys which are accessible through
    dedicated i2c lines. Newer SoCs have Apb mapped hdmi phys.
    Hdmi driver is modified to support apb mapped phys.
    
    Signed-off-by: Rahul Sharma <Rahul.Sharma@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/regs-hdmi.h
+++ drivers/gpu/drm/exynos/regs-hdmi.h
@@ -579,4 +579,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HDMI_TG_VACT_ST4_H		HDMI_TG_BASE(0x0074)
 #define HDMI_TG_3D			HDMI_TG_BASE(0x00F0)
 
+/* HDMI PHY Registers Offsets*/
+#define HDMIPHY_MODE_SET_DONE		(0x7C >> 2)
+
+/* HDMI PHY Values */
+#define HDMI_PHY_DISABLE_MODE_SET	0x80
+#define HDMI_PHY_ENABLE_MODE_SET	0x00
+
 #endif /* SAMSUNG_REGS_HDMI_H */
