18e987e6787f8d1788fbbc28dcd75df2ff9dbd5c
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-05-09 15:03:56 +0200

    staging: rtl8723au: Remove ugly vmalloc() wrappers
    Finally with the last user converted, get rid of ugly vmalloc wrappers
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/os_dep/osdep_service.c
+++ drivers/staging/rtl8723au/os_dep/osdep_service.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <osdep_service.h>
 #include <drv_types.h>
 #include <recv_osdep.h>
-#include <linux/vmalloc.h>
 
 /*
 * Translate the OS dependent @param error_code to OS independent RTW_STATUS_CODE23a
@@ -31,29 +30,6 @@ inline int RTW_STATUS_CODE23a(int error_code)
 	if (error_code >= 0)
 		return _SUCCESS;
 	return _FAIL;
-}
-
-inline u8 *_rtw_vmalloc(u32 sz)
-{
-	u8	*pbuf;
-	pbuf = vmalloc(sz);
-
-	return pbuf;
-}
-
-inline u8 *_rtw_zvmalloc(u32 sz)
-{
-	u8	*pbuf;
-	pbuf = _rtw_vmalloc(sz);
-	if (pbuf != NULL)
-		memset(pbuf, 0, sz);
-
-	return pbuf;
-}
-
-inline void _rtw_vmfree(u8 *pbuf, u32 sz)
-{
-	vfree(pbuf);
 }
 
 void _rtw_init_queue23a(struct rtw_queue *pqueue)
