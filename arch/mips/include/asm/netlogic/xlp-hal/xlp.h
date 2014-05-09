b634425f78cac2eaa354ef8dfe3b00b7e3e42111
Jayachandran C <jchandra@broadcom.com>
2014-05-09 16:35:34 +0530

    MIPS: Netlogic: IRQ mapping for some more SoC blocks
    Add IRQ to IRT (PIC interupt table index) mapping for SATA, GPIO, NAND
    and SPI interfaces on the XLP SoC. Fix offsets for few blocks and add
    device IDs for a few blocks.
    
    Signed-off-by: Jayachandran C <jchandra@broadcom.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/6911/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/netlogic/xlp-hal/xlp.h
+++ arch/mips/include/asm/netlogic/xlp-hal/xlp.h
@@ -59,6 +59,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PIC_I2C_1_IRQ			31
 #define PIC_I2C_2_IRQ			32
 #define PIC_I2C_3_IRQ			33
+#define PIC_SPI_IRQ			34
+#define PIC_NAND_IRQ			37
+#define PIC_SATA_IRQ			38
+#define PIC_GPIO_IRQ			39
 
 #define PIC_PCIE_LINK_MSI_IRQ_BASE	44	/* 44 - 47 MSI IRQ */
 #define PIC_PCIE_LINK_MSI_IRQ(i)	(44 + (i))
