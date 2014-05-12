b24141446e9f5cd4ef8ac34c91ff888f0ba2e8a2
Namhyung Kim <namhyung@kernel.org>
2014-05-12 10:06:18 +0900

    perf tests: Factor out print_hists_*()
    Those print helper functions can be reused by later hist test cases so
    factor them out to a common location.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Link: http://lkml.kernel.org/r/1400480762-22852-20-git-send-email-namhyung@kernel.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/tests/hists_common.h
+++ tools/perf/tests/hists_common.h
@@ -42,4 +42,7 @@ struct machines;
  */
 struct machine *setup_fake_machine(struct machines *machines);
 
+void print_hists_in(struct hists *hists);
+void print_hists_out(struct hists *hists);
+
 #endif /* __PERF_TESTS__HISTS_COMMON_H__ */
