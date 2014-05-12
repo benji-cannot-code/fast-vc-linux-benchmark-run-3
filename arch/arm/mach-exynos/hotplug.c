11df8f5da27af90a033c899e5984973b2a3ac605
Sachin Kamat <sachin.kamat@linaro.org>
2014-05-13 07:12:00 +0900

    ARM: EXYNOS: Remove unnecessary inclusion of cpu.h
    Exynos specific macros and declarations have been moved to
    mach-exynos. Inclusion of plat/cpu.h is no more necessary.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Reviewed-by: Tomasz Figa <t.figa@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/hotplug.c
+++ arch/arm/mach-exynos/hotplug.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cp15.h>
 #include <asm/smp_plat.h>
 
-#include <plat/cpu.h>
-
 #include "common.h"
 #include "regs-pmu.h"
 
