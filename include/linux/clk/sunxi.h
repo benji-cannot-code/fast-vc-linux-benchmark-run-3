3b93a76dd116122dae6abbc7ecde80a5ff03b079
Hans de Goede <hdegoede@redhat.com>
2014-05-12 14:04:47 +0200

    clk: sunxi: Fixup clk_sunxi_mmc_phase_control to take a clk rather then a hw_clk
    __clk_get_hw is supposed to be used by clk providers, not clk consumers.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Mike Turquette <mturquette@linaro.org>

--- include/linux/clk/sunxi.h
+++ include/linux/clk/sunxi.h
@@ -18,6 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk.h>
 
-void clk_sunxi_mmc_phase_control(struct clk_hw *hw, u8 sample, u8 output);
+void clk_sunxi_mmc_phase_control(struct clk *clk, u8 sample, u8 output);
 
 #endif
