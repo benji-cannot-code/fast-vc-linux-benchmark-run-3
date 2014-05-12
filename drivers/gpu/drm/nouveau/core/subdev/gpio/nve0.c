1a2417e8f6e2ee5840a827e738265e1f63c8a4b5
Ben Skeggs <bskeggs@redhat.com>
2014-05-12 14:12:32 +1000

    drm/gk104/gpio: fix incorrect interrupt register usage
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/gpio/nve0.c
+++ drivers/gpu/drm/nouveau/core/subdev/gpio/nve0.c
@@ -45,7 +45,7 @@ nve0_gpio_intr(struct nouveau_subdev *subdev)
 	}
 
 	nv_wr32(priv, 0xdc00, intr0);
-	nv_wr32(priv, 0xdc88, intr1);
+	nv_wr32(priv, 0xdc80, intr1);
 }
 
 void
@@ -53,8 +53,8 @@ nve0_gpio_intr_enable(struct nouveau_event *event, int line)
 {
 	const u32 addr = line < 16 ? 0xdc00 : 0xdc80;
 	const u32 mask = 0x00010001 << (line & 0xf);
-	nv_wr32(event->priv, addr + 0x08, mask);
-	nv_mask(event->priv, addr + 0x00, mask, mask);
+	nv_wr32(event->priv, addr + 0x00, mask);
+	nv_mask(event->priv, addr + 0x08, mask, mask);
 }
 
 void
@@ -62,8 +62,8 @@ nve0_gpio_intr_disable(struct nouveau_event *event, int line)
 {
 	const u32 addr = line < 16 ? 0xdc00 : 0xdc80;
 	const u32 mask = 0x00010001 << (line & 0xf);
-	nv_wr32(event->priv, addr + 0x08, mask);
-	nv_mask(event->priv, addr + 0x00, mask, 0x00000000);
+	nv_mask(event->priv, addr + 0x08, mask, 0x00000000);
+	nv_wr32(event->priv, addr + 0x00, mask);
 }
 
 int
