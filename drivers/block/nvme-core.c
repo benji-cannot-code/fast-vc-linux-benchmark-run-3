6fed7f0faad9859f0197f7de2960da230d74d483
Keith Busch <keith.busch@intel.com>
2014-05-13 11:42:02 -0600

    NVMe: Make admin timeout a module parameter
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    [made admin_timeout static]
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -45,11 +45,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <trace/events/block.h>
 
-#define NVME_Q_DEPTH 1024
+#define NVME_Q_DEPTH		1024
 #define SQ_SIZE(depth)		(depth * sizeof(struct nvme_command))
 #define CQ_SIZE(depth)		(depth * sizeof(struct nvme_completion))
-#define ADMIN_TIMEOUT	(60 * HZ)
-#define IOD_TIMEOUT	(retry_time * HZ)
+#define ADMIN_TIMEOUT		(admin_timeout * HZ)
+#define IOD_TIMEOUT		(retry_time * HZ)
+
+static unsigned char admin_timeout = 60;
+module_param(admin_timeout, byte, 0644);
+MODULE_PARM_DESC(admin_timeout, "timeout in seconds for admin commands");
 
 unsigned char io_timeout = 30;
 module_param(io_timeout, byte, 0644);
