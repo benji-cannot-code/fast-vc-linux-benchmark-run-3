9758db3d8b6138ee78fa5ced2552ec4d4d6a57d0
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-05-13 23:02:14 -0300

    saa7134-alsa: include vmalloc.h
    Changeset 15e64f0dde77 broke compilation on several archs, as it
    forgot to include vmalloc.h.
    
    drivers/media/pci/saa7134/saa7134-alsa.c: In function ‘saa7134_alsa_dma_init’:
      CC [M]  drivers/media/rc/keymaps/rc-behold-columbus.o
    drivers/media/pci/saa7134/saa7134-alsa.c:283:2: error: implicit declaration of function ‘vmalloc_32’ [-Werror=implicit-function-declaration]
      dma->vaddr = vmalloc_32(nr_pages << PAGE_SHIFT);
      ^
    drivers/media/pci/saa7134/saa7134-alsa.c:283:13: warning: assignment makes pointer from integer without a cast [enabled by default]
      dma->vaddr = vmalloc_32(nr_pages << PAGE_SHIFT);
                 ^
    drivers/media/pci/saa7134/saa7134-alsa.c:296:2: error: implicit declaration of function ‘vzalloc’ [-Werror=implicit-function-declaration]
      dma->sglist = vzalloc(dma->nr_pages * sizeof(*dma->sglist));
      ^
    drivers/media/pci/saa7134/saa7134-alsa.c:296:14: warning: assignment makes pointer from integer without a cast [enabled by default]
      dma->sglist = vzalloc(dma->nr_pages * sizeof(*dma->sglist));
                  ^
    drivers/media/pci/saa7134/saa7134-alsa.c:310:2: error: implicit declaration of function ‘vfree’ [-Werror=implicit-function-declaration]
      vfree(dma->sglist);
    
    Add it to avoid such breakages.
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/pci/saa7134/saa7134-alsa.c
+++ drivers/media/pci/saa7134/saa7134-alsa.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm_params.h>
 #include <sound/initval.h>
 #include <linux/interrupt.h>
+#include <linux/vmalloc.h>
 
 #include "saa7134.h"
 #include "saa7134-reg.h"
