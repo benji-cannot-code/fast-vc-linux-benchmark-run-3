ddd36d2fcfc5cdc941096ee96b35857aa5bb22ab
Janusz Dziedzic <janusz.dziedzic@tieto.com>
2014-05-14 13:25:04 +0200

    cfg80211: fix start_radar_detection issue
    After patch:
    cfg80211/mac80211: refactor cfg80211_chandef_dfs_required()
    
    start_radar_detection always fail with -EINVAL.
    
    Acked-by: Luciano Coelho <luciano.coelho@intel.com>
    Signed-off-by: Janusz Dziedzic <janusz.dziedzic@tieto.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/chan.c
+++ net/wireless/chan.c
@@ -341,6 +341,7 @@ int cfg80211_chandef_dfs_required(struct wiphy *wiphy,
 	case NL80211_IFTYPE_AP:
 	case NL80211_IFTYPE_P2P_GO:
 	case NL80211_IFTYPE_MESH_POINT:
+	case NL80211_IFTYPE_UNSPECIFIED:
 		width = cfg80211_chandef_get_width(chandef);
 		if (width < 0)
 			return -EINVAL;
@@ -371,7 +372,6 @@ int cfg80211_chandef_dfs_required(struct wiphy *wiphy,
 	case NL80211_IFTYPE_AP_VLAN:
 	case NL80211_IFTYPE_WDS:
 	case NL80211_IFTYPE_P2P_DEVICE:
-	case NL80211_IFTYPE_UNSPECIFIED:
 		break;
 	case NUM_NL80211_IFTYPES:
 		WARN_ON(1);
