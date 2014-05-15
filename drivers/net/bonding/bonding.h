296c745786de963209050fc7d9da6d30bd793dc1
Veaceslav Falico <vfalico@gmail.com>
2014-05-15 21:39:55 +0200

    bonding: create a macro for bond mode and use it
    CC: Jay Vosburgh <j.vosburgh@gmail.com>
    CC: Andy Gospodarek <andy@greyhouse.net>
    Signed-off-by: Veaceslav Falico <vfalico@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -71,6 +71,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	set_fs(fs);			\
 	res; })
 
+#define BOND_MODE(bond) ((bond)->params.mode)
+
 /* slave list primitives */
 #define bond_slave_list(bond) (&(bond)->dev->adj_list.lower)
 
@@ -272,14 +274,14 @@ static inline struct bonding *bond_get_bond_by_slave(struct slave *slave)
 
 static inline bool bond_should_override_tx_queue(struct bonding *bond)
 {
-	return bond->params.mode == BOND_MODE_ACTIVEBACKUP ||
-	       bond->params.mode == BOND_MODE_ROUNDROBIN;
+	return BOND_MODE(bond) == BOND_MODE_ACTIVEBACKUP ||
+	       BOND_MODE(bond) == BOND_MODE_ROUNDROBIN;
 }
 
 static inline bool bond_is_lb(const struct bonding *bond)
 {
-	return bond->params.mode == BOND_MODE_TLB ||
-	       bond->params.mode == BOND_MODE_ALB;
+	return BOND_MODE(bond) == BOND_MODE_TLB ||
+	       BOND_MODE(bond) == BOND_MODE_ALB;
 }
 
 static inline bool bond_mode_uses_arp(int mode)
@@ -296,7 +298,7 @@ static inline bool bond_mode_uses_primary(int mode)
 
 static inline bool bond_uses_primary(struct bonding *bond)
 {
-	return bond_mode_uses_primary(bond->params.mode);
+	return bond_mode_uses_primary(BOND_MODE(bond));
 }
 
 static inline void bond_set_active_slave(struct slave *slave)
