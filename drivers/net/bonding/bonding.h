e7cb2c6be0473216c0e44e09977f351edc7fb2e6
Veaceslav Falico <vfalico@gmail.com>
2014-05-15 21:39:59 +0200

    bonding: replace SLAVE_IS_OK() with bond_slave_can_tx()
    They're verifying the same thing (except of IFF_UP, which is implied for
    netif_running(), which is also a prerequisite).
    
    CC: Jay Vosburgh <j.vosburgh@gmail.com>
    CC: Andy Gospodarek <andy@greyhouse.net>
    Signed-off-by: Veaceslav Falico <vfalico@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -42,15 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BOND_DEFAULT_MIIMON	100
 
 /*
- * Checks whether slave is ready for transmit.
- */
-#define SLAVE_IS_OK(slave)			        \
-		    (((slave)->dev->flags & IFF_UP)  && \
-		     netif_running((slave)->dev)     && \
-		     ((slave)->link == BOND_LINK_UP) && \
-		     bond_is_active_slave(slave))
-
-/*
  * Less bad way to call ioctl from within the kernel; this needs to be
  * done some other way to get the call out of interrupt context.
  * Needs "ioctl" variable to be supplied by calling context.
