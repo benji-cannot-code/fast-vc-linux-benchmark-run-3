7887e9697bba8e0fc59f4db36f273785733a4791
Ben Skeggs <bskeggs@redhat.com>
2014-05-15 21:50:07 +1000

    drm/nouveau/disp/dp: support aux read interval during link training
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/disp/dport.c
+++ drivers/gpu/drm/nouveau/core/engine/disp/dport.c
@@ -49,7 +49,7 @@ struct dp_state {
 	u8 version;
 	struct nouveau_i2c_port *aux;
 	int head;
-	u8  dpcd[4];
+	u8  dpcd[16];
 	int link_nr;
 	u32 link_bw;
 	u8  stat[6];
@@ -150,7 +150,10 @@ dp_link_train_update(struct dp_state *dp, u32 delay)
 {
 	int ret;
 
-	udelay(delay);
+	if (dp->dpcd[DPCD_RC0E_AUX_RD_INTERVAL])
+		mdelay(dp->dpcd[DPCD_RC0E_AUX_RD_INTERVAL] * 4);
+	else
+		udelay(delay);
 
 	ret = nv_rdaux(dp->aux, DPCD_LS02, dp->stat, 6);
 	if (ret)
