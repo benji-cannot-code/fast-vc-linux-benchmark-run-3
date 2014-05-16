4127da8943b8e28f67fcdab347d4b6454633f7a0
Michal Simek <michal.simek@xilinx.com>
2014-05-16 13:36:54 +0200

    microblaze: Clean device dma_ops structure
    No code is setting up dma_operation for device.
    Use dma_direct_ops for all cases.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/asm/device.h
+++ arch/microblaze/include/asm/device.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct device_node;
 
 struct dev_archdata {
-	/* DMA operations on that device */
-	struct dma_map_ops	*dma_ops;
 };
 
 struct pdev_archdata {
