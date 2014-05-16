6f9db90d3d8501b0858194eac2e913d2cd65eb4d
Michal Simek <michal.simek@xilinx.com>
2014-05-16 13:08:44 +0200

    microblaze: Remove device_to_mask
    Completely unused function - remove it.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/asm/dma-mapping.h
+++ arch/microblaze/include/asm/dma-mapping.h
@@ -36,14 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __dma_alloc_coherent(dev, gfp, size, handle)	NULL
 #define __dma_free_coherent(size, addr)		((void)0)
 
-static inline unsigned long device_to_mask(struct device *dev)
-{
-	if (dev->dma_mask && *dev->dma_mask)
-		return *dev->dma_mask;
-	/* Assume devices without mask can take 32 bit addresses */
-	return 0xfffffffful;
-}
-
 /*
  * Available generic sets of operations
  */
