d977dca3f284d922bb3adafcb52e68b3fa7529f1
Sam Ravnborg <sam@ravnborg.org>
2014-05-16 23:26:05 +0200

    sparc: fix sparse warnings in smp_32.c + smp_64.c
    Fix following warnings:
    smp_32.c:177:5: warning: symbol 'setup_profiling_timer' was not declared. Should it be static?
    smp_64.c:1202:5: warning: symbol 'setup_profiling_timer' was not declared. Should it be static?
    smp_64.c:989:6: warning: symbol 'kgdb_roundup_cpus' was not declared. Should it be static?
    
    Add prototype to include/linux/profile.h of setup_profiling_timer
    Add missing include to smp_64.c
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/smp_32.c
+++ arch/sparc/kernel/smp_32.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/cache.h>
 #include <linux/delay.h>
+#include <linux/profile.h>
 #include <linux/cpu.h>
 
 #include <asm/ptrace.h>
