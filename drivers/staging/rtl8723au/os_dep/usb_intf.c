df36fc0ee673c426bbad8dc8a22050239e67c73b
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-05-16 10:04:26 +0200

    staging: rtl8723au: usb_vendor_req.h isn't used - remove it
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/os_dep/usb_intf.c
+++ drivers/staging/rtl8723au/os_dep/usb_intf.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <hal_intf.h>
 #include <rtw_version.h>
 #include <osdep_intf.h>
-#include <usb_vendor_req.h>
 #include <usb_ops.h>
 #include <rtl8723a_hal.h>
 
