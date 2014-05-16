8648ee2faee005ebe0ee750b70fbf5c66570e02c
Bjørn Mork <bjorn@mork.no>
2014-05-16 21:48:26 +0200

    net: cdc_ncm: use sane defaults for rx/tx buffers
    Lots of devices request much larger buffers than reasonable. This
    cause real problems for users of hosts with limited resources.
    
    Reducing the default buffer size to 16kB for such devices is
    a reasonable trade-off between allowing them to aggregate traffic
    and avoiding memory exhaustion on resource restrained hosts.
    
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/usb/cdc_ncm.h
+++ include/linux/usb/cdc_ncm.h
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	CDC_NCM_NTB_MAX_SIZE_TX			32768	/* bytes */
 #define	CDC_NCM_NTB_MAX_SIZE_RX			32768	/* bytes */
 
+/* Initial NTB length */
+#define	CDC_NCM_NTB_DEF_SIZE_TX			16384	/* bytes */
+#define	CDC_NCM_NTB_DEF_SIZE_RX			16384	/* bytes */
+
 /* Minimum value for MaxDatagramSize, ch. 6.2.9 */
 #define	CDC_NCM_MIN_DATAGRAM_SIZE		1514	/* bytes */
 
