2b05c1d6e7c84310dd87c49b9a3ba4822fbb2525
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-05-16 19:40:22 +0300

    drm/i915: Check for FIFO underruns at the end of modeset on gmch
    FIFO underruns don't generate interrupts on gmch platforms, so
    if we want to know whether a modeset triggered FIFO underruns we
    need to explicitly check for them.
    
    As a modeset on one pipe could cause underruns on other pipes,
    check for underruns on all pipes.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Thomas Wood <thomas.wood@intel.com>
    [danvet: Fix up merge error, kudos to Ville for noticing it.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -4626,6 +4626,9 @@ static void valleyview_crtc_enable(struct drm_crtc *crtc)
 	intel_crtc_enable_planes(crtc);
 
 	drm_crtc_vblank_on(crtc);
+
+	/* Underruns don't raise interrupts, so check manually. */
+	i9xx_check_fifo_underruns(dev);
 }
 
 static void i9xx_set_pll_dividers(struct intel_crtc *crtc)
@@ -4705,6 +4708,9 @@ static void i9xx_crtc_enable(struct drm_crtc *crtc)
 	intel_crtc_enable_planes(crtc);
 
 	drm_crtc_vblank_on(crtc);
+
+	/* Underruns don't raise interrupts, so check manually. */
+	i9xx_check_fifo_underruns(dev);
 }
 
 static void i9xx_pfit_disable(struct intel_crtc *crtc)
