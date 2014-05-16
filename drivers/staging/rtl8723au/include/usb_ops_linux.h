94c1a8da69ace3f26c1431125383a344f5f77e16
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-05-16 10:04:32 +0200

    staging: rtl8723au: Remove unused usb_read_mem/usb_write_mem stubs
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/include/usb_ops_linux.h
+++ drivers/staging/rtl8723au/include/usb_ops_linux.h
@@ -27,16 +27,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define _usbctrl_vendorreq_async_callback(urb, regs)		\
 	_usbctrl_vendorreq_async_callback(urb)
-#define usb_write_mem23a_complete(purb, regs)	usb_write_mem23a_complete(purb)
 #define usb_write_port23a_complete(purb, regs)	usb_write_port23a_complete(purb)
 #define usb_read_port_complete(purb, regs)	usb_read_port_complete(purb)
 #define usb_read_interrupt_complete(purb, regs)			\
 	usb_read_interrupt_complete(purb)
 
 unsigned int ffaddr2pipehdl23a(struct dvobj_priv *pdvobj, u32 addr);
-
-void usb_read_mem23a(struct rtw_adapter *adapter, u32 addr, u32 cnt, u8 *rmem);
-void usb_write_mem23a(struct rtw_adapter *adapter, u32 addr, u32 cnt, u8 *wmem);
 
 void usb_read_port_cancel23a(struct rtw_adapter *adapter);
 
