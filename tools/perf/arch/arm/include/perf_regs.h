ebf0c4f9d64ffded6f466781eb2b5d77476bcf15
Jean Pihet <jean.pihet@linaro.org>
2014-05-16 10:41:10 +0200

    perf tests: Introduce perf_regs_load function on ARM
    Introducing perf_regs_load function, which is going
    to be used for dwarf unwind test in following patches.
    
    It takes single argument as a pointer to the regs dump
    buffer and populates it with current registers values.
    
    Signed-off-by: Jean Pihet <jean.pihet@linaro.org>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Corey Ashford <cjashfor@linux.vnet.ibm.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1400229672-16104-2-git-send-email-jean.pihet@linaro.org
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tools/perf/arch/arm/include/perf_regs.h
+++ tools/perf/arch/arm/include/perf_regs.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/perf_regs.h>
 
+void perf_regs_load(u64 *regs);
+
 #define PERF_REGS_MASK	((1ULL << PERF_REG_ARM_MAX) - 1)
 #define PERF_REG_IP	PERF_REG_ARM_PC
 #define PERF_REG_SP	PERF_REG_ARM_SP
