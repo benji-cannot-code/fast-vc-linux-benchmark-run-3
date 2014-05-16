2412e1b6c21eddca45a420176e49a11fb2df8cac
Phoebe Buckheister <phoebe.buckheister@itwm.fraunhofer.de>
2014-05-16 17:46:40 +0200

    mac802154: integrate llsec with wpan devices
    Signed-off-by: Phoebe Buckheister <phoebe.buckheister@itwm.fraunhofer.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mac802154/mac802154.h
+++ net/mac802154/mac802154.h
@@ -24,8 +24,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef MAC802154_H
 #define MAC802154_H
 
+#include <linux/mutex.h>
 #include <net/mac802154.h>
 #include <net/ieee802154_netdev.h>
+
+#include "llsec.h"
 
 /* mac802154 device private data */
 struct mac802154_priv {
@@ -92,6 +95,13 @@ struct mac802154_sub_if_data {
 	u8 bsn;
 	/* MAC DSN field */
 	u8 dsn;
+
+	/* protects sec from concurrent access by netlink. access by
+	 * encrypt/decrypt/header_create safe without additional protection.
+	 */
+	struct mutex sec_mtx;
+
+	struct mac802154_llsec sec;
 };
 
 #define mac802154_to_priv(_hw)	container_of(_hw, struct mac802154_priv, hw)
