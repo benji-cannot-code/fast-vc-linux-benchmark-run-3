5fa8da7afb900e113f8ef1a2f91e2242d38aed15
Ben Skeggs <bskeggs@redhat.com>
2014-05-17 11:19:54 +1000

    drm/nouveau/disp/dp: create subclass for dp outputs
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/disp/nv50.h
+++ drivers/gpu/drm/nouveau/core/engine/disp/nv50.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dport.h"
 #include "priv.h"
 #include "outp.h"
+#include "outpdp.h"
 
 struct nv50_disp_impl {
 	struct nouveau_disp_impl base;
@@ -200,5 +201,14 @@ void nvd0_disp_intr(struct nouveau_subdev *);
 
 extern const struct nv50_disp_mthd_chan nve0_disp_mast_mthd_chan;
 extern const struct nv50_disp_mthd_chan nve0_disp_ovly_mthd_chan;
+
+extern struct nvkm_output_dp_impl nv50_pior_dp_impl;
+extern struct nouveau_oclass *nv50_disp_outp_sclass[];
+
+extern struct nvkm_output_dp_impl nv94_sor_dp_impl;
+extern struct nouveau_oclass *nv94_disp_outp_sclass[];
+
+extern struct nvkm_output_dp_impl nvd0_sor_dp_impl;
+extern struct nouveau_oclass *nvd0_disp_outp_sclass[];
 
 #endif
--- drivers/gpu/drm/nouveau/core/engine/disp/sornv94.c
+++ drivers/gpu/drm/nouveau/core/engine/disp/sornv94.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <subdev/bios/init.h>
 
 #include "nv50.h"
+#include "outpdp.h"
 
 static inline u32
 nv94_sor_soff(struct dcb_output *outp)
@@ -129,4 +130,15 @@ nv94_sor_dp_func = {
 	.pattern = nv94_sor_dp_pattern,
 	.lnk_ctl = nv94_sor_dp_lnk_ctl,
 	.drv_ctl = nv94_sor_dp_drv_ctl,
+};
+
+struct nvkm_output_dp_impl
+nv94_sor_dp_impl = {
+	.base.base.handle = DCB_OUTPUT_DP,
+	.base.base.ofuncs = &(struct nouveau_ofuncs) {
+		.ctor = _nvkm_output_dp_ctor,
+		.dtor = _nvkm_output_dp_dtor,
+		.init = _nvkm_output_dp_init,
+		.fini = _nvkm_output_dp_fini,
+	},
 };
