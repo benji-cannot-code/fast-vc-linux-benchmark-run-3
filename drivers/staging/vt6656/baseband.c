9e6ca5e59d8db7d55e8d286d82ee1550c7ef21d1
Malcolm Priestley <tvboxspy@gmail.com>
2014-05-17 09:50:28 +0100

    staging: vt6656: srom.h move all macros to device.h
    Remove srom.h
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/baseband.c
+++ drivers/staging/vt6656/baseband.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mac.h"
 #include "baseband.h"
 #include "rf.h"
-#include "srom.h"
 #include "control.h"
 #include "datarate.h"
 #include "rndis.h"
