7ae187601712432520d8e5d1faadb08af024de99
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2014-05-19 22:42:35 +0200

    ARM: OMAP3: Beagle: initialize all the struct pwm_lookup members
    The PWM core can retrieve the period from the PWM lookup table, so the
    struct led_pwm.pwm_period_ns member can be removed.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- arch/arm/mach-omap2/board-omap3beagle.c
+++ arch/arm/mach-omap2/board-omap3beagle.c
@@ -62,7 +62,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct pwm_lookup pwm_lookup[] = {
 	/* LEDB -> PMU_STAT */
-	PWM_LOOKUP("twl-pwmled", 1, "leds_pwm", "beagleboard::pmu_stat"),
+	{
+		.provider = "twl-pwmled",
+		.index = 1,
+		.dev_id = "leds_pwm",
+		.con_id = "beagleboard::pmu_stat",
+		.period = 7812500,
+		.polarity = PWM_POLARITY_NORMAL,
+	},
 };
 
 static struct led_pwm pwm_leds[] = {
