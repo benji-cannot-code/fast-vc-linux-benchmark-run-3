f3c731eade703da12c8093107ff5ca0fb74045cd
Rajkumar Manoharan <rmanohar@qti.qualcomm.com>
2014-05-19 11:44:37 +0530

    ath9k_hw: Abort transmission for sleeping station
    The data transmission to the power save station should be aborted
    immediately, whenever the station informs sleep state. Right now
    the frames queued into into hardware are being transmitted until
    the hardware detects the power save station based excessive retries
    of the data frames due to unacknowlegdement. Then remaining frames
    are returned with filetered status and might be retried later by
    driver or mac80211.
    
    Per WFA certification testing, AP should not send out more than two
    frames after processing nullfunc with PM bit set from associated
    station. To speed up tx filtering, the pending frames in hardware
    queues for given station will be aborted immediately via tx filter
    registers. This transmit filters can be ignored if the descriptor
    is having invalid destination index or clear destination mask set.
    
    Signed-off-by: Rajkumar Manoharan <rmanohar@qti.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -1507,8 +1507,12 @@ static void ath9k_sta_notify(struct ieee80211_hw *hw,
 	case STA_NOTIFY_SLEEP:
 		an->sleeping = true;
 		ath_tx_aggr_sleep(sta, sc, an);
+		if (an->ps_key > 0)
+			ath9k_hw_set_tx_filter(sc->sc_ah, an->ps_key, true);
 		break;
 	case STA_NOTIFY_AWAKE:
+		if (an->ps_key > 0)
+			ath9k_hw_set_tx_filter(sc->sc_ah, an->ps_key, false);
 		an->sleeping = false;
 		ath_tx_aggr_wakeup(sc, an);
 		break;
@@ -1594,6 +1598,8 @@ static int ath9k_set_key(struct ieee80211_hw *hw,
 			ath9k_del_ps_key(sc, vif, sta);
 
 		ret = ath_key_config(common, vif, sta, key);
+		if (sta && (ret > 0))
+			((struct ath_node *)sta->drv_priv)->ps_key = ret;
 		if (ret >= 0) {
 			key->hw_key_idx = ret;
 			/* push IV and Michael MIC generation to stack */
@@ -1608,6 +1614,8 @@ static int ath9k_set_key(struct ieee80211_hw *hw,
 		break;
 	case DISABLE_KEY:
 		ath_key_delete(common, key);
+		if (sta)
+			((struct ath_node *)sta->drv_priv)->ps_key = 0;
 		break;
 	default:
 		ret = -EINVAL;
