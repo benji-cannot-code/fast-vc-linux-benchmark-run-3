88b6167fe32afe3cd7e56efb7fc13d282af78209
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2014-05-20 14:18:53 -0300

    [media] omap3isp: ccdc: Add support for BT.656 YUV format at the CCDC input
    Query the CCDC input media bus type from the subdev connected to the
    CCDC sink pad and configure the CCDC accordingly to support BT.656
    synchronization.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Tested-by: Enrico Butera <ebutera@users.sourceforge.net>
    Acked-by: Sakari Ailus <sakari.ailus@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/omap3isp/ispccdc.h
+++ drivers/media/platform/omap3isp/ispccdc.h
@@ -114,6 +114,7 @@ struct ispccdc_lsc {
  * @lsc: Lens shading compensation configuration
  * @update: Bitmask of controls to update during the next interrupt
  * @shadow_update: Controls update in progress by userspace
+ * @bt656: Whether the input interface uses BT.656 synchronization
  * @underrun: A buffer underrun occurred and a new buffer has been queued
  * @state: Streaming state
  * @lock: Serializes shadow_update with interrupt handler
@@ -142,6 +143,7 @@ struct isp_ccdc_device {
 	unsigned int update;
 	unsigned int shadow_update;
 
+	bool bt656;
 	unsigned int underrun:1;
 	enum isp_pipeline_stream_state state;
 	spinlock_t lock;
