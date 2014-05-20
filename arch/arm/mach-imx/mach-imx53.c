130061a95a9d79c20756cbb4a33052f4c81e06be
Shawn Guo <shawn.guo@freescale.com>
2014-05-20 15:54:58 +0800

    ARM: imx5: remove mx51.h and mx53.h
    Now all the macros in mx51.h and mx53.h are used nowhere, so remove
    them.
    
    Signed-off-by: Shawn Guo <shawn.guo@freescale.com>

--- arch/arm/mach-imx/mach-imx53.c
+++ arch/arm/mach-imx/mach-imx53.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "common.h"
 #include "hardware.h"
-#include "mx53.h"
 
 static void __init imx53_init_early(void)
 {
