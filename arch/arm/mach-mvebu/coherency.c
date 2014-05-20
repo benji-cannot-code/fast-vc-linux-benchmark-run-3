2a09c26363448a81bdfd6221313262e63f94b348
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-05-20 17:13:03 +0200

    ARM: mvebu: coherency: fix registration of PCI bus notifier when !PCI
    Commit b0063aad5dd8 ("ARM: mvebu: use hardware I/O coherency also for
    PCI devices") added a reference to the pci_bus_type variable, but this
    variable is only available when CONFIG_PCI is enabled. Therefore,
    there is now a build failure in !CONFIG_PCI situations.
    
    This commit fixes that by enclosing the entire initcall into a
    IS_ENABLED(CONFIG_PCI) condition.
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Link: https://lkml.kernel.org/r/1400598783-706-1-git-send-email-thomas.petazzoni@free-electrons.com
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-mvebu/coherency.c
+++ arch/arm/mach-mvebu/coherency.c
@@ -423,6 +423,7 @@ static int __init coherency_late_init(void)
 
 postcore_initcall(coherency_late_init);
 
+#if IS_ENABLED(CONFIG_PCI)
 static int __init coherency_pci_init(void)
 {
 	if (coherency_available())
@@ -432,3 +433,4 @@ static int __init coherency_pci_init(void)
 }
 
 arch_initcall(coherency_pci_init);
+#endif
