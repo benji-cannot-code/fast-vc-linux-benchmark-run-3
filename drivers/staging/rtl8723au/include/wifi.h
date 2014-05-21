a5baa125674f99f54dccd7010311441a8d64a2cb
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-05-21 09:37:45 +0200

    staging: rtl8723au: wifi.h: Remove unused SetRetry() + SetAMsdu() macros
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/include/wifi.h
+++ drivers/staging/rtl8723au/include/wifi.h
@@ -88,9 +88,6 @@ enum WIFI_REG_DOMAIN {
 #define SetFrDs(pbuf)	\
 	(*(__le16 *)(pbuf) |= cpu_to_le16(IEEE80211_FCTL_FROMDS))
 
-#define SetRetry(pbuf)	\
-	(*(__le16 *)(pbuf) |= cpu_to_le16(IEEE80211_FCTL_RETRY))
-
 #define SetPwrMgt(pbuf)	\
 	(*(__le16 *)(pbuf) |= cpu_to_le16(IEEE80211_FCTL_PM))
 
@@ -138,9 +135,6 @@ enum WIFI_REG_DOMAIN {
 
 #define SetAckpolicy(pbuf, ack)	\
 	(*(__le16 *)(pbuf) |= cpu_to_le16((ack & 3) << 5))
-
-#define SetAMsdu(pbuf, amsdu)	\
-	(*(__le16 *)(pbuf) |= cpu_to_le16((amsdu & 1) << 7))
 
 #define _ASOCREQ_IE_OFFSET_		4	/*  excluding wlan_hdr */
 #define	_ASOCRSP_IE_OFFSET_		6
