90c2f96c06a02517dede6bf44902eb27a7d8956f
Mark Rutland <mark.rutland@arm.com>
2014-05-21 18:06:13 +0100

    arm: perf: krait: stop using singleton PMU
    Currently the krait_pmu_{enable,disable}_event functions use the global
    cpu_pmu variable while all the other pmu enable/disable functions
    derive this from the event argument.
    
    This patch brings the Krait functions into line with the rest of the PMU
    backends by deriving the address of the pmu from the event argument.
    
    Acked-by: Will Deacon <will.deacon@arm.com>
    Acked-by: Stephen Boyd <sboyd@codeaurora.org>
    Tested-by: Christopher Covington <cov@codeaurora.org>
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm/kernel/perf_event_v7.c
+++ arch/arm/kernel/perf_event_v7.c
@@ -1287,6 +1287,7 @@ static void krait_pmu_disable_event(struct perf_event *event)
 	unsigned long flags;
 	struct hw_perf_event *hwc = &event->hw;
 	int idx = hwc->idx;
+	struct arm_pmu *cpu_pmu = to_arm_pmu(event->pmu);
 	struct pmu_hw_events *events = cpu_pmu->get_hw_events();
 
 	/* Disable counter and interrupt */
@@ -1312,6 +1313,7 @@ static void krait_pmu_enable_event(struct perf_event *event)
 	unsigned long flags;
 	struct hw_perf_event *hwc = &event->hw;
 	int idx = hwc->idx;
+	struct arm_pmu *cpu_pmu = to_arm_pmu(event->pmu);
 	struct pmu_hw_events *events = cpu_pmu->get_hw_events();
 
 	/*
