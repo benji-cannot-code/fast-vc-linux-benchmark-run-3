bf4541f8cba88c8c79412b43a4a64a953b47b5ff
Florian Fainelli <f.fainelli@gmail.com>
2014-05-22 09:47:46 -0700

    net: systemport: use the new fixed PHY helpers
    of_phy_connect_fixed_link() is becoming obsolete, and also required
    platform code to register the fixed PHYs at the specified addresses for
    those to be usable. Get rid of it and use the new of_phy_is_fixed_link()
    plus of_phy_register_fixed_link() helpers to transition over the new
    scheme.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bcmsysport.h
+++ drivers/net/ethernet/broadcom/bcmsysport.h
@@ -657,6 +657,7 @@ struct bcm_sysport_priv {
 	unsigned int		rx_c_index;
 
 	/* PHY device */
+	struct device_node	*phy_dn;
 	struct phy_device	*phydev;
 	phy_interface_t		phy_interface;
 	int			old_pause;
