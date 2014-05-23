4b2216ec722f810941e0a0fefd1ac1a9efc2782d
Nishanth Menon <nm@ti.com>
2014-05-23 00:20:37 -0500

    ARM: OMAP4+: PRM: remove "wkup" event
    "wkup" event at bit offset 0 exists only on OMAP3.
    OMAP4430/60 PRM_IRQSTATUS_A9, OMAP5/DRA7 PRM_IRQSTATUS_MPU
    
    register bit 0 is DPLL_CORE_RECAL_ST not wakeup event like OMAP3.
    
    The same applies to AM437x as well.
    
    Remove the wrong definition.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Acked-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/mach-omap2/prm44xx.c
+++ arch/arm/mach-omap2/prm44xx.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Static data */
 
 static const struct omap_prcm_irq omap4_prcm_irqs[] = {
-	OMAP_PRCM_IRQ("wkup",   0,      0),
 	OMAP_PRCM_IRQ("io",     9,      1),
 };
 
