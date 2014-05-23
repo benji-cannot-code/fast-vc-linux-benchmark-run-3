bb777193f28ab4b1bee4c947b1193fb5634c7f76
Shobhit Kumar <shobhit.kumar@intel.com>
2014-05-23 21:35:27 +0530

    drm/i915: Add support for Generic MIPI panel driver
    This driver makes use of the generic panel information from the VBT.
    Panel information is classified into two - panel configuration and panel
    power sequence which is unique to each panel. The generic driver uses the
    panel configuration and sequence parsed from VBT block #52 and #53
    
    v2: Address review comments by Jani
        - Move all of the things in driver c file from header
        - Make all functions static
        - Make use of video/mipi_display.c instead of redefining
        - Null checks during sequence execution
    
    v3: Address review comments by Damien
        - Rename the panel driver file as intel_dsi_panel_vbt.c
        - Fix style changes as suggested
        - Correct comments for lp->hs and hs->lp count calculations
        - General updating comments to have more clarity
        - using max() instead of ternary operator
        - Fix names (ui_num, ui_den) while using UI in calculations
        - compute max of lp_to_hs switch and hs_to_lp switch while computing
          hs_lp_switch_count
    
    Signed-off-by: Shobhit Kumar <shobhit.kumar@intel.com>
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dsi.c
+++ drivers/gpu/drm/i915/intel_dsi.c
@@ -36,6 +36,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* the sub-encoders aka panel drivers */
 static const struct intel_dsi_device intel_dsi_devices[] = {
+	{
+		.panel_id = MIPI_DSI_GENERIC_PANEL_ID,
+		.name = "vbt-generic-dsi-vid-mode-display",
+		.dev_ops = &vbt_generic_dsi_display_ops,
+	},
 };
 
 static void band_gap_reset(struct drm_i915_private *dev_priv)
