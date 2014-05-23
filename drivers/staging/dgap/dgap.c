9db5f0e0632609bcade00b844b2ff787a58f2607
Mark Hounschell <markh@compro.net>
2014-05-23 14:02:34 -0400

    staging: dgap: Change GFP_ATOMICs to GFP_KERNEL
    We don't need to use GFP_ATOMIC at driver load time
    so use GFP_KERNEL instead.
    
    Signed-off-by: Mark Hounschell <markh@compro.net>
    Tested-by: Mark Hounschell <markh@compro.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap.c
+++ drivers/staging/dgap/dgap.c
@@ -1379,7 +1379,7 @@ static int dgap_tty_init(struct board_t *brd)
 	for (i = 0; i < brd->nasync; i++) {
 		if (!brd->channels[i]) {
 			brd->channels[i] =
-				kzalloc(sizeof(struct channel_t), GFP_ATOMIC);
+				kzalloc(sizeof(struct channel_t), GFP_KERNEL);
 			if (!brd->channels[i])
 				return -ENOMEM;
 		}
@@ -4122,11 +4122,11 @@ static int dgap_after_config_loaded(int board)
 	/*
 	 * allocate flip buffer for board.
 	 */
-	dgap_board[board]->flipbuf = kmalloc(MYFLIPLEN, GFP_ATOMIC);
+	dgap_board[board]->flipbuf = kmalloc(MYFLIPLEN, GFP_KERNEL);
 	if (!dgap_board[board]->flipbuf)
 		return -ENOMEM;
 
-	dgap_board[board]->flipflagbuf = kmalloc(MYFLIPLEN, GFP_ATOMIC);
+	dgap_board[board]->flipflagbuf = kmalloc(MYFLIPLEN, GFP_KERNEL);
 	if (!dgap_board[board]->flipflagbuf) {
 		kfree(dgap_board[board]->flipbuf);
 		return -ENOMEM;
@@ -7233,7 +7233,7 @@ static struct cnode *dgap_newnode(int t)
 {
 	struct cnode *n;
 
-	n = kmalloc(sizeof(struct cnode), GFP_ATOMIC);
+	n = kmalloc(sizeof(struct cnode), GFP_KERNEL);
 	if (n) {
 		memset((char *)n, 0, sizeof(struct cnode));
 		n->type = t;
