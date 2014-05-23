cbe18f0f53109dfb1954dbc83f856ec37edb0fbb
Kinglong Mee <kinglongmee@gmail.com>
2014-05-23 20:54:27 +0800

    NFSD: Cleanup unneeded including net/ipv6.h
    Commit 49b28684fdba ("nfsd: Remove deprecated nfsctl system call and
    related code") removed the only use of ipv6_addr_set_v4mapped(), so
    net/ipv6.h is unneeded now.
    
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/export.c
+++ fs/nfsd/export.c
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/exportfs.h>
 #include <linux/sunrpc/svc_xprt.h>
 
-#include <net/ipv6.h>
-
 #include "nfsd.h"
 #include "nfsfh.h"
 #include "netns.h"
