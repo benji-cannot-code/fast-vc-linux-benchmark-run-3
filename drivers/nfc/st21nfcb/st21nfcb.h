64e8890ce504cf00a163dea9de32916ea2ccfdf0
Christophe Ricard <christophe.ricard@gmail.com>
2014-05-25 22:35:38 +0200

    NFC: st21nfcb: Add driver for STMicroelectronics ST21NFCB NFC chip
    Add driver for STMicroelectronics ST21NFCB NFC controller.
    ST21NFCB is using NCI protocol and a proprietary low level transport
    protocol called NDLC used on top.
    
    NDLC:
    The protocol defines 2 types of frame:
    - One type carrying NCI data (referred as DATAFRAME frames).
    - One type carrying protocol information used for flow control and error
    control mechanisms (referred as SUPERVISOR frames).
    
    After each frame transmission to the NFC controller, the device host
    SHALL waitfor  an ACK (SUPERVISOR frame) reception before sending a
    new frame.
    The NFC controller MAY send a frame at anytime to the device host.
    The NFC controller MAY send a specific WAIT supervisor frame to indicate
    to device host that a NCI data packet has been received but that it could
    take significant time before the NFC controller sends an ACK and thus
    allows next data reception.
    
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

---
+++ drivers/nfc/st21nfcb/st21nfcb.h
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * NCI based Driver for STMicroelectronics NFC Chip
+ *
+ * Copyright (C) 2014  STMicroelectronics SAS. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LOCAL_ST21NFCB_H_
+#define __LOCAL_ST21NFCB_H_
+
+#include <net/nfc/nci_core.h>
+
+#include "ndlc.h"
+
+/* Define private flags: */
+#define ST21NFCB_NCI_RUNNING			1
+
+struct st21nfcb_nci_info {
+	struct llt_ndlc *ndlc;
+	unsigned long flags;
+};
+
+void st21nfcb_nci_remove(struct nci_dev *ndev);
+int st21nfcb_nci_probe(struct llt_ndlc *ndlc, int phy_headroom,
+		int phy_tailroom);
+
+#endif /* __LOCAL_ST21NFCB_H_ */
