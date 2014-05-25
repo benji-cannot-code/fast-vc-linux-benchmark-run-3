b3b1fc1f5dfde696e528b34625114fa9ca0aaf41
Christophe Ricard <christophe.ricard@gmail.com>
2014-05-25 22:46:58 +0200

    NFC: st21nfcb: few code clean up
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- include/linux/platform_data/st21nfcb.h
+++ include/linux/platform_data/st21nfcb.h
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _ST21NFCA_HCI_H_
-#define _ST21NFCA_HCI_H_
+#ifndef _ST21NFCB_NCI_H_
+#define _ST21NFCB_NCI_H_
 
 #include <linux/i2c.h>
 
