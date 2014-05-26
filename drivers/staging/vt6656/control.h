b90dc2160f1d5049f9b9428e928b3bf8247e4b55
Malcolm Priestley <tvboxspy@gmail.com>
2014-05-26 13:59:05 +0100

    staging: vt6656: remove dead code ControlvMaskByte
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/control.h
+++ drivers/staging/vt6656/control.h
@@ -37,7 +37,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void ControlvReadByte(struct vnt_private *pDevice, u8 reg, u8 reg_off,
 			u8 *data);
 
-void ControlvMaskByte(struct vnt_private *pDevice, u8 reg_type, u8 reg_off,
-			u8 reg_mask, u8 data);
-
 #endif /* __CONTROL_H__ */
