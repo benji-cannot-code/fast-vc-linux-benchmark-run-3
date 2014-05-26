6d66341656a389bf22a2567180689084108f720b
Daeseok Youn <daeseok.youn@gmail.com>
2014-05-26 19:23:01 +0900

    staging: dgap: remove unneeded kfree() for ttys in tty_driver
    In destruct_tty_driver() from put_tty_driver() will free the
    ttys in tty_driver.
    
    Signed-off-by: Daeseok Youn <daeseok.youn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgap/dgap.c
+++ drivers/staging/dgap/dgap.c
@@ -1504,8 +1504,6 @@ static void dgap_tty_uninit(struct board_t *brd)
 			tty_unregister_device(brd->serial_driver, i);
 		}
 		tty_unregister_driver(brd->serial_driver);
-		kfree(brd->serial_driver->ttys);
-		brd->serial_driver->ttys = NULL;
 		put_tty_driver(brd->serial_driver);
 		kfree(brd->serial_ports);
 		brd->dgap_major_serial_registered = FALSE;
@@ -1521,8 +1519,6 @@ static void dgap_tty_uninit(struct board_t *brd)
 			tty_unregister_device(brd->print_driver, i);
 		}
 		tty_unregister_driver(brd->print_driver);
-		kfree(brd->print_driver->ttys);
-		brd->print_driver->ttys = NULL;
 		put_tty_driver(brd->print_driver);
 		kfree(brd->printer_ports);
 		brd->dgap_major_transparent_print_registered = FALSE;
