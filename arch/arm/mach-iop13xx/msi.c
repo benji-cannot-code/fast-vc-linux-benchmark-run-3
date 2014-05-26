43de569d6f9c594a5ef4b05eb42d0522440abd0b
Arnd Bergmann <arnd@arndb.de>
2014-05-26 17:12:10 +0200

    ARM: iop13xx: fix msi support with sparse IRQ
    patch 37ebbcff7837 ("arm: iop13xx: Use sparse irqs for MSI") moved
    iop13xx over to sparse IRQ support, but this broke the build for the
    msi.c file, which now has to include mach/irqs.h itself.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Grant Likely <grant.likely@linaro.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Link: http://lkml.kernel.org/r/12285212.fBJyVfk69p@wuerfel
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/arm/mach-iop13xx/msi.c
+++ arch/arm/mach-iop13xx/msi.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/msi.h>
 #include <asm/mach/irq.h>
 #include <asm/irq.h>
+#include <mach/irqs.h>
 
 /* IMIPR0 CP6 R8 Page 1
  */
