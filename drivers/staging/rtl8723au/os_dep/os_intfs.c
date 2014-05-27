d3f43a1e707f76855f9a2371fa4de09c9f784236
Sachin Kamat <sachin.kamat@linaro.org>
2014-05-27 15:28:10 +0530

    staging: rtl8723au: Remove unneeded version.h inclusion in os_intfs.c
    version.h inclusion is not needed as detected by versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/os_dep/os_intfs.c
+++ drivers/staging/rtl8723au/os_dep/os_intfs.c
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <rtl8723a_hal.h>
 
-#include <linux/version.h>
-
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Realtek Wireless Lan Driver");
 MODULE_AUTHOR("Realtek Semiconductor Corp.");
