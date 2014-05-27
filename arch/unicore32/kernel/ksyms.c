ca0fe8095d10ba336f1a0c130ea92cf150ab5e59
Chen Gang <gang.chen.5i5j@gmail.com>
2014-05-27 08:03:19 +0800

    arch: unicore32: ksyms: export additional find_first_*() to avoid compiling failure
    Some modules need find_first_bit() and find_first_zero_bit(), so export
    them.
    
    The related error (with allmodconfig under unicore32):
    
        MODPOST 4039 modules
      ERROR: "find_first_bit" [sound/soc/codecs/snd-soc-uda1380.ko] undefined!
      ERROR: "find_first_zero_bit" [net/sctp/sctp.ko] undefined!
      ...
    
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>
    Acked-by: Xuetao Guan <gxt@mprc.pku.edu.cn>
    Signed-off-by: Xuetao Guan <gxt@mprc.pku.edu.cn>

--- arch/unicore32/kernel/ksyms.c
+++ arch/unicore32/kernel/ksyms.c
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ksyms.h"
 
+EXPORT_SYMBOL(find_first_bit);
+EXPORT_SYMBOL(find_first_zero_bit);
 EXPORT_SYMBOL(find_next_zero_bit);
 EXPORT_SYMBOL(find_next_bit);
 
