03e7ee7ba9f51132ddb0139da894c3d2933be2e7
Sachin Kamat <sachin.kamat@linaro.org>
2014-05-27 15:28:09 +0530

    staging: rtl8723au: Remove unneeded version.h inclusion in ioctl_cfg80211.c
    version.h inclusion is not needed as detected by versioncheck.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/os_dep/ioctl_cfg80211.c
+++ drivers/staging/rtl8723au/os_dep/ioctl_cfg80211.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <xmit_osdep.h>
 
 #include "ioctl_cfg80211.h"
-#include <linux/version.h>
 
 #define RTW_MAX_MGMT_TX_CNT 8
 
