f9dc558116a8255731d3507f8ab3134a6916f93d
Heesub Shin <heesub.shin@samsung.com>
2014-05-28 15:52:59 +0900

    staging: ion: shrink highmem pages on kswapd
    ION system heap keeps pages in its pool for better performance. When the
    system is under memory pressure, slab shrinker calls the callback
    registered and then the pages pooled get freed.
    
    When the shrinker is called, it checks gfp_mask and determines whether
    the pages from highmem need to be freed or the pages from lowmem.
    Usually, slab shrinker is invoked on kswapd context which gfp_mask is
    always GFP_KERNEL, so only lowmem pages are released on kswapd context.
    This means that highmem pages in the pool are never reclaimed until
    direct reclaim occurs. This can be problematic when the page pool holds
    excessive amounts of highmem.
    
    For now, the shrinker callback cannot know exactly which zone should be
    targeted for reclamation, as enough information are not passed to. Thus,
    it makes sense to shrink both lowmem and highmem zone on kswapd context.
    
    Reported-by: Wonseo Choi <wonseo.choi@samsung.com>
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>
    Reviewed-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    Tested-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/swap.h>
 #include "ion_priv.h"
 
 static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
@@ -119,7 +120,10 @@ int ion_page_pool_shrink(struct ion_page_pool *pool, gfp_t gfp_mask,
 	int freed;
 	bool high;
 
-	high = !!(gfp_mask & __GFP_HIGHMEM);
+	if (current_is_kswapd())
+		high = 1;
+	else
+		high = !!(gfp_mask & __GFP_HIGHMEM);
 
 	if (nr_to_scan == 0)
 		return ion_page_pool_total(pool, high);
