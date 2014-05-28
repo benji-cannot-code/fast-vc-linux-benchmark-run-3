89faa58cbee8088bafaa1b5891f80dde429d2184
Sachin Kamat <sachin.kamat@linaro.org>
2014-05-28 09:34:25 -0700

    Input: soc_button_array - remove duplicate inclusion of input.h
    input.h was included twice.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/soc_button_array.c
+++ drivers/input/misc/soc_button_array.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/acpi.h>
 #include <linux/gpio/consumer.h>
 #include <linux/gpio_keys.h>
-#include <linux/input.h>
 #include <linux/platform_device.h>
 #include <linux/pnp.h>
 
