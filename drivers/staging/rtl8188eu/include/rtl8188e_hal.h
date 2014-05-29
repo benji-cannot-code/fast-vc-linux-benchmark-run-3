288b751aebb526e3e4cc1eb90d89011d0c216a9a
navin patidar <navin.patidar@gmail.com>
2014-05-29 23:20:53 +0530

    staging: rtl8188eu: Remove empty files rtl8188e_sreset.[c|h]
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtl8188e_hal.h
+++ drivers/staging/rtl8188eu/include/rtl8188e_hal.h
@@ -32,9 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rtl8188e_xmit.h"
 #include "rtl8188e_cmd.h"
 #include "Hal8188EPwrSeq.h"
-#include "rtl8188e_sreset.h"
 #include "rtw_efuse.h"
-
+#include "rtw_sreset.h"
 #include "odm_precomp.h"
 
 /*  Fw Array */
