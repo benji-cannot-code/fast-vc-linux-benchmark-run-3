abdeb1e197fee2145d5a21afc1bd149846adb743
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-05-29 10:30:02 -0700

    staging: comedi: addi_common.h: remove ADDI_{EN, DIS}ABLE defines
    These defines don't add any additional clarity to the addi_data drivers.
    They are also only used in the addi_apci_3120 driver. Just remove them.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi-data/addi_common.h
+++ drivers/staging/comedi/drivers/addi-data/addi_common.h
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LOWORD(W)	(unsigned short)((W) & 0xFFFF)
 #define HIWORD(W)	(unsigned short)(((W) >> 16) & 0xFFFF)
 
-#define ADDI_ENABLE		1
-#define ADDI_DISABLE		0
-
 #define ADDIDATA_EEPROM		1
 #define ADDIDATA_NO_EEPROM	0
 #define ADDIDATA_93C76		"93C76"
