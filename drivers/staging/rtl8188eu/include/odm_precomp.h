7664b1e1f3c2b40c30ecc6054c9eb4a3c47f940f
navin patidar <navin.patidar@gmail.com>
2014-05-29 23:20:56 +0530

    staging: rtl8188eu: Remove Hal8188EReg.h file
    Driver doesn't need Hal8188EReg.h file.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm_precomp.h
+++ drivers/staging/rtl8188eu/include/odm_precomp.h
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "HalHWImg8188E_MAC.h"
 #include "HalHWImg8188E_RF.h"
 #include "HalHWImg8188E_BB.h"
-#include "Hal8188EReg.h"
 
 #include "odm_RegConfig8188E.h"
 #include "odm_RTL8188E.h"
