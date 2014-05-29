3265807e3f1e2762a8202f839de7fc57ac1a50cb
navin patidar <navin.patidar@gmail.com>
2014-05-29 23:20:52 +0530

    staging: rtl8188eu: Remove function rtl8188e_silentreset_for_specific_platform()
    Remove rtl8188e_silentreset_for_specific_platform() and function pointer
    ->silentreset pointing to it.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/rtl8188e_sreset.c
+++ drivers/staging/rtl8188eu/hal/rtl8188e_sreset.c
@@ -23,7 +23,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <rtl8188e_sreset.h>
 #include <rtl8188e_hal.h>
 
-void rtl8188e_silentreset_for_specific_platform(struct adapter *padapter)
-{
-}
 
--- drivers/staging/rtl8188eu/include/rtl8188e_sreset.h
+++ drivers/staging/rtl8188eu/include/rtl8188e_sreset.h
@@ -25,6 +25,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drv_types.h>
 #include <rtw_sreset.h>
 
-void rtl8188e_silentreset_for_specific_platform(struct adapter *padapter);
 
 #endif
