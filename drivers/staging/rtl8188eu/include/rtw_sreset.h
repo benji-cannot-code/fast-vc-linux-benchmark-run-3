64a57d8db9c19b252ecc6293dfb61c081f499cca
navin patidar <navin.patidar@gmail.com>
2014-05-29 23:20:55 +0530

    staging: rtl8188eu: Remove "last_tx_complete_time" from struct sreset_priv
    value stored in variable last_tx_complete_time isn't being used
    by driver.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_sreset.h
+++ drivers/staging/rtl8188eu/include/rtw_sreset.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct sreset_priv {
 	u8	Wifi_Error_Status;
-	unsigned long last_tx_complete_time;
 };
 
 #include <rtl8188e_hal.h>
