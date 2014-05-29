36f02b45b7049fe8ba5103e7ec225118b3361c5a
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-05-29 23:23:08 +0200

    drm/i915: Fix context locking in debugfs
    This goes all the way back to the introduction of this debugfs file,
    even though back then no locking really was required. None of the
    intermediate patches fixed this.
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_debugfs.c
+++ drivers/gpu/drm/i915/i915_debugfs.c
@@ -1722,7 +1722,7 @@ static int i915_context_status(struct seq_file *m, void *unused)
 	struct intel_context *ctx;
 	int ret, i;
 
-	ret = mutex_lock_interruptible(&dev->mode_config.mutex);
+	ret = mutex_lock_interruptible(&dev->struct_mutex);
 	if (ret)
 		return ret;
 
@@ -1752,7 +1752,7 @@ static int i915_context_status(struct seq_file *m, void *unused)
 		seq_putc(m, '\n');
 	}
 
-	mutex_unlock(&dev->mode_config.mutex);
+	mutex_unlock(&dev->struct_mutex);
 
 	return 0;
 }
