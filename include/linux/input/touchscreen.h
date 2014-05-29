a77d6f479eff334463ccfc478c069ecb04e91ac3
Sebastian Reichel <sre@kernel.org>
2014-05-28 23:51:53 -0700

    Input: add common DT binding for touchscreens
    Add common DT binding documentation for touchscreen devices and
    implement input_parse_touchscreen_of_params, which parses the common
    properties and configures the input device accordingly.
    
    The method currently does not interpret the axis inversion properties,
    since there is no matching flag in the generic linux input device.
    
    Reviewed-by: Pavel Machek <pavel@ucw.cz>
    Acked-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Sebastian Reichel <sre@kernel.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

---
+++ include/linux/input/touchscreen.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (c) 2014 Sebastian Reichel <sre@kernel.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ */
+
+#ifndef _TOUCHSCREEN_H
+#define _TOUCHSCREEN_H
+
+#include <linux/input.h>
+
+#ifdef CONFIG_OF
+void touchscreen_parse_of_params(struct input_dev *dev);
+#else
+static inline void touchscreen_parse_of_params(struct input_dev *dev)
+{
+}
+#endif
+
+#endif
