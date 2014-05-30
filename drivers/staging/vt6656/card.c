cc3155387bb74a97d550e78092d67d4aabd9c334
Malcolm Priestley <tvboxspy@gmail.com>
2014-05-30 23:22:48 +0100

    staging: vt6656: rename CARDbClearCurrentTSF to vnt_clear_current_tsf
    Drop card and rename to vnt_clear_current_tsf
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/card.c
+++ drivers/staging/vt6656/card.c
@@ -634,7 +634,7 @@ bool vnt_get_current_tsf(struct vnt_private *priv, u64 *current_tsf)
  * Return Value: true if success; otherwise false
  *
  */
-bool CARDbClearCurrentTSF(struct vnt_private *priv)
+bool vnt_clear_current_tsf(struct vnt_private *priv)
 {
 
 	MACvRegBitsOn(priv, MAC_REG_TFTCTL, TFTCTL_TSFCNTRST);
@@ -695,7 +695,7 @@ void CARDvSetFirstNextTBTT(struct vnt_private *priv, u16 beacon_interval)
 	u64 next_tbtt = 0;
 	u8 data[8];
 
-	CARDbClearCurrentTSF(priv);
+	vnt_clear_current_tsf(priv);
 
 	next_tbtt = CARDqGetNextTBTT(next_tbtt, beacon_interval);
 
