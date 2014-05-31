9089fffb49aad7f645db7f220ae3d78719f053f8
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-05-31 18:05:10 +0200

    staging: rtl8723au: Use struct ieee80211_mcs_info to describe MCS information
    The original RTL code defined the MCS information as a 16 byte array
    and blindly applied it's masking over the full array, ignoring the
    fact that only the first 10 bytes describe the rx_mask and the rest
    are different fields.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_mlme_ext.c
+++ drivers/staging/rtl8723au/core/rtw_mlme_ext.c
@@ -3499,7 +3499,7 @@ static void issue_assocreq(struct rtw_adapter *padapter)
 				if (pregpriv->rx_stbc)
 					pmlmeinfo->HT_caps.u.HT_cap_element.HT_caps_info |= cpu_to_le16(0x0100);
 
-				memcpy(pmlmeinfo->HT_caps.u.HT_cap_element.MCS_rate, MCS_rate_1R23A, 16);
+				memcpy(&pmlmeinfo->HT_caps.u.HT_cap_element.mcs_info, MCS_rate_1R23A, 16);
 				break;
 
 			case RF_2T2R:
@@ -3520,7 +3520,7 @@ static void issue_assocreq(struct rtw_adapter *padapter)
 						  "STBC\n");
 					pmlmeinfo->HT_caps.u.HT_cap_element.HT_caps_info |= cpu_to_le16(0x0200);/* RX STBC two spatial stream */
 				}
-				memcpy(pmlmeinfo->HT_caps.u.HT_cap_element.MCS_rate, MCS_rate_2R23A, 16);
+				memcpy(&pmlmeinfo->HT_caps.u.HT_cap_element.mcs_info, MCS_rate_2R23A, 16);
 				break;
 			}
 			pmlmeinfo->HT_caps.u.HT_cap_element.HT_caps_info =
