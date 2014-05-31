98eb81a3c00bfe2d5d749cc23ab6a925b98fbc8e
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-05-31 18:05:19 +0200

    staging: rtl8723au: collect_bss_info23a(): ieee80211_mgmt capab_info is little endian
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_mlme_ext.c
+++ drivers/staging/rtl8723au/core/rtw_mlme_ext.c
@@ -4547,7 +4547,7 @@ int collect_bss_info23a(struct rtw_adapter *padapter,
 	if (ieee80211_is_beacon(mgmt->frame_control)) {
 		bssid->reserved = 1;
 		ie_offset = offsetof(struct ieee80211_mgmt, u.beacon.variable);
-		capab_info = mgmt->u.beacon.capab_info;
+		capab_info = get_unaligned_le16(&mgmt->u.beacon.capab_info);
 		bssid->BeaconPeriod =
 			get_unaligned_le16(&mgmt->u.beacon.beacon_int);
 	} else  if (ieee80211_is_probe_req(mgmt->frame_control)) {
@@ -4561,13 +4561,13 @@ int collect_bss_info23a(struct rtw_adapter *padapter,
 		ie_offset = offsetof(struct ieee80211_mgmt,
 				     u.probe_resp.variable);
 		bssid->reserved = 3;
-		capab_info = mgmt->u.probe_resp.capab_info;
+		capab_info = get_unaligned_le16(&mgmt->u.probe_resp.capab_info);
 		bssid->BeaconPeriod =
 			get_unaligned_le16(&mgmt->u.probe_resp.beacon_int);
 	} else {
 		bssid->reserved = 0;
 		ie_offset = offsetof(struct ieee80211_mgmt, u.beacon.variable);
-		capab_info = mgmt->u.beacon.capab_info;
+		capab_info = get_unaligned_le16(&mgmt->u.beacon.capab_info);
 		bssid->BeaconPeriod =
 			padapter->registrypriv.dev_network.BeaconPeriod;
 	}
@@ -4663,7 +4663,7 @@ int collect_bss_info23a(struct rtw_adapter *padapter,
 		return _SUCCESS;
 	}
 
-	if (capab_info & BIT(0)) {
+	if (capab_info & WLAN_CAPABILITY_ESS) {
 		bssid->ifmode = NL80211_IFTYPE_STATION;
 		ether_addr_copy(bssid->MacAddress, mgmt->sa);
 	} else {
@@ -4671,7 +4671,7 @@ int collect_bss_info23a(struct rtw_adapter *padapter,
 		ether_addr_copy(bssid->MacAddress, mgmt->bssid);
 	}
 
-	if (capab_info & BIT(4))
+	if (capab_info & WLAN_CAPABILITY_PRIVACY)
 		bssid->Privacy = 1;
 	else
 		bssid->Privacy = 0;
