a97b3b8f3bcf28b143976ced7a308f77ad153d8c
Fabio Estevam <fabio.estevam@freescale.com>
2014-06-02 15:44:30 -0300

    fec: Include pinctrl header file
    Commit 5bbde4d2ec ("net: fec: use pinctrl PM helpers") caused the following
    build error on m68k:
    
    drivers/net/ethernet/freescale/fec_main.c: In function 'fec_enet_open':
    drivers/net/ethernet/freescale/fec_main.c:1819:2: error: implicit declaration of function 'pinctrl_pm_select_default_state' [-Werror=implicit-function-declaration]
    drivers/net/ethernet/freescale/fec_main.c: In function 'fec_enet_close':
    drivers/net/ethernet/freescale/fec_main.c:1863:2: error: implicit declaration of function 'pinctrl_pm_select_sleep_state' [-Werror=implicit-function-declaration]
    
    In order to fix the build error, include the linux/pinctrl/consumer.h header
    file.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_net.h>
 #include <linux/regulator/consumer.h>
 #include <linux/if_vlan.h>
+#include <linux/pinctrl/consumer.h>
 
 #include <asm/cacheflush.h>
 
