fbce228a5ea7f672993377dc278ef1ce617eed2c
James A Shackleford <shack@linux.com>
2014-06-01 22:19:33 -0400

    staging: vt6655: fix sparse warning for static declarations
    This patch fixes the following sparse warnings:
    
    iwctl.c:76:22: warning: symbol 'iwctl_get_wireless_stats' was not declared. Should it be static?
    iwctl.c:118:5: warning: symbol 'iwctl_giwname' was not declared. Should it be static?
    iwctl.c:131:5: warning: symbol 'iwctl_siwscan' was not declared. Should it be static?
    iwctl.c:192:5: warning: symbol 'iwctl_giwscan' was not declared. Should it be static?
    iwctl.c:344:5: warning: symbol 'iwctl_siwfreq' was not declared. Should it be static?
    iwctl.c:390:5: warning: symbol 'iwctl_giwfreq' was not declared. Should it be static?
    iwctl.c:420:5: warning: symbol 'iwctl_siwmode' was not declared. Should it be static?
    iwctl.c:486:5: warning: symbol 'iwctl_giwmode' was not declared. Should it be static?
    iwctl.c:520:5: warning: symbol 'iwctl_giwrange' was not declared. Should it be static?
    iwctl.c:626:5: warning: symbol 'iwctl_siwap' was not declared. Should it be static?
    iwctl.c:684:5: warning: symbol 'iwctl_giwap' was not declared. Should it be static?
    iwctl.c:711:5: warning: symbol 'iwctl_giwaplist' was not declared. Should it be static?
    iwctl.c:784:5: warning: symbol 'iwctl_siwessid' was not declared. Should it be static?
    iwctl.c:893:5: warning: symbol 'iwctl_giwessid' was not declared. Should it be static?
    iwctl.c:923:5: warning: symbol 'iwctl_siwrate' was not declared. Should it be static?
    iwctl.c:1004:5: warning: symbol 'iwctl_giwrate' was not declared. Should it be static?
    iwctl.c:1049:5: warning: symbol 'iwctl_siwrts' was not declared. Should it be static?
    iwctl.c:1077:5: warning: symbol 'iwctl_giwrts' was not declared. Should it be static?
    iwctl.c:1096:5: warning: symbol 'iwctl_siwfrag' was not declared. Should it be static?
    iwctl.c:1123:5: warning: symbol 'iwctl_giwfrag' was not declared. Should it be static?
    iwctl.c:1141:5: warning: symbol 'iwctl_siwretry' was not declared. Should it be static?
    iwctl.c:1176:5: warning: symbol 'iwctl_giwretry' was not declared. Should it be static?
    iwctl.c:1205:5: warning: symbol 'iwctl_siwencode' was not declared. Should it be static?
    iwctl.c:1336:5: warning: symbol 'iwctl_giwencode' was not declared. Should it be static?
    iwctl.c:1398:5: warning: symbol 'iwctl_siwpower' was not declared. Should it be static?
    iwctl.c:1448:5: warning: symbol 'iwctl_giwpower' was not declared. Should it be static?
    iwctl.c:1478:5: warning: symbol 'iwctl_giwsens' was not declared. Should it be static?
    iwctl.c:1502:5: warning: symbol 'iwctl_siwauth' was not declared. Should it be static?
    iwctl.c:1603:5: warning: symbol 'iwctl_giwauth' was not declared. Should it be static?
    iwctl.c:1611:5: warning: symbol 'iwctl_siwgenie' was not declared. Should it be static?
    iwctl.c:1644:5: warning: symbol 'iwctl_giwgenie' was not declared. Should it be static?
    iwctl.c:1669:5: warning: symbol 'iwctl_siwencodeext' was not declared. Should it be static?
    iwctl.c:1783:5: warning: symbol 'iwctl_giwencodeext' was not declared. Should it be static?
    iwctl.c:1791:5: warning: symbol 'iwctl_siwmlme' was not declared. Should it be static?
    iwctl.c:1900:21: warning: symbol 'iwctl_private_args' was not declared. Should it be static?
    iwctl.c:1906:33: warning: symbol 'iwctl_handler_def' was not declared. Should it be static?
    
    Signed-off-by: James A Shackleford <shack@linux.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/iwctl.c
+++ drivers/staging/vt6655/iwctl.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "device.h"
 #include "ioctl.h"
 #include "iocmd.h"
+#include "iwctl.h"
 #include "mac.h"
 #include "card.h"
 #include "hostap.h"
