a0e5f2a31e4e5c6401c520cc8f0890ad1e54e199
Anand Jain <Anand.Jain@oracle.com>
2014-06-03 11:36:00 +0800

    btrfs: dev delete should remove sysfs entry
    when we delete the device from the mounted btrfs,
    we would need its corresponding sysfs enty to
    be removed as well.
    
    Signed-off-by: Anand Jain <Anand.Jain@oracle.com>
    Reviewed-by: David Sterba <dsterba@suse.cz>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rcu-string.h"
 #include "math.h"
 #include "dev-replace.h"
+#include "sysfs.h"
 
 static int init_first_rw_device(struct btrfs_trans_handle *trans,
 				struct btrfs_root *root,
@@ -1680,6 +1681,9 @@ int btrfs_rm_device(struct btrfs_root *root, char *device_path)
 
 	if (device->bdev)
 		device->fs_devices->open_devices--;
+
+	/* remove sysfs entry */
+	btrfs_kobj_rm_device(root->fs_info, device);
 
 	call_rcu(&device->rcu, free_device);
 
