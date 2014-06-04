57bebf90d0e07132d9a2b708a1dbcf8c5b51e39f
Malcolm Priestley <tvboxspy@gmail.com>
2014-06-04 18:25:26 +0100

    staging: vt6656: rename PSbIsNextTBTTWakeUp to vnt_next_tbtt_wakeup
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/power.c
+++ drivers/staging/vt6656/power.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *      PSbConsiderPowerDown - Decide if we can Power Down
  *      PSvSendPSPOLL - Send PS-POLL packet
  *      PSbSendNullPacket - Send Null packet
- *      PSbIsNextTBTTWakeUp - Decide if we need to wake up at next Beacon
+ *      vnt_next_tbtt_wakeup - Decide if we need to wake up at next Beacon
  *
  * Revision History:
  *
@@ -313,7 +313,7 @@ int PSbSendNullPacket(struct vnt_private *pDevice)
  *
  */
 
-int PSbIsNextTBTTWakeUp(struct vnt_private *priv)
+int vnt_next_tbtt_wakeup(struct vnt_private *priv)
 {
 	struct vnt_manager *mgmt = &priv->vnt_mgmt;
 	int wake_up = false;
