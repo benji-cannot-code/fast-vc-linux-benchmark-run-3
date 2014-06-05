abb38a6b8d19b7f087fdc0e1178d93a13eb43c0d
Konrad Zapalowicz <bergo.torino@gmail.com>
2014-06-05 20:27:42 +0200

    net: phy: fix sparse warning in fixed.c
    This commit fixes the following sparse warning:
    
    drivers/net/phy/fixed.c:207
        - warning: symbol 'fixed_phy_del' was not declared.
          Should it be static?
    
    by adding symbol definition to the phy_fixed.h API file. It is ok to do
    because the function in question is an exported symbol.
    
    Signed-off-by: Konrad Zapalowicz <bergo.torino@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/phy_fixed.h
+++ include/linux/phy_fixed.h
@@ -18,6 +18,7 @@ extern int fixed_phy_add(unsigned int irq, int phy_id,
 extern int fixed_phy_register(unsigned int irq,
 			      struct fixed_phy_status *status,
 			      struct device_node *np);
+extern void fixed_phy_del(int phy_addr);
 #else
 static inline int fixed_phy_add(unsigned int irq, int phy_id,
 				struct fixed_phy_status *status)
@@ -27,6 +28,10 @@ static inline int fixed_phy_add(unsigned int irq, int phy_id,
 static inline int fixed_phy_register(unsigned int irq,
 				     struct fixed_phy_status *status,
 				     struct device_node *np)
+{
+	return -ENODEV;
+}
+static inline int fixed_phy_del(int phy_addr)
 {
 	return -ENODEV;
 }
