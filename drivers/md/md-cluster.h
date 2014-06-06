550deeb5d19683be65a52e70e88b3f26ac51233d
Goldwyn Rodrigues <rgoldwyn@suse.com>
2014-06-06 12:35:34 -0500

    Gather on-going resync information of other nodes
    When a node joins, it does not know of other nodes performing resync.
    So, each node keeps the resync information in it's LVB. When a new
    node joins, it reads the LVB of each "online" bitmap.
    
    [TODO] The new node attempts to get the PW lock on other bitmap, if
    it is successful, it reads the bitmap and performs the resync (if
    required) on it's behalf.
    
    If the node does not get the PW, it requests CR and reads the LVB
    for the resync information.
    
    Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.com>

--- drivers/md/md-cluster.h
+++ drivers/md/md-cluster.h
@@ -12,6 +12,7 @@ struct md_cluster_operations {
 	int (*join)(struct mddev *mddev, int nodes);
 	int (*leave)(struct mddev *mddev);
 	int (*slot_number)(struct mddev *mddev);
+	void (*resync_info_update)(struct mddev *mddev, sector_t lo, sector_t hi);
 };
 
 #endif /* _MD_CLUSTER_H */
