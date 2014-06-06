ca84ed46fbea33bd666a6c44fd3784ee60b2a20d
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-06-06 14:14:05 +0100

    ASoC: wm5102: Add controls to allow shaping of ultrasonic response
    Add controls to allow custom shaping of the ultrasonic response. This
    custom shaping can be turned on/off at runtime, although, it should be
    noted that settings will not affect a currently open audio stream,
    they will be applied when the next audio stream is started.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- include/linux/mfd/arizona/core.h
+++ include/linux/mfd/arizona/core.h
@@ -114,6 +114,9 @@ struct arizona {
 
 	int tdm_width[ARIZONA_MAX_AIF];
 	int tdm_slots[ARIZONA_MAX_AIF];
+
+	uint16_t dac_comp_coeff;
+	uint8_t dac_comp_enabled;
 };
 
 int arizona_clk32k_enable(struct arizona *arizona);
