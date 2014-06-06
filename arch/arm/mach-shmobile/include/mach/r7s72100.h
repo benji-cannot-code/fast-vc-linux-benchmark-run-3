a6b39d809c8cc5b7cb6acd0c23891a350ddb4486
Magnus Damm <damm+renesas@opensource.se>
2014-06-06 14:29:04 +0900

    ARM: shmobile: Use shmobile_init_delay() on r7s72100
    Adjust the r7s72100 SoC support code to use shmobile_init_delay()
    together with CPU Frequency settings from the DTS. Get rid of the
    C code version.
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/r7s72100.h
+++ arch/arm/mach-shmobile/include/mach/r7s72100.h
@@ -4,6 +4,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void r7s72100_add_dt_devices(void);
 void r7s72100_clock_init(void);
-void r7s72100_init_early(void);
 
 #endif /* __ASM_R7S72100_H__ */
