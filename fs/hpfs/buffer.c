289f095622d741c493b0846660be096532bc1cd0
Fabian Frederick <fabf@skynet.be>
2014-06-06 14:36:34 -0700

    fs/hpfs: convert printk to pr_foo()
    No level printk in hptfs_error converted to pr_err (others to pr_warn or
    pr_info)
    
    This patch also fixes if/then/else checkpatch warnings
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Cc: Mikulas Patocka <mikulas@artax.karlin.mff.cuni.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/hpfs/buffer.c
+++ fs/hpfs/buffer.c
@@ -56,7 +56,7 @@ void *hpfs_map_sector(struct super_block *s, unsigned secno, struct buffer_head 
 	if (bh != NULL)
 		return bh->b_data;
 	else {
-		printk("HPFS: hpfs_map_sector: read error\n");
+		pr_warn("HPFS: hpfs_map_sector: read error\n");
 		return NULL;
 	}
 }
@@ -77,7 +77,7 @@ void *hpfs_get_sector(struct super_block *s, unsigned secno, struct buffer_head 
 		set_buffer_uptodate(bh);
 		return bh->b_data;
 	} else {
-		printk("HPFS: hpfs_get_sector: getblk failed\n");
+		pr_warn("HPFS: hpfs_get_sector: getblk failed\n");
 		return NULL;
 	}
 }
@@ -94,7 +94,7 @@ void *hpfs_map_4sectors(struct super_block *s, unsigned secno, struct quad_buffe
 	cond_resched();
 
 	if (secno & 3) {
-		printk("HPFS: hpfs_map_4sectors: unaligned read\n");
+		pr_warn("HPFS: hpfs_map_4sectors: unaligned read\n");
 		return NULL;
 	}
 
@@ -113,7 +113,7 @@ void *hpfs_map_4sectors(struct super_block *s, unsigned secno, struct quad_buffe
 
 	qbh->data = data = kmalloc(2048, GFP_NOFS);
 	if (!data) {
-		printk("HPFS: hpfs_map_4sectors: out of memory\n");
+		pr_warn("HPFS: hpfs_map_4sectors: out of memory\n");
 		goto bail4;
 	}
 
@@ -146,7 +146,7 @@ void *hpfs_get_4sectors(struct super_block *s, unsigned secno,
 	hpfs_lock_assert(s);
 
 	if (secno & 3) {
-		printk("HPFS: hpfs_get_4sectors: unaligned read\n");
+		pr_warn("HPFS: hpfs_get_4sectors: unaligned read\n");
 		return NULL;
 	}
 
@@ -162,7 +162,7 @@ void *hpfs_get_4sectors(struct super_block *s, unsigned secno,
 	}
 
 	if (!(qbh->data = kmalloc(2048, GFP_NOFS))) {
-		printk("HPFS: hpfs_get_4sectors: out of memory\n");
+		pr_warn("HPFS: hpfs_get_4sectors: out of memory\n");
 		goto bail4;
 	}
 	return qbh->data;
