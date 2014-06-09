7cdd0f58b851a66c578f6c51b197061d7fd9f18f
Pranith Kumar <bobby.prani@gmail.com>
2014-06-09 12:55:25 -0300

    [media] update reference, kerneltrap.org no longer works
    kerneltrap.org no longer works, update to a working reference
    
    Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>
    Acked-by: Alexey Klimov <klimov.linux@gmail.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/radio/radio-mr800.c
+++ drivers/media/radio/radio-mr800.c
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * achievements (specifications given).
  * Also, Faidon Liambotis <paravoid@debian.org> wrote nice driver for this radio
  * in 2007. He allowed to use his driver to improve current mr800 radio driver.
- * http://kerneltrap.org/mailarchive/linux-usb-devel/2007/10/11/342492
+ * http://www.spinics.net/lists/linux-usb-devel/msg10109.html
  *
  * Version 0.01:	First working version.
  * 			It's required to blacklist AverMedia USB Radio
