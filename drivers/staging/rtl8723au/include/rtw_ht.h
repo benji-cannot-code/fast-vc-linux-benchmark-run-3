01524e7c1a5779d61801cd8743d3508535375ef4
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-06-09 15:16:06 +0200

    staging: rtl8723au: Make struct ht_priv.ht_option a bool
    Match the return type of rtw_restructure_ht_ie23a() accordingly
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/include/rtw_ht.h
+++ drivers/staging/rtl8723au/include/rtw_ht.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct ht_priv
 {
-	u32	ht_option;
+	bool	ht_option;
 	u32	ampdu_enable;/* for enable Tx A-MPDU */
 	/* u8	baddbareq_issued[16]; */
 	u32	tx_amsdu_enable;/* for enable Tx A-MSDU */
