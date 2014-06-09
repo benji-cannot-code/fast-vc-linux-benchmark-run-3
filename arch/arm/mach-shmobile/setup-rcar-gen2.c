d2d9cdcdee4918f7a0f744ba1039a48d9cfb218a
Magnus Damm <damm+renesas@opensource.se>
2014-06-09 21:38:45 +0900

    ARM: shmobile: Add shared R-Car Gen2 CMA reservation code
    Add R-Car Gen2 CMA memory reservation code that can be
    shared between multiple SoCs and boards. At this point
    r8a7790 and r8a7791 are supported.
    
    The top 256MiB of the legacy 32-bit physical memory space
    is assigned to a separate CMA area that may be assigned
    to various devices later on.
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    [horms+renesas@verge.net.au: rebased]
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-rcar-gen2.c
+++ arch/arm/mach-shmobile/setup-rcar-gen2.c
@@ -21,8 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk/shmobile.h>
 #include <linux/clocksource.h>
+#include <linux/device.h>
+#include <linux/dma-contiguous.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
+#include <linux/of_fdt.h>
 #include <asm/mach/arch.h>
 #include "common.h"
 #include "rcar-gen2.h"
