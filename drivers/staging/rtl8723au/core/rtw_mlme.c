6c53758cb3e03758fe6003bd24bd6d97c7d0127b
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-06-09 15:16:29 +0200

    staging: rtl8723au: Do not embed struct wlan_bssid_ex in struct survey_event
    Instead allocate it separately and reference it from survey_event.
    This will allow for us to pass it on later without having to copy it.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_mlme.c
+++ drivers/staging/rtl8723au/core/rtw_mlme.c
@@ -641,7 +641,7 @@ void rtw_survey_event_cb23a(struct rtw_adapter *adapter, const u8 *pbuf)
 	struct mlme_priv *pmlmepriv = &adapter->mlmepriv;
 	struct survey_event *survey = (struct survey_event *)pbuf;
 
-	pnetwork = &survey->bss;
+	pnetwork = survey->bss;
 
 	RT_TRACE(_module_rtl871x_mlme_c_,_drv_info_,
 		 ("rtw_survey_event_cb23a, ssid=%s\n", pnetwork->Ssid.ssid));
@@ -691,6 +691,9 @@ void rtw_survey_event_cb23a(struct rtw_adapter *adapter, const u8 *pbuf)
 exit:
 
 	spin_unlock_bh(&pmlmepriv->lock);
+
+	kfree(survey->bss);
+	survey->bss = NULL;
 
 	return;
 }
