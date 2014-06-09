842f90e93b2c7d3ecbee8d4f212ddae0bb0138bb
Weston Andros Adamson <dros@primarydata.com>
2014-06-09 11:48:39 -0400

    nfs: remove unneeded EXPORTs
    EXPORT_GPLs of nfs_pageio_add_request and nfs_pageio_complete aren't
    needed anymore.
    
    Suggested-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Weston Andros Adamson <dros@primarydata.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/pagelist.c
+++ fs/nfs/pagelist.c
@@ -948,7 +948,6 @@ int nfs_pageio_add_request(struct nfs_pageio_descriptor *desc,
 	} while (ret);
 	return ret;
 }
-EXPORT_SYMBOL_GPL(nfs_pageio_add_request);
 
 /*
  * nfs_pageio_resend - Transfer requests to new descriptor and resend
@@ -996,7 +995,6 @@ void nfs_pageio_complete(struct nfs_pageio_descriptor *desc)
 			break;
 	}
 }
-EXPORT_SYMBOL_GPL(nfs_pageio_complete);
 
 /**
  * nfs_pageio_cond_complete - Conditional I/O completion
