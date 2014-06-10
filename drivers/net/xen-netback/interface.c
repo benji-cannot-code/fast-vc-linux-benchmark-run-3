b82735ff2d7273e89fd2626efcef665184728ae6
Arnd Bergmann <arnd@arndb.de>
2014-06-10 10:34:36 +0200

    net: xen-netback: include linux/vmalloc.h again
    commit e9ce7cb6b107 ("xen-netback: Factor queue-specific data into
    queue struct") added a use of vzalloc/vfree to interface.c, but
    removed the #include <linux/vmalloc.h> statement at the same time,
    which causes this build error:
    
    drivers/net/xen-netback/interface.c: In function 'xenvif_free':
    drivers/net/xen-netback/interface.c:754:2: error: implicit declaration of function 'vfree' [-Werror=implicit-function-declaration]
      vfree(vif->queues);
      ^
    cc1: some warnings being treated as errors
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Wei Liu <wei.liu2@citrix.com>
    Cc: Andrew J. Bennieston <andrew.bennieston@citrix.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/xen-netback/interface.c
+++ drivers/net/xen-netback/interface.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ethtool.h>
 #include <linux/rtnetlink.h>
 #include <linux/if_vlan.h>
+#include <linux/vmalloc.h>
 
 #include <xen/events.h>
 #include <asm/xen/hypercall.h>
