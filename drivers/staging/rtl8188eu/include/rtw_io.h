bebdeb9b443b2cd671e90e9b7046eef1aebc3fed
navin patidar <navin.patidar@gmail.com>
2014-06-11 22:51:28 +0530

    staging: rtl8188eu: Remove function _rtw_read_port_cancel()
    _rtw_read_port_cancel() is a wrapper function, being used to call
    usb_read_port_cancel().
    Call usb_read_port_cancel() directly and drop _rtw_read_port_cancel().
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_io.h
+++ drivers/staging/rtl8188eu/include/rtw_io.h
@@ -53,7 +53,6 @@ struct _io_ops {
 	int (*_write32)(struct adapter *pintfhdl, u32 addr, u32 val);
 	u32 (*_write_port)(struct adapter *pintfhdl, u32 addr, u32 cnt,
 			   u8 *pmem);
-	void (*_read_port_cancel)(struct adapter *pintfhdl);
 	void (*_write_port_cancel)(struct adapter *pintfhdl);
 };
 
@@ -90,7 +89,7 @@ u8 _rtw_read8(struct adapter *adapter, u32 addr);
 u16 _rtw_read16(struct adapter *adapter, u32 addr);
 u32 _rtw_read32(struct adapter *adapter, u32 addr);
 u32 usb_read_port(struct adapter *adapter, u32 addr, u32 cnt, u8 *pmem);
-void _rtw_read_port_cancel(struct adapter *adapter);
+void usb_read_port_cancel(struct adapter *adapter);
 
 int _rtw_write8(struct adapter *adapter, u32 addr, u8 val);
 int _rtw_write16(struct adapter *adapter, u32 addr, u16 val);
@@ -103,7 +102,6 @@ void _rtw_write_port_cancel(struct adapter *adapter);
 #define rtw_read8(adapter, addr) _rtw_read8((adapter), (addr))
 #define rtw_read16(adapter, addr) _rtw_read16((adapter), (addr))
 #define rtw_read32(adapter, addr) _rtw_read32((adapter), (addr))
-#define rtw_read_port_cancel(adapter) _rtw_read_port_cancel((adapter))
 
 #define  rtw_write8(adapter, addr, val)					\
 	_rtw_write8((adapter), (addr), (val))
