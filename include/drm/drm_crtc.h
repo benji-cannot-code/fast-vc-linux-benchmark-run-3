86938409d97f58353033fc4faa8a6620127e2afc
Vandana Kannan <vandana.kannan@intel.com>
2014-06-11 10:46:48 +0530

    drm/crtc: Add property for aspect ratio
    Added a property to enable user space to set aspect ratio.
    This patch contains declaration of the property and code to create the
    property.
    
    v2: Thierry's review comments.
    	- Made aspect ratio enum generic instead of HDMI/CEA specfic
    	- Removed usage of temporary aspect_ratio variable
    
    v3: Thierry's review comments.
    	- Fixed indentation
    
    v4: Thierry's review comments.
    	- Return ENOMEM when property creation fails
    
    Signed-off-by: Vandana Kannan <vandana.kannan@intel.com>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    Acked-by: Dave Airlie <airlied@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/drm_crtc.h
+++ include/drm/drm_crtc.h
@@ -836,6 +836,7 @@ struct drm_mode_config {
 
 	/* Optional properties */
 	struct drm_property *scaling_mode_property;
+	struct drm_property *aspect_ratio_property;
 	struct drm_property *dirty_info_property;
 
 	/* dumb ioctl parameters */
@@ -1024,6 +1025,7 @@ extern int drm_mode_create_dvi_i_properties(struct drm_device *dev);
 extern int drm_mode_create_tv_properties(struct drm_device *dev, int num_formats,
 				     char *formats[]);
 extern int drm_mode_create_scaling_mode_property(struct drm_device *dev);
+extern int drm_mode_create_aspect_ratio_property(struct drm_device *dev);
 extern int drm_mode_create_dirty_info_property(struct drm_device *dev);
 
 extern int drm_mode_connector_attach_encoder(struct drm_connector *connector,
