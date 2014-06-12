f0de4f879f820169665b2a58a077968df7b248de
Ben Skeggs <bskeggs@redhat.com>
2014-06-12 22:15:21 +1000

    drm/gk208-/gr: stop touching 0x260 inappropriately
    As a side note.. It's a bit hard to figure out how to name this commit..
    GK20A is NVEA, which is before NV108 (GK208).. Confusing.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/graph/nvc0.h
+++ drivers/gpu/drm/nouveau/core/engine/graph/nvc0.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <subdev/vm.h>
 #include <subdev/bar.h>
 #include <subdev/timer.h>
+#include <subdev/mc.h>
 
 #include <engine/fifo.h>
 #include <engine/graph.h>
