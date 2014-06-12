ba4e6013922fefd71d36e8ac112e37697882aa18
Thierry Reding <treding@nvidia.com>
2014-06-12 13:17:48 +0200

    gpu: host1x: Fix typo in comment
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/host1x/hw/cdma_hw.c
+++ drivers/gpu/host1x/hw/cdma_hw.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../debug.h"
 
 /*
- * Put the restart at the end of pushbuffer memor
+ * Put the restart at the end of pushbuffer memory
  */
 static void push_buffer_init(struct push_buffer *pb)
 {
