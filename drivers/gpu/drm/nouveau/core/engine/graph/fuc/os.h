ab8a254633a29fd8ebf3d819bae5d655d5e7e2a8
Ben Skeggs <bskeggs@redhat.com>
2014-06-13 11:26:45 +1000

    drm/gf100-/gr: report class data to host on fwmthd failure
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/engine/graph/fuc/os.h
+++ drivers/gpu/drm/nouveau/core/engine/graph/fuc/os.h
@@ -4,5 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define E_BAD_COMMAND  0x00000001
 #define E_CMD_OVERFLOW 0x00000002
+#define E_BAD_FWMTHD   0x00000003
 
 #endif
--- drivers/gpu/drm/nouveau/core/engine/graph/nvc0.h
+++ drivers/gpu/drm/nouveau/core/engine/graph/nvc0.h
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <engine/fifo.h>
 #include <engine/graph.h>
 
+#include "fuc/os.h"
+
 #define GPC_MAX 32
 #define TPC_MAX (GPC_MAX * 8)
 
