6fb5a46bfcc18a28d73c4c63c21c5ccfe7600b78
Radek Dostal <rd@radekdostal.com>
2014-06-14 16:13:09 +0200

    staging/fwserial: (coding style) remove not needed return statements
    Style-only modifications to make checkpatch.pl -f a bit happier.
    Fixes warning: "void function return statements are not generally useful"
    
    Signed-off-by: Radek Dostal <rd@radekdostal.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fwserial/fwserial.c
+++ drivers/staging/fwserial/fwserial.c
@@ -2616,7 +2616,6 @@ cleanup:
 	if (port)
 		fwserial_release_port(port, false);
 	kfree(pkt);
-	return;
 }
 
 static void fwserial_handle_unplug_req(struct work_struct *work)
@@ -2668,7 +2667,6 @@ cleanup:
 	if (port)
 		fwserial_release_port(port, true);
 	kfree(pkt);
-	return;
 }
 
 static int fwserial_parse_mgmt_write(struct fwtty_peer *peer,
