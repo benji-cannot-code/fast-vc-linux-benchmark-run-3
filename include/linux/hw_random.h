8a95ddbeccb6e5c6def54c79aea0a9e4291047ed
Torsten Duwe <duwe@lst.de>
2014-06-14 23:48:41 -0400

    hwrng: add per-device entropy derating
    This patch introduces a derating factor to struct hwrng for
    the random bits going into the kernel input pool, and a common
    default derating for drivers which do not specify one.
    
    Signed-off-by: Torsten Duwe <duwe@suse.de>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Acked-by: H. Peter Anvin <hpa@zytor.com>

--- include/linux/hw_random.h
+++ include/linux/hw_random.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @read:		New API. drivers can fill up to max bytes of data
  *			into the buffer. The buffer is aligned for any type.
  * @priv:		Private data, for use by the RNG driver.
+ * @quality:		Estimation of true entropy in RNG's bitstream
+ *			(per mill).
  */
 struct hwrng {
 	const char *name;
@@ -39,6 +41,7 @@ struct hwrng {
 	int (*data_read)(struct hwrng *rng, u32 *data);
 	int (*read)(struct hwrng *rng, void *data, size_t max, bool wait);
 	unsigned long priv;
+	unsigned short quality;
 
 	/* internal. */
 	struct list_head list;
