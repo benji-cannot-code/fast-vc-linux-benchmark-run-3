f5282302e994d29b24ce2e9027804c9bc1c0e338
Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
2014-06-16 19:37:23 +0300

    wil6210: track Tx queue state
    Provide both event (netif_tx_[stop|wake]) tracking via printk;
    and state via debugfs 'info'
    
    Signed-off-by: Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/wil6210/main.c
+++ drivers/net/wireless/ath/wil6210/main.c
@@ -474,6 +474,7 @@ void wil_link_on(struct wil6210_priv *wil)
 	wil_dbg_misc(wil, "%s()\n", __func__);
 
 	netif_carrier_on(ndev);
+	wil_dbg_misc(wil, "netif_tx_wake : link on\n");
 	netif_tx_wake_all_queues(ndev);
 }
 
@@ -484,6 +485,7 @@ void wil_link_off(struct wil6210_priv *wil)
 	wil_dbg_misc(wil, "%s()\n", __func__);
 
 	netif_tx_stop_all_queues(ndev);
+	wil_dbg_misc(wil, "netif_tx_stop : link off\n");
 	netif_carrier_off(ndev);
 }
 
