372f413886627ede0d520d612fa7a80ec3bbf472
Stephan Gabert <stephan.gabert@fau.de>
2014-06-16 16:50:49 +0200

    staging/wlags49_h2: checkpatch: including <linux/*.h> instead of <asm/*.h>
    As warned by checkpatch.pl, one should use #include <linux/io.h>
    instead of #include <asm/io.h> and #include <linux/bitops.h>
    instead of #include <asm/bitops.h>.
    
    Signed-off-by: Stephan Gabert <stephan.gabert@fau.de>
    Signed-off-by: Nicolas Pfeiffer <nicolas.pfeiffer@fau.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlags49_h2/wl_cs.c
+++ drivers/staging/wlags49_h2/wl_cs.c
@@ -74,8 +74,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/in.h>
 #include <linux/delay.h>
-#include <asm/io.h>
-#include <asm/bitops.h>
+#include <linux/io.h>
+#include <linux/bitops.h>
 
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
