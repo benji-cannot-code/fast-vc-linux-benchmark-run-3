9befcf449741d0de16ab7a934e24b6d1c7cbce5d
Magnus Damm <damm+renesas@opensource.se>
2014-06-17 16:48:01 +0900

    ARM: shmobile: Move rcar-gen2.h, cleanup r8a7790 case
    Change location of rcar-gen2.h so it can be used as #include "rcar-gen2.h"
    instead of the old style #include <mach/rcar-gen2.h>. Also clean up
    the r8a7790 case to follow the same style as r8a7791.
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-lager.c
+++ arch/arm/mach-shmobile/board-lager.c
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/simple_card.h>
 #include "common.h"
 #include "irqs.h"
+#include "rcar-gen2.h"
 
 /*
  * SSI-AK4643
--- arch/arm/mach-shmobile/setup-r8a7790.c
+++ arch/arm/mach-shmobile/setup-r8a7790.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "common.h"
 #include "dma-register.h"
 #include "irqs.h"
+#include "rcar-gen2.h"
 
 /* Audio-DMAC */
 #define AUDIO_DMAC_SLAVE(_id, _addr, t, r)			\
