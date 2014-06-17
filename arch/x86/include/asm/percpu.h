2fa5e373a92e2b072a7e4125ebb286c09b291cdc
Tejun Heo <tj@kernel.org>
2014-06-17 19:12:34 -0400

    percpu: introduce arch_raw_cpu_ptr()
    Currently, archs can override raw_cpu_ptr() directly; however, we
    wanna build a layer of indirection in the generic part of percpu so
    that we can implement generic features there without affecting archs.
    
    Introduce arch_raw_cpu_ptr() which is used to define raw_cpu_ptr() by
    generic percpu code.  The two are identical for now.  x86 is currently
    the only arch which overrides raw_cpu_ptr() and is converted to
    define arch_raw_cpu_ptr() instead.
    
    This doesn't introduce any functional difference.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Christoph Lameter <cl@linux-foundation.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>

--- arch/x86/include/asm/percpu.h
+++ arch/x86/include/asm/percpu.h
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Compared to the generic __my_cpu_offset version, the following
  * saves one instruction and avoids clobbering a temp register.
  */
-#define raw_cpu_ptr(ptr)				\
+#define arch_raw_cpu_ptr(ptr)				\
 ({							\
 	unsigned long tcp_ptr__;			\
 	__verify_pcpu_ptr(ptr);				\
