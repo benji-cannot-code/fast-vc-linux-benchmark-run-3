07e37aa4b07b902b660fd2732c22210834536d38
Keerthy <j-keerthy@ti.com>
2014-06-18 15:28:57 +0530

    mfd: palmas: shift the palmas_sleep_requestor_info structure definition to the header file
    shift the palmas_sleep_requestor_info structure definition to the header file.
    
    Acked-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Keerthy <j-keerthy@ti.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/mfd/palmas.c
+++ drivers/mfd/palmas.c
@@ -26,16 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mfd/palmas.h>
 #include <linux/of_device.h>
 
-#define PALMAS_EXT_REQ (PALMAS_EXT_CONTROL_ENABLE1 |	\
-			PALMAS_EXT_CONTROL_ENABLE2 |	\
-			PALMAS_EXT_CONTROL_NSLEEP)
-
-struct palmas_sleep_requestor_info {
-	int id;
-	int reg_offset;
-	int bit_pos;
-};
-
 #define EXTERNAL_REQUESTOR(_id, _offset, _pos)		\
 	[PALMAS_EXTERNAL_REQSTR_ID_##_id] = {		\
 		.id = PALMAS_EXTERNAL_REQSTR_ID_##_id,	\
