34be1810572127706bd2888468704edfc422078d
Bill Richardson <wfrichar@chromium.org>
2014-06-18 11:14:03 -0700

    mfd: cros_ec: cleanup: remove unused fields from struct cros_ec_device
    struct cros_ec_device has a superfluous "name" field. We can get all the
    debugging info we need from the existing ec_name and phys_name fields, so
    let's take out the extra field.
    
    The printout also has sufficient info in it without explicitly adding
    the transport.  Before this change:
      cros-ec-spi spi2.0: Chrome EC (SPI)
    
    After this change:
      cros-ec-spi spi2.0: Chrome EC device registered
    
    Signed-off-by: Bill Richardson <wfrichar@chromium.org>
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -68,7 +68,6 @@ struct cros_ec_command {
  * @command_recv: receive a response
  * @command_sendrecv: send a command and receive a response
  *
- * @name: Name of this EC interface
  * @priv: Private data
  * @irq: Interrupt to use
  * @din: input buffer (for data from EC)
@@ -105,7 +104,6 @@ struct cros_ec_device {
 				void *in_buf, int in_len);
 
 	/* These are used to implement the platform-specific interface */
-	const char *name;
 	void *priv;
 	int irq;
 	uint8_t *din;
