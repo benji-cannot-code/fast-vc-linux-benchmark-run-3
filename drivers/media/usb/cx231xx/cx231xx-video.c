9e338d3542e3de0713d7aedbc22838b7b39115f9
Ramakrishnan Muthukrishnan <ramakrmu@cisco.com>
2014-06-19 14:22:58 -0300

    [media] media: remove the setting of the flag V4L2_FL_USE_FH_PRIO
    Since all the drivers that use `struct v4l2_fh' use the core
    priority checking, the setting of the flag in the drivers can
    be removed.
    
    Signed-off-by: Ramakrishnan Muthukrishnan <ramakrmu@cisco.com>
    Reviewed-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/usb/cx231xx/cx231xx-video.c
+++ drivers/media/usb/cx231xx/cx231xx-video.c
@@ -2066,7 +2066,6 @@ static struct video_device *cx231xx_vdev_init(struct cx231xx *dev,
 	vfd->release = video_device_release;
 	vfd->debug = video_debug;
 	vfd->lock = &dev->lock;
-	set_bit(V4L2_FL_USE_FH_PRIO, &vfd->flags);
 
 	snprintf(vfd->name, sizeof(vfd->name), "%s %s", dev->name, type_name);
 
