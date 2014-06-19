a68c888156b05f5c39f1323572aaa205bae06e6b
Alexander Shiyan <shc_work@mail.ru>
2014-06-19 19:26:36 +0400

    ARM: clps711x: clep7312: Add common devices initialization
    Add initialization of commonly used devices to the CLEP7312 generic
    board.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-clps711x/board-clep7312.c
+++ arch/arm/mach-clps711x/board-clep7312.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 
 #include "common.h"
+#include "devices.h"
 
 static void __init
 fixup_clep7312(struct tag *tags, char **cmdline)
@@ -40,5 +41,6 @@ MACHINE_START(CLEP7212, "Cirrus Logic 7212/7312")
 	.map_io		= clps711x_map_io,
 	.init_irq	= clps711x_init_irq,
 	.init_time	= clps711x_timer_init,
+	.init_machine	= clps711x_devices_init,
 	.restart	= clps711x_restart,
 MACHINE_END
