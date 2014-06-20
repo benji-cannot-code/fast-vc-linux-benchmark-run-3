976430a72db344365833638e724d4c13eb62cb54
Xinming Hu <huxm@marvell.com>
2014-06-19 21:38:57 -0700

    mwifiex: update Copyright to 2014
    This patch updates mwifiex Copyright to 2014.
    
    Signed-off-by: Xinming Hu <huxm@marvell.com>
    Signed-off-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mwifiex/pcie.h
+++ drivers/net/wireless/mwifiex/pcie.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @brief This file contains definitions for PCI-E interface.
  * driver.
  *
- * Copyright (C) 2011, Marvell International Ltd.
+ * Copyright (C) 2011-2014, Marvell International Ltd.
  *
  * This software file (the "File") is distributed by Marvell International
  * Ltd. under the terms of the GNU General Public License Version 2, June 1991
