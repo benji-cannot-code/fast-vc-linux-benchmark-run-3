7d9b30b37e5fc3d8604069da9d5427d98b779440
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-06-21 16:50:13 +0200

    staging: rtl8723au: Remove unused struct mlme_priv->wfd_* entries
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_ap.c
+++ drivers/staging/rtl8723au/core/rtw_ap.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern unsigned char WMM_OUI23A[];
 extern unsigned char WPS_OUI23A[];
 extern unsigned char P2P_OUI23A[];
-extern unsigned char WFD_OUI23A[];
 
 void init_mlme_ap_info23a(struct rtw_adapter *padapter)
 {
