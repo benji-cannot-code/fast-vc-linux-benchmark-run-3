c0b5380f4fbdfd5cd75dcb759a2a6bf60484b1a2
Pawel Lebioda <pawel.lebioda89@gmail.com>
2014-06-21 19:29:35 +0200

    staging: cxt1e1: fix sbeCrc function declaration
    This patch fixes the following sparse warning:
    
    drivers/staging/cxt1e1/sbecrc.c:86:1: warning: symbol 'sbeCrc' was not declared. Should it be static?
    
    Furthermore it fixes wring sbeCrc return type in function declaration.
    
    Signed-off-by: Pawel Lebioda <pawel.lebioda89@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/cxt1e1/pmcc4_private.h
+++ drivers/staging/cxt1e1/pmcc4_private.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "musycc.h"
 #include "sbe_promformat.h"
 #include "comet.h"
+#include "sbecom_inline_linux.h"
 
 
 /* driver state */
