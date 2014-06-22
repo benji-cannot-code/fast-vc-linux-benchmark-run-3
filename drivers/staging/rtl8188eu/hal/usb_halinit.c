e92beb71f7a14e9fb89abdfd437d9b7627fa9412
navin patidar <navin.patidar@gmail.com>
2014-06-22 13:49:26 +0530

    staging: rtl8188eu: Remove dummy function Hal_InitChannelPlan()
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/usb_halinit.c
+++ drivers/staging/rtl8188eu/hal/usb_halinit.c
@@ -1150,10 +1150,6 @@ readAdapterInfo_8188EU(
 	Hal_EfuseParseBoardType88E(adapt, eeprom->efuse_eeprom_data, eeprom->bautoload_fail_flag);
 	Hal_ReadThermalMeter_88E(adapt, eeprom->efuse_eeprom_data, eeprom->bautoload_fail_flag);
 
-	/*  */
-	/*  The following part initialize some vars by PG info. */
-	/*  */
-	Hal_InitChannelPlan(adapt);
 }
 
 static void _ReadPROMContent(
