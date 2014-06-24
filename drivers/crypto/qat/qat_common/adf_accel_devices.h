6a928e1c2c8c55838f6b6267e9409313987d6511
Tadeusz Struk <tadeusz.struk@intel.com>
2014-06-24 15:19:24 -0700

    crypto: qat - Fix random config build warnings
    Fix random config build warnings:
    
    Implicit-function-declaration ‘__raw_writel’
    Cast to pointer from integer of different size [-Wint-to-pointer-cast]
    
    Reviewed-by: Bruce Allan <bruce.w.allan@intel.com>
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_common/adf_accel_devices.h
+++ drivers/crypto/qat/qat_common/adf_accel_devices.h
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/atomic.h>
 #include <linux/list.h>
 #include <linux/proc_fs.h>
+#include <linux/io.h>
 #include "adf_cfg_common.h"
 
 #define PCI_VENDOR_ID_INTEL 0x8086
