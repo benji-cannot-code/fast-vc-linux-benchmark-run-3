35fee7d7ab7615338fd3643dbaa647986e037524
Sachin Kamat <sachin.kamat@samsung.com>
2014-06-24 11:48:19 +0530

    spi: omap-uwire: Fix build error
    Fixes the following build error introduced by commit b3f6a57506
    ("spi: omap-uwire: use devm_ functions"):
    drivers/spi/spi-omap-uwire.c:465:2: error: implicit declaration of function ‘devm_ioremap’
    
    Since we are including <linux/io.h>, <asm/io.h> is no longer
    needed. Remove it.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@samsung.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-omap-uwire.c
+++ drivers/spi/spi-omap-uwire.c
@@ -47,10 +47,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_bitbang.h>
 #include <linux/module.h>
+#include <linux/io.h>
 
 #include <asm/irq.h>
 #include <mach/hardware.h>
-#include <asm/io.h>
 #include <asm/mach-types.h>
 
 #include <mach/mux.h>
