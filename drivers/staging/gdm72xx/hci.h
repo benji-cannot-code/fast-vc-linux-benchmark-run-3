4a545152b1725eea7523a673a21c31cb973ca7e9
Ben Chan <benchan@chromium.org>
2014-06-24 00:55:58 -0700

    staging: gdm72xx: fix block comment style
    This patch fixes the following checkpatch warnings, which are issued
    when the gdm72xx driver is moved out of staging into drivers/net/wimax:
    
      WARNING: networking block comments don't use an empty /* line, use /* Comment...
      WARNING: networking block comments start with * on subsequent lines
      WARNING: networking block comments put the trailing */ on a separate line
    
    Signed-off-by: Ben Chan <benchan@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm72xx/hci.h
+++ drivers/staging/gdm72xx/hci.h
@@ -113,13 +113,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define W_SCAN_ALL_SUBSCRIPTION		1
 #define W_SCAN_SPECIFIED_SUBSCRIPTION	2
 
-/*
- * TLV
+/* TLV
  *
  * [31:31] indicates the type is composite.
  * [30:16] is the length of the type. 0 length means length is variable.
  * [15:0] is the actual type.
- *
  */
 #define TLV_L(x)		(((x) >> 16) & 0xff)
 #define TLV_T(x)		((x) & 0xff)
