899bc205a2c4d24737d2c1cadbc7e1f0f42a163c
Malcolm Priestley <tvboxspy@gmail.com>
2014-06-25 21:20:02 +0100

    staging: vt6656: dpc remove dead code RXvMngWorkItem
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/dpc.h
+++ drivers/staging/vt6656/dpc.h
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void RXvWorkItem(struct work_struct *work);
 
-void RXvMngWorkItem(struct work_struct *work);
-
 void RXvFreeRCB(struct vnt_rcb *pRCB, int bReAllocSkb);
 
 int vnt_rx_data(struct vnt_private *, struct vnt_rcb *,
