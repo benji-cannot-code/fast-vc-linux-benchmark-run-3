be7fcb634acb59225502270c29a4e94616a4dd3f
Robert Baldyga <r.baldyga@samsung.com>
2014-06-25 08:26:48 +0200

    tools: ffs-aio-example: convert to new descriptor format
    Since commit [ac8dde11: “Add flags to descriptors block”] functionfs
    supports a new descriptor format, so we update example application
    to make it using recomended version of descriptors.
    
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Robert Baldyga <r.baldyga@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- tools/usb/ffs-aio-example/simple/device_app/aio_simple.c
+++ tools/usb/ffs-aio-example/simple/device_app/aio_simple.c
@@ -26,7 +26,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /******************** Descriptors and Strings *******************************/
 
 static const struct {
-	struct usb_functionfs_descs_head header;
+	struct usb_functionfs_descs_head_v2 header;
+	__le32 fs_count;
+	__le32 hs_count;
 	struct {
 		struct usb_interface_descriptor intf;
 		struct usb_endpoint_descriptor_no_audio bulk_sink;
@@ -34,11 +36,12 @@ static const struct {
 	} __attribute__ ((__packed__)) fs_descs, hs_descs;
 } __attribute__ ((__packed__)) descriptors = {
 	.header = {
-		.magic = htole32(FUNCTIONFS_DESCRIPTORS_MAGIC),
+		.magic = htole32(FUNCTIONFS_DESCRIPTORS_MAGIC_V2),
+		.flags = htole32(FUNCTIONFS_HAS_FS_DESC |
+				     FUNCTIONFS_HAS_HS_DESC),
 		.length = htole32(sizeof(descriptors)),
-		.fs_count = htole32(3),
-		.hs_count = htole32(3),
 	},
+	.fs_count = htole32(3),
 	.fs_descs = {
 		.intf = {
 			.bLength = sizeof(descriptors.fs_descs.intf),
@@ -60,6 +63,7 @@ static const struct {
 			.bmAttributes = USB_ENDPOINT_XFER_BULK,
 		},
 	},
+	.hs_count = htole32(3),
 	.hs_descs = {
 		.intf = {
 			.bLength = sizeof(descriptors.hs_descs.intf),
