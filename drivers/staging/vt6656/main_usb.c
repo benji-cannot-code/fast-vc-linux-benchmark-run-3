b85a8a812a4c03e2b7d468095db7460304e6e751
Malcolm Priestley <tvboxspy@gmail.com>
2014-06-25 21:19:42 +0100

    staging: vt6656: deadcode iwctl.c/h
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/main_usb.c
+++ drivers/staging/vt6656/main_usb.c
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rxtx.h"
 #include "bssdb.h"
 #include "wpactl.h"
-#include "iwctl.h"
 #include "dpc.h"
 #include "datarate.h"
 #include "rf.h"
@@ -193,7 +192,6 @@ static const long frequency_list[] = {
     5700, 5745, 5765, 5785, 5805, 5825
 	};
 
-static const struct iw_handler_def	iwctl_handler_def;
 */
 
 static int vt6656_probe(struct usb_interface *intf,
