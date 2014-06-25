a601e426dbeb02598b84a26ce01f746f7c4b3e11
James A Shackleford <shack@linux.com>
2014-06-24 22:52:34 -0400

    staging: rtl8712: remove wrapper function _init_listhead
    _init_listhead is just an inline wrapper around INIT_LIST_HEAD.  This
    patch removes the wrapper and directly uses INIT_LIST_HEAD instead.
    
    Signed-off-by: James A Shackleford <shack@linux.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/osdep_service.h
+++ drivers/staging/rtl8712/osdep_service.h
@@ -55,7 +55,7 @@ struct	__queue	{
 
 #define _init_queue(pqueue)				\
 	do {						\
-		_init_listhead(&((pqueue)->queue));	\
+		INIT_LIST_HEAD(&((pqueue)->queue));	\
 		spin_lock_init(&((pqueue)->lock));	\
 	} while (0)
 
@@ -136,11 +136,6 @@ static inline u32 _down_sema(struct semaphore *sema)
 		return _FAIL;
 	else
 		return _SUCCESS;
-}
-
-static inline void _init_listhead(struct list_head *list)
-{
-	INIT_LIST_HEAD(list);
 }
 
 static inline u32 _queue_empty(struct  __queue *pqueue)
