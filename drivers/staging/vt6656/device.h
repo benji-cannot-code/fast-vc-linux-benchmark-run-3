39ed29ced5d74b6953a898372ee5714a177ccd71
Malcolm Priestley <tvboxspy@gmail.com>
2014-06-25 21:14:22 +0100

    staging: vt6656: mac80211 conversion: create rx function.
    Add mac80211 header and depends.
    
    Create new function vnt_rx_data to receive 80211 packets which is based
    on RXbBulkInProcessData and rx them into mac80211
    
    The function also relays dbm, tsf_time(as mactime) and sets byBBPreEDRSSI and
    uCurrRSSI.
    
    skb is modified slightly to skb_put the maxium tail room in PIPEnsBulkInUsbRead
    and trim back in vnt_rx_data. dev_alloc_skb is used to reallocate the sk_buff.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/device.h
+++ drivers/staging/vt6656/device.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <linux/usb.h>
 #include <linux/crc32.h>
+#include <net/mac80211.h>
 
 #ifdef SIOCETHTOOL
 #define DEVICE_ETHTOOL_IOCTL_SUPPORT
@@ -393,6 +394,8 @@ typedef struct __device_opt {
 } OPTIONS, *POPTIONS;
 
 struct vnt_private {
+	/* mac80211 */
+	struct ieee80211_hw *hw;
 	/* netdev */
 	struct usb_device *usb;
 	struct net_device *dev;
@@ -402,6 +405,9 @@ struct vnt_private {
 
 	struct work_struct read_work_item;
 	struct work_struct rx_mng_work_item;
+
+	u64 tsf_time;
+	u8 rx_rate;
 
 	u32 rx_buf_sz;
 	int multicast_limit;
