17db59f7c15cf14404aa89f2125f88984d6e6808
Anil Belur <askb23@gmail.com>
2014-06-25 14:57:28 +1000

    staging: lustre: llite: llite_capa.c - fixed warning to use recomended headers <linux/uaccess.h>
    - this fixes "WARNING: Use #include <linux/uaccess.h> instead of
      <asm/uaccess.h>"
    
    Signed-off-by: Anil Belur <askb23@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_capa.c
+++ drivers/staging/lustre/lustre/llite/llite_capa.c
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUG_SUBSYSTEM S_LLITE
 
 #include <linux/fs.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/file.h>
 #include <linux/kmod.h>
 
