f0c0300efd5d59011ecaa85833a150f8603a1b18
Malcolm Priestley <tvboxspy@gmail.com>
2014-06-25 21:14:26 +0100

    staging: vt6656: mac80211 conversion: add channel bands
    Add rates and channels according to rf type for vnt_init_bands which is
    a mac80211 replacement for CHvInitChannelTable.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/main_usb.c
+++ drivers/staging/vt6656/main_usb.c
@@ -1379,6 +1379,8 @@ int vnt_init(struct vnt_private *priv)
 
 	SET_IEEE80211_PERM_ADDR(priv->hw, priv->abyPermanentNetAddr);
 
+	vnt_init_bands(priv);
+
 	if (ieee80211_register_hw(priv->hw))
 		return -ENODEV;
 
