7476ff1fd9a1aa73138d3ff475f19bf08e919f53
Malcolm Priestley <tvboxspy@gmail.com>
2014-06-25 21:19:55 +0100

    staging: vt6656: decode remove tether.c/h
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/baseband.h
+++ drivers/staging/vt6656/baseband.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __BASEBAND_H__
 #define __BASEBAND_H__
 
-#include "tether.h"
 #include "device.h"
 
 #define PREAMBLE_LONG   0
