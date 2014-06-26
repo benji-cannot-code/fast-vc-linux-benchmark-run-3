46c7b16ee6c36add71c0f01295ae9c457f24f3df
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-06-26 10:20:50 +0200

    staging: rtl8723au: rtw_allow_stainfo23a(): Make hwaddr argument const
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_sta_mgt.c
+++ drivers/staging/rtl8723au/core/rtw_sta_mgt.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sta_info.h>
 #include <rtl8723a_hal.h>
 
-static u8 bc_addr[ETH_ALEN] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+static const u8 bc_addr[ETH_ALEN] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
 static void _rtw_init_stainfo(struct sta_info *psta)
 {
@@ -110,7 +110,7 @@ int _rtw_free_sta_priv23a(struct sta_priv *pstapriv)
 }
 
 struct sta_info *
-rtw_alloc_stainfo23a(struct sta_priv *pstapriv, u8 *hwaddr, gfp_t gfp)
+rtw_alloc_stainfo23a(struct sta_priv *pstapriv, const u8 *hwaddr, gfp_t gfp)
 {
 	struct list_head	*phash_list;
 	struct sta_info	*psta;
