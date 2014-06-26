15b7042e510a86c83a886a1aba6d43acee9c09b9
Nishanth Menon <nm@ti.com>
2014-06-26 12:40:26 +0530

    irqchip: crossbar: Fix kerneldoc warning
    Adding missing properties for kerneldoc (@write) and cleanup
    of harmless warnings while we are here.
    
    kerneldoc warnings:
    Warning(drivers/irqchip/irq-crossbar.c:27): missing initial short description on line:
     * struct crossbar_device: crossbar device description
    Info(drivers/irqchip/irq-crossbar.c:27): Scanning doc for struct
    Warning(drivers/irqchip/irq-crossbar.c:39): No description found for parameter 'write'
    2 warnings
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Sricharan R <r.sricharan@ti.com>
    Acked-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Link: https://lkml.kernel.org/r/1403766634-18543-9-git-send-email-r.sricharan@ti.com
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- drivers/irqchip/irq-crossbar.c
+++ drivers/irqchip/irq-crossbar.c
@@ -23,12 +23,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IRQ_SKIP	-3
 #define GIC_IRQ_START	32
 
-/*
+/**
+ * struct crossbar_device - crossbar device description
  * @int_max: maximum number of supported interrupts
  * @safe_map: safe default value to initialize the crossbar
  * @irq_map: array of interrupts to crossbar number mapping
  * @crossbar_base: crossbar base address
  * @register_offsets: offsets for each irq number
+ * @write: register write function pointer
  */
 struct crossbar_device {
 	uint int_max;
