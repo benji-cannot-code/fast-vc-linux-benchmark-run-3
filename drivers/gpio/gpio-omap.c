41f200e389b4da4aeda3359aeddb8f488621ff96
Javier Martinez Canillas <javier@dowhile0.org>
2014-06-27 22:17:36 +0200

    gpio: omap: Remove unneeded include
    The <linux/irqchip/chained_irq.h> header is already included
    when selecting GPIOLIB_IRQCHIP so there is no need to do it
    in the driver. This is a left over from commit fb655f5
    ("gpio: omap: convert driver to use gpiolib irqchip").
    
    Signed-off-by: Javier Martinez Canillas <jmartinez@softcrates.net>
    Acked-by: Kevin Hilman <khilman@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-omap.c
+++ drivers/gpio/gpio-omap.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-#include <linux/irqchip/chained_irq.h>
 #include <linux/gpio.h>
 #include <linux/bitops.h>
 #include <linux/platform_data/gpio-omap.h>
