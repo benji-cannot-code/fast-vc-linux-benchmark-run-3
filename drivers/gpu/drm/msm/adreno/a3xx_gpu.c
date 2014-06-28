30fb867cc4bad8e23deaca63552a30f5388713ce
Rob Clark <robdclark@gmail.com>
2014-06-28 11:11:34 -0400

    drm/msm: fix double struct_mutex acquire
    Mutex is already grabbed in show_locked().. somehow this slipped
    through.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/adreno/a3xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a3xx_gpu.c
@@ -393,12 +393,9 @@ static const unsigned int a3xx_registers[] = {
 #ifdef CONFIG_DEBUG_FS
 static void a3xx_show(struct msm_gpu *gpu, struct seq_file *m)
 {
-	struct drm_device *dev = gpu->dev;
 	int i;
 
 	adreno_show(gpu, m);
-
-	mutex_lock(&dev->struct_mutex);
 
 	gpu->funcs->pm_resume(gpu);
 
@@ -419,8 +416,6 @@ static void a3xx_show(struct msm_gpu *gpu, struct seq_file *m)
 	}
 
 	gpu->funcs->pm_suspend(gpu);
-
-	mutex_unlock(&dev->struct_mutex);
 }
 #endif
 
