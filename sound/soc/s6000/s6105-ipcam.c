b814d6f8b5aaef8f06fbb5a73407d9fc080f1e6e
Lars-Peter Clausen <lars@metafoo.de>
2014-06-30 10:01:38 +0200

    ASoC: s6000: Allow to build when COMPILE_TEST is enabled
    Most of the ASoC s6000 code is architecture independent. This patch makes it
    possible to select the platform when COMPILE_TEST is enabled.
    
    The only architecture dependent code is the PCM driver which will still only be
    selected if XTENSA_VARIANT_S6000 is enabled.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/s6000/s6105-ipcam.c
+++ sound/soc/s6000/s6105-ipcam.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm.h>
 #include <sound/soc.h>
 
-#include <variant/dmac.h>
-
 #include "s6000-pcm.h"
 #include "s6000-i2s.h"
 
