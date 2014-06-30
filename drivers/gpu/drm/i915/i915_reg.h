b8543bf3f96fd60a93e8a73e0c5967792bdd4e6b
Ben Widawsky <ben@bwidawsk.net>
2014-06-30 09:53:38 -0700

    drm/i915/bdw: implement semaphore wait
    Semaphore waits use a new instruction, MI_SEMAPHORE_WAIT. The seqno to
    wait on is all well defined by the table in the previous patch. There is
    nothing else different from previous GEN's semaphore synchronization
    code.
    
    v2: Update macros to not require the other ring's ring->id (Chris)
    
    v3: Add missing VCS2 gen8_ring_wait init besides
        s/ring_buffer/engine_cs (Rodrigo)
    
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -269,6 +269,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   MI_RESTORE_INHIBIT		(1<<0)
 #define MI_SEMAPHORE_SIGNAL	MI_INSTR(0x1b, 0) /* GEN8+ */
 #define   MI_SEMAPHORE_TARGET(engine)	((engine)<<15)
+#define MI_SEMAPHORE_WAIT	MI_INSTR(0x1c, 2) /* GEN8+ */
+#define   MI_SEMAPHORE_POLL		(1<<15)
+#define   MI_SEMAPHORE_SAD_GTE_SDD	(1<<12)
 #define MI_STORE_DWORD_IMM	MI_INSTR(0x20, 1)
 #define   MI_MEM_VIRTUAL	(1 << 22) /* 965+ only */
 #define MI_STORE_DWORD_INDEX	MI_INSTR(0x21, 1)
