f3a11722a40661e08b63cfa7a891a83626ff493a
Alexander Aring <alex.aring@gmail.com>
2014-07-03 00:20:55 +0200

    at86rf230: add new author
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ieee802154/at86rf230.c
+++ drivers/net/ieee802154/at86rf230.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Written by:
  * Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
  * Alexander Smirnov <alex.bluesman.smirnov@gmail.com>
+ * Alexander Aring <aar@pengutronix.de>
  */
 #include <linux/kernel.h>
 #include <linux/module.h>
