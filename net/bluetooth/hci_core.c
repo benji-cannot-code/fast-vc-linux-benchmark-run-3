75ac9b815b375531be2c251d674fd214729142c5
Johan Hedberg <johan.hedberg@intel.com>
2014-07-03 19:33:51 +0300

    Bluetooth: Add identity address check in param lookup functions
    Since we only store entries with identity addresses in the
    le_conn_params and pend_le_conns lists we can avoid unnecessary lookups
    by checking for an identity address before diving into the lists
    themselves.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -3400,6 +3400,10 @@ struct hci_conn_params *hci_conn_params_lookup(struct hci_dev *hdev,
 {
 	struct hci_conn_params *params;
 
+	/* The conn params list only contains identity addresses */
+	if (!hci_is_identity_address(addr, addr_type))
+		return NULL;
+
 	list_for_each_entry(params, &hdev->le_conn_params, list) {
 		if (bacmp(&params->addr, addr) == 0 &&
 		    params->addr_type == addr_type) {
@@ -3432,6 +3436,10 @@ struct hci_conn_params *hci_pend_le_conn_lookup(struct hci_dev *hdev,
 						bdaddr_t *addr, u8 addr_type)
 {
 	struct hci_conn_params *param;
+
+	/* The list only contains identity addresses */
+	if (!hci_is_identity_address(addr, addr_type))
+		return NULL;
 
 	list_for_each_entry(param, &hdev->pend_le_conns, pend_le_conn) {
 		if (bacmp(&param->addr, addr) == 0 &&
