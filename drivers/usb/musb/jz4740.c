9f750f156d95590c5754ee798018478ab9d5e884
Apelete Seketeli <apelete@seketeli.net>
2014-07-06 19:58:03 +0200

    usb: musb: register nop transceiver driver for jz4740
    Following the name change of the NOP transceiver driver in commit
    4525bee (usb: phy: rename usb_nop_xceiv to usb_phy_generic), the
    transceiver driver was no longer operable under its old name.
    
    Register the transceiver driver before calling usb_get_phy() to make
    sure we are noticed by an error message if it is not available.
    
    Signed-off-by: Apelete Seketeli <apelete@seketeli.net>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/jz4740.c
+++ drivers/usb/musb/jz4740.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/usb/usb_phy_generic.h>
 
 #include "musb_core.h"
 
@@ -81,6 +82,7 @@ static struct musb_hdrc_platform_data jz4740_musb_platform_data = {
 
 static int jz4740_musb_init(struct musb *musb)
 {
+	usb_phy_generic_register();
 	musb->xceiv = usb_get_phy(USB_PHY_TYPE_USB2);
 	if (!musb->xceiv) {
 		pr_err("HS UDC: no transceiver configured\n");
@@ -183,6 +185,7 @@ static int jz4740_remove(struct platform_device *pdev)
 	struct jz4740_glue	*glue = platform_get_drvdata(pdev);
 
 	platform_device_unregister(glue->musb);
+	usb_phy_generic_unregister(pdev);
 	clk_disable_unprepare(glue->clk);
 
 	return 0;
