5ca50894a24b3b0d1be2ab27caa358865d217af1
Pankaj Dubey <pankaj.dubey@samsung.com>
2014-07-08 07:54:08 +0900

    ARM: EXYNOS: Move SYSREG definition into sys-reg specific file
    While making PMU implementation to be device tree based, there are
    few register offsets related with SYSREG present in regs-pmu.h, so
    let's make a new header file "regs-sys.h" to keep all such SYSREG
    related register offsets and remove them from "regs-pmu.h"
    
    Signed-off-by: Pankaj Dubey <pankaj.dubey@samsung.com>
    Reviewed-by: Tomasz Figa <t.figa@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/exynos.c
+++ arch/arm/mach-exynos/exynos.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "common.h"
 #include "mfc.h"
 #include "regs-pmu.h"
+#include "regs-sys.h"
 
 static struct map_desc exynos4_iodesc[] __initdata = {
 	{
