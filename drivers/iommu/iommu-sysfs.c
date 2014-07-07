d1102b3d13bed55eb8e3be2c660d758f2619e443
Joerg Roedel <jroedel@suse.de>
2014-07-07 12:01:21 +0200

    iommu: Fix compile error in iommu-sysfs.c
    Add missing include of <linux/slab.h>.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/iommu-sysfs.c
+++ drivers/iommu/iommu-sysfs.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/iommu.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 
 /*
  * We provide a common class "devices" group which initially has no attributes.
