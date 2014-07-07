599bd6117cc2901243cd7435be23e0924ba47d8e
Russell King <rmk+kernel@arm.linux.org.uk>
2014-07-08 00:23:04 +0100

    net: fec: stop the phy before shutting down the MAC
    When the network interface goes down, stop the phy to prevent further
    link up status changes before taking the MAC or netif sections down.
    This prevents further reception of link up events which could
    potentially call fec_restart().
    
    Since phy_stop() takes the mutex which adjust_link() runs under, we
    also ensure that adjust_link() will not already be processing a link
    up event.
    
    We also need to do this when suspending as well - we don't want a
    mis-timed phy state change to restart the MAC after we have stopped
    it for suspend, and thus need to restart the phy when resuming.
    
    Acked-by: Fugang Duan <B38611@freescale.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -2182,13 +2182,14 @@ fec_enet_close(struct net_device *ndev)
 {
 	struct fec_enet_private *fep = netdev_priv(ndev);
 
+	phy_stop(fep->phy_dev);
+
 	/* Don't know what to do yet. */
 	napi_disable(&fep->napi);
 	fep->opened = 0;
 	netif_tx_disable(ndev);
 	fec_stop(ndev);
 
-	phy_stop(fep->phy_dev);
 	phy_disconnect(fep->phy_dev);
 	fep->phy_dev = NULL;
 
@@ -2670,6 +2671,7 @@ fec_suspend(struct device *dev)
 	struct fec_enet_private *fep = netdev_priv(ndev);
 
 	if (netif_running(ndev)) {
+		phy_stop(fep->phy_dev);
 		fec_stop(ndev);
 		netif_device_detach(ndev);
 	}
@@ -2703,6 +2705,7 @@ fec_resume(struct device *dev)
 	if (netif_running(ndev)) {
 		fec_restart(ndev, fep->full_duplex);
 		netif_device_attach(ndev);
+		phy_start(fep->phy_dev);
 	}
 
 	return 0;
