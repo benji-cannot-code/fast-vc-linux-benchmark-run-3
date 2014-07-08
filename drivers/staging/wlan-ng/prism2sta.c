e0310ab747f989d7e3fc8adcfc72f82f45991a60
Tobias Klauser <tklauser@distanz.ch>
2014-07-08 08:37:00 +0200

    staging: wlan-ng: Use net_device_stats from struct net_device
    Instead of using an own copy of struct net_device_stats in struct
    wlandevice, use stats from struct net_device. Also remove the thus
    unnecessary .ndo_get_stats function, as it would now just return
    netdev->stats, which is the default in dev_get_stats().
    
    Furthermore, convert prefix increment of stats counters to the more
    common postfix increment idiom.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/prism2sta.c
+++ drivers/staging/wlan-ng/prism2sta.c
@@ -1849,7 +1849,7 @@ void prism2sta_ev_tx(wlandevice_t *wlandev, u16 status)
 {
 	pr_debug("Tx Complete, status=0x%04x\n", status);
 	/* update linux network stats */
-	wlandev->linux_stats.tx_packets++;
+	wlandev->netdev->stats.tx_packets++;
 }
 
 /*----------------------------------------------------------------
