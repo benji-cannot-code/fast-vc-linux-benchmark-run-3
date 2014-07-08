cbdc3c00aed32f2444c45e4441da181ea1b244d9
Lv Zheng <lv.zheng@intel.com>
2014-07-08 10:06:39 +0800

    ACPICA: Utilities: Cleanup DEFINE_ACPI_GLOBALS by moving acpi_ut_init_global() from utglobal.c to utinit.c
    The utglobal.c is used to define and initialize global variables.  It makes
    sense if just adding utglobal.o to applications that are using such
    variables. But acpi_ut_init_globals() is preventing us from doing so as
    this initialization function references other components' initializations
    code, which leads to the requirement that many files should also get linked
    if one wants to link utglobal.o.
    
    It is possible to just move acpi_ut_init_global() to utinit.c for
    applications that require this function to link.
    
    By linking utglobal.o, we can stop defining DEFINE_ACPI_GLOBALS for
    applications (currently only acpidump is affected). Lv Zheng.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- tools/power/acpi/tools/acpidump/acpidump.h
+++ tools/power/acpi/tools/acpidump/acpidump.h
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef _DECLARE_GLOBALS
 #define EXTERN
 #define INIT_GLOBAL(a,b)        a=b
-#define DEFINE_ACPI_GLOBALS     1
 #else
 #define EXTERN                  extern
 #define INIT_GLOBAL(a,b)        a
