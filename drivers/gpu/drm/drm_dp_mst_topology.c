2e375e346da40e0f72b5af11b83b634c068ed9ec
Dave Airlie <airlied@redhat.com>
2014-07-09 11:15:09 +1000

    drm/mst: fix build with debugfs off.
    seq_file.h was being pulled in via drm_mm.h in that case.
    
    Reported-by: Matthew Thode (on irc)
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_dp_mst_topology.c
+++ drivers/gpu/drm/drm_dp_mst_topology.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/errno.h>
 #include <linux/sched.h>
+#include <linux/seq_file.h>
 #include <linux/i2c.h>
 #include <drm/drm_dp_mst_helper.h>
 #include <drm/drmP.h>
