eddcbd816eebe180acf37df783edd81620ac1ca2
Arnd Bergmann <arnd@arndb.de>
2014-07-09 11:09:42 +0200

    ASoC: samsung/smartq: use dynamic registration
    As a prerequisite for moving s3c64xx into multiplatform configurations,
    we need to change the smartq audio driver to stop using hardcoded
    gpio numbers from the header file, and instead pass the gpio data
    through platform_data.
    
    In order to do that, we also move the code to use module_platform_driver
    and register the platform device using platform_device_register_simple
    and register the gpios through the gpiod API.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>

--- arch/arm/mach-s3c64xx/mach-smartq.c
+++ arch/arm/mach-s3c64xx/mach-smartq.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/fb.h>
 #include <linux/gpio.h>
+#include <linux/gpio/machine.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/pwm.h>
@@ -384,6 +385,15 @@ void __init smartq_map_io(void)
 	smartq_lcd_mode_set();
 }
 
+static struct gpiod_lookup_table smartq_audio_gpios = {
+	.dev_id = "smartq-audio",
+	.table = {
+		GPIO_LOOKUP("GPL", 12, "headphone detect", 0),
+		GPIO_LOOKUP("GPK", 12, "amplifiers shutdown", 0),
+		{ },
+	},
+};
+
 void __init smartq_machine_init(void)
 {
 	s3c_i2c0_set_platdata(NULL);
@@ -403,4 +413,7 @@ void __init smartq_machine_init(void)
 
 	pwm_add_table(smartq_pwm_lookup, ARRAY_SIZE(smartq_pwm_lookup));
 	platform_add_devices(smartq_devices, ARRAY_SIZE(smartq_devices));
+
+	gpiod_add_lookup_table(&smartq_audio_gpios);
+	platform_device_register_simple("smartq-audio", -1, NULL, 0);
 }
