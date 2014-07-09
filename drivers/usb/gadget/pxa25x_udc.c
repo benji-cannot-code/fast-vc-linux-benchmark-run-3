db063a5dd3a5e3fe118ab8f4cf463015fa6d4eba
Arnd Bergmann <arnd@arndb.de>
2014-07-09 11:24:44 +0200

    usb: gadget: pxa25x_udc: use correct header for gpio devm_ functions
    commit c63d2225e7be ("usb: gadget: pxa25x_udc: use devm_ functions")
    introduced the use of devm_gpio_request in this driver, but did not
    correctly include the header file declaring this function, which
    causes a build failure.
    
    This changes pxa25x_udc to include linux/gpio.h instead of asm/gpio.h
    to fix this.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Himangi Saraogi <himangi774@gmail.com>
    Cc: Julia Lawall <julia.lawall@lip6.fr>
    Cc: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/pxa25x_udc.c
+++ drivers/usb/gadget/pxa25x_udc.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* #define VERBOSE_DEBUG */
 
 #include <linux/device.h>
+#include <linux/gpio.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/ioport.h>
@@ -41,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/byteorder.h>
 #include <asm/dma.h>
-#include <asm/gpio.h>
 #include <asm/mach-types.h>
 #include <asm/unaligned.h>
 
