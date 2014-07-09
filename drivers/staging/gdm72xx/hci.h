a71055dab213d778b683045c9c3fdc1c3d992c62
Michalis Pappas <mpappas@fastmail.fm>
2014-07-09 20:21:21 +0100

    staging: gdm72xx: move T_CAPABILITY definitions to hci.h
    Signed-off-by: Michalis Pappas <mpappas@fastmail.fm>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm72xx/hci.h
+++ drivers/staging/gdm72xx/hci.h
@@ -199,6 +199,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define T_FFTSIZE			(0xda	| (4 << 16))
 #define T_DUPLEX_MODE			(0xdb	| (4 << 16))
 
+/* T_CAPABILITY */
+#define T_CAPABILITY_MULTI_CS		(1 << 0)
+#define T_CAPABILITY_WIMAX		(1 << 1)
+#define T_CAPABILITY_QOS		(1 << 2)
+#define T_CAPABILITY_AGGREGATION	(1 << 3)
+
 struct hci_s {
 	__be16	cmd_evt;
 	__be16	length;
