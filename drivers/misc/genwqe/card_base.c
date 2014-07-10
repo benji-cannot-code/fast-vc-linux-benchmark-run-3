8cd4a2b7c80c4856f5888b6bc1fa6a6be3712eb1
Kleber Sacilotto de Souza <klebers@linux.vnet.ibm.com>
2014-07-10 10:17:08 -0300

    GenWQE: Remove unnecessary include
    The include for the UAPI header file from card_base.c can be removed
    since it's already included on card_base.h.
    
    Signed-off-by: Kleber Sacilotto de Souza <klebers@linux.vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/genwqe/card_base.c
+++ drivers/misc/genwqe/card_base.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/notifier.h>
 #include <linux/device.h>
 #include <linux/log2.h>
-#include <linux/genwqe/genwqe_card.h>
 
 #include "card_base.h"
 #include "card_ddcb.h"
