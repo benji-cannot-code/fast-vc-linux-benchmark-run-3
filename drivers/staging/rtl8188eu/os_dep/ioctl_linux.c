69929592ffbef4e6b48ca9ecf55344e61d99b1b1
navin patidar <navin.patidar@gmail.com>
2014-07-10 09:11:24 +0530

    staging: rtl8188eu: Remove files rtw_mp_ioctl.[c,h]
    files are not required after removal of private ioctl handlers.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/ioctl_linux.c
+++ drivers/staging/rtl8188eu/os_dep/ioctl_linux.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <rtw_mlme_ext.h>
 #include <rtw_ioctl.h>
 #include <rtw_ioctl_set.h>
-#include <rtw_mp_ioctl.h>
 #include <rtl8188e_hal.h>
 
 #include <rtw_mp.h>
