47317a260303a1cac38100fb112fdf5ee7fbc40b
Sachin Kamat <sachin.kamat@samsung.com>
2014-07-10 17:33:27 +0530

    pinctrl: samsung: Group all drivers in a sub-dir
    Group all pin control drivers of Samsung platform together in
    a sub-directory for easy maintenance.
    
    Signed-off-by: Sachin Kamat <sachin.kamat@samsung.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/samsung/pinctrl-exynos5440.c
+++ drivers/pinctrl/samsung/pinctrl-exynos5440.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/irqdomain.h>
 #include <linux/of_irq.h>
-#include "core.h"
+#include "../core.h"
 
 /* EXYNOS5440 GPIO and Pinctrl register offsets */
 #define GPIO_MUX		0x00
