e0b23e7cb2bd247cf1ea561ef2d805b32f7366c9
Antti Palosaari <crope@iki.fi>
2014-07-10 08:17:58 -0300

    [media] m88ds3103: fix SNR reporting on 32-bit arch
    There was 32-bit calculation overflow. Use div_u64.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/dvb-frontends/m88ds3103_priv.h
+++ drivers/media/dvb-frontends/m88ds3103_priv.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_math.h"
 #include <linux/firmware.h>
 #include <linux/i2c-mux.h>
+#include <linux/math64.h>
 
 #define M88DS3103_FIRMWARE "dvb-demod-m88ds3103.fw"
 #define M88DS3103_MCLK_KHZ 96000
