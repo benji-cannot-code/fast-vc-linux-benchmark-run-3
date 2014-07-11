8c3a057dad101f25726d28c688f432d718344b6c
Geert Uytterhoeven <geert+renesas@glider.be>
2014-07-11 18:13:25 +0200

    spi: topcliff-pch: Update error messages for dmaengine_prep_slave_sg() API
    Commit 16052827d98fbc13c31ebad560af4bd53e2b4dd5 ("dmaengine/dma_slave:
    introduce inline wrappers") changed the code to use the new API, but forgot
    to update the error messages.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    --
    v2:
      - New
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-topcliff-pch.c
+++ drivers/spi/spi-topcliff-pch.c
@@ -1048,8 +1048,8 @@ static void pch_spi_handle_dma(struct pch_spi_data *data, int *bpw)
 					num, DMA_DEV_TO_MEM,
 					DMA_PREP_INTERRUPT | DMA_CTRL_ACK);
 	if (!desc_rx) {
-		dev_err(&data->master->dev, "%s:device_prep_slave_sg Failed\n",
-			__func__);
+		dev_err(&data->master->dev,
+			"%s:dmaengine_prep_slave_sg Failed\n", __func__);
 		return;
 	}
 	dma_sync_sg_for_device(&data->master->dev, sg, num, DMA_FROM_DEVICE);
@@ -1107,8 +1107,8 @@ static void pch_spi_handle_dma(struct pch_spi_data *data, int *bpw)
 					sg, num, DMA_MEM_TO_DEV,
 					DMA_PREP_INTERRUPT | DMA_CTRL_ACK);
 	if (!desc_tx) {
-		dev_err(&data->master->dev, "%s:device_prep_slave_sg Failed\n",
-			__func__);
+		dev_err(&data->master->dev,
+			"%s:dmaengine_prep_slave_sg Failed\n", __func__);
 		return;
 	}
 	dma_sync_sg_for_device(&data->master->dev, sg, num, DMA_TO_DEVICE);
