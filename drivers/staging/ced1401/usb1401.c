cecba4491793b419233e7ebfb44c00c3de075e53
Luca Ellero <luca.ellero@brickedbrain.com>
2014-07-11 16:56:53 +0200

    staging: ced1401: usb1401.c: join quoted strings
    Fix checkpatch warnings "quoted string split across lines" in file usb1401.c
    
    Signed-off-by: Luca Ellero <luca.ellero@brickedbrain.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ced1401/usb1401.c
+++ drivers/staging/ced1401/usb1401.c
@@ -628,8 +628,7 @@ static void staged_callback(struct urb *urb)
 					ta->blocks[1].size +=
 					    ced->staged_length;
 					dev_dbg(&ced->interface->dev,
-						"RWM_Complete, circ block 1 "
-						"now %d bytes at %d\n",
+						"RWM_Complete, circ block 1 now %d bytes at %d\n",
 						ta->blocks[1].size,
 						ta->blocks[1].offset);
 				} else {
@@ -641,8 +640,7 @@ static void staged_callback(struct urb *urb)
 					ta->blocks[1].size =
 					    ced->staged_length;
 					dev_err(&ced->interface->dev,
-						"%s: ERROR, circ block 1 "
-						"re-started %d bytes at %d\n",
+						"%s: ERROR, circ block 1 re-started %d bytes at %d\n",
 						__func__,
 						ta->blocks[1].size,
 						ta->blocks[1].offset);
@@ -661,9 +659,7 @@ static void staged_callback(struct urb *urb)
 						ta->blocks[0].size +=
 							ced->staged_length;
 						dev_dbg(&ced->interface->dev,
-							"RWM_Complete, circ "
-							"block 0 now %d bytes "
-							"at %d\n",
+							"RWM_Complete, circ block 0 now %d bytes at %d\n",
 							ta->blocks[0].size,
 							ta->blocks[0].offset);
 
@@ -674,9 +670,7 @@ static void staged_callback(struct urb *urb)
 						ta->blocks[1].size =
 						    ced->staged_length;
 						dev_dbg(&ced->interface->dev,
-							"RWM_Complete, circ "
-							"block 1 started %d "
-							"bytes at %d\n",
+							"RWM_Complete, circ block 1 started %d bytes at %d\n",
 							ta->blocks[1].size,
 							ta->blocks[1].offset);
 					}
@@ -687,8 +681,7 @@ static void staged_callback(struct urb *urb)
 					ta->blocks[0].size =
 					    ced->staged_length;
 					dev_dbg(&ced->interface->dev,
-						"RWM_Complete, circ block 0 "
-						"started %d bytes at %d\n",
+						"RWM_Complete, circ block 0 started %d bytes at %d\n",
 						ta->blocks[0].size,
 						ta->blocks[0].offset);
 				}
@@ -697,8 +690,7 @@ static void staged_callback(struct urb *urb)
 
 		if (!cancel) { /*  Don't generate an event if cancelled */
 			dev_dbg(&ced->interface->dev,
-				"RWM_Complete,  bCircular %d, bToHost %d, "
-				"eStart %d, eSize %d\n",
+				"RWM_Complete,  bCircular %d, bToHost %d, eStart %d, eSize %d\n",
 				ta->circular, ta->event_to_host,
 				ta->event_st, ta->event_sz);
 			/* Set a user-mode event...           */
@@ -753,8 +745,7 @@ static void staged_callback(struct urb *urb)
 				int retval;
 
 				dev_info(&ced->interface->dev,
-					 "*** RWM_Complete *** pending transfer"
-					 " will now be set up!!!\n");
+					 "*** RWM_Complete *** pending transfer will now be set up!!!\n");
 				retval =
 				    ced_read_write_mem(ced,
 						       !ced->dma_info.outward,
