9c6f80236ed08f70c1b61f8e4872c94cc86c17b0
Siva Yerramreddy <yshivakrishna@gmail.com>
2014-07-11 14:04:23 -0700

    misc: mic: add dma support in host driver
    This patch adds a dma device on the mic virtual bus and uses this dmaengine
    to transfer data for virtio devices
    
    Reviewed-by: Nikhil Rao <nikhil.rao@intel.com>
    Signed-off-by: Sudeep Dutt <sudeep.dutt@intel.com>
    Signed-off-by: Ashutosh Dixit <ashutosh.dixit@intel.com>
    Signed-off-by: Siva Yerramreddy <yshivakrishna@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mic/host/mic_intr.h
+++ drivers/misc/mic/host/mic_intr.h
@@ -28,8 +28,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The minimum number of msix vectors required for normal operation.
  * 3 for virtio network, console and block devices.
  * 1 for card shutdown notifications.
+ * 4 for host owned DMA channels.
  */
-#define MIC_MIN_MSIX 4
+#define MIC_MIN_MSIX 8
 #define MIC_NUM_OFFSETS 32
 
 /**
