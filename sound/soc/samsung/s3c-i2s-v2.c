e1ad71ab1d0393179a251ee1d52c089eb7c532ae
Arnd Bergmann <arnd@arndb.de>
2014-07-11 15:45:08 +0200

    ASoC: samsung: remove unused DMA data
    The s3c_dma_client structures and the 'ch' and 'ops' members in
    s3c_dma_params were only used by the legacy DMA driver and serve
    no function any more. This removes any reference to them.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/samsung/s3c-i2s-v2.c
+++ sound/soc/samsung/s3c-i2s-v2.c
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 #include <sound/pcm_params.h>
 
-#include <mach/dma.h>
-
 #include "regs-i2s-v2.h"
 #include "s3c-i2s-v2.h"
 #include "dma.h"
