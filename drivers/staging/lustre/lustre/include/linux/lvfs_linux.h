4ab54cde77b7e95bac3567d66abd92354f5bace3
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-07-11 22:56:16 -0700

    staging: lustre: remove l_file define
    It's a "file" structure, so use it instead of trying to rename it.
    
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Cc: Oleg Drokin <oleg.drokin@intel.com>
    Cc: hpdd-discuss <hpdd-discuss@lists.01.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/linux/lvfs_linux.h
+++ drivers/staging/lustre/lustre/include/linux/lvfs_linux.h
@@ -44,12 +44,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../lvfs.h"
 
-#define l_file file
-
 #define l_filp_open filp_open
 
 struct lvfs_run_ctxt;
-struct l_file *l_dentry_open(struct lvfs_run_ctxt *, struct dentry *,
+struct file *l_dentry_open(struct lvfs_run_ctxt *, struct dentry *,
 			     int flags);
 
 struct l_linux_dirent {
