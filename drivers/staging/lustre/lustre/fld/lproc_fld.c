3ef5e2ec5156cc52cd5506742251f9081ef1c656
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-07-11 21:51:13 -0700

    staging: lustre: fld: remove ccflags from Makefile
    Also fix up the relative paths in the .c files to properly build
    now
    
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Cc: Oleg Drokin <oleg.drokin@intel.com>
    Cc: hpdd-discuss <hpdd-discuss@lists.01.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/fld/lproc_fld.c
+++ drivers/staging/lustre/lustre/fld/lproc_fld.c
@@ -45,16 +45,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUG_SUBSYSTEM S_FLD
 
 #include "../../include/linux/libcfs/libcfs.h"
-# include <linux/module.h>
+#include <linux/module.h>
 
-#include <obd.h>
-#include <obd_class.h>
-#include <dt_object.h>
-#include <md_object.h>
-#include <obd_support.h>
-#include <lustre_req_layout.h>
-#include <lustre_fld.h>
-#include <lustre_fid.h>
+#include "../include/obd.h"
+#include "../include/obd_class.h"
+#include "../include/dt_object.h"
+#include "../include/md_object.h"
+#include "../include/obd_support.h"
+#include "../include/lustre_req_layout.h"
+#include "../include/lustre_fld.h"
+#include "../include/lustre_fid.h"
 #include "fld_internal.h"
 
 static int
