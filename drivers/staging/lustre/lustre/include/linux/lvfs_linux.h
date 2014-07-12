56d430fdd0997882bd1d8f1afeb046410b7440a8
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-07-11 22:57:40 -0700

    staging: lustre: remove l_filep_open define
    Just call filep_open(), no need to rename the thing.
    
    Cc: Andreas Dilger <andreas.dilger@intel.com>
    Cc: Oleg Drokin <oleg.drokin@intel.com>
    Cc: hpdd-discuss <hpdd-discuss@lists.01.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/linux/lvfs_linux.h
+++ drivers/staging/lustre/lustre/include/linux/lvfs_linux.h
@@ -44,8 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../lvfs.h"
 
-#define l_filp_open filp_open
-
 struct lvfs_run_ctxt;
 struct file *l_dentry_open(struct lvfs_run_ctxt *, struct dentry *,
 			     int flags);
