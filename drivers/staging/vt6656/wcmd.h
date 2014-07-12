974b490313c0edf5c35a43ba4dcebe4c13c5ce41
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-12 07:53:43 +0100

    staging: vt6656: remove unused variables from struct tagCMD_ITEM
    Also remove local variables
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/wcmd.h
+++ drivers/staging/vt6656/wcmd.h
@@ -59,10 +59,6 @@ typedef enum tagCMD_STATUS {
 
 typedef struct tagCMD_ITEM {
     CMD_CODE eCmd;
-    bool     bNeedRadioOFF;
-    bool     bRadioCmd;
-    bool     bForceSCAN;
-    u16     wDeAuthenReason;
 } CMD_ITEM, *PCMD_ITEM;
 
 // Command state
