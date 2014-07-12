61b0c04abaa5ad5e34172cb2ab3615000014b1b4
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-12 11:30:01 +0100

    staging: vt6656: rf.c remove dead code RFaby11aChannelIndex
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/rf.h
+++ drivers/staging/vt6656/rf.h
@@ -56,8 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VNT_RF_MAX_POWER    0x3f
 
-extern const u8 RFaby11aChannelIndex[200];
-
 int vnt_rf_write_embedded(struct vnt_private *, u32);
 int vnt_rf_setpower(struct vnt_private *, u32, u32);
 int vnt_rf_set_txpower(struct vnt_private *, u8, u32);
