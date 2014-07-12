7fad03ceb3f4dcd7e2ae1d351bf8a15892075dd5
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-12 11:30:02 +0100

    staging: vt6656: vnt_rf_write_embedded add reg length and IFREGCTL_REGW
    The rf register always have  *_REG_LEN and IFREGCTL_REGW macros added.
    
    *_REG_LEN is always 23(0x17) replace them with VNT_RF_REG_LEN.
    
    Remove *_REG_LEN and IFREGCTL_REGW from tables and vnt_rf_set_txpower and
    apply VNT_RF_REG_LEN and IFREGCTL_REGW in vnt_rf_write_embedded
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/rf.h
+++ drivers/staging/vt6656/rf.h
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RF_MASK             0x7F
 
 #define VNT_RF_MAX_POWER    0x3f
+#define	VNT_RF_REG_LEN      0x17 /* 24 bit length */
 
 int vnt_rf_write_embedded(struct vnt_private *, u32);
 int vnt_rf_setpower(struct vnt_private *, u32, u32);
