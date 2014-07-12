cb8e964168ac70f4bb39108e9da6f8d3fed2aa7e
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-07-11 23:49:54 -0700

    staging: lustre: fix makefile build breakage
    Forgot to test with infiniband enabled.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd.h
+++ drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd.h
@@ -63,10 +63,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DEBUG_SUBSYSTEM S_LND
 
-#include <linux/libcfs/libcfs.h>
-#include <linux/lnet/lnet.h>
-#include <linux/lnet/lib-lnet.h>
-#include <linux/lnet/lnet-sysctl.h>
+#include "../../../include/linux/libcfs/libcfs.h"
+#include "../../../include/linux/lnet/lnet.h"
+#include "../../../include/linux/lnet/lib-lnet.h"
+#include "../../../include/linux/lnet/lnet-sysctl.h"
 
 #include <rdma/rdma_cm.h>
 #include <rdma/ib_cm.h>
