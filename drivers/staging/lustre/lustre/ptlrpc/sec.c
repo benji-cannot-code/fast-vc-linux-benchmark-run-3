6a31c263636e5d41946ca571f0e0b38528472da5
Chen Gang <gang.chen.5i5j@gmail.com>
2014-07-14 19:59:13 +0800

    drivers: staging: lustre: Use 'force_die' instead of 'die' to avoid compiling issue
    Some of architectures have already defined 'die' as macro, so can not
    use it as declaration or definition in other modules, or it will cause
    compiling issue.
    
    So use more precise name 'force_die' (like 'wrap_bulk') instead of. And
    the related error (with allmodconfig under score):
    
        CC [M]  drivers/staging/lustre/lustre/ptlrpc/sec.o
      drivers/staging/lustre/lustre/ptlrpc/sec.c: In function 'sptlrpc_cli_ctx_expire':
      drivers/staging/lustre/lustre/ptlrpc/sec.c:309:13: error: 'struct ptlrpc_ctx_ops' has no member named '__die'
        ctx->cc_ops->die(ctx, 0);
                   ^
      drivers/staging/lustre/lustre/ptlrpc/sec.c: In function 'ctx_refresh_timeout':
      drivers/staging/lustre/lustre/ptlrpc/sec.c:594:26: error: 'struct ptlrpc_ctx_ops' has no member named '__die'
         req->rq_cli_ctx->cc_ops->die(req->rq_cli_ctx, 0);
                                ^
      make[5]: *** [drivers/staging/lustre/lustre/ptlrpc/sec.o] Error 1
      make[4]: *** [drivers/staging/lustre/lustre/ptlrpc] Error 2
      make[3]: *** [drivers/staging/lustre/lustre] Error 2
      make[2]: *** [drivers/staging/lustre] Error 2
      make[1]: *** [drivers/staging] Error 2
      make: *** [drivers] Error 2
    
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/sec.c
+++ drivers/staging/lustre/lustre/ptlrpc/sec.c
@@ -306,8 +306,8 @@ EXPORT_SYMBOL(sptlrpc_cli_ctx_put);
  */
 void sptlrpc_cli_ctx_expire(struct ptlrpc_cli_ctx *ctx)
 {
-	LASSERT(ctx->cc_ops->die);
-	ctx->cc_ops->die(ctx, 0);
+	LASSERT(ctx->cc_ops->force_die);
+	ctx->cc_ops->force_die(ctx, 0);
 }
 EXPORT_SYMBOL(sptlrpc_cli_ctx_expire);
 
@@ -592,7 +592,7 @@ int ctx_refresh_timeout(void *data)
 	 * later than the context refresh expire time.
 	 */
 	if (rc == 0)
-		req->rq_cli_ctx->cc_ops->die(req->rq_cli_ctx, 0);
+		req->rq_cli_ctx->cc_ops->force_die(req->rq_cli_ctx, 0);
 	return rc;
 }
 
