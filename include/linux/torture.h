8c981661df8a84905d4e1d776013eaaa0fbfe61c
Joe Perches <joe@perches.com>
2014-07-14 09:16:15 -0400

    rcu: Use pr_alert/pr_cont for printing logs
    User pr_alert/pr_cont for printing the logs from rcutorture module directly
    instead of writing it to a buffer and then printing it. This allows us from not
    having to allocate such buffers. Also remove a resulting empty function.
    
    I tested this using the parse-torture.sh script as follows:
    
    $ dmesg | grep torture > log.txt
    $ bash parse-torture.sh log.txt test
    $
    
    There were no warnings which means that parsing went fine.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/torture.h
+++ include/linux/torture.h
@@ -52,7 +52,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Definitions for online/offline exerciser. */
 int torture_onoff_init(long ooholdoff, long oointerval);
-char *torture_onoff_stats(char *page);
+void torture_onoff_stats(void);
 bool torture_onoff_failures(void);
 
 /* Low-rider random number generator. */
