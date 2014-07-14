53e10d066acb0bc828ccb7f718112241f7d6d267
Willem de Bruijn <willemb@google.com>
2014-07-14 17:55:06 -0400

    net-timestamp: SOCK_RAW and PING timestamping
    Add SO_TIMESTAMPING to sockets of type PF_INET[6]/SOCK_RAW:
    
    Add the necessary sock_tx_timestamp calls to the datapath for RAW
    sockets (ping sockets already had these calls).
    
    Fix the IP output path to pass the timestamp flags on the first
    fragment also for these sockets. The existing code relies on
    transhdrlen != 0 to indicate a first fragment. For these sockets,
    that assumption does not hold.
    
    This fixes http://bugzilla.kernel.org/show_bug.cgi?id=77221
    
    Tested SOCK_RAW on IPv4 and IPv6, not PING.
    
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Acked-by: Richard Cochran <richardcochran@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/raw.c
+++ net/ipv4/raw.c
@@ -366,6 +366,8 @@ static int raw_send_hdrinc(struct sock *sk, struct flowi4 *fl4,
 
 	skb->ip_summed = CHECKSUM_NONE;
 
+	sock_tx_timestamp(sk, &skb_shinfo(skb)->tx_flags);
+
 	skb->transport_header = skb->network_header;
 	err = -EFAULT;
 	if (memcpy_fromiovecend((void *)iph, from, 0, length))
@@ -607,6 +609,8 @@ back_from_confirm:
 				      &rt, msg->msg_flags);
 
 	 else {
+		sock_tx_timestamp(sk, &ipc.tx_flags);
+
 		if (!ipc.addr)
 			ipc.addr = fl4.daddr;
 		lock_sock(sk);
