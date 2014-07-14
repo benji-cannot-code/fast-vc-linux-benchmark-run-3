f349765df66212a054f4296c8bad203ecb286655
Markos Chandras <markos.chandras@imgtec.com>
2014-07-14 12:43:28 +0100

    MIPS: cpu: Add new cpu option for Hardware Table Walker.
    Moreover, report hardware page table walker support as 'htw' in the ASE
    list of /proc/cpuinfo, if the core implements this feature.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/7334/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef cpu_has_eva
 #define cpu_has_eva		(cpu_data[0].options & MIPS_CPU_EVA)
 #endif
+#ifndef cpu_has_htw
+#define cpu_has_htw		(cpu_data[0].options & MIPS_CPU_HTW)
+#endif
 
 /*
  * For the moment we don't consider R6000 and R8000 so we can assume that
