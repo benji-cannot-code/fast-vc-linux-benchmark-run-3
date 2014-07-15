a500849af6fb4a8fd9956c913a8fcf34757177a6
Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
2014-07-15 14:09:57 +0100

    MIPS: kernel: cpu-probe: Detect unique RI/XI exceptions
    Detect if the core supports unique exception codes for the
    Read-Inhibit and Execute-Inhibit exceptions and set the
    option accordingly. The RI/XI exception support is detected
    by setting the 27th bit (IEC) of the PageGrain C0 register
    and reading back the value of that register to verify the
    bit is enabled.
    
    Signed-off-by: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/7340/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mipsregs.h
+++ arch/mips/include/asm/mipsregs.h
@@ -266,6 +266,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PG_XIE		(_ULCAST_(1) <<	 30)
 #define PG_ELPA		(_ULCAST_(1) <<	 29)
 #define PG_ESP		(_ULCAST_(1) <<	 28)
+#define PG_IEC		(_ULCAST_(1) <<  27)
 
 /*
  * R4x00 interrupt enable / cause bits
