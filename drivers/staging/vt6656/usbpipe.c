c16b5b2e223313d8aa5215a6f29b185f201855a5
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-15 19:54:39 +0100

    staging: vt6656: rename PIPEnsSendBulkOut to vnt_tx_context.
    We are just tx the context
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/usbpipe.c
+++ drivers/staging/vt6656/usbpipe.c
@@ -338,7 +338,7 @@ static void s_nsBulkInUsbIoCompleteRead(struct urb *urb)
  *
  */
 
-int PIPEnsSendBulkOut(struct vnt_private *priv,
+int vnt_tx_context(struct vnt_private *priv,
 				struct vnt_usb_send_context *context)
 {
 	int status;
