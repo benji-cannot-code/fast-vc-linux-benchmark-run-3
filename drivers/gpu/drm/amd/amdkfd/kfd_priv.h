5a7df165e5538161d5e7f785b37a3518697ed027
Ben Goz <ben.goz@amd.com>
2014-07-17 00:36:17 +0300

    amdkfd: Add mqd_manager module
    The mqd_manager module handles MQD data structures.
    MQD stands for Memory Queue Descriptor, which is used by the H/W to
    keep the usermode queue state in memory.
    
    v3:
    
    Removed new typedefs
    Removed pragma pack 4
    Remove cik_mqds.h file
    Changed lower_32/upper_32 calls to use linux macros
    Used new gart allocation functions
    Added documentation
    
    v4:
    
    Added missing initialization of the addr field in init_mqd()
    
    Setting the hqd persistent.preload_req bit ON so that when queues switches
    on/off, their context will kept and read from the mqd when the cp reassign
    them, and thus the dispatched workload context kept consistent without any
    interrupts.
    
    v5:
    
    Move amdkfd from drm/radeon/ to drm/amd/
    Change format of mqd structure to match latest KV firmware
    Add support for AQL queues creation to enable working with open-source HSA
    runtime.
    Various fixes
    
    Signed-off-by: Ben Goz <ben.goz@amd.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -120,6 +120,11 @@ void kfd_chardev_exit(void);
 struct device *kfd_chardev(void);
 
 
+enum kfd_preempt_type {
+	KFD_PREEMPT_TYPE_WAVEFRONT,
+	KFD_PREEMPT_TYPE_WAVEFRONT_RESET
+};
+
 /**
  * enum kfd_queue_type
  *
@@ -136,6 +141,11 @@ enum kfd_queue_type  {
 	KFD_QUEUE_TYPE_SDMA,
 	KFD_QUEUE_TYPE_HIQ,
 	KFD_QUEUE_TYPE_DIQ
+};
+
+enum kfd_queue_format {
+	KFD_QUEUE_FORMAT_PM4,
+	KFD_QUEUE_FORMAT_AQL
 };
 
 /**
@@ -184,6 +194,7 @@ enum kfd_queue_type  {
  */
 struct queue_properties {
 	enum kfd_queue_type type;
+	enum kfd_queue_format format;
 	unsigned int queue_id;
 	uint64_t queue_address;
 	uint64_t  queue_size;
@@ -241,6 +252,17 @@ struct queue {
 
 	struct kfd_process	*process;
 	struct kfd_dev		*device;
+};
+
+/*
+ * Please read the kfd_mqd_manager.h description.
+ */
+enum KFD_MQD_TYPE {
+	KFD_MQD_TYPE_CIK_COMPUTE = 0, /* for no cp scheduling */
+	KFD_MQD_TYPE_CIK_HIQ, /* for hiq */
+	KFD_MQD_TYPE_CIK_CP, /* for cp queues and diq */
+	KFD_MQD_TYPE_CIK_SDMA, /* for sdma queues */
+	KFD_MQD_TYPE_MAX
 };
 
 /* Data that is per-process-per device. */
