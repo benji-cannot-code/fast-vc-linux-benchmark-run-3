93fd68890a32aaa6d7e51cd14ad698a5b1964283
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-07-16 11:22:45 -0700

    staging: comedi: amcc_s5933.h: checkpatch.pl cleanup (space prohibited)
    Fix this checkpatch.pl warning:
    
    WARNING: space prohibited between function name and open parenthesis '('
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amcc_s5933.h
+++ drivers/staging/comedi/drivers/amcc_s5933.h
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  AMCC_OP_REG_MCSR_NVCMD  (AMCC_OP_REG_MCSR + 3)	/* Command in byte 3 */
 
 #define AMCC_FIFO_DEPTH_DWORD	8
-#define AMCC_FIFO_DEPTH_BYTES	(8 * sizeof (u32))
+#define AMCC_FIFO_DEPTH_BYTES	(8 * sizeof(u32))
 
 /****************************************************************************/
 /* AMCC - PCI Interrupt Control/Status Register                            */
