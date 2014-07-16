0afbb2e194b487001c7077995b9c96c486624eff
Oded Gabbay <oded.gabbay@amd.com>
2014-07-17 00:06:27 +0300

    amdkfd: Add binding/unbinding calls to amd_iommu driver
    This patch adds the functions to bind and unbind pasid
    from a device through the amd_iommu driver.
    
    The unbind function is called when the mm_struct of the
    process is released.
    
    The bind function is not called here because it is called
    only in the IOCTLs which are not yet implemented at this
    stage of the patchset.
    
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/log2.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
+#include <linux/amd-iommu.h>
 #include <linux/notifier.h>
 struct mm_struct;
 
@@ -164,6 +165,7 @@ static void kfd_process_wq_release(struct work_struct *work)
 
 	list_for_each_entry_safe(pdd, temp, &p->per_device_data,
 							per_device_list) {
+		amd_iommu_unbind_pasid(pdd->dev->pdev, p->pasid);
 		list_del(&pdd->per_device_list);
 
 		kfree(pdd);
@@ -317,12 +319,22 @@ struct kfd_process_device *kfd_bind_process_to_device(struct kfd_dev *dev,
 							struct kfd_process *p)
 {
 	struct kfd_process_device *pdd = kfd_get_process_device_data(dev, p, 1);
+	int err;
 
 	if (pdd == NULL)
 		return ERR_PTR(-ENOMEM);
 
 	if (pdd->bound)
 		return pdd;
+
+	err = amd_iommu_bind_pasid(dev->pdev, p->pasid, p->lead_thread);
+	if (err < 0)
+		return ERR_PTR(err);
+
+	if (err < 0) {
+		amd_iommu_unbind_pasid(dev->pdev, p->pasid);
+		return ERR_PTR(err);
+	}
 
 	pdd->bound = true;
 
