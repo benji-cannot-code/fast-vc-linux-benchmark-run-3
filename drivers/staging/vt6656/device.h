a143a3036021dd74f4543df064eea72c304f6a39
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-16 22:22:05 +0100

    staging: vt6656: remove DBG_PRT kernel messages macro and enums
    All dead code and nolonger used by driver.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/device.h
+++ drivers/staging/vt6656/device.h
@@ -186,16 +186,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* USB registers */
 #define USB_REG4			0x604
 
-#define DBG_PRT(l, p, args...) { if (l <= msglevel) printk(p, ##args); }
-
-enum {
-	MSG_LEVEL_ERR = 0,            /* Errors causing abnormal operation */
-	MSG_LEVEL_NOTICE = 1,         /* Errors needing user notification */
-	MSG_LEVEL_INFO = 2,           /* Normal message. */
-	MSG_LEVEL_VERBOSE = 3,        /* Will report all trival errors. */
-	MSG_LEVEL_DEBUG = 4           /* Only for debug purpose. */
-};
-
 #define DEVICE_INIT_COLD	0x0 /* cold init */
 #define DEVICE_INIT_RESET	0x1 /* reset init or Dx to D0 power remain */
 #define DEVICE_INIT_DXPL	0x2 /* Dx to D0 power lost init */
