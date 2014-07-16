89519ca192670266f0bc9fc6c2f0e257eb490392
Josep Puigdemont <josep.puigdemont@gmail.com>
2014-07-17 01:03:39 +0200

    staging: lustre: plain interger was used as NULL pointer
    This patch fixes the following warnings found when running sparse:
    .../lproc_echo.c:43:11: warning: Using plain integer as NULL pointer
    .../lproc_echo.c:49:11: warning: Using plain integer as NULL pointer
    
    Signed-off-by: Josep Puigdemont <josep.puigdemont@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdecho/lproc_echo.c
+++ drivers/staging/lustre/lustre/obdecho/lproc_echo.c
@@ -41,13 +41,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 LPROC_SEQ_FOPS_RO_TYPE(echo, uuid);
 static struct lprocfs_vars lprocfs_echo_obd_vars[] = {
 	{ "uuid",	 &echo_uuid_fops,	NULL, 0 },
-	{ 0 }
+	{ NULL }
 };
 
 LPROC_SEQ_FOPS_RO_TYPE(echo, numrefs);
 static struct lprocfs_vars lprocfs_echo_module_vars[] = {
 	{ "num_refs",     &echo_numrefs_fops,     NULL, 0 },
-	{ 0 }
+	{ NULL }
 };
 
 void lprocfs_echo_init_vars(struct lprocfs_static_vars *lvars)
