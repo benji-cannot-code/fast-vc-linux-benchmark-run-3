d2a93ea44addc4900872707016b6eb93b5d8e5a0
Peter Ujfalusi <peter.ujfalusi@ti.com>
2014-07-16 15:12:03 +0300

    ASoC: edma-pcm: Add empty functions for !CONFIG_SND_EDMA_SOC builds
    So drivers mixing with other platform drivers, like davinci-mcasp do not
    need to fiddle with CONFIG_SND_EDMA_SOC in their code.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Tested-by: Daniel Mack <daniel@zonque.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/davinci/edma-pcm.h
+++ sound/soc/davinci/edma-pcm.h
@@ -21,6 +21,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __EDMA_PCM_H__
 #define __EDMA_PCM_H__
 
+#if IS_ENABLED(CONFIG_SND_EDMA_SOC)
 int edma_pcm_platform_register(struct device *dev);
+#else
+static inline int edma_pcm_platform_register(struct device *dev)
+{
+	return 0;
+}
+#endif /* CONFIG_SND_EDMA_SOC */
 
 #endif /* __EDMA_PCM_H__ */
