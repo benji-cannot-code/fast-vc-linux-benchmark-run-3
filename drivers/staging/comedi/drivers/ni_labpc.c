b2ea736095873deb1012b13750be6c90cf410dcb
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-07-16 10:43:40 -0700

    staging: comedi: ni_labpc: checkpatch.pl cleanup (else is not useful)
    Fix the checkpatch.pl warning:
    
    WARNING: else is not generally useful after a break or return
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc.c
+++ drivers/staging/comedi/drivers/ni_labpc.c
@@ -1052,9 +1052,9 @@ static int labpc_8255_mmio(int dir, int port, int data, unsigned long iobase)
 	if (dir) {
 		writeb(data, (void __iomem *)(iobase + port));
 		return 0;
-	} else {
-		return readb((void __iomem *)(iobase + port));
 	}
+
+	return readb((void __iomem *)(iobase + port));
 }
 
 /* lowlevel write to eeprom/dac */
