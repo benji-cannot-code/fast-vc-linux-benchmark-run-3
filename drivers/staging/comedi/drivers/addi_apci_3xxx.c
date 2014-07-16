5f248d9a84e4fabcdfa74c6daff4aa905177fca3
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-07-16 10:43:17 -0700

    staging: comedi: addi_apci_3xxx: checkpatch.pl cleanup (else after return)
    Fix the checkpatch.pl warning in this file:
    
    WARNING: else is not generally useful after a break or return
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi_apci_3xxx.c
+++ drivers/staging/comedi/drivers/addi_apci_3xxx.c
@@ -390,9 +390,8 @@ static int apci3xxx_ai_started(struct comedi_device *dev)
 
 	if ((readl(devpriv->mmio + 8) & 0x80000) == 0x80000)
 		return 1;
-	else
-		return 0;
 
+	return 0;
 }
 
 static int apci3xxx_ai_setup(struct comedi_device *dev, unsigned int chanspec)
@@ -697,10 +696,9 @@ static int apci3xxx_dio_insn_config(struct comedi_device *dev,
 		/* ignore all other instructions for ports 0 and 1 */
 		if (chan < 16)
 			return -EINVAL;
-		else
-			/* changing any channel in port 2 */
-			/* changes the entire port        */
-			mask = 0xff0000;
+
+		/* changing any channel in port 2 changes the entire port */
+		mask = 0xff0000;
 	}
 
 	ret = comedi_dio_insn_config(dev, s, insn, data, mask);
