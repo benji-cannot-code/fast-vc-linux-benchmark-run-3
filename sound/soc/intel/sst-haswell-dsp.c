39e7825f81e5b931a6e3c9be7f04fbac0359234f
Lv Zheng <lv.zheng@intel.com>
2014-07-16 16:59:02 +0800

    ASoC: intel: Cleanup useless ACPI inclusion.
    The sst-haswell-dsp.c is an ACPI independent file, this patch removes
    ACPI header files for it.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/intel/sst-haswell-dsp.c
+++ sound/soc/intel/sst-haswell-dsp.c
@@ -29,9 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 #include <linux/pm_runtime.h>
 
-#include <linux/acpi.h>
-#include <acpi/acpi_bus.h>
-
 #include "sst-dsp.h"
 #include "sst-dsp-priv.h"
 #include "sst-haswell-ipc.h"
