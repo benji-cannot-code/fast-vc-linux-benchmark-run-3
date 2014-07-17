eab377235c366022f1d182fa498dadf89a62f436
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-07-17 22:59:53 +0200

    staging: rtl8723au: Use proper name REG_CAMCMD and remove wrapper invalidate_cam_all23a()
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/include/rtl8723a_spec.h
+++ drivers/staging/rtl8723au/include/rtl8723a_spec.h
@@ -455,8 +455,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  */
 /*  9. Security Control Registers	(Offset: ) */
 /*  */
-	/* IN 8190 Data Sheet is called CAMcmd */
-#define	RWCAM				REG_CAMCMD
 	/*  Software write CAM input content */
 #define	WCAMI				REG_CAMWRITE
 	/*  Software read/write CAM config */
