0d1032effc441e784c10dd1b24879bbbaec0d838
Mark Fasheh <mfasheh@suse.de>
2014-07-17 12:39:00 -0700

    btrfs: add trace for qgroup accounting
    We want this to debug qgroup changes on live systems.
    
    Signed-off-by: Mark Fasheh <mfasheh@suse.de>
    Reviewed-by: Josef Bacik <jbacik@fb.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "backref.h"
 #include "tests/btrfs-tests.h"
 
+#include "qgroup.h"
 #define CREATE_TRACE_POINTS
 #include <trace/events/btrfs.h>
 
