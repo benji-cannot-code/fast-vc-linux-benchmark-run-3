23697c4ba9e585f2273bfa704acf489edd87c56b
Antti Palosaari <crope@iki.fi>
2014-07-17 21:47:48 -0300

    [media] airspy: print notice to point SDR API is not 100% stable yet
    Print notice on driver load: "SDR API is still slightly
    experimental and functionality changes may follow". It is just
    remind possible used SDR API is very new and surprises may occur.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/usb/airspy/airspy.c
+++ drivers/media/usb/airspy/airspy.c
@@ -1085,7 +1085,9 @@ static int airspy_probe(struct usb_interface *intf,
 	}
 	dev_info(&s->udev->dev, "Registered as %s\n",
 			video_device_node_name(&s->vdev));
-
+	dev_notice(&s->udev->dev,
+			"%s: SDR API is still slightly experimental and functionality changes may follow\n",
+			KBUILD_MODNAME);
 	return 0;
 
 err_free_controls:
