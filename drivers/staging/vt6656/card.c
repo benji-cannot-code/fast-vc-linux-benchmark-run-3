596518805fe8d65eab28e5bc664482078fae01f4
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-19 12:30:13 +0100

    staging: vt6656: struct vnt_private rename qwCurrTSF to current_tsf
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/card.c
+++ drivers/staging/vt6656/card.c
@@ -584,7 +584,7 @@ void vnt_adjust_tsf(struct vnt_private *priv, u8 rx_rate,
 bool vnt_get_current_tsf(struct vnt_private *priv, u64 *current_tsf)
 {
 
-	*current_tsf = priv->qwCurrTSF;
+	*current_tsf = priv->current_tsf;
 
 	return true;
 }
@@ -605,7 +605,7 @@ bool vnt_clear_current_tsf(struct vnt_private *priv)
 
 	vnt_mac_reg_bits_on(priv, MAC_REG_TFTCTL, TFTCTL_TSFCNTRST);
 
-	priv->qwCurrTSF = 0;
+	priv->current_tsf = 0;
 
 	return true;
 }
