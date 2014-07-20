4d3d28eef51d5e6be4ca954458c40991859e25ed
Igor Bezukh <igbzkh@gmail.com>
2014-07-20 12:34:00 +0300

    Staging: vt6655: Remove redundant comments in wcmd.h
    Removed redundant comments in the header file wcmd.h
    
    Signed-off-by: Igor Bezukh <igbzkh@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/wcmd.h
+++ drivers/staging/vt6655/wcmd.h
@@ -34,12 +34,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "80211hdr.h"
 #include "80211mgr.h"
 
-/*---------------------  Export Definitions -------------------------*/
+#define AUTHENTICATE_TIMEOUT   1000
+#define ASSOCIATE_TIMEOUT      1000
 
-#define AUTHENTICATE_TIMEOUT   1000 //ms
-#define ASSOCIATE_TIMEOUT      1000 //ms
-
-// Command code
 typedef enum tagCMD_CODE {
 	WLAN_CMD_BSSID_SCAN,
 	WLAN_CMD_SSID,
@@ -77,7 +74,6 @@ typedef struct tagCMD_ITEM {
 	bool bForceSCAN;
 } CMD_ITEM, *PCMD_ITEM;
 
-// Command state
 typedef enum tagCMD_STATE {
 	WLAN_CMD_SCAN_START,
 	WLAN_CMD_SCAN_END,
@@ -93,13 +89,6 @@ typedef enum tagCMD_STATE {
 	WLAN_CMD_IDLE
 } CMD_STATE, *PCMD_STATE;
 
-/*---------------------  Export Classes  ----------------------------*/
-
-/*---------------------  Export Variables  --------------------------*/
-
-/*---------------------  Export Types  ------------------------------*/
-
-/*---------------------  Export Functions  --------------------------*/
 void
 vResetCommandTimer(
 	void *hDeviceContext
