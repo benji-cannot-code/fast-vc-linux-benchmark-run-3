498141dd860690f12e7bd7b1e3d855b17768ef01
Fabian Frederick <fabf@skynet.be>
2014-07-20 16:16:11 +0200

    brcm80211: remove unnecessary break after return
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/brcm80211/brcmsmac/phy/phy_n.c
+++ drivers/net/wireless/brcm80211/brcmsmac/phy/phy_n.c
@@ -22917,7 +22917,6 @@ static void wlc_phy_rssi_cal_nphy_rev2(struct brcms_phy *pi, u8 rssi_type)
 		break;
 	default:
 		return;
-		break;
 	}
 
 	classif_state = wlc_phy_classifier_nphy(pi, 0, 0);
