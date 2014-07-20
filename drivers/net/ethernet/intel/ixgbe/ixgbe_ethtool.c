df4a547bd4856a1283569249a5ac099b72f27845
Fabian Frederick <fabf@skynet.be>
2014-07-20 16:25:19 +0200

    ixgbe: remove unnecessary break after return
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_ethtool.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_ethtool.c
@@ -1409,7 +1409,6 @@ static int ixgbe_reg_test(struct ixgbe_adapter *adapter, u64 *data)
 	default:
 		*data = 1;
 		return 1;
-		break;
 	}
 
 	/*
@@ -2867,7 +2866,6 @@ static int ixgbe_get_ts_info(struct net_device *dev,
 		break;
 	default:
 		return ethtool_op_get_ts_info(dev, info);
-		break;
 	}
 	return 0;
 }
