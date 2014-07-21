9004772e965399f729250336279a64eca0cb262f
Lee Jones <lee.jones@linaro.org>
2014-07-21 14:16:23 +0100

    mfd: max8925-core: Fix 'missing blank line after declarations' warning
    This is part of an effort to clean-up the MFD subsystem.
    
    WARNING: Missing a blank line after declarations
    +       struct max8925_chip *chip = irq_data_get_irq_chip_data(data);
    +       max8925_irqs[data->irq - chip->irq_base].enable
    
    WARNING: Missing a blank line after declarations
    +       struct max8925_chip *chip = irq_data_get_irq_chip_data(data);
    +       max8925_irqs[data->irq - chip->irq_base].enable = 0;
    
    total: 0 errors, 2 warnings, 927 lines checked
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/max8925-core.c
+++ drivers/mfd/max8925-core.c
@@ -625,6 +625,7 @@ static void max8925_irq_sync_unlock(struct irq_data *data)
 static void max8925_irq_enable(struct irq_data *data)
 {
 	struct max8925_chip *chip = irq_data_get_irq_chip_data(data);
+
 	max8925_irqs[data->irq - chip->irq_base].enable
 		= max8925_irqs[data->irq - chip->irq_base].offs;
 }
@@ -632,6 +633,7 @@ static void max8925_irq_enable(struct irq_data *data)
 static void max8925_irq_disable(struct irq_data *data)
 {
 	struct max8925_chip *chip = irq_data_get_irq_chip_data(data);
+
 	max8925_irqs[data->irq - chip->irq_base].enable = 0;
 }
 
