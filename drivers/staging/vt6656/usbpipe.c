a3e7bc66e581f02375d87b583d3471d97440e75c
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-21 22:40:43 +0100

    staging: vt6656: Remove static declaration vnt_submit_rx_urb_complete
    Moving vnt_submit_rx_urb in visibility of vnt_submit_rx_urb_complete
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/usbpipe.c
+++ drivers/staging/vt6656/usbpipe.c
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define URB_ASYNC_UNLINK    0
 #endif
 
-static void vnt_submit_rx_urb_complete(struct urb *urb);
 static void vnt_tx_context_complete(struct urb *urb);
 
 int vnt_control_out(struct vnt_private *priv, u8 request, u16 value,
@@ -182,36 +181,6 @@ int vnt_start_interrupt_urb(struct vnt_private *priv)
 	return status;
 }
 
-int vnt_submit_rx_urb(struct vnt_private *priv, struct vnt_rcb *rcb)
-{
-	int status = 0;
-	struct urb *urb;
-
-	urb = rcb->urb;
-	if (rcb->skb == NULL) {
-		dev_dbg(&priv->usb->dev, "rcb->skb is null\n");
-		return status;
-	}
-
-	usb_fill_bulk_urb(urb,
-		priv->usb,
-		usb_rcvbulkpipe(priv->usb, 2),
-		skb_put(rcb->skb, skb_tailroom(rcb->skb)),
-		MAX_TOTAL_SIZE_WITH_ALL_HEADERS,
-		vnt_submit_rx_urb_complete,
-		rcb);
-
-	status = usb_submit_urb(urb, GFP_ATOMIC);
-	if (status != 0) {
-		dev_dbg(&priv->usb->dev, "Submit Rx URB failed %d\n", status);
-		return STATUS_FAILURE ;
-	}
-
-	rcb->in_use = true;
-
-	return status;
-}
-
 static void vnt_submit_rx_urb_complete(struct urb *urb)
 {
 	struct vnt_rcb *rcb = urb->context;
@@ -263,6 +232,37 @@ static void vnt_submit_rx_urb_complete(struct urb *urb)
 
 	return;
 }
+
+int vnt_submit_rx_urb(struct vnt_private *priv, struct vnt_rcb *rcb)
+{
+	int status = 0;
+	struct urb *urb;
+
+	urb = rcb->urb;
+	if (rcb->skb == NULL) {
+		dev_dbg(&priv->usb->dev, "rcb->skb is null\n");
+		return status;
+	}
+
+	usb_fill_bulk_urb(urb,
+			  priv->usb,
+			  usb_rcvbulkpipe(priv->usb, 2),
+			  skb_put(rcb->skb, skb_tailroom(rcb->skb)),
+			  MAX_TOTAL_SIZE_WITH_ALL_HEADERS,
+			  vnt_submit_rx_urb_complete,
+			  rcb);
+
+	status = usb_submit_urb(urb, GFP_ATOMIC);
+	if (status != 0) {
+		dev_dbg(&priv->usb->dev, "Submit Rx URB failed %d\n", status);
+		return STATUS_FAILURE;
+	}
+
+	rcb->in_use = true;
+
+	return status;
+}
+
 
 int vnt_tx_context(struct vnt_private *priv,
 				struct vnt_usb_send_context *context)
