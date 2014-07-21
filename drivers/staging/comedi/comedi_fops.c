c2c253e25042f86b8f91c1048e7d1d1418b67c92
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-07-21 11:01:26 -0700

    staging: comedi: comedi_fops: remove disabled subdevice lock/unlock callback
    According to Ian Abbott, this code was added in comedi-0.7.6 back in June 1999
    and has always been disabled.
    
    The 'lock_f' and 'unlock' members don't even exist in the comedi_subdevice.
    Just remove the disabled code.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_fops.c
+++ drivers/staging/comedi/comedi_fops.c
@@ -1661,14 +1661,6 @@ static int do_lock_ioctl(struct comedi_device *dev, unsigned int arg,
 		s->lock = file;
 	spin_unlock_irqrestore(&s->spin_lock, flags);
 
-#if 0
-	if (ret < 0)
-		return ret;
-
-	if (s->lock_f)
-		ret = s->lock_f(dev, s);
-#endif
-
 	return ret;
 }
 
@@ -1703,14 +1695,8 @@ static int do_unlock_ioctl(struct comedi_device *dev, unsigned int arg,
 	if (s->lock && s->lock != file)
 		return -EACCES;
 
-	if (s->lock == file) {
-#if 0
-		if (s->unlock)
-			s->unlock(dev, s);
-#endif
-
+	if (s->lock == file)
 		s->lock = NULL;
-	}
 
 	return 0;
 }
