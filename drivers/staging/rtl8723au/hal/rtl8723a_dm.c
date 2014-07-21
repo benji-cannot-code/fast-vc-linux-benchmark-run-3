099661ae6eee138defa6c9a00409e5e19e83bb51
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-07-21 11:24:47 +0200

    staging: rtl8723au: Remove no-op function dm_CheckStatistics()
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/hal/rtl8723a_dm.c
+++ drivers/staging/rtl8723au/hal/rtl8723a_dm.c
@@ -35,10 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  Global var */
 /*  */
 
-static void dm_CheckStatistics(struct rtw_adapter *Adapter)
-{
-}
-
 static void dm_CheckPbcGPIO(struct rtw_adapter *padapter)
 {
 	u8	tmp1byte;
@@ -226,9 +222,6 @@ rtl8723a_HalDmWatchDog(
 	bFwPSAwake = rtl8723a_get_fwlps_rf_on(Adapter);
 
 	if (!bFwCurrentInPSMode && bFwPSAwake) {
-		/*  Calculate Tx/Rx statistics. */
-		dm_CheckStatistics(Adapter);
-
 		/*  Read REG_INIDATA_RATE_SEL value for TXDESC. */
 		if (check_fwstate(&Adapter->mlmepriv, WIFI_STATION_STATE)) {
 			pdmpriv->INIDATA_RATE[0] = rtl8723au_read8(Adapter, REG_INIDATA_RATE_SEL) & 0x3f;
