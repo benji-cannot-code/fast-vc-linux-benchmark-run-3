e66c3127cb978c8c6ce32607e5c85800941d570c
Brian Norris <computersforpeace@gmail.com>
2014-07-21 19:07:02 -0700

    mtd: mtdswap: fix integer overflow
    Caught by Coverity.
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/mtdswap.c
+++ drivers/mtd/mtdswap.c
@@ -1475,7 +1475,7 @@ static void mtdswap_add_mtd(struct mtd_blktrans_ops *tr, struct mtd_info *mtd)
 	}
 
 	eblocks = mtd_div_by_eb(use_size, mtd);
-	use_size = eblocks * mtd->erasesize;
+	use_size = (uint64_t)eblocks * mtd->erasesize;
 	bad_blocks = mtdswap_badblocks(mtd, use_size);
 	eavailable = eblocks - bad_blocks;
 
