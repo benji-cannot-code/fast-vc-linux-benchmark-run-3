1f33ad1110b16f847a58c932ccac28fd20fe30bc
Tomasz Nowicki <tomasz.nowicki@linaro.org>
2014-07-22 11:20:13 +0200

    acpi, apei, ghes: Factor out ioremap virtual memory for IRQ and NMI context.
    GHES currently maps two pages with atomic_ioremap.  From now
    on, NMI is architectural depended so there is no need to allocate
    an NMI page for platforms without NMI support.
    
    To make it possible to not use a second page, swap the existing
    page order so that the IRQ context page is first, and the optional
    NMI context page is second.  Then, use HAVE_ACPI_APEI_NMI to decide
    how many pages are to be allocated.
    
    Signed-off-by: Tomasz Nowicki <tomasz.nowicki@linaro.org>
    Acked-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/kernel/acpi/apei.c
+++ arch/x86/kernel/acpi/apei.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <acpi/apei.h>
 
 #include <asm/mce.h>
+#include <asm/tlbflush.h>
 
 int arch_apei_enable_cmcff(struct acpi_hest_header *hest_hdr, void *data)
 {
@@ -54,4 +55,9 @@ void arch_apei_report_mem_error(int sev, struct cper_sec_mem_err *mem_err)
 #ifdef CONFIG_X86_MCE
 	apei_mce_report_mem_error(sev, mem_err);
 #endif
+}
+
+void arch_apei_flush_tlb_one(unsigned long addr)
+{
+	__flush_tlb_one(addr);
 }
