642a7871fdaa740a0c58d8d1ad430985a2406998
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2014-07-22 11:50:22 +0100

    ASoC: wm5110: Set symmetric samplebits on AIFs
    Different playback and capture bits-per-sample
    are not supported on the AIFs
    
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/codecs/wm5110.c
+++ sound/soc/codecs/wm5110.c
@@ -1486,6 +1486,7 @@ static struct snd_soc_dai_driver wm5110_dai[] = {
 		 },
 		.ops = &arizona_dai_ops,
 		.symmetric_rates = 1,
+		.symmetric_samplebits = 1,
 	},
 	{
 		.name = "wm5110-aif2",
@@ -1507,6 +1508,7 @@ static struct snd_soc_dai_driver wm5110_dai[] = {
 		 },
 		.ops = &arizona_dai_ops,
 		.symmetric_rates = 1,
+		.symmetric_samplebits = 1,
 	},
 	{
 		.name = "wm5110-aif3",
@@ -1528,6 +1530,7 @@ static struct snd_soc_dai_driver wm5110_dai[] = {
 		 },
 		.ops = &arizona_dai_ops,
 		.symmetric_rates = 1,
+		.symmetric_samplebits = 1,
 	},
 	{
 		.name = "wm5110-slim1",
