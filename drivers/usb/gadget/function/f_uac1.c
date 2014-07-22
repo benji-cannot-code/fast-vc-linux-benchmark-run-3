281889f2fe9a2e2b79246ceaf23afafea51203eb
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2014-07-22 19:58:37 +0200

    usb: gadget: f_uac1: prepare for separate compilation
    Integrating configfs requires converting f_uac1 to new function interface,
    which in turn requires converting it to the new function interface,
    which involves separate compilation of f_uac1.c into usb_f_uac1.ko.
    u_uac1.c contains some module parameters. After this patch is applied
    they are still a part of the resulting g_audio.ko, but can be guarded
    with a compatiblity flag which will be removed when no users of the old
    function interface of f_uac1 are left.
    
    Tested-by: Sebastian Reimers <sebastian.reimers@googlemail.com>
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/function/f_uac1.c
+++ drivers/usb/gadget/function/f_uac1.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/atomic.h>
 
 #include "u_uac1.h"
+#include "u_uac1.c"
 
 #define OUT_EP_MAX_PACKET_SIZE	200
 static int req_buf_size = OUT_EP_MAX_PACKET_SIZE;
