bf0ebce73952851d7c6ada0c2793521e4655a29a
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2014-07-23 10:30:57 -0300

    [media] omap3isp: resizer: Protect against races when updating crop
    When updating the crop rectangle during streaming, the IRQ handler will
    reprogram the resizer after the current frame. A race condition
    currently exists between the set selection operation and the IRQ
    handler: if the set selection operation is called twice in a row and the
    IRQ handler runs only during the second call, it could reprogram the
    hardware with partially updated values. Use a spinlock to protect
    against that.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/omap3isp/ispresizer.h
+++ drivers/media/platform/omap3isp/ispresizer.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef OMAP3_ISP_RESIZER_H
 #define OMAP3_ISP_RESIZER_H
 
+#include <linux/spinlock.h>
 #include <linux/types.h>
 
 /*
@@ -87,6 +88,7 @@ enum resizer_input_entity {
 
 /*
  * struct isp_res_device - OMAP3 ISP resizer module
+ * @lock: Protects formats and crop rectangles between set_selection and IRQ
  * @crop.request: Crop rectangle requested by the user
  * @crop.active: Active crop rectangle (based on hardware requirements)
  */
@@ -107,6 +109,7 @@ struct isp_res_device {
 	enum isp_pipeline_stream_state state;
 	wait_queue_head_t wait;
 	atomic_t stopping;
+	spinlock_t lock;
 
 	struct {
 		struct v4l2_rect request;
