5bef8cc0d1712e981bd998ef201182cc2935a10c
Philipp Zabel <p.zabel@pengutronix.de>
2014-07-23 12:28:38 -0300

    [media] coda: move coda driver into its own directory
    The coda driver has grown significantly and will continue to grow.
    Move the coda driver into its own directory so it can be split.
    Rename coda.h to coda_regs.h as it contains the register defines.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Kamil Debski <k.debski@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/coda/coda-common.c
+++ drivers/media/platform/coda/coda-common.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/videobuf2-core.h>
 #include <media/videobuf2-dma-contig.h>
 
-#include "coda.h"
+#include "coda_regs.h"
 
 #define CODA_NAME		"coda"
 
