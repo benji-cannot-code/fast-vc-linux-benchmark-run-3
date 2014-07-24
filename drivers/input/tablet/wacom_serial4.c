d5232cf9a5bbd8ef4caf5909c201f631db6bcab4
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2014-07-24 13:27:18 -0700

    Input: wacom_serial4 - prepare for wacom USB moving to HID
    wacom_wac.h will be moving to drivers/hid. Since we only need 3 definitions
    from it let's simply copy them over.
    
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/tablet/wacom_serial4.c
+++ drivers/input/tablet/wacom_serial4.c
@@ -114,7 +114,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serio.h>
 #include <linux/slab.h>
 #include <linux/string.h>
-#include "wacom_wac.h"
 
 MODULE_AUTHOR("Julian Squires <julian@cipht.net>, Hans de Goede <hdegoede@redhat.com>");
 MODULE_DESCRIPTION("Wacom protocol 4 serial tablet driver");
@@ -149,6 +148,11 @@ MODULE_LICENSE("GPL");
 #define F_COVERS_SCREEN		0x01
 #define F_HAS_STYLUS2		0x02
 #define F_HAS_SCROLLWHEEL	0x04
+
+/* device IDs */
+#define STYLUS_DEVICE_ID	0x02
+#define CURSOR_DEVICE_ID	0x06
+#define ERASER_DEVICE_ID	0x0A
 
 enum { STYLUS = 1, ERASER, CURSOR };
 
