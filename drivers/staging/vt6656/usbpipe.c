cde3cd43a1e9f36c13f688a580ffe01081a4c5fd
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-24 21:13:16 +0100

    staging: vt6656: struct vnt_private merge flag and macros
    merge u32 flag into unsigned long Flags
    
    Replacing fMP_DISCONNECTED with DEVICE_FLAGS_DISCONNECTED
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/usbpipe.c
+++ drivers/staging/vt6656/usbpipe.c
@@ -53,7 +53,7 @@ int vnt_control_out(struct vnt_private *priv, u8 request, u16 value,
 {
 	int status = 0;
 
-	if (priv->Flags & fMP_DISCONNECTED)
+	if (priv->flags & DEVICE_FLAGS_DISCONNECTED)
 		return STATUS_FAILURE;
 
 	mutex_lock(&priv->usb_lock);
@@ -81,7 +81,7 @@ int vnt_control_in(struct vnt_private *priv, u8 request, u16 value,
 {
 	int status;
 
-	if (priv->Flags & fMP_DISCONNECTED)
+	if (priv->flags & DEVICE_FLAGS_DISCONNECTED)
 		return STATUS_FAILURE;
 
 	mutex_lock(&priv->usb_lock);
@@ -290,7 +290,7 @@ int vnt_tx_context(struct vnt_private *priv,
 	int status;
 	struct urb *urb;
 
-	if (priv->Flags & fMP_DISCONNECTED) {
+	if (priv->flags & DEVICE_FLAGS_DISCONNECTED) {
 		context->in_use = false;
 		return STATUS_RESOURCES;
 	}
