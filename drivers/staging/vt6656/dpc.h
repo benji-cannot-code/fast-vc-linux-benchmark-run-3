78ea7e854682ccb99d57c30e810015082782613b
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-24 23:34:15 +0100

    staging: vt6656: dpc remove unneeded headers
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/dpc.h
+++ drivers/staging/vt6656/dpc.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __DPC_H__
 
 #include "device.h"
-#include "wcmd.h"
 
 int vnt_rx_data(struct vnt_private *, struct vnt_rcb *,
 	unsigned long bytes_recieved);
