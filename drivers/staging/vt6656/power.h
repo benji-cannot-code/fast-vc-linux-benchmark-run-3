3a16c14016503a2f7fd4571220683b773f5dadec
Malcolm Priestley <tvboxspy@gmail.com>
2014-07-24 23:34:17 +0100

    staging: vt6656: rf.h cleanup C_PWBT comments
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/power.h
+++ drivers/staging/vt6656/power.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __POWER_H__
 #define __POWER_H__
 
-#define     C_PWBT                   1000      // micro sec. power up before TBTT
+#define C_PWBT	1000 /* micro sec. power up before TBTT */
 
 void vnt_disable_power_saving(struct vnt_private *);
 void vnt_enable_power_saving(struct vnt_private *, u16);
