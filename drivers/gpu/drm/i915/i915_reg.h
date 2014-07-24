2a7d7498c95b0abb5476615f579f56849f27847a
Oscar Mateo <oscar.mateo@intel.com>
2014-07-24 17:04:17 +0100

    drm/i915/bdw: Populate LR contexts (somewhat)
    For the most part, logical ring context objects are similar to hardware
    contexts in that the backing object is meant to be opaque. There are
    some exceptions where we need to poke certain offsets of the object for
    initialization, updating the tail pointer or updating the PDPs.
    
    For our basic execlist implementation we'll only need our PPGTT PDs,
    and ringbuffer addresses in order to set up the context. With previous
    patches, we have both, so start prepping the context to be load.
    
    Before running a context for the first time you must populate some
    fields in the context object. These fields begin 1 PAGE + LRCA, ie. the
    first page (in 0 based counting) of the context  image. These same
    fields will be read and written to as contexts are saved and restored
    once the system is up and running.
    
    Many of these fields are completely reused from previous global
    registers: ringbuffer head/tail/control, context control matches some
    previous MI_SET_CONTEXT flags, and page directories. There are other
    fields which we don't touch which we may want in the future.
    
    v2: CTX_LRI_HEADER_0 is MI_LOAD_REGISTER_IMM(14) for render and (11)
    for other engines.
    
    v3: Several rebases and general changes to the code.
    
    v4: Squash with "Extract LR context object populating"
    Also, Damien's review comments:
    - Set the Force Posted bit on the LRI header, as the BSpec suggest we do.
    - Prevent warning when compiling a 32-bits kernel without HIGHMEM64.
    - Add a clarifying comment to the context population code.
    
    v5: Damien's review comments:
    - The third MI_LOAD_REGISTER_IMM in the context does not set Force Posted.
    - Remove dead code.
    
    v6: Add a note about the (presumed) differences between BDW and CHV state
    contexts. Also, Brad's review comments:
    - Use the _MASKED_BIT_ENABLE, upper_32_bits and lower_32_bits macros.
    - Be less magical about how we set the ring size in the context.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net> (v1)
    Signed-off-by: Rafael Barbalho <rafael.barbalho@intel.com> (v2)
    Signed-off-by: Oscar Mateo <oscar.mateo@intel.com>
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -283,6 +283,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   address/value pairs. Don't overdue it, though, x <= 2^4 must hold!
  */
 #define MI_LOAD_REGISTER_IMM(x)	MI_INSTR(0x22, 2*(x)-1)
+#define   MI_LRI_FORCE_POSTED		(1<<12)
 #define MI_STORE_REGISTER_MEM(x) MI_INSTR(0x24, 2*(x)-1)
 #define MI_STORE_REGISTER_MEM_GEN8(x) MI_INSTR(0x24, 3*(x)-1)
 #define   MI_SRM_LRM_GLOBAL_GTT		(1<<22)
