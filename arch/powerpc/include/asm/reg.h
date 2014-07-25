4fdc99c3edb0d199e044c3d62de66fd5c04c7d86
Bharat Bhushan <Bharat.Bhushan@freescale.com>
2014-07-25 11:32:43 +0530

    KVM: PPC: Remove comment saying SPRG1 is used for vcpu pointer
    Scott Wood pointed out that We are no longer using SPRG1 for vcpu pointer,
    but using SPRN_SPRG_THREAD <=> SPRG3 (thread->vcpu). So this comment
    is not valid now.
    
    Note: SPRN_SPRG3R is not supported (do not see any need as of now),
    and if we want to support this in future then we have to shift to using
    SPRG1 for VCPU pointer.
    
    Signed-off-by: Bharat Bhushan <Bharat.Bhushan@freescale.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -945,9 +945,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *      readable variant for reads, which can avoid a fault
  *      with KVM type virtualization.
  *
- *      (*) Under KVM, the host SPRG1 is used to point to
- *      the current VCPU data structure
- *
  * 32-bit 8xx:
  *	- SPRG0 scratch for exception vectors
  *	- SPRG1 scratch for exception vectors
