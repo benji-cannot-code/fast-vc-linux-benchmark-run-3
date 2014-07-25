b4a22cf3052b00536d49e05218918a86278be730
Igor Bezukh <igbzkh@gmail.com>
2014-07-25 19:20:50 +0300

    Staging: vt6655: removed redundant comments from dpc.h
    Removed redundant comments from dpc.h header file.
    
    Signed-off-by: Igor Bezukh <igbzkh@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/dpc.h
+++ drivers/staging/vt6655/dpc.h
@@ -34,14 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "device.h"
 #include "wcmd.h"
 
-/*---------------------  Export Definitions -------------------------*/
-
-/*---------------------  Export Classes  ----------------------------*/
-
-/*---------------------  Export Variables  --------------------------*/
-
-/*---------------------  Export Functions  --------------------------*/
-
 bool
 device_receive_frame(
 	PSDevice pDevice,
