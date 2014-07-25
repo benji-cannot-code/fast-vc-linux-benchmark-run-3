c550b67cdd051514d6cdeb6e8d39dd9288fb2944
Ian Abbott <abbotti@mev.co.uk>
2014-07-25 10:04:53 +0100

    staging: comedi: amplc_pc236: remove 'model' member
    The `model` member of `struct pc236_board` is no longer used since the
    code to remove manual configuration of PCI devices was removed.  Get rid
    of it.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_pc236.c
+++ drivers/staging/comedi/drivers/amplc_pc236.c
@@ -83,20 +83,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 enum pc236_bustype { isa_bustype, pci_bustype };
-enum pc236_model { pc36at_model, pci236_model };
 
 struct pc236_board {
 	const char *name;
 	unsigned short devid;
 	enum pc236_bustype bustype;
-	enum pc236_model model;
 };
 static const struct pc236_board pc236_boards[] = {
 #if DO_ISA
 	{
 		.name = "pc36at",
 		.bustype = isa_bustype,
-		.model = pc36at_model,
 	},
 #endif
 #if DO_PCI
@@ -104,7 +101,6 @@ static const struct pc236_board pc236_boards[] = {
 		.name = "pci236",
 		.devid = PCI_DEVICE_ID_AMPLICON_PCI236,
 		.bustype = pci_bustype,
-		.model = pci236_model,
 	},
 #endif
 };
