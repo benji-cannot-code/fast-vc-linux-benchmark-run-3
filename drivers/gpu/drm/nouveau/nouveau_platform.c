bac59c8cf3146495330b0b73c2fc9c6029feb8e7
Alexandre Courbot <acourbot@nvidia.com>
2014-07-26 18:36:02 +0900

    drm/nouveau: platform: update moved Tegra header
    Header for tegra_powergate functions has moved to soc/tegra/pmc.h.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_platform.c
+++ drivers/gpu/drm/nouveau/nouveau_platform.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/reset.h>
 #include <linux/regulator/consumer.h>
-#include <linux/tegra-powergate.h>
+#include <soc/tegra/pmc.h>
 
 #include "nouveau_drm.h"
 #include "nouveau_platform.h"
