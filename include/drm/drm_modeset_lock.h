6b1799ef344f4267cdee3b9097a2ea7b827fcdbc
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-07-27 19:09:33 +0200

    drm: trylock modest locking for fbdev panics
    In the fbdev code we want to do trylocks only to avoid deadlocks and
    other ugly issues. Thus far we've only grabbed the overall modeset
    lock, but that already failed to exclude a pile of potential
    concurrent operations. With proper atomic support this will be worse.
    
    So add a trylock mode to the modeset locking code which attempts all
    locks only with trylocks, if possible. We need to track this in the
    locking functions themselves and can't restrict this to drivers since
    driver-private w/w mutexes must be treated the same way.
    
    There's still the issue that other driver private locks aren't handled
    here at all, but well can't have everything. With this we will at
    least not regress, even once atomic allows lots of concurrent kms
    activity.
    
    Aside: We should move the acquire context to stack-based allocation in
    the callers to get rid of that awful WARN_ON(kmalloc_failed) control
    flow which just blows up when memory is short. But that's material for
    separate patches.
    
    v2:
    - Fix logic inversion fumble in the fb helper.
    - Add proper kerneldoc.
    
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/drm_modeset_lock.h
+++ include/drm/drm_modeset_lock.h
@@ -54,6 +54,11 @@ struct drm_modeset_acquire_ctx {
 	 * list of held locks (drm_modeset_lock)
 	 */
 	struct list_head locked;
+
+	/**
+	 * Trylock mode, use only for panic handlers!
+	 */
+	bool trylock_only;
 };
 
 /**
@@ -124,6 +129,7 @@ struct drm_device;
 struct drm_crtc;
 
 void drm_modeset_lock_all(struct drm_device *dev);
+int __drm_modeset_lock_all(struct drm_device *dev, bool trylock);
 void drm_modeset_unlock_all(struct drm_device *dev);
 void drm_modeset_lock_crtc(struct drm_crtc *crtc);
 void drm_modeset_unlock_crtc(struct drm_crtc *crtc);
