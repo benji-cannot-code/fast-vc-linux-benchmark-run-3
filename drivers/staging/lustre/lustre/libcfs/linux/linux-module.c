d34acc997770c51685fd119539a9d42f6769143b
Jessica Yu <jyu@cowsay.org>
2014-07-28 06:33:14 -0700

    Staging: lustre: linux-module: remove extraneous parens
    Remove unnecessary parens from return statements, return is not a function
    
    Signed-off-by: Jessica Yu <jyu@cowsay.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/libcfs/linux/linux-module.c
+++ drivers/staging/lustre/lustre/libcfs/linux/linux-module.c
@@ -155,7 +155,7 @@ static long libcfs_ioctl(struct file *file,
 		if (!capable(CFS_CAP_SYS_BOOT))
 			return (-EPERM);
 		panic("debugctl-invoked panic");
-		return (0);
+		return 0;
 	case IOC_LIBCFS_MEMHOG:
 		if (!capable(CFS_CAP_SYS_ADMIN))
 			return -EPERM;
@@ -168,7 +168,7 @@ static long libcfs_ioctl(struct file *file,
 		rc = libcfs_psdev_ops.p_ioctl(&pfile, cmd, (void *)arg);
 	else
 		rc = -EPERM;
-	return (rc);
+	return rc;
 }
 
 static struct file_operations libcfs_fops = {
