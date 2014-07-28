816333372a10b8d7e03b523b44120d99ca718fc8
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-07-28 10:26:56 -0700

    staging: comedi: ni_tio: tidy up Gi_DMA_Config_Reg_Bits
    Convert this enum into defines and rename the CamelCase symbols.
    
    For aesthetics, move the new defines so they are associated with
    the register define.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_tio_internal.h
+++ drivers/staging/comedi/drivers/ni_tio_internal.h
@@ -134,17 +134,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GI_HW_SAVE(x)			(((x) % 2) ? (1 << 13) : (1 << 12))
 #define GI_PERMANENT_STALE(x)		(((x) % 2) ? (1 << 15) : (1 << 14))
 #define NITIO_DMA_CFG_REG(x)		(NITIO_G0_DMA_CFG + (x))
+#define GI_DMA_ENABLE			(1 << 0)
+#define GI_DMA_WRITE			(1 << 1)
+#define GI_DMA_INT_ENA			(1 << 2)
+#define GI_DMA_RESET			(1 << 3)
+#define GI_DMA_BANKSW_ERROR		(1 << 4)
 #define NITIO_DMA_STATUS_REG(x)		(NITIO_G0_DMA_STATUS + (x))
 #define NITIO_ABZ_REG(x)		(NITIO_G0_ABZ + (x))
 #define NITIO_INT_ACK_REG(x)		(NITIO_G0_INT_ACK + (x))
 #define NITIO_STATUS_REG(x)		(NITIO_G0_STATUS + (x))
 #define NITIO_INT_ENA_REG(x)		(NITIO_G0_INT_ENA + (x))
-
-enum Gi_DMA_Config_Reg_Bits {
-	Gi_DMA_Enable_Bit = 0x1,
-	Gi_DMA_Write_Bit = 0x2,
-	Gi_DMA_Int_Bit = 0x4
-};
 
 enum Gi_DMA_Status_Reg_Bits {
 	Gi_DMA_Readbank_Bit = 0x2000,
