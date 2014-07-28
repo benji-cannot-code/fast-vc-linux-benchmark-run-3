66c3dbd23cc183be265eac4ab4fce29cd0ef58e8
Seunghun Lee <waydi1@gmail.com>
2014-07-28 22:51:03 +0900

    staging: dgnc: removes unused if defined code
    DGNC_TRACER and TRC_TO_KMEM are never defined.
    
    This patch removes if defined DGNC_TRACER and TRC_TO_KMEM code.
    
    CC: Lidza Louina <lidza.louina@gmail.com>
    CC: Mark Hounschell <markh@compro.net>
    Signed-off-by: Seunghun Lee <waydi1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_trace.c
+++ drivers/staging/dgnc/dgnc_trace.c
@@ -43,14 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* file level globals */
 static char *dgnc_trcbuf;		/* the ringbuffer */
 
-#if defined(TRC_TO_KMEM)
-static int dgnc_trcbufi = 0;		/* index of the tilde at the end of */
-#endif
-
-#if defined(TRC_TO_KMEM)
-static DEFINE_SPINLOCK(dgnc_tracef_lock);
-#endif
-
 /*
  * dgnc_tracer_free()
  *
