9bf1511cb377b20b39a1d1c90c39d04195fe1770
Ian Abbott <abbotti@mev.co.uk>
2014-07-29 12:58:12 +0100

    staging: comedi: amplc_pci230: blank lines aren't necessary before a close brace '}'
    Fix checkpatch issues: "CHECK: Blank lines aren't necessary before a
    close brace '}'".
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_pci230.c
+++ drivers/staging/comedi/drivers/amplc_pci230.c
@@ -740,7 +740,6 @@ static void put_resources(struct comedi_device *dev, unsigned int res_mask,
 			res_mask &= ~b;
 			if (devpriv->res_owner[i] == owner)
 				devpriv->res_owner[i] = OWNER_NONE;
-
 		}
 	}
 	spin_unlock_irqrestore(&devpriv->res_spinlock, irqflags);
@@ -793,7 +792,6 @@ static unsigned int pci230_choose_clk_count(uint64_t ns, unsigned int *count,
 		cnt = divide_ns(ns, pci230_timebase[clk_src], flags);
 		if ((cnt <= 65536) || (clk_src == CLK_1KHZ))
 			break;
-
 	}
 	*count = cnt;
 	return clk_src;
@@ -1449,7 +1447,6 @@ static void pci230_ao_start(struct comedi_device *dev,
 					   & ~PCI230P2_DAC_TRIG_MASK) |
 			    scantrig;
 			outw(devpriv->daccon, dev->iobase + PCI230_DACCON);
-
 		}
 		switch (cmd->scan_begin_src) {
 		case TRIG_TIMER:
@@ -1513,7 +1510,6 @@ static int pci230_ao_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 		/* Claim Z2-CT1. */
 		if (!get_one_resource(dev, RES_Z2CT1, OWNER_AOCMD))
 			return -EBUSY;
-
 	}
 
 	/* Get number of scans required. */
