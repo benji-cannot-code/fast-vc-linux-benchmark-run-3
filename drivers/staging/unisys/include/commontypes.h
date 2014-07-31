03912b3e4d2e5c9f74559c9ec08f97133b08fe2a
Benjamin Romer <benjamin.romer@unisys.com>
2014-07-31 12:00:50 -0400

    staging: unisys: remove U32 type
    This patch switches all use of the U32 typedef to use the kernel's u32 type
    instead.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/commontypes.h
+++ drivers/staging/unisys/include/commontypes.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <syslog.h>
 #endif
 
-#define U32 uint32_t
 #define U64 uint64_t
 #define S8  int8_t
 #define S16 int16_t
@@ -41,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 
 #ifdef CONFIG_X86_32
-#define UINTN U32
+#define UINTN u32
 #else
 #define UINTN U64
 #endif
@@ -50,7 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <stdint.h>
 #if __WORDSIZE == 32
-#define UINTN U32
+#define UINTN u32
 #elif __WORDSIZE == 64
 #define UINTN U64
 #else
@@ -122,7 +121,7 @@ typedef U64 GUEST_PHYSICAL_ADDRESS;
 		       fil, lin);					\
 	} while (0)
 
-#define CHANNEL_U32_MISMATCH(chType, chName, field, expected, actual, fil, \
+#define CHANNEL_u32_MISMATCH(chType, chName, field, expected, actual, fil, \
 			     lin, logCtx)				\
 	do {								\
 		syslog(LOG_USER | LOG_ERR,				\
--- drivers/staging/unisys/uislib/uislib.c
+++ drivers/staging/unisys/uislib/uislib.c
@@ -121,7 +121,7 @@ static const struct file_operations debugfs_info_fops = {
 };
 
 static void
-init_msg_header(CONTROLVM_MESSAGE *msg, U32 id, uint rsp, uint svr)
+init_msg_header(CONTROLVM_MESSAGE *msg, u32 id, uint rsp, uint svr)
 {
 	memset(msg, 0, sizeof(CONTROLVM_MESSAGE));
 	msg->hdr.Id = id;
@@ -130,7 +130,7 @@ init_msg_header(CONTROLVM_MESSAGE *msg, U32 id, uint rsp, uint svr)
 }
 
 static __iomem void *
-init_vbus_channel(U64 channelAddr, U32 channelBytes)
+init_vbus_channel(U64 channelAddr, u32 channelBytes)
 {
 	void __iomem *rc = NULL;
 	void __iomem *pChan = uislib_ioremap_cache(channelAddr, channelBytes);
@@ -155,7 +155,7 @@ Away:
 static int
 create_bus(CONTROLVM_MESSAGE *msg, char *buf)
 {
-	U32 busNo, deviceCount;
+	u32 busNo, deviceCount;
 	struct bus_info *tmp, *bus;
 	size_t size;
 
@@ -274,7 +274,7 @@ destroy_bus(CONTROLVM_MESSAGE *msg, char *buf)
 {
 	int i;
 	struct bus_info *bus, *prev = NULL;
-	U32 busNo;
+	u32 busNo;
 
 	busNo = msg->cmd.destroyBus.busNo;
 
@@ -342,7 +342,7 @@ create_device(CONTROLVM_MESSAGE *msg, char *buf)
 {
 	struct device_info *dev;
 	struct bus_info *bus;
-	U32 busNo, devNo;
+	u32 busNo, devNo;
 	int result = CONTROLVM_RESP_SUCCESS;
 	U64 minSize = MIN_IO_CHANNEL_SIZE;
 	ReqHandlerInfo_t *pReqHandler;
@@ -536,7 +536,7 @@ Away:
 static int
 pause_device(CONTROLVM_MESSAGE *msg)
 {
-	U32 busNo, devNo;
+	u32 busNo, devNo;
 	struct bus_info *bus;
 	struct device_info *dev;
 	struct guest_msgs cmd;
@@ -608,7 +608,7 @@ pause_device(CONTROLVM_MESSAGE *msg)
 static int
 resume_device(CONTROLVM_MESSAGE *msg)
 {
-	U32 busNo, devNo;
+	u32 busNo, devNo;
 	struct bus_info *bus;
 	struct device_info *dev;
 	struct guest_msgs cmd;
@@ -680,7 +680,7 @@ resume_device(CONTROLVM_MESSAGE *msg)
 static int
 destroy_device(CONTROLVM_MESSAGE *msg, char *buf)
 {
-	U32 busNo, devNo;
+	u32 busNo, devNo;
 	struct bus_info *bus;
 	struct device_info *dev;
 	struct guest_msgs cmd;
@@ -792,7 +792,7 @@ init_chipset(CONTROLVM_MESSAGE *msg, char *buf)
 }
 
 static int
-delete_bus_glue(U32 busNo)
+delete_bus_glue(u32 busNo)
 {
 	CONTROLVM_MESSAGE msg;
 
@@ -806,7 +806,7 @@ delete_bus_glue(U32 busNo)
 }
 
 static int
-delete_device_glue(U32 busNo, U32 devNo)
+delete_device_glue(u32 busNo, u32 devNo)
 {
 	CONTROLVM_MESSAGE msg;
 
@@ -822,7 +822,7 @@ delete_device_glue(U32 busNo, U32 devNo)
 }
 
 int
-uislib_client_inject_add_bus(U32 busNo, uuid_le instGuid,
+uislib_client_inject_add_bus(u32 busNo, uuid_le instGuid,
 			     U64 channelAddr, ulong nChannelBytes)
 {
 	CONTROLVM_MESSAGE msg;
@@ -873,14 +873,14 @@ EXPORT_SYMBOL_GPL(uislib_client_inject_add_bus);
 
 
 int
-uislib_client_inject_del_bus(U32 busNo)
+uislib_client_inject_del_bus(u32 busNo)
 {
 	return delete_bus_glue(busNo);
 }
 EXPORT_SYMBOL_GPL(uislib_client_inject_del_bus);
 
 int
-uislib_client_inject_pause_vhba(U32 busNo, U32 devNo)
+uislib_client_inject_pause_vhba(u32 busNo, u32 devNo)
 {
 	CONTROLVM_MESSAGE msg;
 	int rc;
@@ -900,7 +900,7 @@ uislib_client_inject_pause_vhba(U32 busNo, U32 devNo)
 EXPORT_SYMBOL_GPL(uislib_client_inject_pause_vhba);
 
 int
-uislib_client_inject_resume_vhba(U32 busNo, U32 devNo)
+uislib_client_inject_resume_vhba(u32 busNo, u32 devNo)
 {
 	CONTROLVM_MESSAGE msg;
 	int rc;
@@ -921,8 +921,8 @@ uislib_client_inject_resume_vhba(U32 busNo, U32 devNo)
 EXPORT_SYMBOL_GPL(uislib_client_inject_resume_vhba);
 
 int
-uislib_client_inject_add_vhba(U32 busNo, U32 devNo,
-			      U64 phys_chan_addr, U32 chan_bytes,
+uislib_client_inject_add_vhba(u32 busNo, u32 devNo,
+			      U64 phys_chan_addr, u32 chan_bytes,
 			      int is_test_addr, uuid_le instGuid,
 			      struct InterruptInfo *intr)
 {
@@ -973,15 +973,15 @@ uislib_client_inject_add_vhba(U32 busNo, U32 devNo,
 EXPORT_SYMBOL_GPL(uislib_client_inject_add_vhba);
 
 int
-uislib_client_inject_del_vhba(U32 busNo, U32 devNo)
+uislib_client_inject_del_vhba(u32 busNo, u32 devNo)
 {
 	return delete_device_glue(busNo, devNo);
 }
 EXPORT_SYMBOL_GPL(uislib_client_inject_del_vhba);
 
 int
-uislib_client_inject_add_vnic(U32 busNo, U32 devNo,
-			      U64 phys_chan_addr, U32 chan_bytes,
+uislib_client_inject_add_vnic(u32 busNo, u32 devNo,
+			      U64 phys_chan_addr, u32 chan_bytes,
 			      int is_test_addr, uuid_le instGuid,
 			      struct InterruptInfo *intr)
 {
@@ -1033,7 +1033,7 @@ uislib_client_inject_add_vnic(U32 busNo, U32 devNo,
 EXPORT_SYMBOL_GPL(uislib_client_inject_add_vnic);
 
 int
-uislib_client_inject_pause_vnic(U32 busNo, U32 devNo)
+uislib_client_inject_pause_vnic(u32 busNo, u32 devNo)
 {
 	CONTROLVM_MESSAGE msg;
 	int rc;
@@ -1053,7 +1053,7 @@ uislib_client_inject_pause_vnic(U32 busNo, U32 devNo)
 EXPORT_SYMBOL_GPL(uislib_client_inject_pause_vnic);
 
 int
-uislib_client_inject_resume_vnic(U32 busNo, U32 devNo)
+uislib_client_inject_resume_vnic(u32 busNo, u32 devNo)
 {
 	CONTROLVM_MESSAGE msg;
 	int rc;
@@ -1074,14 +1074,14 @@ uislib_client_inject_resume_vnic(U32 busNo, U32 devNo)
 EXPORT_SYMBOL_GPL(uislib_client_inject_resume_vnic);
 
 int
-uislib_client_inject_del_vnic(U32 busNo, U32 devNo)
+uislib_client_inject_del_vnic(u32 busNo, u32 devNo)
 {
 	return delete_device_glue(busNo, devNo);
 }
 EXPORT_SYMBOL_GPL(uislib_client_inject_del_vnic);
 
 static int
-uislib_client_add_vnic(U32 busNo)
+uislib_client_add_vnic(u32 busNo)
 {
 	BOOL busCreated = FALSE;
 	int devNo = 0;		/* Default to 0, since only one device
@@ -1130,7 +1130,7 @@ AwayCleanup:
 EXPORT_SYMBOL_GPL(uislib_client_add_vnic);
 
 static int
-uislib_client_delete_vnic(U32 busNo)
+uislib_client_delete_vnic(u32 busNo)
 {
 	int devNo = 0;		/* Default to 0, since only one device
 				 * will be created for this bus... */
@@ -1286,7 +1286,7 @@ info_debugfs_read(struct file *file, char __user *buf,
 }
 
 static struct device_info *
-find_dev(U32 busNo, U32 devNo)
+find_dev(u32 busNo, u32 devNo)
 {
 	struct bus_info *bus;
 	struct device_info *dev = NULL;
@@ -1459,7 +1459,7 @@ Initialize_incoming_thread(void)
  *                        function.
  */
 void
-uislib_enable_channel_interrupts(U32 busNo, U32 devNo,
+uislib_enable_channel_interrupts(u32 busNo, u32 devNo,
 				 int (*interrupt)(void *),
 				 void *interrupt_context)
 {
@@ -1485,7 +1485,7 @@ EXPORT_SYMBOL_GPL(uislib_enable_channel_interrupts);
  *  Process_Incoming().
  */
 void
-uislib_disable_channel_interrupts(U32 busNo, U32 devNo)
+uislib_disable_channel_interrupts(u32 busNo, u32 devNo)
 {
 	struct device_info *dev;
 	dev = find_dev(busNo, devNo);
@@ -1518,7 +1518,7 @@ static DECLARE_WORK(Work_wakeup_polling_device_channels,
  *  your device might have more requests.
  */
 void
-uislib_force_channel_interrupt(U32 busNo, U32 devNo)
+uislib_force_channel_interrupt(u32 busNo, u32 devNo)
 {
 	if (en_smart_wakeup == 0)
 		return;
