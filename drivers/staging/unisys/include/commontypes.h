a4a83c3ae1b9b20a392f3143022310c8523a6a34
Benjamin Romer <benjamin.romer@unisys.com>
2014-07-31 12:00:52 -0400

    staging: unisys: remove S8 type
    Delete the S8 type from commontypes.h since it wasn't being used.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/commontypes.h
+++ drivers/staging/unisys/include/commontypes.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <syslog.h>
 #endif
 
-#define S8  int8_t
 #define S16 int16_t
 #define S32 int32_t
 #define S64 int64_t
