94e0028d835e2768780a014b59dda33aadf6c669
Kiran Padwal <kiran.padwal@smartplayin.com>
2014-08-05 13:22:00 +0530

    tty: serial: msm: Fix 'Missing a blank line after declarations' warning
    This patch fixes below checkpatch.pl warning and it remove extra blank lines:
    
    WARNING: Missing a blank line after declarations
    
    Signed-off-by: Kiran Padwal <kiran.padwal@smartplayin.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/msm_serial.c
+++ drivers/tty/serial/msm_serial.c
@@ -315,7 +315,6 @@ static unsigned int msm_get_mctrl(struct uart_port *port)
 	return TIOCM_CAR | TIOCM_CTS | TIOCM_DSR | TIOCM_RTS;
 }
 
-
 static void msm_reset(struct uart_port *port)
 {
 	struct msm_port *msm_port = UART_TO_MSM(port);
@@ -336,6 +335,7 @@ static void msm_reset(struct uart_port *port)
 static void msm_set_mctrl(struct uart_port *port, unsigned int mctrl)
 {
 	unsigned int mr;
+
 	mr = msm_read(port, UART_MR1);
 
 	if (!(mctrl & TIOCM_RTS)) {
@@ -430,7 +430,6 @@ static int msm_set_baud_rate(struct uart_port *port, unsigned int baud)
 
 	return baud;
 }
-
 
 static void msm_init_clock(struct uart_port *port)
 {
@@ -646,6 +645,7 @@ fail_release_port:
 static void msm_config_port(struct uart_port *port, int flags)
 {
 	int ret;
+
 	if (flags & UART_CONFIG_TYPE) {
 		port->type = PORT_MSM;
 		ret = msm_request_port(port);
