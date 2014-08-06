8c910f9d012731a31e23fc4c9f158ca690ecc9d9
Nick Krause <xerofoiffy@gmail.com>
2014-08-06 13:53:17 -0400

    spi: omap-100k: Remove unused definitions
    Remove unused definition which cause the following warnings
    
    drivers/spi/spi-omap-100k.c:73:0: warning: "WRITE" redefined [enabled by default]
    include/linux/fs.h:193:0: note: this is the location of the previous definition
    drivers/spi/spi-omap-100k.c:74:0: warning: "READ" redefined [enabled by default]
    include/linux/fs.h:192:0: note: this is the location of the previous definition
    
    Signed-off-by: Nick Krause <xerofoiffy@gmail.com>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/spi/spi-omap-100k.c
+++ drivers/spi/spi-omap-100k.c
@@ -71,10 +71,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SPI_STATUS_WE                   (1UL << 1)
 #define SPI_STATUS_RD                   (1UL << 0)
 
-#define WRITE 0
-#define READ  1
-
-
 /* use PIO for small transfers, avoiding DMA setup/teardown overhead and
  * cache operations; better heuristics consider wordsize and bitrate.
  */
