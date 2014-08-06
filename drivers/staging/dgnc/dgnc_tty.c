f67c2a5549318bbfa95396f0fe3afd979f263bc4
Konrad Zapalowicz <bergo.torino@gmail.com>
2014-08-06 14:21:23 +0200

    staging: dgnc: Move utility functions out of dgnc_driver.c
    This commit moves the utility functions out of dgnc_driver.c file and
    puts them in the new dgnc_utils.{c,h} files. The accompanying changes
    adjust the existing code to work with this design.
    
    Signed-off-by: Konrad Zapalowicz <bergo.torino@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_tty.c
+++ drivers/staging/dgnc/dgnc_tty.c
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dgnc_cls.h"
 #include "dpacompat.h"
 #include "dgnc_sysfs.h"
+#include "dgnc_utils.h"
 
 #define init_MUTEX(sem)	 sema_init(sem, 1)
 #define DECLARE_MUTEX(name)     \
