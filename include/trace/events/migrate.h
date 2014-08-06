9880a16ae3ee98840c5bf193980559e83507702d
Max Asbock <masbock@linux.vnet.ibm.com>
2014-08-06 16:06:21 -0700

    mm tracing: tell mm_migrate_pages event about numa_misplaced
    The mm_migrate_pages trace event reports a reason for the migration,
    typically as a symbolic string.  The exception is the reason
    MR_NUMA_MISPLACED for which it just displays the numeric value:
    mm_migrate_pages: nr_succeeded=1 nr_failed=0 mode=MIGRATE_ASYNC
    reason=0x5
    
    This patch makes the output consistent by introducing a string value for
    MR_NUMA_MISPLACED.  The event is then reported as: mm_migrate_pages:
    nr_succeeded=1 nr_failed=0 mode=MIGRATE_ASYNC reason=numa_misplaced
    
    Signed-off-by: Max Asbock <masbock@linux.vnet.ibm.com>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/trace/events/migrate.h
+++ include/trace/events/migrate.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	{MR_MEMORY_HOTPLUG,	"memory_hotplug"},		\
 	{MR_SYSCALL,		"syscall_or_cpuset"},		\
 	{MR_MEMPOLICY_MBIND,	"mempolicy_mbind"},		\
+	{MR_NUMA_MISPLACED,	"numa_misplaced"},		\
 	{MR_CMA,		"cma"}
 
 TRACE_EVENT(mm_migrate_pages,
