a4fdbe32f75f32abcc2f583a60962a5fb75aab35
Michal Simek <michal.simek@xilinx.com>
2014-08-07 15:59:05 +0200

    microblaze: Wire-up getrandom syscall
    Add new getrandom syscall.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/uapi/asm/unistd.h
+++ arch/microblaze/include/uapi/asm/unistd.h
@@ -401,5 +401,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_sched_getattr	382
 #define __NR_renameat2		383
 #define __NR_seccomp		384
+#define __NR_getrandom		385
 
 #endif /* _UAPI_ASM_MICROBLAZE_UNISTD_H */
