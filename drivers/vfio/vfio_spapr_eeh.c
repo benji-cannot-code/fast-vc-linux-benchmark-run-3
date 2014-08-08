7b0ea8b1afef4fc023e0765d72513251a8570a18
Alexey Kardashevskiy <aik@ozlabs.ru>
2014-08-08 10:37:51 -0600

    drivers/vfio: Allow EEH to be built as module
    This adds necessary declarations to the SPAPR VFIO EEH module,
    otherwise multiple dynamic linker errors reported:
    
    vfio_spapr_eeh: Unknown symbol eeh_pe_set_option (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_pe_configure (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_pe_reset (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_pe_get_state (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_iommu_group_to_pe (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_dev_open (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_pe_set_option (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_pe_configure (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_pe_reset (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_pe_get_state (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_iommu_group_to_pe (err 0)
    vfio_spapr_eeh: Unknown symbol eeh_dev_open (err 0)
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/vfio/vfio_spapr_eeh.c
+++ drivers/vfio/vfio_spapr_eeh.c
@@ -10,9 +10,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
+#include <linux/module.h>
 #include <linux/uaccess.h>
 #include <linux/vfio.h>
 #include <asm/eeh.h>
+
+#define DRIVER_VERSION	"0.1"
+#define DRIVER_AUTHOR	"Gavin Shan, IBM Corporation"
+#define DRIVER_DESC	"VFIO IOMMU SPAPR EEH"
 
 /* We might build address mapping here for "fast" path later */
 int vfio_spapr_pci_eeh_open(struct pci_dev *pdev)
@@ -89,3 +94,8 @@ long vfio_spapr_iommu_eeh_ioctl(struct iommu_group *group,
 	return ret;
 }
 EXPORT_SYMBOL(vfio_spapr_iommu_eeh_ioctl);
+
+MODULE_VERSION(DRIVER_VERSION);
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
