ecf6e19a5a23cda91ee39c7077f7b437c221deb9
Ben Skeggs <bskeggs@redhat.com>
2014-08-10 04:10:20 +1000

    drm/nouveau/core: import ioctl/event interfaces
    This forms the basis for the new APIs that will be exposed to userspace,
    giving it access to:
    
    - Object method calls, the immediately useful of which is performance
      counters and the abiity to manipulate the ZBC tables.
    - Information on the child classes an object supports, in order to avoid
      having to try all supported classes until successful.
    - Notifications, which will be used in the future to inform the client
      if its channel was killed due to a lockup, etc.
    
    This commit imports the interfaces, but are not currently used.  The DRM
    portion of the driver will be ported to speak to the core using these
    interfaces as much as possible.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/core/notify.c
+++ drivers/gpu/drm/nouveau/core/core/notify.c
@@ -23,9 +23,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Authors: Ben Skeggs <bskeggs@redhat.com>
  */
 
-#include <core/os.h>
+#include <core/client.h>
 #include <core/event.h>
 #include <core/notify.h>
+
+#include <nvif/unpack.h>
+#include <nvif/event.h>
 
 static inline void
 nvkm_notify_put_locked(struct nvkm_notify *notify)
