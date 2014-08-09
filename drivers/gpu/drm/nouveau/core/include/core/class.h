6c7257deeab5a3279870db9c47e1a5ddb8a21061
Ben Skeggs <bskeggs@redhat.com>
2014-08-10 04:10:21 +1000

    drm/nouveau/nvif: import library functions for the ioctl/event interfaces
    This is a wrapper around the interfaces defined in an earlier commit,
    and is also used by various userspace (either by a libdrm backend, or
    libpciaccess) tools/tests.
    
    In the future this will be extended to handle channels, replacing some
    long-unloved code we currently use, and allow fifo/display/mpeg (hi
    Ilia ;)) engines to all be exposed in the same way.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/include/core/class.h
+++ drivers/gpu/drm/nouveau/core/include/core/class.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NOUVEAU_CLASS_H__
 #define __NOUVEAU_CLASS_H__
 
+#include <nvif/class.h>
+
 /* Device class
  *
  * 0080: NV_DEVICE
