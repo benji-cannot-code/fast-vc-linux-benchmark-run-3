2eaac56857231292e8037f38af06bce71a773f66
Ben Skeggs <bskeggs@redhat.com>
2014-08-10 04:10:30 +1000

    drm/nouveau: hide sysfs pstate file behind an option again
    No-one has yet had time to move this to debugfs as discussed during
    the last merge window.  Until this happens, hide the option to make
    it clear it's not going to be here forever.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_sysfs.c
+++ drivers/gpu/drm/nouveau/nouveau_sysfs.c
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "nouveau_sysfs.h"
 
+MODULE_PARM_DESC(pstate, "enable sysfs pstate file, which will be moved in the future");
+static int nouveau_pstate;
+module_param_named(pstate, nouveau_pstate, int, 0400);
+
 static inline struct drm_device *
 drm_device(struct device *d)
 {
@@ -161,7 +165,7 @@ nouveau_sysfs_fini(struct drm_device *dev)
 	struct nouveau_drm *drm = nouveau_drm(dev);
 	struct nvif_device *device = &drm->device;
 
-	if (sysfs->ctrl.priv) {
+	if (sysfs && sysfs->ctrl.priv) {
 		device_remove_file(nv_device_base(nvkm_device(device)), &dev_attr_pstate);
 		nvif_object_fini(&sysfs->ctrl);
 	}
@@ -177,6 +181,9 @@ nouveau_sysfs_init(struct drm_device *dev)
 	struct nvif_device *device = &drm->device;
 	struct nouveau_sysfs *sysfs;
 	int ret;
+
+	if (!nouveau_pstate)
+		return 0;
 
 	sysfs = drm->sysfs = kzalloc(sizeof(*sysfs), GFP_KERNEL);
 	if (!sysfs)
