0664a7b29a37484ccef92e3105ce23702a88aa60
Shuah Khan <shuah.kh@samsung.com>
2014-08-09 21:47:10 -0300

    [media] au0828: remove CONFIG_VIDEO_AU0828_RC scope around au0828_rc_*()
    Remove CONFIG_VIDEO_AU0828_RC scope around au0828_rc_register()
    and au0828_rc_unregister() calls in au0828-core
    
    Signed-off-by: Shuah Khan <shuah.kh@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/usb/au0828/au0828-core.c
+++ drivers/media/usb/au0828/au0828-core.c
@@ -154,9 +154,7 @@ static void au0828_usb_disconnect(struct usb_interface *interface)
 
 	dprintk(1, "%s()\n", __func__);
 
-#ifdef CONFIG_VIDEO_AU0828_RC
 	au0828_rc_unregister(dev);
-#endif
 	/* Digital TV */
 	au0828_dvb_unregister(dev);
 
@@ -267,10 +265,8 @@ static int au0828_usb_probe(struct usb_interface *interface,
 		pr_err("%s() au0282_dev_register failed\n",
 		       __func__);
 
-#ifdef CONFIG_VIDEO_AU0828_RC
 	/* Remote controller */
 	au0828_rc_register(dev);
-#endif
 
 	/*
 	 * Store the pointer to the au0828_dev so it can be accessed in
