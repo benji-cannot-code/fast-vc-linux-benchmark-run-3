7ee378bf95078ea0774f83e5bb85b31038bb77ab
navin patidar <navin.patidar@gmail.com>
2014-08-10 20:14:19 +0530

    staging: rtl8188eu: Remove unused function odm_ConfigMAC_8188E()
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/odm_RegConfig8188E.c
+++ drivers/staging/rtl8188eu/hal/odm_RegConfig8188E.c
@@ -21,14 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "odm_precomp.h"
 
-void odm_ConfigMAC_8188E(struct odm_dm_struct *pDM_Odm, u32 Addr, u8 Data)
-{
-	struct adapter *adapt = pDM_Odm->Adapter;
-
-	usb_write8(adapt, Addr, Data);
-	ODM_RT_TRACE(pDM_Odm, ODM_COMP_INIT, ODM_DBG_TRACE, ("===> ODM_ConfigMACWithHeaderFile: [MAC_REG] %08X %08X\n", Addr, Data));
-}
-
 void odm_ConfigBB_AGC_8188E(struct odm_dm_struct *pDM_Odm, u32 Addr, u32 Bitmask, u32 Data)
 {
 	struct adapter *adapter = pDM_Odm->Adapter;
