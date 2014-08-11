79e1675a157ee04461a033f5c2da718325a8130f
Kiran Padwal <kiran.padwal21@gmail.com>
2014-08-11 16:47:50 +0530

    pinctrl: Make of_device_id array const
    Make of_device_id array const, because all OF functions handle it as
    const.
    
    Signed-off-by: Kiran Padwal <kiran.padwal21@gmail.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-imx35.c
+++ drivers/pinctrl/pinctrl-imx35.c
@@ -1006,7 +1006,7 @@ static struct imx_pinctrl_soc_info imx35_pinctrl_info = {
 	.npins = ARRAY_SIZE(imx35_pinctrl_pads),
 };
 
-static struct of_device_id imx35_pinctrl_of_match[] = {
+static const struct of_device_id imx35_pinctrl_of_match[] = {
 	{ .compatible = "fsl,imx35-iomuxc", },
 	{ /* sentinel */ }
 };
