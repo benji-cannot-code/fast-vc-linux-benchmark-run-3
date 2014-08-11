eb1d599a5b35d669e641fc08065e4a24f671733d
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-08-11 15:30:45 +0800

    greybus: Import most recent greybus code to new repo.

---
+++ drivers/staging/greybus/greybus_id.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* FIXME
+ * move this to include/linux/mod_devicetable.h when merging
+ */
+
+#ifndef __LINUX_GREYBUS_ID_H
+#define __LINUX_GREYBUS_ID_H
+
+#include <linux/types.h>
+#include <linux/mod_devicetable.h>
+
+
+struct greybus_device_id {
+	__u16	match_flags;
+	__u16	wVendor;
+	__u16	wProduct;
+	__u64	lSerialNumber;
+
+	kernel_ulong_t	driver_info
+		__attribute__((aligned(sizeof(kernel_ulong_t))));
+};
+
+/* Used to match the greybus_device_id */
+#define GREYBUS_DEVICE_ID_MATCH_VENDOR		BIT(0)
+#define GREYBUS_DEVICE_ID_MATCH_PRODUCT		BIT(1)
+#define GREYBUS_DEVICE_ID_MATCH_SERIAL		BIT(2)
+
+#endif /* __LINUX_GREYBUS_H */
