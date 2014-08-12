f5b44756cc1c7d396ccbf73075d20a4a17ca947d
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-08-12 18:50:23 -0300

    [media] as102-fe: make it an independent driver
    Move as102-fe to dvb-frontends directory and make it an
    independent driver.
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/dvb-frontends/as102_fe.h
+++ drivers/media/dvb-frontends/as102_fe.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
-#include "as10x_types.h"
+#include "as102_fe_types.h"
 
 struct as102_fe_ops {
 	int (*set_tune)(void *priv, struct as10x_tune_args *tune_args);
