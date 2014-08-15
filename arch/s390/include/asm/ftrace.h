a216a8c9913f0d8c8c19b76172b978bcae9bcd43
Heiko Carstens <heiko.carstens@de.ibm.com>
2014-08-15 12:33:46 +0200

    s390/ftrace: optimize function graph caller code
    When the function graph tracer is disabled we can skip three additional
    instructions. So let's just do this.
    
    So if function tracing is enabled but function graph tracing is
    runtime disabled, we get away with a single unconditional branch.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/ftrace.h
+++ arch/s390/include/asm/ftrace.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 
 extern void _mcount(void);
+extern char ftrace_graph_caller_end;
 
 struct dyn_arch_ftrace { };
 
