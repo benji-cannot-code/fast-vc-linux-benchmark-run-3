9767c51553f64399e8b4d39da94c9e069faa44e0
Eli Billauer <eli.billauer@gmail.com>
2014-08-16 18:58:00 +0300

    staging: xillybus: Removed unnecessary smp_wmb()
    These memory barriers were added "just to be safe".
    
    Signed-off-by: Eli Billauer <eli.billauer@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xillybus/xillybus_core.c
+++ drivers/staging/xillybus/xillybus_core.c
@@ -2062,8 +2062,6 @@ int xillybus_endpoint_discovery(struct xilly_endpoint *endpoint)
 	if (rc)
 		goto failed_idt;
 
-	smp_wmb();
-
 	rc = xilly_obtain_idt(endpoint);
 
 	if (rc)
@@ -2086,8 +2084,6 @@ int xillybus_endpoint_discovery(struct xilly_endpoint *endpoint)
 
 	if (rc)
 		goto failed_idt;
-
-	smp_wmb(); /* mutex_lock below should suffice, but won't hurt.*/
 
 	/*
 	 * endpoint is now completely configured. We put it on the list
