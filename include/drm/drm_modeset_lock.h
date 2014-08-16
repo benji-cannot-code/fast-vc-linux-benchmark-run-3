2c05d93d53e57bb2178984a2aa340ad3e1a57c3b
Randy Dunlap <rdunlap@infradead.org>
2014-08-16 14:15:34 -0700

    drm: fix drm_modeset_lock.h kernel-doc notation
    Fix drm kernel-doc notation to squelch these warnings:
    
    Warning(..//include/drm/drm_modeset_lock.h:41): cannot understand function prototype: 'struct drm_modeset_acquire_ctx '
    Warning(..//include/drm/drm_modeset_lock.h:66): cannot understand function prototype: 'struct drm_modeset_lock '
    
    Need to include the keyword 'struct' for structure descriptions.
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/drm_modeset_lock.h
+++ include/drm/drm_modeset_lock.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct drm_modeset_lock;
 
 /**
- * drm_modeset_acquire_ctx - locking context (see ww_acquire_ctx)
+ * struct drm_modeset_acquire_ctx - locking context (see ww_acquire_ctx)
  * @ww_ctx: base acquire ctx
  * @contended: used internally for -EDEADLK handling
  * @locked: list of held locks
@@ -62,7 +62,7 @@ struct drm_modeset_acquire_ctx {
 };
 
 /**
- * drm_modeset_lock - used for locking modeset resources.
+ * struct drm_modeset_lock - used for locking modeset resources.
  * @mutex: resource locking
  * @head: used to hold it's place on state->locked list when
  *    part of an atomic update
