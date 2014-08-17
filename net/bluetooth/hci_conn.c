f3a71c3f9b286804acb4289680f6b7689ac27174
Johan Hedberg <johan.hedberg@intel.com>
2014-08-18 00:41:43 +0300

    Bluetooth: Set disc_timeout to 0 when calling hci_chan_del
    The hci_chan_del() function is used in scenarios where we've decided we
    want to get rid of the underlying baseband link. It makes therefore
    sense to force the disc_timeout to 0 so that the disconnection routines
    are immediately scheduled.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -1317,6 +1317,9 @@ void hci_chan_del(struct hci_chan *chan)
 
 	synchronize_rcu();
 
+	/* Force the connection to be immediately dropped */
+	conn->disc_timeout = 0;
+
 	hci_conn_drop(conn);
 	hci_conn_put(conn);
 
