7118ab0ddfa5c82d9b00461bbc6c21f58198d053
Johan Hedberg <johan.hedberg@intel.com>
2014-08-18 20:33:30 +0300

    Bluetooth: Remove unused l2cap_conn_shutdown API
    Now that there are no more users of the l2cap_conn_shutdown API (since
    smp.c switched to using hci_disconnect) we can simply remove it along
    with all of it's l2cap_conn variables.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- include/net/bluetooth/l2cap.h
+++ include/net/bluetooth/l2cap.h
@@ -626,9 +626,6 @@ struct l2cap_conn {
 
 	struct delayed_work	info_timer;
 
-	int			disconn_err;
-	struct work_struct	disconn_work;
-
 	struct sk_buff		*rx_skb;
 	__u32			rx_len;
 	__u8			tx_ident;
@@ -948,7 +945,6 @@ void l2cap_logical_cfm(struct l2cap_chan *chan, struct hci_chan *hchan,
 		       u8 status);
 void __l2cap_physical_cfm(struct l2cap_chan *chan, int result);
 
-void l2cap_conn_shutdown(struct l2cap_conn *conn, int err);
 struct l2cap_conn *l2cap_conn_get(struct l2cap_conn *conn);
 void l2cap_conn_put(struct l2cap_conn *conn);
 
