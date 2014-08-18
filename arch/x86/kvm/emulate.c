84751d7b99db99fe9e15b9ad1b3637193d6f0fc4
Paolo Bonzini <pbonzini@redhat.com>
2014-08-18 13:15:51 +0200

    KVM: x86: do not check CS.DPL against RPL during task switch
    This reverts the check added by commit 5045b468037d (KVM: x86: check CS.DPL
    against RPL during task switch, 2014-05-15).  Although the CS.DPL=CS.RPL
    check is mentioned in table 7-1 of the SDM as causing a #TSS exception,
    it is not mentioned in table 6-6 that lists "invalid TSS conditions"
    which cause #TSS exceptions. In fact it causes some tests to fail, which
    pass on bare-metal.
    
    Keep the rest of the commit, since we will find new uses for it in 3.18.
    
    Reported-by: Nadav Amit <namit@cs.technion.ac.il>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -1492,9 +1492,6 @@ static int __load_segment_descriptor(struct x86_emulate_ctxt *ctxt,
 			goto exception;
 		break;
 	case VCPU_SREG_CS:
-		if (in_task_switch && rpl != dpl)
-			goto exception;
-
 		if (!(seg_desc.type & 8))
 			goto exception;
 
