bcba283ca6353bdb1a82440afaa944788d636389
Peter Meerwald <pmeerw@pmeerw.net>
2014-08-19 23:43:00 +0100

    iio:bma180: Rename BMA_180 to BMA180_
    Signed-off-by: Peter Meerwald <pmeerw@pmeerw.net>
    Cc: Oleksandr Kravchenko <o.v.kravchenko@globallogic.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/accel/bma180.c
+++ drivers/iio/accel/bma180.c
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* We have to write this value in reset register to do soft reset */
 #define BMA180_RESET_VAL	0xb6
 
-#define BMA_180_ID_REG_VAL	0x03
+#define BMA180_ID_REG_VAL	0x03
 
 /* Chip power modes */
 #define BMA180_LOW_NOISE	0x00
@@ -259,7 +259,7 @@ static int bma180_chip_init(struct bma180_data *data)
 
 	if (ret < 0)
 		goto err;
-	if (ret != BMA_180_ID_REG_VAL) {
+	if (ret != BMA180_ID_REG_VAL) {
 		ret = -ENODEV;
 		goto err;
 	}
