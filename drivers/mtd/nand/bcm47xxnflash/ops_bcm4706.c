b3ff3b3f7ad14d82036b44fd980755f38ee6a758
Rafał Miłecki <zajec5@gmail.com>
2014-08-19 09:14:16 +0200

    mtd: bcm47xxnflash: NAND_CMD_RESET support
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/nand/bcm47xxnflash/ops_bcm4706.c
+++ drivers/mtd/nand/bcm47xxnflash/ops_bcm4706.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>
+#include <linux/delay.h>
 #include <linux/bcma/bcma.h>
 
 /* Broadcom uses 1'000'000 but it seems to be too many. Tests on WNDR4500 has
@@ -227,7 +228,10 @@ static void bcm47xxnflash_ops_bcm4706_cmdfunc(struct mtd_info *mtd,
 
 	switch (command) {
 	case NAND_CMD_RESET:
-		pr_warn("Chip reset not implemented yet\n");
+		nand_chip->cmd_ctrl(mtd, command, NAND_CTRL_CLE);
+
+		ndelay(100);
+		nand_wait_ready(mtd);
 		break;
 	case NAND_CMD_READID:
 		ctlcode = NCTL_CSA | 0x01000000 | NCTL_CMD1W | NCTL_CMD0;
