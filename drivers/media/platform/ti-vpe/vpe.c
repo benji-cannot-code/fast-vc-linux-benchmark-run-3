b77e4907ec90f11ba5406730dbd595033b61df55
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-08-20 13:41:56 -0600

    [media] enable COMPILE_TEST for ti-vbe
    Allowing COMPILE_TEST here is trivial, but there's one missing
    header to be added:
    
    drivers/media/platform/ti-vpe/vpe.c: In function ‘vpe_probe’:
    drivers/media/platform/ti-vpe/vpe.c:2266:56: error: ‘SZ_32K’ undeclared (first use in this function)
      dev->base = devm_ioremap(&pdev->dev, dev->res->start, SZ_32K);
                                                            ^
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/ti-vpe/vpe.c
+++ drivers/media/platform/ti-vpe/vpe.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/videodev2.h>
 #include <linux/log2.h>
+#include <linux/sizes.h>
 
 #include <media/v4l2-common.h>
 #include <media/v4l2-ctrls.h>
