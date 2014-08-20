381ffa26cde37bbd716d478508a0c7fb5e864839
Geert Uytterhoeven <geert+renesas@glider.be>
2014-08-20 15:39:23 +0200

    ARM: shmobile: Move legacy INTC definitions from irqs.h to intc.h
    Move all definitions for legacy INTC from the common "irqs.h" to the
    INTC-specific "intc.h".
    Include "intc.h" in sh7372/sh73a0 CPU and board files where needed.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Magnus Damm <damm+renesas@opensource.se>
    [horms+renesas@verge.net.au: omitted whitespace change]
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/board-mackerel.c
+++ arch/arm/mach-shmobile/board-mackerel.c
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>
 
 #include "common.h"
+#include "intc.h"
 #include "irqs.h"
 #include "pm-rmobile.h"
 #include "sh-gpio.h"
--- arch/arm/mach-shmobile/setup-sh73a0.c
+++ arch/arm/mach-shmobile/setup-sh73a0.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "common.h"
 #include "dma-register.h"
+#include "intc.h"
 #include "irqs.h"
 #include "sh73a0.h"
 
