cc9e8a5d839debc0433a85596135bf259d9e1e01
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-08-20 13:50:03 -0600

    [media] allow COMPILE_TEST for SAMSUNG_EXYNOS4_IS
    That helps some static checks, so enable it. While there, it was
    noticed that linux/sizes.h was missing:
    
    drivers/media/platform/exynos4-is/mipi-csis.c: In function ‘s5pcsis_s_rx_buffer’:
    drivers/media/platform/exynos4-is/mipi-csis.c:114:31: error: ‘SZ_4K’ undeclared (first use in this function)
     #define S5PCSIS_PKTDATA_SIZE  SZ_4K
                                   ^
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/platform/exynos4-is/mipi-csis.c
+++ drivers/media/platform/exynos4-is/mipi-csis.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
+#include <linux/sizes.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/videodev2.h>
