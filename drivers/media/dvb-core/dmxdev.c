4e2c2f064646c9a8321a425bff739a66cfcdd73a
Changbing Xiong <cb.xiong@samsung.com>
2014-08-20 23:05:07 -0300

    [media] media: correct return value in dvb_demux_poll
    Data type of return value is unsigned int, but in function of dvb_demux_poll,
    when the pointer of dmxdevfilter equals NULL, it will return -EINVAL, which
    is invalid.
    
    Signed-off-by: Changbing Xiong <cb.xiong@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/dvb-core/dmxdev.c
+++ drivers/media/dvb-core/dmxdev.c
@@ -1089,7 +1089,7 @@ static unsigned int dvb_demux_poll(struct file *file, poll_table *wait)
 	unsigned int mask = 0;
 
 	if (!dmxdevfilter)
-		return -EINVAL;
+		return POLLERR;
 
 	poll_wait(file, &dmxdevfilter->buffer.queue, wait);
 
