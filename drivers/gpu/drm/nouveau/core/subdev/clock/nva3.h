65dfa1ba774b672a866cbaf18fc1fdaf087cc7d5
Roy Spliet <rspliet@eclipso.eu>
2014-08-21 13:45:14 +0200

    drm/nva3/clk: HOST clock
    Signed-off-by: Roy Spliet <rspliet@eclipso.eu>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/clock/nva3.h
+++ drivers/gpu/drm/nouveau/core/subdev/clock/nva3.h
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct nva3_clock_info {
 	u32 clk;
 	u32 pll;
+	enum {
+		NVA3_HOST_277,
+		NVA3_HOST_CLK,
+	} host_out;
 };
 
 int nva3_pll_info(struct nouveau_clock *, int, u32, u32,
