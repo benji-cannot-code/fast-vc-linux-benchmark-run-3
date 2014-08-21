f93aa7e7c046b9922e397c50d692c03e2c3efc9c
Antti Palosaari <crope@iki.fi>
2014-08-21 16:27:55 -0300

    [media] m88ts2022: rename device state (priv => dev)
    foo_dev seems to be most correct term for the structure holding data
    of each device instance. It is most used term in Kernel and also
    examples from book Linux Device Drivers, Third Edition, uses it.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/tuners/m88ts2022_priv.h
+++ drivers/media/tuners/m88ts2022_priv.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "m88ts2022.h"
 
-struct m88ts2022_priv {
+struct m88ts2022_dev {
 	struct m88ts2022_config cfg;
 	struct i2c_client *client;
 	struct dvb_frontend *fe;
