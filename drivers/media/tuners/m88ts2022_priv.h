a00dd22ee7b449994bbccee7d42f47146b52cd69
Antti Palosaari <crope@iki.fi>
2014-08-21 18:22:05 -0300

    [media] m88ts2022: convert to RegMap I2C API
    Use RegMap to cover I2C register routines.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- drivers/media/tuners/m88ts2022_priv.h
+++ drivers/media/tuners/m88ts2022_priv.h
@@ -19,10 +19,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define M88TS2022_PRIV_H
 
 #include "m88ts2022.h"
+#include <linux/regmap.h>
 
 struct m88ts2022_dev {
 	struct m88ts2022_config cfg;
 	struct i2c_client *client;
+	struct regmap *regmap;
 	struct dvb_frontend *fe;
 	u32 frequency_khz;
 };
