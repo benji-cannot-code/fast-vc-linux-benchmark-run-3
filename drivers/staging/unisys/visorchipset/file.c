36f2331d7df102fc712e0ab7f355e7aa2517212b
Andrew Morton <akpm@linux-foundation.org>
2014-08-25 16:32:20 -0700

    drivers/staging/unisys: fix build
    drivers/staging/unisys/visorchipset/file.c:31: error: redefinition of typedef 'GUEST_PHYSICAL_ADDRESS'
    drivers/staging/unisys/common-spar/include/channels/controlvmchannel.h:23: note: previous declaration of 'GUEST_PHYSICAL_ADDRESS' was here
    
    Cc: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorchipset/file.c
+++ drivers/staging/unisys/visorchipset/file.c
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CURRENT_FILE_PC VISOR_CHIPSET_PC_file_c
 
-typedef u64 GUEST_PHYSICAL_ADDRESS;
-
 static struct cdev Cdev;
 static VISORCHANNEL **PControlVm_channel;
 static dev_t MajorDev = -1; /**< indicates major num for device */
