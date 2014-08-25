22cecc73277d9eb476b86f8a25019cbf57393ec8
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2014-08-25 09:59:29 +0200

    ray_cs: Add include guards
    The files ray_cs.h and rayctl.h both contain two thirds of what
    appears to be an include guard using the macro name RAYLINK_H (both
    lack the #define). Since RAYLINK_H is not defined anywhere, the
    #ifndefs are confusing no-ops. Add proper include guards using
    different macro names.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ray_cs.h
+++ drivers/net/wireless/ray_cs.h
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    Written by Corey Thomas
 */
 
-#ifndef RAYLINK_H
+#ifndef _RAY_CS_H_
+#define _RAY_CS_H_
 
 struct beacon_rx {
     struct mac_header mac;
@@ -70,4 +71,4 @@ typedef struct ray_dev_t {
 } ray_dev_t;
 /*****************************************************************************/
 
-#endif /* RAYLINK_H */
+#endif /* _RAY_CS_H_ */
