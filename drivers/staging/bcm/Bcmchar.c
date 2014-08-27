c9d322cb7fdc026678723c24d1eba071588391a5
Matthias Beyer <mail@beyermatthias.de>
2014-08-27 12:58:04 +0200

    Staging: bcm: Bcmchar.c: Renamed variable: "WriteOffset" -> "write_off"
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm/Bcmchar.c
+++ drivers/staging/bcm/Bcmchar.c
@@ -1724,7 +1724,7 @@ static int bcm_char_ioctl_flash2x_section_write(void __user *argp,
 	void __user *input_addr;
 	UINT NOB = 0;
 	UINT buff_size = 0;
-	UINT WriteOffset = 0;
+	UINT write_off = 0;
 	UINT write_bytes = 0;
 	INT status = STATUS_FAILURE;
 
@@ -1770,7 +1770,7 @@ static int bcm_char_ioctl_flash2x_section_write(void __user *argp,
 		return STATUS_FAILURE;
 
 	input_addr = sFlash2xWrite.pDataBuff;
-	WriteOffset = sFlash2xWrite.offset;
+	write_off = sFlash2xWrite.offset;
 	NOB = sFlash2xWrite.numOfBytes;
 
 	if (NOB > ad->uiSectorSize)
@@ -1785,9 +1785,9 @@ static int bcm_char_ioctl_flash2x_section_write(void __user *argp,
 
 	/* extracting the remainder of the given offset. */
 	write_bytes = ad->uiSectorSize;
-	if (WriteOffset % ad->uiSectorSize) {
+	if (write_off % ad->uiSectorSize) {
 		write_bytes = ad->uiSectorSize -
-			(WriteOffset % ad->uiSectorSize);
+			(write_off % ad->uiSectorSize);
 	}
 
 	if (NOB < write_bytes)
@@ -1822,7 +1822,7 @@ static int bcm_char_ioctl_flash2x_section_write(void __user *argp,
 		/* Writing the data from Flash 2.x */
 		status = BcmFlash2xBulkWrite(ad, (PUINT)write_buff,
 					     sFlash2xWrite.Section,
-					     WriteOffset,
+					     write_off,
 					     write_bytes,
 					     sFlash2xWrite.bVerify);
 
@@ -1834,7 +1834,7 @@ static int bcm_char_ioctl_flash2x_section_write(void __user *argp,
 
 		NOB = NOB - write_bytes;
 		if (NOB) {
-			WriteOffset = WriteOffset + write_bytes;
+			write_off = write_off + write_bytes;
 			input_addr = input_addr + write_bytes;
 			if (NOB > ad->uiSectorSize)
 				write_bytes = ad->uiSectorSize;
