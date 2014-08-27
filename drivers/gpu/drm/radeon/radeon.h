210c6345320814bb289c78a19383fc0bacc69d88
Maarten Lankhorst <maarten.lankhorst@canonical.com>
2014-08-27 16:45:18 -0400

    drm/radeon: take exclusive_lock in read mode during ring tests, v5
    This is needed for the next commit, because the lockup detection
    will need the read lock to run.
    
    v4 (chk): split out forced fence completion, remove unrelated changes,
              add and handle in_reset flag
    v5 (agd5f): rebase fix
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@canonical.com>
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -2327,7 +2327,7 @@ struct radeon_device {
 	bool				need_dma32;
 	bool				accel_working;
 	bool				fastfb_working; /* IGP feature*/
-	bool				needs_reset;
+	bool				needs_reset, in_reset;
 	struct radeon_surface_reg surface_regs[RADEON_GEM_MAX_SURFACES];
 	const struct firmware *me_fw;	/* all family ME firmware */
 	const struct firmware *pfp_fw;	/* r6/700 PFP firmware */
