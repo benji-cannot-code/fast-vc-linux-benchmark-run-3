4e5e7eaf840f4f7653b6c3b7e198a2b8e3bdf90e
John L. Hammond <john.hammond@intel.com>
2014-08-28 18:35:16 -0500

    staging/lustre: remove linux/lustre_net.h
    Remove the superfluous header lustre/include/linux/lustre_net.h.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre_net.h
+++ drivers/staging/lustre/lustre/include/lustre_net.h
@@ -56,8 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @{
  */
 
-#include "linux/lustre_net.h"
-
 #include "../../include/linux/libcfs/libcfs.h"
 // #include <obd.h>
 #include "../../include/linux/lnet/lnet.h"
--- drivers/staging/lustre/lustre/ptlrpc/service.c
+++ drivers/staging/lustre/lustre/ptlrpc/service.c
@@ -2264,9 +2264,7 @@ static int ptlrpc_main(void *arg)
 	struct ptlrpc_service_part	*svcpt = thread->t_svcpt;
 	struct ptlrpc_service		*svc = svcpt->scp_service;
 	struct ptlrpc_reply_state	*rs;
-#ifdef WITH_GROUP_INFO
 	struct group_info *ginfo = NULL;
-#endif
 	struct lu_env *env;
 	int counter = 0, rc = 0;
 
@@ -2282,7 +2280,6 @@ static int ptlrpc_main(void *arg)
 		      svc->srv_name, thread->t_name, svcpt->scp_cpt);
 	}
 
-#ifdef WITH_GROUP_INFO
 	ginfo = groups_alloc(0);
 	if (!ginfo) {
 		rc = -ENOMEM;
@@ -2291,7 +2288,6 @@ static int ptlrpc_main(void *arg)
 
 	set_current_groups(ginfo);
 	put_group_info(ginfo);
-#endif
 
 	if (svc->srv_ops.so_thr_init != NULL) {
 		rc = svc->srv_ops.so_thr_init(thread);
