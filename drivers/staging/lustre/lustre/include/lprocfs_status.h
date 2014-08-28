44220f38fe063f8e3be570d0d39a8d5d1cb6b15e
John L. Hammond <john.hammond@intel.com>
2014-08-28 18:35:09 -0500

    staging/lustre: remove linux/lprocfs_status.h
    Remove lustre/include/linux/lprocfs_status.h. Include linux/statfs.h
    where needed.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lprocfs_status.h
+++ drivers/staging/lustre/lustre/include/lprocfs_status.h
@@ -43,7 +43,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LPROCFS_SNMP_H
 #define _LPROCFS_SNMP_H
 
-#include "linux/lprocfs_status.h"
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/spinlock.h>
+#include <linux/types.h>
+
 #include "lustre/lustre_idl.h"
 
 struct lprocfs_vars {
--- drivers/staging/lustre/lustre/include/lustre_lib.h
+++ drivers/staging/lustre/lustre/include/lustre_lib.h
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "linux/lustre_lib.h"
 
 /* target.c */
+struct kstatfs;
 struct ptlrpc_request;
 struct obd_export;
 struct lu_target;
