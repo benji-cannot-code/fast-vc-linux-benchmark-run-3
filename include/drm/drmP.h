3ed71eed4255db8801b83d5216bace5204f286e9
David Herrmann <dh.herrmann@gmail.com>
2014-08-29 12:12:29 +0200

    drm: move "struct drm_vma_entry" to drm_vm.c
    Make all the drm_vma_entry handling local to drm_vm.c and hide it from
    global headers. This requires to extract the inlined legacy drm_vma_entry
    cleanup into a small helper and also move a weirdly placed drm_vma_info
    helper into drm_vm.c.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -297,12 +297,6 @@ struct drm_magic_entry {
 	struct drm_file *priv;
 };
 
-struct drm_vma_entry {
-	struct list_head head;
-	struct vm_area_struct *vma;
-	pid_t pid;
-};
-
 /**
  * DMA buffer.
  */
@@ -1449,6 +1443,8 @@ struct drm_local_map *drm_legacy_getsarea(struct drm_device *dev);
 
 int drm_legacy_addbufs_agp(struct drm_device *d, struct drm_buf_desc *req);
 int drm_legacy_addbufs_pci(struct drm_device *d, struct drm_buf_desc *req);
+
+void drm_legacy_vma_flush(struct drm_device *d);
 
 			       /* sysfs support (drm_sysfs.c) */
 struct drm_sysfs_class;
