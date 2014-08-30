ea4ca7981bfc7f0656dea2c51f6eb8ded7ba3abd
Max Vozeler <max@hinterhof.net>
2014-08-30 15:06:30 +0200

    staging: rtl8188eu: Add missing include to pick up prototype
    This fixes the following sparse warning:
    
    drivers/staging/rtl8188eu/hal/HalHWImg8188E_MAC.c:119:6: warning: symbol
    'rtl88e_phy_mac_config' was not declared. Should it be static?
    
    Signed-off-by: Max Vozeler <max@hinterhof.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/HalHWImg8188E_MAC.c
+++ drivers/staging/rtl8188eu/hal/HalHWImg8188E_MAC.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 ******************************************************************************/
 
 #include "odm_precomp.h"
+#include "phy.h"
 #include <rtw_iol.h>
 
 /* MAC_REG.TXT */
