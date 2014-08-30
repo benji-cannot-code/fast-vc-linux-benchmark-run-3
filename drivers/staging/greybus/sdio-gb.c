9c2fc549e11b026c8e67d471f761812e772dd125
Greg Kroah-Hartman <greg@kroah.com>
2014-08-30 16:20:22 -0700

    greybus: start moving the function types into the greybus core

--- drivers/staging/greybus/sdio-gb.c
+++ drivers/staging/greybus/sdio-gb.c
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/host.h>
 #include "greybus.h"
 
-struct sd_gb_host {
+struct gb_sdio_host {
 	struct mmc_host	*mmc;
 	struct mmc_request *mrq;
 	// FIXME - some lock?
@@ -49,9 +49,9 @@ static const struct mmc_host_ops gb_sd_ops = {
 static int sd_gb_probe(struct greybus_device *gdev, const struct greybus_device_id *id)
 {
 	struct mmc_host *mmc;
-	struct sd_gb_host *host;
+	struct gb_sdio_host *host;
 
-	mmc = mmc_alloc_host(sizeof(struct sd_gb_host), &gdev->dev);
+	mmc = mmc_alloc_host(sizeof(struct gb_sdio_host), &gdev->dev);
 	if (!mmc)
 		return -ENOMEM;
 
@@ -68,7 +68,7 @@ static int sd_gb_probe(struct greybus_device *gdev, const struct greybus_device_
 static void sd_gb_disconnect(struct greybus_device *gdev)
 {
 	struct mmc_host *mmc;
-	struct sd_gb_host *host;
+	struct gb_sdio_host *host;
 
 	host = greybus_get_drvdata(gdev);
 	mmc = host->mmc;
