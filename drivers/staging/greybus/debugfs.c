caf14d03cbffb61aba6b73d01e5bd88897b533d4
Greg Kroah-Hartman <greg@kroah.com>
2014-08-31 13:54:59 -0700

    greybus: register the bus with the driver core and add framework for debugfs files.

---
+++ drivers/staging/greybus/debugfs.c
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Greybus debugfs code
+ *
+ * Copyright 2014 Google Inc.
+ *
+ * Released under the GPLv2 only.
+ */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/types.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/debugfs.h>
+
+#include "greybus.h"
+
+static struct dentry *gb_debug_root;
+
+int greybus_debugfs_init(void)
+{
+	gb_debug_root = debugfs_create_dir("greybus", NULL);
+	if (!gb_debug_root)
+		return -ENOENT;
+
+	return 0;
+}
+
+void greybus_debugfs_cleanup(void)
+{
+	debugfs_remove_recursive(gb_debug_root);
+}
