63955d8baf95e8a982bea2d865291da147d00300
navin patidar <navin.patidar@gmail.com>
2014-08-31 12:14:17 +0530

    staging: rtl8188eu: Rework function PHY_QueryBBReg()
    Rename CamelCase variables and function name.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/phy.h
+++ drivers/staging/rtl8188eu/include/phy.h
@@ -2,3 +2,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 bool rtl88eu_phy_mac_config(struct adapter *adapt);
 bool rtl88eu_phy_rf_config(struct adapter *adapt);
 bool rtl88eu_phy_bb_config(struct adapter *adapt);
+
+u32 phy_query_bb_reg(struct adapter *adapt, u32 regaddr, u32 bitmask);
