8c61beaace41ac5d53924a21769d018cc5708f73
Greg Donald <gdonald@gmail.com>
2014-09-01 06:36:14 -0500

    drivers: staging: lustre: Fix 'do not use C99 // comments' errors
    Fix checkpatch.pl 'do not use C99 // comments' errors
    
    Signed-off-by: Greg Donald <gdonald@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/dir.c
+++ drivers/staging/lustre/lustre/llite/dir.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pagemap.h>
 #include <linux/mm.h>
 #include <asm/uaccess.h>
-#include <linux/buffer_head.h>   // for wait_on_buffer
+#include <linux/buffer_head.h>   /* for wait_on_buffer */
 #include <linux/pagevec.h>
 #include <linux/prefetch.h>
 
