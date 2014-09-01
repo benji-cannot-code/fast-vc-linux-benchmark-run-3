255d81074d88150a24c3aa386a09c73924930460
Antti Palosaari <crope@iki.fi>
2014-08-31 22:57:26 -0300

    [media] af9033: remove I2C addr from config
    I2C driver address is passed as a i2c_new_device() parameter when
    device is created. Thus no need to keep it in config struct.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/af9033.h
+++ drivers/media/dvb-frontends/af9033.h
@@ -25,12 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kconfig.h>
 
+/*
+ * I2C address (TODO: are these in 8-bit format?)
+ * 0x38, 0x3a, 0x3c, 0x3e
+ */
 struct af9033_config {
-	/*
-	 * I2C address
-	 */
-	u8 i2c_addr;
-
 	/*
 	 * clock Hz
 	 * 12000000, 22000000, 24000000, 34000000, 32000000, 28000000, 26000000,
