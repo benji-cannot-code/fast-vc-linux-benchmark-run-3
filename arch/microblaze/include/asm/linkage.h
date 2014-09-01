45988b28232234b1f85d5ce94fec71659f06bad5
Michal Simek <michal.simek@xilinx.com>
2014-09-01 15:56:23 +0200

    microblaze: Use empty asm-generic/linkage.h
    The difference between microblaze and default version
    in linux/linkage.h is just value stored in the padding bytes
    which was 0 and in generic is 0x90. Different value shouldn't have
    any effect.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/include/asm/linkage.h
+++ arch/microblaze/include/asm/linkage.h
@@ -1,16 +1,2 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/*
- * Copyright (C) 2006 Atmark Techno, Inc.
- *
- * This file is subject to the terms and conditions of the GNU General Public
- * License. See the file "COPYING" in the main directory of this archive
- * for more details.
- */
-
-#ifndef _ASM_MICROBLAZE_LINKAGE_H
-#define _ASM_MICROBLAZE_LINKAGE_H
-
-#define __ALIGN		.align 4
-#define __ALIGN_STR	".align 4"
-
-#endif /* _ASM_MICROBLAZE_LINKAGE_H */
+#include <asm-generic/linkage.h>
