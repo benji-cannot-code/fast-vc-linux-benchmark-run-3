e3f248bd7801c680cc0d56e52876ed1c7a87b051
navin patidar <navin.patidar@gmail.com>
2014-09-01 21:13:43 +0530

    staging: rtl8188eu: Rename Hal8188EPwrSeq.h to pwrseq.h
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtl8188e_hal.h
+++ drivers/staging/rtl8188eu/include/rtl8188e_hal.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rtl8188e_recv.h"
 #include "rtl8188e_xmit.h"
 #include "rtl8188e_cmd.h"
-#include "Hal8188EPwrSeq.h"
+#include "pwrseq.h"
 #include "rtw_efuse.h"
 #include "rtw_sreset.h"
 #include "odm_precomp.h"
