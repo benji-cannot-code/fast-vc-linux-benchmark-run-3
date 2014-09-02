0870730aaf9cb7d8f99014be3a53649a344940bc
Mark A. Greer <mgreer@animalcreek.com>
2014-09-02 15:12:20 -0700

    NFC: trf7970a: FIFO Size is really 127 bytes
    Despite what the manual says, the FIFO size
    on the trf7970a is really 127 bytes so make
    the code respect that.
    
    Signed-off-by: Mark A. Greer <mgreer@animalcreek.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/trf7970a.c
+++ drivers/nfc/trf7970a.c
@@ -126,7 +126,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define TRF7970A_RX_SKB_ALLOC_SIZE		256
 
-#define TRF7970A_FIFO_SIZE			128
+#define TRF7970A_FIFO_SIZE			127
 
 /* TX length is 3 nibbles long ==> 4KB - 1 bytes max */
 #define TRF7970A_TX_MAX				(4096 - 1)
