210b387d81ff1d17bae9320d79040cdd5a5988b6
Loic Pefferkorn <loic@loicp.eu>
2014-09-03 22:23:14 +0200

    staging: goldfish: document mutex usage
    Coding style: document mutex usage
    
    Signed-off-by: Loic Pefferkorn <loic@loicp.eu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/goldfish/goldfish_nand.c
+++ drivers/staging/goldfish/goldfish_nand.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "goldfish_nand_reg.h"
 
 struct goldfish_nand {
+	/* lock protects access to the device registers */
 	struct mutex            lock;
 	unsigned char __iomem  *base;
 	struct cmd_params       *cmd_params;
