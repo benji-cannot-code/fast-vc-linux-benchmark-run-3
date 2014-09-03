f16cb7a0aa7b2215540feca0df4f71fc74db34b6
Christophe Ricard <christophe.ricard@gmail.com>
2014-09-03 23:30:32 +0200

    NFC: st21nfcb: Remove useless headers
    Remove some headers as they are:
    - not relevent for st21nfcb.
    - called multiple times for no reason: example st21nfcb.h already include
    ndlc.h.
    
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/st21nfcb/i2c.c
+++ drivers/nfc/st21nfcb/i2c.c
@@ -18,23 +18,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/crc-ccitt.h>
 #include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/gpio.h>
 #include <linux/of_irq.h>
 #include <linux/of_gpio.h>
-#include <linux/miscdevice.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/nfc.h>
-#include <linux/firmware.h>
-#include <linux/unaligned/access_ok.h>
 #include <linux/platform_data/st21nfcb.h>
-
-#include <net/nfc/nci.h>
-#include <net/nfc/llc.h>
-#include <net/nfc/nfc.h>
 
 #include "ndlc.h"
 
--- drivers/nfc/st21nfcb/st21nfcb.c
+++ drivers/nfc/st21nfcb/st21nfcb.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/nfc/nci_core.h>
 
 #include "st21nfcb.h"
-#include "ndlc.h"
 
 #define DRIVER_DESC "NCI NFC driver for ST21NFCB"
 
