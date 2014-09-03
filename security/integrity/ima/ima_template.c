4d7fd87c9c26ce0235393be513ebc1f134807d36
Dmitry Kasatkin <d.kasatkin@samsung.com>
2014-09-03 10:19:58 +0300

    ima: add missing '__init' keywords
    Add missing keywords to the function definition to cleanup
    to discard initialization code.
    
    Signed-off-by: Dmitry Kasatkin <d.kasatkin@samsung.com>
    Reviewed-by: Roberto Sassu <roberto.sassu@polito.it>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/ima/ima_template.c
+++ security/integrity/ima/ima_template.c
@@ -153,7 +153,7 @@ out:
 	return result;
 }
 
-static int init_defined_templates(void)
+static int __init init_defined_templates(void)
 {
 	int i = 0;
 	int result = 0;
@@ -179,7 +179,7 @@ struct ima_template_desc *ima_template_desc_current(void)
 	return ima_template;
 }
 
-int ima_init_template(void)
+int __init ima_init_template(void)
 {
 	int result;
 
