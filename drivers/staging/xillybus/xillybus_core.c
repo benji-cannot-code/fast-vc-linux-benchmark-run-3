f542dbc9a5e9763d0c3eec969d02bdf6a74bf48f
Eli Billauer <eli.billauer@gmail.com>
2014-09-04 17:47:42 +0300

    staging: xillybus: Non-interruptible mutex_lock() on release method
    Responding to an interrupt while handling the "release" method can't end
    well anyhow. In practical cases, this can force the user to wait up to
    one second while flushing remaining data is attempted (a timeout on the flush
    mechanism limits the time for flushing).
    
    Suggested-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Eli Billauer <eli.billauer@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xillybus/xillybus_core.c
+++ drivers/staging/xillybus/xillybus_core.c
@@ -1589,7 +1589,6 @@ unlock_wr:
 
 static int xillybus_release(struct inode *inode, struct file *filp)
 {
-	int rc;
 	unsigned long flags;
 	struct xilly_channel *channel = filp->private_data;
 
@@ -1600,13 +1599,7 @@ static int xillybus_release(struct inode *inode, struct file *filp)
 		return -EIO;
 
 	if (filp->f_mode & FMODE_WRITE) {
-		rc = mutex_lock_interruptible(&channel->rd_mutex);
-
-		if (rc) {
-			dev_warn(channel->endpoint->dev,
-				 "Failed to close file. Hardware left in messy state.\n");
-			return rc;
-		}
+		mutex_lock(&channel->rd_mutex);
 
 		channel->rd_ref_count--;
 
@@ -1626,12 +1619,7 @@ static int xillybus_release(struct inode *inode, struct file *filp)
 	}
 
 	if (filp->f_mode & FMODE_READ) {
-		rc = mutex_lock_interruptible(&channel->wr_mutex);
-		if (rc) {
-			dev_warn(channel->endpoint->dev,
-				 "Failed to close file. Hardware left in messy state.\n");
-			return rc;
-		}
+		mutex_lock(&channel->wr_mutex);
 
 		channel->wr_ref_count--;
 
