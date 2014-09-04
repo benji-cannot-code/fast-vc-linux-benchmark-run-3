46560c86aa8fbc14d87533bbaa28a1fc18ccfd50
Malcolm Priestley <tvboxspy@gmail.com>
2014-09-04 23:08:36 +0100

    staging: vt6655: device_free_info() warn: variable dereferenced before check
    pDevice is never NULL when this function is called remove the check.
    
    This fixes smatch warning
    drivers/staging/vt6655/device_main.c:1023 device_free_info() warn: variable dereferenced
    before check 'pDevice' (see line 1021)
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: kbuild@01.org
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/device_main.c
+++ drivers/staging/vt6655/device_main.c
@@ -1009,9 +1009,6 @@ static void device_free_info(struct vnt_private *pDevice)
 {
 	struct net_device *dev = pDevice->dev;
 
-	if (!pDevice)
-		return;
-
 	ASSERT(pDevice);
 //2008-0714-01<Add>by chester
 	device_release_WPADEV(pDevice);
