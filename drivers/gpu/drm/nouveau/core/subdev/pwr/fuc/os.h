441cb3fabc55cb46a9a734b6305f0c5cec07f5e4
Roy Spliet <rspliet@eclipso.eu>
2014-09-04 16:58:49 +0200

    drm/nva3/pwr/memx: Implement "wait for VBLANK"
    Signed-off-by: Roy Spliet <rspliet@eclipso.eu>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/pwr/fuc/os.h
+++ drivers/gpu/drm/nouveau/core/subdev/pwr/fuc/os.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MEMX_WR32   2
 #define MEMX_WAIT   3
 #define MEMX_DELAY  4
+#define MEMX_VBLANK 5
 
 /* I2C_: message identifiers */
 #define I2C__MSG_RD08 0
