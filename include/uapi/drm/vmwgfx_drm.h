cbd5598bf9173f522e7f71650a39175a86efd74e
Josh Boyer <jwboyer@fedoraproject.org>
2014-09-05 13:19:59 -0400

    drm/vmwgfx: Fix drm.h include
    The userspace drm.h include doesn't prefix the drm directory.  This can lead
    to compile failures as /usr/include/drm/ isn't in the standard gcc include
    paths.  Fix it to be <drm/drm.h>, which matches the rest of the driver drm
    header files that get installed into /usr/include/drm.
    
    Red Hat Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1138759
    
    Fixes: 1d7a5cbf8f74e
    Reported-by: Jeffrey Bastian <jbastian@redhat.com>
    Signed-off-by: Josh Boyer <jwboyer@fedoraproject.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/uapi/drm/vmwgfx_drm.h
+++ include/uapi/drm/vmwgfx_drm.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __VMWGFX_DRM_H__
 
 #ifndef __KERNEL__
-#include <drm.h>
+#include <drm/drm.h>
 #endif
 
 #define DRM_VMW_MAX_SURFACE_FACES 6
