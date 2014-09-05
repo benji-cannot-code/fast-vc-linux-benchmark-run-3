aa3898e5d0a9ad9a4bc2fc5238a58b048d04b8ba
Gaurav Minocha <gaurav.minocha.os@gmail.com>
2014-09-05 09:56:13 -0700

    of: Disabling OF functions that use sysfs if CONFIG_SYSFS disabled
    This patch is to the fix the recent runtime bug in kernel reported by
    <fengguang.wu@intel.com>. The bug was exposed by commit b951f9dc,
    "Enabling OF selftest to run without machine's devicetree" and is
    exposed when CONFIG_OF_SELFTEST is enabled and CONFIG_SYSFS is
    disabled.
    
    Mail Subject: [OF test] BUG: unable to handle kernel NULL pointer
    dereference at 00000038
    
    Tested on x86 and arm architecture
    
    Signed-off-by: Gaurav Minocha <gaurav.minocha.os@gmail.com>
    Signed-off-by: Grant Likely <grant.likely@linaro.org>

--- drivers/of/base.c
+++ drivers/of/base.c
@@ -139,6 +139,9 @@ int __of_add_property_sysfs(struct device_node *np, struct property *pp)
 	/* Important: Don't leak passwords */
 	bool secure = strncmp(pp->name, "security-", 9) == 0;
 
+	if (!IS_ENABLED(CONFIG_SYSFS))
+		return 0;
+
 	if (!of_kset || !of_node_is_attached(np))
 		return 0;
 
@@ -158,6 +161,9 @@ int __of_attach_node_sysfs(struct device_node *np)
 	const char *name;
 	struct property *pp;
 	int rc;
+
+	if (!IS_ENABLED(CONFIG_SYSFS))
+		return 0;
 
 	if (!of_kset)
 		return 0;
@@ -1714,6 +1720,9 @@ int __of_remove_property(struct device_node *np, struct property *prop)
 
 void __of_remove_property_sysfs(struct device_node *np, struct property *prop)
 {
+	if (!IS_ENABLED(CONFIG_SYSFS))
+		return;
+
 	/* at early boot, bail here and defer setup to of_init() */
 	if (of_kset && of_node_is_attached(np))
 		sysfs_remove_bin_file(&np->kobj, &prop->attr);
@@ -1778,6 +1787,9 @@ int __of_update_property(struct device_node *np, struct property *newprop,
 void __of_update_property_sysfs(struct device_node *np, struct property *newprop,
 		struct property *oldprop)
 {
+	if (!IS_ENABLED(CONFIG_SYSFS))
+		return;
+
 	/* At early boot, bail out and defer setup to of_init() */
 	if (!of_kset)
 		return;
