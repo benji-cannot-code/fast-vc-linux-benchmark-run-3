855e3c6dd7df07ab6fcd721f67e68f1314a5252b
Rob Clark <robdclark@gmail.com>
2014-09-05 15:03:40 -0400

    drm/msm/adreno: bit of init refactoring
    Push a few bits down into adreno_gpu so they won't have to be duplicated
    as support for additional adreno generations is added.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/adreno/adreno_device.c
+++ drivers/gpu/drm/msm/adreno/adreno_device.c
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ANY_ID 0xff
 
+bool hang_debug = false;
+MODULE_PARM_DESC(hang_debug, "Dump registers when hang is detected (can be slow!)");
+module_param_named(hang_debug, hang_debug, bool, 0600);
+
 struct msm_gpu *a3xx_gpu_init(struct drm_device *dev);
 
 static const struct adreno_info gpulist[] = {
