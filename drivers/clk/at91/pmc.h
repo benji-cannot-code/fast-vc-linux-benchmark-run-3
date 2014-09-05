f64b7afba785a1ad0ec9d02a7d8718ebe4c140a3
Boris Brezillon <boris.brezillon@free-electrons.com>
2014-09-05 09:54:13 +0200

    clk: at91: make use of syscon to share PMC registers in several drivers
    The PMC block is providing several functionnalities:
     - system clk management
     - cpuidle
     - platform suspend
    
    Replace the void __iomem *regs field by a regmap (retrieved using syscon)
    so that we can later share the regmap across several drivers without
    exporting a new specific API or a global void __iomem * variable.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Acked-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/at91/pmc.h
+++ drivers/clk/at91/pmc.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/io.h>
 #include <linux/irqdomain.h>
+#include <linux/regmap.h>
 #include <linux/spinlock.h>
 
 struct clk_range {
@@ -29,7 +30,7 @@ struct at91_pmc_caps {
 };
 
 struct at91_pmc {
-	void __iomem *regbase;
+	struct regmap *regmap;
 	int virq;
 	spinlock_t lock;
 	const struct at91_pmc_caps *caps;
@@ -49,12 +50,16 @@ static inline void pmc_unlock(struct at91_pmc *pmc)
 
 static inline u32 pmc_read(struct at91_pmc *pmc, int offset)
 {
-	return readl(pmc->regbase + offset);
+	unsigned int ret = 0;
+
+	regmap_read(pmc->regmap, offset, &ret);
+
+	return ret;
 }
 
 static inline void pmc_write(struct at91_pmc *pmc, int offset, u32 value)
 {
-	writel(value, pmc->regbase + offset);
+	regmap_write(pmc->regmap, offset, value);
 }
 
 int of_at91_get_clk_range(struct device_node *np, const char *propname,
