a844e5b9563a4d895d0c48e3c45b722c5f48e9d0
navin patidar <navin.patidar@gmail.com>
2014-09-07 16:37:40 +0530

    staging: rtl8188eu: Rework function ODM_TxPwrTrackAdjust88E()
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/HalPhyRf_8188e.h
+++ drivers/staging/rtl8188eu/include/HalPhyRf_8188e.h
@@ -28,12 +28,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AVG_THERMAL_NUM_88E	4
 #define ODM_TARGET_CHNL_NUM_2G_5G	59
 
-void ODM_TxPwrTrackAdjust88E(struct odm_dm_struct *pDM_Odm,
-			     u8 Type,	/* 0 = OFDM, 1 = CCK */
-			     u8 *pDirection,/* 1 = +(incr) 2 = -(decr) */
-			     u32 *pOutWriteVal); /* Tx tracking CCK/OFDM BB
-						     * swing index adjust */
-
+void rtl88eu_dm_txpower_track_adjust(struct odm_dm_struct *dm_odm,
+				     u8 type, /* 0 = OFDM, 1 = CCK */
+				     u8 *dir, /* 1 = +(incr), 2 = -(decr) */
+				     u32 *out_write); /* Tx tracking CCK/OFDM BB
+						       * swing index adjust
+						       */
 
 void odm_TXPowerTrackingCallback_ThermalMeter_8188E(struct adapter *Adapter);
 
