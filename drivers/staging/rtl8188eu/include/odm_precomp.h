6b64e04b7c24938378ef3400664d89b10be8b008
navin patidar <navin.patidar@gmail.com>
2014-09-07 16:38:03 +0530

    staging: rtl8188eu: Remove HalPhyRf_8188e.h
    Move macros and function declarations from HalPhyRf_8188e.h
    to phy.h .
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm_precomp.h
+++ drivers/staging/rtl8188eu/include/odm_precomp.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "odm_RegDefine11AC.h"
 #include "odm_RegDefine11N.h"
 
-#include "HalPhyRf_8188e.h"/* for IQK,LCK,Power-tracking */
 #include "Hal8188ERateAdaptive.h"/* for  RA,Power training */
 #include "rtl8188e_hal.h"
 
