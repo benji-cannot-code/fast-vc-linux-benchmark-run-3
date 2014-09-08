b18097188c3a9f506ba55fe5b6ec310bf0e3ab68
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2014-09-08 15:52:08 +0200

    drm: sti: allow to change hdmi ddc i2c adapter
    Depending of the board configuration i2c for ddc could change,
    this patch allow to use a phandle to specify which i2c controller to use.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>

--- drivers/gpu/drm/sti/sti_hdmi.h
+++ drivers/gpu/drm/sti/sti_hdmi.h
@@ -63,6 +63,7 @@ struct sti_hdmi {
 	wait_queue_head_t wait_event;
 	bool event_received;
 	struct reset_control *reset;
+	struct i2c_adapter *ddc_adapt;
 };
 
 u32 hdmi_read(struct sti_hdmi *hdmi, int offset);
