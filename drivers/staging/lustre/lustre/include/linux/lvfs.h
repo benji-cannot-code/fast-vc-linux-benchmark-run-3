6a99e0d6bb310829d21c9af6ff3b91f81054c373
John L. Hammond <john.hammond@intel.com>
2014-09-09 13:39:03 -0500

    staging/lustre: remove linux/lustre_common.h
    Expand the two uses of ll_inode_blksize() and remove the then
    unnecessary header lustre/include/linux/lustre_common.h.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-on: http://review.whamcloud.com/11495
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/linux/lvfs.h
+++ drivers/staging/lustre/lustre/include/linux/lvfs.h
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #include "lustre_compat25.h"
-#include "lustre_common.h"
 #include "lvfs_linux.h"
 
 #define LLOG_LVFS
