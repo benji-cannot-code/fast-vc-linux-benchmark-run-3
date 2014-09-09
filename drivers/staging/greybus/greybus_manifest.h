fc0976b67ca617770c359f26c11566bdb6c034ea
Alex Elder <elder@linaro.org>
2014-09-09 13:55:04 -0500

    greybus: define struct greybus_manifest
    Define a structure that describes the entire greybus manifest.
    Adjust greybus_new_module() to use that, making it explicit that
    it's not just a header that's being provided to that function.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/greybus_manifest.h
+++ drivers/staging/greybus/greybus_manifest.h
@@ -95,6 +95,11 @@ struct greybus_descriptor {
 	};
 };
 
+struct greybus_manifest {
+	struct greybus_manifest_header		header;
+	struct greybus_descriptor		descriptors[0];
+};
+
 #pragma pack(pop)
 
 #endif /* __GREYBUS_DESC_H */
