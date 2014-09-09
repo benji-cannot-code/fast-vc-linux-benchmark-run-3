d65521a28428114498f054e9f0af09a64daab93d
John L. Hammond <john.hammond@intel.com>
2014-09-09 13:39:08 -0500

    staging/lustre: remove lvfs.h
    Move the definition of struct lvfs_run_ctxt to the one file that
    needed it (lustre/include/obd.h). Remove the then unneeded headers
    lustre/include/lvfs.h and lustre/include/linux/lvfs.h.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/cl_object.h
+++ drivers/staging/lustre/lustre/include/cl_object.h
@@ -99,7 +99,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * super-class definitions.
  */
 #include "lu_object.h"
-#include "lvfs.h"
+#include "linux/lustre_compat25.h"
 #include <linux/mutex.h>
 #include <linux/radix-tree.h>
 
--- drivers/staging/lustre/lustre/obdclass/obd_mount.c
+++ drivers/staging/lustre/lustre/obdclass/obd_mount.c
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PRINT_CMD CDEBUG
 
 #include "../include/obd.h"
-#include "../include/lvfs.h"
+#include "../include/linux/lustre_compat25.h"
 #include "../include/obd_class.h"
 #include "../include/lustre/lustre_user.h"
 #include "../include/lustre_log.h"
