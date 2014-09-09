37d3205805dcf23adc9742dacd57e0b5eb56d134
John L. Hammond <john.hammond@intel.com>
2014-09-09 13:39:07 -0500

    staging/lustre/libcfs: remove upcall cache
    The upcall cache is unused so remove include/linux/libcfs/lucache.h
    and lustre/libcfs/upcall_cache.c.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lvfs.h
+++ drivers/staging/lustre/lustre/include/lvfs.h
@@ -43,6 +43,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../../include/linux/libcfs/libcfs.h"
 #include "linux/lvfs.h"
 
-#include "../../include/linux/libcfs/lucache.h"
-
 #endif
