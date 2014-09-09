3d69ab2dced465e0bfbd9525ddf82a32eae4efd8
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-09-09 16:15:57 -0700

    staging: comedi: pcmuio: remove 'continuous' from private data
    This member of the private data is set based on the cmd->stop_src. Just use the
    cmd->stop_src directly and remove the member.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/pcmuio.c
+++ drivers/staging/comedi/drivers/pcmuio.c
@@ -133,7 +133,6 @@ struct pcmuio_asic {
 	unsigned int enabled_mask;
 	unsigned int stop_count;
 	unsigned int active:1;
-	unsigned int continuous:1;
 };
 
 struct pcmuio_private {
@@ -350,8 +349,7 @@ static void pcmuio_handle_intr_subdev(struct comedi_device *dev,
 	}
 
 	/* Check for end of acquisition. */
-	if (!chip->continuous) {
-		/* stop_src == TRIG_COUNT */
+	if (cmd->stop_src == TRIG_COUNT) {
 		if (chip->stop_count > 0) {
 			chip->stop_count--;
 			if (chip->stop_count == 0) {
@@ -500,12 +498,10 @@ static int pcmuio_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 	/* Set up end of acquisition. */
 	switch (cmd->stop_src) {
 	case TRIG_COUNT:
-		chip->continuous = 0;
 		chip->stop_count = cmd->stop_arg;
 		break;
 	default:
 		/* TRIG_NONE */
-		chip->continuous = 1;
 		chip->stop_count = 0;
 		break;
 	}
