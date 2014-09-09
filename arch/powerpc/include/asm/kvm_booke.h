81fff642abb6ed0c1fe73f3744b3910a60e0089b
Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
2014-09-09 22:37:36 +0530

    powerpc/kvm: common sw breakpoint instr across ppc
    This patch extends the use of illegal instruction as software
    breakpoint instruction across the ppc platform. Patch extends
    booke program interrupt code to support software breakpoint.
    
    Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
    [agraf: Fix bookehv]
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- arch/powerpc/include/asm/kvm_booke.h
+++ arch/powerpc/include/asm/kvm_booke.h
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EHPRIV_OC_SHIFT			11
 /* "ehpriv 1" : ehpriv with OC = 1 is used for debug emulation */
 #define EHPRIV_OC_DEBUG			1
-#define KVMPPC_INST_EHPRIV_DEBUG	(KVMPPC_INST_EHPRIV | \
-					 (EHPRIV_OC_DEBUG << EHPRIV_OC_SHIFT))
 
 static inline void kvmppc_set_gpr(struct kvm_vcpu *vcpu, int num, ulong val)
 {
