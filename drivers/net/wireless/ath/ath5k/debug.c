61d04bfb959ea89195088ed02981e6e9ed236653
Stephen Rothwell <sfr@canb.auug.org.au>
2014-09-09 16:37:11 -0700

    ath5k: Add missing vmalloc.h include.
    After merging the wireless-next tree, today's linux-next build (powerpc
    allyesconfig) failed like this:
    
    drivers/net/wireless/ath/ath5k/debug.c: In function 'open_file_eeprom':
    drivers/net/wireless/ath/ath5k/debug.c:933:2: error: implicit declaration of function 'vmalloc' [-Werror=implicit-function-declaration]
      buf = vmalloc(eesize);
      ^
    drivers/net/wireless/ath/ath5k/debug.c:933:6: warning: assignment makes pointer from integer without a cast
      buf = vmalloc(eesize);
          ^
    drivers/net/wireless/ath/ath5k/debug.c:960:2: error: implicit declaration of function 'vfree' [-Werror=implicit-function-declaration]
      vfree(buf);
      ^
    
    Caused by commit db906eb2101b ("ath5k: added debugfs file for dumping
    eeprom").  Also reported by Guenter Roeck.
    
    I have used Geert Uytterhoeven's suggested fix of including vmalloc.h
    and so added this patch for today:
    
    From: Stephen Rothwell <sfr@canb.auug.org.au>
    Date: Mon, 8 Sep 2014 18:39:23 +1000
    Subject: [PATCH] ath5k: fix debugfs addition
    
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Suggested-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/ath/ath5k/debug.c
+++ drivers/net/wireless/ath/ath5k/debug.c
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/export.h>
 #include <linux/moduleparam.h>
+#include <linux/vmalloc.h>
 
 #include <linux/seq_file.h>
 #include <linux/list.h>
