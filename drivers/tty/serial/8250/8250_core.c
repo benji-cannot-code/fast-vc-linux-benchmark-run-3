134d7c36b02725b6608196a7e0aa5286413660b4
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2014-09-09 07:17:45 +0200

    serial/8250_core: Add reference to uacess.h
    Commit: e676253b19b2d269cccf67fdb1592120a0cd0676 [3/21] serial/8250: Add
    support for RS485 IOCTLs, adds a building error on arch m32r.
    
    All error/warnings:
    
       drivers/tty/serial/8250/8250_core.c: In function 'serial8250_ioctl':
    >> drivers/tty/serial/8250/8250_core.c:2859:3: error: implicit declaration of function 'copy_from_user' [-Werror=implicit-function-declaration]
          if (copy_from_user(&rs485_config, (void __user *)arg,
          ^
    >> drivers/tty/serial/8250/8250_core.c:2871:3: error: implicit declaration of function 'copy_to_user' [-Werror=implicit-function-declaration]
          if (copy_to_user((void __user *)arg, &up->rs485,
          ^
       cc1: some warnings being treated as errors
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_core.c
+++ drivers/tty/serial/8250/8250_core.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nmi.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
+#include <linux/uaccess.h>
 #ifdef CONFIG_SPARC
 #include <linux/sunserialcore.h>
 #endif
