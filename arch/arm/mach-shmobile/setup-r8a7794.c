f7e4ce51809d14bcfc7779bee17196a79ab07978
Simon Horman <horms+renesas@verge.net.au>
2014-09-10 09:43:31 +0900

    ARM: shmobile: r8a7794: Remove unnecessary #ifdef CONFIG_USE_OF
    The r8a7794 support is always compiled using ARCH_MULTIPLATFORM which
    selects USE_OF. So #ifdef CONFIG_USE_OF is unnecessary.
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-r8a7794.c
+++ arch/arm/mach-shmobile/setup-r8a7794.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rcar-gen2.h"
 #include <asm/mach/arch.h>
 
-#ifdef CONFIG_USE_OF
-
 static const char * const r8a7794_boards_compat_dt[] __initconst = {
 	"renesas,r8a7794",
 	NULL,
@@ -33,4 +31,3 @@ DT_MACHINE_START(R8A7794_DT, "Generic R8A7794 (Flattened Device Tree)")
 	.init_time	= rcar_gen2_timer_init,
 	.dt_compat	= r8a7794_boards_compat_dt,
 MACHINE_END
-#endif /* CONFIG_USE_OF */
