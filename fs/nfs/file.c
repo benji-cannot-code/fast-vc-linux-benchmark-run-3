1e581e03585aa36e9cfc9f935c44c9e6f725bb89
Christoph Hellwig <hch@lst.de>
2014-09-10 08:23:30 -0700

    pnfs: add flag to force read-modify-write in ->write_begin
    Like all block based filesystems, the pNFS block layout driver can't read
    or write at a byte granularity and thus has to perform read-modify-write
    cycles on writes smaller than this granularity.
    
    Add a flag so that the core NFS code always reads a whole page when
    starting a smaller write, so that we can do it in the place where the VFS
    expects it instead of doing in very deadlock prone way in the writeback
    handler.
    
    Note that in theory we could do less than page size reads here for disks
    that have a smaller sector size which are served by a server with a smaller
    pnfs block size.  But so far that doesn't seem like a worthwhile
    optimization.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/file.c
+++ fs/nfs/file.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "internal.h"
 #include "iostat.h"
 #include "fscache.h"
+#include "pnfs.h"
 
 #include "nfstrace.h"
 
@@ -327,6 +328,12 @@ static int nfs_want_read_modify_write(struct file *file, struct page *page,
 	unsigned int pglen = nfs_page_length(page);
 	unsigned int offset = pos & (PAGE_CACHE_SIZE - 1);
 	unsigned int end = offset + len;
+
+	if (pnfs_ld_read_whole_page(file->f_mapping->host)) {
+		if (!PageUptodate(page))
+			return 1;
+		return 0;
+	}
 
 	if ((file->f_mode & FMODE_READ) &&	/* open for read? */
 	    !PageUptodate(page) &&		/* Uptodate? */
