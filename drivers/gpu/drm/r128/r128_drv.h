54b6691fb4a7ea13a3c782121ad8a3c0d9a443fd
Dave Airlie <airlied@redhat.com>
2014-09-10 18:23:07 +1000

    drm: split ati_pcigart.h out of drmP.h
    Just move this into a separate header file, and make the
    two users use it.
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/r128/r128_drv.h
+++ drivers/gpu/drm/r128/r128_drv.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __R128_DRV_H__
 #define __R128_DRV_H__
 
+#include <drm/ati_pcigart.h>
 /* General customization:
  */
 #define DRIVER_AUTHOR		"Gareth Hughes, VA Linux Systems Inc."
