ce216dc810fb1dc0b46d0571ab89ddab1dabeaa5
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-09-11 13:28:08 +0200

    drm/i915: Extend BIOS stolen mem handling to all platform
    Based upon a patch from Deepak, but reworked to only apply on gen7+
    and with the logic a bit clarified.
    
    v2: Fix s/SHIFT/MASK/ fumble that Ville spotted.
    
    Cc: Deepak S <deepak.s@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -144,6 +144,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GAB_CTL				0x24000
 #define   GAB_CTL_CONT_AFTER_PAGEFAULT	(1<<8)
 
+#define GEN7_BIOS_RESERVED		0x1082C0
+#define GEN7_BIOS_RESERVED_1M		(0 << 5)
+#define GEN7_BIOS_RESERVED_256K		(1 << 5)
+#define GEN8_BIOS_RESERVED_SHIFT       7
+#define GEN7_BIOS_RESERVED_MASK        0x1
+#define GEN8_BIOS_RESERVED_MASK        0x3
+
+
 /* VGA stuff */
 
 #define VGA_ST01_MDA 0x3ba
