11011e3ba1d69ccd6ec12501445c0ef86fc63428
Ben Skeggs <bskeggs@redhat.com>
2014-09-11 23:04:22 +1000

    drm/nouveau/pwr/memx: fix command ordering around block/unblock
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/pwr/fuc/os.h
+++ drivers/gpu/drm/nouveau/core/subdev/pwr/fuc/os.h
@@ -20,12 +20,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MEMX_MSG_EXEC 1
 
 /* MEMX: script opcode definitions */
-#define MEMX_ENTER  0
-#define MEMX_LEAVE  1
-#define MEMX_WR32   2
-#define MEMX_WAIT   3
-#define MEMX_DELAY  4
-#define MEMX_VBLANK 5
+#define MEMX_ENTER  1
+#define MEMX_LEAVE  2
+#define MEMX_WR32   3
+#define MEMX_WAIT   4
+#define MEMX_DELAY  5
+#define MEMX_VBLANK 6
 
 /* I2C_: message identifiers */
 #define I2C__MSG_RD08 0
