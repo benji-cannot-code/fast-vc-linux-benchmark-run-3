62c01b58c97d6b718afa12d3af99367de5103978
Jeff Layton <jlayton@primarydata.com>
2014-09-12 16:40:20 -0400

    nfsd: remove redundant boot_time parm from grace_done client tracking op
    Since it's stored in nfsd_net, we don't need to pass it in separately.
    
    Signed-off-by: Jeff Layton <jlayton@primarydata.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -4123,7 +4123,7 @@ nfsd4_end_grace(struct nfsd_net *nn)
 
 	dprintk("NFSD: end of grace period\n");
 	nn->grace_ended = true;
-	nfsd4_record_grace_done(nn, nn->boot_time);
+	nfsd4_record_grace_done(nn);
 	locks_end_grace(&nn->nfsd4_manager);
 	/*
 	 * Now that every NFSv4 client has had the chance to recover and
