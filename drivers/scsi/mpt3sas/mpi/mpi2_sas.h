8f4d16b1f39bfec9cd84e4efa2045ed44b004c4d
Sreekanth Reddy <sreekanth.reddy@avagotech.com>
2014-09-12 15:35:28 +0530

    mpt3sas: MPI2.5 Rev H (2.5.3) specifications
    Below is the change set in MPI2.5 Rev H specification and 2.00.32 header files
    1) Added reserved fields to IO Unit Page 7 for future use.
    2) Added optional functionality to IOCInit Request so that
       the host may specify a separate base address for each
       Reply Descriptor Post Queue. IOC support for this is
       indicated using a new IOCCapabilities bit in the IOCFacts Reply.
    3) Added Toolbox Console Text Display Tool
       The host uses the Console Text Display Tool to send a string to
       IOC's Console using different console types (eg: UART serial terminal or Ethernet terminal).
    4) Firmware images can now be signed using an encrypted hash.
    5) Added MPI2_SAS_OP_TRANSMIT_PORT_SELECT_SIGNAL
    6) Added more details about configuration page restrictions when Host Based Discovery is enabled
       Enabling host based discovery affects the availability of some configuration pages and events.
       The SAS Expander, SAS Device, and SAS Enclosure configuration  pages are not available from the IOC.
       The IOC returns an error status to any Configuration Request message attempting to access these pages.
       The IOC does not send the SAS Discovery Event, the SAS Topology Change List Event, or
       the SAS Enclosure Device Status Change Event when host based discovery is enabled.
    7) Bit 13 of the SAS IO Unit Page 1 ControlFlags field is now obsolete.
       It was used to enable limiting direct attached SATA maximum link rate to 1.5 Gbps.
    
    Signed-off-by: Sreekanth Reddy <Sreekanth.Reddy@avagotech.com>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/scsi/mpt3sas/mpi/mpi2_sas.h
+++ drivers/scsi/mpt3sas/mpi/mpi2_sas.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *         Title:  MPI Serial Attached SCSI structures and definitions
  * Creation Date:  February 9, 2007
  *
- * mpi2_sas.h Version:  02.00.07
+ * mpi2_sas.h Version:  02.00.08
  *
  * NOTE: Names (typedefs, defines, etc.) beginning with an MPI25 or Mpi25
  *       prefix are for use only on MPI v2.5 products, and must not be used
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 11-18-11  02.00.06  Incorporating additions for MPI v2.5.
  * 07-10-12  02.00.07  Added MPI2_SATA_PT_SGE_UNION for use in the SATA
  *                     Passthrough Request message.
+ * 08-19-13  02.00.08  Made MPI2_SAS_OP_TRANSMIT_PORT_SELECT_SIGNAL obsolete
+ *			for anything newer than MPI v2.0.
  * --------------------------------------------------------------------------
  */
 
@@ -252,7 +254,7 @@ typedef struct _MPI2_SAS_IOUNIT_CONTROL_REQUEST {
 #define MPI2_SAS_OP_PHY_CLEAR_ERROR_LOG         (0x08)
 #define MPI2_SAS_OP_SEND_PRIMITIVE              (0x0A)
 #define MPI2_SAS_OP_FORCE_FULL_DISCOVERY        (0x0B)
-#define MPI2_SAS_OP_TRANSMIT_PORT_SELECT_SIGNAL (0x0C)
+#define MPI2_SAS_OP_TRANSMIT_PORT_SELECT_SIGNAL (0x0C) /* MPI v2.0 only */
 #define MPI2_SAS_OP_REMOVE_DEVICE               (0x0D)
 #define MPI2_SAS_OP_LOOKUP_MAPPING              (0x0E)
 #define MPI2_SAS_OP_SET_IOC_PARAMETER           (0x0F)
