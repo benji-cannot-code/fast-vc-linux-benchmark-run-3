e979cb326875d1686c596e84a8fd32c0dff16c6d
Sreekanth Reddy <sreekanth.reddy@avagotech.com>
2014-09-12 15:35:29 +0530

    mpt3sas: Copyright in driver sources is updated for year the 2014.
    Copyright in driver sources is updated for year the 2014.
    
    Signed-off-by: Sreekanth Reddy <Sreekanth.Reddy@avagotech.com>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/scsi/mpt3sas/mpi/mpi2_tool.h
+++ drivers/scsi/mpt3sas/mpi/mpi2_tool.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (c) 2000-2013 LSI Corporation.
+ * Copyright (c) 2000-2014 LSI Corporation.
  *
  *
  *          Name:  mpi2_tool.h
--- drivers/scsi/mpt3sas/mpt3sas_base.c
+++ drivers/scsi/mpt3sas/mpt3sas_base.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * for access to MPT (Message Passing Technology) firmware.
  *
  * This code is based on drivers/scsi/mpt3sas/mpt3sas_base.c
- * Copyright (C) 2012-2013  LSI Corporation
+ * Copyright (C) 2012-2014  LSI Corporation
  *  (mailto:DL-MPTFusionLinux@lsi.com)
  *
  * This program is free software; you can redistribute it and/or
