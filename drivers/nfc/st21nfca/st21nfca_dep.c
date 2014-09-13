310cf4c00ad2f7f24627acba0eb53a6b6489195f
Christophe Ricard <christophe.ricard@gmail.com>
2014-09-13 10:28:52 +0200

    NFC: st21nfca: ERR_PTR vs NULL fix
    "skb" can be NULL here but it can't be an ERR_PTR:
    - IS_ERR(NULL) return false and skb migth be NULL.
    - skb cannot be a ERR_PTR as nfc_hci_send_cmd_async it never using such cast.
    
    !skb is more appropriate at those places.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/st21nfca/st21nfca_dep.c
+++ drivers/nfc/st21nfca/st21nfca_dep.c
@@ -444,7 +444,7 @@ static void st21nfca_im_recv_atr_res_cb(void *context, struct sk_buff *skb,
 	if (err != 0)
 		return;
 
-	if (IS_ERR(skb))
+	if (!skb)
 		return;
 
 	switch (info->async_cb_type) {
@@ -556,7 +556,7 @@ static void st21nfca_im_recv_dep_res_cb(void *context, struct sk_buff *skb,
 	if (err != 0)
 		return;
 
-	if (IS_ERR(skb))
+	if (!skb)
 		return;
 
 	switch (info->async_cb_type) {
