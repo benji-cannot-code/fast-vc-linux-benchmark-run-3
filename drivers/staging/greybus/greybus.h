d0fa1e5dc6a8638dbc8a8ed10e1dce1f94357186
Greg Kroah-Hartman <greg@kroah.com>
2014-09-13 18:20:54 -0700

    greybus: gbuf recieve path work, not done, dinner time...

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -43,8 +43,8 @@ struct gbuf;
 struct gdev_cport {
 	u16	number;
 	u16	size;
-	// FIXME, what else?
 	u8	speed;	// valid???
+	// FIXME, what else?
 };
 
 struct gdev_string {
@@ -234,6 +234,11 @@ int gb_thread_init(void);
 void gb_thread_destroy(void);
 int gb_debugfs_init(void);
 void gb_debugfs_cleanup(void);
+
+int gb_register_cport_complete(struct greybus_device *gdev,
+			       gbuf_complete_t handler, int cport,
+			       void *context);
+void gb_deregister_cport_complete(int cport);
 
 extern const struct attribute_group *greybus_module_groups[];
 
