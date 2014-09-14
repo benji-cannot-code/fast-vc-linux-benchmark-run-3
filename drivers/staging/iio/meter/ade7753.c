6a765c598f2a9b4094eb9244495c617c978f3dc6
Tina Johnson <tinajohnson.1234@gmail.com>
2014-09-14 16:30:05 +0530

    Staging: iio: meter: ade7753: Merged assignment with immediately following return statement
    Saved one line of code by merging the assigning and return statements
    of variable ret. And thus removed variable len which was no longer useful.
    
    This patch was done using Coccinelle script and the following semantic
    patch was used:
    
    @@
    expression ret;
    identifier f;
    @@
    
    -ret =
    +return
          f(...);
    -return ret;
    
    Signed-off-by: Tina Johnson <tinajohnson.1234@gmail.com>
    Acked-by: Julia Lawall <julia.lawall@lip6.fr>
    Acked-by: Josh Triplett <josh@joshtriplett.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/iio/meter/ade7753.c
+++ drivers/staging/iio/meter/ade7753.c
@@ -378,7 +378,7 @@ static ssize_t ade7753_read_frequency(struct device *dev,
 		struct device_attribute *attr,
 		char *buf)
 {
-	int ret, len = 0;
+	int ret;
 	u16 t;
 	int sps;
 	ret = ade7753_spi_read_reg_16(dev, ADE7753_MODE, &t);
@@ -388,8 +388,7 @@ static ssize_t ade7753_read_frequency(struct device *dev,
 	t = (t >> 11) & 0x3;
 	sps = 27900 / (1 + t);
 
-	len = sprintf(buf, "%d\n", sps);
-	return len;
+	return sprintf(buf, "%d\n", sps);
 }
 
 static ssize_t ade7753_write_frequency(struct device *dev,
