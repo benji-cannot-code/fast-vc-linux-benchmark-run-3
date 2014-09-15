2fbbc731b3871776ace687472ec57d28c858c493
Felipe Balbi <balbi@ti.com>
2014-09-15 16:15:02 -0500

    arm: omap: irq: move irq.c to drivers/irqchip/
    Just move the code over as it has no dependencies
    on arch/arm/ anymore.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/irqchip/irq-omap-intc.c
+++ drivers/irqchip/irq-omap-intc.c
@@ -18,15 +18,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 
 #include <asm/exception.h>
-#include <asm/mach/irq.h>
 #include <linux/irqdomain.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 
-#include "soc.h"
-#include "common.h"
-#include "../../drivers/irqchip/irqchip.h"
+#include "irqchip.h"
+
+/* Define these here for now until we drop all board-files */
+#define OMAP24XX_IC_BASE	0x480fe000
+#define OMAP34XX_IC_BASE	0x48200000
 
 /* selected INTC register offsets */
 
