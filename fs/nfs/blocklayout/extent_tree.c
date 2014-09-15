520af21866a38621c9dcf7a33948fda88d31b6db
Stephen Rothwell <sfr@canb.auug.org.au>
2014-09-15 17:01:32 +1000

    pnfs/blocklayout: include vmalloc.h for __vmalloc
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/blocklayout/extent_tree.c
+++ fs/nfs/blocklayout/extent_tree.c
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2014 Christoph Hellwig.
  */
 
+#include <linux/vmalloc.h>
+
 #include "blocklayout.h"
 
 #define NFSDBG_FACILITY		NFSDBG_PNFS_LD
