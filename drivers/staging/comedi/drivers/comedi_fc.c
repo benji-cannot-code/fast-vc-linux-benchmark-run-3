1faad12229ebbfb7ce3137871a9e3522940aa095
Ian Abbott <abbotti@mev.co.uk>
2014-09-15 13:45:59 +0100

    staging: comedi: add comedi_inc_scan_progress()
    The "comedi_fc" module contains a few functions useful to Comedi
    drivers.  Their functionality is being migrated to the core "comedi"
    module and renamed to start with the prefix `comedi_`.  As part of this
    migration, move `cfc_inc_scan_progress()` into the core comedi module
    and rename it to `comedi_inc_scan_progress()`.  Change the external
    declaration of `cfc_inc_scan_progress()` into an inline function that
    calls `comedi_inc_scan_progress()`.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/comedi_fc.c
+++ drivers/staging/comedi/drivers/comedi_fc.c
@@ -23,19 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "comedi_fc.h"
 
-void cfc_inc_scan_progress(struct comedi_subdevice *s, unsigned int num_bytes)
-{
-	struct comedi_async *async = s->async;
-	unsigned int scan_length = comedi_bytes_per_scan(s);
-
-	async->scan_progress += num_bytes;
-	if (async->scan_progress >= scan_length) {
-		async->scan_progress %= scan_length;
-		async->events |= COMEDI_CB_EOS;
-	}
-}
-EXPORT_SYMBOL_GPL(cfc_inc_scan_progress);
-
 /* Writes an array of data points to comedi's buffer */
 unsigned int cfc_write_array_to_buffer(struct comedi_subdevice *s,
 				       void *data, unsigned int num_bytes)
