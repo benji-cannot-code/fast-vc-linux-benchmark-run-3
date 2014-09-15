52037569b40a46b92eea8c94513e45fc9eaa1f30
Nicolas Ferre <nicolas.ferre@atmel.com>
2014-09-15 18:15:55 +0200

    ARM: at91: SAMA5D4 SoC detection code and low level routines
    SoC identification code, kernel uncompress and low level
    debugging routines update.
    On SAMA5D4, DBGU is at another address AT91_BASE_DBGU2 so another
    round of detection is needed. We also had to differentiate with
    SAMA5D3 SoC family and rename some variables.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Acked-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- arch/arm/mach-at91/generic.h
+++ arch/arm/mach-at91/generic.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
  /* Map io */
 extern void __init at91_map_io(void);
+extern void __init at91_alt_map_io(void);
 extern void __init at91_init_sram(int bank, unsigned long base,
 				  unsigned int length);
 
