0ea2b1faed4844c68960f9b746be15a3ffe1ed7b
Nicolas Ferre <nicolas.ferre@atmel.com>
2014-09-15 18:15:54 +0200

    ARM: at91: introduce basic SAMA5D4 support
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Acked-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- arch/arm/mach-at91/include/mach/cpu.h
+++ arch/arm/mach-at91/include/mach/cpu.h
@@ -87,6 +87,9 @@ enum at91_soc_type {
 	/* SAMA5D3 */
 	AT91_SOC_SAMA5D3,
 
+	/* SAMA5D4 */
+	AT91_SOC_SAMA5D4,
+
 	/* Unknown type */
 	AT91_SOC_UNKNOWN,
 };
@@ -210,6 +213,12 @@ static inline int at91_soc_is_detected(void)
 #define cpu_is_sama5d3()	(at91_soc_initdata.type == AT91_SOC_SAMA5D3)
 #else
 #define cpu_is_sama5d3()	(0)
+#endif
+
+#ifdef CONFIG_SOC_SAMA5D4
+#define cpu_is_sama5d4()	(at91_soc_initdata.type == AT91_SOC_SAMA5D4)
+#else
+#define cpu_is_sama5d4()	(0)
 #endif
 
 /*
