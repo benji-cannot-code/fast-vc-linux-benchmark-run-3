9325367eab8255129d23c173c2691c11bbfdc787
Fabian Frederick <fabf@skynet.be>
2014-09-16 20:43:33 +0200

    m68k/coldfire: remove second asm/mcfclk.h inclusion in m54xx.c
    asm/mcfclk.h was included twice.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/coldfire/m54xx.c
+++ arch/m68k/coldfire/m54xx.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mcfuart.h>
 #include <asm/mcfclk.h>
 #include <asm/m54xxgpt.h>
-#include <asm/mcfclk.h>
 #ifdef CONFIG_MMU
 #include <asm/mmu_context.h>
 #endif
