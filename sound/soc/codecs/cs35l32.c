f1d7bf63904bbdead083b1a82f088ea56a10d1da
Fabian Frederick <fabf@skynet.be>
2014-09-16 21:02:31 +0200

    ASoC: cs35l32: remove second linux/slab.h inclusion
    linux/slab.h was included twice.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Acked-by: Brian Austin <brian.austin@cirrus.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs35l32.c
+++ sound/soc/codecs/cs35l32.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regulator/consumer.h>
 #include <linux/gpio/consumer.h>
 #include <linux/of_device.h>
-#include <linux/slab.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
