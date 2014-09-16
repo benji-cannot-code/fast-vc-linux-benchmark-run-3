8e62dbbb7491305190027b662922e05cf2f3cece
David Howells <dhowells@redhat.com>
2014-09-16 17:36:11 +0100

    KEYS: Implement binary asymmetric key ID handling
    Implement the first step in using binary key IDs for asymmetric keys rather
    than hex string keys.
    
    The previously added match data preparsing will be able to convert hex
    criterion strings into binary which can then be compared more rapidly.
    
    Further, we actually want more then one ID string per public key.  The problem
    is that X.509 certs refer to other X.509 certs by matching Issuer + AuthKeyId
    to Subject + SubjKeyId, but PKCS#7 messages match against X.509 Issuer +
    SerialNumber.
    
    This patch just provides facilities for a later patch to make use of.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Vivek Goyal <vgoyal@redhat.com>

--- crypto/asymmetric_keys/asymmetric_keys.h
+++ crypto/asymmetric_keys/asymmetric_keys.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 int asymmetric_keyid_match(const char *kid, const char *id);
+extern bool asymmetric_match_key_ids(const struct asymmetric_key_ids *kids,
+				     const struct asymmetric_key_id *match_id);
+
+extern struct asymmetric_key_id *asymmetric_key_hex_to_key_id(const char *id);
 
 static inline const char *asymmetric_key_id(const struct key *key)
 {
