a281ae5145cd24bda0888c7c80cbaf660c9e9f11
Chao Yu <chao2.yu@samsung.com>
2014-09-17 17:26:06 +0800

    ceph: remove redundant code for max file size verification
    Both ceph_update_writeable_page and ceph_setattr will verify file size
    with max size ceph supported.
    There are two caller for ceph_update_writeable_page, ceph_write_begin and
    ceph_page_mkwrite. For ceph_write_begin, we have already verified the size in
    generic_write_checks of ceph_write_iter; for ceph_page_mkwrite, we have no
    chance to change file size when mmap. Likewise we have already verified the size
    in inode_change_ok when we call ceph_setattr.
    So let's remove the redundant code for max file size verification.
    
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    Reviewed-by: Yan, Zheng <zyan@redhat.com>

--- fs/ceph/inode.c
+++ fs/ceph/inode.c
@@ -1814,10 +1814,6 @@ int ceph_setattr(struct dentry *dentry, struct iattr *attr)
 	if (ia_valid & ATTR_SIZE) {
 		dout("setattr %p size %lld -> %lld\n", inode,
 		     inode->i_size, attr->ia_size);
-		if (attr->ia_size > inode->i_sb->s_maxbytes) {
-			err = -EINVAL;
-			goto out;
-		}
 		if ((issued & CEPH_CAP_FILE_EXCL) &&
 		    attr->ia_size > inode->i_size) {
 			inode->i_size = attr->ia_size;
@@ -1897,8 +1893,6 @@ int ceph_setattr(struct dentry *dentry, struct iattr *attr)
 	if (mask & CEPH_SETATTR_SIZE)
 		__ceph_do_pending_vmtruncate(inode);
 	return err;
-out:
-	spin_unlock(&ci->i_ceph_lock);
 out_put:
 	ceph_mdsc_put_request(req);
 	return err;
