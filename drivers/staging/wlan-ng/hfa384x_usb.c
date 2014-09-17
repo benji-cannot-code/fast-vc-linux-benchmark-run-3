8a08f4c84854b65b98a6c6205344c784dd15ffec
Aybuke Ozdemir <aybuke.147@gmail.com>
2014-09-18 01:03:28 +0300

    Staging: wlan-ng: Fix return in void function warning
    This fixes checkpatch.pl warning:
    WARNING: void function return statements are not generally useful
    
    Signed-off-by: Aybuke Ozdemir <aybuke.147@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x_usb.c
+++ drivers/staging/wlan-ng/hfa384x_usb.c
@@ -3531,8 +3531,6 @@ static void hfa384x_usbin_rx(wlandevice_t *wlandev, struct sk_buff *skb)
 				    usbin->rxfrm.desc.status));
 		break;
 	}
-
-	return;
 }
 
 /*----------------------------------------------------------------
@@ -3642,8 +3640,6 @@ static void hfa384x_int_rxmonitor(wlandevice_t *wlandev,
 
 	/* pass it back up */
 	prism2sta_ev_rx(wlandev, skb);
-
-	return;
 }
 
 /*----------------------------------------------------------------
