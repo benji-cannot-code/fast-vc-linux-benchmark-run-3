de2af52058439edc9bbf6d5a1626f7e286686e20
Li Zefan <lizefan@huawei.com>
2014-09-17 18:18:09 +0800

    cgroup: remove some useless forward declarations
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct cgroup_root;
 struct cgroup_subsys;
-struct inode;
 struct cgroup;
 
 extern int cgroup_init_early(void);
