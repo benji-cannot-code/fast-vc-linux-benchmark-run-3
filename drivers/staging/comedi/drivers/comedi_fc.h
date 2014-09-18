ffa93a25d8149961099e2996e06ba662b4d22fa3
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-09-18 11:41:20 -0700

    staging: comedi: comedi_fc.h: remove cfc_bytes_per_scan()
    This inline function is no longer used. Remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/comedi_fc.h
+++ drivers/staging/comedi/drivers/comedi_fc.h
@@ -24,11 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../comedidev.h"
 
-static inline unsigned int cfc_bytes_per_scan(struct comedi_subdevice *s)
-{
-	return comedi_bytes_per_scan(s);
-}
-
 static inline void cfc_inc_scan_progress(struct comedi_subdevice *s,
 					 unsigned int num_bytes)
 {
