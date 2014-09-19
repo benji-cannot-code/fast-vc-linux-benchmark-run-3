8f039d6c795e83d4c12c20990d3ba401b529ef28
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-09-19 17:07:10 +0200

    drm/i915: Update DRIVER_DATE to 20140919
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20140905"
+#define DRIVER_DATE		"20140919"
 
 enum pipe {
 	INVALID_PIPE = -1,
