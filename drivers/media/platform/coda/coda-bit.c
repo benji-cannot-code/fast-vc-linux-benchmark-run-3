11316d4475def71d4ea1badf747f908bef5f58ff
Fabio Estevam <fabio.estevam@freescale.com>
2014-09-19 08:32:30 -0300

    [media] coda: coda-bit: Include "<linux/slab.h>"
    coda-bit uses kmalloc/kfree functions, so the slab header needs to be included
    in order to fix the following build errors:
    
    drivers/media/platform/coda/coda-bit.c: In function 'coda_fill_bitstream':
    drivers/media/platform/coda/coda-bit.c:231:4: error: implicit declaration of function 'kmalloc' [-Werror=implicit-function-declaration]
    drivers/media/platform/coda/coda-bit.c: In function 'coda_alloc_framebuffers':
    drivers/media/platform/coda/coda-bit.c:312:3: error: implicit declaration of function 'kfree' [-Werror=implicit-function-declaration]
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/coda/coda-bit.c
+++ drivers/media/platform/coda/coda-bit.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
+#include <linux/slab.h>
 #include <linux/videodev2.h>
 
 #include <media/v4l2-common.h>
