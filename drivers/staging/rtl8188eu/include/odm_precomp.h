6c26375871c9c2f1df3daa7363d12353d17d9665
navin patidar <navin.patidar@gmail.com>
2014-09-20 19:55:48 +0530

    staging: rtl8188eu: Remove unused header file odm_RegDefine11AC.h
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm_precomp.h
+++ drivers/staging/rtl8188eu/include/odm_precomp.h
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "odm.h"
 #include "odm_HWConfig.h"
 #include "odm_debug.h"
-#include "odm_RegDefine11AC.h"
 #include "odm_RegDefine11N.h"
 
 #include "Hal8188ERateAdaptive.h"/* for  RA,Power training */
