fbfc83ed6676b2cbf9c12c4e71a4505173612d5d
Gulsah Kose <gulsah.1004@gmail.com>
2014-09-21 00:59:11 +0300

    staging: media: lirc: Fix unnecessary return warning.
    This patch fixes "void function return statements are not generally
    useful" checkpatch.pl warning in lirc_sasem.c
    
    Signed-off-by: Gulsah Kose <gulsah.1004@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/lirc/lirc_sasem.c
+++ drivers/staging/media/lirc/lirc_sasem.c
@@ -475,8 +475,6 @@ static void usb_tx_callback(struct urb *urb)
 	/* notify waiters that write has finished */
 	atomic_set(&context->tx.busy, 0);
 	complete(&context->tx.finished);
-
-	return;
 }
 
 /**
@@ -563,7 +561,6 @@ static void ir_close(void *data)
 	}
 
 	mutex_unlock(&context->ctx_lock);
-	return;
 }
 
 /**
@@ -665,7 +662,6 @@ static void usb_rx_callback(struct urb *urb)
 	}
 
 	usb_submit_urb(context->rx_urb, GFP_ATOMIC);
-	return;
 }
 
 
