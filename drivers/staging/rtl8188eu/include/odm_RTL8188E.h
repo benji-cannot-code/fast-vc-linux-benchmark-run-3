5654681ad0b840f02a148f21c2ea836ba35247bb
navin patidar <navin.patidar@gmail.com>
2014-09-20 19:55:52 +0530

    staging: rtl8188eu: Rework function ODM_AntennaDiversityInit_88E()
    Rename CamelCase function name.
    Remove unnecessary debugging messages.
    
    Signed-off-by: navin patidar <navin.patidar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm_RTL8188E.h
+++ drivers/staging/rtl8188eu/include/odm_RTL8188E.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void ODM_DIG_LowerBound_88E(struct odm_dm_struct *pDM_Odm);
 
-void ODM_AntennaDiversityInit_88E(struct odm_dm_struct *pDM_Odm);
+void rtl88eu_dm_antenna_div_init(struct odm_dm_struct *dm_odm);
 
 void ODM_AntennaDiversity_88E(struct odm_dm_struct *pDM_Odm);
 
