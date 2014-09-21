ff915d38227f6c7e39066848f4e9f28035025bdf
Roxana Blaj <roxanagabriela10@gmail.com>
2014-09-21 13:32:32 +0300

    staging: rts5208: add new line after declarations
    This fixes the warning:
    WARNING: Missing a blank line after declarations
    
    Signed-off-by: Roxana Blaj <roxanagabriela10@gmail.com>
    Acked-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rts5208/general.c
+++ drivers/staging/rts5208/general.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int bit1cnt_long(u32 data)
 {
 	int i, cnt = 0;
+
 	for (i = 0; i < 32; i++) {
 		if (data & 0x01)
 			cnt++;
