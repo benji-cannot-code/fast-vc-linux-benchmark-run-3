780168db86905e9df4ba0d8fb81350ca773bc35a
Vineet Gupta <vgupta@synopsys.com>
2014-09-22 16:51:47 +0530

    ARCv2: STAR 9000793984: Handle return from intr to Delay Slot
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/setup.c
+++ arch/arc/kernel/setup.c
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define FIX_PTR(x)  __asm__ __volatile__(";" : "+r"(x))
 
+unsigned int intr_to_DE_cnt;
+
 /* Part of U-boot ABI: see head.S */
 int __initdata uboot_tag;
 char __initdata *uboot_arg;
