a019704a620b943f2dacc624d6b5cb34f27a04e0
Alex Elder <elder@linaro.org>
2014-09-23 12:46:33 -0500

    greybus: get rid of a weird idiom
    It strikes me as strange to add one to a value while checking to see
    if it exceeds a maximum.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/core.c
+++ drivers/staging/greybus/core.c
@@ -283,7 +283,7 @@ static int create_string(struct greybus_module *gmod,
 	int string_size;
 	struct gmod_string *gmod_string;
 
-	if ((gmod->num_strings + 1) >= MAX_STRINGS_PER_MODULE) {
+	if (gmod->num_strings == MAX_STRINGS_PER_MODULE) {
 		dev_err(gmod->dev.parent,
 			"too many strings for this module!\n");
 		return -EINVAL;
@@ -316,7 +316,7 @@ static int create_cport(struct greybus_module *gmod,
 {
 	struct gmod_cport *gmod_cport;
 
-	if ((gmod->num_cports + 1) >= MAX_CPORTS_PER_MODULE) {
+	if (gmod->num_cports == MAX_CPORTS_PER_MODULE) {
 		dev_err(gmod->dev.parent, "too many cports for this module!\n");
 		return -EINVAL;
 	}
