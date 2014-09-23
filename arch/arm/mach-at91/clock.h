0ad4407723ca5ed1d431e67b279cc4bca3adef93
Boris BREZILLON <boris.brezillon@free-electrons.com>
2014-09-23 13:18:33 +0200

    ARM: at91: add clk_lookup entry for RTT devices
    First export the clk32k clk.
    Then add clk_lookup entries for RTT devices so that rtc-at91sam9 driver
    can retrieve and manipulate the slow clk.
    
    Signed-off-by: Boris BREZILLON <boris.brezillon@free-electrons.com>
    Acked-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Acked-by: Johan Hovold <johan@kernel.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/clock.h
+++ arch/arm/mach-at91/clock.h
@@ -35,6 +35,7 @@ struct clk {
 extern int __init clk_register(struct clk *clk);
 extern struct clk mck;
 extern struct clk utmi_clk;
+extern struct clk clk32k;
 
 #define CLKDEV_CON_ID(_id, _clk)			\
 	{						\
