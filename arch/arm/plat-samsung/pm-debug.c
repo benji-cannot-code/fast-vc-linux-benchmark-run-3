764faa49dc9b5de867e66f55e7da6b4cae9247cb
Tomasz Figa <t.figa@samsung.com>
2014-09-24 02:10:04 +0900

    ARM: SAMSUNG: Restore Samsung PM Debug functionality
    Due to recently merged patches and previous merge conflicts, the Samsung
    PM Debug functionality no longer can be enabled. This patch fixes
    incorrect dependency of SAMSUNG_PM_DEBUG on an integer symbol and adds
    missing header inclusion.
    
    Signed-off-by: Tomasz Figa <t.figa@samsung.com>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/plat-samsung/pm-debug.c
+++ arch/arm/plat-samsung/pm-debug.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/serial_core.h>
+#include <linux/serial_s3c.h>
 #include <linux/io.h>
 
 #include <asm/mach/map.h>
