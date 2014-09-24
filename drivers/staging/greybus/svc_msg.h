781f737dc091082ce13d174679957c027030f54f
Alex Elder <elder@linaro.org>
2014-09-24 05:16:17 -0500

    greybus: fix document version number
    Two spots use the old "0.draft" version number for the Greybus
    protocol specification.  We've updated that to be 0.1.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/svc_msg.h
+++ drivers/staging/greybus/svc_msg.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Greybus AP <-> SVC message structure format.
  *
- * See "Greybus Application Protocol" document (version 0.draft) for
+ * See "Greybus Application Protocol" document (version 0.1) for
  * details on these values and structures.
  *
  * Copyright 2014 Google Inc.
