7049fc8abb7b02b4a3416384f7b82e19ef05c1c2
Michal Sojka <sojka@merica.cz>
2014-09-24 22:43:21 +0200

    usb: Add LED triggers for USB activity
    With this patch, USB activity can be signaled by blinking a LED. There
    are two triggers, one for activity on USB host and one for USB gadget.
    
    Both triggers should work with all host/device controllers. Tested only
    with musb.
    
    Performace: I measured performance overheads on ARM Cortex-A8 (TI
    AM335x) running on 600 MHz.
    
    Duration of usb_led_activity():
    - with no LED attached to the trigger:        2 ± 1 µs
    - with one GPIO LED attached to the trigger:  2 ± 1 µs or 8 ± 2 µs (two peaks in histogram)
    
    Duration of functions calling usb_led_activity() (with this patch
    applied and no LED attached to the trigger):
    - __usb_hcd_giveback_urb():    10 - 25 µs
    - usb_gadget_giveback_request(): 2 - 6 µs
    
    Signed-off-by: Michal Sojka <sojka@merica.cz>
    Acked-by: Felipe Balbi <balbi@ti.com>
    Tested-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/udc/udc-core.c
+++ drivers/usb/gadget/udc/udc-core.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
+#include <linux/usb.h>
 
 /**
  * struct usb_udc - describes one usb device controller
@@ -117,6 +118,9 @@ EXPORT_SYMBOL_GPL(usb_gadget_unmap_request);
 void usb_gadget_giveback_request(struct usb_ep *ep,
 		struct usb_request *req)
 {
+	if (likely(req->status == 0))
+		usb_led_activity(USB_LED_EVENT_GADGET);
+
 	req->complete(ep, req);
 }
 EXPORT_SYMBOL_GPL(usb_gadget_giveback_request);
