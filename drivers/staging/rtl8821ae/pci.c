b8f5e37ac4c6a16fec8c9a010707631353891afd
Mathieu OTHACEHE <m.othacehe@gmail.com>
2014-09-24 17:32:04 +0200

    staging: rtl8821ae: fix sparse warning for static declarations
    This patch fixes the following sparse warnings:
    
    drivers/staging/rtl8821ae/pci.c:52:4: warning: symbol '_rtl_mac_to_hwqueue' was not declared. Should it be static?
    drivers/staging/rtl8821ae/pci.c:365:6: warning: symbol 'rtl_pci_check_buddy_priv' was not declared. Should it be static?
    drivers/staging/rtl8821ae/pci.c:409:6: warning: symbol 'rtl_pci_get_linkcontrol_field' was not declared. Should it be static?
    drivers/staging/rtl8821ae/pci.c:1748:6: warning: symbol 'rtl_pci_deinit' was not declared. Should it be static?
    drivers/staging/rtl8821ae/pci.c:1763:5: warning: symbol 'rtl_pci_init' was not declared. Should it be static?
    drivers/staging/rtl8821ae/pci.c:1780:5: warning: symbol 'rtl_pci_start' was not declared. Should it be static?
    drivers/staging/rtl8821ae/pci.c:1814:6: warning: symbol 'rtl_pci_stop' was not declared. Should it be static?
    drivers/staging/rtl8821ae/pci.c:2105:21: warning: symbol 'hw_export' was not declared. Should it be static?
    
    Signed-off-by: Mathieu OTHACEHE <m.othacehe@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8821ae/pci.c
+++ drivers/staging/rtl8821ae/pci.c
@@ -50,7 +50,7 @@ static const u8 ac_to_hwq[] = {
 	BK_QUEUE
 };
 
-u8 _rtl_mac_to_hwqueue(struct ieee80211_hw *hw,
+static u8 _rtl_mac_to_hwqueue(struct ieee80211_hw *hw,
 		struct sk_buff *skb)
 {
 	struct rtl_hal *rtlhal = rtl_hal(rtl_priv(hw));
@@ -363,8 +363,8 @@ static bool rtl_pci_get_amd_l1_patch(struct ieee80211_hw *hw)
 	return status;
 }
 
-bool rtl_pci_check_buddy_priv(struct ieee80211_hw *hw,
-        		      struct rtl_priv **buddy_priv)
+static bool rtl_pci_check_buddy_priv(struct ieee80211_hw *hw,
+				     struct rtl_priv **buddy_priv)
 {
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	struct rtl_pci_priv *pcipriv = rtl_pcipriv(hw);
@@ -407,7 +407,7 @@ bool rtl_pci_check_buddy_priv(struct ieee80211_hw *hw,
 	return b_find_buddy_priv;
 }
 
-void rtl_pci_get_linkcontrol_field(struct ieee80211_hw *hw)
+static void rtl_pci_get_linkcontrol_field(struct ieee80211_hw *hw)
 {
 	struct rtl_pci_priv *pcipriv = rtl_pcipriv(hw);
 	u8 capabilityoffset = pcipriv->ndis_adapter.pcibridge_pciehdr_offset;
@@ -1746,7 +1746,7 @@ static void rtl_pci_flush(struct ieee80211_hw *hw, u32 queues, bool drop)
 	}
 }
 
-void rtl_pci_deinit(struct ieee80211_hw *hw)
+static void rtl_pci_deinit(struct ieee80211_hw *hw)
 {
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	struct rtl_pci *rtlpci = rtl_pcidev(rtl_pcipriv(hw));
@@ -1761,7 +1761,7 @@ void rtl_pci_deinit(struct ieee80211_hw *hw)
 
 }
 
-int rtl_pci_init(struct ieee80211_hw *hw, struct pci_dev *pdev)
+static int rtl_pci_init(struct ieee80211_hw *hw, struct pci_dev *pdev)
 {
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	int err;
@@ -1778,7 +1778,7 @@ int rtl_pci_init(struct ieee80211_hw *hw, struct pci_dev *pdev)
 	return 1;
 }
 
-int rtl_pci_start(struct ieee80211_hw *hw)
+static int rtl_pci_start(struct ieee80211_hw *hw)
 {
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	struct rtl_hal *rtlhal = rtl_hal(rtl_priv(hw));
@@ -1812,7 +1812,7 @@ int rtl_pci_start(struct ieee80211_hw *hw)
 	return 0;
 }
 
-void rtl_pci_stop(struct ieee80211_hw *hw)
+static void rtl_pci_stop(struct ieee80211_hw *hw)
 {
 	struct rtl_priv *rtlpriv = rtl_priv(hw);
 	struct rtl_ps_ctl *ppsc = rtl_psc(rtl_priv(hw));
@@ -2103,7 +2103,7 @@ static int rtl_pci_intr_mode_decide(struct ieee80211_hw *hw)
 
 /* this is used for other modules get
  * hw pointer in rtl_pci_get_hw_pointer */
-struct ieee80211_hw *hw_export = NULL;
+static struct ieee80211_hw *hw_export;
 
 int rtl_pci_probe(struct pci_dev *pdev,
                   const struct pci_device_id *id)
