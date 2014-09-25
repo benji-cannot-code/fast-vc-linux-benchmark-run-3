595163135f40ac33b5b8013f91d2cc62052d3694
Florian Fainelli <f.fainelli@gmail.com>
2014-09-24 17:05:17 -0700

    net: dsa: start and stop the PHY state machine
    dsa_slave_open() should start the PHY library state machine for its PHY
    interface, and dsa_slave_close() should stop the PHY library state
    machine accordingly.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dsa/slave.c
+++ net/dsa/slave.c
@@ -85,6 +85,9 @@ static int dsa_slave_open(struct net_device *dev)
 			goto clear_allmulti;
 	}
 
+	if (p->phy)
+		phy_start(p->phy);
+
 	return 0;
 
 clear_allmulti:
@@ -101,6 +104,9 @@ static int dsa_slave_close(struct net_device *dev)
 {
 	struct dsa_slave_priv *p = netdev_priv(dev);
 	struct net_device *master = p->parent->dst->master_netdev;
+
+	if (p->phy)
+		phy_stop(p->phy);
 
 	dev_mc_unsync(master, dev);
 	dev_uc_unsync(master, dev);
