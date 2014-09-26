bf77f336185fb7f03029f05e6fcd71c58ae53586
Gulsah Kose <gulsah.1004@gmail.com>
2014-09-26 22:55:59 +0300

    staging: ft1000: ft1000-pcmcia: Used "linux" instead of "asm".
    This patch fixes "Use #include <linux/uaccess.h> instead of
    <asm/uaccess.h" checkpatch.pl warning in ft1000_dnld.c
    
    Signed-off-by: Gulsah Kose <gulsah.1004@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ft1000/ft1000-pcmcia/ft1000_dnld.c
+++ drivers/staging/ft1000/ft1000-pcmcia/ft1000_dnld.c
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/timer.h>
 #include <linux/delay.h>
-#include <asm/io.h>
-#include <asm/uaccess.h>
+#include <linux/io.h>
+#include <linux/uaccess.h>
 #include <linux/vmalloc.h>
 
 #include "ft1000.h"
