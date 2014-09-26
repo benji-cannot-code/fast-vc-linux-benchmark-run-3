82dd13fa6960ab578da6bbb2340350f79d7d4593
Arnd Bergmann <arnd@arndb.de>
2014-09-26 23:24:00 +0200

    dma: imx-sdma: fix another incorrect __init annotation
    In e34b731faa7d1 ("dma: imx-sdma: Remove spurious __init annotation on
    sdma_probe()"), Mark found an extraneous __init label and fixed it.
    
    However, he missed another one, because now we get this other warning:
    
    WARNING: drivers/dma/imx-sdma.o(.text+0x3bb4): Section mismatch in reference from the function sdma_probe() to the function .init.text:sdma_get_firmware()
    The function sdma_probe() references
    the function __init sdma_get_firmware().
    
    Same reasoning as the last time, the function may get called at
    runtime, so it can't be __init.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/imx-sdma.c
+++ drivers/dma/imx-sdma.c
@@ -1335,7 +1335,7 @@ err_firmware:
 	release_firmware(fw);
 }
 
-static int __init sdma_get_firmware(struct sdma_engine *sdma,
+static int sdma_get_firmware(struct sdma_engine *sdma,
 		const char *fw_name)
 {
 	int ret;
