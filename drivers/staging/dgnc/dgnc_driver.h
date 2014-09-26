21a4deed5abe0be750ee7c2dbaf61868245962eb
Roberta Dobrescu <roberta.dobrescu@gmail.com>
2014-09-26 23:58:40 +0300

    staging: dgnc: dgnc_driver.h: Remove non-standard spinlock's macros
    This patch removes non-standard spinlock's macros as they are no longer
    used by this driver.
    
    Signed-off-by: Roberta Dobrescu <roberta.dobrescu@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.h
+++ drivers/staging/dgnc/dgnc_driver.h
@@ -139,14 +139,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SNIFF_MASK	(SNIFF_MAX - 1)	/* Sniff wrap mask */
 
 /*
- * Lock function/defines.
- * Makes spotting lock/unlock locations easier.
- */
-# define DGNC_SPINLOCK_INIT(x)		spin_lock_init(&(x))
-# define DGNC_LOCK(x, y)		spin_lock_irqsave(&(x), y)
-# define DGNC_UNLOCK(x, y)		spin_unlock_irqrestore(&(x), y)
-
-/*
  * All the possible states the driver can be while being loaded.
  */
 enum {
