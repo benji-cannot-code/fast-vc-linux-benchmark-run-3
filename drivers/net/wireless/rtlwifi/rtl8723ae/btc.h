bac9db54682af54346986e34ce9fc31c6d9e9e30
Larry Finger <Larry.Finger@lwfinger.net>
2014-09-26 16:40:24 -0500

    rtlwifi: rtl8723ae: Update driver to match 06/28/14 Realtek version
    Not only does this patch update the driver to match the latest Realtek release,
    it is an important step in getting the internal code source at Realtek to match
    the code in the kernel. The primary reason for this is to make it easier for
    Realtek to maintain the kernel source without requiring an intermediate like me.
    
    In this process of merging the two source repositories, there are a lot
    of changes in both, and this commit is rather large.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rtlwifi/rtl8723ae/btc.h
+++ drivers/net/wireless/rtlwifi/rtl8723ae/btc.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  ** FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  ** more details.
  **
- ** You should have received a copy of the GNU General Public License along with
- ** this program; if not, write to the Free Software Foundation, Inc.,
- ** 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
- **
  ** The full GNU General Public License is included in this distribution in the
  ** file called LICENSE.
  **
@@ -25,8 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  ** Hsinchu 300, Taiwan.
  ** Larry Finger <Larry.Finger@lwfinger.net>
  **
- *****************************************************************************
- */
+ ******************************************************************************/
 
 #ifndef __RTL8723E_BTC_H__
 #define __RTL8723E_BTC_H__
