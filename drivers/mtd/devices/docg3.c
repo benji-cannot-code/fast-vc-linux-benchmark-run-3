8f0ab79cf2a6ed28be0fddf9651c301889cfbd4e
Robert Jarzmik <robert.jarzmik@free.fr>
2014-09-27 21:34:29 +0200

    mtd: docg3: add device-tree support
    Add device-tree support. This is straightforward as docg3 only uses the
    standard IOMEM resources.
    
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Cc: devicetree@vger.kernel.org
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/devices/docg3.c
+++ drivers/mtd/devices/docg3.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/errno.h>
+#include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/string.h>
 #include <linux/slab.h>
@@ -2119,10 +2120,17 @@ static int __exit docg3_release(struct platform_device *pdev)
 	return 0;
 }
 
+static struct of_device_id docg3_dt_ids[] = {
+	{ .compatible = "m-systems,diskonchip-g3" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, docg3_dt_ids);
+
 static struct platform_driver g3_driver = {
 	.driver		= {
 		.name	= "docg3",
 		.owner	= THIS_MODULE,
+		.of_match_table = of_match_ptr(docg3_dt_ids),
 	},
 	.suspend	= docg3_suspend,
 	.resume		= docg3_resume,
