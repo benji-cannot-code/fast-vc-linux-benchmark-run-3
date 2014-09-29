054b380c4ec873a30ac4c8c64e9dd8279881de14
Will Deacon <will.deacon@arm.com>
2014-09-29 10:06:19 -0600

    vfio/iommu_type1: add new VFIO_TYPE1_NESTING_IOMMU IOMMU type
    VFIO allows devices to be safely handed off to userspace by putting
    them behind an IOMMU configured to ensure DMA and interrupt isolation.
    This enables userspace KVM clients, such as kvmtool and qemu, to further
    map the device into a virtual machine.
    
    With IOMMUs such as the ARM SMMU, it is then possible to provide SMMU
    translation services to the guest operating system, which are nested
    with the existing translation installed by VFIO. However, enabling this
    feature means that the IOMMU driver must be informed that the VFIO domain
    is being created for the purposes of nested translation.
    
    This patch adds a new IOMMU type (VFIO_TYPE1_NESTING_IOMMU) to the VFIO
    type-1 driver. The new IOMMU type acts identically to the
    VFIO_TYPE1v2_IOMMU type, but additionally sets the DOMAIN_ATTR_NESTING
    attribute on its IOMMU domains.
    
    Cc: Joerg Roedel <joro@8bytes.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/vfio/vfio_iommu_type1.c
+++ drivers/vfio/vfio_iommu_type1.c
@@ -58,7 +58,8 @@ struct vfio_iommu {
 	struct list_head	domain_list;
 	struct mutex		lock;
 	struct rb_root		dma_list;
-	bool v2;
+	bool			v2;
+	bool			nesting;
 };
 
 struct vfio_domain {
@@ -706,6 +707,15 @@ static int vfio_iommu_type1_attach_group(void *iommu_data,
 		goto out_free;
 	}
 
+	if (iommu->nesting) {
+		int attr = 1;
+
+		ret = iommu_domain_set_attr(domain->domain, DOMAIN_ATTR_NESTING,
+					    &attr);
+		if (ret)
+			goto out_domain;
+	}
+
 	ret = iommu_attach_group(domain->domain, iommu_group);
 	if (ret)
 		goto out_domain;
@@ -820,17 +830,26 @@ static void *vfio_iommu_type1_open(unsigned long arg)
 {
 	struct vfio_iommu *iommu;
 
-	if (arg != VFIO_TYPE1_IOMMU && arg != VFIO_TYPE1v2_IOMMU)
-		return ERR_PTR(-EINVAL);
-
 	iommu = kzalloc(sizeof(*iommu), GFP_KERNEL);
 	if (!iommu)
 		return ERR_PTR(-ENOMEM);
 
+	switch (arg) {
+	case VFIO_TYPE1_IOMMU:
+		break;
+	case VFIO_TYPE1_NESTING_IOMMU:
+		iommu->nesting = true;
+	case VFIO_TYPE1v2_IOMMU:
+		iommu->v2 = true;
+		break;
+	default:
+		kfree(iommu);
+		return ERR_PTR(-EINVAL);
+	}
+
 	INIT_LIST_HEAD(&iommu->domain_list);
 	iommu->dma_list = RB_ROOT;
 	mutex_init(&iommu->lock);
-	iommu->v2 = (arg == VFIO_TYPE1v2_IOMMU);
 
 	return iommu;
 }
@@ -886,6 +905,7 @@ static long vfio_iommu_type1_ioctl(void *iommu_data,
 		switch (arg) {
 		case VFIO_TYPE1_IOMMU:
 		case VFIO_TYPE1v2_IOMMU:
+		case VFIO_TYPE1_NESTING_IOMMU:
 			return 1;
 		case VFIO_DMA_CC_IOMMU:
 			if (!iommu)
