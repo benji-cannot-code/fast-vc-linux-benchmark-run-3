4ba5587e4674d34ba61638a5215775940a556b0f
Jesper Nilsson <jespern@axis.com>
2014-09-30 15:34:10 +0200

    CRISv32: Add missing include for mm.h
    Fixes the following compile error.
    
    arch/cris/arch-v32/kernel/time.c: In function 'reset_watchdog':
    arch/cris/arch-v32/kernel/time.c:121:2:
            error: implicit declaration of function 'global_page_state'
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Jesper Nilsson <jesper.nilsson@axis.com>

--- arch/cris/arch-v32/kernel/time.c
+++ arch/cris/arch-v32/kernel/time.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/threads.h>
 #include <linux/cpufreq.h>
+#include <linux/mm.h>
 #include <asm/types.h>
 #include <asm/signal.h>
 #include <asm/io.h>
