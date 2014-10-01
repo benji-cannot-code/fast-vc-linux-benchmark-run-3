a1eea6ed4b1b0f0f345710438db1410107f88459
Feyza Yavuz <feyzaayavuz@gmail.com>
2014-10-01 21:29:51 +0300

    staging: rtl8192e: rtl8192e: Remove unnecessary return statements
    Patch the following checkpatch.pl warnings
    
    drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c:304: warning:
    void function return statements are not generally useful
    drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c:83: warning:
    void function return statements are not generally useful
    
    Signed-off-by: Feyza Yavuz <feyzaayavuz@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c
+++ drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c
@@ -80,7 +80,6 @@ void PHY_SetRF8256Bandwidth(struct net_device *dev,
 
 		}
 	}
-	return;
 }
 
 bool PHY_RF8256_Config(struct net_device *dev)
@@ -301,5 +300,4 @@ void PHY_SetRF8256OFDMTxPower(struct net_device *dev, u8 powerlevel)
 		rtl8192_setBBreg(dev, RegOffset[index], 0x7f7f7f7f, writeVal);
 	}
 
-	return;
 }
