77c5465c7fbd0b0223e6d1ae306912c6f403dc42
Stephen Rothwell <sfr@canb.auug.org.au>
2014-10-01 17:00:49 +1000

    fm10k: using vmalloc requires including linux/vmalloc.h
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/fm10k/fm10k_ethtool.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_ethtool.c
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Intel Corporation, 5200 N.E. Elam Young Parkway, Hillsboro, OR 97124-6497
  */
 
+#include <linux/vmalloc.h>
+
 #include "fm10k.h"
 
 struct fm10k_stats {
