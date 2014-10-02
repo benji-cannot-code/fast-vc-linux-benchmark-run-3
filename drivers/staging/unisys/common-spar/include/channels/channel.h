4bec95c2fa3584a8977aa7bc49dd6015ca251945
Melike Yurtoglu <aysemelikeyurtoglu@gmail.com>
2014-10-02 11:13:53 +0300

    staging: unisys: common-spar: Fix missing blank line warning
    Fixes "Missing a blank line after declarations" checkpatch.pl warning
    
    Signed-off-by: Melike Yurtoglu <aysemelikeyurtoglu@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/channel.h
+++ drivers/staging/unisys/common-spar/include/channels/channel.h
@@ -435,6 +435,7 @@ PathName_Last_N_Nodes(u8 *s, unsigned int n)
 {
 	u8 *p = s;
 	unsigned int node_count = 0;
+
 	while (*p != '\0') {
 		if ((*p == '/') || (*p == '\\'))
 			node_count++;
@@ -578,6 +579,7 @@ ULTRA_channel_client_release_os(void __iomem *pChannel, u8 *chanId,
 				void *logCtx, char *file, int line, char *func)
 {
 	CHANNEL_HEADER __iomem *pChan = pChannel;
+
 	if (readb(&pChan->CliErrorOS) != 0) {
 		/* we are in an error msg throttling state; come out of it */
 		UltraLogEvent(logCtx, CHANNELSTATE_DIAG_EVENTID_TRANSITOK,
