d99cec6c42a8f63ea5baf25afe7b098581bd2a68
Alex Elder <elder@linaro.org>
2014-10-02 12:30:05 -0500

    greybus: get rid of functions now...
    We decided yesterday that we would no longer support the notion of a
    "function."  Instead, a connection will simply exist between the AP
    and an interface on a module (and a CPort Id on each end).  What
    was previously considered the "function type" will now be handled
    as the "protocol" associated with the connection.
    
    Update gb_connection_create() to take just the interface and a cport
    id associated with that interface.
    
    Right now every module points back to a host device, so for now
    we'll establish the connection back to that.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/connection.c
+++ drivers/staging/greybus/connection.c
@@ -23,23 +23,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Returns a pointer to the new connection if successful, or a null
  * pointer otherwise.
  */
-struct gb_connection *gb_connection_create(struct greybus_host_device *hd,
-				struct gb_function *function)
+struct gb_connection *gb_connection_create(struct gb_interface *interface,
+						u16 cport_id)
 {
 	struct gb_connection *connection;
+	struct greybus_host_device *hd;
 
 	connection = kzalloc(sizeof(*connection), GFP_KERNEL);
 	if (!connection)
 		return NULL;
 
-	connection->cport_id = greybus_hd_cport_id_alloc(hd);
-	if (connection->cport_id == CPORT_ID_BAD) {
+	hd = interface->gmod->hd;
+	connection->hd_cport_id = greybus_hd_cport_id_alloc(hd);
+	if (connection->hd_cport_id == CPORT_ID_BAD) {
 		kfree(connection);
 		return NULL;
 	}
-
 	connection->hd = hd;			/* XXX refcount? */
-	connection->function = function;	/* XXX refcount? */
+	connection->interface = interface;	/* XXX refcount? */
+	connection->interface_cport_id = cport_id;
 	INIT_LIST_HEAD(&connection->operations);
 	atomic_set(&connection->op_cycle, 0);
 
@@ -57,9 +59,9 @@ void gb_connection_destroy(struct gb_connection *connection)
 	/* XXX Need to wait for any outstanding requests to complete */
 	WARN_ON(!list_empty(&connection->operations));
 
-	greybus_hd_cport_id_free(connection->hd, connection->cport_id);
-	/* kref_put(function); */
-	/* kref_put(hd); */
+	greybus_hd_cport_id_free(connection->hd, connection->hd_cport_id);
+	/* kref_put(connection->interface); */
+	/* kref_put(connection->hd); */
 	kfree(connection);
 }
 
--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "manifest.h"
 #include "module.h"
 #include "interface.h"
-#include "function.h"
 #include "connection.h"
 #include "operation.h"
 
