1e254cd19fd75802a85d1be44e2d158c9e1be6fe
Alex Elder <elder@linaro.org>
2014-10-01 21:54:13 -0500

    greybus: define greybus function abstraction
    Define new source files "function.h" and "function.c" to contain the
    definitions of the Greybus function abstraction.  A Greybus function
    represents an active entity connected to a CPort implemented by a
    Greybus interface.  A Greybus function has a type, which defines the
    protocol to be used to interact with the function.  A Greybus
    interface normally has at least two functions, but potentially many
    more.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "greybus_manifest.h"
 #include "module.h"
 #include "interface.h"
+#include "function.h"
 
 
 /* Matches up with the Greybus Protocol specification document */
--- drivers/staging/greybus/interface.h
+++ drivers/staging/greybus/interface.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct gb_interface {
 	struct gb_module	*gmod;
 	u8			interface_id;
+	struct list_head	functions;
 
 	struct list_head	links;	/* module->interfaces */
 };
