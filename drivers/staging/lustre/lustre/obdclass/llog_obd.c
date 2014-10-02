f8a7cf5b497d17aac6b4f2aa62b222210d1e463c
Greg Donald <gdonald@gmail.com>
2014-10-02 18:10:23 -0500

    drivers: staging: lustre: Fix "'foo* bar' should be 'foo *bar'" errors
    Fix checkpatch.pl "'foo* bar' should be 'foo *bar'" errors
    
    Signed-off-by: Greg Donald <gdonald@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/llog_obd.c
+++ drivers/staging/lustre/lustre/obdclass/llog_obd.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "llog_internal.h"
 
 /* helper functions for calling the llog obd methods */
-static struct llog_ctxt* llog_new_ctxt(struct obd_device *obd)
+static struct llog_ctxt *llog_new_ctxt(struct obd_device *obd)
 {
 	struct llog_ctxt *ctxt;
 
