7b471e0ace98af535b29d273d22125192efe25be
Denis CIOCCA <denis.ciocca@st.com>
2014-10-03 17:35:38 +0200

    iio:magnetometer: Removed unnecessary parameter on common_probe function
    Signed-off-by: Denis Ciocca <denis.ciocca@st.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/magnetometer/st_magn.h
+++ drivers/iio/magnetometer/st_magn.h
@@ -19,8 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LSM303DLM_MAGN_DEV_NAME		"lsm303dlm_magn"
 #define LIS3MDL_MAGN_DEV_NAME		"lis3mdl"
 
-int st_magn_common_probe(struct iio_dev *indio_dev,
-					struct st_sensors_platform_data *pdata);
+int st_magn_common_probe(struct iio_dev *indio_dev);
 void st_magn_common_remove(struct iio_dev *indio_dev);
 
 #ifdef CONFIG_IIO_BUFFER
