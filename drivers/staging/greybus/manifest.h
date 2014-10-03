88999d5e0e78eddf27ab4f3cf9e1ca0f19c30725
Alex Elder <elder@linaro.org>
2014-10-03 14:14:25 -0500

    greybus: fix module setup
    The code that was setting up a module was not properly initializing
    the module data structure.  Fixing this required a little rework.
    
    Now gb_add_module() (which the host device pointer and module id)
    allocates and initializes the structure, and passes it to
    gb_manifest_parse() for populating it further.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/manifest.h
+++ drivers/staging/greybus/manifest.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MANIFEST_H
 #define __MANIFEST_H
 
-struct gb_module *gb_manifest_parse(void *data, size_t size);
+struct gb_module;
+bool gb_manifest_parse(struct gb_module *gmod, void *data, size_t size);
 
 #endif /* __MANIFEST_H */
