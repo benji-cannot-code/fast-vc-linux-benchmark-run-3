c936371e6f82e8fd86c2c4981e2a93b60636390e
Alex Elder <elder@linaro.org>
2014-10-03 14:14:22 -0500

    greybus: fix some hasty bugs
    Fix some omissions found in the code.
        - initialize and use the host device connections list
        - rename the interface connections list (was "functions")
        - use the interface connections list
        - define a spinlock protecting the connections lists
        - declare gb_operation_submit() in "operation.h"
    
    And the cport id map lock is per-host device, it's shared across all
    host devices.  There's no need for one in struct greybus_host_device.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/connection.c
+++ drivers/staging/greybus/connection.c
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kernel_ver.h"
 #include "greybus.h"
 
+static DEFINE_SPINLOCK(gb_connections_lock);
+
 /*
  * Set up a Greybus connection, representing the bidirectional link
  * between a CPort on a (local) Greybus host device and a CPort on
@@ -44,6 +46,11 @@ struct gb_connection *gb_connection_create(struct gb_interface *interface,
 	connection->interface_cport_id = cport_id;
 	connection->protocol = protocol;
 
+	spin_lock_irq(&gb_connections_lock);
+	list_add_tail(&connection->hd_links, &hd->connections);
+	list_add_tail(&connection->interface_links, &interface->connections);
+	spin_unlock_irq(&gb_connections_lock);
+
 	INIT_LIST_HEAD(&connection->operations);
 	atomic_set(&connection->op_cycle, 0);
 
@@ -60,6 +67,11 @@ void gb_connection_destroy(struct gb_connection *connection)
 
 	/* XXX Need to wait for any outstanding requests to complete */
 	WARN_ON(!list_empty(&connection->operations));
+
+	spin_lock_irq(&gb_connections_lock);
+	list_del(&connection->hd_links);
+	list_del(&connection->interface_links);
+	spin_unlock_irq(&gb_connections_lock);
 
 	greybus_hd_cport_id_free(connection->hd, connection->hd_cport_id);
 	/* kref_put(connection->interface); */
