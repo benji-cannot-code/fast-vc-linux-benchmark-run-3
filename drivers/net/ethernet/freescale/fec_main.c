26ed85c4d8b6208e928c210b1eb0f2f9bc522cd5
Frank Li <Frank.Li@freescale.com>
2014-10-03 14:29:14 -0700

    net: fec: fix build error at m68k platform
    reproduce:
      wget https://git.kernel.org/cgit/linux/kernel/git/wfg/lkp-tests.git/plain/sbin/make.cross -O ~/bin/make.cross
      chmod +x ~/bin/make.cross
      git checkout 1b7bde6d659d30f171259cc2dfba8e5dab34e735
    
      make.cross ARCH=m68k m5275evb_defconfig
      make.cross ARCH=m68k
    
    All error/warnings:
    
       drivers/net/ethernet/freescale/fec_main.c: In function 'fec_enet_rx_queue':
    >> drivers/net/ethernet/freescale/fec_main.c:1470:3: error: implicit declaration of function 'prefetch' [-Werror=implicit-function-declaration]
          prefetch(skb->data - NET_IP_ALIGN);
          ^
       cc1: some warnings being treated as errors
    
    missed included prefetch.h
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Frank Li <Frank.Li@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regulator/consumer.h>
 #include <linux/if_vlan.h>
 #include <linux/pinctrl/consumer.h>
+#include <linux/prefetch.h>
 
 #include <asm/cacheflush.h>
 
