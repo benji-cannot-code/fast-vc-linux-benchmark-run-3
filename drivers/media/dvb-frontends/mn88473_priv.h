27a55eabfd4a4dfe74f5e63d1a753cafe7e72595
Antti Palosaari <crope@iki.fi>
2014-10-03 00:28:18 -0300

    [media] mn88473: convert to RegMap API
    Convert driver to I2C RegMap API. That offers unified register
    access routines, register value caching and more.
    
    We need 3 register maps, one for each register page, as chips has
    3 I2C address.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/mn88473_priv.h
+++ drivers/media/dvb-frontends/mn88473_priv.h
@@ -21,11 +21,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_frontend.h"
 #include "mn88473.h"
 #include <linux/firmware.h>
+#include <linux/regmap.h>
 
 #define MN88473_FIRMWARE "dvb-demod-mn88473-01.fw"
 
 struct mn88473_dev {
 	struct i2c_client *client[3];
+	struct regmap *regmap[3];
 	struct dvb_frontend fe;
 	u16 i2c_wr_max;
 	fe_delivery_system_t delivery_system;
