7b84f581f8116c883cd5c23f4d561faebdd6da4f
Sakari Ailus <sakari.ailus@linux.intel.com>
2014-10-03 11:38:32 -0300

    [media] smiapp: Replace pll_flags quirk with more generic init quirk
    The pll_flags quirk just returned the extra PLL flags the sensor required,
    but the init quirk is far more versatile. It can be used to perform any
    extra initialisation needed by the sensor, including allocating memory for
    sensor specific struct and creating sensor specific new controls.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/i2c/smiapp/smiapp-core.c
+++ drivers/media/i2c/smiapp/smiapp-core.c
@@ -2698,7 +2698,6 @@ static int smiapp_init(struct smiapp_sensor *sensor)
 	pll->bus_type = SMIAPP_PLL_BUS_TYPE_CSI2;
 	pll->csi2.lanes = sensor->platform_data->lanes;
 	pll->ext_clk_freq_hz = sensor->platform_data->ext_clk;
-	pll->flags = smiapp_call_quirk(sensor, pll_flags);
 	pll->scale_n = sensor->limits[SMIAPP_LIMIT_SCALER_N_MIN];
 	/* Profile 0 sensors have no separate OP clock branch. */
 	if (sensor->minfo.smiapp_profile == SMIAPP_PROFILE_0)
@@ -2771,6 +2770,10 @@ static int smiapp_init(struct smiapp_sensor *sensor)
 	smiapp_read_frame_fmt(sensor);
 	rval = smiapp_init_controls(sensor);
 	if (rval < 0)
+		goto out_cleanup;
+
+	rval = smiapp_call_quirk(sensor, init);
+	if (rval)
 		goto out_cleanup;
 
 	rval = smiapp_get_mbus_formats(sensor);
