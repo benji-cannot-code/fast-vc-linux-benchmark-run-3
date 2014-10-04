b5e9e339c6b4be34366000f34e8fdbf3dc3c4904
Andy Zhou <azhou@nicira.com>
2014-10-04 06:19:11 +0000

    fm10k: Add CONFIG_FM10K_VXLAN configuration option
    Compiling with CONFIG_FM10K=y and VXLAN=m resulting in linking error:
    
       drivers/built-in.o: In function `fm10k_open':
       (.text+0x1f9d7a): undefined reference to `vxlan_get_rx_port'
       make: *** [vmlinux] Error 1
    
    The fix follows the same strategy as I40E.
    
    Signed-off-by: Andy Zhou <azhou@nicira.com>
    Acked-by: Alexander Duyck <alexander.h.duyck@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/fm10k/fm10k_netdev.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_netdev.c
@@ -21,9 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "fm10k.h"
 #include <linux/vmalloc.h>
-#if IS_ENABLED(CONFIG_VXLAN)
+#if IS_ENABLED(CONFIG_FM10K_VXLAN)
 #include <net/vxlan.h>
-#endif /* CONFIG_VXLAN */
+#endif /* CONFIG_FM10K_VXLAN */
 
 /**
  * fm10k_setup_tx_resources - allocate Tx resources (Descriptors)
@@ -557,7 +557,7 @@ int fm10k_open(struct net_device *netdev)
 	if (err)
 		goto err_set_queues;
 
-#if IS_ENABLED(CONFIG_VXLAN)
+#if IS_ENABLED(CONFIG_FM10K_VXLAN)
 	/* update VXLAN port configuration */
 	vxlan_get_rx_port(netdev);
 
