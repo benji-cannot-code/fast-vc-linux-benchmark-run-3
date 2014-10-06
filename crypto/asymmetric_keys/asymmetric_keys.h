60dae496a7971697f4d36a9a39c6bc76548fc7dc
Dmitry Kasatkin <d.kasatkin@samsung.com>
2014-10-06 15:21:05 +0100

    KEYS: Restore partial ID matching functionality for asymmetric keys
    Bring back the functionality whereby an asymmetric key can be matched with a
    partial match on one of its IDs.
    
    Whilst we're at it, allow for the possibility of having an increased number of
    IDs.
    
    Reported-by: Dmitry Kasatkin <d.kasatkin@samsung.com>
    Signed-off-by: Dmitry Kasatkin <d.kasatkin@samsung.com>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- crypto/asymmetric_keys/asymmetric_keys.h
+++ crypto/asymmetric_keys/asymmetric_keys.h
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 2 of the Licence, or (at your option) any later version.
  */
 
-extern bool asymmetric_match_key_ids(const struct asymmetric_key_ids *kids,
-				     const struct asymmetric_key_id *match_id);
-
 extern struct asymmetric_key_id *asymmetric_key_hex_to_key_id(const char *id);
 
 static inline
