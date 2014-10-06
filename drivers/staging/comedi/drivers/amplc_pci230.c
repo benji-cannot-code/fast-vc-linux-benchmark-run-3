477cbbf71b042786e2d4843159a0dfc9de9a4e31
Tapasweni Pathak <tapaswenipathak@gmail.com>
2014-10-06 11:26:49 +0530

    staging: comedi: drivers: Replace "the the " with "the"
    This patch replace "the the " with "the".
    The replacement couldn't be automated because sometimes
    the first "the" was meant to be another word.
    
    Signed-off-by: Tapasweni Pathak <tapaswenipathak@gmail.com>
    Acked-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_pci230.c
+++ drivers/staging/comedi/drivers/amplc_pci230.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * automatically.
  *
  * The PCI230+ and PCI260+ have the same PCI device IDs as the PCI230 and
- * PCI260, but can be distinguished by the the size of the PCI regions.  A
+ * PCI260, but can be distinguished by the size of the PCI regions.  A
  * card will be configured as a "+" model if detected as such.
  *
  * Subdevices:
