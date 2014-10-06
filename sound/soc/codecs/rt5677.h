ddb5720127072fd3fe480dc4b4cfa295098d3631
Oder Chiou <oder_chiou@realtek.com>
2014-10-06 16:30:51 +0800

    ASoC: rt5677: Support DSP function for VAD application
    The ALC5677 has a programmable DSP, and there is a SPI that is dadicated for DSP
    firmware loading. Therefore, the patch includes a SPI driver for writing the DSP
    firmware. The VAD(Voice Activaty Detection) has be implemented and use the DSP to
    recognize the key phase.
    
    Signed-off-by: Oder Chiou <oder_chiou@realtek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5677.h
+++ sound/soc/codecs/rt5677.h
@@ -1508,6 +1508,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RT5677_GPIO5_FUNC_GPIO			(0x0 << 9)
 #define RT5677_GPIO5_FUNC_DMIC			(0x1 << 9)
 
+#define RT5677_FIRMWARE1	"rt5677_dsp_fw1.bin"
+#define RT5677_FIRMWARE2	"rt5677_dsp_fw2.bin"
+
 /* System Clock Source */
 enum {
 	RT5677_SCLK_S_MCLK,
@@ -1547,6 +1550,8 @@ struct rt5677_priv {
 	struct snd_soc_codec *codec;
 	struct rt5677_platform_data pdata;
 	struct regmap *regmap;
+	const struct firmware *fw1, *fw2;
+	struct mutex dsp_cmd_lock;
 
 	int sysclk;
 	int sysclk_src;
@@ -1560,6 +1565,7 @@ struct rt5677_priv {
 #ifdef CONFIG_GPIOLIB
 	struct gpio_chip gpio_chip;
 #endif
+	bool dsp_vad_en;
 };
 
 #endif /* __RT5677_H__ */
