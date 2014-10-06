45e6e4ba764e4945114c0abfa6561b7dc3c6fed5
Ulf Hansson <ulf.hansson@linaro.org>
2014-10-06 11:00:15 +0200

    mmc: core: Don't export the to_sdio_driver macro
    The macro is only used by the mmc core, so let's move it in there.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/sdio_bus.c
+++ drivers/mmc/core/sdio_bus.c
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sdio_cis.h"
 #include "sdio_bus.h"
 
+#define to_sdio_driver(d)	container_of(d, struct sdio_driver, drv)
+
 /* show configuration fields */
 #define sdio_config_attr(field, format_string)				\
 static ssize_t								\
