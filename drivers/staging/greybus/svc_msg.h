ebc2d6940dd646228b476ba5a522f05896929af2
Greg Kroah-Hartman <greg@kroah.com>
2014-10-06 20:37:18 -0700

    greybus: svc_msg.h: add bsd license to file so that firmware can use it.

--- drivers/staging/greybus/svc_msg.h
+++ drivers/staging/greybus/svc_msg.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright 2014 Google Inc.
  *
- * Released under the GPLv2 only.
+ * Released under the GPLv2 and BSD license.
  */
 
 #ifndef __SVC_MSG_H
