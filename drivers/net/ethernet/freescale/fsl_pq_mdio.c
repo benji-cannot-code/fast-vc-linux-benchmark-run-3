146507b8096f4e3e4f64501a8ad9dfb782dcf68e
Claudiu Manoil <claudiu.manoil@freescale.com>
2014-10-07 10:44:28 +0300

    net/fsl_pq_mdio: Fix asm/ucc.h compile error for ARM
    The UCC specific code included in fsl_pq_mdio.c (with
    function calls from asm/ucc.h) is already guarded
    by these config options, so this ARM build fix only
    provides consistency with the rest UCC specific code.
    
    Signed-off-by: Claudiu Manoil <claudiu.manoil@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/fsl_pq_mdio.c
+++ drivers/net/ethernet/freescale/fsl_pq_mdio.c
@@ -29,7 +29,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_device.h>
 
 #include <asm/io.h>
+#if IS_ENABLED(CONFIG_UCC_GETH)
 #include <asm/ucc.h>	/* for ucc_set_qe_mux_mii_mng() */
+#endif
 
 #include "gianfar.h"
 
