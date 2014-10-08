095ac25008ee8e1b11fdd9f34747dfc6c03d95c3
Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
2014-10-08 21:57:27 +0200

    serial/max310x: Remove obsolete #ifset TIOC[SG]RS485
    Commit e676253b19b2 ("serial/8250: Add support for RS485 IOCTLs") added
    references to TIOC[SG]RS48 on 8250_core.c. This change triggered the
    need to define them in all the arches that uses tty/serial.
    
    This made #ifdef TIOC[SG]RS48 obsolete.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Jiri Slaby <jslaby@suse.cz>
    Cc: linux-serial@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/max310x.c
+++ drivers/tty/serial/max310x.c
@@ -881,7 +881,6 @@ static void max310x_set_termios(struct uart_port *port,
 static int max310x_ioctl(struct uart_port *port, unsigned int cmd,
 			 unsigned long arg)
 {
-#if defined(TIOCSRS485) && defined(TIOCGRS485)
 	struct serial_rs485 rs485;
 	unsigned int val;
 
@@ -924,7 +923,6 @@ static int max310x_ioctl(struct uart_port *port, unsigned int cmd,
 	default:
 		break;
 	}
-#endif
 
 	return -ENOIOCTLCMD;
 }
