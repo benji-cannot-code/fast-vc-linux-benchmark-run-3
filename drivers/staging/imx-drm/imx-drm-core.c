461e775c7c3317b0de2f7bcb81b87a87d814e1e3
Fabio Estevam <fabio.estevam@freescale.com>
2014-10-07 21:42:19 -0300

    imx-drm: imx-drm-core: Remove unneeded forward declaration
    There is no need to forward declare 'struct imx_drm_crtc' because the
    "imx-drm.h" header file already includes it.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/imx-drm/imx-drm-core.c
+++ drivers/staging/imx-drm/imx-drm-core.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_CRTC	4
 
-struct imx_drm_crtc;
-
 struct imx_drm_component {
 	struct device_node *of_node;
 	struct list_head list;
