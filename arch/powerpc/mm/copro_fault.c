f8543a192b6df096d808b4d188641733c9a09aff
Ian Munsie <imunsie@au1.ibm.com>
2014-10-08 19:55:00 +1100

    powerpc/mm: Add hooks for cxl
    This adds hooks into the core powerpc mm code for cxl.
    
    The core powerpc code sometimes uses local tlbie. Unfortunately this won't
    work with the current cxl driver as it relies on snooping tlbie broadcasts.
    
    The cxl hardware can have TLB entries invalidated via MMIO but this is not
    currently supported by the driver. In future we can make local tlbie smarter so
    that it invalidates cxl contexts via MMIO when it needs to but for now we have
    this workaround.
    
    This workaround checks for any active cxl contexts and if so, disables local
    tlbie.
    
    This also adds a hook for when SLBs are invalidated. This ensures any
    corresponding SLBs in cxl are also invalidated at the same time. This is
    required for segment demotion.
    
    Signed-off-by: Ian Munsie <imunsie@au1.ibm.com>
    Signed-off-by: Michael Neuling <mikey@neuling.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/copro_fault.c
+++ arch/powerpc/mm/copro_fault.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/reg.h>
 #include <asm/copro.h>
 #include <asm/spu.h>
+#include <misc/cxl.h>
 
 /*
  * This ought to be kept in sync with the powerpc specific do_page_fault
@@ -144,5 +145,6 @@ void copro_flush_all_slbs(struct mm_struct *mm)
 #ifdef CONFIG_SPU_BASE
 	spu_flush_all_slbs(mm);
 #endif
+	cxl_slbia(mm);
 }
 EXPORT_SYMBOL_GPL(copro_flush_all_slbs);
