6182f28981f316d4d4cddd18a251574cb27bd095
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2014-10-10 00:02:12 +0400

    usb: phy: remove file names from heading comments
    File names in the heading comments fell out of favor long ago, and these weren't
    even changed when the drivers were moved from drivers/usb/otg/, so remove them
    at last...
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/phy/phy-ab8500-usb.c
+++ drivers/usb/phy/phy-ab8500-usb.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * drivers/usb/otg/ab8500_usb.c
- *
  * USB transceiver driver for AB8500 family chips
  *
  * Copyright (C) 2010-2013 ST-Ericsson AB
--- drivers/usb/phy/phy-generic.c
+++ drivers/usb/phy/phy-generic.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * drivers/usb/otg/nop-usb-xceiv.c
- *
  * NOP USB transceiver for all USB transceiver which are either built-in
  * into USB IP or which are mostly autonomous.
  *
