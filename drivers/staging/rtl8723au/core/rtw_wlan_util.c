a335d21bbd7cb999f479adaa9bf2531f2f13207e
Yeliz Taneroglu <yeliztaneroglu@gmail.com>
2014-10-09 16:35:31 +0300

    staging: rtl8723au: core: rtw_wlan_util: Fix void function return statements style warning.
    The following patch fixes the checkpatch.pl warning:
    
    drivers/staging/rtl8723au/core/rtw_wlan_util.c WARNING: void function return statements are not generally useful
    
    Signed-off-by: Yeliz Taneroglu <yeliztaneroglu@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_wlan_util.c
+++ drivers/staging/rtl8723au/core/rtw_wlan_util.c
@@ -609,8 +609,6 @@ void WMMOnAssocRsp23a(struct rtw_adapter *padapter)
 		DBG_8723A("wmm_para_seq(%d): %d\n", i,
 			  pxmitpriv->wmm_para_seq[i]);
 	}
-
-	return;
 }
 
 static void bwmode_update_check(struct rtw_adapter *padapter, const u8 *p)
@@ -751,7 +749,6 @@ void HT_caps_handler23a(struct rtw_adapter *padapter, const u8 *p)
 		else
 			cap->mcs.rx_mask[i] &= MCS_rate_2R23A[i];
 	}
-	return;
 }
 
 void HT_info_handler23a(struct rtw_adapter *padapter, const u8 *p)
@@ -772,7 +769,6 @@ void HT_info_handler23a(struct rtw_adapter *padapter, const u8 *p)
 
 	pmlmeinfo->HT_info_enable = 1;
 	memcpy(&pmlmeinfo->HT_info, p + 2, p[1]);
-	return;
 }
 
 void HTOnAssocRsp23a(struct rtw_adapter *padapter)
