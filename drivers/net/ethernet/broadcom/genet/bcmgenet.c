a079d0a7f48d9fbd826183247a4cec155fa81cd8
Florian Fainelli <f.fainelli@gmail.com>
2014-10-10 10:51:52 -0700

    net: bcmgenet: fix off-by-one in incrementing read pointer
    Commit b629be5c8399d7c423b92135eb43a86c924d1cbc ("net: bcmgenet: check
    harder for out of memory conditions") moved the increment of the local
    read pointer *before* reading from the hardware descriptor using
    dmadesc_get_length_status(), which creates an off-by-one situation.
    
    Fix this by moving again the read_ptr increment after we have read the
    hardware descriptor to get both the control block and the read pointer
    back in sync.
    
    Fixes: b629be5c8399 ("net: bcmgenet: check harder for out of memory conditions")
    Signed-off-by: Jaedon Shin <jaedon.shin@gmail.com>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Petri Gynther <pgynther@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/genet/bcmgenet.c
+++ drivers/net/ethernet/broadcom/genet/bcmgenet.c
@@ -1286,11 +1286,6 @@ static unsigned int bcmgenet_desc_rx(struct bcmgenet_priv *priv,
 		cb = &priv->rx_cbs[priv->rx_read_ptr];
 		skb = cb->skb;
 
-		rxpktprocessed++;
-
-		priv->rx_read_ptr++;
-		priv->rx_read_ptr &= (priv->num_rx_bds - 1);
-
 		/* We do not have a backing SKB, so we do not have a
 		 * corresponding DMA mapping for this incoming packet since
 		 * bcmgenet_rx_refill always either has both skb and mapping or
@@ -1405,6 +1400,10 @@ refill:
 		err = bcmgenet_rx_refill(priv, cb);
 		if (err)
 			netif_err(priv, rx_err, dev, "Rx refill failed\n");
+
+		rxpktprocessed++;
+		priv->rx_read_ptr++;
+		priv->rx_read_ptr &= (priv->num_rx_bds - 1);
 	}
 
 	return rxpktprocessed;
