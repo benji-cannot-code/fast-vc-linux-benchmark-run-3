cf0fa3c52e9a7c2d0037c3dc068a2abd420e39c1
Tapasweni Pathak <tapaswenipathak@gmail.com>
2014-10-10 07:43:36 +0530

    staging: vt6655: Remove unused code inside switch case
    This patch removes dead code in private_ioctl function and
    iwctl_siwmode function, in file ioctl.c.
    
    Inside switch, for a case no code gets executed
    after break.
    
    This was detected by smatch.
    
    Signed-off-by: Tapasweni Pathak <tapaswenipathak@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/iwctl.c
+++ drivers/staging/vt6655/iwctl.c
@@ -465,15 +465,6 @@ int iwctl_siwmode(struct net_device *dev,
 		rc = -EOPNOTSUPP;
 		break;
 
-		if (pMgmt->eConfigMode != WMAC_CONFIG_AP) {
-			pMgmt->eConfigMode = WMAC_CONFIG_AP;
-			if (pDevice->flags & DEVICE_FLAGS_OPENED)
-				pDevice->bCommit = true;
-
-		}
-		pr_debug("set mode to Access Point\n");
-		break;
-
 	case IW_MODE_REPEAT:
 		pMgmt->eConfigMode = WMAC_CONFIG_ESS_STA;
 		rc = -EOPNOTSUPP;
