9af427af3d5766ea2f1e4b15ed39cb1dcf0c1bac
Georgi Djakov <gdjakov@mm-sol.com>
2014-10-10 16:57:24 +0300

    clk: qcom: Fix duplicate rbcpr clock name
    There is a duplication in a clock name for apq8084 platform that causes
    the following warning: "RBCPR_CLK_SRC" redefined
    
    Resolve this by adding a MMSS_ prefix to this clock and making its name
    coherent with msm8974 platform.
    
    Fixes: 2b46cd23a5a2 ("clk: qcom: Add APQ8084 Multimedia Clock Controller (MMCC) support")
    Signed-off-by: Georgi Djakov <gdjakov@mm-sol.com>
    Reviewed-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Michael Turquette <mturquette@linaro.org>

--- include/dt-bindings/clock/qcom,mmcc-apq8084.h
+++ include/dt-bindings/clock/qcom,mmcc-apq8084.h
@@ -61,7 +61,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ESC1_CLK_SRC			43
 #define HDMI_CLK_SRC			44
 #define VSYNC_CLK_SRC			45
-#define RBCPR_CLK_SRC			46
+#define MMSS_RBCPR_CLK_SRC		46
 #define RBBMTIMER_CLK_SRC		47
 #define MAPLE_CLK_SRC			48
 #define VDP_CLK_SRC			49
