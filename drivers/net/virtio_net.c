3e1e002d24cdf7f22098e96c037109135ea6fde1
Michael S. Tsirkin <mst@redhat.com>
2014-10-15 10:22:29 +1030

    virtio-net: drop config_mutex
    config_mutex served two purposes: prevent multiple concurrent config
    change handlers, and synchronize access to config_enable flag.
    
    Since commit dbf2576e37da0fcc7aacbfbb9fd5d3de7888a3c1
        workqueue: make all workqueues non-reentrant
    all workqueues are non-reentrant, and config_enable
    is now gone.
    
    Get rid of the unnecessary lock.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -133,9 +133,6 @@ struct virtnet_info {
 	/* Work struct for config space updates */
 	struct work_struct config_work;
 
-	/* Lock for config space updates */
-	struct mutex config_lock;
-
 	/* Does the affinity hint is set for virtqueues? */
 	bool affinity_hint_set;
 
@@ -1405,7 +1402,6 @@ static void virtnet_config_changed_work(struct work_struct *work)
 		container_of(work, struct virtnet_info, config_work);
 	u16 v;
 
-	mutex_lock(&vi->config_lock);
 	if (virtio_cread_feature(vi->vdev, VIRTIO_NET_F_STATUS,
 				 struct virtio_net_config, status, &v) < 0)
 		goto done;
@@ -1431,7 +1427,7 @@ static void virtnet_config_changed_work(struct work_struct *work)
 		netif_tx_stop_all_queues(vi->dev);
 	}
 done:
-	mutex_unlock(&vi->config_lock);
+	return;
 }
 
 static void virtnet_config_changed(struct virtio_device *vdev)
@@ -1752,7 +1748,6 @@ static int virtnet_probe(struct virtio_device *vdev)
 		u64_stats_init(&virtnet_stats->rx_syncp);
 	}
 
-	mutex_init(&vi->config_lock);
 	INIT_WORK(&vi->config_work, virtnet_config_changed_work);
 
 	/* If we can receive ANY GSO packets, we must allocate large ones. */
