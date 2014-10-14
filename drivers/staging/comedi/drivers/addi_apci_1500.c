d5452b51ec06a443bb073710343b3b5fb359ae4b
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-10-14 10:44:48 -0700

    staging: comedi: addi_apci_1500: separate from addi_common.h
    Remove the need to include addi_common.h by introducing a new private
    data definition. Only include the members that are actually used by
    the driver.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi_apci_1500.c
+++ drivers/staging/comedi/drivers/addi_apci_1500.c
@@ -1,12 +1,21 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/sched.h>
+#include <linux/interrupt.h>
 
 #include "../comedidev.h"
 #include "comedi_fc.h"
 #include "amcc_s5933.h"
 
-#include "addi-data/addi_common.h"
+struct apci1500_private {
+	int iobase;
+	int i_IobaseAmcc;
+	int i_IobaseAddon;
+	int i_IobaseReserved;
+	unsigned char b_OutputMemoryStatus;
+	struct task_struct *tsk_Current;
+};
 
 #include "addi-data/hwdrv_apci1500.c"
 
@@ -14,7 +23,7 @@ static int apci1500_auto_attach(struct comedi_device *dev,
 				unsigned long context)
 {
 	struct pci_dev *pcidev = comedi_to_pci_dev(dev);
-	struct addi_private *devpriv;
+	struct apci1500_private *devpriv;
 	struct comedi_subdevice *s;
 	int ret;
 
