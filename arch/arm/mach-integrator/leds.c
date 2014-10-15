4a7f6a3653d980979e9261b109486a92bc5d1a27
Linus Walleij <linus.walleij@linaro.org>
2014-10-15 17:33:13 +0200

    ARM: integrator: move debug LEDs to syscon LED driver
    The Integrator debug block is a simple set of registers, make
    it a syscon and register the four LEDs on the Integrator/AP
    baseboard as syscon LEDs.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-integrator/leds.c
+++ arch/arm/mach-integrator/leds.c
@@ -17,12 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if defined(CONFIG_NEW_LEDS) && defined(CONFIG_LEDS_CLASS)
 
-#define ALPHA_REG __io_address(INTEGRATOR_DBG_BASE)
-#define LEDREG	(__io_address(INTEGRATOR_DBG_BASE) + INTEGRATOR_DBG_LEDS_OFFSET)
-
 struct integrator_led {
 	struct led_classdev	cdev;
-	u8			mask;
 };
 
 /*
@@ -33,39 +29,8 @@ static const struct {
 	const char *name;
 	const char *trigger;
 } integrator_leds[] = {
-	{ "integrator:green0", "heartbeat", },
-	{ "integrator:yellow", },
-	{ "integrator:red", },
-	{ "integrator:green1", },
 	{ "integrator:core_module", "cpu0", },
 };
-
-static void integrator_led_set(struct led_classdev *cdev,
-			      enum led_brightness b)
-{
-	struct integrator_led *led = container_of(cdev,
-						 struct integrator_led, cdev);
-	u32 reg = __raw_readl(LEDREG);
-
-	if (b != LED_OFF)
-		reg |= led->mask;
-	else
-		reg &= ~led->mask;
-
-	while (__raw_readl(ALPHA_REG) & 1)
-		cpu_relax();
-
-	__raw_writel(reg, LEDREG);
-}
-
-static enum led_brightness integrator_led_get(struct led_classdev *cdev)
-{
-	struct integrator_led *led = container_of(cdev,
-						 struct integrator_led, cdev);
-	u32 reg = __raw_readl(LEDREG);
-
-	return (reg & led->mask) ? LED_FULL : LED_OFF;
-}
 
 static void cm_led_set(struct led_classdev *cdev,
 			      enum led_brightness b)
@@ -94,19 +59,10 @@ static int __init integrator_leds_init(void)
 		if (!led)
 			break;
 
-
 		led->cdev.name = integrator_leds[i].name;
-
-		if (i == 4) { /* Setting for LED in core module */
-			led->cdev.brightness_set = cm_led_set;
-			led->cdev.brightness_get = cm_led_get;
-		} else {
-			led->cdev.brightness_set = integrator_led_set;
-			led->cdev.brightness_get = integrator_led_get;
-		}
-
+		led->cdev.brightness_set = cm_led_set;
+		led->cdev.brightness_get = cm_led_get;
 		led->cdev.default_trigger = integrator_leds[i].trigger;
-		led->mask = BIT(i);
 
 		if (led_classdev_register(NULL, &led->cdev) < 0) {
 			kfree(led);
