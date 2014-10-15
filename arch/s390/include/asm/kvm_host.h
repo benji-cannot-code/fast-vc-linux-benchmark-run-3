4d21a2b5b6e39c260a9a50034fd3c661c499e46e
David Hildenbrand <dahi@linux.vnet.ibm.com>
2014-10-15 16:48:16 +0200

    KVM: s390: new parameter for SIGP STOP irqs
    In order to get rid of the action_flags and to properly migrate pending SIGP
    STOP irqs triggered e.g. by SIGP STOP AND STORE STATUS, we need to remember
    whether to store the status when stopping.
    
    For this reason, a new parameter (flags) for the SIGP STOP irq is introduced.
    These flags further define details of the requested STOP and can be easily
    migrated.
    
    Reviewed-by: Thomas Huth <thuth@linux.vnet.ibm.com>
    Acked-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: David Hildenbrand <dahi@linux.vnet.ibm.com>
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>

--- arch/s390/include/asm/kvm_host.h
+++ arch/s390/include/asm/kvm_host.h
@@ -379,6 +379,7 @@ struct kvm_s390_interrupt_info {
 		struct kvm_s390_emerg_info emerg;
 		struct kvm_s390_extcall_info extcall;
 		struct kvm_s390_prefix_info prefix;
+		struct kvm_s390_stop_info stop;
 		struct kvm_s390_mchk_info mchk;
 	};
 };
@@ -394,6 +395,7 @@ struct kvm_s390_irq_payload {
 	struct kvm_s390_emerg_info emerg;
 	struct kvm_s390_extcall_info extcall;
 	struct kvm_s390_prefix_info prefix;
+	struct kvm_s390_stop_info stop;
 	struct kvm_s390_mchk_info mchk;
 };
 
