763c331dff66df944a507a87b65f58f1674616e7
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-10-16 21:29:48 +0300

    drm/i915: Don't kick the power seqeuncer just to check if we have vdd/panel power
    If there's no power sequencer assigned to the port currently we can't
    very well have vdd or panel power enabled either. If we would try to
    check that from the pps registers we'd need to pick a power seqeuncer
    and kick it. So let's skip the register read and the kick.
    
    Note that there's still a bit an issue about correctly recovering pps
    state from resume if the bios is nasty: With this check we'll always
    assume that the pps is off. But that's better done in a follow-up
    patch and it shouldn't be too harmful - at most we waste time enabling
    the pps if it's on already.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    [danvet: Add note about resume issues Imre spotted.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -598,6 +598,10 @@ static bool edp_have_panel_power(struct intel_dp *intel_dp)
 
 	lockdep_assert_held(&dev_priv->pps_mutex);
 
+	if (IS_VALLEYVIEW(dev) &&
+	    intel_dp->pps_pipe == INVALID_PIPE)
+		return false;
+
 	return (I915_READ(_pp_stat_reg(intel_dp)) & PP_ON) != 0;
 }
 
@@ -607,6 +611,10 @@ static bool edp_have_panel_vdd(struct intel_dp *intel_dp)
 	struct drm_i915_private *dev_priv = dev->dev_private;
 
 	lockdep_assert_held(&dev_priv->pps_mutex);
+
+	if (IS_VALLEYVIEW(dev) &&
+	    intel_dp->pps_pipe == INVALID_PIPE)
+		return false;
 
 	return I915_READ(_pp_ctrl_reg(intel_dp)) & EDP_FORCE_VDD;
 }
