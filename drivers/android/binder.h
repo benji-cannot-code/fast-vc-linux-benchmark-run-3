9e899b84b28bd10a154bf15079277774641733d1
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-10-16 14:40:38 +0200

    staging: android: binder: move to the "real" part of the kernel
    The Android binder code has been "stable" for many years now.  No matter
    what comes in the future, we are going to have to support this API, so
    might as well move it to the "real" part of the kernel as there's no
    real work that needs to be done to the existing code.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder.h
+++ drivers/android/binder.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BINDER_IPC_32BIT 1
 #endif
 
-#include "uapi/binder.h"
+#include <uapi/linux/android/binder.h>
 
 #endif /* _LINUX_BINDER_H */
 
