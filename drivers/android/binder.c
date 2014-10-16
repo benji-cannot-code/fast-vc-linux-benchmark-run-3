0707e576bbde6a795ffc7b83a719b675c59c6de9
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-10-16 15:26:51 +0200

    android: binder: remove binder.h
    binder.h isn't needed to just include a uapi file and set a single
    define, so move it into binder.c to save a few lines of code.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -39,7 +39,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/pid_namespace.h>
 
-#include "binder.h"
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
+#define BINDER_IPC_32BIT 1
+#endif
+
+#include <uapi/linux/android/binder.h>
 #include "binder_trace.h"
 
 static DEFINE_MUTEX(binder_main_lock);
