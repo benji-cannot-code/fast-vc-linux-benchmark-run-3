35cbe16b588efb426942f28c1819ebcaffdd339f
Tomi Valkeinen <tomi.valkeinen@ti.com>
2014-10-16 12:19:37 +0300

    OMAPFB: fix releasing overlays
    omapfb disables all the overlays when freeing resources, but it should
    also remove those overlays from overlay managers.
    
    Not doing so causes a crash if omapfb is unbound and bound, or omapfb
    module is removed and loaded, while keeping omapdss around.
    
    Fix this by calling unset_manager() for all overlays.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/fbdev/omap2/omapfb/omapfb-main.c
+++ drivers/video/fbdev/omap2/omapfb/omapfb-main.c
@@ -1838,6 +1838,9 @@ static void omapfb_free_resources(struct omapfb2_device *fbdev)
 		struct omap_overlay *ovl = fbdev->overlays[i];
 
 		ovl->disable(ovl);
+
+		if (ovl->manager)
+			ovl->unset_manager(ovl);
 	}
 
 	for (i = 0; i < fbdev->num_fbs; i++)
