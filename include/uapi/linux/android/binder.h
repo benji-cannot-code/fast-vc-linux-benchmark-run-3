cc40bcdb0586e4658fccc671c21277d2f9395232
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-10-16 15:22:30 +0200

    android: uapi: binder.h
    add types.h to .h file to pass the checker scripts, and provide a proper
    uapi .h file.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/android/binder.h
+++ include/uapi/linux/android/binder.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _UAPI_LINUX_BINDER_H
 #define _UAPI_LINUX_BINDER_H
 
+#include <linux/types.h>
 #include <linux/ioctl.h>
 
 #define B_PACK_CHARS(c1, c2, c3, c4) \
