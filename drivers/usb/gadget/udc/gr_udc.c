e26b81bf8d5bd105c064b9ed3c0838a505a08daf
Felipe Balbi <balbi@ti.com>
2014-10-17 11:38:39 -0500

    usb: gadget: udc: gr_udc: remove bind/unbind messages
    now that we provide generic register/unregister
    debugging messages from udc-core, we can remove
    the same messages from this driver.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/udc/gr_udc.c
+++ drivers/usb/gadget/udc/gr_udc.c
@@ -1933,9 +1933,6 @@ static int gr_udc_start(struct usb_gadget *gadget,
 
 	spin_unlock(&dev->lock);
 
-	dev_info(dev->dev, "Started with gadget driver '%s'\n",
-		 driver->driver.name);
-
 	return 0;
 }
 
@@ -1951,8 +1948,6 @@ static int gr_udc_stop(struct usb_gadget *gadget,
 	gr_stop_activity(dev);
 
 	spin_unlock_irqrestore(&dev->lock, flags);
-
-	dev_info(dev->dev, "Stopped\n");
 
 	return 0;
 }
