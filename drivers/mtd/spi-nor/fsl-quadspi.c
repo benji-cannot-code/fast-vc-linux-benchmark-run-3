c1c3cd00b88f723a4b68e50f1ab63bb226336874
Fabio Estevam <fabio.estevam@freescale.com>
2014-10-17 17:14:01 -0300

    mtd: fsl-quadspi: Let the clocks error path be clearer
    When clk_prepare_enable(q->clk) fails it is clearer to disable the previous
    acquired clock (q->clk_en) in the error path rather than doing it locally.
    
    So disable q->clk_en in the error path only.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Huang Shijie <shijie8@gmail.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/spi-nor/fsl-quadspi.c
+++ drivers/mtd/spi-nor/fsl-quadspi.c
@@ -850,9 +850,8 @@ static int fsl_qspi_probe(struct platform_device *pdev)
 
 	ret = clk_prepare_enable(q->clk);
 	if (ret) {
-		clk_disable_unprepare(q->clk_en);
 		dev_err(dev, "can not enable the qspi clock\n");
-		goto map_failed;
+		goto clk_failed;
 	}
 
 	/* find the irq */
@@ -966,6 +965,7 @@ last_init_failed:
 
 irq_failed:
 	clk_disable_unprepare(q->clk);
+clk_failed:
 	clk_disable_unprepare(q->clk_en);
 map_failed:
 	dev_err(dev, "Freescale QuadSPI probe failed\n");
