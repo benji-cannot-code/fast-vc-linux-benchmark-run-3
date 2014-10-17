2751793ff50559600ccb3b5b6388247b5aff1811
Sujith Manoharan <c_manoha@qca.qualcomm.com>
2014-10-17 07:40:28 +0530

    ath9k: Fix HW scan abort
    Instead of using ATH_CHANCTX_EVENT_ASSIGN to abort
    a HW scan when a new interface becomes active, use the
    mgd_prepare_tx() callback. This allows us to make
    sure that the GO's channel becomes operational by
    using flush_work().
    
    Signed-off-by: Sujith Manoharan <c_manoha@qca.qualcomm.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -2366,7 +2366,6 @@ static int ath9k_add_chanctx(struct ieee80211_hw *hw,
 			conf->def.chan->center_freq);
 
 		ath_chanctx_set_channel(sc, ctx, &conf->def);
-		ath_chanctx_event(sc, NULL, ATH_CHANCTX_EVENT_ASSIGN);
 
 		mutex_unlock(&sc->mutex);
 		return 0;
@@ -2496,6 +2495,19 @@ static void ath9k_mgd_prepare_tx(struct ieee80211_hw *hw,
 
 	if (!changed)
 		goto out;
+
+	if (test_bit(ATH_OP_SCANNING, &common->op_flags)) {
+		ath_dbg(common, CHAN_CTX,
+			"%s: Aborting HW scan\n", __func__);
+
+		mutex_unlock(&sc->mutex);
+
+		del_timer_sync(&sc->offchannel.timer);
+		ath_scan_complete(sc, true);
+		flush_work(&sc->chanctx_work);
+
+		mutex_lock(&sc->mutex);
+	}
 
 	go_ctx = ath_is_go_chanctx_present(sc);
 
