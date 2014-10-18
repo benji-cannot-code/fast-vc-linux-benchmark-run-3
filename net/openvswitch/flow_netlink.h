a565a45b9c9f2c3b1ec7ed5444fd36fc0f2bdf99
Joe Stringer <joestringer@nicira.com>
2014-10-18 16:14:14 -0700

    openvswitch: Move key_attr_size() to flow_netlink.h.
    flow-netlink has netlink related code.
    
    Signed-off-by: Joe Stringer <joestringer@nicira.com>
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>

--- net/openvswitch/flow_netlink.h
+++ net/openvswitch/flow_netlink.h
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "flow.h"
 
+size_t ovs_key_attr_size(void);
+
 void ovs_match_init(struct sw_flow_match *match,
 		    struct sw_flow_key *key, struct sw_flow_mask *mask);
 
