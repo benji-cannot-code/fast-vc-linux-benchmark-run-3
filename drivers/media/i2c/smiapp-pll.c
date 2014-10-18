66ef6e130eed2bc390ea42f68d777ffd7bae94b0
Sakari Ailus <sakari.ailus@iki.fi>
2014-10-18 06:16:20 -0300

    [media] smiapp-pll: include linux/device.h in smiapp-pll.c, not in smiapp-pll.h
    struct device has a forward declaration in the header already. The header is
    only needed in the .c file.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/i2c/smiapp-pll.c
+++ drivers/media/i2c/smiapp-pll.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * General Public License for more details.
  */
 
+#include <linux/device.h>
 #include <linux/gcd.h>
 #include <linux/lcm.h>
 #include <linux/module.h>
--- drivers/media/i2c/smiapp-pll.h
+++ drivers/media/i2c/smiapp-pll.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef SMIAPP_PLL_H
 #define SMIAPP_PLL_H
 
-#include <linux/device.h>
-
 /* CSI-2 or CCP-2 */
 #define SMIAPP_PLL_BUS_TYPE_CSI2				0x00
 #define SMIAPP_PLL_BUS_TYPE_PARALLEL				0x01
