6305cf4d046889a5824d6a64a305ffa8d981bbe9
Sebastian Schmidt <yath@yath.de>
2014-10-19 20:05:15 +0200

    pstore: Honor dmesg_restrict sysctl on dmesg dumps
    When the kernel.dmesg_restrict restriction is in place, only users with
    CAP_SYSLOG should be able to access crash dumps (like: attacker is
    trying to exploit a bug, watchdog reboots, attacker can happily read
    crash dumps and logs).
    
    This puts the restriction on console-* types as well as sensitive
    information could have been leaked there.
    
    Other log types are unaffected.
    
    Signed-off-by: Sebastian Schmidt <yath@yath.de>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/linux/syslog.h
+++ include/linux/syslog.h
@@ -49,5 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SYSLOG_FROM_PROC             1
 
 int do_syslog(int type, char __user *buf, int count, bool from_file);
+int check_syslog_permissions(int type, bool from_file);
 
 #endif /* _LINUX_SYSLOG_H */
