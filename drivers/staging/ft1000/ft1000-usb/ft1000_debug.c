3e6064746cc77500de1c87f96886d3aa05a7e837
Chen Weixiang <weixiangchen@gmail.com>
2014-10-19 20:59:44 +0800

    staging: ft1000: do not initialise statics to 0 or NULL
    Remove following checkpatch.pl error from ft1000/ft1000-usb/ft1000_debug.c
    ERROR: do not initialise statics to 0 or NULL
    
    Signed-off-by: Chen Weixiang <weixiang.chen@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ft1000/ft1000-usb/ft1000_debug.c
+++ drivers/staging/ft1000/ft1000-usb/ft1000_debug.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include "ft1000_usb.h"
 
-static int ft1000_flarion_cnt = 0;
+static int ft1000_flarion_cnt;
 
 static int ft1000_open(struct inode *inode, struct file *file);
 static unsigned int ft1000_poll_dev(struct file *file, poll_table *wait);
