0bb4c373474b26a448109d5541104fb16eae5e4a
Wolfram Sang <wsa@the-dreams.de>
2014-10-20 16:20:35 +0200

    gpu: drm: sti: drop owner assignment from platform_drivers
    A platform_driver does not need to set an owner, it will be populated by the
    driver core.
    
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/gpu/drm/sti/sti_drm_drv.c
+++ drivers/gpu/drm/sti/sti_drm_drv.c
@@ -185,7 +185,6 @@ static struct platform_driver sti_drm_master_driver = {
 	.probe = sti_drm_master_probe,
 	.remove = sti_drm_master_remove,
 	.driver = {
-		.owner = THIS_MODULE,
 		.name = DRIVER_NAME "__master",
 	},
 };
@@ -229,7 +228,6 @@ static struct platform_driver sti_drm_platform_driver = {
 	.probe = sti_drm_platform_probe,
 	.remove = sti_drm_platform_remove,
 	.driver = {
-		.owner = THIS_MODULE,
 		.name = DRIVER_NAME,
 		.of_match_table = sti_drm_dt_ids,
 	},
