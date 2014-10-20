25d59d87510001ea23ba26c1e9ebb6e2c625751a
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2014-10-20 14:34:46 +0200

    mfd: max77693: Map charger device to its own of_node
    Add a "maxim,max77693-charger" of_compatible to the mfd_cell so the MFD
    child device (the charger) will have its own of_node set. This will be
    used by the max77693 charger driver in next patches to obtain battery
    configuration from DTS.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/max77693.c
+++ drivers/mfd/max77693.c
@@ -44,7 +44,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static const struct mfd_cell max77693_devs[] = {
 	{ .name = "max77693-pmic", },
-	{ .name = "max77693-charger", },
+	{
+		.name = "max77693-charger",
+		.of_compatible = "maxim,max77693-charger",
+	},
 	{ .name = "max77693-muic", },
 	{
 		.name = "max77693-haptic",
