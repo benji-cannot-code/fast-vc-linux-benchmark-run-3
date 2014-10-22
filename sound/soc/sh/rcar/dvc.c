a0d4d206da420df2b714c284f9249e5307ab89b3
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2014-10-21 18:13:56 -0700

    ASoC: rsnd: tidyup RSND_DVC_VOLUME_NUM to RSND_DVC_CHANNELS
    RSND_DVC_VOLUME_NUM means DVC channel number.
    This patch tidyups this un-understandable naming
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rcar/dvc.c
+++ sound/soc/sh/rcar/dvc.c
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define RSND_DVC_NAME_SIZE	16
 #define RSND_DVC_VOLUME_MAX	100
-#define RSND_DVC_VOLUME_NUM	2
+#define RSND_DVC_CHANNELS	2
 
 #define DVC_NAME "dvc"
 
@@ -21,8 +21,8 @@ struct rsnd_dvc {
 	struct rsnd_dvc_platform_info *info; /* rcar_snd.h */
 	struct rsnd_mod mod;
 	struct clk *clk;
-	u8 volume[RSND_DVC_VOLUME_NUM];
-	u8 mute[RSND_DVC_VOLUME_NUM];
+	u8 volume[RSND_DVC_CHANNELS];
+	u8 mute[RSND_DVC_CHANNELS];
 };
 
 #define rsnd_mod_to_dvc(_mod)	\
@@ -38,11 +38,11 @@ static void rsnd_dvc_volume_update(struct rsnd_mod *mod)
 {
 	struct rsnd_dvc *dvc = rsnd_mod_to_dvc(mod);
 	u32 max = (0x00800000 - 1);
-	u32 vol[RSND_DVC_VOLUME_NUM];
+	u32 vol[RSND_DVC_CHANNELS];
 	u32 mute = 0;
 	int i;
 
-	for (i = 0; i < RSND_DVC_VOLUME_NUM; i++) {
+	for (i = 0; i < RSND_DVC_CHANNELS; i++) {
 		vol[i] = max / RSND_DVC_VOLUME_MAX * dvc->volume[i];
 		mute |= (!!dvc->mute[i]) << i;
 	}
@@ -151,7 +151,7 @@ static int rsnd_dvc_volume_info(struct snd_kcontrol *kctrl,
 	struct rsnd_dvc *dvc = rsnd_mod_to_dvc(mod);
 	u8 *val = (u8 *)kctrl->private_value;
 
-	uinfo->count = RSND_DVC_VOLUME_NUM;
+	uinfo->count = RSND_DVC_CHANNELS;
 	uinfo->value.integer.min = 0;
 
 	if (val == dvc->volume) {
@@ -171,7 +171,7 @@ static int rsnd_dvc_volume_get(struct snd_kcontrol *kctrl,
 	u8 *val = (u8 *)kctrl->private_value;
 	int i;
 
-	for (i = 0; i < RSND_DVC_VOLUME_NUM; i++)
+	for (i = 0; i < RSND_DVC_CHANNELS; i++)
 		ucontrol->value.integer.value[i] = val[i];
 
 	return 0;
@@ -184,7 +184,7 @@ static int rsnd_dvc_volume_put(struct snd_kcontrol *kctrl,
 	u8 *val = (u8 *)kctrl->private_value;
 	int i, change = 0;
 
-	for (i = 0; i < RSND_DVC_VOLUME_NUM; i++) {
+	for (i = 0; i < RSND_DVC_CHANNELS; i++) {
 		change |= (ucontrol->value.integer.value[i] != val[i]);
 		val[i] = ucontrol->value.integer.value[i];
 	}
