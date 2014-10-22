3f1586a6474cd65f201480411421b192938ca8d2
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-10-22 14:36:49 -0700

    staging: comedi: comedi_buf: don't export comedi_buf_memcpy_to()
    This function is only used internally. Remove the export.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_buf.c
+++ drivers/staging/comedi/comedi_buf.c
@@ -443,8 +443,9 @@ int comedi_buf_put(struct comedi_subdevice *s, unsigned short x)
 }
 EXPORT_SYMBOL_GPL(comedi_buf_put);
 
-void comedi_buf_memcpy_to(struct comedi_subdevice *s, unsigned int offset,
-			  const void *data, unsigned int num_bytes)
+static void comedi_buf_memcpy_to(struct comedi_subdevice *s,
+				 unsigned int offset,
+				 const void *data, unsigned int num_bytes)
 {
 	struct comedi_async *async = s->async;
 	unsigned int write_ptr = async->buf_write_ptr + offset;
@@ -468,7 +469,6 @@ void comedi_buf_memcpy_to(struct comedi_subdevice *s, unsigned int offset,
 		write_ptr = 0;
 	}
 }
-EXPORT_SYMBOL_GPL(comedi_buf_memcpy_to);
 
 static void comedi_buf_memcpy_from(struct comedi_subdevice *s,
 				   unsigned int offset,
