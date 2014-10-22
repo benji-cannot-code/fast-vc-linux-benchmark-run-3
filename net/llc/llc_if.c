7caf4605480b4adb08bb5e43a20475b2a22015fa
Fabian Frederick <fabf@skynet.be>
2014-10-22 21:06:26 +0200

    net: llc: include linux/errno.h instead of asm/errno.h
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/llc/llc_if.c
+++ net/llc/llc_if.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
-#include <asm/errno.h>
+#include <linux/errno.h>
 #include <net/llc_if.h>
 #include <net/llc_sap.h>
 #include <net/llc_s_ev.h>
