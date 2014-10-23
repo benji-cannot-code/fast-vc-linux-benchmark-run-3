074b3105e74aa7a279750b3f5b2962c82eb6be4c
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:29:55 -0400

    staging: unisys: remove typedef for CHANNEL_HEADER
    Switch everywhere that CHANNEL_HEADER, pCHANNEL_HEADER, or
    ULTRA_CHANNEL_PROTOCOL was used, to struct channel_header.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/uisqueue.h
+++ drivers/staging/unisys/include/uisqueue.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "controlvmcompletionstatus.h"
 
 struct uisqueue_info {
-	CHANNEL_HEADER __iomem *chan;
+	struct channel_header __iomem *chan;
 	/* channel containing queues in which scsi commands &
 	 * responses are queued
 	 */
