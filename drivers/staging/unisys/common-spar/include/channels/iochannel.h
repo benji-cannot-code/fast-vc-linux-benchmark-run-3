07c26eb50d446d2786d10c18f60a22a61661d117
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:30:55 -0400

    staging: unisys: remove unused macro SPAR_VSWITCH_CHANNEL_OK_CLIENT
    Nobody is using this one either...
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/iochannel.h
+++ drivers/staging/unisys/common-spar/include/channels/iochannel.h
@@ -72,12 +72,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_VERSIONID, \
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_SIGNATURE))
 
-#define SPAR_VSWITCH_CHANNEL_OK_CLIENT(pChannel, logCtx)		\
-	(spar_check_channel_client(pChannel, UltraVswitchChannelProtocolGuid, \
-				   "vswitch", MIN_IO_CHANNEL_SIZE,	\
-				   ULTRA_VSWITCH_CHANNEL_PROTOCOL_VERSIONID, \
-				   ULTRA_VSWITCH_CHANNEL_PROTOCOL_SIGNATURE))
-
 /*
 * Everything necessary to handle SCSI & NIC traffic between Guest Partition and
 * IO Partition is defined below.  */
