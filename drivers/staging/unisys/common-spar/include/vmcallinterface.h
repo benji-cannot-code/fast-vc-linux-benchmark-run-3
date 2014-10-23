c7ce99b4948bf862e5dc77b7f7457851a3a39703
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:29:46 -0400

    staging: unisys: get rid of IO_DATA_STRUCTURE typedef
    Nobody was using this, so just remove it.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/vmcallinterface.h
+++ drivers/staging/unisys/common-spar/include/vmcallinterface.h
@@ -108,7 +108,6 @@ struct phys_info {
 
 #pragma pack(pop)
 /* ///////////// END PRAGMA PACK PUSH 1 /////////////////////////// */
-typedef struct phys_info IO_DATA_STRUCTURE;
 
 /* ///////////// BEGIN PRAGMA PACK PUSH 1 ///////////////////////// */
 /* ///////////// ONLY STRUCT TYPE SHOULD BE BELOW */
