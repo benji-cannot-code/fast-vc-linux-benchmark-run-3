0fdab25e1c360ae9d80397cb05d0808270891e01
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:30:11 -0400

    staging: unisys: refactor ULTRA_check_channel_server()
    Rename spar_check_channel_server() to spar_check_channel_server(),
    remove unused parameters fileName, lineNumber, and logCtx, update
    references to the function and remove unused parameters in macros that
    call it, and fix CamelCase names:
    
    typeGuid => typeuuid
    channelName => name
    expectedMinBytes => expected_min_bytes
    actualBytes => actual_bytes
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/iochannel.h
+++ drivers/staging/unisys/common-spar/include/channels/iochannel.h
@@ -66,17 +66,26 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				   "vhba", MIN_IO_CHANNEL_SIZE,	\
 				   ULTRA_VHBA_CHANNEL_PROTOCOL_VERSIONID, \
 				   ULTRA_VHBA_CHANNEL_PROTOCOL_SIGNATURE))
+#define ULTRA_VHBA_CHANNEL_OK_SERVER(actualBytes)		\
+	(spar_check_channel_server(spar_vhba_channel_protocol_uuid,	\
+				   "vhba", MIN_IO_CHANNEL_SIZE, actualBytes))
 #define SPAR_VNIC_CHANNEL_OK_CLIENT(pChannel, logCtx)			\
 	(spar_check_channel_client(pChannel, spar_vnic_channel_protocol_uuid, \
 				   "vnic", MIN_IO_CHANNEL_SIZE,	\
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_VERSIONID, \
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_SIGNATURE))
+#define ULTRA_VNIC_CHANNEL_OK_SERVER(actualBytes)		\
+	(spar_check_channel_server(spar_vnic_channel_protocol_uuid,	\
+				   "vnic", MIN_IO_CHANNEL_SIZE, actualBytes))
 #define SPAR_VSWITCH_CHANNEL_OK_CLIENT(pChannel, logCtx)		\
 	(spar_check_channel_client(pChannel, UltraVswitchChannelProtocolGuid, \
 				   "vswitch", MIN_IO_CHANNEL_SIZE,	\
 				   ULTRA_VSWITCH_CHANNEL_PROTOCOL_VERSIONID, \
 				   ULTRA_VSWITCH_CHANNEL_PROTOCOL_SIGNATURE))
-
+#define ULTRA_VSWITCH_CHANNEL_OK_SERVER(actualBytes)          \
+	(spar_check_channel_server(UltraVswitchChannelProtocolGuid,	\
+				    "vswitch", MIN_IO_CHANNEL_SIZE,	\
+				    actualBytes))
 /*
 * Everything necessary to handle SCSI & NIC traffic between Guest Partition and
 * IO Partition is defined below.  */
