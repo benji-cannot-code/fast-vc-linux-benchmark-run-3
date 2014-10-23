f2447ccc1b0014b35d6eb32d5836121b60e76349
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:30:54 -0400

    staging: unisys: fix parameters to macro SPAR_VNIC_CHANNEL_OK_CLIENT
    Remove the unused logCtx parameter and fix the CamelCase name:
    
    pChannel => ch
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/iochannel.h
+++ drivers/staging/unisys/common-spar/include/channels/iochannel.h
@@ -66,8 +66,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				   ULTRA_VHBA_CHANNEL_PROTOCOL_VERSIONID, \
 				   ULTRA_VHBA_CHANNEL_PROTOCOL_SIGNATURE))
 
-#define SPAR_VNIC_CHANNEL_OK_CLIENT(pChannel, logCtx)			\
-	(spar_check_channel_client(pChannel, spar_vnic_channel_protocol_uuid, \
+#define SPAR_VNIC_CHANNEL_OK_CLIENT(ch)			\
+	(spar_check_channel_client(ch, spar_vnic_channel_protocol_uuid, \
 				   "vnic", MIN_IO_CHANNEL_SIZE,	\
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_VERSIONID, \
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_SIGNATURE))
