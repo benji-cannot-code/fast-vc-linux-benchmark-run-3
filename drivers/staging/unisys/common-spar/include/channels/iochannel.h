4d73085576685a6c802ee7c7bed6b160ec57233e
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:30:52 -0400

    staging: unisys: remove unused macro ULTRA_VNIC_CHANNEL_OK_SERVER
    This macro isn't being used either, so delete it.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/iochannel.h
+++ drivers/staging/unisys/common-spar/include/channels/iochannel.h
@@ -71,9 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				   "vnic", MIN_IO_CHANNEL_SIZE,	\
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_VERSIONID, \
 				   ULTRA_VNIC_CHANNEL_PROTOCOL_SIGNATURE))
-#define ULTRA_VNIC_CHANNEL_OK_SERVER(actualBytes)		\
-	(spar_check_channel_server(spar_vnic_channel_protocol_uuid,	\
-				   "vnic", MIN_IO_CHANNEL_SIZE, actualBytes))
+
 #define SPAR_VSWITCH_CHANNEL_OK_CLIENT(pChannel, logCtx)		\
 	(spar_check_channel_client(pChannel, UltraVswitchChannelProtocolGuid, \
 				   "vswitch", MIN_IO_CHANNEL_SIZE,	\
