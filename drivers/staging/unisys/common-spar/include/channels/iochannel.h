b731e7fdc2aa7620cfb5643456c42111582856d9
Benjamin Romer <benjamin.romer@unisys.com>
2014-10-23 14:30:58 -0400

    staging: unisys: refactor TASK_MGMT_TYPES
    Get rid of the typedef and use enum task_mgmt_types in its place. Clean up
    the indentation of the enumeration values and update all use of the name to
    the new name.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/iochannel.h
+++ drivers/staging/unisys/common-spar/include/channels/iochannel.h
@@ -197,10 +197,12 @@ enum net_types {
 #define NIC_VENDOR_ID 0x0008000B
 
 /* various types of scsi task mgmt commands  */
-typedef enum { TASK_MGMT_ABORT_TASK =
-	    1, TASK_MGMT_BUS_RESET, TASK_MGMT_LUN_RESET,
-	    TASK_MGMT_TARGET_RESET,
-} TASK_MGMT_TYPES;
+enum task_mgmt_types {
+	TASK_MGMT_ABORT_TASK = 1,
+	TASK_MGMT_BUS_RESET,
+	TASK_MGMT_LUN_RESET,
+	TASK_MGMT_TARGET_RESET,
+};
 
 /* various types of vdisk mgmt commands  */
 typedef enum { VDISK_MGMT_ACQUIRE = 1, VDISK_MGMT_RELEASE,
@@ -552,7 +554,7 @@ struct uiscmdrsp_net {
 };
 
 struct uiscmdrsp_scsitaskmgmt {
-	TASK_MGMT_TYPES tasktype;
+	enum task_mgmt_types tasktype;
 
 	    /* the type of task */
 	struct uisscsi_dest vdest;
