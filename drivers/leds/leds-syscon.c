718664921528207d2f421eff497381a0d6d73cc2
Linus Walleij <linus.walleij@linaro.org>
2014-10-24 03:57:06 -0700

    leds: delete copy/paste mistake
    A chunk of text followed the copied license text, ehm, sorry
    that kind of things happen from time to time.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Bryan Wu <cooloney@gmail.com>

--- drivers/leds/leds-syscon.c
+++ drivers/leds/leds-syscon.c
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
- *
- * This driver provides system reboot functionality for APM X-Gene SoC.
- * For system shutdown, this is board specify. If a board designer
- * implements GPIO shutdown, use the gpio-poweroff.c driver.
  */
 #include <linux/io.h>
 #include <linux/of_device.h>
