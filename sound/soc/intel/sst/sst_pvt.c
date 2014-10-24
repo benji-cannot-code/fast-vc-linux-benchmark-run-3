45e8aeb772193644c6c8bce27f2b8a8183178855
Subhransu S. Prusty <subhransu.s.prusty@intel.com>
2014-10-24 13:49:46 +0530

    ASoC: Intel: mrfld: Replace pci_id with unique device id
    In order to support both ACPI and PCI devices we need to use a genric device
    id in driver, so change all pci_id instances to device_id
    
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/sst/sst_pvt.c
+++ sound/soc/intel/sst/sst_pvt.c
@@ -116,7 +116,7 @@ unsigned long long read_shim_data(struct intel_sst_drv *sst, int addr)
 {
 	unsigned long long val = 0;
 
-	switch (sst->pci_id) {
+	switch (sst->dev_id) {
 	case SST_MRFLD_PCI_ID:
 		val = sst_shim_read64(sst->shim, addr);
 		break;
@@ -127,7 +127,7 @@ unsigned long long read_shim_data(struct intel_sst_drv *sst, int addr)
 void write_shim_data(struct intel_sst_drv *sst, int addr,
 				unsigned long long data)
 {
-	switch (sst->pci_id) {
+	switch (sst->dev_id) {
 	case SST_MRFLD_PCI_ID:
 		sst_shim_write64(sst->shim, addr, (u64) data);
 		break;
