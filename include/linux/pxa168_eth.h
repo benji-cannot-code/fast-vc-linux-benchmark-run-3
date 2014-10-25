16a8bd05f2ac7886ebe7ada96937d11ea0d4e7e7
Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
2014-10-25 12:08:59 +0200

    net: pxa168_eth: Fix providing of phy_interface mode on platform_data
    Do not add phy include to the board file but platform_data include
    instead.
    
    Signed-off-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/pxa168_eth.h
+++ include/linux/pxa168_eth.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_PXA168_ETH_H
 #define __LINUX_PXA168_ETH_H
 
+#include <linux/phy.h>
+
 struct pxa168_eth_platform_data {
 	int	port_number;
 	int	phy_addr;
