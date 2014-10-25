f3c4499acbb0951d3e58c48743bcf141acc51a80
Aya Mahfouz <mahfouz.saif.elyazal@gmail.com>
2014-10-25 23:43:33 +0200

    staging: dgnc: remove unused variable named rawreadok
    This patch removes the variable rawreadok since it is only intialized
    but not used in any of the driver functions.
    
    Signed-off-by: Aya Mahfouz <mahfouz.saif.elyazal@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.c
+++ drivers/staging/dgnc/dgnc_driver.c
@@ -57,7 +57,6 @@ MODULE_SUPPORTED_DEVICE("dgnc");
  * us to specify the variable type, name, initial value, and description.
  */
 PARM_INT(debug,		0x00,		0644,	"Driver debugging level");
-PARM_INT(rawreadok,	1,		0644,	"Bypass flip buffers on input");
 
 /**************************************************************************
  *
@@ -742,7 +741,6 @@ static void dgnc_init_globals(void)
 {
 	int i = 0;
 
-	dgnc_rawreadok		= rawreadok;
 	dgnc_debug		= debug;
 	dgnc_NumBoards		= 0;
 
