7b6a7c2c8c8d354e29b3b69b279aef8cbf61c708
Aya Mahfouz <mahfouz.saif.elyazal@gmail.com>
2014-10-25 23:47:57 +0200

    staging: dgnc: dgnc_kcompat.h: remove unused macro
    This patch removes the macro PARM_INT after removing all the variables
    that depended on it.
    
    Signed-off-by: Aya Mahfouz <mahfouz.saif.elyazal@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_kcompat.h
+++ drivers/staging/dgnc/dgnc_kcompat.h
@@ -43,10 +43,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  define __chk_user_ptr(x) (void)0
 # endif
 
-#  define PARM_INT(VAR, INIT, PERM, DESC) \
-		static int VAR = INIT; \
-		int dgnc_##VAR; \
-		module_param(VAR, int, PERM); \
-		MODULE_PARM_DESC(VAR, DESC);
 
 #endif /* ! __DGNC_KCOMPAT_H */
