4f3199f3a5ceb564bec2e0b8d5126ef3026a6a28
Roberta Dobrescu <roberta.dobrescu@gmail.com>
2014-10-26 23:30:09 +0200

    staging: rtl8723au: core: Remove uneeded return variable
    This patch removes uneeded return variables, using only
    'return _SUCCESS' instead.
    It fixes the following warning detected by coccinelle:
    Unneeded variable.
    
    It was done using the following semantic patch:
    
    @@
    identifier ret;
    type T;
    expression e;
    @@
    
    -T ret = e;
     ... when != ret
         when strict
    -return ret;
    +return e;
    
    Signed-off-by: Roberta Dobrescu <roberta.dobrescu@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_recv.c
+++ drivers/staging/rtl8723au/core/rtw_recv.c
@@ -1555,8 +1555,6 @@ static int wlanhdr_to_ethhdr (struct recv_frame *precvframe)
 	u16	eth_type, len, hdrlen;
 	u8	bsnaphdr;
 	u8	*psnap;
-
-	int ret = _SUCCESS;
 	struct rtw_adapter *adapter = precvframe->adapter;
 	struct mlme_priv *pmlmepriv = &adapter->mlmepriv;
 
@@ -1617,7 +1615,7 @@ static int wlanhdr_to_ethhdr (struct recv_frame *precvframe)
 	}
 
 
-	return ret;
+	return _SUCCESS;
 }
 
 /* perform defrag */
