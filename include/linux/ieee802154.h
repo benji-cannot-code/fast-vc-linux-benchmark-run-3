841d40c9bb5c2531244d0aaa503866b063648b3c
Alexander Aring <alex.aring@gmail.com>
2014-10-27 17:13:40 +0100

    ieee802154: add valid psdu length helper
    This patch adds a generic valid psdu length check function helper. This
    is useful to check the length field after receiving. For example the
    at86rf231 doesn't filter invalid psdu length. Sometimes the CRC can also
    be correct. If we get the lqi value with an invalid frame length the
    kernel may crash because we dereference an invalid pointer in the
    receive buffer.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- include/linux/ieee802154.h
+++ include/linux/ieee802154.h
@@ -24,7 +24,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef LINUX_IEEE802154_H
 #define LINUX_IEEE802154_H
 
+#include <linux/types.h>
+
 #define IEEE802154_MTU			127
+#define IEEE802154_MIN_PSDU_LEN		5
 
 #define IEEE802154_FC_TYPE_BEACON	0x0	/* Frame is beacon */
 #define	IEEE802154_FC_TYPE_DATA		0x1	/* Frame is data */
@@ -186,5 +189,13 @@ enum {
 	IEEE802154_SCAN_IN_PROGRESS = 0xfc,
 };
 
+/**
+ * ieee802154_is_valid_psdu_len - check if psdu len is valid
+ * @len: psdu len with (MHR + payload + MFR)
+ */
+static inline bool ieee802154_is_valid_psdu_len(const u8 len)
+{
+	return (len >= IEEE802154_MIN_PSDU_LEN && len <= IEEE802154_MTU);
+}
 
 #endif /* LINUX_IEEE802154_H */
