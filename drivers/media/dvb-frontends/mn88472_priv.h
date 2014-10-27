6c058316cdcef4247648f484e1e866442d773637
Antti Palosaari <crope@iki.fi>
2014-10-26 23:26:04 -0300

    [media] mn88472: Convert driver to I2C RegMap API
    Convert driver to I2C RegMap API.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/mn88472_priv.h
+++ drivers/media/dvb-frontends/mn88472_priv.h
@@ -21,11 +21,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_frontend.h"
 #include "mn88472.h"
 #include <linux/firmware.h>
+#include <linux/regmap.h>
 
 #define MN88472_FIRMWARE "dvb-demod-mn88472-02.fw"
 
 struct mn88472_dev {
 	struct i2c_client *client[3];
+	struct regmap *regmap[3];
 	struct dvb_frontend fe;
 	u16 i2c_wr_max;
 	fe_delivery_system_t delivery_system;
