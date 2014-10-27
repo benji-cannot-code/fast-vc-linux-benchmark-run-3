1708bd2487c124abaddc4a16690c0df17c9b46d3
Alexander Aring <alex.aring@gmail.com>
2014-10-27 17:13:36 +0100

    mac802154: rx: add CHECKSUM_UNNECESSARY
    This patch adds CHECKSUM_UNNECESSARY to skb->ip_summed before delivery.
    There exist no transceiver with IP checksum functionality.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/mac802154/rx.c
+++ net/mac802154/rx.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int mac802154_process_data(struct net_device *dev, struct sk_buff *skb)
 {
+	skb->ip_summed = CHECKSUM_UNNECESSARY;
 	skb->protocol = htons(ETH_P_IEEE802154);
 
 	return netif_receive_skb(skb);
@@ -227,6 +228,7 @@ mac802154_monitors_rx(struct ieee802154_local *local, struct sk_buff *skb)
 	u16 crc = crc_ccitt(0, skb->data, skb->len);
 	u8 *data;
 
+	skb->ip_summed = CHECKSUM_UNNECESSARY;
 	skb->protocol = htons(ETH_P_IEEE802154);
 
 	rcu_read_lock();
