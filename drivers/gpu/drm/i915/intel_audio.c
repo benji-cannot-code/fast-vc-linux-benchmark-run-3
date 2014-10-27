59368ea14492fffa4203d52358210bb4a835035c
Jani Nikula <jani.nikula@intel.com>
2014-10-27 16:26:44 +0200

    drm/i915/audio: constify hdmi audio clock struct
    Const is good.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_audio.c
+++ drivers/gpu/drm/i915/intel_audio.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "intel_drv.h"
 #include "i915_drv.h"
 
-static struct {
+static const struct {
 	int clock;
 	u32 config;
 } hdmi_audio_clock[] = {
