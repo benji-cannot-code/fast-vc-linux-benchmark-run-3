922cc6aa62abb0e410ce2fc1ce5ddaf0c9b214af
Tero Kristo <t-kristo@ti.com>
2014-10-27 08:39:25 -0700

    ARM: OMAP4+: CM: remove omap4_cm1/cm2_* functions
    These are not used for anything, so remove both the implementations and
    header file references.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Tested-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/cm1_7xx.h
+++ arch/arm/mach-omap2/cm1_7xx.h
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ARCH_ARM_MACH_OMAP2_CM1_7XX_H
 #define __ARCH_ARM_MACH_OMAP2_CM1_7XX_H
 
-#include "cm_44xx_54xx.h"
-
 /* CM1 base address */
 #define DRA7XX_CM_CORE_AON_BASE		0x4a005000
 
