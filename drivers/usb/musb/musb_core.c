1c2eca7aa905377ef5e7e7f24b3099c3412554f2
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2014-10-27 19:06:18 +0100

    usb: musb: core: check link status on resume
    The am335x-evmsk support two kinds of suspend:
    - standby
      the USB device remains powered while the system goes into suspend
    
    - mem
      the USB device becomes powerless while the system goes into suspend.
    
    In the "standby" case the device resumes quickly. In the "mem" case the
    system hangs for a few seconds. It seems to me that the USB-device has
    no address (it was disconnected) and the USB stack thinks that it is
    fully operational and GetPortStatus returns the status from before the
    suspend so it is not a big help here.
    
    This adds a check in the resume path to see if the device mode (A or B)
    and the speed is the same. If the device went missing between
    suspend/resume (VBUS went down) then MUSB seems to go into B mode and
    HS/FS bits are cleared. In that case we clear the port1_status bits and
    assume a disconnect. Once the stack learns this it does a "logical
    disconnect" and removes the USB-device quickly. Should the device remain
    connected during the suspend then MUSB will receives a "CONNECT" interrupt.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2301,6 +2301,8 @@ static int musb_suspend(struct device *dev)
 static int musb_resume(struct device *dev)
 {
 	struct musb	*musb = dev_to_musb(dev);
+	u8		devctl;
+	u8		mask;
 
 	/*
 	 * For static cmos like DaVinci, register values were preserved
@@ -2314,6 +2316,10 @@ static int musb_resume(struct device *dev)
 
 	musb_restore_context(musb);
 
+	devctl = musb_readb(musb->mregs, MUSB_DEVCTL);
+	mask = MUSB_DEVCTL_BDEVICE | MUSB_DEVCTL_FSDEV | MUSB_DEVCTL_LSDEV;
+	if ((devctl & mask) != (musb->context.devctl & mask))
+		musb->port1_status = 0;
 	return 0;
 }
 
