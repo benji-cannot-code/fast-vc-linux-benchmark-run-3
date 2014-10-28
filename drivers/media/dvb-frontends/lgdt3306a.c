dfa4b209b27c1764bbc5250a7d9591b8449eedf2
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2014-10-28 11:27:34 -0200

    [media] lgdt3306a: Remove FSF address
    Fix this CodingStyle error:
    
    	ERROR: Do not include the paragraph about writing to the Free Software Foundation's mailing address from the sample GPL notice. The FSF has changed addresses in the past, and may do so ag$
    	#56: FILE: drivers/media/dvb-frontends/lgdt3306a.c:19:
    	+ *    along with this program; if not, write to the Free Software$
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/lgdt3306a.c
+++ drivers/media/dvb-frontends/lgdt3306a.c
@@ -15,11 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
  */
 
 #include <asm/div64.h>
@@ -1456,6 +1451,7 @@ static u32 lgdt3306a_calculate_snr_x100(struct lgdt3306a_state *state)
 
 static enum lgdt3306a_lock_status lgdt3306a_vsb_lock_poll(struct lgdt3306a_state *state)
 {
+	int ret;
 	u8 cnt = 0;
 	u8 packet_error;
 	u32 snr;
