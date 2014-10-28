6ba5e4671062e237b01b49395a6a64d3202534a2
Arend van Spriel <arend@broadcom.com>
2014-10-28 14:56:08 +0100

    brcmfmac: remove unused defintion
    The define EBRCMF_UNSUPPORTED is not used in the source file so this
    patch removes it.
    
    Reviewed-by: Franky (Zhenhui) Lin <frankyl@broadcom.com>
    Reviewed-by: Hante Meuleman <meuleman@broadcom.com>
    Reviewed-by: Daniel (Deognyoun) Kim <dekim@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/brcm80211/brcmfmac/feature.c
+++ drivers/net/wireless/brcm80211/brcmfmac/feature.c
@@ -25,11 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "feature.h"
 
 /*
- * firmware error code received if iovar is unsupported.
- */
-#define EBRCMF_FEAT_UNSUPPORTED		23
-
-/*
  * expand feature list to array of feature strings.
  */
 #define BRCMF_FEAT_DEF(_f) \
