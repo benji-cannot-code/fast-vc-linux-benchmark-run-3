3b445cc00c0e9d82fe4115cc9fe632f41553a165
Sergey Ryazanov <ryazanov.s.a@gmail.com>
2014-10-29 03:18:40 +0400

    MIPS: ath25: Add basic AR2315 SoC support
    Add basic support for Atheros AR2315+ SoCs: registers definition file
    and initial setup code.
    
    Signed-off-by: Sergey Ryazanov <ryazanov.s.a@gmail.com>
    Cc: Linux MIPS <linux-mips@linux-mips.org>
    Patchwork: https://patchwork.linux-mips.org/patch/8239/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ath25/board.c
+++ arch/mips/ath25/board.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "devices.h"
 #include "ar5312.h"
+#include "ar2315.h"
 
 static void ath25_halt(void)
 {
@@ -33,6 +34,8 @@ void __init plat_mem_setup(void)
 
 	if (is_ar5312())
 		ar5312_plat_mem_setup();
+	else
+		ar2315_plat_mem_setup();
 
 	/* Disable data watchpoints */
 	write_c0_watchlo0(0);
@@ -46,6 +49,8 @@ void __init plat_time_init(void)
 {
 	if (is_ar5312())
 		ar5312_plat_time_init();
+	else
+		ar2315_plat_time_init();
 }
 
 unsigned int __cpuinit get_c0_compare_int(void)
