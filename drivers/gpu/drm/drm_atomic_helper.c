c6d639fc8c3149eaeac5c50d33d6a532fdd2f1e1
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-10-29 11:34:56 +0100

    drm/atomic: Integrate fence support
    This patch is for enabling async commits. It replaces an earlier
    approach which added an async boolean paramter to the ->prepare_fb
    callbacks. The idea is that prepare_fb picks up the right fence to
    synchronize against, which is then used by the synchronous commit
    helper. For async commits drivers can either register a callback to
    the fence or simply do the synchronous wait in their async work queue.
    
    v2: Remove unused variable.
    
    v3: Only wait for fences after the point of no return in the part
    of the commit function which can be run asynchronously. This is after
    the atomic state has been swapped in, hence now check
    plane->state->fence.
    
    Also add a WARN_ON to make sure we don't try to wait on a fence when
    there's no fb, just as a sanity check.
    
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_plane_helper.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_atomic_helper.h>
+#include <linux/fence.h>
 
 static void
 drm_atomic_helper_plane_changed(struct drm_atomic_state *state,
@@ -713,6 +714,26 @@ void drm_atomic_helper_commit_post_planes(struct drm_device *dev,
 }
 EXPORT_SYMBOL(drm_atomic_helper_commit_post_planes);
 
+static void wait_for_fences(struct drm_device *dev,
+			    struct drm_atomic_state *state)
+{
+	int nplanes = dev->mode_config.num_total_plane;
+	int i;
+
+	for (i = 0; i < nplanes; i++) {
+		struct drm_plane *plane = state->planes[i];
+
+		if (!plane || !plane->state->fence)
+			continue;
+
+		WARN_ON(!plane->state->fb);
+
+		fence_wait(plane->state->fence, false);
+		fence_put(plane->state->fence);
+		plane->state->fence = NULL;
+	}
+}
+
 static void
 wait_for_vblanks(struct drm_device *dev, struct drm_atomic_state *old_state)
 {
@@ -809,6 +830,8 @@ int drm_atomic_helper_commit(struct drm_device *dev,
 	 * composition of the next frame right after having submitted the
 	 * current layout.
 	 */
+
+	wait_for_fences(dev, state);
 
 	drm_atomic_helper_commit_pre_planes(dev, state);
 
