eeb227f2be3a730a17153c7be5a032194efbc098
Malcolm Priestley <tvboxspy@gmail.com>
2014-10-29 17:44:03 +0000

    staging: vt6655: s_vGenerateTxParameter remove unused cbMACHdLen
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/rxtx.c
+++ drivers/staging/vt6655/rxtx.c
@@ -1133,7 +1133,6 @@ s_vGenerateTxParameter(
 	unsigned short wCurrentRate
 )
 {
-	unsigned int cbMACHdLen = WLAN_HDR_ADDR3_LEN; //24
 	unsigned short wFifoCtl;
 	bool bDisCRC = false;
 	unsigned char byFBOption = AUTO_FB_NONE;
@@ -1150,9 +1149,6 @@ s_vGenerateTxParameter(
 		byFBOption = AUTO_FB_0;
 	else if (wFifoCtl & FIFOCTL_AUTO_FB_1)
 		byFBOption = AUTO_FB_1;
-
-	if (pDevice->bLongHeader)
-		cbMACHdLen = WLAN_HDR_ADDR3_LEN + 6;
 
 	if (!pvRrvTime)
 		return;
