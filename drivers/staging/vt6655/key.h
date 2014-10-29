7da960c70db06dc5f30bab70164a99a536d8c2d7
Malcolm Priestley <tvboxspy@gmail.com>
2014-10-29 17:55:55 +0000

    staging: vt6655: dead code remove 80211mgr.h
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/key.h
+++ drivers/staging/vt6655/key.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/mac80211.h>
 
 #include "ttype.h"
-#include "80211mgr.h"
 
 /*---------------------  Export Definitions -------------------------*/
 #define MAX_GROUP_KEY       4
