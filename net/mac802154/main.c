40962a721573c0450bdd8d428cf8bcc37d7e1489
Alexander Aring <alex.aring@gmail.com>
2014-10-29 21:34:28 +0100

    mac802154: main: remove unnecessary include
    This patch removes an unnecessary include of driver-ops header file.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/mac802154/main.c
+++ net/mac802154/main.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/cfg802154.h>
 
 #include "ieee802154_i.h"
-#include "driver-ops.h"
 
 static int
 mac802154_netdev_register(struct wpan_phy *phy, struct net_device *dev)
