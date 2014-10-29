63098f2cee79f29bb4331badcbed5654131ab0e6
Malcolm Priestley <tvboxspy@gmail.com>
2014-10-29 17:56:01 +0000

    staging: vt6655: dead code remove 80211hdr.h
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/wmgr.h
+++ drivers/staging/vt6655/wmgr.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __WMGR_H__
 
 #include "ttype.h"
-#include "80211hdr.h"
 #include "card.h"
 
 /*---------------------  Export Definitions -------------------------*/
