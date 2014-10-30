c17d0bfd9c2710d667dc689b56f711d13bcd4564
Tapasweni Pathak <tapaswenipathak@gmail.com>
2014-10-30 17:03:08 +0530

    staging: rtl8188eu: core: Remove useless cast on void pointer
    void pointers do not need to be cast to other pointer types.
    
    The semantic patch used to find this:
    
    @r@
    expression x;
    void* e;
    type T;
    identifier f;
    @@
    
    (
      *((T *)e)
    |
      ((T *)x)[...]
    |
      ((T *)x)->f
    |
    - (T *)
      e
    )
    
    Build tested it.
    
    Signed-off-by: Tapasweni Pathak <tapaswenipathak@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_pwrctrl.c
+++ drivers/staging/rtl8188eu/core/rtw_pwrctrl.c
@@ -285,7 +285,7 @@ exit:
 
 static void pwr_state_check_handler(void *FunctionContext)
 {
-	struct adapter *padapter = (struct adapter *)FunctionContext;
+	struct adapter *padapter = FunctionContext;
 	rtw_ps_cmd(padapter);
 }
 
