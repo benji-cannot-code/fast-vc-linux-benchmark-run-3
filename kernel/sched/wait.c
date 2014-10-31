d3e8c8002e109c6d8218f70236b37ea9aaca33e1
Peter Zijlstra <peterz@infradead.org>
2014-10-31 11:57:30 +0100

    sched/wait: Fix a kthread race with wait_woken()
    There is a race between kthread_stop() and the new wait_woken() that
    can result in a lack of progress.
    
    CPU 0                                    | CPU 1
                                             |
    rfcomm_run()                             | kthread_stop()
      ...                                    |
      if (!test_bit(KTHREAD_SHOULD_STOP))    |
                                             |   set_bit(KTHREAD_SHOULD_STOP)
                                             |   wake_up_process()
        wait_woken()                         |   wait_for_completion()
          set_current_state(INTERRUPTIBLE)   |
          if (!WQ_FLAG_WOKEN)                |
            schedule_timeout()               |
                                             |
    
    After which both tasks will wait.. forever.
    
    Fix this by having wait_woken() check for kthread_should_stop() but
    only for kthreads (obviously).
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Peter Hurley <peter@hurleysoftware.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/wait.c
+++ kernel/sched/wait.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/wait.h>
 #include <linux/hash.h>
+#include <linux/kthread.h>
 
 void __init_waitqueue_head(wait_queue_head_t *q, const char *name, struct lock_class_key *key)
 {
@@ -298,6 +299,10 @@ int autoremove_wake_function(wait_queue_t *wait, unsigned mode, int sync, void *
 }
 EXPORT_SYMBOL(autoremove_wake_function);
 
+static inline bool is_kthread_should_stop(void)
+{
+	return (current->flags & PF_KTHREAD) && kthread_should_stop();
+}
 
 /*
  * DEFINE_WAIT_FUNC(wait, woken_wake_func);
@@ -327,7 +332,7 @@ long wait_woken(wait_queue_t *wait, unsigned mode, long timeout)
 	 * woken_wake_function() such that if we observe WQ_FLAG_WOKEN we must
 	 * also observe all state before the wakeup.
 	 */
-	if (!(wait->flags & WQ_FLAG_WOKEN))
+	if (!(wait->flags & WQ_FLAG_WOKEN) && !is_kthread_should_stop())
 		timeout = schedule_timeout(timeout);
 	__set_current_state(TASK_RUNNING);
 
