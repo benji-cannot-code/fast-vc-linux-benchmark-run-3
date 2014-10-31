aecf8c28efbe1f37ae6bc890b1c2a31a1587b503
Ian Abbott <abbotti@mev.co.uk>
2014-10-31 17:47:39 +0000

    staging: comedi: split out PCI support into new module
    Setting the `CONFIG_COMEDI_PCI_DRIVERS` kernel configuration option
    makes the main "comedi" module depend on the PCI support in the kernel.
    That's not that big a deal since PCI support in the kernel is either
    built into the kernel or is absent, and is not in a separate module.
    Still, not all low-level Comedi drivers need PCI support, so we could
    save a bit of space by not including it.  The Comedi PCI support
    functions are all in "comedi_pci.c".  Turn it into a separate module so
    the support code doesn't have to be loaded unnecessarily.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_pci.c
+++ drivers/staging/comedi/comedi_pci.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
+#include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/interrupt.h>
 
@@ -169,3 +170,18 @@ void comedi_pci_driver_unregister(struct comedi_driver *comedi_driver,
 	comedi_driver_unregister(comedi_driver);
 }
 EXPORT_SYMBOL_GPL(comedi_pci_driver_unregister);
+
+static int __init comedi_pci_init(void)
+{
+	return 0;
+}
+module_init(comedi_pci_init);
+
+static void __exit comedi_pci_exit(void)
+{
+}
+module_exit(comedi_pci_exit);
+
+MODULE_AUTHOR("http://www.comedi.org");
+MODULE_DESCRIPTION("Comedi PCI interface module");
+MODULE_LICENSE("GPL");
