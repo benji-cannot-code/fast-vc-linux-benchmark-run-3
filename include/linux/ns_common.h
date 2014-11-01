eda000a6b20a43764efb5e9a738e517ffeaab3c8
Al Viro <viro@zeniv.linux.org.uk>
2014-10-31 22:56:04 -0400

    common object embedded into various struct ....ns
    for now - just move corresponding ->proc_inum instances over there
    
    Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

---
+++ include/linux/ns_common.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _LINUX_NS_COMMON_H
+#define _LINUX_NS_COMMON_H
+
+struct ns_common {
+	unsigned int inum;
+};
+
+#endif
