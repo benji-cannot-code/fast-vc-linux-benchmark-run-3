5bc3b81bd8c4ca9c62784ff9f5119cde5ddf2b1f
Al Viro <viro@zeniv.linux.org.uk>
2014-11-01 02:32:53 -0400

    copy address of proc_ns_ops into ns_common
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/ns_common.h
+++ include/linux/ns_common.h
@@ -2,7 +2,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_NS_COMMON_H
 #define _LINUX_NS_COMMON_H
 
+struct proc_ns_operations;
+
 struct ns_common {
+	const struct proc_ns_operations *ops;
 	unsigned int inum;
 };
 
