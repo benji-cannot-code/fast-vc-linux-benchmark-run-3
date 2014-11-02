91f50618954027beb68d33df7bdcd9d18a0c7ee1
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2014-11-02 08:35:46 -0300

    [media] cx231xx: too much changes. Bump version number
    The I2C mux changes are significant. Bump version number.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/cx231xx/cx231xx-video.c
+++ drivers/media/usb/cx231xx/cx231xx-video.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "cx231xx-vbi.h"
 
-#define CX231XX_VERSION "0.0.2"
+#define CX231XX_VERSION "0.0.3"
 
 #define DRIVER_AUTHOR   "Srinivasa Deevi <srinivasa.deevi@conexant.com>"
 #define DRIVER_DESC     "Conexant cx231xx based USB video device driver"
