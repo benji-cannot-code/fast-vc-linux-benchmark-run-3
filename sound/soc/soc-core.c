e3647e7382472917397274f9a07ca1480c131d93
Lars-Peter Clausen <lars@metafoo.de>
2014-11-03 10:31:48 +0100

    ASoC: Remove platform field from snd_soc_dai
    Typically a DAI does not need direct access to the platform. Currently the
    only user of this field is in a platform driver where we have a more direct
    way of getting a pointer to the platform. This patch updates the driver to
    use the more direct way and then removes the platform field from the
    snd_soc_dai struct.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1310,7 +1310,6 @@ static int soc_probe_link_dais(struct snd_soc_card *card, int num, int order)
 {
 	struct snd_soc_dai_link *dai_link = &card->dai_link[num];
 	struct snd_soc_pcm_runtime *rtd = &card->rtd[num];
-	struct snd_soc_platform *platform = rtd->platform;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	int i, ret;
 
@@ -1318,7 +1317,6 @@ static int soc_probe_link_dais(struct snd_soc_card *card, int num, int order)
 			card->name, num, order);
 
 	/* config components */
-	cpu_dai->platform = platform;
 	cpu_dai->card = card;
 	for (i = 0; i < rtd->num_codecs; i++)
 		rtd->codec_dais[i]->card = card;
