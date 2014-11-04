ccaf8124b00b5697ea5859115dd70572a3d3301d
Pradeep Bhat <pradeep.bhat@intel.com>
2014-11-04 17:06:40 +0000

    drm/i915/skl: Definition of SKL WM param structs for pipe/plane
    This patch defines the structures needed for computation of
    watermarks of pipes and planes for SKL.
    
    v2: Incorporated Damien's review comments and removed unused fields
        in structs for future features like rotation, drrs and scaling.
        The skl_wm_values struct is now made more generic across planes
        and cursor planes for all pipes.
    
    v3: implemented the plane/cursor split.
    
    v4: Change the wm union back to a structure (Ville, Daniel)
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Pradeep Bhat <pradeep.bhat@intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -413,6 +413,12 @@ struct intel_mmio_flip {
 	struct work_struct work;
 };
 
+struct skl_pipe_wm {
+	struct skl_wm_level wm[8];
+	struct skl_wm_level trans_wm;
+	uint32_t linetime;
+};
+
 struct intel_crtc {
 	struct drm_crtc base;
 	enum pipe pipe;
@@ -460,6 +466,8 @@ struct intel_crtc {
 	struct {
 		/* watermarks currently being used  */
 		struct intel_pipe_wm active;
+		/* SKL wm values currently in use */
+		struct skl_pipe_wm skl_active;
 	} wm;
 
 	int scanline_offset;
