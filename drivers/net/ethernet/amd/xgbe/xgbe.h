fdf91a6e0f8d6780d93d6108b8bf81f2061ec333
Lendacky, Thomas <Thomas.Lendacky@amd.com>
2014-11-04 16:06:56 -0600

    amd-xgbe: Add support for per DMA channel interrupts
    This patch provides support for interrupts that are generated by the
    Tx/Rx DMA channel pairs of the device.  This allows for Tx and Rx
    processing to run across multiple processsors.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/amd/xgbe/xgbe.h
+++ drivers/net/ethernet/amd/xgbe/xgbe.h
@@ -174,6 +174,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Device-tree clock names */
 #define XGBE_DMA_CLOCK		"dma_clk"
 #define XGBE_PTP_CLOCK		"ptp_clk"
+#define XGBE_DMA_IRQS		"amd,per-channel-interrupt"
 
 /* Timestamp support - values based on 50MHz PTP clock
  *   50MHz => 20 nsec
@@ -359,6 +360,12 @@ struct xgbe_channel {
 	/* Queue index and base address of queue's DMA registers */
 	unsigned int queue_index;
 	void __iomem *dma_regs;
+
+	/* Per channel interrupt irq number */
+	int dma_irq;
+
+	/* Netdev related settings */
+	struct napi_struct napi;
 
 	unsigned int saved_ier;
 
@@ -610,7 +617,8 @@ struct xgbe_prv_data {
 	/* XPCS indirect addressing mutex */
 	struct mutex xpcs_mutex;
 
-	int irq_number;
+	int dev_irq;
+	unsigned int per_channel_irq;
 
 	struct xgbe_hw_if hw_if;
 	struct xgbe_desc_if desc_if;
