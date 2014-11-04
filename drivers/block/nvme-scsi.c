4f335d13f49bf2da93d45a297ef599a0763cdca3
Matias Bjørling <m@bjorling.me>
2014-11-04 08:20:14 -0700

    NVMe: Convert to blk-mq
    This converts the NVMe driver to a blk-mq request-based driver.
    
    The NVMe driver is currently bio-based and implements queue logic within
    itself.  By using blk-mq, a lot of these responsibilities can be moved
    and simplified.
    
    The patch is divided into the following blocks:
    
     * Per-command data and cmdid have been moved into the struct request
       field. The cmdid_data can be retrieved using blk_mq_rq_to_pdu() and id
       maintenance are now handled by blk-mq through the rq->tag field.
    
     * The logic for splitting bio's has been moved into the blk-mq layer.
       The driver instead notifies the block layer about limited gap support in
       SG lists.
    
     * blk-mq handles timeouts and is reimplemented within nvme_timeout().
       This both includes abort handling and command cancelation.
    
     * Assignment of nvme queues to CPUs are replaced with the blk-mq
       version. The current blk-mq strategy is to assign the number of
       mapped queues and CPUs to provide synergy, while the nvme driver
       assign as many nvme hw queues as possible. This can be implemented in
       blk-mq if needed.
    
     * NVMe queues are merged with the tags structure of blk-mq.
    
     * blk-mq takes care of setup/teardown of nvme queues and guards invalid
       accesses. Therefore, RCU-usage for nvme queues can be removed.
    
     * IO tracing and accounting are handled by blk-mq and therefore removed.
    
     * Queue suspension logic is replaced with the logic from the block
       layer.
    
    Contributions in this patch from:
    
      Sam Bradshaw <sbradshaw@micron.com>
      Jens Axboe <axboe@fb.com>
      Keith Busch <keith.busch@intel.com>
      Robert Nelson <rlnelson@google.com>
    
    Acked-by: Keith Busch <keith.busch@intel.com>
    Acked-by: Jens Axboe <axboe@fb.com>
    
    Updated for new ->queue_rq() prototype.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/nvme-scsi.c
+++ drivers/block/nvme-scsi.c
@@ -2106,7 +2106,7 @@ static int nvme_trans_do_nvme_io(struct nvme_ns *ns, struct sg_io_hdr *hdr,
 
 		nvme_offset += unit_num_blocks;
 
-		nvme_sc = nvme_submit_io_cmd(dev, &c, NULL);
+		nvme_sc = nvme_submit_io_cmd(dev, ns, &c, NULL);
 		if (nvme_sc != NVME_SC_SUCCESS) {
 			nvme_unmap_user_pages(dev,
 				(is_write) ? DMA_TO_DEVICE : DMA_FROM_DEVICE,
@@ -2659,7 +2659,7 @@ static int nvme_trans_start_stop(struct nvme_ns *ns, struct sg_io_hdr *hdr,
 			c.common.opcode = nvme_cmd_flush;
 			c.common.nsid = cpu_to_le32(ns->ns_id);
 
-			nvme_sc = nvme_submit_io_cmd(ns->dev, &c, NULL);
+			nvme_sc = nvme_submit_io_cmd(ns->dev, ns, &c, NULL);
 			res = nvme_trans_status_code(hdr, nvme_sc);
 			if (res)
 				goto out;
@@ -2687,7 +2687,7 @@ static int nvme_trans_synchronize_cache(struct nvme_ns *ns,
 	c.common.opcode = nvme_cmd_flush;
 	c.common.nsid = cpu_to_le32(ns->ns_id);
 
-	nvme_sc = nvme_submit_io_cmd(ns->dev, &c, NULL);
+	nvme_sc = nvme_submit_io_cmd(ns->dev, ns, &c, NULL);
 
 	res = nvme_trans_status_code(hdr, nvme_sc);
 	if (res)
@@ -2895,7 +2895,7 @@ static int nvme_trans_unmap(struct nvme_ns *ns, struct sg_io_hdr *hdr,
 	c.dsm.nr = cpu_to_le32(ndesc - 1);
 	c.dsm.attributes = cpu_to_le32(NVME_DSMGMT_AD);
 
-	nvme_sc = nvme_submit_io_cmd(dev, &c, NULL);
+	nvme_sc = nvme_submit_io_cmd(dev, ns, &c, NULL);
 	res = nvme_trans_status_code(hdr, nvme_sc);
 
 	dma_free_coherent(&dev->pci_dev->dev, ndesc * sizeof(*range),
