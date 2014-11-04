55f9ec55c62504266827886dbfc9d6db8d71ba12
Benjamin Romer <benjamin.romer@unisys.com>
2014-11-04 11:25:24 -0500

    staging: unisys: refactor visorutil_spar_detect()
    Fix the declaration line so it is just one single line, and add the missing
    brackets on the else clause in the if statement.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorutil/visorkmodutils.c
+++ drivers/staging/unisys/visorutil/visorkmodutils.c
@@ -37,8 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int unisys_spar_platform;
 EXPORT_SYMBOL_GPL(unisys_spar_platform);
 
-static __init uint32_t
-visorutil_spar_detect(void)
+static __init uint32_t visorutil_spar_detect(void)
 {
 	unsigned int eax, ebx, ecx, edx;
 
@@ -48,8 +47,9 @@ visorutil_spar_detect(void)
 		return  (ebx == UNISYS_SPAR_ID_EBX) &&
 			(ecx == UNISYS_SPAR_ID_ECX) &&
 			(edx == UNISYS_SPAR_ID_EDX);
-	} else
+	} else {
 		return 0;
+	}
 }
 
 static __init int
