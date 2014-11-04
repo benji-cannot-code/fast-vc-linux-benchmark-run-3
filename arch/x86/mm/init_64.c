c3d6bc4677c34c6c7a00ff1836f6fb89adc04d7a
Daniel J Blueman <daniel@numascale.com>
2014-11-04 16:29:44 +0800

    x86: mm: Use 2GB memory block size on large-memory x86-64 systems
    On large-memory x86-64 systems of 64GB or more with memory hot-plug
    enabled, use a 2GB memory block size. Eg with 64GB memory, this reduces
    the number of directories in /sys/devices/system/memory from 512 to 32,
    making it more manageable, and reducing the creation time accordingly.
    
    This caveat is that the memory can't be offlined (for hotplug or
    otherwise) with the finer default 128MB granularity, but this is
    unimportant due to the high memory densities generally used with such
    large-memory systems, where eg a single DIMM is the order of 16GB.
    
    Signed-off-by: Daniel J Blueman <daniel@numascale.com>
    Cc: Steffen Persvold <sp@numascale.com>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Link: http://lkml.kernel.org/r/1415089784-28779-4-git-send-email-daniel@numascale.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/numa.h>
 #include <asm/cacheflush.h>
 #include <asm/init.h>
-#include <asm/uv/uv.h>
 #include <asm/setup.h>
 
 #include "mm_internal.h"
@@ -1248,12 +1247,10 @@ static unsigned long probe_memory_block_size(void)
 	/* start from 2g */
 	unsigned long bz = 1UL<<31;
 
-#ifdef CONFIG_X86_UV
-	if (is_uv_system()) {
-		printk(KERN_INFO "UV: memory block size 2GB\n");
+	if (totalram_pages >= (64ULL << (30 - PAGE_SHIFT))) {
+		pr_info("Using 2GB memory block size for large-memory system\n");
 		return 2UL * 1024 * 1024 * 1024;
 	}
-#endif
 
 	/* less than 64g installed */
 	if ((max_pfn << PAGE_SHIFT) < (16UL << 32))
