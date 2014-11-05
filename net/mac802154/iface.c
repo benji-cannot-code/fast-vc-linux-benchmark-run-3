2a1553d37da2336278df52e7f56313d849482a0f
Alexander Aring <alex.aring@gmail.com>
2014-11-05 20:51:16 +0100

    mac802154: move dev_hold out of ieee802154_if_add
    This patch moves the dev_hold call inside of nl-phy ieee802154_add_iface
    function. The ieee802154_add_iface is the only one function which use the
    ieee802154_if_add function and contains the corresponding dev_put call.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/mac802154/iface.c
+++ net/mac802154/iface.c
@@ -502,7 +502,6 @@ ieee802154_if_add(struct ieee802154_local *local, const char *name,
 	if (err)
 		goto err_free;
 
-	dev_hold(dev); /* we return an incremented device refcount */
 	return dev;
 
 err_free:
