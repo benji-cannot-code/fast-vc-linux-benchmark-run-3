0549b7f2971c576bffb430dbe29f8acc5f5a149d
Malcolm Priestley <tvboxspy@gmail.com>
2014-11-05 21:08:49 +0000

    staging: vt6655: mac remove dead variable TxRate_iwconfig
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/mac.c
+++ drivers/staging/vt6655/mac.c
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tmacro.h"
 #include "mac.h"
 
-unsigned short TxRate_iwconfig;//2008-5-8 <add> by chester
 /*---------------------  Static Classes  ----------------------------*/
 
 /*---------------------  Static Variables  --------------------------*/
--- drivers/staging/vt6655/mac.h
+++ drivers/staging/vt6655/mac.h
@@ -971,7 +971,6 @@ do {								\
 
 /*---------------------  Export Functions  --------------------------*/
 
-extern unsigned short TxRate_iwconfig;//2008-5-8 <add> by chester
 void MACvReadAllRegs(void __iomem *dwIoBase, unsigned char *pbyMacRegs);
 
 bool MACbIsRegBitsOn(void __iomem *dwIoBase, unsigned char byRegOfs, unsigned char byTestBits);
