cbc025fc018d2715384e6e8534a43c347867b931
Alexander Aring <alex.aring@gmail.com>
2014-11-05 20:51:13 +0100

    ieee802154: remove nl802154 unused functions
    The include/net/nl802154.h file contains a lot of prototypes which are
    not used inside of ieee802154 subsystem. This patch removes this file
    and make the only one used prototype "ieee802154_nl_start_confirm" as
    static declaration in ieee802154/nl-mac.c
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/mac802154/mac_cmd.c
+++ net/mac802154/mac_cmd.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ieee802154_netdev.h>
 #include <net/cfg802154.h>
 #include <net/mac802154.h>
-#include <net/nl802154.h>
 
 #include "ieee802154_i.h"
 #include "driver-ops.h"
@@ -65,11 +64,6 @@ static int mac802154_mlme_start_req(struct net_device *dev,
 
 		rc = ops->llsec->set_params(dev, &params, changed);
 	}
-
-	/* FIXME: add validation for unused parameters to be sane
-	 * for SoftMAC
-	 */
-	ieee802154_nl_start_confirm(dev, IEEE802154_SUCCESS);
 
 	return rc;
 }
