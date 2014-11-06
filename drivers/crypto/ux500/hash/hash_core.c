0e96ed91f967c748710cfdceea8914c2d56421b3
Ulf Hansson <ulf.hansson@linaro.org>
2014-11-06 14:28:01 +0100

    crypto: ux500/hash - Silence compiler warning for !CONFIG_PM
    The system PM functions were unused when CONFIG_PM is unset. Let's move
    them inside CONFIG_PM_SLEEP to silence the compiler warning.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ux500/hash/hash_core.c
+++ drivers/crypto/ux500/hash/hash_core.c
@@ -1882,6 +1882,7 @@ static void ux500_hash_shutdown(struct platform_device *pdev)
 			__func__);
 }
 
+#ifdef CONFIG_PM_SLEEP
 /**
  * ux500_hash_suspend - Function that suspends the hash device.
  * @dev:	Device to suspend.
@@ -1950,6 +1951,7 @@ static int ux500_hash_resume(struct device *dev)
 
 	return ret;
 }
+#endif
 
 static SIMPLE_DEV_PM_OPS(ux500_hash_pm, ux500_hash_suspend, ux500_hash_resume);
 
