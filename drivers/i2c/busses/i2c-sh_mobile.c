0a74524c3f2aed0dee7cbd7ec5e921970b7732ea
Wolfram Sang <wsa+renesas@sang-engineering.com>
2014-11-07 11:11:40 +0100

    i2c: sh_mobile: sort includes alphabetically
    Improves readability and reduces chances of duplicates.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-sh_mobile.c
+++ drivers/i2c/busses/i2c-sh_mobile.c
@@ -17,20 +17,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/i2c.h>
+#include <linux/i2c/i2c-sh_mobile.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/delay.h>
-#include <linux/platform_device.h>
-#include <linux/interrupt.h>
-#include <linux/i2c.h>
-#include <linux/err.h>
-#include <linux/pm_runtime.h>
-#include <linux/clk.h>
-#include <linux/io.h>
-#include <linux/slab.h>
 #include <linux/of_device.h>
-#include <linux/i2c/i2c-sh_mobile.h>
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <linux/slab.h>
 
 /* Transmit operation:                                                      */
 /*                                                                          */
