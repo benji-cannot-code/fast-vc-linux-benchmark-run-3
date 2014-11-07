4d8c3f1057ac2ffcce1c7b06ae4574184f284e7e
Alex Deucher <alexander.deucher@amd.com>
2014-11-07 11:52:12 -0500

    drm/radeon/ci: handle gpio controlled dpm features properly
    Certain feature enablement depends on entries in the atom
    gpio pin table.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/ci_dpm.h
+++ drivers/gpu/drm/radeon/ci_dpm.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CISLANDS_MAX_HARDWARE_POWERLEVELS 2
 
+#define CISLANDS_UNUSED_GPIO_PIN 0x7F
+
 struct ci_pl {
 	u32 mclk;
 	u32 sclk;
