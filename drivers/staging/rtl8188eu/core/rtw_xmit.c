39927219d668172c755cb89e505d4479aa3ffbf9
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2014-11-07 16:50:46 +0530

    staging: rtl8188eu: unneeded NULL check
    before these NULL checks we are already checking it for NULL, and if
    it is NULL then we are jumping to the error label and handling
    the error before returning.
    So we can reach this part of the code only if the variable is known
    to be not NULL, and if we already know that it is not NULL, then no
    need to check it again.
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_xmit.c
+++ drivers/staging/rtl8188eu/core/rtw_xmit.c
@@ -1026,8 +1026,7 @@ s32 rtw_xmitframe_coalesce(struct adapter *padapter, struct sk_buff *pkt, struct
 
 		/* adding icv, if necessary... */
 		if (pattrib->iv_len) {
-			if (psta != NULL) {
-				switch (pattrib->encrypt) {
+			switch (pattrib->encrypt) {
 				case _WEP40_:
 				case _WEP104_:
 					WEP_IV(pattrib->iv, psta->dot11txpn, pattrib->key_idx);
@@ -1044,7 +1043,6 @@ s32 rtw_xmitframe_coalesce(struct adapter *padapter, struct sk_buff *pkt, struct
 					else
 						AES_IV(pattrib->iv, psta->dot11txpn, 0);
 					break;
-				}
 			}
 
 			memcpy(pframe, pattrib->iv, pattrib->iv_len);
