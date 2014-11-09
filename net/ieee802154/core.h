b455c037b5eb91e2b018bd0ea34541b692f49eb7
Alexander Aring <alex.aring@gmail.com>
2014-11-09 08:36:48 +0100

    ieee802154: add cfg802154_registered_device list
    This patch adds a new cfg802154_rdev_list to remember all registered
    cfg802154_registered_device structs. This is needed to prepare the
    upcomming nl802154 framework.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/ieee802154/core.h
+++ net/ieee802154/core.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct cfg802154_registered_device {
 	const struct cfg802154_ops *ops;
+	struct list_head list;
 
 	/* wpan_phy index, internal only */
 	int wpan_phy_idx;
