8610cfa3835fb123640035335ae6e604359e67b7
Olof Johansson <olof@lixom.net>
2014-11-08 22:39:51 -0800

    ARM: at91: fix build breakage due to legacy board removals
    Fixes the following missing includes:
    
    arch/arm/mach-at91/at91sam9g45.c: In function 'at91sam9g45_init_time':
    arch/arm/mach-at91/at91sam9g45.c:39:23: error: 'NR_IRQS_LEGACY' undeclared (first use in this function)
      at91sam926x_pit_init(NR_IRQS_LEGACY + AT91_ID_SYS);
                           ^
    arch/arm/mach-at91/at91sam9g45.c:39:23: note: each undeclared identifier is reported only once for each function it appears in
    make[3]: *** [arch/arm/mach-at91/at91sam9g45.o] Error 1
    arch/arm/mach-at91/at91sam9rl.c: In function 'at91sam9rl_init_time':
    arch/arm/mach-at91/at91sam9rl.c:51:23: error: 'NR_IRQS_LEGACY' undeclared (first use in this function)
      at91sam926x_pit_init(NR_IRQS_LEGACY + AT91_ID_SYS);
    
    Cc: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-at91/at91sam9rl.c
+++ arch/arm/mach-at91/at91sam9rl.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <asm/system_misc.h>
+#include <asm/irq.h>
 #include <mach/cpu.h>
 #include <mach/at91_dbgu.h>
 #include <mach/hardware.h>
