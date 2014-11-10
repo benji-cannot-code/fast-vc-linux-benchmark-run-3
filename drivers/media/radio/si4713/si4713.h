b130e9372b686632fe57dde192f33d27a0bd65d3
Sebastian Reichel <sre@kernel.org>
2014-11-10 17:34:41 -0300

    [media] si4713: add device tree support
    Add device tree support by changing the device registration order.
    In the device tree the si4713 node is a normal I2C device, which
    will be probed as such. Thus the V4L device must be probed from
    the I2C device and not the other way around.
    
    Signed-off-by: Sebastian Reichel <sre@kernel.org>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/radio/si4713/si4713.h
+++ drivers/media/radio/si4713/si4713.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef SI4713_I2C_H
 #define SI4713_I2C_H
 
+#include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
 #include <linux/gpio/consumer.h>
 #include <media/v4l2-subdev.h>
@@ -239,11 +240,16 @@ struct si4713_device {
 	struct regulator *vdd;
 	struct regulator *vio;
 	struct gpio_desc *gpio_reset;
+	struct platform_device *pd;
 	u32 power_state;
 	u32 rds_enabled;
 	u32 frequency;
 	u32 preemphasis;
 	u32 stereo;
 	u32 tune_rnl;
+};
+
+struct radio_si4713_platform_data {
+	struct i2c_client *subdev;
 };
 #endif /* ifndef SI4713_I2C_H */
--- include/media/si4713.h
+++ include/media/si4713.h
@@ -27,6 +27,7 @@ struct si4713_platform_data {
 	const char * const *supply_names;
 	unsigned supplies;
 	int gpio_reset; /* < 0 if not used */
+	bool is_platform_device;
 };
 
 /*
