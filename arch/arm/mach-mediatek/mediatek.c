8ae42ee04791b3dda74a1ee7e5ea5c9fc07190df
Joe.C <yingjoe.chen@mediatek.com>
2014-11-11 15:53:41 +0800

    ARM: mediatek: Add basic support for mt8127
    This adds a basic dtsi for MT8127 SoC.
    
    Signed-off-by: Joe.C <yingjoe.chen@mediatek.com>
    Signed-off-by: Matthias Brugger <matthias.bgg@gmail.com>

--- arch/arm/mach-mediatek/mediatek.c
+++ arch/arm/mach-mediatek/mediatek.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static const char * const mediatek_board_dt_compat[] = {
 	"mediatek,mt6589",
+	"mediatek,mt8127",
 	NULL,
 };
 
