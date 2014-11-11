1963426e3db178641e694830a22fe5a23aef3fbb
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-11-10 17:57:54 -0700

    staging: comedi: adl_pci9118: use comedi_async 'scans_done' to detect EOA
    The comedi core now counts the number of samples added to the async buffer and
    detects the end-of-scan and increments the comedi_async 'scans_done' counter.
    
    Remove the private data member 'ai_act_scan' and use the 'scans_done' member
    to detect the end-of-acquisition.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/adl_pci9118.c
+++ drivers/staging/comedi/drivers/adl_pci9118.c
@@ -222,7 +222,6 @@ struct pci9118_private {
 	unsigned char int_ctrl;
 	unsigned char ai_cfg;
 	unsigned int ai_do;		/* what do AI? 0=nothing, 1 to 4 mode */
-	unsigned int ai_act_scan;	/* how many scans we finished */
 	unsigned int ai_n_realscanlen;	/*
 					 * what we must transfer for one
 					 * outgoing scan include front/back adds
@@ -478,13 +477,7 @@ static void move_block_from_dma(struct comedi_device *dev,
 				unsigned short *dma_buffer,
 				unsigned int num_samples)
 {
-	struct pci9118_private *devpriv = dev->private;
-	struct comedi_cmd *cmd = &s->async->cmd;
-
 	num_samples = defragment_dma_buffer(dev, s, dma_buffer, num_samples);
-	devpriv->ai_act_scan +=
-	    (s->async->cur_chan + num_samples) / cmd->scan_end_arg;
-
 	comedi_buf_write_samples(s, dma_buffer, num_samples);
 }
 
@@ -572,7 +565,6 @@ static int pci9118_ai_cancel(struct comedi_device *dev,
 	devpriv->ai_do = 0;
 	devpriv->usedma = 0;
 
-	devpriv->ai_act_scan = 0;
 	devpriv->ai_act_dmapos = 0;
 	s->async->inttrig = NULL;
 	devpriv->ai_neverending = 0;
@@ -613,13 +605,9 @@ static void interrupt_pci9118_ai_onesample(struct comedi_device *dev,
 
 	comedi_buf_write_samples(s, &sampl, 1);
 
-	if (s->async->cur_chan == 0) {
-		devpriv->ai_act_scan++;
-		if (!devpriv->ai_neverending) {
-			/* all data sampled? */
-			if (devpriv->ai_act_scan >= cmd->stop_arg)
-				s->async->events |= COMEDI_CB_EOA;
-		}
+	if (!devpriv->ai_neverending) {
+		if (s->async->scans_done >= cmd->stop_arg)
+			s->async->events |= COMEDI_CB_EOA;
 	}
 }
 
@@ -652,8 +640,7 @@ static void interrupt_pci9118_ai_dma(struct comedi_device *dev,
 	}
 
 	if (!devpriv->ai_neverending) {
-		/* all data sampled? */
-		if (devpriv->ai_act_scan >= cmd->stop_arg)
+		if (s->async->scans_done >= cmd->stop_arg)
 			s->async->events |= COMEDI_CB_EOA;
 	}
 
@@ -1115,7 +1102,6 @@ static int pci9118_ai_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 	inl(dev->iobase + PCI9118_AI_STATUS_REG);
 	inl(dev->iobase + PCI9118_INT_CTRL_REG);
 
-	devpriv->ai_act_scan = 0;
 	devpriv->ai_act_dmapos = 0;
 
 	if (devpriv->usedma) {
