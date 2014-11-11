5655d2aaaf0f58aadc2891dd3779403f6ef6f734
Dinh Nguyen <dinguyen@opensource.altera.com>
2014-11-11 11:13:39 -0600

    usb: dwc2: move usb_disabled() call to host driver only
    Since platform.c will get built for both Host and Gadget, if we leave the
    usb_disabled() call in platform.c, it results in the following build error
    when (!USB && USB_GADGET) condition is met.
    
    ERROR: "usb_disabled" [drivers/usb/dwc2/dwc2_platform.ko] undefined!
    
    Since usb_disabled() is mostly used to disable USB host functionality, move
    the call the host portion for the DWC2 driver.
    
    Acked-by: Paul Zimmerman <paulz@synopsys.com>
    Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc2/hcd.c
+++ drivers/usb/dwc2/hcd.c
@@ -2847,6 +2847,9 @@ int dwc2_hcd_init(struct dwc2_hsotg *hsotg, int irq,
 	int i, num_channels;
 	int retval;
 
+	if (usb_disabled())
+		return -ENODEV;
+
 	dev_dbg(hsotg->dev, "DWC OTG HCD INIT\n");
 
 	/* Detect config values from hardware */
