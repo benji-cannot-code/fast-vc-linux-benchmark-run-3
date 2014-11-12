1861b27bf326cca9a8dec5eba1d6cb095f8b60aa
Finn Thain <fthain@telegraphics.com.au>
2014-11-12 16:12:21 +1100

    atari_NCR5380: Move static co-routine variables to host data
    Unlike NCR5380.c, the atari_NCR5380.c core driver is limited to a single
    instance because co-routine state is stored globally.
    
    Fix this by removing the static scsi host pointer. For the co-routine,
    obtain this pointer from the work_struct pointer instead. For the interrupt
    handler, obtain it from the dev_id argument.
    
    Signed-off-by: Finn Thain <fthain@telegraphics.com.au>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Tested-by: Michael Schmitz <schmitzmic@gmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/scsi/atari_scsi.c
+++ drivers/scsi/atari_scsi.c
@@ -111,7 +111,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NCR5380_dma_xfer_len(instance, cmd, phase) \
         atari_dma_xfer_len(cmd->SCp.this_residual, cmd, !((phase) & SR_IO))
 
-#define NCR5380_acquire_dma_irq(instance)      falcon_get_lock()
+#define NCR5380_acquire_dma_irq(instance)      falcon_get_lock(instance)
 #define NCR5380_release_dma_irq(instance)      falcon_release_lock()
 
 #include "NCR5380.h"
@@ -469,15 +469,15 @@ static void falcon_release_lock(void)
  * command immediately but tell the SCSI mid-layer to defer.
  */
 
-static int falcon_get_lock(void)
+static int falcon_get_lock(struct Scsi_Host *instance)
 {
 	if (IS_A_TT())
 		return 1;
 
 	if (in_interrupt())
-		return stdma_try_lock(scsi_falcon_intr, NULL);
+		return stdma_try_lock(scsi_falcon_intr, instance);
 
-	stdma_lock(scsi_falcon_intr, NULL);
+	stdma_lock(scsi_falcon_intr, instance);
 	return 1;
 }
 
