c601aefa0de7a7c8bbb47fa43d90d7bce78bcdd5
Malcolm Priestley <tvboxspy@gmail.com>
2014-11-12 21:11:08 +0000

    staging: vt6655: card Remove function vUpdateIFS
    vUpdateIFS does the same operations of CARDbSetPhyParameter there
    is not need to call this function.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/card.h
+++ drivers/staging/vt6655/card.h
@@ -68,7 +68,6 @@ typedef enum _CARD_STATUS_TYPE {
 struct vnt_private;
 
 void CARDvSetRSPINF(struct vnt_private *, u8);
-void vUpdateIFS(struct vnt_private *);
 void CARDvUpdateBasicTopRate(struct vnt_private *);
 bool CARDbIsOFDMinBasicRate(struct vnt_private *);
 void CARDvSetLoopbackMode(void __iomem *dwIoBase, unsigned short wLoopbackMode);
