86b8f924e7d7efc3bef604dd7ca2c58626eda5b8
Finn Thain <fthain@telegraphics.com.au>
2014-11-12 16:12:16 +1100

    atari_NCR5380: Merge from sun3_NCR5380.c
    There is very little difference between the sun3_NCR5380.c core driver
    and atari_NCR5380.c. The former is a fork of the latter.
    
    Merge the sun3_NCR5380.c core driver into atari_NCR5380.c so that
    sun3_scsi.c can adopt the latter and the former can be deleted.
    
    Signed-off-by: Finn Thain <fthain@telegraphics.com.au>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Tested-by: Michael Schmitz <schmitzmic@gmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/scsi/atari_scsi.c
+++ drivers/scsi/atari_scsi.c
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define REAL_DMA
 #define SUPPORT_TAGS
 #define MAX_TAGS                        32
+#define DMA_MIN_SIZE                    32
 
 #define NCR5380_implementation_fields   /* none */
 
