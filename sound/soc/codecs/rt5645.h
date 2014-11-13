388c88479d46ba838f87740c77964c1ba28e6a59
Bard Liao <bardliao@realtek.com>
2014-11-13 09:55:22 +0800

    ASoC: rt5645: add register setting for TDM
    We need to set extra register to avoid a recording issue in TDM
    mode.
    
    Signed-off-by: Bard Liao <bardliao@realtek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5645.h
+++ sound/soc/codecs/rt5645.h
@@ -1856,6 +1856,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RT5645_M_BB_HPF_R_SFT			6
 #define RT5645_G_BB_BST_MASK			(0x3f)
 #define RT5645_G_BB_BST_SFT			0
+#define RT5645_G_BB_BST_25DB			0x14
 
 /* MP3 Plus Control 1 (0xd0) */
 #define RT5645_M_MP3_L_MASK			(0x1 << 15)
