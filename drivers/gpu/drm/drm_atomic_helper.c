0db8d89d29cbb7b62c9c2024c5191dd7f8606bc6
Rob Clark <robdclark@gmail.com>
2014-11-14 17:12:59 -0500

    drm/atomic: rip out unnecessary locking checks
    For async commit, it is *intentional* that those locks are not held.
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Rob Clark <robdclark@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -1007,8 +1007,6 @@ void drm_atomic_helper_commit_planes(struct drm_device *dev,
 		if (!crtc)
 			continue;
 
-		WARN_ON(!drm_modeset_is_locked(&crtc->mutex));
-
 		funcs = crtc->helper_private;
 
 		if (!funcs || !funcs->atomic_begin)
@@ -1023,8 +1021,6 @@ void drm_atomic_helper_commit_planes(struct drm_device *dev,
 
 		if (!plane)
 			continue;
-
-		WARN_ON(!drm_modeset_is_locked(&plane->mutex));
 
 		funcs = plane->helper_private;
 
