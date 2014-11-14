25cae2dfb405517eb3c7b00cdf150c454c2da5c2
Dave Hansen <dave.hansen@linux.intel.com>
2014-11-14 07:18:17 -0800

    x86, mpx: Rename cfg_reg_u and status_reg
    According to Intel SDM extension, MPX configuration and status registers
    should be BNDCFGU and BNDSTATUS. This patch renames cfg_reg_u and
    status_reg to bndcfgu and bndstatus.
    
    [ tglx: Renamed 'struct bndscr_struct' to 'struct bndscr' ]
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: linux-mm@kvack.org
    Cc: linux-mips@linux-mips.org
    Cc: Dave Hansen <dave@sr71.net>
    Cc: Qiaowei Ren <qiaowei.ren@intel.com>
    Link: http://lkml.kernel.org/r/20141114151817.031762AC@viggo.jf.intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -380,9 +380,9 @@ struct bndreg {
 	u64 upper_bound;
 } __packed;
 
-struct bndcsr_struct {
-	u64 cfg_reg_u;
-	u64 status_reg;
+struct bndcsr {
+	u64 bndcfgu;
+	u64 bndstatus;
 } __packed;
 
 struct xsave_hdr_struct {
@@ -397,7 +397,7 @@ struct xsave_struct {
 	struct ymmh_struct ymmh;
 	struct lwp_struct lwp;
 	struct bndreg bndreg[4];
-	struct bndcsr_struct bndcsr;
+	struct bndcsr bndcsr;
 	/* new processor state extensions will go here */
 } __attribute__ ((packed, aligned (64)));
 
