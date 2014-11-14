fb7bffabc2ac1a53ba27c8f9c6a86e7ea964f034
Dave Hansen <dave.hansen@linux.intel.com>
2014-11-14 07:18:28 -0800

    x86, mpx: Decode MPX instruction to get bound violation information
    This patch sets bound violation fields of siginfo struct in #BR
    exception handler by decoding the user instruction and constructing
    the faulting pointer.
    
    We have to be very careful when decoding these instructions.  They
    are completely controlled by userspace and may be changed at any
    time up to and including the point where we try to copy them in to
    the kernel.  They may or may not be MPX instructions and could be
    completely invalid for all we know.
    
    Note: This code is based on Qiaowei Ren's specialized MPX
    decoder, but uses the generic decoder whenever possible.  It was
    tested for robustness by generating a completely random data
    stream and trying to decode that stream.  I also unmapped random
    pages inside the stream to test the "partial instruction" short
    read code.
    
    We kzalloc() the siginfo instead of stack allocating it because
    we need to memset() it anyway, and doing this makes it much more
    clear when it got initialized by the MPX instruction decoder.
    
    Changes from the old decoder:
     * Use the generic decoder instead of custom functions.  Saved
       ~70 lines of code overall.
     * Remove insn->addr_bytes code (never used??)
     * Make sure never to possibly overflow the regoff[] array, plus
       check the register range correctly in 32 and 64-bit modes.
     * Allow get_reg() to return an error and have mpx_get_addr_ref()
       handle when it sees errors.
     * Only call insn_get_*() near where we actually use the values
       instead if trying to call them all at once.
     * Handle short reads from copy_from_user() and check the actual
       number of read bytes against what we expect from
       insn_get_length().  If a read stops in the middle of an
       instruction, we error out.
     * Actually check the opcodes intead of ignoring them.
     * Dynamically kzalloc() siginfo_t so we don't leak any stack
       data.
     * Detect and handle decoder failures instead of ignoring them.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Based-on-patch-by: Qiaowei Ren <qiaowei.ren@intel.com>
    Cc: linux-mm@kvack.org
    Cc: linux-mips@linux-mips.org
    Cc: Dave Hansen <dave@sr71.net>
    Link: http://lkml.kernel.org/r/20141114151828.5BDD0915@viggo.jf.intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/mpx.h
+++ arch/x86/include/asm/mpx.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <asm/ptrace.h>
+#include <asm/insn.h>
 
 #ifdef CONFIG_X86_64
 
@@ -33,5 +34,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MPX_BT_SIZE_BYTES (1UL<<(MPX_BT_ENTRY_OFFSET+MPX_BT_ENTRY_SHIFT))
 
 #define MPX_BNDSTA_ERROR_CODE	0x3
+
+#ifdef CONFIG_X86_INTEL_MPX
+siginfo_t *mpx_generate_siginfo(struct pt_regs *regs,
+				struct xsave_struct *xsave_buf);
+#else
+static inline siginfo_t *mpx_generate_siginfo(struct pt_regs *regs,
+					      struct xsave_struct *xsave_buf)
+{
+	return NULL;
+}
+#endif /* CONFIG_X86_INTEL_MPX */
 
 #endif /* _ASM_X86_MPX_H */
