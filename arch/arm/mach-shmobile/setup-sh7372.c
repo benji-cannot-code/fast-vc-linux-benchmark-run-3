de6216f1111fb8a0219b7fe75bdd2921d01ec4b1
Geert Uytterhoeven <geert+renesas@glider.be>
2014-11-14 17:07:06 +0100

    ARM: shmobile: sh7372: Spelling/grammar s/entity map/identity mapping/
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/setup-sh7372.c
+++ arch/arm/mach-shmobile/setup-sh7372.c
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sh7372.h"
 
 static struct map_desc sh7372_io_desc[] __initdata = {
-	/* create a 1:1 entity map for 0xe6xxxxxx
+	/* create a 1:1 identity mapping for 0xe6xxxxxx
 	 * used by CPGA, INTC and PFC.
 	 */
 	{
