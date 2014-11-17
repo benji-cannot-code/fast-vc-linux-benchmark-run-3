0ce0c295587e248b824e425414f8208ac3a7b9f0
Jeff Layton <jlayton@primarydata.com>
2014-11-17 16:58:05 -0500

    sunrpc: eliminate RPC_TRACEPOINTS
    It's always set to the same value as CONFIG_TRACEPOINTS, so we can just
    use that instead.
    
    Signed-off-by: Jeff Layton <jlayton@primarydata.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- include/linux/sunrpc/debug.h
+++ include/linux/sunrpc/debug.h
@@ -11,15 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <uapi/linux/sunrpc/debug.h>
 
-
-/*
- * Enable RPC debugging/profiling.
- */
-#ifdef CONFIG_TRACEPOINTS
-#define RPC_TRACEPOINTS
-#endif
-/* #define  RPC_PROFILE */
-
 /*
  * Debugging macros etc
  */
