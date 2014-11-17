a197df320f9ed1beb5a9ce6e948cf8b95625f707
Fabian Frederick <fabf@skynet.be>
2014-11-17 22:08:22 +0100

    net/core: include linux/types.h instead of asm/types.h
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/link_watch.c
+++ net/core/link_watch.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/workqueue.h>
 #include <linux/bitops.h>
-#include <asm/types.h>
+#include <linux/types.h>
 
 
 enum lw_bits {
