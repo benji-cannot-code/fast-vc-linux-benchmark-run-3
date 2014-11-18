c880c05d9fbf007eb270220ed57a8d9f319ef896
Stephane Viau <sviau@codeaurora.org>
2014-11-18 12:49:49 -0500

    drm/msm: add multiple CRTC and overlay support
    MDP5 currently support one single CRTC with its private pipe.
    This change allows the configuration of multiple CRTCs with
    the possibility to attach several public planes to these CRTCs.
    
    Signed-off-by: Stephane Viau <sviau@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/mdp/mdp5/mdp5_cfg.h
+++ drivers/gpu/drm/msm/mdp/mdp5/mdp5_cfg.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 extern const struct mdp5_cfg_hw *mdp5_cfg;
 
+#define MAX_CTL			8
 #define MAX_BASES		8
 #define MAX_SMP_BLOCKS		44
 #define MAX_CLIENTS		32
