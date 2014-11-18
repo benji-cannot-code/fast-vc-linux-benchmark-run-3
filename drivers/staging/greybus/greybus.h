6e6ebaff9d7b6cbebf03271baa18c2cc224b9ad5
Alex Elder <elder@linaro.org>
2014-11-17 18:08:29 -0600

    greybus: kill gbuf->kref
    Since there is only ever one reference to a gbuf, we don't need a
    kref to figure out when it can be freed.  Get rid of the kref and
    its supporting code.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -120,8 +120,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 
 struct gbuf {
-	struct kref kref;
-
 	struct greybus_host_device *hd;
 	u16 dest_cport_id;		/* Destination CPort id */
 	int status;
