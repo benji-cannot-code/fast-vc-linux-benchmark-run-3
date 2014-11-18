b010564eedb83c480bb1e74c691bae8e73758b1c
Alex Elder <elder@linaro.org>
2014-11-17 18:08:30 -0600

    greybus: move the definition of struct gbuf
    We no longer need struct gbuf defined in "greybus.h".  An upcoming
    patch will embed a gbuf struct (not a pointer) into the operation
    structure, and to do that we'll need the struct defined prior to the
    operation.  Just move the gbuf definition into "operation.h".
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/operation.h
+++ drivers/staging/greybus/operation.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/completion.h>
 
+struct gb_operation;
+
 enum gb_operation_status {
 	GB_OP_SUCCESS		= 0,
 	GB_OP_INVALID		= 1,
@@ -21,6 +23,17 @@ enum gb_operation_status {
 	GB_OP_PROTOCOL_BAD	= 5,
 	GB_OP_OVERFLOW		= 6,
 	GB_OP_TIMEOUT		= 0xff,
+};
+
+struct gbuf {
+	struct greybus_host_device *hd;
+	u16 dest_cport_id;		/* Destination CPort id */
+	int status;
+
+	void *transfer_buffer;
+	u32 transfer_buffer_length;
+
+	void *hcd_data;			/* for the HCD to track the gbuf */
 };
 
 /*
@@ -51,7 +64,6 @@ enum gb_operation_status {
  * is guaranteed to be 64-bit aligned.
  * XXX and callback?
  */
-struct gb_operation;
 typedef void (*gb_operation_callback)(struct gb_operation *);
 struct gb_operation {
 	struct gb_connection	*connection;
