53b9a9649ff03c5324eca6a1aab92f94cee46cf0
Dave Hansen <dave.hansen@linux.intel.com>
2014-11-17 16:36:08 -0800

    fs: Do not include mpx.h in exec.c
    We no longer need mpx.h in exec.c.  This will obviously also
    break the build for non-x86 builds.  We get the MPX includes that
    we need from mmu_context.h now.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dave Hansen <dave@sr71.net>
    Link: http://lkml.kernel.org/r/20141118003608.837015B3@viggo.jf.intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- fs/exec.c
+++ fs/exec.c
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
-#include <asm/mpx.h>
 
 #include <trace/events/task.h>
 #include "internal.h"
