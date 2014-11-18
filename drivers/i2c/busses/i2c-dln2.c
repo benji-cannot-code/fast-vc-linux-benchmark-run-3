088ba743c2e307c687ecbd587259f7920df9e9f4
Octavian Purdila <octavian.purdila@intel.com>
2014-11-18 14:57:58 +0200

    i2c: dln2: Simplify return flow for dln2_i2c_enable
    This fixes the following kbuild test robot warning:
    
    >> drivers/i2c/busses/i2c-dln2.c:70:1-4: WARNING: end returns can be simplified if negative or 0 value
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Reported-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>
    Acked-by: Wolfram Sang <wsa@the-dreams.de>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/i2c/busses/i2c-dln2.c
+++ drivers/i2c/busses/i2c-dln2.c
@@ -55,7 +55,6 @@ struct dln2_i2c {
 
 static int dln2_i2c_enable(struct dln2_i2c *dln2, bool enable)
 {
-	int ret;
 	u16 cmd;
 	struct {
 		u8 port;
@@ -68,11 +67,7 @@ static int dln2_i2c_enable(struct dln2_i2c *dln2, bool enable)
 	else
 		cmd = DLN2_I2C_DISABLE;
 
-	ret = dln2_transfer_tx(dln2->pdev, cmd, &tx, sizeof(tx));
-	if (ret < 0)
-		return ret;
-
-	return 0;
+	return dln2_transfer_tx(dln2->pdev, cmd, &tx, sizeof(tx));
 }
 
 static int dln2_i2c_write(struct dln2_i2c *dln2, u8 addr,
