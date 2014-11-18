78c274c6a5de04d3b07e001c017e3bcdc0d135c8
Alex Elder <elder@linaro.org>
2014-11-18 13:26:51 -0600

    greybus: kill the last gbuf remnants
    All the code has now been adjusted such that we can do away with the
    old gbuf structure.
    
    Three unused references remained in "greybus.h", so those are deleted.
    
    Other than that most of the changes were done by simple global
    substitution.  The gb_message structure incorporates the fields that
    were previously found its embedded gbuf structure.  A few names have
    been changed in the process:
        gbuf->transfer_buffer       message->buffer
        gbuf->transfer_buffer_size  message->buffer_size
        gbuf->hcd_data;             message->cookie
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct greybus_host_device;
 struct svc_msg;
-struct gbuf;
 
 /* Buffers allocated from the host driver will be aligned to this multiple */
 #define GB_BUFFER_ALIGN	sizeof(u32)
@@ -157,8 +156,6 @@ int gb_ap_init(void);
 void gb_ap_exit(void);
 int gb_debugfs_init(void);
 void gb_debugfs_cleanup(void);
-int gb_gbuf_init(void);
-void gb_gbuf_exit(void);
 
 extern struct bus_type greybus_bus_type;
 extern const struct attribute_group *greybus_module_groups[];
