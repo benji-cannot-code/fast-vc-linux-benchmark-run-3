feeff23d5456c91f67be16a47376ef08f6ace889
Tero Kristo <t-kristo@ti.com>
2014-11-18 14:59:36 +0200

    ARM: OMAP2+: PRCM: rename of_prcm_init to omap_prcm_init
    This avoids conflicts in the global namespace, and is more descriptive
    of the purpose anyway.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -765,7 +765,7 @@ int __init omap_clk_init(void)
 	ti_clk_init_features();
 
 	if (of_have_populated_dt()) {
-		ret = of_prcm_init();
+		ret = omap_prcm_init();
 		if (ret)
 			return ret;
 
--- arch/arm/mach-omap2/prm.h
+++ arch/arm/mach-omap2/prm.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void __iomem *prm_base;
 extern u16 prm_features;
 extern void omap2_set_globals_prm(void __iomem *prm);
-int of_prcm_init(void);
+int omap_prcm_init(void);
 void omap3_prcm_legacy_iomaps_init(void);
 # endif
 
