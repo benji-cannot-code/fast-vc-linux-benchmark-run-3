324f76cab8330644d5e043e2c58db3c73ba8dd15
Johan Hedberg <johan.hedberg@intel.com>
2014-11-19 13:16:41 +0200

    Bluetooth: Fix setting state back to TASK_RUNNING
    In __hci_cmd_sync_ev() and __hci_req_sync() if the hci_req_run() call
    fails and we return from the functions we should ensure that the state
    doesn't remain in TASK_INTERRUPTIBLE that we just set it to. This patch
    fixes missing calls to set_current_state(TASK_RUNNING) in both places.
    
    Reported-by: Kirill A. Shutemov <kirill@shutemov.name>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Tested-by: Kirill A. Shutemov <kirill@shutemov.name>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1129,6 +1129,7 @@ struct sk_buff *__hci_cmd_sync_ev(struct hci_dev *hdev, u16 opcode, u32 plen,
 	err = hci_req_run(&req, hci_req_sync_complete);
 	if (err < 0) {
 		remove_wait_queue(&hdev->req_wait_q, &wait);
+		set_current_state(TASK_RUNNING);
 		return ERR_PTR(err);
 	}
 
@@ -1197,6 +1198,7 @@ static int __hci_req_sync(struct hci_dev *hdev,
 		hdev->req_status = 0;
 
 		remove_wait_queue(&hdev->req_wait_q, &wait);
+		set_current_state(TASK_RUNNING);
 
 		/* ENODATA means the HCI request command queue is empty.
 		 * This can happen when a request with conditionals doesn't
