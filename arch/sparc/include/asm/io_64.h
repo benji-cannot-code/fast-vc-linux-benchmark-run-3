21e49264b102aa8e35aba760d24e9809c5563962
Arnd Bergmann <arnd@arndb.de>
2014-11-19 10:15:33 +0100

    sparc: io: remove duplicate relaxed accessors on sparc32
    Commit 1191ccb34cf8 ("sparc: io: implement dummy relaxed accessor
    macros for writes") added the relaxed accessors (readl_relaxed etc) in
    a file that is shared between sparc32 and sparc64. However, the earlier
    e1039fb42609 ("sparc32: introduce asm-generic/io.h") had already changed
    the sparc32 implementation to use asm-generic/io.h, which provides the
    same macros, resulting in lots of build errors.
    
    This moves the definitions from the shared sparc file into the
    sparc64-only file to fix the sparc32 build regression.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Fixes: 1191ccb34cf8 ("sparc: io: implement dummy relaxed accessor macros for writes")

--- arch/sparc/include/asm/io_64.h
+++ arch/sparc/include/asm/io_64.h
@@ -102,6 +102,7 @@ static inline void __raw_writeq(u64 q, const volatile void __iomem *addr)
  * the cache by using ASI_PHYS_BYPASS_EC_E_L
  */
 #define readb readb
+#define readb_relaxed readb
 static inline u8 readb(const volatile void __iomem *addr)
 {	u8 ret;
 
@@ -113,6 +114,7 @@ static inline u8 readb(const volatile void __iomem *addr)
 }
 
 #define readw readw
+#define readw_relaxed readw
 static inline u16 readw(const volatile void __iomem *addr)
 {	u16 ret;
 
@@ -125,6 +127,7 @@ static inline u16 readw(const volatile void __iomem *addr)
 }
 
 #define readl readl
+#define readl_relaxed readl
 static inline u32 readl(const volatile void __iomem *addr)
 {	u32 ret;
 
@@ -150,6 +153,7 @@ static inline u64 readq(const volatile void __iomem *addr)
 }
 
 #define writeb writeb
+#define writeb_relaxed writeb
 static inline void writeb(u8 b, volatile void __iomem *addr)
 {
 	__asm__ __volatile__("stba\t%r0, [%1] %2\t/* pci_writeb */"
@@ -159,6 +163,7 @@ static inline void writeb(u8 b, volatile void __iomem *addr)
 }
 
 #define writew writew
+#define writew_relaxed writew
 static inline void writew(u16 w, volatile void __iomem *addr)
 {
 	__asm__ __volatile__("stha\t%r0, [%1] %2\t/* pci_writew */"
@@ -168,6 +173,7 @@ static inline void writew(u16 w, volatile void __iomem *addr)
 }
 
 #define writel writel
+#define writel_relaxed writel
 static inline void writel(u32 l, volatile void __iomem *addr)
 {
 	__asm__ __volatile__("stwa\t%r0, [%1] %2\t/* pci_writel */"
