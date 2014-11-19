8d2dbf4edcacf8ebd3a1907815ee511fe51db94b
Lars-Peter Clausen <lars@metafoo.de>
2014-11-19 18:29:04 +0100

    ASoC: sigmadsp: Drop support support SIGMA_ACTION_DELAY
    The official firmware generation tool never emitted any SIGMA_ACTION_DELAY
    instructions. Keeping support for it with the new restructured loader that
    also supports v2 will be difficult, so drop support for it.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/sigmadsp.c
+++ sound/soc/codecs/sigmadsp.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/crc32.h>
-#include <linux/delay.h>
 #include <linux/firmware.h>
 #include <linux/kernel.h>
 #include <linux/i2c.h>
@@ -29,9 +28,6 @@ enum {
 	SIGMA_ACTION_WRITEXBYTES = 0,
 	SIGMA_ACTION_WRITESINGLE,
 	SIGMA_ACTION_WRITESAFELOAD,
-	SIGMA_ACTION_DELAY,
-	SIGMA_ACTION_PLLWAIT,
-	SIGMA_ACTION_NOOP,
 	SIGMA_ACTION_END,
 };
 
@@ -79,10 +75,6 @@ process_sigma_action(struct sigma_firmware *ssfw, struct sigma_action *sa)
 		ret = ssfw->write(ssfw->control_data, sa, len);
 		if (ret < 0)
 			return -EINVAL;
-		break;
-	case SIGMA_ACTION_DELAY:
-		udelay(len);
-		len = 0;
 		break;
 	case SIGMA_ACTION_END:
 		return 0;
