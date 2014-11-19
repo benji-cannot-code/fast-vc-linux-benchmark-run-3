442c30895b5437e655ade769b30c03578270d4b0
Johannes Berg <johannes.berg@intel.com>
2014-11-19 21:17:22 +0100

    NFC: Don't include linux/unaligned/access_ok.h
    This is a specific implementation, <asm/unaligned.h> is the
    multiplexer that has the arch-specific knowledge of which
    of the implementations needs to be used, so include that.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/st21nfca/i2c.c
+++ drivers/nfc/st21nfca/i2c.c
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/nfc.h>
 #include <linux/firmware.h>
-#include <linux/unaligned/access_ok.h>
 #include <linux/platform_data/st21nfca.h>
+#include <asm/unaligned.h>
 
 #include <net/nfc/hci.h>
 #include <net/nfc/llc.h>
