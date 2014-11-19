b0bf7d24243c40a0936152f8d3bb2d224efbd736
Aaro Koskinen <aaro.koskinen@iki.fi>
2014-11-19 23:52:46 +0200

    MIPS: Loongson: cs5536_pci: Add a missing include
    Add a missing include to get rid of the following sparse warnings:
    warning: symbol 'cs5536_pci_conf_write4' was not declared. Should it be static?
    warning: symbol 'cs5536_pci_conf_read4' was not declared. Should it be static?
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/8526/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/loongson/common/cs5536/cs5536_pci.c
+++ arch/mips/loongson/common/cs5536/cs5536_pci.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/types.h>
+#include <cs5536/cs5536_pci.h>
 #include <cs5536/cs5536_vsm.h>
 
 enum {
