6960ab61a8bd560cbf7e96dd94aa77166c532bea
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-11-20 15:07:32 -0700

    staging: comedi: ni_labpc_common: use subdevice readback for 'caldac'
    Use the comedi_subdevice 'readback' member and the core provided (*insn_read)
    to handle the readback of the write-only caldac subdevice. Remove the then unused
    'caldac' member from the private data.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc_common.c
+++ drivers/staging/comedi/drivers/ni_labpc_common.c
@@ -1175,7 +1175,6 @@ static int labpc_calib_insn_write(struct comedi_device *dev,
 				  struct comedi_insn *insn,
 				  unsigned int *data)
 {
-	struct labpc_private *devpriv = dev->private;
 	unsigned int chan = CR_CHAN(insn->chanspec);
 
 	/*
@@ -1185,26 +1184,11 @@ static int labpc_calib_insn_write(struct comedi_device *dev,
 	if (insn->n > 0) {
 		unsigned int val = data[insn->n - 1];
 
-		if (devpriv->caldac[chan] != val) {
+		if (s->readback[chan] != val) {
 			write_caldac(dev, chan, val);
-			devpriv->caldac[chan] = val;
+			s->readback[chan] = val;
 		}
 	}
-
-	return insn->n;
-}
-
-static int labpc_calib_insn_read(struct comedi_device *dev,
-				 struct comedi_subdevice *s,
-				 struct comedi_insn *insn,
-				 unsigned int *data)
-{
-	struct labpc_private *devpriv = dev->private;
-	unsigned int chan = CR_CHAN(insn->chanspec);
-	int i;
-
-	for (i = 0; i < insn->n; i++)
-		data[i] = devpriv->caldac[chan];
 
 	return insn->n;
 }
@@ -1346,12 +1330,15 @@ int labpc_common_attach(struct comedi_device *dev,
 		s->subdev_flags	= SDF_READABLE | SDF_WRITABLE | SDF_INTERNAL;
 		s->n_chan	= 16;
 		s->maxdata	= 0xff;
-		s->insn_read	= labpc_calib_insn_read;
 		s->insn_write	= labpc_calib_insn_write;
+
+		ret = comedi_alloc_subdev_readback(s);
+		if (ret)
+			return ret;
 
 		for (i = 0; i < s->n_chan; i++) {
 			write_caldac(dev, i, s->maxdata / 2);
-			devpriv->caldac[i] = s->maxdata / 2;
+			s->readback[i] = s->maxdata / 2;
 		}
 	} else {
 		s->type		= COMEDI_SUBD_UNUSED;
