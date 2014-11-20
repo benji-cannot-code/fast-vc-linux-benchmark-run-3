e6ebb3488154eeb25e8a2dd2c5ee1b3aa1a5fa90
Padmavathi Venna <padma.v@samsung.com>
2014-11-20 15:33:17 +0530

    ASoC: samsung: ASoC: samsung: Fix IISMOD setting in i2s_set_sysclk()
    In the i2s_set_sysclk() callback we are currently clearing all bits
    of the IISMOD register in i2s_set_sysclk. It's due to an incorrect
    mask used for the AND operation which is introduced in commit
    a5a56871f804edac93a53b5e871c0e9818fb9033 (ASoC: samsung:
    add support for exynos7 I2S controller) and also adds the missing
    break statement.
    
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Padmavathi Venna <padma.v@samsung.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/samsung/i2s.c
+++ sound/soc/samsung/i2s.c
@@ -495,7 +495,7 @@ static int i2s_set_sysclk(struct snd_soc_dai *dai,
 		if (dir == SND_SOC_CLOCK_IN)
 			mod |= 1 << i2s_regs->cdclkcon_off;
 		else
-			mod &= 0 << i2s_regs->cdclkcon_off;
+			mod &= ~(1 << i2s_regs->cdclkcon_off);
 
 		i2s->rfs = rfs;
 		break;
@@ -552,10 +552,11 @@ static int i2s_set_sysclk(struct snd_soc_dai *dai,
 		}
 
 		if (clk_id == 0)
-			mod &= 0 << i2s_regs->rclksrc_off;
+			mod &= ~(1 << i2s_regs->rclksrc_off);
 		else
 			mod |= 1 << i2s_regs->rclksrc_off;
 
+		break;
 	default:
 		dev_err(&i2s->pdev->dev, "We don't serve that!\n");
 		return -EINVAL;
