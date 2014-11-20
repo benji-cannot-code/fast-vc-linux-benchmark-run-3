30450e3cd08b1ab34fa0f871b29887b20c7452d8
Ley Foon Tan <lftan@altera.com>
2014-11-20 19:01:43 +0800

    nios2: include linux/type.h in io.h
    This is require for __iomem definition.
    
    Signed-off-by: Ley Foon Tan <lftan@altera.com>

--- arch/nios2/include/asm/io.h
+++ arch/nios2/include/asm/io.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_NIOS2_IO_H
 #define _ASM_NIOS2_IO_H
 
+#include <linux/types.h>
 #include <asm/pgtable-bits.h>
 
 /* PCI is not supported in nios2, set this to 0. */
