2fd6fc10d761f3d6ac8aaa7e4888940ac81f78b0
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-11-21 21:54:25 +0200

    drm/i915: Fix gen4 GPU reset
    On pre-ctg the reset bit directly controls the reset signal. We must
    assert it for >=20usec and then deassert it. Bit 1 is a RO status bit
    which should also go down when the reset is no longer asserted.
    
    Tested-by: Kenneth Graunke <kenneth@whitecape.org>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  GRDOM_RENDER	(1<<2)
 #define  GRDOM_MEDIA	(3<<2)
 #define  GRDOM_MASK	(3<<2)
+#define  GRDOM_RESET_STATUS (1<<1)
 #define  GRDOM_RESET_ENABLE (1<<0)
 
 #define ILK_GDSR 0x2ca4 /* MCHBAR offset */
