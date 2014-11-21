3edcb1698d856dc7b92057e41d57b2c09c578238
Oded Gabbay <oded.gabbay@amd.com>
2014-11-21 22:04:44 +0200

    amdkfd: explicitely include io.h in kfd_doorbell.c
    This patch fixes a compilation error when using certain configuration by
    including the file io.h in kfd_doorbell.c
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_doorbell.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_doorbell.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/mman.h>
 #include <linux/slab.h>
+#include <linux/io.h>
 
 /*
  * This extension supports a kernel level doorbells management for
