c1082fbf1fd81548edad5f069836fa64122375a8
Hans Verkuil <hans.verkuil@cisco.com>
2014-11-23 09:39:55 -0300

    [media] v4l2-common: move v4l2_ctrl_check to cx2341x
    The v4l2_ctrl_check() helper function is now only used in cx2341x.
    Move it there and make it static.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/media/v4l2-common.h
+++ include/media/v4l2-common.h
@@ -81,10 +81,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* ------------------------------------------------------------------------- */
 
-/* Control helper functions */
+/* Control helper function */
 
-int v4l2_ctrl_check(struct v4l2_ext_control *ctrl, struct v4l2_queryctrl *qctrl,
-		const char * const *menu_items);
 int v4l2_ctrl_query_fill(struct v4l2_queryctrl *qctrl, s32 min, s32 max, s32 step, s32 def);
 
 /* ------------------------------------------------------------------------- */
