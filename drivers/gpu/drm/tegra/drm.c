78656f83a519bcdd4e9dc136f1c93e04f3e7719a
Thierry Reding <treding@nvidia.com>
2014-11-24 17:08:06 +0100

    drm/tegra: Atomic conversion, phase 3, step 1
    Switch out the regular plane helpers for the atomic plane helpers. Also
    use the default atomic helpers to implement the ->atomic_check() and
    ->atomic_commit() callbacks. The driver now exclusively uses the atomic
    interfaces.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/drm.c
+++ drivers/gpu/drm/tegra/drm.c
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/host1x.h>
 #include <linux/iommu.h>
 
+#include <drm/drm_atomic_helper.h>
+
 #include "drm.h"
 #include "gem.h"
 
@@ -30,6 +32,8 @@ static const struct drm_mode_config_funcs tegra_drm_mode_funcs = {
 #ifdef CONFIG_DRM_TEGRA_FBDEV
 	.output_poll_changed = tegra_fb_output_poll_changed,
 #endif
+	.atomic_check = drm_atomic_helper_check,
+	.atomic_commit = drm_atomic_helper_commit,
 };
 
 static int tegra_drm_load(struct drm_device *drm, unsigned long flags)
