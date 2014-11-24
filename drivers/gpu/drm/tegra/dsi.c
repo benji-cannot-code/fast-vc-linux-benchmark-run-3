1db193928cf63bb05f48aacff40ebc999be40b36
Thierry Reding <treding@nvidia.com>
2014-11-24 16:31:48 +0100

    drm/tegra: dsi: Reset across ->exit()/->init()
    This allows a DRM driver unload/reload cycle to completely reset the DSI
    controller and may help in situations where it's broken.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/dsi.c
+++ drivers/gpu/drm/tegra/dsi.c
@@ -949,6 +949,14 @@ static int tegra_dsi_init(struct host1x_client *client)
 	struct tegra_dsi *dsi = host1x_client_to_dsi(client);
 	int err;
 
+	reset_control_deassert(dsi->rst);
+
+	err = tegra_dsi_pad_calibrate(dsi);
+	if (err < 0) {
+		dev_err(dsi->dev, "MIPI calibration failed: %d\n", err);
+		goto reset;
+	}
+
 	/* Gangsters must not register their own outputs. */
 	if (!dsi->master) {
 		dsi->output.type = TEGRA_OUTPUT_DSI;
@@ -969,6 +977,10 @@ static int tegra_dsi_init(struct host1x_client *client)
 	}
 
 	return 0;
+
+reset:
+	reset_control_assert(dsi->rst);
+	return err;
 }
 
 static int tegra_dsi_exit(struct host1x_client *client)
@@ -997,6 +1009,8 @@ static int tegra_dsi_exit(struct host1x_client *client)
 			return err;
 		}
 	}
+
+	reset_control_assert(dsi->rst);
 
 	return 0;
 }
@@ -1424,13 +1438,6 @@ static int tegra_dsi_probe(struct platform_device *pdev)
 	if (IS_ERR(dsi->rst))
 		return PTR_ERR(dsi->rst);
 
-	err = reset_control_deassert(dsi->rst);
-	if (err < 0) {
-		dev_err(&pdev->dev, "failed to bring DSI out of reset: %d\n",
-			err);
-		return err;
-	}
-
 	dsi->clk = devm_clk_get(&pdev->dev, NULL);
 	if (IS_ERR(dsi->clk)) {
 		dev_err(&pdev->dev, "cannot get DSI clock\n");
@@ -1494,12 +1501,6 @@ static int tegra_dsi_probe(struct platform_device *pdev)
 	if (IS_ERR(dsi->mipi)) {
 		err = PTR_ERR(dsi->mipi);
 		goto disable_vdd;
-	}
-
-	err = tegra_dsi_pad_calibrate(dsi);
-	if (err < 0) {
-		dev_err(dsi->dev, "MIPI calibration failed: %d\n", err);
-		goto mipi_free;
 	}
 
 	dsi->host.ops = &tegra_dsi_host_ops;
