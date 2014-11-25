cba9456e5c44395f6212fd7e668d0747df1f40ac
Thierry Reding <treding@nvidia.com>
2014-11-25 12:09:48 +0100

    drm: Make drm_atomic.h standalone includible
    This header file makes use of a bunch of structures declared in the
    drm_crtc.h header file. Include that to make sure the drm_atomic.h
    header can be included standalone.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/drm_atomic.h
+++ include/drm/drm_atomic.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DRM_ATOMIC_H_
 #define DRM_ATOMIC_H_
 
+#include <drm/drm_crtc.h>
+
 struct drm_atomic_state * __must_check
 drm_atomic_state_alloc(struct drm_device *dev);
 void drm_atomic_state_clear(struct drm_atomic_state *state);
