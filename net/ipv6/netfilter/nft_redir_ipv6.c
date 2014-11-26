1205761162acbaa4241dbc888a6c0966a43be3b7
Pablo Neira Ayuso <pablo@netfilter.org>
2014-11-26 12:46:50 +0100

    netfilter: combine IPv4 and IPv6 nf_nat_redirect code in one module
    This resolves linking problems with CONFIG_IPV6=n:
    
    net/built-in.o: In function `redirect_tg6':
    xt_REDIRECT.c:(.text+0x6d021): undefined reference to `nf_nat_redirect_ipv6'
    
    Reported-by: Andreas Ruprecht <rupran@einserver.de>
    Reported-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv6/netfilter/nft_redir_ipv6.c
+++ net/ipv6/netfilter/nft_redir_ipv6.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/netfilter/nf_tables.h>
 #include <net/netfilter/nf_nat.h>
 #include <net/netfilter/nft_redir.h>
-#include <net/netfilter/ipv6/nf_nat_redirect.h>
+#include <net/netfilter/nf_nat_redirect.h>
 
 static void nft_redir_ipv6_eval(const struct nft_expr *expr,
 				struct nft_data data[NFT_REG_MAX + 1],
