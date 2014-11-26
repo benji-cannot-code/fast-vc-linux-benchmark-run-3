80524658d38e9de83039ee40f920d020ad1847c3
Markos Chandras <markos.chandras@imgtec.com>
2014-11-26 14:08:52 +0000

    MIPS: Emulate the new MIPS R6 BOVC, BEQC and BEQZALC instructions
    MIPS R6 uses the <R6 ADDI opcode for the new BOVC, BEQC and
    BEQZALC instructions.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- arch/mips/include/uapi/asm/inst.h
+++ arch/mips/include/uapi/asm/inst.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum major_op {
 	spec_op, bcond_op, j_op, jal_op,
 	beq_op, bne_op, blez_op, bgtz_op,
-	addi_op, addiu_op, slti_op, sltiu_op,
+	addi_op, cbcond0_op = addi_op, addiu_op, slti_op, sltiu_op,
 	andi_op, ori_op, xori_op, lui_op,
 	cop0_op, cop1_op, cop2_op, cop1x_op,
 	beql_op, bnel_op, blezl_op, bgtzl_op,
