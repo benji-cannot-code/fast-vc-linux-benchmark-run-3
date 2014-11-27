fe7d8e5b8393a13d39df4fabd60f4840e6080c67
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2014-11-27 07:10:20 -0800

    Revert "staging: unisys: fix CamelCase macro names in controlframework.h"
    This reverts commit 75185f57f110a7c48d9d33a585320c93334adf0c because it
    modified the .gitignore file in the root of the tree, without saying it
    did so, which isn't acceptable.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/common-spar/include/channels/controlframework.h
+++ drivers/staging/unisys/common-spar/include/channels/controlframework.h
@@ -29,14 +29,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include "channel.h"
 
-#define ULTRA_MEMORY_COUNT_KI 1024
+#define ULTRA_MEMORY_COUNT_Ki 1024
 
 /* Scale order 0 is one 32-bit (4-byte) word (in 64 or 128-bit
  * architecture potentially 64 or 128-bit word) */
 #define ULTRA_MEMORY_PAGE_WORD 4
 
 /* Define Ki scale page to be traditional 4KB page */
-#define ULTRA_MEMORY_PAGE_KI (ULTRA_MEMORY_PAGE_WORD * ULTRA_MEMORY_COUNT_KI)
+#define ULTRA_MEMORY_PAGE_Ki (ULTRA_MEMORY_PAGE_WORD * ULTRA_MEMORY_COUNT_Ki)
 struct spar_segment_state  {
 	u16 enabled:1;		/* Bit 0: May enter other states */
 	u16 active:1;		/* Bit 1: Assigned to active partition */
