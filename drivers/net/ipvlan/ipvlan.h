aec6f58c3a747a5e5207a83ca74ff3a14aad26f0
Mahesh Bandewar <maheshb@google.com>
2014-11-26 21:13:45 -0800

    ipvlan: ipvlan depends on INET and IPV6
    This driver uses ip_out_local() and ip6_route_output() which are
    defined only if CONFIG_INET and CONFIG_IPV6 are enabled respectively.
    
    Reported-by: Jim Davis <jim.epost@gmail.com>
    Signed-off-by: Mahesh Bandewar <maheshb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ipvlan/ipvlan.h
+++ drivers/net/ipvlan/ipvlan.h
@@ -24,8 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_vlan.h>
 #include <linux/ip.h>
 #include <linux/inetdevice.h>
+#include <net/ip.h>
+#include <net/ip6_route.h>
 #include <net/rtnetlink.h>
-#include <net/gre.h>
 #include <net/route.h>
 #include <net/addrconf.h>
 
