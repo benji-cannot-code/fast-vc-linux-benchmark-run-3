59b45fe2e90f065cb16cd102fe83156f88f7b68e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-11-29 00:59:12 +0900

    ALSA: dice: Move file to its own directory
    In followed commits, dice driver is split into several files. For easily
    managing these files, this commit adds subdirectory and move file into
    the directory.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/dice/dice.c
+++ sound/firewire/dice/dice.c
@@ -27,9 +27,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/initval.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
-#include "amdtp.h"
-#include "iso-resources.h"
-#include "lib.h"
+#include "../amdtp.h"
+#include "../iso-resources.h"
+#include "../lib.h"
 #include "dice-interface.h"
 
 
