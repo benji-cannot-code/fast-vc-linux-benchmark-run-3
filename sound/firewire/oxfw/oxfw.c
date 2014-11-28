1827f0057631a4be8c360d84840c4665278d55ed
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-11-29 00:59:26 +0900

    ALSA: oxfw: Move to its own directory
    Followed commits add much codes. To make the work easy, this commit creates
    own directory and move current file to it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/oxfw/oxfw.c
+++ sound/firewire/oxfw/oxfw.c
@@ -18,10 +18,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/initval.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
-#include "cmp.h"
-#include "fcp.h"
-#include "amdtp.h"
-#include "lib.h"
+#include "../cmp.h"
+#include "../fcp.h"
+#include "../amdtp.h"
+#include "../lib.h"
 
 #define OXFORD_FIRMWARE_ID_ADDRESS	(CSR_REGISTER_BASE + 0x50000)
 /* 0x970?vvvv or 0x971?vvvv, where vvvv = firmware version */
