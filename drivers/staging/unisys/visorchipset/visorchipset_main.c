f8241d07fe4e31c0e4474a616ef6c0ccc1611f36
Julia Lawall <Julia.Lawall@lip6.fr>
2014-11-30 19:14:14 +0100

    staging: unisys: remove unneeded structure
    Delete a local structure that is only used to be initialized by memset.
    
    A semantic patch that makes this change is as follows:
    (http://coccinelle.lip6.fr/)
    
    // <smpl>
    @@
    identifier x,i;
    @@
    
    {
    ... when any
    -struct i x;
    <+... when != x
    - memset(&x,...);
    ...+>
    }
    // </smpl>
    
    Signed-off-by: Julia Lawall <Julia.Lawall@lip6.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorchipset/visorchipset_main.c
+++ drivers/staging/unisys/visorchipset/visorchipset_main.c
@@ -1941,7 +1941,6 @@ static HOSTADDRESS controlvm_get_channel_address(void)
 static void
 controlvm_periodic_work(struct work_struct *work)
 {
-	struct visorchipset_channel_info chanInfo;
 	struct controlvm_message inmsg;
 	BOOL gotACommand = FALSE;
 	BOOL handle_command_failed = FALSE;
@@ -1955,8 +1954,6 @@ controlvm_periodic_work(struct work_struct *work)
 	 */
 	if (visorchipset_clientregwait && !clientregistered)
 		goto Away;
-
-	memset(&chanInfo, 0, sizeof(struct visorchipset_channel_info));
 
 	Poll_Count++;
 	if (Poll_Count >= 250)
