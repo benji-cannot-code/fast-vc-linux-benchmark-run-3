0bf9b536daa3aebac31f4d38a0555be3bbad3f09
Markus Elfring <elfring@users.sourceforge.net>
2014-11-30 16:48:24 -0300

    [media] si2157: One function call less in si2157_init() after error
    The release_firmware() function was called in some cases by the si2157_init()
    function during error handling even if the passed variable contained still
    a null pointer. This implementation detail could be improved
    by the introduction of another jump label.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/tuners/si2157.c
+++ drivers/media/tuners/si2157.c
@@ -158,7 +158,7 @@ static int si2157_init(struct dvb_frontend *fe)
 		dev_err(&s->client->dev, "firmware file '%s' is invalid\n",
 				fw_file);
 		ret = -EINVAL;
-		goto err;
+		goto fw_release_exit;
 	}
 
 	dev_info(&s->client->dev, "downloading firmware from file '%s'\n",
@@ -174,7 +174,7 @@ static int si2157_init(struct dvb_frontend *fe)
 			dev_err(&s->client->dev,
 					"firmware download failed=%d\n",
 					ret);
-			goto err;
+			goto fw_release_exit;
 		}
 	}
 
@@ -196,9 +196,9 @@ warm:
 	s->active = true;
 	return 0;
 
-err:
+fw_release_exit:
 	release_firmware(fw);
-
+err:
 	dev_dbg(&s->client->dev, "failed=%d\n", ret);
 	return ret;
 }
