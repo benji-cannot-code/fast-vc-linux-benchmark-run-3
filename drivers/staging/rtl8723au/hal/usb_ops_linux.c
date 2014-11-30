fae4a8484b2c6deb6191e33bb6995db962849215
Jes Sorensen <Jes.Sorensen@redhat.com>
2014-11-30 16:05:09 -0500

    staging: rtl8723au: rtl8723au_read_interrupt() addr argument wasn't used
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/hal/usb_ops_linux.c
+++ drivers/staging/rtl8723au/hal/usb_ops_linux.c
@@ -318,7 +318,7 @@ urb_submit:
 	}
 }
 
-int rtl8723au_read_interrupt(struct rtw_adapter *adapter, u32 addr)
+int rtl8723au_read_interrupt(struct rtw_adapter *adapter)
 {
 	int err;
 	unsigned int pipe;
