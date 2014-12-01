340db2270b905c2dadb479b7fdedc9bfc9c8fb51
Christophe Ricard <christophe.ricard@gmail.com>
2014-12-01 19:32:52 +0100

    tpm/tpm_i2c_stm_st33: Remove reference to io_serirq
    The serirq gpio pin is used only as interrupt. After driver initialization,
    the serirq signal is always used through interrupt and never with gpio
    kernel API.
    
    The irq can then be initialized during the platform_data definition within the client->irq pin.
    
    Reviewed-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>

--- include/linux/platform_data/tpm_i2c_stm_st33.h
+++ include/linux/platform_data/tpm_i2c_stm_st33.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TPM_ST33_I2C			"st33zp24_i2c"
 
 struct st33zp24_platform_data {
-	int io_serirq;
 	int io_lpcpd;
 };
 
