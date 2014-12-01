3a3b17677850b66ed300ebe4298c1e1623cc40c1
Alexei Starovoitov <ast@plumgrid.com>
2014-12-01 15:06:36 -0800

    samples: bpf: example of stateful socket filtering
    this socket filter example does:
    - creates arraymap in kernel with key 4 bytes and value 8 bytes
    
    - loads eBPF program which assumes that packet is IPv4 and loads one byte of
      IP->proto from the packet and uses it as a key in a map
    
      r0 = skb->data[ETH_HLEN + offsetof(struct iphdr, protocol)];
      *(u32*)(fp - 4) = r0;
      value = bpf_map_lookup_elem(map_fd, fp - 4);
      if (value)
           (*(u64*)value) += 1;
    
    - attaches this program to raw socket
    
    - every second user space reads map[IPPROTO_TCP], map[IPPROTO_UDP], map[IPPROTO_ICMP]
      to see how many packets of given protocol were seen on loopback interface
    
    Usage:
    $sudo samples/bpf/sock_example
    TCP 0 UDP 0 ICMP 0 packets
    TCP 187600 UDP 0 ICMP 4 packets
    TCP 376504 UDP 0 ICMP 8 packets
    TCP 563116 UDP 0 ICMP 12 packets
    TCP 753144 UDP 0 ICMP 16 packets
    
    Signed-off-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- samples/bpf/libbpf.c
+++ samples/bpf/libbpf.c
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netlink.h>
 #include <linux/bpf.h>
 #include <errno.h>
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <linux/if_packet.h>
+#include <arpa/inet.h>
 #include "libbpf.h"
 
 static __u64 ptr_to_u64(void *ptr)
@@ -93,4 +97,28 @@ int bpf_prog_load(enum bpf_prog_type prog_type,
 	bpf_log_buf[0] = 0;
 
 	return syscall(__NR_bpf, BPF_PROG_LOAD, &attr, sizeof(attr));
+}
+
+int open_raw_sock(const char *name)
+{
+	struct sockaddr_ll sll;
+	int sock;
+
+	sock = socket(PF_PACKET, SOCK_RAW | SOCK_NONBLOCK | SOCK_CLOEXEC, htons(ETH_P_ALL));
+	if (sock < 0) {
+		printf("cannot create raw socket\n");
+		return -1;
+	}
+
+	memset(&sll, 0, sizeof(sll));
+	sll.sll_family = AF_PACKET;
+	sll.sll_ifindex = if_nametoindex(name);
+	sll.sll_protocol = htons(ETH_P_ALL);
+	if (bind(sock, (struct sockaddr *)&sll, sizeof(sll)) < 0) {
+		printf("bind to %s: %s\n", name, strerror(errno));
+		close(sock);
+		return -1;
+	}
+
+	return sock;
 }
