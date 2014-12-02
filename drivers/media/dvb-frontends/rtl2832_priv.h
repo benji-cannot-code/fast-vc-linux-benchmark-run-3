75c05d631194ab47693accde60f69761da4c5811
Antti Palosaari <crope@iki.fi>
2014-12-02 10:55:17 -0300

    [media] rtl2832: convert driver to I2C binding
    Convert that driver to I2C driver model.
    Legacy DVB binding is left also for later removal...
    
    Tested-by: Benjamin Larsson <benjamin@southpole.se>
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/rtl2832_priv.h
+++ drivers/media/dvb-frontends/rtl2832_priv.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c-mux.h>
 
 struct rtl2832_priv {
+	struct i2c_client *client;
 	struct i2c_adapter *i2c;
 	struct i2c_adapter *i2c_adapter;
 	struct i2c_adapter *i2c_adapter_tuner;
