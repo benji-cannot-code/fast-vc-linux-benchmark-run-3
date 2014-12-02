7621368790c519aa1ae74339845f3a54f3793e02
Michael Ellerman <mpe@ellerman.id.au>
2014-12-02 16:52:06 +1100

    kcmp: Move kcmp.h into uapi
    kcmp.h appears to be part of the API, it's documented in kcmp(2), and
    the selftests/kcmp code uses it. So move it to uapi so it's actually
    exported.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>

--- include/uapi/linux/kcmp.h
+++ include/uapi/linux/kcmp.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _LINUX_KCMP_H
-#define _LINUX_KCMP_H
+#ifndef _UAPI_LINUX_KCMP_H
+#define _UAPI_LINUX_KCMP_H
 
 /* Comparison type */
 enum kcmp_type {
@@ -15,4 +15,4 @@ enum kcmp_type {
 	KCMP_TYPES,
 };
 
-#endif /* _LINUX_KCMP_H */
+#endif /* _UAPI_LINUX_KCMP_H */
