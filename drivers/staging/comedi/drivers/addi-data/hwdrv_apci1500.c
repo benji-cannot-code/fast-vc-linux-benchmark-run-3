2bbb5344b46fdb08b960176726370a5ae14b4573
H Hartley Sweeten <hsweeten@visionengravers.com>
2014-12-03 11:25:41 -0700

    staging: comedi: addi_apci_1500: remove APCI1500_ADDRESS_RANGE
    This define is not used by the driver. Remove it.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi-data/hwdrv_apci1500.c
+++ drivers/staging/comedi/drivers/addi-data/hwdrv_apci1500.c
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-/* Card Specific information */
-#define APCI1500_ADDRESS_RANGE		4
-
 /* DIGITAL INPUT-OUTPUT DEFINE */
 
 #define APCI1500_AND			2
