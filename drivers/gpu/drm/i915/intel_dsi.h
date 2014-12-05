f3ff76d74b91a84c6bf2ce06085d7e6d6781f102
Gaurav K Singh <gaurav.k.singh@intel.com>
2014-12-05 14:13:41 +0530

    drm/i915: Pixel Clock changes for DSI dual link
    For dual link MIPI Panels, each port needs half of pixel clock. Pixel overlap
    can be enabled if needed by panel, then in that case, pixel clock will be
    increased for extra pixels.
    
    v2 : Address review comments by Jani
         - Removed the bit mask used for ->dual_link
         - Used DSI instead of MIPI for #define variables
    
    v3: Added the VLV_DISPLAY_BASE to VLV_CHICKEN_3 register
    
    Signed-off-by: Gaurav K Singh <gaurav.k.singh@intel.com>
    Signed-off-by: Shobhit Kumar <shobhit.kumar@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dsi.h
+++ drivers/gpu/drm/i915/intel_dsi.h
@@ -29,6 +29,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_crtc.h>
 #include "intel_drv.h"
 
+/* Dual Link support */
+#define DSI_DUAL_LINK_NONE		0
+#define DSI_DUAL_LINK_FRONT_BACK	1
+#define DSI_DUAL_LINK_PIXEL_ALT		2
+
 struct intel_dsi_device {
 	unsigned int panel_id;
 	const char *name;
@@ -106,6 +111,7 @@ struct intel_dsi {
 
 	u8 escape_clk_div;
 	u8 dual_link;
+	u8 pixel_overlap;
 	u32 port_bits;
 	u32 bw_timer;
 	u32 dphy_reg;
