560e9f879477b5e36f2a9e17e2266b4c477b123f
Benjamin Romer <benjamin.romer@unisys.com>
2014-12-05 17:09:06 -0500

    staging: unisys: fix spacing in uisqueue.c
    Correct alignment for a couple of functions and remove the space between
    a typecast and its target.
    
    Signed-off-by: Bryan Thompson <bryan.thompson@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/uislib/uisqueue.c
+++ drivers/staging/unisys/uislib/uisqueue.c
@@ -227,7 +227,7 @@ EXPORT_SYMBOL_GPL(spar_signalqueue_empty);
 
 unsigned long long
 uisqueue_interlocked_or(unsigned long long __iomem *tgt,
-		       unsigned long long set)
+			unsigned long long set)
 {
 	unsigned long long i;
 	unsigned long long j;
@@ -245,7 +245,7 @@ EXPORT_SYMBOL_GPL(uisqueue_interlocked_or);
 
 unsigned long long
 uisqueue_interlocked_and(unsigned long long __iomem *tgt,
-			unsigned long long set)
+			 unsigned long long set)
 {
 	unsigned long long i;
 	unsigned long long j;
@@ -295,7 +295,7 @@ uisqueue_put_cmdrsp_with_lock_client(struct uisqueue_info *queueinfo,
 				     char oktowait, u8 *channel_id)
 {
 	while (!do_locked_client_insert(queueinfo, whichqueue, cmdrsp,
-					(spinlock_t *) insertlock,
+					(spinlock_t *)insertlock,
 					issue_irq_if_empty,
 					irq_handle, channel_id)) {
 		if (oktowait != OK_TO_WAIT) {
