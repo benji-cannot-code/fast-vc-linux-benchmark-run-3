68b64de73d1ad14337eeaec9f7f53c27b0495fe4
Sebastian Ott <sebott@linux.vnet.ibm.com>
2014-12-05 16:47:17 +0100

    s390/scm_block: make the number of reqs per HW req configurable
    Introduce a module parameter to specify the number of requests
    we try to handle with one HW request.
    
    Suggested-by: Peter Oberparleiter <peter.oberparleiter@de.ibm.com>
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/block/scm_blk.h
+++ drivers/s390/block/scm_blk.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/eadm.h>
 
 #define SCM_NR_PARTS 8
-#define SCM_RQ_PER_IO 8
 #define SCM_QUEUE_DELAY 5
 
 struct scm_blk_dev {
@@ -33,7 +32,7 @@ struct scm_blk_dev {
 struct scm_request {
 	struct scm_blk_dev *bdev;
 	struct aidaw *next_aidaw;
-	struct request *request[SCM_RQ_PER_IO];
+	struct request **request;
 	struct aob *aob;
 	struct list_head list;
 	u8 retries;
