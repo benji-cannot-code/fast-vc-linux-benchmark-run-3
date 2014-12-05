68260152486d98a3f5193e0e38c8d742f5331e9d
Benjamin Romer <benjamin.romer@unisys.com>
2014-12-05 17:08:39 -0500

    staging: unisys: get rid of channel stub
    The functions in channels/* aren't used in a lot of places. In fact, the
    functions in channel.c can be moved to uislib/uisqueue.c, and the rest
    of the files in channels can be eliminated.
    
    This patch deletes the channels directory and files, removes it from all
    Kconfigs that referenced them, removes the reference in the Makefile,
    and moves the functions inside of channels.c to uislib/uisqueue.c.
    
    Signed-off-by: Ken Depro <kenneth.depro@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/uislib/uislib.c
+++ drivers/staging/unisys/uislib/uislib.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "sparstop.h"
 #include "visorchipset.h"
-#include "chanstub.h"
 #include "version.h"
 #include "guestlinuxdebug.h"
 
