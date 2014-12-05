8f763540adf69610fe19dafb5d12b6f85c49fa27
Benjamin Romer <benjamin.romer@unisys.com>
2014-12-05 17:08:41 -0500

    staging: unisys: add comment to spinlock in struct charqueue
    Add a comment to the charqueue's spinlock to explain that it is a lock for the
    structure.
    
    Signed-off-by: Ken Depro <kenneth.depro@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorutil/charqueue.c
+++ drivers/staging/unisys/visorutil/charqueue.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct charqueue {
 	int alloc_size;
 	int nslots;
-	spinlock_t lock;
+	spinlock_t lock; /* read/write lock for this structure */
 	int head, tail;
 	unsigned char buf[0];
 };
