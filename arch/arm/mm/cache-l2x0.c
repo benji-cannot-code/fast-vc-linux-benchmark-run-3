eee82d3d7d671cc5f7250171a555b3996acedd49
Pavel Machek <pavel@ucw.cz>
2014-12-06 19:07:28 +0100

    ARM: cache-l2x0.c: Make it clear that cache-l2x0 handles L310 cache controller
    It is not clear from the filename, and comment at the begining adds to the
    confusion by not listing L310. Fix it.
    
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- arch/arm/mm/cache-l2x0.c
+++ arch/arm/mm/cache-l2x0.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/arm/mm/cache-l2x0.c - L210/L220 cache controller support
+ * arch/arm/mm/cache-l2x0.c - L210/L220/L310 cache controller support
  *
  * Copyright (C) 2007 ARM Limited
  *
