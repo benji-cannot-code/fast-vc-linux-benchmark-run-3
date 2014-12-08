9e1c83a0bf8e6cdf986e09e03e223ab5c0b8249a
Sakari Ailus <sakari.ailus@iki.fi>
2014-12-08 17:49:31 -0300

    [media] smiapp: Don't give the source sub-device a temporary name
    The source sub-device's name will be overwritten shortly. Don't give it a
    name in the meantime.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/i2c/smiapp/smiapp-core.c
+++ drivers/media/i2c/smiapp/smiapp-core.c
@@ -2459,8 +2459,6 @@ static int smiapp_identify_module(struct v4l2_subdev *subdev)
 		minfo->name, minfo->manufacturer_id, minfo->model_id,
 		minfo->revision_number_major);
 
-	strlcpy(subdev->name, sensor->minfo.name, sizeof(subdev->name));
-
 	return 0;
 }
 
