75565190763ed5a278f8f2178dffea17e8dc37c3
Damien Lespiau <damien.lespiau@intel.com>
2014-12-08 17:33:51 +0000

    drm/i915/bdw: Fix the write setting up the WIZ hashing mode
    I was playing with clang and oh surprise! a warning trigerred by
    -Wshift-overflow (gcc doesn't have this one):
    
        WA_SET_BIT_MASKED(GEN7_GT_MODE,
                          GEN6_WIZ_HASHING_MASK | GEN6_WIZ_HASHING_16x4);
    
        drivers/gpu/drm/i915/intel_ringbuffer.c:786:2: warning: signed shift result
          (0x28002000000) requires 43 bits to represent, but 'int' only has 32 bits
          [-Wshift-overflow]
            WA_SET_BIT_MASKED(GEN7_GT_MODE,
            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        drivers/gpu/drm/i915/intel_ringbuffer.c:737:15: note: expanded from macro
          'WA_SET_BIT_MASKED'
            WA_REG(addr, _MASKED_BIT_ENABLE(mask), (mask) & 0xffff)
    
    Turned out GEN6_WIZ_HASHING_MASK was already shifted by 16, and we were
    trying to shift it a bit more.
    
    The other thing is that it's not the usual case of setting WA bits here, we
    need to have separate mask and value.
    
    To fix this, I've introduced a new _MASKED_FIELD() macro that takes both the
    (unshifted) mask and the desired value and the rest of the patch ripples
    through from it.
    
    This bug was introduced when reworking the WA emission in:
    
      Commit 7225342ab501befdb64bcec76ded41f5897c0855
      Author: Mika Kuoppala <mika.kuoppala@linux.intel.com>
      Date:   Tue Oct 7 17:21:26 2014 +0300
    
          drm/i915: Build workaround list in ring initialization
    
    v2: Invert the order of the mask and value arguments (Daniel Vetter)
        Rewrite _MASKED_BIT_ENABLE() and _MASKED_BIT_DISABLE() with
        _MASKED_FIELD() (Jani Nikula)
        Make sure we only evaluate 'a' once in _MASKED_BIT_ENABLE() (Dave Gordon)
        Add check to ensure the value is within the mask boundaries (Chris Wilson)
    
    v3: Ensure the the value and mask are 16 bits (Dave Gordon)
    
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Cc: Arun Siluvery <arun.siluvery@linux.intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -6509,7 +6509,7 @@ static void gen6_init_clock_gating(struct drm_device *dev)
 	 * to keep in mind (see 3DSTATE_PS and 3DSTATE_WM).
 	 */
 	I915_WRITE(GEN6_GT_MODE,
-		   GEN6_WIZ_HASHING_MASK | GEN6_WIZ_HASHING_16x4);
+		   _MASKED_FIELD(GEN6_WIZ_HASHING_MASK, GEN6_WIZ_HASHING_16x4));
 
 	ilk_init_lp_watermarks(dev);
 
@@ -6707,7 +6707,7 @@ static void haswell_init_clock_gating(struct drm_device *dev)
 	 * to keep in mind (see 3DSTATE_PS and 3DSTATE_WM).
 	 */
 	I915_WRITE(GEN7_GT_MODE,
-		   GEN6_WIZ_HASHING_MASK | GEN6_WIZ_HASHING_16x4);
+		   _MASKED_FIELD(GEN6_WIZ_HASHING_MASK, GEN6_WIZ_HASHING_16x4));
 
 	/* WaSwitchSolVfFArbitrationPriority:hsw */
 	I915_WRITE(GAM_ECOCHK, I915_READ(GAM_ECOCHK) | HSW_ECOCHK_ARB_PRIO_SOL);
@@ -6804,7 +6804,7 @@ static void ivybridge_init_clock_gating(struct drm_device *dev)
 	 * to keep in mind (see 3DSTATE_PS and 3DSTATE_WM).
 	 */
 	I915_WRITE(GEN7_GT_MODE,
-		   GEN6_WIZ_HASHING_MASK | GEN6_WIZ_HASHING_16x4);
+		   _MASKED_FIELD(GEN6_WIZ_HASHING_MASK, GEN6_WIZ_HASHING_16x4));
 
 	snpcr = I915_READ(GEN6_MBCUNIT_SNPCR);
 	snpcr &= ~GEN6_MBC_SNPCR_MASK;
