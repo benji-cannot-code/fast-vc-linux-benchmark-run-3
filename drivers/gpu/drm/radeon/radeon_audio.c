5adf301eb5b9b7187564593b92e947a648e45cd0
Slava Grigorev <slava.grigorev@amd.com>
2014-12-09 16:44:18 -0500

    radeon/audio: moved audio packet programming to a separate function
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Slava Grigorev <slava.grigorev@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_audio.c
+++ drivers/gpu/drm/radeon/radeon_audio.c
@@ -90,6 +90,9 @@ void r600_set_vbi_packet(struct drm_encoder *encoder, u32 offset);
 void dce4_set_vbi_packet(struct drm_encoder *encoder, u32 offset);
 void dce4_hdmi_set_color_depth(struct drm_encoder *encoder,
 	u32 offset, int bpc);
+void r600_set_audio_packet(struct drm_encoder *encoder, u32 offset);
+void dce3_2_set_audio_packet(struct drm_encoder *encoder, u32 offset);
+void dce4_set_audio_packet(struct drm_encoder *encoder, u32 offset);
 
 static const u32 pin_offsets[7] =
 {
@@ -143,6 +146,7 @@ static struct radeon_audio_funcs r600_hdmi_funcs = {
 	.update_acr = r600_hdmi_update_acr,
 	.set_vbi_packet = r600_set_vbi_packet,
 	.set_avi_packet = r600_set_avi_packet,
+	.set_audio_packet = r600_set_audio_packet,
 };
 
 static struct radeon_audio_funcs dce32_hdmi_funcs = {
@@ -153,6 +157,7 @@ static struct radeon_audio_funcs dce32_hdmi_funcs = {
 	.update_acr = dce3_2_hdmi_update_acr,
 	.set_vbi_packet = r600_set_vbi_packet,
 	.set_avi_packet = r600_set_avi_packet,
+	.set_audio_packet = dce3_2_set_audio_packet,
 };
 
 static struct radeon_audio_funcs dce32_dp_funcs = {
@@ -173,6 +178,7 @@ static struct radeon_audio_funcs dce4_hdmi_funcs = {
 	.set_vbi_packet = dce4_set_vbi_packet,
 	.set_color_depth = dce4_hdmi_set_color_depth,
 	.set_avi_packet = evergreen_set_avi_packet,
+	.set_audio_packet = dce4_set_audio_packet,
 };
 
 static struct radeon_audio_funcs dce4_dp_funcs = {
@@ -195,6 +201,7 @@ static struct radeon_audio_funcs dce6_hdmi_funcs = {
 	.set_vbi_packet = dce4_set_vbi_packet,
 	.set_color_depth = dce4_hdmi_set_color_depth,
 	.set_avi_packet = evergreen_set_avi_packet,
+	.set_audio_packet = dce4_set_audio_packet,
 };
 
 static struct radeon_audio_funcs dce6_dp_funcs = {
@@ -617,4 +624,16 @@ void radeon_hdmi_set_color_depth(struct drm_encoder *encoder)
 
 	if (radeon_encoder->audio && radeon_encoder->audio->set_color_depth)
 		radeon_encoder->audio->set_color_depth(encoder, dig->afmt->offset, bpc);
+}
+
+void radeon_audio_set_audio_packet(struct drm_encoder *encoder)
+{
+    struct radeon_encoder *radeon_encoder = to_radeon_encoder(encoder);
+    struct radeon_encoder_atom_dig *dig = radeon_encoder->enc_priv;
+
+	if (!dig || !dig->afmt)
+		return;
+
+	if (radeon_encoder->audio && radeon_encoder->audio->set_audio_packet)
+		radeon_encoder->audio->set_audio_packet(encoder, dig->afmt->offset);
 }
