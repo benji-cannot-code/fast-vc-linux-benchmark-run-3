077fbb59f680ae408db28f1bbbbb809ecf65f8da
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2014-12-09 09:10:44 +0800

    ASoC: Intel: fix sparse non static symbol warnings
    Fixes the following sparse warnings:
    
    sound/soc/intel/sst/sst_acpi.c:248:5: warning:
     symbol 'sst_acpi_probe' was not declared. Should it be static?
    sound/soc/intel/sst/sst_acpi.c:335:5: warning:
     symbol 'sst_acpi_remove' was not declared. Should it be static?
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/sst/sst_acpi.c
+++ sound/soc/intel/sst/sst_acpi.c
@@ -246,7 +246,7 @@ static struct sst_machines *sst_acpi_find_machine(
 	return NULL;
 }
 
-int sst_acpi_probe(struct platform_device *pdev)
+static int sst_acpi_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	int ret = 0;
@@ -333,7 +333,7 @@ do_sst_cleanup:
 * This function is called by OS when a device is unloaded
 * This frees the interrupt etc
 */
-int sst_acpi_remove(struct platform_device *pdev)
+static int sst_acpi_remove(struct platform_device *pdev)
 {
 	struct intel_sst_drv *ctx;
 
