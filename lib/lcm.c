56928e6ccf3896654bb1e91c24364c6f23f55b82
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2014-12-10 15:51:27 -0800

    lib/lcm.c: ensure correct result whenever it fits
    Ensure that lcm(a,b) returns the mathematically correct result, provided
    it fits in an unsigned long.  The current version returns garbage if a*b
    overflows, even if the final result would fit.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/lcm.c
+++ lib/lcm.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 unsigned long lcm(unsigned long a, unsigned long b)
 {
 	if (a && b)
-		return (a * b) / gcd(a, b);
+		return (a / gcd(a, b)) * b;
 	else if (b)
 		return b;
 
