872c5dc6c457f48b502d333a3ee5242ff37c2a87
Brad Volkin <bradley.d.volkin@intel.com>
2014-12-11 12:13:11 -0800

    drm/i915: Mark shadow batch buffers as purgeable
    By adding a new exec_entry flag, we cleanly mark the shadow objects
    as purgeable after they are on the active list.
    
    v2:
    - Move 'shadow_batch_obj->madv = I915_MADV_WILLNEED' inside _get
      fnc (danvet, from v4 6/7 feedback)
    
    v3:
    - Remove duplicate 'madv = I915_MADV_WILLNEED' (danvet, from v6 4/5)
    
    Issue: VIZ-4719
    Signed-off-by: Brad Volkin <bradley.d.volkin@intel.com>
    Reviewed-By: Jon Bloomfield <jon.bloomfield@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/i915_gem_execbuffer.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  __EXEC_OBJECT_HAS_FENCE (1<<30)
 #define  __EXEC_OBJECT_NEEDS_MAP (1<<29)
 #define  __EXEC_OBJECT_NEEDS_BIAS (1<<28)
+#define  __EXEC_OBJECT_PURGEABLE (1<<27)
 
 #define BATCH_OFFSET_BIAS (256*1024)
 
@@ -227,7 +228,12 @@ i915_gem_execbuffer_unreserve_vma(struct i915_vma *vma)
 	if (entry->flags & __EXEC_OBJECT_HAS_PIN)
 		vma->pin_count--;
 
-	entry->flags &= ~(__EXEC_OBJECT_HAS_FENCE | __EXEC_OBJECT_HAS_PIN);
+	if (entry->flags & __EXEC_OBJECT_PURGEABLE)
+		obj->madv = I915_MADV_DONTNEED;
+
+	entry->flags &= ~(__EXEC_OBJECT_HAS_FENCE |
+			  __EXEC_OBJECT_HAS_PIN |
+			  __EXEC_OBJECT_PURGEABLE);
 }
 
 static void eb_destroy(struct eb_vmas *eb)
@@ -1437,6 +1443,7 @@ i915_gem_do_execbuffer(struct drm_device *dev, void *data,
 
 			vma = i915_gem_obj_to_ggtt(shadow_batch_obj);
 			vma->exec_entry = &shadow_exec_entry;
+			vma->exec_entry->flags = __EXEC_OBJECT_PURGEABLE;
 			drm_gem_object_reference(&shadow_batch_obj->base);
 			list_add_tail(&vma->exec_list, &eb->vmas);
 
