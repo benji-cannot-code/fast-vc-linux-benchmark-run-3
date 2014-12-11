d591d1a24b5d133eef1b6b5cf17b59f7feba849f
Greg Kroah-Hartman <greg@kroah.com>
2014-12-11 17:10:55 -0500

    greybus: interface_block: rename module.[c|h] to interface_block.[c|h]
    "modules" in the driver model here, are really "interface blocks" as
    that is what they are physically tied to.  So rename the files before we
    start changing the code to make it obvious what is going on.
    
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "greybus_id.h"
 #include "greybus_manifest.h"
 #include "manifest.h"
-#include "module.h"
+#include "interface_block.h"
 #include "interface.h"
 #include "connection.h"
 #include "protocol.h"
