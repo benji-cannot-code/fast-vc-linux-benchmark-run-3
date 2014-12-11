4fd9f00ec4ff350b09e3ca174c19ce995039239d
Michael S. Tsirkin <mst@redhat.com>
2014-12-11 14:13:16 +0200

    virtio_pci_common.h: drop VIRTIO_PCI_NO_LEGACY
    Legacy drivers use virtio_pci_common.h too, we should not
    define VIRTIO_PCI_NO_LEGACY there.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- drivers/virtio/virtio_pci_common.h
+++ drivers/virtio/virtio_pci_common.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/virtio.h>
 #include <linux/virtio_config.h>
 #include <linux/virtio_ring.h>
-#define VIRTIO_PCI_NO_LEGACY
 #include <linux/virtio_pci.h>
 #include <linux/highmem.h>
 #include <linux/spinlock.h>
