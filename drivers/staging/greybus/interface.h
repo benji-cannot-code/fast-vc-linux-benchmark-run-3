5881ef8bb3e55476d44c8558ae2eae602aeb95af
Greg Kroah-Hartman <greg@kroah.com>
2014-12-11 17:10:56 -0500

    greybus: interface_block: s/gb_module/gb_interface_block/
    Rename struct gb_module to struct gb_interface_block
    
    It's a complex rename, some functions got their name changed where
    needed, but primarily this change is focused on the structure and where
    it is used.  Future changes will clean up the remaining usages of the
    term "module" in individual changes, this one spanned the whole
    subsystem so do it all at once.
    
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/interface.h
+++ drivers/staging/greybus/interface.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct gb_interface {
 	struct device		dev;
-	struct gb_module	*gmod;
+	struct gb_interface_block	*gb_ib;
 	u8			id;
 	u8			device_id;
 	struct list_head	connections;
@@ -23,11 +23,11 @@ struct gb_interface {
 };
 #define to_gb_interface(d) container_of(d, struct gb_interface, dev)
 
-struct gb_interface *gb_interface_create(struct gb_module *gmod, u8 module_id);
-void gb_interface_destroy(struct gb_module *gmod);
-int gb_interface_init(struct gb_module *gmod, u8 module_id, u8 device_id);
+struct gb_interface *gb_interface_create(struct gb_interface_block *gb_ib, u8 module_id);
+void gb_interface_destroy(struct gb_interface_block *gb_ib);
+int gb_interface_init(struct gb_interface_block *gb_ib, u8 module_id, u8 device_id);
 
-struct gb_interface *gb_interface_find(struct gb_module *gmod, u8 interface_id);
+struct gb_interface *gb_interface_find(struct gb_interface_block *gb_ib, u8 interface_id);
 
 int gb_interface_connections_init(struct gb_interface *interface);
 void gb_interface_connections_exit(struct gb_interface *interface);
