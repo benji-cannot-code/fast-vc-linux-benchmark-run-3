7b81855db0e28acedeaec17130d80eee08e1cb53
Greg Kroah-Hartman <greg@kroah.com>
2014-12-11 17:10:52 -0500

    greybus: uart-gb.c: don't include module.h
    No need to specifically include the greybus module.h here, greybus.h
    already does so and we will be renaming it soon.
    
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/uart-gb.c
+++ drivers/staging/greybus/uart-gb.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kdev_t.h>
 
 #include "greybus.h"
-#include "module.h"
 
 #define GB_NUM_MINORS	255	/* 255 is enough for anyone... */
 #define GB_NAME		"ttyGB"
