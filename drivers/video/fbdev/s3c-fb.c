629f4f4be94c29c2c915ac3b5b9db22601f1dfc4
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2014-12-13 00:42:04 +0100

    video / PM: Replace CONFIG_PM_RUNTIME with CONFIG_PM
    After commit b2b49ccbdd54 (PM: Kconfig: Set PM_RUNTIME if PM_SLEEP is
    selected) PM_RUNTIME is always set if PM is set, so #ifdef blocks
    depending on CONFIG_PM_RUNTIME may now be changed to depend on
    CONFIG_PM.
    
    The alternative of CONFIG_PM_SLEEP and CONFIG_PM_RUNTIME may be
    replaced with CONFIG_PM too.
    
    Make these changes in 2 files under drivers/video/.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Acked-by: Jingoo Han <jg1.han@samsung.com>

--- drivers/video/fbdev/s3c-fb.c
+++ drivers/video/fbdev/s3c-fb.c
@@ -1631,7 +1631,7 @@ static int s3c_fb_resume(struct device *dev)
 }
 #endif
 
-#ifdef CONFIG_PM_RUNTIME
+#ifdef CONFIG_PM
 static int s3c_fb_runtime_suspend(struct device *dev)
 {
 	struct s3c_fb *sfb = dev_get_drvdata(dev);
