b9a01b58f5c4397cd00749781df9179d31631091
Antti Palosaari <crope@iki.fi>
2014-12-14 10:05:49 -0300

    [media] rtl2832: wrap DVBv5 CNR to DVBv3 SNR
    Change legacy DVBv3 read SNR to return values calculated by DVBv5
    statistics.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/rtl2832_priv.h
+++ drivers/media/dvb-frontends/rtl2832_priv.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rtl2832.h"
 #include <linux/i2c-mux.h>
 #include <linux/regmap.h>
+#include <linux/math64.h>
 
 struct rtl2832_dev {
 	struct rtl2832_platform_data *pdata;
