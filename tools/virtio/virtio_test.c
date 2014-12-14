1a9ad35778c7794ecbe2b330a7b3ca7cb4179d02
Michael S. Tsirkin <mst@redhat.com>
2014-12-14 22:49:16 +0200

    tools/virtio: add virtio 1.0 in virtio_test
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tools/virtio/virtio_test.c
+++ tools/virtio/virtio_test.c
@@ -229,6 +229,14 @@ const struct option longopts[] = {
 		.val = 'i',
 	},
 	{
+		.name = "virtio-1",
+		.val = '1',
+	},
+	{
+		.name = "no-virtio-1",
+		.val = '0',
+	},
+	{
 		.name = "delayed-interrupt",
 		.val = 'D',
 	},
@@ -245,6 +253,7 @@ static void help(void)
 	fprintf(stderr, "Usage: virtio_test [--help]"
 		" [--no-indirect]"
 		" [--no-event-idx]"
+		" [--no-virtio-1]"
 		" [--delayed-interrupt]"
 		"\n");
 }
@@ -253,7 +262,7 @@ int main(int argc, char **argv)
 {
 	struct vdev_info dev;
 	unsigned long long features = (1ULL << VIRTIO_RING_F_INDIRECT_DESC) |
-		(1ULL << VIRTIO_RING_F_EVENT_IDX);
+		(1ULL << VIRTIO_RING_F_EVENT_IDX) | (1ULL << VIRTIO_F_VERSION_1);
 	int o;
 	bool delayed = false;
 
@@ -273,6 +282,9 @@ int main(int argc, char **argv)
 			goto done;
 		case 'i':
 			features &= ~(1ULL << VIRTIO_RING_F_INDIRECT_DESC);
+			break;
+		case '0':
+			features &= ~(1ULL << VIRTIO_F_VERSION_1);
 			break;
 		case 'D':
 			delayed = true;
