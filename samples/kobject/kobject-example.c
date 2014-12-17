2503bd89bf6616e5d9b06be2ef363a258808a02f
Rastislav Barlik <barlik@zoho.com>
2014-12-17 21:14:48 +0100

    samples/kobject: Use kstrtoint instead of sscanf
    Use kstrtoint function instead of sscanf and check for return values.
    
    Signed-off-by: Rastislav Barlik <barlik@zoho.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- samples/kobject/kobject-example.c
+++ samples/kobject/kobject-example.c
@@ -37,7 +37,12 @@ static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr,
 static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,
 			 const char *buf, size_t count)
 {
-	sscanf(buf, "%du", &foo);
+	int ret;
+
+	ret = kstrtoint(buf, 10, &foo);
+	if (ret < 0)
+		return ret;
+
 	return count;
 }
 
@@ -64,9 +69,12 @@ static ssize_t b_show(struct kobject *kobj, struct kobj_attribute *attr,
 static ssize_t b_store(struct kobject *kobj, struct kobj_attribute *attr,
 		       const char *buf, size_t count)
 {
-	int var;
+	int var, ret;
 
-	sscanf(buf, "%du", &var);
+	ret = kstrtoint(buf, 10, &var);
+	if (ret < 0)
+		return ret;
+
 	if (strcmp(attr->attr.name, "baz") == 0)
 		baz = var;
 	else
