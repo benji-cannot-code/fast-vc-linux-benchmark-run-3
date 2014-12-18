5af947f5e9a1dfabab46817b8e2c1c790dcfe7df
Andrey Gusakov <andrey.gusakov@cogentembedded.com>
2014-12-18 23:43:03 +0300

    ARM: shmobile: r8a7791: add MLB+ clock
    Add MLB+ clock to R8A7791 device tree.
    
    Signed-off-by: Andrey Gusakov <andrey.gusakov@cogentembedded.com>
    [Sergei: rebased, renamed, added changelog]
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- include/dt-bindings/clock/r8a7791-clock.h
+++ include/dt-bindings/clock/r8a7791-clock.h
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* MSTP8 */
 #define R8A7791_CLK_IPMMU_SGX		0
+#define R8A7791_CLK_MLB			2
 #define R8A7791_CLK_VIN2		9
 #define R8A7791_CLK_VIN1		10
 #define R8A7791_CLK_VIN0		11
