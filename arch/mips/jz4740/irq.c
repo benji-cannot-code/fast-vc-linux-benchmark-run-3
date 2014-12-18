2f2fbc31653ffc25061826bc22dcf5a283db46dc
Brian Norris <computersforpeace@gmail.com>
2014-12-17 18:39:01 -0800

    MIPS: JZ4740: Fixup #include's (sparse)
    Fixes sparse warnings:
    
      arch/mips/jz4740/irq.c:63:6: warning: symbol 'jz4740_irq_suspend' was not declared. Should it be static?
      arch/mips/jz4740/irq.c:69:6: warning: symbol 'jz4740_irq_resume' was not declared. Should it be static?
    
    Also, I've seen some elusive build errors on my automated build test
    where JZ4740_IRQ_BASE and NR_IRQS are missing, but I can't reproduce
    them manually for some reason. Anyway, mach-jz4740/irq.h should help us
    avoid relying on some implicit include.
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/8724/
    Acked-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/jz4740/irq.c
+++ arch/mips/jz4740/irq.c
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq_cpu.h>
 
 #include <asm/mach-jz4740/base.h>
+#include <asm/mach-jz4740/irq.h>
+
+#include "irq.h"
 
 static void __iomem *jz_intc_base;
 
