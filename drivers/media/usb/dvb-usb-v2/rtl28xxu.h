927ef3c9f393f5916e753300f291512049879891
Antti Palosaari <crope@iki.fi>
2014-12-18 08:05:23 -0300

    [media] rtl28xxu: rename state variable 'priv' to 'dev'
    I prefer dev over priv and I want keep all my drivers in line with
    that.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/dvb-usb-v2/rtl28xxu.h
+++ drivers/media/usb/dvb-usb-v2/rtl28xxu.h
@@ -69,7 +69,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CMD_I2C_DA_WR    0x0610
 
 
-struct rtl28xxu_priv {
+struct rtl28xxu_dev {
 	u8 chip_id;
 	u8 tuner;
 	char *tuner_name;
