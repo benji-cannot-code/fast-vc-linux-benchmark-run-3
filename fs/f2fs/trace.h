69ff12abbf0188e004812b3d7999ef68c3f5142a
Jaegeuk Kim <jaegeuk@kernel.org>
2014-12-17 19:51:57 -0800

    f2fs: add key functions for f2fs_io_tracer
    This patch adds two key functions to trace process ids and IOs.
    The basic idea is to
    1. remain process ids, pids, in page->private.
    2. show pids in IO traces.
    
    So, later we can retrieve process information according to IO traces.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/trace.h
+++ fs/f2fs/trace.h
@@ -15,6 +15,24 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_F2FS_IO_TRACE
 #include <trace/events/f2fs.h>
 
+enum file_type {
+	__NORMAL_FILE,
+	__DIR_FILE,
+	__NODE_FILE,
+	__META_FILE,
+	__ATOMIC_FILE,
+	__VOLATILE_FILE,
+	__MISC_FILE,
+};
+
+struct last_io_info {
+	int major, minor;
+	pid_t pid;
+	enum file_type type;
+	struct f2fs_io_info fio;
+	block_t len;
+};
+
 extern void f2fs_trace_pid(struct page *);
 extern void f2fs_trace_ios(struct page *, struct f2fs_io_info *, int);
 #else
