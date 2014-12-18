815256c6fe704caa59602f49bfb3d1d5664cf33e
Thierry Reding <treding@nvidia.com>
2014-12-18 17:11:47 +0100

    drm/panel: sharp: lq101r1sx01: Remove unneeded include
    Nothing in the file needs symbols from include/linux/host1x.h, so remove
    the include.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/panel/panel-sharp-lq101r1sx01.c
+++ drivers/gpu/drm/panel/panel-sharp-lq101r1sx01.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <video/mipi_display.h>
 
-#include <linux/host1x.h>
-
 struct sharp_panel {
 	struct drm_panel base;
 	/* the datasheet refers to them as DSI-LINK1 and DSI-LINK2 */
