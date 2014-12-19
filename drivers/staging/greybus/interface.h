9798621ebc2f8d99c7acbcdda85dd8e26ca41511
Greg Kroah-Hartman <greg@kroah.com>
2014-12-19 14:56:33 -0800

    greybus: interface.h: remove unused #defines
    MAX_CPORTS_PER_MODULE and MAX_STRINGS_PER_MODULE are not used anywhere
    anymore, so remove them lest someone thing we have limits.
    
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>
    Reviewed-by: Alex Elder <elder@linaro.org>

--- drivers/staging/greybus/interface.h
+++ drivers/staging/greybus/interface.h
@@ -11,11 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __INTERFACE_H
 #define __INTERFACE_H
 
-/* Increase these values if needed */
-#define MAX_CPORTS_PER_MODULE	10
-#define MAX_STRINGS_PER_MODULE	10
-
-
 /* Greybus "public" definitions" */
 struct gb_interface {
 	struct device dev;
