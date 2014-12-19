4885bcee1c7437ef9d22838822b20b0b4826104a
Qais Yousef <qais.yousef@imgtec.com>
2014-12-19 11:38:30 +0000

    ALSA: compress_driver.h: include sound/core.h explicitly
    Fixes the following compilation error:
    
       include/sound/compress_driver.h: In function ‘snd_compr_drain_notify’:
       include/sound/compress_driver.h:177:2: error: implicit declaration of function ‘snd_BUG_ON’ [-Werror=implicit-function-declaration]
         if (snd_BUG_ON(!stream))
    
    snd_BUG_ON() is defined in sound/core.h but the file is not included explicitly,
    so include it.
    
    Signed-off-by: Qais Yousef <qais.yousef@imgtec.com>
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/compress_driver.h
+++ include/sound/compress_driver.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <linux/sched.h>
+#include <sound/core.h>
 #include <sound/compress_offload.h>
 #include <sound/asound.h>
 #include <sound/pcm.h>
