7bd46d21e0a5e0054a1cc70d4416db4dc2e754d3
Rob Ward <robert.ward114@googlemail.com>
2014-12-20 18:28:35 +0000

    drivers: char: mem: Replace usage of asm include
    Replaces the use of asm/uaccess.h with linux/uaccess.h.
    
    Signed-off-by: Rob Ward <robert.ward114@googlemail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/aio.h>
 
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #ifdef CONFIG_IA64
 # include <linux/efi.h>
