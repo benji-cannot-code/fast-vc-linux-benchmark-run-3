faa9e9e2425be36d01a7c8c03942783b773be7a7
Aaro Koskinen <aaro.koskinen@iki.fi>
2014-12-21 22:53:58 +0200

    MIPS: OCTEON: add crypto helper functions
    Add crypto helper functions which are needed for kernel level usage.
    The code for these has been extracted from the EdgeRouter Pro GPL tarball.
    
    While at it, also delete duplicate definitions of the functions.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

---
+++ arch/mips/cavium-octeon/crypto/octeon-crypto.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License. See the file "COPYING" in the main directory of this archive
+ * for more details.
+ *
+ * Copyright (C) 2012-2013 Cavium Inc., All Rights Reserved.
+ */
+#ifndef __LINUX_OCTEON_CRYPTO_H
+#define __LINUX_OCTEON_CRYPTO_H
+
+#include <linux/sched.h>
+
+extern unsigned long octeon_crypto_enable(struct octeon_cop2_state *state);
+extern void octeon_crypto_disable(struct octeon_cop2_state *state,
+				  unsigned long flags);
+
+#endif /* __LINUX_OCTEON_CRYPTO_H */
