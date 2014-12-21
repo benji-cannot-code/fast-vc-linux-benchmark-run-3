6b82123bcb2015fd8b22a96f6c1d68f4dc6a61b4
Aaro Koskinen <aaro.koskinen@iki.fi>
2014-12-21 22:54:01 +0200

    crypto: octeon - add MD5 module
    Add OCTEON MD5 module.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/mips/cavium-octeon/crypto/octeon-crypto.h
+++ arch/mips/cavium-octeon/crypto/octeon-crypto.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <asm/mipsregs.h>
 
+#define OCTEON_CR_OPCODE_PRIORITY 300
+
 extern unsigned long octeon_crypto_enable(struct octeon_cop2_state *state);
 extern void octeon_crypto_disable(struct octeon_cop2_state *state,
 				  unsigned long flags);
