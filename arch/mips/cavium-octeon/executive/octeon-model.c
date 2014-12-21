3c1975627877560b05b7f9c713d375092e36cabd
Aaro Koskinen <aaro.koskinen@iki.fi>
2014-12-21 22:54:00 +0200

    MIPS: OCTEON: reintroduce crypto features check
    Reintroduce run-time check for crypto features. The old one was deleted
    because it was unreliable, now decide the crypto availability on early
    boot when the model string is constructed.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/mips/cavium-octeon/executive/octeon-model.c
+++ arch/mips/cavium-octeon/executive/octeon-model.c
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/octeon/octeon.h>
 
+enum octeon_feature_bits __octeon_feature_bits __read_mostly;
+EXPORT_SYMBOL_GPL(__octeon_feature_bits);
+
 /**
  * Read a byte of fuse data
  * @byte_addr:	 address to read
@@ -103,6 +106,9 @@ static const char *__init octeon_model_get_string_buffer(uint32_t chip_id,
 		suffix = "EXP";
 	else
 		suffix = "NSP";
+
+	if (!fus_dat2.s.nocrypto)
+		__octeon_feature_bits |= OCTEON_HAS_CRYPTO;
 
 	/*
 	 * Assume pass number is encoded using <5:3><2:0>. Exceptions
