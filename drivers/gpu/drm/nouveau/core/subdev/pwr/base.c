8fddf4b9f7fbdc46e19645af468c9eaa0629f4f3
Vince Hsu <vinceh@nvidia.com>
2014-12-22 17:11:38 +0800

    drm/nouveau/pwr: make nouveau_pwr_pgob() non-static
    The platform device does not use the common nouveau_pwr_init() to initialize
    the PWR, but it does need the .pgob() be assigned to avoid NULL pointer
    dereference in graph/nve4.c.
    
    Signed-off-by: Vince Hsu <vinceh@nvidia.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/core/subdev/pwr/base.c
+++ drivers/gpu/drm/nouveau/core/subdev/pwr/base.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "priv.h"
 
-static void
+void
 nouveau_pwr_pgob(struct nouveau_pwr *ppwr, bool enable)
 {
 	const struct nvkm_pwr_impl *impl = (void *)nv_oclass(ppwr);
