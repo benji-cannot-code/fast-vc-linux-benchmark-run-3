4e438bf4e7f9e5e499a0f3383ff8691fb8501387
Micky Ching <micky_ching@realsil.com.cn>
2014-12-23 09:19:43 +0800

    mmc: rtsx: init cookie at probe/card_event
    host->cookie is used for handle async request,
    we should init it to negative value when new card inserted,
    make cookie value invalid.
    
    Signed-off-by: Micky Ching <micky_ching@realsil.com.cn>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/rtsx_pci_sdmmc.c
+++ drivers/mmc/host/rtsx_pci_sdmmc.c
@@ -1322,6 +1322,7 @@ static void rtsx_pci_sdmmc_card_event(struct platform_device *pdev)
 {
 	struct realtek_pci_sdmmc *host = platform_get_drvdata(pdev);
 
+	host->cookie = -1;
 	mmc_detect_change(host->mmc, 0);
 }
 
@@ -1354,6 +1355,7 @@ static int rtsx_pci_sdmmc_drv_probe(struct platform_device *pdev)
 	host->pcr = pcr;
 	host->mmc = mmc;
 	host->pdev = pdev;
+	host->cookie = -1;
 	host->power_state = SDMMC_POWER_OFF;
 	INIT_WORK(&host->work, sd_request);
 	platform_set_drvdata(pdev, host);
