b95e56367ce153850bf2dcfb93d9ab5c02f85ac2
Daniel Baluta <daniel.baluta@intel.com>
2014-12-23 15:22:29 +0200

    iio: imu: kmx61: Use false instead of 0 for ev_enable_state
    Signed-off-by: Daniel Baluta <daniel.baluta@intel.com>
    Acked-by: Hartmut Knaack <knaack.h@gmx.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/imu/kmx61.c
+++ drivers/iio/imu/kmx61.c
@@ -973,7 +973,7 @@ static int kmx61_write_event_config(struct iio_dev *indio_dev,
 	mutex_lock(&data->lock);
 
 	if (!state && data->motion_trig_on) {
-		data->ev_enable_state = 0;
+		data->ev_enable_state = false;
 		goto err_unlock;
 	}
 
