585808dfda01d4ca45413139f79ea144f76abf96
Greg Kroah-Hartman <greg@kroah.com>
2014-12-23 15:16:54 -0800

    greybus: i2c-gb: split out into a stand-alone kernel module.
    This splits the i2c-gb protocol into a stand-alone kernel module.
    
    It's not going to stay in this fashion for long, this was done to test
    the "can a protcol be loaded later" logic.  Future refactoring is going
    to move the gpbridge protocols to a separate kernel module, where this
    protocol is going to live.
    
    But for now, split it out, it is good to test with, and shows a bug in
    gbsim at the moment.
    
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/protocol.h
+++ drivers/staging/greybus/protocol.h
@@ -54,9 +54,6 @@ extern void gb_battery_protocol_exit(void);
 extern int gb_gpio_protocol_init(void);
 extern void gb_gpio_protocol_exit(void);
 
-extern int gb_i2c_protocol_init(void);
-extern void gb_i2c_protocol_exit(void);
-
 extern int gb_pwm_protocol_init(void);
 extern void gb_pwm_protocol_exit(void);
 
@@ -74,5 +71,17 @@ extern void gb_usb_protocol_exit(void);
 
 bool gb_protocol_init(void);
 void gb_protocol_exit(void);
+
+#define gb_protocol_driver(__protocol)			\
+static int __init protocol_init(void)			\
+{							\
+	return gb_protocol_register(__protocol);	\
+}							\
+module_init(protocol_init);				\
+static void __exit protocol_exit(void)			\
+{							\
+	gb_protocol_deregister(__protocol);		\
+}							\
+module_exit(protocol_exit);
 
 #endif /* __PROTOCOL_H */
