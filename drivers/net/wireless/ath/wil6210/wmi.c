8e06441724c08c834f11f1f8c7115f5d48a8fd6d
Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
2014-12-23 09:47:13 +0200

    wil6210: Increase number of associated stations
    Change default to support maximum number of associated
    stations to an AP
    
    Signed-off-by: Hamad Kadmany <qca_hkadmany@qca.qualcomm.com>
    Signed-off-by: Vladimir Kondratiev <qca_vkondrat@qca.qualcomm.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/wmi.c
+++ drivers/net/wireless/ath/wil6210/wmi.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wmi.h"
 #include "trace.h"
 
-static uint max_assoc_sta = 1;
+static uint max_assoc_sta = WIL6210_MAX_CID;
 module_param(max_assoc_sta, uint, S_IRUGO | S_IWUSR);
 MODULE_PARM_DESC(max_assoc_sta, " Max number of stations associated to the AP");
 
