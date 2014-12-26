62b636e9732be7462e1d23436233d1abfed8cae6
Markus Pargmann <mpa@pengutronix.de>
2014-12-26 12:41:34 +0100

    batman-adv: iv_ogm, Fix dup_status comment
    Signed-off-by: Markus Pargmann <mpa@pengutronix.de>
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

--- net/batman-adv/bat_iv_ogm.c
+++ net/batman-adv/bat_iv_ogm.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * enum batadv_dup_status - duplicate status
- * @BATADV_NO_DUP: the packet is a duplicate
+ * @BATADV_NO_DUP: the packet is no duplicate
  * @BATADV_ORIG_DUP: OGM is a duplicate in the originator (but not for the
  *  neighbor)
  * @BATADV_NEIGH_DUP: OGM is a duplicate for the neighbor
