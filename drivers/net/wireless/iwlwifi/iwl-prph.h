be33f7d0c85928e241ffaaa869807c8059ff883e
Avri Altman <avri.altman@intel.com>
2014-12-27 09:09:47 +0200

    iwlwifi: mvm: Enable Rx Checksum hw
    TCP software implementation on the host requires extensive computing
    power.  Offloading even some of the TCP/IP stack to the NIC might save
    a significant overhead. In order to enable this feature on our hw,
    we need to configure it first. Once done, we mark this capability,
    to be advertised later to the OS via ieee80211_register_hw.
    The driver Rx indications for TCP Checksum is integrated within the
    standard Rx status. The driver responds to those indications as follows:
    If the frame was tested by hw and checksum ok report CHECKSUM_UNNECESSARY.
    Otherwise, report CHECKSUM_NONE.
    
    Signed-off-by: Avri Altman <avri.altman@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-prph.h
+++ drivers/net/wireless/iwlwifi/iwl-prph.h
@@ -293,6 +293,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*********************** END TX SCHEDULER *************************************/
 
+/* tcp checksum offload */
+#define RX_EN_CSUM		(0x00a00d88)
+
 /* Oscillator clock */
 #define OSC_CLK				(0xa04068)
 #define OSC_CLK_FORCE_CONTROL		(0x8)
