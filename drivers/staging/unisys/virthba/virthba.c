2130d4991fa5520c1b78798273500eb3c3a1e895
Syed Amer Gilani <syed.amer@gilani.eu>
2014-12-27 21:57:43 +0100

    staging: unisys: fix checkpatch space prohibited before comma errors
    Fixes checkpatch Error:
    ERROR: space prohibited before that ',' (ctx:WxW)
    
    Signed-off-by: Syed Amer Gilani <syed.amer@gilani.eu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/virthba/virthba.c
+++ drivers/staging/unisys/virthba/virthba.c
@@ -1439,7 +1439,7 @@ static ssize_t enable_ints_write(struct file *file,
 		return -EFAULT;
 	}
 
-	i = kstrtoint(buf, 10 , &new_value);
+	i = kstrtoint(buf, 10, &new_value);
 
 	if (i != 0) {
 		LOGERR("Failed to scan value for enable_ints, buf<<%.*s>>",
