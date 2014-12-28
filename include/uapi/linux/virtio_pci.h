fba52d28e69b439eb89e0bdc1521e5091bf6fd50
Michael S. Tsirkin <mst@redhat.com>
2014-12-28 12:35:05 +0200

    virtio_pci: drop virtio_config dependency
    virtio_pci does not depend on virtio_config:
    let's not include it, users can pull it in as necessary.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/uapi/linux/virtio_pci.h
+++ include/uapi/linux/virtio_pci.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_VIRTIO_PCI_H
 #define _LINUX_VIRTIO_PCI_H
 
-#include <linux/virtio_config.h>
+#include <linux/types.h>
 
 #ifndef VIRTIO_PCI_NO_LEGACY
 
