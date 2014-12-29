77f7c033e3fff9848fa377d30d6ef91c101764dc
Fabian Frederick <fabf@skynet.be>
2014-12-29 15:32:47 +0100

    GenWQE: remove unnecessary version.h inclusion
    Based on versioncheck.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/genwqe/card_base.h
+++ drivers/misc/genwqe/card_base.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/semaphore.h>
 #include <linux/uaccess.h>
 #include <linux/io.h>
-#include <linux/version.h>
 #include <linux/debugfs.h>
 #include <linux/slab.h>
 
