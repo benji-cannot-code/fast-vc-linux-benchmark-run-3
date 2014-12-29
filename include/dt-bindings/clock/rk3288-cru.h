583a55453fccf4f62fcc609e3d150e64df37b1d4
Roger Chen <roger.chen@rock-chips.com>
2014-12-29 17:43:55 +0800

    GMAC: define clock ID used for GMAC
    changes since v2:
    1. remove SCLK_MAC_PLL
    
    Signed-off-by: Roger Chen <roger.chen@rock-chips.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/dt-bindings/clock/rk3288-cru.h
+++ include/dt-bindings/clock/rk3288-cru.h
@@ -82,6 +82,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SCLK_SDIO1_SAMPLE	120
 #define SCLK_EMMC_SAMPLE	121
 
+#define SCLK_MAC		151
+#define SCLK_MACREF_OUT		152
+
 #define DCLK_VOP0		190
 #define DCLK_VOP1		191
 
