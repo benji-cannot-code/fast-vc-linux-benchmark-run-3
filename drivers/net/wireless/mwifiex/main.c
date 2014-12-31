1347250f4284bd4d3acacc3a04fd7e058b5ffd89
Marc Yang <yangyang@marvell.com>
2014-12-31 02:36:39 -0800

    mwifiex: Adjust calling place of mwifiex_terminate_workqueue
    Workqueue needs to be flushed early when removing card, otherwise
    soft lockup issue may happen because main_process is triggered by
    interrupt while card is being removed.
    
    Signed-off-by: Marc Yang <yangyang@marvell.com>
    Signed-off-by: Cathy Luo <cluo@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/mwifiex/main.c
+++ drivers/net/wireless/mwifiex/main.c
@@ -1167,6 +1167,8 @@ int mwifiex_remove_card(struct mwifiex_adapter *adapter, struct semaphore *sem)
 
 	adapter->surprise_removed = true;
 
+	mwifiex_terminate_workqueue(adapter);
+
 	/* Stop data */
 	for (i = 0; i < adapter->priv_num; i++) {
 		priv = adapter->priv[i];
@@ -1208,8 +1210,6 @@ int mwifiex_remove_card(struct mwifiex_adapter *adapter, struct semaphore *sem)
 
 	wiphy_unregister(adapter->wiphy);
 	wiphy_free(adapter->wiphy);
-
-	mwifiex_terminate_workqueue(adapter);
 
 	/* Unregister device */
 	dev_dbg(adapter->dev, "info: unregister device\n");
