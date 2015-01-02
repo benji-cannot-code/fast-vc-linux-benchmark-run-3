69be9c5a7a873439afb238b74981a13b34051196
Oded Gabbay <oded.gabbay@amd.com>
2015-01-02 23:33:20 +0200

    drm/radeon: Use new cik_structs.h file
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon_kfd.c
+++ drivers/gpu/drm/radeon/radeon_kfd.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "radeon_kfd.h"
 #include "radeon_ucode.h"
 #include <linux/firmware.h>
+#include "cik_structs.h"
 
 #define CIK_PIPE_PER_MEC	(4)
 
