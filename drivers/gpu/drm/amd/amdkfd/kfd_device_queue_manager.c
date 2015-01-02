17d7d425e0b8e1e128ca831bade7b928a4dac12d
Oded Gabbay <oded.gabbay@amd.com>
2015-01-02 23:18:54 +0200

    drm/amdkfd: Don't include header files from radeon
    Because amdkfd will need to work both with radeon and amdgpu, don't include
    header files that are in radeon's folder.
    
    Instead, use the common amd include folder and move amdkfd specific defines to
    amdkfd header files.
    
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kfd_mqd_manager.h"
 #include "cik_regs.h"
 #include "kfd_kernel_queue.h"
-#include "../../radeon/cik_reg.h"
 
 /* Size of the per-pipe EOP queue */
 #define CIK_HPD_EOP_BYTES_LOG2 11
--- drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_mqd_manager.c
@@ -27,8 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kfd_priv.h"
 #include "kfd_mqd_manager.h"
 #include "cik_regs.h"
-#include "../../radeon/cikd.h"
-#include "../../radeon/cik_reg.h"
+#include "cik_structs.h"
 
 inline void busy_wait(unsigned long ms)
 {
