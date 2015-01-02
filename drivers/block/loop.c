f9f77238f7ee3f7faef36266a63a240f37bc2bd1
Jens Axboe <axboe@fb.com>
2015-01-02 15:20:25 -0700

    loop: add blk-mq.h include
    Looks like we pull it in through other ways on x86, but we fail
    on sparc:
    
    In file included from drivers/block/cryptoloop.c:30:0:
    drivers/block/loop.h:63:24: error: field 'tag_set' has incomplete type
    struct blk_mq_tag_set tag_set;
    
    Add the include to loop.h, kill it from loop.c.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -76,7 +76,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysfs.h>
 #include <linux/miscdevice.h>
 #include <linux/falloc.h>
-#include <linux/blk-mq.h>
 #include "loop.h"
 
 #include <asm/uaccess.h>
