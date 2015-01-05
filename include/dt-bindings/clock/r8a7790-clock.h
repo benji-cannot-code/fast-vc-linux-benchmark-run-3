0fa1d89a2f8b4ed873f1c80fec2617858933334e
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2015-01-06 00:33:25 +0300

    ARM: shmobile: r8a7790: add CAN clocks
    The R-Car CAN controllers can derive the CAN bus clock not only from their
    peripheral clock input (clkp1) but also from the other internal clock (clkp2)
    and external clock fed on CAN_CLK pin.  Describe those clocks in the device
    tree,  along with  the USB_EXTAL clock  from which clkp2 is derived.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- include/dt-bindings/clock/r8a7790-clock.h
+++ include/dt-bindings/clock/r8a7790-clock.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define R8A7790_CLK_SD0			7
 #define R8A7790_CLK_SD1			8
 #define R8A7790_CLK_Z			9
+#define R8A7790_CLK_RCAN		10
 
 /* MSTP0 */
 #define R8A7790_CLK_MSIOF0		0
