ed27086c7e85fb18fbc756517d2381a2d1b3389a
Paul Walmsley <pwalmsley@nvidia.com>
2015-01-05 17:38:41 -0700

    arm64: fix missing asm/pgtable-hwdef.h include in asm/processor.h
    On next-20150105, defconfig compilation breaks with:
    
    ./arch/arm64/include/asm/processor.h:47:32: error: ‘PHYS_MASK’ undeclared (first use in this function)
    
    Fix by including asm/pgtable-hwdef.h, where PHYS_MASK is defined.
    
    This second version incorporates a comment from Mark Rutland
    <mark.rutland@arm.com> to keep the includes in alphabetical order
    by filename.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Cc: Paul Walmsley <pwalmsley@nvidia.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/fpsimd.h>
 #include <asm/hw_breakpoint.h>
+#include <asm/pgtable-hwdef.h>
 #include <asm/ptrace.h>
 #include <asm/types.h>
 
