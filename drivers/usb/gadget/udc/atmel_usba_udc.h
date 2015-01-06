c673047781aea918f968dab8bd5301253534f654
Boris Brezillon <boris.brezillon@free-electrons.com>
2015-01-06 14:46:59 +0100

    usb: atmel_usba_udc: Add at91sam9g45 and at91sam9x5 errata handling
    at91sam9g45 and at91sam9x5 SoCs have an hardware bug forcing us to
    generate a pulse on the BIAS signal on "USB end of reset” and
    “USB end of resume" events.
    
    Acked-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Reported-by: Patrice VILCHEZ <patrice.vilchez@atmel.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/udc/atmel_usba_udc.h
+++ drivers/usb/gadget/udc/atmel_usba_udc.h
@@ -307,6 +307,7 @@ struct usba_request {
 
 struct usba_udc_errata {
 	void (*toggle_bias)(struct usba_udc *udc, int is_on);
+	void (*pulse_bias)(struct usba_udc *udc);
 };
 
 struct usba_udc {
@@ -327,6 +328,7 @@ struct usba_udc {
 	struct clk *pclk;
 	struct clk *hclk;
 	struct usba_ep *usba_ep;
+	bool bias_pulse_needed;
 
 	u16 devstatus;
 
