cfb37b3d915e149695a1f99f740359336b395aac
Jens Axboe <axboe@fb.com>
2015-01-07 10:44:04 -0700

    blk-mq: get rid of ->cmd_size in the hardware queue
    We store it in the tag set, we don't need it in the hardware queue.
    While removing cmd_size, place ->queue_num further down to avoid
    a hole on 64-bit archs. It's not used in any fast paths, so we
    can safely move it.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- include/linux/blk-mq.h
+++ include/linux/blk-mq.h
@@ -35,7 +35,6 @@ struct blk_mq_hw_ctx {
 	unsigned long		flags;		/* BLK_MQ_F_* flags */
 
 	struct request_queue	*queue;
-	unsigned int		queue_num;
 	struct blk_flush_queue	*fq;
 
 	void			*driver_data;
@@ -55,7 +54,7 @@ struct blk_mq_hw_ctx {
 	unsigned long		dispatched[BLK_MQ_MAX_DISPATCH_ORDER];
 
 	unsigned int		numa_node;
-	unsigned int		cmd_size;	/* per-request extra data */
+	unsigned int		queue_num;
 
 	atomic_t		nr_active;
 
