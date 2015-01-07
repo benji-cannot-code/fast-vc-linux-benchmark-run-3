8bcbc86d527ebef088061dda8cc413a333b545ef
Jon Medhurst <tixy@linaro.org>
2015-01-07 11:42:30 +0000

    ARM: probes: Use correct action types for MOVW, SEV and WFI
    This doesn't correct any bugs when probing these instructions but makes
    MOVW slightly faster and makes everything more symmetric with the Thumb
    instruction cases.
    
    We can also remove the now redundant PROBES_EMULATE_NONE and
    PROBES_SIMULATE_NOP actions.
    
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- arch/arm/probes/decode-arm.h
+++ arch/arm/probes/decode-arm.h
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "decode.h"
 
 enum probes_arm_action {
-	PROBES_EMULATE_NONE,
-	PROBES_SIMULATE_NOP,
 	PROBES_PRELOAD_IMM,
 	PROBES_PRELOAD_REG,
 	PROBES_BRANCH_IMM,
