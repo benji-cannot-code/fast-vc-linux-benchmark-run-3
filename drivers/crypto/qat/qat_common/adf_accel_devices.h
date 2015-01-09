7b7e3cb849c604b48283f56b1b2bab5b774e7ed8
Allan, Bruce W <bruce.w.allan@intel.com>
2015-01-09 11:55:20 -0800

    crypto: qat - remove unnecessary include of atomic.h header file
    Signed-off-by: Bruce Allan <bruce.w.allan@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_common/adf_accel_devices.h
+++ drivers/crypto/qat/qat_common/adf_accel_devices.h
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ADF_ACCEL_DEVICES_H_
 #define ADF_ACCEL_DEVICES_H_
 #include <linux/module.h>
-#include <linux/atomic.h>
 #include <linux/list.h>
 #include <linux/proc_fs.h>
 #include <linux/io.h>
