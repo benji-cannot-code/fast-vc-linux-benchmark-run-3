58d9977db3fbaf1632eb3f5df3d724ccba0126ea
Willem de Bruijn <willemb@google.com>
2015-01-10 12:08:18 -0500

    doc: fix the compile fix of txtimestamp.c
    A fix to ipv6 structure definitions removed the now superfluous
    definition of in6_pktinfo in this file.
    
    But, use of the glibc definition requires defining _GNU_SOURCE
    (see also https://sourceware.org/bugzilla/show_bug.cgi?id=6775).
    
    Before this change, the following would fail for me:
    
      make
      make headers_install
      make M=Documentation/networking/timestamping
    
    with
    
      Documentation/networking/timestamping/txtimestamp.c: In function '__recv_errmsg_cmsg':
      Documentation/networking/timestamping/txtimestamp.c:205:33: error: dereferencing pointer to incomplete type
      Documentation/networking/timestamping/txtimestamp.c:206:23: error: dereferencing pointer to incomplete type
    
    After this patch compilation succeeded.
    
    Fixes: cd91cc5bdddf ("doc: fix the compile error of txtimestamp.c")
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- Documentation/networking/timestamping/txtimestamp.c
+++ Documentation/networking/timestamping/txtimestamp.c
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#define _GNU_SOURCE
+
 #include <arpa/inet.h>
 #include <asm/types.h>
 #include <error.h>
