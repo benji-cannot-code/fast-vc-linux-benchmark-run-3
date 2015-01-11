82374af61861fcb06ba00c810f789327bb5f6e42
Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
2015-01-11 12:38:14 -0300

    [media] usb: pvrusb2: pvrusb2-hdw: Remove unused function
    Remove the function pvr2_hdw_cmd_powerdown() that is not used anywhere.
    
    This was partially found by using a static code analysis program called cppcheck.
    
    Signed-off-by: Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/pvrusb2/pvrusb2-hdw.c
+++ drivers/media/usb/pvrusb2/pvrusb2-hdw.c
@@ -4030,11 +4030,6 @@ int pvr2_hdw_cmd_powerup(struct pvr2_hdw *hdw)
 }
 
 
-int pvr2_hdw_cmd_powerdown(struct pvr2_hdw *hdw)
-{
-	return pvr2_issue_simple_cmd(hdw,FX2CMD_POWER_OFF);
-}
-
 
 int pvr2_hdw_cmd_decoder_reset(struct pvr2_hdw *hdw)
 {
