8099f9e0d5a8d5bf6b20316fbfd32096862cb5ea
Irina Tirdea <irina.tirdea@intel.com>
2015-01-11 21:10:08 +0200

    iio: core: Introduce DISTANCE channel type
    Some devices export an estimation of the distance the user has covered
    since the last reset.
    
    One of this devices is Freescale's MMA9553L
    (http://www.freescale.com/files/sensors/doc/ref_manual/MMA9553LSWRM.pdf)
    that computes the distance based on the stride length and step rate.
    
    Introduce a new channel type DISTANCE to export these values.
    
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -74,6 +74,7 @@ static const char * const iio_chan_type_name_spec[] = {
 	[IIO_ACTIVITY] = "activity",
 	[IIO_STEPS] = "steps",
 	[IIO_ENERGY] = "energy",
+	[IIO_DISTANCE] = "distance",
 };
 
 static const char * const iio_modifier_names[] = {
