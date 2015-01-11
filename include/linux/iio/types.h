bf4eda062dcbc93ff7640bee770d523ff8bddc88
Irina Tirdea <irina.tirdea@intel.com>
2015-01-11 21:10:09 +0200

    iio: core: Introduce IIO_VELOCITY and IIO_MOD_ROOT_SUM_SQUARED_X_Y_Z
    Some devices export the current speed value of the user.
    
    One of this devices is Freescale's MMA9553L
    (http://www.freescale.com/files/sensors/doc/ref_manual/MMA9553LSWRM.pdf)
    that computes the speed of the user based on the number of steps and
    stride length.
    
    Introduce a new channel type VELOCITY and a modifier for the magniture or
    norm of the velocity vector, IIO_MOD_ROOT_SUM_SQUARED_X_Y_Z.
    
    Signed-off-by: Irina Tirdea <irina.tirdea@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- include/linux/iio/types.h
+++ include/linux/iio/types.h
@@ -35,6 +35,7 @@ enum iio_chan_type {
 	IIO_STEPS,
 	IIO_ENERGY,
 	IIO_DISTANCE,
+	IIO_VELOCITY,
 };
 
 enum iio_modifier {
@@ -69,6 +70,7 @@ enum iio_modifier {
 	IIO_MOD_JOGGING,
 	IIO_MOD_WALKING,
 	IIO_MOD_STILL,
+	IIO_MOD_ROOT_SUM_SQUARED_X_Y_Z,
 };
 
 enum iio_event_type {
