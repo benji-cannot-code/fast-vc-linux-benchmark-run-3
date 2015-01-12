b452819c709ab49f1aa858a9577955f1909a7bc1
Willem de Bruijn <willemb@google.com>
2015-01-12 14:29:34 -0500

    ipv6: directly include libc-compat.h in ipv6.h
    Patch 3b50d9029809 ("ipv6: fix redefinition of in6_pktinfo ...")
    fixed a libc compatibility issue in ipv6 structure definitions
    as described in include/uapi/linux/libc-compat.h.
    
    It relies on including linux/in6.h to include libc-compat.h itself.
    Include that file directly to clearly communicate the dependency
    (libc-compat.h: "This include must be as early as possible").
    
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    
    ----
    
    As discussed in http://patchwork.ozlabs.org/patch/427384/
    Acked-by: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/ipv6.h
+++ include/uapi/linux/ipv6.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _UAPI_IPV6_H
 #define _UAPI_IPV6_H
 
+#include <linux/libc-compat.h>
 #include <linux/types.h>
 #include <linux/in6.h>
 #include <asm/byteorder.h>
