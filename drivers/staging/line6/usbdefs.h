8dc072c62107fe6f781a2e09faf649facd2d6252
Chris Rorvick <chris@rorvick.com>
2015-01-12 12:42:45 -0800

    staging: line6: Rename capability macros
    Including "BIT" in the macro name is pointless.  Replace with "CAP" to
    provide some context for what its value represents.
    
    Signed-off-by: Chris Rorvick <chris@rorvick.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- drivers/staging/line6/usbdefs.h
+++ drivers/staging/line6/usbdefs.h
@@ -16,15 +16,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_INTERVALS_PER_SECOND 1000
 
 /* device supports settings parameter via USB */
-#define LINE6_BIT_CONTROL (1 << 0)
+#define LINE6_CAP_CONTROL (1 << 0)
 /* device supports PCM input/output via USB */
-#define LINE6_BIT_PCM (1 << 1)
+#define LINE6_CAP_PCM (1 << 1)
 /* device support hardware monitoring */
-#define LINE6_BIT_HWMON (1 << 2)
+#define LINE6_CAP_HWMON (1 << 2)
 
-#define LINE6_BIT_CTRL_PCM_HW	(LINE6_BIT_CONTROL |	\
-					 LINE6_BIT_PCM |	\
-					 LINE6_BIT_HWMON)
+#define LINE6_CAP_CTRL_PCM_HW	(LINE6_CAP_CONTROL |	\
+					 LINE6_CAP_PCM |	\
+					 LINE6_CAP_HWMON)
 
 #define LINE6_FALLBACK_INTERVAL 10
 #define LINE6_FALLBACK_MAXPACKETSIZE 16
