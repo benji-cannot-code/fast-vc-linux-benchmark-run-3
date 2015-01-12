05fbd0c08aa119fb58770ba9cd9814e0bd75cea8
Chris Rorvick <chris@rorvick.com>
2015-01-12 12:42:59 -0800

    staging: line6: Call *_disconnect() via pointer
    Which *_disconnect() to call on disconnect is known at initialization.
    Add a function pointer to the `usb_line6' struct and use to call into
    the appropriate logic instead of evaluating the conditional logic.
    
    Signed-off-by: Chris Rorvick <chris@rorvick.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- drivers/staging/line6/podhd.c
+++ drivers/staging/line6/podhd.c
@@ -99,6 +99,8 @@ static int podhd_try_init(struct usb_interface *interface,
 	if ((interface == NULL) || (podhd == NULL))
 		return -ENODEV;
 
+	line6->disconnect = line6_podhd_disconnect;
+
 	/* initialize audio system: */
 	err = line6_init_audio(line6);
 	if (err < 0)
