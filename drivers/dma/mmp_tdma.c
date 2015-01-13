d1941023cb943e6f8036043160ac14b1624becc5
Arnd Bergmann <arnd@arndb.de>
2015-01-13 14:25:19 +0100

    dmaengine: mmp-tdma: don't include mach/regs-icu.h
    The mmp tdma driver does not actually require this header, and
    we want to enable multiplatform support for MMP, which would
    make it inaccessible and cause a build error.
    
    This patch just removes the old #include.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/mmp_tdma.c
+++ drivers/dma/mmp_tdma.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmaengine.h>
 #include <linux/platform_device.h>
 #include <linux/device.h>
-#include <mach/regs-icu.h>
 #include <linux/platform_data/dma-mmp_tdma.h>
 #include <linux/of_device.h>
 #include <linux/of_dma.h>
