0122875ed185ac7b4b9db205f0801c2f597be84d
Christophe Ricard <christophe.ricard@gmail.com>
2015-01-13 23:13:13 +0100

    tpm/tpm_i2c_stm_st33: Change tpm_i2c_stm_st33.h to tpm_stm_st33.h
    include/linux/platform_data/tpm_i2c_stm_st33.h can be used by other st33
    tpm device driver not using i2c protocol.
    
    Reviewed-by: Jason Gunthorpe <jason.gunthorpe@obsidianresearch.com>
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Reviewed-by: Peter Huewe <peterhuewe@gmx.de>
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>

--- drivers/char/tpm/tpm_i2c_stm_st33.c
+++ drivers/char/tpm/tpm_i2c_stm_st33.c
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/of_gpio.h>
 
-#include <linux/platform_data/tpm_i2c_stm_st33.h>
+#include <linux/platform_data/tpm_stm_st33.h>
 #include "tpm.h"
 
 #define TPM_ACCESS			0x0
