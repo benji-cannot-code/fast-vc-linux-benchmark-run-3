a39c8af4f2ef2a4fea71a3ccd09a641050b8c4c7
Tony Lindgren <tony@atomide.com>
2015-01-13 14:23:26 -0800

    irqchip: omap-intc: Remove unused legacy interface for omap2
    Nowadays omap2 is booting in device tree only mode so there is no
    need to keep the legacy interface around for omap2_init_irq().
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Reviewed-by: Felipe Balbi <balbi@ti.com>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Link: http://lkml.kernel.org/r/1421187806-6804-3-git-send-email-tony@atomide.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/irqchip/irq-omap-intc.h
+++ include/linux/irqchip/irq-omap-intc.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __INCLUDE_LINUX_IRQCHIP_IRQ_OMAP_INTC_H
 #define __INCLUDE_LINUX_IRQCHIP_IRQ_OMAP_INTC_H
 
-void omap2_init_irq(void);
 void omap3_init_irq(void);
 
 int omap_irq_pending(void);
