573a92c33414a183105763fd86a5ddd29b6cb214
Maxime Ripard <maxime.ripard@free-electrons.com>
2015-01-13 21:16:23 +0100

    dmaengine: s3c24xx: Fix typo
    A typo has been introduced in the spin_unlock_irqrestore function. Fix it.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/s3c24xx-dma.c
+++ drivers/dma/s3c24xx-dma.c
@@ -407,7 +407,7 @@ static int s3c24xx_dma_set_runtime_config(struct dma_chan *chan,
 	s3cchan->cfg = *config;
 
 out:
-	spin_lock_irqrestore(&s3cchan->vc.lock, flags);
+	spin_unlock_irqrestore(&s3cchan->vc.lock, flags);
 	return ret;
 }
 
