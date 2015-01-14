2a1c2c9d2b15407e4c855e1e03260deea2cbea14
Ben Skeggs <bskeggs@redhat.com>
2015-01-14 16:58:51 +1000

    drm/nouveau/core: split device index enum out on its own
    To avoid having to include core/device.h where it's not otherwise
    required.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/core/subdev.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/subdev.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NOUVEAU_SUBDEV_H__
 
 #include <core/object.h>
+#include <core/devidx.h>
 
 #define NV_SUBDEV_(sub,var) (NV_SUBDEV_CLASS | ((var) << 8) | (sub))
 #define NV_SUBDEV(name,var)  NV_SUBDEV_(NVDEV_SUBDEV_##name, (var))
