5f6cdb707073d214c14f5321136c9afc7e6bcdc1
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-01-14 10:05:10 -0700

    staging: comedi: ni_labpc_isadma: use comedi_isdma module
    The labpc driver has already had the depends on ISA_DMA_API limitation removed
    by moving all the DMA support code into the labpc_isadma module.
    
    For aesthetics, use the comedi_isadma module to provide the actual ISA DMA
    support.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc_isadma.h
+++ drivers/staging/comedi/drivers/ni_labpc_isadma.h
@@ -6,16 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _NI_LABPC_ISADMA_H
 #define _NI_LABPC_ISADMA_H
 
-#define NI_LABPC_HAVE_ISA_DMA	IS_ENABLED(CONFIG_COMEDI_NI_LABPC_ISADMA)
-
-#if NI_LABPC_HAVE_ISA_DMA
-
-static inline bool labpc_have_dma_chan(struct comedi_device *dev)
-{
-	struct labpc_private *devpriv = dev->private;
-
-	return (bool)devpriv->dma_desc.chan;
-}
+#if IS_ENABLED(CONFIG_COMEDI_NI_LABPC_ISADMA)
 
 void labpc_init_dma_chan(struct comedi_device *dev, unsigned int dma_chan);
 void labpc_free_dma_chan(struct comedi_device *dev);
@@ -24,11 +15,6 @@ void labpc_drain_dma(struct comedi_device *dev);
 void labpc_handle_dma_status(struct comedi_device *dev);
 
 #else
-
-static inline bool labpc_have_dma_chan(struct comedi_device *dev)
-{
-	return false;
-}
 
 static inline void labpc_init_dma_chan(struct comedi_device *dev,
 				       unsigned int dma_chan)
