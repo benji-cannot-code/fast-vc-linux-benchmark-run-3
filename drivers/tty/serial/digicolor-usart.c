1fe643b58e9ae45785ee838e6f2303f7780db0fb
Baruch Siach <baruch@tkos.co.il>
2015-01-14 08:04:21 +0200

    tty/serial: digicolor: remove sysrq reference
    The Digicolor USART hardware does not support detecting the BREAK condition.
    This means that we can't support sysrq on this hardware. Remove all reference
    to sysrq from the code.
    
    This also fixes build when sysrq is disabled:
    
    drivers/tty/serial/digicolor-usart.c: In function 'digicolor_uart_console_write':
    drivers/tty/serial/digicolor-usart.c:407:33: error: 'struct uart_port' has no member named 'sysrq'
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/digicolor-usart.c
+++ drivers/tty/serial/digicolor-usart.c
@@ -171,9 +171,6 @@ static void digicolor_uart_rx(struct uart_port *port)
 				ch_flag = TTY_OVERRUN;
 		}
 
-		if (uart_handle_sysrq_char(port, ch))
-			continue;
-
 		if (status & port->ignore_status_mask)
 			continue;
 
@@ -405,7 +402,7 @@ static void digicolor_uart_console_write(struct console *co, const char *c,
 	unsigned long flags;
 	int locked = 1;
 
-	if (port->sysrq || oops_in_progress)
+	if (oops_in_progress)
 		locked = spin_trylock_irqsave(&port->lock, flags);
 	else
 		spin_lock_irqsave(&port->lock, flags);
