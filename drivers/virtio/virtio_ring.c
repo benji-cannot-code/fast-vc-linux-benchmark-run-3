f9e0ba1673dcd17fcf98582b8dc725f36a8c176d
Michael S. Tsirkin <mst@redhat.com>
2015-01-15 13:33:31 +0200

    virtio_ring: coding style fix
    Most of our code has
    struct foo {
    }
    
    Fix one instances where ring is inconsistent.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -55,8 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define END_USE(vq)
 #endif
 
-struct vring_virtqueue
-{
+struct vring_virtqueue {
 	struct virtqueue vq;
 
 	/* Actual memory layout for this queue */
