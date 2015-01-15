3d59ecbb3e909dd34fb58f5931598a52d9551115
Ondrej Zary <linux@rainbow-software.org>
2015-01-15 17:10:45 -0300

    [media] bttv: Convert to generic TEA575x interface
    Remove tea575x-specific code from bttv and use the common driver instead.
    
    Only set_frequency is implemented (signal/stereo detection or seek would
    require more changes to bttv).
    
    It works fine on Video Highway Xtreme (it actually makes the radio usable as
    it currently cannot be tuned properly).
    
    Miro/Pinnacle is untested but seems to be simple and should work.
    
    However, I don't understand the Terratec Active Radio Upgrade code. The HW
    seems to need IOR, IOW and CSEL signals that were taken from ISA bus on
    older cards (IOR and IOW directly and CSEL from some address decoder) and
    are emulated here using GPIOs. But the code manipulating these signals in
    bttv seems to be broken - it never asserts the IOR signal. If anyone has
    this HW, please test if I got that right.
    
    Signed-off-by: Ondrej Zary <linux@rainbow-software.org>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/pci/bt8xx/bttvp.h
+++ drivers/media/pci/bt8xx/bttvp.h
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/tveeprom.h>
 #include <media/rc-core.h>
 #include <media/ir-kbd-i2c.h>
+#include <media/tea575x.h>
 
 #include "bt848.h"
 #include "bttv.h"
@@ -360,6 +361,10 @@ struct bttv_suspend_state {
 	struct bttv_buffer     *vbi;
 };
 
+struct bttv_tea575x_gpio {
+	u8 data, clk, wren, most;
+};
+
 struct bttv {
 	struct bttv_core c;
 
@@ -446,12 +451,9 @@ struct bttv {
 
 	/* miro/pinnacle + Aimslab VHX
 	   philips matchbox (tea5757 radio tuner) support */
-	int has_matchbox;
-	int mbox_we;
-	int mbox_data;
-	int mbox_clk;
-	int mbox_most;
-	int mbox_mask;
+	int has_tea575x;
+	struct bttv_tea575x_gpio tea_gpio;
+	struct snd_tea575x tea;
 
 	/* ISA stuff (Terratec Active Radio Upgrade) */
 	int mbox_ior;
