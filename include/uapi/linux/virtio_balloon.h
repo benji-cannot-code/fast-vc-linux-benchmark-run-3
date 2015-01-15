6950742bcace760c0dcba502130682437c5fe99e
Michael S. Tsirkin <mst@redhat.com>
2015-01-15 13:33:31 +0200

    virtio_balloon: coding style fixes
    Most of our code has
    struct foo {
    }
    
    Fix two instances where balloon is inconsistent.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/uapi/linux/virtio_balloon.h
+++ include/uapi/linux/virtio_balloon.h
@@ -37,8 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Size of a PFN in the balloon interface. */
 #define VIRTIO_BALLOON_PFN_SHIFT 12
 
-struct virtio_balloon_config
-{
+struct virtio_balloon_config {
 	/* Number of pages host wants Guest to give up. */
 	__le32 num_pages;
 	/* Number of pages we've actually got in balloon. */
