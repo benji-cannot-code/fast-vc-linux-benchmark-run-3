0b7262d9a61c51149178b57ca86b11b83ff8c705
Tang Yuantian <Yuantian.Tang@freescale.com>
2015-01-15 14:03:41 +0800

    clk: ppc-corenet: rename driver to clk-qoriq
    Freescale introduced new ARM-based socs which using the compatible
    clock IP block with PowerPC-based socs'. So this driver can be used
    on both platforms.
    Updated relevant descriptions and renamed this driver to better
    represent its meaning and keep the function of driver untouched.
    
    Signed-off-by: Tang Yuantian <Yuantian.Tang@freescale.com>
    Signed-off-by: Michael Turquette <mturquette@linaro.org>

--- drivers/clk/clk-qoriq.c
+++ drivers/clk/clk-qoriq.c
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * clock driver for Freescale PowerPC corenet SoCs.
+ * clock driver for Freescale QorIQ SoCs.
  */
 #include <linux/clk-provider.h>
 #include <linux/io.h>
@@ -167,7 +167,7 @@ static void __init core_pll_init(struct device_node *np)
 
 	base = of_iomap(np, 0);
 	if (!base) {
-		pr_err("clk-ppc: iomap error\n");
+		pr_err("clk-qoriq: iomap error\n");
 		return;
 	}
 
@@ -261,7 +261,7 @@ static void __init sysclk_init(struct device_node *node)
 	u32 rate;
 
 	if (!np) {
-		pr_err("ppc-clk: could not get parent node\n");
+		pr_err("qoriq-clk: could not get parent node\n");
 		return;
 	}
 
