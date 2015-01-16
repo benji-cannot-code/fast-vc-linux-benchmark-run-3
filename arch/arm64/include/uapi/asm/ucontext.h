f9787828ca220ee623293ef1cb9abc5a5f3aeb32
Will Deacon <will.deacon@arm.com>
2015-01-16 13:52:14 +0000

    arm64: uapi: expose our struct ucontext to the uapi headers
    arm64 defines its own ucontext structure which is incompatible with the
    struct defined (and exposed to userspace by) the asm-generic headers.
    
    glibc carries its own struct definition that is compatible with the
    arm64 definition, but we should expose our format in the uapi headers in
    case other libraries want to make use of the ucontext pushed as part of
    an arm64 sigframe.
    
    This patch moves the arm64 asm/ucontext.h to the uapi headers, along
    with the necessary #include of linux/types.h.
    
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Marcus Shawcroft <marcus.shawcroft@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/uapi/asm/ucontext.h
+++ arch/arm64/include/uapi/asm/ucontext.h
@@ -14,8 +14,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef __ASM_UCONTEXT_H
-#define __ASM_UCONTEXT_H
+#ifndef _UAPI__ASM_UCONTEXT_H
+#define _UAPI__ASM_UCONTEXT_H
+
+#include <linux/types.h>
 
 struct ucontext {
 	unsigned long	  uc_flags;
@@ -28,4 +30,4 @@ struct ucontext {
 	struct sigcontext uc_mcontext;
 };
 
-#endif /* __ASM_UCONTEXT_H */
+#endif /* _UAPI__ASM_UCONTEXT_H */
