80fccdb06fdc55f7f1686823ef41eb58055083a2
Jani Nikula <jani.nikula@intel.com>
2015-01-16 14:27:27 +0200

    drm/i915/dsi: remove intel_dsi_cmd.c and the unused functions therein
    The removed functions can be resurrected in intel_dsi.c as need arises.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-By: Shobhit Kumar <shobhit.kumar@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dsi_panel_vbt.c
+++ drivers/gpu/drm/i915/intel_dsi_panel_vbt.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_drv.h"
 #include "intel_drv.h"
 #include "intel_dsi.h"
-#include "intel_dsi_cmd.h"
 
 struct vbt_panel {
 	struct drm_panel panel;
