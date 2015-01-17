c4524530d880e2db06d3f8e9a43e9f940e520e3c
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2015-01-17 19:09:26 +0200

    drm: omapdrm: Apply settings synchronously
    The omapdrm driver implements a mechanism to apply new settings (due to
    plane update, plane disable, plane property set, CRTC mode set or CRTC
    DPMS) asynchronously. While this improves performance, it adds a level
    of complexity that makes transition to the atomic update API close to
    impossible. Furthermore the atomic update API requires part of the apply
    operations to be synchronous (such as pinning the framebuffers), so the
    current implementation needs to be changed.
    
    Simplify the CRTC and plane code by making updates synchronous to
    prepare for the switch to the atomic update API. Asynchronous update
    will be implemented in a second step.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/omap_drv.c
+++ drivers/gpu/drm/omapdrm/omap_drv.c
@@ -551,7 +551,6 @@ static int dev_load(struct drm_device *dev, unsigned long flags)
 static int dev_unload(struct drm_device *dev)
 {
 	struct omap_drm_private *priv = dev->dev_private;
-	int i;
 
 	DBG("unload: dev=%p", dev);
 
@@ -559,10 +558,6 @@ static int dev_unload(struct drm_device *dev)
 
 	if (priv->fbdev)
 		omap_fbdev_free(dev);
-
-	/* flush crtcs so the fbs get released */
-	for (i = 0; i < priv->num_crtcs; i++)
-		omap_crtc_flush(priv->crtcs[i]);
 
 	omap_modeset_free(dev);
 	omap_gem_deinit(dev);
