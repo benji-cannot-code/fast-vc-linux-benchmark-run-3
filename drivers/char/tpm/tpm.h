e224f3b39e15eaf58b2b1fe3906b3a191cfb314a
Bruno E O Meneguele <bmeneguele@gmail.com>
2015-01-17 17:03:30 +0100

    char/tpm: fixed white spaces coding style issues
    Fixed some coding style issues reported by checkpatch.
    
    Signed-off-by: Bruno E O Meneguele <bmeneguele@gmail.com>
    [phuewe: ported to latest code]
    Signed-off-by: Peter Huewe <peterhuewe@gmx.de>

--- drivers/char/tpm/tpm.h
+++ drivers/char/tpm/tpm.h
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Maintained by: <tpmdd-devel@lists.sourceforge.net>
  *
  * Device driver for TCG/TCPA TPM (trusted platform module).
- * Specifications at www.trustedcomputinggroup.org	 
+ * Specifications at www.trustedcomputinggroup.org
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, version 2 of the
  * License.
- * 
+ *
  */
 #include <linux/module.h>
 #include <linux/delay.h>
@@ -142,7 +142,7 @@ struct tpm_vendor_specific {
 	u16 manufacturer_id;
 };
 
-#define TPM_VPRIV(c)	(c)->vendor.priv
+#define TPM_VPRIV(c)     ((c)->vendor.priv)
 
 #define TPM_VID_INTEL    0x8086
 #define TPM_VID_WINBOND  0x1050
