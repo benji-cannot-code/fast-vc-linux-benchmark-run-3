58f45ffd56430c4964e81d395f075558ac8821ef
Eric Anholt <eric@anholt.net>
2015-01-18 09:33:17 +1300

    drm/vc4: Add create and map BO ioctls.
    While there exist dumb APIs for creating and mapping BOs, one of the
    rules is that drivers doing 3D acceleration have to provide their own
    APIs for buffer allocation (besides, the pitch/height parameters of
    the dumb alloc don't really make sense for a lot of 3D allocations).
    
    v2: Use __u32-style types, use "drm.h" instead of <drm/drm.h>.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/vc4/vc4_drv.c
+++ drivers/gpu/drm/vc4/vc4_drv.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include "drm_fb_cma_helper.h"
 
+#include "uapi/drm/vc4_drm.h"
 #include "vc4_drv.h"
 #include "vc4_regs.h"
 
@@ -74,6 +75,8 @@ static const struct file_operations vc4_drm_fops = {
 };
 
 static const struct drm_ioctl_desc vc4_drm_ioctls[] = {
+	DRM_IOCTL_DEF_DRV(VC4_CREATE_BO, vc4_create_bo_ioctl, 0),
+	DRM_IOCTL_DEF_DRV(VC4_MMAP_BO, vc4_mmap_bo_ioctl, 0),
 };
 
 static struct drm_driver vc4_drm_driver = {
