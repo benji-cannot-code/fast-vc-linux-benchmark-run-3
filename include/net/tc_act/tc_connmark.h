ec8a886bc4150143fcd1c87ce268a918a442d19f
Felix Fietkau <nbd@openwrt.org>
2015-01-18 16:35:14 -0500

    net: sched: Introduce connmark action
    This tc action allows you to retrieve the connection tracking mark
    This action has been used heavily by openwrt for a few years now.
    
    There are known limitations currently:
    
    doesn't work for initial packets, since we only query the ct table.
      Fine given use case is for returning packets
    
    no implicit defrag.
      frags should be rare so fix later..
    
    won't work for more complex tasks, e.g. lookup of other extensions
      since we have no means to store results
    
    we still have a 2nd lookup later on via normal conntrack path.
    This shouldn't break anything though since skb->nfct isn't altered.
    
    V2:
    remove unnecessary braces (Jiri)
    change the action identifier to 14 (Jiri)
    Fix some stylistic issues caught by checkpatch
    V3:
    Move module params to bottom (Cong)
    Get rid of tcf_hashinfo_init and friends and conform to newer API (Cong)
    
    Acked-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/net/tc_act/tc_connmark.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __NET_TC_CONNMARK_H
+#define __NET_TC_CONNMARK_H
+
+#include <net/act_api.h>
+
+struct tcf_connmark_info {
+	struct tcf_common common;
+	u16 zone;
+};
+
+#define to_connmark(a) \
+	container_of(a->priv, struct tcf_connmark_info, common)
+
+#endif /* __NET_TC_CONNMARK_H */
