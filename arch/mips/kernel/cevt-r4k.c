1587fb0a79e620fdcd9894166f89226fd480cd06
James Hogan <james.hogan@imgtec.com>
2015-01-19 12:00:55 +0000

    MIPS: cevt-r4k: Drop GIC special case
    The cevt-r4k driver used to call into the GIC driver to find whether the
    timer was pending, but only with External Interrupt Controller (EIC)
    mode, where the Cause.IP bits can't be used as they encode the interrupt
    priority level (Cause.RIPL) instead.
    
    However commit e9de688dac65 ("irqchip: mips-gic: Support local
    interrupts") changed the condition from cpu_has_veic to gic_present.
    This fails on cores such as P5600 which have a GIC but the local
    interrupts aren't routable by the GIC, causing c0_compare_int_usable()
    to consider the interrupt unusable so r4k_clockevent_init() fails.
    
    The previous behaviour, added in commit 98b67c37db33 ("MIPS: Add EIC
    support for GIC."), wasn't really correct either as far as I can tell,
    since P5600 apparently supports EIC mode too, and in any case the use of
    Cause.TI with r2 should have been sufficient anyway since commit
    010c108d7af7 ("MIPS: PowerTV: Fix support for timer interrupts with > 64
    external IRQs").
    
    Therefore drop the call into the gic driver altogether, and add a
    comment in c0_compare_int_pending() to clarify that Cause.TI does get
    checked since MIPS r2.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Fixes: e9de688dac65 ("irqchip: mips-gic: Support local interrupts")
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Steven J. Hill <steven.hill@imgtec.com>
    Cc: Qais Yousef <qais.yousef@imgtec.com>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/9077/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/cevt-r4k.c
+++ arch/mips/kernel/cevt-r4k.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/percpu.h>
 #include <linux/smp.h>
 #include <linux/irq.h>
-#include <linux/irqchip/mips-gic.h>
 
 #include <asm/time.h>
 #include <asm/cevt-r4k.h>
@@ -86,10 +85,7 @@ void mips_event_handler(struct clock_event_device *dev)
  */
 static int c0_compare_int_pending(void)
 {
-#ifdef CONFIG_MIPS_GIC
-	if (gic_present)
-		return gic_get_timer_pending();
-#endif
+	/* When cpu_has_mips_r2, this checks Cause.TI instead of Cause.IP7 */
 	return (read_c0_cause() >> cp0_compare_irq_shift) & (1ul << CAUSEB_IP);
 }
 
