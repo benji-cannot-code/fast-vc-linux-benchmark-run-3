b891bcc5ad6a776ab8fcfc7af48676fd21de2774
Lars Ellenberg <lars.ellenberg@linbit.com>
2015-01-19 15:43:04 +0100

    drbd: drop remnants of connector -- we don't use it anymore in drbd 8.4
    Signed-off-by: Philipp Reisner <philipp.reisner@linbit.com>
    Signed-off-by: Lars Ellenberg <lars.ellenberg@linbit.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- include/linux/drbd.h
+++ include/linux/drbd.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 #ifndef DRBD_H
 #define DRBD_H
-#include <linux/connector.h>
 #include <asm/types.h>
 
 #ifdef __KERNEL__
