31f946dd6678fb79da0e342e9a84c68b43fb4ef1
Ken Depro <kenneth.depro@unisys.com>
2015-01-20 10:01:13 -0500

    staging: unisys: Fix "else not useful after return" warning in virthba.c
    This patch fixes a warning generated during the checkpatch script that stated
    "else not useful after return".  I modified the code to return a designated
    status at the end of the function, and replaced the return statement in the
    "else if" to set the status accordingly.
    
    Signed-off-by: Ken Depro <kenneth.depro@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/virthba/virthba.c
+++ drivers/staging/unisys/virthba/virthba.c
@@ -1585,6 +1585,8 @@ virthba_serverdown_complete(struct work_struct *work)
 static int
 virthba_serverdown(struct virtpci_dev *virtpcidev, u32 state)
 {
+	int stat = 1;
+
 	struct virthba_info *virthbainfo =
 	    (struct virthba_info *)((struct Scsi_Host *)virtpcidev->scsi.
 				     scsihost)->hostdata;
@@ -1599,11 +1601,12 @@ virthba_serverdown(struct virtpci_dev *virtpcidev, u32 state)
 			   &virthbainfo->serverdown_completion);
 	} else if (virthbainfo->serverchangingstate) {
 		LOGERR("Server already processing change state message\n");
-		return 0;
-	} else
+		stat = 0;
+	} else {
 		LOGERR("Server already down, but another server down message received.");
+	}
 
-	return 1;
+	return stat;
 }
 
 /*****************************************************/
