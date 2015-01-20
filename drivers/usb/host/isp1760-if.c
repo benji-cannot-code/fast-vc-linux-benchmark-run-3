6ece582357ed195b787f641656c1ee160afe3778
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2015-01-21 00:55:56 +0200

    usb: isp1760: Reorganize header files
    The isp1760-rhcd.h header contains PTD constants and structures only
    useful for the HCD implementation. It also contains register definitions
    that will be needed by common code when implementing support for the
    ISP1761 device controller, but doesn't contain the isp1760_hcd structure
    definition that will also be used by common code.
    
    Move definitions to the right location and create an isp1760-regs.h to
    store register definitions. No change other than moving definitions and
    modifying indentation is performed.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/host/isp1760-if.c
+++ drivers/usb/host/isp1760-if.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb/hcd.h>
 
 #include "isp1760-hcd.h"
+#include "isp1760-regs.h"
 
 #ifdef CONFIG_PCI
 #include <linux/pci.h>
