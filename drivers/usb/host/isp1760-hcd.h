e25c85b4a124c6bff1c29a9d24a91ed820570255
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2015-01-21 00:55:50 +0200

    usb: isp1760: Remove busname argument to isp1760_register
    The argument is unused, remove it.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/host/isp1760-hcd.h
+++ drivers/usb/host/isp1760-hcd.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* exports for if */
 int isp1760_register(phys_addr_t res_start, resource_size_t res_len, int irq,
 		     unsigned long irqflags, struct device *dev,
-		     const char *busname, unsigned int devflags);
+		     unsigned int devflags);
 void isp1760_unregister(struct device *dev);
 
 int isp1760_init_kmem_once(void);
