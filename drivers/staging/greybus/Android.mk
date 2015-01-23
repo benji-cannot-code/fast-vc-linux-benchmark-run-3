a8fc8c83e343e5223ff46fe99d90c2986e12b19f
Michael Scott <michael.scott@linaro.org>
2015-01-22 16:22:20 -0800

    greybus: build: android: replace hard-coded build destination with variable
    Make sure destination for greybus modules is consistent and easier to maintain
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/Android.mk
+++ drivers/staging/greybus/Android.mk
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 $(PRODUCT_OUT)/ramdisk.img: build-greybus
 
+GREYBUS_MODULE_OUT_PATH := $(PRODUCT_OUT)/root/lib/modules
+
 include $(CLEAR_VARS)
 GREYBUS_SRC_PATH := $(ANDROID_BUILD_TOP)/external/greybus/
 LOCAL_PATH := $(GREYBUS_SRC_PATH)
 LOCAL_SRC_FILES := greybus.ko
 LOCAL_MODULE := $(LOCAL_SRC_FILES)
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/root/lib/modules
+LOCAL_MODULE_PATH := $(GREYBUS_MODULE_OUT_PATH)
 $(LOCAL_PATH)/$(LOCAL_SRC_FILES): build-greybus
 include $(BUILD_PREBUILT)
 
@@ -27,5 +29,5 @@ build-greybus: android_kernel
 	make clean -C $(GREYBUS_SRC_PATH)
 	cd $(GREYBUS_SRC_PATH) &&\
 	$(MAKE) -j$(MAKE_JOBS) CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX) $(ARGS)
-	mkdir -p $(PRODUCT_OUT)/root/lib/modules
-	cp $(GREYBUS_SRC_PATH)/greybus.ko $(PRODUCT_OUT)/root/lib/modules
+	mkdir -p $(GREYBUS_MODULE_OUT_PATH)
+	cp $(GREYBUS_SRC_PATH)/greybus.ko $(GREYBUS_MODULE_OUT_PATH)
