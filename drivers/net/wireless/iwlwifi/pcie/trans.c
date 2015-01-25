6daec0fa10f64b75d20111f558858d8eb520ce4e
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-01-25 10:36:31 +0200

    iwlwifi: pcie: prepare the enablement of 31 TFD queues
    Some devices have 31 TFD queues. Don't enable it yet since
    there are still issues with it, but at least prepare the
    code for it. There was a bug in the read pointer assignment,
    fix that. Also, move the inline functions to iwl-scd.h which
    is the right place.
    
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwlwifi/pcie/trans.c
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-trans.h"
 #include "iwl-csr.h"
 #include "iwl-prph.h"
+#include "iwl-scd.h"
 #include "iwl-agn-hw.h"
 #include "iwl-fw-error-dump.h"
 #include "internal.h"
