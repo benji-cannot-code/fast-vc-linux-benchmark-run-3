000b458910609f8d935e733faefc22d6c8a7d956
Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
2015-01-26 11:24:36 +0200

    staging: vt6655: fix that open brace { should be on the previous line
    This patch fixes the following checkpatch.pl error:
    fix that open brace { should be on the previous line
    
    Signed-off-by: Mariam Mohamed Fawzy <maryammohamed61@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/card.c
+++ drivers/staging/vt6655/card.c
@@ -69,8 +69,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*---------------------  Static Variables  --------------------------*/
 
-static const unsigned short cwRXBCNTSFOff[MAX_RATE] =
-{17, 17, 17, 17, 34, 23, 17, 11, 8, 5, 4, 3};
+static const unsigned short cwRXBCNTSFOff[MAX_RATE] = {
+	17, 17, 17, 17, 34, 23, 17, 11, 8, 5, 4, 3};
 
 /*---------------------  Static Functions  --------------------------*/
 
