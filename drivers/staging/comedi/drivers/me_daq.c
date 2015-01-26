47ec22c16be22901e8d36615d15e4ca3a35e1461
Davidlohr Bueso <dave@stgolabs.net>
2015-01-26 02:15:02 -0800

    drivers/staging: use current->state helpers
    Call __set_current_state() instead of assigning the new state directly.
    These interfaces also aid CONFIG_DEBUG_ATOMIC_SLEEP environments,
    keeping track of who changed the state.
    
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/me_daq.c
+++ drivers/staging/comedi/drivers/me_daq.c
@@ -175,7 +175,7 @@ struct me_private_data {
 
 static inline void sleep(unsigned sec)
 {
-	current->state = TASK_INTERRUPTIBLE;
+	__set_current_state(TASK_INTERRUPTIBLE);
 	schedule_timeout(sec * HZ);
 }
 
