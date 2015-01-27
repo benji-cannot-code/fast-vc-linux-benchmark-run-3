423b1e15cdf0121b94c8d3df443910c3e6702c34
Ian Abbott <abbotti@mev.co.uk>
2015-01-27 18:16:56 +0000

    staging: comedi: comedi_compat32.c: absorb raw_ioctl()
    `comedi_compat_ioctl()` just calls static inline function `raw_ioctl()`
    with the same parameters (although the former returns a `long` and the
    latter returns an `int`).  Since `raw_ioctl()` is not called from
    anywhere else, just absorb its body into `comedi_compat_ioctl()`.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_compat32.c
+++ drivers/staging/comedi/comedi_compat32.c
@@ -411,12 +411,11 @@ static int compat_insn(struct file *file, unsigned long arg)
 }
 
 /*
- * Process untranslated ioctl.
+ * compat_ioctl file operation.
  *
  * Returns -ENOIOCTLCMD for unrecognised ioctl codes.
  */
-static inline int raw_ioctl(struct file *file, unsigned int cmd,
-			    unsigned long arg)
+long comedi_compat_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	int rc;
 
@@ -462,14 +461,4 @@ static inline int raw_ioctl(struct file *file, unsigned int cmd,
 		break;
 	}
 	return rc;
-}
-
-/*
- * compat_ioctl file operation.
- *
- * Returns -ENOIOCTLCMD for unrecognised ioctl codes.
- */
-long comedi_compat_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-{
-	return raw_ioctl(file, cmd, arg);
 }
