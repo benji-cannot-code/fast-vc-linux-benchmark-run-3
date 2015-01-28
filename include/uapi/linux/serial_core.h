300ecace1cac3940236f3af47cf5afd6b5c4a2de
Chunyan Zhang <chunyan.zhang@spreadtrum.com>
2015-01-28 19:08:44 +0800

    tty/serial: Add Spreadtrum sc9836-uart driver support
    Add a full sc9836-uart driver for SC9836 SoC which is based on the
    spreadtrum sharkl64 platform.
    This driver also support earlycon.
    
    Originally-by: Lanqing Liu <lanqing.liu@spreadtrum.com>
    Signed-off-by: Orson Zhai <orson.zhai@spreadtrum.com>
    Signed-off-by: Chunyan Zhang <chunyan.zhang@spreadtrum.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -253,4 +253,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Conexant Digicolor */
 #define PORT_DIGICOLOR	110
 
+/* SPRD SERIAL  */
+#define PORT_SPRD	111
+
 #endif /* _UAPILINUX_SERIAL_CORE_H */
