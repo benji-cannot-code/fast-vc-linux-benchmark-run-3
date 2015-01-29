d2ca8e4a33baa22f48485e52cdbcf1a9c9a6b473
Heba Aamer <heba93aamer@gmail.com>
2015-01-30 00:11:36 +0200

    staging: rtl8712: fix Prefer ether_addr_copy() over memcpy()
    This patch fixes the following checkpatch.pl warning:
    Prefer ether_addr_copy() over memcpy()
    if the Ethernet addresses are __aligned(2)
    
    pahole showed that the struct used pnetdev->dev_addr
    is aligned to u16.
    
    Moreover mac is a simple array, pdata is a pointer that
    starts from an even offset.
    
    Signed-off-by: Heba Aamer <heba93aamer@gmail.com>
    Acked-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/usb_intf.c
+++ drivers/staging/rtl8712/usb_intf.c
@@ -463,7 +463,7 @@ static int r871xu_drv_init(struct usb_interface *pusb_intf,
 				/* Use the mac address stored in the Efuse
 				 * offset = 0x12 for usb in efuse
 				 */
-				memcpy(mac, &pdata[0x12], ETH_ALEN);
+				ether_addr_copy(mac, &pdata[0x12]);
 			}
 			eeprom_CustomerID = pdata[0x52];
 			switch (eeprom_CustomerID) {
@@ -580,7 +580,7 @@ static int r871xu_drv_init(struct usb_interface *pusb_intf,
 		} else
 			dev_info(&udev->dev,
 				"r8712u: MAC Address from efuse = %pM\n", mac);
-		memcpy(pnetdev->dev_addr, mac, ETH_ALEN);
+		ether_addr_copy(pnetdev->dev_addr, mac);
 	}
 	/* step 6. Load the firmware asynchronously */
 	if (rtl871x_load_fw(padapter))
