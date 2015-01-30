44da0d3a5c2ff8a3db792a4549d3b8f0fcd4b39d
Arnd Bergmann <arnd@arndb.de>
2015-01-30 10:45:33 +0100

    ARM: w90x900: make headers more local
    Some header files are never included outside of a mach-w90x900
    directory and do not need to be made visible in include/mach,
    so let's just move them all down one level.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-w90x900/cpu.c
+++ arch/arm/mach-w90x900/cpu.c
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/hardware.h>
 #include <mach/regs-serial.h>
 #include <mach/regs-clock.h>
-#include <mach/regs-ebi.h>
-#include <mach/regs-timer.h>
+#include "regs-ebi.h"
+#include "regs-timer.h"
 
 #include "cpu.h"
 #include "clock.h"
