9adbffb2970055d68418ad79afac1feb05a32482
Arnd Bergmann <arnd@arndb.de>
2015-01-30 10:45:33 +0100

    ARM: iop13xx: make headers more local
    Some header files are never included outside of a mach-iop13xx
    directory and do not need to be made visible in include/mach,
    so let's just move them all down one level.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-iop13xx/iq81340mc.c
+++ arch/arm/mach-iop13xx/iq81340mc.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/pci.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
-#include <mach/pci.h>
+#include "pci.h"
 #include <asm/mach/time.h>
 #include <mach/time.h>
 
