45bd9171b4f0206c2964cd90b1c5fcb3a791a189
Cody P Schafer <cody@linux.vnet.ibm.com>
2015-01-30 13:46:01 -0800

    powerpc/perf/{hv-gpci, hv-common}: generate requests with counters annotated
    This adds (in req-gen/) a framework for defining gpci counter requests.
    It uses macro magic similar to ftrace.
    
    Also convert the existing hv-gpci request structures and enum values to
    use the new framework (and adjust old users of the structs and enum
    values to cope with changes in naming).
    
    In exchange for this macro disaster, we get autogenerated event listing
    for GPCI in sysfs, build time field offset checking, and zero
    duplication of information about GPCI requests.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>
    Signed-off-by: Sukadev Bhattiprolu <sukadev@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

---
+++ arch/powerpc/perf/req-gen/_request-end.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#undef REQUEST
+#undef __field
+#undef __array
+#undef __count
+
+#undef REQUEST_NAME
+#undef REQUEST_NUM
+#undef REQUEST_IDX_KIND
