802f64514ddec4efde2f9e4f914a8706418c5d15
Peter Ujfalusi <peter.ujfalusi@ti.com>
2015-02-02 14:49:42 +0200

    ASoC: davinci-evm: Do not include davinci-pcm.h and davinci-i2s.h
    There is no need for them to be included.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/davinci/davinci-evm.c
+++ sound/soc/davinci/davinci-evm.c
@@ -25,9 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/dma.h>
 #include <asm/mach-types.h>
 
-#include "davinci-pcm.h"
-#include "davinci-i2s.h"
-
 struct snd_soc_card_drvdata_davinci {
 	struct clk *mclk;
 	unsigned sysclk;
