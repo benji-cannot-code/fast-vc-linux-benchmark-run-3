4e24f5b66907da9361fcae991f554915d5fe4360
Niklas Cassel <nks@flawful.org>
2015-02-02 23:19:20 +0100

    serial: driver for ETRAX FS UART
    This is the last missing piece to get a kernel booting to a prompt in qemu-cris.
    
    Signed-off-by: Niklas Cassel <nks@flawful.org>
    Acked-by: Jesper Nilsson <jesper.nilsson@axis.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -256,4 +256,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPRD SERIAL  */
 #define PORT_SPRD	111
 
+/* Cris v10 / v32 SoC */
+#define PORT_CRIS	112
+
 #endif /* _UAPILINUX_SERIAL_CORE_H */
