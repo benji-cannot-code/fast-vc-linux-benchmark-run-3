2161c565071a43391a7eeb52e6ade7124ab015f8
Russell King <rmk+kernel@arm.linux.org.uk>
2015-02-02 11:01:08 +0000

    drm: bridge/dw_hdmi: protect n/cts setting with a mutex
    The HDMI n/cts settings need to be updated whenever the audio sample
    rate or the video pixel clock changes.  This needs to be protected
    against concurrency as there is no synchronisation between these two
    operations.  Introduce a mutex (called audio_mutex) to protect against
    two threads trying to update the video clock rate and pixel clock
    simultaneously.
    
    Acked-by: Philipp Zabel <p.zabel@pengutronix.de>
    Tested-by: Philipp Zabel <p.zabel@pengutronix.de>
    Acked-by: Andy Yan <andy.yan@rock-chips.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/gpu/drm/bridge/dw_hdmi.c
+++ drivers/gpu/drm/bridge/dw_hdmi.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/clk.h>
 #include <linux/hdmi.h>
+#include <linux/mutex.h>
 #include <linux/of_device.h>
 
 #include <drm/drm_of.h>
@@ -127,6 +128,7 @@ struct dw_hdmi {
 	struct i2c_adapter *ddc;
 	void __iomem *regs;
 
+	struct mutex audio_mutex;
 	unsigned int sample_rate;
 	int ratio;
 
@@ -358,12 +360,16 @@ static void hdmi_set_clk_regenerator(struct dw_hdmi *hdmi,
 
 static void hdmi_init_clk_regenerator(struct dw_hdmi *hdmi)
 {
+	mutex_lock(&hdmi->audio_mutex);
 	hdmi_set_clk_regenerator(hdmi, 74250000);
+	mutex_unlock(&hdmi->audio_mutex);
 }
 
 static void hdmi_clk_regenerator_update_pixel_clock(struct dw_hdmi *hdmi)
 {
+	mutex_lock(&hdmi->audio_mutex);
 	hdmi_set_clk_regenerator(hdmi, hdmi->hdmi_data.video_mode.mpixelclock);
+	mutex_unlock(&hdmi->audio_mutex);
 }
 
 /*
@@ -1565,6 +1571,8 @@ int dw_hdmi_bind(struct device *dev, struct device *master,
 	hdmi->sample_rate = 48000;
 	hdmi->ratio = 100;
 	hdmi->encoder = encoder;
+
+	mutex_init(&hdmi->audio_mutex);
 
 	of_property_read_u32(np, "reg-io-width", &val);
 
