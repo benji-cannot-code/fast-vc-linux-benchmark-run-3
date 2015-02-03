653f8414a37d0583bec1cf08cd7a417bec9093f2
Markus Elfring <elfring@users.sourceforge.net>
2015-02-03 13:55:53 +0100

    powerpc: Delete unnecessary checks before kfree()
    The kfree() function tests whether its argument is NULL and then returns
    immediately. Thus the test around the call is not needed.
    
    This issue was detected by using the Coccinelle software.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/cell/celleb_pci.c
+++ arch/powerpc/platforms/cell/celleb_pci.c
@@ -394,11 +394,10 @@ static int __init celleb_setup_fake_pci_device(struct device_node *node,
 
 error:
 	if (mem_init_done) {
-		if (config && *config)
+		if (config)
 			kfree(*config);
-		if (res && *res)
+		if (res)
 			kfree(*res);
-
 	} else {
 		if (config && *config) {
 			size = 256;
