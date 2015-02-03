b8515cb8f2489b198c5a36aa289c2fe1cec41450
Rob Herring <robh@kernel.org>
2015-02-03 17:03:35 -0600

    spi: spi-pxa2xx: only include mach/dma.h for legacy DMA
    Move the include of mach/dma.h to the legacy PXA DMA code where it is used.
    This enables building spi-pxa2xx on ARM64.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-pxa2xx-pxadma.c
+++ drivers/spi/spi-pxa2xx-pxadma.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spi/spi.h>
 #include <linux/spi/pxa2xx_spi.h>
 
+#include <mach/dma.h>
 #include "spi-pxa2xx.h"
 
 #define DMA_INT_MASK		(DCSR_ENDINTR | DCSR_STARTINTR | DCSR_BUSERR)
