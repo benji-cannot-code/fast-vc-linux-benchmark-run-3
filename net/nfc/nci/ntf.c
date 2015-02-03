f19858c0ebc2ca9efc65a09818a6cce46b5c974a
Christophe Ricard <christophe.ricard@gmail.com>
2015-02-03 19:48:04 +0100

    NFC: nci: Add reference to the RF logical connection
    The NCI_STATIC_RF_CONN_ID logical connection is the most used
    connection. Keeping it directly accessible in the nci_dev
    structure will simplify and optimize the access.
    
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- net/nfc/nci/ntf.c
+++ net/nfc/nci/ntf.c
@@ -626,8 +626,7 @@ static void nci_rf_intf_activated_ntf_packet(struct nci_dev *ndev,
 
 exit:
 	if (err == NCI_STATUS_OK) {
-		conn_info = nci_get_conn_info_by_conn_id(ndev,
-							 NCI_STATIC_RF_CONN_ID);
+		conn_info = ndev->rf_conn_info;
 		if (!conn_info)
 			return;
 
@@ -685,8 +684,7 @@ static void nci_rf_deactivate_ntf_packet(struct nci_dev *ndev,
 
 	pr_debug("entry, type 0x%x, reason 0x%x\n", ntf->type, ntf->reason);
 
-	conn_info =
-		nci_get_conn_info_by_conn_id(ndev, NCI_STATIC_RF_CONN_ID);
+	conn_info = ndev->rf_conn_info;
 	if (!conn_info)
 		return;
 
