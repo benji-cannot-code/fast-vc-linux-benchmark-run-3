dd77b9e3619aa4e96f92ccd8eb664af811340a72
Will Deacon <will.deacon@arm.com>
2015-02-04 12:17:55 +0000

    arm64: alternatives: add cpu feature for lse atomics
    Add a CPU feature for the LSE atomic instructions, so that they can be
    patched in at runtime when we detect that they are supported.
    
    Reviewed-by: Steve Capper <steve.capper@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -27,8 +27,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARM64_WORKAROUND_845719			2
 #define ARM64_HAS_SYSREG_GIC_CPUIF		3
 #define ARM64_HAS_PAN				4
+#define ARM64_CPU_FEAT_LSE_ATOMICS		5
 
-#define ARM64_NCAPS				5
+#define ARM64_NCAPS				6
 
 #ifndef __ASSEMBLY__
 
