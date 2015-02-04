6564b263af246e99cd735464376e8776d31a3205
Markus Elfring <elfring@users.sourceforge.net>
2015-02-04 20:28:49 +0100

    brcm80211: Delete unnecessary checks before two function calls
    The functions brcmu_pkt_buf_free_skb() and usb_free_urb() test whether
    their argument is NULL and then return immediately. Thus the test around
    the call is not needed.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/brcm80211/brcmfmac/sdio.c
@@ -2539,8 +2539,7 @@ static void brcmf_sdio_bus_stop(struct device *dev)
 	brcmu_pktq_flush(&bus->txq, true, NULL, NULL);
 
 	/* Clear any held glomming stuff */
-	if (bus->glomd)
-		brcmu_pkt_buf_free_skb(bus->glomd);
+	brcmu_pkt_buf_free_skb(bus->glomd);
 	brcmf_sdio_free_glom(bus);
 
 	/* Clear rx control and wake any waiters */
