0035cc6ea21344533aca30ace1fb8e15f3a00be1
Nicholas Mc Guire <hofrat@osadl.org>
2015-02-04 13:30:20 -0500

    ncr5380: Harmonize jiffies conversion with msecs_to_jiffies
    Instances of var * HZ / 1000 are replaced by msecs_to_jiffies(var).
    In addition some timing constants that assumed HZ 100 were adjusted
    to HZ independent settings based on review comments from Michael Schmitz
    <schmitzmic@gmail.com> and review of the original drivers in 1.0.31 and
    2.2.16.
    
    Signed-off-by: Nicholas Mc Guire <hofrat@osadl.org>
    Acked-by: Michael Schmitz <schmitzmic@gmail.com>
    Signed-off-by: Finn Thain <fthain@telegraphics.com.au>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>

--- drivers/scsi/g_NCR5380.c
+++ drivers/scsi/g_NCR5380.c
@@ -58,9 +58,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 /* settings for DTC3181E card with only Mustek scanner attached */
-#define USLEEP_POLL	1
-#define USLEEP_SLEEP	20
-#define USLEEP_WAITLONG	500
+#define USLEEP_POLL	msecs_to_jiffies(10)
+#define USLEEP_SLEEP	msecs_to_jiffies(200)
+#define USLEEP_WAITLONG	msecs_to_jiffies(5000)
 
 #define AUTOPROBE_IRQ
 
