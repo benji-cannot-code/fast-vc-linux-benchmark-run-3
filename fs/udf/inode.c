3702aec2e69629d616cbda43fa6a218f5e0510be
Fabian Frederick <fabf@skynet.be>
2015-02-04 18:26:27 +0100

    udf: remove bool assignment to 0/1
    Fix the following coccinelle warnings:
    
    fs/udf/inode.c:753:2-13: WARNING: Assignment of bool to 0/1
    fs/udf/inode.c:795:2-13: WARNING: Assignment of bool to 0/1
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/udf/inode.c
+++ fs/udf/inode.c
@@ -751,7 +751,7 @@ static sector_t inode_getblk(struct inode *inode, sector_t block,
 	/* Are we beyond EOF? */
 	if (etype == -1) {
 		int ret;
-		isBeyondEOF = 1;
+		isBeyondEOF = true;
 		if (count) {
 			if (c)
 				laarr[0] = laarr[1];
@@ -793,7 +793,7 @@ static sector_t inode_getblk(struct inode *inode, sector_t block,
 		endnum = c + 1;
 		lastblock = 1;
 	} else {
-		isBeyondEOF = 0;
+		isBeyondEOF = false;
 		endnum = startnum = ((count > 2) ? 2 : count);
 
 		/* if the current extent is in position 0,
