f398c8a8ae76ebb095a6e87f54ab633e3ef0daa3
Sonic Zhang <sonic.zhang@analog.com>
2015-02-03 17:12:17 -0800

    Input: bfin_rotary - move platform header to linux/platform_data
    The platform data definition of the rotary driver should be generic for all
    architectures.
    
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/bfin_rotary.c
+++ drivers/input/misc/bfin_rotary.c
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/input.h>
 #include <linux/slab.h>
+#include <linux/platform_data/bfin_rotary.h>
 
 #include <asm/portmux.h>
-#include <asm/bfin_rotary.h>
 
 static const u16 per_cnt[] = {
 	P_CNT_CUD,
