a43f9949bb99b58f2eaa9d3dbca6a1626e88a6bf
Tiejun Chen <tiejun.chen@intel.com>
2015-02-05 17:22:26 +0800

    kvm: remove KVM_MMIO_SIZE
    After f78146b0f923, "KVM: Fix page-crossing MMIO", and
    87da7e66a405, "KVM: x86: fix vcpu->mmio_fragments overflow",
    actually KVM_MMIO_SIZE is gone.
    
    Signed-off-by: Tiejun Chen <tiejun.chen@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -34,10 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/kvm_host.h>
 
-#ifndef KVM_MMIO_SIZE
-#define KVM_MMIO_SIZE 8
-#endif
-
 /*
  * The bit 16 ~ bit 31 of kvm_memory_region::flags are internally used
  * in kvm, other bits are visible for userspace which are defined in
