fe9b604c2d6b5ec95b67631060e65c0cb5cd1b24
Damien Lespiau <damien.lespiau@intel.com>
2015-02-05 18:30:20 +0000

    drm/i915: Don't try to reference the fb in get_initial_plane_config()
    Tvrtko noticed a new warning on boot:
    
      WARNING: CPU: 1 PID: 353 at include/linux/kref.h:47 drm_framebuffer_reference+0x6c/0x80 [drm]()
      Call Trace:
      [<ffffffff8161f10c>] dump_stack+0x4f/0x7b
      [<ffffffff81052caa>] warn_slowpath_common+0xaa/0xd0
      [<ffffffff81052d8a>] warn_slowpath_null+0x1a/0x20
      [<ffffffffa00d035c>] drm_framebuffer_reference+0x6c/0x80 [drm]
      [<ffffffffa01c0df7>] update_state_fb.isra.54+0x47/0x50 [i915]
      [<ffffffffa01ccd5c>] skylake_get_initial_plane_config+0x93c/0x950 [i915]
      [<ffffffffa01e8721>] intel_modeset_init+0x1551/0x17c0 [i915]
      [<ffffffffa02476e0>] i915_driver_load+0xed0/0x11e0 [i915]
      [<ffffffff81627aa1>] ? _raw_spin_unlock_irqrestore+0x51/0x70
      [<ffffffffa00ca8b7>] drm_dev_register+0x77/0x110 [drm]
      [<ffffffffa00cda3b>] drm_get_pci_dev+0x11b/0x1f0 [drm]
      [<ffffffff81098e3d>] ? trace_hardirqs_on+0xd/0x10
      [<ffffffff81627aa1>] ? _raw_spin_unlock_irqrestore+0x51/0x70
      [<ffffffffa0145276>] i915_pci_probe+0x56/0x60 [i915]
      [<ffffffff813ad59c>] pci_device_probe+0x7c/0x100
      [<ffffffff81466aad>] driver_probe_device+0x16d/0x380
    
    We cannot take a reference at this point, not before
    intel_framebuffer_init() and the underlying drm_framebuffer_init().
    
    Introduced in:
    
      commit 706dc7b549175e47f23e913b7f1e52874a7d0f56
      Author: Matt Roper <matthew.d.roper@intel.com>
      Date:   Tue Feb 3 13:10:04 2015 -0800
    
          drm/i915: Ensure plane->state->fb stays in sync with plane->fb
    
    v2: Don't move update_state_fb(). It was moved around because I
        originally put update_state_fb() in intel_alloc_plane_obj() before
        finding a better place. (Matt)
    
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Reported-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    From drm-next:
    (cherry picked from commit f55548b5af87ebfc586ca75748947f1c1b1a4a52)
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -2439,8 +2439,10 @@ intel_find_plane_obj(struct intel_crtc *intel_crtc,
 	if (!intel_crtc->base.primary->fb)
 		return;
 
-	if (intel_alloc_plane_obj(intel_crtc, plane_config))
+	if (intel_alloc_plane_obj(intel_crtc, plane_config)) {
+		update_state_fb(intel_crtc->base.primary);
 		return;
+	}
 
 	kfree(intel_crtc->base.primary->fb);
 	intel_crtc->base.primary->fb = NULL;
@@ -6664,7 +6666,6 @@ i9xx_get_initial_plane_config(struct intel_crtc *crtc,
 		      plane_config->size);
 
 	crtc->base.primary->fb = fb;
-	update_state_fb(crtc->base.primary);
 }
 
 static void chv_crtc_clock_get(struct intel_crtc *crtc,
@@ -7705,7 +7706,6 @@ skylake_get_initial_plane_config(struct intel_crtc *crtc,
 		      plane_config->size);
 
 	crtc->base.primary->fb = fb;
-	update_state_fb(crtc->base.primary);
 	return;
 
 error:
@@ -7799,7 +7799,6 @@ ironlake_get_initial_plane_config(struct intel_crtc *crtc,
 		      plane_config->size);
 
 	crtc->base.primary->fb = fb;
-	update_state_fb(crtc->base.primary);
 }
 
 static bool ironlake_get_pipe_config(struct intel_crtc *crtc,
