dfc6e0c6b95035bfc70b2c2d1d85d312c0816417
Mitch Williams <mitch.a.williams@intel.com>
2015-02-06 08:52:11 +0000

    i40e: enable packet split only when IOMMU present
    When an IOMMU is in use, the packet split receive path shows a distinct
    advantage over the single-buffer path because it minimizes DMA mapping
    and unmapping. However, this is not an advantage for systems with no
    IOMMU. At init time, check to see if an IOMMU is enabled and enable
    packet split receives.
    
    Change-ID: I4f70d2e9c31bbea3dc8fd0c5734959a6e6602210
    Signed-off-by: Mitch Williams <mitch.a.williams@intel.com>
    Tested-by: Jim Young <james.m.young@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e.h
+++ drivers/net/ethernet/intel/i40e/i40e.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/aer.h>
 #include <linux/netdevice.h>
 #include <linux/ioport.h>
+#include <linux/iommu.h>
 #include <linux/slab.h>
 #include <linux/list.h>
 #include <linux/string.h>
