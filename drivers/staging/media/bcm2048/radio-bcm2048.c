f8059e8d0931831d60ac59f16f843d7a8a5b376f
Luis de Bethencourt <luis@debethencourt.com>
2015-02-08 19:29:11 -0300

    [media] media: bcm2048: remove unused return of function
    Integer return of bcm2048_parse_rds_rt () is never used, changing the return
    type to void.
    
    Signed-off-by: Luis de Bethencourt <luis.bg@samsung.com>
    Acked-by: Pali Rohár <pali.rohar@gmail.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/staging/media/bcm2048/radio-bcm2048.c
+++ drivers/staging/media/bcm2048/radio-bcm2048.c
@@ -1580,7 +1580,7 @@ static void bcm2048_parse_rt_match_d(struct bcm2048_device *bdev, int i,
 		bcm2048_parse_rds_rt_block(bdev, i, index+2, crc);
 }
 
-static int bcm2048_parse_rds_rt(struct bcm2048_device *bdev)
+static void bcm2048_parse_rds_rt(struct bcm2048_device *bdev)
 {
 	int i, index = 0, crc, match_b = 0, match_c = 0, match_d = 0;
 
@@ -1616,8 +1616,6 @@ static int bcm2048_parse_rds_rt(struct bcm2048_device *bdev)
 					match_b = 1;
 		}
 	}
-
-	return 0;
 }
 
 static void bcm2048_parse_rds_ps_block(struct bcm2048_device *bdev, int i,
