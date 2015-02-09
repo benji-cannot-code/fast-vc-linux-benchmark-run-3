c3a649b0df0a99ad0700b598ff645e298e524aa0
Damien Lespiau <damien.lespiau@intel.com>
2015-02-09 19:33:19 +0000

    drm/i915/skl: Implement WaDisableHDCInvalidation
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Reviewed-by: Nick Hoath <nicholas.hoath@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -141,6 +141,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GEN8_RING_PDP_LDW(ring, n)	((ring)->mmio_base+0x270 + (n) * 8)
 
 #define GAM_ECOCHK			0x4090
+#define   BDW_DISABLE_HDC_INVALIDATION	(1<<25)
 #define   ECOCHK_SNB_BIT		(1<<10)
 #define   HSW_ECOCHK_ARB_PRIO_SOL	(1<<6)
 #define   ECOCHK_PPGTT_CACHE64B		(0x3<<3)
