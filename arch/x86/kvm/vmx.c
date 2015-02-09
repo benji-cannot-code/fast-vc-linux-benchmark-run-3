8ce47236465304665aa8cdaf2998230da1355bed
Radim Krčmář <rkrcmar@redhat.com>
2015-02-09 22:44:07 +0100

    KVM: x86: fix build with !CONFIG_SMP
    <asm/apic.h> isn't included directly and without CONFIG_SMP, an option
    that automagically pulls it can't be enabled.
    
    Reported-by: Jim Davis <jim.epost@gmail.com>
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/perf_event.h>
 #include <asm/debugreg.h>
 #include <asm/kexec.h>
+#include <asm/apic.h>
 
 #include "trace.h"
 
