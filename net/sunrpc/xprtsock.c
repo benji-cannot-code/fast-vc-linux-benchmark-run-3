99701bea225da43d28d6b3c5ff55b096376c7256
Trond Myklebust <trond.myklebust@primarydata.com>
2015-02-09 09:26:39 -0500

    SUNRPC: Ensure xs_tcp_shutdown() requests a full close of the connection
    The previous behaviour left the connection half-open in order to try
    to scrape the last replies from the socket. Now that we have more reliable
    reconnection, change the behaviour to close down the socket faster.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -628,7 +628,7 @@ process_status:
  * @xprt: transport
  *
  * Initiates a graceful shutdown of the TCP socket by calling the
- * equivalent of shutdown(SHUT_WR);
+ * equivalent of shutdown(SHUT_RDWR);
  */
 static void xs_tcp_shutdown(struct rpc_xprt *xprt)
 {
@@ -636,7 +636,7 @@ static void xs_tcp_shutdown(struct rpc_xprt *xprt)
 	struct socket *sock = transport->sock;
 
 	if (sock != NULL) {
-		kernel_sock_shutdown(sock, SHUT_WR);
+		kernel_sock_shutdown(sock, SHUT_RDWR);
 		trace_rpc_socket_shutdown(xprt, sock);
 	}
 }
