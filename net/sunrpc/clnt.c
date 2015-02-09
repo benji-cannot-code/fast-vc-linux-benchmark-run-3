afd72ce0fa218d8c733b26735c8c07e1ff934fdf
Trond Myklebust <trond.myklebust@primarydata.com>
2015-02-08 21:44:04 -0500

    SUNRPC: Handle EADDRINUSE on connect
    Now that we're setting SO_REUSEPORT, we still need to handle the
    case where a connect() is attempted, but the old socket is still
    lingering.
    Essentially, all we want to do here is handle the error by waiting
    a few seconds and then retrying.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -1827,6 +1827,7 @@ call_connect_status(struct rpc_task *task)
 	case -ECONNABORTED:
 	case -ENETUNREACH:
 	case -EHOSTUNREACH:
+	case -EADDRINUSE:
 	case -ENOBUFS:
 	case -EPIPE:
 		if (RPC_IS_SOFTCONN(task))
@@ -1935,6 +1936,7 @@ call_transmit_status(struct rpc_task *task)
 		}
 	case -ECONNRESET:
 	case -ECONNABORTED:
+	case -EADDRINUSE:
 	case -ENOTCONN:
 	case -ENOBUFS:
 	case -EPIPE:
@@ -2054,6 +2056,7 @@ call_status(struct rpc_task *task)
 	case -ECONNRESET:
 	case -ECONNABORTED:
 		rpc_force_rebind(clnt);
+	case -EADDRINUSE:
 	case -ENOBUFS:
 		rpc_delay(task, 3*HZ);
 	case -EPIPE:
