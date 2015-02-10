e4de717b339a4db42bcb0da8be8b3a99845298d7
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2015-02-10 17:16:05 +0000

    drm/i915: Add tiled framebuffer modifiers
    To be used from the new addfb2 extension.
    
    v2:
    - Drop Intel-specific untiled modfier.
    - Move to drm_fourcc.h.
    - Document layouts a bit and denote them as platform-specific and not
      useable for cross-driver sharing.
    - Add Y-tiling for completeness.
    - Drop special docstring markers to avoid confusing kerneldoc.
    
    v3: Give Y-tiling a unique idea, noticed by Tvrtko.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com> (v1)
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _I915_DRV_H_
 
 #include <uapi/drm/i915_drm.h>
+#include <uapi/drm/drm_fourcc.h>
 
 #include "i915_reg.h"
 #include "intel_bios.h"
