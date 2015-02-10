a30d13a0e6b806eaae72c550fc1f44298f3a68c3
Kevin Cernekee <cernekee@gmail.com>
2015-02-10 14:08:38 -0800

    sh: eliminate unused irq_reg_{readl,writel} accessors
    Defining these macros way down in arch/sh/.../irq.c doesn't cause
    kernel/irq/generic-chip.c to use them.  As far as I can tell this code has
    no effect.
    
    Fixes: 332fd7c4fef5f3b1 ("genirq: Generic chip: Change irq_reg_{readl,writel} arguments")
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be> (cpp/asm comparison)
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/sh/boards/mach-se/7343/irq.c
+++ arch/sh/boards/mach-se/7343/irq.c
@@ -15,9 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRV_NAME "SE7343-FPGA"
 #define pr_fmt(fmt) DRV_NAME ": " fmt
 
-#define irq_reg_readl	ioread16
-#define irq_reg_writel	iowrite16
-
 #include <linux/init.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
