4f747ef3946dc6ef74792d84340ad593504c0858
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2015-02-10 17:47:04 -0800

    Input: bfin_rotary - fix potential oops in interrupt handler
    The interrupt handler in the driver tries to fetch driver data from
    platform device, unfortunately it is only set up after interrupt handler
    is registered. Since interrupt handler does not really need to access
    the platform device itself let's change it to get the driver data
    instance instead.
    
    Acked-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/bfin_rotary.c
+++ drivers/input/misc/bfin_rotary.c
@@ -60,8 +60,7 @@ static void report_rotary_event(struct bfin_rot *rotary, int delta)
 
 static irqreturn_t bfin_rotary_isr(int irq, void *dev_id)
 {
-	struct platform_device *pdev = dev_id;
-	struct bfin_rot *rotary = platform_get_drvdata(pdev);
+	struct bfin_rot *rotary = dev_id;
 	int delta;
 
 	switch (bfin_read_CNT_STATUS()) {
@@ -153,7 +152,7 @@ static int bfin_rotary_probe(struct platform_device *pdev)
 	}
 
 	error = request_irq(rotary->irq, bfin_rotary_isr,
-			    0, dev_name(&pdev->dev), pdev);
+			    0, dev_name(&pdev->dev), rotary);
 	if (error) {
 		dev_err(&pdev->dev,
 			"unable to claim irq %d; error %d\n",
@@ -187,7 +186,7 @@ static int bfin_rotary_probe(struct platform_device *pdev)
 	return 0;
 
 out2:
-	free_irq(rotary->irq, pdev);
+	free_irq(rotary->irq, rotary);
 out1:
 	input_free_device(input);
 	kfree(rotary);
@@ -203,7 +202,7 @@ static int bfin_rotary_remove(struct platform_device *pdev)
 	bfin_write_CNT_CONFIG(0);
 	bfin_write_CNT_IMASK(0);
 
-	free_irq(rotary->irq, pdev);
+	free_irq(rotary->irq, rotary);
 	input_unregister_device(rotary->input);
 	peripheral_free_list(per_cnt);
 
