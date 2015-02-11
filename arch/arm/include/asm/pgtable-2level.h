b3a4edc64dbbd532d92d24777ac4599a41054ccd
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2015-02-11 15:26:47 -0800

    arm: define __PAGETABLE_PMD_FOLDED for !LPAE
    ARM uses custom implementation of PMD folding in 2-level page table case.
    Generic code expects to see __PAGETABLE_PMD_FOLDED to be defined if PMD is
    folded, but ARM doesn't do this.  Let's fix it.
    
    Defining __PAGETABLE_PMD_FOLDED will drop out unused __pmd_alloc().  It
    also fixes problems with recently-introduced pmd accounting on ARM without
    LPAE.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reported-by: Nishanth Menon <nm@ti.com>
    Reported-by: Simon Horman <horms@verge.net.au>
    Tested-by: Simon Horman <horms+renesas@verge.net.au>
    Tested-by: Fabio Estevam <festevam@gmail.com>
    Tested-by: Felipe Balbi <balbi@ti.com>
    Tested-by: Nishanth Menon <nm@ti.com>
    Tested-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Tested-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: Pavel Emelyanov <xemul@openvz.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm/include/asm/pgtable-2level.h
+++ arch/arm/include/asm/pgtable-2level.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_PGTABLE_2LEVEL_H
 #define _ASM_PGTABLE_2LEVEL_H
 
+#define __PAGETABLE_PMD_FOLDED
+
 /*
  * Hardware-wise, we have a two level page table structure, where the first
  * level has 4096 entries, and the second level has 256 entries.  Each entry
