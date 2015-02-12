e64a40bec3088852b3729981c5ad0ca30f6b5aa0
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2015-02-12 15:02:35 -0800

    lib/sort.c: use simpler includes
    sort.c doesn't use facilities from kernel.h, but does use some types
    defined in linux/types.h.  Include the latter directly instead of relying
    on some other header doing it.  Similarly, include linux/export.h directly
    instead of through module.h.  This removes 80 lines from the dependency
    file .sort.o.cmd.
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/sort.c
+++ lib/sort.c
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Jan 23 2005  Matt Mackall <mpm@selenic.com>
  */
 
-#include <linux/kernel.h>
-#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/export.h>
 #include <linux/sort.h>
 #include <linux/slab.h>
 
