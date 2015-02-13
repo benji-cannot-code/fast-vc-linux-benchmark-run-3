5ad53fc1213b565ea6d99adf9514a1a0d57b80d3
Johan Hovold <johan@hovoldconsulting.com>
2015-02-13 14:58:04 +0800

    greybus: gpio: fix memory leaks at init and exit
    Fix three related memory leaks in the init an exit callbacks, where the
    gpio-lines array was never freed at all and the controller data wasn't
    freed in the init error path.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/gpio.c
+++ drivers/staging/greybus/gpio.c
@@ -735,7 +735,7 @@ static int gb_gpio_connection_init(struct gb_connection *connection)
 
 	ret = gb_gpio_controller_setup(gb_gpio_controller);
 	if (ret)
-		goto out_err;
+		goto err_free_controller;
 
 	irqc = &gb_gpio_controller->irqc;
 	irqc->irq_ack = gb_gpio_ack_irq;
@@ -767,7 +767,7 @@ static int gb_gpio_connection_init(struct gb_connection *connection)
 	ret = gpiochip_add(gpio);
 	if (ret) {
 		pr_err("Failed to register GPIO\n");
-		goto out_err;
+		goto err_free_lines;
 	}
 
 	ret = gb_gpio_irqchip_add(gpio, irqc, 0,
@@ -781,7 +781,9 @@ static int gb_gpio_connection_init(struct gb_connection *connection)
 
 irqchip_err:
 	gb_gpiochip_remove(gpio);
-out_err:
+err_free_lines:
+	kfree(gb_gpio_controller->lines);
+err_free_controller:
 	kfree(gb_gpio_controller);
 	return ret;
 }
@@ -796,6 +798,7 @@ static void gb_gpio_connection_exit(struct gb_connection *connection)
 	gb_gpio_irqchip_remove(gb_gpio_controller);
 	gb_gpiochip_remove(&gb_gpio_controller->chip);
 	/* kref_put(gb_gpio_controller->connection) */
+	kfree(gb_gpio_controller->lines);
 	kfree(gb_gpio_controller);
 }
 
