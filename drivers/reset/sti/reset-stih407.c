11118efa985fc1e3c426bb7f4060e546278a98a3
Philipp Zabel <p.zabel@pengutronix.de>
2015-02-13 12:20:49 +0100

    ARM: STi: DT: Move reset controller constants into common location
    By popular vote, the DT binding includes for reset controllers are located
    in include/dt-bindings/reset/. Move the STi reset constants in there, too,
    to avoid confusion.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Acked-by: Patrice Chotard <patrice.chotard@st.com>

--- drivers/reset/sti/reset-stih407.c
+++ drivers/reset/sti/reset-stih407.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
-#include <dt-bindings/reset-controller/stih407-resets.h>
+#include <dt-bindings/reset/stih407-resets.h>
 #include "reset-syscfg.h"
 
 /* STiH407 Peripheral powerdown definitions. */
--- drivers/reset/sti/reset-stih415.c
+++ drivers/reset/sti/reset-stih415.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
 
-#include <dt-bindings/reset-controller/stih415-resets.h>
+#include <dt-bindings/reset/stih415-resets.h>
 
 #include "reset-syscfg.h"
 
