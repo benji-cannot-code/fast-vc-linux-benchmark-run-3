3d56506ecce87a7b0edf9c234bdc5f8d7e699761
Michal Kazior <michal.kazior@tieto.com>
2015-02-13 13:30:16 +0100

    ath10k: disable multi-vif ps by default
    Not all firmware revisions have a proper
    multi-interface client powersaving implementation,
    e.g. qca6174 WLAN.RM.2.0-00073.
    
    Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -309,6 +309,7 @@ struct ath10k_vif {
 	bool is_started;
 	bool is_up;
 	bool spectral_enabled;
+	bool ps;
 	u32 aid;
 	u8 bssid[ETH_ALEN];
 
@@ -433,6 +434,12 @@ enum ath10k_fw_features {
 	 * code.
 	 */
 	ATH10K_FW_FEATURE_WMI_10_2 = 4,
+
+	/* Some firmware revisions lack proper multi-interface client powersave
+	 * implementation. Enabling PS could result in connection drops,
+	 * traffic stalls, etc.
+	 */
+	ATH10K_FW_FEATURE_MULTI_VIF_PS_SUPPORT = 5,
 
 	/* keep last */
 	ATH10K_FW_FEATURE_COUNT,
