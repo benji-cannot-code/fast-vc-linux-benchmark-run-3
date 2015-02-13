451516fed0b09126aa78dc0304ae6e586fedabb7
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2015-02-13 15:01:13 +0200

    mfd: intel_soc_pmic: Move PMIC interrupt comment to probe function
    intel_soc_pmic_find_gpio_irq() tries to find a GPIO interrupt but doesn't
    select between it or I2C interrupt so it makes more sense to move this
    comment to intel_soc_pmic_i2c_probe() with minor edits.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/intel_soc_pmic_core.c
+++ drivers/mfd/intel_soc_pmic_core.c
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mfd/intel_soc_pmic.h>
 #include "intel_soc_pmic_core.h"
 
-/*
- * On some boards the PMIC interrupt may come from a GPIO line.
- * Try to lookup the ACPI table and see if such connection exists. If not,
- * return -ENOENT and use the IRQ provided by I2C.
- */
 static int intel_soc_pmic_find_gpio_irq(struct device *dev)
 {
 	struct gpio_desc *desc;
@@ -72,6 +67,11 @@ static int intel_soc_pmic_i2c_probe(struct i2c_client *i2c,
 
 	pmic->regmap = devm_regmap_init_i2c(i2c, config->regmap_config);
 
+	/*
+	 * On some boards the PMIC interrupt may come from a GPIO line. Try to
+	 * lookup the ACPI table for a such connection and setup a GPIO
+	 * interrupt if it exists. Otherwise use the IRQ provided by I2C
+	 */
 	irq = intel_soc_pmic_find_gpio_irq(dev);
 	pmic->irq = (irq < 0) ? i2c->irq : irq;
 
