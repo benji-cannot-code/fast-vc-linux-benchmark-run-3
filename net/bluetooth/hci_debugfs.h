6d4ac962e47507f6503db0166f7a31846aa0a55b
Marcel Holtmann <marcel@holtmann.org>
2015-02-14 13:40:06 -0800

    Bluetooth: Provide option to enable/disable debugfs information
    The Bluetooth controllers can export extensive information about
    internal states via debugfs. This patch provides an option to
    choose if these information are provided or not.
    
    For backwards compatibility with existing kernel configuration,
    this option defaults to yes.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/hci_debugfs.h
+++ net/bluetooth/hci_debugfs.h
@@ -21,7 +21,29 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    SOFTWARE IS DISCLAIMED.
 */
 
+#if IS_ENABLED(CONFIG_BT_DEBUGFS)
+
 void hci_debugfs_create_common(struct hci_dev *hdev);
 void hci_debugfs_create_bredr(struct hci_dev *hdev);
 void hci_debugfs_create_le(struct hci_dev *hdev);
 void hci_debugfs_create_conn(struct hci_conn *conn);
+
+#else
+
+static inline void hci_debugfs_create_common(struct hci_dev *hdev)
+{
+}
+
+static inline void hci_debugfs_create_bredr(struct hci_dev *hdev)
+{
+}
+
+static inline void hci_debugfs_create_le(struct hci_dev *hdev)
+{
+}
+
+static inline void hci_debugfs_create_conn(struct hci_conn *conn)
+{
+}
+
+#endif
