f8efd138b2346efe31c7a526b161b768eea213ac
Cass May <cass@cassm.net>
2015-02-15 23:40:18 +0000

    dgnc: Move DG_PART definition from Makefile to dgnc_driver.h
    Avoid deprecated usage of EXTRA_CFLAGS by moving definition of DG_PART into dgnc_driver.h
    
    Signed-off-by: Cass May <cass@cassm.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.h
+++ drivers/staging/dgnc/dgnc_driver.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	PROCSTR		"dgnc"			/* /proc entries	 */
 #define	DEVSTR		"/dev/dg/dgnc"		/* /dev entries		 */
 #define	DRVSTR		"dgnc"			/* Driver name string	 */
+#define	DG_PART		"40002369_F"		/* RPM part number	 */
 
 #define TRC_TO_CONSOLE 1
 
