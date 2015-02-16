9419d67e50ffd1fdc2b74b369a9d42cc818977b4
Priit Laes <plaes@plaes.org>
2015-02-16 15:01:53 +0200

    rtlwifi: Remove unused RF6052_MAX_REG define
    Signed-off-by: Priit Laes <plaes@plaes.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/rtlwifi/rtl8192de/def.h
+++ drivers/net/wireless/rtlwifi/rtl8192de/def.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	MAX_MSS_DENSITY_1T				0x0A
 
 #define RF6052_MAX_TX_PWR				0x3F
-#define RF6052_MAX_REG					0x3F
 #define RF6052_MAX_PATH					2
 
 #define	PHY_RSSI_SLID_WIN_MAX				100
