69592c8e981247fe0d39109b3fae9fedc198e11c
Geoff Levand <geoff@infradead.org>
2015-02-17 13:45:56 -0800

    kexec: add bit definitions for kimage entry flags
    Define new kexec preprocessor macros IND_*_BIT that define the bit
    position of the kimage entry flags.  Change the existing IND_* flag macros
    to be defined as bit shifts of the corresponding IND_*_BIT macros.  Also
    wrap all C language code in kexec.h with #if !defined(__ASSEMBLY__) so
    assembly files can include kexec.h to get the IND_* and IND_*_BIT macros.
    
    Some CPU instruction sets have tests for bit position which are convenient
    in implementing routines that operate on the kimage entry list.  The
    addition of these bit position macros in a common location will avoid
    duplicate definitions and the chance that changes to the IND_* flags will
    not be propagated to assembly files.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>
    Acked-by: Vivek Goyal <vgoyal@redhat.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Maximilian Attems <max@stro.at>
    Cc: Michal Marek <mmarek@suse.cz>
    Cc: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/kexec.h
+++ include/linux/kexec.h
@@ -2,6 +2,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef LINUX_KEXEC_H
 #define LINUX_KEXEC_H
 
+#define IND_DESTINATION_BIT 0
+#define IND_INDIRECTION_BIT 1
+#define IND_DONE_BIT        2
+#define IND_SOURCE_BIT      3
+
+#define IND_DESTINATION  (1 << IND_DESTINATION_BIT)
+#define IND_INDIRECTION  (1 << IND_INDIRECTION_BIT)
+#define IND_DONE         (1 << IND_DONE_BIT)
+#define IND_SOURCE       (1 << IND_SOURCE_BIT)
+
+#if !defined(__ASSEMBLY__)
+
 #include <uapi/linux/kexec.h>
 
 #ifdef CONFIG_KEXEC
@@ -65,10 +77,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 typedef unsigned long kimage_entry_t;
-#define IND_DESTINATION  0x1
-#define IND_INDIRECTION  0x2
-#define IND_DONE         0x4
-#define IND_SOURCE       0x8
 
 struct kexec_segment {
 	/*
@@ -312,4 +320,7 @@ struct task_struct;
 static inline void crash_kexec(struct pt_regs *regs) { }
 static inline int kexec_should_crash(struct task_struct *p) { return 0; }
 #endif /* CONFIG_KEXEC */
+
+#endif /* !defined(__ASSEBMLY__) */
+
 #endif /* LINUX_KEXEC_H */
