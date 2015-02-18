428824db6de49eff7e4e52488bd0bca4f8283892
Rasesh Mody <rasesh.mody@qlogic.com>
2015-02-17 19:26:20 -0500

    bnx2-cnic: Driver Version Update
    This patch updates BNX2 driver version to 2.2.6 and CNIC driver version
    to 2.5.21.
    
    Signed-off-by: Rasesh Mody <rasesh.mody@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/cnic_if.h
+++ drivers/net/ethernet/broadcom/cnic_if.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "bnx2x/bnx2x_mfw_req.h"
 
-#define CNIC_MODULE_VERSION	"2.5.20"
-#define CNIC_MODULE_RELDATE	"March 14, 2014"
+#define CNIC_MODULE_VERSION	"2.5.21"
+#define CNIC_MODULE_RELDATE	"January 29, 2015"
 
 #define CNIC_ULP_RDMA		0
 #define CNIC_ULP_ISCSI		1
