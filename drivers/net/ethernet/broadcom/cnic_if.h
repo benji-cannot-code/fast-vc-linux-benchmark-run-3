87acb1b1a2eaa15578f035e8ec352972d915dcab
Rasesh Mody <rasesh.mody@qlogic.com>
2015-02-17 19:26:18 -0500

    bnx2-cnic: Driver Rebranding Changes
    This patch provides additional changes as a part of BNX2 and CNIC driver
    re-branding effort.
    
    Signed-off-by: Rasesh Mody <rasesh.mody@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/cnic_if.h
+++ drivers/net/ethernet/broadcom/cnic_if.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* cnic_if.h: QLogic CNIC core network driver.
+/* cnic_if.h: QLogic cnic core network driver.
  *
  * Copyright (c) 2006-2014 Broadcom Corporation
- * Copyright (c) 2014 QLogic Corporation
+ * Copyright (c) 2014-2015 QLogic Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
