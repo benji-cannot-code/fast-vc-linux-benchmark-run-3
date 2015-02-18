e5a7f2557862e8ea7bae533c173ec233c62f4a30
Michal Malý <madcatxster@devoid-pointer.net>
2015-02-18 17:59:22 +0100

    HID: hid-lg4ff: Introduce a module parameter to disable automatic switch of compatibility mode
    Introduce a module parameter to disable automatic switch of Logitech gaming
    wheels from compatibility to native mode. This only applies to multimode wheels.
    
    Signed-off-by: Michal Malý <madcatxster@devoid-pointer.net>
    Tested-by: Simon Wood <simon@mungewell.org>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-lg4ff.c
+++ drivers/hid/hid-lg4ff.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "usbhid/usbhid.h"
 #include "hid-lg.h"
+#include "hid-lg4ff.h"
 #include "hid-ids.h"
 
 #define to_hid_device(pdev) container_of(pdev, struct hid_device, dev)
@@ -804,7 +805,8 @@ static int lg4ff_handle_multimode_wheel(struct hid_device *hid, u16 *real_produc
 	/* Switch from "Driving Force" mode to native mode automatically.
 	 * Otherwise keep the wheel in its current mode */
 	if (reported_product_id == USB_DEVICE_ID_LOGITECH_WHEEL &&
-	    reported_product_id != *real_product_id) {
+	    reported_product_id != *real_product_id &&
+	    !lg4ff_no_autoswitch) {
 		const struct lg4ff_compat_mode_switch *s;
 
 		switch (*real_product_id) {
