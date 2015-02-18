013b413525a41c7a1138b8bfe47eb5ffc9a660a3
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2015-02-18 13:21:56 +0200

    drm: rcar-du: Wait for page flip completion when turning the CRTC off
    Turning a CRTC off will prevent a queued page flip from ever completing,
    potentially confusing userspace. Wait for queued page flips to complete
    before turning the CRTC off to avoid this.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/gpu/drm/rcar-du/rcar_du_crtc.h
+++ drivers/gpu/drm/rcar-du/rcar_du_crtc.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __RCAR_DU_CRTC_H__
 
 #include <linux/mutex.h>
+#include <linux/wait.h>
 
 #include <drm/drmP.h>
 #include <drm/drm_crtc.h>
@@ -33,6 +34,8 @@ struct rcar_du_crtc {
 	bool started;
 
 	struct drm_pending_vblank_event *event;
+	wait_queue_head_t flip_wait;
+
 	unsigned int outputs;
 	int dpms;
 
