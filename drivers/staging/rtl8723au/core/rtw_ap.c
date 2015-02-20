4ebdc151db172ad05c2244505be36aa87a4d9933
Vaishali Thakkar <vthakkar1994@gmail.com>
2015-02-20 14:18:45 +0530

    Staging: rtl8723au: Use put_unaligned_le16
    Using byte ordering functions and then memcpy() is risky and
    prone to hide errors which are hard to track down. So, this
    patch introduces the use of function put_unaligned_le16 which
    makes the code clear. Here, use of variable tim_bitmap_le
    and variable itself is removed. Also, to be compatible with the
    changes header file is added too.
    
    Coccinelle is used to do this change and semantic patch used for
    this is as follows:
    
    @a@
    typedef __le16;
    __le16 e16;
    identifier tmp;
    expression ptr;
    expression y,e;
    type T;
    @@
    
    - tmp = cpu_to_le16(y);
    
    <+... when != tmp
    (
    - memcpy(ptr, (T)&tmp, \(2\|sizeof(__le16)\|sizeof(e16)\));
    + put_unaligned_le16(y,ptr);
    |
    - memcpy(ptr, (T)&tmp, ...);
    + put_unaligned_le16(y,ptr);
    )
    ...+>
    ? tmp = e
    
    @@ type T; identifier a.tmp; @@
    
    - T tmp;
    ...when != tmp
    
    Signed-off-by: Vaishali Thakkar <vthakkar1994@gmail.com>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_ap.c
+++ drivers/staging/rtl8723au/core/rtw_ap.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <wifi.h>
 #include <rtl8723a_cmd.h>
 #include <rtl8723a_hal.h>
+#include <asm/unaligned.h>
 
 extern unsigned char WMM_OUI23A[];
 extern unsigned char WPS_OUI23A[];
@@ -73,10 +74,7 @@ static void update_BCNTIM(struct rtw_adapter *padapter)
 	struct wlan_bssid_ex *pnetwork_mlmeext = &pmlmeinfo->network;
 	unsigned char *pie = pnetwork_mlmeext->IEs;
 	u8 *p, *dst_ie, *premainder_ie = NULL, *pbackup_remainder_ie = NULL;
-	__le16 tim_bitmap_le;
 	uint offset, tmp_len, tim_ielen, tim_ie_offset, remainder_ielen;
-
-	tim_bitmap_le = cpu_to_le16(pstapriv->tim_bitmap);
 
 	p = rtw_get_ie23a(pie, WLAN_EID_TIM, &tim_ielen,
 			  pnetwork_mlmeext->IELength);
@@ -144,9 +142,9 @@ static void update_BCNTIM(struct rtw_adapter *padapter)
 		*dst_ie++ = 0;
 
 	if (tim_ielen == 4) {
-		*dst_ie++ = *(u8 *)&tim_bitmap_le;
+		*dst_ie++ = pstapriv->tim_bitmap & 0xff;
 	} else if (tim_ielen == 5) {
-		memcpy(dst_ie, &tim_bitmap_le, 2);
+		put_unaligned_le16(pstapriv->tim_bitmap, dst_ie);
 		dst_ie += 2;
 	}
 
