e8c38a751501fe0ebb44a37a1a7fc8521ab488af
Vineet Gupta <vgupta@synopsys.com>
2015-02-20 19:12:18 +0530

    ARC: [plat_arcfpga]->[plat_sim]
    * Remove remanants of legacy ARC FPGA platforms (AA4, ML509...)
    * Only nsim simulation platform is left, rename platform accordingly
    * AA4 DT stuff is compatible with nsim for ARC700 so rename it too
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/plat-sim/platform.c
+++ arch/arc/plat-sim/platform.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * ARC FPGA Platform support code
+ * ARC simulation Platform support code
  *
  * Copyright (C) 2012 Synopsys, Inc. (www.synopsys.com)
  *
@@ -19,16 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * however as part of early device tree scan, we also select the right
  * callback set, by matching the DT compatible name.
  */
-
-static const char *legacy_fpga_compat[] __initconst = {
-	"snps,arc-angel4",
-	"snps,arc-ml509",
-	NULL,
-};
-
-MACHINE_START(LEGACY_FPGA, "legacy_fpga")
-	.dt_compat	= legacy_fpga_compat,
-MACHINE_END
 
 static const char *simulation_compat[] __initconst = {
 	"snps,nsim",
