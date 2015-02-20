573050ef9ae1a429fe431a88d505932629e8359e
Jiri Kosina <jkosina@suse.cz>
2015-02-20 10:18:59 +0100

    x86/mm/ASLR: Avoid PAGE_SIZE redefinition for UML subarch
    Commit f47233c2d34 ("x86/mm/ASLR: Propagate base load address
    calculation") causes PAGE_SIZE redefinition warnings for UML
    subarch  builds. This is caused by added includes that were
    leftovers from previous  patch versions are are not actually
    needed (especially page_types.h  inlcude in module.c). Drop
    those stray includes.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: H. Peter Anvin <hpa@linux.intel.com>
    Cc: Kees Cook <keescook@chromium.org>
    Link: http://lkml.kernel.org/r/alpine.LNX.2.00.1502201017240.28769@pobox.suse.cz
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/page_types.h
+++ arch/x86/include/asm/page_types.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/const.h>
 #include <linux/types.h>
-#include <asm/bootparam.h>
 
 /* PAGE_SHIFT determines the page size */
 #define PAGE_SHIFT	12
