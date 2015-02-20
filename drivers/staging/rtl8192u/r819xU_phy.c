8ccba496b9682803ec0b10d5a07640b1b9ef6ca5
Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
2015-02-20 22:54:27 +0100

    Staging: rtl8192u: Remove else after return
    This patch simplifies the code by removing else and fixes
    the following checkpatch.pl warning: "else is not useful after
    break or return".
    
    Signed-off-by: Ksenija Stanojevic <ksenija.stanojevic@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r819xU_phy.c
+++ drivers/staging/rtl8192u/r819xU_phy.c
@@ -1366,11 +1366,10 @@ static u8 rtl8192_phy_SwChnlStepByStep(struct net_device *dev, u8 channel,
 			if ((*stage) == 2) {
 				(*delay) = CurrentCmd->msDelay;
 				return true;
-			} else {
-				(*stage)++;
-				(*step) = 0;
-				continue;
 			}
+			(*stage)++;
+			(*step) = 0;
+			continue;
 		}
 
 		switch (CurrentCmd->CmdID) {
