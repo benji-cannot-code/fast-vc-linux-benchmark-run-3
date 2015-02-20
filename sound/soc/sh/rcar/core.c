09d5c97c25bbdfeddbd1bf204e218855d93a2ef1
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2015-02-20 10:22:37 +0000

    ASoC: rsnd: remove SH-DMA-BASE specific implementation
    Renesas R-Car sound had SH-DMA-BASE specific implementation before,
    but, now, it is no longer needed. Let's remove it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rcar/core.c
+++ sound/soc/sh/rcar/core.c
@@ -95,7 +95,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 #include <linux/pm_runtime.h>
-#include <linux/shdma-base.h>
 #include "rsnd.h"
 
 #define RSND_RATES SNDRV_PCM_RATE_8000_96000
