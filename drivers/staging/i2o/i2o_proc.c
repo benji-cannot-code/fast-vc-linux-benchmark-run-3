34b196b38cb92ce31508637f87770678ec7746d9
Aya Mahfouz <mahfouz.saif.elyazal@gmail.com>
2015-02-20 03:44:31 +0200

    staging: i2o: Use #include <linux/uaccess.h> instead of <asm/uaccess.h>
    This patch fixes the following checkpatch.pl warning:
    Use #include <linux/uaccess.h> instead of <asm/uaccess.h>
    
    Signed-off-by: Aya Mahfouz <mahfouz.saif.elyazal@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/i2o/i2o_proc.c
+++ drivers/staging/i2o/i2o_proc.c
@@ -49,9 +49,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/spinlock.h>
 #include <linux/workqueue.h>
+#include <linux/uaccess.h>
 
 #include <asm/io.h>
-#include <asm/uaccess.h>
 #include <asm/byteorder.h>
 
 /* Structure used to define /proc entries */
