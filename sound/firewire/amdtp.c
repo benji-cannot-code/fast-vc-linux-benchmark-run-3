6ae3e0005b694672585e403bba0b6e23348548fb
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-02-21 11:50:17 +0900

    ALSA: firewire-lib: fix an unexpected byte sequence for micro sign
    The sign for microsecond (U+0085, MICRO SIGN) was encoded to '0x c2 b5'
    by UTF-8 character encoding scheme. But the byte sequence was converted
    to '0x c3 82 c2 b5' in a previous commit. As a result, the byte
    sequence cannot represent microsecond sign in UTF-8 or ASCII. This
    may confuse developers.
    
    This commit replaces the sign to string expression with 'microseconds'
    to purge superfluous troubles.
    
    Fixes: 5c697e5b46ef("ALSA: firewire-lib: remove rx_blocks_for_midi quirk")
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/amdtp.c
+++ sound/firewire/amdtp.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define MAX_MIDI_RX_BLOCKS	8
 
-#define TRANSFER_DELAY_TICKS	0x2e00 /* 479.17 Âµs */
+#define TRANSFER_DELAY_TICKS	0x2e00 /* 479.17 microseconds */
 
 /* isochronous header parameters */
 #define ISO_DATA_LENGTH_SHIFT	16
