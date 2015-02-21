0e8bfb029cd9137453f3671c2e92a85b2e38a619
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-02-21 23:55:00 +0900

    ALSA: fireworks/bebob/dice/oxfw: make it possible to shutdown safely
    A part of these drivers, especially BeBoB driver, are programmed to wait
    some events. Thus the drivers should not destroy any data in .remove()
    context.
    
    This commit moves some destructors from 'struct fw_driver.remove()' to
    'struct snd_card.private_free()' to shutdown safely.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Cc: <stable@vger.kernel.org> # 3.19+
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/dice/dice.c
+++ sound/firewire/dice/dice.c
@@ -237,6 +237,7 @@ static void dice_card_free(struct snd_card *card)
 {
 	struct snd_dice *dice = card->private_data;
 
+	snd_dice_stream_destroy_duplex(dice);
 	snd_dice_transaction_destroy(dice);
 	fw_unit_put(dice->unit);
 
@@ -313,10 +314,6 @@ error:
 static void dice_remove(struct fw_unit *unit)
 {
 	struct snd_dice *dice = dev_get_drvdata(&unit->device);
-
-	snd_card_disconnect(dice->card);
-
-	snd_dice_stream_destroy_duplex(dice);
 
 	/* No need to wait for releasing card object in this context. */
 	snd_card_free_when_closed(dice->card);
