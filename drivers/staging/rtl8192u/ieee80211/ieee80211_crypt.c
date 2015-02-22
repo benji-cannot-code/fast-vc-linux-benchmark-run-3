5c355825598760c2731f843c1349ffc5262f9f4e
Navya Sri Nizamkari <navyasri.tech@gmail.com>
2015-02-22 22:11:20 +0530

    staging: rtl8192u: Remove unnecessary comment
    The <linux/config.h> header file is not used anymore,
    so it is commented out in this file.This patch deletes
    that unnecessary comment.
    
    Signed-off-by: Navya Sri Nizamkari <navyasri.tech@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/ieee80211_crypt.c
+++ drivers/staging/rtl8192u/ieee80211/ieee80211_crypt.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-//#include <linux/config.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
