ccd6e044377405e72593551d533e03c7c899cc81
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-02-23 14:57:59 -0700

    staging: comedi: amplc_dio200_common: remove unnecessary 'counter_number' checks
    The 'counter_number' in these functions is the comedi channel number from the
    chanspec. The comedi core validates the chanspec before calling the driver
    functions. Remove the unnecessary checks.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_dio200_common.c
+++ drivers/staging/comedi/drivers/amplc_dio200_common.c
@@ -578,8 +578,6 @@ static int dio200_subdev_8254_set_gate_src(struct comedi_device *dev,
 
 	if (!board->has_clk_gat_sce)
 		return -1;
-	if (counter_number > 2)
-		return -1;
 	if (gate_src > (board->is_pcie ? 31 : 7))
 		return -1;
 
@@ -599,8 +597,6 @@ static int dio200_subdev_8254_get_gate_src(struct comedi_device *dev,
 
 	if (!board->has_clk_gat_sce)
 		return -1;
-	if (counter_number > 2)
-		return -1;
 
 	return subpriv->gate_src[counter_number];
 }
@@ -615,8 +611,6 @@ static int dio200_subdev_8254_set_clock_src(struct comedi_device *dev,
 	unsigned char byte;
 
 	if (!board->has_clk_gat_sce)
-		return -1;
-	if (counter_number > 2)
 		return -1;
 	if (clock_src > (board->is_pcie ? 31 : 7))
 		return -1;
@@ -638,8 +632,6 @@ static int dio200_subdev_8254_get_clock_src(struct comedi_device *dev,
 	unsigned clock_src;
 
 	if (!board->has_clk_gat_sce)
-		return -1;
-	if (counter_number > 2)
 		return -1;
 
 	clock_src = subpriv->clock_src[counter_number];
