b64f0a8dfc37b55119b0abeb3f10252dedb3ad5f
Al Viro <viro@zeniv.linux.org.uk>
2015-02-23 05:46:21 -0500

    whack-a-mole: there's no point doing set_fs(USER_DS) in sigframe setup
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/frv/kernel/signal.c
+++ arch/frv/kernel/signal.c
@@ -177,8 +177,6 @@ static int setup_frame(struct ksignal *ksig, sigset_t *set)
 	struct sigframe __user *frame;
 	int rsig, sig = ksig->sig;
 
-	set_fs(USER_DS);
-
 	frame = get_sigframe(ksig, sizeof(*frame));
 
 	if (!access_ok(VERIFY_WRITE, frame, sizeof(*frame)))
@@ -257,8 +255,6 @@ static int setup_rt_frame(struct ksignal *ksig, sigset_t *set)
 {
 	struct rt_sigframe __user *frame;
 	int rsig, sig = ksig->sig;
-
-	set_fs(USER_DS);
 
 	frame = get_sigframe(ksig, sizeof(*frame));
 
