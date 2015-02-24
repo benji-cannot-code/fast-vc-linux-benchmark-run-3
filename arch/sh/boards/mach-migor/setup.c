7538be08d407802bf8d8b65b290cbe89fa08fec0
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2015-02-24 02:07:07 +0000

    mmc: sh_mobile_sdhi: remove sh_mobile_sdhi_info
    Current sh_mobile_sdhi's platform data is set via sh_mobile_sdhi_info
    and it is just copied to tmio_mmc_data.
    Now, tmio mmc platform data is specified via tmio_mmc_data.
    This patch replace sh_mobile_sdhi_info to tmio_mmc_data
    
    struct sh_mobile_sdhi_info {      -> struct tmio_mmc_data {
            int dma_slave_tx;         ->    void            *chan_priv_tx;
            int dma_slave_rx;         ->    void            *chan_priv_rx;
            unsigned long tmio_flags; ->    unsigned long   flags;
            unsigned long tmio_caps;  ->    unsigned long   capabilities;
            unsigned long tmio_caps2; ->    unsigned long   capabilities2;
            u32 tmio_ocr_mask;        ->    u32             ocr_mask;
            unsigned int cd_gpio;     ->    unsigned int    cd_gpio;
    };                                      unsigned int    hclk;
                                            void (*set_pwr)(...);
                                            void (*set_clk_div)(...);
                                          };
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- arch/sh/boards/mach-migor/setup.c
+++ arch/sh/boards/mach-migor/setup.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/host.h>
 #include <linux/mmc/sh_mobile_sdhi.h>
 #include <linux/mtd/physmap.h>
+#include <linux/mfd/tmio.h>
 #include <linux/mtd/nand.h>
 #include <linux/i2c.h>
 #include <linux/regulator/fixed.h>
@@ -409,10 +410,10 @@ static struct resource sdhi_cn9_resources[] = {
 	},
 };
 
-static struct sh_mobile_sdhi_info sh7724_sdhi_data = {
-	.dma_slave_tx	= SHDMA_SLAVE_SDHI0_TX,
-	.dma_slave_rx	= SHDMA_SLAVE_SDHI0_RX,
-	.tmio_caps      = MMC_CAP_SDIO_IRQ,
+static struct tmio_mmc_data sh7724_sdhi_data = {
+	.chan_priv_tx	= (void *)SHDMA_SLAVE_SDHI0_TX,
+	.chan_priv_rx	= (void *)SHDMA_SLAVE_SDHI0_RX,
+	.capabilities	= MMC_CAP_SDIO_IRQ,
 };
 
 static struct platform_device sdhi_cn9_device = {
