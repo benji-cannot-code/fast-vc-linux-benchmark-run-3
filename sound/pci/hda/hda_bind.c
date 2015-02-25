9cd5047d670f8ae76792cc27ae9dff55eb242ddb
Takashi Iwai <tiwai@suse.de>
2015-02-25 14:42:38 +0100

    ALSA: hda - Add regmap support
    This patch adds an infrastructure to support regmap-based verb
    accesses.  Because o the asymmetric nature of HD-audio verbs,
    especially the amp verbs, we need to translate the verbs as a sort of
    pseudo registers to be mapped uniquely in regmap.
    
    In this patch, a pseudo register is built from the NID, the
    AC_VERB_GET_* and 8bit parameters, i.e. almost in the form to be sent
    to HD-audio bus but without codec address field.  OTOH, for writing,
    the same pseudo register is translated to AC_VERB_SET_* automatically.
    The AC_VERB_SET_AMP_* verb is re-encoded from the corresponding
    AC_VERB_GET_AMP_* verb and parameter at writing.
    
    Some verbs has a single command for read but multiple for writes.  A
    write for such a verb is split automatically to multiple verbs.
    
    The patch provides also a few handy helper functions.  They are
    designed to be accessible even without regmap.  When no regmap is set
    up (e.g. before the codec device instantiation), the direct hardware
    access is used.  Also, it tries to avoid the unnecessary power-up.
    The power up/down sequence is performed only on demand.
    
    The codec driver needs to call snd_hdac_regmap_exit() and
    snd_hdac_regmap_exit() at probe and remove if it wants the regmap
    access.
    
    There is one flag added to hdac_device.  When the flag lazy_cache is
    set, regmap helper ignores a write for a suspended device and returns
    as if it was actually written.  It reduces the hardware access pretty
    much, e.g. when adjusting the mixer volume while in idle.  This
    assumes that the driver will sync the cache later at resume properly,
    so use it carefully.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_bind.c
+++ sound/pci/hda/hda_bind.c
@@ -76,6 +76,9 @@ static int hda_codec_driver_probe(struct device *dev)
 	err = codec_refresh_name(codec, codec->preset->name);
 	if (err < 0)
 		goto error;
+	err = snd_hdac_regmap_init(&codec->core);
+	if (err < 0)
+		goto error;
 
 	if (!try_module_get(owner)) {
 		err = -EINVAL;
@@ -99,6 +102,7 @@ static int hda_codec_driver_probe(struct device *dev)
 		snd_hda_codec_register(codec);
 	}
 
+	codec->core.lazy_cache = true;
 	return 0;
 
  error_module:
--- sound/pci/hda/hda_codec.h
+++ sound/pci/hda/hda_codec.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/hwdep.h>
 #include <sound/hdaudio.h>
 #include <sound/hda_verbs.h>
+#include <sound/hda_regmap.h>
 
 /*
  * Structures
