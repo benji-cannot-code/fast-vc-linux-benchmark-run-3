d01167825ded08743b613219d7f724a8add5f171
Micky Ching <micky_ching@realsil.com.cn>
2015-02-25 13:50:10 +0800

    mfd: rtsx: Update PETXCFG address
    PETXCFG is defined at 0xFF03, the old 0xFE49 not used any more.
    
    Signed-off-by: Micky Ching <micky_ching@realsil.com.cn>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/rtsx_pci.h
+++ include/linux/mfd/rtsx_pci.h
@@ -573,7 +573,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MSGTXDATA2			0xFE46
 #define MSGTXDATA3			0xFE47
 #define MSGTXCTL			0xFE48
-#define PETXCFG				0xFE49
 #define LTR_CTL				0xFE4A
 #define OBFF_CFG			0xFE4C
 
@@ -607,6 +606,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DUMMY_REG_RESET_0		0xFE90
 
 #define AUTOLOAD_CFG_BASE		0xFF00
+#define PETXCFG				0xFF03
 
 #define PM_CTRL1			0xFF44
 #define PM_CTRL2			0xFF45
