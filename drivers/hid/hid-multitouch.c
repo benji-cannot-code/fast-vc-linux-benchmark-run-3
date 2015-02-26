13a9e532c3b7102c262ca5ee7cf4a747d9745bed
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2015-02-26 13:39:48 -0500

    HID: remove 2 unused usb.h includes
    These 2 are left over from the USB dependency cleaning, so there is
    no need to keep them.
    
    [jkosina@suse.cz: fix filename]
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/hid.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/usb.h>
 #include <linux/input/mt.h>
 #include <linux/string.h>
 
