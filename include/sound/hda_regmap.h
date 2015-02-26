bd1e410984fac3e4b30179544909e9427974f986
Takashi Iwai <tiwai@suse.de>
2015-02-26 12:29:03 +0100

    ALSA: hda - Allow driver to add vendor-specific verbs for regmap
    Codecs may have own vendor-specific verbs, and we need to allow each
    driver to give such verbs for cached accesses.  Here a verb can be put
    into a single array and looked through it at readable and writeable
    callbacks.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/hda_regmap.h
+++ include/sound/hda_regmap.h
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int snd_hdac_regmap_init(struct hdac_device *codec);
 void snd_hdac_regmap_exit(struct hdac_device *codec);
-
+int snd_hdac_regmap_add_vendor_verb(struct hdac_device *codec,
+				    unsigned int verb);
 int snd_hdac_regmap_read_raw(struct hdac_device *codec, unsigned int reg,
 			     unsigned int *val);
 int snd_hdac_regmap_write_raw(struct hdac_device *codec, unsigned int reg,
