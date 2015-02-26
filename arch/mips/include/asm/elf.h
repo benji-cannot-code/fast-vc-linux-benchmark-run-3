58a0f91d3ebe4c91a98de00aaeb2720e77ce19fe
Markos Chandras <markos.chandras@imgtec.com>
2015-02-26 11:11:30 +0000

    MIPS: asm: elf: Set O32 default FPU flags
    Set good default FPU flags (FR0) for O32 binaries similar to what the
    kernel does for the N64/N32 ones. This also fixes a regression
    introduced in commit 46490b572544 ("MIPS: kernel: elf: Improve the
    overall ABI and FPU mode checks") when MIPS_O32_FP64_SUPPORT is
    disabled. In that case, the mips_set_personality_fp() did not set the
    FPU mode at all because it assumed that the FPU mode was already set
    properly. That led to O32 userland problems.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Reported-by: Mans Rullgard <mans@mansr.com>
    Fixes: 46490b572544 ("MIPS: kernel: elf: Improve the overall ABI and FPU mode checks")
    Tested-by: Mans Rullgard <mans@mansr.com>
    Tested-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Cc: Matthew Fortune <Matthew.Fortune@imgtec.com>
    Cc: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/9344/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/elf.h
+++ arch/mips/include/asm/elf.h
@@ -295,6 +295,9 @@ do {									\
 	if (personality(current->personality) != PER_LINUX)		\
 		set_personality(PER_LINUX);				\
 									\
+	clear_thread_flag(TIF_HYBRID_FPREGS);				\
+	set_thread_flag(TIF_32BIT_FPREGS);				\
+									\
 	mips_set_personality_fp(state);					\
 									\
 	current->thread.abi = &mips_abi;				\
@@ -320,6 +323,8 @@ do {									\
 	do {								\
 		set_thread_flag(TIF_32BIT_REGS);			\
 		set_thread_flag(TIF_32BIT_ADDR);			\
+		clear_thread_flag(TIF_HYBRID_FPREGS);			\
+		set_thread_flag(TIF_32BIT_FPREGS);			\
 									\
 		mips_set_personality_fp(state);				\
 									\
