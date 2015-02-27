e84885ef24f51faa3f100e8cdca0d8e35f579879
Javier Martinez Canillas <javier.martinez@collabora.co.uk>
2015-02-27 06:37:48 +0100

    platform/chrome: cros_ec_lpc - Include linux/io.h header file
    The driver uses the inb() and outb() I/O functions so should
    include the header file that has these functions definitions.
    
    This patch fixes the following error when the header is not
    explicitly included:
    
    drivers/platform/chrome//cros_ec_lpc.c: In function ‘ec_response_timed_out’:
    drivers/platform/chrome//cros_ec_lpc.c:40:3: error: implicit declaration of function ‘inb’ [-Werror=implicit-function-declaration]
    drivers/platform/chrome//cros_ec_lpc.c: In function ‘cros_ec_cmd_xfer_lpc’:
    drivers/platform/chrome//cros_ec_lpc.c:75:3: error: implicit declaration of function ‘outb’ [-Werror=implicit-function-declaration]
    
    Signed-off-by: Javier Martinez Canillas <javier.martinez@collabora.co.uk>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- drivers/platform/chrome/cros_ec_lpc.c
+++ drivers/platform/chrome/cros_ec_lpc.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/dmi.h>
 #include <linux/delay.h>
+#include <linux/io.h>
 #include <linux/mfd/cros_ec.h>
 #include <linux/mfd/cros_ec_commands.h>
 #include <linux/module.h>
