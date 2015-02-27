72833c699a531de73113fafaae35046c8b5b0aa9
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2015-02-27 08:42:23 -0300

    [media] v4l2: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/pvrusb2/pvrusb2-v4l2.c
+++ drivers/media/usb/pvrusb2/pvrusb2-v4l2.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/slab.h>
-#include <linux/version.h>
 #include "pvrusb2-context.h"
 #include "pvrusb2-hdw.h"
 #include "pvrusb2.h"
