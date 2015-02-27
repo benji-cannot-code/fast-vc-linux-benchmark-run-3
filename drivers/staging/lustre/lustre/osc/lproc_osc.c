5f9fc1f7723fa7dbc9b868d19de331212ac66ec6
Dilek Uzulmez <dilekuzulmez@gmail.com>
2015-02-27 15:05:45 +0200

    Staging: lustre: Added #include <linux/statfs.h> instead of #include <asm/statfs.h>
    The following patch fixes the checkpatch.pl warning:
    WARNING: Use #include <linux/statfs.h> instead of #include <asm/statfs.h>
    
    Signed-off-by: Dilek Uzulmez <dilekuzulmez@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/osc/lproc_osc.c
+++ drivers/staging/lustre/lustre/osc/lproc_osc.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define DEBUG_SUBSYSTEM S_CLASS
 
-#include <asm/statfs.h>
+#include <linux/statfs.h>
 #include "../include/obd_cksum.h"
 #include "../include/obd_class.h"
 #include "../include/lprocfs_status.h"
