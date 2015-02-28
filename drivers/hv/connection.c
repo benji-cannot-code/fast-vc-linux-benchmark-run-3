40208d318e8a031e892da97dbb85a662fd87fa55
K. Y. Srinivasan <kys@microsoft.com>
2015-02-28 11:18:21 -0800

    Drivers: hv: vmbus: Get rid of some unnecessary messages
    Currently we log messages when either we are not able to map an ID to a
    channel or when the channel does not have a callback associated
    (in the channel interrupt handling path). These messages don't add
    any value, get rid of them.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hv/connection.c
+++ drivers/hv/connection.c
@@ -319,10 +319,8 @@ static void process_chn_event(u32 relid)
 	 */
 	channel = pcpu_relid2channel(relid);
 
-	if (!channel) {
-		pr_err("channel not found for relid - %u\n", relid);
+	if (!channel)
 		return;
-	}
 
 	/*
 	 * A channel once created is persistent even when there
@@ -357,10 +355,7 @@ static void process_chn_event(u32 relid)
 			else
 				bytes_to_read = 0;
 		} while (read_state && (bytes_to_read != 0));
-	} else {
-		pr_err("no channel callback for relid - %u\n", relid);
 	}
-
 }
 
 /*
