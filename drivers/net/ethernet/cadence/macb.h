7b2b366bc89d145d3eabd4e80e7bd12432897055
Arun Chandran <achandran@mvista.com>
2015-03-01 11:38:03 +0530

    net: macb: Properly add DMACFG bit definitions
    Add *_SIZE macros for the bits ENDIA_DESC and
    ENDIA_PKT
    
    Signed-off-by: Arun Chandran <achandran@mvista.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cadence/macb.h
+++ drivers/net/ethernet/cadence/macb.h
@@ -231,8 +231,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GEM_FBLDO_OFFSET	0 /* fixed burst length for DMA */
 #define GEM_FBLDO_SIZE		5
 #define GEM_ENDIA_DESC_OFFSET	6 /* endian swap mode for management descriptor access */
+#define GEM_ENDIA_DESC_SIZE	1
 #define GEM_ENDIA_PKT_OFFSET	7 /* endian swap mode for packet data access */
-#define GEM_ENDIA_SIZE		1
+#define GEM_ENDIA_PKT_SIZE	1
 #define GEM_RXBMS_OFFSET	8 /* RX packet buffer memory size select */
 #define GEM_RXBMS_SIZE		2
 #define GEM_TXPBMS_OFFSET	10 /* TX packet buffer memory size select */
