98f4d2baca6482cd1f1576cdf3c7e105fde78805
Gilles Risch <gilles.risch@gmail.com>
2015-03-01 17:11:05 -0300

    [media] Basic support for the Elgato EyeTV Hybrid INT 2008 USB Stick
    This patch will add basic support for the Elgato EyeTV Hybrid INT
    2008 USB Stick.
    
    Signed-off-by: Gilles Risch <gilles.risch@gmail.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/usb/em28xx/em28xx.h
+++ drivers/media/usb/em28xx/em28xx.h
@@ -144,6 +144,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM28178_BOARD_PCTV_292E                   94
 #define EM2861_BOARD_LEADTEK_VC100                95
 #define EM28178_BOARD_TERRATEC_T2_STICK_HD        96
+#define EM2884_BOARD_ELGATO_EYETV_HYBRID_2008     97
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
