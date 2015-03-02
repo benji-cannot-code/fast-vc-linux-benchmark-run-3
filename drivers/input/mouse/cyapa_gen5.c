dd5bbf235717456f8cb99f081a83d1506c70c4f3
Dudley Du <dudl@cypress.com>
2015-03-02 09:37:59 -0800

    Input: cyapa - fix unaligned functions redefinition error
    Use asm/unaligned.h instead of linux/unaligned/access_ok.h header file to
    fix compiling issues such as following while doing cross platform
    compiling:
    
    "include/linux/unaligned/access_ok.h:7:19: error: redefinition of
     'get_unaligned_le16'
    ...
    include/linux/unaligned/le_struct.h:6:19: note: previous definition of
     'get_unaligned_le16' was here".
    
    Reported-by: kbuild test robot <kbuild-all@01.org>
    Signed-off-by: Dudley Du <dudl@cypress.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/mouse/cyapa_gen5.c
+++ drivers/input/mouse/cyapa_gen5.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/completion.h>
 #include <linux/slab.h>
-#include <linux/unaligned/access_ok.h>
+#include <asm/unaligned.h>
 #include <linux/crc-itu-t.h>
 #include "cyapa.h"
 
