dcea05a8392fcdff30d9fd3186a2b850785fa372
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-03-02 15:25:00 -0500

    staging: rtl8723au: Clean up FindMinimumRSSI()
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/hal/odm.c
+++ drivers/staging/rtl8723au/hal/odm.c
@@ -1154,9 +1154,7 @@ void odm_DynamicTxPower23aInit(struct dm_odm_t *pDM_Odm)
 }
 
 static void
-FindMinimumRSSI(
-	struct rtw_adapter *pAdapter
-	)
+FindMinimumRSSI(struct rtw_adapter *pAdapter)
 {
 	struct hal_data_8723a *pHalData = GET_HAL_DATA(pAdapter);
 	struct dm_priv *pdmpriv = &pHalData->dmpriv;
@@ -1164,11 +1162,11 @@ FindMinimumRSSI(
 
 	/* 1 1.Determine the minimum RSSI */
 
-	if ((!pDM_Odm->bLinked) &&
-	    (pdmpriv->EntryMinUndecoratedSmoothedPWDB == 0))
+	if (!pDM_Odm->bLinked && !pdmpriv->EntryMinUndecoratedSmoothedPWDB)
 		pdmpriv->MinUndecoratedPWDBForDM = 0;
 	else
-		pdmpriv->MinUndecoratedPWDBForDM = pdmpriv->EntryMinUndecoratedSmoothedPWDB;
+		pdmpriv->MinUndecoratedPWDBForDM =
+			pdmpriv->EntryMinUndecoratedSmoothedPWDB;
 }
 
 static void odm_RSSIMonitorCheck(struct dm_odm_t *pDM_Odm)
