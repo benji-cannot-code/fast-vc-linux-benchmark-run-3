ef8e97b3ab982b673851ac0807db5c7d3471c6fe
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-03-02 14:35:41 +0200

    iwlwifi: mvm: remove unneeded include iwl-fw-error-dump.h
    The functions related to firmware error dump moved. No need
    for this unclude anymore.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/iwlwifi/mvm/ops.c
@@ -83,7 +83,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rs.h"
 #include "fw-api-scan.h"
 #include "time-event.h"
-#include "iwl-fw-error-dump.h"
 
 #define DRV_DESCRIPTION	"The new Intel(R) wireless AGN driver for Linux"
 MODULE_DESCRIPTION(DRV_DESCRIPTION);
