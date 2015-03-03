2f06b5c133007dac89c658a6fbab8edf9c0044af
Tero Kristo <t-kristo@ti.com>
2015-03-03 10:51:01 +0200

    clk: ti: move omap2_clk_enable_init_clocks under clock driver
    This is no longer used outside clock driver, so move it under the driver
    and remove the export for it from the global header file.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- drivers/clk/ti/clk-816x.c
+++ drivers/clk/ti/clk-816x.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk-provider.h>
 #include <linux/clk/ti.h>
 
+#include "clock.h"
+
 static struct ti_dt_clk dm816x_clks[] = {
 	DT_CLK(NULL, "sys_clkin", "sys_clkin_ck"),
 	DT_CLK(NULL, "timer_sys_ck", "sys_clkin_ck"),
