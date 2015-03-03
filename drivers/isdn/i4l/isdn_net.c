e3d71782cbcfe5abf331e4d12072f89cbb80d2b7
Geert Uytterhoeven <geert+renesas@glider.be>
2015-03-03 11:58:08 +0100

    isdn: fix grammar in comment
    Fix grammar / typo in comment.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Cc: Karsten Keil <isdn@linux-pingi.de>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/isdn/i4l/isdn_net.c
+++ drivers/isdn/i4l/isdn_net.c
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * About SOFTNET:
  * Most of the changes were pretty obvious and basically done by HE already.
  *
- * One problem of the isdn net device code is that is uses struct net_device
+ * One problem of the isdn net device code is that it uses struct net_device
  * for masters and slaves. However, only master interface are registered to
  * the network layer, and therefore, it only makes sense to call netif_*
  * functions on them.
