cf04c92c9a48155a8e53ddf59a8165bdc7000beb
Chen Gang <gang.chen.5i5j@gmail.com>
2015-03-03 11:33:15 -0500

    c6x: kernel: setup: Include "linux/console.h"
    Or c6x will cause building break for allmodconfig, the related error:
    
        CC      arch/c6x/kernel/setup.o
      arch/c6x/kernel/setup.c: In function 'setup_arch':
      arch/c6x/kernel/setup.c:433:2: error: 'conswitchp' undeclared (first use in this function)
        conswitchp = &dummy_con;
        ^
      arch/c6x/kernel/setup.c:433:2: note: each undeclared identifier is reported only once for each function it appears in
      arch/c6x/kernel/setup.c:433:16: error: 'dummy_con' undeclared (first use in this function)
        conswitchp = &dummy_con;
                      ^
    
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>
    [removed unnecessary #ifdef around include]
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- arch/c6x/kernel/setup.c
+++ arch/c6x/kernel/setup.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/fs.h>
 #include <linux/of.h>
-
+#include <linux/console.h>
 
 #include <asm/sections.h>
 #include <asm/div64.h>
