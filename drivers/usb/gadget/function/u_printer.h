6cb58fbfd5e12183381780b97d6bda300a9d69fc
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2015-03-03 10:52:28 +0100

    usb: gadget: f_printer: convert to new function interface with backward compatibility
    In order to add configfs support, a usb function must be converted to use
    the new interface. This patch converts the function to the new interface
    and provides backward compatiblity layer, which can be removed after
    all its users are converted to use the new interface.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

---
+++ drivers/usb/gadget/function/u_printer.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * u_printer.h
+ *
+ * Utility definitions for the printer function
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com
+ *
+ * Author: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef U_PRINTER_H
+#define U_PRINTER_H
+
+#include <linux/usb/composite.h>
+
+#define PNP_STRING_LEN			1024
+
+struct f_printer_opts {
+	struct usb_function_instance	func_inst;
+	int				minor;
+	char				pnp_string[PNP_STRING_LEN];
+	unsigned			q_len;
+};
+
+#endif /* U_PRINTER_H */
