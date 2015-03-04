2698371dc00609b10d340b4400abb817d57f88ef
Eric W. Biederman <ebiederm@xmission.com>
2015-03-03 19:11:20 -0600

    mpls: Add a sysctl to control the size of the mpls label table
    This sysctl gives two benefits.  By defaulting the table size to 0
    mpls even when compiled in and enabled defaults to not forwarding
    any packets.  This prevents unpleasant surprises for users.
    
    The other benefit is that as mpls labels are allocated locally a dense
    table a small dense label table may be used which saves memory and
    is extremely simple and efficient to implement.
    
    This sysctl allows userspace to choose the restrictions on the label
    table size userspace applications need to cope with.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/mpls.h
+++ include/net/netns/mpls.h
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NETNS_MPLS_H__
 
 struct mpls_route;
+struct ctl_table_header;
 
 struct netns_mpls {
 	size_t platform_labels;
 	struct mpls_route __rcu * __rcu *platform_label;
+	struct ctl_table_header *ctl;
 };
 
 #endif /* __NETNS_MPLS_H__ */
