84961f0554fa04449d6e1dc9c5366752b61fb792
Arnd Bergmann <arnd@arndb.de>
2015-03-04 20:49:47 +0100

    of: unittest: fix I2C dependency
    The unittest fails to link if I2C or I2C_MUX is a loadable module:
    
      drivers/built-in.o: In function `selftest_i2c_mux_remove':
      unittest.c:(.text+0xb0ce4): undefined reference to `i2c_del_mux_adapter'
    
    This changes the newly added IS_ENABLED() checks to use IS_BUILTIN()
    instead, which evaluates to false if the other driver is a module.
    
    Reported-by: Chen Gang <gang.chen.5i5j@gmail.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: d5e75500ca401 ("of: unitest: Add I2C overlay unit tests.")
    Signed-off-by: Rob Herring <robh@kernel.org>

--- drivers/of/unittest.c
+++ drivers/of/unittest.c
@@ -980,7 +980,7 @@ static int of_path_platform_device_exists(const char *path)
 	return pdev != NULL;
 }
 
-#if IS_ENABLED(CONFIG_I2C)
+#if IS_BUILTIN(CONFIG_I2C)
 
 /* get the i2c client device instantiated at the path */
 static struct i2c_client *of_path_to_i2c_client(const char *path)
@@ -1446,7 +1446,7 @@ static void of_selftest_overlay_11(void)
 		return;
 }
 
-#if IS_ENABLED(CONFIG_I2C) && IS_ENABLED(CONFIG_OF_OVERLAY)
+#if IS_BUILTIN(CONFIG_I2C) && IS_ENABLED(CONFIG_OF_OVERLAY)
 
 struct selftest_i2c_bus_data {
 	struct platform_device	*pdev;
@@ -1585,7 +1585,7 @@ static struct i2c_driver selftest_i2c_dev_driver = {
 	.id_table = selftest_i2c_dev_id,
 };
 
-#if IS_ENABLED(CONFIG_I2C_MUX)
+#if IS_BUILTIN(CONFIG_I2C_MUX)
 
 struct selftest_i2c_mux_data {
 	int nchans;
@@ -1696,7 +1696,7 @@ static int of_selftest_overlay_i2c_init(void)
 			"could not register selftest i2c bus driver\n"))
 		return ret;
 
-#if IS_ENABLED(CONFIG_I2C_MUX)
+#if IS_BUILTIN(CONFIG_I2C_MUX)
 	ret = i2c_add_driver(&selftest_i2c_mux_driver);
 	if (selftest(ret == 0,
 			"could not register selftest i2c mux driver\n"))
@@ -1708,7 +1708,7 @@ static int of_selftest_overlay_i2c_init(void)
 
 static void of_selftest_overlay_i2c_cleanup(void)
 {
-#if IS_ENABLED(CONFIG_I2C_MUX)
+#if IS_BUILTIN(CONFIG_I2C_MUX)
 	i2c_del_driver(&selftest_i2c_mux_driver);
 #endif
 	platform_driver_unregister(&selftest_i2c_bus_driver);
@@ -1815,7 +1815,7 @@ static void __init of_selftest_overlay(void)
 	of_selftest_overlay_10();
 	of_selftest_overlay_11();
 
-#if IS_ENABLED(CONFIG_I2C)
+#if IS_BUILTIN(CONFIG_I2C)
 	if (selftest(of_selftest_overlay_i2c_init() == 0, "i2c init failed\n"))
 		goto out;
 
