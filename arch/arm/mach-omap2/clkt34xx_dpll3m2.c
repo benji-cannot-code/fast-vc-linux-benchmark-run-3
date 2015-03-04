37ab952cfae93a53e6b3c6484955a4511770c154
Tero Kristo <t-kristo@ti.com>
2015-03-04 11:56:22 +0200

    ARM: OMAP3: clock: remove clock3xxx.c file
    This now only has a couple of variables within it, which are used outside
    the file itself. Move these variables to where they are actually used,
    and remove the file completely as it is now empty.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>

--- arch/arm/mach-omap2/clkt34xx_dpll3m2.c
+++ arch/arm/mach-omap2/clkt34xx_dpll3m2.c
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CYCLES_PER_MHZ			1000000
 
+struct clk *sdrc_ick_p, *arm_fck_p;
+
 /*
  * CORE DPLL (DPLL3) M2 divider rate programming functions
  *
