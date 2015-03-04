85343201a30884189e1e041774fc291a47c2bf43
Chen Gang <xili_gchen_5257@hotmail.com>
2015-03-04 11:09:35 +0800

    c6x: include: asm: dma-mapping: Add dummy dma_sync_single_range_for_device
    c6x does not implement get_dma_ops() for dma-mapping-common.h. And in
    dma-mapping-common.h, dma_sync_single_range_for_device() may be dummy.
    So c6x can just define a dummy function for allmodconfig building.
    
        CC [M]  drivers/net/ethernet/micrel/ks8851.o
      drivers/net/ethernet/micrel/ks8842.c: In function 'ks8842_tx_frame_dma':
      drivers/net/ethernet/micrel/ks8842.c:453:2: error: implicit declaration of function 'dma_sync_single_range_for_device' [-Werror=implicit-function-declaration]
        dma_sync_single_range_for_device(adapter->dev,
        ^
    
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- arch/c6x/include/asm/dma-mapping.h
+++ arch/c6x/include/asm/dma-mapping.h
@@ -18,6 +18,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define dma_supported(d, m)	1
 
+static inline void dma_sync_single_range_for_device(struct device *dev,
+						    dma_addr_t addr,
+						    unsigned long offset,
+						    size_t size,
+						    enum dma_data_direction dir)
+{
+}
+
 static inline int dma_set_mask(struct device *dev, u64 dma_mask)
 {
 	if (!dev->dma_mask || !dma_supported(dev, dma_mask))
