e254e1cfd1322326a264490d1d5dbd2d42e78ff1
Wenyou Yang <wenyou.yang@atmel.com>
2015-03-04 09:44:45 +0800

    ARM: at91: pm: change at91_pm_set_standby() to static
    Since at91_pm_set_standby() will not be used out of the pm.c file,
    change its attribute from extern to static, remove its declaration as well.
    
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>
    Acked-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/pm.h
+++ arch/arm/mach-at91/pm.h
@@ -16,12 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/at91_ramc.h>
 
-#ifdef CONFIG_PM
-extern void at91_pm_set_standby(void (*at91_standby)(void));
-#else
-static inline void at91_pm_set_standby(void (*at91_standby)(void)) { }
-#endif
-
 /*
  * The AT91RM9200 goes into self-refresh mode with this command, and will
  * terminate self-refresh automatically on the next SDRAM access.
