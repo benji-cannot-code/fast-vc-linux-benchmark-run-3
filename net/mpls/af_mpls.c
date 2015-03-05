845f701bf2d0d50c801a3c2f4198f47b1f120179
Stephen Rothwell <sfr@canb.auug.org.au>
2015-03-05 13:37:05 +1100

    mpls: using vzalloc requires including vmalloc.h
    Fixes this build error:
    
    net/mpls/af_mpls.c: In function 'resize_platform_label_table':
    net/mpls/af_mpls.c:767:4: error: implicit declaration of function 'vzalloc' [-Werror=implicit-function-declaration]
        labels = vzalloc(size);
        ^
    
    Fixes: 7720c01f3f59 ("mpls: Add a sysctl to control the size of the mpls label table")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mpls/af_mpls.c
+++ net/mpls/af_mpls.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 #include <linux/ipv6.h>
 #include <linux/mpls.h>
+#include <linux/vmalloc.h>
 #include <net/ip.h>
 #include <net/dst.h>
 #include <net/sock.h>
