79e610f683cf83039ed24508dfe9fef1f19c48de
David Howells <dhowells@redhat.com>
2015-03-06 14:24:37 +0000

    VFS: Fix up some ->d_inode accesses in the chelsio driver
    Fix up some ->d_inode accesses in the chelsio driver.
    
     (1) FILE_DATA() should just be replaced with file_inode().
    
     (2) set_debugfs_file_size() should be removed and debugfs_create_file_size()
         should be used to create the file.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/net/ethernet/chelsio/cxgb4/cxgb4_debugfs.h
+++ drivers/net/ethernet/chelsio/cxgb4/cxgb4_debugfs.h
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/export.h>
 
-#define FILE_DATA(_file) ((_file)->f_path.dentry->d_inode)
-
 #define DEFINE_SIMPLE_DEBUGFS_FILE(name) \
 static int name##_open(struct inode *inode, struct file *file) \
 { \
