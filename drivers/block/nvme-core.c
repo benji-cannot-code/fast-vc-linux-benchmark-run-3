11de144e2249417b4d95ac046c1af62960491c46
Jens Axboe <axboe@fb.com>
2015-03-06 12:56:13 -0700

    NVMe: increase depth of admin queue
    Usually the admin queue depth of 64 is plenty, but for some use cases we
    really need it larger. Examples are use cases like MAT, where you have
    to touch all of NAND for init/format like purposes. In those cases, we
    see a good 2x increase with an increased queue depth.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>
    Acked-by: Keith Busch <keith.busch@intel.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NVME_MINORS		(1U << MINORBITS)
 #define NVME_Q_DEPTH		1024
-#define NVME_AQ_DEPTH		64
+#define NVME_AQ_DEPTH		256
 #define SQ_SIZE(depth)		(depth * sizeof(struct nvme_command))
 #define CQ_SIZE(depth)		(depth * sizeof(struct nvme_completion))
 #define ADMIN_TIMEOUT		(admin_timeout * HZ)
