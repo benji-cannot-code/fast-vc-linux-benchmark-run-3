d6ef12ff70c25fd024f4306864f03db45e628402
Dilek Uzulmez <dilekuzulmez@gmail.com>
2015-03-07 23:33:25 +0200

    Staging: rtl8192u: Replace #include <asm/string.h>
    The following patch fixes the checkpatch.pl warning:
    WARNING: Use #include <linux/string.h> instead of #include <asm/string.h>
    
    Signed-off-by: Dilek Uzulmez <dilekuzulmez@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/ieee80211_crypt_wep.c
+++ drivers/staging/rtl8192u/ieee80211/ieee80211_crypt_wep.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/random.h>
 #include <linux/skbuff.h>
-#include <asm/string.h>
+#include <linux/string.h>
 
 #include "ieee80211.h"
 
