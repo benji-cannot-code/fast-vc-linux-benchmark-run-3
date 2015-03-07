b19e18e98e71d3f568b88d5009648a8b97ee911f
Deng-Cheng Zhu <dengcheng.zhu@imgtec.com>
2015-03-07 10:30:31 -0800

    MIPS: jz4740: Implement read_sched_clock
    Use jz4740 timer counter for sched_clock source. This implementation will
    give high resolution cputime accounting.
    
    Signed-off-by: Deng-Cheng Zhu <dengcheng.zhu@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: macro@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/9485/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/jz4740/time.c
+++ arch/mips/jz4740/time.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/time.h>
 
 #include <linux/clockchips.h>
+#include <linux/sched_clock.h>
 
 #include <asm/mach-jz4740/irq.h>
 #include <asm/mach-jz4740/timer.h>
@@ -43,6 +44,11 @@ static struct clocksource jz4740_clocksource = {
 	.mask = CLOCKSOURCE_MASK(16),
 	.flags = CLOCK_SOURCE_IS_CONTINUOUS,
 };
+
+static u64 notrace jz4740_read_sched_clock(void)
+{
+	return jz4740_timer_get_count(TIMER_CLOCKSOURCE);
+}
 
 static irqreturn_t jz4740_clockevent_irq(int irq, void *devid)
 {
@@ -126,6 +132,8 @@ void __init plat_time_init(void)
 
 	if (ret)
 		printk(KERN_ERR "Failed to register clocksource: %d\n", ret);
+
+	sched_clock_register(jz4740_read_sched_clock, 16, clk_rate);
 
 	setup_irq(JZ4740_IRQ_TCU0, &timer_irqaction);
 
