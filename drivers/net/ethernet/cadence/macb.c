ffcbc08cfbaeff084a6aadfccf187e0add4349aa
Cyrille Pitchen <cyrille.pitchen@atmel.com>
2015-03-07 07:23:31 +0100

    net/macb: unify clock management
    Most of the functions from the Common Clk Framework handle NULL pointer as
    input argument.
    
    Since the TX clock is optional, we now set tx_clk to NULL value
    instead of ERR_PTR(-ENOENT) when this clock is not available. This simplifies
    the clock management and avoid the need to test tx_clk value.
    
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@atmel.com>
    Acked-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Acked-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cadence/macb.c
+++ drivers/net/ethernet/cadence/macb.c
@@ -214,6 +214,9 @@ static void macb_set_tx_clk(struct clk *clk, int speed, struct net_device *dev)
 {
 	long ferr, rate, rate_rounded;
 
+	if (!clk)
+		return;
+
 	switch (speed) {
 	case SPEED_10:
 		rate = 2500000;
@@ -293,8 +296,7 @@ static void macb_handle_link_change(struct net_device *dev)
 
 	spin_unlock_irqrestore(&bp->lock, flags);
 
-	if (!IS_ERR(bp->tx_clk))
-		macb_set_tx_clk(bp->tx_clk, phydev->speed, dev);
+	macb_set_tx_clk(bp->tx_clk, phydev->speed, dev);
 
 	if (status_change) {
 		if (phydev->link) {
@@ -2265,6 +2267,8 @@ static int macb_probe(struct platform_device *pdev)
 	}
 
 	tx_clk = devm_clk_get(&pdev->dev, "tx_clk");
+	if (IS_ERR(tx_clk))
+		tx_clk = NULL;
 
 	err = clk_prepare_enable(pclk);
 	if (err) {
@@ -2278,13 +2282,10 @@ static int macb_probe(struct platform_device *pdev)
 		goto err_out_disable_pclk;
 	}
 
-	if (!IS_ERR(tx_clk)) {
-		err = clk_prepare_enable(tx_clk);
-		if (err) {
-			dev_err(&pdev->dev, "failed to enable tx_clk (%u)\n",
-				err);
-			goto err_out_disable_hclk;
-		}
+	err = clk_prepare_enable(tx_clk);
+	if (err) {
+		dev_err(&pdev->dev, "failed to enable tx_clk (%u)\n", err);
+		goto err_out_disable_hclk;
 	}
 
 	err = -ENOMEM;
@@ -2456,8 +2457,7 @@ err_out_unregister_netdev:
 err_out_free_netdev:
 	free_netdev(dev);
 err_out_disable_clocks:
-	if (!IS_ERR(tx_clk))
-		clk_disable_unprepare(tx_clk);
+	clk_disable_unprepare(tx_clk);
 err_out_disable_hclk:
 	clk_disable_unprepare(hclk);
 err_out_disable_pclk:
@@ -2481,8 +2481,7 @@ static int macb_remove(struct platform_device *pdev)
 		kfree(bp->mii_bus->irq);
 		mdiobus_free(bp->mii_bus);
 		unregister_netdev(dev);
-		if (!IS_ERR(bp->tx_clk))
-			clk_disable_unprepare(bp->tx_clk);
+		clk_disable_unprepare(bp->tx_clk);
 		clk_disable_unprepare(bp->hclk);
 		clk_disable_unprepare(bp->pclk);
 		free_netdev(dev);
@@ -2500,8 +2499,7 @@ static int __maybe_unused macb_suspend(struct device *dev)
 	netif_carrier_off(netdev);
 	netif_device_detach(netdev);
 
-	if (!IS_ERR(bp->tx_clk))
-		clk_disable_unprepare(bp->tx_clk);
+	clk_disable_unprepare(bp->tx_clk);
 	clk_disable_unprepare(bp->hclk);
 	clk_disable_unprepare(bp->pclk);
 
@@ -2516,8 +2514,7 @@ static int __maybe_unused macb_resume(struct device *dev)
 
 	clk_prepare_enable(bp->pclk);
 	clk_prepare_enable(bp->hclk);
-	if (!IS_ERR(bp->tx_clk))
-		clk_prepare_enable(bp->tx_clk);
+	clk_prepare_enable(bp->tx_clk);
 
 	netif_device_attach(netdev);
 
