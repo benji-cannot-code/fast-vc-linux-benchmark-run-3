9533ebcc3de6b1d5fc7c01f4c5a724621394aef7
Somya Anand <somyaanand214@gmail.com>
2015-03-09 19:18:25 +0530

    Staging: rtl8188eu: Remove unneeded return statement
    This patch removes unnecessary return statement from a void
    function and hence make it more compatible.
    
    This issue is identified by checkpatch.pl
    
    Signed-off-by: Somya Anand <somyaanand214@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_wlan_util.c
+++ drivers/staging/rtl8188eu/core/rtw_wlan_util.c
@@ -764,7 +764,6 @@ void HT_caps_handler(struct adapter *padapter, struct ndis_802_11_var_ie *pIE)
 		else
 			pmlmeinfo->HT_caps.u.HT_cap_element.MCS_rate[i] &= MCS_rate_2R[i];
 	}
-	return;
 }
 
 void HT_info_handler(struct adapter *padapter, struct ndis_802_11_var_ie *pIE)
@@ -785,7 +784,6 @@ void HT_info_handler(struct adapter *padapter, struct ndis_802_11_var_ie *pIE)
 
 	pmlmeinfo->HT_info_enable = 1;
 	memcpy(&(pmlmeinfo->HT_info), pIE->data, pIE->Length);
-	return;
 }
 
 void HTOnAssocRsp(struct adapter *padapter)
