e919284366a36e48bfb6cff08968d03c7cd02ed0
Vineet Gupta <vgupta@synopsys.com>
2015-03-09 14:30:19 +0530

    ARCv2: [nsim*hs*] Support simulation platforms for HS38x cores
    Cc: Grant Likely <grant.likely@linaro.org>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: devicetree@vger.kernel.org
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/plat-sim/platform.c
+++ arch/arc/plat-sim/platform.c
@@ -23,7 +23,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static const char *simulation_compat[] __initconst = {
 	"snps,nsim",
+	"snps,nsim_hs",
 	"snps,nsimosci",
+	"snps,nsimosci_hs",
 	NULL,
 };
 
