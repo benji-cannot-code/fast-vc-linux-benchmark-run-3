88b7970ecd79449f27a74699b330d94d1cedb8ec
Marcel Holtmann <marcel@holtmann.org>
2015-03-10 14:04:52 -0700

    Bluetooth: Increment management interface revision
    This patch increments the management interface revision due to
    introduction of new static address setting and fixes for the
    fast connectable feature.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "smp.h"
 
 #define MGMT_VERSION	1
-#define MGMT_REVISION	8
+#define MGMT_REVISION	9
 
 static const u16 mgmt_commands[] = {
 	MGMT_OP_READ_INDEX_LIST,
