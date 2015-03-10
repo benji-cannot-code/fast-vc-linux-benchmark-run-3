9561f9654adc47dd78b5a269771328f82c8c3290
Jiri Kosina <jkosina@suse.cz>
2015-03-10 10:45:30 +0100

    goldfish: goldfish_tty_probe() is not using 'i' any more
    The only place where 'i' has been used was a dead code that
    got removed by 2a2483685a9de ("goldfish: remove unreachable line
    of code"). Remove the last reference to the variable as well.
    
    Fixes: 2a2483685a9de ("goldfish: remove unreachable line of code")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/tty/goldfish.c
+++ drivers/tty/goldfish.c
@@ -230,7 +230,6 @@ static int goldfish_tty_probe(struct platform_device *pdev)
 {
 	struct goldfish_tty *qtty;
 	int ret = -EINVAL;
-	int i;
 	struct resource *r;
 	struct device *ttydev;
 	void __iomem *base;
