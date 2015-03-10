82f622b2fbc7ddb07e4c14affc885f5157999b0b
Ian Abbott <abbotti@mev.co.uk>
2015-03-10 16:10:52 +0000

    staging: comedi: contec_pci_dio: include new "comedi_pci.h" header
    Include the new "../comedi_pci.h" header instead of <linux/pci.h> and
    "../comedidev.h", which will now get included indirectly.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/contec_pci_dio.c
+++ drivers/staging/comedi/drivers/contec_pci_dio.c
@@ -27,9 +27,8 @@ Configuration Options: not applicable, uses comedi PCI auto config
 */
 
 #include <linux/module.h>
-#include <linux/pci.h>
 
-#include "../comedidev.h"
+#include "../comedi_pci.h"
 
 /*
  * Register map
