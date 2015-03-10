fa97e31a5a3b22ab898f19f236640c8f9c0baf95
Andy Lutomirski <luto@amacapital.net>
2015-03-10 11:05:59 -0700

    x86/asm/entry: Unify and fix initial thread_struct::sp0 values
    x86_32 and x86_64 need slightly different thread_struct::sp0 values, and
    x86_32's was incorrect for init.
    
    This never mattered -- the init thread never runs user code, so we never
    used thread_struct::sp0 for anything.
    
    Fix it and mostly unify them.
    
    Signed-off-by: Andy Lutomirski <luto@amacapital.net>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1b810c1d2e797e27bb4a7708c426101161edd1f6.1426009661.git.luto@amacapital.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 __visible DEFINE_PER_CPU_SHARED_ALIGNED(struct tss_struct, cpu_tss) = {
 	.x86_tss = {
-		.sp0 = (unsigned long)&init_stack + sizeof(init_stack),
+		.sp0 = TOP_OF_INIT_STACK,
 #ifdef CONFIG_X86_32
 		.ss0 = __KERNEL_DS,
 		.ss1 = __KERNEL_CS,
