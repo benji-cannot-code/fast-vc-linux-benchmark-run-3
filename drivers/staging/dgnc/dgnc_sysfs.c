baa626e14dbc4b092d6ed8eeb63c4c88d9eabcbd
Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
2015-03-10 21:29:20 +0200

    dgnc: clean up comments at start of files
    Remove FSF address because it's known in the past that it has changed.
    Also, remove the pointless "do not change the coding style" comments
    because it's one of the reasons why it's in staging and it's quite
    contradictory to what it says in TODO. Also, they contain wrong e-mails
    of people which are responsible for these drivers - see TODO or
    MAINTAINERS for that. We can preserve the original copyright at the top
    of the most files because it shows who originally made them.
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_sysfs.c
+++ drivers/staging/dgnc/dgnc_sysfs.c
@@ -12,22 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * but WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *
- *      NOTE TO LINUX KERNEL HACKERS:  DO NOT REFORMAT THIS CODE!
- *
- *      This is shared code between Digi's CVS archive and the
- *      Linux Kernel sources.
- *      Changing the source just for reformatting needlessly breaks
- *      our CVS diff history.
- *
- *      Send any bug fixes/changes to:  Eng.Linux at digi dot com.
- *      Thank you.
- *
  */
 
 
