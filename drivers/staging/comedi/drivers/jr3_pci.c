cd54ef3b6b0cf441d5b3d9054d2e889cf858aaec
Ian Abbott <abbotti@mev.co.uk>
2015-03-10 16:10:59 +0000

    staging: comedi: jr3_pci: include new "comedi_pci.h" header
    Include the new "../comedi_pci.h" header instead of <linux/pci.h> and
    "../comedidev.h", which will now get included indirectly.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/jr3_pci.c
+++ drivers/staging/comedi/drivers/jr3_pci.c
@@ -40,14 +40,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/pci.h>
 #include <linux/delay.h>
 #include <linux/ctype.h>
 #include <linux/jiffies.h>
 #include <linux/slab.h>
 #include <linux/timer.h>
 
-#include "../comedidev.h"
+#include "../comedi_pci.h"
 
 #include "jr3_pci.h"
 
