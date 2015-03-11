7a4dae28a7a7b64aee82175ff73ee25884f7164e
Thomas Betker <thomas.betker@rohde-schwarz.com>
2015-03-11 22:39:27 +0100

    serial: xuartps: Fix iobase use.
    cdns_uart_get_port() sets port->iobase = 1 to "mark port in use", but
    this "in use" condition is not checked anywhere else in the code. So
    remove the line, keeping port->iobase = 0 (which also makes more sense).
    
    Signed-off-by: Thomas Betker <thomas.betker@rohde-schwarz.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/xilinx_uartps.c
+++ drivers/tty/serial/xilinx_uartps.c
@@ -1039,7 +1039,6 @@ static struct uart_port *cdns_uart_get_port(int id)
 	/* At this point, we've got an empty uart_port struct, initialize it */
 	spin_lock_init(&port->lock);
 	port->membase	= NULL;
-	port->iobase	= 1; /* mark port in use */
 	port->irq	= 0;
 	port->type	= PORT_UNKNOWN;
 	port->iotype	= UPIO_MEM32;
