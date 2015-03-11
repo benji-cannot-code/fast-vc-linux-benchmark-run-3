ce0025c227d0be99c75d3721a11dfc2aeee151c4
Takashi Iwai <tiwai@suse.de>
2015-03-11 12:50:15 +0100

    ALSA: seq: Fix init order of snd_seq_device stuff
    When the sequencer driver is built in kernel, it may panic at boot
    because of the uninitialized snd_seq_bus_type.  Initialize it properly
    via subsys_initcall() instead of module_init() to assure that the bus
    is registered beforehand.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Fixes: 7c37ae5c625a ('ALSA: seq: Rewrite sequencer device binding with standard bus')
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/seq/seq_device.c
+++ sound/core/seq/seq_device.c
@@ -312,5 +312,5 @@ static void __exit alsa_seq_device_exit(void)
 	bus_unregister(&snd_seq_bus_type);
 }
 
-module_init(alsa_seq_device_init)
+subsys_initcall(alsa_seq_device_init)
 module_exit(alsa_seq_device_exit)
