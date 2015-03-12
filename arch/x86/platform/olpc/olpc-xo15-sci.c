a99f6860bf2c083f7d7e09d376e8db4c3c413a3d
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-03-12 08:44:16 +0100

    x86/olpc/xo15/sci: Use newly added power_supply_put API
    Replace direct usage of put_device() with new API: power_supply_put().
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Reviewed-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Reviewed-by: Sebastian Reichel <sre@kernel.org>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- arch/x86/platform/olpc/olpc-xo15-sci.c
+++ arch/x86/platform/olpc/olpc-xo15-sci.c
@@ -84,7 +84,7 @@ static void battery_status_changed(void)
 
 	if (psy) {
 		power_supply_changed(psy);
-		put_device(&psy->dev);
+		power_supply_put(psy);
 	}
 }
 
@@ -94,7 +94,7 @@ static void ac_status_changed(void)
 
 	if (psy) {
 		power_supply_changed(psy);
-		put_device(&psy->dev);
+		power_supply_put(psy);
 	}
 }
 
