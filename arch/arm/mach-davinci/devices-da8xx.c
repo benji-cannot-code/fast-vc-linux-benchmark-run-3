b54e7a8572c81224be57239d8b7cba7d0bd36f05
Peter Ujfalusi <peter.ujfalusi@ti.com>
2015-03-12 10:06:26 +0200

    ARM: davinci: devices-da8xx: Add interrupt resource to McASP structs
    On da8xx McASP TX/RX interrupt requests are combined.
    The interrupt can be used for error recovery.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/devices-da8xx.c
+++ arch/arm/mach-davinci/devices-da8xx.c
@@ -476,6 +476,11 @@ static struct resource da830_mcasp1_resources[] = {
 		.end	= DAVINCI_DA830_DMA_MCASP1_AREVT,
 		.flags	= IORESOURCE_DMA,
 	},
+	{
+		.name	= "common",
+		.start	= IRQ_DA8XX_MCASPINT,
+		.flags	= IORESOURCE_IRQ,
+	},
 };
 
 static struct platform_device da830_mcasp1_device = {
@@ -505,6 +510,11 @@ static struct resource da850_mcasp_resources[] = {
 		.start	= DAVINCI_DA8XX_DMA_MCASP0_AREVT,
 		.end	= DAVINCI_DA8XX_DMA_MCASP0_AREVT,
 		.flags	= IORESOURCE_DMA,
+	},
+	{
+		.name	= "common",
+		.start	= IRQ_DA8XX_MCASPINT,
+		.flags	= IORESOURCE_IRQ,
 	},
 };
 
