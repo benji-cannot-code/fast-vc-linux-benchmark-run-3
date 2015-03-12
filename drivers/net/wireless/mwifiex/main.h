55b070858cd3cb1aab533940f7cf5b4832c8bd85
Maithili Hinge <maithili@marvell.com>
2015-03-12 00:38:39 -0700

    mwifiex: Add support for auto ARP in mwifiex.
    This patch adds support for auto ARP feature in mwifiex.
    The device will respond to ARP requests from the network
    with ARP response in suspended state without waking up the host.
    This feature is enabled in the driver by default.
    
    Signed-off-by: Maithili Hinge <maithili@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/mwifiex/main.h
+++ drivers/net/wireless/mwifiex/main.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/of.h>
 #include <linux/idr.h>
+#include <linux/inetdevice.h>
 
 #include "decl.h"
 #include "ioctl.h"
