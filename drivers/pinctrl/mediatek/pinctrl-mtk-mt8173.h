9d4a992f1d2e4e72c46788af2110276d7f1aecee
Axel Lin <axel.lin@ingics.com>
2015-03-12 14:51:14 +0800

    pinctrl: mediatek: mt8135/mt8173: Fix build error due to missing include file
    Fix below build error:
      CC      drivers/pinctrl/mediatek/pinctrl-mt8135.o
    In file included from drivers/pinctrl/mediatek/pinctrl-mt8135.c:24:0:
    drivers/pinctrl/mediatek/pinctrl-mtk-mt8135.h:19:32: fatal error: pinctrl-mtk-common.h: No such file or directory
    compilation terminated.
    make[3]: *** [drivers/pinctrl/mediatek/pinctrl-mt8135.o] Error 1
    make[2]: *** [drivers/pinctrl/mediatek] Error 2
    make[1]: *** [drivers/pinctrl] Error 2
    make: *** [drivers] Error 2
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Reviewed-by: Matthias Brugger <matthias.bgg@gmail.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Hongzhou Yang <hongzhou.yang@mediatek.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/mediatek/pinctrl-mtk-mt8173.h
+++ drivers/pinctrl/mediatek/pinctrl-mtk-mt8173.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __PINCTRL_MTK_MT8173_H
 
 #include <linux/pinctrl/pinctrl.h>
-#include <pinctrl-mtk-common.h>
+#include "pinctrl-mtk-common.h"
 
 static const struct mtk_desc_pin mtk_pins_mt8173[] = {
 	MTK_PIN(
