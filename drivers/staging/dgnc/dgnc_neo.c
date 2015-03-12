aa79fd7837938673cce1bf18c25f0e20837d0701
Dan Carpenter <dan.carpenter@oracle.com>
2015-03-12 20:24:31 +0300

    staging: dgnc: some off by one bugs
    "dgnc_NumBoards" is the number of filled out elements in the
    dgnc_Board[] array.  "->nasync" and "->maxports" are the same value.
    They are the number of channels in the ->channels[] array so these tests
    should be ">=" instead of ">" so we avoid reading past the end of the
    arrays.
    
    I cleaned up the conditions in dgnc_mgmt_ioctl() a bit.  There was a
    work around for the off by one bug in the case where there were no
    boards which is no longer needed.  "channel" is unsigned so it can't be
    negative.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_neo.c
+++ drivers/staging/dgnc/dgnc_neo.c
@@ -392,7 +392,7 @@ static inline void neo_parse_isr(struct dgnc_board *brd, uint port)
 	if (!brd || brd->magic != DGNC_BOARD_MAGIC)
 		return;
 
-	if (port > brd->maxports)
+	if (port >= brd->maxports)
 		return;
 
 	ch = brd->channels[port];
@@ -522,7 +522,7 @@ static inline void neo_parse_lsr(struct dgnc_board *brd, uint port)
 	if (!brd || brd->magic != DGNC_BOARD_MAGIC)
 		return;
 
-	if (port > brd->maxports)
+	if (port >= brd->maxports)
 		return;
 
 	ch = brd->channels[port];
@@ -1004,7 +1004,7 @@ static irqreturn_t neo_intr(int irq, void *voidbrd)
 			 */
 
 			/* Verify the port is in range. */
-			if (port > brd->nasync)
+			if (port >= brd->nasync)
 				continue;
 
 			ch = brd->channels[port];
