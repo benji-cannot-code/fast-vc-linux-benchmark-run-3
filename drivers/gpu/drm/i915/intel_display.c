fc6b793b4260131b8f0959d007fa24e2982ee949
Xi Ruoyao <xry111@outlook.com>
2015-03-12 20:16:32 +0800

    drm/i915: Ensure plane->state->fb stays in sync with plane->fb
    plane->state->fb and plane->fb should always reference the same FB so
    that atomic and legacy codepaths have the same view of display state.
    However, there are some places in kernel code that directly set
    plane->fb and neglect to update plane->state->fb. If we never do a
    successful update through the atomic pipeline, the RmFB cleanup code
    will look at the plane->state->fb pointer, which has never actually
    been set to a legitimate value, and try to clean it up, leading to
    BUG's.
    
    Add a quick helper function to synchronize plane->state->fb with
    plane->fb and call it everywhere the driver tries to manually set
    plane->fb outside of the atomic pipeline. In this function, use
    drm_atomic_set_fb_for_plane instead of writing plane->state->fb
    directly to keep the reference count right.
    
    This is modified from Matt Roper's patch to drm-intel-nightly with
    commit id
    
    commit afd65eb4cc0578a9c07d621acdb8a570e2782bf7
    Author: Matt Roper <matthew.d.roper@intel.com>
    Date:   Tue Feb 3 13:10:04 2015 -0800
    
        drm/i915: Ensure plane->state->fb stays in sync with plane->fb
    
    However this bug exists in mainline kernel too, so I created this to fix
    it in mainline kernel.
    
    A minor change is to use drm_atomic_set_fb_for_plane instead of update
    reference count manually.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=88909
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=93711
    Signed-off-by: Xi Ruoyao <xry111@outlook.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    [Jani: included the patch notes in the commit message]
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/i915_drm.h>
 #include "i915_drv.h"
 #include "i915_trace.h"
+#include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_dp_helper.h>
 #include <drm/drm_crtc_helper.h>
@@ -2417,6 +2418,14 @@ out_unref_obj:
 	return false;
 }
 
+/* Update plane->state->fb to match plane->fb after driver-internal updates */
+static void
+update_state_fb(struct drm_plane *plane)
+{
+	if (plane->fb != plane->state->fb)
+		drm_atomic_set_fb_for_plane(plane->state, plane->fb);
+}
+
 static void
 intel_find_plane_obj(struct intel_crtc *intel_crtc,
 		     struct intel_initial_plane_config *plane_config)
@@ -2463,6 +2472,8 @@ intel_find_plane_obj(struct intel_crtc *intel_crtc,
 			break;
 		}
 	}
+
+	update_state_fb(intel_crtc->base.primary);
 }
 
 static void i9xx_update_primary_plane(struct drm_crtc *crtc,
@@ -6651,6 +6662,7 @@ i9xx_get_initial_plane_config(struct intel_crtc *crtc,
 		      plane_config->size);
 
 	crtc->base.primary->fb = fb;
+	update_state_fb(crtc->base.primary);
 }
 
 static void chv_crtc_clock_get(struct intel_crtc *crtc,
@@ -7688,6 +7700,7 @@ skylake_get_initial_plane_config(struct intel_crtc *crtc,
 		      plane_config->size);
 
 	crtc->base.primary->fb = fb;
+	update_state_fb(crtc->base.primary);
 	return;
 
 error:
@@ -7779,6 +7792,7 @@ ironlake_get_initial_plane_config(struct intel_crtc *crtc,
 		      plane_config->size);
 
 	crtc->base.primary->fb = fb;
+	update_state_fb(crtc->base.primary);
 }
 
 static bool ironlake_get_pipe_config(struct intel_crtc *crtc,
@@ -9817,6 +9831,7 @@ static int intel_crtc_page_flip(struct drm_crtc *crtc,
 	drm_gem_object_reference(&obj->base);
 
 	crtc->primary->fb = fb;
+	update_state_fb(crtc->primary);
 
 	work->pending_flip_obj = obj;
 
@@ -9885,6 +9900,7 @@ cleanup_unpin:
 cleanup_pending:
 	atomic_dec(&intel_crtc->unpin_work_count);
 	crtc->primary->fb = old_fb;
+	update_state_fb(crtc->primary);
 	drm_gem_object_unreference(&work->old_fb_obj->base);
 	drm_gem_object_unreference(&obj->base);
 	mutex_unlock(&dev->struct_mutex);
@@ -13719,6 +13735,7 @@ void intel_modeset_gem_init(struct drm_device *dev)
 				  to_intel_crtc(c)->pipe);
 			drm_framebuffer_unreference(c->primary->fb);
 			c->primary->fb = NULL;
+			update_state_fb(c->primary);
 		}
 	}
 	mutex_unlock(&dev->struct_mutex);
