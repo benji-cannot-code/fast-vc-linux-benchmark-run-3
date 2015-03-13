e29301081725cf10a513cb3a356cf3238227f292
Guenter Roeck <linux@roeck-us.net>
2015-03-13 01:58:46 -0700

    fs: btrfs: Add missing include file
    Building alpha:allmodconfig fails with
    
    fs/btrfs/inode.c: In function 'check_direct_IO':
    fs/btrfs/inode.c:8050:2: error: implicit declaration of function 'iov_iter_alignment'
    
    due to a missing include file.
    
    Fixes: 3737c63e1fb0 ("fs: move struct kiocb to fs.h")
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: David Sterba <dsterba@suse.cz>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/btrfs.h>
 #include <linux/blkdev.h>
 #include <linux/posix_acl_xattr.h>
+#include <linux/uio.h>
 #include "ctree.h"
 #include "disk-io.h"
 #include "transaction.h"
