ff10cc77fa88186bb92ed8eb4243d0f4b66927f3
Claudiu Manoil <claudiu.manoil@freescale.com>
2015-03-13 10:36:28 +0200

    gianfar: Make BDs access endian safe
    Use conversion macros to correctly access the BE
    fields of the Rx and Tx Buffer Descriptors on LE CPUs.
    
    Signed-off-by: Claudiu Manoil <claudiu.manoil@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/gianfar.h
+++ drivers/net/ethernet/freescale/gianfar.h
@@ -545,12 +545,12 @@ struct txbd8
 {
 	union {
 		struct {
-			u16	status;	/* Status Fields */
-			u16	length;	/* Buffer length */
+			__be16	status;	/* Status Fields */
+			__be16	length;	/* Buffer length */
 		};
-		u32 lstatus;
+		__be32 lstatus;
 	};
-	u32	bufPtr;	/* Buffer Pointer */
+	__be32	bufPtr;	/* Buffer Pointer */
 };
 
 struct txfcb {
@@ -566,12 +566,12 @@ struct rxbd8
 {
 	union {
 		struct {
-			u16	status;	/* Status Fields */
-			u16	length;	/* Buffer Length */
+			__be16	status;	/* Status Fields */
+			__be16	length;	/* Buffer Length */
 		};
-		u32 lstatus;
+		__be32 lstatus;
 	};
-	u32	bufPtr;	/* Buffer Pointer */
+	__be32	bufPtr;	/* Buffer Pointer */
 };
 
 struct rxfcb {
@@ -1286,6 +1286,14 @@ static inline void gfar_wmb(void)
 #else
 	wmb(); /* order write acesses for BD (or FCB) fields */
 #endif
+}
+
+static inline void gfar_clear_txbd_status(struct txbd8 *bdp)
+{
+	u32 lstatus = be32_to_cpu(bdp->lstatus);
+
+	lstatus &= BD_LFLAG(TXBD_WRAP);
+	bdp->lstatus = cpu_to_be32(lstatus);
 }
 
 irqreturn_t gfar_receive(int irq, void *dev_id);
