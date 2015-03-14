6b9842c26c7ff19723b2795e3ed4b0d00380ff74
Frank Rowand <frank.rowand@sonymobile.com>
2015-03-14 00:00:36 -0700

    of/unittest: add const where needed
    Fix warnings pointed out by checkpatch.
    
    No bugs fixed, but the test code should be a good example of how to use
    the devicetree API.
    
    Signed-off-by: Frank Rowand <frank.rowand@sonymobile.com>
    Signed-off-by: Grant Likely <grant.likely@linaro.org>

--- drivers/of/unittest.c
+++ drivers/of/unittest.c
@@ -681,7 +681,7 @@ static void __init of_selftest_parse_interrupts_extended(void)
 	of_node_put(np);
 }
 
-static struct of_device_id match_node_table[] = {
+static const struct of_device_id match_node_table[] = {
 	{ .data = "A", .name = "name0", }, /* Name alone is lowest priority */
 	{ .data = "B", .type = "type1", }, /* followed by type alone */
 
@@ -755,7 +755,7 @@ static void __init of_selftest_platform_populate(void)
 	int irq, rc;
 	struct device_node *np, *child, *grandchild;
 	struct platform_device *pdev;
-	struct of_device_id match[] = {
+	const struct of_device_id match[] = {
 		{ .compatible = "test-device", },
 		{}
 	};
@@ -954,7 +954,7 @@ static int selftest_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id selftest_match[] = {
+static const struct of_device_id selftest_match[] = {
 	{ .compatible = "selftest", },
 	{},
 };
@@ -1546,7 +1546,7 @@ static int selftest_i2c_bus_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id selftest_i2c_bus_match[] = {
+static const struct of_device_id selftest_i2c_bus_match[] = {
 	{ .compatible = "selftest-i2c-bus", },
 	{},
 };
