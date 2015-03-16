7cd2cab66f08ab7975710b044fc4b9f99c8c80bd
Thomas Graf <tgraf@suug.ch>
2015-03-16 10:42:27 +0100

    rhashtable: Annotate RCU locking of walkers
    Fixes the following sparse warnings:
    
    lib/rhashtable.c:767:5: warning: context imbalance in 'rhashtable_walk_start' - wrong count at exit
    lib/rhashtable.c:849:6: warning: context imbalance in 'rhashtable_walk_stop' - unexpected unlock
    
    Fixes: f2dba9c6ff0d ("rhashtable: Introduce rhashtable_walk_*")
    Signed-off-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- lib/rhashtable.c
+++ lib/rhashtable.c
@@ -761,6 +761,7 @@ EXPORT_SYMBOL_GPL(rhashtable_walk_exit);
  * by calling rhashtable_walk_next.
  */
 int rhashtable_walk_start(struct rhashtable_iter *iter)
+	__acquires(RCU)
 {
 	struct rhashtable *ht = iter->ht;
 
@@ -848,6 +849,7 @@ EXPORT_SYMBOL_GPL(rhashtable_walk_next);
  * Finish a hash table walk.
  */
 void rhashtable_walk_stop(struct rhashtable_iter *iter)
+	__releases(RCU)
 {
 	struct rhashtable *ht;
 	struct bucket_table *tbl = iter->walker->tbl;
