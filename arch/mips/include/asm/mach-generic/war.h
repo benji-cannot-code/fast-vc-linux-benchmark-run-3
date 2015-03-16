fd994d4e21da038e2f652bb5d8a96e237fd947fc
Kevin Cernekee <cernekee@gmail.com>
2015-03-16 14:43:07 -0700

    MIPS: Create a common <asm/mach-generic/war.h>
    11 platforms require at least one of these workarounds to be enabled; 22
    platforms do not.  In the latter case we can fall back to a generic version.
    
    Note that this also deletes an orphaned reference to RM9000_CDEX_SMP_WAR.
    
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-by: James Hogan <james.hogan@imgtec.com>
    Cc: devicetree@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mips@linux-mips.org
    Cc: Ezequiel Garcia <ezequiel.garcia@imgtec.com>
    Cc: James Hartley <james.hartley@imgtec.com>
    Cc: James Hogan <james.hogan@imgtec.com>
    Patchwork: https://patchwork.linux-mips.org/patch/9567/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-generic/war.h
+++ arch/mips/include/asm/mach-generic/war.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2002, 2004, 2007 by Ralf Baechle <ralf@linux-mips.org>
  */
-#ifndef __ASM_MIPS_MACH_JZ4740_WAR_H
-#define __ASM_MIPS_MACH_JZ4740_WAR_H
+#ifndef __ASM_MACH_GENERIC_WAR_H
+#define __ASM_MACH_GENERIC_WAR_H
 
 #define R4600_V1_INDEX_ICACHEOP_WAR	0
 #define R4600_V1_HIT_CACHEOP_WAR	0
@@ -22,4 +22,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define R10000_LLSC_WAR			0
 #define MIPS34K_MISSED_ITLB_WAR		0
 
-#endif /* __ASM_MIPS_MACH_JZ4740_WAR_H */
+#endif /* __ASM_MACH_GENERIC_WAR_H */
