651cea5ee1bdb31f32c54255ad2c940856881963
Johan Hedberg <johan.hedberg@intel.com>
2015-03-17 13:48:49 +0200

    Bluetooth: Add hdev_init callback for HCI channels
    In order to make the mgmt command handling more generic we can't have a
    direct call to mgmt_init_hdev() from mgmt_control(). This patch adds a
    new callback to struct hci_mgmt_chan. And sets it to point to the
    mgmt_init_hdev() function for the HCI_CHANNEL_CONTROL instance.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -6494,8 +6494,8 @@ int mgmt_control(struct hci_mgmt_chan *chan, struct sock *sk,
 		goto done;
 	}
 
-	if (hdev)
-		mgmt_init_hdev(sk, hdev);
+	if (hdev && chan->hdev_init)
+		chan->hdev_init(sk, hdev);
 
 	cp = buf + sizeof(*hdr);
 
@@ -7707,6 +7707,7 @@ static struct hci_mgmt_chan chan = {
 	.channel	= HCI_CHANNEL_CONTROL,
 	.handler_count	= ARRAY_SIZE(mgmt_handlers),
 	.handlers	= mgmt_handlers,
+	.hdev_init	= mgmt_init_hdev,
 };
 
 int mgmt_init(void)
