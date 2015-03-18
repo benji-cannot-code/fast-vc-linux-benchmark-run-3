4b98ee3905b42bcce2a5b4ced44b96a9e9f8df5a
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2015-03-18 10:07:19 +0200

    ASoC: Intel: Remove support for Intel MID DMA from firmware loader
    Intel MID DMA driver is going to be removed by the coming commit
    36111da7838e ("dmaengine: intel-mid-dma: remove the driver") in spi.git
    tree. Since there are no users for SST_DMA_TYPE_MID type the support for it
    can be removed from here in advance.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/sst-dsp.h
+++ sound/soc/intel/sst-dsp.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Supported SST DMA Devices */
 #define SST_DMA_TYPE_DW		1
-#define SST_DMA_TYPE_MID	2
 
 /* autosuspend delay 5s*/
 #define SST_RUNTIME_SUSPEND_DELAY	(5 * 1000)
