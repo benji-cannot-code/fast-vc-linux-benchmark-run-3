ff65e215047a624d4d7f143bdcd9da84df2b2c5c
jean-michel.hautbois@vodalys.com <jean-michel.hautbois@vodalys.com>
2015-03-18 07:21:47 -0300

    [media] v4l2-subdev: allow subdev to send an event to the v4l2_device notify function
    All drivers use custom notifications, in particular when source changes.
    The bridge only has to map the subdev that sends it to whatever video node it is connected to.
    
    Signed-off-by: Jean-Michel Hautbois <jean-michel.hautbois@vodalys.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/media/v4l2-subdev.h
+++ include/media/v4l2-subdev.h
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define V4L2_SUBDEV_IR_TX_NOTIFY		_IOW('v', 1, u32)
 #define V4L2_SUBDEV_IR_TX_FIFO_SERVICE_REQ	0x00000001
 
+#define	V4L2_DEVICE_NOTIFY_EVENT		_IOW('v', 2, struct v4l2_event)
+
 struct v4l2_device;
 struct v4l2_ctrl_handler;
 struct v4l2_event_subscription;
