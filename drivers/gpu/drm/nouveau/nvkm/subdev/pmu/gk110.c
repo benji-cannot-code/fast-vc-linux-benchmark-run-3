02dd1da35dca9ed4668b49b670daaa83ade676b0
Ben Skeggs <bskeggs@redhat.com>
2015-03-20 15:28:52 +1000

    drm/nouveau/pmu/gk208: implement gr power-up magic with gk110_pmu_pgob()
    Before we moved gk110's implementation of this to pmu, the functions were
    identical.  This commit just switches GK208 to use the new (more complete)
    implementation of the power-up sequence.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/pmu/gk110.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/pmu/gk110.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <subdev/timer.h>
 
-static void
+void
 gk110_pmu_pgob(struct nvkm_pmu *pmu, bool enable)
 {
 	static const struct {
