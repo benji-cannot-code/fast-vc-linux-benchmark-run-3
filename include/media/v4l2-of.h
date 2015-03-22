aa5598317de9a0f6347c94add110fde58b0c376e
Sakari Ailus <sakari.ailus@iki.fi>
2015-03-22 17:38:44 -0300

    [media] v4l: of: Remove the head field in struct v4l2_of_endpoint
    The field is unused. Remove it.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@iki.fi>
    Acked-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/media/v4l2-of.h
+++ include/media/v4l2-of.h
@@ -58,7 +58,6 @@ struct v4l2_of_bus_parallel {
  * @base: struct of_endpoint containing port, id, and local of_node
  * @bus_type: bus type
  * @bus: bus configuration data structure
- * @head: list head for this structure
  */
 struct v4l2_of_endpoint {
 	struct of_endpoint base;
@@ -67,7 +66,6 @@ struct v4l2_of_endpoint {
 		struct v4l2_of_bus_parallel parallel;
 		struct v4l2_of_bus_mipi_csi2 mipi_csi2;
 	} bus;
-	struct list_head head;
 };
 
 /**
