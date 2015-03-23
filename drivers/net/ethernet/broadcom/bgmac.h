9df19bd5b965c2975050c7f6d43296b0d1ee2549
Felix Fietkau <nbd@openwrt.org>
2015-03-23 12:35:35 +0100

    bgmac: fix descriptor frame start/end definitions
    The start-of-frame and end-of-frame bits were accidentally swapped.
    In the current code it does not make any difference, since they are
    always used together.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bgmac.h
+++ drivers/net/ethernet/broadcom/bgmac.h
@@ -346,8 +346,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define BGMAC_DESC_CTL0_EOT			0x10000000	/* End of ring */
 #define BGMAC_DESC_CTL0_IOC			0x20000000	/* IRQ on complete */
-#define BGMAC_DESC_CTL0_SOF			0x40000000	/* Start of frame */
-#define BGMAC_DESC_CTL0_EOF			0x80000000	/* End of frame */
+#define BGMAC_DESC_CTL0_EOF			0x40000000	/* End of frame */
+#define BGMAC_DESC_CTL0_SOF			0x80000000	/* Start of frame */
 #define BGMAC_DESC_CTL1_LEN			0x00001FFF
 
 #define BGMAC_PHY_NOREGS			0x1E
