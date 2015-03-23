65f0d8e48e9a29076cbc370308fc4484ba11a54e
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2015-03-23 14:07:47 +0200

    spi: pxa2xx: Remove needless includes
    These asm/io.h, asm/irq.h and asm/delay.h are needless since they are
    already included by linux/io.h via drivers/spi/spi-pxa2xx.h,
    linux/interrupt.h and linux/delay.h.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-pxa2xx.c
+++ drivers/spi/spi-pxa2xx.c
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm_runtime.h>
 #include <linux/acpi.h>
 
-#include <asm/io.h>
-#include <asm/irq.h>
-#include <asm/delay.h>
-
 #include "spi-pxa2xx.h"
 
 MODULE_AUTHOR("Stephen Street");
