f5d6a25c9966d1d16c129c3dd6ce4670e24781bb
Sasha Levin <sasha.levin@oracle.com>
2015-03-23 15:30:00 -0400

    tipc: validate length of sockaddr in connect() for dgram/rdm
    Commit f2f8036 ("tipc: add support for connect() on dgram/rdm sockets")
    hasn't validated user input length for the sockaddr structure which allows
    a user to overwrite kernel memory with arbitrary input.
    
    Fixes: f2f8036 ("tipc: add support for connect() on dgram/rdm sockets")
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Acked-by: Ying Xue <ying.xue@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/socket.c
+++ net/tipc/socket.c
@@ -1852,6 +1852,8 @@ static int tipc_connect(struct socket *sock, struct sockaddr *dest,
 		if (dst->family == AF_UNSPEC) {
 			memset(&tsk->remote, 0, sizeof(struct sockaddr_tipc));
 			tsk->connected = 0;
+		} else if (destlen != sizeof(struct sockaddr_tipc)) {
+			res = -EINVAL;
 		} else {
 			memcpy(&tsk->remote, dest, destlen);
 			tsk->connected = 1;
