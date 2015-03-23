b40e142a848166f3350252312ba85d0a86e939fe
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-03-23 16:54:08 -0400

    staging: rtl8723au: Fix mis-placed break
    In 7c3a8f2a5ec95e2b13704562f36321e20ddfc190 I made a mistake and moved
    a break below a bracket, causing a situation where the for loop would
    always exit when it shouldn't.
    
    This patch corrects that mistake.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_mlme_ext.c
+++ drivers/staging/rtl8723au/core/rtw_mlme_ext.c
@@ -1615,8 +1615,8 @@ OnAssocReq23a(struct rtw_adapter *padapter, struct recv_frame *precv_frame)
 					else
 						pstat->uapsd_be = 0;
 
+					break;
 				}
-				break;
 			} else {
 				break;
 			}
