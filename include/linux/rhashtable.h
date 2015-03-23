b4e149714ed1054e936997bf22463f48ca33a442
Herbert Xu <herbert@gondor.apana.org.au>
2015-03-24 09:53:17 +1100

    rhashtable: Fix sleeping inside RCU critical section in walk_stop
    The commit 963ecbd41a1026d99ec7537c050867428c397b89 ("rhashtable:
    Fix use-after-free in rhashtable_walk_stop") fixed a real bug
    but created another one because we may end up sleeping inside an
    RCU critical section.
    
    This patch fixes it properly by replacing the mutex with a spin
    lock that specifically protects the walker lists.
    
    Reported-by: Sasha Levin <sasha.levin@oracle.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/rhashtable.h
+++ include/linux/rhashtable.h
@@ -134,6 +134,7 @@ struct rhashtable_params {
  * @p: Configuration parameters
  * @run_work: Deferred worker to expand/shrink asynchronously
  * @mutex: Mutex to protect current/future table swapping
+ * @lock: Spin lock to protect walker list
  * @being_destroyed: True if table is set up for destruction
  */
 struct rhashtable {
@@ -145,6 +146,7 @@ struct rhashtable {
 	struct rhashtable_params	p;
 	struct work_struct		run_work;
 	struct mutex                    mutex;
+	spinlock_t			lock;
 };
 
 /**
