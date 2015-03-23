54b867769d20ef45b0dbb4a5beba98cb2526aa97
Vinson Lee <vlee@twitter.com>
2015-03-23 12:09:16 -0700

    perf tools: Work around lack of sched_getcpu in glibc < 2.6.
    This patch fixes this build error with glibc < 2.6.
    
      CC       util/cloexec.o
    cc1: warnings being treated as errors
    util/cloexec.c: In function ‘perf_flag_probe’:
    util/cloexec.c:24: error: implicit declaration of function
    ‘sched_getcpu’
    util/cloexec.c:24: error: nested extern declaration of ‘sched_getcpu’
    make: *** [util/cloexec.o] Error 1
    
    Signed-off-by: Vinson Lee <vlee@twitter.com>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Yann Droneaud <ydroneaud@opteya.com>
    Cc: stable@vger.kernel.org # 3.18+
    Link: http://lkml.kernel.org/r/1427137761-16119-1-git-send-email-vlee@twopensource.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/cloexec.h
+++ tools/perf/util/cloexec.h
@@ -4,4 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 unsigned long perf_event_open_cloexec_flag(void);
 
+#ifdef __GLIBC_PREREQ
+#if !__GLIBC_PREREQ(2, 6)
+extern int sched_getcpu(void) __THROW;
+#endif
+#endif
+
 #endif /* __PERF_CLOEXEC_H */
