37bcea936fbb2c58d654397828d39025e820600c
Greg Kroah-Hartman <greg@kroah.com>
2015-03-24 20:04:49 +0100

    greybus: es1: use <linux/uaccess.h> and not <asm/uaccess.h>
    Asm is only for when you are doing arch-specific stuff, which we aren't
    doing here.
    
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/staging/greybus/es1.c
+++ drivers/staging/greybus/es1.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb.h>
 #include <linux/kfifo.h>
 #include <linux/debugfs.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #include "greybus.h"
 #include "svc_msg.h"
