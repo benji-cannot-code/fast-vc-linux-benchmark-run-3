d22011cd3636f2ecbddce66682873ded9ba5a3a9
Rusty Russell <rusty@rustcorp.com.au>
2015-03-24 11:51:39 +1030

    lguest: suppress interrupts for single insn, not range.
    The last patch reduced our interrupt-suppression region to one address,
    so simplify the code somewhat.
    
    Also, remove the obsolete undefined instruction ranges and the comment
    which refers to lguest_guest.S instead of head_32.S.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- arch/x86/lguest/boot.c
+++ arch/x86/lguest/boot.c
@@ -88,8 +88,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct lguest_data lguest_data = {
 	.hcall_status = { [0 ... LHCALL_RING_SIZE-1] = 0xFF },
-	.noirq_start = (u32)lguest_noirq_start,
-	.noirq_end = (u32)lguest_noirq_end,
+	.noirq_iret = (u32)lguest_noirq_iret,
 	.kernel_address = PAGE_OFFSET,
 	.blocked_interrupts = { 1 }, /* Block timer interrupts */
 	.syscall_vec = SYSCALL_VECTOR,
