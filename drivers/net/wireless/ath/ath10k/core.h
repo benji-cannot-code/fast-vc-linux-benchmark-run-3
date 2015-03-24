1d7af7d0177018f544ae140bf34c71ab97205d52
Michal Kazior <michal.kazior@tieto.com>
2015-03-24 12:38:11 +0000

    ath10k: fix survey reporting
    Number of channels is stored in a separate macro
    in a header file and channel list is constructed
    independently. The macro is used to define survey
    array.
    
    This fixes a recent regression introduced after
    adding support for 144 channel. The regression
    would lead to a warning and incomplete survey data
    on channel 165:
    
      chan info: invalid frequency 5825 (idx 38 out of bounds)
    
    Also make sure to enforce the sizes and avoid this
    kind of problem in the future.
    
    Fixes: 4a7898fed5f3 ("ath10k: enable channel 144 on 5GHz band")
    Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -46,7 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WMI_READY_TIMEOUT (5 * HZ)
 #define ATH10K_FLUSH_TIMEOUT_HZ (5*HZ)
 #define ATH10K_CONNECTION_LOSS_HZ (3*HZ)
-#define ATH10K_NUM_CHANS 38
+#define ATH10K_NUM_CHANS 39
 
 /* Antenna noise floor */
 #define ATH10K_DEFAULT_NOISE_FLOOR -95
