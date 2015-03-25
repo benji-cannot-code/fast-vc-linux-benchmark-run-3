e0df5d437c496c0ca4a252b0bd60fa004d82fcd4
Joachim Eastwood <manabian@gmail.com>
2015-03-25 08:47:18 +0100

    ARM: 8327/1: zImage: add support for ARMv7-M
    This patch makes it possible to enter zImage in Thumb mode for ARMv7-M
    (Cortex-M) CPUs that do not support ARM mode. The kernel entry is also
    made in Thumb mode.
    
    [ukl: fix spelling in commit log, return early in call_cache_fn]
    
    Signed-off-by: Joachim Eastwood <manabian@gmail.com>
    Tested-by: Stefan Agner <stefan@agner.ch>
    Tested-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
    Tested-by: Chanwoo Choi <cw00.choi@samsung.com>
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/unified.h
+++ arch/arm/include/asm/unified.h
@@ -25,6 +25,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	.syntax unified
 #endif
 
+#ifdef CONFIG_CPU_V7M
+#define AR_CLASS(x...)
+#define M_CLASS(x...)	x
+#else
+#define AR_CLASS(x...)	x
+#define M_CLASS(x...)
+#endif
+
 #ifdef CONFIG_THUMB2_KERNEL
 
 #if __GNUC__ < 4
