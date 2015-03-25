d8768632460284b93da8cbe588bfc976b67984f8
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2015-03-25 13:32:22 -0700

    spi: of: do explicitly request modules for of-registered devices
    Trying to register an SPI device asynchronously (via async_schedule() call)
    results in an ugly complaint from request_module() warning about potential
    deadlock (because request_module tries to wait for async works to
    complete, the caller is also an async work in this case).
    
    While we could try to switch to using request_module_nowait(), other buses,
    as well as SPI itself when not using device tree, do not try to load
    modules explicitly, but rather rely on the standard infrastructure (such as
    udev) to execute module loading. There is no reason why SPI OF-described
    devices should be treated differently.
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/kernel.h>
-#include <linux/kmod.h>
 #include <linux/device.h>
 #include <linux/init.h>
 #include <linux/cache.h>
@@ -1360,7 +1359,6 @@ of_register_spi_device(struct spi_master *master, struct device_node *nc)
 	spi->dev.of_node = nc;
 
 	/* Register the new device */
-	request_module("%s%s", SPI_MODULE_PREFIX, spi->modalias);
 	rc = spi_add_device(spi);
 	if (rc) {
 		dev_err(&master->dev, "spi_device register error %s\n",
