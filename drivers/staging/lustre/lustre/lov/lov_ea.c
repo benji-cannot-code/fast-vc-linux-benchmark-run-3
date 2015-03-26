220ad1a4670a25249f179dad66d0a210da833b1f
Yang Sheng <yang.sheng@intel.com>
2015-03-25 21:53:22 -0400

    staging/lustre/lov: don't crash accessing LOV object with FID{0, 0}
    Some object maybe has a corrupted LOV EA or a hole in
    LOV EA. We should not crash client in such case.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Signed-off-by: Yang Sheng <yang.sheng@intel.com>
    Reviewed-on: http://review.whamcloud.com/12740
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-4958
    Reviewed-by: Jian Yu <jian.yu@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_ea.c
+++ drivers/staging/lustre/lustre/lov/lov_ea.c
@@ -228,6 +228,9 @@ static int lsm_unpackmd_v1(struct lov_obd *lov, struct lov_stripe_md *lsm,
 		ostid_le_to_cpu(&lmm->lmm_objects[i].l_ost_oi, &loi->loi_oi);
 		loi->loi_ost_idx = le32_to_cpu(lmm->lmm_objects[i].l_ost_idx);
 		loi->loi_ost_gen = le32_to_cpu(lmm->lmm_objects[i].l_ost_gen);
+		if (lov_oinfo_is_dummy(loi))
+			continue;
+
 		if (loi->loi_ost_idx >= lov->desc.ld_tgt_count) {
 			CERROR("OST index %d more than OST count %d\n",
 			       loi->loi_ost_idx, lov->desc.ld_tgt_count);
@@ -315,6 +318,9 @@ static int lsm_unpackmd_v3(struct lov_obd *lov, struct lov_stripe_md *lsm,
 		ostid_le_to_cpu(&lmm->lmm_objects[i].l_ost_oi, &loi->loi_oi);
 		loi->loi_ost_idx = le32_to_cpu(lmm->lmm_objects[i].l_ost_idx);
 		loi->loi_ost_gen = le32_to_cpu(lmm->lmm_objects[i].l_ost_gen);
+		if (lov_oinfo_is_dummy(loi))
+			continue;
+
 		if (loi->loi_ost_idx >= lov->desc.ld_tgt_count) {
 			CERROR("OST index %d more than OST count %d\n",
 			       loi->loi_ost_idx, lov->desc.ld_tgt_count);
