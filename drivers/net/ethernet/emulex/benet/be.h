ff19734dd09c24b1a9c271017c538408cbde590a
Padmanabh Ratnakar <padmanabh.ratnakar@emulex.com>
2015-03-26 03:05:08 -0400

    be2net: assign CPU affinity hints to be2net IRQs
    This patch provides hints to irqbalance to map be2net IRQs to
    specific CPU cores. cpumask_set_cpu_local_first() is used, which first
    maps IRQs to near NUMA cores; when those cores are exhausted, IRQs are
    mapped to far NUMA cores.
    
    Signed-off-by: Padmanabh Ratnakar <padmanabh.ratnakar@emulex.com>
    Signed-off-by: Sathya Perla <sathya.perla@emulex.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/emulex/benet/be.h
+++ drivers/net/ethernet/emulex/benet/be.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 #include <linux/slab.h>
 #include <linux/u64_stats_sync.h>
+#include <linux/cpumask.h>
 
 #include "be_hw.h"
 #include "be_roce.h"
@@ -184,6 +185,7 @@ struct be_eq_obj {
 	u16 spurious_intr;
 	struct napi_struct napi;
 	struct be_adapter *adapter;
+	cpumask_var_t  affinity_mask;
 
 #ifdef CONFIG_NET_RX_BUSY_POLL
 #define BE_EQ_IDLE		0
