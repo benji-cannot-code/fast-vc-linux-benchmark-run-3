705f7ea3f562a085f7c71b80615234c75f52ccf3
Ian Abbott <abbotti@mev.co.uk>
2015-03-27 19:13:44 +0000

    staging: comedi: addi_apci_3501: remove #include "comedi_fc.h"
    "addi_apci_3501.c" doesn't use anything from "comedi_fc.h".
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/addi_apci_3501.c
+++ drivers/staging/comedi/drivers/addi_apci_3501.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 
 #include "../comedi_pci.h"
-#include "comedi_fc.h"
 #include "amcc_s5933.h"
 
 /*
