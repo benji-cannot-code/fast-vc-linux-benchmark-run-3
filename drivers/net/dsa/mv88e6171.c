e97ea1bc54a739cff28e1a14c703f5d76ce1093a
Guenter Roeck <linux@roeck-us.net>
2015-03-26 18:36:28 -0700

    net: dsa: mv88e6xxx: Factor out common initialization code
    Code used and needed in mv886xxx.c should be initialized there as well,
    so factor it out from the individual initialization files.
    
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Tested-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/mv88e6171.c
+++ drivers/net/dsa/mv88e6171.c
@@ -293,12 +293,12 @@ static int mv88e6171_setup_port(struct dsa_switch *ds, int p)
 
 static int mv88e6171_setup(struct dsa_switch *ds)
 {
-	struct mv88e6xxx_priv_state *ps = (void *)(ds + 1);
 	int i;
 	int ret;
 
-	mutex_init(&ps->smi_mutex);
-	mutex_init(&ps->stats_mutex);
+	ret = mv88e6xxx_setup_common(ds);
+	if (ret < 0)
+		return ret;
 
 	ret = mv88e6171_switch_reset(ds);
 	if (ret < 0)
@@ -318,8 +318,6 @@ static int mv88e6171_setup(struct dsa_switch *ds)
 		if (ret < 0)
 			return ret;
 	}
-
-	mutex_init(&ps->phy_mutex);
 
 	return 0;
 }
