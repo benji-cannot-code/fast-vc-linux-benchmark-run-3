05d99dcf90d662802fff0660a8bd2908b8dcfdb1
Ian Abbott <abbotti@mev.co.uk>
2015-03-27 19:13:42 +0000

    staging: comedi: 8255: remove #include "comedi_fc.h"
    "8255.c" doesn't use anything from "comedi_fc.h".
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/8255.c
+++ drivers/staging/comedi/drivers/8255.c
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include "../comedidev.h"
 
-#include "comedi_fc.h"
 #include "8255.h"
 
 struct subdev_8255_private {
