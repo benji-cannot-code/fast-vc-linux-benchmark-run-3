1e735c177835bcc2a34feac54d60ec4a4573b082
Darshana Padmadas <darshanapadmadas@gmail.com>
2015-03-28 23:13:54 +0530

    Staging: lustre: Include header for functions prototypes
    This patch includes the header lustre/include/lclient.h that declares
    the functions lov_lsm_put and lov_read_and_clear_async_rc defined in
    lov_object.c.
    
    This eliminates warnings reported by sparse:
    warning: symbol was not declared. Should it be static?
    
    Signed-off-by: Darshana Padmadas <darshanapadmadas@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_object.c
+++ drivers/staging/lustre/lustre/lov/lov_object.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUG_SUBSYSTEM S_LOV
 
 #include "lov_cl_internal.h"
+#include "../include/lclient.h"
 
 /** \addtogroup lov
  *  @{
