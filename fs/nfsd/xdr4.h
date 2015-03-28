8cb0f44fc3787401cbc4945058be3d5bdbdff410
Kinglong Mee <kinglongmee@gmail.com>
2015-03-28 23:46:09 +0800

    nfsd: Remove duplicate macro define for max sec label length
    NFS4_MAXLABELLEN has defined for sec label max length, use it directly.
    
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/xdr4.h
+++ fs/nfsd/xdr4.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "state.h"
 #include "nfsd.h"
 
-#define NFSD4_MAX_SEC_LABEL_LEN	2048
 #define NFSD4_MAX_TAGLEN	128
 #define XDR_LEN(n)                     (((n) + 3) & ~3)
 
