d02b42d25e6c08fa7d9c2f3d6217c589cc2b7d23
Darshana Padmadas <darshanapadmadas@gmail.com>
2015-03-28 23:13:55 +0530

    Staging: lustre: Include header file that declares lprocfs_mdc_init_vars
    This patch includes the header file mdc_internal.h that declares the
    function lprocfs_mdc_init_vars.
    
    This also eliminates the sparse warning:
    warning: symbol 'lprocfs_mdc_init_vars' was not declared. Should it be static?
    
    Signed-off-by: Darshana Padmadas <darshanapadmadas@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/mdc/lproc_mdc.c
+++ drivers/staging/lustre/lustre/mdc/lproc_mdc.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vfs.h>
 #include "../include/obd_class.h"
 #include "../include/lprocfs_status.h"
+#include "mdc_internal.h"
 
 static int mdc_max_rpcs_in_flight_seq_show(struct seq_file *m, void *v)
 {
