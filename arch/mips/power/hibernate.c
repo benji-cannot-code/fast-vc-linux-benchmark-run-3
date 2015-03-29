30b8e47de9a3d34493e65649c4f664e2e2621eac
Huacai Chen <chenhc@lemote.com>
2015-03-29 10:54:06 +0800

    MIPS: Hibernate: Restructure files and functions
    This patch has no functional changes, it just to keep the assembler
    code to a minimum. Files and functions naming is borrowed from X86.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Cc: Steven J. Hill <Steven.Hill@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: Fuxin Zhang <zhangfx@lemote.com>
    Cc: Zhangjin Wu <wuzhangjin@gmail.com>
    Patchwork: https://patchwork.linux-mips.org/patch/9616/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

---
+++ arch/mips/power/hibernate.c
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <asm/tlbflush.h>
+
+extern int restore_image(void);
+
+int swsusp_arch_resume(void)
+{
+	/* Avoid TLB mismatch during and after kernel resume */
+	local_flush_tlb_all();
+	return restore_image();
+}
