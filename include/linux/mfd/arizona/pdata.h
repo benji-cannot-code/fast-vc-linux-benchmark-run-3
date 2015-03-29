8186c42b7da725f366c8378a9a184141e8de3bcf
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2015-03-29 12:45:42 +0100

    mfd: arizona: Correct type of gpio_defaults
    gpio_defaults needs to be specified as an unsigned int rather than an
    int, because the intention of the DT binding is that all out of range
    values for a 16-bit register will cause the defaults to be used,
    however, if gpio_defaults is an int then values that are larger than
    INT_MAX will become negative numbers and be written out directly to the
    hardware. As no where in the code replies on gpio_defaults being an int,
    the simplest fix is to just change it to unsigned.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/arizona/pdata.h
+++ include/linux/mfd/arizona/pdata.h
@@ -95,7 +95,7 @@ struct arizona_pdata {
 	int gpio_base;
 
 	/** Pin state for GPIO pins */
-	int gpio_defaults[ARIZONA_MAX_GPIO];
+	unsigned int gpio_defaults[ARIZONA_MAX_GPIO];
 
 	/**
 	 * Maximum number of channels clocks will be generated for,
