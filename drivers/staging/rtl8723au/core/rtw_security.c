9537527b9e373c5b0e2015279e6c3ba50cb0553b
M. Vefa Bicakci <m.v.b@runbox.com>
2015-03-28 21:07:56 -0400

    staging: rtl8723au: Rework two byte array comparisons
    Prior to this commit, rtl8723au's rtw_security.c had two instances of
    byte array comparisons (for CRC checks) where the individual elements
    of the byte arrays were compared one by one and an error trace would
    be output if the byte arrays were determined to be different.
    
    This commit improves the readability of the CRC verification by
    placing the individual 4 bytes of each byte array into an 32-bit
    unsigned integer and comparing the two resulting integers.
    
    Thanks to Larry Finger for spotting the code style issues in the
    previous version of this commit, and thanks to Joe Perches for
    suggesting the use of 32-bit integer comparisons instead of byte
    array comparisons.
    
    Signed-off-by: M. Vefa Bicakci <m.v.b@runbox.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/core/rtw_security.c
+++ drivers/staging/rtl8723au/core/rtw_security.c
@@ -211,7 +211,7 @@ void rtw_wep_decrypt23a(struct rtw_adapter *padapter,
 		     struct recv_frame *precvframe)
 {
 	/*  exclude ICV */
-	u8 crc[4];
+	u32 actual_crc, expected_crc;
 	struct arc4context mycontext;
 	int length;
 	u32 keylength;
@@ -244,16 +244,14 @@ void rtw_wep_decrypt23a(struct rtw_adapter *padapter,
 	arcfour_encrypt(&mycontext, payload, payload, length);
 
 	/* calculate icv and compare the icv */
-	*((u32 *)crc) = le32_to_cpu(getcrc32(payload, length - 4));
+	actual_crc = le32_to_cpu(getcrc32(payload, length - 4));
+	expected_crc = le32_to_cpu(get_unaligned_le32(&payload[length - 4]));
 
-	if (crc[3] != payload[length - 1] || crc[2] != payload[length - 2] ||
-	    crc[1] != payload[length - 3] || crc[0] != payload[length - 4]) {
+	if (actual_crc != expected_crc) {
 		RT_TRACE(_module_rtl871x_security_c_, _drv_err_,
-			 "rtw_wep_decrypt23a:icv error crc[3](%x)!= payload[length-1](%x) || crc[2](%x)!= payload[length-2](%x) || crc[1](%x)!= payload[length-3](%x) || crc[0](%x)!= payload[length-4](%x)\n",
-			 crc[3], payload[length - 1],
-			 crc[2], payload[length - 2],
-			 crc[1], payload[length - 3],
-			 crc[0], payload[length - 4]);
+			 "rtw_wep_decrypt23a:icv CRC mismatch: "
+			 "actual: %08x, expected: %08x\n",
+			 actual_crc, expected_crc);
 	}
 }
 
@@ -715,7 +713,7 @@ int rtw_tkip_decrypt23a(struct rtw_adapter *padapter,
 	u32 pnh;
 	u8 rc4key[16];
 	u8 ttkey[16];
-	u8 crc[4];
+	u32 actual_crc, expected_crc;
 	struct arc4context mycontext;
 	int length;
 	u32 prwskeylen;
@@ -772,18 +770,14 @@ int rtw_tkip_decrypt23a(struct rtw_adapter *padapter,
 	arcfour_init(&mycontext, rc4key, 16);
 	arcfour_encrypt(&mycontext, payload, payload, length);
 
-	*((u32 *)crc) = le32_to_cpu(getcrc32(payload, length - 4));
+	actual_crc = le32_to_cpu(getcrc32(payload, length - 4));
+	expected_crc = le32_to_cpu(get_unaligned_le32(&payload[length - 4]));
 
-	if (crc[3] != payload[length - 1] ||
-	    crc[2] != payload[length - 2] ||
-	    crc[1] != payload[length - 3] ||
-	    crc[0] != payload[length - 4]) {
+	if (actual_crc != expected_crc) {
 		RT_TRACE(_module_rtl871x_security_c_, _drv_err_,
-			 "rtw_wep_decrypt23a:icv error crc[3](%x)!= payload[length-1](%x) || crc[2](%x)!= payload[length-2](%x) || crc[1](%x)!= payload[length-3](%x) || crc[0](%x)!= payload[length-4](%x)\n",
-			 crc[3], payload[length - 1],
-			 crc[2], payload[length - 2],
-			 crc[1], payload[length - 3],
-			 crc[0], payload[length - 4]);
+			 "rtw_wep_decrypt23a:icv CRC mismatch: "
+			 "actual: %08x, expected: %08x\n",
+			 actual_crc, expected_crc);
 		res = _FAIL;
 	}
 
