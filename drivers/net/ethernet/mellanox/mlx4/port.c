100912c40c671fd460b75cf82d4d787000e56101
Eran Ben Elisha <eranbe@mellanox.com>
2015-03-30 17:45:20 +0300

    net/mlx4_en: Fix port counters statistics bitmask
    Two counters (rx_chksum_complete and tx_chksum_offload) are not displayed
    under SRIOV for the PF via ethtool because their bit mask is off, fix that.
    
    Fixes: f8c6455bb ('net/mlx4_en: Extend checksum offloading by CHECKSUM COMPLETE')
    Fixes: 9fab426de ('mlx4: add a new xmit_more counter')
    
    Signed-off-by: Eran Ben Elisha <eranbe@mellanox.com>
    Signed-off-by: Hadar Hen Zion <hadarh@mellanox.com>
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx4/port.c
+++ drivers/net/ethernet/mellanox/mlx4/port.c
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MLX4_STATS_TRAFFIC_COUNTERS_MASK	0xfULL
 #define MLX4_STATS_TRAFFIC_DROPS_MASK		0xc0ULL
 #define MLX4_STATS_ERROR_COUNTERS_MASK		0x1ffc30ULL
-#define MLX4_STATS_PORT_COUNTERS_MASK		0x1fe00000ULL
+#define MLX4_STATS_PORT_COUNTERS_MASK		0x7fe00000ULL
 
 void mlx4_init_mac_table(struct mlx4_dev *dev, struct mlx4_mac_table *table)
 {
