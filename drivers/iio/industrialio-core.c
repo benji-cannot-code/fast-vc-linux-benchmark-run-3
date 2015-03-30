e9511e6c84d9a9672fd7b7f1cab571503e62b664
Vianney le Clément de Saint-Marcq <vianney.leclement@essensium.com>
2015-03-30 10:34:58 +0200

    iio: core: Introduce IIO_CHAN_INFO_CALIBEMISSIVITY
    Contact-less IR temperature sensors measure the temperature of an object
    by using its thermal radiation.  Surfaces with different emissivity
    ratios emit different amounts of energy at the same temperature.
    
    IIO_CHAN_INFO_CALIBEMISSIVITY allows the user to inform the sensor of the
    emissivity of the object in front of it, in order to effectively measure
    its temperature.
    
    A device providing such setting is Melexis's MLX90614:
    http://melexis.com/Assets/IR-sensor-thermometer-MLX90614-Datasheet-5152.aspx.
    
    Signed-off-by: Vianney le Clément de Saint-Marcq <vianney.leclement@essensium.com>
    Cc: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -129,6 +129,7 @@ static const char * const iio_chan_info_postfix[] = {
 	[IIO_CHAN_INFO_CALIBWEIGHT] = "calibweight",
 	[IIO_CHAN_INFO_DEBOUNCE_COUNT] = "debounce_count",
 	[IIO_CHAN_INFO_DEBOUNCE_TIME] = "debounce_time",
+	[IIO_CHAN_INFO_CALIBEMISSIVITY] = "calibemissivity",
 };
 
 /**
