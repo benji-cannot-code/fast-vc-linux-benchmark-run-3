4e8439875c9a33f07336b25686bd3dbcfc2a50ac
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2015-04-02 23:01:04 +0530

    Staging: sm750fb: Remove zero testing pointer typed value
    Removes variable comparison with 0.
    Done using following coccinelle script.
    
    @ disable is_zero,isnt_zero @
    expression *E;
    expression E1,f;
    @@
    
    E = f(...)
    <...
    (
    - E == 0
    + !E
    |
    - E != 0
    + E
    |
    - 0 == E
    + !E
    |
    - 0 != E
    + E
    )
    ...>
    ?E = E1
    
    @ disable is_zero,isnt_zero @
    expression *E;
    @@
    
    (
      E ==
    - 0
    + NULL
    |
      E !=
    - 0
    + NULL
    |
    - 0
    + NULL
      == E
    |
    - 0
    + NULL
      != E
    )
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_chip.c
+++ drivers/staging/sm750fb/ddk750_chip.c
@@ -111,7 +111,7 @@ void setChipClock(unsigned int frequency)
 		return;
 #endif
 
-	if (frequency != 0) {
+	if (frequency) {
 		/*
 		* Set up PLL, a structure to hold the value to be set in clocks.
 		*/
@@ -140,7 +140,7 @@ void setMemoryClock(unsigned int frequency)
 	if (getChipType() == SM750LE)
 		return;
 #endif
-	if (frequency != 0) {
+	if (frequency) {
 		/* Set the frequency to the maximum frequency that the DDR Memory can take
 		which is 336MHz. */
 		if (frequency > MHz(336))
@@ -188,7 +188,7 @@ void setMasterClock(unsigned int frequency)
 	if (getChipType() == SM750LE)
 		return;
 #endif
-	if (frequency != 0) {
+	if (frequency) {
 		/* Set the frequency to the maximum frequency that the SM750 engine can
 		run, which is about 190 MHz. */
 		if (frequency > MHz(190))
