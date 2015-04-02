6d50d90f5590f11bd36702a866dfa617043af3c7
Steven Rostedt <rostedt@goodmis.org>
2015-04-02 12:37:36 -0400

    tracing/drm: Remove unused TRACE_SYSTEM_STRING define
    The tracing infrastructure is adding a macro TRACE_SYSTEM_STRING, and
    hit the following build failure:
    
       In file included from include/trace/define_trace.h:90:0,
                        from drivers/gpu/drm/.//radeon/radeon_trace.h:209,
                        from drivers/gpu/drm/.//radeon/radeon_trace_points.c:9:
    >> include/trace/ftrace.h:28:0: warning: "TRACE_SYSTEM_STRING" redefined
        #define TRACE_SYSTEM_STRING __app(TRACE_SYSTEM_VAR,__trace_system_name)
    
    Seems that the DRM folks have added their own use to the
    TRACE_SYSTEM_STRING, with:
    
     #define TRACE_SYSTEM_STRING __stringify(TRACE_SYSTEM)
    
    Although, I can not find its use anywhere. I could simply use another
    name, but if this macro is not being used, it should be removed.
    
    Link: http://lkml.kernel.org/r/20150402123736.01eda052@gandalf.local.home
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel.vetter@intel.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Reviewed-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Tested-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- drivers/gpu/drm/i915/i915_trace.h
+++ drivers/gpu/drm/i915/i915_trace.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM i915
-#define TRACE_SYSTEM_STRING __stringify(TRACE_SYSTEM)
 #define TRACE_INCLUDE_FILE i915_trace
 
 /* pipe updates */
