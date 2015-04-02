b4ffd677e7587a8950cfc4d54e632eb96f50fac4
Ralf Baechle <ralf@linux-mips.org>
2015-04-02 16:19:29 +0200

    MIPS: SEAD3: sead3-net is not a module.
    So let's remove everything that only makes sense for kernel modules.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mti-sead3/sead3-net.c
+++ arch/mips/mti-sead3/sead3-net.c
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2012 MIPS Technologies, Inc.  All rights reserved.
  */
-#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/irq.h>
 #include <linux/irqchip/mips-gic.h>
 #include <linux/platform_device.h>
@@ -51,8 +51,4 @@ static int __init sead3_net_init(void)
 	return platform_device_register(&sead3_net_device);
 }
 
-module_init(sead3_net_init);
-
-MODULE_AUTHOR("Chris Dearman <chris@mips.com>");
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Network probe driver for SEAD-3");
+device_initcall(sead3_net_init);
