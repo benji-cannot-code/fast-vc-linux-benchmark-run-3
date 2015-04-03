72c0433c794adb620cbb225dde68e612625abd86
Hans Verkuil <hverkuil@xs4all.nl>
2015-04-03 05:15:09 -0300

    [media] ov2640: add missing consumer.h include
    Needed for mips and sh platforms
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/i2c/soc_camera/ov2640.c
+++ drivers/media/i2c/soc_camera/ov2640.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/of_gpio.h>
 #include <linux/v4l2-mediabus.h>
 #include <linux/videodev2.h>
