9376c1224696a56691397e4d1a9fb0918ce6e864
Jeff Kirsher <jeffrey.t.kirsher@intel.com>
2015-04-03 13:26:51 -0700

    fm10k: Corrected an error in Tx statistics
    The function collecting Tx statistics was actually using values from the RX
    ring. Thus, Tx and Rx statistics values reported by "ifconfig" will
    return identical values. This change corrects this error and the Tx
    statistics is now reading from the Tx ring.
    
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Acked-by: Matthew Vick <matthew.vick@intel.com>
    Tested-by: Krishneil Singh <krishneil.k.singh@intel.com>

--- drivers/net/ethernet/intel/fm10k/fm10k_netdev.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_netdev.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Intel Ethernet Switch Host Interface Driver
- * Copyright(c) 2013 - 2014 Intel Corporation.
+ * Copyright(c) 2013 - 2015 Intel Corporation.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -1127,7 +1127,7 @@ static struct rtnl_link_stats64 *fm10k_get_stats64(struct net_device *netdev,
 	}
 
 	for (i = 0; i < interface->num_tx_queues; i++) {
-		ring = ACCESS_ONCE(interface->rx_ring[i]);
+		ring = ACCESS_ONCE(interface->tx_ring[i]);
 
 		if (!ring)
 			continue;
