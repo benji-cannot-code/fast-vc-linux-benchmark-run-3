9c5532943f52b4212a94ead10a63f560ddf68ecf
Maciej W. Rozycki <macro@linux-mips.org>
2015-04-03 23:26:04 +0100

    MIPS: Correct `nofpu' non-functionality
    The `cpu_has_fpu' feature flag must not be hardcoded to 1 or the `nofpu'
    kernel option will be ignored.  Remove any such overrides and add a
    cautionary note.  Hardcoding to 0 is fine for FPU-less platforms.
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/9694/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef cpu_has_octeon_cache
 #define cpu_has_octeon_cache	0
 #endif
+/* Don't override `cpu_has_fpu' to 1 or the "nofpu" option won't work.  */
 #ifndef cpu_has_fpu
 #define cpu_has_fpu		(current_cpu_data.options & MIPS_CPU_FPU)
 #define raw_cpu_has_fpu		(raw_current_cpu_data.options & MIPS_CPU_FPU)
