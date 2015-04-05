c64d39ed5e0b8ccdf40961a13990ebf824a67316
Marcel Holtmann <marcel@holtmann.org>
2015-04-04 20:29:27 -0700

    Bluetooth: hci_uart: Remove unused h4->rx_state and h4->rx_count field
    The fields h4->rx_state and h4->rx_count are not used at all and with
    that they can be just removed.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- drivers/bluetooth/hci_h4.c
+++ drivers/bluetooth/hci_h4.c
@@ -50,8 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VERSION "1.2"
 
 struct h4_struct {
-	unsigned long rx_state;
-	unsigned long rx_count;
 	struct sk_buff_head txq;
 };
 
