8f1b329be11fe178cba720a3cd3cbba477b45bbe
Marcel Holtmann <marcel@holtmann.org>
2015-04-05 22:52:16 -0700

    Bluetooth: btusb: Update version number of the module
    The version number is cosmetic, but pretty handy for debugging purposes
    and since the Broadcom protocol support is now optional, just increase
    it to indicate the difference.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "btbcm.h"
 
-#define VERSION "0.7"
+#define VERSION "0.8"
 
 static bool disable_scofix;
 static bool force_scofix;
