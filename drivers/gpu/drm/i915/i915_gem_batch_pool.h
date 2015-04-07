3790dedec699cacc3668df452c342e4020143a8a
Chris Wilson <chris@chris-wilson.co.uk>
2015-04-07 16:20:38 +0100

    drm/i915: Split batch pool into size buckets
    Now with the trimmed memcpy before the command parser, we try to
    allocate many different sizes of batches, predominantly one or two
    pages. We can therefore speed up searching for a good sized batch by
    keeping the objects of buckets of roughly the same size.
    
    v2: Add a comment about bucket sizes
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem_batch_pool.h
+++ drivers/gpu/drm/i915/i915_gem_batch_pool.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct i915_gem_batch_pool {
 	struct drm_device *dev;
-	struct list_head cache_list;
+	struct list_head cache_list[4];
 };
 
 /* i915_gem_batch_pool.c */
