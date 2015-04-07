5fb333802c42cc46fb469634eb154b96bc59f4a4
Florian Fainelli <f.fainelli@gmail.com>
2015-04-07 13:34:02 -0700

    MIPS: BCM63xx: Provide a plat_post_dma_flush hook
    Broadcom BCM63xx DSL SoCs utilize BMIPS CPUs, and as such are required
    to perform a read-ahead cache flush after a DMA transfer. Utilize
    asm/bmips.h to provide a plat_post_dma_flush_hook, and
    mach-generic/dma-coherence.h for everything else.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Cc: linux-mips@linux-mips.org
    Cc: cernekee@gmail.com
    Cc: jogo@openwrt.org
    Patchwork: https://patchwork.linux-mips.org/patch/9726/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

---
+++ arch/mips/include/asm/mach-bcm63xx/dma-coherence.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ASM_MACH_BCM63XX_DMA_COHERENCE_H
+#define __ASM_MACH_BCM63XX_DMA_COHERENCE_H
+
+#include <asm/bmips.h>
+
+#define plat_post_dma_flush	bmips_post_dma_flush
+
+#include <asm/mach-generic/dma-coherence.h>
+
+#endif /* __ASM_MACH_BCM63XX_DMA_COHERENCE_H */
