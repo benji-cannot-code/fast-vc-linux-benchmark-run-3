8f7f097224b382930b542b296b52ba1d0af85031
Ralf Baechle <ralf@linux-mips.org>
2015-04-07 14:59:18 +0200

    MIPS: Fix double inclusion of headers in misalignment emulator.
    Introduced in 34c2f668d0f6b2ca1c076d8170d6cd4f2235a9d4 (MIPS: microMIPS:
    Add unaligned access support.)
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/unaligned.c
+++ arch/mips/kernel/unaligned.c
@@ -90,8 +90,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/fpu_emulator.h>
 #include <asm/inst.h>
 #include <asm/uaccess.h>
-#include <asm/fpu.h>
-#include <asm/fpu_emulator.h>
 
 #define STR(x)	__STR(x)
 #define __STR(x)  #x
