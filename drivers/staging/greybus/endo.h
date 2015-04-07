20bb538352bc2525878d4b2589b5edffabd91c03
Greg Kroah-Hartman <gregkh@google.com>
2015-04-07 20:26:53 +0200

    greybus: endo: add endo structures and logic
    This adds endo.c and endo.h and provides functions to create an endo and
    the initial 0x0555 set of modules.
    
    But, it doesn't hook this logic up into the running code yet, that comes
    next.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Reviewed-by: Alex Elder <elder@linaro.org>

---
+++ drivers/staging/greybus/endo.h
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Greybus endo code
+ *
+ * Copyright 2015 Google Inc.
+ *
+ * Released under the GPLv2 only.
+ */
+
+#ifndef __ENDO_H
+#define __ENDO_H
+
+/* Greybus "public" definitions" */
+struct gb_svc {
+	u8 serial_number[10];
+	u8 version[10];
+};
+
+struct gb_endo {
+	struct device dev;
+	struct gb_svc svc;
+	u16 type;
+};
+#define to_gb_endo(d) container_of(d, struct gb_endo, dev)
+
+
+/* Greybus "private" definitions */
+struct greybus_host_device;
+
+struct gb_endo *gb_endo_create(struct greybus_host_device *hd);
+void gb_endo_remove(struct gb_endo *endo);
+
+#endif /* __ENDO_H */
