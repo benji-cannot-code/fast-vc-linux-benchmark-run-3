63c98663be503469169b64b3d9d6c2142d99ca8a
Chris Wilson <chris@chris-wilson.co.uk>
2015-04-07 16:20:36 +0100

    drm/i915: Split the batch pool by engine
    I woke up one morning and found 50k objects sitting in the batch pool
    and every search seemed to iterate the entire list... Painting the
    screen in oils would provide a more fluid display.
    
    One issue with the current design is that we only check for retirements
    on the current ring when preparing to submit a new batch. This means
    that we can have thousands of "active" batches on another ring that we
    have to walk over. The simplest way to avoid that is to split the pools
    per ring and then our LRU execution ordering will also ensure that the
    inactive buffers remain at the front.
    
    v2: execlists still requires duplicate code.
    v3: execlists requires more duplicate code
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by:  Tvrtko Ursulin <tvrtko.ursulin@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_ringbuffer.h
+++ drivers/gpu/drm/i915/intel_ringbuffer.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _INTEL_RINGBUFFER_H_
 
 #include <linux/hashtable.h>
+#include "i915_gem_batch_pool.h"
 
 #define I915_CMD_HASH_ORDER 9
 
@@ -133,6 +134,13 @@ struct  intel_engine_cs {
 	u32		mmio_base;
 	struct		drm_device *dev;
 	struct intel_ringbuffer *buffer;
+
+	/*
+	 * A pool of objects to use as shadow copies of client batch buffers
+	 * when the command parser is enabled. Prevents the client from
+	 * modifying the batch contents after software parsing.
+	 */
+	struct i915_gem_batch_pool batch_pool;
 
 	struct intel_hw_status_page status_page;
 
