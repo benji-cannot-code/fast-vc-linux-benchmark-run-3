c423a179f14f991e8c87dbccf35a9563ed3be7a0
Al Viro <viro@zeniv.linux.org.uk>
2015-04-08 15:45:02 -0400

    ocfs2_file_write_iter: keep return value and current position update in sync
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ocfs2/file.c
+++ fs/ocfs2/file.c
@@ -2409,7 +2409,6 @@ relock:
 			goto out_dio;
 		}
 
-		iocb->ki_pos = *ppos + written_buffered;
 		/* We need to ensure that the page cache pages are written to
 		 * disk and invalidated to preserve the expected O_DIRECT
 		 * semantics.
@@ -2418,6 +2417,7 @@ relock:
 		ret = filemap_write_and_wait_range(file->f_mapping, *ppos,
 				endbyte);
 		if (ret == 0) {
+			iocb->ki_pos = *ppos + written_buffered;
 			written += written_buffered;
 			invalidate_mapping_pages(mapping,
 					*ppos >> PAGE_CACHE_SHIFT,
