db180fd5d627813dd824aae9b413ca3a10d5b69f
Michel Thierry <michel.thierry@intel.com>
2015-04-08 12:13:30 +0100

    drm/i915: num_pd_pages/num_pd_entries isn't useful
    These values are never quite useful for dynamic allocations of the page
    tables. Getting rid of them will help prevent later confusion.
    
    v2: Updated to use unmap_and_free_pd functions.
    v3: Updated gen8_ppgtt_free after teardown logic was removed.
    v4: Rebase after s/page_tables/page_table/.
    v5: Keep allocating all page directories in GEN8+ systems with less
    than 4GB of memory. Updated gen6_for_all_pdes.
    v6: Prevent (harmless) out of range access in gen6_for_all_pdes.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Michel Thierry <michel.thierry@intel.com> (v2+)
    Reviewed-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem_gtt.h
+++ drivers/gpu/drm/i915/i915_gem_gtt.h
@@ -315,8 +315,6 @@ struct i915_hw_ppgtt {
 	struct kref ref;
 	struct drm_mm_node node;
 	unsigned long pd_dirty_rings;
-	unsigned num_pd_entries;
-	unsigned num_pd_pages; /* gen8+ */
 	union {
 		struct i915_page_directory_pointer pdp;
 		struct i915_page_directory pd;
@@ -350,6 +348,11 @@ struct i915_hw_ppgtt {
 	     temp = ALIGN(start+1, 1 << GEN6_PDE_SHIFT) - start, \
 	     temp = min_t(unsigned, temp, length), \
 	     start += temp, length -= temp)
+
+#define gen6_for_all_pdes(pt, ppgtt, iter)  \
+	for (iter = 0;		\
+	     pt = ppgtt->pd.page_table[iter], iter < I915_PDES;	\
+	     iter++)
 
 static inline uint32_t i915_pte_index(uint64_t address, uint32_t pde_shift)
 {
