03e2ba8dca7bcaf7910718d2cb1764102c423a35
Karen Xie <kxie@chelsio.com>
2015-04-10 13:57:21 -0700

    cxgb4i: set the initial sequence number
    set the initial sequence number so the 1st byte of payload is aligned on
    the 8-byte boundary.
    
    Signed-off-by: Karen Xie <kxie@chelsio.com>
    Reviewed-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <JBottomley@Odin.com>

--- drivers/scsi/cxgbi/cxgb4i/cxgb4i.h
+++ drivers/scsi/cxgbi/cxgb4i/cxgb4i.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CXGB4I_TX_HEADER_LEN \
 	(sizeof(struct fw_ofld_tx_data_wr) + sizeof(struct sge_opaque_hdr))
 
+#define T5_ISS_VALID		(1 << 18)
+
 struct ulptx_idata {
 	__be32 cmd_more;
 	__be32 len;
