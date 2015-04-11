7bda3bd8594de504c9c72a8cb1b3d9cb6c9c6984
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-04-11 17:41:03 +0900

    ALSA: ctl: fix a bug to return no identical information in info operation for userspace controls
    In operations of SNDRV_CTL_IOCTL_ELEM_INFO, identical information in
    returned value is cleared. This is not better to userspace application.
    
    This commit confirms to return full identical information to the
    operations.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/control.c
+++ sound/core/control.c
@@ -1041,8 +1041,12 @@ static int snd_ctl_elem_user_info(struct snd_kcontrol *kcontrol,
 				  struct snd_ctl_elem_info *uinfo)
 {
 	struct user_element *ue = kcontrol->private_data;
+	unsigned int offset;
 
+	offset = snd_ctl_get_ioff(kcontrol, &uinfo->id);
 	*uinfo = ue->info;
+	snd_ctl_build_ioff(&uinfo->id, kcontrol, offset);
+
 	return 0;
 }
 
@@ -1052,10 +1056,13 @@ static int snd_ctl_elem_user_enum_info(struct snd_kcontrol *kcontrol,
 	struct user_element *ue = kcontrol->private_data;
 	const char *names;
 	unsigned int item;
+	unsigned int offset;
 
 	item = uinfo->value.enumerated.item;
 
+	offset = snd_ctl_get_ioff(kcontrol, &uinfo->id);
 	*uinfo = ue->info;
+	snd_ctl_build_ioff(&uinfo->id, kcontrol, offset);
 
 	item = min(item, uinfo->value.enumerated.items - 1);
 	uinfo->value.enumerated.item = item;
