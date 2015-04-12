ea0e5085b5cd417d07b20253df93030f3f827306
Valentin Rothberg <valentinrothberg@gmail.com>
2015-04-12 17:54:35 +0200

    serial: bfin: ctsrts: enfore Kconfig naming convention
    The CONFIG_ prefix is reserved for Kconfig options in Make and CPP
    syntax; static analysis tools rely on this convention.  This patch
    enforces this behavior for SERIAL_BFIN_{HARD_}CTSRTS.
    
    Signed-off-by: Valentin Rothberg <valentinrothberg@gmail.com>
    Acked-by: Sonic Zhang <sonic.zhang@analog.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/blackfin/include/asm/bfin_serial.h
+++ arch/blackfin/include/asm/bfin_serial.h
@@ -23,9 +23,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
     defined(CONFIG_BFIN_UART2_CTSRTS) || \
     defined(CONFIG_BFIN_UART3_CTSRTS)
 # if defined(BFIN_UART_BF54X_STYLE) || defined(BFIN_UART_BF60X_STYLE)
-#  define CONFIG_SERIAL_BFIN_HARD_CTSRTS
+#  define SERIAL_BFIN_HARD_CTSRTS
 # else
-#  define CONFIG_SERIAL_BFIN_CTSRTS
+#  define SERIAL_BFIN_CTSRTS
 # endif
 #endif
 
@@ -51,8 +51,8 @@ struct bfin_serial_port {
 #elif ANOMALY_05000363
 	unsigned int anomaly_threshold;
 #endif
-#if defined(CONFIG_SERIAL_BFIN_CTSRTS) || \
-	defined(CONFIG_SERIAL_BFIN_HARD_CTSRTS)
+#if defined(SERIAL_BFIN_CTSRTS) || \
+	defined(SERIAL_BFIN_HARD_CTSRTS)
 	int cts_pin;
 	int rts_pin;
 #endif
