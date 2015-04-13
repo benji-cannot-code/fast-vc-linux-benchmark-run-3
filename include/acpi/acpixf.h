ffb6cded23b97240ff81c0cac27117a9231621e6
Bob Moore <robert.moore@intel.com>
2015-04-13 11:51:29 +0800

    ACPICA: Update version to 20150410.
    ACPICA commit 06198cfd96ef271f554a50f1830a5975468c39ac
    ACPICA commit 8a3c1df1edb5f9fc5c940500c598c0107d30df71
    
    Version 20150410.
    
    Link: https://github.com/acpica/acpica/commit/06198cfd
    Link: https://github.com/acpica/acpica/commit/8a3c1df1
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/acpixf.h
+++ include/acpi/acpixf.h
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20150204
+#define ACPI_CA_VERSION                 0x20150410
 
 #include <acpi/acconfig.h>
 #include <acpi/actypes.h>
