081f45fa3f53307de360adba84a5259cd55246bd
Lv Zheng <lv.zheng@intel.com>
2015-04-13 11:49:47 +0800

    ACPICA: Resources: Correct conditional compilation definitions.
    ACPICA commit f92a08512b0b35dce4a7fc6a73216674a3c2541b
    
    Some conditional compilation definitions are wrong across header and source
    files. This patch corrects them for the resources component.
    
    NOTE that a further patch in this patchset cleans up all ACPI_EXEC_APP,
    converting them into ACPI_DEBUGGER, including part of this change.
    
    Link: https://github.com/acpica/acpica/commit/f92a0851
    Reported-by: Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/rsdump.c
+++ drivers/acpi/acpica/rsdump.c
@@ -356,6 +356,7 @@ static void acpi_rs_dump_address_common(union acpi_resource_data *resource)
 	acpi_rs_dump_descriptor(resource, acpi_rs_dump_general_flags);
 }
 
+#ifdef ACPI_EXEC_APP
 /*******************************************************************************
  *
  * FUNCTION:    acpi_rs_dump_resource_list
@@ -465,6 +466,7 @@ void acpi_rs_dump_irq_list(u8 *route_table)
 					   prt_element, prt_element->length);
 	}
 }
+#endif
 
 /*******************************************************************************
  *
