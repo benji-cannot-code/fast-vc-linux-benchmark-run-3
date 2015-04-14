0632e822f6148dc6a7a4bb563e66d7460418eb14
Jakub Wilk <jwilk@jwilk.net>
2015-04-14 15:43:41 -0700

    ocfs2: fix a typo in the copyright statement
    Signed-off-by: Jakub Wilk <jwilk@jwilk.net>
    Reviewed-by: Eric Ren <zren@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ocfs2/dir.c
+++ fs/ocfs2/dir.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  *   linux/fs/minix/dir.c
  *
- *   Copyright (C) 1991, 1992 Linux Torvalds
+ *   Copyright (C) 1991, 1992 Linus Torvalds
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
