ea35a9a528a07dea8ab87bd829f773a24eecd2b7
Arend van Spriel <arend@broadcom.com>
2015-04-14 20:10:26 +0200

    brcmfmac: add description for feature flags
    Some feature flags were not described in the header file. Adding
    the description.
    
    Reviewed-by: Hante Meuleman <meuleman@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/brcm80211/brcmfmac/feature.h
+++ drivers/net/wireless/brcm80211/brcmfmac/feature.h
@@ -20,7 +20,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Features:
  *
+ * MBSS: multiple BSSID support (eg. guest network in AP mode).
  * MCHAN: multi-channel for concurrent P2P.
+ * WOWL: Wake-On-WLAN.
  */
 #define BRCMF_FEAT_LIST \
 	BRCMF_FEAT_DEF(MBSS) \
