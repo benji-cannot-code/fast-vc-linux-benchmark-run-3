96ee1d586de636b7b71310eaff92a48c91cf27b3
Ben Skeggs <bskeggs@redhat.com>
2015-04-14 12:06:44 +1000

    drm/nouveau/gr/gm206: initial init+ctx code
    Uncertain whether the GPC pack change is due to a newer driver version,
    or a legitimate difference from GM204.  My GM204 has broken vram, so
    can't currently try a newer binary driver on it to confirm.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/gm204.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/gm204.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Graphics object classes
  ******************************************************************************/
 
-static struct nvkm_oclass
+struct nvkm_oclass
 gm204_gr_sclass[] = {
 	{ FERMI_TWOD_A, &nvkm_object_ofuncs },
 	{ KEPLER_INLINE_TO_MEMORY_B, &nvkm_object_ofuncs },
@@ -197,7 +197,7 @@ gm204_gr_init_be_0[] = {
 	{}
 };
 
-static const struct gf100_gr_pack
+const struct gf100_gr_pack
 gm204_gr_pack_mmio[] = {
 	{ gm204_gr_init_main_0 },
 	{ gm204_gr_init_fe_0 },
@@ -249,7 +249,7 @@ gm204_gr_init_ctxctl(struct gf100_gr_priv *priv)
 	return 0;
 }
 
-static int
+int
 gm204_gr_init(struct nvkm_object *object)
 {
 	struct gf100_gr_oclass *oclass = (void *)object->oclass;
