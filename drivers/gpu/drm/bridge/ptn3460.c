171363fd7697ba583334b02ceb92b1eba04697d1
Thierry Reding <treding@nvidia.com>
2015-04-14 15:15:34 +0200

    drm/bridge: Remove stale ptn3460.h include
    This header file declares prototypes of functions that are no longer
    used. Remove this file and all references to it.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/bridge/ptn3460.c
+++ drivers/gpu/drm/bridge/ptn3460.c
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <drm/drm_panel.h>
 
-#include "bridge/ptn3460.h"
-
 #include "drm_crtc.h"
 #include "drm_crtc_helper.h"
 #include "drm_edid.h"
