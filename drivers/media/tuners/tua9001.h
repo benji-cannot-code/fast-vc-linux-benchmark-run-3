91cd7aaa82413be426f27bbd7a6f5d28c6f2e458
Antti Palosaari <crope@iki.fi>
2015-04-15 19:12:34 -0300

    [media] tua9001: add I2C bindings
    Add I2C bindings.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/tuners/tua9001.h
+++ drivers/media/tuners/tua9001.h
@@ -25,6 +25,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kconfig.h>
 #include "dvb_frontend.h"
 
+/*
+ * I2C address
+ * 0x60,
+ */
+
+/**
+ * struct tua9001_platform_data - Platform data for the tua9001 driver
+ * @dvb_frontend: DVB frontend.
+ */
+struct tua9001_platform_data {
+	struct dvb_frontend *dvb_frontend;
+};
+
 struct tua9001_config {
 	/*
 	 * I2C address
