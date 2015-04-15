26ed071e182bd2a9e7d2b2603ffce5fe80af7536
Antti Palosaari <crope@iki.fi>
2015-04-15 19:33:13 -0300

    [media] tua9001: remove media attach
    We are using I2C client binding now, so remove old media attach.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/tuners/tua9001.h
+++ drivers/media/tuners/tua9001.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef TUA9001_H
 #define TUA9001_H
 
-#include <linux/kconfig.h>
 #include "dvb_frontend.h"
 
 /*
@@ -36,13 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct tua9001_platform_data {
 	struct dvb_frontend *dvb_frontend;
-};
-
-struct tua9001_config {
-	/*
-	 * I2C address
-	 */
-	u8 i2c_addr;
 };
 
 /*
@@ -64,17 +56,5 @@ struct tua9001_config {
 #define TUA9001_CMD_CEN     0
 #define TUA9001_CMD_RESETN  1
 #define TUA9001_CMD_RXEN    2
-
-#if IS_REACHABLE(CONFIG_MEDIA_TUNER_TUA9001)
-extern struct dvb_frontend *tua9001_attach(struct dvb_frontend *fe,
-		struct i2c_adapter *i2c, struct tua9001_config *cfg);
-#else
-static inline struct dvb_frontend *tua9001_attach(struct dvb_frontend *fe,
-		struct i2c_adapter *i2c, struct tua9001_config *cfg)
-{
-	printk(KERN_WARNING "%s: driver disabled by Kconfig\n", __func__);
-	return NULL;
-}
-#endif
 
 #endif
