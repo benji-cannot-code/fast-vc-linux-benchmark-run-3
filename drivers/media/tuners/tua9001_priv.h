f38dd803ca09e631c112098ad0f1058b50c0124d
Antti Palosaari <crope@iki.fi>
2015-04-15 20:38:43 -0300

    [media] tua9001: use regmap for I2C register access
    Use regmap for I2C register access.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/tuners/tua9001_priv.h
+++ drivers/media/tuners/tua9001_priv.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TUA9001_PRIV_H
 
 #include "tua9001.h"
+#include <linux/regmap.h>
 
 struct tua9001_reg_val {
 	u8 reg;
@@ -28,6 +29,7 @@ struct tua9001_reg_val {
 struct tua9001_dev {
 	struct dvb_frontend *fe;
 	struct i2c_client *client;
+	struct regmap *regmap;
 };
 
 #endif
