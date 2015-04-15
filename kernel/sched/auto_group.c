7fe0def87c566f26445f8cd9c68b7df1627fe1f2
Tobias Klauser <tklauser@distanz.ch>
2015-04-15 18:51:18 +0200

    sched/autogroup: Remove unnecessary #ifdef guards
    Since commit:
    
      029632fbb7b7 ("sched: Make separate sched*.c translation units")
    
    autogroup is a separate translation unit built from the Makefile and
    thus no longer needs its content wrapped with #ifdef CONFIG_SCHED_AUTOGROUP.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1429116678-17000-1-git-send-email-tklauser@distanz.ch
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/auto_group.c
+++ kernel/sched/auto_group.c
@@ -1,6 +1,4 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifdef CONFIG_SCHED_AUTOGROUP
-
 #include "sched.h"
 
 #include <linux/proc_fs.h>
@@ -250,5 +248,3 @@ int autogroup_path(struct task_group *tg, char *buf, int buflen)
 	return snprintf(buf, buflen, "%s-%ld", "/autogroup", tg->autogroup->id);
 }
 #endif /* CONFIG_SCHED_DEBUG */
-
-#endif /* CONFIG_SCHED_AUTOGROUP */
