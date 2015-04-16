0501b4abf2abe411b66f30ae938e4b72cb017658
Marcel Holtmann <marcel@holtmann.org>
2015-04-16 23:15:50 +0200

    Bluetooth: btusb: Fix two coding style issues
    ERROR: spaces required around that '<' (ctx:WxV)
    +		if (err <0)
     		        ^
    
    ERROR: code indent should use tabs where possible
    +^I^I^I^I        sizeof(ver));$
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -917,7 +917,7 @@ static int btusb_open(struct hci_dev *hdev)
 	 */
 	if (data->setup_on_usb) {
 		err = data->setup_on_usb(hdev);
-		if (err <0)
+		if (err < 0)
 			return err;
 	}
 
@@ -2974,7 +2974,7 @@ static int btusb_setup_qca(struct hci_dev *hdev)
 	int i, err;
 
 	err = btusb_qca_send_vendor_req(hdev, QCA_GET_TARGET_VERSION, &ver,
-				        sizeof(ver));
+					sizeof(ver));
 	if (err < 0)
 		return err;
 
