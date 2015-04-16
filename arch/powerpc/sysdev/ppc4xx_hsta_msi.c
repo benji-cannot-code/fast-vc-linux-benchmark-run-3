dec33dddf60a1f5373b51250012f4086318cf725
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2015-04-16 20:18:50 +0800

    powerpc/4xx: Fix return value check in hsta_msi_probe()
    In case of error, the functions platform_get_resource() and kmalloc()
    returns NULL not ERR_PTR(). The IS_ERR() test in the return value check
    should be replaced with NULL test.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/sysdev/ppc4xx_hsta_msi.c
+++ arch/powerpc/sysdev/ppc4xx_hsta_msi.c
@@ -133,7 +133,7 @@ static int hsta_msi_probe(struct platform_device *pdev)
 	struct pci_controller *phb;
 
 	mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (IS_ERR(mem)) {
+	if (!mem) {
 		dev_err(dev, "Unable to get mmio space\n");
 		return -EINVAL;
 	}
@@ -158,7 +158,7 @@ static int hsta_msi_probe(struct platform_device *pdev)
 		goto out;
 
 	ppc4xx_hsta_msi.irq_map = kmalloc(sizeof(int) * irq_count, GFP_KERNEL);
-	if (IS_ERR(ppc4xx_hsta_msi.irq_map)) {
+	if (!ppc4xx_hsta_msi.irq_map) {
 		ret = -ENOMEM;
 		goto out1;
 	}
