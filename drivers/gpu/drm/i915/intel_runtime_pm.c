d624623cf2e35a17ae4f2d187e6025be4a7c4264
Suketu Shah <suketu.j.shah@intel.com>
2015-04-16 14:22:11 +0530

    drm/i915/skl: Add DC6 Trigger sequence.
    Add triggers for DC6 as per details provided in skl_enable_dc6
    and skl_disable_dc6 implementations.
    
    Also Call POSTING_READ for every write to a register to ensure
    it is written to immediately
    
    v1: Remove POSTING_READ and intel_prepare_ddi calls as they've been added in previous patches.
    
    v2:
    1] Remove check for backlight disabled as it should be the case by that time.
    2] Mark DC5 as disabled when enabling DC6.
    3] Return from DC5-disabling function early if DC5 is already be disabled which can happen
       due to DC6-enabling earlier.
    3] Ensure CSR firmware is loaded after resume from DC6 as corresponding memory contents won't
       be retained after runtime-suspend.
    4] Ensure that CSR isn't identified as loaded before CSR-loading program is called during
       runtime-resume.
    
    v3: Rebase to latest
    Modified as per review comments from Imre and after discussion with Art:
    1] DC6 should be preferably enabled when PG2 is disabled by SW as the check for PG1 being
       disabled is taken of by HW to enter DC6, and disabled when PG2 is enabled respectively.
       This helps save more power, especially in the case when display is disabled but GT is
       enabled. Accordingly, replacing DC5 trigger sequence with DC6 for SKL.
    2] DC6 could be enabled from intel_runtime_suspend() function, if DC5 is already enabled.
    3] Move CSR-load-status setting code from intel_runtime_suspend function to a new function.
    
    v4:
    1] Enable/disable DC6 only when toggling the power-well using a newly defined macro ENABLE_DC6.
    
    v5:
    1] Load CSR on system resume too as firmware may be lost on system suspend preventing
       enabling DC5, DC6.
    2] DDI buffers shouldn't be programmed during driver-load/resume as it's already done
       during modeset initialization then and also that the encoder list is still uninitialized by
       then. Therefore, call intel_prepare_ddi function right after disabling DC6 but outside
       skl_disable_dc6 function and not during driver-load/resume.
    
    v6:
    1] Rebase to latest.
    2] Move SKL_ENABLE_DC6 macro definition from intel_display.c to intel_runtime_pm.c.
    
    v7:
    1) Refactored the code for removing the warning got from checkpatch.
    2) After adding dmc ver 1.0 support rebased on top of nightly. (Animesh)
    
    v8:
    - Reverted the changes done in v7.
    - Removed the condition check in skl_prepare_resune(). (Animesh)
    
    Issue: VIZ-2819
    Signed-off-by: A.Sunil Kamath <sunil.kamath@intel.com>
    Signed-off-by: Suketu Shah <suketu.j.shah@intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Animesh Manna <animesh.manna@intel.com>
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_runtime_pm.c
+++ drivers/gpu/drm/i915/intel_runtime_pm.c
@@ -50,7 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * present for a given platform.
  */
 
-#define GEN9_ENABLE_DC5(dev) (IS_SKYLAKE(dev))
+#define GEN9_ENABLE_DC5(dev) 0
+#define SKL_ENABLE_DC6(dev) IS_SKYLAKE(dev)
 
 #define for_each_power_well(i, power_well, domain_mask, power_domains)	\
 	for (i = 0;							\
@@ -496,6 +497,16 @@ static void gen9_disable_dc5(struct drm_i915_private *dev_priv)
 	POSTING_READ(DC_STATE_EN);
 }
 
+static void skl_enable_dc6(struct drm_i915_private *dev_priv)
+{
+	/* TODO: Implementation to be done. */
+}
+
+static void skl_disable_dc6(struct drm_i915_private *dev_priv)
+{
+	/* TODO: Implementation to be done. */
+}
+
 static void skl_set_power_well(struct drm_i915_private *dev_priv,
 			struct i915_power_well *power_well, bool enable)
 {
@@ -543,9 +554,21 @@ static void skl_set_power_well(struct drm_i915_private *dev_priv,
 				!I915_READ(HSW_PWR_WELL_BIOS),
 				"Invalid for power well status to be enabled, unless done by the BIOS, \
 				when request is to disable!\n");
-			if (GEN9_ENABLE_DC5(dev) &&
-				power_well->data == SKL_DISP_PW_2)
-				gen9_disable_dc5(dev_priv);
+			if ((GEN9_ENABLE_DC5(dev) || SKL_ENABLE_DC6(dev)) &&
+				power_well->data == SKL_DISP_PW_2) {
+				if (SKL_ENABLE_DC6(dev)) {
+					skl_disable_dc6(dev_priv);
+					/*
+					 * DDI buffer programming unnecessary during driver-load/resume
+					 * as it's already done during modeset initialization then.
+					 * It's also invalid here as encoder list is still uninitialized.
+					 */
+					if (!dev_priv->power_domains.initializing)
+						intel_prepare_ddi(dev);
+				} else {
+					gen9_disable_dc5(dev_priv);
+				}
+			}
 			I915_WRITE(HSW_PWR_WELL_DRIVER, tmp | req_mask);
 		}
 
@@ -563,17 +586,23 @@ static void skl_set_power_well(struct drm_i915_private *dev_priv,
 			POSTING_READ(HSW_PWR_WELL_DRIVER);
 			DRM_DEBUG_KMS("Disabling %s\n", power_well->name);
 
-			if (GEN9_ENABLE_DC5(dev) &&
+			if ((GEN9_ENABLE_DC5(dev) || SKL_ENABLE_DC6(dev)) &&
 				power_well->data == SKL_DISP_PW_2) {
 				enum csr_state state;
-
+				/* TODO: wait for a completion event or
+				 * similar here instead of busy
+				 * waiting using wait_for function.
+				 */
 				wait_for((state = intel_csr_load_status_get(dev_priv)) !=
 						FW_UNINITIALIZED, 1000);
 				if (state != FW_LOADED)
 					DRM_ERROR("CSR firmware not ready (%d)\n",
 							state);
 				else
-					gen9_enable_dc5(dev_priv);
+					if (SKL_ENABLE_DC6(dev))
+						skl_enable_dc6(dev_priv);
+					else
+						gen9_enable_dc5(dev_priv);
 			}
 		}
 	}
