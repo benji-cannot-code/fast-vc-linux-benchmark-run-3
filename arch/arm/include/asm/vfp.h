460de498d68a43f9389d13c1ba54f1fe82216169
Florian Fainelli <f.fainelli@gmail.com>
2015-04-17 16:53:58 -0700

    ARM: vfp: Add vfp_disable for problematic platforms
    Some platforms might not be able to fully utilize VFP when e.g: one CPU
    out of two in a SMP complex lacks a VFP unit. Adding code to migrate
    task to the CPU which has a VFP unit would be cumbersome and not
    performant, instead, just add the ability to disable VFP.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

--- arch/arm/include/asm/vfp.h
+++ arch/arm/include/asm/vfp.h
@@ -92,4 +92,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VFPOPDESC_UNUSED_MASK	(0xFF << VFPOPDESC_UNUSED_BIT)
 #define VFPOPDESC_OPDESC_MASK	(~(VFPOPDESC_LENGTH_MASK | VFPOPDESC_UNUSED_MASK))
 
+#ifndef __ASSEMBLY__
+void vfp_disable(void);
+#endif
+
 #endif /* __ASM_VFP_H */
