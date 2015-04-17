e77fa41cbc89e8f025e1fdffd7adc8c619fce9ce
Nicolas Iooss <nicolas.iooss_linux@m4x.org>
2015-04-17 17:41:43 +0800

    Staging: fbtft: fix header guard typo
    drivers/staging/fbtft/internal.h header guard tests for
    __LINUX_FBTFT__INTERNAL_H but then defines __LINUX_FBTFT_INTERNAL_H
    (only 1 underscore) and uses the same name for the #endif comment.
    Use the same name everywhere.
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss_linux@m4x.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fbtft/internal.h
+++ drivers/staging/fbtft/internal.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#ifndef __LINUX_FBTFT__INTERNAL_H
+#ifndef __LINUX_FBTFT_INTERNAL_H
 #define __LINUX_FBTFT_INTERNAL_H
 
 void fbtft_sysfs_init(struct fbtft_par *par);
