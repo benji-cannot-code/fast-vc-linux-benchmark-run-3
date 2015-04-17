0a2a294c89456f3fe61a8a53fbeff2936a19caf5
Sowmini Varadhan <sowmini.varadhan@oracle.com>
2015-04-16 22:28:04 -0400

    iommu-common: Fix PARISC compile-time warnings
    Fixes warnings due to
    - no DMA_ERROR_CODE on PARISC,
    - sizeof (unsigned long) == 4 bytes on PARISC.
    
    Signed-off-by: Sowmini Varadhan <sowmini.varadhan@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- lib/iommu-common.c
+++ lib/iommu-common.c
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iommu-common.h>
 #include <linux/dma-mapping.h>
 
+#ifndef	DMA_ERROR_CODE
+#define	DMA_ERROR_CODE (~(dma_addr_t)0x0)
+#endif
+
 #define IOMMU_LARGE_ALLOC	15
 
 /*
@@ -122,7 +126,7 @@ unsigned long iommu_tbl_range_alloc(struct device *dev,
 		boundary_size = ALIGN(dma_get_seg_boundary(dev) + 1,
 				      1 << iommu->page_table_shift);
 	else
-		boundary_size = ALIGN(1UL << 32, 1 << iommu->page_table_shift);
+		boundary_size = ALIGN(1ULL << 32, 1 << iommu->page_table_shift);
 
 	shift = iommu->page_table_map_base >> iommu->page_table_shift;
 	boundary_size = boundary_size >> iommu->page_table_shift;
