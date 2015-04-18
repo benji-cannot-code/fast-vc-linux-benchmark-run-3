4c55aea346b7e7475171995985007be521756de3
Paul Gortmaker <paul.gortmaker@windriver.com>
2015-04-18 16:40:28 -0400

    mips/mm/tlbex: remove new instance of __cpuinit that crept back in
    We removed __cpuinit support (leaving no-op stubs) quite some time ago.
    However a new instance was added in commit c5b367835cfc7a8ef53b9670a409ff
    ("MIPS: Add support for XPA.")
    
    Since we want to clobber the stubs soon, get this removed now.
    
    Cc: Steven J. Hill <Steven.Hill@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/mips/mm/tlbex.c
+++ arch/mips/mm/tlbex.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uasm.h>
 #include <asm/setup.h>
 
-static int __cpuinitdata mips_xpa_disabled;
+static int mips_xpa_disabled;
 
 static int __init xpa_disable(char *s)
 {
