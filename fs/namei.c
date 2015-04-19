fd7b89155839fd17436e8a6e525f64ff067a4df0
Al Viro <viro@zeniv.linux.org.uk>
2015-04-18 20:14:20 -0400

    link_path_walk: get rid of duplication
    What we do after the second walk_component() + put_link() + depth
    decrement in there is exactly equivalent to what's done right
    after the first walk_component().  Easy to verify and not at all
    surprising, seeing that there we have just walked the last
    component of nested symlink.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -1772,6 +1772,7 @@ static int link_path_walk(const char *name, struct nameidata *nd)
 			return 0;
 
 		err = walk_component(nd, LOOKUP_FOLLOW);
+Walked:
 		if (err < 0)
 			return err;
 
@@ -1790,7 +1791,6 @@ static int link_path_walk(const char *name, struct nameidata *nd)
 			nd->depth++;
 			current->link_count++;
 
-loop:	/* will be gone very soon */
 			link = nd->link;
 			s = get_link(&link, nd, &cookie);
 
@@ -1827,13 +1827,7 @@ loop:	/* will be gone very soon */
 					put_link(nd, &link, cookie);
 					current->link_count--;
 					nd->depth--;
-					if (err < 0)
-						return err;
-					if (err > 0) {
-						current->link_count++;
-						nd->depth++;
-						goto loop;
-					}
+					goto Walked;
 				}
 			}
 		}
