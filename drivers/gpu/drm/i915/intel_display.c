d14de160fa15477cb3bbeba133eef0bf450a4b8f
Ander Conselvan de Oliveira <ander.conselvan.de.oliveira@intel.com>
2015-04-21 17:13:10 +0300

    drm/i915: Delete fb, x and y parameters from mode set functions
    We don't need to pass it down the call chain anymore now that the plane
    state is set up properly.
    
    Signed-off-by: Ander Conselvan de Oliveira <ander.conselvan.de.oliveira@intel.com>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -84,7 +84,6 @@ static void ironlake_pch_clock_get(struct intel_crtc *crtc,
 				   struct intel_crtc_state *pipe_config);
 
 static int intel_set_mode(struct drm_crtc *crtc, struct drm_display_mode *mode,
-			  int x, int y, struct drm_framebuffer *old_fb,
 			  struct drm_atomic_state *state);
 static int intel_framebuffer_init(struct drm_device *dev,
 				  struct intel_framebuffer *ifb,
@@ -9892,7 +9891,7 @@ retry:
 	if (ret)
 		goto fail;
 
-	if (intel_set_mode(crtc, mode, 0, 0, fb, state)) {
+	if (intel_set_mode(crtc, mode, state)) {
 		DRM_DEBUG_KMS("failed to set mode on load-detect pipe\n");
 		if (old->release_fb)
 			old->release_fb->funcs->destroy(old->release_fb);
@@ -9966,7 +9965,7 @@ void intel_release_load_detect_pipe(struct drm_connector *connector,
 		if (ret)
 			goto fail;
 
-		intel_set_mode(crtc, NULL, 0, 0, NULL, state);
+		intel_set_mode(crtc, NULL, state);
 
 		drm_atomic_state_free(state);
 
@@ -12306,7 +12305,6 @@ static int __intel_set_mode_checks(struct drm_atomic_state *state)
 
 static int __intel_set_mode(struct drm_crtc *modeset_crtc,
 			    struct drm_display_mode *mode,
-			    int x, int y, struct drm_framebuffer *fb,
 			    struct intel_crtc_state *pipe_config)
 {
 	struct drm_device *dev = modeset_crtc->dev;
@@ -12416,12 +12414,11 @@ static int __intel_set_mode(struct drm_crtc *modeset_crtc,
 
 static int intel_set_mode_with_config(struct drm_crtc *crtc,
 				      struct drm_display_mode *mode,
-				      int x, int y, struct drm_framebuffer *fb,
 				      struct intel_crtc_state *pipe_config)
 {
 	int ret;
 
-	ret = __intel_set_mode(crtc, mode, x, y, fb, pipe_config);
+	ret = __intel_set_mode(crtc, mode, pipe_config);
 
 	if (ret == 0)
 		intel_modeset_check_state(crtc->dev);
@@ -12431,7 +12428,6 @@ static int intel_set_mode_with_config(struct drm_crtc *crtc,
 
 static int intel_set_mode(struct drm_crtc *crtc,
 			  struct drm_display_mode *mode,
-			  int x, int y, struct drm_framebuffer *fb,
 			  struct drm_atomic_state *state)
 {
 	struct intel_crtc_state *pipe_config;
@@ -12443,7 +12439,7 @@ static int intel_set_mode(struct drm_crtc *crtc,
 		goto out;
 	}
 
-	ret = intel_set_mode_with_config(crtc, mode, x, y, fb, pipe_config);
+	ret = intel_set_mode_with_config(crtc, mode, pipe_config);
 	if (ret)
 		goto out;
 
@@ -12515,8 +12511,7 @@ void intel_crtc_restore_mode(struct drm_crtc *crtc)
 	intel_modeset_setup_plane_state(state, crtc, &crtc->mode,
 					crtc->primary->fb, crtc->x, crtc->y);
 
-	intel_set_mode(crtc, &crtc->mode, crtc->x, crtc->y, crtc->primary->fb,
-		       state);
+	intel_set_mode(crtc, &crtc->mode, state);
 
 	drm_atomic_state_free(state);
 }
@@ -12922,7 +12917,6 @@ static int intel_crtc_set_config(struct drm_mode_set *set)
 
 	if (config->mode_changed) {
 		ret = intel_set_mode_with_config(set->crtc, set->mode,
-						 set->x, set->y, set->fb,
 						 pipe_config);
 	} else if (config->fb_changed) {
 		struct intel_crtc *intel_crtc = to_intel_crtc(set->crtc);
