b9824f586d4036287d1b035e87c39e4885bd5401
Herbert Xu <herbert@gondor.apana.org.au>
2015-04-21 10:46:39 +0800

    crypto: rng - Add crypto_rng_set_entropy
    This patch adds the function crypto_rng_set_entropy.  It is only
    meant to be used by testmgr when testing RNG implementations by
    providing fixed entropy data in order to verify test vectors.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/rng.h
+++ include/crypto/rng.h
@@ -33,6 +33,8 @@ struct crypto_rng;
  *		up a new state, the seed must be provided by the
  *		consumer while invoking this function. The required
  *		size of the seed is defined with @seedsize .
+ * @set_ent:	Set entropy that would otherwise be obtained from
+ *		entropy source.  Internal use only.
  * @seedsize:	The seed size required for a random number generator
  *		initialization defined with this variable. Some
  *		random number generators does not require a seed
@@ -46,6 +48,8 @@ struct rng_alg {
 			const u8 *src, unsigned int slen,
 			u8 *dst, unsigned int dlen);
 	int (*seed)(struct crypto_rng *tfm, const u8 *seed, unsigned int slen);
+	void (*set_ent)(struct crypto_rng *tfm, const u8 *data,
+			unsigned int len);
 
 	unsigned int seedsize;
 
