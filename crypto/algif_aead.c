57fdecf07949bd9a74e9c6935319d77d940c12a6
Herbert Xu <herbert@gondor.apana.org.au>
2015-04-22 15:06:28 +0800

    crypto: algif_aead - Include crypto/aead.h
    All users of AEAD should include crypto/aead.h instead of
    include/linux/crypto.h.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Acked-by: David S. Miller <davem@davemloft.net>

--- crypto/algif_aead.c
+++ crypto/algif_aead.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * any later version.
  */
 
+#include <crypto/aead.h>
 #include <crypto/scatterwalk.h>
 #include <crypto/if_alg.h>
 #include <linux/init.h>
