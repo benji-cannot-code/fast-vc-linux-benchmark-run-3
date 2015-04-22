e10867d86d7f74986de1eaa9eb994718daed0ced
Herbert Xu <herbert@gondor.apana.org.au>
2015-04-22 15:06:32 +0800

    mac80211: Include crypto/aead.h
    All users of AEAD should include crypto/aead.h instead of
    include/linux/crypto.h.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Acked-by: David S. Miller <davem@davemloft.net>

--- net/mac80211/aes_gmac.c
+++ net/mac80211/aes_gmac.c
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/types.h>
-#include <linux/crypto.h>
 #include <linux/err.h>
+#include <crypto/aead.h>
 #include <crypto/aes.h>
 
 #include <net/mac80211.h>
