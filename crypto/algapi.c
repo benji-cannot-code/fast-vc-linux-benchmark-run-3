cf7cbe99faa1bf0361f81671d8dd93e3deb883e7
Herbert Xu <herbert@gondor.apana.org.au>
2015-04-22 13:25:53 +0800

    crypto: api - Include linux/fips.h
    All users of fips_enabled should include linux/fips.h directly
    instead of getting it through internal.h.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/algapi.c
+++ crypto/algapi.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/err.h>
 #include <linux/errno.h>
+#include <linux/fips.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/list.h>
