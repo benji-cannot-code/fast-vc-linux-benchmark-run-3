8c7b4ad777ce30b25d1f47bb3a0dc52536b99f51
Jaegeuk Kim <jaegeuk@kernel.org>
2015-04-23 10:27:21 -0700

    f2fs: add f2fs_may_inline_{data, dentry}
    This patch adds f2fs_may_inline_data and f2fs_may_inline_dentry.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "f2fs.h"
 
-bool f2fs_may_inline(struct inode *inode)
+bool f2fs_may_inline_data(struct inode *inode)
 {
 	if (!test_opt(F2FS_I_SB(inode), INLINE_DATA))
 		return false;
@@ -26,6 +26,17 @@ bool f2fs_may_inline(struct inode *inode)
 		return false;
 
 	if (i_size_read(inode) > MAX_INLINE_DATA)
+		return false;
+
+	return true;
+}
+
+bool f2fs_may_inline_dentry(struct inode *inode)
+{
+	if (!test_opt(F2FS_I_SB(inode), INLINE_DENTRY))
+		return false;
+
+	if (!S_ISDIR(inode->i_mode))
 		return false;
 
 	return true;
