1aa0528eea64b35cc611c1e9eade9bb23b2b57de
Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
2015-04-24 14:48:35 +0800

    nios2: Add types.h header required for __u32 type
    Reported by the header checker (CONFIG_HEADERS_CHECK=y):
    
      CHECK   usr/include/asm/ (31 files)
    ./usr/include/asm/ptrace.h:77: found __[us]{8,16,32,64} type without #include <linux/types.h>
    
    Signed-off-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
    Acked-by: Ley Foon Tan <lftan@altera.com>

--- arch/nios2/include/uapi/asm/ptrace.h
+++ arch/nios2/include/uapi/asm/ptrace.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
+#include <linux/types.h>
+
 /*
  * Register numbers used by 'ptrace' system call interface.
  */
