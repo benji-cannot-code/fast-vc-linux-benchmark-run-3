fc397d75250a7bdd4b05bb824ba4403623035912
Hans Verkuil <hans.verkuil@cisco.com>
2015-04-25 12:36:18 -0300

    [media] dt3155: move out of staging into drivers/media/pci
    The dt3155 code is now in good shape, so move it out of staging into
    drivers/media/pci. Mark in MAINTAINERS that I'll do Odd Fixes for this
    driver.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/pci/dt3155/dt3155.h
+++ drivers/media/pci/dt3155/dt3155.h
@@ -25,8 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-dev.h>
 
 #define DT3155_NAME "dt3155"
-#define DT3155_VER_MAJ 1
-#define DT3155_VER_MIN 1
+#define DT3155_VER_MAJ 2
+#define DT3155_VER_MIN 0
 #define DT3155_VER_EXT 0
 #define DT3155_VERSION  __stringify(DT3155_VER_MAJ)	"."		\
 			__stringify(DT3155_VER_MIN)	"."		\
