c2db161ea06b982dddbc8eabaa8b746bb0a4c663
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-04-28 13:46:20 +0900

    clk: cdce706: Constify parent names in clock init data
    The array of parent names can be made as array of const pointers to
    const strings.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/clk-cdce706.c
+++ drivers/clk/clk-cdce706.c
@@ -95,7 +95,7 @@ static const char * const cdce706_source_name[] = {
 	"clk_in0", "clk_in1",
 };
 
-static const char *cdce706_clkin_name[] = {
+static const char * const cdce706_clkin_name[] = {
 	"clk_in",
 };
 
@@ -103,7 +103,7 @@ static const char * const cdce706_pll_name[] = {
 	"pll1", "pll2", "pll3",
 };
 
-static const char *cdce706_divider_parent_name[] = {
+static const char * const cdce706_divider_parent_name[] = {
 	"clk_in", "pll1", "pll2", "pll2", "pll3",
 };
 
