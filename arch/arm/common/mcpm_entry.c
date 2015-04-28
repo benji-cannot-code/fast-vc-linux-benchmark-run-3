86f4307d4657c16a35e3cbbc352829e7cd94dbcb
Nicolas Pitre <nicolas.pitre@linaro.org>
2015-04-28 14:51:36 -0400

    ARM: MCPM: add references to the available documentation in the code
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Acked-by: Dave Martin <Dave.Martin@arm.com>

--- arch/arm/common/mcpm_entry.c
+++ arch/arm/common/mcpm_entry.c
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cputype.h>
 #include <asm/suspend.h>
 
+/*
+ * The public API for this code is documented in arch/arm/include/asm/mcpm.h.
+ * For a comprehensive description of the main algorithm used here, please
+ * see Documentation/arm/cluster-pm-race-avoidance.txt.
+ */
 
 struct sync_struct mcpm_sync;
 
