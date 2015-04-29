9b1a78554d6e5e14a4eb7025310e0803eff5d038
Qipan Li <Qipan.Li@csr.com>
2015-04-29 06:45:08 +0000

    serial: sirf: use dynamic method allocate uart structure
    In different platform of SiRF SoCs, there is no same uart and usp-uart
    numbers, it is not convenient to use hard-coded ports array and port
    lines.
    
    here we drop the hard-coded ports table , and drop "cell-index". then
    move to use alias id to get line.
    
    for example:
    	aliases {
    		serial0 = &uart0;
    		serial1 = &uart1;
    		serial2 = &uart2;
    		serial3 = &uart3;
    		serial4 = &uart4;
    		serial5 = &uart5;
    		serial6 = &uart6;
    		serial9 = &usp2;
    	};
    
    at the same, enlarge the max port number according to the chip with the most
    UART.
    
    Signed-off-by: Qipan Li <Qipan.Li@csr.com>
    Signed-off-by: Barry Song <Baohua.Song@csr.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/sirfsoc_uart.h
+++ drivers/tty/serial/sirfsoc_uart.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct sirfsoc_uart_param {
 	const char *uart_name;
 	const char *port_name;
-	u32 uart_nr;
-	u32 register_uart_nr;
 };
 
 struct sirfsoc_register {
@@ -184,8 +182,6 @@ struct sirfsoc_uart_register sirfsoc_usp = {
 	.uart_param = {
 		.uart_name = "ttySiRF",
 		.port_name = "sirfsoc-uart",
-		.uart_nr = 2,
-		.register_uart_nr = 3,
 	},
 };
 
@@ -256,8 +252,6 @@ struct sirfsoc_uart_register sirfsoc_uart = {
 	.uart_param = {
 		.uart_name = "ttySiRF",
 		.port_name = "sirfsoc_uart",
-		.uart_nr = 3,
-		.register_uart_nr = 0,
 	},
 };
 /* uart io ctrl */
@@ -366,7 +360,7 @@ struct sirfsoc_uart_register sirfsoc_uart = {
 #define SIRFSOC_UART_MINOR			0
 #define SIRFUART_PORT_NAME			"sirfsoc-uart"
 #define SIRFUART_MAP_SIZE			0x200
-#define SIRFSOC_UART_NR				6
+#define SIRFSOC_UART_NR				11
 #define SIRFSOC_PORT_TYPE			0xa5
 
 /* Uart Common Use Macro*/
