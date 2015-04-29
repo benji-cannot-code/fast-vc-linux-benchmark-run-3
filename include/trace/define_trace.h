60f8341dd44cbb78035ba78ca52b79bade041c75
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2015-04-29 13:11:00 -0400

    tracing: Move the perf code out of trace_event.h
    The trace_event.h file is for the generic trace event code. Move
    the perf related code into its own trace header file perf.h
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/trace/define_trace.h
+++ include/trace/define_trace.h
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_EVENT_TRACING
 #include <trace/trace_events.h>
+#include <trace/perf.h>
 #endif
 
 #undef TRACE_EVENT
