f9766efeaee0c7e37657a240649b7be0d3abf422
Will Deacon <will.deacon@arm.com>
2015-04-30 18:55:50 +0100

    arm64: cpufeature.h: add missing #include of kernel.h
    cpufeature.h makes use of DECLARE_BITMAP, which in turn relies on the
    BITS_TO_LONGS and DIV_ROUND_UP macros.
    
    This patch includes kernel.h in cpufeature.h to prevent all users having
    to do the same thing.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
+#include <linux/kernel.h>
+
 struct arm64_cpu_capabilities {
 	const char *desc;
 	u16 capability;
