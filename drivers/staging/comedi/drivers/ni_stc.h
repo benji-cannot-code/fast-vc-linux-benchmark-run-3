898e2cb7da7773c383fc31a6f61c3a0e71137a4e
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-05-01 14:59:40 -0700

    staging: comedi: ni_stc.h: tidy up AI_Status_2_Register
    Rename the CamelCase.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_stc.h
+++ drivers/staging/comedi/drivers/ni_stc.h
@@ -535,7 +535,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NISTC_G01_STATUS_REG		4
 
-#define AI_Status_2_Register		5
+#define NISTC_AI_STATUS2_REG		5
 
 #define AO_Status_2_Register		6
 
