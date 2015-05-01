818f21f81a6a0c6a81e97ab8ad7ce14eb887a727
Ian Abbott <abbotti@mev.co.uk>
2015-05-01 18:03:59 +0100

    staging: comedi: mite.h: remove "../comedi_pci.h" and make self-reliant
    "mite.h" relies on a lot of things declared including "../comedi_pci.h",
    but doesn't need anything in "../comedi_pci.h" itself.  None of the
    Comedi modules that include "mite.h" rely on it to include
    "../comedi_pci.h" on their behalf.
    
    Remove the inclusion of "../comedi_pci.h" from "mite.h", and add enough
    other includes and (incomplete) `struct` declarations to make it compile
    without warnings or errors even when included first in a .c file.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/mite.h
+++ drivers/staging/comedi/drivers/mite.h
@@ -20,10 +20,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MITE_H_
 #define _MITE_H_
 
+#include <linux/io.h>
 #include <linux/log2.h>
-#include "../comedi_pci.h"
+#include <linux/spinlock.h>
 
 #define MAX_MITE_DMA_CHANNELS 8
+
+struct comedi_device;
+struct comedi_subdevice;
+struct device;
+struct pci_dev;
 
 struct mite_dma_descriptor {
 	__le32 count;
