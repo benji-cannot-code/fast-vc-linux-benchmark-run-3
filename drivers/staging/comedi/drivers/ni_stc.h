b3a9a6673906d08ec2f3ede0becbf5a6e2357998
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-05-01 14:59:48 -0700

    staging: comedi: ni_stc.h: tidy up Joint_Status_2_Register and bits
    Rename the CamelCase. Use the BIT() macro to define the bits.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_stc.h
+++ drivers/staging/comedi/drivers/ni_stc.h
@@ -556,13 +556,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NISTC_DIO_SERIAL_IN_REG		28
 
+#define NISTC_STATUS2_REG		29
+#define NISTC_STATUS2_AO_TMRDACWRS_IN_PROGRESS	BIT(5)
+
 #define AI_SI_Save_Registers		64
 #define AI_SC_Save_Registers		66
-
-#define Joint_Status_2_Register         29
-enum Joint_Status_2_Bits {
-	AO_TMRDACWRs_In_Progress_St = 0x20,
-};
 
 /* command register */
 #define G_Disarm_Copy			_bit15	/* strobe */
