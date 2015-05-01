07205491d8d2a27617d317565d2c66492787dd80
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-05-01 14:59:42 -0700

    staging: comedi: ni_stc.h: tidy up DIO_Parallel_Input_Register
    Rename the CamelCase.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_stc.h
+++ drivers/staging/comedi/drivers/ni_stc.h
@@ -539,7 +539,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NISTC_AO_STATUS2_REG		6
 
-#define DIO_Parallel_Input_Register	7
+#define NISTC_DIO_IN_REG		7
 
 #define AI_SI_Save_Registers		64
 #define AI_SC_Save_Registers		66
