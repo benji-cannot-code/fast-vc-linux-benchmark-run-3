19f2c6f8feced8a8441595cf5c0fc396ea283576
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-05-01 15:00:04 -0700

    staging: comedi: ni_stc.h: tidy up G0_G1_Select register and bits
    Rename the CamelCase. Convert the inline helper functions into macros.
    
    Tidy up the driver code to use the new macros. For consistency, make
    the ni_set_gpct_dma_channel() helper follow the same style as the
    ni_set_ai_dma_channel() and ni_set_ao_dma_channel() helpers.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_stc.h
+++ drivers/staging/comedi/drivers/ni_stc.h
@@ -578,6 +578,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NI_E_DMA_AO_SEL(x)		(((x) & 0xf) << 4)
 #define NI_E_DMA_AO_SEL_MASK		NI_E_DMA_AO_SEL(0xf)
 
+#define NI_E_DMA_G0_G1_SEL_REG		0x0b	/* w8 */
+#define NI_E_DMA_G0_G1_SEL(_g, _c)	(((_c) & 0xf) << ((_g) * 4))
+#define NI_E_DMA_G0_G1_SEL_MASK(_g)	NI_E_DMA_G0_G1_SEL((_g), 0xf)
+
 #define NI_E_SERIAL_CMD_REG		0x0d	/* w8 */
 #define NI_E_SERIAL_CMD_DAC_LD(x)	BIT(3 + (x))
 #define NI_E_SERIAL_CMD_EEPROM_CS	BIT(2)
@@ -589,7 +593,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NI_E_MISC_CMD_EXT_ATRIG		NI_E_MISC_CMD_INTEXT_ATRIG(0)
 #define NI_E_MISC_CMD_INT_ATRIG		NI_E_MISC_CMD_INTEXT_ATRIG(1)
 
-#define G0_G1_Select			0x0b
 static inline unsigned ni_stc_dma_channel_select_bitfield(unsigned channel)
 {
 	if (channel < 4)
@@ -600,18 +603,6 @@ static inline unsigned ni_stc_dma_channel_select_bitfield(unsigned channel)
 		return 0x5;
 	BUG();
 	return 0;
-}
-
-static inline unsigned GPCT_DMA_Select_Bits(unsigned gpct_index,
-					    unsigned mite_channel)
-{
-	return ni_stc_dma_channel_select_bitfield(mite_channel) << (4 *
-								    gpct_index);
-}
-
-static inline unsigned GPCT_DMA_Select_Mask(unsigned gpct_index)
-{
-	return 0xf << (4 * gpct_index);
 }
 
 #define NI_E_AI_CFG_LO_REG		0x10	/* w16 */
