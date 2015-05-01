693e2f4838aa5540270a67c286eb976a8dfcb5c7
Julia Lawall <Julia.Lawall@lip6.fr>
2015-05-01 21:38:00 +0200

    Staging: lustre: llite: llite_lib: remove unneeded null test before free
    Kfree can cope with a null argument, so drop null tests.
    
    The semantic patch that makes this change is as follows:
    (http://coccinelle.lip6.fr/)
    
    // <smpl>
    @@ expression ptr; @@
    
    - if (ptr != NULL)
      kfree(ptr);
    // </smpl>
    
    Signed-off-by: Julia Lawall <Julia.Lawall@lip6.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_lib.c
+++ drivers/staging/lustre/lustre/llite/llite_lib.c
@@ -575,10 +575,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 		get_uuid2fsid(uuid->uuid, strlen(uuid->uuid), &sbi->ll_fsid);
 	}
 
-	if (data != NULL)
-		kfree(data);
-	if (osfs != NULL)
-		kfree(osfs);
+	kfree(data);
+	kfree(osfs);
 
 	return err;
 out_root:
@@ -596,10 +594,8 @@ out_md:
 	obd_disconnect(sbi->ll_md_exp);
 	sbi->ll_md_exp = NULL;
 out:
-	if (data != NULL)
-		kfree(data);
-	if (osfs != NULL)
-		kfree(osfs);
+	kfree(data);
+	kfree(osfs);
 	lprocfs_unregister_mountpoint(sbi);
 	return err;
 }
