7e18bc57529a39f190ba2b6db6bf473f981c0836
Julia Lawall <Julia.Lawall@lip6.fr>
2015-05-01 17:51:13 +0200

    staging: lustre: osc: Use kzalloc and kfree
    Replace OBD_ALLOC, OBD_ALLOC_WAIT, OBD_ALLOC_PTR, and OBD_ALLOC_PTR_WAIT by
    kalloc/kcalloc, and OBD_FREE and OBD_FREE_PTR by kfree.
    
    A simplified version of the semantic patch that makes these changes is as
    follows: (http://coccinelle.lip6.fr/)
    
    // <smpl>
    @@ expression ptr,size; @@
    - OBD_ALLOC(ptr,size)
    + ptr = kzalloc(size, GFP_NOFS)
    
    @@ expression ptr, size; @@
    - OBD_FREE(ptr, size);
    + kfree(ptr);
    // </smpl>
    
    Signed-off-by: Julia Lawall <Julia.Lawall@lip6.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/osc/osc_dev.c
+++ drivers/staging/lustre/lustre/osc/osc_dev.c
@@ -205,7 +205,7 @@ static struct lu_device *osc_device_free(const struct lu_env *env,
 	struct osc_device *od = lu2osc_dev(d);
 
 	cl_device_fini(lu2cl_dev(d));
-	OBD_FREE_PTR(od);
+	kfree(od);
 	return NULL;
 }
 
@@ -218,7 +218,7 @@ static struct lu_device *osc_device_alloc(const struct lu_env *env,
 	struct obd_device *obd;
 	int rc;
 
-	OBD_ALLOC_PTR(od);
+	od = kzalloc(sizeof(*od), GFP_NOFS);
 	if (od == NULL)
 		return ERR_PTR(-ENOMEM);
 
