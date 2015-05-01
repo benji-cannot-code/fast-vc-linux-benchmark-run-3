419920a42f4ae84564aa8e367a747371b802bcc3
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-05-01 14:59:51 -0700

    staging: comedi: ni_stc.h: tidy up Window_{Address,Data} defines
    Rename the CamelCase.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_stc.h
+++ drivers/staging/comedi/drivers/ni_stc.h
@@ -562,11 +562,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NISTC_AI_SI_SAVE_REG		64
 #define NISTC_AI_SC_SAVE_REG		66
 
-/* Additional windowed registers unique to E series */
-
-/* 16 bit registers shadowed from DAQ-STC */
-#define Window_Address			0x00
-#define Window_Data			0x02
+/*
+ * PCI E Series Registers
+ */
+#define NI_E_STC_WINDOW_ADDR_REG	0x00	/* rw16 */
+#define NI_E_STC_WINDOW_DATA_REG	0x02	/* rw16 */
 
 /* i/o port offsets */
 
