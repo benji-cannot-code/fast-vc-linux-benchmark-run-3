845653b757151c484ac57649abd943c1f2e5d460
Greg Kroah-Hartman <gregkh@google.com>
2015-05-01 21:04:47 +0200

    greybus: kernel_ver.h: fix leading space coding style issues
    When cutting and pasting some of the ATTR macros into kernel_ver.h, I
    dropped the tabs.  Fix this up and make checkpatch.pl happy.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/staging/greybus/kernel_ver.h
+++ drivers/staging/greybus/kernel_ver.h
@@ -19,14 +19,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ATTR_WO
 #define __ATTR_WO(_name) {						\
-        .attr   = { .name = __stringify(_name), .mode = S_IWUSR },      \
-        .store  = _name##_store,                                        \
+	.attr	= { .name = __stringify(_name), .mode = S_IWUSR },	\
+	.store	= _name##_store,					\
 }
 #endif
 
 #ifndef __ATTR_RW
 #define __ATTR_RW(_name) __ATTR(_name, (S_IWUSR | S_IRUGO),		\
-		                         _name##_show, _name##_store)
+				_name##_show, _name##_store)
 #endif
 
 #ifndef DEVICE_ATTR_RO
