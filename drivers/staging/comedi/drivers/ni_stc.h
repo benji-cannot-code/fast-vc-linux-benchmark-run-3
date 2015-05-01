75a3e31c1041dbe03f66ac182b5df7fe78791e99
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-05-01 14:59:47 -0700

    staging: comedi: ni_stc.h: tidy up DIO_Serial_Input_Register
    Rename the CamelCase.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_stc.h
+++ drivers/staging/comedi/drivers/ni_stc.h
@@ -554,10 +554,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NISTC_STATUS1_REG		27
 #define NISTC_STATUS1_SERIO_IN_PROG	BIT(12)
 
+#define NISTC_DIO_SERIAL_IN_REG		28
+
 #define AI_SI_Save_Registers		64
 #define AI_SC_Save_Registers		66
 
-#define DIO_Serial_Input_Register       28
 #define Joint_Status_2_Register         29
 enum Joint_Status_2_Bits {
 	AO_TMRDACWRs_In_Progress_St = 0x20,
