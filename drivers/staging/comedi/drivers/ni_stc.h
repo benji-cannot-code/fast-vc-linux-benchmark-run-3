853027285e471451a3d26470768918d3f74f1292
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-05-01 14:58:58 -0700

    staging: comedi: ni_stc.h: tidy up G_Command_Register
    Rename the CamelCase and define the G0 and G1 registers to add clarity
    to the mio_regmap tables.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_stc.h
+++ drivers/staging/comedi/drivers/ni_stc.h
@@ -127,6 +127,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NISTC_AO_CMD2_UPDATE2_PULSE	BIT(1)
 #define NISTC_AO_CMD2_START1_PULSE	BIT(0)
 
+#define NISTC_G0_CMD_REG		6
+#define NISTC_G1_CMD_REG		7
+
 #define AI_Status_1_Register		2
 #define Interrupt_A_St				0x8000
 #define AI_FIFO_Full_St				0x4000
@@ -600,7 +603,6 @@ static unsigned AO_UPDATE_Output_Select(enum ao_update_output_selection
 #define AI_External_Gate_Select(a)		((a) & 0x1f)
 
 #define G_Autoincrement_Register(a)	(68+(a))
-#define G_Command_Register(a)		(6+(a))
 #define G_HW_Save_Register(a)		(8+(a)*2)
 #define G_HW_Save_Register_High(a)	(8+(a)*2)
 #define G_HW_Save_Register_Low(a)	(9+(a)*2)
