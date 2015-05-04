81dfeeeab6406f71436032ce9d91d85062c2efb4
Thierry Reding <treding@nvidia.com>
2015-05-04 16:45:25 +0200

    soc/tegra: fuse: Add spare bit offset for Tegra210
    The offset of the first spare bit register on Tegra210 is 0x380, but
    account for the fixed offset of 0x100 in the fuse accessor.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/soc/tegra/fuse/fuse-tegra30.c
+++ drivers/soc/tegra/fuse/fuse-tegra30.c
@@ -150,6 +150,7 @@ const struct tegra_fuse_soc tegra124_fuse_soc = {
 static const struct tegra_fuse_info tegra210_fuse_info = {
 	.read = tegra30_fuse_read,
 	.size = 0x300,
+	.spare = 0x280,
 };
 
 const struct tegra_fuse_soc tegra210_fuse_soc = {
