1cdd988915aadd93f191600d8b8ca33bb680abfc
Antonio Ospite <ao2@ao2.it>
2015-05-04 11:13:04 +0200

    iio: accel: kxcjk-1013: add some blank lines for readability
    Some extra blank lines between if checks don't hurt and improve
    readability.
    
    Signed-off-by: Antonio Ospite <ao2@ao2.it>
    Cc: Bastien Nocera <hadess@hadess.net>
    Reviewed-by: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/accel/kxcjk-1013.c
+++ drivers/iio/accel/kxcjk-1013.c
@@ -1157,8 +1157,10 @@ static const char *kxcjk1013_match_acpi_device(struct device *dev,
 	id = acpi_match_device(dev->driver->acpi_match_table, dev);
 	if (!id)
 		return NULL;
+
 	if (strcmp(id->id, "SMO8500") == 0)
 		*is_smo8500_device = true;
+
 	*chipset = (enum kx_chipset)id->driver_data;
 
 	return dev_name(dev);
@@ -1173,6 +1175,7 @@ static int kxcjk1013_gpio_probe(struct i2c_client *client,
 
 	if (!client)
 		return -EINVAL;
+
 	if (data->is_smo8500_device)
 		return -ENOTSUPP;
 
