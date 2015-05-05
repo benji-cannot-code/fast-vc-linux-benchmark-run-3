88eceb8d6b5833b4ff630c2307da95fd006468e1
Prarit Bhargava <prarit@redhat.com>
2015-05-05 18:37:02 -0400

    staging: unisys: add acpi pnp driver
    According to Unisys, another OS detects the PNP0A07 as the auto load
    device.  We can also do this in the linux kernel by simply converting the
    driver over to the ACPI driver model.
    
    Notes: This changes the usage of __init and it had to be removed from some
    functions to avoid a !__init function calling an __init function.
    Additionally I also cleaned up the headers in visorchipset.c since I was
    adding a header file.
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorbus_private.h
+++ drivers/staging/unisys/visorbus/visorbus_private.h
@@ -156,6 +156,6 @@ bool visorchipset_get_device_info(u32 bus_no, u32 dev_no,
 bool visorchipset_set_bus_context(u32 bus_no, void *context);
 
 /* visorbus init and exit functions */
-int __init visorbus_init(void);
+int visorbus_init(void);
 void visorbus_exit(void);
 #endif
