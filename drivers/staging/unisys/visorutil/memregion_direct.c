2c946f48bd448979f33adeff0940baedd441b88f
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-05-05 18:36:53 -0400

    staging: unisys: memregion: {un, }mapit() are no longer used
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorutil/memregion_direct.c
+++ drivers/staging/unisys/visorutil/memregion_direct.c
@@ -26,36 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MYDRVNAME "memregion"
 
-static int mapit(struct memregion *memregion);
-static void unmapit(struct memregion *memregion);
-
-static int
-mapit(struct memregion *memregion)
-{
-	ulong physaddr = (ulong)(memregion->physaddr);
-	ulong nbytes = memregion->nbytes;
-
-	if (!request_mem_region(physaddr, nbytes, MYDRVNAME))
-		return -EBUSY;
-
-	memregion->mapped = ioremap_cache(physaddr, nbytes);
-	if (!memregion->mapped)
-		return -EFAULT;
-
-	return 0;
-}
-
-static void
-unmapit(struct memregion *memregion)
-{
-	if (memregion->mapped) {
-		iounmap(memregion->mapped);
-		memregion->mapped = NULL;
-		release_mem_region((unsigned long)memregion->physaddr,
-				   memregion->nbytes);
-	}
-}
-
 int
 visor_memregion_read(struct memregion *memregion, ulong offset, void *dest,
 		     ulong nbytes)
