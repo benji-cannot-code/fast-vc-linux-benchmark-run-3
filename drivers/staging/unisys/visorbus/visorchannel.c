87fe5be1d9976baf8c5df8cb9e89b45ed8772212
Prarit Bhargava <prarit@redhat.com>
2015-05-05 18:36:09 -0400

    staging: unisys: remove globals.h
    globals.h is only included in visorchannel.c and only contains 2 includes.
    These can be included directly in visorchannel.c.
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorchannel.c
+++ drivers/staging/unisys/visorbus/visorchannel.c
@@ -23,7 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  a CM2 implementation and a direct memory implementation.)
  */
 
-#include "globals.h"
+#include "memregion.h"
+#include "version.h"
 #include "visorbus.h"
 #include <linux/uuid.h>
 
