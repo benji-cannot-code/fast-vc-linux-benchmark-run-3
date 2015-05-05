6f516150dc94686297f52317fda851868190a0fc
Erik Arfvidson <erik.arfvidson@unisys.com>
2015-05-05 18:36:12 -0400

    staging: unisys: moving file.h functionality to visorchipset.h
    this patch simply migrates file.h functionality to visorchipset.h
    
    Signed-off-by: Erik Arfvidson <erik.arfvidson@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorchipset/file.c
+++ drivers/staging/unisys/visorchipset/file.c
@@ -20,13 +20,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * communicate with the visorchipset driver using a device/file interface.
  */
 
-#include <linux/mm.h>
 #include <linux/fs.h>
+#include <linux/mm.h>
+
 #include "version.h"
 #include "visorchipset.h"
 #include "visorbus.h"
 #include "uisutils.h"
-#include "file.h"
+#include "visorchipset.h"
 
 #define CURRENT_FILE_PC VISOR_CHIPSET_PC_file_c
 
--- drivers/staging/unisys/visorchipset/visorchipset_main.c
+++ drivers/staging/unisys/visorchipset/visorchipset_main.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "procobjecttree.h"
 #include "visorbus.h"
 #include "periodic_work.h"
-#include "file.h"
 #include "parser.h"
 #include "uisutils.h"
 #include "controlvmcompletionstatus.h"
