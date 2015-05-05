87665c19d994ed96e81c2e24240c97ec559583db
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-05-05 18:35:39 -0400

    staging: unisys: Move MYDRVNAME to visorchipset.h
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorchipset/globals.h
+++ drivers/staging/unisys/visorchipset/globals.h
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "visorchipset_umode.h"
 #include "version.h"
 
-#define MYDRVNAME "visorchipset"
-
 /* module parameters */
 
 #endif
--- drivers/staging/unisys/visorchipset/visorchipset.h
+++ drivers/staging/unisys/visorchipset/visorchipset.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vbusdeviceinfo.h"
 #include "vbushelper.h"
 
+#define MYDRVNAME "visorchipset"
+
 /** Describes the state from the perspective of which controlvm messages have
  *  been received for a bus or device.
  */
