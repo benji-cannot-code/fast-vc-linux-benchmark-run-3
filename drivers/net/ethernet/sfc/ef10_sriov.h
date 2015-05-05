faa40e2efd15249eee3c80a102d3da0e00034a2a
Shradha Shah <sshah@solarflare.com>
2015-05-06 00:58:31 +0100

    sfc: create vports for VFs and assign random MAC addresses
    The parent PF creates vports for all its child VFs and adds MAC
    addresses to these.  When the VF driver loads, it can make an MCDI
    call to get the MAC address that the parent PF assigned it.
    
    The parent PF also assigns a mac address to its own vport because
    implicit creation of a vAdaptor will only work on evb ports with
    MAC addresses assigned.
    
    The vport MAC address needs to be stored in the PF's nic_data
    struct as it can later be changed on the vadaptor (and its net_dev
    struct). When removing a vport the original MAC address must be
    deleted.
    
    A new flag is needed in the VF data structure to identify whether
    a vport has been assigned to the VF.  This is to determine whether
    it needs to be un-assigned before freeing the vport.  Also,
    attempting to un-assign a vport which is not assigned will result
    in an EALREADY error.
    
    Signed-off-by: Shradha Shah <sshah@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/ef10_sriov.h
+++ drivers/net/ethernet/sfc/ef10_sriov.h
@@ -13,6 +13,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "net_driver.h"
 
+/**
+ * struct ef10_vf - PF's store of VF data
+ * @vport_id: vport ID for the VF
+ * @vport_assigned: record whether the vport is currently assigned to the VF
+ * @mac: MAC address for the VF, zero when address is removed from the vport
+ */
+struct ef10_vf {
+	unsigned int vport_id;
+	unsigned int vport_assigned;
+	u8 mac[ETH_ALEN];
+};
+
 static inline bool efx_ef10_sriov_wanted(struct efx_nic *efx)
 {
 	return false;
