90bf8af1f05d4f9010bb7324cfebc90db2a4122a
Jes Sorensen <Jes.Sorensen@redhat.com>
2015-05-05 18:35:47 -0400

    staging: unisys: Remove unused cache object counter
    kmem_cache statistics are available through SLAB anyway
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorchipset/visorchipset_main.c
+++ drivers/staging/unisys/visorchipset/visorchipset_main.c
@@ -214,8 +214,6 @@ struct putfile_request {
 	int completion_status;
 };
 
-static atomic_t visorchipset_cache_buffers_in_use = ATOMIC_INIT(0);
-
 struct parahotplug_request {
 	struct list_head list;
 	int id;
@@ -2078,7 +2076,6 @@ visorchipset_cache_alloc(struct kmem_cache *pool, bool ok_to_block,
 	if (!p)
 		return NULL;
 
-	atomic_inc(&visorchipset_cache_buffers_in_use);
 	return p;
 }
 
@@ -2090,7 +2087,6 @@ visorchipset_cache_free(struct kmem_cache *pool, void *p, char *fn, int ln)
 	if (!p)
 		return;
 
-	atomic_dec(&visorchipset_cache_buffers_in_use);
 	kmem_cache_free(pool, p);
 }
 
