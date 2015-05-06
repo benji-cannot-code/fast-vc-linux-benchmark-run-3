5001f6a2070b1119689d7510002d94e9bb469aa7
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-05-06 14:14:22 +0000

    xen/arm: allow console=hvc0 to be omitted for guests
    From: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    
    This patch registers hvc0 as the preferred console if no console
    has been specified explicitly on the kernel command line.
    
    The purpose is to allow platform agnostic kernels and boot images
    (such as distro installers) to boot in a Xen/ARM domU without the
    need to modify the command line by hand.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Reviewed-by: Julien Grall <julien.grall@linaro.org>

--- arch/arm/xen/enlighten.c
+++ arch/arm/xen/enlighten.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpuidle.h>
 #include <linux/cpufreq.h>
 #include <linux/cpu.h>
+#include <linux/console.h>
 
 #include <linux/mm.h>
 
@@ -184,6 +185,9 @@ void __init xen_early_init(void)
 		xen_start_info->flags |= SIF_INITDOMAIN|SIF_PRIVILEGED;
 	else
 		xen_start_info->flags &= ~(SIF_INITDOMAIN|SIF_PRIVILEGED);
+
+	if (!console_set_on_cmdline && !xen_initial_domain())
+		add_preferred_console("hvc", 0, NULL);
 }
 
 static int __init xen_guest_init(void)
