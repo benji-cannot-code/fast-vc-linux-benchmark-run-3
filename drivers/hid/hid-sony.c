834f34a1db01985f52806b0517d32818eda3c976
Frank Praznik <frank.praznik@oh.rr.com>
2015-05-05 20:47:33 -0400

    HID: sony: Correct a typo in a HID descriptor comment and explain the odd Sixaxis axis mapping
    Correct a spelling mistake in the Sixaxis HID descriptor comment.
    
    Add an explanation as to why the Sixaxis has so many analog axes and why
    some of them are seen as multi-touch axes.
    
    Signed-off-by: Frank Praznik <frank.praznik@oh.rr.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-sony.c
+++ drivers/hid/hid-sony.c
@@ -60,9 +60,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_LEDS 4
 
+/*
+ * The Sixaxis reports both digital and analog values for each button on the
+ * controller except for Start, Select and the PS button.  The controller ends
+ * up reporting 27 axes which causes them to spill over into the multi-touch
+ * axis values.  Additionally, the controller only has 20 actual, physical axes
+ * so there are several unused axes in between the used ones.
+ */
 static __u8 sixaxis_rdesc[] = {
 	0x05, 0x01,         /*  Usage Page (Desktop),               */
-	0x09, 0x04,         /*  Usage (Joystik),                    */
+	0x09, 0x04,         /*  Usage (Joystick),                   */
 	0xA1, 0x01,         /*  Collection (Application),           */
 	0xA1, 0x02,         /*      Collection (Logical),           */
 	0x85, 0x01,         /*          Report ID (1),              */
