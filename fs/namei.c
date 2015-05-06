2d6986d73b2a229913562f74f5ebef88257e6d78
Al Viro <viro@zeniv.linux.org.uk>
2015-05-06 16:01:56 -0400

    namei: kill nd->link
    Just store it in nd->stack[nd->depth].link right in pick_link().
    Now that we make sure of stack expansion in pick_link(), we can
    do so...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namei.c
+++ fs/namei.c
@@ -496,10 +496,7 @@ EXPORT_SYMBOL(path_put);
 #define EMBEDDED_LEVELS 2
 struct nameidata {
 	struct path	path;
-	union {
-		struct qstr	last;
-		struct path	link;
-	};
+	struct qstr	last;
 	struct path	root;
 	struct inode	*inode; /* path.dentry.d_inode */
 	unsigned int	flags;
@@ -788,7 +785,7 @@ static inline int may_follow_link(struct nameidata *nd)
 		return 0;
 
 	/* Allowed if owner and follower match. */
-	inode = nd->link.dentry->d_inode;
+	inode = nd->stack[0].link.dentry->d_inode;
 	if (uid_eq(current_cred()->fsuid, inode->i_uid))
 		return 0;
 
@@ -801,8 +798,8 @@ static inline int may_follow_link(struct nameidata *nd)
 	if (uid_eq(parent->i_uid, inode->i_uid))
 		return 0;
 
-	audit_log_link_denied("follow_link", &nd->link);
-	path_put(&nd->link);
+	audit_log_link_denied("follow_link", &nd->stack[0].link);
+	path_put(&nd->stack[0].link);
 	path_put(&nd->path);
 	return -EACCES;
 }
@@ -880,14 +877,13 @@ static __always_inline
 const char *get_link(struct nameidata *nd)
 {
 	struct saved *last = nd->stack + nd->depth;
-	struct dentry *dentry = nd->link.dentry;
+	struct dentry *dentry = last->link.dentry;
 	struct inode *inode = dentry->d_inode;
 	int error;
 	const char *res;
 
 	BUG_ON(nd->flags & LOOKUP_RCU);
 
-	last->link = nd->link;
 	last->cookie = NULL;
 	nd->depth++;
 
@@ -1561,6 +1557,7 @@ static void terminate_walk(struct nameidata *nd)
 static int pick_link(struct nameidata *nd, struct path *link)
 {
 	int error;
+	struct saved *last;
 	if (unlikely(nd->total_link_count++ >= MAXSYMLINKS)) {
 		path_to_nameidata(link, nd);
 		return -ELOOP;
@@ -1579,7 +1576,8 @@ static int pick_link(struct nameidata *nd, struct path *link)
 		return error;
 	}
 
-	nd->link = *link;
+	last = nd->stack + nd->depth;
+	last->link = *link;
 	return 1;
 }
 
