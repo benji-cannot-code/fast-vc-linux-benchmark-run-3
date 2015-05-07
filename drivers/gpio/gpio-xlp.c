9a069900ed7b3d9aa875cd7853b020ccc63889ff
Axel Lin <axel.lin@ingics.com>
2015-05-07 15:34:37 +0800

    gpio: xlp: Add missing .owner and .label settings for gpio_chip
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-xlp.c
+++ drivers/gpio/gpio-xlp.c
@@ -367,6 +367,8 @@ static int xlp_gpio_probe(struct platform_device *pdev)
 
 	gc = &priv->chip;
 
+	gc->owner = THIS_MODULE;
+	gc->label = dev_name(&pdev->dev);
 	gc->base = 0;
 	gc->dev = &pdev->dev;
 	gc->ngpio = ngpio;
