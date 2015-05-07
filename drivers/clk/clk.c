dd05505d17e7695da393ddfa970eca936893bf34
Stephen Boyd <sboyd@codeaurora.org>
2015-05-06 17:00:54 -0700

    clk: Add some more lockdep assertions
    We don't check to make sure the enable_lock is held across
    enable/disable and we don't check if the prepare_lock is held
    across prepare/unprepare. Add some asserts to catch any future
    locking problems.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -544,6 +544,8 @@ EXPORT_SYMBOL_GPL(__clk_mux_determine_rate_closest);
 
 static void clk_core_unprepare(struct clk_core *core)
 {
+	lockdep_assert_held(&prepare_lock);
+
 	if (!core)
 		return;
 
@@ -589,6 +591,8 @@ EXPORT_SYMBOL_GPL(clk_unprepare);
 static int clk_core_prepare(struct clk_core *core)
 {
 	int ret = 0;
+
+	lockdep_assert_held(&prepare_lock);
 
 	if (!core)
 		return 0;
@@ -645,6 +649,8 @@ EXPORT_SYMBOL_GPL(clk_prepare);
 
 static void clk_core_disable(struct clk_core *core)
 {
+	lockdep_assert_held(&enable_lock);
+
 	if (!core)
 		return;
 
@@ -692,6 +698,8 @@ EXPORT_SYMBOL_GPL(clk_disable);
 static int clk_core_enable(struct clk_core *core)
 {
 	int ret = 0;
+
+	lockdep_assert_held(&enable_lock);
 
 	if (!core)
 		return 0;
