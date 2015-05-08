e5f31c9c07326622762b1cf9946b7c7b11e4a24e
Doug Anderson <dianders@chromium.org>
2015-05-07 21:27:44 -0700

    watchdog: dw_wdt: No need for a spinlock
    Right now the dw_wdt uses a spinlock to protect dw_wdt_open().  The
    problem is that while holding the spinlock we call:
    -> dw_wdt_set_top()
       -> dw_wdt_top_in_seconds()
          -> clk_get_rate()
             -> clk_prepare_lock()
                -> mutex_lock()
    
    Locking a mutex while holding a spinlock is not allowed and leads to
    warnings like "BUG: spinlock wrong CPU on CPU#1", among other
    problems.
    
    There's no reason to use a spinlock.  Only dw_wdt_open() was protected
    and the test_and_set_bit() at the start of that function protects us
    anyway.
    
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Jisheng Zhang <jszhang@marvell.com>
    Reviewed-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/dw_wdt.c
+++ drivers/watchdog/dw_wdt.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm.h>
 #include <linux/platform_device.h>
 #include <linux/reboot.h>
-#include <linux/spinlock.h>
 #include <linux/timer.h>
 #include <linux/uaccess.h>
 #include <linux/watchdog.h>
@@ -62,7 +61,6 @@ MODULE_PARM_DESC(nowayout, "Watchdog cannot be stopped once started "
 #define WDT_TIMEOUT		(HZ / 2)
 
 static struct {
-	spinlock_t		lock;
 	void __iomem		*regs;
 	struct clk		*clk;
 	unsigned long		in_use;
@@ -178,7 +176,6 @@ static int dw_wdt_open(struct inode *inode, struct file *filp)
 	/* Make sure we don't get unloaded. */
 	__module_get(THIS_MODULE);
 
-	spin_lock(&dw_wdt.lock);
 	if (!dw_wdt_is_enabled()) {
 		/*
 		 * The watchdog is not currently enabled. Set the timeout to
@@ -190,8 +187,6 @@ static int dw_wdt_open(struct inode *inode, struct file *filp)
 	}
 
 	dw_wdt_set_next_heartbeat();
-
-	spin_unlock(&dw_wdt.lock);
 
 	return nonseekable_open(inode, filp);
 }
@@ -348,8 +343,6 @@ static int dw_wdt_drv_probe(struct platform_device *pdev)
 	ret = clk_prepare_enable(dw_wdt.clk);
 	if (ret)
 		return ret;
-
-	spin_lock_init(&dw_wdt.lock);
 
 	ret = misc_register(&dw_wdt_miscdev);
 	if (ret)
