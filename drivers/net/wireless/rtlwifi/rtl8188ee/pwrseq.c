e3867ec9f376b6f5ac1c62a6532d61d5619679ef
Lauri Kenttä <lauri.kentta@gmail.com>
2015-05-08 22:01:53 +0300

    rtlwifi: rtl8188ee: Fix pwrseqcmd.h include path.
    Signed-off-by: Lauri Kenttä <lauri.kentta@gmail.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/rtlwifi/rtl8188ee/pwrseq.c
+++ drivers/net/wireless/rtlwifi/rtl8188ee/pwrseq.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  *****************************************************************************/
 
-#include "pwrseqcmd.h"
+#include "../pwrseqcmd.h"
 #include "pwrseq.h"
 
 /* drivers should parse below arrays and do the corresponding actions */
