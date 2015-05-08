5a41ec3a73e76ec87fe3ffe4102ee60761723d73
Eric Dumazet <edumazet@google.com>
2015-05-08 15:05:12 -0700

    codel: add ce_threshold attribute
    For DCTCP or similar ECN based deployments on fabrics with shallow
    buffers, hosts are responsible for a good part of the buffering.
    
    This patch adds an optional ce_threshold to codel & fq_codel qdiscs,
    so that DCTCP can have feedback from queuing in the host.
    
    A DCTCP enabled egress port simply have a queue occupancy threshold
    above which ECT packets get CE mark.
    
    In codel language this translates to a sojourn time, so that one doesn't
    have to worry about bytes or bandwidth but delays.
    
    This makes the host an active participant in the health of the whole
    network.
    
    This also helps experimenting DCTCP in a setup without DCTCP compliant
    fabric.
    
    On following example, ce_threshold is set to 1ms, and we can see from
    'ldelay xxx us' that TCP is not trying to go around the 5ms codel
    target.
    
    Queue has more capacity to absorb inelastic bursts (say from UDP
    traffic), as queues are maintained to an optimal level.
    
    lpaa23:~# ./tc -s -d qd sh dev eth1
    qdisc mq 1: dev eth1 root
     Sent 87910654696 bytes 58065331 pkt (dropped 0, overlimits 0 requeues 42961)
     backlog 3108242b 364p requeues 42961
    qdisc codel 8063: dev eth1 parent 1:1 limit 1000p target 5.0ms ce_threshold 1.0ms interval 100.0ms
     Sent 7363778701 bytes 4863809 pkt (dropped 0, overlimits 0 requeues 5503)
     rate 2348Mbit 193919pps backlog 255866b 46p requeues 5503
      count 0 lastcount 0 ldelay 1.0ms drop_next 0us
      maxpacket 68130 ecn_mark 0 drop_overlimit 0 ce_mark 72384
    qdisc codel 8064: dev eth1 parent 1:2 limit 1000p target 5.0ms ce_threshold 1.0ms interval 100.0ms
     Sent 7636486190 bytes 5043942 pkt (dropped 0, overlimits 0 requeues 5186)
     rate 2319Mbit 191538pps backlog 207418b 64p requeues 5186
      count 0 lastcount 0 ldelay 694us drop_next 0us
      maxpacket 68130 ecn_mark 0 drop_overlimit 0 ce_mark 69873
    qdisc codel 8065: dev eth1 parent 1:3 limit 1000p target 5.0ms ce_threshold 1.0ms interval 100.0ms
     Sent 11569360142 bytes 7641602 pkt (dropped 0, overlimits 0 requeues 5554)
     rate 3041Mbit 251096pps backlog 210446b 59p requeues 5554
      count 0 lastcount 0 ldelay 889us drop_next 0us
      maxpacket 68130 ecn_mark 0 drop_overlimit 0 ce_mark 37780
    ...
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Florian Westphal <fw@strlen.de>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Glenn Judd <glenn.judd@morganstanley.com>
    Cc: Nandita Dukkipati <nanditad@google.com>
    Cc: Neal Cardwell <ncardwell@google.com>
    Cc: Yuchung Cheng <ycheng@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/pkt_sched.h
+++ include/uapi/linux/pkt_sched.h
@@ -680,6 +680,7 @@ enum {
 	TCA_CODEL_LIMIT,
 	TCA_CODEL_INTERVAL,
 	TCA_CODEL_ECN,
+	TCA_CODEL_CE_THRESHOLD,
 	__TCA_CODEL_MAX
 };
 
@@ -696,6 +697,7 @@ struct tc_codel_xstats {
 	__u32	drop_overlimit; /* number of time max qdisc packet limit was hit */
 	__u32	ecn_mark;  /* number of packets we ECN marked instead of dropped */
 	__u32	dropping;  /* are we in dropping state ? */
+	__u32	ce_mark;   /* number of CE marked packets because of ce_threshold */
 };
 
 /* FQ_CODEL */
@@ -708,6 +710,7 @@ enum {
 	TCA_FQ_CODEL_ECN,
 	TCA_FQ_CODEL_FLOWS,
 	TCA_FQ_CODEL_QUANTUM,
+	TCA_FQ_CODEL_CE_THRESHOLD,
 	__TCA_FQ_CODEL_MAX
 };
 
@@ -731,6 +734,7 @@ struct tc_fq_codel_qd_stats {
 				 */
 	__u32	new_flows_len;	/* count of flows in new list */
 	__u32	old_flows_len;	/* count of flows in old list */
+	__u32	ce_mark;	/* packets above ce_threshold */
 };
 
 struct tc_fq_codel_cl_stats {
