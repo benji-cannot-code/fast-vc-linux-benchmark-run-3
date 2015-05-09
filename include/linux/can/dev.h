47ad9c59abf7b843434eaf4a59e1547eaf611263
Marc Kleine-Budde <mkl@pengutronix.de>
2015-05-09 17:47:52 +0200

    can: headers: make header files self contained
    This patch adds the missing #include-s to the dev.h and led.h, so that they can
    be used without including further header files.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- include/linux/can/dev.h
+++ include/linux/can/dev.h
@@ -15,9 +15,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _CAN_DEV_H
 
 #include <linux/can.h>
-#include <linux/can/netlink.h>
 #include <linux/can/error.h>
 #include <linux/can/led.h>
+#include <linux/can/netlink.h>
+#include <linux/netdevice.h>
 
 /*
  * CAN mode
