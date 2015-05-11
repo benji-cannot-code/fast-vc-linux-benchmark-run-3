12aca41c9543cbdd88a21f2fce888dae55ea6e4b
Herbert Xu <herbert@gondor.apana.org.au>
2015-05-11 17:47:44 +0800

    crypto: ixp4xx - Include internal/aead.h
    All AEAD implementations must include internal/aead.h in order
    to access required helpers.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ixp4xx_crypto.c
+++ drivers/crypto/ixp4xx_crypto.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/aes.h>
 #include <crypto/sha.h>
 #include <crypto/algapi.h>
-#include <crypto/aead.h>
+#include <crypto/internal/aead.h>
 #include <crypto/authenc.h>
 #include <crypto/scatterwalk.h>
 
