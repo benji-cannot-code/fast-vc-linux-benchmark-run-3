b43a5128b2d289e0219ea9d570f69dd488704d06
Richard Weinberger <richard@nod.at>
2015-05-12 00:09:52 +0200

    um: Add asm/elf.h to vma.c
    um_vdso_addr is defined in asm/elf.h.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/x86/um/vdso/vma.c
+++ arch/x86/um/vdso/vma.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/mm.h>
 #include <asm/page.h>
+#include <asm/elf.h>
 #include <linux/init.h>
 
 static unsigned int __read_mostly vdso_enabled = 1;
