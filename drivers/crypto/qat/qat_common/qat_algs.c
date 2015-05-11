88d85df544fac2bd77c9d52df881d9391bd106cf
Herbert Xu <herbert@gondor.apana.org.au>
2015-05-11 17:47:46 +0800

    crypto: qat - Include internal/aead.h
    All AEAD implementations must include internal/aead.h in order
    to access required helpers.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_common/qat_algs.c
+++ drivers/crypto/qat/qat_common/qat_algs.c
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/crypto.h>
-#include <crypto/aead.h>
+#include <crypto/internal/aead.h>
 #include <crypto/aes.h>
 #include <crypto/sha.h>
 #include <crypto/hash.h>
