be59065449ad018e849a8606e5a4f90bb70588ae
Johnny Kim <johnny.kim@atmel.com>
2015-05-11 14:30:56 +0900

    staging: wilc1000: Add SDIO/SPI 802.11 driver
    This driver is for the wilc1000 which is a single chip IEEE 802.11
    b/g/n device.
    The driver works together with cfg80211, which is the kernel side of
    configuration management for wireless devices because the wilc1000
    chipset is fullmac where the MLME is managed in hardware.
    
    The driver worked from kernel version 2.6.38 and being now ported
    to several others since then.
    A TODO file is included as well in this commit.
    
    Signed-off-by: Johnny Kim <johnny.kim@atmel.com>
    Signed-off-by: Rachel Kim <rachel.kim@atmel.com>
    Signed-off-by: Dean Lee <dean.lee@atmel.com>
    Signed-off-by: Chris Park <chris.park@atmel.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

---
+++ drivers/staging/wilc1000/coreconfigsimulator.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+
+/*!
+ *  @file	coreconfigsimulator.h
+ *  @brief
+ *  @author
+ *  @sa		coreconfigsimulator.c
+ *  @date	1 Mar 2012
+ *  @version	1.0
+ */
+
+
+#ifndef CORECONFIGSIMULATOR_H
+#define CORECONFIGSIMULATOR_H
+
+
+extern WILC_Sint32 CoreConfigSimulatorInit (void);
+extern WILC_Sint32 CoreConfigSimulatorDeInit (void);
+
+
+#endif
---
+++ drivers/staging/wilc1000/fifo_buffer.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+
+#include "wilc_oswrapper.h"
+
+
+#define tHANDLE	void *
+
+typedef struct {
+	WILC_Uint8		*pu8Buffer;
+	WILC_Uint32 u32BufferLength;
+	WILC_Uint32 u32WriteOffset;
+	WILC_Uint32 u32ReadOffset;
+	WILC_Uint32 u32TotalBytes;
+	WILC_SemaphoreHandle SemBuffer;
+} tstrFifoHandler;
+
+
+extern WILC_Uint32 FIFO_InitBuffer(tHANDLE *hBuffer,
+								   WILC_Uint32 u32BufferLength);
+extern WILC_Uint32 FIFO_DeInit(tHANDLE hFifo);
+extern WILC_Uint32 FIFO_ReadBytes(tHANDLE hFifo, WILC_Uint8 *pu8Buffer,
+				WILC_Uint32 u32BytesToRead, WILC_Uint32 *pu32BytesRead);
+extern WILC_Uint32 FIFO_WriteBytes(tHANDLE hFifo, WILC_Uint8 *pu8Buffer,
+				WILC_Uint32 u32BytesToWrite, WILC_Bool bForceOverWrite);
