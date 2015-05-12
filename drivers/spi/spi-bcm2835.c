156c68d08e6f8227b7eb558ffc9e9c9b1b27fbed
kbuild test robot <fengguang.wu@intel.com>
2015-05-12 19:43:59 +0800

    spi: bcm2835: bcm2835_dma_release() can be static
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-bcm2835.c
+++ drivers/spi/spi-bcm2835.c
@@ -402,7 +402,7 @@ static bool bcm2835_spi_can_dma(struct spi_master *master,
 	return true;
 }
 
-void bcm2835_dma_release(struct spi_master *master)
+static void bcm2835_dma_release(struct spi_master *master)
 {
 	if (master->dma_tx) {
 		dmaengine_terminate_all(master->dma_tx);
@@ -416,7 +416,7 @@ void bcm2835_dma_release(struct spi_master *master)
 	}
 }
 
-void bcm2835_dma_init(struct spi_master *master, struct device *dev)
+static void bcm2835_dma_init(struct spi_master *master, struct device *dev)
 {
 	struct dma_slave_config slave_config;
 	const __be32 *addr;
