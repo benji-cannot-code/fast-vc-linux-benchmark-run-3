0ef8fa91404cadb4e2764c65e1ff07a16740b274
Dinh Nguyen <dinguyen@opensource.altera.com>
2015-05-12 16:49:21 -0500

    ARM: socfpga: use of_iomap to map the SCU
    Use of_iomap to map the "arm,cortex-a9-scu". By doing this, we can remove
    map_io in socfpga.c.
    
    Also, we can remove socfpga_smp_init_cpus, as arm_dt_init_cpu_maps is
    already doing the CPU mapping.
    
    Signed-off-by: Dinh Nguyen <dinguyen@opensource.altera.com>

--- arch/arm/mach-socfpga/socfpga.c
+++ arch/arm/mach-socfpga/socfpga.c
@@ -28,33 +28,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "core.h"
 
-void __iomem *socfpga_scu_base_addr = ((void __iomem *)(SOCFPGA_SCU_VIRT_BASE));
 void __iomem *sys_manager_base_addr;
 void __iomem *rst_manager_base_addr;
 unsigned long socfpga_cpu1start_addr;
-
-static struct map_desc scu_io_desc __initdata = {
-	.virtual	= SOCFPGA_SCU_VIRT_BASE,
-	.pfn		= 0, /* run-time */
-	.length		= SZ_8K,
-	.type		= MT_DEVICE,
-};
-
-static void __init socfpga_scu_map_io(void)
-{
-	unsigned long base;
-
-	/* Get SCU base */
-	asm("mrc p15, 4, %0, c15, c0, 0" : "=r" (base));
-
-	scu_io_desc.pfn = __phys_to_pfn(base);
-	iotable_init(&scu_io_desc, 1);
-}
-
-static void __init socfpga_map_io(void)
-{
-	socfpga_scu_map_io();
-}
 
 void __init socfpga_sysmgr_init(void)
 {
@@ -104,7 +80,6 @@ DT_MACHINE_START(SOCFPGA, "Altera SOCFPGA")
 	.l2c_aux_val	= 0,
 	.l2c_aux_mask	= ~0,
 	.smp		= smp_ops(socfpga_smp_ops),
-	.map_io		= socfpga_map_io,
 	.init_irq	= socfpga_init_irq,
 	.restart	= socfpga_cyclone5_restart,
 	.dt_compat	= altera_dt_match,
