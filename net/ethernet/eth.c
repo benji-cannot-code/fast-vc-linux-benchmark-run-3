ba659bef87013b27cb6e5ec594210f7a16ca4c76
Jiri Pirko <jiri@resnulli.us>
2015-05-12 14:56:09 +0200

    net: move *skb_get_poff declarations into correct header
    Since these functions are defined in flow_dissector.c, move header
    declarations from skbuff.h into flow_dissector.h
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ethernet/eth.c
+++ net/ethernet/eth.c
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ipv6.h>
 #include <net/ip.h>
 #include <net/dsa.h>
+#include <net/flow_dissector.h>
 #include <linux/uaccess.h>
 
 __setup("ether=", netdev_boot_setup);
