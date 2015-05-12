cda945a6331637ef54b4891d066b3979b903ead5
Damien Lespiau <damien.lespiau@intel.com>
2015-05-12 16:13:18 +0100

    drm/i915: Don't use the intel_ prefix for gen-specific data
    We now prefix our functions/enums/data with the first platform it has
    been introduced. Do that for the primary plane formats.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    [danvet: s/gen2/i8xx/ and s/gen4/i965/ ...]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -54,14 +54,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	DRM_FORMAT_ARGB8888
 
 /* Primary plane formats for gen <= 3 */
-static const uint32_t intel_primary_formats_gen2[] = {
+static const uint32_t i8xx_primary_formats[] = {
 	COMMON_PRIMARY_FORMATS,
 	DRM_FORMAT_XRGB1555,
 	DRM_FORMAT_ARGB1555,
 };
 
 /* Primary plane formats for gen >= 4 */
-static const uint32_t intel_primary_formats_gen4[] = {
+static const uint32_t i965_primary_formats[] = {
 	COMMON_PRIMARY_FORMATS, \
 	DRM_FORMAT_XBGR8888,
 	DRM_FORMAT_ABGR8888,
@@ -13289,11 +13289,11 @@ static struct drm_plane *intel_primary_plane_create(struct drm_device *dev,
 		primary->plane = !pipe;
 
 	if (INTEL_INFO(dev)->gen <= 3) {
-		intel_primary_formats = intel_primary_formats_gen2;
-		num_formats = ARRAY_SIZE(intel_primary_formats_gen2);
+		intel_primary_formats = i8xx_primary_formats;
+		num_formats = ARRAY_SIZE(i8xx_primary_formats);
 	} else {
-		intel_primary_formats = intel_primary_formats_gen4;
-		num_formats = ARRAY_SIZE(intel_primary_formats_gen4);
+		intel_primary_formats = i965_primary_formats;
+		num_formats = ARRAY_SIZE(i965_primary_formats);
 	}
 
 	drm_universal_plane_init(dev, &primary->base, 0,
