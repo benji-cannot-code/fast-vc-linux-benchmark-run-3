58a0f73d6a0f3bd59dbe06c76f93b079092e3a4b
David Kershner <david.kershner@unisys.com>
2015-05-13 13:21:59 -0400

    staging: unisys: Remove appos_subsystems.h
    Get rid of common-spar/include/diagnostics/appos_subsystems.h.
    
    No one is using it.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/uisutils.h
+++ drivers/staging/unisys/include/uisutils.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "channel.h"
 #include "uisthread.h"
 #include "uisqueue.h"
-#include "diagnostics/appos_subsystems.h"
 #include "vbusdeviceinfo.h"
 #include <linux/atomic.h>
 
