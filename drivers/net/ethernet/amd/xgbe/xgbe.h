16c27fadfa72c6ca91f46252b4bd0b038e032090
Lendacky, Thomas <Thomas.Lendacky@amd.com>
2015-05-14 11:44:21 -0500

    amd-xgbe: Support defining PHY resources in ETH device node
    Simplify the device tree support of the amd-xgbe driver by defining
    the PHY-related resources within the ethernet device node. The support
    provides backwards compatibility with the original way.
    
    Update the driver version to 1.0.2.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/amd/xgbe/xgbe.h
+++ drivers/net/ethernet/amd/xgbe/xgbe.h
@@ -130,7 +130,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/dcbnl.h>
 
 #define XGBE_DRV_NAME		"amd-xgbe"
-#define XGBE_DRV_VERSION	"1.0.1"
+#define XGBE_DRV_VERSION	"1.0.2"
 #define XGBE_DRV_DESC		"AMD 10 Gigabit Ethernet Driver"
 
 /* Descriptor related defines */
