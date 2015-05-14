5f640d4f7f69c559fb95acc86ee36c600013d8a7
Ira Weiny <ira.weiny@intel.com>
2015-05-13 20:02:56 -0400

    IB/user_mad: Use new start/end port functions
    Use the new common rdma_[start|end]_port functions instead of using
    local variables and figuring it out on the fly.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/core/user_mad.c
+++ drivers/infiniband/core/user_mad.c
@@ -100,7 +100,6 @@ struct ib_umad_port {
 };
 
 struct ib_umad_device {
-	int                  start_port, end_port;
 	struct kobject       kobj;
 	struct ib_umad_port  port[0];
 };
@@ -1276,12 +1275,8 @@ static void ib_umad_add_one(struct ib_device *device)
 	int s, e, i;
 	int count = 0;
 
-	if (device->node_type == RDMA_NODE_IB_SWITCH)
-		s = e = 0;
-	else {
-		s = 1;
-		e = device->phys_port_cnt;
-	}
+	s = rdma_start_port(device);
+	e = rdma_end_port(device);
 
 	umad_dev = kzalloc(sizeof *umad_dev +
 			   (e - s + 1) * sizeof (struct ib_umad_port),
@@ -1290,9 +1285,6 @@ static void ib_umad_add_one(struct ib_device *device)
 		return;
 
 	kobject_init(&umad_dev->kobj, &ib_umad_dev_ktype);
-
-	umad_dev->start_port = s;
-	umad_dev->end_port   = e;
 
 	for (i = s; i <= e; ++i) {
 		if (!rdma_cap_ib_mad(device, i))
@@ -1333,7 +1325,7 @@ static void ib_umad_remove_one(struct ib_device *device)
 	if (!umad_dev)
 		return;
 
-	for (i = 0; i <= umad_dev->end_port - umad_dev->start_port; ++i) {
+	for (i = 0; i <= rdma_end_port(device) - rdma_start_port(device); ++i) {
 		if (rdma_cap_ib_mad(device, i))
 			ib_umad_kill_port(&umad_dev->port[i]);
 	}
