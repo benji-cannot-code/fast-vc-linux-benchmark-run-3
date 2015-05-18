5e88aca2b345dc518a088f00abcfe38270fe7529
Tony Lindgren <tony@atomide.com>
2015-05-18 15:40:29 -0700

    PM / Wakeirq: Add automated device wake IRQ handling
    Turns out we can automate the handling for the device_may_wakeup()
    quite a bit by using the kernel wakeup source list as suggested
    by Rafael J. Wysocki <rjw@rjwysocki.net>.
    
    And as some hardware has separate dedicated wake-up interrupt
    in addition to the IO interrupt, we can automate the handling by
    adding a generic threaded interrupt handler that just calls the
    device PM runtime to wake up the device.
    
    This allows dropping code from device drivers as we currently
    are doing it in multiple ways, and often wrong.
    
    For most drivers, we should be able to drop the following
    boilerplate code from runtime_suspend and runtime_resume
    functions:
    
    	...
    	device_init_wakeup(dev, true);
    	...
    	if (device_may_wakeup(dev))
    		enable_irq_wake(irq);
    	...
    	if (device_may_wakeup(dev))
    		disable_irq_wake(irq);
    	...
    	device_init_wakeup(dev, false);
    	...
    
    We can replace it with just the following init and exit
    time code:
    
    	...
    	device_init_wakeup(dev, true);
    	dev_pm_set_wake_irq(dev, irq);
    	...
    	dev_pm_clear_wake_irq(dev);
    	device_init_wakeup(dev, false);
    	...
    
    And for hardware with dedicated wake-up interrupts:
    
    	...
    	device_init_wakeup(dev, true);
    	dev_pm_set_dedicated_wake_irq(dev, irq);
    	...
    	dev_pm_clear_wake_irq(dev);
    	device_init_wakeup(dev, false);
    	...
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/runtime.c
+++ drivers/base/power/runtime.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/export.h>
 #include <linux/pm_runtime.h>
+#include <linux/pm_wakeirq.h>
 #include <trace/events/rpm.h>
 #include "power.h"
 
@@ -515,6 +516,7 @@ static int rpm_suspend(struct device *dev, int rpmflags)
 
 	callback = RPM_GET_CALLBACK(dev, runtime_suspend);
 
+	dev_pm_enable_wake_irq(dev);
 	retval = rpm_callback(callback, dev);
 	if (retval)
 		goto fail;
@@ -553,6 +555,7 @@ static int rpm_suspend(struct device *dev, int rpmflags)
 	return retval;
 
  fail:
+	dev_pm_disable_wake_irq(dev);
 	__update_runtime_status(dev, RPM_ACTIVE);
 	dev->power.deferred_resume = false;
 	wake_up_all(&dev->power.wait_queue);
@@ -735,10 +738,12 @@ static int rpm_resume(struct device *dev, int rpmflags)
 
 	callback = RPM_GET_CALLBACK(dev, runtime_resume);
 
+	dev_pm_disable_wake_irq(dev);
 	retval = rpm_callback(callback, dev);
 	if (retval) {
 		__update_runtime_status(dev, RPM_SUSPENDED);
 		pm_runtime_cancel_pending(dev);
+		dev_pm_enable_wake_irq(dev);
 	} else {
  no_callback:
 		__update_runtime_status(dev, RPM_ACTIVE);
