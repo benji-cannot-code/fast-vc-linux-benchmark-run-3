eabbf9c44abfec58841de4a8b8542042dede9c90
LEROY Christophe <christophe.leroy@c-s.fr>
2015-05-19 17:18:55 +0200

    powerpc: put csum_tcpudp_magic inline
    csum_tcpudp_magic() is only a few instructions, and does modify
    really few registers. So it is not worth having it as a separate
    function and suffer function branching and saving of volatile
    registers.
    
    This patch makes it inline by use of the already existing
    csum_tcpudp_nofold() function.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Scott Wood <scottwood@freescale.com>

--- arch/powerpc/include/asm/checksum.h
+++ arch/powerpc/include/asm/checksum.h
@@ -21,15 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern __sum16 ip_fast_csum(const void *iph, unsigned int ihl);
 
 /*
- * computes the checksum of the TCP/UDP pseudo-header
- * returns a 16-bit checksum, already complemented
- */
-extern __sum16 csum_tcpudp_magic(__be32 saddr, __be32 daddr,
-					unsigned short len,
-					unsigned short proto,
-					__wsum sum);
-
-/*
  * computes the checksum of a memory block at buff, length len,
  * and adds in "sum" (32-bit)
  *
@@ -126,6 +117,18 @@ static inline __wsum csum_tcpudp_nofold(__be32 saddr, __be32 daddr,
 	: "r" (daddr), "r"(saddr), "r"(proto + len), "0"(sum));
 	return sum;
 #endif
+}
+
+/*
+ * computes the checksum of the TCP/UDP pseudo-header
+ * returns a 16-bit checksum, already complemented
+ */
+static inline __sum16 csum_tcpudp_magic(__be32 saddr, __be32 daddr,
+					unsigned short len,
+					unsigned short proto,
+					__wsum sum)
+{
+	return csum_fold(csum_tcpudp_nofold(saddr, daddr, len, proto, sum));
 }
 
 #endif
