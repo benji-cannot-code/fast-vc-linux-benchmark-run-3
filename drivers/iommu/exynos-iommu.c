67f0501c04e6befa908a75b354eb7d7a3cd2ca92
Marek Szyprowski <m.szyprowski@samsung.com>
2015-05-19 15:20:32 +0200

    iommu/exynos: Add/remove callbacks should fail if no iommu is available
    Return fail if given master device passed to add_device/remove_device
    callbacks doesn't has associated any sysmmu controller.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Javier Martinez Canillas <javier.martinez@collabora.co.uk>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/exynos-iommu.c
+++ drivers/iommu/exynos-iommu.c
@@ -1083,6 +1083,9 @@ static int exynos_iommu_add_device(struct device *dev)
 	struct iommu_group *group;
 	int ret;
 
+	if (!has_sysmmu(dev))
+		return -ENODEV;
+
 	group = iommu_group_get(dev);
 
 	if (!group) {
@@ -1101,6 +1104,9 @@ static int exynos_iommu_add_device(struct device *dev)
 
 static void exynos_iommu_remove_device(struct device *dev)
 {
+	if (!has_sysmmu(dev))
+		return;
+
 	iommu_group_remove_device(dev);
 }
 
