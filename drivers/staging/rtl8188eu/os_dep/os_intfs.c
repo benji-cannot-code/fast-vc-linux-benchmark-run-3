68cb4bcd3b1400648f4c2ec8b1b5e83e9310c039
Luca Ceresoli <luca@lucaceresoli.net>
2015-05-19 11:35:24 +0200

    staging: rtl8188eu: remove useless return value
    The loadparam() function cannot fail, it's called only once and its return
    value is ignored there.
    
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Cc: Greg Kroah-Hartman <gregkh@linux.com>
    Cc: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/os_intfs.c
+++ drivers/staging/rtl8188eu/os_dep/os_intfs.c
@@ -532,7 +532,7 @@ void rtw_proc_remove_one(struct net_device *dev)
 }
 #endif
 
-static uint loadparam(struct adapter *padapter, struct net_device *pnetdev)
+static void loadparam(struct adapter *padapter, struct net_device *pnetdev)
 {
 	struct registry_priv  *registry_par = &padapter->registrypriv;
 
@@ -602,7 +602,6 @@ static uint loadparam(struct adapter *padapter, struct net_device *pnetdev)
 	snprintf(registry_par->ifname, 16, "%s", ifname);
 	snprintf(registry_par->if2name, 16, "%s", if2name);
 	registry_par->notch_filter = (u8)rtw_notch_filter;
-	return _SUCCESS;
 }
 
 static int rtw_net_set_mac_address(struct net_device *pnetdev, void *p)
