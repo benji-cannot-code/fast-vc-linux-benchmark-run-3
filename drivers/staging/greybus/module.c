22d5da08bb08bce675a96c87457f543302006236
Viresh Kumar <viresh.kumar@linaro.org>
2015-05-20 16:48:04 +0530

    greybus: module: name routines consistently
    Routines should be named this way: gb_<object>_<operation>. Fix all
    routines that don't match this.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/module.c
+++ drivers/staging/greybus/module.c
@@ -72,7 +72,7 @@ static struct attribute *module_attrs[] = {
 };
 ATTRIBUTE_GROUPS(module);
 
-static void greybus_module_release(struct device *dev)
+static void gb_module_release(struct device *dev)
 {
 	struct gb_module *module = to_gb_module(dev);
 
@@ -81,7 +81,7 @@ static void greybus_module_release(struct device *dev)
 
 struct device_type greybus_module_type = {
 	.name =		"greybus_module",
-	.release =	greybus_module_release,
+	.release =	gb_module_release,
 };
 
 struct module_find {
