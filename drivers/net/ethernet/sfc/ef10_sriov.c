926f218cd98cc31ee248250f81acd283c52fe4f3
Shradha Shah <sshah@solarflare.com>
2015-05-20 11:12:30 +0100

    sfc: Implement dummy disable of VF spoof check for EF10
    Signed-off-by: Shradha Shah <sshah@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/ef10_sriov.c
+++ drivers/net/ethernet/sfc/ef10_sriov.c
@@ -668,6 +668,12 @@ reset_nic:
 	return rc ? rc : rc2;
 }
 
+int efx_ef10_sriov_set_vf_spoofchk(struct efx_nic *efx, int vf_i,
+				   bool spoofchk)
+{
+	return spoofchk ? -EOPNOTSUPP : 0;
+}
+
 int efx_ef10_sriov_set_vf_link_state(struct efx_nic *efx, int vf_i,
 				     int link_state)
 {
