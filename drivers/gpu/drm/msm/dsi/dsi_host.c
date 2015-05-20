de1228751104f6e3edbd63056df1746791eeee4c
Brian Norris <computersforpeace@gmail.com>
2015-05-20 15:59:31 -0700

    drm/msm: dsi: fix compile errors when CONFIG_GPIOLIB=n
    I'm not sure where, exactly, but somewhere in here we must be relying on
    an implicit include.
    
    drivers/gpu/drm/msm/dsi/dsi_host.c: In function ‘dsi_host_init_panel_gpios’:
    drivers/gpu/drm/msm/dsi/dsi_host.c:1356:2: error: implicit declaration of function ‘devm_gpiod_get’ [-Werror=implicit-function-declaration]
      msm_host->disp_en_gpio = devm_gpiod_get(panel_device,
      ^
    drivers/gpu/drm/msm/dsi/dsi_host.c:1356:25: warning: assignment makes pointer from integer without a cast [enabled by default]
      msm_host->disp_en_gpio = devm_gpiod_get(panel_device,
                             ^
    drivers/gpu/drm/msm/dsi/dsi_host.c:1364:3: error: implicit declaration of function ‘gpiod_direction_output’ [-Werror=implicit-function-declaration]
       ret = gpiod_direction_output(msm_host->disp_en_gpio, 0);
       ^
    drivers/gpu/drm/msm/dsi/dsi_host.c:1371:20: warning: assignment makes pointer from integer without a cast [enabled by default]
      msm_host->te_gpio = devm_gpiod_get(panel_device, "disp-te");
                        ^
    drivers/gpu/drm/msm/dsi/dsi_host.c:1378:3: error: implicit declaration of function ‘gpiod_direction_input’ [-Werror=implicit-function-declaration]
       ret = gpiod_direction_input(msm_host->te_gpio);
       ^
    drivers/gpu/drm/msm/dsi/dsi_host.c: In function ‘msm_dsi_host_power_on’:
    drivers/gpu/drm/msm/dsi/dsi_host.c:1918:3: error: implicit declaration of function ‘gpiod_set_value’ [-Werror=implicit-function-declaration]
       gpiod_set_value(msm_host->disp_en_gpio, 1);
       ^
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Rob Clark <robdclark@gmail.com>
    Cc: Hai Li <hali@codeaurora.org>
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/dsi/dsi_host.c
+++ drivers/gpu/drm/msm/dsi/dsi_host.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
