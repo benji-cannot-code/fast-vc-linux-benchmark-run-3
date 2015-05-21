1605b2211a7d7e093aab5c4e0b932ebdd1103b6e
Tony Lindgren <tony@atomide.com>
2015-05-21 14:50:23 -0700

    ARM: OMAP1: Fix randconfig builds if ARCH_OMAP15XX not selected
    With the omap1 SPARSE_IRQ changes mach/irqs.h is no longer
    automatically included. Turns out now we rely on ARCH_OMAP15XX
    including hardware.h from memory.h, so without ARCH_OMAP15XX
    we get build failures.
    
    As we have legacy drivers still relying on these indirect
    includes, let's not add more mach includes to the drivers.
    Those have to be removed anyways for multiplatform support.
    
    Let's fix up mach-omap1 to include soc.h where cpu_is_omap
    checks are done, and common.h for board-*.c files.
    
    But let's keep the indirect memory.h include for now to avoid
    unnecessary churn in the drivers.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/common.h
+++ arch/arm/mach-omap1/common.h
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mach/irqs.h>
 
+#include "soc.h"
+
 #if defined(CONFIG_ARCH_OMAP730) || defined(CONFIG_ARCH_OMAP850)
 void omap7xx_map_io(void);
 #else
