8430101e63d7a2505cd6425243d3ea296011ec07
Alex Deucher <alexander.deucher@amd.com>
2015-05-21 02:24:48 -0400

    drm/amdgpu: remove unused TRACE_SYSTEM_STRING define
    Port of 77cb2fea1e5fc4b083dd967f231bbf6edd96150e to
    amdgpu.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_trace.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_trace.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM amdgpu
-#define TRACE_SYSTEM_STRING __stringify(TRACE_SYSTEM)
 #define TRACE_INCLUDE_FILE amdgpu_trace
 
 TRACE_EVENT(amdgpu_bo_create,
