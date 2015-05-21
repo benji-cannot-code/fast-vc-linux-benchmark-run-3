d1cbf40562ae935cf869de07cf0dd84014547e6e
Dmitry Eremin <dmitry4ever@gmail.com>
2015-05-21 15:32:11 -0400

    staging/lustre/ldlm: move all remaining files from procfs to debugfs
    Move all files except stats. It will be moved later after change
    type of obddev->obd_proc_entry member.
    
    Signed-off-by: Dmitry Eremin <dmiter4ever@gmail.com>
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ldlm/ldlm_lockd.c
+++ drivers/staging/lustre/lustre/ldlm/ldlm_lockd.c
@@ -1079,7 +1079,7 @@ static int ldlm_setup(void)
 		goto out;
 	}
 
-	rc = ldlm_proc_setup();
+	rc = ldlm_debugfs_setup();
 	if (rc != 0)
 		goto out;
 
@@ -1114,7 +1114,7 @@ static int ldlm_setup(void)
 	};
 	ldlm_state->ldlm_cb_service =
 			ptlrpc_register_service(&conf, ldlm_svc_kset,
-						ldlm_svc_proc_dir);
+						ldlm_svc_debugfs_dir);
 	if (IS_ERR(ldlm_state->ldlm_cb_service)) {
 		CERROR("failed to start service\n");
 		rc = PTR_ERR(ldlm_state->ldlm_cb_service);
@@ -1205,8 +1205,7 @@ static int ldlm_cleanup(void)
 	if (ldlm_kobj)
 		kobject_put(ldlm_kobj);
 
-	ldlm_proc_cleanup();
-
+	ldlm_debugfs_cleanup();
 
 	kfree(ldlm_state);
 	ldlm_state = NULL;
