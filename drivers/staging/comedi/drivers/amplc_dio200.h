67d9c9c96be8042e7e5cd0657562a8416360a2d9
Ian Abbott <abbotti@mev.co.uk>
2015-05-22 18:15:57 +0100

    staging: comedi: amplc_dio200.h: make self-reliant
    The Comedi "amplc_dio200.h" header file included by drivers for Amplicon
    DIO200 series cards does not compile cleanly when it is the first header
    included by the ".c" file.  It uses `struct comedi_device *` in the
    parameter lists of some function prototypes, so just declare `struct
    comedi_device` as an incomplete type.  It also uses `bool`, so include
    <linux/types.h> to declare it.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_dio200.h
+++ drivers/staging/comedi/drivers/amplc_dio200.h
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef AMPLC_DIO200_H_INCLUDED
 #define AMPLC_DIO200_H_INCLUDED
 
+#include <linux/types.h>
+
+struct comedi_device;
+
 /*
  * Subdevice types.
  */
