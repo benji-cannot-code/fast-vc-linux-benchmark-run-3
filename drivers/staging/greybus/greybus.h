e8290b3e935bc245c22cc202eae9c8cb9c372ad4
Alex Elder <elder@linaro.org>
2015-05-22 12:35:32 -0500

    greybus: rename "gpbridge.h"
    The file "gpbridge.h" is now used as a single place to define
    all protocol message structures.  These protocols are not
    necessarily related to the GP bridge, so the name of the
    file is misleading.
    
    Rename it "greybus_protocols.h".
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kernel_ver.h"
 #include "greybus_id.h"
 #include "greybus_manifest.h"
-#include "gpbridge.h"
+#include "greybus_protocols.h"
 #include "manifest.h"
 #include "endo.h"
 #include "module.h"
