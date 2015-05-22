4add5a12a6dbe7a0138662b82f8c9eb6ca770f71
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-05-22 23:00:50 +0900

    ALSA: firewire-lib: add buffer-over-run protection at receiving more data blocks than expected
    In IEC 61883-6, the number of data blocks in a packet is limited up to
    the value of SYT_INTERVAL. Current implementation is compliant to the
    limitation, while it can cause buffer-over-run when the value of dbs
    field in received packet is illegally large.
    
    This commit adds a validator to detect such illegal packets to prevent
    the buffer-over-run. Actually, the buffer is aligned to the size of memory
     page, thus this issue hardly causes system errors due to the room to page
    alignment, as long as a few packets includes such jumbo payload; i.e.
    a packet to several received packets.
    
    Here, Behringer F-Control Audio 202 (based on OXFW 960) has a quirk to
    postpone transferring isochronous packet till finish handling any
    asynchronous packets. In this case, this model is lazy, transfers no
    packets according to several cycle-start packets. After finishing, this
    model pushes required data in next isochronous packet. As a result, the
    packet include more data blocks than IEC 61883-6 defines.
    
    To continue to support this model, this commit adds a new flag to extend
    the length of calculated payload. This flag allows the size of payload
    5 times as large as IEC 61883-6 defines. As a result, packets from this
    model passed the validator successfully.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/amdtp.h
+++ sound/firewire/amdtp.h
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	packet is not continuous from an initial value.
  * @CIP_EMPTY_HAS_WRONG_DBC: Only for in-stream. The value of dbc in empty
  *	packet is wrong but the others are correct.
+ * @CIP_JUMBO_PAYLOAD: Only for in-stream. The number of data blocks in an
+ *	packet is larger than IEC 61883-6 defines. Current implementation
+ *	allows 5 times as large as IEC 61883-6 defines.
  */
 enum cip_flags {
 	CIP_NONBLOCKING		= 0x00,
@@ -41,6 +44,7 @@ enum cip_flags {
 	CIP_SKIP_DBC_ZERO_CHECK	= 0x20,
 	CIP_SKIP_INIT_DBC_CHECK	= 0x40,
 	CIP_EMPTY_HAS_WRONG_DBC	= 0x80,
+	CIP_JUMBO_PAYLOAD	= 0x100,
 };
 
 /**
