e22ffb839a871fc0e5cb3b40897a8360f7dd89da
Rajeev Kumar <rajeevkumar.linux@gmail.com>
2015-05-22 09:58:30 -0700

    Input: update email-id of Rajeev Kumar
    rajeev-dlh.kumar@st.com email-id doesn't exist anymore as I have left the
    company.
    
    Signed-off-by: Rajeev Kumar <rajeevkumar.linux@gmail.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/linux/platform_data/keyboard-spear.h
+++ include/linux/platform_data/keyboard-spear.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright (C) 2010 ST Microelectronics
- * Rajeev Kumar<rajeev-dlh.kumar@st.com>
+ * Rajeev Kumar <rajeevkumar.linux@gmail.com>
  *
  * This file is licensed under the terms of the GNU General Public
  * License version 2. This program is licensed "as is" without any
