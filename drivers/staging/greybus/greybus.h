5726f17bfc0940995d4fa8caa947da5cca026ed2
Alex Elder <elder@linaro.org>
2015-05-22 13:02:08 -0500

    greybus: introduce SVC protocol
    This patch adds support for the Greybus SVC protocol.  We may want
    to rearrange protocol numbers at some point, since this is a pretty
    fundamental protocol.
    
    Note:  It has only been compile tested; no SVC CPorts have yet been
    defined, so this code is not yet exercised.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "greybus_protocols.h"
 #include "manifest.h"
 #include "endo.h"
+#include "svc.h"
 #include "module.h"
 #include "interface.h"
 #include "bundle.h"
---
+++ drivers/staging/greybus/svc.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Greybus SVC code
+ *
+ * Copyright 2015 Google Inc.
+ * Copyright 2015 Linaro Ltd.
+ *
+ * Released under the GPLv2 only.
+ */
+
+#ifndef __SVC_H
+#define __SVC_H
+
+struct gb_svc;
+
+int gb_svc_intf_device_id(struct gb_svc *svc, u8 intf_id, u8 device_id);
+int gb_svc_intf_reset(struct gb_svc *svc, u8 intf_id);
+int gb_svc_connection_create(struct gb_svc *svc, u8 intf1_id, u16 cport1_id,
+						u8 intf2_id, u16 cport2_id);
+int gb_svc_connection_destroy(struct gb_svc *svc, u8 intf1_id, u16 cport1_id,
+						u8 intf2_id, u16 cport2_id);
+
+#endif /* __SVC_H */
