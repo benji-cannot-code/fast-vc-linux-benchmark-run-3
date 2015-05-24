9307a532e51b59347b04f80e7260bef26c3837ce
Paul Burton <paul.burton@imgtec.com>
2015-05-24 16:11:33 +0100

    MIPS: JZ4740: replace use of jz4740_clock_bdata
    Replace uses of the jz4740_clock_bdata struct with calls to clk_get_rate
    for the appropriate clock. This is in preparation for migrating the
    clocks towards common clock framework & devicetree.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: Lars-Peter Clausen <lars@metafoo.de>
    Cc: linux-mips@linux-mips.org
    Cc: Deng-Cheng Zhu <dengcheng.zhu@imgtec.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: Apelete Seketeli <apelete@seketeli.net>
    Patchwork: https://patchwork.linux-mips.org/patch/10149/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/jz4740/time.c
+++ arch/mips/jz4740/time.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/clk.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/time.h>
@@ -116,11 +117,17 @@ void __init plat_time_init(void)
 	int ret;
 	uint32_t clk_rate;
 	uint16_t ctrl;
+	struct clk *ext_clk;
 
 	jz4740_clock_init();
 	jz4740_timer_init();
 
-	clk_rate = jz4740_clock_bdata.ext_rate >> 4;
+	ext_clk = clk_get(NULL, "ext");
+	if (IS_ERR(ext_clk))
+		panic("unable to get ext clock");
+	clk_rate = clk_get_rate(ext_clk) >> 4;
+	clk_put(ext_clk);
+
 	jz4740_jiffies_per_tick = DIV_ROUND_CLOSEST(clk_rate, HZ);
 
 	clockevent_set_clock(&jz4740_clockevent, clk_rate);
