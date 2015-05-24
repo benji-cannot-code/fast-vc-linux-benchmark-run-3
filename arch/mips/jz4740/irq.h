1dcde92fbde996beef127f97781a5b788161724b
Paul Burton <paul.burton@imgtec.com>
2015-05-24 16:11:29 +0100

    MIPS: JZ4740: Avoid JZ4740-specific naming
    Rename the functions including jz4740 in their names to be more generic
    in preparation for supporting further SoCs, and for moving this
    interrupt controller code to drivers/irqchip.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Cc: Brian Norris <computersforpeace@gmail.com>
    Cc: Lars-Peter Clausen <lars@metafoo.de>
    Patchwork: https://patchwork.linux-mips.org/patch/10146/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/jz4740/irq.h
+++ arch/mips/jz4740/irq.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/irq.h>
 
-extern void jz4740_irq_suspend(struct irq_data *data);
-extern void jz4740_irq_resume(struct irq_data *data);
+extern void ingenic_intc_irq_suspend(struct irq_data *data);
+extern void ingenic_intc_irq_resume(struct irq_data *data);
 
 #endif
