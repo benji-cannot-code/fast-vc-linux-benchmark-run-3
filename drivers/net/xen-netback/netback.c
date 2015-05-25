24b300782658105fab380e2f97bac5865720279c
Shailendra Verma <shailendra.capricorn@gmail.com>
2015-05-25 23:19:31 +0530

    net:xen-netback - Change 1 to true for bool type variable.
    The variable separate_tx_rx_irq is bool type so assigning true
    instead of 1.
    
    Signed-off-by: Shailendra Verma <shailendra.capricorn@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/xen-netback/netback.c
+++ drivers/net/xen-netback/netback.c
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * event channels are limited resource. Split event channels are
  * enabled by default.
  */
-bool separate_tx_rx_irq = 1;
+bool separate_tx_rx_irq = true;
 module_param(separate_tx_rx_irq, bool, 0644);
 
 /* The time that packets can stay on the guest Rx internal queue
