6beb06311f7d8b2bdc4d0dbf3eff83792aeb944b
Christian König <christian.koenig@amd.com>
2015-05-26 12:24:15 +0200

    drm/radeon: stop using addr to check for BO move
    It is theoretically possible that a swapped out BO gets the
    same GTT address, but different backing pages while being swapped in.
    
    Instead just use another VA state to note updated areas.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -468,7 +468,6 @@ struct radeon_bo_va {
 	/* protected by bo being reserved */
 	struct list_head		bo_list;
 	uint32_t			flags;
-	uint64_t			addr;
 	struct radeon_fence		*last_pt_update;
 	unsigned			ref_count;
 
@@ -941,6 +940,9 @@ struct radeon_vm {
 
 	/* BOs freed, but not yet updated in the PT */
 	struct list_head	freed;
+
+	/* BOs cleared in the PT */
+	struct list_head	cleared;
 
 	/* contains the page directory */
 	struct radeon_bo	*page_directory;
