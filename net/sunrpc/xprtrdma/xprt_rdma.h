bd200642a5daf04c7da5ecf4cb223497c3030db3
Chuck Lever <chuck.lever@oracle.com>
2015-05-26 11:51:56 -0400

    xprtrdma: Use ib_device pointer safely
    The connect worker can replace ri_id, but prevents ri_id->device
    from changing during the lifetime of a transport instance. The old
    ID is kept around until a new ID is created and the ->device is
    confirmed to be the same.
    
    Cache a copy of ri_id->device in rpcrdma_ia and in rpcrdma_rep.
    The cached copy can be used safely in code that does not serialize
    with the connect worker.
    
    Other code can use it to save an extra address generation (one
    pointer dereference instead of two).
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Reviewed-by: Steve Wise <swise@opengridcomputing.com>
    Tested-By: Devesh Sharma <devesh.sharma@avagotech.com>
    Reviewed-by: Doug Ledford <dledford@redhat.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprtrdma/xprt_rdma.h
+++ net/sunrpc/xprtrdma/xprt_rdma.h
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct rpcrdma_ia {
 	const struct rpcrdma_memreg_ops	*ri_ops;
 	rwlock_t		ri_qplock;
+	struct ib_device	*ri_device;
 	struct rdma_cm_id 	*ri_id;
 	struct ib_pd		*ri_pd;
 	struct ib_mr		*ri_bind_mem;
@@ -174,6 +175,7 @@ struct rpcrdma_buffer;
 
 struct rpcrdma_rep {
 	unsigned int		rr_len;
+	struct ib_device	*rr_device;
 	struct rpcrdma_xprt	*rr_rxprt;
 	struct list_head	rr_list;
 	struct rpcrdma_regbuf	*rr_rdmabuf;
