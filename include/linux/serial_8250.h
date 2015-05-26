fa673462ab43547d821d558e13f81a8b1cb41e67
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-05-26 13:28:48 +0900

    serial: 8250: include <linux/serial_reg.h> from serial_8250.h
    The header file, include/linux/serial_8250.h, contains references to
    UART_LSR_BRK_ERROR_BITS and UART_MSR_ANY_DELTA that are defined in
    <linux/serial_reg.h>.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/serial_8250.h
+++ include/linux/serial_8250.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_SERIAL_8250_H
 
 #include <linux/serial_core.h>
+#include <linux/serial_reg.h>
 #include <linux/platform_device.h>
 
 /*
