0536d2bcac1938f10699bf5cea6a9815cd556b7f
Lior Amsalem <alior@marvell.com>
2015-05-26 15:07:35 +0200

    dmaengine: mv_xor: Enlarge descriptor pool size
    Now that we have 2 channels assigned to 2 CPUs and all requests are chained
    on same channels, we need much more descriptors available to satisfy
    async_tx workload.
    
    3072 descriptors was found in our lab as the number of descriptors which
    allow the async_tx stack to work without waiting for free descriptors on
    submission of new requests.
    
    Signed-off-by: Lior Amsalem <alior@marvell.com>
    Reviewed-by: Nadav Haklai <nadavh@marvell.com>
    Tested-by: Nadav Haklai <nadavh@marvell.com>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/mv_xor.h
+++ drivers/dma/mv_xor.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmaengine.h>
 #include <linux/interrupt.h>
 
-#define MV_XOR_POOL_SIZE		PAGE_SIZE
+#define MV_XOR_POOL_SIZE		(MV_XOR_SLOT_SIZE * 3072)
 #define MV_XOR_SLOT_SIZE		64
 #define MV_XOR_THRESHOLD		1
 #define MV_XOR_MAX_CHANNELS             2
