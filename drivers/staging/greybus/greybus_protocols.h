a06214db099fe9520d106b6d12096b79a91a13a2
Alex Elder <elder@linaro.org>
2015-05-27 14:45:58 -0500

    greybus: greybus_protocols: fix guard tag
    I neglected to update the "#ifndef/#define" when I renamed
    "greybus_protocols.h".  Fix that.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/greybus_protocols.h
+++ drivers/staging/greybus/greybus_protocols.h
@@ -50,8 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef __GB_GPBRIDGE_H__
-#define __GB_GPBRIDGE_H__
+#ifndef __GREYBUS_PROTOCOLS_H
+#define __GREYBUS_PROTOCOLS_H
 
 /* I2C */
 
@@ -541,4 +541,4 @@ struct gb_svc_conn_destroy_request {
 };
 /* connection destroy response has no payload */
 
-#endif /* __GB_GPBRIDGE_H__ */
+#endif /* __GREYBUS_PROTOCOLS_H */
