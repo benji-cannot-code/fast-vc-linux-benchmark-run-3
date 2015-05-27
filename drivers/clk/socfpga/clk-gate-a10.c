1f70f03d4e1a4ced866361b7c1d21d2ed391fcd2
Dan Carpenter <dan.carpenter@oracle.com>
2015-05-27 11:26:27 +0300

    clk: socfpga: remove a stray tab
    This line was indented too far.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Acked-by: Dinh Nguyen <dinguyen@opensource.altera.com>
    Signed-off-by: Michael Turquette <mturquette@linaro.org>

--- drivers/clk/socfpga/clk-gate-a10.c
+++ drivers/clk/socfpga/clk-gate-a10.c
@@ -40,7 +40,7 @@ static unsigned long socfpga_gate_clk_recalc_rate(struct clk_hw *hwclk,
 	else if (socfpgaclk->div_reg) {
 		val = readl(socfpgaclk->div_reg) >> socfpgaclk->shift;
 		val &= div_mask(socfpgaclk->width);
-			div = (1 << val);
+		div = (1 << val);
 	}
 
 	return parent_rate / div;
