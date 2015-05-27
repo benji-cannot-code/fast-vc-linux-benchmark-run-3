be2e94622faef3edf3cd81ba333fcfcbbc64dfb6
Varka Bhadram <varkabhadram@gmail.com>
2015-05-27 14:21:06 +0530

    ieee802154: fix typo for file name
    Signed-off-by: Varka Bhadram <varkab@cdac.in>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/ieee802154/trace.h
+++ net/ieee802154/trace.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* Based on net/wireless/tracing.h */
+/* Based on net/wireless/trace.h */
 
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM cfg802154
