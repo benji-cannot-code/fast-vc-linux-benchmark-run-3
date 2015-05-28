ed073fee8ae542256b9a074b053622650bbef90f
Samuel Ortiz <sameo@linux.intel.com>
2015-05-28 17:10:03 +0200

    NFC: nxp-nci: Fix build warning
    When GPIO is not enabled we hit this kind of warning:
    
       drivers/nfc/nxp-nci/i2c.c: In function 'nxp_nci_i2c_acpi_config':
       drivers/nfc/nxp-nci/i2c.c:320:2: error: implicit declaration of function 'devm_gpiod_get_index' [-Werror=implicit-function-declaration]
         gpiod_en = devm_gpiod_get_index(&client->dev, NULL, 2);
    
    This is fixed by explicitely including gpio/consumer.h.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/nxp-nci/i2c.c
+++ drivers/nfc/nxp-nci/i2c.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/nfc.h>
+#include <linux/gpio/consumer.h>
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/platform_data/nxp-nci.h>
