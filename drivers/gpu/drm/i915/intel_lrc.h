18e082c749f4b67616a3291fe7fa599f6874dcad
John Harrison <John.C.Harrison@Intel.com>
2015-05-29 17:44:09 +0100

    drm/i915: Add *_ring_begin() to request allocation
    Now that the *_ring_begin() functions no longer call the request allocation
    code, it is finally safe for the request allocation code to call *_ring_begin().
    This is important to guarantee that the space reserved for the subsequent
    i915_add_request() call does actually get reserved.
    
    v2: Renamed functions according to review feedback (Tomas Elf).
    
    For: VIZ-5115
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_lrc.h
+++ drivers/gpu/drm/i915/intel_lrc.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Logical Rings */
 int intel_logical_ring_alloc_request_extras(struct drm_i915_gem_request *request);
+int intel_logical_ring_reserve_space(struct drm_i915_gem_request *request);
 void intel_logical_ring_stop(struct intel_engine_cs *ring);
 void intel_logical_ring_cleanup(struct intel_engine_cs *ring);
 int intel_logical_rings_init(struct drm_device *dev);
