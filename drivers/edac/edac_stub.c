f22f232d3b72ee3df5cb725eb793c378d664b1db
Borislav Petkov <bp@suse.de>
2015-05-29 22:01:00 +0200

    EDAC, edac_stub: Drop arch-specific include
    <asm/edac.h> contains only the arch-specific scrubbing function and is
    thus not needed in edac_stub.c. Kill it.
    
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/edac_stub.c
+++ drivers/edac/edac_stub.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/edac.h>
 #include <linux/atomic.h>
 #include <linux/device.h>
-#include <asm/edac.h>
 
 int edac_op_state = EDAC_OPSTATE_INVAL;
 EXPORT_SYMBOL_GPL(edac_op_state);
