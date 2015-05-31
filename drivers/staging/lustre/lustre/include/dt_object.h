07946bf46a4e303bff9bf30f97d9805f5f6fbcde
Oleg Drokin <green@linuxhacker.ru>
2015-05-30 23:27:04 -0400

    staging/lustre/obd: Remove nid_stats tracking
    This nid_stats tracking only makes sense on the server side,
    on the client there are no other clients to keep track of anyway.
    
    Signed-off-by: Dmitry Eremin <dmiter4ever@gmail.com>
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/dt_object.h
+++ drivers/staging/lustre/lustre/include/dt_object.h
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../../include/linux/libcfs/libcfs.h"
 
 struct seq_file;
-struct proc_dir_entry;
 struct lustre_cfg;
 
 struct thandle;
