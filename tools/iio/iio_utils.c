9693bdf27618ccc9cea7335a914693a9a0f986dd
Hartmut Knaack <knaack.h@gmx.de>
2015-05-31 14:40:36 +0200

    tools:iio:iio_utils: pass strings as const
    Mark strings, which are not supposed to be changed (basedir, filename,
    value), as const in function parameters.
    
    Signed-off-by: Hartmut Knaack <knaack.h@gmx.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- tools/iio/iio_utils.c
+++ tools/iio/iio_utils.c
@@ -635,7 +635,8 @@ error_close_dir:
 	return ret;
 }
 
-static int _write_sysfs_int(char *filename, char *basedir, int val, int verify)
+static int _write_sysfs_int(const char *filename, const char *basedir, int val,
+			    int verify)
 {
 	int ret = 0;
 	FILE *sysfsfp;
@@ -708,7 +709,7 @@ error_free:
  *
  * Returns a value >= 0 on success, otherwise a negative error code.
  **/
-int write_sysfs_int(char *filename, char *basedir, int val)
+int write_sysfs_int(const char *filename, const char *basedir, int val)
 {
 	return _write_sysfs_int(filename, basedir, val, 0);
 }
@@ -722,13 +723,14 @@ int write_sysfs_int(char *filename, char *basedir, int val)
  *
  * Returns a value >= 0 on success, otherwise a negative error code.
  **/
-int write_sysfs_int_and_verify(char *filename, char *basedir, int val)
+int write_sysfs_int_and_verify(const char *filename, const char *basedir,
+			       int val)
 {
 	return _write_sysfs_int(filename, basedir, val, 1);
 }
 
-static int _write_sysfs_string(char *filename, char *basedir, char *val,
-			       int verify)
+static int _write_sysfs_string(const char *filename, const char *basedir,
+			       const char *val, int verify)
 {
 	int ret = 0;
 	FILE  *sysfsfp;
@@ -806,7 +808,8 @@ error_free:
  *
  * Returns a value >= 0 on success, otherwise a negative error code.
  **/
-int write_sysfs_string_and_verify(char *filename, char *basedir, char *val)
+int write_sysfs_string_and_verify(const char *filename, const char *basedir,
+				  const char *val)
 {
 	return _write_sysfs_string(filename, basedir, val, 1);
 }
@@ -819,7 +822,8 @@ int write_sysfs_string_and_verify(char *filename, char *basedir, char *val)
  *
  * Returns a value >= 0 on success, otherwise a negative error code.
  **/
-int write_sysfs_string(char *filename, char *basedir, char *val)
+int write_sysfs_string(const char *filename, const char *basedir,
+		       const char *val)
 {
 	return _write_sysfs_string(filename, basedir, val, 0);
 }
@@ -832,7 +836,7 @@ int write_sysfs_string(char *filename, char *basedir, char *val)
  * Returns the read integer value >= 0 on success, otherwise a negative error
  * code.
  **/
-int read_sysfs_posint(char *filename, char *basedir)
+int read_sysfs_posint(const char *filename, const char *basedir)
 {
 	int ret;
 	FILE  *sysfsfp;
@@ -876,7 +880,7 @@ error_free:
  *
  * Returns a value >= 0 on success, otherwise a negative error code.
  **/
-int read_sysfs_float(char *filename, char *basedir, float *val)
+int read_sysfs_float(const char *filename, const char *basedir, float *val)
 {
 	int ret = 0;
 	FILE  *sysfsfp;
