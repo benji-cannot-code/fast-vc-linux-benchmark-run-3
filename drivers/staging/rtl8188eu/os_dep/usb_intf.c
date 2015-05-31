c50ac06cf48e5089a2913444f8b7a0a9d5038f05
Jakub Sitnicki <jsitnicki@gmail.com>
2015-05-31 15:04:14 +0200

    staging: rtl8188eu: Kill dead calls to kill_pid()
    There is no interface to register PIDs of processes the driver should
    send a signal to.  Remove it.
    
    Signed-off-by: Jakub Sitnicki <jsitnicki@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/usb_intf.c
+++ drivers/staging/rtl8188eu/os_dep/usb_intf.c
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <usb_hal.h>
 #include <rtw_ioctl.h>
 
-int ui_pid[3] = {0, 0, 0};
-
 #define USB_VENDER_ID_REALTEK		0x0bda
 
 /* DID_USB_v916_20130116 */
@@ -331,11 +329,6 @@ static int rtw_resume_process(struct adapter *padapter)
 
 	_exit_pwrlock(&pwrpriv->lock);
 
-	if (padapter->pid[1] != 0) {
-		DBG_88E("pid[1]:%d\n", padapter->pid[1]);
-		rtw_signal_process(padapter->pid[1], SIGUSR2);
-	}
-
 	rtw_roaming(padapter, NULL);
 
 	ret = 0;
@@ -510,11 +503,6 @@ static int rtw_drv_init(struct usb_interface *pusb_intf, const struct usb_device
 	if (if1 == NULL) {
 		DBG_88E("rtw_init_primarystruct adapter Failed!\n");
 		goto free_dvobj;
-	}
-
-	if (ui_pid[1] != 0) {
-		DBG_88E("ui_pid[1]:%d\n", ui_pid[1]);
-		rtw_signal_process(ui_pid[1], SIGUSR2);
 	}
 
 	RT_TRACE(_module_hci_intfs_c_, _drv_err_, ("-871x_drv - drv_init, success!\n"));
