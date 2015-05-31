82b70e67ecdd37f1b532a6fa3e9cefb104a976b7
Hans Verkuil <hans.verkuil@cisco.com>
2015-05-31 06:48:00 -0300

    [media] cobalt: support transfer function
    Add support for the transfer function to the cobalt driver: make sure it is
    passed on to/retrieved from the sub-device correctly.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/pci/cobalt/cobalt-driver.h
+++ drivers/media/pci/cobalt/cobalt-driver.h
@@ -232,6 +232,7 @@ struct cobalt_stream {
 	u32 pixfmt;
 	u32 sequence;
 	u32 colorspace;
+	u32 xfer_func;
 	u32 ycbcr_enc;
 	u32 quantization;
 
--- drivers/media/pci/cobalt/cobalt-v4l2.c
+++ drivers/media/pci/cobalt/cobalt-v4l2.c
@@ -171,6 +171,7 @@ static void cobalt_enable_output(struct cobalt_stream *s)
 	}
 
 	sd_fmt.format.colorspace = s->colorspace;
+	sd_fmt.format.xfer_func = s->xfer_func;
 	sd_fmt.format.ycbcr_enc = s->ycbcr_enc;
 	sd_fmt.format.quantization = s->quantization;
 	sd_fmt.format.width = bt->width;
@@ -738,6 +739,7 @@ static int cobalt_g_fmt_vid_cap(struct file *file, void *priv_fh,
 		v4l2_subdev_call(s->sd, pad, get_fmt, NULL, &sd_fmt);
 		v4l2_fill_pix_format(pix, &sd_fmt.format);
 		pix->colorspace = sd_fmt.format.colorspace;
+		pix->xfer_func = sd_fmt.format.xfer_func;
 		pix->ycbcr_enc = sd_fmt.format.ycbcr_enc;
 		pix->quantization = sd_fmt.format.quantization;
 	}
@@ -783,6 +785,7 @@ static int cobalt_try_fmt_vid_cap(struct file *file, void *priv_fh,
 		v4l2_subdev_call(s->sd, pad, get_fmt, NULL, &sd_fmt);
 		v4l2_fill_pix_format(pix, &sd_fmt.format);
 		pix->colorspace = sd_fmt.format.colorspace;
+		pix->xfer_func = sd_fmt.format.xfer_func;
 		pix->ycbcr_enc = sd_fmt.format.ycbcr_enc;
 		pix->quantization = sd_fmt.format.quantization;
 	}
@@ -898,6 +901,7 @@ static int cobalt_g_fmt_vid_out(struct file *file, void *priv_fh,
 	pix->field = V4L2_FIELD_NONE;
 	pix->pixelformat = s->pixfmt;
 	pix->colorspace = s->colorspace;
+	pix->xfer_func = s->xfer_func;
 	pix->ycbcr_enc = s->ycbcr_enc;
 	pix->quantization = s->quantization;
 	pix->sizeimage = pix->bytesperline * pix->height;
@@ -954,11 +958,13 @@ static int cobalt_s_fmt_vid_out(struct file *file, void *priv_fh,
 	s->stride = pix->bytesperline;
 	s->pixfmt = pix->pixelformat;
 	s->colorspace = pix->colorspace;
+	s->xfer_func = pix->xfer_func;
 	s->ycbcr_enc = pix->ycbcr_enc;
 	s->quantization = pix->quantization;
 	sd_fmt.which = V4L2_SUBDEV_FORMAT_ACTIVE;
 	v4l2_subdev_call(s->sd, pad, get_fmt, NULL, &sd_fmt);
 	sd_fmt.format.colorspace = pix->colorspace;
+	sd_fmt.format.xfer_func = pix->xfer_func;
 	sd_fmt.format.ycbcr_enc = pix->ycbcr_enc;
 	sd_fmt.format.quantization = pix->quantization;
 	v4l2_subdev_call(s->sd, pad, set_fmt, NULL, &sd_fmt);
