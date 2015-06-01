8dd5bb5b86ba2cf092346b94af2af81c73461d63
Hari Prasath Gujulan Elango <hgujulan@visteon.com>
2015-06-01 13:06:54 +0000

    staging: dgnc: remove ununsed Macro
    Remove the ununsed Macro
    
    Signed-off-by: Hari Prasath Gujulan Elango <hgujulan@visteon.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.h
+++ drivers/staging/dgnc/dgnc_driver.h
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* 4 extra for alignment play space */
 #define WRITEBUFLEN		((4096) + 4)
-#define MYFLIPLEN		N_TTY_BUF_SIZE
 
 #define dgnc_jiffies_from_ms(a) (((a) * HZ) / 1000)
 
