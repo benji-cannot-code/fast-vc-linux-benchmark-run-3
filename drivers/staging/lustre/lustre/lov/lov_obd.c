d7bcdf6dba28ebe166ca7b2bf33a524dc09d4755
Antonio Murdaca <antoniomurdaca@gmail.com>
2015-06-01 23:14:14 +0200

    staging: lustre: lov: remove unnecessary parentheses
    fix checkpatch.pl warning about unnecessary parentheses
    
    Signed-off-by: Antonio Murdaca <antonio.murdaca@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_obd.c
+++ drivers/staging/lustre/lustre/lov/lov_obd.c
@@ -1401,7 +1401,7 @@ static int lov_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
 		__u32 flags;
 
 		memcpy(&index, data->ioc_inlbuf2, sizeof(__u32));
-		if ((index >= count))
+		if (index >= count)
 			return -ENODEV;
 
 		if (!lov->lov_tgts[index])
