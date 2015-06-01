678696519d749ccb51f7b69d4c1258d3685c6820
Maxime Ripard <maxime.ripard@free-electrons.com>
2015-06-01 11:04:26 +0200

    drivers: soc: sunxi: Introduce SoC driver to map SRAMs
    The Allwinner SoCs have a handful of SRAM that can be either mapped to be
    accessible by devices or the CPU.
    
    That mapping is controlled by an SRAM controller, and that mapping might
    not be set by the bootloader, for example if the device wasn't used at all,
    or if we're using solutions like the U-Boot's Falcon Boot.
    
    We could also imagine changing this at runtime for example to change the
    mapping of these SRAMs to use them for suspend/resume or runtime memory
    rate change, if that ever happens.
    
    These use cases require some API in the kernel to control that mapping,
    exported through a drivers/soc driver.
    
    This driver also implement a debugfs file that shows the SRAM found in the
    system, the current mapping and the SRAM that have been claimed by some
    drivers in the kernel.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Tested-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

---
+++ include/linux/soc/sunxi/sunxi_sram.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Allwinner SoCs SRAM Controller Driver
+ *
+ * Copyright (C) 2015 Maxime Ripard
+ *
+ * Author: Maxime Ripard <maxime.ripard@free-electrons.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef _SUNXI_SRAM_H_
+#define _SUNXI_SRAM_H_
+
+int sunxi_sram_claim(struct device *dev);
+int sunxi_sram_release(struct device *dev);
+
+#endif /* _SUNXI_SRAM_H_ */
