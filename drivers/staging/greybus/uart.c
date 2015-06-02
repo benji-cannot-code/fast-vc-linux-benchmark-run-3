5fb2d3ca574a1ed816108c6742fda0d8a15b0fca
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2015-06-02 13:40:46 +0100

    greybus: uart: Reduce UART count from 255 to 16
    Arbitrary number 255 is both not aligned and probably too big.
    Move the UART count down to 16 which is still large but, more realistic.
    8 may be too few for future testing setups, 16 should accomodate any.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/uart.c
+++ drivers/staging/greybus/uart.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "greybus.h"
 
-#define GB_NUM_MINORS	255	/* 255 is enough for anyone... */
+#define GB_NUM_MINORS	16	/* 16 is is more than enough */
 #define GB_NAME		"ttyGB"
 
 struct gb_tty_line_coding {
