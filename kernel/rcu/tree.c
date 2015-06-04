9ae87884a16991cf4c22dcf7600b88d46ff7647b
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-06-04 10:06:01 -0700

    rcu: Reset rcu_fanout_leaf if out of bounds
    Currently if the rcu_fanout_leaf boot parameter is out of bounds (that
    is, less than RCU_FANOUT_LEAF or greater than the number of bits in an
    unsigned long), a warning is issued and execution continues with the
    out-of-bounds value.  This can result in all manner of failures, so this
    patch resets rcu_fanout_leaf to RCU_FANOUT_LEAF when an out-of-bounds
    condition is detected.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -4104,6 +4104,7 @@ static void __init rcu_init_geometry(void)
 	 */
 	if (rcu_fanout_leaf < RCU_FANOUT_LEAF ||
 	    rcu_fanout_leaf > sizeof(unsigned long) * 8) {
+		rcu_fanout_leaf = RCU_FANOUT_LEAF;
 		WARN_ON(1);
 		return;
 	}
