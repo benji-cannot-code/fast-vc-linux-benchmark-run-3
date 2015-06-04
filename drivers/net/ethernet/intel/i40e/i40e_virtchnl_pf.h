971694aefa1d077b53c1dc4dbdc7bf200464c7c3
Mitch Williams <mitch.a.williams@intel.com>
2015-06-04 16:23:56 -0400

    i40e/i40evf: add macros for virtual channel API version and device capability
    Now that we've rolled the virtual channel API version to 1.1, add some
    macros to test what version is being used by our partner in crime. For the
    VF, add some macros to determine what our device capabilities are.
    
    Change-ID: I79f6683d4c23bd76a8ad9fd492776fcc1208e1dc
    Signed-off-by: Mitch Williams <mitch.a.williams@intel.com>
    Tested-by: Jim Young <james.m.young@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_virtchnl_pf.h
+++ drivers/net/ethernet/intel/i40e/i40e_virtchnl_pf.h
@@ -43,6 +43,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I40E_VLAN_MASK			0xFFF
 #define I40E_PRIORITY_MASK		0x7000
 
+#define VF_IS_V10(_v) (((_v)->vf_ver.major == 1) && ((_v)->vf_ver.minor == 0))
+#define VF_IS_V11(_v) (((_v)->vf_ver.major == 1) && ((_v)->vf_ver.minor == 1))
+
 /* Various queue ctrls */
 enum i40e_queue_ctrl {
 	I40E_QUEUE_CTRL_UNKNOWN = 0,
