85410d4e1371d4bf1cf27af1c90be9453ea8fdf6
Markos Chandras <markos.chandras@imgtec.com>
2015-06-04 11:56:11 +0100

    MIPS: BPF: Free up some callee-saved registers
    Move the two scratch registers from s0 and s1 to t4 and t5 in order
    to free up some callee-saved registers. We will use these callee-saved
    registers to store some permanent data on them in a subsequent patch.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: netdev@vger.kernel.org
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Alexei Starovoitov <ast@plumgrid.com>
    Cc: Daniel Borkmann <dborkman@redhat.com>
    Cc: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mips@linux-mips.org
    Patchwork: http://patchwork.linux-mips.org/patch/10525/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/net/bpf_jit.h
+++ arch/mips/net/bpf_jit.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MIPS_R_V1	3
 #define MIPS_R_A0	4
 #define MIPS_R_A1	5
+#define MIPS_R_T4	12
+#define MIPS_R_T5	13
 #define MIPS_R_T6	14
 #define MIPS_R_T7	15
 #define MIPS_R_S0	16
