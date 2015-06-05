d5213f20dc05d6408db8aec7867299e652d6520a
Ian Abbott <abbotti@mev.co.uk>
2015-06-05 18:30:05 +0100

    staging: comedi: das08.h: change description in copyright header comment
    The copyright header comment includes a single-line description saying
    it is for "das08.c" and "das08_cs.c".  However, it is also used by
    "das08_isa.c" and "das08_pci.c".  Update the description to say it is
    for common DAS08 support, similar to description in "das08.c" (the
    common module for the DAS08 ISA/PCI/PCMCIA drivers).
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/das08.h
+++ drivers/staging/comedi/drivers/das08.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * das08.h
  *
- * Header for das08.c and das08_cs.c
+ * Header for common DAS08 support (used by ISA/PCI/PCMCIA drivers)
  *
  * Copyright (C) 2003 Frank Mori Hess <fmhess@users.sourceforge.net>
  *
