ff1a6e94123ef745f2dd66fdeeb75d9c0c272c16
Sara Sharon <sara.sharon@intel.com>
2015-06-07 09:34:57 +0300

    iwlwifi: deprecate -10.ucode for 3160 / 7260 / 7265
    This firmware is not supported anymore - stop loading this firmware.
    Remove code handling older versions.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-7000.c
+++ drivers/net/wireless/iwlwifi/iwl-7000.c
@@ -77,7 +77,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IWL3165_UCODE_API_OK	13
 
 /* Lowest firmware API version supported */
-#define IWL7260_UCODE_API_MIN	10
+#define IWL7260_UCODE_API_MIN	12
 #define IWL3165_UCODE_API_MIN	13
 
 /* NVM versions */
