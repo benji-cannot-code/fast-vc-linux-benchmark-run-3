45890e9b02231084d95c4ccde6f2719c90f1aa8f
Vineet Gupta <vgupta@synopsys.com>
2015-06-07 11:17:34 +0530

    ARC: [axs101] Add missing __init annotations
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/plat-axs10x/axs10x.c
+++ arch/arc/plat-axs10x/axs10x.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CREG_CPU_ARC770_IRQ_MUX	(AXC001_CREG + 0x114)
 #define CREG_CPU_GPIO_UART_MUX	(AXC001_CREG + 0x120)
 
-static void enable_gpio_intc_wire(void)
+static void __init axs10x_enable_gpio_intc_wire(void)
 {
 	/*
 	 * Peripherals on CPU Card and Mother Board are wired to cpu intc via
@@ -84,7 +84,7 @@ static void enable_gpio_intc_wire(void)
 	iowrite32(1 << MB_TO_GPIO_IRQ, (void __iomem *) GPIO_INTEN);
 }
 
-static void axs10x_print_board_ver(unsigned int creg, const char *str)
+static void __init axs10x_print_board_ver(unsigned int creg, const char *str)
 {
 	union ver {
 		struct {
@@ -102,7 +102,7 @@ static void axs10x_print_board_ver(unsigned int creg, const char *str)
 		board.y);
 }
 
-static void axs10x_early_init(void)
+static void __init axs10x_early_init(void)
 {
 	int mb_rev;
 	char mb[32];
@@ -113,7 +113,7 @@ static void axs10x_early_init(void)
 	else
 		mb_rev = 2;	/* HT-2 (rev2.0) */
 
-	enable_gpio_intc_wire();
+	axs10x_enable_gpio_intc_wire();
 
 	scnprintf(mb, 32, "MainBoard v%d", mb_rev);
 	axs10x_print_board_ver(CREG_MB_VER, mb);
@@ -228,7 +228,7 @@ static const struct aperture axs_mb_memmap[16] = {
 	{AXS_MB_SLV_AXI_TUNNEL_CPU,	0xF},
 };
 
-static noinline void
+static noinline void __init
 axs101_set_memmap(void __iomem *base, const struct aperture map[16])
 {
 	unsigned int slave_select, slave_offset;
@@ -253,7 +253,7 @@ axs101_set_memmap(void __iomem *base, const struct aperture map[16])
 	iowrite32(slave_offset, base + 0xC);	/* OFFSET1 */
 }
 
-static void axs101_early_init(void)
+static void __init axs101_early_init(void)
 {
 	int i;
 
