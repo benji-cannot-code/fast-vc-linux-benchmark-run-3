08c5c87b46b1337fdb3914540f521963c93437d4
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2015-06-07 22:40:27 +0200

    drm/nouveau/pm: allow the userspace to configure sources
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset at gmail.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/pm/priv.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/pm/priv.h
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nvkm_perfctr {
 	struct list_head head;
+	u8 domain;
 	u8  signal[4];
+	u64 source[4][8];
 	int slot;
 	u32 logic_op;
 	u32 ctr;
