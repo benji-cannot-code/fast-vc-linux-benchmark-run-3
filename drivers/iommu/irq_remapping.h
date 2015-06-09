508c1314d0361bc6e5093eeb8ac713932f4287e5
Feng Wu <feng.wu@intel.com>
2015-06-09 13:20:35 +0800

    iommu, x86: Setup Posted-Interrupts capability for Intel iommu
    Set Posted-Interrupts capability for Intel iommu when Interrupt
    Remapping is enabled, clear it when disabled.
    
    Signed-off-by: Feng Wu <feng.wu@intel.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Joerg Roedel <joro@8bytes.org>
    Cc: jiang.liu@linux.intel.com
    Cc: iommu@lists.linux-foundation.org
    Cc: dwmw2@infradead.org
    Link: http://lkml.kernel.org/r/1433827237-3382-9-git-send-email-feng.wu@intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/iommu/irq_remapping.h
+++ drivers/iommu/irq_remapping.h
@@ -35,6 +35,8 @@ extern int disable_sourceid_checking;
 extern int no_x2apic_optout;
 extern int irq_remapping_enabled;
 
+extern int disable_irq_post;
+
 struct irq_remap_ops {
 	/* The supported capabilities */
 	int capability;
@@ -70,6 +72,7 @@ extern void ir_ack_apic_edge(struct irq_data *data);
 
 #define irq_remapping_enabled 0
 #define irq_remap_broken      0
+#define disable_irq_post      1
 
 #endif /* CONFIG_IRQ_REMAP */
 
