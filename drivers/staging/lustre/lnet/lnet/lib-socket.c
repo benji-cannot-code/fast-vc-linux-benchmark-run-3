d9e0e77c0b6fffd6a24711ec2f2e3ec1e851f603
James Simmons <jsimmons@infradead.org>
2015-06-08 22:27:08 -0400

    staging:lustre: remove useless libcfs_sock_release
    There is no reason to have a one line exported function
    libcfs_sock_release. Instead we can call sock_release directly.
    
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/lnet/lib-socket.c
+++ drivers/staging/lustre/lnet/lnet/lib-socket.c
@@ -615,11 +615,3 @@ libcfs_sock_connect (struct socket **sockp, int *fatal,
 }
 
 EXPORT_SYMBOL(libcfs_sock_connect);
-
-void
-libcfs_sock_release (struct socket *sock)
-{
-	sock_release(sock);
-}
-
-EXPORT_SYMBOL(libcfs_sock_release);
