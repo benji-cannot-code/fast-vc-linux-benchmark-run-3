71456b437bf9a04201f5725192767928e13f81f9
Lokesh Vutla <lokeshvutla@ti.com>
2015-06-10 14:56:24 +0530

    ARM: OMAP2+: hwmod: add support for lock and unlock hooks
    Some IP blocks like RTC, needs an additional setting for writing to its
    registers. This is to prevent any spurious writes from changing the
    register values.
    
    This patch adds optional lock and unlock function pointers to
    the IP block's hwmod data. These unlock and lock function pointers
    are called by hwmod code before and after writing sysconfig registers.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    [paul@pwsan.com: fixed indentation level to conform with the rest of the
     structure members]
    Reviewed-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>

--- arch/arm/mach-omap2/omap_hwmod.h
+++ arch/arm/mach-omap2/omap_hwmod.h
@@ -577,6 +577,8 @@ struct omap_hwmod_omap4_prcm {
  * @pre_shutdown: ptr to fn to be executed immediately prior to device shutdown
  * @reset: ptr to fn to be executed in place of the standard hwmod reset fn
  * @enable_preprogram:  ptr to fn to be executed during device enable
+ * @lock: ptr to fn to be executed to lock IP registers
+ * @unlock: ptr to fn to be executed to unlock IP registers
  *
  * Represent the class of a OMAP hardware "modules" (e.g. timer,
  * smartreflex, gpio, uart...)
@@ -601,6 +603,8 @@ struct omap_hwmod_class {
 	int					(*pre_shutdown)(struct omap_hwmod *oh);
 	int					(*reset)(struct omap_hwmod *oh);
 	int					(*enable_preprogram)(struct omap_hwmod *oh);
+	void					(*lock)(struct omap_hwmod *oh);
+	void					(*unlock)(struct omap_hwmod *oh);
 };
 
 /**
