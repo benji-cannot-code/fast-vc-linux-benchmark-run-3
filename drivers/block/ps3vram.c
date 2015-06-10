0a2e6533fe4e5e07cfe6e812d72c3ec45eb9aa2b
Geert Uytterhoeven <geert@linux-m68k.org>
2015-06-10 18:00:17 +0000

    block/ps3vram: Remove obsolete reference to MTD
    The ps3vram driver is a plain block device driver since commit
    f507cd22035fdadd5dbb476dd05e9e7ee21c3b84 ("ps3/block: Replace mtd/ps3vram
    by block/ps3vram").
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Geoff Levand <geoff@infradead.org>
    Acked-by: Jim Paris <jim@jtan.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/ps3vram.c
+++ drivers/block/ps3vram.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * ps3vram - Use extra PS3 video ram as MTD block device.
+ * ps3vram - Use extra PS3 video ram as block device.
  *
  * Copyright 2009 Sony Corporation
  *
