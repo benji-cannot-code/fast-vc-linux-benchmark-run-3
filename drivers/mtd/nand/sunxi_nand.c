4d7ec05baa7ca6c2de2b1e9648a9928175b39e09
Frans Klaver <fransklaver@gmail.com>
2015-06-10 22:39:07 +0200

    mtd: nand: sunxi_nand: drop owner assignment
    Owner is automatically set by mtdcore. Make use of that.
    
    Signed-off-by: Frans Klaver <fransklaver@gmail.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/nand/sunxi_nand.c
+++ drivers/mtd/nand/sunxi_nand.c
@@ -1340,7 +1340,6 @@ static int sunxi_nand_chip_init(struct device *dev, struct sunxi_nfc *nfc,
 	mtd = &chip->mtd;
 	mtd->dev.parent = dev;
 	mtd->priv = nand;
-	mtd->owner = THIS_MODULE;
 
 	ret = nand_scan_ident(mtd, nsels, NULL);
 	if (ret)
