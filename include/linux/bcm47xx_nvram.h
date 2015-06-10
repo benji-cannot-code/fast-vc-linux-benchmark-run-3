d1d07430bf9f104b43b04e918c25e9e65b4cc387
Rafał Miłecki <zajec5@gmail.com>
2015-06-10 23:05:08 +0200

    MIPS: BCM47xx: Move NVRAM driver to the drivers/firmware/
    After Broadcom switched from MIPS to ARM for their home routers we need
    to have NVRAM driver in some common place (not arch/mips/). As explained
    in Kconfig, this driver is responsible for parsing SoC configuration
    data that is passed to the kernel in flash from the bootloader firmware
    called "CFE".
    
    We were thinking about putting it in bus directory, however there are
    two possible buses for MIPS: drivers/ssb/ and drivers/bcma/. So this
    won't fit there and this is why I would like to move this driver to the
    drivers/firmware/.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Reviewed-by: Paul Walmsley <paul@pwsan.com>
    Cc: linux-mips@linux-mips.org
    Cc: Hauke Mehrtens <hauke@hauke-m.de>
    Cc: Seiji Aguchi <seiji.aguchi@hds.com>
    Cc: Greg Kroah-Hartman <gregkh@suse.de>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Mike Waychison <mikew@google.com>
    Cc: Roy Franz <roy.franz@linaro.org>
    Cc: Matt Fleming <matt.fleming@intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Patchwork: https://patchwork.linux-mips.org/patch/10544/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/linux/bcm47xx_nvram.h
+++ include/linux/bcm47xx_nvram.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/vmalloc.h>
 
-#ifdef CONFIG_BCM47XX
+#ifdef CONFIG_BCM47XX_NVRAM
 int bcm47xx_nvram_init_from_mem(u32 base, u32 lim);
 int bcm47xx_nvram_getenv(const char *name, char *val, size_t val_len);
 int bcm47xx_nvram_gpio_pin(const char *name);
