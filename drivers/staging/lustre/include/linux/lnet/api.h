17980385996fc38e1119628696a3b8c45165e1fe
James Simmons <jsimmons@infradead.org>
2015-06-11 15:18:13 -0400

    staging:lustre: fix camel case for LNetInit and LNetFini
    For the functions LNetInit and LNetFini move away from
    camel case to lnet_init and lnet_fini.
    
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/lnet/api.h
+++ drivers/staging/lustre/include/linux/lnet/api.h
@@ -53,9 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /** \defgroup lnet_init_fini Initialization and cleanup
  * The LNet must be properly initialized before any LNet calls can be made.
  * @{ */
-int LNetInit(void);
-void LNetFini(void);
-
 int LNetNIInit(lnet_pid_t requested_pid);
 int LNetNIFini(void);
 /** @} lnet_init_fini */
