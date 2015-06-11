832c4bb676d4233e179bc9bb2d01abc65f282f60
Vincent Cuissard <cuissard@marvell.com>
2015-06-11 11:25:43 +0200

    NFC: nfcmrvl: add support of HCI-based transport
    In some configuration NCI packet can be encapsulated in HCI
    packets. This patch had the support of this.
    
    Signed-off-by: Vincent Cuissard <cuissard@marvell.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/nfcmrvl/nfcmrvl.h
+++ drivers/nfc/nfcmrvl/nfcmrvl.h
@@ -28,7 +28,23 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NFCMRVL_GPIO_PIN_NFC_ACTIVE		0xB
 #define NFCMRVL_NCI_MAX_EVENT_SIZE		260
 
+/*
+** HCI defines
+*/
+
+#define NFCMRVL_HCI_EVENT_HEADER_SIZE		0x04
+#define NFCMRVL_HCI_EVENT_CODE			0x04
+#define NFCMRVL_HCI_NFC_EVENT_CODE		0xFF
+#define NFCMRVL_HCI_COMMAND_CODE		0x01
+#define NFCMRVL_HCI_OGF				0x81
+#define NFCMRVL_HCI_OCF				0xFE
+
+#define NFCMRVL_DEV_FLAG_HCI_MUXED		(1 << 0)
+
 struct nfcmrvl_private {
+
+	/* Tell if NCI packets are encapsulated in HCI ones */
+	int hci_muxed;
 	struct nci_dev *ndev;
 	unsigned long flags;
 	void *drv_data;
@@ -46,4 +62,5 @@ void nfcmrvl_nci_unregister_dev(struct nfcmrvl_private *priv);
 int nfcmrvl_nci_recv_frame(struct nfcmrvl_private *priv, void *data, int count);
 struct nfcmrvl_private *nfcmrvl_nci_register_dev(void *drv_data,
 						 struct nfcmrvl_if_ops *ops,
-						 struct device *dev);
+						 struct device *dev,
+						 unsigned int flags);
