fb25749eda68bba3c97a90bb21b09e1757c3610d
Hyungwon Hwang <human.hwang@samsung.com>
2015-06-12 21:59:02 +0900

    drm/exynos: mic: add MIC driver
    MIC(Mobile image compressor) is newly added IP in Exynos5433. MIC
    resides between decon and mipi dsim, and compresses frame data by 50%.
    With dsi, not display port, to send frame data to the panel, the
    bandwidth is not enough. That is why this compressor is introduced.
    
    Signed-off-by: Hyungwon Hwang <human.hwang@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_drv.c
+++ drivers/gpu/drm/exynos/exynos_drm_drv.c
@@ -358,6 +358,9 @@ static struct platform_driver *const exynos_drm_kms_drivers[] = {
 #ifdef CONFIG_DRM_EXYNOS7_DECON
 	&decon_driver,
 #endif
+#ifdef CONFIG_DRM_EXYNOS_MIC
+	&mic_driver,
+#endif
 #ifdef CONFIG_DRM_EXYNOS_DP
 	&dp_driver,
 #endif
@@ -371,9 +374,6 @@ static struct platform_driver *const exynos_drm_kms_drivers[] = {
 };
 
 static struct platform_driver *const exynos_drm_non_kms_drivers[] = {
-#ifdef CONFIG_DRM_EXYNOS_MIC
-	&mic_driver,
-#endif
 #ifdef CONFIG_DRM_EXYNOS_G2D
 	&g2d_driver,
 #endif
