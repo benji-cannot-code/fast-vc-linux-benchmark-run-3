e55e306cea04cc82bbac1161ea74e049f576f1f0
Alexander Aring <alex.aring@gmail.com>
2015-06-13 22:15:52 +0200

    mac802154: iface: flush workqueue before stop
    This patch flushs the workqueue which is currently used for xmit_sync
    callback before calling stop driver-ops. Flush the queue will ensure all
    pending tx frames are transmitted.
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/mac802154/iface.c
+++ net/mac802154/iface.c
@@ -309,6 +309,7 @@ static int mac802154_slave_close(struct net_device *dev)
 	clear_bit(SDATA_STATE_RUNNING, &sdata->state);
 
 	if (!local->open_count) {
+		flush_workqueue(local->workqueue);
 		hrtimer_cancel(&local->ifs_timer);
 		drv_stop(local);
 	}
