020696f6118d263e5ecb294002c70367028c671d
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-06-13 15:41:19 +0900

    staging: wilc1000: remove itypes.h
    Remove itypes.h that is not needed.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "host_interface.h"
 #include "wilc_oswrapper.h"
-#include "itypes.h"
 #include "coreconfigurator.h"
 
 extern s32 TransportInit(void);
