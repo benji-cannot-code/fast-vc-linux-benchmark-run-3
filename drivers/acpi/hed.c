da1a75c281678015eab8f520b2481155b0709b05
Mathias Krause <minipli@googlemail.com>
2015-06-13 14:26:56 +0200

    ACPI / HED: constify ACPI device ids
    Constify the acpi_hed_ids[] ACPI device IDs array -- no need to have it
    writeable.
    
    Signed-off-by: Mathias Krause <minipli@googlemail.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/hed.c
+++ drivers/acpi/hed.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/acpi.h>
 #include <acpi/hed.h>
 
-static struct acpi_device_id acpi_hed_ids[] = {
+static const struct acpi_device_id acpi_hed_ids[] = {
 	{"PNP0C33", 0},
 	{"", 0},
 };
