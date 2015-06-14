61c386d0c547fd4a01840d6d4858711d19d12fe4
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-06-14 12:49:31 +0900

    ALSA: bebob: obsolete string literal expression for clock source
    The old string literals were completely replaced by new normalized
    representation.
    
    This commit obsoletes it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/bebob/bebob_terratec.c
+++ sound/firewire/bebob/bebob_terratec.c
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "./bebob.h"
 
-static const char *const phase88_rack_clk_src_labels[] = {
-	SND_BEBOB_CLOCK_INTERNAL, "Digital In", "Word Clock"
-};
 static enum snd_bebob_clock_type phase88_rack_clk_src_types[] = {
 	SND_BEBOB_CLOCK_TYPE_INTERNAL,
 	SND_BEBOB_CLOCK_TYPE_EXTERNAL,	/* S/PDIF */
@@ -40,9 +37,6 @@ end:
 	return err;
 }
 
-static const char *const phase24_series_clk_src_labels[] = {
-	SND_BEBOB_CLOCK_INTERNAL, "Digital In"
-};
 static enum snd_bebob_clock_type phase24_series_clk_src_types[] = {
 	SND_BEBOB_CLOCK_TYPE_INTERNAL,
 	SND_BEBOB_CLOCK_TYPE_EXTERNAL,	/* S/PDIF */
@@ -70,7 +64,6 @@ static struct snd_bebob_rate_spec phase_series_rate_spec = {
 /* PHASE 88 Rack FW */
 static struct snd_bebob_clock_spec phase88_rack_clk = {
 	.num	= ARRAY_SIZE(phase88_rack_clk_src_types),
-	.labels	= phase88_rack_clk_src_labels,
 	.types	= phase88_rack_clk_src_types,
 	.get	= &phase88_rack_clk_src_get,
 };
@@ -83,7 +76,6 @@ struct snd_bebob_spec phase88_rack_spec = {
 /* 'PHASE 24 FW' and 'PHASE X24 FW' */
 static struct snd_bebob_clock_spec phase24_series_clk = {
 	.num	= ARRAY_SIZE(phase24_series_clk_src_types),
-	.labels	= phase24_series_clk_src_labels,
 	.types	= phase24_series_clk_src_types,
 	.get	= &phase24_series_clk_src_get,
 };
