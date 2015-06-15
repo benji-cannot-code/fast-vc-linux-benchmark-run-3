825e2c563b3b4273a591e1703fdc276275fc3c30
Michael Ellerman <mpe@ellerman.id.au>
2015-06-15 15:01:32 +1000

    powerpc/powernv: pnv_init_idle_states() should only run on powernv
    Although this init call checks for device tree properties before doing
    anything, it should still only run on powernv machines.
    
    Reviewed-by: Shreyas B Prabhu <shreyas@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/idle.c
+++ arch/powerpc/platforms/powernv/idle.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 
 #include <asm/firmware.h>
+#include <asm/machdep.h>
 #include <asm/opal.h>
 #include <asm/cputhreads.h>
 #include <asm/cpuidle.h>
@@ -290,5 +291,4 @@ out_free:
 out:
 	return 0;
 }
-
-subsys_initcall(pnv_init_idle_states);
+machine_subsys_initcall(powernv, pnv_init_idle_states);
