6a3e10c65ed1a59ace7ac17827fd76c9b0f707db
Sunghoon Cho <ywhsbliss@gmail.com>
2015-06-15 19:42:32 +0900

    staging: wilc1000: remove the warnings on prohibited spaces.
    Remove space prohibited between function name and open parenthesis.
    
    Signed-off-by: Sunghoon Cho <ywhsbliss@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/coreconfigsimulator.h
+++ drivers/staging/wilc1000/coreconfigsimulator.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef CORECONFIGSIMULATOR_H
 #define CORECONFIGSIMULATOR_H
 
-extern s32 CoreConfigSimulatorInit (void);
-extern s32 CoreConfigSimulatorDeInit (void);
+extern s32 CoreConfigSimulatorInit(void);
+extern s32 CoreConfigSimulatorDeInit(void);
 
 #endif
