c044ce415ef8c7c5f16c07dfa2aabdb4fa1c7cf7
Hans de Goede <hdegoede@redhat.com>
2015-06-16 16:28:13 +0200

    ACPI / video: Make acpi_video_unregister_backlight() private
    acpi_video_unregister_backlight() is now only used by video_detect.c
    which is part of the same acpi_video module as video.c, make
    acpi_video_unregister_backlight() private to this module.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Darren Hart <dvhart@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/video_detect.c
+++ drivers/acpi/video_detect.c
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 ACPI_MODULE_NAME("video");
 #define _COMPONENT		ACPI_VIDEO_COMPONENT
 
+void acpi_video_unregister_backlight(void);
+
 static bool backlight_notifier_registered;
 static struct notifier_block backlight_nb;
 
