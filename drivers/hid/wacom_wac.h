69a866673532731cfb4cb2552430fd5cd29a9628
Jason Gerecke <killertofu@gmail.com>
2015-06-15 18:01:42 -0700

    HID: wacom: Treat features->device_type values as flags
    The USB devices that this driver has historically supported segregate the
    pen and touch portions of the tablet. Oftentimes the segregation would be
    done at the interface level, though on occasion (e.g. Cintiq 24HDT) the
    tablet would combine two totally independent USB devices behind an internal
    USB hub. Because pen and touch never shared the same interface, it made
    sense for the 'device_type' to store a single value: "pen" or "touch".
    
    Recently, however, some I2C devices have been created which combine the
    two. A first step to accomodating this is to expand 'device_type' so that
    it can represent two (or potentially more) types simultaneously. To do
    this, we treat it as a bitfield and set/check individual bits rather
    than using the '=' and '==' operators.
    
    This should not result in any functional change since no supported devices
    (that I'm aware of, at least) have HID descriptors that indicate both
    pen and touch reports on a single interface.
    
    Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/wacom_wac.h
+++ drivers/hid/wacom_wac.h
@@ -73,6 +73,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WACOM_QUIRK_MONITOR		0x0004
 #define WACOM_QUIRK_BATTERY		0x0008
 
+/* device types */
+#define WACOM_DEVICETYPE_NONE           0x0000
+#define WACOM_DEVICETYPE_PEN            0x0001
+#define WACOM_DEVICETYPE_TOUCH          0x0002
+
 #define WACOM_VENDORDEFINED_PEN		0xff0d0001
 
 #define WACOM_PEN_FIELD(f)	(((f)->logical == HID_DG_STYLUS) || \
