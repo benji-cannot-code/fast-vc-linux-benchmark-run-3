8af0e269d65b931b2280f30419a68ad04d82be3d
Pablo Neira Ayuso <pablo@netfilter.org>
2015-06-17 10:28:26 -0500

    netfilter: use forward declaration instead of including linux/proc_fs.h
    We don't need to pull the full definitions in that file, a simple forward
    declaration is enough.
    
    Moreover, include linux/procfs.h from nf_synproxy_core, otherwise this hits a
    compilation error due to missing declarations, ie.
    
    net/netfilter/nf_synproxy_core.c: In function ‘synproxy_proc_init’:
    net/netfilter/nf_synproxy_core.c:326:2: error: implicit declaration of function ‘proc_create’ [-Werror=implicit-function-declaration]
      if (!proc_create("synproxy", S_IRUGO, net->proc_net_stat,
      ^
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>

--- include/net/netns/netfilter.h
+++ include/net/netns/netfilter.h
@@ -2,9 +2,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NETNS_NETFILTER_H
 #define __NETNS_NETFILTER_H
 
-#include <linux/proc_fs.h>
 #include <linux/netfilter.h>
 
+struct proc_dir_entry;
 struct nf_logger;
 
 struct netns_nf {
