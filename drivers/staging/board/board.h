70c3e8a8f95246daef6e5b56049a325c6c78cb0c
Geert Uytterhoeven <geert+renesas@glider.be>
2015-06-17 10:38:52 +0200

    staging: board: Add support for translating hwirq to virq numbers
    As of commit 9a1091ef0017c40a ("irqchip: gic: Support hierarchy irq
    domain."), GIC IRQ numbers are virtual, breaking hardcoded hardware IRQ
    numbers in platform device resources.
    
    Add support for translating hardware IRQ numbers to virtual IRQ numbers,
    and fixing up platform device resources with hardcoded IRQ numbers.
    
    Add a copyright header, including the original author.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/board/board.h
+++ drivers/staging/board/board.h
@@ -1,11 +1,16 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __BOARD_H__
 #define __BOARD_H__
+
 #include <linux/init.h>
 #include <linux/of.h>
 
+struct resource;
+
 bool board_staging_dt_node_available(const struct resource *resource,
 				     unsigned int num_resources);
+int board_staging_gic_setup_xlate(const char *gic_match, unsigned int base);
+void board_staging_gic_fixup_resources(struct resource *res, unsigned int nres);
 
 #define board_staging(str, fn)			\
 static int __init runtime_board_check(void)	\
