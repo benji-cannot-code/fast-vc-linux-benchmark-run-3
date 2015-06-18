5c5d2b4d11bff177574c11d89b8e8a410509804a
Al Viro <viro@zeniv.linux.org.uk>
2015-06-18 16:05:18 -0400

    ufs_trunc_branch(): never call with offsets != NULL && depth2 == 0
    For calls in __ufs_truncate_blocks() it's just a matter of not
    incrementing offsets[0] and not making that call - immediately
    following loop will be executed one extra time and we'll be just
    fine.  For recursive call in ufs_trunc_branch() itself, just
    assing NULL to offsets if we would be about to make such call.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ufs/inode.c
+++ fs/ufs/inode.c
@@ -1076,7 +1076,7 @@ static void ufs_trunc_branch(struct inode *inode, unsigned *offsets, int depth2,
 	struct ufs_inode_info *ufsi = UFS_I(inode);
 	struct ufs_buffer_head *ubh;
 	u64 tmp;
-	bool free_it = !offsets || !depth2;
+	bool free_it = !offsets;
 	unsigned from = offsets ? *offsets++ : 0;
 	unsigned i;
 
@@ -1092,9 +1092,11 @@ static void ufs_trunc_branch(struct inode *inode, unsigned *offsets, int depth2,
 	}
 
 	if (--depth) {
+		if (!--depth2)
+			offsets = NULL;
 		for (i = from ; i < uspi->s_apb ; i++, offsets = NULL) {
 			void *ind = ubh_get_data_ptr(uspi, ubh, i);
-			ufs_trunc_branch(inode, offsets, depth2 - 1, depth, ind);
+			ufs_trunc_branch(inode, offsets, depth2, depth, ind);
 			ubh_mark_buffer_dirty(ubh);
 		}
 	} else {
@@ -1238,7 +1240,8 @@ static void __ufs_truncate_blocks(struct inode *inode)
 		ufs_trunc_direct(inode);
 		offsets[0] = UFS_IND_BLOCK;
 	} else {
-		ufs_trunc_branch(inode, offsets + 1, depth2, depth - 1,
+		if (depth2)
+			ufs_trunc_branch(inode, offsets + 1, depth2, depth - 1,
 			   ufs_get_direct_data_ptr(uspi, ufsi, offsets[0]++));
 	}
 	for (i = offsets[0]; i <= UFS_TIND_BLOCK; i++) {
