90500927726b59b33f9d72e6a7c662c9c8345712
Marc Zyngier <marc.zyngier@arm.com>
2015-06-18 15:50:31 +0100

    KVM: arm/arm64: vgic: Remove useless arm-gic.h #include
    Back in the days, vgic.c used to have an intimate knowledge of
    the actual GICv2. These days, this has been abstracted away into
    hardware-specific backends.
    
    Remove the now useless arm-gic.h #include directive, making it
    clear that GICv2 specific code doesn't belong here.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- virt/kvm/arm/vgic.c
+++ virt/kvm/arm/vgic.c
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/uaccess.h>
 
-#include <linux/irqchip/arm-gic.h>
-
 #include <asm/kvm_emulate.h>
 #include <asm/kvm_arm.h>
 #include <asm/kvm_mmu.h>
