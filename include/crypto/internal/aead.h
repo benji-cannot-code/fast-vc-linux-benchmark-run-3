3a357da38e145b4d0931d95fd8439a43fe643184
Herbert Xu <herbert@gondor.apana.org.au>
2015-06-18 14:00:49 +0800

    crypto: aead - Fix aead_instance struct size
    The struct aead_instance is meant to extend struct crypto_instance
    by incorporating the extra members of struct aead_alg.  However,
    the current layout which is copied from shash/ahash does not specify
    the struct fully.  In particular only aead_alg is present.
    
    For shash/ahash this works because users there add extra headroom
    to sizeof(struct crypto_instance) when allocating the instance.
    Unfortunately for aead, this bit was lost when the new aead_instance
    was added.
    
    Rather than fixing it like shash/ahash, this patch simply expands
    struct aead_instance to contain what is supposed to be there, i.e.,
    adding struct crypto_instance.
    
    In order to not break existing AEAD users, this is done through an
    anonymous union.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/internal/aead.h
+++ include/crypto/internal/aead.h
@@ -16,12 +16,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <crypto/aead.h>
 #include <crypto/algapi.h>
+#include <linux/stddef.h>
 #include <linux/types.h>
 
 struct rtattr;
 
 struct aead_instance {
-	struct aead_alg alg;
+	union {
+		struct {
+			char head[offsetof(struct aead_alg, base)];
+			struct crypto_instance base;
+		} s;
+		struct aead_alg alg;
+	};
 };
 
 struct crypto_aead_spawn {
