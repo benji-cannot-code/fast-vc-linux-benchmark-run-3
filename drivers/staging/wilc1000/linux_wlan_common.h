3cf64e50cf0237a944471b73e93b5487c8f8abb5
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-06-19 09:06:28 +0900

    staging: wilc1000: fix checkpatch warning
    Fix warning founded by checkpatch.pl.
    WARNING: __func__ should be used instead of gcc specific __FUNCTION__
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan_common.h
+++ drivers/staging/wilc1000/linux_wlan_common.h
@@ -55,7 +55,7 @@ extern atomic_t DEBUG_LEVEL;
 	do {								\
 		if ((atomic_read(&DEBUG_LEVEL) & DEBUG) &&		\
 		   ((atomic_read(&REGION)) & (region))) {		\
-			printk("DBG [%s: %d]", __FUNCTION__, __LINE__);	\
+			printk("DBG [%s: %d]", __func__, __LINE__);	\
 			printk(__VA_ARGS__);				\
 		}							\
 	} while (0)
@@ -64,7 +64,7 @@ extern atomic_t DEBUG_LEVEL;
 	do {								\
 		if ((atomic_read(&DEBUG_LEVEL) & INFO) &&		\
 		   ((atomic_read(&REGION)) & (region))) {		\
-			printk("INFO [%s]", __FUNCTION__);		\
+			printk("INFO [%s]", __func__);			\
 			printk(__VA_ARGS__);				\
 		}							\
 	} while (0)
@@ -73,7 +73,7 @@ extern atomic_t DEBUG_LEVEL;
 	do {								\
 		if ((atomic_read(&DEBUG_LEVEL) & WRN) &&		\
 		   ((atomic_read(&REGION)) & (region))) {		\
-			printk("WRN [%s: %d]", __FUNCTION__, __LINE__);	\
+			printk("WRN [%s: %d]", __func__, __LINE__);	\
 			printk(__VA_ARGS__);				\
 		}							\
 	} while (0)
@@ -81,7 +81,7 @@ extern atomic_t DEBUG_LEVEL;
 #define PRINT_ER(...)							\
 	do {								\
 		if ((atomic_read(&DEBUG_LEVEL) & ERR)) {		\
-			printk("ERR [%s: %d]", __FUNCTION__, __LINE__);	\
+			printk("ERR [%s: %d]", __func__, __LINE__);	\
 			printk(__VA_ARGS__);				\
 		}							\
 	} while (0)
@@ -97,7 +97,7 @@ extern atomic_t DEBUG_LEVEL;
 #define PRINT_D(region, ...)						\
 	do {								\
 		if (DEBUG == 1 && ((REGION)&(region))) {		\
-			printk("DBG [%s: %d]", __FUNCTION__, __LINE__); \
+			printk("DBG [%s: %d]", __func__, __LINE__);	\
 			printk(__VA_ARGS__);				\
 		}							\
 	} while (0)
@@ -105,7 +105,7 @@ extern atomic_t DEBUG_LEVEL;
 #define PRINT_INFO(region, ...)						\
 	do {								\
 		if (INFO == 1 && ((REGION)&(region))) {			\
-			printk("INFO [%s]", __FUNCTION__);		\
+			printk("INFO [%s]", __func__);			\
 			printk(__VA_ARGS__);				\
 		}							\
 	} while (0)
@@ -113,14 +113,14 @@ extern atomic_t DEBUG_LEVEL;
 #define PRINT_WRN(region, ...)						\
 	do {								\
 		if (WRN == 1 && ((REGION)&(region))) {			\
-			printk("WRN [%s: %d]", __FUNCTION__, __LINE__); \
+			printk("WRN [%s: %d]", __func__, __LINE__);	\
 			printk(__VA_ARGS__);				\
 		}							\
 	} while (0)
 
 #define PRINT_ER(...)							\
 	do {								\
-		printk("ERR [%s: %d]", __FUNCTION__, __LINE__);		\
+		printk("ERR [%s: %d]", __func__, __LINE__);		\
 		printk(__VA_ARGS__);					\
 	} while (0)
 #endif
