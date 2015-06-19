463119b9b5db0841562ab343763a5acacae4790b
Al Viro <viro@zeniv.linux.org.uk>
2015-06-18 20:09:39 -0400

    move marking inode dirty to the end of __ufs_truncate_blocks()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ufs/inode.c
+++ fs/ufs/inode.c
@@ -1028,7 +1028,6 @@ static void ufs_trunc_direct(struct inode *inode)
 	frag1 = ufs_fragnum (frag1);
 
 	ufs_free_fragments(inode, tmp + frag1, frag2);
-	mark_inode_dirty(inode);
 
 next1:
 	/*
@@ -1044,7 +1043,6 @@ next1:
 		write_sequnlock(&ufsi->meta_lock);
 
 		free_data(&ctx, tmp, uspi->s_fpb);
-		mark_inode_dirty(inode);
 	}
 
 	free_data(&ctx, 0, 0);
@@ -1065,7 +1063,6 @@ next1:
 	write_sequnlock(&ufsi->meta_lock);
 
 	ufs_free_fragments (inode, tmp, frag4);
-	mark_inode_dirty(inode);
  next3:
 
 	UFSD("EXIT: ino %lu\n", inode->i_ino);
@@ -1105,7 +1102,6 @@ static void free_full_branch(struct inode *inode, u64 ind_block, int depth)
 				write_sequnlock(&UFS_I(inode)->meta_lock);
 				ubh_mark_buffer_dirty(ubh);
 				free_data(&ctx, block, uspi->s_fpb);
-				mark_inode_dirty(inode);
 			}
 		}
 		free_data(&ctx, 0, 0);
@@ -1113,7 +1109,6 @@ static void free_full_branch(struct inode *inode, u64 ind_block, int depth)
 
 	ubh_bforget(ubh);
 	ufs_free_blocks(inode, ind_block, uspi->s_fpb);
-	mark_inode_dirty(inode);
 }
 
 static void free_branch_tail(struct inode *inode, unsigned from, struct ufs_buffer_head *ubh, int depth)
@@ -1146,7 +1141,6 @@ static void free_branch_tail(struct inode *inode, unsigned from, struct ufs_buff
 				write_sequnlock(&UFS_I(inode)->meta_lock);
 				ubh_mark_buffer_dirty(ubh);
 				free_data(&ctx, block, uspi->s_fpb);
-				mark_inode_dirty(inode);
 			}
 		}
 		free_data(&ctx, 0, 0);
@@ -1284,6 +1278,7 @@ static void __ufs_truncate_blocks(struct inode *inode)
 		}
 	}
 	ufsi->i_lastfrag = DIRECT_FRAGMENT;
+	mark_inode_dirty(inode);
 	mutex_unlock(&ufsi->truncate_mutex);
 }
 
