4e55bc75d68dca586502c22f286073120249fc04
Daniel Vetter <daniel.vetter@ffwll.ch>
2015-06-19 21:17:42 +0200

    drm/i915: Update DRIVER_DATE to 20150619
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
-#define DRIVER_DATE		"20150522"
+#define DRIVER_DATE		"20150619"
 
 #undef WARN_ON
 /* Many gcc seem to no see through this and fall over :( */
