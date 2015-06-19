17f92e7aadbab7de5fb492b4b6765f90751d61ee
Stephen Boyd <sboyd@codeaurora.org>
2015-06-19 15:00:46 -0700

    clk: sirf: Properly include clk.h
    Clock provider drivers generally shouldn't include clk.h because
    it's the consumer API. Move the include of clk.h into
    clk-common.c because that's the only file that's really using
    clk.h, even if it's included into the atlas6 and prima2 files.
    
    Cc: Barry Song <Baohua.Song@csr.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/sirf/clk-common.c
+++ drivers/clk/sirf/clk-common.c
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Licensed under GPLv2 or later.
  */
 
+#include <linux/clk.h>
+
 #define KHZ     1000
 #define MHZ     (KHZ * KHZ)
 
