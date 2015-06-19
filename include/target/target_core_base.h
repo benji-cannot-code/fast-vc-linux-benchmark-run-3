65de74a5f9f5473ad98a111b7a2bf8662bd5abfc
Christoph Hellwig <hch@lst.de>
2015-06-19 15:14:39 +0200

    target: consolidate version defines
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Andy Grover <agrover@redhat.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- include/target/target_core_base.h
+++ include/target/target_core_base.h
@@ -11,8 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <net/tcp.h>
 
-#define TARGET_CORE_MOD_VERSION		"v4.1.0"
-#define TARGET_CORE_VERSION		TARGET_CORE_MOD_VERSION
+#define TARGET_CORE_VERSION		"v4.1.0"
 
 /*
  * By default we use 32-byte CDBs in TCM Core and subsystem plugin code.
