00064a98ba8f02941564462ff21db0cc86553a42
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2015-06-19 17:23:32 +0100

    extcon: arizona: Ensure variables are set for headphone detection
    The detecting flag really refers to the microphone detection stage and
    as such should be cleared before arizona_identify_headphones is called.
    Also the mic flag should be set before identify headphones is called as
    well.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/extcon-arizona.c
+++ drivers/extcon/extcon-arizona.c
@@ -766,9 +766,10 @@ static void arizona_micd_timeout_work(struct work_struct *work)
 	mutex_lock(&info->lock);
 
 	dev_dbg(info->arizona->dev, "MICD timed out, reporting HP\n");
-	arizona_identify_headphone(info);
 
 	info->detecting = false;
+
+	arizona_identify_headphone(info);
 
 	arizona_stop_mic(info);
 
@@ -835,6 +836,9 @@ static void arizona_micd_detect(struct work_struct *work)
 
 	/* If we got a high impedence we should have a headset, report it. */
 	if (info->detecting && (val & ARIZONA_MICD_LVL_8)) {
+		info->mic = true;
+		info->detecting = false;
+
 		arizona_identify_headphone(info);
 
 		ret = extcon_set_cable_state_(info->edev,
@@ -850,8 +854,6 @@ static void arizona_micd_detect(struct work_struct *work)
 				ret);
 		}
 
-		info->mic = true;
-		info->detecting = false;
 		goto handled;
 	}
 
@@ -864,9 +866,10 @@ static void arizona_micd_detect(struct work_struct *work)
 	if (info->detecting && (val & MICD_LVL_1_TO_7)) {
 		if (info->jack_flips >= info->micd_num_modes * 10) {
 			dev_dbg(arizona->dev, "Detected HP/line\n");
-			arizona_identify_headphone(info);
 
 			info->detecting = false;
+
+			arizona_identify_headphone(info);
 
 			arizona_stop_mic(info);
 		} else {
