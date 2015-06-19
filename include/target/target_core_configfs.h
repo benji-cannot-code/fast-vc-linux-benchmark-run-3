3792ac2a15c2a282895bfa80fe0838c344099035
Christoph Hellwig <hch@lst.de>
2015-06-19 15:14:40 +0200

    target: remove unused TARGET_CORE_CONFIG_ROOT define
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Andy Grover <agrover@redhat.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- include/target/target_core_configfs.h
+++ include/target/target_core_configfs.h
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
-#define TARGET_CORE_CONFIG_ROOT	"/sys/kernel/config"
-
 #define TARGET_CORE_NAME_MAX_LEN	64
 #define TARGET_FABRIC_NAME_SIZE		32
 
