2aff3af579a7f53b24ad86e4b1ac247666b0a80d
Stephen Boyd <sboyd@codeaurora.org>
2015-06-19 15:00:46 -0700

    clk: mvebu: Remove clk.h include
    Clock provider drivers generally shouldn't include clk.h because
    it's the consumer API. Only include the header if necessary. The
    clkdev.h include isn't used here either, so drop it and add in
    slab.h to keep things compiling.
    
    Cc: Boris Brezillon <boris.brezillon@free-electrons.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    Cc: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/mvebu/clk-cpu.c
+++ drivers/clk/mvebu/clk-cpu.c
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * warranty of any kind, whether express or implied.
  */
 #include <linux/kernel.h>
-#include <linux/clkdev.h>
+#include <linux/slab.h>
+#include <linux/clk.h>
 #include <linux/clk-provider.h>
 #include <linux/of_address.h>
 #include <linux/io.h>
