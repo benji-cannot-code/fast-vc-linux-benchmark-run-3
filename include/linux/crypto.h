54e032f6b79a43cf620d0bd59a182c448dbcb479
Herbert Xu <herbert@gondor.apana.org.au>
2015-06-22 10:31:40 +0800

    crypto: algif_aead - Temporarily disable all AEAD algorithms
    As the AEAD conversion is still ongoing, we do not yet wish to
    export legacy AEAD implementations to user-space, as their calling
    convention will change.
    
    This patch actually disables all AEAD algorithms because some of
    them (e.g., cryptd) will need to be modified to propagate this flag.
    
    Subsequent patches will reenable them on an individual basis.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/linux/crypto.h
+++ include/linux/crypto.h
@@ -103,6 +103,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CRYPTO_ALG_INTERNAL		0x00002000
 
 /*
+ * Temporary flag used to prevent legacy AEAD implementations from
+ * being used by user-space.
+ */
+#define CRYPTO_ALG_AEAD_NEW		0x00004000
+
+/*
  * Transform masks and values (for crt_flags).
  */
 #define CRYPTO_TFM_REQ_MASK		0x000fff00
