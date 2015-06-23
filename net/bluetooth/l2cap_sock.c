201c4fc4abe0d2342d2f248e2fd2f26863066d05
Dean Jenkins <Dean_Jenkins@mentor.com>
2015-06-23 17:59:38 +0100

    Bluetooth: __l2cap_wait_ack() use msecs_to_jiffies()
    Use msecs_to_jiffies() instead of using HZ so that it
    is easier to specify the time in milliseconds.
    
    Also add a #define L2CAP_WAIT_ACK_POLL_PERIOD to specify the 200ms
    polling period so that it is defined in a single place.
    
    Signed-off-by: Dean Jenkins <Dean_Jenkins@mentor.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/bluetooth/l2cap_sock.c
+++ net/bluetooth/l2cap_sock.c
@@ -1059,7 +1059,7 @@ static int __l2cap_wait_ack(struct sock *sk, struct l2cap_chan *chan)
 {
 	DECLARE_WAITQUEUE(wait, current);
 	int err = 0;
-	int timeo = HZ/5;
+	int timeo = L2CAP_WAIT_ACK_POLL_PERIOD;
 
 	add_wait_queue(sk_sleep(sk), &wait);
 	set_current_state(TASK_INTERRUPTIBLE);
@@ -1067,7 +1067,7 @@ static int __l2cap_wait_ack(struct sock *sk, struct l2cap_chan *chan)
 		BT_DBG("Waiting for %d ACKs", chan->unacked_frames);
 
 		if (!timeo)
-			timeo = HZ/5;
+			timeo = L2CAP_WAIT_ACK_POLL_PERIOD;
 
 		if (signal_pending(current)) {
 			err = sock_intr_errno(timeo);
