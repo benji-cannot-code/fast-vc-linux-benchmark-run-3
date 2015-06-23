6dea208f767b61cd2b08c245a7d56ceb76652547
Tomasz Duszynski <tduszyns@gmail.com>
2015-06-23 20:45:48 +0200

    iio: pressure: ms5611: add support for MS5607 temperature and pressure sensor
    MS5607 is temperature and pressure sensor which hardware is similar to MS5611.
    Both sensors share command protocol and support both I2C and SPI serial
    protocols. They only differ in compensation algorithms.
    
    Signed-off-by: Tomasz Duszynski <tduszyns@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/pressure/ms5611.h
+++ drivers/iio/pressure/ms5611.h
@@ -28,6 +28,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MS5611_PROM_WORDS_NB		8
 
+enum {
+	MS5611,
+	MS5607,
+};
+
+struct ms5611_chip_info {
+	u16 prom[MS5611_PROM_WORDS_NB];
+
+	int (*temp_and_pressure_compensate)(struct ms5611_chip_info *chip_info,
+					    s32 *temp, s32 *pressure);
+};
+
 struct ms5611_state {
 	void *client;
 	struct mutex lock;
@@ -37,9 +49,9 @@ struct ms5611_state {
 	int (*read_adc_temp_and_pressure)(struct device *dev,
 					  s32 *temp, s32 *pressure);
 
-	u16 prom[MS5611_PROM_WORDS_NB];
+	struct ms5611_chip_info *chip_info;
 };
 
-int ms5611_probe(struct iio_dev *indio_dev, struct device *dev);
+int ms5611_probe(struct iio_dev *indio_dev, struct device *dev, int type);
 
 #endif /* _MS5611_H */
