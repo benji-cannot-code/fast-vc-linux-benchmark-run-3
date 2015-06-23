ff294d7a73c855bc5ef6545d0088d5a6ca6057af
Stephen Boyd <sboyd@codeaurora.org>
2015-06-22 17:13:49 -0700

    clk: Remove clk.h from clk-provider.h
    Remove clk.h from clk-provider.h so that we can clearly split clk
    providers from clk consumers. This will allow us to quickly
    detect when clock providers are using the consumer APIs by
    looking at the includes.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_CLK_PROVIDER_H
 #define __LINUX_CLK_PROVIDER_H
 
-#include <linux/clk.h>
 #include <linux/io.h>
 #include <linux/of.h>
 
@@ -34,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_GET_ACCURACY_NOCACHE BIT(8) /* do not use the cached clk accuracy */
 #define CLK_RECALC_NEW_RATES	BIT(9) /* recalc rates after notifications */
 
+struct clk;
 struct clk_hw;
 struct clk_core;
 struct dentry;
