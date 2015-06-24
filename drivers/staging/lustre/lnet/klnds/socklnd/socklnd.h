f88353f353a591e11ba0e421cbcbec0ceeb013b3
Oleg Drokin <green@linuxhacker.ru>
2015-06-24 10:07:45 -0400

    staging/lustre/lnet: Move asm/irq.h include after linux includes
    Apparently m86k cannot build if you include asm/irq.h before
    linux/* includes and fixing it there is hard.
    So just move asm/irq.h include to where it does not cause any problems.
    
    Thanks to Geert Uytterhoeven for getting to the root of it.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Compile-Tested-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/klnds/socklnd/socklnd.h
+++ drivers/staging/lustre/lnet/klnds/socklnd/socklnd.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUG_PORTAL_ALLOC
 #define DEBUG_SUBSYSTEM S_LND
 
-#include <asm/irq.h>
 #include <linux/crc32.h>
 #include <linux/errno.h>
 #include <linux/if.h>
@@ -48,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysctl.h>
 #include <linux/uio.h>
 #include <linux/unistd.h>
+#include <asm/irq.h>
 #include <net/sock.h>
 #include <net/tcp.h>
 
