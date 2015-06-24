8594ca5c1d4e8d1c47710c2bd020746bfbf3728a
Oleg Drokin <green@linuxhacker.ru>
2015-06-24 10:09:24 -0400

    staging/lustre/lov: Move target sysfs symlink removal to LOV freeing
    This helps to avoid use after free on unmount.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_obd.c
+++ drivers/staging/lustre/lustre/lov/lov_obd.c
@@ -108,6 +108,10 @@ static void lov_putref(struct obd_device *obd)
 			/* Disconnect */
 			__lov_del_obd(obd, tgt);
 		}
+
+		if (lov->lov_tgts_kobj)
+			kobject_put(lov->lov_tgts_kobj);
+
 	} else {
 		mutex_unlock(&lov->lov_lock);
 	}
@@ -322,9 +326,6 @@ static int lov_disconnect(struct obd_export *exp)
 			lov_del_target(obd, i, NULL, lov->lov_tgts[i]->ltd_gen);
 		}
 	}
-
-	if (lov->lov_tgts_kobj)
-		kobject_put(lov->lov_tgts_kobj);
 
 	obd_putref(obd);
 
