e4ebb1f2c96da735f44fc890ff338211ad920ae9
Selvan Mani <smani@micron.com>
2015-06-24 08:48:46 -0600

    mtip32xx: Fix accessing freed memory
    In mtip_pci_remove(), driver data 'dd' is accessed after freeing it. This
    is a residue of SRSI code cleanup in the patch 016a41c38821 "mtip32xx: fix
    crash on surprise removal of the drive". Removed the bit flags
    MTIP_DDF_REMOVE_DONE_BIT and MTIP_PF_SR_CLEANUP_BIT.
    
    Reported-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Vignesh Gunasekaran <vgunasekaran@micron.com>
    Signed-off-by: Selvan Mani <smani@micron.com>
    Signed-off-by: Asai Thambi S P <asamymuthupa@micron.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/mtip32xx/mtip32xx.h
+++ drivers/block/mtip32xx/mtip32xx.h
@@ -143,7 +143,6 @@ enum {
 	MTIP_PF_SVC_THD_ACTIVE_BIT  = 4,
 	MTIP_PF_ISSUE_CMDS_BIT      = 5,
 	MTIP_PF_REBUILD_BIT         = 6,
-	MTIP_PF_SR_CLEANUP_BIT      = 7,
 	MTIP_PF_SVC_THD_STOP_BIT    = 8,
 
 	/* below are bit numbers in 'dd_flag' defined in driver_data */
@@ -151,7 +150,6 @@ enum {
 	MTIP_DDF_REMOVE_PENDING_BIT = 1,
 	MTIP_DDF_OVER_TEMP_BIT      = 2,
 	MTIP_DDF_WRITE_PROTECT_BIT  = 3,
-	MTIP_DDF_REMOVE_DONE_BIT    = 4,
 	MTIP_DDF_CLEANUP_BIT        = 5,
 	MTIP_DDF_RESUME_BIT         = 6,
 	MTIP_DDF_INIT_DONE_BIT      = 7,
