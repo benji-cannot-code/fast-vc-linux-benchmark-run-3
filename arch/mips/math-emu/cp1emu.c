b04eb73c48314c249101771bf12faec20a3dcfd8
Markos Chandras <markos.chandras@imgtec.com>
2015-06-24 09:52:00 +0100

    MIPS: Fix branch emulation for BLTC and BGEC instructions
    Commits f1b44067c19258b7614e3cd09dfe8d8e12ff5895 ("MIPS: Emulate the
    new MIPS R6 B{L,G}T{Z,}{AL,}C instructions") and commit
    a8ff66f52d3f17b5ae793955270675c197f73d6c ("MIPS: Emulate the new MIPS
    R6 B{L,G}E{Z,}{AL,}C instructions") added support for emulating various
    branch compact instructions. However, it missed the case for those which
    use the old BLEZL and BGTZL opcodes leading to random crashes when the R6
    emulator is disabled. We fix this by ensuring that the 'rt' field is not
    zero which is always true for these branch compact instructions.
    
    Fixes: f1b44067c192 ("MIPS: Emulate the new MIPS R6 B{L,G}T{Z,}{AL,}C instructions")
    Fixes: a8ff66f52d3f ("MIPS: Emulate the new MIPS R6 B{L,G}E{Z,}{AL,}C instructions")
    Cc: <stable@vger.kernel.org> # 4.0+
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: Markos Chandras <markos.chandras@imgtec.com>
    Patchwork: https://patchwork.linux-mips.org/patch/10582/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/math-emu/cp1emu.c
+++ arch/mips/math-emu/cp1emu.c
@@ -552,7 +552,7 @@ static int isBranchInstr(struct pt_regs *regs, struct mm_decoded_insn dec_insn,
 				dec_insn.next_pc_inc;
 		return 1;
 	case blezl_op:
-		if (NO_R6EMU)
+		if (!insn.i_format.rt && NO_R6EMU)
 			break;
 	case blez_op:
 
@@ -589,7 +589,7 @@ static int isBranchInstr(struct pt_regs *regs, struct mm_decoded_insn dec_insn,
 				dec_insn.next_pc_inc;
 		return 1;
 	case bgtzl_op:
-		if (NO_R6EMU)
+		if (!insn.i_format.rt && NO_R6EMU)
 			break;
 	case bgtz_op:
 		/*
