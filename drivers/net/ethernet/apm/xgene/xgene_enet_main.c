1f2b23b04190f2752851712395df468cd54f4706
Stephen Rothwell <sfr@canb.auug.org.au>
2015-06-24 17:29:51 +1000

    drivers: net: xgene: fix for ACPI support without ACPI
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/apm/xgene/xgene_enet_main.c
+++ drivers/net/ethernet/apm/xgene/xgene_enet_main.c
@@ -872,6 +872,7 @@ static const struct net_device_ops xgene_ndev_ops = {
 	.ndo_set_mac_address = xgene_enet_set_mac_address,
 };
 
+#ifdef CONFIG_ACPI
 static int xgene_get_port_id_acpi(struct device *dev,
 				  struct xgene_enet_pdata *pdata)
 {
@@ -887,6 +888,7 @@ static int xgene_get_port_id_acpi(struct device *dev,
 
 	return 0;
 }
+#endif
 
 static int xgene_get_port_id_dt(struct device *dev, struct xgene_enet_pdata *pdata)
 {
