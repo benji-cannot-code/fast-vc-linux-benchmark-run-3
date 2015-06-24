4fbc57a045d7f47f24532301e902695dd40a1e07
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2015-06-24 14:32:21 +0300

    mmc: block: don't use parameter prefix if built as module
    It's excessive to use prefix for the parameters when you do
    modprobe mmc-block mmcblk.perdev_minors=16
    
    Make this available only for built-in case.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -48,10 +48,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "queue.h"
 
 MODULE_ALIAS("mmc:block");
+
+#ifdef KERNEL
 #ifdef MODULE_PARAM_PREFIX
 #undef MODULE_PARAM_PREFIX
 #endif
 #define MODULE_PARAM_PREFIX "mmcblk."
+#endif
 
 #define INAND_CMD38_ARG_EXT_CSD  113
 #define INAND_CMD38_ARG_ERASE    0x00
