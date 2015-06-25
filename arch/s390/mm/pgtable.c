21ff256ec9173f37af251832508bb5fb8fc5cb14
Dominik Dingel <dingel@linux.vnet.ibm.com>
2015-06-25 14:59:52 -0700

    s390/mm: change HPAGE_SHIFT type to int
    With making HPAGE_SHIFT an unsigned integer we also accidentally changed
    pageblock_order.  In order to avoid compiler warnings we make
    HPAGE_SHFIT an int again.
    
    Signed-off-by: Dominik Dingel <dingel@linux.vnet.ibm.com>
    Suggested-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/s390/mm/pgtable.c
+++ arch/s390/mm/pgtable.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ALLOC_ORDER	2
 #define FRAG_MASK	0x03
 
-unsigned int HPAGE_SHIFT;
+int HPAGE_SHIFT;
 
 unsigned long *crst_table_alloc(struct mm_struct *mm)
 {
