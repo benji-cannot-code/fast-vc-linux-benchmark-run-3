8aeb0830ed3c9d17d4d719e21c6a39a85f5557ec
Alex Dowad <alexinbeijing@gmail.com>
2015-06-25 15:02:52 -0700

    checkpatch: make types found in a source file/patch local
    checkpatch uses various cues in its input patches and files to discover
    the names of user-defined types and modifiers.  It then uses that
    information when processing expressions to discover potential style
    issues.
    
    Unfortunately, in rare cases, this means that checkpatch may give
    different results if you run it on several input files in one execution,
    or one by one!
    
    The reason is that it may identify a type (or something that looks like a
    type) in one file, and then carry this information over when processing a
    different file.
    
    For example, drivers/staging/media/bcm2048/radio-bcm2048.c contains this
    line (in a macro):
    
    	size value;
    
    and drivers/staging/media/davinci_vpfe/vpfe_video.c has this line:
    
    	while (size * *nbuffers > vpfe_dev->video_limit)
    
    If checkpatch processes these 2 files in a single command like:
    	./scripts/checkpatch.pl -f $file1 $file2
    the (spurious) "size" type detected in the first file will cause it to flag
    the second file for improper use of the pointer dereference operator.
    
    To fix this, store types and modifiers found in a file in separate arrays
    from built-in ones, and reset the arrays of types and modifiers found in
    files at the beginning of each new source file.
    
    Signed-off-by: Alex Dowad <alexinbeijing@gmail.com>
    Signed-off-by: Joe Perches <joe@perches.com>
    Acked-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -424,6 +424,7 @@ our @typeList = (
 	qr{${Ident}_handler_fn},
 	@typeListMisordered,
 );
+our @typeListFile = ();
 our @typeListWithAttr = (
 	@typeList,
 	qr{struct\s+$InitAttribute\s+$Ident},
@@ -433,6 +434,7 @@ our @typeListWithAttr = (
 our @modifierList = (
 	qr{fastcall},
 );
+our @modifierListFile = ();
 
 our @mode_permission_funcs = (
 	["module_param", 3],
@@ -516,8 +518,8 @@ if ($codespell) {
 $misspellings = join("|", sort keys %spelling_fix) if keys %spelling_fix;
 
 sub build_types {
-	my $mods = "(?x:  \n" . join("|\n  ", @modifierList) . "\n)";
-	my $all = "(?x:  \n" . join("|\n  ", @typeList) . "\n)";
+	my $mods = "(?x:  \n" . join("|\n  ", (@modifierList, @modifierListFile)) . "\n)";
+	my $all = "(?x:  \n" . join("|\n  ", (@typeList, @typeListFile)) . "\n)";
 	my $Misordered = "(?x:  \n" . join("|\n  ", @typeListMisordered) . "\n)";
 	my $allWithAttr = "(?x:  \n" . join("|\n  ", @typeListWithAttr) . "\n)";
 	$Modifier	= qr{(?:$Attribute|$Sparse|$mods)};
@@ -749,6 +751,9 @@ for my $filename (@ARGV) {
 	@fixed_inserted = ();
 	@fixed_deleted = ();
 	$fixlinenr = -1;
+	@modifierListFile = ();
+	@typeListFile = ();
+	build_types();
 }
 
 exit($exit);
@@ -1613,13 +1618,13 @@ sub possible {
 			for my $modifier (split(' ', $possible)) {
 				if ($modifier !~ $notPermitted) {
 					warn "MODIFIER: $modifier ($possible) ($line)\n" if ($dbg_possible);
-					push(@modifierList, $modifier);
+					push(@modifierListFile, $modifier);
 				}
 			}
 
 		} else {
 			warn "POSSIBLE: $possible ($line)\n" if ($dbg_possible);
-			push(@typeList, $possible);
+			push(@typeListFile, $possible);
 		}
 		build_types();
 	} else {
