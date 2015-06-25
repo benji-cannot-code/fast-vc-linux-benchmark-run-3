c11a81ad57a0a1d010b0b4b85361e5997c390a07
Richard Weinberger <richard@nod.at>
2015-06-25 22:44:11 +0200

    um: Don't pollute kernel namespace with uapi
    Don't include ptrace uapi stuff in arch headers, it will
    pollute the kernel namespace and conflict with existing
    stuff.
    In this case it fixes clashes with common names like R8.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/include/asm/ptrace-generic.h
+++ arch/um/include/asm/ptrace-generic.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
-#include <asm/ptrace-abi.h>
 #include <sysdep/ptrace.h>
 
 struct pt_regs {
--- arch/um/kernel/ptrace.c
+++ arch/um/kernel/ptrace.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/tracehook.h>
 #include <asm/uaccess.h>
+#include <asm/ptrace-abi.h>
 
 void user_enable_single_step(struct task_struct *child)
 {
