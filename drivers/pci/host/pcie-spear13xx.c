83937a7eec5a5a6634eb6bbf1aa5d469dea364c0
Pratyush Anand <pratyush.anand@gmail.com>
2015-06-25 15:01:11 -0700

    Mohit Kumar has moved
    Mohit's email-id doesn't exist anymore as he has left the company.
    Replace ST's id with mohit.kumar.dhaka@gmail.com.
    
    Signed-off-by: Pratyush Anand <pratyush.anand@gmail.com>
    Cc: Mohit Kumar <mohit.kumar.dhaka@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/pci/host/pcie-spear13xx.c
+++ drivers/pci/host/pcie-spear13xx.c
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2010-2014 ST Microelectronics
  * Pratyush Anand <pratyush.anand@gmail.com>
- * Mohit Kumar <mohit.kumar@st.com>
+ * Mohit Kumar <mohit.kumar.dhaka@gmail.com>
  *
  * This file is licensed under the terms of the GNU General Public
  * License version 2. This program is licensed "as is" without any
--- drivers/phy/phy-spear1310-miphy.c
+++ drivers/phy/phy-spear1310-miphy.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2014 ST Microelectronics
  * Pratyush Anand <pratyush.anand@gmail.com>
- * Mohit Kumar <mohit.kumar@st.com>
+ * Mohit Kumar <mohit.kumar.dhaka@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
