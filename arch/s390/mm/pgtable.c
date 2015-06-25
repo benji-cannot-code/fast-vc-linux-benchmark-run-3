fb69139f93621440b2a00a9df300eb8789051f7d
Dominik Dingel <dingel@linux.vnet.ibm.com>
2015-06-25 14:59:39 -0700

    s390/mm: make hugepages_supported a boot time decision
    There is a potential bug with KVM and hugetlbfs if the hardware does not
    support hugepages (EDAT1).  We fix this by making EDAT1 a hard requirement
    for hugepages and therefore removing and simplifying code.
    
    As s390, with the sw-emulated hugepages, was the only user of
    arch_prepare/release_hugepage I also removed theses calls from common and
    other architecture code.
    
    This patch (of 5):
    
    By dropping support for hugepages on machines which do not have the
    hardware feature EDAT1, we fix a potential s390 KVM bug.
    
    The bug would happen if a guest is backed by hugetlbfs (not supported
    currently), but does not get pagetables with PGSTE.  This would lead to
    random memory overwrites.
    
    Signed-off-by: Dominik Dingel <dingel@linux.vnet.ibm.com>
    Acked-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/s390/mm/pgtable.c
+++ arch/s390/mm/pgtable.c
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ALLOC_ORDER	2
 #define FRAG_MASK	0x03
 
+unsigned int HPAGE_SHIFT;
+
 unsigned long *crst_table_alloc(struct mm_struct *mm)
 {
 	struct page *page = alloc_pages(GFP_KERNEL, ALLOC_ORDER);
