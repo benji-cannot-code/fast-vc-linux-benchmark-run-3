eddabbe8834dbd805ae8c8662d61d0463fe05870
Joe Perches <joe@perches.com>
2015-06-25 15:03:29 -0700

    checkpatch: emit "NOTE: <types>" message only once after multiple files
    Make this message similar to the "false positives" message and emit it
    only once when scanning multiple files instead of after each file scanned.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Cc: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -204,7 +204,7 @@ sub hash_save_array_words {
 sub hash_show_words {
 	my ($hashRef, $prefix) = @_;
 
-	if ($quiet == 0 && keys %$hashRef) {
+	if (keys %$hashRef) {
 		print "\nNOTE: $prefix message types:";
 		foreach my $word (sort keys %$hashRef) {
 			print " $word";
@@ -771,6 +771,9 @@ for my $filename (@ARGV) {
 }
 
 if (!$quiet) {
+	hash_show_words(\%use_type, "Used");
+	hash_show_words(\%ignore_type, "Ignored");
+
 	if ($^V lt 5.10.0) {
 		print << "EOM"
 
@@ -5707,9 +5710,6 @@ NOTE: Whitespace errors detected.
 EOM
 		}
 	}
-
-	hash_show_words(\%use_type, "Used");
-	hash_show_words(\%ignore_type, "Ignored");
 
 	if ($clean == 0 && $fix &&
 	    ("@rawlines" ne "@fixed" ||
