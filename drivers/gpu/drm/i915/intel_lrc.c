4a775694ef868e1cd39cc073ad5797e74a94378d
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2015-06-25 18:35:06 +0300

    drm/i915/gtt: Introduce i915_page_dir_dma_addr
    The legacy mode mm switch and the execlist context assignment
    needs dma address for the page directories.
    
    Introduce a function that encapsulates the scratch_pd dma
    fallback if no pd is found.
    
    v2: Rebase, s/ring/req
    
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Michel Thierry <michel.thierry@intel.com> (v1)
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_lrc.c
+++ drivers/gpu/drm/i915/intel_lrc.c
@@ -191,9 +191,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GEN8_CTX_PRIVILEGE (1<<8)
 
 #define ASSIGN_CTX_PDP(ppgtt, reg_state, n) { \
-	const u64 _addr = test_bit(n, ppgtt->pdp.used_pdpes) ? \
-		ppgtt->pdp.page_directory[n]->daddr : \
-		ppgtt->scratch_pd->daddr; \
+	const u64 _addr = i915_page_dir_dma_addr((ppgtt), (n));	\
 	reg_state[CTX_PDP ## n ## _UDW+1] = upper_32_bits(_addr); \
 	reg_state[CTX_PDP ## n ## _LDW+1] = lower_32_bits(_addr); \
 }
