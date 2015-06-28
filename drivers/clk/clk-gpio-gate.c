72963ee87be70394bbe4b18e49f576f6314ceb43
Sergej Sawazki <ce3a@gmx.de>
2015-06-28 16:24:53 +0200

    clk: gpio-gate: Include export.h instead of module.h
    Include export.h for EXPORT_SYMBOL_GPL, no need to include module.h.
    
    Cc: Jyri Sarha <jsarha@ti.com>
    Signed-off-by: Sergej Sawazki <ce3a@gmx.de>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/clk-gpio-gate.c
+++ drivers/clk/clk-gpio-gate.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/clk-provider.h>
-#include <linux/module.h>
+#include <linux/export.h>
 #include <linux/slab.h>
 #include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
