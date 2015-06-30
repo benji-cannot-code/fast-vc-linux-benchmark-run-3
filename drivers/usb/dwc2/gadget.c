e71d5fe4c53e1c15b55fd67143dc7af1c5f0c1b7
Felipe Balbi <balbi@ti.com>
2015-06-29 20:17:22 -0500

    usb: dwc2: gadget: use | instead of + for bitmasks
    It's just a lot clearer to use | operator instead of
    + operator.
    
    Caught by coccicheck:
    
    	"
    	drivers/usb/dwc2/gadget.c:2883:26-27: WARNING: sum of probable
    	bitmasks, consider |
    	"
    
    Cc: John Youn <John.Youn@synopsys.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -2881,7 +2881,7 @@ static int s3c_hsotg_ep_sethalt(struct usb_ep *ep, int value)
 		epctl = readl(hs->regs + epreg);
 
 		if (value) {
-			epctl |= DXEPCTL_STALL + DXEPCTL_SNAK;
+			epctl |= DXEPCTL_STALL | DXEPCTL_SNAK;
 			if (epctl & DXEPCTL_EPENA)
 				epctl |= DXEPCTL_EPDIS;
 		} else {
