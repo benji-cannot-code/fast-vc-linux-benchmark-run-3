f337fe4cb93b2e74c00488bdd00bd7eddf8526c2
Dan Williams <dan.j.williams@intel.com>
2015-06-30 12:07:17 -0400

    sparse: fix misplaced __pmem definition
    Move the definition of __pmem outside of CONFIG_SPARSE_RCU_POINTER to fix:
    
    drivers/nvdimm/pmem.c:198:17: sparse: too many arguments for function __builtin_expect
    drivers/nvdimm/pmem.c:36:33: sparse: expected ; at end of declaration
    drivers/nvdimm/pmem.c:48:21: sparse: void declaration
    
    ...due to __pmem failing to be defined in some configurations when
    CONFIG_SPARSE_RCU_POINTER=y.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -18,11 +18,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # define __release(x)	__context__(x,-1)
 # define __cond_lock(x,c)	((c) ? ({ __acquire(x); 1; }) : 0)
 # define __percpu	__attribute__((noderef, address_space(3)))
+# define __pmem		__attribute__((noderef, address_space(5)))
 #ifdef CONFIG_SPARSE_RCU_POINTER
 # define __rcu		__attribute__((noderef, address_space(4)))
 #else
 # define __rcu
-# define __pmem		__attribute__((noderef, address_space(5)))
 #endif
 extern void __chk_user_ptr(const volatile void __user *);
 extern void __chk_io_ptr(const volatile void __iomem *);
