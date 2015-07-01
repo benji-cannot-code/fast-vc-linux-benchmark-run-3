752976bc5f03237172c809b8606cfa3699bd05bf
Thierry Reding <treding@nvidia.com>
2015-07-01 14:05:09 +0200

    drm: Remove useless blank line
    Blank lines at the end of a function definition are not useful, so get
    rid of it.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -4299,7 +4299,6 @@ void drm_property_unreference_blob(struct drm_property_blob *blob)
 		mutex_unlock(&dev->mode_config.blob_lock);
 	else
 		might_lock(&dev->mode_config.blob_lock);
-
 }
 EXPORT_SYMBOL(drm_property_unreference_blob);
 
