f296ff736415ee75af06a0dec066a36008b253a1
Wang Nan <wangnan0@huawei.com>
2015-07-01 02:13:53 +0000

    bpf tools: Open eBPF object file and do basic validation
    This patch defines basic interface of libbpf. 'struct bpf_object' will
    be the handler of each object file. Its internal structure is hide to
    user. eBPF object files are compiled by LLVM as ELF format. In this
    patch, libelf is used to open those files, read EHDR and do basic
    validation according to e_type and e_machine.
    
    All elf related staffs are grouped together and reside in efile field of
    'struct bpf_object'. bpf_object__elf_finish() is introduced to clear it.
    
    After all eBPF programs in an object file are loaded, related ELF
    information is useless. Close the object file and free those memory.
    
    The zfree() and zclose() functions are introduced to ensure setting NULL
    pointers and negative file descriptors after resources are released.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Acked-by: Alexei Starovoitov <ast@plumgrid.com>
    Cc: Brendan Gregg <brendan.d.gregg@gmail.com>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: He Kuang <hekuang@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kaixu Xia <xiakaixu@huawei.com>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com
    Link: http://lkml.kernel.org/r/1435716878-189507-6-git-send-email-wangnan0@huawei.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/lib/bpf/libbpf.h
+++ tools/lib/bpf/libbpf.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __BPF_LIBBPF_H
 #define __BPF_LIBBPF_H
 
+#include <stdio.h>
+
 /*
  * In include/linux/compiler-gcc.h, __printf is defined. However
  * it should be better if libbpf.h doesn't depend on Linux header file.
@@ -20,5 +22,11 @@ typedef int (*libbpf_print_fn_t)(const char *, ...)
 void libbpf_set_print(libbpf_print_fn_t warn,
 		      libbpf_print_fn_t info,
 		      libbpf_print_fn_t debug);
+
+/* Hide internal to user */
+struct bpf_object;
+
+struct bpf_object *bpf_object__open(const char *path);
+void bpf_object__close(struct bpf_object *object);
 
 #endif
