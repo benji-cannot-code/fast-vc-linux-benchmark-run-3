aae0ec5068f7163df6297280556b936525cbb7c4
Boris Brezillon <boris.brezillon@free-electrons.com>
2015-07-01 10:21:48 +0200

    pwm: Constify PWM device where possible
    The PWM argument is not modified in PWM property accessors, make it a
    const argument so that the accessors can be used from sysfs.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- include/linux/pwm.h
+++ include/linux/pwm.h
@@ -104,7 +104,7 @@ static inline void pwm_set_period(struct pwm_device *pwm, unsigned int period)
 		pwm->period = period;
 }
 
-static inline unsigned int pwm_get_period(struct pwm_device *pwm)
+static inline unsigned int pwm_get_period(const struct pwm_device *pwm)
 {
 	return pwm ? pwm->period : 0;
 }
@@ -115,7 +115,7 @@ static inline void pwm_set_duty_cycle(struct pwm_device *pwm, unsigned int duty)
 		pwm->duty_cycle = duty;
 }
 
-static inline unsigned int pwm_get_duty_cycle(struct pwm_device *pwm)
+static inline unsigned int pwm_get_duty_cycle(const struct pwm_device *pwm)
 {
 	return pwm ? pwm->duty_cycle : 0;
 }
