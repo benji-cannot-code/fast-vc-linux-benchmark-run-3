a7d041b789ce87c1584c9ecac7cc415409b761e1
Leung Timothy Chi King <contact@timothyleung.co>
2015-07-01 09:44:13 -0700

    Staging: rts5208: Fix checkpatch warning: else is not generally useful
    The following checkpatch warning was fixed:
    
    WARNING: else is not generally useful after a break or return
    
    Signed-off-by: Leung Timothy Chi King <contact@timothyleung.co>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rts5208/sd.c
+++ drivers/staging/rts5208/sd.c
@@ -3521,12 +3521,11 @@ int reset_sd_card(struct rtsx_chip *chip)
 			if (chip->sd_io) {
 				rtsx_trace(chip);
 				return STATUS_FAIL;
-			} else {
-				retval = reset_mmc(chip);
-				if (retval != STATUS_SUCCESS) {
-					rtsx_trace(chip);
-					return STATUS_FAIL;
-				}
+			}
+			retval = reset_mmc(chip);
+			if (retval != STATUS_SUCCESS) {
+				rtsx_trace(chip);
+				return STATUS_FAIL;
 			}
 		}
 	}
