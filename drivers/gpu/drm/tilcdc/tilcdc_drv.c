546a29ef1374ab48379697dcbb4a8165c8c3c31d
Jyri Sarha <jsarha@ti.com>
2015-07-02 16:26:12 +0300

    drm/tilcdc: Allocate register storage based on the actual number registers
    Allocate suspend/resume register storage based on the actual number
    registers the driver is aware of. The static allocation for register
    storage had fallen behind badly.
    
    Reported-by: Michael Bode <michael@bumbleB.de>
    Signed-off-by: Jyri Sarha <jsarha@ti.com>
    Reviewed-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/tilcdc/tilcdc_drv.c
+++ drivers/gpu/drm/tilcdc/tilcdc_drv.c
@@ -142,10 +142,13 @@ static int tilcdc_unload(struct drm_device *dev)
 
 	pm_runtime_disable(dev->dev);
 
+	kfree(priv->saved_register);
 	kfree(priv);
 
 	return 0;
 }
+
+static size_t tilcdc_num_regs(void);
 
 static int tilcdc_load(struct drm_device *dev, unsigned long flags)
 {
@@ -158,7 +161,12 @@ static int tilcdc_load(struct drm_device *dev, unsigned long flags)
 	int ret;
 
 	priv = kzalloc(sizeof(*priv), GFP_KERNEL);
-	if (!priv) {
+	if (priv)
+		priv->saved_register = kcalloc(tilcdc_num_regs(),
+					       sizeof(*priv->saved_register),
+					       GFP_KERNEL);
+	if (!priv || !priv->saved_register) {
+		kfree(priv);
 		dev_err(dev->dev, "failed to allocate private data\n");
 		return -ENOMEM;
 	}
@@ -340,6 +348,7 @@ fail_free_wq:
 
 fail_free_priv:
 	dev->dev_private = NULL;
+	kfree(priv->saved_register);
 	kfree(priv);
 	return ret;
 }
@@ -457,6 +466,16 @@ static const struct {
 		REG(2, true,  LCDC_INT_ENABLE_SET_REG),
 #undef REG
 };
+
+static size_t tilcdc_num_regs(void)
+{
+	return ARRAY_SIZE(registers);
+}
+#else
+static size_t tilcdc_num_regs(void)
+{
+	return 0;
+}
 #endif
 
 #ifdef CONFIG_DEBUG_FS
