41bee6a0ebf9ea5fec0f2548858cab34b8f688ec
Jisheng Zhang <jszhang@marvell.com>
2015-07-02 09:33:07 +0800

    arm64: cpuidle: add __init section marker to arm_cpuidle_init
    It is not needed after booting, this patch moves the arm_cpuidle_init()
    function to the __init section.
    
    Signed-off-by: Jisheng Zhang <jszhang@marvell.com>
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/cpuidle.c
+++ arch/arm64/kernel/cpuidle.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpuidle.h>
 #include <asm/cpu_ops.h>
 
-int arm_cpuidle_init(unsigned int cpu)
+int __init arm_cpuidle_init(unsigned int cpu)
 {
 	int ret = -EOPNOTSUPP;
 
