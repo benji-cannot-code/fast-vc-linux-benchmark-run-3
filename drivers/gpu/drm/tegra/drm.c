725754ab8e224a0e633862e0a4fd60ae4217695f
Thierry Reding <treding@nvidia.com>
2015-07-02 17:04:06 +0200

    drm/tegra: dc: Reset VBLANK to off
    Upon driver load, reset the VBLANK machinery to off to reflect the
    hardware state. Since the ->reset() callback is called from the initial
    drm_mode_config_reset() call, move the latter after the VBLANK machinery
    initialization by drm_vblank_init().
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/drm.c
+++ drivers/gpu/drm/tegra/drm.c
@@ -172,8 +172,6 @@ static int tegra_drm_load(struct drm_device *drm, unsigned long flags)
 	if (err < 0)
 		goto fbdev;
 
-	drm_mode_config_reset(drm);
-
 	/*
 	 * We don't use the drm_irq_install() helpers provided by the DRM
 	 * core, so we need to set this manually in order to allow the
@@ -188,6 +186,8 @@ static int tegra_drm_load(struct drm_device *drm, unsigned long flags)
 	err = drm_vblank_init(drm, drm->mode_config.num_crtc);
 	if (err < 0)
 		goto device;
+
+	drm_mode_config_reset(drm);
 
 	err = tegra_drm_fb_init(drm);
 	if (err < 0)
