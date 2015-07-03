99afaf24ee53f56ee071406f6c400118ac4b2dd7
Kinglong Mee <kinglongmee@gmail.com>
2015-07-03 19:34:53 +0800

    nfsd: Drop including client's header file nfs_fs.h
    nfs_fs.h is a client's header file.
    
    # ll fs/nfsd/nfs4acl.o fs/nfsd/nfsd.ko
    -rw-r--r--. 1 root root 328248 Jul  3 19:26 fs/nfsd/nfs4acl.o
    -rw-r--r--. 1 root root 7452016 Jul  3 19:26 fs/nfsd/nfsd.ko
    
    After this patch,
    # ll fs/nfsd/nfs4acl.o fs/nfsd/nfsd.ko
    -rw-r--r--. 1 root root 150872 Jul  3 19:15 fs/nfsd/nfs4acl.o
    -rw-r--r--. 1 root root 7273792 Jul  3 19:23 fs/nfsd/nfsd.ko
    
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4acl.c
+++ fs/nfsd/nfs4acl.c
@@ -35,8 +35,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <linux/fs.h>
 #include <linux/slab.h>
-#include <linux/nfs_fs.h>
+#include <linux/posix_acl.h>
+
 #include "nfsfh.h"
 #include "nfsd.h"
 #include "acl.h"
