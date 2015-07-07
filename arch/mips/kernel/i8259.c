de9826376225acf85e3252978a0a5eec05c799e5
Joel Porquet <joel@porquet.org>
2015-07-07 17:17:33 -0400

    mips/irq: prepare for drivers/irqchip/irqchip.h removal
    The IRQCHIP_DECLARE macro migrated to 'include/linux/irqchip.h'.
    
    See commit 91e20b5040c67c51aad88cf87db4305c5bd7f79d
    ("irqchip: Move IRQCHIP_DECLARE macro to include/linux/irqchip.h").
    
    This patch removes inclusions of private header 'drivers/irqchip/irqchip.h'and
    replaces them with the inclusion of 'include/linux/irqchip.h'.
    
    Signed-off-by: Joel Porquet <joel@porquet.org>
    Cc: vgupta@synopsys.com
    Cc: monstr@monstr.eu
    Cc: ralf@linux-mips.org
    Cc: jason@lakedaemon.net
    Link: http://lkml.kernel.org/r/3357968.0IznKtY3Q9@joel-zenbook
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/mips/kernel/i8259.c
+++ arch/mips/kernel/i8259.c
@@ -19,11 +19,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/syscore_ops.h>
 #include <linux/irq.h>
+#include <linux/irqchip.h>
 
 #include <asm/i8259.h>
 #include <asm/io.h>
-
-#include "../../drivers/irqchip/irqchip.h"
 
 /*
  * This is the 'legacy' 8259A Programmable Interrupt Controller,
