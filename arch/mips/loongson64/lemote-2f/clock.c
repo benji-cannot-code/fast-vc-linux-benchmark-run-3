ac502d3bd09093db523b3b5b0cc735ea960bf437
Ralf Baechle <ralf@linux-mips.org>
2015-07-07 20:56:04 +0200

    MIPS, CPUFREQ: Fix spelling of Institute.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/loongson64/lemote-2f/clock.c
+++ arch/mips/loongson64/lemote-2f/clock.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (C) 2006 - 2008 Lemote Inc. & Insititute of Computing Technology
+ * Copyright (C) 2006 - 2008 Lemote Inc. & Institute of Computing Technology
  * Author: Yanhua, yanh@lemote.com
  *
  * This file is subject to the terms and conditions of the GNU General Public
