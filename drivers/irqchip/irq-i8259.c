6188b7b0f9c1973a7d052919baf1a99d178b0a17
Ralf Baechle <ralf@linux-mips.org>
2015-07-08 14:46:08 +0200

    MIPS/irqchip: Move i8259 irqchip driver to drivers/irqchip
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/irqchip/irq-i8259.c
+++ drivers/irqchip/irq-i8259.c
@@ -13,13 +13,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/interrupt.h>
+#include <linux/irqchip.h>
 #include <linux/irqdomain.h>
 #include <linux/kernel.h>
 #include <linux/of_irq.h>
 #include <linux/spinlock.h>
 #include <linux/syscore_ops.h>
 #include <linux/irq.h>
-#include <linux/irqchip.h>
 
 #include <asm/i8259.h>
 #include <asm/io.h>
