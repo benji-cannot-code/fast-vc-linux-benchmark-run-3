4cd4c52b18c83d717580abc62a3134322d67ae8d
Herbert Xu <herbert@gondor.apana.org.au>
2015-07-09 07:17:15 +0800

    crypto: api - Add instance free function to crypto_type
    Currently the task of freeing an instance is given to the crypto
    template.  However, it has no type information on the instance so
    we have to resort to checking type information at runtime.
    
    This patch introduces a free function to crypto_type that will be
    used to free an instance.  This can then be used to free an instance
    in a type-safe manner.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/algapi.h
+++ include/crypto/algapi.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/skbuff.h>
 
 struct crypto_aead;
+struct crypto_instance;
 struct module;
 struct rtattr;
 struct seq_file;
@@ -31,6 +32,7 @@ struct crypto_type {
 	void (*show)(struct seq_file *m, struct crypto_alg *alg);
 	int (*report)(struct sk_buff *skb, struct crypto_alg *alg);
 	struct crypto_alg *(*lookup)(const char *name, u32 type, u32 mask);
+	void (*free)(struct crypto_instance *inst);
 
 	unsigned int type;
 	unsigned int maskclear;
