6c2f37bbb92e9afc8556ebc8fd90a01945f1e732
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-07-10 03:19:42 -0300

    [media] dvb-frontends: Drop owner assignment from i2c_driver
    i2c_driver does not need to set an owner because i2c_register_driver()
    will set it.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/dvb-frontends/tda10071.c
+++ drivers/media/dvb-frontends/tda10071.c
@@ -1252,7 +1252,6 @@ MODULE_DEVICE_TABLE(i2c, tda10071_id_table);
 
 static struct i2c_driver tda10071_driver = {
 	.driver = {
-		.owner	= THIS_MODULE,
 		.name	= "tda10071",
 		.suppress_bind_attrs = true,
 	},
