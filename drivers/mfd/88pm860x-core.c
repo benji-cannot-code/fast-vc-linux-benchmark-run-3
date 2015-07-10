e29c6347c88edcfb5b83f6ad808268e91f66b946
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-07-10 14:05:40 +0900

    mfd: Drop owner assignment from i2c_drivers
    i2c_driver does not need to set an owner because i2c_register_driver()
    will set it.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/88pm860x-core.c
+++ drivers/mfd/88pm860x-core.c
@@ -1259,7 +1259,6 @@ MODULE_DEVICE_TABLE(of, pm860x_dt_ids);
 static struct i2c_driver pm860x_driver = {
 	.driver	= {
 		.name	= "88PM860x",
-		.owner	= THIS_MODULE,
 		.pm     = &pm860x_pm_ops,
 		.of_match_table	= pm860x_dt_ids,
 	},
