80517b903b23beed165cdfd1219027ba7154c4eb
Daniel Thompson <daniel.thompson@linaro.org>
2015-07-10 14:58:00 +0100

    arm64: Remove unused macros from assembler.h
    Commit 68234df4ea79 ("arm64: kill flush_cache_all()") removed the
    only users of these macros.
    
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -51,18 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	.endm
 
 /*
- * Save/disable and restore interrupts.
- */
-	.macro	save_and_disable_irqs, olddaif
-	mrs	\olddaif, daif
-	disable_irq
-	.endm
-
-	.macro	restore_irqs, olddaif
-	msr	daif, \olddaif
-	.endm
-
-/*
  * Enable and disable debug exceptions.
  */
 	.macro	disable_dbg
