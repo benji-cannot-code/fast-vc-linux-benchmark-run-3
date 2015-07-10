318a520f86cb7f6132e3d55870a82ff0d3d53cbe
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-07-10 15:26:27 +0900

    phy: Drop owner assignment from platform_driver
    platform_driver does not need to set an owner because
    platform_driver_register() will set it.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/phy-miphy28lp.c
+++ drivers/phy/phy-miphy28lp.c
@@ -1269,7 +1269,6 @@ static struct platform_driver miphy28lp_driver = {
 	.probe = miphy28lp_probe,
 	.driver = {
 		.name = "miphy28lp-phy",
-		.owner = THIS_MODULE,
 		.of_match_table = miphy28lp_of_match,
 	}
 };
