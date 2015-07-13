d32f1467266c53d194eeab798a132ca317eb43d7
Allen Hubbe <Allen.Hubbe@emc.com>
2015-07-13 08:07:08 -0400

    NTB: Fix ntb_transport out-of-order RX update
    It was possible for a synchronous update of the RX index in the error
    case to get ahead of the asynchronous RX index update in the normal
    case.  Change the RX processing to preserve an RX completion order.
    
    There were two error cases.  First, if a buffer is not present to
    receive data, there would be no queue entry to preserve the RX
    completion order.  Instead of dropping the RX frame, leave the RX frame
    in the ring.  Schedule RX processing when RX entries are enqueued, in
    case there are RX frames waiting in the ring to be received.
    
    Second, if a buffer is too small to receive data, drop the frame in the
    ring, mark the RX entry as done, and indicate the error in the RX entry
    length.  Check for a negative length in the receive callback in
    ntb_netdev, and count occurrences as rx_length_errors.
    
    Signed-off-by: Allen Hubbe <Allen.Hubbe@emc.com>
    Signed-off-by: Jon Mason <jdmason@kudzu.us>

--- drivers/net/ntb_netdev.c
+++ drivers/net/ntb_netdev.c
@@ -103,6 +103,12 @@ static void ntb_netdev_rx_handler(struct ntb_transport_qp *qp, void *qp_data,
 
 	netdev_dbg(ndev, "%s: %d byte payload received\n", __func__, len);
 
+	if (len < 0) {
+		ndev->stats.rx_errors++;
+		ndev->stats.rx_length_errors++;
+		goto enqueue_again;
+	}
+
 	skb_put(skb, len);
 	skb->protocol = eth_type_trans(skb, ndev);
 	skb->ip_summed = CHECKSUM_NONE;
@@ -122,6 +128,7 @@ static void ntb_netdev_rx_handler(struct ntb_transport_qp *qp, void *qp_data,
 		return;
 	}
 
+enqueue_again:
 	rc = ntb_transport_rx_enqueue(qp, skb, skb->data, ndev->mtu + ETH_HLEN);
 	if (rc) {
 		dev_kfree_skb(skb);
