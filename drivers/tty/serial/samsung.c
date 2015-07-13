22a776d7dae2dba766702c1768e762e9de8e6565
Javier Martinez Canillas <javier@osg.samsung.com>
2015-07-13 13:18:51 +0200

    serial: samsung: Remove redundant DEBUG_LL check
    Commit 84f57d9e3685 ("tty: serial/samsung: fix modular build") fixed
    build issues when the driver was built as a module. One of those was
    that printascii is only accessible when the driver is built-in.
    
    But there is no need to check for defined(CONFIG_DEBUG_LL) since the
    SERIAL_SAMSUNG_DEBUG Kconfig symbol already depends on DEBUG_LL.
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Acked-by: Kukjin Kim <kgene@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "samsung.h"
 
 #if	defined(CONFIG_SERIAL_SAMSUNG_DEBUG) &&	\
-	defined(CONFIG_DEBUG_LL) &&		\
 	!defined(MODULE)
 
 extern void printascii(const char *);
