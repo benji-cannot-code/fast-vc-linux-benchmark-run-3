007278db5c57d3129403db83c9a6add706e293db
Juston Li <juston.h.li@gmail.com>
2015-07-14 21:14:43 -0700

    staging: sm750fb: remove trailing whitespace
    Fixes checkpatch.pl error:
    ERROR: trailing whitespace
    
    Signed-off-by: Juston Li <juston.h.li@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_dvi.c
+++ drivers/staging/sm750fb/ddk750_dvi.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#define USE_DVICHIP 
+#define USE_DVICHIP
 #ifdef USE_DVICHIP
 #include "ddk750_help.h"
 #include "ddk750_reg.h"
