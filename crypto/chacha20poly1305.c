c7c838cbda677181a6e6368ce3bdd5a9f3ef76d4
Martin Willi <martin@strongswan.org>
2015-07-16 19:14:00 +0200

    crypto: chacha20 - Export common ChaCha20 helpers
    As architecture specific drivers need a software fallback, export a
    ChaCha20 en-/decryption function together with some helpers in a header
    file.
    
    Signed-off-by: Martin Willi <martin@strongswan.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/chacha20poly1305.c
+++ crypto/chacha20poly1305.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/internal/hash.h>
 #include <crypto/internal/skcipher.h>
 #include <crypto/scatterwalk.h>
+#include <crypto/chacha20.h>
 #include <linux/err.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -24,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define POLY1305_BLOCK_SIZE	16
 #define POLY1305_DIGEST_SIZE	16
 #define POLY1305_KEY_SIZE	32
-#define CHACHA20_KEY_SIZE	32
-#define CHACHA20_IV_SIZE	16
 #define CHACHAPOLY_IV_SIZE	12
 
 struct chachapoly_instance_ctx {
