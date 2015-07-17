f6f70c46de8bda17517ce00b88e15027bd8a4595
Hartmut Knaack <knaack.h@gmx.de>
2015-07-17 10:52:30 +0200

    iio:magnetometer:bmc150_magn: expand mutex in trigger_handler
    Keep the mutex locked, until the content of data->buffer has been pushed
    out.
    
    Signed-off-by: Hartmut Knaack <knaack.h@gmx.de>
    Acked-by: Irina Tirdea <irina.tirdea@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/magnetometer/bmc150_magn.c
+++ drivers/iio/magnetometer/bmc150_magn.c
@@ -665,7 +665,6 @@ static irqreturn_t bmc150_magn_trigger_handler(int irq, void *p)
 
 	mutex_lock(&data->mutex);
 	ret = bmc150_magn_read_xyz(data, data->buffer);
-	mutex_unlock(&data->mutex);
 	if (ret < 0)
 		goto err;
 
@@ -673,6 +672,7 @@ static irqreturn_t bmc150_magn_trigger_handler(int irq, void *p)
 					   pf->timestamp);
 
 err:
+	mutex_unlock(&data->mutex);
 	iio_trigger_notify_done(indio_dev->trig);
 
 	return IRQ_HANDLED;
