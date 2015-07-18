edd85ca7a4d3b17be6fa2c39da94def087cc0039
Stefan Wahren <stefan.wahren@i2se.com>
2015-07-18 12:30:44 +0000

    iio: mxs-lradc: remove unnecessary header includes
    This patch removes the unnecessary header includes.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/adc/mxs-lradc.c
+++ drivers/staging/iio/adc/mxs-lradc.c
@@ -24,18 +24,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/sysfs.h>
-#include <linux/list.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/platform_device.h>
-#include <linux/spinlock.h>
-#include <linux/wait.h>
-#include <linux/sched.h>
 #include <linux/stmp_device.h>
 #include <linux/bitops.h>
 #include <linux/completion.h>
-#include <linux/delay.h>
 #include <linux/input.h>
 #include <linux/clk.h>
 
