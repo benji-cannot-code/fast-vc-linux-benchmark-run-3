b6fc8b69c6d17dd56a260821899a46b911e66a81
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-07-19 19:27:49 +0200

    staging: rtl8192e: Rename rtl8192_phy_ConfigRFWithHeaderFile
    Use naming schema found in other rtlwifi devices.
    Rename rtl8192_phy_ConfigRFWithHeaderFile to rtl92e_config_rf_path.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c
+++ drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c
@@ -142,7 +142,7 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 		case RF90_PATH_A:
 			while (RF3_Final_Value != RegValueToBeCheck &&
 			       RetryTimes != 0) {
-				ret = rtl8192_phy_ConfigRFWithHeaderFile(dev,
+				ret = rtl92e_config_rf_path(dev,
 						(enum rf90_radio_path)eRFPath);
 				RF3_Final_Value = rtl8192_phy_QueryRFReg(dev,
 						 (enum rf90_radio_path)eRFPath,
@@ -158,7 +158,7 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 		case RF90_PATH_B:
 			while (RF3_Final_Value != RegValueToBeCheck &&
 			       RetryTimes != 0) {
-				ret = rtl8192_phy_ConfigRFWithHeaderFile(dev,
+				ret = rtl92e_config_rf_path(dev,
 						(enum rf90_radio_path)eRFPath);
 				RF3_Final_Value = rtl8192_phy_QueryRFReg(dev,
 						 (enum rf90_radio_path)eRFPath,
@@ -174,7 +174,7 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 		case RF90_PATH_C:
 			while (RF3_Final_Value != RegValueToBeCheck &&
 			       RetryTimes != 0) {
-				ret = rtl8192_phy_ConfigRFWithHeaderFile(dev,
+				ret = rtl92e_config_rf_path(dev,
 						(enum rf90_radio_path)eRFPath);
 				RF3_Final_Value = rtl8192_phy_QueryRFReg(dev,
 						(enum rf90_radio_path)eRFPath,
@@ -190,7 +190,7 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 		case RF90_PATH_D:
 			while (RF3_Final_Value != RegValueToBeCheck &&
 			       RetryTimes != 0) {
-				ret = rtl8192_phy_ConfigRFWithHeaderFile(dev,
+				ret = rtl92e_config_rf_path(dev,
 					       (enum rf90_radio_path)eRFPath);
 				RF3_Final_Value = rtl8192_phy_QueryRFReg(dev,
 					       (enum rf90_radio_path)eRFPath,
