6f492851ed545803c6691d22b3870a7c786e9db7
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-07-19 19:27:18 +0200

    staging: rtl8192e: Rename cmpk_message_handle_tx
    Use naming schema found in other rtlwifi devices.
    Rename cmpk_message_handle_tx to rtl92e_send_cmd_pkt.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8192E_cmdpkt.c
+++ drivers/staging/rtl8192e/rtl8192e/r8192E_cmdpkt.c
@@ -22,11 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "r8192E_hw.h"
 #include "r8192E_cmdpkt.h"
 
-bool cmpk_message_handle_tx(
-	struct net_device *dev,
-	u8	*code_virtual_address,
-	u32	packettype,
-	u32	buffer_len)
+bool rtl92e_send_cmd_pkt(struct net_device *dev, u8 *code_virtual_address,
+			 u32 packettype, u32 buffer_len)
 {
 
 	bool				rt_status = true;
