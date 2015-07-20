dbe8a147cdb6ee2b323f804b5c875324ec9fc261
David Howells <dhowells@redhat.com>
2015-07-20 21:16:27 +0100

    system_keyring.c doesn't need to #include module-internal.h
    system_keyring.c doesn't need to #include module-internal.h as it doesn't use
    the one thing that exports.  Remove the inclusion.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- kernel/system_keyring.c
+++ kernel/system_keyring.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <keys/asymmetric-type.h>
 #include <keys/system_keyring.h>
-#include "module-internal.h"
 
 struct key *system_trusted_keyring;
 EXPORT_SYMBOL_GPL(system_trusted_keyring);
