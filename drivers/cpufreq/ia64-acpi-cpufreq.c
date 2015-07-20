bcdbd56302423c4ba00c249ace9575e44ce1b9c8
Pan Xinhui <xinhuix.pan@intel.com>
2015-07-20 14:24:36 +0800

    cpufreq: ia64: Fix a memory leak in acpi_cpufreq_cpu_exit()
    freq_table should be alloced in ->init and freed in ->exit, but it
    it is not freed.  Fix this memory leak in acpi_cpufreq_cpu_exit().
    
    Signed-off-by: Pan Xinhui <xinhuix.pan@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/ia64-acpi-cpufreq.c
+++ drivers/cpufreq/ia64-acpi-cpufreq.c
@@ -334,6 +334,7 @@ acpi_cpufreq_cpu_exit (
 	if (data) {
 		acpi_io_data[policy->cpu] = NULL;
 		acpi_processor_unregister_performance(policy->cpu);
+		kfree(policy->freq_table);
 		kfree(data);
 	}
 
