a7c0c8deafd510cdfb586274ab076ed8e6deedfb
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-07-21 18:57:24 +0530

    staging: rtl8188eu: remove unused define
    _HCI_INTF_C_ was only defined here but not being used anywhere.
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/usb_intf.c
+++ drivers/staging/rtl8188eu/os_dep/usb_intf.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  *
  ******************************************************************************/
-#define _HCI_INTF_C_
 
 #define pr_fmt(fmt) "R8188EU: " fmt
 #include <osdep_service.h>
