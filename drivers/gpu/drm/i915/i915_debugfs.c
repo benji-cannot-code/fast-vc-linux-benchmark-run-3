abce163ed85197952d4282fae9a07eaef57621ab
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-07-21 17:36:45 +0530

    drm/i915: remove unnecessary null test
    While creating the debugfs file we are setting the inode->i_private to
    dev. That same dev is passed to these functions as private of struct
    seq_file via single_open(). Moreover single_open is setting
    file->private_data->private to dev.
    So at this point it can never be NULL.
    This check was added by commit eb3394faeb97 ("drm/i915: Add debugfs test
    control files for Displayport compliance testing")
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_debugfs.c
+++ drivers/gpu/drm/i915/i915_debugfs.c
@@ -4029,24 +4029,14 @@ static ssize_t i915_displayport_test_active_write(struct file *file,
 {
 	char *input_buffer;
 	int status = 0;
-	struct seq_file *m;
 	struct drm_device *dev;
 	struct drm_connector *connector;
 	struct list_head *connector_list;
 	struct intel_dp *intel_dp;
 	int val = 0;
 
-	m = file->private_data;
-	if (!m) {
-		status = -ENODEV;
-		return status;
-	}
-	dev = m->private;
+	dev = ((struct seq_file *)file->private_data)->private;
 
-	if (!dev) {
-		status = -ENODEV;
-		return status;
-	}
 	connector_list = &dev->mode_config.connector_list;
 
 	if (len == 0)
@@ -4104,9 +4094,6 @@ static int i915_displayport_test_active_show(struct seq_file *m, void *data)
 	struct list_head *connector_list = &dev->mode_config.connector_list;
 	struct intel_dp *intel_dp;
 
-	if (!dev)
-		return -ENODEV;
-
 	list_for_each_entry(connector, connector_list, head) {
 
 		if (connector->connector_type !=
@@ -4151,9 +4138,6 @@ static int i915_displayport_test_data_show(struct seq_file *m, void *data)
 	struct list_head *connector_list = &dev->mode_config.connector_list;
 	struct intel_dp *intel_dp;
 
-	if (!dev)
-		return -ENODEV;
-
 	list_for_each_entry(connector, connector_list, head) {
 
 		if (connector->connector_type !=
@@ -4192,9 +4176,6 @@ static int i915_displayport_test_type_show(struct seq_file *m, void *data)
 	struct drm_connector *connector;
 	struct list_head *connector_list = &dev->mode_config.connector_list;
 	struct intel_dp *intel_dp;
-
-	if (!dev)
-		return -ENODEV;
 
 	list_for_each_entry(connector, connector_list, head) {
 
