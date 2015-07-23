500f11946a128727b8ebf79697babc1638152e59
Nicolas Dichtel <nicolas.dichtel@6wind.com>
2015-07-23 15:43:56 +0200

    lwtunnel: export linux/lwtunnel.h to userspace
    Note also that include/linux/lwtunnel.h is not needed.
    
    CC: Thomas Graf <tgraf@suug.ch>
    CC: Roopa Prabhu <roopa@cumulusnetworks.com>
    Fixes: 499a24256862 ("lwtunnel: infrastructure for handling light weight tunnels like mpls")
    Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Acked-by: Roopa Prabhu <roopa@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/lwtunnel.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _LINUX_LWTUNNEL_H_
-#define _LINUX_LWTUNNEL_H_
-
-#include <uapi/linux/lwtunnel.h>
-
-#endif /* _LINUX_LWTUNNEL_H_ */
