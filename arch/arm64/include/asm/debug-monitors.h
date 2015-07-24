a9aa59c3b6e80a4b82859e9c31f9c39d81576037
Dave P Martin <Dave.Martin@arm.com>
2015-07-24 16:37:44 +0100

    arm64/debug: More consistent naming for the BRK ESR template macro
    The naming of DBG_ESR_VAL_BRK is inconsistent with the way other
    similar macros are named.
    
    This patch makes the naming more consistent, and appends "64"
    as a reminder that this ESR pattern only matches from AArch64
    state.
    
    Signed-off-by: Dave Martin <Dave.Martin@arm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/debug-monitors.h
+++ arch/arm64/include/asm/debug-monitors.h
@@ -47,8 +47,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * ESR values expected for dynamic and compile time BRK instruction
  */
-#define DBG_ESR_VAL_BRK(x) \
-	((ESR_ELx_EC_BRK64 << ESR_ELx_EC_SHIFT) | ESR_ELx_IL | ((x) & 0xffff))
+#define ESR_ELx_VAL_BRK64(imm)					\
+	((ESR_ELx_EC_BRK64 << ESR_ELx_EC_SHIFT) | ESR_ELx_IL |	\
+	 ((imm) & 0xffff))
 
 /*
  * #imm16 values used for BRK instruction generation
