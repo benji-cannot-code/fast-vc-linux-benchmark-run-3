f36a5e33f8254ebf9791414be2fa2b7d1fb4d010
Rob Herring <robh@kernel.org>
2015-07-27 15:55:21 -0500

    PCI: Kill off set_irq_flags() usage
    set_irq_flags is ARM-specific with custom flags which have genirq
    equivalents.  Convert drivers to use the genirq interfaces directly, so we
    can kill off set_irq_flags.  The translation of flags is as follows:
    
      IRQF_VALID -> !IRQ_NOREQUEST
      IRQF_PROBE -> !IRQ_NOPROBE
      IRQF_NOAUTOEN -> IRQ_NOAUTOEN
    
    For IRQs managed by an irqdomain, the irqdomain core code handles clearing
    and setting IRQ_NOREQUEST already, so there is no need to do this in .map()
    functions, and we can simply remove the set_irq_flags calls.  Some users
    also modify IRQ_NOPROBE, and this has been maintained although it is not
    clear that is really needed.  There appears to be a great deal of blind
    copy and paste of this code.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Jingoo Han <jingoohan1@gmail.com>
    CC: Kishon Vijay Abraham I <kishon@ti.com>
    CC: Murali Karicheri <m-karicheri2@ti.com>
    CC: Thierry Reding <thierry.reding@gmail.com>
    CC: Stephen Warren <swarren@wwwdotorg.org>
    CC: Alexandre Courbot <gnurou@gmail.com>
    CC: Jingoo Han <jingoohan1@gmail.com>
    CC: Pratyush Anand <pratyush.anand@gmail.com>
    CC: Simon Horman <horms@verge.net.au>
    CC: Michal Simek <michal.simek@xilinx.com>
    CC: "Sören Brinkmann" <soren.brinkmann@xilinx.com>

--- drivers/pci/host/pcie-xilinx.c
+++ drivers/pci/host/pcie-xilinx.c
@@ -339,7 +339,6 @@ static int xilinx_pcie_msi_map(struct irq_domain *domain, unsigned int irq,
 {
 	irq_set_chip_and_handler(irq, &xilinx_msi_irq_chip, handle_simple_irq);
 	irq_set_chip_data(irq, domain->host_data);
-	set_irq_flags(irq, IRQF_VALID);
 
 	return 0;
 }
@@ -378,7 +377,6 @@ static int xilinx_pcie_intx_map(struct irq_domain *domain, unsigned int irq,
 {
 	irq_set_chip_and_handler(irq, &dummy_irq_chip, handle_simple_irq);
 	irq_set_chip_data(irq, domain->host_data);
-	set_irq_flags(irq, IRQF_VALID);
 
 	return 0;
 }
