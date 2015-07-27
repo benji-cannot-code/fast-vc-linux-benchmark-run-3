731b2e4fbd86e59eeb39e43644d9140c7b796479
Paul Burton <paul.burton@imgtec.com>
2015-07-27 12:58:23 -0700

    MIPS: AT_HWCAP aux vector infrastructure
    In order for userland to determine whether various features are safe to
    use, it will need to know both that the hardware supports those features
    and that the kernel is recent enough & configured appropriately to
    support them. For example under the O32 modeless FP proposal the dynamic
    linker & ifunc resolvers will need this information.  The kernel is the
    only thing in a position to know availability accurately, so the kernel
    needs to provide the information to userland. This patch introduces the
    infrastructure to provide the AT_HWCAP aux vector to userland in order
    to provide that information. It also defines the 2 currently specified
    flags, which indicate MIPSr6 & MSA support.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Matthew Fortune <matthew.fortune@imgtec.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Huacai Chen <chenhc@lemote.com>
    Cc: Maciej W. Rozycki <macro@linux-mips.org>
    Cc: Alex Smith <alex@alex-smith.me.uk>
    Cc: linux-kernel@vger.kernel.org
    Cc: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Cc: Markos Chandras <markos.chandras@imgtec.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Patchwork: https://patchwork.linux-mips.org/patch/10797/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/cpu-probe.c
+++ arch/mips/kernel/cpu-probe.c
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/spram.h>
 #include <asm/uaccess.h>
 
+/* Hardware capabilities */
+unsigned int elf_hwcap __read_mostly;
+
 /*
  * Get the FPU Implementation/Revision.
  */
