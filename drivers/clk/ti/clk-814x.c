fda94937021bde8413ce91899d7e5040844a8b39
Stephen Rothwell <sfr@canb.auug.org.au>
2015-07-27 14:11:17 +1000

    clk: ti: fix for definition movement
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- drivers/clk/ti/clk-814x.c
+++ drivers/clk/ti/clk-814x.c
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk-provider.h>
 #include <linux/clk/ti.h>
 
+#include "clock.h"
+
 static struct ti_dt_clk dm814_clks[] = {
 	DT_CLK(NULL, "devosc_ck", "devosc_ck"),
 	DT_CLK(NULL, "mpu_ck", "mpu_ck"),
