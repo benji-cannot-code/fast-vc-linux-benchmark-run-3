9cbe3764639c04ccca5e15c5afe7b67bceb1c177
zilong.liu <liuziloong@gmail.com>
2015-07-28 15:12:18 -0400

    ext4 crypto: remove duplicate header file
    Remove key.h which is included twice in crypto_fname.c
    
    Signed-off-by: zilong.liu <liuziloong@gmail.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/crypto_fname.c
+++ fs/ext4/crypto_fname.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gfp.h>
 #include <linux/kernel.h>
 #include <linux/key.h>
-#include <linux/key.h>
 #include <linux/list.h>
 #include <linux/mempool.h>
 #include <linux/random.h>
