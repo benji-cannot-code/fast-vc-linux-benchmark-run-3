5ae4345848b34365476d988abef21192e8c04c4e
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-07-28 23:31:40 +0200

    staging: rtl8192e: Remove rtl8192_data_hard_resume
    Function is empty; rtllib checks if handler is NULL.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_core.c
+++ drivers/staging/rtl8192e/rtl8192e/rtl_core.c
@@ -91,7 +91,6 @@ static struct pci_driver rtl8192_pci_driver = {
 static short rtl8192_is_tx_queue_empty(struct net_device *dev);
 static void rtl819x_watchdog_wqcallback(void *data);
 static void watch_dog_timer_callback(unsigned long data);
-static void rtl8192_data_hard_resume(struct net_device *dev);
 static void rtl8192_hard_data_xmit(struct sk_buff *skb, struct net_device *dev,
 				   int rate);
 static int rtl8192_hard_start_xmit(struct sk_buff *skb, struct net_device *dev);
@@ -866,7 +865,6 @@ static void rtl8192_init_priv_handler(struct net_device *dev)
 	priv->rtllib->set_chan			= rtl8192_set_chan;
 	priv->rtllib->link_change		= priv->ops->link_change;
 	priv->rtllib->softmac_data_hard_start_xmit = rtl8192_hard_data_xmit;
-	priv->rtllib->data_hard_resume		= rtl8192_data_hard_resume;
 	priv->rtllib->check_nic_enough_desc	= rtl8192_check_nic_enough_desc;
 	priv->rtllib->handle_assoc_response	= rtl8192_handle_assoc_response;
 	priv->rtllib->handle_beacon		= rtl8192_handle_beacon;
@@ -1366,8 +1364,6 @@ RESET_START:
 
 			rtllib_start_send_beacons(ieee);
 
-			if (ieee->data_hard_resume)
-				ieee->data_hard_resume(ieee->dev);
 			netif_carrier_on(ieee->dev);
 		} else if (ieee->iw_mode == IW_MODE_MESH) {
 			rtl819x_silentreset_mesh_bk(dev, IsPortal);
@@ -1638,10 +1634,6 @@ static void rtl8192_free_tx_ring(struct net_device *dev, unsigned int prio)
 	pci_free_consistent(priv->pdev, sizeof(*ring->desc)*ring->entries,
 	ring->desc, ring->dma);
 	ring->desc = NULL;
-}
-
-static void rtl8192_data_hard_resume(struct net_device *dev)
-{
 }
 
 static void rtl8192_hard_data_xmit(struct sk_buff *skb, struct net_device *dev,
