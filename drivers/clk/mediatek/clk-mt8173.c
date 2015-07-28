b2c345df3955de9b912b5debe7beb4bf16a079dc
James Liao <jamesjj.liao@mediatek.com>
2015-07-28 14:53:29 +0800

    clk: mediatek: Remove unused code from MT8173.
    Remove unused header files from MT8173, and remove unused
    keywords from function declaration.
    
    Signed-off-by: James Liao <jamesjj.liao@mediatek.com>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/clk/mediatek/clk-mt8173.c
+++ drivers/clk/mediatek/clk-mt8173.c
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
-#include <linux/slab.h>
-#include <linux/mfd/syscon.h>
 
 #include "clk-mtk.h"
 #include "clk-gate.h"
