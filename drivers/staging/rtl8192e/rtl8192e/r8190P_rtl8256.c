5e4a6cfebc1afe34635a7c0a9bb8b5179acd99dc
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-07-28 23:31:44 +0200

    staging: rtl8192e: Remove phy_RF8256_Config_ParaFile
    Merge function into rtl92e_config_rf as having them
    separated gives no benefit.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c
+++ drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.c
@@ -65,7 +65,7 @@ void rtl92e_set_bandwidth(struct net_device *dev,
 	}
 }
 
-static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
+bool rtl92e_config_rf(struct net_device *dev)
 {
 	u32	u4RegValue = 0;
 	u8	eRFPath;
@@ -77,6 +77,8 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 	u32	RF3_Final_Value = 0;
 	u8	ConstRetryTimes = 5, RetryTimes = 5;
 	u8 ret = 0;
+
+	priv->NumTotalRFPath = RTL819X_TOTAL_RF_PATH;
 
 	for (eRFPath = (enum rf90_radio_path)RF90_PATH_A;
 	     eRFPath < priv->NumTotalRFPath; eRFPath++) {
@@ -116,7 +118,7 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 		if (!rtStatus) {
 			netdev_err(dev, "%s(): Failed to check RF Path %d.\n",
 				   __func__, eRFPath);
-			goto phy_RF8256_Config_ParaFile_Fail;
+			goto fail;
 		}
 
 		RetryTimes = ConstRetryTimes;
@@ -204,7 +206,7 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 			netdev_err(dev,
 				   "%s(): Failed to initialize RF Path %d.\n",
 				   __func__, eRFPath);
-			goto phy_RF8256_Config_ParaFile_Fail;
+			goto fail;
 		}
 
 	}
@@ -212,16 +214,8 @@ static bool phy_RF8256_Config_ParaFile(struct net_device *dev)
 	RT_TRACE(COMP_PHY, "PHY Initialization Success\n");
 	return true;
 
-phy_RF8256_Config_ParaFile_Fail:
+fail:
 	return false;
-}
-
-bool rtl92e_config_rf(struct net_device *dev)
-{
-	struct r8192_priv *priv = rtllib_priv(dev);
-
-	priv->NumTotalRFPath = RTL819X_TOTAL_RF_PATH;
-	return phy_RF8256_Config_ParaFile(dev);
 }
 
 void rtl92e_set_cck_tx_power(struct net_device *dev, u8 powerlevel)
