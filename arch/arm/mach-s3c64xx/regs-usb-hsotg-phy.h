c0dd6183429264a58eabdd7c15889d592b11a674
Kukjin Kim <kgene@kernel.org>
2015-07-30 02:00:36 +0900

    ARM: SAMSUNG: local regs-usb-hsotg-phy header in mach-s3c64xx
    This patch moves regs-usb-hsotg-phy header file into mach-s3c64xx.
    Because it is not used for others except mach-s3c64xx.
    
    Cc: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Kukjin Kim <kgene@kernel.org>

--- arch/arm/mach-s3c64xx/regs-usb-hsotg-phy.h
+++ arch/arm/mach-s3c64xx/regs-usb-hsotg-phy.h
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* arch/arm/plat-s3c/include/plat/regs-usb-hsotg-phy.h
- *
+/*
  * Copyright 2008 Openmoko, Inc.
  * Copyright 2008 Simtec Electronics
  *      http://armlinux.simtec.co.uk/
