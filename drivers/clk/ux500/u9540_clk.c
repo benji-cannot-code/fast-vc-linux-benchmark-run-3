aa18a6468ae00f07074c7a05983e7285ed279307
Linus Walleij <linus.walleij@linaro.org>
2015-07-30 15:19:25 +0200

    clk/ARM: move Ux500 PRCC bases to the device tree
    The base addresses for the Ux500 PRCC controllers are hardcoded,
    let's move them to the clock node in the device tree and delete
    the constants.
    
    Cc: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Olof Johansson <olof@lixom.net>
    Acked-by: Michael Turquette <mturquette@baylibre.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/ux500/u9540_clk.c
+++ drivers/clk/ux500/u9540_clk.c
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/clk-ux500.h>
 #include "clk.h"
 
-void u9540_clk_init(u32 clkrst1_base, u32 clkrst2_base, u32 clkrst3_base,
-		    u32 clkrst5_base, u32 clkrst6_base)
+void u9540_clk_init(void)
 {
 	/* register clocks here */
 }
