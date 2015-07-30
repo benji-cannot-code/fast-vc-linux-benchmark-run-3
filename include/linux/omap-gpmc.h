19e63c86851824d1feb262cbf049d7cd5ab6e410
Roger Quadros <rogerq@ti.com>
2015-07-30 14:49:23 +0300

    memory: omap-gpmc: Implement IRQ domain for NAND IRQs
    GPMC provides 2 interrupts for NAND use. i.e. fifoevent and termcount.
    Use IRQ domain for this. NAND device tree node can then
    get the necessary interrupts by using gpmc as the interrupt parent.
    
    Legacy boot uses gpmc_get_client_irq to get the
    NAND interrupts from the GPMC IRQ domain.
    Get rid of custom bitmasks and use IRQ domain for that
    as well.
    
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Acked-by: Tony Lindgren <tony@atomide.com>

--- include/linux/omap-gpmc.h
+++ include/linux/omap-gpmc.h
@@ -12,8 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define GPMC_CONFIG_WP		0x00000005
 
-#define GPMC_IRQ_FIFOEVENTENABLE	0x01
-#define GPMC_IRQ_COUNT_EVENT		0x02
+/* IRQ numbers in GPMC IRQ domain for legacy boot use */
+#define GPMC_IRQ_FIFOEVENTENABLE	0
+#define GPMC_IRQ_COUNT_EVENT		1
 
 /**
  * gpmc_nand_ops - Interface between NAND and GPMC
