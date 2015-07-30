094195ae9058812280575c39d85628639714fdff
Oleg Drokin <green@linuxhacker.ru>
2015-07-30 18:27:54 -0400

    staging/lustre: Drop FS_HAS_FIEMAP compat macro
    FS_HAS_FIEMAP was some sort of old RHEL5 construct that's not
    really important anymore
    
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/linux/lustre_compat25.h
+++ drivers/staging/lustre/lustre/include/linux/lustre_compat25.h
@@ -70,11 +70,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # define inode_dio_read(i)		atomic_inc(&(i)->i_dio_count)
 /* inode_dio_done(i) use as-is for read unlock */
 
-
-#ifndef FS_HAS_FIEMAP
-#define FS_HAS_FIEMAP			(0)
-#endif
-
 #ifndef SLAB_DESTROY_BY_RCU
 #define SLAB_DESTROY_BY_RCU 0
 #endif
