3d2368dbe7dbd4bcbc51f310268c8813011cc237
Michel Thierry <michel.thierry@intel.com>
2015-07-30 11:06:23 +0100

    drm/i915/gen8: Add 4 level switching infrastructure and lrc support
    In 64b (48bit canonical) PPGTT addressing, the PDP0 register contains
    the base address to PML4, while the other PDP registers are ignored.
    
    In LRC, the addressing mode must be specified in every context
    descriptor, and the base address to PML4 is stored in the reg state.
    
    v2: PML4 update in legacy context switch is left for historic reasons,
    the preferred mode of operation is with lrc context based submission.
    v3: s/gen8_map_page_directory/gen8_setup_page_directory and
    s/gen8_map_page_directory_pointer/gen8_setup_page_directory_pointer.
    Also, clflush will be needed for bxt. (Akash)
    v4: Squashed lrc-specific code and use a macro to set PML4 register.
    v5: Rebase after Mika's ppgtt cleanup / scratch merge patch series.
    PDP update in bb_start is only for legacy 32b mode.
    v6: Rebase after final merged version of Mika's ppgtt/scratch
    patches.
    v7: There is no need to update the pml4 register value in
    execlists_update_context. (Akash)
    v8: Move pd and pdp setup functions to a previous patch, they do not
    belong here. (Akash)
    v9: Check USES_FULL_48BIT_PPGTT instead of GEN8_CTX_ADDRESSING_MODE in
    gen8_emit_bb_start to check if emit pdps is needed. (Akash)
    
    Cc: Akash Goel <akash.goel@intel.com>
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Michel Thierry <michel.thierry@intel.com> (v2+)
    Reviewed-by: Akash Goel <akash.goel@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem_gtt.c
+++ drivers/gpu/drm/i915/i915_gem_gtt.c
@@ -651,8 +651,8 @@ static int gen8_write_pdp(struct drm_i915_gem_request *req,
 	return 0;
 }
 
-static int gen8_mm_switch(struct i915_hw_ppgtt *ppgtt,
-			  struct drm_i915_gem_request *req)
+static int gen8_legacy_mm_switch(struct i915_hw_ppgtt *ppgtt,
+				 struct drm_i915_gem_request *req)
 {
 	int i, ret;
 
@@ -665,6 +665,12 @@ static int gen8_mm_switch(struct i915_hw_ppgtt *ppgtt,
 	}
 
 	return 0;
+}
+
+static int gen8_48b_mm_switch(struct i915_hw_ppgtt *ppgtt,
+			      struct drm_i915_gem_request *req)
+{
+	return gen8_write_pdp(req, 0, px_dma(&ppgtt->pml4));
 }
 
 static void gen8_ppgtt_clear_pte_range(struct i915_address_space *vm,
@@ -1319,14 +1325,13 @@ static int gen8_ppgtt_init(struct i915_hw_ppgtt *ppgtt)
 	ppgtt->base.unbind_vma = ppgtt_unbind_vma;
 	ppgtt->base.bind_vma = ppgtt_bind_vma;
 
-	ppgtt->switch_mm = gen8_mm_switch;
-
 	if (USES_FULL_48BIT_PPGTT(ppgtt->base.dev)) {
 		ret = setup_px(ppgtt->base.dev, &ppgtt->pml4);
 		if (ret)
 			goto free_scratch;
 
 		ppgtt->base.total = 1ULL << 48;
+		ppgtt->switch_mm = gen8_48b_mm_switch;
 	} else {
 		ret = __pdp_init(false, &ppgtt->pdp);
 		if (ret)
@@ -1341,6 +1346,7 @@ static int gen8_ppgtt_init(struct i915_hw_ppgtt *ppgtt)
 			 */
 			ppgtt->base.total = to_i915(ppgtt->base.dev)->gtt.base.total;
 
+		ppgtt->switch_mm = gen8_legacy_mm_switch;
 		trace_i915_page_directory_pointer_entry_alloc(&ppgtt->base,
 							      0, 0,
 							      GEN8_PML4E_SHIFT);
@@ -1538,8 +1544,9 @@ static void gen8_ppgtt_enable(struct drm_device *dev)
 	int j;
 
 	for_each_ring(ring, dev_priv, j) {
+		u32 four_level = USES_FULL_48BIT_PPGTT(dev) ? GEN8_GFX_PPGTT_48B : 0;
 		I915_WRITE(RING_MODE_GEN7(ring),
-			   _MASKED_BIT_ENABLE(GFX_PPGTT_ENABLE));
+			   _MASKED_BIT_ENABLE(GFX_PPGTT_ENABLE | four_level));
 	}
 }
 
