614e63463fd075e4a67802f173b716f8fbc28b3e
Chunyan Zhang <zhang.chunyan@linaro.org>
2015-07-31 09:37:27 -0600

    coresight-etm3x: Support context-ID tracing when PID namespace is enabled
    The Coresight ETM drivers already support context-ID tracing, but it won't
    work when PID namespace is enabled. This is because when using PID
    namespace a process id (ie. VPID) seen from the current namespace differs
    from the id (ie. PID) seen by kernel.
    
    So when users write the process id seen by themselves to ETM, there needs
    to be a translation from VPID to PID, as such ETM drivers will write the
    PID into the Context ID register correctly.
    
    Signed-off-by: Chunyan Zhang <zhang.chunyan@linaro.org>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/coresight/coresight-etm.h
+++ drivers/hwtracing/coresight/coresight-etm.h
@@ -185,6 +185,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @seq_curr_state: current value of the sequencer register.
  * @ctxid_idx: index for the context ID registers.
  * @ctxid_pid: value for the context ID to trigger on.
+ * @ctxid_vpid:	Virtual PID seen by users if PID namespace is enabled, otherwise
+ *		the same value of ctxid_pid.
  * @ctxid_mask: mask applicable to all the context IDs.
  * @sync_freq:	Synchronisation frequency.
  * @timestamp_event: Defines an event that requests the insertion
@@ -237,6 +239,7 @@ struct etm_drvdata {
 	u32				seq_curr_state;
 	u8				ctxid_idx;
 	u32				ctxid_pid[ETM_MAX_CTXID_CMP];
+	u32				ctxid_vpid[ETM_MAX_CTXID_CMP];
 	u32				ctxid_mask;
 	u32				sync_freq;
 	u32				timestamp_event;
