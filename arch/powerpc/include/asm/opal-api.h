b3fee108cafb7bb5b6dc84b43b03be168263de0b
Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
2015-07-31 21:24:38 +0530

    powerpc/powernv: Invoke opal_cec_reboot2() on unrecoverable machine check errors.
    On non-recoverable MCE errors in kernel space, Linux kernel panics
    and system reboots. On BMC based system opal-prd runs as a daemon
    in the host. Hence, kernel crash may prevent opal-prd to detect and
    analyze this MCE error. This may land us in a situation where the faulty
    memory never gets de-configured and Linux would keep hitting same MCE error
    again and again. If this happens in early stage of kernel initialization,
    then Linux will keep crashing and rebooting in a loop.
    
    This patch fixes this issue by invoking new opal_cec_reboot2() call with
    reboot type OPAL_REBOOT_PLATFORM_ERROR to inform BMC/OCC about this
    error, so that BMC can collect relevant data for error analysis and
    decide what component to de-configure before rebooting.
    
    This patch is dependent on OPAL patchset posted on skiboot mailing list
    at https://lists.ozlabs.org/pipermail/skiboot/2015-July/001771.html that
    introduces opal_cec_reboot2() opal call.
    
    Signed-off-by: Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/opal-api.h
+++ arch/powerpc/include/asm/opal-api.h
@@ -155,7 +155,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OPAL_FLASH_WRITE			111
 #define OPAL_FLASH_ERASE			112
 #define OPAL_PRD_MSG				113
-#define OPAL_LAST				113
+#define OPAL_CEC_REBOOT2			116
+#define OPAL_LAST				116
 
 /* Device tree flags */
 
@@ -856,6 +857,12 @@ enum OpalSysTemp {
 /* Cooling EPOW */
 enum OpalSysCooling {
 	OPAL_SYSCOOL_INSF	= 0x0001, /* System insufficient cooling */
+};
+
+/* Argument to OPAL_CEC_REBOOT2() */
+enum {
+	OPAL_REBOOT_NORMAL		= 0,
+	OPAL_REBOOT_PLATFORM_ERROR	= 1,
 };
 
 #endif /* __ASSEMBLY__ */
