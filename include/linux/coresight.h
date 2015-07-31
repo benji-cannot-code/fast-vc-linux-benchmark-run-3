4d7dcae97e2ff16627e990ca113c3599fc2f215f
Mark Brown <broonie@kernel.org>
2015-07-31 09:37:30 -0600

    coresight: Fix implicit inclusion of linux/sched.h
    The patch "Coresight: Add an interface for supporting ETM3/4 Context ID
    tracing" adds uses of find_task_by_vpid() and task_pid_nr() from
    linux/sched.h but does not include that header causing build errors in
    at least an ARM allmodconfig where it is not implicitly included. Add an
    explicit include to fix that.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/coresight.h
+++ include/linux/coresight.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_CORESIGHT_H
 
 #include <linux/device.h>
+#include <linux/sched.h>
 
 /* Peripheral id registers (0xFD0-0xFEC) */
 #define CORESIGHT_PERIPHIDR4	0xfd0
