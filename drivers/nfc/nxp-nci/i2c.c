ff2a117d76d92ee3b43e8222fe8c91f157e4ef16
Guenter Roeck <linux@roeck-us.net>
2015-08-01 06:59:29 -0700

    NFC: nxp-nci: Include unaligned.h instead of access_ok.h
    Directly including access_ok.h can result in the following compile errors
    if an architecture such as ia64 does not support direct unaligned accesses.
    
    include/linux/unaligned/access_ok.h:7:19: error:
    	redefinition of 'get_unaligned_le16'
    include/linux/unaligned/le_struct.h:6:19: note:
    	previous definition of 'get_unaligned_le16' was here
    include/linux/unaligned/access_ok.h:12:19: error:
    	redefinition of 'get_unaligned_le32'
    include/linux/unaligned/le_struct.h:11:19: note:
    	previous definition of 'get_unaligned_le32' was here
    
    Include asm/unaligned.h instead and let the architecture decide which
    access functions to use.
    
    Cc: Clément Perrochaud <clement.perrochaud@effinnov.com>
    Cc: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/nxp-nci/i2c.c
+++ drivers/nfc/nxp-nci/i2c.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/platform_data/nxp-nci.h>
-#include <linux/unaligned/access_ok.h>
+#include <asm/unaligned.h>
 
 #include <net/nfc/nfc.h>
 
