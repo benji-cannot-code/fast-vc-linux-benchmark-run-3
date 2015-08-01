61c2fccfbe6aa82b46aec1c5592c67b1d41d7af4
Nicolas Iooss <nicolas.iooss_linux@m4x.org>
2015-08-01 21:32:17 +0800

    ACPI: fix acpi_debugfs_init prototype
    acpi_debugfs_init function is declared with return type int in
    drivers/acpi/internal.h when CONFIG_DEBUG_FS is enabled, but its
    definition in drivers/acpi/debugfs.c has return type void. This is due
    to commit aecad432fd68 ("ACPI: Cleanup custom_method debug stuff"),
    which changed the return type from int to void without updating the
    declaration.
    
    Fix this inconsistency by updating acpi_debugfs_init prototype.  While
    at it, include internal.h in debugfs.c so that the compiler can check
    that the declaration and definition remain compatible.
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss_linux@m4x.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/debugfs.c
+++ drivers/acpi/debugfs.c
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include <linux/acpi.h>
 
+#include "internal.h"
+
 #define _COMPONENT		ACPI_SYSTEM_COMPONENT
 ACPI_MODULE_NAME("debugfs");
 
