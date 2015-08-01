a0a29e9f90060157771ac44a5061374abc91f25c
Azael Avalos <coproscefalo@gmail.com>
2015-07-31 21:58:16 -0600

    toshiba_acpi: Bump driver version to 0.23
    Given that some features were added (/dev/toshiba_acpi device), some
    clean-ups and minor (cosmetic) changes all over the driver code, bump
    the driver version to 0.23 to reflect these overall changes.
    
    Signed-off-by: Azael Avalos <coproscefalo@gmail.com>
    Signed-off-by: Darren Hart <dvhart@linux.intel.com>

--- drivers/platform/x86/toshiba_acpi.c
+++ drivers/platform/x86/toshiba_acpi.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#define TOSHIBA_ACPI_VERSION	"0.22"
+#define TOSHIBA_ACPI_VERSION	"0.23"
 #define PROC_INTERFACE_VERSION	1
 
 #include <linux/kernel.h>
