a8fb7caaf76b7d18ca84d5da3daecb28066db36a
Tomas Winkler <tomas.winkler@intel.com>
2015-08-02 22:20:58 +0300

    mei: hbm: bump supported HBM version to 2.0
    HBM 2.0 version for Sunrise point Skylake (PCH) based devices
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/hw.h
+++ drivers/misc/mei/hw.h
@@ -39,8 +39,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * MEI Version
  */
-#define HBM_MINOR_VERSION                   1
-#define HBM_MAJOR_VERSION                   1
+#define HBM_MINOR_VERSION                   0
+#define HBM_MAJOR_VERSION                   2
 
 /*
  * MEI version with PGI support
