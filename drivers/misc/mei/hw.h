5876a414243c6495eb2a509b4215f6bd465c8446
Alexander Usyskin <alexander.usyskin@intel.com>
2015-08-02 22:20:54 +0300

    mei: me: d0i3: add d0i3 enter/exit state machine
    Rework mei power gating state machine to support entry and exit to and
    from D0i3 power state.
    The choice between legacy and D0i3 routines is conditioned on
    d0i3_supported flag.
    
    The patch introduces warning:
    drivers/misc/mei/hw-me.c:901:12: warning: ‘mei_me_d0i3_enter’ defined but not used [-Wunused-function]
    it will go away in consequent patch
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mei/hw.h
+++ drivers/misc/mei/hw.h
@@ -32,8 +32,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MEI_IAMTHIF_STALL_TIMER    12  /* HPS */
 #define MEI_IAMTHIF_READ_TIMER     10  /* HPS */
 
-#define MEI_PGI_TIMEOUT            1  /* PG Isolation time response 1 sec */
-#define MEI_HBM_TIMEOUT            1   /* 1 second */
+#define MEI_PGI_TIMEOUT             1  /* PG Isolation time response 1 sec */
+#define MEI_D0I3_TIMEOUT            5  /* D0i3 set/unset max response time */
+#define MEI_HBM_TIMEOUT             1  /* 1 second */
 
 /*
  * MEI Version
