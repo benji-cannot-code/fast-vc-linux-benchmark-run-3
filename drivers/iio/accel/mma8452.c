297f3495499e1b40b465f928073befe1df7a0aa8
Hartmut Knaack <knaack.h@gmx.de>
2015-08-02 22:43:47 +0200

    iio:accel:mma8452: drop double include
    One inclusion of linux/iio/trigger_consumer.h is sufficient.
    
    Fixes: 	ae6d9ce05691b ("iio: mma8452: Add support for interrupt driven triggers.")
    Signed-off-by: Hartmut Knaack <knaack.h@gmx.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/accel/mma8452.c
+++ drivers/iio/accel/mma8452.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
-#include <linux/iio/trigger_consumer.h>
 #include <linux/iio/buffer.h>
 #include <linux/iio/trigger.h>
 #include <linux/iio/trigger_consumer.h>
