d97a3e8c70501ea73563a0918fda7bb54b48d9bd
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-03 08:36:18 +0530

    cpufreq: remove redundant 'policy' field from user_policy
    Its always same as policy->policy, and there is no need to keep another
    copy of it. Remove it.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -55,7 +55,6 @@ struct cpufreq_cpuinfo {
 struct cpufreq_real_policy {
 	unsigned int		min;    /* in kHz */
 	unsigned int		max;    /* in kHz */
-	unsigned int		policy; /* see above */
 };
 
 struct cpufreq_policy {
