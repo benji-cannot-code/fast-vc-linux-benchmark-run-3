84728a376a5ce1f75aae4f7a67acfa7b8d12f053
Geert Uytterhoeven <geert+renesas@glider.be>
2015-08-04 20:03:11 +0200

    ARM: shmobile: Remove obsolete earlytimer registration
    The last caller of shmobile_earlytimer_init() was removed in commit
    c99cd90d98a98aa1 ("ARM: shmobile: r8a7779: Remove legacy SoC code").
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/common.h
+++ arch/arm/mach-shmobile/common.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ARCH_MACH_COMMON_H
 #define __ARCH_MACH_COMMON_H
 
-extern void shmobile_earlytimer_init(void);
 extern void shmobile_init_delay(void);
 struct twd_local_timer;
 extern void shmobile_boot_vector(void);
