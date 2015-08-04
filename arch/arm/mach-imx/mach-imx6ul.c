ec6e7e750f6ba093aab623050b07d4cba1c1b3d2
Anson Huang <b20788@freescale.com>
2015-08-05 01:48:37 +0800

    ARM: imx: add suspend/resume support for i.mx6ul
    This patch adds suspend function for i.MX6UL, it supports
    "standby" and "mem" mode, for "standby" mode, SoC will
    enter STOP mode only, while for "mem" mode, SoC will
    enter STOP mode and DDR IO will be set to low power
    mode.
    
    As i.MX6UL contains a "Cortex-A7" ARM core which has no
    PL310, so we need to avoid any PL310 operations during
    suspend/resume, also, we need to flush Cortex-A7's inernal
    L2 cache before suspend.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>

--- arch/arm/mach-imx/mach-imx6ul.c
+++ arch/arm/mach-imx/mach-imx6ul.c
@@ -68,6 +68,7 @@ static void __init imx6ul_init_machine(void)
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 	imx6ul_enet_init();
 	imx_anatop_init();
+	imx6ul_pm_init();
 }
 
 static void __init imx6ul_init_irq(void)
@@ -75,6 +76,7 @@ static void __init imx6ul_init_irq(void)
 	imx_init_revision_from_anatop();
 	imx_src_init();
 	irqchip_init();
+	imx6_pm_ccm_init("fsl,imx6ul-ccm");
 }
 
 static const char *imx6ul_dt_compat[] __initconst = {
