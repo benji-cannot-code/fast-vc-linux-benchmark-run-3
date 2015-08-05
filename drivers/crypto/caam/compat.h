dfaabf9d7989c33a3dab9161aead73dceedbab6f
Victoria Milhoan <vicki.milhoan@freescale.com>
2015-08-05 11:28:37 -0700

    crypto: caam - Enable and disable clocks on Freescale i.MX platforms
    ARM-based systems may disable clocking to the CAAM device on the
    Freescale i.MX platform for power management purposes.  This patch
    enables the required clocks when the CAAM module is initialized and
    disables the required clocks when the CAAM module is shut down.
    
    Signed-off-by: Victoria Milhoan <vicki.milhoan@freescale.com>
    Tested-by: Horia Geantă <horia.geanta@freescale.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/caam/compat.h
+++ drivers/crypto/caam/compat.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/debugfs.h>
 #include <linux/circ_buf.h>
+#include <linux/clk.h>
 #include <net/xfrm.h>
 
 #include <crypto/algapi.h>
