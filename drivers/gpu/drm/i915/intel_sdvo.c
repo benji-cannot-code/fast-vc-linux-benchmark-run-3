8e688c71b44b8901b7388169928d4018797a2c1b
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2015-08-05 12:37:00 +0200

    drm/i915: Validate the state after an atomic modeset only, and pass the state.
    First step in removing dpms and validating atomic state.
    
    There can still be a mismatch in the connector state because the dpms
    callbacks are still used, but this can not happen immediately after a modeset.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Ander Conselvan de Oliveira <conselvan2@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_sdvo.c
+++ drivers/gpu/drm/i915/intel_sdvo.c
@@ -1551,8 +1551,6 @@ static int intel_sdvo_dpms(struct drm_connector *connector, int mode)
 		intel_sdvo_set_active_outputs(intel_sdvo, intel_sdvo->attached_output);
 	}
 
-	intel_modeset_check_state(connector->dev);
-
 	return 0;
 }
 
