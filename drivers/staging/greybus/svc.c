597e7bea8965a8740aa6280322a67ad6b3d49898
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-06 12:44:54 +0530

    greybus: svc: Print expected/actual payload size on mismatch
    Print (expected-payload-size actual-payload-size), when the size doesn't
    match for requests received by the module. This gives more details
    required for debugging the issue.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/svc.c
+++ drivers/staging/greybus/svc.c
@@ -270,7 +270,8 @@ static int gb_svc_intf_hotplug_recv(struct gb_operation *op)
 	int ret;
 
 	if (request->payload_size < sizeof(*hotplug)) {
-		dev_err(dev, "%s: short hotplug request received\n", __func__);
+		dev_err(dev, "%s: short hotplug request received (%zu < %zu)\n",
+			__func__, request->payload_size, sizeof(*hotplug));
 		return -EINVAL;
 	}
 
@@ -373,7 +374,8 @@ static int gb_svc_intf_hot_unplug_recv(struct gb_operation *op)
 
 	if (request->payload_size < sizeof(*hot_unplug)) {
 		dev_err(&op->connection->dev,
-			"short hot unplug request received\n");
+			"short hot unplug request received (%zu < %zu)\n",
+			request->payload_size, sizeof(*hot_unplug));
 		return -EINVAL;
 	}
 
@@ -401,7 +403,8 @@ static int gb_svc_intf_reset_recv(struct gb_operation *op)
 
 	if (request->payload_size < sizeof(*reset)) {
 		dev_err(&op->connection->dev,
-			"short reset request received\n");
+			"short reset request received (%zu < %zu)\n",
+			request->payload_size, sizeof(*reset));
 		return -EINVAL;
 	}
 	reset = request->payload;
