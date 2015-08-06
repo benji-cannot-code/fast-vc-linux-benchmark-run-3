e140c06fea4e68eb882f844df3a8d927b3259eb8
Felipe Balbi <balbi@ti.com>
2015-08-06 11:23:32 -0500

    usb: gadget: f_mass_storage: add mising <linux/uaccess.h>
    <linux/uaccess.h> was originally being pulled
    indirectly through some other header, however
    it's not anymore, so we need to include it
    directly
    
    Reported-by: Jim Davis <jim.epost@gmail.com>
    Suggested-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -215,6 +215,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/freezer.h>
 #include <linux/module.h>
+#include <linux/uaccess.h>
 
 #include <linux/usb/ch9.h>
 #include <linux/usb/gadget.h>
