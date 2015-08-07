523daa4564c639386e95bf224a6357df2efb4e5e
Leilk Liu <leilk.liu@mediatek.com>
2015-08-07 15:19:50 +0800

    spi: mediatek: Add spi bus for Mediatek MT8173
    This patch adds basic spi bus for MT8173.
    
    Signed-off-by: Leilk Liu <leilk.liu@mediatek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

---
+++ include/linux/platform_data/spi-mt65xx.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ *  MTK SPI bus driver definitions
+ *
+ * Copyright (c) 2015 MediaTek Inc.
+ * Author: Leilk Liu <leilk.liu@mediatek.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef ____LINUX_PLATFORM_DATA_SPI_MTK_H
+#define ____LINUX_PLATFORM_DATA_SPI_MTK_H
+
+/* Board specific platform_data */
+struct mtk_chip_config {
+	u32 tx_mlsb;
+	u32 rx_mlsb;
+	u32 tx_endian;
+	u32 rx_endian;
+};
+#endif
