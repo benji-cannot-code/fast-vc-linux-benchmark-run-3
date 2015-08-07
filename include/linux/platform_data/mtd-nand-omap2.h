97991d4d0f57b24019a7a56e1e26e80fea258a7a
Roger Quadros <rogerq@ti.com>
2015-08-07 10:38:13 +0300

    memory: omap-gpmc: Prevent GPMC_STATUS from being accessed via gpmc_regs
    GPMC_STATUS register is private to the GPMC module and must not be
    accessed directly by NAND driver through the gpmc_regs.
    
    They must use gpmc_omap_get_nand_ops() instead.
    
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>

--- include/linux/platform_data/mtd-nand-omap2.h
+++ include/linux/platform_data/mtd-nand-omap2.h
@@ -46,7 +46,6 @@ enum omap_ecc {
 };
 
 struct gpmc_nand_regs {
-	void __iomem	*gpmc_status;
 	void __iomem	*gpmc_nand_command;
 	void __iomem	*gpmc_nand_address;
 	void __iomem	*gpmc_nand_data;
@@ -65,6 +64,8 @@ struct gpmc_nand_regs {
 	void __iomem	*gpmc_bch_result4[GPMC_BCH_NUM_REMAINDER];
 	void __iomem	*gpmc_bch_result5[GPMC_BCH_NUM_REMAINDER];
 	void __iomem	*gpmc_bch_result6[GPMC_BCH_NUM_REMAINDER];
+	/* Deprecated. Do not use */
+	void __iomem	*gpmc_status;
 };
 
 struct omap_nand_platform_data {
