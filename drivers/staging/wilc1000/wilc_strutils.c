9208b667c7a9d1a3db02179ca963ca16c375bfaa
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-08-10 11:33:21 +0900

    staging: wilc1000: remove WILC_memcpy_INTERNAL
    Remove WILC_memcpy_INTERNAL that is used in the WILC_memcpy
    because WILC_memcpy is replaced by memcpy.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_strutils.c
+++ drivers/staging/wilc1000/wilc_strutils.c
@@ -7,15 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 
-/*!
- *  @author	syounan
- *  @date	18 Aug 2010
- *  @version	1.0
- */
-void WILC_memcpy_INTERNAL(void *pvTarget, const void *pvSource, u32 u32Count)
-{
-	memcpy(pvTarget, pvSource, u32Count);
-}
 
 
 
--- drivers/staging/wilc1000/wilc_strutils.h
+++ drivers/staging/wilc1000/wilc_strutils.h
@@ -16,17 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wilc_errorsupport.h"
 
 
-/*!
- *  @brief	Internal implementation for memory copy
- *  @param[in]	pvTarget the target buffer to which the data is copied into
- *  @param[in]	pvSource pointer to the second memory location
- *  @param[in]	u32Count the size of the data to copy
- *  @note	this function should not be used directly, use WILC_memcpy instead
- *  @author	syounan
- *  @date	18 Aug 2010
- *  @version	1.0
- */
-void WILC_memcpy_INTERNAL(void *pvTarget, const void *pvSource, u32 u32Count);
 
 
 
