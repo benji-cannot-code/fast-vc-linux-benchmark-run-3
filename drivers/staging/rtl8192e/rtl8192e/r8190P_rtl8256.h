2b9a57a19d7087f80f8eb476935996a024775322
Joe Perches <joe@perches.com>
2015-08-10 14:51:29 -0700

    staging: rtl8192e: Remove unnecessary externs
    Using 'extern' is not necessary for function prototypes.
    
    Miscellanea:
    
    o Reflow alignments
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.h
+++ drivers/staging/rtl8192e/rtl8192e/r8190P_rtl8256.h
@@ -22,10 +22,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RTL8225H
 
 #define RTL819X_TOTAL_RF_PATH 2
-extern void rtl92e_set_bandwidth(struct net_device *dev,
-				 enum ht_channel_width Bandwidth);
-extern bool rtl92e_config_rf(struct net_device *dev);
-extern void rtl92e_set_cck_tx_power(struct net_device *dev, u8	powerlevel);
-extern void rtl92e_set_ofdm_tx_power(struct net_device *dev, u8 powerlevel);
+void rtl92e_set_bandwidth(struct net_device *dev,
+			  enum ht_channel_width Bandwidth);
+bool rtl92e_config_rf(struct net_device *dev);
+void rtl92e_set_cck_tx_power(struct net_device *dev, u8	powerlevel);
+void rtl92e_set_ofdm_tx_power(struct net_device *dev, u8 powerlevel);
 
 #endif
