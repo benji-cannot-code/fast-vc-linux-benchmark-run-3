df6d65d4ac91a73fc965ee00cc3c0f23192ac0e9
Alexander Aring <alex.aring@gmail.com>
2015-08-10 21:15:55 +0200

    mac802154: cfg: remove test and set checks
    This patch removes several checks if a value is really changed. This
    makes only sense if we have another layer call e.g. calling the
    driver_ops which is done by callbacks like "set_channel".
    
    For MAC settings which need to be set by phy registers (if the phy
    supports that handling) this is set by doing an interface up currently
    and are not direct driver_ops calls, so we remove the checks from these
    configuration callbacks.
    
    Reviewed-by: Stefan Schmidt <stefan@osg.samsung.com>
    Suggested-by: Phoebe Buckheister <phoebe.buckheister@itwm.fraunhofer.de>
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- net/mac802154/cfg.c
+++ net/mac802154/cfg.c
@@ -210,10 +210,6 @@ ieee802154_set_backoff_exponent(struct wpan_phy *wpan_phy,
 {
 	ASSERT_RTNL();
 
-	if (wpan_dev->min_be == min_be &&
-	    wpan_dev->max_be == max_be)
-		return 0;
-
 	wpan_dev->min_be = min_be;
 	wpan_dev->max_be = max_be;
 	return 0;
@@ -224,9 +220,6 @@ ieee802154_set_short_addr(struct wpan_phy *wpan_phy, struct wpan_dev *wpan_dev,
 			  __le16 short_addr)
 {
 	ASSERT_RTNL();
-
-	if (wpan_dev->short_addr == short_addr)
-		return 0;
 
 	wpan_dev->short_addr = short_addr;
 	return 0;
@@ -239,9 +232,6 @@ ieee802154_set_max_csma_backoffs(struct wpan_phy *wpan_phy,
 {
 	ASSERT_RTNL();
 
-	if (wpan_dev->csma_retries == max_csma_backoffs)
-		return 0;
-
 	wpan_dev->csma_retries = max_csma_backoffs;
 	return 0;
 }
@@ -253,9 +243,6 @@ ieee802154_set_max_frame_retries(struct wpan_phy *wpan_phy,
 {
 	ASSERT_RTNL();
 
-	if (wpan_dev->frame_retries == max_frame_retries)
-		return 0;
-
 	wpan_dev->frame_retries = max_frame_retries;
 	return 0;
 }
@@ -265,9 +252,6 @@ ieee802154_set_lbt_mode(struct wpan_phy *wpan_phy, struct wpan_dev *wpan_dev,
 			bool mode)
 {
 	ASSERT_RTNL();
-
-	if (wpan_dev->lbt == mode)
-		return 0;
 
 	wpan_dev->lbt = mode;
 	return 0;
