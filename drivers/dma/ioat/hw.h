b5ba2079a0df38b860c07bcc002e583923dc6df0
Dave Jiang <dave.jiang@intel.com>
2015-08-11 08:48:04 -0700

    dmaengine: ioatdma: deprecating and removal of old ioatdma devices
    Removal of any devices that are ioatdma pre-3.0. This is the first step
    in attempting to clean up the ioatdma driver and remove hw no longer
    supported.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Acked-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/ioat/hw.h
+++ drivers/dma/ioat/hw.h
@@ -22,11 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IOAT_MMIO_BAR		0
 
 /* CB device ID's */
-#define IOAT_PCI_DID_5000       0x1A38
-#define IOAT_PCI_DID_CNB        0x360B
-#define IOAT_PCI_DID_SCNB       0x65FF
-#define IOAT_PCI_DID_SNB        0x402F
-
 #define PCI_DEVICE_ID_INTEL_IOAT_IVB0	0x0e20
 #define PCI_DEVICE_ID_INTEL_IOAT_IVB1	0x0e21
 #define PCI_DEVICE_ID_INTEL_IOAT_IVB2	0x0e22
