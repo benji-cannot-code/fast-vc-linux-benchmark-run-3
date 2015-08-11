965752d141c7ac914f09f4e215941e0d8abe39e7
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2015-08-11 18:58:31 -0300

    [media] c8sectpfe: fix pinctrl dependencies
    compiling on some archs fail with:
    
    drivers/media/platform/sti/c8sectpfe/c8sectpfe-core.c:540:8: error: implicit declaration of function ‘pinctrl_select_state’ [-Werror=implicit-function-declaration]
      ret = pinctrl_select_state(fei->pinctrl, tsin->pstate);
    
    That's due the need of including pinctrl.h header and because
    CONFIG_PINCTRL needs to be true.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/sti/c8sectpfe/c8sectpfe-core.c
+++ drivers/media/platform/sti/c8sectpfe/c8sectpfe-core.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/time.h>
 #include <linux/version.h>
 #include <linux/wait.h>
+#include <linux/pinctrl/pinctrl.h>
 
 #include "c8sectpfe-core.h"
 #include "c8sectpfe-common.h"
