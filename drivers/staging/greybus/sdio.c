03a06d22955d3706e566322aca648370d530aaa6
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-11 07:36:09 +0530

    greybus: sdio: Drop get_version support
    This is done from a common place now, no need to replicate it.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/sdio.c
+++ drivers/staging/greybus/sdio.c
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct gb_sdio_host {
 	struct gb_connection	*connection;
-	u8			version_major;
-	u8			version_minor;
 	struct mmc_host		*mmc;
 	struct mmc_request	*mrq;
 	struct mutex		lock;	/* lock for this host */
@@ -36,9 +34,6 @@ struct gb_sdio_host {
 };
 
 static struct workqueue_struct *gb_sdio_mrq_workqueue;
-
-/* Define get_version() routine */
-define_get_version(gb_sdio_host, SDIO);
 
 #define GB_SDIO_RSP_R1_R5_R6_R7	(GB_SDIO_RSP_PRESENT | GB_SDIO_RSP_CRC | \
 				 GB_SDIO_RSP_OPCODE)
@@ -695,10 +690,6 @@ static int gb_sdio_connection_init(struct gb_connection *connection)
 
 	host->connection = connection;
 	connection->private = host;
-
-	ret = get_version(host);
-	if (ret < 0)
-		goto free_mmc;
 
 	ret = gb_sdio_get_caps(host);
 	if (ret < 0)
