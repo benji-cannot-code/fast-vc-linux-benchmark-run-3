db2acd6dcff45933debf0e46f2c09601fb84d1fe
Dave Jiang <dave.jiang@intel.com>
2015-08-11 08:48:16 -0700

    dmaengine: ioatdma: remove ioatdma v2 registration
    Removal of support for ioatdma v2 device support.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Acked-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/ioat/dma.c
+++ drivers/dma/ioat/dma.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 #include <linux/workqueue.h>
 #include <linux/prefetch.h>
-#include <linux/i7300_idle.h>
 #include "dma.h"
 #include "registers.h"
 #include "hw.h"
