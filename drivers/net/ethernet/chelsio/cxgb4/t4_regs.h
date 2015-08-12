5a69d83cc0a9d262a8252011d70e9c687099d67b
Hariprasad Shenai <hariprasad@chelsio.com>
2015-08-12 16:55:05 +0530

    cxgb4: Differentiates between TIDs being used in TCAM and HASH
    For the tid info, differentiate from which region the TID is allocated
    from. It can be from TCAM region or HASH region.
    
    Signed-off-by: Hariprasad Shenai <hariprasad@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/cxgb4/t4_regs.h
+++ drivers/net/ethernet/chelsio/cxgb4/t4_regs.h
@@ -2737,6 +2737,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LE_DB_HASH_TBL_BASE_ADDR_A 0x19c30
 #define LE_DB_INT_CAUSE_A 0x19c3c
 #define LE_DB_TID_HASHBASE_A 0x19df8
+#define T6_LE_DB_HASH_TID_BASE_A 0x19df8
 
 #define HASHEN_S    20
 #define HASHEN_V(x) ((x) << HASHEN_S)
