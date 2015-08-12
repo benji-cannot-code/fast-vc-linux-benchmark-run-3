57a7357f3eea1d77d6e4713bea67846c23d0f736
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-12 09:19:33 +0530

    greybus: Add firmware protocol driver
    This adds firmware protocol driver based on the latest specs available
    on mailing lists. This uses the firmware framework present in kernel.
    
    Refer Documentation/firmware_class/README on how it works.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

---
+++ drivers/staging/greybus/firmware.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Greybus firmware code
+ *
+ * Copyright 2015 Google Inc.
+ * Copyright 2015 Linaro Ltd.
+ *
+ * Released under the GPLv2 only.
+ */
+
+#ifndef __FIRMWARE_H
+#define __FIRMWARE_H
+
+int gb_firmware_protocol_init(void);
+void gb_firmware_protocol_exit(void);
+
+#endif /* __FIRMWARE_H */
