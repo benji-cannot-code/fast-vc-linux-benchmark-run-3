996dc31ddf50a6f119f9d8d070fc3fe74cc19b53
Chanwoo Choi <cw00.choi@samsung.com>
2015-08-13 09:19:43 +0900

    extcon: palmas: Fix build break due to devm_gpiod_get_optional API change
    With commit b17d1bf16cc7 ("gpio: make flags mandatory for gpiod_get functions")
    it becomes necessary to pass the flags argument. And this patch adds the gpio
    header file to fix the build breakage when build testing with random
    configuration files.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- drivers/extcon/extcon-palmas.c
+++ drivers/extcon/extcon-palmas.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/of_gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/workqueue.h>
 
 #define USB_GPIO_DEBOUNCE_MS	20	/* ms */
@@ -209,7 +210,8 @@ static int palmas_usb_probe(struct platform_device *pdev)
 			palmas_usb->wakeup = pdata->wakeup;
 	}
 
-	palmas_usb->id_gpiod = devm_gpiod_get_optional(&pdev->dev, "id");
+	palmas_usb->id_gpiod = devm_gpiod_get_optional(&pdev->dev, "id",
+							GPIOD_IN);
 	if (IS_ERR(palmas_usb->id_gpiod)) {
 		dev_err(&pdev->dev, "failed to get id gpio\n");
 		return PTR_ERR(palmas_usb->id_gpiod);
