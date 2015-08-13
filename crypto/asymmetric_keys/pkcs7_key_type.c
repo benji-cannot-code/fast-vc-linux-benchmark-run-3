fc84129f9135a45584fbc40a45abfabe1175c0e1
David Howells <dhowells@redhat.com>
2015-08-13 02:51:33 +0100

    PKCS#7: Add MODULE_LICENSE() to test module
    Add a MODULE_LICENSE() line to the PKCS#7 test key module to fix this
    warning:
    
    	WARNING: modpost: missing MODULE_LICENSE() in
    	crypto/asymmetric_keys/pkcs7_test_key.o
    
    Whilst we're at it, also add a module description.
    
    Reported-by: James Morris <jmorris@namei.org>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- crypto/asymmetric_keys/pkcs7_key_type.c
+++ crypto/asymmetric_keys/pkcs7_key_type.c
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <keys/system_keyring.h>
 #include "pkcs7_parser.h"
 
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("PKCS#7 testing key type");
+
 static unsigned pkcs7_usage;
 module_param_named(usage, pkcs7_usage, uint, S_IWUSR | S_IRUGO);
 MODULE_PARM_DESC(pkcs7_usage,
