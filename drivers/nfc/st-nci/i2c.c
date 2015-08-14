a8f7da1f2a0729a7fd23a70f7beddf275a3ef00e
Christophe Ricard <christophe.ricard@gmail.com>
2015-08-14 22:33:30 +0200

    nfc: st-nci: Remove duplicate file platform_data/st_nci.h
    commit "nfc: st-nci: Rename st21nfcb to st-nci" adds
    include/linux/platform_data/st_nci.h duplicated with
    include/linux/platform_data/st-nci.h.
    
    Only drivers/nfc/st-nci/i2c.c uses platform_data/st_nci.h.
    
    Cc: stable@vger.kernel.org
    Reported-by: Hauke Mehrtens <hauke@hauke-m.de>
    Signed-off-by: Christophe Ricard <christophe-h.ricard@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/st-nci/i2c.c
+++ drivers/nfc/st-nci/i2c.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/nfc.h>
-#include <linux/platform_data/st_nci.h>
+#include <linux/platform_data/st-nci.h>
 
 #include "ndlc.h"
 
