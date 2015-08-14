99462e4f60b4a4c9adf4985605e0e9f926d6b8e6
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-14 07:57:38 +0530

    greybus: Drop protocol specific _PROTOCOL_VERSION and _INVALID macros
    Greybus core supports protocol independent macros for this now, use
    them.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/greybus_protocols.h
+++ drivers/staging/greybus/greybus_protocols.h
@@ -119,8 +119,6 @@ struct gb_protocol_version_response {
 #define GB_CONTROL_VERSION_MINOR		0x01
 
 /* Greybus control request types */
-#define GB_CONTROL_TYPE_INVALID			0x00
-#define GB_CONTROL_TYPE_PROTOCOL_VERSION	0x01
 #define GB_CONTROL_TYPE_PROBE_AP		0x02
 #define GB_CONTROL_TYPE_GET_MANIFEST_SIZE	0x03
 #define GB_CONTROL_TYPE_GET_MANIFEST		0x04
@@ -155,8 +153,6 @@ struct gb_control_disconnected_request {
 #define GB_FIRMWARE_VERSION_MINOR		0x01
 
 /* Greybus firmware request types */
-#define GB_FIRMWARE_TYPE_INVALID		0x00
-#define GB_FIRMWARE_TYPE_PROTOCOL_VERSION	0x01
 #define GB_FIRMWARE_TYPE_FIRMWARE_SIZE		0x02
 #define GB_FIRMWARE_TYPE_GET_FIRMWARE		0x03
 #define GB_FIRMWARE_TYPE_READY_TO_BOOT		0x04
@@ -266,8 +262,6 @@ struct gb_battery_voltage_response {
 #define GB_HID_VERSION_MINOR		0x01
 
 /* Greybus HID operation types */
-#define GB_HID_TYPE_INVALID		0x00
-#define GB_HID_TYPE_PROTOCOL_VERSION	0x01
 #define GB_HID_TYPE_GET_DESC		0x02
 #define GB_HID_TYPE_GET_REPORT_DESC	0x03
 #define GB_HID_TYPE_PWR_ON		0x04
@@ -318,8 +312,6 @@ struct gb_hid_input_report_request {
 #define GB_I2C_VERSION_MINOR		0x01
 
 /* Greybus i2c request types */
-#define GB_I2C_TYPE_INVALID		0x00
-#define GB_I2C_TYPE_PROTOCOL_VERSION	0x01
 #define GB_I2C_TYPE_FUNCTIONALITY	0x02
 #define GB_I2C_TYPE_TIMEOUT		0x03
 #define GB_I2C_TYPE_RETRIES		0x04
@@ -375,8 +367,6 @@ struct gb_i2c_transfer_response {
 #define GB_GPIO_VERSION_MINOR		0x01
 
 /* Greybus GPIO request types */
-#define GB_GPIO_TYPE_INVALID		0x00
-#define GB_GPIO_TYPE_PROTOCOL_VERSION	0x01
 #define GB_GPIO_TYPE_LINE_COUNT		0x02
 #define GB_GPIO_TYPE_ACTIVATE		0x03
 #define GB_GPIO_TYPE_DEACTIVATE		0x04
@@ -480,8 +470,6 @@ struct gb_gpio_irq_event_request {
 #define GB_PWM_VERSION_MINOR		0x01
 
 /* Greybus PWM operation types */
-#define GB_PWM_TYPE_INVALID		0x00
-#define GB_PWM_TYPE_PROTOCOL_VERSION	0x01
 #define GB_PWM_TYPE_PWM_COUNT		0x02
 #define GB_PWM_TYPE_ACTIVATE		0x03
 #define GB_PWM_TYPE_DEACTIVATE		0x04
@@ -524,7 +512,6 @@ struct gb_pwm_disable_request {
 
 /* I2S */
 
-#define GB_I2S_MGMT_TYPE_PROTOCOL_VERSION		0x01
 #define GB_I2S_MGMT_TYPE_GET_SUPPORTED_CONFIGURATIONS	0x02
 #define GB_I2S_MGMT_TYPE_SET_CONFIGURATION		0x03
 #define GB_I2S_MGMT_TYPE_SET_SAMPLES_PER_MESSAGE	0x04
@@ -651,7 +638,6 @@ struct gb_i2s_mgmt_report_event_request {
 };
 /* report event response has no payload */
 
-#define GB_I2S_DATA_TYPE_PROTOCOL_VERSION		0x01
 #define GB_I2S_DATA_TYPE_SEND_DATA			0x02
 
 struct gb_i2s_send_data_request {
@@ -688,8 +674,6 @@ struct gb_i2s_send_data_request {
 #define GB_SPI_FLAG_NO_TX		BIT(2)		/* can't do buffer write */
 
 /* Greybus spi operation types */
-#define GB_SPI_TYPE_INVALID		0x00
-#define GB_SPI_TYPE_PROTOCOL_VERSION	0x01
 #define GB_SPI_TYPE_MODE		0x02
 #define GB_SPI_TYPE_FLAGS		0x03
 #define GB_SPI_TYPE_BITS_PER_WORD_MASK	0x04
@@ -752,8 +736,6 @@ struct gb_spi_transfer_response {
 #define GB_SVC_VERSION_MINOR		0x01
 
 /* Greybus SVC request types */
-#define GB_SVC_TYPE_INVALID		0x00
-#define GB_SVC_TYPE_PROTOCOL_VERSION	0x01
 #define GB_SVC_TYPE_SVC_HELLO		0x02
 #define GB_SVC_TYPE_INTF_DEVICE_ID	0x03
 #define GB_SVC_TYPE_INTF_HOTPLUG	0x04
@@ -847,8 +829,6 @@ struct gb_raw_send_request {
 #define GB_UART_VERSION_MINOR		0x01
 
 /* Greybus UART operation types */
-#define GB_UART_TYPE_INVALID			0x00
-#define GB_UART_TYPE_PROTOCOL_VERSION		0x01
 #define GB_UART_TYPE_SEND_DATA			0x02
 #define GB_UART_TYPE_RECEIVE_DATA		0x03	/* Unsolicited data */
 #define GB_UART_TYPE_SET_LINE_CODING		0x04
@@ -920,8 +900,6 @@ struct gb_uart_serial_state_request {
 #define GB_LOOPBACK_VERSION_MINOR		0x01
 
 /* Greybus loopback request types */
-#define GB_LOOPBACK_TYPE_INVALID		0x00
-#define GB_LOOPBACK_TYPE_PROTOCOL_VERSION	0x01
 #define GB_LOOPBACK_TYPE_PING			0x02
 #define GB_LOOPBACK_TYPE_TRANSFER		0x03
 #define GB_LOOPBACK_TYPE_SINK			0x04
@@ -941,8 +919,6 @@ struct gb_loopback_transfer_response {
 #define GB_SDIO_VERSION_MINOR		0x01
 
 /* Greybus SDIO operation types */
-#define GB_SDIO_TYPE_INVALID			0x00
-#define GB_SDIO_TYPE_PROTOCOL_VERSION		0x01
 #define GB_SDIO_TYPE_GET_CAPABILITIES		0x02
 #define GB_SDIO_TYPE_SET_IOS			0x03
 #define GB_SDIO_TYPE_COMMAND			0x04
