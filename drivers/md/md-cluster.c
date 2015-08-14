d352f0d9853846b9f25c018f06e22a39778c7c3d
NeilBrown <neilb@suse.com>
2015-08-14 10:22:00 +1000

    md-cluster: remove inappropriate try_module_get from join()
    md_setup_cluster already calls try_module_get(), so this
    try_module_get isn't needed.
    Also, there is no matching module_put (except in error patch),
    so this leaves an unbalanced module count.
    
    Signed-off-by: NeilBrown <neilb@suse.com>

--- drivers/md/md-cluster.c
+++ drivers/md/md-cluster.c
@@ -688,9 +688,6 @@ static int join(struct mddev *mddev, int nodes)
 	int ret, ops_rv;
 	char str[64];
 
-	if (!try_module_get(THIS_MODULE))
-		return -ENOENT;
-
 	cinfo = kzalloc(sizeof(struct md_cluster_info), GFP_KERNEL);
 	if (!cinfo)
 		return -ENOMEM;
@@ -772,7 +769,6 @@ err:
 		dlm_release_lockspace(cinfo->lockspace, 2);
 	mddev->cluster_info = NULL;
 	kfree(cinfo);
-	module_put(THIS_MODULE);
 	return ret;
 }
 
