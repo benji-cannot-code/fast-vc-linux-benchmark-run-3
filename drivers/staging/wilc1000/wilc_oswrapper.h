ce3a908fa7d6229ff30212fb061cce65459f2bd9
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2015-08-14 19:42:23 -0700

    staging: wilc1000: remove WILC_Sleep()
    It was just a wrapper around usleep_range() so call that directly
    instead and remove the now-empty file.
    
    Cc: Johnny Kim <johnny.kim@atmel.com>
    Cc: Rachel Kim <rachel.kim@atmel.com>
    Cc: Dean Lee <dean.lee@atmel.com>
    Cc: Chris Park <chris.park@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_oswrapper.h
+++ drivers/staging/wilc1000/wilc_oswrapper.h
@@ -24,9 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Error reporting and handling support */
 #include "wilc_errorsupport.h"
 
-/* Sleep support */
-#include "wilc_sleep.h"
-
 /* Timer support */
 #include "wilc_timer.h"
 
