79f43b295296442d630bd29e8d7afb6aad0d82e1
Len Brown <len.brown@intel.com>
2015-08-16 11:45:47 -0400

    x86/smpboot: Remove SIPI delays from cpu_up()
    MPS 1.4 example code shows the following required delays during processor
    on-lining:
    
    	INIT
    	 udelay(10,000)
    	SIPI
    	 udelay(200)
    	SIPI
    	 udelay(200) /* Linux actually implements this as udelay(300) */
    
    Linux skips the udelay(10,000) on modern processors.
    This patch removes the udelay(200) after each SIPI
    on those same processors.
    
    All three legacy delays can be restored by the cmdline
    "cpu_init_udelay=10000".
    
    As measured by analyze_suspend.py, this patch speeds
    processor resume time on my desktop from 2.4ms to 1.8ms, per AP.
    
    Signed-off-by: Len Brown <len.brown@intel.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: Jan H. Schönherr <jschoenh@amazon.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Zhu Guihua <zhugh.fnst@cn.fujitsu.com>
    Link: http://lkml.kernel.org/r/a5dfdbc8fbfdd813784da204aad5677fe459ac37.1439739165.git.len.brown@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -666,7 +666,8 @@ wakeup_secondary_cpu_via_init(int phys_apicid, unsigned long start_eip)
 		/*
 		 * Give the other CPU some time to accept the IPI.
 		 */
-		udelay(300);
+		if (init_udelay)
+			udelay(300);
 
 		pr_debug("Startup point 1\n");
 
@@ -676,7 +677,8 @@ wakeup_secondary_cpu_via_init(int phys_apicid, unsigned long start_eip)
 		/*
 		 * Give the other CPU some time to accept the IPI.
 		 */
-		udelay(200);
+		if (init_udelay)
+			udelay(200);
 
 		if (maxlvt > 3)		/* Due to the Pentium erratum 3AP.  */
 			apic_write(APIC_ESR, 0);
