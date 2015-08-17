b1b993efd8c691401a5ddfe7c37d7773516a3980
Dave Gordon <david.s.gordon@intel.com>
2015-08-17 17:30:52 +0100

    drm/i915: fix driver's versions of WARN_ON & WARN_ON_ONCE
    The current versions of these two macros don't work correctly if the
    argument expression happens to contain a modulo operator (%) -- when
    stringified, it gets interpreted as a printf formatting character!
    With a specifically crafted parameter, this could probably cause a
    kernel OOPS; consider WARN_ON(p%s) or WARN_ON(f %*pEp).
    
    Instead, we should use an explicit "%s" format, with the stringified
    expression as the coresponding literal-string argument.
    
    Signed-off-by: Dave Gordon <david.s.gordon@intel.com>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -69,11 +69,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		BUILD_BUG_ON(__i915_warn_cond); \
 	WARN(__i915_warn_cond, "WARN_ON(" #x ")"); })
 #else
-#define WARN_ON(x) WARN((x), "WARN_ON(" #x ")")
+#define WARN_ON(x) WARN((x), "WARN_ON(%s)", #x )
 #endif
 
 #undef WARN_ON_ONCE
-#define WARN_ON_ONCE(x) WARN_ONCE((x), "WARN_ON_ONCE(" #x ")")
+#define WARN_ON_ONCE(x) WARN_ONCE((x), "WARN_ON_ONCE(%s)", #x )
 
 #define MISSING_CASE(x) WARN(1, "Missing switch case (%lu) in %s\n", \
 			     (long) (x), __func__);
