f789ed2c111d5872391f9bd6c079fbb9b84881dc
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2015-08-17 11:17:57 -0700

    staging: wilc1000: delete wilc_log.h
    The macros are not used in the driver at all, except in one commented
    out line, so just remove the .h file so that no one thinks it is a good
    idea to add any code to use them in the future.
    
    Cc: Johnny Kim <johnny.kim@atmel.com>
    Cc: Rachel Kim <rachel.kim@atmel.com>
    Cc: Dean Lee <dean.lee@atmel.com>
    Cc: Chris Park <chris.park@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_oswrapper.h
+++ drivers/staging/wilc1000/wilc_oswrapper.h
@@ -18,9 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wilc_osconfig.h"
 #include "wilc_platform.h"
 
-/* Logging Functions */
-#include "wilc_log.h"
-
 /* Error reporting and handling support */
 #include "wilc_errorsupport.h"
 
