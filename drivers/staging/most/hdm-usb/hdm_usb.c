e783de2d3d51e977f5e98c6a663d1cba992afaf3
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-08-18 20:48:27 +0530

    staging: most: remove unused variable
    The variable conf was only assigned the value but was never used.
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/hdm-usb/hdm_usb.c
+++ drivers/staging/most/hdm-usb/hdm_usb.c
@@ -569,7 +569,6 @@ static void hdm_read_completion(struct urb *urb)
 	struct device *dev;
 	unsigned long flags;
 	unsigned int channel;
-	struct most_channel_config *conf;
 
 	mbo = urb->context;
 	anchor = mbo->priv;
@@ -582,8 +581,6 @@ static void hdm_read_completion(struct urb *urb)
 		complete(&anchor->urb_compl);
 		return;
 	}
-
-	conf = &mdev->conf[channel];
 
 	if (unlikely(urb->status && !(urb->status == -ENOENT ||
 				      urb->status == -ECONNRESET ||
