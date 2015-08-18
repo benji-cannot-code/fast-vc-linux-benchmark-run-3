62493d217dc9419760a3e563fd958a3e4d46be04
Frederic Weisbecker <fweisbec@gmail.com>
2015-08-18 16:18:28 +0200

    hrtimer: Simplify get_target_base() by returning current base
    Instead of fetching again the current cpu base, just take it from the
    parameter.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Link: http://lkml.kernel.org/r/1439907509-9553-2-git-send-email-fweisbec@gmail.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -184,7 +184,7 @@ struct hrtimer_cpu_base *get_target_base(struct hrtimer_cpu_base *base,
 					 int pinned)
 {
 	if (pinned || !base->migration_enabled)
-		return this_cpu_ptr(&hrtimer_bases);
+		return base;
 	return &per_cpu(hrtimer_bases, get_nohz_timer_target());
 }
 #else
@@ -192,7 +192,7 @@ static inline
 struct hrtimer_cpu_base *get_target_base(struct hrtimer_cpu_base *base,
 					 int pinned)
 {
-	return this_cpu_ptr(&hrtimer_bases);
+	return base;
 }
 #endif
 
