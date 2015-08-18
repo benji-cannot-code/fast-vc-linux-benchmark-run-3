25aa0a380c78d3f1b23985b72acf2565e6fd61e2
Grygorii Strashko <grygorii.strashko@ti.com>
2015-08-18 14:10:54 +0300

    gpio: omap: fix omap2_set_gpio_debounce
    According to TRMs:
    
    Required input line stable =
      (the value of the GPIO_DEBOUNCINGTIME[7:0].DEBOUNCETIME + 1) Ã— 31,
    where the value of the GPIO_DEBOUNCINGTIME[7:0].DEBOUNCETIME bit field
    is from 0 to 255.
    
    But now omap2_set_gpio_debounce() will calculate debounce time and
    behave incorrectly in the following cases:
    1) requested debounce time is !0 and <32
       calculated DEBOUNCETIME = 0x1 == 62 us;
       expected value of DEBOUNCETIME = 0x0 == 31us
    2) requested debounce time is 0
       calculated DEBOUNCETIME = 0x1 == 62 us;
       expected: disable debounce and DEBOUNCETIME = 0x0
    3) requested debounce time is >32 and <63
       calculated DEBOUNCETIME = 0x0 and debounce will be disabled;
       expected: enable debounce and DEBOUNCETIME = 0x1 == 62 us
    
    Hence, rework omap2_set_gpio_debounce() to fix above cases:
    1) introduce local variable "enable" and use it to identify
    when debounce need to be enabled or disabled. Disable debounce
    if requested debounce time is 0.
    2) use below formula for debounce time calculation:
       debounce = (DIV_ROUND_UP(debounce, 31) - 1) & 0xFF;
    
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Acked-by: Santosh Shilimkar <ssantosh@kernel.org>
    Tested-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-omap.c
+++ drivers/gpio/gpio-omap.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/gpio-omap.h>
 
 #define OFF_MODE	1
+#define OMAP4_GPIO_DEBOUNCINGTIME_MASK 0xFF
 
 static LIST_HEAD(omap_gpio_list);
 
@@ -205,8 +206,9 @@ static inline void omap_gpio_dbck_disable(struct gpio_bank *bank)
  * @offset: the gpio number on this @bank
  * @debounce: debounce time to use
  *
- * OMAP's debounce time is in 31us steps so we need
- * to convert and round up to the closest unit.
+ * OMAP's debounce time is in 31us steps
+ *   <debounce time> = (GPIO_DEBOUNCINGTIME[7:0].DEBOUNCETIME + 1) x 31
+ * so we need to convert and round up to the closest unit.
  */
 static void omap2_set_gpio_debounce(struct gpio_bank *bank, unsigned offset,
 				    unsigned debounce)
@@ -214,16 +216,15 @@ static void omap2_set_gpio_debounce(struct gpio_bank *bank, unsigned offset,
 	void __iomem		*reg;
 	u32			val;
 	u32			l;
+	bool			enable = !!debounce;
 
 	if (!bank->dbck_flag)
 		return;
 
-	if (debounce < 32)
-		debounce = 0x01;
-	else if (debounce > 7936)
-		debounce = 0xff;
-	else
-		debounce = (debounce / 0x1f) - 1;
+	if (enable) {
+		debounce = DIV_ROUND_UP(debounce, 31) - 1;
+		debounce &= OMAP4_GPIO_DEBOUNCINGTIME_MASK;
+	}
 
 	l = BIT(offset);
 
@@ -234,7 +235,7 @@ static void omap2_set_gpio_debounce(struct gpio_bank *bank, unsigned offset,
 	reg = bank->base + bank->regs->debounce_en;
 	val = readl_relaxed(reg);
 
-	if (debounce)
+	if (enable)
 		val |= l;
 	else
 		val &= ~l;
