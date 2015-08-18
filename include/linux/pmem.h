6da50a0876e55e5d316f132493176832c5e8b3cb
Ross Zwisler <ross.zwisler@linux.intel.com>
2015-08-18 13:55:36 -0600

    pmem, x86: move x86 PMEM API to new pmem.h header
    Move the x86 PMEM API implementation out of asm/cacheflush.h and into
    its own header asm/pmem.h.  This will allow members of the PMEM API to
    be more easily identified on this and other architectures.
    
    Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Suggested-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- include/linux/pmem.h
+++ include/linux/pmem.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 
 #ifdef CONFIG_ARCH_HAS_PMEM_API
-#include <asm/cacheflush.h>
+#include <asm/pmem.h>
 #else
 static inline void arch_wmb_pmem(void)
 {
