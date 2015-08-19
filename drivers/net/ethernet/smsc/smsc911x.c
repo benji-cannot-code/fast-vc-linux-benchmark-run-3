75ac326d1d8ad9fce05ba8a0e829d3201fd02229
Jeremy Linton <jeremy.linton@arm.com>
2015-08-19 11:46:43 -0500

    smsc911x: Remove dev==NULL check.
    The dev==NULL check in smsc911x_probe_config is useless
    and isn't providing any additional protection. If a fwnode
    doesn't exist then an appropriate error should be returned
    by device_get_phy_mode() covering the original case
    of a missing of/fwnode.
    
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/smsc/smsc911x.c
+++ drivers/net/ethernet/smsc/smsc911x.c
@@ -2371,9 +2371,6 @@ static int smsc911x_probe_config(struct smsc911x_platform_config *config,
 	int phy_interface;
 	u32 width = 0;
 
-	if (!dev)
-		return -ENODEV;
-
 	phy_interface = device_get_phy_mode(dev);
 	if (phy_interface < 0)
 		return phy_interface;
