8578e208d62103612c96331efd29c72a01d415b2
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:17 +1000

    drm/nouveau/xtensa: directly use instmem for ucode image
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/engine/xtensa.h
+++ drivers/gpu/drm/nouveau/include/nvkm/engine/xtensa.h
@@ -2,13 +2,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NVKM_XTENSA_H__
 #define __NVKM_XTENSA_H__
 #include <core/engine.h>
-struct nvkm_gpuobj;
 
 struct nvkm_xtensa {
 	struct nvkm_engine engine;
 
 	u32 addr;
-	struct nvkm_gpuobj *gpu_fw;
+	struct nvkm_memory *gpu_fw;
 	u32 fifo_val;
 	u32 unkd28;
 };
