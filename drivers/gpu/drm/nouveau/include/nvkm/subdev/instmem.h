36f666f32815b3c5e3b7d46018980493fd9b26dc
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:14 +1000

    drm/nouveau/imem: use object accessor function for suspend/resume
    Very much not ideal, but this will be improved later.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/instmem.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/instmem.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <core/subdev.h>
 
 struct nvkm_instobj {
-	struct nvkm_object base;
+	struct nvkm_object object;
 	struct list_head head;
 	u32 *suspend;
 	u64 addr;
