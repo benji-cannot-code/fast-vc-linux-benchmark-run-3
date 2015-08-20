3367667c51260c697dd1855dffe874835ccc8394
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:19 +1000

    drm/nouveau/disp: split user classes out from engine implementations
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/disp/hdagf119.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/disp/hdagf119.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <nvif/unpack.h>
 
 int
-gf110_hda_eld(NV50_DISP_MTHD_V1)
+gf119_hda_eld(NV50_DISP_MTHD_V1)
 {
 	struct nvkm_device *device = disp->base.engine.subdev.device;
 	union {
--- drivers/gpu/drm/nouveau/nvkm/engine/disp/hdmigf119.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/disp/hdmigf119.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <nvif/unpack.h>
 
 int
-gf110_hdmi_ctrl(NV50_DISP_MTHD_V1)
+gf119_hdmi_ctrl(NV50_DISP_MTHD_V1)
 {
 	struct nvkm_device *device = disp->base.engine.subdev.device;
 	const u32 hoff = (head * 0x800);
