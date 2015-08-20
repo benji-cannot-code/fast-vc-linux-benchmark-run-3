21463e1eb08dc0ed899cfca5bf9339527b85d173
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:06 +1000

    drm/nouveau/ibus: cosmetic changes
    This is purely preparation for upcoming commits, there should be no
    code changes here.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/ibus.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/ibus.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <core/subdev.h>
 
 struct nvkm_ibus {
-	struct nvkm_subdev base;
+	struct nvkm_subdev subdev;
 };
 
 static inline struct nvkm_ibus *
@@ -17,11 +17,11 @@ nvkm_ibus(void *obj)
 	nvkm_subdev_create_((p), (e), (o), 0, "PIBUS", "ibus",              \
 			       sizeof(**d), (void **)d)
 #define nvkm_ibus_destroy(p)                                                \
-	nvkm_subdev_destroy(&(p)->base)
+	nvkm_subdev_destroy(&(p)->subdev)
 #define nvkm_ibus_init(p)                                                   \
-	nvkm_subdev_init(&(p)->base)
+	nvkm_subdev_init(&(p)->subdev)
 #define nvkm_ibus_fini(p,s)                                                 \
-	nvkm_subdev_fini(&(p)->base, (s))
+	nvkm_subdev_fini(&(p)->subdev, (s))
 
 #define _nvkm_ibus_dtor _nvkm_subdev_dtor
 #define _nvkm_ibus_init _nvkm_subdev_init
