d15978b0b06eb87b0dd6d37eb7e19a783092c98b
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:17 +1000

    drm/nouveau/gr: directly use instmem where currently possible
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/nv20.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/nv20.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nv20_gr {
 	struct nvkm_gr base;
-	struct nvkm_gpuobj *ctxtab;
+	struct nvkm_memory *ctxtab;
 };
 
 struct nv20_gr_chan {
