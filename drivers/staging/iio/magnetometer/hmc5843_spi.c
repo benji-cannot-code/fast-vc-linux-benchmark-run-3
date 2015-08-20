945b288f6f2964dd043e3792b02be7c9ed6b8502
Javier Martinez Canillas <javier@osg.samsung.com>
2015-08-20 09:07:15 +0200

    staging: iio: hmc5843: Export missing SPI module alias information
    The SPI core always reports the MODALIAS uevent as "spi:<modalias>"
    regardless of the mechanism that was used to register the device
    (i.e: OF or board code) and the table that is used later to match
    the driver with the device (i.e: SPI id table or OF match table).
    
    So drivers needs to export the SPI id table and this be built into
    the module or udev won't have the necessary information to autoload
    the needed driver module when the device is added.
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/magnetometer/hmc5843_spi.c
+++ drivers/staging/iio/magnetometer/hmc5843_spi.c
@@ -83,6 +83,7 @@ static const struct spi_device_id hmc5843_id[] = {
 	{ "hmc5983", HMC5983_ID },
 	{ }
 };
+MODULE_DEVICE_TABLE(spi, hmc5843_id);
 
 static struct spi_driver hmc5843_driver = {
 		.driver = {
