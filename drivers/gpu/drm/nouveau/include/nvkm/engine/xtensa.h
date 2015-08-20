711ab6c4004f50817b0b105a651b2af6b3fd5cee
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:07 +1000

    drm/nouveau/xtensa: cosmetic changes
    This is purely preparation for upcoming commits, there should be no
    code changes here.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/engine/xtensa.h
+++ drivers/gpu/drm/nouveau/include/nvkm/engine/xtensa.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct nvkm_gpuobj;
 
 struct nvkm_xtensa {
-	struct nvkm_engine base;
+	struct nvkm_engine engine;
 
 	u32 addr;
 	struct nvkm_gpuobj *gpu_fw;
