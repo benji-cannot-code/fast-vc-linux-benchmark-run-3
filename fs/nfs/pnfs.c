9a63b6c239f831a7fe4754fc37e13bd32bc36a4f
Trond Myklebust <trond.myklebust@primarydata.com>
2015-08-19 23:00:50 -0500

    pNFS: Fix an unused variable warning in pnfs_roc_get_barrier
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -1151,7 +1151,6 @@ void pnfs_roc_get_barrier(struct inode *ino, u32 *barrier)
 {
 	struct nfs_inode *nfsi = NFS_I(ino);
 	struct pnfs_layout_hdr *lo;
-	nfs4_stateid stateid;
 	u32 current_seqid;
 
 	spin_lock(&ino->i_lock);
@@ -1162,7 +1161,6 @@ void pnfs_roc_get_barrier(struct inode *ino, u32 *barrier)
 	 * a barrier, we choose the worst-case barrier.
 	 */
 	*barrier = current_seqid + atomic_read(&lo->plh_outstanding);
-	stateid = lo->plh_stateid;
 	spin_unlock(&ino->i_lock);
 }
 
