5862007a9b32f7e600d86b72559e1dcbbdaae3cd
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:13 +1000

    drm/nouveau/gr: switch to subdev printk macros
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/core/enum.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/enum.h
@@ -18,6 +18,5 @@ struct nvkm_bitfield {
 	const char *name;
 };
 
-void nvkm_bitfield_print(const struct nvkm_bitfield *, u32 value);
 void nvkm_snprintbf(char *, int, const struct nvkm_bitfield *, u32 value);
 #endif
