a064d33369699c310fada69af9530025d2e7b30b
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:08 +1000

    drm/nouveau/msppp: cosmetic changes
    This is purely preparation for upcoming commits, there should be no
    code changes here.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/msppp/g98.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/msppp/g98.c
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <engine/msppp.h>
 #include <engine/falcon.h>
 
-struct g98_msppp_priv {
-	struct nvkm_falcon base;
-};
-
 /*******************************************************************************
  * MSPPP object classes
  ******************************************************************************/
@@ -64,15 +60,15 @@ g98_msppp_cclass = {
 static int
 g98_msppp_init(struct nvkm_object *object)
 {
-	struct g98_msppp_priv *priv = (void *)object;
+	struct nvkm_falcon *msppp = (void *)object;
 	int ret;
 
-	ret = nvkm_falcon_init(&priv->base);
+	ret = nvkm_falcon_init(msppp);
 	if (ret)
 		return ret;
 
-	nv_wr32(priv, 0x086010, 0x0000ffd2);
-	nv_wr32(priv, 0x08601c, 0x0000fff2);
+	nv_wr32(msppp, 0x086010, 0x0000ffd2);
+	nv_wr32(msppp, 0x08601c, 0x0000fff2);
 	return 0;
 }
 
@@ -81,18 +77,18 @@ g98_msppp_ctor(struct nvkm_object *parent, struct nvkm_object *engine,
 	       struct nvkm_oclass *oclass, void *data, u32 size,
 	       struct nvkm_object **pobject)
 {
-	struct g98_msppp_priv *priv;
+	struct nvkm_falcon *msppp;
 	int ret;
 
 	ret = nvkm_falcon_create(parent, engine, oclass, 0x086000, true,
-				 "PMSPPP", "msppp", &priv);
-	*pobject = nv_object(priv);
+				 "PMSPPP", "msppp", &msppp);
+	*pobject = nv_object(msppp);
 	if (ret)
 		return ret;
 
-	nv_subdev(priv)->unit = 0x00400002;
-	nv_engine(priv)->cclass = &g98_msppp_cclass;
-	nv_engine(priv)->sclass = g98_msppp_sclass;
+	nv_subdev(msppp)->unit = 0x00400002;
+	nv_engine(msppp)->cclass = &g98_msppp_cclass;
+	nv_engine(msppp)->sclass = g98_msppp_sclass;
 	return 0;
 }
 
--- drivers/gpu/drm/nouveau/nvkm/engine/msppp/gf100.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/msppp/gf100.c
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <engine/msppp.h>
 #include <engine/falcon.h>
 
-struct gf100_msppp_priv {
-	struct nvkm_falcon base;
-};
-
 /*******************************************************************************
  * MSPPP object classes
  ******************************************************************************/
@@ -63,15 +59,15 @@ gf100_msppp_cclass = {
 static int
 gf100_msppp_init(struct nvkm_object *object)
 {
-	struct gf100_msppp_priv *priv = (void *)object;
+	struct nvkm_falcon *msppp = (void *)object;
 	int ret;
 
-	ret = nvkm_falcon_init(&priv->base);
+	ret = nvkm_falcon_init(msppp);
 	if (ret)
 		return ret;
 
-	nv_wr32(priv, 0x086010, 0x0000fff2);
-	nv_wr32(priv, 0x08601c, 0x0000fff2);
+	nv_wr32(msppp, 0x086010, 0x0000fff2);
+	nv_wr32(msppp, 0x08601c, 0x0000fff2);
 	return 0;
 }
 
@@ -80,19 +76,19 @@ gf100_msppp_ctor(struct nvkm_object *parent, struct nvkm_object *engine,
 		 struct nvkm_oclass *oclass, void *data, u32 size,
 		 struct nvkm_object **pobject)
 {
-	struct gf100_msppp_priv *priv;
+	struct nvkm_falcon *msppp;
 	int ret;
 
 	ret = nvkm_falcon_create(parent, engine, oclass, 0x086000, true,
-				 "PMSPPP", "msppp", &priv);
-	*pobject = nv_object(priv);
+				 "PMSPPP", "msppp", &msppp);
+	*pobject = nv_object(msppp);
 	if (ret)
 		return ret;
 
-	nv_subdev(priv)->unit = 0x00000002;
-	nv_subdev(priv)->intr = nvkm_falcon_intr;
-	nv_engine(priv)->cclass = &gf100_msppp_cclass;
-	nv_engine(priv)->sclass = gf100_msppp_sclass;
+	nv_subdev(msppp)->unit = 0x00000002;
+	nv_subdev(msppp)->intr = nvkm_falcon_intr;
+	nv_engine(msppp)->cclass = &gf100_msppp_cclass;
+	nv_engine(msppp)->sclass = gf100_msppp_sclass;
 	return 0;
 }
 
