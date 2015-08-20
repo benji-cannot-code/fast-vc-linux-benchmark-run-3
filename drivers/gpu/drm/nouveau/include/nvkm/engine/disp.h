db339712456b0859f73db9964544ca98eca1172e
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:07 +1000

    drm/nouveau/disp: cosmetic changes
    This is purely preparation for upcoming commits, there should be no
    code changes here.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/engine/disp.h
+++ drivers/gpu/drm/nouveau/include/nvkm/engine/disp.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <core/event.h>
 
 struct nvkm_disp {
-	struct nvkm_engine base;
+	struct nvkm_engine engine;
 
 	struct list_head outp;
 
