e7a91681c6fc92bc39532a233c35b9dcad5559a6
Ben Skeggs <bskeggs@redhat.com>
2015-08-20 14:54:05 +1000

    drm/nouveau/subdev: add direct pointer to nvkm_device
    Will be utilised in upcoming commits to remove the need for heuristics
    to lookup the device a subdev belongs to.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/core/subdev.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/subdev.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nvkm_subdev {
 	struct nvkm_object object;
+
+	struct nvkm_device *device;
+
 	struct mutex mutex;
 	const char *name;
 	void __iomem *mmio;
