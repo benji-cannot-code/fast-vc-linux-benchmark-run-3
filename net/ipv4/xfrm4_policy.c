e72d6a787ddb234e672784a279d2423a116dd766
David Ahern <dsa@cumulusnetworks.com>
2015-08-20 15:06:30 -0700

    xfrm: Use VRF master index if output device is enslaved
    Directs route lookups to VRF table. Compiles out if NET_VRF is not
    enabled. With this patch able to successfully bring up ipsec tunnels
    in VRFs, even with duplicate network configuration.
    
    Signed-off-by: David Ahern <dsa@cumulusnetworks.com>
    Acked-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
    Acked-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/xfrm4_policy.c
+++ net/ipv4/xfrm4_policy.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/dst.h>
 #include <net/xfrm.h>
 #include <net/ip.h>
+#include <net/vrf.h>
 
 static struct xfrm_policy_afinfo xfrm4_policy_afinfo;
 
@@ -108,8 +109,10 @@ _decode_session4(struct sk_buff *skb, struct flowi *fl, int reverse)
 	struct flowi4 *fl4 = &fl->u.ip4;
 	int oif = 0;
 
-	if (skb_dst(skb))
-		oif = skb_dst(skb)->dev->ifindex;
+	if (skb_dst(skb)) {
+		oif = vrf_master_ifindex(skb_dst(skb)->dev) ?
+			: skb_dst(skb)->dev->ifindex;
+	}
 
 	memset(fl4, 0, sizeof(struct flowi4));
 	fl4->flowi4_mark = skb->mark;
