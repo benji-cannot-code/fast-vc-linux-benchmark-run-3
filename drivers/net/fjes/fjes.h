b1219ee43414507f6a75eab9ac38b3faba717b94
Taku Izumi <izumi.taku@jp.fujitsu.com>
2015-08-21 17:29:18 +0900

    fjes: Hardware initialization routine
    This patch adds hardware initialization routine to be
    invoked at driver's .probe routine.
    
    Signed-off-by: Taku Izumi <izumi.taku@jp.fujitsu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/fjes/fjes.h
+++ drivers/net/fjes/fjes.h
@@ -29,5 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern char fjes_driver_name[];
 extern char fjes_driver_version[];
+extern const u32 fjes_support_mtu[];
 
 #endif /* FJES_H_ */
