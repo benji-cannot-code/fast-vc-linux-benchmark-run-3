d808323ffdc2e76e376e5297df437292eea70efa
Geert Uytterhoeven <geert+renesas@glider.be>
2015-08-21 20:02:46 +0200

    serial: sh-sci: Stop acknowledging DMA transmit completions
    As dmaengine_prep_slave_sg() is called with the DMA_CTRL_ACK flag set
    for DMA transmit requests, there's no need to explicitly acknowledge DMA
    transmit requests in the DMA transmit completion callback.
    
    Hence remove the call to async_tx_ack(), and remove the now unused
    dma_async_tx_descriptor pointer in the sci_port structure.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/sh-sci.c
+++ drivers/tty/serial/sh-sci.c
@@ -105,7 +105,6 @@ struct sci_port {
 	struct dma_chan			*chan_rx;
 
 #ifdef CONFIG_SERIAL_SH_SCI_DMA
-	struct dma_async_tx_descriptor	*desc_tx;
 	struct dma_async_tx_descriptor	*desc_rx[2];
 	dma_cookie_t			cookie_tx;
 	dma_cookie_t			cookie_rx[2];
@@ -1286,9 +1285,6 @@ static void sci_dma_tx_complete(void *arg)
 
 	port->icount.tx += s->tx_dma_len;
 
-	async_tx_ack(s->desc_tx);
-	s->desc_tx = NULL;
-
 	if (uart_circ_chars_pending(xmit) < WAKEUP_CHARS)
 		uart_write_wakeup(port);
 
@@ -1529,7 +1525,6 @@ static void work_fn_tx(struct work_struct *work)
 				   DMA_TO_DEVICE);
 
 	spin_lock_irq(&port->lock);
-	s->desc_tx = desc;
 	desc->callback = sci_dma_tx_complete;
 	desc->callback_param = s;
 	spin_unlock_irq(&port->lock);
