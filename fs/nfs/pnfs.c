7341cfa807ddf727ab901e40836500e8b1928f9c
Trond Myklebust <trond.myklebust@primarydata.com>
2015-08-24 20:39:18 -0400

    NFSv4.2/pnfs: Make the layoutstats timer configurable
    Allow advanced users to set the layoutstats timer in order to lengthen
    or shorten the period between layoutstat transmissions to the server.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -2286,3 +2286,7 @@ out_put:
 }
 EXPORT_SYMBOL_GPL(pnfs_report_layoutstat);
 #endif
+
+unsigned int layoutstats_timer;
+module_param(layoutstats_timer, uint, 0644);
+EXPORT_SYMBOL_GPL(layoutstats_timer);
