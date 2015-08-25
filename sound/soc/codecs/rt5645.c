a417a5385da03fd36b9bd625912322fbceebc6fe
Oder Chiou <oder_chiou@realtek.com>
2015-08-25 16:03:48 +0800

    ASoC: rt5645: Add struct dmi_system_id "Google Celes" for chrome platform
    Signed-off-by: Oder Chiou <oder_chiou@realtek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Cc: stable@vger.kernel.org

--- sound/soc/codecs/rt5645.c
+++ sound/soc/codecs/rt5645.c
@@ -3230,6 +3230,13 @@ static struct dmi_system_id dmi_platform_intel_braswell[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "Strago"),
 		},
 	},
+	{
+		.ident = "Google Celes",
+		.callback = strago_quirk_cb,
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "Celes"),
+		},
+	},
 	{ }
 };
 
