e14db0af0b5f0869dd8f7b923a05da5c1fc2dad8
Paul Gortmaker <paul.gortmaker@windriver.com>
2015-08-25 16:59:16 -0400

    nfsd: don't WARN/backtrace for invalid container deployment.
    These messages, combined with the backtrace they trigger, makes it seem
    like a serious problem, though a quick search shows distros marking
    it as a "won't fix" non-issue when the problem is reported by users.
    
    The backtrace is overkill, and only really manages to show that if
    you follow the code path, you can't really avoid it with bootargs
    or configuration settings in the container.
    
    Given that, lets tone it down a bit and get rid of the WARN severity,
    and the associated backtrace, so people aren't needlessly alarmed.
    
    Also, lets drop the split printk line, since they are grep unfriendly.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfs4recover.c
+++ fs/nfsd/nfs4recover.c
@@ -548,8 +548,7 @@ nfsd4_legacy_tracking_init(struct net *net)
 
 	/* XXX: The legacy code won't work in a container */
 	if (net != &init_net) {
-		WARN(1, KERN_ERR "NFSD: attempt to initialize legacy client "
-			"tracking in a container!\n");
+		pr_warn("NFSD: attempt to initialize legacy client tracking in a container ignored.\n");
 		return -EINVAL;
 	}
 
@@ -1261,8 +1260,7 @@ nfsd4_umh_cltrack_init(struct net *net)
 
 	/* XXX: The usermode helper s not working in container yet. */
 	if (net != &init_net) {
-		WARN(1, KERN_ERR "NFSD: attempt to initialize umh client "
-			"tracking in a container!\n");
+		pr_warn("NFSD: attempt to initialize umh client tracking in a container ignored.\n");
 		return -EINVAL;
 	}
 
