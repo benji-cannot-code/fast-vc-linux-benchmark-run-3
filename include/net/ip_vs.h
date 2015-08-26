33b2ff8c990c16432c8dd90ca2f4766c3a5eb8b7
Alex Gartrell <agartrell@fb.com>
2015-08-26 09:40:34 -0700

    ipvs: add schedule_icmp sysctl
    This sysctl will be used to enable the scheduling of icmp packets.
    
    Signed-off-by: Alex Gartrell <agartrell@fb.com>
    Acked-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- include/net/ip_vs.h
+++ include/net/ip_vs.h
@@ -998,6 +998,7 @@ struct netns_ipvs {
 	int			sysctl_pmtu_disc;
 	int			sysctl_backup_only;
 	int			sysctl_conn_reuse_mode;
+	int			sysctl_schedule_icmp;
 
 	/* ip_vs_lblc */
 	int			sysctl_lblc_expiration;
@@ -1116,6 +1117,11 @@ static inline int sysctl_conn_reuse_mode(struct netns_ipvs *ipvs)
 	return ipvs->sysctl_conn_reuse_mode;
 }
 
+static inline int sysctl_schedule_icmp(struct netns_ipvs *ipvs)
+{
+	return ipvs->sysctl_schedule_icmp;
+}
+
 #else
 
 static inline int sysctl_sync_threshold(struct netns_ipvs *ipvs)
@@ -1186,6 +1192,11 @@ static inline int sysctl_backup_only(struct netns_ipvs *ipvs)
 static inline int sysctl_conn_reuse_mode(struct netns_ipvs *ipvs)
 {
 	return 1;
+}
+
+static inline int sysctl_schedule_icmp(struct netns_ipvs *ipvs)
+{
+	return 0;
 }
 
 #endif
