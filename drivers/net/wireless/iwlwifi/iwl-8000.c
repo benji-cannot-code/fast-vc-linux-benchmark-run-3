906023c65256d71ed10713842bcdf2674a686574
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-08-26 13:48:55 +0300

    iwlwifi: mvm: don't load -12.ucode anymore
    -13.ucode has been published long ago. Deprecate old
    versions of the firmware.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Luciano Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-8000.c
+++ drivers/net/wireless/iwlwifi/iwl-8000.c
@@ -73,10 +73,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IWL8000_UCODE_API_MAX	17
 
 /* Oldest version we won't warn about */
-#define IWL8000_UCODE_API_OK	12
+#define IWL8000_UCODE_API_OK	13
 
 /* Lowest firmware API version supported */
-#define IWL8000_UCODE_API_MIN	12
+#define IWL8000_UCODE_API_MIN	13
 
 /* NVM versions */
 #define IWL8000_NVM_VERSION		0x0a1d
