e1fbf95522114f65483a85153a25953c12de9919
Joe Stringer <joestringer@nicira.com>
2015-08-27 15:25:46 -0700

    openvswitch: Include ip6_fib.h.
    kbuild test robot reports that certain configurations will not
    automatically pick up on the "struct rt6_info" definition, so explicitly
    include the header for this structure.
    
    Fixes: 7f8a436 "openvswitch: Add conntrack action"
    Signed-off-by: Joe Stringer <joestringer@nicira.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/openvswitch/actions.c
+++ net/openvswitch/actions.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/dst.h>
 #include <net/ip.h>
 #include <net/ipv6.h>
+#include <net/ip6_fib.h>
 #include <net/checksum.h>
 #include <net/dsfield.h>
 #include <net/mpls.h>
