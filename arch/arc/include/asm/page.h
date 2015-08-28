4f97a08bacd47c2a3a0cae31194c77531245d4bc
Vineet Gupta <vgupta@synopsys.com>
2015-08-28 08:39:57 +0530

    ARC: mm: switch pgtable_to to pte_t *
    ARC is the only arch with unsigned long type (vs. struct page *).
    Historically this was done to avoid the page_address() calls in various
    arch hooks which need to get the virtual/logical address of the table.
    
    Some arches alternately define it as pte_t *, and is as efficient as
    unsigned long (generated code doesn't change)
    
    Suggested-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/page.h
+++ arch/arc/include/asm/page.h
@@ -44,7 +44,6 @@ typedef struct {
 typedef struct {
 	unsigned long pgprot;
 } pgprot_t;
-typedef unsigned long pgtable_t;
 
 #define pte_val(x)      ((x).pte)
 #define pgd_val(x)      ((x).pgd)
@@ -61,7 +60,6 @@ typedef unsigned long pgtable_t;
 typedef unsigned long pte_t;
 typedef unsigned long pgd_t;
 typedef unsigned long pgprot_t;
-typedef unsigned long pgtable_t;
 
 #define pte_val(x)	(x)
 #define pgd_val(x)	(x)
@@ -71,6 +69,8 @@ typedef unsigned long pgtable_t;
 #define pte_pgprot(x)	(x)
 
 #endif
+
+typedef pte_t * pgtable_t;
 
 #define ARCH_PFN_OFFSET     (CONFIG_LINUX_LINK_BASE >> PAGE_SHIFT)
 
