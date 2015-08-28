787fb33c45e58a025efc88119dec8204c7f64b83
Hans Verkuil <hans.verkuil@cisco.com>
2015-08-28 08:48:31 -0300

    [media] saa7164: remove unused videobuf references
    This driver includes videobuf headers and selects VIDEOBUF_DVB, but
    videobuf isn't used at all.
    
    Remove this.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/pci/saa7164/saa7164.h
+++ drivers/media/pci/saa7164/saa7164.h
@@ -55,8 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/tuner.h>
 #include <media/tveeprom.h>
-#include <media/videobuf-dma-sg.h>
-#include <media/videobuf-dvb.h>
 #include <dvb_demux.h>
 #include <dvb_frontend.h>
 #include <dvb_net.h>
