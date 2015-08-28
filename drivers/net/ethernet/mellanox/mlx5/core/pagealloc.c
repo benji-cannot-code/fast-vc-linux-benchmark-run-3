5fec806d899477d40dcc89c05d021a5a9559d31b
Christoph Hellwig <hch@lst.de>
2015-08-28 09:27:19 +0200

    mlx5: stop including <asm-generic/kmap_types.h>
    <linux/highmem.h> is the placace the get the kmap type flags, asm-generic
    files are generic implementations only to be used by architecture code.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/net/ethernet/mellanox/mlx5/core/pagealloc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/pagealloc.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * SOFTWARE.
  */
 
-#include <asm-generic/kmap_types.h>
+#include <linux/highmem.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mlx5/driver.h>
