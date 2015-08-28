d1b12c4e72eabbf28260a05b799cf5c2f091b896
Axel Lin <axel.lin@ingics.com>
2015-08-28 11:23:05 +0800

    ASoC: ics43432: Fix module license
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/ics43432.c
+++ sound/soc/codecs/ics43432.c
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (c) 2015 Axis Communications AB
  *
- * Licensed under GPL2.
+ * Licensed under GPL v2.
  */
 
 #include <linux/module.h>
@@ -74,4 +74,4 @@ module_platform_driver(ics43432_driver);
 
 MODULE_DESCRIPTION("ASoC ICS43432 driver");
 MODULE_AUTHOR("Ricard Wanderlof <ricardw@axis.com>");
-MODULE_LICENSE("GPLv2");
+MODULE_LICENSE("GPL v2");
