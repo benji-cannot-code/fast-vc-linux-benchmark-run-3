c4e4f8095b79b01ac2d09a667c9125934f86c5b1
Hans Verkuil <hans.verkuil@cisco.com>
2015-08-28 08:48:29 -0300

    [media] saa7164: add support for control events
    Now that saa7164 uses v4l2_fh and that poll() has been fixed, it is
    trivial to add support for control events.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/pci/saa7164/saa7164.h
+++ drivers/media/pci/saa7164/saa7164.h
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ctrls.h>
+#include <media/v4l2-event.h>
 
 #include "saa7164-reg.h"
 #include "saa7164-types.h"
