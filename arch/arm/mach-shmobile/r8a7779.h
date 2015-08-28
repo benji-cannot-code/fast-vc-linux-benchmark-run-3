bd09c32583e8c35417aa21542f8dc085a20eb6c4
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2015-08-29 00:37:17 +0300

    ARM: shmobile: Remove legacy clock support leftovers
    The shmobile_clk_init() function has been removed and the linux/sh_clk.h
    header doesn't need to be included anymore.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/r8a7779.h
+++ arch/arm/mach-shmobile/r8a7779.h
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_R8A7779_H__
 #define __ASM_R8A7779_H__
 
-#include <linux/sh_clk.h>
-
 extern void r8a7779_pm_init(void);
 
 #ifdef CONFIG_PM
