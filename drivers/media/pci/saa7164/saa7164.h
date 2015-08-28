d023e9aea30566b24f8727455051a6405d0017f8
Hans Verkuil <hans.verkuil@cisco.com>
2015-08-28 08:48:26 -0300

    [media] saa7164: convert to the control framework
    Convert this driver to the control framework. Note that the VBI device
    nodes have no controls as there is nothing to control.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/pci/saa7164/saa7164.h
+++ drivers/media/pci/saa7164/saa7164.h
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-common.h>
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-device.h>
+#include <media/v4l2-ctrls.h>
 
 #include "saa7164-reg.h"
 #include "saa7164-types.h"
@@ -382,6 +383,7 @@ struct saa7164_port {
 	/* Encoder */
 	/* Defaults established in saa7164-encoder.c */
 	struct saa7164_tvnorm encodernorm;
+	struct v4l2_ctrl_handler ctrl_handler;
 	v4l2_std_id std;
 	u32 height;
 	u32 width;
