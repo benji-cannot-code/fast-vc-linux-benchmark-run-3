47759e0852b65dc1b891340f5d4a75cbc227bba5
Javier Martinez Canillas <javier@osg.samsung.com>
2015-08-31 10:52:00 +0200

    power: Remove unnecessary MODULE_ALIAS() for I2C drivers
    These drivers already have an I2C device id table that is used to create
    module aliases and the used MODULE_ALIAS() was either already in the I2C
    table so it was redundant or wasn't a valid I2C id so it was never used.
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- drivers/power/smb347-charger.c
+++ drivers/power/smb347-charger.c
@@ -1333,4 +1333,3 @@ MODULE_AUTHOR("Bruce E. Robertson <bruce.e.robertson@intel.com>");
 MODULE_AUTHOR("Mika Westerberg <mika.westerberg@linux.intel.com>");
 MODULE_DESCRIPTION("SMB347 battery charger driver");
 MODULE_LICENSE("GPL");
-MODULE_ALIAS("i2c:smb347");
