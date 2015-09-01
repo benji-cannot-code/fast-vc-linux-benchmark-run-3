ca2d479c7eb7575b7948eedb38c608676b1d0399
Tom Herbert <tom@herbertland.com>
2015-09-01 09:24:30 -0700

    flow_dissector: Add flag to stop parsing at L3
    Add an input flag to flow dissector on rather dissection should be
    stopped when an L3 packet is encountered. This would be useful if a
    caller just wanted to get IP addresses of the outermost header (e.g.
    to do an L3 hash).
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/flow_dissector.c
+++ net/core/flow_dissector.c
@@ -201,6 +201,9 @@ ip:
 			}
 		}
 
+		if (flags & FLOW_DISSECTOR_F_STOP_AT_L3)
+			goto out_good;
+
 		break;
 	}
 	case htons(ETH_P_IPV6): {
@@ -238,6 +241,9 @@ ipv6:
 				key_tags->flow_label = ntohl(flow_label);
 			}
 		}
+
+		if (flags & FLOW_DISSECTOR_F_STOP_AT_L3)
+			goto out_good;
 
 		break;
 	}
