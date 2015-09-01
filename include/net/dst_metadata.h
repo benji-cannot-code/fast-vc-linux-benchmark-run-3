b228cc5d8ed05f54a9c46795a80a757e44c410dc
Pravin B Shelar <pshelar@nicira.com>
2015-08-31 20:05:57 -0700

    tun_dst: Remove opts_size
    opts_size is only written and never read. Following patch
    removes this unused variable.
    
    Signed-off-by: Pravin B Shelar <pshelar@nicira.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/dst_metadata.h
+++ include/net/dst_metadata.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct metadata_dst {
 	struct dst_entry		dst;
-	size_t				opts_len;
 	union {
 		struct ip_tunnel_info	tun_info;
 	} u;
