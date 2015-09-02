e8e1fc710e9a5504d8377f77b600706c1fc121ef
Fabien Parent <fparent@baylibre.com>
2015-09-02 15:50:35 +0200

    greybus: add num_cports field to greybus hd
    This commit is doing the preparation work in order to get the number of cports
    supported from the UniPro IP instead of using a constant defined in a Kconfig
    file.
    
    Greybus host device is now holding the cport count, and all the code will now
    use this value instead of the constant CPORT_ID_MAX when referring to an AP's
    CPort ID.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>
    [johan: es1 supports 256 cports, minor style changes ]
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>

--- drivers/staging/greybus/core.c
+++ drivers/staging/greybus/core.c
@@ -169,7 +169,8 @@ static void free_hd(struct kref *kref)
 
 struct greybus_host_device *greybus_create_hd(struct greybus_host_driver *driver,
 					      struct device *parent,
-					      size_t buffer_size_max)
+					      size_t buffer_size_max,
+					      size_t num_cports)
 {
 	struct greybus_host_device *hd;
 
@@ -185,6 +186,11 @@ struct greybus_host_device *greybus_create_hd(struct greybus_host_driver *driver
 	if (buffer_size_max < GB_OPERATION_MESSAGE_SIZE_MIN) {
 		dev_err(parent, "greybus host-device buffers too small\n");
 		return NULL;
+	}
+
+	if (num_cports == 0 || num_cports > CPORT_ID_MAX) {
+		dev_err(parent, "Invalid number of CPorts: %zu\n", num_cports);
+		return ERR_PTR(-EINVAL);
 	}
 
 	/*
@@ -208,6 +214,7 @@ struct greybus_host_device *greybus_create_hd(struct greybus_host_driver *driver
 	INIT_LIST_HEAD(&hd->connections);
 	ida_init(&hd->cport_id_map);
 	hd->buffer_size_max = buffer_size_max;
+	hd->num_cports = num_cports;
 
 	/*
 	 * Initialize AP's SVC protocol connection:
--- drivers/staging/greybus/es2.c
+++ drivers/staging/greybus/es2.c
@@ -129,7 +129,7 @@ static void usb_log_disable(struct es1_ap_dev *es1);
 
 static int cport_to_ep(struct es1_ap_dev *es1, u16 cport_id)
 {
-	if (cport_id >= CPORT_COUNT)
+	if (cport_id >= es1->hd->num_cports)
 		return 0;
 	return es1->cport_to_ep[cport_id];
 }
@@ -140,7 +140,7 @@ static int ep_in_use(struct es1_ap_dev *es1, int bulk_ep_set)
 {
 	int i;
 
-	for (i = 0; i < CPORT_COUNT; i++) {
+	for (i = 0; i < es1->hd->num_cports; i++) {
 		if (es1->cport_to_ep[i] == bulk_ep_set)
 			return 1;
 	}
@@ -155,7 +155,7 @@ int map_cport_to_ep(struct es1_ap_dev *es1,
 
 	if (bulk_ep_set == 0 || bulk_ep_set >= NUM_BULKS)
 		return -EINVAL;
-	if (cport_id >= CPORT_COUNT)
+	if (cport_id >= es1->hd->num_cports)
 		return -EINVAL;
 	if (bulk_ep_set && ep_in_use(es1, bulk_ep_set))
 		return -EINVAL;
@@ -288,7 +288,7 @@ static int message_send(struct greybus_host_device *hd, u16 cport_id,
 	 * of where the data should be sent.  Do one last check of
 	 * the target CPort id before filling it in.
 	 */
-	if (!cport_id_valid(cport_id)) {
+	if (!cport_id_valid(hd, cport_id)) {
 		pr_err("invalid destination cport 0x%02x\n", cport_id);
 		return -EINVAL;
 	}
@@ -473,7 +473,7 @@ static void cport_in_callback(struct urb *urb)
 	header = urb->transfer_buffer;
 	cport_id = gb_message_cport_unpack(header);
 
-	if (cport_id_valid(cport_id))
+	if (cport_id_valid(hd, cport_id))
 		greybus_data_rcvd(hd, cport_id, urb->transfer_buffer,
 							urb->actual_length);
 	else
@@ -661,7 +661,8 @@ static int ap_probe(struct usb_interface *interface,
 
 	udev = usb_get_dev(interface_to_usbdev(interface));
 
-	hd = greybus_create_hd(&es1_driver, &udev->dev, ES1_GBUF_MSG_SIZE_MAX);
+	hd = greybus_create_hd(&es1_driver, &udev->dev, ES1_GBUF_MSG_SIZE_MAX,
+			       CPORT_COUNT);
 	if (IS_ERR(hd)) {
 		usb_put_dev(udev);
 		return PTR_ERR(hd);
