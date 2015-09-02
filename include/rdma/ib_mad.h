64d9e6eb0e82300d4e974b5fe4c6f9c74e7cf79c
Ira Weiny <ira.weiny@intel.com>
2015-09-02 18:45:54 -0400

    IB/core: Remove unnecessary defines from ib_mad.h
    Remove the unused IB_NOTICE_REPRESS_* defines.
    
    When the hfi1 driver was added these definitions were moved from the qib driver
    to ib_mad.h.  They should have been removed instead.
    
    Fixes: d4ab347005fb ("IB/core: Add core header changes needed for OPA")
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: Hal Rosenstock <hal@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- include/rdma/ib_mad.h
+++ include/rdma/ib_mad.h
@@ -158,18 +158,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IB_NOTICE_TRAP_BAD_QKEY		cpu_to_be16(258)
 
 /*
- * Repress trap/notice flags
- */
-#define IB_NOTICE_REPRESS_LLI_THRESH	(1 << 0)
-#define IB_NOTICE_REPRESS_EBO_THRESH	(1 << 1)
-#define IB_NOTICE_REPRESS_FLOW_UPDATE	(1 << 2)
-#define IB_NOTICE_REPRESS_CAP_MASK_CHG	(1 << 3)
-#define IB_NOTICE_REPRESS_SYS_GUID_CHG	(1 << 4)
-#define IB_NOTICE_REPRESS_BAD_MKEY	(1 << 5)
-#define IB_NOTICE_REPRESS_BAD_PKEY	(1 << 6)
-#define IB_NOTICE_REPRESS_BAD_QKEY	(1 << 7)
-
-/*
  * Generic trap/notice other local changes flags (trap 144).
  */
 #define IB_NOTICE_TRAP_LSE_CHG		0x04	/* Link Speed Enable changed */
