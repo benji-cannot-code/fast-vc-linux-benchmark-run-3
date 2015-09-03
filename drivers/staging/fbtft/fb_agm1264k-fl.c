02d9780cd53f584b58f0ba9d0e5aa462a3b675ae
Anish Bhatt <anish7@gmail.com>
2015-09-03 00:53:35 -0700

    staging/fbtft : Remove unicode characters
    Remove stray unicode quotes around name
    
    Signed-off-by: Anish Bhatt <anish@chelsio.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fbtft/fb_agm1264k-fl.c
+++ drivers/staging/fbtft/fb_agm1264k-fl.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CS1			gpio.aux[1]
 
 
-/* diffusing error (“Floyd-Steinberg”) */
+/* diffusing error (Floyd-Steinberg) */
 #define DIFFUSING_MATRIX_WIDTH	2
 #define DIFFUSING_MATRIX_HEIGHT	2
 
