6e103025178be1a04ee376da3cbf9b393907f825
Tony Lindgren <tony@atomide.com>
2015-09-03 10:31:27 -0700

    gpio: omap: Fix GPIO numbering for deferred probe
    If gpio-omap probe fails with -EPROBE_DEFER, the GPIO numbering
    keeps increasing. Only increase the gpio count if gpiochip_add()
    was successful as otherwise the numbers will increase for each
    probe attempt.
    
    Cc: Javier Martinez Canillas <javier@dowhile0.org>
    Cc: Kevin Hilman <khilman@deeprootsystems.com>
    Cc: Santosh Shilimkar <ssantosh@kernel.org>
    Reviewed-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-omap.c
+++ drivers/gpio/gpio-omap.c
@@ -1099,7 +1099,6 @@ static int omap_gpio_chip_init(struct gpio_bank *bank, struct irq_chip *irqc)
 	} else {
 		bank->chip.label = "gpio";
 		bank->chip.base = gpio;
-		gpio += bank->width;
 	}
 	bank->chip.ngpio = bank->width;
 
@@ -1108,6 +1107,9 @@ static int omap_gpio_chip_init(struct gpio_bank *bank, struct irq_chip *irqc)
 		dev_err(bank->dev, "Could not register gpio chip %d\n", ret);
 		return ret;
 	}
+
+	if (!bank->is_mpuio)
+		gpio += bank->width;
 
 #ifdef CONFIG_ARCH_OMAP1
 	/*
