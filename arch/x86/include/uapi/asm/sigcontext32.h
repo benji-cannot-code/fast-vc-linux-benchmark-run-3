69816075b2c5704c8462676307249888b7973118
Ingo Molnar <mingo@kernel.org>
2015-09-05 09:32:29 +0200

    x86/headers: Fix (old) header file dependency bug in uapi/asm/sigcontext32.h
    Mikko Rapeli reported that the following standalone user-space
    header does not compile:
    
      #include <asm/sigcontext32.h>
    
    Due to undefined 'struct __fpx_sw_bytes' which is defined in
    asm/sigcontext.h.
    
    The following header order works:
    
      #include <asm/sigcontext.h>
      #include <asm/sigcontext32.h>
    
    and that's probably how everyone's been using these headers for
    the past decade or so, but it's a legit header file dependency
    bug, so include asm/sigcontext.h in sigcontext32.h to allow it
    to be built standlone.
    
    Reported-by: Mikko Rapeli <mikko.rapeli@iki.fi>
    Acked-by: Mikko Rapeli <mikko.rapeli@iki.fi>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Link: http://lkml.kernel.org/r/1441438363-9999-2-git-send-email-mingo@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/uapi/asm/sigcontext32.h
+++ arch/x86/include/uapi/asm/sigcontext32.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
+#include <asm/sigcontext.h>
+
 /* signal context for 32bit programs. */
 
 #define X86_FXSR_MAGIC		0x0000
