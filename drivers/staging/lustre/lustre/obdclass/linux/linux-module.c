c94abdfba0e380456558d97f9a4ef9bf5b562f3c
Matthew Needes <mneedes@gmail.com>
2015-09-05 13:03:38 -0700

    staging: lustre/obdclass/linux/linux-module.c
    Fixed sparse warning by removing unused variable "proc_version".
    
    Signed-off-by: Matthew Needes <mneedes@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/linux/linux-module.c
+++ drivers/staging/lustre/lustre/obdclass/linux/linux-module.c
@@ -74,8 +74,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../../include/lustre_ver.h"
 #include "../../include/lustre/lustre_build_version.h"
 
-int proc_version;
-
 /* buffer MUST be at least the size of obd_ioctl_hdr */
 int obd_ioctl_getdata(char **buf, int *len, void *arg)
 {
