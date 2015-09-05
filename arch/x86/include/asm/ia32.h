401b073526a447bf8341682ba46e3655830165cb
Ingo Molnar <mingo@kernel.org>
2015-09-05 09:32:42 +0200

    x86/headers: Remove direct sigcontext32.h uses
    Now that all sigcontext types are defined in asm/sigcontext.h,
    remove the various sigcontext32.h uses in the kernel.
    
    We still keep the header itself, which includes sigcontext.h, in
    case user-space relies on it.
    
    Acked-by: Mikko Rapeli <mikko.rapeli@iki.fi>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Link: http://lkml.kernel.org/r/1441438363-9999-15-git-send-email-mingo@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/ia32.h
+++ arch/x86/include/asm/ia32.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 32 bit structures for IA32 support.
  */
 
-#include <asm/sigcontext32.h>
+#include <asm/sigcontext.h>
 
 /* signal.h */
 
--- arch/x86/include/uapi/asm/sigcontext32.h
+++ arch/x86/include/uapi/asm/sigcontext32.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_SIGCONTEXT32_H
 #define _ASM_X86_SIGCONTEXT32_H
 
-/* Signal context definitions for compat 32-bit programs: */
+/* This is a legacy file - all the type definitions are in sigcontext.h: */
 
 #include <asm/sigcontext.h>
 
