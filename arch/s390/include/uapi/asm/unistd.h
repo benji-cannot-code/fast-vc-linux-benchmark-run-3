19aeccde4f75083f20ad145025cd7d04d7b849a3
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-09-07 12:15:56 -0400

    s390/s390x: allocate sys_membarrier system call number
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: linux-api@vger.kernel.org
    CC: Heiko Carstens <heiko.carstens@de.ibm.com>
    CC: linux-s390@vger.kernel.org
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/uapi/asm/unistd.h
+++ arch/s390/include/uapi/asm/unistd.h
@@ -292,7 +292,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_s390_pci_mmio_read		353
 #define __NR_execveat		354
 #define __NR_userfaultfd	355
-#define NR_syscalls 356
+#define __NR_membarrier		356
+#define NR_syscalls 357
 
 /* 
  * There are some system calls that are not present on 64 bit, some
