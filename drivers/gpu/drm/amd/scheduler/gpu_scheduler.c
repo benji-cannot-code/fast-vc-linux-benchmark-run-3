2f4d289f25ac7ed9855ba9161c681d215feba31d
Chunming Zhou <david1.zhou@amd.com>
2015-09-07 16:06:53 +0800

    drm/amdgpu: add tracepoint for scheduler (v2)
    track sched job status like the length of job queue and hw job queue.
    
    v2: fix build after rebase
    
    Signed-off-by: Chunming Zhou <david1.zhou@amd.com>
    Reviewed-by: Jammy Zhou <Jammy.Zhou@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/scheduler/gpu_scheduler.c
+++ drivers/gpu/drm/amd/scheduler/gpu_scheduler.c
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drmP.h>
 #include "gpu_scheduler.h"
 
+#define CREATE_TRACE_POINTS
+#include "gpu_sched_trace.h"
+
 static struct amd_sched_job *
 amd_sched_entity_pop_job(struct amd_sched_entity *entity);
 static void amd_sched_wakeup(struct amd_gpu_scheduler *sched);
@@ -274,7 +277,7 @@ int amd_sched_entity_push_job(struct amd_sched_job *sched_job)
 
 	wait_event(entity->scheduler->job_scheduled,
 		   amd_sched_entity_in(sched_job));
-
+	trace_amd_sched_job(sched_job);
 	return 0;
 }
 
