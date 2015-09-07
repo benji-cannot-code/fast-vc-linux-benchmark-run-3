57d364b9699ba2387e2c0a76a70cb4eab978bb69
Anders Fridlund <anders.fridlund@gmail.com>
2015-09-08 00:56:48 +0200

    staging: lustre: ptlrpc: include ptlrpc_internal.h
    Remove sparse warning "symbol 'sptlrpc_plain_init' was not declared"
    by including ptlrpc_internal.h, which includes sptlrpc_plain_init.
    
    Signed-off-by: Anders Fridlund <anders.fridlund@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/sec_plain.c
+++ drivers/staging/lustre/lustre/ptlrpc/sec_plain.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../include/obd_class.h"
 #include "../include/lustre_net.h"
 #include "../include/lustre_sec.h"
+#include "ptlrpc_internal.h"
 
 struct plain_sec {
 	struct ptlrpc_sec       pls_base;
