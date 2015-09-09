1c06699a9866b1d26f83ec54ca39e043fbbcd056
Jyri Sarha <jsarha@ti.com>
2015-09-09 21:27:42 +0300

    ASoC: davinci-mcasp: Set .symmetric_rates = 1 in snd_soc_dai_driver
    The TX and RX direction share the same bit clock and frame sync, so
    the samplerate must be the same to both directions.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>
    Acked-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/davinci/davinci-mcasp.c
+++ sound/soc/davinci/davinci-mcasp.c
@@ -1299,6 +1299,7 @@ static struct snd_soc_dai_driver davinci_mcasp_dai[] = {
 		.ops 		= &davinci_mcasp_dai_ops,
 
 		.symmetric_samplebits	= 1,
+		.symmetric_rates	= 1,
 	},
 	{
 		.name		= "davinci-mcasp.1",
