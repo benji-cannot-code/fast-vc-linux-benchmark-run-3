546ecec1b6dadb23cbfe2b9d647258095c2523f0
John Lin <john.lin@realtek.com>
2015-09-09 16:47:48 +0800

    ASoC: rt5645: Add struct dmi_system_id "Google Ultima" for chrome platform
    Signed-off-by: John Lin <john.lin@realtek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5645.c
+++ sound/soc/codecs/rt5645.c
@@ -3237,6 +3237,13 @@ static struct dmi_system_id dmi_platform_intel_braswell[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "Celes"),
 		},
 	},
+	{
+		.ident = "Google Ultima",
+		.callback = strago_quirk_cb,
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "Ultima"),
+		},
+	},
 	{ }
 };
 
