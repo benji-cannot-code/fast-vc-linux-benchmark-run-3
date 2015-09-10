b2cbdfd389f2dcb559b1c70bf1e273761fcb8bd0
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2015-09-10 09:28:39 -0300

    [media] v4l: vsp1: Implement atomic update for the DRM driver
    Add two API functions named vsp1_du_atomic_begin() and
    vsp1_du_atomic_flush() to signal the start and end of an atomic update.
    The vsp1_du_setup_rpf() function is renamed to vsp1_du_atomic_update()
    for consistency.
    
    With this new API, the driver will reprogram all modified inputs
    atomically before restarting the video stream.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/vsp1/vsp1_drm.h
+++ drivers/media/platform/vsp1/vsp1_drm.h
@@ -16,8 +16,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "vsp1_pipe.h"
 
+/**
+ * vsp1_drm - State for the API exposed to the DRM driver
+ * @pipe: the VSP1 pipeline used for display
+ * @num_inputs: number of active pipeline inputs at the beginning of an update
+ * @update: the pipeline configuration has been updated
+ */
 struct vsp1_drm {
 	struct vsp1_pipeline pipe;
+	unsigned int num_inputs;
 	bool update;
 };
 
