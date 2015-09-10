c3ba5a8ac8bddba7c40e97a3b5baa504a799a918
Alim Akhtar <alim.akhtar@samsung.com>
2015-09-10 14:14:35 +0530

    clk: samsung: exynos7: Correct CMU_FSYS1 clocks names
    This patch renames CMU_FSYS1 clocks names to match with user manual.
    And also adds missing gate clock for aclk_fsys1_200.
    
    Signed-off-by: Alim Akhtar <alim.akhtar@samsung.com>
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>

--- include/dt-bindings/clock/exynos7-clk.h
+++ include/dt-bindings/clock/exynos7-clk.h
@@ -64,7 +64,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_SCLK_MMC1			7
 #define CLK_SCLK_MMC0			8
 #define CLK_ACLK_FSYS0_200		9
-#define TOP1_NR_CLK			10
+#define CLK_ACLK_FSYS1_200		10
+#define TOP1_NR_CLK			11
 
 /* CCORE */
 #define PCLK_RTC			1
