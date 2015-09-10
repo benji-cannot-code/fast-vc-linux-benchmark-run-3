192d9a264c7a2de0cc13a77c02d94fb0863d04ae
Rami Rosen <ramirose@gmail.com>
2015-09-10 23:48:09 +0300

    ACPI: change init_acpi_device_notify() to return void
    This patch changes the type of the return value of the init_acpi_device_notify()
    method to be void, as this method never fails and its return value is never
    used.
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/internal.h
+++ drivers/acpi/internal.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PREFIX "ACPI: "
 
 acpi_status acpi_os_initialize1(void);
-int init_acpi_device_notify(void);
+void init_acpi_device_notify(void);
 int acpi_scan_init(void);
 void acpi_pci_root_init(void);
 void acpi_pci_link_init(void);
