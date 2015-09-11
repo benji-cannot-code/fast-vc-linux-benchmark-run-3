2c7d7a930f3ddd45a6b931e9507ed0485ecc2e4d
Russell King <rmk+kernel@arm.linux.org.uk>
2015-09-11 08:17:39 +0100

    ARM: domains: thread_info.h no longer needs asm/domains.h
    As of 1eef5d2f1b46 ("ARM: domains: switch to keeping domain value in
    register") we no longer need to include asm/domains.h into
    asm/thread_info.h.  Remove it.
    
    Tested-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/thread_info.h
+++ arch/arm/include/asm/thread_info.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct task_struct;
 
 #include <asm/types.h>
-#include <asm/domain.h>
 
 typedef unsigned long mm_segment_t;
 
