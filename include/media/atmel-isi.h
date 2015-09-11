7a24e2ea781b7aaade36307a90911dc9144f4b64
Josh Wu <josh.wu@atmel.com>
2015-09-11 04:00:14 -0300

    [media] atmel-isi: setup the ISI_CFG2 register directly
    In the function configure_geometry(), we will setup the ISI CFG2
    according to the sensor output format.
    
    It make no sense to just read back the CFG2 register and just set part
    of it.
    
    So just set up this register directly makes things simpler.
    Currently only support YUV format from camera sensor.
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/media/atmel-isi.h
+++ include/media/atmel-isi.h
@@ -67,6 +67,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Bitfields in CFG2 */
 #define ISI_CFG2_GRAYSCALE			(1 << 13)
+#define ISI_CFG2_COL_SPACE_YCbCr		(0 << 15)
+#define ISI_CFG2_COL_SPACE_RGB			(1 << 15)
 /* Constants for YCC_SWAP(ISI_V2) */
 #define		ISI_CFG2_YCC_SWAP_DEFAULT	(0 << 28)
 #define		ISI_CFG2_YCC_SWAP_MODE_1	(1 << 28)
