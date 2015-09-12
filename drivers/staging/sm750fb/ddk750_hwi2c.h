2e93391bdedf310166882a159c031ed981107d65
Mike Rapoport <mike.rapoport@gmail.com>
2015-09-12 11:07:35 +0300

    staging: sm750fb: rename hwI2CReadReg to sm750_hw_i2c_read_reg
    Fix the checkpatch warning about CamelCase
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_hwi2c.h
+++ drivers/staging/sm750fb/ddk750_hwi2c.h
@@ -6,6 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int sm750_hw_i2c_init(unsigned char busSpeedMode);
 void sm750_hw_i2c_close(void);
 
-unsigned char hwI2CReadReg(unsigned char deviceAddress, unsigned char registerIndex);
+unsigned char sm750_hw_i2c_read_reg(unsigned char deviceAddress, unsigned char registerIndex);
 int hwI2CWriteReg(unsigned char deviceAddress, unsigned char registerIndex, unsigned char data);
 #endif
--- drivers/staging/sm750fb/ddk750_sii164.c
+++ drivers/staging/sm750fb/ddk750_sii164.c
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef USE_HW_I2C
     #define i2cWriteReg hwI2CWriteReg
-    #define i2cReadReg  hwI2CReadReg
+    #define i2cReadReg  sm750_hw_i2c_read_reg
 #else
     #define i2cWriteReg swI2CWriteReg
     #define i2cReadReg  swI2CReadReg
