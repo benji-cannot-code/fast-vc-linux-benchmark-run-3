01a230397f549413d520acc2359dd332bda847b3
Geert Uytterhoeven <geert@linux-m68k.org>
2015-09-13 11:49:13 +0200

    m68k: Wire up membarrier
    $ ./membarrier_test
    membarrier MEMBARRIER_CMD_QUERY syscall available.
    membarrier: MEMBARRIER_CMD_SHARED success.
    membarrier: tests done!
    $
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Greg Ungerer <gerg@uclinux.org>

--- arch/m68k/include/uapi/asm/unistd.h
+++ arch/m68k/include/uapi/asm/unistd.h
@@ -380,5 +380,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_recvmmsg		371
 #define __NR_sendmmsg		372
 #define __NR_userfaultfd	373
+#define __NR_membarrier		374
 
 #endif /* _UAPI_ASM_M68K_UNISTD_H_ */
