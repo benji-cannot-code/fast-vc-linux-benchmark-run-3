1f51e48cf613664e79a36ace0d3a851b4dedd7f2
Lucas Georges <lucas.georges@outlook.com>
2015-09-14 16:52:35 +0200

    staging: ft1000: code style cleanup
    There was a stray '*' in a comment block.
    
    Signed-off-by: Lucas Georges <lucas.georges@outlook.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ft1000/ft1000-usb/ft1000_hw.c
+++ drivers/staging/ft1000/ft1000-usb/ft1000_hw.c
@@ -1547,7 +1547,7 @@ int ft1000_poll(void *dev_id)
 					       FT1000_REG_MAG_WATERMARK);
 			/* ring doorbell to tell DSP that
 			 * ASIC is out of reset
-			 * */
+			 */
 			status = ft1000_write_register(dev,
 						       FT1000_ASIC_RESET_DSP,
 						       FT1000_REG_DOORBELL);
