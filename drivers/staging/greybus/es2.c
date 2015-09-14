ed39ab0b1db4141422133ced3349af5ebd1e65a3
Alexandre Bailon <abailon@baylibre.com>
2015-09-14 18:20:43 +0200

    greybus: es2: change (un)map methods to static
    Endpoints pair will only be managed by es2 driver.
    map_cport_to_ep() and unmap_cport() should be static.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/es2.c
+++ drivers/staging/greybus/es2.c
@@ -144,7 +144,7 @@ static int ep_pair_in_use(struct es1_ap_dev *es1, int ep_pair)
 	return 0;
 }
 
-int map_cport_to_ep(struct es1_ap_dev *es1,
+static int map_cport_to_ep(struct es1_ap_dev *es1,
 				u16 cport_id, int ep_pair)
 {
 	int retval;
@@ -181,7 +181,7 @@ int map_cport_to_ep(struct es1_ap_dev *es1,
 	return retval;
 }
 
-int unmap_cport(struct es1_ap_dev *es1, u16 cport_id)
+static int unmap_cport(struct es1_ap_dev *es1, u16 cport_id)
 {
 	return map_cport_to_ep(es1, cport_id, 0);
 }
