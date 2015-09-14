76b377adeb765d1c222fa28acb8ed34a59b31653
Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
2015-09-14 21:12:45 +0900

    mfd: s2mps11: Add manual shutdown method for Odroid XU3
    On Odroid XU3 board (with S2MPS11 PMIC) the PWRHOLD bit in CTRL1
    register must be manually set to 0 before initiating power off sequence.
    
    One of usual power down methods for Exynos based devices looks like:
    1. PWRHOLD pin of PMIC is connected to PSHOLD of Exynos SoC.
    2. Exynos holds up this pin during system operation.
    3. ACOKB pin of PMIC is pulled up to VBATT and optionally to pin in
       other device.
    4. When PWRHOLD/PSHOLD goes low, the PMIC will turn off the power if
       ACOKB goes high.
    
    On Odroid XU3 family the difference is in (3) - the ACOKB is grounded.
    This means that PMIC must manually set PWRHOLD field to low and then
    wait for signal from Application Processor (the usual change in
    PWRHOLD/PSHOLD pin will actually cut off the power).
    
    The patch adds respective binding allowing Odroid XU3 device to be
    powered off.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski.k@gmail.com>
    Reported-by: Anand Moon <linux.amoon@gmail.com>
    Tested-by: Anand Moon <linux.amoon@gmail.com>
    Reviewed-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/samsung/core.h
+++ include/linux/mfd/samsung/core.h
@@ -133,6 +133,8 @@ struct sec_platform_data {
 	int				buck2_init;
 	int				buck3_init;
 	int				buck4_init;
+	/* Whether or not manually set PWRHOLD to low during shutdown. */
+	bool				manual_poweroff;
 };
 
 /**
