727f12a563a61e88a2ad3d6198f54c05a17f4a58
Florian Westphal <fw@strlen.de>
2015-09-14 17:06:27 +0200

    netfilter: bridge: fix routing of bridge frames with call-iptables=1
    We can't re-use the physoutdev storage area.
    
    1.  When using NFQUEUE in PREROUTING, we attempt to bump a bogus
    refcnt since nf_bridge->physoutdev is garbage (ipv4/ipv6 address)
    
    2. for same reason, we crash in physdev match in FORWARD or later if
    skb is routed instead of bridged.
    
    This increases nf_bridge_info to 40 bytes, but we have no other choice.
    
    Fixes: 72b1e5e4cac7 ("netfilter: bridge: reduce nf_bridge_info to 32 bytes again")
    Reported-by: Sander Eikelenboom <linux@eikelenboom.it>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -180,6 +180,9 @@ struct nf_bridge_info {
 	u8			bridged_dnat:1;
 	__u16			frag_max_size;
 	struct net_device	*physindev;
+
+	/* always valid & non-NULL from FORWARD on, for physdev match */
+	struct net_device	*physoutdev;
 	union {
 		/* prerouting: detect dnat in orig/reply direction */
 		__be32          ipv4_daddr;
@@ -190,9 +193,6 @@ struct nf_bridge_info {
 		 * skb is out in neigh layer.
 		 */
 		char neigh_header[8];
-
-		/* always valid & non-NULL from FORWARD on, for physdev match */
-		struct net_device *physoutdev;
 	};
 };
 #endif
