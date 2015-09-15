dc16caedb496d34a4a2f18de11dc9ceeb606295f
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-09-15 14:06:11 +0900

    staging: wilc1000: remove duplicated include
    wilc_wfi_cfgoperations.h and linux_wlan_common.h are included as
    duplicated in linux_wlan.c.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan.c
+++ drivers/staging/wilc1000/linux_wlan.c
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "linux_wlan_common.h"
 #include "wilc_wlan_if.h"
 #include "wilc_wlan.h"
-#include "wilc_wfi_cfgoperations.h"
-
-#include "linux_wlan_common.h"
 
 #include <linux/slab.h>
 #include <linux/sched.h>
