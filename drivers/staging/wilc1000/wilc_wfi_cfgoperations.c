daba62143e3c7198ef57190d2cbd3e3f162f73e3
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-09-15 14:06:27 +0900

    staging: wilc1000: remove if defined codes of FORCE_P2P_CLIENT
    This patch removes if defined codes of FORCE_P2P_CLIENT.
    This macro is deleted because it it commented out.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
@@ -2029,11 +2029,7 @@ void WILC_WFI_CfgParseRxAction(u8 *buf, u32 len)
 
 	while (index < len) {
 		if (buf[index] == GO_INTENT_ATTR_ID) {
-			#ifdef FORCE_P2P_CLIENT
-			buf[index + 3] = (buf[index + 3]  & 0x01) | (0x0f << 1);
-			#else
 			buf[index + 3] = (buf[index + 3]  & 0x01) | (0x00 << 1);
-			#endif
 		}
 
 		if (buf[index] ==  CHANLIST_ATTR_ID)
@@ -2086,11 +2082,7 @@ void WILC_WFI_CfgParseTxAction(u8 *buf, u32 len, bool bOperChan, u8 iftype)
 
 	while (index < len) {
 		if (buf[index] == GO_INTENT_ATTR_ID) {
-			#ifdef FORCE_P2P_CLIENT
-			buf[index + 3] = (buf[index + 3]  & 0x01) | (0x00 << 1);
-			#else
 			buf[index + 3] = (buf[index + 3]  & 0x01) | (0x0f << 1);
-			#endif
 
 			break;
 		}
