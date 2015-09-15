87bfbc9dca05fe2f2bbbd0e567a082cda9942f52
Igor Kotrasinski <i.kotrasinsk@samsung.com>
2015-09-15 16:55:32 +0200

    usb: gadget: dummy_hcd: in transfer(), return data sent, not limit
    dummy_timer uses transfer() to update transfer limit. However,
    limit passed to dummy_timer changes depending on transfer type,
    so the actual limit is overwritten.
    
    This can cause unpredictably slow / fast bulk transfers when
    coupled with control / interrupt transfers.
    
    Fix by returning actual amount of data sent in transfer() and
    substracting from total.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/gadget/udc/dummy_hcd.c
+++ drivers/usb/gadget/udc/dummy_hcd.c
@@ -1349,6 +1349,7 @@ static int transfer(struct dummy_hcd *dum_hcd, struct urb *urb,
 {
 	struct dummy		*dum = dum_hcd->dum;
 	struct dummy_request	*req;
+	int			sent = 0;
 
 top:
 	/* if there's no request queued, the device is NAKing; return */
@@ -1403,6 +1404,7 @@ top:
 				req->req.status = len;
 			} else {
 				limit -= len;
+				sent += len;
 				urb->actual_length += len;
 				req->req.actual += len;
 			}
@@ -1473,7 +1475,7 @@ top:
 		if (rescan)
 			goto top;
 	}
-	return limit;
+	return sent;
 }
 
 static int periodic_bytes(struct dummy *dum, struct dummy_ep *ep)
@@ -1903,7 +1905,7 @@ restart:
 		default:
 treat_control_like_bulk:
 			ep->last_io = jiffies;
-			total = transfer(dum_hcd, urb, ep, limit, &status);
+			total -= transfer(dum_hcd, urb, ep, limit, &status);
 			break;
 		}
 
