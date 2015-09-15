69f90a52d9b50e4b20cf8da4f778d313301a3053
Alex Gartrell <agartrell@fb.com>
2015-09-14 23:23:05 -0700

    ipvs: add sysctl to ignore tunneled packets
    This is a way to avoid nasty routing loops when multiple ipvs instances can
    forward to eachother.
    
    Signed-off-by: Alex Gartrell <agartrell@fb.com>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- include/net/ip_vs.h
+++ include/net/ip_vs.h
@@ -999,6 +999,7 @@ struct netns_ipvs {
 	int			sysctl_backup_only;
 	int			sysctl_conn_reuse_mode;
 	int			sysctl_schedule_icmp;
+	int			sysctl_ignore_tunneled;
 
 	/* ip_vs_lblc */
 	int			sysctl_lblc_expiration;
@@ -1122,6 +1123,11 @@ static inline int sysctl_schedule_icmp(struct netns_ipvs *ipvs)
 	return ipvs->sysctl_schedule_icmp;
 }
 
+static inline int sysctl_ignore_tunneled(struct netns_ipvs *ipvs)
+{
+	return ipvs->sysctl_ignore_tunneled;
+}
+
 #else
 
 static inline int sysctl_sync_threshold(struct netns_ipvs *ipvs)
@@ -1195,6 +1201,11 @@ static inline int sysctl_conn_reuse_mode(struct netns_ipvs *ipvs)
 }
 
 static inline int sysctl_schedule_icmp(struct netns_ipvs *ipvs)
+{
+	return 0;
+}
+
+static inline int sysctl_ignore_tunneled(struct netns_ipvs *ipvs)
 {
 	return 0;
 }
