341638f06a7491aaf8212379ff210df45d8ad898
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-09-15 14:06:24 +0900

    staging: wilc1000: remove USE_SUPPLICANT_GO_INTENT
    This patch removes USE_SUPPLICANT_GO_INTENT macro that is commented.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.h
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TKIP			BIT(6)
 
 #ifdef WILC_P2P
-/* #define	USE_SUPPLICANT_GO_INTENT */
 
 /*Public action frame index IDs*/
 #define		FRAME_TYPE_ID					0
