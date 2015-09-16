32ab2bee680a7468b1e5facacd1fd938b6aa8ccb
Leo Kim <leo.kim@atmel.com>
2015-09-16 18:36:04 +0900

    staging: wilc1000: remove wilc_errorsupport.h
    This patch removes the wilc_errorsupport.h which is not used anymore and
    also deletes #include "wilc_errorsupport.h" from the source code.
    
    In addition, adds linux_wlan_common.h file in the wilc_msgqueue.c file
    in order to use PRINT macros defined in the linux_wlan_common.h file.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_oswrapper.h
+++ drivers/staging/wilc1000/wilc_oswrapper.h
@@ -17,9 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Os Configuration File */
 #include "wilc_platform.h"
 
-/* Error reporting and handling support */
-#include "wilc_errorsupport.h"
-
 /* Message Queue */
 #include "wilc_msgqueue.h"
 
