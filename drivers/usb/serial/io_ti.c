1820a4e01d81a35d069a72f2f602dd5bdc9867b0
Peter E. Berger <pberger@brimson.com>
2015-09-16 03:13:03 -0500

    USB: io_ti: Remove extra blank lines separating functions
    Remove extra blank lines in the several places where functions were
    separated by more than one.
    
    Signed-off-by: Peter E. Berger <pberger@brimson.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/usb/serial/io_ti.c
+++ drivers/usb/serial/io_ti.c
@@ -443,7 +443,6 @@ static int write_boot_mem(struct edgeport_serial *serial,
 	return status;
 }
 
-
 /* Write edgeport I2C memory to TI chip	*/
 static int write_i2c_mem(struct edgeport_serial *serial,
 		int start_address, int length, __u8 address_type, __u8 *buffer)
@@ -632,8 +631,6 @@ static int write_rom(struct edgeport_serial *serial, int start_address,
 						serial->TI_I2C_Type, buffer);
 	return -EINVAL;
 }
-
-
 
 /* Read a descriptor header from I2C based on type */
 static int get_descriptor_addr(struct edgeport_serial *serial,
@@ -1515,7 +1512,6 @@ stayinbootmode:
 	return 0;
 }
 
-
 static int ti_do_config(struct edgeport_port *port, int feature, int on)
 {
 	int port_number = port->port->port_number;
@@ -1525,7 +1521,6 @@ static int ti_do_config(struct edgeport_port *port, int feature, int on)
 			feature, (__u8)(UMPM_UART1_PORT + port_number),
 			on, NULL, 0);
 }
-
 
 static int restore_mcr(struct edgeport_port *port, __u8 mcr)
 {
@@ -1631,7 +1626,6 @@ static void handle_new_lsr(struct edgeport_port *edge_port, int lsr_data,
 	if (new_lsr & LSR_FRM_ERR)
 		icount->frame++;
 }
-
 
 static void edge_interrupt_callback(struct urb *urb)
 {
