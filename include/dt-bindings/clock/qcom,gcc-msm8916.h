e16490eafcfb250ad1a93cab0df57a9fc2836b73
Georgi Djakov <georgi.djakov@linaro.org>
2015-09-17 19:39:26 +0300

    clk: qcom: Add MSM8916 iommu clocks
    Add support for the msm8916 TCU (Translation Control Unit) clocks that
    are needed for IOMMU.
    
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/dt-bindings/clock/qcom,gcc-msm8916.h
+++ include/dt-bindings/clock/qcom,gcc-msm8916.h
@@ -153,6 +153,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GCC_VENUS0_AHB_CLK			135
 #define GCC_VENUS0_AXI_CLK			136
 #define GCC_VENUS0_VCODEC0_CLK			137
+#define BIMC_DDR_CLK_SRC			138
+#define GCC_APSS_TCU_CLK			139
+#define GCC_GFX_TCU_CLK				140
 
 /* Indexes for GDSCs */
 #define BIMC_GDSC				0
