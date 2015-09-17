3f907e15d01e54bae1bfd183068508d70fdd2e71
Juston Li <juston.h.li@gmail.com>
2015-09-16 19:51:56 -0700

    staging/lustre: use __noreturn for lbug_with_loc
    fixes sparse error:
    drivers/staging/lustre/lustre/libcfs/linux/linux-debug.c:149:6: error:
    symbol 'lbug_with_loc'redeclared with different type (originally declared at
    drivers/staging/lustre/lustre/libcfs/linux/../../../include/linux/libcfs/libcfs_private.h:82)
    - different modifiers
    
    Use the __noreturn macro instead of __attribute__((noreturn))
    The macro is exactly that but its usage seems more common and
    it is bit more readable.
    
    Signed-off-by: Juston Li <juston.h.li@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/libcfs_private.h
+++ drivers/staging/lustre/include/linux/libcfs/libcfs_private.h
@@ -80,7 +80,7 @@ do {									\
 
 #define KLASSERT(e) LASSERT(e)
 
-void lbug_with_loc(struct libcfs_debug_msg_data *)__attribute__((noreturn));
+void __noreturn lbug_with_loc(struct libcfs_debug_msg_data *);
 
 #define LBUG()							  \
 do {								    \
