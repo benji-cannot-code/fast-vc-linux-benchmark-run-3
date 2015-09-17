beabdc5f44bdbbabb0e77af9ec7391912c08bc57
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-09-17 16:48:47 +0900

    staging: wilc1000: remove WILC_TIME typedef
    This patch removes WILC_TIME typedef that is not used anymore.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_platform.h
+++ drivers/staging/wilc1000/wilc_platform.h
@@ -34,8 +34,6 @@ typedef struct __MessageQueue_struct {
 
 
 
-/*Time represented in 64 bit format*/
-typedef time_t WILC_Time;
 
 
 /*******************************************************************
