971a62396c939eb228babdbc9be92136c55efc3d
Ian Abbott <abbotti@mev.co.uk>
2015-09-17 17:19:16 +0100

    staging: comedi: drivers.c: replace #include <linux/dma-mapping.h>
    Comedi's "drivers.c" doesn't use anything from `<linux/dma-mapping.h>`,
    but it does use `DMA_NONE` from `<linux/dma-direction.h>`, so replace
    the appropriate `#include` directive.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers.c
+++ drivers/staging/comedi/drivers.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/ioport.h>
 #include <linux/slab.h>
-#include <linux/dma-mapping.h>
+#include <linux/dma-direction.h>
 #include <linux/interrupt.h>
 #include <linux/firmware.h>
 
