1303b0fb2e59d94d4d1c27abe8b1ea0a1032ecee
Ian Abbott <abbotti@mev.co.uk>
2015-09-17 17:19:15 +0100

    staging: comedi: drivers.c: remove irrelevant #includes
    Comedi's "drivers.c" doesn't use anything from these included headers,
    so remove them.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers.c
+++ drivers/staging/comedi/drivers.c
@@ -21,18 +21,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/module.h>
 #include <linux/errno.h>
-#include <linux/kconfig.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/fcntl.h>
 #include <linux/ioport.h>
-#include <linux/mm.h>
 #include <linux/slab.h>
-#include <linux/highmem.h>	/* for SuSE brokenness */
-#include <linux/vmalloc.h>
-#include <linux/cdev.h>
 #include <linux/dma-mapping.h>
-#include <linux/io.h>
 #include <linux/interrupt.h>
 #include <linux/firmware.h>
 
