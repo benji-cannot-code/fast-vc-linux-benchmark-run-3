77b7eed67cd22ba9c31845002a1f3bb6240e43f1
Arnaldo Carvalho de Melo <acme@redhat.com>
2015-09-17 12:54:30 -0300

    tools build: Add test for presence of __get_cpuid() gcc builtin
    The auxtrace code needed by Intel PT uses the __get_cpuid() gcc builtin,
    that is not present in old systems, breaking the build.
    
    Add a test to check for that builtin and disable AUXTRACE in those
    systems.
    
      [acme@rhel5 linux]$  make NO_LIBPERL=1 -C tools/perf O=/tmp/build/perf install-bin
      make: Entering directory `/home/acme/git/linux/tools/perf'
        BUILD:   Doing 'make -j2' parallel build
    
      Auto-detecting system features:
      <SNIP>
      ...                          lzma: [ on  ]
      ...                     get_cpuid: [ OFF ]
      <SNIP>
      config/Makefile:630: Your gcc lacks the __get_cpuid() builtin, disables support for auxtrace/Intel PT, please install a newer gcc
        MKDIR    /tmp/build/perf/util/
      <SNIP>
    
    This fixes the build on old systems such as RHEL/CentOS 5.11.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: "Naveen N. Rao" <naveen.n.rao@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Victor Kamensky <victor.kamensky@linaro.org>
    Cc: Vinson Lee <vlee@twopensource.com>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-d4puslul0jltoodzpx9r4sje@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/build/Makefile.feature
+++ tools/build/Makefile.feature
@@ -53,7 +53,8 @@ FEATURE_TESTS ?=			\
 	timerfd				\
 	libdw-dwarf-unwind		\
 	zlib				\
-	lzma
+	lzma				\
+	get_cpuid
 
 FEATURE_DISPLAY ?=			\
 	dwarf				\
@@ -70,7 +71,8 @@ FEATURE_DISPLAY ?=			\
 	libunwind			\
 	libdw-dwarf-unwind		\
 	zlib				\
-	lzma
+	lzma				\
+	get_cpuid
 
 # Set FEATURE_CHECK_(C|LD)FLAGS-all for all FEATURE_TESTS features.
 # If in the future we need per-feature checks/flags for features not
--- tools/build/feature/test-all.c
+++ tools/build/feature/test-all.c
@@ -122,6 +122,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # include "test-lzma.c"
 #undef main
 
+#define main main_test_get_cpuid
+# include "test-get_cpuid.c"
+#undef main
+
 int main(int argc, char *argv[])
 {
 	main_test_libpython();
@@ -149,6 +153,7 @@ int main(int argc, char *argv[])
 	main_test_zlib();
 	main_test_pthread_attr_setaffinity_np();
 	main_test_lzma();
+	main_test_get_cpuid();
 
 	return 0;
 }
