d9527cec7d7f41c071cd2bde4e83434f8f7b987c
Eric W. Biederman <ebiederm@xmission.com>
2015-09-18 14:33:00 -0500

    netfilter: nf_tables: Pass struct net in nft_pktinfo
    nft_pktinfo is passed on the stack so this does not bloat any in core
    data structures.
    
    By centrally computing this information this makes maintence of the code
    simpler, and understading of the code easier.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nft_pktinfo {
 	struct sk_buff			*skb;
+	struct net			*net;
 	const struct net_device		*in;
 	const struct net_device		*out;
 	u8				pf;
@@ -31,7 +32,7 @@ static inline void nft_set_pktinfo(struct nft_pktinfo *pkt,
 				   const struct nf_hook_state *state)
 {
 	pkt->skb = skb;
-	pkt->xt.net = state->net;
+	pkt->net = pkt->xt.net = state->net;
 	pkt->in = pkt->xt.in = state->in;
 	pkt->out = pkt->xt.out = state->out;
 	pkt->hook = pkt->xt.hooknum = state->hook;
