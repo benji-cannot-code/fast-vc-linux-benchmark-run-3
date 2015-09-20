37a89ea957324ebed50ac7d7f99cc8dc7901f886
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-09-20 10:19:39 +0200

    staging: rtl8192e: Rename dm_CheckTXPowerTracking_ThermalMeter
    Use naming schema found in other rtlwifi devices.
    Rename dm_CheckTXPowerTracking_ThermalMeter to
    _rtl92e_dm_check_tx_power_tracking_thermal.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_dm.c
+++ drivers/staging/rtl8192e/rtl8192e/rtl_dm.c
@@ -1010,7 +1010,8 @@ static void dm_CheckTXPowerTracking_TSSI(struct net_device *dev)
 	}
 
 }
-static void dm_CheckTXPowerTracking_ThermalMeter(struct net_device *dev)
+
+static void _rtl92e_dm_check_tx_power_tracking_thermal(struct net_device *dev)
 {
 	struct r8192_priv *priv = rtllib_priv(dev);
 	static u8	TM_Trigger;
@@ -1051,7 +1052,7 @@ static void _rtl92e_dm_check_tx_power_tracking(struct net_device *dev)
 	if (priv->IC_Cut >= IC_VersionCut_D)
 		dm_CheckTXPowerTracking_TSSI(dev);
 	else
-		dm_CheckTXPowerTracking_ThermalMeter(dev);
+		_rtl92e_dm_check_tx_power_tracking_thermal(dev);
 }
 
 static void _rtl92e_dm_cck_tx_power_adjust_tssi(struct net_device *dev,
