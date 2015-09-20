daad2a6a157b9c1cec36c1226a239826e44755c5
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-09-20 10:14:16 +0200

    staging: rtl8192e: Rename r8192_wx_adapter_power_status
    Use naming schema found in other rtlwifi devices.
    Rename r8192_wx_adapter_power_status to _rtl92e_wx_adapter_power_status.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_wx.c
+++ drivers/staging/rtl8192e/rtl8192e/rtl_wx.c
@@ -175,9 +175,9 @@ static int r8192_wx_force_reset(struct net_device *dev,
 
 }
 
-static int r8192_wx_adapter_power_status(struct net_device *dev,
-		struct iw_request_info *info,
-		union iwreq_data *wrqu, char *extra)
+static int _rtl92e_wx_adapter_power_status(struct net_device *dev,
+					   struct iw_request_info *info,
+					   union iwreq_data *wrqu, char *extra)
 {
 	struct r8192_priv *priv = rtllib_priv(dev);
 	struct rt_pwr_save_ctrl *pPSC = (struct rt_pwr_save_ctrl *)
@@ -1198,7 +1198,7 @@ static iw_handler r8192_private_handler[] = {
 	(iw_handler)r8192_wx_force_reset,
 	(iw_handler)NULL,
 	(iw_handler)NULL,
-	(iw_handler)r8192_wx_adapter_power_status,
+	(iw_handler)_rtl92e_wx_adapter_power_status,
 	(iw_handler)NULL,
 	(iw_handler)NULL,
 	(iw_handler)NULL,
