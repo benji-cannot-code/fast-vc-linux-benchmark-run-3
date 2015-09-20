38bd9e6d38961bc43e8e67cf88ff49ab40be8f64
Daniel J Blueman <daniel@numascale.com>
2015-09-21 01:02:01 +0800

    x86/numachip: Add Numachip IPI optimisations
    When sending IPIs, first check if the non-local part of the source and
    destination APIC IDs match; if so, send via the local APIC for efficiency.
    
    Secondly, since the AMD BIOS-kernel developer guide states IPI delivery
    will occur invarient of prior deliver status, avoid polling the delivery
    status bit for efficiency.
    
    Signed-off-by: Daniel J Blueman <daniel@numascale.com>
    Acked-by: Steffen Persvold <sp@numascale.com>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: http://lkml.kernel.org/r/1442768522-19217-3-git-send-email-daniel@numascale.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/numachip/numachip_csr.h
+++ arch/x86/include/asm/numachip/numachip_csr.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NUMACHIP_LCSR_BASE	0x3ffffe000000ULL
 #define NUMACHIP_LCSR_LIM	0x3fffffffffffULL
 #define NUMACHIP_LCSR_SIZE	(NUMACHIP_LCSR_LIM - NUMACHIP_LCSR_BASE + 1)
+#define NUMACHIP_LAPIC_BITS	8
 
 static inline void *lcsr_address(unsigned long offset)
 {
