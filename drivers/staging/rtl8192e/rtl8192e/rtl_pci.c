6ee1b41877fe8fe3a68ac7f57ac2bcfcacbf17a2
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2015-09-20 10:19:19 +0200

    staging: rtl8192e: Rename rtl8192_parse_pci_configuration
    Use naming schema found in other rtlwifi devices.
    Rename rtl8192_parse_pci_configuration to _rtl92e_parse_pci_configuration.
    
    Signed-off-by: Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_pci.c
+++ drivers/staging/rtl8192e/rtl8192e/rtl_pci.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rtl_pci.h"
 #include "rtl_core.h"
 
-static void rtl8192_parse_pci_configuration(struct pci_dev *pdev,
+static void _rtl92e_parse_pci_configuration(struct pci_dev *pdev,
 					    struct net_device *dev)
 {
 	struct r8192_priv *priv = (struct r8192_priv *)rtllib_priv(dev);
@@ -93,7 +93,7 @@ bool rtl92e_check_adapter(struct pci_dev *pdev, struct net_device *dev)
 		return false;
 	}
 
-	rtl8192_parse_pci_configuration(pdev, dev);
+	_rtl92e_parse_pci_configuration(pdev, dev);
 
 	return true;
 }
