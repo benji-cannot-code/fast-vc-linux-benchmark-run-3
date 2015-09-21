75b38a93eacec468405ef0c7e71fe4cf5e4c3f62
Tony Cho <tony.cho@atmel.com>
2015-09-21 12:16:53 +0900

    staging: wilc1000: remove typedef from the struct tstrHostIfSetOperationMode
    This patch removes typedef from the struct tstrHostIfSetOperationMode
    and renames it to op_mode in order to comply with the Linux coding
    style.
    
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -434,7 +434,7 @@ union message_body {
 	struct set_ip_addr strHostIfSetIP;
 	struct drv_handler strHostIfSetDrvHandler;
 	struct set_multicast strHostIfSetMulti;
-	tstrHostIfSetOperationMode strHostIfSetOperationMode;
+	struct op_mode strHostIfSetOperationMode;
 	tstrHostIfSetMacAddress strHostIfSetMacAddress;
 	tstrHostIfGetMacAddress strHostIfGetMacAddress;
 	tstrHostIfBASessionInfo strHostIfBASessionInfo;
@@ -709,7 +709,8 @@ static s32 Handle_SetWfiDrvHandler(tstrWILC_WFIDrv *drvHandler,
  *  @date
  *  @version	1.0
  */
-static s32 Handle_SetOperationMode(tstrWILC_WFIDrv *drvHandler, tstrHostIfSetOperationMode *pstrHostIfSetOperationMode)
+static s32 Handle_SetOperationMode(tstrWILC_WFIDrv *drvHandler,
+				   struct op_mode *pstrHostIfSetOperationMode)
 {
 
 	s32 s32Error = 0;
