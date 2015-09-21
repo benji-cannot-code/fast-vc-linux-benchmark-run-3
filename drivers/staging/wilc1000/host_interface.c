aef4c567c5bb3fa7a2f25de638d75208ed6ed941
Tony Cho <tony.cho@atmel.com>
2015-09-21 12:17:02 +0900

    staging: wilc1000: remove u32Intialized which is unnecessary
    This patch removes the global variable, u32Intialized which is not necessary
    from the host_interface.c file.
    
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -6356,7 +6356,6 @@ void host_int_send_network_info_to_host
  *  @date		8 March 2012
  *  @version		1.0
  */
-static u32 u32Intialized;
 static u32 clients_count;
 
 s32 host_int_init(tstrWILC_WFIDrv **phWFIDrv)
@@ -6365,12 +6364,6 @@ s32 host_int_init(tstrWILC_WFIDrv **phWFIDrv)
 	tstrWILC_WFIDrv *pstrWFIDrv;
 	int err;
 
-	/*if(u32Intialized == 1)
-	 * {
-	 *      PRINT_D(HOSTINF_DBG,"Host interface is previously initialized\n");
-	 * *phWFIDrv = (WILC_WFIDrvHandle)gWFiDrvHandle; //Will be adjusted later for P2P
-	 *      return 0;
-	 * }	*/
 	PRINT_D(HOSTINF_DBG, "Initializing host interface for client %d\n", clients_count + 1);
 
 	gbScanWhileConnected = false;
@@ -6481,7 +6474,6 @@ s32 host_int_init(tstrWILC_WFIDrv **phWFIDrv)
 		goto _fail_mem_;
 	}
 
-	u32Intialized = 1;
 	clients_count++; /* increase number of created entities */
 
 	return s32Error;
@@ -6521,12 +6513,6 @@ s32 host_int_deinit(tstrWILC_WFIDrv *hWFIDrv)
 
 	/*obtain driver handle*/
 	tstrWILC_WFIDrv *pstrWFIDrv = (tstrWILC_WFIDrv *)hWFIDrv;
-	/*if(u32Intialized == 0)
-	 * {
-	 *      PRINT_ER("Host Interface is not initialized\n");
-	 *      return 0;
-	 * }*/
-
 
 	if (pstrWFIDrv == NULL)	{
 		PRINT_ER("pstrWFIDrv = NULL\n");
@@ -6600,7 +6586,6 @@ s32 host_int_deinit(tstrWILC_WFIDrv *hWFIDrv)
 	down(&(pstrWFIDrv->gtOsCfgValuesSem));
 
 	/*Setting the gloabl driver handler with NULL*/
-	u32Intialized = 0;
 	/* gWFiDrvHandle = NULL; */
 	ret = remove_handler_in_list(pstrWFIDrv);
 	if (ret)
