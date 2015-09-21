90b20dc7e9a2f6c5ce4fbedf39e3068f54e61450
Tony Cho <tony.cho@atmel.com>
2015-09-21 12:16:51 +0900

    staging: wilc1000: remove typedef from the struct tstrHostIfSetDrvHandler
    This patch removes typedef from the struct tstrHostIfSetDrvHandler and
    renames it to drv_handler in order to comply with the Linux coding
    style.
    
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.h
+++ drivers/staging/wilc1000/host_interface.h
@@ -256,9 +256,9 @@ typedef struct {
 	void *u32UserConnectPvoid;
 } tstrWILC_UsrConnReq;
 
-typedef struct {
+struct drv_handler {
 	u32 u32Address;
-} tstrHostIfSetDrvHandler;
+};
 
 typedef struct {
 	u32 u32Mode;
