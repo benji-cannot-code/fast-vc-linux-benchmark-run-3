490c67ffa154ea055c770de1a643a2f76706a582
Paul Burton <paul.burton@imgtec.com>
2015-09-22 11:42:53 -0700

    MIPS: Allow RIXI for 32-bit kernels on MIPS64
    Commit a68d09a156b2 ("MIPS: Don't use RI/XI with 32-bit kernels on
    64-bit CPUs") prevented use of RIXI on MIPS64 systems, stating that the
    "TLB handlers cannot handle this case". What they actually couldn't
    handle was cases where there were less fill bits in the Entry{Lo,Hi}
    registers than bits used by software in PTEs. The handlers can now deal
    with this case, so enable RIXI for MIPS32 kernels on MIPS64 systems.
    
    Note that beyond the obvious benefits provided by having RIXI on such
    systems, this is required for systems implementing MIPSr6 where RIXI
    cannot be disabled.
    
    This reverts commit a68d09a156b2 ("MIPS: Don't use RI/XI with 32-bit
    kernels on 64-bit CPUs").
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: Joshua Kinard <kumba@gentoo.org>
    Cc: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Cc: Maciej W. Rozycki <macro@linux-mips.org>
    Cc: linux-kernel@vger.kernel.org
    Cc: James Hogan <james.hogan@imgtec.com>
    Cc: Markos Chandras <markos.chandras@imgtec.com>
    Patchwork: https://patchwork.linux-mips.org/patch/11219/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -132,11 +132,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #ifndef cpu_has_rixi
-# ifdef CONFIG_64BIT
-# define cpu_has_rixi		(cpu_data[0].options & MIPS_CPU_RIXI)
-# else /* CONFIG_32BIT */
-# define cpu_has_rixi		((cpu_data[0].options & MIPS_CPU_RIXI) && !cpu_has_64bits)
-# endif
+#define cpu_has_rixi		(cpu_data[0].options & MIPS_CPU_RIXI)
 #endif
 
 #ifndef cpu_has_mmips
