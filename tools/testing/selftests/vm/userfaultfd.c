a5ce5b44b5e549a9eaa27c9477ae9e753b857fd6
Thierry Reding <treding@nvidia.com>
2015-09-22 14:58:52 -0700

    userfaultfd: selftests: vm: pick up sanitized kernel headers
    Add the usr/include subdirectory of the top-level tree to the include
    path, and make sure to include headers without relative paths to make
    sure the sanitized headers get picked up.  Otherwise the compiler will
    not be able to find the linux/compiler.h header included by the non-
    sanitized include/uapi/linux/userfaultfd.h.
    
    While at it, make sure to only hardcode the syscall numbers on x86 and
    PowerPC if they haven't been properly picked up from the headers.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Acked-by: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Shuah Khan <shuahkh@osg.samsung.com>
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/vm/userfaultfd.c
+++ tools/testing/selftests/vm/userfaultfd.c
@@ -65,8 +65,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/syscall.h>
 #include <sys/ioctl.h>
 #include <pthread.h>
-#include "../../../../include/uapi/linux/userfaultfd.h"
+#include <linux/userfaultfd.h>
 
+#ifndef __NR_userfaultfd
 #ifdef __x86_64__
 #define __NR_userfaultfd 323
 #elif defined(__i386__)
@@ -77,6 +78,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_userfaultfd 355
 #else
 #error "missing __NR_userfaultfd definition"
+#endif
 #endif
 
 static unsigned long nr_cpus, nr_pages, nr_pages_per_cpu, page_size;
