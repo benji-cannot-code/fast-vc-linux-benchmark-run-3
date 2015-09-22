ed246bfe7c39f2887de293b8b994a3bec9f4d652
Andrea Arcangeli <aarcange@redhat.com>
2015-09-22 14:58:55 -0700

    userfaultfd: selftest: headers fixup
    Depend on "make headers_install" to create proper headers to include and
    provide syscall numbers.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Shuah Khan <shuahkh@osg.samsung.com>
    Cc: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/vm/userfaultfd.c
+++ tools/testing/selftests/vm/userfaultfd.c
@@ -68,17 +68,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/userfaultfd.h>
 
 #ifndef __NR_userfaultfd
-#ifdef __x86_64__
-#define __NR_userfaultfd 323
-#elif defined(__i386__)
-#define __NR_userfaultfd 374
-#elif defined(__powewrpc__)
-#define __NR_userfaultfd 364
-#elif defined(__s390__)
-#define __NR_userfaultfd 355
-#else
 #error "missing __NR_userfaultfd definition"
-#endif
 #endif
 
 static unsigned long nr_cpus, nr_pages, nr_pages_per_cpu, page_size;
