3bc0070989b0e102238a08b8d157e752f83491ca
Boris Barbulovski <bbarbulovski@gmail.com>
2015-09-22 11:36:28 -0700

    Add current selection check.
    Signed-off-by: Boris Barbulovski <bbarbulovski@gmail.com>
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>
    [mmarek: I missed it in the original Qt5 patch set, which caused a crash]
    Signed-off-by: Michal Marek <mmarek@suse.com>

--- scripts/kconfig/qconf.cc
+++ scripts/kconfig/qconf.cc
@@ -401,6 +401,9 @@ void ConfigList::updateSelection(void)
 	struct menu *menu;
 	enum prop_type type;
 
+	if (selectedItems().count() == 0)
+		return;
+
 	ConfigItem* item = (ConfigItem*)selectedItems().first();
 	if (!item)
 		return;
@@ -1625,6 +1628,10 @@ void ConfigMainWindow::goBack(void)
 	configList->setParentMenu();
 	if (configList->rootEntry == &rootmenu)
 		backAction->setEnabled(false);
+
+	if (menuList->selectedItems().count() == 0)
+		return;
+
 	item = (ConfigItem*)menuList->selectedItems().first();
 	oldSelection = item;
 	while (item) {
