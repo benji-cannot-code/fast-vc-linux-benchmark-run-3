d588389c608351b14c8fb5d356cc1d05a34cec5b
Mike Rapoport <mike.rapoport@gmail.com>
2015-09-22 10:52:16 +0300

    staging: sm750fb: sm750_help.h: spacing fix
    Remove unnecessary space in sm750_help.h to fix the following
    checkpatch.pl error:
    drivers/staging/sm750fb/sm750_help.h:46: space prohibited after that '~' (ctx:ExW)
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/sm750_help.h
+++ drivers/staging/sm750fb/sm750_help.h
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define FIELD_CLEAR(reg, field) \
 ( \
-	~ _F_MASK(reg ## _ ## field) \
+	~_F_MASK(reg ## _ ## field) \
 )
 
 /* Field Macros */
