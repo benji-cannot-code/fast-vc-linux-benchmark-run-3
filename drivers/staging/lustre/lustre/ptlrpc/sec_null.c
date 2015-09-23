3690af8f47d8b0f2da0a7606c456082ef54ef2e6
Anton Gerasimov <anton.gerasimov@openmailbox.org>
2015-09-24 00:04:04 +0300

    staging: lustre: include header with local ptlrpc declarations
    Include the header with declarations of variables that are shared between
    several sources in ptlrpc module into the appropriate source files.
    
    Signed-off-by: Anton Gerasimov <anton.gerasimov@openmailbox.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/sec_null.c
+++ drivers/staging/lustre/lustre/ptlrpc/sec_null.c
@@ -48,6 +48,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../include/lustre_net.h"
 #include "../include/lustre_sec.h"
 
+#include "ptlrpc_internal.h"
+
 static struct ptlrpc_sec_policy null_policy;
 static struct ptlrpc_sec	null_sec;
 static struct ptlrpc_cli_ctx    null_cli_ctx;
