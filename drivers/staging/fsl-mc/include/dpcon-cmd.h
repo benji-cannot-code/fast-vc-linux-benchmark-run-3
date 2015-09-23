945f8da2055381c3b9637870b552ee32793187fc
J. German Rivera <German.Rivera@freescale.com>
2015-09-23 16:11:02 -0500

    staging: fsl-mc: up-rev dpcon binary interface to v2.0
    dpcon object minor version number updated to match latest
    MC firmware. This change is needed because the dpcon
    object binds to the allocator and the current driver match
    logic uses object version numbers.
    
    Signed-off-by: J. German Rivera <German.Rivera@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/include/dpcon-cmd.h
+++ drivers/staging/fsl-mc/include/dpcon-cmd.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* DPCON Version */
 #define DPCON_VER_MAJOR				2
-#define DPCON_VER_MINOR				0
+#define DPCON_VER_MINOR				1
 
 /* Command IDs */
 #define DPCON_CMDID_CLOSE				0x800
