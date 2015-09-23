6b9441e6f3adfb33d707afa87951eb8b0dc27759
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-09-23 18:03:46 +0900

    staging: wilc1000: remove wilc_platform.h include file
    This patch removes wilc_platform.h include file that is defined in
    wilc_msgqueue.h file. After removing it, compilation error occurs so
    that it is included two header files as <linux/semaphore.h> and
    <linux/slab.h> at wilc_msgqueue.h and wilc_msgqueue.c
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_msgqueue.c
+++ drivers/staging/wilc1000/wilc_msgqueue.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include "linux_wlan_common.h"
 #include <linux/errno.h>
+#include <linux/slab.h>
 
 /*!
  *  @author		syounan
--- drivers/staging/wilc1000/wilc_msgqueue.h
+++ drivers/staging/wilc1000/wilc_msgqueue.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  @version	1.0
  */
 
-#include "wilc_platform.h"
+#include <linux/semaphore.h>
 
 /* Message Queue type is a structure */
 typedef struct __Message_struct {
