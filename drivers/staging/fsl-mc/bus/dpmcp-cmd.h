06cd64d6285fa94630ea4ffbf576cd20aad597ea
J. German Rivera <German.Rivera@freescale.com>
2015-09-23 16:11:01 -0500

    staging: fsl-mc: up-rev dpmcp binary interface to v2.0
    Add cmd_flags parameter to all dpbp APIs to comply
    with the dpmcp 2.0 MC interface. Updated version
    major number. Pass irq args in struct instead of
    separate args.
    
    Signed-off-by: J. German Rivera <German.Rivera@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/dpmcp-cmd.h
+++ drivers/staging/fsl-mc/bus/dpmcp-cmd.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* DPMCP Version */
 #define DPMCP_VER_MAJOR				2
-#define DPMCP_VER_MINOR				0
+#define DPMCP_VER_MINOR				1
 
 /* Command IDs */
 #define DPMCP_CMDID_CLOSE				0x800
