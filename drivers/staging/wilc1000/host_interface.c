63690a64cc97290032335055f6574a8c45dfd20e
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-09-23 18:03:47 +0900

    staging: wilc1000: move wilc_msgqueue.h include file
    This patch moves wilc_msgqueue.h include file from wilc_oswrapper.h to
    host_interface.c because message queue function as wilc_mq_create,
    wilc_mq_send, wilc_mq_recv and wilc_mq_destroy are used only at
    host_interface.c file.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "host_interface.h"
 #include "coreconfigurator.h"
 #include "wilc_wlan_if.h"
+#include "wilc_msgqueue.h"
 
 extern u8 connecting;
 
