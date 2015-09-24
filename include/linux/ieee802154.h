8b22bdd029bf2d76ac2c1a5fff06b54d361dc60c
Alexander Aring <alex.aring@gmail.com>
2015-09-24 09:37:10 +0200

    ieee802154: remove unnecessary includes
    This patch removes some unnecessary includes from ieee802154 header,
    which was introduced by commit b609fb54adfa ("ieee802154: add helpers for
    frame control checks").
    
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- include/linux/ieee802154.h
+++ include/linux/ieee802154.h
@@ -26,9 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <linux/random.h>
-#include <linux/skbuff.h>
-#include <linux/unaligned/memmove.h>
-#include <asm/byteorder.h>
 
 #define IEEE802154_MTU			127
 #define IEEE802154_ACK_PSDU_LEN		5
