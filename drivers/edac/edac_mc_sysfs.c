d7602e8b5d891a9b52e240b1a62d85230ac370e6
Toshi Kani <toshi.kani@hpe.com>
2015-09-24 13:59:27 -0600

    EDAC: Don't allow empty DIMM labels
    Updating dimm_label to an empty string does not make much sense. Change
    the sysfs dimm_label store operation to fail a request when an input
    string is empty.
    
    Suggested-by: Borislav Petkov <bp@alien8.de>
    Signed-off-by: Toshi Kani <toshi.kani@hpe.com>
    Cc: elliott@hpe.com
    Cc: Mauro Carvalho Chehab <mchehab@osg.samsung.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Link: http://lkml.kernel.org/r/1443124767.25474.172.camel@hpe.com
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/edac_mc_sysfs.c
+++ drivers/edac/edac_mc_sysfs.c
@@ -249,7 +249,7 @@ static ssize_t channel_dimm_label_store(struct device *dev,
 	if (data[count - 1] == '\0' || data[count - 1] == '\n')
 		copy_count -= 1;
 
-	if (copy_count >= sizeof(rank->dimm->label))
+	if (copy_count == 0 || copy_count >= sizeof(rank->dimm->label))
 		return -EINVAL;
 
 	strncpy(rank->dimm->label, data, copy_count);
@@ -510,7 +510,7 @@ static ssize_t dimmdev_label_store(struct device *dev,
 	if (data[count - 1] == '\0' || data[count - 1] == '\n')
 		copy_count -= 1;
 
-	if (copy_count >= sizeof(dimm->label))
+	if (copy_count == 0 || copy_count >= sizeof(dimm->label))
 		return -EINVAL;
 
 	strncpy(dimm->label, data, copy_count);
