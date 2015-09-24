184f0007b80092c960ded56ff19784a62d2ffe8e
Axel Lin <axel.lin@ingics.com>
2015-09-24 23:34:01 +0800

    ASoC: rl6347a: Clean up unneeded inclusion of header files
    Also move the include of sound/hda_verbs.h to rl6347a.h because it is used
    in rl6347a.h.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt286.c
+++ sound/soc/codecs/rt286.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/jack.h>
 #include <linux/workqueue.h>
 #include <sound/rt286.h>
-#include <sound/hda_verbs.h>
 
 #include "rl6347a.h"
 #include "rt286.h"
