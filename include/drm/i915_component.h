7ab6b4e02de897ca745ab7e80afd5c1665f0e97f
Libin Yang <libin.yang@intel.com>
2015-09-25 09:36:12 +0800

    drm/i915: set proper N/CTS in modeset
    When modeset occurs and the TMDS frequency is set to some
    speical values, the N/CTS need to be set manually if audio
    is playing.
    
    Signed-off-by: Libin Yang <libin.yang@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/drm/i915_component.h
+++ include/drm/i915_component.h
@@ -25,8 +25,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _I915_COMPONENT_H_
 #define _I915_COMPONENT_H_
 
+/* MAX_PORT is the number of port
+ * It must be sync with I915_MAX_PORTS defined i915_drv.h
+ * 5 should be enough as only HSW, BDW, SKL need such fix.
+ */
+#define MAX_PORTS 5
+
 struct i915_audio_component {
 	struct device *dev;
+	/**
+	 * @aud_sample_rate: the array of audio sample rate per port
+	 */
+	int aud_sample_rate[MAX_PORTS];
 
 	const struct i915_audio_component_ops {
 		struct module *owner;
