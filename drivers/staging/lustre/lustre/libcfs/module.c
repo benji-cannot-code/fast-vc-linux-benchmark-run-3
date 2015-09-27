bc4543f2242092f783082f08394b5a041963603c
Arnd Bergmann <arnd@arndb.de>
2015-09-27 16:45:33 -0400

    staging/lustre: remove wrappers for timer functions
    This is a simple cleanup that I did after noticing that the abstraction
    for the timer functions in completely pointless, and the one user (ptlrpc)
    can just as well call the native Linux functions.
    
    For good measure, this also removes the empty libcfs_arch_init()
    and libcfs_arch_cleanup() functions that are defined in the
    same file.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/libcfs/module.c
+++ drivers/staging/lustre/lustre/libcfs/module.c
@@ -702,7 +702,6 @@ static int init_libcfs_module(void)
 {
 	int rc;
 
-	libcfs_arch_init();
 	libcfs_init_nidstrings();
 
 	rc = libcfs_debug_init(5 * 1024 * 1024);
@@ -778,8 +777,6 @@ static void exit_libcfs_module(void)
 	rc = libcfs_debug_cleanup();
 	if (rc)
 		pr_err("LustreError: libcfs_debug_cleanup: %d\n", rc);
-
-	libcfs_arch_cleanup();
 }
 
 MODULE_VERSION("1.0.0");
