634a6060be24289e29cc6d0d5a7a252a18b0fdf2
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-09-30 08:15:42 +0900

    staging: wilc1000: delete wilc_oswrapper.h
    This patch deletes wilc_oswrapper.h that has nothing inside.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan_if.h
+++ drivers/staging/wilc1000/wilc_wlan_if.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* #define USE_OLD_SPI_SW */
 
 #include <linux/semaphore.h>
-#include "wilc_oswrapper.h"
 #include "linux_wlan_common.h"
 
 /********************************************
