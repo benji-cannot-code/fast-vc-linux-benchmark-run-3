b397a6136548a0d5590d4eae0f610a187b0db14f
Oleg Drokin <green@linuxhacker.ru>
2015-09-28 23:43:55 -0400

    staging/lustre/obdclass: Remove unused functions from genops.c
    These functions are mostly used on the server.
    
    class_uuid2obd, get_devices_count, class_obd_list, class_conn2obd,
    class_conn2cliimp, class_connected_export, obd_exports_barrier,
    kuc_* (kernel-userspace communications).
    
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre_import.h
+++ drivers/staging/lustre/lustre/include/lustre_import.h
@@ -375,7 +375,6 @@ extern unsigned int at_max;
 /* genops.c */
 struct obd_export;
 struct obd_import *class_exp2cliimp(struct obd_export *);
-struct obd_import *class_conn2cliimp(struct lustre_handle *);
 
 /** @} import */
 
