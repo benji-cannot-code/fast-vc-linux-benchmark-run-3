8fc9f5222e749cc3050a66b510ec7199362f5bf7
Tony Cho <tony.cho@atmel.com>
2015-09-30 18:44:25 +0900

    staging: wilc1000: rename strHostIFCfgParamAttr
    This patch renames strHostIFCfgParamAttr to cfg_info to avoid CamelCase
    naming convention.
    
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -420,7 +420,7 @@ union message_body {
 	struct rcvd_net_info net_info;
 	struct rcvd_async_info async_info;
 	struct key_attr key_info;
-	struct cfg_param_attr strHostIFCfgParamAttr;
+	struct cfg_param_attr cfg_info;
 	struct set_channel strHostIFSetChan;
 	struct get_channel strHostIFGetChan;
 	struct set_beacon strHostIFSetBeacon;
@@ -4125,7 +4125,7 @@ static int hostIFthread(void *pvArg)
 
 		case HOST_IF_MSG_CFG_PARAMS:
 
-			Handle_CfgParam(msg.drvHandler, &msg.body.strHostIFCfgParamAttr);
+			Handle_CfgParam(msg.drvHandler, &msg.body.cfg_info);
 			break;
 
 		case HOST_IF_MSG_SET_CHANNEL:
@@ -5855,7 +5855,7 @@ s32 hif_set_cfg(tstrWILC_WFIDrv *hWFIDrv, tstrCfgParamVal *pstrCfgParamVal)
 	/* prepare the WiphyParams Message */
 	memset(&msg, 0, sizeof(struct host_if_msg));
 	msg.id = HOST_IF_MSG_CFG_PARAMS;
-	msg.body.strHostIFCfgParamAttr.pstrCfgParamVal = *pstrCfgParamVal;
+	msg.body.cfg_info.pstrCfgParamVal = *pstrCfgParamVal;
 	msg.drvHandler = hWFIDrv;
 
 	s32Error = wilc_mq_send(&gMsgQHostIF, &msg, sizeof(struct host_if_msg));
