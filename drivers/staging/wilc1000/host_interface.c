34e807b53b61ccd170fe0ad0f27b5dddd6d7e35d
Tony Cho <tony.cho@atmel.com>
2015-09-30 18:44:29 +0900

    staging: wilc1000: remove struct del_beacon
    This patch removes struct del_beacon which is not necessary. This patch
    also changes Handle_DelBeacon function by removing 2nd parameter, struct
    del_beacon because it is not used inside.
    
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -270,22 +270,6 @@ struct set_beacon {
 											*                              after the TIM inormation element	*/
 };
 
-
-
-/*!
- *  @struct             del_beacon
- *  @brief		Del Beacon  message body
- *  @details
- *  @todo
- *  @sa
- *  @author		Adham Abozaeid
- *  @date		15 July 2012
- *  @version		1.0
- */
-struct del_beacon {
-	u8 u8dummy;
-};
-
 /*!
  *  @struct             set_multicast
  *  @brief		set Multicast filter Address
@@ -409,7 +393,6 @@ union message_body {
 	struct cfg_param_attr cfg_info;
 	struct channel_attr channel_info;
 	struct set_beacon strHostIFSetBeacon;
-	struct del_beacon strHostIFDelBeacon;
 	struct add_sta_param strAddStaParam;
 	struct del_sta strDelStaParam;
 	struct add_sta_param strEditStaParam;
@@ -3245,14 +3228,13 @@ ERRORHANDLER:
 /**
  *  @brief Handle_AddBeacon
  *  @details       Sending config packet to delete beacon
- *  @param[in]   struct del_beacon *pstrDelBeacon
+ *  @param[in]	tstrWILC_WFIDrv *drvHandler
  *  @return         NONE
  *  @author
  *  @date
  *  @version	1.0
  */
-static void Handle_DelBeacon(tstrWILC_WFIDrv *drvHandler,
-			     struct del_beacon *pstrDelBeacon)
+static void Handle_DelBeacon(tstrWILC_WFIDrv *drvHandler)
 {
 	s32 s32Error = 0;
 	tstrWID strWID;
@@ -4157,7 +4139,7 @@ static int hostIFthread(void *pvArg)
 			break;
 
 		case HOST_IF_MSG_DEL_BEACON:
-			Handle_DelBeacon(msg.drvHandler, &msg.body.strHostIFDelBeacon);
+			Handle_DelBeacon(msg.drvHandler);
 			break;
 
 		case HOST_IF_MSG_ADD_STATION:
