e9133307a0e58616e98f30c38556587d4106230a
Prasanna Karthik <mkarthi3@visteon.com>
2015-09-30 12:27:46 +0000

    Bluetooth: btuart_cs: remove obsolete header
    Use <linux/io.h> instead of <asm/io.h>, fixes checkpatch
    Warning;
    
    Signed-off-by: Prasanna Karthik <mkarthi3@visteon.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/btuart_cs.c
+++ drivers/bluetooth/btuart_cs.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial.h>
 #include <linux/serial_reg.h>
 #include <linux/bitops.h>
-#include <asm/io.h>
+#include <linux/io.h>
 
 #include <pcmcia/cistpl.h>
 #include <pcmcia/ciscode.h>
