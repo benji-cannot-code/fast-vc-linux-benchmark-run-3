a02a1e4dc89ed69ae3ce1a44265f998221a28611
Brian Norris <computersforpeace@gmail.com>
2015-09-30 09:54:26 -0700

    mtd: nand: vf610_nfc: include missing pincrl/consumer.h
    This must have been implicitly included on the builds I tested. Reported
    by numerous test bots:
    
       drivers/mtd/nand/vf610_nfc.c: In function 'vf610_nfc_resume':
       drivers/mtd/nand/vf610_nfc.c:660:2: error: implicit declaration of function 'pinctrl_pm_select_default_state' [-Werror=implicit-function-declaration]
         pinctrl_pm_select_default_state(dev);
         ^
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Acked-by: Stefan Agner <stefan@agner.ch>

--- drivers/mtd/nand/vf610_nfc.c
+++ drivers/mtd/nand/vf610_nfc.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/partitions.h>
 #include <linux/of_mtd.h>
 #include <linux/of_device.h>
+#include <linux/pinctrl/consumer.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 
