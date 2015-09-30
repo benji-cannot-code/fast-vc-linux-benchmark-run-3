baa55b9fb123b5c08868a7a437e58c9d30f3de81
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-09-30 09:39:16 +0900

    ALSA: firewire-digi00x: add skeleton for Digi 002/003 family
    This commit adds a new driver for Digidesign 002/003 family. This commit
    just creates/removes card instance according to bus event. More functions
    will be added in following commits.
    
    Digidesign 002/003 family consists of:
     * Agere FW802B for IEEE 1394 PHY layer
     * PDI 1394L40 for IEEE 1394 LINK layer and IEC 61883 interface
     * ALTERA ACEX EP1K50 for IEC 61883 layer and DSP controller
     * ADSP-21065L for signal processing
    
    [minor cleanup using skip_spaces() by tiwai]
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

---
+++ sound/firewire/digi00x/digi00x.h
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * digi00x.h - a part of driver for Digidesign Digi 002/003 family
+ *
+ * Copyright (c) 2014-2015 Takashi Sakamoto
+ *
+ * Licensed under the terms of the GNU General Public License, version 2.
+ */
+
+#ifndef SOUND_DIGI00X_H_INCLUDED
+#define SOUND_DIGI00X_H_INCLUDED
+
+#include <linux/compat.h>
+#include <linux/device.h>
+#include <linux/firewire.h>
+#include <linux/module.h>
+#include <linux/mod_devicetable.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+
+#include <sound/core.h>
+#include <sound/initval.h>
+
+#include "../lib.h"
+
+struct snd_dg00x {
+	struct snd_card *card;
+	struct fw_unit *unit;
+
+	struct mutex mutex;
+};
+
+#endif
