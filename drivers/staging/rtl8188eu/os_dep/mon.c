ee652e0b1f40a00a6d6040ea7c8b9b74228570c8
Larry Finger <Larry.Finger@lwfinger.net>
2015-09-30 10:20:47 -0500

    staging: rtl8188eu: Add missing include
    In commit 0a0796eb16f3, there is no include to mon.h, a newly created
    header file. This omission leads to the following Sparse warnings:
    
      CHECK   drivers/staging/rtl8188eu/os_dep/mon.c
    drivers/staging/rtl8188eu/os_dep/mon.c:81:6: warning: symbol 'rtl88eu_mon_recv_hook' was not declared. Should it be static?
    drivers/staging/rtl8188eu/os_dep/mon.c:113:6: warning: symbol 'rtl88eu_mon_xmit_hook' was not declared. Should it be static?
    drivers/staging/rtl8188eu/os_dep/mon.c:167:19: warning: symbol 'rtl88eu_mon_init' was not declared. Should it be static?
    drivers/staging/rtl8188eu/os_dep/mon.c:188:6: warning: symbol 'rtl88eu_mon_deinit' was not declared. Should it be static?
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: Jakub Sitnicki <jsitnicki@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/mon.c
+++ drivers/staging/rtl8188eu/os_dep/mon.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drv_types.h>
 #include <rtw_recv.h>
 #include <rtw_xmit.h>
+#include <mon.h>
 
 /**
  * unprotect_frame() - unset Protected flag and strip off IV and ICV/MIC
