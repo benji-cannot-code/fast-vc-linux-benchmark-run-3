e1702b7083edd88eeebced942ce7549b8676c2ec
Glen Lee <glen.lee@atmel.com>
2015-10-01 16:03:40 +0900

    staging: wilc1000: remove function pointer wlan_cfg_set
    This patch removes function pointer wlan_cfg_set and just call
    the function wilc_wlan_cfg_set. Remove static from the function also.
    
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/coreconfigurator.c
+++ drivers/staging/wilc1000/coreconfigurator.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "coreconfigurator.h"
 #include "wilc_wlan_if.h"
+#include "wilc_wlan.h"
 #include <linux/errno.h>
 #include <linux/slab.h>
 #define TAG_PARAM_OFFSET	(MAC_HDR_LEN + TIME_STAMP_LEN + \
@@ -614,8 +615,7 @@ s32 send_config_pkt(u8 u8Mode, tstrWID *pstrWIDs,
 	} else {
 		PRINT_D(CORECONFIG_DBG, "Net Dev is initialized\n");
 	}
-	if (gpstrWlanOps->wlan_cfg_set == NULL ||
-	    gpstrWlanOps->wlan_cfg_get == NULL)	{
+	if (gpstrWlanOps->wlan_cfg_get == NULL)	{
 		PRINT_D(CORECONFIG_DBG, "Set and Get is still not initialized\n");
 		return 1;
 	} else {
@@ -643,10 +643,12 @@ s32 send_config_pkt(u8 u8Mode, tstrWID *pstrWIDs,
 	} else if (u8Mode == SET_CFG) {
 		for (counter = 0; counter < u32WIDsCount; counter++) {
 			PRINT_D(CORECONFIG_DBG, "Sending config SET PACKET WID:%x\n", pstrWIDs[counter].u16WIDid);
-			if (!gpstrWlanOps->wlan_cfg_set(!counter,
-							pstrWIDs[counter].u16WIDid, pstrWIDs[counter].ps8WidVal,
-							pstrWIDs[counter].s32ValueSize,
-							(counter == u32WIDsCount - 1), drvHandler)) {
+			if (!wilc_wlan_cfg_set(!counter,
+					       pstrWIDs[counter].u16WIDid,
+					       pstrWIDs[counter].ps8WidVal,
+					       pstrWIDs[counter].s32ValueSize,
+					       (counter == u32WIDsCount - 1),
+					       drvHandler)) {
 				ret = -1;
 				printk("[Sendconfigpkt]Set Timed out\n");
 				break;
