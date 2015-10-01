dc7913f2616de0480a1e6c6d7c07d0ca46b1567b
Glen Lee <glen.lee@atmel.com>
2015-10-01 16:03:38 +0900

    staging: wilc1000: remove function pointer wlan_cleanup
    This patch removes function pointer wlan_cleanup and just call the function
    wilc_wlan_cleanup. Remove static from the function also.
    After changing function pointer wlan_cleanup with wilc_wlan_cleanup,
    the define wilc_wlan_deinit will be like folowing.
    
    -define wilc_wlan_deinit(nic)  { wilc_wlan_cleanup(); }
    
    The define is unnecessary so just call wilc_wlan_cleanup instead of
    wilc_wlan_deinit() and remove the define also.
    
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan.c
+++ drivers/staging/wilc1000/linux_wlan.c
@@ -87,10 +87,6 @@ static struct notifier_block g_dev_notifier = {
 	.notifier_call = dev_state_ev_handler
 };
 
-#define wilc_wlan_deinit(nic)	{ if (&g_linux_wlan->oup != NULL)	 \
-		if (g_linux_wlan->oup.wlan_cleanup != NULL) \
-			g_linux_wlan->oup.wlan_cleanup(); }
-
 #define IRQ_WAIT	1
 #define IRQ_NO_WAIT	0
 /*
@@ -984,7 +980,7 @@ void wilc1000_wlan_deinit(linux_wlan_t *nic)
 		wilc_wlan_stop();
 
 		PRINT_D(INIT_DBG, "Deinitializing WILC Wlan\n");
-		wilc_wlan_deinit(nic);
+		wilc_wlan_cleanup();
 #if (defined WILC_SDIO) && (!defined WILC_SDIO_IRQ_GPIO)
   #if defined(PLAT_ALLWINNER_A20) || defined(PLAT_ALLWINNER_A23) || defined(PLAT_ALLWINNER_A31)
 		PRINT_D(INIT_DBG, "Disabling IRQ 2\n");
@@ -1133,7 +1129,7 @@ u8 wilc1000_prepare_11b_core(wilc_wlan_inp_t *nwi,	wilc_wlan_oup_t *nwo, linux_w
 
 	while ((core_11b_ready() && (READY_CHECK_THRESHOLD > (trials++)))) {
 		PRINT_D(INIT_DBG, "11b core not ready yet: %u\n", trials);
-		wilc_wlan_deinit(nic);
+		wilc_wlan_cleanup();
 		wilc_wlan_global_reset();
 		sdio_unregister_driver(&wilc_bus);
 
@@ -1331,7 +1327,7 @@ _fail_irq_init_:
 _fail_threads_:
 		wlan_deinitialize_threads(g_linux_wlan);
 _fail_wilc_wlan_:
-		wilc_wlan_deinit(g_linux_wlan);
+		wilc_wlan_cleanup();
 _fail_locks_:
 		wlan_deinit_locks(g_linux_wlan);
 		PRINT_ER("WLAN Iinitialization FAILED\n");
