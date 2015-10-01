f95f3f5c1b62e0e3caa31cec70ff9a934a73694e
Chris Wilson <chris@chris-wilson.co.uk>
2015-10-01 12:18:29 +0100

    drm/i915: Avoid GPU stalls from kswapd
    Exclude active GPU pages from the purview of the background shrinker
    (kswapd), as these cause uncontrollable GPU stalls. Given that the
    shrinker is rerun until the freelists are satisfied, we should have
    opportunity in subsequent passes to recover the pages once idle. If the
    machine does run out of memory entirely, we have the forced idling in the
    oom-notifier as a means of releasing all the pages we can before an oom
    is prematurely executed.
    
    Note that this relies upon an up-front retire_requests to keep the
    inactive list in shape, which was added in a previous patch, mostly as
    execlist ctx pinning band-aids.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Damien Lespiau <damien.lespiau@intel.com>
    [danvet: Add note about retire_requests.]
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_gem_shrinker.c
+++ drivers/gpu/drm/i915/i915_gem_shrinker.c
@@ -127,6 +127,9 @@ i915_gem_shrink(struct drm_i915_private *dev_priv,
 			    obj->madv != I915_MADV_DONTNEED)
 				continue;
 
+			if ((flags & I915_SHRINK_ACTIVE) == 0 && obj->active)
+				continue;
+
 			drm_gem_object_reference(&obj->base);
 
 			/* For the unbound phase, this should be a no-op! */
@@ -165,7 +168,9 @@ i915_gem_shrink(struct drm_i915_private *dev_priv,
 unsigned long i915_gem_shrink_all(struct drm_i915_private *dev_priv)
 {
 	return i915_gem_shrink(dev_priv, -1UL,
-			       I915_SHRINK_BOUND | I915_SHRINK_UNBOUND);
+			       I915_SHRINK_BOUND |
+			       I915_SHRINK_UNBOUND |
+			       I915_SHRINK_ACTIVE);
 }
 
 static bool i915_gem_shrinker_lock(struct drm_device *dev, bool *unlock)
@@ -218,7 +223,7 @@ i915_gem_shrinker_count(struct shrinker *shrinker, struct shrink_control *sc)
 			count += obj->base.size >> PAGE_SHIFT;
 
 	list_for_each_entry(obj, &dev_priv->mm.bound_list, global_list) {
-		if (obj->pages_pin_count == num_vma_bound(obj))
+		if (!obj->active && obj->pages_pin_count == num_vma_bound(obj))
 			count += obj->base.size >> PAGE_SHIFT;
 	}
 
