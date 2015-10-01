8ee57f5b7f56258fd7bb0eb1ec150d067cad1dc3
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2015-10-01 09:35:01 -0300

    [media] DocBook: Fix documentation for struct v4l2_dv_timings
    Fix this warning:
    	include/media/v4l2-dv-timings.h:29: warning: cannot understand function prototype: 'const struct v4l2_dv_timings v4l2_dv_timings_presets[]; '
    
    Unfortunately, currently, it seems that doc-nano doesn't support
    documenting extern static vars.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/media/v4l2-dv-timings.h
+++ include/media/v4l2-dv-timings.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/videodev2.h>
 
-/**
+/*
  * v4l2_dv_timings_presets: list of all dv_timings presets.
  */
 extern const struct v4l2_dv_timings v4l2_dv_timings_presets[];
