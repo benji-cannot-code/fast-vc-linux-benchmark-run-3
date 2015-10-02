d43232637012ebd3bf5f5ec9cce34a91b7556725
Daniel Borkmann <daniel@iogearbox.net>
2015-10-02 18:42:00 +0200

    ebpf: include perf_event only where really needed
    Commit ea317b267e9d ("bpf: Add new bpf map type to store the pointer
    to struct perf_event") added perf_event.h to the main eBPF header, so
    it gets included for all users. perf_event.h is actually only needed
    from array map side, so lets sanitize this a bit.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Kaixu Xia <xiakaixu@huawei.com>
    Acked-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <uapi/linux/bpf.h>
 #include <linux/workqueue.h>
 #include <linux/file.h>
-#include <linux/perf_event.h>
 
 struct bpf_map;
 
