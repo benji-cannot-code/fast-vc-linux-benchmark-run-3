c96dfbbd5d76d7c52091901aa8ab8ade58f8e318
Shraddha Barke <shraddha.6596@gmail.com>
2015-10-02 16:19:27 +0530

    Staging: lustre: ptlrpc: Declare sptlrpc_rule_set_free as static
    Declare sptlrpc_rule_set_free as static since it is accessed from this
    particular file only. Also remove its declaration from
    header file
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/sec_config.c
+++ drivers/staging/lustre/lustre/ptlrpc/sec_config.c
@@ -216,7 +216,7 @@ int sptlrpc_parse_rule(char *param, struct sptlrpc_rule *rule)
 }
 EXPORT_SYMBOL(sptlrpc_parse_rule);
 
-void sptlrpc_rule_set_free(struct sptlrpc_rule_set *rset)
+static void sptlrpc_rule_set_free(struct sptlrpc_rule_set *rset)
 {
 	LASSERT(rset->srs_nslot ||
 		(rset->srs_nrule == 0 && rset->srs_rules == NULL));
@@ -226,7 +226,6 @@ void sptlrpc_rule_set_free(struct sptlrpc_rule_set *rset)
 		sptlrpc_rule_set_init(rset);
 	}
 }
-EXPORT_SYMBOL(sptlrpc_rule_set_free);
 
 /*
  * return 0 if the rule set could accommodate one more rule.
