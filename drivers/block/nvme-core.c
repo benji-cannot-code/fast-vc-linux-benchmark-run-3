8bbd99321afceac2d81701d4bf2ee2af68d15b9b
Christoph Hellwig <hch@lst.de>
2015-10-02 15:25:49 +0200

    nvme: move hardware structures out of the uapi version of nvme.h
    Currently all NVMe command and completion structures are exposed to userspace
    through the uapi version of nvme.h.  They are not an ABI between the kernel
    and userspace, and will change in C-incompatible way for future versions of
    the spec.  Move them to the kernel version of the file and rename the uapi
    header to nvme_ioctl.h so that userspace can easily detect the presence of
    the new clean header.  Nvme-cli already carries a local copy of the header,
    so it won't be affected by this move.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/sg.h>
 #include <asm-generic/io-64-nonatomic-lo-hi.h>
 
+#include <uapi/linux/nvme_ioctl.h>
 #include "nvme.h"
 
 #define NVME_MINORS		(1U << MINORBITS)
