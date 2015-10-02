082d63a42ce5808b91cddec9651977ad63f6e23d
Cathy Avery <cathy.avery@oracle.com>
2015-10-02 09:35:01 -0400

    xen-blkfront: check for null drvdata in blkback_changed (XenbusStateClosing)
    xen-blkfront will crash if the check to talk_to_blkback()
    in blkback_changed()(XenbusStateInitWait) returns an error.
    The driver data is freed and info is set to NULL. Later during
    the close process via talk_to_blkback's call to xenbus_dev_fatal()
    the null pointer is passed to and dereference in blkfront_closing.
    
    CC: stable@vger.kernel.org
    Signed-off-by: Cathy Avery <cathy.avery@oracle.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/block/xen-blkfront.c
+++ drivers/block/xen-blkfront.c
@@ -1969,7 +1969,8 @@ static void blkback_changed(struct xenbus_device *dev,
 			break;
 		/* Missed the backend's Closing state -- fallthrough */
 	case XenbusStateClosing:
-		blkfront_closing(info);
+		if (info)
+			blkfront_closing(info);
 		break;
 	}
 }
