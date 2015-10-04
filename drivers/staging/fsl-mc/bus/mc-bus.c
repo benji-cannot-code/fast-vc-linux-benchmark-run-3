6f5580065896e68326256d67bdcbb089bfdf679f
Itai Katz <itai.katz@freescale.com>
2015-10-04 10:09:51 +0300

    staging: fsl-mc: abstract test for whether a dprc is a root dprc
    Instead of relying on assumptions about fields in data
    structures, abstract the test for whether a dprc is a root
    dprc into a function.
    
    Signed-off-by: Itai Katz <itai.katz@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/mc-bus.c
+++ drivers/staging/fsl-mc/bus/mc-bus.c
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct kmem_cache *mc_dev_cache;
 
+static bool fsl_mc_is_root_dprc(struct device *dev);
+
 /**
  * fsl_mc_bus_match - device to driver matching callback
  * @dev: the MC object device structure to match against
@@ -51,7 +53,7 @@ static int fsl_mc_bus_match(struct device *dev, struct device_driver *drv)
 	 * Only exception is the root DPRC, which is a special case.
 	 */
 	if ((mc_dev->obj_desc.state & DPRC_OBJ_STATE_PLUGGED) == 0 &&
-	    &mc_dev->dev != fsl_mc_bus_type.dev_root)
+	    !fsl_mc_is_root_dprc(&mc_dev->dev))
 		goto out;
 
 	/*
@@ -215,6 +217,14 @@ bool fsl_mc_bus_exists(void)
 	return fsl_mc_bus_type.dev_root;
 }
 EXPORT_SYMBOL_GPL(fsl_mc_bus_exists);
+
+/**
+ * fsl_mc_is_root_dprc - function to check if a given device is a root dprc
+ */
+static bool fsl_mc_is_root_dprc(struct device *dev)
+{
+	return dev == fsl_mc_bus_type.dev_root;
+}
 
 static int get_dprc_icid(struct fsl_mc_io *mc_io,
 			 int container_id, u16 *icid)
@@ -501,7 +511,7 @@ void fsl_mc_device_remove(struct fsl_mc_device *mc_dev)
 			mc_dev->mc_io = NULL;
 		}
 
-		if (&mc_dev->dev == fsl_mc_bus_type.dev_root)
+		if (fsl_mc_is_root_dprc(&mc_dev->dev))
 			fsl_mc_bus_type.dev_root = NULL;
 	}
 
@@ -727,7 +737,7 @@ static int fsl_mc_bus_remove(struct platform_device *pdev)
 {
 	struct fsl_mc *mc = platform_get_drvdata(pdev);
 
-	if (WARN_ON(&mc->root_mc_bus_dev->dev != fsl_mc_bus_type.dev_root))
+	if (WARN_ON(!fsl_mc_is_root_dprc(&mc->root_mc_bus_dev->dev)))
 		return -EINVAL;
 
 	fsl_mc_device_remove(mc->root_mc_bus_dev);
