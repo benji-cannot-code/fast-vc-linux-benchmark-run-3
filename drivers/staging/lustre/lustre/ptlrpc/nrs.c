7b485d359404c926ec091cb10f3a1f239dbaa54b
Shraddha Barke <shraddha.6596@gmail.com>
2015-10-04 13:00:09 +0530

    Staging: lustre: ptlrpc: nrs: Declare as static
    Declare ptlrpc_nrs_policy_register as static since it is used
    only in this particular file. Also remove corresponding declaration
    from header files.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/nrs.c
+++ drivers/staging/lustre/lustre/ptlrpc/nrs.c
@@ -1112,7 +1112,7 @@ again:
  * \retval -ve error
  * \retval   0 success
  */
-int ptlrpc_nrs_policy_register(struct ptlrpc_nrs_pol_conf *conf)
+static int ptlrpc_nrs_policy_register(struct ptlrpc_nrs_pol_conf *conf)
 {
 	struct ptlrpc_service *svc;
 	struct ptlrpc_nrs_pol_desc *desc;
@@ -1248,7 +1248,6 @@ fail:
 
 	return rc;
 }
-EXPORT_SYMBOL(ptlrpc_nrs_policy_register);
 
 /**
  * Setup NRS heads on all service partitions of service \a svc, and register
