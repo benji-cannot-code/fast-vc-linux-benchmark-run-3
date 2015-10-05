6e476a21c80a2b2e894ceafe3cb9bb4fbfdfd6bd
Suman Anna <s-anna@ti.com>
2015-10-05 18:28:21 -0500

    ARM: OMAP: dmtimer: check for fixed timers during config
    The omap_dm_timer_set_source() function provides a means for client
    users to configure the mux parent for a GPTimer's functional clock.
    However, not all timers are configurable (Eg: Timer12 on DRA7 is fed
    by an internal 32k oscillator clock, and does not have configurable
    parent clocks). So, check for such cases and proceed with out throwing
    an error.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/plat-omap/dmtimer.c
+++ arch/arm/plat-omap/dmtimer.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/clk.h>
+#include <linux/clk-provider.h>
 #include <linux/module.h>
 #include <linux/io.h>
 #include <linux/device.h>
@@ -533,6 +534,12 @@ int omap_dm_timer_set_source(struct omap_dm_timer *timer, int source)
 
 	if (IS_ERR(timer->fclk))
 		return -EINVAL;
+
+#if defined(CONFIG_COMMON_CLK)
+	/* Check if the clock has configurable parents */
+	if (clk_hw_get_num_parents(__clk_get_hw(timer->fclk)) < 2)
+		return 0;
+#endif
 
 	switch (source) {
 	case OMAP_TIMER_SRC_SYS_CLK:
