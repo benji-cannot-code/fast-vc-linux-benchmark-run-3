8f2337375223c60af537643801ac99a0402fcf85
Thierry Reding <thierry.reding@gmail.com>
2015-10-05 14:42:37 +0200

    MIPS: JZ4740: Remove unused linux/leds_pwm.h include
    The board code never sets up a leds-pwm device, so including the header
    is not necessary.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>
    Acked-by: Lars-Peter Clausen <lars@metafoo.de>
    Cc: Lars-Peter Clausen <lars@metafoo.de>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/11262/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/jz4740/board-qi_lb60.c
+++ arch/mips/jz4740/board-qi_lb60.c
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regulator/fixed.h>
 #include <linux/regulator/machine.h>
 
-#include <linux/leds_pwm.h>
-
 #include <asm/mach-jz4740/platform.h>
 
 #include "clock.h"
