9fc7219a14ceda4ac5e9b17cdc2575d94cd40814
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-10-05 14:23:08 -0700

    staging: comedi: quatech_daqp_cs: fix daqp_ai_cancel()
    Make sure interrupts are disabled and the event flags are cleared when
    an analog input async command is canceled.
    
    Remove the unnecessary calls to stop any running conversions in the
    (*insn_read) and (*do_cmd) functions. The comedi core will only call
    these functions if the subdevice is not busy (it has already been
    canceled).
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/quatech_daqp_cs.c
+++ drivers/staging/comedi/drivers/quatech_daqp_cs.c
@@ -172,19 +172,21 @@ static int daqp_clear_events(struct comedi_device *dev, int loops)
 	return -EBUSY;
 }
 
-/* Cancel a running acquisition */
-
-static int daqp_ai_cancel(struct comedi_device *dev, struct comedi_subdevice *s)
+static int daqp_ai_cancel(struct comedi_device *dev,
+			  struct comedi_subdevice *s)
 {
 	struct daqp_private *devpriv = dev->private;
 
 	if (devpriv->stop)
 		return -EIO;
 
+	/*
+	 * Stop any conversions, disable interrupts, and clear
+	 * the status event flags.
+	 */
 	outb(DAQP_CMD_STOP, dev->iobase + DAQP_CMD_REG);
-
-	/* flush any linguring data in FIFO - superfluous here */
-	/* outb(DAQP_CMD_RSTF, dev->iobase + DAQP_CMD_REG); */
+	outb(0, dev->iobase + DAQP_CTRL_REG);
+	inb(dev->iobase + DAQP_STATUS_REG);
 
 	return 0;
 }
@@ -298,9 +300,6 @@ static int daqp_ai_insn_read(struct comedi_device *dev,
 
 	if (devpriv->stop)
 		return -EIO;
-
-	/* Stop any running conversion */
-	daqp_ai_cancel(dev, s);
 
 	outb(0, dev->iobase + DAQP_AUX_REG);
 
@@ -466,9 +465,6 @@ static int daqp_ai_cmd(struct comedi_device *dev, struct comedi_subdevice *s)
 
 	if (devpriv->stop)
 		return -EIO;
-
-	/* Stop any running conversion */
-	daqp_ai_cancel(dev, s);
 
 	outb(0, dev->iobase + DAQP_AUX_REG);
 
