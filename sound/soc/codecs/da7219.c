084716c2ac7eff48895141f4590da4d889230d9b
Adam Thomson <Adam.Thomson.Opensource@diasemi.com>
2015-10-05 15:10:04 +0100

    ASoC: da7219: Improve error handling for regulator supplies
    Currently if bulk_enable() of supplies fails, the code still goes on
    to try and put the device into active state, and set expected IO
    voltage of the device. This doesn't really make sense so code now
    returns on bulk_enable() failure, with an error message. Also,
    to help with debug, failure to get supplies also provides an error
    message.
    
    Signed-off-by: Adam Thomson <Adam.Thomson.Opensource@diasemi.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/da7219.c
+++ sound/soc/codecs/da7219.c
@@ -1555,8 +1555,10 @@ static int da7219_handle_supplies(struct snd_soc_codec *codec)
 
 	ret = devm_regulator_bulk_get(codec->dev, DA7219_NUM_SUPPLIES,
 				      da7219->supplies);
-	if (ret)
+	if (ret) {
+		dev_err(codec->dev, "Failed to get supplies");
 		return ret;
+	}
 
 	/* Determine VDDIO voltage provided */
 	vddio = da7219->supplies[DA7219_SUPPLY_VDDIO].consumer;
@@ -1568,6 +1570,10 @@ static int da7219_handle_supplies(struct snd_soc_codec *codec)
 
 	/* Enable main supplies */
 	ret = regulator_bulk_enable(DA7219_NUM_SUPPLIES, da7219->supplies);
+	if (ret) {
+		dev_err(codec->dev, "Failed to enable supplies");
+		return ret;
+	}
 
 	/* Ensure device in active mode */
 	snd_soc_write(codec, DA7219_SYSTEM_ACTIVE, DA7219_SYSTEM_ACTIVE_MASK);
@@ -1575,7 +1581,7 @@ static int da7219_handle_supplies(struct snd_soc_codec *codec)
 	/* Update IO voltage level range */
 	snd_soc_write(codec, DA7219_IO_CTRL, io_voltage_lvl);
 
-	return ret;
+	return 0;
 }
 
 static void da7219_handle_pdata(struct snd_soc_codec *codec)
