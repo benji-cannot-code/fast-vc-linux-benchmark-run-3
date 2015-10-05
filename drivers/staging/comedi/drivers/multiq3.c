6494e6d0e7e2d777a051d8c2cac02e1ab60a5e49
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-10-05 15:33:11 -0700

    staging: comedi: multiq3: tidy up status register bit defines
    For aesthetics, move these defines closer to the register define and
    use the BIT macro to define the bits.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/multiq3.c
+++ drivers/staging/comedi/drivers/multiq3.c
@@ -48,6 +48,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MULTIQ3_AI_REG			0x04
 #define MULTIQ3_AI_CONV_REG		0x04
 #define MULTIQ3_STATUS_REG		0x06
+#define MULTIQ3_STATUS_EOC		BIT(3)
+#define MULTIQ3_STATUS_EOC_I		BIT(4)
 #define MULTIQ3_CTRL_REG		0x06
 #define MULTIQ3_CLK_REG			0x08
 #define MULTIQ3_ENC_DATA_REG		0x0c
@@ -64,12 +66,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MULTIQ3_DA_LOAD                0x1800
 
 #define MULTIQ3_CONTROL_MUST    0x0600
-
-/*
- * flags for STATUS register
- */
-#define MULTIQ3_STATUS_EOC      0x008
-#define MULTIQ3_STATUS_EOC_I    0x010
 
 /*
  * flags for encoder control
