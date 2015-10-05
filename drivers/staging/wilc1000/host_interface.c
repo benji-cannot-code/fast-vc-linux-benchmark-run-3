3dc370303f95bde5b3acaa7544d4c49d118e0aa7
Chaehyun Lim <chaehyun.lim@gmail.com>
2015-10-05 19:35:02 +0900

    staging: wilc1000: remove multiple blank in host_int_remove_wep_key
    This patch removes multiple blank in host_int_remove_wep_key found by
    checkpatch.
    
    CHECK: Please don't use multiple blank lines
    FILE: drivers/staging/wilc1000/host_interface.c:4299:
    FILE: drivers/staging/wilc1000/host_interface.c:4309:
    FILE: drivers/staging/wilc1000/host_interface.c:4315:
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -4194,7 +4194,6 @@ int host_int_remove_wep_key(tstrWILC_WFIDrv *wfi_drv, u8 index)
 	int result = 0;
 	struct host_if_msg msg;
 
-
 	if (wfi_drv == NULL) {
 		result = -EFAULT;
 		PRINT_ER("Failed to send setup multicast config packet\n");
@@ -4204,13 +4203,10 @@ int host_int_remove_wep_key(tstrWILC_WFIDrv *wfi_drv, u8 index)
 	/* prepare the Remove Wep Key Message */
 	memset(&msg, 0, sizeof(struct host_if_msg));
 
-
 	msg.id = HOST_IF_MSG_KEY;
 	msg.body.key_info.enuKeyType = WEP;
 	msg.body.key_info.u8KeyAction = REMOVEKEY;
 	msg.drvHandler = wfi_drv;
-
-
 
 	msg.body.key_info.
 	uniHostIFkeyAttr.strHostIFwepAttr.u8Wepidx = index;
