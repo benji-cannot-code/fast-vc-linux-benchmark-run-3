29a6185596cca35386402afc4ffec71cebfbd6a4
Arnd Bergmann <arnd@arndb.de>
2015-10-06 22:59:40 +0200

    genirq: Fix handle_bad_irq kerneldoc comment
    A recent cleanup removed the 'irq' parameter from many functions, but
    left the documentation for this in place for at least one function.
    
    This removes it.
    
    Fixes: bd0b9ac405e1 ("genirq: Remove irq argument from irq flow handlers")
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Grygorii Strashko <grygorii.strashko@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: kbuild-all@01.org
    Cc: Austin Schuh <austin@peloton-tech.com>
    Cc: Santosh Shilimkar <ssantosh@kernel.org>
    Cc: linux-arm-kernel@lists.infradead.org
    Link: http://lkml.kernel.org/r/5400000.cD19rmgWjV@wuerfel
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/irq/handle.c
+++ kernel/irq/handle.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * handle_bad_irq - handle spurious and unhandled irqs
- * @irq:       the interrupt number
  * @desc:      description of the interrupt
  *
  * Handles spurious and unhandled IRQ's. It also prints a debugmessage.
