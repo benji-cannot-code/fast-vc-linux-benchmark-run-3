981c0ff90d80bc4e21e1282cd025db5e78cb2770
Ulf Hansson <ulf.hansson@linaro.org>
2015-10-06 14:27:42 +0200

    PM / Domains: Remove pm_genpd_poweroff_unused() API
    As the last user of the pm_genpd_poweroff_unused() API has moved into
    relying on genpd to deal with this internally from a late_initcall, let's
    remove the API.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -126,9 +126,7 @@ extern int pm_genpd_remove_subdomain(struct generic_pm_domain *genpd,
 				     struct generic_pm_domain *target);
 extern void pm_genpd_init(struct generic_pm_domain *genpd,
 			  struct dev_power_governor *gov, bool is_off);
-
 extern int pm_genpd_poweron(struct generic_pm_domain *genpd);
-extern void pm_genpd_poweroff_unused(void);
 
 extern struct dev_power_governor simple_qos_governor;
 extern struct dev_power_governor pm_domain_always_on_gov;
@@ -171,7 +169,6 @@ static inline int pm_genpd_poweron(struct generic_pm_domain *genpd)
 {
 	return -ENOSYS;
 }
-static inline void pm_genpd_poweroff_unused(void) {}
 #endif
 
 static inline int pm_genpd_add_device(struct generic_pm_domain *genpd,
