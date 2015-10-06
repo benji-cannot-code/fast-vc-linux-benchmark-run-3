86e5ea8102d4e4a8832b404ce7e5f3ff449787b1
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-10-06 11:11:06 -0700

    staging: comedi: mf6x4: prefer using the BIT macro
    Fix the checkpatch.pl issues.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
     drivers/staging/comedi/drivers/mf6x4.c | 9 ---------
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/mf6x4.c
+++ drivers/staging/comedi/drivers/mf6x4.c
@@ -34,9 +34,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Registers present in BAR0 memory region */
 #define MF624_GPIOC_R					0x54
 
-#define MF6X4_GPIOC_EOLC /* End Of Last Conversion */	(1 << 17)
-#define MF6X4_GPIOC_LDAC /* Load DACs */		(1 << 23)
-#define MF6X4_GPIOC_DACEN				(1 << 26)
+#define MF6X4_GPIOC_EOLC	BIT(17)	/* End Of Last Conversion */
+#define MF6X4_GPIOC_LDAC	BIT(23)	/* Load DACs */
+#define MF6X4_GPIOC_DACEN	BIT(26)
 
 /* BAR1 registers */
 #define MF6X4_DIN_R					0x10
