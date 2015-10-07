d6d33343127e38424b107a84da6ed7a1e159e213
H Hartley Sweeten <hsweeten@visionengravers.com>
2015-10-06 17:23:41 -0700

    staging: comedi: me_daq: tidy up dac data registers
    For aesthetics, use a macro that takes the analog output channel
    and returns the correct offset for the data register.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/me_daq.c
+++ drivers/staging/comedi/drivers/me_daq.c
@@ -95,10 +95,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   ME_DAC_CTRL_GAIN(x)		BIT(11 - ((x) & 0x3))
 #define   ME_DAC_CTRL_MASK(x)		(ME_DAC_CTRL_BIPOLAR(x) |	\
 					 ME_DAC_CTRL_GAIN(x))
-#define ME_DAC_DATA_A			0x0014	/* - | W */
-#define ME_DAC_DATA_B			0x0016	/* - | W */
-#define ME_DAC_DATA_C			0x0018	/* - | W */
-#define ME_DAC_DATA_D			0x001A	/* - | W */
+#define ME_AO_DATA_REG(x)		(0x14 + ((x) * 2))	/* - | W */
 #define ME_COUNTER_ENDDATA_A		0x001C	/* - | W */
 #define ME_COUNTER_ENDDATA_B		0x001E	/* - | W */
 #define ME_COUNTER_STARTDATA_A		0x0020	/* - | W */
@@ -333,7 +330,7 @@ static int me_ao_insn_write(struct comedi_device *dev,
 	for (i = 0; i < insn->n; i++) {
 		val = data[i];
 
-		writew(val, dev->mmio + ME_DAC_DATA_A + (chan << 1));
+		writew(val, dev->mmio + ME_AO_DATA_REG(chan));
 	}
 	s->readback[chan] = val;
 
