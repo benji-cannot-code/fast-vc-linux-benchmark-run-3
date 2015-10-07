eb3be1ab203072d9a70eb7a98b1caf01232ccd61
Geert Uytterhoeven <geert+renesas@glider.be>
2015-10-07 12:42:22 +0200

    ASoC: rsnd: Remove obsolete platform data support
    Since commit 3d7608e4c169af03 ("ARM: shmobile: bockw: remove legacy
    board file and config"), Renesas R-Car SoCs are only supported in
    generic DT-only ARM multi-platform builds.  The driver doesn't need to
    use platform data anymore, hence remove platform data configuration.
    
    Move <sound/rcar_snd.h> to sound/soc/sh/rcar/, as it's no longer needed
    by platform code.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rcar/rsnd.h
+++ sound/soc/sh/rcar/rsnd.h
@@ -22,9 +22,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/sh_dma.h>
 #include <linux/workqueue.h>
-#include <sound/rcar_snd.h>
 #include <sound/soc.h>
 #include <sound/pcm_params.h>
+
+#include "rcar_snd.h"
 
 /*
  *	pseudo register
