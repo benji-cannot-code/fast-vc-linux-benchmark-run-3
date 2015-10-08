e49b8633fd3859c907c367ee8e70f395980e30c7
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2015-10-09 03:10:15 +0530

    staging: sm750fb: Remove space after cast
    Remove unnecessary space after cast. Problem found using checkpatch.pl
    CHECK: No space is necessary after a cast
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_chip.c
+++ drivers/staging/sm750fb/ddk750_chip.c
@@ -134,7 +134,7 @@ static void setMemoryClock(unsigned int frequency)
 			frequency = MHz(336);
 
 		/* Calculate the divisor */
-		divisor = (unsigned int) roundedDiv(getChipClock(), frequency);
+		divisor = (unsigned int)roundedDiv(getChipClock(), frequency);
 
 		/* Set the corresponding divisor in the register. */
 		ulReg = PEEK32(CURRENT_GATE);
@@ -181,7 +181,7 @@ static void setMasterClock(unsigned int frequency)
 			frequency = MHz(190);
 
 		/* Calculate the divisor */
-		divisor = (unsigned int) roundedDiv(getChipClock(), frequency);
+		divisor = (unsigned int)roundedDiv(getChipClock(), frequency);
 
 		/* Set the corresponding divisor in the register. */
 		ulReg = PEEK32(CURRENT_GATE);
