78168e232f6589b7b14433a3524a02a0a5466cb0
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2015-10-08 14:34:05 +0900

    thermal: exynos: Remove unneeded semicolon
    Remove semicolons after switch statement.
    
    Acked-by: Lukasz Majewski <l.majewski@samsung.com>
    Tested-by: Lukasz Majewski <l.majewski@samsung.com>
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/samsung/exynos_tmu.c
+++ drivers/thermal/samsung/exynos_tmu.c
@@ -549,7 +549,7 @@ static int exynos5433_tmu_initialize(struct platform_device *pdev)
 	default:
 		pdata->cal_type = TYPE_ONE_POINT_TRIMMING;
 		break;
-	};
+	}
 
 	dev_info(&pdev->dev, "Calibration type is %d-point calibration\n",
 			cal_type ?  2 : 1);
@@ -1357,7 +1357,7 @@ static int exynos_tmu_probe(struct platform_device *pdev)
 		break;
 	default:
 		break;
-	};
+	}
 
 	/*
 	 * data->tzd must be registered before calling exynos_tmu_initialize(),
