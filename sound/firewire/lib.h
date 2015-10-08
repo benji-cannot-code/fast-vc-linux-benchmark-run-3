2e2aedabb53c6d904de4e09843e84a7d73fce3dd
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-10-09 08:10:29 +0900

    ALSA: firewire-lib: avoid endless loop to transfer MIDI messages at fatal error
    Currently, when asynchronous transactions finish in error state and
    retries, work scheduling and work running also continues. This
    should be canceled at fatal error because it can cause endless loop.
    
    This commit enables to cancel transferring MIDI messages when transactions
    encounter fatal errors. This is achieved by setting error state.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/lib.h
+++ sound/firewire/lib.h
@@ -33,6 +33,7 @@ struct snd_fw_async_midi_port {
 	struct work_struct work;
 	bool idling;
 	ktime_t next_ktime;
+	bool error;
 
 	u64 addr;
 	struct fw_transaction transaction;
@@ -59,8 +60,10 @@ static inline void
 snd_fw_async_midi_port_run(struct snd_fw_async_midi_port *port,
 			   struct snd_rawmidi_substream *substream)
 {
-	port->substream = substream;
-	schedule_work(&port->work);
+	if (!port->error) {
+		port->substream = substream;
+		schedule_work(&port->work);
+	}
 }
 
 /**
@@ -71,6 +74,7 @@ static inline void
 snd_fw_async_midi_port_finish(struct snd_fw_async_midi_port *port)
 {
 	port->substream = NULL;
+	port->error = false;
 }
 
 #endif
