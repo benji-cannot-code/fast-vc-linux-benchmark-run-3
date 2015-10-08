7914a427c55d11cc24857423333e9d014c0d9738
Ivan Safonov <insafonov@gmail.com>
2015-10-08 13:40:11 +0700

    staging: rtl8188eu: unused macro read_next_pair removed
    Unused macro read_next_pair removed from rtl8188eu driver.
    
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/bb_cfg.c
+++ drivers/staging/rtl8188eu/hal/bb_cfg.c
@@ -23,14 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <phy.h>
 
-#define read_next_pair(array, v1, v2, i)		\
-	 do {						\
-		i += 2;					\
-		v1 = array[i];				\
-		v2 = array[i+1];			\
-	 } while (0)
-
-
 /* AGC_TAB_1T.TXT */
 
 static u32 array_agc_tab_1t_8188e[] = {
