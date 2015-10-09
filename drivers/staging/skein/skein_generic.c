2838dbf8e4e24c6e484d4e43423324c567f46056
Aybuke Ozdemir <aybuke.147@gmail.com>
2015-10-09 16:31:37 +0300

    Staging: skein: Remove multiple blank lines
    Remove multiple blank lines. Problem found using checkpatch.pl
    CHECK: Please don't use multiple blank lines.
    
    Signed-off-by: Aybuke Ozdemir <aybuke.147@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/skein/skein_generic.c
+++ drivers/staging/skein/skein_generic.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/internal/hash.h>
 #include "skein_base.h"
 
-
 static int skein256_init(struct shash_desc *desc)
 {
 	return skein_256_init((struct skein_256_ctx *) shash_desc_ctx(desc),
