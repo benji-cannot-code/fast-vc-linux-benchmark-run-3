d4cf0d1c8b09c7d17a405d0a938f898030a73b9f
Xinliang Liu <xinliang.liu@linaro.org>
2015-10-09 18:00:56 +0800

    drm/hisilicon: Add hisilicon kirin drm master driver
    Add kirin DRM master driver for hi6220 SoC which used in HiKey board.
    Add dumb buffer feature.
    Add prime dmabuf feature.
    
    v9: Add OF and ARM64 depends on in Kconfig
    v8: None.
    v7:
    - Add config.mutex protection when accessing mode_config.connector_list.
    - Clean up match data getting.
    v6: None.
    v5: None.
    v4: None.
    v3:
    - Move and rename all the files to kirin sub-directory.
      So that we could separate different seires SoCs' driver.
    - Replace drm_platform_init, load, unload implementation.
    v2:
    - Remove abtraction layer.
    
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>
    Signed-off-by: Xinwei Kong <kong.kongxinwei@hisilicon.com>

---
+++ drivers/gpu/drm/hisilicon/kirin/kirin_drm_drv.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (c) 2016 Linaro Limited.
+ * Copyright (c) 2014-2016 Hisilicon Limited.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#ifndef __KIRIN_DRM_DRV_H__
+#define __KIRIN_DRM_DRV_H__
+
+/* display controller init/cleanup ops */
+struct kirin_dc_ops {
+	int (*init)(struct drm_device *dev);
+	void (*cleanup)(struct drm_device *dev);
+};
+
+#endif /* __KIRIN_DRM_DRV_H__ */
