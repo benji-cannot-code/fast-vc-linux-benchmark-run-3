b9fa5dbecd491ac8f2cc3e09eaf6c1ed72c30637
huangdaode <huangdaode@hisilicon.com>
2015-10-10 17:20:38 +0800

    net: hns: fix the unknown phy_nterface_t type error
    This patch fix the building error reported by Jiri Pirko <jiri@resnulli.us>
    
    drivers/net/ethernet/hisilicon/hns/hnae.h:465:2: error: unknown type
    name 'phy_interface_t'
            phy_interface_t phy_if;
    	^
    the full build log is on https://lists.01.org/pipermail/kbuild-all.
    
    Signed-off-by: huangdaode <huangdaode@hisilicon.com>
    Signed-off-by: yankejian <yankejian@huawei.com>
    Reviewed-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns/hnae.h
+++ drivers/net/ethernet/hisilicon/hns/hnae.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <linux/notifier.h>
+#include <linux/phy.h>
 #include <linux/types.h>
 
 #define HNAE_DRIVER_VERSION "1.3.0"
