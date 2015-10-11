2b318ce20f622caeed895e8b9bd6228f01780cc2
Reto Schneider <code@reto-schneider.ch>
2015-10-12 01:44:51 +0200

    iio: adis16203: Fix copyright year
    The copyright year can not be in the future.
    
    Signed-off-by: Reto Schneider <code@reto-schneider.ch>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/accel/adis16203_core.c
+++ drivers/staging/iio/accel/adis16203_core.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * ADIS16203 Programmable 360 Degrees Inclinometer
  *
- * Copyright 2030 Analog Devices Inc.
+ * Copyright 2010 Analog Devices Inc.
  *
  * Licensed under the GPL-2 or later.
  */
