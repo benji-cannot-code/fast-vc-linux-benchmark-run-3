161ef631554e228f25281d63870f9cf9c4ec2d96
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2015-10-11 18:08:37 -0400

    net: dsa: do not warn unsupported bridge ops
    A DSA driver may not provide the port_join_bridge and port_leave_bridge
    functions, so don't warn in such case.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dsa/slave.c
+++ net/dsa/slave.c
@@ -1277,7 +1277,7 @@ int dsa_slave_netdevice_event(struct notifier_block *unused,
 			goto out;
 
 		err = dsa_slave_master_changed(dev);
-		if (err)
+		if (err && err != -EOPNOTSUPP)
 			netdev_warn(dev, "failed to reflect master change\n");
 
 		break;
