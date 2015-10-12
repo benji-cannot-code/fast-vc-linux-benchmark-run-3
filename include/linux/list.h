a4aea37feafdec7bf9d547706ac5d257500bb601
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-10-12 16:56:42 -0700

    list: Use WRITE_ONCE() when initializing list_head structures
    Code that does lockless emptiness testing of non-RCU lists is relying
    on INIT_LIST_HEAD() to write the list head's ->next pointer atomically,
    particularly when INIT_LIST_HEAD() is invoked from list_del_init().
    This commit therefore adds WRITE_ONCE() to this function's pointer stores
    that could affect the head's ->next pointer.
    
    Reported-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/list.h
+++ include/linux/list.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline void INIT_LIST_HEAD(struct list_head *list)
 {
-	list->next = list;
+	WRITE_ONCE(list->next, list);
 	list->prev = list;
 }
 
