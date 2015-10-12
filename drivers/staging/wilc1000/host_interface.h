a7145cb3a1b5b1f289111013bd966589c6b6c14f
Leo Kim <leo.kim@atmel.com>
2015-10-12 16:55:57 +0900

    staging: wilc1000: remove unused struct cfg_param_t
    This patch removes unused the struct cfg_param_t.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.h
+++ drivers/staging/wilc1000/host_interface.h
@@ -58,12 +58,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SCAN_EVENT_DONE_ABORTED
 #define NUM_CONCURRENT_IFC			2
 
-typedef struct {
-	u16 cfg_wid;
-	enum WID_TYPE cfg_type;
-	s8     *pu8Para;
-} cfg_param_t;
-
 typedef struct _tstrStatistics {
 	u8 u8LinkSpeed;
 	s8 s8RSSI;
