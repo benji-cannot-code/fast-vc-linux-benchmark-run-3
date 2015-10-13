9ab0bd9a168e17960ee4f3d0a904b6b03533fd97
Lars-Peter Clausen <lars@metafoo.de>
2015-10-13 18:10:24 +0200

    iio: Set device watermark based on watermark of all attached buffers
    Currently the watermark of the device is only set based on the watermark
    that is set for the user space buffer. This doesn't consider the watermarks
    set on any attached in-kernel buffers.
    
    Change this so that the watermark of the device should be the minimum of
    the watermarks over all attached buffers.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/industrialio-buffer.c
+++ drivers/iio/industrialio-buffer.c
@@ -611,6 +611,7 @@ static void iio_free_scan_mask(struct iio_dev *indio_dev,
 
 struct iio_device_config {
 	unsigned int mode;
+	unsigned int watermark;
 	const unsigned long *scan_mask;
 	unsigned int scan_bytes;
 	bool scan_timestamp;
@@ -643,10 +644,14 @@ static int iio_verify_update(struct iio_dev *indio_dev,
 		if (buffer == remove_buffer)
 			continue;
 		modes &= buffer->access->modes;
+		config->watermark = min(config->watermark, buffer->watermark);
 	}
 
-	if (insert_buffer)
+	if (insert_buffer) {
 		modes &= insert_buffer->access->modes;
+		config->watermark = min(config->watermark,
+			insert_buffer->watermark);
+	}
 
 	/* Definitely possible for devices to support both of these. */
 	if ((modes & INDIO_BUFFER_TRIGGERED) && indio_dev->trig) {
@@ -743,6 +748,10 @@ static int iio_enable_buffers(struct iio_dev *indio_dev,
 			goto err_run_postdisable;
 		}
 	}
+
+	if (indio_dev->info->hwfifo_set_watermark)
+		indio_dev->info->hwfifo_set_watermark(indio_dev,
+			config->watermark);
 
 	indio_dev->currentmode = config->mode;
 
@@ -975,9 +984,6 @@ static ssize_t iio_buffer_store_watermark(struct device *dev,
 	}
 
 	buffer->watermark = val;
-
-	if (indio_dev->info->hwfifo_set_watermark)
-		indio_dev->info->hwfifo_set_watermark(indio_dev, val);
 out:
 	mutex_unlock(&indio_dev->mlock);
 
