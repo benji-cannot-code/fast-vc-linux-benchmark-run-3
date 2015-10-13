640824c8d69d344e4be394b583a2232a18b0c88a
Leo Kim <leo.kim@atmel.com>
2015-10-13 19:49:59 +0900

    staging: wilc1000: rename pJoinParams of struct connect_attr
    This patch renames pJoinParams of struct connect_attr to params to
    avoid CamelCase naming convention.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -119,7 +119,7 @@ struct connect_attr {
 	void *arg;
 	enum AUTHTYPE auth_type;
 	u8 ch;
-	void *pJoinParams;
+	void *params;
 };
 
 struct rcvd_async_info {
@@ -1025,7 +1025,7 @@ static s32 Handle_Connect(struct host_if_drv *hif_drv,
 
 	PRINT_INFO(HOSTINF_DBG, "Saving connection parameters in global structure\n");
 
-	ptstrJoinBssParam = (struct join_bss_param *)pstrHostIFconnectAttr->pJoinParams;
+	ptstrJoinBssParam = (struct join_bss_param *)pstrHostIFconnectAttr->params;
 	if (ptstrJoinBssParam == NULL) {
 		PRINT_ER("Required BSSID not found\n");
 		s32Error = -ENOENT;
@@ -3617,7 +3617,7 @@ s32 host_int_set_join_req(struct host_if_drv *hif_drv, u8 *pu8bssid,
 	msg.body.con_info.ch = u8channel;
 	msg.body.con_info.result = pfConnectResult;
 	msg.body.con_info.arg = pvUserArg;
-	msg.body.con_info.pJoinParams = pJoinParams;
+	msg.body.con_info.params = pJoinParams;
 	msg.drv = hif_drv ;
 
 	if (pu8bssid != NULL) {
