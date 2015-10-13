9205f986d9c93101c3ca511ca197d76b6f3fe1f7
Shraddha Barke <shraddha.6596@gmail.com>
2015-10-13 22:20:28 +0530

    Staging: lustre: lov: Remove unused #include header file
    Remove lclient.h header since it is not used.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_object.c
+++ drivers/staging/lustre/lustre/lov/lov_object.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUG_SUBSYSTEM S_LOV
 
 #include "lov_cl_internal.h"
-#include "../include/lclient.h"
 
 /** \addtogroup lov
  *  @{
