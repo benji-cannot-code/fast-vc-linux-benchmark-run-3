defe4959b4d85c4d991ccea6c596618b92c30560
Antoine Ténart <antoine.tenart@free-electrons.com>
2015-10-13 23:31:41 +0200

    pinctrl: berlin: fix my family name spelling
    My family name contained an accent when I submitted the
    Berlin pinctrl series in the first place. There was an
    encoding issue when the series was applied. Fix this.
    
    Signed-off-by: Antoine Ténart <antoine.tenart@free-electrons.com>
    Acked-by: Sebastian Hesselbarth <sebastian.hesselbarth@gmail.com>
    [Je me rendis tous les accents aigus]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/berlin/berlin.c
+++ drivers/pinctrl/berlin/berlin.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2014 Marvell Technology Group Ltd.
  *
- * Antoine TÃ©nart <antoine.tenart@free-electrons.com>
+ * Antoine Ténart <antoine.tenart@free-electrons.com>
  *
  * This file is licensed under the terms of the GNU General Public
  * License version 2. This program is licensed "as is" without any
