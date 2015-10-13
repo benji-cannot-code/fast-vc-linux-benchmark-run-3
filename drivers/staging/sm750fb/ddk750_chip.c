e0484e1dbb16e2fe77601750bb0617eb226a30d8
Mike Rapoport <mike.rapoport@gmail.com>
2015-10-13 09:26:45 +0300

    staging: sm750fb: rename getChipClock to get_mxclk_freq
    The getChipClock is used to detect MXCLK frequency. Make it's name
    reflect what the function is actually doing.
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_chip.c
+++ drivers/staging/sm750fb/ddk750_chip.c
@@ -35,7 +35,7 @@ logical_chip_type_t getChipType(void)
 	return chip;
 }
 
-static unsigned int getChipClock(void)
+static unsigned int get_mxclk_freq(void)
 {
 	unsigned int pll_reg;
 	unsigned int M, N, OD, POD;
@@ -101,7 +101,7 @@ static void setMemoryClock(unsigned int frequency)
 			frequency = MHz(336);
 
 		/* Calculate the divisor */
-		divisor = (unsigned int)roundedDiv(getChipClock(), frequency);
+		divisor = (unsigned int)roundedDiv(get_mxclk_freq(), frequency);
 
 		/* Set the corresponding divisor in the register. */
 		ulReg = PEEK32(CURRENT_GATE);
@@ -148,7 +148,7 @@ static void setMasterClock(unsigned int frequency)
 			frequency = MHz(190);
 
 		/* Calculate the divisor */
-		divisor = (unsigned int)roundedDiv(getChipClock(), frequency);
+		divisor = (unsigned int)roundedDiv(get_mxclk_freq(), frequency);
 
 		/* Set the corresponding divisor in the register. */
 		ulReg = PEEK32(CURRENT_GATE);
