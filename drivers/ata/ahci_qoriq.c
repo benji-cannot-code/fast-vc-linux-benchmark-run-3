6199074e435aa94f8f7be02183a1a96b32717665
Arnd Bergmann <arnd@arndb.de>
2015-10-14 16:46:52 +0800

    ahci: qoriq: Fix a compiling warning
    kbuild test robot reports the warnings:
    drivers/ata/ahci_qoriq.c: In function 'ahci_qoriq_hardreset':
    >> include/asm-generic/io.h:163:2: warning: 'px_is' may be used
    >> uninitialized in this function [-Wuninitialized]
    drivers/ata/ahci_qoriq.c:70:14: note: 'px_is' was declared here
    >> include/asm-generic/io.h:163:2: warning: 'px_cmd' may be used
    >> uninitialized in this function [-Wuninitialized]
    drivers/ata/ahci_qoriq.c:70:6: note: 'px_cmd' was declared here
    
    This patch fixed it by introducing a local variable.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Tang Yuantian <Yuantian.Tang@freescale.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/ahci_qoriq.c
+++ drivers/ata/ahci_qoriq.c
@@ -77,6 +77,7 @@ static int ahci_qoriq_hardreset(struct ata_link *link, unsigned int *class,
 	struct ata_taskfile tf;
 	bool online;
 	int rc;
+	bool ls1021a_workaround = (qoriq_priv->type == AHCI_LS1021A);
 
 	DPRINTK("ENTER\n");
 
@@ -93,7 +94,7 @@ static int ahci_qoriq_hardreset(struct ata_link *link, unsigned int *class,
 	 * After the sequence is complete, software should restore the
 	 * PxCMD and PxIS with the stored values.
 	 */
-	if (qoriq_priv->type == AHCI_LS1021A) {
+	if (ls1021a_workaround) {
 		px_cmd = readl(port_mmio + PORT_CMD);
 		px_is = readl(port_mmio + PORT_IRQ_STAT);
 	}
@@ -107,7 +108,7 @@ static int ahci_qoriq_hardreset(struct ata_link *link, unsigned int *class,
 				 ahci_check_ready);
 
 	/* restore the PxCMD and PxIS on ls1021 */
-	if (qoriq_priv->type == AHCI_LS1021A) {
+	if (ls1021a_workaround) {
 		px_val = readl(port_mmio + PORT_CMD);
 		if (px_val != px_cmd)
 			writel(px_cmd, port_mmio + PORT_CMD);
