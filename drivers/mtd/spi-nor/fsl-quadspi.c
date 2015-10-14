7ae6b12a397d135c6b8de0ebd448bd73b0a1bbe4
Fabio Estevam <fabio.estevam@freescale.com>
2015-10-14 00:39:44 -0300

    mtd: fsl-quadspi: Include <linux/sizes.h> to avoid build error
    Building for x86 results in the following build errors:
    
       drivers/mtd/spi-nor/fsl-quadspi.c: In function 'fsl_qspi_init_lut':
    >> drivers/mtd/spi-nor/fsl-quadspi.c:355:21: error: 'SZ_16M' undeclared (first use in this function)
         if (q->nor_size <= SZ_16M) {
                            ^
       drivers/mtd/spi-nor/fsl-quadspi.c:355:21: note: each undeclared identifier is reported only once for each function it appears in
       drivers/mtd/spi-nor/fsl-quadspi.c: In function 'fsl_qspi_read':
    >> drivers/mtd/spi-nor/fsl-quadspi.c:208:27: error: 'SZ_4M' undeclared (first use in this function)
        #define QUADSPI_MIN_IOMAP SZ_4M
                                  ^
    >> drivers/mtd/spi-nor/fsl-quadspi.c:845:25: note: in expansion of macro 'QUADSPI_MIN_IOMAP'
          q->memmap_len = len > QUADSPI_MIN_IOMAP ? len : QUADSPI_MIN_IOMAP;
    
    Explicitly include <linux/sizes.h> to fix the problem.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/spi-nor/fsl-quadspi.c
+++ drivers/mtd/spi-nor/fsl-quadspi.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/spi-nor.h>
 #include <linux/mutex.h>
 #include <linux/pm_qos.h>
+#include <linux/sizes.h>
 
 /* Controller needs driver to swap endian */
 #define QUADSPI_QUIRK_SWAP_ENDIAN	(1 << 0)
