336be1aa0c6d35eaf6a64468034208390bc23815
Mikko Rapeli <mikko.rapeli@iki.fi>
2015-10-15 07:56:48 +0200

    mtd: mtd-user: remove stdint.h include
    Kernel headers should use linux/types.h instead.
    
    Signed-off-by: Mikko Rapeli <mikko.rapeli@iki.fi>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- include/uapi/mtd/mtd-user.h
+++ include/uapi/mtd/mtd-user.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MTD_USER_H__
 #define __MTD_USER_H__
 
-#include <stdint.h>
-
 /* This file is blessed for inclusion by userspace */
 #include <mtd/mtd-abi.h>
 
