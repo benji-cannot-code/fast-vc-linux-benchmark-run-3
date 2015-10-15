e37f110262c35327ae67921e3c1c217a232b652c
Leo Kim <leo.kim@atmel.com>
2015-10-15 13:25:01 +0900

    staging: wilc1000: rename variable gs8Rssi
    This patch renames variable gs8Rssi to rssi
    to avoid CamelCase naming convention.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Tony Cho <tony.cho@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -248,7 +248,7 @@ static u8 rcv_assoc_resp[MAX_ASSOC_RESP_FRAME_SIZE];
 
 static bool scan_while_connected;
 
-static s8 gs8Rssi;
+static s8 rssi;
 static s8 gs8lnkspd;
 static u8 gu8Chnl;
 static u8 gs8SetIP[2][4];
@@ -2136,7 +2136,7 @@ static void Handle_GetRssi(struct host_if_drv *hif_drv)
 
 	strWID.id = (u16)WID_RSSI;
 	strWID.type = WID_CHAR;
-	strWID.val = &gs8Rssi;
+	strWID.val = &rssi;
 	strWID.size = sizeof(char);
 
 	PRINT_D(HOSTINF_DBG, "Getting RSSI value\n");
@@ -3910,9 +3910,7 @@ s32 host_int_get_rssi(struct host_if_drv *hif_drv, s8 *ps8Rssi)
 		return -EFAULT;
 	}
 
-
-	*ps8Rssi = gs8Rssi;
-
+	*ps8Rssi = rssi;
 
 	return s32Error;
 }
