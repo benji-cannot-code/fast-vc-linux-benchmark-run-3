e23a9f79a786c63a54a78aa2413c0b3ec716af80
Sebastian Reichel <sre@kernel.org>
2015-10-16 15:44:11 +0200

    power: bq27xxx_battery: move irq handler to i2c section
    The IRQ handler is not used by the platform based
    code resulting in a 'defined but not used' warning,
    if CONFIG_BQ27XXX_I2C is not enabled.
    
    Signed-off-by: Sebastian Reichel <sre@kernel.org>
    Acked-by: Pali Rohár <pali.rohar@gmail.com>
    Acked-by: Andrew F. Davis <afd@ti.com>

--- drivers/power/bq27xxx_battery.c
+++ drivers/power/bq27xxx_battery.c
@@ -763,15 +763,6 @@ static void bq27xxx_battery_update(struct bq27xxx_device_info *di)
 	di->last_update = jiffies;
 }
 
-static irqreturn_t bq27xxx_battery_irq_handler_thread(int irq, void *data)
-{
-	struct bq27xxx_device_info *di = data;
-
-	bq27xxx_battery_update(di);
-
-	return IRQ_HANDLED;
-}
-
 static void bq27xxx_battery_poll(struct work_struct *work)
 {
 	struct bq27xxx_device_info *di =
@@ -1061,6 +1052,15 @@ static void bq27xxx_powersupply_unregister(struct bq27xxx_device_info *di)
  */
 static DEFINE_IDR(battery_id);
 static DEFINE_MUTEX(battery_mutex);
+
+static irqreturn_t bq27xxx_battery_irq_handler_thread(int irq, void *data)
+{
+	struct bq27xxx_device_info *di = data;
+
+	bq27xxx_battery_update(di);
+
+	return IRQ_HANDLED;
+}
 
 static int bq27xxx_battery_i2c_read(struct bq27xxx_device_info *di, u8 reg,
 				    bool single)
