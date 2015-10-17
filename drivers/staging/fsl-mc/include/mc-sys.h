8a4c29d863263dc79d3924ec13f7167c299abfd5
J. German Rivera <German.Rivera@freescale.com>
2015-10-17 15:33:14 -0500

    staging: fsl-mc:Added support for atomic portals
    Refactored mc_send_command() to support two flavors of polling:
    - preemptible (for non-atomic portals), which was already supported.
      It calls usleep_range() between polling iterations.
    - non-preemptible (for atomic portals), which is needed when
      mc_send_command() is called with interrupts disabled.
      It calls udelay() between polling iterations.
    
    Signed-off-by: J. German Rivera <German.Rivera@freescale.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/include/mc-sys.h
+++ drivers/staging/fsl-mc/include/mc-sys.h
@@ -41,6 +41,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/dma-mapping.h>
 
+/**
+ * Bit masks for a MC I/O object (struct fsl_mc_io) flags
+ */
+#define FSL_MC_IO_ATOMIC_CONTEXT_PORTAL	0x0001
+
 struct fsl_mc_resource;
 struct mc_command;
 
