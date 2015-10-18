b642aef5d533f4b7143e0324b2853c3acc490637
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2015-10-18 21:27:57 +0530

    staging: sm750fb: Replace uint32_t with u32
    Replace uint32_t with u32 consistent with Linux kernel coding
    practice.
    Problem found using checkpatch.pl
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_power.c
+++ drivers/staging/sm750fb/ddk750_power.c
@@ -109,7 +109,7 @@ void setCurrentGate(unsigned int gate)
  */
 void enable2DEngine(unsigned int enable)
 {
-	uint32_t gate;
+	u32 gate;
 
 	gate = PEEK32(CURRENT_GATE);
 	if (enable) {
@@ -125,7 +125,7 @@ void enable2DEngine(unsigned int enable)
 
 void enableDMA(unsigned int enable)
 {
-	uint32_t gate;
+	u32 gate;
 
 	/* Enable DMA Gate */
 	gate = PEEK32(CURRENT_GATE);
@@ -142,7 +142,7 @@ void enableDMA(unsigned int enable)
  */
 void enableGPIO(unsigned int enable)
 {
-	uint32_t gate;
+	u32 gate;
 
 	/* Enable GPIO Gate */
 	gate = PEEK32(CURRENT_GATE);
@@ -159,7 +159,7 @@ void enableGPIO(unsigned int enable)
  */
 void enableI2C(unsigned int enable)
 {
-	uint32_t gate;
+	u32 gate;
 
 	/* Enable I2C Gate */
 	gate = PEEK32(CURRENT_GATE);
