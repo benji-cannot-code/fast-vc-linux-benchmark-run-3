d347495ee91e2c8e1ea316e6926773d630b8c192
Namhyung Kim <namhyung@kernel.org>
2015-10-20 00:23:49 +0900

    perf test: Suppress libtraceevent warnings
    Currently libtraceevent emits warning on unsupported event formats.
    However it'd be better to see them only -v option is given.  To do that,
    it needs to override the warning() function which is used in the
    libtracevent.  Thus add set_warning_routine() same as set_die_routine()
    and check the verbose flag in our warning routine.
    
    Before:
      # perf test 5
       5: parse events tests                                       :
        Warning: [kvmmmu:kvm_mmu_get_page] bad op token {
        Warning: [kvmmmu:kvm_mmu_sync_page] bad op token {
        Warning: [kvmmmu:kvm_mmu_unsync_page] bad op token {
        Warning: [kvmmmu:kvm_mmu_prepare_zap_page] bad op token {
        Warning: [kvmmmu:fast_page_fault] function is_writable_pte not defined
        ...
       Ok
    
    After:
      # perf test 5
       5: parse events tests                                       : Ok
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Acked-by: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/r/1445268229-1601-2-git-send-email-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/parse-events.c
+++ tools/perf/tests/parse-events.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <api/fs/fs.h>
 #include "tests.h"
 #include "debug.h"
+#include "util.h"
 #include <linux/hw_breakpoint.h>
 #include <api/fs/fs.h>
 
@@ -1754,6 +1755,17 @@ static int test_pmu_events(void)
 	return ret;
 }
 
+static void debug_warn(const char *warn, va_list params)
+{
+	char msg[1024];
+
+	if (!verbose)
+		return;
+
+	vsnprintf(msg, sizeof(msg), warn, params);
+	fprintf(stderr, " Warning: %s\n", msg);
+}
+
 int test__parse_events(void)
 {
 	int ret1, ret2 = 0;
@@ -1764,6 +1776,8 @@ do {							\
 	if (!ret2)					\
 		ret2 = ret1;				\
 } while (0)
+
+	set_warning_routine(debug_warn);
 
 	TEST_EVENTS(test__events);
 
