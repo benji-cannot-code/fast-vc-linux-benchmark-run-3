b6e688f238b25ded3afe9f57d12881c137a40d91
Arnaldo Carvalho de Melo <acme@redhat.com>
2015-10-19 18:17:25 -0300

    perf bench: Use named initializers in the trailer too
    To avoid this splat with gcc 4.4.7:
    
      cc1: warnings being treated as errors
      bench/mem-functions.c:273: error: missing initializer
      bench/mem-functions.c:273: error: (near initialization for ‘memcpy_functions[4].desc’)
      bench/mem-functions.c:366: error: missing initializer
      bench/mem-functions.c:366: error: (near initialization for ‘memset_functions[4].desc’)
    
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Hitoshi Mitake <mitake@dcl.info.waseda.ac.jp>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/n/tip-0s8o6tgw1pdwvdv02llb9tkd@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/bench/mem-functions.c
+++ tools/perf/bench/mem-functions.c
@@ -271,7 +271,7 @@ struct function memcpy_functions[] = {
 # undef MEMCPY_FN
 #endif
 
-	{ NULL, }
+	{ .name = NULL, }
 };
 
 static const char * const bench_mem_memcpy_usage[] = {
@@ -364,7 +364,7 @@ static const struct function memset_functions[] = {
 # undef MEMSET_FN
 #endif
 
-	{ NULL, }
+	{ .name = NULL, }
 };
 
 int bench_mem_memset(int argc, const char **argv, const char *prefix __maybe_unused)
