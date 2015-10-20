e4e4ce720b0fa8d20def3e27cc388d8a0be3c5c6
Mike Rapoport <mike.rapoport@gmail.com>
2015-10-20 12:39:45 +0300

    staging: lustre: replace OBDO_ALLOC with kmem_cache_alloc
    Use direct call to kmem_cache_alloc instead of wrapping macro
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_request.c
+++ drivers/staging/lustre/lustre/lov/lov_request.c
@@ -203,7 +203,7 @@ static int common_attr_done(struct lov_request_set *set)
 	if (!atomic_read(&set->set_success))
 		return -EIO;
 
-	OBDO_ALLOC(tmp_oa);
+	tmp_oa = kmem_cache_alloc(obdo_cachep, GFP_NOFS | __GFP_ZERO);
 	if (tmp_oa == NULL) {
 		rc = -ENOMEM;
 		goto out;
@@ -310,7 +310,8 @@ int lov_prep_getattr_set(struct obd_export *exp, struct obd_info *oinfo,
 		req->rq_stripe = i;
 		req->rq_idx = loi->loi_ost_idx;
 
-		OBDO_ALLOC(req->rq_oi.oi_oa);
+		req->rq_oi.oi_oa = kmem_cache_alloc(obdo_cachep,
+						    GFP_NOFS | __GFP_ZERO);
 		if (req->rq_oi.oi_oa == NULL) {
 			kfree(req);
 			rc = -ENOMEM;
@@ -392,7 +393,8 @@ int lov_prep_destroy_set(struct obd_export *exp, struct obd_info *oinfo,
 		req->rq_stripe = i;
 		req->rq_idx = loi->loi_ost_idx;
 
-		OBDO_ALLOC(req->rq_oi.oi_oa);
+		req->rq_oi.oi_oa = kmem_cache_alloc(obdo_cachep,
+						    GFP_NOFS | __GFP_ZERO);
 		if (req->rq_oi.oi_oa == NULL) {
 			kfree(req);
 			rc = -ENOMEM;
@@ -507,7 +509,8 @@ int lov_prep_setattr_set(struct obd_export *exp, struct obd_info *oinfo,
 		req->rq_stripe = i;
 		req->rq_idx = loi->loi_ost_idx;
 
-		OBDO_ALLOC(req->rq_oi.oi_oa);
+		req->rq_oi.oi_oa = kmem_cache_alloc(obdo_cachep,
+						    GFP_NOFS | __GFP_ZERO);
 		if (req->rq_oi.oi_oa == NULL) {
 			kfree(req);
 			rc = -ENOMEM;
