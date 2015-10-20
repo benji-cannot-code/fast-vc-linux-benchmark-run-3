47ef647a3d1e618cbff9cac2919278bcd8d15553
Liviu Dudau <Liviu.Dudau@arm.com>
2015-10-20 10:23:12 +0100

    drm: Introduce generic probe function for component based masters.
    A lot of component based DRM drivers use a variant of the same code
    as the probe function. They bind the crtc ports in the first iteration
    and then scan through the child nodes and bind the encoders attached
    to the remote endpoints. Factor the common code into a separate
    function called drm_of_component_probe() in order to increase code
    reuse.
    
    Cc: David Airlie <airlied@linux.ie>
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/1445332995-11212-2-git-send-email-Liviu.Dudau@arm.com
    Acked-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/drm/drm_of.h
+++ include/drm/drm_of.h
@@ -2,17 +2,30 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DRM_OF_H__
 #define __DRM_OF_H__
 
+struct component_master_ops;
+struct device;
 struct drm_device;
 struct device_node;
 
 #ifdef CONFIG_OF
 extern uint32_t drm_of_find_possible_crtcs(struct drm_device *dev,
 					   struct device_node *port);
+extern int drm_of_component_probe(struct device *dev,
+				  int (*compare_of)(struct device *, void *),
+				  const struct component_master_ops *m_ops);
 #else
 static inline uint32_t drm_of_find_possible_crtcs(struct drm_device *dev,
 						  struct device_node *port)
 {
 	return 0;
+}
+
+static inline int
+drm_of_component_probe(struct device *dev,
+		       int (*compare_of)(struct device *, void *),
+		       const struct component_master_ops *m_ops)
+{
+	return -EINVAL;
 }
 #endif
 
