cb1acd959f701b9d2590d9935b4ee5e5d9b14a5e
Minghuan Lian <Minghuan.Lian@freescale.com>
2015-10-21 16:13:37 -0500

    PCI: layerscape: Remove unused fields from struct ls_pcie
    Removed unused node, dev, and bus fields from struct ls_pcie.
    
    [bhelgaas: split into separate patch]
    Signed-off-by: Minghuan Lian <Minghuan.Lian@freescale.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/host/pci-layerscape.c
+++ drivers/pci/host/pci-layerscape.c
@@ -40,9 +40,6 @@ struct ls_pcie_drvdata {
 };
 
 struct ls_pcie {
-	struct list_head node;
-	struct device *dev;
-	struct pci_bus *bus;
 	void __iomem *dbi;
 	struct regmap *scfg;
 	struct pcie_port pp;
