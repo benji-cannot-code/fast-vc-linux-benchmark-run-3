3d2662a0038e6c22d1898f4a6eaf891efba10915
Christian Gromm <christian.gromm@microchip.com>
2015-10-21 17:50:50 +0200

    staging: most: use preferred kzalloc parameters
    This patch uses the preferred call to kzalloc. It replaces
    kzalloc(sizeof(struct aim_fh)...) by kzalloc(sizeof(*fh)...).
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/aim-v4l2/video.c
+++ drivers/staging/most/aim-v4l2/video.c
@@ -89,7 +89,7 @@ static int aim_vdev_open(struct file *filp)
 		return -EINVAL;
 	}
 
-	fh = kzalloc(sizeof(struct aim_fh), GFP_KERNEL);
+	fh = kzalloc(sizeof(*fh), GFP_KERNEL);
 	if (!fh)
 		return -ENOMEM;
 
