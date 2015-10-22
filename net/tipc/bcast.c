8e29eb7a7132325e034124c8ad650785cb6646cd
Jon Paul Maloy <jon.maloy@ericsson.com>
2015-10-22 08:51:40 -0400

    tipc: introduce capability bit for broadcast synchronization
    Until now, we have tried to support both the newer, dedicated broadcast
    synchronization mechanism along with the older, less safe, RESET_MSG/
    ACTIVATE_MSG based one. The latter method has turned out to be a hazard
    in a highly dynamic cluster, so we find it safer to disable it completely
    when we find that the former mechanism is supported by the peer node.
    
    For this purpose, we now introduce a new capabability bit,
    TIPC_BCAST_SYNCH, to inform any peer nodes that dedicated broadcast
    syncronization is supported by the present node. The new bit is conveyed
    between peers in the 'capabilities' field of neighbor discovery messages.
    
    Signed-off-by: Jon Maloy <jon.maloy@ericsson.com>
    Reviewed-by: Ying Xue <ying.xue@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/bcast.c
+++ net/tipc/bcast.c
@@ -1046,6 +1046,7 @@ int tipc_bcast_init(struct net *net)
 	if (!tipc_link_bc_create(&bb->node,
 				 MAX_PKT_DEFAULT_MCAST,
 				 BCLINK_WIN_DEFAULT,
+				 0,
 				 &bb->inputq,
 				 &bb->namedq,
 				 &l))
