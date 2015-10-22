aa55ea0f169c781a454bbc3569cf2f808c750732
Eva Rachel Retuya <eraretuya@gmail.com>
2015-10-23 01:07:38 +0800

    Staging: mt29f_spinand: Add blank line after function declaration
    Add missing blank line in-between function declarations.
    Issue found by checkpatch.
    
    CHECK: Please use a blank line after function/struct/union/enum
    declarations
    
    Signed-off-by: Eva Rachel Retuya <eraretuya@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt29f_spinand/mt29f_spinand.c
+++ drivers/staging/mt29f_spinand/mt29f_spinand.c
@@ -187,6 +187,7 @@ static int wait_till_ready(struct spi_device *spi_nand)
 
 	return -1;
 }
+
 /**
  * spinand_get_otp- send command 0xf to read the SPI Nand OTP register
  * Description:
@@ -840,6 +841,7 @@ static int spinand_lock_block(struct spi_device *spi_nand, u8 lock)
 
 	return ret;
 }
+
 /*
  * spinand_probe - [spinand Interface]
  * @spi_nand: registered device driver.
