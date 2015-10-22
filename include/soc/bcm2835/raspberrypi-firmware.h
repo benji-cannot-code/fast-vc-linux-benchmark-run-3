ed9237212eaa3f4d8a898592642a1047bdc7548f
Alexander Aring <alex.aring@gmail.com>
2015-10-22 20:46:05 +0200

    ARM: bcm2835: add mutual inclusion protection
    This patch adds mutual inclusion protection for the rpi firmware header.
    
    Cc: Eric Anholt <eric@anholt.net>
    Cc: Stephen Warren <swarren@wwwdotorg.org>
    Signed-off-by: Alexander Aring <alex.aring@gmail.com>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

--- include/soc/bcm2835/raspberrypi-firmware.h
+++ include/soc/bcm2835/raspberrypi-firmware.h
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
+#ifndef __SOC_RASPBERRY_FIRMWARE_H__
+#define __SOC_RASPBERRY_FIRMWARE_H__
+
 #include <linux/types.h>
 #include <linux/of_device.h>
 
@@ -114,3 +117,5 @@ int rpi_firmware_property(struct rpi_firmware *fw,
 int rpi_firmware_property_list(struct rpi_firmware *fw,
 			       void *data, size_t tag_size);
 struct rpi_firmware *rpi_firmware_get(struct device_node *firmware_node);
+
+#endif /* __SOC_RASPBERRY_FIRMWARE_H__ */
