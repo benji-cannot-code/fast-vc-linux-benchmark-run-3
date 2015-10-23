6d86200bc97c32e28462690495e2fe3e60c70edd
Caesar Wang <wxt@rock-chips.com>
2015-10-23 19:25:27 +0800

    thermal: rockchip: support the sleep pinctrl state to avoid glitches in s2r
    When we come out of system suspend state (S3) the tsadc will have been
    reset and back at its default state.  While reprogramming the tsadc
    it's possible that we'll glitch the output and unintentionally cause
    the "over temperature" GPIO to be asserted.  Since the over
    temperature GPIO is often hooked up to something that will cause a
    reboot or shutdown in hardware, this glitch can be catastrophic on
    some boards.
    
    We'll add support for selecting the "sleep" pinctrl state at suspend
    time.  Boards can use this to effectively disable the tsadc at suspend
    time and avoid glitches when the system is resumed.
    
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/rockchip_thermal.c
+++ drivers/thermal/rockchip_thermal.c
@@ -639,6 +639,8 @@ static int __maybe_unused rockchip_thermal_suspend(struct device *dev)
 	clk_disable(thermal->pclk);
 	clk_disable(thermal->clk);
 
+	pinctrl_pm_select_sleep_state(dev);
+
 	return 0;
 }
 
@@ -674,6 +676,8 @@ static int __maybe_unused rockchip_thermal_resume(struct device *dev)
 
 	for (i = 0; i < ARRAY_SIZE(thermal->sensors); i++)
 		rockchip_thermal_toggle_sensor(&thermal->sensors[i], true);
+
+	pinctrl_pm_select_default_state(dev);
 
 	return 0;
 }
