92f4ead0dc025287efe7395b7d1ba097899b214d
Florian Westphal <fw@strlen.de>
2015-10-23 12:43:18 +0200

    netfilter: ingress: don't use nf_hook_list_active
    nf_hook_list_active() always returns true once at least one device has
    NF_INGRESS hook enabled.
    
    Thus, don't use this function. Instead, inverse the test and use the static
    key to elide list_empty test if no NF_INGRESS hooks are active.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/linux/netfilter_ingress.h
+++ include/linux/netfilter_ingress.h
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 
 #ifdef CONFIG_NETFILTER_INGRESS
-static inline int nf_hook_ingress_active(struct sk_buff *skb)
+static inline bool nf_hook_ingress_active(const struct sk_buff *skb)
 {
-	return nf_hook_list_active(&skb->dev->nf_hooks_ingress,
-				   NFPROTO_NETDEV, NF_NETDEV_INGRESS);
+#ifdef HAVE_JUMP_LABEL
+	if (!static_key_false(&nf_hooks_needed[NFPROTO_NETDEV][NF_NETDEV_INGRESS]))
+		return false;
+#endif
+	return !list_empty(&skb->dev->nf_hooks_ingress);
 }
 
 static inline int nf_hook_ingress(struct sk_buff *skb)
