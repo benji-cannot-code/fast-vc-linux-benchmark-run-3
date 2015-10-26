ce8f19affdc1cc31e5cf073587d24a464d829aea
Dean Luick <dean.luick@intel.com>
2015-10-26 10:28:43 -0400

    staging/rdma/hfi1: Add irqsaves in the packet processing path
    In preparation for threading the receive interrupt, add irqsaves in the packet
    processing path.
    
    When the receive interrupt is threaded, the packet processing path is no longer
    guaranteed to have IRQs disabled.  Add irqsaves where needed on several locks
    in the packet processing path.  Anything that did not have an obvious, "close"
    irqsave in its caller is a candidate.
    
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Dean Luick <dean.luick@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rdma/hfi1/init.c
+++ drivers/staging/rdma/hfi1/init.c
@@ -414,6 +414,7 @@ static enum hrtimer_restart cca_timer_fn(struct hrtimer *t)
 	int sl;
 	u16 ccti, ccti_timer, ccti_min;
 	struct cc_state *cc_state;
+	unsigned long flags;
 
 	cca_timer = container_of(t, struct cca_timer, hrtimer);
 	ppd = cca_timer->ppd;
@@ -437,7 +438,7 @@ static enum hrtimer_restart cca_timer_fn(struct hrtimer *t)
 	ccti_min = cc_state->cong_setting.entries[sl].ccti_min;
 	ccti_timer = cc_state->cong_setting.entries[sl].ccti_timer;
 
-	spin_lock(&ppd->cca_timer_lock);
+	spin_lock_irqsave(&ppd->cca_timer_lock, flags);
 
 	ccti = cca_timer->ccti;
 
@@ -446,7 +447,7 @@ static enum hrtimer_restart cca_timer_fn(struct hrtimer *t)
 		set_link_ipg(ppd);
 	}
 
-	spin_unlock(&ppd->cca_timer_lock);
+	spin_unlock_irqrestore(&ppd->cca_timer_lock, flags);
 
 	rcu_read_unlock();
 
