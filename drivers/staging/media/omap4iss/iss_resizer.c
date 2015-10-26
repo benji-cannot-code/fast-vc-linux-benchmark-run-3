4ea278fb634528806dafc325e8bae40efb82d5a8
Amarjargal Gundjalam <amarjargal.gundjalam@gmail.com>
2015-10-26 06:09:30 -0700

    staging: media: omap4iss: Removes unnecessary blank lines
    This patch fixes checkpatch issues:
    
    CHECK: Blank lines aren't necessary before a close brace '}'
    CHECK: Please don't use multiple blank lines
    
    Signed-off-by: Amarjargal Gundjalam <amarjargal.gundjalam@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/omap4iss/iss_resizer.c
+++ drivers/staging/media/omap4iss/iss_resizer.c
@@ -483,7 +483,6 @@ resizer_try_format(struct iss_resizer_device *resizer,
 		fmt->width &= ~15;
 		fmt->height = clamp_t(u32, height, 32, fmt->height);
 		break;
-
 	}
 
 	fmt->colorspace = V4L2_COLORSPACE_JPEG;
@@ -734,7 +733,6 @@ static int resizer_link_setup(struct media_entity *entity,
 			resizer->input = RESIZER_INPUT_IPIPEIF;
 		else if (remote->entity == &iss->ipipe.subdev.entity)
 			resizer->input = RESIZER_INPUT_IPIPE;
-
 
 		break;
 
