9316e1e8e93e2d61310dbaba88b191ff5e34ac0a
Mike Rapoport <mike.rapoport@gmail.com>
2015-10-26 09:05:53 +0200

    staging: sm750fb: remove unused smi_indent variable
    The smi_indent variable is only declared, but never used. Remove it.
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/sm750.c
+++ drivers/staging/sm750fb/sm750.c
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "modedb.h"
 
-int smi_indent;
-
 /*
  * #ifdef __BIG_ENDIAN
  * ssize_t lynxfb_ops_write(struct fb_info *info, const char __user *buf,
--- drivers/staging/sm750fb/sm750.h
+++ drivers/staging/sm750fb/sm750.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FB_ACCEL_SMI 0xab
 
 #define MHZ(x) ((x) * 1000000)
-extern int smi_indent;
-
 
 struct lynx_accel {
 	/* base virtual address of DPR registers */
