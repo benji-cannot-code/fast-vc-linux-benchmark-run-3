2b03a455445fadcc2dab0999116feea64e503206
Mike Rapoport <mike.rapoport@gmail.com>
2015-10-26 09:05:54 +0200

    staging: sm750fb: merge sm750_hw.h into sm750.h
    There is no actual need to split type definitions and global function
    declarations between lots of small headers.
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/sm750.c
+++ drivers/staging/sm750fb/sm750.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #include <asm/fb.h>
 #include "sm750.h"
-#include "sm750_hw.h"
 #include "sm750_accel.h"
 #include "sm750_cursor.h"
 
--- drivers/staging/sm750fb/sm750_hw.c
+++ drivers/staging/sm750fb/sm750_hw.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sizes.h>
 
 #include "sm750.h"
-#include "sm750_hw.h"
 #include "ddk750.h"
 #include "sm750_accel.h"
 
