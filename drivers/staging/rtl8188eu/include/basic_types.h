cebf3a76aa209d83d46080b39a2e4e99f5ded017
Ivan Safonov <insafonov@gmail.com>
2015-10-27 22:21:28 +0700

    staging: rtl8188eu: unused SUCCESS and FAIL macros removed
    This patch removes unused SUCCESS and FAIL macros in include/basic_types.h.
    
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/basic_types.h
+++ drivers/staging/rtl8188eu/include/basic_types.h
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __BASIC_TYPES_H__
 #define __BASIC_TYPES_H__
 
-#define SUCCESS	0
-#define FAIL	(-1)
-
 #include <linux/types.h>
 #define NDIS_OID uint
 
