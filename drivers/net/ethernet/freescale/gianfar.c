23ba959aac4264466387cad10a94a0b9bf15be5a
Abhimanyu <abhimanyu@freescale.com>
2015-10-27 14:17:43 +0530

    gianfar: Increase TX_TIMEOUT to 5HZ
    Increased TX_TIMEOUT to 5HZ to accommodate worst case situation
    for traffic and CPU intensive use cases
    
    Signed-off-by: Priyanka Jain <Priyanka.Jain@freescale.com>
    Signed-off-by: Abhimanyu <abhimanyu@freescale.com>
    Acked-by: Claudiu Manoil <claudiu.manoil@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/gianfar.c
+++ drivers/net/ethernet/freescale/gianfar.c
@@ -108,7 +108,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "gianfar.h"
 
-#define TX_TIMEOUT      (1*HZ)
+#define TX_TIMEOUT      (5*HZ)
 
 const char gfar_driver_version[] = "2.0";
 
