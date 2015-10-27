81a6f561f9e1828f02d3df244da10fd9c3bba1e7
Glen Lee <glen.lee@atmel.com>
2015-10-27 18:27:55 +0900

    staging: wilc1000: wilc_wlan_rxq_remove: add argument wilc and use it
    This patch adds new argument struct wilc and use it instead of g_linux_wlan.
    Pass wilc to wilc_wlan_rxq_remove.
    
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan.c
+++ drivers/staging/wilc1000/wilc_wlan.c
@@ -613,7 +613,7 @@ static int wilc_wlan_rxq_add(struct rxq_entry_t *rqe)
 	return p->rxq_entries;
 }
 
-static struct rxq_entry_t *wilc_wlan_rxq_remove(void)
+static struct rxq_entry_t *wilc_wlan_rxq_remove(struct wilc *wilc)
 {
 	wilc_wlan_dev_t *p = &g_wlan;
 
@@ -621,12 +621,12 @@ static struct rxq_entry_t *wilc_wlan_rxq_remove(void)
 	if (p->rxq_head) {
 		struct rxq_entry_t *rqe;
 
-		mutex_lock(&g_linux_wlan->rxq_cs);
+		mutex_lock(&wilc->rxq_cs);
 		rqe = p->rxq_head;
 		p->rxq_head = p->rxq_head->next;
 		p->rxq_entries -= 1;
 		PRINT_D(RX_DBG, "RXQ entries decreased\n");
-		mutex_unlock(&g_linux_wlan->rxq_cs);
+		mutex_unlock(&wilc->rxq_cs);
 		return rqe;
 	}
 	PRINT_D(RX_DBG, "Nothing to get from Q\n");
@@ -1133,7 +1133,7 @@ static void wilc_wlan_handle_rxq(struct wilc *wilc)
 			up(&wilc->cfg_event);
 			break;
 		}
-		rqe = wilc_wlan_rxq_remove();
+		rqe = wilc_wlan_rxq_remove(wilc);
 		if (rqe == NULL) {
 			PRINT_D(RX_DBG, "nothing in the queue - exit 1st do-while\n");
 			break;
@@ -1655,6 +1655,11 @@ void wilc_wlan_cleanup(struct net_device *dev)
 	struct rxq_entry_t *rqe;
 	u32 reg = 0;
 	int ret;
+	perInterface_wlan_t *nic;
+	struct wilc *wilc;
+
+	nic = netdev_priv(dev);
+	wilc = nic->wilc;
 
 	p->quit = 1;
 	do {
@@ -1667,7 +1672,7 @@ void wilc_wlan_cleanup(struct net_device *dev)
 	} while (1);
 
 	do {
-		rqe = wilc_wlan_rxq_remove();
+		rqe = wilc_wlan_rxq_remove(wilc);
 		if (rqe == NULL)
 			break;
 #ifndef MEMORY_STATIC
