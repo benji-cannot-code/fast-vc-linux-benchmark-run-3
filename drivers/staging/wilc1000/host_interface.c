dce0fc03b1b2882363d342c0313c38ac674489d2
Leo Kim <leo.kim@atmel.com>
2015-10-28 15:59:24 +0900

    staging: wilc1000: fixes missing a blank line after declarations
    This patch fixes the warnings reported by checkpatch.pl
    for Missing a blank line after declarations.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -472,6 +472,7 @@ static s32 Handle_SetMacAddress(struct host_if_drv *hif_drv,
 	s32 result = 0;
 	struct wid strWID;
 	u8 *mac_buf = kmalloc(ETH_ALEN, GFP_KERNEL);
+
 	if (!mac_buf) {
 		PRINT_ER("No buffer to send mac address\n");
 		return -EFAULT;
@@ -771,6 +772,7 @@ static s32 Handle_CfgParam(struct host_if_drv *hif_drv,
 	}
 	if (strHostIFCfgParamAttr->cfg_attr_info.flag & CURRENT_TX_RATE) {
 		enum CURRENT_TXRATE curr_tx_rate = strHostIFCfgParamAttr->cfg_attr_info.curr_tx_rate;
+
 		if (curr_tx_rate == AUTORATE || curr_tx_rate == MBPS_1
 		    || curr_tx_rate == MBPS_2 || curr_tx_rate == MBPS_5_5
 		    || curr_tx_rate == MBPS_11 || curr_tx_rate == MBPS_6
@@ -3689,8 +3691,8 @@ int host_int_set_mac_chnl_num(struct host_if_drv *hif_drv, u8 channel)
 int host_int_wait_msg_queue_idle(void)
 {
 	int result = 0;
-
 	struct host_if_msg msg;
+
 	memset(&msg, 0, sizeof(struct host_if_msg));
 	msg.id = HOST_IF_MSG_Q_IDLE;
 	result = wilc_mq_send(&hif_msg_q, &msg, sizeof(struct host_if_msg));
@@ -3707,8 +3709,8 @@ int host_int_wait_msg_queue_idle(void)
 int host_int_set_wfi_drv_handler(struct host_if_drv *hif_drv)
 {
 	int result = 0;
-
 	struct host_if_msg msg;
+
 	memset(&msg, 0, sizeof(struct host_if_msg));
 	msg.id = HOST_IF_MSG_SET_WFIDRV_HANDLER;
 	msg.body.drv.u32Address = get_id_from_handler(hif_drv);
@@ -3726,8 +3728,8 @@ int host_int_set_wfi_drv_handler(struct host_if_drv *hif_drv)
 int host_int_set_operation_mode(struct host_if_drv *hif_drv, u32 mode)
 {
 	int result = 0;
-
 	struct host_if_msg msg;
+
 	memset(&msg, 0, sizeof(struct host_if_msg));
 	msg.id = HOST_IF_MSG_SET_OPERATION_MODE;
 	msg.body.mode.u32Mode = mode;
@@ -3827,8 +3829,8 @@ s32 host_int_get_rssi(struct host_if_drv *hif_drv, s8 *ps8Rssi)
 {
 	s32 result = 0;
 	struct host_if_msg msg;
-	memset(&msg, 0, sizeof(struct host_if_msg));
 
+	memset(&msg, 0, sizeof(struct host_if_msg));
 	msg.id = HOST_IF_MSG_GET_RSSI;
 	msg.drv = hif_drv;
 
@@ -3854,8 +3856,8 @@ s32 host_int_get_link_speed(struct host_if_drv *hif_drv, s8 *ps8lnkspd)
 {
 	struct host_if_msg msg;
 	s32 result = 0;
-	memset(&msg, 0, sizeof(struct host_if_msg));
 
+	memset(&msg, 0, sizeof(struct host_if_msg));
 	msg.id = HOST_IF_MSG_GET_LINKSPEED;
 	msg.drv = hif_drv;
 
@@ -3881,8 +3883,8 @@ s32 host_int_get_statistics(struct host_if_drv *hif_drv, struct rf_info *pstrSta
 {
 	s32 result = 0;
 	struct host_if_msg msg;
-	memset(&msg, 0, sizeof(struct host_if_msg));
 
+	memset(&msg, 0, sizeof(struct host_if_msg));
 	msg.id = HOST_IF_MSG_GET_STATISTICS;
 	msg.body.data = (char *)pstrStatistics;
 	msg.drv = hif_drv;
@@ -4585,6 +4587,7 @@ s32 host_int_add_station(struct host_if_drv *hif_drv,
 	memcpy(pstrAddStationMsg, pstrStaParams, sizeof(struct add_sta_param));
 	if (pstrAddStationMsg->u8NumRates > 0) {
 		u8 *rates = kmalloc(pstrAddStationMsg->u8NumRates, GFP_KERNEL);
+
 		if (!rates)
 			return -ENOMEM;
 
@@ -4701,6 +4704,7 @@ s32 host_int_edit_station(struct host_if_drv *hif_drv,
 	memcpy(pstrAddStationMsg, pstrStaParams, sizeof(struct add_sta_param));
 	if (pstrAddStationMsg->u8NumRates > 0) {
 		u8 *rates = kmalloc(pstrAddStationMsg->u8NumRates, GFP_KERNEL);
+
 		if (!rates)
 			return -ENOMEM;
 
