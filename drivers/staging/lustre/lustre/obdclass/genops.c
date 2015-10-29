158ae7098ff5c49534046a7d9307dfd5667685ed
Dan Carpenter <dan.carpenter@oracle.com>
2015-10-29 12:24:40 +0300

    staging: lustre: remove o_ prefix from function pointers
    We mostly refer to these function pointers using macros that use macro
    magic to add the "o_" prefix to the front.  It means that you can't use
    cscope to find the caller.  Heck, you can't even grep for it.
    
    I looked at preserving the "o_" prefix by removing the macro magic and
    adding "o_" to all the call sites but then I realized that, really, the
    prefix doesn't add any value.  Let's just remove it.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/genops.c
+++ drivers/staging/lustre/lustre/obdclass/genops.c
@@ -133,7 +133,7 @@ static struct obd_type *class_get_type(const char *name)
 	if (type) {
 		spin_lock(&type->obd_type_lock);
 		type->typ_refcnt++;
-		try_module_get(type->typ_dt_ops->o_owner);
+		try_module_get(type->typ_dt_ops->owner);
 		spin_unlock(&type->obd_type_lock);
 	}
 	return type;
@@ -144,7 +144,7 @@ void class_put_type(struct obd_type *type)
 	LASSERT(type);
 	spin_lock(&type->obd_type_lock);
 	type->typ_refcnt--;
-	module_put(type->typ_dt_ops->o_owner);
+	module_put(type->typ_dt_ops->owner);
 	spin_unlock(&type->obd_type_lock);
 }
 EXPORT_SYMBOL(class_put_type);
