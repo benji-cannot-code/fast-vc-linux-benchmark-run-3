5783a19efba0714e75a54ca08b9808249b722820
Leo Kim <leo.kim@atmel.com>
2015-10-29 12:05:44 +0900

    staging: wilc1000: rename pu8Rates of struct add_sta_param
    This patch renames pu8Rates of struct add_sta_param to rates
    to avoid CamelCase naming convention.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -2317,7 +2317,7 @@ static u32 WILC_HostIf_PackStaParam(u8 *pu8Buffer,
 
 	*pu8CurrByte++ = pstrStationParam->rates_len;
 	if (pstrStationParam->rates_len > 0)
-		memcpy(pu8CurrByte, pstrStationParam->pu8Rates,
+		memcpy(pu8CurrByte, pstrStationParam->rates,
 		       pstrStationParam->rates_len);
 	pu8CurrByte += pstrStationParam->rates_len;
 
@@ -2373,7 +2373,7 @@ static void Handle_AddStation(struct host_if_drv *hif_drv,
 		PRINT_ER("Failed to send add station config packet\n");
 
 ERRORHANDLER:
-	kfree(pstrStationParam->pu8Rates);
+	kfree(pstrStationParam->rates);
 	kfree(wid.val);
 }
 
@@ -2475,7 +2475,7 @@ static void Handle_EditStation(struct host_if_drv *hif_drv,
 		PRINT_ER("Failed to send edit station config packet\n");
 
 ERRORHANDLER:
-	kfree(pstrStationParam->pu8Rates);
+	kfree(pstrStationParam->rates);
 	kfree(wid.val);
 }
 
@@ -4553,9 +4553,9 @@ s32 host_int_add_station(struct host_if_drv *hif_drv,
 		if (!rates)
 			return -ENOMEM;
 
-		memcpy(rates, pstrStaParams->pu8Rates,
+		memcpy(rates, pstrStaParams->rates,
 		       pstrAddStationMsg->rates_len);
-		pstrAddStationMsg->pu8Rates = rates;
+		pstrAddStationMsg->rates = rates;
 	}
 
 	result = wilc_mq_send(&hif_msg_q, &msg, sizeof(struct host_if_msg));
@@ -4670,9 +4670,9 @@ s32 host_int_edit_station(struct host_if_drv *hif_drv,
 		if (!rates)
 			return -ENOMEM;
 
-		memcpy(rates, pstrStaParams->pu8Rates,
+		memcpy(rates, pstrStaParams->rates,
 		       pstrAddStationMsg->rates_len);
-		pstrAddStationMsg->pu8Rates = rates;
+		pstrAddStationMsg->rates = rates;
 	}
 
 	result = wilc_mq_send(&hif_msg_q, &msg, sizeof(struct host_if_msg));
