6bf7d6b6a0313254a250f97a231553a250038d18
James Simmons <uja.ornl@yahoo.com>
2015-10-29 17:35:18 -0400

    staging: lustre: remove obsolete comment in libcfs_hash.h
    Remove comment hash_long which was removed long ago.
    
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/libcfs_hash.h
+++ drivers/staging/lustre/include/linux/libcfs/libcfs_hash.h
@@ -57,13 +57,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  2^63 + 2^61 - 2^57 + 2^54 - 2^51 - 2^18 + 1 */
 #define CFS_GOLDEN_RATIO_PRIME_64 0x9e37fffffffc0001ULL
 
-/*
- * Ideally we would use HAVE_HASH_LONG for this, but on linux we configure
- * the linux kernel and user space at the same time, so we need to differentiate
- * between them explicitly. If this is not needed on other architectures, then
- * we'll need to move the functions to architecture specific headers.
- */
-
 #include <linux/hash.h>
 
 /** disable debug */
