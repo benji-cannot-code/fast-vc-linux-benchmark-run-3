34d29cd0f0a2a646564bd98ff417534c7d1dcadd
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-10-30 11:46:16 +0900

    serial: 8250_uniphier: add earlycon support
    This reuses the code of drivers/tty/serial/8250/8250_early.c except
    
      - Overwrite device->port.iotype and device->port.regshift for
        UPIO_MEM32 because of_setup_earlycon() has set them for UPIO_MEM.
    
      - Set device->baud to zero to prevent early8250_setup() from
        initializing the divisor register because port->uartclk does not
        match the frequency expected by this hardware.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_uniphier.c
+++ drivers/tty/serial/8250/8250_uniphier.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/clk.h>
+#include <linux/console.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of.h>
@@ -34,6 +35,29 @@ struct uniphier8250_priv {
 	struct clk *clk;
 	spinlock_t atomic_write_lock;
 };
+
+#ifdef CONFIG_SERIAL_8250_CONSOLE
+static int __init uniphier_early_console_setup(struct earlycon_device *device,
+					       const char *options)
+{
+	if (!device->port.membase)
+		return -ENODEV;
+
+	/* This hardware always expects MMIO32 register interface. */
+	device->port.iotype = UPIO_MEM32;
+	device->port.regshift = 2;
+
+	/*
+	 * Do not touch the divisor register in early_serial8250_setup();
+	 * we assume it has been initialized by a boot loader.
+	 */
+	device->baud = 0;
+
+	return early_serial8250_setup(device, options);
+}
+OF_EARLYCON_DECLARE(uniphier, "socionext,uniphier-uart",
+		    uniphier_early_console_setup);
+#endif
 
 /*
  * The register map is slightly different from that of 8250.
