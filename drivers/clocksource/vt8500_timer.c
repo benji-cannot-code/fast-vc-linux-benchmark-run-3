c92cefa26a404f81e74935099905ccc1e4d3174d
Daniel Lezcano <daniel.lezcano@linaro.org>
2015-10-31 20:20:43 +0100

    clocksource/drivers/vt8500: Remove unneeded header
    Remove the <asm/time.h> header inclusion which is pointless.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/vt8500_timer.c
+++ drivers/clocksource/vt8500_timer.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clocksource.h>
 #include <linux/clockchips.h>
 #include <linux/delay.h>
-#include <asm/mach/time.h>
 
 #include <linux/of.h>
 #include <linux/of_address.h>
