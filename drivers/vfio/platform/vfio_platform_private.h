cff61a750f31c663d82ecae54a3460e0835c000f
Eric Auger <eric.auger@linaro.org>
2015-11-03 18:12:17 +0000

    vfio: platform: use list of registered reset function
    Remove the static lookup table and use the dynamic list of registered
    reset functions instead. Also load the reset module through its alias.
    The reset struct module pointer is stored in vfio_platform_device.
    
    We also remove the useless struct device pointer parameter in
    vfio_platform_get_reset.
    
    This patch fixes the issue related to the usage of __symbol_get, which
    besides from being moot, prevented compilation with CONFIG_MODULES
    disabled.
    
    Also usage of MODULE_ALIAS makes possible to add a new reset module
    without needing to update the framework. This was suggested by Arnd.
    
    Signed-off-by: Eric Auger <eric.auger@linaro.org>
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/vfio/platform/vfio_platform_private.h
+++ drivers/vfio/platform/vfio_platform_private.h
@@ -59,6 +59,7 @@ struct vfio_platform_device {
 	struct mutex			igate;
 	struct module			*parent_module;
 	const char			*compat;
+	struct module			*reset_module;
 
 	/*
 	 * These fields should be filled by the bus specific binder
@@ -80,12 +81,6 @@ struct vfio_platform_reset_node {
 	char *compat;
 	struct module *owner;
 	vfio_platform_reset_fn_t reset;
-};
-
-struct vfio_platform_reset_combo {
-	const char *compat;
-	const char *reset_function_name;
-	const char *module_name;
 };
 
 extern int vfio_platform_probe_common(struct vfio_platform_device *vdev,
