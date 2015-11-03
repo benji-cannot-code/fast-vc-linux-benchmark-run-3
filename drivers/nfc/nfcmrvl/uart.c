2cdec9f780096219266afeef690e376dbb0adee6
Vincent Cuissard <cuissard@marvell.com>
2015-11-03 19:19:35 +0100

    NFC: nfcmrvl: remove unneeded CONFIG_OF switches
    Signed-off-by: Vincent Cuissard <cuissard@marvell.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/nfcmrvl/uart.c
+++ drivers/nfc/nfcmrvl/uart.c
@@ -68,8 +68,6 @@ static struct nfcmrvl_if_ops uart_ops = {
 	.nci_update_config = nfcmrvl_uart_nci_update_config
 };
 
-#ifdef CONFIG_OF
-
 static int nfcmrvl_uart_parse_dt(struct device_node *node,
 				 struct nfcmrvl_platform_data *pdata)
 {
@@ -102,16 +100,6 @@ static int nfcmrvl_uart_parse_dt(struct device_node *node,
 
 	return 0;
 }
-
-#else
-
-static int nfcmrvl_uart_parse_dt(struct device_node *node,
-				 struct nfcmrvl_platform_data *pdata)
-{
-	return -ENODEV;
-}
-
-#endif
 
 /*
 ** NCI UART OPS
