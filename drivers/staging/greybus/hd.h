e400ec1b7f711565f23a0dfec504661615703557
Johan Hovold <johan@hovoldconsulting.com>
2015-11-03 18:03:24 +0100

    greybus: hd: rename host-driver structure
    Rename host-driver structure to gb_hd_driver to more clearly separate it
    from the host-device structure.
    
    Also remove an outdated description of the struct.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/hd.h
+++ drivers/staging/greybus/hd.h
@@ -14,10 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct gb_host_device;
 struct gb_message;
 
-/* Greybus "Host driver" structure, needed by a host controller driver to be
- * able to handle both SVC control as well as "real" greybus messages
- */
-struct greybus_host_driver {
+struct gb_hd_driver {
 	size_t	hd_priv_size;
 
 	int (*cport_enable)(struct gb_host_device *hd, u16 cport_id);
@@ -32,7 +29,7 @@ struct greybus_host_driver {
 struct gb_host_device {
 	struct kref kref;
 	struct device *parent;
-	const struct greybus_host_driver *driver;
+	const struct gb_hd_driver *driver;
 
 	struct list_head interfaces;
 	struct list_head connections;
@@ -52,7 +49,7 @@ struct gb_host_device {
 	unsigned long hd_priv[0] __aligned(sizeof(s64));
 };
 
-struct gb_host_device *greybus_create_hd(struct greybus_host_driver *hd,
+struct gb_host_device *greybus_create_hd(struct gb_hd_driver *driver,
 					      struct device *parent,
 					      size_t buffer_size_max,
 					      size_t num_cports);
