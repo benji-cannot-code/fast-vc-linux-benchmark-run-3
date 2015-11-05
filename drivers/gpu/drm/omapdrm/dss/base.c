a5e86a816a9c3b9831c5e9249073a2f6f5585091
Tomi Valkeinen <tomi.valkeinen@ti.com>
2015-11-05 17:23:14 +0200

    drm/omap: move dss_initialized to omapdss-base
    omapdss_is_initialized() is used to find out if omapdss has been probed
    successfully. This patch moves the related code to the common
    omapdss-base module, so that the same support will be there for both
    omapdss and omapdss6.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/dss/base.c
+++ drivers/gpu/drm/omapdrm/dss/base.c
@@ -2,6 +2,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+static bool dss_initialized;
+
+void omapdss_set_is_initialized(bool set)
+{
+	dss_initialized = set;
+}
+EXPORT_SYMBOL(omapdss_set_is_initialized);
+
+bool omapdss_is_initialized(void)
+{
+	return dss_initialized;
+}
+EXPORT_SYMBOL(omapdss_is_initialized);
+
 MODULE_AUTHOR("Tomi Valkeinen <tomi.valkeinen@ti.com>");
 MODULE_DESCRIPTION("OMAP Display Subsystem Base");
 MODULE_LICENSE("GPL v2");
