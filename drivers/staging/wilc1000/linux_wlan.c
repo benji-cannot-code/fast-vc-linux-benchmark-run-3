614a4c990b13ea8e256895b0e99c6c5cbc382a6d
Leo Kim <leo.kim@atmel.com>
2015-11-05 14:36:15 +0900

    staging: wilc1000: rename pstrWFIDrv of function mac_close
    This patch renames pstrWFIDrv of function mac_close to hif_drv
    to avoid CamelCase naming convention.
    And, some debug print modification that has been included name 'pstrWFIDrv'.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan.c
+++ drivers/staging/wilc1000/linux_wlan.c
@@ -1431,7 +1431,7 @@ int mac_close(struct net_device *ndev)
 {
 	struct wilc_priv *priv;
 	perInterface_wlan_t *nic;
-	struct host_if_drv *pstrWFIDrv;
+	struct host_if_drv *hif_drv;
 	struct wilc *wl;
 
 	nic = netdev_priv(ndev);
@@ -1450,7 +1450,7 @@ int mac_close(struct net_device *ndev)
 		return 0;
 	}
 
-	pstrWFIDrv = (struct host_if_drv *)priv->hWILCWFIDrv;
+	hif_drv = (struct host_if_drv *)priv->hWILCWFIDrv;
 
 	PRINT_D(GENERIC_DBG, "Mac close\n");
 
@@ -1459,8 +1459,8 @@ int mac_close(struct net_device *ndev)
 		return 0;
 	}
 
-	if (!pstrWFIDrv) {
-		PRINT_ER("pstrWFIDrv = NULL\n");
+	if (!hif_drv) {
+		PRINT_ER("hif_drv = NULL\n");
 		return 0;
 	}
 
