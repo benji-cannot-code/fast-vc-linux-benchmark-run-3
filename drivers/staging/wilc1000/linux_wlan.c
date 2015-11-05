6719873d234afb79e789ed5fe96869ea60217273
Leo Kim <leo.kim@atmel.com>
2015-11-05 14:36:20 +0900

    staging: wilc1000: remove extern function in c file and move it to header file.
    This patch removes extern resolve_disconnect_aberration in c file and move
    it to proper header file. Rename argument also to match with declaration.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan.c
+++ drivers/staging/wilc1000/linux_wlan.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  #define _linux_wlan_device_removal()		{}
 
 extern bool g_obtainingIP;
-extern void resolve_disconnect_aberration(void *drvHandler);
 extern u8 multicast_mac_addr_list[WILC_MULTICAST_TABLE_SIZE][ETH_ALEN];
 extern struct timer_list hDuringIpTimer;
 
