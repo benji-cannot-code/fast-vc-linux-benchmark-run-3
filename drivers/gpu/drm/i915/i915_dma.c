4d41b76dc100ef83ff18be79ee10c35395e0598c
Ville Syrjälä <ville.syrjala@linux.intel.com>
2015-11-06 15:08:33 +0200

    drm/i915: Move the fbdev async_schedule() into intel_fbdev.c
    Reading the driver load/unload code leaves one confused as there's
    an async_schedule() in the load, but not async_synchronize_full()
    in sight. In fact it's hidden inside intel_fbdev.c. So let's move the
    async_schedule() into intel_fbdev.c as well so that it's next to the
    async_synchronize_full(), which should make the relationship easier
    to see.
    
    Plus this way we won't schedule a nop function call when fbdev is
    disabled. And we were passing a pointer to a static inline
    function to async_schedule(), which seems rather dubious to me.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1446815313-9490-4-git-send-email-ville.syrjala@linux.intel.com
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/async.h>
 #include <drm/drmP.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_fb_helper.h>
@@ -470,7 +469,7 @@ static int i915_load_modeset_init(struct drm_device *dev)
 	 * scanning against hotplug events. Hence do this first and ignore the
 	 * tiny window where we will loose hotplug notifactions.
 	 */
-	async_schedule(intel_fbdev_initial_config, dev_priv);
+	intel_fbdev_initial_config_async(dev);
 
 	drm_kms_helper_poll_init(dev);
 
