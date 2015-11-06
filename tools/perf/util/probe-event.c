c635c257c7bd56bbebb7568d8be3f46b524d7cd7
Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
2015-11-06 17:30:03 +0900

    perf probe: Cleanup find_perf_probe_point_from_map to reduce redundancy
    In find_perf_probe_point_from_map(), the 'ret' variable is initialized
    with -ENOENT but overwritten by the return code of
    kernel_get_symbol_address_by_name(), and after that it is re-initialized
    with -ENOENT again.
    
    Setting ret=-ENOENT twice looks a bit redundant. This avoids the
    overwriting and just returns -ENOENT if some error happens to simplify
    the code.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Cc: Zefan Li <lizefan@huawei.com>
    Link: http://lkml.kernel.org/n/tip-ufp1zgbktzmttcputozneomd@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/probe-event.c
+++ tools/perf/util/probe-event.c
@@ -1896,9 +1896,8 @@ static int find_perf_probe_point_from_map(struct probe_trace_point *tp,
 		sym = map__find_symbol(map, addr, NULL);
 	} else {
 		if (tp->symbol && !addr) {
-			ret = kernel_get_symbol_address_by_name(tp->symbol,
-							&addr, true, false);
-			if (ret < 0)
+			if (kernel_get_symbol_address_by_name(tp->symbol,
+						&addr, true, false) < 0)
 				goto out;
 		}
 		if (addr) {
@@ -1907,8 +1906,6 @@ static int find_perf_probe_point_from_map(struct probe_trace_point *tp,
 		}
 	}
 
-	/* ret may has be overwritten so reset it */
-	ret = -ENOENT;
 	if (!sym)
 		goto out;
 
