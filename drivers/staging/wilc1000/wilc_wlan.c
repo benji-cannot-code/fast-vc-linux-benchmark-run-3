93cc879ef29124f64d8c96f20994b4678ba9a641
Leo Kim <leo.kim@atmel.com>
2015-11-06 11:19:55 +0900

    staging: wilc1000: replace numeric type to kernel error type
    This patch replaces numeric type to generic type by kernel style.
     -5 -> -EIO
     -105 -> -ENOBUFS
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan.c
+++ drivers/staging/wilc1000/wilc_wlan.c
@@ -1195,7 +1195,7 @@ int wilc_wlan_firmware_download(const u8 *buffer, u32 buffer_size)
 
 	dma_buffer = kmalloc(blksz, GFP_KERNEL);
 	if (!dma_buffer) {
-		ret = -5;
+		ret = -EIO;
 		PRINT_ER("Can't allocate buffer for firmware download IO error\n ");
 		goto _fail_1;
 	}
@@ -1230,7 +1230,7 @@ int wilc_wlan_firmware_download(const u8 *buffer, u32 buffer_size)
 		release_bus(RELEASE_ONLY);
 
 		if (!ret) {
-			ret = -5;
+			ret = -EIO;
 			PRINT_ER("Can't download firmware IO error\n ");
 			goto _fail_;
 		}
@@ -1264,7 +1264,7 @@ int wilc_wlan_start(void)
 	if (!ret) {
 		wilc_debug(N_ERR, "[wilc start]: fail write reg vmm_core_cfg...\n");
 		release_bus(RELEASE_ONLY);
-		ret = -5;
+		ret = -EIO;
 		return ret;
 	}
 	reg = 0;
@@ -1299,7 +1299,7 @@ int wilc_wlan_start(void)
 	if (!ret) {
 		wilc_debug(N_ERR, "[wilc start]: fail write WILC_GP_REG_1 ...\n");
 		release_bus(RELEASE_ONLY);
-		ret = -5;
+		ret = -EIO;
 		return ret;
 	}
 
@@ -1309,7 +1309,7 @@ int wilc_wlan_start(void)
 	if (!ret) {
 		wilc_debug(N_ERR, "[wilc start]: fail read reg 0x1000 ...\n");
 		release_bus(RELEASE_ONLY);
-		ret = -5;
+		ret = -EIO;
 		return ret;
 	}
 
@@ -1662,7 +1662,7 @@ int wilc_wlan_init(struct net_device *dev, wilc_wlan_inp_t *inp)
 
 	if ((inp->io_func.io_type & 0x1) == HIF_SDIO) {
 		if (!hif_sdio.hif_init(inp, wilc_debug)) {
-			ret = -5;
+			ret = -EIO;
 			goto _fail_;
 		}
 		memcpy((void *)&g_wlan.hif_func, &hif_sdio,
@@ -1670,19 +1670,19 @@ int wilc_wlan_init(struct net_device *dev, wilc_wlan_inp_t *inp)
 	} else {
 		if ((inp->io_func.io_type & 0x1) == HIF_SPI) {
 			if (!hif_spi.hif_init(inp, wilc_debug)) {
-				ret = -5;
+				ret = -EIO;
 				goto _fail_;
 			}
 			memcpy((void *)&g_wlan.hif_func, &hif_spi,
 			       sizeof(wilc_hif_func_t));
 		} else {
-			ret = -5;
+			ret = -EIO;
 			goto _fail_;
 		}
 	}
 
 	if (!wilc_wlan_cfg_init(wilc_debug)) {
-		ret = -105;
+		ret = -ENOBUFS;
 		goto _fail_;
 	}
 
@@ -1691,7 +1691,7 @@ int wilc_wlan_init(struct net_device *dev, wilc_wlan_inp_t *inp)
 	PRINT_D(TX_DBG, "g_wlan.tx_buffer = %p\n", g_wlan.tx_buffer);
 
 	if (!g_wlan.tx_buffer) {
-		ret = -105;
+		ret = -ENOBUFS;
 		PRINT_ER("Can't allocate Tx Buffer");
 		goto _fail_;
 	}
@@ -1701,14 +1701,14 @@ int wilc_wlan_init(struct net_device *dev, wilc_wlan_inp_t *inp)
 		g_wlan.rx_buffer = kmalloc(LINUX_RX_SIZE, GFP_KERNEL);
 	PRINT_D(TX_DBG, "g_wlan.rx_buffer =%p\n", g_wlan.rx_buffer);
 	if (!g_wlan.rx_buffer) {
-		ret = -105;
+		ret = -ENOBUFS;
 		PRINT_ER("Can't allocate Rx Buffer");
 		goto _fail_;
 	}
 #endif
 
 	if (!init_chip(dev)) {
-		ret = -5;
+		ret = -EIO;
 		goto _fail_;
 	}
 #ifdef	TCP_ACK_FILTER
