8beec9d93bfc11a20b9f6a0083f8f460f913551a
Daniel Lezcano <daniel.lezcano@linaro.org>
2015-11-07 14:26:46 +0100

    clocksource/drivers/h8300_tpu: Remove pointless headers for TPU
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/h8300_tpu.c
+++ drivers/clocksource/h8300_tpu.c
@@ -7,14 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/errno.h>
-#include <linux/sched.h>
 #include <linux/kernel.h>
-#include <linux/interrupt.h>
 #include <linux/init.h>
-#include <linux/platform_device.h>
-#include <linux/slab.h>
 #include <linux/clocksource.h>
-#include <linux/module.h>
 #include <linux/clk.h>
 #include <linux/io.h>
 #include <linux/of.h>
