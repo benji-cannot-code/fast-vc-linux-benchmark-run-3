06c5ff1d17c443a75e1d9afe0492d25f9744b808
Shraddha Barke <shraddha.6596@gmail.com>
2015-11-07 09:22:55 +0530

    Staging: fwserial: Declare fwtty_port_get as static
    Declare the function fwtty_port_get as static since it is used
    only in this particular file. Also remove the corresponding
    declaration from header file.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fwserial/fwserial.c
+++ drivers/staging/fwserial/fwserial.c
@@ -829,7 +829,7 @@ static void fwtty_write_xchar(struct fwtty_port *port, char ch)
 	rcu_read_unlock();
 }
 
-struct fwtty_port *fwtty_port_get(unsigned index)
+static struct fwtty_port *fwtty_port_get(unsigned index)
 {
 	struct fwtty_port *port;
 
@@ -843,7 +843,6 @@ struct fwtty_port *fwtty_port_get(unsigned index)
 	mutex_unlock(&port_table_lock);
 	return port;
 }
-EXPORT_SYMBOL(fwtty_port_get);
 
 static int fwtty_ports_add(struct fw_serial *serial)
 {
