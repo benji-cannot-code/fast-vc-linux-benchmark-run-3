bbe40da3190b59e4fdbc1e8b01a6b2525bb96aa3
Ben Skeggs <bskeggs@redhat.com>
2015-11-08 12:23:16 +1000

    drm/nouveau/nvif: split out device interface definitions
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvif/device.h
+++ drivers/gpu/drm/nouveau/include/nvif/device.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NVIF_DEVICE_H__
 
 #include <nvif/object.h>
-#include <nvif/class.h>
+#include <nvif/cl0080.h>
 
 struct nvif_device {
 	struct nvif_object object;
--- drivers/gpu/drm/nouveau/nvkm/engine/device/user.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/user.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <subdev/timer.h>
 
 #include <nvif/class.h>
+#include <nvif/cl0080.h>
 #include <nvif/unpack.h>
 
 struct nvkm_udevice {
