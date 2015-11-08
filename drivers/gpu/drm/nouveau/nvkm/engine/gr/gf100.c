fa9959a43d6592411d02a68cf44d6686cfcc80c9
Ben Skeggs <bskeggs@redhat.com>
2015-11-08 10:15:09 +1000

    drm/nouveau/nvif: split out fermi interface definitions
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <engine/fifo.h>
 
 #include <nvif/class.h>
+#include <nvif/cl9097.h>
 #include <nvif/unpack.h>
 
 /*******************************************************************************
