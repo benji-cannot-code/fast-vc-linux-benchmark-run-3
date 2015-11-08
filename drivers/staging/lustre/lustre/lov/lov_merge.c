60b830d122f87abdb32d3d025e2d7069a3efddf4
Andreas Dilger <andreas.dilger@intel.com>
2015-11-08 18:09:37 -0500

    staging: lustre: update Intel copyright messages 2015
    Update copyright messages in files modified by Intel employees
    in 2015 by non-trivial patches.  Exclude patches that are only
    deleting code, renaming functions, or adding or removing whitespace.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-7243
    Reviewed-on: http://review.whamcloud.com/16758
    Reviewed-by: James Nunez <james.a.nunez@intel.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_merge.c
+++ drivers/staging/lustre/lustre/lov/lov_merge.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
  *
- * Copyright (c) 2012, Intel Corporation.
+ * Copyright (c) 2012, 2015, Intel Corporation.
  */
 /*
  * This file is part of Lustre, http://www.lustre.org/
