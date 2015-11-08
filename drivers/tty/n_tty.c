6d78ea0027d2a4e2f60f05523a693d5886967150
Peter Hurley <peter@hurleysoftware.com>
2015-11-08 13:01:13 -0500

    tty: Define tty_*() printk macros
    Since not all ttys are devices (eg., SysV ptys), dev_*() printk macros
    cannot be used. Define tty_*() printk macros that output in similar
    format to dev_*() macros (ie., <driver> <tty>: .....).
    
    Transform the most-trivial printk( LEVEL ...) usage to tty_*() usage.
    NB: The function name has been eliminated from messages with unique
    context, or prefixed to the format when given.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/n_tty.c
+++ drivers/tty/n_tty.c
@@ -1202,9 +1202,7 @@ static void n_tty_receive_overrun(struct tty_struct *tty)
 	ldata->num_overrun++;
 	if (time_after(jiffies, ldata->overrun_time + HZ) ||
 			time_after(ldata->overrun_time, jiffies)) {
-		printk(KERN_WARNING "%s: %d input overrun(s)\n",
-			tty_name(tty),
-			ldata->num_overrun);
+		tty_warn(tty, "%d input overrun(s)\n", ldata->num_overrun);
 		ldata->overrun_time = jiffies;
 		ldata->num_overrun = 0;
 	}
@@ -1487,8 +1485,7 @@ n_tty_receive_char_flagged(struct tty_struct *tty, unsigned char c, char flag)
 		n_tty_receive_overrun(tty);
 		break;
 	default:
-		printk(KERN_ERR "%s: unknown flag %d\n",
-		       tty_name(tty), flag);
+		tty_err(tty, "unknown flag %d\n", flag);
 		break;
 	}
 }
