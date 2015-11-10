c87c628a1a33542f3e82fe8497340f26cffa3e52
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2015-11-10 12:36:46 +0000

    missing include asm/paravirt.h in cputime.c
    Add include asm/paravirt.h to cputime.c, as steal_account_process_tick
    calls paravirt_steal_clock, which is defined in asm/paravirt.h.
    
    The ifdef CONFIG_PARAVIRT is necessary because not all archs have an
    asm/paravirt.h to include.
    
    The reason why currently cputime.c compiles, even though include
    <asm/paravirt.h> is missing, is that on x86 asm/paravirt.h is included
    by one of the other headers included in kernel/sched/cputime.c:
    
    On arm and arm64, where I am about to introduce asm/paravirt.h and
    stolen time support, without #include <asm/paravirt.h> in cputime.c, I
    would get an error.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/static_key.h>
 #include <linux/context_tracking.h>
 #include "sched.h"
+#ifdef CONFIG_PARAVIRT
+#include <asm/paravirt.h>
+#endif
 
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
