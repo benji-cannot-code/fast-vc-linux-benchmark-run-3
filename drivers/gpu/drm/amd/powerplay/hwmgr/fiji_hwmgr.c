f39f9246d253b63a5d32bcc26fdc507ef6506529
Alex Deucher <alexander.deucher@amd.com>
2015-11-11 20:18:52 -0500

    drm/amdgpu: extract pcie helpers to common header
    These will be used by multiple powerplay drivers and
    other IP modules.
    
    Reviewed-by: Jammy Zhou <Jammy.Zhou@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/fiji_hwmgr.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/fiji_hwmgr.c
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tonga_pptable.h"
 #include "pp_debug.h"
 #include "pp_acpi.h"
+#include "amd_pcie_helpers.h"
 
 #define VOLTAGE_SCALE	4
 #define SMC_RAM_END		0x40000
