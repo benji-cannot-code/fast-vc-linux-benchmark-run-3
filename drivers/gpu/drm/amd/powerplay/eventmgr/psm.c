d124c0ac07da44fe2e15d1b30eae6fadb049fa44
rezhu <Rex.Zhu@amd.com>
2015-11-12 16:40:50 +0800

    drm/amd/powerplay: fix warning of cast to pointer from integer of different size.
    Signed-off-by: Rex Zhu <Rex.Zhu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/eventmgr/psm.c
+++ drivers/gpu/drm/amd/powerplay/eventmgr/psm.c
@@ -38,7 +38,7 @@ int psm_get_ui_state(struct pp_eventmgr *eventmgr, enum PP_StateUILabel ui_label
 			*state_id = state->id;
 			return 0;
 		}
-		state = (struct pp_power_state *)((uint64_t)state + hwmgr->ps_size);
+		state = (struct pp_power_state *)((unsigned long)state + hwmgr->ps_size);
 	}
 	return -1;
 }
@@ -58,7 +58,7 @@ int psm_get_state_by_classification(struct pp_eventmgr *eventmgr, enum PP_StateC
 			*state_id = state->id;
 			return 0;
 		}
-		state = (struct pp_power_state *)((uint64_t)state + hwmgr->ps_size);
+		state = (struct pp_power_state *)((unsigned long)state + hwmgr->ps_size);
 	}
 	return -1;
 }
@@ -78,7 +78,7 @@ int psm_set_performance_states(struct pp_eventmgr *eventmgr, unsigned long *stat
 			hwmgr->request_ps = state;
 			return 0;
 		}
-		state = (struct pp_power_state *)((uint64_t)state + hwmgr->ps_size);
+		state = (struct pp_power_state *)((unsigned long)state + hwmgr->ps_size);
 	}
 	return -1;
 }
