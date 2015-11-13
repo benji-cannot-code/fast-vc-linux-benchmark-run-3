aa692a512639aae9fd50e8395662ac51b7e73991
Viresh Kumar <viresh.kumar@linaro.org>
2015-11-13 08:59:49 +0530

    greybus: es2: remove fake vendor/product ids
    These aren't used anymore, remove them.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/es2.c
+++ drivers/staging/greybus/es2.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ES2_GBUF_MSG_SIZE_MAX	2048
 
 static const struct usb_device_id id_table[] = {
-	{ USB_DEVICE(0xffff, 0x0002) },	/* Made up number, delete once firmware is fixed to use real number */
 	{ USB_DEVICE(0x18d1, 0x1eaf) },
 	{ },
 };
