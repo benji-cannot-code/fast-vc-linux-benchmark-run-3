451c351ad042397801ac2a72c4b3f822c0f3765a
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2015-11-13 12:01:33 +0100

    crypto: rsa - only require output buffers as big as needed.
    rhe RSA operations explicitly left-align the integers being written
    skipping any leading zero bytes, but still require the output buffers to
    include just enough space for the integer + the leading zero bytes.
    Since the size of integer + the leading zero bytes (i.e. the key modulus
    size) can now be obtained more easily through crypto_akcipher_maxsize
    change the operations to only require as big a buffer as actually needed
    if the caller has that information.  The semantics for request->dst_len
    don't change.
    
    Signed-off-by: Andrew Zaborowski <andrew.zaborowski@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/rsa.c
+++ crypto/rsa.c
@@ -92,12 +92,6 @@ static int rsa_enc(struct akcipher_request *req)
 		goto err_free_c;
 	}
 
-	if (req->dst_len < mpi_get_size(pkey->n)) {
-		req->dst_len = mpi_get_size(pkey->n);
-		ret = -EOVERFLOW;
-		goto err_free_c;
-	}
-
 	ret = -ENOMEM;
 	m = mpi_read_raw_from_sgl(req->src, req->src_len);
 	if (!m)
@@ -134,12 +128,6 @@ static int rsa_dec(struct akcipher_request *req)
 
 	if (unlikely(!pkey->n || !pkey->d)) {
 		ret = -EINVAL;
-		goto err_free_m;
-	}
-
-	if (req->dst_len < mpi_get_size(pkey->n)) {
-		req->dst_len = mpi_get_size(pkey->n);
-		ret = -EOVERFLOW;
 		goto err_free_m;
 	}
 
@@ -181,12 +169,6 @@ static int rsa_sign(struct akcipher_request *req)
 		goto err_free_s;
 	}
 
-	if (req->dst_len < mpi_get_size(pkey->n)) {
-		req->dst_len = mpi_get_size(pkey->n);
-		ret = -EOVERFLOW;
-		goto err_free_s;
-	}
-
 	ret = -ENOMEM;
 	m = mpi_read_raw_from_sgl(req->src, req->src_len);
 	if (!m)
@@ -223,12 +205,6 @@ static int rsa_verify(struct akcipher_request *req)
 
 	if (unlikely(!pkey->n || !pkey->e)) {
 		ret = -EINVAL;
-		goto err_free_m;
-	}
-
-	if (req->dst_len < mpi_get_size(pkey->n)) {
-		req->dst_len = mpi_get_size(pkey->n);
-		ret = -EOVERFLOW;
 		goto err_free_m;
 	}
 
