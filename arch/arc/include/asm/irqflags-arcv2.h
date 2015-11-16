62c18d9625139c14f48509d98b166ddaf89e9547
Vineet Gupta <vgupta@synopsys.com>
2015-11-16 13:52:07 +0530

    ARC: Abstract out ISA specific SLEEP args
    No semantical changes, prepares for ARCv2 specific change in next commit
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/irqflags-arcv2.h
+++ arch/arc/include/asm/irqflags-arcv2.h
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ISA_INIT_STATUS_BITS	(STATUS_IE_MASK | STATUS_AD_MASK | \
 					(ARCV2_IRQ_DEF_PRIO << 1))
 
+#define ISA_SLEEP_ARG		0x10
+
 #ifndef __ASSEMBLY__
 
 /*
