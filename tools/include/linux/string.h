7b7482e7ca89e1163db5381490e4853f5ae38d68
Wang Nan <wangnan0@huawei.com>
2015-11-16 11:42:05 -0300

    tools: Clone the kernel's strtobool function
    Copying it to tools/lib/string.c, the counterpart to the kernel's
    lib/string.c.
    
    This is preparation for enhancing BPF program configuration, which will
    allow config string like 'inlines=yes'.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Jonathan Cameron <jic23@cam.ac.uk>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com
    Link: http://lkml.kernel.org/r/1447675815-166222-6-git-send-email-wangnan0@huawei.com
    [ Copied it to tools/lib/string.c instead, to make it usable by other tools/ ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/include/linux/string.h
+++ tools/include/linux/string.h
@@ -7,4 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void *memdup(const void *src, size_t len);
 
+int strtobool(const char *s, bool *res);
+
 #endif /* _LINUX_STRING_H_ */
