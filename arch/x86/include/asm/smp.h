fa9e5de86c1a7990054dc13fc7c09d3009216b74
Juergen Gross <jgross@suse.com>
2015-11-17 13:05:43 +0100

    x86: Remove unused function cpu_has_ht_siblings()
    It is used nowhere.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Link: http://lkml.kernel.org/r/1447761943-770-1-git-send-email-jgross@suse.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/smp.h
+++ arch/x86/include/asm/smp.h
@@ -22,15 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern int smp_num_siblings;
 extern unsigned int num_processors;
 
-static inline bool cpu_has_ht_siblings(void)
-{
-	bool has_siblings = false;
-#ifdef CONFIG_SMP
-	has_siblings = cpu_has_ht && smp_num_siblings > 1;
-#endif
-	return has_siblings;
-}
-
 DECLARE_PER_CPU_READ_MOSTLY(cpumask_var_t, cpu_sibling_map);
 DECLARE_PER_CPU_READ_MOSTLY(cpumask_var_t, cpu_core_map);
 /* cpus sharing the last level cache: */
