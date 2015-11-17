0e16ab4daa58549bf433456b4242d581169b0b82
Christian König <christian.koenig@amd.com>
2015-11-17 11:02:47 +0100

    drm/amdgpu: fix typo in firmware name
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Chunming Zhou <david1.zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gmc_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v7_0.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static void gmc_v7_0_set_gart_funcs(struct amdgpu_device *adev);
 static void gmc_v7_0_set_irq_funcs(struct amdgpu_device *adev);
 
-MODULE_FIRMWARE("radeon/boniare_mc.bin");
+MODULE_FIRMWARE("radeon/bonaire_mc.bin");
 MODULE_FIRMWARE("radeon/hawaii_mc.bin");
 
 /**
