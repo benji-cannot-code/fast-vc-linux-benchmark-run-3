5d7eabe2bdbd398ae60bdb2dd283a670315d142c
Ian Abbott <abbotti@mev.co.uk>
2015-11-18 17:55:09 +0000

    staging: comedi: return error on "write" if no command set up
    The "write" file operation handler, `comedi_write()` returns an error
    for pretty much any condition that prevents a "write" going ahead.  One
    of the conditions that prevents a "write" going ahead is that no
    asynchronous command has been set up, but that currently results in a
    return value of 0 (unless COMEDI instructions are being processed or an
    asynchronous command has been set up by a different file object).
    Change it to return `-EINVAL` in this case.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi_fops.c
+++ drivers/staging/comedi/comedi_fops.c
@@ -2332,9 +2332,12 @@ static ssize_t comedi_write(struct file *file, const char __user *buf,
 	}
 
 	async = s->async;
-
-	if (!s->busy || !nbytes)
+	if (!nbytes)
 		goto out;
+	if (!s->busy) {
+		retval = -EINVAL;
+		goto out;
+	}
 	if (s->busy != file) {
 		retval = -EACCES;
 		goto out;
@@ -2374,8 +2377,10 @@ static ssize_t comedi_write(struct file *file, const char __user *buf,
 				retval = -ERESTARTSYS;
 				break;
 			}
-			if (!s->busy)
+			if (!s->busy) {
+				retval = -EINVAL;
 				break;
+			}
 			if (s->busy != file) {
 				retval = -EACCES;
 				break;
