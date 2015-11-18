7471c6cbc543eb8958dc6867bf70a34103e56995
Michael Hornung <mhornung.linux@gmail.com>
2015-11-18 20:59:15 +0100

    staging: rtl8712: Replace FSF mailing address by URL
    * Fix checkpatch.pl output about not including the paragraph
      about writing to the Free Software Foundation's mailing address
    
    Signed-off-by: Michael Hornung <mhornung.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/ieee80211.h
+++ drivers/staging/rtl8712/ieee80211.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ * this program; if not, see <http://www.gnu.org/licenses/>.
  *
  * Modifications for inclusion into the Linux staging tree are
  * Copyright(c) 2010 Larry Finger. All rights reserved.
