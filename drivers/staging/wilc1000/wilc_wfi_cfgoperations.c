7bc68eb553e1b48bfdf9f3035f754a9ff7711920
Leo Kim <leo.kim@atmel.com>
2015-11-19 15:56:21 +0900

    staging: wilc1000: rename wilc_connected_SSID variable
    This patch renames wilc_connected_SSID variable to wilc_connected_ssid
    to avoid camelcase.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
@@ -589,7 +589,7 @@ static void CfgConnectResult(enum conn_event enuConnDisconnEvent,
 			 *  = SUCCESSFUL_STATUSCODE, while mac status is MAC_DISCONNECTED (which means something wrong happened) */
 			u16ConnectStatus = WLAN_STATUS_UNSPECIFIED_FAILURE;
 			wilc_wlan_set_bssid(priv->dev, NullBssid);
-			eth_zero_addr(wilc_connected_SSID);
+			eth_zero_addr(wilc_connected_ssid);
 
 			if (!pstrWFIDrv->p2p_connect)
 				wlan_channel = INVALID_CHANNEL;
@@ -647,7 +647,7 @@ static void CfgConnectResult(enum conn_event enuConnDisconnEvent,
 		wilc_ie = false;
 		eth_zero_addr(priv->au8AssociatedBss);
 		wilc_wlan_set_bssid(priv->dev, NullBssid);
-		eth_zero_addr(wilc_connected_SSID);
+		eth_zero_addr(wilc_connected_ssid);
 
 		if (!pstrWFIDrv->p2p_connect)
 			wlan_channel = INVALID_CHANNEL;
