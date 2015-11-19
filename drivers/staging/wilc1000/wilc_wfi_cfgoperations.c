1666674d47bfc96df671f715b579fa559ab6160f
Leo Kim <leo.kim@atmel.com>
2015-11-19 15:56:30 +0900

    staging: wilc1000: rename pUserVoid in add_network_to_shadow function
    This patch renames pUserVoid to user_void that is
    second argument of add_network_to_shadow function to avoid camelcase.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
@@ -355,9 +355,10 @@ static int is_network_in_shadow(tstrNetworkInfo *pstrNetworkInfo,
 	return state;
 }
 
-static void add_network_to_shadow(tstrNetworkInfo *pstrNetworkInfo, void *pUserVoid, void *pJoinParams)
+static void add_network_to_shadow(tstrNetworkInfo *pstrNetworkInfo,
+				  void *user_void, void *pJoinParams)
 {
-	int ap_found = is_network_in_shadow(pstrNetworkInfo, pUserVoid);
+	int ap_found = is_network_in_shadow(pstrNetworkInfo, user_void);
 	u32 ap_index = 0;
 	u8 rssi_index = 0;
 
