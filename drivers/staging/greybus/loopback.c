01c7e3eea84f839bf7b85e04d2b6bad70ae0e454
Alexandre Bailon <abailon@baylibre.com>
2015-11-19 12:44:46 +0100

    greybus: loopback: fix invalid response size
    The size of timestamps is not taken into account, which makes the
    loopback driver in the firmware drop invalid packages.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Reviewed-by: Johan Hovold <johan@hovoldconsulting.com>
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/loopback.c
+++ drivers/staging/greybus/loopback.c
@@ -535,8 +535,8 @@ static int gb_loopback_request_recv(u8 type, struct gb_operation *operation)
 		}
 
 		if (len) {
-			if (!gb_operation_response_alloc(operation, len,
-							 GFP_KERNEL)) {
+			if (!gb_operation_response_alloc(operation,
+					len + sizeof(*response), GFP_KERNEL)) {
 				dev_err(dev, "error allocating response\n");
 				return -ENOMEM;
 			}
