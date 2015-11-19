4031c322648956bbf47b0877c070e9a65c4836ca
Geert Uytterhoeven <geert+renesas@glider.be>
2015-11-19 19:32:06 +0100

    drivers: sh: clk: Avoid crashes when passing NULL clocks
    Several clock API functions handle NULL clocks when the Common Clock
    Framework is used, while their legacy SH counterparts don't, and would
    just crash when a NULL clock is passed.
    
    Add NULL checks to clk_get_rate(), clk_set_rate(), clk_get_parent(), and
    clk_round_rate(), to avoid different behavior in drivers shared between
    legacy and CCF-based platforms.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/sh/clk/core.c
+++ drivers/sh/clk/core.c
@@ -470,6 +470,9 @@ void clk_enable_init_clocks(void)
 
 unsigned long clk_get_rate(struct clk *clk)
 {
+	if (!clk)
+		return 0;
+
 	return clk->rate;
 }
 EXPORT_SYMBOL_GPL(clk_get_rate);
@@ -478,6 +481,9 @@ int clk_set_rate(struct clk *clk, unsigned long rate)
 {
 	int ret = -EOPNOTSUPP;
 	unsigned long flags;
+
+	if (!clk)
+		return 0;
 
 	spin_lock_irqsave(&clock_lock, flags);
 
@@ -536,12 +542,18 @@ EXPORT_SYMBOL_GPL(clk_set_parent);
 
 struct clk *clk_get_parent(struct clk *clk)
 {
+	if (!clk)
+		return NULL;
+
 	return clk->parent;
 }
 EXPORT_SYMBOL_GPL(clk_get_parent);
 
 long clk_round_rate(struct clk *clk, unsigned long rate)
 {
+	if (!clk)
+		return 0;
+
 	if (likely(clk->ops && clk->ops->round_rate)) {
 		unsigned long flags, rounded;
 
