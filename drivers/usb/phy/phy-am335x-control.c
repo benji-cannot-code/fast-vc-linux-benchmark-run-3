4c10e4e5037aff1a3e4d125520bff9ce92a5b375
Bin Liu <b-liu@ti.com>
2015-11-20 16:13:07 -0600

    usb: phy: correct the am335x phy header filename
    The filename of am35x-phy-control.h is confusing. The header is used
    by the am335x phy driver, but the filename refers to am35x. Even worse
    there is indeed another device called am35x but it does not use this
    header at all.
    
    Signed-off-by: Bin Liu <b-liu@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/phy/phy-am335x-control.c
+++ drivers/usb/phy/phy-am335x-control.c
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/io.h>
 #include <linux/delay.h>
-#include "am35x-phy-control.h"
+#include "phy-am335x-control.h"
 
 struct am335x_control_usb {
 	struct device *dev;
