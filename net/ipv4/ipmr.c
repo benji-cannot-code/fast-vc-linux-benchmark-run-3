5b1f067177df79931a6206a53e6b401b2f6c31a6
Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
2015-11-21 15:57:29 +0100

    net: ipmr: drop an instance of CONFIG_IP_MROUTE_MULTIPLE_TABLES
    Trivial replace of ifdef with IS_BUILTIN().
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ipmr.c
+++ net/ipv4/ipmr.c
@@ -1397,11 +1397,12 @@ int ip_mroute_setsockopt(struct sock *sk, int optname, char __user *optval, unsi
 		rtnl_unlock();
 		return ret;
 	}
-#ifdef CONFIG_IP_MROUTE_MULTIPLE_TABLES
 	case MRT_TABLE:
 	{
 		u32 v;
 
+		if (!IS_BUILTIN(CONFIG_IP_MROUTE_MULTIPLE_TABLES))
+			return -ENOPROTOOPT;
 		if (optlen != sizeof(u32))
 			return -EINVAL;
 		if (get_user(v, (u32 __user *)optval))
@@ -1421,7 +1422,6 @@ int ip_mroute_setsockopt(struct sock *sk, int optname, char __user *optval, unsi
 		rtnl_unlock();
 		return ret;
 	}
-#endif
 	/* Spurious command, or MRT_VERSION which you cannot set. */
 	default:
 		return -ENOPROTOOPT;
