7136248544b47e22391b950988fb93c789db796f
Egor Uleyskiy <egor.ulieiskii@gmail.com>
2015-11-22 11:27:56 +0200

    drivers: staging: vme: Fixed checking NULL and 0 code style
    Signed-off-by: Egor Uleyskiy <egor.ulieiskii@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vme/devices/vme_user.c
+++ drivers/staging/vme/devices/vme_user.c
@@ -310,7 +310,7 @@ static int vme_user_ioctl(struct inode *inode, struct file *file,
 		case VME_IRQ_GEN:
 			copied = copy_from_user(&irq_req, argp,
 						sizeof(irq_req));
-			if (copied != 0) {
+			if (copied) {
 				pr_warn("Partial copy from userspace\n");
 				return -EFAULT;
 			}
@@ -336,7 +336,7 @@ static int vme_user_ioctl(struct inode *inode, struct file *file,
 
 			copied = copy_to_user(argp, &master,
 					      sizeof(master));
-			if (copied != 0) {
+			if (copied) {
 				pr_warn("Partial copy to userspace\n");
 				return -EFAULT;
 			}
@@ -351,7 +351,7 @@ static int vme_user_ioctl(struct inode *inode, struct file *file,
 			}
 
 			copied = copy_from_user(&master, argp, sizeof(master));
-			if (copied != 0) {
+			if (copied) {
 				pr_warn("Partial copy from userspace\n");
 				return -EFAULT;
 			}
@@ -381,7 +381,7 @@ static int vme_user_ioctl(struct inode *inode, struct file *file,
 
 			copied = copy_to_user(argp, &slave,
 					      sizeof(slave));
-			if (copied != 0) {
+			if (copied) {
 				pr_warn("Partial copy to userspace\n");
 				return -EFAULT;
 			}
@@ -391,7 +391,7 @@ static int vme_user_ioctl(struct inode *inode, struct file *file,
 		case VME_SET_SLAVE:
 
 			copied = copy_from_user(&slave, argp, sizeof(slave));
-			if (copied != 0) {
+			if (copied) {
 				pr_warn("Partial copy from userspace\n");
 				return -EFAULT;
 			}
@@ -758,7 +758,7 @@ static int __init vme_user_init(void)
 	 * we just change the code in vme_user_match().
 	 */
 	retval = vme_register_driver(&vme_user_driver, VME_MAX_SLOTS);
-	if (retval != 0)
+	if (retval)
 		goto err_reg;
 
 	return retval;
