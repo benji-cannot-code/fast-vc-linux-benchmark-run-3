89a6ce732e1a915d987aa719b7a2e08fd46f3219
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2015-11-23 10:37:12 +0000

    xen/arm: introduce HYPERVISOR_platform_op on arm and arm64
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- arch/arm/include/asm/xen/interface.h
+++ arch/arm/include/asm/xen/interface.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		(hnd).p = val;				\
 	} while (0)
 
+#define __HYPERVISOR_platform_op_raw __HYPERVISOR_platform_op
+
 #ifndef __ASSEMBLY__
 /* Explicitly size integers that represent pfns in the interface with
  * Xen so that we can have one ABI that works for 32 and 64 bit guests.
