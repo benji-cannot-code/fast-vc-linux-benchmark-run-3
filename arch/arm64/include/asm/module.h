8290584663c0e2dd1fc3e26bbe5e771c26a6f588
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-11-24 12:37:35 +0100

    arm64: add support for module PLTs
    This adds support for emitting PLTs at module load time for relative
    branches that are out of range. This is a prerequisite for KASLR, which
    may place the kernel and the modules anywhere in the vmalloc area,
    making it more likely that branch target offsets exceed the maximum
    range of +/- 128 MB.
    
    In this version, I removed the distinction between relocations against
    .init executable sections and ordinary executable sections. The reason
    is that it is hardly worth the trouble, given that .init.text usually
    does not contain that many far branches, and this version now only
    reserves PLT entry space for jump and call relocations against undefined
    symbols (since symbols defined in the same module can be assumed to be
    within +/- 128 MB)
    
    For example, the mac80211.ko module (which is fairly sizable at ~400 KB)
    built with -mcmodel=large gives the following relocation counts:
    
                        relocs    branches   unique     !local
      .text              3925       3347       518        219
      .init.text           11          8         7          1
      .exit.text            4          4         4          1
      .text.unlikely       81         67        36         17
    
    ('unique' means branches to unique type/symbol/addend combos, of which
    !local is the subset referring to undefined symbols)
    
    IOW, we are only emitting a single PLT entry for the .init sections, and
    we are better off just adding it to the core PLT section instead.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/module.h
+++ arch/arm64/include/asm/module.h
@@ -21,4 +21,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MODULE_ARCH_VERMAGIC	"aarch64"
 
+#ifdef CONFIG_ARM64_MODULE_PLTS
+struct mod_arch_specific {
+	struct elf64_shdr	*plt;
+	int			plt_num_entries;
+	int			plt_max_entries;
+};
+#endif
+
+u64 module_emit_plt_entry(struct module *mod, const Elf64_Rela *rela,
+			  Elf64_Sym *sym);
+
 #endif /* __ASM_MODULE_H */
