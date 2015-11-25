a86fc3f5c6a57a60adf036e6f56d96361e688687
Arnd Bergmann <arnd@arndb.de>
2015-11-25 17:32:18 +0100

    ARM: realview: remove private barrier implementation
    The realview barrier implementation tries to avoid calling outer_sync in order
    to not lock up as a result of a bug in the l220 cache controller.
    
    This gets in the way of the multiplatform support, but we can still remove
    it if we make sure that the outer_sync function never gets called, by replacing
    the function pointer with NULL, right after initialization.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    [Fixed up header inclusions]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-realview/realview_pb11mp.c
+++ arch/arm/mach-realview/realview_pb11mp.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/flash.h>
 #include <asm/mach/map.h>
 #include <asm/mach/time.h>
+#include <asm/outercache.h>
 
 #include <mach/board-pb11mp.h>
 #include <mach/irqs.h>
@@ -346,6 +347,11 @@ static void __init realview_pb11mp_init(void)
 	 * Bits:  .... ...0 0111 1001 0000 .... .... ....
 	 */
 	l2x0_init(__io_address(REALVIEW_TC11MP_L220_BASE), 0x00790000, 0xfe000fff);
+	/*
+	 * due to a bug in the l220 cache controller, we must not call
+	 * the sync function. stub it out here instead!
+	 */
+	outer_cache.sync = NULL;
 #endif
 
 	realview_flash_register(realview_pb11mp_flash_resource,
