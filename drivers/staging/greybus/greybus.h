bf20172f4f1115a6f1fd33f7b35a202c35d8a449
Johan Hovold <johan@hovoldconsulting.com>
2015-11-25 15:59:21 +0100

    greybus: kill the endo
    Remove the now unused endo and module code.
    
    Note that the never-implemented serial and version attributes of the
    endo can be implemented as svc attributes if needed.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -27,10 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "greybus_protocols.h"
 #include "manifest.h"
 #include "hd.h"
-#include "endo.h"
 #include "svc.h"
 #include "firmware.h"
-#include "module.h"
 #include "control.h"
 #include "interface.h"
 #include "bundle.h"
@@ -106,8 +104,6 @@ struct dentry *gb_debugfs_get(void);
 extern struct bus_type greybus_bus_type;
 
 extern struct device_type greybus_hd_type;
-extern struct device_type greybus_endo_type;
-extern struct device_type greybus_module_type;
 extern struct device_type greybus_interface_type;
 extern struct device_type greybus_bundle_type;
 extern struct device_type greybus_svc_type;
@@ -115,16 +111,6 @@ extern struct device_type greybus_svc_type;
 static inline int is_gb_host_device(const struct device *dev)
 {
 	return dev->type == &greybus_hd_type;
-}
-
-static inline int is_gb_endo(const struct device *dev)
-{
-	return dev->type == &greybus_endo_type;
-}
-
-static inline int is_gb_module(const struct device *dev)
-{
-	return dev->type == &greybus_module_type;
 }
 
 static inline int is_gb_interface(const struct device *dev)
--- drivers/staging/greybus/hd.h
+++ drivers/staging/greybus/hd.h
@@ -41,7 +41,6 @@ struct gb_host_device {
 	/* Host device buffer constraints */
 	size_t buffer_size_max;
 
-	struct gb_endo *endo;
 	struct gb_svc *svc;
 	struct gb_connection *svc_connection;
 
