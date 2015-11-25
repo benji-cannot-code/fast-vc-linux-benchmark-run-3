8d65a2fc69bdede856ed8857a0e11588c9b2e95c
Johan Hovold <johan@hovoldconsulting.com>
2015-11-25 15:59:08 +0100

    greybus: svc: register svc device at hello
    Register the svc device upon reception of the HELLO request.
    
    The SVC HELLO request contains the endo id and AP interface id, which
    will be exposed from the svc device rather than the endo.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/greybus.h
+++ drivers/staging/greybus/greybus.h
@@ -110,6 +110,7 @@ extern struct device_type greybus_endo_type;
 extern struct device_type greybus_module_type;
 extern struct device_type greybus_interface_type;
 extern struct device_type greybus_bundle_type;
+extern struct device_type greybus_svc_type;
 
 static inline int is_gb_host_device(const struct device *dev)
 {
@@ -134,6 +135,11 @@ static inline int is_gb_interface(const struct device *dev)
 static inline int is_gb_bundle(const struct device *dev)
 {
 	return dev->type == &greybus_bundle_type;
+}
+
+static inline int is_gb_svc(const struct device *dev)
+{
+	return dev->type == &greybus_svc_type;
 }
 
 static inline bool cport_id_valid(struct gb_host_device *hd, u16 cport_id)
--- drivers/staging/greybus/svc.h
+++ drivers/staging/greybus/svc.h
@@ -11,7 +11,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SVC_H
 #define __SVC_H
 
-struct gb_svc;
+enum gb_svc_state {
+	GB_SVC_STATE_RESET,
+	GB_SVC_STATE_PROTOCOL_VERSION,
+	GB_SVC_STATE_SVC_HELLO,
+};
+
+struct gb_svc {
+	struct device		dev;
+
+	struct gb_connection	*connection;
+	enum gb_svc_state	state;
+	struct ida		device_id_map;
+};
+#define to_gb_svc(d) container_of(d, struct gb_svc, d)
 
 int gb_svc_intf_reset(struct gb_svc *svc, u8 intf_id);
 int gb_svc_connection_create(struct gb_svc *svc, u8 intf1_id, u16 cport1_id,
