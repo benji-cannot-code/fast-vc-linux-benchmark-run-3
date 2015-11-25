a5855c905fb4b7e977ccbc1228348884a06e13b8
Johan Hovold <johan@hovoldconsulting.com>
2015-11-25 15:59:15 +0100

    greybus: connection: handle static connections
    Use host-device device and connection name for log messages rather than
    assume that all connections have a bundle (e.g. not true for static
    connections).
    
    Note that the "initial" svc connection has never had a bundle.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/connection.c
+++ drivers/staging/greybus/connection.c
@@ -209,8 +209,8 @@ gb_connection_create(struct gb_host_device *hd, int hd_cport_id,
 
 	retval = gb_connection_bind_protocol(connection);
 	if (retval) {
-		dev_err(&hd->dev, "%d: failed to bind protocol: %d\n",
-			cport_id, retval);
+		dev_err(&hd->dev, "%s: failed to bind protocol: %d\n",
+			connection->name, retval);
 		gb_connection_destroy(connection);
 		return NULL;
 	}
@@ -321,8 +321,9 @@ gb_connection_svc_connection_create(struct gb_connection *connection)
 			connection->intf_cport_id,
 			intf->boot_over_unipro);
 	if (ret) {
-		dev_err(&connection->bundle->dev,
-			"failed to create svc connection: %d\n", ret);
+		dev_err(&connection->hd->dev,
+			"%s: failed to create svc connection: %d\n",
+			connection->name, ret);
 		return ret;
 	}
 
@@ -514,8 +515,9 @@ void gb_connection_latency_tag_enable(struct gb_connection *connection)
 
 	ret = hd->driver->latency_tag_enable(hd, connection->hd_cport_id);
 	if (ret) {
-		dev_err(&connection->bundle->dev,
-			"failed to enable latency tag: %d\n", ret);
+		dev_err(&connection->hd->dev,
+			"%s: failed to enable latency tag: %d\n",
+			connection->name, ret);
 	}
 }
 EXPORT_SYMBOL_GPL(gb_connection_latency_tag_enable);
@@ -530,8 +532,9 @@ void gb_connection_latency_tag_disable(struct gb_connection *connection)
 
 	ret = hd->driver->latency_tag_disable(hd, connection->hd_cport_id);
 	if (ret) {
-		dev_err(&connection->bundle->dev,
-			"failed to disable latency tag: %d\n", ret);
+		dev_err(&connection->hd->dev,
+			"%s: failed to disable latency tag: %d\n",
+			connection->name, ret);
 	}
 }
 EXPORT_SYMBOL_GPL(gb_connection_latency_tag_disable);
