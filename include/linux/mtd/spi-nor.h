cfb1c641b222dfe51f424f88c1b98467bd43125d
Rafał Miłecki <zajec5@gmail.com>
2015-11-26 09:05:04 +0100

    mtd: spi-nor: include mtd.h header for struct mtd_info definition
    So far struct spi_nor was using just a pointer to struct mtd_info so it
    wasn't needed to have it fully defined there. After recent change we
    embed whole struct so we need to include a proper header.
    
    Fixes: 1976367173a4 ("mtd: spi-nor: embed struct mtd_info within struct spi_nor")
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- include/linux/mtd/spi-nor.h
+++ include/linux/mtd/spi-nor.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/bitops.h>
 #include <linux/mtd/cfi.h>
+#include <linux/mtd/mtd.h>
 
 /*
  * Manufacturer IDs
@@ -117,8 +118,6 @@ enum spi_nor_ops {
 enum spi_nor_option_flags {
 	SNOR_F_USE_FSR		= BIT(0),
 };
-
-struct mtd_info;
 
 /**
  * struct spi_nor - Structure for defining a the SPI NOR layer
