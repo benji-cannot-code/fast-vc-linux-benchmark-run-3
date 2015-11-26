64cd0284638a07e053b8cc76dedced4a590a6d9c
Jaedon Shin <jaedon.shin@gmail.com>
2015-11-26 11:56:33 +0900

    phy: phy_brcmstb_sata: remove duplicate definitions
    Remove duplicate definitions.
    
    Signed-off-by: Jaedon Shin <jaedon.shin@gmail.com>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Brian Norris <computersforpeace@gmail.com>
    Tested-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/phy-brcmstb-sata.c
+++ drivers/phy/phy-brcmstb-sata.c
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define SATA_MDIO_BANK_OFFSET				0x23c
 #define SATA_MDIO_REG_OFFSET(ofs)			((ofs) * 4)
-#define SATA_MDIO_REG_SPACE_SIZE			0x1000
-#define SATA_MDIO_REG_LENGTH				0x1f00
 
 #define MAX_PORTS					2
 
