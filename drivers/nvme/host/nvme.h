347f6b0b0aeb6ee3b4ca13275dc55988e6b095c8
Christoph Hellwig <hch@lst.de>
2015-11-26 12:58:11 +0100

    nvme: add NVME_SC_CANCELLED
    To properly document how we are using a negative Linux error value to
    communicate request cancellations inside the driver.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -20,6 +20,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kref.h>
 #include <linux/blk-mq.h>
 
+enum {
+	/*
+	 * Driver internal status code for commands that were cancelled due
+	 * to timeouts or controller shutdown.  The value is negative so
+	 * that it a) doesn't overlap with the unsigned hardware error codes,
+	 * and b) can easily be tested for.
+	 */
+	NVME_SC_CANCELLED		= -EINTR,
+};
+
 extern unsigned char nvme_io_timeout;
 #define NVME_IO_TIMEOUT	(nvme_io_timeout * HZ)
 
