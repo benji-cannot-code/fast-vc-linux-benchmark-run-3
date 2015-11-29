0584a8895cdc3d3d5da5bcd220ed1e41404a8471
Axel Lin <axel.lin@ingics.com>
2015-11-29 15:02:21 +0800

    regulator: lm363x: Staticise ldo_cont_enable_time
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/lm363x-regulator.c
+++ drivers/regulator/lm363x-regulator.c
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LM363X_STEP_50mV		50000
 #define LM363X_STEP_500mV		500000
 
-const int ldo_cont_enable_time[] = {
+static const int ldo_cont_enable_time[] = {
 	0, 2000, 5000, 10000, 20000, 50000, 100000, 200000,
 };
 
