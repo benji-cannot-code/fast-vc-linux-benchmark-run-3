29f6b912daf5de3cba20393bd39b8e9ec8e022b7
David Sterba <dsterba@suse.com>
2015-11-30 11:02:31 +0100

    btrfs: cleanup, remove stray return statements
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/raid56.c
+++ fs/btrfs/raid56.c
@@ -504,7 +504,6 @@ static void cache_rbio(struct btrfs_raid_bio *rbio)
 	}
 
 	spin_unlock_irqrestore(&table->cache_lock, flags);
-	return;
 }
 
 /*
@@ -907,7 +906,6 @@ static void raid_write_end_io(struct bio *bio)
 		err = -EIO;
 
 	rbio_orig_end_io(rbio, err);
-	return;
 }
 
 /*
