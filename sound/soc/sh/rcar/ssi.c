db971c4878cbd0d6eacb506962f415dc4232bac6
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2015-11-30 08:53:04 +0000

    ASoC: rsnd: set SSIWSR setting on rsnd_ssi_config_init()
    It will have TDM settings on SSIWSR. Actually, we would like to set
    it on rsnd_ssi_config_init(), but we can't. Because SSI might be used
    as clock master (It doesn't need to call rsnd_ssi_config_init() when
    clock master mode).
    This patch adds new ssi->wsr and set it on rsnd_ssi_start().
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rcar/ssi.c
+++ sound/soc/sh/rcar/ssi.c
@@ -70,6 +70,7 @@ struct rsnd_ssi {
 	u32 cr_own;
 	u32 cr_clk;
 	u32 cr_mode;
+	u32 wsr;
 	int chan;
 	int rate;
 	int err;
@@ -215,10 +216,9 @@ static int rsnd_ssi_master_clk_start(struct rsnd_ssi *ssi,
 		if (0 == ret) {
 			ssi->cr_clk	= FORCE | SWL_32 |
 				SCKD | SWSD | CKDV(j);
+			ssi->wsr = CONT;
 
 			ssi->rate = rate;
-
-			rsnd_mod_write(mod, SSIWSR, CONT);
 
 			dev_dbg(dev, "%s[%d] outputs %u Hz\n",
 				rsnd_mod_name(mod),
@@ -422,6 +422,7 @@ static int __rsnd_ssi_start(struct rsnd_mod *mod,
 		EN;
 
 	rsnd_mod_write(mod, SSICR, cr);
+	rsnd_mod_write(mod, SSIWSR, ssi->wsr);
 
 	return 0;
 }
