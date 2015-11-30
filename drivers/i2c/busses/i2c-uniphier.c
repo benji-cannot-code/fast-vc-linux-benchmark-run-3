0786d12ec1379eca2715f97a7c08bf97f71ff717
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-11-30 18:53:33 +0900

    i2c: uniphier: error out if clock rate is zero
    This input clock is used to generate the sampling clock for I2C bus.
    If the clock rate is zero, there is something wrong with the clock
    driver.  Bail out with the appropriate error message in such a case.
    It would make it easier to find the root cause of failure.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-uniphier.c
+++ drivers/i2c/busses/i2c-uniphier.c
@@ -343,6 +343,10 @@ static int uniphier_i2c_clk_init(struct device *dev,
 		return ret;
 
 	clk_rate = clk_get_rate(priv->clk);
+	if (!clk_rate) {
+		dev_err(dev, "input clock rate should not be zero\n");
+		return -EINVAL;
+	}
 
 	uniphier_i2c_reset(priv, true);
 
@@ -389,7 +393,7 @@ static int uniphier_i2c_probe(struct platform_device *pdev)
 
 	ret = uniphier_i2c_clk_init(dev, priv);
 	if (ret)
-		return ret;
+		goto err;
 
 	ret = devm_request_irq(dev, irq, uniphier_i2c_interrupt, 0, pdev->name,
 			       priv);
