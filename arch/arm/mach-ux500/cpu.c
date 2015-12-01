61b6d6639f47f4912be5605e4294436c78684403
Linus Walleij <linus.walleij@linaro.org>
2015-12-01 15:00:24 +0100

    ARM/clocksource: use automatic DT probing for ux500 PRCMU
    The ARM core kernel already calls clocksource_of_init() so why
    go to all the trouble of locating and probing this node in the
    machine. CLOCKSOURCE_OF_DECLARE() will take care of it in the
    clocksource driver, and thus we can also get rid of the
    dangling header file <linux/clksrc-dbx500-prcmu.h>
    
    Suggested-by: Arnd Bergmann <arndb@linaro.org>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ux500/cpu.c
+++ arch/arm/mach-ux500/cpu.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/io.h>
 #include <linux/mfd/dbx500-prcmu.h>
-#include <linux/clksrc-dbx500-prcmu.h>
 #include <linux/sys_soc.h>
 #include <linux/err.h>
 #include <linux/slab.h>
