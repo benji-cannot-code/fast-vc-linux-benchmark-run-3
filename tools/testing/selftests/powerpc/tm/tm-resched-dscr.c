c2255f8ad7229597daeb8c0073a88e742dc93a41
Michael Ellerman <mpe@ellerman.id.au>
2015-12-02 16:00:04 +1100

    selftests/powerpc: Skip tm-resched-dscr if we don't have TM
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- tools/testing/selftests/powerpc/tm/tm-resched-dscr.c
+++ tools/testing/selftests/powerpc/tm/tm-resched-dscr.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/tm.h>
 
 #include "utils.h"
+#include "tm.h"
 
 #define TBEGIN          ".long 0x7C00051D ;"
 #define TEND            ".long 0x7C00055D ;"
@@ -42,6 +43,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int test_body(void)
 {
 	uint64_t rv, dscr1 = 1, dscr2, texasr;
+
+	SKIP_IF(!have_htm());
 
 	printf("Check DSCR TM context switch: ");
 	fflush(stdout);
