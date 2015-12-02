c4bbde068c34e5e78ce6b781c620f99f4273b35a
Ira Weiny <ira.weiny@intel.com>
2015-12-02 00:43:26 -0500

    staging/rdma/hfi1: diag.c use BIT macros
    Use BIT macros rather than shifts.
    
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rdma/hfi1/diag.c
+++ drivers/staging/rdma/hfi1/diag.c
@@ -79,8 +79,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	hfi1_cdbg(SNOOP, fmt, ##__VA_ARGS__)
 
 /* Snoop option mask */
-#define SNOOP_DROP_SEND	(1 << 0)
-#define SNOOP_USE_METADATA	(1 << 1)
+#define SNOOP_DROP_SEND		BIT(0)
+#define SNOOP_USE_METADATA	BIT(1)
 
 static u8 snoop_flags;
 
