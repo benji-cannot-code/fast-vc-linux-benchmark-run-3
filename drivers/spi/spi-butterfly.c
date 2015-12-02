4336dcc5cfb63b4d87e5636912968fda95965905
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-12-02 19:17:50 +0530

    spi: butterfly: correct alignment
    checkpatch complains about the allignment with open parenthesis.
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-butterfly.c
+++ drivers/spi/spi-butterfly.c
@@ -144,9 +144,8 @@ static void butterfly_chipselect(struct spi_device *spi, int value)
 #include "spi-bitbang-txrx.h"
 
 static u32
-butterfly_txrx_word_mode0(struct spi_device *spi,
-		unsigned nsecs,
-		u32 word, u8 bits)
+butterfly_txrx_word_mode0(struct spi_device *spi, unsigned nsecs, u32 word,
+			  u8 bits)
 {
 	return bitbang_txrx_be_cpha0(spi, nsecs, 0, 0, word, bits);
 }
@@ -224,8 +223,8 @@ static void butterfly_attach(struct parport *p)
 	 */
 	pp->port = p;
 	pd = parport_register_device(p, "spi_butterfly",
-			NULL, NULL, NULL,
-			0 /* FLAGS */, pp);
+				     NULL, NULL, NULL,
+				     0 /* FLAGS */, pp);
 	if (!pd) {
 		status = -ENOMEM;
 		goto clean0;
@@ -276,7 +275,7 @@ static void butterfly_attach(struct parport *p)
 	pp->dataflash = spi_new_device(pp->bitbang.master, &pp->info[0]);
 	if (pp->dataflash)
 		pr_debug("%s: dataflash at %s\n", p->name,
-				dev_name(&pp->dataflash->dev));
+			 dev_name(&pp->dataflash->dev));
 
 	pr_info("%s: AVR Butterfly\n", p->name);
 	butterfly = pp;
