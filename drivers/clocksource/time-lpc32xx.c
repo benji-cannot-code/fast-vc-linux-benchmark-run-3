e7ff940528e9f808413e2ed4b57be1b862f6c4a1
Vladimir Zapolskiy <vz@mleia.com>
2015-12-02 08:02:08 +0200

    clocksource/drivers/lpc32: Correct pr_err() output format
    If by some reason timerclk is not available, both clockevent and
    clocksource initializations correctly exit, but output of errno to
    kernel log buffer may be confusing:
    
      lpc32xx_clk_init: failed to map system control block registers
      lpc32xx_clocksource_init: clock get failed (4294966779)
      lpc32xx_clockevent_init: clock get failed (4294966779)
    
    Use signed integer output in the correspondent pr_err() string formats:
    
      lpc32xx_clocksource_init: clock get failed (-517)
      lpc32xx_clockevent_init: clock get failed (-517)
    
    Signed-off-by: Vladimir Zapolskiy <vz@mleia.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/time-lpc32xx.c
+++ drivers/clocksource/time-lpc32xx.c
@@ -126,7 +126,7 @@ static int __init lpc32xx_clocksource_init(struct device_node *np)
 
 	clk = of_clk_get_by_name(np, "timerclk");
 	if (IS_ERR(clk)) {
-		pr_err("clock get failed (%lu)\n", PTR_ERR(clk));
+		pr_err("clock get failed (%ld)\n", PTR_ERR(clk));
 		return PTR_ERR(clk);
 	}
 
@@ -185,7 +185,7 @@ static int __init lpc32xx_clockevent_init(struct device_node *np)
 
 	clk = of_clk_get_by_name(np, "timerclk");
 	if (IS_ERR(clk)) {
-		pr_err("clock get failed (%lu)\n", PTR_ERR(clk));
+		pr_err("clock get failed (%ld)\n", PTR_ERR(clk));
 		return PTR_ERR(clk);
 	}
 
