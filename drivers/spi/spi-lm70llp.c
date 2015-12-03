a6c975ea24ae734badacbf01d03b14e7cf64df74
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-12-03 18:29:53 +0530

    spi: lm70llp: remove multiple blank lines
    checkpatch complains about multiple blank lines.
    
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-lm70llp.c
+++ drivers/spi/spi-lm70llp.c
@@ -24,10 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysfs.h>
 #include <linux/workqueue.h>
 
-
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_bitbang.h>
-
 
 /*
  * The LM70 communicates with a host processor using a 3-wire variant of
@@ -88,7 +86,6 @@ struct spi_lm70llp {
 
 /* REVISIT : ugly global ; provides "exclusive open" facility */
 static struct spi_lm70llp *lm70llp;
-
 
 /*-------------------------------------------------------------------*/
 
@@ -319,7 +316,6 @@ static void spi_lm70llp_detach(struct parport *p)
 
 	lm70llp = NULL;
 }
-
 
 static struct parport_driver spi_lm70llp_drv = {
 	.name =		DRVNAME,
