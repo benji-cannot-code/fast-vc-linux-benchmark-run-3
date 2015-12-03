86cafb0322be8b60017f7d5e7bc9769bbbf661d8
Takashi Iwai <tiwai@suse.de>
2015-12-03 17:19:31 +0100

    ALSA: Fix compat_ioctl handling for OSS emulations
    The ALSA PCM, mixer and sequencer OSS emulations provide the 32bit
    compatible ioctl, but they just call the 64bit native ioctl as is.
    Although this works in most cases, passing the argument value as-is
    isn't guaranteed to work on all architectures.  We need to convert it
    via compat_ptr() instead.
    
    This patch addresses the missing conversions.  Since all relevant
    ioctls in these functions take the argument as a pointer, we do the
    pointer conversion in each compat_ioctl and pass it as a 64bit value
    to the native ioctl.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/oss/pcm_oss.c
+++ sound/core/oss/pcm_oss.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/math64.h>
 #include <linux/string.h>
+#include <linux/compat.h>
 #include <sound/core.h>
 #include <sound/minors.h>
 #include <sound/pcm.h>
@@ -2649,7 +2650,11 @@ static long snd_pcm_oss_ioctl(struct file *file, unsigned int cmd, unsigned long
 
 #ifdef CONFIG_COMPAT
 /* all compatible */
-#define snd_pcm_oss_ioctl_compat	snd_pcm_oss_ioctl
+static long snd_pcm_oss_ioctl_compat(struct file *file, unsigned int cmd,
+				     unsigned long arg)
+{
+	return snd_pcm_oss_ioctl(file, cmd, (unsigned long)compat_ptr(arg));
+}
 #else
 #define snd_pcm_oss_ioctl_compat	NULL
 #endif
