dc953b03e9b7ad967659a96fba6e581830c04ed4
Robin Murphy <robin.murphy@arm.com>
2015-12-04 17:53:01 +0000

    iommu/io-pgtable: Make io_pgtable_ops_to_pgtable() macro common
    There is no need to keep a useful accessor for a public structure hidden
    away in a private implementation. Move it out alongside the structure
    definition so that other implementations may reuse it.
    
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- drivers/iommu/io-pgtable-arm.c
+++ drivers/iommu/io-pgtable-arm.c
@@ -39,9 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define io_pgtable_to_data(x)						\
 	container_of((x), struct arm_lpae_io_pgtable, iop)
 
-#define io_pgtable_ops_to_pgtable(x)					\
-	container_of((x), struct io_pgtable, ops)
-
 #define io_pgtable_ops_to_data(x)					\
 	io_pgtable_to_data(io_pgtable_ops_to_pgtable(x))
 
