5b0a7b11d07bafc805aa608084deb51fd2212ff5
Viresh Kumar <viresh.kumar@linaro.org>
2015-12-04 21:30:09 +0530

    greybus: don't use %h and %hh for printing short and char variables
    Because the width of our fields is already known, we can use %0Nx (for
    hex) to print N bytes and %u (for unsigned decimal), instead of using %h
    and %hh, which isn't that readable.
    
    This patch makes following changes:
    - s/%hx/%04x
    - s/%04hx/%04x
    - s/%hhx/%02x
    - s/%02hhx/%02x
    - s/%hhu/%u
    - s/%hu/%u
    - s/%x/%02x for u8 value (only at a single place)
    
    Suggested-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/operation.c
+++ drivers/staging/greybus/operation.c
@@ -230,7 +230,7 @@ static void gb_operation_request_handle(struct gb_operation *operation)
 		status = protocol->request_recv(operation->type, operation);
 	} else {
 		dev_err(&connection->hd->dev,
-			"%s: unexpected incoming request of type 0x%02hhx\n",
+			"%s: unexpected incoming request of type 0x%02x\n",
 			connection->name, operation->type);
 
 		status = -EPROTONOSUPPORT;
@@ -239,7 +239,7 @@ static void gb_operation_request_handle(struct gb_operation *operation)
 	ret = gb_operation_response_send(operation, status);
 	if (ret) {
 		dev_err(&connection->hd->dev,
-			"%s: failed to send response %d for type 0x%02hhx: %d\n",
+			"%s: failed to send response %d for type 0x%02x: %d\n",
 			connection->name, status, operation->type, ret);
 			return;
 	}
@@ -798,7 +798,7 @@ void greybus_message_sent(struct gb_host_device *hd,
 	if (message == operation->response) {
 		if (status) {
 			dev_err(&connection->hd->dev,
-				"%s: error sending response 0x%02hhx: %d\n",
+				"%s: error sending response 0x%02x: %d\n",
 				connection->name, operation->type, status);
 		}
 		gb_operation_put_active(operation);
@@ -869,7 +869,7 @@ static void gb_connection_recv_response(struct gb_connection *connection,
 	operation = gb_operation_find_outgoing(connection, operation_id);
 	if (!operation) {
 		dev_err(&connection->hd->dev,
-			"%s: unexpected response id 0x%04hx received\n",
+			"%s: unexpected response id 0x%04x received\n",
 			connection->name, operation_id);
 		return;
 	}
@@ -878,7 +878,7 @@ static void gb_connection_recv_response(struct gb_connection *connection,
 	message_size = sizeof(*message->header) + message->payload_size;
 	if (!errno && size != message_size) {
 		dev_err(&connection->hd->dev,
-			"%s: malformed response 0x%02hhx received (%zu != %zu)\n",
+			"%s: malformed response 0x%02x received (%zu != %zu)\n",
 			connection->name, message->header->type, size,
 			message_size);
 		errno = -EMSGSIZE;
@@ -927,7 +927,7 @@ void gb_connection_recv(struct gb_connection *connection,
 	msg_size = le16_to_cpu(header.size);
 	if (size < msg_size) {
 		dev_err(dev,
-			"%s: incomplete message 0x%04hx of type 0x%02hhx received (%zu < %zu)\n",
+			"%s: incomplete message 0x%04x of type 0x%02x received (%zu < %zu)\n",
 			connection->name, le16_to_cpu(header.operation_id),
 			header.type, size, msg_size);
 		return;		/* XXX Should still complete operation */
@@ -1037,7 +1037,7 @@ int gb_operation_sync_timeout(struct gb_connection *connection, int type,
 	ret = gb_operation_request_send_sync_timeout(operation, timeout);
 	if (ret) {
 		dev_err(&connection->hd->dev,
-			"%s: synchronous operation of type 0x%02hhx failed: %d\n",
+			"%s: synchronous operation of type 0x%02x failed: %d\n",
 			connection->name, type, ret);
 	} else {
 		if (response_size) {
