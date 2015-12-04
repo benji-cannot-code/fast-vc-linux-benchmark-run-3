e6892e7850408be128acfa43f5dc2f8c5eed6cf5
Jisheng Zhang <jszhang@marvell.com>
2015-12-04 21:57:00 +0800

    phy: berlin-usb: remove non-necessary header files
    We don't need gpio related header files, so remove them.
    
    Signed-off-by: Jisheng Zhang <jszhang@marvell.com>
    Acked-by: Antoine Tenart <antoine.tenart@free-electrons.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/phy-berlin-usb.c
+++ drivers/phy/phy-berlin-usb.c
@@ -10,11 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * warranty of any kind, whether express or implied.
  */
 
-#include <linux/gpio.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
-#include <linux/of_gpio.h>
 #include <linux/phy/phy.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
