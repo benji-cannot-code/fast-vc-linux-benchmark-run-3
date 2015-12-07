78019f7912aee61de10936e5a5af695f6b115cf1
Tejun Heo <tj@kernel.org>
2015-12-07 17:38:54 -0500

    netfilter: prepare xt_cgroup for multi revisions
    xt_cgroup will grow cgroup2 path based match.  Postfix existing
    symbols with _v0 and prepare for multi revision registration.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Daniel Wagner <daniel.wagner@bmw-carit.de>
    CC: Neil Horman <nhorman@tuxdriver.com>
    Cc: Jan Engelhardt <jengelh@inai.de>
    Cc: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/uapi/linux/netfilter/xt_cgroup.h
+++ include/uapi/linux/netfilter/xt_cgroup.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-struct xt_cgroup_info {
+struct xt_cgroup_info_v0 {
 	__u32 id;
 	__u32 invert;
 };
