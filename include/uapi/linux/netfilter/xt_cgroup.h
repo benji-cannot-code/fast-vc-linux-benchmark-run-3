6aabe8bd10a894a0f64ed0b079e92b55ca355ba6
Tejun Heo <tj@kernel.org>
2015-12-07 17:38:55 -0500

    netfilter: implement xt_cgroup cgroup2 path match
    This patch implements xt_cgroup path match which matches cgroup2
    membership of the associated socket.  The match is recursive and
    invertible.
    
    For rationales on introducing another cgroup based match, please refer
    to a preceding commit "sock, cgroup: add sock->sk_cgroup".
    
    v3: Folded into xt_cgroup as a new revision interface as suggested by
        Pablo.
    
    v2: Included linux/limits.h from xt_cgroup2.h for PATH_MAX.  Added
        explicit alignment to the priv field.  Both suggested by Jan.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Daniel Wagner <daniel.wagner@bmw-carit.de>
    CC: Neil Horman <nhorman@tuxdriver.com>
    Cc: Jan Engelhardt <jengelh@inai.de>
    Cc: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/uapi/linux/netfilter/xt_cgroup.h
+++ include/uapi/linux/netfilter/xt_cgroup.h
@@ -3,10 +3,23 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _UAPI_XT_CGROUP_H
 
 #include <linux/types.h>
+#include <linux/limits.h>
 
 struct xt_cgroup_info_v0 {
 	__u32 id;
 	__u32 invert;
+};
+
+struct xt_cgroup_info_v1 {
+	__u8		has_path;
+	__u8		has_classid;
+	__u8		invert_path;
+	__u8		invert_classid;
+	char		path[PATH_MAX];
+	__u32		classid;
+
+	/* kernel internal data */
+	void		*priv __attribute__((aligned(8)));
 };
 
 #endif /* _UAPI_XT_CGROUP_H */
