88083d9684aa17f59cfc56141c2e5d15ed68f55e
Johan Hovold <johan@hovoldconsulting.com>
2015-12-07 15:05:34 +0100

    greybus: connection: separate connection creation and enabling
    Separate connection creation from enabling.
    
    This will ultimately allow connection structures to be created while
    parsing manifests, but the connections to not be enabled until a driver
    is bound.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/connection.c
+++ drivers/staging/greybus/connection.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int gb_connection_bind_protocol(struct gb_connection *connection);
 static void gb_connection_unbind_protocol(struct gb_connection *connection);
-static int gb_connection_init(struct gb_connection *connection);
 
 
 static DEFINE_SPINLOCK(gb_connections_lock);
@@ -126,7 +125,6 @@ gb_connection_create(struct gb_host_device *hd, int hd_cport_id,
 	struct gb_connection *connection;
 	struct ida *id_map = &hd->cport_id_map;
 	int ida_start, ida_end;
-	int retval;
 	u8 major = 0;
 	u8 minor = 1;
 
@@ -194,14 +192,6 @@ gb_connection_create(struct gb_host_device *hd, int hd_cport_id,
 		INIT_LIST_HEAD(&connection->bundle_links);
 
 	spin_unlock_irq(&gb_connections_lock);
-
-	retval = gb_connection_init(connection);
-	if (retval) {
-		dev_err(&hd->dev, "%s: failed to initialize connection: %d\n",
-			connection->name, retval);
-		gb_connection_destroy(connection);
-		return NULL;
-	}
 
 	return connection;
 
@@ -397,7 +387,7 @@ static int gb_connection_protocol_get_version(struct gb_connection *connection)
 	return 0;
 }
 
-static int gb_connection_init(struct gb_connection *connection)
+int gb_connection_init(struct gb_connection *connection)
 {
 	int ret;
 
