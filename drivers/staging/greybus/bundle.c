6b5d3373c10ff1a0ab65fe4b8b6cee1abf5044c6
Johan Hovold <johan@hovoldconsulting.com>
2015-12-07 15:05:40 +0100

    greybus: bundle: kill the bundle lock
    Kill the bundle lock, which looked like it protected the interface
    bundle lists but really did not as lock-less manipulations were still
    made.
    
    No locking for the interface bundle list is in fact needed as bundles
    are created along with the interface, and the list is only used to check
    for duplicate bundle ids when parsing the manifest and to destroy the
    bundles when removing the interface itself.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/bundle.c
+++ drivers/staging/greybus/bundle.c
@@ -66,6 +66,19 @@ static struct attribute *bundle_attrs[] = {
 
 ATTRIBUTE_GROUPS(bundle);
 
+static struct gb_bundle *gb_bundle_find(struct gb_interface *intf,
+							u8 bundle_id)
+{
+	struct gb_bundle *bundle;
+
+	list_for_each_entry(bundle, &intf->bundles, links) {
+		if (bundle->id == bundle_id)
+			return bundle;
+	}
+
+	return NULL;
+}
+
 static void gb_bundle_release(struct device *dev)
 {
 	struct gb_bundle *bundle = to_gb_bundle(dev);
@@ -78,9 +91,6 @@ struct device_type greybus_bundle_type = {
 	.name =		"greybus_bundle",
 	.release =	gb_bundle_release,
 };
-
-/* XXX This could be per-host device or per-module */
-static DEFINE_SPINLOCK(gb_bundles_lock);
 
 /*
  * Create a gb_bundle structure to represent a discovered
@@ -128,9 +138,7 @@ struct gb_bundle *gb_bundle_create(struct gb_interface *intf, u8 bundle_id,
 		return NULL;
 	}
 
-	spin_lock_irq(&gb_bundles_lock);
 	list_add(&bundle->links, &intf->bundles);
-	spin_unlock_irq(&gb_bundles_lock);
 
 	return bundle;
 }
@@ -150,25 +158,10 @@ static void gb_bundle_connections_exit(struct gb_bundle *bundle)
  */
 void gb_bundle_destroy(struct gb_bundle *bundle)
 {
-	spin_lock_irq(&gb_bundles_lock);
 	list_del(&bundle->links);
-	spin_unlock_irq(&gb_bundles_lock);
 
 	gb_bundle_connections_exit(bundle);
 	device_unregister(&bundle->dev);
 }
 
-struct gb_bundle *gb_bundle_find(struct gb_interface *intf, u8 bundle_id)
-{
-	struct gb_bundle *bundle;
 
-	spin_lock_irq(&gb_bundles_lock);
-	list_for_each_entry(bundle, &intf->bundles, links)
-		if (bundle->id == bundle_id)
-			goto found;
-	bundle = NULL;
-found:
-	spin_unlock_irq(&gb_bundles_lock);
-
-	return bundle;
-}
