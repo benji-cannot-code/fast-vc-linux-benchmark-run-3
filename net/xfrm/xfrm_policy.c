2d9cfbc2824ee560becbb6c73d4a41bc17f0259f
Eric Dumazet <edumazet@google.com>
2015-12-07 08:53:17 -0800

    xfrm: take care of request sockets
    TCP SYNACK messages might now be attached to request sockets.
    
    XFRM needs to get back to a listener socket.
    
    Adds new helpers that might be used elsewhere :
    sk_to_full_sk() and sk_const_to_full_sk()
    
    Note: We also need to add RCU protection for xfrm lookups,
    now TCP/DCCP have lockless listener processing. This will
    be addressed in separate patches.
    
    Fixes: ca6fb0651883 ("tcp: attach SYNACK messages to request sockets instead of listener")
    Reported-by: Dave Jones <davej@codemonkey.org.uk>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/xfrm/xfrm_policy.c
+++ net/xfrm/xfrm_policy.c
@@ -2199,6 +2199,7 @@ struct dst_entry *xfrm_lookup(struct net *net, struct dst_entry *dst_orig,
 	xdst = NULL;
 	route = NULL;
 
+	sk = sk_const_to_full_sk(sk);
 	if (sk && sk->sk_policy[XFRM_POLICY_OUT]) {
 		num_pols = 1;
 		pols[0] = xfrm_sk_policy_lookup(sk, XFRM_POLICY_OUT, fl);
@@ -2478,6 +2479,7 @@ int __xfrm_policy_check(struct sock *sk, int dir, struct sk_buff *skb,
 	}
 
 	pol = NULL;
+	sk = sk_to_full_sk(sk);
 	if (sk && sk->sk_policy[dir]) {
 		pol = xfrm_sk_policy_lookup(sk, dir, &fl);
 		if (IS_ERR(pol)) {
