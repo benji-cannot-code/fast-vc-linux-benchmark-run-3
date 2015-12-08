181e389a86cdcf31b168c082c1bdcec7e1a3faf2
Bin Liu <b-liu@ti.com>
2015-12-08 10:31:50 -0600

    usb: phy: phy-am335x: bypass first VBUS sensing for host-only mode
    To prevent VBUS contention, the am335x MUSB phy senses VBUS first before
    transitioning to host mode. However, for host-only mode, VBUS could be
    directly tied to 5V power rail which could prevent MUSB transitions to
    host mode.
    
    This change receives dr_mode of the controller then bypass the first
    VBUS sensing for host-only mode, so that MUSB can work in host mode
    event if VBUS is tied to 5V.
    
    Signed-off-by: Bin Liu <b-liu@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>

--- drivers/usb/phy/phy-am335x-control.h
+++ drivers/usb/phy/phy-am335x-control.h
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _AM335x_PHY_CONTROL_H_
 
 struct phy_control {
-	void (*phy_power)(struct phy_control *phy_ctrl, u32 id, bool on);
+	void (*phy_power)(struct phy_control *phy_ctrl, u32 id,
+			enum usb_dr_mode dr_mode, bool on);
 	void (*phy_wkup)(struct phy_control *phy_ctrl, u32 id, bool on);
 };
 
-static inline void phy_ctrl_power(struct phy_control *phy_ctrl, u32 id, bool on)
+static inline void phy_ctrl_power(struct phy_control *phy_ctrl, u32 id,
+				enum usb_dr_mode dr_mode, bool on)
 {
-	phy_ctrl->phy_power(phy_ctrl, id, on);
+	phy_ctrl->phy_power(phy_ctrl, id, dr_mode, on);
 }
 
 static inline void phy_ctrl_wkup(struct phy_control *phy_ctrl, u32 id, bool on)
