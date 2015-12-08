4a6dd59a20708f30175cfab4b27eb72653e2d091
Arnd Bergmann <arnd@arndb.de>
2015-12-08 23:42:07 +0100

    ARM: pxa: cm-x2xx: avoid duplicate macro warnings
    In an old commit, we worked around the duplicate definition of
    GPIO24_SSP1_SFRM in cm-x2xx.c, which includes files for both
    pxa25x and pxa27x. Apparently the problem has come back and we
    now have four additional duplicate symbols that cause warnings:
    
    In file included from /git/arm-soc/arch/arm/mach-pxa/pxa27x.h:7:0,
                     from /git/arm-soc/arch/arm/mach-pxa/cm-x2xx.c:27:
    /git/arm-soc/arch/arm/mach-pxa/mfp-pxa27x.h:21:0: warning: "GPIO86_GPIO" redefined
     #define GPIO86_GPIO  MFP_CFG_IN(GPIO86, AF0)
    
    This uses the same hack as before and undefines all symbols that
    are defined more than once. Fortunately, cm-x2xx does not need
    any of these.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- arch/arm/mach-pxa/cm-x2xx.c
+++ arch/arm/mach-pxa/cm-x2xx.c
@@ -25,7 +25,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "pxa25x.h"
 #undef GPIO24_SSP1_SFRM
+#undef GPIO86_GPIO
+#undef GPIO87_GPIO
+#undef GPIO88_GPIO
+#undef GPIO89_GPIO
 #include "pxa27x.h"
+#undef GPIO24_SSP1_SFRM
+#undef GPIO86_GPIO
+#undef GPIO87_GPIO
+#undef GPIO88_GPIO
+#undef GPIO89_GPIO
 #include <mach/audio.h>
 #include <linux/platform_data/video-pxafb.h>
 #include <mach/smemc.h>
