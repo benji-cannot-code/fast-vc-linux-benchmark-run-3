4709a0da7129b876dfaf4d3ff73cfbc56ef40f31
Sudip Mukherjee <sudipm.mukherjee@gmail.com>
2015-12-08 14:17:55 +0530

    of: fix declaration of of_io_request_and_map
    We are having build failure with linux-next for sparc allmodconfig with
    the error messages:
    
    drivers/built-in.o: In function `meson6_timer_init':
    meson6_timer.c:(.init.text+0x5fe8): undefined reference to `of_io_request_and_map'
    drivers/built-in.o: In function `mtk_timer_init':
    mtk_timer.c:(.init.text+0x6af0): undefined reference to `of_io_request_and_map'
    drivers/built-in.o: In function `asm9260_timer_init':
    asm9260_timer.c:(.init.text+0x6c48): undefined reference to `of_io_request_and_map'
    
    CONFIG_OF is defined for sparc so it is expected that we have a
    definition of of_io_request_and_map() but of/address.c is only compiled
    if it is !SPARC. In other words, CONFIG_OF_ADDRESS is not defined for
    sparc so we get the build failure.
    
    Fixes: e572f844ca66 ("clocksource/drivers/meson6: Add the COMPILE_TEST option")
    Fixes: bec8c4617611 ("clocksource/drivers/mediatek: Add the COMPILE_TEST option")
    Fixes: 4a373b45f94a ("clocksource/drivers/asm9260: Add the COMPILE_TEST option")
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Sudip Mukherjee <sudip@vectorindia.org>
    [robh: move include of io.h out of ifdef's]
    Signed-off-by: Rob Herring <robh@kernel.org>

--- include/linux/of_address.h
+++ include/linux/of_address.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioport.h>
 #include <linux/errno.h>
 #include <linux/of.h>
+#include <linux/io.h>
 
 struct of_pci_range_parser {
 	struct device_node *node;
@@ -37,6 +38,8 @@ extern struct device_node *of_find_matching_node_by_address(
 					const struct of_device_id *matches,
 					u64 base_address);
 extern void __iomem *of_iomap(struct device_node *device, int index);
+void __iomem *of_io_request_and_map(struct device_node *device,
+				    int index, const char *name);
 
 /* Extract an address from a device, returns the region size and
  * the address space flags too. The PCI version uses a BAR number
@@ -58,6 +61,11 @@ extern int of_dma_get_range(struct device_node *np, u64 *dma_addr,
 				u64 *paddr, u64 *size);
 extern bool of_dma_is_coherent(struct device_node *np);
 #else /* CONFIG_OF_ADDRESS */
+static inline void __iomem *of_io_request_and_map(struct device_node *device,
+						  int index, const char *name)
+{
+	return IOMEM_ERR_PTR(-EINVAL);
+}
 
 static inline u64 of_translate_address(struct device_node *np,
 				       const __be32 *addr)
@@ -113,12 +121,7 @@ static inline bool of_dma_is_coherent(struct device_node *np)
 extern int of_address_to_resource(struct device_node *dev, int index,
 				  struct resource *r);
 void __iomem *of_iomap(struct device_node *node, int index);
-void __iomem *of_io_request_and_map(struct device_node *device,
-					int index, const char *name);
 #else
-
-#include <linux/io.h>
-
 static inline int of_address_to_resource(struct device_node *dev, int index,
 					 struct resource *r)
 {
@@ -128,12 +131,6 @@ static inline int of_address_to_resource(struct device_node *dev, int index,
 static inline void __iomem *of_iomap(struct device_node *device, int index)
 {
 	return NULL;
-}
-
-static inline void __iomem *of_io_request_and_map(struct device_node *device,
-					int index, const char *name)
-{
-	return IOMEM_ERR_PTR(-EINVAL);
 }
 #endif
 
