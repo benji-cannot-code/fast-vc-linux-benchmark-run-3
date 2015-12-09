61913a3e944ac86ff0f2cb40832e0cf7fcc00d2f
Linus Walleij <linus.walleij@linaro.org>
2015-12-09 11:01:33 +0100

    gpio: pca953x: make inclusion of <linux/of_platform.h> unconditional
    After adding the DT matching in
    commit 6f29c9afbe636fc0e35c82a11eaf45c3b85eb07a
    "gpio: pca935x: fix of-only probed devices"
    compilation fails like this:
    
    CC [M]  drivers/gpio/gpio-pca953x.o
    gpio-pca953x.c: In function ‘pca953x_probe’:
    gpio-pca953x.c:693:11: error: implicit declaration of
    function ‘of_match_device’ [-Werror=implicit-function-declaration]
    match = of_match_device(pca953x_dt_ids, &client->dev);
            ^
    gpio-pca953x.c:693:9: warning: assignment makes pointer from
    integer without a cast [-Wint-conversion]
    match = of_match_device(pca953x_dt_ids, &client->dev);
            ^
    cc1: some warnings being treated as errors
    ../scripts/Makefile.build:264: recipe for target
    'drivers/gpio/gpio-pca953x.o' failed
    
    After removing the conditional inclusion guards compilation
    works fine again. Might be a module problem so that
    fix.
    
    Cc: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -19,9 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/platform_data/pca953x.h>
 #include <linux/slab.h>
-#ifdef CONFIG_OF_GPIO
 #include <linux/of_platform.h>
-#endif
 #include <linux/acpi.h>
 
 #define PCA953X_INPUT		0
