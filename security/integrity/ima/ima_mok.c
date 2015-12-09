e88ffb1ebb2a7317a0ada6d4d10efd9e3979d01c
Paul Gortmaker <paul.gortmaker@windriver.com>
2015-12-09 17:37:16 -0500

    security/integrity: make ima/ima_mok.c explicitly non-modular
    The Kconfig currently controlling compilation of this code is:
    
    ima/Kconfig:config IMA_MOK_KEYRING
    ima/Kconfig: bool "Create IMA machine owner keys (MOK) and blacklist keyrings"
    
    ...meaning that it currently is not being built as a module by anyone.
    
    Lets remove the couple of traces of modularity so that when reading the
    driver there is no doubt it really is builtin-only.
    
    Since module_init translates to device_initcall in the non-modular
    case, the init ordering remains unchanged with this commit.
    
    Cc: Mimi Zohar <zohar@linux.vnet.ibm.com>
    Cc: Dmitry Kasatkin <dmitry.kasatkin@gmail.com>
    Cc: James Morris <james.l.morris@oracle.com>
    Cc: "Serge E. Hallyn" <serge@hallyn.com>
    Cc: linux-ima-devel@lists.sourceforge.net
    Cc: linux-ima-user@lists.sourceforge.net
    Cc: linux-security-module@vger.kernel.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/ima/ima_mok.c
+++ security/integrity/ima/ima_mok.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/cred.h>
 #include <linux/err.h>
-#include <linux/module.h>
+#include <linux/init.h>
 #include <keys/asymmetric-type.h>
 
 
@@ -53,5 +53,4 @@ __init int ima_mok_init(void)
 	set_bit(KEY_FLAG_KEEP, &ima_blacklist_keyring->flags);
 	return 0;
 }
-
-module_init(ima_mok_init);
+device_initcall(ima_mok_init);
