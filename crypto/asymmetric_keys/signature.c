039ad58ae06bc23e0b5916181dc9c7266b4a9325
Paul Gortmaker <paul.gortmaker@windriver.com>
2015-12-09 15:05:28 -0500

    crypto: asymmetric_keys - signature.c does not need <module.h>
    This file does not contain any modular related function calls.  So get
    rid of module.h since it drags in a lot of other headers and adds to
    the preprocessing load.  It does export some symbols though, so we'll
    need to ensure it has export.h present instead.
    
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: linux-crypto@vger.kernel.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/asymmetric_keys/signature.c
+++ crypto/asymmetric_keys/signature.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) "SIG: "fmt
 #include <keys/asymmetric-subtype.h>
-#include <linux/module.h>
+#include <linux/export.h>
 #include <linux/err.h>
 #include <crypto/public_key.h>
 #include "asymmetric_keys.h"
