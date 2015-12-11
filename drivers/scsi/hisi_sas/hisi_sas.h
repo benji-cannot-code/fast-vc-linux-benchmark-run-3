3b4bbc7ede72d75c37addef37ce3018a6cf1cab9
John Garry <john.garry@huawei.com>
2015-12-11 20:03:21 +0800

    hisi_sas: use platform_get_irq()
    It is preferred that drivers use platform_get_irq() instead of
    irq_of_parse_and_map(), so replace.
    
    Signed-off-by: John Garry <john.garry@huawei.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
 #include <linux/of_address.h>
-#include <linux/of_irq.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <scsi/libsas.h>
