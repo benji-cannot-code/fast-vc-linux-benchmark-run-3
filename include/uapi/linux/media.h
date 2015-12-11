4058f1166bf051afa4d2c7c59f481c81f5367c77
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2015-12-11 16:07:57 -0200

    [media] media-device: Use u64 ints for pointers
    By using u64 integers and pointers, we can get rid of compat32
    logic. So, let's do it!
    
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Suggested-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/uapi/linux/media.h
+++ include/uapi/linux/media.h
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_MEDIA_H
 #define __LINUX_MEDIA_H
 
+#ifndef __KERNEL__
+#include <stdint.h>
+#endif
 #include <linux/ioctl.h>
 #include <linux/types.h>
 #include <linux/version.h>
@@ -321,26 +324,25 @@ struct media_v2_link {
 };
 
 struct media_v2_topology {
-	__u32 topology_version;
+	__u64 topology_version;
 
-	__u32 num_entities;
-	struct media_v2_entity *entities;
+	__u64 num_entities;
+	__u64 ptr_entities;
 
-	__u32 num_interfaces;
-	struct media_v2_interface *interfaces;
+	__u64 num_interfaces;
+	__u64 ptr_interfaces;
 
-	__u32 num_pads;
-	struct media_v2_pad *pads;
+	__u64 num_pads;
+	__u64 ptr_pads;
 
-	__u32 num_links;
-	struct media_v2_link *links;
-
-	struct {
-		__u32 reserved_num;
-		void *reserved_ptr;
-	} reserved_types[16];
-	__u32 reserved[8];
+	__u64 num_links;
+	__u64 ptr_links;
 };
+
+static inline void __user *media_get_uptr(__u64 arg)
+{
+	return (void __user *)(uintptr_t)arg;
+}
 
 /* ioctls */
 
