1cc18f0685d45ac2051f3fa210d9d2c4aa3ca14f
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2015-12-11 07:23:09 -0200

    [media] media-device: put headers in alphabetic order
    It is better to keep the headers in alphabetic order.
    
    Suggested-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/media-device.c
+++ drivers/media/media-device.c
@@ -25,8 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/export.h>
 #include <linux/ioctl.h>
 #include <linux/media.h>
-#include <linux/types.h>
 #include <linux/slab.h>
+#include <linux/types.h>
 
 #include <media/media-device.h>
 #include <media/media-devnode.h>
