2fcb8c54a1f5d5283ee61f4bc1357b018fc03fc3
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2015-12-15 11:29:45 +0000

    ASoC: wm_adsp: Add support for opening a compressed stream
    Allow user-space to open a compressed stream, although no data will be
    passed yet, as part of this adding the ability to define supported
    capabilities per firmware and check these match the stream being opened.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/wm_adsp.h
+++ sound/soc/codecs/wm_adsp.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <sound/soc.h>
 #include <sound/soc-dapm.h>
+#include <sound/compress_driver.h>
 
 #include "wmfw.h"
 
@@ -30,6 +31,8 @@ struct wm_adsp_alg_region {
 	int type;
 	unsigned int base;
 };
+
+struct wm_adsp_compr;
 
 struct wm_adsp {
 	const char *part;
@@ -59,6 +62,8 @@ struct wm_adsp {
 	struct list_head ctl_list;
 
 	struct work_struct boot_work;
+
+	struct wm_adsp_compr *compr;
 
 	struct mutex pwr_lock;
 
@@ -97,5 +102,13 @@ int wm_adsp2_early_event(struct snd_soc_dapm_widget *w,
 			 struct snd_kcontrol *kcontrol, int event);
 int wm_adsp2_event(struct snd_soc_dapm_widget *w,
 		   struct snd_kcontrol *kcontrol, int event);
+
+extern int wm_adsp_compr_open(struct wm_adsp *dsp,
+			      struct snd_compr_stream *stream);
+extern int wm_adsp_compr_free(struct snd_compr_stream *stream);
+extern int wm_adsp_compr_set_params(struct snd_compr_stream *stream,
+				    struct snd_compr_params *params);
+extern int wm_adsp_compr_get_caps(struct snd_compr_stream *stream,
+				  struct snd_compr_caps *caps);
 
 #endif
