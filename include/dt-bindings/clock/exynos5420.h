e5ce2001789064158639b69ac5b855ca3e51d0fd
Thomas Abraham <thomas.ab@samsung.com>
2015-12-15 18:33:16 +0100

    clk: samsung: exynos5420: add cpu clock configuration data and instantiate cpu clock
    With the addition of the new Samsung specific cpu-clock type, the
    arm clock can be represented as a cpu-clock type. Add the CPU clock
    configuration data and instantiate the CPU clock type for Exynos5420.
    
    Changes by Bartlomiej:
    - split Exynos5420 support from the original patches
    - moved E5420_[EGL,KFC]_DIV0() macros to clk-exynos5420.c
    
    Signed-off-by: Thomas Abraham <thomas.ab@samsung.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>

--- include/dt-bindings/clock/exynos5420.h
+++ include/dt-bindings/clock/exynos5420.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_FOUT_MPLL		10
 #define CLK_FOUT_BPLL		11
 #define CLK_FOUT_KPLL		12
+#define CLK_ARM_CLK		13
+#define CLK_KFC_CLK		14
 
 /* gate for special clocks (sclk) */
 #define CLK_SCLK_UART0		128
