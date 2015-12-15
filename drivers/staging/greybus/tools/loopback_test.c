142cc8c4a6a7246715e7a6ac8ad25c9282681659
Greg Kroah-Hartman <gregkh@google.com>
2015-12-15 13:57:22 -0800

    greybus: loopback_test: null terminate the dict structure
    This lets us test for any number of entries, no need to do an
    ARRAY_SIZE-type comparison.  This fixes a build warning of comparing
    signed/unsigned values.)
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/tools/loopback_test.c
+++ drivers/staging/greybus/tools/loopback_test.c
@@ -35,7 +35,8 @@ struct dict {
 static struct dict dict[] = {
 	{"ping", 2},
 	{"transfer", 3},
-	{"sink", 4}
+	{"sink", 4},
+	{NULL,}		/* list termination */
 };
 
 struct loopback_results {
@@ -806,7 +807,7 @@ void loopback_run(struct loopback_test *t)
 	int i;
 	int ret;
 
-	for (i = 0; i < sizeof(dict) / sizeof(struct dict); i++) {
+	for (i = 0; dict[i].name != NULL; i++) {
 		if (strstr(dict[i].name, t->test_name))
 			t->test_id = dict[i].type;
 	}
