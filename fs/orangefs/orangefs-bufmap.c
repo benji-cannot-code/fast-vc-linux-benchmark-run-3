9fada0c53ed5db52a8c42b28d1d556a5cf675dee
Martin Brandenburg <martin@omnibond.com>
2015-12-15 14:45:12 -0500

    orangefs: Change visibility of several bufmap helpers to static.
    Signed-off-by: Mike Marshall <hubcap@omnibond.com>
    Signed-off-by: Martin Brandenburg <martin@omnibond.com>

--- fs/orangefs/orangefs-bufmap.c
+++ fs/orangefs/orangefs-bufmap.c
@@ -10,6 +10,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 DECLARE_WAIT_QUEUE_HEAD(orangefs_bufmap_init_waitq);
 
+/* used to describe mapped buffers */
+struct orangefs_bufmap_desc {
+	void *uaddr;			/* user space address pointer */
+	struct page **page_array;	/* array of mapped pages */
+	int array_count;		/* size of above arrays */
+	struct list_head list_link;
+};
+
 static struct orangefs_bufmap {
 	atomic_t refcnt;
 
@@ -51,7 +59,7 @@ orangefs_bufmap_free(struct orangefs_bufmap *bufmap)
 	kfree(bufmap);
 }
 
-struct orangefs_bufmap *orangefs_bufmap_ref(void)
+static struct orangefs_bufmap *orangefs_bufmap_ref(void)
 {
 	struct orangefs_bufmap *bufmap = NULL;
 
@@ -64,7 +72,7 @@ struct orangefs_bufmap *orangefs_bufmap_ref(void)
 	return bufmap;
 }
 
-void orangefs_bufmap_unref(struct orangefs_bufmap *bufmap)
+static void orangefs_bufmap_unref(struct orangefs_bufmap *bufmap)
 {
 	if (atomic_dec_and_lock(&bufmap->refcnt, &orangefs_bufmap_lock)) {
 		__orangefs_bufmap = NULL;
