7c045d4401daf83616208ce73137ac263a5e9d4f
Johan Hovold <johan@hovoldconsulting.com>
2015-12-15 15:28:57 +0100

    greybus: interface: fix hot-unplug control-connection timeouts
    Set an interface disconnected flag when the interface has been
    hot-unplugged (e.g. forcibly removed or after a reboot), and use it to
    disable the control connection early when deregistering the interface
    and its bundles.
    
    This avoids a one-second (default) timeout for every enabled connection
    (e.g. one per bundle) at hot-unplug, something which for the default
    gpbridge manifest currently amounts to five seconds.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/interface.h
+++ drivers/staging/greybus/interface.h
@@ -36,6 +36,7 @@ struct gb_interface {
 
 	/* The interface needs to boot over unipro */
 	bool boot_over_unipro;
+	bool disconnected;
 };
 #define to_gb_interface(d) container_of(d, struct gb_interface, dev)
 
