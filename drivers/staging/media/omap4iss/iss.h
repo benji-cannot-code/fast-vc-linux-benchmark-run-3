ed32b1efae9220cf37321bde5f913ebf3ae3a4d5
Sakari Ailus <sakari.ailus@linux.intel.com>
2015-12-16 11:32:33 -0200

    [media] staging: v4l: omap4iss: Use media entity enumeration interface
    Instead of using a bitmap directly in a driver, use the new media
    entity enumeration interface to perform the same.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/staging/media/omap4iss/iss.h
+++ drivers/staging/media/omap4iss/iss.h
@@ -83,7 +83,7 @@ struct iss_reg {
 /*
  * struct iss_device - ISS device structure.
  * @syscon: Regmap for the syscon register space
- * @crashed: Bitmask of crashed entities (indexed by entity ID)
+ * @crashed: Crashed entities
  */
 struct iss_device {
 	struct v4l2_device v4l2_dev;
@@ -102,7 +102,7 @@ struct iss_device {
 	u64 raw_dmamask;
 
 	struct mutex iss_mutex;	/* For handling ref_count field */
-	unsigned int crashed;
+	struct media_entity_enum crashed;
 	int has_context;
 	int ref_count;
 
