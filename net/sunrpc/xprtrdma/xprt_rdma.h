acdaaefdf574a6d8478677753b720e3670cf7744
Chuck Lever <chuck.lever@oracle.com>
2015-12-16 17:22:31 -0500

    xprtrdma: Move struct ib_send_wr off the stack
    For FRWR FASTREG and LOCAL_INV, move the ib_*_wr structure off
    the stack. This allows frwr_op_map and frwr_op_unmap to chain
    WRs together without limit to register or invalidate a set of MRs
    with a single ib_post_send().
    
    (This will be for chaining LOCAL_INV requests).
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Tested-by: Devesh Sharma <devesh.sharma@avagotech.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprtrdma/xprt_rdma.h
+++ net/sunrpc/xprtrdma/xprt_rdma.h
@@ -208,6 +208,10 @@ struct rpcrdma_frmr {
 	enum rpcrdma_frmr_state		fr_state;
 	struct work_struct		fr_work;
 	struct rpcrdma_xprt		*fr_xprt;
+	union {
+		struct ib_reg_wr	fr_regwr;
+		struct ib_send_wr	fr_invwr;
+	};
 };
 
 struct rpcrdma_fmr {
