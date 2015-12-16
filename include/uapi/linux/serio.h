5af3e339dde9f162101a39a59168fd8e374bc673
Böszörményi Zoltán <zboszormenyi@sicom.com>
2015-12-16 11:11:50 -0800

    Input: add eGalaxTouch serial touchscreen driver
    There are two EETI touchscreen drivers in the kernel (eeti_ts and
    egalax_ts) but both are for I2C-connected panels. This is for a different,
    serial and not multi-touch touchscreen panel. The protocol documentation is
    at http://www.eeti.com.tw/pdf/Software%20Programming%20Guide_v2.0.pdf
    
    Signed-off-by: Böszörményi Zoltán <zboszor@pr.hu>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/uapi/linux/serio.h
+++ include/uapi/linux/serio.h
@@ -78,5 +78,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SERIO_PS2MULT	0x3c
 #define SERIO_TSC40	0x3d
 #define SERIO_WACOM_IV	0x3e
+#define SERIO_EGALAX	0x3f
 
 #endif /* _UAPI_SERIO_H */
