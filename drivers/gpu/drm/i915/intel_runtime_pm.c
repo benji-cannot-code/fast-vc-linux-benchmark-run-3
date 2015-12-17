d8e2a22a852a604feb0c1687d1e32d3901e39ef2
Imre Deak <imre.deak@intel.com>
2015-12-17 13:48:51 +0200

    drm/i915: remove HAS_RUNTIME_PM check from RPM get/put/assert helpers
    We don't really need to check this flag in the get/put/assert helpers,
    as on platforms without RPM support we won't ever enable RPM. That means
    pm.suspend will be always false and the assert will be always true.
    
    Do this to simplify the code and to let us extend the RPM asserts to all
    platforms for a better coverage.
    
    Motivated by Ville.
    
    v2-v3:
    - unchanged
    v4:
    - remove the HAS_RUNTIME_PM check from intel_runtime_pm_enable() too
      made possible by the previous two patches
    v5:
    - rebased on the previous new patch in the series that keeps
      HAS_RUNTIME_PM() in intel_runtime_pm_enable() with a permanent
      reference taken there
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk> (v3)
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1450352931-16498-1-git-send-email-imre.deak@intel.com

--- drivers/gpu/drm/i915/intel_runtime_pm.c
+++ drivers/gpu/drm/i915/intel_runtime_pm.c
@@ -2242,9 +2242,6 @@ void intel_runtime_pm_get(struct drm_i915_private *dev_priv)
 	struct drm_device *dev = dev_priv->dev;
 	struct device *device = &dev->pdev->dev;
 
-	if (!HAS_RUNTIME_PM(dev))
-		return;
-
 	pm_runtime_get_sync(device);
 	WARN(dev_priv->pm.suspended, "Device still suspended.\n");
 }
@@ -2271,9 +2268,6 @@ void intel_runtime_pm_get_noresume(struct drm_i915_private *dev_priv)
 	struct drm_device *dev = dev_priv->dev;
 	struct device *device = &dev->pdev->dev;
 
-	if (!HAS_RUNTIME_PM(dev))
-		return;
-
 	WARN(dev_priv->pm.suspended, "Getting nosync-ref while suspended.\n");
 	pm_runtime_get_noresume(device);
 }
@@ -2290,9 +2284,6 @@ void intel_runtime_pm_put(struct drm_i915_private *dev_priv)
 {
 	struct drm_device *dev = dev_priv->dev;
 	struct device *device = &dev->pdev->dev;
-
-	if (!HAS_RUNTIME_PM(dev))
-		return;
 
 	pm_runtime_mark_last_busy(device);
 	pm_runtime_put_autosuspend(device);
