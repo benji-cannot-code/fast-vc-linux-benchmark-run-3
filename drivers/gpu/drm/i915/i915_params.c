8c063bc3b5c79094f57422b1de541c893b054e01
Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
2015-12-18 13:08:15 +0200

    drm/i915: Decouple struct i915_params i915 into i915_params.h
    Otherwise usage in the i915 debug macros yields problems due to
    i915_drv.h <-> i915_trace.h <-> intel_drv.h include loops.
    
    v2:
    - Document not-so-obvious need for linux/cache.h (Chris)
    
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1450436898-20408-2-git-send-email-joonas.lahtinen@linux.intel.com
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_params.c
+++ drivers/gpu/drm/i915/i915_params.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * IN THE SOFTWARE.
  */
 
+#include "i915_params.h"
 #include "i915_drv.h"
 
 struct i915_params i915 __read_mostly = {
