7dee0facc7709d8b730333c2d6cc1dd5d054ae43
Peter Ujfalusi <peter.ujfalusi@ti.com>
2015-12-18 15:00:49 +0200

    serial: 8250: of: Fix the driver and actually compile the 8250_of
    The 8250_of never compiled since in the Kconfig we have SERIAL_OF_PLATFORM
    but in the makefile we expect to have SERIAL_8250_OF...
    
    When the 8250_of.c is actually compiled we will have two errors:
    missing linux/nwpserial.h and 8250/8250.h.
    Fix those as well at the same time when enable the compilation of the
    driver.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Fixes: afd7f88f1577 ("serial: 8250: move of_serial code to 8250 directory")
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    CC: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Jon Hunter <jonathanh@nvidia.com>
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_of.c
+++ drivers/tty/serial/8250/8250_of.c
@@ -19,10 +19,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
-#include <linux/nwpserial.h>
 #include <linux/clk.h>
 
-#include "8250/8250.h"
+#include "8250.h"
 
 struct of_serial_info {
 	struct clk *clk;
