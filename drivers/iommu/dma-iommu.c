5c339eea5f5d085b3a3935e758d9282c7231505a
Robin Murphy <robin.murphy@arm.com>
2015-12-18 17:01:46 +0000

    iommu/dma: Add some missing #includes
    dma-iommu.c was naughtily relying on an implicit transitive #include of
    linux/vmalloc.h, which is apparently not present on some architectures.
    Add that, plus a couple more headers for other functions which are used
    similarly.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/dma-iommu.c
+++ drivers/iommu/dma-iommu.c
@@ -22,10 +22,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/device.h>
 #include <linux/dma-iommu.h>
+#include <linux/gfp.h>
 #include <linux/huge_mm.h>
 #include <linux/iommu.h>
 #include <linux/iova.h>
 #include <linux/mm.h>
+#include <linux/scatterlist.h>
+#include <linux/vmalloc.h>
 
 int iommu_dma_init(void)
 {
