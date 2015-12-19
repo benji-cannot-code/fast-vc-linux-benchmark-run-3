46299bba7450431d779048c7813b51f8ce4b67a3
Robert Jarzmik <robert.jarzmik@free.fr>
2015-12-19 13:14:31 +0100

    video: fbdev: pxafb: fix out of memory error path
    As seen by Julia, the initial allocation memory is not checked anymore
    after commit "video: fbdev: pxafb: initial devicetree conversion".
    Introduce back the removed test.
    
    Reported-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/fbdev/pxafb.c
+++ drivers/video/fbdev/pxafb.c
@@ -2250,6 +2250,8 @@ static int pxafb_probe(struct platform_device *dev)
 	ret = -ENOMEM;
 	pdata = dev_get_platdata(&dev->dev);
 	inf = devm_kmalloc(&dev->dev, sizeof(*inf), GFP_KERNEL);
+	if (!inf)
+		goto failed;
 
 	if (pdata) {
 		*inf = *pdata;
