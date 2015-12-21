d850b7778565e3cb7404270bc772750e3c4dc8d2
Linus Walleij <linus.walleij@linaro.org>
2015-12-21 11:36:28 +0100

    gpio: sa1100: Be sure to clamp return value
    As we want gpio_chip .get() calls to be able to return negative
    error codes and propagate to drivers, we need to go over all
    drivers and make sure their return values are clamped to [0,1].
    We do this by using the ret = !!(val) design pattern.
    
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Cc: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
    Cc: Kristoffer Ericson <kristoffer.ericson@gmail.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-sa1100.c
+++ drivers/gpio/gpio-sa1100.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int sa1100_gpio_get(struct gpio_chip *chip, unsigned offset)
 {
-	return GPLR & GPIO_GPIO(offset);
+	return !!(GPLR & GPIO_GPIO(offset));
 }
 
 static void sa1100_gpio_set(struct gpio_chip *chip, unsigned offset, int value)
