d4febc3df88357da8871c35d741defa434f5ff2b
Glen Lee <glen.lee@atmel.com>
2015-12-21 14:18:20 +0900

    staging: wilc1000: linux_wlan_spi.c: remove braces for single statement
    This patches fixes checkpatch warning: braces {} are not necessary for single
    statement blocks. Remove some comments also.
    
    Signed-off-by: Glen Lee <glen.lee@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan_spi.c
+++ drivers/staging/wilc1000/linux_wlan_spi.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "linux_wlan_common.h"
 #include "wilc_wlan_if.h"
 
-#define USE_SPI_DMA     0       /* johnny add */
+#define USE_SPI_DMA     0
 
 static const struct wilc1000_ops wilc1000_spi_ops;
 
@@ -88,16 +88,13 @@ int wilc_spi_write(struct wilc *wilc, u8 *b, u32 len)
 
 		memset(&msg, 0, sizeof(msg));
 		spi_message_init(&msg);
-/* [[johnny add */
 		msg.spi = spi;
 		msg.is_dma_mapped = USE_SPI_DMA;
-/* ]] */
 		spi_message_add_tail(&tr, &msg);
 
 		ret = spi_sync(spi, &msg);
-		if (ret < 0) {
+		if (ret < 0)
 			dev_err(&spi->dev, "SPI transaction failed\n");
-		}
 
 		kfree(r_buffer);
 	} else {
@@ -138,16 +135,13 @@ int wilc_spi_read(struct wilc *wilc, u8 *rb, u32 rlen)
 
 		memset(&msg, 0, sizeof(msg));
 		spi_message_init(&msg);
-/* [[ johnny add */
 		msg.spi = spi;
 		msg.is_dma_mapped = USE_SPI_DMA;
-/* ]] */
 		spi_message_add_tail(&tr, &msg);
 
 		ret = spi_sync(spi, &msg);
-		if (ret < 0) {
+		if (ret < 0)
 			dev_err(&spi->dev, "SPI transaction failed\n");
-		}
 		kfree(t_buffer);
 	} else {
 		dev_err(&spi->dev,
@@ -184,9 +178,8 @@ int wilc_spi_write_read(struct wilc *wilc, u8 *wb, u8 *rb, u32 rlen)
 
 		spi_message_add_tail(&tr, &msg);
 		ret = spi_sync(spi, &msg);
-		if (ret < 0) {
+		if (ret < 0)
 			dev_err(&spi->dev, "SPI transaction failed\n");
-		}
 	} else {
 		dev_err(&spi->dev,
 			"can't read data with the following length: %u\n",
