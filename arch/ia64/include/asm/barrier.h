44d72f8bbcdf6625663adbc7661b98c4c0eddabe
Michael S. Tsirkin <mst@redhat.com>
2015-12-21 09:22:18 +0200

    ia64: reuse asm-generic/barrier.h
    On ia64 smp_rmb, smp_wmb, read_barrier_depends, smp_read_barrier_depends
    and smp_store_mb() match the asm-generic variants exactly. Drop the
    local definitions and pull in asm-generic/barrier.h instead.
    
    This is in preparation to refactoring this code area.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- arch/ia64/include/asm/barrier.h
+++ arch/ia64/include/asm/barrier.h
@@ -49,12 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # define smp_mb()	barrier()
 #endif
 
-#define smp_rmb()	smp_mb()
-#define smp_wmb()	smp_mb()
-
-#define read_barrier_depends()		do { } while (0)
-#define smp_read_barrier_depends()	do { } while (0)
-
 #define smp_mb__before_atomic()	barrier()
 #define smp_mb__after_atomic()	barrier()
 
@@ -78,12 +72,12 @@ do {									\
 	___p1;								\
 })
 
-#define smp_store_mb(var, value) do { WRITE_ONCE(var, value); smp_mb(); } while (0)
-
 /*
  * The group barrier in front of the rsm & ssm are necessary to ensure
  * that none of the previous instructions in the same group are
  * affected by the rsm/ssm.
  */
+
+#include <asm-generic/barrier.h>
 
 #endif /* _ASM_IA64_BARRIER_H */
