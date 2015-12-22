08d91ced39ddfdf4d2dc13def8a88c5ca7311003
Christian Gromm <christian.gromm@microchip.com>
2015-12-22 10:52:51 +0100

    staging: most: add missing call to ida_simple_remove
    This patch adds two missing calls to function ida_simpel_remove.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/mostcore/core.c
+++ drivers/staging/most/mostcore/core.c
@@ -1753,6 +1753,7 @@ struct kobject *most_register_interface(struct most_interface *iface)
 	inst = create_most_inst_obj(name);
 	if (!inst) {
 		pr_info("Failed to allocate interface instance\n");
+		ida_simple_remove(&mdev_id, id);
 		return ERR_PTR(-ENOMEM);
 	}
 
@@ -1809,6 +1810,7 @@ struct kobject *most_register_interface(struct most_interface *iface)
 free_instance:
 	pr_info("Failed allocate channel(s)\n");
 	list_del(&inst->list);
+	ida_simple_remove(&mdev_id, id);
 	destroy_most_inst_obj(inst);
 	return ERR_PTR(-ENOMEM);
 }
