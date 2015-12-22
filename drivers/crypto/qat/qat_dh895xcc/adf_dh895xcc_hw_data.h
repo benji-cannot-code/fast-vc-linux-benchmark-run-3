6bf7daa238a588e813ae7ff3a984064ebf7ab45b
Ahsan Atta <ahsan.atta@intel.com>
2015-12-22 11:20:34 -0800

    crypto: qat - Rename dh895xcc mmp firmware
    Resending again. The fw name suppoed to be 895xcc instead of 895xxcc.
    Sorry for the noise.
    Rename dh895xcc mmp fw to make it consistent with other mmp images.
    
    Signed-off-by: Ahsan Atta <ahsan.atta@intel.com>
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_dh895xcc/adf_dh895xcc_hw_data.h
+++ drivers/crypto/qat/qat_dh895xcc/adf_dh895xcc_hw_data.h
@@ -83,7 +83,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ADF_DH895XCC_VINTMSK_OFFSET(i)	(0x3A000 + 0x200 + ((i) * 0x04))
 /* FW names */
 #define ADF_DH895XCC_FW "qat_895xcc.bin"
-#define ADF_DH895XCC_MMP "qat_mmp.bin"
+#define ADF_DH895XCC_MMP "qat_895xcc_mmp.bin"
 
 void adf_init_hw_data_dh895xcc(struct adf_hw_device_data *hw_data);
 void adf_clean_hw_data_dh895xcc(struct adf_hw_device_data *hw_data);
