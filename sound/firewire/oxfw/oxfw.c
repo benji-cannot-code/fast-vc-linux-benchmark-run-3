b43cad32a9a2376d08ffbd3015854ff152d447fc
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2015-12-22 09:15:45 +0900

    ALSA: oxfw: obsolete scs1x module
    Now ALSA oxfw driver gains functionalities which scs1x module has.
    
    This commit obsoletes the scs1x module, and adds a line of MODULE_ALIAS
    to load oxfw module instead of scs1x module.
    
    In scs1x module, the name of 'shortname' field is fixed as 'SCS1x'. This
    field is used to name MIDI ports for both of SCS.1m and SCS.1d. This is
    not good because typically some SCS.1m and SCS.1d are used in the same
    system. It's better to distinguish them according to name of the ports.
    This commit applies model name in config ROM to the 'shortname'.
    
    For the name of 'driver' and 'longname', this commit uses the same way
    applied to the other models. This change may not bring disadvantages to
    users because userspace applications use ALSA rawmidi or seq interface
    and these interfaces are not influenced by them directly.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/oxfw/oxfw.c
+++ sound/firewire/oxfw/oxfw.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VENDOR_BEHRINGER	0x001564
 #define VENDOR_LACIE		0x00d04b
 #define VENDOR_TASCAM		0x00022e
+#define OUI_STANTON		0x001260
 
 #define MODEL_SATELLITE		0x00200f
 
@@ -30,6 +31,7 @@ MODULE_DESCRIPTION("Oxford Semiconductor FW970/971 driver");
 MODULE_AUTHOR("Clemens Ladisch <clemens@ladisch.de>");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("snd-firewire-speakers");
+MODULE_ALIAS("snd-scs1x");
 
 struct compat_info {
 	const char *driver_name;
@@ -160,6 +162,13 @@ static int detect_quirks(struct snd_oxfw *oxfw)
 		return snd_oxfw_add_spkr(oxfw, true);
 
 	/*
+	 * Stanton models supports asynchronous transactions for unique MIDI
+	 * messages.
+	 */
+	if (oxfw->entry->vendor_id == OUI_STANTON)
+		return snd_oxfw_scs1x_add(oxfw);
+
+	/*
 	 * TASCAM FireOne has physical control and requires a pair of additional
 	 * MIDI ports.
 	 */
@@ -276,6 +285,9 @@ static void oxfw_bus_reset(struct fw_unit *unit)
 		snd_oxfw_stream_update_simplex(oxfw, &oxfw->tx_stream);
 
 	mutex_unlock(&oxfw->mutex);
+
+	if (oxfw->entry->vendor_id == OUI_STANTON)
+		snd_oxfw_scs1x_update(oxfw);
 }
 
 static void oxfw_remove(struct fw_unit *unit)
@@ -352,6 +364,20 @@ static const struct ieee1394_device_id oxfw_id_table[] = {
 				  IEEE1394_MATCH_MODEL_ID,
 		.vendor_id	= VENDOR_TASCAM,
 		.model_id	= 0x800007,
+	},
+	/* Stanton, Stanton Controllers & Systems 1 Mixer (SCS.1m) */
+	{
+		.match_flags	= IEEE1394_MATCH_VENDOR_ID |
+				  IEEE1394_MATCH_MODEL_ID,
+		.vendor_id	= OUI_STANTON,
+		.model_id	= 0x001000,
+	},
+	/* Stanton, Stanton Controllers & Systems 1 Deck (SCS.1d) */
+	{
+		.match_flags	= IEEE1394_MATCH_VENDOR_ID |
+				  IEEE1394_MATCH_MODEL_ID,
+		.vendor_id	= OUI_STANTON,
+		.model_id	= 0x002000,
 	},
 	{ }
 };
