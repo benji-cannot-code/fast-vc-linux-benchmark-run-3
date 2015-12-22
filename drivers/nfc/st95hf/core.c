3097b3f319fba3fe10f861a77a49b74d0af6656d
Shikha Singh <shikha.singh@st.com>
2015-12-22 04:53:37 -0500

    NFC: st95hf: Fix build error
    This fixes a build error on the mn10300 architecture:
    
    drivers/nfc/st95hf/core.c:765:20: error: conflicting types for 'irq_handler'
       static irqreturn_t irq_handler(int irq, void  *st95hfcontext)
                           ^
       In file included from arch/mn10300/include/asm/reset-regs.h:16:0,
                        from arch/mn10300/include/asm/irq.h:18,
                        from include/linux/irq.h:26,
                        from arch/mn10300/include/asm/hardirq.h:16,
                        from include/linux/hardirq.h:8,
                        from include/linux/interrupt.h:12,
                        from drivers/nfc/st95hf/core.c:23:
       arch/mn10300/include/asm/exceptions.h:107:24: note: previous declaration of 'irq_handler' was here
        extern asmlinkage void irq_handler(void);
    
    Signed-off-by: Shikha Singh <shikha.singh@st.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/st95hf/core.c
+++ drivers/nfc/st95hf/core.c
@@ -763,7 +763,7 @@ static int st95hf_response_handler(struct st95hf_context *stcontext,
 	return result;
 }
 
-static irqreturn_t irq_handler(int irq, void  *st95hfcontext)
+static irqreturn_t st95hf_irq_handler(int irq, void  *st95hfcontext)
 {
 	struct st95hf_context *stcontext  =
 		(struct st95hf_context *)st95hfcontext;
@@ -777,7 +777,7 @@ static irqreturn_t irq_handler(int irq, void  *st95hfcontext)
 	return IRQ_WAKE_THREAD;
 }
 
-static irqreturn_t irq_thread_handler(int irq, void  *st95hfcontext)
+static irqreturn_t st95hf_irq_thread_handler(int irq, void  *st95hfcontext)
 {
 	int result = 0;
 	int res_len;
@@ -1141,8 +1141,8 @@ static int st95hf_probe(struct spi_device *nfc_spi_dev)
 	if (nfc_spi_dev->irq > 0) {
 		if (devm_request_threaded_irq(&nfc_spi_dev->dev,
 					      nfc_spi_dev->irq,
-					      irq_handler,
-					      irq_thread_handler,
+					      st95hf_irq_handler,
+					      st95hf_irq_thread_handler,
 					      IRQF_TRIGGER_FALLING,
 					      "st95hf",
 					      (void *)st95context) < 0) {
