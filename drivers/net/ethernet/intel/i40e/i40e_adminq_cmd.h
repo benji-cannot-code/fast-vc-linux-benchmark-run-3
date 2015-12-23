6152aceb05a6f1a2c662837e743107790b21197c
Shannon Nelson <shannon.nelson@intel.com>
2015-12-23 12:05:45 -0800

    i40e: Bump AQ minor version to 1.5 for new FW features
    Bump AQ minor version to 1.5 for new FW features.
    
    Change-ID: I5a790f7f519a2a8921aaa1c5663727dd1897ffec
    Signed-off-by: Shannon Nelson <shannon.nelson@intel.com>
    Acked-by: Kevin Scott <kevin.c.scott@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_adminq_cmd.h
+++ drivers/net/ethernet/intel/i40e/i40e_adminq_cmd.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #define I40E_FW_API_VERSION_MAJOR	0x0001
-#define I40E_FW_API_VERSION_MINOR	0x0004
+#define I40E_FW_API_VERSION_MINOR	0x0005
 
 struct i40e_aq_desc {
 	__le16 flags;
