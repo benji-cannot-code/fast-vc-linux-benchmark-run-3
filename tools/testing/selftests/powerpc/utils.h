48b42facd05cac84bba000eb7aeb9c96914c5af7
Rashmica Gupta <rashmicy@gmail.com>
2015-12-23 16:49:50 +1100

    selftests/powerpc: Make reg.h common to all powerpc selftests
    Currently there is a reg.h in pmu/ebb that has defines that are useful
    in other powerpc selftests so move this up into selftests/powerpc
    folder. Also include in utils.h - as this is often used in self tests.
    Add in some other useful register defines.
    
    Signed-off-by: Rashmica Gupta <rashmicy@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- tools/testing/selftests/powerpc/utils.h
+++ tools/testing/selftests/powerpc/utils.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdint.h>
 #include <stdbool.h>
 #include <linux/auxvec.h>
+#include "reg.h"
 
 /* Avoid headaches with PRI?64 - just use %ll? always */
 typedef unsigned long long u64;
