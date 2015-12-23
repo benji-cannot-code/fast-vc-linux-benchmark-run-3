5e3603abfc0bd3549649ece5c8457dfae7cf4abf
Matan Barak <matanb@mellanox.com>
2015-12-23 14:56:54 +0200

    IB/rdma_cm: Add wrapper for cma reference count
    Currently, cma users can't increase or decrease the cma reference
    count. This is necassary when setting cma attributes (like the
    default GID type) in order to avoid use-after-free errors.
    Adding cma_ref_dev and cma_deref_dev APIs.
    
    Signed-off-by: Matan Barak <matanb@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/core/core_priv.h
+++ drivers/infiniband/core/core_priv.h
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <rdma/ib_verbs.h>
 
+struct cma_device;
+void cma_ref_dev(struct cma_device *cma_dev);
+void cma_deref_dev(struct cma_device *cma_dev);
+
 int  ib_device_register_sysfs(struct ib_device *device,
 			      int (*port_callback)(struct ib_device *,
 						   u8, struct kobject *));
