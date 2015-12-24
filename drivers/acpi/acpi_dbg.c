753cada40b3f1caf128e6f8ed2160fc7dd0a096f
Dan Carpenter <dan.carpenter@oracle.com>
2015-12-24 10:23:03 +0300

    ACPI / debugger: remove some unneeded conditions
    "count" is unsigned so checking for less than zero here causes a static
    checker warning.  And really it's better to let the access_ok() check
    fail if the user passes in a NULL "buf" pointer because -EFAULT is the
    correct error code.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpi_dbg.c
+++ drivers/acpi/acpi_dbg.c
@@ -611,8 +611,6 @@ static ssize_t acpi_aml_read(struct file *file, char __user *buf,
 	int ret = 0;
 	int size = 0;
 
-	if (!buf || count < 0)
-		return -EINVAL;
 	if (!count)
 		return 0;
 	if (!access_ok(VERIFY_WRITE, buf, count))
@@ -682,8 +680,6 @@ static ssize_t acpi_aml_write(struct file *file, const char __user *buf,
 	int ret = 0;
 	int size = 0;
 
-	if (!buf || count < 0)
-		return -EINVAL;
 	if (!count)
 		return 0;
 	if (!access_ok(VERIFY_READ, buf, count))
