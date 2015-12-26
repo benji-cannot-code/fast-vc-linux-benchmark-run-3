4ebcc57c01aad7d2acb3b165f28abff65f0eed42
Shraddha Barke <shraddha.6596@gmail.com>
2015-12-27 02:44:42 +0530

    Staging: gdm72xx: Add appropriate comment for spinlock_t definition
    Fix checkpatch issue: "CHECK: spinlock_t definition without comment".
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm72xx/gdm_qos.c
+++ drivers/staging/gdm72xx/gdm_qos.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct {
 	struct list_head head;
 	int cnt;
-	spinlock_t lock;
+	spinlock_t lock; /* protect structure fields */
 } qos_free_list;
 
 static void init_qos_entry_list(void)
