63e2dd33393d7421917b29a953e267c37f9072e8
Geliang Tang <geliangtang@163.com>
2015-12-27 17:25:20 +0800

    HID: move to_hid_device() to hid.h
    to_hid_device() macro is defined in both hid-lg4ff.c and
    hid-logitech-hidpp.c. So I move it to include/linux/hid.h.
    
    Signed-off-by: Geliang Tang <geliangtang@163.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-lg4ff.c
+++ drivers/hid/hid-lg4ff.c
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hid-lg4ff.h"
 #include "hid-ids.h"
 
-#define to_hid_device(pdev) container_of(pdev, struct hid_device, dev)
-
 #define LG4FF_MMODE_IS_MULTIMODE 0
 #define LG4FF_MMODE_SWITCHED 1
 #define LG4FF_MMODE_NOT_MULTIMODE 2
