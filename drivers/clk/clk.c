5b1c8998d7356bd4734ccafb167f683292ba9c30
Masahiro Yamada <yamada.masahiro@socionext.com>
2015-12-28 19:23:08 +0900

    clk: make sure parent is not NULL in clk_fetch_parent_index()
    If parent is given with NULL, clk_fetch_parent_index() could return
    a positive index value.
    
    Currently, parent is checked by the callers of this function, but
    it would be safer to do it in this function.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Vladimir Zapolskiy <vz@mleia.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -1067,6 +1067,9 @@ static int clk_fetch_parent_index(struct clk_core *core,
 {
 	int i;
 
+	if (!parent)
+		return -EINVAL;
+
 	/*
 	 * find index of new parent clock using cached parent ptrs,
 	 * or if not yet cached, use string name comparison and cache
