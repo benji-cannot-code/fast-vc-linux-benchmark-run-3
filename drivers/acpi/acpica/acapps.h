d470212abc2be4e51933f976a1e152699bcf933d
Bob Moore <robert.moore@intel.com>
2015-12-29 13:56:46 +0800

    ACPICA: acpiexec/acpinames: Update for error checking macros
    ACPICA commit 5bdfb4f43f4d315d23b31b39bac5a0b1ca420c2b
    
    Standardize naming, deploy in common header.
    
    Link: https://github.com/acpica/acpica/commit/5bdfb4f4
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/acapps.h
+++ drivers/acpi/acpica/acapps.h
@@ -90,6 +90,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ACPI_OPTION(name, description) \
 	acpi_os_printf (" %-18s%s\n", name, description);
 
+/* Check for unexpected exceptions */
+
+#define ACPI_CHECK_STATUS(name, status, expected) \
+	if (status != expected) \
+	{ \
+		acpi_os_printf ("Unexpected %s from %s (%s-%d)\n", \
+			acpi_format_exception (status), #name, _acpi_module_name, __LINE__); \
+	}
+
+/* Check for unexpected non-AE_OK errors */
+
+#define ACPI_CHECK_OK(name, status)   ACPI_CHECK_STATUS (name, status, AE_OK);
+
 #define FILE_SUFFIX_DISASSEMBLY     "dsl"
 #define FILE_SUFFIX_BINARY_TABLE    ".dat"	/* Needs the dot */
 
