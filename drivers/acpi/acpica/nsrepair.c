4adf9d2d0f4ed7d5b129d949f4534c9396ba8392
Lv Zheng <lv.zheng@intel.com>
2015-12-29 13:57:38 +0800

    ACPICA: Namespace: Add scope information to the simple object repair mechanism
    ACPICA commit 51cbd324420ca5e381cb2c57ce95139053518a35
    
    The acpi_object_converter callbacks are lack in scope information to
    convert name_string. This patch fixes this issue by passing the evaluation
    method/object node to the converter callbacks. Lv Zheng.
    
    Link: https://github.com/acpica/acpica/commit/51cbd324
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/nsrepair.c
+++ drivers/acpi/acpica/nsrepair.c
@@ -173,8 +173,8 @@ acpi_ns_simple_repair(struct acpi_evaluate_info *info,
 					      "Missing expected return value"));
 		}
 
-		status =
-		    predefined->object_converter(return_object, &new_object);
+		status = predefined->object_converter(info->node, return_object,
+						      &new_object);
 		if (ACPI_FAILURE(status)) {
 
 			/* A fatal error occurred during a conversion */
