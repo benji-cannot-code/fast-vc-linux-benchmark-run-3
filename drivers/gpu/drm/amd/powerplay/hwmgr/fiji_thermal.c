5c353ed674ce53a2e1047e507eb235a8dd93ef79
Stephen Rothwell <sfr@canb.auug.org.au>
2015-12-31 21:20:20 +1100

    drm/amdgpu/powerplay: include asm/div64.h for do_div()
    Fixes: 1e4854e96c35 ("drm/amdgpu/powerplay: implement thermal control for tonga.")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/fiji_thermal.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/fiji_thermal.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-
+#include <asm/div64.h>
 #include "fiji_thermal.h"
 #include "fiji_hwmgr.h"
 #include "fiji_smumgr.h"
--- drivers/gpu/drm/amd/powerplay/hwmgr/tonga_thermal.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/tonga_thermal.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-
+#include <asm/div64.h>
 #include "tonga_thermal.h"
 #include "tonga_hwmgr.h"
 #include "tonga_smumgr.h"
