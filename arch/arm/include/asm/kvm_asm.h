1ee264ef46d422cac1c7d528c8126834b3119065
Marc Zyngier <marc.zyngier@arm.com>
2016-01-02 13:57:18 +0000

    ARM: KVM: Move the  HYP code to its own section
    In order to be able to spread the HYP code into multiple compilation
    units, adopt a layout similar to that of arm64:
    - the HYP text is emited in its own section (.hyp.text)
    - two linker generated symbols are use to identify the boundaries
      of that section
    
    No functionnal change.
    
    Acked-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/include/asm/kvm_asm.h
+++ arch/arm/include/asm/kvm_asm.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ARM_KVM_ASM_H__
 #define __ARM_KVM_ASM_H__
 
+#include <asm/virt.h>
+
 /* 0 is reserved as an invalid value. */
 #define c0_MPIDR	1	/* MultiProcessor ID Register */
 #define c0_CSSELR	2	/* Cache Size Selection Register */
@@ -92,8 +94,8 @@ extern char __kvm_hyp_exit_end[];
 
 extern char __kvm_hyp_vector[];
 
-extern char __kvm_hyp_code_start[];
-extern char __kvm_hyp_code_end[];
+#define	__kvm_hyp_code_start	__hyp_text_start
+#define __kvm_hyp_code_end	__hyp_text_end
 
 extern void __kvm_flush_vm_context(void);
 extern void __kvm_tlb_flush_vmid_ipa(struct kvm *kvm, phys_addr_t ipa);
