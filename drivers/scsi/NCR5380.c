e7f5bf66bd715db1f02441ec5fd0bfe66a67ad21
Finn Thain <fthain@telegraphics.com.au>
2016-01-03 16:05:31 +1100

    ncr5380: Add missing break after case MESSAGE_REJECT
    MESSAGE REJECT does not imply DISCONNECT: the target is about to enter
    MESSAGE IN or MESSAGE OUT phase.
    
    This bug fix comes from atari_NCR5380.c. Unfortunately it never made it
    into the original NCR5380.c core driver.
    
    Signed-off-by: Finn Thain <fthain@telegraphics.com.au>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Tested-by: Ondrej Zary <linux@rainbow-software.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/NCR5380.c
+++ drivers/scsi/NCR5380.c
@@ -1991,6 +1991,7 @@ static void NCR5380_information_transfer(struct Scsi_Host *instance) {
 					default:
 						break;
 					}
+					break;
 				case DISCONNECT:{
 						/* Accept message by clearing ACK */
 						NCR5380_write(INITIATOR_COMMAND_REG, ICR_BASE);
