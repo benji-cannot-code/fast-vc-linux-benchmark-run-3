581be0f4c46a31880f3c753d3c04d318f3c851d8
Oleg Drokin <green@linuxhacker.ru>
2016-01-03 12:05:54 -0500

    staging/lustre: Mark lmv_hsm_ct_register/unregister uarg as __user
    Since it is a userspace pointer, this makes things neater and
    sparse happier.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lmv/lmv_obd.c
+++ drivers/staging/lustre/lustre/lmv/lmv_obd.c
@@ -795,7 +795,8 @@ static void lmv_hsm_req_build(struct lmv_obd *lmv,
 }
 
 static int lmv_hsm_ct_unregister(struct lmv_obd *lmv, unsigned int cmd, int len,
-				 struct lustre_kernelcomm *lk, void *uarg)
+				 struct lustre_kernelcomm *lk,
+				 void __user *uarg)
 {
 	int rc = 0;
 	__u32 i;
@@ -817,7 +818,7 @@ static int lmv_hsm_ct_unregister(struct lmv_obd *lmv, unsigned int cmd, int len,
 }
 
 static int lmv_hsm_ct_register(struct lmv_obd *lmv, unsigned int cmd, int len,
-			       struct lustre_kernelcomm *lk, void *uarg)
+			       struct lustre_kernelcomm *lk, void __user *uarg)
 {
 	struct file *filp;
 	__u32 i, j;
