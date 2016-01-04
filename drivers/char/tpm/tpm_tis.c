3d2e69d1d4c724c6f8808cff177cc3d6b33f3613
Jerry Snitselaar <jsnitsel@redhat.com>
2016-01-04 12:19:43 -0700

    tpm: remove unneeded include of actbl2.h
    tpm_tis.c already gets actbl2.h via linux/acpi.h -> acpi/acpi.h ->
    acpi/actbl.h -> acpi/actbl2.h, so the direct include in tpm_tis.c
    is not needed.
    
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Acked-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Acked-by: Peter Huewe <peterhuewe@gmx.de>

--- drivers/char/tpm/tpm_tis.c
+++ drivers/char/tpm/tpm_tis.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/wait.h>
 #include <linux/acpi.h>
 #include <linux/freezer.h>
-#include <acpi/actbl2.h>
 #include "tpm.h"
 
 enum tis_access {
