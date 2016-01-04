9e2156c4286fbf00366b96734dfa941192d89927
Fabio Estevam <fabio.estevam@nxp.com>
2016-01-04 09:15:37 -0200

    i2c: imx: Remove unneeded comments
    These multi-lines comments do not follow the standard kernel coding
    style. In fact, they are not useful comments, so get rid of them.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-imx.c
+++ drivers/i2c/busses/i2c-imx.c
@@ -30,9 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-/** Includes *******************************************************************
-*******************************************************************************/
-
 #include <linux/clk.h>
 #include <linux/completion.h>
 #include <linux/delay.h>
@@ -57,9 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm_runtime.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
-
-/** Defines ********************************************************************
-*******************************************************************************/
 
 /* This will be the driver name the kernel reports */
 #define DRIVER_NAME "imx-i2c"
@@ -123,9 +117,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I2CR_IEN_OPCODE_1	I2CR_IEN
 
 #define I2C_PM_TIMEOUT		10 /* ms */
-
-/** Variables ******************************************************************
-*******************************************************************************/
 
 /*
  * sorted list of clock divider, register value pairs
@@ -419,9 +410,6 @@ static void i2c_imx_dma_free(struct imx_i2c_struct *i2c_imx)
 
 	dma->chan_using = NULL;
 }
-
-/** Functions for IMX I2C adapter driver ***************************************
-*******************************************************************************/
 
 static int i2c_imx_bus_busy(struct imx_i2c_struct *i2c_imx, int for_busy)
 {
