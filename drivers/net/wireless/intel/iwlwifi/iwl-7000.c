8d12ff8ba678e04e7b0137f7c10e7f55064d37ff
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2016-01-04 10:19:17 +0200

    iwlwifi: set max firmware version of 7265 to 17
    Just like 7260, 7265 will not have firmware releases newer
    than iwlwifi-7265-17.ucode. 7265D is still supported in
    latest firmware releases.
    
    Fixes: 628a2918afe4 ("iwlwifi: separate firmware version for 7260 devices")
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>

--- drivers/net/wireless/intel/iwlwifi/iwl-7000.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-7000.c
@@ -73,7 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Highest firmware API version supported */
 #define IWL7260_UCODE_API_MAX	17
-#define IWL7265_UCODE_API_MAX	19
+#define IWL7265_UCODE_API_MAX	17
 #define IWL7265D_UCODE_API_MAX	19
 
 /* Oldest version we won't warn about */
