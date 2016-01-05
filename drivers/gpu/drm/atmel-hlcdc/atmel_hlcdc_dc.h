198d063c190e51ec2e5adf3a6d9e2f0fb60f58d3
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-01-05 18:11:39 +0100

    drm: atmel-hlcdc: support extended timing ranges on sama5d4 and sama5d2
    The display timings on old SoCs older than the sama5d4 are quite limited
    and prevent the use of many displays. Add support for extended timing
    ranges on sama5d2 and sama5d4.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Tested-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_dc.h
+++ drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_dc.h
@@ -51,6 +51,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @min_height: minimum height supported by the Display Controller
  * @max_width: maximum width supported by the Display Controller
  * @max_height: maximum height supported by the Display Controller
+ * @max_spw: maximum vertical/horizontal pulse width
+ * @max_vpw: maximum vertical back/front porch width
+ * @max_hpw: maximum horizontal back/front porch width
  * @layers: a layer description table describing available layers
  * @nlayers: layer description table size
  */
@@ -59,6 +62,9 @@ struct atmel_hlcdc_dc_desc {
 	int min_height;
 	int max_width;
 	int max_height;
+	int max_spw;
+	int max_vpw;
+	int max_hpw;
 	const struct atmel_hlcdc_layer_desc *layers;
 	int nlayers;
 };
