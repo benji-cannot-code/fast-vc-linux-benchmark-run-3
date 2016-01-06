ebf9358b1e71a6fbb31c3c79bbb7822f5de39f34
Dan Williams <dan.j.williams@intel.com>
2016-01-06 12:03:41 -0800

    libnvdimm, pmem: move definition of nvdimm_namespace_add_poison to nd.h
    nd-core.h is private to the libnvdimm core internals and should not be
    used by drivers.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/nvdimm/pmem.c
+++ drivers/nvdimm/pmem.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/pmem.h>
 #include <linux/nd.h>
-#include "nd-core.h"
 #include "pfn.h"
 #include "nd.h"
 
