cc487d74b90fcf15797f792f761d78bff92c0777
Vaibhav Hiremath <vaibhav.hiremath@linaro.org>
2016-01-06 11:31:20 +0530

    greybus: arche-platform: Export gpio (reset & sys_boot) to user
    In order to allow user to flash the firmware to,
    
    SVC:
    user need to assert the reset first, set sysboot pin and
    deassert reset. And then issue a flashing command.
    
    And APB:
    User need to assert the reset first, and then issue flashing
    command.
    
    So this patch exports the gpio's to user.
    
    Signed-off-by: Vaibhav Hiremath <vaibhav.hiremath@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/arche-apb-ctrl.c
+++ drivers/staging/greybus/arche-apb-ctrl.c
@@ -72,6 +72,17 @@ static inline void deassert_gpio(unsigned int gpio)
 	gpio_set_value(gpio, 0);
 }
 
+/* Export gpio's to user space */
+static void export_gpios(struct arche_apb_ctrl_drvdata *apb)
+{
+	gpio_export(apb->resetn_gpio, false);
+}
+
+static void unexport_gpios(struct arche_apb_ctrl_drvdata *apb)
+{
+	gpio_unexport(apb->resetn_gpio);
+}
+
 static irqreturn_t apb_ctrl_wake_detect_irq(int irq, void *devid)
 {
 	struct arche_apb_ctrl_drvdata *apb = devid;
@@ -329,6 +340,8 @@ int arche_apb_ctrl_probe(struct platform_device *pdev)
 
 	assert_gpio(apb->resetn_gpio);
 
+	export_gpios(apb);
+
 	dev_info(&pdev->dev, "Device registered successfully\n");
 	return 0;
 
@@ -345,6 +358,7 @@ int arche_apb_ctrl_remove(struct platform_device *pdev)
 		apb_ctrl_cleanup(apb);
 
 	platform_set_drvdata(pdev, NULL);
+	unexport_gpios(apb);
 
 	return 0;
 }
