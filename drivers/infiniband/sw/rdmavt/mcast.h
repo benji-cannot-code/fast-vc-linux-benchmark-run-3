2576d72f97046a45dbfc83a408a8b8b3ed45b1e3
Dennis Dalessandro <dennis.dalessandro@intel.com>
2016-01-06 09:58:23 -0800

    IB/rdmavt: Add multicast stubs
    Adds the function stubs for attach and detach multicast.
    
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/sw/rdmavt/mcast.h
+++ drivers/infiniband/sw/rdmavt/mcast.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef DEF_RDMAVT_H
-#define DEF_RDMAVT_H
+#ifndef DEF_RVTMCAST_H
+#define DEF_RVTMCAST_H
 
 /*
  * Copyright(c) 2015 Intel Corporation.
@@ -50,12 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <rdma/rdma_vt.h>
-#include "dma.h"
-#include "pd.h"
-#include "qp.h"
-#include "ah.h"
-#include "mr.h"
-#include "srq.h"
-#include "mcast.h"
 
-#endif          /* DEF_RDMAVT_H */
+int rvt_attach_mcast(struct ib_qp *ibqp, union ib_gid *gid, u16 lid);
+int rvt_detach_mcast(struct ib_qp *ibqp, union ib_gid *gid, u16 lid);
+
+#endif          /* DEF_RVTMCAST_H */
