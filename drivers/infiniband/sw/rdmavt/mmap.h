25d3aef43420327e570749a1c9d9c2e0dd45ea00
Dennis Dalessandro <dennis.dalessandro@intel.com>
2016-01-06 10:04:57 -0800

    IB/rdmavt: Add mmap related functions
    The mmap data structure was moved in a previous commit. This patch now
    pulls in the related functions.
    
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/sw/rdmavt/mmap.h
+++ drivers/infiniband/sw/rdmavt/mmap.h
@@ -51,6 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <rdma/rdma_vt.h>
 
-int rvt_mmap(struct ib_ucontext *context, struct vm_area_struct *vma);
+void rvt_mmap_init(struct rvt_dev_info *rdi);
 
 #endif          /* DEF_RDMAVTMMAP_H */
