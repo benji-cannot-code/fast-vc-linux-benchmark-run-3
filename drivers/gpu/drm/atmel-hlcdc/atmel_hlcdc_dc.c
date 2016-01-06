bab70b214cb486699c2fe90ea809ad7e07e52a6d
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-01-06 10:16:32 +0100

    drm: atmel-hlcdc: rework the output code to support drm bridges
    The current output code only supports connection to drm panels.
    First simplify the drm panel code, and then add support for external drm
    bridges.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Tested-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_dc.c
+++ drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_dc.c
@@ -552,7 +552,7 @@ static int atmel_hlcdc_dc_modeset_init(struct drm_device *dev)
 
 	ret = atmel_hlcdc_create_outputs(dev);
 	if (ret) {
-		dev_err(dev->dev, "failed to create panel: %d\n", ret);
+		dev_err(dev->dev, "failed to create HLCDC outputs: %d\n", ret);
 		return ret;
 	}
 
