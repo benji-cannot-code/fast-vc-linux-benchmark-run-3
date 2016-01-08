d67ac70525e9d0b8a5ddaeed17bcb265b6d49e77
Russell Currey <ruscur@russell.cc>
2016-01-08 16:16:47 +1100

    powerpc/powernv: Remove misleading comment in pci.c
    PCI in powernv now supports quite a bit more than p5ioc2, so remove the
    outdated comment.
    
    Signed-off-by: Russell Currey <ruscur@russell.cc>
    Acked-by: Stewart Smith <stewart@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/pci.c
+++ arch/powerpc/platforms/powernv/pci.c
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Support PCI/PCIe on PowerNV platforms
  *
- * Currently supports only P5IOC2
- *
  * Copyright 2011 Benjamin Herrenschmidt, IBM Corp.
  *
  * This program is free software; you can redistribute it and/or
