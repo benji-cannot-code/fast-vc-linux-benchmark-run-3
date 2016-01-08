a6cef57e3f556330615ab0635531d4c2cb922566
Fabio Estevam <fabio.estevam@nxp.com>
2016-01-08 10:19:16 -0200

    net: lpc_eth: Remove unused variables
    Commit e7f4dc3536a400 ("mdio: Move allocation of interrupts into core")
    introduced the following build warnings:
    
    drivers/net/ethernet/nxp/lpc_eth.c: In function 'lpc_mii_init':
    drivers/net/ethernet/nxp/lpc_eth.c:865:1: warning: label 'err_out_1' defined but not used [-Wunused-label]
    drivers/net/ethernet/nxp/lpc_eth.c:826:20: warning: unused variable 'i' [-Wunused-variable]
    
    Remove the unused variables to fix them.
    
    Reported-by: Olof's autobuilder <build@lixom.net>
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Acked-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/nxp/lpc_eth.c
+++ drivers/net/ethernet/nxp/lpc_eth.c
@@ -824,7 +824,7 @@ static int lpc_mii_probe(struct net_device *ndev)
 
 static int lpc_mii_init(struct netdata_local *pldat)
 {
-	int err = -ENXIO, i;
+	int err = -ENXIO;
 
 	pldat->mii_bus = mdiobus_alloc();
 	if (!pldat->mii_bus) {
@@ -863,7 +863,6 @@ static int lpc_mii_init(struct netdata_local *pldat)
 
 err_out_unregister_bus:
 	mdiobus_unregister(pldat->mii_bus);
-err_out_1:
 	mdiobus_free(pldat->mii_bus);
 err_out:
 	return err;
