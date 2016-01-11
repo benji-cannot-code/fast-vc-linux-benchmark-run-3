6b99a9d5cf16dcf1ece7502a3da759d2d34a47c7
Wang Nan <wangnan0@huawei.com>
2016-01-11 13:48:03 +0000

    perf test: Reset err after using it hold errcode in hist testcases
    All hists test cases forget to reset err after using it to hold an
    error code. If error occure in setup_fake_machine() it incorrectly
    return TEST_OK.
    
    This patch fixes it.
    
    Suggested-and-Acked-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com
    Link: http://lkml.kernel.org/r/1452520124-2073-13-git-send-email-wangnan0@huawei.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/hists_link.c
+++ tools/perf/tests/hists_link.c
@@ -294,6 +294,7 @@ int test__hists_link(int subtest __maybe_unused)
 	if (err)
 		goto out;
 
+	err = TEST_FAIL;
 	/* default sort order (comm,dso,sym) will be used */
 	if (setup_sorting(NULL) < 0)
 		goto out;
