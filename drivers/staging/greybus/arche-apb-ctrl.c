d7859b62342a743ae19c809c8c2c09b998f12681
Viresh Kumar <viresh.kumar@linaro.org>
2016-01-11 11:29:17 +0530

    greybus: arche: Remove unwanted headers and rearrange others
    This removes few unwanted headers related to irq, interrupt, regulator,
    spinlock, etc.
    
    Also arrange the rest in alphabetical order to make it more readable.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Vaibhav Hiremath <vaibhav.hiremath@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/arche-apb-ctrl.c
+++ drivers/staging/greybus/arche-apb-ctrl.c
@@ -8,22 +8,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Released under the GPLv2 only.
  */
 
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <linux/sched.h>
-#include <linux/pm.h>
-#include <linux/delay.h>
-#include <linux/platform_device.h>
-#include <linux/gpio.h>
 #include <linux/clk.h>
-#include <linux/of_platform.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
-#include <linux/spinlock.h>
-#include <linux/regulator/consumer.h>
+#include <linux/module.h>
 #include <linux/pinctrl/consumer.h>
+#include <linux/platform_device.h>
+#include <linux/pm.h>
+#include <linux/regulator/consumer.h>
+#include <linux/spinlock.h>
 #include "arche_platform.h"
 
 enum apb_state {
