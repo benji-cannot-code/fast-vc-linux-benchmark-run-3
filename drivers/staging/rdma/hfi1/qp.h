c0cc61a4e34c335aab44c297e5ff948b6bd239c9
jubin.john@intel.com <jubin.john@intel.com>
2016-01-11 18:30:52 -0500

    staging/hfi1: Use BIT macro
    This patch fixes the checkpatch issue:
    CHECK: Prefer using the BIT macro
    
    Use of BIT macro for HDRQ_INCREMENT in chip.h causes a change in
    format specifier for error message in init.c in order to avoid a
    build warning.
    
    Reviewed-by: Dean Luick <dean.luick@intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Jubin John <jubin.john@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/staging/rdma/hfi1/qp.h
+++ drivers/staging/rdma/hfi1/qp.h
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "verbs.h"
 #include "sdma.h"
 
-#define QPN_MAX                 (1 << 24)
+#define QPN_MAX                 BIT(24)
 #define QPNMAP_ENTRIES          (QPN_MAX / PAGE_SIZE / BITS_PER_BYTE)
 
 /*
