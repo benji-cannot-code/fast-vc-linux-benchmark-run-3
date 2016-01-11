18fb656f8a184d347454a395eaa705551626bd09
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-01-11 13:13:12 -0800

    rcutorture: Update scripting to accommodate rcuperf
    This commit adds the scripting changes to add support for the shiny
    new rcuperf kernel module.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- tools/testing/selftests/rcutorture/bin/kvm-recheck.sh
+++ tools/testing/selftests/rcutorture/bin/kvm-recheck.sh
@@ -49,7 +49,10 @@ do
 				cat $i/Make.oldconfig.err
 			fi
 			parse-build.sh $i/Make.out $configfile
-			parse-torture.sh $i/console.log $configfile
+			if test "$TORTURE_SUITE" != rcuperf
+			then
+				parse-torture.sh $i/console.log $configfile
+			fi
 			parse-console.sh $i/console.log $configfile
 			if test -r $i/Warnings
 			then
