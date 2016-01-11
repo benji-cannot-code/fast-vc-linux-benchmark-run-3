ad27c01484a4a377b1441279119432ab499c3c21
Maor Gottlieb <maorg@mellanox.com>
2016-01-11 10:26:07 +0200

    IB/mlx5: Add flow steering support
    Adding flow steering support by creating a flow-table per
    priority (if rules exist in the priority). mlx5_ib uses
    autogrouping and thus only creates the required destinations.
    
    Also includes adding of these flow steering utilities
    
    1. Parsing verbs flow attributes hardware steering specs.
    
    2. Check if flow is multicast - this is required in order to decide
    to which flow table will we add the steering rule.
    
    3. Set outer headers in flow match criteria to zeros.
    
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>
    Signed-off-by: Moni Shoua <monis@mellanox.com>
    Signed-off-by: Matan Barak <matanb@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/mlx5/fs.h
+++ include/linux/mlx5/fs.h
@@ -39,6 +39,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MLX5_FS_DEFAULT_FLOW_TAG 0x0
 
+#define LEFTOVERS_RULE_NUM	 2
+static inline void build_leftovers_ft_param(int *priority,
+					    int *n_ent,
+					    int *n_grp)
+{
+	*priority = 0; /* Priority of leftovers_prio-0 */
+	*n_ent = LEFTOVERS_RULE_NUM;
+	*n_grp = LEFTOVERS_RULE_NUM;
+}
+
 enum mlx5_flow_namespace_type {
 	MLX5_FLOW_NAMESPACE_BYPASS,
 	MLX5_FLOW_NAMESPACE_KERNEL,
