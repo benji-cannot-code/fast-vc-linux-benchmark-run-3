3f1aec9ee1768b688b4deeff52a6e157ff58799e
Matias Bjørling <m@bjorling.me>
2016-01-12 07:49:36 +0100

    lightnvm: core on-disk initialization
    An Open-Channel SSD shall be initialized before use. To initialize, we
    define an on-disk format, that keeps a small set of metadata to bring up
    the media manager on top of the device.
    
    The initial step is introduced to allow a user to format the disks for a
    given media manager. During format, a system block is stored on one to
    three separate luns on the device. Each lun has the system block
    duplicated. During initialization, the system block can be retrieved and
    the appropriate media manager can initialized.
    
    The on-disk format currently covers (struct nvm_system_block):
    
     - Magic value "NVMS".
     - Monotonic increasing sequence number.
     - The physical block erase count.
     - Version of the system block format.
     - Media manager type.
     - Media manager superblock physical address.
    
    The interface provides three functions to manage the system block:
    
     int nvm_init_sysblock(struct nvm_dev *, struct nvm_sb_info *)
     int nvm_get_sysblock(struct nvm *dev, struct nvm_sb_info *)
     int nvm_update_sysblock(struct nvm *dev, struct nvm_sb_info *)
    
    Each implement a part of the logic to manage the system block. The
    initialization creates the first system blocks and mark them on the
    device. Get retrieves the latest system block by scanning all pages in
    the associated system blocks. The update sysblock writes new metadata
    and allocates new block if necessary.
    
    Signed-off-by: Matias Bjørling <m@bjorling.me>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- include/uapi/linux/lightnvm.h
+++ include/uapi/linux/lightnvm.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NVM_TTYPE_NAME_MAX 48
 #define NVM_TTYPE_MAX 63
+#define NVM_MMTYPE_LEN 8
 
 #define NVM_CTRL_FILE "/dev/lightnvm/control"
 
