fc03f786610813f14da3f9c4538ac15935ac161d
Mike Marshall <hubcap@omnibond.com>
2016-01-13 11:34:59 -0500

    Orangefs: change ORANGEFS_VERSION from "Unknown" to "upstream"
    Signed-off-by: Mike Marshall <hubcap@omnibond.com>

--- fs/orangefs/orangefs-mod.c
+++ fs/orangefs/orangefs-mod.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* ORANGEFS_VERSION is a ./configure define */
 #ifndef ORANGEFS_VERSION
-#define ORANGEFS_VERSION "Unknown"
+#define ORANGEFS_VERSION "upstream"
 #endif
 
 /*
