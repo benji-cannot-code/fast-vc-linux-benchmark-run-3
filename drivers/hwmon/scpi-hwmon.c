635ec55b976b20f4556309fbbf9d00f945d0d70d
Sudeep Holla <sudeep.holla@arm.com>
2016-01-14 17:58:02 +0000

    firmware: arm_scpi: add support for 64-bit sensor values
    SCPI specification version 1.1 extended the sensor from 32-bit to 64-bit
    values in order to accommodate new sensor class with 64-bit requirements
    
    Since the SCPI driver sets the higher 32-bit for older protocol version
    to zeros, there's no need to explicitly check the SCPI protocol version
    and the backward compatibility is maintainted.
    
    Acked-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Punit Agrawal <punit.agrawal@arm.com>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/hwmon/scpi-hwmon.c
+++ drivers/hwmon/scpi-hwmon.c
@@ -53,7 +53,7 @@ static int scpi_read_temp(void *dev, int *temp)
 	struct scpi_sensors *scpi_sensors = zone->scpi_sensors;
 	struct scpi_ops *scpi_ops = scpi_sensors->scpi_ops;
 	struct sensor_data *sensor = &scpi_sensors->data[zone->sensor_id];
-	u32 value;
+	u64 value;
 	int ret;
 
 	ret = scpi_ops->sensor_get_value(sensor->info.sensor_id, &value);
@@ -71,7 +71,7 @@ scpi_show_sensor(struct device *dev, struct device_attribute *attr, char *buf)
 	struct scpi_sensors *scpi_sensors = dev_get_drvdata(dev);
 	struct scpi_ops *scpi_ops = scpi_sensors->scpi_ops;
 	struct sensor_data *sensor;
-	u32 value;
+	u64 value;
 	int ret;
 
 	sensor = container_of(attr, struct sensor_data, dev_attr_input);
@@ -80,7 +80,7 @@ scpi_show_sensor(struct device *dev, struct device_attribute *attr, char *buf)
 	if (ret)
 		return ret;
 
-	return sprintf(buf, "%u\n", value);
+	return sprintf(buf, "%llu\n", value);
 }
 
 static ssize_t
