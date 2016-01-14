15f2879ff8faf8bce21a83e5abc6d1cd1d1ae9b8
Anton Wuerfel <anton.wuerfel@fau.de>
2016-01-14 16:08:12 +0100

    tty: serial: 8250: Fix braces after struct
    This patch fixes a checkpatch warning by moving an opening curly brace
    to its correct position.
    
    Signed-off-by: Anton Würfel <anton.wuerfel@fau.de>
    Signed-off-by: Phillip Raffeck <phillip.raffeck@fau.de>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Peter Hurley <peter@hurleysoftware.com>
    Cc: linux-kernel@i4.cs.fau.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_hp300.c
+++ drivers/tty/serial/8250/8250_hp300.c
@@ -25,8 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #ifdef CONFIG_HPAPCI
-struct hp300_port
-{
+struct hp300_port {
 	struct hp300_port *next;	/* next port */
 	int line;			/* line (tty) number */
 };
