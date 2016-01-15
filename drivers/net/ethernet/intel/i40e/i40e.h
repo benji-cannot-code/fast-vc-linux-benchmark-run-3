842aa5f6796ed094e44a72e94218690046c62d4a
Kiran Patil <kiran.patil@intel.com>
2016-01-15 14:33:14 -0800

    i40e: Removal of code which relies on BASE VEB SEID
    Fixed mapping of SEID is removed from specification. Hence
    this patch removes code which was using hard coded base VEB SEID.
    
    Changed FCoE code to use "hw->pf_id" to obtain correct "idx"
    and verified.
    
    Removed defines for BASE VSI/VEB SEID and BASE_PF_SEID since it
    is not used anymore.
    
    Change-ID: Id507cf4b1fae1c0145e3f08ae9ea5846ea5840de
    Signed-off-by: Kiran Patil <kiran.patil@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e.h
+++ drivers/net/ethernet/intel/i40e/i40e.h
@@ -65,9 +65,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i40e_dcb.h"
 
 /* Useful i40e defaults */
-#define I40E_BASE_PF_SEID     16
-#define I40E_BASE_VSI_SEID    512
-#define I40E_BASE_VEB_SEID    288
 #define I40E_MAX_VEB          16
 
 #define I40E_MAX_NUM_DESCRIPTORS      4096
