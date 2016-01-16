46bb691030eb5be248d3fa20e19b69d0c310fe17
Antonio Quartulli <antonio@open-mesh.com>
2016-01-16 16:40:14 +0800

    batman-adv: add throughput override attribute to hard_ifaces
    This attribute is exported to user space to disable the link
    throughput auto-detection by setting a fixed value.
    The throughput override value is used when batman-adv is
    computing the link throughput towards a neighbour.
    
    If the value is set to 0 then batman-adv will try to detect
    the throughput by itself.
    
    Signed-off-by: Antonio Quartulli <antonio@open-mesh.com>
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

--- net/batman-adv/bat_v.c
+++ net/batman-adv/bat_v.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bat_algo.h"
 #include "main.h"
 
+#include <linux/atomic.h>
 #include <linux/cache.h>
 #include <linux/init.h>
 
@@ -37,6 +38,11 @@ static int batadv_v_iface_enable(struct batadv_hard_iface *hard_iface)
 	ret = batadv_v_ogm_iface_enable(hard_iface);
 	if (ret < 0)
 		batadv_v_elp_iface_disable(hard_iface);
+
+	/* enable link throughput auto-detection by setting the throughput
+	 * override to zero
+	 */
+	atomic_set(&hard_iface->bat_v.throughput_override, 0);
 
 	return ret;
 }
