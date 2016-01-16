5ef4d52f0c68b8dcd69b784b6f0019b2e846afae
Antonio Quartulli <antonio@open-mesh.com>
2016-01-16 16:40:13 +0800

    batman-adv: OGMv2 - implement originators logic
    Add the support for recognising new originators in the
    network and rebroadcast their OGMs.
    
    Signed-off-by: Antonio Quartulli <antonio@open-mesh.com>
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>

--- net/batman-adv/main.h
+++ net/batman-adv/main.h
@@ -64,6 +64,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* B.A.T.M.A.N. V */
 #define BATADV_ELP_MAX_AGE 64
+#define BATADV_OGM_MAX_ORIGDIFF 5
+#define BATADV_OGM_MAX_AGE 64
 
 /* number of OGMs sent with the last tt diff */
 #define BATADV_TT_OGM_APPEND_MAX 3
