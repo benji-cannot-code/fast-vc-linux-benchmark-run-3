8f4aaff00694b77556f6bebc737beb0f3adebf3e
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2016-01-17 10:18:12 -0800

    Input: rotary_encoder - convert to use gpiod API
    Instead of using old GPIO API, let's switch to GPIOD API, which
    automatically handles polarity.
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/linux/rotary_encoder.h
+++ include/linux/rotary_encoder.h
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct rotary_encoder_platform_data {
 	unsigned int steps;
 	unsigned int axis;
-	unsigned int gpio_a;
-	unsigned int gpio_b;
-	unsigned int inverted_a;
-	unsigned int inverted_b;
 	unsigned int steps_per_period;
 	bool relative_axis;
 	bool rollover;
