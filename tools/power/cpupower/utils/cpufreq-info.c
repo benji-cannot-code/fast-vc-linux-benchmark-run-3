76226e7f19ccd6d170d00dc1e669802181603b0c
Shreyas B. Prabhu <shreyas@linux.vnet.ibm.com>
2016-01-18 20:44:43 +0530

    cpupower: Fix build error in cpufreq-info
    Fix the following build error by including limits.h -
    
    utils/cpufreq-info.c: In function ‘get_latency’:
    utils/cpufreq-info.c:437:29: error: ‘UINT_MAX’ undeclared (first use in
    this function)
      if (!latency || latency == UINT_MAX) {
                                 ^
    Signed-off-by: Shreyas B. Prabhu <shreyas@linux.vnet.ibm.com>
    Fixes: e98f033f94f3 (cpupower: fix how "cpupower frequency-info" interprets latency)
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- tools/power/cpupower/utils/cpufreq-info.c
+++ tools/power/cpupower/utils/cpufreq-info.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #include <getopt.h>
 
