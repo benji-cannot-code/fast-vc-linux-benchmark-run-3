6496b034bfade91b353c9c93b89a4228cd7d7871
Mike Marshall <hubcap@omnibond.com>
2016-01-19 12:04:40 -0500

    Orangefs: make gossip statement more palatable to xtensa
    Thanks to Intel's kbuild test robot
    
    Signed-off-by: Mike Marshall <hubcap@omnibond.com>

--- fs/orangefs/orangefs-kernel.h
+++ fs/orangefs/orangefs-kernel.h
@@ -76,9 +76,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ORANGEFS_MAX_MOUNT_OPT_LEN        0x00000080
 #define ORANGEFS_MAX_FSKEY_LEN            64
 
-#define MAX_DEV_REQ_UPSIZE (2*sizeof(__s32) +   \
+#define MAX_DEV_REQ_UPSIZE (2 * sizeof(__s32) +   \
 sizeof(__u64) + sizeof(struct orangefs_upcall_s))
-#define MAX_DEV_REQ_DOWNSIZE (2*sizeof(__s32) + \
+#define MAX_DEV_REQ_DOWNSIZE (2 * sizeof(__s32) + \
 sizeof(__u64) + sizeof(struct orangefs_downcall_s))
 
 /* borrowed from irda.h */
