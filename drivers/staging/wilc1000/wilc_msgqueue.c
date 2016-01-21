a1f83999e1735f2ca3aa633e2500b1f2589b3d55
Chaehyun Lim <chaehyun.lim@gmail.com>
2016-01-21 20:30:41 +0900

    staging: wilc1000: rename pHandle in wilc_mq_create
    This patch renames pHandle to mq to avoid camelcase
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_msgqueue.c
+++ drivers/staging/wilc1000/wilc_msgqueue.c
@@ -12,13 +12,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  @note		copied from FLO glue implementatuion
  *  @version		1.0
  */
-int wilc_mq_create(struct message_queue *pHandle)
+int wilc_mq_create(struct message_queue *mq)
 {
-	spin_lock_init(&pHandle->lock);
-	sema_init(&pHandle->sem, 0);
-	pHandle->msg_list = NULL;
-	pHandle->recv_count = 0;
-	pHandle->exiting = false;
+	spin_lock_init(&mq->lock);
+	sema_init(&mq->sem, 0);
+	mq->msg_list = NULL;
+	mq->recv_count = 0;
+	mq->exiting = false;
 	return 0;
 }
 
