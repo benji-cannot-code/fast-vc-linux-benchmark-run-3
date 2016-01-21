6878d612576a8f9ba421f7a16ff9378cdeabc683
Chaehyun Lim <chaehyun.lim@gmail.com>
2016-01-21 20:30:32 +0900

    staging: wilc1000: rename pvBuffer in struct message
    This patch renames pvBuffer to buf to avoid camelcase.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_msgqueue.h
+++ drivers/staging/wilc1000/wilc_msgqueue.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Message Queue type is a structure */
 struct message {
-	void *pvBuffer;
+	void *buf;
 	u32 u32Length;
 	struct message *pstrNext;
 };
