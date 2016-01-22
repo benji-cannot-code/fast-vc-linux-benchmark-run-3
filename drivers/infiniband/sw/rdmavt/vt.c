595ea3c971f1f7774f1ab368dbf9ea2f342bfdc8
Dennis Dalessandro <dennis.dalessandro@intel.com>
2016-01-22 13:04:51 -0800

    IB/rdmavt: Add mad agents to rdmavt
    This patch adds mad agent create and free to rdmavt.
    
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/sw/rdmavt/vt.c
+++ drivers/infiniband/sw/rdmavt/vt.c
@@ -417,6 +417,8 @@ int rvt_register_device(struct rvt_dev_info *rdi)
 		goto bail_cq;
 	}
 
+	rvt_create_mad_agents(rdi);
+
 	rvt_pr_info(rdi, "Registration with rdmavt done.\n");
 	return ret;
 
@@ -438,6 +440,8 @@ void rvt_unregister_device(struct rvt_dev_info *rdi)
 	trace_rvt_dbg(rdi, "Driver is unregistering.");
 	if (!rdi)
 		return;
+
+	rvt_free_mad_agents(rdi);
 
 	ib_unregister_device(&rdi->ibdev);
 	rvt_cq_exit(rdi);
