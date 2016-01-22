4e45d0ab953e71b038a81f7384cb6859d926ae6b
Dennis Dalessandro <dennis.dalessandro@intel.com>
2016-01-22 12:45:02 -0800

    IB/qib: Use rdmavt lid defines in qib
    Original patch for AH changes from Kamal Heib <kamalh@mellanox.com>, split
    apart from original. This patch also removes the qib specific multicast
    lid base and permissive lid defines since they are no longer needed.
    
    Use common LID defines in qib driver.
    
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Kamal Heib <kamalh@mellanox.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/qib/qib_qp.c
+++ drivers/infiniband/hw/qib/qib_qp.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/vmalloc.h>
 #include <linux/jhash.h>
+#include <rdma/rdma_vt.h>
 #ifdef CONFIG_DEBUG_FS
 #include <linux/seq_file.h>
 #endif
