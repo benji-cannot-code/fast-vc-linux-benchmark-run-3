be38760c320399164c8bd2198d9e7300b8132f88
Herbert Xu <herbert@gondor.apana.org.au>
2016-01-25 10:29:33 +0800

    eCryptfs: Use skcipher and shash
    This patch replaces uses of ablkcipher and blkcipher with skcipher,
    and the long obsolete hash interface with shash.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- fs/ecryptfs/inode.c
+++ fs/ecryptfs/inode.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dcache.h>
 #include <linux/namei.h>
 #include <linux/mount.h>
-#include <linux/crypto.h>
 #include <linux/fs_stack.h>
 #include <linux/slab.h>
 #include <linux/xattr.h>
