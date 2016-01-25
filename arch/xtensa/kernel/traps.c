b2970bc802768edce485b61f23a47bde6f969def
Max Filippov <jcmvbkbc@gmail.com>
2016-01-25 09:57:31 +0300

    xtensa: remove remaining non-functional KGDB bits
    KGDB is not supported on xtensa, but there are bits of related code
    under arch/xtensa/kernel. Remove these bits.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/kernel/traps.c
+++ arch/xtensa/kernel/traps.c
@@ -41,11 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 #include <asm/traps.h>
 
-#ifdef CONFIG_KGDB
-extern int gdb_enter;
-extern int return_from_debug_flag;
-#endif
-
 /*
  * Machine specific interrupt handlers
  */
@@ -345,20 +340,6 @@ do_unaligned_user (struct pt_regs *regs)
 void
 do_debug(struct pt_regs *regs)
 {
-#ifdef CONFIG_KGDB
-	/* If remote debugging is configured AND enabled, we give control to
-	 * kgdb.  Otherwise, we fall through, perhaps giving control to the
-	 * native debugger.
-	 */
-
-	if (gdb_enter) {
-		extern void gdb_handle_exception(struct pt_regs *);
-		gdb_handle_exception(regs);
-		return_from_debug_flag = 1;
-		return;
-	}
-#endif
-
 	__die_if_kernel("Breakpoint in kernel", regs, SIGKILL);
 
 	/* If in user mode, send SIGTRAP signal to current process */
