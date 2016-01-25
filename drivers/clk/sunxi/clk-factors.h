a90718238d1c9165827ba65c55374bb06dc3b414
Chen-Yu Tsai <wens@csie.org>
2016-01-25 21:15:43 +0800

    clk: sunxi: factors: Support custom formulas
    Some clocks cannot be modelled using the standard factors clk formula,
    such as clocks with special pre-dividers on one parent, or clocks
    with all power-of-two dividers.
    
    Add support for a custom .recalc callback for factors clk. Also pass
    the current parent index to the .get_factor and .recalc callbacks.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/clk/sunxi/clk-factors.h
+++ drivers/clk/sunxi/clk-factors.h
@@ -23,6 +23,7 @@ struct clk_factors_config {
 struct factors_request {
 	unsigned long rate;
 	unsigned long parent_rate;
+	u8 parent_index;
 	u8 n;
 	u8 k;
 	u8 m;
@@ -35,6 +36,7 @@ struct factors_data {
 	int muxmask;
 	const struct clk_factors_config *table;
 	void (*getter)(struct factors_request *req);
+	void (*recalc)(struct factors_request *req);
 	const char *name;
 };
 
@@ -43,6 +45,7 @@ struct clk_factors {
 	void __iomem *reg;
 	const struct clk_factors_config *config;
 	void (*get_factors)(struct factors_request *req);
+	void (*recalc)(struct factors_request *req);
 	spinlock_t *lock;
 	/* for cleanup */
 	struct clk_mux *mux;
