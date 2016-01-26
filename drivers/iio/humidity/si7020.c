25f0338b535b802b97a7aa3676e6a2b7eccf9807
Cristina Moraru <cristina.moraru09@gmail.com>
2016-01-26 22:17:49 +0200

    iio: si7020: add support for Hoperf th06
    This patch adds support for Hoperf th06 humidity and
    temperature sensor as it uses same register definitions
    as si7020
    
    th06 Datasheet: http://www.hoperf.com/upload/sensor/TH06.pdf
    
    Signed-off-by: Cristina Moraru <cristina.moraru09@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/humidity/si7020.c
+++ drivers/iio/humidity/si7020.c
@@ -150,6 +150,7 @@ static int si7020_probe(struct i2c_client *client,
 
 static const struct i2c_device_id si7020_id[] = {
 	{ "si7020", 0 },
+	{ "th06", 0 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, si7020_id);
