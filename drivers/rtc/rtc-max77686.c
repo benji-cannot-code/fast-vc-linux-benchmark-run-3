90394640c9927939ca9c0c9ea4cdc98b10ac774b
Javier Martinez Canillas <javier@osg.samsung.com>
2016-01-27 00:36:43 -0300

    rtc: max77686: Fix unsupported year message
    The max77686 RTC only supports a range of 99 years so instead of using
    year 1900 as the base, the year 2000 is used. This means that 1900 to
    1999 are unsupported years.
    
    The driver was printing a warning for those values but was returning a
    error so for consistency, print an error message instead and don't say
    that a year 2000 is assumed, since the year is not set.
    
    Also, it is better to use dev_* log functions instead of pr_* to print
    information about the device in the kernel log in a standardized way.
    
    This also allows to remove the local pr_fmt() defined macro.
    
    Suggested-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Reviewed-by: Andi Shyti <andi.shyti@samsung.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- drivers/rtc/rtc-max77686.c
+++ drivers/rtc/rtc-max77686.c
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
 #include <linux/slab.h>
 #include <linux/rtc.h>
 #include <linux/delay.h>
@@ -250,7 +248,7 @@ static int max77686_rtc_tm_to_data(struct rtc_time *tm, u8 *data,
 	data[RTC_YEAR] = tm->tm_year > 100 ? (tm->tm_year - 100) : 0;
 
 	if (tm->tm_year < 100) {
-		pr_warn("RTC cannot handle the year %d.  Assume it's 2000.\n",
+		dev_err(info->dev, "RTC cannot handle the year %d.\n",
 			1900 + tm->tm_year);
 		return -EINVAL;
 	}
