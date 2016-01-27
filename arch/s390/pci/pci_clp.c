8fc75fc1b17c7f74374e61415a0172914bac8497
Sebastian Ott <sebott@linux.vnet.ibm.com>
2016-01-27 13:33:30 +0100

    s390/pci: use unique UIDs for domain enumeration
    Use UIDs as domain numbers if the UID checking rules apply (in this
    case the FW guarantees uniqueness of these values).
    
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/pci/pci_clp.c
+++ arch/s390/pci/pci_clp.c
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/clp.h>
 #include <uapi/asm/clp.h>
 
+bool zpci_unique_uid;
+
 static inline void zpci_err_clp(unsigned int rsp, int rc)
 {
 	struct {
@@ -316,6 +318,7 @@ static int clp_list_pci(struct clp_req_rsp_list_pci *rrb,
 			goto out;
 		}
 
+		zpci_unique_uid = rrb->response.uid_checking;
 		WARN_ON_ONCE(rrb->response.entry_size !=
 			sizeof(struct clp_fh_list_entry));
 
