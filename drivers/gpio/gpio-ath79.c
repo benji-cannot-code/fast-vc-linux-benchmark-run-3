de5033f9df7203e0f2b949050a382f8e2818ce14
Alban Bedel <albeu@free.fr>
2016-01-28 20:44:30 +0100

    gpio: ath79: Allow building in compile tests
    To allow building the driver in compile tests we must drop the
    dependency on asm/mach-ath79/ar71xx_regs.h. For this we replace the
    include with local definition of the registers needed for this driver.
    
    Signed-off-by: Alban Bedel <albeu@free.fr>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-ath79.c
+++ drivers/gpio/gpio-ath79.c
@@ -17,7 +17,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/gpio-ath79.h>
 #include <linux/of_device.h>
 
-#include <asm/mach-ath79/ar71xx_regs.h>
+#define AR71XX_GPIO_REG_OE		0x00
+#define AR71XX_GPIO_REG_IN		0x04
+#define AR71XX_GPIO_REG_SET		0x0c
+#define AR71XX_GPIO_REG_CLEAR		0x10
 
 struct ath79_gpio_ctrl {
 	struct gpio_chip gc;
