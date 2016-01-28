b0d48c37b7f03924908b15a9ea9f70bd3dc5ab54
Sumit Saxena <sumit.saxena@avagotech.com>
2016-01-28 21:04:31 +0530

    megaraid_sas: IO throttling support
    This patch will add capability in driver to tell firmware that it can
    throttle IOs in case controller's queue depth is downgraded post OFU
    (online firmware upgrade). This feature will ensure firmware can be
    downgraded from higher queue depth to lower queue depth without needing
    system reboot. Added throttling code in IO path of driver, in case OS
    tries to send more IOs than post OFU firmware's queue depth.
    
    Signed-off-by: Sumit Saxena <sumit.saxena@avagotech.com>
    Signed-off-by: Kashyap Desai <kashyap.desai@avagotech.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/megaraid/megaraid_sas.h
+++ drivers/scsi/megaraid/megaraid_sas.h
@@ -1538,7 +1538,8 @@ union megasas_sgl_frame {
 typedef union _MFI_CAPABILITIES {
 	struct {
 #if   defined(__BIG_ENDIAN_BITFIELD)
-		u32     reserved:21;
+		u32     reserved:20;
+		u32     support_qd_throttling:1;
 		u32     support_fp_rlbypass:1;
 		u32     support_vfid_in_ioframe:1;
 		u32     support_ext_io_size:1;
@@ -1562,7 +1563,8 @@ typedef union _MFI_CAPABILITIES {
 		u32     support_ext_io_size:1;
 		u32     support_vfid_in_ioframe:1;
 		u32     support_fp_rlbypass:1;
-		u32     reserved:21;
+		u32     support_qd_throttling:1;
+		u32     reserved:20;
 #endif
 	} mfi_capabilities;
 	__le32		reg;
