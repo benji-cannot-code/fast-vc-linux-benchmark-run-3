943630b43bef37de032d4be7006d12f507e58a49
K. Y. Srinivasan <kys@microsoft.com>
2016-01-27 18:22:44 -0800

    storvsc: Install the storvsc specific timeout handler for FC devices
    The default timeout routine used for FC transport is not suitable for FC
    devices managed by storvsc since FC devices managed by storvsc driver do
    not have an rport associated with them. Use the time out handler used
    for SCSI devices for FC devices as well.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Reviewed-by: Alex Ng <alexng@microsoft.com>
    Tested-by: Vivek Yadav <vyadav@microsoft.com>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/storvsc_drv.c
+++ drivers/scsi/storvsc_drv.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/scsi_devinfo.h>
 #include <scsi/scsi_dbg.h>
 #include <scsi/scsi_transport_fc.h>
+#include <scsi/scsi_transport.h>
 
 /*
  * All wire protocol details (storage protocol between the guest and the host)
@@ -1771,6 +1772,11 @@ static int __init storvsc_drv_init(void)
 	fc_transport_template = fc_attach_transport(&fc_transport_functions);
 	if (!fc_transport_template)
 		return -ENODEV;
+
+	/*
+	 * Install Hyper-V specific timeout handler.
+	 */
+	fc_transport_template->eh_timed_out = storvsc_eh_timed_out;
 #endif
 
 	ret = vmbus_driver_register(&storvsc_drv);
