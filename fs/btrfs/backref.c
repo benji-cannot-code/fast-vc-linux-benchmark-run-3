71ed2981d7e10ab3f27c0b25254af22a09daad05
Kinglong Mee <kinglongmee@gmail.com>
2016-01-29 21:36:35 +0800

    btrfs: drop null testing before destroy functions
    Cleanup.
    
    kmem_cache_destroy has support NULL argument checking,
    so drop the double null testing before calling it.
    
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/backref.c
+++ fs/btrfs/backref.c
@@ -149,8 +149,7 @@ int __init btrfs_prelim_ref_init(void)
 
 void btrfs_prelim_ref_exit(void)
 {
-	if (btrfs_prelim_ref_cache)
-		kmem_cache_destroy(btrfs_prelim_ref_cache);
+	kmem_cache_destroy(btrfs_prelim_ref_cache);
 }
 
 /*
