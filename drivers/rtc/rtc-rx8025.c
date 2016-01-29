d5faaf1ee274ac5d7e2f035b80fd0385df22cef4
Leslie Lau <laulinxk@gmail.com>
2016-01-28 21:17:22 -0800

    drivers/rtc: broken link fix
    In drivers/rtc/rtc-rx8025.c is a broken link that is supposed to
    lead to a form allowing users to subscribe to the lm-sensors mailing list.
    
    The link <http://lists.lm-sensors.org/mailman/listinfo/lmsensors> leads
    to a page with a 404 error. I believe the link should be replaced
    with <lm-sensors@lm-sensors.org>.
    
    Signed-off-by: Leslie Lau <laulinxk@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/rtc/rtc-rx8025.c
+++ drivers/rtc/rtc-rx8025.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * All rights reserved.
  *
  * Modified by fengjh at rising.com.cn
- * <http://lists.lm-sensors.org/mailman/listinfo/lm-sensors>
+ * <lm-sensors@lm-sensors.org>
  * 2006.11
  *
  * Code cleanup by Sergei Poselenov, <sposelenov@emcraft.com>
