1fab4fdc4fd048ef8822b5d30ad129a98104bf93
Brian Norris <computersforpeace@gmail.com>
2016-01-29 11:25:30 -0800

    mtd: spi-nor: wait for SR_WIP to clear on initial unlock
    Fixup a piece leftover by commit 32321e950d8a ("mtd: spi-nor: wait until
    lock/unlock operations are ready"). That commit made us wait for the WIP
    bit to settle after lock/unlock operations, but it missed the open-coded
    "unlock" that happens at probe() time.
    
    We should probably have this code utilize the unlock() routines in the
    future, to avoid duplication, but unfortunately, flash which need to be
    unlocked don't all have a proper ->flash_unlock() callback.
    
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>
    Cc: Stas Sergeev <stsp@users.sourceforge.net>
    Reviewed-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
    Tested-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1194,6 +1194,7 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 	    JEDEC_MFR(info) == SNOR_MFR_SST) {
 		write_enable(nor);
 		write_sr(nor, 0);
+		spi_nor_wait_till_ready(nor);
 	}
 
 	if (!mtd->name)
