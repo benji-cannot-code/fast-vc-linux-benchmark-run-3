480fc5bfd63174c32a36fde3d866c8d609486e06
SirnamSwetha <theonly.ultimate@gmail.com>
2016-01-31 02:16:26 +0530

    Staging: clocking-wizard: Avoid CamelCase
    This patch fixes the checkpatch.pl issue:
    
    CHECK: Avoid CamelCase: <WZRD_CLkOUT0_FRAC_EN>
    
    CHECK: Avoid CamelCase: <WZRD_CLkFBOUT_FRAC_EN>
    
    Signed-off-by: SirnamSwetha <theonly.ultimate@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/clocking-wizard/clk-xlnx-clock-wizard.c
+++ drivers/staging/clocking-wizard/clk-xlnx-clock-wizard.c
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define WZRD_CLK_CFG_REG(n)	(0x200 + 4 * (n))
 
-#define WZRD_CLkOUT0_FRAC_EN	BIT(18)
-#define WZRD_CLkFBOUT_FRAC_EN	BIT(26)
+#define WZRD_CLKOUT0_FRAC_EN	BIT(18)
+#define WZRD_CLKFBOUT_FRAC_EN	BIT(26)
 
 #define WZRD_CLKFBOUT_MULT_SHIFT	8
 #define WZRD_CLKFBOUT_MULT_MASK		(0xff << WZRD_CLKFBOUT_MULT_SHIFT)
@@ -196,9 +196,9 @@ static int clk_wzrd_probe(struct platform_device *pdev)
 
 	/* we don't support fractional div/mul yet */
 	reg = readl(clk_wzrd->base + WZRD_CLK_CFG_REG(0)) &
-		    WZRD_CLkFBOUT_FRAC_EN;
+		    WZRD_CLKFBOUT_FRAC_EN;
 	reg |= readl(clk_wzrd->base + WZRD_CLK_CFG_REG(2)) &
-		     WZRD_CLkOUT0_FRAC_EN;
+		     WZRD_CLKOUT0_FRAC_EN;
 	if (reg)
 		dev_warn(&pdev->dev, "fractional div/mul not supported\n");
 
