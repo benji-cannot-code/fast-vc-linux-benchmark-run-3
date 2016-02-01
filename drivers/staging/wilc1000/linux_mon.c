441c696d8f4e38352cae18e74815b3380588c292
Chaehyun Lim <chaehyun.lim@gmail.com>
2016-02-01 21:26:43 +0900

    staging: wilc1000: linux_mon: insert blank line after a function
    This patch inserts a blank line afer function found by checkpatch.pl
    CHECK: Please use a blank line after function/struct/union/enum
    declarations
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_mon.c
+++ drivers/staging/wilc1000/linux_mon.c
@@ -153,6 +153,7 @@ static void mgmt_tx_complete(void *priv, int status)
 
 	kfree(pv_data);
 }
+
 static int mon_mgmt_tx(struct net_device *dev, const u8 *buf, size_t len)
 {
 	struct tx_complete_mon_data *mgmt_tx = NULL;
