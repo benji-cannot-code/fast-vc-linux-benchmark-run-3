a92f090a9923a317008e0a3593270622a3b582cf
Christian König <christian.koenig@amd.com>
2016-02-01 12:31:01 +0100

    drm/amdgpu: rename amdgpu_sched.c to amdgpu_job.c
    That's probably a better matching name.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucer@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_job.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_job.c
@@ -97,7 +97,7 @@ int amdgpu_job_submit(struct amdgpu_job *job, struct amdgpu_ring *ring,
 	return 0;
 }
 
-static struct fence *amdgpu_sched_dependency(struct amd_sched_job *sched_job)
+static struct fence *amdgpu_job_dependency(struct amd_sched_job *sched_job)
 {
 	struct amdgpu_job *job = to_amdgpu_job(sched_job);
 	struct amdgpu_sync *sync = &job->ibs->sync;
@@ -122,7 +122,7 @@ static struct fence *amdgpu_sched_dependency(struct amd_sched_job *sched_job)
 	return fence;
 }
 
-static struct fence *amdgpu_sched_run_job(struct amd_sched_job *sched_job)
+static struct fence *amdgpu_job_run(struct amd_sched_job *sched_job)
 {
 	struct fence *fence = NULL;
 	struct amdgpu_job *job;
@@ -147,6 +147,6 @@ err:
 }
 
 struct amd_sched_backend_ops amdgpu_sched_ops = {
-	.dependency = amdgpu_sched_dependency,
-	.run_job = amdgpu_sched_run_job,
+	.dependency = amdgpu_job_dependency,
+	.run_job = amdgpu_job_run,
 };
