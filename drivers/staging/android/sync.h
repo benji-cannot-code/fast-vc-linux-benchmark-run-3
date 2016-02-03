88d62e9575f07d59e7d2dfdaa4594985d423ecd9
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2016-02-03 11:25:30 -0200

    staging/android: remove SYNC_WAIT ioctl
    This ioctl is replicating the work of poll() syscall so let's take the
    opportunity that this is still on staging tree and remove the duplication
    and force new users to use the poll() standard interface.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/sync.h
+++ drivers/staging/android/sync.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ktime.h>
 #include <linux/list.h>
 #include <linux/spinlock.h>
-#include <linux/wait.h>
 #include <linux/fence.h>
 
 #include "uapi/sync.h"
@@ -230,18 +229,6 @@ void sync_file_put(struct sync_file *sync_file);
  * get_unused_fd_flags(O_CLOEXEC).
  */
 void sync_file_install(struct sync_file *sync_file, int fd);
-
-/**
- * sync_file_wait() - wait on sync file
- * @sync_file:	file to wait on
- * @tiemout:	timeout in ms
- *
- * Wait for @sync_file to be signaled or have an error. Waits indefinitely
- * if @timeout < 0.
- *
- * Returns 0 if fence signaled, > 0 if it is still active and <0 on error
- */
-int sync_file_wait(struct sync_file *sync_file, long timeout);
 
 #ifdef CONFIG_DEBUG_FS
 
