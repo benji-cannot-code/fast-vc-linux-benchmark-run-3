56a370053cdb2bb97e0abb85975a5d1f33f3f9a9
Paul Burton <paul.burton@imgtec.com>
2016-02-03 03:15:21 +0000

    MIPS: Detect MIPSr6 Virtual Processor support
    MIPSr6 introduces support for "Virtual Processors", which are
    conceptually similar to VPEs from the now-deprecated MT ASE. Detect
    whether the system supports VPs using the VP bit in Config5, adding
    cpu_has_vp for use by later patches.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: Maciej W. Rozycki <macro@imgtec.com>
    Cc: Joshua Kinard <kumba@gentoo.org>
    Cc: Steven J. Hill <sjhill@realitydiluted.com>
    Cc: Leonid Yegoshin <Leonid.Yegoshin@imgtec.com>
    Cc: James Hogan <james.hogan@imgtec.com>
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/12327/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -312,6 +312,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define cpu_has_mipsmt		(cpu_data[0].ases & MIPS_ASE_MIPSMT)
 #endif
 
+#ifndef cpu_has_vp
+#define cpu_has_vp		(cpu_data[0].options & MIPS_CPU_VP)
+#endif
+
 #ifndef cpu_has_userlocal
 #define cpu_has_userlocal	(cpu_data[0].options & MIPS_CPU_ULRI)
 #endif
