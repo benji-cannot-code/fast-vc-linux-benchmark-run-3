7ee1988e711a608dd655b243f4b533bd9e367ccd
John Garry <john.garry@huawei.com>
2016-02-04 02:26:08 +0800

    hisi_sas: use Unified Device Properties API
    The hisi_sas driver is required to support both device tree and
    ACPI. The scanning of the device properties now uses the Unified Device
    Properties API, which serves both OF and ACPI.
    
    Since syscon is not supported by ACPI, syscon is only used in the driver
    when device tree is used.
    
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -13,11 +13,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _HISI_SAS_H_
 #define _HISI_SAS_H_
 
+#include <linux/acpi.h>
 #include <linux/dmapool.h>
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
 #include <linux/of_address.h>
 #include <linux/platform_device.h>
+#include <linux/property.h>
 #include <linux/regmap.h>
 #include <scsi/sas_ata.h>
 #include <scsi/libsas.h>
