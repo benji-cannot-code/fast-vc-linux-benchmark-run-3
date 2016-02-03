3a6f975e260d3789f34c8abb82bca94a487b42f1
Xinming Hu <huxm@marvell.com>
2016-02-02 22:05:05 -0800

    mwifiex: keep original structure in decl header file
    memory_type_mapping strucuture did not refer to other mwifiex
    specific strture. A better software design method would keep
    it in decl header file, which does not include other mwifiex
    header file.
    
    Signed-off-by: Xinming Hu <huxm@marvell.com>
    Signed-off-by: Cathy Luo <cluo@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/mwifiex/pcie.h
+++ drivers/net/wireless/marvell/mwifiex/pcie.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include    <linux/pcieport_if.h>
 #include    <linux/interrupt.h>
 
+#include    "decl.h"
 #include    "main.h"
 
 #define PCIE8766_DEFAULT_FW_NAME "mrvl/pcie8766_uapsta.bin"
