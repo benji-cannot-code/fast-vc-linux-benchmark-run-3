728900cf81208924eaa54e259f564e7fccaa7783
Jes Sorensen <Jes.Sorensen@redhat.com>
2016-02-03 13:39:39 -0500

    rtl8xxxu: Define SYS_CFG_SW_OFFLOAD_EN
    This is needed for chips with IOL support.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_regs.h
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_regs.h
@@ -214,6 +214,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  SYS_CFG_PCIRSTB		BIT(4)
 #define  SYS_CFG_V15_VLD		BIT(5)
 #define  SYS_CFG_TRP_B15V_EN		BIT(7)
+#define  SYS_CFG_SW_OFFLOAD_EN		BIT(7)	/* For chips with IOL support */
 #define  SYS_CFG_SIC_IDLE		BIT(8)
 #define  SYS_CFG_BD_MAC2		BIT(9)
 #define  SYS_CFG_BD_MAC1		BIT(10)
