712b40e32aa97b4a03cb65e907466e0226ed1076
Paul Burton <paul.burton@imgtec.com>
2016-02-03 03:15:34 +0000

    MIPS: smp-cps: Add nothreads kernel parameter
    When debugging a new system or core it can be useful to disable the use
    of multithreading. Introduce a "nothreads" kernel command line parameter
    that can be set in order to do so.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: Matt Redfearn <matt.redfearn@imgtec.com>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Niklas Cassel <niklas.cassel@axis.com>
    Cc: Ezequiel Garcia <ezequiel.garcia@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/12340/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/smp-cps.c
+++ arch/mips/kernel/smp-cps.c
@@ -28,13 +28,24 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/time.h>
 #include <asm/uasm.h>
 
+static bool threads_disabled;
 static DECLARE_BITMAP(core_power, NR_CPUS);
 
 struct core_boot_config *mips_cps_core_bootcfg;
 
+static int __init setup_nothreads(char *s)
+{
+	threads_disabled = true;
+	return 0;
+}
+early_param("nothreads", setup_nothreads);
+
 static unsigned core_vpe_count(unsigned core)
 {
 	unsigned cfg;
+
+	if (threads_disabled)
+		return 1;
 
 	if ((!config_enabled(CONFIG_MIPS_MT_SMP) || !cpu_has_mipsmt)
 		&& (!config_enabled(CONFIG_CPU_MIPSR6) || !cpu_has_vp))
