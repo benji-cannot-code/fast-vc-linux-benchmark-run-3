0cac14c8bcfa6ef2e65c6f6260c75f803ebf93ce
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2016-02-03 16:40:05 -0500

    xen/blback: Fit the important information of the thread in 17 characters
    The processes names are truncated to 17, while we had the length
    of the process as name 20 - which meant that while we filled
    it out with various details - the last 3 characters (which had
    the queue number) never surfaced to the user-space.
    
    To simplify this and be able to fit the device name, domain id,
    and the queue number we remove the 'blkback' from the name.
    
    Prior to this patch the device name is "blkback.<domid>.<name>"
    for example: blkback.8.xvda, blkback.11.hda.
    
    With the multiqueue block backend we add "-%d" for the queue.
    But sadly this is already way past the limit so it gets stripped.
    
    Possible solution had been identified by Ian:
    http://lists.xenproject.org/archives/html/xen-devel/2015-05/msg03516.html
    
      "
      If you are pressed for space then the "xvd" is probably a bit redundant
      in a string which starts blkbk.
    
      The guest may not even call the device xvdN (iirc BSD has another
      prefix) any how, so having blkback say so seems of limited use anyway.
    
      Since this seems to not include a partition number how does this work in
      the split partition scheme? (i.e. one where the guest is given xvda1 and
      xvda2 rather than xvda with a partition table)
    
    [It will be 'blkback.8.xvda1', and 'blkback.11.xvda2']
    
      Perhaps something derived from one of the schemes in
      http://xenbits.xen.org/docs/unstable/misc/vbd-interface.txt might be a
      better fit?
    
    After a bit of discussion (see
    http://lists.xenproject.org/archives/html/xen-devel/2015-12/msg01588.html)
    we settled on dropping the "blback" part.
    
    This will make it possible to have the <domid>.<name>-<queue>:
    
     [1.xvda-0]
     [1.xvda-1]
    
    And we enough space to make it go up to:
    
     [32100.xvdfg9-5]
    
    Acked-by: Roger Pau Monné <roger.pau@citrix.com>
    Reported-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/block/xen-blkback/xenbus.c
+++ drivers/block/xen-blkback/xenbus.c
@@ -24,8 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <xen/grant_table.h>
 #include "common.h"
 
-/* Enlarge the array size in order to fully show blkback name. */
-#define BLKBACK_NAME_LEN (20)
+/* On the XenBus the max length of 'ring-ref%u'. */
 #define RINGREF_NAME_LEN (20)
 
 struct backend_info {
@@ -77,7 +76,7 @@ static int blkback_name(struct xen_blkif *blkif, char *buf)
 	else
 		devname  = devpath;
 
-	snprintf(buf, BLKBACK_NAME_LEN, "blkback.%d.%s", blkif->domid, devname);
+	snprintf(buf, TASK_COMM_LEN, "%d.%s", blkif->domid, devname);
 	kfree(devpath);
 
 	return 0;
@@ -86,7 +85,7 @@ static int blkback_name(struct xen_blkif *blkif, char *buf)
 static void xen_update_blkif_status(struct xen_blkif *blkif)
 {
 	int err;
-	char name[BLKBACK_NAME_LEN];
+	char name[TASK_COMM_LEN];
 	struct xen_blkif_ring *ring;
 	int i;
 
