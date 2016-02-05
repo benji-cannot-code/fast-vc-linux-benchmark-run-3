78de5f79a0e4964ea270fe2e69dd71d8d470803f
Javier Martinez Canillas <javier@osg.samsung.com>
2016-02-05 17:09:58 -0200

    [media] tvp5150: add HW input connectors support
    The tvp5150 decoder has different input connectors. The actual list of
    HW inputs depends on the device version but all have at least these 3:
    
    1) Composite0
    2) Composite1
    3) S-Video
    
    and some variants have a 4th possible input connector:
    
    4) Signal generator
    
    The driver currently uses the .s_routing callback to switch the input
    connector but since these are separate HW blocks, it's better to use
    media entities to represent the input connectors and their source pads
    linked with the decoder's sink pad.
    
    This allows user-space to use the MEDIA_IOC_SETUP_LINK ioctl to choose
    the input connector. For example using the media-ctl user-space tool:
    
    $ media-ctl -r -l '"Composite0":0->"tvp5150 1-005c":0[1]'
    
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- include/dt-bindings/media/tvp5150.h
+++ include/dt-bindings/media/tvp5150.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TVP5150_SVIDEO     2
 #define TVP5150_GENERATOR  3
 
+#define TVP5150_INPUT_NUM  4
+
 /* TVP5150 HW outputs */
 #define TVP5150_NORMAL       0
 #define TVP5150_BLACK_SCREEN 1
