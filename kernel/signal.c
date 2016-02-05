5b91139d2f8b98591714a703cd4448c045e275bf
Sasha Levin <sasha.levin@oracle.com>
2016-02-05 15:36:05 -0800

    signals: avoid random wakeups in sigsuspend()
    A random wakeup can get us out of sigsuspend() without TIF_SIGPENDING
    being set.
    
    Avoid that by making sure we were signaled, like sys_pause() does.
    
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Acked-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -3509,8 +3509,10 @@ static int sigsuspend(sigset_t *set)
 	current->saved_sigmask = current->blocked;
 	set_current_blocked(set);
 
-	__set_current_state(TASK_INTERRUPTIBLE);
-	schedule();
+	while (!signal_pending(current)) {
+		__set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+	}
 	set_restore_sigmask();
 	return -ERESTARTNOHAND;
 }
