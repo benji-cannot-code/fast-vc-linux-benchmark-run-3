7d30aecc6b0fb16df6d02d61073665a3a6bfbf86
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-02-05 09:56:07 +0900

    ALSA: firewire-tascam: remove a flag for controller
    Currently, 'struct snd_tscm_spec' has a member named as 'is_controller' to
    identify MIDI controller. This member was originally added to skip
    parse control and status messages in isochronous packets for non-controller
    model.
    
    As long as I investigate, FW-1804 (non-controller) also transfers the
    control and status message, thus it becomes meaningless.
    
    This commit removes it.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/tascam/tascam.c
+++ sound/firewire/tascam/tascam.c
@@ -22,7 +22,6 @@ static struct snd_tscm_spec model_specs[] = {
 		.pcm_playback_analog_channels = 8,
 		.midi_capture_ports = 4,
 		.midi_playback_ports = 4,
-		.is_controller = true,
 	},
 	{
 		.name = "FW-1082",
@@ -32,7 +31,6 @@ static struct snd_tscm_spec model_specs[] = {
 		.pcm_playback_analog_channels = 2,
 		.midi_capture_ports = 2,
 		.midi_playback_ports = 2,
-		.is_controller = true,
 	},
 	{
 		.name = "FW-1804",
@@ -42,7 +40,6 @@ static struct snd_tscm_spec model_specs[] = {
 		.pcm_playback_analog_channels = 2,
 		.midi_capture_ports = 2,
 		.midi_playback_ports = 4,
-		.is_controller = false,
 	},
 };
 
