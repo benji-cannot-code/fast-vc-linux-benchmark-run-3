31554dff82ceafa0a8c6e66bba8c30697e300e63
Mitko Haralanov <mitko.haralanov@intel.com>
2016-02-05 11:57:58 -0500

    staging/hfi1: Enable TID caching feature
    This commit "flips the switch" on the TID caching feature
    implemented in this patch series.
    
    As well as enabling the new feature by tying the new function
    with the PSM API, it also cleans up the old unneeded code,
    data structure members, and variables.
    
    Due to difference in operation and information, the tracing
    functions related to expected receives had to be changed. This
    patch include these changes.
    
    The tracing function changes could not be split into a separate
    commit without including both tracing variants at the same time.
    This would have caused other complications and ugliness.
    
    Signed-off-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- include/uapi/rdma/hfi/hfi1_user.h
+++ include/uapi/rdma/hfi/hfi1_user.h
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The major version changes when data structures change in an incompatible
  * way. The driver must be the same for initialization to succeed.
  */
-#define HFI1_USER_SWMAJOR 4
+#define HFI1_USER_SWMAJOR 5
 
 /*
  * Minor version differences are always compatible
@@ -242,11 +242,6 @@ struct hfi1_tid_info {
 	__u32 tidcnt;
 	/* length of transfer buffer programmed by this request */
 	__u32 length;
-	/*
-	 * pointer to bitmap of TIDs used for this call;
-	 * checked for being large enough at open
-	 */
-	__u64 tidmap;
 };
 
 struct hfi1_cmd {
