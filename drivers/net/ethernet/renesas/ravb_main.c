65e0559921e225a0e447a4334d3d543c671479be
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2016-02-06 17:46:35 +0300

    ravb: kill duplicate setting of CCC.CSEL
    When  adding support for the  R-Car gen3 gPTP active in configuration mode,
    the code setting the CCC.CSEL field  was duplicated due to an oversight.
    For R-Car gen 2 it's just redundant  and for R-Car gen3 the write at this
    time is probably  ignored due to CCC.GAC bit being already  set...
    
    Fixes: f5d7837f96e5 ("ravb: ptp: Add CONFIG mode support")
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/renesas/ravb_main.c
+++ drivers/net/ethernet/renesas/ravb_main.c
@@ -1815,10 +1815,6 @@ static int ravb_probe(struct platform_device *pdev)
 			   CCC_OPC_CONFIG | CCC_GAC | CCC_CSEL_HPB, CCC);
 	}
 
-	/* Set CSEL value */
-	ravb_write(ndev, (ravb_read(ndev, CCC) & ~CCC_CSEL) | CCC_CSEL_HPB,
-		   CCC);
-
 	/* Set GTI value */
 	error = ravb_set_gti(ndev);
 	if (error)
