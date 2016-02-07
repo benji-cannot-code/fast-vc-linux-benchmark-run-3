88f80c10464506bfd57622430de783067658c713
Stephen Boyd <sboyd@codeaurora.org>
2016-02-06 23:34:55 -0800

    clk: provider: Remove of_gpio_{gate,mux}_clk_setup() prototypes
    These functions either never existed or were only used in
    OF_CLK_DECLARE() macros. Remove the dead prototypes.
    
    Cc: Jyri Sarha <jsarha@ti.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -627,8 +627,6 @@ struct clk *clk_register_gpio_gate(struct device *dev, const char *name,
 		const char *parent_name, unsigned gpio, bool active_low,
 		unsigned long flags);
 
-void of_gpio_clk_gate_setup(struct device_node *node);
-
 /**
  * struct clk_gpio_mux - gpio controlled clock multiplexer
  *
@@ -643,8 +641,6 @@ extern const struct clk_ops clk_gpio_mux_ops;
 struct clk *clk_register_gpio_mux(struct device *dev, const char *name,
 		const char * const *parent_names, u8 num_parents, unsigned gpio,
 		bool active_low, unsigned long flags);
-
-void of_gpio_mux_clk_setup(struct device_node *node);
 
 /**
  * clk_register - allocate a new clock, register it and return an opaque cookie
