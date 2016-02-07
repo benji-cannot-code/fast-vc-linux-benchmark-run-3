509590e09de37db452860e1c105d949f78a7236f
Vineet Gupta <vgupta@synopsys.com>
2016-02-07 12:54:35 +0530

    ARCv2: intc: Allow interruption by lowest priority interrupt
    ARC HS Cores support configurable multiple interrupt priorities of upto
    16 levels.
    
    There is processor "interrupt preemption threshhold" in STATUS32.E[4:1]
    And several places need to set this up:
    1. seed value as kernel is booting
    2. seed value for user space programs
    3. Arg to SLEEP instruction in idle task (what interrupt prio can wake)
    4. Per-IRQ line prioirty (i.e. what is the priority of interrupt
       raised by a peripheral or timer or perf counter...
    
    Currently above sites use the highest priority 0. This can be potential
    problem when multiple priorities are supported. e.g. user space could
    only be interrupted by P0 interrupt, not others...
    So turn this over and instead make default interruption level to be
    the lowest priority possible 15. This should be fine even if there are
    fewer priority levels configured (say two: P0 HIGH, P1 LOW)
    
    This feature also effectively disables FIRQ feature if present in
    hardware config. With old code, a P0 interrupt would be FIRQ, needing
    special handling (ISR or Register Banks) which is NOT supported yet.
    Now it not be P0 (P15 or whatever is lowest prio) so FIRQ is not
    triggered.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/include/asm/irqflags-arcv2.h
+++ arch/arc/include/asm/irqflags-arcv2.h
@@ -31,8 +31,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Was Intr taken in User Mode */
 #define AUX_IRQ_ACT_BIT_U	31
 
-/* 0 is highest level, but taken by FIRQs, if present in design */
-#define ARCV2_IRQ_DEF_PRIO		0
+/*
+ * User space should be interruptable even by lowest prio interrupt
+ * Safe even if actual interrupt priorities is fewer or even one
+ */
+#define ARCV2_IRQ_DEF_PRIO	15
 
 /* seed value for status register */
 #define ISA_INIT_STATUS_BITS	(STATUS_IE_MASK | STATUS_AD_MASK | \
