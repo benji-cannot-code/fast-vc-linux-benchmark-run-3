62e022b33ada32306e51eab1bcae5f621b43cd85
Sergio Prado <sergio.prado@e-labworks.com>
2016-02-09 12:07:16 -0200

    net: macb: add wake-on-lan support via magic packet
    Tested on Acqua A5 SoM (http://www.acmesystems.it/acqua).
    
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cadence/macb.h
+++ drivers/net/ethernet/cadence/macb.h
@@ -313,6 +313,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MACB_PFR_SIZE		1
 #define MACB_PTZ_OFFSET		13 /* Enable pause time zero interrupt */
 #define MACB_PTZ_SIZE		1
+#define MACB_WOL_OFFSET		14 /* Enable wake-on-lan interrupt */
+#define MACB_WOL_SIZE		1
 
 /* Bitfields in MAN */
 #define MACB_DATA_OFFSET	0 /* data */
@@ -843,6 +845,8 @@ struct macb {
 
 	unsigned int		rx_frm_len_mask;
 	unsigned int		jumbo_max_len;
+
+	u32			wol;
 };
 
 static inline bool macb_is_gem(struct macb *bp)
