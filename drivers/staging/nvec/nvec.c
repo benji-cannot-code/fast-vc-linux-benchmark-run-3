8a794bf07b5ad954887a27eb359c0c6b3beb4980
Janani Ravichandran <janani.rvchndrn@gmail.com>
2016-02-09 14:52:51 -0500

    staging: nvec: Remove unneeded comment
    Remove this comment as it does not have a meaning here.
    
    Signed-off-by: Janani Ravichandran <janani.rvchndrn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/nvec/nvec.c
+++ drivers/staging/nvec/nvec.c
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-/* #define DEBUG */
-
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/atomic.h>
