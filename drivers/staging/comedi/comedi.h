b6274dec3bef5b587fbaa119c8d508d68940054d
Ian Abbott <abbotti@mev.co.uk>
2016-02-09 15:17:20 +0000

    staging: comedi: comedi.h: tweak start of header comment
    The wording at the start of the header comment is a bit misleading,
    particularly the part in parentheses: "(installed as
    /usr/include/comedi.h)".  This version of "comedi.h" certainly won't be
    installed as that pathname.  Fix it.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedi.h
+++ drivers/staging/comedi/comedi.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * include/comedi.h (installed as /usr/include/comedi.h)
- * header file for comedi
+ * comedi.h
+ * header file for COMEDI user API
  *
  * COMEDI - Linux Control and Measurement Device Interface
  * Copyright (C) 1998-2001 David A. Schleef <ds@schleef.org>
