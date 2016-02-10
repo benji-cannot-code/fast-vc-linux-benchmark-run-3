f6618db34cd5691df4c4e557c19899d3fd857011
Mike Rapoport <mike.rapoport@gmail.com>
2016-02-10 18:34:00 +0200

    staging: sm750fb: use BIT macro for VGA_CONFIGURATION single-bit fields
    Replace complex defintion of VGA_CONFIGURATION register fields with BIT()
    macro and use open-coded implementation for VGA_CONFIGURATION
    manipulations.
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_reg.h
+++ drivers/staging/sm750fb/ddk750_reg.h
@@ -549,12 +549,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VGA_CONFIGURATION                             0x000088
 #define VGA_CONFIGURATION_USER_DEFINE                 5:4
-#define VGA_CONFIGURATION_PLL                         2:2
-#define VGA_CONFIGURATION_PLL_VGA                     0
-#define VGA_CONFIGURATION_PLL_PANEL                   1
-#define VGA_CONFIGURATION_MODE                        1:1
-#define VGA_CONFIGURATION_MODE_TEXT                   0
-#define VGA_CONFIGURATION_MODE_GRAPHIC                1
+#define VGA_CONFIGURATION_PLL                         BIT(2)
+#define VGA_CONFIGURATION_MODE                        BIT(1)
 
 #endif
 
