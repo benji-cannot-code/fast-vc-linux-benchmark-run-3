sbourdelin/linux

kobject: export kset_find_obj() for module use
Signed-off-by: Gabriel Somlo <somlo@cmu.edu>

lib/kobject.c
@@ -861,6 +861,7 @@ struct kobject *kset_find_obj(struct kset *kset, const char *name)
 	spin_unlock(&kset->list_lock);
 	return ret;
 }
+EXPORT_SYMBOL(kset_find_obj);
 
 static void kset_release(struct kobject *kobj)
 {

benji-cannot-code/fast-vc-linux-benchmark-run-3

Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

lib/kobject.c
@@ -862,6 +862,7 @@ struct kobject *kset_find_obj(struct kset *kset, const char *name)
 	spin_unlock(&kset->list_lock);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(kset_find_obj);
 
 static void kset_release(struct kobject *kobj)
 {