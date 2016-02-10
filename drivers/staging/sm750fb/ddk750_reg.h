c8b6df41ae755b9268f1ea0eb498df0dd6766873
Mike Rapoport <mike.rapoport@gmail.com>
2016-02-10 18:34:13 +0200

    staging: sm750fb: change definition of PANEL_WINDOW_WIDTH fields
    Use stratight-forward defintion of PANEL_WINDOW_WIDTH register fields and
    use open-coded implementation for register manipulations
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_reg.h
+++ drivers/staging/sm750fb/ddk750_reg.h
@@ -839,8 +839,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PANEL_FB_WIDTH_OFFSET_MASK                    0x3fff
 
 #define PANEL_WINDOW_WIDTH                            0x080014
-#define PANEL_WINDOW_WIDTH_WIDTH                      27:16
-#define PANEL_WINDOW_WIDTH_X                          11:0
+#define PANEL_WINDOW_WIDTH_WIDTH_SHIFT                16
+#define PANEL_WINDOW_WIDTH_WIDTH_MASK                 (0xfff << 16)
+#define PANEL_WINDOW_WIDTH_X_MASK                     0xfff
 
 #define PANEL_WINDOW_HEIGHT                           0x080018
 #define PANEL_WINDOW_HEIGHT_HEIGHT                    27:16
