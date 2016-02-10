5a1b816ac0b4127a810407d66cd52052d86c1b68
Mike Rapoport <mike.rapoport@gmail.com>
2016-02-10 18:34:04 +0200

    staging: sm750fb: share common bits in display control registers
    The display control registers for primary and secondary display share some
    of the bits and those bits can be defined in a single place and then used
    for manipulations of the relevant registers.
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_display.c
+++ drivers/staging/sm750fb/ddk750_display.c
@@ -24,12 +24,10 @@ static void setDisplayControl(int ctrl, int disp_state)
 			 * because changing at the same time does not guarantee that
 			 * the plane will also enabled or disabled.
 			 */
-			reg = FIELD_SET(reg,
-								PANEL_DISPLAY_CTRL, TIMING, ENABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, TIMING, ENABLE);
 			POKE32(PANEL_DISPLAY_CTRL, reg);
 
-			reg = FIELD_SET(reg,
-								PANEL_DISPLAY_CTRL, PLANE, ENABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, PLANE, ENABLE);
 
 			/* Added some masks to mask out the reserved bits.
 			 * Sometimes, the reserved bits are set/reset randomly when
@@ -58,12 +56,10 @@ static void setDisplayControl(int ctrl, int disp_state)
 			 * next vertical sync. Need to find out if it is necessary to
 			 * wait for 1 vsync before modifying the timing enable bit.
 			 * */
-			reg = FIELD_SET(reg,
-								PANEL_DISPLAY_CTRL, PLANE, DISABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, PLANE, DISABLE);
 			POKE32(PANEL_DISPLAY_CTRL, reg);
 
-			reg = FIELD_SET(reg,
-								PANEL_DISPLAY_CTRL, TIMING, DISABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, TIMING, DISABLE);
 			POKE32(PANEL_DISPLAY_CTRL, reg);
 		}
 
@@ -75,12 +71,10 @@ static void setDisplayControl(int ctrl, int disp_state)
 			/* Timing should be enabled first before enabling the plane because changing at the
 			   same time does not guarantee that the plane will also enabled or disabled.
 			   */
-			reg = FIELD_SET(reg,
-								CRT_DISPLAY_CTRL, TIMING, ENABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, TIMING, ENABLE);
 			POKE32(CRT_DISPLAY_CTRL, reg);
 
-			reg = FIELD_SET(reg,
-								CRT_DISPLAY_CTRL, PLANE, ENABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, PLANE, ENABLE);
 
 			/* Added some masks to mask out the reserved bits.
 			 * Sometimes, the reserved bits are set/reset randomly when
@@ -107,12 +101,10 @@ static void setDisplayControl(int ctrl, int disp_state)
 			 * vertical sync. Need to find out if it is necessary to
 			 * wait for 1 vsync before modifying the timing enable bit.
 			 */
-			reg = FIELD_SET(reg,
-								CRT_DISPLAY_CTRL, PLANE, DISABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, PLANE, DISABLE);
 			POKE32(CRT_DISPLAY_CTRL, reg);
 
-			reg = FIELD_SET(reg,
-								CRT_DISPLAY_CTRL, TIMING, DISABLE);
+			reg = FIELD_SET(reg, DISPLAY_CTRL, TIMING, DISABLE);
 			POKE32(CRT_DISPLAY_CTRL, reg);
 		}
 	}
@@ -128,8 +120,9 @@ static void waitNextVerticalSync(int ctrl, int delay)
 		/* Do not wait when the Primary PLL is off or display control is already off.
 		   This will prevent the software to wait forever. */
 		if (!(PEEK32(PANEL_PLL_CTRL) & PLL_CTRL_POWER) ||
-			(FIELD_GET(PEEK32(PANEL_DISPLAY_CTRL), PANEL_DISPLAY_CTRL, TIMING) ==
-			 PANEL_DISPLAY_CTRL_TIMING_DISABLE)) {
+			(FIELD_GET(PEEK32(PANEL_DISPLAY_CTRL),
+				DISPLAY_CTRL, TIMING) ==
+				DISPLAY_CTRL_TIMING_DISABLE)) {
 			return;
 		}
 
@@ -150,8 +143,9 @@ static void waitNextVerticalSync(int ctrl, int delay)
 		/* Do not wait when the Primary PLL is off or display control is already off.
 			   This will prevent the software to wait forever. */
 		if (!(PEEK32(CRT_PLL_CTRL) & PLL_CTRL_POWER) ||
-			(FIELD_GET(PEEK32(CRT_DISPLAY_CTRL), CRT_DISPLAY_CTRL, TIMING) ==
-			 CRT_DISPLAY_CTRL_TIMING_DISABLE)) {
+			(FIELD_GET(PEEK32(CRT_DISPLAY_CTRL),
+				DISPLAY_CTRL, TIMING) ==
+				DISPLAY_CTRL_TIMING_DISABLE)) {
 			return;
 		}
 
