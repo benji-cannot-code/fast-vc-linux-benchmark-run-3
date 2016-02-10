0f911a90c8532a86b51c6984c9bccdc6a5257499
Mike Rapoport <mike.rapoport@gmail.com>
2016-02-10 18:34:01 +0200

    staging: sm750fb: change definition of VGA_CONFIGURATION multi-bit field
    Use more straight-forward definitions for multi-bit field of
    VGA_CONFIGURATION register.
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/ddk750_reg.h
+++ drivers/staging/sm750fb/ddk750_reg.h
@@ -548,7 +548,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MXCLK_PLL_CTRL                                0x000070
 
 #define VGA_CONFIGURATION                             0x000088
-#define VGA_CONFIGURATION_USER_DEFINE                 5:4
+#define VGA_CONFIGURATION_USER_DEFINE_MASK            (0x3 << 4)
 #define VGA_CONFIGURATION_PLL                         BIT(2)
 #define VGA_CONFIGURATION_MODE                        BIT(1)
 
