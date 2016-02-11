c74ec4af6a2a4a2579a326ea8d650203214e96ca
Linus Walleij <linus.walleij@linaro.org>
2016-02-11 11:37:48 +0100

    gpio: move the subdriver data pointer into gpio_device
    We move to manage this pointer under gpiolib control rather than
    leave it in the subdevice's gpio_chip. We can not NULL it after
    gpiochip_remove so at to keep things tight.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib.h
+++ drivers/gpio/gpiolib.h
@@ -38,6 +38,7 @@ struct acpi_device;
  * of the @descs array.
  * @base: GPIO base in the DEPRECATED global Linux GPIO numberspace, assigned
  * at device creation time.
+ * @data: per-instance data assigned by the driver
  * @list: links gpio_device:s together for traversal
  *
  * This state container holds most of the runtime variable data
@@ -55,6 +56,7 @@ struct gpio_device {
 	struct gpio_desc	*descs;
 	int			base;
 	u16			ngpio;
+	void			*data;
 	struct list_head        list;
 
 #ifdef CONFIG_PINCTRL
