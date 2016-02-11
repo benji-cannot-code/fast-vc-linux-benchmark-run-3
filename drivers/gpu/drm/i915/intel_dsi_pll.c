6e66c9bd1bb1a888a300db5127956adb95805c11
Deepak M <m.deepak@intel.com>
2016-02-11 20:33:27 +0530

    drm/i915/dsi: Using the bpp value wrt the pixel format
    The bpp value which is used while calulating the txbyteclkhs values
    should be wrt the pixel format value. Currently bpp is coming
    from pipe config to calculate txbyteclkhs. Fix it in this patch.
    
    V2: dsi_pixel_format_bpp is used to retrieve the bpp from pixel_format
    	[Review: Jani]
    
    Signed-off-by: Deepak M <m.deepak@intel.com>
    Signed-off-by: Yogesh Mohan Marimuthu <yogesh.mohan.marimuthu@intel.com>
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Tested-by: Mika Kahola <mika.kahola@intel.com> # BYT
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1455203007-10850-1-git-send-email-ramalingam.c@intel.com

--- drivers/gpu/drm/i915/intel_dsi_pll.c
+++ drivers/gpu/drm/i915/intel_dsi_pll.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_drv.h"
 #include "intel_dsi.h"
 
-static int dsi_pixel_format_bpp(int pixel_format)
+int dsi_pixel_format_bpp(int pixel_format)
 {
 	int bpp;
 
