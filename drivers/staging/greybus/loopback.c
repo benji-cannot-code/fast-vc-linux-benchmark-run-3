bc78d1a45f3e77cddbe10a72d2b782a56f7b3fed
Johan Hovold <johan@hovoldconsulting.com>
2016-02-11 13:52:49 +0100

    greybus: loopback: add missing pr_fmt
    Add missing pr_fmt so we can at least tell what module the sole
    remaining pr_err was from.
    
    Testing Done:
    Tested on DB3.5 with the generic bridge firmware on APB2.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/loopback.c
+++ drivers/staging/greybus/loopback.c
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Released under the GPLv2 only.
  */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
