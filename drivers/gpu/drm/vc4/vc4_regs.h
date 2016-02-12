00c77bc9837f42701a558f0f3c49253996df8934
Eric Anholt <eric@anholt.net>
2016-02-12 14:15:14 -0800

    drm/vc4: Bring HDMI up from power off if necessary.
    If the firmware hadn't brought up HDMI for us, we need to do its
    power-on reset sequence (reset HD and and clear its STANDBY bits,
    reset HDMI, and leave the PHY disabled).
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/vc4/vc4_regs.h
+++ drivers/gpu/drm/vc4/vc4_regs.h
@@ -457,6 +457,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VC4_HDMI_TX_PHY_RESET_CTL		0x2c0
 
 #define VC4_HD_M_CTL				0x00c
+# define VC4_HD_M_REGISTER_FILE_STANDBY		(3 << 6)
+# define VC4_HD_M_RAM_STANDBY			(3 << 4)
 # define VC4_HD_M_SW_RST			BIT(2)
 # define VC4_HD_M_ENABLE			BIT(0)
 
