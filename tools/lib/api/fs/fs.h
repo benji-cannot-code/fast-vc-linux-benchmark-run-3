66a23dba28d3395e923a7221000b590a9792eddd
Jiri Olsa <jolsa@kernel.org>
2016-02-14 17:03:44 +0100

    tools lib api fs: Add sysfs__read_str function
    Adding sysfs__read_str function to ease up reading string files from
    sysfs. New interface is:
    
      int sysfs__read_str(const char *entry, char **buf, size_t *sizep);
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1455465826-8426-4-git-send-email-jolsa@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/lib/api/fs/fs.h
+++ tools/lib/api/fs/fs.h
@@ -33,4 +33,5 @@ int filename__read_str(const char *filename, char **buf, size_t *sizep);
 int sysctl__read_int(const char *sysctl, int *value);
 int sysfs__read_int(const char *entry, int *value);
 int sysfs__read_ull(const char *entry, unsigned long long *value);
+int sysfs__read_str(const char *entry, char **buf, size_t *sizep);
 #endif /* __API_FS__ */
