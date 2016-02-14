75d4dc86f6dff91a471bcb743d99d8c0f3d1d2f3
Eli Cohen <eli@mellanox.com>
2016-02-14 17:07:48 +0200

    IB/core: IB/core: Allow legacy verbs through extended interfaces
    When an extended verb is an extension to a legacy verb, the original
    functionality is preserved. Hence we do not require each hardware driver
    to set the extended capability. This will allow the use of the extended
    verb in its simple form with drivers that do not support the extended
    capability.
    
    Signed-off-by: Eli Cohen <eli@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/core/uverbs_main.c
+++ drivers/infiniband/core/uverbs_main.c
@@ -684,6 +684,21 @@ out:
 	return ev_file;
 }
 
+static int verify_command_mask(struct ib_device *ib_dev, __u32 command)
+{
+	u64 mask;
+
+	if (command <= IB_USER_VERBS_CMD_OPEN_QP)
+		mask = ib_dev->uverbs_cmd_mask;
+	else
+		mask = ib_dev->uverbs_ex_cmd_mask;
+
+	if (mask & ((u64)1 << command))
+		return 0;
+
+	return -1;
+}
+
 static ssize_t ib_uverbs_write(struct file *filp, const char __user *buf,
 			     size_t count, loff_t *pos)
 {
@@ -716,6 +731,10 @@ static ssize_t ib_uverbs_write(struct file *filp, const char __user *buf,
 	}
 
 	command = hdr.command & IB_USER_VERBS_CMD_COMMAND_MASK;
+	if (verify_command_mask(ib_dev, command)) {
+		ret = -EOPNOTSUPP;
+		goto out;
+	}
 
 	flags = (hdr.command &
 		 IB_USER_VERBS_CMD_FLAGS_MASK) >> IB_USER_VERBS_CMD_FLAGS_SHIFT;
@@ -730,11 +749,6 @@ static ssize_t ib_uverbs_write(struct file *filp, const char __user *buf,
 		if (!file->ucontext &&
 		    command != IB_USER_VERBS_CMD_GET_CONTEXT) {
 			ret = -EINVAL;
-			goto out;
-		}
-
-		if (!(ib_dev->uverbs_cmd_mask & (1ull << command))) {
-			ret = -ENOSYS;
 			goto out;
 		}
 
@@ -762,11 +776,6 @@ static ssize_t ib_uverbs_write(struct file *filp, const char __user *buf,
 
 		if (!file->ucontext) {
 			ret = -EINVAL;
-			goto out;
-		}
-
-		if (!(ib_dev->uverbs_ex_cmd_mask & (1ull << command))) {
-			ret = -ENOSYS;
 			goto out;
 		}
 
