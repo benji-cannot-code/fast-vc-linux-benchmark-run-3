22f2a577487d04a33e00ad5b4b2dc37102d20619
Cristina Moraru <cristina.moraru09@gmail.com>
2016-02-15 00:37:39 +0200

    iio: hmc5843: Move hmc5843 out of staging
    This patch moves hmc5843 driver from staging/iio/magnetometer
    to iio/magnetometer, updates the corresponding Makefiles and
    moves the hmc5843* entries to the 'Industrial I/O support ->
    Magnetometer sensors' menu.
    
    Signed-off-by: Cristina Moraru <cristina.moraru09@gmail.com>
    Cc: Daniel Baluta <daniel.baluta@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/magnetometer/hmc5843_core.c
+++ drivers/iio/magnetometer/hmc5843_core.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  */
 
 #include <linux/module.h>
@@ -596,9 +595,9 @@ static int hmc5843_init(struct hmc5843_data *data)
 
 static const struct iio_info hmc5843_info = {
 	.attrs = &hmc5843_group,
-	.read_raw = hmc5843_read_raw,
-	.write_raw = hmc5843_write_raw,
-	.write_raw_get_fmt = hmc5843_write_raw_get_fmt,
+	.read_raw = &hmc5843_read_raw,
+	.write_raw = &hmc5843_write_raw,
+	.write_raw_get_fmt = &hmc5843_write_raw_get_fmt,
 	.driver_module = THIS_MODULE,
 };
 
