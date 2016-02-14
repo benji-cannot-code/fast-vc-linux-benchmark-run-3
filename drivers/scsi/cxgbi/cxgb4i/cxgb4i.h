1d2077e8f5bc46c45c7c790824007239842b27d1
Varun Prakash <varun@chelsio.com>
2016-02-14 23:04:09 +0530

    cxgb4, iw_cxgb4, cxgb4i: remove duplicate definitions
    move struct ulptx_idata definition to
    common header file t4_msg.h.
    
    Signed-off-by: Varun Prakash <varun@chelsio.com>
    Acked-by: Hariprasad Shenai <hariprasad@chelsio.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/scsi/cxgbi/cxgb4i/cxgb4i.h
+++ drivers/scsi/cxgbi/cxgb4i/cxgb4i.h
@@ -26,11 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define T5_ISS_VALID		(1 << 18)
 
-struct ulptx_idata {
-	__be32 cmd_more;
-	__be32 len;
-};
-
 struct cpl_rx_data_ddp {
 	union opcode_tid ot;
 	__be16 urg;
