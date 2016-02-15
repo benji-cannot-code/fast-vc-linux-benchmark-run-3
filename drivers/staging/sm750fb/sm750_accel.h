1a32522a3a494a9c60701357e27d2967d011feb5
Mike Rapoport <mike.rapoport@gmail.com>
2016-02-15 19:54:03 +0200

    staging: sm750fb: change definition of DE_WINDOW_WIDTH fields
    Use stratight-forward definition of DE_WINDOW_WIDTH register fields
    and use open-coded implementation for register manipulation
    
    Signed-off-by: Mike Rapoport <mike.rapoport@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/sm750_accel.h
+++ drivers/staging/sm750fb/sm750_accel.h
@@ -158,8 +158,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DE_MONO_PATTERN_HIGH_PATTERN                    31:0
 
 #define DE_WINDOW_WIDTH                                 0x00003C
-#define DE_WINDOW_WIDTH_DESTINATION                     28:16
-#define DE_WINDOW_WIDTH_SOURCE                          12:0
+#define DE_WINDOW_WIDTH_DST_SHIFT                       16
+#define DE_WINDOW_WIDTH_DST_MASK                        (0x1fff << 16)
+#define DE_WINDOW_WIDTH_SRC_MASK                        0x1fff
 
 #define DE_WINDOW_SOURCE_BASE                           0x000040
 #define DE_WINDOW_SOURCE_BASE_EXT                       27:27
