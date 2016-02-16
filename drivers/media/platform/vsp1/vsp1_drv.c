ba5c467b2aa565c70202e59221311a434355017a
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2016-02-16 11:49:39 -0200

    [media] v4l: vsp1: Don't handle clocks manually
    The power domain performs functional clock handling when using runtime
    PM, there's no need to enable and disable the clock manually.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/vsp1/vsp1_drv.c
+++ drivers/media/platform/vsp1/vsp1_drv.c
@@ -515,10 +515,6 @@ static int vsp1_pm_resume(struct device *dev)
 
 static int vsp1_pm_runtime_suspend(struct device *dev)
 {
-	struct vsp1_device *vsp1 = dev_get_drvdata(dev);
-
-	clk_disable_unprepare(vsp1->clock);
-
 	return 0;
 }
 
@@ -527,16 +523,10 @@ static int vsp1_pm_runtime_resume(struct device *dev)
 	struct vsp1_device *vsp1 = dev_get_drvdata(dev);
 	int ret;
 
-	ret = clk_prepare_enable(vsp1->clock);
-	if (ret < 0)
-		return ret;
-
 	if (vsp1->info) {
 		ret = vsp1_device_init(vsp1);
-		if (ret < 0) {
-			clk_disable_unprepare(vsp1->clock);
+		if (ret < 0)
 			return ret;
-		}
 	}
 
 	return 0;
@@ -641,17 +631,11 @@ static int vsp1_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, vsp1);
 
-	/* I/O, IRQ and clock resources */
+	/* I/O and IRQ resources (clock managed by the clock PM domain) */
 	io = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	vsp1->mmio = devm_ioremap_resource(&pdev->dev, io);
 	if (IS_ERR(vsp1->mmio))
 		return PTR_ERR(vsp1->mmio);
-
-	vsp1->clock = devm_clk_get(&pdev->dev, NULL);
-	if (IS_ERR(vsp1->clock)) {
-		dev_err(&pdev->dev, "failed to get clock\n");
-		return PTR_ERR(vsp1->clock);
-	}
 
 	irq = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
 	if (!irq) {
