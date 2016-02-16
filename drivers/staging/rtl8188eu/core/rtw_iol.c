dfd431920644313efc347f41c7265fcf1ad0e8e8
Colin Vidal <colin@cvidal.org>
2016-02-16 23:12:17 +0100

    Staging: rtl8188eu/core: Coding style fix, set conform spaces between identifiers
    And a space after include keyword, making the preprocessor more
    readable, and remove an unexpected space between a type and variable
    name.
    
    Signed-off-by: Colin Vidal <colin@cvidal.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_iol.c
+++ drivers/staging/rtl8188eu/core/rtw_iol.c
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  ******************************************************************************/
 
-#include<rtw_iol.h>
+#include <rtw_iol.h>
 
-bool rtw_IOL_applied(struct adapter  *adapter)
+bool rtw_IOL_applied(struct adapter *adapter)
 {
 	if (1 == adapter->registrypriv.fw_iol)
 		return true;
