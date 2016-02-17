b827bcc531259fac3e9fb684fe49faa004a578c6
Shraddha Barke <shraddha.6596@gmail.com>
2016-02-17 18:01:26 +0530

    Staging: gdm72xx: Add space around that "+", "&" and "/"
    Add missing spaces around "+", "&" and "/" to follow kernel coding
    style. Warning detected by checkpatch.
    
    Signed-off-by: Shraddha Barke <shraddha.6596@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gdm72xx/gdm_sdio.c
+++ drivers/staging/gdm72xx/gdm_sdio.c
@@ -34,10 +34,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SDU_TX_BUF_SIZE	2048
 #define TX_BUF_SIZE	2048
 #define TX_CHUNK_SIZE	(2048 - TYPE_A_HEADER_SIZE)
-#define RX_BUF_SIZE	(25*1024)
+#define RX_BUF_SIZE	(25 * 1024)
 
 #define TX_HZ		2000
-#define TX_INTERVAL	(NSEC_PER_SEC/TX_HZ)
+#define TX_INTERVAL	(NSEC_PER_SEC / TX_HZ)
 
 static struct sdio_tx *alloc_tx_struct(struct tx_cxt *tx)
 {
