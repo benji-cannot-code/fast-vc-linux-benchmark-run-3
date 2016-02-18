0596ce4ee7734ce44f7671db451f33ee23797a6d
Javier Martinez Canillas <javier@osg.samsung.com>
2016-02-18 15:19:11 -0300

    thermal: exynos: Defer probe if vtmu is present but not registered
    The driver doesn't check if the regulator_get_optional return value is
    -EPROBE_DEFER so it will wrongly assume that the regulator couldn't be
    found just because the regulator driver wasn't registered yet, i.e:
    
    exynos-tmu 10060000.tmu: Regulator node (vtmu) not found
    
    In this case the return value should be propagated to allow the driver
    probe function to be deferred until the regulator driver is registered.
    
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Reviewed-by: Andi Shyti <andi.shyti@samsung.com>
    Signed-off-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/samsung/exynos_tmu.c
+++ drivers/thermal/samsung/exynos_tmu.c
@@ -1319,6 +1319,8 @@ static int exynos_tmu_probe(struct platform_device *pdev)
 			return ret;
 		}
 	} else {
+		if (PTR_ERR(data->regulator) == -EPROBE_DEFER)
+			return -EPROBE_DEFER;
 		dev_info(&pdev->dev, "Regulator node (vtmu) not found\n");
 	}
 
