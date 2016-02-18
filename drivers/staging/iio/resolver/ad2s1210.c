33ca99496b9f3b23e41a57c0e56c04c0fbb9e094
Eva Rachel Retuya <eraretuya@gmail.com>
2016-02-18 18:59:36 +0800

    staging: iio: resolver: add missing braces on if-else statements
    Add braces around the else clause to adhere to kernel coding style. This
    clears the following checkpatch issue:
    
    CHECK: braces {} should be used on all arms of this statement
    
    Signed-off-by: Eva Rachel Retuya <eraretuya@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/iio/resolver/ad2s1210.c
+++ drivers/staging/iio/resolver/ad2s1210.c
@@ -320,9 +320,9 @@ static ssize_t ad2s1210_store_control(struct device *dev,
 		data = ad2s1210_read_resolution_pin(st);
 		if (data != st->resolution)
 			dev_warn(dev, "ad2s1210: resolution settings not match\n");
-	} else
+	} else {
 		ad2s1210_set_resolution_pin(st);
-
+	}
 	ret = len;
 	st->hysteresis = !!(data & AD2S1210_ENABLE_HYSTERESIS);
 
@@ -382,8 +382,9 @@ static ssize_t ad2s1210_store_resolution(struct device *dev,
 		data = ad2s1210_read_resolution_pin(st);
 		if (data != st->resolution)
 			dev_warn(dev, "ad2s1210: resolution settings not match\n");
-	} else
+	} else {
 		ad2s1210_set_resolution_pin(st);
+	}
 	ret = len;
 error_ret:
 	mutex_unlock(&st->lock);
