2446ac6d55acaf66ffe5e0e09795d607972dc0f1
Alex Deucher <alexander.deucher@amd.com>
2016-02-19 15:18:45 -0500

    drm/amdgpu: fix locking in force performance level
    Looks like a copy paste typo when we added powerplay
    support.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_pm.c
@@ -158,6 +158,7 @@ static ssize_t amdgpu_set_dpm_forced_performance_level(struct device *dev,
 		mutex_lock(&adev->pm.mutex);
 		if (adev->pm.dpm.thermal_active) {
 			count = -EINVAL;
+			mutex_unlock(&adev->pm.mutex);
 			goto fail;
 		}
 		ret = amdgpu_dpm_force_performance_level(adev, level);
@@ -168,8 +169,6 @@ static ssize_t amdgpu_set_dpm_forced_performance_level(struct device *dev,
 		mutex_unlock(&adev->pm.mutex);
 	}
 fail:
-	mutex_unlock(&adev->pm.mutex);
-
 	return count;
 }
 
