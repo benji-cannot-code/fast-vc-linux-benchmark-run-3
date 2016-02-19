6a0c5d2ffd9cc5ebf012d23f000dbae0943520b9
Linus Walleij <linus.walleij@linaro.org>
2016-02-19 09:22:41 +0100

    gpio: tps65218: remove unused #include
    Just as it says: after adding the proper interfaces to gpiolib,
    this is no longer needed.
    
    Suggested-by: Nicolas Saenz Julienne <nicolassaenzj@gmail.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-tps65218.c
+++ drivers/gpio/gpio-tps65218.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio/driver.h>
 #include <linux/platform_device.h>
 #include <linux/mfd/tps65218.h>
-#include "gpiolib.h"
 
 struct tps65218_gpio {
 	struct tps65218 *tps65218;
