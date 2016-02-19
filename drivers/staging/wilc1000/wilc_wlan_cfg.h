93181507b93d796d6586e02965a40d79d0dacebe
Bhumika Goyal <bhumirks@gmail.com>
2016-02-19 18:59:20 +0530

    Staging: wilc1000: Remove and rename struct typedefs
    This patch gets rid of struct typedefs wilc_cfg_byte_t, wilc_cfg_hword_t,
    wilc_cfg_word_t and wilc_cfg_str_t as as linux kernel coding style
    guidelines suggest not using typdefs for structure types.
    Also drop '_t' from the names.
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan_cfg.h
+++ drivers/staging/wilc1000/wilc_wlan_cfg.h
@@ -11,25 +11,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef WILC_WLAN_CFG_H
 #define WILC_WLAN_CFG_H
 
-typedef struct {
+struct wilc_cfg_byte {
 	u16 id;
 	u16 val;
-} wilc_cfg_byte_t;
+};
 
-typedef struct {
+struct wilc_cfg_hword {
 	u16 id;
 	u16 val;
-} wilc_cfg_hword_t;
+};
 
-typedef struct {
+struct wilc_cfg_word {
 	u32 id;
 	u32 val;
-} wilc_cfg_word_t;
+};
 
-typedef struct {
+struct wilc_cfg_str {
 	u32 id;
 	u8 *str;
-} wilc_cfg_str_t;
+};
 
 struct wilc;
 int wilc_wlan_cfg_set_wid(u8 *frame, u32 offset, u16 id, u8 *buf, int size);
