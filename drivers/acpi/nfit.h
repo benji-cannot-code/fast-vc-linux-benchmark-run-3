f5ca4c6205c0c039e2d0102f0b780afce978bc46
Dan Williams <dan.j.williams@intel.com>
2016-02-19 12:16:34 -0800

    nfit, libnvdimm: async region scrub workqueue
    Introduce a workqueue that will be used to run address range scrub
    asynchronously with the rest of nvdimm device probing.
    
    Userspace still wants notification when probing operations complete, so
    introduce a new callback to flush this workqueue when userspace is
    awaiting probe completion.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/acpi/nfit.h
+++ drivers/acpi/nfit.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifndef __NFIT_H__
 #define __NFIT_H__
+#include <linux/workqueue.h>
 #include <linux/libnvdimm.h>
 #include <linux/types.h>
 #include <linux/uuid.h>
@@ -124,6 +125,8 @@ struct acpi_nfit_desc {
 	struct list_head idts;
 	struct nvdimm_bus *nvdimm_bus;
 	struct device *dev;
+	struct work_struct work;
+	unsigned int cancel:1;
 	unsigned long dimm_dsm_force_en;
 	unsigned long bus_dsm_force_en;
 	int (*blk_do_io)(struct nd_blk_region *ndbr, resource_size_t dpa,
