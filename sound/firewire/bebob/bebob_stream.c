96f639621aae6ad2c3d45b639eae804dbb916c6a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-02-20 16:18:57 +0900

    ALSA: bebob: move mutex from function callee to callers
    Currently, critical section is protected by mutex in functions of
    fireworks_stream.c. Callers increments/decrements substreams counter
    before calling the functions. Moving mutex to the callers code allows
    to change type of the substream counter from atomic_t to unsigned int.
    
    This commit is a preparation for obsoleting usage of atomic_t for
    substream counter.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/bebob/bebob_stream.c
+++ sound/firewire/bebob/bebob_stream.c
@@ -590,8 +590,6 @@ int snd_bebob_stream_start_duplex(struct snd_bebob *bebob, unsigned int rate)
 	unsigned int curr_rate;
 	int err = 0;
 
-	mutex_lock(&bebob->mutex);
-
 	/* Need no substreams */
 	if (atomic_read(&bebob->substreams_counter) == 0)
 		goto end;
@@ -723,7 +721,6 @@ int snd_bebob_stream_start_duplex(struct snd_bebob *bebob, unsigned int rate)
 		}
 	}
 end:
-	mutex_unlock(&bebob->mutex);
 	return err;
 }
 
@@ -739,8 +736,6 @@ void snd_bebob_stream_stop_duplex(struct snd_bebob *bebob)
 		master = &bebob->tx_stream;
 	}
 
-	mutex_lock(&bebob->mutex);
-
 	if (atomic_read(&bebob->substreams_counter) == 0) {
 		amdtp_stream_pcm_abort(master);
 		amdtp_stream_stop(master);
@@ -750,8 +745,6 @@ void snd_bebob_stream_stop_duplex(struct snd_bebob *bebob)
 
 		break_both_connections(bebob);
 	}
-
-	mutex_unlock(&bebob->mutex);
 }
 
 void snd_bebob_stream_update_duplex(struct snd_bebob *bebob)
