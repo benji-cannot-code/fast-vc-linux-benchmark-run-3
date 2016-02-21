fc4c611b2529fddd1d2e7db96449263cd1b174cd
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2016-02-21 15:23:37 +0530

    staging: rts5208: Prefer using BIT macro
    Replace all instances of bit shifting on 1 with the BIT(x) macro.
    This was done using Coccinelle.
    
    @@ int c; @@
    - (1 << c)
    + BIT(c)
    
    Signed-off-by: Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rts5208/rtsx_transport.c
+++ drivers/staging/rts5208/rtsx_transport.c
@@ -225,7 +225,7 @@ void rtsx_add_cmd(struct rtsx_chip *chip,
 
 void rtsx_send_cmd_no_wait(struct rtsx_chip *chip)
 {
-	u32 val = 1 << 31;
+	u32 val = BIT(31);
 
 	rtsx_writel(chip, RTSX_HCBAR, chip->host_cmds_addr);
 
@@ -239,7 +239,7 @@ int rtsx_send_cmd(struct rtsx_chip *chip, u8 card, int timeout)
 {
 	struct rtsx_dev *rtsx = chip->rtsx;
 	struct completion trans_done;
-	u32 val = 1 << 31;
+	u32 val = BIT(31);
 	long timeleft;
 	int err = 0;
 
@@ -648,7 +648,7 @@ static int rtsx_transfer_buf(struct rtsx_chip *chip, u8 card, void *buf,
 	dma_addr_t addr;
 	u8 dir;
 	int err = 0;
-	u32 val = 1 << 31;
+	u32 val = BIT(31);
 	long timeleft;
 
 	if (!buf || (len <= 0))
