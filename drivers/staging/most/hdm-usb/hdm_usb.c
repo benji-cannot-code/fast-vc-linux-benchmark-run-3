9bc5b30b36df266a9c2f4b4b21a39fab5caab500
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2016-02-22 17:57:42 +0530

    staging: most: hdm-usb: Use macro DIV_ROUND_UP
    The macro DIV_ROUND_UP performs the computation (((n) + (d) - 1) /(d)).
    It clarifies the divisor calculations. This occurence was detected using the
    coccinelle script:
    @@
    expression e1;
    expression e2;
    @@
    (
    - ((e1) + e2 - 1) / (e2)
    + DIV_ROUND_UP(e1,e2)
    |
    - ((e1) + (e2 - 1))/ (e2)
    + DIV_ROUND_UP(e1,e2)
    )
    
    The macro CEILING was dropped altogether and has been replaced with
    DIV_ROUND_UP.
    
    Signed-off-by: Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/hdm-usb/hdm_usb.c
+++ drivers/staging/most/hdm-usb/hdm_usb.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_SUFFIX_LEN		10
 #define MAX_STRING_LEN		80
 #define MAX_BUF_SIZE		0xFFFF
-#define CEILING(x, y)		(((x) + (y) - 1) / (y))
 
 #define USB_VENDOR_ID_SMSC	0x0424  /* VID: SMSC */
 #define USB_DEV_ID_BRDG		0xC001  /* PID: USB Bridge */
@@ -787,7 +786,7 @@ static int hdm_configure_channel(struct most_interface *iface, int channel,
 	temp_size += tail_space;
 
 	/* calculate extra length to comply w/ HW padding */
-	conf->extra_len = (CEILING(temp_size, USB_MTU) * USB_MTU)
+	conf->extra_len = (DIV_ROUND_UP(temp_size, USB_MTU) * USB_MTU)
 			  - conf->buffer_size;
 exit:
 	mdev->conf[channel] = *conf;
