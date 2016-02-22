7a5f9601a5dc7ea77de3cd91247dd6560a393a57
Chris Park <chris.park@atmel.com>
2016-02-22 13:11:59 +0900

    staging: wilc1000: removes unused CFG80211_DBG tag
    This patch removes unused CFG80211_DBG tag.
    
    Signed-off-by: Chris Park <chris.park@atmel.com>
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/linux_wlan_common.h
+++ drivers/staging/wilc1000/linux_wlan_common.h
@@ -3,12 +3,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LINUX_WLAN_COMMON_H
 
 enum debug_region {
-	CFG80211_debug = 0,
-	Init_debug,
+	Init_debug = 0,
 	COMP = 0xFFFFFFFF,
 };
 
-#define CFG80211_DBG            (1 << CFG80211_debug)
 #define INIT_DBG                (1 << Init_debug)
 
 #if defined(WILC_DEBUGFS)
