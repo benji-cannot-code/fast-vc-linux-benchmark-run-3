8de0eb2611ba2d635f5c29a87258268010020772
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2016-02-22 14:14:10 +0530

    staging: sm750fb: Remove header files
    Remove duplicate include files. Found using includecheck.
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/sm750fb/sm750.c
+++ drivers/staging/sm750fb/sm750.c
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 #include <linux/pagemap.h>
 #include <linux/screen_info.h>
-#include <linux/vmalloc.h>
-#include <linux/pagemap.h>
 #include <linux/console.h>
 #include <asm/fb.h>
 #include "sm750.h"
