924d3d43b707f62fd39547c5d0f979855658f8b2
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2016-02-22 14:14:24 +0530

    staging: wilc1000: wilc_wfi_netdevice: Remove header files
    Remove duplicate header files. Found using includecheck.
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_netdevice.h
+++ drivers/staging/wilc1000/wilc_wfi_netdevice.h
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/skbuff.h>
 #include <linux/ieee80211.h>
 #include <net/cfg80211.h>
-#include <linux/ieee80211.h>
-#include <net/cfg80211.h>
 #include <net/ieee80211_radiotap.h>
 #include <linux/if_arp.h>
 #include <linux/in6.h>
