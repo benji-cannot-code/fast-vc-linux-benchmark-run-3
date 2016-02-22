76ffbcc1cf169192576ac5ed4bd07460c444b0f0
Finn Thain <fthain@telegraphics.com.au>
2016-02-23 10:07:04 +1100

    ncr5380: Correctly clear command pointers and lists after bus reset
    Commands subject to exception handling are to be returned to the scsi
    mid-layer. Make sure that the various command pointers and command lists
    in the low-level driver are correctly cleansed of affected commands.
    
    This fixes some bugs that I accidentally introduced in v4.5-rc1 including
    the removal of INIT_LIST_HEAD for the 'autosense' and 'disconnected'
    command lists, and the possible NULL pointer dereference in
    NCR5380_bus_reset() that was reported by Dan Carpenter.
    
    hostdata->sensing may also point to an affected command so this pointer
    also has to be cleared. The abort handler calls complete_cmd() to take
    care of this; let's have the bus reset handler do the same.
    
    The issue queue may also contain an affected command. If so, remove it.
    This also follows the abort handler logic.
    
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Fixes: 62717f537e1b ("ncr5380: Implement new eh_bus_reset_handler")
    Tested-by: Michael Schmitz <schmitzmic@gmail.com>
    Cc: <stable@vger.kernel.org> # 4.5
    Signed-off-by: Finn Thain <fthain@telegraphics.com.au>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/NCR5380.c
+++ drivers/scsi/NCR5380.c
@@ -2451,7 +2451,16 @@ static int NCR5380_bus_reset(struct scsi_cmnd *cmd)
 	 * commands!
 	 */
 
-	hostdata->selecting = NULL;
+	if (list_del_cmd(&hostdata->unissued, cmd)) {
+		cmd->result = DID_RESET << 16;
+		cmd->scsi_done(cmd);
+	}
+
+	if (hostdata->selecting) {
+		hostdata->selecting->result = DID_RESET << 16;
+		complete_cmd(instance, hostdata->selecting);
+		hostdata->selecting = NULL;
+	}
 
 	list_for_each_entry(ncmd, &hostdata->disconnected, list) {
 		struct scsi_cmnd *cmd = NCR5380_to_scmd(ncmd);
@@ -2459,6 +2468,7 @@ static int NCR5380_bus_reset(struct scsi_cmnd *cmd)
 		set_host_byte(cmd, DID_RESET);
 		cmd->scsi_done(cmd);
 	}
+	INIT_LIST_HEAD(&hostdata->disconnected);
 
 	list_for_each_entry(ncmd, &hostdata->autosense, list) {
 		struct scsi_cmnd *cmd = NCR5380_to_scmd(ncmd);
@@ -2466,17 +2476,12 @@ static int NCR5380_bus_reset(struct scsi_cmnd *cmd)
 		set_host_byte(cmd, DID_RESET);
 		cmd->scsi_done(cmd);
 	}
+	INIT_LIST_HEAD(&hostdata->autosense);
 
 	if (hostdata->connected) {
 		set_host_byte(hostdata->connected, DID_RESET);
 		complete_cmd(instance, hostdata->connected);
 		hostdata->connected = NULL;
-	}
-
-	if (hostdata->sensing) {
-		set_host_byte(hostdata->connected, DID_RESET);
-		complete_cmd(instance, hostdata->sensing);
-		hostdata->sensing = NULL;
 	}
 
 	for (i = 0; i < 8; ++i)
