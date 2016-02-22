4294300f4a9a66499c942155a6ab10fcbbbf2464
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2016-02-22 14:13:49 +0530

    staging: lustre: obdclass: Remove header file
    Remove duplicate include file. Found using includecheck.
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/linux/linux-module.c
+++ drivers/staging/lustre/lustre/obdclass/linux/linux-module.c
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/highmem.h>
 #include <linux/io.h>
 #include <asm/ioctls.h>
-#include <linux/poll.h>
 #include <linux/uaccess.h>
 #include <linux/miscdevice.h>
 #include <linux/seq_file.h>
