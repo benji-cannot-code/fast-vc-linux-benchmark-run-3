28f7d4e504f49089b45237c292b75623b7c849ff
Frank Zago <fzago@cray.com>
2016-02-22 17:29:20 -0500

    staging: lustre: remove unnecessary EXPORT_SYMBOL from lnet layer
    A lot of symbols don't need to be exported at all because they are
    only used in the module they belong to.
    
    Signed-off-by: Frank Zago <fzago@cray.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-5829
    Reviewed-on: http://review.whamcloud.com/13320
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Isaac Huang <he.huang@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/lnet/lib-socket.c
+++ drivers/staging/lustre/lnet/lnet/lib-socket.c
@@ -515,7 +515,6 @@ lnet_sock_listen(struct socket **sockp, __u32 local_ip, int local_port,
 	sock_release(*sockp);
 	return rc;
 }
-EXPORT_SYMBOL(lnet_sock_listen);
 
 int
 lnet_sock_accept(struct socket **newsockp, struct socket *sock)
@@ -559,7 +558,6 @@ failed:
 	sock_release(newsock);
 	return rc;
 }
-EXPORT_SYMBOL(lnet_sock_accept);
 
 int
 lnet_sock_connect(struct socket **sockp, int *fatal, __u32 local_ip,
@@ -597,4 +595,3 @@ lnet_sock_connect(struct socket **sockp, int *fatal, __u32 local_ip,
 	sock_release(*sockp);
 	return rc;
 }
-EXPORT_SYMBOL(lnet_sock_connect);
