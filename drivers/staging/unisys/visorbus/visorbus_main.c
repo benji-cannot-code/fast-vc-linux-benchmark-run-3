5b58c9faebe086549d17fdbfdc00ab53efcdcc0c
Benjamin Romer <benjamin.romer@unisys.com>
2016-02-23 10:01:49 -0500

    staging: unisys: fix return value for visorbus pci probe
    Instead of returning -1, return -ENODEV when there is no probe function
    found for the device.
    
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorbus_main.c
+++ drivers/staging/unisys/visorbus/visorbus_main.c
@@ -772,7 +772,7 @@ visordriver_probe_device(struct device *xdev)
 	get_device(&dev->device);
 	if (!drv->probe) {
 		up(&dev->visordriver_callback_lock);
-		rc = -1;
+		rc = -ENODEV;
 		goto away;
 	}
 	rc = drv->probe(dev);
