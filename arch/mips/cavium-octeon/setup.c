c8f443db0995e80176855fea4440055952f9ecc9
Aaro Koskinen <aaro.koskinen@iki.fi>
2016-02-24 00:52:05 +0200

    MIPS: Octeon: device_tree_init: use separate pass to fill mac addresses
    Use separate pass to fill MAC addresses. This is needed because we want
    to do this also for the appended DTB.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Acked-by: David Daney <david.daney@cavium.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/12588/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/cavium-octeon/setup.c
+++ arch/mips/cavium-octeon/setup.c
@@ -1109,6 +1109,7 @@ void __init prom_free_prom_memory(void)
 	}
 }
 
+void __init octeon_fill_mac_addresses(void);
 int octeon_prune_device_tree(void);
 
 extern const char __appended_dtb;
@@ -1144,6 +1145,7 @@ void __init device_tree_init(void)
 
 	if (do_prune) {
 		octeon_prune_device_tree();
+		octeon_fill_mac_addresses();
 		pr_info("Using internal Device Tree.\n");
 	}
 	unflatten_and_copy_device_tree();
