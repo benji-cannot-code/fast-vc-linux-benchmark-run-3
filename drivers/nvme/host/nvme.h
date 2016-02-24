0d9bdfb62dcf668de1b76b1ed016703e7c2a40de
Keith Busch <keith.busch@intel.com>
2016-02-24 09:15:53 -0700

    NVMe: Use IDA for namespace disk naming
    A namespace may be detached from a controller, but a user may be holding
    a reference to it. Attaching a new namespace with the same NSID will create
    duplicate names when using the NSID to name the disk.
    
    This patch uses an IDA that is released only when the last reference is
    released instead of using the namespace ID.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Sagi Grimberg <sagig@mellanox.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -73,6 +73,7 @@ struct nvme_ctrl {
 	struct mutex namespaces_mutex;
 	struct device *device;	/* char device */
 	struct list_head node;
+	struct ida ns_ida;
 
 	char name[12];
 	char serial[20];
@@ -103,6 +104,7 @@ struct nvme_ns {
 	struct request_queue *queue;
 	struct gendisk *disk;
 	struct kref kref;
+	int instance;
 
 	u8 eui[8];
 	u8 uuid[16];
