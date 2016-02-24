5bc8cdcf538a69c1596ac1913c0f57babfce84c7
Laxman Dewangan <ldewangan@nvidia.com>
2016-02-24 14:44:07 +0530

    pinctrl: tegra: Use devm_pinctrl_register() for pinctrl registration
    Use devm_pinctrl_register() for pin control registration and remove
    need of .remove callback.
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Cc: Stephen Warren <swarren@wwwdotorg.org>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: Alexandre Courbot <gnurou@gmail.com>
    Acked-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/tegra/pinctrl-tegra30.c
+++ drivers/pinctrl/tegra/pinctrl-tegra30.c
@@ -2501,7 +2501,6 @@ static struct platform_driver tegra30_pinctrl_driver = {
 		.of_match_table = tegra30_pinctrl_of_match,
 	},
 	.probe = tegra30_pinctrl_probe,
-	.remove = tegra_pinctrl_remove,
 };
 module_platform_driver(tegra30_pinctrl_driver);
 
