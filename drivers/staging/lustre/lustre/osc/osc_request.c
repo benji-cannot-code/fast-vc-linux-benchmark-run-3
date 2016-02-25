fe24cf93bce119b98b3d30661a3abf1ac3eb736e
Oleg Drokin <green@linuxhacker.ru>
2016-02-24 22:00:39 -0500

    staging/lustre: Fix indentation mistakes
    none look like bugs or bugs-inducing, mostly just
    off by a couple of spaces all around.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/osc/osc_request.c
+++ drivers/staging/lustre/lustre/osc/osc_request.c
@@ -2436,7 +2436,7 @@ static int osc_statfs_interpret(const struct lu_env *env,
 		 * aware of the problem and takes care
 		 * of the clean up
 		 */
-		 return rc;
+		return rc;
 
 	if ((rc == -ENOTCONN || rc == -EAGAIN) &&
 	    (aa->aa_oi->oi_flags & OBD_STATFS_NODELAY)) {
