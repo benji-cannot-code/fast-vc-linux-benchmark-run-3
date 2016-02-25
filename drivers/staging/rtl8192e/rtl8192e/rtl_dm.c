7bd2ac03d14def17bb27351063f1f483b24fb2b9
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2016-02-25 09:23:31 +0530

    staging: rtl8192e: rtl8192e: Remove create_workqueue()
    With conccurency managed workqueues, use of dedicated workqueues can
    be replaced by system_wq. Drop priv_wq by using system_wq.
    
    Since there are multiple work items per priv but they do not need
    to be ordered, increase of concurrency by switching to system_wq
    should not break anything.
    
    All work items are sync canceled on so it is guaranteed that
    no work is running when rtl92e_suspend function is called.
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/rtl_dm.c
+++ drivers/staging/rtl8192e/rtl8192e/rtl_dm.c
@@ -995,8 +995,7 @@ static void _rtl92e_dm_check_tx_power_tracking_tssi(struct net_device *dev)
 
 
 	 if (tx_power_track_counter >= 180) {
-		queue_delayed_work_rsl(priv->priv_wq,
-				       &priv->txpower_tracking_wq, 0);
+		schedule_delayed_work(&priv->txpower_tracking_wq, 0);
 		tx_power_track_counter = 0;
 	}
 
@@ -1029,7 +1028,7 @@ static void _rtl92e_dm_check_tx_power_tracking_thermal(struct net_device *dev)
 		return;
 	}
 	netdev_info(dev, "===============>Schedule TxPowerTrackingWorkItem\n");
-	queue_delayed_work_rsl(priv->priv_wq, &priv->txpower_tracking_wq, 0);
+	schedule_delayed_work(&priv->txpower_tracking_wq, 0);
 	TM_Trigger = 0;
 
 }
@@ -2122,7 +2121,7 @@ static void _rtl92e_dm_check_rx_path_selection(struct net_device *dev)
 {
 	struct r8192_priv *priv = rtllib_priv(dev);
 
-	queue_delayed_work_rsl(priv->priv_wq, &priv->rfpath_check_wq, 0);
+	schedule_delayed_work(&priv->rfpath_check_wq, 0);
 }
 
 
