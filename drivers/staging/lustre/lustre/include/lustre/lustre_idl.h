527d42b83805d221f73287c1fbade4b41ce1f957
Oleg Drokin <green@linuxhacker.ru>
2016-02-24 21:59:52 -0500

    staging/lustre: Get rid of mds_reint_t, mdt_reint_t typedefs
    Replace with enum mdt_reint_cmd
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre/lustre_idl.h
+++ drivers/staging/lustre/lustre/include/lustre/lustre_idl.h
@@ -2066,7 +2066,7 @@ typedef enum {
  * Do not exceed 63
  */
 
-typedef enum {
+enum mdt_reint_cmd {
 	REINT_SETATTR  = 1,
 	REINT_CREATE   = 2,
 	REINT_LINK     = 3,
@@ -2077,7 +2077,7 @@ typedef enum {
 	REINT_RMENTRY  = 8,
 //      REINT_WRITE    = 9,
 	REINT_MAX
-} mds_reint_t, mdt_reint_t;
+};
 
 void lustre_swab_generic_32s(__u32 *val);
 
