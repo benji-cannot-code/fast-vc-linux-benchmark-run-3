bb0e42fa1c4f8939e4f6c0c120caaccb6d935671
Oleg Drokin <green@linuxhacker.ru>
2016-02-24 21:59:59 -0500

    staging/lustre: Get rid of object update code
    Object update was originally planned to be used for cross-server
    updates, so nothign to do with the client. The idea was
    later significantly reworked anyway so even wire structs should
    not be retained.
    
    This gets rid of update_cmd_t, UPDATE and UPDATE_REPLY rpc and
    corresponding subcommands, swabbig logic and such.
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/lproc_ptlrpc.c
+++ drivers/staging/lustre/lustre/ptlrpc/lproc_ptlrpc.c
@@ -132,7 +132,6 @@ static struct ll_rpc_opcode {
 	{ SEC_CTX_INIT_CONT, "sec_ctx_init_cont" },
 	{ SEC_CTX_FINI,     "sec_ctx_fini" },
 	{ FLD_QUERY,	"fld_query" },
-	{ UPDATE_OBJ,	    "update_obj" },
 };
 
 static struct ll_eopcode {
