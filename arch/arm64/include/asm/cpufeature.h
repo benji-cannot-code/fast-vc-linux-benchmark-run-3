066480bedcd6b1c11d28c4a5bcff0dc57c19f688
Andrew Pinski <apinski@cavium.com>
2016-02-24 17:44:57 -0800

    arm64: Add workaround for Cavium erratum 27456
    On ThunderX T88 pass 1.x through 2.1 parts, broadcast TLBI
    instructions may cause the icache to become corrupted if it contains
    data for a non-current ASID.
    
    This patch implements the workaround (which invalidates the local
    icache when switching the mm) by using code patching.
    
    Signed-off-by: Andrew Pinski <apinski@cavium.com>
    Signed-off-by: David Daney <david.daney@cavium.com>
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -34,8 +34,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARM64_HAS_NO_HW_PREFETCH		8
 #define ARM64_HAS_UAO				9
 #define ARM64_ALT_PAN_NOT_UAO			10
+#define ARM64_WORKAROUND_CAVIUM_27456		12
 
-#define ARM64_NCAPS				11
+#define ARM64_NCAPS				13
 
 #ifndef __ASSEMBLY__
 
