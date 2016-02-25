b5b11a374f427b9af67c58e4336fb9167ae9d0db
Dan Carpenter <dan.carpenter@oracle.com>
2016-02-25 10:50:38 +0300

    ASoC: max9867: silence and array overflow warning
    Smatch complains that we might reach the end of this loop without
    finding what we're looking for leading to a buffer overflow.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/max9867.c
+++ sound/soc/codecs/max9867.c
@@ -157,6 +157,8 @@ static inline int get_ni_value(int mclk, int rate)
 		if (ni_div[i].mclk >= mclk)
 			break;
 	}
+	if (i == ARRAY_SIZE(ni_div))
+		return -EINVAL;
 
 	switch (rate) {
 	case 8000:
