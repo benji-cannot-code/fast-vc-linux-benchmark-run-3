9c34075dd19c8b3431b051bb52826943239f8de9
Jubin John <jubin.john@intel.com>
2016-02-26 13:33:33 -0800

    staging/rdma/hfi1: Fix memory leaks
    Fix 3 memory leaks reported by the LeakCheck tool in the KEDR framework.
    
    The following resources were allocated memory during their respective
    initializations but not freed during cleanup:
    1. SDMA map elements
    2. PIO map elements
    3. HW send context to SW index map
    
    This patch fixes the memory leaks by freeing the allocated memory in the
    cleanup path.
    
    Reviewed-by: Dean Luick <dean.luick@intel.com>
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Jubin John <jubin.john@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/staging/rdma/hfi1/init.c
+++ drivers/staging/rdma/hfi1/init.c
@@ -1325,6 +1325,8 @@ static void cleanup_device_data(struct hfi1_devdata *dd)
 	dd->num_send_contexts = 0;
 	kfree(dd->send_contexts);
 	dd->send_contexts = NULL;
+	kfree(dd->hw_to_sw);
+	dd->hw_to_sw = NULL;
 	kfree(dd->boardname);
 	vfree(dd->events);
 	vfree(dd->status);
