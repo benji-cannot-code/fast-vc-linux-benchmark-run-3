e2ac433298c2dc61a54b99d33bcb551bf2ad17ea
Namhyung Kim <namhyung@kernel.org>
2016-02-27 03:52:47 +0900

    perf report: Update column width of dynamic entries
    The column width of dynamic entries is updated when comparing hist
    entries.  However some unique entries can miss the chance to update.  So
    move the update to output resort stage to make sure every entry will get
    called before display.
    
    To do that, abuse ->sort callback to update the width when the third
    argument is NULL.  When resorting entries in normal path, it never be
    NULL so it should be fine IMHO.
    
    Before:
    
      #       Overhead  ptr / bytes_req / gfp_flags
      # ..............  ..........................................
      #
          37.50%        0xffff8803f7669400
             37.50%        448
                37.50%        GFP_ATOMIC|GFP_NOWARN|GFP_NOMEMALLOC
          10.42%        0xffff8803f766be00
              8.33%        96
                 8.33%        GFP_ATOMIC|GFP_NOWARN|GFP_NOMEMALLOC
              2.08%        512
                 2.08%        GFP_KERNEL|GFP_NOWARN|GFP_REPEAT|GFP   <-- here
    
    After:
    
      #       Overhead  ptr / bytes_req / gfp_flags
      # ..............  .....................................................
      #
          37.50%        0xffff8803f7669400
             37.50%        448
                37.50%        GFP_ATOMIC|GFP_NOWARN|GFP_NOMEMALLOC
          10.42%        0xffff8803f766be00
              8.33%        96
                 8.33%        GFP_ATOMIC|GFP_NOWARN|GFP_NOMEMALLOC
              2.08%        512
                 2.08%        GFP_KERNEL|GFP_NOWARN|GFP_REPEAT|GFP_NOMEMALLOC
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/r/1456512767-1164-5-git-send-email-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -1372,6 +1372,10 @@ static void hierarchy_insert_output_entry(struct rb_root *root,
 
 	rb_link_node(&he->rb_node, parent, p);
 	rb_insert_color(&he->rb_node, root);
+
+	/* update column width of dynamic entry */
+	if (perf_hpp__is_dynamic_entry(he->fmt))
+		he->fmt->sort(he->fmt, he, NULL);
 }
 
 static void hists__hierarchy_output_resort(struct hists *hists,
@@ -1441,6 +1445,7 @@ static void __hists__insert_output_entry(struct rb_root *entries,
 	struct rb_node **p = &entries->rb_node;
 	struct rb_node *parent = NULL;
 	struct hist_entry *iter;
+	struct perf_hpp_fmt *fmt;
 
 	if (use_callchain) {
 		if (callchain_param.mode == CHAIN_GRAPH_REL) {
@@ -1467,6 +1472,12 @@ static void __hists__insert_output_entry(struct rb_root *entries,
 
 	rb_link_node(&he->rb_node, parent, p);
 	rb_insert_color(&he->rb_node, entries);
+
+	perf_hpp_list__for_each_sort_list(&perf_hpp_list, fmt) {
+		if (perf_hpp__is_dynamic_entry(fmt) &&
+		    perf_hpp__defined_dynamic_entry(fmt, he->hists))
+			fmt->sort(fmt, he, NULL);  /* update column width */
+	}
 }
 
 static void output_resort(struct hists *hists, struct ui_progress *prog,
