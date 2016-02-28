cb6ffa9873cfe113d72d60cd3f953486bf12ca1a
Bhumika Goyal <bhumirks@gmail.com>
2016-02-28 22:55:29 +0530

    Staging: rtl8723au: Remove print statements and debug messages
    The  memory allocation functions generates a call stack containing
    all the context information on failure, so print statements and debug
    messages can be removed on failure of these functions. Also remove
    unwanted {} around if block after removal of these messages.
    Done using coccinelle:
    
    @@
    expression e;
    @@
    
    e=\(kmalloc\|kmalloc\|kmalloc_array\|alloc_netdev\|kzalloc\|
       devm_kzalloc\|devm_ioremap\)(...);
    ...
    if(!e){
    - \(DBG_8723A\|printk\|pr_err\|CERROR\|DBG_88E\)(...);
    ...when any
    }
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723au/os_dep/ioctl_cfg80211.c
+++ drivers/staging/rtl8723au/os_dep/ioctl_cfg80211.c
@@ -2611,8 +2611,6 @@ static int rtw_cfg80211_add_monitor_if(struct rtw_adapter *padapter, char *name,
 	/*  wdev */
 	mon_wdev = kzalloc(sizeof(struct wireless_dev), GFP_KERNEL);
 	if (!mon_wdev) {
-		DBG_8723A("%s(%s): allocate mon_wdev fail\n", __func__,
-			  padapter->pnetdev->name);
 		ret = -ENOMEM;
 		goto out;
 	}
@@ -3263,7 +3261,6 @@ int rtw_wdev_alloc(struct rtw_adapter *padapter, struct device *dev)
 	/*  wdev */
 	wdev = kzalloc(sizeof(struct wireless_dev), GFP_KERNEL);
 	if (!wdev) {
-		DBG_8723A("Couldn't allocate wireless device\n");
 		ret = -ENOMEM;
 		goto free_wiphy;
 	}
