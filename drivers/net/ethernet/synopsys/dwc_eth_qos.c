c52202895d53d92109ba675b94e8a76cbe51dc5d
Rabin Vincent <rabinv@axis.com>
2016-02-29 16:22:33 +0100

    dwc_eth_qos: use DWCEQOS_MSG_DEFAULT
    Since debug is hardcoded to 3, the defaults in the DWCEQOS_MSG_DEFAULT
    macro are never used, which does not seem to be the intended behaviour
    here.  Set debug to -1 like other drivers so that DWCEQOS_MSG_DEFAULT is
    actually used by default.
    
    Signed-off-by: Rabin Vincent <rabinv@axis.com>
    Signed-off-by: Lars Persson <larper@axis.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/synopsys/dwc_eth_qos.c
+++ drivers/net/ethernet/synopsys/dwc_eth_qos.c
@@ -427,7 +427,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DWC_MMC_RXOCTETCOUNT_GB          0x0784
 #define DWC_MMC_RXPACKETCOUNT_GB         0x0780
 
-static int debug = 3;
+static int debug = -1;
 module_param(debug, int, 0);
 MODULE_PARM_DESC(debug, "DWC_eth_qos debug level (0=none,...,16=all)");
 
