a754b12ea69ea7109b3a8d5295f0f4f7d0b2d18f
santosh.shilimkar@oracle.com <santosh.shilimkar@oracle.com>
2016-03-01 15:20:53 -0800

    RDS: IB: allocate extra space on queues for FRMR support
    Fastreg MR(FRMR) memory registration and invalidation makes use
    of work request and completion queues for its operation. Patch
    allocates extra queue space towards these operation(s).
    
    Signed-off-by: Santosh Shilimkar <ssantosh@kernel.org>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define RDS_IB_DEFAULT_RECV_WR		1024
 #define RDS_IB_DEFAULT_SEND_WR		256
+#define RDS_IB_DEFAULT_FR_WR		512
 
 #define RDS_IB_DEFAULT_RETRY_COUNT	2
 
@@ -122,6 +123,9 @@ struct rds_ib_connection {
 	struct ib_cq		*i_recv_cq;
 	struct ib_wc		i_send_wc[RDS_IB_WC_MAX];
 	struct ib_wc		i_recv_wc[RDS_IB_WC_MAX];
+
+	/* To control the number of wrs from fastreg */
+	atomic_t		i_fastreg_wrs;
 
 	/* interrupt handling */
 	struct tasklet_struct	i_send_tasklet;
