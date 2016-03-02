7f931917ff2035fae07ef1d47c9bef8fb2b09856
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2016-03-02 08:00:15 -0300

    [media] rc-core: allow calling rc_open with device not initialized
    The device initialization completes only after calling
    input_register_device(). However, rc_open() can be called while
    the device is being registered by the input/evdev core. So, we
    can't expect that rc_dev->initialized to be true.
    
    Change the logic to don't require initialized == true at rc_open
    and change the type of initialized to be atomic.
    
    this way, we can check for it earlier where it is really needed,
    without needing to lock the mutex just for testing it.
    
    Tested with nuvoton_cir driver on a NUC5i7RYB with CIR integrated on it.
    
    Reported-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/rc/rc-main.c
+++ drivers/media/rc/rc-main.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <media/rc-core.h>
+#include <linux/atomic.h>
 #include <linux/spinlock.h>
 #include <linux/delay.h>
 #include <linux/input.h>
@@ -724,10 +725,6 @@ int rc_open(struct rc_dev *rdev)
 		return -EINVAL;
 
 	mutex_lock(&rdev->lock);
-	if (!rdev->initialized) {
-		rval = -EINVAL;
-		goto unlock;
-	}
 
 	if (!rdev->users++ && rdev->open != NULL)
 		rval = rdev->open(rdev);
@@ -735,7 +732,6 @@ int rc_open(struct rc_dev *rdev)
 	if (rval)
 		rdev->users--;
 
-unlock:
 	mutex_unlock(&rdev->lock);
 
 	return rval;
@@ -880,11 +876,10 @@ static ssize_t show_protocols(struct device *device,
 	if (!dev)
 		return -EINVAL;
 
+	if (!atomic_read(&dev->initialized))
+		return -ERESTARTSYS;
+
 	mutex_lock(&dev->lock);
-	if (!dev->initialized) {
-		mutex_unlock(&dev->lock);
-		return -EINVAL;
-	}
 
 	if (fattr->type == RC_FILTER_NORMAL) {
 		enabled = dev->enabled_protocols;
@@ -1065,6 +1060,9 @@ static ssize_t store_protocols(struct device *device,
 	if (!dev)
 		return -EINVAL;
 
+	if (!atomic_read(&dev->initialized))
+		return -ERESTARTSYS;
+
 	if (fattr->type == RC_FILTER_NORMAL) {
 		IR_dprintk(1, "Normal protocol change requested\n");
 		current_protocols = &dev->enabled_protocols;
@@ -1085,10 +1083,6 @@ static ssize_t store_protocols(struct device *device,
 	}
 
 	mutex_lock(&dev->lock);
-	if (!dev->initialized) {
-		rc = -EINVAL;
-		goto out;
-	}
 
 	old_protocols = *current_protocols;
 	new_protocols = old_protocols;
@@ -1169,11 +1163,10 @@ static ssize_t show_filter(struct device *device,
 	if (!dev)
 		return -EINVAL;
 
+	if (!atomic_read(&dev->initialized))
+		return -ERESTARTSYS;
+
 	mutex_lock(&dev->lock);
-	if (!dev->initialized) {
-		mutex_unlock(&dev->lock);
-		return -EINVAL;
-	}
 
 	if (fattr->type == RC_FILTER_NORMAL)
 		filter = &dev->scancode_filter;
@@ -1224,6 +1217,9 @@ static ssize_t store_filter(struct device *device,
 	if (!dev)
 		return -EINVAL;
 
+	if (!atomic_read(&dev->initialized))
+		return -ERESTARTSYS;
+
 	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
@@ -1242,10 +1238,6 @@ static ssize_t store_filter(struct device *device,
 		return -EINVAL;
 
 	mutex_lock(&dev->lock);
-	if (!dev->initialized) {
-		ret = -EINVAL;
-		goto unlock;
-	}
 
 	new_filter = *filter;
 	if (fattr->mask)
@@ -1432,6 +1424,7 @@ int rc_register_device(struct rc_dev *dev)
 	dev->minor = minor;
 	dev_set_name(&dev->dev, "rc%u", dev->minor);
 	dev_set_drvdata(&dev->dev, dev);
+	atomic_set(&dev->initialized, 0);
 
 	dev->dev.groups = dev->sysfs_groups;
 	dev->sysfs_groups[attr++] = &rc_dev_protocol_attr_grp;
@@ -1456,10 +1449,6 @@ int rc_register_device(struct rc_dev *dev)
 	dev->input_dev->phys = dev->input_phys;
 	dev->input_dev->name = dev->input_name;
 
-	rc = input_register_device(dev->input_dev);
-	if (rc)
-		goto out_table;
-
 	/*
 	 * Default delay of 250ms is too short for some protocols, especially
 	 * since the timeout is currently set to 250ms. Increase it to 500ms,
@@ -1474,6 +1463,11 @@ int rc_register_device(struct rc_dev *dev)
 	 * to do.
 	 */
 	dev->input_dev->rep[REP_PERIOD] = 125;
+
+	/* rc_open will be called here */
+	rc = input_register_device(dev->input_dev);
+	if (rc)
+		goto out_table;
 
 	path = kobject_get_path(&dev->dev.kobj, GFP_KERNEL);
 	dev_info(&dev->dev, "%s as %s\n",
@@ -1498,8 +1492,9 @@ int rc_register_device(struct rc_dev *dev)
 		dev->enabled_protocols = rc_type;
 	}
 
+	/* Allow the RC sysfs nodes to be accessible */
 	mutex_lock(&dev->lock);
-	dev->initialized = true;
+	atomic_set(&dev->initialized, 1);
 	mutex_unlock(&dev->lock);
 
 	IR_dprintk(1, "Registered rc%u (driver: %s, remote: %s, mode %s)\n",
