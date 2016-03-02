a12c70a46c1e921444dda1f784c4ed57f3213639
Bhumika Goyal <bhumirks@gmail.com>
2016-03-03 00:46:37 +0530

    Staging: lustre: lnet: Remove and rename struct typedefs
    Remove typedefs from structures stt_timer_t and lst_ping_data_t as typedef
    for a structure type is not preferred. The suffix '_t' and the typedefs
    are removed using coccinelle.
    
    Script 1:
    //Drop typedefs and '_t'
    @r1@
    type T;
    @@
    
    typedef struct { ... } T;
    
    @script:python c1@
    T2;
    T << r1.T;
    @@
    if T[-2:] =="_t":
      coccinelle.T2 = T[:-2];
      print T
    else:
      coccinelle.T2=T;
    
    @@
    type r1.T;
    identifier c1.T2;
    @@
    -typedef
    struct
    + T2
    { ... }
    -T
    ;
    
    Script 2:
    //Replacement
    @@
    typedef stt_timer_t;
    typedef lst_ping_data_t;
    @@
    (
    - stt_timer_t
    + struct stt_timer
    |
    - lst_ping_data_t
    + struct lst_ping_data
    )
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/selftest/framework.c
+++ drivers/staging/lustre/lnet/selftest/framework.c
@@ -161,7 +161,7 @@ static void
 sfw_add_session_timer(void)
 {
 	sfw_session_t *sn = sfw_data.fw_session;
-	stt_timer_t *timer = &sn->sn_timer;
+	struct stt_timer *timer = &sn->sn_timer;
 
 	LASSERT(!sfw_data.fw_shuttingdown);
 
@@ -262,7 +262,7 @@ static inline void
 sfw_init_session(sfw_session_t *sn, lst_sid_t sid,
 		 unsigned features, const char *name)
 {
-	stt_timer_t *timer = &sn->sn_timer;
+	struct stt_timer *timer = &sn->sn_timer;
 
 	memset(sn, 0, sizeof(sfw_session_t));
 	INIT_LIST_HEAD(&sn->sn_list);
