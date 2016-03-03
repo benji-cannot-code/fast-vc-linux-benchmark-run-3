eed82ec3d174142ed465a6656ab410aa611cce25
Chaehyun Lim <chaehyun.lim@gmail.com>
2016-03-03 14:28:22 +0900

    staging: wilc1000: remove typedef from sdio_cmd53_t
    This patch removes typedef from struct sdio_cmd53_t and renames it to
    sdio_cmd53.
    
    Signed-off-by: Chaehyun Lim <chaehyun.lim@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan_if.h
+++ drivers/staging/wilc1000/wilc_wlan_if.h
@@ -59,7 +59,7 @@ struct sdio_cmd52 {
 	u32 data:		8;
 };
 
-typedef struct {
+struct sdio_cmd53 {
 	u32 read_write:		1;
 	u32 function:		3;
 	u32 block_mode:		1;
@@ -68,7 +68,7 @@ typedef struct {
 	u32 count:		9;
 	u8 *buffer;
 	u32 block_size;
-} sdio_cmd53_t;
+};
 
 #define WILC_MAC_INDICATE_STATUS	0x1
 #define WILC_MAC_STATUS_INIT		-1
