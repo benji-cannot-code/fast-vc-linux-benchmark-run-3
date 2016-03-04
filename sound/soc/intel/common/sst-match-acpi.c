219e39261ad80983026695e8c097af5b9c63af21
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2016-03-03 21:36:36 -0600

    ASoC: Intel: common: add translation from HID to codec-name
    BIOS vendors sometimes declare multiple devices in the DSDT
    table that all refer to the same HID. This is not very smart
    but not illegal as long as only one device reports present with
    the _STA method. The ACPI subsystem tracks each device with an
    extension, e.g. 10EC5640:00 and 10EC5640:01
    
    In the ASoC machine driver, the DAI codec name needs to refer to the
    ACPI device that reported present, e.g. "i2c-10EC5640:01". The
    extension will vary depending on how the BIOS is written and which
    ACPI device is activated.
    
    This patch adds a translation function that provides the codec
    name from the ACPI HID to avoid any hard-coded values in the
    machine driver.
    
    Suggested-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/common/sst-match-acpi.c
+++ sound/soc/intel/common/sst-match-acpi.c
@@ -17,6 +17,39 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "sst-acpi.h"
 
+static acpi_status sst_acpi_find_name(acpi_handle handle, u32 level,
+				      void *context, void **ret)
+{
+	struct acpi_device *adev;
+	const char *name = NULL;
+
+	if (acpi_bus_get_device(handle, &adev))
+		return AE_OK;
+
+	if (adev->status.present && adev->status.functional) {
+		name = acpi_dev_name(adev);
+		*(const char **)ret = name;
+		return AE_CTRL_TERMINATE;
+	}
+
+	return AE_OK;
+}
+
+const char *sst_acpi_find_name_from_hid(const u8 hid[ACPI_ID_LEN])
+{
+	const char *name = NULL;
+	acpi_status status;
+
+	status = acpi_get_devices(hid, sst_acpi_find_name, NULL,
+				  (void **)&name);
+
+	if (ACPI_FAILURE(status) || name[0] == '\0')
+		return NULL;
+
+	return name;
+}
+EXPORT_SYMBOL_GPL(sst_acpi_find_name_from_hid);
+
 static acpi_status sst_acpi_mach_match(acpi_handle handle, u32 level,
 				       void *context, void **ret)
 {
