ee2cabd19573397413131fdb414d9dc1d357fd0f
Chuck Lever <chuck.lever@oracle.com>
2016-03-04 11:27:26 -0500

    xprtrdma: Clean up unused RPCRDMA_INLINE_PAD_THRESH macro
    Fixes: b3221d6a53c4 ('xprtrdma: Remove logic that constructs...')
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Reviewed-by: Sagi Grimberg <sagig@mellanox.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- include/linux/sunrpc/xprtrdma.h
+++ include/linux/sunrpc/xprtrdma.h
@@ -55,8 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define RPCRDMA_DEF_INLINE  (1024)	/* default inline max */
 
-#define RPCRDMA_INLINE_PAD_THRESH  (512)/* payload threshold to pad (bytes) */
-
 /* Memory registration strategies, by number.
  * This is part of a kernel / user space API. Do not remove. */
 enum rpcrdma_memreg {
