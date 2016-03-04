ce2f8daee9644e6210d578ca967c47c1596d7caf
Alexander Shishkin <alexander.shishkin@linux.intel.com>
2016-03-04 16:16:31 +0200

    intel_th: msu: Handle kstrndup() failure
    Currently, the nr_pages attribute store does not check if kstrndup()
    succeeded. Fix this.
    
    Reported-by: Alan Cox <alan.cox@intel.com>
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Reviewed-by: Laurent Fert <laurent.fert@intel.com>

--- drivers/hwtracing/intel_th/msu.c
+++ drivers/hwtracing/intel_th/msu.c
@@ -1394,6 +1394,11 @@ nr_pages_store(struct device *dev, struct device_attribute *attr,
 	do {
 		end = memchr(p, ',', len);
 		s = kstrndup(p, end ? end - p : len, GFP_KERNEL);
+		if (!s) {
+			ret = -ENOMEM;
+			goto free_win;
+		}
+
 		ret = kstrtoul(s, 10, &val);
 		kfree(s);
 
