8eb84ce2c91d25cde68b31ff6a7b734233a7fed4
Malcolm Priestley <tvboxspy@gmail.com>
2016-03-06 12:57:09 +0000

    staging: vt6655: mac Replace dwCurrDescAddr with curr_desc_addr.
    Removing camel case and type prefix.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/mac.c
+++ drivers/staging/vt6655/mac.c
@@ -546,14 +546,14 @@ void MACvInitialize(struct vnt_private *priv)
  * Parameters:
  *  In:
  *      io_base        - Base Address for MAC
- *      dwCurrDescAddr  - Descriptor Address
+ *      curr_desc_addr  - Descriptor Address
  *  Out:
  *      none
  *
  * Return Value: none
  *
  */
-void MACvSetCurrRx0DescAddr(struct vnt_private *priv, u32 dwCurrDescAddr)
+void MACvSetCurrRx0DescAddr(struct vnt_private *priv, u32 curr_desc_addr)
 {
 	void __iomem *io_base = priv->PortOffset;
 	unsigned short ww;
@@ -568,7 +568,7 @@ void MACvSetCurrRx0DescAddr(struct vnt_private *priv, u32 dwCurrDescAddr)
 			break;
 	}
 
-	iowrite32(dwCurrDescAddr, io_base + MAC_REG_RXDMAPTR0);
+	iowrite32(curr_desc_addr, io_base + MAC_REG_RXDMAPTR0);
 	if (byOrgDMACtl & DMACTL_RUN)
 		iowrite8(DMACTL_RUN, io_base + MAC_REG_RXDMACTL0);
 }
@@ -580,14 +580,14 @@ void MACvSetCurrRx0DescAddr(struct vnt_private *priv, u32 dwCurrDescAddr)
  * Parameters:
  *  In:
  *      io_base        - Base Address for MAC
- *      dwCurrDescAddr  - Descriptor Address
+ *      curr_desc_addr  - Descriptor Address
  *  Out:
  *      none
  *
  * Return Value: none
  *
  */
-void MACvSetCurrRx1DescAddr(struct vnt_private *priv, u32 dwCurrDescAddr)
+void MACvSetCurrRx1DescAddr(struct vnt_private *priv, u32 curr_desc_addr)
 {
 	void __iomem *io_base = priv->PortOffset;
 	unsigned short ww;
@@ -602,7 +602,7 @@ void MACvSetCurrRx1DescAddr(struct vnt_private *priv, u32 dwCurrDescAddr)
 			break;
 	}
 
-	iowrite32(dwCurrDescAddr, io_base + MAC_REG_RXDMAPTR1);
+	iowrite32(curr_desc_addr, io_base + MAC_REG_RXDMAPTR1);
 	if (byOrgDMACtl & DMACTL_RUN)
 		iowrite8(DMACTL_RUN, io_base + MAC_REG_RXDMACTL1);
 
@@ -615,7 +615,7 @@ void MACvSetCurrRx1DescAddr(struct vnt_private *priv, u32 dwCurrDescAddr)
  * Parameters:
  *  In:
  *      io_base        - Base Address for MAC
- *      dwCurrDescAddr  - Descriptor Address
+ *      curr_desc_addr  - Descriptor Address
  *  Out:
  *      none
  *
@@ -623,7 +623,7 @@ void MACvSetCurrRx1DescAddr(struct vnt_private *priv, u32 dwCurrDescAddr)
  *
  */
 void MACvSetCurrTx0DescAddrEx(struct vnt_private *priv,
-			      u32 dwCurrDescAddr)
+			      u32 curr_desc_addr)
 {
 	void __iomem *io_base = priv->PortOffset;
 	unsigned short ww;
@@ -638,7 +638,7 @@ void MACvSetCurrTx0DescAddrEx(struct vnt_private *priv,
 			break;
 	}
 
-	iowrite32(dwCurrDescAddr, io_base + MAC_REG_TXDMAPTR0);
+	iowrite32(curr_desc_addr, io_base + MAC_REG_TXDMAPTR0);
 	if (byOrgDMACtl & DMACTL_RUN)
 		iowrite8(DMACTL_RUN, io_base + MAC_REG_TXDMACTL0);
 }
@@ -650,7 +650,7 @@ void MACvSetCurrTx0DescAddrEx(struct vnt_private *priv,
  * Parameters:
  *  In:
  *      io_base        - Base Address for MAC
- *      dwCurrDescAddr  - Descriptor Address
+ *      curr_desc_addr  - Descriptor Address
  *  Out:
  *      none
  *
@@ -659,7 +659,7 @@ void MACvSetCurrTx0DescAddrEx(struct vnt_private *priv,
  */
 /* TxDMA1 = AC0DMA */
 void MACvSetCurrAC0DescAddrEx(struct vnt_private *priv,
-			      u32 dwCurrDescAddr)
+			      u32 curr_desc_addr)
 {
 	void __iomem *io_base = priv->PortOffset;
 	unsigned short ww;
@@ -675,18 +675,18 @@ void MACvSetCurrAC0DescAddrEx(struct vnt_private *priv,
 	}
 	if (ww == W_MAX_TIMEOUT)
 		pr_debug(" DBG_PORT80(0x26)\n");
-	iowrite32(dwCurrDescAddr, io_base + MAC_REG_AC0DMAPTR);
+	iowrite32(curr_desc_addr, io_base + MAC_REG_AC0DMAPTR);
 	if (byOrgDMACtl & DMACTL_RUN)
 		iowrite8(DMACTL_RUN, io_base + MAC_REG_AC0DMACTL);
 }
 
 void MACvSetCurrTXDescAddr(int iTxType, struct vnt_private *priv,
-			   u32 dwCurrDescAddr)
+			   u32 curr_desc_addr)
 {
 	if (iTxType == TYPE_AC0DMA)
-		MACvSetCurrAC0DescAddrEx(priv, dwCurrDescAddr);
+		MACvSetCurrAC0DescAddrEx(priv, curr_desc_addr);
 	else if (iTxType == TYPE_TXDMA0)
-		MACvSetCurrTx0DescAddrEx(priv, dwCurrDescAddr);
+		MACvSetCurrTx0DescAddrEx(priv, curr_desc_addr);
 }
 
 /*
