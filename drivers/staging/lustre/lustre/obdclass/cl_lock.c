fa2f702dcfb2f599e8aab7cb14df373fd17834c4
James Nunez <james.a.nunez@intel.com>
2016-03-07 18:10:21 -0500

    staging: lustre: Correct missing newline
    Several error messages are missing newline characters
    at the end of the message. Newlines are added where
    necessary and other minor corrections; no punctuation
    at the end of an error message, add a return code to
    the end of error messages, device name at the beginning,
    etc.
    
    There are just a couple of places where newlines are
    removed and this is only in LDLM_DEBUG_NOLOCK. The definition
    of LDLM_DEBUG_NOLOCK already has a newline in it and
    resulted in double newlines printed.
    
    Signed-off-by: James Nunez <james.a.nunez@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-4871
    Reviewed-on: http://review.whamcloud.com/10000
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Cliff White <cliff.white@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/cl_lock.c
+++ drivers/staging/lustre/lustre/obdclass/cl_lock.c
@@ -98,7 +98,7 @@ static int cl_lock_invariant(const struct lu_env *env,
 	result = atomic_read(&lock->cll_ref) > 0 &&
 		cl_lock_invariant_trusted(env, lock);
 	if (!result && env)
-		CL_LOCK_DEBUG(D_ERROR, env, lock, "invariant broken");
+		CL_LOCK_DEBUG(D_ERROR, env, lock, "invariant broken\n");
 	return result;
 }
 
