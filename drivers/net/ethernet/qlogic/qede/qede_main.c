40683e83e2b8c5c6020a7134c4768dcf4762524f
Manish Chopra <manish.chopra@qlogic.com>
2016-03-08 04:09:44 -0500

    qede: Fix net-next "make ARCH=x86_64"
    'commit 55482edc25f0606851de42e73618f813f310d009
    ("qede: Add slowpath/fastpath support and enable hardware GRO")'
    introduces below error when compiling net-next with "make ARCH=x86_64"
    
    drivers/built-in.o: In function `qede_rx_int':
    qede_main.c:(.text+0x6101a0): undefined reference to `tcp_gro_complete'
    
    Signed-off-by: Manish Chopra <manish.chopra@qlogic.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qlogic/qede/qede_main.c
+++ drivers/net/ethernet/qlogic/qede/qede_main.c
@@ -1002,6 +1002,7 @@ static void qede_tpa_start(struct qede_dev *edev,
 	}
 }
 
+#ifdef CONFIG_INET
 static void qede_gro_ip_csum(struct sk_buff *skb)
 {
 	const struct iphdr *iph = ip_hdr(skb);
@@ -1030,12 +1031,14 @@ static void qede_gro_ipv6_csum(struct sk_buff *skb)
 				  &iph->saddr, &iph->daddr, 0);
 	tcp_gro_complete(skb);
 }
+#endif
 
 static void qede_gro_receive(struct qede_dev *edev,
 			     struct qede_fastpath *fp,
 			     struct sk_buff *skb,
 			     u16 vlan_tag)
 {
+#ifdef CONFIG_INET
 	if (skb_shinfo(skb)->gso_size) {
 		switch (skb->protocol) {
 		case htons(ETH_P_IP):
@@ -1050,7 +1053,7 @@ static void qede_gro_receive(struct qede_dev *edev,
 			       ntohs(skb->protocol));
 		}
 	}
-
+#endif
 	skb_record_rx_queue(skb, fp->rss_id);
 	qede_skb_receive(edev, fp, skb, vlan_tag);
 }
