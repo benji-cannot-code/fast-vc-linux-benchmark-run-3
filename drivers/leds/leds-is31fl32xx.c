3c17b5e1246689d7b21bd479e66cd9167878eef5
David Rivshin <drivshin@allworx.com>
2016-03-07 19:57:16 -0500

    leds: Add SN3218 and SN3216 support to the IS31FL32XX driver
    Si-En Technology was acquired by ISSI in 2011, and it appears that
    the IS31FL3218/IS31FL3216 are just rebranded SN3218/SN3216 devices.
    
    Add the "si-en,sn3218" and "si-en,sn3216" compatible strings into the
    IS31FL32XX driver as aliases for the issi equivalents, and update
    binding documentation.
    
    Datasheets:
        IS31FL3218: http://www.issi.com/WW/pdf/31FL3218.pdf
        SN3218:     http://www.si-en.com/uploadpdf/s2011517171720.pdf
    
        IS31FL3216: http://www.issi.com/WW/pdf/31FL3216.pdf
        SN3216:     http://www.si-en.com/uploadpdf/SN3216201152410148.pdf
    
    Signed-off-by: David Rivshin <drivshin@allworx.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: Jacek Anaszewski <j.anaszewski@samsung.com>

--- drivers/leds/leds-is31fl32xx.c
+++ drivers/leds/leds-is31fl32xx.c
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * Datasheets: http://www.issi.com/US/product-analog-fxled-driver.shtml
+ * Datasheets:
+ *   http://www.issi.com/US/product-analog-fxled-driver.shtml
+ *   http://www.si-en.com/product.asp?parentid=890
  */
 
 #include <linux/device.h>
@@ -425,7 +427,9 @@ static const struct of_device_id of_is31fl31xx_match[] = {
 	{ .compatible = "issi,is31fl3236", .data = &is31fl3236_cdef, },
 	{ .compatible = "issi,is31fl3235", .data = &is31fl3235_cdef, },
 	{ .compatible = "issi,is31fl3218", .data = &is31fl3218_cdef, },
+	{ .compatible = "si-en,sn3218",    .data = &is31fl3218_cdef, },
 	{ .compatible = "issi,is31fl3216", .data = &is31fl3216_cdef, },
+	{ .compatible = "si-en,sn3216",    .data = &is31fl3216_cdef, },
 	{},
 };
 
