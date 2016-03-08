885b45f929a70cf1fd4c82b9d98b92d125a118a3
Simon Horman <horms+renesas@verge.net.au>
2016-03-08 09:48:55 +0900

    clk: renesas: div6: use RENESAS for #define
    Name the #define guarding compilation of this header
    __RENESAS_CLK_DIV6_H__ rather than __SHMOBILE_CLK_DIV6_H__.
    
    This is a follow-up to renaming the directory in which this file lives from
    shmobile to renesas which is in turn part of an ongoing process to migrate
    from ARCH_SHMOBILE to ARCH_RENESAS the motivation for which being that
    RENESAS seems to be a more appropriate name than SHMOBILE for the majority
    of Renesas ARM based SoCs.
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Acked-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/renesas/clk-div6.h
+++ drivers/clk/renesas/clk-div6.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef __SHMOBILE_CLK_DIV6_H__
-#define __SHMOBILE_CLK_DIV6_H__
+#ifndef __RENESAS_CLK_DIV6_H__
+#define __RENESAS_CLK_DIV6_H__
 
 struct clk *cpg_div6_register(const char *name, unsigned int num_parents,
 			      const char **parent_names, void __iomem *reg);
