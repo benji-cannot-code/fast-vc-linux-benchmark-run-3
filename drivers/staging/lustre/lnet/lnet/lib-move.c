511511c6c5cddea50fe44ab1f32d5b49915ee095
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2016-03-12 01:30:15 +0530

    staging: lustre: lnet: lib-move: Use list_for_each_entry_safe
    Doubly linked lists which are  iterated  using list_empty
    and list_entry macros have been replaced with list_for_each_entry_safe
    macro.
    This makes the iteration simpler and more readable.
    
    This patch replaces the while loop containing list_empty and list_entry
    with list_for_each_entry_safe.
    
    This was done with Coccinelle.
    
    @@
    expression E1;
    identifier I1, I2;
    type T;
    iterator name list_for_each_entry_safe;
    @@
    
    T *I1;
    + T *tmp;
    ...
    - while (list_empty(&E1) == 0)
    + list_for_each_entry_safe (I1, tmp, &E1, I2)
    {
    ...when != T *I1;
    - I1 = list_entry(E1.next, T, I2);
    ...
    }
    
    Signed-off-by: Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/lnet/lib-move.c
+++ drivers/staging/lustre/lnet/lnet/lib-move.c
@@ -51,6 +51,7 @@ int
 lnet_fail_nid(lnet_nid_t nid, unsigned int threshold)
 {
 	lnet_test_peer_t *tp;
+	lnet_test_peer_t *temp;
 	struct list_head *el;
 	struct list_head *next;
 	struct list_head cull;
@@ -89,9 +90,7 @@ lnet_fail_nid(lnet_nid_t nid, unsigned int threshold)
 
 	lnet_net_unlock(0);
 
-	while (!list_empty(&cull)) {
-		tp = list_entry(cull.next, lnet_test_peer_t, tp_list);
-
+	list_for_each_entry_safe(tp, temp, &cull, tp_list) {
 		list_del(&tp->tp_list);
 		LIBCFS_FREE(tp, sizeof(*tp));
 	}
@@ -102,6 +101,7 @@ static int
 fail_peer(lnet_nid_t nid, int outgoing)
 {
 	lnet_test_peer_t *tp;
+	lnet_test_peer_t *temp;
 	struct list_head *el;
 	struct list_head *next;
 	struct list_head cull;
@@ -148,8 +148,7 @@ fail_peer(lnet_nid_t nid, int outgoing)
 
 	lnet_net_unlock(0);
 
-	while (!list_empty(&cull)) {
-		tp = list_entry(cull.next, lnet_test_peer_t, tp_list);
+	list_for_each_entry_safe(tp, temp, &cull, tp_list) {
 		list_del(&tp->tp_list);
 
 		LIBCFS_FREE(tp, sizeof(*tp));
