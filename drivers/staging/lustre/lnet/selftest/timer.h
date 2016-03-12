f7673a03fa1f923e0372def6c71d980552ba0ba7
James Simmons <jsimmons@infradead.org>
2016-03-11 20:29:44 -0500

    staging: lustre: fix spacing issues checkpatch reported in lnet selftest
    Remove any extra spacing as reported by checkpatch.
    
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/selftest/timer.h
+++ drivers/staging/lustre/lnet/selftest/timer.h
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct stt_timer {
 	struct list_head stt_list;
 	time64_t         stt_expires;
-	void             (*stt_func) (void *);
+	void             (*stt_func)(void *);
 	void             *stt_data;
 };
 
