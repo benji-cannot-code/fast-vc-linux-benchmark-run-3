d8292e9d2d7e357f0e7670524a24fb281739e69d
Sandhya Bankar <bankarsandhya512@gmail.com>
2016-03-13 15:50:03 +0530

    Staging: gs_fpgaboot: Remove unnecessary blank lines.
    Remove unnecessary blank lines.
    
    Signed-off-by: Sandhya Bankar <bankarsandhya512@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gs_fpgaboot/gs_fpgaboot.c
+++ drivers/staging/gs_fpgaboot/gs_fpgaboot.c
@@ -94,7 +94,6 @@ static int readlength_bitstream(char *bitdata, int *lendata, int *offset)
 	return 0;
 }
 
-
 /*
  * read first 13 bytes to check bitstream magic number
  */
@@ -332,7 +331,6 @@ err_out1:
 	kfree(fimage);
 
 	return -1;
-
 }
 
 static int __init gs_fpgaboot_init(void)
