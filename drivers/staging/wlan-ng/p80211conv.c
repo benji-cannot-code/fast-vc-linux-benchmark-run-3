9dd1de95e7630e3f757cdb077ec3b4b4ed0c9f05
Claudiu Beznea <claudiu.beznea@gmail.com>
2016-03-14 22:45:13 +0200

    Staging: wlan-ng: defined oui_rfc1042[] and oui_8021h[] arrays as const arrays
    This patch defines oui_rfc1042[] and oui_8021h[] arrays from
    p80211conv.c as const arrays since these are not changed
    anywhere in code.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/p80211conv.c
+++ drivers/staging/wlan-ng/p80211conv.c
@@ -76,8 +76,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "p80211ioctl.h"
 #include "p80211req.h"
 
-static u8 oui_rfc1042[] = { 0x00, 0x00, 0x00 };
-static u8 oui_8021h[] = { 0x00, 0x00, 0xf8 };
+static const u8 oui_rfc1042[] = { 0x00, 0x00, 0x00 };
+static const u8 oui_8021h[] = { 0x00, 0x00, 0xf8 };
 
 /*----------------------------------------------------------------
 * p80211pb_ether_to_80211
