3f01d20b69208043a03d5bcf7c28c9a501c07340
Liu Ying <gnuiyl@gmail.com>
2016-03-14 16:10:11 +0800

    drm/imx: ipuv3-plane: Configure DMFC wait4eot bit after slots are determined
    Just as the function ipu_dmfc_config_wait4eot() tells, the DMFC wait4eot bit
    depends on the number of DMFC slots to be used, so it should be called after
    the slots are determined in the function ipu_dmfc_alloc_bandwidth().
    Based on tests, this patch may eliminate display distortion issue on overlay
    plane with small resolutions.  To reproduce the issue, we may run this drm
    modetest case - 'modetest -P 19:64x64'.
    
    Signed-off-by: Liu Ying <gnuiyl@gmail.com>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/drm/imx/ipuv3-plane.c
+++ drivers/gpu/drm/imx/ipuv3-plane.c
@@ -281,8 +281,6 @@ int ipu_plane_mode_set(struct ipu_plane *ipu_plane, struct drm_crtc *crtc,
 		}
 	}
 
-	ipu_dmfc_config_wait4eot(ipu_plane->dmfc, crtc_w);
-
 	ret = ipu_dmfc_alloc_bandwidth(ipu_plane->dmfc,
 			calc_bandwidth(crtc_w, crtc_h,
 				       calc_vref(mode)), 64);
@@ -290,6 +288,8 @@ int ipu_plane_mode_set(struct ipu_plane *ipu_plane, struct drm_crtc *crtc,
 		dev_err(dev, "allocating dmfc bandwidth failed with %d\n", ret);
 		return ret;
 	}
+
+	ipu_dmfc_config_wait4eot(ipu_plane->dmfc, crtc_w);
 
 	ipu_cpmem_zero(ipu_plane->ipu_ch);
 	ipu_cpmem_set_resolution(ipu_plane->ipu_ch, src_w, src_h);
