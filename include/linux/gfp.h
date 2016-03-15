586507f3aaac0da29caa479a79c94f34f5a2316c
Vlastimil Babka <vbabka@suse.cz>
2016-03-15 14:55:45 -0700

    mm, tracing: make show_gfp_flags() up to date
    The show_gfp_flags() macro provides human-friendly printing of gfp flags
    in tracepoints.  However, it is somewhat out of date and missing several
    flags.  This patches fills in the missing flags, and distinguishes
    properly between GFP_ATOMIC and __GFP_ATOMIC which were both translated
    to "GFP_ATOMIC".  More generally, all __GFP_X flags which were
    previously printed as GFP_X, are now printed as __GFP_X, since ommiting
    the underscores results in output that doesn't actually match the source
    code, and can only lead to confusion.  Where both variants are defined
    equal (e.g.  _DMA and _DMA32), the variant without underscores are
    preferred.
    
    Also add a note in gfp.h so hopefully future changes will be synced
    better.
    
    __GFP_MOVABLE is defined twice in include/linux/gfp.h with different
    comments.  Leave just the newer one, which was intended to replace the
    old one.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Michal Hocko <mhocko@suse.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Sasha Levin <sasha.levin@oracle.com>
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -10,6 +10,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct vm_area_struct;
 
+/*
+ * In case of changes, please don't forget to update
+ * include/trace/events/gfpflags.h
+ */
+
 /* Plain integer GFP bitmasks. Do not use this directly. */
 #define ___GFP_DMA		0x01u
 #define ___GFP_HIGHMEM		0x02u
@@ -49,7 +54,6 @@ struct vm_area_struct;
 #define __GFP_DMA	((__force gfp_t)___GFP_DMA)
 #define __GFP_HIGHMEM	((__force gfp_t)___GFP_HIGHMEM)
 #define __GFP_DMA32	((__force gfp_t)___GFP_DMA32)
-#define __GFP_MOVABLE	((__force gfp_t)___GFP_MOVABLE)  /* Page is movable */
 #define __GFP_MOVABLE	((__force gfp_t)___GFP_MOVABLE)  /* ZONE_MOVABLE allowed */
 #define GFP_ZONEMASK	(__GFP_DMA|__GFP_HIGHMEM|__GFP_DMA32|__GFP_MOVABLE)
 
