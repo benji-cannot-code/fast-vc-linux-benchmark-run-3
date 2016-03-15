c6fd8dffbf71742a3737f51e7d5c024d9b9bca82
Michael Ellerman <mpe@ellerman.id.au>
2016-03-15 20:30:20 +1100

    powerpc/rcpm: Fix build break when SMP=n
    Add an include of asm/smp.h to fix a build break when SMP=n:
    
      arch/powerpc/sysdev/fsl_rcpm.c:32:2: error: implicit declaration of
      function 'get_hard_smp_processor_id'
    
    Fixes: d17799f9c10e ("powerpc/rcpm: add RCPM driver")
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/sysdev/fsl_rcpm.c
+++ arch/powerpc/sysdev/fsl_rcpm.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fsl/guts.h>
 #include <asm/cputhreads.h>
 #include <asm/fsl_pm.h>
+#include <asm/smp.h>
 
 static struct ccsr_rcpm_v1 __iomem *rcpm_v1_regs;
 static struct ccsr_rcpm_v2 __iomem *rcpm_v2_regs;
