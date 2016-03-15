6c91625d5e09a17949228d3b6633b0dff2c5826b
Leo Kim <leo.kim@atmel.com>
2016-03-15 18:48:13 +0900

    staging: wilc1000: removes goto definitions from wilc_wlan_firmware_download
    This patch removes goto definitions from wilc_wlan_firmware_download function.
    Goto '_fail_1' feature is error return.
    It returns error type directly without result variable replacement as well.
    
    Signed-off-by: Leo Kim <leo.kim@atmel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan.c
+++ drivers/staging/wilc1000/wilc_wlan.c
@@ -947,10 +947,8 @@ int wilc_wlan_firmware_download(struct wilc *wilc, const u8 *buffer,
 	blksz = BIT(12);
 
 	dma_buffer = kmalloc(blksz, GFP_KERNEL);
-	if (!dma_buffer) {
-		ret = -EIO;
-		goto _fail_1;
-	}
+	if (!dma_buffer)
+		return -EIO;
 
 	offset = 0;
 	do {
@@ -987,8 +985,6 @@ int wilc_wlan_firmware_download(struct wilc *wilc, const u8 *buffer,
 _fail_:
 
 	kfree(dma_buffer);
-
-_fail_1:
 
 	return (ret < 0) ? ret : 0;
 }
