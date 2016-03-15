a821b5e2c288ffd5d16fe759fcd6e6e3cf9c092f
Christophe Leroy <christophe.leroy@c-s.fr>
2016-03-15 14:07:49 +0100

    powerpc/8xx: Fix do_mtspr_cpu6() build on older compilers
    GCC < 4.9 is unable to build this, saying:
    
      arch/powerpc/mm/8xx_mmu.c:139:2: error: memory input 1 is not directly addressable
    
    Change the one-element array into a simple variable to avoid this.
    
    Fixes: 1458dd951f7c ("powerpc/8xx: Handle CPU6 ERRATA directly in mtspr() macro")
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Cc: Scott Wood <oss@buserror.net>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/reg_8xx.h
+++ arch/powerpc/include/asm/reg_8xx.h
@@ -54,7 +54,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_8xx_CPU6
 #define do_mtspr_cpu6(rn, rn_addr, v)	\
 	do {								\
-		int _reg_cpu6 = rn_addr, _tmp_cpu6[1];		\
+		int _reg_cpu6 = rn_addr, _tmp_cpu6;		\
 		asm volatile("stw %0, %1;"				\
 			     "lwz %0, %1;"				\
 			     "mtspr " __stringify(rn) ",%2" :		\
