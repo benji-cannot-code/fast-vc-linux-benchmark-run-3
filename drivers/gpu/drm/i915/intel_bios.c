488bb4e4f8f7428ae87e77e7d0cb0ce5d61b34c3
Jani Nikula <jani.nikula@intel.com>
2016-03-16 12:43:35 +0200

    drm/i915: hide away VBT private data in a separate header
    We've been accumulating code across the driver that depends on the VBT
    specific structures and defines. The VBT is an uncontrollable
    beast. Encourage encapsulation of the VBT data by hiding the structures
    and defines in a private header only to be included from intel_bios.c.
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1458125015-7931-7-git-send-email-jani.nikula@intel.com

--- drivers/gpu/drm/i915/intel_bios.c
+++ drivers/gpu/drm/i915/intel_bios.c
@@ -30,7 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drmP.h>
 #include <drm/i915_drm.h>
 #include "i915_drv.h"
-#include "intel_bios.h"
+
+#define _INTEL_BIOS_PRIVATE
+#include "intel_vbt_defs.h"
 
 /**
  * DOC: Video BIOS Table (VBT)
