6892aee5ef34d7b3bbbd129d84e2b4698ead8ad0
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2016-03-17 14:18:45 -0700

    thp, vmstats: count deferred split events
    Count how many times we put a THP in split queue.  Currently, it happens
    on partial unmap of a THP.
    
    Rapidly growing value can indicate that an application behaves
    unfriendly wrt THP: often fault in huge page and then unmap part of it.
    This leads to unnecessary memory fragmentation and the application may
    require tuning.
    
    The event also can help with debugging kernel [mis-]behaviour.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -849,6 +849,7 @@ const char * const vmstat_text[] = {
 	"thp_collapse_alloc_failed",
 	"thp_split_page",
 	"thp_split_page_failed",
+	"thp_deferred_split_page",
 	"thp_split_pmd",
 	"thp_zero_page_alloc",
 	"thp_zero_page_alloc_failed",
