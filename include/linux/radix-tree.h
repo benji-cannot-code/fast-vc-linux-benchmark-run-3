a1344504451710f6676c3a2ea4ec05d3474032ae
Matthew Wilcox <willy@linux.intel.com>
2016-03-17 14:21:42 -0700

    radix-tree: add an explicit include of bitops.h
    The radix-tree header uses the __ffs() function, which is defined in
    bitops.h.  The current kernel headers implicitly include bitops.h, but
    the userspace test harness does not.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Matthew Wilcox <willy@linux.intel.com>
    Cc: "Kirill A. Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
    Cc: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/radix-tree.h
+++ include/linux/radix-tree.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_RADIX_TREE_H
 #define _LINUX_RADIX_TREE_H
 
+#include <linux/bitops.h>
 #include <linux/preempt.h>
 #include <linux/types.h>
 #include <linux/bug.h>
