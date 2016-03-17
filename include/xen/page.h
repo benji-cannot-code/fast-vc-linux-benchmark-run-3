39ef2562336788e6dd4e37e67e4b2427b6ee4abb
Ross Lagerwall <ross.lagerwall@citrix.com>
2016-03-17 16:51:59 +0000

    xen: Fix page <-> pfn conversion on 32 bit systems
    Commit 1084b1988d22dc165c9dbbc2b0e057f9248ac4db (xen: Add Xen specific
    page definition) caused a regression in 4.4.
    
    The xen functions to convert between pages and pfns fail due to an
    overflow on systems where a physical address may not fit in an
    unsigned long (e.g. x86 32 bit PAE systems). Rework the conversion to
    avoid overflow. This should also result in simpler object code.
    
    This bug manifested itself as disk corruption with Linux 4.4 when
    using blkfront in a Xen HVM x86 32 bit guest with more than 4 GiB of
    memory.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Cc: <stable@vger.kernel.org> # 4.4+
    Signed-off-by: David Vrabel <david.vrabel@citrix.com>

--- include/xen/page.h
+++ include/xen/page.h
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #define xen_pfn_to_page(xen_pfn)	\
-	((pfn_to_page(((unsigned long)(xen_pfn) << XEN_PAGE_SHIFT) >> PAGE_SHIFT)))
+	(pfn_to_page((unsigned long)(xen_pfn) >> (PAGE_SHIFT - XEN_PAGE_SHIFT)))
 #define page_to_xen_pfn(page)		\
-	(((page_to_pfn(page)) << PAGE_SHIFT) >> XEN_PAGE_SHIFT)
+	((page_to_pfn(page)) << (PAGE_SHIFT - XEN_PAGE_SHIFT))
 
 #define XEN_PFN_PER_PAGE	(PAGE_SIZE / XEN_PAGE_SIZE)
 
