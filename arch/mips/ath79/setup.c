3330cb7c8bda5adba29fd29ab0a482290f349c5c
Antony Pavlov <antonynpavlov@gmail.com>
2016-03-17 06:34:15 +0300

    MIPS: ath79: update devicetree clock support for AR9132
    Current ath79 clock.c code does not read reference clock and
    pll setup from devicetree. E.g. you can set any clock rate value
    in board DTS but it will have no effect on the real clk calculation.
    
    This patch fixes some AR9132 devicetree clock support defects:
    
      * clk initialization function ath79_clocks_init_dt_ng()
        is introduced; it actually gets pll block base register
        address and reference clock from devicetree;
      * pll register parsing code is moved to the separate
        ar724x_clk_init() function; this function
        can be called from platform code or from devicetree code.
    
    Also mips_hpt_frequency value is set from dt, so the appropriate
    clock parameter is added to the cpu@0 devicetree node.
    
    The same approach can be used for adding AR9331 devicetree support.
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Cc: Gabor Juhos <juhosg@openwrt.org>
    Cc: Alban Bedel <albeu@free.fr>
    Cc: Michael Turquette <mturquette@baylibre.com>
    Cc: Stephen Boyd <sboyd@codeaurora.org>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: linux-clk@vger.kernel.org
    Cc: linux-mips@linux-mips.org
    Cc: devicetree@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/12876/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ath79/setup.c
+++ arch/mips/ath79/setup.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bootmem.h>
 #include <linux/err.h>
 #include <linux/clk.h>
+#include <linux/clk-provider.h>
 #include <linux/of_platform.h>
 #include <linux/of_fdt.h>
 
@@ -223,12 +224,47 @@ void __init plat_mem_setup(void)
 	pm_power_off = ath79_halt;
 }
 
+static void __init ath79_of_plat_time_init(void)
+{
+	struct device_node *np;
+	struct clk *clk;
+	unsigned long cpu_clk_rate;
+
+	of_clk_init(NULL);
+
+	np = of_get_cpu_node(0, NULL);
+	if (!np) {
+		pr_err("Failed to get CPU node\n");
+		return;
+	}
+
+	clk = of_clk_get(np, 0);
+	if (IS_ERR(clk)) {
+		pr_err("Failed to get CPU clock: %ld\n", PTR_ERR(clk));
+		return;
+	}
+
+	cpu_clk_rate = clk_get_rate(clk);
+
+	pr_info("CPU clock: %lu.%03lu MHz\n",
+		cpu_clk_rate / 1000000, (cpu_clk_rate / 1000) % 1000);
+
+	mips_hpt_frequency = cpu_clk_rate / 2;
+
+	clk_put(clk);
+}
+
 void __init plat_time_init(void)
 {
 	unsigned long cpu_clk_rate;
 	unsigned long ahb_clk_rate;
 	unsigned long ddr_clk_rate;
 	unsigned long ref_clk_rate;
+
+	if (IS_ENABLED(CONFIG_OF) && mips_machtype == ATH79_MACH_GENERIC_OF) {
+		ath79_of_plat_time_init();
+		return;
+	}
 
 	ath79_clocks_init();
 
