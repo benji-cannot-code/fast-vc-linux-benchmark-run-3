675901676c5c0bc644b8043a00744426320bad9b
Arnd Bergmann <arnd@arndb.de>
2016-03-18 16:53:31 +0100

    mfd: db8500 stop passing around platform data
    Except for the constant DB8500_PRCMU_FW_VERSION_OFFSET number, nothing
    is ever passed through the platform data and used in a driver, so we
    can simply stop passing it around.
    
    Acked-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/setup.h
+++ arch/arm/mach-ux500/setup.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach/arch.h>
 #include <linux/init.h>
-#include <linux/mfd/abx500/ab8500.h>
 
 void ux500_restart(enum reboot_mode mode, const char *cmd);
 
