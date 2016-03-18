ffd0d7eb2c6b0c711dc5ddaeef7c01d96b7dbc7f
Arnd Bergmann <arnd@arndb.de>
2016-03-18 16:21:36 +0100

    ARM: ux500: move ab8500_regulator_plat_data into driver
    There is only one instance of ab8500_regulator_platform_data, and it's
    safe to assume we won't ever merge another one, so it's rather pointless
    to pass it through multiple levels of platform data pointers.
    
    This moves the structure and everything referenced by it into the
    driver that uses it.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -29,13 +29,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "setup.h"
 
-#include "board-mop500-regulators.h"
 #include "board-mop500.h"
 #include "db8500-regs.h"
 #include "id.h"
 
 static struct ab8500_platform_data ab8500_platdata = {
-	.regulator	= &ab8500_regulator_plat_data,
 };
 
 static struct prcmu_pdata db8500_prcmu_pdata = {
