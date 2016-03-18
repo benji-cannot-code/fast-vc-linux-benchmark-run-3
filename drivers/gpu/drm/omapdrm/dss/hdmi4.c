cd30a9aa8fd2f7c2812b20be8fe031f40849e21b
Tomi Valkeinen <tomi.valkeinen@ti.com>
2016-03-18 09:02:18 +0200

    drm/omap: Fix missing includes
    With certain kernel config options many omapdrm files fail to compile
    due to missing include of linux/gpio/consumer.h and linux/of.h.
    
    This patch adds those includes.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reported-by: Dan Murphy <dmurphy@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/gpu/drm/omapdrm/dss/hdmi4.c
+++ drivers/gpu/drm/omapdrm/dss/hdmi4.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/regulator/consumer.h>
 #include <linux/component.h>
+#include <linux/of.h>
 #include <video/omapdss.h>
 #include <sound/omap-hdmi-audio.h>
 
