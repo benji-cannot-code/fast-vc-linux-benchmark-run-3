fc78e883a36de3ef163152b597b88324c4220d59
James Simmons <jsimmons@infradead.org>
2016-03-22 19:04:13 -0400

    staging: lustre: libcfs: add uapi headers to libcfs_ioctl.h
    Need a few uapi headers to make libcfs_ioctl.h compilable in
    userland.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6245
    Reviewed-on: http://review.whamcloud.com/17643
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/libcfs_ioctl.h
+++ drivers/staging/lustre/include/linux/libcfs/libcfs_ioctl.h
@@ -42,6 +42,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LIBCFS_IOCTL_H__
 #define __LIBCFS_IOCTL_H__
 
+#include <linux/types.h>
+#include <linux/ioctl.h>
+
 #define LIBCFS_IOCTL_VERSION	0x0001000a
 #define LIBCFS_IOCTL_VERSION2	0x0001000b
 
