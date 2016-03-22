f2b78fc7fc9e8df61f01377a4f02348ae498354c
Andy Lutomirski <luto@kernel.org>
2016-03-22 14:25:21 -0700

    input: redefine INPUT_COMPAT_TEST as in_compat_syscall()
    The input compat code should work like all other compat code: for 32-bit
    syscalls, use the 32-bit ABI and for 64-bit syscalls, use the 64-bit
    ABI.  We have a helper for that (in_compat_syscall()): just use it.
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>
    Cc: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/input/input-compat.h
+++ drivers/input/input-compat.h
@@ -18,17 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_COMPAT
 
-/* Note to the author of this code: did it ever occur to
-   you why the ifdefs are needed? Think about it again. -AK */
-#if defined(CONFIG_X86_64) || defined(CONFIG_TILE)
-#  define INPUT_COMPAT_TEST is_compat_task()
-#elif defined(CONFIG_S390)
-#  define INPUT_COMPAT_TEST test_thread_flag(TIF_31BIT)
-#elif defined(CONFIG_MIPS)
-#  define INPUT_COMPAT_TEST test_thread_flag(TIF_32BIT_ADDR)
-#else
-#  define INPUT_COMPAT_TEST test_thread_flag(TIF_32BIT)
-#endif
+#define INPUT_COMPAT_TEST in_compat_syscall()
 
 struct input_event_compat {
 	struct compat_timeval time;
