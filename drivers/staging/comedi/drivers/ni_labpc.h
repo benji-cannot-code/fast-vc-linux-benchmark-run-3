8fb4163f9eaf86241be6db5822897a8af857f945
H Hartley Sweeten <hsweeten@visionengravers.com>
2016-03-22 11:21:35 -0700

    staging: comedi: ni_labpc: remove some unnecessary defines
    The EEPROM_SIZE and NUM_AO_CHAN defines are only used once and they
    don't add any significant clarity to the driver. They are also pretty
    generic symbol names. Remove them and just open code the values.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/ni_labpc.h
+++ drivers/staging/comedi/drivers/ni_labpc.h
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _NI_LABPC_H
 #define _NI_LABPC_H
 
-#define EEPROM_SIZE	256	/*  256 byte eeprom */
-#define NUM_AO_CHAN	2	/*  boards have two analog output channels */
-
 enum transfer_type { fifo_not_empty_transfer, fifo_half_full_transfer,
 	isa_dma_transfer
 };
