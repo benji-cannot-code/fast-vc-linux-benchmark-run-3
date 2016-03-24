baebe151fd97322d13956bd4013cb58539f7298c
Lv Zheng <lv.zheng@intel.com>
2016-03-24 09:38:28 +0800

    ACPICA: Linuxize: reduce divergences for 20160212 release
    The patch reduces source code differences between the Linux kernel and the
    ACPICA upstream so that the linuxized ACPICA 20160212 release can be
    applied with reduced human intervention.
    
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    [ rjw: White space damage fixes ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/nsinit.c
+++ drivers/acpi/acpica/nsinit.c
@@ -603,7 +603,6 @@ acpi_ns_init_one_device(acpi_handle obj_handle,
 	info->flags = ACPI_IGNORE_RETURN_VALUE;
 
 	status = acpi_ns_evaluate(info);
-
 	if (ACPI_SUCCESS(status)) {
 		walk_info->num_INI++;
 	}
