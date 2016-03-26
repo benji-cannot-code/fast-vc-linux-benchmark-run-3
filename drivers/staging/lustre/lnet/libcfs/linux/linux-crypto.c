469db961918ce00da8108a300189f858ec7a61e6
Andreas Dilger <andreas.dilger@intel.com>
2016-03-26 15:40:45 -0400

    staging: lustre: libcfs: limit scope of libcfs_crypto.h
    Remove <libcfs/libcfs_crypto.h> from <libcfs/libcfs.h> and only
    include it into the places where it is actually needed.  This
    works out to be the same places as <obd_cksum.h>, so put it there.
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-5053
    Reviewed-on: http://review.whamcloud.com/9990
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/libcfs/linux/linux-crypto.c
+++ drivers/staging/lustre/lnet/libcfs/linux/linux-crypto.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/hash.h>
 #include <linux/scatterlist.h>
 #include "../../../include/linux/libcfs/libcfs.h"
+#include "../../../include/linux/libcfs/libcfs_crypto.h"
 #include "linux-crypto.h"
 /**
  *  Array of  hash algorithm speed in MByte per second
