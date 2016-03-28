5cd447c5f1f78824d921ab1d4512bb9131fe6161
Anup Patel <anup.patel@broadcom.com>
2016-03-28 10:18:26 +0530

    phy: Rename phy-brcmstb-sata driver to phy-brcm-sata driver
    Currently, we have a common SATA3 PHY driver for all Broadcom
    STB SoCs. This driver can be extended and re-used for Broadcom
    iProc SoCs having same SATA3 PHY.
    
    This patch renames existing Broadcom STB SATA3 PHY driver to
    common Broadcom SATA3 PHY driver to share this PHY driver across
    Broadcom SoCs.
    
    Signed-off-by: Anup Patel <anup.patel@broadcom.com>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/phy-brcm-sata.c
+++ drivers/phy/phy-brcm-sata.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Broadcom SATA3 AHCI Controller PHY Driver
  *
- * Copyright © 2009-2015 Broadcom Corporation
+ * Copyright (C) 2016 Broadcom
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -239,13 +239,13 @@ static struct platform_driver brcm_sata_phy_driver = {
 	.probe	= brcm_sata_phy_probe,
 	.driver	= {
 		.of_match_table	= brcm_sata_phy_of_match,
-		.name		= "brcmstb-sata-phy",
+		.name		= "brcm-sata-phy",
 	}
 };
 module_platform_driver(brcm_sata_phy_driver);
 
-MODULE_DESCRIPTION("Broadcom STB SATA PHY driver");
+MODULE_DESCRIPTION("Broadcom SATA PHY driver");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Marc Carino");
 MODULE_AUTHOR("Brian Norris");
-MODULE_ALIAS("platform:phy-brcmstb-sata");
+MODULE_ALIAS("platform:phy-brcm-sata");
