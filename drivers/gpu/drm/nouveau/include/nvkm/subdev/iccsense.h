eafd03147292d253cea55b4c29073c50f649d20c
Karol Herbst <nouveau@karolherbst.de>
2016-03-28 14:24:31 +0200

    drm/nouveau/iccsense: split sensor into own struct
    v2: add list_del call, reword error message
    
    Signed-off-by: Karol Herbst <nouveau@karolherbst.de>
    Reviewed-by: Martin Peres <martin.peres@free.fr>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/iccsense.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/iccsense.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct nvkm_iccsense {
 	struct nvkm_subdev subdev;
 	bool data_valid;
+	struct list_head sensors;
 	struct list_head rails;
 };
 
