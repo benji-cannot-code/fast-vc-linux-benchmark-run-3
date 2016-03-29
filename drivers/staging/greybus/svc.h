d0aead0d5c69ec03782d4923157d6a27dee7d444
Johan Hovold <johan@hovoldconsulting.com>
2016-03-29 18:55:59 -0400

    greybus: svc: indent CPort flag defines properly
    Use tabs to indent CPort-flag values.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/svc.h
+++ drivers/staging/greybus/svc.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SVC_H
 #define __SVC_H
 
-#define GB_SVC_CPORT_FLAG_E2EFC       BIT(0)
-#define GB_SVC_CPORT_FLAG_CSD_N       BIT(1)
-#define GB_SVC_CPORT_FLAG_CSV_N       BIT(2)
+#define GB_SVC_CPORT_FLAG_E2EFC		BIT(0)
+#define GB_SVC_CPORT_FLAG_CSD_N		BIT(1)
+#define GB_SVC_CPORT_FLAG_CSV_N		BIT(2)
 
 enum gb_svc_state {
 	GB_SVC_STATE_RESET,
