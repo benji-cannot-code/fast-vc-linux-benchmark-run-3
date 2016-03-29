70b1cb5c70f358264049096ee7f11a84087ce71d
Zubair Lutfullah Kakakhel <Zubair.Kakakhel@imgtec.com>
2016-03-29 15:50:25 +0100

    MIPS: Detect DSP v3 support
    DSPv3 is supported on all MIPSr6 systems which indicate support for DSPv2.
    
    This doesn't require any changes to the kernel's handling of DSP
    resources. The patch is to detect support and indicate it in /proc/cpuinfo
    
    DSP v3 introduces a new instruction BPOSGE32C
    
    Signed-off-by: Zubair Lutfullah Kakakhel <Zubair.Kakakhel@imgtec.com>
    Reviewed-by: Paul Burton <paul.burton@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/12918/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -308,6 +308,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define cpu_has_dsp2		(cpu_data[0].ases & MIPS_ASE_DSP2P)
 #endif
 
+#ifndef cpu_has_dsp3
+#define cpu_has_dsp3		(cpu_data[0].ases & MIPS_ASE_DSP3)
+#endif
+
 #ifndef cpu_has_mipsmt
 #define cpu_has_mipsmt		(cpu_data[0].ases & MIPS_ASE_MIPSMT)
 #endif
