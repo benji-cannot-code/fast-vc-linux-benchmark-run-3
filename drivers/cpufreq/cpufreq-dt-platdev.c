03607f3a80d2ed48a2289645ff9f3c5414d923b5
Viresh Kumar <viresh.kumar@linaro.org>
2016-03-30 13:45:26 +0530

    cpufreq: dt: Add generic platform-device creation support
    Multiple platforms are using the generic cpufreq-dt driver now, and all
    of them are required to create a platform device with name "cpufreq-dt",
    in order to get the cpufreq-dt probed.
    
    Many of them do it from platform code, others have special drivers just
    to do that.
    
    It would be more sensible to do this at a generic place, where all such
    platform can mark their entries.
    
    This patch adds a separate file to get this device created. Currently
    the compat list of platforms that we support is empty, and will be
    filled in as and when we move platforms to use it.
    
    It always compiles as part of the kernel and so doesn't need a
    module-exit operation.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

---
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (C) 2016 Linaro.
+ * Viresh Kumar <viresh.kumar@linaro.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/err.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+
+static const struct of_device_id machines[] = {
+};
+
+static int __init cpufreq_dt_platdev_init(void)
+{
+	struct device_node *np = of_find_node_by_path("/");
+
+	if (!np)
+		return -ENODEV;
+
+	if (!of_match_node(machines, np))
+		return -ENODEV;
+
+	of_node_put(of_root);
+
+	return PTR_ERR_OR_ZERO(platform_device_register_simple("cpufreq-dt", -1,
+							       NULL, 0));
+}
+device_initcall(cpufreq_dt_platdev_init);
