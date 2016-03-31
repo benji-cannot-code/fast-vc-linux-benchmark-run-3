27bf02a6974eb03bdc9a94768cb3575f90069069
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2016-03-31 09:57:29 -0300

    [media] Revert "[media] sound/usb: Use Media Controller API to share media resources"
    Unfortunately, this patch caused several regressions at au0828 and
    snd-usb-audio, like this one:
    	https://bugzilla.kernel.org/show_bug.cgi?id=115561
    
    It also showed several troubles at the MC core that handles pretty
    poorly the memory protections and data lifetime management.
    
    So, better to revert it and fix the core before reapplying this
    change.
    
    This reverts commit aebb2b89bff0 ("[media] sound/usb: Use Media
    Controller API to share media resources")'
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- sound/usb/stream.c
+++ sound/usb/stream.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "format.h"
 #include "clock.h"
 #include "stream.h"
-#include "media.h"
 
 /*
  * free a substream
@@ -54,7 +53,6 @@ static void free_substream(struct snd_usb_substream *subs)
 		kfree(fp);
 	}
 	kfree(subs->rate_list.list);
-	media_snd_stream_delete(subs);
 }
 
 
