297e6c63c6fba5f687a45fc5843987bb69ab9382
Johannes Berg <johannes.berg@intel.com>
2016-03-31 20:02:06 +0300

    mac80211: remove rx_stats.last_rx update after sta alloc
    There's no need to update rx_stats.last_rx after allocating
    a station since it's already updated during allocation.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/ibss.c
+++ net/mac80211/ibss.c
@@ -650,8 +650,6 @@ ieee80211_ibss_add_sta(struct ieee80211_sub_if_data *sdata, const u8 *bssid,
 		return NULL;
 	}
 
-	sta->rx_stats.last_rx = jiffies;
-
 	/* make sure mandatory rates are always added */
 	sband = local->hw.wiphy->bands[band];
 	sta->sta.supp_rates[band] = supp_rates |
@@ -1236,8 +1234,6 @@ void ieee80211_ibss_rx_no_sta(struct ieee80211_sub_if_data *sdata,
 	sta = sta_info_alloc(sdata, addr, GFP_ATOMIC);
 	if (!sta)
 		return;
-
-	sta->rx_stats.last_rx = jiffies;
 
 	/* make sure mandatory rates are always added */
 	sband = local->hw.wiphy->bands[band];
