87f25c5bd07fd13eb19af5ed7ed0f4b0e6a2fe05
James Simmons <jsimmons@infradead.org>
2016-03-31 10:18:38 -0400

    staging: lustre: libcfs: remove MMSPACE macros
    Another abstraction that is not needed.
    
    Signed-off-by: James Simmons <uja.ornl@gmail.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6245
    Reviewed-on: http://review.whamcloud.com/13841
    Reviewed-by: frank zago <fzago@cray.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/linux-mem.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/linux-mem.h
@@ -66,9 +66,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NUM_CACHEPAGES totalram_pages
 #endif
 
-#define DECL_MMSPACE		mm_segment_t __oldfs
-#define MMSPACE_OPEN \
-	do { __oldfs = get_fs(); set_fs(get_ds()); } while (0)
-#define MMSPACE_CLOSE	       set_fs(__oldfs)
-
 #endif /* __LINUX_CFS_MEM_H__ */
