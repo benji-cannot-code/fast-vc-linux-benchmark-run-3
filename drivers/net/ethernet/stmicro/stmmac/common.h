60a9aa45da83a5f88cbeba2b6ab48939970a9a23
Alexandre TORGUE <alexandre.torgue@st.com>
2016-04-01 11:37:26 +0200

    stmmac: rework the routines to show the ring status
    To avoid lot of check in stmmac_main for display ring management
    and support the GMAC4 chip, the display_ring function is moved
    into dedicated descriptor file.
    
    Signed-off-by: Alexandre TORGUE <alexandre.torgue@st.com>
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/common.h
+++ drivers/net/ethernet/stmicro/stmmac/common.h
@@ -381,6 +381,8 @@ struct stmmac_desc_ops {
 	 u64(*get_timestamp) (void *desc, u32 ats);
 	/* get rx timestamp status */
 	int (*get_rx_timestamp_status) (void *desc, u32 ats);
+	/* Display ring */
+	void (*display_ring)(void *head, unsigned int size, bool rx);
 };
 
 extern const struct stmmac_desc_ops enh_desc_ops;
