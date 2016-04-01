bda2704f858f55fa43b3adaaa6ee6006fd62abcf
Oleg Drokin <green@linuxhacker.ru>
2016-04-01 15:18:01 -0400

    staging/lustre: Fix braces {} style
    This fixes all checkpatch form of this from the Lustre tree:
    CHECK: braces {} should be used on all arms of this statement
    
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lovsub_lock.c
+++ drivers/staging/lustre/lustre/lov/lovsub_lock.c
@@ -78,8 +78,9 @@ int lovsub_lock_init(const struct lu_env *env, struct cl_object *obj,
 		INIT_LIST_HEAD(&lsk->lss_parents);
 		cl_lock_slice_add(lock, &lsk->lss_cl, obj, &lovsub_lock_ops);
 		result = 0;
-	} else
+	} else {
 		result = -ENOMEM;
+	}
 	return result;
 }
 
