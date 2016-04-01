63b8f6cd1d7da6a5f99fba4740f12150f4b0dd82
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-04-01 14:54:21 +0200

    mtd: nand: remove unneeded of_mtd.h inclusions
    Some drivers are including linux/of_mtd.h even if they don't use any of
    the of_get_nand_xxx() helpers.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Acked-by: Harvey Hunt <harvey.hunt@imgtec.com>

--- drivers/mtd/nand/lpc32xx_mlc.c
+++ drivers/mtd/nand/lpc32xx_mlc.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/completion.h>
 #include <linux/interrupt.h>
 #include <linux/of.h>
-#include <linux/of_mtd.h>
 #include <linux/of_gpio.h>
 #include <linux/mtd/lpc32xx_mlc.h>
 #include <linux/io.h>
--- drivers/mtd/nand/vf610_nfc.c
+++ drivers/mtd/nand/vf610_nfc.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/nand.h>
 #include <linux/mtd/partitions.h>
-#include <linux/of_mtd.h>
 #include <linux/of_device.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/platform_device.h>
