a6517a850d0223ba7247d99598df06c62955442c
Sara Sharon <sara.sharon@intel.com>
2016-04-03 15:27:55 +0300

    iwlwifi: mvm: remove RX_PHY support for 9000 device
    In multiple RX queues architecture, the RX_PHY notification
    is no longer useful as it is received in the default queue
    even for packets that are received on RSS queue, and cannot
    be accessed without locking.
    All the needed data is in the new RX packet metadata and
    firmware will no longer send this notification for 9000
    devices. Remove support of it.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/rxmq.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/rxmq.c
@@ -66,19 +66,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "fw-api.h"
 #include "fw-dbg.h"
 
-void iwl_mvm_rx_phy_cmd_mq(struct iwl_mvm *mvm, struct iwl_rx_cmd_buffer *rxb)
-{
-	mvm->ampdu_ref++;
-
-#ifdef CONFIG_IWLWIFI_DEBUGFS
-	if (mvm->last_phy_info.phy_flags & cpu_to_le16(RX_RES_PHY_FLAGS_AGG)) {
-		spin_lock(&mvm->drv_stats_lock);
-		mvm->drv_rx_stats.ampdu_count++;
-		spin_unlock(&mvm->drv_stats_lock);
-	}
-#endif
-}
-
 static inline int iwl_mvm_check_pn(struct iwl_mvm *mvm, struct sk_buff *skb,
 				   int queue, struct ieee80211_sta *sta)
 {
