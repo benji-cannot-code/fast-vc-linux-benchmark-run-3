975765dd8bd633dd468bf8dffc8beafc322bab94
Arnaldo Carvalho de Melo <acme@redhat.com>
2016-04-04 17:52:18 -0300

    perf tools: Allow generating per-arch syscall table arrays
    Tools should use a mechanism similar to arch/x86/entry/syscalls/ to
    generate a header file with the definitions for two variables:
    
      static const char *syscalltbl_x86_64[] = {
    	[0] = "read",
    	[1] = "write",
      <SNIP>
    	[324] = "membarrier",
    	[325] = "mlock2",
    	[326] = "copy_file_range",
      };
      static const int syscalltbl_x86_64_max_id = 326;
    
    In a per arch file that should then be included in
    tools/perf/util/syscalltbl.c.
    
    First one will be for x86_64.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-02uuamkxgccczdth8komspgp@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/syscalltbl.h
+++ tools/perf/util/syscalltbl.h
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct syscalltbl {
 	union {
 		int audit_machine;
+		struct {
+			int nr_entries;
+			void *entries;
+		} syscalls;
 	};
 };
 
