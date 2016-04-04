480c32029658980932aeee8a8c4dca66ad7348c2
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2016-04-04 17:35:11 +0300

    serial: 8250_mid: include missed linux/bitops.h
    The BIT() macro, that is definded in bitops.h, is used in the driver. Include
    necessary header for that.
    
    While here, reorder included headers alphabetically.
    
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_mid.c
+++ drivers/tty/serial/8250/8250_mid.c
@@ -10,9 +10,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
-#include <linux/rational.h>
+#include <linux/bitops.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/rational.h>
 
 #include <linux/dma/hsu.h>
 #include <linux/8250_pci.h>
