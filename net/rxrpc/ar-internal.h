3520b192af51cd35c3fc7fa42968f85e0de092d6
David Howells <dhowells@redhat.com>
2016-04-04 14:00:40 +0100

    rxrpc: Split service connection code out into its own file
    Split the service-specific connection code out into into its own file.  The
    client-specific code has already been split out.  This will leave just the
    common code in the original file.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- net/rxrpc/ar-internal.h
+++ net/rxrpc/ar-internal.h
@@ -564,6 +564,9 @@ extern unsigned int rxrpc_connection_expiry;
 extern struct list_head rxrpc_connections;
 extern rwlock_t rxrpc_connection_lock;
 
+void rxrpc_conn_hash_proto_key(struct rxrpc_conn_proto *);
+void rxrpc_extract_conn_params(struct rxrpc_conn_proto *,
+			       struct rxrpc_local *, struct sk_buff *);
 struct rxrpc_connection *rxrpc_alloc_connection(gfp_t);
 struct rxrpc_connection *rxrpc_find_connection(struct rxrpc_local *,
 					       struct rxrpc_peer *,
@@ -572,9 +575,6 @@ void __rxrpc_disconnect_call(struct rxrpc_call *);
 void rxrpc_disconnect_call(struct rxrpc_call *);
 void rxrpc_put_connection(struct rxrpc_connection *);
 void __exit rxrpc_destroy_all_connections(void);
-struct rxrpc_connection *rxrpc_incoming_connection(struct rxrpc_local *,
-						   struct rxrpc_peer *,
-						   struct sk_buff *);
 
 static inline bool rxrpc_conn_is_client(const struct rxrpc_connection *conn)
 {
@@ -603,6 +603,13 @@ static inline void rxrpc_queue_conn(struct rxrpc_connection *conn)
 	    !rxrpc_queue_work(&conn->processor))
 		rxrpc_put_connection(conn);
 }
+
+/*
+ * conn_service.c
+ */
+struct rxrpc_connection *rxrpc_incoming_connection(struct rxrpc_local *,
+						   struct rxrpc_peer *,
+						   struct sk_buff *);
 
 /*
  * input.c
