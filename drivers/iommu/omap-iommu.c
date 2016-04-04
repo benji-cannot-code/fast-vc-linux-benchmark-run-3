ecac5b126448a69533ddd9ec3ab0af3a11e8a44c
Suman Anna <s-anna@ti.com>
2016-04-04 17:46:20 -0500

    iommu/omap: Use WARN_ON for page table alignment check
    The OMAP IOMMU page table needs to be aligned on a 16K boundary,
    and the current code uses a BUG_ON on the alignment sanity check
    in the .domain_alloc() ops implementation. Replace this with a
    less severe WARN_ON and bail out gracefully.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/omap-iommu.c
+++ drivers/iommu/omap-iommu.c
@@ -1163,7 +1163,8 @@ static struct iommu_domain *omap_iommu_domain_alloc(unsigned type)
 	 * should never fail, but please keep this around to ensure
 	 * we keep the hardware happy
 	 */
-	BUG_ON(!IS_ALIGNED((long)omap_domain->pgtable, IOPGD_TABLE_SIZE));
+	if (WARN_ON(!IS_ALIGNED((long)omap_domain->pgtable, IOPGD_TABLE_SIZE)))
+		goto fail_align;
 
 	clean_dcache_area(omap_domain->pgtable, IOPGD_TABLE_SIZE);
 	spin_lock_init(&omap_domain->lock);
@@ -1174,6 +1175,8 @@ static struct iommu_domain *omap_iommu_domain_alloc(unsigned type)
 
 	return &omap_domain->domain;
 
+fail_align:
+	kfree(omap_domain->pgtable);
 fail_nomem:
 	kfree(omap_domain);
 out:
