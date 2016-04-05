34dc9b991548d7473bef6de4364da353a52ffc67
Krzysztof Kozlowski <k.kozlowski@samsung.com>
2016-04-05 11:05:15 +0900

    regulator: max8997/max77802: Fix misspelled Samsung address
    Correct smasung.com into samsung.com.
    
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Reviewed-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/max8997-regulator.c
+++ drivers/regulator/max8997-regulator.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * max8997.c - Regulator driver for the Maxim 8997/8966
  *
  * Copyright (C) 2011 Samsung Electronics
- * MyungJoo Ham <myungjoo.ham@smasung.com>
+ * MyungJoo Ham <myungjoo.ham@samsung.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
