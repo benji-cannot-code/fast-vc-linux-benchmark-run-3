6a00c64caf52782753b47180537d1e2ff387d964
Sebastien Buisson <sbuisson@ddn.com>
2016-04-04 21:36:46 -0400

    staging: lustre: ldlm: fix 'deadcode' errors
    Fix 'deadcode' issues found by Coverity version 6.5.1:
    Logically dead code (DEADCODE)
    Execution cannot reach this statement.
    
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-3097
    Reviewed-on: http://review.whamcloud.com/7167
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ldlm/ldlm_flock.c
+++ drivers/staging/lustre/lustre/ldlm/ldlm_flock.c
@@ -531,12 +531,6 @@ granted:
 		return -EIO;
 	}
 
-	if (rc) {
-		LDLM_DEBUG(lock, "client-side enqueue waking up: failed (%d)",
-			   rc);
-		return rc;
-	}
-
 	LDLM_DEBUG(lock, "client-side enqueue granted");
 
 	lock_res_and_lock(lock);
