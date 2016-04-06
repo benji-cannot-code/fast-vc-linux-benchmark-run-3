3f24d81b9910a1840f0bec23155369068dddb529
Ido Schimmel <idosch@mellanox.com>
2016-04-06 17:10:14 +0200

    mlxsw: spectrum: Add support for PAUSE frames
    When a packet ingress the switch it's placed in its assigned priority
    group (PG) buffer in the port's headroom buffer while it goes through
    the switch's pipeline. After going through the pipeline - which
    determines its egress port(s) and traffic class - it's moved to the
    switch's shared buffer awaiting transmission.
    
    However, some packets are not eligible to enter the shared buffer due to
    exceeded quotas or insufficient space. Marking their associated PGs as
    lossless will cause the packets to accumulate in the PG buffer. Another
    reason for packets accumulation are complicated pipelines (e.g.
    involving a lot of ACLs).
    
    To prevent packets from being dropped a user can enable PAUSE frames on
    the port. This will mark all the active PGs as lossless and set their
    size according to the maximum delay, as it's not configured by user.
    
                             +----------------+   +
                             |                |   |
                             |                |   |
                             |                |   |
                             |                |   |
                             |                |   |
                             |                |   | Delay
                             |                |   |
                             |                |   |
                             |                |   |
                             |                |   |
                             |                |   |
        Xon/Xoff threshold   +----------------+   +
                             |                |   |
                             |                |   | 2 * MTU
                             |                |   |
                             +----------------+   +
    
    The delay (612 [Cells]) was calculated according to worst-case scenario
    involving maximum MTU and 100m cables.
    
    After marking the PGs as lossless the device is configured to respect
    incoming PAUSE frames (Rx PAUSE) and generate PAUSE frames (Tx PAUSE)
    according to user's settings.
    
    Whenever the port's headroom configuration changes we take into account
    the PAUSE configuration, so that we correctly set the PG's type (lossy /
    lossless), size and threshold. This can happen when:
    
    a) The port's MTU changes, as it directly affects the PG's size.
    
    b) A PG is created following user configuration, by binding a priority
    to it.
    
    Note that the relevant SUPPORTED flags were already mistakenly set by
    the driver before this commit.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.h
@@ -68,6 +68,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MLXSW_SP_BYTES_TO_CELLS(b) DIV_ROUND_UP(b, MLXSW_SP_BYTES_PER_CELL)
 
+/* Maximum delay buffer needed in case of PAUSE frames, in cells.
+ * Assumes 100m cable and maximum MTU.
+ */
+#define MLXSW_SP_PAUSE_DELAY 612
+
 struct mlxsw_sp_port;
 
 struct mlxsw_sp_upper {
@@ -173,6 +178,10 @@ struct mlxsw_sp_port {
 		u16 vid;
 	} vport;
 	struct {
+		u8 tx_pause:1,
+		   rx_pause:1;
+	} link;
+	struct {
 		struct ieee_ets *ets;
 		struct ieee_maxrate *maxrate;
 	} dcb;
@@ -183,6 +192,12 @@ struct mlxsw_sp_port {
 	struct list_head vports_list;
 	struct devlink_port devlink_port;
 };
+
+static inline bool
+mlxsw_sp_port_is_pause_en(const struct mlxsw_sp_port *mlxsw_sp_port)
+{
+	return mlxsw_sp_port->link.tx_pause || mlxsw_sp_port->link.rx_pause;
+}
 
 static inline struct mlxsw_sp_port *
 mlxsw_sp_port_lagged_get(struct mlxsw_sp *mlxsw_sp, u16 lag_id, u8 port_index)
@@ -281,7 +296,7 @@ int mlxsw_sp_port_ets_set(struct mlxsw_sp_port *mlxsw_sp_port,
 int mlxsw_sp_port_prio_tc_set(struct mlxsw_sp_port *mlxsw_sp_port,
 			      u8 switch_prio, u8 tclass);
 int __mlxsw_sp_port_headroom_set(struct mlxsw_sp_port *mlxsw_sp_port, int mtu,
-				 u8 *prio_tc);
+				 u8 *prio_tc, bool pause_en);
 int mlxsw_sp_port_ets_maxrate_set(struct mlxsw_sp_port *mlxsw_sp_port,
 				  enum mlxsw_reg_qeec_hr hr, u8 index,
 				  u8 next_index, u32 maxrate);
