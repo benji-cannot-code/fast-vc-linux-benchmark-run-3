ab26b8f9e55855ea0097561c140e73443b8e38ce
David Howells <dhowells@redhat.com>
2016-04-06 16:13:33 +0100

    KEYS: Add identifier pointers to public_key_signature struct
    Add key identifier pointers to public_key_signature struct so that they can
    be used to retain the identifier of the key to be used to verify the
    signature in both PKCS#7 and X.509.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- crypto/asymmetric_keys/signature.c
+++ crypto/asymmetric_keys/signature.c
@@ -25,7 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 void public_key_signature_free(struct public_key_signature *sig)
 {
+	int i;
+
 	if (sig) {
+		for (i = 0; i < ARRAY_SIZE(sig->auth_ids); i++)
+			kfree(sig->auth_ids[i]);
 		kfree(sig->s);
 		kfree(sig->digest);
 		kfree(sig);
