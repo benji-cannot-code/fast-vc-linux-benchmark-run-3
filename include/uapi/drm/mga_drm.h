50bf428d6b8c4525d60eec3f20c0c9fd8c1e56d5
Emil Velikov <emil.l.velikov@gmail.com>
2016-04-07 19:01:39 +0100

    drm/mga: add extern C guard for the UAPI header
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- include/uapi/drm/mga_drm.h
+++ include/uapi/drm/mga_drm.h
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "drm.h"
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 /* WARNING: If you change any of these defines, make sure to change the
  * defines in the Xserver file (mga_sarea.h)
  */
@@ -416,5 +420,9 @@ typedef struct drm_mga_getparam {
 	int param;
 	void __user *value;
 } drm_mga_getparam_t;
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
