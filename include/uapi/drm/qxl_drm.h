031fe7e218e4a8e6598f2738aff960daf22ac0e3
Emil Velikov <emil.l.velikov@gmail.com>
2016-04-07 19:09:39 +0100

    drm/qxl: add extern C guard for the UAPI header
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Dave Airlie <airlied@redhat.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- include/uapi/drm/qxl_drm.h
+++ include/uapi/drm/qxl_drm.h
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "drm.h"
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 /* Please note that modifications to all structs defined here are
  * subject to backwards-compatibility constraints.
  *
@@ -147,5 +151,9 @@ struct drm_qxl_alloc_surf {
 #define DRM_IOCTL_QXL_ALLOC_SURF \
 	DRM_IOWR(DRM_COMMAND_BASE + DRM_QXL_ALLOC_SURF,\
 		struct drm_qxl_alloc_surf)
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif
