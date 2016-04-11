3b4dc5e1360ee51400af4e1e3e108054c55c3b46
Julien Grall <julien.grall@arm.com>
2016-04-11 16:32:54 +0100

    irqchip/gic-v2: Parse and export virtual GIC information
    For now, the firmware tables are parsed 2 times: once in the GIC
    drivers, the other timer when initializing the vGIC. It means code
    duplication and make more tedious to add the support for another
    firmware table (like ACPI).
    
    Introduce a new structure and set of helpers to get/set the virtual GIC
    information. Also fill up the structure for GICv2.
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- drivers/irqchip/irq-gic-common.h
+++ drivers/irqchip/irq-gic-common.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/of.h>
 #include <linux/irqdomain.h>
+#include <linux/irqchip/arm-gic-common.h>
 
 struct gic_quirk {
 	const char *desc;
@@ -35,5 +36,7 @@ void gic_dist_config(void __iomem *base, int gic_irqs,
 void gic_cpu_config(void __iomem *base, void (*sync_access)(void));
 void gic_enable_quirks(u32 iidr, const struct gic_quirk *quirks,
 		void *data);
+
+void gic_set_kvm_info(const struct gic_kvm_info *info);
 
 #endif /* _IRQ_GIC_COMMON_H */
