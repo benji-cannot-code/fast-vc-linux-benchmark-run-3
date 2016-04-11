9607fe7370c6277d06dc04448164af052dee110f
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2016-04-11 11:57:21 -0700

    eeprom: at24: replace msleep() with usleep_range()
    We cannot expect msleep(1) to actually sleep for a period shorter than
    20 ms. Replace all calls to msleep() with usleep_range().
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/misc/eeprom/at24.c
+++ drivers/misc/eeprom/at24.c
@@ -246,8 +246,7 @@ static ssize_t at24_eeprom_read(struct at24_data *at24, char *buf,
 		if (status == count)
 			return count;
 
-		/* REVISIT: at HZ=100, this is sloooow */
-		msleep(1);
+		usleep_range(1000, 1500);
 	} while (time_before(read_time, timeout));
 
 	return -ETIMEDOUT;
@@ -366,8 +365,7 @@ static ssize_t at24_eeprom_write(struct at24_data *at24, const char *buf,
 		if (status == count)
 			return count;
 
-		/* REVISIT: at HZ=100, this is sloooow */
-		msleep(1);
+		usleep_range(1000, 1500);
 	} while (time_before(write_time, timeout));
 
 	return -ETIMEDOUT;
