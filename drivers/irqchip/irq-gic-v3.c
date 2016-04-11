6ec436c147a098c0b8e5e523f6ef5ad7066c57ce
Julien Grall <julien.grall@arm.com>
2016-04-11 16:32:55 +0100

    irqchip/gic-v3: Prefix all pr_* messages by "GICv3: "
    Currently, most of the pr_* messages in the GICv3 driver don't have a
    prefix.  Add one to make clear where the messages come from.
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define pr_fmt(fmt)	"GICv3: " fmt
+
 #include <linux/acpi.h>
 #include <linux/cpu.h>
 #include <linux/cpu_pm.h>
