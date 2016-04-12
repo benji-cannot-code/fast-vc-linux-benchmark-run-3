f4513666d405f89308b9f8265c85438118e7e6d5
Mitko Haralanov <mitko.haralanov@intel.com>
2016-04-12 10:46:41 -0700

    IB/hfi1: Correctly compute node interval
    The computation of the interval of an interval RB node
    was incorrect leading to data corruption due to the RB
    search algorithm not properly finding the all RB nodes
    in an MMU invalidation interval.
    
    The problem stemmed from the fact that the beginning
    address of the node's range was being aligned to a page
    boundary. For certain buffer sizes, this would lead to
    a end address calculation that was off by 1 page.
    
    An important aspect of keeping the RB same is also
    updating the node's range in the case it's being extended.
    
    Reviewed-by: Dean Luick <dean.luick@intel.com>
    Signed-off-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/staging/rdma/hfi1/user_sdma.c
+++ drivers/staging/rdma/hfi1/user_sdma.c
@@ -1077,7 +1077,6 @@ static int pin_vector_pages(struct user_sdma_request *req,
 			return -ENOMEM;
 
 		node->rb.addr = (unsigned long)iovec->iov.iov_base;
-		node->rb.len = iovec->iov.iov_len;
 		node->pq = pq;
 		atomic_set(&node->refcount, 0);
 		INIT_LIST_HEAD(&node->list);
@@ -1118,6 +1117,7 @@ retry:
 			goto bail;
 		}
 		kfree(node->pages);
+		node->rb.len = iovec->iov.iov_len;
 		node->pages = pages;
 		node->npages += pinned;
 		npages = node->npages;
