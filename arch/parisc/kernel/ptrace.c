9acb1328f76b1465308a4b10d1371e89665fc540
Helge Deller <deller@gmx.de>
2016-04-13 22:44:54 +0200

    parisc: Add syscall tracepoint support
    This patch adds support for the TIF_SYSCALL_TRACEPOINT on the parisc
    architecture. Basically, it calls the appropriate tracepoints on syscall
    entry and exit.
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/kernel/ptrace.c
+++ arch/parisc/kernel/ptrace.c
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* PSW bits we allow the debugger to modify */
 #define USER_PSW_BITS	(PSW_N | PSW_B | PSW_V | PSW_CB)
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/syscalls.h>
+
 /*
  * Called by kernel/ptrace.c when detaching..
  *
@@ -284,6 +287,10 @@ long do_syscall_trace_enter(struct pt_regs *regs)
 		regs->gr[20] = -1UL;
 		goto out;
 	}
+#ifdef CONFIG_HAVE_SYSCALL_TRACEPOINTS
+	if (unlikely(test_thread_flag(TIF_SYSCALL_TRACEPOINT)))
+		trace_sys_enter(regs, regs->gr[20]);
+#endif
 
 #ifdef CONFIG_64BIT
 	if (!is_compat_task())
@@ -311,6 +318,11 @@ void do_syscall_trace_exit(struct pt_regs *regs)
 		test_thread_flag(TIF_BLOCKSTEP);
 
 	audit_syscall_exit(regs);
+
+#ifdef CONFIG_HAVE_SYSCALL_TRACEPOINTS
+	if (unlikely(test_thread_flag(TIF_SYSCALL_TRACEPOINT)))
+		trace_sys_exit(regs, regs->gr[20]);
+#endif
 
 	if (stepping || test_thread_flag(TIF_SYSCALL_TRACE))
 		tracehook_report_syscall_exit(regs, stepping);
