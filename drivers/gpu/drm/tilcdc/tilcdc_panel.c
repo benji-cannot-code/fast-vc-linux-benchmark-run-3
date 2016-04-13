1da878b18feeadf47a5be343fc3e0dac9923183e
Jyri Sarha <jsarha@ti.com>
2016-04-13 18:49:29 +0300

    drm/tilcdc: panel: Add atomic modeset helpers to connector funcs
    Add atomic modeset helpers to panel connector funcs. Property handling
    related helpers, atomic reset helper, and new dpms helper is needed in
    connector for atomic modeseting to work. The default helper functions
    are enough.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- drivers/gpu/drm/tilcdc/tilcdc_panel.c
+++ drivers/gpu/drm/tilcdc/tilcdc_panel.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <video/display_timing.h>
 #include <video/of_display_timing.h>
 #include <video/videomode.h>
+#include <drm/drm_atomic_helper.h>
 
 #include "tilcdc_drv.h"
 
@@ -195,9 +196,12 @@ static struct drm_encoder *panel_connector_best_encoder(
 
 static const struct drm_connector_funcs panel_connector_funcs = {
 	.destroy            = panel_connector_destroy,
-	.dpms               = drm_helper_connector_dpms,
+	.dpms               = drm_atomic_helper_connector_dpms,
 	.detect             = panel_connector_detect,
 	.fill_modes         = drm_helper_probe_single_connector_modes,
+	.reset              = drm_atomic_helper_connector_reset,
+	.atomic_duplicate_state = drm_atomic_helper_connector_duplicate_state,
+	.atomic_destroy_state = drm_atomic_helper_connector_destroy_state,
 };
 
 static const struct drm_connector_helper_funcs panel_connector_helper_funcs = {
