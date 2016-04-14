a46d90619fde4edc3f54c10afefec11891ef795e
Jes Sorensen <Jes.Sorensen@redhat.com>
2016-04-14 14:58:44 -0400

    rtl8xxxu: Implement generic init_queue_reserved_page() function
    Longer term we should switch all the chips over to use this function
    instead of the random chip specific ifdef hacks.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_regs.h
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_regs.h
@@ -388,7 +388,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define REG_RQPN			0x0200
 #define  RQPN_HI_PQ_SHIFT		0
 #define  RQPN_LO_PQ_SHIFT		8
-#define  RQPN_NORM_PQ_SHIFT		16
+#define  RQPN_PUB_PQ_SHIFT		16
 #define  RQPN_LOAD			BIT(31)
 
 #define REG_FIFOPAGE			0x0204
