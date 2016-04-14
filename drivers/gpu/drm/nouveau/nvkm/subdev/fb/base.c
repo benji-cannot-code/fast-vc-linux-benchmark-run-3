cffab1da77dbd7b6b896163be5fc895a2353bcf7
Ben Skeggs <bskeggs@redhat.com>
2016-04-14 10:38:55 +1000

    drm/nouveau/fb: allow chipset-specific actions for oneinit()
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/fb/base.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/fb/base.c
@@ -99,6 +99,7 @@ static int
 nvkm_fb_oneinit(struct nvkm_subdev *subdev)
 {
 	struct nvkm_fb *fb = nvkm_fb(subdev);
+
 	if (fb->func->ram_new) {
 		int ret = fb->func->ram_new(fb, &fb->ram);
 		if (ret) {
@@ -106,6 +107,13 @@ nvkm_fb_oneinit(struct nvkm_subdev *subdev)
 			return ret;
 		}
 	}
+
+	if (fb->func->oneinit) {
+		int ret = fb->func->oneinit(fb);
+		if (ret)
+			return ret;
+	}
+
 	return 0;
 }
 
