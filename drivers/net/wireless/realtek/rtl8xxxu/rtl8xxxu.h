0d5361d6754ce88b14b75b37a652c954d4aa977e
Jes Sorensen <Jes.Sorensen@redhat.com>
2016-04-14 14:58:52 -0400

    rtl8xxxu: Name RX descriptor types rxdesc16/rxdesc24
    This caught a bug where too little memory was allocated for RX urbs
    for parts using 24 byte RX descriptors
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu.h
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu.h
@@ -102,7 +102,7 @@ enum rtl8xxxu_rx_type {
 	RX_TYPE_ERROR = -1
 };
 
-struct rtl8xxxu_rx_desc {
+struct rtl8xxxu_rxdesc16 {
 #ifdef __LITTLE_ENDIAN
 	u32 pktlen:14;
 	u32 crc32:1;
@@ -238,7 +238,7 @@ struct rtl8xxxu_rx_desc {
 #endif
 };
 
-struct rtl8723bu_rx_desc {
+struct rtl8xxxu_rxdesc24 {
 #ifdef __LITTLE_ENDIAN
 	u32 pktlen:14;
 	u32 crc32:1;
@@ -1304,6 +1304,7 @@ struct rtl8xxxu_fileops {
 	u16 mbox_ext_reg;
 	char mbox_ext_width;
 	char tx_desc_size;
+	char rx_desc_size;
 	char has_s0s1;
 	u32 adda_1t_init;
 	u32 adda_1t_path_on;
