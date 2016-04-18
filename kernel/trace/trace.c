8722ceac29407a7a64497b72851378c6d27dbbc7
Wang Xiaoqiang <wangxq10@lzu.edu.cn>
2016-04-18 15:23:29 +0800

    tracing: Don't use the address of the buffer array name in copy_from_user
    With the following code snippet:
    
        ...
        char buf[64];
        ...
        if (copy_from_user(&buf, ubuf, cnt))
        ...
    
    Even though the value of "&buf" equals "buf", but there is no need
    to get the address of the "buf" again. Use "buf" instead of "&buf".
    
    Link: http://lkml.kernel.org/r/20160418152329.18b72bea@debian
    
    Signed-off-by: Wang Xiaoqiang <wangxq10@lzu.edu.cn>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -3665,7 +3665,7 @@ tracing_trace_options_write(struct file *filp, const char __user *ubuf,
 	if (cnt >= sizeof(buf))
 		return -EINVAL;
 
-	if (copy_from_user(&buf, ubuf, cnt))
+	if (copy_from_user(buf, ubuf, cnt))
 		return -EFAULT;
 
 	buf[cnt] = 0;
@@ -4538,7 +4538,7 @@ tracing_set_trace_write(struct file *filp, const char __user *ubuf,
 	if (cnt > MAX_TRACER_SIZE)
 		cnt = MAX_TRACER_SIZE;
 
-	if (copy_from_user(&buf, ubuf, cnt))
+	if (copy_from_user(buf, ubuf, cnt))
 		return -EFAULT;
 
 	buf[cnt] = 0;
@@ -5328,7 +5328,7 @@ static ssize_t tracing_clock_write(struct file *filp, const char __user *ubuf,
 	if (cnt >= sizeof(buf))
 		return -EINVAL;
 
-	if (copy_from_user(&buf, ubuf, cnt))
+	if (copy_from_user(buf, ubuf, cnt))
 		return -EFAULT;
 
 	buf[cnt] = 0;
