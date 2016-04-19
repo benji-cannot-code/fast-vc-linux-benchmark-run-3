b827a382a0aba301fe93f3584f43f1989fe50755
Ben Skeggs <bskeggs@redhat.com>
2016-04-19 10:07:04 +1000

    drm/nouveau/bios/pll: prevent oops when limits table can't be parsed
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/bios/pll.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/bios/pll.c
@@ -157,7 +157,7 @@ pll_map_reg(struct nvkm_bios *bios, u32 reg, u32 *type, u8 *ver, u8 *len)
 	}
 
 	map = pll_map(bios);
-	while (map->reg) {
+	while (map && map->reg) {
 		if (map->reg == reg && *ver >= 0x20) {
 			u16 addr = (data += hdr);
 			*type = map->type;
@@ -199,7 +199,7 @@ pll_map_type(struct nvkm_bios *bios, u8 type, u32 *reg, u8 *ver, u8 *len)
 	}
 
 	map = pll_map(bios);
-	while (map->reg) {
+	while (map && map->reg) {
 		if (map->type == type && *ver >= 0x20) {
 			u16 addr = (data += hdr);
 			*reg = map->reg;
