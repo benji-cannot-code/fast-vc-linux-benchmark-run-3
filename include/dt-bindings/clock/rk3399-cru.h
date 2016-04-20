0c3718afece816811871aae67466910af7547d03
Xing Zheng <zhengxing@rock-chips.com>
2016-04-20 19:06:49 +0800

    clk: rockchip: rename rga clock-id on rk3399
    The rga clock supplying the working clock on the rk3399 is actually
    called rga-core in the manual. As the clock id has neither been
    assigned nor released with a full kernel release, we can still change
    the id to the more appropriate naming.
    
    Signed-off-by: Xing Zheng <zhengxing@rock-chips.com>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- include/dt-bindings/clock/rk3399-cru.h
+++ include/dt-bindings/clock/rk3399-cru.h
@@ -73,7 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SCLK_MACREF_OUT			106
 #define SCLK_VOP0_PWM			107
 #define SCLK_VOP1_PWM			108
-#define SCLK_RGA			109
+#define SCLK_RGA_CORE			109
 #define SCLK_ISP0			110
 #define SCLK_ISP1			111
 #define SCLK_HDMI_CEC			112
