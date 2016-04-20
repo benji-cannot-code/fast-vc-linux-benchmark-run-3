5d0616ceb8baae50a89989c903a77844ce70d7a1
H Hartley Sweeten <hsweeten@visionengravers.com>
2016-04-20 10:36:42 -0700

    staging: comedi: mite: remove BUG_ON() in MITE_IODWBSR_1_WSIZE_bits()
    Prior to calling this function pci_ioremap_bar() is called. If the
    pci_resource_len(), i.e. the 'size', was 0 the ioremap would fail
    so this function would never be called. So the first BUG_ON() can
    never occur.
    
    The 'order' returned by ilog2() will always be > 0 so the second
    BUG_ON() will also never occur.
    
    Remove the unnecessary BUG_ON() checks and tidy up the function.
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/mite.c
+++ drivers/staging/comedi/drivers/mite.c
@@ -158,12 +158,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static unsigned int MITE_IODWBSR_1_WSIZE_bits(unsigned int size)
 {
-	unsigned int order = 0;
-
-	BUG_ON(size == 0);
-	order = ilog2(size);
-	BUG_ON(order < 1);
-	return (order - 1) & 0x1f;
+	return (ilog2(size) - 1) & 0x1f;
 }
 
 static unsigned int mite_retry_limit(unsigned int retry_limit)
