d4b3c899d68f511b808c03556e76063d86e8122c
Viresh Kumar <viresh.kumar@linaro.org>
2016-04-21 08:11:57 +0530

    greybus: audio-codec: Staticize few routines to fix build warnings
    This fixes below warnings ..
    
    greybus/audio_codec.c:20:32: warning: symbol 'find_data' was not declared. Should it be static?
    greybus/audio_codec.c:955:6: warning: symbol 'gbaudio_codec_cleanup' was not declared. Should it be static?
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Vaibhav Agarwal <vaibhav.agarwal@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/audio_codec.c
+++ drivers/staging/greybus/audio_codec.c
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct gbaudio_codec_info *gbcodec;
 
-struct gbaudio_data_connection *find_data(struct gbaudio_module_info *module,
-					  const char *name)
+static struct gbaudio_data_connection *
+find_data(struct gbaudio_module_info *module, const char *name)
 {
 	struct gbaudio_data_connection *data;
 
@@ -953,7 +953,7 @@ int gbaudio_register_module(struct gbaudio_module_info *module)
 }
 EXPORT_SYMBOL(gbaudio_register_module);
 
-void gbaudio_codec_cleanup(struct gbaudio_module_info *module)
+static void gbaudio_codec_cleanup(struct gbaudio_module_info *module)
 {
 	struct gbaudio_data_connection *data;
 	int pb_state = gbcodec->stream[0].state;
