44ac3d1e36420c9437c443cf90a9af71742f28f2
Michal Kazior <michal.kazior@tieto.com>
2016-04-22 14:15:59 +0200

    codel: split into multiple files
    It was impossible to include codel.h for the
    purpose of having access to codel_params or
    codel_vars structure definitions and using them
    for embedding in other more complex structures.
    
    This splits allows codel.h itself to be treated
    like any other header file while codel_qdisc.h and
    codel_impl.h contain function definitions with
    logic that was previously in codel.h.
    
    This copies over copyrights and doesn't involve
    code changes other than adding a few additional
    include directives to net/sched/sch*codel.c.
    
    Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_codel.c
+++ net/sched/sch_codel.c
@@ -50,6 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/prefetch.h>
 #include <net/pkt_sched.h>
 #include <net/codel.h>
+#include <net/codel_impl.h>
+#include <net/codel_qdisc.h>
 
 
 #define DEFAULT_CODEL_LIMIT 1000
--- net/sched/sch_fq_codel.c
+++ net/sched/sch_fq_codel.c
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/netlink.h>
 #include <net/pkt_sched.h>
 #include <net/codel.h>
+#include <net/codel_impl.h>
+#include <net/codel_qdisc.h>
 
 /*	Fair Queue CoDel.
  *
