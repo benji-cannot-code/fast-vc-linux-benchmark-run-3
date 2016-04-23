536463da5aec0276d6c9450239581a9c6b9a97d0
Johan Hovold <johan@hovoldconsulting.com>
2016-04-23 18:47:25 +0200

    greybus: module: implement controlled module removal
    Implement controlled module removal through a new module attribute
    "eject".
    
    When a non-zero argument is written to the attribute, all interfaces of
    the module are disabled (e.g. bundles are deregistered) and deactivated
    (e.g. powered off) before instructing the SVC to physically eject the
    module.
    
    Note that the module device is not deregistered until the SVC has
    reported the physical removal of all of its interfaces.
    
    A new interface mutex is added to enforce interface state-change
    serialisation.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/interface.h
+++ drivers/staging/greybus/interface.h
@@ -40,7 +40,10 @@ struct gb_interface {
 
 	unsigned long quirks;
 
+	struct mutex mutex;
+
 	bool disconnected;
+	bool ejected;
 	bool enabled;
 };
 #define to_gb_interface(d) container_of(d, struct gb_interface, dev)
