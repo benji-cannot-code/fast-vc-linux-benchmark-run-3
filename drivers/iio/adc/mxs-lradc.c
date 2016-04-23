a8f6d0ae5799e628876767f30eb7449ac6a2fe22
Stefan Wahren <stefan.wahren@i2se.com>
2016-04-23 19:57:58 +0000

    iio: mxs-lradc: remove mxs_lradc_ts_unregister
    After using devm_input_allocate_device for registration
    the function mxs_lradc_ts_unregister isn't necessary anymore
    since mxs_lradc_ts_close already does the job.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Suggested-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/adc/mxs-lradc.c
+++ drivers/iio/adc/mxs-lradc.c
@@ -1149,14 +1149,6 @@ static int mxs_lradc_ts_register(struct mxs_lradc *lradc)
 	return input_register_device(input);
 }
 
-static void mxs_lradc_ts_unregister(struct mxs_lradc *lradc)
-{
-	if (!lradc->use_touchscreen)
-		return;
-
-	mxs_lradc_disable_ts(lradc);
-}
-
 /*
  * IRQ Handling
  */
@@ -1716,13 +1708,11 @@ static int mxs_lradc_probe(struct platform_device *pdev)
 	ret = iio_device_register(iio);
 	if (ret) {
 		dev_err(dev, "Failed to register IIO device\n");
-		goto err_ts;
+		return ret;
 	}
 
 	return 0;
 
-err_ts:
-	mxs_lradc_ts_unregister(lradc);
 err_ts_register:
 	mxs_lradc_hw_stop(lradc);
 err_dev:
@@ -1740,7 +1730,6 @@ static int mxs_lradc_remove(struct platform_device *pdev)
 	struct mxs_lradc *lradc = iio_priv(iio);
 
 	iio_device_unregister(iio);
-	mxs_lradc_ts_unregister(lradc);
 	mxs_lradc_hw_stop(lradc);
 	mxs_lradc_trigger_remove(iio);
 	iio_triggered_buffer_cleanup(iio);
