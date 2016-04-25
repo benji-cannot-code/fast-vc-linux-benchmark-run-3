e2a7fa42f9c224e20c8fbe7725b9eb91f526ea7e
Knud Poulsen <knpo@ieee.org>
2016-04-25 17:34:47 +0200

    watchdog: f71808e_wdt: Fix typo
    Spelling/ typo
    
    Signed-off-by: Knud Poulsen <knpo@ieee.org>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/f71808e_wdt.c
+++ drivers/watchdog/f71808e_wdt.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define SIO_F71808FG_LD_WDT	0x07	/* Watchdog timer logical device */
 #define SIO_UNLOCK_KEY		0x87	/* Key to enable Super-I/O */
-#define SIO_LOCK_KEY		0xAA	/* Key to diasble Super-I/O */
+#define SIO_LOCK_KEY		0xAA	/* Key to disable Super-I/O */
 
 #define SIO_REG_LDSEL		0x07	/* Logical device select */
 #define SIO_REG_DEVID		0x20	/* Device ID (2 bytes) */
