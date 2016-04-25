f21bee3118f68703680221f18e57b74bfc7a12e9
Vladimir Murzin <vladimir.murzin@arm.com>
2016-04-25 09:47:47 +0100

    serial: mps2-uart: add MPS2 UART driver
    This driver adds support to the UART controller found on ARM MPS2
    platform.
    
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Signed-off-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -265,4 +265,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* MVEBU UART */
 #define PORT_MVEBU	114
 
+/* MPS2 UART */
+#define PORT_MPS2UART	115
+
 #endif /* _UAPILINUX_SERIAL_CORE_H */
