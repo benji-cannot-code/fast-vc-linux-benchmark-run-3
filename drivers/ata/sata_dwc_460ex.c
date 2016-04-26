ede24784d9bc53d80a2770046be76e974d2f13b7
Christian Lamparter <chunkeey@googlemail.com>
2016-04-26 12:03:03 +0300

    ata: sata_dwc_460ex: fix crash on offline links without an attached drive
    This patch fixes Machine Check "Data Write PLB Error" which happens
    when libata-sff's ata_sff_dev_select is trying to write into the
    device_addr in order to select a drive. However, SATA has no master
    or slave devices like the old ATA Bus, therefore selecting a
    different drive is kind of pointless.
    
    Data Write PLB Error
    Oops: Machine check, sig: 7 [#1]
    PowerPC 44x Platform
    Modules linked in:
    CPU: 0 PID: 508 Comm: scsi_eh_0 Not tainted 4.6.0-rc3-next-20160412+ #10
    [...]
    NIP [c027e820] ata_sff_dev_select+0x3c/0x44
    LR [c027e810] ata_sff_dev_select+0x2c/0x44
    Call Trace:
    [cec31cd0] [c027da00] ata_sff_postreset+0x40/0xb4 (unreliable)
    [cec31ce0] [c027a03c] ata_eh_reset+0x5cc/0x928
    [cec31d60] [c027a840] ata_eh_recover+0x330/0x10bc
    [cec31df0] [c027bae0] ata_do_eh+0x4c/0xa4
    [...]
    
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/sata_dwc_460ex.c
+++ drivers/ata/sata_dwc_460ex.c
@@ -1135,6 +1135,11 @@ static int sata_dwc_hardreset(struct ata_link *link, unsigned int *class,
 	return ret;
 }
 
+static void sata_dwc_dev_select(struct ata_port *ap, unsigned int device)
+{
+	/* SATA DWC is master only */
+}
+
 /*
  * scsi mid-layer and libata interface structures
  */
@@ -1164,6 +1169,8 @@ static struct ata_port_operations sata_dwc_ops = {
 
 	.port_start		= sata_dwc_port_start,
 	.port_stop		= sata_dwc_port_stop,
+
+	.sff_dev_select		= sata_dwc_dev_select,
 
 	.bmdma_setup		= sata_dwc_bmdma_setup,
 	.bmdma_start		= sata_dwc_bmdma_start,
