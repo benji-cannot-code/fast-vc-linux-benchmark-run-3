816d32cdf77a22c70625653bc6de0f55a153c9f0
Felipe Balbi <felipe.balbi@linux.intel.com>
2016-04-26 10:49:07 +0300

    usb: dwc3: gadget: update DCFG.NumP to max burst size
    NumP field of DCFG register is used on NumP field of
    ACK TP header and it tells the host how many packets
    an endpoint can receive before waiting for
    synchronization.
    
    Documentation says it should be set to anything
    <=bMaxBurst. Interestingly, however, this setting is
    not per-endpoint how it should be (different
    endpoints could have different burst sizes), but
    things seem to work okay right now.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -272,6 +272,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DWC3_DCFG_LOWSPEED	(2 << 0)
 #define DWC3_DCFG_FULLSPEED1	(3 << 0)
 
+#define DWC3_DCFG_NUMP_SHIFT	17
+#define DWC3_DCFG_NUMP(n)	(((n) & 0x1f) >> DWC3_DCFG_NUMP_SHIFT)
+#define DWC3_DCFG_NUMP_MASK	(0x1f << DWC3_DCFG_NUMP_SHIFT)
 #define DWC3_DCFG_LPM_CAP	(1 << 22)
 
 /* Device Control Register */
