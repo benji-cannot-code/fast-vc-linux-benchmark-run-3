668018525a80481e9f376f0b65d3f89de0627b9c
Marco Angaroni <marcoangaroni@gmail.com>
2016-04-26 21:20:22 +0200

    ipvs: make drop_entry protection effective for SIP-pe
    DoS protection policy that deletes connections to avoid out of memory is
    currently not effective for SIP-pe plus OPS-mode for two reasons:
      1) connection templates (holding SIP call-id) are always skipped in
         ip_vs_random_dropentry()
      2) in_pkts counter (used by drop_entry algorithm) is not incremented
         for connection templates
    
    This patch addresses such problems with the following changes:
      a) connection templates associated (via their dest) to virtual-services
         configured in OPS mode are included in ip_vs_random_dropentry()
         monitoring. This applies to SIP-pe over UDP (which requires OPS mode),
         but is more general principle: when OPS is controlled by templates
         memory can be used only by templates themselves, since OPS conns are
         deleted after packet is forwarded.
      b) OPS connections, if controlled by a template, cause increment of
         in_pkts counter of their template. This is already happening but only
         in case director is in master-slave mode (see ip_vs_sync_conn()).
    
    Signed-off-by: Marco Angaroni <marcoangaroni@gmail.com>
    Acked-by: Julian Anastasov <ja@ssi.bg>
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- net/netfilter/ipvs/ip_vs_conn.c
+++ net/netfilter/ipvs/ip_vs_conn.c
@@ -1262,6 +1262,16 @@ static inline int todrop_entry(struct ip_vs_conn *cp)
 	return 1;
 }
 
+static inline bool ip_vs_conn_ops_mode(struct ip_vs_conn *cp)
+{
+	struct ip_vs_service *svc;
+
+	if (!cp->dest)
+		return false;
+	svc = rcu_dereference(cp->dest->svc);
+	return svc && (svc->flags & IP_VS_SVC_F_ONEPACKET);
+}
+
 /* Called from keventd and must protect itself from softirqs */
 void ip_vs_random_dropentry(struct netns_ipvs *ipvs)
 {
@@ -1276,11 +1286,16 @@ void ip_vs_random_dropentry(struct netns_ipvs *ipvs)
 		unsigned int hash = prandom_u32() & ip_vs_conn_tab_mask;
 
 		hlist_for_each_entry_rcu(cp, &ip_vs_conn_tab[hash], c_list) {
-			if (cp->flags & IP_VS_CONN_F_TEMPLATE)
-				/* connection template */
-				continue;
 			if (cp->ipvs != ipvs)
 				continue;
+			if (cp->flags & IP_VS_CONN_F_TEMPLATE) {
+				if (atomic_read(&cp->n_control) ||
+				    !ip_vs_conn_ops_mode(cp))
+					continue;
+				else
+					/* connection template of OPS */
+					goto try_drop;
+			}
 			if (cp->protocol == IPPROTO_TCP) {
 				switch(cp->state) {
 				case IP_VS_TCP_S_SYN_RECV:
@@ -1308,6 +1323,7 @@ void ip_vs_random_dropentry(struct netns_ipvs *ipvs)
 					continue;
 				}
 			} else {
+try_drop:
 				if (!todrop_entry(cp))
 					continue;
 			}
