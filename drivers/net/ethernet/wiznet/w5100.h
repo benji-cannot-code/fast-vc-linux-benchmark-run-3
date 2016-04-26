6087c8e2fb84411b986f2194a94799a3e58ca1e5
Akinobu Mita <akinobu.mita@gmail.com>
2016-04-27 05:43:48 +0900

    net: w5100: support W5500
    This adds support for W5500 chip.
    
    W5500 has similar register and memory organization with W5100 and W5200.
    There are a few important differences listed below but it is still
    possible to share common code with W5100 and W5200.
    
    * W5500 register and memory are organized by multiple blocks.  Each one
    is selected by 16bits offset address and 5bits block select bits.
    
    But the existing register access operations take u16 address.  This change
    extends the addess by u32 address and put offset address to lower 16bits
    and block select bits to upper 16bits.
    
    This change also adds the offset addresses for socket register and TX/RX
    memory blocks to the driver private data structure in order to reduce
    conditional switches for each chip.
    
    * W5500 has the different register offset for socket interrupt mask
    register.  Newly added internal functions w5100_enable_intr() and
    w5100_disable_intr() take care of the diffrence.
    
    * W5500 has the different register offset for retry time-value register.
    But this register is only used to verify that the reset value is correctly
    read at initialization.  So move the verification to w5100_hw_reset()
    which already does different things for different chips.
    
    Signed-off-by: Akinobu Mita <akinobu.mita@gmail.com>
    Cc: Mike Sinkovsky <msink@permonline.ru>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/wiznet/w5100.h
+++ drivers/net/ethernet/wiznet/w5100.h
@@ -11,17 +11,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum {
 	W5100,
 	W5200,
+	W5500,
 };
 
 struct w5100_ops {
 	bool may_sleep;
 	int chip_id;
-	int (*read)(struct net_device *ndev, u16 addr);
-	int (*write)(struct net_device *ndev, u16 addr, u8 data);
-	int (*read16)(struct net_device *ndev, u16 addr);
-	int (*write16)(struct net_device *ndev, u16 addr, u16 data);
-	int (*readbulk)(struct net_device *ndev, u16 addr, u8 *buf, int len);
-	int (*writebulk)(struct net_device *ndev, u16 addr, const u8 *buf,
+	int (*read)(struct net_device *ndev, u32 addr);
+	int (*write)(struct net_device *ndev, u32 addr, u8 data);
+	int (*read16)(struct net_device *ndev, u32 addr);
+	int (*write16)(struct net_device *ndev, u32 addr, u16 data);
+	int (*readbulk)(struct net_device *ndev, u32 addr, u8 *buf, int len);
+	int (*writebulk)(struct net_device *ndev, u32 addr, const u8 *buf,
 			 int len);
 	int (*reset)(struct net_device *ndev);
 	int (*init)(struct net_device *ndev);
