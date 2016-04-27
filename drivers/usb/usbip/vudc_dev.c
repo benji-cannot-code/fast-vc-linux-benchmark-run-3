6694c3dbca13166948fc825442ea32387dcf3acf
Krzysztof Opasiak <k.opasiak@samsung.com>
2016-04-27 20:02:07 +0200

    usb: usbip: vudc: Rename find_endpoint() to vudc_find_endpoint()
    As find_endpoint() is a global funcion rename it to vudc_find_endpoint()
    to clearly mark where does it come from.
    
    Signed-off-by: Krzysztof Opasiak <k.opasiak@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/usbip/vudc_dev.c
+++ drivers/usb/usbip/vudc_dev.c
@@ -116,7 +116,7 @@ static void stop_activity(struct vudc *udc)
 	}
 }
 
-struct vep *find_endpoint(struct vudc *udc, u8 address)
+struct vep *vudc_find_endpoint(struct vudc *udc, u8 address)
 {
 	int i;
 
