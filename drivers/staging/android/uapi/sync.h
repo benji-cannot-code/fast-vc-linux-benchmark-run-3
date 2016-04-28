1c0c648e7bfaa5786bbf47e08b3cd3d5da139dd3
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2016-04-28 10:46:48 -0300

    staging/android: remove redundant comments on sync_merge_data
    struct sync_merge_data already have documentation on top of the
    struct definition. No need to duplicate it.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/uapi/sync.h
+++ drivers/staging/android/uapi/sync.h
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @pad:	padding for 64-bit alignment, should always be zero
  */
 struct sync_merge_data {
-	char	name[32]; /* name of new fence */
-	__s32	fd2; /* fd of second fence */
-	__s32	fence; /* fd on newly created fence */
+	char	name[32];
+	__s32	fd2;
+	__s32	fence;
 	__u32	flags;
 	__u32	pad;
 };
@@ -34,8 +34,8 @@ struct sync_merge_data {
 /**
  * struct sync_fence_info - detailed fence information
  * @obj_name:		name of parent sync_timeline
- * @driver_name:	name of driver implementing the parent
- * @status:		status of the fence 0:active 1:signaled <0:error
+* @driver_name:	name of driver implementing the parent
+* @status:		status of the fence 0:active 1:signaled <0:error
  * @flags:		fence_info flags
  * @timestamp_ns:	timestamp of status change in nanoseconds
  */
