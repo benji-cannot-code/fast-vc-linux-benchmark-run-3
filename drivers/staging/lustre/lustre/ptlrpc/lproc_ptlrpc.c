2a283b715637169d5ef0212099a447384c116760
wang di <di.wang@intel.com>
2016-04-28 12:07:34 -0400

    staging: lustre: fld: add local fldb to each target
    Add local FLDB to each MDT, so OSD/OUT can check whether
    FID is remote by looking up local FLDB, i.e. no need send RPC
    to MDT0. This is just the client part of the work.
    
    Signed-off-by: wang di <di.wang@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-4076
    Reviewed-on: http://review.whamcloud.com/7884
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ptlrpc/lproc_ptlrpc.c
+++ drivers/staging/lustre/lustre/ptlrpc/lproc_ptlrpc.c
@@ -132,6 +132,7 @@ static struct ll_rpc_opcode {
 	{ SEC_CTX_INIT_CONT, "sec_ctx_init_cont" },
 	{ SEC_CTX_FINI,     "sec_ctx_fini" },
 	{ FLD_QUERY,	"fld_query" },
+	{ FLD_READ,	"fld_read" },
 };
 
 static struct ll_eopcode {
