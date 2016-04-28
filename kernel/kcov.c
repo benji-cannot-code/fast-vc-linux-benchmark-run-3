0d9271b746cc29f85e6043a30b0bc34a65d61ab9
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-04-28 16:18:55 -0700

    kcov: don't profile branches in kcov
    Profiling 'if' statements in __sanitizer_cov_trace_pc() leads to
    unbound recursion and crash:
    
    	__sanitizer_cov_trace_pc() ->
    		ftrace_likely_update ->
    			__sanitizer_cov_trace_pc() ...
    
    Define DISABLE_BRANCH_PROFILING to disable this tracer.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/kcov.c
+++ kernel/kcov.c
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) "kcov: " fmt
 
+#define DISABLE_BRANCH_PROFILING
 #include <linux/compiler.h>
 #include <linux/types.h>
 #include <linux/file.h>
