f7da82721b63eb049cfe903f9d5b14fd11fd45fe
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2016-04-29 23:25:26 +1000

    powerpc/mm: Drop PTE_ATOMIC_UPDATES from pmd_hugepage_update()
    pmd_hugepage_update() is inside #ifdef CONFIG_TRANSPARENT_HUGEPAGE. THP
    can only be enabled if PPC_BOOK3S_64=y && PPC_64K_PAGES=y, aka. hash64.
    
    On hash64 we always define PTE_ATOMIC_UPDATES to 1, meaning the #ifdef
    in pmd_hugepage_update() is unnecessary, so drop it.
    
    That is also the only use of PTE_ATOMIC_UPDATES in any of the hash code,
    meaning we no longer need to #define it at all in the hash headers.
    
    Note it's still #defined and used in the nohash code.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/book3s/64/hash.h
+++ arch/powerpc/include/asm/book3s/64/hash.h
@@ -128,8 +128,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PTEIDX_SECONDARY	0x8
 #define _PTEIDX_GROUP_IX	0x7
 
-/* Hash table based platforms need atomic updates of the linux PTE */
-#define PTE_ATOMIC_UPDATES	1
 #define _PTE_NONE_MASK	_PAGE_HPTEFLAGS
 /*
  * The mask convered by the RPN must be a ULL on 32-bit platforms with
