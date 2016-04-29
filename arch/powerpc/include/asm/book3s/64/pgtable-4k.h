a0aba2e1ec81540f8f3e977dec77d07b4f829e65
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2016-04-29 23:25:55 +1000

    powerpc/mm: Move hugetlb and THP related pmd accessors to pgtable.h
    Here we create pgtable-64/4k.h and move pmd accessors that are common
    between hash and radix there. We can't do much sharing with 4K Linux
    page size because 4K Linux page size with hash config doesn't support
    THP. So for now it is empty. In later patches we will add functions that
    does conditional hash/radix accessors there.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

---
+++ arch/powerpc/include/asm/book3s/64/pgtable-4k.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _ASM_POWERPC_BOOK3S_64_PGTABLE_4K_H
+#define _ASM_POWERPC_BOOK3S_64_PGTABLE_4K_H
+/*
+ * hash 4k can't share hugetlb and also doesn't support THP
+ */
+
+#endif /*_ASM_POWERPC_BOOK3S_64_PGTABLE_4K_H */
