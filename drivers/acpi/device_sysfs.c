7b10b5a43e42be207a820ab39f2649ce7f19717f
Betty Dall <betty.dall@hpe.com>
2016-04-30 10:03:38 -0600

    ACPI / device_sysfs: Change _SUN and _STA show functions error return to EIO
    The error return from a sysfs show function is passed up through
    the call chain and visible as the return from the read system call.
    The show functions for the _STA and _SUN object currently return
    -ENODEV. This patch changes the return to -EIO. ENODEV makes less
    sense since the "device' exists or there wouldn't be a sysfs file.
    
    Signed-off-by: Betty Dall <betty.dall@hpe.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/device_sysfs.c
+++ drivers/acpi/device_sysfs.c
@@ -468,7 +468,7 @@ acpi_device_sun_show(struct device *dev, struct device_attribute *attr,
 
 	status = acpi_evaluate_integer(acpi_dev->handle, "_SUN", NULL, &sun);
 	if (ACPI_FAILURE(status))
-		return -ENODEV;
+		return -EIO;
 
 	return sprintf(buf, "%llu\n", sun);
 }
@@ -497,7 +497,7 @@ static ssize_t status_show(struct device *dev, struct device_attribute *attr,
 
 	status = acpi_evaluate_integer(acpi_dev->handle, "_STA", NULL, &sta);
 	if (ACPI_FAILURE(status))
-		return -ENODEV;
+		return -EIO;
 
 	return sprintf(buf, "%llu\n", sta);
 }
