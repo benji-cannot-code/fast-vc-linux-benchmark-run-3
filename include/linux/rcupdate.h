9fb4a56eabb65f53c9cca5f04d0d17d2841d0b07
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-05-02 11:58:56 -0700

    rcu: Make call_rcu_tasks() tolerate first call with irqs disabled
    Currently, if the very first call to call_rcu_tasks() has irqs disabled,
    it will create the rcu_tasks_kthread with irqs disabled, which will
    result in a splat in the memory allocator, which kthread_run() invokes
    with the expectation that irqs are enabled.
    
    This commit fixes this problem by deferring kthread creation if called
    with irqs disabled.  The first call to call_rcu_tasks() that has irqs
    enabled will create the kthread.
    
    This bug was detected by rcutorture changes that were motivated by
    Iftekhar Ahmed's mutation-testing efforts.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bug.h>
 #include <linux/compiler.h>
 #include <linux/ktime.h>
+#include <linux/irqflags.h>
 
 #include <asm/barrier.h>
 
