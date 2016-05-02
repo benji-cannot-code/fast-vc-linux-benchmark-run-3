5644ae6fd0bed198d4086621562375cc28d04ccc
Geert Uytterhoeven <geert@linux-m68k.org>
2016-05-02 10:32:24 +0200

    m68k/mvme16x: Include generic <linux/rtc.h>
    The MVME16x RTC driver doesn't use any MC146818-specific definitions,
    hence include the generic <linux/rtc.h> instead of
    <linux/mc146818rtc.h>.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- arch/m68k/mvme16x/rtc.c
+++ arch/m68k/mvme16x/rtc.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fcntl.h>
 #include <linux/init.h>
 #include <linux/poll.h>
-#include <linux/mc146818rtc.h>	/* For struct rtc_time and ioctls, etc */
+#include <linux/rtc.h>	/* For struct rtc_time and ioctls, etc */
 #include <linux/bcd.h>
 #include <asm/mvme16xhw.h>
 
