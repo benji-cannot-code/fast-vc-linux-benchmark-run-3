41032b8191dfcae4e19f0f56e895b31196ce6195
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-05-02 22:08:23 +0300

    drm/i915: Respect DP++ adaptor TMDS clock limit
    Try to detect the max TMDS clock limit for the DP++ adaptor (if any)
    and take it into account when checking the port clock.
    
    Note that as with the sink (HDMI vs. DVI) TMDS clock limit we'll ignore
    the adaptor TMDS clock limit in the modeset path, in case users are
    already "overclocking" their TMDS links. One subtle change here is that
    we'll have to respect the adaptor TMDS clock limit when we decide whether
    to do 12bpc or 8bpc, otherwise we might end up picking 12bpc and
    accidentally driving the TMDS link out of spec even when the user chose
    a mode that fits wihting the limits at 8bpc. This means you can't
    "overclock" your DP++ dongle at 12bpc anymore, but you can continue to
    do so at 8bpc.
    
    Note that for simplicity we'll use the I2C access method for all dual
    mode adaptors including type 2. Otherwise we'd have to start mixing
    DP AUX and HDMI together. In the future we may need to do that if we
    come across any board designs that don't hook up the DDC pins to the
    DP++ connectors. Such boards would obviously only work with type 2
    dual mode adaptors, and not type 1.
    
    v2: Store adaptor type under indel_hdmi->dp_dual_mode
        Deal with DRM_DP_DUAL_MODE_UNKNOWN
        Pass adaptor type to drm_dp_dual_mode_max_tmds_clock(),
        and use it for type1 adaptors as well
    
    Cc: stable@vger.kernel.org
    Reported-by: Tore Anderson <tore@fud.no>
    Fixes: 7a0baa623446 ("Revert "drm/i915: Disable 12bpc hdmi for now"")
    Cc: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Cc: Shashank Sharma <shashank.sharma@intel.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1462216105-20881-3-git-send-email-ville.syrjala@linux.intel.com
    Reviewed-by: Shashank Sharma <shashank.sharma@intel.com>
    (cherry picked from commit b1ba124d8e95cca48d33502a4a76b1ed09d213ce)
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_crtc.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_fb_helper.h>
+#include <drm/drm_dp_dual_mode_helper.h>
 #include <drm/drm_dp_mst_helper.h>
 #include <drm/drm_rect.h>
 #include <drm/drm_atomic.h>
@@ -754,6 +755,10 @@ struct cxsr_latency {
 struct intel_hdmi {
 	i915_reg_t hdmi_reg;
 	int ddc_bus;
+	struct {
+		enum drm_dp_dual_mode_type type;
+		int max_tmds_clock;
+	} dp_dual_mode;
 	bool limited_color_range;
 	bool color_range_auto;
 	bool has_hdmi_sink;
