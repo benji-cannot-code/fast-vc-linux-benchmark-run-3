e01c6ab2363ec1c74b7823874dfa15d599ffe2f9
Lukas Wunner <lukas@wunner.de>
2016-05-03 21:44:40 +0200

    mwifiex: Drop unnecessary include pcieport_if.h
    This header file is only needed for drivers binding to a PCI bridge
    device allocated by drivers/pci/pcie/portdrv_core.c. The mwifiex driver
    doesn't do that nor use any symbols defined in pcieport_if.h.
    
    Cc: Amitkumar Karwar <akarwar@marvell.com>
    Cc: Nishant Sarmukadam <nishants@marvell.com>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/mwifiex/pcie.h
+++ drivers/net/wireless/marvell/mwifiex/pcie.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	_MWIFIEX_PCIE_H
 
 #include    <linux/pci.h>
-#include    <linux/pcieport_if.h>
 #include    <linux/interrupt.h>
 
 #include    "decl.h"
