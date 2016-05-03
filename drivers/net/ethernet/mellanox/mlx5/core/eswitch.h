6c2cf6bf6af809a6a391b238b6828c93c17e7050
Mohamad Haj Yahia <mohamad@mellanox.com>
2016-05-03 17:14:03 +0300

    net/mlx5: E-Switch, Implement promiscuous rx modes vf request handling
    Add promisc_change as a trigger to vport context change event.
    Add set vport promisc/allmulti functions to add vport to promiscuous
    flowtable rules.
    Upon promisc/allmulti rx mode vf request add the vport to
    the relevant promiscuous group (Allmulti/Promisc group) so the relevant
    traffic will be forwarded to it.
    Upon allmulti vf request add the vport to each existing multicast fdb
    rule.
    Upon adding/removing mcast address from a vport, update all other
    allmulti vports.
    
    Signed-off-by: Mohamad Haj Yahia <mohamad@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
@@ -112,6 +112,8 @@ struct mlx5_vport {
 	int                     vport;
 	struct hlist_head       uc_list[MLX5_L2_ADDR_HASH_SIZE];
 	struct hlist_head       mc_list[MLX5_L2_ADDR_HASH_SIZE];
+	struct mlx5_flow_rule   *promisc_rule;
+	struct mlx5_flow_rule   *allmulti_rule;
 	struct work_struct      vport_change_handler;
 
 	struct vport_ingress    ingress;
@@ -120,6 +122,7 @@ struct mlx5_vport {
 	u16                     vlan;
 	u8                      qos;
 	bool                    spoofchk;
+	bool                    trusted;
 	bool                    enabled;
 	u16                     enabled_events;
 };
@@ -150,6 +153,7 @@ struct mlx5_eswitch {
 	 * and async SRIOV admin state changes
 	 */
 	struct mutex            state_lock;
+	struct esw_mc_addr      *mc_promisc;
 };
 
 /* E-Switch API */
