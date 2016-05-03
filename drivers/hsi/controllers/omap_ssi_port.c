73ad8f966940098a92974ae70ae6bcead4971fee
Arnd Bergmann <arnd@arndb.de>
2016-05-03 17:16:21 +0200

    HSI: omap-ssi: include pinctrl header files
    The driver now uses some pinctrl functions, but fails
    to build if PINCTRL is disabled because the respective
    header files are only included indirectly:
    
    drivers/hsi/controllers/omap_ssi_core.c: In function 'ssi_clk_event':
    drivers/hsi/controllers/omap_ssi_core.c:317:4: error: implicit declaration of function 'pinctrl_pm_select_idle_state' [-Werror=implicit-function-declaration]
    drivers/hsi/controllers/omap_ssi_core.c:339:4: error: implicit declaration of function 'pinctrl_pm_select_default_state' [-Werror=implicit-function-declaration]
    drivers/hsi/controllers/omap_ssi_port.c: In function 'ssi_flush':
    drivers/hsi/controllers/omap_ssi_port.c:520:2: error: implicit declaration of function 'pinctrl_pm_select_idle_state' [-Werror=implicit-function-declaration]
    
    This includes the headers from the files that call the functions,
    which works even if pinctrl is turned off.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: 4bcf7414528a ("HSI: omap-ssi: add clk change support")
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- drivers/hsi/controllers/omap_ssi_port.c
+++ drivers/hsi/controllers/omap_ssi_port.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 
 #include <linux/gpio/consumer.h>
+#include <linux/pinctrl/consumer.h>
 #include <linux/debugfs.h>
 
 #include "omap_ssi_regs.h"
