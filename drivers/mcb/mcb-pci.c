29bfd9e721b9d90b7bcf8b1f682f1bca2b9b616e
Andreas Werner <andreas.werner@men.de>
2016-05-03 12:42:02 +0200

    mcb: Delete num_cells variable which is not required
    The num_cells variable is only used in the dev_dbg print,
    but we can directly use the ret variable which also includes the same
    value.
    
    Signed-off-by: Andreas Werner <andreas.werner@men.de>
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mcb/mcb-pci.c
+++ drivers/mcb/mcb-pci.c
@@ -36,7 +36,6 @@ static int mcb_pci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	struct resource *res;
 	struct priv *priv;
 	int ret;
-	int num_cells;
 	unsigned long flags;
 
 	priv = devm_kzalloc(&pdev->dev, sizeof(struct priv), GFP_KERNEL);
@@ -93,9 +92,8 @@ static int mcb_pci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	ret = chameleon_parse_cells(priv->bus, priv->mapbase, priv->base);
 	if (ret < 0)
 		goto out_mcb_bus;
-	num_cells = ret;
 
-	dev_dbg(&pdev->dev, "Found %d cells\n", num_cells);
+	dev_dbg(&pdev->dev, "Found %d cells\n", ret);
 
 	mcb_bus_add_devices(priv->bus);
 
