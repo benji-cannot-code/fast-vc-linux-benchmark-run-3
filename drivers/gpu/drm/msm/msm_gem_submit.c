015e300c6825365288547251ec854b1755b02647
Rob Clark <robdclark@gmail.com>
2016-05-03 10:10:15 -0400

    drm/msm: print offender task name on hangcheck recovery
    Track the pid per submit, so we can print the name of the task which
    submitted the batch that caused the gpu to hang.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/msm_gem_submit.c
+++ drivers/gpu/drm/msm/msm_gem_submit.c
@@ -46,6 +46,7 @@ static struct msm_gem_submit *submit_create(struct drm_device *dev,
 
 	submit->dev = dev;
 	submit->gpu = gpu;
+	submit->pid = get_pid(task_pid(current));
 
 	/* initially, until copy_from_user() and bo lookup succeeds: */
 	submit->nr_bos = 0;
@@ -61,6 +62,7 @@ void msm_gem_submit_free(struct msm_gem_submit *submit)
 {
 	fence_put(submit->fence);
 	list_del(&submit->node);
+	put_pid(submit->pid);
 	kfree(submit);
 }
 
