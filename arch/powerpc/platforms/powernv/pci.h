10d42126b853033abadf4588c242e8abe8647883
Gavin Shan <gwshan@linux.vnet.ibm.com>
2016-05-03 15:41:34 +1000

    powerpc/powernv: Remove DMA32 PE list
    PEs are put into PHB DMA32 list (phb->ioda.pe_dma_list) according
    to their DMA32 weight. The PEs on the list are iterated to setup
    their TCE32 tables at system booting time. The list is used for
    once at boot time and no need to keep it.
    
    This moves the logic calculating DMA32 weight of PHB and PE to
    pnv_ioda_setup_dma() to drop PHB's DMA32 list. Also, every PE
    traces the consumed DMA32 segment by @tce32_seg and @tce32_segcount
    are useless and they're removed.
    
    Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Reviewed-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/pci.h
+++ arch/powerpc/platforms/powernv/pci.h
@@ -54,14 +54,7 @@ struct pnv_ioda_pe {
 	/* PE number */
 	unsigned int		pe_number;
 
-	/* "Weight" assigned to the PE for the sake of DMA resource
-	 * allocations
-	 */
-	unsigned int		dma_weight;
-
 	/* "Base" iommu table, ie, 4K TCEs, 32-bit DMA */
-	int			tce32_seg;
-	int			tce32_segcount;
 	struct iommu_table_group table_group;
 
 	/* 64-bit TCE bypass region */
@@ -79,7 +72,6 @@ struct pnv_ioda_pe {
 	struct list_head	slaves;
 
 	/* Link in list of PE#s */
-	struct list_head	dma_link;
 	struct list_head	list;
 };
 
@@ -169,17 +161,6 @@ struct pnv_phb {
 
 		/* 32-bit TCE tables allocation */
 		unsigned long		tce32_count;
-
-		/* Total "weight" for the sake of DMA resources
-		 * allocation
-		 */
-		unsigned int		dma_weight;
-		unsigned int		dma_pe_count;
-
-		/* Sorted list of used PE's, sorted at
-		 * boot for resource allocation purposes
-		 */
-		struct list_head	pe_dma_list;
 
 		/* TCE cache invalidate registers (physical and
 		 * remapped)
