164ecc89d1196ba1e5eb4c7cc81d825530051bd3
Vaibhav Agarwal <vaibhav.agarwal@linaro.org>
2016-05-04 16:29:21 +0530

    greybus: audio: Remove redundant lock protection & is_connected field
    Each module maintains connected status & a lock to protect it.
    Using codec->lock we can safely serialize ASoC specific callbacks
    (in response to mixer_ctl update or dai_ops) and gb module
    disconnect. Thus is_connected field can be removed.
    
    Signed-off-by: Vaibhav Agarwal <vaibhav.agarwal@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/audio_module.c
+++ drivers/staging/greybus/audio_module.c
@@ -27,12 +27,10 @@ static int gbaudio_request_jack(struct gbaudio_module_info *module,
 	dev_warn(module->dev, "Jack Event received: type: %u, event: %u\n",
 		 req->jack_attribute, req->event);
 
-	mutex_lock(&module->lock);
 	if (req->event == GB_AUDIO_JACK_EVENT_REMOVAL) {
 		module->jack_type = 0;
 		button_status = module->button_status;
 		module->button_status = 0;
-		mutex_unlock(&module->lock);
 		if (button_status)
 			snd_soc_jack_report(&module->button_jack, 0,
 					    GBCODEC_JACK_BUTTON_MASK);
@@ -49,7 +47,6 @@ static int gbaudio_request_jack(struct gbaudio_module_info *module,
 			 module->jack_type, report);
 
 	module->jack_type = report;
-	mutex_unlock(&module->lock);
 	snd_soc_jack_report(&module->headset_jack, report, GBCODEC_JACK_MASK);
 
 	return 0;
@@ -64,10 +61,8 @@ static int gbaudio_request_button(struct gbaudio_module_info *module,
 		 req->button_id, req->event);
 
 	/* currently supports 4 buttons only */
-	mutex_lock(&module->lock);
 	if (!module->jack_type) {
 		dev_err(module->dev, "Jack not present. Bogus event!!\n");
-		mutex_unlock(&module->lock);
 		return -EINVAL;
 	}
 
@@ -100,8 +95,6 @@ static int gbaudio_request_button(struct gbaudio_module_info *module,
 		report = report & ~soc_button_id;
 
 	module->button_status = report;
-
-	mutex_unlock(&module->lock);
 
 	snd_soc_jack_report(&module->button_jack, report,
 			    GBCODEC_JACK_BUTTON_MASK);
@@ -248,7 +241,6 @@ static int gb_audio_probe(struct gb_bundle *bundle,
 		return -ENOMEM;
 
 	gbmodule->num_data_connections = bundle->num_cports - 1;
-	mutex_init(&gbmodule->lock);
 	INIT_LIST_HEAD(&gbmodule->data_list);
 	INIT_LIST_HEAD(&gbmodule->widget_list);
 	INIT_LIST_HEAD(&gbmodule->ctl_list);
@@ -328,7 +320,6 @@ static int gb_audio_probe(struct gb_bundle *bundle,
 		if (ret)
 			goto disable_data_connection;
 	}
-	gbmodule->is_connected = 1;
 
 	/* inform above layer for uevent */
 	dev_dbg(dev, "Inform set_event:%d to above layer\n", 1);
