342743b4b68ea7e6193891f17b4da9eee68ad205
Chuck Lever <chuck.lever@oracle.com>
2016-05-04 10:41:48 -0400

    xprtrdma: Remove qplock
    Clean up.
    
    After "xprtrdma: Remove ro_unmap() from all registration modes",
    there are no longer any sites that take rpcrdma_ia::qplock for read.
    The one site that takes it for write is always single-threaded. It
    is safe to remove it.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprtrdma/xprt_rdma.h
+++ net/sunrpc/xprtrdma/xprt_rdma.h
@@ -66,7 +66,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct rpcrdma_ia {
 	const struct rpcrdma_memreg_ops	*ri_ops;
-	rwlock_t		ri_qplock;
 	struct ib_device	*ri_device;
 	struct rdma_cm_id 	*ri_id;
 	struct ib_pd		*ri_pd;
