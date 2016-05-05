3498ce6ec9ac68e989e42e6d6779930df9bcb0c1
Christoph Hellwig <hch@lst.de>
2016-05-05 16:41:05 +0200

    irqchip/irq-alpine-msi: Don't use <asm-generic/msi.h>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- drivers/irqchip/irq-alpine-msi.c
+++ drivers/irqchip/irq-alpine-msi.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 
 #include <asm/irq.h>
-#include <asm-generic/msi.h>
+#include <asm/msi.h>
 
 /* MSIX message address format: local GIC target */
 #define ALPINE_MSIX_SPI_TARGET_CLUSTER0		BIT(16)
