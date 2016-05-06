2eb8f98d2f5c3fe9cea097526b88b3e0547bb5f9
Mark Brown <broonie@kernel.org>
2016-05-06 18:13:17 +0100

    ASoC: da7129: Add missing include of acpi.h
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/da7219.c
+++ sound/soc/codecs/da7219.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * option) any later version.
  */
 
+#include <linux/acpi.h>
 #include <linux/clk.h>
 #include <linux/i2c.h>
 #include <linux/of_device.h>
