52d082089a2cdca60dd9c4aec00799654f1ad459
Tom O'Rourke <Tom.O'Rourke@intel.com>
2016-05-06 11:42:52 +0100

    drm/i915/guc: Use major_minor version for filename
    Load guc firmware from file with major_minor number
    in filename instead of using symolic link with only
    major number.
    
    This change is so that new firmwares can only be used
    with a kernel change. This in case there is a regression
    with a new firmware, it won't be used by default without
    some testing.
    
    Issue: VIZ-7713
    Signed-off-by: Tom O'Rourke <Tom.O'Rourke@intel.com>
    Signed-off-by: Nick Hoath <nicholas.hoath@intel.com>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: peter.antoine@intel.com
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>

--- drivers/gpu/drm/i915/intel_guc_loader.c
+++ drivers/gpu/drm/i915/intel_guc_loader.c
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#define I915_SKL_GUC_UCODE "i915/skl_guc_ver6.bin"
+#define I915_SKL_GUC_UCODE "i915/skl_guc_ver6_1.bin"
 MODULE_FIRMWARE(I915_SKL_GUC_UCODE);
 
 /* User-friendly representation of an enum */
