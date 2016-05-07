53dca837325b8c4592610743bfaf189bd1f15b26
Heiko Carstens <heiko.carstens@de.ibm.com>
2016-05-07 12:15:34 +0200

    s390: add missing include statements
    arch_mmap_rnd, cpu_have_feature, and arch_randomize_brk are all
    defined as globally visible variables.
    However the files they are defined in do not include the header files
    with the declaration. To avoid a possible mismatch add the missing
    include statements so we have proper type checking in place.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/process.c
+++ arch/s390/kernel/process.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *		 Denis Joseph Barrow,
  */
 
+#include <linux/elf-randomize.h>
 #include <linux/compiler.h>
 #include <linux/cpu.h>
 #include <linux/sched.h>
