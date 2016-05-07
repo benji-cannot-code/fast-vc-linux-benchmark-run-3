63c086272cc20b46be4595b7edabd8d7ff8966c4
Archit Taneja <architt@codeaurora.org>
2016-05-07 23:11:25 +0530

    drm/msm/mdp5: Add MDSS top level driver
    SoCs that contain MDP5 have a top level wrapper called MDSS that manages
    clocks, power and irq for the sub-blocks within it.
    
    Currently, the MDSS portions are stuffed into the MDP5 driver. This makes
    it hard to represent the DT bindings in the correct way. We create a top
    level MDSS helper that handles these parts. This is essentially moving out
    some of the mdp5_kms irq code and MDSS register space and keeping it as a
    separate entity. We haven't given any clocks to the top level MDSS yet,
    but a AHB clock would be added in the future to access registers.
    
    One thing to note is that the resources allocated by this helper are
    tied to the top level platform_device (the one that allocates the
    drm_device struct too). This device would be the parent to MDSS
    sub-blocks like MDP5, DSI, eDP etc.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct msm_kms;
 struct msm_gpu;
 struct msm_mmu;
+struct msm_mdss;
 struct msm_rd_state;
 struct msm_perf_state;
 struct msm_gem_submit;
@@ -82,6 +83,9 @@ struct msm_drm_private {
 
 	/* subordinate devices, if present: */
 	struct platform_device *gpu_pdev;
+
+	/* top level MDSS wrapper device (for MDP5 only) */
+	struct msm_mdss *mdss;
 
 	/* possibly this should be in the kms component, but it is
 	 * shared by both mdp4 and mdp5..
