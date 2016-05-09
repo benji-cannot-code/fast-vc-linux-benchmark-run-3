b63e4a2799e3888a4baec8ebe0320e59ee354c3e
Viresh Kumar <viresh.kumar@linaro.org>
2016-05-09 18:15:08 +0530

    greybus: pwm: Create separate module
    Create separate module for pwm gpbridge driver.
    
    Tested on EVT 1.5 by inserting GP test module, all the devices were
    enumerated correctly.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/pwm.c
+++ drivers/staging/greybus/pwm.c
@@ -257,6 +257,7 @@ static const struct gpbridge_device_id gb_pwm_id_table[] = {
 	{ GPBRIDGE_PROTOCOL(GREYBUS_PROTOCOL_PWM) },
 	{ },
 };
+MODULE_DEVICE_TABLE(gpbridge, gb_pwm_id_table);
 
 static struct gpbridge_driver pwm_driver = {
 	.name		= "pwm",
@@ -264,4 +265,6 @@ static struct gpbridge_driver pwm_driver = {
 	.remove		= gb_pwm_remove,
 	.id_table	= gb_pwm_id_table,
 };
-gb_gpbridge_builtin_driver(pwm_driver);
+
+module_gpbridge_driver(pwm_driver);
+MODULE_LICENSE("GPL v2");
