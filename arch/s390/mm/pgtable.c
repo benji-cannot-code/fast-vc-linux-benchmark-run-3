3e7fb0536eeb1d9d449d8426e7fa2b21d9b8fd5f
David Hildenbrand <dahi@linux.vnet.ibm.com>
2016-05-10 13:51:54 +0200

    s390/mm: don't drop errors in get_guest_storage_key
    Commit 1e133ab296f3 ("s390/mm: split arch/s390/mm/pgtable.c") changed
    the return value of get_guest_storage_key to an unsigned char, resulting
    in -EFAULT getting interpreted as a valid storage key.
    
    Cc: stable@vger.kernel.org # 4.6+
    Reviewed-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: David Hildenbrand <dahi@linux.vnet.ibm.com>
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>

--- arch/s390/mm/pgtable.c
+++ arch/s390/mm/pgtable.c
@@ -544,7 +544,7 @@ int set_guest_storage_key(struct mm_struct *mm, unsigned long addr,
 }
 EXPORT_SYMBOL(set_guest_storage_key);
 
-unsigned char get_guest_storage_key(struct mm_struct *mm, unsigned long addr)
+unsigned long get_guest_storage_key(struct mm_struct *mm, unsigned long addr)
 {
 	unsigned char key;
 	spinlock_t *ptl;
