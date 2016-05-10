9f7f54181de842c95686a0f6557c97d14b54d1ee
Andrew Lunn <andrew@lunn.ch>
2016-05-10 23:27:23 +0200

    dsa: Remove master_dev from switch structure
    The switch drivers only use the master_dev member for dev_info()
    messages.  Now that the device is passed to the old style probe, and
    new style drivers are probed as true linux drivers, this is no longer
    needed.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -121,6 +121,8 @@ struct dsa_switch_tree {
 };
 
 struct dsa_switch {
+	struct device *dev;
+
 	/*
 	 * Parent switch tree, and switch index.
 	 */
@@ -142,11 +144,6 @@ struct dsa_switch {
 	 * The used switch driver.
 	 */
 	struct dsa_switch_driver	*drv;
-
-	/*
-	 * Reference to host device to use.
-	 */
-	struct device		*master_dev;
 
 #ifdef CONFIG_NET_DSA_HWMON
 	/*
