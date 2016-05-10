22df2277e92c177ed5b8d61a6fd450435bbda4c6
John Keeping <john@metanate.com>
2016-05-10 17:03:55 +0100

    drm/rockchip: fb: add missing header
    This fixes the following sparse warnings:
    
    drivers/gpu/drm/rockchip/rockchip_drm_fb.c:32:23: warning: symbol 'rockchip_fb_get_gem_obj' was not declared. Should it be static?
    drivers/gpu/drm/rockchip/rockchip_drm_fb.c:315:24: warning: symbol 'rockchip_drm_framebuffer_init' was not declared. Should it be static?
    drivers/gpu/drm/rockchip/rockchip_drm_fb.c:329:6: warning: symbol 'rockchip_drm_mode_config_init' was not declared. Should it be static?
    
    Signed-off-by: John Keeping <john@metanate.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_fb.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_fb.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_crtc_helper.h>
 
 #include "rockchip_drm_drv.h"
+#include "rockchip_drm_fb.h"
 #include "rockchip_drm_gem.h"
 
 #define to_rockchip_fb(x) container_of(x, struct rockchip_drm_fb, fb)
