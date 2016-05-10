e08b2a6ebba8288229f76081683d1a5241f64895
Adam Thomson <Adam.Thomson.Opensource@diasemi.com>
2016-05-10 16:11:06 +0100

    ASoC: da7213: Default PC counter to free-running when DAI disabled
    Currently PC counter is always synchronised to DAI which means that
    when the DAI is disabled, features such as ALC calibration cannot
    be executed successfully. This patch makes sure that when the DAI
    is disabled, PC counter is set to free-running.
    
    Signed-off-by: Adam Thomson <Adam.Thomson.Opensource@diasemi.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/da7213.h
+++ sound/soc/codecs/da7213.h
@@ -414,6 +414,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DA7213_DMIC_CLK_RATE_SHIFT				2
 #define DA7213_DMIC_CLK_RATE_MASK				(0x1 << 2)
 
+/* DA7213_PC_COUNT = 0x94 */
+#define DA7213_PC_FREERUN_MASK					(0x1 << 0)
+
 /* DA7213_DIG_CTRL = 0x99 */
 #define DA7213_DAC_L_INV_SHIFT					3
 #define DA7213_DAC_R_INV_SHIFT					7
