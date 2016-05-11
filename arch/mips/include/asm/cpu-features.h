6542b58014769d982e637cdfd9e8e1a4560ea7f3
James Hogan <james.hogan@imgtec.com>
2016-05-11 13:50:52 +0100

    MIPS: Add defs & probing of [X]ContextConfig
    The CP0_[X]ContextConfig registers are present if CP0_Config3.CTXTC or
    CP0_Config3.SM are set, and provide more control over which bits of
    CP0_[X]Context are set to the faulting virtual address on a TLB
    exception.
    
    KVM/VZ will need to be able to save and restore these registers in the
    guest context, so add the relevant definitions and probing of the
    ContextConfig feature in the root context first.
    
    [ralf@linux-mips.org: resolve merge conflict.]
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/13225/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -451,4 +451,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # define cpu_has_badinstrp	(cpu_data[0].options & MIPS_CPU_BADINSTRP)
 #endif
 
+#ifndef cpu_has_contextconfig
+# define cpu_has_contextconfig	(cpu_data[0].options & MIPS_CPU_CTXTC)
+#endif
+
 #endif /* __ASM_CPU_FEATURES_H */
