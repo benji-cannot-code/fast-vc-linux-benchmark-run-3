f9d9aded87caf4144fc810e40983d975464bd108
Stephen Boyd <sboyd@codeaurora.org>
2016-05-11 18:02:28 -0700

    tty: serial: msm: Cleanup include usage
    The hrtimer include isn't used and neither is serial. Drop those
    ones. The irq.h header really should be interrupt.h because this
    is an interrupt user and not an interrupt chip. Finally add
    wait.h for the wake_up*() usage in this driver and kernel.h for
    container_of().
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Acked-by: Andy Gross <andy.gross@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/msm_serial.c
+++ drivers/tty/serial/msm_serial.c
@@ -20,26 +20,26 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # define SUPPORT_SYSRQ
 #endif
 
+#include <linux/kernel.h>
 #include <linux/atomic.h>
 #include <linux/dma-mapping.h>
 #include <linux/dmaengine.h>
-#include <linux/hrtimer.h>
 #include <linux/module.h>
 #include <linux/io.h>
 #include <linux/ioport.h>
-#include <linux/irq.h>
+#include <linux/interrupt.h>
 #include <linux/init.h>
 #include <linux/console.h>
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
 #include <linux/serial_core.h>
-#include <linux/serial.h>
 #include <linux/slab.h>
 #include <linux/clk.h>
 #include <linux/platform_device.h>
 #include <linux/delay.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/wait.h>
 
 #define UART_MR1			0x0000
 
