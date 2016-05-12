2f25ee50efb078613bdf2cbd5e3dbc6199e511f1
Jubin John <jubin.john@intel.com>
2016-05-12 10:23:16 -0700

    IB/hfi1: Remove unnecessary header
    While running perftests, there is a significant utilization of the
    random number daemon. This is due to the linux/random.h header being
    included in qp.c and verbs.c. However, none of the functions from this
    header are being used in these files, so remove the unnecessary header.
    
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Jubin John <jubin.john@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/staging/rdma/hfi1/qp.c
+++ drivers/staging/rdma/hfi1/qp.c
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 #include <linux/hash.h>
 #include <linux/module.h>
-#include <linux/random.h>
 #include <linux/seq_file.h>
 #include <rdma/rdma_vt.h>
 #include <rdma/rdmavt_qp.h>
--- drivers/staging/rdma/hfi1/verbs.c
+++ drivers/staging/rdma/hfi1/verbs.c
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/utsname.h>
 #include <linux/rculist.h>
 #include <linux/mm.h>
-#include <linux/random.h>
 #include <linux/vmalloc.h>
 
 #include "hfi.h"
