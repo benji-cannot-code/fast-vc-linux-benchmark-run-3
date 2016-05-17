ffe2d843b9d8deb9edcfe1b9d762433d7167f5b7
Liviu Dudau <Liviu.Dudau@arm.com>
2016-05-17 10:06:54 +0100

    drm: hdlcd: Revamp runtime power management
    Because the HDLCD driver acts as a component master it can end
    up enabling the runtime PM functionality before the encoders
    are initialised. This can cause crashes if the component slave
    never probes (missing module) or if the PM operations kick in
    before the probe finishes.
    
    Move the enabling of the runtime PM after the component master
    has finished collecting the slave components and use the DRM
    atomic helpers to suspend and resume the device.
    
    Tested-by: Robin Murphy <Robin.Murphy@arm.com>
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>

--- drivers/gpu/drm/arm/hdlcd_crtc.c
+++ drivers/gpu/drm/arm/hdlcd_crtc.c
@@ -34,8 +34,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+static void hdlcd_crtc_cleanup(struct drm_crtc *crtc)
+{
+	struct hdlcd_drm_private *hdlcd = crtc_to_hdlcd_priv(crtc);
+
+	/* stop the controller on cleanup */
+	hdlcd_write(hdlcd, HDLCD_REG_COMMAND, 0);
+	drm_crtc_cleanup(crtc);
+}
+
 static const struct drm_crtc_funcs hdlcd_crtc_funcs = {
-	.destroy = drm_crtc_cleanup,
+	.destroy = hdlcd_crtc_cleanup,
 	.set_config = drm_atomic_helper_set_config,
 	.page_flip = drm_atomic_helper_page_flip,
 	.reset = drm_atomic_helper_crtc_reset,
@@ -156,8 +165,8 @@ static void hdlcd_crtc_disable(struct drm_crtc *crtc)
 	if (!crtc->primary->fb)
 		return;
 
-	clk_disable_unprepare(hdlcd->clk);
 	hdlcd_write(hdlcd, HDLCD_REG_COMMAND, 0);
+	clk_disable_unprepare(hdlcd->clk);
 	drm_crtc_vblank_off(crtc);
 }
 
@@ -293,16 +302,6 @@ static struct drm_plane *hdlcd_plane_init(struct drm_device *drm)
 	hdlcd->plane = plane;
 
 	return plane;
-}
-
-void hdlcd_crtc_suspend(struct drm_crtc *crtc)
-{
-	hdlcd_crtc_disable(crtc);
-}
-
-void hdlcd_crtc_resume(struct drm_crtc *crtc)
-{
-	hdlcd_crtc_enable(crtc);
 }
 
 int hdlcd_setup_crtc(struct drm_device *drm)
