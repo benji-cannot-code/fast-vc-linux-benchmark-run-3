a9be14ba605b8a6315549bbcffe359025a8b6ca1
Heiko Carstens <heiko.carstens@de.ibm.com>
2016-05-17 12:17:51 +0200

    s390/vmem: make use of pte_clear()
    Use pte_clear() instead of open-coding it.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Acked-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/mm/vmem.c
+++ arch/s390/mm/vmem.c
@@ -149,9 +149,7 @@ static void vmem_remove_range(unsigned long start, unsigned long size)
 	pud_t *pu_dir;
 	pmd_t *pm_dir;
 	pte_t *pt_dir;
-	pte_t  pte;
 
-	pte_val(pte) = _PAGE_INVALID;
 	while (address < end) {
 		pg_dir = pgd_offset_k(address);
 		if (pgd_none(*pg_dir)) {
@@ -179,7 +177,7 @@ static void vmem_remove_range(unsigned long start, unsigned long size)
 			continue;
 		}
 		pt_dir = pte_offset_kernel(pm_dir, address);
-		*pt_dir = pte;
+		pte_clear(&init_mm, address, pt_dir);
 		address += PAGE_SIZE;
 	}
 	flush_tlb_kernel_range(start, end);
