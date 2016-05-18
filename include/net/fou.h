5f348c3a1d380fead5807d18509817baee0c2697
Tom Herbert <tom@herbertland.com>
2016-05-18 09:06:15 -0700

    fou: Split out {fou,gue}_build_header
    Create __fou_build_header and __gue_build_header. These implement the
    protocol generic parts of building the fou and gue header.
    fou_build_header and gue_build_header implement the IPv4 specific
    functions and call the __*_build_header functions.
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/fou.h
+++ include/net/fou.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 size_t fou_encap_hlen(struct ip_tunnel_encap *e);
 static size_t gue_encap_hlen(struct ip_tunnel_encap *e);
 
-int fou_build_header(struct sk_buff *skb, struct ip_tunnel_encap *e,
-		     u8 *protocol, struct flowi4 *fl4);
-int gue_build_header(struct sk_buff *skb, struct ip_tunnel_encap *e,
-		     u8 *protocol, struct flowi4 *fl4);
+int __fou_build_header(struct sk_buff *skb, struct ip_tunnel_encap *e,
+		       u8 *protocol, __be16 *sport, int type);
+int __gue_build_header(struct sk_buff *skb, struct ip_tunnel_encap *e,
+		       u8 *protocol, __be16 *sport, int type);
 
 #endif
