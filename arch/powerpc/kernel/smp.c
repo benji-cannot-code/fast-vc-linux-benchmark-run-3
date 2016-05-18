1983bd94cc1961a592c4e7d21118641c1ea5cd1a
Daniel Axtens <dja@axtens.net>
2016-05-18 11:16:51 +1000

    powerpc/sparse: Include headers containing prototypes
    Sometimes headers that provide prototypes for functions are
    accidentally omitted from the files that define the functions.
    
    Fix a couple of times that occurs.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/smp.c
+++ arch/powerpc/kernel/smp.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/notifier.h>
 #include <linux/topology.h>
+#include <linux/profile.h>
 
 #include <asm/ptrace.h>
 #include <linux/atomic.h>
