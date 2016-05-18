5b232c1e9d220d4e3d7d243ad78cdb51e7d671ed
Daniel Axtens <dja@axtens.net>
2016-05-18 11:16:49 +1000

    powerpc/sparse: make some things static
    This is just a smattering of things picked up by sparse that should
    be made static.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/idle.c
+++ arch/powerpc/platforms/powernv/idle.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static u32 supported_cpuidle_states;
 
-int pnv_save_sprs_for_winkle(void)
+static int pnv_save_sprs_for_winkle(void)
 {
 	int cpu;
 	int rc;
