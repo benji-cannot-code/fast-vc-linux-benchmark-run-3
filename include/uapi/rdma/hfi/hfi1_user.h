c2bd0ed587aee9f0364ad2c75bdd5d3c5363dc45
Dennis Dalessandro <dennis.dalessandro@intel.com>
2016-05-19 05:26:17 -0700

    IB/hfi1: Remove unused user command
    The HFI1_CMD_SDMA_STATUS_UPD command was never implemented it has no
    reason to live in the driver. Remove it.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- include/uapi/rdma/hfi/hfi1_user.h
+++ include/uapi/rdma/hfi/hfi1_user.h
@@ -115,7 +115,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HFI1_CMD_TID_UPDATE      4	/* update expected TID entries */
 #define HFI1_CMD_TID_FREE        5	/* free expected TID entries */
 #define HFI1_CMD_CREDIT_UPD      6	/* force an update of PIO credit */
-#define HFI1_CMD_SDMA_STATUS_UPD 7      /* force update of SDMA status ring */
 
 #define HFI1_CMD_RECV_CTRL       8	/* control receipt of packets */
 #define HFI1_CMD_POLL_TYPE       9	/* set the kind of polling we want */
