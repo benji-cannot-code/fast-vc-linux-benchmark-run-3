f7832e27646bada78254533960d7725ec9fd628e
Ian Abbott <abbotti@mev.co.uk>
2016-05-20 14:49:22 +0100

    staging: comedi: plx9080.h: include headers for declarations
    Add `#include` lines to declare stuff used by "plx9080.h" itself.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/plx9080.h
+++ drivers/staging/comedi/drivers/plx9080.h
@@ -20,6 +20,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __COMEDI_PLX9080_H
 #define __COMEDI_PLX9080_H
 
+#include <linux/compiler.h>
+#include <linux/types.h>
+#include <linux/bitops.h>
+#include <linux/delay.h>
+#include <linux/errno.h>
+#include <linux/io.h>
+
 /**
  * struct plx_dma_desc - DMA descriptor format for PLX PCI 9080
  * @pci_start_addr:	PCI Bus address for transfer (DMAPADR).
