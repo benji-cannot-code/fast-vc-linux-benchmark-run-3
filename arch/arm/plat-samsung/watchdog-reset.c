ec4b41263ce14b7ab7d9ce487753bab10e73f3f2
Andrea Gelmini <andrea.gelmini@gelma.net>
2016-05-21 13:51:23 +0200

    ARM: SAMSUNG: Fix typos
    Fix some language typos in comments.
    
    Signed-off-by: Andrea Gelmini <andrea.gelmini@gelma.net>
    Signed-off-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>

--- arch/arm/plat-samsung/watchdog-reset.c
+++ arch/arm/plat-samsung/watchdog-reset.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2008 Simtec Electronics
  *	Ben Dooks <ben@simtec.co.uk>
  *
- * Coyright (c) 2013 Tomasz Figa <tomasz.figa@gmail.com>
+ * Copyright (c) 2013 Tomasz Figa <tomasz.figa@gmail.com>
  *
  * Watchdog reset support for Samsung SoCs.
  *
