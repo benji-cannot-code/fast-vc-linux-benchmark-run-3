5e1314bd37670d08667f1b2f75ab9aa28864c41e
Ross Zwisler <ross.zwisler@linux.intel.com>
2016-05-20 17:01:42 -0700

    radix tree test suite: allow testing other fan-out values
    The defines in regression2.c are already in radix-tree.h and duplicating
    them in the test case makes experimenting with other values for the
    fan-out harder than necessary.  Allow the user of the radix tree to
    decide what the fan-out should be rather than fixing it to 8 for
    non-kernel uses.
    
    Signed-off-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Cc: Konstantin Khlebnikov <koct9i@gmail.com>
    Cc: Kirill Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Jan Kara <jack@suse.com>
    Cc: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/radix-tree/linux/kernel.h
+++ tools/testing/radix-tree/linux/kernel.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CONFIG_SHMEM
 #define CONFIG_SWAP
 
+#define RADIX_TREE_MAP_SHIFT	3
+
 #ifndef NULL
 #define NULL	0
 #endif
