05fc819b78243a6c8584a8557ea85fd99b5d9b8e
Matthew Wilcox <willy@linux.intel.com>
2016-05-20 17:01:54 -0700

    raxix-tree: introduce CONFIG_RADIX_TREE_MULTIORDER
    I've been receiving increasingly concerned notes from 0day about how
    much my recent changes have been bloating the radix tree.  Make it
    happier by only including multiorder support if
    CONFIG_TRANSPARENT_HUGEPAGES is set.
    
    This is an independent Kconfig option, so other radix tree users can
    also set it if they have a need.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Reviewed-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Cc: Konstantin Khlebnikov <koct9i@gmail.com>
    Cc: Kirill Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Jan Kara <jack@suse.com>
    Cc: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/radix-tree/linux/kernel.h
+++ tools/testing/radix-tree/linux/kernel.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../../include/linux/compiler.h"
 
+#define CONFIG_RADIX_TREE_MULTIORDER
 #define CONFIG_SHMEM
 #define CONFIG_SWAP
 
