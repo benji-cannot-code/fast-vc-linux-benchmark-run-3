64e4082f50023a88996aacd55037e5cc3023b15e
Matthew Wilcox <willy@linux.intel.com>
2016-05-20 17:01:39 -0700

    radix tree test suite: add tests for radix_tree_locate_item()
    Fairly simple tests; add various items to the tree, then make sure we
    can find them again.  Also check that a pointer that we know isn't in
    the tree is not found.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Reviewed-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Cc: Konstantin Khlebnikov <koct9i@gmail.com>
    Cc: Kirill Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Jan Kara <jack@suse.com>
    Cc: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/radix-tree/linux/kernel.h
+++ tools/testing/radix-tree/linux/kernel.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../../include/linux/compiler.h"
 
+#define CONFIG_SHMEM
+#define CONFIG_SWAP
+
 #ifndef NULL
 #define NULL	0
 #endif
