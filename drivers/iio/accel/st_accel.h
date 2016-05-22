f3a0d69afb7ff8dc3b7363e6a8468905ca04ef71
Jonathan Cameron <jic23@kernel.org>
2016-05-22 20:39:29 +0100

    iio: accel: st_accel: Add lis3l02dq support
    Time to finally kill off the venerable (it was one of my first drivers)
    lis3l02dq driver in favour of adding support in the st sensors framework.
    
    This does loose us the event support that driver always had, but I think
    that will reappear at some point and in the meantime the maintenance
    advantages of dropping the 'special' driver for this one part outweigh
    the issues.
    
    It's worth noting this part is ancient and I may well be the only person
    who still has any on hardware running recent kernels.
    
    It has a few 'quirks'.
     - No WAI register so that just became optional.
     - A BDU option that really does block updates.  Completely.
       Whatever you do, you don't get any more data with it set.
       It is documented the same as more modern parts but I presume they
       are actually clearing for updates after a read of both bytes!
     - Fixed scale.
     - It's too quick.  Even at slowest rate (280Hz) I can't read out fast
       enough on my board (stargate 2) to beat new data coming in. Linus'
       repeat read patch doesn't help in this case. It just means I get 10
       readings before dying... So in reality this will get used with
       software triggers only unless someone has this long out of production
       device on a quick board.
    
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Cc: Denis CIOCCA <denis.ciocca@st.com>
    Cc: Crestez Dan Leonard <leonard.crestez@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/accel/st_accel.h
+++ drivers/iio/accel/st_accel.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LSM330_ACCEL_DEV_NAME		"lsm330_accel"
 #define LSM303AGR_ACCEL_DEV_NAME	"lsm303agr_accel"
 #define LIS2DH12_ACCEL_DEV_NAME		"lis2dh12_accel"
+#define LIS3L02DQ_ACCEL_DEV_NAME	"lis3l02dq"
 
 /**
 * struct st_sensors_platform_data - default accel platform data
