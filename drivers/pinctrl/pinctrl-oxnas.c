53a8e2f104011d89960a4c9fc8f176479526dd6e
Neil Armstrong <narmstrong@baylibre.com>
2016-05-24 12:03:24 +0200

    pinctrl: oxnas: Add GPIO get_direction
    Implement a get_direction callback for the OXNAS GPIO driver in order
    to have pin output polarity in debugfs and new userspace ABI.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-oxnas.c
+++ drivers/pinctrl/pinctrl-oxnas.c
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* GPIO Registers */
 #define INPUT_VALUE	0x00
+#define OUTPUT_EN	0x04
 #define IRQ_PENDING	0x0c
 #define OUTPUT_SET	0x14
 #define OUTPUT_CLEAR	0x18
@@ -432,6 +433,15 @@ static int oxnas_gpio_request_enable(struct pinctrl_dev *pctldev,
 	return 0;
 }
 
+static int oxnas_gpio_get_direction(struct gpio_chip *chip,
+				      unsigned int offset)
+{
+	struct oxnas_gpio_bank *bank = gpiochip_get_data(chip);
+	u32 mask = BIT(offset);
+
+	return !(readl_relaxed(bank->reg_base + OUTPUT_EN) & mask);
+}
+
 static int oxnas_gpio_direction_input(struct gpio_chip *chip,
 				      unsigned int offset)
 {
@@ -665,6 +675,7 @@ static void oxnas_gpio_irq_handler(struct irq_desc *desc)
 			.label = "GPIO" #_bank,				\
 			.request = gpiochip_generic_request,		\
 			.free = gpiochip_generic_free,			\
+			.get_direction = oxnas_gpio_get_direction,	\
 			.direction_input = oxnas_gpio_direction_input,	\
 			.direction_output = oxnas_gpio_direction_output, \
 			.get = oxnas_gpio_get,				\
