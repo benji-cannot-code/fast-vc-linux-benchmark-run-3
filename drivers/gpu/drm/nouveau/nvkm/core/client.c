e34a0db23637968a1291631bcf7a20a2ed0bc253
Ben Skeggs <bskeggs@redhat.com>
2016-05-25 17:08:21 +1000

    drm/nouveau/core/client: pass notification callback to nvkm_client_new
    Preparation for supporting subclients.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/core/client.c
+++ drivers/gpu/drm/nouveau/nvkm/core/client.c
@@ -239,7 +239,9 @@ nvkm_client = {
 
 int
 nvkm_client_new(const char *name, u64 device, const char *cfg,
-		const char *dbg, struct nvkm_client **pclient)
+		const char *dbg,
+		int (*ntfy)(const void *, u32, const void *, u32),
+		struct nvkm_client **pclient)
 {
 	struct nvkm_oclass oclass = { .base = nvkm_uclient_sclass };
 	struct nvkm_client *client;
@@ -253,5 +255,6 @@ nvkm_client_new(const char *name, u64 device, const char *cfg,
 	client->device = device;
 	client->debug = nvkm_dbgopt(dbg, "CLIENT");
 	client->objroot = RB_ROOT;
+	client->ntfy = ntfy;
 	return 0;
 }
