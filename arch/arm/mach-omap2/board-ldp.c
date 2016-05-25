637a614f267069e605ba2e81adc837811c5acb1b
Peter Ujfalusi <peter.ujfalusi@ti.com>
2016-05-25 17:53:30 +0300

    ARM: OMAP2: Use the platform_data header for omapdss
    Instead of the full omapdss internal header, include only the platform_data
    header.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-ldp.c
+++ arch/arm/mach-omap2/board-ldp.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "gpmc.h"
 #include "gpmc-smsc911x.h"
 
-#include <video/omapdss.h>
+#include <linux/platform_data/omapdss.h>
 #include <video/omap-panel-data.h>
 
 #include "board-flash.h"
