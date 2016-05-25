d6795c8638f21f4a12399497a53f001bb24fd31b
Ben Skeggs <bskeggs@redhat.com>
2016-05-26 08:43:00 +1000

    drm/nouveau/core/client: modify prefix on nvif structures, for consistency
    Preparation for supporting subclients.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvif/if0000.h
+++ drivers/gpu/drm/nouveau/include/nvif/if0000.h
@@ -2,9 +2,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NVIF_IF0000_H__
 #define __NVIF_IF0000_H__
 
-#define NV_CLIENT_DEVLIST                                                  0x00
+#define NVIF_CLIENT_V0_DEVLIST                                             0x00
 
-struct nv_client_devlist_v0 {
+struct nvif_client_devlist_v0 {
 	__u8  version;
 	__u8  count;
 	__u8  pad02[6];
--- drivers/gpu/drm/nouveau/include/nvkm/core/client.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/client.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NVKM_CLIENT_H__
 #define __NVKM_CLIENT_H__
+#define nvkm_client(p) container_of((p), struct nvkm_client, object)
 #include <core/object.h>
 
 struct nvkm_client {
