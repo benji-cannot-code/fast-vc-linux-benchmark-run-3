2927d8a01fed050945398828f277028468e569ac
Peter Ujfalusi <peter.ujfalusi@ti.com>
2016-05-26 16:43:01 +0300

    omapdss: omap-panel-data.h: Remove struct omap_dss_device declaration
    The omap_dss_device is not needed by anything in the header file.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- include/video/omap-panel-data.h
+++ include/video/omap-panel-data.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <video/omapdss.h>
 #include <video/display_timing.h>
 
-struct omap_dss_device;
-
 /**
  * connector_atv platform data
  * @name: name for this display entity
