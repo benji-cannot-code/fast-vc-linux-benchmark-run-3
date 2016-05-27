8deddbce9d101c36e9292aa86b1da617bd91cf2e
Minfei Huang <mnghuan@gmail.com>
2016-05-27 14:17:10 +0800

    pvclock: Add CPU barriers to get correct version value
    Protocol for the "version" fields is: hypervisor raises it (making it
    uneven) before it starts updating the fields and raises it again (making
    it even) when it is done.  Thus the guest can make sure the time values
    it got are consistent by checking the version before and after reading
    them.
    
    Add CPU barries after getting version value just like what function
    vread_pvclock does, because all of callees in this function is inline.
    
    Fixes: 502dfeff239e8313bfbe906ca0a1a6827ac8481b
    Cc: stable@vger.kernel.org
    Signed-off-by: Minfei Huang <mnghuan@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kernel/pvclock.c
+++ arch/x86/kernel/pvclock.c
@@ -67,6 +67,8 @@ u8 pvclock_read_flags(struct pvclock_vcpu_time_info *src)
 
 	do {
 		version = __pvclock_read_cycles(src, &ret, &flags);
+		/* Make sure that the version double-check is last. */
+		smp_rmb();
 	} while ((src->version & 1) || version != src->version);
 
 	return flags & valid_flags;
@@ -81,6 +83,8 @@ cycle_t pvclock_clocksource_read(struct pvclock_vcpu_time_info *src)
 
 	do {
 		version = __pvclock_read_cycles(src, &ret, &flags);
+		/* Make sure that the version double-check is last. */
+		smp_rmb();
 	} while ((src->version & 1) || version != src->version);
 
 	if (unlikely((flags & PVCLOCK_GUEST_STOPPED) != 0)) {
