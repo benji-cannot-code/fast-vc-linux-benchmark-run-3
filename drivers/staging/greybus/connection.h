c7d130f0b5d2c20ddac70a7de3e600c67915d05c
Johan Hovold <johan@hovoldconsulting.com>
2016-05-27 17:26:25 +0200

    greybus: connection: add control connection flag
    Add control connection flag which will be set for control connections.
    
    Also add a helper function to test if the flag is set.
    
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/connection.h
+++ drivers/staging/greybus/connection.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GB_CONNECTION_FLAG_NO_FLOWCTRL	BIT(1)
 #define GB_CONNECTION_FLAG_OFFLOADED	BIT(2)
 #define GB_CONNECTION_FLAG_CDSI1	BIT(3)
+#define GB_CONNECTION_FLAG_CONTROL	BIT(4)
 
 enum gb_connection_state {
 	GB_CONNECTION_STATE_INVALID	= 0,
@@ -103,6 +104,11 @@ gb_connection_flow_control_disabled(struct gb_connection *connection)
 static inline bool gb_connection_is_offloaded(struct gb_connection *connection)
 {
 	return connection->flags & GB_CONNECTION_FLAG_OFFLOADED;
+}
+
+static inline bool gb_connection_is_control(struct gb_connection *connection)
+{
+	return connection->flags & GB_CONNECTION_FLAG_CONTROL;
 }
 
 static inline void *gb_connection_get_data(struct gb_connection *connection)
