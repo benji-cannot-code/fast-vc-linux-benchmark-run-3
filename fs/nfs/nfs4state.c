31aad35bd5d971f929d9ae74e51aac613352e2c7
Linus Torvalds <torvalds@linux-foundation.org>
2016-05-27 17:20:27 -0700

    nfs: fix anonymous member initializer build failure with older compilers
    Older versions of gcc don't understand named initializers inside a
    anonymous structure or union member.  It can be worked around by adding
    the bracin gin the initializer for the anonymous member.
    
    Without this, gcc 4.4.4 will fail the build with
    
        CC      fs/nfs/nfs4state.o
      fs/nfs/nfs4state.c:69: error: unknown field ‘data’ specified in initializer
      fs/nfs/nfs4state.c:69: warning: missing braces around initializer
      fs/nfs/nfs4state.c:69: warning: (near initialization for ‘zero_stateid.<anonymous>.data’)
      make[2]: *** [fs/nfs/nfs4state.o] Error 1
    
    introduced in commit 93b717fd81bf ("NFSv4: Label stateids with the type")
    
    Reported-and-tested-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Anna Schumaker <Anna.Schumaker@netapp.com>
    Cc: Trond Myklebust <trond.myklebust@primarydata.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/nfs/nfs4state.c
+++ fs/nfs/nfs4state.c
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OPENOWNER_POOL_SIZE	8
 
 const nfs4_stateid zero_stateid = {
-	.data = { 0 },
+	{ .data = { 0 } },
 	.type = NFS4_SPECIAL_STATEID_TYPE,
 };
 static DEFINE_MUTEX(nfs_clid_init_mutex);
