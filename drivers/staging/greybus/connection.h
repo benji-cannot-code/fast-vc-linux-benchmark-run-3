b96a24168762b8e60b2659df6625f5f1fdd0ae87
Viresh Kumar <viresh.kumar@linaro.org>
2016-05-30 11:31:53 +0530

    greybus: gbphy: Remove protocol specific version handling
    We should be using the generic version handling at bundle level, instead
    of at protocol level for bridged PHY devices as well.
    
    The bundle version handling is already in place, though it is *not* used
    today as we haven't bumped the version of control protocol yet.
    
    Remove protocol specific handling for bridged PHY devices.
    
    Tested on EVT 1.5 with gpbridge-test module. No nuttx changes are
    required with this.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/connection.h
+++ drivers/staging/greybus/connection.h
@@ -45,9 +45,6 @@ struct gb_connection {
 	gb_request_handler_t		handler;
 	unsigned long			flags;
 
-	u8				module_major;
-	u8				module_minor;
-
 	struct mutex			mutex;
 	spinlock_t			lock;
 	enum gb_connection_state	state;
