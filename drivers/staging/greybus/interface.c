a35cff92ccb6710f15abd4e7ec6619b2a4db220d
Viresh Kumar <viresh.kumar@linaro.org>
2016-05-30 13:05:10 +0530

    greybus: interface: Mode switch takes over a second
    Mode switch takes just over a second to complete and the current timeout
    of one second isn't sufficient.
    
    Mode-switch logs from EVT 1.5:
    
    [   56.709082] gb-firmware 1-3.3.1: Requested firmware package 'ara_00000126_00001002_fffe0001_ff980067_03.tftf'
    [   57.003968] gb_control_mode_switch_operation: 176
    [   58.041616] greybus 1-3.3: mode switch detected
    
    Increase the timeout to two seconds.
    
    Tested with EVT 1.5 with gpbridge-test module.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Tested-by: Karthik Ravi Shankar <karthikrs@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/interface.c
+++ drivers/staging/greybus/interface.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "greybus.h"
 #include "greybus_trace.h"
 
-#define GB_INTERFACE_MODE_SWITCH_TIMEOUT	1000
+#define GB_INTERFACE_MODE_SWITCH_TIMEOUT	2000
 
 #define GB_INTERFACE_DEVICE_ID_BAD	0xff
 
