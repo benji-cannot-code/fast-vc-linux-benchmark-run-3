dbe0dff013c78c9874c9a8f504bd1b9b5497645a
Peter Ujfalusi <peter.ujfalusi@ti.com>
2016-05-30 11:23:48 +0300

    ASoC: omap-mcbsp: Rename omap_mcbsp_sysfs_remove() to omap_mcbsp_cleanup()
    The function will do more then removing the sysfs files in the future.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Acked-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- sound/soc/omap/mcbsp.h
+++ sound/soc/omap/mcbsp.h
@@ -350,6 +350,6 @@ int omap_st_disable(struct omap_mcbsp *mcbsp);
 int omap_st_is_enabled(struct omap_mcbsp *mcbsp);
 
 int omap_mcbsp_init(struct platform_device *pdev);
-void omap_mcbsp_sysfs_remove(struct omap_mcbsp *mcbsp);
+void omap_mcbsp_cleanup(struct omap_mcbsp *mcbsp);
 
 #endif /* __ASOC_MCBSP_H */
