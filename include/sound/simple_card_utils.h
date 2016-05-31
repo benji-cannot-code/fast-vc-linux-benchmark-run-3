21b84a867c65deb4028d986e6972b4f697818bbf
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2016-05-31 09:00:14 +0000

    ASoC: add new simple-card-utils.c
    Current ALSA SoC has simple-card driver which is supporting both
    platform and DT probe.
    Now, some sound cards driver are created based on simple-card.
    They have similar feature or function, but implemented separately
    on each drivers. This is a waste of code.
    OTOH, merging these driver into same driver is highly risk,
    because it will be very difficult to keep compatibility.
    More over, ALSA SoC want to have graph base of DT feature in the
    future. Maybe it want to use simple-card like feature / function.
    Because of these background, this patch creates simple-card
    helper utils, and provides common function to each drivers.
    1st is asoc_simple_card_parse_daifmt()
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

---
+++ include/sound/simple_card_utils.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * simple_card_core.h
+ *
+ * Copyright (c) 2016 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#ifndef __SIMPLE_CARD_CORE_H
+#define __SIMPLE_CARD_CORE_H
+
+#include <sound/soc.h>
+
+int asoc_simple_card_parse_daifmt(struct device *dev,
+				  struct device_node *node,
+				  struct device_node *codec,
+				  char *prefix,
+				  unsigned int *retfmt);
+
+#endif /* __SIMPLE_CARD_CORE_H */
