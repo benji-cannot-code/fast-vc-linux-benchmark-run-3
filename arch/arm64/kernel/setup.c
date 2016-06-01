02f5b3e87628a9caa6d00baab6667d0ff5809be0
Kefeng Wang <wangkefeng.wang@huawei.com>
2016-06-01 14:52:57 +0800

    arm64: Remove unnecessary of_platform_populate with default match table
    After patch "of/platform: Add common method to populate default bus",
    it is possible for arch code to remove unnecessary callers of
    of_platform_populate with default match table.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/proc_fs.h>
 #include <linux/memblock.h>
 #include <linux/of_fdt.h>
-#include <linux/of_platform.h>
 #include <linux/efi.h>
 #include <linux/psci.h>
 
@@ -301,18 +300,6 @@ void __init setup_arch(char **cmdline_p)
 			boot_args[1], boot_args[2], boot_args[3]);
 	}
 }
-
-static int __init arm64_device_init(void)
-{
-	if (of_have_populated_dt()) {
-		of_platform_populate(NULL, of_default_bus_match_table,
-				     NULL, NULL);
-	} else if (acpi_disabled) {
-		pr_crit("Device tree not populated\n");
-	}
-	return 0;
-}
-arch_initcall_sync(arm64_device_init);
 
 static int __init topology_init(void)
 {
