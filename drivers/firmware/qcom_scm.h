a3b7fd61621ab7ec883107788fe7899fe2a64d59
Kumar Gala <galak@codeaurora.org>
2016-06-03 18:25:26 -0500

    firmware: qcom: scm: Add support for ARM64 SoCs
    Add an implementation of the SCM interface that works on ARM64 SoCs.  This
    is used by things like determine if we have HDCP support or not on the
    system.
    
    Signed-off-by: Kumar Gala <galak@codeaurora.org>
    Signed-off-by: Andy Gross <andy.gross@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/firmware/qcom_scm.h
+++ drivers/firmware/qcom_scm.h
@@ -39,7 +39,10 @@ extern int __qcom_scm_is_call_available(struct device *dev, u32 svc_id,
 extern int __qcom_scm_hdcp_req(struct device *dev,
 		struct qcom_scm_hdcp_req *req, u32 req_cnt, u32 *resp);
 
+extern void __qcom_scm_init(void);
+
 /* common error codes */
+#define QCOM_SCM_V2_EBUSY	-12
 #define QCOM_SCM_ENOMEM		-5
 #define QCOM_SCM_EOPNOTSUPP	-4
 #define QCOM_SCM_EINVAL_ADDR	-3
@@ -59,6 +62,8 @@ static inline int qcom_scm_remap_error(int err)
 		return -EOPNOTSUPP;
 	case QCOM_SCM_ENOMEM:
 		return -ENOMEM;
+	case QCOM_SCM_V2_EBUSY:
+		return -EBUSY;
 	}
 	return -EINVAL;
 }
