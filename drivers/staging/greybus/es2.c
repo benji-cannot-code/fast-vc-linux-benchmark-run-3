1ce8bbe7153a78e5b76fdac03d7e74fa5eb80604
Alex Elder <elder@linaro.org>
2016-06-03 15:55:38 -0500

    greybus: tracing: add timing traces
    Bryan reports he used certain message traces to determine when time
    sync messages transit the boundary between the Greybus core and the
    host device.  This patch adds two trace events--one a message event
    for outbound messages (because it indicates its operation and its
    destination), and one host device event for incoming messages
    (because message information isn't available as early as desired).
    These events are being created to allow the same sort of analysis
    of messages without having to store extra information for every
    message trace.  (The next patch changes the information a message
    trace records.)
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/es2.c
+++ drivers/staging/greybus/es2.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/unaligned.h>
 
 #include "greybus.h"
+#include "greybus_trace.h"
 #include "kernel_ver.h"
 #include "connection.h"
 
@@ -468,6 +469,9 @@ static int message_send(struct gb_host_device *hd, u16 cport_id,
 			  message->buffer, buffer_size,
 			  cport_out_callback, message);
 	urb->transfer_flags |= URB_ZERO_PACKET;
+
+	trace_gb_message_submit(message);
+
 	retval = usb_submit_urb(urb, gfp_mask);
 	if (retval) {
 		dev_err(&udev->dev, "failed to submit out-urb: %d\n", retval);
--- drivers/staging/greybus/hd.c
+++ drivers/staging/greybus/hd.c
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "greybus.h"
 #include "greybus_trace.h"
 
+EXPORT_TRACEPOINT_SYMBOL_GPL(gb_message_submit);
+EXPORT_TRACEPOINT_SYMBOL_GPL(gb_hd_in);
+
 static struct ida gb_hd_bus_id_map;
 
 int gb_hd_output(struct gb_host_device *hd, void *req, u16 size, u8 cmd,
