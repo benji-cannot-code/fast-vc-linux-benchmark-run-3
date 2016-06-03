4a101eea54de125b64152f2b9ceb00b2124282e8
Daniel Lezcano <daniel.lezcano@linaro.org>
2016-06-03 13:43:58 +0200

    clocksource/drivers/prima2: Remove useless header <asm/mach/time.h>
    The driver includes the <asm/mach/time.h> header but it is pointless.
    
    Remove it.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/timer-prima2.c
+++ drivers/clocksource/timer-prima2.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/of_address.h>
 #include <linux/sched_clock.h>
-#include <asm/mach/time.h>
 
 #define PRIMA2_CLOCK_FREQ 1000000
 
