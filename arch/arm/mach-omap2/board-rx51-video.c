03d2bf08505c7c01eab2a751dfd4e019bb7a0cae
Peter Ujfalusi <peter.ujfalusi@ti.com>
2016-06-03 13:25:46 +0300

    ARM: OMAP: rx51-video: Do not set TV connector_type
    OMAP_DSS_VENC_TYPE_COMPOSITE is 0. There is no need to explicitly set the
    connector_type.
    This change is needed for the omapdss header cleanup.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-rx51-video.c
+++ arch/arm/mach-omap2/board-rx51-video.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct connector_atv_platform_data rx51_tv_pdata = {
 	.name = "tv",
 	.source = "venc.0",
-	.connector_type = OMAP_DSS_VENC_TYPE_COMPOSITE,
 	.invert_polarity = false,
 };
 
