ec0a1eba6bc90a51e80e4c396e28c65afb8e70e0
Alex Elder <elder@linaro.org>
2016-06-03 15:55:30 -0500

    greybus: define BUNDLE_ID_NONE
    Define a bundle ID that means "no bundle".  This will be used for
    tracing connection events during the portion of a connection's
    lifetime when it has no bundle associated with it.  Don't allow
    any bundle to be created using that ID.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/bundle.h
+++ drivers/staging/greybus/bundle.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/list.h>
 
+#define	BUNDLE_ID_NONE	U8_MAX
+
 /* Greybus "public" definitions" */
 struct gb_bundle {
 	struct device		dev;
