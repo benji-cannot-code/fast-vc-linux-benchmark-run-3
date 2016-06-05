0b1de102a6d5ddffe9c209be5851e774256d91ae
Geert Uytterhoeven <geert@linux-m68k.org>
2016-06-05 11:17:10 +0200

    NFC: fdp: Detect errors from fdp_nci_create_conn()
    drivers/nfc/fdp/fdp.c: In function ‘fdp_nci_patch_otp’:
    drivers/nfc/fdp/fdp.c:373: warning: comparison is always false due to limited range of data type
    drivers/nfc/fdp/fdp.c: In function ‘fdp_nci_patch_ram’:
    drivers/nfc/fdp/fdp.c:444: warning: comparison is always false due to limited range of data type
    
    fdp_nci_create_conn() may return a negative error code, which is
    silently ignored by assigning it to a u8.
    
    Change conn_id from u8 to int to fix this.
    
    Fixes: a06347c04c13e380 ("NFC: Add Intel Fields Peak NFC solution driver")
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/fdp/fdp.c
+++ drivers/nfc/fdp/fdp.c
@@ -354,7 +354,7 @@ static int fdp_nci_patch_otp(struct nci_dev *ndev)
 {
 	struct fdp_nci_info *info = nci_get_drvdata(ndev);
 	struct device *dev = &info->phy->i2c_dev->dev;
-	u8 conn_id;
+	int conn_id;
 	int r = 0;
 
 	if (info->otp_version >= info->otp_patch_version)
@@ -425,7 +425,7 @@ static int fdp_nci_patch_ram(struct nci_dev *ndev)
 {
 	struct fdp_nci_info *info = nci_get_drvdata(ndev);
 	struct device *dev = &info->phy->i2c_dev->dev;
-	u8 conn_id;
+	int conn_id;
 	int r = 0;
 
 	if (info->ram_version >= info->ram_patch_version)
