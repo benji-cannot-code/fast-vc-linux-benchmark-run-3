61d719831fea92a1f80c566c762fafd280b74287
Hans de Goede <hdegoede@redhat.com>
2016-06-05 15:50:48 +0200

    mfd: axp20x: Add a 500ms delay at the end of axp20x_power_off
    The kernel expects the power_off function to not return, and if it does
    it panics. Add a slight delay after the i2c write which turns off power
    through the PMIC, to give capacitors etc. some time to drain.
    
    Without this the kernel lives on long enough after the poweroff to
    print the following on the serial console on my Mele A1000G quad:
    
    [  248.583588] reboot: Power down
    [  248.600490] Kernel pa
    
    With the delay the start of printing "Kernel panic" is gone.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/axp20x.c
+++ drivers/mfd/axp20x.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/err.h>
+#include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -668,6 +669,9 @@ static void axp20x_power_off(void)
 
 	regmap_write(axp20x_pm_power_off->regmap, AXP20X_OFF_CTRL,
 		     AXP20X_OFF);
+
+	/* Give capacitors etc. time to drain to avoid kernel panic msg. */
+	msleep(500);
 }
 
 int axp20x_match_device(struct axp20x_dev *axp20x)
