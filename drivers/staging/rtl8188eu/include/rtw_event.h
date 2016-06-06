37b7964999cc2531d0038a40475c2105f4990d55
Binoy Jayan <binoy.jayan@linaro.org>
2016-06-06 10:08:07 +0530

    rtl8188eu: Remove unused semaphores
    The semaphores xmit_sema, terminate_xmitthread_sema and tx_retevt
    have no users, hence remove all references to them.
    
    Signed-off-by: Binoy Jayan <binoy.jayan@linaro.org>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_event.h
+++ drivers/staging/rtl8188eu/include/rtw_event.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <osdep_service.h>
 
 #include <wlan_bssdef.h>
-#include <linux/semaphore.h>
 #include <linux/mutex.h>
 #include <linux/sem.h>
 
