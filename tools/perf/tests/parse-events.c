6ea15acf7f0fe0fdf6471a77cf74c965aab2291f
Andi Kleen <ak@linux.intel.com>
2016-06-06 07:36:06 -0700

    perf test: Ignore .scale and other special files
    'perf test' tries to parse all entries in /sys/devices/cpu/events/.
    Ignore the special entries like '.scale', which cannot be directly
    parsed as an event. This patch assumes all files containing a '.' are
    special and can be ignored.
    
    Reported-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Link: http://lkml.kernel.org/r/1465223766-29902-1-git-send-email-andi@firstfloor.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/parse-events.c
+++ tools/perf/tests/parse-events.c
@@ -1784,8 +1784,8 @@ static int test_pmu_events(void)
 		struct evlist_test e;
 		char name[MAX_NAME];
 
-		if (!strcmp(ent->d_name, ".") ||
-		    !strcmp(ent->d_name, ".."))
+		/* Names containing . are special and cannot be used directly */
+		if (strchr(ent->d_name, '.'))
 			continue;
 
 		snprintf(name, MAX_NAME, "cpu/event=%s/u", ent->d_name);
