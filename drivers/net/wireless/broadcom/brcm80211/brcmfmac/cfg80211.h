62fec4c1ec16ae3db84a1f602c9834a6ed2a9908
Rafał Miłecki <zajec5@gmail.com>
2016-06-07 08:20:21 +0200

    brcmfmac: include required headers in cfg80211.h
    Without this including cfg80211.h in a wrong order could result in:
    
    drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h:122:24: error: array type has incomplete element type
      struct brcmf_wsec_key key[BRCMF_MAX_DEFAULT_KEYS];
                            ^
    drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h:291:24: error: field ‘p2p’ has incomplete type
      struct brcmf_p2p_info p2p;
                            ^
    drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h:297:27: error: field ‘pmk_list’ has incomplete type
      struct brcmf_pmk_list_le pmk_list;
                               ^
    drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h:317:28: error: field ‘assoclist’ has incomplete type
      struct brcmf_assoclist_le assoclist;
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.h
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* for brcmu_d11inf */
 #include <brcmu_d11.h>
 
+#include "fwil_types.h"
+#include "p2p.h"
+
 #define WL_NUM_SCAN_MAX			10
 #define WL_TLV_INFO_MAX			1024
 #define WL_BSS_INFO_MAX			2048
