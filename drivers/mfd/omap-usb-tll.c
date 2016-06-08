5a4781de6a33d2cb3e2948526c5080314fd5182c
Ben Dooks <ben.dooks@codethink.co.uk>
2016-06-08 19:06:20 +0100

    mfd: omap-usb-tll: Include omap-usb.h
    Fix the warnings about the following functions not being
    declared by including omap-usb.h which declares them:
    
    drivers/mfd/omap-usb-tll.c:333:5: warning: symbol 'omap_tll_init' was not declared. Should it be static?
    drivers/mfd/omap-usb-tll.c:408:5: warning: symbol 'omap_tll_enable' was not declared. Should it be static?
    drivers/mfd/omap-usb-tll.c:442:5: warning: symbol 'omap_tll_disable' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/omap-usb-tll.c
+++ drivers/mfd/omap-usb-tll.c
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_data/usb-omap.h>
 #include <linux/of.h>
 
+#include "omap-usb.h"
+
 #define USBTLL_DRIVER_NAME	"usbhs_tll"
 
 /* TLL Register Set */
