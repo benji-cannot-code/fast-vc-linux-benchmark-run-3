3459de5e5e9bdf904072bcac9bd70706a87383e3
Lee Jones <lee.jones@linaro.org>
2016-06-08 10:21:23 +0100

    pwm: Add PWM capture support
    Supply a PWM capture callback op in order to pass back information
    obtained by running analysis on a PWM signal. This would normally (at
    least during testing) be called from the sysfs routines with a view to
    printing out PWM capture data which has been encoded into a string.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    [thierry.reding@gmail.com: make capture data unsigned int for symmetry]
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- include/linux/pwm.h
+++ include/linux/pwm.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/of.h>
 
+struct pwm_capture;
 struct seq_file;
+
 struct pwm_chip;
 
 /**
@@ -154,6 +156,7 @@ static inline void pwm_get_args(const struct pwm_device *pwm,
  * @free: optional hook for freeing a PWM
  * @config: configure duty cycles and period length for this PWM
  * @set_polarity: configure the polarity of this PWM
+ * @capture: capture and report PWM signal
  * @enable: enable PWM output toggling
  * @disable: disable PWM output toggling
  * @apply: atomically apply a new PWM config. The state argument
@@ -173,6 +176,8 @@ struct pwm_ops {
 		      int duty_ns, int period_ns);
 	int (*set_polarity)(struct pwm_chip *chip, struct pwm_device *pwm,
 			    enum pwm_polarity polarity);
+	int (*capture)(struct pwm_chip *chip, struct pwm_device *pwm,
+		       struct pwm_capture *result, unsigned long timeout);
 	int (*enable)(struct pwm_chip *chip, struct pwm_device *pwm);
 	void (*disable)(struct pwm_chip *chip, struct pwm_device *pwm);
 	int (*apply)(struct pwm_chip *chip, struct pwm_device *pwm,
@@ -211,6 +216,16 @@ struct pwm_chip {
 					const struct of_phandle_args *args);
 	unsigned int of_pwm_n_cells;
 	bool can_sleep;
+};
+
+/**
+ * struct pwm_capture - PWM capture data
+ * @period: period of the PWM signal (in nanoseconds)
+ * @duty_cycle: duty cycle of the PWM signal (in nanoseconds)
+ */
+struct pwm_capture {
+	unsigned int period;
+	unsigned int duty_cycle;
 };
 
 #if IS_ENABLED(CONFIG_PWM)
@@ -323,6 +338,8 @@ static inline void pwm_disable(struct pwm_device *pwm)
 
 
 /* PWM provider APIs */
+int pwm_capture(struct pwm_device *pwm, struct pwm_capture *result,
+		unsigned long timeout);
 int pwm_set_chip_data(struct pwm_device *pwm, void *data);
 void *pwm_get_chip_data(struct pwm_device *pwm);
 
@@ -370,6 +387,13 @@ static inline int pwm_adjust_config(struct pwm_device *pwm)
 
 static inline int pwm_config(struct pwm_device *pwm, int duty_ns,
 			     int period_ns)
+{
+	return -EINVAL;
+}
+
+static inline int pwm_capture(struct pwm_device *pwm,
+			      struct pwm_capture *result,
+			      unsigned long timeout)
 {
 	return -EINVAL;
 }
