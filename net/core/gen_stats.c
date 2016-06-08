2d8db3d6a7d6346c0ba481e08cab7f44edcc189c
Eric Dumazet <edumazet@google.com>
2016-06-08 07:22:49 -0700

    net: sched: fix missing doc annotations
    "make htmldocs" complains otherwise:
    
    .//net/core/gen_stats.c:168: warning: No description found for parameter 'running'
    .//include/linux/netdevice.h:1867: warning: No description found for parameter 'qdisc_running_key'
    
    Fixes: f9eb8aea2a1e ("net_sched: transform qdisc running bit into a seqcount")
    Fixes: edb09eb17ed8 ("net: sched: do not acquire qdisc spinlock in qdisc/class stats dump")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/gen_stats.c
+++ net/core/gen_stats.c
@@ -151,6 +151,7 @@ EXPORT_SYMBOL(__gnet_stats_copy_basic);
 
 /**
  * gnet_stats_copy_basic - copy basic statistics into statistic TLV
+ * @running: seqcount_t pointer
  * @d: dumping handle
  * @cpu: copy statistic per cpu
  * @b: basic statistics
