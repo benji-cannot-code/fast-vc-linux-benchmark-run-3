147041401ff9b6d9eca2dec4df129ff14a7220a6
Jes Sorensen <Jes.Sorensen@redhat.com>
2016-06-09 14:38:47 -0400

    rtl8xxxu: Add bit definitions for REG_USB_SPECIAL_OPTION
    Documentation for enabling USB aggregation and whether to select
    interrupt or bulk delivery of interrupt events.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_regs.h
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_regs.h
@@ -1053,6 +1053,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  USB_HIMR_ROK			BIT(0)	/*  Receive DMA OK Interrupt */
 
 #define REG_USB_SPECIAL_OPTION		0xfe55
+#define  USB_SPEC_USB_AGG_ENABLE	BIT(3)	/* Enable USB aggregation */
+#define  USB_SPEC_INT_BULK_SELECT	BIT(4)	/* Use interrupt endpoint to
+						   deliver interrupt packet.
+						   0: Use int, 1: use bulk */
 #define REG_USB_HRPWM			0xfe58
 #define REG_USB_DMA_AGG_TO		0xfe5b
 #define REG_USB_AGG_TO			0xfe5c
