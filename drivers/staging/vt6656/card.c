5650debbfa985491eaba8db209a9e98365cef729
Rithvik Patibandla <rithvikp98@gmail.com>
2016-06-12 01:38:58 +0530

    staging:vt6656:card.c:Fix comment block issue
    Fix "Block comments use * on subsequent lines" and "Block comments use
    */ on trailing lines" warnings thrown by checkpatch.pl
    
    Signed-off-by: Rithvik Patibandla <rithvikp98@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/card.c
+++ drivers/staging/vt6656/card.c
@@ -48,7 +48,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "usbpipe.h"
 
 /* const u16 cwRXBCNTSFOff[MAX_RATE] =
-   {17, 34, 96, 192, 34, 23, 17, 11, 8, 5, 4, 3}; */
+ *   {17, 34, 96, 192, 34, 23, 17, 11, 8, 5, 4, 3};
+ */
 
 static const u16 cwRXBCNTSFOff[MAX_RATE] = {
 	192, 96, 34, 17, 34, 23, 17, 11, 8, 5, 4, 3
