ec2bd5b48af6de20dcef95064307f7161abdf3b7
Viresh Kumar <viresh.kumar@linaro.org>
2016-06-13 15:08:01 +0530

    greybus: camera: Initialize mutex before using it
    We are using the mutex from gb_camera_cleanup(), which can get called
    even before the mutex is initialized.
    
    Fix it by initializing the mutex early enough.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@linaro.org>
    Reviewed-by: Vaibhav Hiremath <vaibhav.hiremath@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/camera.c
+++ drivers/staging/greybus/camera.c
@@ -1000,6 +1000,7 @@ static int gb_camera_probe(struct gb_bundle *bundle,
 		return -ENOMEM;
 
 	gcam->bundle = bundle;
+	mutex_init(&gcam->mutex);
 
 	conn = gb_connection_create(bundle, mgmt_cport_id,
 				    gb_camera_request_handler);
@@ -1014,8 +1015,6 @@ static int gb_camera_probe(struct gb_bundle *bundle,
 	ret = gb_connection_enable(conn);
 	if (ret)
 		goto error;
-
-	mutex_init(&gcam->mutex);
 
 	/*
 	 * Create the data connection between the camera module data CPort and
