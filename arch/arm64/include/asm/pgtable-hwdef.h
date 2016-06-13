d07eab9b4b90c533416e25c383258435c8106c75
Marc Zyngier <marc.zyngier@arm.com>
2016-06-13 15:00:46 +0100

    arm64: Add PTE_HYP_XN page table flag
    EL2 page tables can be configured to deny code from being
    executed, which is done by setting bit 54 in the page descriptor.
    
    It is the same bit as PTE_UXN, but the "USER" reference felt odd
    in the hypervisor code.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/arm64/include/asm/pgtable-hwdef.h
+++ arch/arm64/include/asm/pgtable-hwdef.h
@@ -165,6 +165,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PTE_CONT		(_AT(pteval_t, 1) << 52)	/* Contiguous range */
 #define PTE_PXN			(_AT(pteval_t, 1) << 53)	/* Privileged XN */
 #define PTE_UXN			(_AT(pteval_t, 1) << 54)	/* User XN */
+#define PTE_HYP_XN		(_AT(pteval_t, 1) << 54)	/* HYP XN */
 
 /*
  * AttrIndx[2:0] encoding (mapping attributes defined in the MAIR* registers).
