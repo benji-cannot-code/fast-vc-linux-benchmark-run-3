5e640cd5f456e67ace4a4cbc20c9e030ece30241
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-06-14 14:58:56 +0900

    kconfig.h: use already defined macros for IS_REACHABLE() define
    For the same reason as commit 02d699f1f464 ("include/linux/kconfig.h:
    ese macros which are already defined"), it is better to use macros
    IS_BUILTIN() and IS_MODULE() for defining IS_REACHABLE().
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Michal Marek <mmarek@suse.com>

--- include/linux/kconfig.h
+++ include/linux/kconfig.h
@@ -43,8 +43,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This is similar to IS_ENABLED(), but returns false when invoked from
  * built-in code when CONFIG_FOO is set to 'm'.
  */
-#define IS_REACHABLE(option) (config_enabled(option) || \
-		 (config_enabled(option##_MODULE) && __is_defined(MODULE)))
+#define IS_REACHABLE(option) (IS_BUILTIN(option) || \
+		 (IS_MODULE(option) && __is_defined(MODULE)))
 
 /*
  * IS_ENABLED(CONFIG_FOO) evaluates to 1 if CONFIG_FOO is set to 'y' or 'm',
