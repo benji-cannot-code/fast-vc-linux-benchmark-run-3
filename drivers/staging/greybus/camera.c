4b871ed91d80e7fa8e300e6c854cade2401f75e0
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2016-06-14 15:59:24 +0300

    greybus: camera: Fix data connection setup
    When the module is in the configured state, an attempt to change the
    configuration must first tear down the data connection to update its
    parameters, as the APB1 bridge doesn't support modifying the CSI
    transmitter configuration when it is already started.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Gjorgji Rosikopulos <grosikopulos@mm-sol.com>
    Tested-by: Gjorgji Rosikopulos <grosikopulos@mm-sol.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/camera.c
+++ drivers/staging/greybus/camera.c
@@ -34,11 +34,17 @@ struct gb_camera_debugfs_buffer {
 	size_t length;
 };
 
+enum gb_camera_state {
+	GB_CAMERA_STATE_UNCONFIGURED,
+	GB_CAMERA_STATE_CONFIGURED,
+};
+
 /**
  * struct gb_camera - A Greybus Camera Device
  * @connection: the greybus connection for camera management
  * @data_connection: the greybus connection for camera data
- * @mutex: protects the connection field
+ * @mutex: protects the connection and state fields
+ * @state: the current module state
  * @debugfs: debugfs entries for camera protocol operations testing
  * @module: Greybus camera module registered to HOST processor.
  */
@@ -46,7 +52,9 @@ struct gb_camera {
 	struct gb_bundle *bundle;
 	struct gb_connection *connection;
 	struct gb_connection *data_connection;
+
 	struct mutex mutex;
+	enum gb_camera_state state;
 
 	struct {
 		struct dentry *root;
@@ -301,7 +309,6 @@ static int gb_camera_configure_streams(struct gb_camera *gcam,
 {
 	struct gb_camera_configure_streams_request *req;
 	struct gb_camera_configure_streams_response *resp;
-
 	unsigned int nstreams = *num_streams;
 	unsigned int i;
 	size_t req_size;
@@ -386,6 +393,11 @@ static int gb_camera_configure_streams(struct gb_camera *gcam,
 		goto done;
 	}
 
+	if (gcam->state == GB_CAMERA_STATE_CONFIGURED) {
+		gb_camera_teardown_data_connection(gcam);
+		gcam->state = GB_CAMERA_STATE_UNCONFIGURED;
+	}
+
 	if (resp->num_streams) {
 		ret = gb_camera_setup_data_connection(gcam, resp, csi_params);
 		if (ret < 0) {
@@ -395,8 +407,8 @@ static int gb_camera_configure_streams(struct gb_camera *gcam,
 					  req, req_size, resp, resp_size);
 			goto done;
 		}
-	} else {
-		gb_camera_teardown_data_connection(gcam);
+
+		gcam->state = GB_CAMERA_STATE_CONFIGURED;
 	}
 
 	*flags = resp->flags;
@@ -1040,8 +1052,10 @@ static int gb_camera_probe(struct gb_bundle *bundle,
 	if (!gcam)
 		return -ENOMEM;
 
-	gcam->bundle = bundle;
 	mutex_init(&gcam->mutex);
+
+	gcam->bundle = bundle;
+	gcam->state = GB_CAMERA_STATE_UNCONFIGURED;
 
 	conn = gb_connection_create(bundle, mgmt_cport_id,
 				    gb_camera_request_handler);
