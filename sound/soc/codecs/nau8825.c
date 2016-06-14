c50b2e1fb7d3d4755502e45a7d62fddabf2ad0b5
Arnd Bergmann <arnd@arndb.de>
2016-06-14 12:19:31 +0200

    ASoC: nau8825: mark pm functions __maybe_unused
    The newly added nau8825_dai_is_active() function is only called from
    the PM logic that is build-time conditional in this driver, so we get
    a warning when CONFIG_PM is disabled:
    
    sound/soc/codecs/nau8825.c:229:13: error: 'nau8825_dai_is_active' defined but not used [-Werror=unused-function]
     static bool nau8825_dai_is_active(struct nau8825 *nau8825)
    
    By replacing the #ifdef around the functions with a __maybe_unused
    annotation, the code becomes more robust to this kind of problem
    and we no longer get the warning while also slightly improving
    readability.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: b50455fab459 ("ASoC: nau8825: cross talk suppression measurement function")
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/nau8825.c
+++ sound/soc/codecs/nau8825.c
@@ -2217,8 +2217,7 @@ static int nau8825_set_bias_level(struct snd_soc_codec *codec,
 	return 0;
 }
 
-#ifdef CONFIG_PM
-static int nau8825_suspend(struct snd_soc_codec *codec)
+static int __maybe_unused nau8825_suspend(struct snd_soc_codec *codec)
 {
 	struct nau8825 *nau8825 = snd_soc_codec_get_drvdata(codec);
 
@@ -2230,7 +2229,7 @@ static int nau8825_suspend(struct snd_soc_codec *codec)
 	return 0;
 }
 
-static int nau8825_resume(struct snd_soc_codec *codec)
+static int __maybe_unused nau8825_resume(struct snd_soc_codec *codec)
 {
 	struct nau8825 *nau8825 = snd_soc_codec_get_drvdata(codec);
 
@@ -2254,10 +2253,6 @@ static int nau8825_resume(struct snd_soc_codec *codec)
 
 	return 0;
 }
-#else
-#define nau8825_suspend NULL
-#define nau8825_resume NULL
-#endif
 
 static struct snd_soc_codec_driver nau8825_codec_driver = {
 	.probe = nau8825_codec_probe,
