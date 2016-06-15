13063930de09d4152007cedc5dd566c064dc4a35
Archit Taneja <architt@codeaurora.org>
2016-06-15 17:01:27 +0530

    drm/bridge: adv7533: Create a MIPI DSI device
    In order to pass DSI specific parameters to the DSI host, we need the
    driver to create a mipi_dsi_device DSI device that attaches to the
    host.
    
    Use of_graph helpers to get the DSI host DT node. Create a MIPI DSI
    device using this host. Finally, attach this device to the DSI host.
    
    Populate DT parameters (number of data lanes for now) that are required
    for DSI RX to work correctly. Hardcode few other parameters (rgb,
    embedded_sync) for now.
    
    Select DRM_MIPI_DSI config option only when ADV7533 support is enabled.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/gpu/drm/bridge/adv7511/adv7511.h
+++ drivers/gpu/drm/bridge/adv7511/adv7511.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regmap.h>
 
 #include <drm/drm_crtc_helper.h>
+#include <drm/drm_mipi_dsi.h>
 
 #define ADV7511_REG_CHIP_REVISION		0x00
 #define ADV7511_REG_N0				0x01
@@ -325,6 +326,11 @@ struct adv7511 {
 
 	struct gpio_desc *gpio_pd;
 
+	/* ADV7533 DSI RX related params */
+	struct device_node *host_node;
+	struct mipi_dsi_device *dsi;
+	u8 num_dsi_lanes;
+
 	enum adv7511_type type;
 };
 
@@ -334,6 +340,9 @@ void adv7533_dsi_power_off(struct adv7511 *adv);
 int adv7533_patch_registers(struct adv7511 *adv);
 void adv7533_uninit_cec(struct adv7511 *adv);
 int adv7533_init_cec(struct adv7511 *adv);
+int adv7533_attach_dsi(struct adv7511 *adv);
+void adv7533_detach_dsi(struct adv7511 *adv);
+int adv7533_parse_dt(struct device_node *np, struct adv7511 *adv);
 #else
 static inline void adv7533_dsi_power_on(struct adv7511 *adv)
 {
@@ -353,6 +362,20 @@ static inline void adv7533_uninit_cec(struct adv7511 *adv)
 }
 
 static inline int adv7533_init_cec(struct adv7511 *adv)
+{
+	return -ENODEV;
+}
+
+static inline int adv7533_attach_dsi(struct adv7511 *adv)
+{
+	return -ENODEV;
+}
+
+static inline void adv7533_detach_dsi(struct adv7511 *adv)
+{
+}
+
+static inline int adv7533_parse_dt(struct device_node *np, struct adv7511 *adv)
 {
 	return -ENODEV;
 }
