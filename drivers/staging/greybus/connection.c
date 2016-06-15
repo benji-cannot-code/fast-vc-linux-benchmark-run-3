661e089e61b00b8122d9562900fb6e5a2520a23f
Viresh Kumar <viresh.kumar@linaro.org>
2016-06-15 08:25:56 +0530

    greybus: connection: add trace events before disabling connection
    This is what we are doing elsewhere:
    - Send enable/create trace events after enabling/creating stuff.
    - Send disable/remove trace events before disabling/removing stuff.
    
    This wasn't followed in a same way while disabling connections. Fix it.
    
    Compile tested.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/connection.c
+++ drivers/staging/greybus/connection.c
@@ -791,6 +791,8 @@ void gb_connection_disable(struct gb_connection *connection)
 	if (connection->state == GB_CONNECTION_STATE_DISABLED)
 		goto out_unlock;
 
+	trace_gb_connection_disable(connection);
+
 	gb_connection_control_disconnecting(connection);
 
 	spin_lock_irq(&connection->lock);
@@ -808,8 +810,6 @@ void gb_connection_disable(struct gb_connection *connection)
 	gb_connection_control_disconnected(connection);
 
 	connection->state = GB_CONNECTION_STATE_DISABLED;
-
-	trace_gb_connection_disable(connection);
 
 	/* control-connection tear down is deferred when mode switching */
 	if (!connection->mode_switch) {
@@ -830,6 +830,8 @@ void gb_connection_disable_forced(struct gb_connection *connection)
 	if (connection->state == GB_CONNECTION_STATE_DISABLED)
 		goto out_unlock;
 
+	trace_gb_connection_disable(connection);
+
 	spin_lock_irq(&connection->lock);
 	connection->state = GB_CONNECTION_STATE_DISABLED;
 	gb_connection_cancel_operations(connection, -ESHUTDOWN);
@@ -839,8 +841,6 @@ void gb_connection_disable_forced(struct gb_connection *connection)
 	gb_connection_hd_cport_features_disable(connection);
 	gb_connection_svc_connection_destroy(connection);
 	gb_connection_hd_cport_disable(connection);
-
-	trace_gb_connection_disable(connection);
 
 out_unlock:
 	mutex_unlock(&connection->mutex);
