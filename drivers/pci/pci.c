bacf1cb848a32eb326773974e8fac4436a4294f0
Ben Dooks <ben.dooks@codethink.co.uk>
2016-06-17 16:05:13 +0100

    PCI: Include <asm/dma.h> for isa_dma_bridge_buggy
    At least on arm, <asm/dma.h> does not get included when building
    drivers/pci/pci.o.  This causes the following build warning which can be
    fixed by including <asm/dma.h>:
    
      drivers/pci/pci.c:37:5: warning: symbol 'isa_dma_bridge_buggy' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm_runtime.h>
 #include <linux/pci_hotplug.h>
 #include <asm/setup.h>
+#include <asm/dma.h>
 #include <linux/aer.h>
 #include "pci.h"
 
