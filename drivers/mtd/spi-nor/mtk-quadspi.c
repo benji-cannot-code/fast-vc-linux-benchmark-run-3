09a381221c957696bbf23509930cf8a9dba86fea
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2016-06-17 17:21:16 +0000

    mtd: mtk-nor: remove duplicated include from mtk-quadspi.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/spi-nor/mtk-quadspi.c
+++ drivers/mtd/spi-nor/mtk-quadspi.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioport.h>
 #include <linux/math64.h>
 #include <linux/module.h>
-#include <linux/mtd/mtd.h>
 #include <linux/mutex.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
