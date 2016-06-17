99b6cd950439f8b7c02d855cb12eda33b09a9887
Garlic Tseng <garlic.tseng@mediatek.com>
2016-06-17 15:43:52 +0800

    ASoC: mediatek: Refine mt8173 driver and change config option
    move mt8173 driver to another folder and add prefix.
    add config option SND_SOC_MT8173
    
    Signed-off-by: Garlic Tseng <garlic.tseng@mediatek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt8173/mt8173-max98090.c
+++ sound/soc/mediatek/mt8173/mt8173-max98090.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 #include <sound/jack.h>
 #include <linux/gpio.h>
-#include "../codecs/max98090.h"
+#include "../../codecs/max98090.h"
 
 static struct snd_soc_jack mt8173_max98090_jack;
 
