03f76b606e21394d809d8e353e00d62e2132da26
Wolfram Sang <wsa@the-dreams.de>
2016-06-17 10:46:54 +0200

    staging: ks7010: collect all includes and sort them
    Includes probably need a driver-wide clean up, but for now let's at
    least group all includes from the sdio file together.
    
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks7010_sdio.c
+++ drivers/staging/ks7010/ks7010_sdio.c
@@ -11,15 +11,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   published by the Free Software Foundation.
  */
 
-#include <linux/workqueue.h>
-#include <asm/atomic.h>
+#include <linux/firmware.h>
 #include <linux/mmc/card.h>
 #include <linux/mmc/sdio_func.h>
+#include <linux/workqueue.h>
+#include <asm/atomic.h>
 
 #include "ks_wlan.h"
 #include "ks_wlan_ioctl.h"
 #include "ks_hostif.h"
-
 #include "ks7010_sdio.h"
 
 #define KS7010_FUNC_NUM 1
@@ -778,7 +778,6 @@ static int ks7010_sdio_data_compare(struct ks_wlan_private *priv, u32 address,
 	return rc;
 }
 
-#include <linux/firmware.h>
 static int ks79xx_upload_firmware(struct ks_wlan_private *priv,
 				  struct ks_sdio_card *card)
 {
