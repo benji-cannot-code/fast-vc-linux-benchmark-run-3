7167fa850ce0f05d90fa3309d490e14fec8c9659
Arnd Bergmann <arnd@arndb.de>
2016-06-20 22:27:23 +0200

    ARM: ux500: move l2x0 init to .init_irq
    The generic IRQ init function also enables the l2 cache
    implicitly when the machine descriptor sets an .l2c_aux_mask.
    
    Let's use that on ux500 and remove the ux500_l2x0_init()
    along with the cpu_is_u8500_family checks.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/setup.h
+++ arch/arm/mach-ux500/setup.h
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <linux/init.h>
 
+
+void ux500_l2x0_init(void);
+
 void ux500_restart(enum reboot_mode mode, const char *cmd);
 
 void __init ux500_setup_id(void);
