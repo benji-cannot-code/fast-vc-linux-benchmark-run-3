cba4fa608baf8f2ff7a54e1f20004c3d092b1209
Arnd Bergmann <arnd@arndb.de>
2016-06-20 23:21:20 +0200

    ARM: ux500: call ux500_setup_id later
    ux500_setup_id is currently called from u8500_map_io(), which is
    really early, but nothing relies on the ID any more, other than
    a printk message that is not really all that important to
    have early during boot.
    
    If we move the call to ux500_setup_id() into ux500_soc_device_init(),
    that file becomes usuable almost entirely standalone, and we can kill
    off the u8500_map_io() callback as it just does the default
    debug_ll_io_init() now.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- arch/arm/mach-ux500/cpu-db8500.c
+++ arch/arm/mach-ux500/cpu-db8500.c
@@ -28,13 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "board-mop500.h"
 #include "db8500-regs.h"
-#include "id.h"
-
-static void __init u8500_map_io(void)
-{
-	debug_ll_io_init();
-	ux500_setup_id();
-}
 
 /*
  * The PMU IRQ lines of two cores are wired together into a single interrupt.
@@ -118,7 +111,6 @@ static const char * stericsson_dt_platform_compat[] = {
 DT_MACHINE_START(U8500_DT, "ST-Ericsson Ux5x0 platform (Device Tree Support)")
 	.l2c_aux_val    = 0,
 	.l2c_aux_mask	= ~0,
-	.map_io		= u8500_map_io,
 	.init_irq	= ux500_init_irq,
 	.init_machine	= u8500_init_machine,
 	.dt_compat      = stericsson_dt_platform_compat,
