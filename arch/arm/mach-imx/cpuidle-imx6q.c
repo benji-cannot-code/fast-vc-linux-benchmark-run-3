b6cc47816a61768248b88a4237ee9b3c3026cf08
Ben Dooks <ben.dooks@codethink.co.uk>
2016-06-21 13:13:11 +0100

    ARM: imx6: fix missing <soc/imx/cpuidle.h> in cpuidle-imx6q.c
    The <soc/imx/cpuidle.h> file has declarations of two funcitons
    exported from cpuidle-imx6q.c but it is not included. Fix the
    following warnings by adding the include:
    
    arch/arm/mach-imx/cpuidle-imx6q.c:71:6: warning: symbol 'imx6q_cpuidle_fec_irqs_used' was not declared. Should it be static?
    arch/arm/mach-imx/cpuidle-imx6q.c:76:6: warning: symbol 'imx6q_cpuidle_fec_irqs_unused' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Shawn Guo <shawnguo@kernel.org>

--- arch/arm/mach-imx/cpuidle-imx6q.c
+++ arch/arm/mach-imx/cpuidle-imx6q.c
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <asm/cpuidle.h>
 
+#include <soc/imx/cpuidle.h>
+
 #include "common.h"
 #include "cpuidle.h"
 #include "hardware.h"
