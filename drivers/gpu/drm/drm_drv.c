77f6cd5238b5ead07678bf38672bad7823c2cf64
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2016-06-21 16:37:09 +0200

    drm: Add callbacks for late registering
    Like what has been done for connectors add callbacks on encoder,
    crtc and plane to let driver do actions after drm device registration.
    
    Correspondingly, add callbacks called before unregister drm device.
    
    version 2:
    add drm_modeset_register_all() and drm_modeset_unregister_all()
    to centralize all calls
    
    version 3:
    in error case unwind registers in drm_modeset_register_all
    fix uninitialed return value
    inverse order of unregistration in drm_modeset_unregister_all
    
    version 4:
    move function definitions in drm_crtc_internal.h
    remove not needed documentation
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1466519829-4000-1-git-send-email-benjamin.gaignard@linaro.org

--- drivers/gpu/drm/drm_drv.c
+++ drivers/gpu/drm/drm_drv.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <drm/drmP.h>
 #include <drm/drm_core.h>
+#include "drm_crtc_internal.h"
 #include "drm_legacy.h"
 #include "drm_internal.h"
 
@@ -689,7 +690,7 @@ int drm_dev_register(struct drm_device *dev, unsigned long flags)
 	}
 
 	if (drm_core_check_feature(dev, DRIVER_MODESET))
-		drm_connector_register_all(dev);
+		drm_modeset_register_all(dev);
 
 	ret = 0;
 	goto out_unlock;
@@ -722,7 +723,7 @@ void drm_dev_unregister(struct drm_device *dev)
 	drm_lastclose(dev);
 
 	if (drm_core_check_feature(dev, DRIVER_MODESET))
-		drm_connector_unregister_all(dev);
+		drm_modeset_unregister_all(dev);
 
 	if (dev->driver->unload)
 		dev->driver->unload(dev);
