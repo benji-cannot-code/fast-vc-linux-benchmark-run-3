826be290b772869327c7791010930c696525375a
Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
2016-06-22 21:55:06 +0530

    powerpc/bpf/jit: Isolate classic BPF JIT specifics into a separate header
    Break out classic BPF JIT specifics into a separate header in
    preparation for eBPF JIT implementation. Note that ppc32 will still need
    the classic BPF JIT.
    
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/net/bpf_jit_comp.c
+++ arch/powerpc/net/bpf_jit_comp.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/filter.h>
 #include <linux/if_vlan.h>
 
-#include "bpf_jit.h"
+#include "bpf_jit32.h"
 
 int bpf_jit_enable __read_mostly;
 
