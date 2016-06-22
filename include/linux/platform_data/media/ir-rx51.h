6146cf25da6c3a997a37bf8fed93bbb5845016f8
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2016-06-22 22:22:21 +0300

    ir-rx51: use hrtimer instead of dmtimer
    Drop dmtimer usage for pulse timer in favor of hrtimer. That allows
    removing PWM dmitimer platform data usage.
    
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
    Acked-by: Pali Rohár <pali.rohar@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/linux/platform_data/media/ir-rx51.h
+++ include/linux/platform_data/media/ir-rx51.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct lirc_rx51_platform_data {
 	int(*set_max_mpu_wakeup_lat)(struct device *dev, long t);
-	struct pwm_omap_dmtimer_pdata *dmtimer;
 };
 
 #endif
