6cb6d9614edc0557b727c91140faa13c24447b02
Wang Nan <wangnan0@huawei.com>
2016-06-24 11:22:07 +0000

    perf data ctf: Pass convert options through opts structure
    Following commits will add new option to 'perf data convert'. All options
    should be grouped into a structure and passed to low level converter
    (currently there's only one converter).
    
    Introduce data-convert.h and define 'struct perf_data_convert_opts' in
    it. Pass 'force' through opts.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Zefan Li <lizefan@huawei.com>
    Cc: pi3orama@163.com
    Link: http://lkml.kernel.org/r/1466767332-114472-3-git-send-email-wangnan0@huawei.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

---
+++ tools/perf/util/data-convert.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __DATA_CONVERT_H
+#define __DATA_CONVERT_H
+
+struct perf_data_convert_opts {
+	bool force;
+};
+
+#endif /* __DATA_CONVERT_H */
