fc41cc3bf6dd4dd38fa6592386d28eadb2a251a9
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2016-06-27 00:03:00 +0200

    rtc: ds1286: move header to linux/rtc
    Move ds1286.h to rtc specific folder.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- drivers/rtc/rtc-ds1286.c
+++ drivers/rtc/rtc-ds1286.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rtc.h>
 #include <linux/platform_device.h>
 #include <linux/bcd.h>
-#include <linux/ds1286.h>
+#include <linux/rtc/ds1286.h>
 #include <linux/io.h>
 #include <linux/slab.h>
 
