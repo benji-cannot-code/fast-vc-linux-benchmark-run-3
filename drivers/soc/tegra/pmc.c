ff1d56f63e84d08c5f5482c6cbfeb5e64f93af92
Jon Hunter <jonathanh@nvidia.com>
2016-06-28 11:38:26 +0100

    soc/tegra: pmc: Ensure mutex is always initialised
    The mutex used by the PMC driver may not be initialised if early
    initialisation of the driver fails. If this does happen, then it could
    be possible for callers of the public PMC functions to still attempt to
    acquire the mutex. Fix this by initialising the mutex as soon as
    possible to ensure it will always be initialised.
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/soc/tegra/pmc.c
+++ drivers/soc/tegra/pmc.c
@@ -1505,6 +1505,8 @@ static int __init tegra_pmc_early_init(void)
 	bool invert;
 	u32 value;
 
+	mutex_init(&pmc->powergates_lock);
+
 	np = of_find_matching_node_and_match(NULL, tegra_pmc_match, &match);
 	if (!np) {
 		/*
@@ -1548,8 +1550,6 @@ static int __init tegra_pmc_early_init(void)
 		pr_err("failed to map PMC registers\n");
 		return -ENXIO;
 	}
-
-	mutex_init(&pmc->powergates_lock);
 
 	if (np) {
 		pmc->soc = match->data;
