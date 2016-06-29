bcdc8a1c4595dd7dad6b822ddce12edda873a0aa
Nipun Gupta <nipun.gupta@nxp.com>
2016-06-29 22:44:39 +0530

    fsl-mc: add helper macro to determine if a device is of fsl_mc type
    Add a helper macro to return if a device has a bus type of fsl_mc.
    This makes the bus driver code more readable and provides a way for
    drivers like the SMMU driver to easily check the bus type.
    
    Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>
    Signed-off-by: Bharat Bhushan <bharat.bhushan@nxp.com>
    Acked-by: Stuart Yoder <stuart.yoder@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/dprc-driver.c
+++ drivers/staging/fsl-mc/bus/dprc-driver.c
@@ -650,7 +650,7 @@ static int dprc_probe(struct fsl_mc_device *mc_dev)
 		/*
 		 * This is a child DPRC:
 		 */
-		if (WARN_ON(parent_dev->bus != &fsl_mc_bus_type))
+		if (WARN_ON(!dev_is_fsl_mc(parent_dev)))
 			return -EINVAL;
 
 		if (WARN_ON(mc_dev->obj_desc.region_count == 0))
@@ -682,7 +682,7 @@ static int dprc_probe(struct fsl_mc_device *mc_dev)
 		 */
 		struct irq_domain *mc_msi_domain;
 
-		if (WARN_ON(parent_dev->bus == &fsl_mc_bus_type))
+		if (WARN_ON(dev_is_fsl_mc(parent_dev)))
 			return -EINVAL;
 
 		error = fsl_mc_find_msi_domain(parent_dev,
