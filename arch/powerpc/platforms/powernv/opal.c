828c9b8688c37be482708a33f9292fa6a7852f24
Suraj Jitindar Singh <sjitindarsingh@gmail.com>
2016-06-29 13:38:39 +1000

    powerpc/powernv: Add driver for operator panel on FSP machines
    Implement new character device driver to allow access from user space
    to the operator panel display present on IBM Power Systems machines
    with FSPs.
    
    This will allow status information to be presented on the display which
    is visible to a user.
    
    The driver implements a character buffer which a user can read/write
    by accessing the device (/dev/op_panel). This buffer is then displayed on
    the operator panel display. Any attempt to write past the last character
    position will have no effect and attempts to write more characters than
    the size of the display will be truncated. The device may only be accessed
    by a single process at a time.
    
    Signed-off-by: Suraj Jitindar Singh <sjitindarsingh@gmail.com>
    Reviewed-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/opal.c
+++ arch/powerpc/platforms/powernv/opal.c
@@ -752,6 +752,9 @@ static int __init opal_init(void)
 	opal_pdev_init(opal_node, "ibm,opal-flash");
 	opal_pdev_init(opal_node, "ibm,opal-prd");
 
+	/* Initialise platform device: oppanel interface */
+	opal_pdev_init(opal_node, "ibm,opal-oppanel");
+
 	/* Initialise OPAL kmsg dumper for flushing console on panic */
 	opal_kmsg_init();
 
@@ -886,3 +889,5 @@ EXPORT_SYMBOL_GPL(opal_i2c_request);
 /* Export these symbols for PowerNV LED class driver */
 EXPORT_SYMBOL_GPL(opal_leds_get_ind);
 EXPORT_SYMBOL_GPL(opal_leds_set_ind);
+/* Export this symbol for PowerNV Operator Panel class driver */
+EXPORT_SYMBOL_GPL(opal_write_oppanel_async);
