e8f4c32999d1a6d09667d73be00319f0bc529b1b
Arnd Bergmann <arnd@arndb.de>
2016-06-29 16:33:17 +0200

    ASoC: cs35l33: mark PM functions as __maybe_unused
    The newly added cs35l33 driver produces a harmless warning when
    CONFIG_PM is disabled:
    
    sound/soc/codecs/cs35l33.c:908:12: error: 'cs35l33_runtime_suspend' defined but not used [-Werror=unused-function]
    sound/soc/codecs/cs35l33.c:868:12: error: 'cs35l33_runtime_resume' defined but not used [-Werror=unused-function]
    
    This adds __maybe_unused annotations to shut up the warning
    regardless of the configuration.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs35l33.c
+++ sound/soc/codecs/cs35l33.c
@@ -863,7 +863,7 @@ static const struct regmap_config cs35l33_regmap = {
 	.use_single_rw = true,
 };
 
-static int cs35l33_runtime_resume(struct device *dev)
+static int __maybe_unused cs35l33_runtime_resume(struct device *dev)
 {
 	struct cs35l33_private *cs35l33 = dev_get_drvdata(dev);
 	int ret;
@@ -903,7 +903,7 @@ err:
 	return ret;
 }
 
-static int cs35l33_runtime_suspend(struct device *dev)
+static int __maybe_unused cs35l33_runtime_suspend(struct device *dev)
 {
 	struct cs35l33_private *cs35l33 = dev_get_drvdata(dev);
 
