8a277baafd87949d5d3d8285de89f9d76771213f
Viresh Kumar <viresh.kumar@linaro.org>
2016-06-29 13:42:26 -0700

    greybus: light: Initialize mutex before using it
    Light protocol driver is suffering from the same issue that was fixed in
    camera driver earlier (commit a7c3b0c3c8da).
    
    Big cleanup function is used instead of fine grained control in the
    error path, and in one of the cases the mutex was found uninitialized
    and so the oops seen in SW-6752.
    
    Initialize the mutex before any code can access it.
    
    Compile tested only.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Rui Miguel Silva <rui.silva@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/light.c
+++ drivers/staging/greybus/light.c
@@ -1222,6 +1222,8 @@ static int gb_lights_probe(struct gb_bundle *bundle,
 	if (!glights)
 		return -ENOMEM;
 
+	mutex_init(&glights->lights_lock);
+
 	connection = gb_connection_create(bundle, le16_to_cpu(cport_desc->id),
 					  gb_lights_request_handler);
 	if (IS_ERR(connection)) {
@@ -1231,8 +1233,6 @@ static int gb_lights_probe(struct gb_bundle *bundle,
 
 	glights->connection = connection;
 	gb_connection_set_data(connection, glights);
-
-	mutex_init(&glights->lights_lock);
 
 	greybus_set_drvdata(bundle, glights);
 
