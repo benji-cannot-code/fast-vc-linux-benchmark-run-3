64cfa6bbb36f7d33cef71525f70fc1e06773beb4
Sara Sharon <sara.sharon@intel.com>
2016-06-29 12:08:48 +0300

    iwlwifi: pcie: fix ucode load flow for a000 devices
    Turns out we should access TFH relative addresses.
    Also, the FH_UCODE_LOAD_STATUS was replaced by
    UREG_UCODE_LOAD_STATUS.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/iwl-prph.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-prph.h
@@ -304,6 +304,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define FH_UCODE_LOAD_STATUS		(0x1AF0)
 #define CSR_UCODE_LOAD_STATUS_ADDR	(0x1E70)
+
+/*
+ * Replacing FH_UCODE_LOAD_STATUS
+ * This register is writen by driver and is read by uCode during boot flow.
+ * Note this address is cleared after MAC reset.
+ */
+#define UREG_UCODE_LOAD_STATUS		(0xa05c40)
+
 enum secure_load_status_reg {
 	LMPM_CPU_UCODE_LOADING_STARTED			= 0x00000001,
 	LMPM_CPU_HDRS_LOADING_COMPLETED			= 0x00000003,
