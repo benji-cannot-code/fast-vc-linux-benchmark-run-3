ff1d789f86ee1c0799ae0a82e68eb07a58be8844
Venkat Reddy Talla <vreddytalla@nvidia.com>
2016-06-30 17:54:00 +0900

    extcon: adc-jack: add suspend/resume support
    adding suspend and resume funtionality for extcon-adc-jack
    driver to configure system wake up for extcon events,
    also adding support to enable/disable system wakeup
    through flag wakeup_source based on platform requirement.
    
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- include/linux/extcon/extcon-adc-jack.h
+++ include/linux/extcon/extcon-adc-jack.h
@@ -54,6 +54,7 @@ struct adc_jack_cond {
  *			milli-seconds after the interrupt occurs. You may
  *			describe such delays with @handling_delay_ms, which
  *			is rounded-off by jiffies.
+ * @wakeup_source:	flag to wake up the system for extcon events.
  */
 struct adc_jack_pdata {
 	const char *name;
@@ -66,6 +67,7 @@ struct adc_jack_pdata {
 
 	unsigned long irq_flags;
 	unsigned long handling_delay_ms; /* in ms */
+	bool wakeup_source;
 };
 
 #endif /* _EXTCON_ADC_JACK_H */
