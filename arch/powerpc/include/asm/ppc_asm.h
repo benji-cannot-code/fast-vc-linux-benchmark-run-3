40838c22956387eea245d702b44646a588429f72
Anton Blanchard <anton@samba.org>
2016-07-01 08:19:44 +1000

    powerpc: define FUNC_START/FUNC_END
    gcc provides FUNC_START/FUNC_END macros to help with creating
    assembly functions. Mirror these in the kernel so we can more easily
    share code between userspace and the kernel. FUNC_END is just a
    stub since we don't currently annotate the end of kernel functions.
    
    It might make sense to do a wholesale search and replace, but for
    now just create a couple of defines.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/powerpc/include/asm/ppc_asm.h
+++ arch/powerpc/include/asm/ppc_asm.h
@@ -287,6 +287,9 @@ n:
 
 #endif
 
+#define FUNC_START(name)	_GLOBAL(name)
+#define FUNC_END(name)
+
 /* 
  * LOAD_REG_IMMEDIATE(rn, expr)
  *   Loads the value of the constant expression 'expr' into register 'rn'
