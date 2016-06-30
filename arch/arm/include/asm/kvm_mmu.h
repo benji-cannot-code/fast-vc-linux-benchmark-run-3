da67a928bc2c4ff428ad2853e507e5dc2b6d5096
Marc Zyngier <marc.zyngier@arm.com>
2016-06-30 18:40:46 +0100

    arm/arm64: KVM: Kill free_boot_hyp_pgd
    There is no way to free the boot PGD, because it doesn't exist
    anymore as a standalone entity.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/arm/include/asm/kvm_mmu.h
+++ arch/arm/include/asm/kvm_mmu.h
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int create_hyp_mappings(void *from, void *to, pgprot_t prot);
 int create_hyp_io_mappings(void *from, void *to, phys_addr_t);
-void free_boot_hyp_pgd(void);
 void free_hyp_pgds(void);
 
 void stage2_unmap_vm(struct kvm *kvm);
