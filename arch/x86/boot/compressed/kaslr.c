1b217566ce6917e356be07204876f42df46b6ff0
Baoquan He <bhe@redhat.com>
2016-07-01 15:34:40 +0800

    x86/KASLR: Fix boot crash with certain memory configurations
    Ye Xiaolong reported this boot crash:
    
    |
    |  XZ-compressed data is corrupt
    |
    |   -- System halted
    |
    
    Fix the bug in mem_avoid_overlap() of finding the earliest overlap.
    
    Reported-and-tested-by: Ye Xiaolong <xiaolong.ye@intel.com>
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/boot/compressed/kaslr.c
+++ arch/x86/boot/compressed/kaslr.c
@@ -286,6 +286,7 @@ static bool mem_avoid_overlap(struct mem_vector *img,
 		if (mem_overlaps(img, &mem_avoid[i]) &&
 		    mem_avoid[i].start < earliest) {
 			*overlap = mem_avoid[i];
+			earliest = overlap->start;
 			is_overlapping = true;
 		}
 	}
@@ -300,6 +301,7 @@ static bool mem_avoid_overlap(struct mem_vector *img,
 
 		if (mem_overlaps(img, &avoid) && (avoid.start < earliest)) {
 			*overlap = avoid;
+			earliest = overlap->start;
 			is_overlapping = true;
 		}
 
