ec76521199a8e8a5ececc31ed08812065a829bde
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-02 19:13:32 -0400

    PCI: thunder-ecam: Make explicitly non-modular
    This code is not being built as a module by anyone:
    
      drivers/pci/host/Kconfig:config PCI_HOST_THUNDER_ECAM
      drivers/pci/host/Kconfig:       bool "Cavium Thunder ECAM controller to on-chip devices on pass-1.x silicon"
    
    Remove uses of MODULE_DESCRIPTION(), MODULE_AUTHOR(), MODULE_LICENSE(),
    etc., so that when reading the driver there is no doubt it is builtin-only.
    The information is preserved in comments at the top of the file.
    
    Replace module_platform_driver() with builtin_platform_driver(), which uses
    the same init level priority, so init ordering is unchanged.
    
    Note that MODULE_DEVICE_TABLE is a no-op for non-modular code.
    
    [bhelgaas: changelog]
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    CC: David Daney <david.daney@cavium.com>

--- drivers/pci/host/pci-thunder-ecam.c
+++ drivers/pci/host/pci-thunder-ecam.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/kernel.h>
-#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/of_pci.h>
 #include <linux/of.h>
@@ -361,7 +361,6 @@ static const struct of_device_id thunder_ecam_of_match[] = {
 	{ .compatible = "cavium,pci-host-thunder-ecam" },
 	{ },
 };
-MODULE_DEVICE_TABLE(of, thunder_ecam_of_match);
 
 static int thunder_ecam_probe(struct platform_device *pdev)
 {
@@ -375,7 +374,4 @@ static struct platform_driver thunder_ecam_driver = {
 	},
 	.probe = thunder_ecam_probe,
 };
-module_platform_driver(thunder_ecam_driver);
-
-MODULE_DESCRIPTION("Thunder ECAM PCI host driver");
-MODULE_LICENSE("GPL v2");
+builtin_platform_driver(thunder_ecam_driver);
