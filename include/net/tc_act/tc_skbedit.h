c56593cec0faca581919e76e4ec05743411c7979
Jamal Hadi Salim <jhs@mojatatu.com>
2016-07-02 06:43:15 -0400

    net sched actions: skbedit add support for mod-ing skb pkt_type
    Extremely useful for setting packet type to host so i dont
    have to modify the dst mac address using pedit (which requires
    that i know the mac address)
    
    Example usage:
    tc filter add dev eth0 parent ffff: protocol ip pref 9 u32 \
    match ip src 5.5.5.5/32 \
    flowid 1:5 action skbedit ptype host
    
    This will tag all packets incoming from 5.5.5.5 with type
    PACKET_HOST
    
    Signed-off-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tc_act/tc_skbedit.h
+++ include/net/tc_act/tc_skbedit.h
@@ -25,11 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct tcf_skbedit {
 	struct tcf_common	common;
-	u32			flags;
-	u32     		priority;
-	u32     		mark;
-	u16			queue_mapping;
-	/* XXX: 16-bit pad here? */
+	u32		flags;
+	u32		priority;
+	u32		mark;
+	u16		queue_mapping;
+	u16		ptype;
 };
 #define to_skbedit(a) \
 	container_of(a->priv, struct tcf_skbedit, common)
