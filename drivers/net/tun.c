5a38600c6064beb1d0e217e1683b021514be642d
Jason Wang <jasowang@redhat.com>
2016-07-04 13:53:38 +0800

    tun: fix build warnings
    Stephen Rothwell reports a build warnings(powerpc ppc64_defconfig)
    
    drivers/net/tun.c: In function 'tun_do_read.part.5':
    /home/sfr/next/next/drivers/net/tun.c:1491:6: warning: 'err' may be
    used uninitialized in this function [-Wmaybe-uninitialized]
       int err;
    
    This is because tun_ring_recv() may return an uninitialized err, fix this.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1448,12 +1448,13 @@ static struct sk_buff *tun_ring_recv(struct tun_file *tfile, int noblock,
 {
 	DECLARE_WAITQUEUE(wait, current);
 	struct sk_buff *skb = NULL;
+	int error = 0;
 
 	skb = skb_array_consume(&tfile->tx_array);
 	if (skb)
 		goto out;
 	if (noblock) {
-		*err = -EAGAIN;
+		error = -EAGAIN;
 		goto out;
 	}
 
@@ -1465,11 +1466,11 @@ static struct sk_buff *tun_ring_recv(struct tun_file *tfile, int noblock,
 		if (skb)
 			break;
 		if (signal_pending(current)) {
-			*err = -ERESTARTSYS;
+			error = -ERESTARTSYS;
 			break;
 		}
 		if (tfile->socket.sk->sk_shutdown & RCV_SHUTDOWN) {
-			*err = -EFAULT;
+			error = -EFAULT;
 			break;
 		}
 
@@ -1480,6 +1481,7 @@ static struct sk_buff *tun_ring_recv(struct tun_file *tfile, int noblock,
 	remove_wait_queue(&tfile->wq.wait, &wait);
 
 out:
+	*err = error;
 	return skb;
 }
 
