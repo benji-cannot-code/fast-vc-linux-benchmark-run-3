dda7e48c8af66bd22f0eb59d91fe45886829df69
Florian Fainelli <f.fainelli@gmail.com>
2016-07-04 14:36:09 -0700

    net: r6040: Bump version and date
    Bump version to 0.28 and date to 4th of July 2016.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/rdc/r6040.c
+++ drivers/net/ethernet/rdc/r6040.c
@@ -49,8 +49,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 
 #define DRV_NAME	"r6040"
-#define DRV_VERSION	"0.28"
-#define DRV_RELDATE	"07Oct2011"
+#define DRV_VERSION	"0.29"
+#define DRV_RELDATE	"04Jul2016"
 
 /* Time in jiffies before concluding the transmitter is hung. */
 #define TX_TIMEOUT	(6000 * HZ / 1000)
