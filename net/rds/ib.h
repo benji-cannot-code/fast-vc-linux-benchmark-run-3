c23bedbccdda2c8bf1666fedbcf1bbdf998ae86c
Santosh Shilimkar <santosh.shilimkar@oracle.com>
2016-07-04 15:31:21 -0700

    RDS: IB: make the transport retry count smallest
    Transport retry is not much useful since it indicate packet loss
    in fabric so its better to failover fast rather than longer retry.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RDS_IB_DEFAULT_SEND_WR		256
 #define RDS_IB_DEFAULT_FR_WR		512
 
-#define RDS_IB_DEFAULT_RETRY_COUNT	2
+#define RDS_IB_DEFAULT_RETRY_COUNT	1
 
 #define RDS_IB_SUPPORTED_PROTOCOLS	0x00000003	/* minor versions supported */
 
