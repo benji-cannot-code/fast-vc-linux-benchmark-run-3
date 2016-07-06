126a84342744381bf9d8f56e93271621c307cbcf
Paolo Bonzini <pbonzini@redhat.com>
2016-07-06 14:57:24 +0200

    KVM: SVM: fix trashing of MSR_TSC_AUX
    I don't know what I was thinking when I wrote commit 46896c73c1a4 ("KVM:
    svm: add support for RDTSCP", 2015-11-12); I missed write_rdtscp_aux which
    obviously uses MSR_TSC_AUX.
    
    Therefore we do need to save/restore MSR_TSC_AUX in svm_vcpu_run.
    
    Cc: stable@vger.kernel.org
    Cc: Borislav Petkov <bp@alien8.de>
    Fixes: 46896c73c1a4 ("KVM: svm: add support for RDTSCP")
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/desc.h>
 #include <asm/debugreg.h>
 #include <asm/kvm_para.h>
+#include <asm/vgtod.h>
 
 #include <asm/virtext.h>
 #include "trace.h"
@@ -1531,9 +1532,6 @@ static void svm_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 			wrmsrl(MSR_AMD64_TSC_RATIO, tsc_ratio);
 		}
 	}
-	/* This assumes that the kernel never uses MSR_TSC_AUX */
-	if (static_cpu_has(X86_FEATURE_RDTSCP))
-		wrmsrl(MSR_TSC_AUX, svm->tsc_aux);
 
 	avic_vcpu_load(vcpu, cpu);
 }
@@ -4475,6 +4473,8 @@ static void svm_vcpu_run(struct kvm_vcpu *vcpu)
 	svm->vmcb->save.cr2 = vcpu->arch.cr2;
 
 	clgi();
+	if (static_cpu_has(X86_FEATURE_RDTSCP))
+		wrmsrl(MSR_TSC_AUX, svm->tsc_aux);
 
 	local_irq_enable();
 
@@ -4551,6 +4551,8 @@ static void svm_vcpu_run(struct kvm_vcpu *vcpu)
 #endif
 		);
 
+	if (static_cpu_has(X86_FEATURE_RDTSCP))
+		wrmsrl(MSR_TSC_AUX, __getcpu());
 #ifdef CONFIG_X86_64
 	wrmsrl(MSR_GS_BASE, svm->host.gs_base);
 #else
