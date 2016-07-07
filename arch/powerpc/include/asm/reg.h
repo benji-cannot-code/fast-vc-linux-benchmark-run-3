ab7fa9a06473fe3c0875f82b684a901dc9461687
Michael Ellerman <mpe@ellerman.id.au>
2016-07-07 22:54:30 +1000

    powerpc/xmon: Dump ISA 2.07 SPRs
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -269,6 +269,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define   DSISR_KEYFAULT	0x00200000	/* Key fault */
 #define SPRN_TBRL	0x10C	/* Time Base Read Lower Register (user, R/O) */
 #define SPRN_TBRU	0x10D	/* Time Base Read Upper Register (user, R/O) */
+#define SPRN_CIR	0x11B	/* Chip Information Register (hyper, R/0) */
 #define SPRN_TBWL	0x11C	/* Time Base Lower Register (super, R/W) */
 #define SPRN_TBWU	0x11D	/* Time Base Upper Register (super, R/W) */
 #define SPRN_TBU40	0x11E	/* Timebase upper 40 bits (hyper, R/W) */
