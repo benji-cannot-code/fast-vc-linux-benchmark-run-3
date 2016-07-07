b0ddcdcaa48166d6d5b300e613bbe5edd86ba646
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-07-07 06:52:10 -0300

    doc-rst: fix parsing comments and '{' on a separate line
    The dmx.h header has two things that causes the parser to
    break while handling enums:
     per-header enums and the '{' starts on a new line
    
    Both makes the parser to get lexical marks to be detected
    as if they were symbols.
    
    Fix it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- Documentation/sphinx/parse-headers.pl
+++ Documentation/sphinx/parse-headers.pl
@@ -30,9 +30,12 @@ my %structs;
 my $is_enum = 0;
 open IN, $file_in or die "Can't open $file_in";
 while (<IN>) {
+	my $ln = $_;
+	$ln =~ s,/\*.*\*/,,;
+
 	$data .= $_;
 
-	if ($is_enum && m/^\s*([^\s\}\,\=]+)\s*[\,=]?/) {
+	if ($is_enum && $ln =~ m/^\s*([_A-Z][^\s\}\,\=]+)\s*[\,=]?/) {
 		my $s = $1;
 		my $n = $1;
 		$n =~ tr/A-Z/a-z/;
@@ -45,7 +48,7 @@ while (<IN>) {
 	}
 	$is_enum = 0 if ($is_enum && m/\}/);
 
-	if (m/^\s*#\s*define\s+([_A-Z]\S+)\s+_IO/) {
+	if ($ln =~ m/^\s*#\s*define\s+([_A-Z]\S+)\s+_IO/) {
 		my $s = $1;
 		my $n = $1;
 		$n =~ tr/A-Z/a-z/;
@@ -54,7 +57,7 @@ while (<IN>) {
 		next;
 	}
 
-	if (m/^\s*#\s*define\s+([_A-Z]\S+)\s+/) {
+	if ($ln =~ m/^\s*#\s*define\s+([_A-Z]\S+)\s+/) {
 		my $s = $1;
 		my $n = $1;
 		$n =~ tr/A-Z/a-z/;
@@ -64,7 +67,7 @@ while (<IN>) {
 		next;
 	}
 
-	if (m/^\s*typedef\s+.*\s+([_\w]\S+);/) {
+	if ($ln =~ m/^\s*typedef\s+.*\s+([_\w]\S+);/) {
 		my $s = $1;
 		my $n = $1;
 		$n =~ tr/A-Z/a-z/;
@@ -73,7 +76,8 @@ while (<IN>) {
 		$typedefs{$s} = $n;
 		next;
 	}
-	if (m/^\s*enum\s+(\S+)\s+\{/ || m/^\s*enum\s+(\S+)$/) {
+	if ($ln =~ m/^\s*enum\s+(\S+)\s+\{/
+	    || $ln =~ m/^\s*enum\s+(\S+)$/) {
 		my $s = $1;
 		my $n = $1;
 		$n =~ tr/A-Z/a-z/;
@@ -84,7 +88,8 @@ while (<IN>) {
 		$is_enum = $1;
 		next;
 	}
-	if (m/^\s*struct\s+([_A-Za-z_]\S+)\s+\{/ || m/^\s*struct\s+([A-Za-z_]\S+)$/) {
+	if ($ln =~ m/^\s*struct\s+([_A-Za-z_]\S+)\s+\{/
+	    || $ln =~ m/^\s*struct\s+([A-Za-z_]\S+)$/) {
 		my $s = $1;
 		my $n = $1;
 		$n =~ tr/A-Z/a-z/;
