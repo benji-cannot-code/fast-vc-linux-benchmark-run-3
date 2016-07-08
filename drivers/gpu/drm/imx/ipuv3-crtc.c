0d3d1edcb34f50f1e89eefb18e82b6b71cec8e62
Liu Ying <gnuiyl@gmail.com>
2016-07-08 17:40:57 +0800

    drm/imx: atomic phase 2 step 2: Track plane_state->fb correctly in ->page_flip
    Use drm_atomic_set_fb_for_plane() in the legacy ->page_flip path to track
    the pointer plane_state->fb correctly.
    
    Signed-off-by: Liu Ying <gnuiyl@gmail.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/drm/imx/ipuv3-crtc.c
+++ drivers/gpu/drm/imx/ipuv3-crtc.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/platform_device.h>
 #include <drm/drmP.h>
+#include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
 #include <linux/fb.h>
@@ -217,6 +218,9 @@ static int ipu_page_flip(struct drm_crtc *crtc,
 	} else {
 		ipu_crtc->flip_state = IPU_FLIP_SUBMITTED;
 	}
+
+	if (crtc->primary->state)
+		drm_atomic_set_fb_for_plane(crtc->primary->state, fb);
 
 	return 0;
 
