e0e71c12f4977c7a919b1add84c75c4281049520
Ben Skeggs <bskeggs@redhat.com>
2016-07-09 10:41:01 +1000

    drm/nouveau/bios/gp104: initial support
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2185,6 +2185,7 @@ nv130_chipset = {
 static const struct nvkm_device_chip
 nv134_chipset = {
 	.name = "GP104",
+	.bios = nvkm_bios_new,
 	.mc = gp100_mc_new,
 	.pci = gp100_pci_new,
 	.timer = gk20a_timer_new,
