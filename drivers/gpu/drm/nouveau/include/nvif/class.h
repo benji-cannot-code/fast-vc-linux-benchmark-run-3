ea98956d2c43467ecda891541c22e88f31d4d005
Ben Skeggs <bskeggs@redhat.com>
2016-07-09 10:41:01 +1000

    drm/nouveau/gr/gp100: initial support
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvif/class.h
+++ drivers/gpu/drm/nouveau/include/nvif/class.h
@@ -109,6 +109,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAXWELL_A                                     /* cl9097.h */ 0x0000b097
 #define MAXWELL_B                                     /* cl9097.h */ 0x0000b197
 
+#define PASCAL_A                                      /* cl9097.h */ 0x0000c097
+
 #define NV74_BSP                                                     0x000074b0
 
 #define GT212_MSVLD                                                  0x000085b1
@@ -142,6 +144,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KEPLER_COMPUTE_B                                             0x0000a1c0
 #define MAXWELL_COMPUTE_A                                            0x0000b0c0
 #define MAXWELL_COMPUTE_B                                            0x0000b1c0
+#define PASCAL_COMPUTE_A                                             0x0000c0c0
 
 #define NV74_CIPHER                                                  0x000074c1
 #endif
--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2178,6 +2178,7 @@ nv130_chipset = {
 	.dma = gf119_dma_new,
 	.disp = gp100_disp_new,
 	.fifo = gp100_fifo_new,
+	.gr = gp100_gr_new,
 };
 
 static int
