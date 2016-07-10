e6b24da3c080f53bb3b07177f15622eba1318e21
Guenter Roeck <linux@roeck-us.net>
2016-07-10 09:43:21 -0700

    hwmon: (core) Order include files alphabetically
    Ordering include files alphabetically makes it easier to add new ones.
    Stop including linux/spinlock.h and linux/kdev_t.h since both are not
    needed.
    
    Reviewed-by: Jonathan Cameron <jic23@kernel.org>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/hwmon.c
+++ drivers/hwmon/hwmon.c
@@ -13,16 +13,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/module.h>
 #include <linux/device.h>
 #include <linux/err.h>
-#include <linux/slab.h>
-#include <linux/kdev_t.h>
-#include <linux/idr.h>
-#include <linux/hwmon.h>
 #include <linux/gfp.h>
-#include <linux/spinlock.h>
+#include <linux/hwmon.h>
+#include <linux/idr.h>
+#include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/slab.h>
 #include <linux/string.h>
 
 #define HWMON_ID_PREFIX "hwmon"
