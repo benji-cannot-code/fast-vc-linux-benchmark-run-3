b81b117aa72d59d3cc13cb3e9118da7739b3780c
Santosh Shilimkar <santosh.shilimkar@oracle.com>
2016-07-09 18:31:38 -0700

    RDS: IB: track and log active side endpoint in connection
    Useful to know the active and passive end points in a
    RDS IB connection.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -182,6 +182,9 @@ struct rds_ib_connection {
 
 	/* Batched completions */
 	unsigned int		i_unsignaled_wrs;
+
+	/* Endpoint role in connection */
+	bool			i_active_side;
 };
 
 /* This assumes that atomic_t is at least 32 bits */
