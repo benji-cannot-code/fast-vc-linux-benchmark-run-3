d104ed7096788ca337b0cc3be81adc2bfbcf4407
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-11 18:05:16 -0400

    ACPI / PMIC: remove modular references from non-modular code
    The Kconfig currently controlling compilation of these files are:
    
    drivers/acpi/Kconfig:menuconfig PMIC_OPREGION
    drivers/acpi/Kconfig:   bool "PMIC (Power Management Integrated Circuit) operation region support"
    
    drivers/acpi/Kconfig:config BXT_WC_PMIC_OPREGION
    drivers/acpi/Kconfig:   bool "ACPI operation region support for BXT WhiskeyCove PMIC"
    
    drivers/acpi/Kconfig:config XPOWER_PMIC_OPREGION
    drivers/acpi/Kconfig:   bool "ACPI operation region support for XPower AXP288 PMIC"
    
    ...meaning they currently are not being built as a module by anyone.
    
    Lets remove the couple traces of modular infrastructure use, so that
    when reading the code there is no doubt it is builtin-only.
    
    We delete the MODULE_LICENSE tag etc. since all that information
    is already contained at the top of the file in the comments.
    
    One file was using module_init.  Since module_init translates to
    device_initcall in the non-modular case, the init ordering remains
    unchanged with this commit.
    
    In one case we replace the module.h with export.h since that file
    is exporting some symbols, but does not use __init.  The other two
    are using __init and so module.h gets replaced with init.h there.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/pmic/intel_pmic.c
+++ drivers/acpi/pmic/intel_pmic.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
-#include <linux/module.h>
+#include <linux/export.h>
 #include <linux/acpi.h>
 #include <linux/regmap.h>
 #include <acpi/acpi_lpat.h>
@@ -320,5 +320,3 @@ out_error:
 	return ret;
 }
 EXPORT_SYMBOL_GPL(intel_pmic_install_opregion_handler);
-
-MODULE_LICENSE("GPL");
--- drivers/acpi/pmic/intel_pmic_bxtwc.c
+++ drivers/acpi/pmic/intel_pmic_bxtwc.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
-#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/acpi.h>
 #include <linux/mfd/intel_soc_pmic.h>
 #include <linux/regmap.h>
@@ -418,8 +418,4 @@ static int __init intel_bxtwc_pmic_opregion_driver_init(void)
 {
 	return platform_driver_register(&intel_bxtwc_pmic_opregion_driver);
 }
-
 device_initcall(intel_bxtwc_pmic_opregion_driver_init);
-
-MODULE_DESCRIPTION("BXT WhiskeyCove ACPI opregion driver");
-MODULE_LICENSE("GPL");
