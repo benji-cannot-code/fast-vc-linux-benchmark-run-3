defe5dc29c18f18044c74432e01ffd9a88906e7f
Linus Lüssing <linus.luessing@c0d3.blue>
2016-07-11 11:16:36 +0200

    batman-adv: Use bitwise instead of arithmetic operator for flags
    This silences the following coccinelle warning:
    
    "WARNING: sum of probable bitmasks, consider |"
    
    Signed-off-by: Linus Lüssing <linus.luessing@c0d3.blue>
    Signed-off-by: Marek Lindner <mareklindner@neomailbox.ch>
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/multicast.c
+++ net/batman-adv/multicast.c
@@ -529,7 +529,7 @@ update:
 	}
 
 	return !(mcast_data.flags &
-		 (BATADV_MCAST_WANT_ALL_IPV4 + BATADV_MCAST_WANT_ALL_IPV6));
+		 (BATADV_MCAST_WANT_ALL_IPV4 | BATADV_MCAST_WANT_ALL_IPV6));
 }
 
 /**
