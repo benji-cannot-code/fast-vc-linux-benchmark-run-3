69685f050405d74975c505fa3583f48ec9f9d387
Jiri Pirko <jiri@mellanox.com>
2016-07-12 18:05:03 +0200

    devlink: add hardware messages tracing facility
    Define a tracepoint and allow user to trace messages going to and from
    hardware associated with devlink instance.
    
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/devlink.c
+++ net/core/devlink.c
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/net_namespace.h>
 #include <net/sock.h>
 #include <net/devlink.h>
+#define CREATE_TRACE_POINTS
+#include <trace/events/devlink.h>
+
+EXPORT_TRACEPOINT_SYMBOL_GPL(devlink_hwmsg);
 
 static LIST_HEAD(devlink_list);
 
