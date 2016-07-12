094fe38ff885e4dddcd5ab121e21082f9c9610df
Karol Herbst <karolherbst@gmail.com>
2016-07-12 21:36:08 +0200

    drm/nouveau/volt: Parse the max voltage map entries
    There are at least three "max" entries, which specify the max voltage.
    Because they are actually normal voltage map entries, they can also be
    affected by the temperature.
    
    Nvidia respects those entries and if they get changed, nvidia uses the
    lower voltage from all three.
    
    We shouldn't exceed those voltages at any given time.
    
    v2: State what those entries do in the source.
    v3: Add the third max entry.
    v5: Better describe the entries.
    
    Signed-off-by: Karol Herbst <karolherbst@gmail.com>
    Reviewed-by: Martin Peres <martin.peres@free.fr>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/bios/vmap.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/bios/vmap.h
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NVBIOS_VMAP_H__
 #define __NVBIOS_VMAP_H__
 struct nvbios_vmap {
+	u8  max0;
+	u8  max1;
+	u8  max2;
 };
 
 u16 nvbios_vmap_table(struct nvkm_bios *, u8 *ver, u8 *hdr, u8 *cnt, u8 *len);
