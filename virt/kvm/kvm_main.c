5223d764b591288c1a5d72bbc0ebf6ebaa834257
Liu Shuo <shuo.a.liu@intel.com>
2016-07-12 17:38:42 +0800

    KVM: release anon file in failure path of vm creation
    The failure of create debugfs of VM will return directly without release
    the anon file. It will leak memory and file descriptors, even through
    be not serious.
    
    Signed-off-by: Liu Shuo <shuo.a.liu@intel.com>
    Fixes: 536a6f88c49dd739961ffd53774775afed852c83
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/sort.h>
 #include <linux/bsearch.h>
+#include <linux/syscalls.h>
 
 #include <asm/processor.h>
 #include <asm/io.h>
@@ -3070,6 +3071,7 @@ static int kvm_dev_ioctl_create_vm(unsigned long type)
 
 	if (kvm_create_vm_debugfs(kvm, r) < 0) {
 		kvm_put_kvm(kvm);
+		sys_close(r);
 		return -ENOMEM;
 	}
 
