f4c66c0e3f0c95ed1d3e205612462339c2f006b8
Ian Munsie <imunsie@au1.ibm.com>
2016-07-14 07:17:07 +1000

    cxl: Add support for using the kernel API with a real PHB
    This hooks up support for using the kernel API with a real PHB. After
    the AFU initialisation has completed it calls into the PHB code to pass
    it the AFU that will be used by other peer physical functions on the
    adapter.
    
    The cxl_pci_to_afu API is extended to work with peer PCI devices,
    retrieving the peer AFU from the PHB. This API may also now return an
    error if it is called on a PCI device that is not associated with either
    a cxl vPHB or a peer PCI device to an AFU, and this error is propagated
    down.
    
    Signed-off-by: Ian Munsie <imunsie@au1.ibm.com>
    Reviewed-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- drivers/misc/cxl/api.c
+++ drivers/misc/cxl/api.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/file.h>
 #include <misc/cxl.h>
 #include <linux/fs.h>
+#include <asm/pnv-pci.h>
 
 #include "cxl.h"
 
@@ -25,6 +26,8 @@ struct cxl_context *cxl_dev_context_init(struct pci_dev *dev)
 	int rc;
 
 	afu = cxl_pci_to_afu(dev);
+	if (IS_ERR(afu))
+		return ERR_CAST(afu);
 
 	ctx = cxl_context_alloc();
 	if (IS_ERR(ctx)) {
@@ -439,6 +442,8 @@ EXPORT_SYMBOL_GPL(cxl_perst_reloads_same_image);
 ssize_t cxl_read_adapter_vpd(struct pci_dev *dev, void *buf, size_t count)
 {
 	struct cxl_afu *afu = cxl_pci_to_afu(dev);
+	if (IS_ERR(afu))
+		return -ENODEV;
 
 	return cxl_ops->read_adapter_vpd(afu->adapter, buf, count);
 }
