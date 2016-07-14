ebd2f84aa82c94c6833926a45cdf03486663aef4
Philippe Reynes <tremyfr@gmail.com>
2016-07-14 23:44:52 +0200

    net: ethernet: pasemi_mac: use phydev from struct net_device
    The private structure contain a pointer to phydev, but the structure
    net_device already contain such pointer. So we can remove the pointer
    phy in the private structure, and update the driver to use the
    one contained in struct net_device.
    
    Signed-off-by: Philippe Reynes <tremyfr@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/pasemi/pasemi_mac_ethtool.c
+++ drivers/net/ethernet/pasemi/pasemi_mac_ethtool.c
@@ -67,8 +67,7 @@ static int
 pasemi_mac_ethtool_get_settings(struct net_device *netdev,
 			       struct ethtool_cmd *cmd)
 {
-	struct pasemi_mac *mac = netdev_priv(netdev);
-	struct phy_device *phydev = mac->phydev;
+	struct phy_device *phydev = netdev->phydev;
 
 	if (!phydev)
 		return -EOPNOTSUPP;
@@ -80,8 +79,7 @@ static int
 pasemi_mac_ethtool_set_settings(struct net_device *netdev,
 			       struct ethtool_cmd *cmd)
 {
-	struct pasemi_mac *mac = netdev_priv(netdev);
-	struct phy_device *phydev = mac->phydev;
+	struct phy_device *phydev = netdev->phydev;
 
 	if (!phydev)
 		return -EOPNOTSUPP;
