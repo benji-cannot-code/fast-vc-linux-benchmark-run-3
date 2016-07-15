971fc851d2a6823993ee2f715a36c06c0dea0f59
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-15 19:53:18 -0400

    x86/platform: Delete extraneous MODULE_* tags fromm ts5500
    This file doesn't do anything modular and hence while the tristate
    Kconfig used for the gpio portion is fine, it recently got swept up in
    an audit of files using the module.h header but not using any modular
    registration functions.
    
    However it is not compiled in any of the normal build coverage, and
    so some remaining extraneous MODULE macro use were not found until a
    randconfig from the kbuild robot came across it.
    
    Here we remove the remaining no-op MODULE macros from the built in
    portion of code relating to this Kconfig option.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Savoir-faire Linux Inc. <kernel@savoirfairelinux.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kbuild-all@01.org
    Cc: linux-kernel@vger.kernel.org
    Fixes: cc3ae7b0af27 ("x86/platform: Audit and remove any unnecessary uses of module.h")
    Link: http://lkml.kernel.org/r/20160715235318.GD10758@windriver.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/platform/ts5500/ts5500.c
+++ arch/x86/platform/ts5500/ts5500.c
@@ -346,7 +346,3 @@ error:
 	return err;
 }
 device_initcall(ts5500_init);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Savoir-faire Linux Inc. <kernel@savoirfairelinux.com>");
-MODULE_DESCRIPTION("Technologic Systems TS-5500 platform driver");
