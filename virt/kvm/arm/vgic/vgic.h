de295e59ffa2959315df3fde90e4427f61a3c89e
Andre Przywara <andre.przywara@arm.com>
2016-07-15 12:43:38 +0100

    KVM: arm64: vgic-its: Enable ITS emulation as a virtual MSI controller
    Now that all ITS emulation functionality is in place, we advertise
    MSI functionality to userland and also the ITS device to the guest - if
    userland has configured that.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Tested-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- virt/kvm/arm/vgic/vgic.h
+++ virt/kvm/arm/vgic/vgic.h
@@ -78,6 +78,7 @@ int vgic_v3_probe(const struct gic_kvm_info *info);
 int vgic_v3_map_resources(struct kvm *kvm);
 int vgic_register_redist_iodevs(struct kvm *kvm, gpa_t dist_base_address);
 bool vgic_has_its(struct kvm *kvm);
+int kvm_vgic_register_its_device(void);
 void vgic_enable_lpis(struct kvm_vcpu *vcpu);
 int vgic_its_inject_msi(struct kvm *kvm, struct kvm_msi *msi);
 #else
@@ -135,6 +136,11 @@ static inline int vgic_register_redist_iodevs(struct kvm *kvm,
 static inline bool vgic_has_its(struct kvm *kvm)
 {
 	return false;
+}
+
+static inline int kvm_vgic_register_its_device(void)
+{
+	return -ENODEV;
 }
 
 static inline void vgic_enable_lpis(struct kvm_vcpu *vcpu)
