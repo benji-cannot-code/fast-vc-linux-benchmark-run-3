e675d39e4fa21761fcbcf6ae50d308f7e85e4413
Archit Taneja <architt@codeaurora.org>
2016-07-18 08:44:04 +0530

    drm: rcar-du: Remove i2c slave encoder interface for hdmi encoder
    The hdmi output in rcar-du uses the i2c slave encoder interface to link
    to the adv7511 encoder chip. The kms driver creates encoder and connector
    entities that internally uses the drm_encoder_slave_funcs ops provided by
    the slave encoder driver.
    
    Change the driver such that it expects a bridge entity instead of a slave
    encoder. The hdmi connector code isn't needed anymore as we expect the
    adv7511 bridge driver to create/manage the connector.
    
    Note that the kms driver still expects a connector node for hdmi to be
    present in DT. This node has no connection to the connector created
    by the bridge driver.
    
    Tested-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/gpu/drm/rcar-du/rcar_du_encoder.c
+++ drivers/gpu/drm/rcar-du/rcar_du_encoder.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "rcar_du_drv.h"
 #include "rcar_du_encoder.h"
-#include "rcar_du_hdmicon.h"
 #include "rcar_du_hdmienc.h"
 #include "rcar_du_kms.h"
 #include "rcar_du_lvdscon.h"
@@ -175,7 +174,7 @@ int rcar_du_encoder_init(struct rcar_du_device *rcdu,
 		break;
 
 	case DRM_MODE_ENCODER_TMDS:
-		ret = rcar_du_hdmi_connector_init(rcdu, renc);
+		/* connector managed by the bridge driver */
 		break;
 
 	default:
