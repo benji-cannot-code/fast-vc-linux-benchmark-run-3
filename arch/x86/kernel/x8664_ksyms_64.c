9ef3e8c88cbe6432e229f162b4f37f2385cf5419
Stephen Rothwell <sfr@canb.auug.org.au>
2016-07-18 18:23:24 +1000

    x86/headers: Include spinlock_types.h in x8664_ksyms_64.c for missing spinlock_t
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-next@vger.kernel.org
    Fixes: 186f43608a5c ("x86/kernel: Audit and remove any unnecessary uses of module.h")
    Link: http://lkml.kernel.org/r/20160718182922.7b41f923@canb.auug.org.au
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/x8664_ksyms_64.c
+++ arch/x86/kernel/x8664_ksyms_64.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    All C exports should go in the respective C files. */
 
 #include <linux/export.h>
+#include <linux/spinlock_types.h>
 #include <linux/smp.h>
 
 #include <net/checksum.h>
