c420899d342f61acaee7cce29fa2533181ea4a0b
Wei Yongjun <yongjun_wei@trendmicro.com.cn>
2016-07-19 12:02:11 +0000

    x86/ce4100: Remove duplicated include from ce4100.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <yongjun_wei@trendmicro.com.cn>
    Cc: Paul Gortmaker <paul.gortmaker@windriver.com>
    Link: http://lkml.kernel.org/r/1468929731-8900-1-git-send-email-weiyj_lk@163.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/platform/ce4100/ce4100.c
+++ arch/x86/platform/ce4100/ce4100.c
@@ -12,11 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/irq.h>
-#include <linux/init.h>
 #include <linux/reboot.h>
 #include <linux/serial_reg.h>
 #include <linux/serial_8250.h>
-#include <linux/reboot.h>
 
 #include <asm/ce4100.h>
 #include <asm/prom.h>
