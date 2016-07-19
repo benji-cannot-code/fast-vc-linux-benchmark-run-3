82c69f38cb101f7d721b16290dd7784b5fb0222e
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2016-07-19 15:04:37 +0100

    greybus: timesync: reduce initial startup time
    Currently we have a long initial delay when initiating the first operation
    to synchronize the FrameTime. That made sense during development of the
    feature since we did synchronization asynchronously and wanted to give some
    grace time for new Interfaces to be added before initiating a new
    synchronization. On the PM runtime resume path though we do a synchronous
    restoration of FrameTime and in this case waiting for 1 second before
    initiating the first operation is clearly wrong.
    
    This patch reduces the initial operation time bringing the synchronous call
    time down by 990 milliseconds.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/timesync.c
+++ drivers/staging/greybus/timesync.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GB_TIMESYNC_DEFAULT_OFFSET_US		1000
 
 /* Work queue timers long, short and SVC strobe timeout */
-#define GB_TIMESYNC_DELAYED_WORK_LONG		msecs_to_jiffies(1000)
+#define GB_TIMESYNC_DELAYED_WORK_LONG		msecs_to_jiffies(10)
 #define GB_TIMESYNC_DELAYED_WORK_SHORT		msecs_to_jiffies(1)
 #define GB_TIMESYNC_MAX_WAIT_SVC		msecs_to_jiffies(5000)
 #define GB_TIMESYNC_KTIME_UPDATE		msecs_to_jiffies(1000)
