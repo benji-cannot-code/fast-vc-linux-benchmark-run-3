2ee419fbe50cc8685b5cd061c8003d70ed76947d
Maxime Ripard <maxime.ripard@free-electrons.com>
2016-07-19 15:17:27 +0200

    drm/sun4i: Store TCON's device structure pointer
    We will need to access TCON's struct device from outside of TCON's driver
    bind function. Store it in our private structure.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/gpu/drm/sun4i/sun4i_tcon.h
+++ drivers/gpu/drm/sun4i/sun4i_tcon.h
@@ -144,6 +144,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SUN4I_TCON_MAX_CHANNELS		2
 
 struct sun4i_tcon {
+	struct device			*dev;
 	struct drm_device		*drm;
 	struct regmap			*regs;
 
