1e21d7652f6d0aba2bf3803160c682359feaf521
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-18 20:30:30 -0400

    Revert "pinctrl: amd: make it explicitly non-modular"
    This reverts commit b8c2b10a9bc0272a20e096852f8fbbf361749dda.
    
    This patch was in my queue at the same time that a conversion of
    the same driver from bool --> tristate was pending and merged.
    
    That is commit 337ea0fb1535 ("pinctrl: Turn AMD support to tristate")
    
    Normally the conflict would show up in the build coverage I do,
    however in this case an avoidable instance of linux/module.h in
    linux/gpio/driver.h (!) causes the build failure to be masked and
    instead the tristate gets built-in even for selected "=m".
    
    In working on removing module.h from driver.h this issue was then
    revealed (along with other implicit module.h assumptions in gpio,
    and mfd -- which will be fixed separately.)
    
    Cc: linux-gpio@vger.kernel.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Acked-by: Jean Delvare <jdelvare@suse.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-amd.c
+++ drivers/pinctrl/pinctrl-amd.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/bug.h>
 #include <linux/kernel.h>
-#include <linux/init.h>
+#include <linux/module.h>
 #include <linux/spinlock.h>
 #include <linux/compiler.h>
 #include <linux/types.h>
@@ -829,18 +829,35 @@ out2:
 	return ret;
 }
 
+static int amd_gpio_remove(struct platform_device *pdev)
+{
+	struct amd_gpio *gpio_dev;
+
+	gpio_dev = platform_get_drvdata(pdev);
+
+	gpiochip_remove(&gpio_dev->gc);
+
+	return 0;
+}
+
 static const struct acpi_device_id amd_gpio_acpi_match[] = {
 	{ "AMD0030", 0 },
 	{ "AMDI0030", 0},
 	{ },
 };
+MODULE_DEVICE_TABLE(acpi, amd_gpio_acpi_match);
 
 static struct platform_driver amd_gpio_driver = {
 	.driver		= {
 		.name	= "amd_gpio",
-		.suppress_bind_attrs = true,
 		.acpi_match_table = ACPI_PTR(amd_gpio_acpi_match),
 	},
 	.probe		= amd_gpio_probe,
+	.remove		= amd_gpio_remove,
 };
-builtin_platform_driver(amd_gpio_driver);
+
+module_platform_driver(amd_gpio_driver);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Ken Xue <Ken.Xue@amd.com>, Jeff Wu <Jeff.Wu@amd.com>");
+MODULE_DESCRIPTION("AMD GPIO pinctrl driver");
