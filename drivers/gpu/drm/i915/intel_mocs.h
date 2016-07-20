ee72ce324ae695ca00b3e66e092fbddf9e5cd72c
Dave Gordon <david.s.gordon@intel.com>
2016-07-20 18:16:06 +0100

    drm/i915: rename 'ring' where it refers to an engine or engine_id
    'ring' is an old deprecated term for a GPU engine. Chris Wilson wants to
    use the name for what is currently known as an intel_ringbuffer, but it
    will be dreadfully confusing if some rings are ringbuffers but other
    rings are still engines. So this patch changes the names of a bunch of
    parameters called 'ring' to either 'engine' or 'engine_id' according to
    what they actually are.
    
    Signed-off-by: Dave Gordon <david.s.gordon@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1469034967-15840-3-git-send-email-david.s.gordon@intel.com
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_mocs.h
+++ drivers/gpu/drm/i915/intel_mocs.h
@@ -55,6 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int intel_rcs_context_init_mocs(struct drm_i915_gem_request *req);
 void intel_mocs_init_l3cc_table(struct drm_device *dev);
-int intel_mocs_init_engine(struct intel_engine_cs *ring);
+int intel_mocs_init_engine(struct intel_engine_cs *engine);
 
 #endif
