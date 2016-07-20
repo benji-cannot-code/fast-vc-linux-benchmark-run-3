5951c5da39d2c09193ad55b3173ce5b24d3e9f3d
Wei Yongjun <weiyj.lk@gmail.com>
2016-07-20 12:06:16 +0000

    hwmon: (ftsteutates) Remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/ftsteutates.c
+++ drivers/hwmon/ftsteutates.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/sysfs.h>
 #include <linux/uaccess.h>
-#include <linux/version.h>
 #include <linux/watchdog.h>
 
 #define FTS_DEVICE_ID_REG		0x0000
