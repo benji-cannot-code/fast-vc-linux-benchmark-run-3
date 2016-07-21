8b9e7c3a0bdbec9a4791df68e164fa3871e092eb
Johan Hovold <johan@hovoldconsulting.com>
2016-07-21 12:48:57 +0200

    greybus: interface: fix interface_id attribute group
    All interfaces, regardless of type, should have an interface_id
    attribute reflecting its position on the frame.
    
    This has been reported to cause an assertion failure in libmoduleutil
    for dummy modules.
    
    Testing done: Verified that the attribute is present for registered
    dummy interfaces.
    
    Reported-by: Gjorgji Rosikopulos <grosikopulos@mm-sol.com>
    Tested-by: Gjorgji Rosikopulos <grosikopulos@mm-sol.com>
    Reviewed-by: Patrick Titiano <ptitiano@baylibre.com>
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/interface.c
+++ drivers/staging/greybus/interface.c
@@ -588,7 +588,6 @@ static struct attribute *interface_unipro_attrs[] = {
 };
 
 static struct attribute *interface_greybus_attrs[] = {
-	&dev_attr_interface_id.attr,
 	&dev_attr_vendor_id.attr,
 	&dev_attr_product_id.attr,
 	&dev_attr_serial_number.attr,
@@ -604,6 +603,7 @@ static struct attribute *interface_power_attrs[] = {
 };
 
 static struct attribute *interface_common_attrs[] = {
+	&dev_attr_interface_id.attr,
 	&dev_attr_interface_type.attr,
 	NULL
 };
