18302a58a2c3cccf3ec2776c548312d35c3e4c8f
Joel Porquet <porquet_joel@projectara.com>
2016-07-21 15:57:38 -0700

    greybus: firmware.h: rename TFTF firmware filenames
    Replace prefix "ara_" with prefix "gmp_" for TFTF firmware filenames.
    
    Testing Done: compiled and flashed EVT2 (bootimage) and tested that
                  "gmp_" prefixed tftf files are expected. Pushed one from
    	      FDK and checked that it's properly loaded.
    
    Signed-off-by: Joel Porquet <porquet_joel@projectara.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Sandeep Patil <sspatil@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/firmware.h
+++ drivers/staging/greybus/firmware.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "greybus.h"
 
-#define FW_NAME_PREFIX	"ara_"
+#define FW_NAME_PREFIX	"gmp_"
 
 /* Length of the string in format: "FW_NAME_PREFIX""%08x_%08x_%08x_%08x_%s.tftf" */
 #define FW_NAME_LEN		56
