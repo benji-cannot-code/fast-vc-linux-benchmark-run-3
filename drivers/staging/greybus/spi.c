09872c4153ed38d7660945e7f8e181b3fff967f0
Viresh Kumar <viresh.kumar@linaro.org>
2016-07-21 14:45:06 -0700

    greybus: spilib: make spilib independent of gbphy
    spilib is used by multiple users currently (spi.c and fw-core.c) but
    commit aa52b62a0556 broke that hierarchy and introduced gbphy dependent
    code in spilib.
    
    This may have unreliable consequences as we are doing following
    operation unconditionally now:
    
    	gbphy_dev = to_gbphy_dev(spi->parent);
    	gbphy_runtime_get_sync(gbphy_dev);
    
    which may not go well when the parent is of type &bundle->dev
    (fw-core.c).
    
    This patch introduces spilib_ops and lets the users of the core register
    them. This shall have no functional change for the spi.c usecase and
    shall fix the unreliable results for the fw-core.c usecase.
    
    Tested by writing to mtd0 dev and verifying (with print messages) that
    the below routines are getting called for a gpbridge-test module.
    
    Fixes: aa52b62a0556 ("spi: Add runtime_pm support")
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/spi.c
+++ drivers/staging/greybus/spi.c
@@ -14,6 +14,27 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "gbphy.h"
 #include "spilib.h"
 
+#ifndef SPI_CORE_SUPPORT_PM
+static int gbphy_spi_prepare_transfer_hardware(struct device *dev)
+{
+	return gbphy_runtime_get_sync(to_gbphy_dev(dev));
+}
+
+static void gbphy_spi_unprepare_transfer_hardware(struct device *dev)
+{
+	gbphy_runtime_put_autosuspend(to_gbphy_dev(dev));
+}
+
+static struct spilib_ops __spilib_ops = {
+	.prepare_transfer_hardware = gbphy_spi_prepare_transfer_hardware,
+	.unprepare_transfer_hardware = gbphy_spi_unprepare_transfer_hardware,
+};
+
+static struct spilib_ops *spilib_ops = &__spilib_ops;
+#else
+static struct spilib_ops *spilib_ops = NULL;
+#endif
+
 static int gb_spi_probe(struct gbphy_device *gbphy_dev,
 			const struct gbphy_device_id *id)
 {
@@ -30,7 +51,7 @@ static int gb_spi_probe(struct gbphy_device *gbphy_dev,
 	if (ret)
 		goto exit_connection_destroy;
 
-	ret = gb_spilib_master_init(connection, &gbphy_dev->dev);
+	ret = gb_spilib_master_init(connection, &gbphy_dev->dev, spilib_ops);
 	if (ret)
 		goto exit_connection_disable;
 
--- drivers/staging/greybus/spilib.h
+++ drivers/staging/greybus/spilib.h
@@ -11,9 +11,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SPILIB_H
 #define __SPILIB_H
 
+struct device;
 struct gb_connection;
 
-int gb_spilib_master_init(struct gb_connection *connection, struct device *dev);
+struct spilib_ops {
+	int (*prepare_transfer_hardware)(struct device *dev);
+	void (*unprepare_transfer_hardware)(struct device *dev);
+};
+
+int gb_spilib_master_init(struct gb_connection *connection, struct device *dev, struct spilib_ops *ops);
 void gb_spilib_master_exit(struct gb_connection *connection);
 
 #endif /* __SPILIB_H */
