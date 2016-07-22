b109cf4daf1c0ac8949d4dc2f94ae35e6b6ba8ae
Wei Yongjun <weiyj.lk@gmail.com>
2016-07-22 14:05:32 +0000

    libcxgb: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/libcxgb/libcxgb_ppm.c
+++ drivers/net/ethernet/chelsio/libcxgb/libcxgb_ppm.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) DRV_NAME ": " fmt
 
 #include <linux/kernel.h>
-#include <linux/version.h>
 #include <linux/module.h>
 #include <linux/errno.h>
 #include <linux/types.h>
