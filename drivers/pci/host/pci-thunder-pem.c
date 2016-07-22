c6e5ea3c51db9eb6f6d4ba539ad7a28fa1d66f41
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-22 16:24:49 -0500

    PCI: thunder-pem: Make explicitly non-modular
    This code is not being built as a module by anyone:
    
      drivers/pci/host/Kconfig:config PCI_HOST_THUNDER_PEM
      drivers/pci/host/Kconfig:       bool "Cavium Thunder PCIe controller to off-chip devices"
    
    Remove uses of MODULE_DESCRIPTION(), MODULE_AUTHOR(), MODULE_LICENSE(),
    etc., so that when reading the driver there is no doubt it is builtin-only.
    The information is preserved in comments at the top of the file.
    
    Replace module_platform_driver() with builtin_platform_driver(), which uses
    the same init level priority, so init ordering is unchanged.
    
    Note that MODULE_DEVICE_TABLE is a no-op for non-modular code.
    
    [bhelgaas: changelog]
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    CC: David Daney <david.daney@cavium.com>

--- drivers/pci/host/pci-thunder-pem.c
+++ drivers/pci/host/pci-thunder-pem.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/kernel.h>
-#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/of_address.h>
 #include <linux/of_pci.h>
 #include <linux/platform_device.h>
@@ -347,7 +347,6 @@ static const struct of_device_id thunder_pem_of_match[] = {
 	{ .compatible = "cavium,pci-host-thunder-pem" },
 	{ },
 };
-MODULE_DEVICE_TABLE(of, thunder_pem_of_match);
 
 static int thunder_pem_probe(struct platform_device *pdev)
 {
@@ -361,7 +360,4 @@ static struct platform_driver thunder_pem_driver = {
 	},
 	.probe = thunder_pem_probe,
 };
-module_platform_driver(thunder_pem_driver);
-
-MODULE_DESCRIPTION("Thunder PEM PCIe host driver");
-MODULE_LICENSE("GPL v2");
+builtin_platform_driver(thunder_pem_driver);
