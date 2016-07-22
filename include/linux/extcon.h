df0a095d896a74ec2824ea8e06d79d6b1698fb1b
Chanwoo Choi <cw00.choi@samsung.com>
2016-07-22 13:16:34 +0900

    extcon: Add the synchronization extcon APIs to support the notification
    This patch adds the synchronization extcon APIs to support the notifications
    for both state and property. When extcon_*_sync() functions is called,
    the extcon informs the information from extcon provider to extcon client.
    
    The extcon driver may need to change the both state and multiple properties
    at the same time. After setting the data of a external connector,
    the extcon send the notification to client driver with the extcon_*_sync().
    
    The list of new extcon APIs as following:
    - extcon_sync() : Send the notification for each external connector to
    		synchronize the information between extcon provider driver
    		and extcon client driver.
    - extcon_set_state_sync() : Set the state of external connector with noti.
    - extcon_set_property_sync() : Set the property of external connector with noti.
    
    For example,
    case 1, change the state of external connector and synchronized the data.
    	extcon_set_state_sync(edev, EXTCON_USB, 1);
    
    case 2, change both the state and property of external connector
    	and synchronized the data.
    	extcon_set_state(edev, EXTCON_USB, 1);
    	extcon_set_property(edev, EXTCON_USB, EXTCON_PROP_USB_VBUS 1);
    	extcon_sync(edev, EXTCON_USB);
    
    case 3, change the property of external connector and synchronized the data.
    	extcon_set_property(edev, EXTCON_USB, EXTCON_PROP_USB_VBUS, 0);
    	extcon_sync(edev, EXTCON_USB);
    
    case 4, change the property of external connector and synchronized the data.
    	extcon_set_property_sync(edev, EXTCON_USB, EXTCON_PROP_USB_VBUS, 0);
    
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>
    Tested-by: Chris Zhong <zyw@rock-chips.com>
    Tested-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- include/linux/extcon.h
+++ include/linux/extcon.h
@@ -223,6 +223,13 @@ extern void devm_extcon_dev_free(struct device *dev, struct extcon_dev *edev);
 extern int extcon_get_state(struct extcon_dev *edev, unsigned int id);
 extern int extcon_set_state(struct extcon_dev *edev, unsigned int id,
 				   bool cable_state);
+extern int extcon_set_state_sync(struct extcon_dev *edev, unsigned int id,
+				bool cable_state);
+
+/*
+ * Synchronize the state and property data for a specific external connector.
+ */
+extern int extcon_sync(struct extcon_dev *edev, unsigned int id);
 
 /*
  * get/set_property access the property value of each external connector.
@@ -232,6 +239,9 @@ extern int extcon_get_property(struct extcon_dev *edev, unsigned int id,
 				unsigned int prop,
 				union extcon_property_value *prop_val);
 extern int extcon_set_property(struct extcon_dev *edev, unsigned int id,
+				unsigned int prop,
+				union extcon_property_value prop_val);
+extern int extcon_set_property_sync(struct extcon_dev *edev, unsigned int id,
 				unsigned int prop,
 				union extcon_property_value prop_val);
 
@@ -318,6 +328,17 @@ static inline int extcon_set_state(struct extcon_dev *edev, unsigned int id,
 	return 0;
 }
 
+static inline int extcon_set_state_sync(struct extcon_dev *edev, unsigned int id,
+				bool cable_state)
+{
+	return 0;
+}
+
+static inline int extcon_sync(struct extcon_dev *edev, unsigned int id)
+{
+	return 0;
+}
+
 static inline int extcon_get_property(struct extcon_dev *edev, unsigned int id,
 					unsigned int prop,
 					union extcon_property_value *prop_val)
@@ -326,6 +347,13 @@ static inline int extcon_get_property(struct extcon_dev *edev, unsigned int id,
 }
 static inline int extcon_set_property(struct extcon_dev *edev, unsigned int id,
 					unsigned int prop,
+					union extcon_property_value prop_val)
+{
+	return 0;
+}
+
+static inline int extcon_set_property_sync(struct extcon_dev *edev,
+					unsigned int id, unsigned int prop,
 					union extcon_property_value prop_val)
 {
 	return 0;
@@ -412,6 +440,6 @@ static inline int extcon_get_cable_state_(struct extcon_dev *edev, unsigned int 
 static inline int extcon_set_cable_state_(struct extcon_dev *edev, unsigned int id,
 				   bool cable_state)
 {
-	return extcon_set_state(edev, id, cable_state);
+	return extcon_set_state_sync(edev, id, cable_state);
 }
 #endif /* __LINUX_EXTCON_H__ */
