4bf6b38604b09c6a0332798457258f6ab32da32d
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-23 14:01:45 -0400

    sh: migrate exception table users off module.h and onto extable.h
    These files were only including module.h for exception table
    related functions.  We've now separated that content out into its
    own file "extable.h" so now move over to that and avoid all the
    extra header content in module.h that we don't really need to compile
    these files.
    
    One uses "print_modules" so that prevents us removing module.h in
    that case, however.
    
    We also delete a duplicate prototype that doesn't need to exist, as
    it duplicates content in extable.h
    
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: Rich Felker <dalias@libc.org>
    Cc: linux-sh@vger.kernel.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- arch/sh/mm/extable_64.c
+++ arch/sh/mm/extable_64.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * for more details.
  */
 #include <linux/rwsem.h>
-#include <linux/module.h>
+#include <linux/extable.h>
 #include <linux/uaccess.h>
 
 extern unsigned long copy_user_memcpy, copy_user_memcpy_end;
