4aa19efd9a71014e4d4add9b1ef234c75230f165
Johan Hovold <johan@kernel.org>
2016-07-24 14:10:58 +0200

    memory: omap-gpmc: allow probe of child nodes to fail
    A recent commit (inadvertently?) changed how failed probe of a gpmc
    child node was handled. Instead of proceeding with setting up any other
    children as before, a single error now aborts the whole process.
    
    This change broke networking on some Overo boards due to probe failing
    for an unrelated nand node. This second issue should obviously be
    fixed, but let's restore the old behaviour of allowing child-node
    probe to fail to avoid further similar breakage on other systems.
    
    Fixes: d2d00862dfbb ("memory: omap-gpmc: Support general purpose input
    for WAITPINs")
    
    Cc: stable@vger.kernel.org  # v4.7+
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Roger Quadros <rogerq@ti.com>

--- drivers/memory/omap-gpmc.c
+++ drivers/memory/omap-gpmc.c
@@ -2186,7 +2186,7 @@ static int gpmc_probe_dt(struct platform_device *pdev)
 	return 0;
 }
 
-static int gpmc_probe_dt_children(struct platform_device *pdev)
+static void gpmc_probe_dt_children(struct platform_device *pdev)
 {
 	int ret;
 	struct device_node *child;
@@ -2201,11 +2201,11 @@ static int gpmc_probe_dt_children(struct platform_device *pdev)
 		else
 			ret = gpmc_probe_generic_child(pdev, child);
 
-		if (ret)
-			return ret;
+		if (ret) {
+			dev_err(&pdev->dev, "failed to probe DT child '%s': %d\n",
+				child->name, ret);
+		}
 	}
-
-	return 0;
 }
 #else
 static int gpmc_probe_dt(struct platform_device *pdev)
@@ -2213,9 +2213,8 @@ static int gpmc_probe_dt(struct platform_device *pdev)
 	return 0;
 }
 
-static int gpmc_probe_dt_children(struct platform_device *pdev)
+static void gpmc_probe_dt_children(struct platform_device *pdev)
 {
-	return 0;
 }
 #endif /* CONFIG_OF */
 
@@ -2370,16 +2369,10 @@ static int gpmc_probe(struct platform_device *pdev)
 		goto setup_irq_failed;
 	}
 
-	rc = gpmc_probe_dt_children(pdev);
-	if (rc < 0) {
-		dev_err(gpmc->dev, "failed to probe DT children\n");
-		goto dt_children_failed;
-	}
+	gpmc_probe_dt_children(pdev);
 
 	return 0;
 
-dt_children_failed:
-	gpmc_free_irq(gpmc);
 setup_irq_failed:
 	gpmc_gpio_exit(gpmc);
 gpio_init_failed:
