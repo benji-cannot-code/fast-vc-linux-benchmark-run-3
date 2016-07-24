5a4f948abac0c109ef022fa8e1d12c363ed5c9a9
Guenter Roeck <linux@roeck-us.net>
2016-07-23 17:00:15 -0700

    MIPS: ath79: Add missing include file
    Commit ddd0ce87bfde ("mips: Remove unnecessary of_platform_populate with
    default match table") dropped the include of linux/clk-provider.h from
    arch/mips/ath79/setup.c. This results in the following build error.
    
    arch/mips/ath79/setup.c: In function 'ath79_of_plat_time_init':
    arch/mips/ath79/setup.c:232:2: error:
    	implicit declaration of function 'of_clk_init'
    
    Fixes: ddd0ce87bfde ("mips: Remove unnecessary of_platform_populate with default match table")
    Cc: Rob Herring <robh@kernel.org>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- arch/mips/ath79/setup.c
+++ arch/mips/ath79/setup.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bootmem.h>
 #include <linux/err.h>
 #include <linux/clk.h>
+#include <linux/clk-provider.h>
 #include <linux/of_fdt.h>
 
 #include <asm/bootinfo.h>
