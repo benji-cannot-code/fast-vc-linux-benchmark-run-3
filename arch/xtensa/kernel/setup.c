4620f9ca97fa0fc6187f3f163d09c079801eab2e
Guenter Roeck <linux@roeck-us.net>
2016-07-23 17:24:55 -0700

    xtensa: Fix build error due to missing include file
    Commit 69d99e6c0d621f ("xtensa: Remove unnecessary of_platform_populate
    with default match table") dropped various include files from
    arch/xtensa/kernel/setup.c. This results in the following build error.
    
    arch/xtensa/kernel/setup.c: In function ‘xtensa_dt_io_area’:
    arch/xtensa/kernel/setup.c:213:2: error:
    	implicit declaration of function ‘of_read_ulong’
    
    Fixes: 69d99e6c0d621f ("xtensa: Remove unnecessary of_platform_populate with default match table")
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Rob Herring <robh@kernel.org>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Max Filippov <jcmvbkbc@gmail.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- arch/xtensa/kernel/setup.c
+++ arch/xtensa/kernel/setup.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/percpu.h>
 #include <linux/cpu.h>
+#include <linux/of.h>
 #include <linux/of_fdt.h>
 
 #if defined(CONFIG_VGA_CONSOLE) || defined(CONFIG_DUMMY_CONSOLE)
