826e7e4c6b597a035ba7f8db42f05e9240542748
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-24 20:51:44 -0400

    module.h: remove extable.h include now users have migrated
    With hopefully most/all users of module.h that were looking for
    exception table functions moved over to the new extable.h header,
    we can remove the back-compat include that let us transition
    without introducing build regressions.
    
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Jessica Yu <jeyu@redhat.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- include/linux/module.h
+++ include/linux/module.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/moduleparam.h>
 #include <linux/jump_label.h>
 #include <linux/export.h>
-#include <linux/extable.h>	/* only as arch move module.h -> extable.h */
 #include <linux/rbtree_latch.h>
 
 #include <linux/percpu.h>
