49a465ba6aa210f21b09d0851d4729c6b17abae4
Steven J. Hill <sjhill@bethel-hill.org>
2016-07-25 15:44:21 -0500

    MIPS: Octeon: Remove forced mappings of USB interrupts.
    Get rid of unnecessary forced interrupt mappings for
    the USB host controller on OCTEON II.
    
    Signed-off-by: Steven J. Hill <steven.hill@cavium.com>
    Acked-by: David Daney <david.daney@cavium.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/13824/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/cavium-octeon/octeon-irq.c
+++ arch/mips/cavium-octeon/octeon-irq.c
@@ -1543,10 +1543,6 @@ static int __init octeon_irq_init_ciu(
 			goto err;
 	}
 
-	r = octeon_irq_force_ciu_mapping(ciu_domain, OCTEON_IRQ_USB0, 0, 56);
-	if (r)
-		goto err;
-
 	r = octeon_irq_force_ciu_mapping(ciu_domain, OCTEON_IRQ_TWSI2, 0, 59);
 	if (r)
 		goto err;
@@ -1559,10 +1555,6 @@ static int __init octeon_irq_init_ciu(
 		if (r)
 			goto err;
 	}
-
-	r = octeon_irq_force_ciu_mapping(ciu_domain, OCTEON_IRQ_USB1, 1, 17);
-	if (r)
-		goto err;
 
 	/* Enable the CIU lines */
 	set_c0_status(STATUSF_IP3 | STATUSF_IP2);
@@ -2077,10 +2069,6 @@ static int __init octeon_irq_init_ciu2(
 		if (r)
 			goto err;
 	}
-
-	r = octeon_irq_force_ciu_mapping(ciu_domain, OCTEON_IRQ_USB0, 3, 44);
-	if (r)
-		goto err;
 
 	for (i = 0; i < 4; i++) {
 		r = octeon_irq_force_ciu_mapping(
