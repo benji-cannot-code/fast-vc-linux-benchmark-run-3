34567ec9b05e436bd11c250625a4363ac3bb98b9
Amitkumar Karwar <akarwar@marvell.com>
2016-07-26 19:31:44 +0530

    mwifiex: add PCIe function level reset support
    This patch implements pre and post FLR handlers to support PCIe FLR
    functionality. Software cleanup is performed in pre-FLR whereas
    firmware is downloaded and software is re-initialised in
    post-FLR handler.
    
    Following command triggers FLR.
    echo "1" > /sys/bus/pci/devices/$NUMBER/reset
    
    This feature can be used as a recovery mechanism when firmware gets
    hang.
    
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/mwifiex/pcie.h
+++ drivers/net/wireless/marvell/mwifiex/pcie.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCIEUART8997_FW_NAME_V2 "mrvl/pcieuart8997_combo_v2.bin"
 #define PCIEUSB8997_FW_NAME_Z "mrvl/pcieusb8997_combo.bin"
 #define PCIEUSB8997_FW_NAME_V2 "mrvl/pcieusb8997_combo_v2.bin"
+#define PCIE8997_DEFAULT_WIFIFW_NAME "mrvl/pcie8997_wlan.bin"
 
 #define PCIE_VENDOR_ID_MARVELL              (0x11ab)
 #define PCIE_VENDOR_ID_V2_MARVELL           (0x1b4b)
