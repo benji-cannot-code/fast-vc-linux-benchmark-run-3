e952bc50ff16878685f6d90ead14dd42cf22a78b
Minchan Kim <minchan@kernel.org>
2016-07-26 15:23:23 -0700

    zsmalloc: introduce zspage structure
    We have squeezed meta data of zspage into first page's descriptor.  So,
    to get meta data from subpage, we should get first page first of all.
    But it makes trouble to implment page migration feature of zsmalloc
    because any place where to get first page from subpage can be raced with
    first page migration.  IOW, first page it got could be stale.  For
    preventing it, I have tried several approahces but it made code
    complicated so finally, I concluded to separate metadata from first
    page.  Of course, it consumes more memory.  IOW, 16bytes per zspage on
    32bit at the moment.  It means we lost 1% at *worst case*(40B/4096B)
    which is not bad I think at the cost of maintenance.
    
    Link: http://lkml.kernel.org/r/1464736881-24886-9-git-send-email-minchan@kernel.org
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/compaction.c
+++ mm/compaction.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/backing-dev.h>
 #include <linux/sysctl.h>
 #include <linux/sysfs.h>
-#include <linux/balloon_compaction.h>
 #include <linux/page-isolation.h>
 #include <linux/kasan.h>
 #include <linux/kthread.h>
