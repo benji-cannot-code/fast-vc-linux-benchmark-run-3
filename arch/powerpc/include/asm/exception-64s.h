7aa2ddb231bf08f3a773ee26fdbf3f56ac6ffea0
Michael Ellerman <mpe@ellerman.id.au>
2016-07-26 15:29:29 +1000

    powerpc/64: Correct comment on LOAD_HANDLER()
    The comment for LOAD_HANDLER() was wrong. The part about kdump has not
    been true since 1f6a93e4c35e ("powerpc: Make it possible to move the
    interrupt handlers away from the kernel").
    
    Describe how it currently works, and combine the two separate comments
    into one.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Reviewed-by: Nick Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/exception-64s.h
+++ arch/powerpc/include/asm/exception-64s.h
@@ -85,12 +85,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * We're short on space and time in the exception prolog, so we can't
- * use the normal SET_REG_IMMEDIATE macro. Normally we just need the
- * low halfword of the address, but for Kdump we need the whole low
- * word.
+ * use the normal LOAD_REG_IMMEDIATE macro to load the address of label.
+ * Instead we get the base of the kernel from paca->kernelbase and or in the low
+ * part of label. This requires that the label be within 64KB of kernelbase, and
+ * that kernelbase be 64K aligned.
  */
 #define LOAD_HANDLER(reg, label)					\
-	/* Handlers must be within 64K of kbase, which must be 64k aligned */ \
 	ori	reg,reg,(label)-_stext;	/* virt addr of handler ... */
 
 /* Exception register prefixes */
