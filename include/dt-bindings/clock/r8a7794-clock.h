df3c0dc5182e160f4e9d14cdfe5d0a68eb7472f6
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2016-07-27 23:59:59 +0300

    ARM: dts: r8a7794: add MSTP5 clocks
    Add some MSTP5 clocks to the R8A7794 device tree.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- include/dt-bindings/clock/r8a7794-clock.h
+++ include/dt-bindings/clock/r8a7794-clock.h
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define R8A7794_CLK_IRQC		7
 
 /* MSTP5 */
+#define R8A7794_CLK_AUDIO_DMAC0		2
 #define R8A7794_CLK_PWM			23
 
 /* MSTP7 */
