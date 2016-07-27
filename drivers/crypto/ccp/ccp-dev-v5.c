2b4ac724bb6056454c285103c65662363e2706fb
Gary R Hook <gary.hook@amd.com>
2016-07-26 19:10:40 -0500

    crypto: ccp - Enable DMA service on a v5 CCP
    Every CCP is capable of providing general DMA services.
    Register the device as a provider.
    
    Signed-off-by: Gary R Hook <gary.hook@amd.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccp/ccp-dev-v5.c
+++ drivers/crypto/ccp/ccp-dev-v5.c
@@ -833,6 +833,11 @@ static int ccp5_init(struct ccp_device *ccp)
 	if (ret)
 		goto e_kthread;
 
+	/* Register the DMA engine support */
+	ret = ccp_dmaengine_register(ccp);
+	if (ret)
+		goto e_hwrng;
+
 	return 0;
 
 e_kthread:
@@ -856,6 +861,9 @@ static void ccp5_destroy(struct ccp_device *ccp)
 	struct ccp_cmd_queue *cmd_q;
 	struct ccp_cmd *cmd;
 	unsigned int i;
+
+	/* Unregister the DMA engine */
+	ccp_dmaengine_unregister(ccp);
 
 	/* Unregister the RNG */
 	ccp_unregister_rng(ccp);
