50a5692270cf8ac53fa293311e0d96f456e72bd5
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-07-27 23:11:47 -0400

    extable.h: add stddef.h so "NULL" definition is not implicit
    While not an issue now, eventually we will have independent users of
    the extable.h file and we will stop sourcing it via module.h header.
    
    In testing that pending work, with very sparse builds, characteristic
    of an "allnoconfig" on various architectures, we can sometimes hit an
    instance where the very basic standard definitions aren't present,
    resulting in:
    
     include/linux/extable.h:26:9: error: 'NULL' undeclared (first use in this function)
    
    To be clear, this isn't a regression, since currently extable.h is
    only used by module.h -- however, we will need this addition present
    before we start migrating exception table users off module.h and onto
    extable.h during the next release cycle.
    
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/linux/extable.h
+++ include/linux/extable.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_EXTABLE_H
 #define _LINUX_EXTABLE_H
 
+#include <linux/stddef.h>	/* for NULL */
+
 struct module;
 struct exception_table_entry;
 
