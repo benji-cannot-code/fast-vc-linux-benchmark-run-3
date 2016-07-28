b743bc6ffabf06ed0241f3576f35c47dc0f0b87b
Rex Zhu <Rex.Zhu@amd.com>
2016-07-28 16:51:47 +0800

    drm/amdgpu: add module parameters to ctrl powerplay feature
    Signed-off-by: Rex Zhu <Rex.Zhu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/amd_powerplay.c
+++ drivers/gpu/drm/amd/powerplay/amd_powerplay.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "eventmanager.h"
 #include "pp_debug.h"
 
+
 #define PP_CHECK(handle)						\
 	do {								\
 		if ((handle) == NULL || (handle)->pp_valid != PP_VALID)	\
