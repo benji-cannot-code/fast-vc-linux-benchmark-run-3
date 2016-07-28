8a10cd648b1ad4209f0911d4a40c9fec88171a66
Anshuman Khandual <khandual@linux.vnet.ibm.com>
2016-07-28 10:57:38 +0800

    powerpc/ptrace: Enable support for NT_PPC_CVMX
    This patch enables support for TM checkpointed VMX register
    set ELF core note NT_PPC_CVMX based ptrace requests through
    PTRACE_GETREGSET, PTRACE_SETREGSET calls. This is achieved
    through adding a register set REGSET_CVMX in powerpc
    corresponding to the ELF core note section added. It
    implements the get, set and active functions for this new
    register set added.
    
    Signed-off-by: Anshuman Khandual <khandual@linux.vnet.ibm.com>
    Signed-off-by: Simon Guo <wei.guo.simon@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/uapi/asm/elf.h
+++ arch/powerpc/include/uapi/asm/elf.h
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ELF_NGREG	48	/* includes nip, msr, lr, etc. */
 #define ELF_NFPREG	33	/* includes fpscr */
+#define ELF_NVMX	34	/* includes all vector registers */
 
 typedef unsigned long elf_greg_t64;
 typedef elf_greg_t64 elf_gregset_t64[ELF_NGREG];
