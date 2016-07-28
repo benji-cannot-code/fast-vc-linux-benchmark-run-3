6984b225d1d5cd2a953f1ae2fce14d0cf3c33f69
Rich Felker <dalias@libc.org>
2016-07-28 19:21:10 +0000

    sh: add J2 atomics using the cas.l instruction
    Signed-off-by: Rich Felker <dalias@libc.org>

--- arch/sh/include/asm/barrier.h
+++ arch/sh/include/asm/barrier.h
@@ -30,6 +30,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define wmb()		mb()
 #define ctrl_barrier()	__icbi(PAGE_OFFSET)
 #else
+#if defined(CONFIG_CPU_J2) && defined(CONFIG_SMP)
+#define __smp_mb()	do { int tmp = 0; __asm__ __volatile__ ("cas.l %0,%0,@%1" : "+r"(tmp) : "z"(&tmp) : "memory", "t"); } while(0)
+#define __smp_rmb()	__smp_mb()
+#define __smp_wmb()	__smp_mb()
+#endif
 #define ctrl_barrier()	__asm__ __volatile__ ("nop;nop;nop;nop;nop;nop;nop;nop")
 #endif
 
