be0306def45358400624fc4e2fa28586cfde49d9
Anshuman Khandual <khandual@linux.vnet.ibm.com>
2016-07-28 10:57:40 +0800

    powerpc/ptrace: Enable support for TM SPR state
    This patch enables support for TM SPR state related ELF core
    note NT_PPC_TM_SPR based ptrace requests through PTRACE_GETREGSET,
    PTRACE_SETREGSET calls. This is achieved through adding a register
    set REGSET_TM_SPR in powerpc corresponding to the ELF core note
    section added. It implements the get, set and active functions for
    this new register set added.
    
    Signed-off-by: Anshuman Khandual <khandual@linux.vnet.ibm.com>
    Signed-off-by: Simon Guo <wei.guo.simon@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/uapi/asm/elf.h
+++ arch/powerpc/include/uapi/asm/elf.h
@@ -94,6 +94,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ELF_NFPREG	33	/* includes fpscr */
 #define ELF_NVMX	34	/* includes all vector registers */
 #define ELF_NVSX	32	/* includes all VSX registers */
+#define ELF_NTMSPRREG	3	/* include tfhar, tfiar, texasr */
 
 typedef unsigned long elf_greg_t64;
 typedef elf_greg_t64 elf_gregset_t64[ELF_NGREG];
