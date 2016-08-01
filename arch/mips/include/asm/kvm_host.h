8d7304c8419ac9d716e49879e78a3ca387a97adf
James Hogan <james.hogan@imgtec.com>
2016-08-01 09:07:52 +0100

    KVM: MIPS: Override HVA error values for EVA
    MIPS Enhanced Virtual Addressing (EVA) allows the user mode and kernel
    mode address spaces to overlap, breaking the assumption that PAGE_OFFSET
    is an appropriate KVM HVA error value, since PAGE_OFFSET may be as low
    as zero.
    
    Fix this in the same way that s390 does in commit bf640876e21f ("KVM:
    s390: Make KVM_HVA_ERR_BAD usable on s390"), by overriding
    KVM_HVA_ERR_[RO_]BAD and kvm_is_error_hva() in asm/kvm_host.h.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: "Radim Krčmář" <rkrcmar@redhat.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: linux-mips@linux-mips.org
    Cc: kvm@vger.kernel.org

--- arch/mips/include/asm/kvm_host.h
+++ arch/mips/include/asm/kvm_host.h
@@ -108,6 +108,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KVM_INVALID_INST		0xdeadbeef
 #define KVM_INVALID_ADDR		0xdeadbeef
 
+/*
+ * EVA has overlapping user & kernel address spaces, so user VAs may be >
+ * PAGE_OFFSET. For this reason we can't use the default KVM_HVA_ERR_BAD of
+ * PAGE_OFFSET.
+ */
+
+#define KVM_HVA_ERR_BAD			(-1UL)
+#define KVM_HVA_ERR_RO_BAD		(-2UL)
+
+static inline bool kvm_is_error_hva(unsigned long addr)
+{
+	return IS_ERR_VALUE(addr);
+}
+
 extern atomic_t kvm_mips_instance;
 
 struct kvm_vm_stat {
