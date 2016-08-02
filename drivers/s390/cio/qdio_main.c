eb0097597943df1683aeaac49306c5efd35d227f
Sebastian Ott <sebott@linux.vnet.ibm.com>
2016-08-02 16:01:39 +0200

    s390/qdio: remove checks for ccw device internal state
    Prior to starting IO qdio checks for the internal state of the ccw
    device. These checks happen without locking, so consistency between
    state evaluation and starting of the IO is not guaranteed.
    
    Since the internal state is checked during ccw_device_start it is
    safe to get rid of these additional checks.
    
    Signed-off-by: Sebastian Ott <sebott@linux.vnet.ibm.com>
    Reviewed-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/cio/qdio_main.c
+++ drivers/s390/cio/qdio_main.c
@@ -1353,9 +1353,6 @@ int qdio_establish(struct qdio_initialize *init_data)
 	if (!irq_ptr)
 		return -ENODEV;
 
-	if (cdev->private->state != DEV_STATE_ONLINE)
-		return -EINVAL;
-
 	mutex_lock(&irq_ptr->setup_mutex);
 	qdio_setup_irq(init_data);
 
@@ -1425,9 +1422,6 @@ int qdio_activate(struct ccw_device *cdev)
 	irq_ptr = cdev->private->qdio_data;
 	if (!irq_ptr)
 		return -ENODEV;
-
-	if (cdev->private->state != DEV_STATE_ONLINE)
-		return -EINVAL;
 
 	mutex_lock(&irq_ptr->setup_mutex);
 	if (irq_ptr->state == QDIO_IRQ_STATE_INACTIVE) {
