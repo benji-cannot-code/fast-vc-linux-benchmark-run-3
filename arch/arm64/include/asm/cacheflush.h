694e033c31bfca4e3a6e0640832d7dd744854854
Kwangwoo Lee <kwangwoo.lee@sk.com>
2016-08-02 09:50:50 +0900

    arm64: mm: convert __dma_* routines to use start, size
    __dma_* routines have been converted to use start and size instread of
    start and end addresses. The patch was origianlly for adding
    __clean_dcache_area_poc() which will be used in pmem driver to clean
    dcache to the PoC(Point of Coherency) in arch_wb_cache_pmem().
    
    The functionality of __clean_dcache_area_poc()  was equivalent to
    __dma_clean_range(). The difference was __dma_clean_range() uses the end
    address, but __clean_dcache_area_poc() uses the size to clean.
    
    Thus, __clean_dcache_area_poc() has been revised with a fallthrough
    function of __dma_clean_range() after the change that __dma_* routines
    use start and size instead of using start and end.
    
    As a consequence of using start and size, the name of __dma_* routines
    has also been altered following the terminology below:
        area: takes a start and size
        range: takes a start and end
    
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Kwangwoo Lee <kwangwoo.lee@sk.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/cacheflush.h
+++ arch/arm64/include/asm/cacheflush.h
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void flush_cache_range(struct vm_area_struct *vma, unsigned long start, unsigned long end);
 extern void flush_icache_range(unsigned long start, unsigned long end);
 extern void __flush_dcache_area(void *addr, size_t len);
+extern void __clean_dcache_area_poc(void *addr, size_t len);
 extern void __clean_dcache_area_pou(void *addr, size_t len);
 extern long __flush_cache_user_range(unsigned long start, unsigned long end);
 
@@ -86,7 +87,7 @@ static inline void flush_cache_page(struct vm_area_struct *vma,
  */
 extern void __dma_map_area(const void *, size_t, int);
 extern void __dma_unmap_area(const void *, size_t, int);
-extern void __dma_flush_range(const void *, const void *);
+extern void __dma_flush_area(const void *, size_t);
 
 /*
  * Copy user data from/to a page which is mapped into a different
