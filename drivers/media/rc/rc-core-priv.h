197d5dc8fc33ca00a68bf89df6523eb75355fd49
Heiner Kallweit <hkallweit1@gmail.com>
2016-08-02 02:44:07 -0300

    [media] rc: refactor raw handler kthread
    I think we can get rid of the spinlock protecting the kthread from being
    interrupted by a wakeup in certain parts.
    Even with the current implementation of the kthread the only lost wakeup
    scenario could happen if the wakeup occurs between the kfifo_len check
    and setting the state to TASK_INTERRUPTIBLE.
    
    In the changed version we could lose a wakeup if it occurs between
    processing the fifo content and setting the state to TASK_INTERRUPTIBLE.
    This scenario is covered by an additional check for available events in
    the fifo and setting the state to TASK_RUNNING in this case.
    
    In addition the changed version flushes the kfifo before ending
    when the kthread is stopped.
    
    With this patch we gain:
    - Get rid of the spinlock
    - Simplify code
    - Don't grep / release the mutex for each individual event but just once
      for the complete fifo content. This reduces overhead if a driver e.g.
      triggers processing after writing the content of a hw fifo to the kfifo.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/rc/rc-core-priv.h
+++ drivers/media/rc/rc-core-priv.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	MAX_IR_EVENT_SIZE	512
 
 #include <linux/slab.h>
-#include <linux/spinlock.h>
 #include <media/rc-core.h>
 
 struct ir_raw_handler {
@@ -38,7 +37,6 @@ struct ir_raw_handler {
 struct ir_raw_event_ctrl {
 	struct list_head		list;		/* to keep track of raw clients */
 	struct task_struct		*thread;
-	spinlock_t			lock;
 	/* fifo for the pulse/space durations */
 	DECLARE_KFIFO(kfifo, struct ir_raw_event, MAX_IR_EVENT_SIZE);
 	ktime_t				last_event;	/* when last event occurred */
