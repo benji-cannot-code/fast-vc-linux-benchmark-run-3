62d657494a4ffe3a45fbd9123c5dd66fbdf73372
Vegard Nossum <vegard.nossum@oracle.com>
2016-08-02 14:02:22 -0700

    mm: fail prefaulting if page table allocation fails
    I ran into this:
    
        BUG: sleeping function called from invalid context at mm/page_alloc.c:3784
        in_atomic(): 0, irqs_disabled(): 0, pid: 1434, name: trinity-c1
        2 locks held by trinity-c1/1434:
         #0:  (&mm->mmap_sem){......}, at: [<ffffffff810ce31e>] __do_page_fault+0x1ce/0x8f0
         #1:  (rcu_read_lock){......}, at: [<ffffffff81378f86>] filemap_map_pages+0xd6/0xdd0
    
        CPU: 0 PID: 1434 Comm: trinity-c1 Not tainted 4.7.0+ #58
        Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS Ubuntu-1.8.2-1ubuntu1 04/01/2014
        Call Trace:
          dump_stack+0x65/0x84
          panic+0x185/0x2dd
          ___might_sleep+0x51c/0x600
          __might_sleep+0x90/0x1a0
          __alloc_pages_nodemask+0x5b1/0x2160
          alloc_pages_current+0xcc/0x370
          pte_alloc_one+0x12/0x90
          __pte_alloc+0x1d/0x200
          alloc_set_pte+0xe3e/0x14a0
          filemap_map_pages+0x42b/0xdd0
          handle_mm_fault+0x17d5/0x28b0
          __do_page_fault+0x310/0x8f0
          trace_do_page_fault+0x18d/0x310
          do_async_page_fault+0x27/0xa0
          async_page_fault+0x28/0x30
    
    The important bits from the above is that filemap_map_pages() is calling
    into the page allocator while holding rcu_read_lock (sleeping is not
    allowed inside RCU read-side critical sections).
    
    According to Kirill Shutemov, the prefaulting code in do_fault_around()
    is supposed to take care of this, but missing error handling means that
    the allocation failure can go unnoticed.
    
    We don't need to return VM_FAULT_OOM (or any other error) here, since we
    can just let the normal fault path try again.
    
    Fixes: 7267ec008b5c ("mm: postpone page table allocation until we have page to map")
    Link: http://lkml.kernel.org/r/1469708107-11868-1-git-send-email-vegard.nossum@oracle.com
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: "Hillf Danton" <hillf.zj@alibaba-inc.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -3134,6 +3134,8 @@ static int do_fault_around(struct fault_env *fe, pgoff_t start_pgoff)
 
 	if (pmd_none(*fe->pmd)) {
 		fe->prealloc_pte = pte_alloc_one(fe->vma->vm_mm, fe->address);
+		if (!fe->prealloc_pte)
+			goto out;
 		smp_wmb(); /* See comment in __pte_alloc() */
 	}
 
