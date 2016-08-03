3d47797c5b8debc933e0f82b13d67fff8c8ec78d
Dan Carpenter <dan.carpenter@oracle.com>
2016-08-03 22:05:00 +0300

    nfsd: remove some dead code in nfsd_create_locked()
    We changed this around in f135af1041f ('nfsd: reorganize nfsd_create')
    so "dchild" can't be an error pointer any more.  Also, dchild can't be
    NULL here (and dput would already handle this even if it was).
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -1142,7 +1142,7 @@ nfsd_create_locked(struct svc_rqst *rqstp, struct svc_fh *fhp,
 		char *fname, int flen, struct iattr *iap,
 		int type, dev_t rdev, struct svc_fh *resfhp)
 {
-	struct dentry	*dentry, *dchild = NULL;
+	struct dentry	*dentry, *dchild;
 	struct inode	*dirp;
 	__be32		err;
 	__be32		err2;
@@ -1209,8 +1209,7 @@ nfsd_create_locked(struct svc_rqst *rqstp, struct svc_fh *fhp,
 	if (!err)
 		err = fh_update(resfhp);
 out:
-	if (dchild && !IS_ERR(dchild))
-		dput(dchild);
+	dput(dchild);
 	return err;
 
 out_nfserr:
