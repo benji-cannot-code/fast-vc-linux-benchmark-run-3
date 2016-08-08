eaa0cc1dd92a02b7d4c4623f4bbf3b3cc6a43f2c
Antti Palosaari <crope@iki.fi>
2016-08-08 20:13:45 -0300

    [media] cxd2820r: dvbv5 statistics for DVB-T
    Implement dvbv5 statistics for DVB-T.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/cxd2820r_priv.h
+++ drivers/media/dvb-frontends/cxd2820r_priv.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_math.h"
 #include "cxd2820r.h"
 #include <linux/gpio.h>
+#include <linux/math64.h>
 
 struct reg_val_mask {
 	u32 reg;
@@ -41,6 +42,7 @@ struct cxd2820r_priv {
 	struct i2c_adapter *i2c;
 	struct dvb_frontend fe;
 	struct cxd2820r_config cfg;
+	u64 post_bit_error;
 
 	bool ber_running;
 
