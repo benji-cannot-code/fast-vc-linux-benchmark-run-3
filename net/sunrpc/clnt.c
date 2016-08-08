822c79cd3aa582842523deaf5d397ba0c02a7b5b
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2016-08-08 14:43:49 +0530

    sunrpc: Remove unnecessary variable
    The variable `err` is not used anywhere and just returns the
    predefined value `0` at the end of the function. Hence, remove the
    variable and return 0 explicitly.
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/clnt.c
+++ net/sunrpc/clnt.c
@@ -185,7 +185,6 @@ static int __rpc_clnt_handle_event(struct rpc_clnt *clnt, unsigned long event,
 				   struct super_block *sb)
 {
 	struct dentry *dentry;
-	int err = 0;
 
 	switch (event) {
 	case RPC_PIPEFS_MOUNT:
@@ -202,7 +201,7 @@ static int __rpc_clnt_handle_event(struct rpc_clnt *clnt, unsigned long event,
 		printk(KERN_ERR "%s: unknown event: %ld\n", __func__, event);
 		return -ENOTSUPP;
 	}
-	return err;
+	return 0;
 }
 
 static int __rpc_pipefs_event(struct rpc_clnt *clnt, unsigned long event,
