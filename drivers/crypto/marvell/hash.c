636bb198bc1840f49af7b47f37c8eb73bcd6e880
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-08-09 11:03:16 +0200

    crypto: marvell - turn mv_cesa_ahash_init() into a function returning void
    The mv_cesa_ahash_init() function always returns 0, and the return
    value is anyway never checked. Turn it into a function returning void.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/marvell/hash.c
+++ drivers/crypto/marvell/hash.c
@@ -375,7 +375,7 @@ static const struct mv_cesa_req_ops mv_cesa_ahash_req_ops = {
 	.complete = mv_cesa_ahash_complete,
 };
 
-static int mv_cesa_ahash_init(struct ahash_request *req,
+static void mv_cesa_ahash_init(struct ahash_request *req,
 			      struct mv_cesa_op_ctx *tmpl, bool algo_le)
 {
 	struct mv_cesa_ahash_req *creq = ahash_request_ctx(req);
@@ -391,8 +391,6 @@ static int mv_cesa_ahash_init(struct ahash_request *req,
 	creq->op_tmpl = *tmpl;
 	creq->len = 0;
 	creq->algo_le = algo_le;
-
-	return 0;
 }
 
 static inline int mv_cesa_ahash_cra_init(struct crypto_tfm *tfm)
