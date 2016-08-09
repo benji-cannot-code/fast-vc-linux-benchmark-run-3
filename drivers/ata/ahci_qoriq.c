bd460e65954080cd20160dc898280d8b877e83d0
Tang Yuantian <Yuantian.Tang@nxp.com>
2016-08-09 09:51:21 +0800

    ahci: qoriq: adjust sata parameter
    The default values for Port Phy2Cfg register and
    Port Phy3Cfg register are better, no need to overwrite them.
    
    Signed-off-by: Tang Yuantian <yuantian.tang@nxp.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/ahci_qoriq.c
+++ drivers/ata/ahci_qoriq.c
@@ -45,10 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define SATA_ECC_DISABLE	0x00020000
 
-/* for ls1043a */
-#define LS1043A_PORT_PHY2	0x28184d1f
-#define LS1043A_PORT_PHY3	0x0e081509
-
 enum ahci_qoriq_type {
 	AHCI_LS1021A,
 	AHCI_LS1043A,
@@ -167,8 +163,6 @@ static int ahci_qoriq_phy_init(struct ahci_host_priv *hpriv)
 
 	case AHCI_LS1043A:
 		writel(AHCI_PORT_PHY_1_CFG, reg_base + PORT_PHY1);
-		writel(LS1043A_PORT_PHY2, reg_base + PORT_PHY2);
-		writel(LS1043A_PORT_PHY3, reg_base + PORT_PHY3);
 		writel(AHCI_PORT_TRANS_CFG, reg_base + PORT_TRANS);
 		break;
 
