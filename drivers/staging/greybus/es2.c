6305fc84ba0e9175a8479d87a697693dd741dfd4
Johan Hovold <johan@hovoldconsulting.com>
2016-08-10 12:58:41 +0200

    greybus: arpc: move arpc definitions to their own header
    Move the ARPC definitions to their own header.
    
    ARPC is not part of greybus, but is rather an implementation-specific
    means of communicating with a certain class of host-device hardware.
    
    Note that the same is true for the APBA USB vendor requests, but we keep
    them in the greybus header for the time being.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/es2.c
+++ drivers/staging/greybus/es2.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <asm/unaligned.h>
 
+#include "arpc.h"
 #include "greybus.h"
 #include "greybus_trace.h"
 #include "kernel_ver.h"
