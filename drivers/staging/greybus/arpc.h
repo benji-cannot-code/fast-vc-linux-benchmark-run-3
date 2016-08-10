9024615f56e6d2ca6c3a6d4aa8632f44e12a0c48
Greg Kroah-Hartman <gregkh@google.com>
2016-08-10 17:02:05 +0200

    greybus: fix up copyright dates on arpc.h
    I got them wrong, Johan was right, my fault.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/arpc.h
+++ drivers/staging/greybus/arpc.h
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * GPL LICENSE SUMMARY
  *
- * Copyright(c) 2014-2016 Google Inc. All rights reserved.
- * Copyright(c) 2014-2016 Linaro Ltd. All rights reserved.
+ * Copyright(c) 2016 Google Inc. All rights reserved.
+ * Copyright(c) 2016 Linaro Ltd. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * BSD LICENSE
  *
- * Copyright(c) 2014-2016 Google Inc. All rights reserved.
- * Copyright(c) 2014-2016 Linaro Ltd. All rights reserved.
+ * Copyright(c) 2016 Google Inc. All rights reserved.
+ * Copyright(c) 2016 Linaro Ltd. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
