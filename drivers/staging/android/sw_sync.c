9a1d925835762c73c73bd2f9c995991b5174104e
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2016-08-11 12:26:41 -0300

    staging/android: move trace/sync.h to sync_trace.h
    The common behaviour for trace headers is to have them in the same folder
    they are used, instead of creating a special trace/ directory.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/sw_sync.c
+++ drivers/staging/android/sw_sync.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sync_debug.h"
 
 #define CREATE_TRACE_POINTS
-#include "trace/sync.h"
+#include "sync_trace.h"
 
 struct sw_sync_create_fence_data {
 	__u32	value;
