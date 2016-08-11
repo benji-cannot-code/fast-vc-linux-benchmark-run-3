1dc1ee8b41aab425a260f2b8a1e083a9fe2d15b9
Viresh Kumar <viresh.kumar@linaro.org>
2016-08-11 13:27:25 -0700

    greybus: firmware: add comment to show size calculations
    The firmware file name's maximum length is set to 56 right now, but it
    isn't very much readable how we got to that value (unless someone reads
    that line).
    
    Update the comment to show calculations.
    
    Reported-by: Alex Elder <alex.elder@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/firmware.h
+++ drivers/staging/greybus/firmware.h
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define FW_NAME_PREFIX	"gmp_"
 
-/* Length of the string in format: "FW_NAME_PREFIX""%08x_%08x_%08x_%08x_%s.tftf" */
+/*
+ * Length of the string in format: "FW_NAME_PREFIX""%08x_%08x_%08x_%08x_%s.tftf"
+ *                                  (3 + 1 + 4 * (8 + 1) + 10 + 1 + 4 + 1)
+ */
 #define FW_NAME_SIZE		56
 
 /* Firmware Management Protocol specific functions */
