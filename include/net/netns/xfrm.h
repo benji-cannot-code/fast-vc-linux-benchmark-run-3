449a2494f20851ca1ea11826ce73620d49d7c48a
Florian Westphal <fw@strlen.de>
2016-08-11 15:17:56 +0200

    xfrm: policy: make xfrm_policy_lookup_bytype lockless
    side effect: no longer disables BH (should be fine).
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- include/net/netns/xfrm.h
+++ include/net/netns/xfrm.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ctl_table_header;
 
 struct xfrm_policy_hash {
-	struct hlist_head	*table;
+	struct hlist_head	__rcu *table;
 	unsigned int		hmask;
 	u8			dbits4;
 	u8			sbits4;
