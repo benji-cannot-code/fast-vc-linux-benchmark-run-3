1cc1e55536cf63b51555d9e115fd3ff9e34b3d2a
Shreyas NC <shreyas.nc@intel.com>
2016-08-12 12:29:52 +0530

    ASoC: Intel: Skylake: Remove dfw config and associated structures
    The skl_dfw_config structure is no longer required as the module
    config is populated by parsing and reading the token values.
    
    So, remove the structure.
    
    Signed-off-by: Shreyas NC <shreyas.nc@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/skl-sst-utils.c
+++ sound/soc/intel/skylake/skl-sst-utils.c
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* FW Extended Manifest Header id = $AE1 */
 #define SKL_EXT_MANIFEST_HEADER_MAGIC   0x31454124
 
-struct skl_dfw_module_mod {
-	char name[100];
-	struct skl_dfw_module skl_dfw_mod;
-};
-
 struct UUID {
 	u8 id[16];
 };
