8451dee8c36c54283df56cca336710b61525cd46
Luis de Bethencourt <luisbg@osg.samsung.com>
2016-08-14 18:41:30 +0100

    befs: remove signatures from comments
    No idea why some comments have signatures. These predate git. Removing them
    since they add noise and no information.
    
    Signed-off-by: Luis de Bethencourt <luisbg@osg.samsung.com>

--- fs/befs/io.c
+++ fs/befs/io.c
@@ -20,8 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Converts befs notion of disk addr to a disk offset and uses
  * linux kernel function sb_bread() to get the buffer containing
- * the offset. -Will Dyson
- *
+ * the offset.
  */
 
 struct buffer_head *
