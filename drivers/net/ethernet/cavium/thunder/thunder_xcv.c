4091cd4ed04a2c34faf8451a3fffd2c67e43a200
Wei Yongjun <weiyj.lk@gmail.com>
2016-08-15 22:51:29 +0000

    net: thunderx: Remove unnecessary pci_set_drvdata()
    The driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Wei Yongjun <weiyj.lk@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cavium/thunder/thunder_xcv.c
+++ drivers/net/ethernet/cavium/thunder/thunder_xcv.c
@@ -195,7 +195,6 @@ err_release_regions:
 err_disable_device:
 	pci_disable_device(pdev);
 err_kfree:
-	pci_set_drvdata(pdev, NULL);
 	devm_kfree(dev, xcv);
 	xcv = NULL;
 	return err;
@@ -212,7 +211,6 @@ static void xcv_remove(struct pci_dev *pdev)
 
 	pci_release_regions(pdev);
 	pci_disable_device(pdev);
-	pci_set_drvdata(pdev, NULL);
 }
 
 static struct pci_driver xcv_driver = {
