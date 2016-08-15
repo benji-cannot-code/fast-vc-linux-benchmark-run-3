0f2cae2e335d163a5b3424041fafcc47ef75346a
Martin Brandenburg <martin@omnibond.com>
2016-08-15 15:33:42 -0400

    orangefs: g_orangefs_stats -> orangefs_stats for consistency
    Signed-off-by: Martin Brandenburg <martin@omnibond.com>

--- fs/orangefs/orangefs-mod.c
+++ fs/orangefs/orangefs-mod.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * global variables declared here
  */
 
-struct orangefs_stats g_orangefs_stats;
+struct orangefs_stats orangefs_stats;
 
 /* the size of the hash tables for ops in progress */
 int hash_table_size = 509;
