e5dc4810188dd55cf1853c76f7bbb76353455292
Tal Shorer <tal.shorer@gmail.com>
2016-08-16 19:04:47 +0300

    usb: ulpi: add new api functions, {read|write}_dev()
    Add these two new api callbacks to struct ulpi_ops. These are different
    than read, write in that they pass the parent device directly instead
    of via the ops argument.
    They are intended to replace the old api functions.
    
    If the new api callbacks are missing, revert to calling the old ones
    as before.
    
    Acked-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/common/ulpi.c
+++ drivers/usb/common/ulpi.c
@@ -22,13 +22,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int ulpi_read(struct ulpi *ulpi, u8 addr)
 {
-	return ulpi->ops->read(ulpi->ops, addr);
+	if (!ulpi->ops->read_dev)
+		return ulpi->ops->read(ulpi->ops, addr);
+	return ulpi->ops->read_dev(ulpi->dev.parent, addr);
 }
 EXPORT_SYMBOL_GPL(ulpi_read);
 
 int ulpi_write(struct ulpi *ulpi, u8 addr, u8 val)
 {
-	return ulpi->ops->write(ulpi->ops, addr, val);
+	if (!ulpi->ops->write_dev)
+		return ulpi->ops->write(ulpi->ops, addr, val);
+	return ulpi->ops->write_dev(ulpi->dev.parent, addr, val);
 }
 EXPORT_SYMBOL_GPL(ulpi_write);
 
