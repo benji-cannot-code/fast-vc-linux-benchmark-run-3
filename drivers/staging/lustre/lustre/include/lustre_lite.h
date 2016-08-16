4dda6200a03fc8604a813b8896f290fb2b1ed6b9
Lai Siyao <lai.siyao@intel.com>
2016-08-16 16:19:16 -0400

    staging: lustre: fid: do open-by-fid by default
    Currently client open-by-fid often packs name into the request,
    but the name may be invalid, eg. NFS export, and even if it's
    valid, it may cause inconsistency because this operation is done
    on this fid, which is globally unique, but name not.
    
    Since open-by-fid doesn't pack name, for striped dir we can't know
    parent stripe fid on client, so we set parent fid the same as
    child fid, and MDT has to find its parent fid from linkea (this is
    already supported by MDT).
    
    M_CHECK_STALE becomes obsolete.
    
    Unset MDS_OPEN_FL_INTERNAL from open syscall flags, because these
    flags are internally used, and should not be set from user space.
    
    It's not necessary to store parent fid in lli_pfid, because MDT
    can get it's parent fid from linkea, and now that DNE stripe
    directory stores master inode fid in lli_pfid, stop storing parent
    fid to avoid conflict.
    
    Signed-off-by: Lai Siyao <lai.siyao@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-3544
    Reviewed-on: http://review.whamcloud.com/7476
    Reviewed-on: http://review.whamcloud.com/10692
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Reviewed-by: wangdi <di.wang@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre_lite.h
+++ drivers/staging/lustre/lustre/include/lustre_lite.h
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "obd_class.h"
 #include "lustre_net.h"
-#include "lustre_mds.h"
 #include "lustre_ha.h"
 
 /* 4UL * 1024 * 1024 */
--- drivers/staging/lustre/lustre/mdc/mdc_internal.h
+++ drivers/staging/lustre/lustre/mdc/mdc_internal.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _MDC_INTERNAL_H
 
 #include "../include/lustre_mdc.h"
-#include "../include/lustre_mds.h"
 
 void lprocfs_mdc_init_vars(struct lprocfs_static_vars *lvars);
 
