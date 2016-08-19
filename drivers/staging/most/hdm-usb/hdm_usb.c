3abe7f6954c9e98a7f7e4a68597601340fc5e482
Christian Gromm <christian.gromm@microchip.com>
2016-08-19 11:12:56 +0200

    staging: most: hdm-usb: synchronize release of struct buf_anchor
    In case a channel that is going to be destroyed has been tagged as not
    "healthy" by the function hdm_poison_channel() while the functions
    hdm_write_completion() or hdm_read_completion() are being executed, they
    race for destruction of buf_anchor.
    
    This patch fixes the problem.
    
    Signed-off-by: Andrey Shvetsov <andrey.shvetsov@k2l.de>
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/hdm-usb/hdm_usb.c
+++ drivers/staging/most/hdm-usb/hdm_usb.c
@@ -286,6 +286,8 @@ static unsigned int get_stream_frame_size(struct most_channel_config *cfg)
 static int hdm_poison_channel(struct most_interface *iface, int channel)
 {
 	struct most_dev *mdev;
+	unsigned long flags;
+	spinlock_t *lock; /* temp. lock */
 
 	mdev = to_mdev(iface);
 	if (unlikely(!iface)) {
@@ -297,7 +299,10 @@ static int hdm_poison_channel(struct most_interface *iface, int channel)
 		return -ECHRNG;
 	}
 
+	lock = mdev->anchor_list_lock + channel;
+	spin_lock_irqsave(lock, flags);
 	mdev->is_channel_healthy[channel] = false;
+	spin_unlock_irqrestore(lock, flags);
 
 	cancel_work_sync(&mdev->clear_work[channel].ws);
 
@@ -408,8 +413,10 @@ static void hdm_write_completion(struct urb *urb)
 	dev = &mdev->usb_device->dev;
 	lock = mdev->anchor_list_lock + channel;
 
+	spin_lock_irqsave(lock, flags);
 	if ((urb->status == -ENOENT) || (urb->status == -ECONNRESET) ||
 	    (!mdev->is_channel_healthy[channel])) {
+		spin_unlock_irqrestore(lock, flags);
 		complete(&anchor->urb_compl);
 		return;
 	}
@@ -420,6 +427,7 @@ static void hdm_write_completion(struct urb *urb)
 		case -EPIPE:
 			dev_warn(dev, "Broken OUT pipe detected\n");
 			mdev->is_channel_healthy[channel] = false;
+			spin_unlock_irqrestore(lock, flags);
 			mbo->status = MBO_E_INVAL;
 			mdev->clear_work[channel].pipe = urb->pipe;
 			schedule_work(&mdev->clear_work[channel].ws);
@@ -437,7 +445,6 @@ static void hdm_write_completion(struct urb *urb)
 		mbo->processed_length = urb->actual_length;
 	}
 
-	spin_lock_irqsave(lock, flags);
 	list_del(&anchor->list);
 	spin_unlock_irqrestore(lock, flags);
 	kfree(anchor);
@@ -572,8 +579,10 @@ static void hdm_read_completion(struct urb *urb)
 	dev = &mdev->usb_device->dev;
 	lock = mdev->anchor_list_lock + channel;
 
+	spin_lock_irqsave(lock, flags);
 	if ((urb->status == -ENOENT) || (urb->status == -ECONNRESET) ||
 	    (!mdev->is_channel_healthy[channel])) {
+		spin_unlock_irqrestore(lock, flags);
 		complete(&anchor->urb_compl);
 		return;
 	}
@@ -584,6 +593,7 @@ static void hdm_read_completion(struct urb *urb)
 		case -EPIPE:
 			dev_warn(dev, "Broken IN pipe detected\n");
 			mdev->is_channel_healthy[channel] = false;
+			spin_unlock_irqrestore(lock, flags);
 			mbo->status = MBO_E_INVAL;
 			mdev->clear_work[channel].pipe = urb->pipe;
 			schedule_work(&mdev->clear_work[channel].ws);
@@ -607,7 +617,7 @@ static void hdm_read_completion(struct urb *urb)
 			mbo->status = MBO_E_INVAL;
 		}
 	}
-	spin_lock_irqsave(lock, flags);
+
 	list_del(&anchor->list);
 	spin_unlock_irqrestore(lock, flags);
 	kfree(anchor);
