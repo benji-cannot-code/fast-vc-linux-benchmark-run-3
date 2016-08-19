b6c93a53ca1d53a6ed22e0ad3f45b2b815d81d06
Russell King <rmk+kernel@armlinux.org.uk>
2016-08-19 16:24:36 +0100

    ARM: document and update UNCACHEABLE_ADDR definitions
    Document the UNCACHEABLE_ADDR definitions for footbridge and SA1100
    so that we know where they're located and/or what they're accessing.
    Change RiscPC to calculate the UNCACHEABLE_ADDR value from FLUSH_BASE
    as that's where we locate that.
    
    UNCACHEABLE_ADDR is used to perform an uncached access (ARMv4
    terminology) necessary to force a CPU clock-switch to the memory-
    speed clock, as required for entering WFI.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/mach-sa1100/include/mach/hardware.h
+++ arch/arm/mach-sa1100/include/mach/hardware.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARCH_HARDWARE_H
 
 
-#define UNCACHEABLE_ADDR	0xfa050000
+#define UNCACHEABLE_ADDR	0xfa050000	/* ICIP */
 
 
 /*
