f6a8d2d224396a4adff863553ec7f5f308c6c69a
Johan Hovold <johan@hovoldconsulting.com>
2016-08-19 09:42:21 +0200

    greybus: svc: drop unnecessary symbol exports
    The svc functions are only supposed to be called by core and should not
    be exported.
    
    Most of these functions should never have been exported in the first
    place, while a few no longer needs to be (e.g. since core gained support
    for offloaded connections).
    
    The only remaining exception is gb_svc_intf_set_power_mode() which is
    needed by the camera driver until proper link management is in place.
    
    Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/staging/greybus/svc.c
+++ drivers/staging/greybus/svc.c
@@ -448,7 +448,6 @@ int gb_svc_dme_peer_get(struct gb_svc *svc, u8 intf_id, u16 attr, u16 selector,
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(gb_svc_dme_peer_get);
 
 int gb_svc_dme_peer_set(struct gb_svc *svc, u8 intf_id, u16 attr, u16 selector,
 			u32 value)
@@ -481,7 +480,6 @@ int gb_svc_dme_peer_set(struct gb_svc *svc, u8 intf_id, u16 attr, u16 selector,
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(gb_svc_dme_peer_set);
 
 int gb_svc_connection_create(struct gb_svc *svc,
 				u8 intf1_id, u16 cport1_id,
@@ -500,7 +498,6 @@ int gb_svc_connection_create(struct gb_svc *svc,
 	return gb_operation_sync(svc->connection, GB_SVC_TYPE_CONN_CREATE,
 				 &request, sizeof(request), NULL, 0);
 }
-EXPORT_SYMBOL_GPL(gb_svc_connection_create);
 
 void gb_svc_connection_destroy(struct gb_svc *svc, u8 intf1_id, u16 cport1_id,
 			       u8 intf2_id, u16 cport2_id)
@@ -521,7 +518,6 @@ void gb_svc_connection_destroy(struct gb_svc *svc, u8 intf1_id, u16 cport1_id,
 				intf1_id, cport1_id, intf2_id, cport2_id, ret);
 	}
 }
-EXPORT_SYMBOL_GPL(gb_svc_connection_destroy);
 
 int gb_svc_timesync_enable(struct gb_svc *svc, u8 count, u64 frame_time,
 			   u32 strobe_delay, u32 refclk)
@@ -537,7 +533,6 @@ int gb_svc_timesync_enable(struct gb_svc *svc, u8 count, u64 frame_time,
 				 GB_SVC_TYPE_TIMESYNC_ENABLE,
 				 &request, sizeof(request), NULL, 0);
 }
-EXPORT_SYMBOL_GPL(gb_svc_timesync_enable);
 
 int gb_svc_timesync_disable(struct gb_svc *svc)
 {
@@ -547,7 +542,6 @@ int gb_svc_timesync_disable(struct gb_svc *svc)
 				 GB_SVC_TYPE_TIMESYNC_DISABLE,
 				 NULL, 0, NULL, 0);
 }
-EXPORT_SYMBOL_GPL(gb_svc_timesync_disable);
 
 int gb_svc_timesync_authoritative(struct gb_svc *svc, u64 *frame_time)
 {
@@ -565,7 +559,6 @@ int gb_svc_timesync_authoritative(struct gb_svc *svc, u64 *frame_time)
 		frame_time[i] = le64_to_cpu(response.frame_time[i]);
 	return 0;
 }
-EXPORT_SYMBOL_GPL(gb_svc_timesync_authoritative);
 
 int gb_svc_timesync_ping(struct gb_svc *svc, u64 *frame_time)
 {
@@ -583,7 +576,6 @@ int gb_svc_timesync_ping(struct gb_svc *svc, u64 *frame_time)
 	*frame_time = le64_to_cpu(response.frame_time);
 	return 0;
 }
-EXPORT_SYMBOL_GPL(gb_svc_timesync_ping);
 
 int gb_svc_timesync_wake_pins_acquire(struct gb_svc *svc, u32 strobe_mask)
 {
@@ -596,7 +588,6 @@ int gb_svc_timesync_wake_pins_acquire(struct gb_svc *svc, u32 strobe_mask)
 				 &request, sizeof(request),
 				 NULL, 0);
 }
-EXPORT_SYMBOL_GPL(gb_svc_timesync_wake_pins_acquire);
 
 int gb_svc_timesync_wake_pins_release(struct gb_svc *svc)
 {
@@ -606,7 +597,6 @@ int gb_svc_timesync_wake_pins_release(struct gb_svc *svc)
 				 GB_SVC_TYPE_TIMESYNC_WAKE_PINS_RELEASE,
 				 NULL, 0, NULL, 0);
 }
-EXPORT_SYMBOL_GPL(gb_svc_timesync_wake_pins_release);
 
 /* Creates bi-directional routes between the devices */
 int gb_svc_route_create(struct gb_svc *svc, u8 intf1_id, u8 dev1_id,
@@ -729,7 +719,6 @@ int gb_svc_ping(struct gb_svc *svc)
 					 NULL, 0, NULL, 0,
 					 GB_OPERATION_TIMEOUT_DEFAULT * 2);
 }
-EXPORT_SYMBOL_GPL(gb_svc_ping);
 
 static int gb_svc_version_request(struct gb_operation *op)
 {
