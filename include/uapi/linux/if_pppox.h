95ca069ec36b78594bccfb58e68852ba8ee29d15
Mikko Rapeli <mikko.rapeli@iki.fi>
2016-08-22 20:32:38 +0200

    include/uapi/linux/if_pppox.h: include linux/if.h
    Fixes userspace compilation error:
    
    error: ‘IFNAMSIZ’ undeclared here (not in a function)
    
    Signed-off-by: Mikko Rapeli <mikko.rapeli@iki.fi>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/if_pppox.h
+++ include/uapi/linux/if_pppox.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/byteorder.h>
 
 #include <linux/socket.h>
+#include <linux/if.h>
 #include <linux/if_ether.h>
 #include <linux/if_pppol2tp.h>
 
