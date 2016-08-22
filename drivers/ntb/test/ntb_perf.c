3d0dda5b84611d0afdba447e30a38f27ce1d5ae4
Nicholas Mc Guire <hofrat@osadl.org>
2016-08-22 18:51:36 +0200

    ntb: make DMA_OUT_RESOURCE_TO HZ independent
    schedule_timeout_* takes a timeout in jiffies but the code currently is
    passing in a constant which makes this timeout HZ dependent, so pass it
    through msecs_to_jiffies() to fix this up.
    
    Signed-off-by: Nicholas Mc Guire <hofrat@osadl.org>
    Acked-by: Dave Jiang <dave.jiang@intel.com>
    Signed-off-by: Jon Mason <jdmason@kudzu.us>

--- drivers/ntb/test/ntb_perf.c
+++ drivers/ntb/test/ntb_perf.c
@@ -73,7 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_THREADS		32
 #define MAX_TEST_SIZE		SZ_1M
 #define MAX_SRCS		32
-#define DMA_OUT_RESOURCE_TO	50
+#define DMA_OUT_RESOURCE_TO	msecs_to_jiffies(50)
 #define DMA_RETRIES		20
 #define SZ_4G			(1ULL << 32)
 #define MAX_SEG_ORDER		20 /* no larger than 1M for kmalloc buffer */
