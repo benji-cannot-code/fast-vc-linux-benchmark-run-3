58cd606da9960a7d8e5cd4b3784287a0ddcdb91a
Markus Heiser <markus.heiser@darmarit.de>
2016-08-22 15:16:21 -0600

    doc-rst: add boilerplate to customize c-domain
    Add a sphinx-extension to customize the sphinx c-domain.  No functional
    changes right yet, just the boilerplate code.
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarIT.de>
    [ jc: coding-style tweak ]
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

---
+++ Documentation/sphinx/cdomain.py
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+# -*- coding: utf-8; mode: python -*-
+u"""
+    cdomain
+    ~~~~~~~
+
+    Replacement for the sphinx c-domain.
+
+    :copyright:  Copyright (C) 2016  Markus Heiser
+    :license:    GPL Version 2, June 1991 see Linux/COPYING for details.
+"""
+
+from sphinx.domains.c import CObject as Base_CObject
+from sphinx.domains.c import CDomain as Base_CDomain
+
+__version__  = '1.0'
+
+def setup(app):
+
+    app.override_domain(CDomain)
+
+    return dict(
+        version = __version__,
+        parallel_read_safe = True,
+        parallel_write_safe = True
+    )
+
+class CObject(Base_CObject):
+
+    """
+    Description of a C language object.
+    """
+
+class CDomain(Base_CDomain):
+
+    """C language domain."""
+    name = 'c'
+    label = 'C'
+    directives = {
+        'function': CObject,
+        'member':   CObject,
+        'macro':    CObject,
+        'type':     CObject,
+        'var':      CObject,
+    }
