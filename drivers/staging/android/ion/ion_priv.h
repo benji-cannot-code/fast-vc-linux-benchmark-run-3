c09625f2da279a9a1e51f4cb5f06011bcfda21e5
Johanna Abrahamsson <johanna@mjao.org>
2016-08-24 00:42:39 +0200

    staging: android: ion: Remove unnused function ion_handle_buffer
    Remove the function ion_handle_buffer since it is not used anywhere.
    
    Signed-off-by: Johanna Abrahamsson <johanna@mjao.org>
    Acked-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion_priv.h
+++ drivers/staging/android/ion/ion_priv.h
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ion.h"
 
-struct ion_buffer *ion_handle_buffer(struct ion_handle *handle);
-
 /**
  * struct ion_buffer - metadata for a particular buffer
  * @ref:		reference count
