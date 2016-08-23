56d7ea415ac5b0722796acdef49788e0c74fb58a
Stuart Yoder <stuart.yoder@nxp.com>
2016-08-23 17:13:30 -0500

    staging: fsl-mc: create private include for fsl-mc bus components
    The mc-bus.h header file is intended to be public for system related
    drivers that may need to make fsl-mc bus specific calls-- gic its,
    smmu, vfio.  But, currently it contains definitions private/internal
    to the fsl-mc bus itself.  Split those definitions out into a new
    fsl-mc bus private header.
    
    Signed-off-by: Stuart Yoder <stuart.yoder@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/dprc-driver.c
+++ drivers/staging/fsl-mc/bus/dprc-driver.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/msi.h>
 #include "dprc-cmd.h"
+#include "fsl-mc-private.h"
 
 struct dprc_child_objs {
 	int child_count;
--- drivers/staging/fsl-mc/bus/fsl-mc-allocator.c
+++ drivers/staging/fsl-mc/bus/fsl-mc-allocator.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../include/dpcon-cmd.h"
 #include "dpmcp-cmd.h"
 #include "dpmcp.h"
+#include "fsl-mc-private.h"
 #include <linux/msi.h>
 
 #define FSL_MC_IS_ALLOCATABLE(_obj_type) \
