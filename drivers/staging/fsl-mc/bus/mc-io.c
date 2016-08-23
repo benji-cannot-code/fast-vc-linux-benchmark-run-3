d4a07233e9217a39036caa19f938b7c0e1fe036a
Stuart Yoder <stuart.yoder@nxp.com>
2016-08-23 17:14:18 -0500

    staging: fsl-mc: explicitly include files where they are needed
    Instead of relying on mc-sys.h to include misc system headers, have
    the source files that need them explicitly do it themselves.
    
    Signed-off-by: Stuart Yoder <stuart.yoder@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/mc-io.c
+++ drivers/staging/fsl-mc/bus/mc-io.c
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../include/mc-bus.h"
 #include "../include/mc-sys.h"
+#include <linux/io.h>
+
 #include "fsl-mc-private.h"
 #include "dpmcp.h"
 #include "dpmcp-cmd.h"
