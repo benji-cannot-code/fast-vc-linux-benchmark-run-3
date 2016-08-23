ef817d14132297a41737c5c3520a88da1df15df0
Paul Gortmaker <paul.gortmaker@windriver.com>
2016-08-23 17:19:39 -0400

    pinctrl: mediatek: make mtk-common explicitly non-modular
    The Makefile currently controlling compilation of this code is:
    
      drivers/pinctrl/mediatek/pinctrl-mtk-common.o
        ---> drivers/pinctrl/mediatek/Makefile:obj-y += pinctrl-mtk-common.o
    
    ...meaning that it currently is not being built as a module by anyone.
    
    Lets remove the couple traces of modular infrastructure use, so that
    when reading the driver there is no doubt it is builtin-only.
    
    We delete the MODULE_LICENSE tag etc. since all that information
    is already contained at the top of the file in the comments.
    
    We don't replace module.h with init.h since the file doesn't need that.
    
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Hongzhou Yang <hongzhou.yang@mediatek.com>
    Cc: linux-gpio@vger.kernel.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/mediatek/pinctrl-mtk-common.c
+++ drivers/pinctrl/mediatek/pinctrl-mtk-common.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/io.h>
 #include <linux/gpio/driver.h>
-#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_device.h>
@@ -1497,7 +1496,3 @@ chip_error:
 	gpiochip_remove(pctl->chip);
 	return ret;
 }
-
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("MediaTek Pinctrl Driver");
-MODULE_AUTHOR("Hongzhou Yang <hongzhou.yang@mediatek.com>");
