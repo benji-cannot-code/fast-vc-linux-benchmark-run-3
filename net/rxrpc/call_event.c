42020e3c0bbfe8294cf774409e8104e2af365e4c
David Howells <dhowells@redhat.com>
2016-08-23 15:27:23 +0100

    rxrpc: Remove RXRPC_CALL_PROC_BUSY
    Remove RXRPC_CALL_PROC_BUSY as work queue items are now 100% non-reentrant.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- net/rxrpc/call_event.c
+++ net/rxrpc/call_event.c
@@ -833,11 +833,6 @@ void rxrpc_process_call(struct work_struct *work)
 	       call->debug_id, rxrpc_call_states[call->state], call->events,
 	       (jiffies - call->creation_jif) / (HZ / 10));
 
-	if (test_and_set_bit(RXRPC_CALL_PROC_BUSY, &call->flags)) {
-		_debug("XXXXXXXXXXXXX RUNNING ON MULTIPLE CPUS XXXXXXXXXXXXX");
-		return;
-	}
-
 	if (!call->conn)
 		goto skip_msg_init;
 
@@ -1282,7 +1277,6 @@ maybe_reschedule:
 	}
 
 error:
-	clear_bit(RXRPC_CALL_PROC_BUSY, &call->flags);
 	kfree(acks);
 
 	/* because we don't want two CPUs both processing the work item for one
