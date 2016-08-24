269a763bba2d4cd881ba1c00733cea4da0ff4497
Dmitry Eremin <dmitry.eremin@intel.com>
2016-08-24 11:11:55 -0400

    staging/lustre/llite: Fix suspicious dereference of pointer 'vma->vm_file'
    Remove useless LASSERT(vma->vm_file) because of if it's NULL it
    will crash early in file_inode(vma->vm_file).
    
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-on: http://review.whamcloud.com/21171
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-8372
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Signed-off-by: Oleg Drokin <green@linuxhacker.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_mmap.c
+++ drivers/staging/lustre/lustre/llite/llite_mmap.c
@@ -430,7 +430,6 @@ static void ll_vm_open(struct vm_area_struct *vma)
 	struct inode *inode    = file_inode(vma->vm_file);
 	struct vvp_object *vob = cl_inode2vvp(inode);
 
-	LASSERT(vma->vm_file);
 	LASSERT(atomic_read(&vob->vob_mmap_cnt) >= 0);
 	atomic_inc(&vob->vob_mmap_cnt);
 }
@@ -443,7 +442,6 @@ static void ll_vm_close(struct vm_area_struct *vma)
 	struct inode      *inode = file_inode(vma->vm_file);
 	struct vvp_object *vob   = cl_inode2vvp(inode);
 
-	LASSERT(vma->vm_file);
 	atomic_dec(&vob->vob_mmap_cnt);
 	LASSERT(atomic_read(&vob->vob_mmap_cnt) >= 0);
 }
