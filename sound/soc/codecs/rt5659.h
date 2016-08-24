1d6753ccc75aadb88973862d5e67bc33696bf036
Nicolin Chen <nicoleotsuka@gmail.com>
2016-08-24 11:00:27 -0700

    ASoC: rt5659: Fix incorrect register addresses
    According to the datasheets of Realtek ALC5658 (Rev. 0.9)
    and ALC5659 (Rev. 0.13), the "IRQ Control 4" register is
    located at MX-00BAh while the "IRQ control 5" register is
    at MX-00BBh.
    
    There is no "IRQ Control 6" register in the datasheets but
    it is supposed to be behind the "IRQ Control 5" register.
    
    So this patch corrects these addresses.
    
    Signed-off-by: Nicolin Chen <nicoleotsuka@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5659.h
+++ sound/soc/codecs/rt5659.h
@@ -181,9 +181,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RT5659_IRQ_CTRL_1			0x00b6
 #define RT5659_IRQ_CTRL_2			0x00b7
 #define RT5659_IRQ_CTRL_3			0x00b8
-#define RT5659_IRQ_CTRL_4			0x00b9
-#define RT5659_IRQ_CTRL_5			0x00ba
-#define RT5659_IRQ_CTRL_6			0x00bb
+#define RT5659_IRQ_CTRL_4			0x00ba
+#define RT5659_IRQ_CTRL_5			0x00bb
+#define RT5659_IRQ_CTRL_6			0x00bc
 #define RT5659_INT_ST_1				0x00be
 #define RT5659_INT_ST_2				0x00bf
 #define RT5659_GPIO_CTRL_1			0x00c0
