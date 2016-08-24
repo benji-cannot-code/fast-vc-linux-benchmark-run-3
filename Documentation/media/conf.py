60200a884da3d116cc1499ada1331e7b37003bf4
Markus Heiser <markus.heiser@darmarIT.de>
2016-08-24 17:36:15 +0200

    doc-rst: define PDF's of the media folder
    To build only the PDF of the media folder run::
    
      make SPHINXDIRS=media pdfdocs
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarIT.de>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- Documentation/media/conf.py
+++ Documentation/media/conf.py
@@ -4,3 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 project = 'Linux Media Subsystem Documentation'
 
 tags.add("subproject")
+
+latex_documents = [
+    ('index', 'media.tex', 'Linux Media Subsystem Documentation',
+     'The kernel development community', 'manual'),
+]
