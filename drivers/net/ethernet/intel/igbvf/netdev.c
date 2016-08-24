95c95018cfb70f1fa940c9dc8e424ebeb075cf0e
Todd Fujinaka <todd.fujinaka@intel.com>
2016-08-24 08:38:46 -0700

    igbvf: bump version to igbvf-2.4.0
    Bump version to match other igbvf drivers.
    
    Signed-off-by: Todd Fujinaka <todd.fujinaka@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igbvf/netdev.c
+++ drivers/net/ethernet/intel/igbvf/netdev.c
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "igbvf.h"
 
-#define DRV_VERSION "2.0.2-k"
+#define DRV_VERSION "2.4.0-k"
 char igbvf_driver_name[] = "igbvf";
 const char igbvf_driver_version[] = DRV_VERSION;
 static const char igbvf_driver_string[] =
