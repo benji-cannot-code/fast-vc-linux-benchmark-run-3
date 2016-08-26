2c8ce33c9d0cc1cded0a981206836fcbf7230800
Baoyou Xie <baoyou.xie@linaro.org>
2016-08-26 17:56:24 +0800

    crypto: caam - add missing header dependencies
    We get 1 warning when biuld kernel with W=1:
    drivers/crypto/caam/ctrl.c:398:5: warning: no previous prototype for 'caam_get_era' [-Wmissing-prototypes]
    
    In fact, this function is declared in drivers/crypto/caam/ctrl.h,
    so this patch add missing header dependencies.
    
    Signed-off-by: Baoyou Xie <baoyou.xie@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/caam/ctrl.c
+++ drivers/crypto/caam/ctrl.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "jr.h"
 #include "desc_constr.h"
 #include "error.h"
+#include "ctrl.h"
 
 bool caam_little_end;
 EXPORT_SYMBOL(caam_little_end);
