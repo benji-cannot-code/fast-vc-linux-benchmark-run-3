122d47702fe2a2a26b06bfa1ec655043f6f3f0f1
Mathias Kresin <dev@kresin.me>
2016-08-26 09:16:53 +0200

    rt2x00: add support for mac addr from device tree
    On some devices the EEPROMs of Ralink Wi-Fi chips have a default Ralink
    MAC address set (RT3062F: 00:0C:43:30:62:00, RT3060F:
    00:0C:43:30:60:00). Using multiple of these devices in the same network
    can cause nasty issues.
    
    Allow to override the MAC in the EEPROM with (a known good) one set in
    the device tree to bypass the issue.
    
    Signed-off-by: Mathias Kresin <dev@kresin.me>
    Acked-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ralink/rt2x00/rt2x00dev.c
+++ drivers/net/wireless/ralink/rt2x00/rt2x00dev.c
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/log2.h>
+#include <linux/of.h>
+#include <linux/of_net.h>
 
 #include "rt2x00.h"
 #include "rt2x00lib.h"
@@ -931,6 +933,21 @@ static void rt2x00lib_rate(struct ieee80211_rate *entry,
 	if (rate->flags & DEV_RATE_SHORT_PREAMBLE)
 		entry->flags |= IEEE80211_RATE_SHORT_PREAMBLE;
 }
+
+void rt2x00lib_set_mac_address(struct rt2x00_dev *rt2x00dev, u8 *eeprom_mac_addr)
+{
+	const char *mac_addr;
+
+	mac_addr = of_get_mac_address(rt2x00dev->dev->of_node);
+	if (mac_addr)
+		ether_addr_copy(eeprom_mac_addr, mac_addr);
+
+	if (!is_valid_ether_addr(eeprom_mac_addr)) {
+		eth_random_addr(eeprom_mac_addr);
+		rt2x00_eeprom_dbg(rt2x00dev, "MAC: %pM\n", eeprom_mac_addr);
+	}
+}
+EXPORT_SYMBOL_GPL(rt2x00lib_set_mac_address);
 
 static int rt2x00lib_probe_hw_modes(struct rt2x00_dev *rt2x00dev,
 				    struct hw_mode_spec *spec)
