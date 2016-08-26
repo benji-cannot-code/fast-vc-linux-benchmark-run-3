b13c06e05894c141c07560eee10da7a2add98faf
Russell King <rmk+kernel@armlinux.org.uk>
2016-08-26 17:41:37 +0100

    usb: ohci-sa1111: remove mach/hardware.h include
    The mach/hardware.h include doesn't seem to be necessary to build
    ohci-sa1111, so let's remove it to kill off an unnecessary platform
    specific include.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ohci-sa1111.c
+++ drivers/usb/host/ohci-sa1111.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This file is licenced under the GPL.
  */
 
-#include <mach/hardware.h>
 #include <asm/mach-types.h>
 #include <asm/hardware/sa1111.h>
 
