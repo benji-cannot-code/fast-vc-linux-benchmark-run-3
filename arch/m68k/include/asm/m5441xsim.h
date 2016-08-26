58755a3bfd17771a58ac91de163beaad7cf8e143
Greg Ungerer <gerg@linux-m68k.org>
2016-08-26 15:59:30 +1000

    m68k: generalize io memory region setup for ColdFire ACR registers
    The ACR registers of the ColdFire define at a macro level what regions
    of the addresses space should have caching or other attribute types applied.
    
    Currently for the MMU enabled setups we map the interal IO peripheral addres
    space as uncachable based on the define for the MBAR address (CONFIG_MBAR).
    Not all ColdFire SoC use a programmable MBAR register address. Some parts
    have fixed addressing for their internal peripheral registers.
    
    Generalize the way we get the internal peripheral base address so all types
    can be accomodated in the ACR definitions. Each ColdFire SoC type now sets
    its IO memory base and size definitions (which may be based on MBAR) which
    are then used in the ACR definitions.
    
    Signed-off-by: Greg Ungerer <gerg@linux-m68k.org>

--- arch/m68k/include/asm/m5441xsim.h
+++ arch/m68k/include/asm/m5441xsim.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MCF_BUSCLK		(MCF_CLK / 2)
 #define MACHINE			MACH_M5441X
 #define FPUTYPE			0
+#define IOMEMBASE		0xe0000000
+#define IOMEMSIZE		0x20000000
 
 #include <asm/m54xxacr.h>
 
