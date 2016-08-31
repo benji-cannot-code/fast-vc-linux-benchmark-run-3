9b9cdc7d782ea16b3903f401581f8328710e441f
Russell King <rmk+kernel@armlinux.org.uk>
2016-08-31 08:49:47 +0100

    ARM: sa1100/assabet: convert to generic CF sockets
    Convert Assabet to use the generic CF socket support.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- drivers/pcmcia/sa1100_generic.h
+++ drivers/pcmcia/sa1100_generic.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Declaration for all machine specific init/exit functions.
  */
 extern int pcmcia_adsbitsy_init(struct device *);
-extern int pcmcia_assabet_init(struct device *);
 extern int pcmcia_badge4_init(struct device *);
 extern int pcmcia_cerf_init(struct device *);
 extern int pcmcia_flexanet_init(struct device *);
