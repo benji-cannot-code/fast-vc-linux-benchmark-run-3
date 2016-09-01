9f080627b31199145026707c66c8427cf3923082
Bjorn Andersson <bjorn.andersson@linaro.org>
2016-09-01 15:28:04 -0700

    rpmsg: Split rpmsg core and virtio backend
    Extract the generic rpmsg core functionality from the virtio rpmsg
    implementation, splitting the implementation in a rpmsg core and a
    virtio backend.
    
    Based on initial work by Sricharan R <sricharan@codeaurora.org>
    
    Cc: Sricharan R <sricharan@codeaurora.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/rpmsg/rpmsg_internal.h
+++ drivers/rpmsg/rpmsg_internal.h
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define to_rpmsg_device(d) container_of(d, struct rpmsg_device, dev)
 #define to_rpmsg_driver(d) container_of(d, struct rpmsg_driver, drv)
 
+int rpmsg_register_device(struct rpmsg_device *rpdev);
+int rpmsg_unregister_device(struct device *parent,
+			    struct rpmsg_channel_info *chinfo);
+
 struct device *rpmsg_find_device(struct device *parent,
 				 struct rpmsg_channel_info *chinfo);
 
