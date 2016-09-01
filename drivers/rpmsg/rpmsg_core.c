def9b95a61d6161ddfcfdec3a5d796c0670d1886
Bjorn Andersson <bjorn.andersson@linaro.org>
2016-09-01 15:28:05 -0700

    rpmsg: Hide rpmsg indirection tables
    Move the device and endpoint indirection tables to the rpmsg internal
    header file, to hide them from the public API.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/rpmsg/rpmsg_core.c
+++ drivers/rpmsg/rpmsg_core.c
@@ -21,7 +21,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) "%s: " fmt, __func__
 
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/rpmsg.h>
+#include <linux/of_device.h>
+#include <linux/slab.h>
 
 #include "rpmsg_internal.h"
 
