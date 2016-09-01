1bafefe0c8b905ecb85d4a02b61ed37ed20fd525
Linus Walleij <linus.walleij@linaro.org>
2016-09-01 11:44:47 +0200

    iio: accel: kxsd9: Replace "parent" with "dev"
    What is passed to the .probe() and .remove() functions is
    technically the parent of the created IIO device but it becomes
    a big confusion for the head to have it named like this since
    it is usually clear from context the "dev" refers to the physical
    device, and when next adding PM callbacks a clean
    "struct device *dev" pointer is passed to these and that makes
    it even more confused. Rename "parent" to "dev" like in most
    other drivers.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/accel/kxsd9.h
+++ drivers/iio/accel/kxsd9.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KXSD9_STATE_RX_SIZE 2
 #define KXSD9_STATE_TX_SIZE 2
 
-int kxsd9_common_probe(struct device *parent,
+int kxsd9_common_probe(struct device *dev,
 		       struct regmap *map,
 		       const char *name);
-int kxsd9_common_remove(struct device *parent);
+int kxsd9_common_remove(struct device *dev);
