48076194869038901a12704ce90c38ff5cd02a55
Zhi Wang <zhi.a.wang@intel.com>
2016-09-02 13:33:29 +0800

    drm/i915/gvt: vGPU MMIO virtualization
    This patch introduces the generic vGPU MMIO emulation intercept
    framework.  The MPT modules will request GVT-g core logic to
    emulate MMIO read/write through IO emulation operations
    callback when hypervisor trapped a guest GTTMMIO read/write.
    
    Signed-off-by: Zhi Wang <zhi.a.wang@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/debug.h
+++ drivers/gpu/drm/i915/gvt/debug.h
@@ -37,4 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define gvt_dbg_mm(fmt, args...) \
 	DRM_DEBUG_DRIVER("gvt: mm: "fmt, ##args)
 
+#define gvt_dbg_mmio(fmt, args...) \
+	DRM_DEBUG_DRIVER("gvt: mmio: "fmt, ##args)
+
 #endif
--- drivers/gpu/drm/i915/gvt/hypercall.h
+++ drivers/gpu/drm/i915/gvt/hypercall.h
@@ -35,13 +35,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _GVT_HYPERCALL_H_
 
 struct intel_gvt_io_emulation_ops {
-	int (*emulate_cfg_read)(void *, unsigned int,
-				void *, unsigned int);
-	int (*emulate_cfg_write)(void *, unsigned int,
-				 void *, unsigned int);
+	int (*emulate_cfg_read)(void *, unsigned int, void *, unsigned int);
+	int (*emulate_cfg_write)(void *, unsigned int, void *, unsigned int);
+	int (*emulate_mmio_read)(void *, u64, void *, unsigned int);
+	int (*emulate_mmio_write)(void *, u64, void *, unsigned int);
 };
 
-extern struct intel_gvt_io_emulation_ops *gvt_io_emulation_ops;
+extern struct intel_gvt_io_emulation_ops intel_gvt_io_emulation_ops;
 
 /*
  * Specific GVT-g MPT modules function collections. Currently GVT-g supports
