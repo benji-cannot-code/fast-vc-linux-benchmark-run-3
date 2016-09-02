c7dec97e303a2aa195bf1da5d1b631ca7de2c433
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2016-09-02 14:45:33 -0400

    net: dsa: mv88e6xxx: move Global2 code
    Marvell chips are composed of multiple SMI devices. One of them at
    address 0x1C is called Global2. It provides an extended set of
    registers, used for interrupt control, EEPROM access, indirect PHY
    access (to bypass the PHY Polling Unit) and cross-chip related setup.
    
    Most chips have it, but some others don't (older ones such as 6060).
    
    Now that its related code is isolated in mv88e6xxx_g2_* functions, move
    it to its own global2.c file, making most of its setup code static.
    Document each registers in the meantime.
    
    Its compilation can be later avoided for chips without such registers.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ drivers/net/dsa/mv88e6xxx/global2.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Marvell 88E6xxx Switch Global 2 Registers support (device address 0x1C)
+ *
+ * Copyright (c) 2008 Marvell Semiconductor
+ *
+ * Copyright (c) 2016 Vivien Didelot <vivien.didelot@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef _MV88E6XXX_GLOBAL2_H
+#define _MV88E6XXX_GLOBAL2_H
+
+#include "mv88e6xxx.h"
+
+int mv88e6xxx_g2_smi_phy_read(struct mv88e6xxx_chip *chip, int addr, int reg,
+			      u16 *val);
+int mv88e6xxx_g2_smi_phy_write(struct mv88e6xxx_chip *chip, int addr, int reg,
+			       u16 val);
+int mv88e6xxx_g2_set_switch_mac(struct mv88e6xxx_chip *chip, u8 *addr);
+int mv88e6xxx_g2_get_eeprom16(struct mv88e6xxx_chip *chip,
+			      struct ethtool_eeprom *eeprom, u8 *data);
+int mv88e6xxx_g2_set_eeprom16(struct mv88e6xxx_chip *chip,
+			      struct ethtool_eeprom *eeprom, u8 *data);
+int mv88e6xxx_g2_setup(struct mv88e6xxx_chip *chip);
+
+#endif /* _MV88E6XXX_GLOBAL2_H */
