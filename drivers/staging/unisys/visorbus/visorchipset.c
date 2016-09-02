e2acfe0e916554d510a3a6f77c0f97f2b76caff2
David Kershner <david.kershner@unisys.com>
2016-09-02 16:41:37 -0400

    staging: unisys: visorbus: Merge controlvm completion status to controlvmchannel
    The completion status are valid only for the controlvm channel
    so they should be moved into its header file.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <Timothy.Sell@unisys.com>
    Reported-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorchipset.c
+++ drivers/staging/unisys/visorbus/visorchipset.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "channel.h"
 #include "controlvmchannel.h"
-#include "controlvmcompletionstatus.h"
 #include "guestlinuxdebug.h"
 #include "version.h"
 #include "visorbus.h"
