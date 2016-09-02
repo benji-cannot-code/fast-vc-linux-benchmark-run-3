ccd78fecc4d39a9a63f9add45e0806d94cd1b262
David Kershner <david.kershner@unisys.com>
2016-09-02 16:41:45 -0400

    staging: unisys: visorbus: cleanup include listings
    Cleaned up include section of files so that they only
    include the files needed by them. If an include file includes
    the file already don't repeat it.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <Timothy.Sell@unisys.com>
    Reported-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorchipset.c
+++ drivers/staging/unisys/visorbus/visorchipset.c
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uuid.h>
 #include <linux/crash_dump.h>
 
-#include "channel.h"
-#include "controlvmchannel.h"
 #include "version.h"
 #include "visorbus.h"
 #include "visorbus_private.h"
