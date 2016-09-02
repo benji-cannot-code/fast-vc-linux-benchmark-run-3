31deadf56da50d7ec5ca1baff3f9f783da576fa6
Kieran Bingham <kieran+renesas@bingham.xyz>
2016-09-02 07:48:27 -0300

    [media] v4l: vsp1: Ensure pipeline locking in resume path
    The vsp1_pipeline_ready() and vsp1_pipeline_run() functions must be
    called with the pipeline lock held, fix the resume code path.
    
    Signed-off-by: Kieran Bingham <kieran+renesas@bingham.xyz>
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Acked-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/vsp1/vsp1_pipe.c
+++ drivers/media/platform/vsp1/vsp1_pipe.c
@@ -366,6 +366,7 @@ void vsp1_pipelines_suspend(struct vsp1_device *vsp1)
 
 void vsp1_pipelines_resume(struct vsp1_device *vsp1)
 {
+	unsigned long flags;
 	unsigned int i;
 
 	/* Resume all running pipelines. */
@@ -380,7 +381,9 @@ void vsp1_pipelines_resume(struct vsp1_device *vsp1)
 		if (pipe == NULL)
 			continue;
 
+		spin_lock_irqsave(&pipe->irqlock, flags);
 		if (vsp1_pipeline_ready(pipe))
 			vsp1_pipeline_run(pipe);
+		spin_unlock_irqrestore(&pipe->irqlock, flags);
 	}
 }
