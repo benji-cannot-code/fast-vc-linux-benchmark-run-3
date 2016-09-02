ddf6d8974dad674c31b4cd082b6b235d75771a74
Zhi Wang <zhi.a.wang@intel.com>
2016-09-02 12:41:29 +0800

    drm/i915/gvt: vGPU HW resource management
    This patch introduces the GVT-g vGPU HW resource management. Under
    GVT-g virtualizaion environment, each vGPU requires portions HW
    resources, including aperture, hidden GM space, and fence registers.
    
    When creating a vGPU, GVT-g will request these HW resources from host,
    and return them to host after a vGPU is destroyed.
    
    Signed-off-by: Zhi Wang <zhi.a.wang@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/debug.h
+++ drivers/gpu/drm/i915/gvt/debug.h
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __GVT_DEBUG_H__
 #define __GVT_DEBUG_H__
 
+#define gvt_err(fmt, args...) \
+	DRM_ERROR("gvt: "fmt, ##args)
+
 #define gvt_dbg_core(fmt, args...) \
 	DRM_DEBUG_DRIVER("gvt: core: "fmt, ##args)
 
--- drivers/gpu/drm/i915/intel_gvt.h
+++ drivers/gpu/drm/i915/intel_gvt.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _INTEL_GVT_H_
 #define _INTEL_GVT_H_
 
+#include "i915_pvinfo.h"
 #include "gvt/gvt.h"
 
 #ifdef CONFIG_DRM_I915_GVT
