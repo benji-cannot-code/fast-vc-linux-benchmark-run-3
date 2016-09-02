aad5a9d5fd0d23e0ade04a43abfdc7446039f0ce
Erik Arfvidson <erik.arfvidson@unisys.com>
2016-09-02 16:41:27 -0400

    staging: unisys: visornic_main.c remove unused pound defines
    This removes two unused pound defines in the visornic_main.c
    file.
    
    Signed-off-by: Erik Arfvidson <erik.arfvidson@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visornic/visornic_main.c
+++ drivers/staging/unisys/visornic/visornic_main.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iochannel.h"
 
 #define VISORNIC_INFINITE_RSP_WAIT 0
-#define VISORNICSOPENMAX 32
-#define MAXDEVICES     16384
 
 /* MAX_BUF = 64 lines x 32 MAXVNIC x 80 characters
  *         = 163840 bytes
