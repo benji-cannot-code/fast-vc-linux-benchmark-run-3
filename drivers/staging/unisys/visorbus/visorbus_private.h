d92a519687477019bca3148f0069786084940c34
David Kershner <david.kershner@unisys.com>
2016-09-02 16:41:39 -0400

    staging: unisys: visorbus: merge vbusdeviceinfo.h into vbuschannel
    vbusdeviceinfo.h was only used by vbuschannel.h so merge them
    together.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <Timothy.Sell@unisys.com>
    Reported-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorbus_private.h
+++ drivers/staging/unisys/visorbus/visorbus_private.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uuid.h>
 
 #include "controlvmchannel.h"
-#include "vbusdeviceinfo.h"
+#include "vbuschannel.h"
 
 /* TARGET_HOSTNAME specified as -DTARGET_HOSTNAME=\"thename\" on the
  * command line
