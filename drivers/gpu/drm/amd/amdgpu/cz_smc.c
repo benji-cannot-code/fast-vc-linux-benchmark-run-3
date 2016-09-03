93de387446b8aeb02b6d126fcd69f4b20e42e651
Baoyou Xie <baoyou.xie@linaro.org>
2016-09-03 14:01:41 +0800

    drm/amdgpu: add missing header dependencies
    We get a few warnings when building kernel with W=1:
    drivers/gpu/drm/amd/amdgpu/amdgpu_pll.c:113:6: warning: no previous prototype for 'amdgpu_pll_compute' [-Wmissing-prototypes]
    drivers/gpu/drm/amd/amdgpu/cz_smc.c:38:10: warning: no previous prototype for 'cz_get_argument' [-Wmissing-prototypes]
    drivers/gpu/drm/amd/amdgpu/cz_smc.c:302:5: warning: no previous prototype for 'cz_smu_start' [-Wmissing-prototypes]
    ....
    
    In fact, these functions are declared in
    drivers/gpu/drm/amd/amdgpu/atombios_i2c.h
    drivers/gpu/drm/amd/amdgpu/amdgpu_pll.h
    drivers/gpu/drm/amd/amdgpu/cz_dpm.h
    drivers/gpu/drm/amd/amdgpu/vi_dpm.h.
    So this patch adds missing header dependencies.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Baoyou Xie <baoyou.xie@linaro.org>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/cz_smc.c
+++ drivers/gpu/drm/amd/amdgpu/cz_smc.c
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cz_smumgr.h"
 #include "smu_ucode_xfer_cz.h"
 #include "amdgpu_ucode.h"
+#include "cz_dpm.h"
+#include "vi_dpm.h"
 
 #include "smu/smu_8_0_d.h"
 #include "smu/smu_8_0_sh_mask.h"
