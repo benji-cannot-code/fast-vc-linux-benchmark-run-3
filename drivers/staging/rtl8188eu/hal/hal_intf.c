dd432988a1ec0d93416fb42fdb96f02a2539152b
Ivan Safonov <insafonov@gmail.com>
2016-09-03 22:33:34 +0700

    staging: r8188eu: rename ReadChipVersion8188E to rtw_hal_read_chip_version
    And remove two one-line wrappers.
    
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/hal_intf.c
+++ drivers/staging/rtl8188eu/hal/hal_intf.c
@@ -20,12 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <hal_intf.h>
 #include <usb_hal.h>
 
-void rtw_hal_read_chip_version(struct adapter *adapt)
-{
-	if (adapt->HalFunc.read_chip_version)
-		adapt->HalFunc.read_chip_version(adapt);
-}
-
 void rtw_hal_free_data(struct adapter *adapt)
 {
 	if (adapt->HalFunc.free_hal_data)
