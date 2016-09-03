b10a8d929df7818daba340db7b9028cffc19c26d
Ivan Safonov <insafonov@gmail.com>
2016-09-03 22:33:40 +0700

    staging: r8188eu: rename rtl8188e_free_hal_data to rtw_hal_free_data
    And remove two one-line wrappers.
    
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/hal_intf.c
+++ drivers/staging/rtl8188eu/hal/hal_intf.c
@@ -20,12 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <hal_intf.h>
 #include <usb_hal.h>
 
-void rtw_hal_free_data(struct adapter *adapt)
-{
-	if (adapt->HalFunc.free_hal_data)
-		adapt->HalFunc.free_hal_data(adapt);
-}
-
 uint	 rtw_hal_init(struct adapter *adapt)
 {
 	uint	status = _SUCCESS;
