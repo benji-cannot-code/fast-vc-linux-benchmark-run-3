8477e3921f44746d964f3bc8a30df74dab43238d
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-09-05 07:21:18 -0300

    [media] tda18271: use prefix on all printk messages
    Some messages have a hardcoded prefix; others not. Use the
    pr_fmt() to ensure that all messages will use the same prefix.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/tuners/tda18271-priv.h
+++ drivers/media/tuners/tda18271-priv.h
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __TDA18271_PRIV_H__
 #define __TDA18271_PRIV_H__
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/mutex.h>
