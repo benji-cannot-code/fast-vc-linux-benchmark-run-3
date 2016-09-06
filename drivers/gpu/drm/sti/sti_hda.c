db55f1d902f396a40a8017b51d8c3c3ea1b33d60
Fabien Dessenne <fabien.dessenne@st.com>
2016-09-06 09:41:35 +0200

    drm/sti: fix debug logs
    Add some missing \n in logs.
    
    Signed-off-by: Fabien Dessenne <fabien.dessenne@st.com>
    Acked-by: Vincent Abriou <vincent.abriou@st.com>

--- drivers/gpu/drm/sti/sti_hda.c
+++ drivers/gpu/drm/sti/sti_hda.c
@@ -314,7 +314,7 @@ static void hda_enable_hd_dacs(struct sti_hda *hda, bool enable)
 			mask = DAC_CFG_HD_HZUVW_OFF_MASK;
 			break;
 		default:
-			DRM_INFO("Video DACS control register not supported!");
+			DRM_INFO("Video DACS control register not supported\n");
 			return;
 		}
 
@@ -363,7 +363,7 @@ static void hda_dbg_video_dacs_ctrl(struct seq_file *s, void __iomem *reg)
 		mask = DAC_CFG_HD_HZUVW_OFF_MASK;
 		break;
 	default:
-		DRM_DEBUG_DRIVER("Warning: DACS ctrl register not supported!");
+		DRM_DEBUG_DRIVER("Warning: DACS ctrl register not supported\n");
 		return;
 	}
 
