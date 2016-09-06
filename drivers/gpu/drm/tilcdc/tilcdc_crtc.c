073b0cc027941d013d534078acac3505505a6262
Jyri Sarha <jsarha@ti.com>
2016-09-06 22:55:33 +0300

    drm/tilcdc: Flush flip-work workqueue before drm_flip_work_cleanup()
    Flush flip-work workqueue before drm_flip_work_cleanup(). It causes a
    nasty warning if there is unfinished flip-work in the queue when
    drm_flip_work_cleanup() is called. The flush_workqueue() has to be
    called before drm_crtc_cleanup() for unref_worker() to be able to do
    its job.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>
    Reviewed-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/tilcdc/tilcdc_crtc.c
+++ drivers/gpu/drm/tilcdc/tilcdc_crtc.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "drm_flip_work.h"
 #include <drm/drm_plane_helper.h>
 #include <drm/drm_atomic_helper.h>
+#include <linux/workqueue.h>
 
 #include "tilcdc_drv.h"
 #include "tilcdc_regs.h"
@@ -228,8 +229,11 @@ static bool tilcdc_crtc_is_on(struct drm_crtc *crtc)
 static void tilcdc_crtc_destroy(struct drm_crtc *crtc)
 {
 	struct tilcdc_crtc *tilcdc_crtc = to_tilcdc_crtc(crtc);
+	struct tilcdc_drm_private *priv = crtc->dev->dev_private;
 
 	tilcdc_crtc_disable(crtc);
+
+	flush_workqueue(priv->wq);
 
 	of_node_put(crtc->port);
 	drm_crtc_cleanup(crtc);
