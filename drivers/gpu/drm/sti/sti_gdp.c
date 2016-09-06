6f3ad86f14cf005b048ce7cdd139260ae74781b3
Fabien Dessenne <fabien.dessenne@st.com>
2016-09-06 09:42:00 +0200

    drm/sti: run gdp init sequence only once
    Do not rely on plane->status to define whether this is the first update
    but rather check for gdp->vtg.
    This avoids multiple and unwanted calls to sti_vtg_register_client()
    which breaks the kernel scheduler.
    
    Signed-off-by: Fabien Dessenne <fabien.dessenne@st.com>
    Acked-by: Vincent Abriou <vincent.abriou@st.com>

--- drivers/gpu/drm/sti/sti_gdp.c
+++ drivers/gpu/drm/sti/sti_gdp.c
@@ -461,6 +461,7 @@ static void sti_gdp_disable(struct sti_gdp *gdp)
 		clk_disable_unprepare(gdp->clk_pix);
 
 	gdp->plane.status = STI_PLANE_DISABLED;
+	gdp->vtg = NULL;
 }
 
 /**
@@ -612,7 +613,6 @@ static int sti_gdp_atomic_check(struct drm_plane *drm_plane,
 	struct drm_crtc *crtc = state->crtc;
 	struct sti_compositor *compo = dev_get_drvdata(gdp->dev);
 	struct drm_framebuffer *fb =  state->fb;
-	bool first_prepare = plane->status == STI_PLANE_DISABLED ? true : false;
 	struct drm_crtc_state *crtc_state;
 	struct sti_mixer *mixer;
 	struct drm_display_mode *mode;
@@ -649,7 +649,7 @@ static int sti_gdp_atomic_check(struct drm_plane *drm_plane,
 		return -EINVAL;
 	}
 
-	if (first_prepare) {
+	if (!gdp->vtg) {
 		/* Register gdp callback */
 		gdp->vtg = mixer->id == STI_MIXER_MAIN ?
 					compo->vtg_main : compo->vtg_aux;
