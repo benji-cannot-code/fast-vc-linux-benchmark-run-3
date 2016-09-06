6b038159630a7b17020b03a176545621887a2961
Arnd Bergmann <arnd@arndb.de>
2016-09-06 15:20:05 +0200

    dmaengine: cppi41: mark PM functions as __maybe_unused
    When CONFIG_PM_SLEEP is disabled, we get a build error in
    the cppi41 dmaengine driver, since the runtime-pm functions
    are hidden within the wrong #ifdef:
    
    drivers/dma/cppi41.c:1158:21: error: 'cppi41_runtime_suspend' undeclared here (not in a function)
    
    This removes the #ifdef and instead uses __maybe_unused
    annotations that cannot have this problem.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: fdea2d09b997 ("dmaengine: cppi41: Add basic PM runtime support")
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/cppi41.c
+++ drivers/dma/cppi41.c
@@ -1091,8 +1091,7 @@ static int cppi41_dma_remove(struct platform_device *pdev)
 	return 0;
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int cppi41_suspend(struct device *dev)
+static int __maybe_unused cppi41_suspend(struct device *dev)
 {
 	struct cppi41_dd *cdd = dev_get_drvdata(dev);
 
@@ -1103,7 +1102,7 @@ static int cppi41_suspend(struct device *dev)
 	return 0;
 }
 
-static int cppi41_resume(struct device *dev)
+static int __maybe_unused cppi41_resume(struct device *dev)
 {
 	struct cppi41_dd *cdd = dev_get_drvdata(dev);
 	struct cppi41_channel *c;
@@ -1128,7 +1127,7 @@ static int cppi41_resume(struct device *dev)
 	return 0;
 }
 
-static int cppi41_runtime_suspend(struct device *dev)
+static int __maybe_unused cppi41_runtime_suspend(struct device *dev)
 {
 	struct cppi41_dd *cdd = dev_get_drvdata(dev);
 
@@ -1137,7 +1136,7 @@ static int cppi41_runtime_suspend(struct device *dev)
 	return 0;
 }
 
-static int cppi41_runtime_resume(struct device *dev)
+static int __maybe_unused cppi41_runtime_resume(struct device *dev)
 {
 	struct cppi41_dd *cdd = dev_get_drvdata(dev);
 	struct cppi41_channel *c, *_c;
@@ -1152,7 +1151,6 @@ static int cppi41_runtime_resume(struct device *dev)
 
 	return 0;
 }
-#endif
 
 static const struct dev_pm_ops cppi41_pm_ops = {
 	SET_LATE_SYSTEM_SLEEP_PM_OPS(cppi41_suspend, cppi41_resume)
