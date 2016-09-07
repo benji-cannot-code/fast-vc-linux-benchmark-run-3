4a4363deacd47b472665835c3c3164e960e7be15
Laura Abbott <labbott@redhat.com>
2016-09-07 11:49:59 -0700

    staging: android: ion: Add ioctl to query available heaps
    Ion clients currently lack a good method to determine what
    heaps are available and what ids they map to. This leads
    to tight coupling between user and kernel space and headaches.
    Add a query ioctl to let userspace know the availability of
    heaps.
    
    Signed-off-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion_priv.h
+++ drivers/staging/android/ion/ion_priv.h
@@ -103,6 +103,7 @@ struct ion_device {
 	struct dentry *debug_root;
 	struct dentry *heaps_debug_root;
 	struct dentry *clients_debug_root;
+	int heap_cnt;
 };
 
 /**
@@ -467,5 +468,7 @@ struct ion_handle *ion_handle_get_by_id(struct ion_client *client,
 						int id);
 
 int ion_handle_put(struct ion_handle *handle);
+
+int ion_query_heaps(struct ion_client *client, struct ion_heap_query *query);
 
 #endif /* _ION_PRIV_H */
