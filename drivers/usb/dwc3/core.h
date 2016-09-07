0fdc8bc02eb0ee78afe85bf21bdd23fcfb1264b0
Thinh Nguyen <thinhn@synopsys.com>
2016-09-06 19:22:03 -0700

    usb: dwc3: Fix dr_mode validation
    This patch follows the similar fix in dwc2. See
    commit 5268ed9d2e3b ("usb: dwc2: Fix dr_mode validation")
    
    Currently, the dr_mode is only checked against the module configuration.
    It also needs to be checked against the hardware capablities.
    
    The driver now checks if both the module configuration and hardware are
    capable of the dr_mode value. If not, then it will issue a warning and
    fall back to a supported value. If it is unable to fall back to a
    suitable value, then the probe will fail.
    
    Behavior summary:
    
          module          :  actual
     HW   config  dr_mode :  dr_mode
    ---------------------------------
     host  host   any     :  host
     host  dev    any     :  INVALID
     host  otg    any     :  host
    
     dev   host   any     :  INVALID
     dev   dev    any     :  dev
     dev   otg    any     :  dev
    
     otg   host   any     :  host
     otg   dev    any     :  dev
     otg   otg    any     :  dr_mode
    
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>
    Signed-off-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -246,7 +246,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DWC3_GEVNTSIZ_SIZE(n)		((n) & 0xffff)
 
 /* Global HWPARAMS0 Register */
-#define DWC3_GHWPARAMS0_USB3_MODE(n)	((n) & 0x3)
+#define DWC3_GHWPARAMS0_MODE(n)		((n) & 0x3)
+#define DWC3_GHWPARAMS0_MODE_GADGET	0
+#define DWC3_GHWPARAMS0_MODE_HOST	1
+#define DWC3_GHWPARAMS0_MODE_DRD	2
 #define DWC3_GHWPARAMS0_MBUS_TYPE(n)	(((n) >> 3) & 0x7)
 #define DWC3_GHWPARAMS0_SBUS_TYPE(n)	(((n) >> 6) & 0x3)
 #define DWC3_GHWPARAMS0_MDWIDTH(n)	(((n) >> 8) & 0xff)
