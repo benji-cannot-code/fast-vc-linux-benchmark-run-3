2282b8866d4691ea654c06c4bd79251a9edbb912
Russell King <rmk+kernel@armlinux.org.uk>
2016-09-07 19:21:06 -0700

    Input: jornada720_kbd - remove unneeded mach/hardware.h include
    The Jornada720 keyboard driver does not make use of anything from the
    mach/hardware.h header, so this header can be removed.
    
    Tested-by: Adam Wysocki <armlinux@chmurka.net>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/keyboard/jornada720_kbd.c
+++ drivers/input/keyboard/jornada720_kbd.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 
 #include <mach/jornada720.h>
-#include <mach/hardware.h>
 
 MODULE_AUTHOR("Kristoffer Ericson <Kristoffer.Ericson@gmail.com>");
 MODULE_DESCRIPTION("HP Jornada 710/720/728 keyboard driver");
