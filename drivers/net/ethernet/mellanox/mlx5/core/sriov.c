4db4f2d33b0929fd5adcb2336e7e66de10549cf8
Mohamad Haj Yahia <mohamad@mellanox.com>
2016-09-09 17:35:23 +0300

    net/mlx5: Align sriov/eswitch modules with the new load/unload flow.
    Init/cleanup sriov/eswitch in the core software context init/cleanup
    flows.
    Attach/detach sriov/eswitch in the core load/unload flows.
    
    Signed-off-by: Mohamad Haj Yahia <mohamad@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/sriov.c
+++ drivers/net/ethernet/mellanox/mlx5/core/sriov.c
@@ -223,7 +223,7 @@ int mlx5_sriov_init(struct mlx5_core_dev *dev)
 	if (!sriov->vfs_ctx)
 		return -ENOMEM;
 
-	return mlx5_sriov_attach(dev);
+	return 0;
 }
 
 void mlx5_sriov_cleanup(struct mlx5_core_dev *dev)
@@ -232,6 +232,6 @@ void mlx5_sriov_cleanup(struct mlx5_core_dev *dev)
 
 	if (!mlx5_core_is_pf(dev))
 		return;
-	mlx5_sriov_detach(dev);
+
 	kfree(sriov->vfs_ctx);
 }
