bdedbe1867d49c09e3e92c42f2c4e50f2dda8c99
Christian Gromm <christian.gromm@microchip.com>
2016-09-09 15:25:35 +0200

    staging: most: hdm-usb: extend DCI access
    This patch extends the direct communication interface ABI of the USB
    network interface controller. It is needed to provide applications
    arbitrary access to DCI registers and the ability to synchronize the
    internal endpoint logic of the controller.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/hdm-usb/hdm_usb.c
+++ drivers/staging/most/hdm-usb/hdm_usb.c
@@ -80,10 +80,12 @@ struct buf_anchor {
  * struct most_dci_obj - Direct Communication Interface
  * @kobj:position in sysfs
  * @usb_device: pointer to the usb device
+ * @reg_addr: register address for arbitrary DCI access
  */
 struct most_dci_obj {
 	struct kobject kobj;
 	struct usb_device *usb_device;
+	u16 reg_addr;
 };
 
 #define to_dci_obj(p) container_of(p, struct most_dci_obj, kobj)
@@ -949,6 +951,10 @@ static struct usb_device_id usbid[] = {
 	struct most_dci_attribute most_dci_attr_##_name = \
 		__ATTR(_name, S_IRUGO | S_IWUSR, show_value, store_value)
 
+#define MOST_DCI_WO_ATTR(_name) \
+	struct most_dci_attribute most_dci_attr_##_name = \
+		__ATTR(_name, S_IWUSR, show_value, store_value)
+
 /**
  * struct most_dci_attribute - to access the attributes of a dci object
  * @attr: attributes of a dci object
@@ -1032,6 +1038,9 @@ static ssize_t show_value(struct most_dci_obj *dci_obj,
 	u16 reg_addr;
 	int err;
 
+	if (!strcmp(attr->attr.name, "arb_address"))
+		return snprintf(buf, PAGE_SIZE, "%04x\n", dci_obj->reg_addr);
+
 	if (!strcmp(attr->attr.name, "ni_state"))
 		reg_addr = DRCI_REG_NI_STATE;
 	else if (!strcmp(attr->attr.name, "packet_bandwidth"))
@@ -1056,6 +1065,8 @@ static ssize_t show_value(struct most_dci_obj *dci_obj,
 		reg_addr = DRCI_REG_HW_ADDR_MI;
 	else if (!strcmp(attr->attr.name, "mep_eui48_lo"))
 		reg_addr = DRCI_REG_HW_ADDR_LO;
+	else if (!strcmp(attr->attr.name, "arb_value"))
+		reg_addr = dci_obj->reg_addr;
 	else
 		return -EIO;
 
@@ -1074,6 +1085,14 @@ static ssize_t store_value(struct most_dci_obj *dci_obj,
 	u16 reg_addr;
 	int err;
 
+	err = kstrtou16(buf, 16, &val);
+	if (err)
+		return err;
+
+	if (!strcmp(attr->attr.name, "arb_address")) {
+		dci_obj->reg_addr = val;
+		return count;
+	}
 	if (!strcmp(attr->attr.name, "mep_filter"))
 		reg_addr = DRCI_REG_MEP_FILTER;
 	else if (!strcmp(attr->attr.name, "mep_hash0"))
@@ -1090,12 +1109,15 @@ static ssize_t store_value(struct most_dci_obj *dci_obj,
 		reg_addr = DRCI_REG_HW_ADDR_MI;
 	else if (!strcmp(attr->attr.name, "mep_eui48_lo"))
 		reg_addr = DRCI_REG_HW_ADDR_LO;
-	else
-		return -EIO;
+	else if (!strcmp(attr->attr.name, "arb_value"))
+		reg_addr = dci_obj->reg_addr;
+	else if (!strcmp(attr->attr.name, "sync_ep")) {
+		u16 ep = val;
 
-	err = kstrtou16(buf, 16, &val);
-	if (err)
-		return err;
+		reg_addr = DRCI_REG_BASE + DRCI_COMMAND + ep * 16;
+		val = 1;
+	} else
+		return -EIO;
 
 	err = drci_wr_reg(dci_obj->usb_device, reg_addr, val);
 	if (err < 0)
@@ -1108,6 +1130,7 @@ static MOST_DCI_RO_ATTR(ni_state);
 static MOST_DCI_RO_ATTR(packet_bandwidth);
 static MOST_DCI_RO_ATTR(node_address);
 static MOST_DCI_RO_ATTR(node_position);
+static MOST_DCI_WO_ATTR(sync_ep);
 static MOST_DCI_ATTR(mep_filter);
 static MOST_DCI_ATTR(mep_hash0);
 static MOST_DCI_ATTR(mep_hash1);
@@ -1116,6 +1139,8 @@ static MOST_DCI_ATTR(mep_hash3);
 static MOST_DCI_ATTR(mep_eui48_hi);
 static MOST_DCI_ATTR(mep_eui48_mi);
 static MOST_DCI_ATTR(mep_eui48_lo);
+static MOST_DCI_ATTR(arb_address);
+static MOST_DCI_ATTR(arb_value);
 
 /**
  * most_dci_def_attrs - array of default attribute files of the dci object
@@ -1125,6 +1150,7 @@ static struct attribute *most_dci_def_attrs[] = {
 	&most_dci_attr_packet_bandwidth.attr,
 	&most_dci_attr_node_address.attr,
 	&most_dci_attr_node_position.attr,
+	&most_dci_attr_sync_ep.attr,
 	&most_dci_attr_mep_filter.attr,
 	&most_dci_attr_mep_hash0.attr,
 	&most_dci_attr_mep_hash1.attr,
@@ -1133,6 +1159,8 @@ static struct attribute *most_dci_def_attrs[] = {
 	&most_dci_attr_mep_eui48_hi.attr,
 	&most_dci_attr_mep_eui48_mi.attr,
 	&most_dci_attr_mep_eui48_lo.attr,
+	&most_dci_attr_arb_address.attr,
+	&most_dci_attr_arb_value.attr,
 	NULL,
 };
 
