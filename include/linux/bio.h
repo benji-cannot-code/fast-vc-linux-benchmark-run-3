82d04789c06b6738ce7222f4a185de86288e55a8
Christoph Hellwig <hch@lst.de>
2016-09-11 16:03:03 +0200

    bio.h: remove a very outdated comment
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- include/linux/bio.h
+++ include/linux/bio.h
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * 2.5 block I/O model
- *
  * Copyright (C) 2001 Jens Axboe <axboe@suse.de>
  *
  * This program is free software; you can redistribute it and/or modify
