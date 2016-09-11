58426056aa20d3c75e7ac77ff78e48bf32c7695b
PrasannaKumar Muralidharan <prasannatsmkumar@gmail.com>
2016-09-11 20:54:26 +0530

    hwrng: geode - Use linux/io.h instead of asm/io.h
    Fix checkpatch.pl warning by changing from asm/io.h to linux/io.h. In
    the mean time arrange the includes in alphabetical order.
    
    Signed-off-by: PrasannaKumar Muralidharan <prasannatsmkumar@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/char/hw_random/geode-rng.c
+++ drivers/char/hw_random/geode-rng.c
@@ -25,12 +25,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * warranty of any kind, whether express or implied.
  */
 
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/pci.h>
-#include <linux/hw_random.h>
 #include <linux/delay.h>
-#include <asm/io.h>
+#include <linux/hw_random.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/pci.h>
 
 #define GEODE_RNG_DATA_REG   0x50
 #define GEODE_RNG_STATUS_REG 0x54
