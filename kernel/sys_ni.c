fdb3f9a1b41910c206f0953078c7906c67c73063
Dave Hansen <dave.hansen@intel.com>
2016-09-12 13:38:42 -0700

    x86/pkeys: Fix pkeys build breakage for some non-x86 arches
    Guenter Roeck reported breakage on the h8300 and c6x architectures (among
    others) caused by the new memory protection keys syscalls.  This patch does
    what Arnd suggested and adds them to kernel/sys_ni.c.
    
    Fixes: a60f7b69d92c ("generic syscalls: Wire up memory protection keys syscalls")
    Reported-and-tested-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Dave Hansen <dave.hansen@intel.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Cc: linux-arch@vger.kernel.org
    Cc: Dave Hansen <dave@sr71.net>
    Cc: linux-api@vger.kernel.org
    Link: http://lkml.kernel.org/r/20160912203842.48E7AC50@viggo.jf.intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -251,3 +251,8 @@ cond_syscall(sys_execveat);
 
 /* membarrier */
 cond_syscall(sys_membarrier);
+
+/* memory protection keys */
+cond_syscall(sys_pkey_mprotect);
+cond_syscall(sys_pkey_alloc);
+cond_syscall(sys_pkey_free);
