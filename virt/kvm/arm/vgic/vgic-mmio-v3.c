36915153c8e6e3668e99967b287dd8ba60589030
Vladimir Murzin <vladimir.murzin@arm.com>
2016-09-12 15:49:20 +0100

    KVM: arm: vgic: Support 64-bit data manipulation on 32-bit host systems
    We have couple of 64-bit registers defined in GICv3 architecture, so
    unsigned long accesses to these registers will only access a single
    32-bit part of that regitser. On the other hand these registers can't
    be accessed as 64-bit with a single instruction like ldrd/strd or
    ldmia/stmia if we run a 32-bit host because KVM does not support
    access to MMIO space done by these instructions.
    
    It means that a 32-bit guest accesses these registers in 32-bit
    chunks, so the only thing we need to do is to ensure that
    extract_bytes() always takes 64-bit data.
    
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- virt/kvm/arm/vgic/vgic-mmio-v3.c
+++ virt/kvm/arm/vgic/vgic-mmio-v3.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vgic-mmio.h"
 
 /* extract @num bytes at @offset bytes offset in data */
-unsigned long extract_bytes(unsigned long data, unsigned int offset,
+unsigned long extract_bytes(u64 data, unsigned int offset,
 			    unsigned int num)
 {
 	return (data >> (offset * 8)) & GENMASK_ULL(num * 8 - 1, 0);
