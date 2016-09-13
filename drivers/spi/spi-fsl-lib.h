a55c43bdb44c06b65695871ee3a2d897e1845ded
Heiner Kallweit <hkallweit1@gmail.com>
2016-09-13 23:16:02 +0200

    spi: fsl-espi: align register access with other drivers
    Change register access to the method used in other drivers too.
    - use register names as in the chip spec for constants
    - avoid hard to read statements like
      __be32 __iomem *espi_mode = &reg_base->mode
    - get rid of old powerpc-specific functions like in_8
    
    In addition annotate reg_base in struct mpc8xxx_spi as __iomem.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-fsl-lib.h
+++ drivers/spi/spi-fsl-lib.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPI/eSPI Controller driver's private data. */
 struct mpc8xxx_spi {
 	struct device *dev;
-	void *reg_base;
+	void __iomem *reg_base;
 
 	/* rx & tx bufs from the spi_transfer */
 	const void *tx;
