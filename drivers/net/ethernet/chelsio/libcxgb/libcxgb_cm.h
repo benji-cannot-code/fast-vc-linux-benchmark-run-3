44d6b799403d5c49707c5c4f5dda9f6d14abc9e9
Varun Prakash <varun@chelsio.com>
2016-09-13 21:23:57 +0530

    libcxgb,iw_cxgb4,cxgbit: add cxgb_find_route()
    Add cxgb_find_route() in libcxgb_cm.c to remove
    it's duplicate definitions from cxgb4/cm.c and
    cxgbit/cxgbit_cm.c.
    
    Signed-off-by: Varun Prakash <varun@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/libcxgb/libcxgb_cm.h
+++ drivers/net/ethernet/chelsio/libcxgb/libcxgb_cm.h
@@ -40,4 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void
 cxgb_get_4tuple(struct cpl_pass_accept_req *, enum chip_type,
 		int *, __u8 *, __u8 *, __be16 *, __be16 *);
+struct dst_entry *
+cxgb_find_route(struct cxgb4_lld_info *,
+		struct net_device *(*)(struct net_device *),
+		__be32, __be32, __be16,	__be16, u8);
 #endif
