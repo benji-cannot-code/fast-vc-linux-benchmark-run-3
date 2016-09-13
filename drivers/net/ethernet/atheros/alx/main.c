85f9a863c0cc4da64c7d8da7876e405290533c4c
Tobias Regnery <tobias.regnery@gmail.com>
2016-09-13 12:06:57 +0200

    alx: fix error handling in __alx_open
    In commit 9ee7b683ea63 we moved the enablement of msi interrupts earlier in
    alx_init_intr. If there is an error in alx_alloc_rings, __alx_open returns
    with an error but msi (or msi-x) interrupts stays enabled. Add a new error
    label to disable msi (or msi-x) interrupts.
    
    Fixes: 9ee7b683ea63 ("alx: refactor msi enablement and disablement")
    Signed-off-by: Tobias Regnery <tobias.regnery@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/atheros/alx/main.c
+++ drivers/net/ethernet/atheros/alx/main.c
@@ -1029,7 +1029,7 @@ static int __alx_open(struct alx_priv *alx, bool resume)
 
 	err = alx_alloc_rings(alx);
 	if (err)
-		return err;
+		goto out_disable_adv_intr;
 
 	alx_configure(alx);
 
@@ -1050,6 +1050,8 @@ static int __alx_open(struct alx_priv *alx, bool resume)
 
 out_free_rings:
 	alx_free_rings(alx);
+out_disable_adv_intr:
+	alx_disable_advanced_intr(alx);
 	return err;
 }
 
