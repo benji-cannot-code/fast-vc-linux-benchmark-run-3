e599fd0984c8a573c1a40af4d285650e6b0dd558
Nikolai Kondrashov <spbnick@gmail.com>
2016-09-14 21:38:12 +0300

    HID: Remove broken links to tablet descriptions
    Remove comments with broken links to tablet descriptions from
    hid-kye/uclogic/waltop drivers.
    
    They pointed to now dead DIGImend project wiki at sf.net. Even though
    the DIGImend project still hosts descriptions, now at
    digimend.github.io, it's better to avoid them going stale again, and let
    anyone interested search the web instead.
    
    Signed-off-by: Nikolai Kondrashov <spbnick@gmail.com>
    Reviewed-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-kye.c
+++ drivers/hid/hid-kye.c
@@ -20,11 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "hid-ids.h"
 
-/*
- * See EasyPen i405X description, device and HID report descriptors at
- * http://sf.net/apps/mediawiki/digimend/?title=KYE_EasyPen_i405X
- */
-
 /* Original EasyPen i405X report descriptor size */
 #define EASYPEN_I405X_RDESC_ORIG_SIZE	476
 
@@ -82,11 +77,6 @@ static __u8 easypen_i405x_rdesc_fixed[] = {
 	0xC0,             /*    End Collection,               */
 	0xC0              /*  End Collection                  */
 };
-
-/*
- * See MousePen i608X description, device and HID report descriptors at
- * http://sf.net/apps/mediawiki/digimend/?title=KYE_MousePen_i608X
- */
 
 /* Original MousePen i608X report descriptor size */
 #define MOUSEPEN_I608X_RDESC_ORIG_SIZE	476
@@ -186,11 +176,6 @@ static __u8 mousepen_i608x_rdesc_fixed[] = {
 	0xC0,             /*    End Collection,               */
 	0xC0              /*  End Collection                  */
 };
-
-/*
- * See EasyPen M610X description, device and HID report descriptors at
- * http://sf.net/apps/mediawiki/digimend/?title=KYE_EasyPen_M610X
- */
 
 /* Original EasyPen M610X report descriptor size */
 #define EASYPEN_M610X_RDESC_ORIG_SIZE	476
