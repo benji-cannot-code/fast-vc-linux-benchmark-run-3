137bb925132b6ce297f42c036293a3bb4a97a942
Dave Gordon <david.s.gordon@intel.com>
2016-09-14 13:10:33 +0100

    drm/i915: Only expand COND once in wait_for()
    Commentary from Chris Wilson's original version:
    
    > I was looking at some wait_for() timeouts on a slow system, with lots of
    > debug enabled (KASAN, lockdep, mmio_debug). Thinking that we were
    > mishandling the timeout, I tried to ensure that we loop at least once
    > after first testing COND. However, the double test of COND either side
    > of the timeout check makes that unlikely. But we can do an equivalent
    > loop, that keeps the COND check after testing for timeout (required so
    > that we are not preempted between testing COND and then testing for a
    > timeout) without expanding COND twice.
    >
    > The advantage of only expanding COND once is a dramatic reduction in
    > code size:
    >
    >    text	   data	    bss	    dec	    hex
    >    1308733	   5184	   1152	1315069	 1410fd	before
    >    1305341	   5184	   1152	1311677	 1403bd	after
    
    but it turned out that due to a missing iniitialiser, gcc had "gone
    wild trimming undefined code" :( This version acheives a rather more
    modest (but still worthwhile) gain of ~550 bytes.
    
    Signed-off-by: Dave Gordon <david.s.gordon@intel.com>
    Original-idea-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Zanoni, Paulo R <paulo.r.zanoni@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1473855033-26980-1-git-send-email-david.s.gordon@intel.com
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -53,11 +53,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define _wait_for(COND, US, W) ({ \
 	unsigned long timeout__ = jiffies + usecs_to_jiffies(US) + 1;	\
-	int ret__ = 0;							\
-	while (!(COND)) {						\
-		if (time_after(jiffies, timeout__)) {			\
-			if (!(COND))					\
-				ret__ = -ETIMEDOUT;			\
+	int ret__;							\
+	for (;;) {							\
+		bool expired__ = time_after(jiffies, timeout__);	\
+		if (COND) {						\
+			ret__ = 0;					\
+			break;						\
+		}							\
+		if (expired__) {					\
+			ret__ = -ETIMEDOUT;				\
 			break;						\
 		}							\
 		if ((W) && drm_can_sleep()) {				\
