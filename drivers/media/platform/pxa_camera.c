8c3d0984992db5693cdb3dd7c10191397d489614
Wei Yongjun <weiyongjun1@huawei.com>
2016-09-14 23:21:45 -0300

    [media] pxa_camera: fix error return code in pxa_camera_probe()
    Fix to return error code -ENODEV from dma_request_slave_channel_compat()
    error handling case instead of 0, as done elsewhere in this function.
    
    Also fix to release resources in v4l2_clk_register() error handling.
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/pxa_camera.c
+++ drivers/media/platform/pxa_camera.c
@@ -2403,6 +2403,7 @@ static int pxa_camera_probe(struct platform_device *pdev)
 						 &params, &pdev->dev, "CI_U");
 	if (!pcdev->dma_chans[1]) {
 		dev_err(&pdev->dev, "Can't request DMA for Y\n");
+		err = -ENODEV;
 		goto exit_free_dma_y;
 	}
 
@@ -2412,6 +2413,7 @@ static int pxa_camera_probe(struct platform_device *pdev)
 						 &params, &pdev->dev, "CI_V");
 	if (!pcdev->dma_chans[2]) {
 		dev_err(&pdev->dev, "Can't request DMA for V\n");
+		err = -ENODEV;
 		goto exit_free_dma_u;
 	}
 
@@ -2462,8 +2464,10 @@ static int pxa_camera_probe(struct platform_device *pdev)
 
 		pcdev->mclk_clk = v4l2_clk_register(&pxa_camera_mclk_ops,
 						    clk_name, NULL);
-		if (IS_ERR(pcdev->mclk_clk))
-			return PTR_ERR(pcdev->mclk_clk);
+		if (IS_ERR(pcdev->mclk_clk)) {
+			err = PTR_ERR(pcdev->mclk_clk);
+			goto exit_free_v4l2dev;
+		}
 	}
 
 	err = v4l2_async_notifier_register(&pcdev->v4l2_dev, &pcdev->notifier);
