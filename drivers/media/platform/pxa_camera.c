8c83d3af7225408080e1e8084b733ac951cb8abb
Wei Yongjun <weiyongjun1@huawei.com>
2016-09-14 23:22:03 -0300

    [media] pxa_camera: remove duplicated include from pxa_camera.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/pxa_camera.c
+++ drivers/media/platform/pxa_camera.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/moduleparam.h>
 #include <linux/of.h>
 #include <linux/time.h>
-#include <linux/device.h>
 #include <linux/platform_device.h>
 #include <linux/clk.h>
 #include <linux/sched.h>
