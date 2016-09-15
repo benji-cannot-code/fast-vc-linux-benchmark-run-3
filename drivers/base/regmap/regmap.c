d22d205e6843640c73a708bc1bea698f1057c606
Tony Lindgren <tony@atomide.com>
2016-09-15 13:56:11 -0700

    regmap: Add missing little endian functions
    This with the longer read and write masks allow supporting more
    exotic devices. For example a little endian SPI device:
    
    static const struct regmap_config foo_regmap_config = {
    	.reg_bits = 16,
    	.reg_stride = 4,
    	.val_bits = 16,
    	.write_flag_mask = 0x8000,
    	.reg_format_endian = REGMAP_ENDIAN_LITTLE,
    	.val_format_endian = REGMAP_ENDIAN_LITTLE,
    	...
    };
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/base/regmap/regmap.c
+++ drivers/base/regmap/regmap.c
@@ -750,6 +750,9 @@ struct regmap *__regmap_init(struct device *dev,
 		case REGMAP_ENDIAN_BIG:
 			map->format.format_reg = regmap_format_16_be;
 			break;
+		case REGMAP_ENDIAN_LITTLE:
+			map->format.format_reg = regmap_format_16_le;
+			break;
 		case REGMAP_ENDIAN_NATIVE:
 			map->format.format_reg = regmap_format_16_native;
 			break;
@@ -769,6 +772,9 @@ struct regmap *__regmap_init(struct device *dev,
 		case REGMAP_ENDIAN_BIG:
 			map->format.format_reg = regmap_format_32_be;
 			break;
+		case REGMAP_ENDIAN_LITTLE:
+			map->format.format_reg = regmap_format_32_le;
+			break;
 		case REGMAP_ENDIAN_NATIVE:
 			map->format.format_reg = regmap_format_32_native;
 			break;
@@ -782,6 +788,9 @@ struct regmap *__regmap_init(struct device *dev,
 		switch (reg_endian) {
 		case REGMAP_ENDIAN_BIG:
 			map->format.format_reg = regmap_format_64_be;
+			break;
+		case REGMAP_ENDIAN_LITTLE:
+			map->format.format_reg = regmap_format_64_le;
 			break;
 		case REGMAP_ENDIAN_NATIVE:
 			map->format.format_reg = regmap_format_64_native;
