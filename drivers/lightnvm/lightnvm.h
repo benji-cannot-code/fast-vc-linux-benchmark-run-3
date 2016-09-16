3b2bfa8eb3ee43c5d209713a601fd6161fec38be
Arnd Bergmann <arnd@arndb.de>
2016-09-16 14:25:09 +0200

    lightnvm: propagate device_add() error code
    device_add() may fail, and all callers are supposed to check the
    return value, but one new user in lightnvm doesn't:
    
    drivers/lightnvm/sysfs.c: In function 'nvm_sysfs_register_dev':
    drivers/lightnvm/sysfs.c:184:2: error: ignoring return value of 'device_add',
      declared with attribute warn_unused_result [-Werror=unused-result]
    
    This changes the caller to propagate any error codes, which avoids
    the warning.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Fixes: 38c9e260b9f9 ("lightnvm: expose device geometry through sysfs")
    Signed-off-by: Matias Bjørling <m@bjorling.me>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/lightnvm/lightnvm.h
+++ drivers/lightnvm/lightnvm.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/lightnvm.h>
 
 /* core -> sysfs.c */
-int nvm_sysfs_register_dev(struct nvm_dev *);
+int __must_check nvm_sysfs_register_dev(struct nvm_dev *);
 void nvm_sysfs_unregister_dev(struct nvm_dev *);
 int nvm_sysfs_register(void);
 void nvm_sysfs_unregister(void);
