744e5df7fac3c177bc7ebef0363a1d89a1f96074
Cathal Mullaney <chuckleberryfinn@gmail.com>
2016-09-18 00:04:42 +0100

    Staging: octeon: ethernet-rx: fixed three redundant blank line style issues
    Fixed three minor coding style issues.
    
    Signed-off-by: Cathal Mullaney <chuckleberryfinn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/octeon/ethernet-rx.c
+++ drivers/staging/octeon/ethernet-rx.c
@@ -451,12 +451,10 @@ void cvm_oct_poll_controller(struct net_device *dev)
 		return;
 
 	for (i = 0; i < ARRAY_SIZE(oct_rx_group); i++) {
-
 		if (!(pow_receive_groups & BIT(i)))
 			continue;
 
 		cvm_oct_poll(&oct_rx_group[i], 16);
-
 	}
 }
 #endif
@@ -538,7 +536,6 @@ void cvm_oct_rx_shutdown(void)
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(oct_rx_group); i++) {
-
 		if (!(pow_receive_groups & BIT(i)))
 			continue;
 
