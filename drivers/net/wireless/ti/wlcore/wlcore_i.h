484bccaa929640b397d3cad9bc9cd70ab75b242c
Tony Lindgren <tony@atomide.com>
2016-09-17 09:06:32 -0700

    wlcore: Fix config firmware loading issues
    Booting multiple wl12xx and wl18xx devices using the same rootfs is
    a pain. You currently have to symlink the right nvs file depending
    on the wl12xx type.
    
    For example, with wl1271-nvs.bin being a symlink to wl127x-nvs.bin
    by default and trying to bring up a wl128x based device:
    
    wlcore: ERROR nvs size is not as expected: 1113 != 912
    wlcore: ERROR NVS file is needed during boot
    wlcore: ERROR NVS file is needed during boot
    wlcore: ERROR firmware boot failed despite 3 retries
    
    Note that wl18xx uses a separate config firmware wl18xx-conf.bin
    that can be generated with tools using the following two git repos:
    
    git.ti.com/wilink8-wlan/18xx-ti-utils
    git.ti.com/wilink8-wlan/wl18xx_fw
    
    So let's not configure the nvs file for wl18xx as it's not needed
    AFAIK. If it turns out that we also need the nvs file for wl18xx,
    we can just add it to the config firmware data for wl18xx.
    
    Let's fix the issue by using the chip specific config firmware
    data, and make sure we produce understandable warnings if something
    is missing.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ti/wlcore/wlcore_i.h
+++ drivers/net/wireless/ti/wlcore/wlcore_i.h
@@ -36,13 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "conf.h"
 #include "ini.h"
 
-/*
- * wl127x and wl128x are using the same NVS file name. However, the
- * ini parameters between them are different.  The driver validates
- * the correct NVS size in wl1271_boot_upload_nvs().
- */
-#define WL12XX_NVS_NAME "ti-connectivity/wl1271-nvs.bin"
-
 struct wilink_family_data {
 	const char *name;
 	const char *nvs_name;	/* wl12xx nvs file */
