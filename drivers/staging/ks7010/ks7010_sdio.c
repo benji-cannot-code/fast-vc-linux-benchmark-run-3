5c2b64844797b88a631dc78c620a35b5c4dac58a
sayli karnik <karniksayli1995@gmail.com>
2016-09-18 12:51:03 +0530

    staging: ks7010: Use #include <linux/atomic.h> instead of <asm/atomic.h>
    This patch replaces <asm/atomic.h> with <linux/atomic.h>, as suggested
    by checkpatch.pl.
    
    Signed-off-by: sayli karnik <karniksayli1995@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks7010_sdio.c
+++ drivers/staging/ks7010/ks7010_sdio.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mmc/card.h>
 #include <linux/mmc/sdio_func.h>
 #include <linux/workqueue.h>
-#include <asm/atomic.h>
+#include <linux/atomic.h>
 
 #include "ks_wlan.h"
 #include "ks_wlan_ioctl.h"
