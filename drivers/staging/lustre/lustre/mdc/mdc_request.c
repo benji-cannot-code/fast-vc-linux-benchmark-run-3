c766cf6ca39de6f0d5865ff0bb2e00eaa3b4e902
Henri Doreau <henri.doreau@cea.fr>
2016-09-18 16:37:22 -0400

    staging: lustre: mdc: Report D_CHANGELOG messages as D_HSM
    Removed the D_CHANGELOG pseudo-debug flag that wasn't actually defined
    as a usable one. Report the D_CHANGELOG messages as D_HSM ones instead
    since this is the primary user of these messages.
    
    Signed-off-by: Henri Doreau <henri.doreau@cea.fr>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-5538
    Reviewed-on: http://review.whamcloud.com/11558
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: James Nunez <james.a.nunez@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/mdc/mdc_request.c
+++ drivers/staging/lustre/lustre/mdc/mdc_request.c
@@ -1858,8 +1858,6 @@ static struct kuc_hdr *changelog_kuc_hdr(char *buf, int len, int flags)
 	return lh;
 }
 
-#define D_CHANGELOG 0
-
 struct changelog_show {
 	__u64		cs_startrec;
 	__u32		cs_flags;
@@ -1886,12 +1884,12 @@ static int changelog_kkuc_cb(const struct lu_env *env, struct llog_handle *llh,
 
 	if (rec->cr.cr_index < cs->cs_startrec) {
 		/* Skip entries earlier than what we are interested in */
-		CDEBUG(D_CHANGELOG, "rec=%llu start=%llu\n",
+		CDEBUG(D_HSM, "rec=%llu start=%llu\n",
 		       rec->cr.cr_index, cs->cs_startrec);
 		return 0;
 	}
 
-	CDEBUG(D_CHANGELOG, "%llu %02d%-5s %llu 0x%x t="DFID" p="DFID
+	CDEBUG(D_HSM, "%llu %02d%-5s %llu 0x%x t=" DFID " p=" DFID
 		" %.*s\n", rec->cr.cr_index, rec->cr.cr_type,
 		changelog_type2str(rec->cr.cr_type), rec->cr.cr_time,
 		rec->cr.cr_flags & CLF_FLAGMASK,
@@ -1905,7 +1903,7 @@ static int changelog_kkuc_cb(const struct lu_env *env, struct llog_handle *llh,
 	memcpy(lh + 1, &rec->cr, len - sizeof(*lh));
 
 	rc = libcfs_kkuc_msg_put(cs->cs_fp, lh);
-	CDEBUG(D_CHANGELOG, "kucmsg fp %p len %d rc %d\n", cs->cs_fp, len, rc);
+	CDEBUG(D_HSM, "kucmsg fp %p len %d rc %d\n", cs->cs_fp, len, rc);
 
 	return rc;
 }
@@ -1918,7 +1916,7 @@ static int mdc_changelog_send_thread(void *csdata)
 	struct kuc_hdr *kuch;
 	int rc;
 
-	CDEBUG(D_CHANGELOG, "changelog to fp=%p start %llu\n",
+	CDEBUG(D_HSM, "changelog to fp=%p start %llu\n",
 	       cs->cs_fp, cs->cs_startrec);
 
 	cs->cs_buf = kzalloc(KUC_CHANGELOG_MSG_MAXSIZE, GFP_NOFS);
@@ -1997,7 +1995,7 @@ static int mdc_ioc_changelog_send(struct obd_device *obd,
 		kfree(cs);
 	} else {
 		rc = 0;
-		CDEBUG(D_CHANGELOG, "%s: started changelog thread\n",
+		CDEBUG(D_HSM, "%s: started changelog thread\n",
 		       obd->obd_name);
 	}
 
