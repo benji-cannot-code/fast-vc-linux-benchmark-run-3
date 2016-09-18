dd5c93bea63975e728c38b7a668a6e433a7cfe6c
Lai Siyao <lai.siyao@intel.com>
2016-09-18 16:37:56 -0400

    staging: lustre: statahead: ll_intent_drop_lock() called in spinlock
    ll_intent_drop_lock() may sleep, which should not be called inside
    spinlock.
    
    Signed-off-by: Lai Siyao <lai.siyao@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-2272
    Reviewed-on: http://review.whamcloud.com/9665
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/statahead.c
+++ drivers/staging/lustre/lustre/llite/statahead.c
@@ -662,6 +662,7 @@ static int ll_statahead_interpret(struct ptlrpc_request *req,
 	struct ll_inode_info     *lli = ll_i2info(dir);
 	struct ll_statahead_info *sai = lli->lli_sai;
 	struct sa_entry *entry = (struct sa_entry *)minfo->mi_cbdata;
+	__u64 handle = 0;
 	bool wakeup;
 
 	if (it_disposition(it, DISP_LOOKUP_NEG))
@@ -678,6 +679,21 @@ static int ll_statahead_interpret(struct ptlrpc_request *req,
 	CDEBUG(D_READA, "sa_entry %.*s rc %d\n",
 	       entry->se_qstr.len, entry->se_qstr.name, rc);
 
+	if (rc) {
+		ll_intent_release(it);
+		iput(dir);
+		kfree(minfo);
+	} else {
+		/*
+		 * release ibits lock ASAP to avoid deadlock when statahead
+		 * thread enqueues lock on parent in readdir and another
+		 * process enqueues lock on child with parent lock held, eg.
+		 * unlink.
+		 */
+		handle = it->it_lock_handle;
+		ll_intent_drop_lock(it);
+	}
+
 	spin_lock(&lli->lli_sa_lock);
 	if (rc) {
 		wakeup = __sa_make_ready(sai, entry, rc);
@@ -690,8 +706,7 @@ static int ll_statahead_interpret(struct ptlrpc_request *req,
 		 * for readpage and other tries to enqueue lock on child
 		 * with parent's lock held, for example: unlink.
 		 */
-		entry->se_handle = it->it_lock_handle;
-		ll_intent_drop_lock(it);
+		entry->se_handle = handle;
 		wakeup = !sa_has_callback(sai);
 		list_add_tail(&entry->se_list, &sai->sai_interim_entries);
 	}
@@ -701,11 +716,6 @@ static int ll_statahead_interpret(struct ptlrpc_request *req,
 		wake_up(&sai->sai_thread.t_ctl_waitq);
 	spin_unlock(&lli->lli_sa_lock);
 
-	if (rc) {
-		ll_intent_release(it);
-		iput(dir);
-		kfree(minfo);
-	}
 	return rc;
 }
 
