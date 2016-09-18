09add497e3d424ac3a9359fff6eb308c31488867
Lai Siyao <lai.siyao@intel.com>
2016-09-18 16:38:09 -0400

    staging: lustre: statahead: small fixes and cleanup
    small fixes:
     * when 'unplug' is set for ll_statahead(), sa_put() shouldn't kill
       the entry found, because its inflight RPC may not finish yet.
     * remove 'sai_generation', add 'lli_sa_generation' because the
       former one is not safe to access without lock.
     * revalidate_statahead_dentry() may fail to wait for statahead
       entry to become ready, in this case it should not release this
       entry, because it may be used by inflight statahead RPC.
    
    cleanups:
     * rename ll_statahead_enter() to ll_statahead().
     * move dentry 'lld_sa_generation' update to ll_statahead() to
       simplify code and logic.
     * other small cleanups.
    
    Signed-off-by: Lai Siyao <lai.siyao@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-3270
    Reviewed-on: http://review.whamcloud.com/9667
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6222
    Reviewed-on: http://review.whamcloud.com/13708
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Bobi Jam <bobijam@hotmail.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/namei.c
+++ drivers/staging/lustre/lustre/llite/namei.c
@@ -523,8 +523,8 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 	if (!it || it->it_op == IT_GETXATTR)
 		it = &lookup_it;
 
-	if (it->it_op == IT_GETATTR) {
-		rc = ll_statahead_enter(parent, &dentry, 0);
+	if (it->it_op == IT_GETATTR && dentry_may_statahead(parent, dentry)) {
+		rc = ll_statahead(parent, &dentry, 0);
 		if (rc == 1) {
 			if (dentry == save)
 				retval = NULL;
@@ -575,11 +575,8 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 		retval = NULL;
 	else
 		retval = dentry;
- out:
-	if (req)
-		ptlrpc_req_finished(req);
-	if (it->it_op == IT_GETATTR && (!retval || retval == dentry))
-		ll_statahead_mark(parent, dentry);
+out:
+	ptlrpc_req_finished(req);
 	return retval;
 }
 
