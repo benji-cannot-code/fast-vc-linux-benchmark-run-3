ca89f98edcab8a324ac72b647850605ff78f5e34
Sandhya Bankar <bankarsandhya512@gmail.com>
2016-09-19 00:31:12 +0530

    Staging: i4l: Remove unused variable
    Remove unused variable.
    
    This change is made by below coccinelle script:
    @@
    type T;
    identifier i;
    constant C;
    @@
    (
    extern T i;
    |
    - T i;
      <+... when != i
    - i = C;
      ...+>
    )
    
    Signed-off-by: Sandhya Bankar <bankarsandhya512@gmail.com>
    Acked-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/i4l/act2000/act2000_isa.c
+++ drivers/staging/i4l/act2000/act2000_isa.c
@@ -400,7 +400,6 @@ act2000_isa_download(act2000_card *card, act2000_ddef __user *cb)
 	unsigned int length;
 	int l;
 	int c;
-	long timeout;
 	u_char *b;
 	u_char __user *p;
 	u_char *buf;
@@ -418,7 +417,6 @@ act2000_isa_download(act2000_card *card, act2000_ddef __user *cb)
 	buf = kmalloc(1024, GFP_KERNEL);
 	if (!buf)
 		return -ENOMEM;
-	timeout = 0;
 	while (length) {
 		l = (length > 1024) ? 1024 : length;
 		c = 0;
