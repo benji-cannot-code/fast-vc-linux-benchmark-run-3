328da6396520ae703960cbe268d27dfd370a252a
Stephen Rothwell <sfr@canb.auug.org.au>
2016-09-19 15:50:16 +1000

    blk_mq: linux/blk-mq.h does not include all the headers it depends on
    and building block/blk-mq-pci.o should depend on CONFIG_BLOCK
    
    Fixes: 973c4e372c8f ("blk-mq: provide a default queue mapping for PCI device")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-mq-pci.c
+++ block/blk-mq-pci.c
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  */
+#include <linux/kobject.h>
+#include <linux/blkdev.h>
 #include <linux/blk-mq.h>
 #include <linux/blk-mq-pci.h>
 #include <linux/pci.h>
