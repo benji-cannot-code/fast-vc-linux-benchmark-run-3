7c6e194860a78ed77389d25241d51bd702d5cd93
Christophe Leroy <christophe.leroy@c-s.fr>
2016-09-19 12:58:54 +0200

    powerpc: Fix usage of _PAGE_RO in hugepage
    On some CPUs like the 8xx, _PAGE_RW hence _PAGE_WRITE is defined
    as 0 and _PAGE_RO has to be set when a page is not writable
    
    _PAGE_RO is defined by default in pte-common.h, however BOOK3S/64
    doesn't include that file so _PAGE_RO has to be defined explicitly
    in book3s/64/pgtable.h
    
    Fixes: a7b9f671f2d14 ("powerpc32: adds handling of _PAGE_RO")
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/book3s/64/pgtable.h
+++ arch/powerpc/include/asm/book3s/64/pgtable.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define _PAGE_BIT_SWAP_TYPE	0
 
+#define _PAGE_RO		0
+
 #define _PAGE_EXEC		0x00001 /* execute permission */
 #define _PAGE_WRITE		0x00002 /* write access allowed */
 #define _PAGE_READ		0x00004	/* read access allowed */
