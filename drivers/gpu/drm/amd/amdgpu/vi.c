6707c0468b8fcc3390260df4dbd68553982c05a5
Rex Zhu <Rex.Zhu@amd.com>
2016-09-19 15:44:50 +0800

    drm/amdgpu: use powerplay module for dgpu in Vi.
    delete non-pp code and files. It was just a temporary
    solution and not support dynamic power management.
    
    Signed-off-by: Rex Zhu <Rex.Zhu@amd.com>
    Acked-by: Edward O'Callaghan <funfunctor@folklore1984.net>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vi.c
+++ drivers/gpu/drm/amd/amdgpu/vi.c
@@ -80,6 +80,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 #include "dce_virtual.h"
 
+MODULE_FIRMWARE("amdgpu/topaz_smc.bin");
+MODULE_FIRMWARE("amdgpu/tonga_smc.bin");
+MODULE_FIRMWARE("amdgpu/fiji_smc.bin");
 MODULE_FIRMWARE("amdgpu/polaris10_smc.bin");
 MODULE_FIRMWARE("amdgpu/polaris10_smc_sk.bin");
 MODULE_FIRMWARE("amdgpu/polaris11_smc.bin");
