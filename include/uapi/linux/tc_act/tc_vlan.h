758317d0ed3b82bdec1b5be64ef1b637a82142d5
Shmulik Ladkani <shmulik.ladkani@gmail.com>
2016-09-19 19:11:10 +0300

    net/sched: act_vlan: Introduce TCA_VLAN_ACT_MODIFY vlan action
    TCA_VLAN_ACT_MODIFY allows one to change an existing tag.
    
    It accepts same attributes as TCA_VLAN_ACT_PUSH (protocol, id,
    priority).
    If packet is vlan tagged, then the tag gets overwritten according to
    user specified attributes.
    
    For example, this allows user to replace a tag's vid while preserving
    its priority bits (as opposed to "action vlan pop pipe action vlan push").
    
    Signed-off-by: Shmulik Ladkani <shmulik.ladkani@gmail.com>
    Acked-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/tc_act/tc_vlan.h
+++ include/uapi/linux/tc_act/tc_vlan.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define TCA_VLAN_ACT_POP	1
 #define TCA_VLAN_ACT_PUSH	2
+#define TCA_VLAN_ACT_MODIFY	3
 
 struct tc_vlan {
 	tc_gen;
