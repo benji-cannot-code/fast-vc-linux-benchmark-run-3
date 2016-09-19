3523eedf003bff96db66e706a29228a214daf0ec
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-09-19 15:18:01 -0300

    [media] vsp1: fix CodingStyle violations on multi-line comments
    Several multi-line comments added at the vsp1 patch series
    violate the Kernel CodingStyle. Fix them.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/vsp1/vsp1_rwpf.c
+++ drivers/media/platform/vsp1/vsp1_rwpf.c
@@ -133,7 +133,8 @@ static int vsp1_rwpf_get_selection(struct v4l2_subdev *subdev,
 	struct v4l2_mbus_framefmt *format;
 	int ret = 0;
 
-	/* Cropping is only supported on the RPF and is implemented on the sink
+	/*
+	 * Cropping is only supported on the RPF and is implemented on the sink
 	 * pad.
 	 */
 	if (rwpf->entity.type == VSP1_ENTITY_WPF || sel->pad != RWPF_PAD_SINK)
@@ -181,7 +182,8 @@ static int vsp1_rwpf_set_selection(struct v4l2_subdev *subdev,
 	struct v4l2_rect *crop;
 	int ret = 0;
 
-	/* Cropping is only supported on the RPF and is implemented on the sink
+	/*
+	 * Cropping is only supported on the RPF and is implemented on the sink
 	 * pad.
 	 */
 	if (rwpf->entity.type == VSP1_ENTITY_WPF || sel->pad != RWPF_PAD_SINK)
