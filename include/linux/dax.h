1abe1a66bb8267127bbc9bdbbd1ff47fb20c2d51
Christoph Hellwig <hch@lst.de>
2016-09-19 11:24:49 +1000

    dax: provide an iomap based dax read/write path
    This is a much simpler implementation of the DAX read/write path
    that makes use of the iomap infrastructure.  It does not try to
    mirror the direct I/O calling conventions and thus doesn't have to
    deal with i_dio_count or the end_io handler, but instead leaves
    locking and filesystem-specific I/O completion to the caller.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Ross Zwisler <ross.zwisler@linux.intel.com>
    Signed-off-by: Dave Chinner <david@fromorbit.com>

--- include/linux/dax.h
+++ include/linux/dax.h
@@ -7,9 +7,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/radix-tree.h>
 #include <asm/pgtable.h>
 
+struct iomap_ops;
+
 /* We use lowest available exceptional entry bit for locking */
 #define RADIX_DAX_ENTRY_LOCK (1 << RADIX_TREE_EXCEPTIONAL_SHIFT)
 
+ssize_t iomap_dax_rw(struct kiocb *iocb, struct iov_iter *iter,
+		struct iomap_ops *ops);
 ssize_t dax_do_io(struct kiocb *, struct inode *, struct iov_iter *,
 		  get_block_t, dio_iodone_t, int flags);
 int dax_zero_page_range(struct inode *, loff_t from, unsigned len, get_block_t);
