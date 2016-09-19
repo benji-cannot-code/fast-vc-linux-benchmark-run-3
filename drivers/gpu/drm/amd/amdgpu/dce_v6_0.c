a8858ce1a34738e20d90eb7803112ec11fbe5c63
Alex Deucher <alexander.deucher@amd.com>
2016-09-19 09:30:21 -0400

    drm/amdgpu/dce6: fix off by one in interrupt setup
    Reviewed-by: Tom St Denis <tom.stdenis@amd.com>
    Reported-by: Christian Inci <chris.pcguy.inci@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
@@ -186,7 +186,7 @@ static void dce_v6_0_pageflip_interrupt_init(struct amdgpu_device *adev)
 	unsigned i;
 
 	/* Enable pflip interrupts */
-	for (i = 0; i <= adev->mode_info.num_crtc; i++)
+	for (i = 0; i < adev->mode_info.num_crtc; i++)
 		amdgpu_irq_get(adev, &adev->pageflip_irq, i);
 }
 
@@ -195,7 +195,7 @@ static void dce_v6_0_pageflip_interrupt_fini(struct amdgpu_device *adev)
 	unsigned i;
 
 	/* Disable pflip interrupts */
-	for (i = 0; i <= adev->mode_info.num_crtc; i++)
+	for (i = 0; i < adev->mode_info.num_crtc; i++)
 		amdgpu_irq_put(adev, &adev->pageflip_irq, i);
 }
 
