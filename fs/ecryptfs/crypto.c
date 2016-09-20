19aa28222632943962762fa3479a5a48f684c371
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2016-09-21 01:17:24 +0200

    ecryptfs: remove private bin2hex implementation
    Calling sprintf in a loop is not very efficient, and in any case, we
    already have an implementation of bin-to-hex conversion in lib/ which
    we might as well use.
    
    Note that ecryptfs_to_hex used to nul-terminate the destination (and
    the kernel doc was wrong about the required output size), while
    bin2hex doesn't. [All but one user of ecryptfs_to_hex explicitly
    nul-terminates the result anyway.]
    
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    [tyhicks: Include <linux/kernel.h> in ecryptfs_kernel.h]
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>

--- fs/ecryptfs/crypto.c
+++ fs/ecryptfs/crypto.c
@@ -43,21 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ENCRYPT		1
 
 /**
- * ecryptfs_to_hex
- * @dst: Buffer to take hex character representation of contents of
- *       src; must be at least of size (src_size * 2)
- * @src: Buffer to be converted to a hex string representation
- * @src_size: number of bytes to convert
- */
-void ecryptfs_to_hex(char *dst, char *src, size_t src_size)
-{
-	int x;
-
-	for (x = 0; x < src_size; x++)
-		sprintf(&dst[x * 2], "%.2x", (unsigned char)src[x]);
-}
-
-/**
  * ecryptfs_from_hex
  * @dst: Buffer to take the bytes from src hex; must be at least of
  *       size (src_size / 2)
