400f79d4e5cf231c14d2e7f0e47f3a3d57e55f62
Christophe Leroy <christophe.leroy@c-s.fr>
2016-09-21 16:49:27 +0200

    powerpc/64: get rid of MIN_HUGEPTE_SHIFT
    MIN_HUGEPTE_SHIFT hasn't been used since commit d1837cba5d5d5
    ("powerpc/mm: Cleanup initialization of hugepages on powerpc")
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/nohash/64/pgtable-64k.h
+++ arch/powerpc/include/asm/nohash/64/pgtable-64k.h
@@ -32,9 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PTRS_PER_PMD	(1 << PMD_INDEX_SIZE)
 #define PTRS_PER_PGD	(1 << PGD_INDEX_SIZE)
 
-/* With 4k base page size, hugepage PTEs go at the PMD level */
-#define MIN_HUGEPTE_SHIFT	PAGE_SHIFT
-
 /* PMD_SHIFT determines what a second-level page table entry can map */
 #define PMD_SHIFT	(PAGE_SHIFT + PTE_INDEX_SIZE)
 #define PMD_SIZE	(1UL << PMD_SHIFT)
