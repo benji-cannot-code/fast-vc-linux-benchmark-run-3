014d6fed0e0e8169c7190e34488db9f92a4ebd2c
Christian Gromm <christian.gromm@microchip.com>
2016-09-21 14:49:05 +0200

    staging: most: make function most_submit_mbo return void
    Function most_submit_mbo() causes an exception only if either the pointer
    mbo or mbo->context equals NULL. From the underlying architecture's point
    of view both cases must _not_ come true and would happen only, if something
    has tampered with the pointers. This would render runtime code unable to
    recover anyway. So, instead trying to hide that things are already
    critically out of control we're better off with a WARN_ON() assertion.
    
    This patch replaces the return type of the function most_submit_mbo() with
    'void' and adds a WARN_ONCE() assertion. Additionally, code calling the
    function is adapted accordingly.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/aim-sound/sound.c
+++ drivers/staging/most/aim-sound/sound.c
@@ -235,7 +235,6 @@ static int playback_thread(void *data)
 	while (!kthread_should_stop()) {
 		struct mbo *mbo = NULL;
 		bool period_elapsed = false;
-		int ret;
 
 		wait_event_interruptible(
 			channel->playback_waitq,
@@ -251,10 +250,7 @@ static int playback_thread(void *data)
 		else
 			memset(mbo->virt_address, 0, mbo->buffer_length);
 
-		ret = most_submit_mbo(mbo);
-		if (ret)
-			channel->is_stream_running = false;
-
+		most_submit_mbo(mbo);
 		if (period_elapsed)
 			snd_pcm_period_elapsed(channel->substream);
 	}
