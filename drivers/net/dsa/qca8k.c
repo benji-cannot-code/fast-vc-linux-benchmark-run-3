679003a2aab5b576bb310d4b33f5064905ee6cd1
Wei Yongjun <weiyongjun1@huawei.com>
2016-09-21 15:04:43 +0000

    net: dsa: qca8k: fix non static symbol warning
    Fixes the following sparse warning:
    
    drivers/net/dsa/qca8k.c:259:22: warning:
     symbol 'qca8k_regmap_config' was not declared. Should it be static?
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/qca8k.c
+++ drivers/net/dsa/qca8k.c
@@ -257,7 +257,7 @@ static struct regmap_access_table qca8k_readable_table = {
 	.n_yes_ranges = ARRAY_SIZE(qca8k_readable_ranges),
 };
 
-struct regmap_config qca8k_regmap_config = {
+static struct regmap_config qca8k_regmap_config = {
 	.reg_bits = 16,
 	.val_bits = 32,
 	.reg_stride = 4,
