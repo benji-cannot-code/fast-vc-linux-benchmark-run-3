35026cc3979f5a0e23112b478c779b063cb0a1f2
Christoph Hellwig <hch@lst.de>
2016-09-22 07:48:17 -0700

    timekeeping: Include the correct header for errno definitions
    asm-generic headers are only defaults for architectures.  We need to get
    the proper defintion, which goes through <linux/errno.h> and <asm/errno.h>.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Cc: john.stultz@linaro.org
    Link: http://lkml.kernel.org/r/1474555697-8206-1-git-send-email-hch@lst.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/timekeeping.h
+++ include/linux/timekeeping.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_TIMEKEEPING_H
 #define _LINUX_TIMEKEEPING_H
 
-#include <asm-generic/errno-base.h>
+#include <linux/errno.h>
 
 /* Included from linux/ktime.h */
 
