2d0b0be4827f28981f413398beace8d686f6e3bf
Juliana Rodrigues <juliana.orod@gmail.com>
2016-09-22 00:24:40 -0300

    staging: wlan-ng: fixed block comment formatting
    This patch fixes a checkpatch warning in
    a block comment by adapting it to the community
    preferred coding style.
    
    Signed-off-by: Juliana Rodrigues <juliana.orod@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x.h
+++ drivers/staging/wlan-ng/hfa384x.h
@@ -111,7 +111,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define		HFA384x_ADDR_FLAT_CMD_OFF_MASK	(0x0000ffff)
 
 /* Mask bits for discarding unwanted pieces in AUX format
-   16-bit address parts */
+ * 16-bit address parts
+ */
 #define		HFA384x_ADDR_AUX_PAGE_MASK	(0xffff)
 #define		HFA384x_ADDR_AUX_OFF_MASK	(0x007f)
 
