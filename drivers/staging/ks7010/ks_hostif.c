af30876a1a6a762c0741ddc0aff04f2b7c934c7d
Johan Svensson <johan.svensson692@gmail.com>
2016-09-22 20:11:21 +0200

    staging: ks7010: add blank line after variable declarations
    Fixes warnings found by checkpatch
    Missing a blank line after declarations
    
    Signed-off-by: Johan Svensson <johan.svensson692@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_hostif.c
+++ drivers/staging/ks7010/ks_hostif.c
@@ -36,6 +36,7 @@ static
 inline u8 get_BYTE(struct ks_wlan_private *priv)
 {
 	u8 data;
+
 	data = *(priv->rxp)++;
 	/* length check in advance ! */
 	--(priv->rx_size);
@@ -46,6 +47,7 @@ static
 inline u16 get_WORD(struct ks_wlan_private *priv)
 {
 	u16 data;
+
 	data = (get_BYTE(priv) & 0xff);
 	data |= ((get_BYTE(priv) << 8) & 0xff00);
 	return data;
@@ -55,6 +57,7 @@ static
 inline u32 get_DWORD(struct ks_wlan_private *priv)
 {
 	u32 data;
+
 	data = (get_BYTE(priv) & 0xff);
 	data |= ((get_BYTE(priv) << 8) & 0x0000ff00);
 	data |= ((get_BYTE(priv) << 16) & 0x00ff0000);
