8d1c0bda0de8c592e4d6524f03a4b6f52aead78f
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2016-09-23 15:45:20 +0200

    staging: wlan-ng: avoid new typedef: p80211pstr_t
    This patch fixes the following checkpatch.pl warning in p80211types.h:
    WARNING: do not add new typedefs
    
    It applies for typedef p80211pstr_t
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/p80211types.h
+++ drivers/staging/wlan-ng/p80211types.h
@@ -216,9 +216,9 @@ struct p80211enum {
 /*  messages. */
 
 /* Template pascal string */
-typedef struct p80211pstr {
+struct p80211pstr {
 	u8 len;
-} __packed p80211pstr_t;
+} __packed;
 
 typedef struct p80211pstrd {
 	u8 len;
