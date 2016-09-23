a43a1e9dbbbf639b9b3e55923dde50ce8acc3c08
Andrzej Hajda <a.hajda@samsung.com>
2016-09-23 15:21:38 +0200

    drm/exynos: use drm core to handle page-flip event
    Exynos DRM framework handled page-flip event with custom code.
    The patch replaces it with drm-core vblank queue.
    
    Signed-off-by: Andrzej Hajda <a.hajda@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_vidi.c
+++ drivers/gpu/drm/exynos/exynos_drm_vidi.c
@@ -176,7 +176,6 @@ static const struct exynos_drm_crtc_ops vidi_crtc_ops = {
 static void vidi_fake_vblank_timer(unsigned long arg)
 {
 	struct vidi_context *ctx = (void *)arg;
-	int win;
 
 	if (ctx->pipe < 0)
 		return;
@@ -184,15 +183,6 @@ static void vidi_fake_vblank_timer(unsigned long arg)
 	if (drm_crtc_handle_vblank(&ctx->crtc->base))
 		mod_timer(&ctx->timer,
 			jiffies + msecs_to_jiffies(VIDI_REFRESH_TIME) - 1);
-
-	for (win = 0 ; win < WINDOWS_NR ; win++) {
-		struct exynos_drm_plane *plane = &ctx->planes[win];
-
-		if (!plane->pending_fb)
-			continue;
-
-		exynos_drm_crtc_finish_update(ctx->crtc, plane);
-	}
 }
 
 static ssize_t vidi_show_connection(struct device *dev,
