d78c1775d5cf24e2f3efdb28580a7d61cf62fc7c
David Sterba <dsterba@suse.com>
2016-09-23 13:57:06 +0200

    btrfs: tests: uninline member definitions in free_space_extent
    The recommended way is to put all members on separate lines.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/tests/free-space-tree-tests.c
+++ fs/btrfs/tests/free-space-tree-tests.c
@@ -25,7 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../transaction.h"
 
 struct free_space_extent {
-	u64 start, length;
+	u64 start;
+	u64 length;
 };
 
 static int __check_free_space_extents(struct btrfs_trans_handle *trans,
