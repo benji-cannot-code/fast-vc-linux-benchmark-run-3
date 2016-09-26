9278e7dcde28fe9972bd5c338aedd1c4b8f43c1c
Daeseok Youn <daeseok.youn@gmail.com>
2016-09-26 09:42:02 +0900

    staging: dgnc: kfree for board structure in dgnc_found_board()
    The board structure should be freed when any function was failed
    in dgnc_found_board(). And the board strucure will be stored
    into dgnc_board array when the dgnc_found_board() function has no error.
    
    Signed-off-by: Daeseok Youn <daeseok.youn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.c
+++ drivers/staging/dgnc/dgnc_driver.c
@@ -354,9 +354,7 @@ static int dgnc_found_board(struct pci_dev *pdev, int id)
 	int rc = 0;
 
 	/* get the board structure and prep it */
-	dgnc_board[dgnc_num_boards] = kzalloc(sizeof(*brd), GFP_KERNEL);
-	brd = dgnc_board[dgnc_num_boards];
-
+	brd = kzalloc(sizeof(*brd), GFP_KERNEL);
 	if (!brd)
 		return -ENOMEM;
 
@@ -412,7 +410,8 @@ static int dgnc_found_board(struct pci_dev *pdev, int id)
 		if (!brd->membase) {
 			dev_err(&brd->pdev->dev,
 				"Card has no PCI IO resources, failing.\n");
-			return -ENODEV;
+			rc = -ENODEV;
+			goto failed;
 		}
 
 		brd->membase_end = pci_resource_end(pdev, 4);
@@ -503,7 +502,8 @@ static int dgnc_found_board(struct pci_dev *pdev, int id)
 	default:
 		dev_err(&brd->pdev->dev,
 			"Didn't find any compatible Neo/Classic PCI boards.\n");
-		return -ENXIO;
+		rc = -ENXIO;
+		goto failed;
 	}
 
 	/*
@@ -540,14 +540,15 @@ static int dgnc_found_board(struct pci_dev *pdev, int id)
 
 	wake_up_interruptible(&brd->state_wait);
 
+	dgnc_board[dgnc_num_boards] = brd;
+
 	return 0;
 
 failed:
 	dgnc_tty_uninit(brd);
-	brd->state = BOARD_FAILED;
-	brd->dpastatus = BD_NOFEP;
+	kfree(brd);
 
-	return -ENXIO;
+	return rc;
 }
 
 static int dgnc_finalize_board_init(struct dgnc_board *brd)
