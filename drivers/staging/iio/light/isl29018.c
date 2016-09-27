3ba648c4ad2de2c02fca21e2b03ae0071915f7d9
Brian Masney <masneyb@onstation.org>
2016-09-26 20:20:20 -0400

    staging: iio: isl29018: check if the chip is in a suspended state
    Add a check to isl29018_write_raw() to ensure that the chip is not in a
    suspended state. This makes the code consistent with what is present
    in isl29018_read_raw().
    
    Signed-off-by: Brian Masney <masneyb@onstation.org>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/light/isl29018.c
+++ drivers/staging/iio/light/isl29018.c
@@ -356,6 +356,10 @@ static int isl29018_write_raw(struct iio_dev *indio_dev,
 	int ret = -EINVAL;
 
 	mutex_lock(&chip->lock);
+	if (chip->suspended) {
+		ret = -EBUSY;
+		goto write_done;
+	}
 	switch (mask) {
 	case IIO_CHAN_INFO_CALIBSCALE:
 		if (chan->type == IIO_LIGHT) {
@@ -375,8 +379,9 @@ static int isl29018_write_raw(struct iio_dev *indio_dev,
 	default:
 		break;
 	}
-	mutex_unlock(&chip->lock);
 
+write_done:
+	mutex_unlock(&chip->lock);
 	return ret;
 }
 
