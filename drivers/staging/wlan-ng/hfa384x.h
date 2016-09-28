e30927c032ddfb45b6dd9edc4132c648abc1294b
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2016-09-28 20:19:04 +0200

    staging: wlang-ng: avoid new typedef: hfa384x_dbmcommsquality_t
    This patch fixes the following checkpatch.pl warning in hfa384x.h:
    WARNING: do not add new typedefs
    
    It applies for typedef hfa384x_dbmcommsquality_t
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x.h
+++ drivers/staging/wlan-ng/hfa384x.h
@@ -265,7 +265,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *--------------------------------------------------------------------
  */
 #define		HFA384x_RID_DBMCOMMSQUALITY_LEN	 \
-	((u16)sizeof(hfa384x_dbmcommsquality_t))
+	((u16)sizeof(struct hfa384x_dbmcommsquality))
 #define		HFA384x_RID_JOINREQUEST_LEN \
 	((u16)sizeof(struct hfa384x_JoinRequest_data))
 
@@ -462,11 +462,11 @@ struct hfa384x_commsquality {
 } __packed;
 
 /*-- Information Record: dmbcommsquality --*/
-typedef struct hfa384x_dbmcommsquality {
+struct hfa384x_dbmcommsquality {
 	u16 CQdbm_currBSS;
 	u16 ASLdbm_currBSS;
 	u16 ANLdbm_currFC;
-} __packed hfa384x_dbmcommsquality_t;
+} __packed;
 
 /*--------------------------------------------------------------------
  * FRAME STRUCTURES: Communication Frames
