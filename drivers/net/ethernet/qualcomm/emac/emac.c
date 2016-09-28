3734dd1eadde298583b610cf5ac352ccfc90285e
Timur Tabi <timur@codeaurora.org>
2016-09-28 11:58:44 -0500

    net: qcom/emac: initial ACPI support
    Add support for reading addresses, interrupts, and _DSD properties
    from ACPI tables, just like with device tree.  The HID for the
    EMAC device itself is QCOM8070.  The internal PHY is represented
    by a child node with a HID of QCOM8071.
    
    The EMAC also has some complex clock initialization requirements
    that are not represented by this patch.  This will be addressed
    in a future patch.
    
    Signed-off-by: Timur Tabi <timur@codeaurora.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qualcomm/emac/emac.c
+++ drivers/net/ethernet/qualcomm/emac/emac.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_device.h>
 #include <linux/phy.h>
 #include <linux/platform_device.h>
+#include <linux/acpi.h>
 #include "emac.h"
 #include "emac-mac.h"
 #include "emac-phy.h"
@@ -576,6 +577,16 @@ static const struct of_device_id emac_dt_match[] = {
 	{}
 };
 
+#if IS_ENABLED(CONFIG_ACPI)
+static const struct acpi_device_id emac_acpi_match[] = {
+	{
+		.id = "QCOM8070",
+	},
+	{}
+};
+MODULE_DEVICE_TABLE(acpi, emac_acpi_match);
+#endif
+
 static int emac_probe(struct platform_device *pdev)
 {
 	struct net_device *netdev;
@@ -735,6 +746,7 @@ static struct platform_driver emac_platform_driver = {
 	.driver = {
 		.name		= "qcom-emac",
 		.of_match_table = emac_dt_match,
+		.acpi_match_table = ACPI_PTR(emac_acpi_match),
 	},
 };
 
