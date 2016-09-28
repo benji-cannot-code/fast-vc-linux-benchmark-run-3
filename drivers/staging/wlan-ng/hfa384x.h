780c86455001918e7e7c07c0eb0eaa2f5aebbb69
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2016-09-28 20:19:20 +0200

    staging: wlang-ng: avoid new typedef: hfa384x_infodata_t
    This patch fixes the following checkpatch.pl warning in hfa384x.h:
    WARNING: do not add new typedefs
    
    It applies for typedef hfa384x_infodata_t
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x.h
+++ drivers/staging/wlan-ng/hfa384x.h
@@ -762,7 +762,7 @@ struct hfa384x_KeyIDChanged {
 } __packed;
 
 /*--  Collection of all Inf frames ---------------*/
-typedef union hfa384x_infodata {
+union hfa384x_infodata {
 	struct hfa384x_CommTallies16 commtallies16;
 	struct hfa384x_CommTallies32 commtallies32;
 	struct hfa384x_ScanResult scanresult;
@@ -773,12 +773,12 @@ typedef union hfa384x_infodata {
 	struct hfa384x_AuthRequest authreq;
 	struct hfa384x_PSUserCount psusercnt;
 	struct hfa384x_KeyIDChanged keyidchanged;
-} __packed hfa384x_infodata_t;
+} __packed;
 
 typedef struct hfa384x_InfFrame {
 	u16 framelen;
 	u16 infotype;
-	hfa384x_infodata_t info;
+	union hfa384x_infodata info;
 } __packed hfa384x_InfFrame_t;
 
 /*--------------------------------------------------------------------
