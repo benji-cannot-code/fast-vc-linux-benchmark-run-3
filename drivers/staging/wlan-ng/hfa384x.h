226516fdd88829581dec9cc088ff729fb84516d9
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2016-09-28 20:19:08 +0200

    staging: wlang-ng: avoid new typedef: hfa384x_CommTallies32_t
    This patch fixes the following checkpatch.pl warning in hfa384x.h:
    WARNING: do not add new typedefs
    
    It applies for typedef hfa384x_CommTallies32_t
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x.h
+++ drivers/staging/wlan-ng/hfa384x.h
@@ -635,7 +635,7 @@ struct hfa384x_CommTallies16 {
 	u16 rxmsginbadmsgfrag;
 } __packed;
 
-typedef struct hfa384x_CommTallies32 {
+struct hfa384x_CommTallies32 {
 	u32 txunicastframes;
 	u32 txmulticastframes;
 	u32 txfragments;
@@ -657,7 +657,7 @@ typedef struct hfa384x_CommTallies32 {
 	u32 rxdiscardswepundecr;
 	u32 rxmsginmsgfrag;
 	u32 rxmsginbadmsgfrag;
-} __packed hfa384x_CommTallies32_t;
+} __packed;
 
 /*--  Inquiry Frame, Diagnose: Scan Results & Subfields--*/
 typedef struct hfa384x_ScanResultSub {
@@ -764,7 +764,7 @@ typedef struct hfa384x_KeyIDChanged {
 /*--  Collection of all Inf frames ---------------*/
 typedef union hfa384x_infodata {
 	struct hfa384x_CommTallies16 commtallies16;
-	hfa384x_CommTallies32_t commtallies32;
+	struct hfa384x_CommTallies32 commtallies32;
 	hfa384x_ScanResult_t scanresult;
 	hfa384x_ChInfoResult_t chinforesult;
 	hfa384x_HScanResult_t hscanresult;
@@ -1365,7 +1365,7 @@ typedef struct hfa384x {
 	struct hfa384x_caplevel cap_act_ap_mfi;	/* ap f/w to modem interface */
 
 	u32 psusercount;	/* Power save user count. */
-	hfa384x_CommTallies32_t tallies;	/* Communication tallies. */
+	struct hfa384x_CommTallies32 tallies;	/* Communication tallies. */
 	u8 comment[WLAN_COMMENT_MAX + 1];	/* User comment */
 
 	/* Channel Info request results (AP only) */
