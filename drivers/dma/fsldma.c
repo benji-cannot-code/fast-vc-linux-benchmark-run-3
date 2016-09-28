37c338f825581e376d0ad53306165cf363b11bb3
Arvind Yadav <arvind.yadav.cs@gmail.com>
2016-09-28 16:15:11 +0530

    dmaengine: fsldma: Unmap region obtained by of_iomap
    Free memory mapping, if probe is not successful.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Acked-by: Li Yang <leoyang.li@nxp.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/fsldma.c
+++ drivers/dma/fsldma.c
@@ -1352,7 +1352,7 @@ static int fsldma_of_probe(struct platform_device *op)
 	if (!fdev->regs) {
 		dev_err(&op->dev, "unable to ioremap registers\n");
 		err = -ENOMEM;
-		goto out_free_fdev;
+		goto out_free;
 	}
 
 	/* map the channel IRQ if it exists, but don't hookup the handler yet */
@@ -1417,6 +1417,8 @@ static int fsldma_of_probe(struct platform_device *op)
 
 out_free_fdev:
 	irq_dispose_mapping(fdev->irq);
+	iounmap(fdev->regs);
+out_free:
 	kfree(fdev);
 out_return:
 	return err;
