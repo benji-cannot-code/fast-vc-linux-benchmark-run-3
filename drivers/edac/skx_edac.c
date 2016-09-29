3f0d0c8222a1172d0b6a7ed968d7ca8730b4702a
Dave Hansen <dave.hansen@intel.com>
2016-09-29 13:43:21 -0700

    EDAC, {sb,skx}_edac: Use Intel model macros instead of open-coding them
    We now have symbolic names for a bunch of Intel CPU models via
    asm/intel-family.h. The original conversion missed the EDAC drivers.
    Convert them.
    
    Signed-off-by: Dave Hansen <dave.hansen@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Link: http://lkml.kernel.org/r/20160929204321.9FAE5F84@viggo.jf.intel.com
    [ Remove comment, macro name is descriptive enough. ]
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/skx_edac.c
+++ drivers/edac/skx_edac.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/math64.h>
 #include <linux/mod_devicetable.h>
 #include <asm/cpu_device_id.h>
+#include <asm/intel-family.h>
 #include <asm/processor.h>
 #include <asm/mce.h>
 
@@ -264,7 +265,7 @@ fail:
 }
 
 const struct x86_cpu_id skx_cpuids[] = {
-	{ X86_VENDOR_INTEL, 6, 0x55, 0, 0 },	/* Skylake */
+	{ X86_VENDOR_INTEL, 6, INTEL_FAM6_SKYLAKE_X, 0, 0 },
 	{ }
 };
 MODULE_DEVICE_TABLE(x86cpu, skx_cpuids);
