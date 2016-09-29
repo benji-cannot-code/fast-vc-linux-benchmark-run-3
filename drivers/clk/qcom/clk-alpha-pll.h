b60c72ebd941812fba614e2a60673a538ee6f8b0
Rajendra Nayak <rnayak@codeaurora.org>
2016-09-29 14:05:42 +0530

    clk: qcom: Add support for alpha pll hwfsm ops
    Some PLLs can support an HW FSM mode (different from the Votable FSMs,
    though its the same bit used to enable Votable FSMs as well as HW FSMs)
    which enables the HW to do the bypass/reset/enable-output-ctrl sequence
    on its own. So all thats needed from SW is to set the FSM_ENA bit.
    PLL_ACTIVE_FLAG is whats used to check if the PLL is active/enabled.
    
    Some of the PLLs which support HW FSM can also need an OFFLINE request
    that needs to be toggled across the enable/disable. We use a flag to
    identify such cases and handle them.
    
    Signed-off-by: Rajendra Nayak <rnayak@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -35,6 +35,8 @@ struct clk_alpha_pll {
 
 	const struct pll_vco *vco_table;
 	size_t num_vco;
+#define SUPPORTS_OFFLINE_REQ	BIT(0)
+	u8 flags;
 
 	struct clk_regmap clkr;
 };
@@ -53,6 +55,7 @@ struct clk_alpha_pll_postdiv {
 };
 
 extern const struct clk_ops clk_alpha_pll_ops;
+extern const struct clk_ops clk_alpha_pll_hwfsm_ops;
 extern const struct clk_ops clk_alpha_pll_postdiv_ops;
 
 #endif
