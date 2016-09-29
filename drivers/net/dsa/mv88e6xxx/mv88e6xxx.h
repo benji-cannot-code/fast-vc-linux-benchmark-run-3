bd1b3b3cd68431f6ffdda088e3fcff026888b60f
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2016-09-29 12:21:59 -0400

    net: dsa: mv88e6xxx: rename mv88e6xxx_ops
    The mv88e6xxx_ops is used to describe how to access the chip registers.
    It can be through SMI (via an MDIO bus), or via another interface such
    as crafted remote management frames.
    
    The correct BUS operations structure is chosen at runtime, depending on
    the chip address and connectivity.
    
    We will need the mv88e6xxx_ops name for future chip-wide operation
    structure, thus rename mv88e6xxx_ops to more explicit mv88e6xxx_bus_ops.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/mv88e6xxx/mv88e6xxx.h
+++ drivers/net/dsa/mv88e6xxx/mv88e6xxx.h
@@ -649,7 +649,7 @@ struct mv88e6xxx_vtu_entry {
 	u8	data[DSA_MAX_PORTS];
 };
 
-struct mv88e6xxx_ops;
+struct mv88e6xxx_bus_ops;
 
 struct mv88e6xxx_priv_port {
 	struct net_device *bridge_dev;
@@ -670,14 +670,14 @@ struct mv88e6xxx_chip {
 	/* The MII bus and the address on the bus that is used to
 	 * communication with the switch
 	 */
-	const struct mv88e6xxx_ops *smi_ops;
+	const struct mv88e6xxx_bus_ops *smi_ops;
 	struct mii_bus *bus;
 	int sw_addr;
 
 	/* Handles automatic disabling and re-enabling of the PHY
 	 * polling unit.
 	 */
-	const struct mv88e6xxx_ops *phy_ops;
+	const struct mv88e6xxx_bus_ops *phy_ops;
 	struct mutex		ppu_mutex;
 	int			ppu_disabled;
 	struct work_struct	ppu_work;
@@ -706,7 +706,7 @@ struct mv88e6xxx_chip {
 	struct mii_bus *mdio_bus;
 };
 
-struct mv88e6xxx_ops {
+struct mv88e6xxx_bus_ops {
 	int (*read)(struct mv88e6xxx_chip *chip, int addr, int reg, u16 *val);
 	int (*write)(struct mv88e6xxx_chip *chip, int addr, int reg, u16 val);
 };
