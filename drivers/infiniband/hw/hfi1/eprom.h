ddf57dd00b282dbb437fe2487797d5ecafb9dfea
Dean Luick <dean.luick@intel.com>
2016-09-30 04:41:40 -0700

    IB/hfi1: Add ability to read platform config from the EPROM
    Add a function to read the platform configuration file from
    the EPROM.
    
    Reviewed-by: Easwar Hariharan <easwar.hariharan@intel.com>
    Signed-off-by: Dean Luick <dean.luick@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hfi1/eprom.h
+++ drivers/infiniband/hw/hfi1/eprom.h
@@ -49,3 +49,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct hfi1_devdata;
 
 int eprom_init(struct hfi1_devdata *dd);
+int eprom_read_platform_config(struct hfi1_devdata *dd, void **buf_ret,
+			       u32 *size_ret);
