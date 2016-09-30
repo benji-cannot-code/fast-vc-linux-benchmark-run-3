a0865ab9534baa1d0cb445aa58917c70136a439a
Maxime Ripard <maxime.ripard@free-electrons.com>
2016-09-30 22:16:51 +0200

    clk: sunxi-ng: Implement minimum for multipliers
    Allow the CCU drivers to specify a multiplier for their clocks.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Acked-by: Chen-Yu Tsai <wens@csie.org>

--- drivers/clk/sunxi-ng/ccu_mult.h
+++ drivers/clk/sunxi-ng/ccu_mult.h
@@ -8,13 +8,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ccu_mult_internal {
 	u8	shift;
 	u8	width;
+	u8	min;
 };
 
-#define _SUNXI_CCU_MULT(_shift, _width)		\
-	{					\
-		.shift	= _shift,		\
-		.width	= _width,		\
+#define _SUNXI_CCU_MULT_MIN(_shift, _width, _min)	\
+	{						\
+		.shift	= _shift,			\
+		.width	= _width,			\
+		.min	= _min,				\
 	}
+
+#define _SUNXI_CCU_MULT(_shift, _width)		\
+	_SUNXI_CCU_MULT_MIN(_shift, _width, 1)
 
 struct ccu_mult {
 	u32			enable;
