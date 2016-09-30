e99f662661df45447f63a6e4a899bb343829e670
Eric Engestrom <eric.engestrom@imgtec.com>
2016-09-30 02:14:56 -0400

    ext4: remove unused variable
    Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>

--- fs/ext4/dir.c
+++ fs/ext4/dir.c
@@ -629,7 +629,7 @@ int ext4_check_all_de(struct inode *dir, struct buffer_head *bh, void *buf,
 		      int buf_size)
 {
 	struct ext4_dir_entry_2 *de;
-	int nlen, rlen;
+	int rlen;
 	unsigned int offset = 0;
 	char *top;
 
@@ -639,7 +639,6 @@ int ext4_check_all_de(struct inode *dir, struct buffer_head *bh, void *buf,
 		if (ext4_check_dir_entry(dir, NULL, de, bh,
 					 buf, buf_size, offset))
 			return -EFSCORRUPTED;
-		nlen = EXT4_DIR_REC_LEN(de->name_len);
 		rlen = ext4_rec_len_from_disk(de->rec_len, buf_size);
 		de = (struct ext4_dir_entry_2 *)((char *)de + rlen);
 		offset += rlen;
