4e275d5c993f659af892666ab2095b69ae3e2464
Ram Amrani <Ram.Amrani@caviumnetworks.com>
2016-10-01 21:59:56 +0300

    qede: Add qedr framework
    Adds a skeletal implementation of the qede RoCE driver -
    The qedr has some dependencies of the state of the underlying base
    interface. This adds some logic required with mutual registrations
    and the ability to pass updates on 'intresting' events.
    
    Signed-off-by: Ram Amrani <Ram.Amrani@caviumnetworks.com>
    Signed-off-by: Yuval Mintz <Yuval.Mintz@caviumnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qlogic/qede/qede.h
+++ drivers/net/ethernet/qlogic/qede/qede.h
@@ -107,6 +107,13 @@ struct qede_vlan {
 	bool configured;
 };
 
+struct qede_rdma_dev {
+	struct qedr_dev *qedr_dev;
+	struct list_head entry;
+	struct list_head roce_event_list;
+	struct workqueue_struct *roce_wq;
+};
+
 struct qede_dev {
 	struct qed_dev			*cdev;
 	struct net_device		*ndev;
@@ -186,6 +193,8 @@ struct qede_dev {
 	unsigned long			sp_flags;
 	u16				vxlan_dst_port;
 	u16				geneve_dst_port;
+
+	struct qede_rdma_dev		rdma_info;
 };
 
 enum QEDE_STATE {
