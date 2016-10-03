dfb03deba631fcf2b1dfc23e609b9710154e103a
Eric Anholt <eric@anholt.net>
2016-10-03 11:52:07 -0700

    staging/vchi: Update for rename of page_cache_release() to put_page().
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_2835_arm.c
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_2835_arm.c
@@ -440,7 +440,7 @@ create_pagelist(char __user *buf, size_t count, unsigned short type,
 			while (actual_pages > 0)
 			{
 				actual_pages--;
-				page_cache_release(pages[actual_pages]);
+				put_page(pages[actual_pages]);
 			}
 			kfree(pagelist);
 			if (actual_pages == 0)
@@ -579,7 +579,7 @@ free_pagelist(PAGELIST_T *pagelist, int actual)
 				offset = 0;
 				set_page_dirty(pg);
 			}
-			page_cache_release(pg);
+			put_page(pg);
 		}
 	}
 
