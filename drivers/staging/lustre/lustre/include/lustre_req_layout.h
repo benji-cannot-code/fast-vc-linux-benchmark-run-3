3219345530b4f1b5d12a628347a544594a07a5c9
Niu Yawei <yawei.niu@intel.com>
2016-10-02 22:28:26 -0400

    staging: lustre: quota: remove obsolete quota code
    Remove the obsolete quotacheck, quotaon and quotaoff, which
    were retained for the interoperability with old (< 2.4) client
    and server.
    
    Some other obsolete quota code related to LL_IOC_QUOTACTL_18,
    Q_INVLIDATE and Q_FINVALIDATE are removed as well.
    
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-5975
    Reviewed-on: http://review.whamcloud.com/14705
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre_req_layout.h
+++ drivers/staging/lustre/lustre/include/lustre_req_layout.h
@@ -166,9 +166,7 @@ extern struct req_format RQF_MDS_REINT_LINK;
 extern struct req_format RQF_MDS_REINT_RENAME;
 extern struct req_format RQF_MDS_REINT_SETATTR;
 extern struct req_format RQF_MDS_REINT_SETXATTR;
-extern struct req_format RQF_MDS_QUOTACHECK;
 extern struct req_format RQF_MDS_QUOTACTL;
-extern struct req_format RQF_QC_CALLBACK;
 extern struct req_format RQF_MDS_SWAP_LAYOUTS;
 /* MDS hsm formats */
 extern struct req_format RQF_MDS_HSM_STATE_GET;
@@ -181,7 +179,6 @@ extern struct req_format RQF_MDS_HSM_REQUEST;
 /* OST req_format */
 extern struct req_format RQF_OST_CONNECT;
 extern struct req_format RQF_OST_DISCONNECT;
-extern struct req_format RQF_OST_QUOTACHECK;
 extern struct req_format RQF_OST_QUOTACTL;
 extern struct req_format RQF_OST_GETATTR;
 extern struct req_format RQF_OST_SETATTR;
