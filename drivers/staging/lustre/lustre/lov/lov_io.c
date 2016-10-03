93e6942c84e0a37ab024f71017f20051824a76a4
Jinshan Xiong <jinshan.xiong@intel.com>
2016-10-02 22:28:22 -0400

    staging: lustre: llite: remove lli_has_smd
    Remove the lli_has_smd flag from struct ll_inode_info. The empty
    layout case will be handled by the LOV layer. Remove the unused
    function cl_local_size().
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-5814
    Reviewed-on: http://review.whamcloud.com/13690
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lov/lov_io.c
+++ drivers/staging/lustre/lustre/lov/lov_io.c
@@ -919,6 +919,13 @@ static void lov_empty_io_fini(const struct lu_env *env,
 		wake_up_all(&lov->lo_waitq);
 }
 
+static int lov_empty_io_submit(const struct lu_env *env,
+			       const struct cl_io_slice *ios,
+			       enum cl_req_type crt, struct cl_2queue *queue)
+{
+	return -EBADF;
+}
+
 static void lov_empty_impossible(const struct lu_env *env,
 				 struct cl_io_slice *ios)
 {
@@ -969,7 +976,7 @@ static const struct cl_io_operations lov_empty_io_ops = {
 			.cio_fini   = lov_empty_io_fini
 		}
 	},
-	.cio_submit                    = LOV_EMPTY_IMPOSSIBLE,
+	.cio_submit			= lov_empty_io_submit,
 	.cio_commit_async              = LOV_EMPTY_IMPOSSIBLE
 };
 
