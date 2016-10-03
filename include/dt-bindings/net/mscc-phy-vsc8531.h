c08afa8a9b6008e7c7161a100efc72c6b089ed21
Raju Lakkaraju <Raju.Lakkaraju@microsemi.com>
2016-10-03 12:53:13 +0530

    net: phy: Add Edge-rate driver for Microsemi PHYs.
    Edge-rate:
    As system and networking speeds increase, a signal's output transition,
    also know as the edge rate or slew rate (V/ns), takes on greater importance
    because high-speed signals come with a price. That price is an assortment of
    interference problems like ringing on the line, signal overshoot and
    undershoot, extended signal settling times, crosstalk noise, transmission
    line reflections, false signal detection by the receiving device and
    electromagnetic interference (EMI) -- all of which can negate the potential
    gains designers are seeking when they try to increase system speeds through
    the use of higher performance logic devices. The fact is, faster signaling
    edge rates can cause a higher level of electrical noise or other type of
    interference that can actually lead to slower line speeds and lower maximum
    system frequencies. This parameter allow the board designers to change the
    driving strange, and thereby change the EMI behavioral.
    
    Edge-rate parameters (vddmac, edge-slowdown) get from Device Tree.
    
    Tested on Beaglebone Black with VSC 8531 PHY.
    
    Signed-off-by: Raju Lakkaraju <Raju.Lakkaraju@microsemi.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/dt-bindings/net/mscc-phy-vsc8531.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Device Tree constants for Microsemi VSC8531 PHY
+ *
+ * Author: Nagaraju Lakkaraju
+ *
+ * License: Dual MIT/GPL
+ * Copyright (c) 2016 Microsemi Corporation
+ */
+
+#ifndef _DT_BINDINGS_MSCC_VSC8531_H
+#define _DT_BINDINGS_MSCC_VSC8531_H
+
+/* MAC interface Edge rate control VDDMAC in milli Volts */
+#define MSCC_VDDMAC_3300		 3300
+#define MSCC_VDDMAC_2500		 2500
+#define MSCC_VDDMAC_1800		 1800
+#define MSCC_VDDMAC_1500		 1500
+#define MSCC_VDDMAC_MAX			 4
+#define MSCC_SLOWDOWN_MAX		 8
+
+#endif
