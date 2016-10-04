cbe16ea59433876bc23df4e635e6d1968da46e4f
Rajat Jain <rajatja@google.com>
2016-10-04 13:37:42 -0700

    mwifiex: report wakeup for wowlan
    Register the WLAN device as a wakeup source since it can
    wake the system via wake-on-wireless-lan. In an actual wowlan
    event, notify the PM core that we are the current wakeup source.
    This allows the PM core to update the wakeup attributes in /sys.
    
    This was causing wakeup issues on chromeos as the system was
    apparently confused about the wakeup source.
    
    Signed-off-by: Wei-Ning Huang <wnhuang@google.com>
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Acked-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/mwifiex/sdio.h
+++ drivers/net/wireless/marvell/mwifiex/sdio.h
@@ -156,6 +156,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 } while (0)
 
 struct mwifiex_plt_wake_cfg {
+	struct device *dev;
 	int irq_wifi;
 	bool wake_by_wifi;
 };
