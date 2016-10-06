18d9cc5687d2b44520b0c0547186574bded2e630
Nelson Chang <nelson.chang@mediatek.com>
2016-10-06 19:44:02 +0800

    net: ethernet: mediatek: get hw lro capability by the chip id instead of by the dtsi
    Because hw lro started to be supported from MT7623, the proper way to check if
    the feature is capable is to judge by the chip id instead of by the dtsi.
    
    Signed-off-by: Nelson Chang <nelson.chang@mediatek.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.h
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.h
@@ -346,6 +346,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* ethernet subsystem chip id register */
 #define ETHSYS_CHIPID0_3	0x0
 #define ETHSYS_CHIPID4_7	0x4
+#define MT7623_ETH		7623
 
 /* ethernet subsystem config register */
 #define ETHSYS_SYSCFG0		0x14
