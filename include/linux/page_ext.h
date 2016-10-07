a78c43d209a8c2003db7fbdfb7d892c4fc0658d4
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2016-10-07 16:58:27 -0700

    mm/page_ext: support extra space allocation by page_ext user
    Until now, if some page_ext users want to use it's own field on
    page_ext, it should be defined in struct page_ext by hard-coding.  It
    has a problem that wastes memory in following situation.
    
      struct page_ext {
       #ifdef CONFIG_A
      	int a;
       #endif
       #ifdef CONFIG_B
      	int b;
       #endif
      };
    
    Assume that kernel is built with both CONFIG_A and CONFIG_B.  Even if we
    enable feature A and doesn't enable feature B at runtime, each entry of
    struct page_ext takes two int rather than one int.  It's undesirable
    result so this patch tries to fix it.
    
    To solve above problem, this patch implements to support extra space
    allocation at runtime.  When need() callback returns true, it's extra
    memory requirement is summed to entry size of page_ext.  Also, offset
    for each user's extra memory space is returned.  With this offset, user
    can use this extra space and there is no need to define needed field on
    page_ext by hard-coding.
    
    This patch only implements an infrastructure.  Following patch will use
    it for page_owner which is only user having it's own fields on page_ext.
    
    Link: http://lkml.kernel.org/r/1471315879-32294-6-git-send-email-iamjoonsoo.kim@lge.com
    Signed-off-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/page_ext.h
+++ include/linux/page_ext.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct pglist_data;
 struct page_ext_operations {
+	size_t offset;
+	size_t size;
 	bool (*need)(void);
 	void (*init)(void);
 };
