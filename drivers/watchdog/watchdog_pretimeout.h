35850106c8cb0a026596e1226945d3d63d496152
Vladimir Zapolskiy <vladimir_zapolskiy@mentor.com>
2016-10-07 15:39:57 +0300

    watchdog: pretimeout: add pretimeout_available_governors attribute
    The change adds an option to a user with CONFIG_WATCHDOG_SYSFS and
    CONFIG_WATCHDOG_PRETIMEOUT_GOV enabled to get information about all
    registered watchdog pretimeout governors by reading watchdog device
    attribute named "pretimeout_available_governors".
    
    Signed-off-by: Vladimir Zapolskiy <vladimir_zapolskiy@mentor.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/watchdog_pretimeout.h
+++ drivers/watchdog/watchdog_pretimeout.h
@@ -19,6 +19,7 @@ void watchdog_unregister_governor(struct watchdog_governor *gov);
 /* Interfaces to watchdog_dev.c */
 int watchdog_register_pretimeout(struct watchdog_device *wdd);
 void watchdog_unregister_pretimeout(struct watchdog_device *wdd);
+int watchdog_pretimeout_available_governors_get(char *buf);
 int watchdog_pretimeout_governor_get(struct watchdog_device *wdd, char *buf);
 int watchdog_pretimeout_governor_set(struct watchdog_device *wdd,
 				     const char *buf);
@@ -37,6 +38,11 @@ static inline int watchdog_register_pretimeout(struct watchdog_device *wdd)
 
 static inline void watchdog_unregister_pretimeout(struct watchdog_device *wdd)
 {
+}
+
+static inline int watchdog_pretimeout_available_governors_get(char *buf)
+{
+	return -EINVAL;
 }
 
 static inline int watchdog_pretimeout_governor_get(struct watchdog_device *wdd,
