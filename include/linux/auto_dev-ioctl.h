9740c848f9c9216a378a913922ed20e386d873ac
Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
2016-10-11 13:52:51 -0700

    autofs: remove AUTOFS_DEVID_LEN
    This macro was never used by neither kernel nor userspace, and also
    doesn't represent "devid length" in bytes.  (unless it was added to mean
    something else).
    
    Link: http://lkml.kernel.org/r/20160812024820.12352.21210.stgit@pluto.themaw.net
    Signed-off-by: Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
    Signed-off-by: Ian Kent <ikent@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/auto_dev-ioctl.h
+++ include/linux/auto_dev-ioctl.h
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AUTOFS_DEV_IOCTL_VERSION_MAJOR	1
 #define AUTOFS_DEV_IOCTL_VERSION_MINOR	0
 
-#define AUTOFS_DEVID_LEN		16
-
 #define AUTOFS_DEV_IOCTL_SIZE		sizeof(struct autofs_dev_ioctl)
 
 /*
