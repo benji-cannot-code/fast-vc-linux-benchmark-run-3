60451e9b5e6d72468b0680f7f8eb6e41466ddcfc
Wayne Porter <wporter82@gmail.com>
2016-10-11 21:56:50 +0000

    staging: rts5208: rtsx.c: Spacing
    Remove blank line after opening {
    
    Signed-off-by: Wayne Porter <wporter82@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rts5208/rtsx.c
+++ drivers/staging/rts5208/rtsx.c
@@ -537,7 +537,6 @@ static int rtsx_polling_thread(void *__dev)
 	wait_timeout((delay_use + 5) * 1000);
 
 	for (;;) {
-
 		set_current_state(TASK_INTERRUPTIBLE);
 		schedule_timeout(msecs_to_jiffies(POLLING_INTERVAL));
 
