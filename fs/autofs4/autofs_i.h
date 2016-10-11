e32edced5d9cc14082dab11da9e70abeb27149c6
Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
2016-10-11 13:53:16 -0700

    autofs: remove possibly misleading /* #define DEBUG */
    Having this in autofs_i.h gives illusion that uncommenting this enables
    pr_debug(), but it doesn't enable all the pr_debug() in autofs because
    inclusion order matters.
    
    XFS has the same DEBUG macro in its core header fs/xfs/xfs.h, however XFS
    seems to have a rule to include this prior to other XFS headers as well as
    kernel headers.  This is not the case with autofs, and DEBUG could be
    enabled via Makefile, so autofs should just get rid of this comment to
    make the code less confusing.  It's a comment, so there is literally no
    functional difference.
    
    Link: http://lkml.kernel.org/r/20160831033409.9910.77067.stgit@pluto.themaw.net
    Signed-off-by: Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
    Signed-off-by: Ian Kent <raven@themaw.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/autofs4/autofs_i.h
+++ fs/autofs4/autofs_i.h
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/current.h>
 #include <linux/uaccess.h>
 
-/* #define DEBUG */
-
 #ifdef pr_fmt
 #undef pr_fmt
 #endif
