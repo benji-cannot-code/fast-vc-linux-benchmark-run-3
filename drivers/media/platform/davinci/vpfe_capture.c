8a4999835d8a1812cada8e91a39e5e6b2d20831c
Markus Elfring <elfring@users.sourceforge.net>
2016-10-12 06:22:23 -0300

    [media] DaVinci-VPFE-Capture: Move two assignments in vpfe_s_input()
    Move assignments for two local variables into an else branch so that
    their setting will only be performed after corresponding data processing
    succeeded by this function.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/davinci/vpfe_capture.c
+++ drivers/media/platform/davinci/vpfe_capture.c
@@ -1110,7 +1110,7 @@ static int vpfe_s_input(struct file *file, void *priv, unsigned int index)
 	struct vpfe_subdev_info *sdinfo;
 	int subdev_index, inp_index;
 	struct vpfe_route *route;
-	u32 input = 0, output = 0;
+	u32 input, output;
 	int ret;
 
 	v4l2_dbg(1, debug, &vpfe_dev->v4l2_dev, "vpfe_s_input\n");
@@ -1143,6 +1143,9 @@ static int vpfe_s_input(struct file *file, void *priv, unsigned int index)
 	if (route && sdinfo->can_route) {
 		input = route->input;
 		output = route->output;
+	} else {
+		input = 0;
+		output = 0;
 	}
 
 	if (sd)
