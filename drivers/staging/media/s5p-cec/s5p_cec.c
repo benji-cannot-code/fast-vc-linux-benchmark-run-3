bf6426825e960248d60b27793a625678343073d7
Arnd Bergmann <arnd@arndb.de>
2016-10-13 11:39:04 -0300

    [media] s5p-cec: mark PM functions as __maybe_unused again
    A bugfix removed the two callers of s5p_cec_runtime_suspend
    and s5p_cec_runtime_resume, leading to the return of a harmless
    warning that I had previously fixed in commit aee8937089b1
    ("[media] s5p_cec: mark suspend/resume as __maybe_unused"):
    
    staging/media/s5p-cec/s5p_cec.c:234:12: error: ‘s5p_cec_runtime_suspend’ defined but not used [-Werror=unused-function]
    staging/media/s5p-cec/s5p_cec.c:242:12: error: ‘s5p_cec_runtime_resume’ defined but not used [-Werror=unused-function]
    
    This adds the __maybe_unused annotations to the function that
    were not removed and that are now unused when CONFIG_PM
    is disabled.
    
    Fixes: 57b978ada073 ("[media] s5p-cec: fix system and runtime PM integration")
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/staging/media/s5p-cec/s5p_cec.c
+++ drivers/staging/media/s5p-cec/s5p_cec.c
@@ -232,7 +232,7 @@ static int s5p_cec_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static int s5p_cec_runtime_suspend(struct device *dev)
+static int __maybe_unused s5p_cec_runtime_suspend(struct device *dev)
 {
 	struct s5p_cec_dev *cec = dev_get_drvdata(dev);
 
@@ -240,7 +240,7 @@ static int s5p_cec_runtime_suspend(struct device *dev)
 	return 0;
 }
 
-static int s5p_cec_runtime_resume(struct device *dev)
+static int __maybe_unused s5p_cec_runtime_resume(struct device *dev)
 {
 	struct s5p_cec_dev *cec = dev_get_drvdata(dev);
 	int ret;
