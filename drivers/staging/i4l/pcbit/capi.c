c5affeede121671337505274d8afecab8b884eb5
Alexander Alemayhu <alexander@alemayhu.com>
2016-10-13 05:57:20 +0200

    staging: i4l: pcbit: replace some include asm/*.h with linux/*.h
    Fixes the following checkpatch output:
    
    ./pcbit/capi.c:39: WARNING: Use #include <linux/io.h> instead of <asm/io.h>
    ./pcbit/capi.c:40: WARNING: Use #include <linux/string.h> instead of <asm/string.h>
    ./pcbit/callbacks.c:25: WARNING: Use #include <linux/io.h> instead of <asm/io.h>
    ./pcbit/layer2.c:39: WARNING: Use #include <linux/io.h> instead of <asm/io.h>
    ./pcbit/drv.c:34: WARNING: Use #include <linux/string.h> instead of <asm/string.h>
    ./pcbit/drv.c:35: WARNING: Use #include <linux/io.h> instead of <asm/io.h>
    ./pcbit/edss1.c:26: WARNING: Use #include <linux/io.h> instead of <asm/io.h>
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/i4l/pcbit/capi.c
+++ drivers/staging/i4l/pcbit/capi.c
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/skbuff.h>
 
-#include <asm/io.h>
-#include <asm/string.h>
+#include <linux/io.h>
+#include <linux/string.h>
 
 #include <linux/isdnif.h>
 
