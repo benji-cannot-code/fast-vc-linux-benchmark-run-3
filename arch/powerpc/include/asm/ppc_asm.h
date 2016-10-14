42d008570ada23604c5b5a31b94be490972eee2a
Michael Ellerman <mpe@ellerman.id.au>
2016-10-14 15:58:28 +1100

    powerpc/asm: Allow including ppc_asm.h in asm files
    There's no reason to #error if we include ppc_asm.h in asm files, the
    ifdef already prevents any problems.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/ppc_asm.h
+++ arch/powerpc/include/asm/ppc_asm.h
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ppc-opcode.h>
 #include <asm/firmware.h>
 
-#ifndef __ASSEMBLY__
-#error __FILE__ should only be used in assembler files
-#else
+#ifdef __ASSEMBLY__
 
 #define SZL			(BITS_PER_LONG/8)
 
@@ -780,5 +778,7 @@ END_FTR_SECTION_IFCLR(CPU_FTR_601)
 	.long 0xa6037b7d; /* mtsrr1 r11				*/ \
 	.long 0x2400004c  /* rfid				*/
 #endif /* !CONFIG_PPC_BOOK3E */
+
 #endif /*  __ASSEMBLY__ */
+
 #endif /* _ASM_POWERPC_PPC_ASM_H */
