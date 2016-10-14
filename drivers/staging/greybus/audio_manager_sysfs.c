644211bfaccc645fb7a958bd47a4f5b1ded185b5
Elise Lennion <elise.lennion@gmail.com>
2016-10-14 19:00:11 -0300

    staging: greybus: audio_manager_sysfs: Replace sscanf with kstrto* to single variable conversion.
    Fix checkpatch warning:
    
    WARNING: Prefer kstrto<type> to single variable sscanf
    
    kstrto* is designed to convert string to numerical value and makes
    it easier to understand what the code does.
    
    Signed-off-by: Elise Lennion <elise.lennion@gmail.com>
    Reviewed-by: Mark Greer <mgreer@animalcreek.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/greybus/audio_manager_sysfs.c
+++ drivers/staging/greybus/audio_manager_sysfs.c
@@ -45,7 +45,7 @@ static ssize_t manager_sysfs_remove_store(
 {
 	int id;
 
-	int num = sscanf(buf, "%d", &id);
+	int num = kstrtoint(buf, 10, &id);
 
 	if (num != 1)
 		return -EINVAL;
@@ -66,7 +66,7 @@ static ssize_t manager_sysfs_dump_store(
 {
 	int id;
 
-	int num = sscanf(buf, "%d", &id);
+	int num = kstrtoint(buf, 10, &id);
 
 	if (num == 1) {
 		num = gb_audio_manager_dump_module(id);
