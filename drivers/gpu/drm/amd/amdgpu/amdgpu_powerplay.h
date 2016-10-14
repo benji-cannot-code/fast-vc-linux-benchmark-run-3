3693a6b68b27aae9ed2280205e604da421d691ba
Alex Deucher <alexander.deucher@amd.com>
2016-10-14 11:27:15 -0400

    drm/amdgpu/powerplay: fix spelling in amdgpu_powerplay.h
    and update a comment as well.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_powerplay.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_powerplay.h
@@ -24,11 +24,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#ifndef __AMDGPU_POPWERPLAY_H__
-#define __AMDGPU_POPWERPLAY_H__
+#ifndef __AMDGPU_POWERPLAY_H__
+#define __AMDGPU_POWERPLAY_H__
 
 #include "amd_shared.h"
 
 extern const struct amd_ip_funcs amdgpu_pp_ip_funcs;
 
-#endif /* __AMDSOC_DM_H__ */
+#endif /* __AMDGPU_POWERPLAY_H__ */
