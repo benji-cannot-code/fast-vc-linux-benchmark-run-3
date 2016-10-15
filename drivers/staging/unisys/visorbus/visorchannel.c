e27f986b332c6ec353f8b3df99760e44b708ad9d
Laurent Navet <laurent.navet@gmail.com>
2016-10-15 11:50:11 +0200

    staging: unisys: visorbus: add missing include
    this removes lots of sparse warnings :
    warning: symbol 'visorchannel_destroy' was not declared. Should it be static?
    warning: symbol 'visorchannel_get_physaddr' was not declared. Should it be static?
    warning: symbol 'visorchannel_get_nbytes' was not declared. Should it be static?
    warning: symbol 'visorchannel_uuid_id' was not declared. Should it be static?
    warning: symbol 'visorchannel_id' was not declared. Should it be static?
    warning: symbol 'visorchannel_zoneid' was not declared. Should it be static?
    warning: symbol 'visorchannel_get_clientpartition' was not declared. Should it be static?
    warning: symbol 'visorchannel_set_clientpartition' was not declared. Should it be static?
    warning: symbol 'visorchannel_read' was not declared. Should it be static?
    warning: symbol 'visorchannel_write' was not declared. Should it be static?
    warning: symbol 'visorchannel_get_header' was not declared. Should it be static?
    warning: symbol 'visorchannel_create' was not declared. Should it be static?
    warning: symbol 'visorchannel_create_with_lock' was not declared. Should it be static?
    
    Signed-off-by: Laurent Navet <laurent.navet@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorchannel.c
+++ drivers/staging/unisys/visorbus/visorchannel.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 
 #include "visorbus.h"
+#include "visorbus_private.h"
 #include "controlvmchannel.h"
 
 #define MYDRVNAME "visorchannel"
