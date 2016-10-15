7372c6a1e87b69275b023a4feeba15a3a0c04f89
Lars-Peter Clausen <lars@metafoo.de>
2016-10-15 19:32:51 +0200

    ASoC: wm9705: Remove unused DAI ID defines
    The DAI ID defines are back from the time when DAIs were referenced by a
    numerical ID. These days a string is used for matching instead and the
    defines are unused. The last user of these defines was removed in commit
    f0fba2ad1b6b ("ASoC: multi-component - ASoC Multi-Component Support"). So
    remove the defines as well.
    
    This also means the wm9705.h file no longer has any content and can be
    removed.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/wm9705.c
+++ sound/soc/codecs/wm9705.c
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/initval.h>
 #include <sound/soc.h>
 
-#include "wm9705.h"
-
 #define WM9705_VENDOR_ID 0x574d4c05
 #define WM9705_VENDOR_ID_MASK 0xffffffff
 
