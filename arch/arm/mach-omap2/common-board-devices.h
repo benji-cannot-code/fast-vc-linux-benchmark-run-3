201aafb91c634f5da2c5eb03fe4518c2391b21b9
Tony Lindgren <tony@atomide.com>
2016-10-17 00:30:02 -0700

    ARM: OMAP2+: Remove legacy twl4030 platform init code
    This code is no longer used and can be removed as we
    are using device tree.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/common-board-devices.h
+++ arch/arm/mach-omap2/common-board-devices.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <sound/tlv320aic3x.h>
 #include <linux/mfd/menelaus.h>
-#include "twl-common.h"
 
 #define NAND_BLOCK_SIZE	SZ_128K
 
