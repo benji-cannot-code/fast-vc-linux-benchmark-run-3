9092d9ebc7317a650c2fd8053c2ce3ccf7438cfa
Thomas Falcon <tlfalcon@linux.vnet.ibm.com>
2016-10-17 15:56:29 -0500

    ibmvnic: Driver Version 1.0.1
    Increment driver version to reflect features that have
    been added since release.
    
    Signed-off-by: Thomas Falcon <tlfalcon@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ibm/ibmvnic.h
+++ drivers/net/ethernet/ibm/ibmvnic.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**************************************************************************/
 
 #define IBMVNIC_NAME		"ibmvnic"
-#define IBMVNIC_DRIVER_VERSION	"1.0"
+#define IBMVNIC_DRIVER_VERSION	"1.0.1"
 #define IBMVNIC_INVALID_MAP	-1
 #define IBMVNIC_STATS_TIMEOUT	1
 /* basic structures plus 100 2k buffers */
