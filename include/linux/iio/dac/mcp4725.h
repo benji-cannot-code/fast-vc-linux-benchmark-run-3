de16b3a96f3342572ee8537125738bedd53083f6
Tomas Novotny <tomas@novotny.cz>
2016-10-18 19:43:08 +0200

    iio: dac: mcp4725: support voltage reference selection
    MCP47x6 chip supports selection of a voltage reference (VDD, VREF buffered
    or unbuffered). MCP4725 doesn't have this feature thus the eventual setting
    is ignored and user is warned.
    
    The setting is stored only in the volatile memory of the chip. You need to
    manually store it to the EEPROM of the chip via 'store_eeprom' sysfs entry.
    
    Signed-off-by: Tomas Novotny <tomas@novotny.cz>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- include/linux/iio/dac/mcp4725.h
+++ include/linux/iio/dac/mcp4725.h
@@ -10,7 +10,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef IIO_DAC_MCP4725_H_
 #define IIO_DAC_MCP4725_H_
 
+/**
+ * struct mcp4725_platform_data - MCP4725/6 DAC specific data.
+ * @use_vref: Whether an external reference voltage on Vref pin should be used.
+ *            Additional vref-supply must be specified when used.
+ * @vref_buffered: Controls buffering of the external reference voltage.
+ *
+ * Vref related settings are available only on MCP4756. See
+ * Documentation/devicetree/bindings/iio/dac/mcp4725.txt for more information.
+ */
 struct mcp4725_platform_data {
+	bool use_vref;
+	bool vref_buffered;
 };
 
 #endif /* IIO_DAC_MCP4725_H_ */
