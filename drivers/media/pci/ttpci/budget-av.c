27544831c54adae9c7e41fd77a0aaba8b26e9466
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-10-18 17:44:07 -0200

    [media] ttpci: don't break long lines
    Due to the 80-cols restrictions, and latter due to checkpatch
    warnings, several strings were broken into multiple lines. This
    is not considered a good practice anymore, as it makes harder
    to grep for strings at the source code.
    
    As we're right now fixing other drivers due to KERN_CONT, we need
    to be able to identify what printk strings don't end with a "\n".
    It is a way easier to detect those if we don't break long lines.
    
    So, join those continuation lines.
    
    The patch was generated via the script below, and manually
    adjusted if needed.
    
    </script>
    use Text::Tabs;
    while (<>) {
    	if ($next ne "") {
    		$c=$_;
    		if ($c =~ /^\s+\"(.*)/) {
    			$c2=$1;
    			$next =~ s/\"\n$//;
    			$n = expand($next);
    			$funpos = index($n, '(');
    			$pos = index($c2, '",');
    			if ($funpos && $pos > 0) {
    				$s1 = substr $c2, 0, $pos + 2;
    				$s2 = ' ' x ($funpos + 1) . substr $c2, $pos + 2;
    				$s2 =~ s/^\s+//;
    
    				$s2 = ' ' x ($funpos + 1) . $s2 if ($s2 ne "");
    
    				print unexpand("$next$s1\n");
    				print unexpand("$s2\n") if ($s2 ne "");
    			} else {
    				print "$next$c2\n";
    			}
    			$next="";
    			next;
    		} else {
    			print $next;
    		}
    		$next="";
    	} else {
    		if (m/\"$/) {
    			if (!m/\\n\"$/) {
    				$next=$_;
    				next;
    			}
    		}
    	}
    	print $_;
    }
    </script>
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/ttpci/budget-av.c
+++ drivers/media/pci/ttpci/budget-av.c
@@ -1637,5 +1637,4 @@ module_exit(budget_av_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ralph Metzler, Marcus Metzler, Michael Hunold, others");
-MODULE_DESCRIPTION("driver for the SAA7146 based so-called "
-		   "budget PCI DVB w/ analog input and CI-module (e.g. the KNC cards)");
+MODULE_DESCRIPTION("driver for the SAA7146 based so-called budget PCI DVB w/ analog input and CI-module (e.g. the KNC cards)");
