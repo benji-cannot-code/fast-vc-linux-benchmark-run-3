88bc6d129de0f6ac5e7c3f153d1611d83c753e17
Wei Yongjun <weiyongjun1@huawei.com>
2016-10-18 14:37:26 +0000

    staging: i4l: pcbit: drv: remove duplicated include from drv.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/i4l/pcbit/drv.c
+++ drivers/staging/i4l/pcbit/drv.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/mm.h>
 #include <linux/interrupt.h>
-#include <linux/string.h>
 #include <linux/skbuff.h>
 
 #include <linux/isdnif.h>
