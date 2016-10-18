d5de85b13e1dfc5f9d79c013d493210e408b7358
Wei Yongjun <weiyongjun1@huawei.com>
2016-10-18 14:37:10 +0000

    staging: i4l: pcbit: remove duplicated include from capi.c
    Remove duplicated include.
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/i4l/pcbit/capi.c
+++ drivers/staging/i4l/pcbit/capi.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *              encode our number in CallerPN and ConnectedPN
  */
 
-#include <linux/string.h>
 #include <linux/kernel.h>
 
 #include <linux/types.h>
