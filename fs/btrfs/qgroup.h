9f18ddc69829ff7c71b06592dfbd46c4356affc2
Qu Wenruo <quwenruo@cn.fujitsu.com>
2016-10-18 09:31:26 +0800

    btrfs: qgroup: Add comments explaining how btrfs qgroup works
    Add explaination how btrfs qgroups work.
    
    Qgroup is split into 3 main phrases:
    1) Reserve
       To ensure qgroup doesn't exceed its limit
    
    2) Trace
       To info qgroup to trace which extent
    
    3) Account
       Calculate qgroup number change for each traced extent.
    
    This should save quite some time for new developers.
    
    Signed-off-by: Qu Wenruo <quwenruo@cn.fujitsu.com>
    Reviewed-by: Goldwyn Rodrigues <rgoldwyn@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/qgroup.h
+++ fs/btrfs/qgroup.h
@@ -24,6 +24,34 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "delayed-ref.h"
 
 /*
+ * Btrfs qgroup overview
+ *
+ * Btrfs qgroup splits into 3 main part:
+ * 1) Reserve
+ *    Reserve metadata/data space for incoming operations
+ *    Affect how qgroup limit works
+ *
+ * 2) Trace
+ *    Tell btrfs qgroup to trace dirty extents.
+ *
+ *    Dirty extents including:
+ *    - Newly allocated extents
+ *    - Extents going to be deleted (in this trans)
+ *    - Extents whose owner is going to be modified
+ *
+ *    This is the main part affects whether qgroup numbers will stay
+ *    consistent.
+ *    Btrfs qgroup can trace clean extents and won't cause any problem,
+ *    but it will consume extra CPU time, it should be avoided if possible.
+ *
+ * 3) Account
+ *    Btrfs qgroup will updates its numbers, based on dirty extents traced
+ *    in previous step.
+ *
+ *    Normally at qgroup rescan and transaction commit time.
+ */
+
+/*
  * Record a dirty extent, and info qgroup to update quota on it
  * TODO: Use kmem cache to alloc it.
  */
