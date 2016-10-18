50637d45ed5052b89d037857a21a3df75289529b
Eric Dumazet <edumazet@google.com>
2016-10-18 13:24:07 -0700

    tcp: do not export sysctl_tcp_low_latency
    Since commit b2fb4f54ecd4 ("tcp: uninline tcp_prequeue()") we no longer
    access sysctl_tcp_low_latency from a module.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -87,7 +87,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int sysctl_tcp_tw_reuse __read_mostly;
 int sysctl_tcp_low_latency __read_mostly;
-EXPORT_SYMBOL(sysctl_tcp_low_latency);
 
 #ifdef CONFIG_TCP_MD5SIG
 static int tcp_v4_md5_hash_hdr(char *md5_hash, const struct tcp_md5sig_key *key,
