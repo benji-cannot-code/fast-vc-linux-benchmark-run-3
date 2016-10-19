37d66ea261d3ca97e094fe00eb79ae2381ae7fa4
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2016-10-19 20:49:50 +0200

    staging: rtl8188eu: os_dep: fix block comment alignment issue
    Fix coding style issue in block comment in osdep_service.c found by
    checkpatch tool
    
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/os_dep/osdep_service.c
+++ drivers/staging/rtl8188eu/os_dep/osdep_service.c
@@ -23,9 +23,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <rtw_ioctl_set.h>
 
 /*
-* Translate the OS dependent @param error_code to OS independent RTW_STATUS_CODE
-* @return: one of RTW_STATUS_CODE
-*/
+ * Translate the OS dependent @param error_code to OS independent
+ * RTW_STATUS_CODE
+ * @return: one of RTW_STATUS_CODE
+ */
 inline int RTW_STATUS_CODE(int error_code)
 {
 	if (error_code >= 0)
