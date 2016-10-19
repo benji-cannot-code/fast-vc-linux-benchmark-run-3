6d0a46a7e017f4bccb0d67d573bf955bb6c35709
Leon Romanovsky <leon@kernel.org>
2016-10-19 20:13:07 +0300

    IB/hns: Move HNS RoCE user vendor structures
    This patch moves HNS vendor's specific structures to
    common UAPI folder which will be visible to all consumers.
    
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hns/hns_roce_cq.c
+++ drivers/infiniband/hw/hns/hns_roce_cq.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hns_roce_device.h"
 #include "hns_roce_cmd.h"
 #include "hns_roce_hem.h"
-#include "hns_roce_user.h"
+#include <rdma/hns-abi.h>
 #include "hns_roce_common.h"
 
 static void hns_roce_ib_cq_comp(struct hns_roce_cq *hr_cq)
