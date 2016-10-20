e3044ccc2320b8f6cbf47e2abd6086983026019f
Eva Rachel Retuya <eraretuya@gmail.com>
2016-10-20 17:19:23 +0800

    tools: iio: iio_generic_buffer: rename and change type of force variable
    Replace the type of 'force' flag from int to bool and at the same time
    rename it to 'force_autochannels' for better readability.
    
    Suggested-by: Peter Meerwald-Stadler <pmeerw@pmeerw.net>
    Signed-off-by: Eva Rachel Retuya <eraretuya@gmail.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- tools/iio/iio_generic_buffer.c
+++ tools/iio/iio_generic_buffer.c
@@ -349,7 +349,7 @@ int main(int argc, char **argv)
 	int noevents = 0;
 	int notrigger = 0;
 	char *dummy;
-	int force = 0;
+	bool force_autochannels = false;
 
 	struct iio_channel_info *channels = NULL;
 
@@ -363,7 +363,7 @@ int main(int argc, char **argv)
 			break;
 		case 'A':
 			autochannels = AUTOCHANNELS_ENABLED;
-			force = 1;
+			force_autochannels = true;
 			break;	
 		case 'c':
 			errno = 0;
@@ -527,14 +527,15 @@ int main(int argc, char **argv)
 			"diag %s\n", dev_dir_name);
 		goto error;
 	}
-	if ((num_channels && autochannels == AUTOCHANNELS_ENABLED) && !force) {
+	if ((num_channels && autochannels == AUTOCHANNELS_ENABLED) &&
+	    !force_autochannels) {
 		fprintf(stderr, "Auto-channels selected but some channels "
 			"are already activated in sysfs\n");
 		fprintf(stderr, "Proceeding without activating any channels\n");
 	}
 
 	if ((!num_channels && autochannels == AUTOCHANNELS_ENABLED) ||
-	    ((autochannels == AUTOCHANNELS_ENABLED) && force)) {
+	    ((autochannels == AUTOCHANNELS_ENABLED) && force_autochannels)) {
 		fprintf(stderr, "Enabling all channels\n");
 
 		ret = enable_disable_all_channels(dev_dir_name, 1);
