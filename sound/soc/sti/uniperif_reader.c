08c48be4de269df0addc9b610927a3c7bea2ec66
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2016-10-24 16:42:54 +0200

    ASoC: sti: clean unused include
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sti/uniperif_reader.c
+++ sound/soc/sti/uniperif_reader.c
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * License terms:  GNU General Public License (GPL), version 2
  */
 
-#include <linux/clk.h>
-#include <linux/io.h>
-
 #include <sound/soc.h>
 
 #include "uniperif.h"
