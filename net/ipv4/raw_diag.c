bfd572fb73ac5465761c75821b8a27ec224b3ea7
Arnd Bergmann <arnd@arndb.de>
2016-10-25 17:53:22 +0200

    net: ip, diag: include net/inet_sock.h
    The newly added raw_diag.c fails to build in some configurations
    unless we include this header:
    
    In file included from net/ipv4/raw_diag.c:6:0:
    include/net/raw.h:71:21: error: field 'inet' has incomplete type
    net/ipv4/raw_diag.c: In function 'raw_diag_dump':
    net/ipv4/raw_diag.c:166:29: error: implicit declaration of function 'inet_sk'
    
    Fixes: 432490f9d455 ("net: ip, diag -- Add diag interface for raw sockets")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/raw_diag.c
+++ net/ipv4/raw_diag.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/inet_diag.h>
 #include <linux/sock_diag.h>
 
+#include <net/inet_sock.h>
 #include <net/raw.h>
 #include <net/rawv6.h>
 
