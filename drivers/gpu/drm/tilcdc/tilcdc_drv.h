e42cd1626c4c69db29fbc899ef421a810c8c9151
Jyri Sarha <jsarha@ti.com>
2016-10-25 12:27:31 +0300

    drm/tilcdc: Fix race from forced shutdown of crtc in unload
    Fix race from forced shutdown of crtc in unload by adding internal
    locking and a boolean telling if device is going to be shutdown.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- drivers/gpu/drm/tilcdc/tilcdc_drv.h
+++ drivers/gpu/drm/tilcdc/tilcdc_drv.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_fb_cma_helper.h>
+#include <drm/drm_bridge.h>
 
 /* Defaulting to pixel clock defined on AM335x */
 #define TILCDC_DEFAULT_MAX_PIXELCLOCK  126000
@@ -174,7 +175,7 @@ void tilcdc_crtc_set_simulate_vesa_sync(struct drm_crtc *crtc,
 					bool simulate_vesa_sync);
 int tilcdc_crtc_mode_valid(struct drm_crtc *crtc, struct drm_display_mode *mode);
 int tilcdc_crtc_max_width(struct drm_crtc *crtc);
-void tilcdc_crtc_off(struct drm_crtc *crtc);
+void tilcdc_crtc_shutdown(struct drm_crtc *crtc);
 int tilcdc_crtc_update_fb(struct drm_crtc *crtc,
 		struct drm_framebuffer *fb,
 		struct drm_pending_vblank_event *event);
