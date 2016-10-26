c6e0da5a53cccda36de26120aac2c0a8eb57f1c6
Tobias Klauser <tklauser@distanz.ch>
2016-10-26 11:00:12 +0200

    NFC: nfcmrvl: Include unaligned.h instead of access_ok.h
    Including linux/unaligned/access_ok.h causes the allmodconfig build on
    ia64 (and maybe others) to fail with the following warnings:
    
    include/linux/unaligned/access_ok.h:7:19: error: redefinition of 'get_unaligned_le16'
    include/linux/unaligned/access_ok.h:12:19: error: redefinition of 'get_unaligned_le32'
    include/linux/unaligned/access_ok.h:17:19: error: redefinition of 'get_unaligned_le64'
    include/linux/unaligned/access_ok.h:22:19: error: redefinition of 'get_unaligned_be16'
    include/linux/unaligned/access_ok.h:27:19: error: redefinition of 'get_unaligned_be32'
    include/linux/unaligned/access_ok.h:32:19: error: redefinition of 'get_unaligned_be64'
    include/linux/unaligned/access_ok.h:37:20: error: redefinition of 'put_unaligned_le16'
    include/linux/unaligned/access_ok.h:42:20: error: redefinition of 'put_unaligned_le32'
    include/linux/unaligned/access_ok.h:42:20: error: redefinition of 'put_unaligned_le64'
    include/linux/unaligned/access_ok.h:42:20: error: redefinition of 'put_unaligned_be16'
    include/linux/unaligned/access_ok.h:42:20: error: redefinition of 'put_unaligned_be32'
    include/linux/unaligned/access_ok.h:42:20: error: redefinition of 'put_unaligned_be64'
    
    Fix these by including asm/unaligned.h instead and leave it up to the
    architecture to decide how to implement unaligned accesses.
    
    Fixes: 3194c6870158 ("NFC: nfcmrvl: add firmware download support")
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Link: https://lkml.org/lkml/2016/10/22/247
    Cc: Vincent Cuissard <cuissard@marvell.com>
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/nfcmrvl/fw_dnld.c
+++ drivers/nfc/nfcmrvl/fw_dnld.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/module.h>
-#include <linux/unaligned/access_ok.h>
+#include <asm/unaligned.h>
 #include <linux/firmware.h>
 #include <linux/nfc.h>
 #include <net/nfc/nci.h>
