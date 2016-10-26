6209f7699b7c8fdf41fbe2f8d26468ccab7d67a8
Stuart Yoder <stuart.yoder@nxp.com>
2016-10-26 11:20:31 -0500

    staging: fsl-mc: cleanup: improve clarity of messages
    improve some messages by shortening and rewording
    
    Signed-off-by: Stuart Yoder <stuart.yoder@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/fsl-mc-allocator.c
+++ drivers/staging/fsl-mc/bus/fsl-mc-allocator.c
@@ -606,7 +606,7 @@ static int fsl_mc_allocator_probe(struct fsl_mc_device *mc_dev)
 		return error;
 
 	dev_dbg(&mc_dev->dev,
-		"Allocatable MC object device bound to fsl_mc_allocator driver");
+		"Allocatable fsl-mc device bound to fsl_mc_allocator driver");
 	return 0;
 }
 
@@ -628,7 +628,7 @@ static int fsl_mc_allocator_remove(struct fsl_mc_device *mc_dev)
 	}
 
 	dev_dbg(&mc_dev->dev,
-		"Allocatable MC object device unbound from fsl_mc_allocator driver");
+		"Allocatable fsl-mc device unbound from fsl_mc_allocator driver");
 	return 0;
 }
 
