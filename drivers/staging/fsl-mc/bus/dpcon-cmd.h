8bb33c19bb12861714316a0f59f0410b15c4ae53
Stuart Yoder <stuart.yoder@nxp.com>
2016-10-26 11:20:33 -0500

    staging: fsl-mc: update copyright dates
    Many source files have evolved without copyright date
    updates.  Update the dates to reflect work through
    2016.
    
    Signed-off-by: Stuart Yoder <stuart.yoder@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/dpcon-cmd.h
+++ drivers/staging/fsl-mc/bus/dpcon-cmd.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright 2013-2015 Freescale Semiconductor Inc.
+ * Copyright 2013-2016 Freescale Semiconductor Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
--- drivers/staging/fsl-mc/bus/fsl-mc-bus.c
+++ drivers/staging/fsl-mc/bus/fsl-mc-bus.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Freescale Management Complex (MC) bus driver
  *
- * Copyright (C) 2014 Freescale Semiconductor, Inc.
+ * Copyright (C) 2014-2016 Freescale Semiconductor, Inc.
  * Author: German Rivera <German.Rivera@freescale.com>
  *
  * This file is licensed under the terms of the GNU General Public
