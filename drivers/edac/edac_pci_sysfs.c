cfdaaf5b1ce265feeda8bb0d393c43a862eb60cc
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-10-26 16:13:43 -0200

    edac: move documentation from edac_pci*.c to edac_pci.h
    Several functions are documented at edac_pci.c and edac_pci_sysfs.c.
    
    As we'll be including edac_pci.h at drivers-api book, move those,
    in order for the kernel-doc markups be part of the API
    documentation book.
    
    As several of those kernel-doc macros are not in the right format,
    fix them.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/edac/edac_pci_sysfs.c
+++ drivers/edac/edac_pci_sysfs.c
@@ -419,12 +419,6 @@ static void edac_pci_main_kobj_teardown(void)
 	}
 }
 
-/*
- *
- * edac_pci_create_sysfs
- *
- *	Create the controls/attributes for the specified EDAC PCI device
- */
 int edac_pci_create_sysfs(struct edac_pci_ctl_info *pci)
 {
 	int err;
@@ -460,11 +454,6 @@ unregister_cleanup:
 	return err;
 }
 
-/*
- * edac_pci_remove_sysfs
- *
- *	remove the controls and attributes for this EDAC PCI device
- */
 void edac_pci_remove_sysfs(struct edac_pci_ctl_info *pci)
 {
 	edac_dbg(0, "index=%d\n", pci->pci_idx);
