0b6e0e7f7aca964c4976034beca536aa808d3a17
Fenghua Yu <fenghua.yu@intel.com>
2016-10-28 15:04:44 -0700

    x86/intel_rdt: Add mkdir to resctrl file system
    Resource control groups are represented as directories in the resctrl
    file system. The root directory describes the default resources available
    to tasks that have not been assigned specific resources. Other directories
    can be created at the root level to make new resource groups. It is not
    permitted to make directories within other directories.
    
    Hardware uses a CLOSID (Class of service ID) to determine which resource
    limits are currently in effect. The exact number available is enumerated
    by CPUID leaf 0x10, but on current implementations it is a small number.
    We implement a simple bitmask allocator for CLOSIDs.
    
    Each resource control group uses one CLOSID, which limits the total number
    of directories that can be created.
    
    Resource groups can be removed using rmdir.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Cc: "Ravi V Shankar" <ravi.v.shankar@intel.com>
    Cc: "Tony Luck" <tony.luck@intel.com>
    Cc: "Shaohua Li" <shli@fb.com>
    Cc: "Sai Prakhya" <sai.praneeth.prakhya@intel.com>
    Cc: "Peter Zijlstra" <peterz@infradead.org>
    Cc: "Stephane Eranian" <eranian@google.com>
    Cc: "Dave Hansen" <dave.hansen@intel.com>
    Cc: "David Carrillo-Cisneros" <davidcc@google.com>
    Cc: "Nilay Vaish" <nilayvaish@gmail.com>
    Cc: "Vikas Shivappa" <vikas.shivappa@linux.intel.com>
    Cc: "Ingo Molnar" <mingo@elte.hu>
    Cc: "Borislav Petkov" <bp@suse.de>
    Cc: "H. Peter Anvin" <h.peter.anvin@intel.com>
    Link: http://lkml.kernel.org/r/1477692289-37412-6-git-send-email-fenghua.yu@intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/intel_rdt.h
+++ arch/x86/include/asm/intel_rdt.h
@@ -15,12 +15,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @kn:				kernfs node
  * @rdtgroup_list:		linked list for all rdtgroups
  * @closid:			closid for this rdtgroup
+ * @flags:			status bits
+ * @waitcount:			how many cpus expect to find this
  */
 struct rdtgroup {
 	struct kernfs_node	*kn;
 	struct list_head	rdtgroup_list;
 	int			closid;
+	int			flags;
+	atomic_t		waitcount;
 };
+
+/* rdtgroup.flags */
+#define	RDT_DELETED		1
 
 /* List of all resource groups */
 extern struct list_head rdt_all_groups;
@@ -157,4 +164,6 @@ union cpuid_0x10_1_edx {
 };
 
 void rdt_cbm_update(void *arg);
+struct rdtgroup *rdtgroup_kn_lock_live(struct kernfs_node *kn);
+void rdtgroup_kn_unlock(struct kernfs_node *kn);
 #endif /* _ASM_X86_INTEL_RDT_H */
