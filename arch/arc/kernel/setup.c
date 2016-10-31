52d4956bac324f2fe4a0506a98641b28312bbf65
Vineet Gupta <vgupta@synopsys.com>
2016-10-31 13:26:25 -0700

    ARC: time: move time_init() out of the driver
    to allow future git mv of the driver into drivers/clocksource
    
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/setup.c
+++ arch/arc/kernel/setup.c
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/delay.h>
 #include <linux/root_dev.h>
+#include <linux/clk-provider.h>
+#include <linux/clocksource.h>
 #include <linux/console.h>
 #include <linux/module.h>
 #include <linux/cpu.h>
@@ -448,6 +450,15 @@ void __init setup_arch(char **cmdline_p)
 #endif
 
 	arc_unwind_init();
+}
+
+/*
+ * Called from start_kernel() - boot CPU only
+ */
+void __init time_init(void)
+{
+	of_clk_init(NULL);
+	clocksource_probe();
 }
 
 static int __init customize_machine(void)
