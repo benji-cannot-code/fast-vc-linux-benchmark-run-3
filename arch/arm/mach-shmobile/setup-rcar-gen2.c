17011f5d9ab9f0124808f3894f103ff5862068fc
Simon Horman <horms@verge.net.au>
2016-10-31 10:52:17 +0100

    ARM: shmobile: only call rcar_gen2_clocks_init() if present
    The RZ/G1M (r8a7743) uses the R-Car Gen2 core, but not the R-Car Gen2 clock
    driver. This is a harbinger of a transition for R-Car Gen2 SoCs. As the
    process to get all the required pieces in place is somewhat complex it
    seems useful to try to disentangle dependencies where possible.
    
    The approach here is to temporarily disable calling rcar_gen2_clocks_init()
    if no R-Car Gen2 SoC are configured and thus the symbol will not be
    present.
    
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Acked-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- arch/arm/mach-shmobile/setup-rcar-gen2.c
+++ arch/arm/mach-shmobile/setup-rcar-gen2.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
+#include <linux/clk-provider.h>
 #include <linux/clk/renesas.h>
 #include <linux/clocksource.h>
 #include <linux/device.h>
@@ -131,7 +132,15 @@ void __init rcar_gen2_timer_init(void)
 	iounmap(base);
 #endif /* CONFIG_ARM_ARCH_TIMER */
 
-	rcar_gen2_clocks_init(mode);
+	if (IS_ENABLED(CONFIG_ARCH_R8A7790) ||
+	    IS_ENABLED(CONFIG_ARCH_R8A7791) ||
+	    IS_ENABLED(CONFIG_ARCH_R8A7792) ||
+	    IS_ENABLED(CONFIG_ARCH_R8A7793) ||
+	    IS_ENABLED(CONFIG_ARCH_R8A7794))
+		rcar_gen2_clocks_init(mode);
+	else
+		of_clk_init(NULL);
+
 	clocksource_probe();
 }
 
