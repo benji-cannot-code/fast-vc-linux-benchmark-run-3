e86bedf335a4a1c7f8a179e9bbbcf76393568088
Borislav Petkov <bp@suse.de>
2016-11-02 17:51:17 +0100

    x86/intel_rdt: Add a missing #include
    ... to fix this build warning:
    
      In file included from arch/x86/kernel/cpu/intel_rdt.c:33:0:
      ./arch/x86/include/asm/intel_rdt.h:56:25: warning: ‘struct kernfs_open_file’ declared inside parameter list will not be visible outside of this definition or declaration
        int (*seq_show)(struct kernfs_open_file *of,
                               ^~~~~~~~~~~~~~~~
      ...
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: David Carrillo-Cisneros <davidcc@google.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: H. Peter Anvin <h.peter.anvin@intel.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Nilay Vaish <nilayvaish@gmail.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ravi V Shankar <ravi.v.shankar@intel.com>
    Cc: Sai Prakhya <sai.praneeth.prakhya@intel.com>
    Cc: Shaohua Li <shli@fb.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Vikas Shivappa <vikas.shivappa@linux.intel.com>
    Link: http://lkml.kernel.org/r/20161102165117.23545-1-bp@alien8.de
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/intel_rdt.h
+++ arch/x86/include/asm/intel_rdt.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_INTEL_RDT_A
 
+#include <linux/kernfs.h>
 #include <linux/jump_label.h>
 
 #include <asm/intel_rdt_common.h>
