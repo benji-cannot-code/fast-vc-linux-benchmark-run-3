4577fa4840ef0b49158383352ef01f99b7579a45
Axel Haslam <ahaslam@baylibre.com>
2016-11-03 17:03:08 +0100

    usb: ohci-da8xx: rename driver to ohci-da8xx
    The davinci ohci driver name (currently "ohci") is too generic.
    To be consistent with other usb dirvers, append the "-da8xx" postfix
    to the name.
    
    Signed-off-by: Axel Haslam <ahaslam@baylibre.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ohci-da8xx.c
+++ drivers/usb/host/ohci-da8xx.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ohci.h"
 
 #define DRIVER_DESC "DA8XX"
-#define DRV_NAME "ohci"
+#define DRV_NAME "ohci-da8xx"
 
 static struct hc_driver __read_mostly ohci_da8xx_hc_driver;
 
