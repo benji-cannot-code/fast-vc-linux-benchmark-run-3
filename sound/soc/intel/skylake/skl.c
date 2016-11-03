a22eb559f80d6db3f444f338cd6cc30ab5f4b04c
Jayachandran B <jayachandran.b@intel.com>
2016-11-03 17:07:22 +0530

    ASoC: Intel: Skylake: remove pci device enabling calls on suspend
    We were invoking pci_disable_device() while going to suspend-to-idle and
    pci_enable_device() while coming back to active state.
    
    Turns out that we do not need these calls as we only need system to be
    wake capable when in suspend-to-idle state. The wake capability is
    already done by enable_irq_wake() calls, so remove these unwanted calls
    in driver.
    
    Signed-off-by: Jayachandran B <jayachandran.b@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/skl.c
+++ sound/soc/intel/skylake/skl.c
@@ -291,7 +291,6 @@ static int skl_suspend(struct device *dev)
 
 		enable_irq_wake(bus->irq);
 		pci_save_state(pci);
-		pci_disable_device(pci);
 	} else {
 		ret = _skl_suspend(ebus);
 		if (ret < 0)
@@ -334,7 +333,6 @@ static int skl_resume(struct device *dev)
 	 */
 	if (skl->supend_active) {
 		pci_restore_state(pci);
-		ret = pci_enable_device(pci);
 		snd_hdac_ext_bus_link_power_up_all(ebus);
 		disable_irq_wake(bus->irq);
 		/*
