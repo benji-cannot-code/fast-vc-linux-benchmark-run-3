e87e90bec519587e075f59408f86e110a10e5037
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-11-04 19:43:09 +0900

    mtd: nand: nandsim: remove unneeded checks for nand_scan_ident/tail()
    The nand_scan_ident/tail() never returns a positive value when it
    fails.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Marek Vasut <marek.vasut@gmail.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/nandsim.c
+++ drivers/mtd/nand/nandsim.c
@@ -2314,8 +2314,6 @@ static int __init ns_init_module(void)
 	retval = nand_scan_ident(nsmtd, 1, NULL);
 	if (retval) {
 		NS_ERR("cannot scan NAND Simulator device\n");
-		if (retval > 0)
-			retval = -ENXIO;
 		goto error;
 	}
 
@@ -2351,8 +2349,6 @@ static int __init ns_init_module(void)
 	retval = nand_scan_tail(nsmtd);
 	if (retval) {
 		NS_ERR("can't register NAND Simulator\n");
-		if (retval > 0)
-			retval = -ENXIO;
 		goto error;
 	}
 
