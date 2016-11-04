7cabd6bcc6390f3a77839e92d74a47adc58a8491
Lars-Peter Clausen <lars@metafoo.de>
2016-11-04 18:26:55 +0100

    ASoC: stac9766: Move register defines to main source file
    The stac9766 driver has a header file that defines 3 register locations.
    Move these to the main source file since it is not really worth it having a
    separate file for them. The header file is now empty and can be removed.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/stac9766.c
+++ sound/soc/codecs/stac9766.c
@@ -27,10 +27,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/soc.h>
 #include <sound/tlv.h>
 
-#include "stac9766.h"
-
 #define STAC9766_VENDOR_ID 0x83847666
 #define STAC9766_VENDOR_ID_MASK 0xffffffff
+
+#define AC97_STAC_DA_CONTROL 0x6A
+#define AC97_STAC_ANALOG_SPECIAL 0x6E
+#define AC97_STAC_STEREO_MIC 0x78
 
 /*
  * STAC9766 register cache
