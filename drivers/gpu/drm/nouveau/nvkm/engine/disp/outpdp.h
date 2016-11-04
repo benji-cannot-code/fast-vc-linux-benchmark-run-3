ad993610d09df1368ead89475224bdab725ae7e4
Ben Skeggs <bskeggs@redhat.com>
2016-11-04 17:20:35 +1000

    drm/nouveau/disp/dp: remove workqueue for link training
    There haven't been any callers from an atomic context for a while now,
    so let's remove the extra complexity.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/disp/outpdp.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/disp/outpdp.h
@@ -30,9 +30,8 @@ struct nvkm_output_dp {
 	bool present;
 	u8 dpcd[16];
 
+	struct mutex mutex;
 	struct {
-		struct work_struct work;
-		wait_queue_head_t wait;
 		atomic_t done;
 	} lt;
 };
@@ -44,7 +43,7 @@ struct nvkm_output_dp_func {
 	int (*drv_ctl)(struct nvkm_output_dp *, int ln, int vs, int pe, int pc);
 };
 
-int nvkm_output_dp_train(struct nvkm_output *, u32 rate, bool wait);
+int nvkm_output_dp_train(struct nvkm_output *, u32 rate);
 
 int nvkm_output_dp_ctor(const struct nvkm_output_dp_func *, struct nvkm_disp *,
 			int index, struct dcb_output *, struct nvkm_i2c_aux *,
