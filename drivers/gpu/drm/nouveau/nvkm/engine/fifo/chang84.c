7a04cee3c201858f5db59c9152e9817610d8cdad
Ben Skeggs <bskeggs@redhat.com>
2016-11-05 13:05:14 +1000

    drm/nouveau/fifo/gf100-: provide notification to user if channel is killed
    There are instances (such as non-recoverable GPU page faults) where
    NVKM decides that a channel's context is no longer viable, and will
    be removed from the runlist.
    
    This commit notifies the owner of the channel when this happens, so
    it has the opportunity to take some kind of recovery action instead
    of hanging.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/fifo/chang84.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/fifo/chang84.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <nvif/cl826e.h>
 
-int
+static int
 g84_fifo_chan_ntfy(struct nvkm_fifo_chan *chan, u32 type,
 		   struct nvkm_event **pevent)
 {
