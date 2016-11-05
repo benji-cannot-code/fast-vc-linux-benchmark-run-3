ca0bf7cae11e4a26f68fa3a643f133bb3844999e
Ben Skeggs <bskeggs@redhat.com>
2016-11-05 13:12:57 +1000

    drm/nouveau/fifo: tidy up channel creation event code
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/fifo/priv.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/fifo/priv.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int nvkm_fifo_ctor(const struct nvkm_fifo_func *, struct nvkm_device *,
 		   int index, int nr, struct nvkm_fifo *);
 void nvkm_fifo_uevent(struct nvkm_fifo *);
+void nvkm_fifo_cevent(struct nvkm_fifo *);
 
 struct nvkm_fifo_chan_oclass;
 struct nvkm_fifo_func {
