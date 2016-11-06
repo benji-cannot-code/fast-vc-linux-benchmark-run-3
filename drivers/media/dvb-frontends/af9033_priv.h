7a79dd6b3ea0a6e72a887168d3c7db06181c5bd7
Antti Palosaari <crope@iki.fi>
2016-11-06 15:27:16 -0200

    [media] af9033: use 64-bit div macro where possible
    Replace Booth's binary division algo with 64-bit multiply and division.
    Fix related IF calculations.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/af9033_priv.h
+++ drivers/media/dvb-frontends/af9033_priv.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "af9033.h"
 #include <linux/math64.h>
 #include <linux/regmap.h>
+#include <linux/kernel.h>
 
 struct reg_val {
 	u32 reg;
