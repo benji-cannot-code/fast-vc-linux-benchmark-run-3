f45c50c8fd92f670767d2444547c85f6a0c49b21
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-11-06 12:45:27 +0900

    s390: delete unneeded #include <linux/kconfig.h> from facilities_src.h
    The header facilities_src.h is only included from gen_facilities.c
    and the tool is compiled with the following extra options:
    
        HOSTCFLAGS_gen_facilities.o += -Wall $(LINUXINCLUDE)
    
    Please note $(LINUXINCLUDE) is expanded into build options including:
    
        -include $(srctree)/include/linux/kconfig.h
    
    So, the Makefile always forces the tool to include kconfig.h, i.e.,
    the #include <linux/kconfig.h> directive in the header is redundant.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Acked-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/facilities_src.h
+++ arch/s390/include/asm/facilities_src.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #error "This file can only be included by gen_facilities.c"
 #endif
 
-#include <linux/kconfig.h>
-
 struct facility_def {
 	char *name;
 	int *bits;
