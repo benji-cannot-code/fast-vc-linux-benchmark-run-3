8859855885e01da0a890160145c9e5dd6c32d2c1
Pratyush Anand <panand@redhat.com>
2016-11-07 23:07:22 +0530

    arm64: fix error: conflicting types for 'kprobe_fault_handler'
    When CONFIG_KPROBE is disabled but CONFIG_UPROBE_EVENT is enabled, we get
    following compilation error:
    
    In file included from
    .../arch/arm64/kernel/probes/decode-insn.c:20:0:
    .../arch/arm64/include/asm/kprobes.h:52:5: error:
    conflicting types for 'kprobe_fault_handler'
     int kprobe_fault_handler(struct pt_regs *regs, unsigned int fsr);
         ^~~~~~~~~~~~~~~~~~~~
    In file included from
    .../arch/arm64/kernel/probes/decode-insn.c:17:0:
    .../include/linux/kprobes.h:398:90: note:
    previous definition of 'kprobe_fault_handler' was here
     static inline int kprobe_fault_handler(struct pt_regs *regs, int trapnr)
                                                                                              ^
    .../scripts/Makefile.build:290: recipe for target
    'arch/arm64/kernel/probes/decode-insn.o' failed
    
    <asm/kprobes.h> is already included from <linux/kprobes.h> under #ifdef
    CONFIG_KPROBE. So, this patch fixes the error by removing it from
    decode-insn.c.
    
    Reported-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Pratyush Anand <panand@redhat.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/probes/decode-insn.c
+++ arch/arm64/kernel/probes/decode-insn.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kprobes.h>
 #include <linux/module.h>
 #include <linux/kallsyms.h>
-#include <asm/kprobes.h>
 #include <asm/insn.h>
 #include <asm/sections.h>
 
