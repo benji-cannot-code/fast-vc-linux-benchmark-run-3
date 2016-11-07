6e5ac0fa8061dc6ce7359ff6d3754ba10875340d
Tom St Denis <tom.stdenis@amd.com>
2016-11-07 14:06:01 -0500

    drm/amd/amdgpu: add SI defines/registers
    Add missing gca MMIO registers and defines necessary for the
    next patch which re-works a lot of gfx v6 to use the new SI
    headers.
    
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/include/asic_reg/oss/oss_1_0_d.h
+++ drivers/gpu/drm/amd/include/asic_reg/oss/oss_1_0_d.h
@@ -270,4 +270,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define mmVCE_CONFIG 0x0F94
 #define mmXDMA_MSTR_MEM_OVERFLOW_CNTL 0x03F8
 
+/* from the old sid.h */
+#define mmDMA_TILING_CONFIG 0x342E
+
 #endif
