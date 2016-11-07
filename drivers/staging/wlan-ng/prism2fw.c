f016080e9a858c53972b8226b76c02c535800301
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2016-11-07 18:55:27 +0100

    staging: wlan-ng: remove unnecessary blank lines in prism2fw.c
    This patch removes unnecessary blank lines in prism2fw.c file to
    comply with the standard kernel coding style.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/prism2fw.c
+++ drivers/staging/wlan-ng/prism2fw.c
@@ -444,7 +444,6 @@ static int crcimage(struct imgchunk *fchunk, unsigned int nfchunks,
 		dest = fchunk[c].data + chunkoff;
 		*dest = 0xde;
 		*(dest + 1) = 0xc0;
-
 	}
 	return result;
 }
@@ -470,7 +469,6 @@ static void free_chunks(struct imgchunk *fchunk, unsigned int *nfchunks)
 
 	*nfchunks = 0;
 	memset(fchunk, 0, sizeof(*fchunk));
-
 }
 
 /*----------------------------------------------------------------
@@ -646,7 +644,6 @@ static int mkpdrlist(struct pda *pda)
 
 		(pda->nrec)++;
 		curroff += le16_to_cpu(pda16[curroff]) + 1;
-
 	}
 	if (curroff >= (HFA384x_PDA_LEN_MAX / 2 - 1)) {
 		pr_err("no end record found or invalid lengths in PDR data, exiting. %x %d\n",
@@ -762,7 +759,6 @@ static int plugimage(struct imgchunk *fchunk, unsigned int nfchunks,
 		}
 	}
 	return result;
-
 }
 
 /*----------------------------------------------------------------
@@ -891,7 +887,6 @@ static int read_fwfile(const struct ihex_binrec *record)
 	pr_debug("Reading fw file ...\n");
 
 	while (record) {
-
 		rcnt++;
 
 		len = be16_to_cpu(record->len);
@@ -1119,7 +1114,6 @@ static int writeimage(struct wlandevice *wlandev, struct imgchunk *fchunk,
 				result = 1;
 				goto free_result;
 			}
-
 		}
 	}
 
