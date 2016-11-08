e109bd4d49fd08723085d668b15965d3a709e3d9
Hugh Dickins <hughd@google.com>
2016-11-07 22:28:21 -0800

    powerpc: Fix exception vector build with 2.23 era binutils
    The changes to use gas sections for constructing the exception vectors
    causes a build break when using binutils 2.23:
    
      arch/powerpc/kernel/exceptions-64s.S:770: Error: operand out of range
      (0xffffffffffff8100 is not between 0x0000000000000000 and 0x000000000000ffff)
    
    And so on.
    
    Reported by Hugh with binutils-2.23.2-8.1.4.ppc64 from openSUSE 13.1 and
    also Naveen & Denis using 2.23.52.0.1-26.el7 from RHEL 7. Strangely
    binutils 2.22 (what I test with) is not affected.
    
    This is caused by the use of @l in LOAD_HANDLER(). The @l was only
    recently added in commit a24553dd02dc ("powerpc/pseries: Remove
    unnecessary syscall trampoline").
    
    Luckily the gas section changes split out the LOAD_SYSCALL_HANDLER()
    macro, which means we actually *don't* need to use @l in LOAD_HANDLER()
    any more, only in LOAD_SYSCALL_HANDLER().
    
    So drop the @l from LOAD_HANDLER().
    
    Fixes: 57f266497d81 ("powerpc: Use gas sections for arranging exception vectors")
    Signed-off-by: Hugh Dickins <hughd@google.com>
    [mpe: Add gory details to change log]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/exception-64s.h
+++ arch/powerpc/include/asm/exception-64s.h
@@ -92,7 +92,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define LOAD_HANDLER(reg, label)					\
 	ld	reg,PACAKBASE(r13);	/* get high part of &label */	\
-	ori	reg,reg,(FIXED_SYMBOL_ABS_ADDR(label))@l;
+	ori	reg,reg,FIXED_SYMBOL_ABS_ADDR(label);
 
 #define __LOAD_HANDLER(reg, label)					\
 	ld	reg,PACAKBASE(r13);					\
