58b1098879f1630a42119eb11326be247840e0a5
Tom St Denis <tom.stdenis@amd.com>
2016-11-08 11:55:42 -0500

    drm/amd/amdgpu:  Port GMC v6 driver to new SI headers (v2)
    Port the GMC v6 driver over to the new SI headers.
    
    Tested with a Tahiti SI ASIC.
    
    (v2) Fixed a couple of typos (in commented code) and moved
         defines to si_enums.h
    
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/si_enums.h
+++ drivers/gpu/drm/amd/amdgpu/si_enums.h
@@ -24,6 +24,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef SI_ENUMS_H
 #define SI_ENUMS_H
 
+#define AMDGPU_NUM_OF_VMIDS                     8
+#define SI_CRTC0_REGISTER_OFFSET                0
+#define SI_CRTC1_REGISTER_OFFSET                0x300
+#define SI_CRTC2_REGISTER_OFFSET                0x2600
+#define SI_CRTC3_REGISTER_OFFSET                0x2900
+#define SI_CRTC4_REGISTER_OFFSET                0x2c00
+#define SI_CRTC5_REGISTER_OFFSET                0x2f00
+
 #define DMA0_REGISTER_OFFSET 0x000
 #define DMA1_REGISTER_OFFSET 0x200
 #define ES_AND_GS_AUTO       3
