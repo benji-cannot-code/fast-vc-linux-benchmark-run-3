5a57461cc7720ee90d960a119f39f92f7df70ca2
Namhyung Kim <namhyung@kernel.org>
2016-11-08 22:08:31 +0900

    perf hists browser: Show folded sign properly on --hierarchy
    When horizontal scrolling is used in hierarchy mode, the folded signed
    disappears at the right most column.
    
    Committer note:
    
    To test it, run 'perf top --hierarchy, see the '+' symbol at the first
    column, then press the right arrow key, the '+' symbol will disappear,
    this patch fixes that.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Tested-by: Markus Trippelsdorf <markus@trippelsdorf.de>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/20161108130833.9263-3-namhyung@kernel.org
    [ Move 'width -= 2' invariant to right after the if/else ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/ui/browsers/hists.c
+++ tools/perf/ui/browsers/hists.c
@@ -1382,7 +1382,13 @@ static int hist_browser__show_hierarchy_entry(struct hist_browser *browser,
 		}
 
 		perf_hpp_list__for_each_format(entry->hpp_list, fmt) {
-			ui_browser__write_nstring(&browser->b, "", 2);
+			if (first) {
+				ui_browser__printf(&browser->b, "%c ", folded_sign);
+				first = false;
+			} else {
+				ui_browser__write_nstring(&browser->b, "", 2);
+			}
+
 			width -= 2;
 
 			/*
