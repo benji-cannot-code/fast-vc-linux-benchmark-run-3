79855c1f1d92576a96319fc0bb01397a8a4bfd05
David Lebrun <david.lebrun@uclouvain.be>
2016-11-08 14:57:40 +0100

    ipv6: sr: add code base for control plane support of SR-IPv6
    This patch adds the necessary hooks and structures to provide support
    for SR-IPv6 control plane, essentially the Generic Netlink commands
    that will be used for userspace control over the Segment Routing
    kernel structures.
    
    The genetlink commands provide control over two different structures:
    tunnel source and HMAC data. The tunnel source is the source address
    that will be used by default when encapsulating packets into an
    outer IPv6 header + SRH. If the tunnel source is set to :: then an
    address of the outgoing interface will be selected as the source.
    
    The HMAC commands currently just return ENOTSUPP and will be implemented
    in a future patch.
    
    Signed-off-by: David Lebrun <david.lebrun@uclouvain.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/seg6.h
+++ include/net/seg6.h
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _NET_SEG6_H
 #define _NET_SEG6_H
 
+#include <linux/net.h>
+#include <linux/ipv6.h>
+
 static inline void update_csum_diff4(struct sk_buff *skb, __be32 from,
 				     __be32 to)
 {
@@ -33,5 +36,18 @@ static inline void update_csum_diff16(struct sk_buff *skb, __be32 *from,
 
 	skb->csum = ~csum_partial((char *)diff, sizeof(diff), ~skb->csum);
 }
+
+struct seg6_pernet_data {
+	struct mutex lock;
+	struct in6_addr __rcu *tun_src;
+};
+
+static inline struct seg6_pernet_data *seg6_pernet(struct net *net)
+{
+	return net->ipv6.seg6_data;
+}
+
+extern int seg6_init(void);
+extern void seg6_exit(void);
 
 #endif
