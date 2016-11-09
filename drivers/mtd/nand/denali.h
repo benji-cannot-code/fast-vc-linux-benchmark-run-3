89f0ef5bc2eb5fd51da0cf6964e1e22ba68f7fbf
Masahiro Yamada <yamada.masahiro@socionext.com>
2016-11-09 13:35:21 +0900

    mtd: nand: denali: remove unused struct member denali_nand_info::idx
    The struct member "idx" was used as an index for debug_array long
    ago, but the DEBUG_DENALI feature was removed by commit 7cfffac06ca0
    ("nand/denali: use dev_xx debug function to replace nand_dbg_print
    and some printk").  Since then, this has been only initialized, but
    never referenced.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Marek Vasut <marek.vasut@gmail.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/denali.h
+++ drivers/mtd/nand/denali.h
@@ -468,7 +468,6 @@ struct denali_nand_info {
 	spinlock_t irq_lock;
 	uint32_t irq_status;
 	int irq_debug_array[32];
-	int idx;
 	int irq;
 
 	uint32_t devnum;	/* represent how many nands connected */
