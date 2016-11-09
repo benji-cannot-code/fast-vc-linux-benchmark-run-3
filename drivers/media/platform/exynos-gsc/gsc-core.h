6e8e16a5a24b4888df78acc7791d9f3ca3708098
Ulf Hansson <ulf.hansson@linaro.org>
2016-11-09 12:23:57 -0200

    [media] exynos-gsc: Simplify system PM
    It's not needed to keep a local flag about the current system PM state.
    Let's just remove that code and the corresponding debug print.
    
    [mszyprow: rebased onto v4.9-rc4]
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/exynos-gsc/gsc-core.h
+++ drivers/media/platform/exynos-gsc/gsc-core.h
@@ -49,9 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	GSC_CTX_ABORT			(1 << 7)
 
 enum gsc_dev_flags {
-	/* for global */
-	ST_SUSPEND,
-
 	/* for m2m node */
 	ST_M2M_OPEN,
 	ST_M2M_RUN,
