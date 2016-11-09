7d896392ef26a7ab61806cf4b637772ca428fd3f
Kirtika Ruchandani <kirtika.ruchandani@gmail.com>
2016-11-08 21:50:33 -0800

    iwlwifi: mvm: rs: Remove unused 'mcs' variable
    Commit 5fc0f76c4 introduced Rx stats from debugfs, the function
    iwl_mvm_reset_frame_stats from that commit defines and sets mcs but
    does not use it. Compiling iwlwifi with W=1 gives this warning -
    
    iwlwifi/mvm/rs.c: In function ‘iwl_mvm_update_frame_stats’:
    iwlwifi/mvm/rs.c:3074:14: warning: variable ‘mcs’ set but not used [-Wunused-but-set-variable]
    
    Fixes: 5fc0f76c43bd (iwlwifi: mvm: add Rx frames statistics via debugfs)
    Signed-off-by: Kirtika Ruchandani <kirtika@google.com>
    Cc: Eyal Shapira <eyal@wizery.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/rs.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/rs.c
@@ -3066,7 +3066,7 @@ static void iwl_mvm_reset_frame_stats(struct iwl_mvm *mvm)
 
 void iwl_mvm_update_frame_stats(struct iwl_mvm *mvm, u32 rate, bool agg)
 {
-	u8 nss = 0, mcs = 0;
+	u8 nss = 0;
 
 	spin_lock(&mvm->drv_stats_lock);
 
@@ -3094,11 +3094,9 @@ void iwl_mvm_update_frame_stats(struct iwl_mvm *mvm, u32 rate, bool agg)
 
 	if (rate & RATE_MCS_HT_MSK) {
 		mvm->drv_rx_stats.ht_frames++;
-		mcs = rate & RATE_HT_MCS_RATE_CODE_MSK;
 		nss = ((rate & RATE_HT_MCS_NSS_MSK) >> RATE_HT_MCS_NSS_POS) + 1;
 	} else if (rate & RATE_MCS_VHT_MSK) {
 		mvm->drv_rx_stats.vht_frames++;
-		mcs = rate & RATE_VHT_MCS_RATE_CODE_MSK;
 		nss = ((rate & RATE_VHT_MCS_NSS_MSK) >>
 		       RATE_VHT_MCS_NSS_POS) + 1;
 	} else {
