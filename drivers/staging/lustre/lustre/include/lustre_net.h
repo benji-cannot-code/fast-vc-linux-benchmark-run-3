8d3f8929f5ca2ff9bbda314f8a6ff85d3490d6a2
Andreas Dilger <andreas.dilger@intel.com>
2016-11-10 12:30:49 -0500

    staging: lustre: obdclass: remove structure holes to reduce memory
    Fix the alignment of fields in commonly-used structures to reduce
    memory usage on the client and server.  Structures fixed:
    
    ptlrpc_reply_state: reduced by 8 bytes
    obd_device:         reduced by 16 bytes
    niobuf_local:       reduced by 8 bytes
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-3281
    Reviewed-on: http://review.whamcloud.com/16692
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre_net.h
+++ drivers/staging/lustre/lustre/include/lustre_net.h
@@ -439,6 +439,10 @@ struct ptlrpc_reply_state {
 	unsigned long	  rs_committed:1;/* the transaction was committed
 					  * and the rs was dispatched
 					  */
+	atomic_t		rs_refcount;	/* number of users */
+	/** Number of locks awaiting client ACK */
+	int			rs_nlocks;
+
 	/** Size of the state */
 	int		    rs_size;
 	/** opcode */
@@ -451,7 +455,6 @@ struct ptlrpc_reply_state {
 	struct ptlrpc_service_part *rs_svcpt;
 	/** Lnet metadata handle for the reply */
 	lnet_handle_md_t       rs_md_h;
-	atomic_t	   rs_refcount;
 
 	/** Context for the service thread */
 	struct ptlrpc_svc_ctx *rs_svc_ctx;
@@ -468,8 +471,6 @@ struct ptlrpc_reply_state {
 	 */
 	struct lustre_msg     *rs_msg;	  /* reply message */
 
-	/** Number of locks awaiting client ACK */
-	int		    rs_nlocks;
 	/** Handles of locks awaiting client reply ACK */
 	struct lustre_handle   rs_locks[RS_MAX_LOCKS];
 	/** Lock modes of locks in \a rs_locks */
