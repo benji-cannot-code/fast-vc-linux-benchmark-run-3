b07f75a652d9ff8da7c6043b071ea80116d2c3c5
Lendacky, Thomas <Thomas.Lendacky@amd.com>
2016-11-10 17:11:41 -0600

    amd-xgbe: Add support for a KR redriver
    This patch provides support for the presence of a KR redriver chip in
    between the device PCS and an external PHY.  When a redriver chip is
    present the device must perform clause 73 auto-negotiation in order to
    set the redriver chip for the downstream connection.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/amd/xgbe/xgbe.h
+++ drivers/net/ethernet/amd/xgbe/xgbe.h
@@ -509,6 +509,7 @@ enum xgbe_xpcs_access {
 
 enum xgbe_an_mode {
 	XGBE_AN_MODE_CL73 = 0,
+	XGBE_AN_MODE_CL73_REDRV,
 	XGBE_AN_MODE_CL37,
 	XGBE_AN_MODE_CL37_SGMII,
 	XGBE_AN_MODE_NONE,
@@ -807,6 +808,9 @@ struct xgbe_phy_impl_if {
 
 	/* Configure auto-negotiation settings */
 	int (*an_config)(struct xgbe_prv_data *);
+
+	/* Set/override auto-negotiation advertisement settings */
+	unsigned int (*an_advertising)(struct xgbe_prv_data *);
 
 	/* Process results of auto-negotiation */
 	enum xgbe_mode (*an_outcome)(struct xgbe_prv_data *);
@@ -1124,6 +1128,8 @@ struct xgbe_prv_data {
 	int mdio_mmd;
 	unsigned long link_check;
 	struct completion mdio_complete;
+
+	unsigned int kr_redrv;
 
 	char an_name[IFNAMSIZ + 32];
 	struct workqueue_struct *an_workqueue;
