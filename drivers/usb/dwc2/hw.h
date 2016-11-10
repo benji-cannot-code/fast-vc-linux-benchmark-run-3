b8944a550103687eda98e874dd3d2ef76d0c00e1
Vahram Aharonyan <vahrama@synopsys.com>
2016-11-09 19:27:46 -0800

    usb: dwc2: Make the DMA descriptor structure packed
    Make the DMA descriptor structure packed to guarantee alignment and size
    in memory.
    
    Signed-off-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc2/hw.h
+++ drivers/usb/dwc2/hw.h
@@ -803,7 +803,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct dwc2_dma_desc {
 	u32 status;
 	u32 buf;
-};
+} __packed;
 
 #define HOST_DMA_A			(1 << 31)
 #define HOST_DMA_STS_MASK		(0x3 << 28)
