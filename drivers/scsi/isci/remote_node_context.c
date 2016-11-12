42d16876c9b9473574f5559483c5c11981e7ff7c
Colin Ian King <colin.king@canonical.com>
2016-11-12 16:49:50 +0000

    scsi: isci: fix spelling mistakes in dev_warn messages
    Trivial fix to spelling mistake "suspeneded" to "suspended" in dev_warn
    messages.
    
    [mkp: corrected description. Patch is against the isci driver, not iscsi]
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Reviewed-by: Bart Van Assche <bart.vanassche@sandisk.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/isci/remote_node_context.c
+++ drivers/scsi/isci/remote_node_context.c
@@ -455,7 +455,7 @@ enum sci_status sci_remote_node_context_event_handler(struct sci_remote_node_con
 				 * the device since it's being invalidated anyway */
 				dev_warn(scirdev_to_dev(rnc_to_dev(sci_rnc)),
 					"%s: SCIC Remote Node Context 0x%p was "
-					"suspeneded by hardware while being "
+					"suspended by hardware while being "
 					"invalidated.\n", __func__, sci_rnc);
 				break;
 			default:
@@ -474,7 +474,7 @@ enum sci_status sci_remote_node_context_event_handler(struct sci_remote_node_con
 				 * the device since it's being resumed anyway */
 				dev_warn(scirdev_to_dev(rnc_to_dev(sci_rnc)),
 					"%s: SCIC Remote Node Context 0x%p was "
-					"suspeneded by hardware while being resumed.\n",
+					"suspended by hardware while being resumed.\n",
 					__func__, sci_rnc);
 				break;
 			default:
