b163337532199823220389fb0209b13040181584
Wolfram Sang <wsa+renesas@sang-engineering.com>
2016-11-13 15:29:12 +0100

    mmc: tmio: remove SDIO from TODO list
    We surely have SDIO support by now :)
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/tmio_mmc_pio.c
+++ drivers/mmc/host/tmio_mmc_pio.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * TODO:
  *   Investigate using a workqueue for PIO transfers
  *   Eliminate FIXMEs
- *   SDIO support
  *   Better Power management
  *   Handle MMC errors better
  *   double buffer support
