d936fe3d330c59450f9dd28aa139c6256fdf9c96
Nayna Jain <nayna@linux.vnet.ibm.com>
2016-11-14 05:00:53 -0500

    tpm: fix the missing .owner in tpm_bios_measurements_ops
    This patch fixes the missing .owner field in
    tpm_bios_measurements_ops definition.
    
    Suggested-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    Signed-off-by: Nayna Jain <nayna@linux.vnet.ibm.com>
    Reviewed-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Tested-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

--- drivers/char/tpm/tpm_eventlog.c
+++ drivers/char/tpm/tpm_eventlog.c
@@ -350,6 +350,7 @@ static int tpm_bios_measurements_open(struct inode *inode,
 }
 
 static const struct file_operations tpm_bios_measurements_ops = {
+	.owner = THIS_MODULE,
 	.open = tpm_bios_measurements_open,
 	.read = seq_read,
 	.llseek = seq_lseek,
