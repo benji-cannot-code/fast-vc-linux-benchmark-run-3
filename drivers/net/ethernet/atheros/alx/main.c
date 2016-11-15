bbc535a5de13ec74516a7574ef1f29cc15d75e2b
Tobias Regnery <tobias.regnery@gmail.com>
2016-11-15 12:43:15 +0100

    alx: enable msi-x interrupts by default
    Remove the module parameter to enable msi-x support and enable msi-x
    interrupts unconditionally by default. This is a preparatory step to enable
    multi queue support by default, because this is only working with msi-x
    interrupts.
    
    Signed-off-by: Tobias Regnery <tobias.regnery@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/atheros/alx/main.c
+++ drivers/net/ethernet/atheros/alx/main.c
@@ -52,10 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 const char alx_drv_name[] = "alx";
 
-static bool msix = false;
-module_param(msix, bool, 0);
-MODULE_PARM_DESC(msix, "Enable msi-x interrupt support");
-
 static void alx_free_txbuf(struct alx_tx_queue *txq, int entry)
 {
 	struct alx_buffer *txb = &txq->bufs[entry];
@@ -1227,7 +1223,7 @@ static int __alx_open(struct alx_priv *alx, bool resume)
 {
 	int err;
 
-	alx_init_intr(alx, msix);
+	alx_init_intr(alx, true);
 
 	if (!resume)
 		netif_carrier_off(alx->dev);
