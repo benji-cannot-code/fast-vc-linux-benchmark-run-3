a6367871f754b7477dd3083526dbd8528f38d0b3
Bart Van Assche <bart.vanassche@sandisk.com>
2016-11-15 11:23:01 -0800

    sbp-target: Add an #include directive
    usleep_range() is called from sbp_target.c. Hence include header
    file <linux/delay.h>.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
    Cc: Chris Boot <bootc@bootc.net>

--- drivers/target/sbp/sbp_target.c
+++ drivers/target/sbp/sbp_target.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/configfs.h>
 #include <linux/ctype.h>
+#include <linux/delay.h>
 #include <linux/firewire.h>
 #include <linux/firewire-constants.h>
 #include <scsi/scsi_proto.h>
