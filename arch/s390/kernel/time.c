a478bf825f5d0d7645b5aa8a11fcaf1386547017
Martin Schwidefsky <schwidefsky@de.ibm.com>
2016-11-16 09:48:41 +0100

    s390/debug: make debug event time stamps relative to the boot TOD clock
    The debug features currently uses absolute TOD time stamps for the
    debug events. Given that the TOD clock can jump forward and backward
    due to STP sync checks the order of debug events can get obfuscated.
    
    Replace the absolute TOD time stamps with a delta to the IPL time
    stamp. On a STP sync check the TOD clock correction is added to
    the IPL time stamp as well to make the deltas unaffected by STP
    sync check.
    
    The readout of the debug feature entries will convert the deltas
    back to absolute time stamps based on the Unix epoch.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/time.c
+++ arch/s390/kernel/time.c
@@ -111,7 +111,7 @@ unsigned long long monotonic_clock(void)
 }
 EXPORT_SYMBOL(monotonic_clock);
 
-void tod_to_timeval(__u64 todval, struct timespec64 *xt)
+static void tod_to_timeval(__u64 todval, struct timespec64 *xt)
 {
 	unsigned long long sec;
 
@@ -121,7 +121,6 @@ void tod_to_timeval(__u64 todval, struct timespec64 *xt)
 	todval -= (sec * 1000000) << 12;
 	xt->tv_nsec = ((todval * 1000) >> 12);
 }
-EXPORT_SYMBOL(tod_to_timeval);
 
 void clock_comparator_work(void)
 {
