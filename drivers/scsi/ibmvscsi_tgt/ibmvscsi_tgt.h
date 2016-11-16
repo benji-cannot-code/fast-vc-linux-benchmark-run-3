c2c8a25c27f6c20f28f24af4b98c6567e78d4b76
Bryant G. Ly <bryantly@linux.vnet.ibm.com>
2016-11-16 09:06:23 -0800

    ibmvscsi_tgt: Add two #include directives
    Signed-off-by: Bryant G. Ly <bryantly@linux.vnet.ibm.com>
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>

--- drivers/scsi/ibmvscsi_tgt/ibmvscsi_tgt.h
+++ drivers/scsi/ibmvscsi_tgt/ibmvscsi_tgt.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __H_IBMVSCSI_TGT
 #define __H_IBMVSCSI_TGT
 
+#include <linux/interrupt.h>
 #include "libsrp.h"
 
 #define SYS_ID_NAME_LEN		64
