974c796f2ff08ac54b0f01c541db4f126dcbbf2c
Claudiu Manoil <claudiu.manoil@nxp.com>
2016-11-16 16:40:22 +0200

    soc/qman: test: Don't use dummy platform device for dma mapping
    Replace dummy platform device hack with a reference to a portal's
    platform device, in order to dma map the test frame for this
    small unit test.  The 2 qman symbols need to be exported because
    this self test is a kernel module.
    
    Signed-off-by: Claudiu Manoil <claudiu.manoil@nxp.com>
    Signed-off-by: Scott Wood <oss@buserror.net>

--- drivers/soc/fsl/qbman/qman_portal.c
+++ drivers/soc/fsl/qbman/qman_portal.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "qman_priv.h"
 
 struct qman_portal *qman_dma_portal;
+EXPORT_SYMBOL(qman_dma_portal);
 
 /* Enable portal interupts (as opposed to polling mode) */
 #define CONFIG_FSL_DPA_PIRQ_SLOW  1
