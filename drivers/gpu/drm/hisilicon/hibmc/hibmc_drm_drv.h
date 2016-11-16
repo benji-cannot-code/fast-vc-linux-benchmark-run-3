7b4d6f6dc2d9b603985720ea2e92776cb2eafce1
Rongrong Zou <zourongrong@gmail.com>
2016-11-16 20:54:52 +0800

    drm/hisilicon/hibmc: Add support for display engine
    Add display engine function, crtc/plane is initialized here.
    
    Signed-off-by: Rongrong Zou <zourongrong@gmail.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Xinliang Liu <xinliang.liu@linaro.org>
    Acked-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HIBMC_DRM_DRV_H
 
 #include <drm/drmP.h>
+#include <drm/drm_atomic.h>
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_gem.h>
 #include <drm/ttm/ttm_bo_driver.h>
@@ -45,6 +46,8 @@ struct hibmc_drm_private {
 
 	/* drm */
 	struct drm_device  *dev;
+	bool mode_config_initialized;
+	struct drm_atomic_state *suspend_state;
 
 	/* ttm */
 	struct drm_global_reference mem_global_ref;
@@ -83,6 +86,7 @@ void hibmc_set_power_mode(struct hibmc_drm_private *priv,
 void hibmc_set_current_gate(struct hibmc_drm_private *priv,
 			    unsigned int gate);
 
+int hibmc_de_init(struct hibmc_drm_private *priv);
 int hibmc_fbdev_init(struct hibmc_drm_private *priv);
 void hibmc_fbdev_fini(struct hibmc_drm_private *priv);
 
@@ -103,5 +107,7 @@ int hibmc_dumb_create(struct drm_file *file, struct drm_device *dev,
 int hibmc_dumb_mmap_offset(struct drm_file *file, struct drm_device *dev,
 			   u32 handle, u64 *offset);
 int hibmc_mmap(struct file *filp, struct vm_area_struct *vma);
+
+extern const struct drm_mode_config_funcs hibmc_mode_funcs;
 
 #endif
