8697b9b9a03f280b255808e9b5f91558e303b243
James Simmons <jsimmons@infradead.org>
2016-11-17 14:35:53 -0500

    staging: lustre: libcfs: Make char * array envp static constant
    The envp char array can be made static constant.
    
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/libcfs/linux/linux-debug.c
+++ drivers/staging/lustre/lnet/libcfs/linux/linux-debug.c
@@ -70,10 +70,11 @@ void libcfs_run_debug_log_upcall(char *file)
 {
 	char *argv[3];
 	int rc;
-	char *envp[] = {
+	static const char * const envp[] = {
 		"HOME=/",
 		"PATH=/sbin:/bin:/usr/sbin:/usr/bin",
-		NULL};
+		NULL
+	};
 
 	argv[0] = lnet_debug_log_upcall;
 
@@ -82,7 +83,7 @@ void libcfs_run_debug_log_upcall(char *file)
 
 	argv[2] = NULL;
 
-	rc = call_usermodehelper(argv[0], argv, envp, 1);
+	rc = call_usermodehelper(argv[0], argv, (char **)envp, 1);
 	if (rc < 0 && rc != -ENOENT) {
 		CERROR("Error %d invoking LNET debug log upcall %s %s; check /sys/kernel/debug/lnet/debug_log_upcall\n",
 		       rc, argv[0], argv[1]);
@@ -96,10 +97,11 @@ void libcfs_run_upcall(char **argv)
 {
 	int rc;
 	int argc;
-	char *envp[] = {
+	static const char * const envp[] = {
 		"HOME=/",
 		"PATH=/sbin:/bin:/usr/sbin:/usr/bin",
-		NULL};
+		NULL
+	};
 
 	argv[0] = lnet_upcall;
 	argc = 1;
@@ -108,7 +110,7 @@ void libcfs_run_upcall(char **argv)
 
 	LASSERT(argc >= 2);
 
-	rc = call_usermodehelper(argv[0], argv, envp, 1);
+	rc = call_usermodehelper(argv[0], argv, (char **)envp, 1);
 	if (rc < 0 && rc != -ENOENT) {
 		CERROR("Error %d invoking LNET upcall %s %s%s%s%s%s%s%s%s; check /sys/kernel/debug/lnet/upcall\n",
 		       rc, argv[0], argv[1],
