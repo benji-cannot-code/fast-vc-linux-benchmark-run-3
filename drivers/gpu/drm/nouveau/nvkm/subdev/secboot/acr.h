e4b12858767bf151cb3239543966628933b58591
Alexandre Courbot <acourbot@nvidia.com>
2016-11-18 17:47:08 +0900

    drm/nouveau/secboot: support optional falcons
    PMU support has been enabled for r352 ACR, but it must remain optional
    if we want to preserve existing user-space that do not include it. Allow
    ACR to be instanciated with a list of optional LS falcons, that will not
    produce a fatal error if their firmware is not loaded. Also change the
    secure boot bootstrap logic to be able to fall back to legacy behavior
    if it turns out the boot falcon's LS firmware cannot be loaded.
    
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/secboot/acr.h
+++ drivers/gpu/drm/nouveau/nvkm/subdev/secboot/acr.h
@@ -49,6 +49,7 @@ struct nvkm_acr_func {
  *
  * @boot_falcon: ID of the falcon that will perform secure boot
  * @managed_falcons: bitfield of falcons managed by this ACR
+ * @optional_falcons: bitfield of falcons we can live without
  * @start_address: virtual start address of the HS bootloader
  */
 struct nvkm_acr {
@@ -57,6 +58,7 @@ struct nvkm_acr {
 
 	enum nvkm_secboot_falcon boot_falcon;
 	unsigned long managed_falcons;
+	unsigned long optional_falcons;
 	u32 start_address;
 };
 
