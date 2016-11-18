db21116d8b614e30417db33430cc1aedc11cd1a5
Geliang Tang <geliangtang@gmail.com>
2016-11-18 22:21:16 +0800

    skd_main: drop duplicate header scatterlist.h
    Drop duplicate header scatterlist.h from skd_main.c.
    
    Signed-off-by: Geliang Tang <geliangtang@gmail.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/skd_main.c
+++ drivers/block/skd_main.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/scatterlist.h>
 #include <linux/version.h>
 #include <linux/err.h>
-#include <linux/scatterlist.h>
 #include <linux/aer.h>
 #include <linux/ctype.h>
 #include <linux/wait.h>
