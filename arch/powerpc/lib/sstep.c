013ebcf8fe4a7a74e9abd54354ece95283cdd855
Michael Ellerman <mpe@ellerman.id.au>
2016-11-18 11:51:14 +1100

    powerpc/lib: Fix randconfig build failure in sstep.c
    Under some configs we need to explicitly include cpu_has_feature.h,
    otherwise we fail with:
    
      arch/powerpc/lib/sstep.c:1992:7: error: implicit declaration of function 'cpu_has_feature'
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/lib/sstep.c
+++ arch/powerpc/lib/sstep.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sstep.h>
 #include <asm/processor.h>
 #include <asm/uaccess.h>
+#include <asm/cpu_has_feature.h>
 #include <asm/cputable.h>
 
 extern char system_call_common[];
