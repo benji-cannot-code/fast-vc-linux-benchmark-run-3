db719055b571013d4161fb88f4df95806d75d9dc
Bart Van Assche <bart.vanassche@sandisk.com>
2016-11-18 15:31:45 -0800

    target/user: Add an #include directive
    Since this driver uses kmap_atomic(), include the highmem header file.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
    Cc: Nicholas Bellinger <nab@linux-iscsi.org>
    Cc: Andy Grover <agrover@redhat.com>

--- drivers/target/target_core_user.c
+++ drivers/target/target_core_user.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uio_driver.h>
 #include <linux/stringify.h>
 #include <linux/bitops.h>
+#include <linux/highmem.h>
 #include <net/genetlink.h>
 #include <scsi/scsi_common.h>
 #include <scsi/scsi_proto.h>
