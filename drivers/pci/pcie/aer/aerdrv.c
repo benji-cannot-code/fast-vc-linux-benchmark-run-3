bf64bdbfd1b401321b3e3d0814939dc53a395892
Bjorn Helgaas <bhelgaas@google.com>
2016-11-21 15:19:29 -0600

    PCI/AER: Remove unused version macros
    Remove the unused DRIVER_VERSION, DRIVER_AUTHOR, and DRIVER_DESC macros.
    The author information is already included in a comment above.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/pcie/aer/aerdrv.c
+++ drivers/pci/pcie/aer/aerdrv.c
@@ -31,13 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "aerdrv.h"
 #include "../../pci.h"
 
-/*
- * Version Information
- */
-#define DRIVER_VERSION "v1.0"
-#define DRIVER_AUTHOR "tom.l.nguyen@intel.com"
-#define DRIVER_DESC "Root Port Advanced Error Reporting Driver"
-
 static int aer_probe(struct pcie_device *dev);
 static void aer_remove(struct pcie_device *dev);
 static pci_ers_result_t aer_error_detected(struct pci_dev *dev,
