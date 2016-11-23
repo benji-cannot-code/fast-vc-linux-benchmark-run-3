7f85820a183318df612e1b4bc0e3017ec2b79242
Guenter Roeck <linux@roeck-us.net>
2016-11-22 17:57:02 -0800

    Input: synaptics-rmi4 - propagate correct number of rx and tx electrodes to F54
    F54 diagnostics report functions provide data based on the number of
    enabled rx and tx electrodes, which is not identical to the number of
    electrodes reported with F54:Query0 and F54:Query1. Those values report
    the number of supported electrodes, not the number of enabled electrodes.
    The number of enabled electrodes can be determined by analyzing F55:Ctrl1
    (sensor receiver assignment) and F55:Ctrl2 (sensor transmitter assignment).
    
    Propagate the number of enabled electrodes from F55 to F54 to avoid
    corrupted output if not all electrodes are enabled.
    
    Fixes: 3a762dbd5347 ("[media] Input: synaptics-rmi4 - add support for F54 ...")
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Nick Dyer <nick@shmanahar.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/rmi4/rmi_f55.c
+++ drivers/input/rmi4/rmi_f55.c
@@ -39,6 +39,8 @@ struct f55_data {
 
 static int rmi_f55_detect(struct rmi_function *fn)
 {
+	struct rmi_device *rmi_dev = fn->rmi_dev;
+	struct rmi_driver_data *drv_data = dev_get_drvdata(&rmi_dev->dev);
 	struct f55_data *f55;
 	int error;
 
@@ -57,6 +59,9 @@ static int rmi_f55_detect(struct rmi_function *fn)
 
 	f55->cfg_num_rx_electrodes = f55->num_rx_electrodes;
 	f55->cfg_num_tx_electrodes = f55->num_rx_electrodes;
+
+	drv_data->num_rx_electrodes = f55->cfg_num_rx_electrodes;
+	drv_data->num_tx_electrodes = f55->cfg_num_rx_electrodes;
 
 	if (f55->qry[F55_PHYS_CHAR_OFFSET] & F55_CAP_SENSOR_ASSIGN) {
 		int i, total;
@@ -79,6 +84,7 @@ static int rmi_f55_detect(struct rmi_function *fn)
 					total++;
 			}
 			f55->cfg_num_rx_electrodes = total;
+			drv_data->num_rx_electrodes = total;
 		}
 
 		error = rmi_read_block(fn->rmi_dev,
@@ -91,6 +97,7 @@ static int rmi_f55_detect(struct rmi_function *fn)
 					total++;
 			}
 			f55->cfg_num_tx_electrodes = total;
+			drv_data->num_tx_electrodes = total;
 		}
 	}
 
