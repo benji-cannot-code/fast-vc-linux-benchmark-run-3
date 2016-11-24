fb9931ac343ab3ee949f30ff84262e4061379938
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-11-24 09:52:35 -0200

    [media] ivtv: mark DVB "borrowed" ioctls as deprecated
    changeset da8ec560e3b4 ("[media] ivtv: implement new decoder command
    ioctls") implemented proper support for mpeg audio and video control
    at V4L2 API. Since then, the usage of the the DVB APIs is deprecated.
    
    However, we never actually marked it as deprecated nor provided any
    way to disable it. Let's do it now.
    
    This patch prepares for the removal of this bad usage on a couple
    of Kernel versions.
    
    Fixes: da8ec560e3b4 ("[media] ivtv: implement new decoder command ioctls")
    
    Acked-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/ivtv/ivtv-driver.h
+++ drivers/media/pci/ivtv/ivtv-driver.h
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/byteorder.h>
 #include <linux/delay.h>
 #include <linux/device.h>
-#include <linux/dvb/audio.h>
-#include <linux/dvb/video.h>
 #include <linux/fs.h>
 #include <linux/i2c.h>
 #include <linux/i2c-algo-bit.h>
