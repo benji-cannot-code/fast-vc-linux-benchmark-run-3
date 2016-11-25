5e656e7ed1d5f74db15fc0a92b91d5292d1da5bf
Arkadiusz Hiler <arkadiusz.hiler@intel.com>
2016-11-25 18:59:36 +0100

    drm/i915/guc: Init send_mutex in intel_uc_init_early()
    send_mutex is used to serialise communication with GuC via
    intel_guc_send().
    
    Since functions that utilize it are no longer limited to submission,
    initialization should be handled as a part of general setup.
    
    v2: move initialization to *_early()
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Michal Winiarski <michal.winiarski@intel.com>
    Signed-off-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/1480096777-12573-5-git-send-email-arkadiusz.hiler@intel.com
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_uc.c
+++ drivers/gpu/drm/i915/intel_uc.c
@@ -26,6 +26,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_drv.h"
 #include "intel_uc.h"
 
+void intel_uc_init_early(struct drm_i915_private *dev_priv)
+{
+	mutex_init(&dev_priv->guc.send_mutex);
+}
+
 /*
  * Read GuC command/status register (SOFT_SCRATCH_0)
  * Return true if it contains a response rather than a command
