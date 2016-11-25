bbbb1bae0771b1d4a5026b1715bc4e6dcf6f6cc8
Linus Walleij <linus.walleij@linaro.org>
2016-11-25 10:53:39 +0100

    gpio: pl061: move platform data into driver
    No boardfile defines any PL061 platform data anymore: the
    Integrator IM/PD-1 includes the file but is not making use
    of the struct. Let's delete the include and all references,
    then move the platform data into the driver for later
    consolidation into the driver state container.
    
    The only resource defined by the IM/PD-1 is the IRQ which
    is passed through the AMBA PrimeCell bus abstraction
    struct amba_device.
    
    Cc: arm@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: Russell King <linux@armlinux.org.uk>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-pl061.c
+++ drivers/gpio/gpio-pl061.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/device.h>
 #include <linux/amba/bus.h>
-#include <linux/amba/pl061.h>
 #include <linux/slab.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/pm.h>
@@ -39,6 +38,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GPIOIC  0x41C
 
 #define PL061_GPIO_NR	8
+
+struct pl061_platform_data {
+	/* number of the first GPIO */
+	unsigned	gpio_base;
+
+	/* number of the first IRQ.
+	 * If the IRQ functionality in not desired this must be set to 0.
+	 */
+	unsigned	irq_base;
+
+	u8		directions;	/* startup directions, 1: out, 0: in */
+	u8		values;		/* startup values */
+};
 
 #ifdef CONFIG_PM
 struct pl061_context_save_regs {
