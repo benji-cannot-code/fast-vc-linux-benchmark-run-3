96b880a415f534f8c4aabe2fd5a14c6cb32873c9
Pablo Neira Ayuso <pablo@netfilter.org>
2016-11-28 00:04:43 +0100

    netfilter: nft_quota: add stateful object type
    Register a new quota stateful object type into the new stateful object
    infrastructure.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/uapi/linux/netfilter/nf_tables.h
+++ include/uapi/linux/netfilter/nf_tables.h
@@ -1188,6 +1188,7 @@ enum nft_fib_flags {
 
 #define NFT_OBJECT_UNSPEC	0
 #define NFT_OBJECT_COUNTER	1
+#define NFT_OBJECT_QUOTA	2
 
 /**
  * enum nft_object_attributes - nf_tables stateful object netlink attributes
