1b900d8873feecb321e39154575446a1865de225
Herbert Xu <herbert@gondor.apana.org.au>
2016-11-30 21:17:24 +0800

    crypto: arm64/aes-ce-ccm - Fix AEAD decryption length
    This patch fixes the ARM64 CE CCM implementation decryption by
    using skcipher_walk_aead_decrypt instead of skcipher_walk_aead,
    which ensures the correct length is used when doing the walk.
    
    Fixes: cf2c0fe74084 ("crypto: aes-ce-ccm - Use skcipher walk interface")
    Reported-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/arm64/crypto/aes-ce-ccm-glue.c
+++ arch/arm64/crypto/aes-ce-ccm-glue.c
@@ -168,7 +168,7 @@ static int ccm_encrypt(struct aead_request *req)
 	/* preserve the original iv for the final round */
 	memcpy(buf, req->iv, AES_BLOCK_SIZE);
 
-	err = skcipher_walk_aead(&walk, req, true);
+	err = skcipher_walk_aead_encrypt(&walk, req, true);
 
 	while (walk.nbytes) {
 		u32 tail = walk.nbytes % AES_BLOCK_SIZE;
@@ -220,7 +220,7 @@ static int ccm_decrypt(struct aead_request *req)
 	/* preserve the original iv for the final round */
 	memcpy(buf, req->iv, AES_BLOCK_SIZE);
 
-	err = skcipher_walk_aead(&walk, req, true);
+	err = skcipher_walk_aead_decrypt(&walk, req, true);
 
 	while (walk.nbytes) {
 		u32 tail = walk.nbytes % AES_BLOCK_SIZE;
