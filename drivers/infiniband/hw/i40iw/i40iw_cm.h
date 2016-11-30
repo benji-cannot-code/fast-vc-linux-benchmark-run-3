e9069cc6dcdf2a9615fb7c82751db23310af851b
Shiraz Saleem <shiraz.saleem@intel.com>
2016-11-30 15:09:34 -0600

    i40iw: Set TOS field in IP header
    Set the TOS field in IP header with the value passed in
    from application. If there is mismatch between the remote
    client's TOS and listener, set the listener Tos to the higher
    of the two values.
    
    Signed-off-by: Shiraz Saleem <shiraz.saleem@intel.com>
    Signed-off-by: Faisal Latif <faisal.latif@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/i40iw/i40iw_cm.h
+++ drivers/infiniband/hw/i40iw/i40iw_cm.h
@@ -298,6 +298,7 @@ struct i40iw_cm_listener {
 	enum i40iw_cm_listener_state listener_state;
 	u32 reused_node;
 	u8 user_pri;
+	u8 tos;
 	u16 vlan_id;
 	bool qhash_set;
 	bool ipv4;
@@ -344,6 +345,7 @@ struct i40iw_cm_node {
 	atomic_t passive_state;
 	bool qhash_set;
 	u8 user_pri;
+	u8 tos;
 	bool ipv4;
 	bool snd_mark_en;
 	u16 lsmm_size;
@@ -369,6 +371,7 @@ struct i40iw_cm_info {
 	u16 vlan_id;
 	int backlog;
 	u8 user_pri;
+	u8 tos;
 	bool ipv4;
 };
 
