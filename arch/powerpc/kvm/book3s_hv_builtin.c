8d66490dc7dbc75eb9d74e1a082a8fc444e5e3c9
Paul Mackerras <paulus@ozlabs.org>
2016-12-01 14:03:46 +1100

    KVM: PPC: Book3S: Move prototypes for KVM functions into kvm_ppc.h
    This moves the prototypes for functions that are only called from
    assembler code out of asm/asm-prototypes.h into asm/kvm_ppc.h.
    The prototypes were added in commit ebe4535fbe7a ("KVM: PPC:
    Book3S HV: sparse: prototypes for functions called from assembler",
    2016-10-10), but given that the functions are KVM functions,
    having them in a KVM header will be better for long-term
    maintenance.
    
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- arch/powerpc/kvm/book3s_hv_builtin.c
+++ arch/powerpc/kvm/book3s_hv_builtin.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/dbell.h>
 #include <asm/cputhreads.h>
 #include <asm/io.h>
-#include <asm/asm-prototypes.h>
 #include <asm/opal.h>
 #include <asm/smp.h>
 
