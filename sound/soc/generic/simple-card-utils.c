522d4492356d21641f4a0ce8dc2858796b0e8a4d
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2016-12-02 05:27:30 +0000

    ASoC: simple_card_utils: tidyup file comment/define
    simple_card_utils was created as simple_card_core in 1st prototype,
    and current code still have it. Let's tidyup
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/generic/simple-card-utils.c
+++ sound/soc/generic/simple-card-utils.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * simple-card-core.c
+ * simple-card-utils.c
  *
  * Copyright (c) 2016 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
  *
