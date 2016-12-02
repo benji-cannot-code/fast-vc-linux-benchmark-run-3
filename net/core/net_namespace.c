3099c02a58e1817c0f4b0eb008bfe51b9a61024b
Alexey Dobriyan <adobriyan@gmail.com>
2016-12-02 04:11:34 +0300

    netns: publish net_generic correctly
    Publishing net_generic pointer is done with silly mistake: new array is
    published BEFORE setting freshly acquired pernet subsystem pointer.
    
    	memcpy
    	rcu_assign_pointer
    	kfree_rcu
    	ng->ptr[id - 1] = data;
    
    This bug was introduced with commit dec827d174d7f76c457238800183ca864a639365
    ("[NETNS]: The generic per-net pointers.") in the glorious days of
    chopping networking stack into containers proper 8.5 years ago (whee...)
    
    How it didn't trigger for so long?
    Well, you need quite specific set of conditions:
    
    *) race window opens once per pernet subsystem addition
       (read: modprobe or boot)
    
    *) not every pernet subsystem is eligible (need ->id and ->size)
    
    *) not every pernet subsystem is vulnerable (need incorrect or absense
       of ordering of register_pernet_sybsys() and actually using net_generic())
    
    *) to hide the bug even more, default is to preallocate 13 pointers which
       is actually quite a lot. You need IPv6, netfilter, bridging etc together
       loaded to trigger reallocation in the first place. Trimmed down
       config are OK.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -65,9 +65,10 @@ static int net_assign_generic(struct net *net, unsigned int id, void *data)
 
 	old_ng = rcu_dereference_protected(net->gen,
 					   lockdep_is_held(&net_mutex));
-	ng = old_ng;
-	if (old_ng->len >= id)
-		goto assign;
+	if (old_ng->len >= id) {
+		old_ng->ptr[id - 1] = data;
+		return 0;
+	}
 
 	ng = net_alloc_generic();
 	if (ng == NULL)
@@ -85,11 +86,10 @@ static int net_assign_generic(struct net *net, unsigned int id, void *data)
 	 */
 
 	memcpy(&ng->ptr, &old_ng->ptr, old_ng->len * sizeof(void*));
+	ng->ptr[id - 1] = data;
 
 	rcu_assign_pointer(net->gen, ng);
 	kfree_rcu(old_ng, rcu);
-assign:
-	ng->ptr[id - 1] = data;
 	return 0;
 }
 
