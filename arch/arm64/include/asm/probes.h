5c926b6429084c2fd256c2c5f65205e363d978f8
Marc Zyngier <marc.zyngier@arm.com>
2016-12-03 14:05:37 +0000

    arm64: Remove reference to asm/opcodes.h
    The asm/opcodes.h file is now gone, but probes.h still references it
    for not obvious reason. Removing the #include directive fixes
    the compilation.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/probes.h
+++ arch/arm64/include/asm/probes.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ARM_PROBES_H
 #define _ARM_PROBES_H
 
-#include <asm/opcodes.h>
-
 typedef u32 probe_opcode_t;
 typedef void (probes_handler_t) (u32 opcode, long addr, struct pt_regs *);
 
