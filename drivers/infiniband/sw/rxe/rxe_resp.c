cc2a1d372e588c396b1eb21cfd5458f7c330134a
Andrew Boyer <andrew.boyer@dell.com>
2016-12-05 08:43:21 -0500

    IB/rxe: Hold refs when running tasklets
    It might be possible for all of a QP's references to be dropped
    while one of that QP's tasklets is running.
    
    For example, the completer might run during QP destroy.
    If qp->valid is false, it will drop all of the packets on
    the resp_pkts list, potentially removing the last reference.
    Then it tries to advance the SQ consumer pointer. If the
    SQ's buffer has already been destroyed, the system will
    panic.
    
    To be safe, hold a reference on the QP for the duration
    of each tasklet.
    
    Signed-off-by: Andrew Boyer <andrew.boyer@dell.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/sw/rxe/rxe_resp.c
+++ drivers/infiniband/sw/rxe/rxe_resp.c
@@ -1213,6 +1213,8 @@ int rxe_responder(void *arg)
 	struct rxe_pkt_info *pkt = NULL;
 	int ret = 0;
 
+	rxe_add_ref(qp);
+
 	qp->resp.aeth_syndrome = AETH_ACK_UNLIMITED;
 
 	if (!qp->valid) {
@@ -1401,5 +1403,6 @@ int rxe_responder(void *arg)
 exit:
 	ret = -EAGAIN;
 done:
+	rxe_drop_ref(qp);
 	return ret;
 }
