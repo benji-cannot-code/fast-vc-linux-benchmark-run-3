c0c0cdefe007e76f49e85ed0afc971a86423e6d1
Mats Randgaard <matrandg@cisco.com>
2016-12-06 08:24:28 -0200

    [media] tc358743: Disable HDCP with "manual HDCP authentication" bit
    Originally Toshiba told us that the only way to disable HDCP was to
    set the receiver in repeater mode, that would make the authentication
    fail because of missing software support. It has worked fine with all
    the sources we and our customers has used, until it was reported
    problems with Apple MacBook (Retina, 12-inch, Early 2015)
    (https://support.apple.com/kb/SP712?locale=en_US&viewlocale=en_US)
    with Apple A1612 USB type-C multiport adapter
    (http://www.apple.com/shop/product/MJ1K2AM/A/usb-c-digital-av-multiport-adapter)
    
    Finally Toshiba came up with a hidden bit that is named "Manual HDCP
    authentication". In this patch the original "repeater mode" concept is
    removed, and the new bit is set instead.
    
    With his patch HDCP is disabled when connected to the Apple MacBook
    and all other sources we have tested so far. The Apple MacBook is
    constantly trying to authenticate, but fails and continues to transmit
    unencrypted video.
    
    Signed-off-by: Mats Randgaard <matrandg@cisco.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/i2c/tc358743_regs.h
+++ drivers/media/i2c/tc358743_regs.h
@@ -421,6 +421,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MASK_MODE_RST_TN                      0x20
 #define MASK_LINE_REKEY                       0x10
 #define MASK_AUTO_CLR                         0x04
+#define MASK_MANUAL_AUTHENTICATION            0x02 /* Not in REF_01 */
 
 #define HDCP_REG1                             0x8563 /* Not in REF_01 */
 #define MASK_AUTH_UNAUTH_SEL                  0x70
