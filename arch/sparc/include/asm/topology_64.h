f28b20f7e6ba319ba57014118ae67c82d57d8c02
Gonglei \(Arei\) <arei.gonglei@huawei.com>
2016-12-08 12:37:08 +0800

    sparc: fix a building error reported by kbuild
    >> arch/sparc/include/asm/topology_64.h:44:44:
    error: implicit declaration of function 'cpu_data'
    [-Werror=implicit-function-declaration]
    
     #define topology_physical_package_id(cpu) (cpu_data(cpu).proc_id)
                                                   ^
    Let's include cpudata.h in topology_64.h.
    
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: sparclinux@vger.kernel.org
    Suggested-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/topology_64.h
+++ arch/sparc/include/asm/topology_64.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_NUMA
 
 #include <asm/mmzone.h>
+#include <asm/cpudata.h>
 
 static inline int cpu_to_node(int cpu)
 {
