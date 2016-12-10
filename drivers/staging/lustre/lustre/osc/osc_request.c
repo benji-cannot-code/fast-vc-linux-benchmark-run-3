c9a549e3d007240192980d71b27e0eacb6a7cdc0
Ben Evans <bevans@cray.com>
2016-12-10 13:05:57 -0500

    staging: lustre: obdclass: Create a header for obdo related functions
    Remove all obdo related functions from lustre_idl.h
    Create lustre_odbo.h. Include where appropriate.
    Make the functions lustre_get_wire_obdo and
    lustre_set_wire_obdo to not be inlined functions.
    
    Signed-off-by: Ben Evans <bevans@cray.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6401
    Reviewed-on: http://review.whamcloud.com/16917
    Reviewed-on: http://review.whamcloud.com/19266
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/osc/osc_request.c
+++ drivers/staging/lustre/lustre/osc/osc_request.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../include/lprocfs_status.h"
 #include "../include/lustre/lustre_ioctl.h"
 #include "../include/lustre_debug.h"
+#include "../include/lustre_obdo.h"
 #include "../include/lustre_param.h"
 #include "../include/lustre_fid.h"
 #include "../include/obd_class.h"
