ebcfbf7963ebc88325d3441dcec0db51d815c862
Corentin LABBE <clabbe.montjoie@gmail.com>
2016-12-13 15:51:14 +0100

    hwrng: core - remove unused PFX macro
    This patch remove the unused PFX macro.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/char/hw_random/core.c
+++ drivers/char/hw_random/core.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 
 #define RNG_MODULE_NAME		"hw_random"
-#define PFX			RNG_MODULE_NAME ": "
 
 static struct hwrng *current_rng;
 static struct task_struct *hwrng_fill;
