61d2651e84ab20468536ecbd7e01890d35887f94
Matthew Wilcox <willy@infradead.org>
2016-12-14 15:08:02 -0800

    radix tree test suite: track preempt_count
    Rather than simply NOP out preempt_enable() and preempt_disable(), keep
    track of preempt_count and display it regularly in case either the test
    suite or the code under test is forgetting to balance the enables &
    disables.  Only found a test-case that was forgetting to re-enable
    preemption, but it's a possibility worth checking.
    
    Link: http://lkml.kernel.org/r/1480369871-5271-39-git-send-email-mawilcox@linuxonhyperv.com
    Signed-off-by: Matthew Wilcox <willy@infradead.org>
    Tested-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Konstantin Khlebnikov <koct9i@gmail.com>
    Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
    Cc: Matthew Wilcox <mawilcox@microsoft.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/radix-tree/linux.c
+++ tools/testing/radix-tree/linux.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <urcu/uatomic.h>
 
 int nr_allocated;
+int preempt_count;
 
 void *mempool_alloc(mempool_t *pool, int gfp_mask)
 {
