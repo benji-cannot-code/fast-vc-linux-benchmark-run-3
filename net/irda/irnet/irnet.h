c9b8bf24734f8a0fb33a41831bbc602c937e98aa
LABBE Corentin <clabbe.montjoie@gmail.com>
2016-12-15 11:42:47 +0100

    irda: irnet: Move linux/miscdevice.h include
    The only use of miscdevice is irda_ppp so no need to include
    linux/miscdevice.h for all irda files.
    This patch move the linux/miscdevice.h include to irnet_ppp.h
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/irda/irnet/irnet.h
+++ net/irda/irnet/irnet.h
@@ -246,7 +246,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty.h>
 #include <linux/proc_fs.h>
 #include <linux/netdevice.h>
-#include <linux/miscdevice.h>
 #include <linux/poll.h>
 #include <linux/capability.h>
 #include <linux/ctype.h>	/* isspace() */
--- net/irda/irnet/irnet_ppp.h
+++ net/irda/irnet/irnet_ppp.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /***************************** INCLUDES *****************************/
 
 #include "irnet.h"		/* Module global include */
+#include <linux/miscdevice.h>
 
 /************************ CONSTANTS & MACROS ************************/
 
