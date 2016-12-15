5919271f49f1df7d22bbb2a790df85e5ab187236
Corentin Labbe <clabbe.montjoie@gmail.com>
2016-12-15 15:22:44 +0100

    nfc: nxp-nci: Remove unneeded linux/miscdevice.h include
    drivers/nfc/nxp-nci/i2c.c does not use any miscdevice, so this patch
    remove this unnecessary inclusion.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/nxp-nci/i2c.c
+++ drivers/nfc/nxp-nci/i2c.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
-#include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/nfc.h>
 #include <linux/gpio/consumer.h>
