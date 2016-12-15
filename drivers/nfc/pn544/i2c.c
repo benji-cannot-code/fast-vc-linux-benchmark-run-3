1fa1010cdffd39db9c4b64e92cae4158f2650839
Corentin Labbe <clabbe.montjoie@gmail.com>
2016-12-15 15:22:45 +0100

    nfc: pn544: Remove unneeded linux/miscdevice.h include
    drivers/nfc/pn544/i2c.c does not use any miscdevice, so this
    patch remove this unnecessary inclusion.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- drivers/nfc/pn544/i2c.c
+++ drivers/nfc/pn544/i2c.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/acpi.h>
-#include <linux/miscdevice.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/nfc.h>
