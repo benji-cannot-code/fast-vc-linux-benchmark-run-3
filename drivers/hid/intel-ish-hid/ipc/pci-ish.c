34b4cbf4166a558059601f25cd072da5d4ad38a3
Corentin Labbe <clabbe.montjoie@gmail.com>
2016-12-15 16:27:11 +0100

    HID: intel-ish-hid: Remove unneeded linux/miscdevice.h include
    hid/intel-ish-hid does not use any miscdevice so this patch remove
    this unnecessary inclusion.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/intel-ish-hid/ipc/pci-ish.c
+++ drivers/hid/intel-ish-hid/ipc/pci-ish.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/interrupt.h>
 #include <linux/workqueue.h>
-#include <linux/miscdevice.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/intel_ish.h>
 #include "ishtp-dev.h"
--- drivers/hid/intel-ish-hid/ishtp/hbm.c
+++ drivers/hid/intel-ish-hid/ishtp/hbm.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/wait.h>
 #include <linux/spinlock.h>
-#include <linux/miscdevice.h>
 #include "ishtp-dev.h"
 #include "hbm.h"
 #include "client.h"
