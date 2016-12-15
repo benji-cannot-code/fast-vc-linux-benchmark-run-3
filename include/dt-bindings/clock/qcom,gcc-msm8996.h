7de40096f270b934ea581a61f642c77556a1b22a
Avaneesh Kumar Dwivedi <akdwived@codeaurora.org>
2016-12-15 17:51:30 +0530

    clk: qcom: Add GCC_MSS_RESET support
    Add support to use reset control framework for resetting MSS
    with hexagon v56 1.5.0.
    
    Signed-off-by: Avaneesh Kumar Dwivedi <akdwived@codeaurora.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/dt-bindings/clock/qcom,gcc-msm8996.h
+++ include/dt-bindings/clock/qcom,gcc-msm8996.h
@@ -340,6 +340,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GCC_PCIE_PHY_COM_NOCSR_BCR				102
 #define GCC_USB3_PHY_BCR					103
 #define GCC_USB3PHY_PHY_BCR					104
+#define GCC_MSS_RESTART						105
 
 
 /* Indexes for GDSCs */
