56241f9b391ae39a7fda47315bfd4f6f0b95d6c4
Flora Cui <Flora.Cui@amd.com>
2016-12-15 14:58:28 +0800

    drm/amdgpu: update rev id for hainan
    Signed-off-by: Flora Cui <Flora.Cui@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org

--- drivers/gpu/drm/amd/amdgpu/si.c
+++ drivers/gpu/drm/amd/amdgpu/si.c
@@ -1280,6 +1280,7 @@ static int si_common_early_init(void *handle)
 			AMD_CG_SUPPORT_HDP_LS |
 			AMD_CG_SUPPORT_HDP_MGCG;
 		adev->pg_flags = 0;
+		adev->external_rev_id = 70;
 		break;
 
 	default:
