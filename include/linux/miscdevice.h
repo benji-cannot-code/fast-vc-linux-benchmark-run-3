dba1e97e2daedd46057bfbcd0593b3d368b6f5c8
LABBE Corentin <clabbe.montjoie@gmail.com>
2016-12-15 11:42:48 +0100

    irnet: ppp: move IRNET_MINOR to include/linux/miscdevice.h
    This patch move the define for IRNET_MINOR to include/linux/miscdevice.h
    It is better that all minor number definitions are in the same place.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/miscdevice.h
+++ include/linux/miscdevice.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define STORE_QUEUE_MINOR	155	/* unused */
 #define I2O_MINOR		166
 #define MICROCODE_MINOR		184
+#define IRNET_MINOR		187
 #define VFIO_MINOR		196
 #define TUN_MINOR		200
 #define CUSE_MINOR		203
