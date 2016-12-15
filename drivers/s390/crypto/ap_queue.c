a4d67b95e940bf62a899866d3f45f5e8a8805869
Heiko Carstens <heiko.carstens@de.ibm.com>
2016-12-15 11:28:52 +0100

    s390/zcrypt: make structures static
    Get rid of these:
    drivers/s390/crypto/ap_card.c:140:20:
      warning: symbol 'ap_card_type' was not declared. Should it be static?
    drivers/s390/crypto/ap_queue.c:567:20:
     warning: symbol 'ap_queue_type' was not declared. Should it be static?
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/crypto/ap_queue.c
+++ drivers/s390/crypto/ap_queue.c
@@ -565,7 +565,7 @@ static const struct attribute_group *ap_queue_dev_attr_groups[] = {
 	NULL
 };
 
-struct device_type ap_queue_type = {
+static struct device_type ap_queue_type = {
 	.name = "ap_queue",
 	.groups = ap_queue_dev_attr_groups,
 };
