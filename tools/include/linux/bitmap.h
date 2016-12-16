5c3870d7999c8070481a3752203f7dabe4754a30
Matthew Wilcox <mawilcox@microsoft.com>
2016-12-16 11:52:43 -0500

    radix tree test suite: Remove duplicate bitops code
    By adding __set_bit and __clear_bit to the tools include directory, we
    can share the bitops code.  This reveals an include loop between kernel.h,
    log2.h, bitmap.h and bitops.h.  Break it the same way as the kernel does;
    by moving the kernel.h include from bitops.h to bitmap.h.
    
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>

--- tools/include/linux/bitmap.h
+++ tools/include/linux/bitmap.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <string.h>
 #include <linux/bitops.h>
 #include <stdlib.h>
+#include <linux/kernel.h>
 
 #define DECLARE_BITMAP(name,bits) \
 	unsigned long name[BITS_TO_LONGS(bits)]
--- tools/testing/radix-tree/linux/kernel.h
+++ tools/testing/radix-tree/linux/kernel.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stddef.h>
 #include <limits.h>
 
-#include "../../include/linux/compiler.h"
-#include "../../include/linux/err.h"
+#include <linux/compiler.h>
+#include <linux/err.h>
+#include <linux/bitops.h>
+#include <linux/log2.h>
 #include "../../../include/linux/kconfig.h"
 
 #ifdef BENCHMARK
