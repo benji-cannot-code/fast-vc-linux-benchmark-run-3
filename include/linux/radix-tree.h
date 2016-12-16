a025e4cb4b3bbefa7af3f49d0c43c0410ef4d5cc
Matthew Wilcox <mawilcox@microsoft.com>
2016-12-16 14:46:09 -0500

    radix tree: Add some implicit includes
    We were using spinlock_t and INIT_LIST_HEAD without including spinlock.h
    or list.h.  They were being implicitly included through some other header
    file, but that's fragile.
    
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>

--- include/linux/radix-tree.h
+++ include/linux/radix-tree.h
@@ -23,11 +23,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_RADIX_TREE_H
 
 #include <linux/bitops.h>
-#include <linux/preempt.h>
-#include <linux/types.h>
 #include <linux/bug.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/preempt.h>
 #include <linux/rcupdate.h>
+#include <linux/spinlock.h>
+#include <linux/types.h>
 
 /*
  * The bottom two bits of the slot determine how the remaining bits in the
