89cd6f6a446baab0ac7a80d7c45b63b3ea4a09d7
Hans de Goede <hdegoede@redhat.com>
2016-12-19 01:13:07 +0100

    extcon: axp288: Remove usb_phy notification code
    The usb_phy based intel-usb-phy code never got merged into the
    mainline kernel, so the devm_usb_get_phy() call will always fail,
    blocking the driver from loading.
    
    Since new drivers should use the generic-phy framework, not the
    old-style usb_phy stuff, keeping this around is not useful.
    
    Therefor this patch removes the usb_phy notification bits, which together
    with the patch to remove the platform_data dependency, makes this driver
    actually successfully probe on systems with an axp288 pmic.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Chanwoo Choi <cw00.choi@samsung.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/extcon-axp288.c
+++ drivers/extcon/extcon-axp288.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
 #include <linux/property.h>
-#include <linux/usb/phy.h>
 #include <linux/notifier.h>
 #include <linux/extcon.h>
 #include <linux/regmap.h>
@@ -117,7 +116,6 @@ struct axp288_extcon_info {
 	int irq[EXTCON_IRQ_END];
 	struct extcon_dev *edev;
 	struct notifier_block extcon_nb;
-	struct usb_phy *otg;
 };
 
 /* Power up/down reason string array */
@@ -221,9 +219,6 @@ notify_otg:
 			gpiod_set_value(info->gpio_mux_cntl,
 				vbus_attach ? EXTCON_GPIO_MUX_SEL_SOC
 						: EXTCON_GPIO_MUX_SEL_PMIC);
-
-		atomic_notifier_call_chain(&info->otg->notifier,
-			vbus_attach ? USB_EVENT_VBUS : USB_EVENT_NONE, NULL);
 	}
 
 	if (notify_charger)
@@ -302,13 +297,6 @@ static int axp288_extcon_probe(struct platform_device *pdev)
 	if (ret) {
 		dev_err(&pdev->dev, "failed to register extcon device\n");
 		return ret;
-	}
-
-	/* Get otg transceiver phy */
-	info->otg = devm_usb_get_phy(&pdev->dev, USB_PHY_TYPE_USB2);
-	if (IS_ERR(info->otg)) {
-		dev_err(&pdev->dev, "failed to get otg transceiver\n");
-		return PTR_ERR(info->otg);
 	}
 
 	/* Set up gpio control for USB Mux */
