92c7cec4bdb3cf2d2c2952a8ad99a29a523c5195
Matthew Wilcox <mawilcox@microsoft.com>
2016-12-19 11:07:17 -0500

    tools: Provide a definition of WARN_ON
    The definition of WARN_ON being used by the radix tree test suite was
    deficient in two ways: it did not provide a return value, and it stopped
    execution instead of continuing.  This version of WARN_ON tells you
    which file & line the assertion was triggered in.
    
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>

--- tools/testing/radix-tree/linux/kernel.h
+++ tools/testing/radix-tree/linux/kernel.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #define BUG_ON(expr)	assert(!(expr))
-#define WARN_ON(expr)	assert(!(expr))
 #define __init
 #define __must_check
 #define panic(expr)
