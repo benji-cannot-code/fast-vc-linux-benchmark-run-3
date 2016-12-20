64985e1f2a97fa25fccc5512f6fb760c385d8c01
Tony Cheng <tony.cheng@amd.com>
2016-12-20 10:54:38 -0500

    drm/amd/display: track cursor width in ipp
    Signed-off-by: Tony Cheng <tony.cheng@amd.com>
    Reviewed-by: Yongqiang Sun <yongqiang.sun@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/inc/hw/ipp.h
+++ drivers/gpu/drm/amd/display/dc/inc/hw/ipp.h
@@ -36,8 +36,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* IPP RELATED */
 struct input_pixel_processor {
 	struct  dc_context *ctx;
-	uint32_t inst;
+	unsigned int inst;
 	const struct ipp_funcs *funcs;
+
+	unsigned int cusor_width;
 };
 
 enum ipp_prescale_mode {
