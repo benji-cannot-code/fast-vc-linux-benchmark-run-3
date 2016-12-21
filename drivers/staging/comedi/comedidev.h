fc09e8d6777b8c138bcee6ec34a7d411e0b58b4e
Cheah Kok Cheong <thrust73@gmail.com>
2016-12-22 03:13:19 +0800

    Staging: comedi: comedidev.h: Drop old style zero-length array
    According to Documentation/Changes, the minimum gcc version required
    to compile the kernel is 3.2 (this is probably outdated too).
    
    Signed-off-by: Cheah Kok Cheong <thrust73@gmail.com>
    Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/comedidev.h
+++ drivers/staging/comedi/comedidev.h
@@ -613,12 +613,6 @@ extern const struct comedi_lrange range_unknown;
 
 #define range_digital		range_unipolar5
 
-#if __GNUC__ >= 3
-#define GCC_ZERO_LENGTH_ARRAY
-#else
-#define GCC_ZERO_LENGTH_ARRAY 0
-#endif
-
 /**
  * struct comedi_lrange - Describes a COMEDI range table
  * @length: Number of entries in the range table.
@@ -632,7 +626,7 @@ extern const struct comedi_lrange range_unknown;
  */
 struct comedi_lrange {
 	int length;
-	struct comedi_krange range[GCC_ZERO_LENGTH_ARRAY];
+	struct comedi_krange range[];
 };
 
 /**
