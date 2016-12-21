f5f1e40c9cd3165d2323a3f3d039007a7100d12b
Baolin Wang <baolin.wang@linaro.org>
2016-12-21 15:51:26 +0900

    extcon: axp288: Set EXTCON_USB when EXTCON_CHG_USB_SDP was set
    According to the documentation, we should set the EXTCON_USB when
    one SDP charger connector was reported.
    
    Signed-off-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/extcon-axp288.c
+++ drivers/extcon/extcon-axp288.c
@@ -97,6 +97,7 @@ static const unsigned int axp288_extcon_cables[] = {
 	EXTCON_CHG_USB_SDP,
 	EXTCON_CHG_USB_CDP,
 	EXTCON_CHG_USB_DCP,
+	EXTCON_USB,
 	EXTCON_NONE,
 };
 
@@ -207,8 +208,15 @@ no_vbus:
 					: EXTCON_GPIO_MUX_SEL_PMIC);
 
 	extcon_set_state_sync(info->edev, info->previous_cable, false);
+	if (info->previous_cable == EXTCON_CHG_USB_SDP)
+		extcon_set_state_sync(info->edev, EXTCON_USB, false);
+
 	if (vbus_attach) {
 		extcon_set_state_sync(info->edev, cable, vbus_attach);
+		if (cable == EXTCON_CHG_USB_SDP)
+			extcon_set_state_sync(info->edev, EXTCON_USB,
+						vbus_attach);
+
 		info->previous_cable = cable;
 	}
 
