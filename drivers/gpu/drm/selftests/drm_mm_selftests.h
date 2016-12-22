f4b489aab33a4da027a4342458737a253fe8758c
Chris Wilson <chris@chris-wilson.co.uk>
2016-12-22 08:36:09 +0000

    drm: Add some kselftests for the DRM range manager (struct drm_mm)
    First we introduce a smattering of infrastructure for writing selftests.
    The idea is that we have a test module that exercises a particular
    portion of the exported API, and that module provides a set of tests
    that can either be run as an ensemble via kselftest or individually via
    an igt harness (in this case igt/drm_mm). To accommodate selecting
    individual tests, we export a boolean parameter to control selection of
    each test - that is hidden inside a bunch of reusable boilerplate macros
    to keep writing the tests simple.
    
    v2: Choose a random random_seed unless one is specified by the user.
    v3: More parameters to control max_iterations and max_prime of the
    tests.
    
    Testcase: igt/drm_mm
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161222083641.2691-7-chris@chris-wilson.co.uk

---
+++ drivers/gpu/drm/selftests/drm_mm_selftests.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* List each unit test as selftest(name, function)
+ *
+ * The name is used as both an enum and expanded as igt__name to create
+ * a module parameter. It must be unique and legal for a C identifier.
+ *
+ * Tests are executed in order by igt/drm_mm
+ */
+selftest(sanitycheck, igt_sanitycheck) /* keep first (selfcheck for igt) */
