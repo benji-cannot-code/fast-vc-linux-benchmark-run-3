96f0c84b961b172312fcb21ab88945df1b70a62a
Chris Wilson <chris@chris-wilson.co.uk>
2016-12-22 08:36:12 +0000

    drm: kselftest for drm_mm_reserve_node()
    Exercise drm_mm_reserve_node(), check that we can't reserve an already
    occupied range and that the lists are correct after reserving/removing.
    
    v2: Check for invalid node reservation.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161222083641.2691-10-chris@chris-wilson.co.uk

--- drivers/gpu/drm/selftests/drm_mm_selftests.h
+++ drivers/gpu/drm/selftests/drm_mm_selftests.h
@@ -9,3 +9,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 selftest(sanitycheck, igt_sanitycheck) /* keep first (selfcheck for igt) */
 selftest(init, igt_init)
 selftest(debug, igt_debug)
+selftest(reserve, igt_reserve)
