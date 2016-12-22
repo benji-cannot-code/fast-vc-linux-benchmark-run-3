1c7df09d3319bf5da4bf274b9d964fde04c2c145
Chris Wilson <chris@chris-wilson.co.uk>
2016-12-22 08:36:10 +0000

    drm: kselftest for drm_mm_init()
    Simple first test to just exercise initialisation of struct drm_mm.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161222083641.2691-8-chris@chris-wilson.co.uk

--- drivers/gpu/drm/selftests/drm_mm_selftests.h
+++ drivers/gpu/drm/selftests/drm_mm_selftests.h
@@ -7,3 +7,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Tests are executed in order by igt/drm_mm
  */
 selftest(sanitycheck, igt_sanitycheck) /* keep first (selfcheck for igt) */
+selftest(init, igt_init)
