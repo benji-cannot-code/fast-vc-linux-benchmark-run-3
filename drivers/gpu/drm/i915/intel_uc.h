643733d57f8b893062453886bf85f936af7c64be
Chris Wilson <chris@chris-wilson.co.uk>
2016-12-24 19:31:46 +0000

    drm/i915/guc: Assert that all GGTT offsets used by the GuC are mappable
    Add an assertion to the plain i915_ggtt_offset() to double check that
    any offset we hand to the GuC is outside of its unmappable ranges.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161224193146.4402-1-chris@chris-wilson.co.uk
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>

--- drivers/gpu/drm/i915/intel_uc.h
+++ drivers/gpu/drm/i915/intel_uc.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_guc_reg.h"
 #include "intel_ringbuffer.h"
 
+#include "i915_vma.h"
+
 struct drm_i915_gem_request;
 
 /*
@@ -198,5 +200,12 @@ void i915_guc_flush_logs(struct drm_i915_private *dev_priv);
 void i915_guc_register(struct drm_i915_private *dev_priv);
 void i915_guc_unregister(struct drm_i915_private *dev_priv);
 int i915_guc_log_control(struct drm_i915_private *dev_priv, u64 control_val);
+
+static inline u32 guc_ggtt_offset(struct i915_vma *vma)
+{
+	u32 offset = i915_ggtt_offset(vma);
+	GEM_BUG_ON(offset < GUC_WOPCM_TOP);
+	return offset;
+}
 
 #endif
