2d9b1ea18738013306c19b81d0ef276cc75ced9e
Daniel Vetter <daniel.vetter@ffwll.ch>
2016-12-27 11:10:57 +0100

    drm/mm: Document locking rules
    Drivers need to take care. Motivated by a discussion between Mark and
    Rob on dri-devel.
    
    Cc: Mark yao <mark.yao@rock-chips.com>
    Cc: Rob Clark <robdclark@gmail.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    [danvet: s/alloc|freeing/modifications/ per Chris' suggestion.]
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1482833457-29592-1-git-send-email-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/drm_mm.c
+++ drivers/gpu/drm/drm_mm.c
@@ -91,6 +91,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Finally iteration helpers to walk all nodes and all holes are provided as are
  * some basic allocator dumpers for debugging.
+ *
+ * Note that this range allocator is not thread-safe, drivers need to protect
+ * modifications with their on locking. The idea behind this is that for a full
+ * memory manager additional data needs to be protected anyway, hence internal
+ * locking would be fully redundant.
  */
 
 static struct drm_mm_node *drm_mm_search_free_in_range_generic(const struct drm_mm *mm,
