04aa85d23ff22a10008ceca9a68f7b55c6e93ffa
Heiko Carstens <heiko.carstens@de.ibm.com>
2016-12-27 15:14:26 +0100

    s390/mem_detect: add debugging output
    The s390 specific memory detection code does not call memblock_add,
    which would generate debug output if memblock=debug is specified on
    the kernel command line. Instead it directly calls memblock_add_range,
    which doesn't generate any debug output.
    To have a chance to debug early memblock related bugs add an s390
    specific memblock_dbg call and a (missing) memblock_dump_all call.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/mm/mem_detect.c
+++ arch/s390/mm/mem_detect.c
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline void memblock_physmem_add(phys_addr_t start, phys_addr_t size)
 {
+	memblock_dbg("memblock_physmem_add: [%#016llx-%#016llx]\n",
+		     start, start + size - 1);
 	memblock_add_range(&memblock.memory, start, size, 0, 0);
 	memblock_add_range(&memblock.physmem, start, size, 0, 0);
 }
@@ -56,4 +58,5 @@ void __init detect_memory_memblock(void)
 	memblock_set_bottom_up(false);
 	if (!max_physmem_end)
 		max_physmem_end = memblock_end_of_DRAM();
+	memblock_dump_all();
 }
