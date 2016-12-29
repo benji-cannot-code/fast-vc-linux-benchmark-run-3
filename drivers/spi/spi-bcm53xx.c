bfaa44bf3f4e582aa04e62fe817f3f7762c70145
Rafał Miłecki <rafal@milecki.pl>
2016-12-29 20:13:13 +0100

    spi: bcm53xx: (re)license code to the GPL v2
    My intention was to release this code under GPL v2 license. For some
    reason my initial commit 0fc6a323e191 ("spi: bcm53xx: driver for SPI
    controller on Broadcom bcma SoC") totally missed licensing info.
    MODULE_LICENSE was later added by Axel specifying "GNU Public License
    v2 or later".
    
    This patch clarifies situation by adding a proper header (with Copyright
    line) and adjusting MODULE_LICENSE. It should be acked by every driver
    contributor.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Acked-by: Nicholas Mc Guire <hofrat@osadl.org>
    Reviewed-by: Jingoo Han <jingoohan1@gmail.com>
    Acked-by: Jingoo Han <jingoohan1@gmail.com>
    Acked-by: Joe Perches <joe@perches.com>
    Acked-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: Vaishali Thakkar <vaishali.thakkar@oracle.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-bcm53xx.c
+++ drivers/spi/spi-bcm53xx.c
@@ -1,4 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (C) 2014-2016 Rafał Miłecki <rafal@milecki.pl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
 #define pr_fmt(fmt)		KBUILD_MODNAME ": " fmt
 
 #include <linux/kernel.h>
@@ -356,4 +364,4 @@ module_exit(bcm53xxspi_module_exit);
 
 MODULE_DESCRIPTION("Broadcom BCM53xx SPI Controller driver");
 MODULE_AUTHOR("Rafał Miłecki <zajec5@gmail.com>");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
