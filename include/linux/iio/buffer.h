1101943f7bbbf4adee55f570ebdb46eaec83fa76
Jonathan Cameron <jic23@kernel.org>
2017-01-02 19:28:26 +0000

    iio:buffer: Introduced a function to assign the buffer specific attrs.
    This is a necessary step in taking the buffer implementation
    opaque.
    
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    Reviewed-by: Lars-Peter Clausen <lars@metafoo.de>

--- include/linux/iio/buffer.h
+++ include/linux/iio/buffer.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct iio_buffer;
 
+void iio_buffer_set_attrs(struct iio_buffer *buffer,
+			 const struct attribute **attrs);
 /**
  * INDIO_BUFFER_FLAG_FIXED_WATERMARK - Watermark level of the buffer can not be
  *   configured. It has a fixed value which will be buffer specific.
