66be968ec4498404a30c64a79ff22b80fbe0ac95
Colin Ian King <colin.king@canonical.com>
2017-01-03 13:21:22 +0000

    crypto: mediatek - don't return garbage err on successful return
    In the case where keylen <= bs mtk_sha_setkey returns an uninitialized
    return value in err.  Fix this by returning 0 instead of err.
    
    Issue detected by static analysis with cppcheck.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/mediatek/mtk-sha.c
+++ drivers/crypto/mediatek/mtk-sha.c
@@ -879,7 +879,7 @@ static int mtk_sha_setkey(struct crypto_ahash *tfm,
 		bctx->opad[i] ^= 0x5c;
 	}
 
-	return err;
+	return 0;
 }
 
 static int mtk_sha_export(struct ahash_request *req, void *out)
