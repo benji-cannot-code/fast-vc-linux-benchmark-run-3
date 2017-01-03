2bd8442e3858f6c554b52b4fd4b5f3afbd5376f8
Paulo Zanoni <paulo.r.zanoni@intel.com>
2017-01-03 18:04:18 -0200

    drm/i915: fix INTEL_BDW_IDS definition
    Remove duplicated IDs from the list. Currently, this definition is
    only used by early-quirks.c. From my understanding of the code, having
    duplicated IDs shouldn't be causing any bugs.
    
    Fixes: 8d9c20e1d1e3 ("drm/i915: Remove .is_mobile field from platform struct")
    Cc: Carlos Santa <carlos.santa@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1483473860-17644-1-git-send-email-paulo.r.zanoni@intel.com

--- include/drm/i915_pciids.h
+++ include/drm/i915_pciids.h
@@ -241,8 +241,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	INTEL_BDW_GT12_IDS(info), \
 	INTEL_BDW_GT3_IDS(info), \
 	INTEL_BDW_RSVDM_IDS(info), \
-	INTEL_BDW_GT12_IDS(info), \
-	INTEL_BDW_GT3_IDS(info), \
 	INTEL_BDW_RSVDD_IDS(info)
 
 #define INTEL_CHV_IDS(info) \
