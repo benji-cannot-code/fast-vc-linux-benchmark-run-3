597fe6f789276f30872333f53f73bd671ad132d6
Thierry Reding <thierry.reding@gmail.com>
2017-01-04 09:39:52 +0100

    pwm: Remove pwm_can_sleep()
    The last user of this function has been removed, so it is no longer
    needed.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- include/linux/pwm.h
+++ include/linux/pwm.h
@@ -452,8 +452,6 @@ struct pwm_device *devm_pwm_get(struct device *dev, const char *con_id);
 struct pwm_device *devm_of_pwm_get(struct device *dev, struct device_node *np,
 				   const char *con_id);
 void devm_pwm_put(struct device *dev, struct pwm_device *pwm);
-
-bool pwm_can_sleep(struct pwm_device *pwm);
 #else
 static inline struct pwm_device *pwm_request(int pwm_id, const char *label)
 {
@@ -566,11 +564,6 @@ static inline struct pwm_device *devm_of_pwm_get(struct device *dev,
 
 static inline void devm_pwm_put(struct device *dev, struct pwm_device *pwm)
 {
-}
-
-static inline bool pwm_can_sleep(struct pwm_device *pwm)
-{
-	return false;
 }
 #endif
 
