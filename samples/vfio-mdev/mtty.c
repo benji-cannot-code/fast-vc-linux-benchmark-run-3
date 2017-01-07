c1dc06d280c4509a4bc95b721b462601cd4b2e7c
Dan Carpenter <dan.carpenter@oracle.com>
2017-01-07 09:28:40 +0300

    vfio-mdev: buffer overflow in ioctl()
    This is a sample driver for documentation so the impact is probably
    pretty low.  But we should check that bar_index is valid so we
    don't write beyond the end of the mdev_state->region_info[] array.
    
    Fixes: 9d1a546c53b4 ("docs: Sample driver to demonstrate how to use Mediated device framework.")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Kirti Wankhede <kwankhede@nvidia.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- samples/vfio-mdev/mtty.c
+++ samples/vfio-mdev/mtty.c
@@ -1074,7 +1074,7 @@ int mtty_get_region_info(struct mdev_device *mdev,
 {
 	unsigned int size = 0;
 	struct mdev_state *mdev_state;
-	int bar_index;
+	u32 bar_index;
 
 	if (!mdev)
 		return -EINVAL;
@@ -1083,8 +1083,11 @@ int mtty_get_region_info(struct mdev_device *mdev,
 	if (!mdev_state)
 		return -EINVAL;
 
-	mutex_lock(&mdev_state->ops_lock);
 	bar_index = region_info->index;
+	if (bar_index >= VFIO_PCI_NUM_REGIONS)
+		return -EINVAL;
+
+	mutex_lock(&mdev_state->ops_lock);
 
 	switch (bar_index) {
 	case VFIO_PCI_CONFIG_REGION_INDEX:
