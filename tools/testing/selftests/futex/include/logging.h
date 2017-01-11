016de0686d3f80344ee756fa67c94b369d9a63ab
Stafford Horne <shorne@gmail.com>
2017-01-12 00:11:14 +0900

    selftests/futex: Add stdio used for logging
    Fix missing printf compile warnings.
    
    Signed-off-by: Stafford Horne <shorne@gmail.com>
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>

--- tools/testing/selftests/futex/include/logging.h
+++ tools/testing/selftests/futex/include/logging.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LOGGING_H
 #define _LOGGING_H
 
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <linux/futex.h>
