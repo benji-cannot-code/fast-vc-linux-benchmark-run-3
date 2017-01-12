37344c78a6da9d35e40da4e841ebf53fcb2fb0c5
Bogdan Purcareata <bogdan.purcareata@gmail.com>
2017-01-13 00:01:55 +0200

    staging: android/ion: Align comment mark
    Fix missing space right before multiple line comment closing.
    
    Signed-off-by: Bogdan Purcareata <bogdan.purcareata@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion_priv.h
+++ drivers/staging/android/ion/ion_priv.h
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *			handle, used for debugging
  * @pid:		pid of last client to reference this buffer in a
  *			handle, used for debugging
-*/
+ */
 struct ion_buffer {
 	struct kref ref;
 	union {
