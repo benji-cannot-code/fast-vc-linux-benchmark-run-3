4ce6d174b7ecc1b21828f15d38fd21922429c770
Gustavo A. R. Silva <silvagustavosilva@gmail.com>
2017-01-11 20:29:35 -0600

    power: supply: ab8500_btemp: Compress return logic into one line.
    Simplify return logic to avoid unnecessary variable assignments.
    These issues were detected using Coccinelle and the following semantic patch:
    
    @@
    local idexpression ret;
    expression e;
    @@
    
    -ret =
    +return
         e;
    -return ret;
    
    Signed-off-by: Gustavo A. R. Silva <garsilva@embeddedor.com>
    Signed-off-by: Sebastian Reichel <sre@kernel.org>

--- drivers/power/supply/ab8500_btemp.c
+++ drivers/power/supply/ab8500_btemp.c
@@ -124,10 +124,7 @@ static LIST_HEAD(ab8500_btemp_list);
  */
 struct ab8500_btemp *ab8500_btemp_get(void)
 {
-	struct ab8500_btemp *btemp;
-	btemp = list_first_entry(&ab8500_btemp_list, struct ab8500_btemp, node);
-
-	return btemp;
+	return list_first_entry(&ab8500_btemp_list, struct ab8500_btemp, node);
 }
 EXPORT_SYMBOL(ab8500_btemp_get);
 
@@ -471,7 +468,7 @@ static int ab8500_btemp_get_batctrl_res(struct ab8500_btemp *di)
 static int ab8500_btemp_res_to_temp(struct ab8500_btemp *di,
 	const struct abx500_res_to_temp *tbl, int tbl_size, int res)
 {
-	int i, temp;
+	int i;
 	/*
 	 * Calculate the formula for the straight line
 	 * Simple interpolation if we are within
@@ -489,9 +486,8 @@ static int ab8500_btemp_res_to_temp(struct ab8500_btemp *di,
 			i++;
 	}
 
-	temp = tbl[i].temp + ((tbl[i + 1].temp - tbl[i].temp) *
+	return tbl[i].temp + ((tbl[i + 1].temp - tbl[i].temp) *
 		(res - tbl[i].resist)) / (tbl[i + 1].resist - tbl[i].resist);
-	return temp;
 }
 
 /**
