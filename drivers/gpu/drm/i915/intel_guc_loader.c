ba93337788d395cc96cc521656ae22c5823f9d2f
Michal Wajdeczko <michal.wajdeczko@intel.com>
2017-01-13 17:41:57 +0000

    drm/i915/guc: Move GuC log related functions into dedicated file
    Functions supporting GuC logging capabilities were spread across
    many files, with unnecessary exposures and mixed with unrelated
    code. Dedicate file will make maintenance of all GuC functions
    easier as more functions are coming to support GuC submissions.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170113174157.104492-1-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/intel_guc_loader.c
+++ drivers/gpu/drm/i915/intel_guc_loader.c
@@ -52,12 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 512K. In order to exclude 0-512K address space from GGTT, all gfx objects
  * used by GuC is pinned with PIN_OFFSET_BIAS along with size of WOPCM.
  *
- * Firmware log:
- * Firmware log is enabled by setting i915.guc_log_level to non-negative level.
- * Log data is printed out via reading debugfs i915_guc_log_dump. Reading from
- * i915_guc_load_status will print out firmware loading status and scratch
- * registers value.
- *
  */
 
 #define SKL_FW_MAJOR 6
