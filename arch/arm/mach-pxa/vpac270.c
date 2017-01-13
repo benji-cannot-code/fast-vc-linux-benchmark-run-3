332fae325a91d2ec1dd8d4f069d4c1321e8ef8e6
Ulf Hansson <ulf.hansson@linaro.org>
2017-01-13 14:14:02 +0100

    ARM: pxa: Don't rely on public mmc header to include leds.h
    Some of the pxa platforms, balloon3, colibri-pxa270-income, corgi,
    trizeps4, vpac270, zeus and zylonite depends on leds.h. Explicitly include
    it instead of relying on the public mmc header host.h.
    
    Cc: Daniel Mack <daniel@zonque.org>
    Cc: Haojian Zhuang <haojian.zhuang@gmail.com>
    Cc: Robert Jarzmik <robert.jarzmik@free.fr>
    Cc: <linux-arm-kernel@lists.infradead.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Acked-by: Robert Jarzmik <robert.jarzmik@free.fr>

--- arch/arm/mach-pxa/vpac270.c
+++ arch/arm/mach-pxa/vpac270.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
+#include <linux/leds.h>
 #include <linux/gpio.h>
 #include <linux/usb/gpio_vbus.h>
 #include <linux/mtd/mtd.h>
