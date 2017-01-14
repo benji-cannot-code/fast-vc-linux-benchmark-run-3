e94671c5f6bec93b86c10214c878f8817992a726
Markus Elfring <elfring@users.sourceforge.net>
2017-01-14 12:36:59 +0100

    selinux: Adjust four checks for null pointers
    The script "checkpatch.pl" pointed information out like the following.
    
    Comparison to NULL could be written !…
    
    Thus fix affected source code places.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- security/selinux/ss/hashtab.c
+++ security/selinux/ss/hashtab.c
@@ -18,7 +18,7 @@ struct hashtab *hashtab_create(u32 (*hash_value)(struct hashtab *h, const void *
 	u32 i;
 
 	p = kzalloc(sizeof(*p), GFP_KERNEL);
-	if (p == NULL)
+	if (!p)
 		return p;
 
 	p->size = size;
@@ -26,7 +26,7 @@ struct hashtab *hashtab_create(u32 (*hash_value)(struct hashtab *h, const void *
 	p->hash_value = hash_value;
 	p->keycmp = keycmp;
 	p->htable = kmalloc_array(size, sizeof(*p->htable), GFP_KERNEL);
-	if (p->htable == NULL) {
+	if (!p->htable) {
 		kfree(p);
 		return NULL;
 	}
@@ -59,7 +59,7 @@ int hashtab_insert(struct hashtab *h, void *key, void *datum)
 		return -EEXIST;
 
 	newnode = kzalloc(sizeof(*newnode), GFP_KERNEL);
-	if (newnode == NULL)
+	if (!newnode)
 		return -ENOMEM;
 	newnode->key = key;
 	newnode->datum = datum;
@@ -88,7 +88,7 @@ void *hashtab_search(struct hashtab *h, const void *key)
 	while (cur && h->keycmp(h, key, cur->key) > 0)
 		cur = cur->next;
 
-	if (cur == NULL || (h->keycmp(h, key, cur->key) != 0))
+	if (!cur || (h->keycmp(h, key, cur->key) != 0))
 		return NULL;
 
 	return cur->datum;
