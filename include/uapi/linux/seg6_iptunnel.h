e6b8a73ec7349171bad4d0e9206571773031636b
David Lebrun <david.lebrun@uclouvain.be>
2017-01-15 15:26:16 +0100

    ipv6: sr: add missing Kbuild export for header files
    Add missing IPv6-SR header files in include/uapi/linux/Kbuild.
    
    Also, prevent seg6_lwt_headroom() from being exported and add
    missing linux/types.h include.
    
    Signed-off-by: David Lebrun <david.lebrun@uclouvain.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/seg6_iptunnel.h
+++ include/uapi/linux/seg6_iptunnel.h
@@ -34,6 +34,8 @@ enum {
 	SEG6_IPTUN_MODE_ENCAP,
 };
 
+#ifdef __KERNEL__
+
 static inline size_t seg6_lwt_headroom(struct seg6_iptunnel_encap *tuninfo)
 {
 	int encap = (tuninfo->mode == SEG6_IPTUN_MODE_ENCAP);
@@ -41,5 +43,7 @@ static inline size_t seg6_lwt_headroom(struct seg6_iptunnel_encap *tuninfo)
 	return ((tuninfo->srh->hdrlen + 1) << 3) +
 	       (encap * sizeof(struct ipv6hdr));
 }
+
+#endif
 
 #endif
