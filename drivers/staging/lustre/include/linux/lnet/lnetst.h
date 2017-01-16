60f2c6bc7b1b084c3c3b107f24c4f8ebf2b6bc07
James Simmons <jsimmons@infradead.org>
2017-01-16 16:30:09 -0500

    staging: lustre: lnet: change lstcon_batch_ent_t to proper structure
    Change lstcon_batch_ent_t from typedef to proper structure.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6142
    Reviewed-on: https://review.whamcloud.com/24188
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/lnet/lnetst.h
+++ drivers/staging/lustre/include/linux/lnet/lnetst.h
@@ -106,11 +106,11 @@ struct lstcon_test_ent {
 };				/*** test summary entry, for
 				 *** list_batch command */
 
-typedef struct {
+struct lstcon_batch_ent {
 	int	bae_state;	/* batch status */
 	int	bae_timeout;	/* batch timeout */
 	int	bae_ntest;	/* # of tests in the batch */
-} lstcon_batch_ent_t;		/*** batch summary entry, for
+};				/*** batch summary entry, for
 				 *** list_batch command */
 
 typedef struct {
@@ -120,7 +120,7 @@ typedef struct {
 						 * entry */
 	union {
 		struct lstcon_test_ent	tbe_test; /* test entry */
-		lstcon_batch_ent_t tbe_batch;	/* batch entry */
+		struct lstcon_batch_ent tbe_batch;/* batch entry */
 	} u;
 } lstcon_test_batch_ent_t;	/*** test/batch verbose information entry,
 				 *** for list_batch command */
