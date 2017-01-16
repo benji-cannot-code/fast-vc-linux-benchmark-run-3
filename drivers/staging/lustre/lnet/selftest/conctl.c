d5972eed8d29598054e6f3bd00d5edee7060c96e
James Simmons <jsimmons@infradead.org>
2017-01-16 16:30:20 -0500

    staging: lustre: lnet: change lstio_group_nodes_args_t to proper structure
    Change lstio_group_nodes_args_t from typedef to proper structure.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6142
    Reviewed-on: https://review.whamcloud.com/24188
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/selftest/conctl.c
+++ drivers/staging/lustre/lnet/selftest/conctl.c
@@ -314,7 +314,7 @@ lst_group_update_ioctl(struct lstio_group_update_args *args)
 }
 
 static int
-lst_nodes_add_ioctl(lstio_group_nodes_args_t *args)
+lst_nodes_add_ioctl(struct lstio_group_nodes_args *args)
 {
 	unsigned int feats;
 	int rc;
@@ -877,7 +877,7 @@ lstcon_ioctl_entry(unsigned int cmd, struct libcfs_ioctl_hdr *hdr)
 		rc = lst_group_update_ioctl((struct lstio_group_update_args *)buf);
 		break;
 	case LSTIO_NODES_ADD:
-		rc = lst_nodes_add_ioctl((lstio_group_nodes_args_t *)buf);
+		rc = lst_nodes_add_ioctl((struct lstio_group_nodes_args *)buf);
 		break;
 	case LSTIO_GROUP_LIST:
 		rc = lst_group_list_ioctl((lstio_group_list_args_t *)buf);
