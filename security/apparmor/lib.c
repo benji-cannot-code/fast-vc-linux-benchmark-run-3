2ce04a4b1481dffc90fcc250e6f014ede94df759
John Johansen <john.johansen@canonical.com>
2017-01-16 00:42:13 -0800

    apparmor: move lib definitions into separate lib include
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/lib.c
+++ security/apparmor/lib.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "include/audit.h"
 #include "include/apparmor.h"
-
+#include "include/lib.h"
 
 /**
  * aa_split_fqname - split a fqname into a profile and namespace name
--- security/apparmor/match.c
+++ security/apparmor/match.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/kref.h>
 
-#include "include/apparmor.h"
+#include "include/lib.h"
 #include "include/match.h"
 
 #define base_idx(X) ((X) & 0xffffff)
