6404a9bc1caae0c4529c200cc9d4993752bbdbea
John Johansen <john.johansen@canonical.com>
2017-01-16 00:42:15 -0800

    apparmor: split apparmor policy namespaces code into its own file
    Policy namespaces will be diverging from profile management and
    expanding so put it in its own file.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/audit.c
+++ security/apparmor/audit.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "include/apparmor.h"
 #include "include/audit.h"
 #include "include/policy.h"
+#include "include/policy_ns.h"
 
 const char *const op_table[] = {
 	"null",
--- security/apparmor/domain.c
+++ security/apparmor/domain.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "include/match.h"
 #include "include/path.h"
 #include "include/policy.h"
+#include "include/policy_ns.h"
 
 /**
  * aa_free_domain_entries - free entries in a domain table
