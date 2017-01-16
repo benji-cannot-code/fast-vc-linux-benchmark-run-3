67955b8f8906eb018e7cc055521c1494e3630b04
James Simmons <jsimmons@infradead.org>
2017-01-16 16:30:27 -0500

    staging: lustre: lnet: change lstio_batch_query_args_t to proper structure
    Change lstio_batch_query_args_t from typedef to proper structure.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6142
    Reviewed-on: https://review.whamcloud.com/24188
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/lnet/lnetst.h
+++ drivers/staging/lustre/include/linux/lnet/lnetst.h
@@ -387,7 +387,7 @@ struct lstio_batch_stop_args {
 								result buffer */
 };
 
-typedef struct {
+struct lstio_batch_query_args {
 	int			 lstio_bat_key;		/* IN: session key */
 	int			 lstio_bat_testidx;	/* IN: test index */
 	int			 lstio_bat_client;	/* IN: we testing
@@ -398,7 +398,7 @@ typedef struct {
 	char __user		*lstio_bat_namep;	/* IN: batch name */
 	struct list_head __user	*lstio_bat_resultp;	/* OUT: list head of
 								result buffer */
-} lstio_batch_query_args_t;
+};
 
 typedef struct {
 	int	 lstio_bat_key;		/* IN: session key */
