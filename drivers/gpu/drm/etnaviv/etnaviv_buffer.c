a3f082e8016884bd2018690bcc4b155300cba11c
Lucas Stach <l.stach@pengutronix.de>
2017-01-16 16:09:51 +0100

    drm/etnaviv: move cmdbuf de-/allocation into own file
    This will get more complex with the following changes, so move it
    into its own place.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>

--- drivers/gpu/drm/etnaviv/etnaviv_buffer.c
+++ drivers/gpu/drm/etnaviv/etnaviv_buffer.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "etnaviv_cmdbuf.h"
 #include "etnaviv_gpu.h"
 #include "etnaviv_gem.h"
 #include "etnaviv_mmu.h"
