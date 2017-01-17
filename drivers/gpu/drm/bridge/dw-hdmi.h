5026e3bad55154edd002e7c7185973417972003b
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2017-01-17 10:29:03 +0200

    drm: bridge: dw-hdmi: Reject invalid product IDs
    The DWC HDMI TX can be recognized by the two product identification
    registers. If the registers don't read as expect the IP will be very
    different than what the driver has been designed for, or will be
    misconfigured in a way that makes it non-operational (invalid memory
    address, incorrect clocks, ...). We should reject this situation with an
    error.
    
    While this isn't critical for proper operation with supported IPs at the
    moment, the driver will soon gain automatic device-specific handling
    based on runtime device identification. This change makes it easier to
    implement that without having to default to a random guess in case the
    device can't be identified.
    
    While at it print a readable version number in the device identification
    message instead of raw register values.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Jose Abreu <joabreu@synopsys.com>
    Signed-off-by: Archit Taneja <architt@codeaurora.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170117082910.27023-14-laurent.pinchart+renesas@ideasonboard.com

--- drivers/gpu/drm/bridge/dw-hdmi.h
+++ drivers/gpu/drm/bridge/dw-hdmi.h
@@ -546,6 +546,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HDMI_I2CM_FS_SCL_LCNT_0_ADDR            0x7E12
 
 enum {
+/* PRODUCT_ID0 field values */
+	HDMI_PRODUCT_ID0_HDMI_TX = 0xa0,
+
+/* PRODUCT_ID1 field values */
+	HDMI_PRODUCT_ID1_HDCP = 0xc0,
+	HDMI_PRODUCT_ID1_HDMI_RX = 0x02,
+	HDMI_PRODUCT_ID1_HDMI_TX = 0x01,
+
 /* CONFIG0_ID field values */
 	HDMI_CONFIG0_I2S = 0x10,
 
