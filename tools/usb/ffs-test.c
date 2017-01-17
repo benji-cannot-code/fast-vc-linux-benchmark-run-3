f90f7bed0e8a69e75c1a96002bc526246ddcdbed
Felipe Balbi <felipe.balbi@linux.intel.com>
2017-01-17 15:04:07 +0200

    tools: usb: ffs-test: switch to _DEFAULT_SOURCE
    _BSD_SOURCE is deprecated and gives a build warning. Let's use
    _DEFAULT_SOURCE instead.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- tools/usb/ffs-test.c
+++ tools/usb/ffs-test.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* $(CROSS_COMPILE)cc -Wall -Wextra -g -o ffs-test ffs-test.c -lpthread */
 
 
-#define _BSD_SOURCE /* for endian.h */
+#define _DEFAULT_SOURCE /* for endian.h */
 
 #include <endian.h>
 #include <errno.h>
