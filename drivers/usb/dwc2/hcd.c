4f2af79d3abeb72e7832335f89096c4a8729ab2d
John Youn <John.Youn@synopsys.com>
2017-01-17 20:31:58 -0800

    usb: dwc2: Fix lines over 80 characters
    Fix lines over 80 characters.
    
    Signed-off-by: John Youn <johnyoun@synopsys.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc2/hcd.c
+++ drivers/usb/dwc2/hcd.c
@@ -4237,7 +4237,7 @@ void dwc2_host_complete(struct dwc2_hsotg *hsotg, struct dwc2_qtd *qtd,
 
 		if (ep)
 			dwc2_free_bus_bandwidth(dwc2_hsotg_to_hcd(hsotg),
-						dwc2_hcd_get_ep_bandwidth(hsotg, ep),
+					dwc2_hcd_get_ep_bandwidth(hsotg, ep),
 					urb);
 	}
 
@@ -4683,7 +4683,7 @@ static int _dwc2_hcd_urb_enqueue(struct usb_hcd *hcd, struct urb *urb,
 
 	if (alloc_bandwidth) {
 		dwc2_allocate_bus_bandwidth(hcd,
-					    dwc2_hcd_get_ep_bandwidth(hsotg, ep),
+				dwc2_hcd_get_ep_bandwidth(hsotg, ep),
 				urb);
 	}
 
