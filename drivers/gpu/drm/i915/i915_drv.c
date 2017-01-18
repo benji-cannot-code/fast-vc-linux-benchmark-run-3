c0d0eaef0d29d7014e7f4873aae9b51432b61ca2
Anusha Srivatsa <anusha.srivatsa@intel.com>
2017-01-18 08:05:58 -0800

    drm/i915/get_params: Add HuC status to getparams
    This patch will allow for getparams to return the status of the HuC.
    As the HuC has to be validated by the GuC this patch uses the validated
    status to show when the HuC is loaded and ready for use. You cannot use
    the loaded status as with the GuC as the HuC is verified after it is
    loaded and is not usable until it is verified.
    
    v2: removed the forewakes as the registers are already force-woken.
         (T.Ursulin)
    v3: rebased on top of drm-tip. Removed any reference to intel_huc.h
    v4: rebased. Rename I915_PARAM_HAS_HUC to I915_PARAM_HUC_STATUS.
    Remove intel_is_huc_valid() since it is used only in one place.
    Put the case of I915_PARAM_HAS_HUC() in the right place.
    v5: rebased. Add a comment to specify that I915_READ(reg)
    does not read garbage value. The register HUC_STATUS2 is force
    woken and no rpm is needed.
    
    Signed-off-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Signed-off-by: Peter Antoine <peter.antoine@intel.com>
    Reviewed-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1484755558-1234-6-git-send-email-anusha.srivatsa@intel.com

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_trace.h"
 #include "i915_vgpu.h"
 #include "intel_drv.h"
+#include "intel_uc.h"
 
 static struct drm_driver driver;
 
@@ -315,6 +316,12 @@ static int i915_getparam(struct drm_device *dev, void *data,
 		break;
 	case I915_PARAM_MIN_EU_IN_POOL:
 		value = INTEL_INFO(dev_priv)->sseu.min_eu_in_pool;
+		break;
+	case I915_PARAM_HUC_STATUS:
+		/* The register is already force-woken. We dont need
+		 * any rpm here
+		 */
+		value = I915_READ(HUC_STATUS2) & HUC_FW_VERIFIED;
 		break;
 	case I915_PARAM_MMAP_GTT_VERSION:
 		/* Though we've started our numbering from 1, and so class all
