39042349ab6b3aafba071f40ee1eec4ab753241f
K. Y. Srinivasan <kys@microsoft.com>
2017-01-19 11:51:50 -0700

    Drivers: hv: vmbus: Move the code to signal end of message
    As part of the effort to separate out architecture specific code, move the
    code for signaling end of message.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hv/channel_mgmt.c
+++ drivers/hv/channel_mgmt.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/completion.h>
 #include <linux/delay.h>
 #include <linux/hyperv.h>
+#include <asm/mshyperv.h>
 
 #include "hyperv_vmbus.h"
 
