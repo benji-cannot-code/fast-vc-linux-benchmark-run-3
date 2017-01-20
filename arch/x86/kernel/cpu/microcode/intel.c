40ee267d7ab263feb10989e2d2786d5a70b73b23
Borislav Petkov <bp@suse.de>
2017-01-20 21:29:40 +0100

    x86/microcode/intel: Drop stashed AP patch pointer optimization
    This was meant to save us the scanning of the microcode containter in
    the initrd since the first AP had already done that but it can also hurt
    us:
    
    Imagine a single hyperthreaded CPU (Intel(R) Atom(TM) CPU N270, for
    example) which updates the microcode on the BSP but since the microcode
    engine is shared between the two threads, the update on CPU1 doesn't
    happen because it has already happened on CPU0 and we don't find a newer
    microcode revision on CPU1.
    
    Which doesn't set the intel_ucode_patch pointer and at initrd
    jettisoning time we don't save the microcode patch for later
    application.
    
    Now, when we suspend to RAM, the loaded microcode gets cleared so we
    need to reload but there's no patch saved in the cache.
    
    Removing the optimization fixes this issue and all is fine and dandy.
    
    Fixes: 06b8534cb728 ("x86/microcode: Rework microcode loading")
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20170120202955.4091-2-bp@alien8.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/cpu/microcode/intel.c
+++ arch/x86/kernel/cpu/microcode/intel.c
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static const char ucode_path[] = "kernel/x86/microcode/GenuineIntel.bin";
 
-/* Current microcode patch used in early patching */
+/* Current microcode patch used in early patching on the APs. */
 struct microcode_intel *intel_ucode_patch;
 
 static inline bool cpu_signatures_match(unsigned int s1, unsigned int p1,
@@ -608,12 +608,6 @@ int __init save_microcode_in_initrd_intel(void)
 	struct ucode_cpu_info uci;
 	struct cpio_data cp;
 
-	/*
-	 * AP loading didn't find any microcode patch, no need to save anything.
-	 */
-	if (!intel_ucode_patch || IS_ERR(intel_ucode_patch))
-		return 0;
-
 	if (!load_builtin_intel_microcode(&cp))
 		cp = find_microcode_in_initrd(ucode_path, false);
 
@@ -628,7 +622,6 @@ int __init save_microcode_in_initrd_intel(void)
 
 	return 0;
 }
-
 
 /*
  * @res_patch, output: a pointer to the patch we found.
