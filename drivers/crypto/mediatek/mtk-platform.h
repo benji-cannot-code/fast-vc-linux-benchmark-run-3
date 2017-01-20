ef2268effb6a5a5a18319c34725ab616852daa42
Ryder Lee <ryder.lee@mediatek.com>
2017-01-20 13:41:15 +0800

    crypto: mediatek - add support to GCM mode
    This patch adds support to the GCM mode.
    
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/mediatek/mtk-platform.h
+++ drivers/crypto/mediatek/mtk-platform.h
@@ -14,8 +14,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __MTK_PLATFORM_H_
 
 #include <crypto/algapi.h>
+#include <crypto/internal/aead.h>
 #include <crypto/internal/hash.h>
 #include <crypto/scatterwalk.h>
+#include <crypto/skcipher.h>
 #include <linux/crypto.h>
 #include <linux/dma-mapping.h>
 #include <linux/interrupt.h>
