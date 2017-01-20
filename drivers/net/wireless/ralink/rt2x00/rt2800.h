27dc1f9002ecb66de98155ff8cb59042132cc8f7
Serge Vasilugin <vasilugin@yandex.ru>
2017-01-20 14:28:26 +0100

    rt2x00: add support for RT5350 WiSoC
    Support for the RT5350 WiSoC was added to OpenWrt after having a
    lengthy debate about the legality of the original submission, see
    https://lists.openwrt.org/pipermail/openwrt-devel/2013-January/018224.html
    MTK/Ralink Acked replied and says we can merge this patch under the GPL.
    https://dev.openwrt.org/changeset/36177
    
    Signed-off-by: Serge Vasilugin <vasilugin@yandex.ru>
    Tested-by: Michel Stempin <michel.stempin@wanadoo.fr>
    Acked-by: John Crispin <blogic@openwrt.org>
    [daniel@makrotopia.org: added commit message, cleaned up code]
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ralink/rt2x00/rt2800.h
+++ drivers/net/wireless/ralink/rt2x00/rt2800.h
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RF5592				0x000f
 #define RF3070				0x3070
 #define RF3290				0x3290
+#define RF5350				0x5350
 #define RF5360				0x5360
 #define RF5362				0x5362
 #define RF5370				0x5370
