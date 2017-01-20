9dccd96711810946fad51c0a946f0fcd17ca5029
Tang Yuantian <Yuantian.Tang@nxp.com>
2017-01-20 14:59:35 +0800

    ahci: qoriq: report error when ecc register address is missing in dts
    For ls1021a, and armv8 chasis 2 socs, sata ecc must be disabled.
    If ecc register is not found in sata node in dts, report error.
    
    This is a chip erratum described as bellow:
    The Read DMA operations get early termination indication from the
    controller. This issue is observed as CRC error in the status registers.
    The issue is due to address collision at address 0 in the dual port
    memory. The read is a dummy read to flush out the header, but due to
    collision the controller logs the mbit error reported by the ECC check
    logic. This results in the early termination of the Read DMA operation
    by the controller. The issue happens to all the interface
    speeds(GEN1/2/3) for all the products.
    
    Workaround:
    Disable ECC feature on those platforms.
    
    Signed-off-by: Tang Yuantian <yuantian.tang@nxp.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/ahci_qoriq.c
+++ drivers/ata/ahci_qoriq.c
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LS1021A_AXICC_ADDR	0xC0
 
 #define SATA_ECC_DISABLE	0x00020000
-#define LS1046A_SATA_ECC_DIS	0x80000000
+#define ECC_DIS_ARMV8_CH2	0x80000000
 
 enum ahci_qoriq_type {
 	AHCI_LS1021A,
@@ -159,6 +159,8 @@ static int ahci_qoriq_phy_init(struct ahci_host_priv *hpriv)
 
 	switch (qpriv->type) {
 	case AHCI_LS1021A:
+		if (!qpriv->ecc_addr)
+			return -EINVAL;
 		writel(SATA_ECC_DISABLE, qpriv->ecc_addr);
 		writel(AHCI_PORT_PHY_1_CFG, reg_base + PORT_PHY1);
 		writel(LS1021A_PORT_PHY2, reg_base + PORT_PHY2);
@@ -172,6 +174,9 @@ static int ahci_qoriq_phy_init(struct ahci_host_priv *hpriv)
 		break;
 
 	case AHCI_LS1043A:
+		if (!qpriv->ecc_addr)
+			return -EINVAL;
+		writel(ECC_DIS_ARMV8_CH2, qpriv->ecc_addr);
 		writel(AHCI_PORT_PHY_1_CFG, reg_base + PORT_PHY1);
 		writel(AHCI_PORT_TRANS_CFG, reg_base + PORT_TRANS);
 		if (qpriv->is_dmacoherent)
@@ -186,7 +191,9 @@ static int ahci_qoriq_phy_init(struct ahci_host_priv *hpriv)
 		break;
 
 	case AHCI_LS1046A:
-		writel(LS1046A_SATA_ECC_DIS, qpriv->ecc_addr);
+		if (!qpriv->ecc_addr)
+			return -EINVAL;
+		writel(ECC_DIS_ARMV8_CH2, qpriv->ecc_addr);
 		writel(AHCI_PORT_PHY_1_CFG, reg_base + PORT_PHY1);
 		writel(AHCI_PORT_TRANS_CFG, reg_base + PORT_TRANS);
 		if (qpriv->is_dmacoherent)
