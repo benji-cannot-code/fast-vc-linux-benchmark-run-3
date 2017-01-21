fe99d0d498314faef61dc0dfb610df5841af1b36
Chris Wilson <chris@chris-wilson.co.uk>
2017-01-21 18:19:44 +0000

    drm: Update drm_cache.c to pull in the new drm_cache.h
    The prototypes were moved to a new header, but the function definitions
    were not updated to pull in the declarations.
    
    drivers/gpu/drm/drm_cache.c:79:1: warning: no previous prototype for ‘drm_clflush_pages’ [-Wmissing-prototypes]
    drivers/gpu/drm/drm_cache.c:120:1: warning: no previous prototype for ‘drm_clflush_sg’ [-Wmissing-prototypes]
    drivers/gpu/drm/drm_cache.c:152:1: warning: no previous prototype for ‘drm_clflush_virt_range’ [-Wmissing-prototypes]
    
    Fixes: f9a87bd7d5b6 ("drm: Move drm_clflush prototypes to drm_cache header file")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Gabriel Krisman Bertazi <krisman@collabora.co.uk>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Gabriel Krisman Bertazi <krisman@collabora.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170121181944.24672-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/drm_cache.c
+++ drivers/gpu/drm/drm_cache.c
@@ -30,7 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/export.h>
-#include <drm/drmP.h>
+#include <linux/highmem.h>
+
+#include <drm/drm_cache.h>
 
 #if defined(CONFIG_X86)
 #include <asm/smp.h>
