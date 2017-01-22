bcd9b231f2649ac2e9fc1f02936984a686b5405d
Nicolas Iooss <nicolas.iooss_linux@m4x.org>
2017-01-22 14:47:39 +0100

    drm/amd/powerplay: fix misspelling in header guard
    In smu7_clockpowergating.h, the #ifndef statement which prevents
    multiple inclusions of the header file uses _SMU7_CLOCK_POWER_GATING_H_
    but the following #define statement uses _SMU7_CLOCK__POWER_GATING_H_.
    
    Signed-off-by: Nicolas Iooss <nicolas.iooss_linux@m4x.org>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/smu7_clockpowergating.h
+++ drivers/gpu/drm/amd/powerplay/hwmgr/smu7_clockpowergating.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #ifndef _SMU7_CLOCK_POWER_GATING_H_
-#define _SMU7_CLOCK__POWER_GATING_H_
+#define _SMU7_CLOCK_POWER_GATING_H_
 
 #include "smu7_hwmgr.h"
 #include "pp_asicblocks.h"
