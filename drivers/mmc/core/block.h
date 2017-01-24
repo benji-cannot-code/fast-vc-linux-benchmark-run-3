2e63401f86f5d6fb6c957747b2c37ee14b64470e
Linus Walleij <linus.walleij@linaro.org>
2017-01-24 11:17:57 +0100

    mmc: block: stop passing around pointless return values
    The mmc_blk_issue_rq() function is called in exactly one place
    in queue.c and there the return value is ignored. So the
    functions called from that function that also meticulously
    return 0/1 do so for no good reason.
    
    Error reporting on the asynchronous requests are done upward to
    the block layer when the requests are eventually completed or
    fail, which may happen during the flow of the mmc_blk_issue_*
    functions directly (for "special commands") or later, when an
    asynchronous read/write request is completed.
    
    The issuing functions do not give rise to errors on their own,
    and there is nothing to return back to the caller in queue.c.
    Drop all return values and make the function return void.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/block.h
+++ drivers/mmc/core/block.h
@@ -5,6 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct mmc_queue;
 struct request;
 
-int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req);
+void mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req);
 
 #endif
