f30c2f85cecafdd2a55d5369b44a1f8fad0a9cfa
Jerome Anand <jerome.anand@intel.com>
2017-01-25 04:27:50 +0530

    drm/i915: Add support for audio driver notifications
    Notifiations like mode change, hot plug and edid to
    the audio driver are added. This is inturn used by the
    audio driver for its functionality.
    
    A new interface file capturing the notifications needed by the
    audio driver is added
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Jerome Anand <jerome.anand@intel.com>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- drivers/gpu/drm/i915/intel_hdmi.c
+++ drivers/gpu/drm/i915/intel_hdmi.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_edid.h>
 #include "intel_drv.h"
 #include <drm/i915_drm.h>
+#include <drm/intel_lpe_audio.h>
 #include "i915_drv.h"
 
 static struct drm_device *intel_hdmi_to_dev(struct intel_hdmi *intel_hdmi)
