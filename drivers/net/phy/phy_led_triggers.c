d24bef387e66d4acfcd031186940d7970bbb1c56
Geert Uytterhoeven <geert+renesas@glider.be>
2017-01-25 11:39:49 +0100

    net: phy: leds: Break dependency of phy.h on phy_led_triggers.h
    <linux/phy.h> includes <linux/phy_led_triggers.h>, which is not really
    needed.  Drop the include from <linux/phy.h>, and add it to all users
    that didn't include it explicitly.
    
    Suggested-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/phy_led_triggers.c
+++ drivers/net/phy/phy_led_triggers.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/leds.h>
 #include <linux/phy.h>
+#include <linux/phy_led_triggers.h>
 #include <linux/netdevice.h>
 
 static struct phy_led_trigger *phy_speed_to_led_trigger(struct phy_device *phy,
