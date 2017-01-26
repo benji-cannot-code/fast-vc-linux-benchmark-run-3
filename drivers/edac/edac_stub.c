b7b005f573e21349335c4ccd20069aa1b3cc3378
Borislav Petkov <bp@suse.de>
2017-01-26 16:49:59 +0100

    EDAC: Get rid of edac_handlers
    Use mc_devices list instead to check whether we have EDAC driver
    instances successfully registered with EDAC core.
    
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/edac_stub.c
+++ drivers/edac/edac_stub.c
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int edac_op_state = EDAC_OPSTATE_INVAL;
 EXPORT_SYMBOL_GPL(edac_op_state);
 
-atomic_t edac_handlers = ATOMIC_INIT(0);
-EXPORT_SYMBOL_GPL(edac_handlers);
-
 int edac_err_assert = 0;
 EXPORT_SYMBOL_GPL(edac_err_assert);
 
