3857311b226527a90bb3cd9137b399b96f91e6ea
Vijaya Kumar K <Vijaya.Kumar@cavium.com>
2017-01-26 19:50:50 +0530

    KVM: arm/arm64: vgic: Introduce VENG0 and VENG1 fields to vmcr struct
    ICC_VMCR_EL2 supports virtual access to ICC_IGRPEN1_EL1.Enable
    and ICC_IGRPEN0_EL1.Enable fields. Add grpen0 and grpen1 member
    variables to struct vmcr to support read and write of these fields.
    
    Also refactor vgic_set_vmcr and vgic_get_vmcr() code.
    Drop ICH_VMCR_CTLR_SHIFT and ICH_VMCR_CTLR_MASK macros and instead
    use ICH_VMCR_EOI* and ICH_VMCR_CBPR* macros.
    
    Signed-off-by: Vijaya Kumar K <Vijaya.Kumar@cavium.com>
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -405,8 +405,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ICH_HCR_EN			(1 << 0)
 #define ICH_HCR_UIE			(1 << 1)
 
-#define ICH_VMCR_CTLR_SHIFT		0
-#define ICH_VMCR_CTLR_MASK		(0x21f << ICH_VMCR_CTLR_SHIFT)
 #define ICH_VMCR_CBPR_SHIFT		4
 #define ICH_VMCR_CBPR_MASK		(1 << ICH_VMCR_CBPR_SHIFT)
 #define ICH_VMCR_EOIM_SHIFT		9
