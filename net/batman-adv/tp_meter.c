f0744716311bb95e65e2a814505e897c461794ba
Sven Eckelmann <sven@narfation.org>
2017-01-28 10:23:30 +0100

    batman-adv: Fix includes for IS_ERR/ERR_PTR
    IS_ERR/ERR_PTR are not defined in linux/device.h but in linux/err.h. The
    files using these macros therefore have to include the correct one.
    
    Reported-by: Linus Luessing <linus.luessing@web.de>
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/tp_meter.c
+++ net/batman-adv/tp_meter.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/byteorder/generic.h>
 #include <linux/cache.h>
 #include <linux/compiler.h>
-#include <linux/device.h>
+#include <linux/err.h>
 #include <linux/etherdevice.h>
 #include <linux/fs.h>
 #include <linux/if_ether.h>
