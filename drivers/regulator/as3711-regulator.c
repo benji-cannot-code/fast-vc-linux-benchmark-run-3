53635337997e651fde4f3f6cf8c3a886acc6f154
Bhumika Goyal <bhumirks@gmail.com>
2017-01-28 19:25:00 +0530

    regulator: as3711-regulator: constify regulator_ops structure
    Declare regulator_ops structure as const as it is only stored in the ops
    field of a regulator_desc structure. This field is of type const, so
    regulator_ops structures having this property can be made const too.
    
    File size before: drivers/regulator/as3711-regulator.o
       text	   data	    bss	    dec	    hex	filename
       1517	   4144	      0	   5661	   161d regulator/as3711-regulator.o
    
    File size after: drivers/regulator/as3711-regulator.o
       text	   data	    bss	    dec	    hex	filename
       2301	   3376	      0	   5677	   162d regulator/as3711-regulator.o
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/as3711-regulator.c
+++ drivers/regulator/as3711-regulator.c
@@ -83,7 +83,7 @@ static unsigned int as3711_get_mode_sd(struct regulator_dev *rdev)
 	return -EINVAL;
 }
 
-static struct regulator_ops as3711_sd_ops = {
+static const struct regulator_ops as3711_sd_ops = {
 	.is_enabled		= regulator_is_enabled_regmap,
 	.enable			= regulator_enable_regmap,
 	.disable		= regulator_disable_regmap,
@@ -95,7 +95,7 @@ static struct regulator_ops as3711_sd_ops = {
 	.set_mode		= as3711_set_mode_sd,
 };
 
-static struct regulator_ops as3711_aldo_ops = {
+static const struct regulator_ops as3711_aldo_ops = {
 	.is_enabled		= regulator_is_enabled_regmap,
 	.enable			= regulator_enable_regmap,
 	.disable		= regulator_disable_regmap,
@@ -105,7 +105,7 @@ static struct regulator_ops as3711_aldo_ops = {
 	.map_voltage		= regulator_map_voltage_linear_range,
 };
 
-static struct regulator_ops as3711_dldo_ops = {
+static const struct regulator_ops as3711_dldo_ops = {
 	.is_enabled		= regulator_is_enabled_regmap,
 	.enable			= regulator_enable_regmap,
 	.disable		= regulator_disable_regmap,
