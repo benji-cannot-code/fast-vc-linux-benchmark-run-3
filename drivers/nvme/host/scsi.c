dfa9223c833a2e8363a0d4cd54a858634fd1ed0b
Jens Axboe <axboe@fb.com>
2017-01-29 20:04:49 -0700

    nvme: fix compilation of scsi component
    Since we moved the cdb parts and define out of the block proper,
    we need to include scsi/scsi_request.h for the nvme scsi layer.
    
    Fixes: 82ed4db499b8 ("block: split scsi_request out of struct request")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/nvme/host/scsi.c
+++ drivers/nvme/host/scsi.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/unaligned.h>
 #include <scsi/sg.h>
 #include <scsi/scsi.h>
+#include <scsi/scsi_request.h>
 
 #include "nvme.h"
 
