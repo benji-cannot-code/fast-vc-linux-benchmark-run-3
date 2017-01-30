48cac43ffb2c3e194b554f2eafa8b36401ed5a84
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2017-01-30 18:11:56 +1100

    powerpc: Add support for non-PCI ISA bridges
    The POWER9 chip supports an LPC bus that isn't hanging
    off a PCI bus, so let's add support for that, mapping it
    to the reserved space at ISA_IO_BASE
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/isa-bridge.h
+++ arch/powerpc/include/asm/isa-bridge.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_PPC64
 
 extern void isa_bridge_find_early(struct pci_controller *hose);
+extern void isa_bridge_init_non_pci(struct device_node *np);
 
 static inline int isa_vaddr_is_ioport(void __iomem *address)
 {
