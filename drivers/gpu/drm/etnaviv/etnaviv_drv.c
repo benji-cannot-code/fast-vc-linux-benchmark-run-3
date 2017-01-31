fe34d93dcfedf82796c140f182f704191a966ec7
Liviu Dudau <liviu@dudau.co.uk>
2017-01-31 18:56:20 +0000

    drm/etnaviv: Remove duplicate header file include
    etnaviv_gem.h header gets included twice. Remove duplicate.
    
    Signed-off-by: Liviu Dudau <liviu@dudau.co.uk>
    Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/gpu/drm/etnaviv/etnaviv_drv.c
+++ drivers/gpu/drm/etnaviv/etnaviv_drv.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "etnaviv_gpu.h"
 #include "etnaviv_gem.h"
 #include "etnaviv_mmu.h"
-#include "etnaviv_gem.h"
 
 #ifdef CONFIG_DRM_ETNAVIV_REGISTER_LOGGING
 static bool reglog;
