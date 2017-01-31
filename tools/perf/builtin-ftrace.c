2c9fbd51c54b9430af186d025f2cf12171624721
Taeung Song <treeze.taeung@gmail.com>
2017-01-31 20:38:29 +0900

    perf ftrace: Add ftrace.tracer config option
    Currently 'perf ftrace' command allows selecting 'function_graph' or
    'function', defaulting to 'function_graph'.
    
    Add ftrace.tracer config option to select the default tracer:
    
        # cat ~/.perfconfig
        [ftrace]
            tracer = function
    
        # perf ftrace usleep 123456 | head -10
          <...>-14450 [002] d... 10089.284231: finish_task_switch <-__schedule
          <...>-14450 [002] .... 10089.284232: finish_wait <-pipe_wait
          <...>-14450 [002] .... 10089.284232: mutex_lock <-pipe_wait
          <...>-14450 [002] .... 10089.284232: _cond_resched <-mutex_lock
    
    Committer notes:
    
    Retesting it with invalid variables, invalid values for ftrace.tracer,
    and a valid one:
    
      # cat ~/.perfconfig
      [ftrace]
            trace = function
      # perf ftrace usleep 1
      Error: wrong config key-value pair ftrace.trace=function
      # cat ~/.perfconfig
      [ftrace]
            tracer = functin
      # perf ftrace usleep 1
      Please select "function_graph" (default) or "function"
      Error: wrong config key-value pair ftrace.tracer=functin
      # cat ~/.perfconfig
      [ftrace]
            tracer = function
      # perf ftrace usleep 1 | head -5
              <idle>-0     [000] d...  3855.820847: switch_mm_irqs_off <-__schedule
               <...>-18550 [000] d...  3855.820849: finish_task_switch <-__schedule
               <...>-18550 [000] d...  3855.820851: smp_irq_work_interrupt <-irq_work_interrupt
               <...>-18550 [000] d...  3855.820851: irq_enter <-smp_irq_work_interrupt
               <...>-18550 [000] d...  3855.820851: rcu_irq_enter <-irq_enter
      #
    
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/r/1485862711-20216-3-git-send-email-treeze.taeung@gmail.com
    [ Added missign space in error message, changed the logic to make it more compact and less error prone ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-ftrace.c
+++ tools/perf/builtin-ftrace.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "evlist.h"
 #include "target.h"
 #include "thread_map.h"
+#include "util/config.h"
 
 
 #define DEFAULT_TRACER  "function_graph"
@@ -199,6 +200,26 @@ out:
 	return done ? 0 : -1;
 }
 
+static int perf_ftrace_config(const char *var, const char *value, void *cb)
+{
+	struct perf_ftrace *ftrace = cb;
+
+	if (prefixcmp(var, "ftrace."))
+		return 0;
+
+	if (strcmp(var, "ftrace.tracer"))
+		return -1;
+
+	if (!strcmp(value, "function_graph") ||
+	    !strcmp(value, "function")) {
+		ftrace->tracer = value;
+		return 0;
+	}
+
+	pr_err("Please select \"function_graph\" (default) or \"function\"\n");
+	return -1;
+}
+
 int cmd_ftrace(int argc, const char **argv, const char *prefix __maybe_unused)
 {
 	int ret;
@@ -218,6 +239,10 @@ int cmd_ftrace(int argc, const char **argv, const char *prefix __maybe_unused)
 		 "be more verbose"),
 	OPT_END()
 	};
+
+	ret = perf_config(perf_ftrace_config, &ftrace);
+	if (ret < 0)
+		return -1;
 
 	argc = parse_options(argc, argv, ftrace_options, ftrace_usage,
 			    PARSE_OPT_STOP_AT_NON_OPTION);
