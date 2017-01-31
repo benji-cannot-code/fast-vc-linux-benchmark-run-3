4c62094dc4ebaa278f6eeacc78dd939d0c8f28aa
Miquel Raynal <raynal.miquel@gmail.com>
2017-01-31 14:32:08 +0100

    staging: bcm2835-audio: fix style problem in vc_vchi_audioserv_defs.h
    Remove spaces around parenthesized expression to conform to the
    kernel coding style. Issue found by checkpatch.pl script.
    
    Signed-off-by: Miquel Raynal <raynal.miquel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm2835-audio/vc_vchi_audioserv_defs.h
+++ drivers/staging/bcm2835-audio/vc_vchi_audioserv_defs.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VC_AUDIO_SERVER_NAME  MAKE_FOURCC("AUDS")
 
 /* Maximum message length */
-#define VC_AUDIO_MAX_MSG_LEN  (sizeof( VC_AUDIO_MSG_T ))
+#define VC_AUDIO_MAX_MSG_LEN  (sizeof(VC_AUDIO_MSG_T))
 
 /*
  *  List of screens that are currently supported
