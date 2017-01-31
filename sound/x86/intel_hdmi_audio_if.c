d8fd9a9b7aee069a167a1cf6917c7e2085e8b7b9
Takashi Iwai <tiwai@suse.de>
2017-01-31 08:14:34 +0100

    ALSA: x86: Embed snd_intelhad into snd_card
    Instead of allocating snd_intelhad struct, use the card's private_data
    and embed it.  It simplifies the code a lot.
    
    While we're at it, embed had_stream into snd_intelhad struct instead
    of individually allocating, and rename had_pvt_data to a bit more
    specific name, had_stream_data.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/x86/intel_hdmi_audio_if.c
+++ sound/x86/intel_hdmi_audio_if.c
@@ -46,13 +46,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int hdmi_audio_query(void *haddata, struct hdmi_audio_event event)
 {
 	struct snd_pcm_substream *substream = NULL;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	unsigned long flag_irqs;
 	struct snd_intelhad *intelhaddata = (struct snd_intelhad *)haddata;
 
 	if (intelhaddata->stream_info.had_substream)
 		substream = intelhaddata->stream_info.had_substream;
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 	switch (event.type) {
 	case HAD_EVENT_QUERY_IS_AUDIO_BUSY:
 		spin_lock_irqsave(&intelhaddata->had_spinlock, flag_irqs);
@@ -99,14 +99,14 @@ int hdmi_audio_query(void *haddata, struct hdmi_audio_event event)
 int hdmi_audio_suspend(void *haddata)
 {
 	int caps, retval = 0;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	unsigned long flag_irqs;
 	struct snd_pcm_substream *substream;
 	struct snd_intelhad *intelhaddata = (struct snd_intelhad *)haddata;
 
 	pr_debug("Enter:%s\n", __func__);
 
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 	substream = intelhaddata->stream_info.had_substream;
 
 	if (intelhaddata->dev->power.runtime_status != RPM_SUSPENDED) {
@@ -200,10 +200,10 @@ static inline int had_chk_intrmiss(struct snd_intelhad *intelhaddata,
 	int i, intr_count = 0;
 	enum intel_had_aud_buf_type buff_done;
 	u32 buf_size, buf_addr;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	unsigned long flag_irqs;
 
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 
 	buff_done = buf_id;
 
@@ -245,12 +245,12 @@ int had_process_buffer_done(struct snd_intelhad *intelhaddata)
 	enum intel_had_aud_buf_type buff_done;
 	struct pcm_stream_info *stream;
 	u32 buf_size;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	int intr_count;
 	enum had_status_stream		stream_type;
 	unsigned long flag_irqs;
 
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 	stream = &intelhaddata->stream_info;
 	intr_count = 1;
 
@@ -332,12 +332,12 @@ int had_process_buffer_underrun(struct snd_intelhad *intelhaddata)
 {
 	enum intel_had_aud_buf_type buf_id;
 	struct pcm_stream_info *stream;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	enum had_status_stream stream_type;
 	unsigned long flag_irqs;
 	int drv_status;
 
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 	stream = &intelhaddata->stream_info;
 
 	spin_lock_irqsave(&intelhaddata->had_spinlock, flag_irqs);
@@ -373,13 +373,13 @@ int had_process_hot_plug(struct snd_intelhad *intelhaddata)
 {
 	enum intel_had_aud_buf_type buf_id;
 	struct snd_pcm_substream *substream;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	unsigned long flag_irqs;
 
 	pr_debug("Enter:%s\n", __func__);
 
 	substream = intelhaddata->stream_info.had_substream;
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 
 	spin_lock_irqsave(&intelhaddata->had_spinlock, flag_irqs);
 	if (intelhaddata->drv_status == HAD_DRV_CONNECTED) {
@@ -414,12 +414,12 @@ int had_process_hot_unplug(struct snd_intelhad *intelhaddata)
 {
 	int caps, retval = 0;
 	enum intel_had_aud_buf_type buf_id;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	unsigned long flag_irqs;
 
 	pr_debug("Enter:%s\n", __func__);
 
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 	buf_id = intelhaddata->curr_buf;
 
 	spin_lock_irqsave(&intelhaddata->had_spinlock, flag_irqs);
@@ -477,11 +477,11 @@ int had_event_handler(enum had_event_type event_type, void *data)
 	struct snd_intelhad *intelhaddata = data;
 	enum intel_had_aud_buf_type buf_id;
 	struct snd_pcm_substream *substream;
-	struct had_pvt_data *had_stream;
+	struct had_stream_data *had_stream;
 	unsigned long flag_irqs;
 
 	buf_id = intelhaddata->curr_buf;
-	had_stream = intelhaddata->private_data;
+	had_stream = &intelhaddata->stream_data;
 
 	/* Switching to a function can drop atomicity even in INTR context.
 	 * Thus, a big lock is acquired to maintain atomicity.
