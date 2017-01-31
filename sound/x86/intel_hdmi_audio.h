4240222e86887618ab1672c8520f3f921fb181e0
Takashi Iwai <tiwai@suse.de>
2017-01-31 18:14:15 +0100

    ALSA: x86: Remove _v[12] suffices
    Although we dropped the most of the obsoleted *_v1 definitions and
    codes, some codes still keep the _v1 or _v2 suffices.  Now they are
    ripped off.
    
    The only thing to be done carefully here is the definition of control
    offsets.  The original code defines enum hdmi_ctrl_reg_offset_v1 and
    a few new elements just for v2 on its top.  After this cleanup, we
    remove the old AUD_HDMI_STATUS and AUD_HDMIW_INFOFR definitions and
    replace with the v2 values.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/x86/intel_hdmi_audio.h
+++ sound/x86/intel_hdmi_audio.h
@@ -70,7 +70,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LAYOUT0			0
 #define LAYOUT1			1
 #define SWAP_LFE_CENTER		0x00fac4c8
-#define AUD_CONFIG_CH_MASK_V2	0x70
+#define AUD_CONFIG_CH_MASK	0x70
 
 struct pcm_stream_info {
 	int		str_id;
