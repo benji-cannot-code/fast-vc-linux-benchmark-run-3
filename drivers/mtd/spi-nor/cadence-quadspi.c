1d9bc7d9f1c5964b8b5788d986b05f9532086a35
Colin Ian King <colin.king@canonical.com>
2017-01-31 15:53:17 +0000

    mtd: spi-nor: cqspi: remove redundant dead code on error return check
    Checking for ret < 0 is redundant because a previous check on ret
    being non-zero already handles the ret < 0 case. Remove the redundant
    code. Found by CoverityScan, CID#1398863, CID#1398864
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Reviewed-by: Richard Weinberger <richard@nod.at>
    Acked-by: Marek Vasut <marek.vasut@gmail.com>
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@atmel.com>

--- drivers/mtd/spi-nor/cadence-quadspi.c
+++ drivers/mtd/spi-nor/cadence-quadspi.c
@@ -894,7 +894,7 @@ static ssize_t cqspi_write(struct spi_nor *nor, loff_t to,
 	if (ret)
 		return ret;
 
-	return (ret < 0) ? ret : len;
+	return len;
 }
 
 static ssize_t cqspi_read(struct spi_nor *nor, loff_t from,
@@ -914,7 +914,7 @@ static ssize_t cqspi_read(struct spi_nor *nor, loff_t from,
 	if (ret)
 		return ret;
 
-	return (ret < 0) ? ret : len;
+	return len;
 }
 
 static int cqspi_erase(struct spi_nor *nor, loff_t offs)
