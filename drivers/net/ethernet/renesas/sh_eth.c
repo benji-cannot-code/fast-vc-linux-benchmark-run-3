7bbf705bc75bc60b260f232c390ed381ff271f02
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-02-01 15:41:55 +0100

    sh_eth: fix wakeup event reporting from MagicPacket
    If a link change interrupt happens along side the MagicPacket interrupt
    and the link change interrupt is ignored the interrupt handler will
    return and the wakeup event is not registered. Fix this by moving the
    MagicPacket check before the link change check.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reported-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/renesas/sh_eth.c
+++ drivers/net/ethernet/renesas/sh_eth.c
@@ -1606,6 +1606,8 @@ static void sh_eth_emac_interrupt(struct net_device *ndev)
 	sh_eth_write(ndev, felic_stat, ECSR);	/* clear int */
 	if (felic_stat & ECSR_ICD)
 		ndev->stats.tx_carrier_errors++;
+	if (felic_stat & ECSR_MPD)
+		pm_wakeup_event(&mdp->pdev->dev, 0);
 	if (felic_stat & ECSR_LCHNG) {
 		/* Link Changed */
 		if (mdp->cd->no_psr || mdp->no_ether_link)
@@ -1625,8 +1627,6 @@ static void sh_eth_emac_interrupt(struct net_device *ndev)
 			sh_eth_rcv_snd_enable(ndev);
 		}
 	}
-	if (felic_stat & ECSR_MPD)
-		pm_wakeup_event(&mdp->pdev->dev, 0);
 }
 
 /* error control function */
