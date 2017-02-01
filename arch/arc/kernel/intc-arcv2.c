accbad7516dc3bd08db3cc2eae6c38a762a57da5
Vineet Gupta <vgupta@synopsys.com>
2017-02-01 10:14:11 -0800

    ARC: [intc-*]: confine NR_CPU_IRQS to intc code
    And even this willl change in subsequent patches where we resort to
    using run time info instead...
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arch/arc/kernel/intc-arcv2.c
+++ arch/arc/kernel/intc-arcv2.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irqchip.h>
 #include <asm/irq.h>
 
+#define NR_CPU_IRQS	32	/* number of irq lines coming in */
+
 /*
  * Early Hardware specific Interrupt setup
  * -Called very early (start_kernel -> setup_arch -> setup_processor)
