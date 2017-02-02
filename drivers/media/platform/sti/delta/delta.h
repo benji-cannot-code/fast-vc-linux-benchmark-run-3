4d7eebbc6ea78d8f657fbb75f5cfcd34ba94bb14
Hugues Fruchet <hugues.fruchet@st.com>
2017-02-02 12:59:51 -0200

    [media] st-delta: EOS (End Of Stream) support
    EOS (End Of Stream) support allows user to get
    all the potential decoded frames remaining in decoder
    pipeline after having reached the end of video bitstream.
    To do so, user calls VIDIOC_DECODER_CMD(V4L2_DEC_CMD_STOP)
    which will drain the decoder and get the drained frames
    that are then returned to user.
    User is informed of EOS completion in two ways:
     - dequeue of an empty frame flagged to V4L2_BUF_FLAG_LAST
     - reception of a V4L2_EVENT_EOS event.
    If, unfortunately, no buffer is available on CAPTURE queue
    to return the empty frame, EOS is delayed till user queue
    one CAPTURE buffer.
    
    Acked-by: Peter Griffin <peter.griffin@linaro.org>
    Signed-off-by: Hugues Fruchet <hugues.fruchet@st.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/sti/delta/delta.h
+++ drivers/media/platform/sti/delta/delta.h
@@ -28,11 +28,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *@DELTA_STATE_READY:
  *	Decoding instance is ready to decode compressed access unit.
  *
+ *@DELTA_STATE_WF_EOS:
+ *	Decoding instance is waiting for EOS (End Of Stream) completion.
+ *
+ *@DELTA_STATE_EOS:
+ *	EOS (End Of Stream) is completed (signaled to user). Decoding instance
+ *	should then be closed.
  */
 enum delta_state {
 	DELTA_STATE_WF_FORMAT,
 	DELTA_STATE_WF_STREAMINFO,
 	DELTA_STATE_READY,
+	DELTA_STATE_WF_EOS,
+	DELTA_STATE_EOS
 };
 
 /*
@@ -238,6 +246,7 @@ struct delta_ctx;
  * @get_frame:		get the next decoded frame available, see below
  * @recycle:		recycle the given frame, see below
  * @flush:		(optional) flush decoder, see below
+ * @drain:		(optional) drain decoder, see below
  */
 struct delta_dec {
 	const char *name;
@@ -372,6 +381,18 @@ struct delta_dec {
 	 * decoding logic.
 	 */
 	int (*flush)(struct delta_ctx *ctx);
+
+	/*
+	 * drain() - drain decoder
+	 * @ctx:	(in) instance
+	 *
+	 * Optional.
+	 * Mark decoder pending frames (decoded but not yet output) as ready
+	 * so that they can be output to client at EOS (End Of Stream).
+	 * get_frame() is to be called in a loop right after drain() to
+	 * get all those pending frames.
+	 */
+	int (*drain)(struct delta_ctx *ctx);
 };
 
 struct delta_dev;
@@ -498,6 +519,8 @@ static inline char *frame_type_str(u32 flags)
 		return "P";
 	if (flags & V4L2_BUF_FLAG_BFRAME)
 		return "B";
+	if (flags & V4L2_BUF_FLAG_LAST)
+		return "EOS";
 	return "?";
 }
 
