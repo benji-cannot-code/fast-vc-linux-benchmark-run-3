a0f5cdabfc99825328d0d3b01f69893ec0ff62d8
Ingo Molnar <mingo@kernel.org>
2017-02-02 20:56:33 +0100

    sched/headers: Prepare to remove the <linux/gfp.h> include from <linux/sched.h>
    <linux/topology.h> is still needed - also update other headers
    and .c files that depend on sched.h including gfp.h (and its
    sub-headers) for them.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- lib/percpu_ida.c
+++ lib/percpu_ida.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * General Public License for more details.
  */
 
+#include <linux/mm.h>
 #include <linux/bitmap.h>
 #include <linux/bitops.h>
 #include <linux/bug.h>
