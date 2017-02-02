e45cebe0e0ac9181afdca3397f38f96e960edd57
Ingo Molnar <mingo@kernel.org>
2017-02-02 11:44:31 +0100

    sched/headers: Remove #include <linux/capability.h> from <linux/sched.h>
    The <linux/sched.h> header does not actually make use of any
    types or APIs defined in <linux/capability.h>, so remove its inclusion.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/sched/prio.h>
 
-#include <linux/capability.h>
 #include <linux/mutex.h>
 #include <linux/plist.h>
 #include <linux/mm_types_task.h>
