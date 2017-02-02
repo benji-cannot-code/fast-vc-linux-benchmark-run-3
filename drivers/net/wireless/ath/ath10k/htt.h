df41800ba2486f915d4da53c024e5074945c10b8
Kalle Valo <kvalo@qca.qualcomm.com>
2017-02-02 12:47:45 +0200

    ath10k: prefer unsigned int over just unsigned
    Fixes new checkpatch warnings:
    
    drivers/net/wireless/ath/ath10k/htt.h:1639: Prefer 'unsigned int' to bare use of 'unsigned'
    drivers/net/wireless/ath/ath10k/htt.h:1660: Prefer 'unsigned int' to bare use of 'unsigned'
    
    Signed-off-by: Kalle Valo <kvalo@qca.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/htt.h
+++ drivers/net/wireless/ath/ath10k/htt.h
@@ -1637,7 +1637,7 @@ struct ath10k_htt {
 		int size;
 
 		/* size - 1 */
-		unsigned size_mask;
+		unsigned int size_mask;
 
 		/* how many rx buffers to keep in the ring */
 		int fill_level;
@@ -1658,7 +1658,7 @@ struct ath10k_htt {
 
 		/* where HTT SW has processed bufs filled by rx MAC DMA */
 		struct {
-			unsigned msdu_payld;
+			unsigned int msdu_payld;
 		} sw_rd_idx;
 
 		/*
