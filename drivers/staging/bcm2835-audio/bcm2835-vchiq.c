2bca0a40480236d1dcbf02590d7c4ffd61a0e30d
Seraphime Kirkovski <kirkseraph@gmail.com>
2017-02-03 00:12:43 +0100

    staging: bcm2835: cleanup: remove deprecated <asm/uaccess.h> include
    This replaces the last occurrence of the deprecated <asm/uaccess.h> include
    in the staging directory with the newer <linux/uaccess.h>
    
    Signed-off-by: Seraphime Kirkovski <kirkseraph@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm2835-audio/bcm2835-vchiq.c
+++ drivers/staging/bcm2835-audio/bcm2835-vchiq.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/file.h>
 #include <linux/mm.h>
 #include <linux/syscalls.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/slab.h>
 #include <linux/delay.h>
 #include <linux/atomic.h>
