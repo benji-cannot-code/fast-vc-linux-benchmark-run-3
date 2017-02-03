ac6f9f48a26ece382d1bc3e94d181b3f9f074e67
Borislav Petkov <bp@suse.de>
2017-02-03 18:18:05 +0100

    EDAC: Remove EDAC_MM_EDAC
    Move all the EDAC core functionality behind CONFIG_EDAC and get rid of
    that indirection. Update defconfigs which had it.
    
    While at it, fix dependencies such that EDAC depends on RAS for the
    tracepoints.
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linuxppc-dev@lists.ozlabs.org
    Cc: Chris Metcalf <cmetcalf@mellanox.com>
    Cc: linux-edac@vger.kernel.org

--- drivers/edac/edac_stub.c
+++ drivers/edac/edac_stub.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int edac_report_status = EDAC_REPORTING_ENABLED;
 EXPORT_SYMBOL_GPL(edac_report_status);
 
-static int __init edac_report_setup(char *str)
+static int __init __maybe_unused edac_report_setup(char *str)
 {
 	if (!str)
 		return -EINVAL;
