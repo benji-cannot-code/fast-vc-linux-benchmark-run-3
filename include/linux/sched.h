c1f4ff7f65e6af8287f001fe0c37914ef7b7879d
Ingo Molnar <mingo@kernel.org>
2017-02-03 10:08:30 +0100

    sched/headers: Remove <linux/rtmutex.h> from <linux/sched.h>
    This reduces header dependencies.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pid.h>
 #include <linux/seccomp.h>
 #include <linux/rculist.h>
-#include <linux/rtmutex.h>
 
 #include <linux/resource.h>
 #include <linux/hrtimer.h>
