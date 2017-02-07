3149a6f8a6a7693f7d0e189fc3cb67ef6e16b1bb
Russell King <rmk+kernel@armlinux.org.uk>
2017-02-07 15:03:00 -0800

    net: mvneta: fix build errors when linux/phy*.h is removed from net/dsa.h
    drivers/net/ethernet/marvell/mvneta.c:2694:26: error: storage size of 'status' isn't known
    drivers/net/ethernet/marvell/mvneta.c:2695:26: error: storage size of 'changed' isn't known
    drivers/net/ethernet/marvell/mvneta.c:2695:9: error: variable 'changed' has initializer but incomplete type
    drivers/net/ethernet/marvell/mvneta.c:2709:2: error: implicit declaration of function 'fixed_phy_update_state' [-Werror=implicit-function-declaration]
    
    Add linux/phy_fixed.h to mvneta.c
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvneta.c
+++ drivers/net/ethernet/marvell/mvneta.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_mdio.h>
 #include <linux/of_net.h>
 #include <linux/phy.h>
+#include <linux/phy_fixed.h>
 #include <linux/platform_device.h>
 #include <linux/skbuff.h>
 #include <net/hwbm.h>
