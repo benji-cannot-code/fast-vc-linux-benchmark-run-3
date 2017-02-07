abcdd9435a1866c69260d328967f5f5c286e169d
Amir Vadai <amir@vadai.me>
2017-02-07 09:56:08 +0200

    net/act_pedit: Introduce 'add' operation
    This command could be useful to inc/dec fields.
    
    For example, to forward any TCP packet and decrease its TTL:
    $ tc filter add dev enp0s9 protocol ip parent ffff: \
        flower ip_proto tcp \
        action pedit munge ip ttl add 0xff pipe \
        action mirred egress redirect dev veth0
    
    In the example above, adding 0xff to this u8 field is actually
    decreasing it by one, since the operation is masked.
    
    Signed-off-by: Amir Vadai <amir@vadai.me>
    Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tc_act/tc_pedit.h
+++ include/net/tc_act/tc_pedit.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct tcf_pedit_key_ex {
 	enum pedit_header_type htype;
+	enum pedit_cmd cmd;
 };
 
 struct tcf_pedit {
