318f7af7df26d9fe14fc5b4de9c083290489b4ef
Lv Zheng <lv.zheng@intel.com>
2017-02-08 11:00:01 +0800

    ACPICA: Linuxize: Restore and fix Intel compiler build
    ACPICA commit b59347d0b8b676cb555fe8da5cad08fcd4eeb0d3
    
    The following commit cleans up compiler specific inclusions:
    
      Commit: 9fa1cebdbfff3db8953cebca8ee327d75edefc40
      Subject: ACPICA: OSL: Cleanup the inclusion order of the compiler-specific headers
    
    But breaks one thing due to the following old issue:
    
     Buidling Linux kernel with Intel compiler originally depends on acgcc.h
     not acintel.h.
    
    So after making Intel compiler build working in ACPICA upstream by
    correctly using acintel.h, it becomes unable to build Linux kernel using
    Intel compiler as there is no acintel.h in the kernel source tree.
    
    This patch releases acintel.h to Linux kernel and fixes its inclusion in
    acenv.h.
    
    Fixes: 9fa1cebdbfff (ACPICA: OSL: Cleanup the inclusion order of the compiler-specific headers)
    Link: https://github.com/acpica/acpica/commit/b59347d0
    Cc: 4.9+ <stable@vger.kernel.org> # 4.9+
    Tested-by: Stepan M Mishura <stepan.m.mishura@intel.com>
    Signed-off-by: Lv Zheng <lv.zheng@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/platform/acenv.h
+++ include/acpi/platform/acenv.h
@@ -179,7 +179,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "acmsvc.h"
 
 #elif defined(__INTEL_COMPILER)
-#include "acintel.h"
+#include <acpi/platform/acintel.h>
 
 #endif
 
