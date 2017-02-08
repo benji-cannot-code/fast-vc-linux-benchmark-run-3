821e3bc66be40202111f197720af58491cc6989d
Ingo Molnar <mingo@kernel.org>
2017-02-08 18:51:55 +0100

    sched/headers: Remove <linux/sched.h> from <linux/sched/user.h>
    If we add <linux/uidgid.h> then <linux/sched/user.h> becomes a
    self-contained header and users of it either don't need <linux/sched.h>
    or have already included it.
    
    This reduces the size of the header dependency graph.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched/user.h
+++ include/linux/sched/user.h
@@ -2,7 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SCHED_USER_H
 #define _LINUX_SCHED_USER_H
 
-#include <linux/sched.h>
+#include <linux/uidgid.h>
+#include <linux/atomic.h>
 
 struct key;
 
