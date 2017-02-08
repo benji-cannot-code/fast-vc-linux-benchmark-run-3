877ae6f7c81ee549a45243ab53339188d3de7aa2
Ingo Molnar <mingo@kernel.org>
2017-02-08 18:51:54 +0100

    sched/headers: Remove <linux/sched.h> from <linux/sched/mm.h>
    The <linux/sched/mm.h> file is a self-contained header and users of
    it either don't need <linux/sched.h> - or have already included it.
    
    Include kernel.h and atomic.h.
    
    This reduces the size of the header dependency graph.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched/mm.h
+++ include/linux/sched/mm.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SCHED_MM_H
 #define _LINUX_SCHED_MM_H
 
+#include <linux/kernel.h>
+#include <linux/atomic.h>
 #include <linux/sched.h>
 #include <linux/mm_types.h>
 #include <linux/gfp.h>
