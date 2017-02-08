dffac0d55de1b0baab8d319fa681b4b05e207046
Ingo Molnar <mingo@kernel.org>
2017-02-08 18:51:55 +0100

    sched/headers: Remove <linux/sched.h> from <linux/sched/cpufreq.h>
    Make the <linux/sched/cpufreq.h> file a self-contained header and
    remove the <linux/sched.h> dependency: users of it either don't
    need <linux/sched.h> - or have already included it.
    
    This reduces the size of the header dependency graph.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SCHED_CPUFREQ_H
 #define _LINUX_SCHED_CPUFREQ_H
 
-#include <linux/sched.h>
+#include <linux/types.h>
 
 /*
  * Interface between cpufreq drivers and the scheduler:
