00f3afd8646d391bdd0d1ac7e65d7f0bb0427742
Ingo Molnar <mingo@kernel.org>
2017-02-08 18:51:54 +0100

    sched/headers: Remove <linux/sched.h> from <linux/sched/coredump.h>
    The <linux/sched/coredump.h> file is a self-contained header and users of
    it either don't need <linux/sched.h> - or have already included it.
    
    Include <linux/mm_types.h>.
    
    This reduces the size of the header dependency graph.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched/coredump.h
+++ include/linux/sched/coredump.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SCHED_COREDUMP_H
 #define _LINUX_SCHED_COREDUMP_H
 
-#include <linux/sched.h>
 #include <linux/mm_types.h>
 
 #define SUID_DUMP_DISABLE	0	/* No setuid dumping */
