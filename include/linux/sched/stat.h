b55b5d76891278aa29f1fe47f2f98733c51b13ea
Ingo Molnar <mingo@kernel.org>
2017-02-08 18:51:57 +0100

    sched/headers: Remove <linux/sched.h> from <linux/sched/stat.h>
    The <linux/sched/stat.h> file is a largely self-contained header and users of
    it either don't need <linux/sched.h> - or have already included it.
    
    ( Keep the <linux/percpu.h> dependency.)
    
    This reduces the size of the header dependency graph.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched/stat.h
+++ include/linux/sched/stat.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SCHED_STAT_H
 #define _LINUX_SCHED_STAT_H
 
-#include <linux/sched.h>
+#include <linux/percpu.h>
 
 /*
  * Various counters maintained by the scheduler and fork(),
