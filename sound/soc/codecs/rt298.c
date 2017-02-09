ae7279a3b24a86e42a2cd360d617165ad6672727
Vinod Koul <vinod.koul@intel.com>
2017-02-09 16:44:02 +0530

    ASoC: rt298: Add DMI match for Geminilake reference platform
    Geminilake reference platform also uses combo jack for audio connector
    so we need to set codec pdata to use this based on DMI match for this
    board.
    
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt298.c
+++ sound/soc/codecs/rt298.c
@@ -1164,6 +1164,13 @@ static const struct dmi_system_id force_combo_jack_table[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "Broxton P")
 		}
 	},
+	{
+		.ident = "Intel Gemini Lake",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Intel Corp"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Geminilake")
+		}
+	},
 	{ }
 };
 
