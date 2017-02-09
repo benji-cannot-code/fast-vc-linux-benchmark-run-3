b59c55ff50299b0e7806247c6bae2ebb9551c624
Tero Kristo <t-kristo@ti.com>
2017-02-09 14:45:45 +0200

    clk: ti: divider: convert TI divider clock to use its own data representation
    Instead of using the generic clock driver data struct, use one internal
    for the TI clock driver itself. This allows modifying the register access
    parts in subsequent patch.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Acked-by: Tony Lindgren <tony@atomide.com>

--- drivers/clk/ti/clock.h
+++ drivers/clk/ti/clock.h
@@ -17,6 +17,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DRIVERS_CLK_TI_CLOCK__
 #define __DRIVERS_CLK_TI_CLOCK__
 
+struct clk_omap_divider {
+	struct clk_hw		hw;
+	void __iomem		*reg;
+	u8			shift;
+	u8			width;
+	u8			flags;
+	const struct clk_div_table	*table;
+};
+
+#define to_clk_omap_divider(_hw) container_of(_hw, struct clk_omap_divider, hw)
+
 struct clk_omap_mux {
 	struct clk_hw		hw;
 	void __iomem		*reg;
