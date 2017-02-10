0f50b1b3258233dc42b155c0f3660e7017bd4131
Hans de Goede <hdegoede@redhat.com>
2017-02-10 11:27:51 +0100

    x86/platform/intel/iosf_mbi: Add a mutex for P-Unit access
    One some systems the P-Unit accesses the PMIC to change various voltages
    through the same bus as other kernel drivers use for e.g. battery
    monitoring.
    
    If a driver sends requests to the P-Unit which require the P-Unit to access
    the PMIC bus while another driver is also accessing the PMIC bus various
    bad things happen.
    
    This commit adds a mutex to protect the P-Unit against simultaneous
    accesses and 2 functions to lock / unlock this mutex.
    
    Note on these systems the i2c-bus driver will request a sempahore from the
    P-Unit for exclusive access to the PMIC bus when i2c drivers are accessing
    it, but this does not appear to be sufficient, we still need to avoid
    making certain P-Unit requests during the access window to avoid problems.
    
    BugLink: https://bugzilla.kernel.org/show_bug.cgi?id=155241
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Tested-by: tagorereddy <tagore.chandan@gmail.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170210102802.20898-2-hdegoede@redhat.com

--- arch/x86/platform/intel/iosf_mbi.c
+++ arch/x86/platform/intel/iosf_mbi.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct pci_dev *mbi_pdev;
 static DEFINE_SPINLOCK(iosf_mbi_lock);
+static DEFINE_MUTEX(iosf_mbi_punit_mutex);
 
 static inline u32 iosf_mbi_form_mcr(u8 op, u8 port, u8 offset)
 {
@@ -190,6 +191,18 @@ bool iosf_mbi_available(void)
 	return mbi_pdev;
 }
 EXPORT_SYMBOL(iosf_mbi_available);
+
+void iosf_mbi_punit_acquire(void)
+{
+	mutex_lock(&iosf_mbi_punit_mutex);
+}
+EXPORT_SYMBOL(iosf_mbi_punit_acquire);
+
+void iosf_mbi_punit_release(void)
+{
+	mutex_unlock(&iosf_mbi_punit_mutex);
+}
+EXPORT_SYMBOL(iosf_mbi_punit_release);
 
 #ifdef CONFIG_IOSF_MBI_DEBUG
 static u32	dbg_mdr;
