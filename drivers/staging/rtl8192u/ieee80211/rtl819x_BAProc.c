67247e6bec542adbe4f31f5d93b258428a10ea8f
simran singhal <singhalsimran0@gmail.com>
2017-02-12 02:23:15 +0530

    staging: rtl8192u: Removing true and false comparison
    Remove comparison to true and false in if statement.
    Problem found usingcheckpatch.pl.
    CHECK: Using comparison to true is error prone
    CHECK: Using comparison to false is error prone
    
    Signed-off-by: simran singhal <singhalsimran0@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/rtl819x_BAProc.c
+++ drivers/staging/rtl8192u/ieee80211/rtl819x_BAProc.c
@@ -497,7 +497,7 @@ int ieee80211_rx_ADDBARsp(struct ieee80211_device *ieee, struct sk_buff *skb)
 	// Check if related BA is waiting for setup.
 	// If not, reject by sending DELBA frame.
 	//
-	if((pAdmittedBA->bValid==true))
+	if (pAdmittedBA->bValid)
 	{
 		// Since BA is already setup, we ignore all other ADDBA Response.
 		IEEE80211_DEBUG(IEEE80211_DL_BA, "OnADDBARsp(): Recv ADDBA Rsp. Drop because already admit it! \n");
@@ -651,7 +651,7 @@ TsInitAddBA(
 {
 	PBA_RECORD			pBA = &pTS->TxPendingBARecord;
 
-	if(pBA->bValid==true && bOverwritePending==false)
+	if (pBA->bValid && !bOverwritePending)
 		return;
 
 	// Set parameters to "Pending" variable set
