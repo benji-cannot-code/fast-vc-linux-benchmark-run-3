9d508de2f280828373450c3e9020d419ef87c19c
Chris Wilson <chris@chris-wilson.co.uk>
2017-02-13 17:15:19 +0000

    drm/i915: Mock infrastructure for request emission
    Create a fake engine that runs requests using a timer to simulate hw.
    
    v2: Prevent leaks of ctx->name along error paths
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170213171558.20942-8-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/selftests/mock_gem_device.h
+++ drivers/gpu/drm/i915/selftests/mock_gem_device.h
@@ -5,5 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct drm_i915_private;
 
 struct drm_i915_private *mock_gem_device(void);
+void mock_device_flush(struct drm_i915_private *i915);
 
 #endif /* !__MOCK_GEM_DEVICE_H__ */
