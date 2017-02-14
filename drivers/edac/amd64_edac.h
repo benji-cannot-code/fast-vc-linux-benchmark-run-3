39f2e36afacdbbc7ec7cf6307c3b7c3a9955833d
Borislav Petkov <bp@suse.de>
2017-02-14 11:58:05 +0100

    EDAC, amd64: Bump driver version
    Last time we did that was when we enabled Bulldozer. Now, we enabled Zen
    so it is only natural ... :-)
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Yazen Ghannam <Yazen.Ghannam@amd.com>

--- drivers/edac/amd64_edac.h
+++ drivers/edac/amd64_edac.h
@@ -86,7 +86,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *         sections 3.5.4 and 3.5.5 for more information.
  */
 
-#define EDAC_AMD64_VERSION		"3.4.0"
+#define EDAC_AMD64_VERSION		"3.5.0"
 #define EDAC_MOD_STR			"amd64_edac"
 
 /* Extended Model from CPUID, for CPU Revision numbers */
