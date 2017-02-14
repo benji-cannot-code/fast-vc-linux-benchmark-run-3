8a15e86996338c9b3577dab50f8b27e0e4ccba08
Dan Carpenter <dan.carpenter@oracle.com>
2017-02-15 02:15:44 +0300

    staging: bcm2835-audio: allocate enough data for work queues
    We accidentally allocate sizeof(void *) bytes instead of 112 bytes.  It
    results in memory corruption.
    
    Fixes: 23b028c871e1 ("staging: bcm2835-audio: initial staging submission")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm2835-audio/bcm2835-vchiq.c
+++ drivers/staging/bcm2835-audio/bcm2835-vchiq.c
@@ -136,8 +136,9 @@ int bcm2835_audio_start(struct bcm2835_alsa_stream *alsa_stream)
 
 	LOG_DBG(" .. IN\n");
 	if (alsa_stream->my_wq) {
-		struct bcm2835_audio_work *work =
-			kmalloc(sizeof(struct bcm2835_audio_work *), GFP_ATOMIC);
+		struct bcm2835_audio_work *work;
+
+		work = kmalloc(sizeof(*work), GFP_ATOMIC);
 		/*--- Queue some work (item 1) ---*/
 		if (work) {
 			INIT_WORK(&work->my_work, my_wq_function);
@@ -158,8 +159,9 @@ int bcm2835_audio_stop(struct bcm2835_alsa_stream *alsa_stream)
 
 	LOG_DBG(" .. IN\n");
 	if (alsa_stream->my_wq) {
-		struct bcm2835_audio_work *work =
-			kmalloc(sizeof(struct bcm2835_audio_work *), GFP_ATOMIC);
+		struct bcm2835_audio_work *work;
+
+		work = kmalloc(sizeof(*work), GFP_ATOMIC);
 		/*--- Queue some work (item 1) ---*/
 		if (work) {
 			INIT_WORK(&work->my_work, my_wq_function);
@@ -181,8 +183,9 @@ int bcm2835_audio_write(struct bcm2835_alsa_stream *alsa_stream,
 
 	LOG_DBG(" .. IN\n");
 	if (alsa_stream->my_wq) {
-		struct bcm2835_audio_work *work =
-			kmalloc(sizeof(struct bcm2835_audio_work *), GFP_ATOMIC);
+		struct bcm2835_audio_work *work;
+
+		work = kmalloc(sizeof(*work), GFP_ATOMIC);
 		/*--- Queue some work (item 1) ---*/
 		if (work) {
 			INIT_WORK(&work->my_work, my_wq_function);
