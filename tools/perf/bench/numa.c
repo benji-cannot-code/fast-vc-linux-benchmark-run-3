095b20db5390fd98f90281c60d7c65187de62160
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-02-14 14:19:34 -0300

    perf bench numa: Make sure dprintf() is not defined
    When building with clang we get this error:
    
      bench/numa.c:46:9: error: 'dprintf' macro redefined [-Werror,-Wmacro-redefined]
      #define dprintf(x...) do { if (g && g->p.show_details >= 1) printf(x); } while (0)
              ^
      /usr/include/bits/stdio2.h:145:12: note: previous definition is here
      #   define dprintf(fd, ...) \
                 ^
        CC       /tmp/build/perf/tests/parse-no-sample-id-all.o
      1 error generated.
    
    So, make sure it is undefined before using that name.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Davidlohr Bueso <dbueso@suse.de>
    Cc: Hitoshi Mitake <mitake@dcl.info.waseda.ac.jp>
    Cc: Jakub Jelen <jjelen@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-f654o2svtrutamvxt7igwz74@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/bench/numa.c
+++ tools/perf/bench/numa.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Debug printf:
  */
+#undef dprintf
 #define dprintf(x...) do { if (g && g->p.show_details >= 1) printf(x); } while (0)
 
 struct thread_data {
