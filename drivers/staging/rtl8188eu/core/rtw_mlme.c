b2c8d83d940edd5fa86e52dd474882f035e22c74
Ivan Safonov <insafonov@gmail.com>
2017-02-15 01:25:32 +0300

    staging:r8188eu: remove unused MCS_rate_2R array
    MCS_rate_2R[] does not used. Remove it.
    
    Signed-off-by: Ivan Safonov <insafonov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_mlme.c
+++ drivers/staging/rtl8188eu/core/rtw_mlme.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <rtw_ioctl_set.h>
 #include <linux/vmalloc.h>
 
-extern unsigned char	MCS_rate_2R[16];
 extern unsigned char	MCS_rate_1R[16];
 
 int rtw_init_mlme_priv(struct adapter *padapter)
