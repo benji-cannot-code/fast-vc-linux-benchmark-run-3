ff09c817685bb7e0b1968e7de8932f525e31e04f
Johannes Berg <johannes.berg@intel.com>
2017-02-15 15:02:07 +0100

    mac80211: remove local pointer from rate_ctrl_ref
    This pointer really isn't needed, so remove it.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/rate.h
+++ net/mac80211/rate.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "driver-ops.h"
 
 struct rate_control_ref {
-	struct ieee80211_local *local;
 	const struct rate_control_ops *ops;
 	void *priv;
 };
