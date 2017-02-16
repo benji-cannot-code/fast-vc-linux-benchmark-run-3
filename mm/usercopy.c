9daee1a37cd335ecf71cb49641310188c1ceea12
Sahara <keun-o.park@darkmatter.ae>
2017-02-16 18:29:15 +0000

    usercopy: Move enum for arch_within_stack_frames()
    This patch moves the arch_within_stack_frames() return value enum up in
    the header files so that per-architecture implementations can reuse the
    same return values.
    
    Signed-off-by: Sahara <keun-o.park@darkmatter.ae>
    Signed-off-by: James Morse <james.morse@arm.com>
    [kees: adjusted naming and commit log]
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- mm/usercopy.c
+++ mm/usercopy.c
@@ -20,14 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/sched/task.h>
 #include <linux/sched/task_stack.h>
+#include <linux/thread_info.h>
 #include <asm/sections.h>
-
-enum {
-	BAD_STACK = -1,
-	NOT_STACK = 0,
-	GOOD_FRAME,
-	GOOD_STACK,
-};
 
 /*
  * Checks if a given pointer and length is contained by the current
