70ef82ded0a1d2eed73a6653f27f1b7d0a846259
Simon Horman <simon.horman@netronome.com>
2017-02-17 08:57:54 +0100

    nfp: Use PCI_DEVICE_ID_NETRONOME_NFP* defines
    Use PCI_DEVICE_ID_NETRONOME_NFP*, defined in linux/pci_ids.h,
    rather than replicating the same values in the NFP driver.
    
    Signed-off-by: Simon Horman <simon.horman@netronome.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/netronome/nfp/nfpcore/nfp_cpp.h
+++ drivers/net/ethernet/netronome/nfp/nfpcore/nfp_cpp.h
@@ -59,11 +59,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PCI_64BIT_BAR_COUNT             3
 
-/* NFP hardware vendor/device ids.
- */
-#define PCI_DEVICE_NFP4000              0x4000
-#define PCI_DEVICE_NFP6000              0x6000
-
 #define NFP_CPP_NUM_TARGETS             16
 
 struct device;
