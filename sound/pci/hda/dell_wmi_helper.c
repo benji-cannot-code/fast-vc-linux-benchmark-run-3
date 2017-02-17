48dfc5b07575e182e0bc641cc3ccfe3d0973add1
Michał Kępień <kernel@kempniu.pl>
2017-02-17 08:57:50 +0100

    platform/x86: dell-laptop: import dell_micmute_led_set() from drivers/leds/dell-led.c
    To ensure all users of dell-smbios are in drivers/platform/x86, move the
    dell_micmute_led_set() method from drivers/leds/dell-led.c to
    drivers/platform/x86/dell-laptop.c.
    
    Signed-off-by: Michał Kępień <kernel@kempniu.pl>
    Tested-by: Alex Hung <alex.hung@canonical.com>
    Reviewed-by: Pali Rohár <pali.rohar@gmail.com>
    Acked-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Acked-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jacek Anaszewski <jacek.anaszewski@gmail.com>

--- sound/pci/hda/dell_wmi_helper.c
+++ sound/pci/hda/dell_wmi_helper.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * to be included from codec driver
  */
 
-#if IS_ENABLED(CONFIG_LEDS_DELL_NETBOOKS)
+#if IS_ENABLED(CONFIG_DELL_LAPTOP)
 #include <linux/dell-led.h>
 
 static int dell_led_value;
@@ -68,10 +68,10 @@ static void alc_fixup_dell_wmi(struct hda_codec *codec,
 	}
 }
 
-#else /* CONFIG_LEDS_DELL_NETBOOKS */
+#else /* CONFIG_DELL_LAPTOP */
 static void alc_fixup_dell_wmi(struct hda_codec *codec,
 			       const struct hda_fixup *fix, int action)
 {
 }
 
-#endif /* CONFIG_LEDS_DELL_NETBOOKS */
+#endif /* CONFIG_DELL_LAPTOP */
