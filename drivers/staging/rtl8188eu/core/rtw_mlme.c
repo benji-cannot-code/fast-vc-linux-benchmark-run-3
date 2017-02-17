be101baf4da1ddce3b8980f2079f361e19d11b70
Gargi Sharma <gs051095@gmail.com>
2017-02-17 11:50:28 +0530

    staging: rtl8188eu: Remove unnecessary blank lines
    Remove unnecessary blank lines to fix the checkpatch issue,
    blank lines are not required before '}'.
    
    Signed-off-by: Gargi Sharma <gs051095@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_mlme.c
+++ drivers/staging/rtl8188eu/core/rtw_mlme.c
@@ -392,7 +392,6 @@ void update_network(struct wlan_bssid_ex *dst, struct wlan_bssid_ex *src,
 	dst->PhyInfo.SignalStrength = ss_final;
 	dst->PhyInfo.SignalQuality = sq_final;
 	dst->Rssi = rssi_final;
-
 }
 
 static void update_current_network(struct adapter *adapter, struct wlan_bssid_ex *pnetwork)
@@ -494,7 +493,6 @@ void rtw_update_scanned_network(struct adapter *adapter, struct wlan_bssid_ex *t
 
 exit:
 	spin_unlock_bh(&queue->lock);
-
 }
 
 static void rtw_add_network(struct adapter *adapter,
