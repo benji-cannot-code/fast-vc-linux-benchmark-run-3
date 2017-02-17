de134b57ff5bd5a599772a4bef3fb3fbd48b2389
Y.C. Chen <yc_chen@aspeedtech.com>
2017-02-17 10:56:47 +0800

    drm/ast: Fixed vram size incorrect issue on POWER
    The default value of VGA scratch may incorrect.
    Should initial h/w before get vram info.
    
    Acked-by: Joel Stanley <joel@jms.id.au>
    Tested-by: Y.C. Chen <yc_chen@aspeedtech.com>
    Signed-off-by: Y.C. Chen <yc_chen@aspeedtech.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/ast/ast_main.c
+++ drivers/gpu/drm/ast/ast_main.c
@@ -519,6 +519,9 @@ int ast_driver_load(struct drm_device *dev, unsigned long flags)
 
 	ast_detect_chip(dev, &need_post);
 
+	if (need_post)
+		ast_post_gpu(dev);
+
 	if (ast->chip != AST1180) {
 		ret = ast_get_dram_info(dev);
 		if (ret)
@@ -528,9 +531,6 @@ int ast_driver_load(struct drm_device *dev, unsigned long flags)
 			 ast->mclk, ast->dram_type,
 			 ast->dram_bus_width, ast->vram_size);
 	}
-
-	if (need_post)
-		ast_post_gpu(dev);
 
 	ret = ast_mm_init(ast);
 	if (ret)
