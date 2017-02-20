3aa7da122feeaf163586425aa4c6b2bd6771a938
Jerome Brunet <jbrunet@baylibre.com>
2017-02-20 18:02:34 +0100

    clk: meson: gxbb: add cts_mclk_i958
    Add the spdif master clock also referred as cts_mclk_i958
    
    Acked-by: Michael Turquette <mturquette@baylibre.com>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- drivers/clk/meson/gxbb.h
+++ drivers/clk/meson/gxbb.h
@@ -281,8 +281,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_CTS_AMCLK		  107
 #define CLKID_CTS_AMCLK_SEL	  108
 #define CLKID_CTS_AMCLK_DIV	  109
+#define CLKID_CTS_MCLK_I958	  110
+#define CLKID_CTS_MCLK_I958_SEL	  111
+#define CLKID_CTS_MCLK_I958_DIV	  112
 
-#define NR_CLKS			  110
+#define NR_CLKS			  113
 
 /* include the CLKIDs that have been made part of the stable DT binding */
 #include <dt-bindings/clock/gxbb-clkc.h>
