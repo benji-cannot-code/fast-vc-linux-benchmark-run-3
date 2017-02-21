be9aa38bb94415ff87fc52001578c4ccd9b5854b
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2017-02-21 09:13:44 +0000

    drm/i915/tracepoints: Adjust i915_gem_ring_dispatch
    Rename it to i915_gem_request_queue and fix the logged info
    equivalent to the i915_gem_request even class. Also moved it
    a bit further apart from the i915_gem_request_add tracepoint
    since they otherwise provide similar information too close in
    time.
    
    v2: Remove sw fence singalling. We will rely on the soon to
        come GuC scheduling backend to enable that. (Chris Wilson)
    
    v3: Log hex with 0x prefix for clarity.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/i915_gem_execbuffer.c
@@ -1494,8 +1494,6 @@ execbuf_submit(struct i915_execbuffer_params *params,
 	if (ret)
 		return ret;
 
-	trace_i915_gem_ring_dispatch(params->request, params->dispatch_flags);
-
 	i915_gem_execbuffer_move_to_active(vmas, params->request);
 
 	return 0;
@@ -1836,6 +1834,8 @@ i915_gem_do_execbuffer(struct drm_device *dev, void *data,
 	params->engine                    = engine;
 	params->dispatch_flags          = dispatch_flags;
 	params->ctx                     = ctx;
+
+	trace_i915_gem_request_queue(params->request, dispatch_flags);
 
 	ret = execbuf_submit(params, args, &eb->vmas);
 err_request:
