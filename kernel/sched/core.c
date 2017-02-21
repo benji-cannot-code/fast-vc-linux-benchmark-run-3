8f55bdcd9559c41c632d8f3a012946391317d43a
Mark Brown <broonie@kernel.org>
2017-02-21 09:29:01 -0800

    sched/core: Fix build paravirt build on arm and arm64
    Commit 004172bdad64 ("sched/core: Remove unnecessary #include headers")
    removed the inclusion of asm/paravirt.h which is used to get
    declarations of paravirt_steal_rq_enabled and paravirt_steal_clock.
    
    It is implicitly included on x86 but not on arm and arm64 breaking the
    build if paravirtualization is used.  Since things from that header are
    used directly fix the build by putting the direct inclusion back.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
+#ifdef CONFIG_PARAVIRT
+#include <asm/paravirt.h>
+#endif
 
 #include "sched.h"
 #include "../workqueue_internal.h"
