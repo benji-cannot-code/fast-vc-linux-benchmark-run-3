d15c4e1e9d887e79b6feb5e774b6e0e4e621aeb3
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2017-02-21 13:03:57 +0100

    tty/serial: atmel: remove cache when unnecessary
    struct cache is only used in suspend/resume. Exclude it when PM is not
    selected.
    
    Suggested-by: Richard Genoud <richard.genoud@gmail.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Acked-by: Richard Genoud <richard.genoud@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/atmel_serial.c
+++ drivers/tty/serial/atmel_serial.c
@@ -177,6 +177,7 @@ struct atmel_uart_port {
 	unsigned int		pending_status;
 	spinlock_t		lock_suspended;
 
+#ifdef CONFIG_PM
 	struct {
 		u32		cr;
 		u32		mr;
@@ -187,6 +188,7 @@ struct atmel_uart_port {
 		u32		fmr;
 		u32		fimr;
 	} cache;
+#endif
 
 	int (*prepare_rx)(struct uart_port *port);
 	int (*prepare_tx)(struct uart_port *port);
