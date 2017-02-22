c16b2179b68fdba9f4f3008fa4670de2a07d35d3
Mike Kravetz <mike.kravetz@oracle.com>
2017-02-22 15:43:04 -0800

    userfaultfd: hugetlbfs: allow registration of ranges containing huge pages
    Expand the userfaultfd_register/unregister routines to allow VM_HUGETLB
    vmas.  huge page alignment checking is performed after a VM_HUGETLB vma
    is encountered.
    
    Also, since there is no UFFDIO_ZEROPAGE support for huge pages do not
    return that as a valid ioctl method for huge page ranges.
    
    Link: http://lkml.kernel.org/r/20161216144821.5183-22-aarcange@redhat.com
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Cc: "Dr. David Alan Gilbert" <dgilbert@redhat.com>
    Cc: Hillf Danton <hillf.zj@alibaba-inc.com>
    Cc: Michael Rapoport <RAPOPORT@il.ibm.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Pavel Emelyanov <xemul@parallels.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/uapi/linux/userfaultfd.h
+++ include/uapi/linux/userfaultfd.h
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	((__u64)1 << _UFFDIO_WAKE |		\
 	 (__u64)1 << _UFFDIO_COPY |		\
 	 (__u64)1 << _UFFDIO_ZEROPAGE)
+#define UFFD_API_RANGE_IOCTLS_HPAGE		\
+	((__u64)1 << _UFFDIO_WAKE |		\
+	 (__u64)1 << _UFFDIO_COPY)
 
 /*
  * Valid ioctl command number range with this API is from 0x00 to
