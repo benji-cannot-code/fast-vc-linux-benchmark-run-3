d4c92633dfcbafea9e9c0c773bada5b4d5f42519
Abhijit Naik <abhijitnaik27@gmail.com>
2017-02-23 18:30:59 +0530

    staging: bcm2835-audi: Removed return statement from void functions
    bcm2835-vchiq.c:
    fixing WARNING: void function return statements are not generally useful
    
    Signed-off-by: Abhijit Naik <abhijitnaik27@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/bcm2835-audio/bcm2835-vchiq.c
+++ drivers/staging/bcm2835-audio/bcm2835-vchiq.c
@@ -205,7 +205,6 @@ int bcm2835_audio_write(struct bcm2835_alsa_stream *alsa_stream,
 static void my_workqueue_init(struct bcm2835_alsa_stream *alsa_stream)
 {
 	alsa_stream->my_wq = alloc_workqueue("my_queue", WQ_HIGHPRI, 1);
-	return;
 }
 
 static void my_workqueue_quit(struct bcm2835_alsa_stream *alsa_stream)
@@ -215,7 +214,6 @@ static void my_workqueue_quit(struct bcm2835_alsa_stream *alsa_stream)
 		destroy_workqueue(alsa_stream->my_wq);
 		alsa_stream->my_wq = NULL;
 	}
-	return;
 }
 
 static void audio_vchi_callback(void *param,
@@ -888,7 +886,6 @@ void bcm2835_audio_flush_buffers(struct bcm2835_alsa_stream *alsa_stream)
 {
 	LOG_DBG(" .. IN\n");
 	LOG_DBG(" .. OUT\n");
-	return;
 }
 
 /**
