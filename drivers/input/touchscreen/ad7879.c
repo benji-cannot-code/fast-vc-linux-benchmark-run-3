83b8f4382adf3aeb8d06a110ee09a7b20e9d3692
Randy Dunlap <rdunlap@infradead.org>
2017-02-25 10:32:58 -0800

    Input: ad7879 - add header file to fix ad7879.c build errors
    Add header file to fix these build errors:
    
    ../drivers/input/touchscreen/ad7879.c: In function 'ad7879_parse_dt':
    ../drivers/input/touchscreen/ad7879.c:505:2: error: implicit declaration of function 'device_property_read_u32' [-Werror=implicit-function-declaration]
      err = device_property_read_u32(dev, "adi,resistance-plate-x", &tmp);
      ^
    ../drivers/input/touchscreen/ad7879.c:512:2: error: implicit declaration of function 'device_property_read_u8' [-Werror=implicit-function-declaration]
      device_property_read_u8(dev, "adi,first-conversion-delay",
      ^
    ../drivers/input/touchscreen/ad7879.c:521:2: error: implicit declaration of function 'device_property_read_bool' [-Werror=implicit-function-declaration]
      ts->swap_xy = device_property_read_bool(dev, "touchscreen-swapped-x-y");
      ^
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/ad7879.c
+++ drivers/input/touchscreen/ad7879.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/property.h>
 #include <linux/regmap.h>
 #include <linux/slab.h>
 #include <linux/gpio.h>
