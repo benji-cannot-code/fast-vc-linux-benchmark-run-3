96469782e76ae9982ec3d9d26ba3c9bc71dd1856
James Simmons <jsimmons@infradead.org>
2017-02-26 19:41:33 -0500

    staging: lustre: lnet: change lnet_eq_t to proper structure
    Change lnet_eq_t from typedef to proper structure.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-on: https://review.whamcloud.com/20831
    Reviewed-by: Olaf Weber <olaf@sgi.com>
    Reviewed-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/lnet/lib-types.h
+++ drivers/staging/lustre/include/linux/lnet/lib-types.h
@@ -117,7 +117,7 @@ struct lnet_libhandle {
 #define lh_entry(ptr, type, member) \
 	((type *)((char *)(ptr) - (char *)(&((type *)0)->member)))
 
-typedef struct lnet_eq {
+struct lnet_eq {
 	struct list_head	  eq_list;
 	struct lnet_libhandle	  eq_lh;
 	lnet_seq_t		  eq_enq_seq;
@@ -126,7 +126,7 @@ typedef struct lnet_eq {
 	lnet_eq_handler_t	  eq_callback;
 	lnet_event_t		 *eq_events;
 	int			**eq_refs;	/* percpt refcount for EQ */
-} lnet_eq_t;
+};
 
 typedef struct lnet_me {
 	struct list_head	 me_list;
@@ -153,7 +153,7 @@ typedef struct lnet_libmd {
 	unsigned int		 md_options;
 	unsigned int		 md_flags;
 	void			*md_user_ptr;
-	lnet_eq_t		*md_eq;
+	struct lnet_eq		*md_eq;
 	unsigned int		 md_niov;	/* # frags */
 	union {
 		struct kvec	iov[LNET_MAX_IOV];
