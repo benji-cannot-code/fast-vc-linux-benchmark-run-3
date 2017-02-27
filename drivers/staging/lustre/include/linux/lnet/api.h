5798153e7d6cb178e373eedf25a6c48c1a0dff6a
James Simmons <jsimmons@infradead.org>
2017-02-26 19:41:54 -0500

    staging: lustre: lnet: change lnet_ins_pos_t to proper enum
    Change lnet_ins_pos_t from typedef to proper enum.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-on: https://review.whamcloud.com/20831
    Reviewed-by: Olaf Weber <olaf@sgi.com>
    Reviewed-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/lnet/api.h
+++ drivers/staging/lustre/include/linux/lnet/api.h
@@ -98,7 +98,7 @@ int LNetMEAttach(unsigned int      portal,
 		 __u64		   match_bits_in,
 		 __u64		   ignore_bits_in,
 		 enum lnet_unlink unlink_in,
-		 lnet_ins_pos_t    pos_in,
+		 enum lnet_ins_pos pos_in,
 		 struct lnet_handle_me *handle_out);
 
 int LNetMEInsert(struct lnet_handle_me current_in,
@@ -106,7 +106,7 @@ int LNetMEInsert(struct lnet_handle_me current_in,
 		 __u64		   match_bits_in,
 		 __u64		   ignore_bits_in,
 		 enum lnet_unlink unlink_in,
-		 lnet_ins_pos_t    position_in,
+		 enum lnet_ins_pos position_in,
 		 struct lnet_handle_me *handle_out);
 
 int LNetMEUnlink(struct lnet_handle_me current_in);
