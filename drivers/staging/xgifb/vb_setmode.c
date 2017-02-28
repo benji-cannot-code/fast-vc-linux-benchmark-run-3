b5f15b01e5023b24a8d56c7c9f864f52dafde2c6
Manoj Sawai <mas@iitkgp.ac.in>
2017-02-28 17:44:53 +0530

    Staging: xgifb: vb_setmode.c: Fix checkpath warnings
    Fix the following checkpatch.pl warnings:
    
    1: Avoid multiple line dereference - prefer
       'XGI330_RefIndex[RefreshRateTableIndex+i].ModeID'
    
    2: Avoid multiple line dereference - prefer
       'XGI330_RefIndex[RefreshRateTableIndex+(*i)].Ext_InfoFlag'
    
    Signed-off-by: Manoj Sawai <mas@iitkgp.ac.in>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/xgifb/vb_setmode.c
+++ drivers/staging/xgifb/vb_setmode.c
@@ -5091,8 +5091,7 @@ unsigned short XGI_GetRatePtrCRT2(struct xgi_hw_device_info *pXGIHWDE,
 
 	i = 0;
 	do {
-		if (XGI330_RefIndex[RefreshRateTableIndex + i].
-			ModeID != ModeNo)
+		if (XGI330_RefIndex[RefreshRateTableIndex + i].ModeID != ModeNo)
 			break;
 		temp = XGI330_RefIndex[RefreshRateTableIndex + i].Ext_InfoFlag;
 		temp &= ModeTypeMask;
@@ -5104,8 +5103,7 @@ unsigned short XGI_GetRatePtrCRT2(struct xgi_hw_device_info *pXGIHWDE,
 	} while (index != 0xFFFF);
 	if (!(pVBInfo->VBInfo & SetCRT2ToRAMDAC)) {
 		if (pVBInfo->VBInfo & SetInSlaveMode) {
-			temp = XGI330_RefIndex[RefreshRateTableIndex + i - 1].
-				Ext_InfoFlag;
+			temp = XGI330_RefIndex[RefreshRateTableIndex + i - 1].Ext_InfoFlag;
 			if (temp & InterlaceMode)
 				i++;
 		}
