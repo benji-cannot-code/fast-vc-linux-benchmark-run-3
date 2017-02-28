2222a9899fe435f9a9b1d10dd627b11d724a8602
Josh Poimboeuf <jpoimboe@redhat.com>
2017-02-27 22:21:16 -0600

    objtool: Enclose contents of unreachable() macro in a block
    Guenter Roeck reported a boot failure in mips64.  It was bisected to the
    following commit:
    
      d1091c7fa3d5 ("objtool: Improve detection of BUG() and other dead ends")
    
    The unreachable() macro was formerly only composed of a single
    statement.  The above commit added a second statement, but neglected to
    enclose the statements in a block.
    
    Suggested-by: Guenter Roeck <linux@roeck-us.net>
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Fixes: d1091c7fa3d5 ("objtool: Improve detection of BUG() and other dead ends")
    Link: http://lkml.kernel.org/r/20170228042116.glmwmwiohcix7o4a@treble
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -216,7 +216,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * this in the preprocessor, but we can live with this because they're
  * unreleased.  Really, we need to have autoconf for the kernel.
  */
-#define unreachable() annotate_unreachable(); __builtin_unreachable()
+#define unreachable() \
+	do { annotate_unreachable(); __builtin_unreachable(); } while (0)
 
 /* Mark a function definition as prohibited from being cloned. */
 #define __noclone	__attribute__((__noclone__, __optimize__("no-tracer")))
