89d57e3a6cae0b6719699a9b8d1f69446c9061da
Satish Kharat <satishkh@cisco.com>
2017-02-28 16:15:59 -0800

    scsi: fnic: Adding debug IO and Abort latency counter to fnic stats
    The IO and Abort latency counter counts the time taken to complete the
    IO and abort command into broad buckets. This is not intended for
    performance measurement, just a debug statistic.  current_max_io_time
    tries to keep track of the maximum time an IO has taken to complete if
    it is > 30sec.
    
    Signed-off-by: Satish Kharat <satishkh@cisco.com>
    Signed-off-by: Sesidhar Baddela <sebaddel@cisco.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/fnic/fnic.h
+++ drivers/scsi/fnic/fnic.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME		"fnic"
 #define DRV_DESCRIPTION		"Cisco FCoE HBA Driver"
-#define DRV_VERSION		"1.6.0.21"
+#define DRV_VERSION		"1.6.0.34"
 #define PFX			DRV_NAME ": "
 #define DFX                     DRV_NAME "%d: "
 
