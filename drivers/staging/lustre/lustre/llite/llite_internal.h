f3a9ccce17c72c504519054e7576e9a99b501952
Gargi Sharma <gs051095@gmail.com>
2017-03-02 23:12:55 +0530

    staging: lustre: Remove lustre_eacl.h
    The structures and the macros in the header file are not used
    anywhere inside the kernel (verified by using grep). The structures
    and macros were leftover from the patch
    341f1f0affed1c24712f37c95bb654b3b33ab2c6 "staging: lustre: remove
    remote client support". Also, removed the include statements for
    lustre_eacl.h.
    
    Signed-off-by: Gargi Sharma <gs051095@gmail.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/llite_internal.h
+++ drivers/staging/lustre/lustre/llite/llite_internal.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../include/lustre_debug.h"
 #include "../include/lustre_ver.h"
 #include "../include/lustre_disk.h"	/* for s2sbi */
-#include "../include/lustre_eacl.h"
 #include "../include/lustre_linkea.h"
 
 /* for struct cl_lock_descr and struct cl_io */
--- drivers/staging/lustre/lustre/llite/xattr.c
+++ drivers/staging/lustre/lustre/llite/xattr.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../include/obd_support.h"
 #include "../include/lustre_dlm.h"
 #include "../include/lustre_ver.h"
-#include "../include/lustre_eacl.h"
 
 #include "llite_internal.h"
 
