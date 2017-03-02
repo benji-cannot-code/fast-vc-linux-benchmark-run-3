d0a1a318b9e9efa302261149a275fa6886dd62ea
Tamara Diaconita <diaconitatamara@gmail.com>
2017-03-02 20:41:58 +0200

    staging: cxd2099: Remove multiple blank lines
    Removed multiple blank lines to fix the checkpath.pl issue:
    CHECK: Please don't use multiple blank lines.
    
    Signed-off-by: Tamara Diaconita <diaconita.tamara@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/cxd2099/cxd2099.c
+++ drivers/staging/media/cxd2099/cxd2099.c
@@ -132,7 +132,6 @@ static int read_reg(struct cxd *ci, u8 reg, u8 *val)
 	return read_block(ci, reg, val, 1);
 }
 
-
 static int read_pccard(struct cxd *ci, u16 address, u8 *data, u8 n)
 {
 	int status;
@@ -292,8 +291,6 @@ static void cam_mode(struct cxd *ci, int mode)
 	}
 	ci->cammode = mode;
 }
-
-
 
 static int init(struct cxd *ci)
 {
@@ -573,7 +570,6 @@ static int slot_ts_enable(struct dvb_ca_en50221 *ca, int slot)
 	return 0;
 }
 
-
 static int campoll(struct cxd *ci)
 {
 	u8 istat;
@@ -616,7 +612,6 @@ static int campoll(struct cxd *ci)
 	}
 	return 0;
 }
-
 
 static int poll_slot_status(struct dvb_ca_en50221 *ca, int slot, int open)
 {
