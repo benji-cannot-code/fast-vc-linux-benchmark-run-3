7f16cc85d2f69953948044abe7166610ef43cdee
Katie Dunne <kdunne@mail.ccsf.edu>
2017-03-03 07:38:37 -0800

    staging: iio: meter: ade7753: Clean up includes
    Alphabetize header files.
    
    Signed-off-by: Katie Dunne <kdunne@mail.ccsf.edu>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/staging/iio/meter/ade7753.c
+++ drivers/staging/iio/meter/ade7753.c
@@ -7,20 +7,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Licensed under the GPL-2 or later.
  */
 
+#include <linux/delay.h>
+#include <linux/device.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
-#include <linux/delay.h>
-#include <linux/mutex.h>
-#include <linux/device.h>
 #include <linux/kernel.h>
-#include <linux/spi/spi.h>
-#include <linux/slab.h>
-#include <linux/sysfs.h>
 #include <linux/list.h>
 #include <linux/module.h>
-
+#include <linux/mutex.h>
+#include <linux/slab.h>
+#include <linux/sysfs.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
+#include <linux/spi/spi.h>
 #include "meter.h"
 
 #define ADE7753_WAVEFORM   0x01
