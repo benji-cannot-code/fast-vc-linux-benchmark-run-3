363b34fa22e58ee5250f786214027593feff9e13
James Hogan <james.hogan@imgtec.com>
2017-03-04 00:32:03 +0000

    MIPS: Include asm/ptrace.h now linux/sched.h doesn't
    Use of the task_pt_regs() based macros in MIPS' asm/processor.h for
    accessing the user context on the kernel stack need the definition of
    struct pt_regs from asm/ptrace.h. __own_fpu() in asm/fpu.h uses these
    macros but implicitly depended on linux/sched.h to include asm/ptrace.h.
    
    Since commit f780d89a0e82 ("sched/headers: Remove <asm/ptrace.h> from
    <linux/sched.h>") however linux/sched.h no longer includes asm/ptrace.h,
    so include it explicitly from asm/fpu.h where it is needed instead.
    
    This fixes build errors such as:
    
    ./arch/mips/include/asm/fpu.h: In function '__own_fpu':
    ./arch/mips/include/asm/processor.h:385:31: error: invalid application of 'sizeof' to incomplete type 'struct pt_regs'
         THREAD_SIZE - 32 - sizeof(struct pt_regs))
                                   ^
    
    Fixes: f780d89a0e82 ("sched/headers: Remove <asm/ptrace.h> from <linux/sched.h>")
    Signed-off-by: James Hogan <james.hogan@imgtec.com>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/15386/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/fpu.h
+++ arch/mips/include/asm/fpu.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpu-features.h>
 #include <asm/fpu_emulator.h>
 #include <asm/hazards.h>
+#include <asm/ptrace.h>
 #include <asm/processor.h>
 #include <asm/current.h>
 #include <asm/msa.h>
