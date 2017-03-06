386a441f7b1413c2340bfc1c2bdce510205b8e50
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-03-06 11:09:02 +0100

    staging: media: atomisp: handle sched.h breakup
    This fixes a build issue where sched.h changed around a bit in 4.11-rc1.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/atomisp/pci/atomisp2/hmm/hmm_bo.c
+++ drivers/staging/media/atomisp/pci/atomisp2/hmm/hmm_bo.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cacheflush.h>
 #include <linux/io.h>
 #include <asm/current.h>
-#include <linux/sched.h>
+#include <linux/sched/signal.h>
 #include <linux/file.h>
 
 #include "atomisp_internal.h"
