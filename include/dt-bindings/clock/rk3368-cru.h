5bca8cc88eba5cad561505e0c875299b4a8ba528
Elaine Zhang <zhangqing@rock-chips.com>
2017-03-07 19:05:17 +0800

    clk: rockchip: add clock ids for timer10-15 of RK3368 SoCs
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- include/dt-bindings/clock/rk3368-cru.h
+++ include/dt-bindings/clock/rk3368-cru.h
@@ -82,6 +82,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SCLK_SFC		126
 #define SCLK_MAC		127
 #define SCLK_MACREF_OUT		128
+#define SCLK_TIMER10		133
+#define SCLK_TIMER11		134
+#define SCLK_TIMER12		135
+#define SCLK_TIMER13		136
+#define SCLK_TIMER14		137
+#define SCLK_TIMER15		138
 
 #define DCLK_VOP		190
 #define MCLK_CRYPTO		191
