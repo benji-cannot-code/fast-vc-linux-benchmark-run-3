401cb5dcd4551753d83b41821a54112d471e2798
Ian Abbott <abbotti@mev.co.uk>
2017-03-08 16:02:57 +0000

    USB: serial: ftdi_sio: don't access latency timer on old chips
    The latency timer was introduced with the FT232BM and FT245BM chips.  Do
    not bother attempting to read or write it for older chip versions.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -1407,6 +1407,9 @@ static int write_latency_timer(struct usb_serial_port *port)
 	int rv;
 	int l = priv->latency;
 
+	if (priv->chip_type == SIO || priv->chip_type == FT8U232AM)
+		return -EINVAL;
+
 	if (priv->flags & ASYNC_LOW_LATENCY)
 		l = 1;
 
@@ -1429,6 +1432,9 @@ static int read_latency_timer(struct usb_serial_port *port)
 	struct usb_device *udev = port->serial->dev;
 	unsigned char *buf;
 	int rv;
+
+	if (priv->chip_type == SIO || priv->chip_type == FT8U232AM)
+		return -EINVAL;
 
 	buf = kmalloc(1, GFP_KERNEL);
 	if (!buf)
