9756913046c71807717f66e6d27f7c35f9bd54ce
Ryder Lee <ryder.lee@mediatek.com>
2017-03-09 10:11:15 +0800

    crypto: mediatek - simplify descriptor ring management
    This patch replaces cmd_pos/res_pos with pointer cmd_next/res_next.
    
    In old code, we must to add one to shift ring to the next segment, and
    then use this value to caculate current offset from ring base for each
    DMA operation. Now these pointers helps us to simplify flow, so we just
    need to move pointers and check the boundaries of ring.
    
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/mediatek/mtk-platform.c
+++ drivers/crypto/mediatek/mtk-platform.c
@@ -466,6 +466,9 @@ static int mtk_desc_ring_alloc(struct mtk_cryp *cryp)
 					   GFP_KERNEL);
 		if (!ring[i]->res_base)
 			goto err_cleanup;
+
+		ring[i]->cmd_next = ring[i]->cmd_base;
+		ring[i]->res_next = ring[i]->res_base;
 	}
 	return 0;
 
