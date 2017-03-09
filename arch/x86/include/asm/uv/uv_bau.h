dbc9532b59bdeddde1fad7b61e98e6ccc3238710
Andrew Banman <abanman@hpe.com>
2017-03-09 10:42:09 -0600

    x86/platform/uv/BAU: Add uv_bau_version enumerated constants
    Define enumerated constants for each UV hub version and replace magic
    numbers with the appropriate constant.
    
    Signed-off-by: Andrew Banman <abanman@hpe.com>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Cc: sivanich@hpe.com
    Cc: rja@hpe.com
    Cc: mike.travis@hpe.com
    Cc: akpm@linux-foundation.org
    Link: http://lkml.kernel.org/r/1489077734-111753-2-git-send-email-abanman@hpe.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/uv/uv_bau.h
+++ arch/x86/include/asm/uv/uv_bau.h
@@ -186,6 +186,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MSG_REGULAR			1
 #define MSG_RETRY			2
 
+enum uv_bau_version {
+	UV_BAU_V1 = 1,
+	UV_BAU_V2,
+	UV_BAU_V3,
+	UV_BAU_V4,
+};
+
 /*
  * Distribution: 32 bytes (256 bits) (bytes 0-0x1f of descriptor)
  * If the 'multilevel' flag in the header portion of the descriptor
