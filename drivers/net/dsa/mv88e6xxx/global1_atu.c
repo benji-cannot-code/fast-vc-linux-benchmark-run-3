d322eef79d2a236e35a7bc73b2ad6d8dfaa69391
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2017-03-11 16:12:51 -0500

    net: dsa: mv88e6xxx: enable ATU Learn2All
    The ATU Learn2All feature allows newly learnt addresses to be spanned on
    ports marked as "Message Port", currently all DSA ports.
    
    This commit enables this feature which is necessary and quite convenient
    for multi-chip switch fabrics.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/mv88e6xxx/global1_atu.c
+++ drivers/net/dsa/mv88e6xxx/global1_atu.c
@@ -16,6 +16,23 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Offset 0x0A: ATU Control Register */
 
+int mv88e6xxx_g1_atu_set_learn2all(struct mv88e6xxx_chip *chip, bool learn2all)
+{
+	u16 val;
+	int err;
+
+	err = mv88e6xxx_g1_read(chip, GLOBAL_ATU_CONTROL, &val);
+	if (err)
+		return err;
+
+	if (learn2all)
+		val |= GLOBAL_ATU_CONTROL_LEARN2ALL;
+	else
+		val &= ~GLOBAL_ATU_CONTROL_LEARN2ALL;
+
+	return mv88e6xxx_g1_write(chip, GLOBAL_ATU_CONTROL, val);
+}
+
 int mv88e6xxx_g1_atu_set_age_time(struct mv88e6xxx_chip *chip,
 				  unsigned int msecs)
 {
