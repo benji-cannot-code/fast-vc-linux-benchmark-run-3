828835bc1a437735bb1ccdb2096a6f308aa28eb9
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-03-14 18:35:36 +0100

    x86/xen: separate PV and HVM hypervisors
    As a preparation to splitting the code we need to untangle it:
    
    x86_hyper_xen -> x86_hyper_xen_hvm and x86_hyper_xen_pv
    xen_platform() -> xen_platform_hvm() and xen_platform_pv()
    xen_cpu_up_prepare() -> xen_cpu_up_prepare_pv() and xen_cpu_up_prepare_hvm()
    xen_cpu_dead() -> xen_cpu_dead_pv() and xen_cpu_dead_pv_hvm()
    
    Add two parameters to xen_cpuhp_setup() to pass proper cpu_up_prepare and
    cpu_dead hooks. xen_set_cpu_features() is now PV-only so the redundant
    xen_pv_domain() check can be dropped.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- arch/x86/kernel/cpu/hypervisor.c
+++ arch/x86/kernel/cpu/hypervisor.c
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const __initconst struct hypervisor_x86 * const hypervisors[] =
 {
 #ifdef CONFIG_XEN
-	&x86_hyper_xen,
+	&x86_hyper_xen_pv,
+	&x86_hyper_xen_hvm,
 #endif
 	&x86_hyper_vmware,
 	&x86_hyper_ms_hyperv,
