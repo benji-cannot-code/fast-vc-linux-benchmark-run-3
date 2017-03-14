2a001ede5360d8630dedd2bf32180392bbe34ae9
Stafford Horne <shorne@gmail.com>
2017-03-14 22:54:22 +0900

    openrisc: Switch to use export.h instead of module.h
    Reduce dependencies on module.h since all we need here is export.h
    for EXPORT_SYMBOL.
    
    Fixes: f50169324df4 ("module.h: split out the EXPORT_SYMBOL into export.h")
    Signed-off-by: Stafford Horne <shorne@gmail.com>

--- arch/openrisc/kernel/or32_ksyms.c
+++ arch/openrisc/kernel/or32_ksyms.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *      2 of the License, or (at your option) any later version.
  */
 
-#include <linux/module.h>
+#include <linux/export.h>
 #include <linux/elfcore.h>
 #include <linux/sched.h>
 #include <linux/in6.h>
