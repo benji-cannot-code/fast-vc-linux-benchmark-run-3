b566fa8fd9f69d3ace59e7178c7866273e2286d5
Tobin C. Harding <me@tobin.cc>
2017-03-15 10:44:29 +1100

    staging: dgnc: preserve return code
    Return code from tty_check_change() should be being
    preserved.
    
    Preserve return code from call to tty_check_change().
    
    Signed-off-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_tty.c
+++ drivers/staging/dgnc/dgnc_tty.c
@@ -2395,7 +2395,7 @@ static int dgnc_tty_ioctl(struct tty_struct *tty, unsigned int cmd,
 		rc = tty_check_change(tty);
 		spin_unlock_irqrestore(&ch->ch_lock, flags);
 		if (rc)
-			return -ENODEV;
+			return rc;
 
 		rc = ch_bd_ops->drain(tty, 0);
 		if (rc)
@@ -2420,7 +2420,7 @@ static int dgnc_tty_ioctl(struct tty_struct *tty, unsigned int cmd,
 		rc = tty_check_change(tty);
 		spin_unlock_irqrestore(&ch->ch_lock, flags);
 		if (rc)
-			return -ENODEV;
+			return rc;
 
 		rc = ch_bd_ops->drain(tty, 0);
 		if (rc)
@@ -2438,7 +2438,7 @@ static int dgnc_tty_ioctl(struct tty_struct *tty, unsigned int cmd,
 		rc = tty_check_change(tty);
 		spin_unlock_irqrestore(&ch->ch_lock, flags);
 		if (rc)
-			return -ENODEV;
+			return rc;
 
 		rc = ch_bd_ops->drain(tty, 0);
 		if (rc)
