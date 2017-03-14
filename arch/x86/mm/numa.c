11cc1a8bc9866ffbc777e668e34b481119df40cb
Wei Yang <richard.weiyang@gmail.com>
2017-03-14 11:08:00 +0800

    x86/mm/numa: Improve alloc_node_data() error path message
    alloc_node_data() tries to allocate from the local node first and, if
    that attempt fails, falls back to any node. Improve the error message to
    issue the initial node for ease during debugging.
    
    Fix a typo in the comments, while at it.
    
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    Link: http://lkml.kernel.org/r/20170314030801.13656-1-richard.weiyang@gmail.com
    [ Masssage commit message. ]
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/mm/numa.c
+++ arch/x86/mm/numa.c
@@ -202,7 +202,7 @@ static void __init alloc_node_data(int nid)
 		nd_pa = __memblock_alloc_base(nd_size, SMP_CACHE_BYTES,
 					      MEMBLOCK_ALLOC_ACCESSIBLE);
 		if (!nd_pa) {
-			pr_err("Cannot find %zu bytes in node %d\n",
+			pr_err("Cannot find %zu bytes in any node (initial node: %d)\n",
 			       nd_size, nid);
 			return;
 		}
@@ -226,7 +226,7 @@ static void __init alloc_node_data(int nid)
  * numa_cleanup_meminfo - Cleanup a numa_meminfo
  * @mi: numa_meminfo to clean up
  *
- * Sanitize @mi by merging and removing unncessary memblks.  Also check for
+ * Sanitize @mi by merging and removing unnecessary memblks.  Also check for
  * conflicts and clear unused memblks.
  *
  * RETURNS:
