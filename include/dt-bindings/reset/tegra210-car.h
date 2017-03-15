fc66d2d048bf5576f09fa22af7a40e6eaa6ae5ea
Peter De Schrijver <pdeschrijver@nvidia.com>
2017-03-15 14:59:32 +0200

    clk: tegra: Add Tegra210 special resets
    Tegra210 has 2 special resets which don't follow the normal pattern:
    DVCO and ADSP. Add them in this patch.
    
    Changelog:
    
    v2: add DT bindings file
    
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

---
+++ include/dt-bindings/reset/tegra210-car.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * This header provides Tegra210-specific constants for binding
+ * nvidia,tegra210-car.
+ */
+
+#ifndef _DT_BINDINGS_RESET_TEGRA210_CAR_H
+#define _DT_BINDINGS_RESET_TEGRA210_CAR_H
+
+#define TEGRA210_RESET(x)		(7 * 32 + (x))
+#define TEGRA210_RST_DFLL_DVCO		TEGRA210_RESET(0)
+#define TEGRA210_RST_ADSP		TEGRA210_RESET(1)
+
+#endif	/* _DT_BINDINGS_RESET_TEGRA210_CAR_H */
