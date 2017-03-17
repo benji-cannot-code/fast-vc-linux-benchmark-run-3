a629de635f5c8ced516364f5d64c4fb8ad268e07
Marc Gonzalez <marc_gonzalez@sigmadesigns.com>
2017-03-17 15:53:19 +0100

    PCI: Include pci.h for struct pci_ops definition
    struct pci_ecam_ops embeds a struct pci_ops.  Explicitly request the
    definition for struct pci_ops, otherwise gcc might complain:
    
      include/linux/pci-ecam.h:29:19: error: field 'pci_ops' has incomplete type
    
    Signed-off-by: Marc Gonzalez <marc_gonzalez@sigmadesigns.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- include/linux/pci-ecam.h
+++ include/linux/pci-ecam.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DRIVERS_PCI_ECAM_H
 #define DRIVERS_PCI_ECAM_H
 
+#include <linux/pci.h>
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 
