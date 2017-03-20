619206ec12f30b0d61d777b181837e94f15b5372
Arnd Bergmann <arnd@arndb.de>
2017-03-20 09:51:13 +0100

    net: dwc-xlgmac: add module license
    When building the driver as a module, we get a warning about the
    lack of a license:
    
    WARNING: modpost: missing MODULE_LICENSE() in drivers/net/ethernet/synopsys/dwc-xlgmac.o
    see include/linux/module.h for more information
    
    Curiously the text in the .c files only mentions GPLv2+, while the license
    tag in the PCI driver contains both GPL and BSD. I picked the license text
    as the more definite reference here and put a GPL tag in there.
    
    Fixes: 65e0ace2c5cd ("net: dwc-xlgmac: Initial driver for DesignWare Enterprise Ethernet")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/synopsys/dwc-xlgmac-common.c
+++ drivers/net/ethernet/synopsys/dwc-xlgmac-common.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int debug = -1;
 module_param(debug, int, 0644);
+MODULE_LICENSE("GPL");
 MODULE_PARM_DESC(debug, "DWC ethernet debug level (0=none,...,16=all)");
 static const u32 default_msg_level = (NETIF_MSG_LINK | NETIF_MSG_IFDOWN |
 				      NETIF_MSG_IFUP);
