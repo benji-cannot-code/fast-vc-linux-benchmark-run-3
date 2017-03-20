16492efa4417b33c5c2083315989ac04f6564193
Heiko Carstens <heiko.carstens@de.ibm.com>
2017-03-20 14:39:28 +0100

    s390/facilities: get rid of __ASSEMBLY__ in facility header file
    There is no need for the __ASSEMBLY__ ifdefery anymore since the
    architecture level set code that deals with facility bits was
    converted to C in the meantime.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/facility.h
+++ arch/s390/include/asm/facility.h
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_FACILITY_H
 
 #include <generated/facilities.h>
-
-#ifndef __ASSEMBLY__
-
 #include <linux/string.h>
 #include <linux/preempt.h>
 #include <asm/lowcore.h>
@@ -73,5 +70,4 @@ static inline void stfle(u64 *stfle_fac_list, int size)
 	preempt_enable();
 }
 
-#endif /* __ASSEMBLY__ */
 #endif /* __ASM_FACILITY_H */
