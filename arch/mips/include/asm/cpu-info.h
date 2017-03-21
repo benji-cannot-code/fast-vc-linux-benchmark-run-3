e7fb29fc07541dd0da4f84e3de62a97718f64b4c
Ralf Baechle <ralf@linux-mips.org>
2017-03-21 22:40:43 +0100

    MIPS: Delete redundant definition of SMP_CACHE_BYTES.
    <linux/cache.h> already defines SMP_CACHE_BYTES as L1_CACHE_BYTES.
    
    This change results in a build error in <asm/cpu-info.h> which directly
    includes <asm/cache.h>.  Fix this by including <linux/cache.h> instead.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/cpu-info.h
+++ arch/mips/include/asm/cpu-info.h
@@ -13,9 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_CPU_INFO_H
 #define __ASM_CPU_INFO_H
 
+#include <linux/cache.h>
 #include <linux/types.h>
-
-#include <asm/cache.h>
 
 /*
  * Descriptor for a cache
