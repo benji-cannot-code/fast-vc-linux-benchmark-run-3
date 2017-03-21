0ce1bde03a307577c6cca68ff5455b30690bc860
Dan Carpenter <dan.carpenter@oracle.com>
2017-03-21 23:43:05 +0300

    raid5-ppl: silence a misleading warning message
    The "need_cache_flush" variable is never set to false.  When the
    variable is true that means we print a warning message at the end of
    the function.
    
    Fixes: 3418d036c81d ("raid5-ppl: Partial Parity Log write logging implementation")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Artur Paszkiewicz <artur.paszkiewicz@intel.com>
    Signed-off-by: Shaohua Li <shli@fb.com>

--- drivers/md/raid5-ppl.c
+++ drivers/md/raid5-ppl.c
@@ -1071,7 +1071,7 @@ int ppl_init_log(struct r5conf *conf)
 	struct mddev *mddev = conf->mddev;
 	int ret = 0;
 	int i;
-	bool need_cache_flush;
+	bool need_cache_flush = false;
 
 	pr_debug("md/raid:%s: enabling distributed Partial Parity Log\n",
 		 mdname(conf->mddev));
