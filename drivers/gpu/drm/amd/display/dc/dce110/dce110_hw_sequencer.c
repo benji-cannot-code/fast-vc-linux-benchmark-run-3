7b4e894bf25ad2b71239880fb23a366bb0320df3
Yongqiang Sun <yongqiang.sun@amd.com>
2017-03-23 10:34:06 -0400

    drm/amd/display: Power on front end during set mode.
    Signed-off-by: Yongqiang Sun <yongqiang.sun@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
+++ drivers/gpu/drm/amd/display/dc/dce110/dce110_hw_sequencer.c
@@ -1723,6 +1723,9 @@ enum dc_status dce110_apply_ctx_to_hw(
 				context,
 				dc);
 
+		if (dc->hwss.power_on_front_end)
+			dc->hwss.power_on_front_end(dc, pipe_ctx, context);
+
 		if (DC_OK != status)
 			return status;
 	}
