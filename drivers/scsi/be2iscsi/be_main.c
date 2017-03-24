53cad7ccd0d37a71f789f5a205eda1732ec61aa1
Jitendra Bhivare <jitendra.bhivare@broadcom.com>
2017-03-24 14:11:42 +0530

    scsi: be2iscsi: Replace spin_unlock_bh with spin_lock
    spin_unlock_bh back_lock is used in beiscsi_eh_device_reset instead of
    spin_lock.
    
    Signed-off-by: Jitendra Bhivare <jitendra.bhivare@broadcom.com>
    Reviewed-by: Tomas Henzl <thenzl@redhat.com>
    Reviewed-by: Chris Leech <cleech@redhat.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/be2iscsi/be_main.c
+++ drivers/scsi/be2iscsi/be_main.c
@@ -338,7 +338,7 @@ static int beiscsi_eh_device_reset(struct scsi_cmnd *sc)
 		inv_tbl->task[nents] = task;
 		nents++;
 	}
-	spin_unlock_bh(&session->back_lock);
+	spin_unlock(&session->back_lock);
 	spin_unlock_bh(&session->frwd_lock);
 
 	rc = SUCCESS;
