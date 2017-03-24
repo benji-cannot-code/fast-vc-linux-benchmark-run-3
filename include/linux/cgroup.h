b4c041cfe5a3c5727fbbb862fc57624ac9e878f7
Geliang Tang <geliangtang@gmail.com>
2017-03-24 22:13:35 +0800

    cgroup: drop duplicate header nsproxy.h
    Drop duplicate header nsproxy.h from linux/cgroup.h.
    
    Signed-off-by: Geliang Tang <geliangtang@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/kernfs.h>
 #include <linux/jump_label.h>
-#include <linux/nsproxy.h>
 #include <linux/types.h>
 #include <linux/ns_common.h>
 #include <linux/nsproxy.h>
