b58383de9a89bf914fce09e29c65a0c8ed8d12c7
Saeed Mahameed <saeedm@mellanox.com>
2017-03-25 00:52:10 +0300

    net/mlx5e: Optimize XDP frame xmit
    XDP SQ has a fixed size WQE (MLX5E_XDP_TX_WQEBBS = 1) and only posts
    one kind of WQE (MLX5_OPCODE_SEND),
    
    Also we initialize SQ descriptors static fields once on open_xdpsq,
    rather than every time on critical path.
    
    Optimize the code in light of those facts and add a prefetch of the TX
    descriptor first thing in the xdp xmit function.
    
    Performance improvement:
    System: Intel(R) Xeon(R) CPU E5-2620 v3 @ 2.40GHz
    
    Test case              Before     Now        improvement
    ---------------------------------------------------------------
    XDP TX   (1 core)      13Mpps    13.7Mpps       5%
    
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Reviewed-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -117,12 +117,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(DIV_ROUND_UP(sizeof(struct mlx5e_umr_wqe), MLX5_SEND_WQE_BB))
 
 #define MLX5E_XDP_MIN_INLINE (ETH_HLEN + VLAN_HLEN)
-#define MLX5E_XDP_IHS_DS_COUNT \
-	DIV_ROUND_UP(MLX5E_XDP_MIN_INLINE - 2, MLX5_SEND_WQE_DS)
 #define MLX5E_XDP_TX_DS_COUNT \
 	((sizeof(struct mlx5e_tx_wqe) / MLX5_SEND_WQE_DS) + 1 /* SG DS */)
-#define MLX5E_XDP_TX_WQEBBS \
-	DIV_ROUND_UP(MLX5E_XDP_TX_DS_COUNT, MLX5_SEND_WQEBB_NUM_DS)
 
 #define MLX5E_NUM_MAIN_GROUPS 9
 
@@ -353,7 +349,6 @@ struct mlx5e_sq {
 		} txq;
 		struct mlx5e_sq_wqe_info *ico_wqe;
 		struct {
-			struct mlx5e_sq_wqe_info  *wqe_info;
 			struct mlx5e_dma_info     *di;
 			bool                       doorbell;
 		} xdp;
