2c3025d68c3bb00e5bab5de6a1bbec220020ac45
Alex Deucher <alexander.deucher@amd.com>
2017-03-27 16:52:40 -0400

    drm/amdgpu/gfx9: rename some functions
    To better match where they are used.  Called from sw_init
    and sw_fini.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v9_0.c
@@ -647,7 +647,7 @@ static void gfx_v9_0_kiq_free_ring(struct amdgpu_ring *ring,
 }
 
 /* create MQD for each compute queue */
-static int gfx_v9_0_compute_mqd_soft_init(struct amdgpu_device *adev)
+static int gfx_v9_0_compute_mqd_sw_init(struct amdgpu_device *adev)
 {
 	struct amdgpu_ring *ring = NULL;
 	int r, i;
@@ -685,7 +685,7 @@ static int gfx_v9_0_compute_mqd_soft_init(struct amdgpu_device *adev)
 	return 0;
 }
 
-static void gfx_v9_0_compute_mqd_soft_fini(struct amdgpu_device *adev)
+static void gfx_v9_0_compute_mqd_sw_fini(struct amdgpu_device *adev)
 {
 	struct amdgpu_ring *ring = NULL;
 	int i;
@@ -1113,7 +1113,7 @@ static int gfx_v9_0_sw_init(void *handle)
 			return r;
 
 		/* create MQD for all compute queues as wel as KIQ for SRIOV case */
-		r = gfx_v9_0_compute_mqd_soft_init(adev);
+		r = gfx_v9_0_compute_mqd_sw_init(adev);
 		if (r)
 			return r;
 	}
@@ -1164,7 +1164,7 @@ static int gfx_v9_0_sw_fini(void *handle)
 		amdgpu_ring_fini(&adev->gfx.compute_ring[i]);
 
 	if (amdgpu_sriov_vf(adev)) {
-		gfx_v9_0_compute_mqd_soft_fini(adev);
+		gfx_v9_0_compute_mqd_sw_fini(adev);
 		gfx_v9_0_kiq_free_ring(&adev->gfx.kiq.ring, &adev->gfx.kiq.irq);
 		gfx_v9_0_kiq_fini(adev);
 	}
