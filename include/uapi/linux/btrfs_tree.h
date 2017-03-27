81523eca197de01c6af1fa4098abbd1317d92cf6
Nicolas Dichtel <nicolas.dichtel@6wind.com>
2017-03-27 14:20:12 +0200

    btrfs_tree.h: fix include from userland
    This patch prepares the uapi export by fixing the following errors:
    
    .../linux/btrfs_tree.h:283:2: error: #error "UUID items require BTRFS_UUID_SIZE == 16!"
     #error "UUID items require BTRFS_UUID_SIZE == 16!"
    
    .../linux/btrfs_tree.h:390:12: error: ‘BTRFS_UUID_SIZE’ undeclared here (not in a function)
      __u8 uuid[BTRFS_UUID_SIZE];
                ^
    .../linux/btrfs_tree.h:796:16: error: ‘BTRFS_DEV_STAT_VALUES_MAX’ undeclared here (not in a function)
      __le64 values[BTRFS_DEV_STAT_VALUES_MAX];
    
    Signed-off-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- include/uapi/linux/btrfs_tree.h
+++ include/uapi/linux/btrfs_tree.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _BTRFS_CTREE_H_
 #define _BTRFS_CTREE_H_
 
+#include <linux/btrfs.h>
 #include <linux/types.h>
 
 /*
