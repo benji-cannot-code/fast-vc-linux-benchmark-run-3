b8cc23a3819476853e82d7a1e933871ee627f89f
David Kershner <david.kershner@unisys.com>
2017-03-28 09:34:54 -0400

    staging: unisys: visorbus: add error handling to chipset_device_pause/resume
    If there is an error in chipset_device_pause/resume don't try to respond,
    error out and let the calling functions respond to this error just like
    any other error they encounter.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorbus_main.c
+++ drivers/staging/unisys/visorbus/visorbus_main.c
@@ -1242,7 +1242,7 @@ initiate_chipset_device_pause_resume(struct visor_device *dev, bool is_pause)
  * that device.  Success/failure result is returned asynchronously
  * via a callback function; see pause_state_change_complete().
  */
-void
+int
 chipset_device_pause(struct visor_device *dev_info)
 {
 	int err;
@@ -1251,8 +1251,10 @@ chipset_device_pause(struct visor_device *dev_info)
 
 	if (err < 0) {
 		dev_info->pausing = false;
-		device_pause_response(dev_info, err);
+		return err;
 	}
+
+	return 0;
 }
 
 /**
@@ -1263,7 +1265,7 @@ chipset_device_pause(struct visor_device *dev_info)
  * that device.  Success/failure result is returned asynchronously
  * via a callback function; see resume_state_change_complete().
  */
-void
+int
 chipset_device_resume(struct visor_device *dev_info)
 {
 	int err;
@@ -1271,9 +1273,11 @@ chipset_device_resume(struct visor_device *dev_info)
 	err = initiate_chipset_device_pause_resume(dev_info, false);
 
 	if (err < 0) {
-		device_resume_response(dev_info, err);
 		dev_info->resuming = false;
+		return err;
 	}
+
+	return 0;
 }
 
 int
--- drivers/staging/unisys/visorbus/visorchipset.c
+++ drivers/staging/unisys/visorbus/visorchipset.c
@@ -884,7 +884,7 @@ my_device_changestate(struct controlvm_message *inmsg)
 	u32 dev_no = cmd->device_change_state.dev_no;
 	struct spar_segment_state state = cmd->device_change_state.state;
 	struct visor_device *dev_info;
-	int err;
+	int err = 0;
 
 	dev_info = visorbus_get_device_by_id(bus_no, dev_no, NULL);
 	if (!dev_info) {
@@ -919,7 +919,7 @@ my_device_changestate(struct controlvm_message *inmsg)
 	if (state.alive == segment_state_running.alive &&
 	    state.operating == segment_state_running.operating)
 		/* Response will be sent from chipset_device_resume */
-		chipset_device_resume(dev_info);
+		err = chipset_device_resume(dev_info);
 	/* ServerNotReady / ServerLost / SegmentStateStandby */
 	else if (state.alive == segment_state_standby.alive &&
 		 state.operating == segment_state_standby.operating)
@@ -927,7 +927,10 @@ my_device_changestate(struct controlvm_message *inmsg)
 		 * technically this is standby case where server is lost.
 		 * Response will be sent from chipset_device_pause.
 		 */
-		chipset_device_pause(dev_info);
+		err = chipset_device_pause(dev_info);
+	if (err)
+		goto err_respond;
+
 	return 0;
 
 err_respond:
