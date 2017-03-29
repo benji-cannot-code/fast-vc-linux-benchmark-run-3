2429b03beedab8525a5ff6e2e8bb3a4a85421596
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-03-29 20:43:53 +0200

    thermal: rcar_gen3_thermal: record and check number of TSCs found
    Record how many TSCs are found in struct rcar_gen3_thermal_priv, this is
    needed to be able to add hardware interrupts for trip points later. Also
    add a check to make sure at least one TSC is found.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/rcar_gen3_thermal.c
+++ drivers/thermal/rcar_gen3_thermal.c
@@ -76,6 +76,7 @@ struct rcar_gen3_thermal_tsc {
 
 struct rcar_gen3_thermal_priv {
 	struct rcar_gen3_thermal_tsc *tscs[TSC_MAX_NUM];
+	unsigned int num_tscs;
 };
 
 struct rcar_gen3_thermal_data {
@@ -306,6 +307,13 @@ static int rcar_gen3_thermal_probe(struct platform_device *pdev)
 			goto error_unregister;
 		}
 		tsc->zone = zone;
+	}
+
+	priv->num_tscs = i;
+
+	if (!priv->num_tscs) {
+		ret = -ENODEV;
+		goto error_unregister;
 	}
 
 	return 0;
