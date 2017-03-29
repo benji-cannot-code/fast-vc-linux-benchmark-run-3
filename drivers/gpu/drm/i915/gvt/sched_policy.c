7f9ae807ce2c5df89a023c9944b6f657dbe9f145
Ping Gao <ping.a.gao@intel.com>
2017-03-30 00:36:37 +0800

    drm/i915/gvt: define weight according to vGPU type
    The weight defines proportional control of physical GPU resource
    shared between vGPUs. So far the weight is tied to a specific vGPU
    type, i.e when creating multiple vGPUs with different types, they
    will inherit different weights.
    
    e.g. The weight of type GVTg_V5_2 is 8, the weight of type GVTg_V5_4
    is 4, so vGPU of type GVTg_V5_2 has double vGPU resource of vGPU type
    GVTg_V5_4.
    
    TODO: allow user control the weight setting in the future.
    
    Signed-off-by: Ping Gao <ping.a.gao@intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/sched_policy.c
+++ drivers/gpu/drm/i915/gvt/sched_policy.c
@@ -238,6 +238,7 @@ static int tbs_sched_init_vgpu(struct intel_vgpu *vgpu)
 	if (!data)
 		return -ENOMEM;
 
+	data->sched_ctl.weight = vgpu->sched_ctl.weight;
 	data->vgpu = vgpu;
 	INIT_LIST_HEAD(&data->lru_list);
 
