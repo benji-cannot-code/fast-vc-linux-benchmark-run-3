eb54cfbb5e4e25e9828e887b5438f08ac87524b5
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-03-30 15:45:57 +0900

    mtd: nand: denali: allow to override revision number
    Commit 271707b1d817 ("mtd: nand: denali: max_banks calculation
    changed in revision 5.1") added a revision check to support the
    new max_banks encoding.  Its git-log states "The encoding of
    max_banks changed in Denali revision 5.1".
    
    There are exceptional cases, for example, the revision register on
    some UniPhier SoCs says the IP is 5.0 but the max_banks is encoded
    in the new format.
    
    This IP updates the resister specification from time to time (often
    breaking the backward compatibility), but the revision number is not
    incremented correctly.
    
    The max_banks is not only the case that needs revision checking.
    Let's allow to override an incorrect revision number.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/denali_dt.c
+++ drivers/mtd/nand/denali_dt.c
@@ -31,6 +31,7 @@ struct denali_dt {
 };
 
 struct denali_dt_data {
+	unsigned int revision;
 	unsigned int caps;
 };
 
@@ -61,8 +62,10 @@ static int denali_dt_probe(struct platform_device *pdev)
 	denali = &dt->denali;
 
 	data = of_device_get_match_data(&pdev->dev);
-	if (data)
+	if (data) {
+		denali->revision = data->revision;
 		denali->caps = data->caps;
+	}
 
 	denali->platform = DT;
 	denali->dev = &pdev->dev;
