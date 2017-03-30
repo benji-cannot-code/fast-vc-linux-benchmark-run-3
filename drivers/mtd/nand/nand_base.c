7f0e14b746c803794bb05c760d34140f06860da9
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-03-30 15:45:47 +0900

    mtd: nand: relax ecc.read_page() return value for uncorrectable ECC
    The comment for ecc.read_page() requires that it should return
    "0 if bitflips uncorrectable".
    
    Actually, drivers could return positive values when uncorrectable
    bitflips occur.  For example, nand_read_page_swecc() is the case.
    If ecc.correct() returns -EBADMSG for the first ECC sector, and
    a positive value for the second one, nand_read_page_swecc() returns
    a positive max_bitflips and increments ecc_stats.failed for the same
    page.
    
    The requirement can be relaxed by tweaking nand_do_read_ops().
    Move the max_bitflips calculation below the retry.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Suggested-by: Boris Brezillon <boris.brezillon@free-electrons.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/nand_base.c
+++ drivers/mtd/nand/nand_base.c
@@ -1994,8 +1994,6 @@ read_retry:
 				break;
 			}
 
-			max_bitflips = max_t(unsigned int, max_bitflips, ret);
-
 			/* Transfer not aligned data */
 			if (use_bufpoi) {
 				if (!NAND_HAS_SUBPAGE_READ(chip) && !oob &&
@@ -2046,6 +2044,7 @@ read_retry:
 			}
 
 			buf += bytes;
+			max_bitflips = max_t(unsigned int, max_bitflips, ret);
 		} else {
 			memcpy(buf, chip->buffers->databuf + col, bytes);
 			buf += bytes;
