90abc3fc066043bf668fd9168d71f37a763463ef
Helmut Klein <hgkr.klein@gmail.com>
2017-03-31 18:54:34 +0200

    clk: meson-gxbb: expose UART clocks
    Expose the clock ids of the three none AO uarts to the dt-bindings
    
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Helmut Klein <hgkr.klein@gmail.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    [tidy the commit message to match similar change]
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- include/dt-bindings/clock/gxbb-clkc.h
+++ include/dt-bindings/clock/gxbb-clkc.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_I2C		22
 #define CLKID_SAR_ADC		23
 #define CLKID_RNG0		25
+#define CLKID_UART0		26
 #define CLKID_SPI		34
 #define CLKID_ETH		36
 #define CLKID_AIU_GLUE		38
@@ -25,12 +26,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_I2S_OUT		40
 #define CLKID_MIXER_IFACE	44
 #define CLKID_AIU		47
+#define CLKID_UART1		48
 #define CLKID_USB0		50
 #define CLKID_USB1		51
 #define CLKID_USB		55
 #define CLKID_HDMI_PCLK		63
 #define CLKID_USB1_DDR_BRIDGE	64
 #define CLKID_USB0_DDR_BRIDGE	65
+#define CLKID_UART2		68
 #define CLKID_SANA		69
 #define CLKID_GCLK_VENCI_INT0	77
 #define CLKID_AOCLK_GATE	80
