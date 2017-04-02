3768431ebb82a965d2fbb32704c2129356f51044
Helge Deller <deller@gmx.de>
2017-04-02 16:51:58 +0200

    parisc: Drop per_cpu uaccess related exception_data struct
    The last users have been migrated off by commits d19f5e41b344 ("parisc:
    Clean up fixup routines for get_user()/put_user()") and 554bfeceb8a2
    ("parisc: Fix access fault handling in pa_memcpy()").
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/mm/fault.c
+++ arch/parisc/mm/fault.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BITSSET		0x1c0	/* for identifying LDCW */
 
 
-DEFINE_PER_CPU(struct exception_data, exception_data);
-
 int show_unhandled_signals = 1;
 
 /*
@@ -144,13 +142,6 @@ int fixup_exception(struct pt_regs *regs)
 
 	fix = search_exception_tables(regs->iaoq[0]);
 	if (fix) {
-		struct exception_data *d;
-		d = this_cpu_ptr(&exception_data);
-		d->fault_ip = regs->iaoq[0];
-		d->fault_gp = regs->gr[27];
-		d->fault_space = regs->isr;
-		d->fault_addr = regs->ior;
-
 		/*
 		 * Fix up get_user() and put_user().
 		 * ASM_EXCEPTIONTABLE_ENTRY_EFAULT() sets the least-significant
