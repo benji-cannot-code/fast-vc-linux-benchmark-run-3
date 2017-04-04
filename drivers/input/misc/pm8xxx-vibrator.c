e06233b4c1e3cf5bbbda9741b6b152f652c80677
Damien Riegel <damien.riegel@savoirfairelinux.com>
2017-04-04 16:25:42 -0700

    Input: pm8xxx-vib - reorder header alphabetically
    Signed-off-by: Damien Riegel <damien.riegel@savoirfairelinux.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/pm8xxx-vibrator.c
+++ drivers/input/misc/pm8xxx-vibrator.c
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/errno.h>
-#include <linux/platform_device.h>
 #include <linux/input.h>
-#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
 #include <linux/regmap.h>
+#include <linux/slab.h>
 
 #define VIB_DRV			0x4A
 
