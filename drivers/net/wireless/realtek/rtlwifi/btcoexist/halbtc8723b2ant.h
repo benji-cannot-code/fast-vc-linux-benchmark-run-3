f31549cac3c2fe302cc0ccd0c1f5dde5898cf794
Yan-Hsuan Chuang <yhchuang@realtek.com>
2017-04-04 11:50:05 -0500

    rtlwifi: btcoex: 23b 2ant: turn off antenna when rssi is too high/low
    For 2-antenna combo card, the signal of the neighbor antenna could be
    over noise level and cause be severe interference. So we monitor the
    rssi and turn off one of the antennas when the other is transmitting
    and the rssi level is beyond a threshold.
    
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: Pkshih <pkshih@realtek.com>
    Cc: Birming Chiu <birming@realtek.com>
    Cc: Shaofu <shaofu@realtek.com>
    Cc: Steven Ting <steventing@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/btcoexist/halbtc8723b2ant.h
+++ drivers/net/wireless/realtek/rtlwifi/btcoexist/halbtc8723b2ant.h
@@ -42,6 +42,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define BTC_RSSI_COEX_THRESH_TOL_8723B_2ANT		2
 
+/* WiFi RSSI Threshold for 2-Ant TDMA/1-Ant PS-TDMA translation */
+#define BT_8723B_2ANT_WIFI_RSSI_COEXSWITCH_THRES	42
+/* BT RSSI Threshold for 2-Ant TDMA/1-Ant PS-TDMA translation */
+#define BT_8723B_2ANT_BT_RSSI_COEXSWITCH_THRES		46
+
 enum BT_INFO_SRC_8723B_2ANT {
 	BT_INFO_SRC_8723B_2ANT_WIFI_FW			= 0x0,
 	BT_INFO_SRC_8723B_2ANT_BT_RSP			= 0x1,
@@ -128,6 +133,8 @@ struct coex_dm_8723b_2ant {
 	u8 pre_rpwm;
 	u8 cur_rpwm;
 
+	bool is_switch_to_1dot5_ant;
+	u8 switch_thres_offset;
 	bool need_recover_0x948;
 	u16 backup_0x948;
 };
