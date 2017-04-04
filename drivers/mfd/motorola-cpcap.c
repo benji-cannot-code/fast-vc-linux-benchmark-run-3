0b158902c4644b386aa9e00ce8765595f555f307
Tony Lindgren <tony@atomide.com>
2017-04-03 20:15:55 -0700

    mfd: cpcap: Use ack_invert interrupts
    We should use ack_invert as the int_read_and_clear() in the Motorola
    kernel tree does "ireg_val & ~mreg_val" before writing to the mask
    register.
    
    Fixes: 56e1d40d3bea ("mfd: cpcap: Add minimal support")
    Tested-by: Sebastian Reichel <sre@kernel.org>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/motorola-cpcap.c
+++ drivers/mfd/motorola-cpcap.c
@@ -100,6 +100,7 @@ static struct regmap_irq_chip cpcap_irq_chip[CPCAP_NR_IRQ_CHIPS] = {
 		.ack_base = CPCAP_REG_MI1,
 		.mask_base = CPCAP_REG_MIM1,
 		.use_ack = true,
+		.ack_invert = true,
 	},
 	{
 		.name = "cpcap-m2",
@@ -108,6 +109,7 @@ static struct regmap_irq_chip cpcap_irq_chip[CPCAP_NR_IRQ_CHIPS] = {
 		.ack_base = CPCAP_REG_MI2,
 		.mask_base = CPCAP_REG_MIM2,
 		.use_ack = true,
+		.ack_invert = true,
 	},
 	{
 		.name = "cpcap1-4",
@@ -117,6 +119,7 @@ static struct regmap_irq_chip cpcap_irq_chip[CPCAP_NR_IRQ_CHIPS] = {
 		.mask_base = CPCAP_REG_INTM1,
 		.type_base = CPCAP_REG_INTS1,
 		.use_ack = true,
+		.ack_invert = true,
 	},
 };
 
