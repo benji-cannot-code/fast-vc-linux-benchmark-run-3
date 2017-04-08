6c61968a62ca452b6bb439f09767181e2903e5fb
Larry Finger <Larry.Finger@lwfinger.net>
2017-04-08 11:07:27 -0500

    staging: rtl8723bs: Fix indenting warning in os_dep/os_intfs.c
    Smatch logs the following warning:
    
      CHECK   drivers/staging/rtl8723bs/os_dep/os_intfs.c
    drivers/staging/rtl8723bs/os_dep/os_intfs.c:1082 ips_netdrv_open() warn: inconsistent indenting
    
    A simple change in the white space handles this warning.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/os_intfs.c
+++ drivers/staging/rtl8723bs/os_dep/os_intfs.c
@@ -1080,7 +1080,7 @@ static int  ips_netdrv_open(struct adapter *padapter)
 
 	_set_timer(&padapter->mlmepriv.dynamic_chk_timer, 2000);
 
-	 return _SUCCESS;
+	return _SUCCESS;
 
 netdev_open_error:
 	/* padapter->bup = false; */
