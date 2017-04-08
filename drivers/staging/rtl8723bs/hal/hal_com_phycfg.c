dd3a0c895708d497ab608ff7cad86a0093c6f7d4
Larry Finger <Larry.Finger@lwfinger.net>
2017-04-08 11:07:32 -0500

    staging: rtl8723bs: Fix indening problem in hal/hal_com_phycfg.c
    Smatch reports the following:
    
      CHECK   drivers/staging/rtl8723bs/hal/hal_com_phycfg.c
    drivers/staging/rtl8723bs/hal/hal_com_phycfg.c:2090 Hal_ChannelPlanToRegulation() warn: inconsistent indenting
    
    This warning is fixed with a white-space change.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/hal_com_phycfg.c
+++ drivers/staging/rtl8723bs/hal/hal_com_phycfg.c
@@ -2088,7 +2088,7 @@ void Hal_ChannelPlanToRegulation(struct adapter *Adapter, u16 ChannelPlan)
 	case RT_CHANNEL_DOMAIN_WORLD_FCC2:
 		pHalData->Regulation2_4G = TXPWR_LMT_FCC;
 		pHalData->Regulation5G = TXPWR_LMT_FCC;
-				break;
+		break;
 	case RT_CHANNEL_DOMAIN_WORLD_FCC3:
 		pHalData->Regulation2_4G = TXPWR_LMT_FCC;
 		pHalData->Regulation5G = TXPWR_LMT_FCC;
