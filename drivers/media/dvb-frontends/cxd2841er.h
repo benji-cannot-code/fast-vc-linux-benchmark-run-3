c22c5e18160d3c2b9a18fb5db695337e7718ec10
Daniel Scheller <d.scheller@gmx.net>
2017-04-09 16:38:16 -0300

    [media] dvb-frontends/cxd2841er: make call to i2c_gate_ctrl optional
    Some cards/bridges wrap i2c_gate_ctrl handling with a mutex_lock(). This is
    e.g. done in ddbridge to protect against concurrent tuner access with
    regards to the dual tuner HW, where concurrent tuner reconfiguration can
    result in tuning fails or bad reception quality. When the tuner driver
    additionally tries to open the I2C gate (which e.g. the tda18212 driver
    does) when the demod already did this, this will lead to a deadlock. This
    makes the calls to i2c_gatectrl from the demod driver optional when the
    flag is set, leaving this to the tuner driver. For readability reasons and
    to not have the check duplicated multiple times, the setup is factored
    into cxd2841er_tuner_set().
    
    This commit also updates the netup card driver (which seems to be the only
    consumer of the cxd2841er as of now).
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Acked-by: Abylay Ospan <aospan@netup.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/cxd2841er.h
+++ drivers/media/dvb-frontends/cxd2841er.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/dvb/frontend.h>
 
+#define CXD2841ER_USE_GATECTRL	1
+
 enum cxd2841er_xtal {
 	SONY_XTAL_20500, /* 20.5 MHz */
 	SONY_XTAL_24000, /* 24 MHz */
