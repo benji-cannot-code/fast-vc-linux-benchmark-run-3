bf0802133b50d18e09f622c3b85bebba112d05df
Daniel Scheller <d.scheller@gmx.net>
2017-04-09 16:38:28 -0300

    [media] ddbridge: hardware IDs for new C2T2 cards and other devices
    Adds hardware IDs for all Sony CXD-based Cine-cards and MaxA8 devices, also
    adds some other yet missing IDs like the Octopus V3, Octopus OEM and
    Octopus Mini, as well as cards with unknown/deleted sub-ids.
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/ddbridge/ddbridge.h
+++ drivers/media/pci/ddbridge/ddbridge.h
@@ -55,8 +55,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct ddb_info {
 	int   type;
-#define DDB_NONE         0
-#define DDB_OCTOPUS      1
+#define DDB_NONE		0
+#define DDB_OCTOPUS		1
+#define DDB_OCTOPUS_MAX_CT	6
 	char *name;
 	int   port_num;
 	u32   port_type[DDB_MAX_PORT];
