70555ad062e0cb640d6f74e4f063a39155aec8dd
Daniel Scheller <d.scheller@gmx.net>
2017-04-09 16:38:22 -0300

    [media] dvb-frontends/cxd2841er: configurable IFAGCNEG
    Adds a flag to enable or disable the IFAGCNEG bit in cxd2841er_init_tc().
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Acked-by: Abylay Ospan <aospan@netup.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/cxd2841er.h
+++ drivers/media/dvb-frontends/cxd2841er.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CXD2841ER_ASCOT		8	/* bit 3 */
 #define CXD2841ER_EARLY_TUNE	16	/* bit 4 */
 #define CXD2841ER_NO_WAIT_LOCK	32	/* bit 5 */
+#define CXD2841ER_NO_AGCNEG	64	/* bit 6 */
 
 enum cxd2841er_xtal {
 	SONY_XTAL_20500, /* 20.5 MHz */
