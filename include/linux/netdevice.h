341a201d19cf12303a4ff11e1e65ce11b3219421
Alexey Dobriyan <adobriyan@gmail.com>
2017-04-10 11:25:26 +0300

    net: make struct net_device::min_header_len 8-bit
    This field is never big enough to warrant 16-bitness.
    
    8-bit accesses enjoy shorted encoding on i386/x86_64 than 16-bit
    accesses:
    
    	add/remove: 0/0 grow/shrink: 0/2 up/down: 0/-10 (-10)
    	function                                     old     new   delta
    	loopback_setup                               169     164      -5
    	ether_setup                                  148     143      -5
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1716,7 +1716,7 @@ struct net_device {
 	unsigned int		max_mtu;
 	unsigned short		type;
 	unsigned short		hard_header_len;
-	unsigned short		min_header_len;
+	unsigned char		min_header_len;
 
 	unsigned short		needed_headroom;
 	unsigned short		needed_tailroom;
