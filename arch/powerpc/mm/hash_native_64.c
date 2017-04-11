622c25e4a8ab503ecc430d67cdb05a172ca626e7
Balbir Singh <bsingharora@gmail.com>
2017-04-11 15:23:25 +1000

    powerpc/mm: Trace tlbie(l) instructions
    Add a trace point for tlbie(l) (Translation Lookaside Buffer Invalidate
    Entry (Local)) instructions.
    
    The tlbie instruction has changed over the years, so not all versions
    accept the same operands. Use the ISA v3 field operands because they are
    the most verbose, we may change them in future.
    
    Example output:
    
      qemu-system-ppc-5371  [016]  1412.369519: tlbie:
      	tlbie with lpid 0, local 1, rb=67bd8900174c11c1, rs=0, ric=0 prs=0 r=0
    
    Signed-off-by: Balbir Singh <bsingharora@gmail.com>
    [mpe: Add some missing trace_tlbie()s, reword change log]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/hash_native_64.c
+++ arch/powerpc/mm/hash_native_64.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mmu_context.h>
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
+#include <asm/trace.h>
 #include <asm/tlb.h>
 #include <asm/cputable.h>
 #include <asm/udbg.h>
@@ -99,6 +100,7 @@ static inline void __tlbie(unsigned long vpn, int psize, int apsize, int ssize)
 			     : "memory");
 		break;
 	}
+	trace_tlbie(0, 0, va, 0, 0, 0, 0);
 }
 
 static inline void __tlbiel(unsigned long vpn, int psize, int apsize, int ssize)
@@ -148,6 +150,7 @@ static inline void __tlbiel(unsigned long vpn, int psize, int apsize, int ssize)
 			     : "memory");
 		break;
 	}
+	trace_tlbie(0, 1, va, 0, 0, 0, 0);
 
 }
 
