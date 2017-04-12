a57d9462e0633f0c763e4d38fe8f12a5bca4cf47
Joerg Roedel <jroedel@suse.de>
2017-04-12 00:21:29 -0500

    iommu/omap: Store iommu_dev pointer in arch_data
    Instead of finding the matching IOMMU for a device using
    string comparision functions, store the pointer to the
    iommu_dev in arch_data during the omap_iommu_add_device
    callback and reset it during the omap_iommu_remove_device
    callback functions.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    [s-anna@ti.com: few minor cleanups]
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/omap-iommu.h
+++ drivers/iommu/omap-iommu.h
@@ -73,7 +73,6 @@ struct omap_iommu {
 
 /**
  * struct omap_iommu_arch_data - omap iommu private data
- * @name: name of the iommu device
  * @iommu_dev: handle of the iommu device
  *
  * This is an omap iommu private data object, which binds an iommu user
@@ -82,7 +81,6 @@ struct omap_iommu {
  * utilize omap-specific plumbing anymore.
  */
 struct omap_iommu_arch_data {
-	const char *name;
 	struct omap_iommu *iommu_dev;
 };
 
