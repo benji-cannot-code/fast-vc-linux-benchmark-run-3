a8f0d3191db4ef735c8918f21fbe1937f9593b02
Xinming Hu <huxm@marvell.com>
2017-04-13 13:10:33 -0700

    mwifiex: pcie: extract wifi part from combo firmware during function level reset
    A separate wifi-only firmware was download during pcie function level
    reset. It is in fact the tail part of wifi/bt combo firmware. Per
    Brian's and Dmitry's suggestion, this patch extract the wifi part from
    combo firmware.
    
    After that, the mrvl/pcie8997_wlan_v4.bin image in linux-firmware repo
    is redundant (though I guess we keep it around to support older
    kernels).
    
    Signed-off-by: Xinming Hu <huxm@marvell.com>
    Signed-off-by: Ganapathi Bhat <gbhat@marvell.com>
    Signed-off-by: Cathy Luo <cluo@marvell.com>
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/marvell/mwifiex/pcie.h
+++ drivers/net/wireless/marvell/mwifiex/pcie.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCIE8897_B0_FW_NAME "mrvl/pcie8897_uapsta.bin"
 #define PCIEUART8997_FW_NAME_V4 "mrvl/pcieuart8997_combo_v4.bin"
 #define PCIEUSB8997_FW_NAME_V4 "mrvl/pcieusb8997_combo_v4.bin"
-#define PCIE8997_DEFAULT_WIFIFW_NAME "mrvl/pcie8997_wlan_v4.bin"
 
 #define PCIE_VENDOR_ID_MARVELL              (0x11ab)
 #define PCIE_VENDOR_ID_V2_MARVELL           (0x1b4b)
@@ -120,6 +119,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MWIFIEX_DEF_SLEEP_COOKIE			0xBEEFBEEF
 #define MWIFIEX_SLEEP_COOKIE_SIZE			4
 #define MWIFIEX_MAX_DELAY_COUNT				100
+
+#define MWIFIEX_PCIE_FLR_HAPPENS 0xFEDCBABA
 
 struct mwifiex_pcie_card_reg {
 	u16 cmd_addr_lo;
