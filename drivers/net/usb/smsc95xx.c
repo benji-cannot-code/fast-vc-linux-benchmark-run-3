a00935bacb281e26cc51aeefbcd6de44968d9860
Martin Wetterwald <martin@wetterwald.eu>
2017-04-13 10:08:44 +0200

    smsc95xx: Add comments to the registers definition
    This chip is used by a lot of embedded devices and also by the Raspberry
    Pi 1, 2 & 3 which were created to promote the study of computer
    sciences. Students wanting to learn kernel / network device driver
    programming through those devices can only rely on the Linux kernel
    driver source to make their own.
    
    This commit adds a lot of comments to the registers definition to expand
    the register names.
    
    Cc: Steve Glendinning <steve.glendinning@shawell.net>
    Cc: Microchip Linux Driver Support <UNGLinuxDriver@microchip.com>
    CC: David Miller <davem@davemloft.net>
    Signed-off-by: Martin Wetterwald <martin@wetterwald.eu>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Acked-by: Steve Glendinning <steve.glendinning@shawell.net>
    Acked-by: Woojung Huh <Woojung.Huh@microchip.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/smsc95xx.c
+++ drivers/net/usb/smsc95xx.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "smsc95xx.h"
 
 #define SMSC_CHIPNAME			"smsc95xx"
-#define SMSC_DRIVER_VERSION		"1.0.5"
+#define SMSC_DRIVER_VERSION		"1.0.6"
 #define HS_USB_PKT_SIZE			(512)
 #define FS_USB_PKT_SIZE			(64)
 #define DEFAULT_HS_BURST_CAP_SIZE	(16 * 1024 + 5 * HS_USB_PKT_SIZE)
@@ -1500,7 +1500,7 @@ static int smsc95xx_enter_suspend3(struct usbnet *dev)
 	if (ret < 0)
 		return ret;
 
-	if (val & 0xFFFF) {
+	if (val & RX_FIFO_INF_USED_) {
 		netdev_info(dev->net, "rx fifo not empty in autosuspend\n");
 		return -EBUSY;
 	}
