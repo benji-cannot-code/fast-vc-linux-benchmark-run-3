23b6f3d51eb48efa4cbb29beb206590f8f3bcf8f
Hans de Goede <hdegoede@redhat.com>
2017-04-14 20:32:51 +0200

    power: supply: max17042_battery: Add support for the STATUS property
    Userspace prefers the driver having a status property over having to guess
    itself. Specifically this will properly make the GNOME3 UI (and likely
    others) properly show discharging / charging / full status, instead
    of always showing discharging as status.
    
    Note that in the case there is no charger driver supplying the max17042,
    then a status of unknown will get returned. At least upower treats
    this the same as not having a status attribute, so in this case nothing
    changes from a userspace pov.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>

--- include/linux/power/max17042_battery.h
+++ include/linux/power/max17042_battery.h
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX17042_DEFAULT_TEMP_MIN	(0)    /* For sys without temp sensor */
 #define MAX17042_DEFAULT_TEMP_MAX	(700)  /* 70 degrees Celcius */
 
+/* Consider RepCap which is less then 10 units below FullCAP full */
+#define MAX17042_FULL_THRESHOLD		10
+
 #define MAX17042_CHARACTERIZATION_DATA_SIZE 48
 
 enum max17042_register {
