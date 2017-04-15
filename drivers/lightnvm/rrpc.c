376baf7c0de9ed7c121b924153b17f9cb462f6d1
Javier González <jg@lightnvm.io>
2017-04-15 20:55:45 +0200

    lightnvm: allow to init targets on factory mode
    Target initialization has two responsibilities: creating the target
    partition and instantiating the target. This patch enables to create a
    factory partition (e.g., do not trigger recovery on the given target).
    This is useful for target development and for being able to restore the
    device state at any moment in time without requiring a full-device
    erase.
    
    Signed-off-by: Javier González <javier@cnexlabs.com>
    Signed-off-by: Matias Bjørling <matias@cnexlabs.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/lightnvm/rrpc.c
+++ drivers/lightnvm/rrpc.c
@@ -1507,7 +1507,8 @@ err:
 
 static struct nvm_tgt_type tt_rrpc;
 
-static void *rrpc_init(struct nvm_tgt_dev *dev, struct gendisk *tdisk)
+static void *rrpc_init(struct nvm_tgt_dev *dev, struct gendisk *tdisk,
+		       int flags)
 {
 	struct request_queue *bqueue = dev->q;
 	struct request_queue *tqueue = tdisk->queue;
