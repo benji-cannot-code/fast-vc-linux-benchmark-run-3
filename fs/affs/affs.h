f770e2aaa7ec201d96741c5fee20c0f78a290c4c
Fabian Frederick <fabf@skynet.be>
2017-04-15 08:54:56 +0200

    fs/affs: import amigaffs.h
    Have that file in global include/linux is not needed.
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/affs/affs.h
+++ fs/affs/affs.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/buffer_head.h>
-#include <linux/amigaffs.h>
+#include "amigaffs.h"
 #include <linux/mutex.h>
 #include <linux/workqueue.h>
 
