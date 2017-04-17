9a1785a3e37d3053a6dc6962baf7f8396578fcdf
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-04-17 15:25:00 -0300

    perf tools: Remove PRI[xu] macros from perf.h
    We get them from inttypes.h.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-qla4e4mwbf1oewafp1ee2etd@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <poll.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
-#include <inttypes.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <sys/ttydefaults.h>
@@ -50,25 +49,6 @@ extern const char *graph_dotted_line;
 extern const char *spaces;
 extern const char *dots;
 extern char buildid_dir[];
-
-/* On most systems <limits.h> would have given us this, but
- * not on some systems (e.g. GNU/Hurd).
- */
-#ifndef PATH_MAX
-#define PATH_MAX 4096
-#endif
-
-#ifndef PRIuMAX
-#define PRIuMAX "llu"
-#endif
-
-#ifndef PRIu32
-#define PRIu32 "u"
-#endif
-
-#ifndef PRIx32
-#define PRIx32 "x"
-#endif
 
 #ifndef PATH_SEP
 #define PATH_SEP ':'
