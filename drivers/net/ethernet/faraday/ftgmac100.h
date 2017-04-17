242d0315258f3deafd2dacfdef34620cb20a6454
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2017-04-18 08:36:59 +1000

    ftgmac100: Add pause frames configuration and support
    Hopefully my understanding of how the hardware works is correct,
    as the documentation isn't completely clear. So far I have seen
    no obvious issue. Pause seem to also work with NC-SI.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/faraday/ftgmac100.h
+++ drivers/net/ethernet/faraday/ftgmac100.h
@@ -200,6 +200,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FTGMAC100_PHYDATA_MIIRDATA(phydata)	(((phydata) >> 16) & 0xffff)
 
 /*
+ * Flow control register
+ */
+#define FTGMAC100_FCR_FC_EN		(1 << 0)
+#define FTGMAC100_FCR_FCTHR_EN		(1 << 2)
+#define FTGMAC100_FCR_PAUSE_TIME(x)	(((x) & 0xffff) << 16)
+
+/*
  * Transmit descriptor, aligned to 16 bytes
  */
 struct ftgmac100_txdes {
