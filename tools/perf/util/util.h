8c44b35c83b0c6f943d55013374444b8457a3d2d
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-04-18 11:42:23 -0300

    perf tools: No need to include bitops.h in util.h
    When we switched to the kernel's roundup_pow_of_two we forgot to remove
    this include from util.h, do it now.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Fixes: 91529834d1de ("perf evlist: Use roundup_pow_of_two")
    Link: http://lkml.kernel.org/n/tip-kfye5rxivib6155cltx0bw4h@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <api/fs/tracing_path.h>
-#include <linux/bitops.h>
 
 extern char buildid_dir[];
 
