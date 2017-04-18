253c34dd7e649e6113faccd05fcfe9005ef77945
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-04-18 12:33:30 -0300

    perf tools: Remove regex.h and fnmatch.h from util.h
    The users of regex and fnmatch functions should include those headers
    instead.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-ixzm5kuamsq1ixbkuv6kmwzj@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -24,9 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/time.h>
 #include <time.h>
 #include <signal.h>
-#include <fnmatch.h>
 #include <assert.h>
-#include <regex.h>
 #include <utime.h>
 #include <sys/wait.h>
 #include <poll.h>
