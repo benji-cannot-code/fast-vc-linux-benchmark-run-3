5dd3da1e59d202cae9d6dd7b2b6ecb45551299dc
Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
2017-04-19 20:59:51 +0530

    powerpc/kprobes: Blacklist exception handlers
    Introduce __head_end to mark end of the early fixed sections and use it to
    blacklist all exception handlers from kprobes.
    
    mpe: We do not need to do anything special for relocatable kernels, where the
    exception vectors are split from the main kernel, as the split vectors are
    already excluded by the check for kernel_text_address().
    
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    [mpe: Move __head_end outside #ifdef 64-bit to unbreak the 32-bit build]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/kprobes.c
+++ arch/powerpc/kernel/kprobes.c
@@ -36,12 +36,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/code-patching.h>
 #include <asm/cacheflush.h>
 #include <asm/sstep.h>
+#include <asm/sections.h>
 #include <linux/uaccess.h>
 
 DEFINE_PER_CPU(struct kprobe *, current_kprobe) = NULL;
 DEFINE_PER_CPU(struct kprobe_ctlblk, kprobe_ctlblk);
 
 struct kretprobe_blackpoint kretprobe_blacklist[] = {{NULL, NULL}};
+
+bool arch_within_kprobe_blacklist(unsigned long addr)
+{
+	return  (addr >= (unsigned long)__kprobes_text_start &&
+		 addr < (unsigned long)__kprobes_text_end) ||
+		(addr >= (unsigned long)_stext &&
+		 addr < (unsigned long)__head_end);
+}
 
 int arch_prepare_kprobe(struct kprobe *p)
 {
