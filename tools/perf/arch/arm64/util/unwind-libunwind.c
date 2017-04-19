4b869339d44d425db99a010cb6d936d1fd7b6817
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-04-19 12:49:58 -0300

    perf unwind arm64: Add missing errno.h header
    Since it uses EINVAL unconditionally, it needs to also unconditionally
    include errno.h.
    
    Detected when recent changes made errno.h not be included by chance when
    tools/perf/arch/arm64/util/unwind-libunwind.c gets included by
    tools/perf/util/libunwind/arm64.c.
    
    Putting this changeset just before that change so that we don't lose
    bisectability on arm64.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jean Pihet <jean.pihet@linaro.org>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Fixes: 8ab596afb97b ("perf tools ARM64: Wire up perf_regs and unwind support")
    Link: http://lkml.kernel.org/n/tip-60zjev2o1locp5ivod38epa2@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/arm64/util/unwind-libunwind.c
+++ tools/perf/arch/arm64/util/unwind-libunwind.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <errno.h>
 
 #ifndef REMOTE_UNWIND_LIBUNWIND
-#include <errno.h>
 #include <libunwind.h>
 #include "perf_regs.h"
 #include "../../util/unwind.h"
