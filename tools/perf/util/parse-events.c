b30b1916155265d2d0972b7c0c51c96999adac0d
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-04-19 19:03:14 -0300

    perf tools: Remove sys/ioctl.h from util.h
    Not needed in this header, added to the places that need 'struct
    winsize' and the ioctl defines.
    
    Link: http://lkml.kernel.org/n/tip-2pznlli3146y4242otlcm70m@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <dirent.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 #include <sys/param.h>
 #include "term.h"
 #include "../perf.h"
--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <string.h>
 #include <sys/wait.h>
 #include <poll.h>
-#include <sys/ioctl.h>
 #include <linux/types.h>
 
 extern char buildid_dir[];
