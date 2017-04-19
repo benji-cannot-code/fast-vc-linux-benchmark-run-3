8f5e3284f4eeacd55a2b394071a99170e6914d1f
Paul Gortmaker <paul.gortmaker@windriver.com>
2017-04-19 15:59:05 -0400

    pinctrl: make artpec6 explicitly non-modular
    The Kconfig currently controlling compilation of this code is:
    
    drivers/pinctrl/Kconfig:config PINCTRL_ARTPEC6
    drivers/pinctrl/Kconfig:        bool "Axis ARTPEC-6 pin controller driver"
    
    ...meaning that it currently is not being built as a module by anyone.
    
    Lets remove the modular code that is essentially orphaned, so that
    when reading the driver there is no doubt it is builtin-only.
    
    Also note that MODULE_DEVICE_TABLE is a no-op for non-modular code.
    
    We also delete the MODULE_LICENSE tag etc. since all that information
    is already contained at the top of the file in the comments.
    
    Cc: Lars Persson <lars.persson@axis.com>
    Cc: Niklas Cassel <niklas.cassel@axis.com>
    Cc: linux-arm-kernel@axis.com
    Cc: linux-gpio@vger.kernel.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Acked-by: Jesper Nilsson <jesper.nilsson@axis.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/pinctrl-artpec6.c
+++ drivers/pinctrl/pinctrl-artpec6.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/init.h>
 #include <linux/io.h>
-#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/pinctrl/pinctrl.h>
@@ -980,14 +979,3 @@ static int __init artpec6_pmx_init(void)
 	return platform_driver_register(&artpec6_pmx_driver);
 }
 arch_initcall(artpec6_pmx_init);
-
-static void __exit artpec6_pmx_exit(void)
-{
-	platform_driver_unregister(&artpec6_pmx_driver);
-}
-module_exit(artpec6_pmx_exit);
-
-MODULE_AUTHOR("Chris Paterson <chris.paterson@linux.pieboy.co.uk>");
-MODULE_DESCRIPTION("Axis ARTPEC-6 pin control driver");
-MODULE_LICENSE("GPL v2");
-MODULE_DEVICE_TABLE(of, artpec6_pinctrl_match);
