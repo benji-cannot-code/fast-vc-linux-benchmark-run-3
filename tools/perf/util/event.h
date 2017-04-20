55be2db9a7e8df12ad6df5e01a85d2334238b2c4
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-04-19 21:33:07 -0300

    perf tools: Add the right header to obtain PERF_ALIGN()
    The util/event.h header needs PERF_ALIGN(), but wasn't including
    linux/kernel.h, where it is defined, instead it was getting it by
    luck by including map.h, which it doesn't need at all.
    
    Fix it by including the right header.
    
    Link: http://lkml.kernel.org/n/tip-nf3t9blzm5ncoxsczi8oy9mx@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/event.h
+++ tools/perf/util/event.h
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <limits.h>
 #include <stdio.h>
+#include <linux/kernel.h>
 
 #include "../perf.h"
-#include "map.h"
 #include "build-id.h"
 #include "perf_regs.h"
 
