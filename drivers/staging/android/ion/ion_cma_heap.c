dad2aa30f3cab21e17bfa5e53766848ee518829f
Paolo Cretaro <melko@frugalware.org>
2017-04-22 00:47:59 +0200

    staging: android: ion: Replace spaces with tabs for indentation
    Fix checkpatch ERRORs: code indent should use tabs where possible.
    
    Signed-off-by: Paolo Cretaro <melko@frugalware.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion_cma_heap.c
+++ drivers/staging/android/ion/ion_cma_heap.c
@@ -109,7 +109,7 @@ static struct ion_heap *__ion_cma_heap_create(struct cma *cma)
 
 int __ion_add_cma_heaps(struct cma *cma, void *data)
 {
-        struct ion_heap *heap;
+	struct ion_heap *heap;
 
 	heap = __ion_cma_heap_create(cma);
 	if (IS_ERR(heap))
@@ -117,8 +117,8 @@ int __ion_add_cma_heaps(struct cma *cma, void *data)
 
 	heap->name = cma_get_name(cma);
 
-        ion_device_add_heap(heap);
-        return 0;
+	ion_device_add_heap(heap);
+	return 0;
 }
 
 static int ion_add_cma_heaps(void)
