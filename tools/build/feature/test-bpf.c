3a96c650159df8d78cadac546aa69ab2e21b8908
David S. Miller <davem@davemloft.net>
2017-04-22 12:31:05 -0700

    bpf: Add sparc support to tools and samples.
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/build/feature/test-bpf.c
+++ tools/build/feature/test-bpf.c
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  define __NR_bpf 321
 # elif defined(__aarch64__)
 #  define __NR_bpf 280
+# elif defined(__sparc__)
+#  define __NR_bpf 349
+# else
 #  error __NR_bpf not defined. libbpf does not support your arch.
 # endif
 #endif
