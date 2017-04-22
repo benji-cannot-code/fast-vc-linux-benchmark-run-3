807297dd7286f4c66aaad6a7b3d28425343c84ad
Leon Romanovsky <leon@kernel.org>
2017-04-22 17:28:49 +0300

    Ib/core: Mark local uverbs_std_types functions to be static
    Functions declared in uverbs_std_types.c are local to that file, but
    they lack static declarations. This produces a lot of sparse warnings,
    like the one below:
    
    drivers/infiniband/core/uverbs_std_types.c:41:5: warning: symbol
    				'uverbs_free_ah' was not declared.
    				Should it be static?
    
    So mark them as static.
    
    CC: Matan Barak <matanb@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/core/uverbs_std_types.c
+++ drivers/infiniband/core/uverbs_std_types.c
@@ -39,26 +39,26 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rdma_core.h"
 #include "uverbs.h"
 
-int uverbs_free_ah(struct ib_uobject *uobject,
-		   enum rdma_remove_reason why)
+static int uverbs_free_ah(struct ib_uobject *uobject,
+			  enum rdma_remove_reason why)
 {
 	return ib_destroy_ah((struct ib_ah *)uobject->object);
 }
 
-int uverbs_free_flow(struct ib_uobject *uobject,
-		     enum rdma_remove_reason why)
+static int uverbs_free_flow(struct ib_uobject *uobject,
+			    enum rdma_remove_reason why)
 {
 	return ib_destroy_flow((struct ib_flow *)uobject->object);
 }
 
-int uverbs_free_mw(struct ib_uobject *uobject,
-		   enum rdma_remove_reason why)
+static int uverbs_free_mw(struct ib_uobject *uobject,
+			  enum rdma_remove_reason why)
 {
 	return uverbs_dealloc_mw((struct ib_mw *)uobject->object);
 }
 
-int uverbs_free_qp(struct ib_uobject *uobject,
-		   enum rdma_remove_reason why)
+static int uverbs_free_qp(struct ib_uobject *uobject,
+			  enum rdma_remove_reason why)
 {
 	struct ib_qp *qp = uobject->object;
 	struct ib_uqp_object *uqp =
@@ -83,8 +83,8 @@ int uverbs_free_qp(struct ib_uobject *uobject,
 	return ret;
 }
 
-int uverbs_free_rwq_ind_tbl(struct ib_uobject *uobject,
-			    enum rdma_remove_reason why)
+static int uverbs_free_rwq_ind_tbl(struct ib_uobject *uobject,
+				   enum rdma_remove_reason why)
 {
 	struct ib_rwq_ind_table *rwq_ind_tbl = uobject->object;
 	struct ib_wq **ind_tbl = rwq_ind_tbl->ind_tbl;
@@ -96,8 +96,8 @@ int uverbs_free_rwq_ind_tbl(struct ib_uobject *uobject,
 	return ret;
 }
 
-int uverbs_free_wq(struct ib_uobject *uobject,
-		   enum rdma_remove_reason why)
+static int uverbs_free_wq(struct ib_uobject *uobject,
+			  enum rdma_remove_reason why)
 {
 	struct ib_wq *wq = uobject->object;
 	struct ib_uwq_object *uwq =
@@ -110,8 +110,8 @@ int uverbs_free_wq(struct ib_uobject *uobject,
 	return ret;
 }
 
-int uverbs_free_srq(struct ib_uobject *uobject,
-		    enum rdma_remove_reason why)
+static int uverbs_free_srq(struct ib_uobject *uobject,
+			   enum rdma_remove_reason why)
 {
 	struct ib_srq *srq = uobject->object;
 	struct ib_uevent_object *uevent =
@@ -135,8 +135,8 @@ int uverbs_free_srq(struct ib_uobject *uobject,
 	return ret;
 }
 
-int uverbs_free_cq(struct ib_uobject *uobject,
-		   enum rdma_remove_reason why)
+static int uverbs_free_cq(struct ib_uobject *uobject,
+			  enum rdma_remove_reason why)
 {
 	struct ib_cq *cq = uobject->object;
 	struct ib_uverbs_event_queue *ev_queue = cq->cq_context;
@@ -154,14 +154,14 @@ int uverbs_free_cq(struct ib_uobject *uobject,
 	return ret;
 }
 
-int uverbs_free_mr(struct ib_uobject *uobject,
-		   enum rdma_remove_reason why)
+static int uverbs_free_mr(struct ib_uobject *uobject,
+			  enum rdma_remove_reason why)
 {
 	return ib_dereg_mr((struct ib_mr *)uobject->object);
 }
 
-int uverbs_free_xrcd(struct ib_uobject *uobject,
-		     enum rdma_remove_reason why)
+static int uverbs_free_xrcd(struct ib_uobject *uobject,
+			    enum rdma_remove_reason why)
 {
 	struct ib_xrcd *xrcd = uobject->object;
 	struct ib_uxrcd_object *uxrcd =
@@ -179,8 +179,8 @@ int uverbs_free_xrcd(struct ib_uobject *uobject,
 	return ret;
 }
 
-int uverbs_free_pd(struct ib_uobject *uobject,
-		   enum rdma_remove_reason why)
+static int uverbs_free_pd(struct ib_uobject *uobject,
+			  enum rdma_remove_reason why)
 {
 	struct ib_pd *pd = uobject->object;
 
@@ -191,8 +191,8 @@ int uverbs_free_pd(struct ib_uobject *uobject,
 	return 0;
 }
 
-int uverbs_hot_unplug_completion_event_file(struct ib_uobject_file *uobj_file,
-					    enum rdma_remove_reason why)
+static int uverbs_hot_unplug_completion_event_file(struct ib_uobject_file *uobj_file,
+						   enum rdma_remove_reason why)
 {
 	struct ib_uverbs_completion_event_file *comp_event_file =
 		container_of(uobj_file, struct ib_uverbs_completion_event_file,
