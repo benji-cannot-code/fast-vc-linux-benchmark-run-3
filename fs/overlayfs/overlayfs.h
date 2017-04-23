e55a7f34cebc5f942e6eaca357d169b671b87548
Amir Goldstein <amir73il@gmail.com>
2017-04-23 23:12:34 +0300

    ovl: set the ORIGIN type flag
    For directory entries, non zero oe->numlower implies OVL_TYPE_MERGE.
    Define a new type flag OVL_TYPE_ORIGIN to indicate that an entry holds a
    reference to its lower copy up origin.
    
    For directory entries ORIGIN := MERGE && UPPER. For non-dir entries ORIGIN
    means that a lower type dentry has been recently copied up or that we were
    able to find the copy up origin from overlay.origin xattr.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/overlayfs.h
+++ fs/overlayfs/overlayfs.h
@@ -14,10 +14,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum ovl_path_type {
 	__OVL_PATH_UPPER	= (1 << 0),
 	__OVL_PATH_MERGE	= (1 << 1),
+	__OVL_PATH_ORIGIN	= (1 << 2),
 };
 
 #define OVL_TYPE_UPPER(type)	((type) & __OVL_PATH_UPPER)
 #define OVL_TYPE_MERGE(type)	((type) & __OVL_PATH_MERGE)
+#define OVL_TYPE_ORIGIN(type)	((type) & __OVL_PATH_ORIGIN)
 
 #define OVL_XATTR_PREFIX XATTR_TRUSTED_PREFIX "overlay."
 #define OVL_XATTR_OPAQUE OVL_XATTR_PREFIX "opaque"
