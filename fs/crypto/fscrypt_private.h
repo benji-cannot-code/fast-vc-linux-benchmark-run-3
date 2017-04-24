bfa59b4c4a8e8b5f587c5ae061e79295aefa9aeb
Eric Biggers <ebiggers@google.com>
2017-04-24 10:00:10 -0700

    fscrypt: introduce helper function for filename matching
    Introduce a helper function fscrypt_match_name() which tests whether a
    fscrypt_name matches a directory entry.  Also clean up the magic numbers
    and document things properly.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/crypto/fscrypt_private.h
+++ fs/crypto/fscrypt_private.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/fscrypt_supp.h>
 
-#define FS_FNAME_CRYPTO_DIGEST_SIZE	32
-
 /* Encryption parameters */
 #define FS_XTS_TWEAK_SIZE		16
 #define FS_AES_128_ECB_KEY_SIZE		16
