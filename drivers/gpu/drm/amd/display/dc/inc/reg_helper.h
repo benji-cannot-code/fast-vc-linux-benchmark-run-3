9c7e7a5f6a506ce335b5335d2f91bfa7794cca3e
Tony Cheng <tony.cheng@amd.com>
2017-04-23 23:33:26 -0400

    drm/amd/display: clarify delay param for REG_WAIT
    Signed-off-by: Tony Cheng <tony.cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Reviewed-by: Dmytro Laktyushkin <Dmytro.Laktyushkin@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/inc/reg_helper.h
+++ drivers/gpu/drm/amd/display/dc/inc/reg_helper.h
@@ -186,10 +186,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* macro to poll and wait for a register field to read back given value */
 
-#define REG_WAIT(reg_name, field, val, delay, max_try)	\
+#define REG_WAIT(reg_name, field, val, delay_between_poll_us, max_try)	\
 		generic_reg_wait(CTX, \
 				REG(reg_name), FN(reg_name, field), val,\
-				delay, max_try, __func__)
+				delay_between_poll_us, max_try, __func__)
 
 /* macro to update (read, modify, write) register fields
  */
