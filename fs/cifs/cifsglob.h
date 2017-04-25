9d4f2e746b6fbc49e2d2a14e52349a626ecdd0a4
Pavel Shilovsky <pshilov@microsoft.com>
2017-04-25 11:52:30 -0700

    CIFS: Add asynchronous read support through kernel AIO
    This patch adds support to process read calls passed by io_submit()
    asynchronously. It based on the previously introduced async context
    that allows to process i/o responses in a separate thread and
    return the caller immediately for asynchronous calls.
    
    This improves reading performance of single threaded applications
    with increasing of i/o queue depth size.
    
    Signed-off-by: Pavel Shilovsky <pshilov@microsoft.com>
    Signed-off-by: Steve French <smfrench@gmail.com>

--- fs/cifs/cifsglob.h
+++ fs/cifs/cifsglob.h
@@ -1141,6 +1141,7 @@ struct cifs_readdata {
 	struct completion		done;
 	struct cifsFileInfo		*cfile;
 	struct address_space		*mapping;
+	struct cifs_aio_ctx		*ctx;
 	__u64				offset;
 	unsigned int			bytes;
 	unsigned int			got_bytes;
