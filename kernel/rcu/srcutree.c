071d077e33e4aa6ef4163c69864e243c4df55008
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2017-04-28 11:20:29 -0700

    srcu: Make exp_holdoff module parameter be static
    Because exp_holdoff is not used outside of srcutree.c, it can be static.
    This commit therefore makes this change.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcu/srcutree.c
+++ kernel/rcu/srcutree.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rcu.h"
 #include "rcu_segcblist.h"
 
-ulong exp_holdoff = 25 * 1000; /* Holdoff (ns) for auto-expediting. */
+static ulong exp_holdoff = 25 * 1000; /* Holdoff (ns) for auto-expediting. */
 module_param(exp_holdoff, ulong, 0444);
 
 static void srcu_invoke_callbacks(struct work_struct *work);
