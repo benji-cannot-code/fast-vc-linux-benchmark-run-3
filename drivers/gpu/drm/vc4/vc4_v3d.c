24860d44bd1db302bd45753e3cc15c98c9ac3ac5
Eric Anholt <eric@anholt.net>
2017-04-28 15:42:21 -0700

    drm/vc4: Turn the V3D clock on at runtime.
    For the Raspberry Pi's bindings, the power domain also implicitly
    turns on the clock and deasserts reset, but for the new Cygnus port we
    start representing the clock in the devicetree.
    
    v2: Document the clock-names property, check for -ENOENT for no clock
        in DT.
    v3: Drop NULL checks around clk calls which embed NULL checks.
    v4: Drop clk-names (feedback by Rob Herring)
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Acked-by: Rob Herring <robh@kernel.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170428224223.21904-1-eric@anholt.net

--- drivers/gpu/drm/vc4/vc4_v3d.c
+++ drivers/gpu/drm/vc4/vc4_v3d.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "linux/clk.h"
 #include "linux/component.h"
 #include "linux/pm_runtime.h"
 #include "vc4_drv.h"
@@ -306,6 +307,8 @@ static int vc4_v3d_runtime_suspend(struct device *dev)
 	drm_gem_object_put_unlocked(&vc4->bin_bo->base.base);
 	vc4->bin_bo = NULL;
 
+	clk_disable_unprepare(v3d->clk);
+
 	return 0;
 }
 
@@ -317,6 +320,10 @@ static int vc4_v3d_runtime_resume(struct device *dev)
 
 	ret = vc4_allocate_bin_bo(vc4->dev);
 	if (ret)
+		return ret;
+
+	ret = clk_prepare_enable(v3d->clk);
+	if (ret != 0)
 		return ret;
 
 	vc4_v3d_init_hw(vc4->dev);
@@ -349,15 +356,37 @@ static int vc4_v3d_bind(struct device *dev, struct device *master, void *data)
 	vc4->v3d = v3d;
 	v3d->vc4 = vc4;
 
+	v3d->clk = devm_clk_get(dev, NULL);
+	if (IS_ERR(v3d->clk)) {
+		int ret = PTR_ERR(v3d->clk);
+
+		if (ret == -ENOENT) {
+			/* bcm2835 didn't have a clock reference in the DT. */
+			ret = 0;
+			v3d->clk = NULL;
+		} else {
+			if (ret != -EPROBE_DEFER)
+				dev_err(dev, "Failed to get V3D clock: %d\n",
+					ret);
+			return ret;
+		}
+	}
+
 	if (V3D_READ(V3D_IDENT0) != V3D_EXPECTED_IDENT0) {
 		DRM_ERROR("V3D_IDENT0 read 0x%08x instead of 0x%08x\n",
 			  V3D_READ(V3D_IDENT0), V3D_EXPECTED_IDENT0);
 		return -EINVAL;
 	}
 
-	ret = vc4_allocate_bin_bo(drm);
-	if (ret)
+	ret = clk_prepare_enable(v3d->clk);
+	if (ret != 0)
 		return ret;
+
+	ret = vc4_allocate_bin_bo(drm);
+	if (ret) {
+		clk_disable_unprepare(v3d->clk);
+		return ret;
+	}
 
 	/* Reset the binner overflow address/size at setup, to be sure
 	 * we don't reuse an old one.
