1d331cdc94ba4c2a5c37044e205bf1383f3309b4
Cezary Gapinski <gapalinux@gmail.com>
2017-04-29 19:54:30 +0200

    staging/ks7010: Fix type assignment for struct hostif_hdr
    Sparse spits out a warnings about __le16 and unsigned short assignment.
    Change the type of size and event members of struct hostif_hdr
    to __le16 and correct conversion to the proper cpu type.
    
    Signed-off-by: Cezary Gapinski <gapalinux@gmail.com>
    Reviewed-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_hostif.h
+++ drivers/staging/ks7010/ks_hostif.h
@@ -63,8 +63,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 struct hostif_hdr {
-	u16 size;
-	u16 event;
+	__le16 size;
+	__le16 event;
 } __packed;
 
 struct hostif_data_request_t {
