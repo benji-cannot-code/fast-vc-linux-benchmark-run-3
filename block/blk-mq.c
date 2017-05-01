27e52ce648b033a2e2c9a8af7deea5a1bb03bff9
Ming Lei <ming.lei@redhat.com>
2017-05-02 07:28:02 +0800

    block: don't call blk_mq_quiesce_queue() after queue is frozen
    After queue is frozen, no request in this queue can be in use at all, so
    there can't be any .queue_rq() running on this queue.  It isn't
    necessary to call blk_mq_quiesce_queue() any more, so remove it in both
    elevator_switch_mq() and blk_mq_update_nr_requests().
    
    Cc: Bart Van Assche <bart.vanassche@sandisk.com>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    
    Fixed up the description a bit.
    
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2610,7 +2610,6 @@ int blk_mq_update_nr_requests(struct request_queue *q, unsigned int nr)
 		return -EINVAL;
 
 	blk_mq_freeze_queue(q);
-	blk_mq_quiesce_queue(q);
 
 	ret = 0;
 	queue_for_each_hw_ctx(q, hctx, i) {
@@ -2636,7 +2635,6 @@ int blk_mq_update_nr_requests(struct request_queue *q, unsigned int nr)
 		q->nr_requests = nr;
 
 	blk_mq_unfreeze_queue(q);
-	blk_mq_start_stopped_hw_queues(q, true);
 
 	return ret;
 }
