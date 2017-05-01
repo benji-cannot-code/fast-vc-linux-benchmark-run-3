b2e7cfa95c29675d8aec9d76062b58c48428313e
David Härdeman <david@hardeman.nu>
2017-05-01 13:04:37 -0300

    [media] lirc_dev: return POLLHUP and POLLERR when device is gone
    Most drivers return both values when the device is gone.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/rc/lirc_dev.c
+++ drivers/media/rc/lirc_dev.c
@@ -373,7 +373,7 @@ unsigned int lirc_dev_fop_poll(struct file *file, poll_table *wait)
 	}
 
 	if (!ir->attached)
-		return POLLERR;
+		return POLLHUP | POLLERR;
 
 	if (ir->buf) {
 		poll_wait(file, &ir->buf->wait_poll, wait);
