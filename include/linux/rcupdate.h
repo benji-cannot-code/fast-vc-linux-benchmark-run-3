56e62aaf47fda29a713dd73890db32aa42bce03b
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2017-05-03 11:18:01 -0700

    rcu: Remove linux/debugobjects.h from rcupdate.h
    The include/linux/rcupdate.h file does not actually need anything from
    linux/debugobjects.h, so this commit removes the inclusion.
    
    Reported-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpumask.h>
 #include <linux/seqlock.h>
 #include <linux/lockdep.h>
-#include <linux/debugobjects.h>
 #include <linux/bug.h>
 #include <linux/compiler.h>
 #include <linux/ktime.h>
