d7067404904912eed4537d0c7aa0781df94a1fe8
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-05-04 12:18:43 +0300

    drm/omap: remove rfbi
    The RFBI driver has not worked nor compiled for many years. There are
    very few boards out there that use RFBI, and no one has stepped up to
    fix it.
    
    So let's remove the RFBI code that doesn't even compile.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/gpu/drm/omapdrm/dss/core.c
+++ drivers/gpu/drm/omapdrm/dss/core.c
@@ -209,9 +209,6 @@ static int (*dss_output_drv_reg_funcs[])(void) __initdata = {
 #ifdef CONFIG_OMAP2_DSS_DSI
 	dsi_init_platform_driver,
 #endif
-#ifdef CONFIG_OMAP2_DSS_RFBI
-	rfbi_init_platform_driver,
-#endif
 #ifdef CONFIG_OMAP2_DSS_VENC
 	venc_init_platform_driver,
 #endif
@@ -232,9 +229,6 @@ static void (*dss_output_drv_unreg_funcs[])(void) = {
 #endif
 #ifdef CONFIG_OMAP2_DSS_VENC
 	venc_uninit_platform_driver,
-#endif
-#ifdef CONFIG_OMAP2_DSS_RFBI
-	rfbi_uninit_platform_driver,
 #endif
 #ifdef CONFIG_OMAP2_DSS_DSI
 	dsi_uninit_platform_driver,
