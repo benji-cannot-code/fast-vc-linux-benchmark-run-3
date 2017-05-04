7caf93bebf8f4081f6ac5aa04079df4021564334
Larry Finger <Larry.Finger@lwfinger.net>
2017-05-04 13:46:58 -0500

    rtlwifi: btcoex: Remove 21a 1ant configuration parameter
    In file halbtc8821a1ant.c, there are directives that depend on an
    undocumented configuration parameter BT_AUTO_REPORT_ONLY_8821A_1ANT
    that cannot be set from Kconfig. This parameter is replaced by a
    boolean in the main structure used by all routines. It still cannot
    be changed dynamically, but it is easier to document.
    
    Using a suggestion from Realtek, the auto report is turned on with this
    patch.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Cc: Pkshih <pkshih@realtek.com>
    Cc: Birming Chiu <birming@realtek.com>
    Cc: Shaofu <shaofu@realtek.com>
    Cc: Steven Ting <steventing@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/btcoexist/halbtc8821a1ant.h
+++ drivers/net/wireless/realtek/rtlwifi/btcoexist/halbtc8821a1ant.h
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The following is for 8821A 1ANT BT Co-exist definition
  *===========================================
  */
-#define	BT_AUTO_REPORT_ONLY_8821A_1ANT				0
-
 #define	BT_INFO_8821A_1ANT_B_FTP	BIT7
 #define	BT_INFO_8821A_1ANT_B_A2DP	BIT6
 #define	BT_INFO_8821A_1ANT_B_HID	BIT5
