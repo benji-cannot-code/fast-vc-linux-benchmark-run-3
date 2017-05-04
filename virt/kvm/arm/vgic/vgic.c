0d7b8fd51613a215c68db919303f9aa41d1b7daf
Christoffer Dall <cdall@linaro.org>
2017-05-04 13:54:17 +0200

    KVM: arm/arm64: Move shared files to virt/kvm/arm
    For some time now we have been having a lot of shared functionality
    between the arm and arm64 KVM support in arch/arm, which not only
    required a horrible inter-arch reference from the Makefile in
    arch/arm64/kvm, but also created confusion for newcomers to the code
    base, as was recently seen on the mailing list.
    
    Further, it causes confusion for things like cscope, which needs special
    attention to index specific shared files for arm64 from the arm tree.
    
    Move the shared files into virt/kvm/arm and move the trace points along
    with it.  When moving the tracepoints we have to modify the way the vgic
    creates definitions of the trace points, so we take the chance to
    include the VGIC tracepoints in its very own special vgic trace.h file.
    
    Signed-off-by: Christoffer Dall <cdall@linaro.org>

--- virt/kvm/arm/vgic/vgic.c
+++ virt/kvm/arm/vgic/vgic.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vgic.h"
 
 #define CREATE_TRACE_POINTS
-#include "../trace.h"
+#include "trace.h"
 
 #ifdef CONFIG_DEBUG_SPINLOCK
 #define DEBUG_SPINLOCK_BUG_ON(p) BUG_ON(p)
