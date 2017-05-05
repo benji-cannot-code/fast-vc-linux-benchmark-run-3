66d12cb23255f107a85ecd05ac006b4ebd010e2d
Björn Jacke <bj@sernet.de>
2017-05-05 04:36:16 +0200

    CIFS: add misssing SFM mapping for doublequote
    SFM is mapping doublequote to 0xF020
    
    Without this patch creating files with doublequote fails to Windows/Mac
    
    Signed-off-by: Bjoern Jacke <bjacke@samba.org>
    Signed-off-by: Steve French <smfrench@gmail.com>
    CC: stable <stable@vger.kernel.org>

--- fs/cifs/cifs_unicode.h
+++ fs/cifs/cifs_unicode.h
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * not conflict (although almost does) with the mapping above.
  */
 
+#define SFM_DOUBLEQUOTE ((__u16) 0xF020)
 #define SFM_ASTERISK    ((__u16) 0xF021)
 #define SFM_QUESTION    ((__u16) 0xF025)
 #define SFM_COLON       ((__u16) 0xF022)
