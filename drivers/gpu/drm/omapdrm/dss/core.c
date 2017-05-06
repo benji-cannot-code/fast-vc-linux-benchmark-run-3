10abc7237110bceeaa63f140538dde6df2c755df
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2017-05-07 01:37:24 +0300

    drm: omapdrm: sdi: Remove platform driver
    The SDI platform driver was used for non-DT platforms only. On DT
    platforms the SDI port is handled manually. As OMAP display devices are
    now instantiated from DT only, remove the SDI platform driver.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/dss/core.c
+++ drivers/gpu/drm/omapdrm/dss/core.c
@@ -209,9 +209,6 @@ static int (*dss_output_drv_reg_funcs[])(void) __initdata = {
 #ifdef CONFIG_OMAP2_DSS_DSI
 	dsi_init_platform_driver,
 #endif
-#ifdef CONFIG_OMAP2_DSS_SDI
-	sdi_init_platform_driver,
-#endif
 #ifdef CONFIG_OMAP2_DSS_RFBI
 	rfbi_init_platform_driver,
 #endif
@@ -238,9 +235,6 @@ static void (*dss_output_drv_unreg_funcs[])(void) = {
 #endif
 #ifdef CONFIG_OMAP2_DSS_RFBI
 	rfbi_uninit_platform_driver,
-#endif
-#ifdef CONFIG_OMAP2_DSS_SDI
-	sdi_uninit_platform_driver,
 #endif
 #ifdef CONFIG_OMAP2_DSS_DSI
 	dsi_uninit_platform_driver,
