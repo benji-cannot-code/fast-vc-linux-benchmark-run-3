ebd9d07ac6c67a7172ac14033ee94c8d4f447b10
Song Hongyan <hongyan.song@intel.com>
2017-05-07 18:24:25 +0800

    iio: hid-sensor-rotation: Add geomagnetic orientation sensor hid support.
    Geomagnetic orientation(AM) sensor is one kind of orientation 6dof sensor.
    It gives the device rotation in respect to the earth center and the
    magnetic north. The sensor is implemented through use of an accelerometer
    and magnetometer do not use gyroscope. It is a standard HID sensor.
    
    More information can be found in:
    http://www.usb.org/developers/hidpage/HUTRR59_-_Usages_for_Wearables.pdf
    
    Geomagnetic orientation(AM) sensor and dev rotation sensor have same
    channel and share channel usage id. So the most of the code for relative
    orientation sensor can be reused.
    
    Signed-off-by: Song Hongyan <hongyan.song@intel.com>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>

--- drivers/iio/orientation/hid-sensor-rotation.c
+++ drivers/iio/orientation/hid-sensor-rotation.c
@@ -242,6 +242,9 @@ static int hid_dev_rot_probe(struct platform_device *pdev)
 	case HID_USAGE_SENSOR_RELATIVE_ORIENTATION:
 		name = "relative_orientation";
 		break;
+	case HID_USAGE_SENSOR_GEOMAGNETIC_ORIENTATION:
+		name = "geomagnetic_orientation";
+		break;
 	default:
 		return -EINVAL;
 	}
@@ -339,6 +342,10 @@ static const struct platform_device_id hid_dev_rot_ids[] = {
 	{
 		/* Relative orientation(AG) sensor */
 		.name = "HID-SENSOR-20008e",
+	},
+	{
+		/* Geomagnetic orientation(AM) sensor */
+		.name = "HID-SENSOR-2000c1",
 	},
 	{ /* sentinel */ }
 };
