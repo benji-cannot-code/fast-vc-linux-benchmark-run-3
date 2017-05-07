8e96f829f9a005bb03f83b0399dae85d0517f980
Gilad Ben-Yossef <gilad@benyossef.com>
2017-05-07 16:35:56 +0300

    staging: ccree: remove min/max macros
    Remove useless and wrong min/max macros.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/cc_crypto_ctx.h
+++ drivers/staging/ccree/cc_crypto_ctx.h
@@ -21,12 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-
-#ifndef max
-#define max(a, b) ((a) > (b) ? (a) : (b))
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#endif
-
 /* context size */
 #ifndef CC_CTX_SIZE_LOG2
 #if (CC_SUPPORT_SHA > 256)
