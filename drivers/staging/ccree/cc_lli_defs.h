62cc32a471348757552d7dfc461ad62486903577
Gilad Ben-Yossef <gilad@benyossef.com>
2017-05-07 16:36:02 +0300

    staging: ccree: drop __KERNEL__ include guard
    Drop uneeded include guard for __KERNEL__.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/cc_lli_defs.h
+++ drivers/staging/ccree/cc_lli_defs.h
@@ -17,11 +17,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef _CC_LLI_DEFS_H_
 #define _CC_LLI_DEFS_H_
-#ifdef __KERNEL__
+
 #include <linux/types.h>
-#else
-#include <stdint.h>
-#endif
+
 #include "cc_bitops.h"
 
 /* Max DLLI size */
