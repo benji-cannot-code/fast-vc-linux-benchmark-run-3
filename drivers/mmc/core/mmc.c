d3697d3b5483ca6e0241bbb33f0f40aec5c63c84
Ulf Hansson <ulf.hansson@linaro.org>
2017-05-08 23:52:04 +0200

    mmc: core: Don't do eMMC HW reset when resuming the eMMC card
    In case if a pwrseq-emmc has been bound to the host, a call to
    mmc_power_up() triggers an eMMC HW reset via the pwrseq_emmc's
    ->post_power_on() callback. This isn't really what we want, as
    mmc_power_up() is called each time when resuming the card.
    
    As a matter of fact, the current approach may also violate the eMMC spec,
    as the involved delays managed in pwrseq_emmc assumes both VCC and VCCQ has
    been turned on, which isn't the case for VCCQ, unless the regulator is
    always on.
    
    Fix this behaviour by aligning to the same procedure used when the mmc host
    implements the ->hw_reset() callback and has the MMC_CAP_HW_RESET flag set.
    In this way the eMMC HW reset is issued at card detection scan, to cope
    with bogus bootloaders and in the error recovery path via the mmc specific
    bus_ops->reset() callback.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mmc_ops.h"
 #include "quirks.h"
 #include "sd_ops.h"
+#include "pwrseq.h"
 
 #define DEFAULT_CMD6_TIMEOUT_MS	500
 
@@ -2128,6 +2129,7 @@ static int mmc_reset(struct mmc_host *host)
 	} else {
 		/* Do a brute force power cycle */
 		mmc_power_cycle(host, card->ocr);
+		mmc_pwrseq_reset(host);
 	}
 	return mmc_init_card(host, card->ocr, card);
 }
