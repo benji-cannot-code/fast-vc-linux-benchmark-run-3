a5d5966e14e72c3b441db73d460706f761d9f1c7
Andrew Morton <akpm@linux-foundation.org>
2017-05-08 15:58:44 -0700

    drivers/staging/media/atomisp/pci/atomisp2: use set_memory.h
    Cc: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/staging/media/atomisp/pci/atomisp2/hmm/hmm_dynamic_pool.c
+++ drivers/staging/media/atomisp/pci/atomisp2/hmm/hmm_dynamic_pool.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/mm.h>
 
-#include "asm/cacheflush.h"
+#include <asm/set_memory.h>
 
 #include "atomisp_internal.h"
 
