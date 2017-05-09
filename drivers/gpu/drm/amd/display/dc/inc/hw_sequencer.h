e5ca46c92f66884760c06711ffd4b46869af56e5
Amy Zhang <Amy.Zhang@amd.com>
2017-05-09 14:45:54 -0400

    drm/amd/display: Move output transfer function to stream updates
    Signed-off-by: Amy Zhang <Amy.Zhang@amd.com>
    Reviewed-by: Anthony Koo <Anthony.Koo@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/inc/hw_sequencer.h
+++ drivers/gpu/drm/amd/display/dc/inc/hw_sequencer.h
@@ -81,7 +81,6 @@ struct hw_sequencer_funcs {
 
 	bool (*set_output_transfer_func)(
 				struct pipe_ctx *pipe_ctx,
-				const struct core_surface *surface,
 				const struct core_stream *stream);
 
 	void (*power_down)(struct core_dc *dc);
