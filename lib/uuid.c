f1fc11aa51ad568dfd656f9168ca9b4510344c0b
Christoph Hellwig <hch@lst.de>
2017-05-10 14:22:11 +0200

    uuid: don't export guid_index and uuid_index
    These are only used in uuid.c and vsprintf.c and aren't something modules
    should use directly.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- lib/uuid.c
+++ lib/uuid.c
@@ -23,9 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/random.h>
 
 const u8 guid_index[16] = {3,2,1,0,5,4,7,6,8,9,10,11,12,13,14,15};
-EXPORT_SYMBOL(guid_index);
 const u8 uuid_index[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-EXPORT_SYMBOL(uuid_index);
 
 /***************************************************************
  * Random UUID interface
