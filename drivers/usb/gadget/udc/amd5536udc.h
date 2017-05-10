19da4758da464bb7f61e68c063004b16c43bf768
Raviteja Garimella <raviteja.garimella@broadcom.com>
2017-05-10 18:21:18 +0530

    usb: gadget: udc: make debug prints compatible with both pci and platform devices
    This patch adds a struct device member to UDC data structure and
    makes changes to the arguments of dev_err and dev_dbg calls so that
    the debug prints work for both pci and platform devices.
    
    Signed-off-by: Raviteja Garimella <raviteja.garimella@broadcom.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/gadget/udc/amd5536udc.h
+++ drivers/usb/gadget/udc/amd5536udc.h
@@ -564,6 +564,8 @@ struct udc {
 	u16				cur_config;
 	u16				cur_intf;
 	u16				cur_alt;
+
+	struct device			*dev;
 };
 
 #define to_amd5536_udc(g)	(container_of((g), struct udc, gadget))
@@ -640,7 +642,7 @@ MODULE_PARM_DESC(use_fullspeed, "true for fullspeed only");
 
 /* debug macros ------------------------------------------------------------*/
 
-#define DBG(udc , args...)	dev_dbg(&(udc)->pdev->dev, args)
+#define DBG(udc , args...)	dev_dbg(udc->dev, args)
 
 #ifdef UDC_VERBOSE
 #define VDBG			DBG
