de932e1a748e4fa33e7c8a7da5bf68f554814387
Derek Robson <robsonde@gmail.com>
2017-05-10 14:29:17 +1200

    Staging: rtl8192u - changed include of asm/io.h
    Changed include of <asm/io.h> to be <linux/io.h>
    Complies, but I don't have hardware.
    Found using checkpatch.
    
    Signed-off-by: Derek Robson <robsonde@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8192U.h
+++ drivers/staging/rtl8192u/r8192U.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/proc_fs.h>
 #include <linux/if_arp.h>
 #include <linux/random.h>
-#include <asm/io.h>
+#include <linux/io.h>
 #include "ieee80211/ieee80211.h"
 
 #define RTL8192U
