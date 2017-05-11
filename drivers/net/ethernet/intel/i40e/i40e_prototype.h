da87d85e7f7a11538a4bc170d7742c6bf2be8848
Jesse Brandeburg <jesse.brandeburg@intel.com>
2017-05-11 11:23:10 -0700

    i40e: use new unified virtchnl header file
    This patch changes the i40e driver to start using the new virtchnl
    interface header file, and removes an already existing duplicate of the
    i40e_virtchnl.h file contained in the i40e directory.
    
    Signed-off-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_prototype.h
+++ drivers/net/ethernet/intel/i40e/i40e_prototype.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "i40e_type.h"
 #include "i40e_alloc.h"
-#include "i40e_virtchnl.h"
+#include <linux/avf/virtchnl.h>
 
 /* Prototypes for shared code functions that are not in
  * the standard function pointer structures.  These are
