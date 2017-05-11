e677f04954f809995e6d688c1fe012f5fa817eda
Geert Uytterhoeven <geert@linux-m68k.org>
2017-05-11 09:38:19 +0200

    staging: ccree: Fix indentation of driver_hash[] initializers
    Closing braces should match the first characters of the openings.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/ssi_hash.c
+++ drivers/staging/ccree/ssi_hash.c
@@ -2003,8 +2003,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = SHA1_DIGEST_SIZE,
 					.statesize = sizeof(struct sha1_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_SHA1,
 		.hw_mode = DRV_HASH_HW_SHA1,
@@ -2032,8 +2032,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = SHA256_DIGEST_SIZE,
 					.statesize = sizeof(struct sha256_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_SHA256,
 		.hw_mode = DRV_HASH_HW_SHA256,
@@ -2061,8 +2061,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = SHA224_DIGEST_SIZE,
 					.statesize = sizeof(struct sha256_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_SHA224,
 		.hw_mode = DRV_HASH_HW_SHA256,
@@ -2091,8 +2091,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = SHA384_DIGEST_SIZE,
 					.statesize = sizeof(struct sha512_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_SHA384,
 		.hw_mode = DRV_HASH_HW_SHA512,
@@ -2120,8 +2120,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = SHA512_DIGEST_SIZE,
 					.statesize = sizeof(struct sha512_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_SHA512,
 		.hw_mode = DRV_HASH_HW_SHA512,
@@ -2150,8 +2150,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = MD5_DIGEST_SIZE,
 					.statesize = sizeof(struct md5_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_MD5,
 		.hw_mode = DRV_HASH_HW_MD5,
@@ -2177,8 +2177,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = AES_BLOCK_SIZE,
 					.statesize = sizeof(struct aeshash_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_NULL,
 		.hw_mode = DRV_CIPHER_XCBC_MAC,
@@ -2205,8 +2205,8 @@ static struct ssi_hash_template driver_hash[] = {
 				.halg = {
 					.digestsize = AES_BLOCK_SIZE,
 					.statesize = sizeof(struct aeshash_state),
-					},
 				},
+			},
 		},
 		.hash_mode = DRV_HASH_NULL,
 		.hw_mode = DRV_CIPHER_CMAC,
