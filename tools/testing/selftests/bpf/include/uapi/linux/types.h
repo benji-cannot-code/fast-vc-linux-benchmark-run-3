203c287dd8557e92703b1960f38f69ab4aef3c17
David S. Miller <davem@davemloft.net>
2017-05-11 12:00:50 -0700

    bpf: Provide a linux/types.h override for bpf selftests.
    We do not want to use the architecture's type.h header when
    building BPF programs which are always 64-bit.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ tools/testing/selftests/bpf/include/uapi/linux/types.h
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _UAPI_LINUX_TYPES_H
+#define _UAPI_LINUX_TYPES_H
+
+#include <asm-generic/int-ll64.h>
+
+#endif /* _UAPI_LINUX_TYPES_H */
