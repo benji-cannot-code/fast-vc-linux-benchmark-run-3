b6e26d685ebcb5b22d0cebaa68670526fe541061
Vivek Gautam <vivek.gautam@codeaurora.org>
2017-05-11 12:17:41 +0530

    phy: Move ULPI phy header out of drivers to include path
    Although ULPI phy is currently being used by tusb1210,
    there can be other consumers too in future. So move this
    to the includes path for phy.
    
    Signed-off-by: Vivek Gautam <vivek.gautam@codeaurora.org>
    Cc: Stephen Boyd <stephen.boyd@linaro.org>
    Cc: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Cc: Kishon Vijay Abraham I <kishon@ti.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-omap@vger.kernel.org
    Cc: linux-usb@vger.kernel.org
    Acked-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/phy-tusb1210.c
+++ drivers/phy/phy-tusb1210.c
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/ulpi/driver.h>
 #include <linux/gpio/consumer.h>
-
-#include "ulpi_phy.h"
+#include <linux/phy/ulpi_phy.h>
 
 #define TUSB1210_VENDOR_SPECIFIC2		0x80
 #define TUSB1210_VENDOR_SPECIFIC2_IHSTX_SHIFT	0
