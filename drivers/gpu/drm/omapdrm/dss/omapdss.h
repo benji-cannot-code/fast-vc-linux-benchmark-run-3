4a129f108d1781b0461ccaf6ed9fe58b7384c0f7
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-05-12 12:38:46 +0300

    drm/omap: sdi: remove legacy set_datapairs
    Non-DT booting is no longer supported, so remove legacy code.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/gpu/drm/omapdrm/dss/omapdss.h
+++ drivers/gpu/drm/omapdrm/dss/omapdss.h
@@ -491,8 +491,6 @@ struct omapdss_sdi_ops {
 			    struct videomode *vm);
 	void (*get_timings)(struct omap_dss_device *dssdev,
 			    struct videomode *vm);
-
-	void (*set_datapairs)(struct omap_dss_device *dssdev, int datapairs);
 };
 
 struct omapdss_dvi_ops {
@@ -641,10 +639,6 @@ struct omap_dss_device {
 			u8 channel;
 			u8 data_lines;
 		} rfbi;
-
-		struct {
-			u8 datapairs;
-		} sdi;
 
 		struct {
 			int module;
