d196a69d0c62ef08e90d8d5a619c1a0de3b02040
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-05-12 12:37:04 +0300

    drm/omap: connector-dvi/hdmi: remove extra include
    Remove extra includes related to non-DT boot.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/gpu/drm/omapdrm/displays/connector-dvi.c
+++ drivers/gpu/drm/omapdrm/displays/connector-dvi.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 
 #include <drm/drm_edid.h>
-#include <video/omap-panel-data.h>
 
 #include "../dss/omapdss.h"
 
