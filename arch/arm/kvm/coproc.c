ef91265427a3b9d9c28c914274a4f5d986f33705
Marc Zyngier <marc.zyngier@arm.com>
2017-05-12 11:04:52 +0100

    ARM: KVM: Fix tracepoint generation after move to virt/kvm/arm/
    Moving most of the shared code to virt/kvm/arm had for consequence
    that KVM/ARM doesn't build anymore, because the code that used to
    define the tracepoints is now somewhere else.
    
    Fix this by defining CREATE_TRACE_POINTS in coproc.c, and clean-up
    trace.h as well.
    
    Fixes: 35d2d5d490e2 ("KVM: arm/arm64: Move shared files to virt/kvm/arm")
    Reported-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <cdall@linaro.org>

--- arch/arm/kvm/coproc.c
+++ arch/arm/kvm/coproc.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/vfp.h>
 #include "../vfp/vfpinstr.h"
 
+#define CREATE_TRACE_POINTS
 #include "trace.h"
 #include "coproc.h"
 
