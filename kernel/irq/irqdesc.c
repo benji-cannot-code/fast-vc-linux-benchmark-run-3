5a2ad789a993352f951a70c29e1ef2e6f349730f
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2017-05-14 12:03:39 -0300

    irq: update genericirq book location
    This book got converted from DocBook. Update its references to
    point to the current location.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- kernel/irq/irqdesc.c
+++ kernel/irq/irqdesc.c
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * This file contains the interrupt descriptor management code
  *
- * Detailed information is available in Documentation/DocBook/genericirq
+ * Detailed information is available in Documentation/core-api/genericirq.rst
  *
  */
 #include <linux/irq.h>
