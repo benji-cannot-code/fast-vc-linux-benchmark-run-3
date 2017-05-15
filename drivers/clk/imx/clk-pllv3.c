24cc95a8b8e6dba17d54a2ebae5637dc175aa3eb
Fabio Estevam <fabio.estevam@nxp.com>
2017-05-15 08:55:05 -0300

    clk: imx7d: Fix the powerdown bit location of PLL DDR
    According to the MX7D Reference Manual the powerdown bit of
    CCM_ANALOG_PLL_DDRn register is bit 20, so fix it accordingly.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Reviewed-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/imx/clk-pllv3.c
+++ drivers/clk/imx/clk-pllv3.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BM_PLL_POWER		(0x1 << 12)
 #define BM_PLL_LOCK		(0x1 << 31)
 #define IMX7_ENET_PLL_POWER	(0x1 << 5)
+#define IMX7_DDR_PLL_POWER	(0x1 << 20)
 
 /**
  * struct clk_pllv3 - IMX PLL clock version 3
@@ -451,6 +452,10 @@ struct clk *imx_clk_pllv3(enum imx_pllv3_type type, const char *name,
 	case IMX_PLLV3_ENET:
 		pll->ref_clock = 500000000;
 		ops = &clk_pllv3_enet_ops;
+		break;
+	case IMX_PLLV3_DDR_IMX7:
+		pll->power_bit = IMX7_ENET_PLL_POWER;
+		ops = &clk_pllv3_av_ops;
 		break;
 	default:
 		ops = &clk_pllv3_ops;
