85ec314eef3432c24d2bbd15421237368faccca8
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-05-16 12:05:24 +0300

    drm/omap: DRM_MODE_REFLECT_* instead of mirror boolean
    Change dispc driver to use the DRM_MODE_REFLECT flags instead of a mirror
    boolean.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/gpu/drm/omapdrm/dss/omapdss.h
+++ drivers/gpu/drm/omapdrm/dss/omapdss.h
@@ -264,7 +264,6 @@ struct omap_overlay_info {
 	u32 fourcc;
 	u8 rotation;
 	enum omap_dss_rotation_type rotation_type;
-	bool mirror;
 
 	u16 pos_x;
 	u16 pos_y;
@@ -312,7 +311,6 @@ struct omap_dss_writeback_info {
 	u32 fourcc;
 	u8 rotation;
 	enum omap_dss_rotation_type rotation_type;
-	bool mirror;
 	u8 pre_mult_alpha;
 };
 
