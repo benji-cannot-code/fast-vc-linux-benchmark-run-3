fba0038f819d30a8039da247eb02ee78db8d4826
Liu Bo <bo.li.liu@oracle.com>
2017-05-15 17:20:07 -0700

    Btrfs: record error if one block has failed to retry
    In the nocsum case of dio read endio, it returns immediately if an error
    gets returned when repairing, which leaves the rest blocks unrepaired.  The
    behavior is different from how buffered read endio works in the same case.
    This changes it to record error only and go on repairing the rest blocks.
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -8079,6 +8079,7 @@ static int __btrfs_correct_data_nocsum(struct inode *inode,
 	u32 sectorsize;
 	int nr_sectors;
 	int ret;
+	int err = 0;
 
 	fs_info = BTRFS_I(inode)->root->fs_info;
 	sectorsize = fs_info->sectorsize;
@@ -8100,8 +8101,10 @@ next_block_or_try_again:
 				pgoff, start, start + sectorsize - 1,
 				io_bio->mirror_num,
 				btrfs_retry_endio_nocsum, &done);
-		if (ret)
-			return ret;
+		if (ret) {
+			err = ret;
+			goto next;
+		}
 
 		wait_for_completion(&done.done);
 
@@ -8110,6 +8113,7 @@ next_block_or_try_again:
 			goto next_block_or_try_again;
 		}
 
+next:
 		start += sectorsize;
 
 		nr_sectors--;
@@ -8120,7 +8124,7 @@ next_block_or_try_again:
 		}
 	}
 
-	return 0;
+	return err;
 }
 
 static void btrfs_retry_endio(struct bio *bio)
