c3058ca27bb79244bb9a672adc5a357429c0f0a6
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-05-16 15:34:45 +0300

    drm: add missing declaration to drm_blend.h
    drm_blend.h is missing declaration for 'struct drm_plane'. Add it.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1494938085-21805-1-git-send-email-tomi.valkeinen@ti.com

--- include/drm/drm_blend.h
+++ include/drm/drm_blend.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct drm_device;
 struct drm_atomic_state;
+struct drm_plane;
 
 /*
  * Rotation property bits. DRM_ROTATE_<degrees> rotates the image by the
