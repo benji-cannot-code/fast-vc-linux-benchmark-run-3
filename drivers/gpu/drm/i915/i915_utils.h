c023648169a1c2666f1f21ae7ea815e7f88fb5b4
Chris Wilson <chris@chris-wilson.co.uk>
2017-05-17 13:09:58 +0100

    drm/i915: Make ptr_unpack_bits() more function-like
    ptr_unpack_bits() is a function-like macro, as such it is meant to be
    replaceable by a function. In this case, we should be passing in the
    out-param as a pointer.
    
    Bizarrely this does affect code generation:
    
    function                                     old     new   delta
    i915_gem_object_pin_map                      409     389     -20
    
    An improvement(?) in this case, but one can't help wonder what
    strict-aliasing optimisations we are preventing.
    
    The generated code looks identical in using ptr_unpack_bits (no extra
    motions to stack, the pointer and bits appear to be kept in registers),
    the difference appears to be code ordering and with a reorder it is able
    to use smaller forward jumps.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170517121007.27224-3-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_utils.h
+++ drivers/gpu/drm/i915/i915_utils.h
@@ -78,7 +78,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define ptr_unpack_bits(ptr, bits) ({					\
 	unsigned long __v = (unsigned long)(ptr);			\
-	(bits) = __v & ~PAGE_MASK;					\
+	*(bits) = __v & ~PAGE_MASK;					\
 	(typeof(ptr))(__v & PAGE_MASK);					\
 })
 
