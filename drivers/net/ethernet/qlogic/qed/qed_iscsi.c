d1a7a4b2fa84ada716fc2e944046cf52467ffa45
Wei Yongjun <weiyongjun1@huawei.com>
2017-05-18 15:26:29 +0000

    qed: Remove unused including <linux/version.h>
    Remove including <linux/version.h> that is not needed.
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qlogic/qed/qed_iscsi.c
+++ drivers/net/ethernet/qlogic/qed/qed_iscsi.c
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/stddef.h>
 #include <linux/string.h>
-#include <linux/version.h>
 #include <linux/workqueue.h>
 #include <linux/errno.h>
 #include <linux/list.h>
--- drivers/net/ethernet/qlogic/qed/qed_main.c
+++ drivers/net/ethernet/qlogic/qed/qed_main.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>
-#include <linux/version.h>
 #include <linux/delay.h>
 #include <asm/byteorder.h>
 #include <linux/dma-mapping.h>
