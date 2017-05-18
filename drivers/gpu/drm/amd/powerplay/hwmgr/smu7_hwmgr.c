cc34c353d68e06508b537335786c4ee8946133f4
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-05-18 13:43:32 +0900

    drm/amd: include <linux/delay.h> instead of "linux/delay.h"
    Use <...> notation to include headers located in include/linux.
    While we are here, tweak the includes order a bit to sort them
    alphabetically.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1495082612-10385-1-git-send-email-yamada.masahiro@socionext.com

--- drivers/gpu/drm/amd/powerplay/hwmgr/smu7_hwmgr.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/smu7_hwmgr.c
@@ -22,11 +22,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 #include "pp_debug.h"
+#include <linux/delay.h>
+#include <linux/fb.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/fb.h>
 #include <asm/div64.h>
-#include "linux/delay.h"
 #include "pp_acpi.h"
 #include "ppatomctrl.h"
 #include "atombios.h"
--- drivers/gpu/drm/amd/powerplay/smumgr/smumgr.c
+++ drivers/gpu/drm/amd/powerplay/smumgr/smumgr.c
@@ -21,15 +21,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-#include <linux/types.h>
+
+#include <linux/delay.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/types.h>
 #include <drm/amdgpu_drm.h>
 #include "pp_instance.h"
 #include "smumgr.h"
 #include "cgs_common.h"
-#include "linux/delay.h"
 
 MODULE_FIRMWARE("amdgpu/topaz_smc.bin");
 MODULE_FIRMWARE("amdgpu/topaz_k_smc.bin");
