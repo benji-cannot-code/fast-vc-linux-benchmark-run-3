99c98da76abfee7fe6b80b9a97e9f90e72ba05aa
Avraham Shukron <avraham.shukron@gmail.com>
2017-05-18 10:50:15 -0300

    [media] atomisp: fixed coding style errors
    Fix for error (not warnings) reported by checkpatch.pl
    Specifically:
     - missing whitespace around "=" and after ","
     - indentation with spaces instead of tabs
     - lines starting with a whitespace
    
    This patch does not affect the compiled code in any way.
    
    Signed-off-by: Avraham Shukron <avraham.shukron@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/staging/media/atomisp/platform/intel-mid/intel_mid_pcihelpers.c
+++ drivers/staging/media/atomisp/platform/intel-mid/intel_mid_pcihelpers.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 static inline int platform_is(u8 model)
 {
-        return (boot_cpu_data.x86_model == model);
+	return (boot_cpu_data.x86_model == model);
 }
 
 #include "../../include/asm/intel_mid_pcihelpers.h"
