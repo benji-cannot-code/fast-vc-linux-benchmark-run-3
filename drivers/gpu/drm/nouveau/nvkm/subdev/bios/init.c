36fceee67daa7dec4ed7f7a8947618e03766cee3
Ben Skeggs <bskeggs@redhat.com>
2017-05-19 23:59:34 +1000

    drm/nouveau/bios/init: rename nvbios_init() to nvbios_devinit()
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/bios/init.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/bios/init.c
@@ -2291,7 +2291,7 @@ nvbios_exec(struct nvbios_init *init)
 }
 
 int
-nvbios_init(struct nvkm_subdev *subdev, bool execute)
+nvbios_post(struct nvkm_subdev *subdev, bool execute)
 {
 	struct nvkm_bios *bios = subdev->device->bios;
 	int ret = 0;
