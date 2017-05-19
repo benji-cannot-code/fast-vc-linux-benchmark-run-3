849ca20850382031317ca6765eabe853fec2457b
Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
2017-05-19 16:17:54 -0400

    staging: unisys: include: renamed function spar_check_channel in channel.h to match driver namespace
    Renamed function spar_check_channel() to visor_check_channel().
    
    Signed-off-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/iochannel.h
+++ drivers/staging/unisys/include/iochannel.h
@@ -52,13 +52,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ULTRA_VSWITCH_CHANNEL_PROTOCOL_VERSIONID 1
 
 #define SPAR_VHBA_CHANNEL_OK_CLIENT(ch) \
-	(spar_check_channel(ch, spar_vhba_channel_protocol_uuid, \
+	(visor_check_channel(ch, spar_vhba_channel_protocol_uuid, \
 			    "vhba", MIN_IO_CHANNEL_SIZE,	\
 			    ULTRA_VHBA_CHANNEL_PROTOCOL_VERSIONID, \
 			    ULTRA_VHBA_CHANNEL_PROTOCOL_SIGNATURE))
 
 #define SPAR_VNIC_CHANNEL_OK_CLIENT(ch) \
-	(spar_check_channel(ch, spar_vnic_channel_protocol_uuid, \
+	(visor_check_channel(ch, spar_vnic_channel_protocol_uuid, \
 			    "vnic", MIN_IO_CHANNEL_SIZE,	\
 			    ULTRA_VNIC_CHANNEL_PROTOCOL_VERSIONID, \
 			    ULTRA_VNIC_CHANNEL_PROTOCOL_SIGNATURE))
--- drivers/staging/unisys/visorbus/controlvmchannel.h
+++ drivers/staging/unisys/visorbus/controlvmchannel.h
@@ -36,12 +36,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VISOR_CONTROLVM_CHANNEL_VERSIONID 1
 
 #define VISOR_CONTROLVM_CHANNEL_OK_CLIENT(ch) \
-	(spar_check_channel(ch, \
-			    VISOR_CONTROLVM_CHANNEL_UUID, \
-			    "controlvm", \
-			    sizeof(struct visor_controlvm_channel), \
-			    VISOR_CONTROLVM_CHANNEL_VERSIONID, \
-			    VISOR_CONTROLVM_CHANNEL_SIGNATURE))
+	(visor_check_channel(ch, \
+			     VISOR_CONTROLVM_CHANNEL_UUID, \
+			     "controlvm", \
+			     sizeof(struct visor_controlvm_channel), \
+			     VISOR_CONTROLVM_CHANNEL_VERSIONID, \
+			     VISOR_CONTROLVM_CHANNEL_SIGNATURE))
 
 /* Defines for various channel queues */
 #define CONTROLVM_QUEUE_REQUEST	 0
