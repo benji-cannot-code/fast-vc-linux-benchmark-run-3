40c2b27864910a4a0cd43966e70bbe9267c65d31
Geert Uytterhoeven <geert+renesas@glider.be>
2017-05-19 10:35:10 +0200

    soc: renesas: Rework Kconfig and Makefile logic
    The goals are to:
      - Allow precise control over and automatic selection of which
        (sub)drivers are used for which SoC,
      - Allow adding support for new SoCs easily,
      - Allow compile-testing of all (sub)drivers,
      - Keep driver selection logic in the subsystem-specific Kconfig,
        independent from the architecture-specific Kconfig (i.e. no "select"
        from arch/arm64/Kconfig.platforms), to avoid dependencies.
    
    This is implemented by:
      - Introducing Kconfig symbols for all drivers and sub-drivers,
      - Introducing the Kconfig symbol SOC_RENESAS, which is enabled
        automatically when building for a Renesas ARM platform, and which
        enables all required drivers without interaction of the user, based
        on SoC-specific ARCH_* symbols,
      - Allowing the user to enable any Kconfig symbol manually if
        COMPILE_TEST is enabled,
      - Using the new Kconfig symbols instead of the ARCH_* symbols to
        control compilation in the Makefile,
      - Always entering drivers/soc/renesas/ during the build.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- include/linux/soc/renesas/rcar-rst.h
+++ include/linux/soc/renesas/rcar-rst.h
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_SOC_RENESAS_RCAR_RST_H__
 #define __LINUX_SOC_RENESAS_RCAR_RST_H__
 
-#if defined(CONFIG_ARCH_RCAR_GEN1) || defined(CONFIG_ARCH_RCAR_GEN2) || \
-    defined(CONFIG_ARCH_R8A7795) || defined(CONFIG_ARCH_R8A7796)
+#ifdef CONFIG_RST_RCAR
 int rcar_rst_read_mode_pins(u32 *mode);
 #else
 static inline int rcar_rst_read_mode_pins(u32 *mode) { return -ENODEV; }
