e80675400a0d68af142ce036fd0d320c6444c232
Christoph Hellwig <hch@lst.de>
2017-05-21 12:21:50 +0200

    arm64: remove DMA_ERROR_CODE
    The dma alloc interface returns an error by return NULL, and the
    mapping interfaces rely on the mapping_error method, which the dummy
    ops already implement correctly.
    
    Thus remove the DMA_ERROR_CODE define.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>

--- arch/arm64/include/asm/dma-mapping.h
+++ arch/arm64/include/asm/dma-mapping.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <xen/xen.h>
 #include <asm/xen/hypervisor.h>
 
-#define DMA_ERROR_CODE	(~(dma_addr_t)0)
 extern const struct dma_map_ops dummy_dma_ops;
 
 static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
