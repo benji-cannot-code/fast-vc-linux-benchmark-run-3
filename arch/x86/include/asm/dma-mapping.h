b4a86c25a68b5a01dbb41ecf26373ae2c1d96f9f
Christoph Hellwig <hch@lst.de>
2017-05-22 10:37:09 +0200

    x86: remove DMA_ERROR_CODE
    All dma_map_ops instances now handle their errors through
    ->mapping_error.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- arch/x86/include/asm/dma-mapping.h
+++ arch/x86/include/asm/dma-mapping.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # define ISA_DMA_BIT_MASK DMA_BIT_MASK(32)
 #endif
 
-#define DMA_ERROR_CODE	0
-
 extern int iommu_merge;
 extern struct device x86_dma_fallback_dev;
 extern int panic_on_overflow;
