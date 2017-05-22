101c5bef4634b7117e89e492eef206af481ecad7
Gustavo A. R. Silva <garsilva@embeddedor.com>
2017-05-22 14:12:37 -0500

    drm/nouveau/kms/nv50: add null check before pointer dereference
    Add null check before dereferencing pointer asyc
    
    Addresses-Coverity-ID: 1397932
    Signed-off-by: Gustavo A. R. Silva <garsilva@embeddedor.com>
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nv50_display.c
+++ drivers/gpu/drm/nouveau/nv50_display.c
@@ -2108,7 +2108,8 @@ nv50_head_atomic_check(struct drm_crtc *crtc, struct drm_crtc_state *state)
 					asyc->set.dither = true;
 			}
 		} else {
-			asyc->set.mask = ~0;
+			if (asyc)
+				asyc->set.mask = ~0;
 			asyh->set.mask = ~0;
 		}
 
