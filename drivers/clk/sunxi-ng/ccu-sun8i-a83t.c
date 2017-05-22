91948029d80faebce91cc8bf2a92081f6b9d75fd
Chen-Yu Tsai <wens@csie.org>
2017-05-22 14:25:47 +0800

    clk: sunxi-ng: a83t: Fix PLL lock status register offset
    The offset for the PLL lock status register was incorrectly set to
    0x208, which actually points to an unused register. The correct
    register offset is 0x20c.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- drivers/clk/sunxi-ng/ccu-sun8i-a83t.c
+++ drivers/clk/sunxi-ng/ccu-sun8i-a83t.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ccu-sun8i-a83t.h"
 
-#define CCU_SUN8I_A83T_LOCK_REG	0x208
+#define CCU_SUN8I_A83T_LOCK_REG	0x20c
 
 /*
  * The CPU PLLs are actually NP clocks, with P being /1 or /4. However
