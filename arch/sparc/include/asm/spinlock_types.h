7ec3914b7b20b8a002b127a39d06c01860bcf04f
Babu Moger <babu.moger@oracle.com>
2017-05-24 17:55:15 -0600

    arch/sparc: Enable queued spinlock support for SPARC
    This patch makes the necessary changes in SPARC architecture to enable
    queued spinlock support. Here are some of the earlier discussions about
    this feature.
    https://lwn.net/Articles/561775/
    https://lwn.net/Articles/590243/
    
    Cleaned-up the spinlock_64.h. The definitions of arch_spin_xxx are
    replaced by the function in <asm-generic/qspinlock.h>
    
    Signed-off-by: Babu Moger <babu.moger@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Jane Chu <jane.chu@oracle.com>
    Reviewed-by: Shannon Nelson <shannon.nelson@oracle.com>
    Reviewed-by: Vijay Kumar <vijay.ac.kumar@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/spinlock_types.h
+++ arch/sparc/include/asm/spinlock_types.h
@@ -2,11 +2,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SPARC_SPINLOCK_TYPES_H
 #define __SPARC_SPINLOCK_TYPES_H
 
+#ifdef CONFIG_QUEUED_SPINLOCKS
+#include <asm-generic/qspinlock_types.h>
+#else
+
 typedef struct {
 	volatile unsigned char lock;
 } arch_spinlock_t;
 
 #define __ARCH_SPIN_LOCK_UNLOCKED	{ 0 }
+#endif /* CONFIG_QUEUED_SPINLOCKS */
 
 #ifdef CONFIG_QUEUED_RWLOCKS
 #include <asm-generic/qrwlock_types.h>
