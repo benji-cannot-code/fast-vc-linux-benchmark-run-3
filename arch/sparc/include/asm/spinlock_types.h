e688ff237105b5b29617ffcf9613411e61692450
Babu Moger <babu.moger@oracle.com>
2017-05-24 17:55:09 -0600

    arch/sparc: Remove the check #ifndef __LINUX_SPINLOCK_TYPES_H
    Saw these compile errors on SPARC when queued rwlock feature is enabled.
    
       CC      kernel/locking/qrwlock.o
    In file included from ./include/asm-generic/qrwlock_types.h:5,
                      from ./arch/sparc/include/asm/qrwlock.h:4,
                      from kernel/locking/qrwlock.c:24:
    ./arch/sparc/include/asm/spinlock_types.h:5:3: error:
             #error "please don't include this file directly"
    
    SPARC has this guard which causes compile error when spinlock_types.h
    is included directly.
    @ifndef __LINUX_SPINLOCK_TYPES_H
    @ error "please don't include this file directly"
    @endif
    
    Remove this un-necessary "ifndef __LINUX_SPINLOCK_TYPES_H" stanza from SPARC.
    
    Signed-off-by: Babu Moger <babu.moger@oracle.com>
    Suggested-by: David Miller <davem@davemloft.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/asm/spinlock_types.h
+++ arch/sparc/include/asm/spinlock_types.h
@@ -2,10 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SPARC_SPINLOCK_TYPES_H
 #define __SPARC_SPINLOCK_TYPES_H
 
-#ifndef __LINUX_SPINLOCK_TYPES_H
-# error "please don't include this file directly"
-#endif
-
 typedef struct {
 	volatile unsigned char lock;
 } arch_spinlock_t;
