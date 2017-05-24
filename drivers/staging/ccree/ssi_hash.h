7b61058a3471e5ab382e00e273366bdaf121ecc7
Derek Robson <robsonde@gmail.com>
2017-05-24 16:43:11 +1200

    Drivers: ccree: ssi_hash.h - align block comments
    Fixed block comment alignment, Style fix only
    Found using checkpatch
    
    Signed-off-by: Derek Robson <robsonde@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/ssi_hash.h
+++ drivers/staging/ccree/ssi_hash.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 /* \file ssi_hash.h
-   ARM CryptoCell Hash Crypto API
+ * ARM CryptoCell Hash Crypto API
  */
 
 #ifndef __SSI_HASH_H__
