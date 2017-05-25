0774837f696ecf5d8dca367d05a341097ea84329
Levin, Alexander (Sasha Levin) <alexander.levin@verizon.com>
2017-05-25 12:58:47 +0000

    tools/lib/lockdep: Fix compilation for 4.11
     - More rcu stubs
     - New dummy headers due to sched header split
     - jhash2 included in due to kernel lockdep inclusion and usage
    
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: a.p.zijlstra@chello.nl
    Cc: ben@decadent.org.uk
    Link: http://lkml.kernel.org/r/20170525130005.5947-13-alexander.levin@verizon.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/lib/lockdep/uinclude/linux/compiler.h
+++ tools/lib/lockdep/uinclude/linux/compiler.h
@@ -8,4 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WRITE_ONCE(x, val) x=(val)
 #define RCU_INIT_POINTER(p, v) p=(v)
 
+#define __packed	__attribute__((packed))
+
 #endif
--- tools/lib/lockdep/uinclude/linux/rcu.h
+++ tools/lib/lockdep/uinclude/linux/rcu.h
@@ -19,4 +19,6 @@ static inline bool rcu_is_watching(void)
 	return false;
 }
 
+#define rcu_assign_pointer(p, v) ((p) = (v))
+
 #endif
