ae996bbda96956589b3957df4d0f877880dbc951
Levin, Alexander (Sasha Levin) <alexander.levin@verizon.com>
2017-05-25 12:58:53 +0000

    tools/lib/lockdep: Include err.h
    This provides PTR_ERR() now used by lockdep.
    
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: a.p.zijlstra@chello.nl
    Cc: ben@decadent.org.uk
    Link: http://lkml.kernel.org/r/20170525130005.5947-18-alexander.levin@verizon.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/lib/lockdep/uinclude/linux/kernel.h
+++ tools/lib/lockdep/uinclude/linux/kernel.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rcu.h>
 #include <linux/hardirq.h>
 #include <linux/kern_levels.h>
+#include <linux/err.h>
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
