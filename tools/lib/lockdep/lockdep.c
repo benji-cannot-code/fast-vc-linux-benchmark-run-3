9a37b6c448a963b8f02dcc6e9826f510fb165dd2
Ben Hutchings <ben@decadent.org.uk>
2017-05-25 12:58:31 +0000

    tools/lib/lockdep: Fix undefined symbol prandom_u32
    __lock_pin_lock() now calls prandom_u32() which is not defined in
    liblockdep.  __lock_pin_lock() and its caller lock_pin_lock() are dead
    code in liblockdep, but we still need to provide a definition of
    prandom_u32() in case lazy binding is disabled.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: a.p.zijlstra@chello.nl
    Fixes: e7904a28f533 ("locking/lockdep, sched/core: Implement a better ...")
    Link: http://lkml.kernel.org/r/20170525130005.5947-2-alexander.levin@verizon.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- tools/lib/lockdep/lockdep.c
+++ tools/lib/lockdep/lockdep.c
@@ -1,9 +1,16 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/lockdep.h>
+#include <stdlib.h>
 
 /* Trivial API wrappers, we don't (yet) have RCU in user-space: */
 #define hlist_for_each_entry_rcu	hlist_for_each_entry
 #define hlist_add_head_rcu		hlist_add_head
 #define hlist_del_rcu			hlist_del
+
+u32 prandom_u32(void)
+{
+	/* Used only by lock_pin_lock() which is dead code */
+	abort();
+}
 
 #include "../../../kernel/locking/lockdep.c"
