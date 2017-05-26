b8e759763af7ddf6d540427fffec410f27ea7157
Ido Schimmel <idosch@mellanox.com>
2017-05-26 08:37:27 +0200

    mlxsw: spectrum: Change signature of FID leave function
    When a vPort is destroyed, it leaves the FID it's currently mapped to
    (if any) and drops the reference. The FID's leave function expects to
    get the vPort as its argument, but this will have to change when the
    vPort model is retired.
    
    Change the function signature to expect a Port-VLAN struct instead and
    patch the call sites accordingly.
    
    The code introduced in this patch will be removed later in the patchset,
    but this intermediary step is required in order to ease the code review.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.h
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MLXSW_SP_KVD_LINEAR_SIZE 65536 /* entries */
 #define MLXSW_SP_KVD_GRANULARITY 128
 
+struct mlxsw_sp_port_vlan;
 struct mlxsw_sp_port;
 struct mlxsw_sp_rif;
 
@@ -80,7 +81,7 @@ struct mlxsw_sp_upper {
 };
 
 struct mlxsw_sp_fid {
-	void (*leave)(struct mlxsw_sp_port *mlxsw_sp_vport);
+	void (*leave)(struct mlxsw_sp_port_vlan *mlxsw_sp_port_vlan);
 	struct list_head list;
 	unsigned int ref_count;
 	struct net_device *dev;
