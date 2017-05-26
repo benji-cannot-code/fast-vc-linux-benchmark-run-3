875699253083f2feca4c7b0943c8b3832b2aa02c
Adrian Hunter <adrian.hunter@intel.com>
2017-05-26 11:17:16 +0300

    perf intel-pt: Add decoder support for CBR events
    Add decoder support for informing the tools of changes to the core-to-bus
    ratio (CBR).
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Link: http://lkml.kernel.org/r/1495786658-18063-16-git-send-email-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/intel-pt-decoder/intel-pt-decoder.h
+++ tools/perf/util/intel-pt-decoder/intel-pt-decoder.h
@@ -37,6 +37,7 @@ enum intel_pt_sample_type {
 	INTEL_PT_PWR_ENTRY	= 1 << 5,
 	INTEL_PT_EX_STOP	= 1 << 6,
 	INTEL_PT_PWR_EXIT	= 1 << 7,
+	INTEL_PT_CBR_CHG	= 1 << 8,
 };
 
 enum intel_pt_period_type {
@@ -74,6 +75,7 @@ struct intel_pt_state {
 	uint64_t mwait_payload;
 	uint64_t pwre_payload;
 	uint64_t pwrx_payload;
+	uint64_t cbr_payload;
 	uint32_t flags;
 	enum intel_pt_insn_op insn_op;
 	int insn_len;
