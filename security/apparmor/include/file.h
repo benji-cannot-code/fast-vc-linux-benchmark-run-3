ffa775aebee42c0862d7e0bea3a2d4dafa3cd424
John Johansen <john.johansen@canonical.com>
2017-05-26 15:07:22 -0700

    apparmor: rework perm mapping to a slightly broader set
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/include/file.h
+++ security/apparmor/include/file.h
@@ -23,10 +23,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct aa_profile;
 struct path;
 
+#define mask_mode_t(X) (X & (MAY_EXEC | MAY_WRITE | MAY_READ | MAY_APPEND))
 
 #define AA_AUDIT_FILE_MASK	(MAY_READ | MAY_WRITE | MAY_EXEC | MAY_APPEND |\
 				 AA_MAY_CREATE | AA_MAY_DELETE |	\
-				 AA_MAY_META_READ | AA_MAY_META_WRITE | \
+				 AA_MAY_GETATTR | AA_MAY_SETATTR | \
 				 AA_MAY_CHMOD | AA_MAY_CHOWN | AA_MAY_LOCK | \
 				 AA_EXEC_MMAP | AA_MAY_LINK)
 
@@ -38,7 +39,7 @@ struct path;
  * ctx struct will expand in the future so we keep the struct.
  */
 struct aa_file_ctx {
-	u16 allow;
+	u32 allow;
 };
 
 /**
