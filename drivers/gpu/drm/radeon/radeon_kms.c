8529079ccf5ee85d368d10cd555b6fc392d94a60
Michel Dänzer <michel.daenzer@amd.com>
2017-05-29 17:32:38 +0900

    drm/radeon: Make si_support and cik_support parameters always available
    This will allow amdgpu-pro / other out-of-tree amdgpu builds to make use
    of these options for using the out-of-tree amdgpu driver instead of the
    in-tree radeon driver in a clean way.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Michel Dänzer <michel.daenzer@amd.com>

--- drivers/gpu/drm/radeon/radeon_kms.c
+++ drivers/gpu/drm/radeon/radeon_kms.c
@@ -99,7 +99,6 @@ int radeon_driver_load_kms(struct drm_device *dev, unsigned long flags)
 	struct radeon_device *rdev;
 	int r, acpi_status;
 
-#ifdef CONFIG_DRM_AMDGPU_SI
 	if (!radeon_si_support) {
 		switch (flags & RADEON_FAMILY_MASK) {
 		case CHIP_TAHITI:
@@ -112,8 +111,6 @@ int radeon_driver_load_kms(struct drm_device *dev, unsigned long flags)
 			return -ENODEV;
 		}
 	}
-#endif
-#ifdef CONFIG_DRM_AMDGPU_CIK
 	if (!radeon_cik_support) {
 		switch (flags & RADEON_FAMILY_MASK) {
 		case CHIP_KAVERI:
@@ -129,7 +126,6 @@ int radeon_driver_load_kms(struct drm_device *dev, unsigned long flags)
 			return -ENODEV;
 		}
 	}
-#endif
 
 	rdev = kzalloc(sizeof(struct radeon_device), GFP_KERNEL);
 	if (rdev == NULL) {
