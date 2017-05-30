2301d024d932da0298f8de6b0b7c2c7e5d6d4845
Derek Robson <robsonde@gmail.com>
2017-05-30 18:15:09 +1200

    Drivers: ccree: ssi_cipher.h - align block comments
    Fixed block comment alignment, Style fix only
    Found using checkpatch
    
    Signed-off-by: Derek Robson <robsonde@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/ssi_cipher.h
+++ drivers/staging/ccree/ssi_cipher.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 /* \file ssi_cipher.h
-   ARM CryptoCell Cipher Crypto API
+ * ARM CryptoCell Cipher Crypto API
  */
 
 #ifndef __SSI_CIPHER_H__
