2d9422935776d175d76b283e33ea71f335f9e4d3
Srikanth Jampala <Jampala.Srikanth@cavium.com>
2017-05-30 17:28:02 +0530

    crypto: cavium - Add debugfs support in CNN55XX driver.
    Add debugfs support in CNN55XX Physical Function driver.
    Provides hardware counters and firmware information.
    
    Signed-off-by: Srikanth Jampala <Jampala.Srikanth@cavium.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/cavium/nitrox/nitrox_dev.h
+++ drivers/crypto/cavium/nitrox/nitrox_dev.h
@@ -119,6 +119,7 @@ struct nitrox_bh {
  * @msix: MSI-X information
  * @bh: post processing work
  * @hw: hardware information
+ * @debugfs_dir: debugfs directory
  */
 struct nitrox_device {
 	struct list_head list;
@@ -142,6 +143,9 @@ struct nitrox_device {
 	struct nitrox_bh bh;
 
 	struct nitrox_hw hw;
+#if IS_ENABLED(CONFIG_DEBUG_FS)
+	struct dentry *debugfs_dir;
+#endif
 };
 
 /**
