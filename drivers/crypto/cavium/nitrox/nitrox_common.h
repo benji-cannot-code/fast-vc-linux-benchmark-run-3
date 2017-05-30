f2af6966a4e7104d9c67f935de5ea081503a1277
Srikanth Jampala <Jampala.Srikanth@cavium.com>
2017-05-30 17:28:03 +0530

    crypto: cavium - Register the CNN55XX supported crypto algorithms.
    Register the Symmetric crypto algorithms supported by
    CNN55XX driver with crypto subsystem.
    
    The following Symmetric crypto algorithms are supported,
      - aes with cbc, ecb, cfb, xts, ctr and cts modes
      - des3_ede with cbc and ecb modes
    
    Signed-off-by: Srikanth Jampala <Jampala.Srikanth@cavium.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/cavium/nitrox/nitrox_common.h
+++ drivers/crypto/cavium/nitrox/nitrox_common.h
@@ -5,6 +5,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "nitrox_dev.h"
 #include "nitrox_req.h"
 
+int nitrox_crypto_register(void);
+void nitrox_crypto_unregister(void);
+void *crypto_alloc_context(struct nitrox_device *ndev);
+void crypto_free_context(void *ctx);
+struct nitrox_device *nitrox_get_first_device(void);
+void nitrox_put_device(struct nitrox_device *ndev);
+
 void nitrox_pf_cleanup_isr(struct nitrox_device *ndev);
 int nitrox_pf_init_isr(struct nitrox_device *ndev);
 
