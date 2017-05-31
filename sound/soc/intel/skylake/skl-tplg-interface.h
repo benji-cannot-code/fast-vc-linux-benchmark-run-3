6a76b18f65acf768499f443bf9b6405b25de362f
Dharageswari R <dharageswari.r@intel.com>
2017-05-31 10:30:25 +0530

    ASoC: Intel: Skylake: Add enum control for mic selection
    User may prefer to select data from particular mics. A mic-select module
    in DSP allows this selection.
    
    Create possible enum controls to allow user to select a combination of
    mics to capture data from. Based on the user selection, parameters are
    generated and passed to mic-select module during init.
    
    Signed-off-by: Dharageswari R <dharageswari.r@intel.com>
    Signed-off-by: Subhransu S. Prusty <subhransu.s.prusty@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/skl-tplg-interface.h
+++ sound/soc/intel/skylake/skl-tplg-interface.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * SST types start at higher to avoid any overlapping in future
  */
 #define SKL_CONTROL_TYPE_BYTE_TLV	0x100
+#define SKL_CONTROL_TYPE_MIC_SELECT	0x102
 
 #define HDA_SST_CFG_MAX	900 /* size of copier cfg*/
 #define MAX_IN_QUEUE 8
