51430f03f4b4e643c540eefbbc1106d323bbd368
Chen-Yu Tsai <wens@csie.org>
2017-05-31 15:58:21 +0800

    clk: sunxi-ng: h3: Export PLL_PERIPH0 clock for the PRCM
    The PRCM takes PLL_PERIPH0 as one of its parents for the AR100 clock.
    As such we need to be able to describe this relationship in the device
    tree.
    
    Export the PLL_PERIPH0 clock so we can reference it in the PRCM node.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- include/dt-bindings/clock/sun8i-h3-ccu.h
+++ include/dt-bindings/clock/sun8i-h3-ccu.h
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _DT_BINDINGS_CLK_SUN8I_H3_H_
 #define _DT_BINDINGS_CLK_SUN8I_H3_H_
 
+#define CLK_PLL_PERIPH0		9
+
 #define CLK_CPUX		14
 
 #define CLK_BUS_CE		20
