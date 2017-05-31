c3c3c2bdf0651f19f292ddc3990c7535e8099f21
David Sterba <dsterba@suse.com>
2017-05-31 19:44:31 +0200

    btrfs: adjust includes after vmalloc removal
    As we don't use vmalloc/vzalloc/vfree directly in ctree.c, we can now
    use the proper header that defines kvmalloc.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/rbtree.h>
-#include <linux/vmalloc.h>
+#include <linux/mm.h>
 #include "ctree.h"
 #include "disk-io.h"
 #include "transaction.h"
