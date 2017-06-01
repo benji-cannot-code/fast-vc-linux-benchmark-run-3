9b549ca2ee3a0d34ad18c38404cdc62f6c48ebd5
Hersen Wu <hersenxs.wu@amd.com>
2017-06-01 15:49:16 -0400

    drm/amd/display: remove disable_clk_gate debug flag for DCN
    Signed-off-by: Hersen Wu <hersenxs.wu@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_resource.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_resource.c
@@ -439,7 +439,6 @@ static const struct dc_debug debug_defaults_drv = {
 };
 
 static const struct dc_debug debug_defaults_diags = {
-		.disable_clock_gate = true,
 		.disable_dmcu = true,
 		.force_abm_enable = false,
 		.timing_trace = true,
