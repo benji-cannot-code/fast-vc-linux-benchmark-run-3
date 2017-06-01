31adee7411e2806d5efcd88d74cb34302ca231aa
Markus Elfring <elfring@users.sourceforge.net>
2017-06-01 22:17:03 +0200

    ARM: OMAP1: Fix a typo in a comment line
    Adjust a line in this description for the software module.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap1/timer.c
+++ arch/arm/mach-omap1/timer.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Contains first level initialization routines which internally
  * generates timer device information and registers with linux
- * device model. It also has low level function to chnage the timer
+ * device model. It also has a low level function to change the timer
  * input clock source.
  *
  * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
