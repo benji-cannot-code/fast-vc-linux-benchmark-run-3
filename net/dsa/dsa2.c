2f5a4fca4970b9c09d9e580359ca3a01ecfa7e51
Florian Fainelli <f.fainelli@gmail.com>
2017-06-02 12:31:21 -0700

    net: dsa: Pass dsa_port reference to ethtool setup/restore
    We do not need to have a reference to a dsa_switch, instead we should
    pass a reference to a CPU dsa_port, change that. This is a preliminary
    change to better support multiple CPU ports.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dsa/dsa2.c
+++ net/dsa/dsa2.c
@@ -445,7 +445,7 @@ static int dsa_dst_apply(struct dsa_switch_tree *dst)
 	}
 
 	if (dst->cpu_dp) {
-		err = dsa_cpu_port_ethtool_setup(dst->cpu_dp->ds);
+		err = dsa_cpu_port_ethtool_setup(dst->cpu_dp);
 		if (err)
 			return err;
 	}
@@ -486,7 +486,7 @@ static void dsa_dst_unapply(struct dsa_switch_tree *dst)
 	}
 
 	if (dst->cpu_dp)
-		dsa_cpu_port_ethtool_restore(dst->cpu_dp->ds);
+		dsa_cpu_port_ethtool_restore(dst->cpu_dp);
 
 	pr_info("DSA: tree %d unapplied\n", dst->tree);
 	dst->applied = false;
