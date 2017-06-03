48906825ac59b944dabf1fc0fb28890cb1457c5a
Christoph Hellwig <hch@lst.de>
2017-06-03 21:01:02 +0200

    signal: Remove non-uapi <asm/siginfo.h>
    By moving the kernel side __SI_* defintions right next to the userspace
    ones we can kill the non-uapi versions of <asm/siginfo.h> include
    include/asm-generic/siginfo.h and untangle the unholy mess of includes.
    
    [ tglx: Removed uapi/asm/siginfo.h from m32r, microblaze, mn10300 and score ]
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-arch@vger.kernel.org
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: linux-ia64@vger.kernel.org
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: sparclinux@vger.kernel.org
    Cc: "David S. Miller" <davem@davemloft.net>
    Link: http://lkml.kernel.org/r/20170603190102.28866-6-hch@lst.de

--- arch/m32r/include/uapi/asm/siginfo.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _M32R_SIGINFO_H
-#define _M32R_SIGINFO_H
-
-#include <asm-generic/siginfo.h>
-
-#endif /* _M32R_SIGINFO_H */
