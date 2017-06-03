00aade2f18538fa15f01cd0318e1bb1c2639bb28
Christoph Hellwig <hch@lst.de>
2017-06-03 21:00:58 +0200

    sparc: Simplify <asm/siginfo.h>
    There is no need for the forward declaration of compat_siginfo provided
    here.  We can't yet use the generic header as we need to pull in the
    sparc-specific version of the uapi <asm/siginfo.h>, but this prepares
    for removing the non-uapi <asm/siginfo.h> entirely.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: "David S. Miller" <davem@davemloft.net>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-arch@vger.kernel.org
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: linux-ia64@vger.kernel.org
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: sparclinux@vger.kernel.org
    Link: http://lkml.kernel.org/r/20170603190102.28866-2-hch@lst.de

--- arch/sparc/include/asm/siginfo.h
+++ arch/sparc/include/asm/siginfo.h
@@ -1,14 +1,2 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef __SPARC_SIGINFO_H
-#define __SPARC_SIGINFO_H
-
 #include <uapi/asm/siginfo.h>
-
-
-#ifdef CONFIG_COMPAT
-
-struct compat_siginfo;
-
-#endif /* CONFIG_COMPAT */
-
-#endif /* !(__SPARC_SIGINFO_H) */
