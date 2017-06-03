d457a93ee23ac345c4bea8fdf406629cef9928be
Christoph Hellwig <hch@lst.de>
2017-06-03 21:01:01 +0200

    signal: Move copy_siginfo_to_user to <linux/signal.h>
    Having it in asm-generic/siginfo.h doesn't make any sense as it is in no way
    architecture specific.  Move it to signal.h instead where several related
    functions already reside.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-arch@vger.kernel.org
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: linux-ia64@vger.kernel.org
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: sparclinux@vger.kernel.org
    Cc: "David S. Miller" <davem@davemloft.net>
    Link: http://lkml.kernel.org/r/20170603190102.28866-5-hch@lst.de

--- include/asm-generic/siginfo.h
+++ include/asm-generic/siginfo.h
@@ -15,8 +15,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __SI_SYS	(7 << 16)
 #define __SI_CODE(T,N)	((T) | ((N) & 0xffff))
 
-struct siginfo;
-
-extern int copy_siginfo_to_user(struct siginfo __user *to, const struct siginfo *from);
-
 #endif
