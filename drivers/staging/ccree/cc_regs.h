e39a6d65d8589aa4c51c2593059291101a5a95d1
Gilad Ben-Yossef <gilad@benyossef.com>
2017-06-04 11:02:35 +0300

    staging: ccree: remove spurious blank line
    Remove spurious blank line from cc_regs.h
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/cc_regs.h
+++ drivers/staging/ccree/cc_regs.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-
 /*!
  * @file
  * @brief This file contains macro definitions for accessing ARM TrustZone
