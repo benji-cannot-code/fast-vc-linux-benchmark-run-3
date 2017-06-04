ee6c065b26a5b1dd628f568ac475f21e9cab6d93
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2017-06-04 20:33:39 +0200

    clk: meson: meson8b: add compatibles for Meson8 and Meson8m2
    The clock controller on Meson8, Meson8b and Meson8m2 is very similar
    based on the code from the Amlogic GPL kernel sources. Add separate
    compatibles for each SoC to make sure that we can easily implement
    all the small differences for each SoC later on.
    
    In general the Meson8 and Meson8m2 seem to be almost identical as they
    even share the same mach-meson8 directory in Amlogic's GPL kernel
    sources.
    The main clocks on Meson8, Meson8b and Meson8m2 are very similar,
    because they are all using the same PLL values, 90% of the clock gates
    are the same (the actual diffstat of the mach-meson8/clock.c and
    mach-meson8b/clock.c files is around 30 to 40 lines, when excluding
    all commented out code).
    The difference between the Meson8 and Meson8b clock gates seem to be:
    - Meson8 has AIU_PCLK, HDMI_RX, VCLK2_ENCT, VCLK2_ENCL, UART3,
      CSI_DIG_CLKIN gates which don't seem to be available on Meson8b
    - the gate on Meson8 for bit 7 seems to be named "_1200XXX" instead
      of "PERIPHS_TOP" (on Meson8b)
    - Meson8b has a SANA gate which doesn't seem to exist on Meson8 (or
      on Meson8 the same bit is used by the UART3 gate in Amlogic's GPL
      kernel sources)
    None of these gates is added for now, since it's unclear whether these
    definitions are actually correct (the VCLK2_ENCT gate for example is
    defined, but only used in some commented block).
    
    The main difference between all three SoCs seem to be the video (VPU)
    clocks. Apart from different supported clock rates (according to vpu.c
    in mach-meson8 and mach-meson8b from Amlogic's GPL kernel sources) the
    most notable difference is that Meson8m2 has a GP_PLL clock and a mux
    (probably the same as on the Meson GX SoCs) to support glitch-free
    (clock rate) switching.
    None of these VPU clocks are not supported by our mainline meson8b
    clock driver yet though.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Acked-by: Kevin Hilman <khilman@baylibre.com>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- drivers/clk/meson/meson8b.c
+++ drivers/clk/meson/meson8b.c
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * AmLogic S805 / Meson8b Clock Controller Driver
+ * AmLogic S802 (Meson8) / S805 (Meson8b) / S812 (Meson8m2) Clock Controller
+ * Driver
  *
  * Copyright (c) 2015 Endless Mobile, Inc.
  * Author: Carlo Caione <carlo@endlessm.com>
@@ -778,7 +779,9 @@ iounmap:
 }
 
 static const struct of_device_id meson8b_clkc_match_table[] = {
+	{ .compatible = "amlogic,meson8-clkc" },
 	{ .compatible = "amlogic,meson8b-clkc" },
+	{ .compatible = "amlogic,meson8m2-clkc" },
 	{ }
 };
 
