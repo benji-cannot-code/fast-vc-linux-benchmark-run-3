5a8064b3f62b476e059d206ea46f9e6d22505619
Julian Wiedmann <jwi@linux.vnet.ibm.com>
2017-06-06 14:33:50 +0200

    s390/qeth: do early device setup for z/VM IQD NICs
    qeth currently supports early setup for OSM and OSN devices.
    This patch adds early setup support for z/VM HiperSockets,
    since they can only be coupled to L3 networks.
    
    Based on an initial version by Dmitriy Lakhvich.
    
    Signed-off-by: Julian Wiedmann <jwi@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/s390/net/qeth_core.h
+++ drivers/s390/net/qeth_core.h
@@ -660,6 +660,7 @@ struct qeth_card_info {
 	int max_mtu;
 	int broadcast_capable;
 	int unique_id;
+	bool layer_enforced;
 	struct qeth_card_blkt blkt;
 	enum qeth_ipa_promisc_modes promisc_mode;
 	__u32 diagass_support;
@@ -697,6 +698,7 @@ struct qeth_osn_info {
 };
 
 enum qeth_discipline_id {
+	QETH_DISCIPLINE_UNDETERMINED = -1,
 	QETH_DISCIPLINE_LAYER3 = 0,
 	QETH_DISCIPLINE_LAYER2 = 1,
 };
