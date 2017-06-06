6c191805db8b6bcc2bfe44e23040f6a688942da8
Jeremy Kerr <jk@ozlabs.org>
2017-06-06 16:08:39 -0500

    lib: Add crc4 module
    Add a little helper for crc4 calculations. This works 4-bits-at-a-time,
    using a simple table approach.
    
    We will need this in the FSI core code, as well as any master
    implementations that need to calculate CRCs in software.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Chris Bostic <cbostic@linux.vnet.ibm.com>
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

---
+++ include/linux/crc4.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _LINUX_CRC4_H
+#define _LINUX_CRC4_H
+
+#include <linux/types.h>
+
+extern uint8_t crc4(uint8_t c, uint64_t x, int bits);
+
+#endif /* _LINUX_CRC4_H */
