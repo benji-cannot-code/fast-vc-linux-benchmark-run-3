7f2fcd150fb3dedb17c41bc2e69dcb8d4708e1b8
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-06-06 17:00:22 +0000

    arm64: ftrace: add support for far branches to dynamic ftrace
    Currently, dynamic ftrace support in the arm64 kernel assumes that all
    core kernel code is within range of ordinary branch instructions that
    occur in module code, which is usually the case, but is no longer
    guaranteed now that we have support for module PLTs and address space
    randomization.
    
    Since on arm64, all patching of branch instructions involves function
    calls to the same entry point [ftrace_caller()], we can emit the modules
    with a trampoline that has unlimited range, and patch both the trampoline
    itself and the branch instruction to redirect the call via the trampoline.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    [will: minor clarification to smp_wmb() comment]
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/module.h
+++ arch/arm64/include/asm/module.h
@@ -31,6 +31,9 @@ struct mod_plt_sec {
 struct mod_arch_specific {
 	struct mod_plt_sec	core;
 	struct mod_plt_sec	init;
+
+	/* for CONFIG_DYNAMIC_FTRACE */
+	void			*ftrace_trampoline;
 };
 #endif
 
