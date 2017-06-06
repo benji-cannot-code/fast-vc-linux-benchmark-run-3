484bcf6e871c4405bbc5a2cd807e1d8789d817a0
Ioana Radulescu <ruxandra.radulescu@nxp.com>
2017-06-06 10:00:32 -0500

    staging: fsl-dpaa2/eth: Reset dpbp
    Reset the buffer pool object before using it, like we do
    for the other DPAA2 objects.
    
    Signed-off-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
+++ drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
@@ -1688,6 +1688,12 @@ static int setup_dpbp(struct dpaa2_eth_priv *priv)
 		goto err_open;
 	}
 
+	err = dpbp_reset(priv->mc_io, 0, dpbp_dev->mc_handle);
+	if (err) {
+		dev_err(dev, "dpbp_reset() failed\n");
+		goto err_reset;
+	}
+
 	err = dpbp_enable(priv->mc_io, 0, dpbp_dev->mc_handle);
 	if (err) {
 		dev_err(dev, "dpbp_enable() failed\n");
@@ -1706,6 +1712,7 @@ static int setup_dpbp(struct dpaa2_eth_priv *priv)
 err_get_attr:
 	dpbp_disable(priv->mc_io, 0, dpbp_dev->mc_handle);
 err_enable:
+err_reset:
 	dpbp_close(priv->mc_io, 0, dpbp_dev->mc_handle);
 err_open:
 	fsl_mc_object_free(dpbp_dev);
