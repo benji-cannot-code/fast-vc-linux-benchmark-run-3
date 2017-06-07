a31c18dd913ceb807b26c212218ad0958d90ded7
Ulf Hansson <ulf.hansson@linaro.org>
2017-06-07 11:08:21 +0200

    Bluetooth: hci_ll: Add support for the external clock
    Add support to manage the external clock provided to the WiLink combo chip
    as it's needed for any of the transport interfaces.
    
    To avoid breaking platforms not yet specifying the external clock, we make
    it optional. In case the clock is successfully fetched during ->probe(),
    let's manage it via the ->open|close() callbacks, to make sure the device
    get properly powered on/off.
    
    Fixes: ea452678734e ("arm64: dts: hikey: Fix WiFi support")
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Tested-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_ll.c
+++ drivers/bluetooth/hci_ll.c
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serdev.h>
 #include <linux/skbuff.h>
 #include <linux/ti_wilink_st.h>
+#include <linux/clk.h>
 
 #include <net/bluetooth/bluetooth.h>
 #include <net/bluetooth/hci_core.h>
@@ -85,6 +86,7 @@ struct ll_device {
 	struct hci_uart hu;
 	struct serdev_device *serdev;
 	struct gpio_desc *enable_gpio;
+	struct clk *ext_clk;
 };
 
 struct ll_struct {
@@ -147,8 +149,12 @@ static int ll_open(struct hci_uart *hu)
 
 	hu->priv = ll;
 
-	if (hu->serdev)
+	if (hu->serdev) {
+		struct ll_device *lldev = serdev_device_get_drvdata(hu->serdev);
 		serdev_device_open(hu->serdev);
+		if (!IS_ERR(lldev->ext_clk))
+			clk_prepare_enable(lldev->ext_clk);
+	}
 
 	return 0;
 }
@@ -181,6 +187,8 @@ static int ll_close(struct hci_uart *hu)
 	if (hu->serdev) {
 		struct ll_device *lldev = serdev_device_get_drvdata(hu->serdev);
 		gpiod_set_value_cansleep(lldev->enable_gpio, 0);
+
+		clk_disable_unprepare(lldev->ext_clk);
 
 		serdev_device_close(hu->serdev);
 	}
@@ -721,6 +729,10 @@ static int hci_ti_probe(struct serdev_device *serdev)
 	lldev->enable_gpio = devm_gpiod_get_optional(&serdev->dev, "enable", GPIOD_OUT_LOW);
 	if (IS_ERR(lldev->enable_gpio))
 		return PTR_ERR(lldev->enable_gpio);
+
+	lldev->ext_clk = devm_clk_get(&serdev->dev, "ext_clock");
+	if (IS_ERR(lldev->ext_clk) && PTR_ERR(lldev->ext_clk) != -ENOENT)
+		return PTR_ERR(lldev->ext_clk);
 
 	of_property_read_u32(serdev->dev.of_node, "max-speed", &max_speed);
 	hci_uart_set_speeds(hu, 115200, max_speed);
