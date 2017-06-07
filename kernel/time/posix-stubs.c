2b6b6051645861b16773dc9f09567944726d1f4c
Al Viro <viro@zeniv.linux.org.uk>
2017-06-07 09:42:35 +0100

    posix-timers: Take compat timer_settime(2) to native one
    ... and get rid of set_fs() in there
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/20170607084241.28657-10-viro@ZenIV.linux.org.uk

--- kernel/time/posix-stubs.c
+++ kernel/time/posix-stubs.c
@@ -43,6 +43,7 @@ SYS_NI(setitimer);
 SYS_NI(alarm);
 #endif
 COMPAT_SYS_NI(clock_adjtime);
+COMPAT_SYS_NI(timer_settime);
 
 /*
  * We preserve minimal support for CLOCK_REALTIME and CLOCK_MONOTONIC
