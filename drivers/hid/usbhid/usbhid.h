dd6b6a3cf6a57b96c5f58d4687edc9f98b3241d4
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2017-06-06 23:59:32 -0700

    HID: hiddev: use hid_hw_power instead of usbhid_get/put_power
    Instead of calling into usbhid code directly, let's use the standard
    accessors for the transport HID drivers, and stop clobbering their error
    codes with -EIO.
    
    This also allows us to remove usbhid_get/put_power(), leaving only
    usbhid_power().
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Reviewed-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/usbhid/usbhid.h
+++ drivers/hid/usbhid/usbhid.h
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*  API provided by hid-core.c for USB HID drivers */
 void usbhid_init_reports(struct hid_device *hid);
-int usbhid_get_power(struct hid_device *hid);
-void usbhid_put_power(struct hid_device *hid);
 struct usb_interface *usbhid_find_interface(int minor);
 
 /* iofl flags */
