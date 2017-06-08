d58bf729264c0c3be4422ec17968adb7780eaa89
Ulf Hansson <ulf.hansson@linaro.org>
2017-06-08 15:27:37 +0200

    mmc: core: Don't export some eMMC specific functions from core.c
    The mmc_start|stop_bkops(), mmc_read_bkops_status() and mmc_interrupt_hpi()
    functions are all used from within the mmc core module, thus there are no
    need to use EXPORT_SYMBOL() for them, so let's remove it.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Shawn Lin <shawn.lin@rock-chips.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -429,7 +429,6 @@ void mmc_start_bkops(struct mmc_card *card, bool from_exception)
 out:
 	mmc_release_host(card->host);
 }
-EXPORT_SYMBOL(mmc_start_bkops);
 
 /*
  * mmc_wait_data_done() - done callback for data request
@@ -812,7 +811,6 @@ out:
 	mmc_release_host(card->host);
 	return err;
 }
-EXPORT_SYMBOL(mmc_interrupt_hpi);
 
 /**
  *	mmc_wait_for_cmd - start a command and wait for completion
@@ -870,7 +868,6 @@ int mmc_stop_bkops(struct mmc_card *card)
 
 	return err;
 }
-EXPORT_SYMBOL(mmc_stop_bkops);
 
 int mmc_read_bkops_status(struct mmc_card *card)
 {
@@ -888,7 +885,6 @@ int mmc_read_bkops_status(struct mmc_card *card)
 	kfree(ext_csd);
 	return 0;
 }
-EXPORT_SYMBOL(mmc_read_bkops_status);
 
 /**
  *	mmc_set_data_timeout - set the timeout for a data command
