bf68e1ec8a6284670b743c435f861ea6b4fef01a
Laurentiu Tudor <laurentiu.tudor@nxp.com>
2017-06-08 17:28:51 +0300

    staging: fsl-mc: drop unused forward declaration
    This forward declaration of "struct fsl_mc_resource" is of no use so
    drop it.
    
    Signed-off-by: Laurentiu Tudor <laurentiu.tudor@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/include/mc-sys.h
+++ drivers/staging/fsl-mc/include/mc-sys.h
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define FSL_MC_IO_ATOMIC_CONTEXT_PORTAL	0x0001
 
-struct fsl_mc_resource;
 struct mc_command;
 
 /**
