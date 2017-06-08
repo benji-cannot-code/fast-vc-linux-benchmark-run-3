14dc4cda69bc179149f3926483bc675822cdf3d0
Yuyang Du <yuyang.du@intel.com>
2017-06-08 13:04:05 +0800

    usbip: vhci-hcd: Rename function names to reflect their struct names
    These helper function names are renamed to have their full struct
    names to avoid confusion:
    
     - hcd_to_vhci() -> hcd_to_vhci_hcd()
     - vhci_to_hcd() -> vhci_hcd_to_hcd()
     - vdev_to_vhci() -> vdev_to_vhci_hcd()
    
    Signed-off-by: Yuyang Du <yuyang.du@intel.com>
    Acked-by: Shuah Khan <shuahkh@osg.samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/usbip/vhci_sysfs.c
+++ drivers/usb/usbip/vhci_sysfs.c
@@ -44,7 +44,7 @@ static ssize_t status_show_vhci(int pdev_nr, char *out)
 		return 0;
 	}
 
-	vhci = hcd_to_vhci(platform_get_drvdata(pdev));
+	vhci = hcd_to_vhci_hcd(platform_get_drvdata(pdev));
 
 	spin_lock_irqsave(&vhci->lock, flags);
 
@@ -213,7 +213,7 @@ static ssize_t store_detach(struct device *dev, struct device_attribute *attr,
 		return -EAGAIN;
 	}
 
-	ret = vhci_port_disconnect(hcd_to_vhci(hcd), rhport);
+	ret = vhci_port_disconnect(hcd_to_vhci_hcd(hcd), rhport);
 	if (ret < 0)
 		return -EINVAL;
 
@@ -293,7 +293,7 @@ static ssize_t store_attach(struct device *dev, struct device_attribute *attr,
 		dev_err(dev, "port %d is not ready\n", port);
 		return -EAGAIN;
 	}
-	vhci = hcd_to_vhci(hcd);
+	vhci = hcd_to_vhci_hcd(hcd);
 	vdev = &vhci->vdev[rhport];
 
 	/* Extract socket from fd. */
