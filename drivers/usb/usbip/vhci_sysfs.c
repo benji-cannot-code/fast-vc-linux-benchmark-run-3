63b42d7ec1cd99ffc784d41e8b8a3bac71e33253
Yuyang Du <yuyang.du@intel.com>
2017-06-08 13:04:13 +0800

    usbip: vhci-hcd: Clean up the code by adding a new macro
    Each vhci has 2*VHCI_HC_PORTS ports, in which VHCI_HC_PORTS
    ports are HighSpeed (or below), and VHCI_HC_PORTS are SuperSpeed.
    This new macro VHCI_PORTS reflects this configuration.
    
    Signed-off-by: Yuyang Du <yuyang.du@intel.com>
    Acked-by: Shuah Khan <shuahkh@osg.samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/usbip/vhci_sysfs.c
+++ drivers/usb/usbip/vhci_sysfs.c
@@ -93,7 +93,7 @@ static ssize_t status_show_vhci(int pdev_nr, char *out)
 
 		spin_lock(&vdev->ud.lock);
 		port_show_vhci(&out, HUB_SPEED_HIGH,
-			       pdev_nr * VHCI_HC_PORTS * 2 + i, vdev);
+			       pdev_nr * VHCI_PORTS + i, vdev);
 		spin_unlock(&vdev->ud.lock);
 	}
 
@@ -102,7 +102,7 @@ static ssize_t status_show_vhci(int pdev_nr, char *out)
 
 		spin_lock(&vdev->ud.lock);
 		port_show_vhci(&out, HUB_SPEED_SUPER,
-			       pdev_nr * VHCI_HC_PORTS * 2 + VHCI_HC_PORTS + i, vdev);
+			       pdev_nr * VHCI_PORTS + VHCI_HC_PORTS + i, vdev);
 		spin_unlock(&vdev->ud.lock);
 	}
 
@@ -118,7 +118,7 @@ static ssize_t status_show_not_ready(int pdev_nr, char *out)
 
 	for (i = 0; i < VHCI_HC_PORTS; i++) {
 		out += sprintf(out, "hs  %04u %03u ",
-				    (pdev_nr * VHCI_HC_PORTS * 2) + i,
+				    (pdev_nr * VHCI_PORTS) + i,
 				    VDEV_ST_NOTASSIGNED);
 		out += sprintf(out, "000 00000000 0000000000000000 0-0");
 		out += sprintf(out, "\n");
@@ -126,7 +126,7 @@ static ssize_t status_show_not_ready(int pdev_nr, char *out)
 
 	for (i = 0; i < VHCI_HC_PORTS; i++) {
 		out += sprintf(out, "ss  %04u %03u ",
-				    (pdev_nr * VHCI_HC_PORTS * 2) + VHCI_HC_PORTS + i,
+				    (pdev_nr * VHCI_PORTS) + VHCI_HC_PORTS + i,
 				    VDEV_ST_NOTASSIGNED);
 		out += sprintf(out, "000 00000000 0000000000000000 0-0");
 		out += sprintf(out, "\n");
@@ -177,7 +177,7 @@ static ssize_t nports_show(struct device *dev, struct device_attribute *attr,
 	/*
 	 * Half the ports are for SPEED_HIGH and half for SPEED_SUPER, thus the * 2.
 	 */
-	out += sprintf(out, "%d\n", VHCI_HC_PORTS * vhci_num_controllers * 2);
+	out += sprintf(out, "%d\n", VHCI_PORTS * vhci_num_controllers);
 	return out - s;
 }
 static DEVICE_ATTR_RO(nports);
