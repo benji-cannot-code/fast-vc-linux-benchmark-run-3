d9b2da1597abd979573ca208131bd6c1bf87ab69
Laurentiu Tudor <laurentiu.tudor@nxp.com>
2017-06-08 17:28:49 +0300

    staging: fsl-mc: drop a few useless #includes
    Some #includes were needlessly done from header files. Drop them from
    there and update the only .c file that implicitly needed one of those
    #includes.
    
    Signed-off-by: Laurentiu Tudor <laurentiu.tudor@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/fsl-mc-msi.c
+++ drivers/staging/fsl-mc/bus/fsl-mc-msi.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irqdomain.h>
 #include <linux/msi.h>
 #include "../include/mc-bus.h"
+#include "../include/mc-cmd.h"
 #include "fsl-mc-private.h"
 
 /*
--- drivers/staging/fsl-mc/include/dprc.h
+++ drivers/staging/fsl-mc/include/dprc.h
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _FSL_DPRC_H
 #define _FSL_DPRC_H
 
-#include "mc-cmd.h"
-
 /*
  * Data Path Resource Container API
  * Contains DPRC API for managing and querying DPAA resources
