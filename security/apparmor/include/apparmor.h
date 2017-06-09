b17130341fa2eb29db3abee422dcc53b97bc6e39
John Johansen <john.johansen@canonical.com>
2017-06-09 14:22:14 -0700

    apparmor: move ptrace checks to using labels
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/include/apparmor.h
+++ security/apparmor/include/apparmor.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AA_CLASS_NET		4
 #define AA_CLASS_RLIMITS	5
 #define AA_CLASS_DOMAIN		6
+#define AA_CLASS_PTRACE		9
 #define AA_CLASS_LABEL		16
 
 #define AA_CLASS_LAST		AA_CLASS_LABEL
