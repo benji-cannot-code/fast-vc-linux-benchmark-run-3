c66728a8ea6842ae2ab3fc841d3d8ff6661d2d71
Arend Van Spriel <arend.vanspriel@broadcom.com>
2017-06-09 12:19:20 +0100

    brcmfmac: add support multi-scheduled scan
    This change adds support for multi-scheduled scan in the driver. It
    currently relies on g-scan support in firmware and will set struct
    wiphy::max_sched_scan_reqs accordingly. This is limited to 16 concurrent
    requests.
    
    The firmware currently has a limit of 64 channels that can be configured
    for all requests in total regardless whether there are duplicates. So if
    a request uses 35 channels there are 29 channels left for another request.
    When user-space does not specify any channels cfg80211 will add all
    channels defined by the wiphy instance to the request, which makes
    reaching the limit rather easy for dual-band devices.
    
    Reviewed-by: Hante Meuleman <hante.meuleman@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieter-paul.giesberts@broadcom.com>
    Reviewed-by: Franky Lin <franky.lin@broadcom.com>
    Signed-off-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "debug.h"
 #include "fwil_types.h"
 #include "p2p.h"
+#include "pno.h"
 #include "cfg80211.h"
 #include "fwil.h"
 #include "feature.h"
