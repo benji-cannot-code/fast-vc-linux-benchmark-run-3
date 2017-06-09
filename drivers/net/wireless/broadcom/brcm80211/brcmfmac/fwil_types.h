edee8f142b08920ad54382c8d0e7d2bc3f451dfb
Arend van Spriel <arend@broadcom.com>
2017-06-09 13:08:47 +0100

    brcmfmac: support 4-way handshake offloading for WPA/WPA2-PSK
    The firmware may have supplicant code built-in. This is detected
    by the driver and indicated in the wiphy features flags. User-space
    can use this flag to determine whether or not to provide the
    pre-shared key material in the nl80211 CONNECT command.
    
    Reviewed-by: Gautam (Gautam Kumar) Shukla <gautams@broadcom.com>
    Reviewed-by: Hante Meuleman <meuleman@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/fwil_types.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/fwil_types.h
@@ -46,6 +46,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BRCMF_SCAN_PARAMS_COUNT_MASK	0x0000ffff
 #define BRCMF_SCAN_PARAMS_NSSID_SHIFT	16
 
+#define BRCMF_WSEC_MAX_PSK_LEN		32
+#define	BRCMF_WSEC_PASSPHRASE		BIT(0)
+
 /* primary (ie tx) key */
 #define BRCMF_PRIMARY_KEY		(1 << 1)
 #define DOT11_BSSTYPE_ANY		2
@@ -469,6 +472,19 @@ struct brcmf_wsec_key_le {
 	} rxiv;
 	__le32 pad_4[2];
 	u8 ea[ETH_ALEN];	/* per station */
+};
+
+/**
+ * struct brcmf_wsec_pmk_le - firmware pmk material.
+ *
+ * @key_len: number of octets in key material.
+ * @flags: key handling qualifiers.
+ * @key: PMK key material.
+ */
+struct brcmf_wsec_pmk_le {
+	__le16  key_len;
+	__le16  flags;
+	u8 key[2 * BRCMF_WSEC_MAX_PSK_LEN + 1];
 };
 
 /* Used to get specific STA parameters */
