d0b411eb9b1d0e7ba6c47a4e0c3954c15ce9a764
Marc Zyngier <marc.zyngier@arm.com>
2017-06-09 12:49:55 +0100

    KVM: arm64: Log an error if trapping a read-from-write-only GICv3 access
    A read-from-write-only GICv3 access should UNDEF at EL1. But since
    we're in complete paranoia-land with broken CPUs, let's assume the
    worse and gracefully handle the case.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Reviewed-by: Christoffer Dall <cdall@linaro.org>
    Signed-off-by: Christoffer Dall <cdall@linaro.org>

--- virt/kvm/arm/hyp/vgic-v3-sr.c
+++ virt/kvm/arm/hyp/vgic-v3-sr.c
@@ -981,6 +981,8 @@ int __hyp_text __vgic_v3_perform_cpuif_access(struct kvm_vcpu *vcpu)
 		break;
 	case SYS_ICC_EOIR0_EL1:
 	case SYS_ICC_EOIR1_EL1:
+		if (unlikely(is_read))
+			return 0;
 		fn = __vgic_v3_write_eoir;
 		break;
 	case SYS_ICC_GRPEN1_EL1:
@@ -1040,6 +1042,8 @@ int __hyp_text __vgic_v3_perform_cpuif_access(struct kvm_vcpu *vcpu)
 			fn = __vgic_v3_write_bpr0;
 		break;
 	case SYS_ICC_DIR_EL1:
+		if (unlikely(is_read))
+			return 0;
 		fn = __vgic_v3_write_dir;
 		break;
 	case SYS_ICC_RPR_EL1:
