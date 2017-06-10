1f6a786f9f0495a5e579c51b2a280ea48a3cbe85
John Johansen <john.johansen@canonical.com>
2017-06-09 17:29:12 -0700

    apparmor: support v7 transition format compatible with label_parse
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/apparmorfs.c
+++ security/apparmor/apparmorfs.c
@@ -2104,6 +2104,8 @@ static struct aa_sfs_entry aa_sfs_entry_domain[] = {
 
 static struct aa_sfs_entry aa_sfs_entry_versions[] = {
 	AA_SFS_FILE_BOOLEAN("v5",	1),
+	AA_SFS_FILE_BOOLEAN("v6",	1),
+	AA_SFS_FILE_BOOLEAN("v7",	1),
 	{ }
 };
 
