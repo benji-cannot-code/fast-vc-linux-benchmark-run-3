01e37e565451653d68fc0e258abc0bedd0714d9e
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2017-06-11 12:16:36 +0200

    clk: meson8b: export the ethernet gate clock
    Export the ethernet gate clock to the dt-bindings.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- drivers/clk/meson/meson8b.h
+++ drivers/clk/meson/meson8b.h
@@ -101,7 +101,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_ASSIST_MISC	33
 #define CLKID_SPI		34
 #define CLKID_I2S_SPDIF		35
-#define CLKID_ETH		36
+/* #define CLKID_ETH */
 #define CLKID_DEMUX		37
 #define CLKID_AIU_GLUE		38
 #define CLKID_IEC958		39
