9980d1dd6980eaff656c10b6a28d90a72af3cea6
Antti Palosaari <crope@iki.fi>
2017-06-12 02:49:33 -0300

    [media] af9013: convert to regmap api
    Use regmap for register access. Own low level i2c read and write
    routines for regmap is still needed because chip uses single command
    byte in addition to typical i2c register access.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/af9013_priv.h
+++ drivers/media/dvb-frontends/af9013_priv.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "af9013.h"
 #include <linux/firmware.h>
 #include <linux/math64.h>
+#include <linux/regmap.h>
 
 #define AF9013_FIRMWARE "dvb-fe-af9013.fw"
 
