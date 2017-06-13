5cc25c02df2b3dbdb18f388667e2919ab91c70bb
Dou Liyang <douly.fnst@cn.fujitsu.com>
2017-06-13 10:30:29 +0800

    x86/time: Make setup_default_timer_irq() static
    This function isn't used outside of time.c, so let's mark it static.
    
    Signed-off-by: Dou Liyang <douly.fnst@cn.fujitsu.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1497321029-29049-1-git-send-email-douly.fnst@cn.fujitsu.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/time.c
+++ arch/x86/kernel/time.c
@@ -67,7 +67,7 @@ static struct irqaction irq0  = {
 	.name = "timer"
 };
 
-void __init setup_default_timer_irq(void)
+static void __init setup_default_timer_irq(void)
 {
 	if (!nr_legacy_irqs())
 		return;
