ceacea2a6ebb2a2a3332d0525ca9a63303550178
Dong Aisheng <aisheng.dong@nxp.com>
2017-06-13 10:55:52 +0800

    tty: serial: lpuart: add imx7ulp support
    The lpuart of imx7ulp is basically the same as ls1021a. It's also
    32 bit width register, but unlike ls1021a, it's little endian.
    Besides that, imx7ulp lpuart has a minor different register layout
    from ls1021a that it has four extra registers (verid, param, global,
    pincfg) located at the beginning of register map, which are currently
    not used by the driver and less to be used later.
    
    To ease the register difference handling, we add a reg_off member
    in lpuart_soc_data structure to represent if the normal
    lpuart32_{read|write} requires plus a offset to hide the issue.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Jiri Slaby <jslaby@suse.com>
    Cc: Stefan Agner <stefan@agner.ch>
    Cc: Mingkai Hu <Mingkai.Hu@nxp.com>
    Cc: Yangbo Lu <yangbo.lu@nxp.com>
    Cc: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -232,6 +232,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEV_NAME	"ttyLP"
 #define UART_NR		6
 
+/* IMX lpuart has four extra unused regs located at the beginning */
+#define IMX_REG_OFF	0x10
+
 struct lpuart_port {
 	struct uart_port	port;
 	struct clk		*clk;
@@ -260,6 +263,7 @@ struct lpuart_port {
 
 struct lpuart_soc_data {
 	char	iotype;
+	u8	reg_off;
 };
 
 static const struct lpuart_soc_data vf_data = {
@@ -270,9 +274,15 @@ static const struct lpuart_soc_data ls_data = {
 	.iotype = UPIO_MEM32BE,
 };
 
+static struct lpuart_soc_data imx_data = {
+	.iotype = UPIO_MEM32,
+	.reg_off = IMX_REG_OFF,
+};
+
 static const struct of_device_id lpuart_dt_ids[] = {
 	{ .compatible = "fsl,vf610-lpuart",	.data = &vf_data, },
 	{ .compatible = "fsl,ls1021a-lpuart",	.data = &ls_data, },
+	{ .compatible = "fsl,imx7ulp-lpuart",	.data = &imx_data, },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, lpuart_dt_ids);
@@ -2020,6 +2030,7 @@ static int lpuart_probe(struct platform_device *pdev)
 	if (IS_ERR(sport->port.membase))
 		return PTR_ERR(sport->port.membase);
 
+	sport->port.membase += sdata->reg_off;
 	sport->port.mapbase = res->start;
 	sport->port.dev = &pdev->dev;
 	sport->port.type = PORT_LPUART;
