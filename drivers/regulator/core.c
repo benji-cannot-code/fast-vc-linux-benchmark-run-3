b6bab0a96f6753045a471fb24f210bd0e3a652fc
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2017-06-13 16:12:47 +0100

    regulator: core: Prioritise consumer mappings over regulator name
    Currently, when looking up a regulator supply, the regulator name
    takes priority over the consumer mappings. As there are a lot of
    regulator names that are in fairly common use (VDD, MICVDD, etc.) this
    can easily lead to obtaining the wrong supply, when a system contains
    two regulators that share a name.
    
    The explicit consumer mappings contain much less ambiguity as they
    specify both a name and a consumer device. As such prioritise those if
    one exists and only fall back to the regulator name if there are no
    matching explicit mappings.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1463,7 +1463,7 @@ static struct regulator_dev *regulator_lookup_by_name(const char *name)
 static struct regulator_dev *regulator_dev_lookup(struct device *dev,
 						  const char *supply)
 {
-	struct regulator_dev *r;
+	struct regulator_dev *r = NULL;
 	struct device_node *node;
 	struct regulator_map *map;
 	const char *devname = NULL;
@@ -1490,10 +1490,6 @@ static struct regulator_dev *regulator_dev_lookup(struct device *dev,
 	if (dev)
 		devname = dev_name(dev);
 
-	r = regulator_lookup_by_name(supply);
-	if (r)
-		return r;
-
 	mutex_lock(&regulator_list_mutex);
 	list_for_each_entry(map, &regulator_map_list, list) {
 		/* If the mapping has a device set up it must match */
@@ -1509,6 +1505,10 @@ static struct regulator_dev *regulator_dev_lookup(struct device *dev,
 	}
 	mutex_unlock(&regulator_list_mutex);
 
+	if (r)
+		return r;
+
+	r = regulator_lookup_by_name(supply);
 	if (r)
 		return r;
 
