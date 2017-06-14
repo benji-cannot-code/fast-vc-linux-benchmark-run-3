f85b0b012eb43d97e7b92f05268c59d4bf2dcaa7
Dmitry Osipenko <digetx@gmail.com>
2017-06-15 02:18:26 +0300

    drm/tegra: Check for malformed offsets and sizes in the 'submit' IOCTL
    If commands buffer claims a number of words that is higher than its BO can
    fit, a kernel OOPS will be fired on the out-of-bounds BO access. This was
    triggered by an opentegra Xorg driver that erroneously pushed too many
    commands to the pushbuf.
    
    The CDMA commands buffer address is 4 bytes aligned, so check its
    alignment.
    
    The maximum number of the CDMA gather fetches is 16383, add a check for
    it.
    
    Add a sanity check for the relocations in a same way.
    
    [   46.829393] Unable to handle kernel paging request at virtual address f09b2000
    ...
    [<c04a3ba4>] (host1x_job_pin) from [<c04dfcd0>] (tegra_drm_submit+0x474/0x510)
    [<c04dfcd0>] (tegra_drm_submit) from [<c04deea0>] (tegra_submit+0x50/0x6c)
    [<c04deea0>] (tegra_submit) from [<c04c07c0>] (drm_ioctl+0x1e4/0x3ec)
    [<c04c07c0>] (drm_ioctl) from [<c02541a0>] (do_vfs_ioctl+0x9c/0x8e4)
    [<c02541a0>] (do_vfs_ioctl) from [<c0254a1c>] (SyS_ioctl+0x34/0x5c)
    [<c0254a1c>] (SyS_ioctl) from [<c0107640>] (ret_fast_syscall+0x0/0x3c)
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Reviewed-by: Erik Faye-Lund <kusmabite@gmail.com>
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/gem.c
+++ drivers/gpu/drm/tegra/gem.c
@@ -21,11 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "drm.h"
 #include "gem.h"
 
-static inline struct tegra_bo *host1x_to_tegra_bo(struct host1x_bo *bo)
-{
-	return container_of(bo, struct tegra_bo, base);
-}
-
 static void tegra_bo_put(struct host1x_bo *bo)
 {
 	struct tegra_bo *obj = host1x_to_tegra_bo(bo);
