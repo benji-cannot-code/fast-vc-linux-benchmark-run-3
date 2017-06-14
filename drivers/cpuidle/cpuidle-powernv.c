7a57a350ec99cc0e3f19481b48d1f988e7640983
Nicholas Piggin <npiggin@gmail.com>
2017-06-14 23:02:39 +1000

    cpuidle: powerpc: cpuidle set polling before enabling irqs
    local_irq_enable can cause interrupts to be taken which could
    take significant amount of processing time. The idle process
    should set its polling flag before this, so another process that
    wakes it during this time will not have to send an IPI.
    
    Expand the TIF_POLLING_NRFLAG coverage to as large as possible.
    
    Reviewed-by: Gautham R. Shenoy <ego@linux.vnet.ibm.com>
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- drivers/cpuidle/cpuidle-powernv.c
+++ drivers/cpuidle/cpuidle-powernv.c
@@ -52,8 +52,9 @@ static int snooze_loop(struct cpuidle_device *dev,
 {
 	u64 snooze_exit_time;
 
-	local_irq_enable();
 	set_thread_flag(TIF_POLLING_NRFLAG);
+
+	local_irq_enable();
 
 	snooze_exit_time = get_tb() + snooze_timeout;
 	ppc64_runlatch_off();
@@ -67,6 +68,7 @@ static int snooze_loop(struct cpuidle_device *dev,
 	ppc64_runlatch_on();
 	clear_thread_flag(TIF_POLLING_NRFLAG);
 	smp_mb();
+
 	return index;
 }
 
