7a6f5e32cc12d97ecd83a83255f424cb4474fdcf
Stanimir Varbanov <stanimir.varbanov@linaro.org>
2017-06-15 13:31:51 -0300

    [media] media: venus: hfi: fix mutex unlock
    This fixed a warning when build driver with gcc7:
    
    drivers/media/platform/qcom/venus/hfi.c:171
    hfi_core_ping() warn: inconsistent returns 'mutex:&core->lock'.
      Locked on:   line 159
      Unlocked on: line 171
    
    Signed-off-by: Stanimir Varbanov <stanimir.varbanov@linaro.org>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/qcom/venus/hfi.c
+++ drivers/media/platform/qcom/venus/hfi.c
@@ -157,7 +157,7 @@ int hfi_core_ping(struct venus_core *core)
 
 	ret = core->ops->core_ping(core, 0xbeef);
 	if (ret)
-		return ret;
+		goto unlock;
 
 	ret = wait_for_completion_timeout(&core->done, TIMEOUT);
 	if (!ret) {
