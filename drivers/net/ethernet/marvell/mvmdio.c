94f302cc953b852138093b1dfa4485b1503e3499
Antoine Ténart <antoine.tenart@free-electrons.com>
2017-06-15 16:43:22 +0200

    net: mvmdio: check the MII_ADDR_C45 bit is not set for smi operations
    Add a check for the read and write smi operations, to ensure the
    MII_ADDR_C45 bit isn't set. This will be needed as soon as the xSMI
    support is added to the mvmdio driver.
    
    Signed-off-by: Antoine Tenart <antoine.tenart@free-electrons.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvmdio.c
+++ drivers/net/ethernet/marvell/mvmdio.c
@@ -127,6 +127,9 @@ static int orion_mdio_read(struct mii_bus *bus, int mii_id,
 	u32 val;
 	int ret;
 
+	if (regnum & MII_ADDR_C45)
+		return -EOPNOTSUPP;
+
 	ret = orion_mdio_wait_ready(&orion_mdio_smi_ops, bus);
 	if (ret < 0)
 		goto out;
@@ -157,6 +160,9 @@ static int orion_mdio_write(struct mii_bus *bus, int mii_id,
 {
 	struct orion_mdio_dev *dev = bus->priv;
 	int ret;
+
+	if (regnum & MII_ADDR_C45)
+		return -EOPNOTSUPP;
 
 	ret = orion_mdio_wait_ready(&orion_mdio_smi_ops, bus);
 	if (ret < 0)
