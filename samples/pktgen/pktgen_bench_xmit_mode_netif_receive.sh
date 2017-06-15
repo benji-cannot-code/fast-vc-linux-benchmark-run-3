00eba152342c2163fb1038601b2d0bfd76e129dd
Tariq Toukan <tariqt@mellanox.com>
2017-06-15 19:07:22 +0300

    pktgen: Specify the index of first thread
    Use "-f <num>", to specify the index of the first
    sender thread.
    In default first thread is #0.
    
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Cc: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- samples/pktgen/pktgen_bench_xmit_mode_netif_receive.sh
+++ samples/pktgen/pktgen_bench_xmit_mode_netif_receive.sh
@@ -49,7 +49,7 @@ DELAY="0"        # Zero means max speed
 pg_ctrl "reset"
 
 # Threads are specified with parameter -t value in $THREADS
-for ((thread = 0; thread < $THREADS; thread++)); do
+for ((thread = $F_THREAD; thread <= $L_THREAD; thread++)); do
     # The device name is extended with @name, using thread number to
     # make then unique, but any name will do.
     dev=${DEV}@${thread}
@@ -82,7 +82,7 @@ pg_ctrl "start"
 echo "Done" >&2
 
 # Print results
-for ((thread = 0; thread < $THREADS; thread++)); do
+for ((thread = $F_THREAD; thread <= $L_THREAD; thread++)); do
     dev=${DEV}@${thread}
     echo "Device: $dev"
     cat /proc/net/pktgen/$dev | grep -A2 "Result:"
