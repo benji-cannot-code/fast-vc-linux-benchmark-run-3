ae1a08450c8c4bb686aad6ce3b401725fcd1c6a0
Wolfram Sang <wsa@the-dreams.de>
2017-06-15 23:20:54 +0200

    i2c: stub: fix build warning regression
    Commit 6c42778780c40c ("i2c: stub: use pr_fmt") changed the DEBUG
    handling and caused build warnings. Revert back to the original.
    
    Fixes: 6c42778780c40c ("i2c: stub: use pr_fmt")
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/i2c-stub.c
+++ drivers/i2c/i2c-stub.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
     GNU General Public License for more details.
 */
 
-#define DEBUG
+#define DEBUG 1
 #define pr_fmt(fmt) "i2c-stub: " fmt
 
 #include <linux/errno.h>
