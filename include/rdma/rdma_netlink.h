d39597befaa3fa4f41309e9e71971de0ba6047f4
Leon Romanovsky <leonro@mellanox.com>
2017-06-15 12:46:33 +0300

    RDMA/netlink: Add and implement doit netlink callback
    The .doit callback is used by netlink core to differentiate
    between get and set operations. Common convention is to use
    that call for command operations like (SET, ADD, e.t.c.) and/or
    access without NLF_M_DUMP flag.
    
    This commit adds proper declaration and implementation
    to RDMA netlink.
    
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Reviewed-by: Steve Wise <swise@opengridcomputing.com>

--- include/rdma/rdma_netlink.h
+++ include/rdma/rdma_netlink.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <uapi/rdma/rdma_netlink.h>
 
 struct rdma_nl_cbs {
+	int (*doit)(struct sk_buff *skb, struct nlmsghdr *nlh,
+		    struct netlink_ext_ack *extack);
 	int (*dump)(struct sk_buff *skb, struct netlink_callback *nlcb);
 	u8 flags;
 };
