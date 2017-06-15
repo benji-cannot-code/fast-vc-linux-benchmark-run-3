d0c54893ccf980d85ac7203a6d4da45386c7a75c
Antoine Ténart <antoine.tenart@free-electrons.com>
2017-06-15 09:56:18 +0200

    crypto: inside-secure - fix the ring wr_cache offset
    The EIP197_HIA_xDR_CFG_WR_CACHE macro was defined to use an offset of
    23, which is wrong as it's actually 25. Fix this.
    
    Reported-by: Igal Liberman <igall@marvell.com>
    Signed-off-by: Antoine Tenart <antoine.tenart@free-electrons.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/inside-secure/safexcel.h
+++ drivers/crypto/inside-secure/safexcel.h
@@ -100,7 +100,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EIP197_HIA_xDR_WR_RES_BUF		BIT(22)
 #define EIP197_HIA_xDR_WR_CTRL_BUG		BIT(23)
 #define EIP197_HIA_xDR_WR_OWN_BUF		BIT(24)
-#define EIP197_HIA_xDR_CFG_WR_CACHE(n)		(((n) & 0x7) << 23)
+#define EIP197_HIA_xDR_CFG_WR_CACHE(n)		(((n) & 0x7) << 25)
 #define EIP197_HIA_xDR_CFG_RD_CACHE(n)		(((n) & 0x7) << 29)
 
 /* EIP197_HIA_CDR_THRESH */
