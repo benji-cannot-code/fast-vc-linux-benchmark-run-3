b4b04850dda27843c930d2fbc3714f19825104f3
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-06-15 16:30:20 +0530

    sparc/time: make of_device_ids const
    of_device_ids are not supposed to change at runtime. All functions
    working with of_device_ids provided by <linux/of.h> work with const
    of_device_ids. So mark the non-const structs as const.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/time_32.c
+++ arch/sparc/kernel/time_32.c
@@ -299,7 +299,7 @@ static int clock_probe(struct platform_device *op)
 	return 0;
 }
 
-static struct of_device_id clock_match[] = {
+static const struct of_device_id clock_match[] = {
 	{
 		.name = "eeprom",
 	},
