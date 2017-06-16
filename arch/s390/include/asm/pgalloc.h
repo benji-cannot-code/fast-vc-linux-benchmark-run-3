ea19ca39555041ee98c3ddead6770e2857926dba
Heiko Carstens <heiko.carstens@de.ibm.com>
2017-06-16 17:24:39 +0200

    s390/mm: introduce defines to reflect the hardware mmu
    Add various defines like e.g. _REGION1_SHIFT to reflect the hardware
    mmu. We have quite a bit code that does not make use of the Linux
    memory management primitives but directly modifies page, segment and
    region values.
    
    Most of this is open-coded like e.g. "1UL << 53". In order to clean
    this up introduce a couple of new defines. The existing Linux memory
    management defines are changed, so the mapping to the hardware
    implementation is reflected.
    
    Reviewed-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/pgalloc.h
+++ arch/s390/include/asm/pgalloc.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gfp.h>
 #include <linux/mm.h>
 
+#define CRST_ALLOC_ORDER 2
+
 unsigned long *crst_table_alloc(struct mm_struct *);
 void crst_table_free(struct mm_struct *, unsigned long *);
 
