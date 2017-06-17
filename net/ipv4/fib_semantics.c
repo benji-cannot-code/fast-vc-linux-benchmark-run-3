b33f1c9e196094eccd7f0835ccb64d2bb9a199d1
Wei Wang <weiwan@google.com>
2017-06-17 10:42:29 -0700

    ipv4: take dst->__refcnt when caching dst in fib
    In IPv4 routing code, fib_nh and fib_nh_exception can hold pointers
    to struct rtable but they never increment dst->__refcnt.
    This leads to the need of the dst garbage collector because when user
    is done with this dst and calls dst_release(), it can only decrement
    dst->__refcnt and can not free the dst even it sees dst->__refcnt
    drops from 1 to 0 (unless DST_NOCACHE flag is set) because the routing
    code might still hold reference to it.
    And when the routing code tries to delete a route, it has to put the
    dst to the gc_list if dst->__refcnt is not yet 0 and have a gc thread
    running periodically to check on dst->__refcnt and finally to free dst
    when refcnt becomes 0.
    
    This patch increments dst->__refcnt when
    fib_nh/fib_nh_exception holds reference to this dst and properly release
    the dst when fib_nh/fib_nh_exception has been updated with a new dst.
    
    This patch is a preparation in order to fully get rid of dst gc later.
    
    Signed-off-by: Wei Wang <weiwan@google.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/fib_semantics.c
+++ net/ipv4/fib_semantics.c
@@ -153,6 +153,7 @@ static void rt_fibinfo_free(struct rtable __rcu **rtp)
 	 * free_fib_info_rcu()
 	 */
 
+	dst_release(&rt->dst);
 	dst_free(&rt->dst);
 }
 
@@ -195,8 +196,10 @@ static void rt_fibinfo_free_cpus(struct rtable __rcu * __percpu *rtp)
 		struct rtable *rt;
 
 		rt = rcu_dereference_protected(*per_cpu_ptr(rtp, cpu), 1);
-		if (rt)
+		if (rt) {
+			dst_release(&rt->dst);
 			dst_free(&rt->dst);
+		}
 	}
 	free_percpu(rtp);
 }
