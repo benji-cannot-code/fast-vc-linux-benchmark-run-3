fc7c33aa114b567b51f5c1d01e9704b4c2f9bcb6
Leon Romanovsky <leonro@mellanox.com>
2017-06-18 16:37:27 +0300

    RDMA/netlink: Update copyright
    Add Mellanox to the copyright header.
    
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Reviewed-by: Steve Wise <swise@opengridcomputing.com>

--- drivers/infiniband/core/netlink.c
+++ drivers/infiniband/core/netlink.c
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
+ * Copyright (c) 2017 Mellanox Technologies Inc.  All rights reserved.
  * Copyright (c) 2010 Voltaire Inc.  All rights reserved.
  *
  * This software is available to you under a choice of one of two
