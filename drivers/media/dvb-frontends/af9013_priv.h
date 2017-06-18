6de60c93fe8b2644204b8d69e0a9576d260f8a89
Antti Palosaari <crope@iki.fi>
2017-06-18 06:23:15 -0400

    media: af9013: dvbv5 cnr
    Implement dvbv5 cnr.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/af9013_priv.h
+++ drivers/media/dvb-frontends/af9013_priv.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AF9013_PRIV_H
 
 #include <media/dvb_frontend.h>
+#include <media/dvb_math.h>
 #include "af9013.h"
 #include <linux/firmware.h>
 #include <linux/math64.h>
