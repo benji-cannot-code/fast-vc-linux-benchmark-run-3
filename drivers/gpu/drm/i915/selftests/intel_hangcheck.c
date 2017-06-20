125bc319a21d3962d497394034830140c4b06856
Chris Wilson <chris@chris-wilson.co.uk>
2017-06-20 10:57:43 +0100

    drm/i915: Wait for concurrent global resets to complete
    If we enter i915_handle_error() a second time and a global reset is
    already in progress, we can simply wait for completion of the first
    reset. Currently we exit early prior to the actual reset being
    performed -- the worst of both worlds!
    
    v2: Plug into the existing reset_queue, and remember that kselftests is
    playing games with I915_RESET_BACKOFF to prevent hangcheck from screwing
    up.
    v3: Rename to i915_reset_device to fit in better with i915_reset_engine
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Cc: Michel Thierry <michel.thierry@intel.com>
    Reviewed-by: Michel Thierry <michel.thierry@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170620095751.13127-2-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/selftests/intel_hangcheck.c
+++ drivers/gpu/drm/i915/selftests/intel_hangcheck.c
@@ -317,6 +317,8 @@ static int igt_global_reset(void *arg)
 
 	GEM_BUG_ON(test_bit(I915_RESET_HANDOFF, &i915->gpu_error.flags));
 	clear_bit(I915_RESET_BACKOFF, &i915->gpu_error.flags);
+	wake_up_all(&i915->gpu_error.reset_queue);
+
 	if (i915_terminally_wedged(&i915->gpu_error))
 		err = -EIO;
 
@@ -405,6 +407,7 @@ fini:
 unlock:
 	mutex_unlock(&i915->drm.struct_mutex);
 	clear_bit(I915_RESET_BACKOFF, &i915->gpu_error.flags);
+	wake_up_all(&i915->gpu_error.reset_queue);
 
 	if (i915_terminally_wedged(&i915->gpu_error))
 		return -EIO;
@@ -520,6 +523,7 @@ fini:
 unlock:
 	mutex_unlock(&i915->drm.struct_mutex);
 	clear_bit(I915_RESET_BACKOFF, &i915->gpu_error.flags);
+	wake_up_all(&i915->gpu_error.reset_queue);
 
 	if (i915_terminally_wedged(&i915->gpu_error))
 		return -EIO;
