25755aa3d6492e5bd3caf1e1e759df16bec3d0c9
Hoegeun Kwon <hoegeun.kwon@samsung.com>
2017-06-21 19:51:43 +0900

    drm/exynos/dsi: Remove error handling for bridge_node DT parsing
    Remove the error handling of bridge_node because the bridge_node is
    optional.
    
    For example, In case of Exynos SoC, a bridge device such as mDNIe and
    MIC could be placed between Display Controller and MIPI DSI device but
    the bridge device is optional.
    
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@samsung.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_dsi.c
+++ drivers/gpu/drm/exynos/exynos_drm_dsi.c
@@ -1652,8 +1652,6 @@ static int exynos_dsi_parse_dt(struct exynos_dsi *dsi)
 		return ret;
 
 	dsi->bridge_node = of_graph_get_remote_node(node, DSI_PORT_IN, 0);
-	if (!dsi->bridge_node)
-		return -EINVAL;
 
 	return 0;
 }
