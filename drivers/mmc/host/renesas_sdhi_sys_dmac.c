7cae883114e76899c8f57e1fc48b44983ecea47c
Simon Horman <horms+renesas@verge.net.au>
2017-06-21 16:00:29 +0200

    mmc: renesas-sdhi: add support for R-Car Gen3 SDHI DMAC
    Add a new variant of the SDHI driver to support R-Car Gen3 with DMA via
    on-chip bus mastering.  Since the DMAC is in a part of the SDHI module it
    is not suitable to be used via DMA Engine.
    
    Clearing of DM_CM_INFO1 after DMA thanks to Dirk Behme
    
    Cc: Dirk Behme <dirk.behme@de.bosch.com>
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Ai Kyuse <ai.kyuse.uw@renesas.com>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/renesas_sdhi_sys_dmac.c
+++ drivers/mmc/host/renesas_sdhi_sys_dmac.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * DMA function for TMIO MMC implementations
+ * DMA support use of SYS DMAC with SDHI SD/SDIO controller
  *
  * Copyright (C) 2016-17 Renesas Electronics Corporation
  * Copyright (C) 2016-17 Sang Engineering, Wolfram Sang
