aabcbee5590a7ddcecb412156a4a9b38bdba90b5
Bart Van Assche <bart.vanassche@wdc.com>
2017-06-21 10:55:45 -0700

    block: Declare local symbols static
    Avoid that building with W=1 causes the compiler to complain that
    a declaration for bounce_bio_set and bounce_bio_split is missing.
    
    References: commit a8821f3f32be ("block: Improvements to bounce-buffer handling")
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Cc: Neil Brown <neilb@suse.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Hannes Reinecke <hare@suse.com>
    Cc: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/bounce.c
+++ block/bounce.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define POOL_SIZE	64
 #define ISA_POOL_SIZE	16
 
-struct bio_set *bounce_bio_set, *bounce_bio_split;
+static struct bio_set *bounce_bio_set, *bounce_bio_split;
 static mempool_t *page_pool, *isa_page_pool;
 
 #if defined(CONFIG_HIGHMEM) || defined(CONFIG_NEED_BOUNCE_POOL)
