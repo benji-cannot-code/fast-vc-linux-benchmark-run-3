6aa8d33ffd4fcd69870b605b62543b1516373704
Antti Palosaari <crope@iki.fi>
2017-06-22 12:18:21 -0400

    media: af9013: add i2c mux adapter for tuner bus
    Add muxed i2c adapter for demod tuner i2c bus gate control.
    
    Signed-off-by: Antti Palosaari <crope@iki.fi>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/af9013_priv.h
+++ drivers/media/dvb-frontends/af9013_priv.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/dvb_math.h>
 #include "af9013.h"
 #include <linux/firmware.h>
+#include <linux/i2c-mux.h>
 #include <linux/math64.h>
 #include <linux/regmap.h>
 
