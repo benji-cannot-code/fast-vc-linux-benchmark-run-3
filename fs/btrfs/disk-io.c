0e5e86d6af54e864152b224f8233754ecac524fa
David Sterba <dsterba@suse.com>
2017-06-22 03:59:40 +0200

    btrfs: drop chunk locks at the end of close_ctree
    The pinned chunks might be left over so we clean them but at this point
    of close_ctree, there's noone to race with, the locking can be removed.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -3945,7 +3945,6 @@ void close_ctree(struct btrfs_fs_info *fs_info)
 	__btrfs_free_block_rsv(root->orphan_block_rsv);
 	root->orphan_block_rsv = NULL;
 
-	mutex_lock(&fs_info->chunk_mutex);
 	while (!list_empty(&fs_info->pinned_chunks)) {
 		struct extent_map *em;
 
@@ -3954,7 +3953,6 @@ void close_ctree(struct btrfs_fs_info *fs_info)
 		list_del_init(&em->list);
 		free_extent_map(em);
 	}
-	mutex_unlock(&fs_info->chunk_mutex);
 }
 
 int btrfs_buffer_uptodate(struct extent_buffer *buf, u64 parent_transid,
