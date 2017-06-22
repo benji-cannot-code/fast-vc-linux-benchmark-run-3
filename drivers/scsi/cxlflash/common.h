17c4f4c4b5849671c50a3f3fb5aeeac1dc13170e
Uma Krishnan <ukrishn@linux.vnet.ibm.com>
2017-06-21 21:14:17 -0500

    scsi: cxlflash: Schedule asynchronous reset of the host
    A context reset failure indicates the AFU is in a bad state. At present,
    when such a situation occurs, no further action is taken. This leaves the
    adapter in an unusable state with no recoverable actions.
    
    To avoid this situation, context reset failures will be escalated to a host
    reset operation. This will be done asynchronously to allow the acting
    thread to return to the user with a failure.
    
    Signed-off-by: Uma Krishnan <ukrishn@linux.vnet.ibm.com>
    Acked-by: Matthew R. Ochs <mrochs@linux.vnet.ibm.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/cxlflash/common.h
+++ drivers/scsi/cxlflash/common.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _CXLFLASH_COMMON_H
 #define _CXLFLASH_COMMON_H
 
+#include <linux/async.h>
 #include <linux/irq_poll.h>
 #include <linux/list.h>
 #include <linux/rwsem.h>
@@ -145,6 +146,7 @@ struct cxlflash_cfg {
 	bool tmf_active;
 	wait_queue_head_t reset_waitq;
 	enum cxlflash_state state;
+	async_cookie_t async_reset_cookie;
 };
 
 struct afu_cmd {
