086321c72992f02ef50bee6e8a922c6ea8d7e0f4
Simon Horman <simon.horman@netronome.com>
2017-06-23 22:12:08 +0200

    nfp: add flower app
    Add app for flower offload. At this point the PF netdev and phys port
    representor netdevs are initialised. Follow-up work will add support for
    VF and PF representors and beyond that offloading the flower classifier.
    
    Based in part on work by Benjamin LaHaise and Bert van Leeuwen.
    
    Signed-off-by: Simon Horman <simon.horman@netronome.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/netronome/nfp/nfp_app.c
+++ drivers/net/ethernet/netronome/nfp/nfp_app.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const struct nfp_app_type *apps[] = {
 	&app_nic,
 	&app_bpf,
+	&app_flower,
 };
 
 const char *nfp_app_mip_name(struct nfp_app *app)
