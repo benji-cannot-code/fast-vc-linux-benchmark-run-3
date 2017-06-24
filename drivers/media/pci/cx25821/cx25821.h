c3a016410ae6ee1bdbfa190b122b38062ca3719a
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2017-06-24 16:40:24 -0400

    media: cx25821: get rid of CX25821_VERSION_CODE
    This is used just for printing a version number. As this is
    never incremented, it makes no sense to keep it :-)
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/cx25821/cx25821.h
+++ drivers/media/pci/cx25821/cx25821.h
@@ -42,8 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/version.h>
 #include <linux/mutex.h>
 
-#define CX25821_VERSION_CODE KERNEL_VERSION(0, 0, 106)
-
 #define UNSET (-1U)
 #define NO_SYNC_LINE (-1U)
 
