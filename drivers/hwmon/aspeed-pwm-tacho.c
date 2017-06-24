a417871e4433dc6d6690247ef0e062ac46c6e30f
Patrick Venture <venture@google.com>
2017-06-24 08:39:32 -0700

    hwmon: (aspeed-pwm-tacho) reduce fan_tach period
    Reduce the fan_tach period such that the fan controller uses a shorter
    period to measure the rpm.
    
    The original period of 0x1000 was chosen as a conversative value from the
    reference implementation.  Through experimentation on the quanta-q71l
    board, I was able to drive the number down which ultimately reduced the
    time the controller would use to determine the fan_tach.  This value was
    recently tested and accepted downstream on the IBM Zaius board which uses
    the ast2500.
    
    Future work: It may be worthwhile as this is a tunable parameter to the
    system, to allow overriding it through the device tree.
    
    Testing: Tested on an ast2400 sitting on a quanta-q71l and ast2500 on
    power9.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/aspeed-pwm-tacho.c
+++ drivers/hwmon/aspeed-pwm-tacho.c
@@ -161,7 +161,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 11: reserved.
  */
 #define M_TACH_MODE 0x02 /* 10b */
-#define M_TACH_UNIT 0x1000
+#define M_TACH_UNIT 0x00c0
 #define INIT_FAN_CTRL 0xFF
 
 struct aspeed_pwm_tacho_data {
