eefecdb2f0800e92694d2b7c4fcea91847f0cf15
Gilad Ben-Yossef <gilad@benyossef.com>
2017-06-25 10:47:19 +0300

    staging: ccree: fix hash import/export
    Hash import and export was saving and restoring the wrong context
    and therefore disabled. Fix it by restoring intermediate digest
    and additional state needed.
    
    The hash and mac transform now pass testmgr partial hash tests.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/ssi_hash.h
+++ drivers/staging/ccree/ssi_hash.h
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define XCBC_MAC_K2_OFFSET 16
 #define XCBC_MAC_K3_OFFSET 32
 
+#define CC_EXPORT_MAGIC 0xC2EE1070U
+
 // this struct was taken from drivers/crypto/nx/nx-aes-xcbc.c and it is used for xcbc/cmac statesize
 struct aeshash_state {
 	u8 state[AES_BLOCK_SIZE];
