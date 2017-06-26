c84802746ba16644731517d9e9c47b78195ad98b
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-06-26 13:51:14 +0200

    Revert "staging: fsl-mc: drop useless #includes"
    This reverts commit bb4a64b79f3b9973316e775f6c2910a98b6a562a.
    
    The whole series is broken, so back it all out.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Cc: Laurentiu Tudor <laurentiu.tudor@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/fsl-mc-allocator.c
+++ drivers/staging/fsl-mc/bus/fsl-mc-allocator.c
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../include/mc-bus.h"
 #include "../include/mc-sys.h"
 
+#include "dpbp-cmd.h"
+#include "dpcon-cmd.h"
 #include "fsl-mc-private.h"
 
 static bool __must_check fsl_mc_is_allocatable(const char *obj_type)
