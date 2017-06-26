b1c4d25ab9f452692e08b4dde3c3e1b211fd6385
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-06-26 13:50:41 +0200

    Revert "staging: fsl-mc: move mc-sys.h contents in the public header"
    This reverts commit 7d6e221d73904aedcbd46ce2db6a545be55d2296.
    
    The whole series is broken, so back it all out.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Cc: Laurentiu Tudor <laurentiu.tudor@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
+++ drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iommu.h>
 
 #include "../../fsl-mc/include/mc.h"
+#include "../../fsl-mc/include/mc-sys.h"
 #include "dpaa2-eth.h"
 
 /* CREATE_TRACE_POINTS only needs to be defined once. Other dpa files
--- drivers/staging/fsl-mc/bus/dpio/dpio.c
+++ drivers/staging/fsl-mc/bus/dpio/dpio.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <linux/kernel.h>
-#include "../../include/mc.h"
+#include "../../include/mc-sys.h"
 #include "../../include/mc-cmd.h"
 
 #include "dpio.h"
