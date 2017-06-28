be014922252a9127168001915952eb102842e862
Joerg Roedel <jroedel@suse.de>
2017-06-28 12:52:48 +0200

    iommu: Warn once when device_group callback returns NULL
    This callback should never return NULL. Print a warning if
    that happens so that we notice and can fix it.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1011,6 +1011,9 @@ struct iommu_group *iommu_group_get_for_dev(struct device *dev)
 	if (ops && ops->device_group)
 		group = ops->device_group(dev);
 
+	if (WARN_ON_ONCE(group == NULL))
+		return ERR_PTR(-EINVAL);
+
 	if (IS_ERR(group))
 		return group;
 
