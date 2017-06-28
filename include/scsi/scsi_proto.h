33bf51add0b2757a742bc1264f2946483766e4d2
Damien Le Moal <damien.lemoal@wdc.com>
2017-06-28 14:58:56 +0900

    target: Use macro for WRITE_VERIFY_32 operation codes
    Add WRITE_VERIFY_32 definition to scsi prototypes and use this macro
    definition isntead of the hard coded value.
    
    (Drop WRITE_VERIFY_16 that's already part of another patch - nab)
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- include/scsi/scsi_proto.h
+++ include/scsi/scsi_proto.h
@@ -162,6 +162,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define READ_32		      0x09
 #define VERIFY_32	      0x0a
 #define WRITE_32	      0x0b
+#define WRITE_VERIFY_32	      0x0c
 #define WRITE_SAME_32	      0x0d
 
 /* Values for T10/04-262r7 */
