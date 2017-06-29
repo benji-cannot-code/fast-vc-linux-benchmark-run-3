39a4ac2a89dd125507b175679437e8916bff744a
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-06-29 16:31:26 +0530

    net: constify attribute_group structures.
    attribute_groups are not supposed to change at runtime. All functions
    working with attribute_groups provided by <linux/device.h> work with const
    attribute_group. So mark the non-const structs as const.
    
    File size before:
       text	   data	    bss	    dec	    hex	filename
       9968	   3168	     16	  13152	   3360	net/core/net-sysfs.o
    
    File size After adding 'const':
       text	   data	    bss	    dec	    hex	filename
      10160	   2976	     16	  13152	   3360	net/core/net-sysfs.o
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -627,7 +627,7 @@ static struct attribute *netstat_attrs[] = {
 };
 
 
-static struct attribute_group netstat_group = {
+static const struct attribute_group netstat_group = {
 	.name  = "statistics",
 	.attrs  = netstat_attrs,
 };
@@ -637,7 +637,7 @@ static struct attribute *wireless_attrs[] = {
 	NULL
 };
 
-static struct attribute_group wireless_group = {
+static const struct attribute_group wireless_group = {
 	.name = "wireless",
 	.attrs = wireless_attrs,
 };
@@ -1205,7 +1205,7 @@ static struct attribute *dql_attrs[] = {
 	NULL
 };
 
-static struct attribute_group dql_group = {
+static const struct attribute_group dql_group = {
 	.name  = "byte_queue_limits",
 	.attrs  = dql_attrs,
 };
