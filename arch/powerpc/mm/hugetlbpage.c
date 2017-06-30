782f24c73cc24ba21dc56b364db326acd0d17b34
Oliver O'Halloran <oohall@gmail.com>
2017-06-30 16:52:35 +1000

    powerpc/hugetlbfs: Export HPAGE_SHIFT
    Export it so it can be referenced inside a module.
    
    Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/hugetlbpage.c
+++ arch/powerpc/mm/hugetlbpage.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PAGE_SHIFT_16G	34
 
 unsigned int HPAGE_SHIFT;
+EXPORT_SYMBOL(HPAGE_SHIFT);
 
 /*
  * Tracks gpages after the device tree is scanned and before the
