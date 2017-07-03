faf2ff89ddcb0022037822ed6ca234e21c1001f6
Vinod Koul <vinod.koul@intel.com>
2017-07-03 13:54:46 +0530

    ASoC: Intel: Skylake: explicitly add the headers sst-dsp.h
    Commit bdd0384a5ada ("ASoC: Intel: Skylake: Add support to read firmware
    registers") introduced firmware register read so added sst-dsp-priv.h but
    missed adding sst-dsp.h as that leads to below compiler warning:
    
       In file included from sound/soc/intel/skylake/skl-debug.c:23:0:
    >> sound/soc/intel/skylake/../common/sst-dsp-priv.h:63:42: warning:
    'struct sst_pdata' declared inside parameter list [enabled by default]
         int (*init)(struct sst_dsp *sst, struct sst_pdata *pdata);
                                                 ^
    >> sound/soc/intel/skylake/../common/sst-dsp-priv.h:63:42: warning:
    its scope is only this definition or declaration, which is probably
    not what you want [enabled by default]
    
    So add the missing header.
    
    Fixes: bdd0384a5ada ("ASoC: Intel: Skylake: Add support to read firmware registers")
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Guneshwor Singh <guneshwor.o.singh@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/skl-debug.c
+++ sound/soc/intel/skylake/skl-debug.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "skl-sst-ipc.h"
 #include "skl-tplg-interface.h"
 #include "skl-topology.h"
+#include "../common/sst-dsp.h"
 #include "../common/sst-dsp-priv.h"
 
 #define MOD_BUF		PAGE_SIZE
