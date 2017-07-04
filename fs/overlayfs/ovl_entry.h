368b2127d5d60418f939a7f2e5169edc8bab24da
Miklos Szeredi <mszeredi@redhat.com>
2017-07-04 22:03:16 +0200

    ovl: move cache and version to ovl_inode
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/ovl_entry.h
+++ fs/overlayfs/ovl_entry.h
@@ -36,12 +36,8 @@ struct ovl_fs {
 
 /* private information held for every overlayfs dentry */
 struct ovl_entry {
-	struct ovl_dir_cache *cache;
 	union {
-		struct {
-			u64 version;
-			bool opaque;
-		};
+		bool opaque;
 		struct rcu_head rcu;
 	};
 	unsigned numlower;
@@ -51,7 +47,9 @@ struct ovl_entry {
 struct ovl_entry *ovl_alloc_entry(unsigned int numlower);
 
 struct ovl_inode {
+	struct ovl_dir_cache *cache;
 	const char *redirect;
+	u64 version;
 	unsigned long flags;
 	struct inode vfs_inode;
 	struct dentry *__upperdentry;
