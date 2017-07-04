9be6f1e2fc9c095dfba547b594e337e16cad6c6f
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-07-04 18:34:39 +0300

    platform/x86: ideapad-laptop: Fix indentation in DMI table
    There are couple of places where 8 spaces are used instead of tabs.
    Replace former by latter. No functional change intended.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/ideapad-laptop.c
+++ drivers/platform/x86/ideapad-laptop.c
@@ -933,8 +933,8 @@ static const struct dmi_system_id no_hw_rfkill_list[] = {
 	{
 		.ident = "Lenovo V310-15ISK",
 		.matches = {
-		        DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
-		        DMI_MATCH(DMI_PRODUCT_VERSION, "Lenovo V310-15ISK"),
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_VERSION, "Lenovo V310-15ISK"),
 		},
 	},
 	{
@@ -982,8 +982,8 @@ static const struct dmi_system_id no_hw_rfkill_list[] = {
 	{
 		.ident = "Lenovo ideapad 310-15IKB",
 		.matches = {
-		        DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
-		        DMI_MATCH(DMI_PRODUCT_VERSION, "Lenovo ideapad 310-15IKB"),
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_PRODUCT_VERSION, "Lenovo ideapad 310-15IKB"),
 		},
 	},
 	{
