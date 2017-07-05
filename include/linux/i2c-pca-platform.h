f1df64eb3805fa9e78794108f0317abcf72a9462
Chris Packham <chris.packham@alliedtelesis.co.nz>
2017-07-05 22:13:58 +1200

    i2c: pca-platform: drop gpio from platform data
    Now that the i2c-pca-plaform driver is using the device managed API for
    gpios there is no need for the reset gpio to be specified via
    i2c_pca9564_pf_platform_data.
    
    Signed-off-by: Chris Packham <chris.packham@alliedtelesis.co.nz>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- include/linux/i2c-pca-platform.h
+++ include/linux/i2c-pca-platform.h
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I2C_PCA9564_PLATFORM_H
 
 struct i2c_pca9564_pf_platform_data {
-	int gpio;		/* pin to reset chip. driver will work when
-				 * not supplied (negative value), but it
-				 * cannot exit some error conditions then */
 	int i2c_clock_speed;	/* values are defined in linux/i2c-algo-pca.h */
 	int timeout;		/* timeout in jiffies */
 };
