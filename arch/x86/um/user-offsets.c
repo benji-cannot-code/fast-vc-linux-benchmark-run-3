0c40935e554e9c006c29ced1c99b436cb22051cd
Richard Weinberger <richard@nod.at>
2017-07-06 09:35:27 +0200

    um: Correctly check for PTRACE_GETRESET/SETREGSET
    When checking for PTRACE_GETRESET/SETREGSET, make sure that
    the correct header file is included. We need linux/ptrace.h
    which contains all ptrace UAPI related defines.
    Otherwise #if defined(PTRACE_GETRESET) is always false.
    
    Cc: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/x86/um/user-offsets.c
+++ arch/x86/um/user-offsets.c
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/mman.h>
 #include <sys/user.h>
 #define __FRAME_OFFSETS
-#include <asm/ptrace.h>
+#include <linux/ptrace.h>
 #include <asm/types.h>
 
 #ifdef __i386__
