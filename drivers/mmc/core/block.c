db0ca0cee25b49ff0de0242025ba610e93c1b54a
Shawn Lin <shawn.lin@rock-chips.com>
2017-07-06 16:26:55 +0800

    mmc: block: remove unused struct mmc_card *card
    It was never used and introduced a long standing compile
    warning:
    
    drivers/mmc/core/block.c: In function 'power_ro_lock_store':
    drivers/mmc/core/block.c:191:19: warning: variable 'card' set but not
    used [-Wunused-but-set-variable]
    
    Remove it to fix the warning.
    
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/block.c
+++ drivers/mmc/core/block.c
@@ -189,7 +189,6 @@ static ssize_t power_ro_lock_store(struct device *dev,
 {
 	int ret;
 	struct mmc_blk_data *md, *part_md;
-	struct mmc_card *card;
 	struct mmc_queue *mq;
 	struct request *req;
 	unsigned long set;
@@ -202,7 +201,6 @@ static ssize_t power_ro_lock_store(struct device *dev,
 
 	md = mmc_blk_get(dev_to_disk(dev));
 	mq = &md->queue;
-	card = md->queue.card;
 
 	/* Dispatch locking to the block layer */
 	req = blk_get_request(mq->queue, REQ_OP_DRV_OUT, __GFP_RECLAIM);
