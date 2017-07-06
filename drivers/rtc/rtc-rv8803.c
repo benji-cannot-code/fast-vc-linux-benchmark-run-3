a4d67105166760bcc707d2988ff2a5483b6c4170
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2017-07-06 11:42:05 +0200

    rtc: rv8803: remove rv8803_remove
    rv8803_remove() is now empty, remove it.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- drivers/rtc/rtc-rv8803.c
+++ drivers/rtc/rtc-rv8803.c
@@ -613,11 +613,6 @@ static int rv8803_probe(struct i2c_client *client,
 	return 0;
 }
 
-static int rv8803_remove(struct i2c_client *client)
-{
-	return 0;
-}
-
 static const struct i2c_device_id rv8803_id[] = {
 	{ "rv8803", rv_8803 },
 	{ "rx8900", rx_8900 },
@@ -644,7 +639,6 @@ static struct i2c_driver rv8803_driver = {
 		.of_match_table = of_match_ptr(rv8803_of_match),
 	},
 	.probe		= rv8803_probe,
-	.remove		= rv8803_remove,
 	.id_table	= rv8803_id,
 };
 module_i2c_driver(rv8803_driver);
