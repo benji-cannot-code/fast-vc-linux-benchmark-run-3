776de530fc4f9b18aaee1533c664e68ed37ce00a
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-07-06 12:07:08 -0400

    media: staging: atomisp: ov2680: constify acpi_device_id
    acpi_device_id are not supposed to change at runtime. All functions
    working with acpi_device_id provided by <acpi/acpi_bus.h> work with
    const acpi_device_id. So mark the non-const structs as const.
    
    File size before:
       text	   data	    bss	    dec	    hex	filename
      12466	   3120	      8	  15594	   3cea drivers/staging/media/atomisp/i2c/ov2680.o
    
    File size After adding 'const':
       text	   data	    bss	    dec	    hex	filename
      12530	   3056	      8	  15594	   3cea drivers/staging/media/atomisp/i2c/ov2680.o
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Acked-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/staging/media/atomisp/i2c/ov2680.c
+++ drivers/staging/media/atomisp/i2c/ov2680.c
@@ -1518,7 +1518,7 @@ out_free:
 	return ret;
 }
 
-static struct acpi_device_id ov2680_acpi_match[] = {
+static const struct acpi_device_id ov2680_acpi_match[] = {
 	{"XXOV2680"},
 	{"OVTI2680"},
 	{},
