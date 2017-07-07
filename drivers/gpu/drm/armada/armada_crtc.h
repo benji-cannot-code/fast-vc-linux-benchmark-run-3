2e2bd2026b51b62e82219f2a1566b8f8e1ace6c5
Russell King <rmk+kernel@armlinux.org.uk>
2017-07-07 15:55:53 +0100

    drm/armada: store plane in armada_plane_work
    Store the plane in the armada_plane_work structure rather than passing
    it around; it doesn't get used very much in the work structures, so
    passing it around is a needless expense.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- drivers/gpu/drm/armada/armada_crtc.h
+++ drivers/gpu/drm/armada/armada_crtc.h
@@ -37,9 +37,8 @@ struct armada_plane;
 struct armada_variant;
 
 struct armada_plane_work {
-	void			(*fn)(struct armada_crtc *,
-				      struct armada_plane *,
-				      struct armada_plane_work *);
+	void (*fn)(struct armada_crtc *, struct armada_plane_work *);
+	struct drm_plane *plane;
 };
 
 struct armada_plane_state {
@@ -61,7 +60,7 @@ struct armada_plane {
 
 int armada_drm_plane_init(struct armada_plane *plane);
 int armada_drm_plane_work_queue(struct armada_crtc *dcrtc,
-	struct armada_plane *plane, struct armada_plane_work *work);
+	struct armada_plane_work *work);
 int armada_drm_plane_work_wait(struct armada_plane *plane, long timeout);
 void armada_drm_plane_work_cancel(struct armada_crtc *dcrtc,
 	struct armada_plane *plane);
