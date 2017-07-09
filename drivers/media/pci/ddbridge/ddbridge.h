7602b71b654a3f4ea40d0d269d7520c792615e1c
Daniel Scheller <d.scheller@gmx.net>
2017-07-09 15:42:44 -0400

    media: ddbridge: support MaxLinear MXL5xx based cards (MaxS4/8)
    This enables MaxS4/S8 and Octopus Max card support in ddbridge by adding
    glue code into ddbridge-core, having another PCI ID, and have the LNB IC
    control code (and all other MaxS4/8 related code) in ddbridge-maxs8.c
    (rather than another ~400 LoC in ddbridge-core.c like it's done in the
    original vendor driver package).
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/ddbridge/ddbridge.h
+++ drivers/media/pci/ddbridge/ddbridge.h
@@ -116,6 +116,7 @@ struct ddb_info {
 #define DDB_NONE         0
 #define DDB_OCTOPUS      1
 #define DDB_OCTOPUS_CI   2
+#define DDB_OCTOPUS_MAX  5
 #define DDB_OCTOPUS_MAX_CT  6
 	char *name;
 	u32   i2c_mask;
@@ -296,6 +297,15 @@ struct ddb_port {
 
 #define TS_CAPTURE_LEN  (4096)
 
+struct ddb_lnb {
+	struct mutex           lock;
+	u32                    tone;
+	enum fe_sec_voltage    oldvoltage[4];
+	u32                    voltage[4];
+	u32                    voltages;
+	u32                    fmode;
+};
+
 struct ddb_link {
 	struct ddb            *dev;
 	struct ddb_info       *info;
@@ -303,6 +313,7 @@ struct ddb_link {
 	u32                    regs;
 	spinlock_t             lock;
 	struct mutex           flash_mutex;
+	struct ddb_lnb         lnb;
 	struct tasklet_struct  tasklet;
 	struct ddb_ids         ids;
 
