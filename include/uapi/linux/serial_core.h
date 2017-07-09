bdbc0933bec2c1d2aa472e50e53efa199144dba7
Andreas Färber <afaerber@suse.de>
2017-07-09 22:29:42 +0200

    tty: serial: owl: Implement console driver
    Implement serial console driver to complement earlycon.
    
    Based on LeMaker linux-actions tree.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PORT_CLPS711X	33
 #define PORT_SA1100	34
 #define PORT_UART00	35
+#define PORT_OWL	36
 #define PORT_21285	37
 
 /* Sparc type numbers.  */
