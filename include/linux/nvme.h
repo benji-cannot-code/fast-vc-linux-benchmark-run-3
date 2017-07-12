6916bc0917fabd2368f792fec886f19025c56c02
Arnav Dawn <a.dawn@samsung.com>
2017-07-12 16:11:53 +0530

    nvme: define NVME_NSID_ALL
    Define the constant "0xffffffff" (used as nsid for all namespaces)
    as NVME_NSID_ALL.
    
    Signed-off-by: Arnav Dawn <a.dawn@samsung.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>

--- include/linux/nvme.h
+++ include/linux/nvme.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NVME_RDMA_IP_PORT	4420
 
+#define NVME_NSID_ALL		0xffffffff
+
 enum nvme_subsys_type {
 	NVME_NQN_DISC	= 1,		/* Discovery type target subsystem */
 	NVME_NQN_NVME	= 2,		/* NVME type target subsystem */
