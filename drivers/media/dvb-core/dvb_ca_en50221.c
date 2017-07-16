aa00742bc891638eac30f32a97f178dd9aafbe91
Jasmin Jessich <jasmin@anw.at>
2017-07-15 20:43:13 -0400

    media: dvb_ca_en50221: Fixed typo
    - "dont" -> "don't"
    
    Signed-off-by: Jasmin Jessich <jasmin@anw.at>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-core/dvb_ca_en50221.c
+++ drivers/media/dvb-core/dvb_ca_en50221.c
@@ -1305,7 +1305,7 @@ static void dvb_ca_en50221_thread_state_machine(struct dvb_ca_private *ca,
 			 */
 			if (dvb_ca_en50221_check_camstatus(ca, slot)) {
 				/*
-				 * we dont want to sleep on the next iteration
+				 * we don't want to sleep on the next iteration
 				 * so we can handle the cam change
 				 */
 				ca->wakeup = 1;
@@ -1315,7 +1315,7 @@ static void dvb_ca_en50221_thread_state_machine(struct dvb_ca_private *ca,
 			/* check if we've hit our limit this time */
 			if (++pktcount >= MAX_RX_PACKETS_PER_ITERATION) {
 				/*
-				 * dont sleep; there is likely to be more data
+				 * don't sleep; there is likely to be more data
 				 * to read
 				 */
 				ca->wakeup = 1;
