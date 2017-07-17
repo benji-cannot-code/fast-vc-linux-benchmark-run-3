bdfea3af829c6b9c6ba0ab5b9660380cdaed780d
Alex Curtin <alexander.curtin@unisys.com>
2017-07-17 16:16:53 -0400

    staging: unisys: include: iochannel.h: removed VISOR_VNIC_CHANNEL_SIGNATURE
    VISOR_VNIC_CHANNEL_SIGNATURE is a redundant definition that points to
    VISOR_CHANNEL_SIGNATURE. This replaces that definition with
    VISOR_CHANNEL_SIGNATURE.
    
    Signed-off-by: Alex Curtin <alexander.curtin@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: David Binder <david.binder@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/iochannel.h
+++ drivers/staging/unisys/include/iochannel.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-direction.h>
 #include "channel.h"
 
-#define VISOR_VNIC_CHANNEL_SIGNATURE VISOR_CHANNEL_SIGNATURE
 #define VISOR_VSWITCH_CHANNEL_SIGNATURE VISOR_CHANNEL_SIGNATURE
 
 /*
@@ -56,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(visor_check_channel(ch, visor_vnic_channel_uuid, \
 			     "vnic", MIN_IO_CHANNEL_SIZE, \
 			     VISOR_VNIC_CHANNEL_VERSIONID, \
-			     VISOR_VNIC_CHANNEL_SIGNATURE))
+			     VISOR_CHANNEL_SIGNATURE))
 
 /*
  * Everything necessary to handle SCSI & NIC traffic between Guest Partition and
