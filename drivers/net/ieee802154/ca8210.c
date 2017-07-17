056c4340da56cf84e4484b4444a59a02c9a6de71
Florian Westphal <fw@strlen.de>
2017-07-17 13:57:18 +0200

    drivers: net: add missing interrupt.h include
    these drivers use tasklets or irq apis, but don't include interrupt.h.
    Once flow cache is removed the implicit interrupt.h inclusion goes away
    which will break the build.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ieee802154/ca8210.c
+++ drivers/net/ieee802154/ca8210.c
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/string.h>
 #include <linux/workqueue.h>
+#include <linux/interrupt.h>
 
 #include <net/ieee802154_netdev.h>
 #include <net/mac802154.h>
