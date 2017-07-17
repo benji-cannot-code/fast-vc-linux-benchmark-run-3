54f95da128c315eb82457a075c55982e77599fa4
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-07-17 16:04:38 +0530

    thermal: int340x: constify attribute_group structures.
    attribute_groups are not supposed to change at runtime. All functions
    working with attribute_groups provided by <linux/sysfs.h> work
    with const attribute_group. So mark the non-const structs as const.
    
    File size before:
       text	   data	    bss	    dec	    hex	filename
       1687	    592	      0	   2279	    8e7	int3400_thermal.o
    
    File size After adding 'const':
       text	   data	    bss	    dec	    hex	filename
       1751	    528	      0	   2279	    8e7	int3400_thermal.o
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- drivers/thermal/int340x_thermal/int3400_thermal.c
+++ drivers/thermal/int340x_thermal/int3400_thermal.c
@@ -105,7 +105,7 @@ static struct attribute *uuid_attrs[] = {
 	NULL
 };
 
-static struct attribute_group uuid_attribute_group = {
+static const struct attribute_group uuid_attribute_group = {
 	.attrs = uuid_attrs,
 	.name = "uuids"
 };
