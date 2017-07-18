947c4658a6af3548bd886e482642b24eba417aba
Fabio Estevam <fabio.estevam@nxp.com>
2017-07-17 21:54:08 -0300

    mtd: nand: vf610: Remove unneeded pinctrl_pm_select_default_state()
    pinctrl_pm_select_default_state() is already the default pinctrl state and
    since pinctrl_pm_select_sleep_state() is not used in this driver, there
    is no need to explicitly call pinctrl_pm_select_default_state().
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/vf610_nfc.c
+++ drivers/mtd/nand/vf610_nfc.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/nand.h>
 #include <linux/mtd/partitions.h>
 #include <linux/of_device.h>
-#include <linux/pinctrl/consumer.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 
@@ -819,8 +818,6 @@ static int vf610_nfc_resume(struct device *dev)
 
 	struct mtd_info *mtd = dev_get_drvdata(dev);
 	struct vf610_nfc *nfc = mtd_to_nfc(mtd);
-
-	pinctrl_pm_select_default_state(dev);
 
 	err = clk_prepare_enable(nfc->clk);
 	if (err)
