80919da5c7ebb96c6c914600f6ae8a3d2fc1df9e
Laurentiu Tudor <laurentiu.tudor@nxp.com>
2017-07-19 14:42:31 +0300

    staging: fsl-mc: make the driver compile on 32-bit
    Since there's no real constrain in MC to do only atomic 64-bit we can
    enable this driver on 32-bit platforms too.
    Include linux/io-64-nonatomic-hi-lo.h to make quad device io apis used
    in the driver available on 32-bit platforms.
    
    Signed-off-by: Laurentiu Tudor <laurentiu.tudor@nxp.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/mc-sys.c
+++ drivers/staging/fsl-mc/bus/mc-sys.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioport.h>
 #include <linux/device.h>
 #include <linux/io.h>
+#include <linux/io-64-nonatomic-hi-lo.h>
 #include "../include/mc.h"
 
 #include "dpmcp.h"
