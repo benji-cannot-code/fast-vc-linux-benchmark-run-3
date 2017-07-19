434841eadd09bbdc12d4d3632f6deec9f4782e49
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-07-19 14:17:44 +0200

    USB: usbip: remove unneeded MODULE_VERSION() usage
    MODULE_VERSION is useless for in-kernel drivers, so just remove all
    usage of it in the USB usbip drivers.  Along with this, the
    USBIP_VERSION macros was removed as is was also pointless, as well as
    printing out the driver version to the syslog at init time, which is not
    necessary at all.
    
    Cc: Valentina Manea <valentina.manea.m@gmail.com>
    Acked-by: Shuah Khan <shuahkh@osg.samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/usbip/vhci_hcd.c
+++ drivers/usb/usbip/vhci_hcd.c
@@ -1517,7 +1517,6 @@ static int __init vhci_hcd_init(void)
 		}
 	}
 
-	pr_info(DRIVER_DESC " v" USBIP_VERSION "\n");
 	return ret;
 
 err_add_hcd:
@@ -1543,4 +1542,3 @@ module_exit(vhci_hcd_exit);
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
-MODULE_VERSION(USBIP_VERSION);
