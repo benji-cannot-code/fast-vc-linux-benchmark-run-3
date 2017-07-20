95479db8e8185de8552f146d15c9b3b98d4b2f46
Arnaldo Carvalho de Melo <acme@redhat.com>
2017-07-20 10:46:34 -0300

    tools include uapi: Grab a copy of linux/sched.h
    So that we make sure we have recent enough defines for things
    such as 'perf trace' system call argument beautifiers.
    
    For instance, the 'clone' syscall argument 'flag' needs to use
    CLONE_NEWCGROUP, and that is not available in RHEL7.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: http://lkml.kernel.org/n/tip-81sln0ng4a2lcxrth14vcov4@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/check-headers.sh
+++ tools/perf/check-headers.sh
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 HEADERS='
 include/uapi/linux/fcntl.h
 include/uapi/linux/perf_event.h
+include/uapi/linux/sched.h
 include/uapi/linux/stat.h
 include/linux/hash.h
 include/uapi/linux/hw_breakpoint.h
