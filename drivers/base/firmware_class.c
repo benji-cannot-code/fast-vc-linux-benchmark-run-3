1f4d0a4c5cec8f6d2f5d1d87308948748932f1ef
Luis R. Rodriguez <mcgrof@kernel.org>
2017-07-20 13:13:40 -0700

    firmware: define pr_fmt
    For some reason we have always forgotten this. Without this
    we don't get a nice prefix on our pr_debug() / pr_*() messages.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/capability.h>
 #include <linux/device.h>
 #include <linux/module.h>
