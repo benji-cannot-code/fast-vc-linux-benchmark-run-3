a096d293ddce1d752c2831d076addb28efccde08
Daniel Scheller <d.scheller@gmx.net>
2017-07-23 06:13:13 -0400

    media: dvb-frontends/stv6111: coding style cleanup
    Fix up all remainders reported by checkpatch-strict.
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/dvb-frontends/stv6111.h
+++ drivers/media/dvb-frontends/stv6111.h
@@ -4,13 +4,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if IS_REACHABLE(CONFIG_DVB_STV6111)
 
-extern struct dvb_frontend *stv6111_attach(struct dvb_frontend *fe,
-				struct i2c_adapter *i2c, u8 adr);
+struct dvb_frontend *stv6111_attach(struct dvb_frontend *fe,
+				    struct i2c_adapter *i2c, u8 adr);
 
 #else
 
 static inline struct dvb_frontend *stv6111_attach(struct dvb_frontend *fe,
-				struct i2c_adapter *i2c, u8 adr)
+						  struct i2c_adapter *i2c,
+						  u8 adr)
 {
 	pr_warn("%s: Driver disabled by Kconfig\n", __func__);
 	return NULL;
