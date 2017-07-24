b94dd03add6ae20a3ad0e95d6196daff8b053d12
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2017-07-24 14:28:02 +1000

    powerpc/mm: Optimize detection of thread local mm's
    Instead of comparing the whole CPU mask every time, let's
    keep a counter of how many bits are set in the mask. Thus
    testing for a local mm only requires testing if that counter
    is 1 and the current CPU bit is set in the mask.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/mmu_context.h
+++ arch/powerpc/include/asm/mmu_context.h
@@ -97,6 +97,14 @@ static inline void switch_mm_pgdir(struct task_struct *tsk,
 				   struct mm_struct *mm) { }
 #endif
 
+#ifdef CONFIG_PPC_BOOK3S_64
+static inline void inc_mm_active_cpus(struct mm_struct *mm)
+{
+	atomic_inc(&mm->context.active_cpus);
+}
+#else
+static inline void inc_mm_active_cpus(struct mm_struct *mm) { }
+#endif
 
 /*
  * switch_mm is the entry point called from the architecture independent
@@ -111,6 +119,7 @@ static inline void switch_mm_irqs_off(struct mm_struct *prev,
 	/* Mark this context has been used on the new CPU */
 	if (!cpumask_test_cpu(smp_processor_id(), mm_cpumask(next))) {
 		cpumask_set_cpu(smp_processor_id(), mm_cpumask(next));
+		inc_mm_active_cpus(next);
 
 		/*
 		 * This full barrier orders the store to the cpumask above vs
