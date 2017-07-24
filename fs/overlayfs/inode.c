0125127b36fb147a61bd80933718830c62e25b49
Chandan Rajendra <chandan@linux.vnet.ibm.com>
2017-07-24 01:57:54 -0500

    ovl: return anonymous st_dev for lower inodes
    For non-samefs setup, to make sure that st_dev/st_ino pair is unique
    across the system, we return a unique anonymous st_dev for stat(2)
    of lower layer inode.
    
    A following patch is going to fix constant st_dev/st_ino across copy up
    by returning origin st_dev/st_ino for copied up objects.
    
    If the st_dev/st_ino for copied up object would have been the same as
    that of the real underlying lower file, running diff on underlying lower
    file and overlay copied up file would result in diff reporting that the
    2 files are equal when in fact, they may have different content.
    
    [amir: simplify ovl_get_pseudo_dev()
           split from allocate anonymous bdev patch]
    
    Signed-off-by: Chandan Rajendra <chandan@linux.vnet.ibm.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/inode.c
+++ fs/overlayfs/inode.c
@@ -16,6 +16,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ratelimit.h>
 #include "overlayfs.h"
 
+
+static dev_t ovl_get_pseudo_dev(struct dentry *dentry)
+{
+	struct ovl_entry *oe = dentry->d_fsdata;
+
+	return oe->lowerstack[0].layer->pseudo_dev;
+}
+
 int ovl_setattr(struct dentry *dentry, struct iattr *attr)
 {
 	int err;
@@ -122,6 +130,13 @@ int ovl_getattr(const struct path *path, struct kstat *stat,
 		 */
 		stat->dev = dentry->d_sb->s_dev;
 		stat->ino = dentry->d_inode->i_ino;
+	} else if (!OVL_TYPE_UPPER(type)) {
+		/*
+		 * For non-samefs setup, to make sure that st_dev/st_ino pair
+		 * is unique across the system, we use a unique anonymous
+		 * st_dev for lower layer inode.
+		 */
+		stat->dev = ovl_get_pseudo_dev(dentry);
 	}
 
 	/*
