1d0e402db9594186d8cb37a7437e225e4167744d
Jeff Layton <jlayton@redhat.com>
2017-07-24 06:22:16 -0400

    Documentation: add some docs for errseq_t
    ...and fix up a few comments in the code.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- include/linux/errseq.h
+++ include/linux/errseq.h
@@ -1,8 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * See Documentation/errseq.rst and lib/errseq.c
+ */
 #ifndef _LINUX_ERRSEQ_H
 #define _LINUX_ERRSEQ_H
-
-/* See lib/errseq.c for more info */
 
 typedef u32	errseq_t;
 
