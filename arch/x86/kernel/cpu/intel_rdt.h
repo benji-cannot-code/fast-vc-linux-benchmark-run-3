8fa8d60fda21b443c1fcb4e2c831049fd95bdc98
Vikas Shivappa <vikas.shivappa@linux.intel.com>
2017-07-25 14:14:28 -0700

    x86/intel_rdt/cqm: Add RMID (Resource monitoring ID) management
    Hardware uses RMID(Resource monitoring ID) to keep track of each of the
    RDT events associated with tasks. The number of RMIDs is dependent on
    the SKU and is enumerated via CPUID. We add support to manage the RMIDs
    which include managing the RMID allocation and reading LLC occupancy
    for an RMID.
    
    RMID allocation is managed by keeping a free list which is initialized
    to all available RMIDs except for RMID 0 which is always reserved for
    root group. RMIDs goto a limbo list once they are
    freed since the RMIDs are still tagged to cache lines of the tasks which
    were using them - thereby still having some occupancy. They continue to
    be in limbo list until the occupancy < threshold_occupancy. The
    threshold_occupancy is a user configurable value.
    OS uses IA32_QM_CTR MSR to read the occupancy associated with an RMID
    after programming the IA32_EVENTSEL MSR with the RMID.
    
    [Tony: Improved limbo search]
    
    Signed-off-by: Vikas Shivappa <vikas.shivappa@linux.intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: ravi.v.shankar@intel.com
    Cc: tony.luck@intel.com
    Cc: fenghua.yu@intel.com
    Cc: peterz@infradead.org
    Cc: eranian@google.com
    Cc: vikas.shivappa@intel.com
    Cc: ak@linux.intel.com
    Cc: davidcc@google.com
    Cc: reinette.chatre@intel.com
    Link: http://lkml.kernel.org/r/1501017287-28083-10-git-send-email-vikas.shivappa@linux.intel.com

--- arch/x86/kernel/cpu/intel_rdt.h
+++ arch/x86/kernel/cpu/intel_rdt.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define QOS_L3_OCCUP_EVENT_ID		0x01
 #define QOS_L3_MBM_TOTAL_EVENT_ID	0x02
 #define QOS_L3_MBM_LOCAL_EVENT_ID	0x03
+#define RMID_VAL_ERROR			BIT_ULL(63)
+#define RMID_VAL_UNAVAIL		BIT_ULL(62)
 
 /**
  * struct mon_evt - Entry in the event list of a resource
@@ -99,6 +101,8 @@ struct rftype {
  * @list:	all instances of this resource
  * @id:		unique id for this instance
  * @cpu_mask:	which cpus share this resource
+ * @rmid_busy_llc:
+ *		bitmap of which limbo RMIDs are above threshold
  * @ctrl_val:	array of cache or mem ctrl values (indexed by CLOSID)
  * @new_ctrl:	new ctrl value to be loaded
  * @have_new_ctrl: did user provide new_ctrl for this domain
@@ -107,6 +111,7 @@ struct rdt_domain {
 	struct list_head	list;
 	int			id;
 	struct cpumask		cpu_mask;
+	unsigned long		*rmid_busy_llc;
 	u32			*ctrl_val;
 	u32			new_ctrl;
 	bool			have_new_ctrl;
@@ -283,6 +288,7 @@ ssize_t rdtgroup_schemata_write(struct kernfs_open_file *of,
 				char *buf, size_t nbytes, loff_t off);
 int rdtgroup_schemata_show(struct kernfs_open_file *of,
 			   struct seq_file *s, void *v);
+struct rdt_domain *get_domain_from_cpu(int cpu, struct rdt_resource *r);
 int rdt_get_mon_l3_config(struct rdt_resource *r);
 
 #endif /* _ASM_X86_INTEL_RDT_H */
