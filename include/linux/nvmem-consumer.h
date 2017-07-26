181b24741f9875968f57da8d880efeaccbc6b6db
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2017-07-26 11:34:48 +0200

    nvmem: include linux/err.h from header
    missing err.h header file can cause simillar errors like below
    in some configurations.
    "error: implicit declaration of function 'ERR_PTR'
    [-Werror=implicit-function-declaration]"
    
    This adds the missing include to ensure we can always include
    the header.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/nvmem-consumer.h
+++ include/linux/nvmem-consumer.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_NVMEM_CONSUMER_H
 #define _LINUX_NVMEM_CONSUMER_H
 
+#include <linux/err.h>
+#include <linux/errno.h>
+
 struct device;
 struct device_node;
 /* consumer cookie */
