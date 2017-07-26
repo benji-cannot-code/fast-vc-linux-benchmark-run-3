570462e9824c36a12be50eaf0d54a98963ae1817
Dmitry Eremin <dmitry.eremin@intel.com>
2017-07-26 11:22:35 -0400

    staging: lustre: llite: add xattr.h header to xattr.c
    The reason xattr.c can compile without xattr.h is due
    to lustre_compact.h being included. That header will
    eventually go away so lets directly include xattr.h.
    
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-7244
    Reviewed-on: http://review.whamcloud.com/16707
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/llite/xattr.c
+++ drivers/staging/lustre/lustre/llite/xattr.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/sched.h>
 #include <linux/mm.h>
+#include <linux/xattr.h>
 #include <linux/selinux.h>
 
 #define DEBUG_SUBSYSTEM S_LLITE
