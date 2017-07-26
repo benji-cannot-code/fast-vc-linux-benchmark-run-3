7b24e52b382d5aee8bef2719abdaaca66d3a01f1
Kalderon, Michal <Michal.Kalderon@cavium.com>
2017-07-26 14:41:51 +0300

    RDMA/qedr: Rename the qedr_cm file as a preparation for iWARP support
    The main differences between iWARP and RoCE lay in the communication
     management functions. These will be placed in separate files.
    
    Signed-off-by: Michal Kalderon <Michal.Kalderon@cavium.com>
    Signed-off-by: Ram Amrani <Ram.Amrani@cavium.com>
    Signed-off-by: Ariel Elior <Ariel.Elior@cavium.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/qedr/verbs.c
+++ drivers/infiniband/hw/qedr/verbs.c
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "qedr.h"
 #include "verbs.h"
 #include <rdma/qedr-abi.h>
-#include "qedr_cm.h"
+#include "qedr_roce_cm.h"
 
 #define DB_ADDR_SHIFT(addr)		((addr) << DB_PWM_ADDR_OFFSET_SHIFT)
 
