e02f5d7be8750cf3333b23fe95d8c35a7d2da552
John L. Hammond <john.hammond@intel.com>
2017-07-26 11:22:21 -0400

    staging: lustre: lov: remove unused code
    Remove:
      the tested but never set flag OBD_STATFS_PTLRPCD,
      the empty file lustre/lov/lovsub_io.c,
      the unused ld_emerg member of struct lov_device,
      the unused struct lov_device_emerg and supporting functions,
      the unused struct lov_lock_link and supporting functions, and
      the unused, get only, or set only members of struct
      lovsub_device, lovsub_lock, lov_sublock_env, lov_thread_info,
      lov_io_sub, lov_io, lov_request, and lov_request_set.
    Reduce the scope of several functions from lov_request.c.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-5814
    Reviewed-on: http://review.whamcloud.com/14878
    Reviewed-by: Frank Zago <fzago@cray.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/obd_class.h
+++ drivers/staging/lustre/lustre/include/obd_class.h
@@ -47,14 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OBD_STATFS_FROM_CACHE   0x0002  /* the statfs callback should not update
 					 * obd_osfs_age
 					 */
-#define OBD_STATFS_PTLRPCD      0x0004  /* requests will be sent via ptlrpcd
-					 * instead of a specific set. This
-					 * means that we cannot rely on the set
-					 * interpret routine to be called.
-					 * lov_statfs_fini() must thus be called
-					 * by the request interpret routine
-					 */
-#define OBD_STATFS_FOR_MDT0	0x0008	/* The statfs is only for retrieving
+#define OBD_STATFS_FOR_MDT0	0x0004	/* The statfs is only for retrieving
 					 * information from MDT0.
 					 */
 
--- drivers/staging/lustre/lustre/lov/lov_obd.c
+++ drivers/staging/lustre/lustre/lov/lov_obd.c
@@ -948,7 +948,8 @@ out:
 	return rc;
 }
 
-int lov_statfs_interpret(struct ptlrpc_request_set *rqset, void *data, int rc)
+static int
+lov_statfs_interpret(struct ptlrpc_request_set *rqset, void *data, int rc)
 {
 	struct lov_request_set *lovset = (struct lov_request_set *)data;
 	int err;
