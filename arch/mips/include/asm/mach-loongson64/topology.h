12a66f7c8d3eb02c1fbfba6a8db2e6232bea2c1b
Dou Liyang <douly.fnst@cn.fujitsu.com>
2017-07-26 21:34:29 +0800

    MIPS: NUMA: Remove the unused parent_node() macro
    Commit a7be6e5a7f8d ("mm: drop useless local parameters of
    __register_one_node()") removes the last user of parent_node().
    
    The parent_node() macros in both IP27 and Loongson64 are unnecessary.
    
    Remove it for cleanup.
    
    Reported-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Dou Liyang <douly.fnst@cn.fujitsu.com>
    Cc: James Hogan <james.hogan@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Cc: linux-kernel@vger.kernel.org
    Patchwork: https://patchwork.linux-mips.org/patch/16873/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/include/asm/mach-loongson64/topology.h
+++ arch/mips/include/asm/mach-loongson64/topology.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_NUMA
 
 #define cpu_to_node(cpu)	(cpu_logical_map(cpu) >> 2)
-#define parent_node(node)	(node)
 #define cpumask_of_node(node)	(&__node_data[(node)]->cpumask)
 
 struct pci_bus;
