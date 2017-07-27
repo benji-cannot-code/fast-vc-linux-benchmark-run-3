c96118f56d3530c436b107b8bc0a2098e080c95e
Alex Deucher <alexander.deucher@amd.com>
2017-07-27 15:24:49 -0400

    drm/amdgpu/sdma4: drop unused register header
    nbio registers are not used in this file.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Cc: Frank Min <Frank.Min@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/sdma_v4_0.c
+++ drivers/gpu/drm/amd/amdgpu/sdma_v4_0.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vega10/MMHUB/mmhub_1_0_offset.h"
 #include "vega10/MMHUB/mmhub_1_0_sh_mask.h"
 #include "vega10/HDP/hdp_4_0_offset.h"
-#include "vega10/NBIO/nbio_6_1_offset.h"
 #include "raven1/SDMA0/sdma0_4_1_default.h"
 
 #include "soc15_common.h"
