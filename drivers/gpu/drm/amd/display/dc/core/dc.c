7ea8a990c5b71e4b597f25d586b3c241daedd105
Andrey Grodzovsky <Andrey.Grodzovsky@amd.com>
2017-07-27 16:47:14 -0400

    drm/amd/display: Add stateless surface validation to validate_resources
    Signed-off-by: Andrey Grodzovsky <Andrey.Grodzovsky@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -676,6 +676,21 @@ static bool validate_streams (
 	return true;
 }
 
+static bool validate_surfaces(
+		const struct dc *dc,
+		const struct dc_validation_set set[],
+		int set_count)
+{
+	int i, j;
+
+	for (i = 0; i < set_count; i++)
+		for (j = 0; j < set[i].surface_count; j++)
+			if (!dc_validate_surface(dc, set[i].surfaces[j]))
+				return false;
+
+	return true;
+}
+
 struct validate_context *dc_get_validate_context(
 		const struct dc *dc,
 		const struct dc_validation_set set[],
@@ -725,6 +740,9 @@ bool dc_validate_resources(
 	struct validate_context *context;
 
 	if (!validate_streams(dc, set, set_count))
+		return false;
+
+	if (!validate_surfaces(dc, set, set_count))
 		return false;
 
 	context = dm_alloc(sizeof(struct validate_context));
@@ -1064,6 +1082,9 @@ bool dc_commit_streams(
 	}
 
 	if (!validate_streams(dc, set, stream_count))
+		return false;
+
+	if (!validate_surfaces(dc, set, stream_count))
 		return false;
 
 	context = dm_alloc(sizeof(struct validate_context));
