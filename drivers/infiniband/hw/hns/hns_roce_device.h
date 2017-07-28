3b6963a3efb944cbfbc0da39ebb4cbff7cbdc9eb
Doug Ledford <dledford@redhat.com>
2017-07-28 15:17:18 -0400

    RDMA/hns: fix build regression
    The 0day build system flags implicit includes as errors.  A patch from
    Matan Barak to allow hns_roce, an aarch64 specific RDMA driver, to be
    built on other arches, but it resulted in build regressions.  The
    problem is that hns_roce_device.h needs a definition for __raw_writeq
    but did not have an include to provide it.  Add <linux/io.h> as an
    include to resolve the issue.
    
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hns/hns_roce_device.h
+++ drivers/infiniband/hw/hns/hns_roce_device.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _HNS_ROCE_DEVICE_H
 #define _HNS_ROCE_DEVICE_H
 
+#include <linux/io.h>
 #include <rdma/ib_verbs.h>
 
 #define DRV_NAME "hns_roce"
