877a2dc25fdf2d951b171e75de5d521a949846f2
Archit Taneja <architt@codeaurora.org>
2017-07-28 16:17:02 +0530

    drm/msm/hdmi: Set up runtime PM for HDMI
    Enable rudimentary runtime PM in the HDMI driver. We can't really do
    agressive PM toggling at the moment because we need to leave the hpd
    clocks enabled all the time. There isn't much benefit of creating
    suspend/resume ops to toggle clocks either.
    
    We just make sure that we configure the power domain in the HDMI bridge's
    enable/disable paths, and the HDMI connector's detect() op.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/hdmi/hdmi_bridge.c
+++ drivers/gpu/drm/msm/hdmi/hdmi_bridge.c
@@ -36,6 +36,8 @@ static void msm_hdmi_power_on(struct drm_bridge *bridge)
 	const struct hdmi_platform_config *config = hdmi->config;
 	int i, ret;
 
+	pm_runtime_get_sync(&hdmi->pdev->dev);
+
 	for (i = 0; i < config->pwr_reg_cnt; i++) {
 		ret = regulator_enable(hdmi->pwr_regs[i]);
 		if (ret) {
@@ -85,6 +87,8 @@ static void power_off(struct drm_bridge *bridge)
 					config->pwr_reg_names[i], ret);
 		}
 	}
+
+	pm_runtime_put_autosuspend(&hdmi->pdev->dev);
 }
 
 #define AVI_IFRAME_LINE_NUMBER 1
