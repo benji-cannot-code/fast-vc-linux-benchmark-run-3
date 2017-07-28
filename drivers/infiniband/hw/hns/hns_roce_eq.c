8ab3319052eb42c9aac185a65017c692051f4734
Arnd Bergmann <arnd@arndb.de>
2017-07-28 15:35:22 +0200

    IB/hns: include linux/interrupt.h
    I ran into this build error on linux-next:
    
    drivers/infiniband/hw/hns/hns_roce_eq.c:477:8: error: unknown type name 'irqreturn_t'
     static irqreturn_t hns_roce_msi_x_interrupt(int irq, void *eq_ptr)
            ^~~~~~~~~~~
    drivers/infiniband/hw/hns/hns_roce_eq.c: In function 'hns_roce_msi_x_interrupt':
    drivers/infiniband/hw/hns/hns_roce_eq.c:485:9: error: implicit declaration of function 'IRQ_RETVAL'; did you mean 'BPF_RVAL'? [-Werror=implicit-function-declaration]
      return IRQ_RETVAL(int_work);
    
    I have bisected this to a seemingly unrelated change that happened
    to remove some indirect header inclusions. Simply including the
    required header explicitly fixes the build failure.
    
    Fixes: 09c7570480f7 ("xfrm: remove flow cache")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hns/hns_roce_eq.c
+++ drivers/infiniband/hw/hns/hns_roce_eq.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/platform_device.h>
+#include <linux/interrupt.h>
 #include "hns_roce_common.h"
 #include "hns_roce_device.h"
 #include "hns_roce_eq.h"
