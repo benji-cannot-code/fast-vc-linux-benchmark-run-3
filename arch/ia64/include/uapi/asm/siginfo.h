de134984c61afe28bece46569d8700bb3f2c6a66
Eric W. Biederman <ebiederm@xmission.com>
2017-07-31 15:47:40 -0500

    signal/ia64: switch the last arch-specific copy_siginfo_to_user() to generic version
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- arch/ia64/include/uapi/asm/siginfo.h
+++ arch/ia64/include/uapi/asm/siginfo.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define __ARCH_SI_PREAMBLE_SIZE	(4 * sizeof(int))
 
-#define HAVE_ARCH_COPY_SIGINFO_TO_USER
-
 #include <asm-generic/siginfo.h>
 
 #define si_imm		_sifields._sigfault._imm	/* as per UNIX SysV ABI spec */
