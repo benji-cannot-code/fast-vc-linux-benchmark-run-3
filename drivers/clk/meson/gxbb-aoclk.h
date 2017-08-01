16cd86c37fd81a60b157606addde051d0231de89
Neil Armstrong <narmstrong@baylibre.com>
2017-08-01 13:56:57 +0200

    clk: meson: gxbb-aoclk: Switch to regmap for register access
    Switch the aoclk driver to use the new bindings and switch all the
    registers access to regmap only.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

---
+++ drivers/clk/meson/gxbb-aoclk.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (c) 2017 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#ifndef __GXBB_AOCLKC_H
+#define __GXBB_AOCLKC_H
+
+/* AO Configuration Clock registers offsets */
+#define AO_RTI_GEN_CNTL_REG0	0x40
+
+struct aoclk_gate_regmap {
+	struct clk_hw hw;
+	unsigned bit_idx;
+	struct regmap *regmap;
+	spinlock_t *lock;
+};
+
+#define to_aoclk_gate_regmap(_hw) \
+	container_of(_hw, struct aoclk_gate_regmap, hw)
+
+extern const struct clk_ops meson_aoclk_gate_regmap_ops;
+
+#endif /* __GXBB_AOCLKC_H */
