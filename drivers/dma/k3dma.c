2fa58102fe8febc5749e46be0ff93a8039af31c4
Antonio Borneo <borneo.antonio@gmail.com>
2017-08-01 22:09:27 +0200

    dmaengine: k3dma: remove useless ON_WARN_ONCE()
    Commit 36387a2b1f62b5c087c5fe6f0f7b23b94f722ad7 ("k3dma: Fix
    memory handling in preparation for cyclic mode") adds few
    calls to ON_WARN_ONCE() to track the use of ds_run/ds_done.
    
    After the two fixes:
    - dmaengine: k3dma: fix non-cyclic mode
    - dmaengine: k3dma: fix re-free of the same descriptor
    the behaviour of ds_run/ds_done is properly fixed.
    The remaining ON_WARN_ONCE() are never triggered and can be
    removed.
    
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/k3dma.c
+++ drivers/dma/k3dma.c
@@ -224,7 +224,6 @@ static irqreturn_t k3_dma_int_handler(int irq, void *dev_id)
 			if (c && (tc1 & BIT(i))) {
 				spin_lock_irqsave(&c->vc.lock, flags);
 				vchan_cookie_complete(&p->ds_run->vd);
-				WARN_ON_ONCE(p->ds_done);
 				p->ds_done = p->ds_run;
 				p->ds_run = NULL;
 				spin_unlock_irqrestore(&c->vc.lock, flags);
@@ -275,7 +274,6 @@ static int k3_dma_start_txd(struct k3_dma_chan *c)
 		 */
 		list_del(&ds->vd.node);
 
-		WARN_ON_ONCE(c->phy->ds_run);
 		c->phy->ds_run = ds;
 		c->phy->ds_done = NULL;
 		/* start dma */
