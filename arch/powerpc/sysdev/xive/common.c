369abab831b577c8885bd584726d2bef8c75cb5a
Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
2017-08-02 23:55:38 +0530

    powerpc/xmon: Exclude all of xmon from ftrace
    Exclude core xmon files from ftrace (along with an xmon xive helper
    outside of xmon/) to minimize impact of ftrace while within xmon.
    
    Before:
      /sys/kernel/debug/tracing# grep -ci xmon available_filter_functions
      26
    
    After:
      /sys/kernel/debug/tracing# grep -ci xmon available_filter_functions
      0
    
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    [mpe: Use $(subst ..) on KBUILD_CFLAGS rather than CFLAGS_REMOVE_xxx]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/sysdev/xive/common.c
+++ arch/powerpc/sysdev/xive/common.c
@@ -191,7 +191,7 @@ static u32 xive_scan_interrupts(struct xive_cpu *xc, bool just_peek)
  * This is used to perform the magic loads from an ESB
  * described in xive.h
  */
-static u8 xive_poke_esb(struct xive_irq_data *xd, u32 offset)
+static notrace u8 xive_poke_esb(struct xive_irq_data *xd, u32 offset)
 {
 	u64 val;
 
@@ -205,7 +205,7 @@ static u8 xive_poke_esb(struct xive_irq_data *xd, u32 offset)
 }
 
 #ifdef CONFIG_XMON
-static void xive_dump_eq(const char *name, struct xive_q *q)
+static notrace void xive_dump_eq(const char *name, struct xive_q *q)
 {
 	u32 i0, i1, idx;
 
@@ -219,7 +219,7 @@ static void xive_dump_eq(const char *name, struct xive_q *q)
 		    q->toggle, i0, i1);
 }
 
-void xmon_xive_do_dump(int cpu)
+notrace void xmon_xive_do_dump(int cpu)
 {
 	struct xive_cpu *xc = per_cpu(xive_cpu, cpu);
 
