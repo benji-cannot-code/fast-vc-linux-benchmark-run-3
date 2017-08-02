6f862f82737f3d19452237da87102db50bfeecdd
Breno Leitao <leitao@debian.org>
2017-08-02 17:14:06 -0300

    powerpc/xmon: Disable tracing when entering xmon
    If tracing is enabled and you get into xmon, the tracing buffer
    continues to be updated, causing possible loss of data and unnecessary
    tracing information coming from xmon functions.
    
    This patch simple disables tracing when entering xmon, and re-enables it
    if the kernel is resumed (with 'x').
    
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Acked-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/xmon/xmon.c
+++ arch/powerpc/xmon/xmon.c
@@ -90,6 +90,7 @@ static unsigned long nidump = 16;
 static unsigned long ncsum = 4096;
 static int termch;
 static char tmpstr[128];
+static int tracing_enabled;
 
 static long bus_error_jmp[JMP_BUF_LEN];
 static int catch_memory_errors;
@@ -462,6 +463,9 @@ static int xmon_core(struct pt_regs *regs, int fromipi)
 
 	local_irq_save(flags);
 	hard_irq_disable();
+
+	tracing_enabled = tracing_is_on();
+	tracing_off();
 
 	bp = in_breakpoint_table(regs->nip, &offset);
 	if (bp != NULL) {
@@ -983,6 +987,8 @@ cmds(struct pt_regs *excp)
 			break;
 		case 'x':
 		case 'X':
+			if (tracing_enabled)
+				tracing_on();
 			return cmd;
 		case EOF:
 			printf(" <no input ...>\n");
@@ -2242,8 +2248,6 @@ static void dump_tracing(void)
 		ftrace_dump(DUMP_ORIG);
 	else
 		ftrace_dump(DUMP_ALL);
-
-	tracing_on();
 }
 
 #ifdef CONFIG_PPC64
