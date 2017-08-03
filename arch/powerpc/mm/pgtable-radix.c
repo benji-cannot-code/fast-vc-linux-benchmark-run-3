bce83d77d7ff1940eb09e5726b5c0cd9f9a0718c
Suraj Jitindar Singh <sjitindarsingh@gmail.com>
2017-08-03 14:15:51 +1000

    powerpc/mm: Properly invalidate when setting process table base
    The host process table base is stored in the partition table by calling
    the function native_register_process_table(). Currently this just sets
    the entry in memory and is missing a subsequent cache invalidation
    instruction. Any update to the partition table should be followed by a
    cache invalidation instruction specifying invalidation of the caching of
    any partition table entries (RIC = 2, PRS = 0).
    
    We already have a function to update the partition table with the
    required cache invalidation instructions - mmu_partition_table_set_entry().
    Update the native_register_process_table() function to call
    mmu_partition_table_set_entry(), this ensures all appropriate
    invalidation will be performed.
    
    Signed-off-by: Suraj Jitindar Singh <sjitindarsingh@gmail.com>
    Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    [mpe: Use a local for patb0 to clean it up slightly]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/pgtable-radix.c
+++ arch/powerpc/mm/pgtable-radix.c
@@ -29,9 +29,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static int native_register_process_table(unsigned long base, unsigned long pg_sz,
 					 unsigned long table_size)
 {
-	unsigned long patb1 = base | table_size | PATB_GR;
+	unsigned long patb0, patb1;
 
-	partition_tb->patb1 = cpu_to_be64(patb1);
+	patb0 = be64_to_cpu(partition_tb[0].patb0);
+	patb1 = base | table_size | PATB_GR;
+
+	mmu_partition_table_set_entry(0, patb0, patb1);
+
 	return 0;
 }
 
