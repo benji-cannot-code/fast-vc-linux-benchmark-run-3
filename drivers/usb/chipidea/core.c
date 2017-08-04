67d0fca75f560f4f9b392efdf16588f5f3400e53
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-08-04 17:36:38 +0530

    usb: chipidea: constify attribute_group structures.
    attribute_group are not supposed to change at runtime. All functions
    working with attribute_group provided by <linux/sysfs.h> work with
    const attribute_group. So mark the non-const structs as const.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/chipidea/core.c
+++ drivers/usb/chipidea/core.c
@@ -888,7 +888,7 @@ static struct attribute *ci_attrs[] = {
 	NULL,
 };
 
-static struct attribute_group ci_attr_group = {
+static const struct attribute_group ci_attr_group = {
 	.attrs = ci_attrs,
 };
 
