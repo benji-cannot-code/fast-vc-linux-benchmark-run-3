2450a25c62eecf54bfc1a533af5b40cf146d50fe
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2017-08-05 01:44:16 +0300

    drm: omapdrm: Move PCD, LINEWIDTH and DOWNSCALE features to dispc driver
    The FEAT_PARAM_DSS_PCD, FEAT_PARAM_LINEWIDTH and FEAT_PARAM_DOWNSCALE
    features are specific to the DISPC, move them from the omap_dss_features
    structure to the dispc driver.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/dss/dss_features.h
+++ drivers/gpu/drm/omapdrm/dss/dss_features.h
@@ -28,9 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 enum dss_range_param {
 	FEAT_PARAM_DSS_FCK,
-	FEAT_PARAM_DSS_PCD,
-	FEAT_PARAM_DOWNSCALE,
-	FEAT_PARAM_LINEWIDTH,
 };
 
 /* DSS Feature Functions */
