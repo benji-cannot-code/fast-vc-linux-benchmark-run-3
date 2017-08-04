5dcd5f4bb5844dd48f5f169eacb54c9d8685f22e
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2017-08-05 01:44:17 +0300

    drm: omapdrm: Move DSS_FCK feature to dss driver
    The FEAT_PARAM_DSS_FCK feature is specific to the DSS, move it from the
    omap_dss_features structure to the dss driver.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/dss/dss_features.h
+++ drivers/gpu/drm/omapdrm/dss/dss_features.h
@@ -26,14 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_DSS_LCD_MANAGERS	3
 #define MAX_NUM_DSI		2
 
-enum dss_range_param {
-	FEAT_PARAM_DSS_FCK,
-};
-
-/* DSS Feature Functions */
-unsigned long dss_feat_get_param_min(enum dss_range_param param);
-unsigned long dss_feat_get_param_max(enum dss_range_param param);
-
 void dss_features_init(enum omapdss_version version);
 
 enum omap_dss_output_id dss_feat_get_supported_outputs(enum omap_channel channel);
