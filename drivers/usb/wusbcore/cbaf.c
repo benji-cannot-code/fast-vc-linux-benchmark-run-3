60e19cbc53840b723d59e129418fb8466079298b
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-08-04 17:36:45 +0530

    usb: wusbcore: cbaf: constify attribute_group structures.
    attribute_group are not supposed to change at runtime. All functions
    working with attribute_group provided by <linux/sysfs.h> work with
    const attribute_group. So mark the non-const structs as const.
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/wusbcore/cbaf.c
+++ drivers/usb/wusbcore/cbaf.c
@@ -587,7 +587,7 @@ static struct attribute *cbaf_dev_attrs[] = {
 	NULL,
 };
 
-static struct attribute_group cbaf_dev_attr_group = {
+static const struct attribute_group cbaf_dev_attr_group = {
 	.name = NULL,	/* we want them in the same directory */
 	.attrs = cbaf_dev_attrs,
 };
