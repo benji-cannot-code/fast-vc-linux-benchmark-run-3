7b997d13fc34640be01676d2530be0bfd71d3ae1
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2017-08-08 07:27:05 -0400

    media: s5p-jpeg: disable encoder/decoder in exynos4-like hardware after use
    Clearing the bits turns off the encoder/decoder. If the hardware
    is not turned off after use, at subsequent uses it does not work
    in a stable manner, resulting in incorrect interrupt status value
    being read and e.g. erroneous read of compressed bitstream size.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/s5p-jpeg/jpeg-core.h
+++ drivers/media/platform/s5p-jpeg/jpeg-core.h
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define S5P_JPEG_ENCODE		0
 #define S5P_JPEG_DECODE		1
+#define S5P_JPEG_DISABLE	-1
 
 #define FMT_TYPE_OUTPUT		0
 #define FMT_TYPE_CAPTURE	1
