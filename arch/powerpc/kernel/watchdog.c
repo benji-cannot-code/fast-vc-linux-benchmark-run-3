73ed52e0d3e4c1ccb0aedb2736a708bd53910031
Nicholas Piggin <npiggin@gmail.com>
2017-08-09 22:41:24 +1000

    powerpc/watchdog: Fix final-check recovered case
    When the watchdog decides to panic, it takes the lock and double
    checks everything (to avoid races with the CPU being unstuck or
    panic()ed by something else).
    
    The exit label was misplaced and would result in all-CPUs backtrace
    and watchdog panic even in the case that the condition was found to be
    resolved.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/watchdog.c
+++ arch/powerpc/kernel/watchdog.c
@@ -145,7 +145,6 @@ static void watchdog_smp_panic(int cpu, u64 tb)
 	for_each_cpu(c, &wd_smp_cpus_pending)
 		set_cpu_stuck(c, tb);
 
-out:
 	wd_smp_unlock(&flags);
 
 	printk_safe_flush();
@@ -158,6 +157,11 @@ out:
 
 	if (hardlockup_panic)
 		nmi_panic(NULL, "Hard LOCKUP");
+
+	return;
+
+out:
+	wd_smp_unlock(&flags);
 }
 
 static void wd_smp_clear_cpu_pending(int cpu, u64 tb)
