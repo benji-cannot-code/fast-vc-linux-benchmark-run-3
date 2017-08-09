80b8c00bdf5727953658795eae6ac8a5674ce30e
Baoquan He <bhe@redhat.com>
2017-08-09 16:33:37 +0800

    iommu/amd: Add function copy_dev_tables()
    Add function copy_dev_tables to copy the old DEV table entries of the panicked
    kernel to the new allocated device table. Since all iommus share the same device
    table the copy only need be done one time. Here add a new global old_dev_tbl_cpy
    to point to the newly allocated device table which the content of old device
    table will be copied to. Besides, we also need to:
    
      - Check whether all IOMMUs actually use the same device table with the same size
    
      - Verify that the size of the old device table is the expected size.
    
      - Reserve the old domain id occupied in 1st kernel to avoid touching the old
        io-page tables. Then on-flight DMA can continue looking it up.
    
    And also define MACRO DEV_DOMID_MASK to replace magic number 0xffffULL, it can be
    reused in copy_dev_tables().
    
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/amd_iommu_types.h
+++ drivers/iommu/amd_iommu_types.h
@@ -337,6 +337,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DTE_FLAG_MASK	(0x3ffULL << 32)
 #define DTE_GLX_SHIFT	(56)
 #define DTE_GLX_MASK	(3)
+#define DEV_DOMID_MASK	0xffffULL
 
 #define DTE_GCR3_VAL_A(x)	(((x) >> 12) & 0x00007ULL)
 #define DTE_GCR3_VAL_B(x)	(((x) >> 15) & 0x0ffffULL)
