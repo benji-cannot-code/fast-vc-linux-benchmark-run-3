c9312542b857325685fc375208dff8f32674a5a1
Baoquan He <bhe@redhat.com>
2017-08-09 16:33:39 +0800

    iommu/amd: Do sanity check for address translation and irq remap of old dev table entry
    Firstly split the dev table entry copy into address translation part
    and irq remapping part. Because these two parts could be enabled
    independently.
    
    Secondly do sanity check for address translation and irq remap of old
    dev table entry separately.
    
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/amd_iommu_types.h
+++ drivers/iommu/amd_iommu_types.h
@@ -251,6 +251,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define GA_GUEST_NR		0x1
 
+/* Bit value definition for dte irq remapping fields*/
+#define DTE_IRQ_PHYS_ADDR_MASK	(((1ULL << 45)-1) << 6)
+#define DTE_IRQ_REMAP_INTCTL_MASK	(0x3ULL << 60)
+#define DTE_IRQ_TABLE_LEN_MASK	(0xfULL << 1)
+#define DTE_IRQ_REMAP_INTCTL    (2ULL << 60)
+#define DTE_IRQ_TABLE_LEN       (8ULL << 1)
+#define DTE_IRQ_REMAP_ENABLE    1ULL
+
 #define PAGE_MODE_NONE    0x00
 #define PAGE_MODE_1_LEVEL 0x01
 #define PAGE_MODE_2_LEVEL 0x02
