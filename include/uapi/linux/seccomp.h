abb6622dbb83d50eae7be256b02cfa3e13acb486
Kees Cook <keescook@chromium.org>
2017-08-11 12:53:18 -0700

    seccomp: Rename SECCOMP_RET_KILL to SECCOMP_RET_KILL_THREAD
    In preparation for adding SECCOMP_RET_KILL_PROCESS, rename SECCOMP_RET_KILL
    to the more accurate SECCOMP_RET_KILL_THREAD.
    
    The existing selftest values are intentionally left as SECCOMP_RET_KILL
    just to be sure we're exercising the alias.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/uapi/linux/seccomp.h
+++ include/uapi/linux/seccomp.h
@@ -28,7 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The ordering ensures that a min_t() over composed return values always
  * selects the least permissive choice.
  */
-#define SECCOMP_RET_KILL	0x00000000U /* kill the task immediately */
+#define SECCOMP_RET_KILL_THREAD	0x00000000U /* kill the thread */
+#define SECCOMP_RET_KILL	SECCOMP_RET_KILL_THREAD
 #define SECCOMP_RET_TRAP	0x00030000U /* disallow and force a SIGSYS */
 #define SECCOMP_RET_ERRNO	0x00050000U /* returns an errno */
 #define SECCOMP_RET_TRACE	0x7ff00000U /* pass to a tracer or disallow */
