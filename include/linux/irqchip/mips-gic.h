f0040f634ef80dba78bfb3fb57b8f7543514493d
Paul Burton <paul.burton@imgtec.com>
2017-08-12 21:36:23 -0700

    irqchip: mips-gic: Remove gic_map_to_vpe()
    Remove the gic_map_to_vpe() function in favour of using the new
    write_gic_map_vp() accessor function which isn't any more complex to
    use & allows us to drop a level of abstraction.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/17033/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/linux/irqchip/mips-gic.h
+++ include/linux/irqchip/mips-gic.h
@@ -38,12 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Set/Clear corresponding bit in Edge Detect Register */
 #define GIC_SH_WEDGE_OFS		0x0280
 
-/* Maps Interrupt X to a VPE */
-#define GIC_SH_INTR_MAP_TO_VPE_BASE_OFS 0x2000
-#define GIC_SH_MAP_TO_VPE_REG_OFF(intr, vpe) \
-	((32 * (intr)) + (((vpe) / 32) * 4))
-#define GIC_SH_MAP_TO_VPE_REG_BIT(vpe)	(1 << ((vpe) % 32))
-
 /* Register Map for Local Section */
 #define GIC_VPE_CTL_OFS			0x0000
 #define GIC_VPE_PEND_OFS		0x0004
