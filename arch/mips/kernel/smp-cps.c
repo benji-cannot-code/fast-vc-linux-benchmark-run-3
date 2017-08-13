725e7ef953f18d24ecb9b2941dfa5a904dd93e09
Paul Burton <paul.burton@imgtec.com>
2017-08-12 21:36:38 -0700

    MIPS: Remove unnecessary inclusions of linux/irqchip/mips-gic.h
    linux/irqchip/mips-gic.h is included in a few files that don't actually
    use it at all. Remove these unnecessary inclusions in preparation for
    removing the header.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    Cc: Jason Cooper <jason@lakedaemon.net>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/17048/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/smp-cps.c
+++ arch/mips/kernel/smp-cps.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/delay.h>
 #include <linux/io.h>
-#include <linux/irqchip/mips-gic.h>
 #include <linux/sched/task_stack.h>
 #include <linux/sched/hotplug.h>
 #include <linux/slab.h>
