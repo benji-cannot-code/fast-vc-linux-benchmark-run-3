2e75f782bc78732e10c536f9ef14a73780c7a245
John Stultz <john.stultz@linaro.org>
2017-08-14 13:46:32 -0700

    kselftests: timers: freq-step: Define ADJ_SETOFFSET if device has older kernel headers
    On some systems, the kernel headers haven't been updated to include
    ADJ_SETOFFSET, so define it in the test if needed.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Miroslav Lichvar <mlichvar@redhat.com>
    Cc: Richard Cochran <richardcochran@gmail.com>
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: Stephen Boyd <stephen.boyd@linaro.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: linux-kselftest@vger.kernel.org
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- tools/testing/selftests/timers/freq-step.c
+++ tools/testing/selftests/timers/freq-step.c
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_FREQ_ERROR 10e-6
 #define MAX_STDDEV 1000e-9
 
+#ifndef ADJ_SETOFFSET
+  #define ADJ_SETOFFSET 0x0100
+#endif
+
 struct sample {
 	double offset;
 	double time;
