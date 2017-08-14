2edf360dd1081644b06f42b2824978691a56b10a
Wei Yongjun <weiyongjun1@huawei.com>
2017-08-14 10:31:13 -0700

    Input: mxs-lradc - make symbol mxs_lradc_ts_irq_names static
    Fixes the following sparse warning:
    
    drivers/input/touchscreen/mxs-lradc-ts.c:33:12: warning:
     symbol 'mxs_lradc_ts_irq_names' was not declared. Should it be static?
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    [dtor: changed to static const char * const]
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/mxs-lradc-ts.c
+++ drivers/input/touchscreen/mxs-lradc-ts.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/platform_device.h>
 
-const char *mxs_lradc_ts_irq_names[] = {
+static const char * const mxs_lradc_ts_irq_names[] = {
 	"mxs-lradc-touchscreen",
 	"mxs-lradc-channel6",
 	"mxs-lradc-channel7",
