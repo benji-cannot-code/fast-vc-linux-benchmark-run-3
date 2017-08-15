3d4d6d0126bc481c1bf375fdaf6df80fe58dc5cc
Hannes Reinecke <hare@suse.de>
2017-08-15 10:21:43 +0200

    scsi: ses: make page2 support optional
    Simple subenclosures do not need to support SES page 2, so make it
    optional.
    
    Signed-off-by: Hannes Reinecke <hare@suse.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ses.c
+++ drivers/scsi/ses.c
@@ -52,6 +52,13 @@ struct ses_component {
 	u64 addr;
 };
 
+static bool ses_page2_supported(struct enclosure_device *edev)
+{
+	struct ses_device *ses_dev = edev->scratch;
+
+	return (ses_dev->page2 != NULL);
+}
+
 static int ses_probe(struct device *dev)
 {
 	struct scsi_device *sdev = to_scsi_device(dev);
@@ -205,6 +212,10 @@ static void ses_get_fault(struct enclosure_device *edev,
 {
 	unsigned char *desc;
 
+	if (!ses_page2_supported(edev)) {
+		ecomp->fault = 0;
+		return;
+	}
 	desc = ses_get_page2_descriptor(edev, ecomp);
 	if (desc)
 		ecomp->fault = (desc[3] & 0x60) >> 4;
@@ -216,6 +227,9 @@ static int ses_set_fault(struct enclosure_device *edev,
 {
 	unsigned char desc[4];
 	unsigned char *desc_ptr;
+
+	if (!ses_page2_supported(edev))
+		return -EINVAL;
 
 	desc_ptr = ses_get_page2_descriptor(edev, ecomp);
 
@@ -244,6 +258,10 @@ static void ses_get_status(struct enclosure_device *edev,
 {
 	unsigned char *desc;
 
+	if (!ses_page2_supported(edev)) {
+		ecomp->status = 0;
+		return;
+	}
 	desc = ses_get_page2_descriptor(edev, ecomp);
 	if (desc)
 		ecomp->status = (desc[0] & 0x0f);
@@ -254,6 +272,10 @@ static void ses_get_locate(struct enclosure_device *edev,
 {
 	unsigned char *desc;
 
+	if (!ses_page2_supported(edev)) {
+		ecomp->locate = 0;
+		return;
+	}
 	desc = ses_get_page2_descriptor(edev, ecomp);
 	if (desc)
 		ecomp->locate = (desc[2] & 0x02) ? 1 : 0;
@@ -265,6 +287,9 @@ static int ses_set_locate(struct enclosure_device *edev,
 {
 	unsigned char desc[4];
 	unsigned char *desc_ptr;
+
+	if (!ses_page2_supported(edev))
+		return -EINVAL;
 
 	desc_ptr = ses_get_page2_descriptor(edev, ecomp);
 
@@ -293,6 +318,9 @@ static int ses_set_active(struct enclosure_device *edev,
 {
 	unsigned char desc[4];
 	unsigned char *desc_ptr;
+
+	if (!ses_page2_supported(edev))
+		return -EINVAL;
 
 	desc_ptr = ses_get_page2_descriptor(edev, ecomp);
 
@@ -330,6 +358,11 @@ static void ses_get_power_status(struct enclosure_device *edev,
 {
 	unsigned char *desc;
 
+	if (!ses_page2_supported(edev)) {
+		ecomp->power_status = 0;
+		return;
+	}
+
 	desc = ses_get_page2_descriptor(edev, ecomp);
 	if (desc)
 		ecomp->power_status = (desc[3] & 0x10) ? 0 : 1;
@@ -341,6 +374,9 @@ static int ses_set_power_status(struct enclosure_device *edev,
 {
 	unsigned char desc[4];
 	unsigned char *desc_ptr;
+
+	if (!ses_page2_supported(edev))
+		return -EINVAL;
 
 	desc_ptr = ses_get_page2_descriptor(edev, ecomp);
 
@@ -675,7 +711,7 @@ static int ses_intf_add(struct device *cdev,
 	page = 2;
 	result = ses_recv_diag(sdev, page, hdr_buf, INIT_ALLOC_SIZE);
 	if (result)
-		goto recv_failed;
+		goto page2_not_supported;
 
 	len = (hdr_buf[2] << 8) + hdr_buf[3] + 4;
 	buf = kzalloc(len, GFP_KERNEL);
@@ -708,6 +744,7 @@ static int ses_intf_add(struct device *cdev,
 		ses_dev->page10_len = len;
 		buf = NULL;
 	}
+page2_not_supported:
 	scomp = kzalloc(sizeof(struct ses_component) * components, GFP_KERNEL);
 	if (!scomp)
 		goto err_free;
