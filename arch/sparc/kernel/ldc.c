f2341844dc4c18d6ca939e5f6737e3fa7f85ba98
Jag Raman <jag.raman@oracle.com>
2017-08-15 17:03:02 -0400

    sparc64: vcc: Add RX & TX timer for delayed LDC operation
    Add RX & TX timers to perform delayed/asynchronous LDC
    read and write operations.
    
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Shannon Nelson <shannon.nelson@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/kernel/ldc.c
+++ arch/sparc/kernel/ldc.c
@@ -1481,6 +1481,7 @@ int ldc_rx_reset(struct ldc_channel *lp)
 {
 	return __set_rx_head(lp, lp->rx_tail);
 }
+EXPORT_SYMBOL(ldc_rx_reset);
 
 void __ldc_print(struct ldc_channel *lp, const char *caller)
 {
