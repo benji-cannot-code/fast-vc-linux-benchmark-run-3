8ae6331e1f3f147a4dffb708a96d2d8dc54fb900
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2017-08-17 10:55:54 -0700

    Revert "staging: imx: fix non-static declarations"
    This reverts commit 9a5a6911aa3fb679f4640d5cf2346d095a7641f1 as it
    causes unneeded build warnings :(
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: JB Van Puyvelde <jbvanpuyvelde@gmail.com>
    Signed-off-by: Greg Kroah-Hartman gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/atomisp/i2c/imx/imx.c
+++ drivers/staging/media/atomisp/i2c/imx/imx.c
@@ -1085,7 +1085,7 @@ static int imx_g_bin_factor_y(struct v4l2_subdev *sd, s32 *val)
 	return 0;
 }
 
-static int imx_vcm_power_up(struct v4l2_subdev *sd)
+int imx_vcm_power_up(struct v4l2_subdev *sd)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->power_up)
@@ -1093,7 +1093,7 @@ static int imx_vcm_power_up(struct v4l2_subdev *sd)
 	return 0;
 }
 
-static int imx_vcm_power_down(struct v4l2_subdev *sd)
+int imx_vcm_power_down(struct v4l2_subdev *sd)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->power_down)
@@ -1101,7 +1101,7 @@ static int imx_vcm_power_down(struct v4l2_subdev *sd)
 	return 0;
 }
 
-static int imx_vcm_init(struct v4l2_subdev *sd)
+int imx_vcm_init(struct v4l2_subdev *sd)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->init)
@@ -1109,7 +1109,7 @@ static int imx_vcm_init(struct v4l2_subdev *sd)
 	return 0;
 }
 
-static int imx_t_focus_vcm(struct v4l2_subdev *sd, u16 val)
+int imx_t_focus_vcm(struct v4l2_subdev *sd, u16 val)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->t_focus_vcm)
@@ -1117,15 +1117,14 @@ static int imx_t_focus_vcm(struct v4l2_subdev *sd, u16 val)
 	return 0;
 }
 
-static int imx_t_focus_abs(struct v4l2_subdev *sd, s32 value)
+int imx_t_focus_abs(struct v4l2_subdev *sd, s32 value)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->t_focus_abs)
 		return dev->vcm_driver->t_focus_abs(sd, value);
 	return 0;
 }
-
-static int imx_t_focus_rel(struct v4l2_subdev *sd, s32 value)
+int imx_t_focus_rel(struct v4l2_subdev *sd, s32 value)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->t_focus_rel)
@@ -1133,7 +1132,7 @@ static int imx_t_focus_rel(struct v4l2_subdev *sd, s32 value)
 	return 0;
 }
 
-static int imx_q_focus_status(struct v4l2_subdev *sd, s32 *value)
+int imx_q_focus_status(struct v4l2_subdev *sd, s32 *value)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->q_focus_status)
@@ -1141,7 +1140,7 @@ static int imx_q_focus_status(struct v4l2_subdev *sd, s32 *value)
 	return 0;
 }
 
-static int imx_q_focus_abs(struct v4l2_subdev *sd, s32 *value)
+int imx_q_focus_abs(struct v4l2_subdev *sd, s32 *value)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->q_focus_abs)
@@ -1149,7 +1148,7 @@ static int imx_q_focus_abs(struct v4l2_subdev *sd, s32 *value)
 	return 0;
 }
 
-static int imx_t_vcm_slew(struct v4l2_subdev *sd, s32 value)
+int imx_t_vcm_slew(struct v4l2_subdev *sd, s32 value)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->t_vcm_slew)
@@ -1157,7 +1156,7 @@ static int imx_t_vcm_slew(struct v4l2_subdev *sd, s32 value)
 	return 0;
 }
 
-static int imx_t_vcm_timing(struct v4l2_subdev *sd, s32 value)
+int imx_t_vcm_timing(struct v4l2_subdev *sd, s32 value)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
 	if (dev->vcm_driver && dev->vcm_driver->t_vcm_timing)
@@ -2107,7 +2106,8 @@ imx_s_parm(struct v4l2_subdev *sd, struct v4l2_streamparm *param)
 	return 0;
 }
 
-static int imx_g_frame_interval(struct v4l2_subdev *sd,
+int
+imx_g_frame_interval(struct v4l2_subdev *sd,
 				struct v4l2_subdev_frame_interval *interval)
 {
 	struct imx_device *dev = to_imx_sensor(sd);
