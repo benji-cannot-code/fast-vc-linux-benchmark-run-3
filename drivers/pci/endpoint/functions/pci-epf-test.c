5786a0761e14ad36a2b5e75fca2cc926deeeadb0
Shawn Lin <shawn.lin@rock-chips.com>
2017-08-18 20:28:01 +0530

    PCI: endpoint: Remove the ->remove() callback
    epf_test is allocated using devm_kzalloc(). Hence it's not required to
    explicitly free it in remove() callback. Since ->remove() callback doesn't
    do anything other than freeing epf_test, remove the ->remove() callback.
    
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/endpoint/functions/pci-epf-test.c
+++ drivers/pci/endpoint/functions/pci-epf-test.c
@@ -497,14 +497,6 @@ static int pci_epf_test_probe(struct pci_epf *epf)
 	return 0;
 }
 
-static int pci_epf_test_remove(struct pci_epf *epf)
-{
-	struct pci_epf_test *epf_test = epf_get_drvdata(epf);
-
-	kfree(epf_test);
-	return 0;
-}
-
 static struct pci_epf_ops ops = {
 	.unbind	= pci_epf_test_unbind,
 	.bind	= pci_epf_test_bind,
@@ -514,7 +506,6 @@ static struct pci_epf_ops ops = {
 static struct pci_epf_driver test_driver = {
 	.driver.name	= "pci_epf_test",
 	.probe		= pci_epf_test_probe,
-	.remove		= pci_epf_test_remove,
 	.id_table	= pci_epf_test_ids,
 	.ops		= &ops,
 	.owner		= THIS_MODULE,
