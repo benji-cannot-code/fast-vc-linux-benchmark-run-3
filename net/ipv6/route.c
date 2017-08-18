ef56ee3e24277c7aa8665805c3c4e0afcce36ad1
Arnd Bergmann <arnd@arndb.de>
2017-08-18 13:34:22 +0200

    ipv6: fix false-postive maybe-uninitialized warning
    Adding a lock around one of the assignments prevents gcc from
    tracking the state of the local 'fibmatch' variable, so it can no
    longer prove that 'dst' is always initialized, leading to a bogus
    warning:
    
    net/ipv6/route.c: In function 'inet6_rtm_getroute':
    net/ipv6/route.c:3659:2: error: 'dst' may be used uninitialized in this function [-Werror=maybe-uninitialized]
    
    This moves the other assignment into the same lock to shut up the
    warning.
    
    Fixes: 121622dba8da ("ipv6: route: make rtm_getroute not assume rtnl is locked")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -3625,6 +3625,8 @@ static int inet6_rtm_getroute(struct sk_buff *in_skb, struct nlmsghdr *nlh,
 
 		if (!fibmatch)
 			dst = ip6_route_input_lookup(net, dev, &fl6, flags);
+		else
+			dst = ip6_route_lookup(net, &fl6, 0);
 
 		rcu_read_unlock();
 	} else {
@@ -3632,10 +3634,10 @@ static int inet6_rtm_getroute(struct sk_buff *in_skb, struct nlmsghdr *nlh,
 
 		if (!fibmatch)
 			dst = ip6_route_output(net, NULL, &fl6);
+		else
+			dst = ip6_route_lookup(net, &fl6, 0);
 	}
 
-	if (fibmatch)
-		dst = ip6_route_lookup(net, &fl6, 0);
 
 	rt = container_of(dst, struct rt6_info, dst);
 	if (rt->dst.error) {
