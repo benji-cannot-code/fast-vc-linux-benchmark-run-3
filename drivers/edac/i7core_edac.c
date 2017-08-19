7b522f62e6dd0a04ecb4e440cb204d1ae763d040
Bhumika Goyal <bhumirks@gmail.com>
2017-08-19 13:52:12 +0530

    EDAC: Make device_type const
    Make these const as they are only stored in the type field of a device
    structure, which is const.
    
    Done using Coccinelle.
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Link: http://lkml.kernel.org/r/1503130946-2854-2-git-send-email-bhumirks@gmail.com
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/i7core_edac.c
+++ drivers/edac/i7core_edac.c
@@ -1095,7 +1095,7 @@ static void addrmatch_release(struct device *device)
 	kfree(device);
 }
 
-static struct device_type addrmatch_type = {
+static const struct device_type addrmatch_type = {
 	.groups		= addrmatch_groups,
 	.release	= addrmatch_release,
 };
@@ -1126,7 +1126,7 @@ static void all_channel_counts_release(struct device *device)
 	kfree(device);
 }
 
-static struct device_type all_channel_counts_type = {
+static const struct device_type all_channel_counts_type = {
 	.groups		= all_channel_counts_groups,
 	.release	= all_channel_counts_release,
 };
