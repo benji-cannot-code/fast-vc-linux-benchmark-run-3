ab79b734ea47f3848ad74f1203833ad21693d720
Daniel Scheller <d.scheller@gmx.net>
2017-08-20 06:41:10 -0400

    media: ddbridge: move ddb_unmap(), cleanup modparams
    adapter_alloc is only used from within ddbridge-core, so move it there,
    this removes the need for prototyping/referencing the variable. While at
    it, msi isn't needed outside of ddbridge-main, so don't extref that one
    aswell.
    
    Signed-off-by: Daniel Scheller <d.scheller@gmx.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/ddbridge/ddbridge-main.c
+++ drivers/media/pci/ddbridge/ddbridge-main.c
@@ -42,16 +42,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /****************************************************************************/
 /* module parameters */
 
-int adapter_alloc;
-module_param(adapter_alloc, int, 0444);
-MODULE_PARM_DESC(adapter_alloc,
-		 "0-one adapter per io, 1-one per tab with io, 2-one per tab, 3-one for all");
-
 #ifdef CONFIG_PCI_MSI
 #ifdef CONFIG_DVB_DDBRIDGE_MSIENABLE
-int msi = 1;
+static int msi = 1;
 #else
-int msi;
+static int msi;
 #endif
 module_param(msi, int, 0444);
 #ifdef CONFIG_DVB_DDBRIDGE_MSIENABLE
@@ -90,19 +85,8 @@ module_param(stv0910_single, int, 0444);
 MODULE_PARM_DESC(stv0910_single, "use stv0910 cards as single demods");
 
 /****************************************************************************/
-
-struct workqueue_struct *ddb_wq;
-
 /****************************************************************************/
 /****************************************************************************/
-/****************************************************************************/
-
-static void ddb_unmap(struct ddb *dev)
-{
-	if (dev->regs)
-		iounmap(dev->regs);
-	vfree(dev);
-}
 
 static void ddb_irq_disable(struct ddb *dev)
 {
