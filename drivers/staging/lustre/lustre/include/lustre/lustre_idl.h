56b1442d7b5eddc5d2f76d5d2894b4a3af7871a4
James Simmons <jsimmons@infradead.org>
2017-08-19 22:26:34 -0400

    staging: lustre: uapi: remove libcfs.h from lustre_id.h/lustre_user.h
    These are the last UAPI headers that contain libcfs.h which is
    internal kernel header. Since it is not available to user land
    remove libcfs.h and add the need headers that libcfs.h provided.
    
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6245
    Reviewed-on: https://review.whamcloud.com/22138
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/include/lustre/lustre_idl.h
+++ drivers/staging/lustre/lustre/include/lustre/lustre_idl.h
@@ -70,7 +70,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LUSTRE_IDL_H_
 #define _LUSTRE_IDL_H_
 
-#include "../../../include/linux/libcfs/libcfs.h"
+#include <linux/types.h>
+
 #include "../../../include/uapi/linux/lnet/lnet-types.h"
 /* Defn's shared with user-space. */
 #include "lustre_user.h"
