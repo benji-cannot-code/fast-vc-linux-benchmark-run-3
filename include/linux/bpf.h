3c27a00f6f816ebb4fe444331be5bfdd160c1cd4
David S. Miller <davem@davemloft.net>
2017-08-19 23:34:03 -0700

    bpf: linux/bpf.h needs linux/numa.h
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/percpu.h>
 #include <linux/err.h>
 #include <linux/rbtree_latch.h>
+#include <linux/numa.h>
 
 struct perf_event;
 struct bpf_prog;
