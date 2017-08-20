662fdcb15a7ff6214395c5714fff1fb027cbced2
James Simmons <jsimmons@infradead.org>
2017-08-19 22:26:56 -0400

    staging: lustre: libcfs: add include path to Makefile
    Rationalize include paths in the libcfs source code files.
    
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/libcfs/tracefile.c
+++ drivers/staging/lustre/lnet/libcfs/tracefile.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) "Lustre: " fmt
 #include "tracefile.h"
 
-#include "../../include/linux/libcfs/libcfs.h"
+#include <linux/libcfs/libcfs.h>
 
 /* XXX move things up to the top, comment */
 union cfs_trace_data_union (*cfs_trace_data[TCD_MAX_TYPES])[NR_CPUS] __cacheline_aligned;
