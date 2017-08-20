77b5f0f314d01bb0f083c807168f21a57f732c2e
Ben Evans <bevans@cray.com>
2017-08-19 22:25:59 -0400

    staging: lustre: uapi: move lu_fid, ost_id funcs out of lustre_idl.h
    Move lu_fid functions into lustre/lustre_fid.h
    Move ost_id functions into lustre/lustre_ostid.h
    Fix indenting, include new headers as needed.
    
    Several inline functions in the header lustre_ostid.h
    are using debug macros instead of returning proper errors.
    Remove the debug macros and properly handle the returned
    error codes. Place both UAPI headers lustre_fid.h and
    lustre_ostid.h into the uapi directory.
    
    Signed-off-by: Ben Evans <bevans@cray.com>
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-6401
    Reviewed-on: https://review.whamcloud.com/22712
    Reviewed-on: https://review.whamcloud.com/24569
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Frank Zago <fzago@cray.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/ldlm/ldlm_request.c
+++ drivers/staging/lustre/lustre/ldlm/ldlm_request.c
@@ -58,6 +58,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DEBUG_SUBSYSTEM S_LDLM
 
+#include "../include/lustre/lustre_errno.h"
+
 #include "../include/lustre_dlm.h"
 #include "../include/obd_class.h"
 #include "../include/obd.h"
