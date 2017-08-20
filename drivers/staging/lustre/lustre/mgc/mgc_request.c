2e82e9318d1e8e893e02a173b4dfd3c942dda142
James Simmons <jsimmons@infradead.org>
2017-08-19 22:26:46 -0400

    staging: lustre: mgc: add include path to Makefile
    Rationalize include paths in the mgc source code files.
    
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/mgc/mgc_request.c
+++ drivers/staging/lustre/lustre/mgc/mgc_request.c
@@ -40,12 +40,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 
-#include "../include/lprocfs_status.h"
-#include "../include/lustre_dlm.h"
-#include "../include/lustre_disk.h"
-#include "../include/lustre_log.h"
-#include "../include/lustre_swab.h"
-#include "../include/obd_class.h"
+#include <lprocfs_status.h>
+#include <lustre_dlm.h>
+#include <lustre_disk.h>
+#include <lustre_log.h>
+#include <lustre_swab.h>
+#include <obd_class.h>
 
 #include "mgc_internal.h"
 
