b0f307efa2fdf53041baad7725a624017a1cac80
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2017-08-21 18:00:38 +0200

    rtc: sa1100: fix unbalanced clk_prepare_enable/clk_disable_unprepare
    In the error path of sa1100_rtc_open(), info->clk is disabled which will
    happen again in sa1100_rtc_remove() when the module is removed whereas it
    is only enabled once in sa1100_rtc_init().
    
    Fixes: 0cc0c38e9139 ("drivers/rtc/rtc-sa1100.c: move clock enable/disable to probe/remove")
    Acked-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>

--- drivers/rtc/rtc-sa1100.c
+++ drivers/rtc/rtc-sa1100.c
@@ -105,7 +105,7 @@ static int sa1100_rtc_open(struct device *dev)
 	ret = request_irq(info->irq_1hz, sa1100_rtc_interrupt, 0, "rtc 1Hz", dev);
 	if (ret) {
 		dev_err(dev, "IRQ %d already in use.\n", info->irq_1hz);
-		goto fail_ui;
+		return ret;
 	}
 	ret = request_irq(info->irq_alarm, sa1100_rtc_interrupt, 0, "rtc Alrm", dev);
 	if (ret) {
@@ -119,8 +119,6 @@ static int sa1100_rtc_open(struct device *dev)
 
  fail_ai:
 	free_irq(info->irq_1hz, dev);
- fail_ui:
-	clk_disable_unprepare(info->clk);
 	return ret;
 }
 
