d4934cad7aee8c786a5b4b2187cb4167e34af34a
Marek Szyprowski <m.szyprowski@samsung.com>
2017-08-21 10:05:00 +0200

    clk: samsung: Add support for runtime PM
    This patch adds struct device pointer to samsung_clk_provider and forwarding it
    to clk_register_* functions, so drivers can register clocks, which use runtime
    pm feature.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Chanwoo Choi <cw00.choi@samsung.com>
    Tested-by: Chanwoo Choi <cw00.choi@samsung.com>
    Reviewed-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: Michael Turquette <mturquette@baylibre.com>
    Link: lkml.kernel.org/r/1503302703-13801-3-git-send-email-m.szyprowski@samsung.com

--- drivers/clk/samsung/clk.h
+++ drivers/clk/samsung/clk.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct samsung_clk_provider {
 	void __iomem *reg_base;
+	struct device *dev;
 	spinlock_t lock;
 	/* clk_data must be the last entry due to variable lenght 'hws' array */
 	struct clk_hw_onecell_data clk_data;
