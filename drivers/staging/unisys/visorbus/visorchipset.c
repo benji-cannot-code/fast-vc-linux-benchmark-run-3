5c358eea35464d6c1ffe9c8aee3037c702f0749b
David Kershner <david.kershner@unisys.com>
2017-08-22 13:27:15 -0400

    staging: unisys: visorbus: Remove unnecessary includes for visorchipset.c
    Visorchipset.c was including unnecessary include files, either included by
    other headers or not needed at all.
    
    Reviewed-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorchipset.c
+++ drivers/staging/unisys/visorbus/visorchipset.c
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/acpi.h>
-#include <linux/mm.h>
-#include <linux/uuid.h>
 #include <linux/crash_dump.h>
 
 #include "visorbus.h"
