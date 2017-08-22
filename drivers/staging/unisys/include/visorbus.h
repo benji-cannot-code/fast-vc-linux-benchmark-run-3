45359aac879b15a202736ed6742dbab1f474b0c7
David Kershner <david.kershner@unisys.com>
2017-08-22 13:27:12 -0400

    staging: unisys: include: Remove unnecessary forward declaration
    The struct visor_device had a forward declaration that was not needed.
    Remove it.
    
    Reviewed-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/visorbus.h
+++ drivers/staging/unisys/include/visorbus.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "channel.h"
 
-struct visor_driver;
 struct visor_device;
 extern struct bus_type visorbus_type;
 
