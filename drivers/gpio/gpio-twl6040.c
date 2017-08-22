0099c4028a822dd50c1e7c941fe8b2f317182c4d
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-08-23 02:33:55 +0900

    gpio: twl6040: remove unneeded forward declaration
    There is no reference to twl6040gpo_chip before its definition.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-twl6040.c
+++ drivers/gpio/gpio-twl6040.c
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/mfd/twl6040.h>
 
-static struct gpio_chip twl6040gpo_chip;
-
 static int twl6040gpo_get(struct gpio_chip *chip, unsigned offset)
 {
 	struct twl6040 *twl6040 = dev_get_drvdata(chip->parent->parent);
