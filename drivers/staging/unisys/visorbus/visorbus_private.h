e258a733e61d98cd7b0510ba06f411ebec5e6485
Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
2017-08-22 13:27:35 -0400

    staging: unisys: visorbus: Get rid of passthrough function visorchipset_device_destroy
    The function visorchipset_device_destroy just called remove_visor_device,
    we can just combine it with remove_visor_device.
    
    Signed-off-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorbus_private.h
+++ drivers/staging/unisys/visorbus/visorbus_private.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int visorbus_create_instance(struct visor_device *dev);
 void visorbus_remove_instance(struct visor_device *bus_info);
 int create_visor_device(struct visor_device *dev_info);
-void visorchipset_device_destroy(struct visor_device *dev_info);
+void remove_visor_device(struct visor_device *dev_info);
 int visorchipset_device_pause(struct visor_device *dev_info);
 int visorchipset_device_resume(struct visor_device *dev_info);
 
