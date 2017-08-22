cf6efe9fde9283bf28ccf0f89918aea3e2a3ac82
Colin Ian King <colin.king@canonical.com>
2017-08-22 13:51:08 +0100

    staging: r8822be: fix a couple of spelling mistakes
    Trivial fixes to spelling mistake in pr_err error messages:
    
    errorr -> error
    mismactch -> mismatch
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtlwifi/halmac/halmac_88xx/halmac_func_88xx.c
+++ drivers/staging/rtlwifi/halmac/halmac_88xx/halmac_func_88xx.c
@@ -2478,7 +2478,7 @@ halmac_parse_psd_data_88xx(struct halmac_adapter *halmac_adapter, u8 *c2h_buf,
 			"[TRACE]Seq num : h2c -> %d c2h -> %d\n",
 			psd_set->seq_num, h2c_seq);
 	if (h2c_seq != psd_set->seq_num) {
-		pr_err("[ERR]Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("[ERR]Seq num mismatch : h2c -> %d c2h -> %d\n",
 		       psd_set->seq_num, h2c_seq);
 		return HALMAC_RET_SUCCESS;
 	}
@@ -2532,7 +2532,7 @@ halmac_parse_efuse_data_88xx(struct halmac_adapter *halmac_adapter, u8 *c2h_buf,
 			halmac_adapter->halmac_state.efuse_state_set.seq_num,
 			h2c_seq);
 	if (h2c_seq != halmac_adapter->halmac_state.efuse_state_set.seq_num) {
-		pr_err("[ERR]Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("[ERR]Seq num mismatch : h2c -> %d c2h -> %d\n",
 		       halmac_adapter->halmac_state.efuse_state_set.seq_num,
 		       h2c_seq);
 		return HALMAC_RET_SUCCESS;
@@ -2729,7 +2729,7 @@ halmac_parse_h2c_ack_phy_efuse_88xx(struct halmac_adapter *halmac_adapter,
 			halmac_adapter->halmac_state.efuse_state_set.seq_num,
 			h2c_seq);
 	if (h2c_seq != halmac_adapter->halmac_state.efuse_state_set.seq_num) {
-		pr_err("[ERR]Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("[ERR]Seq num mismatch : h2c -> %d c2h -> %d\n",
 		       halmac_adapter->halmac_state.efuse_state_set.seq_num,
 		       h2c_seq);
 		return HALMAC_RET_SUCCESS;
@@ -2766,7 +2766,7 @@ halmac_parse_h2c_ack_cfg_para_88xx(struct halmac_adapter *halmac_adapter,
 			h2c_seq);
 	if (h2c_seq !=
 	    halmac_adapter->halmac_state.cfg_para_state_set.seq_num) {
-		pr_err("Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("Seq num mismatch : h2c -> %d c2h -> %d\n",
 		       halmac_adapter->halmac_state.cfg_para_state_set.seq_num,
 		       h2c_seq);
 		return HALMAC_RET_SUCCESS;
@@ -2834,7 +2834,7 @@ halmac_parse_h2c_ack_update_packet_88xx(struct halmac_adapter *halmac_adapter,
 			halmac_adapter->halmac_state.update_packet_set.seq_num,
 			h2c_seq);
 	if (h2c_seq != halmac_adapter->halmac_state.update_packet_set.seq_num) {
-		pr_err("[ERR]Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("[ERR]Seq num mismatch : h2c -> %d c2h -> %d\n",
 		       halmac_adapter->halmac_state.update_packet_set.seq_num,
 		       h2c_seq);
 		return HALMAC_RET_SUCCESS;
@@ -2917,7 +2917,7 @@ halmac_parse_h2c_ack_channel_switch_88xx(struct halmac_adapter *halmac_adapter,
 			halmac_adapter->halmac_state.scan_state_set.seq_num,
 			h2c_seq);
 	if (h2c_seq != halmac_adapter->halmac_state.scan_state_set.seq_num) {
-		pr_err("[ERR]Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("[ERR]Seq num misactch : h2c -> %d c2h -> %d\n",
 		       halmac_adapter->halmac_state.scan_state_set.seq_num,
 		       h2c_seq);
 		return HALMAC_RET_SUCCESS;
@@ -2969,7 +2969,7 @@ halmac_parse_h2c_ack_iqk_88xx(struct halmac_adapter *halmac_adapter,
 			"[TRACE]Seq num : h2c -> %d c2h -> %d\n",
 			halmac_adapter->halmac_state.iqk_set.seq_num, h2c_seq);
 	if (h2c_seq != halmac_adapter->halmac_state.iqk_set.seq_num) {
-		pr_err("[ERR]Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("[ERR]Seq num misactch : h2c -> %d c2h -> %d\n",
 		       halmac_adapter->halmac_state.iqk_set.seq_num, h2c_seq);
 		return HALMAC_RET_SUCCESS;
 	}
@@ -3019,7 +3019,7 @@ halmac_parse_h2c_ack_power_tracking_88xx(struct halmac_adapter *halmac_adapter,
 			h2c_seq);
 	if (h2c_seq !=
 	    halmac_adapter->halmac_state.power_tracking_set.seq_num) {
-		pr_err("[ERR]Seq num mismactch : h2c -> %d c2h -> %d\n",
+		pr_err("[ERR]Seq num mismatch : h2c -> %d c2h -> %d\n",
 		       halmac_adapter->halmac_state.power_tracking_set.seq_num,
 		       h2c_seq);
 		return HALMAC_RET_SUCCESS;
