234f1098c26bf056c9be170baa4ae3ec18dbde59
David Kershner <david.kershner@unisys.com>
2017-08-22 13:27:13 -0400

    staging: unisys: include: Remove unneeded includes from visorbus.h
    A bunch of code churn has caused the following include files to not be
    needed by visorbus.h:
    
    linux/poll.h
    linux/kernel.h
    linux/uuid.h
    linux/seq_file.h
    
    Reviewed-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/visorbus.h
+++ drivers/staging/unisys/include/visorbus.h
@@ -32,10 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/device.h>
 #include <linux/module.h>
-#include <linux/poll.h>
-#include <linux/kernel.h>
-#include <linux/uuid.h>
-#include <linux/seq_file.h>
 #include <linux/slab.h>
 
 #include "channel.h"
