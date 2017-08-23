a89b6f0109f2f6b1ec6afa55a0ba2787b43115e8
Joerg Roedel <jroedel@suse.de>
2017-08-23 16:28:09 +0200

    iommu/pamu: Fix PAMU boot crash
    Commit 68a17f0be6fe introduced an initialization order
    problem, where devices are linked against an iommu which is
    not yet initialized. Fix it by initializing the iommu-device
    before the iommu-ops are registered against the bus.
    
    Reported-by: Michael Ellerman <mpe@ellerman.id.au>
    Fixes: 68a17f0be6fe ('iommu/pamu: Add support for generic iommu-device')
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/fsl_pamu_domain.h
+++ drivers/iommu/fsl_pamu_domain.h
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "fsl_pamu.h"
 
-const struct iommu_ops fsl_pamu_ops;
-
 struct dma_window {
 	phys_addr_t paddr;
 	u64 size;
