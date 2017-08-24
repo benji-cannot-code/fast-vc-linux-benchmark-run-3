2323b8425322e6c765d099c0441dca88fac38fd8
Sricharan R <sricharan@codeaurora.org>
2017-08-24 12:51:32 +0530

    rpmsg: glink: Add support for transport version negotiation
    G-link supports a version number and feature flags for each transport.
    A combination of the version number and feature flags enable/disable:
    
     (*) G-Link software updates for each edge
     (*) Individual features for each edge
    
    Endpoints negotiate both the version and the supported flags when
    the transport is opened and they cannot be changed after negotiation has
    been completed.
    
    Each full implementation of G-Link must support a minimum of the current
    version, the previous version, and the base negotiation version called v0.
    
    Acked-by: Arun Kumar Neelakantam <aneela@codeaurora.org>
    Signed-off-by: Sricharan R <sricharan@codeaurora.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/rpmsg/qcom_glink_native.h
+++ drivers/rpmsg/qcom_glink_native.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __QCOM_GLINK_NATIVE_H__
 #define __QCOM_GLINK_NATIVE_H__
 
+#define GLINK_FEATURE_INTENT_REUSE	BIT(0)
+#define GLINK_FEATURE_MIGRATION		BIT(1)
+#define GLINK_FEATURE_TRACER_PKT	BIT(2)
+
 struct qcom_glink_pipe {
 	size_t length;
 
@@ -32,6 +36,7 @@ struct qcom_glink_pipe {
 struct qcom_glink;
 
 struct qcom_glink *qcom_glink_native_probe(struct device *dev,
+					   unsigned long features,
 					   struct qcom_glink_pipe *rx,
 					   struct qcom_glink_pipe *tx);
 void qcom_glink_native_remove(struct qcom_glink *glink);
