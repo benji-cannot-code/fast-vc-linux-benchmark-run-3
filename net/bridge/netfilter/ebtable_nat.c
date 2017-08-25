59220b98a0c814766c4d60f67350a1a741657e81
Bhumika Goyal <bhumirks@gmail.com>
2017-08-25 19:51:42 +0530

    bridge: make ebt_table const
    Make this const as it is only passed to a const argument of the function
    ebt_register_table.
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/netfilter/ebtable_nat.c
+++ net/bridge/netfilter/ebtable_nat.c
@@ -49,7 +49,7 @@ static int check(const struct ebt_table_info *info, unsigned int valid_hooks)
 	return 0;
 }
 
-static struct ebt_table frame_nat = {
+static const struct ebt_table frame_nat = {
 	.name		= "nat",
 	.table		= &initial_table,
 	.valid_hooks	= NAT_VALID_HOOKS,
