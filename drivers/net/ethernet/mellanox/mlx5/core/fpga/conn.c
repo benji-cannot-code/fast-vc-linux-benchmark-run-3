f81e12b160895961ec961b37260b4726c682d066
Ilya Lesokhin <ilyal@mellanox.com>
2017-08-28 09:47:32 +0300

    net/mlx5: FPGA, Call DMA unmap with the right size
    When mlx5_fpga_conn_unmap_buf is called buf->sg[0].size
    should equal the actual buffer size, not the message size.
    Otherwise we will trigger the following dma debug warning
    "DMA-API: device driver frees DMA memory with different size"
    
    Fixes: 537a50574175 ('net/mlx5: FPGA, Add high-speed connection routines')
    Signed-off-by: Ilya Lesokhin <ilyal@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/fpga/conn.c
+++ drivers/net/ethernet/mellanox/mlx5/core/fpga/conn.c
@@ -257,8 +257,6 @@ static void mlx5_fpga_conn_rq_cqe(struct mlx5_fpga_conn *conn,
 	ix = be16_to_cpu(cqe->wqe_counter) & (conn->qp.rq.size - 1);
 	buf = conn->qp.rq.bufs[ix];
 	conn->qp.rq.bufs[ix] = NULL;
-	if (!status)
-		buf->sg[0].size = be32_to_cpu(cqe->byte_cnt);
 	conn->qp.rq.cc++;
 
 	if (unlikely(status && (status != MLX5_CQE_SYNDROME_WR_FLUSH_ERR)))
@@ -276,6 +274,7 @@ static void mlx5_fpga_conn_rq_cqe(struct mlx5_fpga_conn *conn,
 		return;
 	}
 
+	buf->sg[0].size = be32_to_cpu(cqe->byte_cnt);
 	mlx5_fpga_dbg(conn->fdev, "Message with %u bytes received successfully\n",
 		      buf->sg[0].size);
 	conn->recv_cb(conn->cb_arg, buf);
