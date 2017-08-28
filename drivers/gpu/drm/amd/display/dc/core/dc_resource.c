f08ee4b751a8db6d7408c9931bb91f12c8c2d575
Harry Wentland <harry.wentland@amd.com>
2017-08-28 10:59:39 -0400

    drm/amd/display: Don't spam log with failed validation
    In a lot of case we fail validation of timings for legitimate
    reasons. No need to print an error.
    
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Andrey Grodzovsky <Andrey.Grodzovsky@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <Harry.Wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_resource.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_resource.c
@@ -2712,7 +2712,6 @@ void resource_build_bit_depth_reduction_params(struct dc_stream_state *stream,
 bool dc_validate_stream(struct dc *dc, struct dc_stream_state *stream)
 {
 	struct dc  *core_dc = dc;
-	struct dc_context *dc_ctx = core_dc->ctx;
 	struct dc_link *link = stream->sink->link;
 	struct timing_generator *tg = core_dc->res_pool->timing_generators[0];
 	enum dc_status res = DC_OK;
@@ -2733,10 +2732,6 @@ bool dc_validate_stream(struct dc *dc, struct dc_stream_state *stream)
 		res = dc_link_validate_mode_timing(stream,
 		      link,
 		      &stream->timing);
-
-	if (res != DC_OK)
-		DC_ERROR("Failed validation for stream %p, err:%d, !\n",
-				stream, res);
 
 	return res == DC_OK;
 }
