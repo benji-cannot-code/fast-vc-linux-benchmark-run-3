b1e0773109bfb679e1fc1ce43097927792e69f24
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2017-08-29 14:47:25 +0200

    mcb: Fix an error handling path in 'chameleon_parse_cells()'
    If 'chameleon_get_bar()' fails, we will return 0, which mean success.
    We should return the corresponding error code instead.
    
    Remove the useless initialisation of 'ret' which was hiding the issue.
    (if 'ret' is not set, gcc generates a warning ("warning: ‘ret’ may be used
    uninitialized in this function"))
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mcb/mcb-parse.c
+++ drivers/mcb/mcb-parse.c
@@ -183,7 +183,7 @@ int chameleon_parse_cells(struct mcb_bus *bus, phys_addr_t mapbase,
 	int num_cells = 0;
 	uint32_t dtype;
 	int bar_count;
-	int ret = 0;
+	int ret;
 	u32 hsize;
 
 	hsize = sizeof(struct chameleon_fpga_header);
@@ -211,8 +211,10 @@ int chameleon_parse_cells(struct mcb_bus *bus, phys_addr_t mapbase,
 		 header->filename);
 
 	bar_count = chameleon_get_bar(&p, mapbase, &cb);
-	if (bar_count < 0)
+	if (bar_count < 0) {
+		ret = bar_count;
 		goto free_header;
+	}
 
 	for_each_chameleon_cell(dtype, p) {
 		switch (dtype) {
