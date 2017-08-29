9f172df36d4c6ba24f33431a8c5fc23c72118c06
David Howells <dhowells@redhat.com>
2017-08-29 10:18:43 +0100

    rxrpc: Don't negate call->error before returning it
    call->error is stored as 0 or a negative error code.  Don't negate this
    value (ie. make it positive) before returning it from a kernel function
    (though it should still be negated before passing to userspace through a
    control message).
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- net/rxrpc/sendmsg.c
+++ net/rxrpc/sendmsg.c
@@ -62,7 +62,7 @@ static int rxrpc_wait_for_tx_window(struct rxrpc_sock *rx,
 			  call->cong_cwnd + call->cong_extra))
 			break;
 		if (call->state >= RXRPC_CALL_COMPLETE) {
-			ret = -call->error;
+			ret = call->error;
 			break;
 		}
 		if (signal_pending(current)) {
@@ -365,8 +365,8 @@ out:
 
 call_terminated:
 	rxrpc_free_skb(skb, rxrpc_skb_tx_freed);
-	_leave(" = %d", -call->error);
-	return -call->error;
+	_leave(" = %d", call->error);
+	return call->error;
 
 maybe_error:
 	if (copied)
@@ -661,7 +661,7 @@ int rxrpc_kernel_send_data(struct socket *sock, struct rxrpc_call *call,
 		break;
 	case RXRPC_CALL_COMPLETE:
 		read_lock_bh(&call->state_lock);
-		ret = -call->error;
+		ret = call->error;
 		read_unlock_bh(&call->state_lock);
 		break;
 	default:
