a107735fd60cf2fd89fe405b9fd68be94ccc9f2f
Milosz Tanski <milosz@adfin.com>
2017-08-29 16:13:19 +0200

    fs: support IOCB_NOWAIT in generic_file_buffered_read
    Allow generic_file_buffered_read to bail out early instead of waiting for
    the page lock or reading a page if IOCB_NOWAIT is specified.
    
    Signed-off-by: Milosz Tanski <milosz@adfin.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jeff Moyer <jmoyer@redhat.com>
    Acked-by: Sage Weil <sage@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1940,6 +1940,8 @@ find_page:
 
 		page = find_get_page(mapping, index);
 		if (!page) {
+			if (iocb->ki_flags & IOCB_NOWAIT)
+				goto would_block;
 			page_cache_sync_readahead(mapping,
 					ra, filp,
 					index, last_index - index);
@@ -1953,6 +1955,11 @@ find_page:
 					index, last_index - index);
 		}
 		if (!PageUptodate(page)) {
+			if (iocb->ki_flags & IOCB_NOWAIT) {
+				put_page(page);
+				goto would_block;
+			}
+
 			/*
 			 * See comment in do_read_cache_page on why
 			 * wait_on_page_locked is used to avoid unnecessarily
@@ -2134,6 +2141,8 @@ no_cached_page:
 		goto readpage;
 	}
 
+would_block:
+	error = -EAGAIN;
 out:
 	ra->prev_pos = prev_index;
 	ra->prev_pos <<= PAGE_SHIFT;
