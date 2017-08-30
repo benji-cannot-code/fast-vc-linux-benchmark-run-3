aaf9765ebad5784b144c3896d116b1a81ef6d360
Colin Ian King <colin.king@canonical.com>
2017-08-30 18:15:25 +0100

    bpf: test_maps: fix typos, "conenct" and "listeen"
    Trivial fix to typos in printf error messages:
    "conenct" -> "connect"
    "listeen" -> "listen"
    
    thanks to Daniel Borkmann for spotting one of these mistakes
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Shuah Khan <shuahkh@osg.samsung.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/testing/selftests/bpf/test_maps.c
+++ tools/testing/selftests/bpf/test_maps.c
@@ -505,7 +505,7 @@ static void test_sockmap(int tasks, void *data)
 		}
 		err = listen(sfd[i], 32);
 		if (err < 0) {
-			printf("failed to listeen\n");
+			printf("failed to listen\n");
 			goto out;
 		}
 	}
@@ -526,7 +526,7 @@ static void test_sockmap(int tasks, void *data)
 		addr.sin_port = htons(ports[i - 2]);
 		err = connect(sfd[i], (struct sockaddr *)&addr, sizeof(addr));
 		if (err) {
-			printf("failed to conenct\n");
+			printf("failed to connect\n");
 			goto out;
 		}
 	}
