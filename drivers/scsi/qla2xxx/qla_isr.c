3aa68576b3a3b31b62f308ff90984d34d86b0f72
Johannes Thumshirn <jthumshirn@suse.de>
2017-08-30 15:12:31 +0200

    scsi: qla2xxx: add missing includes for qla_isr
    Since commit 7401bc18d1ee ("scsi: qla2xxx: Add FC-NVMe command
    handling") we make use of 'struct nvmefc_fcp_req' in
    qla24xx_nvme_iocb_entry() without including linux/nvme-fc-driver.h where
    it is defined.
    
    Add linux/nvme-fc-driver.h (and scsi/fc/fc_fs.h as nvme-fc-driver.h
    needs the definition of 'struct fc_ba_rjt' from scsi/fc/fc_fs.h) to the
    header files included by qla_isr.c.
    
    Fixes: 7401bc18d1ee ("scsi: qla2xxx: Add FC-NVMe command handling")
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>
    Acked-by: Himanshu Madhani <himanshu.madhani@cavium.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/scsi_tcq.h>
 #include <scsi/scsi_bsg_fc.h>
 #include <scsi/scsi_eh.h>
+#include <scsi/fc/fc_fs.h>
+#include <linux/nvme-fc-driver.h>
 
 static void qla2x00_mbx_completion(scsi_qla_host_t *, uint16_t);
 static void qla2x00_status_entry(scsi_qla_host_t *, struct rsp_que *, void *);
