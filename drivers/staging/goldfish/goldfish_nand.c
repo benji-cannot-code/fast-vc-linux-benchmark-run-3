2ed1eb3e0b59ba879610ef57fef4529ec9e53a33
Edvard Holst <edvard.holst@gmail.com>
2017-08-30 11:33:22 +0000

    staging: goldfish: (Coding Style) Fixed parenthesis alignment.
    Fixed paranthesis alignment for compliance with checkpatch.
    
    Signed-off-by: Edvard Holst <edvard.holst@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/goldfish/goldfish_nand.c
+++ drivers/staging/goldfish/goldfish_nand.c
@@ -154,7 +154,7 @@ static int goldfish_nand_read_oob(struct mtd_info *mtd, loff_t ofs,
 	ofs += mtd->writesize + ops->ooboffs;
 	if (ops->oobbuf)
 		ops->oobretlen = goldfish_nand_cmd(mtd, NAND_CMD_READ, ofs,
-						ops->ooblen, ops->oobbuf);
+						   ops->ooblen, ops->oobbuf);
 	return 0;
 
 invalid_arg:
@@ -186,7 +186,7 @@ static int goldfish_nand_write_oob(struct mtd_info *mtd, loff_t ofs,
 	ofs += mtd->writesize + ops->ooboffs;
 	if (ops->oobbuf)
 		ops->oobretlen = goldfish_nand_cmd(mtd, NAND_CMD_WRITE, ofs,
-						ops->ooblen, ops->oobbuf);
+						   ops->ooblen, ops->oobbuf);
 	return 0;
 
 invalid_arg:
