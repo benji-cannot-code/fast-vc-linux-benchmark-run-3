2cc4209635d2ed051bbcbb62b7109cac76784de9
Michael Ellerman <mpe@ellerman.id.au>
2017-08-30 17:41:29 +1000

    powerpc/mm/radix: Add pr_fmt() to pgtable-radix.c
    Make the printks look a bit nicer by adding a prefix.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/pgtable-radix.c
+++ arch/powerpc/mm/pgtable-radix.c
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * as published by the Free Software Foundation; either version
  * 2 of the License, or (at your option) any later version.
  */
+
+#define pr_fmt(fmt) "radix-mmu: " fmt
+
+#include <linux/kernel.h>
 #include <linux/sched/mm.h>
 #include <linux/memblock.h>
 #include <linux/of_fdt.h>
