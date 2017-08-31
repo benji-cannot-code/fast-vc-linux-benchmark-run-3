030be77850e2a19c9687222469dd217059090ad6
Derek Robson <robsonde@gmail.com>
2017-08-31 18:37:48 +1200

    Staging: Pi433: style fix - tabs and spaces
    Fixed checkpatch errors, spaces before tabs.
    Found using checkpatch.
    
    Signed-off-by: Derek Robson <robsonde@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/pi433/rf69.h
+++ drivers/staging/pi433/rf69.h
@@ -21,10 +21,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rf69_enum.h"
 #include "rf69_registers.h"
 
-#define F_OSC 		32000000  /* in Hz */
+#define F_OSC		32000000  /* in Hz */
 #define FREQUENCY	433920000 /* in Hz, modifying this value impacts CE certification */
-#define FIFO_SIZE 	66        /* in byte */
-#define FIFO_THRESHOLD	15 	  /* in byte */
+#define FIFO_SIZE	66		/* in byte */
+#define FIFO_THRESHOLD	15		/* in byte */
 
 int rf69_set_mode(struct spi_device *spi, enum mode mode);
 int rf69_set_data_mode(struct spi_device *spi, enum dataMode dataMode);
