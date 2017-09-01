688f9bfd7c40e3c94d3fca963b961a2fa11fd318
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-09-01 09:36:39 -0400

    media: staging: atomisp: Remove dead code for MID (#4)
    Since we switched to upstream IOSF MBI API the custom code
    become not in use anymore.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/staging/media/atomisp/pci/atomisp2/atomisp_internal.h
+++ drivers/staging/media/atomisp/pci/atomisp2/atomisp_internal.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/idr.h>
 
 #include <asm/intel-mid.h>
-#include "../../include/asm/intel_mid_pcihelpers.h"
 
 #include <media/media-device.h>
 #include <media/v4l2-subdev.h>
