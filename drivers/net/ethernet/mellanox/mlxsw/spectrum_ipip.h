b5008602ad1440cc9e2b5d59d90f3612f2d0d85a
Petr Machata <petrm@mellanox.com>
2017-09-02 23:49:29 +0200

    mlxsw: spectrum_router: Support GRE tunnels
    This patch introduces callbacks and tunnel type to offload GRE tunnels.
    
    Signed-off-by: Petr Machata <petrm@mellanox.com>
    Reviewed-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_ipip.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_ipip.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ip_fib.h>
 
 enum mlxsw_sp_ipip_type {
+	MLXSW_SP_IPIP_TYPE_GRE4,
 	MLXSW_SP_IPIP_TYPE_MAX,
 };
 
