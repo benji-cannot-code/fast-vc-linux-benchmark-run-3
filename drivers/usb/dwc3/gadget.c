ea464a4679bec3c9f0bac5f41acfbb1c42d46d6f
Felipe Balbi <felipe.balbi@linux.intel.com>
2017-09-05 14:28:46 +0300

    usb: dwc3: gadget: check for lack of TRBs a bit earlier
    This will let us call __dwc3_gadget_kick_transfer()
    unconditionally. No functional changes, cleanup only.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1152,9 +1152,6 @@ static void dwc3_prepare_trbs(struct dwc3_ep *dep)
 
 	BUILD_BUG_ON_NOT_POWER_OF_2(DWC3_TRB_NUM);
 
-	if (!dwc3_calc_trbs_left(dep))
-		return;
-
 	/*
 	 * We can get in a situation where there's a request in the started list
 	 * but there weren't enough TRBs to fully kick it in the first time
@@ -1202,6 +1199,9 @@ static int __dwc3_gadget_kick_transfer(struct dwc3_ep *dep, u16 cmd_param)
 	int				starting;
 	int				ret;
 	u32				cmd;
+
+	if (!dwc3_calc_trbs_left(dep))
+		return 0;
 
 	starting = !(dep->flags & DWC3_EP_BUSY);
 
