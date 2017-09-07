b465310d156229b5ba3548692506a04c97d457fc
Lucas Stach <l.stach@pengutronix.de>
2017-09-07 17:06:28 +0200

    drm/etnaviv: remove IOMMU dependency
    Using the IOMMU API to manage the internal GPU MMU has been an
    historical accident and it keeps getting in the way, as well as
    entangling the driver with the inner workings of the IOMMU
    subsystem.
    
    Clean this up by removing the usage of iommu_domain, which is the
    last piece linking etnaviv to the IOMMU subsystem.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/gpu/drm/etnaviv/etnaviv_drv.h
+++ drivers/gpu/drm/etnaviv/etnaviv_drv.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm_runtime.h>
 #include <linux/slab.h>
 #include <linux/list.h>
-#include <linux/iommu.h>
 #include <linux/types.h>
 #include <linux/sizes.h>
 
