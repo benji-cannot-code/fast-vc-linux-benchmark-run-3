0a1c3a438f393d4721e2bbc9e149757a09ddfcf0
Dan Carpenter <dan.carpenter@oracle.com>
2017-09-08 16:15:48 -0700

    lib/string.c: check for kmalloc() failure
    This is mostly to keep the number of static checker warnings down so we
    can spot new bugs instead of them being drowned in noise.  This function
    doesn't return normal kernel error codes but instead the return value is
    used to display exactly which memory failed.  I chose -1 as hopefully
    that's a helpful thing to print.
    
    Link: http://lkml.kernel.org/r/20170817115420.uikisjvfmtrqkzjn@mwanda
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Matthew Wilcox <mawilcox@microsoft.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Mauro Carvalho Chehab <mchehab@kernel.org>
    Cc: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Cc: Daniel Micay <danielmicay@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/string.c
+++ lib/string.c
@@ -1060,7 +1060,11 @@ EXPORT_SYMBOL(fortify_panic);
 static __init int memset16_selftest(void)
 {
 	unsigned i, j, k;
-	u16 v, *p = kmalloc(256 * 2 * 2, GFP_KERNEL);
+	u16 v, *p;
+
+	p = kmalloc(256 * 2 * 2, GFP_KERNEL);
+	if (!p)
+		return -1;
 
 	for (i = 0; i < 256; i++) {
 		for (j = 0; j < 256; j++) {
@@ -1092,7 +1096,11 @@ fail:
 static __init int memset32_selftest(void)
 {
 	unsigned i, j, k;
-	u32 v, *p = kmalloc(256 * 2 * 4, GFP_KERNEL);
+	u32 v, *p;
+
+	p = kmalloc(256 * 2 * 4, GFP_KERNEL);
+	if (!p)
+		return -1;
 
 	for (i = 0; i < 256; i++) {
 		for (j = 0; j < 256; j++) {
@@ -1124,7 +1132,11 @@ fail:
 static __init int memset64_selftest(void)
 {
 	unsigned i, j, k;
-	u64 v, *p = kmalloc(256 * 2 * 8, GFP_KERNEL);
+	u64 v, *p;
+
+	p = kmalloc(256 * 2 * 8, GFP_KERNEL);
+	if (!p)
+		return -1;
 
 	for (i = 0; i < 256; i++) {
 		for (j = 0; j < 256; j++) {
