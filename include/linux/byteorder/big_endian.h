ac1272316d20ed0d220c7686d8330dc01178d9fd
Babu Moger <babu.moger@oracle.com>
2017-09-08 16:14:29 -0700

    include: warn for inconsistent endian config definition
    We have seen some generic code use config parameter CONFIG_CPU_BIG_ENDIAN
    to decide the endianness.
    
    Here are the few examples.
    include/asm-generic/qrwlock.h
    drivers/of/base.c
    drivers/of/fdt.c
    drivers/tty/serial/earlycon.c
    drivers/tty/serial/serial_core.c
    
    Display warning if CPU_BIG_ENDIAN is not defined on big endian
    architecture and also warn if it defined on little endian architectures.
    
    Here is our original discussion
    https://lkml.org/lkml/2017/5/24/620
    
    Link: http://lkml.kernel.org/r/1499358861-179979-4-git-send-email-babu.moger@oracle.com
    Signed-off-by: Babu Moger <babu.moger@oracle.com>
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: "James E.J. Bottomley" <jejb@parisc-linux.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Greg KH <gregkh@linuxfoundation.org>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Jonas Bonn <jonas@southpole.se>
    Cc: Max Filippov <jcmvbkbc@gmail.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au> (powerpc)
    Cc: Michal Simek <monstr@monstr.eu>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stafford Horne <shorne@gmail.com>
    Cc: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/byteorder/big_endian.h
+++ include/linux/byteorder/big_endian.h
@@ -4,5 +4,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <uapi/linux/byteorder/big_endian.h>
 
+#ifndef CONFIG_CPU_BIG_ENDIAN
+#warning inconsistent configuration, needs CONFIG_CPU_BIG_ENDIAN
+#endif
+
 #include <linux/byteorder/generic.h>
 #endif /* _LINUX_BYTEORDER_BIG_ENDIAN_H */
