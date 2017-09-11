8adba69fa4a1920fad5495919780ba296cf5c954
Patrick Venture <venture@google.com>
2017-09-11 15:41:55 -0700

    hwmon: (aspeed-pwm-tacho) increase fan tach period
    The previous value reduced the time required to determine
    the fan value, however, it's also used as the final timeout
    mechanism.  The prevous value would work for any fan speed
    greater than around 3k RPM.  This increased value, lets the fan
    speeds return quickly but will wait longer to handle speeds below 3k
    RPM.
    
    Testing: this value was determined through experimentation on the ast2400
    on the Quanta-q71l.  This configurations runs 8 fans attached to the
    controller.
    
    Signed-off-by: Patrick Venture <venture@google.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/aspeed-pwm-tacho.c
+++ drivers/hwmon/aspeed-pwm-tacho.c
@@ -162,7 +162,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 11: reserved.
  */
 #define M_TACH_MODE 0x02 /* 10b */
-#define M_TACH_UNIT 0x00c0
+#define M_TACH_UNIT 0x0210
 #define INIT_FAN_CTRL 0xFF
 
 /* How long we sleep in us while waiting for an RPM result. */
