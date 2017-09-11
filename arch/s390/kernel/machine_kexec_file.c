1b2033b88f2f86f8dcbac7458dc81e9df7d05e0d
Philipp Rudo <prudo@linux.vnet.ibm.com>
2017-09-11 15:15:29 +0200

    s390/kexec_file: Add ELF loader
    Add an ELF loader for kexec_file. The main task here is to do proper sanity
    checks on the ELF file. Basically all other functionality was already
    implemented for the image loader.
    
    Signed-off-by: Philipp Rudo <prudo@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/machine_kexec_file.c
+++ arch/s390/kernel/machine_kexec_file.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/setup.h>
 
 const struct kexec_file_ops * const kexec_file_loaders[] = {
+	&s390_kexec_elf_ops,
 	&s390_kexec_image_ops,
 	NULL,
 };
