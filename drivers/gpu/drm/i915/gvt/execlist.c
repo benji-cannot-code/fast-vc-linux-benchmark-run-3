d434556c91309906507d997de83d37d53ae33258
Zhi Wang <zhi.a.wang@intel.com>
2017-09-13 00:31:29 +0800

    drm/i915/gvt: Introduce vGPU submission ops
    Introduce vGPU submission ops to support easy switching submission mode
    of one vGPU between different OSes.
    
    Signed-off-by: Zhi Wang <zhi.a.wang@intel.com>

--- drivers/gpu/drm/i915/gvt/execlist.c
+++ drivers/gpu/drm/i915/gvt/execlist.c
@@ -549,7 +549,7 @@ static void clean_workloads(struct intel_vgpu *vgpu, unsigned long engine_mask)
 	}
 }
 
-void intel_vgpu_clean_execlist(struct intel_vgpu *vgpu)
+void clean_execlist(struct intel_vgpu *vgpu)
 {
 	enum intel_engine_id i;
 	struct intel_engine_cs *engine;
@@ -565,7 +565,7 @@ void intel_vgpu_clean_execlist(struct intel_vgpu *vgpu)
 	}
 }
 
-int intel_vgpu_init_execlist(struct intel_vgpu *vgpu)
+int init_execlist(struct intel_vgpu *vgpu)
 {
 	enum intel_engine_id i;
 	struct intel_engine_cs *engine;
@@ -587,3 +587,9 @@ void intel_vgpu_reset_execlist(struct intel_vgpu *vgpu,
 	for_each_engine_masked(engine, dev_priv, engine_mask, tmp)
 		init_vgpu_execlist(vgpu, engine->id);
 }
+
+const struct intel_vgpu_submission_ops intel_vgpu_execlist_submission_ops = {
+	.name = "execlist",
+	.init = init_execlist,
+	.clean = clean_execlist,
+};
