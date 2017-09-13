7d26d30873e65f9c62064a598ff3ef24edc46c04
Thomas Gleixner <tglx@linutronix.de>
2017-09-13 23:29:45 +0200

    x86/vector: Untangle internal state from irq_cfg
    The vector management state is not required to live in irq_cfg. irq_cfg is
    only relevant for the depending irq domains (IOAPIC, DMAR, MSI ...).
    
    The seperation of the vector management status allows to direct a shut down
    interrupt to a special shutdown vector w/o confusing the internal state of
    the vector management.
    
    Preparatory change for the rework of managed interrupts and the global
    vector reservation scheme.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Juergen Gross <jgross@suse.com>
    Tested-by: Yu Chen <yu.c.chen@intel.com>
    Acked-by: Juergen Gross <jgross@suse.com>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Alok Kataria <akataria@vmware.com>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: "Rafael J. Wysocki" <rjw@rjwysocki.net>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Rui Zhang <rui.zhang@intel.com>
    Cc: "K. Y. Srinivasan" <kys@microsoft.com>
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Len Brown <lenb@kernel.org>
    Link: https://lkml.kernel.org/r/20170913213155.683712356@linutronix.de

--- arch/x86/include/asm/hw_irq.h
+++ arch/x86/include/asm/hw_irq.h
@@ -125,8 +125,7 @@ struct irq_alloc_info {
 
 struct irq_cfg {
 	unsigned int		dest_apicid;
-	u8			vector;
-	u8			old_vector;
+	unsigned int		vector;
 };
 
 extern struct irq_cfg *irq_cfg(unsigned int irq);
