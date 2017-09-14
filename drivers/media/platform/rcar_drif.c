41c742baeea48312606566e90b553a82f1f4f7fd
Arnd Bergmann <arnd@arndb.de>
2017-09-14 08:07:27 -0300

    [media] rcar_drif: fix potential uninitialized variable use
    Older compilers like gcc-4.6 may run into a case that returns
    an uninitialized variable from rcar_drif_enable_rx() if that
    function was ever called with an empty cur_ch_mask:
    
    drivers/media/platform/rcar_drif.c:658:2: error: ‘ret’ may be used uninitialized in this function [-Werror=uninitialized]
    
    Newer compilers don't have that problem as they optimize the
    'ret' variable away and just return zero in that case.
    
    This changes the function to return -EINVAL for this particular
    failure, to make it consistent across all compiler versions.
    In case gcc gets changed to report a warning for it in the
    future, it's also a good idea to shut it up now.
    
    Link: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=82203
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Ramesh Shanmugasundaram <ramesh.shanmugasundaram@bp.renesas.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/platform/rcar_drif.c
+++ drivers/media/platform/rcar_drif.c
@@ -631,7 +631,7 @@ static int rcar_drif_enable_rx(struct rcar_drif_sdr *sdr)
 {
 	unsigned int i;
 	u32 ctr;
-	int ret;
+	int ret = -EINVAL;
 
 	/*
 	 * When both internal channels are enabled, they can be synchronized
