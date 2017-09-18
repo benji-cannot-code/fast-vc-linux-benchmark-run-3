fb11a98eb37cef43bad84bf58597ec4cc6671961
Claudiu Manoil <claudiu.manoil@nxp.com>
2017-09-18 16:39:46 -0400

    soc/fsl/qbman: Add missing headers on ARM
    Unlike PPC builds, ARM builds need following headers
    explicitly:
    +#include <linux/io.h>		for ioread32be()
    +#include <linux/delay.h>		for udelay()
    
    Signed-off-by: Claudiu Manoil <claudiu.manoil@nxp.com>
    Signed-off-by: Roy Pledge <roy.pledge@nxp.com>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/soc/fsl/qbman/dpaa_sys.h
+++ drivers/soc/fsl/qbman/dpaa_sys.h
@@ -45,6 +45,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/prefetch.h>
 #include <linux/genalloc.h>
 #include <asm/cacheflush.h>
+#include <linux/io.h>
+#include <linux/delay.h>
 
 /* For 2-element tables related to cache-inhibited and cache-enabled mappings */
 #define DPAA_PORTAL_CE 0
