0c12d0c112280696d2fc2c31ea6ec8d0d49b820e
Claudiu Manoil <claudiu.manoil@nxp.com>
2017-09-18 16:39:41 -0400

    soc/fsl/qbman: Drop L1_CACHE_BYTES compile time check
    Not relevant and arch dependent. Overkill for PPC.
    
    Signed-off-by: Claudiu Manoil <claudiu.manoil@nxp.com>
    Signed-off-by: Roy Pledge <roy.pledge@nxp.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/soc/fsl/qbman/dpaa_sys.h
+++ drivers/soc/fsl/qbman/dpaa_sys.h
@@ -50,10 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DPAA_PORTAL_CE 0
 #define DPAA_PORTAL_CI 1
 
-#if (L1_CACHE_BYTES != 32) && (L1_CACHE_BYTES != 64)
-#error "Unsupported Cacheline Size"
-#endif
-
 static inline void dpaa_flush(void *p)
 {
 #ifdef CONFIG_PPC
