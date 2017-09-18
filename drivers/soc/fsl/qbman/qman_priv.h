934c1e2718c8a7be1ac6b3a1f3f0491b656d3267
Roy Pledge <roy.pledge@nxp.com>
2017-09-18 16:39:38 -0400

    soc/fsl/qbman: Use shared-dma-pool for QMan private memory allocations
    Use the shared-memory-pool mechanism for frame queue descriptor and
    packed frame descriptor record area allocations.
    
    Signed-off-by: Roy Pledge <roy.pledge@nxp.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/soc/fsl/qbman/qman_priv.h
+++ drivers/soc/fsl/qbman/qman_priv.h
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
 #include "dpaa_sys.h"
 
 #include <soc/fsl/qman.h>
