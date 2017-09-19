286246f2e1fec08a14109461b6f573eb965287c1
Mustafa Ismail <mustafa.ismail@intel.com>
2017-09-19 09:19:12 -0500

    i40iw: Add missing VLAN priority
    Set the VLAN priority which is in the upper 3 bits of the VLAN
    tag field in the QP context.
    
    Signed-off-by: Mustafa Ismail <mustafa.ismail@intel.com>
    Signed-off-by: Shiraz Saleem <shiraz.saleem@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/i40iw/i40iw_cm.h
+++ drivers/infiniband/hw/i40iw/i40iw_cm.h
@@ -72,6 +72,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	I40IW_HW_IRD_SETTING_32	32
 #define	I40IW_HW_IRD_SETTING_64	64
 
+#define I40IW_VLAN_PRIO_SHIFT   13
+
 enum ietf_mpa_flags {
 	IETF_MPA_FLAGS_MARKERS = 0x80,	/* receive Markers */
 	IETF_MPA_FLAGS_CRC = 0x40,	/* receive Markers */
