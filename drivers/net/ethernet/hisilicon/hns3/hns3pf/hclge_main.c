7666819f0c51ccae3440543991ce2af3e0c2702f
Lipeng <lipeng321@huawei.com>
2017-09-19 17:17:11 +0100

    net: hns3: Fixes the command used to unmap ring from vector
    This patch fixes the IMP command being used to unmap the vector
    from the corresponding ring.
    
    Fixes: 6427264ef330 ("net: hns3: Add HNS3 Acceleration Engine &
    Compatibility Layer Support")
    Signed-off-by: Lipeng <lipeng321@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
@@ -2780,7 +2780,7 @@ static int hclge_unmap_ring_from_vector(
 			}
 			i = 0;
 			hclge_cmd_setup_basic_desc(&desc,
-						   HCLGE_OPC_ADD_RING_TO_VECTOR,
+						   HCLGE_OPC_DEL_RING_TO_VECTOR,
 						   false);
 			req->int_vector_id = vector_id;
 		}
