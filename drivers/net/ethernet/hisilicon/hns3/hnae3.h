5ff14c16fb2f9c0c629dc6c7283f2e1278f69875
Yunsheng Lin <linyunsheng@huawei.com>
2017-09-20 18:52:50 +0800

    net: hns3: Cleanup for ROCE capability flag in ae_dev
    This patch add the ROCE supported flag in the driver_data
    field of pci_device_id, delete roce_pci_tbl and change
    HNAE_DEV_SUPPORT_ROCE_B to HNAE3_DEV_SUPPORT_ROCE_B.
    This cleanup is done in order to support adding capability
    in pci_device_id and to fix initialization failure when
    cmd is not supported.
    
    Signed-off-by: Yunsheng Lin <linyunsheng@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hnae3.h
+++ drivers/net/ethernet/hisilicon/hns3/hnae3.h
@@ -50,7 +50,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HNAE3_CLASS_NAME_SIZE 16
 
 #define HNAE3_DEV_INITED_B			0x0
-#define HNAE_DEV_SUPPORT_ROCE_B			0x1
+#define HNAE3_DEV_SUPPORT_ROCE_B		0x1
+
+#define hnae3_dev_roce_supported(hdev) \
+	hnae_get_bit(hdev->ae_dev->flag, HNAE3_DEV_SUPPORT_ROCE_B)
 
 #define ring_ptr_move_fw(ring, p) \
 	((ring)->p = ((ring)->p + 1) % (ring)->desc_num)
