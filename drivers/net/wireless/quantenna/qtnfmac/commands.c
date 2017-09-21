a5cdf24723bcaf754d79fd3441d37c3535cd7390
Igor Mitsyanko <igor.mitsyanko.os@quantenna.com>
2017-09-21 14:34:36 -0700

    qtnfmac: remove unused mac::status field
    There are no users of this field and it can safely be removed.
    
    Signed-off-by: Igor Mitsyanko <igor.mitsyanko.os@quantenna.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/quantenna/qtnfmac/commands.c
+++ drivers/net/wireless/quantenna/qtnfmac/commands.c
@@ -2338,7 +2338,6 @@ int qtnf_cmd_send_chan_switch(struct qtnf_vif *vif,
 
 	switch (res_code) {
 	case QLINK_CMD_RESULT_OK:
-		mac->status |= QTNF_MAC_CSA_ACTIVE;
 		ret = 0;
 		break;
 	case QLINK_CMD_RESULT_ENOTFOUND:
