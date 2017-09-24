40142f493c3f6ca1c595dd73dfe00789b1b90d94
Christian Gmeiner <christian.gmeiner@gmail.com>
2017-09-24 15:15:21 +0200

    drm/etnaviv: add infrastructure to query perf counter
    Make it possible that userspace can query all performance domains and
    its signals. This information is needed to sample those signals via
    submit ioctl.
    
    At the moment no performance domain is available.
    
    Changes from v1 -> v2:
    - use a 16 bit value for signals
    - fix padding issues
    - add id member to domain and signal struct
    
    Changes v4 -> v5
    - provide for each pipe an own set of pm domains
    
    Signed-off-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

---
+++ drivers/gpu/drm/etnaviv/etnaviv_perfmon.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * Copyright (C) 2017 Etnaviv Project
+ * Copyright (C) 2017 Zodiac Inflight Innovations
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ETNAVIV_PERFMON_H__
+#define __ETNAVIV_PERFMON_H__
+
+struct etnaviv_gpu;
+struct drm_etnaviv_pm_domain;
+struct drm_etnaviv_pm_signal;
+
+int etnaviv_pm_query_dom(struct etnaviv_gpu *gpu,
+	struct drm_etnaviv_pm_domain *domain);
+
+int etnaviv_pm_query_sig(struct etnaviv_gpu *gpu,
+	struct drm_etnaviv_pm_signal *signal);
+
+#endif /* __ETNAVIV_PERFMON_H__ */
