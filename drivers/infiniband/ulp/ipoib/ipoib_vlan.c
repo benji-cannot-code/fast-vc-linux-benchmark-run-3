5d6648764d7ab755ac13829e3d246dcbaf1e7456
Shalom Lagziel <shaloml@mellanox.com>
2017-09-24 21:46:32 +0300

    IB/ipoib: Fix sysfs Pkey create<->remove possible deadlock
    A possible ABBA lock can happen with RTNL and vlan_rwsem.
    For example:
    
    Flow A: Device Flush
    	__ipoib_ib_dev_flush
    	down_read(vlan_rwsem) 			// Lock A
    	ipoib_flush_ah
    	flush_workqueue(priv->wq) 		// Wait for completion
    	A work on shared WQ (Mcast carrier)
    		ipoib_mcast_carrier_on_task
    		while (!rtnl_trylock())         // Wait for lock B
    
    Flow B: Sysfs PKEY delete
    	ipoib_vlan_delete
    	lock(RTNL) 				// Lock B
    	down_write(vlan_rwsem) 			// Wait for lock A
    
    This can happen with PKEY creates as well. The solution is to release
    the RTNL lock in sysfs functions in case it is not possible to lock
    VLAN RW semaphore and reset the SYS call.
    
    Fixes: 69956d83267e ("IB/ipoib: Sync between remove_one to sysfs calls that use rtnl_lock")
    Signed-off-by: Shalom Lagziel <shaloml@mellanox.com>
    Signed-off-by: Alex Vesker <valex@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/ulp/ipoib/ipoib_vlan.c
+++ drivers/infiniband/ulp/ipoib/ipoib_vlan.c
@@ -142,14 +142,17 @@ int ipoib_vlan_add(struct net_device *pdev, unsigned short pkey)
 		return restart_syscall();
 	}
 
-	priv = ipoib_intf_alloc(ppriv->ca, ppriv->port, intf_name);
-	if (!priv) {
+	if (!down_write_trylock(&ppriv->vlan_rwsem)) {
 		rtnl_unlock();
 		mutex_unlock(&ppriv->sysfs_mutex);
-		return -ENOMEM;
+		return restart_syscall();
 	}
 
-	down_write(&ppriv->vlan_rwsem);
+	priv = ipoib_intf_alloc(ppriv->ca, ppriv->port, intf_name);
+	if (!priv) {
+		result = -ENOMEM;
+		goto out;
+	}
 
 	/*
 	 * First ensure this isn't a duplicate. We check the parent device and
@@ -176,7 +179,7 @@ out:
 	rtnl_unlock();
 	mutex_unlock(&ppriv->sysfs_mutex);
 
-	if (result) {
+	if (result && priv) {
 		free_netdev(priv->dev);
 		kfree(priv);
 	}
@@ -205,7 +208,12 @@ int ipoib_vlan_delete(struct net_device *pdev, unsigned short pkey)
 		return restart_syscall();
 	}
 
-	down_write(&ppriv->vlan_rwsem);
+	if (!down_write_trylock(&ppriv->vlan_rwsem)) {
+		rtnl_unlock();
+		mutex_unlock(&ppriv->sysfs_mutex);
+		return restart_syscall();
+	}
+
 	list_for_each_entry_safe(priv, tpriv, &ppriv->child_intfs, list) {
 		if (priv->pkey == pkey &&
 		    priv->child_type == IPOIB_LEGACY_CHILD) {
