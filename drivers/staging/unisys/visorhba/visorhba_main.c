acf24e3d78d2c49d52f6822477f11b2527d54401
David Kershner <david.kershner@unisys.com>
2017-09-27 13:14:29 -0400

    staging: unisys: only include a file where it is used.
    The header file visorbus.h included several linux headers that were
    used by the source files that include it. Move the includes to the
    files that actually use them.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorhba/visorhba_main.c
+++ drivers/staging/unisys/visorhba/visorhba_main.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 #include <linux/kthread.h>
 #include <linux/idr.h>
+#include <linux/module.h>
 #include <linux/seq_file.h>
 #include <scsi/scsi.h>
 #include <scsi/scsi_host.h>
