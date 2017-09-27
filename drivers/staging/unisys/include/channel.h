26f3909fd1e25b48e6ec39014104441fb46aa31a
David Kershner <david.kershner@unisys.com>
2017-09-27 13:14:30 -0400

    staging: unisys: include: remove io.h from channel.h
    Channel.h was including io.h and not using it. Remove the #include.
    
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/include/channel.h
+++ drivers/staging/unisys/include/channel.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __CHANNEL_H__
 
 #include <linux/types.h>
-#include <linux/io.h>
 #include <linux/uuid.h>
 
 #define SIGNATURE_16(A, B) ((A) | ((B) << 8))
