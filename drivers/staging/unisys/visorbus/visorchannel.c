f1cb65dda43b95df8b8040574e69a77109770df7
Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
2017-09-27 13:14:44 -0400

    staging: unisys: visorbus: simplify visorchannel_create_guts
    Removing the two wrapper functions dealing with visorchannel_create() and
    instead just always use a new version of visorchannel_create() with an
    additional parameter.
    
    Signed-off-by: Sameer Wadgaonkar <sameer.wadgaonkar@unisys.com>
    Signed-off-by: David Kershner <david.kershner@unisys.com>
    Reviewed-by: Tim Sell <timothy.sell@unisys.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorbus/visorchannel.c
+++ drivers/staging/unisys/visorbus/visorchannel.c
@@ -343,9 +343,9 @@ static int signalinsert_inner(struct visorchannel *channel, u32 queue,
 }
 
 /*
- * visorchannel_create_guts() - creates the struct visorchannel abstraction for
- *				a data area in memory, but does NOT modify this
- *				data area
+ * visorchannel_create() - creates the struct visorchannel abstraction for a
+ *                         data area in memory, but does NOT modify this data
+ *                         area
  * @physaddr:      physical address of start of channel
  * @gfp:           gfp_t to use when allocating memory for the data struct
  * @guid:          GUID that identifies channel type;
@@ -356,9 +356,8 @@ static int signalinsert_inner(struct visorchannel *channel, u32 queue,
  * Return: pointer to visorchannel that was created if successful,
  *         otherwise NULL
  */
-static struct visorchannel *visorchannel_create_guts(u64 physaddr, gfp_t gfp,
-						     const guid_t *guid,
-						     bool needs_lock)
+struct visorchannel *visorchannel_create(u64 physaddr, gfp_t gfp,
+					 const guid_t *guid, bool needs_lock)
 {
 	struct visorchannel *channel;
 	int err;
@@ -415,18 +414,6 @@ static struct visorchannel *visorchannel_create_guts(u64 physaddr, gfp_t gfp,
 err_destroy_channel:
 	visorchannel_destroy(channel);
 	return NULL;
-}
-
-struct visorchannel *visorchannel_create(u64 physaddr, gfp_t gfp,
-					 const guid_t *guid)
-{
-	return visorchannel_create_guts(physaddr, gfp, guid, false);
-}
-
-struct visorchannel *visorchannel_create_with_lock(u64 physaddr, gfp_t gfp,
-						   const guid_t *guid)
-{
-	return visorchannel_create_guts(physaddr, gfp, guid, true);
 }
 
 /**
