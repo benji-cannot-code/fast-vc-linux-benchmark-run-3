b76087ebefd59fcb3ab0dd189192ec81eacdfaaf
Abhishek Sahu <absahu@codeaurora.org>
2017-09-28 23:20:46 +0530

    clk: qcom: support Huayra type Alpha PLL
    The Huayra type Alpha PLL  has a 16 bit alpha value, and
    depending on the alpha_mode, the alpha value can be treated as
    M/N value or as a two’s compliment number. This PLL supports
    dynamic programming.
    
    Since the decoding of alpha val and dynamic programming are
    completely different from other Alpha PLLs we add separate
    functions for Huayra PLLs.
    
    Signed-off-by: Abhishek Sahu <absahu@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Alpha PLL types */
 enum {
 	CLK_ALPHA_PLL_TYPE_DEFAULT,
+	CLK_ALPHA_PLL_TYPE_HUAYRA,
 	CLK_ALPHA_PLL_TYPE_MAX,
 };
 
@@ -105,6 +106,7 @@ struct alpha_pll_config {
 extern const struct clk_ops clk_alpha_pll_ops;
 extern const struct clk_ops clk_alpha_pll_hwfsm_ops;
 extern const struct clk_ops clk_alpha_pll_postdiv_ops;
+extern const struct clk_ops clk_alpha_pll_huayra_ops;
 
 void clk_alpha_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 			     const struct alpha_pll_config *config);
