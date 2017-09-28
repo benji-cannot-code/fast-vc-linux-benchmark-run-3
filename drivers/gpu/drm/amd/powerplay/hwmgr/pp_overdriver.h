d1114e94a01454498f8c0960402cfe4db1b95806
Dave Airlie <airlied@redhat.com>
2017-09-28 16:34:44 +1000

    amdgpu/pp: reduce size of vega10_fuses_default
    I've no idea why this is like this, why store 64-bit fields
    as a string, and then parse the strings, this is just over
    engineered.
    
    Reduce the size of the amdgpu.o by 80k.
    
       text	   data	    bss	    dec	    hex	filename
    1331332	  17982	   1008	1350322	 149ab2	amdgpu.o
    1244668	  17982	   1008	1263658	 13482a	amdgpu.o
    
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Alex Deucer <alexander.deucher@amd.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/pp_overdriver.h
+++ drivers/gpu/drm/amd/powerplay/hwmgr/pp_overdriver.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 
 struct phm_fuses_default {
-	const char *key;
+	uint64_t key;
 	uint32_t VFT2_m1;
 	uint32_t VFT2_m2;
 	uint32_t VFT2_b;
