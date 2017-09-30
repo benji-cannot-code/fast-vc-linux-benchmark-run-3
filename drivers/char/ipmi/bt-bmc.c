e7e9bfc5451f476da7336f45d7a1a5d8e6ab8471
Al Viro <viro@zeniv.linux.org.uk>
2017-09-29 20:42:14 -0400

    ipmi: get rid of pointless access_ok()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/char/ipmi/bt-bmc.c
+++ drivers/char/ipmi/bt-bmc.c
@@ -205,9 +205,6 @@ static ssize_t bt_bmc_read(struct file *file, char __user *buf,
 	ssize_t ret = 0;
 	ssize_t nread;
 
-	if (!access_ok(VERIFY_WRITE, buf, count))
-		return -EFAULT;
-
 	WARN_ON(*ppos);
 
 	if (wait_event_interruptible(bt_bmc->queue,
@@ -277,9 +274,6 @@ static ssize_t bt_bmc_write(struct file *file, const char __user *buf,
 	 */
 	if (count < 5)
 		return -EINVAL;
-
-	if (!access_ok(VERIFY_READ, buf, count))
-		return -EFAULT;
 
 	WARN_ON(*ppos);
 
