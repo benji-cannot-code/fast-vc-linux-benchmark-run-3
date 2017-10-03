939d7df105916a6d62116c2b1f45876da1386688
Anup Patel <anup.patel@broadcom.com>
2017-10-03 10:51:51 +0530

    mailbox: bcm-flexrm-mailbox: Use common GPL comment header
    This patch makes the comment header of Broadcom FlexRM driver
    similar to the GPL comment header used across Broadcom driver
    sources.
    
    Signed-off-by: Anup Patel <anup.patel@broadcom.com>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- drivers/mailbox/bcm-flexrm-mailbox.c
+++ drivers/mailbox/bcm-flexrm-mailbox.c
@@ -1,11 +1,19 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* Broadcom FlexRM Mailbox Driver
- *
+/*
  * Copyright (C) 2017 Broadcom
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * Broadcom FlexRM Mailbox Driver
  *
  * Each Broadcom FlexSparx4 offload engine is implemented as an
  * extension to Broadcom FlexRM ring manager. The FlexRM ring
