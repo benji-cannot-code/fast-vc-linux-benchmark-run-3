ffd450273a0b6411f702a8e16bf48659d2493579
Dave Airlie <airlied@redhat.com>
2017-10-03 12:38:57 +1000

    amdgpu/dc: convert dc_transfer to use a kref.
    Rolling your own atomic ref counts is frowned upon.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/os_types.h
+++ drivers/gpu/drm/amd/display/dc/os_types.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <drm/drmP.h>
 
+#include <linux/kref.h>
+
 #include "cgs_linux.h"
 
 #if defined(__BIG_ENDIAN) && !defined(BIGENDIAN_CPU)
