62776df2ccc6fa3e4e88950bfde914ebfef0486c
Heiner Kallweit <hkallweit1@gmail.com>
2017-10-04 20:56:44 +0200

    firmware: arm_scpi: improve struct dvfs_info to make code better readable
    Making the header subfields members of struct dvfs_info allows to make
    the code better readable and avoids some macro magic.
    
    In addition remove a useless statement using info->latency.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/firmware/arm_scpi.c
+++ drivers/firmware/arm_scpi.c
@@ -73,8 +73,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_DVFS_DOMAINS	8
 #define MAX_DVFS_OPPS		16
-#define DVFS_LATENCY(hdr)	(le32_to_cpu(hdr) >> 16)
-#define DVFS_OPP_COUNT(hdr)	((le32_to_cpu(hdr) >> 8) & 0xff)
 
 #define PROTOCOL_REV_MINOR_BITS	16
 #define PROTOCOL_REV_MINOR_MASK	((1U << PROTOCOL_REV_MINOR_BITS) - 1)
@@ -329,7 +327,9 @@ struct legacy_clk_set_value {
 } __packed;
 
 struct dvfs_info {
-	__le32 header;
+	u8 domain;
+	u8 opp_count;
+	__le16 latency;
 	struct {
 		__le32 freq;
 		__le32 m_volt;
@@ -668,8 +668,8 @@ static int scpi_dvfs_populate_info(struct device *dev, u8 domain)
 	if (!info)
 		return -ENOMEM;
 
-	info->count = DVFS_OPP_COUNT(buf.header);
-	info->latency = DVFS_LATENCY(buf.header) * 1000; /* uS to nS */
+	info->count = buf.opp_count;
+	info->latency = le16_to_cpu(buf.latency) * 1000; /* uS to nS */
 
 	info->opps = devm_kcalloc(dev, info->count, sizeof(*opp), GFP_KERNEL);
 	if (!info->opps)
@@ -721,9 +721,6 @@ static int scpi_dvfs_get_transition_latency(struct device *dev)
 
 	if (IS_ERR(info))
 		return PTR_ERR(info);
-
-	if (!info->latency)
-		return 0;
 
 	return info->latency;
 }
