225962c92408003523292adc19fa9c787e6a80ef
Bjorn Helgaas <bhelgaas@google.com>
2017-10-04 15:40:46 -0500

    PCI: Remove unused declarations
    Remove these unused declarations:
    
      pcibios_config_init()              # never defined anywhere
      pcibios_scan_root()                # only defined by x86
      pcibios_get_irq_routing_table()    # only defined by x86
      pcibios_set_irq_routing()          # only defined by x86
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>

--- arch/cris/include/asm/pci.h
+++ arch/cris/include/asm/pci.h
@@ -17,12 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PCIBIOS_MIN_CARDBUS_IO	0x4000
 
-void pcibios_config_init(void);
-struct pci_bus * pcibios_scan_root(int bus);
-
-struct irq_routing_table *pcibios_get_irq_routing_table(void);
-int pcibios_set_irq_routing(struct pci_dev *dev, int pin, int irq);
-
 /* Dynamic DMA mapping stuff.
  * i386 has everything mapped statically.
  */
