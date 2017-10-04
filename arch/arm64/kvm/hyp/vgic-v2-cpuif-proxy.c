e8963dd6f35e6bd5d57511c56dfb996915f1d4e3
Christoffer Dall <christoffer.dall@linaro.org>
2017-10-04 23:25:24 +0200

    KVM: arm/arm64: Move arm64-only vgic-v2-sr.c file to arm64
    The vgic-v2-sr.c file now only contains the logic to replay unaligned
    accesses to the virtual CPU interface on 16K and 64K page systems, which
    is only relevant on 64-bit platforms.  Therefore move this file to the
    arm64 KVM tree, remove the compile directive from the 32-bit side
    makefile, and remove the ifdef in the C file.
    
    Since this file also no longer saves/restores anything, rename the file
    to vgic-v2-cpuif-proxy.c to more accurately describe the logic in this
    file.
    
    Reviewed-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/kvm/hyp/vgic-v2-cpuif-proxy.c
+++ arch/arm64/kvm/hyp/vgic-v2-cpuif-proxy.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/kvm_hyp.h>
 #include <asm/kvm_mmu.h>
 
-#ifdef CONFIG_ARM64
 /*
  * __vgic_v2_perform_cpuif_access -- perform a GICV access on behalf of the
  *				     guest.
@@ -78,4 +77,3 @@ int __hyp_text __vgic_v2_perform_cpuif_access(struct kvm_vcpu *vcpu)
 
 	return 1;
 }
-#endif
