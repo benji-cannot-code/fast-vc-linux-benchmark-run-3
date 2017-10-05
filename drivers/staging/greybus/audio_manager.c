15658f1b763f0bacfda8a14653bdb425c957b127
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2017-10-05 14:56:54 +0200

    greybus: audio: don't inclide rwlock.h directly.
    rwlock.h should not be included directly. Instead linux/splinlock.h
    should be included. One thing it does is to break the RT build.
    
    Cc: Vaibhav Agarwal <vaibhav.sr@gmail.com>
    Cc: Johan Hovold <johan@kernel.org>
    Cc: Alex Elder <elder@kernel.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: greybus-dev@lists.linaro.org
    Cc: devel@driverdev.osuosl.org
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Reviewed-by: Mark Greer <mgreer@animalcreek.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/greybus/audio_manager.c
+++ drivers/staging/greybus/audio_manager.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysfs.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/rwlock.h>
+#include <linux/spinlock.h>
 #include <linux/idr.h>
 
 #include "audio_manager.h"
