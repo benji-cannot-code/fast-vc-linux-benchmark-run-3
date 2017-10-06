7f17af1ec78950b698bfecfb4a8381a7c355dbed
Al Viro <viro@zeniv.linux.org.uk>
2017-10-06 11:19:24 -0400

    acct.h: get rid of detritus
    unused forward declarations of structs
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/acct.h
+++ include/linux/acct.h
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #ifdef CONFIG_BSD_PROCESS_ACCT
-struct vfsmount;
-struct super_block;
-struct pacct_struct;
 struct pid_namespace;
 extern int acct_parm[]; /* for sysctl */
 extern void acct_collect(long exitcode, int group_dead);
