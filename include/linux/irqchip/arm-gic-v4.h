8e121eef7b8c81959bdb7f416dcd5a0e89dfa650
Marc Zyngier <marc.zyngier@arm.com>
2017-10-08 18:48:06 +0100

    irqchip/gic-v3-its: Make GICv4_ITS_LIST_MAX globally available
    As we're about to make use of the maximum number of ITSs in
    a GICv4 system, let's make this value global (and rename it to
    GICv4_ITS_LIST_MAX).
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/linux/irqchip/arm-gic-v4.h
+++ include/linux/irqchip/arm-gic-v4.h
@@ -21,6 +21,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct its_vpe;
 
+/*
+ * Maximum number of ITTs when GITS_TYPER.VMOVP == 0, using the
+ * ITSList mechanism to perform inter-ITS synchronization.
+ */
+#define GICv4_ITS_LIST_MAX		16
+
 /* Embedded in kvm.arch */
 struct its_vm {
 	struct fwnode_handle	*fwnode;
