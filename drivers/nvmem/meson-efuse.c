8f1fc9decf333df5d553a1b3b29734c933f9d90e
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2017-10-09 15:26:40 +0200

    nvmem: meson-efuse: indicate that this driver is only for Meson GX SoCs
    The current Amlogic Meson eFuse driver only supports the 64-bit SoCs
    (GXBB and newer). Older SoCs cannot be supported by the same driver
    because they do not use the meson secure monitor firmware to access the
    hardware.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/nvmem/meson-efuse.c
+++ drivers/nvmem/meson-efuse.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Amlogic eFuse Driver
+ * Amlogic Meson GX eFuse Driver
  *
  * Copyright (c) 2016 Endless Computers, Inc.
  * Author: Carlo Caione <carlo@endlessm.com>
@@ -90,5 +90,5 @@ static struct platform_driver meson_efuse_driver = {
 module_platform_driver(meson_efuse_driver);
 
 MODULE_AUTHOR("Carlo Caione <carlo@endlessm.com>");
-MODULE_DESCRIPTION("Amlogic Meson NVMEM driver");
+MODULE_DESCRIPTION("Amlogic Meson GX NVMEM driver");
 MODULE_LICENSE("GPL v2");
