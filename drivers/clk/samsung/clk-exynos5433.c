43074c2280b47a347f39343dc83818bd24af6f6a
Arnd Bergmann <arnd@arndb.de>
2017-10-10 11:15:12 +0200

    clk: samsung: exynos5433: mark PM functions as __maybe_unused
    The suspend/resume functions are referenced conditionally, causing
    a harmless warning when CONFIG_PM is disabled:
    
    drivers/clk/samsung/clk-exynos5433.c:5476:12: error: 'exynos5433_cmu_resume' defined but not used [-Werror=unused-function]
    drivers/clk/samsung/clk-exynos5433.c:5453:12: error: 'exynos5433_cmu_suspend' defined but not used [-Werror=unused-function]
    
    This marks both as __maybe_unused to shut up the warning.
    
    Fixes: 523d3de41f02 ("clk: samsung: exynos5433: Add support for runtime PM")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Acked-by: Chanwoo Choi <cw00.choi@samsung.com>
    Acked-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Sylwester Nawrocki <s.nawrocki@samsung.com>

--- drivers/clk/samsung/clk-exynos5433.c
+++ drivers/clk/samsung/clk-exynos5433.c
@@ -5451,7 +5451,7 @@ struct exynos5433_cmu_data {
 	struct samsung_clk_provider ctx;
 };
 
-static int exynos5433_cmu_suspend(struct device *dev)
+static int __maybe_unused exynos5433_cmu_suspend(struct device *dev)
 {
 	struct exynos5433_cmu_data *data = dev_get_drvdata(dev);
 	int i;
@@ -5474,7 +5474,7 @@ static int exynos5433_cmu_suspend(struct device *dev)
 	return 0;
 }
 
-static int exynos5433_cmu_resume(struct device *dev)
+static int __maybe_unused exynos5433_cmu_resume(struct device *dev)
 {
 	struct exynos5433_cmu_data *data = dev_get_drvdata(dev);
 	int i;
