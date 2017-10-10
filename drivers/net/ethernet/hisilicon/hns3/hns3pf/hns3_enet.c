703bf482e06fc9f75ada851bcd286f0e93197a82
Lipeng <lipeng321@huawei.com>
2017-10-10 16:42:04 +0800

    net: hns3: add support for set_ringparam
    This patch supports the ethtool's set_ringparam().
    
    Signed-off-by: Lipeng <lipeng321@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hns3_enet.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hns3_enet.c
@@ -2638,7 +2638,7 @@ static void hns3_init_ring_hw(struct hns3_enet_ring *ring)
 	}
 }
 
-static int hns3_init_all_ring(struct hns3_nic_priv *priv)
+int hns3_init_all_ring(struct hns3_nic_priv *priv)
 {
 	struct hnae3_handle *h = priv->ae_handle;
 	int ring_num = h->kinfo.num_tqps * 2;
@@ -2667,7 +2667,7 @@ out_when_alloc_ring_memory:
 	return -ENOMEM;
 }
 
-static int hns3_uninit_all_ring(struct hns3_nic_priv *priv)
+int hns3_uninit_all_ring(struct hns3_nic_priv *priv)
 {
 	struct hnae3_handle *h = priv->ae_handle;
 	int i;
