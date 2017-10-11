53e02dc67048dd4f321dfb2448817b902517c7e5
Himanshu Jha <himanshujha199640@gmail.com>
2017-10-11 21:06:14 +0530

    scsi: be2iscsi: Use kasprintf
    Use kasprintf instead of combination of kmalloc and sprintf.  Also,
    remove BEISCSI_MSI_NAME macro used to specify size of string as
    kasprintf handles size computations.
    
    Signed-off-by: Himanshu Jha <himanshujha199640@gmail.com>
    Reviewed-by: Kyle Fortin <kyle.fortin@oracle.com>
    Reviewed-by: Jitendra Bhivare <jitendra.bhivare@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/be2iscsi/be_main.h
+++ drivers/scsi/be2iscsi/be_main.h
@@ -154,8 +154,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PAGES_REQUIRED(x) \
 	((x < PAGE_SIZE) ? 1 :  ((x + PAGE_SIZE - 1) / PAGE_SIZE))
 
-#define BEISCSI_MSI_NAME 20 /* size of msi_name string */
-
 #define MEM_DESCR_OFFSET 8
 #define BEISCSI_DEFQ_HDR 1
 #define BEISCSI_DEFQ_DATA 0
