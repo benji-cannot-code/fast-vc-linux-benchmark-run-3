69182c085a4e50c586143a87af318a847b9bc5f1
Chris Lew <clew@codeaurora.org>
2017-10-11 14:29:10 -0700

    soc: qcom: smem: Increase the number of hosts
    Increase the maximum number of hosts in a system to 10.
    
    Signed-off-by: Chris Lew <clew@codeaurora.org>
    Acked-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

--- drivers/soc/qcom/smem.c
+++ drivers/soc/qcom/smem.c
@@ -92,7 +92,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SMEM_GLOBAL_HOST	0xfffe
 
 /* Max number of processors/hosts in a system */
-#define SMEM_HOST_COUNT		9
+#define SMEM_HOST_COUNT		10
 
 /**
   * struct smem_proc_comm - proc_comm communication struct (legacy)
