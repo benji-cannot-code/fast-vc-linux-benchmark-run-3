947de89374b0995c8e7ffc4ffb834b6f6dc0c7e2
Sakari Ailus <sakari.ailus@linux.intel.com>
2017-10-11 11:06:13 +0300

    ACPI: properties: Fix __acpi_node_get_property_reference() return codes
    Fix more return codes for device property: Align return codes of
    __acpi_node_get_property_reference().
    
    In particular, what was missed previously:
    
     -EPROTO could be returned in certain cases, now -EINVAL;
     -EINVAL was returned if the property was not found, now -ENOENT;
     -EINVAL was returned also if the index was higher than the number of
             entries in a package, now -ENOENT.
    
    Reported-by: Hyungwoo Yang <hyungwoo.yang@intel.com>
    Fixes: 3e3119d3088f (device property: Introduce fwnode_property_get_reference_args)
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Tested-by: Hyungwoo Yang <hyungwoo.yang@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/property.c
+++ drivers/acpi/property.c
@@ -594,7 +594,7 @@ int __acpi_node_get_property_reference(const struct fwnode_handle *fwnode,
 
 	ret = acpi_data_get_property(data, propname, ACPI_TYPE_ANY, &obj);
 	if (ret)
-		return ret;
+		return ret == -EINVAL ? -ENOENT : -EINVAL;
 
 	/*
 	 * The simplest case is when the value is a single reference.  Just
@@ -606,7 +606,7 @@ int __acpi_node_get_property_reference(const struct fwnode_handle *fwnode,
 
 		ret = acpi_bus_get_device(obj->reference.handle, &device);
 		if (ret)
-			return ret;
+			return ret == -ENODEV ? -EINVAL : ret;
 
 		args->adev = device;
 		args->nargs = 0;
@@ -622,8 +622,10 @@ int __acpi_node_get_property_reference(const struct fwnode_handle *fwnode,
 	 * The index argument is then used to determine which reference
 	 * the caller wants (along with the arguments).
 	 */
-	if (obj->type != ACPI_TYPE_PACKAGE || index >= obj->package.count)
-		return -EPROTO;
+	if (obj->type != ACPI_TYPE_PACKAGE)
+		return -EINVAL;
+	if (index >= obj->package.count)
+		return -ENOENT;
 
 	element = obj->package.elements;
 	end = element + obj->package.count;
