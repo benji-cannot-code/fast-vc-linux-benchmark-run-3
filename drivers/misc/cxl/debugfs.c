f392515cd537e0e148a021a1e9e4b7fcec674c1c
Vaibhav Jain <vaibhav@linux.vnet.ibm.com>
2017-10-11 18:00:20 +0530

    cxl: Rework the implementation of cxl_stop_trace_psl9()
    Presently the PSL9 specific cxl_stop_trace_psl9() only stops the RX0
    traces on the CXL adapter when a PSL error irq is triggered. The patch
    updates the function to stop all the traces arrays and move them to
    the FIN state. The implementation issues the mmio to TRACECFG register
    to stop the trace array iff it already not in FIN state. This prevents
    the issue of trace data being reset in case of multiple stop mmio
    issued for a single trace array.
    
    Also the patch does some refactoring of existing cxl_stop_trace_psl9()
    and cxl_stop_trace_psl8() functions by moving them to 'pci.c' from
    'debugfs.c' file and marking them as static.
    
    Signed-off-by: Vaibhav Jain <vaibhav@linux.vnet.ibm.com>
    Acked-by: Frederic Barrat <fbarrat@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- drivers/misc/cxl/debugfs.c
+++ drivers/misc/cxl/debugfs.c
@@ -16,28 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct dentry *cxl_debugfs;
 
-void cxl_stop_trace_psl9(struct cxl *adapter)
-{
-	/* Stop the trace */
-	cxl_p1_write(adapter, CXL_PSL9_TRACECFG, 0x4480000000000000ULL);
-}
-
-void cxl_stop_trace_psl8(struct cxl *adapter)
-{
-	int slice;
-
-	/* Stop the trace */
-	cxl_p1_write(adapter, CXL_PSL_TRACE, 0x8000000000000017LL);
-
-	/* Stop the slice traces */
-	spin_lock(&adapter->afu_list_lock);
-	for (slice = 0; slice < adapter->slices; slice++) {
-		if (adapter->afu[slice])
-			cxl_p1n_write(adapter->afu[slice], CXL_PSL_SLICE_TRACE, 0x8000000000000000LL);
-	}
-	spin_unlock(&adapter->afu_list_lock);
-}
-
 /* Helpers to export CXL mmaped IO registers via debugfs */
 static int debugfs_io_u64_get(void *data, u64 *val)
 {
