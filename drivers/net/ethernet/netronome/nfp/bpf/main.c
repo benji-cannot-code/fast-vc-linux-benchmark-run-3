30be93d4df9f6a2e097a1c190c93262400a60246
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-10-12 10:34:13 -0700

    nfp: bpf: support BPF offload only on little endian
    eBPF is host-endian specific.  Translating both BE and LE eBPF
    to the NFP is feasible, but would require quite a bit of indirection.
    The fact that I don't have access to any BE hosts that would fit
    a 25G/40G/100G NIC is also limiting my ability to test big endian.
    
    For now restrict the offload to little endian hosts only.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/netronome/nfp/bpf/main.c
+++ drivers/net/ethernet/netronome/nfp/bpf/main.c
@@ -43,9 +43,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static bool nfp_net_ebpf_capable(struct nfp_net *nn)
 {
+#ifdef __LITTLE_ENDIAN
 	if (nn->cap & NFP_NET_CFG_CTRL_BPF &&
 	    nn_readb(nn, NFP_NET_CFG_BPF_ABI) == NFP_NET_BPF_ABI)
 		return true;
+#endif
 	return false;
 }
 
