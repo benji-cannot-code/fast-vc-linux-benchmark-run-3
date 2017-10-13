7f67c6f14ff9acb367fcffafcb867c96ed18d04b
Hans Verkuil <hansverk@cisco.com>
2017-10-13 18:01:32 +0200

    media: cec-pin.h: move non-kAPI parts into cec-pin-priv.h
    The kAPI cec-pin.h header also defined data structures that did
    not belong here but were private to the CEC core code.
    
    Split that part off into a cec-pin-priv.h header.
    
    Signed-off-by: Hans Verkuil <hansverk@cisco.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/cec/cec-pin.c
+++ drivers/media/cec/cec-pin.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched/types.h>
 
 #include <media/cec-pin.h>
+#include "cec-pin-priv.h"
 
 /* All timings are in microseconds */
 
