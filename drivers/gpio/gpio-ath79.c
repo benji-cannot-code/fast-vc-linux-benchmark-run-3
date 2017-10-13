fed5559d2450c6bba5101ad9e7a3df12afc882c8
Gustavo A. R. Silva <garsilva@embeddedor.com>
2017-10-13 15:43:53 -0500

    gpio: mark expected switch fall-throughs
    In preparation to enabling -Wimplicit-fallthrough, mark switch cases
    where we are expecting to fall through.
    
    Signed-off-by: Gustavo A. R. Silva <garsilva@embeddedor.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-ath79.c
+++ drivers/gpio/gpio-ath79.c
@@ -133,6 +133,7 @@ static int ath79_gpio_irq_set_type(struct irq_data *data,
 
 	case IRQ_TYPE_LEVEL_HIGH:
 		polarity |= mask;
+		/* fall through */
 	case IRQ_TYPE_LEVEL_LOW:
 		type |= mask;
 		break;
