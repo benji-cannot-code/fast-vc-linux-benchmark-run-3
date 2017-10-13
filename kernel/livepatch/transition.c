242f10bb7a29196a0792a3cfa7e087a8b22b2713
Joe Lawrence <joe.lawrence@redhat.com>
2017-10-13 15:08:42 -0400

    livepatch: move transition "complete" notice into klp_complete_transition()
    klp_complete_transition() performs a bit of housework before a
    transition to KLP_PATCHED or KLP_UNPATCHED is actually completed
    (including post-(un)patch callbacks).  To be consistent, move the
    transition "complete" kernel log notice out of
    klp_try_complete_transition() and into klp_complete_transition().
    
    Suggested-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>
    Acked-by: Miroslav Benes <mbenes@suse.cz>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- kernel/livepatch/transition.c
+++ kernel/livepatch/transition.c
@@ -137,6 +137,9 @@ done:
 			klp_post_unpatch_callback(obj);
 	}
 
+	pr_notice("'%s': %s complete\n", klp_transition_patch->mod->name,
+		  klp_target_state == KLP_PATCHED ? "patching" : "unpatching");
+
 	/*
 	 * See complementary comment in __klp_enable_patch() for why we
 	 * keep the module reference for immediate patches.
@@ -424,9 +427,6 @@ void klp_try_complete_transition(void)
 	}
 
 success:
-	pr_notice("'%s': %s complete\n", klp_transition_patch->mod->name,
-		  klp_target_state == KLP_PATCHED ? "patching" : "unpatching");
-
 	/* we're done, now cleanup the data structures */
 	klp_complete_transition();
 }
