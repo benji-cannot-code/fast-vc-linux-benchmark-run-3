f54c38184d40a25ae0b383ddb16fa562abea00cd
Michal Wajdeczko <michal.wajdeczko@intel.com>
2017-10-16 14:47:20 +0000

    drm/i915: Update DMC firmware load error messages
    Some of the error messages from DMC load were too generic and
    may be confusing for the user. Lets explicitly add DMC words there.
    Also as homepage of DMC firmware is same as for the GuC and Huc,
    lets reuse URL definition.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Imre Deak <imre.deak@intel.com>
    Cc: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171016144724.17244-12-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/intel_uc_fw.h
+++ drivers/gpu/drm/i915/intel_uc_fw.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct drm_i915_private;
 
+/* Home of GuC, HuC and DMC firmwares */
 #define INTEL_UC_FIRMWARE_URL "https://01.org/linuxgraphics/downloads/firmware"
 
 enum intel_uc_fw_status {
