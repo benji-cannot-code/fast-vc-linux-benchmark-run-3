522d7963895adb3862703ba686f004aa90c31b27
Tom St Denis <tom.stdenis@amd.com>
2017-10-16 13:25:42 -0400

    drm/amd/powerplay: Tidy up cz_dpm_powerdown_uvd()
    Use PP_CAP and tidy up indentation.
    
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/cz_hwmgr.c
+++ drivers/gpu/drm/amd/powerplay/hwmgr/cz_hwmgr.c
@@ -1275,10 +1275,8 @@ static int cz_dpm_force_dpm_level(struct pp_hwmgr *hwmgr,
 
 int cz_dpm_powerdown_uvd(struct pp_hwmgr *hwmgr)
 {
-	if (phm_cap_enabled(hwmgr->platform_descriptor.platformCaps,
-					 PHM_PlatformCaps_UVDPowerGating))
-		return smum_send_msg_to_smc(hwmgr,
-						     PPSMC_MSG_UVDPowerOFF);
+	if (PP_CAP(PHM_PlatformCaps_UVDPowerGating))
+		return smum_send_msg_to_smc(hwmgr, PPSMC_MSG_UVDPowerOFF);
 	return 0;
 }
 
