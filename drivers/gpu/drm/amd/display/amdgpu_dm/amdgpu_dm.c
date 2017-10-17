66c49ce3a2dd106914d6c8b6ead119fdfef7c33a
Tom St Denis <tom.stdenis@amd.com>
2017-10-17 09:00:33 -0400

    drm/amd/display: Fix brace style in dm_handle_hpd_rx_irq()
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>
    Acked-by: Andrey Grodzovsky  <andey.grodzovsky@amd.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -1011,8 +1011,9 @@ static void dm_handle_hpd_rx_irq(struct amdgpu_dm_connector *aconnector)
 				dpcd_bytes_to_read);
 
 			new_irq_handled = false;
-		} else
+		} else {
 			break;
+		}
 	}
 
 	if (process_count == max_process_count)
