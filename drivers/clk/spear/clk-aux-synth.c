c69f3e986bc5add443a07efb658c7920d6a11e46
Bhumika Goyal <bhumirks@gmail.com>
2017-10-17 16:38:34 +0200

    CLK: SPEAr: make aux_clk_masks structures const
    Make these const as they are either stored in the masks 'const' field
    of a clk_aux structure or passed to the function clk_register_aux
    having the argument as const.
    
    Signed-off-by: Bhumika Goyal <bhumirks@gmail.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/spear/clk-aux-synth.c
+++ drivers/clk/spear/clk-aux-synth.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define to_clk_aux(_hw) container_of(_hw, struct clk_aux, hw)
 
-static struct aux_clk_masks default_aux_masks = {
+static const  struct aux_clk_masks default_aux_masks = {
 	.eq_sel_mask = AUX_EQ_SEL_MASK,
 	.eq_sel_shift = AUX_EQ_SEL_SHIFT,
 	.eq1_mask = AUX_EQ1_SEL,
