0d1ca352c249db36a6576ad85bb6777d4782b2a7
Kees Cook <keescook@chromium.org>
2017-10-20 07:36:05 -0700

    waitid(): Avoid unbalanced user_access_end() on access_ok() error
    As pointed out by Linus and David, the earlier waitid() fix resulted in
    a (currently harmless) unbalanced user_access_end() call.  This fixes it
    to just directly return EFAULT on access_ok() failure.
    
    Fixes: 96ca579a1ecc ("waitid(): Add missing access_ok() checks")
    Acked-by: David Daney <david.daney@cavium.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -1612,7 +1612,7 @@ SYSCALL_DEFINE5(waitid, int, which, pid_t, upid, struct siginfo __user *,
 		return err;
 
 	if (!access_ok(VERIFY_WRITE, infop, sizeof(*infop)))
-		goto Efault;
+		return -EFAULT;
 
 	user_access_begin();
 	unsafe_put_user(signo, &infop->si_signo, Efault);
@@ -1740,7 +1740,7 @@ COMPAT_SYSCALL_DEFINE5(waitid,
 		return err;
 
 	if (!access_ok(VERIFY_WRITE, infop, sizeof(*infop)))
-		goto Efault;
+		return -EFAULT;
 
 	user_access_begin();
 	unsafe_put_user(signo, &infop->si_signo, Efault);
