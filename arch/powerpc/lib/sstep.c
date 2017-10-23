5dd591028e2ff6ea3a14fa5791386f0422b17618
Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
2017-10-23 22:07:40 +0530

    powerpc/kprobes: Blacklist emulate_update_regs() from kprobes
    Commit 3cdfcbfd32b9d ("powerpc: Change analyse_instr so it doesn't
    modify *regs") introduced emulate_update_regs() to perform part of what
    emulate_step() was doing earlier. However, this function was not added
    to the kprobes blacklist. Add it so as to prevent it from being probed.
    
    Signed-off-by: Naveen N. Rao <naveen.n.rao@linux.vnet.ibm.com>
    Acked-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/lib/sstep.c
+++ arch/powerpc/lib/sstep.c
@@ -2718,6 +2718,7 @@ void emulate_update_regs(struct pt_regs *regs, struct instruction_op *op)
 	}
 	regs->nip = next_pc;
 }
+NOKPROBE_SYMBOL(emulate_update_regs);
 
 /*
  * Emulate a previously-analysed load or store instruction.
