05efdd98dcb4e7db53decbe8d9f010a45c5f3296
Xiang Chen <chenxiang66@hisilicon.com>
2017-10-24 23:51:35 +0800

    scsi: hisi_sas: fix SATA breakpoint memory size
    Currently the size of memory we allocate for SATA breakpoint buffer is
    incorrect.
    
    The breakpoint memory size should be as follows: 32 (NCQ tags) * 128 *
    2048 (max #devs) = 8MB
    
    Currently we only allocate 0.5MB, but get away with it as we never
    have SATA device index > 128 typically.
    
    To conserve precious DMA memory (8MB may not be even available), limit
    the number of devices per HBA to 1024, which means 4MB of memory
    required for SATA breakpoint.
    
    The 1024 device limit applied to all HW versions. For v3 hw, we need
    to configure this value.
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HISI_SAS_MAX_PHYS	9
 #define HISI_SAS_MAX_QUEUES	32
 #define HISI_SAS_QUEUE_SLOTS 512
-#define HISI_SAS_MAX_ITCT_ENTRIES 2048
+#define HISI_SAS_MAX_ITCT_ENTRIES 1024
 #define HISI_SAS_MAX_DEVICES HISI_SAS_MAX_ITCT_ENTRIES
 #define HISI_SAS_RESET_BIT	0
 #define HISI_SAS_REJECT_CMD_BIT	1
@@ -343,7 +343,11 @@ struct hisi_sas_initial_fis {
 };
 
 struct hisi_sas_breakpoint {
-	u8	data[128];	/*io128 byte*/
+	u8	data[128];
+};
+
+struct hisi_sas_sata_breakpoint {
+	struct hisi_sas_breakpoint tag[32];
 };
 
 struct hisi_sas_sge {
