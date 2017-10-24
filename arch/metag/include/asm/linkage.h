4b2c98fb92561b40d230ac262477f671b69c80e2
James Hogan <jhogan@kernel.org>
2017-10-24 13:07:54 +0100

    metag: Remove arch/metag/
    The earliest Meta architecture port of Linux I have a record of was an
    import of a Meta port of Linux v2.4.1 in February 2004, which was worked
    on significantly over the next few years by Graham Whaley, Will Newton,
    Matt Fleming, myself and others.
    
    Eventually the port was merged into mainline in v3.9 in March 2013, not
    long after Imagination Technologies bought MIPS Technologies and shifted
    its CPU focus over to the MIPS architecture.
    
    As a result, though the port was maintained for a while, kept on life
    support for a while longer, and useful for testing a few specific
    drivers for which I don't have ready access to the equivalent MIPS
    hardware, it is now essentially dead with no users.
    
    It is also stuck using an out-of-tree toolchain based on GCC 4.2.4 which
    is no longer maintained, now struggles to build modern kernels due to
    toolchain bugs, and doesn't itself build with a modern GCC. The latest
    buildroot port is still using an old uClibc snapshot which is no longer
    served, and the latest uClibc doesn't build with GCC 4.2.4.
    
    So lets call it a day and drop the Meta architecture port from the
    kernel. RIP Meta.
    
    Signed-off-by: James Hogan <jhogan@kernel.org>
    Link: https://lkml.kernel.org/r/95906b76-6ce1-3f84-eaba-c29b4ae952eb@roeck-us.net
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Graham Whaley <graham.whaley@gmail.com>
    Cc: linux-metag@vger.kernel.org

--- arch/metag/include/asm/linkage.h
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __ASM_LINKAGE_H
-#define __ASM_LINKAGE_H
-
-#define __ALIGN .p2align 2
-#define __ALIGN_STR ".p2align 2"
-
-#endif
