3372a905b50fe4d6df5bfd9f57078e98ca20c324
Greentime Hu <greentime@andestech.com>
2017-10-25 14:37:17 +0800

    nds32: Loadable modules
    This patch adds support for loadable modules.
    
    Signed-off-by: Vincent Chen <vincentc@andestech.com>
    Signed-off-by: Greentime Hu <greentime@andestech.com>
    Acked-by:  Arnd Bergmann <arnd@arndb.de>

---
+++ arch/nds32/include/asm/module.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2005-2017 Andes Technology Corporation
+
+#ifndef _ASM_NDS32_MODULE_H
+#define _ASM_NDS32_MODULE_H
+
+#include <asm-generic/module.h>
+
+#define MODULE_ARCH_VERMAGIC	"NDS32v3"
+
+#endif /* _ASM_NDS32_MODULE_H */
