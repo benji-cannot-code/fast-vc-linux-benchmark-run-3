3b731c64b0f1c78b483639eaede1dc5ec1ad8314
John Allen <jallen@linux.vnet.ibm.com>
2017-10-26 16:24:15 -0500

    ibmvnic: Fix failover error path for non-fatal resets
    For all non-fatal reset conditions, the hypervisor will send a failover when
    we attempt to initialize the crq and the vnic client is expected to handle
    that failover instead of the existing non-fatal reset. To handle this, we
    need to return from init with a return code that indicates that we have hit
    this case.
    
    Signed-off-by: John Allen <jallen@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ibm/ibmvnic.c
+++ drivers/net/ethernet/ibm/ibmvnic.c
@@ -1508,7 +1508,7 @@ static int do_reset(struct ibmvnic_adapter *adapter,
 
 		rc = ibmvnic_init(adapter);
 		if (rc)
-			return 0;
+			return IBMVNIC_INIT_FAILED;
 
 		/* If the adapter was in PROBE state prior to the reset,
 		 * exit here.
@@ -1611,7 +1611,7 @@ static void __ibmvnic_reset(struct work_struct *work)
 	while (rwi) {
 		rc = do_reset(adapter, rwi, reset_state);
 		kfree(rwi);
-		if (rc)
+		if (rc && rc != IBMVNIC_INIT_FAILED)
 			break;
 
 		rwi = get_next_rwi(adapter);
--- drivers/net/ethernet/ibm/ibmvnic.h
+++ drivers/net/ethernet/ibm/ibmvnic.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IBMVNIC_DRIVER_VERSION	"1.0.1"
 #define IBMVNIC_INVALID_MAP	-1
 #define IBMVNIC_STATS_TIMEOUT	1
+#define IBMVNIC_INIT_FAILED	2
+
 /* basic structures plus 100 2k buffers */
 #define IBMVNIC_IO_ENTITLEMENT_DEFAULT	610305
 
