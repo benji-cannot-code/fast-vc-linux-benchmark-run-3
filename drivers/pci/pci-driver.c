914caf3c3daa283f56b0d2f3ae299c23e8a25e58
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-10-26 12:12:16 +0200

    PCI / PM: Drop unnecessary invocations of pcibios_pm_ops callbacks
    The only user of non-empty pcibios_pm_ops is s390 and it only uses
    "noirq" callbacks, so drop the invocations of the other pcibios_pm_ops
    callbacks from the PCI PM code.
    
    That will allow subsequent changes to be somewhat simpler.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/pci/pci-driver.c
+++ drivers/pci/pci-driver.c
@@ -923,9 +923,6 @@ static int pci_pm_freeze(struct device *dev)
 			return error;
 	}
 
-	if (pcibios_pm_ops.freeze)
-		return pcibios_pm_ops.freeze(dev);
-
 	return 0;
 }
 
@@ -987,12 +984,6 @@ static int pci_pm_thaw(struct device *dev)
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	int error = 0;
 
-	if (pcibios_pm_ops.thaw) {
-		error = pcibios_pm_ops.thaw(dev);
-		if (error)
-			return error;
-	}
-
 	if (pci_has_legacy_pm_support(pci_dev))
 		return pci_legacy_resume(dev);
 
@@ -1036,9 +1027,6 @@ static int pci_pm_poweroff(struct device *dev)
 
  Fixup:
 	pci_fixup_device(pci_fixup_suspend, pci_dev);
-
-	if (pcibios_pm_ops.poweroff)
-		return pcibios_pm_ops.poweroff(dev);
 
 	return 0;
 }
@@ -1111,12 +1099,6 @@ static int pci_pm_restore(struct device *dev)
 	struct pci_dev *pci_dev = to_pci_dev(dev);
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	int error = 0;
-
-	if (pcibios_pm_ops.restore) {
-		error = pcibios_pm_ops.restore(dev);
-		if (error)
-			return error;
-	}
 
 	/*
 	 * This is necessary for the hibernation error path in which restore is
