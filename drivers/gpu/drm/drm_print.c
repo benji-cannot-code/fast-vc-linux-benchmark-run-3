e41e05cc41c44719b9533852bc7ce98e8ab9712f
Chris Wilson <chris@chris-wilson.co.uk>
2017-10-27 12:06:02 +0100

    drm: Enable pr_debug() for drm_printer
    pr_debug() is conditionally compiled and requires either
    dynamic-debugging to be enabled or for the code to opt-in using #define
    DEBUG. Since drm_print provides a central debugging facility using
    pr_debug(), make sure it will always produce output.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Rob Clark <robdclark@gmail.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171027110602.31519-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/drm_print.c
+++ drivers/gpu/drm/drm_print.c
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Rob Clark <robdclark@gmail.com>
  */
 
+#define DEBUG /* for pr_debug() */
+
 #include <stdarg.h>
 #include <linux/seq_file.h>
 #include <drm/drmP.h>
