347186e0ec58e0bd237f2e3ea5b1b2e13a5e5b6d
Marc Zyngier <marc.zyngier@arm.com>
2017-10-27 15:28:45 +0100

    KVM: arm/arm64: GICv4: Propagate property updates to VLPIs
    Upon updating a property, we propagate it all the way to the physical
    ITS, and ask for an INV command to be executed there.
    
    Acked-by: Christoffer Dall <cdall@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- virt/kvm/arm/vgic/vgic-its.c
+++ virt/kvm/arm/vgic/vgic-its.c
@@ -298,6 +298,9 @@ static int update_lpi_config(struct kvm *kvm, struct vgic_irq *irq,
 		spin_unlock_irqrestore(&irq->irq_lock, flags);
 	}
 
+	if (irq->hw)
+		return its_prop_update_vlpi(irq->host_irq, prop, true);
+
 	return 0;
 }
 
