ccfec32bb0803a23b0b25014c7ea2494ef4cb6c4
Arvind Yadav <arvind.yadav.cs@gmail.com>
2017-10-28 23:51:47 +0530

    libata: fix spelling mistake: 'ambigious' -> 'ambiguous'
    Trivial fix to spelling mistakes in ata_parse_force_one().
    
    Signed-off-by: Arvind Yadav <arvind.yadav.cs@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -6909,7 +6909,7 @@ static int __init ata_parse_force_one(char **cur,
 		return -EINVAL;
 	}
 	if (nr_matches > 1) {
-		*reason = "ambigious value";
+		*reason = "ambiguous value";
 		return -EINVAL;
 	}
 
