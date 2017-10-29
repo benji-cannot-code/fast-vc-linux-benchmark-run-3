a0d7f3ee6399f716d72b1209c83ebb2d2d0b4b9a
Vitaly Kuznetsov <vkuznets@redhat.com>
2017-10-29 12:21:00 -0700

    hyper-v: trace vmbus_on_msg_dpc()
    Add tracing subsystem to Hyper-V VMBus module and add tracepoint
    to vmbus_on_msg_dpc() which is called when we receive a message from host.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hv/hyperv_vmbus.h
+++ drivers/hv/hyperv_vmbus.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/hyperv.h>
 #include <linux/interrupt.h>
 
+#include "hv_trace.h"
+
 /*
  * Timeout for services such as KVP and fcopy.
  */
