489bf4f40c2115cf9709e6e7e1ec8c2eb7da07df
Stefano Stabellini <sstabellini@kernel.org>
2017-10-30 15:40:54 -0700

    xen/pvcalls: implement socket command and handle events
    Send a PVCALLS_SOCKET command to the backend, use the masked
    req_prod_pvt as req_id. This way, req_id is guaranteed to be between 0
    and PVCALLS_NR_REQ_PER_RING. We already have a slot in the rsp array
    ready for the response, and there cannot be two outstanding responses
    with the same req_id.
    
    Wait for the response by waiting on the inflight_req waitqueue and
    check for the req_id field in rsp[req_id]. Use atomic accesses and
    barriers to read the field. Note that the barriers are simple smp
    barriers (as opposed to virt barriers) because they are for internal
    frontend synchronization, not frontend<->backend communication.
    
    Once a response is received, clear the corresponding rsp slot by setting
    req_id to PVCALLS_INVALID_ID. Note that PVCALLS_INVALID_ID is invalid
    only from the frontend point of view. It is not part of the PVCalls
    protocol.
    
    pvcalls_front_event_handler is in charge of copying responses from the
    ring to the appropriate rsp slot. It is done by copying the body of the
    response first, then by copying req_id atomically. After the copies,
    wake up anybody waiting on waitqueue.
    
    socket_lock protects accesses to the ring.
    
    Convert the pointer to sock_mapping into an uintptr_t and use it as
    id for the new socket to pass to the backend. The struct will be fully
    initialized later on connect or bind.
    
    sock->sk->sk_send_head is not used for ip sockets: reuse the field to
    store a pointer to the struct sock_mapping corresponding to the socket.
    This way, we can easily get the struct sock_mapping from the struct
    socket.
    
    Signed-off-by: Stefano Stabellini <stefano@aporeto.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    CC: boris.ostrovsky@oracle.com
    CC: jgross@suse.com
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

---
+++ drivers/xen/pvcalls-front.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __PVCALLS_FRONT_H__
+#define __PVCALLS_FRONT_H__
+
+#include <linux/net.h>
+
+int pvcalls_front_socket(struct socket *sock);
+
+#endif
