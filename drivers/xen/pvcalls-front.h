9e4e6de4e41d37f18388e92c341fa0fefb505c80
Stefano Stabellini <sstabellini@kernel.org>
2017-10-30 15:40:56 -0700

    xen/pvcalls: implement bind command
    Send PVCALLS_BIND to the backend. Introduce a new structure, part of
    struct sock_mapping, to store information specific to passive sockets.
    
    Introduce a status field to keep track of the status of the passive
    socket.
    
    Signed-off-by: Stefano Stabellini <stefano@aporeto.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    CC: boris.ostrovsky@oracle.com
    CC: jgross@suse.com
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- drivers/xen/pvcalls-front.h
+++ drivers/xen/pvcalls-front.h
@@ -7,5 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int pvcalls_front_socket(struct socket *sock);
 int pvcalls_front_connect(struct socket *sock, struct sockaddr *addr,
 			  int addr_len, int flags);
+int pvcalls_front_bind(struct socket *sock,
+		       struct sockaddr *addr,
+		       int addr_len);
 
 #endif
