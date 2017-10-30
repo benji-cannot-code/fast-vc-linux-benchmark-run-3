342fe504221840af6fd84a1ef8782e3f668ac9ff
Chuck Lever <chuck.lever@oracle.com>
2017-10-30 16:22:06 -0400

    xprtrdma: Remove include for linux/prefetch.h
    Clean up. This include should have been removed by
    commit 23826c7aeac7 ("xprtrdma: Serialize credit accounting again").
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.sharma@broadcom.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprtrdma/verbs.c
+++ net/sunrpc/xprtrdma/verbs.c
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/interrupt.h>
 #include <linux/slab.h>
-#include <linux/prefetch.h>
 #include <linux/sunrpc/addr.h>
 #include <linux/sunrpc/svc_rdma.h>
 
