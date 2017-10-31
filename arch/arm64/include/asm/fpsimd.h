348269d1926ada23370976fb822e11ea96f399ed
Dave Martin <Dave.Martin@arm.com>
2017-10-31 15:51:08 +0000

    arm64/sve: Backend logic for setting the vector length
    This patch implements the core logic for changing a task's vector
    length on request from userspace.  This will be used by the ptrace
    and prctl frontends that are implemented in later patches.
    
    The SVE architecture permits, but does not require, implementations
    to support vector lengths that are not a power of two.  To handle
    this, logic is added to check a requested vector length against a
    possibly sparse bitmap of available vector lengths at runtime, so
    that the best supported value can be chosen.
    
    Signed-off-by: Dave Martin <Dave.Martin@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/fpsimd.h
+++ arch/arm64/include/asm/fpsimd.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
+#include <linux/cache.h>
 #include <linux/stddef.h>
 
 /*
@@ -71,10 +72,15 @@ extern void fpsimd_update_current_state(struct fpsimd_state *state);
 
 extern void fpsimd_flush_task_state(struct task_struct *target);
 
+/* Maximum VL that SVE VL-agnostic software can transparently support */
+#define SVE_VL_ARCH_MAX 0x100
+
 extern void sve_save_state(void *state, u32 *pfpsr);
 extern void sve_load_state(void const *state, u32 const *pfpsr,
 			   unsigned long vq_minus_1);
 extern unsigned int sve_get_vl(void);
+
+extern int __ro_after_init sve_max_vl;
 
 #ifdef CONFIG_ARM64_SVE
 
@@ -82,6 +88,8 @@ extern size_t sve_state_size(struct task_struct const *task);
 
 extern void sve_alloc(struct task_struct *task);
 extern void fpsimd_release_task(struct task_struct *task);
+extern int sve_set_vector_length(struct task_struct *task,
+				 unsigned long vl, unsigned long flags);
 
 #else /* ! CONFIG_ARM64_SVE */
 
