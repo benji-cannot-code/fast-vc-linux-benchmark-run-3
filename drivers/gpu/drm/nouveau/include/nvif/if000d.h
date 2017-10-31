1ace4ecf6acb6daa67fac27d77d5353f0bb90547
Ben Skeggs <bskeggs@redhat.com>
2017-11-01 03:56:19 +1000

    drm/nouveau/mmu/nv04: implement new vmm backend
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvif/if000d.h
+++ drivers/gpu/drm/nouveau/include/nvif/if000d.h
@@ -6,4 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct nv04_vmm_vn {
 	/* nvif_vmm_vX ... */
 };
+
+struct nv04_vmm_map_vn {
+	/* nvif_vmm_map_vX ... */
+};
 #endif
