e384abcdb55f85413f363a461b0bda6a9116d92d
Ben Skeggs <bskeggs@redhat.com>
2017-11-01 03:56:19 +1000

    drm/nouveau/mmu/nv04-nv4x: move global vmm to nvkm_mmu
    In a future commit, this will be constructed by common code.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/mmu.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/mmu.h
@@ -59,6 +59,8 @@ struct nvkm_mmu {
 	u64 limit;
 	u8  dma_bits;
 	u8  lpg_shift;
+
+	struct nvkm_vmm *vmm;
 };
 
 int nv04_mmu_new(struct nvkm_device *, int, struct nvkm_mmu **);
--- drivers/gpu/drm/nouveau/nvkm/subdev/mmu/nv04.h
+++ drivers/gpu/drm/nouveau/nvkm/subdev/mmu/nv04.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nv04_mmu {
 	struct nvkm_mmu base;
-	struct nvkm_vm *vm;
 	dma_addr_t null;
 	void *nullp;
 };
