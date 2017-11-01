e094fd176aefa529ecb7d46f7c64715cae70d3c6
Liviu Dudau <Liviu.Dudau@arm.com>
2017-11-01 14:04:36 +0000

    drm: Spelling fixes
    Minor spelling fix for 'monster' and replace 'on' with 'own' in
    comments.
    
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171101140436.2743-1-Liviu.Dudau@arm.com

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -1809,7 +1809,7 @@ int drm_atomic_debugfs_init(struct drm_minor *minor)
 #endif
 
 /*
- * The big monstor ioctl
+ * The big monster ioctl
  */
 
 static struct drm_pending_vblank_event *create_vblank_event(
