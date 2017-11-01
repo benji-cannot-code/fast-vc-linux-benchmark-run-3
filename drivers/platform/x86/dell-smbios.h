3c48b136a007de57481b20e8aeda9a44eed254de
Mario Limonciello <mario.limonciello@dell.com>
2017-11-01 14:25:34 -0500

    platform/x86: dell-smbios: Add filtering support
    When a userspace interface is introduced to dell-smbios filtering
    support will be used to make sure that userspace doesn't make calls
    deemed unsafe or that can cause the kernel drivers to get out of
    sync.
    
    A blacklist is provided for the following:
    - Items that are in use by other kernel drivers
    - Items that are deemed unsafe (diagnostics, write-once, etc)
    - Any items in the blacklist will be rejected.
    
    Following that a whitelist is provided as follows:
    - Each item has an associated capability.  If a userspace interface
      accesses this item, that capability will be tested to filter
      the request.
    - If the process provides CAP_SYS_RAWIO the whitelist will be
      overridden.
    
    When an item is not in the blacklist, or whitelist and the process
    is run with insufficient capabilities the call will be rejected.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@dell.com>
    Reviewed-by: Edward O'Callaghan <quasisec@google.com>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- drivers/platform/x86/dell-smbios.h
+++ drivers/platform/x86/dell-smbios.h
@@ -27,9 +27,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SELECT_TOKEN_AC 2
 #define CLASS_KBD_BACKLIGHT 4
 #define SELECT_KBD_BACKLIGHT 11
+#define CLASS_FLASH_INTERFACE 7
+#define SELECT_FLASH_INTERFACE 3
+#define CLASS_ADMIN_PROP 10
+#define SELECT_ADMIN_PROP 3
 #define CLASS_INFO 17
 #define SELECT_RFKILL 11
 #define SELECT_APP_REGISTRATION	3
+#define SELECT_DOCK 22
 
 /* Tokens used in kernel drivers, any of these
  * should be filtered from userspace access
@@ -45,6 +50,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KBD_LED_AUTO_100_TOKEN	0x02F6
 #define GLOBAL_MIC_MUTE_ENABLE	0x0364
 #define GLOBAL_MIC_MUTE_DISABLE	0x0365
+
+/* tokens whitelisted to userspace use */
+#define CAPSULE_EN_TOKEN	0x0461
+#define CAPSULE_DIS_TOKEN	0x0462
 #define WSMT_EN_TOKEN		0x04EC
 #define WSMT_DIS_TOKEN		0x04ED
 
@@ -81,6 +90,8 @@ int dell_smbios_register_device(struct device *d, void *call_fn);
 void dell_smbios_unregister_device(struct device *d);
 
 int dell_smbios_error(int value);
+int dell_smbios_call_filter(struct device *d,
+	struct calling_interface_buffer *buffer);
 int dell_smbios_call(struct calling_interface_buffer *buffer);
 
 struct calling_interface_token *dell_smbios_find_token(int tokenid);
