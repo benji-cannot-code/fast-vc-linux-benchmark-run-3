b87de3cbd4d0cca4c7c7eff494dd2153d226428a
Amir Goldstein <amir73il@gmail.com>
2017-11-01 10:13:51 +0200

    ovl: cleanup temp index entries
    A previous failed attempt to create or whiteout a directory index may
    leave index entries named '#%x' in the index dir. Cleanup those temp
    entries on mount instead of failing the mount.
    
    In the future, we may drop 'work' dir and use 'index' dir instead.
    This change is enough for cleaning up copy up leftovers 'from the future',
    but it is not enough for cleaning up rmdir leftovers 'from the future'
    (i.e. temp dir containing whiteouts).
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/namei.c
+++ fs/overlayfs/namei.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/fs.h>
 #include <linux/cred.h>
+#include <linux/ctype.h>
 #include <linux/namei.h>
 #include <linux/xattr.h>
 #include <linux/ratelimit.h>
@@ -468,6 +469,12 @@ static struct dentry *ovl_index_upper(struct ovl_fs *ofs, struct dentry *index)
 	return upper;
 }
 
+/* Is this a leftover from create/whiteout of directory index entry? */
+static bool ovl_is_temp_index(struct dentry *index)
+{
+	return index->d_name.name[0] == '#';
+}
+
 /*
  * Verify that an index entry name matches the origin file handle stored in
  * OVL_XATTR_ORIGIN and that origin file handle can be decoded to lower path.
@@ -484,6 +491,11 @@ int ovl_verify_index(struct ovl_fs *ofs, struct dentry *index)
 
 	if (!d_inode(index))
 		return 0;
+
+	/* Cleanup leftover from index create/cleanup attempt */
+	err = -ESTALE;
+	if (ovl_is_temp_index(index))
+		goto fail;
 
 	err = -EINVAL;
 	if (index->d_name.len < sizeof(struct ovl_fh)*2)
