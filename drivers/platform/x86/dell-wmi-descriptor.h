7977472bfe9763a24b7bb5d7f1d449aad1c0270b
Mario Limonciello <mario.limonciello@dell.com>
2017-11-01 14:25:27 -0500

    platform/x86: dell-wmi-descriptor: split WMI descriptor into it's own driver
    All communication on individual GUIDs should occur in separate drivers.
    Allowing a driver to communicate with the bus to another GUID is just
    a hack that discourages drivers to adopt the bus model.
    
    The information found from the WMI descriptor driver is now exported
    for use by other drivers.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@dell.com>
    Reviewed-by: Edward O'Callaghan <quasisec@google.com>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

---
+++ drivers/platform/x86/dell-wmi-descriptor.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ *  Dell WMI descriptor driver
+ *
+ *  Copyright (c) 2017 Dell Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ */
+
+#ifndef _DELL_WMI_DESCRIPTOR_H_
+#define _DELL_WMI_DESCRIPTOR_H_
+
+#include <linux/wmi.h>
+
+#define DELL_WMI_DESCRIPTOR_GUID "8D9DDCBC-A997-11DA-B012-B622A1EF5492"
+
+bool dell_wmi_get_interface_version(u32 *version);
+bool dell_wmi_get_size(u32 *size);
+
+#endif
