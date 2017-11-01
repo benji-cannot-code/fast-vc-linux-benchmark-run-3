1f35bcb1cd3f74476bb1d0efe92ab040b04d9747
Mario Limonciello <mario.limonciello@dell.com>
2017-11-01 14:25:36 -0500

    platform/x86: dell-smbios-wmi: introduce userspace interface
    It's important for the driver to provide a R/W ioctl to ensure that
    two competing userspace processes don't race to provide or read each
    others data.
    
    This userspace character device will be used to perform SMBIOS calls
    from any applications.
    
    It provides an ioctl that will allow passing the WMI calling
    interface buffer between userspace and kernel space.
    
    This character device is intended to deprecate the dcdbas kernel module
    and the interface that it provides to userspace.
    
    To perform an SMBIOS IOCTL call using the character device userspace will
    perform a read() on the the character device.  The WMI bus will provide
    a u64 variable containing the necessary size of the IOCTL buffer.
    
    The API for interacting with this interface is defined in documentation
    as well as the WMI uapi header provides the format of the structures.
    
    Not all userspace requests will be accepted.  The dell-smbios filtering
    functionality will be used to prevent access to certain tokens and calls.
    
    All whitelisted commands and tokens are now shared out to userspace so
    applications don't need to define them in their own headers.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@dell.com>
    Reviewed-by: Edward O'Callaghan <quasisec@google.com>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- drivers/platform/x86/dell-smbios.h
+++ drivers/platform/x86/dell-smbios.h
@@ -18,23 +18,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _DELL_SMBIOS_H_
 
 #include <linux/device.h>
+#include <uapi/linux/wmi.h>
 
-/* Classes and selects used in kernel drivers */
-#define CLASS_TOKEN_READ 0
-#define CLASS_TOKEN_WRITE 1
-#define SELECT_TOKEN_STD 0
-#define SELECT_TOKEN_BAT 1
-#define SELECT_TOKEN_AC 2
+/* Classes and selects used only in kernel drivers */
 #define CLASS_KBD_BACKLIGHT 4
 #define SELECT_KBD_BACKLIGHT 11
-#define CLASS_FLASH_INTERFACE 7
-#define SELECT_FLASH_INTERFACE 3
-#define CLASS_ADMIN_PROP 10
-#define SELECT_ADMIN_PROP 3
-#define CLASS_INFO 17
-#define SELECT_RFKILL 11
-#define SELECT_APP_REGISTRATION	3
-#define SELECT_DOCK 22
 
 /* Tokens used in kernel drivers, any of these
  * should be filtered from userspace access
@@ -51,23 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GLOBAL_MIC_MUTE_ENABLE	0x0364
 #define GLOBAL_MIC_MUTE_DISABLE	0x0365
 
-/* tokens whitelisted to userspace use */
-#define CAPSULE_EN_TOKEN	0x0461
-#define CAPSULE_DIS_TOKEN	0x0462
-#define WSMT_EN_TOKEN		0x04EC
-#define WSMT_DIS_TOKEN		0x04ED
-
 struct notifier_block;
-
-/* This structure will be modified by the firmware when we enter
- * system management mode, hence the volatiles */
-
-struct calling_interface_buffer {
-	u16 cmd_class;
-	u16 cmd_select;
-	volatile u32 input[4];
-	volatile u32 output[4];
-} __packed;
 
 struct calling_interface_token {
 	u16 tokenID;
