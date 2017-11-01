0c878055edb7674906e8a8395a9e08ec8b144ae5
Yisheng Xie <xieyisheng1@huawei.com>
2017-11-01 17:46:19 +0800

    arm64: suspend: remove useless included file
    After commit 9e8e865bbe29 ("arm64: unify idmap removal"), we no need to
    flush tlb in suspend.c, so the included file tlbflush.h can be removed.
    
    Signed-off-by: Yisheng Xie <xieyisheng1@huawei.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/suspend.c
+++ arch/arm64/kernel/suspend.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mmu_context.h>
 #include <asm/smp_plat.h>
 #include <asm/suspend.h>
-#include <asm/tlbflush.h>
 
 /*
  * This is allocated by cpu_suspend_init(), and used to store a pointer to
