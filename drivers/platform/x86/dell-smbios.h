efc821bb901a22716786209168b3f90d18f9a8ab
Mario Limonciello <mario.limonciello@dell.com>
2017-11-01 14:25:33 -0500

    platform/x86: dell-smbios-smm: test for WSMT
    WSMT is as an attestation to the OS that the platform won't
    modify memory outside of pre-defined areas.
    
    If a platform has WSMT enabled in BIOS setup, SMM calls through
    dcdbas will fail.  The only way to access platform data in these
    instances is through the WMI SMBIOS calling interface.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@dell.com>
    Reviewed-by: Edward O'Callaghan <quasisec@google.com>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- drivers/platform/x86/dell-smbios.h
+++ drivers/platform/x86/dell-smbios.h
@@ -45,6 +45,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KBD_LED_AUTO_100_TOKEN	0x02F6
 #define GLOBAL_MIC_MUTE_ENABLE	0x0364
 #define GLOBAL_MIC_MUTE_DISABLE	0x0365
+#define WSMT_EN_TOKEN		0x04EC
+#define WSMT_DIS_TOKEN		0x04ED
 
 struct notifier_block;
 
