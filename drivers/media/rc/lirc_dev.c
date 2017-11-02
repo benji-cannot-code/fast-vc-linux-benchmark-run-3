3965e6e00fbda7be0d80a637d057a3d514c6d0a3
Sean Young <sean@mess.org>
2017-11-02 16:44:21 -0400

    media: rc: include <uapi/linux/lirc.h> rather than <media/lirc.h>
    This removes the need for include/media/lirc.h, which just includes
    the uapi file.
    
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/rc/lirc_dev.c
+++ drivers/media/rc/lirc_dev.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/wait.h>
 
 #include "rc-core-priv.h"
-#include <media/lirc.h>
+#include <uapi/linux/lirc.h>
 
 #define LOGHEAD		"lirc_dev (%s[%d]): "
 #define LIRCBUF_SIZE	256
