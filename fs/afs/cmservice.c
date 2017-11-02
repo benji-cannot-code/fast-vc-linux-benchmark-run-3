fa4e1e4b3486f77fa89c5ad5d7cb3268124f2923
David Howells <dhowells@redhat.com>
2017-11-02 15:27:49 +0000

    afs: Rename struct afs_call server member to cm_server
    Rename the server member of struct afs_call to cm_server as we're only
    going to be using it for incoming calls for the Cache Manager service.
    This makes it easier to differentiate from the pointer to the target server
    for the client, which will point to a different structure to allow for
    callback handling.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/afs/cmservice.c
+++ fs/afs/cmservice.c
@@ -145,12 +145,10 @@ static void afs_cm_destructor(struct afs_call *call)
 	 * afs_deliver_cb_callback().
 	 */
 	if (call->unmarshall == 5) {
-		ASSERT(call->server && call->count && call->request);
-		afs_break_callbacks(call->server, call->count, call->request);
+		ASSERT(call->cm_server && call->count && call->request);
+		afs_break_callbacks(call->cm_server, call->count, call->request);
 	}
 
-	afs_put_server(call->net, call->server);
-	call->server = NULL;
 	kfree(call->buffer);
 	call->buffer = NULL;
 }
@@ -171,7 +169,7 @@ static void SRXAFSCB_CallBack(struct work_struct *work)
 	 * yet */
 	afs_send_empty_reply(call);
 
-	afs_break_callbacks(call->server, call->count, call->request);
+	afs_break_callbacks(call->cm_server, call->count, call->request);
 	afs_put_call(call);
 	_leave("");
 }
@@ -291,7 +289,7 @@ static int afs_deliver_cb_callback(struct afs_call *call)
 	server = afs_find_server(call->net, &srx);
 	if (!server)
 		return -ENOTCONN;
-	call->server = server;
+	call->cm_server = server;
 
 	return afs_queue_call_work(call);
 }
@@ -303,9 +301,9 @@ static void SRXAFSCB_InitCallBackState(struct work_struct *work)
 {
 	struct afs_call *call = container_of(work, struct afs_call, work);
 
-	_enter("{%p}", call->server);
+	_enter("{%p}", call->cm_server);
 
-	afs_init_callback_state(call->server);
+	afs_init_callback_state(call->cm_server);
 	afs_send_empty_reply(call);
 	afs_put_call(call);
 	_leave("");
@@ -336,7 +334,7 @@ static int afs_deliver_cb_init_call_back_state(struct afs_call *call)
 	server = afs_find_server(call->net, &srx);
 	if (!server)
 		return -ENOTCONN;
-	call->server = server;
+	call->cm_server = server;
 
 	return afs_queue_call_work(call);
 }
@@ -408,7 +406,7 @@ static int afs_deliver_cb_init_call_back_state3(struct afs_call *call)
 	server = afs_find_server(call->net, &srx);
 	if (!server)
 		return -ENOTCONN;
-	call->server = server;
+	call->cm_server = server;
 
 	return afs_queue_call_work(call);
 }
