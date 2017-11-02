431908d3cee7e3a56fdbf5e8073aa239ef7348a3
Sébastien Szymanski <sebastien.szymanski@armadeus.com>
2017-11-02 12:12:42 +0100

    HID: cp2112: fix interface specification URL
    Signed-off-by: Sébastien Szymanski <sebastien.szymanski@armadeus.com>
    Acked-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-cp2112.c
+++ drivers/hid/hid-cp2112.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Data Sheet:
  *   http://www.silabs.com/Support%20Documents/TechnicalDocs/CP2112.pdf
  * Programming Interface Specification:
- *   http://www.silabs.com/Support%20Documents/TechnicalDocs/AN495.pdf
+ *   https://www.silabs.com/documents/public/application-notes/an495-cp2112-interface-specification.pdf
  */
 
 #include <linux/gpio.h>
