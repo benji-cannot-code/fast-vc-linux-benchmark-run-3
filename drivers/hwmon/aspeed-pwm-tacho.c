5a23c87364c3c932c74f7c6616cad21bf214a2d4
Joel <joel@jms.id.au>
2017-11-02 14:53:47 +1100

    hwmon: (aspeed-pwm-tacho) Sort headers
    Sort the headers in preperation for future changes.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/aspeed-pwm-tacho.c
+++ drivers/hwmon/aspeed-pwm-tacho.c
@@ -8,19 +8,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/clk.h>
+#include <linux/delay.h>
 #include <linux/errno.h>
 #include <linux/gpio/consumer.h>
-#include <linux/delay.h>
 #include <linux/hwmon.h>
 #include <linux/hwmon-sysfs.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/of_platform.h>
 #include <linux/of_device.h>
+#include <linux/of_platform.h>
 #include <linux/platform_device.h>
-#include <linux/sysfs.h>
 #include <linux/regmap.h>
+#include <linux/sysfs.h>
 #include <linux/thermal.h>
 
 /* ASPEED PWM & FAN Tach Register Definition */
