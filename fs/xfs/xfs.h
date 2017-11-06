341d4e3e816be799f0fbff77e86fde2d51e869ca
Christoph Hellwig <hch@lst.de>
2017-11-06 11:53:58 -0800

    xfs: always define STATIC to static noinline
    Ever since we added the noinline tag there is no good reason to define
    away the static for debug builds - we'll get just as good debug
    information with our without it, so don't mess up sparse and other
    checkers due to it.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs.h
+++ fs/xfs/xfs.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __XFS_H__
 
 #ifdef CONFIG_XFS_DEBUG
-#define STATIC
 #define DEBUG 1
 #define XFS_BUF_LOCK_TRACKING 1
 #endif
