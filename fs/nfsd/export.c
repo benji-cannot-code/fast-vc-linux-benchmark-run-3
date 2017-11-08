653cb5b8cdac5aa968a862d6a8b6ab2500886a24
Vasily Averin <vvs@virtuozzo.com>
2017-11-08 08:55:22 +0300

    nfsd: remove net pointer from debug messages
    Publishing of net pointer is not safe,
    replace it in debug meesages by net->ns.inum
    
    [  119.989161] nfsd: initializing export module (net: f00001e7).
    [  171.767188] NFSD: starting 90-second grace period (net f00001e7)
    [  322.185240] nfsd: shutting down export module (net: f00001e7).
    [  322.186062] nfsd: export shutdown complete (net: f00001e7).
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/export.c
+++ fs/nfsd/export.c
@@ -1231,7 +1231,7 @@ nfsd_export_init(struct net *net)
 	int rv;
 	struct nfsd_net *nn = net_generic(net, nfsd_net_id);
 
-	dprintk("nfsd: initializing export module (net: %p).\n", net);
+	dprintk("nfsd: initializing export module (net: %x).\n", net->ns.inum);
 
 	nn->svc_export_cache = cache_create_net(&svc_export_cache_template, net);
 	if (IS_ERR(nn->svc_export_cache))
@@ -1279,7 +1279,7 @@ nfsd_export_shutdown(struct net *net)
 {
 	struct nfsd_net *nn = net_generic(net, nfsd_net_id);
 
-	dprintk("nfsd: shutting down export module (net: %p).\n", net);
+	dprintk("nfsd: shutting down export module (net: %x).\n", net->ns.inum);
 
 	cache_unregister_net(nn->svc_expkey_cache, net);
 	cache_unregister_net(nn->svc_export_cache, net);
@@ -1287,5 +1287,5 @@ nfsd_export_shutdown(struct net *net)
 	cache_destroy_net(nn->svc_export_cache, net);
 	svcauth_unix_purge(net);
 
-	dprintk("nfsd: export shutdown complete (net: %p).\n", net);
+	dprintk("nfsd: export shutdown complete (net: %x).\n", net->ns.inum);
 }
