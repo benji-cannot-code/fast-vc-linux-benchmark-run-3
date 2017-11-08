320df08ef87d4eafe325f888fd250b57e89db0a3
Ji-Ze Hong (Peter Hong) <hpeter@gmail.com>
2017-11-08 11:05:46 +0800

    serial: 8250_fintek: Fix crash with baud rate B0
    The 8250_fintek.c is support the Fintek F81866/F81216 with dynamic clock.
    But It'll generate "division by zero" exception and crash in
    fintek_8250_set_termios() with baud rate 0 on baudrate_table[i] % baud.
    
    It can be tested with following C code:
    
    	...
    	struct termios options;
    
    	tcgetattr(fd, &options);
    	...
    	options.c_cflag = CS8 | CREAD; /* baud rate 0 */
    	tcsetattr(fd, TCSANOW, &options);
    	tcflush(fd, TCIOFLUSH);
    
    Fixes: 195638b6d44f ("serial: 8250_fintek: UART dynamic clocksource on Fintek F81866")
    Reported-by: Lukas Redlinger <rel+kernel@agilox.net>
    Cc: Lukas Redlinger <rel+kernel@agilox.net>
    Signed-off-by: Ji-Ze Hong (Peter Hong) <hpeter+linux_kernel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/8250/8250_fintek.c
+++ drivers/tty/serial/8250/8250_fintek.c
@@ -316,6 +316,13 @@ void fintek_8250_set_termios(struct uart_port *port, struct ktermios *termios,
 			F81866_UART_CLK_14_769MHZ, F81866_UART_CLK_18_432MHZ,
 			F81866_UART_CLK_24MHZ };
 
+	/*
+	 * We'll use serial8250_do_set_termios() for baud = 0, otherwise It'll
+	 * crash on baudrate_table[i] % baud with "division by zero".
+	 */
+	if (!baud)
+		goto exit;
+
 	switch (pdata->pid) {
 	case CHIP_ID_F81216H:
 		reg = RS485;
@@ -328,8 +335,7 @@ void fintek_8250_set_termios(struct uart_port *port, struct ktermios *termios,
 		dev_warn(port->dev,
 			"%s: pid: %x Not support. use default set_termios.\n",
 			__func__, pdata->pid);
-		serial8250_do_set_termios(port, termios, old);
-		return;
+		goto exit;
 	}
 
 	for (i = 0; i < ARRAY_SIZE(baudrate_table); ++i) {
@@ -357,6 +363,7 @@ void fintek_8250_set_termios(struct uart_port *port, struct ktermios *termios,
 		tty_termios_encode_baud_rate(termios, baud, baud);
 	}
 
+exit:
 	serial8250_do_set_termios(port, termios, old);
 }
 
