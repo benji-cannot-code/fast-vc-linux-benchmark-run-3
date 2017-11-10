9321ba254c0773ac93bb0108f5dd7474cb05ec57
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-11-10 15:40:28 +0200

    gpiolib: acpi: Assign polarity when call acpi_populate_gpio_lookup()
    There is no need, since we preserve firmware settings, to override
    polarity for GpioInt() resources.
    
    While Documentation/gpio-properties.txt refers to any from GpioIo() /
    GpioInt() resources, the active_low flag has been introduced to fill the
    gap only for GpioIo() which lacks of that information.
    
    Moreover, in case of GpioInt() existed solution was broken anyway, it
    overrides only in one direction, i.e. from 0 to 1, otherwise it would be
    still 1 as defined in the resource macro.
    
    So, move the assignment to a right place and forbid to (semi-)override
    polarity for GpioInt() type of resources.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-acpi.c
+++ drivers/gpio/gpiolib-acpi.c
@@ -532,8 +532,8 @@ static int acpi_populate_gpio_lookup(struct acpi_resource *ares, void *data)
 			lookup->info.triggering = agpio->triggering;
 		} else {
 			lookup->info.flags = acpi_gpio_to_gpiod_flags(agpio);
+			lookup->info.polarity = lookup->active_low;
 		}
-
 	}
 
 	return 1;
@@ -558,11 +558,8 @@ static int acpi_gpio_resource_lookup(struct acpi_gpio_lookup *lookup,
 	if (!lookup->desc)
 		return -ENOENT;
 
-	if (info) {
+	if (info)
 		*info = lookup->info;
-		if (lookup->active_low)
-			info->polarity = lookup->active_low;
-	}
 	return 0;
 }
 
