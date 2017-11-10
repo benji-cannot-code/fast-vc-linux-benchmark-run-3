c76d5e5d8c00fb4712ca4163974dc9bd98a21115
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2017-11-10 15:40:29 +0200

    gpiolib: acpi: Don't contaminate return parameter in case of error
    If error occurs, leave lookup parameter untouched.
    
    There is no functional change, since all current callers just bail out
    in case of error without using the assigned pieces.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-acpi.c
+++ drivers/gpio/gpiolib-acpi.c
@@ -586,7 +586,6 @@ static int acpi_gpio_property_lookup(struct fwnode_handle *fwnode,
 	 * The property was found and resolved, so need to lookup the GPIO based
 	 * on returned args.
 	 */
-	lookup->adev = args.adev;
 	if (args.nargs != 3)
 		return -EPROTO;
 
@@ -594,6 +593,7 @@ static int acpi_gpio_property_lookup(struct fwnode_handle *fwnode,
 	lookup->pin_index = args.args[1];
 	lookup->active_low = !!args.args[2];
 
+	lookup->adev = args.adev;
 	return 0;
 }
 
