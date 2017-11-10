3137f93d11c6c5a00e69a52db1b2a5755e0c97c4
oulijun <oulijun@huawei.com>
2017-11-10 16:55:53 +0800

    RDMA/hns: Modify the usage of cmd_sn in hip08
    The cmd_sn field of CQ doorbell inits for 0. It should be
    increment on each first db rung after a completion Event.
    if the cmd_sn of notify doorbell Adjacent two times is the
    same, the hardware will distinguish it for the same notify
    request and update its type according to the priority level
    of next event and solicited event.
    
    Signed-off-by: Lijun Ou <oulijun@huawei.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hns/hns_roce_cq.c
+++ drivers/infiniband/hw/hns/hns_roce_cq.c
@@ -157,6 +157,7 @@ static int hns_roce_cq_alloc(struct hns_roce_dev *hr_dev, int nent,
 	}
 
 	hr_cq->cons_index = 0;
+	hr_cq->arm_sn = 1;
 	hr_cq->uar = hr_uar;
 
 	atomic_set(&hr_cq->refcount, 1);
@@ -457,6 +458,7 @@ void hns_roce_cq_completion(struct hns_roce_dev *hr_dev, u32 cqn)
 		return;
 	}
 
+	++cq->arm_sn;
 	cq->comp(cq);
 }
 
