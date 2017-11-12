59ed8da5be02f9ebbfc0f48758b9b6162f610985
Vasily Averin <vvs@virtuozzo.com>
2017-11-12 22:30:01 +0300

    fib_rules: exit_net cleanup check added
    Be sure that rules_ops list initialized in net_init hook was return
    to initial state.
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -1023,8 +1023,14 @@ static int __net_init fib_rules_net_init(struct net *net)
 	return 0;
 }
 
+static void __net_exit fib_rules_net_exit(struct net *net)
+{
+	WARN_ON_ONCE(!list_empty(&net->rules_ops));
+}
+
 static struct pernet_operations fib_rules_net_ops = {
 	.init = fib_rules_net_init,
+	.exit = fib_rules_net_exit,
 };
 
 static int __init fib_rules_init(void)
