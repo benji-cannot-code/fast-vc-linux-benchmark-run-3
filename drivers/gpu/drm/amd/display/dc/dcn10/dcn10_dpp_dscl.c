0b634b14f1224b6785535e719a8f771ed948fdf5
Yongqiang Sun <yongqiang.sun@amd.com>
2017-11-13 17:22:17 -0500

    drm/amd/display: Only program watermark for full update.
    For scaling and position change, it isn't necessary to program
    watermark and check P-State as well.
    
    Signed-off-by: Yongqiang Sun <yongqiang.sun@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_dpp_dscl.c
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_dpp_dscl.c
@@ -652,6 +652,8 @@ void dpp1_dscl_set_scaler_manual_scale(
 	if (memcmp(&dpp->scl_data, scl_data, sizeof(*scl_data)) == 0)
 		return;
 
+	PERF_TRACE();
+
 	dpp->scl_data = *scl_data;
 
 	/* Recout */
@@ -705,4 +707,5 @@ void dpp1_dscl_set_scaler_manual_scale(
 		SCL_H_NUM_TAPS_C, scl_data->taps.h_taps_c - 1);
 
 	dpp1_dscl_set_scl_filter(dpp, scl_data, ycbcr);
+	PERF_TRACE();
 }
