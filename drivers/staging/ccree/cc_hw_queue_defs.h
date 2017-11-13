424057b1bd2595189b5d496cfc7fdb314ab6d6d6
Gilad Ben-Yossef <gilad@benyossef.com>
2017-11-13 14:45:52 +0000

    staging: ccree: drop unused macro
    The CC_REG_NAME macro is unused. Drop it.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/cc_hw_queue_defs.h
+++ drivers/staging/ccree/cc_hw_queue_defs.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Define max. available slots in HW queue */
 #define HW_QUEUE_SLOTS_MAX              15
 
-#define CC_REG_NAME(word, name) DX_DSCRPTR_QUEUE_WORD ## word ## _ ## name
-
 #define CC_REG_LOW(word, name)  \
 	(DX_DSCRPTR_QUEUE_WORD ## word ## _ ## name ## _BIT_SHIFT)
 
