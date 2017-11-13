c43bcc12e002d91436938799ca0978a1758e4623
Yonatan Cohen <yonatanc@mellanox.com>
2017-11-13 10:51:14 +0200

    IB/mlx4: Exposing modify CQ callback to uverbs layer
    Exposed mlx4_ib_modify_cq to be called from ib device
    verb list.
    
    Signed-off-by: Yonatan Cohen <yonatanc@mellanox.com>
    Reviewed-by: Majd Dibbiny <majd@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/mlx4/mlx4_ib.h
+++ drivers/infiniband/hw/mlx4/mlx4_ib.h
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mlx4/device.h>
 #include <linux/mlx4/doorbell.h>
 #include <linux/mlx4/qp.h>
+#include <linux/mlx4/cq.h>
 
 #define MLX4_IB_DRV_NAME	"mlx4_ib"
 
