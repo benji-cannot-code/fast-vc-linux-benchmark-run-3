338087a7f9ff3a546cb5baa0af8472cceeb48cde
Rodrigo Vivi <rodrigo.vivi@intel.com>
2017-11-14 11:47:57 -0800

    drm/i915/cnl: Don't blindly replace qdiv.
    Accordingly to spec "If Kdiv != 2, then Qdiv must be 1."
    but we already handle qdiv values properly and this case here
    should be spurious. But instead of blindly replacing let's
    warn loudly instead. Because it means something was really
    wrong on initial setup.
    
    Cc: Mika Kahola <mika.kahola@intel.com>
    Cc: Manasi Navare <manasi.d.navare@intel.com>
    Cc: James Ausmus <james.ausmus@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Manasi Navare <manasi.d.navare@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171114194759.24541-6-rodrigo.vivi@intel.com

--- drivers/gpu/drm/i915/intel_dpll_mgr.c
+++ drivers/gpu/drm/i915/intel_dpll_mgr.c
@@ -2185,8 +2185,7 @@ static void cnl_wrpll_params_populate(struct skl_wrpll_params *params,
 		WARN(1, "Incorrect PDiv\n");
 	}
 
-	if (kdiv != 2)
-		qdiv = 1;
+	WARN_ON(kdiv != 2 && qdiv != 1);
 
 	params->qdiv_ratio = qdiv;
 	params->qdiv_mode = (qdiv == 1) ? 0 : 1;
