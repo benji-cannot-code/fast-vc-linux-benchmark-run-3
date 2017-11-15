fc77b4cdc696d08e7d91fa65ce6a045a3101bcbd
Feifei Xu <Feifei.Xu@amd.com>
2017-11-15 16:01:30 +0800

    drm/amd/include:cleanup vega10 sdma0/1 header files.
    To remove include/asic_reg/vega10 folder,create IP folders sdma0/1.
    This patch cleanup asic_reg/vega10/SDMA folders.
    
    Signed-off-by: Feifei Xu <Feifei.Xu@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/psp_v3_1.c
+++ drivers/gpu/drm/amd/amdgpu/psp_v3_1.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vega10/MP/mp_9_0_offset.h"
 #include "vega10/MP/mp_9_0_sh_mask.h"
 #include "vega10/GC/gc_9_0_offset.h"
-#include "vega10/SDMA0/sdma0_4_0_offset.h"
+#include "sdma0/sdma0_4_0_offset.h"
 #include "vega10/NBIO/nbio_6_1_offset.h"
 
 MODULE_FIRMWARE("amdgpu/vega10_sos.bin");
