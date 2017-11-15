5e3ce2df51d38db08b0115cd7bf4a3f49a76fea2
Mirza Krak <mirza.krak@endian.se>
2017-11-15 08:24:46 +0000

    drm/rockchip: dw-mipi-dsi: fix possible un-balanced runtime PM enable
    In the case where the bind gets deferred we would end up with a
    un-balanced runtime PM enable call.
    
    Fix this by simply moving the pm_runtime_enable call to the end of
    the bind function when all paths have succeeded.
    
    Signed-off-by: Mirza Krak <mirza.krak@endian.se>
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1510734286-37434-1-git-send-email-mirza.krak@endian.se

--- drivers/gpu/drm/rockchip/dw-mipi-dsi.c
+++ drivers/gpu/drm/rockchip/dw-mipi-dsi.c
@@ -1276,8 +1276,6 @@ static int dw_mipi_dsi_bind(struct device *dev, struct device *master,
 		goto err_pllref;
 	}
 
-	pm_runtime_enable(dev);
-
 	dsi->dsi_host.ops = &dw_mipi_dsi_host_ops;
 	dsi->dsi_host.dev = dev;
 	ret = mipi_dsi_host_register(&dsi->dsi_host);
@@ -1292,6 +1290,7 @@ static int dw_mipi_dsi_bind(struct device *dev, struct device *master,
 	}
 
 	dev_set_drvdata(dev, dsi);
+	pm_runtime_enable(dev);
 	return 0;
 
 err_mipi_dsi_host:
