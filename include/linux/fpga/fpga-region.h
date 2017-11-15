821338214b60e56a92fd26d810842245dfe91165
Alan Tull <atull@kernel.org>
2017-11-15 14:20:23 -0600

    fpga: region: add register/unregister functions
    Another step in separating common code from device tree specific
    code for FPGA regions.
    
    * add FPGA region register/unregister functions.
    * add the register/unregister functions to the header
    * use devm_kzalloc to alloc the region.
    * add a method for getting bridges to the region struct
    * add priv to the region struct
    * use region->info in of_fpga_region_get_bridges
    
    Signed-off-by: Alan Tull <atull@kernel.org>
    Acked-by: Moritz Fischer <mdf@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/fpga/fpga-region.h
+++ include/linux/fpga/fpga-region.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @bridge_list: list of FPGA bridges specified in region
  * @mgr: FPGA manager
  * @info: FPGA image info
+ * @priv: private data
+ * @get_bridges: optional function to get bridges to a list
  */
 struct fpga_region {
 	struct device dev;
@@ -20,10 +22,15 @@ struct fpga_region {
 	struct list_head bridge_list;
 	struct fpga_manager *mgr;
 	struct fpga_image_info *info;
+	void *priv;
+	int (*get_bridges)(struct fpga_region *region);
 };
 
 #define to_fpga_region(d) container_of(d, struct fpga_region, dev)
 
 int fpga_region_program_fpga(struct fpga_region *region);
+
+int fpga_region_register(struct device *dev, struct fpga_region *region);
+int fpga_region_unregister(struct fpga_region *region);
 
 #endif /* _FPGA_REGION_H */
