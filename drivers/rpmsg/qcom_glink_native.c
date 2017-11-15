d100b9be9a9fa74d585764d2cc9349edc215993e
Bjorn Andersson <bjorn.andersson@linaro.org>
2017-11-14 21:45:34 -0800

    rpmsg: glink: Add missing MODULE_LICENSE
    The qcom_glink_native driver is missing a MODULE_LICENSE(), correct
    this.
    
    Fixes: 835764ddd9af ("rpmsg: glink: Move the common glink protocol implementation to glink_native.c")
    Cc: stable@vger.kernel.org
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1642,3 +1642,6 @@ void qcom_glink_native_unregister(struct qcom_glink *glink)
 	device_unregister(glink->dev);
 }
 EXPORT_SYMBOL_GPL(qcom_glink_native_unregister);
+
+MODULE_DESCRIPTION("Qualcomm GLINK driver");
+MODULE_LICENSE("GPL v2");
