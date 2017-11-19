159c1361958d1d9bddbd149e6171f0e2f0e2697f
Sven Eckelmann <sven@narfation.org>
2017-11-19 17:12:04 +0100

    batman-adv: Include net.h for net_ratelimited_function
    The linux/net.h provides the net_ratelimited_function. It should
    therefore be included directly before it is used.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/translation-table.c
+++ net/batman-adv/translation-table.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kref.h>
 #include <linux/list.h>
 #include <linux/lockdep.h>
+#include <linux/net.h>
 #include <linux/netdevice.h>
 #include <linux/netlink.h>
 #include <linux/rculist.h>
