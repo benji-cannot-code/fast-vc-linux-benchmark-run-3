9ce03bda3a936e02a9c3b53e33570f530243e8d6
Sven Eckelmann <sven@narfation.org>
2017-11-19 17:12:06 +0100

    batman-adv: Remove unused sched.h include
    The linux/wait.h include was removed with commit 421d988b2c08 ("batman-adv:
    Consolidate logging related functions"). The previously required (but not
    unused) linux/sched.h include can also be dropped now.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/debugfs.c
+++ net/batman-adv/debugfs.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/netdevice.h>
 #include <linux/printk.h>
-#include <linux/sched.h> /* for linux/wait.h */
 #include <linux/seq_file.h>
 #include <linux/stat.h>
 #include <linux/stddef.h>
