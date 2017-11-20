0e9a7e6304f786c2ab0b1f9ba8d2b5da34e8c429
Vasily Gorbik <gor@linux.ibm.com>
2017-11-20 12:17:19 +0100

    s390/dumpstack: disable __dump_trace kasan instrumentation
    Walking async_stack produces false positives. Disable __dump_trace
    function instrumentation for now.
    
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/dumpstack.c
+++ arch/s390/kernel/dumpstack.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The stack trace can start at any of the three stacks and can potentially
  * touch all of them. The order is: panic stack, async stack, sync stack.
  */
-static unsigned long
+static unsigned long __no_sanitize_address
 __dump_trace(dump_trace_func_t func, void *data, unsigned long sp,
 	     unsigned long low, unsigned long high)
 {
