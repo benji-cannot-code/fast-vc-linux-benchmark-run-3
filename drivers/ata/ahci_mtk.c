bc62f428c6e988e218ec664bdb5181560931c203
Albert Pool <albertpool@solcon.nl>
2017-11-20 14:20:09 +0100

    ata: mediatek: Fix typo in module description
    Signed-off-by: Albert Pool <albertpool@solcon.nl>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- drivers/ata/ahci_mtk.c
+++ drivers/ata/ahci_mtk.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * MeidaTek AHCI SATA driver
+ * MediaTek AHCI SATA driver
  *
  * Copyright (c) 2017 MediaTek Inc.
  * Author: Ryder Lee <ryder.lee@mediatek.com>
@@ -193,5 +193,5 @@ static struct platform_driver mtk_ahci_driver = {
 };
 module_platform_driver(mtk_ahci_driver);
 
-MODULE_DESCRIPTION("MeidaTek SATA AHCI Driver");
+MODULE_DESCRIPTION("MediaTek SATA AHCI Driver");
 MODULE_LICENSE("GPL v2");
