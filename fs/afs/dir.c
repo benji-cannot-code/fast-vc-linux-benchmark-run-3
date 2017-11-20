490569b0f40c076b10a54783ec5760c08bfeb93f
David Howells <dhowells@redhat.com>
2017-11-20 23:04:08 +0000

    afs: Fix some dentry handling in dir ops and missing key_puts
    Fix some of dentry handling in AFS directory ops:
    
     (1) Do d_drop() on the new_dentry before assigning a new inode to it in
         afs_vnode_new_inode().  It's fine to do this before calling afs_iget()
         because the operation has taken place on the server.
    
     (2) Replace d_instantiate()/d_rehash() with d_add().
    
     (3) Don't d_drop() the new_dentry in afs_rename() on error.
    
    Also fix afs_link() and afs_rename() to call key_put() on all error paths
    where the key is taken.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/afs/dir.c
+++ fs/afs/dir.c
@@ -766,6 +766,8 @@ static void afs_vnode_new_inode(struct afs_fs_cursor *fc,
 	if (fc->ac.error < 0)
 		return;
 
+	d_drop(new_dentry);
+
 	inode = afs_iget(fc->vnode->vfs_inode.i_sb, fc->key,
 			 newfid, newstatus, newcb, fc->cbi);
 	if (IS_ERR(inode)) {
@@ -776,9 +778,7 @@ static void afs_vnode_new_inode(struct afs_fs_cursor *fc,
 		return;
 	}
 
-	d_instantiate(new_dentry, inode);
-	if (d_unhashed(new_dentry))
-		d_rehash(new_dentry);
+	d_add(new_dentry, inode);
 }
 
 /*
@@ -1054,7 +1054,7 @@ static int afs_link(struct dentry *from, struct inode *dir,
 	if (afs_begin_vnode_operation(&fc, dvnode, key)) {
 		if (mutex_lock_interruptible_nested(&vnode->io_lock, 1) < 0) {
 			afs_end_vnode_operation(&fc);
-			return -ERESTARTSYS;
+			goto error_key;
 		}
 
 		while (afs_select_fileserver(&fc)) {
@@ -1181,7 +1181,7 @@ static int afs_rename(struct inode *old_dir, struct dentry *old_dentry,
 		if (orig_dvnode != new_dvnode) {
 			if (mutex_lock_interruptible_nested(&new_dvnode->io_lock, 1) < 0) {
 				afs_end_vnode_operation(&fc);
-				return -ERESTARTSYS;
+				goto error_key;
 			}
 		}
 		while (afs_select_fileserver(&fc)) {
@@ -1200,14 +1200,9 @@ static int afs_rename(struct inode *old_dir, struct dentry *old_dentry,
 			goto error_key;
 	}
 
-	key_put(key);
-	_leave(" = 0");
-	return 0;
-
 error_key:
 	key_put(key);
 error:
-	d_drop(new_dentry);
 	_leave(" = %d", ret);
 	return ret;
 }
