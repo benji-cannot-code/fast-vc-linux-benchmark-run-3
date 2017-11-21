b2e42f5b513b1a5787da07afdfe75b4e7ecf6fc7
Christian Gromm <christian.gromm@microchip.com>
2017-11-21 15:04:41 +0100

    staging: most: usb: rename module
    This patch renames the folder of the usb module. It is needed
    to clean up the  directory layout of the driver.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/usb/usb.c
+++ drivers/staging/most/usb/usb.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 /*
- * hdm_usb.c - Hardware dependent module for USB
+ * usb.c - Hardware dependent module for USB
  *
  * Copyright (C) 2013-2015 Microchip Technology Germany II GmbH & Co. KG
  */
