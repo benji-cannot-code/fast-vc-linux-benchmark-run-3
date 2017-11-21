b4dd6bfa870be0bfa6fa51dd703d548b4072a948
Christian Gromm <christian.gromm@microchip.com>
2017-11-21 15:04:56 +0100

    staging: most: core: replace struct most_inst_obj
    This patch introduces struct interface_private as a replacement for
    the struct most_inst_obj. This structure holds private data that is
    only needed by the core module and will be accessed by a pointer from
    within the most_interface structure. As a result of this replacement
    the bus helper functions can be used to search for devices.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/core.h
+++ drivers/staging/most/core.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 
 struct module;
+struct interface_private;
 
 /**
  * Interface type
@@ -249,6 +250,7 @@ struct most_interface {
 						   unsigned char link_stat,
 						   unsigned char *mac_addr));
 	void *priv;
+	struct interface_private *p;
 };
 
 #define to_most_interface(d) container_of(d, struct most_interface, dev)
