0074c22f01e03ebdf31e13c64d1dd50a45bc91f8
James Smart <jsmart2021@gmail.com>
2017-11-20 16:00:36 -0800

    scsi: lpfc: Adjust default value of lpfc_nvmet_mrq
    The current default for async hw receive queues is 1, which presents
    issues under heavy load as number of queues influence the available
    async receive buffer limits.
    
    Raise the default to the either the current hw limit (16) or the number
    of hw qs configured (io channel value).
    
    Revise the attribute definition for mrq to better reflect what we do for
    hw queues. E.g. 0 means default to optimal (# of cpus), non-zero
    specifies a specific limit. Before this change, mrq=0 meant target mode
    was disabled. As 0 now has a different meaning, rework the if tests to
    use the better nvmet_support check.
    
    Signed-off-by: Dick Kennedy <dick.kennedy@broadcom.com>
    Signed-off-by: James Smart <james.smart@broadcom.com>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/lpfc/lpfc_nvmet.h
+++ drivers/scsi/lpfc/lpfc_nvmet.h
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LPFC_NVMET_RQE_DEF_COUNT	512
 #define LPFC_NVMET_SUCCESS_LEN	12
 
+#define LPFC_NVMET_MRQ_OFF		0xffff
+#define LPFC_NVMET_MRQ_AUTO		0
+#define LPFC_NVMET_MRQ_MAX		16
+
 /* Used for NVME Target */
 struct lpfc_nvmet_tgtport {
 	struct lpfc_hba *phba;
