9a53717917dd49c9e09822462366d66dc94ae6ed
Lucas Stach <l.stach@pengutronix.de>
2017-11-23 17:57:12 +0100

    drm/etnaviv: rename submit fence to out_fence
    This is the fence passed out on a sucessful GPU submit. Make the name
    more clear.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/gpu/drm/etnaviv/etnaviv_gem_submit.c
+++ drivers/gpu/drm/etnaviv/etnaviv_gem_submit.c
@@ -190,10 +190,10 @@ static void submit_attach_object_fences(struct etnaviv_gem_submit *submit)
 
 		if (submit->bos[i].flags & ETNA_SUBMIT_BO_WRITE)
 			reservation_object_add_excl_fence(etnaviv_obj->resv,
-							  submit->fence);
+							  submit->out_fence);
 		else
 			reservation_object_add_shared_fence(etnaviv_obj->resv,
-							    submit->fence);
+							    submit->out_fence);
 
 		submit_unlock_object(submit, i);
 	}
@@ -360,8 +360,8 @@ static void submit_cleanup(struct etnaviv_gem_submit *submit)
 	}
 
 	ww_acquire_fini(&submit->ticket);
-	if (submit->fence)
-		dma_fence_put(submit->fence);
+	if (submit->out_fence)
+		dma_fence_put(submit->out_fence);
 	kfree(submit);
 }
 
@@ -538,7 +538,7 @@ int etnaviv_ioctl_gem_submit(struct drm_device *dev, void *data,
 		 * fence to the sync file here, eliminating the ENOMEM
 		 * possibility at this stage.
 		 */
-		sync_file = sync_file_create(submit->fence);
+		sync_file = sync_file_create(submit->out_fence);
 		if (!sync_file) {
 			ret = -ENOMEM;
 			goto out;
@@ -547,7 +547,7 @@ int etnaviv_ioctl_gem_submit(struct drm_device *dev, void *data,
 	}
 
 	args->fence_fd = out_fence_fd;
-	args->fence = submit->fence->seqno;
+	args->fence = submit->out_fence->seqno;
 
 out:
 	submit_unpin_objects(submit);
