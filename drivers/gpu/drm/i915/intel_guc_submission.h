b73ae48e96179b61abd746c97c475b349322a3a0
Sagar Arun Kamble <sagar.a.kamble@intel.com>
2017-11-24 13:50:50 +0530

    drm/i915/guc: Update header guard name of intel_guc_submission.h
    While renaming i915_guc_submission.h to intel_guc_submission.h, update of
    header guard name to _INTEL_GUC_SUBMISSION_H_ was missed.
    
    References: a269574489d4 (drm/i915/guc: Rename i915_guc_submission.c|h to intel_guc_submission.c|h)
    Signed-off-by: Sagar Arun Kamble <sagar.a.kamble@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Reviewed-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1511511650-13756-1-git-send-email-sagar.a.kamble@intel.com
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- drivers/gpu/drm/i915/intel_guc_submission.h
+++ drivers/gpu/drm/i915/intel_guc_submission.h
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#ifndef _I915_GUC_SUBMISSION_H_
-#define _I915_GUC_SUBMISSION_H_
+#ifndef _INTEL_GUC_SUBMISSION_H_
+#define _INTEL_GUC_SUBMISSION_H_
 
 #include <linux/spinlock.h>
 
