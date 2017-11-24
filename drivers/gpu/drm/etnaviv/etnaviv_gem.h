56cb43713384c3e948aa336125b1240e80553736
Lucas Stach <l.stach@pengutronix.de>
2017-11-24 11:36:03 +0100

    drm/etnaviv: refcount the submit object
    The submit object lifetime will get extended to the actual GPU
    execution. As multiple users will depend on this, add a kref to
    properly control destruction of the object.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/drm/etnaviv/etnaviv_gem.h
+++ drivers/gpu/drm/etnaviv/etnaviv_gem.h
@@ -102,6 +102,7 @@ struct etnaviv_gem_submit_bo {
  * lasts for the duration of the submit-ioctl.
  */
 struct etnaviv_gem_submit {
+	struct kref refcount;
 	struct etnaviv_gpu *gpu;
 	struct dma_fence *out_fence, *in_fence;
 	u32 flags;
@@ -109,6 +110,8 @@ struct etnaviv_gem_submit {
 	struct etnaviv_gem_submit_bo bos[0];
 	/* No new members here, the previous one is variable-length! */
 };
+
+void etnaviv_submit_put(struct etnaviv_gem_submit * submit);
 
 int etnaviv_gem_wait_bo(struct etnaviv_gpu *gpu, struct drm_gem_object *obj,
 	struct timespec *timeout);
