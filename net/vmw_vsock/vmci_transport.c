53984b79b154f23b4ee33914532f1d789c3fa79c
Jorgen Hansen <jhansen@vmware.com>
2017-11-27 05:29:32 -0800

    VSOCK: Don't set sk_state to TCP_CLOSE before testing it
    A recent commit (3b4477d2dcf2) converted the sk_state to use
    TCP constants. In that change, vmci_transport_handle_detach
    was changed such that sk->sk_state was set to TCP_CLOSE before
    we test whether it is TCP_SYN_SENT. This change moves the
    sk_state change back to the original locations in that function.
    
    Signed-off-by: Jorgen Hansen <jhansen@vmware.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/vmw_vsock/vmci_transport.c
+++ net/vmw_vsock/vmci_transport.c
@@ -805,8 +805,6 @@ static void vmci_transport_handle_detach(struct sock *sk)
 		 */
 		if (vsk->local_addr.svm_cid == VMADDR_CID_HOST ||
 		    vsock_stream_has_data(vsk) <= 0) {
-			sk->sk_state = TCP_CLOSE;
-
 			if (sk->sk_state == TCP_SYN_SENT) {
 				/* The peer may detach from a queue pair while
 				 * we are still in the connecting state, i.e.,
@@ -816,10 +814,12 @@ static void vmci_transport_handle_detach(struct sock *sk)
 				 * event like a reset.
 				 */
 
+				sk->sk_state = TCP_CLOSE;
 				sk->sk_err = ECONNRESET;
 				sk->sk_error_report(sk);
 				return;
 			}
+			sk->sk_state = TCP_CLOSE;
 		}
 		sk->sk_state_change(sk);
 	}
