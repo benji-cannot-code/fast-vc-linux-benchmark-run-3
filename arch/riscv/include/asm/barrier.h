87d378a47e6cdef83cdad26fc1c9e0673fbaa873
Palmer Dabbelt <palmer@sifive.com>
2017-11-28 14:03:55 -0800

    RISC-V: Remove smb_mb__{before,after}_spinlock()
    These are obselete.
    
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- arch/riscv/include/asm/barrier.h
+++ arch/riscv/include/asm/barrier.h
@@ -39,14 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define smp_rmb()	RISCV_FENCE(r,r)
 #define smp_wmb()	RISCV_FENCE(w,w)
 
-/*
- * These barriers prevent accesses performed outside a spinlock from being moved
- * inside a spinlock.  Since RISC-V sets the aq/rl bits on our spinlock only
- * enforce release consistency, we need full fences here.
- */
-#define smb_mb__before_spinlock()	smp_mb()
-#define smb_mb__after_spinlock()	smp_mb()
-
 #include <asm-generic/barrier.h>
 
 #endif /* __ASSEMBLY__ */
