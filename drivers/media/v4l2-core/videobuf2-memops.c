7506c816324b9320e21b38c934f938dae327fe99
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2017-11-29 03:56:18 -0500

    media: v4l2-core: Fix kernel-doc markups
    There are some troubles on kernel-doc markups, as warned:
    
    	drivers/media/v4l2-core/v4l2-dv-timings.c:259: warning: No description found for parameter 't1'
    	drivers/media/v4l2-core/v4l2-dv-timings.c:259: warning: No description found for parameter 't2'
    	drivers/media/v4l2-core/v4l2-dv-timings.c:259: warning: No description found for parameter 'pclock_delta'
    	drivers/media/v4l2-core/v4l2-dv-timings.c:259: warning: No description found for parameter 'match_reduced_fps'
    	drivers/media/v4l2-core/tuner-core.c:242: warning: bad line: internal parameters, like LNA mode
    	drivers/media/v4l2-core/tuner-core.c:765: warning: No description found for parameter 'mode'
    	drivers/media/v4l2-core/videobuf2-memops.c:127: warning: cannot understand function prototype: 'const struct vm_operations_struct vb2_common_vm_ops = '
    	drivers/media/v4l2-core/v4l2-mem2mem.c:190: warning: No description found for parameter 'm2m_dev'
    	drivers/media/v4l2-core/v4l2-mem2mem.c:291: warning: No description found for parameter 'm2m_ctx'
    	drivers/media/v4l2-core/videobuf-core.c:233: warning: No description found for parameter 'q'
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/v4l2-core/videobuf2-memops.c
+++ drivers/media/v4l2-core/videobuf2-memops.c
@@ -121,7 +121,7 @@ static void vb2_common_vm_close(struct vm_area_struct *vma)
 	h->put(h->arg);
 }
 
-/**
+/*
  * vb2_common_vm_ops - common vm_ops used for tracking refcount of mmaped
  * video buffers
  */
