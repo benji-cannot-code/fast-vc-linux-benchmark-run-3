a397e3d7a44a05cda0d4bd40dbbb8e84b6223ee3
Quentin Monnet <quentin.monnet@netronome.com>
2017-11-28 17:44:29 -0800

    tools: bpftool: clean up the JSON writer before exiting in usage()
    The writer is cleaned at the end of the main function, but not if the
    program exits sooner in usage(). Let's keep it clean and destroy the
    writer before exiting.
    
    Destruction and actual call to exit() are moved to another function so
    that clean exit can also be performed without printing usage() hints.
    
    Fixes: d35efba99d92 ("tools: bpftool: introduce --json and --pretty options")
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/bpf/bpftool/main.h
+++ tools/bpf/bpftool/main.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdbool.h>
 #include <stdio.h>
 #include <linux/bpf.h>
+#include <linux/compiler.h>
 #include <linux/kernel.h>
 #include <linux/hashtable.h>
 
@@ -81,7 +82,7 @@ void p_info(const char *fmt, ...);
 
 bool is_prefix(const char *pfx, const char *str);
 void fprint_hex(FILE *f, void *arg, unsigned int n, const char *sep);
-void usage(void) __attribute__((noreturn));
+void usage(void) __noreturn;
 
 struct pinned_obj_table {
 	DECLARE_HASHTABLE(table, 16);
