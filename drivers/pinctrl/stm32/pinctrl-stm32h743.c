102b2319996b55bcb1cff025566790aa3e3aa605
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2017-11-30 09:46:48 +0100

    pinctrl: stm32: Fix copyright
    Uniformize STMicroelectronics copyrights header
    Add SPDX identifier
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@st.com>
    Acked-by: Alexandre TORGUE <alexandre.torgue@st.com>
    Acked-by: Maxime Coquelin <mcoquelin.stm32@gmail.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/stm32/pinctrl-stm32h743.c
+++ drivers/pinctrl/stm32/pinctrl-stm32h743.c
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) Alexandre Torgue 2017
- * Author:  Alexandre Torgue <alexandre.torgue@st.com>
- * License terms:  GNU General Public License (GPL), version 2
+ * Copyright (C) STMicroelectronics 2017
+ * Author:  Alexandre Torgue <alexandre.torgue@st.com> for STMicroelectronics.
  */
 #include <linux/init.h>
 #include <linux/of.h>
