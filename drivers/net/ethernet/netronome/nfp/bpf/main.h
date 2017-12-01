80ab18724eeaab2d7643b0dbbb1cd4af8da44d93
Jiong Wang <jiong.wang@netronome.com>
2017-11-30 21:32:49 -0800

    nfp: bpf: support backward jump
    This patch adds support for backward jump on NFP.
    
      - restrictions on backward jump in various functions have been removed.
      - nfp_fixup_branches now supports backward jump.
    
    There is one thing to note, currently an input eBPF JMP insn may generate
    several NFP insns, for example,
    
      NFP imm move insn A \
      NFP compare insn  B  --> 3 NFP insn jited from eBPF JMP insn M
      NFP branch insn   C /
      ---
      NFP insn X           --> 1 NFP insn jited from eBPF insn N
      ---
      ...
    
    therefore, we are doing sanity check to make sure the last jited insn from
    an eBPF JMP is a NFP branch instruction.
    
    Once backward jump is allowed, it is possible an eBPF JMP insn is at the
    end of the program. This is however causing trouble for the sanity check.
    Because the sanity check requires the end index of the NFP insns jited from
    one eBPF insn while only the start index is recorded before this patch that
    we can only get the end index by:
    
      start_index_of_the_next_eBPF_insn - 1
    
    or for the above example:
    
      start_index_of_eBPF_insn_N (which is the index of NFP insn X) - 1
    
    nfp_fixup_branches was using nfp_for_each_insn_walk2 to expose *next* insn
    to each iteration during the traversal so the last index could be
    calculated from which. Now, it needs some extra code to handle the last
    insn. Meanwhile, the use of walk2 is actually unnecessary, we could simply
    use generic single instruction walk to do this, the next insn could be
    easily calculated using list_next_entry.
    
    So, this patch migrates the jump fixup traversal method to
    *list_for_each_entry*, this simplifies the code logic a little bit.
    
    The other thing to note is a new state variable "last_bpf_off" is
    introduced to track the index of the last jited NFP insn. This is necessary
    because NFP is generating special purposes epilogue sequences, so the index
    of the last jited NFP insn is *not* always nfp_prog->prog_len - 1.
    
    Suggested-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Jiong Wang <jiong.wang@netronome.com>
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- drivers/net/ethernet/netronome/nfp/bpf/main.h
+++ drivers/net/ethernet/netronome/nfp/bpf/main.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (C) 2016 Netronome Systems, Inc.
+ * Copyright (C) 2016-2017 Netronome Systems, Inc.
  *
  * This software is dual licensed under the GNU General License Version 2,
  * June 1991 as shown in the file COPYING in the top-level directory of this
@@ -143,6 +143,7 @@ static inline u8 mbpf_mode(const struct nfp_insn_meta *meta)
  * @verifier_meta: temporary storage for verifier's insn meta
  * @type: BPF program type
  * @start_off: address of the first instruction in the memory
+ * @last_bpf_off: address of the last instruction translated from BPF
  * @tgt_out: jump target for normal exit
  * @tgt_abort: jump target for abort (e.g. access outside of packet buffer)
  * @tgt_done: jump target to get the next packet
@@ -161,6 +162,7 @@ struct nfp_prog {
 	enum bpf_prog_type type;
 
 	unsigned int start_off;
+	unsigned int last_bpf_off;
 	unsigned int tgt_out;
 	unsigned int tgt_abort;
 	unsigned int tgt_done;
