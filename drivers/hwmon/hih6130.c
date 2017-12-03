b420fbf8973235943b19c06aecdd33a9f050928a
Guenter Roeck <linux@roeck-us.net>
2017-12-03 15:24:57 -0800

    hwmon: (hih6130) Fix documentation of struct hih6130
    Sparse reports:
    
    drivers/hwmon/hih6130.c:56: warning:
    	No description found for parameter 'client'
    drivers/hwmon/hih6130.c:56: warning:
    	Excess struct member 'hwmon_dev' description in 'hih6130'
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/hih6130.c
+++ drivers/hwmon/hih6130.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * struct hih6130 - HIH-6130 device specific data
- * @hwmon_dev: device registered with hwmon
+ * @client: pointer to I2C client device
  * @lock: mutex to protect measurement values
  * @valid: only false before first measurement is taken
  * @last_update: time of last update (jiffies)
