c85cfb1189e6610c4bd2c6026e2c9ccdb1ec940a
Marc Zyngier <marc.zyngier@arm.com>
2017-12-04 16:43:23 +0000

    KVM: arm/arm64: Keep GICv2 HYP VAs in kvm_vgic_global_state
    As we're about to change the way we map devices at HYP, we need
    to move away from kern_hyp_va on an IO address.
    
    One way of achieving this is to store the VAs in kvm_vgic_global_state,
    and use that directly from the HYP code. This requires a small change
    to create_hyp_io_mappings so that it can also return a HYP VA.
    
    We take this opportunity to nuke the vctrl_base field in the emulated
    distributor, as it is not used anymore.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/include/asm/kvm_mmu.h
+++ arch/arm/include/asm/kvm_mmu.h
@@ -52,7 +52,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int create_hyp_mappings(void *from, void *to, pgprot_t prot);
 int create_hyp_io_mappings(phys_addr_t phys_addr, size_t size,
-			   void __iomem **kaddr);
+			   void __iomem **kaddr,
+			   void __iomem **haddr);
 void free_hyp_pgds(void);
 
 void stage2_unmap_vm(struct kvm *kvm);
