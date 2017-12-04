45b1ec2c43088e2039ce12166f5e2b3c5af8e1ac
Al Viro <viro@zeniv.linux.org.uk>
2017-12-04 14:43:20 -0500

    ppc: for put_user() pull linux/uaccess.h, not asm/uaccess.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/powerpc/kvm/book3s_xive.c
+++ arch/powerpc/kvm/book3s_xive.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/percpu.h>
 #include <linux/cpumask.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <asm/kvm_book3s.h>
 #include <asm/kvm_ppc.h>
 #include <asm/hvcall.h>
