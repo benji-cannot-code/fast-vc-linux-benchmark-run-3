660e9ee614fd719161386e9a2cd24798aacf33ff
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2017-12-05 15:57:21 +0100

    pwm: stm32: Adopt SPDX identifier
    Add SPDX identifier to make it easier to determine the license of the
    file.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@st.com>
    Acked-by: Philippe Ombredanne <pombredanne@nexb.com>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-stm32-lp.c
+++ drivers/pwm/pwm-stm32-lp.c
@@ -1,12 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * STM32 Low-Power Timer PWM driver
  *
  * Copyright (C) STMicroelectronics 2017
  *
  * Author: Gerald Baeza <gerald.baeza@st.com>
- *
- * License terms: GNU General Public License (GPL), version 2
  *
  * Inspired by Gerald Baeza's pwm-stm32 driver
  */
