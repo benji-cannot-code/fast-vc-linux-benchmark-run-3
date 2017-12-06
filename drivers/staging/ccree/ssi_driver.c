1737b309a5769341d41a118f62daf3a8a7f5742c
Pravin Shedge <pravin.shedge4linux@gmail.com>
2017-12-06 22:42:08 +0530

    drivers: staging: remove duplicate includes
    These duplicate includes have been found with scripts/checkincludes.pl but
    they have been removed manually to avoid removing false positives.
    
    Signed-off-by: Pravin Shedge <pravin.shedge4linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/ssi_driver.c
+++ drivers/staging/ccree/ssi_driver.c
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/pagemap.h>
 #include <linux/sched.h>
-#include <linux/random.h>
 #include <linux/of.h>
 #include <linux/clk.h>
 #include <linux/of_address.h>
