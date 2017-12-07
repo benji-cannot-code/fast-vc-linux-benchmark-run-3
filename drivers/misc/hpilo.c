783994da84de549ec4e0ab1e5c086d9f1db1f421
Dhaval Shah <dhaval.experiance@gmail.com>
2017-12-07 11:01:48 +0530

    misc: hpilo: Use SPDX-License-Identifier
    SPDX-License-Identifier is used for the hpilo.
    
    Signed-off-by: Dhaval Shah <dhaval.experiance@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/hpilo.c
+++ drivers/misc/hpilo.c
@@ -1,13 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Driver for the HP iLO management processor.
  *
  * Copyright (C) 2008 Hewlett-Packard Development Company, L.P.
  *	David Altobelli <david.altobelli@hpe.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 #include <linux/kernel.h>
 #include <linux/types.h>
