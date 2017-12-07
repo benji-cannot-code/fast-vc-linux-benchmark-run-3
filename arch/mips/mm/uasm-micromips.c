febaea2575bef74f4fe9f504933bbd06533599cb
James Hogan <jhogan@kernel.org>
2017-12-07 07:14:17 +0000

    MIPS: mm: Fix duplicate "const" on insn_table_MM
    Fix the following gcc 7.x build error on microMIPS builds:
    
    arch/mips/mm/uasm-micromips.c:43:26: error: duplicate ‘const’ declaration specifier [-Werror=duplicate-decl-specifier]
     static const struct insn const insn_table_MM[insn_invalid] = {
                              ^~~~~
    
    The same issue has already been fixed in uasm-mips by commit
    00e06297b351 ("MIPS: mm: remove duplicate "const" qualifier on
    insn_table").
    
    Signed-off-by: James Hogan <jhogan@kernel.org>
    Fixes: ce807d5f67ed ("MIPS: Optimize uasm insn lookup.")
    Cc: David Daney <david.daney@cavium.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/17889/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mm/uasm-micromips.c
+++ arch/mips/mm/uasm-micromips.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "uasm.c"
 
-static const struct insn const insn_table_MM[insn_invalid] = {
+static const struct insn insn_table_MM[insn_invalid] = {
 	[insn_addu]	= {M(mm_pool32a_op, 0, 0, 0, 0, mm_addu32_op), RT | RS | RD},
 	[insn_addiu]	= {M(mm_addiu32_op, 0, 0, 0, 0, 0), RT | RS | SIMM},
 	[insn_and]	= {M(mm_pool32a_op, 0, 0, 0, 0, mm_and_op), RT | RS | RD},
