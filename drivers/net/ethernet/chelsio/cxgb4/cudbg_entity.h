f6ff0bb1638de773c730648f2e1bfedd6552461c
Rahul Lakkireddy <rahul.lakkireddy@chelsio.com>
2017-12-08 09:48:37 +0530

    cxgb4: collect MC memory dump
    Use meminfo to get base address and size of MC memory.  Also use same
    meminfo for EDC memory dumps.
    
    Signed-off-by: Rahul Lakkireddy <rahul.lakkireddy@chelsio.com>
    Signed-off-by: Ganesh Goudar <ganeshgr@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/chelsio/cxgb4/cudbg_entity.h
+++ drivers/net/ethernet/chelsio/cxgb4/cudbg_entity.h
@@ -19,16 +19,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __CUDBG_ENTITY_H__
 #define __CUDBG_ENTITY_H__
 
-#define EDC0_FLAG 3
-#define EDC1_FLAG 4
+#define EDC0_FLAG 0
+#define EDC1_FLAG 1
+#define MC_FLAG 2
+#define MC0_FLAG 3
+#define MC1_FLAG 4
 
 #define CUDBG_ENTITY_SIGNATURE 0xCCEDB001
-
-struct card_mem {
-	u16 size_edc0;
-	u16 size_edc1;
-	u16 mem_flag;
-};
 
 struct cudbg_mbox_log {
 	struct mbox_cmd entry;
