282a7d32183f6a7a4390059dce9c71cccf078b18
Changbin Du <changbin.du@intel.com>
2017-12-08 14:56:23 +0800

    drm/i915/gvt: Rename file render.{c, h} to mmio_context.{c, h}
    Rename the files to reflect their real role - to switch the mmio context of
    each vGPU engine.
    
    v2: update Makefile.
    
    Signed-off-by: Changbin Du <changbin.du@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/gvt.h
+++ drivers/gpu/drm/i915/gvt/gvt.h
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "execlist.h"
 #include "scheduler.h"
 #include "sched_policy.h"
-#include "render.h"
+#include "mmio_context.h"
 #include "cmd_parser.h"
 #include "fb_decoder.h"
 #include "dmabuf.h"
