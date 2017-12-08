5c9036aa4a36a1e240d978066d2a2b36dfe6c8da
Changbin Du <changbin.du@intel.com>
2017-12-08 14:56:21 +0800

    drm/i915/gvt: Select appropriate mmio list at initialization time
    Select appropriate mmio list at initialization time, so we don't need to
    do duplicated work at where requires the mmio list.
    
    V2:
      - Add a termination mark of mmio list.
    
    Signed-off-by: Changbin Du <changbin.du@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/render.h
+++ drivers/gpu/drm/i915/gvt/render.h
@@ -37,8 +37,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __GVT_RENDER_H__
 #define __GVT_RENDER_H__
 
+struct engine_mmio {
+	int ring_id;
+	i915_reg_t reg;
+	u32 mask;
+	bool in_context;
+	u32 value;
+};
+
 void intel_gvt_switch_mmio(struct intel_vgpu *pre,
 			   struct intel_vgpu *next, int ring_id);
 
+void intel_gvt_init_engine_mmio_context(struct intel_gvt *gvt);
 
 #endif
