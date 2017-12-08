cf536abe03ab464f5de5e9924ca1ed89a2616e30
Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
2017-12-08 11:56:14 +0900

    workqueue: remove unneeded kallsyms include
    The filw was converted from print_symbol() to %pf some time
    ago (044c782ce3a901fb "workqueue: fix checkpatch issues").
    kallsyms does not seem to be needed anymore.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/hardirq.h>
 #include <linux/mempolicy.h>
 #include <linux/freezer.h>
-#include <linux/kallsyms.h>
 #include <linux/debug_locks.h>
 #include <linux/lockdep.h>
 #include <linux/idr.h>
