5d0065934f5478a5d3d5abaa539b9a45a8eb8138
Dhaval Shah <dhaval.shah@softnautics.com>
2017-12-08 13:43:04 +0530

    misc: ad525x_dpot: please, no space before tabs
    Resolved the please, no space beofore tabs checkpatch
    warning. Issue found by checkpatch.
    
    Signed-off-by: Dhaval Shah <dhaval.shah@softnautics.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/ad525x_dpot.c
+++ drivers/misc/ad525x_dpot.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2009-2010 Analog Devices, Inc.
  * Author: Michael Hennerich <hennerich@blackfin.uclinux.org>
  *
- * DEVID		#Wipers		#Positions 	Resistor Options (kOhm)
+ * DEVID		#Wipers		#Positions	Resistor Options (kOhm)
  * AD5258		1		64		1, 10, 50, 100
  * AD5259		1		256		5, 10, 50, 100
  * AD5251		2		64		1, 10, 50, 100
