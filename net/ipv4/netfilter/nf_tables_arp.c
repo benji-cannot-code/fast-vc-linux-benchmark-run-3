df28457a0be66eb7e16bf5dace01543bd1bf3b81
Pablo Neira Ayuso <pablo@netfilter.org>
2017-12-09 15:43:17 +0100

    netfilter: nf_tables: remove hooks from family definition
    They don't belong to the family definition, move them to the filter
    chain type definition instead.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv4/netfilter/nf_tables_arp.c
+++ net/ipv4/netfilter/nf_tables_arp.c
@@ -32,10 +32,6 @@ static struct nft_af_info nft_af_arp __read_mostly = {
 	.family		= NFPROTO_ARP,
 	.nhooks		= NF_ARP_NUMHOOKS,
 	.owner		= THIS_MODULE,
-	.hooks		= {
-		[NF_ARP_IN]		= nft_do_chain_arp,
-		[NF_ARP_OUT]		= nft_do_chain_arp,
-	},
 };
 
 static int nf_tables_arp_init_net(struct net *net)
@@ -73,6 +69,10 @@ static const struct nf_chain_type filter_arp = {
 	.owner		= THIS_MODULE,
 	.hook_mask	= (1 << NF_ARP_IN) |
 			  (1 << NF_ARP_OUT),
+	.hooks		= {
+		[NF_ARP_IN]		= nft_do_chain_arp,
+		[NF_ARP_OUT]		= nft_do_chain_arp,
+	},
 };
 
 static int __init nf_tables_arp_init(void)
