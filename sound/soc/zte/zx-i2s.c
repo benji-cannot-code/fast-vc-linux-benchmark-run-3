46b9bdfbd947012e99b849ac05cfd9db40b5d08f
Pravin Shedge <pravin.shedge4linux@gmail.com>
2017-12-11 00:02:03 +0530

    ASoC: zte: remove duplicate includes
    These duplicate includes have been found with scripts/checkincludes.pl but
    they have been removed manually to avoid removing false positives.
    
    Signed-off-by: Pravin Shedge <pravin.shedge4linux@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/zte/zx-i2s.c
+++ sound/soc/zte/zx-i2s.c
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/core.h>
 #include <sound/dmaengine_pcm.h>
 #include <sound/initval.h>
-#include <sound/pcm.h>
-#include <sound/pcm_params.h>
-#include <sound/soc.h>
 
 #define ZX_I2S_PROCESS_CTRL	0x04
 #define ZX_I2S_TIMING_CTRL	0x08
