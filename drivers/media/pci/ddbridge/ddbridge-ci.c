4b3a1ec561e6c978b586002bcbea10ba08a156d4
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2017-12-11 12:18:46 -0500

    media: ddbridge: shut up a new warning
    drivers/media/pci/ddbridge/ddbridge-ci.c:321:5: warning: no previous prototype for 'ddb_ci_attach' [-Wmissing-prototypes]
     int ddb_ci_attach(struct ddb_port *port, u32 bitrate)
         ^~~~~~~~~~~~~
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/pci/ddbridge/ddbridge-ci.c
+++ drivers/media/pci/ddbridge/ddbridge-ci.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ddbridge.h"
 #include "ddbridge-regs.h"
+#include "ddbridge-ci.h"
 #include "ddbridge-io.h"
 #include "ddbridge-i2c.h"
 
