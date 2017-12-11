fc19df8ae1b5f2a54b65646741835a484bca5988
Lukasz Majewski <lukma@denx.de>
2017-12-12 00:36:22 +0100

    ARM: ep93xx: ts72xx: Provide include guards for ts72xx.h file
    This commit adds include file guards to ts72xx.h
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>
    Acked-by: Alexander Sverdlin <alexander.sverdlin@gmail.com>
    Acked-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Alexander Sverdlin <alexander.sverdlin@gmail.com>

--- arch/arm/mach-ep93xx/ts72xx.h
+++ arch/arm/mach-ep93xx/ts72xx.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * febfd000	22800000	4K	options register #2
  */
 
+#ifndef __TS72XX_H_
+#define __TS72XX_H_
+
 #define TS72XX_MODEL_PHYS_BASE		0x22000000
 #define TS72XX_MODEL_VIRT_BASE		IOMEM(0xfebff000)
 #define TS72XX_MODEL_SIZE		0x00001000
@@ -84,3 +87,4 @@ static inline int is_ts9420_installed(void)
 					TS72XX_OPTIONS2_TS9420);
 }
 #endif
+#endif /* __TS72XX_H_ */
