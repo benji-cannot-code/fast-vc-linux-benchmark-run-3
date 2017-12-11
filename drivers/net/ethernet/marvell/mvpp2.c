63ecd4ea2558f186a2d46bd313b40b6e9451b602
Antoine Tenart <antoine.tenart@free-electrons.com>
2017-12-11 09:13:29 +0100

    net: mvpp2: adjust the coalescing parameters
    This patch adjust the coalescing parameters to the vendor
    recommendations for the PPv2 network controller.
    
    Suggested-by: Yan Markman <ymarkman@marvell.com>
    Signed-off-by: Antoine Tenart <antoine.tenart@free-electrons.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvpp2.c
+++ drivers/net/ethernet/marvell/mvpp2.c
@@ -455,11 +455,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Various constants */
 
 /* Coalescing */
-#define MVPP2_TXDONE_COAL_PKTS_THRESH	15
+#define MVPP2_TXDONE_COAL_PKTS_THRESH	64
 #define MVPP2_TXDONE_HRTIMER_PERIOD_NS	1000000UL
 #define MVPP2_TXDONE_COAL_USEC		1000
 #define MVPP2_RX_COAL_PKTS		32
-#define MVPP2_RX_COAL_USEC		100
+#define MVPP2_RX_COAL_USEC		64
 
 /* The two bytes Marvell header. Either contains a special value used
  * by Marvell switches when a specific hardware mode is enabled (not
