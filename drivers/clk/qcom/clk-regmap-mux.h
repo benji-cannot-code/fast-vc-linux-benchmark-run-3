c421817486e4dc174211adadedeac525d1d38f9a
Abhishek Sahu <absahu@codeaurora.org>
2017-12-13 19:55:33 +0530

    clk: qcom: add parent map for regmap mux
    Currently the driver assumes the register configuration value
    is identical to its index in the parent map. This patch adds
    the parent map field in regmap mux clock node which contains
    the mapping of parent index with actual register configuration
    value. If regmap node contains this parent map then the
    configuration value will be taken from this
    parent map instead of simply writing the index value.
    
    Signed-off-by: Abhishek Sahu <absahu@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/qcom/clk-regmap-mux.h
+++ drivers/clk/qcom/clk-regmap-mux.h
@@ -17,11 +17,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk-provider.h>
 #include "clk-regmap.h"
+#include "common.h"
 
 struct clk_regmap_mux {
 	u32			reg;
 	u32			shift;
 	u32			width;
+	const struct parent_map	*parent_map;
 	struct clk_regmap	clkr;
 };
 
