b7a472af6e4db1e21f91f447076e5f46b7858f78
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2017-12-13 15:46:58 +0900

    usb: renesas_usbhs: add a new callback for extcon notifier
    To set host/peripheral mode by using extcon notifier, this patch
    adds a new callback as "notifier" in renesas_usbhs_platform_callback.
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- include/linux/usb/renesas_usbhs.h
+++ include/linux/usb/renesas_usbhs.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifndef RENESAS_USB_H
 #define RENESAS_USB_H
+#include <linux/notifier.h>
 #include <linux/platform_device.h>
 #include <linux/usb/ch9.h>
 
@@ -99,6 +100,13 @@ struct renesas_usbhs_platform_callback {
 	 * VBUS control is needed for Host
 	 */
 	int (*set_vbus)(struct platform_device *pdev, int enable);
+
+	/*
+	 * option:
+	 * extcon notifier to set host/peripheral mode.
+	 */
+	int (*notifier)(struct notifier_block *nb, unsigned long event,
+			void *data);
 };
 
 /*
