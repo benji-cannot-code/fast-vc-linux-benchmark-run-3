7de6a3b90b8ff66b23d78381f2e964aa5fa732eb
Alex Deucher <alexander.deucher@amd.com>
2017-12-14 16:39:02 -0500

    drm/amdgpu: move fw_reserve functions to amdgpu_ttm.c
    It's the only place they are used.
    
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -1388,9 +1388,6 @@ struct amdgpu_fw_vram_usage {
 	void *va;
 };
 
-int amdgpu_fw_reserve_vram_init(struct amdgpu_device *adev);
-void amdgpu_fw_reserve_vram_fini(struct amdgpu_device *adev);
-
 /*
  * CGS
  */
