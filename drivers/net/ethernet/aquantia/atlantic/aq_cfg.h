4af06a7e8c2ba63d5ddbb0584ea5dfd2c47afffd
Igor Russkikh <igor.russkikh@aquantia.com>
2017-12-14 12:34:48 +0300

    net: aquantia: Increment driver version
    Add a suffix to distinguish kernel mainline version and aquantia releases
    
    Signed-off-by: Igor Russkikh <igor.russkikh@aquantia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/aquantia/atlantic/aq_cfg.h
+++ drivers/net/ethernet/aquantia/atlantic/aq_cfg.h
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AQ_CFG_DRV_VERSION	__stringify(NIC_MAJOR_DRIVER_VERSION)"."\
 				__stringify(NIC_MINOR_DRIVER_VERSION)"."\
 				__stringify(NIC_BUILD_DRIVER_VERSION)"."\
-				__stringify(NIC_REVISION_DRIVER_VERSION)
+				__stringify(NIC_REVISION_DRIVER_VERSION) \
+				AQ_CFG_DRV_VERSION_SUFFIX
 
 #endif /* AQ_CFG_H */
