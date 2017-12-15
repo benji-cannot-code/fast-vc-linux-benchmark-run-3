c373dfcbb401a35ba2ffa4ff61b15c242642c318
Chuck Lever <chuck.lever@oracle.com>
2017-12-14 20:56:26 -0500

    xprtrdma: Per-mode handling for Remote Invalidation
    Refactoring change: Remote Invalidation is particular to the memory
    registration mode that is use. Use a callout instead of a generic
    function to handle Remote Invalidation.
    
    This gets rid of the 8-byte flags field in struct rpcrdma_mw, of
    which only a single bit flag has been allocated.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprtrdma/xprt_rdma.h
+++ net/sunrpc/xprtrdma/xprt_rdma.h
@@ -273,7 +273,6 @@ struct rpcrdma_mw {
 	struct scatterlist	*mw_sg;
 	int			mw_nents;
 	enum dma_data_direction	mw_dir;
-	unsigned long		mw_flags;
 	union {
 		struct rpcrdma_fmr	fmr;
 		struct rpcrdma_frmr	frmr;
@@ -283,11 +282,6 @@ struct rpcrdma_mw {
 	u32			mw_length;
 	u64			mw_offset;
 	struct list_head	mw_all;
-};
-
-/* mw_flags */
-enum {
-	RPCRDMA_MW_F_RI		= 1,
 };
 
 /*
@@ -486,6 +480,8 @@ struct rpcrdma_memreg_ops {
 			(*ro_map)(struct rpcrdma_xprt *,
 				  struct rpcrdma_mr_seg *, int, bool,
 				  struct rpcrdma_mw **);
+	void		(*ro_reminv)(struct rpcrdma_rep *rep,
+				     struct list_head *mws);
 	void		(*ro_unmap_sync)(struct rpcrdma_xprt *,
 					 struct list_head *);
 	void		(*ro_recover_mr)(struct rpcrdma_mw *);
