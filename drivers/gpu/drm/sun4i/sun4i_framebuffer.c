f6751080e5719b57dc4501cdf65d62aa1da1c601
Maxime Ripard <maxime.ripard@free-electrons.com>
2017-12-17 18:06:05 +0100

    drm/sun4i: framebuffer: Add a custom atomic_check
    In order to support normalized zpos, we need to call
    drm_atomic_normalize_zpos in our driver's drm_mode_config_funcs'
    atomic_check.
    
    Let's duplicate the definition of drm_atomic_helper_check for now.
    
    Acked-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/67cb4ca9889e6bf29314db37127ff15eed279c53.1516617243.git-series.maxime.ripard@free-electrons.com

--- drivers/gpu/drm/sun4i/sun4i_framebuffer.c
+++ drivers/gpu/drm/sun4i/sun4i_framebuffer.c
@@ -21,9 +21,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sun4i_drv.h"
 #include "sun4i_framebuffer.h"
 
+static int sun4i_de_atomic_check(struct drm_device *dev,
+				 struct drm_atomic_state *state)
+{
+	int ret;
+
+	ret = drm_atomic_helper_check_modeset(dev, state);
+	if (ret)
+		return ret;
+
+	return drm_atomic_helper_check_planes(dev, state);
+}
+
 static const struct drm_mode_config_funcs sun4i_de_mode_config_funcs = {
 	.output_poll_changed	= drm_fb_helper_output_poll_changed,
-	.atomic_check		= drm_atomic_helper_check,
+	.atomic_check		= sun4i_de_atomic_check,
 	.atomic_commit		= drm_atomic_helper_commit,
 	.fb_create		= drm_gem_fb_create,
 };
