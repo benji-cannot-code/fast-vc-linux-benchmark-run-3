9bebfb063b91f8c2a8e737143bea86fb66e6ad65
Harry Wentland <harry.wentland@amd.com>
2017-12-18 12:01:30 -0500

    drm/amd/display: Call update_stream_signal directly from amdgpu_dm
    There's no good place in DC to cover all place where stream signal should
    be updated. update_stream_signal depends on timing which comes from DM.
    
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Reviewed-by: Tony Cheng <Tony.Cheng@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_stream.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_stream.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*******************************************************************************
  * Private functions
  ******************************************************************************/
-static void update_stream_signal(struct dc_stream_state *stream)
+void update_stream_signal(struct dc_stream_state *stream)
 {
 
 	struct dc_sink *dc_sink = stream->sink;
