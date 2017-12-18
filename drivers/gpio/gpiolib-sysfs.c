cf31b997bc1e375b9f31c0b411508fdc35107feb
Christophe Leroy <christophe.leroy@c-s.fr>
2017-12-18 11:08:35 +0100

    gpio: sysfs: avoid using kstrtol() in 'value' attribute write
    A 'perf record' on an app continuously writing in the 'value'
    attribute show that most of the time is spent in kstrtol()
    
    --17.99%--value_store
              |
              |--10.17%--kstrtoint
              |          |
              |          |--8.82%--kstrtoll
              |
              |--2.50%--gpiod_set_value_cansleep
              |
              |--1.82%--u16_gpio_set
              |
              |--1.46%--value_store
    
    The normal case is to write 0 or 1 in the attribute, therefore
    this patch avoids the call to kstrtol() in the most common cases
    
    Then 'perf record' shows
    
    --7.21%--value_store
              |
              |--2.69%--u16_gpio_set
              |
              |--1.47%--value_store
              |
              |--1.08%--gpiod_set_value_cansleep
              |
              |--0.60%--mutex_lock
              |
               --0.58%--mutex_unlock
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-sysfs.c
+++ drivers/gpio/gpiolib-sysfs.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/kdev_t.h>
 #include <linux/slab.h>
+#include <linux/ctype.h>
 
 #include "gpiolib.h"
 
@@ -125,7 +126,7 @@ static ssize_t value_store(struct device *dev,
 {
 	struct gpiod_data *data = dev_get_drvdata(dev);
 	struct gpio_desc *desc = data->desc;
-	ssize_t			status;
+	ssize_t status = 0;
 
 	mutex_lock(&data->mutex);
 
@@ -134,7 +135,11 @@ static ssize_t value_store(struct device *dev,
 	} else {
 		long		value;
 
-		status = kstrtol(buf, 0, &value);
+		if (size <= 2 && isdigit(buf[0]) &&
+		    (size == 1 || buf[1] == '\n'))
+			value = buf[0] - '0';
+		else
+			status = kstrtol(buf, 0, &value);
 		if (status == 0) {
 			gpiod_set_value_cansleep(desc, value);
 			status = size;
