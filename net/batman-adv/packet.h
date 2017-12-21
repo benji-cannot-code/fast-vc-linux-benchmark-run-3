8d13859362bf392c09f8d02dc7a1cd8f767980ce
Sven Eckelmann <sven.eckelmann@openmesh.com>
2017-12-21 10:17:38 +0100

    batman-adv: Let packet.h include its headers directly
    The headers used by packet.h should also be included by it directly. main.h
    is currently dealing with it in batman-adv, but this will no longer work
    when this header is moved to include/uapi/linux/.
    
    Signed-off-by: Sven Eckelmann <sven.eckelmann@openmesh.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/batman-adv/packet.h
+++ net/batman-adv/packet.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _NET_BATMAN_ADV_PACKET_H_
 
 #include <asm/byteorder.h>
+#include <linux/bitops.h>
+#include <linux/if_ether.h>
 #include <linux/types.h>
 
 /**
