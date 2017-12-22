71ad67f6972b8a439e63cf71d691348785f5a007
Randy Dunlap <rdunlap@infradead.org>
2017-12-22 14:08:27 -0800

    scsi: doc: fix iscsi-related kernel-doc warnings
    Fix kernel-doc warnings in drivers/scsi/ that are related to iscsi
    support interfaces.
    
    Fixes these kernel-doc warnings: (tested by adding these files to a new
    target.rst documentation file: WIP)
    
    ../drivers/scsi/libiscsi.c:2740: warning: No description found for parameter 'dd_size'
    ../drivers/scsi/libiscsi.c:2740: warning: No description found for parameter 'id'
    ../drivers/scsi/libiscsi.c:2961: warning: No description found for parameter 'cls_conn'
    ../drivers/scsi/iscsi_tcp.c:313: warning: No description found for parameter 'conn'
    ../drivers/scsi/iscsi_tcp.c:363: warning: No description found for parameter 'conn'
    ../drivers/scsi/libiscsi_tcp.c:810: warning: No description found for parameter 'tcp_conn'
    ../drivers/scsi/libiscsi_tcp.c:810: warning: No description found for parameter 'segment'
    ../drivers/scsi/libiscsi_tcp.c:887: warning: No description found for parameter 'offloaded'
    ../drivers/scsi/libiscsi_tcp.c:887: warning: No description found for parameter 'status'
    ../drivers/scsi/libiscsi_tcp.c:887: warning: Excess function parameter 'offload' description in 'iscsi_tcp_recv_skb'
    ../drivers/scsi/libiscsi_tcp.c:964: warning: Excess function parameter 'conn' description in 'iscsi_tcp_task_init'
    ../drivers/scsi/libiscsi_tcp.c:964: warning: Excess function parameter 'sc' description in 'iscsi_tcp_task_init'
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: "Nicholas A. Bellinger" <nab@linux-iscsi.org>
    Cc: linux-scsi@vger.kernel.org
    Cc: target-devel@vger.kernel.org
    Cc: Sagi Grimberg <sagi@grimberg.me>
    Cc: linux-rdma@vger.kernel.org
    Cc: "James E.J. Bottomley" <jejb@linux.vnet.ibm.com>
    Cc: "Martin K. Petersen" <martin.petersen@oracle.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/iscsi_tcp.c
+++ drivers/scsi/iscsi_tcp.c
@@ -308,6 +308,7 @@ static int iscsi_sw_tcp_xmit_segment(struct iscsi_tcp_conn *tcp_conn,
 
 /**
  * iscsi_sw_tcp_xmit - TCP transmit
+ * @conn: iscsi connection
  **/
 static int iscsi_sw_tcp_xmit(struct iscsi_conn *conn)
 {
@@ -358,6 +359,7 @@ error:
 
 /**
  * iscsi_tcp_xmit_qlen - return the number of bytes queued for xmit
+ * @conn: iscsi connection
  */
 static inline int iscsi_sw_tcp_xmit_qlen(struct iscsi_conn *conn)
 {
