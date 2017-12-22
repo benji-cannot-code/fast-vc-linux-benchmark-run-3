6b2f86d970323aad196d50e57dae54897573dea3
Christoph Hellwig <hch@lst.de>
2017-12-22 11:07:22 +0100

    s390: move s390_pci_dma_ops to asm/pci_dma.h
    This is not needed in drivers, so move it to a private header.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- arch/s390/include/asm/dma-mapping.h
+++ arch/s390/include/asm/dma-mapping.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-debug.h>
 #include <linux/io.h>
 
-extern const struct dma_map_ops s390_pci_dma_ops;
-
 static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 {
 	return &dma_noop_ops;
