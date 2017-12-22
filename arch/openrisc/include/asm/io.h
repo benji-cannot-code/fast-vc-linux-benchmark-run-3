e6d9ff5d41d5073379205c230e644bd62192f38a
Greentime Hu <greentime@andestech.com>
2017-12-22 22:18:40 +0800

    openrisc: add ioremap_nocache declaration before include asm-generic/io.h and sync ioremap prototype with it.
    A future commit for the nds32 architecture bootstrap("asm-generic/io.h: move
    ioremap_nocache/ioremap_uc/ioremap_wc/ioremap_wt out of ifndef CONFIG_MMU")
    will move the ioremap_nocache out of the CONFIG_MMU ifdef. This means that
    in order to suppress re-definition errors we need to setup #define's before
    importing asm-generic/io.h.
    
    Also, the change adds a prototype for ioremap where size is size_t so fix that
    as well.
    
    Signed-off-by: Greentime Hu <greentime@andestech.com>
    Acked-by: Stafford Horne <shorne@gmail.com>

--- arch/openrisc/include/asm/io.h
+++ arch/openrisc/include/asm/io.h
@@ -30,13 +30,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PIO_OFFSET		0
 #define PIO_MASK		0
 
+#define ioremap_nocache ioremap_nocache
 #include <asm-generic/io.h>
 #include <asm/pgtable.h>
 
 extern void __iomem *__ioremap(phys_addr_t offset, unsigned long size,
 				pgprot_t prot);
 
-static inline void __iomem *ioremap(phys_addr_t offset, unsigned long size)
+static inline void __iomem *ioremap(phys_addr_t offset, size_t size)
 {
 	return __ioremap(offset, size, PAGE_KERNEL);
 }
