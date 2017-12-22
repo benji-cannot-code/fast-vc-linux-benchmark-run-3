238cf32b82c620d75d0b4de024beb921ca63f920
Fuyun Liang <liangfuyun1@huawei.com>
2017-12-22 12:21:53 +0800

    net: hns3: add Asym Pause support to phy default features
    commit c4fb2cdf575d ("net: hns3: fix a bug for phy supported feature
    initialization") adds default supported features for phy, but our hardware
    also supports Asym Pause. This patch adds Asym Pause support to phy
    default features to prevent Asym Pause can not be advertised when the phy
    negotiates flow control.
    
    Fixes: c4fb2cdf575d ("net: hns3: fix a bug for phy supported feature initialization")
    Signed-off-by: Fuyun Liang <liangfuyun1@huawei.com>
    Signed-off-by: Peng Li <lipeng321@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_mdio.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_mdio.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HCLGE_PHY_SUPPORTED_FEATURES	(SUPPORTED_Autoneg | \
 					 SUPPORTED_TP | \
 					 SUPPORTED_Pause | \
+					 SUPPORTED_Asym_Pause | \
 					 PHY_10BT_FEATURES | \
 					 PHY_100BT_FEATURES | \
 					 PHY_1000BT_FEATURES)
