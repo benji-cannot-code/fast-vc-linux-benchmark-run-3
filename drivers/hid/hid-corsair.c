ad8192fedb74df5cb8af9576ebb52e49e1d11936
Aaron Bottegal <aaronbottegal@gmail.com>
2017-12-22 03:38:14 -0500

    HID: corsair: Add K70 Vengeance and K70 RAPIDFIRE to
    K70 Vengeance sends garbage keypresses when changing  backlight brightness.
    This hooks to the existing corsair driver, which filters out those
    invalid keypresses on similar devices in the input mapping code.
    
    V2: Fix spelling.
    
    Signed-off-by: Aaron Bottegal <aaronbottegal@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-corsair.c
+++ drivers/hid/hid-corsair.c
@@ -3,11 +3,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * HID driver for Corsair devices
  *
  * Supported devices:
+ *  - Vengeance K70 Keyboard
+ *  - K70 RAPIDFIRE Keyboard
  *  - Vengeance K90 Keyboard
  *  - Scimitar PRO RGB Gaming Mouse
  *
  * Copyright (c) 2015 Clement Vuchener
  * Copyright (c) 2017 Oscar Campos
+ * Copyright (c) 2017 Aaron Bottegal
  */
 
 /*
@@ -722,6 +725,11 @@ static const struct hid_device_id corsair_devices[] = {
             USB_DEVICE_ID_CORSAIR_GLAIVE_RGB) },
 	{ HID_USB_DEVICE(USB_VENDOR_ID_CORSAIR,
             USB_DEVICE_ID_CORSAIR_SCIMITAR_PRO_RGB) },
+	/*
+	 * Vengeance K70 and K70 RAPIDFIRE share product IDs.
+	 */
+	{ HID_USB_DEVICE(USB_VENDOR_ID_CORSAIR,
+            USB_DEVICE_ID_CORSAIR_K70R) },
 	{}
 };
 
