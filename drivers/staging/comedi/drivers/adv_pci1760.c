a8653b14f93f564368d94c12030b37ac863416de
Richard Sudaryono <richardsudaryono@gmail.com>
2017-12-24 16:47:45 +0800

    staging: comedi: adv_pci1760: fix typo in comments
    Changed 'firware' to 'firmware'
    
    Signed-off-by: Richard Sudaryono <richardsudaryono@gmail.com>

--- drivers/staging/comedi/drivers/adv_pci1760.c
+++ drivers/staging/comedi/drivers/adv_pci1760.c
@@ -61,7 +61,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCI1760_CMD_SET_DO		0x01	/* Set output state */
 #define PCI1760_CMD_GET_DO		0x02	/* Read output status */
 #define PCI1760_CMD_GET_STATUS		0x03	/* Read current status */
-#define PCI1760_CMD_GET_FW_VER		0x0e	/* Read firware version */
+#define PCI1760_CMD_GET_FW_VER		0x0e	/* Read firmware version */
 #define PCI1760_CMD_GET_HW_VER		0x0f	/* Read hardware version */
 #define PCI1760_CMD_SET_PWM_HI(x)	(0x10 + (x) * 2) /* Set "hi" period */
 #define PCI1760_CMD_SET_PWM_LO(x)	(0x11 + (x) * 2) /* Set "lo" period */
