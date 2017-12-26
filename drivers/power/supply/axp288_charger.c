c107114829fd1e2300b16b28bc920be0136af35e
Hans de Goede <hdegoede@redhat.com>
2017-12-26 13:59:02 +0100

    power: supply: axp288_charger: Cleanup some double empty lines
    While we are doing cleanups, also remove some double blank lines.
    
    Reviewed-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>

--- drivers/power/supply/axp288_charger.c
+++ drivers/power/supply/axp288_charger.c
@@ -262,7 +262,6 @@ static inline int axp288_charger_set_vbus_inlmt(struct axp288_chrg_info *info,
 	else
 		dev_err(&info->pdev->dev, "charger BAK control %d\n", ret);
 
-
 set_inlmt_fail:
 	return ret;
 }
@@ -281,7 +280,6 @@ static int axp288_charger_vbus_path_select(struct axp288_chrg_info *info,
 
 	if (ret < 0)
 		dev_err(&info->pdev->dev, "axp288 vbus path select %d\n", ret);
-
 
 	return ret;
 }
