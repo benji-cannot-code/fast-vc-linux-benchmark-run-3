e568e1e593861b68fcfe00532c58597b51923570
Raghava Aditya Renukunta <RaghavaAditya.Renukunta@microsemi.com>
2017-12-26 20:34:47 -0800

    scsi: aacraid: Fix hang while scanning in eh recovery
    Add back the ability to scan for hotplug changes while eh was in progress.
    
    Schedule a rescan for a later time in the eh recovery code and wait for
    eh to complete in the rescan worker.
    
    Signed-off-by: Raghava Aditya Renukunta <RaghavaAditya.Renukunta@microsemi.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/aacraid/aacraid.h
+++ drivers/scsi/aacraid/aacraid.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/interrupt.h>
 #include <linux/pci.h>
+#include <scsi/scsi_host.h>
 
 /*------------------------------------------------------------------------------
  *              D E F I N E S
@@ -2655,6 +2656,9 @@ static inline void aac_safw_rescan_worker(struct work_struct *work)
 {
 	struct aac_dev *dev = container_of(to_delayed_work(work),
 		struct aac_dev, safw_rescan_work);
+
+	wait_event(dev->scsi_host_ptr->host_wait,
+		!scsi_host_in_recovery(dev->scsi_host_ptr));
 
 	aac_scan_host(dev, AAC_RESCAN);
 }
