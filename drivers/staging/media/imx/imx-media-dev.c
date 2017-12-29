55c6a843945fd0c773309457299945e773554e4a
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2017-12-29 14:55:41 -0500

    media: imx: fix breakages when compiling for arm
    As reported:
    
       drivers/staging/media/imx/imx-media-dev.c: In function 'find_async_subdev':
    >> drivers/staging/media/imx/imx-media-dev.c:55:49: error: request for member 'name' in something not a structure or union
           if (devname && !strcmp(asd->match.device_name.name,
                                                        ^
       drivers/staging/media/imx/imx-media-dev.c: In function 'imx_media_add_async_subdev':
       drivers/staging/media/imx/imx-media-dev.c:110:25: error: request for member 'name' in something not a structure or union
          asd->match.device_name.name = devname;
                                ^
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/staging/media/imx/imx-media-dev.c
+++ drivers/staging/media/imx/imx-media-dev.c
@@ -53,7 +53,7 @@ find_async_subdev(struct imx_media_dev *imxmd,
 				return asd;
 			break;
 		case V4L2_ASYNC_MATCH_DEVNAME:
-			if (devname && !strcmp(asd->match.device_name.name,
+			if (devname && !strcmp(asd->match.device_name,
 					       devname))
 				return asd;
 			break;
@@ -108,7 +108,7 @@ int imx_media_add_async_subdev(struct imx_media_dev *imxmd,
 		asd->match.fwnode = fwnode;
 	} else {
 		asd->match_type = V4L2_ASYNC_MATCH_DEVNAME;
-		asd->match.device_name.name = devname;
+		asd->match.device_name = devname;
 		imxasd->pdev = pdev;
 	}
 
