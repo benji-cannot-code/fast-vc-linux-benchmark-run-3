0e3fe5e5cac6a22cb06463a18cd41ca25a9fa678
Stephen Boyd <sboyd@codeaurora.org>
2018-01-02 15:47:07 -0800

    clk: Prepare to remove asm-generic/clkdev.h
    Now that all the users of asm/clkdev.h have been replaced with
    the generic file we can get rid of the asm-generic file as well
    and implement that code directly where it's used.
    
    We only have one caller of __clkdev_alloc(), in clkdev.c so we
    can easily remove that and drop the include of asm/clkdev.h in
    linux/clkdev.h by putting the __clk_get/__clk_put inlines in
    their respective location.
    
    Cc: Russell King <linux@armlinux.org.uk>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/linux/clkdev.h
+++ include/linux/clkdev.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __CLKDEV_H
 #define __CLKDEV_H
 
-#include <asm/clkdev.h>
+#include <linux/slab.h>
 
 struct clk;
 struct clk_hw;
@@ -56,6 +56,9 @@ int clk_hw_register_clkdev(struct clk_hw *, const char *, const char *);
 #ifdef CONFIG_COMMON_CLK
 int __clk_get(struct clk *clk);
 void __clk_put(struct clk *clk);
+#else
+static inline int __clk_get(struct clk *clk) { return 1; }
+static inline void __clk_put(struct clk *clk) { }
 #endif
 
 #endif
