1285730482aa55c627c093c59a520cf37289e5af
Arnd Bergmann <arnd@arndb.de>
2018-01-02 11:48:54 +0100

    slimbus: qcom-ctrl: use normal allocation
    The previous patch addressed a warning but not the cause:
    
    drivers/slimbus/qcom-ctrl.c: In function 'qcom_slim_probe':
    drivers/slimbus/qcom-ctrl.c:584:9: error: passing argument 3 of 'dmam_alloc_coherent' from incompatible pointer type [-Werror=incompatible-pointer-types]
    
    There are two things wrong here:
    
    - The naming is very confusing, we now have a member named 'phys'
      that doesn't refer to a phys_addr_t but a dma_addr_t. If we needed
      a dma address, it should be named 'dma' to avoid confusion, and
      to make it less likely that someone passes it into a function that
      expects a physical address.
    
    - The dma address is not used at all at this point. It may have been
      designed to support DMA in the future, but today it doesn't, so
      the only effect right now is to make transfers artificially slower
      by using uncached memory instead of cached memory for a temporary
      buffer.
    
    This removes the unused structure member and instead changes the code
    to call devm_kcalloc(), which matches the usage of the 'base' pointer
    as an array of temporary buffers.
    
    Fixes: db809859c8ce ("slimbus: qcom: fix incompatible pointer warning")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/slimbus/qcom-ctrl.c
+++ drivers/slimbus/qcom-ctrl.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/clk.h>
 #include <linux/of.h>
-#include <linux/dma-mapping.h>
 #include <linux/pm_runtime.h>
 #include "slimbus.h"
 
@@ -94,7 +93,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct slim_ctrl_buf {
 	void		*base;
-	dma_addr_t	phy;
 	spinlock_t	lock;
 	int		head;
 	int		tail;
@@ -580,17 +578,15 @@ static int qcom_slim_probe(struct platform_device *pdev)
 	if (ret)
 		goto err_rclk_enable_failed;
 
-	ctrl->tx.base = dmam_alloc_coherent(&pdev->dev,
-					   (ctrl->tx.sl_sz * ctrl->tx.n),
-					   &ctrl->tx.phy, GFP_KERNEL);
+	ctrl->tx.base = devm_kcalloc(&pdev->dev, ctrl->tx.n, ctrl->tx.sl_sz,
+				     GFP_KERNEL);
 	if (!ctrl->tx.base) {
 		ret = -ENOMEM;
 		goto err;
 	}
 
-	ctrl->rx.base = dmam_alloc_coherent(&pdev->dev,
-					   (ctrl->rx.sl_sz * ctrl->rx.n),
-					   &ctrl->rx.phy, GFP_KERNEL);
+	ctrl->rx.base = devm_kcalloc(&pdev->dev,ctrl->rx.n, ctrl->rx.sl_sz,
+				     GFP_KERNEL);
 	if (!ctrl->rx.base) {
 		ret = -ENOMEM;
 		goto err;
