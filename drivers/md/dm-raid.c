62c3716e62f52e381d140c778d2e013bc868340d
Wei Yongjun <weiyongjun1@huawei.com>
2018-01-02 06:18:10 +0000

    dm raid: make raid_sets symbol static
    Fixes the following sparse warning:
    
    drivers/md/dm-raid.c:33:1: warning:
     symbol 'raid_sets' was not declared. Should it be static?
    
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>

--- drivers/md/dm-raid.c
+++ drivers/md/dm-raid.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	MIN_RAID456_JOURNAL_SPACE (4*2048)
 
 /* Global list of all raid sets */
-LIST_HEAD(raid_sets);
+static LIST_HEAD(raid_sets);
 
 static bool devices_handle_discard_safely = false;
 
