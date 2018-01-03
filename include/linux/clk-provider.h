1d6aaaeca007179822dcc4aa582883ad31b634f7
Geert Uytterhoeven <geert+renesas@glider.be>
2018-01-03 12:06:16 +0100

    clk: Show symbolic clock flags in debugfs
    Currently the virtual "clk_flags" file in debugfs shows the numeric
    value of the top-level framework flags for the specified clock.
    Hence the user must manually interpret these values.
    
    Moreover, on big-endian 64-bit systems, the wrong half of the value is
    shown, due to the cast from "unsigned long *" to "u32 *".
    
    Fix both issues by showing the symbolic flag names instead.
    Any non-standard flags are shown as a hex number.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * flags used across common struct clk.  these flags should only affect the
  * top-level framework.  custom flags for dealing with hardware specifics
  * belong in struct clk_foo
+ *
+ * Please update clk_flags[] in drivers/clk/clk.c when making changes here!
  */
 #define CLK_SET_RATE_GATE	BIT(0) /* must be gated across rate change */
 #define CLK_SET_PARENT_GATE	BIT(1) /* must be gated across re-parent */
