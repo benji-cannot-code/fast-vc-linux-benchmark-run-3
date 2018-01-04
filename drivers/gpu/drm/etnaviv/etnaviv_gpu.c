c34758268451a4f66b1110366cb2dfa677a0d09a
Lucas Stach <l.stach@pengutronix.de>
2018-01-04 13:40:03 +0100

    drm/etnaviv: don't fail to build on arches without PHYS_OFFSET
    Some architecture ports like ARC don't provide the PHYS_OFFSET symbol.
    Define it to 0 in that case, which is the most conservative default in
    the usage context of the etnaviv driver.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/gpu/drm/etnaviv/etnaviv_gpu.c
+++ drivers/gpu/drm/etnaviv/etnaviv_gpu.c
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "state_hi.xml.h"
 #include "cmdstream.xml.h"
 
+#ifndef PHYS_OFFSET
+#define PHYS_OFFSET 0
+#endif
+
 static const struct platform_device_id gpu_ids[] = {
 	{ .name = "etnaviv-gpu,2d" },
 	{ },
