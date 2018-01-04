ace9d68ae2be50967df86a44eaabe782b5e9b185
Anusha Srivatsa <anusha.srivatsa@intel.com>
2018-01-04 15:51:42 -0800

    drm/i915/dmc: DMC 1.07 for Cannonlake
    There is a new version of DMC available for CNL.
    
    The release notes mentions:
    1. Fix for the issue where DC_STATE was getting enabled
    even when disabled by driver causing data corruption
    
    v2: Since the firmware is  merged to linux-firmware.git,
    add MODULE_FIRMWARE.
    
    v3: rebased. Correct commit message(Jani)
    
    Cc: Jani Saarinen <jani.saarinen@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1515109902-14076-1-git-send-email-anusha.srivatsa@intel.com

--- drivers/gpu/drm/i915/intel_csr.c
+++ drivers/gpu/drm/i915/intel_csr.c
@@ -38,8 +38,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I915_CSR_GLK "i915/glk_dmc_ver1_04.bin"
 #define GLK_CSR_VERSION_REQUIRED	CSR_VERSION(1, 4)
 
-#define I915_CSR_CNL "i915/cnl_dmc_ver1_06.bin"
-#define CNL_CSR_VERSION_REQUIRED	CSR_VERSION(1, 6)
+#define I915_CSR_CNL "i915/cnl_dmc_ver1_07.bin"
+MODULE_FIRMWARE(I915_CSR_CNL);
+#define CNL_CSR_VERSION_REQUIRED	CSR_VERSION(1, 7)
 
 #define I915_CSR_KBL "i915/kbl_dmc_ver1_04.bin"
 MODULE_FIRMWARE(I915_CSR_KBL);
