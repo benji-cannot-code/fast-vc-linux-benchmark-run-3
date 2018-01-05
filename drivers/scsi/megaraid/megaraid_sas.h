fcaa90bea233efb437ad03f9d079f12eca66a556
Shivasharan S <shivasharan.srikanteshwara@broadcom.com>
2018-01-05 05:27:42 -0800

    scsi: megaraid_sas: Avoid firing DCMDs while OCR is in progress
    Driver needs to avoid PCI writes while OCR is in progress.  Use
    reset_mutex to synchronize between firing DCMDs MR_DCMD_PD_GET_INFO and
    MR_DCMD_DRV_GET_TARGET_PROP while OCR is triggered.  Without this fix,
    if Device/VD add/creation is in progress and at the same time MR
    Firmware is going through OCR, user may see OCR never completed and it
    may need system reboot.  This scenario is rare to occur. Fix is provided
    as part of review.
    
    Signed-off-by: Sumit Saxena <sumit.saxena@broadcom.com>
    Signed-off-by: Shivasharan S <shivasharan.srikanteshwara@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/megaraid/megaraid_sas.h
+++ drivers/scsi/megaraid/megaraid_sas.h
@@ -2189,7 +2189,6 @@ struct megasas_instance {
 	struct megasas_evt_detail *evt_detail;
 	dma_addr_t evt_detail_h;
 	struct megasas_cmd *aen_cmd;
-	struct mutex hba_mutex;
 	struct semaphore ioctl_sem;
 
 	struct Scsi_Host *host;
