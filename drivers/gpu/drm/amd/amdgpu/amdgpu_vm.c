8202bbfe3b5abe128d11971876654710c043c3df
Alex Deucher <alexander.deucher@amd.com>
2018-01-05 10:33:48 -0500

    drm/amdgpu: drop extra tlb invalidation in gpuvm
    We only need to flush the HDP here, not invalidate the TLB.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vm.c
@@ -858,7 +858,6 @@ restart:
 		/* Flush HDP */
 		mb();
 		amdgpu_asic_flush_hdp(adev);
-		amdgpu_gart_flush_gpu_tlb(adev, 0);
 	} else if (params.ib->length_dw == 0) {
 		amdgpu_job_free(job);
 	} else {
@@ -1460,7 +1459,6 @@ int amdgpu_vm_bo_update(struct amdgpu_device *adev,
 		/* Flush HDP */
 		mb();
 		amdgpu_asic_flush_hdp(adev);
-		amdgpu_gart_flush_gpu_tlb(adev, 0);
 	}
 
 	spin_lock(&vm->status_lock);
