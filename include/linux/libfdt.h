51b7bbf5cac010df6cba30cec0f896572ffa550d
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-01-07 12:02:16 +0900

    libfdt: remove unnecessary include directive from <linux/libfdt.h>
    <linux/libfdt.h> is a wrapper of scripts/dtc/libfdt/libfdt.h
    It should not do more than its job.  In fact, libfdt.h depends
    on fdt.h, and scripts/dtc/libfdt/libfdt.h includes fdt.h already.
    Trust the work in the upstream DTC project.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- include/linux/libfdt.h
+++ include/linux/libfdt.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _INCLUDE_LIBFDT_H_
 
 #include <linux/libfdt_env.h>
-#include "../../scripts/dtc/libfdt/fdt.h"
 #include "../../scripts/dtc/libfdt/libfdt.h"
 
 #endif /* _INCLUDE_LIBFDT_H_ */
