09def4b5522a855f8a743fc4409fe23b21726e87
Zhu Yanjun <yanjun.zhu@oracle.com>
2018-01-07 07:08:48 -0500

    IB/rxe: add the static type to the variable
    The variable recv_sockets is only used in the file rxe_net.c. So
    it is better to add static type to it.
    
    CC: Srinivas Eeda <srinivas.eeda@oracle.com>
    CC: Joe Jin <joe.jin@oracle.com>
    CC: Junxiao Bi <junxiao.bi@oracle.com>
    Signed-off-by: Zhu Yanjun <yanjun.zhu@oracle.com>
    Reviewed-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/sw/rxe/rxe_net.c
+++ drivers/infiniband/sw/rxe/rxe_net.c
@@ -83,7 +83,7 @@ struct rxe_dev *get_rxe_by_name(const char *name)
 }
 
 
-struct rxe_recv_sockets recv_sockets;
+static struct rxe_recv_sockets recv_sockets;
 
 struct device *rxe_dma_device(struct rxe_dev *rxe)
 {
