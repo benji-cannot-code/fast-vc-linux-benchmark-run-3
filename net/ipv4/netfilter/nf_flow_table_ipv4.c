4a54346c7dbd825018a39ce3755f8666ad0b341b
Pablo Neira Ayuso <pablo@netfilter.org>
2018-01-07 01:04:22 +0100

    netfilter: flow table support for the mixed IPv4/IPv6 family
    This patch adds the IPv6 flow table type, that implements the datapath
    flow table to forward IPv6 traffic.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv4/netfilter/nf_flow_table_ipv4.c
+++ net/ipv4/netfilter/nf_flow_table_ipv4.c
@@ -203,7 +203,7 @@ static bool nf_flow_exceeds_mtu(struct sk_buff *skb, const struct rtable *rt)
 	return false;
 }
 
-static unsigned int
+unsigned int
 nf_flow_offload_ip_hook(void *priv, struct sk_buff *skb,
 			const struct nf_hook_state *state)
 {
@@ -255,6 +255,7 @@ nf_flow_offload_ip_hook(void *priv, struct sk_buff *skb,
 
 	return NF_STOLEN;
 }
+EXPORT_SYMBOL_GPL(nf_flow_offload_ip_hook);
 
 static struct nf_flowtable_type flowtable_ipv4 = {
 	.family		= NFPROTO_IPV4,
