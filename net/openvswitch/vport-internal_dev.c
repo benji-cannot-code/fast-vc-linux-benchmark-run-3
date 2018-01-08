dae43c137a1c009138d18dbcc8a2afe61ea74e45
Yang Shi <yang.s@alibaba-inc.com>
2018-01-09 03:52:53 +0800

    net: ovs: remove unused hardirq.h
    Preempt counter APIs have been split out, currently, hardirq.h just
    includes irq_enter/exit APIs which are not used by openvswitch at all.
    
    So, remove the unused hardirq.h.
    
    Signed-off-by: Yang Shi <yang.s@alibaba-inc.com>
    Acked-by: Pravin B Shelar <pshelar@ovn.org>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: dev@openvswitch.org
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/openvswitch/vport-internal_dev.c
+++ net/openvswitch/vport-internal_dev.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 02110-1301, USA
  */
 
-#include <linux/hardirq.h>
 #include <linux/if_vlan.h>
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
