792c671db225a05a4479d33d3b985d9b4f6ab2d1
Chris Brandt <chris.brandt@renesas.com>
2018-01-08 07:30:53 -0500

    usb: renesas_usbhs: Add support for RZ/A1
    This patch adds the capability to support RZ/A1 SoCs.
    
    Signed-off-by: Chris Brandt <chris.brandt@renesas.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/renesas_usbhs/common.c
+++ drivers/usb/renesas_usbhs/common.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "common.h"
 #include "rcar2.h"
 #include "rcar3.h"
+#include "rza.h"
 
 /*
  *		image of renesas_usbhs
@@ -489,6 +490,10 @@ static const struct of_device_id usbhs_of_match[] = {
 		.compatible = "renesas,rcar-gen3-usbhs",
 		.data = (void *)USBHS_TYPE_RCAR_GEN3,
 	},
+	{
+		.compatible = "renesas,rza1-usbhs",
+		.data = (void *)USBHS_TYPE_RZA1,
+	},
 	{ },
 };
 MODULE_DEVICE_TABLE(of, usbhs_of_match);
@@ -517,6 +522,11 @@ static struct renesas_usbhs_platform_info *usbhs_parse_dt(struct device *dev)
 	    dparam->type == USBHS_TYPE_RCAR_GEN3 ||
 	    dparam->type == USBHS_TYPE_RCAR_GEN3_WITH_PLL) {
 		dparam->has_usb_dmac = 1;
+		dparam->pipe_configs = usbhsc_new_pipe;
+		dparam->pipe_size = ARRAY_SIZE(usbhsc_new_pipe);
+	}
+
+	if (dparam->type == USBHS_TYPE_RZA1) {
 		dparam->pipe_configs = usbhsc_new_pipe;
 		dparam->pipe_size = ARRAY_SIZE(usbhsc_new_pipe);
 	}
@@ -591,6 +601,9 @@ static int usbhs_probe(struct platform_device *pdev)
 			if (ret < 0)
 				dev_err(&pdev->dev, "no notifier registered\n");
 		}
+		break;
+	case USBHS_TYPE_RZA1:
+		priv->pfunc = usbhs_rza1_ops;
 		break;
 	default:
 		if (!info->platform_callback.get_id) {
