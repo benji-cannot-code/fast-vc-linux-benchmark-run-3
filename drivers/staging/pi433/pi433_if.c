c8f768dee2dd54bd5c1d009e6a77980e763e035a
Valentin Vidic <Valentin.Vidic@CARNet.hr>
2018-01-08 18:38:31 +0100

    staging: pi433: replace shifting with BIT macro
    Fixes checkpatch warnings:
    
      CHECK: Prefer using the BIT macro
    
    Signed-off-by: Valentin Vidic <Valentin.Vidic@CARNet.hr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/pi433/pi433_if.c
+++ drivers/staging/pi433/pi433_if.c
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "pi433_if.h"
 #include "rf69.h"
 
-#define N_PI433_MINORS			(1U << MINORBITS) /*32*/	/* ... up to 256 */
+#define N_PI433_MINORS			BIT(MINORBITS) /*32*/	/* ... up to 256 */
 #define MAX_MSG_SIZE			900	/* min: FIFO_SIZE! */
 #define MSG_FIFO_SIZE			65536   /* 65536 = 2^16  */
 #define NUM_DIO				2
