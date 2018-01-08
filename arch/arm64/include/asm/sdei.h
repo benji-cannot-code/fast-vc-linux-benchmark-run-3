ca75f401ab99542a082936bd6902540462e7bbbd
James Morse <james.morse@arm.com>
2018-01-08 15:38:09 +0000

    firmware: arm_sdei: Add driver for Software Delegated Exceptions
    The Software Delegated Exception Interface (SDEI) is an ARM standard
    for registering callbacks from the platform firmware into the OS.
    This is typically used to implement firmware notifications (such as
    firmware-first RAS) or promote an IRQ that has been promoted to a
    firmware-assisted NMI.
    
    Add the code for detecting the SDEI version and the framework for
    registering and unregistering events. Subsequent patches will add the
    arch-specific backend code and the necessary power management hooks.
    
    Only shared events are supported, power management, private events and
    discovery for ACPI systems will be added by later patches.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

---
+++ arch/arm64/include/asm/sdei.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2017 Arm Ltd.
+#ifndef __ASM_SDEI_H
+#define __ASM_SDEI_H
+
+/* Later patches add the arch specific bits */
+
+#endif /* __ASM_SDEI_H */
