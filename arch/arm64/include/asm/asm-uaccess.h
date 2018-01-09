9e98a35521b9911c7284aa41554d39d84bec2704
Christoffer Dall <christoffer.dall@linaro.org>
2018-01-09 11:51:58 +0100

    arm64: mm: Add additional parameter to uaccess_ttbr0_enable
    Add an extra temporary register parameter to uaccess_ttbr0_enable which
    is about to be required for arm64 PAN support.
    
    This patch doesn't introduce any functional change but ensures that the
    kernel compiles once the KVM/ARM tree is merged with the arm64 tree by
    ensuring a trivially mergable conflict with commit
    27a921e75711d924617269e0ba4adb8bae9fd0d1
    ("arm64: mm: Fix and re-enable ARM64_SW_TTBR0_PAN").
    
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/arm64/include/asm/asm-uaccess.h
+++ arch/arm64/include/asm/asm-uaccess.h
@@ -32,7 +32,7 @@ alternative_if_not ARM64_HAS_PAN
 alternative_else_nop_endif
 	.endm
 
-	.macro	uaccess_ttbr0_enable, tmp1, tmp2
+	.macro	uaccess_ttbr0_enable, tmp1, tmp2, tmp3
 alternative_if_not ARM64_HAS_PAN
 	save_and_disable_irq \tmp2		// avoid preemption
 	__uaccess_ttbr0_enable \tmp1
@@ -43,7 +43,7 @@ alternative_else_nop_endif
 	.macro	uaccess_ttbr0_disable, tmp1
 	.endm
 
-	.macro	uaccess_ttbr0_enable, tmp1, tmp2
+	.macro	uaccess_ttbr0_enable, tmp1, tmp2, tmp3
 	.endm
 #endif
 
