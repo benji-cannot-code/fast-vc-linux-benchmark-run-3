4b813e4c9c49f8adfd8249bab6b3f4c69129ced8
Gilad Ben-Yossef <gilad@benyossef.com>
2018-01-09 10:24:31 +0000

    staging: ccree: add missing include
    Add the missing include of include file with function declarations.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ccree/cc_debugfs.c
+++ drivers/staging/ccree/cc_debugfs.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/stringify.h>
 #include "cc_driver.h"
 #include "cc_crypto_ctx.h"
+#include "cc_debugfs.h"
 
 struct cc_debugfs_ctx {
 	struct dentry *dir;
