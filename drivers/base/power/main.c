795021e5bfaa835d44d269dc1dc578a9182cfbcd
Ulf Hansson <ulf.hansson@linaro.org>
2018-01-09 10:03:40 +0100

    PM / core: Propagate wakeup_path status flag in __device_suspend_late()
    Currently the wakeup_path status flag becomes propagated from a child
    device to its parent device at __device_suspend(). This allows a driver
    dealing with a parent device to act on the flag from its ->suspend()
    callback.
    
    However, in situations when the wakeup_path status flag needs to be set
    from a ->suspend_late() callback, its value doesn't get propagated to the
    parent by the PM core. Let's address this limitation, by also propagating
    the flag at __device_suspend_late().
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1448,6 +1448,21 @@ int dpm_suspend_noirq(pm_message_t state)
 	return ret;
 }
 
+static void dpm_propagate_wakeup_to_parent(struct device *dev)
+{
+	struct device *parent = dev->parent;
+
+	if (!parent)
+		return;
+
+	spin_lock_irq(&parent->power.lock);
+
+	if (dev->power.wakeup_path && !parent->power.ignore_children)
+		parent->power.wakeup_path = true;
+
+	spin_unlock_irq(&parent->power.lock);
+}
+
 static pm_callback_t dpm_subsys_suspend_late_cb(struct device *dev,
 						pm_message_t state,
 						const char **info_p)
@@ -1528,6 +1543,7 @@ Run:
 		async_error = error;
 		goto Complete;
 	}
+	dpm_propagate_wakeup_to_parent(dev);
 
 Skip:
 	dev->power.is_late_suspended = true;
@@ -1659,21 +1675,6 @@ static int legacy_suspend(struct device *dev, pm_message_t state,
 	initcall_debug_report(dev, calltime, error, state, info);
 
 	return error;
-}
-
-static void dpm_propagate_wakeup_to_parent(struct device *dev)
-{
-	struct device *parent = dev->parent;
-
-	if (!parent)
-		return;
-
-	spin_lock_irq(&parent->power.lock);
-
-	if (dev->power.wakeup_path && !parent->power.ignore_children)
-		parent->power.wakeup_path = true;
-
-	spin_unlock_irq(&parent->power.lock);
 }
 
 static void dpm_clear_superiors_direct_complete(struct device *dev)
