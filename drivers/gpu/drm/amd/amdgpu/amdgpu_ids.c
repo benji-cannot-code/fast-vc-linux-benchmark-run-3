717d631ba504ae8b5f0602d6cb6230a2fe96aff8
Christian König <christian.koenig@amd.com>
2018-01-09 19:32:58 +0100

    drm/amdgpu: trace allocated PASIDs
    Trace all allocated PASIDs.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Chunming Zhou <david1.zhou@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ids.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ids.c
@@ -70,6 +70,9 @@ int amdgpu_pasid_alloc(unsigned int bits)
 			break;
 	}
 
+	if (pasid >= 0)
+		trace_amdgpu_pasid_allocated(pasid);
+
 	return pasid;
 }
 
@@ -79,6 +82,7 @@ int amdgpu_pasid_alloc(unsigned int bits)
  */
 void amdgpu_pasid_free(unsigned int pasid)
 {
+	trace_amdgpu_pasid_freed(pasid);
 	ida_simple_remove(&amdgpu_pasid_ida, pasid);
 }
 
