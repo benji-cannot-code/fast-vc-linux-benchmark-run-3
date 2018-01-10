898cf64b7f241b5ce34559f3f510e4a549dd2222
Shuah Khan <shuahkh@osg.samsung.com>
2018-01-09 18:36:55 -0700

    selftests: kselftest_install.sh: Add SPDX license identifier
    Replace GPL license statement with SPDX GPL-2.0 license identifier.
    
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>

--- tools/testing/selftests/kselftest_install.sh
+++ tools/testing/selftests/kselftest_install.sh
@@ -1,13 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
 #
 # Kselftest Install
 # Install kselftest tests
 # Author: Shuah Khan <shuahkh@osg.samsung.com>
 # Copyright (C) 2015 Samsung Electronics Co., Ltd.
-
-# This software may be freely redistributed under the terms of the GNU
-# General Public License (GPLv2).
 
 install_loc=`pwd`
 
