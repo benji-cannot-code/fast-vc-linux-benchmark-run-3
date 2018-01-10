4fd2bd9c9e71ad0778ed7263fefa19c4ab801c32
Jeffy Chen <jeffy.chen@rock-chips.com>
2018-01-10 17:23:42 +0100

    drm/bridge: analogix_dp: Fix connector and encoder cleanup
    Since we are initing connector in the core driver and encoder in the
    plat driver, let's clean them up in the right places.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Signed-off-by: Thierry Escande <thierry.escande@collabora.com>
    Reviewed-by: Andrzej Hajda <a.hajda@samsung.com>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180110162348.22765-3-thierry.escande@collabora.com

--- drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
@@ -1408,7 +1408,6 @@ analogix_dp_bind(struct device *dev, struct drm_device *drm_dev,
 	ret = analogix_dp_create_bridge(drm_dev, dp);
 	if (ret) {
 		DRM_ERROR("failed to create bridge (%d)\n", ret);
-		drm_encoder_cleanup(dp->encoder);
 		goto err_disable_pm_runtime;
 	}
 
@@ -1431,7 +1430,6 @@ void analogix_dp_unbind(struct analogix_dp_device *dp)
 {
 	analogix_dp_bridge_disable(dp->bridge);
 	dp->connector.funcs->destroy(&dp->connector);
-	dp->encoder->funcs->destroy(dp->encoder);
 
 	if (dp->plat_data->panel) {
 		if (drm_panel_unprepare(dp->plat_data->panel))
