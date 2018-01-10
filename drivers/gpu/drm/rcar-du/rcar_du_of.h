fe85f614bf5188bb2729470d4ed513889e9b8a28
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2018-01-10 02:40:27 +0200

    drm: rcar-du: Fix legacy DT to create LVDS encoder nodes
    The internal LVDS encoders now have their own DT bindings. Before
    switching the driver infrastructure to those new bindings, implement
    backward-compatibility through live DT patching.
    
    Patching is disabled and will be enabled along with support for the new
    DT bindings in the DU driver.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Frank Rowand <frank.rowand@sony.com>

---
+++ drivers/gpu/drm/rcar-du/rcar_du_of.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * rcar_du_of.h - Legacy DT bindings compatibility
+ *
+ * Copyright (C) 2018 Laurent Pinchart <laurent.pinchart@ideasonboard.com>
+ */
+#ifndef __RCAR_DU_OF_H__
+#define __RCAR_DU_OF_H__
+
+#include <linux/init.h>
+
+struct of_device_id;
+
+#ifdef CONFIG_DRM_RCAR_LVDS
+void __init rcar_du_of_init(const struct of_device_id *of_ids);
+#else
+static inline void rcar_du_of_init(const struct of_device_id *of_ids) { }
+#endif /* CONFIG_DRM_RCAR_LVDS */
+
+#endif /* __RCAR_DU_OF_H__ */
