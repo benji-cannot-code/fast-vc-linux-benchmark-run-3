c7e10511a86d8799088551e5c43a4f052dce8fec
Evan Quan <evan.quan@amd.com>
2018-01-10 15:37:20 -0500

    drm/amd/powerplay: export the thermal ranges of VI asics (V2)
    V2: move the SMU7Thermal structure to newly created header file
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/inc/pp_thermal.h
+++ drivers/gpu/drm/amd/powerplay/inc/pp_thermal.h
@@ -32,4 +32,10 @@ static const struct PP_TemperatureRange SMU7ThermalWithDelayPolicy[] =
 	{ 120000, 120000},
 };
 
+static const struct PP_TemperatureRange SMU7ThermalPolicy[] =
+{
+	{-273150,  99000},
+	{ 120000, 120000},
+};
+
 #endif
