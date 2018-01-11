bd94204aeb2c8b1a5a870a15167d830fc0099b33
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-01-11 22:39:39 +0900

    kconfig: make input_mode static
    Sparse reports:
      warning: symbol 'input_mode' was not declared. Should it be static?
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -36,7 +36,8 @@ enum input_mode {
 	savedefconfig,
 	listnewconfig,
 	olddefconfig,
-} input_mode = oldaskconfig;
+};
+static enum input_mode input_mode = oldaskconfig;
 
 static int indent = 1;
 static int tty_stdio;
