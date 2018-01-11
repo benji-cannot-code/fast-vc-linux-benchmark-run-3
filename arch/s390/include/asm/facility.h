fb575012d7e6fd3fc61579c9ea16798397602b83
Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
2018-01-11 12:13:18 +0100

    s390/tools: generate header files in arch/s390/include/generated/
    Previously, the generated dis.h and facilities.h header files have been
    stored in include/generated.  Because they are s390 specific, store them
    in the arch/s390/include/generated/asm/ directory.  Also update
    references to the header files respectively.
    
    To prevent name collisions with those header files in asm/ that
    include the generated ones, rename the generated headers files
    and add an -defs suffix.  Also update the generators to create
    the ifdef guards respectively.
    
    Signed-off-by: Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
    Acked-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/facility.h
+++ arch/s390/include/asm/facility.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_FACILITY_H
 #define __ASM_FACILITY_H
 
-#include <generated/facilities.h>
+#include <asm/facility-defs.h>
 #include <linux/string.h>
 #include <linux/preempt.h>
 #include <asm/lowcore.h>
