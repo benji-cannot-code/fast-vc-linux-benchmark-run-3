25fff0966281e39e87de67e2fde7f6b30e36dcde
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-01-11 22:05:40 +0900

    fixdep: remove unnecessary <arpa/inet.h> inclusion
    <arpa/inet.h> was included for ntohl(), but it was removed by
    commit dee81e988674 ("fixdep: faster CONFIG_ search").
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- scripts/basic/fixdep.c
+++ scripts/basic/fixdep.c
@@ -113,7 +113,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <limits.h>
 #include <ctype.h>
-#include <arpa/inet.h>
 
 int insert_extra_deps;
 char *target;
