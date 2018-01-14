0b64a25d6aa2b44b602d1366d1f50840ff5908c8
Eric W. Biederman <ebiederm@xmission.com>
2018-01-13 18:40:33 -0600

    signal/blackfin: Remove pointless UID16_SIGINFO_COMPAT_NEEDED
    Nothing tests this define so just remove it.
    
    I suspect the intention was to make the uid field in siginfo 16bit
    however I can't find any code that ever tested this defined, and
    even if it did it the layout has been this way for 8 years so
    changing it now would break the ABI with userspace.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- arch/blackfin/include/uapi/asm/siginfo.h
+++ arch/blackfin/include/uapi/asm/siginfo.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm-generic/siginfo.h>
 
-#define UID16_SIGINFO_COMPAT_NEEDED
-
 #define si_uid16	_sifields._kill._uid
 
 #endif /* _UAPI_BFIN_SIGINFO_H */
