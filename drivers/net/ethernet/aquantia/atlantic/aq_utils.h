724503978dbc6181ecac897b4d9c758cb675bba0
Igor Russkikh <igor.russkikh@aquantia.com>
2018-01-15 16:41:13 +0300

    net: aquantia: Eliminate AQ_DIMOF, replace with ARRAY_SIZE
    Signed-off-by: Igor Russkikh <igor.russkikh@aquantia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/aquantia/atlantic/aq_utils.h
+++ drivers/net/ethernet/aquantia/atlantic/aq_utils.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "aq_common.h"
 
-#define AQ_DIMOF(_ARY_)  ARRAY_SIZE(_ARY_)
-
 struct aq_obj_s {
 	atomic_t flags;
 };
