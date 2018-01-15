a690d3e085f54a0bc980ec488951cfd0ab0e9361
Hawking Zhang <Hawking.Zhang@amd.com>
2018-01-15 15:11:06 +0800

    drm/amd/pp: include new ip and ip offset headers
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/inc/pp_soc15.h
+++ drivers/gpu/drm/amd/powerplay/inc/pp_soc15.h
@@ -24,7 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef PP_SOC15_H
 #define PP_SOC15_H
 
-#include "soc15ip.h"
+#include "soc15_hw_ip.h"
+#include "vega10_ip_offset.h"
 
 inline static uint32_t soc15_get_register_offset(
 		uint32_t hw_id,
