c2c1f89126f068f4230f83b81a9fe529d23497be
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2018-01-15 21:00:00 +0100

    staging: dgnc: delete dgnc_neo.* files
    As the Neo devices were never actually supported by the driver, delete
    the files that purported to control that type of device as they are not
    needed.  This will let us shrink the driver a lot over time.
    
    Cc: Lidza Louina <lidza.louina@gmail.com>
    Cc: Mark Hounschell <markh@compro.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/dgnc/dgnc_driver.c
+++ drivers/staging/dgnc/dgnc_driver.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dgnc_pci.h"
 #include "dgnc_tty.h"
 #include "dgnc_cls.h"
-#include "dgnc_neo.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Digi International, http://www.digi.com");
