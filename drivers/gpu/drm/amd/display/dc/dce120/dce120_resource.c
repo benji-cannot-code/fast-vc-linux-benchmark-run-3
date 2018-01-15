ed2a160021a5b7e1e6606279ad8c915a822ca76c
Hawking Zhang <Hawking.Zhang@amd.com>
2018-01-15 15:43:23 +0800

    drm/amd/dc: include new ip and ip_offset headers
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce120/dce120_resource.c
+++ drivers/gpu/drm/amd/display/dc/dce120/dce120_resource.c
@@ -57,7 +57,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dce/dce_12_0_offset.h"
 #include "dce/dce_12_0_sh_mask.h"
-#include "soc15ip.h"
+#include "soc15_hw_ip.h"
+#include "vega10_ip_offset.h"
 #include "nbio/nbio_6_1_offset.h"
 #include "reg_helper.h"
 
