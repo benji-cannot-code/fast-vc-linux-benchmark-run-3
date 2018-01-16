9f5863750e44bb08b5c900f70bb7fb4da1b4ee0a
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2018-01-16 20:53:14 +1100

    usb: uhci: Add clk support to uhci-platform
    The Aspeed SoCs use uhci-platform. With the new dynamic clock
    control framework, the corresponding IP block clock must be
    properly enabled.
    
    This is a simplified variant of what ehci-platform does, it
    looks for *one* clock attached to the device, and if it's
    there, enables it.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/uhci-hcd.h
+++ drivers/usb/host/uhci-hcd.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/list.h>
 #include <linux/usb.h>
+#include <linux/clk.h>
 
 #define usb_packetid(pipe)	(usb_pipein(pipe) ? USB_PID_IN : USB_PID_OUT)
 #define PIPE_DEVEP_MASK		0x0007ff00
@@ -447,6 +448,8 @@ struct uhci_hcd {
 
 	int total_load;				/* Sum of array values */
 	short load[MAX_PHASE];			/* Periodic allocations */
+
+	struct clk *clk;			/* (optional) clock source */
 
 	/* Reset host controller */
 	void	(*reset_hc) (struct uhci_hcd *uhci);
