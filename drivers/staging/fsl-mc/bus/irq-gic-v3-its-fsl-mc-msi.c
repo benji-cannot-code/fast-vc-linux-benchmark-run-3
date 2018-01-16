e249159bae384facee87ace961c7af5266700c1f
Ioana Radulescu <ruxandra.radulescu@nxp.com>
2018-01-16 15:19:08 +0200

    staging: fsl-mc: Update include header
    With the declarations of its_fsl_mc_msi_init/cleanup() now removed
    from fsl-mc-private.h, irq-gic-v3-its-fsl-mc-msi.c only needs a
    couple of definitions from mc.h, so include that header directly.
    
    Signed-off-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>
    Reviewed-by: Laurentiu Tudor <laurentiu.tudor@nxp.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fsl-mc/bus/irq-gic-v3-its-fsl-mc-msi.c
+++ drivers/staging/fsl-mc/bus/irq-gic-v3-its-fsl-mc-msi.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/msi.h>
 #include <linux/of.h>
 #include <linux/of_irq.h>
-#include "fsl-mc-private.h"
+#include "../include/mc.h"
 
 static struct irq_chip its_msi_irq_chip = {
 	.name = "ITS-fMSI",
