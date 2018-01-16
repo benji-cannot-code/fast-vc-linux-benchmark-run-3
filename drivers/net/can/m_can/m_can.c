0858a52780b6b6fa84bc8344ee1e3cf9da6bbf2e
Marc Kleine-Budde <mkl@pengutronix.de>
2018-01-16 13:58:53 +0100

    can: m_can: get rid of function free_m_can_dev()
    As the previous patch removed alloc_m_can_dev(), let's get rid of the
    corresponding free_m_can_dev() and call free_candev() directly.
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -1184,11 +1184,6 @@ static int m_can_set_mode(struct net_device *dev, enum can_mode mode)
 	return 0;
 }
 
-static void free_m_can_dev(struct net_device *dev)
-{
-	free_candev(dev);
-}
-
 /* Checks core release number of M_CAN
  * returns 0 if an unsupported device is detected
  * else it returns the release and step coded as:
@@ -1690,7 +1685,7 @@ static int m_can_plat_probe(struct platform_device *pdev)
 	goto disable_cclk_ret;
 
 failed_free_dev:
-	free_m_can_dev(dev);
+	free_candev(dev);
 disable_cclk_ret:
 	clk_disable_unprepare(cclk);
 disable_hclk_ret:
@@ -1754,7 +1749,7 @@ static int m_can_plat_remove(struct platform_device *pdev)
 	unregister_m_can_dev(dev);
 	platform_set_drvdata(pdev, NULL);
 
-	free_m_can_dev(dev);
+	free_candev(dev);
 
 	return 0;
 }
