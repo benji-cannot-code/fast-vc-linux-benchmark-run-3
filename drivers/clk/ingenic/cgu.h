2ce882d1c964e429c4054a91dbdd52ef1c2ba04e
Paul Cercueil <paul@crapouillou.net>
2018-01-16 16:47:53 +0100

    clk: ingenic: support PLLs with no bypass bit
    The second PLL of the JZ4770 does not have a bypass bit.
    This commit makes it possible to support it with the current common CGU
    code.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Acked-by: Stephen Boyd <sboyd@codeaurora.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Maarten ter Huurne <maarten@treewalker.org>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/18479/
    Signed-off-by: James Hogan <jhogan@kernel.org>

--- drivers/clk/ingenic/cgu.h
+++ drivers/clk/ingenic/cgu.h
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @bypass_bit: the index of the bypass bit in the PLL control register
  * @enable_bit: the index of the enable bit in the PLL control register
  * @stable_bit: the index of the stable bit in the PLL control register
+ * @no_bypass_bit: if set, the PLL has no bypass functionality
  */
 struct ingenic_cgu_pll_info {
 	unsigned reg;
@@ -59,6 +60,7 @@ struct ingenic_cgu_pll_info {
 	u8 bypass_bit;
 	u8 enable_bit;
 	u8 stable_bit;
+	bool no_bypass_bit;
 };
 
 /**
