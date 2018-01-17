4e04e4f37ab4eb8fb08e587c64a4c9b49d011f36
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-01-18 01:28:15 +0900

    mmc: tmio: clear force_pio flag before starting data transfer
    Currently, force_pio is cleared when the driver exits.  Then, it
    resulted in clearing it in multiple places since MMC drivers in
    general have multiple exit points.
    
     tmio_mmc_reset_work - bails out on timeout
     tmio_process_mrq - error out when it cannot send a command
     tmio_mmc_finish_request - successful exit
    
    This is error-prone since we may miss to cover all bail-out points.
    
    To simplify the code, the data structure should be initialized just
    before used since we have a single entrance.  force_pio is only used
    for data transfer, so tmio_mmc_start_data() will be a suitable place
    to clear this flag.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/tmio_mmc_core.c
+++ drivers/mmc/host/tmio_mmc_core.c
@@ -279,7 +279,6 @@ static void tmio_mmc_reset_work(struct work_struct *work)
 
 	host->cmd = NULL;
 	host->data = NULL;
-	host->force_pio = false;
 
 	spin_unlock_irqrestore(&host->lock, flags);
 
@@ -760,6 +759,7 @@ static int tmio_mmc_start_data(struct tmio_mmc_host *host,
 
 	tmio_mmc_init_sg(host, data);
 	host->data = data;
+	host->force_pio = false;
 
 	/* Set transfer length / blocksize */
 	sd_ctrl_write16(host, CTL_SD_XFER_LEN, data->blksz);
@@ -851,7 +851,6 @@ static void tmio_process_mrq(struct tmio_mmc_host *host,
 	return;
 
 fail:
-	host->force_pio = false;
 	host->mrq = NULL;
 	mrq->cmd->error = ret;
 	mmc_request_done(host->mmc, mrq);
@@ -901,7 +900,6 @@ static void tmio_mmc_finish_request(struct tmio_mmc_host *host)
 	if (host->cmd != mrq->sbc) {
 		host->cmd = NULL;
 		host->data = NULL;
-		host->force_pio = false;
 		host->mrq = NULL;
 	}
 
