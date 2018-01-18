b8e2935c7c301b941b622dabb65e9d22e45836bc
Harry Wentland <harry.wentland@amd.com>
2018-01-18 09:54:44 -0500

    drm/amd/display: dc: Remove unused display_mode_vba.c
    We're currently not using this.
    
    v2: More files and includes to remove.
    
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dml/dml1_display_rq_dlg_calc.h
+++ drivers/gpu/drm/amd/display/dc/dml/dml1_display_rq_dlg_calc.h
@@ -28,9 +28,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __DISPLAY_RQ_DLG_CALC_H__
 
 #include "dml_common_defs.h"
-#include "display_rq_dlg_helpers.h"
 
 struct display_mode_lib;
+
+#include "display_rq_dlg_helpers.h"
 
 void dml1_extract_rq_regs(
 		struct display_mode_lib *mode_lib,
