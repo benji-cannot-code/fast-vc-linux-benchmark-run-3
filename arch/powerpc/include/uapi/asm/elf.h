2d819e212313598212d41365aaf32b7fcd4b6fa2
Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
2018-01-18 17:50:43 -0800

    powerpc/ptrace: Add memory protection key regset
    The AMR/IAMR/UAMOR are part of the program context.
    Allow it to be accessed via ptrace and through core files.
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/uapi/asm/elf.h
+++ arch/powerpc/include/uapi/asm/elf.h
@@ -98,6 +98,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ELF_NTMSPRREG	3	/* include tfhar, tfiar, texasr */
 #define ELF_NEBB	3	/* includes ebbrr, ebbhr, bescr */
 #define ELF_NPMU	5	/* includes siar, sdar, sier, mmcr2, mmcr0 */
+#define ELF_NPKEY	3	/* includes amr, iamr, uamor */
 
 typedef unsigned long elf_greg_t64;
 typedef elf_greg_t64 elf_gregset_t64[ELF_NGREG];
