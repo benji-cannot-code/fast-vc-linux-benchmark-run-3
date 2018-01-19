6175d56149f32cd420cdc3c61ce1535edaa64747
Samuel Li <Samuel.Li@amd.com>
2018-01-19 15:28:27 -0500

    drm/amdgpu: rename amdgpu_update_display_priority
    Rename as amdgpu_display_update_priority for consistency.
    
    Signed-off-by: Samuel Li <Samuel.Li@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v10_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v10_0.c
@@ -1206,7 +1206,7 @@ static void dce_v10_0_bandwidth_update(struct amdgpu_device *adev)
 	u32 num_heads = 0, lb_size;
 	int i;
 
-	amdgpu_update_display_priority(adev);
+	amdgpu_display_update_priority(adev);
 
 	for (i = 0; i < adev->mode_info.num_crtc; i++) {
 		if (adev->mode_info.crtcs[i]->base.enabled)
