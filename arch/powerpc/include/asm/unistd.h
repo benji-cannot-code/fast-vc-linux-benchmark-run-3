7fb635ed319bc89af17b2b24a00160ae96cd49e7
Ram Pai <linuxram@us.ibm.com>
2018-01-18 17:50:46 -0800

    powerpc: sys_pkey_mprotect() system call
    Patch provides the ability for a process to
    associate a pkey with a address range.
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/unistd.h
+++ arch/powerpc/include/asm/unistd.h
@@ -13,11 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <uapi/asm/unistd.h>
 
 
-#define NR_syscalls		386
+#define NR_syscalls		387
 
 #define __NR__exit __NR_exit
-
-#define __IGNORE_pkey_mprotect
 
 #ifndef __ASSEMBLY__
 
