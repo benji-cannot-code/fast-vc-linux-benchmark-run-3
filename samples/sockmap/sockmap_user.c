a37dafbb1b8fa668525024a8d821006618e9d62d
John Fastabend <john.fastabend@gmail.com>
2018-01-22 10:37:11 -0800

    bpf: sockmap set rlimit
    Avoid extra step of setting limit from cmdline and do it directly in
    the program.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- samples/sockmap/sockmap_user.c
+++ samples/sockmap/sockmap_user.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <time.h>
 
 #include <sys/time.h>
+#include <sys/resource.h>
 #include <sys/types.h>
 
 #include <linux/netlink.h>
@@ -448,6 +449,7 @@ enum {
 int main(int argc, char **argv)
 {
 	int iov_count = 1, length = 1024, rate = 1, verbose = 0;
+	struct rlimit r = {10 * 1024 * 1024, RLIM_INFINITY};
 	int opt, longindex, err, cg_fd = 0;
 	int test = PING_PONG;
 	char filename[256];
@@ -500,6 +502,11 @@ int main(int argc, char **argv)
 		fprintf(stderr, "%s requires cgroup option: --cgroup <path>\n",
 			argv[0]);
 		return -1;
+	}
+
+	if (setrlimit(RLIMIT_MEMLOCK, &r)) {
+		perror("setrlimit(RLIMIT_MEMLOCK)");
+		return 1;
 	}
 
 	snprintf(filename, sizeof(filename), "%s_kern.o", argv[0]);
