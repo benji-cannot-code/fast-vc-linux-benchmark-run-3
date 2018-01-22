4cb631741a7e7a26a83d4f1de50dd365cc1dfd4e
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2018-01-22 16:21:53 +0100

    sysfs: remove DEBUG defines
    It isn't needed at all in these files, dynamic debug is the best way to
    enable this type of thing, if you really want it.  As it is, these
    defines were not doing anything at all.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/sysfs/dir.c
+++ fs/sysfs/dir.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #define pr_fmt(fmt)	"sysfs: " fmt
-#undef DEBUG
 
 #include <linux/fs.h>
 #include <linux/kobject.h>
--- fs/sysfs/mount.c
+++ fs/sysfs/mount.c
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Please see Documentation/filesystems/sysfs.txt for more information.
  */
 
-#define DEBUG
-
 #include <linux/fs.h>
 #include <linux/magic.h>
 #include <linux/mount.h>
