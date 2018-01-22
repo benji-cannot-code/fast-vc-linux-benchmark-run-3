60d3134134173617c29f41846b1eb49f43208612
Niklas Cassel <niklas.cassel@axis.com>
2018-01-22 16:59:50 +0100

    net: stmmac: do not use a bitwise AND operator with a bool operand
    Doing a bitwise AND between a bool and an int is generally not a good idea.
    The bool will be promoted to an int with value 0 or 1,
    the int is generally regarded as true with a non-zero value,
    thus ANDing them has the potential to yield an undesired result.
    
    This commit fixes the following smatch warnings:
    
    drivers/net/ethernet/stmicro/stmmac/enh_desc.c:344 enh_desc_prepare_tx_desc() warn: maybe use && instead of &
    drivers/net/ethernet/stmicro/stmmac/dwmac4_descs.c:337 dwmac4_rd_prepare_tx_desc() warn: maybe use && instead of &
    drivers/net/ethernet/stmicro/stmmac/dwmac4_descs.c:380 dwmac4_rd_prepare_tso_tx_desc() warn: maybe use && instead of &
    
    Signed-off-by: Niklas Cassel <niklas.cassel@axis.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwmac4_descs.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac4_descs.c
@@ -335,7 +335,7 @@ static void dwmac4_rd_prepare_tx_desc(struct dma_desc *p, int is_fs, int len,
 	if (tx_own)
 		tdes3 |= TDES3_OWN;
 
-	if (is_fs & tx_own)
+	if (is_fs && tx_own)
 		/* When the own bit, for the first frame, has to be set, all
 		 * descriptors for the same frame has to be set before, to
 		 * avoid race condition.
@@ -378,7 +378,7 @@ static void dwmac4_rd_prepare_tso_tx_desc(struct dma_desc *p, int is_fs,
 	if (tx_own)
 		tdes3 |= TDES3_OWN;
 
-	if (is_fs & tx_own)
+	if (is_fs && tx_own)
 		/* When the own bit, for the first frame, has to be set, all
 		 * descriptors for the same frame has to be set before, to
 		 * avoid race condition.
