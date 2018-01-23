eb1c9b012ee6b53ff44a03b4404635322efdeff4
Alexei Starovoitov <ast@kernel.org>
2018-01-22 20:48:40 -0800

    selftests/bpf: fix test_dev_cgroup
    The test incorrectly doing
    mkdir /mnt/cgroup-test-work-dirtest-bpf-based-device-cgroup
    instead of
    mkdir /mnt/cgroup-test-work-dir/test-bpf-based-device-cgroup
    
    somehow such mkdir succeeds and new directory appears:
    /mnt/cgroup-test-work-dir/cgroup-test-work-dirtest-bpf-based-device-cgroup
    
    Later cleanup via nftw("/mnt/cgroup-test-work-dir", ...);
    doesn't walk this directory.
    "rmdir /mnt/cgroup-test-work-dir" succeeds, but bpf program and
    dangling cgroup stays in memory.
    That's a separate issue on a cgroup side.
    For now fix the test.
    
    Fixes: 37f1ba0909df ("selftests/bpf: add a test for device cgroup controller")
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/testing/selftests/bpf/test_dev_cgroup.c
+++ tools/testing/selftests/bpf/test_dev_cgroup.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DEV_CGROUP_PROG "./dev_cgroup.o"
 
-#define TEST_CGROUP "test-bpf-based-device-cgroup/"
+#define TEST_CGROUP "/test-bpf-based-device-cgroup/"
 
 int main(int argc, char **argv)
 {
