849c364b76f43c6ab395f3eb559bdd938ad7d233
Lucas Stach <l.stach@pengutronix.de>
2018-01-24 15:30:29 +0100

    drm/etnaviv: remove the need for a gpu-subsystem DT node
    The module autoloading can be triggered through the GPU core nodes
    and the necessary platform device for the DRM toplevel device will
    be instantiated on module init.
    
    Suggested-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Rob Herring <robh@kernel.org>

--- drivers/gpu/drm/etnaviv/etnaviv_gpu.c
+++ drivers/gpu/drm/etnaviv/etnaviv_gpu.c
@@ -1638,6 +1638,7 @@ static const struct of_device_id etnaviv_gpu_match[] = {
 	},
 	{ /* sentinel */ }
 };
+MODULE_DEVICE_TABLE(of, etnaviv_gpu_match);
 
 static int etnaviv_gpu_platform_probe(struct platform_device *pdev)
 {
