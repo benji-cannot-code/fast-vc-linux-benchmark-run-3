217ad129d2424cedadc6991543e8062f7cfc76b0
Qu Wenruo <wqu@suse.com>
2018-01-25 14:56:18 +0800

    btrfs: tree-checker: Replace root parameter with fs_info
    When inspecting the error message with real corruption, the "root=%llu"
    always shows "1" (root tree), instead of the correct owner.
    
    The problem is that we are getting @root from page->mapping->host, which
    points the same btree inode, so we will always get the same root.
    
    This makes the root owner output meaningless, and harder to port
    tree-checker to btrfs-progs.
    
    So get rid of the false and meaningless @root parameter and replace it
    with @fs_info.
    To get the owner, we can only rely on btrfs_header_owner() now.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/tree-checker.h
+++ fs/btrfs/tree-checker.h
@@ -26,14 +26,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Will check not only the item pointers, but also every possible member
  * in item data.
  */
-int btrfs_check_leaf_full(struct btrfs_root *root, struct extent_buffer *leaf);
+int btrfs_check_leaf_full(struct btrfs_fs_info *fs_info,
+			  struct extent_buffer *leaf);
 
 /*
  * Less strict leaf checker.
  * Will only check item pointers, not reading item data.
  */
-int btrfs_check_leaf_relaxed(struct btrfs_root *root,
+int btrfs_check_leaf_relaxed(struct btrfs_fs_info *fs_info,
 			     struct extent_buffer *leaf);
-int btrfs_check_node(struct btrfs_root *root, struct extent_buffer *node);
+int btrfs_check_node(struct btrfs_fs_info *fs_info, struct extent_buffer *node);
 
 #endif
