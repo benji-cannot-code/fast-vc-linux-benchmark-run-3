460f4d582468e8734fcaa396530aa35d34a33d70
Bjorn Helgaas <bhelgaas@google.com>
2018-01-26 14:12:23 -0600

    PCI: Add SPDX GPL-2.0 to replace COPYING boilerplate
    Add SPDX GPL-2.0 to all PCI files that referred to the kernel default
    "COPYING" file, which specifies GPL version 2.
    
    Remove the boilerplate language referring to the GPL and "COPYING", relying
    on the assertion in b24413180f56 ("License cleanup: add SPDX GPL-2.0
    license identifier to files with no license") that the SPDX identifier may
    be used instead of the full boilerplate text.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pci/hotplug/sgi_hotplug.c
+++ drivers/pci/hotplug/sgi_hotplug.c
@@ -1,9 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
- *
  * Copyright (C) 2005-2006 Silicon Graphics, Inc. All rights reserved.
  *
  * This work was based on the 2.4/2.6 kernel development by Dick Reigner.
