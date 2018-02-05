54c4784bfe76b8f6d08ccfba16acede28e818604
Boris Brezillon <boris.brezillon@bootlin.com>
2018-02-05 23:02:04 +0100

    mtd: nand: move raw NAND related code to the raw/ subdir
    As part of the process of sharing more code between different NAND
    based devices, we need to move all raw NAND related code to the raw/
    subdirectory.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>

--- drivers/mtd/sm_ftl.c
+++ drivers/mtd/sm_ftl.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/slab.h>
 #include <linux/mtd/nand_ecc.h>
-#include "nand/sm_common.h"
+#include "nand/raw/sm_common.h"
 #include "sm_ftl.h"
 
 
