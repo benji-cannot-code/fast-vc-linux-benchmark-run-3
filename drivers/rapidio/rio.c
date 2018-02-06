ea4efbe3d65d1e1c7cec02370732259b8694927c
Markus Elfring <elfring@users.sourceforge.net>
2018-02-06 15:39:41 -0800

    rapidio: delete an error message for a failed memory allocation in rio_init_mports()
    Patch series "RapidIO: Adjustments for some function implementations".
    
    This patch (of 7):
    
    Omit an extra message for a memory allocation failure in this function.
    
    This issue was detected by using the Coccinelle software.
    
    Link: http://lkml.kernel.org/r/dfd6887b-2521-cbf2-af5b-e70b5fa4c33c@users.sourceforge.net
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Acked-by: Alexandre Bounine <alexandre.bounine@idt.com>
    Cc: Matt Porter <mporter@kernel.crashing.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rapidio/rio.c
+++ drivers/rapidio/rio.c
@@ -2190,7 +2190,6 @@ int rio_init_mports(void)
 
 	work = kcalloc(n, sizeof *work, GFP_KERNEL);
 	if (!work) {
-		pr_err("RIO: no memory for work struct\n");
 		destroy_workqueue(rio_wq);
 		goto no_disc;
 	}
