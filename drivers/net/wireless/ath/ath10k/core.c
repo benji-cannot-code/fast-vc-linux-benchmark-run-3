83f8c5001b6a6bb87266cf913cbd41e1e9002c15
Ryan Hsu <ryanhsu@codeaurora.org>
2018-02-07 15:51:23 +0200

    Revert "ath10k: add sanity check to ie_len before parsing fw/board ie"
    This reverts commit 9ed4f91628737c820af6a1815b65bc06bd31518f.
    
    The commit introduced a regression that over read the ie with
    the padding.
    
    - the expected IE information
    
    ath10k_pci 0000:03:00.0: found firmware features ie (1 B)
    ath10k_pci 0000:03:00.0: Enabling feature bit: 6
    ath10k_pci 0000:03:00.0: Enabling feature bit: 7
    ath10k_pci 0000:03:00.0: features
    ath10k_pci 0000:03:00.0: 00000000: c0 00 00 00 00 00 00 00
    
    - the wrong IE with padding is read (0x77)
    
    ath10k_pci 0000:03:00.0: found firmware features ie (4 B)
    ath10k_pci 0000:03:00.0: Enabling feature bit: 6
    ath10k_pci 0000:03:00.0: Enabling feature bit: 7
    ath10k_pci 0000:03:00.0: Enabling feature bit: 8
    ath10k_pci 0000:03:00.0: Enabling feature bit: 9
    ath10k_pci 0000:03:00.0: Enabling feature bit: 10
    ath10k_pci 0000:03:00.0: Enabling feature bit: 12
    ath10k_pci 0000:03:00.0: Enabling feature bit: 13
    ath10k_pci 0000:03:00.0: Enabling feature bit: 14
    ath10k_pci 0000:03:00.0: Enabling feature bit: 16
    ath10k_pci 0000:03:00.0: Enabling feature bit: 17
    ath10k_pci 0000:03:00.0: Enabling feature bit: 18
    ath10k_pci 0000:03:00.0: features
    ath10k_pci 0000:03:00.0: 00000000: c0 77 07 00 00 00 00 00
    
    Tested-by: Mike Lothian <mike@fireburn.co.uk>
    Signed-off-by: Ryan Hsu <ryanhsu@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -1306,10 +1306,7 @@ static int ath10k_core_fetch_board_data_api_n(struct ath10k *ar,
 		len -= sizeof(*hdr);
 		data = hdr->data;
 
-		/* jump over the padding */
-		ie_len = ALIGN(ie_len, 4);
-
-		if (len < ie_len) {
+		if (len < ALIGN(ie_len, 4)) {
 			ath10k_err(ar, "invalid length for board ie_id %d ie_len %zu len %zu\n",
 				   ie_id, ie_len, len);
 			ret = -EINVAL;
@@ -1347,6 +1344,9 @@ static int ath10k_core_fetch_board_data_api_n(struct ath10k *ar,
 			/* board data found */
 			goto out;
 		}
+
+		/* jump over the padding */
+		ie_len = ALIGN(ie_len, 4);
 
 		len -= ie_len;
 		data += ie_len;
@@ -1478,9 +1478,6 @@ int ath10k_core_fetch_firmware_api_n(struct ath10k *ar, const char *name,
 		len -= sizeof(*hdr);
 		data += sizeof(*hdr);
 
-		/* jump over the padding */
-		ie_len = ALIGN(ie_len, 4);
-
 		if (len < ie_len) {
 			ath10k_err(ar, "invalid length for FW IE %d (%zu < %zu)\n",
 				   ie_id, len, ie_len);
@@ -1585,6 +1582,9 @@ int ath10k_core_fetch_firmware_api_n(struct ath10k *ar, const char *name,
 				    le32_to_cpu(hdr->id));
 			break;
 		}
+
+		/* jump over the padding */
+		ie_len = ALIGN(ie_len, 4);
 
 		len -= ie_len;
 		data += ie_len;
