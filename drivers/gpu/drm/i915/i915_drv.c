7726ff7a7fc84552971fa5e8762f9f1313d85e01
Chris Wilson <chris@chris-wilson.co.uk>
2018-02-07 11:15:45 +0000

    drm/i915: Tidy up some error messages around reset failure
    On blb and pnv, we are seeing sporadic
    
      i915 0000:00:02.0: Resetting chip after gpu hang
      [drm:intel_gpu_reset [i915]] rcs0: timed out on STOP_RING
      [drm:i915_reset [i915]] *ERROR* Failed hw init on reset -5
    
    which notably lack the actual root cause of the error. Ostensibly it
    should be the init_ring_common() that failed, but it's error paths are
    covered by DRM_ERROR.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180207111545.17078-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1958,7 +1958,8 @@ void i915_reset(struct drm_i915_private *i915, unsigned int flags)
 	 */
 	ret = i915_ggtt_enable_hw(i915);
 	if (ret) {
-		DRM_ERROR("Failed to re-enable GGTT following reset %d\n", ret);
+		DRM_ERROR("Failed to re-enable GGTT following reset (%d)\n",
+			  ret);
 		goto error;
 	}
 
@@ -1975,7 +1976,8 @@ void i915_reset(struct drm_i915_private *i915, unsigned int flags)
 	 */
 	ret = i915_gem_init_hw(i915);
 	if (ret) {
-		DRM_ERROR("Failed hw init on reset %d\n", ret);
+		DRM_ERROR("Failed to initialise HW following reset (%d)\n",
+			  ret);
 		goto error;
 	}
 
