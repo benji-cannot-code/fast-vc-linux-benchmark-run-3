9517fb1e02861e6dc1372375ac1c0d104961f676
Suganath Prabu Subramani <suganath-prabu.subramani@broadcom.com>
2018-02-07 02:51:47 -0800

    scsi: mpt3sas: Introduce API to get BAR0 mapped buffer address
    For MPI Endpoint/Mcpu, driver should double buffer data buffer/SGLs.
    This is normally copied from host to internal memory of IOC by DMA
    engine of PCI device. Since the interface to DMA from host to mCPU is
    not present for Mcpu/MPI Endpoint device, driver does double copy of
    those buffers directly to the mCPU memory region via BAR0 region.
    
    Introduced API to calculate and return BAR0 mapped host buffer's
    physical and virtual address for the provided smid.
    
    Signed-off-by: Suganath Prabu S <suganath-prabu.subramani@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/mpt3sas/mpt3sas_base.h
+++ drivers/scsi/mpt3sas/mpt3sas_base.h
@@ -121,6 +121,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MPT3SAS_NVME_QUEUE_DEPTH	128
 #define MPT_NAME_LENGTH			32	/* generic length of strings */
 #define MPT_STRING_LENGTH		64
+#define MPI_FRAME_START_OFFSET		256
+#define REPLY_FREE_POOL_SIZE		512 /*(32 maxcredix *4)*(4 times)*/
 
 #define MPT_MAX_CALLBACKS		32
 
