5b7aa1a9c14e5d56dc03a2b0127b38d018c4edba
Jason Gunthorpe <jgg@mellanox.com>
2018-02-07 16:45:51 -0700

    RDMA: Do not used __packed in uapi headers
    __packed is not available in linux/types.h, so we cannot use it in
    the uapi headers.
    
    The construction struct ABC {} __packed; may still compile even if
    __packed is not defined, however it simply creates a variable called
    __packed, and doesn't set the alignment.
    
    All these uses of packed are on structs that already have aligned
    members.
    
    While use in hfi may indicate the struct itself is unaligned,
    the use in ocrdma is on a UHW struct which should never be unaligned,
    so just delete it there.
    
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- include/uapi/rdma/hfi/hfi1_user.h
+++ include/uapi/rdma/hfi/hfi1_user.h
@@ -220,7 +220,7 @@ struct sdma_req_info {
 	 * in charge of managing its own ring.
 	 */
 	__u16 comp_idx;
-} __packed;
+} __attribute__((__packed__));
 
 /*
  * SW KDETH header.
@@ -231,7 +231,7 @@ struct hfi1_kdeth_header {
 	__le16 jkey;
 	__le16 hcrc;
 	__le32 swdata[7];
-} __packed;
+}  __attribute__((__packed__));
 
 /*
  * Structure describing the headers that User space uses. The
@@ -242,7 +242,7 @@ struct hfi1_pkt_header {
 	__be16 lrh[4];
 	__be32 bth[3];
 	struct hfi1_kdeth_header kdeth;
-} __packed;
+}  __attribute__((__packed__));
 
 
 /*
