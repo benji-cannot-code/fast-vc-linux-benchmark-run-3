5f44bf5e30ee481e72f74b6977a485530a58e09d
Michał Kępień <kernel@kempniu.pl>
2018-02-11 22:07:23 +0100

    platform/x86: fujitsu-laptop: Simplify error paths
    Replace the last few lines of acpi_fujitsu_bl_add() with a simple return
    in order to improve code readability without changing the logic.
    
    As acpi_fujitsu_laptop_add() uses a managed memory allocation for
    device-specific data, it is fine to just return immediately upon kfifo
    allocation failure.  Do that instead of jumping to the end of the
    function to improve code readability.  Running out of memory while
    allocating the kfifo does not seem probable enough to warrant logging an
    error message, so do not do it.
    
    Signed-off-by: Michał Kępień <kernel@kempniu.pl>
    Reviewed-by: Jonathan Woithe <jwoithe@just42.net>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- drivers/platform/x86/fujitsu-laptop.c
+++ drivers/platform/x86/fujitsu-laptop.c
@@ -411,11 +411,7 @@ static int acpi_fujitsu_bl_add(struct acpi_device *device)
 	if (ret)
 		return ret;
 
-	ret = fujitsu_backlight_register(device);
-	if (ret)
-		return ret;
-
-	return 0;
+	return fujitsu_backlight_register(device);
 }
 
 /* Brightness notify */
@@ -791,10 +787,8 @@ static int acpi_fujitsu_laptop_add(struct acpi_device *device)
 	spin_lock_init(&priv->fifo_lock);
 	ret = kfifo_alloc(&priv->fifo, RINGBUFFERSIZE * sizeof(int),
 			  GFP_KERNEL);
-	if (ret) {
-		pr_err("kfifo_alloc failed\n");
-		goto err_stop;
-	}
+	if (ret)
+		return ret;
 
 	pr_info("ACPI: %s [%s]\n",
 		acpi_device_name(device), acpi_device_bid(device));
@@ -846,7 +840,7 @@ static int acpi_fujitsu_laptop_add(struct acpi_device *device)
 
 err_free_fifo:
 	kfifo_free(&priv->fifo);
-err_stop:
+
 	return ret;
 }
 
