ddd8eaba0df0d1b46b76e053cf5a250cf4b9b447
Marcus Folkesson <marcus.folkesson@gmail.com>
2018-02-11 21:08:45 +0100

    watchdog: mtk: allow setting timeout in devicetree
    watchdog_init_timeout() will allways pick timeout_param since it
    defaults to a valid timeout.
    
    By following best practice described in
    Documentation/watchdog/watchdog-kernel-api.txt, it also
    let us to set timout-sec property in devicetree.
    
    Signed-off-by: Marcus Folkesson <marcus.folkesson@gmail.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Matthias Brugger <matthias.bgg@gmail.com>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/watchdog/mtk_wdt.c
+++ drivers/watchdog/mtk_wdt.c
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRV_VERSION		"1.0"
 
 static bool nowayout = WATCHDOG_NOWAYOUT;
-static unsigned int timeout = WDT_MAX_TIMEOUT;
+static unsigned int timeout;
 
 struct mtk_wdt_dev {
 	struct watchdog_device wdt_dev;
