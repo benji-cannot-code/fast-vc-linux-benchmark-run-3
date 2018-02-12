b7379f0656c3e6b24becc4287955d014e50fdf64
Alexandre Belloni <alexandre.belloni@bootlin.com>
2018-02-12 23:47:34 +0100

    rtc: remove nvmem_config
    Because nvmem_config is only used and copied at nvmem registration, remove
    it from struct rtc_device.
    
    All the rtc drivers using nvmem are now calling rtc_nvmem_register
    directly.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/class.c
+++ drivers/rtc/class.c
@@ -455,8 +455,6 @@ int __rtc_register_device(struct module *owner, struct rtc_device *rtc)
 
 	rtc_proc_add_device(rtc);
 
-	rtc_nvmem_register(rtc, rtc->nvmem_config);
-
 	rtc->registered = true;
 	dev_info(rtc->dev.parent, "registered as %s\n",
 		 dev_name(&rtc->dev));
