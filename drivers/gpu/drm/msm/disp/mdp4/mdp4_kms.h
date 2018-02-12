e62556ba0fed401597a0133e5dc6df5975d466c0
Rob Clark <robdclark@gmail.com>
2018-02-12 08:18:27 -0500

    drm/msm: rename mdp->disp
    Since new display controller is called "dpu" instead of "mdp".  Lets
    make the name of the toplevel directory for the display controllers a
    bit more generic.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/msm/disp/mdp4/mdp4_kms.h
+++ drivers/gpu/drm/msm/disp/mdp4/mdp4_kms.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "msm_drv.h"
 #include "msm_kms.h"
-#include "mdp/mdp_kms.h"
+#include "disp/mdp_kms.h"
 #include "mdp4.xml.h"
 
 struct device_node;
