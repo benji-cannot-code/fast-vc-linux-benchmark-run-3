98eb6010a97ad9cbe6b9775ab2a6aaa2a5061ba0
NeilBrown <neilb@suse.com>
2018-02-13 08:22:36 +1100

    staging: lustre: introduce and use l_wait_event_abortable()
    lustre sometimes wants to wait for an event, but abort if
    one of a specific list of signals arrives.  This is a little
    bit like wait_event_killable(), except that the signals are
    identified a different way.
    
    So introduce l_wait_event_abortable() which provides this
    functionality.
    Having separate functions for separate needs is more in line
    with the pattern set by include/linux/wait.h, than having a
    single function which tries to include all possible needs.
    
    Also introduce l_wait_event_abortable_exclusive().
    
    Note that l_wait_event() return -EINTR on a signal, while
    Linux wait_event functions return -ERESTARTSYS.
    l_wait_event_{abortable_,}exclusive follow the Linux pattern.
    
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: NeilBrown <neilb@suse.com>
    Reviewed-by: Patrick Farrell <paf@cray.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdclass/genops.c
+++ drivers/staging/lustre/lustre/obdclass/genops.c
@@ -1333,7 +1333,6 @@ static bool obd_request_slot_avail(struct client_obd *cli,
 int obd_get_request_slot(struct client_obd *cli)
 {
 	struct obd_request_slot_waiter orsw;
-	struct l_wait_info lwi;
 	int rc;
 
 	spin_lock(&cli->cl_loi_list_lock);
@@ -1348,11 +1347,9 @@ int obd_get_request_slot(struct client_obd *cli)
 	orsw.orsw_signaled = false;
 	spin_unlock(&cli->cl_loi_list_lock);
 
-	lwi = LWI_INTR(LWI_ON_SIGNAL_NOOP, NULL);
-	rc = l_wait_event(orsw.orsw_waitq,
-			  obd_request_slot_avail(cli, &orsw) ||
-			  orsw.orsw_signaled,
-			  &lwi);
+	rc = l_wait_event_abortable(orsw.orsw_waitq,
+				    obd_request_slot_avail(cli, &orsw) ||
+				    orsw.orsw_signaled);
 
 	/*
 	 * Here, we must take the lock to avoid the on-stack 'orsw' to be
