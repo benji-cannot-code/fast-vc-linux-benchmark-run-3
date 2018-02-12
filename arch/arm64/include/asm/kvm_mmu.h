c40f691216b1d8a777c2f82dd050a0113a520ef1
Marc Zyngier <marc.zyngier@arm.com>
2018-02-12 16:50:19 +0000

    arm64: KVM: Move vector offsetting from hyp-init.S to kvm_get_hyp_vector
    We currently provide the hyp-init code with a kernel VA, and expect
    it to turn it into a HYP va by itself. As we're about to provide
    the hypervisor with mappings that are not necessarily in the memory
    range, let's move the kern_hyp_va macro to kvm_get_hyp_vector.
    
    No functionnal change.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/include/asm/kvm_mmu.h
+++ arch/arm64/include/asm/kvm_mmu.h
@@ -375,6 +375,7 @@ static inline void *kvm_get_hyp_vector(void)
 			vect = lm_alias(vect);
 	}
 
+	vect = kern_hyp_va(vect);
 	return vect;
 }
 
@@ -388,7 +389,7 @@ static inline int kvm_map_vectors(void)
 #else
 static inline void *kvm_get_hyp_vector(void)
 {
-	return kvm_ksym_ref(__kvm_hyp_vector);
+	return kern_hyp_va(kvm_ksym_ref(__kvm_hyp_vector));
 }
 
 static inline int kvm_map_vectors(void)
