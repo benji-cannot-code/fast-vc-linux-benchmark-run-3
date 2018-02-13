50780decd8af52273b3898f81d3d36c9a6b1ad91
Fabio Estevam <fabio.estevam@nxp.com>
2018-02-13 17:11:35 -0200

    drm/imx: ipuv3-plane: Include "imx-drm.h" header file
    ipu_planes_assign_pre() prototype is in "imx-drm.h" header file, so
    include it to fix the following sparse warning:
    
    drivers/gpu/drm/imx/ipuv3-plane.c:729:5: warning: symbol 'ipu_planes_assign_pre' was not declared. Should it be static?
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/drm/imx/ipuv3-plane.c
+++ drivers/gpu/drm/imx/ipuv3-plane.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_plane_helper.h>
 
 #include "video/imx-ipu-v3.h"
+#include "imx-drm.h"
 #include "ipuv3-plane.h"
 
 struct ipu_plane_state {
