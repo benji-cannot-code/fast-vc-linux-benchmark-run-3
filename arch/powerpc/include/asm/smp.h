c85a02125eae51d6ddaa1c39867946fecbcc0aff
Nicholas Piggin <npiggin@gmail.com>
2018-02-14 01:08:18 +1000

    powerpc/setup: Add cpu_to_phys_id array
    Build an array that finds hardware CPU number from logical CPU
    number in firmware CPU discovery. Use that rather than setting
    paca of other CPUs directly, to begin with. Subsequent patch will
    not have pacas allocated at this point.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    [mpe: Fix SMP=n build by adding #ifdef in arch_match_cpu_phys_id()]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/smp.h
+++ arch/powerpc/include/asm/smp.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int boot_cpuid;
 extern int spinning_secondaries;
+extern u32 *cpu_to_phys_id;
 
 extern void cpu_die(void);
 extern int cpu_to_chip_id(int cpu);
