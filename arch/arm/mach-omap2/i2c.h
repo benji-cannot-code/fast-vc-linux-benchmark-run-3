710681f11263109f2a0f9928aaf799909425abf6
Suman Anna <s-anna@ti.com>
2018-02-12 19:32:42 -0600

    ARM: OMAP: Move plat/i2c.h into mach-omap1 folder
    The current contents of plat/i2c.h are only relevant for OMAP1
    platforms, as all the equivalent functions for OMAP2 has been
    dropped in commit 65fa3e719f36 ("ARM: OMAP2+: Remove legacy i2c.c
    platform init code"), and which has also moved the left-over code
    from plat-omap into mach-omap1. The hwmod is a concept only
    applicable for OMAP2 platforms, and the omap_i2c_reset() is
    already declared in mach-omap2/i2c.h. So, move the current
    plat/i2c.h header file into the mach-omap1 folder, and adjust
    the header usage accordingly.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/i2c.h
+++ arch/arm/mach-omap2/i2c.h
@@ -20,10 +20,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#include <plat/i2c.h>
-
 #ifndef __MACH_OMAP2_I2C_H
 #define __MACH_OMAP2_I2C_H
+
+struct omap_hwmod;
 
 int omap_i2c_reset(struct omap_hwmod *oh);
 
