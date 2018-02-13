0cc270d96577f5a9f46b42bfb103320291f134a3
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-02-13 14:00:37 +0200

    drm: omapdrm: dsi: Don't pass channel to dispc init/uninit functions
    The dsi_display_init_dispc() and dsi_display_uninit_dispc() functions
    take a channel argument that is reduntant as it is always identical to
    the dsi->output.dispc_channel. Remove the argument and use the field
    directly in the functions to avoid misuse.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>

--- drivers/gpu/drm/omapdrm/dss/dsi.c
+++ drivers/gpu/drm/omapdrm/dss/dsi.c
@@ -216,10 +216,8 @@ struct dsi_reg { u16 module; u16 idx; };
 typedef void (*omap_dsi_isr_t) (void *arg, u32 mask);
 struct dsi_data;
 
-static int dsi_display_init_dispc(struct dsi_data *dsi,
-	enum omap_channel channel);
-static void dsi_display_uninit_dispc(struct dsi_data *dsi,
-	enum omap_channel channel);
+static int dsi_display_init_dispc(struct dsi_data *dsi);
+static void dsi_display_uninit_dispc(struct dsi_data *dsi);
 
 static int dsi_vc_send_null(struct dsi_data *dsi, int channel);
 
@@ -3846,7 +3844,7 @@ static int dsi_enable_video_output(struct omap_dss_device *dssdev, int channel)
 		return -ENODEV;
 	}
 
-	r = dsi_display_init_dispc(dsi, dispc_channel);
+	r = dsi_display_init_dispc(dsi);
 	if (r)
 		goto err_init_dispc;
 
@@ -3896,7 +3894,7 @@ err_mgr_enable:
 		dsi_vc_enable(dsi, channel, false);
 	}
 err_pix_fmt:
-	dsi_display_uninit_dispc(dsi, dispc_channel);
+	dsi_display_uninit_dispc(dsi);
 err_init_dispc:
 	return r;
 }
@@ -3919,7 +3917,7 @@ static void dsi_disable_video_output(struct omap_dss_device *dssdev, int channel
 
 	dss_mgr_disable(dispc_channel);
 
-	dsi_display_uninit_dispc(dsi, dispc_channel);
+	dsi_display_uninit_dispc(dsi);
 }
 
 static void dsi_update_screen_dispc(struct dsi_data *dsi)
@@ -4105,9 +4103,9 @@ static int dsi_configure_dispc_clocks(struct dsi_data *dsi)
 	return 0;
 }
 
-static int dsi_display_init_dispc(struct dsi_data *dsi,
-				  enum omap_channel channel)
+static int dsi_display_init_dispc(struct dsi_data *dsi)
 {
+	enum omap_channel channel = dsi->output.dispc_channel;
 	int r;
 
 	dss_select_lcd_clk_source(dsi->dss, channel, dsi->module_id == 0 ?
@@ -4168,9 +4166,10 @@ err:
 	return r;
 }
 
-static void dsi_display_uninit_dispc(struct dsi_data *dsi,
-				     enum omap_channel channel)
+static void dsi_display_uninit_dispc(struct dsi_data *dsi)
 {
+	enum omap_channel channel = dsi->output.dispc_channel;
+
 	if (dsi->mode == OMAP_DSS_DSI_CMD_MODE)
 		dss_mgr_unregister_framedone_handler(channel,
 				dsi_framedone_irq_callback, dsi);
