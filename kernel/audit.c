611f1428a4cc2185daed6f76726d706ea7f6d64b
Richard Guy Briggs <rgb@redhat.com>
2018-02-14 11:18:21 -0500

    audit: make ANOM_LINK obey audit_enabled and audit_dummy_context
    Audit link denied events emit disjointed records when audit is disabled.
    No records should be emitted when audit is disabled.
    
    See: https://github.com/linux-audit/audit-kernel/issues/21
    
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -2316,6 +2316,9 @@ void audit_log_link_denied(const char *operation, const struct path *link)
 	struct audit_buffer *ab;
 	struct audit_names *name;
 
+	if (!audit_enabled || audit_dummy_context())
+		return;
+
 	name = kzalloc(sizeof(*name), GFP_NOFS);
 	if (!name)
 		return;
