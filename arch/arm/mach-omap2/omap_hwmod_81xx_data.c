b325e989d61f5a6565d1bf77b278131a249178e6
Tony Lindgren <tony@atomide.com>
2018-02-14 09:35:20 -0800

    ARM: OMAP2+: Include types.h directly for hwmod data
    This is needed in preparation for dropping some unused
    headers that indirectly include types.h.
    
    Tested-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap_hwmod_81xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_81xx_data.c
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/types.h>
+
 #include <linux/platform_data/gpio-omap.h>
 #include <linux/platform_data/hsmmc-omap.h>
 #include <linux/platform_data/spi-omap2-mcspi.h>
