854321144107b235647914d7d4d167c05d5ef1ee
Xin Long <lucien.xin@gmail.com>
2018-02-16 18:04:56 +0800

    netfilter: unlock xt_table earlier in __do_replace
    Now it's doing cleanup_entry for oldinfo under the xt_table lock,
    but it's not really necessary. After the replacement job is done
    in xt_replace_table, oldinfo is not used elsewhere any more, and
    it can be freed without xt_table lock safely.
    
    The important thing is that rtnl_lock is called in some xt_target
    destroy, which means rtnl_lock, a big lock is used in xt_table
    lock, a smaller one. It usually could be the reason why a dead
    lock may happen.
    
    Besides, all xt_target/match checkentry is called out of xt_table
    lock. It's better also to move all cleanup_entry calling out of
    xt_table lock, just as do_replace_finish does for ebtables.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv4/netfilter/arp_tables.c
+++ net/ipv4/netfilter/arp_tables.c
@@ -926,6 +926,8 @@ static int __do_replace(struct net *net, const char *name,
 	    (newinfo->number <= oldinfo->initial_entries))
 		module_put(t->me);
 
+	xt_table_unlock(t);
+
 	get_old_counters(oldinfo, counters);
 
 	/* Decrease module usage counts and free resource */
@@ -940,7 +942,6 @@ static int __do_replace(struct net *net, const char *name,
 		net_warn_ratelimited("arptables: counters copy to user failed while replacing table\n");
 	}
 	vfree(counters);
-	xt_table_unlock(t);
 	return ret;
 
  put_module:
--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -1088,6 +1088,8 @@ __do_replace(struct net *net, const char *name, unsigned int valid_hooks,
 	    (newinfo->number <= oldinfo->initial_entries))
 		module_put(t->me);
 
+	xt_table_unlock(t);
+
 	get_old_counters(oldinfo, counters);
 
 	/* Decrease module usage counts and free resource */
@@ -1101,7 +1103,6 @@ __do_replace(struct net *net, const char *name, unsigned int valid_hooks,
 		net_warn_ratelimited("iptables: counters copy to user failed while replacing table\n");
 	}
 	vfree(counters);
-	xt_table_unlock(t);
 	return ret;
 
  put_module:
