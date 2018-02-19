fc58f689d0a2369af929b0637a24cd68edeb24ee
Ilan Peer <ilan.peer@intel.com>
2018-02-19 14:48:41 +0200

    mac80211: Call mgd_prep_tx before transmitting deauthentication
    In multi channel scenarios, when disassociating from the AP before a
    beacon was heard from the AP, it is not guaranteed that the virtual
    interface is granted air time for the transmission of the
    deauthentication frame. This in turn can lead to various issues as
    the AP might never get the deauthentication frame.
    
    To mitigate such possible issues, add a HW flag indicating that the
    driver requires mac80211 to call the mgd_prep_tx() driver callback
    to make sure that the virtual interface is granted immediate airtime
    to be able to transmit the frame, in case that no beacon was heard
    from the AP.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright 2007, Michael Wu <flamingice@sourmilk.net>
  * Copyright 2013-2014  Intel Mobile Communications GmbH
  * Copyright (C) 2015 - 2017 Intel Deutschland GmbH
+ * Copyright (C) 2018        Intel Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -2009,9 +2010,22 @@ static void ieee80211_set_disassoc(struct ieee80211_sub_if_data *sdata,
 		ieee80211_flush_queues(local, sdata, true);
 
 	/* deauthenticate/disassociate now */
-	if (tx || frame_buf)
+	if (tx || frame_buf) {
+		struct ieee80211_if_managed *ifmgd = &sdata->u.mgd;
+
+		/*
+		 * In multi channel scenarios guarantee that the virtual
+		 * interface is granted immediate airtime to transmit the
+		 * deauthentication frame by calling mgd_prepare_tx, if the
+		 * driver requested so.
+		 */
+		if (ieee80211_hw_check(&local->hw, DEAUTH_NEED_MGD_TX_PREP) &&
+		    !ifmgd->have_beacon)
+			drv_mgd_prepare_tx(sdata->local, sdata);
+
 		ieee80211_send_deauth_disassoc(sdata, ifmgd->bssid, stype,
 					       reason, tx, frame_buf);
+	}
 
 	/* flush out frame - make sure the deauth was actually sent */
 	if (tx)
