7798dccebd1e7669db486ddc54aba9ff4cebee64
Eric Biggers <ebiggers@google.com>
2018-02-19 23:48:21 -0800

    crypto: x86/camellia - remove XTS algorithm
    The XTS template now wraps an ECB mode algorithm rather than the block
    cipher directly.  Therefore it is now redundant for crypto modules to
    wrap their ECB code with generic XTS code themselves via xts_crypt().
    
    Remove the xts-camellia-asm algorithm which did this.  Users who request
    xts(camellia) and previously would have gotten xts-camellia-asm will now
    get xts(ecb-camellia-asm) instead, which is just as fast.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/x86/include/asm/crypto/camellia.h
+++ arch/x86/include/asm/crypto/camellia.h
@@ -3,8 +3,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ASM_X86_CAMELLIA_H
 #define ASM_X86_CAMELLIA_H
 
-#include <linux/kernel.h>
+#include <crypto/b128ops.h>
 #include <linux/crypto.h>
+#include <linux/kernel.h>
 
 #define CAMELLIA_MIN_KEY_SIZE	16
 #define CAMELLIA_MAX_KEY_SIZE	32
