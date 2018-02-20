62da4bf266b468f65b852949faf544b39e6f3ca1
NeilBrown <neilb@suse.com>
2018-02-20 13:23:37 +1100

    staging: lustre: replace all CFS_CAP_* macros with CAP_*
    Lustre defines a few CFS_CAP_* macros which are exactly the
    same as the corresponding CAP_* macro, with one exception.
    
    CFS_CAP_SYS_BOOT is 23
    CAP_SYS_BOOT is 22.
    
    CFS_CAP_SYS_BOOT is only used through CFS_CAP_FS_MASK and
    causes capability 23 (CAP_SYS_NICE) to be dropped in certain
    circumstances.
    It is probable that the intention was to drop CAP_SYS_BOOT,
    and this is what is now done.
    
    CFS_CAP_CHOWN_MASK and CFS_CAP_SYS_RESOURCE_MASK are never
    used, so they have been removed.
    
    Signed-off-by: NeilBrown <neilb@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/obdecho/echo_client.c
+++ drivers/staging/lustre/lustre/obdecho/echo_client.c
@@ -1503,7 +1503,7 @@ echo_client_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
 
 	switch (cmd) {
 	case OBD_IOC_CREATE:		    /* may create echo object */
-		if (!capable(CFS_CAP_SYS_ADMIN)) {
+		if (!capable(CAP_SYS_ADMIN)) {
 			rc = -EPERM;
 			goto out;
 		}
@@ -1512,7 +1512,7 @@ echo_client_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
 		goto out;
 
 	case OBD_IOC_DESTROY:
-		if (!capable(CFS_CAP_SYS_ADMIN)) {
+		if (!capable(CAP_SYS_ADMIN)) {
 			rc = -EPERM;
 			goto out;
 		}
@@ -1535,7 +1535,7 @@ echo_client_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
 		goto out;
 
 	case OBD_IOC_SETATTR:
-		if (!capable(CFS_CAP_SYS_ADMIN)) {
+		if (!capable(CAP_SYS_ADMIN)) {
 			rc = -EPERM;
 			goto out;
 		}
@@ -1548,7 +1548,7 @@ echo_client_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
 		goto out;
 
 	case OBD_IOC_BRW_WRITE:
-		if (!capable(CFS_CAP_SYS_ADMIN)) {
+		if (!capable(CAP_SYS_ADMIN)) {
 			rc = -EPERM;
 			goto out;
 		}
