e1f238d43f3823748fc1715f2095d518fc458ee9
Will Deacon <will.deacon@arm.com>
2018-02-20 15:18:13 +0000

    arm64: Use arm64_force_sig_info instead of force_sig_info
    Using arm64_force_sig_info means that printing messages about unhandled
    signals is dealt with for us, so use that in preference to force_sig_info
    and remove any homebrew printing code.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/debug-monitors.c
+++ arch/arm64/kernel/debug-monitors.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/daifflags.h>
 #include <asm/debug-monitors.h>
 #include <asm/system_misc.h>
+#include <asm/traps.h>
 
 /* Determine debug architecture. */
 u8 debug_monitors_arch(void)
@@ -224,7 +225,7 @@ static void send_user_sigtrap(int si_code)
 	if (interrupts_enabled(regs))
 		local_irq_enable();
 
-	force_sig_info(SIGTRAP, &info, current);
+	arm64_force_sig_info(&info, "User debug trap", current);
 }
 
 static int single_step_handler(unsigned long addr, unsigned int esr,
