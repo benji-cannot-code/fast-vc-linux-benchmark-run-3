32e7b4c4748b748104cdbf62ffaa4e9da18e98b3
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-02-20 20:05:05 +0200

    ACPI, x86/boot: Introduce the ->reduced_hw_early_init() ACPI callback
    Some ACPI hardware reduced platforms need to initialize certain devices
    defined by the ACPI hardware specification even though in principle
    those devices should not be present in an ACPI hardware reduced platform.
    
    To allow that to happen, make it possible to override the generic
    x86_init callbacks and provide a custom legacy_pic value, add a new
    ->reduced_hw_early_init() callback to struct x86_init_acpi and make
    acpi_reduced_hw_init() use it.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: Juergen Gross <jgross@suse.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J . Wysocki <rafael.j.wysocki@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-acpi@vger.kernel.org
    Link: http://lkml.kernel.org/r/20180220180506.65523-2-andriy.shevchenko@linux.intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/x86_init.c
+++ arch/x86/kernel/x86_init.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/export.h>
 #include <linux/pci.h>
 
+#include <asm/acpi.h>
 #include <asm/bios_ebda.h>
 #include <asm/paravirt.h>
 #include <asm/pci_x86.h>
@@ -96,6 +97,7 @@ struct x86_init_ops x86_init __initdata = {
 
 	.acpi = {
 		.get_root_pointer	= u64_x86_init_noop,
+		.reduced_hw_early_init	= acpi_generic_reduced_hw_init,
 	},
 };
 
