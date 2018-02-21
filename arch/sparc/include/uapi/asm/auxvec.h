244aae3662746f42646cf91c2911e3d5dcf5d63a
Khalid Aziz <khalid.aziz@oracle.com>
2018-02-21 10:15:48 -0700

    sparc64: Add auxiliary vectors to report platform ADI properties
    ADI feature on M7 and newer processors has three important properties
    relevant to userspace apps using ADI capabilities - (1) Size of block of
    memory an ADI version tag applies to, (2) Number of uppermost bits in
    virtual address used to encode ADI tag, and (3) The value M7 processor
    will force the ADI tags to if it detects uncorrectable error in an ADI
    tagged cacheline. Kernel can retrieve these properties for a platform
    through machine description provided by the firmware. This patch adds
    code to retrieve these properties and report them to userspace through
    auxiliary vectors.
    
    Signed-off-by: Khalid Aziz <khalid.aziz@oracle.com>
    Cc: Khalid Aziz <khalid@gonehiking.org>
    Reviewed-by: Anthony Yznaga <anthony.yznaga@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/include/uapi/asm/auxvec.h
+++ arch/sparc/include/uapi/asm/auxvec.h
@@ -4,6 +4,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define AT_SYSINFO_EHDR		33
 
+#ifdef CONFIG_SPARC64
+/* Avoid overlap with other AT_* values since they are consolidated in
+ * glibc and any overlaps can cause problems
+ */
+#define AT_ADI_BLKSZ	48
+#define AT_ADI_NBITS	49
+#define AT_ADI_UEONADI	50
+
+#define AT_VECTOR_SIZE_ARCH	4
+#else
 #define AT_VECTOR_SIZE_ARCH	1
+#endif
 
 #endif /* !(__ASMSPARC_AUXVEC_H) */
