d557965e3e4370ee0484c4554028231fff1b964f
Jacopo Mondi <jacopo+renesas@jmondi.org>
2018-02-21 12:48:03 -0500

    media: i2c: tw9910: Remove soc_camera dependencies
    Remove soc_camera framework dependencies from tw9910 sensor driver.
    - Handle clock and gpios
    - Register async subdevice
    - Remove soc_camera specific g/s_mbus_config operations
    - Add kernel doc to driver interface header file
    - Adjust build system
    
    This commit does not remove the original soc_camera based driver as long
    as other platforms depends on soc_camera-based CEU driver.
    
    Signed-off-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- include/media/i2c/tw9910.h
+++ include/media/i2c/tw9910.h
@@ -19,6 +19,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/soc_camera.h>
 
+/**
+ * tw9910_mpout_pin - MPOUT (multi-purpose output) pin functions
+ */
 enum tw9910_mpout_pin {
 	TW9910_MPO_VLOSS,
 	TW9910_MPO_HLOCK,
@@ -30,6 +33,12 @@ enum tw9910_mpout_pin {
 	TW9910_MPO_RTCO,
 };
 
+/**
+ * tw9910_video_info -	tw9910 driver interface structure
+ * @buswidth:		Parallel data bus width (8 or 16).
+ * @mpout:		Selected function of MPOUT (multi-purpose output) pin.
+ *			See &enum tw9910_mpout_pin
+ */
 struct tw9910_video_info {
 	unsigned long		buswidth;
 	enum tw9910_mpout_pin	mpout;
