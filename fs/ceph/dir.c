2f7c53962448a149b05099bc8a96348da652a67e
Al Viro <viro@zeniv.linux.org.uk>
2018-02-22 14:28:50 -0500

    get rid of pointless includes of fs_struct.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ceph/dir.c
+++ fs/ceph/dir.c
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ceph/ceph_debug.h>
 
 #include <linux/spinlock.h>
-#include <linux/fs_struct.h>
 #include <linux/namei.h>
 #include <linux/slab.h>
 #include <linux/sched.h>
--- kernel/exec_domain.c
+++ kernel/exec_domain.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/syscalls.h>
 #include <linux/sysctl.h>
 #include <linux/types.h>
-#include <linux/fs_struct.h>
 
 #ifdef CONFIG_PROC_FS
 static int execdomains_proc_show(struct seq_file *m, void *v)
