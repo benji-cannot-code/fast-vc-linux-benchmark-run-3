32e067abc0db90b64691e62a2153a14e39178cb5
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2018-02-23 15:21:40 -0500

    media: v4l: vsp1: Remove unused field from vsp1_drm_pipeline structure
    The vsp1_drm_pipeline enabled field is set but never used. Remove it.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/vsp1/vsp1_drm.h
+++ drivers/media/platform/vsp1/vsp1_drm.h
@@ -21,13 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**
  * vsp1_drm_pipeline - State for the API exposed to the DRM driver
  * @pipe: the VSP1 pipeline used for display
- * @enabled: pipeline state at the beginning of an update
  * @du_complete: frame completion callback for the DU driver (optional)
  * @du_private: data to be passed to the du_complete callback
  */
 struct vsp1_drm_pipeline {
 	struct vsp1_pipeline pipe;
-	bool enabled;
 
 	/* Frame synchronisation */
 	void (*du_complete)(void *, bool);
