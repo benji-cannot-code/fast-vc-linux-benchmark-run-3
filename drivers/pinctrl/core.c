a54286852e4931a83248671d9b8a57a4fe15c4be
Richard Fitzgerald <rf@opensource.cirrus.com>
2018-02-23 13:43:52 +0000

    pinctrl: core: Add missing EXPORT on pinctrl_register_mappings
    Systems that don't have devicetree need pinctrl_register_mappings.
    It should be EXPORT_SYMBOL_GPL so that it can be called from
    pinctrl drivers built as modules.
    
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/core.c
+++ drivers/pinctrl/core.c
@@ -1417,6 +1417,7 @@ int pinctrl_register_mappings(const struct pinctrl_map *maps,
 {
 	return pinctrl_register_map(maps, num_maps, true);
 }
+EXPORT_SYMBOL_GPL(pinctrl_register_mappings);
 
 void pinctrl_unregister_map(const struct pinctrl_map *map)
 {
