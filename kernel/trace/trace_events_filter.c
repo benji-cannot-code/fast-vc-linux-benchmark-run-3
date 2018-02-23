660a518e65641b56ea95fb84587491ac91fd8778
Steven Rostedt (VMware) <rostedt@goodmis.org>
2018-02-23 09:45:27 -0500

    tracing: Embed replace_filter_string() helper function
    The replace_filter_string() frees the current string and then copies a given
    string. But in the two locations that it was used, the allocation happened
    right after the filter was allocated (nothing to replace). There's no need
    for this to be a helper function. Embedding the allocation in the two places
    where it was called will make changing the code in the future easier.
    
    Also make the variable consistent (always use "filter_string" as the name,
    as it was used in one instance as "filter_str")
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_events_filter.c
+++ kernel/trace/trace_events_filter.c
@@ -665,17 +665,6 @@ static void remove_filter_string(struct event_filter *filter)
 	filter->filter_string = NULL;
 }
 
-static int replace_filter_string(struct event_filter *filter,
-				 char *filter_string)
-{
-	kfree(filter->filter_string);
-	filter->filter_string = kstrdup(filter_string, GFP_KERNEL);
-	if (!filter->filter_string)
-		return -ENOMEM;
-
-	return 0;
-}
-
 static void append_filter_err(struct filter_parse_state *ps,
 			      struct event_filter *filter)
 {
@@ -1803,9 +1792,8 @@ static int replace_system_preds(struct trace_subsystem_dir *dir,
 		if (!filter)
 			goto fail_mem;
 
-		/* Can only fail on no memory */
-		err = replace_filter_string(filter, filter_string);
-		if (err)
+		filter->filter_string = kstrdup(filter_string, GFP_KERNEL);
+		if (!filter->filter_string)
 			goto fail_mem;
 
 		err = replace_preds(file->event_call, filter, ps, false);
@@ -1869,7 +1857,7 @@ static int replace_system_preds(struct trace_subsystem_dir *dir,
 	return -ENOMEM;
 }
 
-static int create_filter_start(char *filter_str, bool set_str,
+static int create_filter_start(char *filter_string, bool set_str,
 			       struct filter_parse_state **psp,
 			       struct event_filter **filterp)
 {
@@ -1881,8 +1869,11 @@ static int create_filter_start(char *filter_str, bool set_str,
 
 	/* allocate everything, and if any fails, free all and fail */
 	filter = kzalloc(sizeof(*filter), GFP_KERNEL);
-	if (filter && set_str)
-		err = replace_filter_string(filter, filter_str);
+	if (filter && set_str) {
+		filter->filter_string = kstrdup(filter_string, GFP_KERNEL);
+		if (!filter->filter_string)
+			err = -ENOMEM;
+	}
 
 	ps = kzalloc(sizeof(*ps), GFP_KERNEL);
 
@@ -1896,7 +1887,7 @@ static int create_filter_start(char *filter_str, bool set_str,
 	*filterp = filter;
 	*psp = ps;
 
-	parse_init(ps, filter_ops, filter_str);
+	parse_init(ps, filter_ops, filter_string);
 	err = filter_parse(ps);
 	if (err && set_str)
 		append_filter_err(ps, filter);
