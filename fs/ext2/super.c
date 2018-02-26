eb9c80f605c5808a0e74e358a9bf62c135cc21db
Chengguang Xu <cgxu519@icloud.com>
2018-02-26 17:45:12 +0800

    ext2: change return code to -ENOMEM when failing memory allocation
    Change return code to -ENOMEM from -EINVAL when failing memory
    allocation in fill_super(), meanwhile delete redundant initial
    assignment of variable err.
    
    Signed-off-by: Chengguang Xu <cgxu519@icloud.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/ext2/super.c
+++ fs/ext2/super.c
@@ -828,7 +828,7 @@ static int ext2_fill_super(struct super_block *sb, void *data, int silent)
 	unsigned long logic_sb_block;
 	unsigned long offset = 0;
 	unsigned long def_mount_opts;
-	long ret = -EINVAL;
+	long ret = -ENOMEM;
 	int blocksize = BLOCK_SIZE;
 	int db_count;
 	int i, j;
@@ -836,7 +836,6 @@ static int ext2_fill_super(struct super_block *sb, void *data, int silent)
 	int err;
 	struct ext2_mount_options opts;
 
-	err = -ENOMEM;
 	sbi = kzalloc(sizeof(*sbi), GFP_KERNEL);
 	if (!sbi)
 		goto failed;
@@ -852,6 +851,7 @@ static int ext2_fill_super(struct super_block *sb, void *data, int silent)
 	sbi->s_daxdev = dax_dev;
 
 	spin_lock_init(&sbi->s_lock);
+	ret = -EINVAL;
 
 	/*
 	 * See what the current blocksize for the device is, and
