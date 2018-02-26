4138a92a4ccf49c508ebb7f0afc9d18d2a6d7f61
Eric Dumazet <edumazet@google.com>
2018-02-26 10:52:46 -0800

    test_bpf: add a schedule point
    test_bpf() is taking 1.6 seconds nowadays, it is time
    to add a schedule point in it.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- lib/test_bpf.c
+++ lib/test_bpf.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_vlan.h>
 #include <linux/random.h>
 #include <linux/highmem.h>
+#include <linux/sched.h>
 
 /* General test specific settings */
 #define MAX_SUBTESTS	3
@@ -6583,6 +6584,7 @@ static __init int test_bpf(void)
 		struct bpf_prog *fp;
 		int err;
 
+		cond_resched();
 		if (exclude_test(i))
 			continue;
 
