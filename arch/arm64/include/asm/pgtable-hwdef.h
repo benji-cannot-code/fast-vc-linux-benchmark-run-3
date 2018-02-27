b9fff3eb1396e9c0463f38a1f75f1e51cb509abf
Will Deacon <will.deacon@arm.com>
2018-02-27 14:15:49 +0000

    arm64: kaslr: Set TCR_EL1.NFD1 when CONFIG_RANDOMIZE_BASE=y
    TCR_EL1.NFD1 was allocated by SVE and ensures that fault-surpressing SVE
    memory accesses (e.g. speculative accesses from a first-fault gather load)
    which translate via TTBR1_EL1 result in a translation fault if they
    miss in the TLB when executed from EL0. This mitigates some timing attacks
    against KASLR, where the kernel address space could otherwise be probed
    efficiently using the FFR in conjunction with suppressed faults on SVE
    loads.
    
    Cc: Dave Martin <Dave.Martin@arm.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/pgtable-hwdef.h
+++ arch/arm64/include/asm/pgtable-hwdef.h
@@ -292,6 +292,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TCR_TBI0		(UL(1) << 37)
 #define TCR_HA			(UL(1) << 39)
 #define TCR_HD			(UL(1) << 40)
+#define TCR_NFD1		(UL(1) << 54)
 
 /*
  * TTBR.
