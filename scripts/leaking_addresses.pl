e519410988507f72e548a7608eb0d8c4d330964b
Tobin C. Harding <me@tobin.cc>
2018-02-27 15:15:34 +1100

    leaking_addresses: remove version number
    We have git now, we don't need a version number.  This was originally
    added because leaking_addresses.pl shamelessly (and mindlessly) copied
    checkpatch.pl
    
    Remove version number from script.
    
    Signed-off-by: Tobin C. Harding <me@tobin.cc>

--- scripts/leaking_addresses.pl
+++ scripts/leaking_addresses.pl
@@ -32,7 +32,6 @@ use bigint qw/hex/;
 use feature 'state';
 
 my $P = $0;
-my $V = '0.01';
 
 # Directories to scan.
 my @DIRS = ('/proc', '/sys');
@@ -86,7 +85,6 @@ sub help
 	print << "EOM";
 
 Usage: $P [OPTIONS]
-Version: $V
 
 Options:
 
