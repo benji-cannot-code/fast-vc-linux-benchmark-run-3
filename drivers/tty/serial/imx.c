fa8b4a8a798dc4c0623caf97b9483a1c6bc7eece
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2018-03-02 11:07:21 +0100

    serial: imx: simplify some conditions related to dma
    Neither .dma_is_txing nor .dma_is_rxing can evaluate to true if
    .dma_is_enabled evaluates to false:
    
    The only function that sets .dma_is_txing to a non-zero value is
    imx_dma_tx() which is only called if .dma_is_enabled is true. Same for
    .dma_is_rxing and start_rx_dma(). And before .dma_is_enabled is set to 0
    when imx_shutdown calls imx_disable_dma(), .dma_is_rxing and
    .dma_is_txing are reset to zero before, too.
    
    For this reason
    
    	sport->dma_is_enabled && sport->dma_is_rxing
    
    has the same value as
    
    	sport->dma_is_rxing
    
    which allows to simplify three if conditions.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -414,7 +414,7 @@ static void imx_stop_tx(struct uart_port *port)
 	 * We are maybe in the SMP context, so if the DMA TX thread is running
 	 * on other cpu, we have to wait for it to finish.
 	 */
-	if (sport->dma_is_enabled && sport->dma_is_txing)
+	if (sport->dma_is_txing)
 		return;
 
 	temp = imx_uart_readl(sport, UCR1);
@@ -443,7 +443,7 @@ static void imx_stop_rx(struct uart_port *port)
 	struct imx_port *sport = (struct imx_port *)port;
 	unsigned long temp;
 
-	if (sport->dma_is_enabled && sport->dma_is_rxing) {
+	if (sport->dma_is_rxing) {
 		if (sport->port.suspended) {
 			dmaengine_terminate_all(sport->dma_chan_rx);
 			sport->dma_is_rxing = 0;
@@ -901,7 +901,7 @@ static unsigned int imx_tx_empty(struct uart_port *port)
 	ret = (imx_uart_readl(sport, USR2) & USR2_TXDC) ?  TIOCSER_TEMT : 0;
 
 	/* If the TX DMA is working, return 0. */
-	if (sport->dma_is_enabled && sport->dma_is_txing)
+	if (sport->dma_is_txing)
 		ret = 0;
 
 	return ret;
