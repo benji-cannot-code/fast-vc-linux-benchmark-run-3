928474cb654bb0fad93622e9ad44ebc5b7828bb6
Xin Long <lucien.xin@gmail.com>
2018-03-05 20:44:19 +0800

    sctp: add support for SCTP_DSTADDRV4/6 Information for sendmsg
    This patch is to add support for Destination IPv4/6 Address options
    for sendmsg, as described in section 5.3.9/10 of RFC6458.
    
    With this option, you can provide more than one destination addrs
    to sendmsg when creating asoc, like sctp_connectx.
    
    It's also a necessary send info for sctp_sendv.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Acked-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sctp/structs.h
+++ include/net/sctp/structs.h
@@ -2114,6 +2114,7 @@ struct sctp_cmsgs {
 	struct sctp_sndrcvinfo *srinfo;
 	struct sctp_sndinfo *sinfo;
 	struct sctp_prinfo *prinfo;
+	struct msghdr *addrs_msg;
 };
 
 /* Structure for tracking memory objects */
