3fef14b7664b6a55067409a3255c67deb51db30d
Xiong Zhang <xiong.y.zhang@intel.com>
2018-03-06 06:07:27 +0800

    drm/i915/gvt: Release gvt->lock at the failure of finding page track
    page_track_handler take lock at the beginning, the lock should be released
    at the failure of finding page track. Otherwise deadlock will happen.
    
    Fixes: e502a2af4c35 ("drm/i915/gvt: Provide generic page_track infrastructure for write-protected page")
    Signed-off-by: Xiong Zhang <xiong.y.zhang@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/page_track.c
+++ drivers/gpu/drm/i915/gvt/page_track.c
@@ -166,7 +166,7 @@ int intel_vgpu_page_track_handler(struct intel_vgpu *vgpu, u64 gpa,
 
 	page_track = intel_vgpu_find_page_track(vgpu, gpa >> PAGE_SHIFT);
 	if (!page_track)
-		return 0;
+		goto out;
 
 	if (unlikely(vgpu->failsafe)) {
 		/* Remove write protection to prevent furture traps. */
@@ -177,6 +177,7 @@ int intel_vgpu_page_track_handler(struct intel_vgpu *vgpu, u64 gpa,
 			gvt_err("guest page write error, gpa %llx\n", gpa);
 	}
 
+out:
 	mutex_unlock(&gvt->lock);
 	return ret;
 }
