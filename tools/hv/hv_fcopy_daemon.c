69e01d2dc906d9c3061a54dda3a3bb3b8315e8db
Olaf Hering <olaf@aepfle.de>
2018-03-04 22:17:15 -0700

    tools: hv: include string.h in hv_fcopy_daemon
    The usage of strchr requires inclusion of string.h.
    
    Fixes: 0c38cda64aec ("tools: hv: remove unnecessary header files and netlink related code")
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/hv/hv_fcopy_daemon.c
+++ tools/hv/hv_fcopy_daemon.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <errno.h>
 #include <linux/hyperv.h>
 #include <linux/limits.h>
