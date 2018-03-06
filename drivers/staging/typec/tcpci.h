d1e63cb000ec7c06607ea16fb23ed7a08ab1482c
ShuFan Lee <shufan_lee@richtek.com>
2018-03-06 09:50:32 +0800

    staging: typec: handle vendor defined part and modify drp toggling flow
    Handle vendor defined behavior in tcpci_init, tcpci_set_vconn, tcpci_start_drp_toggling
    and export tcpci_irq. More operations can be extended in tcpci_data if needed.
    According to TCPCI specification, 4.4.5.2 ROLE_CONTROL,
    TCPC shall not start DRP toggling until subsequently the TCPM
    writes to the COMMAND register to start DRP toggling.
    DRP toggling flow is changed as following:
      - Write DRP = 1, Rp level and RC.CCx to Rd/Rd or Rp/Rp
      - Set LOOK4CONNECTION command
    
    Signed-off-by: ShuFan Lee <shufan_lee@richtek.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/typec/tcpci.h
+++ drivers/staging/typec/tcpci.h
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TCPC_POWER_CTRL_VCONN_ENABLE	BIT(0)
 
 #define TCPC_CC_STATUS			0x1d
+#define TCPC_CC_STATUS_DRPRST		BIT(5)
 #define TCPC_CC_STATUS_TERM		BIT(4)
 #define TCPC_CC_STATUS_CC2_SHIFT	2
 #define TCPC_CC_STATUS_CC2_MASK		0x3
@@ -121,5 +122,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TCPC_VBUS_STOP_DISCHARGE_THRESH		0x74
 #define TCPC_VBUS_VOLTAGE_ALARM_HI_CFG		0x76
 #define TCPC_VBUS_VOLTAGE_ALARM_LO_CFG		0x78
+
+struct tcpci;
+struct tcpci_data {
+	struct regmap *regmap;
+	int (*init)(struct tcpci *tcpci, struct tcpci_data *data);
+	int (*set_vconn)(struct tcpci *tcpci, struct tcpci_data *data,
+			 bool enable);
+	int (*start_drp_toggling)(struct tcpci *tcpci, struct tcpci_data *data,
+				  enum typec_cc_status cc);
+};
+
+struct tcpci *tcpci_register_port(struct device *dev, struct tcpci_data *data);
+void tcpci_unregister_port(struct tcpci *tcpci);
+irqreturn_t tcpci_irq(struct tcpci *tcpci);
 
 #endif /* __LINUX_USB_TCPCI_H */
