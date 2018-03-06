1ac0ea084fd9dad7ee60896d1a590fb4ce7ab6ce
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-03-06 23:34:53 +0200

    drm/omap: dss: Remove the dss_mgr_(dis)connect() operations
    The dss_mgr .connect() and .disconnect() are implemented as no-op in
    omapdrm. The operations are unneeded, remove them.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/dss/dsi.c
+++ drivers/gpu/drm/omapdrm/dss/dsi.c
@@ -4887,15 +4887,9 @@ static int dsi_connect(struct omap_dss_device *src,
 {
 	int r;
 
-	r = dss_mgr_connect(dst);
+	r = omapdss_device_connect(dst->dss, dst, dst->next);
 	if (r)
 		return r;
-
-	r = omapdss_device_connect(dst->dss, dst, dst->next);
-	if (r) {
-		dss_mgr_disconnect(dst);
-		return r;
-	}
 
 	dst->dispc_channel_connected = true;
 	return 0;
@@ -4907,8 +4901,6 @@ static void dsi_disconnect(struct omap_dss_device *src,
 	dst->dispc_channel_connected = false;
 
 	omapdss_device_disconnect(dst, dst->next);
-
-	dss_mgr_disconnect(dst);
 }
 
 static const struct omap_dss_device_ops dsi_ops = {
