54249366dae64fc545fd2dff03ddb8188acdec4f
Arnd Bergmann <arnd@arndb.de>
2018-03-07 21:46:52 +0100

    arch: remove score port
    The Sunplus S+core architecture was added in 2009 by Chen Liqin,
    who has been co-maintaining it with Lennox Wu <lennox.wu@gmail.com>
    since then, but after they both left the company, nobody else has shown
    any interest in the port and it has seen almost no activity other than
    tree-wide changes.
    
    The gcc port was removed a few years ago due to the inactivity.
    
    While the sunplus website still advertises products with unspecified
    RISC cores that might be S+core based, it's very clear that the Linux
    port is completely abandoned at this point.
    
    This removes all files related to the architecture.
    
    Acked-by: Lennox Wu <lennox.wu@gmail.com>
    Link: http://www.sunplus.com/
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/score/include/asm/topology.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _ASM_SCORE_TOPOLOGY_H
-#define _ASM_SCORE_TOPOLOGY_H
-
-#include <asm-generic/topology.h>
-
-#endif /* _ASM_SCORE_TOPOLOGY_H */
