fc54211c5b7a0505a99b6139515b243ab507f7cf
Arnd Bergmann <arnd@arndb.de>
2018-03-07 21:36:19 +0100

    arch: remove m32r port
    The Mitsubishi/Renesas m32r architecture has been around for many years,
    but the Linux port has been obsolete for a very long time as well, with
    the last significant updates done for linux-2.6.14.
    
    While some m32r microcontrollers are still being marketed by Renesas,
    those are apparently no longer possible to support, mainly due to the
    lack of an external memory interface.
    
    Hirokazu Takata was the maintainer until the architecture got marked
    Orphaned in 2014.
    
    Link: http://www.linux-m32r.org/
    Link: https://www.renesas.com/en-eu/products/microcontrollers-microprocessors/m32r.html
    Cc: Hirokazu Takata <takata@linux-m32r.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/m32r/include/uapi/asm/ioctls.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef __ARCH_M32R_IOCTLS_H__
-#define __ARCH_M32R_IOCTLS_H__
-
-#include <asm-generic/ioctls.h>
-
-#endif /* __ARCH_M32R_IOCTLS_H__ */
