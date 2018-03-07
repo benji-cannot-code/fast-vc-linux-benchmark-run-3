43d21c2951d6a871549eb6a1da7e0d4324e7689c
Quytelda Kahja <quytelda@tamalin.org>
2018-03-06 17:31:41 -0800

    staging: most: Remove unnecessary usage of BUG_ON().
    There is no need for the calls to BUG_ON() in this driver, which are
    used to check if mbo or mbo->context are NULL; mbo is never NULL, and
    if mbo->context is NULL it would have already been dereferenced and
    oopsed before reaching the BUG_ON().
    
    Signed-off-by: Quytelda Kahja <quytelda@tamalin.org>
    Acked-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/core.c
+++ drivers/staging/most/core.c
@@ -917,7 +917,6 @@ static void arm_mbo(struct mbo *mbo)
 	unsigned long flags;
 	struct most_channel *c;
 
-	BUG_ON((!mbo) || (!mbo->context));
 	c = mbo->context;
 
 	if (c->is_poisoned) {
@@ -1019,8 +1018,6 @@ EXPORT_SYMBOL_GPL(most_submit_mbo);
 static void most_write_completion(struct mbo *mbo)
 {
 	struct most_channel *c;
-
-	BUG_ON((!mbo) || (!mbo->context));
 
 	c = mbo->context;
 	if (mbo->status == MBO_E_INVAL)
