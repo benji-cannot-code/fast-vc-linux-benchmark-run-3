bb033c48796b4de92154e9852513a11839d8c3ce
Jeffy Chen <jeffy.chen@rock-chips.com>
2018-03-08 16:03:27 -0800

    Input: gpio-keys - add support for wakeup event action
    Add support for specifying event actions to trigger wakeup when using
    the gpio-keys input device as a wakeup source.
    
    This would allow the device to configure when to wakeup the system. For
    example a gpio-keys input device for pen insert, may only want to wakeup
    the system when ejecting the pen.
    
    Suggested-by: Brian Norris <briannorris@chromium.org>
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

---
+++ include/dt-bindings/input/gpio-keys.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * This header provides constants for gpio keys bindings.
+ */
+
+#ifndef _DT_BINDINGS_GPIO_KEYS_H
+#define _DT_BINDINGS_GPIO_KEYS_H
+
+#define EV_ACT_ANY		0x00	/* asserted or deasserted */
+#define EV_ACT_ASSERTED		0x01	/* asserted */
+#define EV_ACT_DEASSERTED	0x02	/* deasserted */
+
+#endif /* _DT_BINDINGS_GPIO_KEYS_H */
