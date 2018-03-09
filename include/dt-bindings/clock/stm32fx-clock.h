3f100d100eff314a3eabe2e133c2870d7d36164e
Gabriel Fernandez <gabriel.fernandez@st.com>
2018-03-09 07:57:31 +0100

    clk: stm32: Add DSI clock for STM32F469 Board
    This patch adds DSI clock for STM32F469 board
    
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@st.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/dt-bindings/clock/stm32fx-clock.h
+++ include/dt-bindings/clock/stm32fx-clock.h
@@ -36,8 +36,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_SAIQ_PDIV		13
 #define CLK_HSI			14
 #define CLK_SYSCLK		15
+#define CLK_F469_DSI		16
 
-#define END_PRIMARY_CLK		16
+#define END_PRIMARY_CLK		17
 
 #define CLK_HDMI_CEC		16
 #define CLK_SPDIF		17
