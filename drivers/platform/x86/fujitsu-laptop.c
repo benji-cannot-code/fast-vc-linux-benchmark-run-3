f8d23aade8c462999c686e39d2b3f9f13949d648
Michał Kępień <kernel@kempniu.pl>
2018-03-10 21:43:53 +0100

    platform/x86: fujitsu-laptop: Revert UNSUPPORTED_CMD back to an int
    UNSUPPORTED_CMD was previously 0x80000000 (int), but commit 819cddae7cfa
    ("platform/x86: fujitsu-laptop: Clean up constants") changed it into an
    unsigned long due to BIT() being used to define it.  As call_fext_func()
    returns an int, 0x80000000 would get type promoted when compared to an
    unsigned long, which on a 64-bit system would cause it to become
    0xffffffff80000000 due to sign extension.  This causes one logical
    condition in fujitsu-laptop to always be true and another one to always
    be false on 64-bit systems.  Fix this by reverting UNSUPPORTED_CMD back
    to an int.
    
    This patch fixes the following smatch warnings:
    
    drivers/platform/x86/fujitsu-laptop.c:763 acpi_fujitsu_laptop_leds_register() warn: always true condition '(call_fext_func(device, ((1 << (12)) | (1 << (0))), 2, (1 << (16)), 0) != (1 << (31))) => (s32min-s32max != 2147483648)'
    drivers/platform/x86/fujitsu-laptop.c:816 acpi_fujitsu_laptop_add() warn: impossible condition '(priv->flags_supported == (1 << (31))) => (0-2147483647,18446744071562067968-u64max == 2147483648)'
    
    Fixes: 819cddae7cfa ("platform/x86: fujitsu-laptop: Clean up constants")
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Michał Kępień <kernel@kempniu.pl>
    Reviewed-by: Jonathan Woithe <jwoithe@just42.net>
    Signed-off-by: Darren Hart (VMware) <dvhart@infradead.org>

--- drivers/platform/x86/fujitsu-laptop.c
+++ drivers/platform/x86/fujitsu-laptop.c
@@ -86,7 +86,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FUNC_BACKLIGHT			(BIT(12) | BIT(2))
 
 /* FUNC interface - responses */
-#define UNSUPPORTED_CMD			BIT(31)
+#define UNSUPPORTED_CMD			0x80000000
 
 /* FUNC interface - status flags */
 #define FLAG_RFKILL			BIT(5)
