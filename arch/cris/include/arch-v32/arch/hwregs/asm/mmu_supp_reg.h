d7b81c4f69199f04b9a7bc0ed760584c3c635bec
Jesper Nilsson <jesper@jni.nu>
2018-03-11 11:05:23 +0100

    CRIS: Drop support for the CRIS port
    The port was added back in 2000 so it's no longer even a good source
    of inspiration for newer ports (if it ever was)
    
    The last SoC (ARTPEC-3) with a CRIS main CPU was launched in 2008.
    
    Coupled with time and working developer board hardware being
    in low supply, it's time to drop the port from Linux.
    
    So long and thanks for all the fish!
    
    Signed-off-by: Jesper Nilsson <jesper.nilsson@axis.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/cris/include/arch-v32/arch/hwregs/asm/mmu_supp_reg.h
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 */
-#define RW_MM_CFG	0
-#define RW_MM_KBASE_LO	1
-#define RW_MM_KBASE_HI	2
-#define R_MM_CAUSE	3
-#define RW_MM_TLB_SEL	4
-#define RW_MM_TLB_LO	5
-#define RW_MM_TLB_HI	6
--- arch/cris/include/asm/swab.h
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _CRIS_SWAB_H
-#define _CRIS_SWAB_H
-
-#include <arch/swab.h>
-#include <uapi/asm/swab.h>
-
-#endif /* _CRIS_SWAB_H */
