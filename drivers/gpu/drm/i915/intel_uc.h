60d7453866a7ab6d6453f490ceb8e8efa1d34f43
Michal Wajdeczko <michal.wajdeczko@intel.com>
2018-03-12 13:03:06 +0000

    drm/i915/uc: Sanitize uC options early
    We are sanitizing uC related modparams together with other driver
    modparams in intel_sanitize_options called from i915_driver_init_hw,
    but this is too late for us as we will want to use USES_GUC/USES_HUC
    macros at earlier stage. Since our sanitizing does not require any
    MMIO access, we can do it in intel_uc_init_early right after we resolve
    firmware names.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Sagar Arun Kamble <sagar.a.kamble@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180312130308.22952-2-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/intel_uc.h
+++ drivers/gpu/drm/i915/intel_uc.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "intel_huc.h"
 #include "i915_params.h"
 
-void intel_uc_sanitize_options(struct drm_i915_private *dev_priv);
 void intel_uc_init_early(struct drm_i915_private *dev_priv);
 void intel_uc_init_mmio(struct drm_i915_private *dev_priv);
 int intel_uc_register(struct drm_i915_private *dev_priv);
