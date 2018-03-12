1ad5796af38066664c7810955abb734db8c84003
Linus Walleij <linus.walleij@linaro.org>
2018-03-12 12:16:34 +0100

    gpio: kempld: Include the right header
    This driver is a pure GPIO driver and should only include
    <linux/gpio/driver.h>.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-kempld.c
+++ drivers/gpio/gpio-kempld.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/errno.h>
 #include <linux/platform_device.h>
-#include <linux/gpio.h>
+#include <linux/gpio/driver.h>
 #include <linux/mfd/kempld.h>
 
 #define KEMPLD_GPIO_MAX_NUM		16
