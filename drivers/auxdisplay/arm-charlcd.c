577976d167440519ea4c62e2a9ed62982d39ea13
Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
2018-03-13 00:59:40 +0100

    auxdisplay: arm-charlcd: Fix struct charlcd doc line
    There was a missing first line and a missing member,
    which gave a warning under W=1:
    
        CC      drivers/auxdisplay/arm-charlcd.o
      drivers/auxdisplay/arm-charlcd.c:57: warning: Cannot understand
       * @dev: a pointer back to containing device on line 57 - I thought
      it was a doc line
    
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>

--- drivers/auxdisplay/arm-charlcd.c
+++ drivers/auxdisplay/arm-charlcd.c
@@ -55,12 +55,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HD_BUSY_FLAG			0x80U
 
 /**
+ * struct charlcd - Private data structure
  * @dev: a pointer back to containing device
  * @phybase: the offset to the controller in physical memory
  * @physize: the size of the physical page
  * @virtbase: the offset to the controller in virtual memory
  * @irq: reserved interrupt number
  * @complete: completion structure for the last LCD command
+ * @init_work: delayed work structure to initialize the display on boot
  */
 struct charlcd {
 	struct device *dev;
