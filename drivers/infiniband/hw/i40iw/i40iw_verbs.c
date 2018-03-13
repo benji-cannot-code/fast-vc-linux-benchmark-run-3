c3f408885b2fd2623159283cafb0522de2f90a1f
Arnd Bergmann <arnd@arndb.de>
2018-03-13 13:06:20 +0100

    RDMA/i40iw: include linux/irq.h
    We get a build failure on ARM unless the header is included explicitly:
    
    drivers/infiniband/hw/i40iw/i40iw_verbs.c: In function 'i40iw_get_vector_affinity':
    drivers/infiniband/hw/i40iw/i40iw_verbs.c:2747:9: error: implicit declaration of function 'irq_get_affinity_mask'; did you mean 'irq_create_affinity_masks'? [-Werror=implicit-function-declaration]
      return irq_get_affinity_mask(msix_vec->irq);
             ^~~~~~~~~~~~~~~~~~~~~
             irq_create_affinity_masks
    drivers/infiniband/hw/i40iw/i40iw_verbs.c:2747:9: error: returning 'int' from a function with return type 'const struct cpumask *' makes pointer from integer without a cast [-Werror=int-conversion]
      return irq_get_affinity_mask(msix_vec->irq);
    
    Fixes: 7e952b19eb63 ("i40iw: Implement get_vector_affinity API")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/i40iw/i40iw_verbs.c
+++ drivers/infiniband/hw/i40iw/i40iw_verbs.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/highmem.h>
 #include <linux/time.h>
 #include <linux/hugetlb.h>
+#include <linux/irq.h>
 #include <asm/byteorder.h>
 #include <net/ip.h>
 #include <rdma/ib_verbs.h>
