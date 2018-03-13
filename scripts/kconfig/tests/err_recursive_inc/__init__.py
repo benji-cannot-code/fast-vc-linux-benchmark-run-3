4d1e98e3b52732e7baac85d6b23a1943295d2362
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-03-13 18:12:12 +0900

    kconfig: tests: test if recursive inclusion is detected
    If recursive inclusion is detected, it should fail with error
    messages.  Test this.
    
    This also tests the line numbers in the error message, fixed by
    commit 5ae6fcc4bb82 ("kconfig: fix line number in recursive inclusion
    error message").
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Ulf Magnusson <ulfalizer@gmail.com>

---
+++ scripts/kconfig/tests/err_recursive_inc/__init__.py
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+"""
+Detect recursive inclusion error.
+
+If recursive inclusion is detected, it should fail with error messages.
+"""
+
+
+def test(conf):
+    assert conf.oldaskconfig() != 0
+    assert conf.stderr_contains('expected_stderr')
