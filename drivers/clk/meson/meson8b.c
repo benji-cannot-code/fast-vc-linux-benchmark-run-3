6c48df643ab52d10c246f82eb9707ee955d88911
Stephen Boyd <sboyd@kernel.org>
2018-03-14 15:36:31 -0700

    clk: meson: Drop unused local variable and add static
    Fixes the following warnings:
    
    drivers/clk/meson/meson8b.c:512:19: warning: symbol 'meson8b_mpeg_clk_div' was not declared. Should it be static?
    drivers/clk/meson/meson8b.c:526:19: warning: symbol 'meson8b_clk81' was not declared. Should it be static?
    drivers/clk/meson/meson8b.c:540:19: warning: symbol 'meson8b_cpu_in_sel' was not declared. Should it be static?
    drivers/clk/meson/meson8b.c:591:19: warning: symbol 'meson8b_cpu_scale_div' was not declared. Should it be static?
    drivers/clk/meson/meson8b.c:608:19: warning: symbol 'meson8b_cpu_scale_out_sel' was not declared. Should it be static?
    drivers/clk/meson/meson8b.c:626:19: warning: symbol 'meson8b_cpu_clk' was not declared. Should it be static?
    drivers/clk/meson/gxbb.c:392:27: warning: symbol 'gxbb_gp0_init_regs' was not declared. Should it be static?
    drivers/clk/meson/gxbb.c:439:27: warning: symbol 'gxl_gp0_init_regs' was not declared. Should it be static?
    drivers/clk/meson/axg.c:195:27: warning: symbol 'axg_gp0_init_regs' was not declared. Should it be static?
    drivers/clk/meson/axg.c:248:27: warning: symbol 'axg_hifi_init_regs' was not declared. Should it be static?
    drivers/clk/meson/meson8b.c: In function 'meson8b_clkc_probe':
    drivers/clk/meson/meson8b.c:1052:14: warning: unused variable 'clk' [-Wunused-variable]
    
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/meson/meson8b.c
+++ drivers/clk/meson/meson8b.c
@@ -510,7 +510,7 @@ static struct clk_regmap meson8b_mpeg_clk_sel = {
 	},
 };
 
-struct clk_regmap meson8b_mpeg_clk_div = {
+static struct clk_regmap meson8b_mpeg_clk_div = {
 	.data = &(struct clk_regmap_div_data){
 		.offset = HHI_MPEG_CLK_CNTL,
 		.shift = 0,
@@ -524,7 +524,7 @@ struct clk_regmap meson8b_mpeg_clk_div = {
 	},
 };
 
-struct clk_regmap meson8b_clk81 = {
+static struct clk_regmap meson8b_clk81 = {
 	.data = &(struct clk_regmap_gate_data){
 		.offset = HHI_MPEG_CLK_CNTL,
 		.bit_idx = 7,
@@ -538,7 +538,7 @@ struct clk_regmap meson8b_clk81 = {
 	},
 };
 
-struct clk_regmap meson8b_cpu_in_sel = {
+static struct clk_regmap meson8b_cpu_in_sel = {
 	.data = &(struct clk_regmap_mux_data){
 		.offset = HHI_SYS_CPU_CLK_CNTL0,
 		.mask = 0x1,
@@ -589,7 +589,7 @@ static const struct clk_div_table cpu_scale_table[] = {
 	{ /* sentinel */ },
 };
 
-struct clk_regmap meson8b_cpu_scale_div = {
+static struct clk_regmap meson8b_cpu_scale_div = {
 	.data = &(struct clk_regmap_div_data){
 		.offset =  HHI_SYS_CPU_CLK_CNTL1,
 		.shift = 20,
@@ -606,7 +606,7 @@ struct clk_regmap meson8b_cpu_scale_div = {
 	},
 };
 
-struct clk_regmap meson8b_cpu_scale_out_sel = {
+static struct clk_regmap meson8b_cpu_scale_out_sel = {
 	.data = &(struct clk_regmap_mux_data){
 		.offset = HHI_SYS_CPU_CLK_CNTL0,
 		.mask = 0x3,
@@ -624,7 +624,7 @@ struct clk_regmap meson8b_cpu_scale_out_sel = {
 	},
 };
 
-struct clk_regmap meson8b_cpu_clk = {
+static struct clk_regmap meson8b_cpu_clk = {
 	.data = &(struct clk_regmap_mux_data){
 		.offset = HHI_SYS_CPU_CLK_CNTL0,
 		.mask = 0x1,
@@ -1050,7 +1050,6 @@ static const struct regmap_config clkc_regmap_config = {
 static int meson8b_clkc_probe(struct platform_device *pdev)
 {
 	int ret, i;
-	struct clk *clk;
 	struct device *dev = &pdev->dev;
 	struct regmap *map;
 
