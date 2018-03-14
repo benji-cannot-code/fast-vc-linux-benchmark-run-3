b7abd6e10ec5c2edfed887ace0e1d5e276618a77
Luca Coelho <luciano.coelho@intel.com>
2018-03-14 16:02:01 +0200

    iwlwifi: pcie: include tcp.h implicitly
    If CONFIG_IPV6 is not enabled in the kernel, tcp.h is not included
    implicitly from other header files, causing compilation errors.  To
    solve that, explicitly include it in tx-gen2.c.
    
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/pcie/tx-gen2.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/tx-gen2.c
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *****************************************************************************/
 #include <linux/pm_runtime.h>
 #include <net/tso.h>
+#include <linux/tcp.h>
 
 #include "iwl-debug.h"
 #include "iwl-csr.h"
