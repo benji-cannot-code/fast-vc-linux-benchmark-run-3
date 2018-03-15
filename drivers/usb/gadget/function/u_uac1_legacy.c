72460dfa03ecadeffe127ee9b502a371216eaaa5
Aaron Wu <aaron.wu@analog.com>
2018-03-15 18:50:17 +0800

    usb: Remove Blackfin references in USB support
    The website is no longer used for tracking bugs.
    
    Signed-off-by: Aaron Wu <aaron.wu@analog.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/usb/gadget/function/u_uac1_legacy.c
+++ drivers/usb/gadget/function/u_uac1_legacy.c
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2008 Bryan Wu <cooloney@kernel.org>
  * Copyright (C) 2008 Analog Devices, Inc
- *
- * Enter bugs at http://blackfin.uclinux.org/
  */
 
 #include <linux/kernel.h>
