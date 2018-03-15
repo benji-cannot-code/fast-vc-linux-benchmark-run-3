0311595acaf2f7d9cc4062b8743544060d8e9d64
Arnd Bergmann <arnd@arndb.de>
2018-03-15 16:34:08 +0100

    pstore: fix crypto dependencies
    The new crypto API use causes some problems with Kconfig dependencies,
    including this link error:
    
    fs/pstore/platform.o: In function `pstore_register':
    platform.c:(.text+0x248): undefined reference to `crypto_has_alg'
    platform.c:(.text+0x2a0): undefined reference to `crypto_alloc_base'
    fs/pstore/platform.o: In function `pstore_unregister':
    platform.c:(.text+0x498): undefined reference to `crypto_destroy_tfm'
    crypto/lz4hc.o: In function `lz4hc_sdecompress':
    lz4hc.c:(.text+0x1a): undefined reference to `LZ4_decompress_safe'
    crypto/lz4hc.o: In function `lz4hc_decompress_crypto':
    lz4hc.c:(.text+0x5a): undefined reference to `LZ4_decompress_safe'
    crypto/lz4hc.o: In function `lz4hc_scompress':
    lz4hc.c:(.text+0xaa): undefined reference to `LZ4_compress_HC'
    crypto/lz4hc.o: In function `lz4hc_mod_init':
    lz4hc.c:(.init.text+0xf): undefined reference to `crypto_register_alg'
    lz4hc.c:(.init.text+0x1f): undefined reference to `crypto_register_scomp'
    lz4hc.c:(.init.text+0x2f): undefined reference to `crypto_unregister_alg'
    
    The problem is that with CONFIG_CRYPTO=m, we must not 'select CRYPTO_LZ4'
    from a bool symbol, or call crypto API functions from a built-in
    module.
    
    This turns the sub-options into 'tristate' ones so the dependencies
    are honored, and makes the pstore itself select the crypto core
    if necessary.
    
    Fixes: cb3bee0369bc ("pstore: Use crypto compress API")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -29,10 +29,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #include <linux/module.h>
 #include <linux/pstore.h>
-#ifdef CONFIG_PSTORE_LZO_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_LZO_COMPRESS)
 #include <linux/lzo.h>
 #endif
-#if defined(CONFIG_PSTORE_LZ4_COMPRESS) || defined(CONFIG_PSTORE_LZ4HC_COMPRESS)
+#if IS_ENABLED(CONFIG_PSTORE_LZ4_COMPRESS) || IS_ENABLED(CONFIG_PSTORE_LZ4HC_COMPRESS)
 #include <linux/lz4.h>
 #endif
 #include <linux/crypto.h>
@@ -143,7 +143,7 @@ bool pstore_cannot_block_path(enum kmsg_dump_reason reason)
 }
 EXPORT_SYMBOL_GPL(pstore_cannot_block_path);
 
-#ifdef CONFIG_PSTORE_DEFLATE_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_DEFLATE_COMPRESS)
 static int zbufsize_deflate(size_t size)
 {
 	size_t cmpr;
@@ -172,21 +172,21 @@ static int zbufsize_deflate(size_t size)
 }
 #endif
 
-#ifdef CONFIG_PSTORE_LZO_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_LZO_COMPRESS)
 static int zbufsize_lzo(size_t size)
 {
 	return lzo1x_worst_compress(size);
 }
 #endif
 
-#if defined(CONFIG_PSTORE_LZ4_COMPRESS) || defined(CONFIG_PSTORE_LZ4HC_COMPRESS)
+#if IS_ENABLED(CONFIG_PSTORE_LZ4_COMPRESS) || IS_ENABLED(CONFIG_PSTORE_LZ4HC_COMPRESS)
 static int zbufsize_lz4(size_t size)
 {
 	return LZ4_compressBound(size);
 }
 #endif
 
-#ifdef CONFIG_PSTORE_842_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_842_COMPRESS)
 static int zbufsize_842(size_t size)
 {
 	return size;
@@ -196,31 +196,31 @@ static int zbufsize_842(size_t size)
 static const struct pstore_zbackend *zbackend __ro_after_init;
 
 static const struct pstore_zbackend zbackends[] = {
-#ifdef CONFIG_PSTORE_DEFLATE_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_DEFLATE_COMPRESS)
 	{
 		.zbufsize	= zbufsize_deflate,
 		.name		= "deflate",
 	},
 #endif
-#ifdef CONFIG_PSTORE_LZO_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_LZO_COMPRESS)
 	{
 		.zbufsize	= zbufsize_lzo,
 		.name		= "lzo",
 	},
 #endif
-#ifdef CONFIG_PSTORE_LZ4_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_LZ4_COMPRESS)
 	{
 		.zbufsize	= zbufsize_lz4,
 		.name		= "lz4",
 	},
 #endif
-#ifdef CONFIG_PSTORE_LZ4HC_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_LZ4HC_COMPRESS)
 	{
 		.zbufsize	= zbufsize_lz4,
 		.name		= "lz4hc",
 	},
 #endif
-#ifdef CONFIG_PSTORE_842_COMPRESS
+#if IS_ENABLED(CONFIG_PSTORE_842_COMPRESS)
 	{
 		.zbufsize	= zbufsize_842,
 		.name		= "842",
