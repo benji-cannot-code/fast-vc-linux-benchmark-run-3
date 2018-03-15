6440b33ab498fd491a89445c76757532b04e51fc
Alex Hung <alex.hung@canonical.com>
2018-03-15 16:22:51 -0700

    intel-hid: clean up and sort header files
    Removed unused header files and sorted them alphabetically. No
    functional changes are made.
    
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/intel-hid.c
+++ drivers/platform/x86/intel-hid.c
@@ -17,16 +17,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/acpi.h>
+#include <linux/dmi.h>
+#include <linux/input.h>
+#include <linux/input/sparse-keymap.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/input.h>
 #include <linux/platform_device.h>
-#include <linux/input/sparse-keymap.h>
-#include <linux/acpi.h>
 #include <linux/suspend.h>
-#include <acpi/acpi_bus.h>
-#include <linux/dmi.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Alex Hung");
