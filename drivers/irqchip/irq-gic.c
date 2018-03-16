227ac21b92184d3bcf844f13cfb0208c0f7f9435
Marc Zyngier <marc.zyngier@arm.com>
2018-03-16 14:35:17 +0000

    irqchip/gic: Loudly complain about the use of IRQ_TYPE_NONE
    There is a huge number of broken device trees out there. Just
    grepping through the tree for the use of IRQ_TYPE_NONE in conjunction
    with the GIC is scary.
    
    People just don't realise that IRQ_TYPE_NONE just doesn't exist, and
    you just get whatever junk was there before. So let's make them aware
    of the issue.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -1012,6 +1012,9 @@ static int gic_irq_domain_translate(struct irq_domain *d,
 			*hwirq += 16;
 
 		*type = fwspec->param[2] & IRQ_TYPE_SENSE_MASK;
+
+		/* Make it clear that broken DTs are... broken */
+		WARN_ON(*type == IRQ_TYPE_NONE);
 		return 0;
 	}
 
@@ -1021,6 +1024,8 @@ static int gic_irq_domain_translate(struct irq_domain *d,
 
 		*hwirq = fwspec->param[0];
 		*type = fwspec->param[1];
+
+		WARN_ON(*type == IRQ_TYPE_NONE);
 		return 0;
 	}
 
