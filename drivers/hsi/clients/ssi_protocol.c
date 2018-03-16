cfaa99ba6fa2cc6e4e8c42afcb3b6cdd6dbfd9f4
Colin Ian King <colin.king@canonical.com>
2018-03-16 14:33:01 +0000

    HSI: ssi_protocol: fix spelling mistake: "trigerred" -> "triggered"
    Trivial fix to spelling mistake in dev_err error message text.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>

--- drivers/hsi/clients/ssi_protocol.c
+++ drivers/hsi/clients/ssi_protocol.c
@@ -496,7 +496,7 @@ static void ssip_rx_wd(struct timer_list *t)
 	struct ssi_protocol *ssi = from_timer(ssi, t, rx_wd);
 	struct hsi_client *cl = ssi->cl;
 
-	dev_err(&cl->device, "Watchdog trigerred\n");
+	dev_err(&cl->device, "Watchdog triggered\n");
 	ssip_error(cl);
 }
 
@@ -505,7 +505,7 @@ static void ssip_tx_wd(struct timer_list *t)
 	struct ssi_protocol *ssi = from_timer(ssi, t, tx_wd);
 	struct hsi_client *cl = ssi->cl;
 
-	dev_err(&cl->device, "Watchdog trigerred\n");
+	dev_err(&cl->device, "Watchdog triggered\n");
 	ssip_error(cl);
 }
 
