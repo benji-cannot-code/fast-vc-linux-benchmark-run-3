004f58190c0475fcba7bbd72afc069640a03802c
Michał Winiarski <michal.winiarski@intel.com>
2018-03-19 10:53:40 +0100

    drm/i915/guc: Split relay control and GuC log level
    Those two concepts are really separate. Since GuC is writing data into
    its own buffer and we even provide a way for userspace to read directly
    from it using i915_guc_log_dump debugfs, there's no real reason to tie
    log level with relay creation.
    Let's create a separate debugfs, giving userspace a way to create a
    relay on demand, when it wants to read a continuous log rather than a
    snapshot.
    
    v2: Don't touch guc_log_level on relay creation error, adjust locking
        after rebase, s/dev_priv/i915, pass guc to file->private_data (Sagar)
        Use struct_mutex rather than runtime.lock for set_log_level
    v3: Tidy ordering of definitions (Sagar)
    
    Signed-off-by: Michał Winiarski <michal.winiarski@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Sagar Arun Kamble <sagar.a.kamble@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Sagar Arun Kamble <sagar.a.kamble@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180319095348.9716-5-michal.winiarski@intel.com

--- drivers/gpu/drm/i915/intel_uc.h
+++ drivers/gpu/drm/i915/intel_uc.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void intel_uc_init_early(struct drm_i915_private *dev_priv);
 void intel_uc_init_mmio(struct drm_i915_private *dev_priv);
-int intel_uc_register(struct drm_i915_private *dev_priv);
-void intel_uc_unregister(struct drm_i915_private *dev_priv);
 void intel_uc_init_fw(struct drm_i915_private *dev_priv);
 void intel_uc_fini_fw(struct drm_i915_private *dev_priv);
 int intel_uc_init_misc(struct drm_i915_private *dev_priv);
