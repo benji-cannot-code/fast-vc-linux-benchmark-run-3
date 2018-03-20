56ec25f694026f4298d3769fa63846119f6f5724
Al Viro <viro@zeniv.linux.org.uk>
2018-03-20 10:34:57 -0400

    sparc: switch compat {f,}truncate64() to COMPAT_SYSCALL_DEFINE
    ... and drop the pointless checks - sys_truncate() itself
    might've lacked the check when that stuff was first written,
    but it has already grown one by the time that stuff went into
    mainline.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/sparc/kernel/sys_sparc32.c
+++ arch/sparc/kernel/sys_sparc32.c
@@ -53,20 +53,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "systbls.h"
 
-asmlinkage long sys32_truncate64(const char __user * path, unsigned long high, unsigned long low)
+COMPAT_SYSCALL_DEFINE3(truncate64, const char __user *, path, u32, high, u32, low)
 {
-	if ((int)high < 0)
-		return -EINVAL;
-	else
-		return sys_truncate(path, (high << 32) | low);
+	return sys_truncate(path, ((u64)high << 32) | low);
 }
 
-asmlinkage long sys32_ftruncate64(unsigned int fd, unsigned long high, unsigned long low)
+COMPAT_SYSCALL_DEFINE3(ftruncate64, unsigned int, fd, u32, high, u32, low)
 {
-	if ((int)high < 0)
-		return -EINVAL;
-	else
-		return sys_ftruncate(fd, (high << 32) | low);
+	return sys_ftruncate(fd, ((u64)high << 32) | low);
 }
 
 static int cp_compat_stat64(struct kstat *stat,
