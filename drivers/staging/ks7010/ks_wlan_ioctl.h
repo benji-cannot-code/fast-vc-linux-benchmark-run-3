40538168f76e7ed947a70f9a1ef4230ed723300f
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2018-03-20 18:13:12 +0100

    staging: ks7010: remove not used function signature ks_wlan_read_config_file
    This commit removes definition of function ks_wlan_read_config_file
    which is not being used at all.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_wlan_ioctl.h
+++ drivers/staging/ks7010/ks_wlan_ioctl.h
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ks_wlan.h"
 #include <linux/netdevice.h>
 
-int ks_wlan_read_config_file(struct ks_wlan_private *priv);
 int ks_wlan_setup_parameter(struct ks_wlan_private *priv,
 			    unsigned int commit_flag);
 
