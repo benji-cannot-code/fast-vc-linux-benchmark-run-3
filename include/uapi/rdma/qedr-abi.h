a777b073184f0b4ff56d2db0b7674a18aba5e10c
Jason Gunthorpe <jgg@mellanox.com>
2018-03-20 14:19:48 -0600

    RDMA/qedr: Fix uABI structure layouts for 32/64 compat
    struct qedr_alloc_ucontext_resp is a different length in 32 and 64
    bit compiles due to implicit compiler padding.
    
    The structs alloc_pd_uresp, create_cq_uresp and create_qp_uresp are
    not padded by the compiler, but in user space the compiler pads them
    due to the way the core and driver structs are concatenated. Make
    this padding explicit and consistent for future sanity.
    
    The kernel driver can already handle the user buffer being smaller
    than required and copies correctly, so no compat or ABI break happens
    from introducing the explicit padding.
    
    Acked-by: Michal Kalderon <michal.kalderon@cavium.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- include/uapi/rdma/qedr-abi.h
+++ include/uapi/rdma/qedr-abi.h
@@ -54,6 +54,7 @@ struct qedr_alloc_ucontext_resp {
 	__u8 dpm_enabled;
 	__u8 wids_enabled;
 	__u16 wid_count;
+	__u32 reserved;
 };
 
 struct qedr_alloc_pd_ureq {
@@ -62,6 +63,7 @@ struct qedr_alloc_pd_ureq {
 
 struct qedr_alloc_pd_uresp {
 	__u32 pd_id;
+	__u32 reserved;
 };
 
 struct qedr_create_cq_ureq {
@@ -72,6 +74,7 @@ struct qedr_create_cq_ureq {
 struct qedr_create_cq_uresp {
 	__u32 db_offset;
 	__u16 icid;
+	__u16 reserved;
 };
 
 struct qedr_create_qp_ureq {
@@ -106,6 +109,7 @@ struct qedr_create_qp_uresp {
 	__u16 rq_icid;
 
 	__u32 rq_db2_offset;
+	__u32 reserved;
 };
 
 #endif /* __QEDR_USER_H__ */
