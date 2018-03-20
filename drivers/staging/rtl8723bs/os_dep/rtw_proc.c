c26c1995ed7d659633b9e920b54c21e2793aeaf5
Paul McQuade <paulmcquad@gmail.com>
2018-03-20 20:26:18 +0000

    Staging:rtl8723bs static variables are always 0
    C standard guarantees that:
    global and static variables will be implicitly initialized to 0 or NULL
    if no explicit initializer is given.
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/rtw_proc.c
+++ drivers/staging/rtl8723bs/os_dep/rtw_proc.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef PROC_DEBUG
 
-static struct proc_dir_entry *rtw_proc = NULL;
+static struct proc_dir_entry *rtw_proc;
 
 #define RTW_PROC_NAME "rtl8723bs"
 
