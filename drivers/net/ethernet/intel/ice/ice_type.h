939571de315a5e969cdf58e774109f1a3dab1cb3
Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
2018-03-20 07:58:05 -0700

    ice: Add basic driver framework for Intel(R) E800 Series
    This patch adds a basic driver framework for the Intel(R) E800 Ethernet
    Series of network devices. There is no functionality right now other than
    the ability to load.
    
    Signed-off-by: Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
    Tested-by: Tony Brelinski <tonyx.brelinski@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

---
+++ drivers/net/ethernet/intel/ice/ice_type.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2018, Intel Corporation. */
+
+#ifndef _ICE_TYPE_H_
+#define _ICE_TYPE_H_
+
+/* Bus parameters */
+struct ice_bus_info {
+	u16 device;
+	u8 func;
+};
+
+/* Port hardware description */
+struct ice_hw {
+	u8 __iomem *hw_addr;
+	void *back;
+
+	/* pci info */
+	u16 device_id;
+	u16 vendor_id;
+	u16 subsystem_device_id;
+	u16 subsystem_vendor_id;
+	u8 revision_id;
+
+	struct ice_bus_info bus;
+};
+
+#endif /* _ICE_TYPE_H_ */
