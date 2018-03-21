f1f17aa1824108cee28f68b30d97b5149cddf31f
Peng Li <lipeng321@huawei.com>
2018-03-21 15:49:24 +0800

    net: hns3: increase the max time for IMP handle command
    It may need more time for IMP handle some command, such as reset.
    This patch enlarges the max time for cmd timeout.
    
    Driver will check the IMP result every us, it may break through the
    loop when get the right result. So not all command need the max time.
    
    Signed-off-by: Peng Li <lipeng321@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/io.h>
 
-#define HCLGE_CMDQ_TX_TIMEOUT		1000
+#define HCLGE_CMDQ_TX_TIMEOUT		30000
 
 struct hclge_dev;
 struct hclge_desc {
