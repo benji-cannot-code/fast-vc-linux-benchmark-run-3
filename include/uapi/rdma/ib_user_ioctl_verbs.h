5f2f4bb8c38ad48d4da375c2985d43dcc0c18310
Matan Barak <matanb@mellanox.com>
2018-03-22 11:52:02 +0200

    IB/uverbs: UAPI pointers should use __aligned_u64 type
    The ioctl() UAPIs are meant to be used by both user-space
    and kernel ioctl() handlers.
    
    Mostly, these UAPI structs tend to consist of simple types, but
    sometimes user-space pointers may be passed between user-space and
    kernel. We would like to avoid dereferencing a user-space pointer in
    the kernel, thus - we always define RDMA_UAPI_PTR as a __aligned_u64
    type.
    
    Fixes: 1f7ff9d5d36a ('IB/uverbs: Move to new headers and make naming consistent')
    Signed-off-by: Matan Barak <matanb@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- include/uapi/rdma/ib_user_ioctl_verbs.h
+++ include/uapi/rdma/ib_user_ioctl_verbs.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 #ifndef RDMA_UAPI_PTR
-#define RDMA_UAPI_PTR(_type, _name)	_type __attribute__((aligned(8))) _name
+#define RDMA_UAPI_PTR(_type, _name)	__aligned_u64 _name
 #endif
 
 #endif
