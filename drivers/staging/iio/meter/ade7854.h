33e5429adae17e3c151597feae233463c8f685db
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2018-03-23 11:27:27 -0300

    staging:iio:ade7854: Remove read_reg_* duplications
    The original code had a read function per data size; after updates, all
    read functions tasks were centralized in a single function, but the old
    signature was kept to maintain the module working without problems. This
    patch removes a set of duplications associated with read_reg_*, and
    update the areas that calling the old interface by the new one.
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/meter/ade7854.h
+++ drivers/staging/iio/meter/ade7854.h
@@ -147,6 +147,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**
  * struct ade7854_state - device instance specific data
  * @spi:		actual spi_device
+ * @read_reg		Wrapper function for I2C and SPI read
  * @write_reg		Wrapper function for I2C and SPI write
  * @indio_dev:		industrial I/O device structure
  * @buf_lock:		mutex to protect tx and rx
@@ -156,10 +157,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ade7854_state {
 	struct spi_device *spi;
 	struct i2c_client *i2c;
-	int (*read_reg_8)(struct device *dev, u16 reg_address, u8 *val);
-	int (*read_reg_16)(struct device *dev, u16 reg_address, u16 *val);
-	int (*read_reg_24)(struct device *dev, u16 reg_address, u32 *val);
-	int (*read_reg_32)(struct device *dev, u16 reg_address, u32 *val);
+	int (*read_reg)(struct device *dev, u16 reg_address, u32 *val,
+			int bits);
 	int (*write_reg)(struct device *dev, u16 reg_address, u32 val,
 			 int bits);
 	int irq;
