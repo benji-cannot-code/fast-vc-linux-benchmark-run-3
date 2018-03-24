35d77e096be795666a5740dec1f0df4abab483a8
Quytelda Kahja <quytelda@tamalin.org>
2018-03-23 23:40:43 -0700

    staging: ks7010: Fix spelling mistakes.
    Fix two spelling mistakes in comments.
    
    Signed-off-by: Quytelda Kahja <quytelda@tamalin.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_hostif.h
+++ drivers/staging/ks7010/ks_hostif.h
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * HOST-MAC I/F data structure
- * Byte alignmet Little Endian
+ * Byte alignment Little Endian
  */
 
 struct hostif_hdr {
