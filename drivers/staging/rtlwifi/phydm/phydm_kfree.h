2c5178cf3e7a5638de42a4a77e7bab1672a11894
Colin Ian King <colin.king@canonical.com>
2018-03-25 18:05:21 +0100

    staging: r8822be: fix typos in header guard macros
    The macros for __PHYDMKFREE_H__ and __PHYDM_FEATURES_H__ contain
    typos and don't match the #if guard check. Defined them correctly.
    
    Cleans up clang warnings:
    warning: '__PHYDMKFREE_H__' is used as a header guard here, followed
    by #define of a different macro [-Wheader-guard]
    warning: '__PHYDM_FEATURES_H__' is used as a header guard here, followed
    by #define of a different macro [-Wheader-guard]
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtlwifi/phydm/phydm_kfree.h
+++ drivers/staging/rtlwifi/phydm/phydm_kfree.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *****************************************************************************/
 
 #ifndef __PHYDMKFREE_H__
-#define __PHYDKFREE_H__
+#define __PHYDMKFREE_H__
 
 #define KFREE_VERSION "1.0"
 
