5fbb1f28dfffe12f3398c22c17ad5cefc9576ae0
Uma Krishnan <ukrishn@linux.vnet.ibm.com>
2018-03-26 11:30:51 -0500

    scsi: cxlflash: Introduce OCXL backend
    Add initial infrastructure to support a new cxlflash transport, OCXL.
    
    Claim a dependency on OCXL and add a new file, ocxl_hw.c, which will host the
    backend routines that are specific to OCXL.
    
    Signed-off-by: Uma Krishnan <ukrishn@linux.vnet.ibm.com>
    Acked-by: Matthew R. Ochs <mrochs@linux.vnet.ibm.com>
    Reviewed-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/cxlflash/backend.h
+++ drivers/scsi/cxlflash/backend.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 extern const struct cxlflash_backend_ops cxlflash_cxl_ops;
+extern const struct cxlflash_backend_ops cxlflash_ocxl_ops;
 
 struct cxlflash_backend_ops {
 	struct module *module;
