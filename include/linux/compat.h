aa7259ded270aacce679c53e8e6cde0fa8a2bb55
Arnd Bergmann <arnd@arndb.de>
2018-03-26 16:59:15 +0200

    time: Add an asm-generic/compat.h file
    We have a couple of files that try to include asm/compat.h on
    architectures where this is available. Those should generally use the
    higher-level linux/compat.h file, but that in turn fails to include
    asm/compat.h when CONFIG_COMPAT is disabled, unless we can provide
    that header on all architectures.
    
    This adds the asm/compat.h for all remaining architectures to
    simplify the dependencies.
    
    Architectures that are getting removed in linux-4.17 are not changed
    here, to avoid needless conflicts with the removal patches. Those
    architectures are broken by this patch, but we have already shown
    that they have no users.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/compat.h
+++ include/linux/compat.h
@@ -19,8 +19,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/unistd.h>
 
-#ifdef CONFIG_COMPAT
 #include <asm/compat.h>
+
+#ifdef CONFIG_COMPAT
 #include <asm/siginfo.h>
 #include <asm/signal.h>
 #endif
