2c3015368955ea8260bb23ebf79fe64a087e90c3
Yuval Mintz <yuvalm@mellanox.com>
2018-03-26 15:01:43 +0300

    mlxsw: spectrum_mr: Add ipv6 specific operations
    Populate the various operation structures meant for IPv6 with logic
    unique to that protocol suite.
    
    Signed-off-by: Yuval Mintz <yuvalm@mellanox.com>
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_mr.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_mr.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _MLXSW_SPECTRUM_MCROUTER_H
 
 #include <linux/mroute.h>
+#include <linux/mroute6.h>
 #include "spectrum_router.h"
 #include "spectrum.h"
 
