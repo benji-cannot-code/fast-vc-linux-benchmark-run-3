2422e74984b4d636e0a941e3c3950119d2bdd245
Uma Krishnan <ukrishn@linux.vnet.ibm.com>
2018-03-26 11:31:44 -0500

    scsi: cxlflash: Setup AFU PASID
    Per the OCXL specification, the maximum PASID supported by the AFU is
    indicated by a field within the configuration space. Similar to acTags,
    implementations can choose to use any sub-range of PASID within their assigned
    range. For cxlflash, the entire range is used.
    
    Signed-off-by: Uma Krishnan <ukrishn@linux.vnet.ibm.com>
    Acked-by: Matthew R. Ochs <mrochs@linux.vnet.ibm.com>
    Reviewed-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Reviewed-by: Frederic Barrat <fbarrat@linux.vnet.ibm.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/cxlflash/ocxl_hw.h
+++ drivers/scsi/cxlflash/ocxl_hw.h
@@ -26,5 +26,6 @@ struct ocxl_hw_afu {
 	int afu_actag_base;		/* AFU acTag base */
 	int afu_actag_enabled;		/* AFU acTag number enabled */
 
+	int max_pasid;			/* Maximum number of contexts */
 	bool is_present;		/* Function has AFUs defined */
 };
