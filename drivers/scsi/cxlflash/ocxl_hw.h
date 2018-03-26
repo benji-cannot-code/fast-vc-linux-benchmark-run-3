216bcf7250a6dc6c72a4997c8cd1af1294adf7aa
Uma Krishnan <ukrishn@linux.vnet.ibm.com>
2018-03-26 11:31:09 -0500

    scsi: cxlflash: Read host function configuration
    Per the OCXL specification, the underlying host can have multiple AFUs per
    function with each function supporting its own configuration. The host
    function configuration is read on the initialization path to evaluate the
    number of functions present and identify the features and configuration of the
    functions present. This data is cached for use in later configuration
    steps. Note that for the OCXL hardware supported by the cxlflash driver, only
    one AFU per function is expected.
    
    Signed-off-by: Uma Krishnan <ukrishn@linux.vnet.ibm.com>
    Acked-by: Matthew R. Ochs <mrochs@linux.vnet.ibm.com>
    Reviewed-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Reviewed-by: Frederic Barrat <fbarrat@linux.vnet.ibm.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/cxlflash/ocxl_hw.h
+++ drivers/scsi/cxlflash/ocxl_hw.h
@@ -17,4 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct ocxl_hw_afu {
 	struct pci_dev *pdev;		/* PCI device */
 	struct device *dev;		/* Generic device */
+
+	struct ocxl_fn_config fcfg;	/* DVSEC config of the function */
+
+	bool is_present;		/* Function has AFUs defined */
 };
