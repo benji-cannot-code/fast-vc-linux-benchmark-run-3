05574291809dc11fd8f569dc08afc66c28326975
Pablo Neira Ayuso <pablo@netfilter.org>
2018-03-27 11:53:06 +0200

    netfilter: nf_tables: nft_register_chain_type() returns void
    Use WARN_ON() instead since it should not happen that neither family
    goes over NFPROTO_NUMPROTO nor there is already a chain of this type
    already registered.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/ipv6/netfilter/nf_tables_ipv6.c
+++ net/ipv6/netfilter/nf_tables_ipv6.c
@@ -50,7 +50,9 @@ static const struct nft_chain_type filter_ipv6 = {
 
 static int __init nf_tables_ipv6_init(void)
 {
-	return nft_register_chain_type(&filter_ipv6);
+	nft_register_chain_type(&filter_ipv6);
+
+	return 0;
 }
 
 static void __exit nf_tables_ipv6_exit(void)
