5ac40becfc165a7d703a363938e6e25a0a5f8183
Yongqiang Sun <yongqiang.sun@amd.com>
2018-03-27 10:05:10 -0400

    drm/amd/display: Change disable backlight ramp change threshold from 0 to maximum value.
    Instead of user set brightness with range of percentage,
    HLK test set brightness level with range of normal, this will result in
    HLK test case set brightness from 0 to 255, DC set brightness with ramp is 0,
    and disabled ramp change which will fail the HLK test.
    Fix:
    In case of unblank stream and turn on edp, change brightness level in
    stream to 0xFFFFFFFF(actural maximum level is 0xFF), use that value as
    a flag to recogonize this the case of resume from S3.
    
    Signed-off-by: Yongqiang Sun <yongqiang.sun@amd.com>
    Reviewed-by: Eric Yang <eric.yang2@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/inc/hw_sequencer.h
+++ drivers/gpu/drm/amd/display/dc/inc/hw_sequencer.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "inc/hw/link_encoder.h"
 #include "core_status.h"
 
+#define EDP_BACKLIGHT_RAMP_DISABLE_LEVEL 0xFFFFFFFF
+
 enum pipe_gating_control {
 	PIPE_GATING_CONTROL_DISABLE = 0,
 	PIPE_GATING_CONTROL_ENABLE,
