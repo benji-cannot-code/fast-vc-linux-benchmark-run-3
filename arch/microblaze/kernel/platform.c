ba32ece5391fbe91037eef46bc85ca5e832089f2
Rob Herring <robh@kernel.org>
2018-03-27 21:06:58 -0500

    microblaze: remove unnecessary prom.h includes
    In preparation to remove prom.h, remove unnecessary prom.h includes.
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Cc: Michal Simek <monstr@monstr.eu>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- arch/microblaze/kernel/platform.c
+++ arch/microblaze/kernel/platform.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/init.h>
 #include <linux/of_platform.h>
-#include <asm/prom.h>
 #include <asm/setup.h>
 
 static struct of_device_id xilinx_of_bus_ids[] __initdata = {
