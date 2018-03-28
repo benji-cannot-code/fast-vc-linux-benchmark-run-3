f54e8af08576b2f477811a2b54eb4025a2c6412d
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2018-03-28 17:24:23 +0200

    staging: ks7010: replace GetUInt32 macro with get_unaligned_le32
    This commit replaces custom GetUInt32 macro with get_unaligned_le32
    which is included in the linux kernel.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/michael_mic.c
+++ drivers/staging/ks7010/michael_mic.c
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/unaligned.h>
 #include "michael_mic.h"
 
-// Convert from Byte[] to UInt32 in a portable way
-#define getUInt32(A, B)	((uint32_t)(A[B + 0] << 0) \
-		+ (A[B + 1] << 8) + (A[B + 2] << 16) + (A[B + 3] << 24))
 
 // Reset the state to the empty message.
 static inline void michael_clear(struct michael_mic_t *mic)
@@ -31,8 +28,8 @@ static inline void michael_clear(struct michael_mic_t *mic)
 static void michael_init(struct michael_mic_t *mic, uint8_t *key)
 {
 	// Set the key
-	mic->k0 = getUInt32(key, 0);
-	mic->k1 = getUInt32(key, 4);
+	mic->k0 = get_unaligned_le32(key);
+	mic->k1 = get_unaligned_le32(key + 4);
 
 	//clear();
 	michael_clear(mic);
@@ -66,13 +63,13 @@ static void michael_append(struct michael_mic_t *mic, uint8_t *src, int bytes)
 		if (mic->m_bytes < 4)
 			return;
 
-		mic->l ^= getUInt32(mic->m, 0);
+		mic->l ^= get_unaligned_le32(mic->m);
 		MichaelBlockFunction(mic->l, mic->r);
 		mic->m_bytes = 0;
 	}
 
 	while (bytes >= 4) {
-		mic->l ^= getUInt32(src, 0);
+		mic->l ^= get_unaligned_le32(src);
 		MichaelBlockFunction(mic->l, mic->r);
 		src += 4;
 		bytes -= 4;
