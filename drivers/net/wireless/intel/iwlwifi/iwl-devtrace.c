ca5c71f79c39fc04488a6b423e36a96a64bca060
Alexei Starovoitov <ast@kernel.org>
2018-03-28 12:05:35 -0700

    net/wireless/iwlwifi: fix iwlwifi_dev_ucode_error tracepoint
    fix iwlwifi_dev_ucode_error tracepoint to pass pointer to a table
    instead of all 17 arguments by value.
    dvm/main.c and mvm/utils.c have 'struct iwl_error_event_table'
    defined with very similar yet subtly different fields and offsets.
    tracepoint is still common and using definition of 'struct iwl_error_event_table'
    from dvm/commands.h while copying fields.
    Long term this tracepoint probably should be split into two.
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- drivers/net/wireless/intel/iwlwifi/iwl-devtrace.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-devtrace.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __CHECKER__
 #include "iwl-trans.h"
 
+#include "dvm/commands.h"
 #define CREATE_TRACE_POINTS
 #include "iwl-devtrace.h"
 
