55ea71ec9e6f2f333c40cae6ff14a02540067075
NeilBrown <neilb@suse.com>
2018-03-29 15:26:48 +1100

    staging: lustre: discard linux-time.h
    Very little is left in linux-time.h.
    Move CFS_TICK to libcfs.h discard the test.
    
    Signed-off-by: NeilBrown <neilb@suse.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/libcfs.h
+++ drivers/staging/lustre/include/linux/libcfs/libcfs.h
@@ -54,6 +54,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LOWEST_BIT_SET(x)       ((x) & ~((x) - 1))
 
 /*
+ * One jiffy
+ */
+#define CFS_TICK		(1UL)
+
+/*
  * Lustre Error Checksum: calculates checksum
  * of Hex number by XORing each bit.
  */
