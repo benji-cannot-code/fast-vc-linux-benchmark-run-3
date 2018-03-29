3fc66c0f7421b0a393c7c99a53dbfcc42781e88d
NeilBrown <neilb@suse.com>
2018-03-29 15:26:48 +1100

    staging: lustre: libcfs: discard cfs_time_add/sub
    cfs_time_add adds its arguments.
    cfs_time_sub subtracts finds the difference.
    Discard these and use '+' and '-' directly.
    
    This change highlighted a type error.  The structure field
    cr_queued_time was used to store jiffies, but was declared
    as time_t (meant for seconds).  So the time is changed to
    "unsigned long".
    
    Signed-off-by: NeilBrown <neilb@suse.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/libcfs_time.h
+++ drivers/staging/lustre/include/linux/libcfs/libcfs_time.h
@@ -41,16 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * generic time manipulation functions.
  */
 
-static inline unsigned long cfs_time_add(unsigned long t, long d)
-{
-	return (unsigned long)(t + d);
-}
-
-static inline unsigned long cfs_time_sub(unsigned long t1, unsigned long t2)
-{
-	return (unsigned long)(t1 - t2);
-}
-
 static inline int cfs_time_after(unsigned long t1, unsigned long t2)
 {
 	return time_before(t2, t1);
@@ -63,7 +53,7 @@ static inline int cfs_time_aftereq(unsigned long t1, unsigned long t2)
 
 static inline unsigned long cfs_time_shift(int seconds)
 {
-	return cfs_time_add(jiffies, seconds * HZ);
+	return jiffies + seconds * HZ;
 }
 
 /*
