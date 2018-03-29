38b9eb08fdab10ea9f0a10c7093e7b42037137fd
Chris Coffey <cmc@babblebit.net>
2018-03-29 13:47:45 +0100

    staging: mt7621-eth: Fix sparse warning in ethtool.c
    This fixes the following sparse warning:
    
    drivers/staging/mt7621-eth/ethtool.c:213:6: warning: symbol
    'mtk_set_ethtool_ops' was not declared. Should it be static?
    
    Signed-off-by: Chris Coffey <cmc@babblebit.net>
    Reviewed-by: NeilBrown <neil@brown.name>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-eth/ethtool.c
+++ drivers/staging/mt7621-eth/ethtool.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "mtk_eth_soc.h"
+#include "ethtool.h"
 
 static const char mtk_gdma_str[][ETH_GSTRING_LEN] = {
 #define _FE(x...)	# x,
