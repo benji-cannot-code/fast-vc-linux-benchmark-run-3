f913ae9e29374c311d1c9c037524d9d375b3e129
Jiong Wang <jiong.wang@netronome.com>
2018-03-28 17:48:25 -0700

    nfp: bpf: read from packet data cache for PTR_TO_PACKET
    This patch assumes there is a packet data cache, and would try to read
    packet data from the cache instead of from memory.
    
    This patch only implements the optimisation "backend", it doesn't build
    the packet data cache, so this optimisation is not enabled.
    
    This patch has only enabled aligned packet data read, i.e. when the read
    offset to the start of cache is REG_WIDTH aligned.
    
    Signed-off-by: Jiong Wang <jiong.wang@netronome.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- drivers/net/ethernet/netronome/nfp/nfp_asm.h
+++ drivers/net/ethernet/netronome/nfp/nfp_asm.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 #define REG_NONE	0
+#define REG_WIDTH	4
 
 #define RE_REG_NO_DST	0x020
 #define RE_REG_IMM	0x020
