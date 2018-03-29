f12926e730df4c213ed52ffd7a3c2f2adc4aa6e9
NeilBrown <neilb@suse.com>
2018-03-29 15:26:48 +1100

    staging: lustre: discard cfs_duration_sec()
    cfs_duration_sec() simply divides by HZ.
    It is mostly used to report durations in debug messages.
    Remove and just use X/HZ.
    
    Signed-off-by: NeilBrown <neilb@suse.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/include/linux/libcfs/linux/linux-time.h
+++ drivers/staging/lustre/include/linux/libcfs/linux/linux-time.h
@@ -61,11 +61,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Generic kernel stuff
  */
 
-static inline long cfs_duration_sec(long d)
-{
-	return d / msecs_to_jiffies(MSEC_PER_SEC);
-}
-
 static inline int cfs_time_before_64(u64 t1, u64 t2)
 {
 	return (__s64)t2 - (__s64)t1 > 0;
