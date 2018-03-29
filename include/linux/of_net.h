a78ae31f36338d6cfdfd206cc2aea20bbc343dc3
Mike Looijmans <mike.looijmans@topic.nl>
2018-03-29 07:29:48 +0200

    of_net: Implement of_get_nvmem_mac_address helper
    It's common practice to store MAC addresses for network interfaces into
    nvmem devices. However the code to actually do this in the kernel lacks,
    so this patch adds of_get_nvmem_mac_address() for drivers to obtain the
    address from an nvmem cell provider.
    
    This is particulary useful on devices where the ethernet interface cannot
    be configured by the bootloader, for example because it's in an FPGA.
    
    Signed-off-by: Mike Looijmans <mike.looijmans@topic.nl>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/of_net.h
+++ include/linux/of_net.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct net_device;
 extern int of_get_phy_mode(struct device_node *np);
 extern const void *of_get_mac_address(struct device_node *np);
+extern int of_get_nvmem_mac_address(struct device_node *np, void *addr);
 extern struct net_device *of_find_net_device_by_node(struct device_node *np);
 #else
 static inline int of_get_phy_mode(struct device_node *np)
@@ -24,6 +25,11 @@ static inline int of_get_phy_mode(struct device_node *np)
 static inline const void *of_get_mac_address(struct device_node *np)
 {
 	return NULL;
+}
+
+static inline int of_get_nvmem_mac_address(struct device_node *np, void *addr)
+{
+	return -ENODEV;
 }
 
 static inline struct net_device *of_find_net_device_by_node(struct device_node *np)
