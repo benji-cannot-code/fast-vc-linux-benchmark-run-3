89f3453ee60b8a27856ca8e1a19a94bbf19efc3c
Daniel Stone <daniels@collabora.com>
2018-03-30 15:11:27 +0100

    drm/tegra: tegra_fb -> drm_framebuffer
    Since tegra_fb is now the same as drm_framebuffer, we can just replace
    the type completely.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: linux-tegra@vger.kernel.org
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/drm.h
+++ drivers/gpu/drm/tegra/drm.h
@@ -30,14 +30,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct reset_control;
 
-struct tegra_fb {
-	struct drm_framebuffer base;
-};
-
 #ifdef CONFIG_DRM_FBDEV_EMULATION
 struct tegra_fbdev {
 	struct drm_fb_helper base;
-	struct tegra_fb *fb;
+	struct drm_framebuffer *fb;
 };
 #endif
 
