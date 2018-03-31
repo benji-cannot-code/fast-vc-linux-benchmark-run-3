800ba7c8cfdcdadc30d7134b3a78cbf247466ae8
Quytelda Kahja <quytelda@tamalin.org>
2018-03-30 23:08:11 -0700

    staging: ks7010: Remove trailing _t from 'struct hostif_request_t'.
    The "_t" suffix is not needed for structure names in this driver, and is a
    reflection of an older typedef system that is no longer in place. Replace
    all occurences of 'struct hostif_request_t' with 'struct hostif_request'.
    
    Signed-off-by: Quytelda Kahja <quytelda@tamalin.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_hostif.h
+++ drivers/staging/ks7010/ks_hostif.h
@@ -311,7 +311,7 @@ struct hostif_stop_request {
 #define CTS_MODE_FALSE	0
 #define CTS_MODE_TRUE	1
 
-struct hostif_request_t {
+struct hostif_request {
 	__le16 phy_type;
 	__le16 cts_mode;
 	__le16 scan_type;
@@ -328,7 +328,7 @@ struct hostif_request_t {
  */
 struct hostif_ps_adhoc_set_request_t {
 	struct hostif_hdr header;
-	struct hostif_request_t request;
+	struct hostif_request request;
 	__le16 channel;
 } __packed;
 
@@ -349,7 +349,7 @@ struct hostif_ps_adhoc_set_confirm_t {
  */
 struct hostif_infrastructure_set_request_t {
 	struct hostif_hdr header;
-	struct hostif_request_t request;
+	struct hostif_request request;
 	struct ssid ssid;
 	__le16 beacon_lost_count;
 	__le16 auth_type;
@@ -371,7 +371,7 @@ struct hostif_infrastructure_set_confirm_t {
  */
 struct hostif_adhoc_set_request_t {
 	struct hostif_hdr header;
-	struct hostif_request_t request;
+	struct hostif_request request;
 	struct ssid ssid;
 	__le16 channel;
 } __packed;
@@ -385,7 +385,7 @@ struct hostif_adhoc_set_request_t {
  */
 struct hostif_adhoc_set2_request_t {
 	struct hostif_hdr header;
-	struct hostif_request_t request;
+	struct hostif_request request;
 	__le16 reserved;
 	struct ssid ssid;
 	struct channel_list channel_list;
