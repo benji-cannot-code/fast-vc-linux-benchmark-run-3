a3ebc0d15b35b3340383ed03870cdca3eca588a1
Michael Chan <michael.chan@broadcom.com>
2018-03-31 13:54:06 -0400

    bnxt_en: Update firmware interface to 1.9.1.15.
    Minor changes, such as new extended port statistics.
    
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -13,11 +13,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BNXT_H
 
 #define DRV_MODULE_NAME		"bnxt_en"
-#define DRV_MODULE_VERSION	"1.9.0"
+#define DRV_MODULE_VERSION	"1.9.1"
 
 #define DRV_VER_MAJ	1
 #define DRV_VER_MIN	9
-#define DRV_VER_UPD	0
+#define DRV_VER_UPD	1
 
 #include <linux/interrupt.h>
 #include <linux/rhashtable.h>
