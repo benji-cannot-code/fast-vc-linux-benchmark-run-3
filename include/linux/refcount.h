881dce5aebd7ca966e39deac02fb91ab0b7113f1
Alexey Dobriyan <adobriyan@gmail.com>
2018-04-01 01:00:36 +0300

    locking/refcounts: Include fewer headers in <linux/refcount.h>
    Debloat <linux/refcount.h>'s dependencies:
    
    - <linux/kernel.h> is not needed, but <linux/compiler.h> is.
    - <linux/mutex.h> is not needed, only a forward declaration of "struct mutex".
    - <linux/spinlock.h> is not needed, <linux/spinlock_types.h> is enough.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Will Deacon <will.deacon@arm.com>
    Link: https://lkml.kernel.org/lkml/20180331220036.GA7676@avx2
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/refcount.h
+++ include/linux/refcount.h
@@ -4,9 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_REFCOUNT_H
 
 #include <linux/atomic.h>
-#include <linux/mutex.h>
-#include <linux/spinlock.h>
-#include <linux/kernel.h>
+#include <linux/compiler.h>
+#include <linux/spinlock_types.h>
+
+struct mutex;
 
 /**
  * struct refcount_t - variant of atomic_t specialized for reference counts
--- lib/refcount.c
+++ lib/refcount.c
@@ -36,7 +36,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/mutex.h>
 #include <linux/refcount.h>
+#include <linux/spinlock.h>
 #include <linux/bug.h>
 
 #ifdef CONFIG_REFCOUNT_FULL
