23d035cb2680280bddec06649e091ede2ca1f93c
Nicholas Piggin <npiggin@gmail.com>
2018-04-01 15:50:35 +1000

    KVM: PPC: Book3S HV: Fix ppc_breakpoint_available compile error
    arch/powerpc/kvm/book3s_hv.c: In function ‘kvmppc_h_set_mode’:
    arch/powerpc/kvm/book3s_hv.c:745:8: error: implicit declaration of function ‘ppc_breakpoint_available’
       if (!ppc_breakpoint_available())
            ^~~~~~~~~~~~~~~~~~~~~~~~
    
    Fixes: 398e712c007f ("KVM: PPC: Book3S HV: Return error from h_set_mode(SET_DAWR) on POWER9")
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kvm/book3s_hv.c
+++ arch/powerpc/kvm/book3s_hv.c
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/reg.h>
 #include <asm/ppc-opcode.h>
 #include <asm/asm-prototypes.h>
+#include <asm/debug.h>
 #include <asm/disassemble.h>
 #include <asm/cputable.h>
 #include <asm/cacheflush.h>
