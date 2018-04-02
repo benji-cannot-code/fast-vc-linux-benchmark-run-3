499b8e612b1558d2695679840bad6cab460211a5
Tariq Toukan <tariqt@mellanox.com>
2018-04-02 16:28:10 +0300

    net/mlx5e: RX, Remove HW LRO support in legacy RQ
    Current LRO implementation in Legacy RQ uses high-order pages.
    In downstream patches of this series we complete the transition
    to using only order-0 pages in RX datapath (which was already done
    in Striding RQ).
    
    Unlike the more advanced Striding RQ, Legacy RQ does not make reuse
    of any non-consumed buffers of non-full LRO sessions, and combining
    it with order-0 pages has many performance drawbacks.
    
    Hence, here we totally remove LRO support in Legacy RQ.
    This guarantees having no out-of-order completions, which allows using
    a cyclic work queue (instead of a linked-list) in a downstream patch.
    
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
@@ -1516,6 +1516,9 @@ static int set_pflag_rx_striding_rq(struct net_device *netdev, bool enable)
 			return -EOPNOTSUPP;
 		if (!mlx5e_striding_rq_possible(mdev, &priv->channels.params))
 			return -EINVAL;
+	} else if (priv->channels.params.lro_en) {
+		netdev_warn(netdev, "Can't set legacy RQ with LRO, disable LRO first\n");
+		return -EINVAL;
 	}
 
 	new_channels.params = priv->channels.params;
@@ -1590,6 +1593,10 @@ static int mlx5e_set_priv_flags(struct net_device *netdev, u32 pflags)
 
 out:
 	mutex_unlock(&priv->state_lock);
+
+	/* Need to fix some features.. */
+	netdev_update_features(netdev);
+
 	return err;
 }
 
