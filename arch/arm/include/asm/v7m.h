253f5bbdb3d0a660b946483f877ac2e306474896
Vladimir Murzin <vladimir.murzin@arm.com>
2018-04-03 10:39:23 +0100

    ARM: 8757/1: NOMMU: Support PMSAv8 MPU
    ARMv8R/M architecture defines new memory protection scheme - PMSAv8
    which is not compatible with PMSAv7.
    
    Key differences to PMSAv7 are:
     - Region geometry is defined by base and limit addresses
     - Addresses need to be either 32 or 64 byte aligned
     - No region priority due to overlapping regions are not allowed
     - It is unified, i.e. no distinction between data/instruction regions
     - Memory attributes are controlled via MAIR
    
    This patch implements support for PMSAv8 MPU defined by ARMv8R/M
    architecture.
    
    Signed-off-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/include/asm/v7m.h
+++ arch/arm/include/asm/v7m.h
@@ -69,6 +69,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PMSAv7_RBAR		0x9c
 #define PMSAv7_RASR		0xa0
 
+#define PMSAv8_RNR		0x98
+#define PMSAv8_RBAR		0x9c
+#define PMSAv8_RLAR		0xa0
+#define PMSAv8_RBAR_A(n)	(PMSAv8_RBAR + 8*(n))
+#define PMSAv8_RLAR_A(n)	(PMSAv8_RLAR + 8*(n))
+#define PMSAv8_MAIR0		0xc0
+#define PMSAv8_MAIR1		0xc4
+
 /* Cache opeartions */
 #define	V7M_SCB_ICIALLU		0x250	/* I-cache invalidate all to PoU */
 #define	V7M_SCB_ICIMVAU		0x258	/* I-cache invalidate by MVA to PoU */
