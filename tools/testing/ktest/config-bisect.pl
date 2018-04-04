92a4e7127ce8c909207457d5f1c39fd163c11002
Steven Rostedt (VMware) <rostedt@goodmis.org>
2018-04-04 15:42:08 -0400

    ktest.pl: Have ktest.pl pass -r to config-bisect.pl to reset bisect
    If config-bisect.pl sees that a config_bisect has already been started, it
    will ask on the command line if it should bisect or not. This will mess up
    running config_bisect from ktest.pl.
    
    Have ktest.pl pass in '-r' to config-bisect.pl and have config-bisect.pl
    recognize that to reset without asking.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- tools/testing/ktest/config-bisect.pl
+++ tools/testing/ktest/config-bisect.pl
@@ -53,11 +53,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 # If "good" or "bad" is not stated at the end, it will copy the good and
 # bad configs to the .tmp versions. If a .tmp version already exists, it will
-# warn before writing over them. If the last config is labeled "good", then
-# it will copy it to the good .tmp version. If the last config is labeled
-# "bad", it will copy it to the bad .tmp version. It will continue this until
-# it can not merge the two any more without the result being equal to either
-# the good or bad .tmp configs.
+# warn before writing over them (-r will not warn, and just write over them).
+# If the last config is labeled "good", then it will copy it to the good .tmp
+# version. If the last config is labeled "bad", it will copy it to the bad
+# .tmp version. It will continue this until it can not merge the two any more
+# without the result being equal to either the good or bad .tmp configs.
 
 my $start = 0;
 my $val = "";
@@ -68,6 +68,7 @@ my $tree = $pwd;
 my $build;
 
 my $output_config;
+my $reset_bisect;
 
 sub usage {
     print << "EOF"
@@ -655,6 +656,10 @@ while ($#ARGV >= 0) {
 	$tree = $val;
     }
 
+    elsif ($opt eq "-r") {
+	$reset_bisect = 1;
+    }
+
     elsif ($opt eq "-h") {
 	usage;
     }
@@ -722,8 +727,10 @@ if ($start) {
 	    $p = "$p$bad exists\n";
 	}
 
-	if (!read_yn "${p}Overwrite and start new bisect anyway?") {
-	    exit (-1);
+	if (!defined($reset_bisect)) {
+	    if (!read_yn "${p}Overwrite and start new bisect anyway?") {
+		exit (-1);
+	    }
 	}
     }
     run_command "cp $good_start $good" or die "failed to copy to $good\n";
