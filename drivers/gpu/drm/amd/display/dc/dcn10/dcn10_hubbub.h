41d530e58b068f1e5d80f35ea1d348e19c16e76e
Eric Bernstein <eric.bernstein@amd.com>
2018-04-05 17:09:20 -0400

    drm/amd/display: Move DCC support functions into dchubbub
    Added dchububu.h header file for common enum/struct definitions.
    Added new interface functions get_dcc_compression_cap,
    dcc_support_swizzle, dcc_support_pixel_format.
    
    Signed-off-by: Eric Bernstein <eric.bernstein@amd.com>
    Reviewed-by: Dmytro Laktyushkin <Dmytro.Laktyushkin@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hubbub.h
+++ drivers/gpu/drm/amd/display/dc/dcn10/dcn10_hubbub.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __DC_HUBBUB_DCN10_H__
 
 #include "core_types.h"
+#include "dchubbub.h"
 
 #define HUBHUB_REG_LIST_DCN()\
 	SR(DCHUBBUB_ARB_DATA_URGENCY_WATERMARK_A),\
@@ -172,12 +173,6 @@ struct dcn_hubbub_wm_set {
 
 struct dcn_hubbub_wm {
 	struct dcn_hubbub_wm_set sets[4];
-};
-
-struct hubbub_funcs {
-	void (*update_dchub)(
-			struct hubbub *hubbub,
-			struct dchub_init_data *dh_data);
 };
 
 struct hubbub {
