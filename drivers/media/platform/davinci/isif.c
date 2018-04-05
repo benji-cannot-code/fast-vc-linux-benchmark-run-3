cdbe83f5046606d2faca6aebc8e0bed05f3fda3c
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2018-04-05 12:21:39 -0400

    media: davinci: allow building isif code
    The only reason why this driver doesn't build with COMPILE_TEST
    is because it includes mach/mux.h. It turns that none of the
    macros defined there are used.
    
    So, get rid of it, in order to allow it to build with
    COMPILE_TEST.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/platform/davinci/isif.c
+++ drivers/media/platform/davinci/isif.c
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/module.h>
 
-#include <mach/mux.h>
-
 #include <media/davinci/isif.h>
 #include <media/davinci/vpss.h>
 
