167340b5d2677035e5513541ce94b0ad45edba3e
Christian Lütke-Stetzkamp <christian@lkamp.de>
2018-04-07 10:16:18 +0200

    staging: mt7621-mmc: Remove unused global: msdc_6575_host
    The mt6575_host variable is never used, only set and
    exported. Removing it for code cleanup.
    
    Signed-off-by: Christian Lütke-Stetzkamp <christian@lkamp.de>
    Reviewed-by: NeilBrown <neil@brown.name>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/sd.c
+++ drivers/staging/mt7621-mmc/sd.c
@@ -155,7 +155,6 @@ static int cd_active_low = 1;
 //#define PERI_MSDC2_PDN    (17)
 //#define PERI_MSDC3_PDN    (18)
 
-struct msdc_host *msdc_6575_host[] = {NULL, NULL, NULL, NULL};
 #if 0 /* --- by chhung */
 /* gate means clock power down */
 static int g_clk_gate = 0;
@@ -2772,8 +2771,6 @@ static int msdc_drv_probe(struct platform_device *pdev)
 		goto release_mem;
 	}
 	msdc_init_gpd_bd(host, &host->dma);
-	/*for emmc*/
-	msdc_6575_host[pdev->id] = host;
 
 #if 0
 	tasklet_init(&host->card_tasklet, msdc_tasklet_card, (ulong)host);
@@ -2950,5 +2947,3 @@ module_exit(mt_msdc_exit);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("MediaTek MT6575 SD/MMC Card Driver");
 MODULE_AUTHOR("Infinity Chen <infinity.chen@mediatek.com>");
-
-EXPORT_SYMBOL(msdc_6575_host);
