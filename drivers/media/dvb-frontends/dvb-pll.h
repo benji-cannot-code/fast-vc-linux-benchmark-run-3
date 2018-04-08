b6943bea49889149792bc5301003443d3ec3ed9c
Akihiro Tsukada <tskd08@gmail.com>
2018-04-08 13:21:35 -0400

    media: dvb-frontends/dvb-pll: add tua6034 ISDB-T tuner used in Friio
    This driver already contains tua6034-based device settings,
    but they are not for ISDB-T and have different parameters.
    
    Signed-off-by: Akihiro Tsukada <tskd08@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/dvb-frontends/dvb-pll.h
+++ drivers/media/dvb-frontends/dvb-pll.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_SAMSUNG_TBMU24112      17
 #define DVB_PLL_TDEE4		       18
 #define DVB_PLL_THOMSON_DTT7520X       19
+#define DVB_PLL_TUA6034_FRIIO          20
 
 struct dvb_pll_config {
 	struct dvb_frontend *fe;
