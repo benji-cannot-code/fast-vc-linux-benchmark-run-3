e95a948aef985f15a6ee7b5a0369e46fccbb766d
Akihiro Tsukada <tskd08@gmail.com>
2018-04-08 13:21:34 -0400

    media: dvb-frontends/dvb-pll: add i2c driver support
    registers the module as an i2c driver,
    but keeps dvb_pll_attach() untouched for compatibility.
    
    Signed-off-by: Akihiro Tsukada <tskd08@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/dvb-frontends/dvb-pll.h
+++ drivers/media/dvb-frontends/dvb-pll.h
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_TDEE4		       18
 #define DVB_PLL_THOMSON_DTT7520X       19
 
+struct dvb_pll_config {
+	struct dvb_frontend *fe;
+};
+
 #if IS_REACHABLE(CONFIG_DVB_PLL)
 /**
  * Attach a dvb-pll to the supplied frontend structure.
