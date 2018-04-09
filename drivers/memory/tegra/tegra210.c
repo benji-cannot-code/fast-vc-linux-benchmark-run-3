4a168a942e05e2742f2bc41d3314539277f964da
Dmitry Osipenko <digetx@gmail.com>
2018-04-09 22:28:30 +0300

    memory: tegra: Remove unused headers inclusions
    Tegra210 contains some unused leftover headers, remove them for
    consistency.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/memory/tegra/tegra210.c
+++ drivers/memory/tegra/tegra210.c
@@ -7,11 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * published by the Free Software Foundation.
  */
 
-#include <linux/of.h>
-#include <linux/mm.h>
-
-#include <asm/cacheflush.h>
-
 #include <dt-bindings/memory/tegra210-mc.h>
 
 #include "mc.h"
