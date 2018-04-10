c2c4a62dacc3afad1a05edb0f87f4e71598917ca
Aaro Koskinen <aaro.koskinen@iki.fi>
2018-04-10 16:34:34 -0700

    init/ramdisk: use pr_cont() at the end of ramdisk loading
    Use pr_cont() at the end of ramdisk loading.  This will avoid the
    rotator and an extra newline appearing in the dmesg.
    
    Before:
      RAMDISK: Loading 2436KiB [1 disk] into ram disk... |
      done.
    
    After:
      RAMDISK: Loading 2436KiB [1 disk] into ram disk... done.
    
    Link: http://lkml.kernel.org/r/20180302205552.16031-1-aaro.koskinen@iki.fi
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- init/do_mounts_rd.c
+++ init/do_mounts_rd.c
@@ -256,7 +256,7 @@ int __init rd_load_image(char *from)
 		nblocks, ((nblocks-1)/devblocks)+1, nblocks>devblocks ? "s" : "");
 	for (i = 0, disk = 1; i < nblocks; i++) {
 		if (i && (i % devblocks == 0)) {
-			printk("done disk #%d.\n", disk++);
+			pr_cont("done disk #%d.\n", disk++);
 			rotate = 0;
 			if (ksys_close(in_fd)) {
 				printk("Error closing the disk.\n");
@@ -279,7 +279,7 @@ int __init rd_load_image(char *from)
 		}
 #endif
 	}
-	printk("done.\n");
+	pr_cont("done.\n");
 
 successful_load:
 	res = 1;
