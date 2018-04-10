41b673e1441deffb1d7f61e65c463abdd8ad531b
Hari Bathini <hbathini@linux.vnet.ibm.com>
2018-04-10 19:11:31 +0530

    powerpc/fadump: Do not use hugepages when fadump is active
    FADump capture kernel boots in restricted memory environment preserving
    the context of previous kernel to save vmcore. Supporting hugepages in
    such environment makes things unnecessarily complicated, as hugepages
    need memory set aside for them. This means most of the capture kernel's
    memory is used in supporting hugepages. In most cases, this results in
    out-of-memory issues while booting FADump capture kernel. But hugepages
    are not of much use in capture kernel whose only job is to save vmcore.
    So, disabling hugepages support, when fadump is active, is a reliable
    solution for the out of memory issues. Introducing a flag variable to
    disable HugeTLB support when fadump is active.
    
    Signed-off-by: Hari Bathini <hbathini@linux.vnet.ibm.com>
    Reviewed-by: Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/hugetlbpage.c
+++ arch/powerpc/mm/hugetlbpage.c
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PAGE_SHIFT_16M	24
 #define PAGE_SHIFT_16G	34
 
+bool hugetlb_disabled = false;
+
 unsigned int HPAGE_SHIFT;
 EXPORT_SYMBOL(HPAGE_SHIFT);
 
@@ -651,6 +653,11 @@ struct kmem_cache *hugepte_cache;
 static int __init hugetlbpage_init(void)
 {
 	int psize;
+
+	if (hugetlb_disabled) {
+		pr_info("HugeTLB support is disabled!\n");
+		return 0;
+	}
 
 #if !defined(CONFIG_PPC_FSL_BOOK3E) && !defined(CONFIG_PPC_8xx)
 	if (!radix_enabled() && !mmu_has_feature(MMU_FTR_16M_PAGE))
