c15127b471b71503ff485598aeca9041c4a57c41
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-04-11 02:10:29 +0000

    ASoC: rsnd: add RSND_GEN3 for R-Car Gen3
    rsnd driver is supporting Gen3. The difference between Gen1 and Gen2
    were very big, but, between Gen2 and Gen3 are not so much.
    Thus, it is assuming Gen2 and Gen3 have compatible, therefore,
    there is no RSND_GEN3 and rsnd_is_gen3() macro.
    But in the future, it will need Gen2 and Gen3 different operation,
    and for Gen4.
    This patch adds missing RSND_GEN3 and rsnd_is_gen3() macro.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Tested-by: Nguyen Viet Dung <dung.nguyen.aj@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sh/rcar/core.c
+++ sound/soc/sh/rcar/core.c
@@ -112,7 +112,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const struct of_device_id rsnd_of_match[] = {
 	{ .compatible = "renesas,rcar_sound-gen1", .data = (void *)RSND_GEN1 },
 	{ .compatible = "renesas,rcar_sound-gen2", .data = (void *)RSND_GEN2 },
-	{ .compatible = "renesas,rcar_sound-gen3", .data = (void *)RSND_GEN2 }, /* gen2 compatible */
+	{ .compatible = "renesas,rcar_sound-gen3", .data = (void *)RSND_GEN3 },
 	{},
 };
 MODULE_DEVICE_TABLE(of, rsnd_of_match);
