ee272818d005f7db1122723aea530bb42a2eec6f
Fabio Estevam <fabio.estevam@nxp.com>
2018-04-11 09:45:19 -0300

    crypto: caam - staticize caam_get_era()
    caam_get_era() is only used locally, so do not export this function
    and make it static instead.
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Reviewed-by: Horia Geantă <horia.geanta@nxp.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/caam/ctrl.c
+++ drivers/crypto/caam/ctrl.c
@@ -401,7 +401,7 @@ start_rng:
  * caam_get_era() - Return the ERA of the SEC on SoC, based
  * on "sec-era" propery in the DTS. This property is updated by u-boot.
  **/
-int caam_get_era(void)
+static int caam_get_era(void)
 {
 	struct device_node *caam_node;
 	int ret;
@@ -413,7 +413,6 @@ int caam_get_era(void)
 
 	return ret ? -ENOTSUPP : prop;
 }
-EXPORT_SYMBOL(caam_get_era);
 
 static const struct of_device_id caam_match[] = {
 	{
