b512bd9fee29e09e38a4bb6b66ab626bc33e1a7a
Michael Ellerman <mpe@ellerman.id.au>
2018-04-12 23:54:00 +1000

    x86/pkeys: Move vma_pkey() into asm/pkeys.h
    Move the last remaining pkey helper, vma_pkey() into asm/pkeys.h
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Reviewed-by: Dave Hansen <dave.hansen@intel.com>

--- include/linux/pkeys.h
+++ include/linux/pkeys.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_PKEYS_H
 #define _LINUX_PKEYS_H
 
-#include <linux/mm_types.h>
+#include <linux/mm.h>
 
 #ifdef CONFIG_ARCH_HAS_PKEYS
 #include <asm/pkeys.h>
