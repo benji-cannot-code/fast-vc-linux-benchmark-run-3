bbea57f92e2d8b21f91edb8e48a445abc92ad36c
Laura Abbott <labbott@redhat.com>
2018-04-13 14:24:12 -0700

    drm/amdkfd: Remove vla
    There's an ongoing effort to remove VLAs[1] from the kernel to eventually
    turn on -Wvla. Switch to a constant value that covers all hardware.
    
    [1] https://lkml.org/lkml/2018/3/7/621
    
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@gmail.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_priv.h
+++ drivers/gpu/drm/amd/amdkfd/kfd_priv.h
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "amd_shared.h"
 
+#define KFD_MAX_RING_ENTRY_SIZE	8
+
 #define KFD_SYSFS_FILE_MODE 0444
 
 /* GPU ID hash width in bits */
