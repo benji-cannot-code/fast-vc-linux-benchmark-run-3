73eba282e7d03ca5529cbb5c43d0604acc526dc6
Philipp Rudo <prudo@linux.vnet.ibm.com>
2018-04-13 15:36:46 -0700

    kernel/kexec_file.c: move purgatories sha256 to common code
    The code to verify the new kernels sha digest is applicable for all
    architectures.  Move it to common code.
    
    One problem is the string.c implementation on x86.  Currently sha256
    includes x86/boot/string.h which defines memcpy and memset to be gcc
    builtins.  By moving the sha256 implementation to common code and
    changing the include to linux/string.h both functions are no longer
    defined.  Thus definitions have to be provided in x86/purgatory/string.c
    
    Link: http://lkml.kernel.org/r/20180321112751.22196-12-prudo@linux.vnet.ibm.com
    Signed-off-by: Philipp Rudo <prudo@linux.vnet.ibm.com>
    Acked-by: Dave Young <dyoung@redhat.com>
    Cc: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/sha256.c
+++ lib/sha256.c
@@ -17,9 +17,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/bitops.h>
+#include <linux/sha256.h>
+#include <linux/string.h>
 #include <asm/byteorder.h>
-#include "sha256.h"
-#include "../boot/string.h"
 
 static inline u32 Ch(u32 x, u32 y, u32 z)
 {
