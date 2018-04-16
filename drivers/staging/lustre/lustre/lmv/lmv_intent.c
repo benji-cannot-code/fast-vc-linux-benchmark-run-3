ad6ddd48562f8667517b6f8ac86030b7be543733
Vitaly Fertman <vitaly.fertman@seagate.com>
2018-04-16 00:14:53 -0400

    staging: lustre: ldlm: xattr locks are lost on mdt
    On the server side mdt_intent_getxattr() can return EFAULT if a
    buffer cannot be found, it is returned after lock_replace, where a
    new lock is installed into lockp. An error forces ldlm_lock_enqueue()
    to destroy the original lock, but ldlm_handle_enqueue0() drops the
    reference on the new lock. The xattr client code implied intent
    error is returned under a lock, which is immediately cancelled.
    Check if a lock obtained and cancel it properly for error cases.
    Note: we should support both cases for interop needs, an intent
    error under a lock and with a lock abort. Keep returning a lock
    with an intent error for interop purposes for now, to be dropped
    later when client will get old enough. make all intent ops to
    work through md_intent_lock: getxattr and layout, which should
    extract the intent error.
    
    Signed-off-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Intel-bug-id: https://jira.hpdd.intel.com/browse/LU-7433
    Seagate-bug-id: MRP-3072 MRP-3137
    Reviewed-on: http://review.whamcloud.com/17220
    Reviewed-by: Andrew Perepechko <andrew.perepechko@seagate.com>
    Reviewed-by: Andriy Skulysh <andriy.skulysh@seagate.com>
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Lai Siyao <lai.siyao@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/lmv/lmv_intent.c
+++ drivers/staging/lustre/lustre/lmv/lmv_intent.c
@@ -448,6 +448,9 @@ static int lmv_intent_lookup(struct obd_export *exp,
 		}
 	}
 
+	if (!it_has_reply_body(it))
+		return 0;
+
 	/*
 	 * MDS has returned success. Probably name has been resolved in
 	 * remote inode. Let's check this.
@@ -484,7 +487,7 @@ int lmv_intent_lock(struct obd_export *exp, struct md_op_data *op_data,
 	       (int)op_data->op_namelen, op_data->op_name,
 	       PFID(&op_data->op_fid1));
 
-	if (it->it_op & (IT_LOOKUP | IT_GETATTR | IT_LAYOUT))
+	if (it->it_op & (IT_LOOKUP | IT_GETATTR | IT_LAYOUT | IT_GETXATTR))
 		rc = lmv_intent_lookup(exp, op_data, it, reqp, cb_blocking,
 				       extra_lock_flags);
 	else if (it->it_op & IT_OPEN)
@@ -498,7 +501,8 @@ int lmv_intent_lock(struct obd_export *exp, struct md_op_data *op_data,
 
 		if (it->it_lock_mode) {
 			lock_handle.cookie = it->it_lock_handle;
-			ldlm_lock_decref(&lock_handle, it->it_lock_mode);
+			ldlm_lock_decref_and_cancel(&lock_handle,
+						    it->it_lock_mode);
 		}
 
 		it->it_lock_handle = 0;
@@ -506,8 +510,8 @@ int lmv_intent_lock(struct obd_export *exp, struct md_op_data *op_data,
 
 		if (it->it_remote_lock_mode) {
 			lock_handle.cookie = it->it_remote_lock_handle;
-			ldlm_lock_decref(&lock_handle,
-					 it->it_remote_lock_mode);
+			ldlm_lock_decref_and_cancel(&lock_handle,
+						    it->it_remote_lock_mode);
 		}
 
 		it->it_remote_lock_handle = 0;
