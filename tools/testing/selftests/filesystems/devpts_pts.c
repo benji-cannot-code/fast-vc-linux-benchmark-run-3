651e2c045fde11ec6541191e10b6bcadc180947c
Anders Roxell <anders.roxell@linaro.org>
2018-04-18 09:52:55 +0200

    selftests/filesystems: devpts_pts included wrong header
    We were picking up the wrong header should use asm/ioctls.h form the kernel
    and not the header from the system (sys/ioctl.h). In the current code we
    added the correct include and we added the kernel headers path to the CFLAGS.
    
    Fixes: ce290a19609d ("selftests: add devpts selftests")
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Signed-off-by: Shuah Khan (Samsung OSG) <shuah@kernel.org>

--- tools/testing/selftests/filesystems/devpts_pts.c
+++ tools/testing/selftests/filesystems/devpts_pts.c
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
+#include <asm/ioctls.h>
 #include <sys/mount.h>
 #include <sys/wait.h>
 
