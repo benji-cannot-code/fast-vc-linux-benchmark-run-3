52dcb5b2f0c3c18cd09677a1f0731034d583b648
Andrew Lunn <andrew@lunn.ch>
2018-04-19 01:02:59 +0200

    net: phy: mdio-gpio: Remove redundant platform data header
    The platform data header file is now unused. Remove it, but add
    an extra include which it brought in.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/mdio-gpio.c
+++ drivers/net/phy/mdio-gpio.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mdio-bitbang.h>
 #include <linux/mdio-gpio.h>
 #include <linux/gpio.h>
-#include <linux/platform_data/mdio-gpio.h>
+#include <linux/gpio/consumer.h>
 
 #include <linux/of_gpio.h>
 #include <linux/of_mdio.h>
