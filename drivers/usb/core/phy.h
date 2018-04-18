470f3201ee715ecf0279bf8ae1a85cab97aee36d
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2018-04-18 21:39:47 +0200

    usb: core: split usb_phy_roothub_{init,alloc}
    Before this patch usb_phy_roothub_init served two purposes (from a
    caller's point of view - like hcd.c):
    - parsing the PHYs and allocating the list entries
    - calling phy_init on each list entry
    
    While this worked so far it has one disadvantage: if we need to call
    phy_init for each PHY instance then the existing code cannot be re-used.
    Solve this by splitting off usb_phy_roothub_alloc which only parses the
    PHYs and allocates the list entries.
    usb_phy_roothub_init then gets a struct usb_phy_roothub and only calls
    phy_init on each PHY instance (along with the corresponding cleanup if
    that failed somewhere).
    
    This is a preparation step for adding proper suspend support for some
    hardware that requires phy_exit to be called during suspend and phy_init
    to be called during resume.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Tested-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Reviewed-by: Roger Quadros <rogerq@ti.com>
    Tested-by: Keerthy <j-keerthy@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/phy.h
+++ drivers/usb/core/phy.h
@@ -1,7 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct usb_phy_roothub;
 
-struct usb_phy_roothub *usb_phy_roothub_init(struct device *dev);
+struct usb_phy_roothub *usb_phy_roothub_alloc(struct device *dev);
+
+int usb_phy_roothub_init(struct usb_phy_roothub *phy_roothub);
 int usb_phy_roothub_exit(struct usb_phy_roothub *phy_roothub);
 
 int usb_phy_roothub_power_on(struct usb_phy_roothub *phy_roothub);
