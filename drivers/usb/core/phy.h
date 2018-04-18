79e09c8f38b1a2ec63673b4ef35fd5b85addead9
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2018-04-18 21:39:50 +0200

    usb: core: phy: add missing forward declaration for "struct device"
    Currently hcd.c is the only consumer of the usb_phy_roothub logic. This
    already includes the required header files so struct device is known.
    However, future consumers might not know about struct device.
    Add a forward declaration for struct device to fix potential future
    consumers which don't include any of the struct device API headers.
    
    Fixes: 07dbff0ddbd86c ("usb: core: add a wrapper for the USB PHYs on the HCD")
    Suggested-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/phy.h
+++ drivers/usb/core/phy.h
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+struct device;
 struct usb_phy_roothub;
 
 struct usb_phy_roothub *usb_phy_roothub_alloc(struct device *dev);
