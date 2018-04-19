f053afd166a75c7dd00d670cf75c76b47d33589e
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-04-19 08:24:29 -0500

    ipmi_ssif: Fix uninitialized variable issue
    Currently, function ssif_remove returns _rv_, which is a variable that
    is never initialized.
    
    Fix this by removing variable _rv_ and return 0 instead.
    
    Addresses-Coverity-ID: 1467999 ("Uninitialized scalar variable")
    Fixes: 6a0d23ed338e ("ipmi: ipmi_unregister_smi() cannot fail, have it
    return void")
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- drivers/char/ipmi/ipmi_ssif.c
+++ drivers/char/ipmi/ipmi_ssif.c
@@ -1228,7 +1228,6 @@ static int ssif_remove(struct i2c_client *client)
 	struct ssif_info *ssif_info = i2c_get_clientdata(client);
 	struct ipmi_smi *intf;
 	struct ssif_addr_info *addr_info;
-	int rv;
 
 	if (!ssif_info)
 		return 0;
@@ -1248,7 +1247,7 @@ static int ssif_remove(struct i2c_client *client)
 		}
 	}
 
-	return rv;
+	return 0;
 }
 
 static int do_cmd(struct i2c_client *client, int len, unsigned char *msg,
