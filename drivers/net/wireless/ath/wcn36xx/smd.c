8a32d66130b909e12b520245057c2203db3b957d
Daniel Mack <daniel@zonque.org>
2018-04-19 19:40:11 +0300

    wcn36xx: pass correct BSS index when deleting BSS keys
    The firmware message to delete BSS keys expects a BSS index to be passed.
    This field is currently hard-coded to 0. Fix this by passing in the index
    we received from the firmware when the BSS was configured.
    
    The encryption type in that message also needs to be set to what was used
    when the key was set, so the assignment of vif_priv->encrypt_type is now
    done after the firmware command was sent. This reportedly fixes the
    following error in AP mode:
    
      wcn36xx: ERROR hal_remove_bsskey response failed err=6
    
    Also, AFAIU, when a BSS is deleted, the firmware apparently drops all the
    keys associated with it. Trying to remove the key explicitly afterwards
    will hence lead to the following message:
    
      wcn36xx: ERROR hal_remove_bsskey response failed err=16
    
    This is now suppressed with an extra check for the BSS index validity.
    
    Signed-off-by: Daniel Mack <daniel@zonque.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/wcn36xx/smd.c
+++ drivers/net/wireless/ath/wcn36xx/smd.c
@@ -1637,6 +1637,7 @@ out:
 
 int wcn36xx_smd_set_bsskey(struct wcn36xx *wcn,
 			   enum ani_ed_type enc_type,
+			   u8 bssidx,
 			   u8 keyidx,
 			   u8 keylen,
 			   u8 *key)
@@ -1646,7 +1647,7 @@ int wcn36xx_smd_set_bsskey(struct wcn36xx *wcn,
 
 	mutex_lock(&wcn->hal_mutex);
 	INIT_HAL_MSG(msg_body, WCN36XX_HAL_SET_BSSKEY_REQ);
-	msg_body.bss_idx = 0;
+	msg_body.bss_idx = bssidx;
 	msg_body.enc_type = enc_type;
 	msg_body.num_keys = 1;
 	msg_body.keys[0].id = keyidx;
@@ -1707,6 +1708,7 @@ out:
 
 int wcn36xx_smd_remove_bsskey(struct wcn36xx *wcn,
 			      enum ani_ed_type enc_type,
+			      u8 bssidx,
 			      u8 keyidx)
 {
 	struct wcn36xx_hal_remove_bss_key_req_msg msg_body;
@@ -1714,7 +1716,7 @@ int wcn36xx_smd_remove_bsskey(struct wcn36xx *wcn,
 
 	mutex_lock(&wcn->hal_mutex);
 	INIT_HAL_MSG(msg_body, WCN36XX_HAL_RMV_BSSKEY_REQ);
-	msg_body.bss_idx = 0;
+	msg_body.bss_idx = bssidx;
 	msg_body.enc_type = enc_type;
 	msg_body.key_id = keyidx;
 
