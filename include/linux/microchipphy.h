7197764f68233d336e86315b48135a56ee8204b2
Phil Elwell <phil@raspberrypi.org>
2018-04-19 17:59:39 +0100

    lan78xx: Read LED states from Device Tree
    Add support for DT property "microchip,led-modes", a vector of zero
    to four cells (u32s) in the range 0-15, each of which sets the mode
    for one of the LEDs. Some possible values are:
    
        0=link/activity          1=link1000/activity
        2=link100/activity       3=link10/activity
        4=link100/1000/activity  5=link10/1000/activity
        6=link10/100/activity    14=off    15=on
    
    These values are given symbolic constants in a dt-bindings header.
    
    Also use the presence of the DT property to indicate that the
    LEDs should be enabled - necessary in the event that no valid OTP
    or EEPROM is available.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/microchipphy.h
+++ include/linux/microchipphy.h
@@ -71,4 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	LAN88XX_MMD3_CHIP_ID			(32877)
 #define	LAN88XX_MMD3_CHIP_REV			(32878)
 
+/* Registers specific to the LAN7800/LAN7850 embedded phy */
+#define LAN78XX_PHY_LED_MODE_SELECT		(0x1D)
+
 #endif /* _MICROCHIPPHY_H */
