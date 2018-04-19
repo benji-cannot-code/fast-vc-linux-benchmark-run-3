eb487a97c2b4da1554ecfcec9bc93107732009fa
Michael Schmitz <schmitzmic@gmail.com>
2018-04-19 14:05:25 +1200

    net-next: ax88796: release platform device drvdata on probe error and module remove
    The net device struct pointer is stored as platform device drvdata on
    module probe - clear the drvdata entry on probe fail there, as well as
    when unloading the module.
    
    Signed-off-by: Michael Schmitz <schmitzmic@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/8390/ax88796.c
+++ drivers/net/ethernet/8390/ax88796.c
@@ -827,6 +827,7 @@ static int ax_remove(struct platform_device *pdev)
 		release_mem_region(mem->start, resource_size(mem));
 	}
 
+	platform_set_drvdata(pdev, NULL);
 	free_netdev(dev);
 
 	return 0;
@@ -960,6 +961,7 @@ static int ax_probe(struct platform_device *pdev)
 	release_mem_region(mem->start, mem_size);
 
  exit_mem:
+	platform_set_drvdata(pdev, NULL);
 	free_netdev(dev);
 
 	return ret;
