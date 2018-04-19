7150c836d3ac44ec05f46143a08b7ecb78c12892
Wolfram Sang <wsa@the-dreams.de>
2018-04-19 22:00:09 +0200

    i2c: ocores: move header to platform_data
    This header only contains platform_data. Move it to the proper directory.
    
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    Acked-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/timberdale.c
+++ drivers/mfd/timberdale.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timb_gpio.h>
 
 #include <linux/i2c.h>
-#include <linux/i2c-ocores.h>
+#include <linux/platform_data/i2c-ocores.h>
 #include <linux/i2c-xiic.h>
 
 #include <linux/spi/spi.h>
