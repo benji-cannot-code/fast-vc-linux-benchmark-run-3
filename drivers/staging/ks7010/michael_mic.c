98f1e2798dcea4ebd75b1c2a0dee232cb2343e90
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2018-04-19 07:08:10 +0200

    staging: ks7010: review includes of michael_mic.c file
    This commit reviews includes of michael_mic.c source file
    removing those which are not being used at all and
    reordering the remaining ones in alphabetically order.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/michael_mic.c
+++ drivers/staging/ks7010/michael_mic.c
@@ -10,10 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   published by the Free Software Foundation.
  */
 
-#include <linux/types.h>
-#include <linux/string.h>
-#include <linux/bitops.h>
 #include <asm/unaligned.h>
+#include <linux/bitops.h>
+#include <linux/string.h>
 #include "michael_mic.h"
 
 
