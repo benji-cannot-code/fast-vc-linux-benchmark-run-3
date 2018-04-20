ce81737b347fb0d2b98121deaac5aa602db2a10f
Linus Walleij <linus.walleij@linaro.org>
2018-04-20 10:29:51 +0200

    block: mq: Add some minor doc for core structs
    As it came up in discussion on the mailing list that the semantic
    meaning of 'blk_mq_ctx' and 'blk_mq_hw_ctx' isn't completely
    obvious to everyone, let's add some minimal kerneldoc for a
    starter.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq.h
+++ block/blk-mq.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct blk_mq_tag_set;
 
+/**
+ * struct blk_mq_ctx - State for a software queue facing the submitting CPUs
+ */
 struct blk_mq_ctx {
 	struct {
 		spinlock_t		lock;
