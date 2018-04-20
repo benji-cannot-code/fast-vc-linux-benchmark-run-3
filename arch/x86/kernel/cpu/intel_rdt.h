a94dfdfe1aa11d8fce3474bfee7db77cbcff8a51
Vikas Shivappa <vikas.shivappa@linux.intel.com>
2018-04-20 15:36:21 -0700

    x86/intel_rdt/mba_sc: Feedback loop to dynamically update mem bandwidth
    mba_sc is a feedback loop where we periodically read MBM counters and
    try to restrict the bandwidth below a max value so the below is always
    true:
    
      "current bandwidth(cur_bw) < user specified bandwidth(user_bw)"
    
    The frequency of these checks is currently 1s and we just tag along the
    MBM overflow timer to do the updates. Doing it once in a second also
    makes the calculation of bandwidth easy. The steps of increase or
    decrease of bandwidth is the minimum granularity specified by the
    hardware.
    
    Although the MBA's goal is to restrict the bandwidth below a maximum,
    there may be a need to even increase the bandwidth. Since MBA controls
    the L2 external bandwidth where as MBM measures the L3 external
    bandwidth, we may end up restricting some rdtgroups unnecessarily. This
    may happen in the sequence where rdtgroup (set of jobs) had high
    "L3 <-> memory traffic" in initial phases -> mba_sc kicks in and reduced
    bandwidth percentage values -> but after some it has mostly "L2 <-> L3"
    traffic. In this scenario mba_sc increases the bandwidth percentage when
    there is lesser memory traffic.
    
    Signed-off-by: Vikas Shivappa <vikas.shivappa@linux.intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: ravi.v.shankar@intel.com
    Cc: tony.luck@intel.com
    Cc: fenghua.yu@intel.com
    Cc: vikas.shivappa@intel.com
    Cc: ak@linux.intel.com
    Cc: hpa@zytor.com
    Link: https://lkml.kernel.org/r/1524263781-14267-7-git-send-email-vikas.shivappa@linux.intel.com

--- arch/x86/kernel/cpu/intel_rdt.h
+++ arch/x86/kernel/cpu/intel_rdt.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MBM_CNTR_WIDTH			24
 #define MBM_OVERFLOW_INTERVAL		1000
+#define MAX_MBA_BW			100u
 
 #define RMID_VAL_ERROR			BIT_ULL(63)
 #define RMID_VAL_UNAVAIL		BIT_ULL(62)
@@ -462,6 +463,7 @@ void mbm_setup_overflow_handler(struct rdt_domain *dom,
 void mbm_handle_overflow(struct work_struct *work);
 bool is_mba_sc(struct rdt_resource *r);
 void setup_default_ctrlval(struct rdt_resource *r, u32 *dc, u32 *dm);
+u32 delay_bw_map(unsigned long bw, struct rdt_resource *r);
 void cqm_setup_limbo_handler(struct rdt_domain *dom, unsigned long delay_ms);
 void cqm_handle_limbo(struct work_struct *work);
 bool has_busy_rmid(struct rdt_resource *r, struct rdt_domain *d);
