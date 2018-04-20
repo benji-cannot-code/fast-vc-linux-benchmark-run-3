5b5375d8cfdbfe3d6d30b041caf312e667225673
Arnd Bergmann <arnd@arndb.de>
2018-04-20 18:14:25 +0200

    rtc: ls1x: remove mktime usage
    The loongson1 platform is 32-bit, so storing a time value in 32 bits
    suffers from limited range. In this case it is likely to be correct
    until 2106, but it's better to avoid the limitation and just use
    the time64_t based mktime64() and rtc_time64_to_tm() interfaces.
    
    The hardware uses a 32-bit year number, and time64_t can cover that
    entire range.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-ls1x.c
+++ drivers/rtc/rtc-ls1x.c
@@ -88,16 +88,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int ls1x_rtc_read_time(struct device *dev, struct rtc_time *rtm)
 {
-	unsigned long v, t;
+	unsigned long v;
+	time64_t t;
 
 	v = readl(SYS_TOYREAD0);
 	t = readl(SYS_TOYREAD1);
 
 	memset(rtm, 0, sizeof(struct rtc_time));
-	t  = mktime((t & LS1X_YEAR_MASK), ls1x_get_month(v),
+	t  = mktime64((t & LS1X_YEAR_MASK), ls1x_get_month(v),
 			ls1x_get_day(v), ls1x_get_hour(v),
 			ls1x_get_min(v), ls1x_get_sec(v));
-	rtc_time_to_tm(t, rtm);
+	rtc_time64_to_tm(t, rtm);
 
 	return 0;
 }
