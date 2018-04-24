e252abe29e88897f6e6f010dec6272f417474cb5
Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
2018-04-24 16:39:42 +0100

    drm: rcar-du: of: Include header to define prototypes
    The symbol 'rcar_du_of_init' is defined by the rcar_du_of module header,
    but it is not included by the C implementation.
    
    Include the header to correctly define the function prototypes.
    
    Fixes the following warning:
    
    linux/drivers/gpu/drm/rcar-du/rcar_du_of.c:319:13:
       warning: symbol 'rcar_du_of_init' was not declared. Should it be static?
        CC      drivers/gpu/drm/rcar-du/rcar_du_of.o
    
    Fixes: 81c0e3dd8292 ("drm: rcar-du: Fix legacy DT to create LVDS encoder nodes")
    Signed-off-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Reviewed-by: Vaishali Thakkar <vthakkar@vaishalithakkar.in>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/gpu/drm/rcar-du/rcar_du_of.c
+++ drivers/gpu/drm/rcar-du/rcar_du_of.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "rcar_du_crtc.h"
 #include "rcar_du_drv.h"
+#include "rcar_du_of.h"
 
 /* -----------------------------------------------------------------------------
  * Generic Overlay Handling
