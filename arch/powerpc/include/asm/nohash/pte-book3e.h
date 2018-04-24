91425b694584503c8daf379ff78c7df1c58d9c75
Christophe Leroy <christophe.leroy@c-s.fr>
2018-04-24 18:31:30 +0200

    powerpc/nohash: Remove _PAGE_BUSY
    _PAGE_BUSY is always 0, remove it.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/nohash/pte-book3e.h
+++ arch/powerpc/include/asm/nohash/pte-book3e.h
@@ -58,12 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PAGE_USER		(_PAGE_BAP_UR | _PAGE_BAP_SR) /* Can be read */
 #define _PAGE_PRIVILEGED	(_PAGE_BAP_SR)
 
-#define _PAGE_BUSY	0
-
 #define _PAGE_SPECIAL	_PAGE_SW0
-
-/* Flags to be preserved on PTE modifications */
-#define _PAGE_HPTEFLAGS	_PAGE_BUSY
 
 /* Base page size */
 #ifdef CONFIG_PPC_64K_PAGES
