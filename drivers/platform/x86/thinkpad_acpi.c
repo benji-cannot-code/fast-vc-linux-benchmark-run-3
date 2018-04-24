8645ca7cd0fcafc0a8d96e53296d38deb19bafef
Henrique de Moraes Holschuh <hmh@hmh.eng.br>
2018-04-24 16:56:05 -0300

    platform/x86: thinkpad_acpi: silence false-positive-prone pr_warn
    Do not consider unknown HKEY events in the 0x6000 range to be thermal
    warnings.  Instead, handle them as a generic unknown HKEY event, which
    are reported to the kernel log at priority "notice", and do not trigger
    a thermal registers state dump to the log.
    
    Signed-off-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Tested-by: Jordan Glover <Golden_Miller83@protonmail.ch>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/thinkpad_acpi.c
+++ drivers/platform/x86/thinkpad_acpi.c
@@ -4040,8 +4040,6 @@ static bool hotkey_notify_6xxx(const u32 hkey,
 				 bool *send_acpi_ev,
 				 bool *ignore_acpi_ev)
 {
-	bool known = true;
-
 	/* 0x6000-0x6FFF: thermal alarms/notices and keyboard events */
 	*send_acpi_ev = true;
 	*ignore_acpi_ev = false;
@@ -4108,13 +4106,12 @@ static bool hotkey_notify_6xxx(const u32 hkey,
 		return true;
 
 	default:
-		pr_warn("unknown possible thermal alarm or keyboard event received\n");
-		known = false;
+		/* report simply as unknown, no sensor dump */
+		return false;
 	}
 
 	thermal_dump_all_sensors();
-
-	return known;
+	return true;
 }
 
 static void hotkey_notify(struct ibm_struct *ibm, u32 event)
