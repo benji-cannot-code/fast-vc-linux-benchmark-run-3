dca348501125f29332e0dd433c1c40c0a85c2b7a
Viresh Kumar <viresh.kumar@linaro.org>
2018-04-24 15:09:45 +0530

    cpufreq: dt: Allow platform specific suspend/resume callbacks
    Platforms may need to implement platform specific suspend/resume hooks.
    Update cpufreq-dt driver's platform data to contain those for such
    platforms.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Tested-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq-dt.h
+++ drivers/cpufreq/cpufreq-dt.h
@@ -13,8 +13,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
+struct cpufreq_policy;
+
 struct cpufreq_dt_platform_data {
 	bool have_governor_per_policy;
+
+	int (*suspend)(struct cpufreq_policy *policy);
+	int (*resume)(struct cpufreq_policy *policy);
 };
 
 #endif /* __CPUFREQ_DT_H__ */
