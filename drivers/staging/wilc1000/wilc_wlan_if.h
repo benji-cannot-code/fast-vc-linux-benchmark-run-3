b3014710766517b9e494199211f6e477002759b4
Ajay Singh <ajay.kathat@microchip.com>
2018-04-25 22:48:15 +0530

    staging: wilc1000: remove used #define HIF_SDIO_GPIO_IRQ
    Cleanup patch to remove unused #define.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan_if.h
+++ drivers/staging/wilc1000/wilc_wlan_if.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define HIF_SDIO		(0)
 #define HIF_SPI			BIT(0)
-#define HIF_SDIO_GPIO_IRQ	BIT(2)
 
 /********************************************
  *
