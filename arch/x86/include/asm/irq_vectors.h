5f8326ab5d0c423a6dd827792bad6a77217d434a
Dou Liyang <douly.fnst@cn.fujitsu.com>
2018-04-25 10:05:53 +0800

    x86/vector: Remove the macro VECTOR_OFFSET_START
    Now, Linux uses matrix allocator for vector assignment, the original
    assignment code which used VECTOR_OFFSET_START has been removed.
    
    So remove the stale macro as well.
    
    Fixes: commit 69cde0004a4b ("x86/vector: Use matrix allocator for vector assignment")
    Signed-off-by: Dou Liyang <douly.fnst@cn.fujitsu.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: hpa@zytor.com
    Link: https://lkml.kernel.org/r/20180425020553.17210-1-douly.fnst@cn.fujitsu.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/include/asm/irq_vectors.h
+++ arch/x86/include/asm/irq_vectors.h
@@ -35,11 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * (0x80 is the syscall vector, 0x30-0x3f are for ISA)
  */
 #define FIRST_EXTERNAL_VECTOR		0x20
-/*
- * We start allocating at 0x21 to spread out vectors evenly between
- * priority levels. (0x80 is the syscall vector)
- */
-#define VECTOR_OFFSET_START		1
 
 /*
  * Reserve the lowest usable vector (and hence lowest priority)  0x20 for
