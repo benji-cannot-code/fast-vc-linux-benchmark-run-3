f12ce01e02c5898486bf6916f8fb32af1304dfc5
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-04-25 11:45:03 +0300

    dmaengine: ti: New directory for Texas Instruments DMA drivers
    Collect the Texas Instruments DMA drivers under drivers/dma/ti/
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/ti/cppi41.c
+++ drivers/dma/ti/cppi41.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/of_address.h>
 #include <linux/pm_runtime.h>
-#include "dmaengine.h"
+#include "../dmaengine.h"
 
 #define DESC_TYPE	27
 #define DESC_TYPE_HOST	0x10
