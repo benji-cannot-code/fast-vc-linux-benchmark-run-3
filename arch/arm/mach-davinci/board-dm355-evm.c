84ae4f79f06e01858ba62bb1137e47dc9f452472
Sekhar Nori <nsekhar@ti.com>
2018-04-25 14:53:23 +0530

    ARM: davinci: board-dm355-evm: fix broken networking
    Since commit 09f3756bb9a8 ("dm9000: Return an ERR_PTR() in all
    error conditions of dm9000_parse_dt()"), passing either non-NULL
    platform data or device-tree for dm9000 driver to probe is
    mandatory.
    
    DM335 board was using none, so networking failed to initialize.
    Fix it by passing non-NULL (but empty) platform data.
    
    Fixes: 09f3756bb9a8 ("dm9000: Return an ERR_PTR() in all error conditions of dm9000_parse_dt()")
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/board-dm355-evm.c
+++ arch/arm/mach-davinci/board-dm355-evm.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/gpio/machine.h>
 #include <linux/clk.h>
+#include <linux/dm9000.h>
 #include <linux/videodev2.h>
 #include <media/i2c/tvp514x.h>
 #include <linux/spi/spi.h>
@@ -180,11 +181,16 @@ static struct resource dm355evm_dm9000_rsrc[] = {
 	},
 };
 
+static struct dm9000_plat_data dm335evm_dm9000_platdata;
+
 static struct platform_device dm355evm_dm9000 = {
 	.name		= "dm9000",
 	.id		= -1,
 	.resource	= dm355evm_dm9000_rsrc,
 	.num_resources	= ARRAY_SIZE(dm355evm_dm9000_rsrc),
+	.dev		= {
+		.platform_data = &dm335evm_dm9000_platdata,
+	},
 };
 
 static struct tvp514x_platform_data tvp5146_pdata = {
