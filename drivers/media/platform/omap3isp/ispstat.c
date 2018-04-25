0f67dcf0ebf88f0f6666d89a944da108d6a51f1f
Arnd Bergmann <arnd@arndb.de>
2018-04-25 17:30:10 -0400

    media: omap3isp: support 64-bit version of omap3isp_stat_data
    C libraries with 64-bit time_t use an incompatible format for
    struct omap3isp_stat_data. This changes the kernel code to
    support either version, by moving over the normal handling
    to the 64-bit variant, and adding compatiblity code to handle
    the old binary format with the existing ioctl command code.
    
    Fortunately, the command code includes the size of the structure,
    so the difference gets handled automatically. In the process of
    eliminating the references to 'struct timeval' from the kernel,
    I also change the way the timestamp is generated internally,
    basically by open-coding the v4l2_get_timestamp() call.
    
    [Sakari Ailus: Alphabetical order of headers, clean up compat code]
    
    Cc: Sakari Ailus <sakari.ailus@iki.fi>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/omap3isp/ispstat.c
+++ drivers/media/platform/omap3isp/ispstat.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/dma-mapping.h>
 #include <linux/slab.h>
+#include <linux/timekeeping.h>
 #include <linux/uaccess.h>
 
 #include "isp.h"
@@ -238,7 +239,7 @@ static int isp_stat_buf_queue(struct ispstat *stat)
 	if (!stat->active_buf)
 		return STAT_NO_BUF;
 
-	v4l2_get_timestamp(&stat->active_buf->ts);
+	ktime_get_ts64(&stat->active_buf->ts);
 
 	stat->active_buf->buf_size = stat->buf_size;
 	if (isp_stat_buf_check_magic(stat, stat->active_buf)) {
@@ -499,7 +500,8 @@ int omap3isp_stat_request_statistics(struct ispstat *stat,
 		return PTR_ERR(buf);
 	}
 
-	data->ts = buf->ts;
+	data->ts.tv_sec = buf->ts.tv_sec;
+	data->ts.tv_usec = buf->ts.tv_nsec / NSEC_PER_USEC;
 	data->config_counter = buf->config_counter;
 	data->frame_number = buf->frame_number;
 	data->buf_size = buf->buf_size;
@@ -507,6 +509,23 @@ int omap3isp_stat_request_statistics(struct ispstat *stat,
 	buf->empty = 1;
 	isp_stat_buf_release(stat);
 	mutex_unlock(&stat->ioctl_lock);
+
+	return 0;
+}
+
+int omap3isp_stat_request_statistics_time32(struct ispstat *stat,
+					struct omap3isp_stat_data_time32 *data)
+{
+	struct omap3isp_stat_data data64;
+	int ret;
+
+	ret = omap3isp_stat_request_statistics(stat, &data64);
+	if (ret)
+		return ret;
+
+	data->ts.tv_sec = data64.ts.tv_sec;
+	data->ts.tv_usec = data64.ts.tv_usec;
+	memcpy(&data->buf, &data64.buf, sizeof(*data) - sizeof(data->ts));
 
 	return 0;
 }
