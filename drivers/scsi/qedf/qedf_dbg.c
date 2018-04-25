2adfa1c301595e90a28b07750b12e1d17f3fdcaf
Chad Dupuis <chad.dupuis@cavium.com>
2018-04-25 06:09:04 -0700

    scsi: qedf: Update copyright for 2018
    Signed-off-by: Chad Dupuis <chad.dupuis@cavium.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qedf/qedf_dbg.c
+++ drivers/scsi/qedf/qedf_dbg.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *  QLogic FCoE Offload Driver
- *  Copyright (c) 2016 Cavium Inc.
+ *  Copyright (c) 2016-2018 Cavium Inc.
  *
  *  This software is available under the terms of the GNU General Public License
  *  (GPL) Version 2, available from the file COPYING in the main directory of
