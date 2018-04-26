27654425c4aff603be8d385d57dc71d8acb7c85a
Keith Busch <keith.busch@intel.com>
2018-04-26 14:24:29 -0600

    nvme/multipath: Disable runtime writable enabling parameter
    We can't allow the user to change multipath settings at runtime, as this
    will create naming conflicts due to the different naming schemes used
    for each mode.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/nvme/host/multipath.c
+++ drivers/nvme/host/multipath.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "nvme.h"
 
 static bool multipath = true;
-module_param(multipath, bool, 0644);
+module_param(multipath, bool, 0444);
 MODULE_PARM_DESC(multipath,
 	"turn on native support for multiple controllers per subsystem");
 
