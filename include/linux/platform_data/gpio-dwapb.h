dc1c027ef5121577434bb6342c89ca08a3498671
Phil Edworthy <phil.edworthy@renesas.com>
2018-04-26 17:19:47 +0100

    gpio: dwapb: Add support for 1 interrupt per port A GPIO
    The DesignWare GPIO IP can be configured for either 1 interrupt or 1
    per GPIO in port A, but the driver currently only supports 1 interrupt.
    See the DesignWare DW_apb_gpio Databook description of the
    'GPIO_INTR_IO' parameter.
    
    This change allows the driver to work with up to 32 interrupts, it will
    get as many interrupts as specified in the DT 'interrupts' property.
    It doesn't do anything clever with the different interrupts, it just calls
    the same handler used for single interrupt hardware.
    
    ACPI companion code provided by Hoan Tran <hotran@apm.com>. This was tested
    on X-Gene by Hoan.
    
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Acked-by: Lee Jones <lee.jones@linaro.org>
    Acked-by: Hoan Tran <hotran@apm.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- include/linux/platform_data/gpio-dwapb.h
+++ include/linux/platform_data/gpio-dwapb.h
@@ -20,7 +20,8 @@ struct dwapb_port_property {
 	unsigned int	idx;
 	unsigned int	ngpio;
 	unsigned int	gpio_base;
-	unsigned int	irq;
+	unsigned int	irq[32];
+	bool		has_irq;
 	bool		irq_shared;
 };
 
