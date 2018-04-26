b776161a627795cb740c800936a0bcba46d0ce56
Dou Liyang <douly.fnst@cn.fujitsu.com>
2018-04-26 14:08:32 +0800

    x86/vector: Remove the unused macro FPU_IRQ
    The macro FPU_IRQ has never been used since v3.10, So remove it.
    
    Signed-off-by: Dou Liyang <douly.fnst@cn.fujitsu.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: hpa@zytor.com
    Link: https://lkml.kernel.org/r/20180426060832.27312-1-douly.fnst@cn.fujitsu.com

--- arch/x86/include/asm/irq_vectors.h
+++ arch/x86/include/asm/irq_vectors.h
@@ -115,8 +115,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FIRST_SYSTEM_VECTOR		NR_VECTORS
 #endif
 
-#define FPU_IRQ				  13
-
 /*
  * Size the maximum number of interrupts.
  *
