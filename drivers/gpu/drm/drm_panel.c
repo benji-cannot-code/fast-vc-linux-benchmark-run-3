27b47d452ea3255e286ca6f3cc2ef16ea1e1cd3f
Jyri Sarha <jsarha@ti.com>
2018-04-26 11:07:00 +0300

    drm/panel: Add device_link from panel device to DRM device
    Add device_link from panel device (supplier) to DRM device (consumer)
    when drm_panel_attach() is called. This patch should protect the master
    DRM driver if an attached panel driver unbinds while it is in use. The
    device_link should make sure the DRM device is unbound before the panel
    driver becomes unavailable.
    
    The device_link is removed when drm_panel_detach() is called. The
    drm_panel_detach() should be called by the consumer DRM driver, not the
    panel driver, otherwise both drivers are racing to delete the same link.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/b53584fd988d045c13de22d81825395b0ae0aad7.1524727888.git.jsarha@ti.com

--- drivers/gpu/drm/drm_panel.c
+++ drivers/gpu/drm/drm_panel.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/module.h>
 
+#include <drm/drm_device.h>
 #include <drm/drm_crtc.h>
 #include <drm/drm_panel.h>
 
@@ -105,6 +106,13 @@ int drm_panel_attach(struct drm_panel *panel, struct drm_connector *connector)
 	if (panel->connector)
 		return -EBUSY;
 
+	panel->link = device_link_add(connector->dev->dev, panel->dev, 0);
+	if (!panel->link) {
+		dev_err(panel->dev, "failed to link panel to %s\n",
+			dev_name(connector->dev->dev));
+		return -EINVAL;
+	}
+
 	panel->connector = connector;
 	panel->drm = connector->dev;
 
@@ -126,6 +134,8 @@ EXPORT_SYMBOL(drm_panel_attach);
  */
 int drm_panel_detach(struct drm_panel *panel)
 {
+	device_link_del(panel->link);
+
 	panel->connector = NULL;
 	panel->drm = NULL;
 
