9496f12a122de08efca6cc244811b0e0ad052841
Jiri Olsa <jolsa@kernel.org>
2018-04-26 10:18:01 +0200

    tools, bpftool: Display license GPL compatible in prog show/list
    Display the license "gpl" string in bpftool prog command, like:
    
      # bpftool prog list
      5: tracepoint  name func  tag 57cd311f2e27366b  gpl
              loaded_at Apr 26/09:37  uid 0
              xlated 16B  not jited  memlock 4096B
    
      # bpftool --json --pretty prog show
      [{
              "id": 5,
              "type": "tracepoint",
              "name": "func",
              "tag": "57cd311f2e27366b",
              "gpl_compatible": true,
              "loaded_at": "Apr 26/09:37",
              "uid": 0,
              "bytes_xlated": 16,
              "jited": false,
              "bytes_memlock": 4096
          }
      ]
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/bpf/bpftool/prog.c
+++ tools/bpf/bpftool/prog.c
@@ -236,6 +236,8 @@ static void print_prog_json(struct bpf_prog_info *info, int fd)
 		     info->tag[0], info->tag[1], info->tag[2], info->tag[3],
 		     info->tag[4], info->tag[5], info->tag[6], info->tag[7]);
 
+	jsonw_bool_field(json_wtr, "gpl_compatible", info->gpl_compatible);
+
 	print_dev_json(info->ifindex, info->netns_dev, info->netns_ino);
 
 	if (info->load_time) {
@@ -296,6 +298,7 @@ static void print_prog_plain(struct bpf_prog_info *info, int fd)
 	printf("tag ");
 	fprint_hex(stdout, info->tag, BPF_TAG_SIZE, "");
 	print_dev_plain(info->ifindex, info->netns_dev, info->netns_ino);
+	printf("%s", info->gpl_compatible ? "  gpl" : "");
 	printf("\n");
 
 	if (info->load_time) {
