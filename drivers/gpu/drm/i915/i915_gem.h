69cbf297f9bb57aed902e5bc47e39eb443801bc5
Chris Wilson <chris@chris-wilson.co.uk>
2018-04-26 11:32:19 +0100

    drm/i915: Compile out engine debug for release
    The majority of the engine state dumping is too voluminous to be useful
    outside of a controlled setup, though a few do accompany severe errors.
    Keep the debug dumps next to the errors, but hide the others behind a CI
    compile flag. This becomes more useful when adding more dumps to latency
    sensitive paths.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180426103219.22181-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gem.h
+++ drivers/gpu/drm/i915/i915_gem.h
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct drm_i915_private;
 
 #ifdef CONFIG_DRM_I915_DEBUG_GEM
+
+#define GEM_SHOW_DEBUG() (drm_debug & DRM_UT_DRIVER)
+
 #define GEM_BUG_ON(condition) do { if (unlikely((condition))) {	\
 		pr_err("%s:%d GEM_BUG_ON(%s)\n", \
 		       __func__, __LINE__, __stringify(condition)); \
@@ -46,6 +49,9 @@ struct drm_i915_private;
 #define GEM_DEBUG_BUG_ON(expr) GEM_BUG_ON(expr)
 
 #else
+
+#define GEM_SHOW_DEBUG() (0)
+
 #define GEM_BUG_ON(expr) BUILD_BUG_ON_INVALID(expr)
 #define GEM_WARN_ON(expr) (BUILD_BUG_ON_INVALID(expr), 0)
 
