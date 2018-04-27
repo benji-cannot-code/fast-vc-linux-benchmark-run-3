3366359122798205a467620295a27be338b864a8
Kai Chieh Chuang <kaichieh.chuang@mediatek.com>
2018-04-27 09:54:44 +0800

    ASoC: mt6797: switch to SPDX license tag
    Signed-off-by: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
    Reviewed-by: Matthias Brugger <matthias.bgg@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt6797/mt6797-afe-pcm.c
+++ sound/soc/mediatek/mt6797/mt6797-afe-pcm.c
@@ -1,19 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/*
- * Mediatek ALSA SoC AFE platform driver for 6797
- *
- * Copyright (c) 2018 MediaTek Inc.
- * Author: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+// SPDX-License-Identifier: GPL-2.0
+//
+// Mediatek ALSA SoC AFE platform driver for 6797
+//
+// Copyright (c) 2018 MediaTek Inc.
+// Author: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
 
 #include <linux/delay.h>
 #include <linux/module.h>
