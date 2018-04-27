f9179ce0257154e062a809279bd435a554557627
Kai Chieh Chuang <kaichieh.chuang@mediatek.com>
2018-04-27 09:54:45 +0800

    ASoC: mt6351 switch to SPDX license tag
    Signed-off-by: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/mt6351.c
+++ sound/soc/codecs/mt6351.c
@@ -1,11 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
-/*
- * mt6351.c  --  mt6351 ALSA SoC audio codec driver
- *
- * Copyright (c) 2018 MediaTek Inc.
- * Author: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
- */
+//
+// mt6351.c  --  mt6351 ALSA SoC audio codec driver
+//
+// Copyright (c) 2018 MediaTek Inc.
+// Author: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
 
 #include <linux/dma-mapping.h>
 #include <linux/platform_device.h>
