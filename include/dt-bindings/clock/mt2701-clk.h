60ef563f239f9fa0f31840b41cf3e28892ff1459
Sean Wang <sean.wang@mediatek.com>
2018-04-27 16:14:44 +0800

    dt-bindings: clock: mediatek: add entry for Mali-450 node to refer
    Just add binding for a required clock referenced by Mali-450 on MT7623
    or MT2701 SoC.
    
    Cc: devicetree@vger.kernel.org
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/dt-bindings/clock/mt2701-clk.h
+++ include/dt-bindings/clock/mt2701-clk.h
@@ -432,6 +432,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLK_ETHSYS_CRYPTO			8
 #define CLK_ETHSYS_NR				9
 
+/* G3DSYS */
+#define CLK_G3DSYS_CORE				1
+#define CLK_G3DSYS_NR				2
+
 /* BDP */
 
 #define CLK_BDP_BRG_BA				1
