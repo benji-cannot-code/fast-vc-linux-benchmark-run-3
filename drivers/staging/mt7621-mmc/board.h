618dfd730f5b0d918b7bd4649381840ed6ca6175
Christian Lütke-Stetzkamp <christian@lkamp.de>
2018-04-29 19:32:16 +0200

    staging: mt7621-mmc: Remove unused field data_edge from msdc_hw
    The field data_edge of msdc_hw is set to a constant and only used once,
    replace that position with the constant and remove the unused field.
    
    Signed-off-by: Christian Lütke-Stetzkamp <christian@lkamp.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/board.h
+++ drivers/staging/mt7621-mmc/board.h
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct msdc_hw {
 	unsigned char  clk_src;          /* host clock source */
-	unsigned char  data_edge;        /* data latch edge */
 	unsigned char  clk_drv;          /* clock pad driving */
 	unsigned char  cmd_drv;          /* command pad driving */
 	unsigned char  dat_drv;          /* data pad driving */
