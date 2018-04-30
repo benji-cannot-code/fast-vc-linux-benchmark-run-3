199b975db4cdb1954db688bc0b364a256996cb7f
Wolfram Sang <wsa+renesas@sang-engineering.com>
2018-04-30 14:08:42 +0200

    i2c: mux: demux-pinctrl: disable PM user interface
    The demux device is only a logical device with no children. So, no
    RuntimePM is needed, let's disable the sysfs interface for it.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Peter Rosin <peda@axentia.se>

--- drivers/i2c/muxes/i2c-demux-pinctrl.c
+++ drivers/i2c/muxes/i2c-demux-pinctrl.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
 #include <linux/slab.h>
 #include <linux/sysfs.h>
 
@@ -254,6 +255,8 @@ static int i2c_demux_pinctrl_probe(struct platform_device *pdev)
 	priv->dev = &pdev->dev;
 
 	platform_set_drvdata(pdev, priv);
+
+	pm_runtime_no_callbacks(&pdev->dev);
 
 	/* switch to first parent as active master */
 	i2c_demux_activate_master(priv, 0);
