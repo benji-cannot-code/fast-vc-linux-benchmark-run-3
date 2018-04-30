83413de070bf4b08ecfceb07958e54bbfa893d59
Emre Ucan <eucan@de.adit-jv.com>
2018-04-30 14:02:04 +0200

    drm: rcar-du: Track dma-buf fences
    We have to check dma-buf reservation objects of our framebuffers before
    we use them. Otherwise, another driver might be writing on the same
    buffer which we are using. This would cause visible tearing effects
    on display.
    
    We can use existing atomic helper functions to solve this problem.
    
    Signed-off-by: Emre Ucan <eucan@de.adit-jv.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Daniel Vetter <daniel@ffwll.ch>
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/gpu/drm/rcar-du/rcar_du_vsp.c
+++ drivers/gpu/drm/rcar-du/rcar_du_vsp.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_fb_cma_helper.h>
 #include <drm/drm_gem_cma_helper.h>
+#include <drm/drm_gem_framebuffer_helper.h>
 #include <drm/drm_plane_helper.h>
 
 #include <linux/bitops.h>
@@ -237,6 +238,10 @@ static int rcar_du_vsp_plane_prepare_fb(struct drm_plane *plane,
 			goto fail;
 		}
 	}
+
+	ret = drm_gem_fb_prepare_fb(plane, state);
+	if (ret)
+		goto fail;
 
 	return 0;
 
