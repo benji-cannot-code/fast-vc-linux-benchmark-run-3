99957b5c26d61615376b9f9eccc7ca26aa1cc975
Mike Looijmans <mike.looijmans@topic.nl>
2018-05-01 13:42:12 +0200

    i2c: mux: pca954x: force reset on probe if available
    Instead of just hogging the reset GPIO into deactivated state, activate and
    then de-activate the reset. This allows for better recovery if the CPU was
    reset halfway through an I2C transaction for example.
    
    Signed-off-by: Mike Looijmans <mike.looijmans@topic.nl>
    Signed-off-by: Peter Rosin <peda@axentia.se>

--- drivers/i2c/muxes/i2c-mux-pca954x.c
+++ drivers/i2c/muxes/i2c-mux-pca954x.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/device.h>
+#include <linux/delay.h>
 #include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
 #include <linux/i2c-mux.h>
@@ -390,10 +391,16 @@ static int pca954x_probe(struct i2c_client *client,
 	i2c_set_clientdata(client, muxc);
 	data->client = client;
 
-	/* Get the mux out of reset if a reset GPIO is specified. */
-	gpio = devm_gpiod_get_optional(&client->dev, "reset", GPIOD_OUT_LOW);
+	/* Reset the mux if a reset GPIO is specified. */
+	gpio = devm_gpiod_get_optional(&client->dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(gpio))
 		return PTR_ERR(gpio);
+	if (gpio) {
+		udelay(1);
+		gpiod_set_value_cansleep(gpio, 0);
+		/* Give the chip some time to recover. */
+		udelay(1);
+	}
 
 	match = of_match_device(of_match_ptr(pca954x_of_match), &client->dev);
 	if (match)
