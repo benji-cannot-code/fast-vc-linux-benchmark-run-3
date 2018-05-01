3fed8f17507da4382f2a871092a9ab569ff74198
Yong Zhao <yong.zhao@amd.com>
2018-05-01 17:56:06 -0400

    drm/amdkfd: Separate trap handler assembly code and its hex values
    Since the assembly code is inside "#if 0", it is ineffective. Despite that,
    during debugging, we need to change the assembly code, extract it into
    a separate file and compile the new file into hex values using sp3.
    That process also requires us to remove "#if 0" and modify lines starting
    with "#", so that sp3 can successfully compile the new file.
    
    With this change, all the above chore is no longer needed, and
    cwsr_trap_handler_gfx*.asm can be directly used by sp3 to generate its
    hex values.
    
    Signed-off-by: Yong Zhao <yong.zhao@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@gmail.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device.c
@@ -27,8 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kfd_priv.h"
 #include "kfd_device_queue_manager.h"
 #include "kfd_pm4_headers_vi.h"
-#include "cwsr_trap_handler_gfx8.asm"
-#include "cwsr_trap_handler_gfx9.asm"
+#include "cwsr_trap_handler.h"
 #include "kfd_iommu.h"
 
 #define MQD_SIZE_ALIGNED 768
