66204ca5045d1f7daa6def484742d6f67cdbc224
Tim Collier <osdevtc@gmail.com>
2018-05-02 20:30:49 +0100

    staging: wlan-ng: add missing parameter name to prototype
    Fix checkpatch warning for missing parameter name for function
    prototype.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/hfa384x_usb.c
+++ drivers/staging/wlan-ng/hfa384x_usb.c
@@ -203,7 +203,7 @@ static void unlocked_usbctlx_complete(struct hfa384x *hw,
 				      struct hfa384x_usbctlx *ctlx);
 
 struct usbctlx_completor {
-	int (*complete)(struct usbctlx_completor *);
+	int (*complete)(struct usbctlx_completor *completor);
 };
 
 static int
