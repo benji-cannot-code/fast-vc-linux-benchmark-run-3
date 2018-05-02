671313d1faf48ea17d5d01f340f557cf4e73e5e1
Dave Chinner <dchinner@redhat.com>
2018-05-02 12:54:52 -0700

    iomap: iomap_dio_rw() handles all sync writes
    Currently iomap_dio_rw() only handles (data)sync write completions
    for AIO. This means we can't optimised non-AIO IO to minimise device
    flushes as we can't tell the caller whether a flush is required or
    not.
    
    To solve this problem and enable further optimisations, make
    iomap_dio_rw responsible for data sync behaviour for all IO, not
    just AIO.
    
    In doing so, the sync operation is now accounted as part of the DIO
    IO by inode_dio_end(), hence post-IO data stability updates will no
    long race against operations that serialise via inode_dio_wait()
    such as truncate or hole punch.
    
    Signed-Off-By: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -569,11 +569,6 @@ out:
 	 * complete fully or fail.
 	 */
 	ASSERT(ret < 0 || ret == count);
-
-	if (ret > 0) {
-		/* Handle various SYNC-type writes */
-		ret = generic_write_sync(iocb, ret);
-	}
 	return ret;
 }
 
