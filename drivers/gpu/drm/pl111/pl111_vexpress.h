4a3170305b5bcb3d1925257f3876f54d26a677bb
Linus Walleij <linus.walleij@linaro.org>
2018-05-02 15:47:18 +0200

    drm/pl111: Support the Versatile Express
    The Versatile Express uses a special configuration controller
    deeply embedded in the system motherboard FPGA to multiplex the
    two to three (!) display controller instances out to the single
    SiI9022 bridge.
    
    Set up an extra file with the logic to probe to the FPGA mux
    register on the system controller bus, then parse the device
    tree to see if there is a CLCD or HDLCD instance on the core
    tile (also known as the daughterboard) by looking in the
    root of the device tree for compatible nodes.
    
    - If there is a HDLCD on the core tile, and there is a driver
      for it, we exit probe and deactivate the motherboard CLCD.
      We do not touch the DVI mux in this case, to make sure we
      don't break HDLCD.
    
    - If there is a CLCD on both the motherboard and the core tile
      (only the CA9 has this) the core tile CLCD takes precedence
      and get muxed to the DVI connector.
    
    - Only if there is no working graphics on the core tile, the
      motherboard CLCD is probed and muxed to the DVI connector.
    
    Core tile graphics should always take precedence as it can
    address all memory and is also faster, however the motherboard
    CLCD is good to have around for diagnostics and testing.
    
    It is possible to test the motherboard CLCD by setting the
    status = "disabled" property on the core tile CLCD or
    HDLCD.
    
    Scale down the Versatile Express to 16BPP so we can support a
    1024x768 display despite the bus bandwidth restrictions on this
    platform. (The motherboard CLCD supports slightly lower
    resolution.)
    
    Cc: Liviu Dudau <liviu.dudau@arm.com>
    Cc: Pawel Moll <pawel.moll@arm.com>
    Acked-by: Eric Anholt <eric@anholt.net>
    Tested-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180502134719.8388-1-linus.walleij@linaro.org

---
+++ drivers/gpu/drm/pl111/pl111_vexpress.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+
+struct device;
+struct pl111_drm_dev_private;
+struct regmap;
+
+#ifdef CONFIG_ARCH_VEXPRESS
+
+int pl111_vexpress_clcd_init(struct device *dev,
+			     struct pl111_drm_dev_private *priv,
+			     struct regmap *map);
+
+#else
+
+static inline int pl111_vexpress_clcd_init(struct device *dev,
+					   struct pl111_drm_dev_private *priv,
+					   struct regmap *map)
+{
+	return -ENODEV;
+}
+
+#endif
