cd55ce80c1815ad8b3901e1eddd73d5d75d8cd4c
Dmitry Osipenko <digetx@gmail.com>
2018-05-04 01:55:34 +0300

    pinctrl: tegra20: Provide CDEV1/2 clock muxes
    Muxing of pins MCLK1/2 determine the muxing of the corresponding clocks.
    Make pinctrl driver to provide clock muxes for the CDEV1/2 pingroups, so
    that main clk-controller driver could get an actual parent clock for the
    CDEV1/2 clocks.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Reviewed-by: Marcel Ziswiler <marcel@ziswiler.com>
    Tested-by: Marcel Ziswiler <marcel@ziswiler.com>
    Tested-by: Marc Dietrich <marvin24@gmx.de>
    Acked-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/tegra/pinctrl-tegra.c
+++ drivers/pinctrl/tegra/pinctrl-tegra.c
@@ -34,17 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "../pinctrl-utils.h"
 #include "pinctrl-tegra.h"
 
-struct tegra_pmx {
-	struct device *dev;
-	struct pinctrl_dev *pctl;
-
-	const struct tegra_pinctrl_soc_data *soc;
-	const char **group_pins;
-
-	int nbanks;
-	void __iomem **regs;
-};
-
 static inline u32 pmx_readl(struct tegra_pmx *pmx, u32 bank, u32 reg)
 {
 	return readl(pmx->regs[bank] + reg);
