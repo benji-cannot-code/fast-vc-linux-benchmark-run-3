8aa755891d20044851993ffd923b90d827386f33
Mylène Josserand <mylene.josserand@bootlin.com>
2018-05-04 21:05:45 +0200

    ARM: shmobile: Convert file to use cntvoff
    Now that a common function is available for CNTVOFF's
    initialization, let's convert shmobile-apmu code to use
    this function.
    
    Signed-off-by: Mylène Josserand <mylene.josserand@bootlin.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>

--- arch/arm/mach-shmobile/setup-rcar-gen2.c
+++ arch/arm/mach-shmobile/setup-rcar-gen2.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_fdt.h>
 #include <linux/of_platform.h>
 #include <asm/mach/arch.h>
+#include <asm/secure_cntvoff.h>
 #include "common.h"
 #include "rcar-gen2.h"
 
@@ -71,7 +72,7 @@ void __init rcar_gen2_timer_init(void)
 	void __iomem *base;
 	u32 freq;
 
-	shmobile_init_cntvoff();
+	secure_cntvoff_init();
 
 	if (of_machine_is_compatible("renesas,r8a7745") ||
 	    of_machine_is_compatible("renesas,r8a7792") ||
