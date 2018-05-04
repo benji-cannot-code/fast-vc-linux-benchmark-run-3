6698e2660228143baa77cc08c6ce10a6629ba2ff
Arnaldo Carvalho de Melo <acme@redhat.com>
2018-05-04 15:59:16 -0300

    perf bpf: Add probe() helper to reduce kprobes boilerplate
    So that kprobe definitions become:
    
      int probe(function, variables)(void *ctx, int err, var1, var2, ...)
    
    The existing 5sec.c, got converted and goes from:
    
      SEC("func=hrtimer_nanosleep rqtp->tv_sec")
      int func(void *ctx, int err, long sec)
      {
      }
    
    To:
    
      int probe(hrtimer_nanosleep, rqtp->tv_sec)(void *ctx, int err, long sec)
      {
      }
    
    If we decide to add tv_nsec as well, then it becomes:
    
      $ cat tools/perf/examples/bpf/5sec.c
      #include <bpf.h>
    
      int probe(hrtimer_nanosleep, rqtp->tv_sec rqtp->tv_nsec)(void *ctx, int err, long sec, long nsec)
      {
    	  return sec == 5;
      }
    
      license(GPL);
      $
    
    And if we run it, system wide as before and run some 'sleep' with values
    for the tv_nsec field, we get:
    
      # perf trace --no-syscalls -e tools/perf/examples/bpf/5sec.c
         0.000 perf_bpf_probe:hrtimer_nanosleep:(ffffffff9811b5f0) tv_sec=5 tv_nsec=100000000
      9641.650 perf_bpf_probe:hrtimer_nanosleep:(ffffffff9811b5f0) tv_sec=5 tv_nsec=123450001
      ^C#
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: https://lkml.kernel.org/n/tip-1v9r8f6ds5av0w9pcwpeknyl@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/include/bpf/bpf.h
+++ tools/perf/include/bpf/bpf.h
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PERF_BPF_H
 #define SEC(NAME) __attribute__((section(NAME),  used))
 
+#define probe(function, vars) \
+	SEC(#function "=" #function " " #vars) function
+
 #define license(name) \
 char _license[] SEC("license") = #name; \
 int _version SEC("version") = LINUX_VERSION_CODE;
