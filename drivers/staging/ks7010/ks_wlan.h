a45bc8421a6e89f88b71e898f1735663d7dc6815
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2018-05-04 06:16:29 +0200

    staging: ks7010: change type for rsn_enabled in wpa_status struct
    Field rsn_enabled included in wpa_status struct is declared as
    unsigned int but it is only be set using 0 and 1 values and
    in conditional if code is just being used as a boolean. Change
    its type to be a boolean.
    
    Signed-off-by: Sergio Paracuellos <sergio.paracuellos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/ks7010/ks_wlan.h
+++ drivers/staging/ks7010/ks_wlan.h
@@ -339,7 +339,7 @@ struct mic_failure {
 
 struct wpa_status {
 	int wpa_enabled;
-	unsigned int rsn_enabled;
+	bool rsn_enabled;
 	int version;
 	int pairwise_suite;	/* unicast cipher */
 	int group_suite;	/* multicast cipher */
