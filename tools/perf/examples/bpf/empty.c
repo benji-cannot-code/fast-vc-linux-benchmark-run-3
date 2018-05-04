17145b0664637a15b8a1052042154034055abb4a
Arnaldo Carvalho de Melo <acme@redhat.com>
2018-05-04 12:23:29 -0300

    perf bpf: Add bpf.h to be used in eBPF proggies
    So, the first helper is the one shortening a variable/function section
    attribute, from, for instance:
    
      char _license[] __attribute__((section("license"), used)) = "GPL";
    
    to:
    
      char _license[] SEC("license") = "GPL";
    
    Convert empty.c to that and it becomes:
    
      # cat ~acme/lib/examples/perf/bpf/empty.c
      #include <bpf.h>
    
      char _license[] SEC("license") = "GPL";
      int _version SEC("version") = LINUX_VERSION_CODE;
      #
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: https://lkml.kernel.org/n/tip-zmeg52dlvy51rdlhyumfl5yf@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/examples/bpf/empty.c
+++ tools/perf/examples/bpf/empty.c
@@ -1,3 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-char _license[] __attribute__((section("license"), used)) = "GPL";
-int _version __attribute__((section("version"), used)) = LINUX_VERSION_CODE;
+#include <bpf.h>
+
+char _license[] SEC("license") = "GPL";
+int _version SEC("version") = LINUX_VERSION_CODE;
---
+++ tools/perf/include/bpf/bpf.h
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+#ifndef _PERF_BPF_H
+#define _PERF_BPF_H
+#define SEC(NAME) __attribute__((section(NAME),  used))
+#endif /* _PERF_BPF_H */
