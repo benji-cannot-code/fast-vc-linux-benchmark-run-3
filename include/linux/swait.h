4773ee340488f5bf1e268bdea5cdfaa3ecf05f6c
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2018-05-04 12:42:24 +0200

    sched/wait: Include <linux/wait.h> in <linux/swait.h>
    kbuild bot reported against an intermediate RT patch that the build
    fails with:
    
    >    In file included from include/linux/completion.h:12:0,
    >                     from include/linux/rcupdate_wait.h:10,
    >                     from kernel/rcu/srcutiny.c:27:
    >    kernel/rcu/srcutiny.c: In function 'srcu_drive_gp':
    > >> include/linux/swait.h:172:7: error: implicit declaration of function '___wait_is_interruptible'; did you mean '__swait_event_interruptible'?
    >       if (___wait_is_interruptible(state) && __int) {  \
    
    That error vanishes a few patches later (in the RT queue) because wait.h
    is then pulled in by other means. It does not seem to surface on !RT.
    I think that swait should include a header file for a function/macro
    (___wait_is_interruptible()) it is using.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Link: http://lkml.kernel.org/r/20180504104224.20218-1-bigeasy@linutronix.de
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/swait.h
+++ include/linux/swait.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/stddef.h>
 #include <linux/spinlock.h>
+#include <linux/wait.h>
 #include <asm/current.h>
 
 /*
