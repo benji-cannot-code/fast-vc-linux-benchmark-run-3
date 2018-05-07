c3011e5e3c62780e7c899a7eefbeb3853f9df261
Felipe Balbi <felipe.balbi@linux.intel.com>
2018-05-07 15:19:31 +0300

    usb: dwc3: gadget: init req->{direction,epnum} from alloc_request()
    We dont' need to touch req->direction or req->epnum from
    ep_queue(). It's enough that we initialize both fields from
    alloc_request() and just keep them for the entire lifetime of the
    request.
    
    No functional changes.
    
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -838,6 +838,7 @@ static struct usb_request *dwc3_gadget_ep_alloc_request(struct usb_ep *ep,
 	if (!req)
 		return NULL;
 
+	req->direction	= dep->direction;
 	req->epnum	= dep->number;
 	req->dep	= dep;
 
@@ -1286,8 +1287,6 @@ static int __dwc3_gadget_ep_queue(struct dwc3_ep *dep, struct dwc3_request *req)
 
 	req->request.actual	= 0;
 	req->request.status	= -EINPROGRESS;
-	req->direction		= dep->direction;
-	req->epnum		= dep->number;
 
 	trace_dwc3_ep_queue(req);
 
