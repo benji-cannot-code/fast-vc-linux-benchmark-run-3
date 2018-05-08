ec851b775fb357a24f54fce09419f1e1fe67c205
Ben Skeggs <bskeggs@redhat.com>
2018-05-08 20:39:46 +1000

    drm/nouveau/disp/nv50-: simplify definition of cursor channels
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/disp/rootnv50.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/disp/rootnv50.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NV50_DISP_ROOT_H__
 #define nv50_disp_root(p) container_of((p), struct nv50_disp_root, object)
 #include "nv50.h"
-#include "channv50.h"
 #include "dmacnv50.h"
 
 struct nv50_disp_root {
@@ -15,7 +14,6 @@ struct nv50_disp_root {
 
 struct nv50_disp_root_func {
 	const struct nv50_disp_dmac_oclass *dmac[1];
-	const struct nv50_disp_pioc_oclass *pioc[1];
 	struct nv50_disp_user {
 		struct nvkm_sclass base;
 		int (*ctor)(const struct nvkm_oclass *, void *argv, u32 argc,
