ee832033c0dbc88d5afe8ac95347b6ae4d6d0148
Christian Gromm <christian.gromm@microchip.com>
2018-05-08 11:45:04 +0200

    staging: most: sound: call snd_card_new with struct device
    This patch is needed as function snd_card_new needs a valid
    parent device. Passing a NULL pointer leads to kernel Ooops.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/core.h
+++ drivers/staging/most/core.h
@@ -231,6 +231,7 @@ struct mbo {
  */
 struct most_interface {
 	struct device dev;
+	struct device *driver_dev;
 	struct module *mod;
 	enum most_interface_type interface;
 	const char *description;
