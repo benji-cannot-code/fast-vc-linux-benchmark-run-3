b640587dc6d08d510711a050be8f43f1e8e8c3df
Ben Skeggs <bskeggs@redhat.com>
2018-05-08 20:39:46 +1000

    drm/nouveau/gr/gf100-: support clkgate_pack everywhere
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <subdev/fb.h>
 #include <subdev/mc.h>
 #include <subdev/pmu.h>
+#include <subdev/therm.h>
 #include <subdev/timer.h>
 #include <engine/fifo.h>
 
@@ -1949,6 +1950,9 @@ gf100_gr_init(struct gf100_gr *gr)
 
 	if (gr->func->init_r405a14)
 		gr->func->init_r405a14(gr);
+
+	if (gr->func->clkgate_pack)
+		nvkm_therm_clkgate_init(device->therm, gr->func->clkgate_pack);
 
 	nvkm_mask(device, TPC_UNIT(0, 0, 0x05c), 0x00000001, 0x00000001);
 
