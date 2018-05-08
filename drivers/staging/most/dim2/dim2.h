36a7efe47c4699ec4c6b5d5b8347e1768b3a6d65
Christian Gromm <christian.gromm@microchip.com>
2018-05-08 11:44:50 +0200

    staging: most: dim2: remove clock speed processing from the HDM
    This removes the module parameter clock_speed from the HDM code.
    
    Instead, the platform-dependent clock speed must be delivered by the
    platform driver with the help of the dim2_platform_data.clk_speed.
    
    Signed-off-by: Andrey Shvetsov <andrey.shvetsov@k2l.de>
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/dim2/dim2.h
+++ drivers/staging/most/dim2/dim2.h
@@ -9,14 +9,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DIM2_HDM_H
 #define	DIM2_HDM_H
 
+#include <linux/types.h>
+
 struct device;
 
 /* platform dependent data for dim2 interface */
 struct dim2_platform_data {
-	int (*init)(struct dim2_platform_data *pd, void __iomem *io_base,
-		    int clk_speed);
+	int (*init)(struct dim2_platform_data *pd, void __iomem *io_base);
 	void (*destroy)(struct dim2_platform_data *pd);
-	void *priv;
+	u8 clk_speed;
 };
 
 #endif	/* DIM2_HDM_H */
