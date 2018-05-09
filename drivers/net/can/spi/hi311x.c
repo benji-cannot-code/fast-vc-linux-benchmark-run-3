eb435b89ca188753d543be7affb19544e4dddbb6
Lukas Wunner <lukas@wunner.de>
2018-05-09 14:43:43 +0200

    can: hi311x: Work around TX complete interrupt erratum
    When sending packets as fast as possible using "cangen -g 0 -i -x", the
    HI-3110 occasionally latches the interrupt pin high on completion of a
    packet, but doesn't set the TXCPLT bit in the INTF register.  The INTF
    register contains 0x00 as if no interrupt has occurred.  Even waiting
    for a few milliseconds after the interrupt doesn't help.
    
    Work around this apparent erratum by instead checking the TXMTY bit in
    the STATF register ("TX FIFO empty").  We know that we've queued up a
    packet for transmission if priv->tx_len is nonzero.  If the TX FIFO is
    empty, transmission of that packet must have completed.
    
    Note that this is congruent with our handling of received packets, which
    likewise gleans from the STATF register whether a packet is waiting in
    the RX FIFO, instead of looking at the INTF register.
    
    Cc: Mathias Duckeck <m.duckeck@kunbus.de>
    Cc: Akshay Bhat <akshay.bhat@timesys.com>
    Cc: Casey Fitzpatrick <casey.fitzpatrick@timesys.com>
    Cc: stable@vger.kernel.org # v4.12+
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Acked-by: Akshay Bhat <akshay.bhat@timesys.com>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/hi311x.c
+++ drivers/net/can/spi/hi311x.c
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HI3110_STAT_BUSOFF BIT(2)
 #define HI3110_STAT_ERRP BIT(3)
 #define HI3110_STAT_ERRW BIT(4)
+#define HI3110_STAT_TXMTY BIT(7)
 
 #define HI3110_BTR0_SJW_SHIFT 6
 #define HI3110_BTR0_BRP_SHIFT 0
@@ -738,10 +739,7 @@ static irqreturn_t hi3110_can_ist(int irq, void *dev_id)
 			}
 		}
 
-		if (intf == 0)
-			break;
-
-		if (intf & HI3110_INT_TXCPLT) {
+		if (priv->tx_len && statf & HI3110_STAT_TXMTY) {
 			net->stats.tx_packets++;
 			net->stats.tx_bytes += priv->tx_len - 1;
 			can_led_event(net, CAN_LED_EVENT_TX);
@@ -751,6 +749,9 @@ static irqreturn_t hi3110_can_ist(int irq, void *dev_id)
 			}
 			netif_wake_queue(net);
 		}
+
+		if (intf == 0)
+			break;
 	}
 	mutex_unlock(&priv->hi3110_lock);
 	return IRQ_HANDLED;
