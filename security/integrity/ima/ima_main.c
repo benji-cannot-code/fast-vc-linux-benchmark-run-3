a6f9771ac5f5dc5e7a22c36d416d77e92f39d78d
Petr Vorel <pvorel@suse.cz>
2018-05-10 17:15:48 +0200

    ima: Remove unused variable ima_initialized
    Commit a756024 ("ima: added ima_policy_flag variable") replaced
    ima_initialized with ima_policy_flag, but didn't remove ima_initialized.
    This patch removes it.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: James Morris <james.morris@microsoft.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/ima/ima_main.c
+++ security/integrity/ima/ima_main.c
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ima.h"
 
-int ima_initialized;
-
 #ifdef CONFIG_IMA_APPRAISE
 int ima_appraise = IMA_APPRAISE_ENFORCE;
 #else
@@ -518,10 +516,9 @@ static int __init init_ima(void)
 		error = ima_init();
 	}
 
-	if (!error) {
-		ima_initialized = 1;
+	if (!error)
 		ima_update_policy_flag();
-	}
+
 	return error;
 }
 
