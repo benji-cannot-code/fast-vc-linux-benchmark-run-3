bf7f8f8ade00baa117bb655683f9a252d5221ac5
Brian Masney <masneyb@onstation.org>
2018-05-10 20:12:23 -0400

    staging: iio: tsl2x7x/tsl2772: move out of staging
    Move the tsl2772 driver out of staging and into mainline.
    
    Signed-off-by: Brian Masney <masneyb@onstation.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/light/tsl2772.c
+++ drivers/iio/light/tsl2772.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iio/events.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
-#include "tsl2772.h"
+#include <linux/platform_data/tsl2772.h>
 
 /* Cal defs */
 #define PROX_STAT_CAL			0
