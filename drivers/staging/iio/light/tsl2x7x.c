3438f3a8a0cd320e16900b5269868d7e92d6dcbd
Brian Masney <masneyb@onstation.org>
2018-05-10 20:12:21 -0400

    staging: iio: tsl2x7x: add range checking to tsl2x7x_write_raw
    The CALIBBIAS and INT_TIME masks in tsl2x7x_write_raw did not have any
    range checking in place so this patch adds the appropriate range
    checking. The defines TSL2X7X_ALS_GAIN_TRIM_{MIN,MAX} are also
    introduced by this patch.
    
    Signed-off-by: Brian Masney <masneyb@onstation.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/light/tsl2x7x.c
+++ drivers/staging/iio/light/tsl2x7x.c
@@ -104,6 +104,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TSL2X7X_CNTL_PROXPON_ENBL	0x0F
 #define TSL2X7X_CNTL_INTPROXPON_ENBL	0x2F
 
+#define TSL2X7X_ALS_GAIN_TRIM_MIN	250
+#define TSL2X7X_ALS_GAIN_TRIM_MAX	4000
+
 /* TAOS txx2x7x Device family members */
 enum {
 	tsl2571,
@@ -582,7 +585,7 @@ static int tsl2x7x_als_calibrate(struct iio_dev *indio_dev)
 
 	ret = (chip->settings.als_cal_target * chip->settings.als_gain_trim) /
 			lux_val;
-	if (ret < 250 || ret > 4000)
+	if (ret < TSL2X7X_ALS_GAIN_TRIM_MIN || ret > TSL2X7X_ALS_GAIN_TRIM_MAX)
 		return -ERANGE;
 
 	chip->settings.als_gain_trim = ret;
@@ -1210,9 +1213,17 @@ static int tsl2x7x_write_raw(struct iio_dev *indio_dev,
 		}
 		break;
 	case IIO_CHAN_INFO_CALIBBIAS:
+		if (val < TSL2X7X_ALS_GAIN_TRIM_MIN ||
+		    val > TSL2X7X_ALS_GAIN_TRIM_MAX)
+			return -EINVAL;
+
 		chip->settings.als_gain_trim = val;
 		break;
 	case IIO_CHAN_INFO_INT_TIME:
+		if (val != 0 || val2 < tsl2x7x_int_time_avail[chip->id][1] ||
+		    val2 > tsl2x7x_int_time_avail[chip->id][5])
+			return -EINVAL;
+
 		chip->settings.als_time = 256 -
 			(val2 / tsl2x7x_int_time_avail[chip->id][3]);
 		break;
