ba39480f489282ccb64a4ba033e187bde5e742da
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-05-11 12:07:02 +0100

    nvmem: core: describe add missing dev function parameter
    Document dev parameter which not described in devm_nvmem_unregister
    and devm_nvmem_register functions.
    
    Fix below warnings when kernel is compiled with W=1
    drivers/nvmem/core.c:579: warning: Function parameter or member
     'dev' not described in 'devm_nvmem_register'
    nvmem/core.c:615: warning: Function parameter or member 'dev'
     not described in 'devm_nvmem_unregister'
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/nvmem/core.c
+++ drivers/nvmem/core.c
@@ -570,6 +570,7 @@ static void devm_nvmem_release(struct device *dev, void *res)
  * nvmem_config.
  * Also creates an binary entry in /sys/bus/nvmem/devices/dev-name/nvmem
  *
+ * @dev: Device that uses the nvmem device.
  * @config: nvmem device configuration with which nvmem device is created.
  *
  * Return: Will be an ERR_PTR() on error or a valid pointer to nvmem_device
@@ -608,6 +609,7 @@ static int devm_nvmem_match(struct device *dev, void *res, void *data)
  * devm_nvmem_unregister() - Unregister previously registered managed nvmem
  * device.
  *
+ * @dev: Device that uses the nvmem device.
  * @nvmem: Pointer to previously registered nvmem device.
  *
  * Return: Will be an negative on error or a zero on success.
