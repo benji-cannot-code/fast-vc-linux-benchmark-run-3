37db071556e29b25c98f412448784dd64fd5a723
Vivek Goyal <vgoyal@redhat.com>
2018-05-11 11:49:33 -0400

    ovl: Do not do metadata only copy-up for truncate operation
    truncate should copy up full file (and not do metacopy only), otherwise it
    will be broken.  For example, use truncate to increase size of a file so
    that any read beyong existing size will return null bytes.  If we don't
    copy up full file, then we end up opening lower file and read from it only
    reads upto the old size (and not new size after truncate).  Hence to avoid
    such situations, copy up data as well when file size changes.
    
    So far it was being done by d_real(O_WRONLY) call in truncate() path.  Now
    that patch has been reverted.  So force full copy up in ovl_setattr() if
    size of file is changing.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/inode.c
+++ fs/overlayfs/inode.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int ovl_setattr(struct dentry *dentry, struct iattr *attr)
 {
 	int err;
+	bool full_copy_up = false;
 	struct dentry *upperdentry;
 	const struct cred *old_cred;
 
@@ -37,9 +38,15 @@ int ovl_setattr(struct dentry *dentry, struct iattr *attr)
 		err = -ETXTBSY;
 		if (atomic_read(&realinode->i_writecount) < 0)
 			goto out_drop_write;
+
+		/* Truncate should trigger data copy up as well */
+		full_copy_up = true;
 	}
 
-	err = ovl_copy_up(dentry);
+	if (!full_copy_up)
+		err = ovl_copy_up(dentry);
+	else
+		err = ovl_copy_up_with_data(dentry);
 	if (!err) {
 		struct inode *winode = NULL;
 
