9e3f755aa86fa12554f57fef60f877a7af1a1a61
Catalin Marinas <catalin.marinas@arm.com>
2018-05-11 13:25:49 +0100

    Revert "arm64: Increase the max granular size"
    This reverts commit 97303480753e48fb313dc0e15daaf11b0451cdb8.
    
    Commit 97303480753e ("arm64: Increase the max granular size") increased
    the cache line size to 128 to match Cavium ThunderX, apparently for some
    performance benefit which could not be confirmed. This change, however,
    has an impact on the network packet allocation in certain circumstances,
    requiring slightly over a 4K page with a significant performance
    degradation. The patch reverts L1_CACHE_SHIFT back to 6 (64-byte cache
    line).
    
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/cache.h
+++ arch/arm64/include/asm/cache.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ICACHE_POLICY_VIPT	2
 #define ICACHE_POLICY_PIPT	3
 
-#define L1_CACHE_SHIFT		7
+#define L1_CACHE_SHIFT		(6)
 #define L1_CACHE_BYTES		(1 << L1_CACHE_SHIFT)
 
 /*
