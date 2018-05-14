bd763d6628922ccfb002c4498665578d914db82b
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-05-14 09:27:38 +0300

    ALSA: xen-front: Read sound driver configuration from Xen store
    Read configuration values from Xen store according
    to xen/interface/io/sndif.h protocol:
      - introduce configuration structures for different
        components, e.g. sound card, device, stream
      - read PCM HW parameters, e.g rate, format etc.
      - detect stream type (capture/playback)
      - read device and card parameters
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/xen/xen_snd_front.h
+++ sound/xen/xen_snd_front.h
@@ -12,8 +12,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __XEN_SND_FRONT_H
 #define __XEN_SND_FRONT_H
 
+#include "xen_snd_front_cfg.h"
+
 struct xen_snd_front_info {
 	struct xenbus_device *xb_dev;
+
+	struct xen_front_cfg_card cfg;
 };
 
 #endif /* __XEN_SND_FRONT_H */
