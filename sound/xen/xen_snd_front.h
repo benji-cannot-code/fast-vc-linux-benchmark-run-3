dacd264f513999f9347239fb07e785df908d09f6
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-05-14 09:27:37 +0300

    ALSA: xen-front: Introduce Xen para-virtualized sound frontend driver
    Introduce skeleton of the para-virtualized Xen sound
    frontend driver.
    
    Initial handling for Xen bus states: implement
    Xen bus state machine for the frontend driver according to
    the state diagram and recovery flow from sound para-virtualized
    protocol: xen/interface/io/sndif.h.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

---
+++ sound/xen/xen_snd_front.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 OR MIT */
+
+/*
+ * Xen para-virtual sound device
+ *
+ * Copyright (C) 2016-2018 EPAM Systems Inc.
+ *
+ * Author: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
+ */
+
+#ifndef __XEN_SND_FRONT_H
+#define __XEN_SND_FRONT_H
+
+struct xen_snd_front_info {
+	struct xenbus_device *xb_dev;
+};
+
+#endif /* __XEN_SND_FRONT_H */
