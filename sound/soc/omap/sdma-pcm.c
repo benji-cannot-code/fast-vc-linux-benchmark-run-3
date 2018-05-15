7bb6f51ee056c25006238255c4d78da5bf854cf2
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-05-15 09:43:34 +0300

    ASoC: omap: sdma-pcm: Fix modpost warning
    WARNING: modpost: missing MODULE_LICENSE() in sound/soc/omap/snd-soc-sdma.o
    see include/linux/module.h for more information
    WARNING: modpost: missing MODULE_LICENSE() in sound/soc/omap/snd-soc-sdma.o
    see include/linux/module.h for more information
    
    Add the missing MODULE_LICENSE.
    
    This patch also going to solve:
    snd_soc_sdma: Unknown symbol devm_kmalloc (err 0)
    snd_soc_sdma: Unknown symbol omap_dma_filter_fn (err 0)
    snd_soc_sdma: Unknown symbol snd_dmaengine_pcm_prepare_slave_config (err 0)
    snd_soc_sdma: Unknown symbol devm_snd_dmaengine_pcm_register (err 0)
    
    Reported-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/omap/sdma-pcm.c
+++ sound/soc/omap/sdma-pcm.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Author: Peter Ujfalusi <peter.ujfalusi@ti.com>
  */
 
+#include <linux/module.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
@@ -67,3 +68,7 @@ int sdma_pcm_platform_register(struct device *dev,
 	return devm_snd_dmaengine_pcm_register(dev, config, flags);
 }
 EXPORT_SYMBOL_GPL(sdma_pcm_platform_register);
+
+MODULE_AUTHOR("Peter Ujfalusi <peter.ujfalusi@ti.com>");
+MODULE_DESCRIPTION("sDMA PCM ASoC platform driver");
+MODULE_LICENSE("GPL v2");
