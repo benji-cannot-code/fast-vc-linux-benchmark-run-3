ce6b1dc4d108e9c0c9f95a42653e6262a9e01bd4
Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
2018-05-15 12:41:10 +0200

    video: fbdev: via: allow COMPILE_TEST build
    This patch allows viafb driver to be build on !X86 archs
    using COMPILE_TEST config option.
    
    Since via-camera driver (VIDEO_VIA_CAMERA) depends on viafb
    it also needs a little fixup.
    
    Cc: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Reviewed-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- drivers/video/fbdev/via/via-core.c
+++ drivers/video/fbdev/via/via-core.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/list.h>
 #include <linux/pm.h>
-#include <asm/olpc.h>
 
 /*
  * The default port config.
--- drivers/video/fbdev/via/viafbdev.c
+++ drivers/video/fbdev/via/viafbdev.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/stat.h>
 #include <linux/via-core.h>
 #include <linux/via_i2c.h>
-#include <asm/olpc.h>
 
 #define _MASTER_FILE
 #include "global.h"
