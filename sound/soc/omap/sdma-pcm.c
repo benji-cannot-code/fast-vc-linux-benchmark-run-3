de0b5b4b60fdbe7e18893073f8a7f98fbf75c00f
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-05-15 09:43:35 +0300

    ASoC: omap: sdma-pcm: Correction for the include files
    The sdma-pcm does not need any information from omap-dma.h, it only needs
    to include the omap-dmaengine.h - for the omap_dma_filter_fn, but that
    might not be needed at all as OMAP1 was converted to dma_slave_map, but
    I can not test OMAP1.
    
    Add the linux/device.h include as well for devm_kzalloc()
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/omap/sdma-pcm.c
+++ sound/soc/omap/sdma-pcm.c
@@ -5,13 +5,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Author: Peter Ujfalusi <peter.ujfalusi@ti.com>
  */
 
+#include <linux/device.h>
 #include <linux/module.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
 #include <sound/dmaengine_pcm.h>
-#include <linux/omap-dma.h>
+#include <linux/omap-dmaengine.h>
 
 #include "sdma-pcm.h"
 
