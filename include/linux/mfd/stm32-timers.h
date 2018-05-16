5a81be4d07a2cfb8f5b71f5c83abce91e73b2e97
Fabrice Gasnier <fabrice.gasnier@st.com>
2018-05-16 09:36:00 +0200

    pwm: stm32: Use input prescaler to improve period capture
    Using input prescaler, capture unit will trigger DMA once every
    configurable /2, /4 or /8 events (rising edge). This helps improve
    period (only) capture accuracy at high rates.
    
    Signed-off-by: Fabrice Gasnier <fabrice.gasnier@st.com>
    Reviewed-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    Acked-by: Thierry Reding <thierry.reding@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/stm32-timers.h
+++ include/linux/mfd/stm32-timers.h
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TIM_DCR_DBL	GENMASK(12, 8)	/* DMA burst len */
 
 #define MAX_TIM_PSC		0xFFFF
+#define MAX_TIM_ICPSC		0x3
 #define TIM_CR2_MMS_SHIFT	4
 #define TIM_CR2_MMS2_SHIFT	20
 #define TIM_SMCR_TS_SHIFT	4
