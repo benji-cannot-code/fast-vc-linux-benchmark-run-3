21f02fe4770581c5c6816e9643bfe4adabf0d426
Stephen Rothwell <sfr@canb.auug.org.au>
2018-05-16 16:43:34 +1000

    drm/amdgpu: include pagemap.h for release_pages()
    Fixes: 5ae0283e831a ("drm/amdgpu: Add userptr support for KFD"
    Cc: Felix Kuehling <Felix.Kuehling@amd.com>
    Cc: Oded Gabbay <oded.gabbay@gmail.com>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) "kfd2kgd: " fmt
 
 #include <linux/list.h>
+#include <linux/pagemap.h>
 #include <linux/sched/mm.h>
 #include <drm/drmP.h>
 #include "amdgpu_object.h"
