f675515c0ec9b7069b1f8d54df04f0603a0b7040
Alexandre Belloni <alexandre.belloni@bootlin.com>
2018-05-17 21:23:05 +0200

    net: mscc: Add SPDX identifier
    ocelot_qsys.h is missing the SPDX identfier, fix that.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Reviewed-by: Allan W. Nielsen <allan.nielsen@microsemi.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mscc/ocelot_qsys.h
+++ drivers/net/ethernet/mscc/ocelot_qsys.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
 /*
  * Microsemi Ocelot Switch driver
  *
- * License: Dual MIT/GPL
  * Copyright (c) 2017 Microsemi Corporation
  */
 
