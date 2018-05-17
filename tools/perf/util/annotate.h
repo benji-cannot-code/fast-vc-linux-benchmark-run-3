943698fe2315b03e3d42bb849aa40e9119a3ea50
Jin Yao <yao.jin@linux.intel.com>
2018-05-17 22:58:37 +0800

    perf annotate: Record the min/max cycles
    Currently perf has a feature to account cycles for LBRs
    
    For example, on skylake:
    
      perf record -b ...
      perf report or perf annotate
    
    And then browsing the annotate browser gives average cycle counts for
    program blocks.
    
    For some analysis it would be useful if we could know not only the
    average cycles but also the min and max cycles.
    
    This patch records the min and max cycles.
    
    Signed-off-by: Jin Yao <yao.jin@linux.intel.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1526569118-14217-2-git-send-email-yao.jin@linux.intel.com
    [ Switch from max/min to min/max ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/annotate.h
+++ tools/perf/util/annotate.h
@@ -106,6 +106,8 @@ struct annotation_line {
 	int			 jump_sources;
 	float			 ipc;
 	u64			 cycles;
+	u64			 cycles_max;
+	u64			 cycles_min;
 	size_t			 privsize;
 	char			*path;
 	u32			 idx;
@@ -187,6 +189,8 @@ struct cyc_hist {
 	u64	start;
 	u64	cycles;
 	u64	cycles_aggr;
+	u64	cycles_max;
+	u64	cycles_min;
 	u32	num;
 	u32	num_aggr;
 	u8	have_start;
