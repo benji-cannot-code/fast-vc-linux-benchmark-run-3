e6daed54625035757e830ad5ada2bd535481e37a
Marco Felsch <m.felsch@pengutronix.de>
2018-05-18 11:55:06 +0200

    ASoC: ssm2305: fix header layout
    Make C-header and SPDX-License-Identifier header uniform.
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/ssm2305.c
+++ sound/soc/codecs/ssm2305.c
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
-/*
- * Analog Devices SSM2305 Amplifier Driver
- *
- * Copyright (C) 2018 Pengutronix, Marco Felsch <kernel@pengutronix.de>
- */
+//
+// Analog Devices SSM2305 Amplifier Driver
+//
+// Copyright (C) 2018 Pengutronix, Marco Felsch <kernel@pengutronix.de>
+//
 
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
