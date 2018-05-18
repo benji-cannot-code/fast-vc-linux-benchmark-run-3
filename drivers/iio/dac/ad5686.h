506de88fdecd114821f5e46b4115fea975c119b6
Stefan Popa <stefan.popa@analog.com>
2018-05-18 18:23:34 +0300

    iio:dac:ad5686: Add AD5681R/AD5682R/AD5683/AD5683R support
    The AD5681R/AD5682R/AD5683/AD5683R are a family of one channel DACs with
    12-bit, 14-bit and 16-bit precision respectively. The devices have either
    no built-in reference, or built-in 2.5V reference.
    
    These devices are similar to AD5691R/AD5692R/AD5693/AD5693R except
    with a few notable differences:
     * they use the SPI interface instead of I2C
     * in the write control register, DB18 and DB17 are used for setting the
       power mode, while DB16 is the REF bit. This is why a new regmap type
       was defined and checked accordingly.
     * the shift register is 24 bits wide, the first four bits are the command
       bits followed by the data bits. As the data comprises of 20-bit, 18-bit
       or 16-bit input code, this means that 4 LSB bits are don't care. This is
       why the data needs to be shifted on the left with four bits. Therefore,
       AD5683_REGMAP is checked inside a switch case in the ad5686_spi_write()
       function. On the other hand, similar devices such as AD5693R family,
       have the 4 MSB command bits followed by 4 don't care bits.
    
    Datasheet:
    http://www.analog.com/media/en/technical-documentation/data-sheets/AD5683R_5682R_5681R_5683.pdf
    
    Signed-off-by: Stefan Popa <stefan.popa@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ad5686.h
+++ drivers/iio/dac/ad5686.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/kernel.h>
 
+#define AD5683_DATA(x)				((x) << 4)
 #define AD5686_ADDR(x)				((x) << 16)
 #define AD5686_CMD(x)				((x) << 20)
 
@@ -37,6 +38,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AD5686_LDAC_PWRDN_3STATE		0x3
 
 #define AD5686_CMD_CONTROL_REG			0x4
+#define AD5686_CMD_READBACK_ENABLE_V2		0x5
+#define AD5683_REF_BIT_MSK			BIT(12)
 #define AD5693_REF_BIT_MSK			BIT(12)
 
 /**
@@ -48,6 +51,10 @@ enum ad5686_supported_device_ids {
 	ID_AD5675R,
 	ID_AD5676,
 	ID_AD5676R,
+	ID_AD5681R,
+	ID_AD5682R,
+	ID_AD5683,
+	ID_AD5683R,
 	ID_AD5684,
 	ID_AD5684R,
 	ID_AD5685R,
@@ -65,6 +72,7 @@ enum ad5686_supported_device_ids {
 };
 
 enum ad5686_regmap_type {
+	AD5683_REGMAP,
 	AD5686_REGMAP,
 	AD5693_REGMAP
 };
