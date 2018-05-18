bb1a771ca67e72c20cd64747db852f53f575edec
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-05-18 13:56:01 +0100

    ASoC: qdsp6: q6asm: Add q6asm driver
    This patch adds basic support to Q6 ASM (Audio Stream Manager) module on
    Q6DSP. ASM supports up to 8 concurrent streams. each stream can be setup
    as playback/capture. ASM provides top control functions like
    Pause/flush/resume for playback and record. ASM can Create/destroy encoder,
    decoder and also provides POPP dynamic services.
    
    This patch adds support to basic features to allow hdmi playback.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Reviewed-and-tested-by: Rohit kumar <rohitkr@codeaurora.org>
    Reviewed-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

---
+++ sound/soc/qcom/qdsp6/q6asm.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __Q6_ASM_H__
+#define __Q6_ASM_H__
+
+#define MAX_SESSIONS	8
+
+typedef void (*q6asm_cb) (uint32_t opcode, uint32_t token,
+			  void *payload, void *priv);
+struct audio_client;
+struct audio_client *q6asm_audio_client_alloc(struct device *dev,
+					      q6asm_cb cb, void *priv,
+					      int session_id, int perf_mode);
+void q6asm_audio_client_free(struct audio_client *ac);
+int q6asm_get_session_id(struct audio_client *ac);
+#endif /* __Q6_ASM_H__ */
