87a91d6a5dea9fc0126c58c7e745153a33956b77
Thomas Gleixner <tglx@linutronix.de>
2018-05-19 16:38:59 +0200

    x86/Hyper-V/hv_apic: Include asm/apic.h
    Not all configurations magically include asm/apic.h, but the Hyper-V code
    requires it. Include it explicitely.
    
    Fixes: 6b48cb5f8347 ("X86/Hyper-V: Enlighten APIC access")
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Michael Kelley <mikelley@microsoft.com>

--- arch/x86/hyperv/hv_apic.c
+++ arch/x86/hyperv/hv_apic.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpuhotplug.h>
 #include <asm/hypervisor.h>
 #include <asm/mshyperv.h>
+#include <asm/apic.h>
 
 #ifdef CONFIG_X86_64
 #if IS_ENABLED(CONFIG_HYPERV)
