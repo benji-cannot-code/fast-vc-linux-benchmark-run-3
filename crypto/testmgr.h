1639f8916fc76a4c2c7b0eac9d9a9cf08eb70be2
Eric Biggers <ebiggers@google.com>
2018-05-19 22:07:42 -0700

    crypto: testmgr - add more unkeyed crc32 and crc32c test vectors
    crc32c has an unkeyed test vector but crc32 did not.  Add the crc32c one
    (which uses an empty input) to crc32 too, and also add a new one to both
    that uses a nonempty input.  These test vectors verify that crc32 and
    crc32c implementations use the correct default initial state.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/testmgr.h
+++ crypto/testmgr.h
@@ -42294,6 +42294,15 @@ static const struct hash_testvec michael_mic_tv_template[] = {
  */
 static const struct hash_testvec crc32_tv_template[] = {
 	{
+		.psize = 0,
+		.digest = "\x00\x00\x00\x00",
+	},
+	{
+		.plaintext = "abcdefg",
+		.psize = 7,
+		.digest = "\xd8\xb5\x46\xac",
+	},
+	{
 		.key = "\x87\xa9\xcb\xed",
 		.ksize = 4,
 		.psize = 0,
@@ -42728,6 +42737,11 @@ static const struct hash_testvec crc32c_tv_template[] = {
 	{
 		.psize = 0,
 		.digest = "\x00\x00\x00\x00",
+	},
+	{
+		.plaintext = "abcdefg",
+		.psize = 7,
+		.digest = "\x41\xf4\x27\xe6",
 	},
 	{
 		.key = "\x87\xa9\xcb\xed",
