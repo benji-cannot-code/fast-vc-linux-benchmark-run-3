b7cafb0ae7619e585f2df3d1c7f150d3294126e5
NeilBrown <neilb@suse.com>
2018-05-21 14:35:13 +1000

    staging: lustre: remove libcfs_all.h from includes lustre/lnet
    Some of these include files didn't need to
    include libcfs_all.h.  A couple did so replace with just the
    base include files needed.
    
    Signed-off-by: NeilBrown <neilb@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/libcfs/tracefile.h
+++ drivers/staging/lustre/lnet/libcfs/tracefile.h
@@ -35,7 +35,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LIBCFS_TRACEFILE_H__
 #define __LIBCFS_TRACEFILE_H__
 
-#include <linux/libcfs/libcfs_all.h>
+#include <linux/spinlock.h>
+#include <linux/list.h>
+#include <linux/cache.h>
+#include <linux/threads.h>
+#include <linux/limits.h>
+#include <linux/smp.h>
+#include <linux/libcfs/libcfs.h>
 
 enum cfs_trace_buf_type {
 	CFS_TCD_TYPE_PROC = 0,
