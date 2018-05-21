58d94acb5b7b75ecea4bbad2a080ff7224590db6
NeilBrown <neilb@suse.com>
2018-05-21 14:35:13 +1000

    staging: lustre: remove libcfs_all.h includes from lnet/klnd
    Other smaller include files are sufficient.
    
    Signed-off-by: NeilBrown <neilb@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/klnds/socklnd/socklnd.h
+++ drivers/staging/lustre/lnet/klnds/socklnd/socklnd.h
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <net/tcp.h>
 
-#include <linux/libcfs/libcfs_all.h>
 #include <linux/lnet/lib-lnet.h>
 #include <linux/lnet/socklnd.h>
 
--- drivers/staging/lustre/lnet/klnds/socklnd/socklnd_lib.c
+++ drivers/staging/lustre/lnet/klnds/socklnd/socklnd_lib.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Lustre is a trademark of Sun Microsystems, Inc.
  */
 
+#include <linux/highmem.h>
 #include "socklnd.h"
 
 int
