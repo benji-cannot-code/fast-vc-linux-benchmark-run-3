34ff8c5f70a0807c601ace13c4fc8132d419d614
Björn Töpel <bjorn.topel@intel.com>
2018-05-22 09:34:59 +0200

    xsk: remove explicit ring structure from uapi
    In this commit we remove the explicit ring structure from the the
    uapi. It is tricky for an uapi to depend on a certain L1 cache line
    size, since it can differ for variants of the same architecture. Now,
    we let the user application determine the offsets of the producer,
    consumer and descriptors by asking the socket via getsockopt.
    
    A typical flow would be (Rx ring):
    
      struct xdp_mmap_offsets off;
      struct xdp_desc *ring;
      u32 *prod, *cons;
      void *map;
      ...
    
      getsockopt(fd, SOL_XDP, XDP_MMAP_OFFSETS, &off, &optlen);
    
      map = mmap(NULL, off.rx.desc +
    		   NUM_DESCS * sizeof(struct xdp_desc),
    		   PROT_READ | PROT_WRITE,
    		   MAP_SHARED | MAP_POPULATE, sfd,
    		   XDP_PGOFF_RX_RING);
      prod = map + off.rx.producer;
      cons = map + off.rx.consumer;
      ring = map + off.rx.desc;
    
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/xdp/xsk_queue.h
+++ net/xdp/xsk_queue.h
@@ -14,6 +14,23 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define RX_BATCH_SIZE 16
 
+struct xdp_ring {
+	u32 producer ____cacheline_aligned_in_smp;
+	u32 consumer ____cacheline_aligned_in_smp;
+};
+
+/* Used for the RX and TX queues for packets */
+struct xdp_rxtx_ring {
+	struct xdp_ring ptrs;
+	struct xdp_desc desc[0] ____cacheline_aligned_in_smp;
+};
+
+/* Used for the fill and completion queues for buffers */
+struct xdp_umem_ring {
+	struct xdp_ring ptrs;
+	u32 desc[0] ____cacheline_aligned_in_smp;
+};
+
 struct xsk_queue {
 	struct xdp_umem_props umem_props;
 	u32 ring_mask;
