8c01e6a8c8859893d2f2782b73e4a9b84887fc47
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2018-05-22 17:30:31 -0700

    Input: psmouse-smbus - allow to control psmouse_deactivate
    This seems to be Synaptics specific, as some Elan touchpads are not
    correctly switching to SMBus if we call deactivate before switching to
    SMBus on cold boot and on resume.
    
    Tested with the T480s
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Acked-by: KT Liao <kt.liao@emc.com.tw>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/mouse/psmouse-smbus.c
+++ drivers/input/mouse/psmouse-smbus.c
@@ -24,6 +24,7 @@ struct psmouse_smbus_dev {
 	struct i2c_client *client;
 	struct list_head node;
 	bool dead;
+	bool need_deactivate;
 };
 
 static LIST_HEAD(psmouse_smbus_list);
@@ -119,7 +120,10 @@ static psmouse_ret_t psmouse_smbus_process_byte(struct psmouse *psmouse)
 
 static int psmouse_smbus_reconnect(struct psmouse *psmouse)
 {
-	psmouse_deactivate(psmouse);
+	struct psmouse_smbus_dev *smbdev = psmouse->private;
+
+	if (smbdev->need_deactivate)
+		psmouse_deactivate(psmouse);
 
 	return 0;
 }
@@ -226,6 +230,7 @@ void psmouse_smbus_cleanup(struct psmouse *psmouse)
 int psmouse_smbus_init(struct psmouse *psmouse,
 		       const struct i2c_board_info *board,
 		       const void *pdata, size_t pdata_size,
+		       bool need_deactivate,
 		       bool leave_breadcrumbs)
 {
 	struct psmouse_smbus_dev *smbdev;
@@ -237,6 +242,7 @@ int psmouse_smbus_init(struct psmouse *psmouse,
 
 	smbdev->psmouse = psmouse;
 	smbdev->board = *board;
+	smbdev->need_deactivate = need_deactivate;
 
 	if (pdata) {
 		smbdev->board.platform_data = kmemdup(pdata, pdata_size,
@@ -247,14 +253,15 @@ int psmouse_smbus_init(struct psmouse *psmouse,
 		}
 	}
 
+	if (need_deactivate)
+		psmouse_deactivate(psmouse);
+
 	psmouse->private = smbdev;
 	psmouse->protocol_handler = psmouse_smbus_process_byte;
 	psmouse->reconnect = psmouse_smbus_reconnect;
 	psmouse->fast_reconnect = psmouse_smbus_reconnect;
 	psmouse->disconnect = psmouse_smbus_disconnect;
 	psmouse->resync_time = 0;
-
-	psmouse_deactivate(psmouse);
 
 	mutex_lock(&psmouse_smbus_mutex);
 	list_add_tail(&smbdev->node, &psmouse_smbus_list);
