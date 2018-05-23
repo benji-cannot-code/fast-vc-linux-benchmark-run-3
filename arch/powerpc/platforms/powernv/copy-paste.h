21d85c2573e794e2624a146540ca1639ee3f6f38
Simon Guo <wei.guo.simon@gmail.com>
2018-05-23 15:01:45 +0800

    powerpc/reg: Add TEXASR related macros
    This patches add some macros for CR0/TEXASR bits so that PR KVM TM
    logic (tbegin./treclaim./tabort.) can make use of them later.
    
    Signed-off-by: Simon Guo <wei.guo.simon@gmail.com>
    Reviewed-by: Paul Mackerras <paulus@ozlabs.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/copy-paste.h
+++ arch/powerpc/platforms/powernv/copy-paste.h
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 2 of the License, or (at your option) any later version.
  */
 #include <asm/ppc-opcode.h>
+#include <asm/reg.h>
 
-#define CR0_SHIFT	28
-#define CR0_MASK	0xF
 /*
  * Copy/paste instructions:
  *
