b137263272836528e25074fd80c8d4406cddda1e
Eric Long <eric.long@spreadtrum.com>
2018-05-23 17:31:11 +0800

    dmaengine: sprd: Add Spreadtrum DMA configuration
    This patch adds the 'device_config' and 'device_prep_slave_sg' interfaces
    for users to configure DMA, as well as adding one 'struct sprd_dma_config'
    structure to save Spreadtrum DMA configuration for each DMA channel.
    
    Signed-off-by: Eric Long <eric.long@spreadtrum.com>
    Signed-off-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- include/linux/dma/sprd-dma.h
+++ include/linux/dma/sprd-dma.h
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SPRD_DMA_H_
 #define _SPRD_DMA_H_
 
+#define SPRD_DMA_REQ_SHIFT 16
+#define SPRD_DMA_FLAGS(req_mode, int_type) \
+	((req_mode) << SPRD_DMA_REQ_SHIFT | (int_type))
+
 /*
  * enum sprd_dma_req_mode: define the DMA request mode
  * @SPRD_DMA_FRAG_REQ: fragment request mode
