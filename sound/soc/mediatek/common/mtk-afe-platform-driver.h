20d5ab58234aa14fb7a8e895e7ca1fa4e4b887dc
KaiChieh Chuang <kaichieh.chuang@mediatek.com>
2018-05-25 11:48:16 +0800

    ASoC: mediatek: add sub dai to mtk_base_afe
    In MediaTek SoC chip we have multiple DAI,
    such as I2S, ADDA, PCM, etc.
    
    Organize each DAI in to one sub dai,
    with its dai driver, controls, widgets, routes.
    
    add mtk_afe_combine_sub_dai() to combine
    dai driver from each DAI.
    
    add mtk_afe_add_sub_dai_control() to register
    the control, widget, routes to component.
    
    Signed-off-by: KaiChieh Chuang <kaichieh.chuang@mediatek.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/common/mtk-afe-platform-driver.h
+++ sound/soc/mediatek/common/mtk-afe-platform-driver.h
@@ -13,5 +13,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AFE_PCM_NAME "mtk-afe-pcm"
 extern const struct snd_soc_component_driver mtk_afe_pcm_platform;
 
+struct mtk_base_afe;
+struct snd_soc_component;
+
+int mtk_afe_combine_sub_dai(struct mtk_base_afe *afe);
+int mtk_afe_add_sub_dai_control(struct snd_soc_component *component);
 #endif
 
