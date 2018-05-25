58af63a8fbcdea0b3878b9870fc1918c2d04b880
Arnd Bergmann <arnd@arndb.de>
2018-05-25 11:25:09 -0400

    media: v4l: cadence: include linux/slab.h
    I ran into a randconfig build error with the new driver:
    
    drivers/media/platform/cadence/cdns-csi2tx.c: In function 'csi2tx_probe':
    drivers/media/platform/cadence/cdns-csi2tx.c:477:11: error: implicit declaration of function 'kzalloc'; did you mean 'd_alloc'? [-Werror=implicit-function-declaration]
    
    kzalloc() is declared in linux/slab.h, so let's include this to make it
    build in all configurations.
    
    Fixes: 84b477e6d4bc ("media: v4l: cadence: Add Cadence MIPI-CSI2 TX driver")
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/cadence/cdns-csi2tx.c
+++ drivers/media/platform/cadence/cdns-csi2tx.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_graph.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
 
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-device.h>
