87d7891a6eadf1fa44ab58a7e0a209d6526a18dc
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2018-05-26 08:57:25 +0200

    staging: lustre: fix build error in mdc_request.c
    Turns out we need some more .h files to build properly on all arches.
    Specifically prefetch.h for this file.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Fixes: 73d65c8d1a85 ("staging: lustre: remove libcfs_all.h from lustre/include/*.h")
    Cc: NeilBrown <neilb@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lustre/mdc/mdc_request.c
+++ drivers/staging/lustre/lustre/mdc/mdc_request.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # include <linux/utsname.h>
 # include <linux/file.h>
 # include <linux/kthread.h>
+#include <linux/prefetch.h>
 
 #include <lustre_errno.h>
 #include <cl_object.h>
