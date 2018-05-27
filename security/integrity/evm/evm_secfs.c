5d90be4009003ccf45706e93c9b2cdb0556a5cec
Colin Ian King <colin.king@canonical.com>
2018-05-27 23:15:02 +0100

    EVM: fix memory leak of temporary buffer 'temp'
    The allocation of 'temp' is not kfree'd and hence there is a memory
    leak on each call of evm_read_xattrs.  Fix this by kfree'ing it
    after copying data from it back to the user space buffer 'buf'.
    
    Detected by CoverityScan, CID#1469386 ("Resource Leak")
    
    Fixes: fa516b66a1bf ("EVM: Allow runtime modification of the set of verified xattrs")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>

--- security/integrity/evm/evm_secfs.c
+++ security/integrity/evm/evm_secfs.c
@@ -159,6 +159,8 @@ static ssize_t evm_read_xattrs(struct file *filp, char __user *buf,
 	mutex_unlock(&xattr_list_mutex);
 	rc = simple_read_from_buffer(buf, count, ppos, temp, strlen(temp));
 
+	kfree(temp);
+
 	return rc;
 }
 
