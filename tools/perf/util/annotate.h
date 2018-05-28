8f956b81001cd2190c0fb91f7702860d24bead10
Arnaldo Carvalho de Melo <acme@redhat.com>
2018-05-28 11:50:21 -0300

    perf annotate: Move disassembler_style global to annotation_options
    Continuing to group annotation specific stuff into a struct.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Link: https://lkml.kernel.org/n/tip-p3cdhltj58jt0byjzg3g7obx@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/annotate.h
+++ tools/perf/util/annotate.h
@@ -81,6 +81,7 @@ struct annotation_options {
 	int  min_pcnt;
 	int  max_lines;
 	int  context;
+	const char *disassembler_style;
 };
 
 enum {
@@ -368,8 +369,6 @@ static inline int symbol__tui_annotate(struct symbol *sym __maybe_unused,
 	return 0;
 }
 #endif
-
-extern const char	*disassembler_style;
 
 void annotation_config__init(void);
 
