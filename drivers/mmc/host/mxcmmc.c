e84876deeb55a1fb44b618e87c59a451e0ff16e1
Arnd Bergmann <arnd@arndb.de>
2018-05-29 12:30:35 +0200

    mmc: mxmmc: include linux/highmem.h
    The highmem conversion caused a build error in some configurations:
    
    drivers/mmc/host/mxcmmc.c: In function 'mxcmci_transfer_data':
    drivers/mmc/host/mxcmmc.c:622:10: error: implicit declaration of function 'kmap_atomic'; did you mean 'in_atomic'? [-Werror=implicit-function-declaration]
    
    This includes the correct header file.
    
    Fixes: b189e7589f6d ("mmc: mxcmmc: handle highmem pages")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/mxcmmc.c
+++ drivers/mmc/host/mxcmmc.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/platform_device.h>
+#include <linux/highmem.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
 #include <linux/blkdev.h>
