0e5489b53d79492f7a3267be37ccd9577614588e
Shuah Khan (Samsung OSG) <shuah@kernel.org>
2018-05-30 11:27:13 -0600

    selftests: lib: fix prime_numbers module search and skip logic
    prime_numbers modules search and skip logic removes the module instead
    of searching for it. Fix it.
    
    Signed-off-by: Shuah Khan (Samsung OSG) <shuah@kernel.org>

--- tools/testing/selftests/lib/prime_numbers.sh
+++ tools/testing/selftests/lib/prime_numbers.sh
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # Kselftest framework requirement - SKIP code is 4.
 ksft_skip=4
 
-if ! /sbin/modprobe -q -r prime_numbers; then
+if ! /sbin/modprobe -q -n prime_numbers; then
 	echo "prime_numbers: module prime_numbers is not found [SKIP]"
 	exit $ksft_skip
 fi
