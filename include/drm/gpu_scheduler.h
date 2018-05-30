58f4532cd452686ca770a338fdebd94296d159b4
Andrey Grodzovsky <andrey.grodzovsky@amd.com>
2018-05-30 15:11:01 -0400

    drm/scheduler: Avoid using wait_event_killable for dying process (V4)
    Dying process might be blocked from receiving any more signals
    so avoid using it.
    
    Also retire enity->fini_status and just check the SW queue,
    if it's not empty do the fallback cleanup.
    
    Also handle entity->last_scheduled == NULL use case which
    happens when HW ring is already hangged whem a  new entity
    tried to enqeue jobs.
    
    v2:
    Return the remaining timeout and use that as parameter for the next call.
    This way when we need to cleanup multiple queues we don't wait for the
    entire TO period for each queue but rather in total.
    Styling comments.
    Rebase.
    
    v3:
    Update types from unsigned to long.
    Work with jiffies instead of ms.
    Return 0 when TO expires.
    Rebase.
    
    v4:
    Remove unnecessary timeout calculation.
    
    Signed-off-by: Andrey Grodzovsky <andrey.grodzovsky@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- include/drm/gpu_scheduler.h
+++ include/drm/gpu_scheduler.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/spsc_queue.h>
 #include <linux/dma-fence.h>
 
+#define MAX_WAIT_SCHED_ENTITY_Q_EMPTY msecs_to_jiffies(1000)
+
 struct drm_gpu_scheduler;
 struct drm_sched_rq;
 
@@ -85,7 +87,6 @@ struct drm_sched_entity {
 	struct dma_fence		*dependency;
 	struct dma_fence_cb		cb;
 	atomic_t			*guilty;
-	int                             fini_status;
 	struct dma_fence                *last_scheduled;
 };
 
@@ -284,8 +285,8 @@ int drm_sched_entity_init(struct drm_gpu_scheduler *sched,
 			  struct drm_sched_entity *entity,
 			  struct drm_sched_rq *rq,
 			  atomic_t *guilty);
-void drm_sched_entity_do_release(struct drm_gpu_scheduler *sched,
-			   struct drm_sched_entity *entity);
+long drm_sched_entity_do_release(struct drm_gpu_scheduler *sched,
+			   struct drm_sched_entity *entity, long timeout);
 void drm_sched_entity_cleanup(struct drm_gpu_scheduler *sched,
 			   struct drm_sched_entity *entity);
 void drm_sched_entity_fini(struct drm_gpu_scheduler *sched,
