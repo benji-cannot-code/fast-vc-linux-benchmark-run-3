505431845f03a68ca15750a2148cb6630064dd9b
Arnd Bergmann <arnd@arndb.de>
2018-05-30 23:38:48 +0200

    staging: lustre: include linux/highmem.h when needed
    Something in recent linux-next kernels caused linux/highmem.h to
    no longer be included implicitly from o2iblnd_cb.c, causing a build
    failure:
    
    drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd_cb.c: In function 'kiblnd_kvaddr_to_page':
    drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd_cb.c:549:15: error: 'PKMAP_BASE' undeclared (first use in this function); did you mean 'RTM_BASE'?
      if (vaddr >= PKMAP_BASE &&
                   ^~~~~~~~~~
                   RTM_BASE
    drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd_cb.c:549:15: note: each undeclared identifier is reported only once for each function it appears in
    drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd_cb.c:550:28: error: 'LAST_PKMAP' undeclared (first use in this function); did you mean 'AT_HWCAP'?
          vaddr < (PKMAP_BASE + LAST_PKMAP * PAGE_SIZE)) {
                                ^~~~~~~~~~
                                AT_HWCAP
    
    This adds back an explicit include for the header.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: NeilBrown <neilb@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd_cb.c
+++ drivers/staging/lustre/lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Author: Eric Barton <eric@bartonsoftware.com>
  */
 
+#include <linux/highmem.h>
 #include "o2iblnd.h"
 
 #define MAX_CONN_RACES_BEFORE_ABORT 20
