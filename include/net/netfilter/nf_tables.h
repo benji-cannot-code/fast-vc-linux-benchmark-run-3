f98ed4c8935d9fc7f8f5f33103c17294f689405c
Pablo Neira Ayuso <pablo@netfilter.org>
2018-06-02 23:38:46 +0200

    netfilter: nf_tables: pass context to object destroy indirection
    The new connlimit object needs this to properly deal with conntrack
    dependencies.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -1071,7 +1071,8 @@ struct nft_object_ops {
 	int				(*init)(const struct nft_ctx *ctx,
 						const struct nlattr *const tb[],
 						struct nft_object *obj);
-	void				(*destroy)(struct nft_object *obj);
+	void				(*destroy)(const struct nft_ctx *ctx,
+						   struct nft_object *obj);
 	int				(*dump)(struct sk_buff *skb,
 						struct nft_object *obj,
 						bool reset);
