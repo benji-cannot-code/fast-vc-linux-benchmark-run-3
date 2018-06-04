e769188ba594980d34e7a7750cd3c4b953623df3
Björn Töpel <bjorn.topel@intel.com>
2018-06-04 14:05:51 +0200

    xsk: moved struct xdp_umem definition
    Moved struct xdp_umem to xdp_sock.h, in order to prepare for zero-copy
    support.
    
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/xdp/xsk_queue.h
+++ net/xdp/xsk_queue.h
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <linux/if_xdp.h>
-
-#include "xdp_umem_props.h"
+#include <net/xdp_sock.h>
 
 #define RX_BATCH_SIZE 16
 
