a85b786e93fae4b742361b2c7d44c89aa127654e
Arnd Bergmann <arnd@arndb.de>
2018-06-05 13:50:07 +0200

    arm64: cpu_errata: include required headers
    Without including psci.h and arm-smccc.h, we now get a build failure in
    some configurations:
    
    arch/arm64/kernel/cpu_errata.c: In function 'arm64_update_smccc_conduit':
    arch/arm64/kernel/cpu_errata.c:278:10: error: 'psci_ops' undeclared (first use in this function); did you mean 'sysfs_ops'?
    
    arch/arm64/kernel/cpu_errata.c: In function 'arm64_set_ssbd_mitigation':
    arch/arm64/kernel/cpu_errata.c:311:3: error: implicit declaration of function 'arm_smccc_1_1_hvc' [-Werror=implicit-function-declaration]
       arm_smccc_1_1_hvc(ARM_SMCCC_ARCH_WORKAROUND_2, state, NULL);
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <linux/arm-smccc.h>
+#include <linux/psci.h>
 #include <linux/types.h>
 #include <asm/cpu.h>
 #include <asm/cputype.h>
