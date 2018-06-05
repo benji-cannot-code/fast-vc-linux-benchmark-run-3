0824b1b575f9aa12ac4f1fca52c0d2628bc47bbf
Colin Ian King <colin.king@canonical.com>
2018-06-05 16:14:56 +0100

    lightnvm: pblk: fix resource leak of invalid_bitmap
    Currently the error exit path when the emeta could not be
    interpreted is via fail_free_ws and this fails to free
    invalid_bitmap.  Fix this by adding another exit label and
    exiting via this to kfree invalid_bitmap.
    
    Detected by CoverityScan, CID#1469659 ("Resource leak")
    
    Fixes: 48b8d20895f8 ("lightnvm: pblk: garbage collect lines with failed writes")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/lightnvm/pblk-gc.c
+++ drivers/lightnvm/pblk-gc.c
@@ -204,7 +204,7 @@ static void pblk_gc_line_prepare_ws(struct work_struct *work)
 		if (!lba_list) {
 			pr_err("pblk: could not interpret emeta (line %d)\n",
 					line->id);
-			goto fail_free_ws;
+			goto fail_free_invalid_bitmap;
 		}
 	}
 
@@ -281,6 +281,7 @@ fail_free_gc_rq:
 	kfree(gc_rq);
 fail_free_lba_list:
 	pblk_mfree(lba_list, l_mg->emeta_alloc_type);
+fail_free_invalid_bitmap:
 	kfree(invalid_bitmap);
 fail_free_ws:
 	kfree(line_ws);
