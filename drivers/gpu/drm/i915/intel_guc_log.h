279f365710885ce32063756a2fff695609253a83
Piotr Piorkowski <piotr.piorkowski@intel.com>
2018-06-05 17:13:30 +0200

    drm/i915/guc: Add support for define guc_log_size in megabytes.
    At this moment we can define GuC logs sizes only using pages.
    But GuC also allows use for this values expressed in megabytes.
    Lets add support for define guc_log_size in megabytes when we
    debug of GuC.
    
    v2:
    - change buffers size to more friendly (Michał Wajdeczko)
    - merge statements in guc_ctl_log_params_flags() (Michał Wajdeczko)
    v3:
    - fix ifdef (rename DRM_I915_DEBUG_GUC to CONFIG_DRM_I915_DEBUG_GUC)
    (Michał Wajdeczko)
    - use SZ_* macros to define buffers sizes (Michał Wajdeczko)
    
    Signed-off-by: Piotr Piórkowski <piotr.piorkowski@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Michał Winiarski <michal.winiarski@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180605151330.9954-2-piotr.piorkowski@intel.com

--- drivers/gpu/drm/i915/intel_guc_log.h
+++ drivers/gpu/drm/i915/intel_guc_log.h
@@ -35,9 +35,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct intel_guc;
 
+#ifdef CONFIG_DRM_I915_DEBUG_GUC
+#define CRASH_BUFFER_SIZE	SZ_2M
+#define DPC_BUFFER_SIZE		SZ_8M
+#define ISR_BUFFER_SIZE		SZ_8M
+#else
 #define CRASH_BUFFER_SIZE	SZ_8K
 #define DPC_BUFFER_SIZE		SZ_32K
 #define ISR_BUFFER_SIZE		SZ_32K
+#endif
 
 /*
  * While we're using plain log level in i915, GuC controls are much more...
