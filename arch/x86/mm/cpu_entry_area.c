8c59c3ada685f72264f40c1693dfcf9fb3a26b5a
Adrian Hunter <adrian.hunter@intel.com>
2018-06-06 15:54:11 +0300

    x86: Add entry trampolines to kcore
    Without program headers for PTI entry trampoline pages, the trampoline
    virtual addresses do not map to anything.
    
    Example before:
    
     sudo gdb --quiet vmlinux /proc/kcore
     Reading symbols from vmlinux...done.
     [New process 1]
     Core was generated by `BOOT_IMAGE=/boot/vmlinuz-4.16.0 root=UUID=a6096b83-b763-4101-807e-f33daff63233'.
     #0  0x0000000000000000 in irq_stack_union ()
     (gdb) x /21ib 0xfffffe0000006000
        0xfffffe0000006000:  Cannot access memory at address 0xfffffe0000006000
     (gdb) quit
    
    After:
    
     sudo gdb --quiet vmlinux /proc/kcore
     [sudo] password for ahunter:
     Reading symbols from vmlinux...done.
     [New process 1]
     Core was generated by `BOOT_IMAGE=/boot/vmlinuz-4.16.0-fix-4-00005-gd6e65a8b4072 root=UUID=a6096b83-b7'.
     #0  0x0000000000000000 in irq_stack_union ()
     (gdb) x /21ib 0xfffffe0000006000
        0xfffffe0000006000:  swapgs
        0xfffffe0000006003:  mov    %rsp,-0x3e12(%rip)        # 0xfffffe00000021f8
        0xfffffe000000600a:  xchg   %ax,%ax
        0xfffffe000000600c:  mov    %cr3,%rsp
        0xfffffe000000600f:  bts    $0x3f,%rsp
        0xfffffe0000006014:  and    $0xffffffffffffe7ff,%rsp
        0xfffffe000000601b:  mov    %rsp,%cr3
        0xfffffe000000601e:  mov    -0x3019(%rip),%rsp        # 0xfffffe000000300c
        0xfffffe0000006025:  pushq  $0x2b
        0xfffffe0000006027:  pushq  -0x3e35(%rip)        # 0xfffffe00000021f8
        0xfffffe000000602d:  push   %r11
        0xfffffe000000602f:  pushq  $0x33
        0xfffffe0000006031:  push   %rcx
        0xfffffe0000006032:  push   %rdi
        0xfffffe0000006033:  mov    $0xffffffff91a00010,%rdi
        0xfffffe000000603a:  callq  0xfffffe0000006046
        0xfffffe000000603f:  pause
        0xfffffe0000006041:  lfence
        0xfffffe0000006044:  jmp    0xfffffe000000603f
        0xfffffe0000006046:  mov    %rdi,(%rsp)
        0xfffffe000000604a:  retq
     (gdb) quit
    
    In addition, entry trampolines all map to the same page.  Represent that
    by giving the corresponding program headers in kcore the same offset.
    
    This has the benefit that, when perf tools uses /proc/kcore as a source
    for kernel object code, samples from different CPU trampolines are
    aggregated together.  Note, such aggregation is normal for profiling
    i.e. people want to profile the object code, not every different virtual
    address the object code might be mapped to (across different processes
    for example).
    
    Notes by PeterZ:
    
    This also adds the KCORE_REMAP functionality.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Acked-by: Andi Kleen <ak@linux.intel.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: x86@kernel.org
    Link: http://lkml.kernel.org/r/1528289651-4113-4-git-send-email-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- arch/x86/mm/cpu_entry_area.c
+++ arch/x86/mm/cpu_entry_area.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/percpu.h>
 #include <linux/kallsyms.h>
+#include <linux/kcore.h>
 
 #include <asm/cpu_entry_area.h>
 #include <asm/pgtable.h>
@@ -15,6 +16,7 @@ static DEFINE_PER_CPU_PAGE_ALIGNED(struct entry_stack_page, entry_stack_storage)
 #ifdef CONFIG_X86_64
 static DEFINE_PER_CPU_PAGE_ALIGNED(char, exception_stacks
 	[(N_EXCEPTION_STACKS - 1) * EXCEPTION_STKSZ + DEBUG_STKSZ]);
+static DEFINE_PER_CPU(struct kcore_list, kcore_entry_trampoline);
 #endif
 
 struct cpu_entry_area *get_cpu_entry_area(int cpu)
@@ -148,6 +150,14 @@ static void __init setup_cpu_entry_area(int cpu)
 
 	cea_set_pte(&get_cpu_entry_area(cpu)->entry_trampoline,
 		     __pa_symbol(_entry_trampoline), PAGE_KERNEL_RX);
+	/*
+	 * The cpu_entry_area alias addresses are not in the kernel binary
+	 * so they do not show up in /proc/kcore normally.  This adds entries
+	 * for them manually.
+	 */
+	kclist_add_remap(&per_cpu(kcore_entry_trampoline, cpu),
+			 _entry_trampoline,
+			 &get_cpu_entry_area(cpu)->entry_trampoline, PAGE_SIZE);
 #endif
 	percpu_setup_debug_store(cpu);
 }
