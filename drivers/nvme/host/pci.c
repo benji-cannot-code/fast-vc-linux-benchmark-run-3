3f356d5b5c0aa0928ee3f9576db5309e6ebb9221
Keith Busch <keith.busch@intel.com>
2018-06-06 08:13:06 -0600

    nvme-pci: queue creation fixes
    We've been ignoring NVMe error status on queue creations. Fortunately they
    are uncommon, but we should handle these anyway. This patch adds checks
    for the a positive error return value that indicates an NVMe status.
    
    If we do see a negative return, the controller isn't usable, so this
    patch returns immediately in since we can't unwind that failure.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Jens Axboe <axboe@kernel.dk>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -1476,11 +1476,13 @@ static int nvme_create_queue(struct nvme_queue *nvmeq, int qid)
 	 */
 	vector = dev->num_vecs == 1 ? 0 : qid;
 	result = adapter_alloc_cq(dev, qid, nvmeq, vector);
-	if (result < 0)
-		goto out;
+	if (result)
+		return result;
 
 	result = adapter_alloc_sq(dev, qid, nvmeq);
 	if (result < 0)
+		return result;
+	else if (result)
 		goto release_cq;
 
 	/*
@@ -1502,7 +1504,6 @@ release_sq:
 	adapter_delete_sq(dev, qid);
 release_cq:
 	adapter_delete_cq(dev, qid);
-out:
 	return result;
 }
 
