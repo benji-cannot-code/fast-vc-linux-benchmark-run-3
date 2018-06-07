5c08bf69bde284d41dff2f357c1e917b529696a3
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-06-07 19:55:04 +0300

    drm/omap: Don't call .set_timings() operation recursively
    Instead of calling the .set_timings() operation recursively from the
    display device backwards, iterate over the devices manually in the DRM
    encoder code. This moves the complexity to a single central location and
    simplifies the logic in omap_dss_device drivers.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.co.uk>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/gpu/drm/omapdrm/displays/panel-sharp-ls037v7dw01.c
+++ drivers/gpu/drm/omapdrm/displays/panel-sharp-ls037v7dw01.c
@@ -127,14 +127,6 @@ static void sharp_ls_disable(struct omap_dss_device *dssdev)
 	dssdev->state = OMAP_DSS_DISPLAY_DISABLED;
 }
 
-static void sharp_ls_set_timings(struct omap_dss_device *dssdev,
-				 const struct videomode *vm)
-{
-	struct omap_dss_device *src = dssdev->src;
-
-	src->ops->set_timings(src, vm);
-}
-
 static void sharp_ls_get_timings(struct omap_dss_device *dssdev,
 				 struct videomode *vm)
 {
@@ -150,7 +142,6 @@ static const struct omap_dss_device_ops sharp_ls_ops = {
 	.enable		= sharp_ls_enable,
 	.disable	= sharp_ls_disable,
 
-	.set_timings	= sharp_ls_set_timings,
 	.get_timings	= sharp_ls_get_timings,
 };
 
