73417d649569298655133696d5a88f6e99967207
Christian König <christian.koenig@amd.com>
2018-06-07 10:28:47 +0200

    drm/amdgpu: add independent DMA-buf import v9
    Instead of relying on the DRM functions just implement our own import
    functions. This prepares support for taking care of unpinned DMA-buf.
    
    v2: enable for all exporters, not just amdgpu, fix invalidation
        handling, lock reservation object while setting callback
    v3: change to new dma_buf attach interface
    v4: split out from unpinned DMA-buf work
    v5: rebased and cleanup on new DMA-buf interface
    v6: squash with invalidation callback change,
        stop using _(map|unmap)_locked
    v7: drop invalidations when the BO is already in system domain
    v8: rebase on new DMA-buf patch and drop move notification
    v9: cleanup comments
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Link: https://patchwork.freedesktop.org/patch/337948/

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.h
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <drm/drm_gem.h>
 
-struct drm_gem_object *
-amdgpu_gem_prime_import_sg_table(struct drm_device *dev,
-				 struct dma_buf_attachment *attach,
-				 struct sg_table *sg);
 struct dma_buf *amdgpu_gem_prime_export(struct drm_gem_object *gobj,
 					int flags);
 struct drm_gem_object *amdgpu_gem_prime_import(struct drm_device *dev,
