fa2687264c2d74ceee2684fa812b94f0c1437664
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2018-06-07 17:05:10 -0700

    net/9p/trans_xen.c: don't inclide rwlock.h directly
    rwlock.h should not be included directly.  Instead linux/splinlock.h
    should be included.  One thing it does is to break the RT build.
    
    Link: http://lkml.kernel.org/r/20180504100319.11880-1-bigeasy@linutronix.de
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Eric Van Hensbergen <ericvh@gmail.com>
    Cc: Ron Minnich <rminnich@sandia.gov>
    Cc: Latchesar Ionkov <lucho@ionkov.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- net/9p/trans_xen.c
+++ net/9p/trans_xen.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/spinlock.h>
-#include <linux/rwlock.h>
 #include <net/9p/9p.h>
 #include <net/9p/client.h>
 #include <net/9p/transport.h>
