baefc4afbe19ae3e56383dad7d6e3a80395104bf
Stefan Agner <stefan@agner.ch>
2018-06-07 17:06:15 -0700

    mm/memblock: introduce PHYS_ADDR_MAX
    So far code was using ULLONG_MAX and type casting to obtain a
    phys_addr_t with all bits set.  The typecast is necessary to silence
    compiler warnings on 32-bit platforms.
    
    Use the simpler but still type safe approach "~(phys_addr_t)0" to create a
    preprocessor define for all bits set.
    
    Link: http://lkml.kernel.org/r/20180406213809.566-1-stefan@agner.ch
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Pavel Tatashin <pasha.tatashin@oracle.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LLONG_MIN	(-LLONG_MAX - 1)
 #define ULLONG_MAX	(~0ULL)
 #define SIZE_MAX	(~(size_t)0)
+#define PHYS_ADDR_MAX	(~(phys_addr_t)0)
 
 #define U8_MAX		((u8)~0U)
 #define S8_MAX		((s8)(U8_MAX>>1))
