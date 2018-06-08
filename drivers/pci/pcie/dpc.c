60d33994d579b3503e8fab75b314fe1a5ec6a9ce
Bjorn Helgaas <bhelgaas@google.com>
2018-06-08 08:40:16 -0500

    PCI/AER: Move pcie_aer_get_firmware_first() to portdrv.h
    Move pcie_aer_get_firmware_first() to portdrv.h, where it can be more
    easily shared between AER and DPC.  Then DPC no longer needs to include
    aer/aerdrv.h.  No functional change intended.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Keith Busch <keith.busch@intel.com>

--- drivers/pci/pcie/dpc.c
+++ drivers/pci/pcie/dpc.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "portdrv.h"
 #include "../pci.h"
-#include "aer/aerdrv.h"
 
 struct dpc_dev {
 	struct pcie_device	*dev;
