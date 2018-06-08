63713ac626acb10202342e899db7e528a7ba1a85
Bjorn Helgaas <bhelgaas@google.com>
2018-06-08 08:40:25 -0500

    PCI/AER: Move aer_irq() declaration to portdrv.h
    The aer_irq() declaration is the only thing needed by aer_inject.c.  Move
    it to portdrv.h so we eventually get rid of aerdrv.h completely.  No
    functional change intended.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Keith Busch <keith.busch@intel.com>

--- drivers/pci/pcie/aer/aer_inject.c
+++ drivers/pci/pcie/aer/aer_inject.c
@@ -22,7 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/stddef.h>
 #include <linux/device.h>
-#include "aerdrv.h"
+
+#include "../portdrv.h"
 
 /* Override the existing corrected and uncorrected error masks */
 static bool aer_mask_override;
