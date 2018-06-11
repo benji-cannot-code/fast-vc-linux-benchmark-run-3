696e60a12afcea9ab0d6fb54f716ad82c4e135f7
Katsuhiro Suzuki <suzuki.katsuhiro@socionext.com>
2018-06-11 17:32:12 +0900

    ASoC: simple-card-utils: move hp and mic detect gpios from simple-card
    This patch moves headphone and microphone jack detection gpios from
    simple-card driver. It is preparing for using this feature from other
    drivers.
    
    Signed-off-by: Katsuhiro Suzuki <suzuki.katsuhiro@socionext.com>
    Acked-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/simple_card_utils.h
+++ include/sound/simple_card_utils.h
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <sound/soc.h>
 
+#define asoc_simple_card_init_hp(card, sjack, prefix) \
+	asoc_simple_card_init_jack(card, sjack, 1, prefix)
+#define asoc_simple_card_init_mic(card, sjack, prefix) \
+	asoc_simple_card_init_jack(card, sjack, 0, prefix)
+
 struct asoc_simple_dai {
 	const char *name;
 	unsigned int sysclk;
@@ -27,6 +32,12 @@ struct asoc_simple_dai {
 struct asoc_simple_card_data {
 	u32 convert_rate;
 	u32 convert_channels;
+};
+
+struct asoc_simple_jack {
+	struct snd_soc_jack jack;
+	struct snd_soc_jack_pin pin;
+	struct snd_soc_jack_gpio gpio;
 };
 
 int asoc_simple_card_parse_daifmt(struct device *dev,
@@ -107,5 +118,9 @@ int asoc_simple_card_of_parse_routing(struct snd_soc_card *card,
 				      int optional);
 int asoc_simple_card_of_parse_widgets(struct snd_soc_card *card,
 				      char *prefix);
+
+int asoc_simple_card_init_jack(struct snd_soc_card *card,
+			       struct asoc_simple_jack *sjack,
+			       int is_hp, char *prefix);
 
 #endif /* __SIMPLE_CARD_UTILS_H */
