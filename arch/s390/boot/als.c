4e3ad56b16fb551700c032b88faf309161af5660
Vasily Gorbik <gor@linux.ibm.com>
2018-06-12 15:11:40 +0200

    s390/boot: make head.S and als.c be part of the decompressor only
    Since uncompressed kernel image does not have to be bootable anymore,
    move head.S, head_kdump.S and als.c to boot/ folder and compile them
    in just in the decompressor.
    
    Reviewed-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/boot/als.c
+++ arch/s390/boot/als.c
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/facility.h>
 #include <asm/lowcore.h>
 #include <asm/sclp.h>
-#include "entry.h"
 
 /*
  * The code within this file will be called very early. It may _not_
