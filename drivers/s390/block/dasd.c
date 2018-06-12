cf17d42897c23b6e986207d31e7450ee330320b8
Stefan Haberland <sth@linux.ibm.com>
2018-06-12 16:47:10 +0200

    s390/dasd: reduce the default queue depth and nr of hardware queues
    Reduce the default values for the number of hardware queues and queue depth
    to significantly reduce the memory footprint of a DASD device.
    The memory consumption per DASD device reduces from approximately 40MB to
    approximately 1.5MB.
    
    This is necessary to build systems with a large number of DASD devices and
    a reasonable amount of memory.
    Performance measurements showed that good performance results are possible
    with the new default values even on systems with lots of CPUs and lots of
    alias devices.
    
    Fixes: e443343e509a ("s390/dasd: blk-mq conversion")
    Reviewed-by: Jan Hoeppner <hoeppner@linux.ibm.com>
    Reviewed-by: Peter Oberparleiter <oberpar@linux.ibm.com>
    Signed-off-by: Stefan Haberland <sth@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/block/dasd.c
+++ drivers/s390/block/dasd.c
@@ -42,6 +42,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DASD_DIAG_MOD		"dasd_diag_mod"
 
+static unsigned int queue_depth = 32;
+static unsigned int nr_hw_queues = 4;
+
+module_param(queue_depth, uint, 0444);
+MODULE_PARM_DESC(queue_depth, "Default queue depth for new DASD devices");
+
+module_param(nr_hw_queues, uint, 0444);
+MODULE_PARM_DESC(nr_hw_queues, "Default number of hardware queues for new DASD devices");
+
 /*
  * SECTION: exported variables of dasd.c
  */
@@ -3116,8 +3125,8 @@ static int dasd_alloc_queue(struct dasd_block *block)
 
 	block->tag_set.ops = &dasd_mq_ops;
 	block->tag_set.cmd_size = sizeof(struct dasd_ccw_req);
-	block->tag_set.nr_hw_queues = DASD_NR_HW_QUEUES;
-	block->tag_set.queue_depth = DASD_MAX_LCU_DEV * DASD_REQ_PER_DEV;
+	block->tag_set.nr_hw_queues = nr_hw_queues;
+	block->tag_set.queue_depth = queue_depth;
 	block->tag_set.flags = BLK_MQ_F_SHOULD_MERGE;
 
 	rc = blk_mq_alloc_tag_set(&block->tag_set);
