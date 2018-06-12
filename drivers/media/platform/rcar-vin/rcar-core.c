4eed1fd90252c54405df98557047ab4173a5122a
Jacopo Mondi <jacopo+renesas@jmondi.org>
2018-06-12 05:43:26 -0400

    media: rcar-vin: Cleanup notifier in error path
    During the notifier initialization, memory for the list of associated async
    subdevices is reserved during the fwnode endpoint parsing from the v4l2-async
    framework. If the notifier registration fails, that memory should be released
    and the notifier 'cleaned up'.
    
    Catch the notifier registration error and perform the cleanup both for the
    group and the parallel notifiers.
    
    Signed-off-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Acked-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/rcar-vin/rcar-core.c
+++ drivers/media/platform/rcar-vin/rcar-core.c
@@ -572,6 +572,7 @@ static int rvin_parallel_graph_init(struct rvin_dev *vin)
 	ret = v4l2_async_notifier_register(&vin->v4l2_dev, &vin->notifier);
 	if (ret < 0) {
 		vin_err(vin, "Notifier registration failed\n");
+		v4l2_async_notifier_cleanup(&vin->group->notifier);
 		return ret;
 	}
 
@@ -774,6 +775,7 @@ static int rvin_mc_parse_of_graph(struct rvin_dev *vin)
 					   &vin->group->notifier);
 	if (ret < 0) {
 		vin_err(vin, "Notifier registration failed\n");
+		v4l2_async_notifier_cleanup(&vin->group->notifier);
 		return ret;
 	}
 
