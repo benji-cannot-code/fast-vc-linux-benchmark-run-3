14b2daf92046fe4411b171476127e8c82026f7dc
Ezequiel Garcia <ezequiel@collabora.com>
2018-06-14 11:34:05 -0400

    media: rcar_vpu: Drop unneeded job_ready
    It is not required to implement job_ready().
    Since this one is dummy, we can drop it.
    
    Signed-off-by: Ezequiel Garcia <ezequiel@collabora.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/rcar_jpu.c
+++ drivers/media/platform/rcar_jpu.c
@@ -1495,11 +1495,6 @@ static void jpu_device_run(void *priv)
 	spin_unlock_irqrestore(&ctx->jpu->lock, flags);
 }
 
-static int jpu_job_ready(void *priv)
-{
-	return 1;
-}
-
 static void jpu_job_abort(void *priv)
 {
 	struct jpu_ctx *ctx = priv;
@@ -1511,7 +1506,6 @@ static void jpu_job_abort(void *priv)
 
 static const struct v4l2_m2m_ops jpu_m2m_ops = {
 	.device_run	= jpu_device_run,
-	.job_ready	= jpu_job_ready,
 	.job_abort	= jpu_job_abort,
 };
 
