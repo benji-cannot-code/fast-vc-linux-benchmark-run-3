b89481a35ea9d42826b379852ba531a20dbd82e7
Will Deacon <will.deacon@arm.com>
2018-06-15 11:50:42 +0100

    arm64: ssbd: Drop #ifdefs for PR_SPEC_STORE_BYPASS
    Now that we're all merged nicely into mainline, there's no need to check
    to see if PR_SPEC_STORE_BYPASS is defined.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/kernel/ssbd.c
+++ arch/arm64/kernel/ssbd.c
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * prctl interface for SSBD
- * FIXME: Drop the below ifdefery once merged in 4.18.
  */
-#ifdef PR_SPEC_STORE_BYPASS
 static int ssbd_prctl_set(struct task_struct *task, unsigned long ctrl)
 {
 	int state = arm64_get_ssbd_state();
@@ -108,4 +106,3 @@ int arch_prctl_spec_ctrl_get(struct task_struct *task, unsigned long which)
 		return -ENODEV;
 	}
 }
-#endif	/* PR_SPEC_STORE_BYPASS */
