02f71777bed7ac88e13625c642dc1d39b98de4d9
David S. Miller <davem@davemloft.net>
2018-06-17 08:38:55 +0900

    bluetooth: hci_nokia: Don't include linux/unaligned/le_struct.h directly.
    This breaks the build as this header is not meant to be used in this
    way.
    
    ./include/linux/unaligned/access_ok.h:8:28: error: redefinition of ‘get_unaligned_le16’
     static __always_inline u16 get_unaligned_le16(const void *p)
                                ^~~~~~~~~~~~~~~~~~
    In file included from drivers/bluetooth/hci_nokia.c:32:
    ./include/linux/unaligned/le_struct.h:7:19: note: previous definition of ‘get_unaligned_le16’ was here
     static inline u16 get_unaligned_le16(const void *p)
    
    Use asm/unaligned.h instead.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/bluetooth/hci_nokia.c
+++ drivers/bluetooth/hci_nokia.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/types.h>
-#include <linux/unaligned/le_struct.h>
+#include <asm/unaligned.h>
 #include <net/bluetooth/bluetooth.h>
 #include <net/bluetooth/hci_core.h>
 
