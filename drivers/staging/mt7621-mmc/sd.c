f2d6f415da3afef6c8afba74a8f9d18657c12367
Christian Lütke-Stetzkamp <christian@lkamp.de>
2018-06-16 16:14:56 +0200

    staging: mt7621-mmc: Remove unused macro MAX_BD_PER_GPD
    The macro MAX_BD_PER_GPD is never used, so remove it.
    
    Signed-off-by: Christian Lütke-Stetzkamp <christian@lkamp.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/sd.c
+++ drivers/staging/mt7621-mmc/sd.c
@@ -92,7 +92,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_GPD_NUM         (1 + 1)  /* one null gpd */
 #define MAX_BD_NUM          (1024)
-#define MAX_BD_PER_GPD      (MAX_BD_NUM)
 
 #define MAX_HW_SGMTS        (MAX_BD_NUM)
 #define MAX_PHY_SGMTS       (MAX_BD_NUM)
