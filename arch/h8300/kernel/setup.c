5e0ebc5c0291b39f59cd6c8905461a28a5e2ad28
Rob Herring <robh@kernel.org>
2018-06-19 15:40:54 -0600

    h8300: remove unnecessary of_platform_populate call
    The DT core will call of_platform_populate, so it is not necessary for
    arch specific code to call it unless there are custom match entries,
    auxdata or parent device. Neither of those apply here, so remove the call.
    
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: uclinux-h8-devel@lists.sourceforge.jp
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Yoshinori Sato <ysato@users.sourceforge.jp>

--- arch/h8300/kernel/setup.c
+++ arch/h8300/kernel/setup.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/of.h>
 #include <linux/of_fdt.h>
-#include <linux/of_platform.h>
 #include <linux/of_address.h>
 #include <linux/clk-provider.h>
 #include <linux/memblock.h>
@@ -168,15 +167,6 @@ const struct seq_operations cpuinfo_op = {
 	.stop	= c_stop,
 	.show	= show_cpuinfo,
 };
-
-static int __init device_probe(void)
-{
-	of_platform_populate(NULL, NULL, NULL, NULL);
-
-	return 0;
-}
-
-device_initcall(device_probe);
 
 #if defined(CONFIG_CPU_H8300H)
 #define get_wait(base, addr) ({		\
