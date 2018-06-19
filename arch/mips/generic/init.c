f01252799d3e11e73ee14891a066d8ca2e1b2f2f
Rob Herring <robh@kernel.org>
2018-06-19 15:47:09 -0600

    MIPS: generic: remove unnecessary of_platform_populate call
    The DT core will call of_platform_populate, so it is not necessary for
    arch specific code to call it unless there are custom match entries,
    auxdata or parent device. Neither of those apply here, so remove the call.
    
    Cc: Paul Burton <paul.burton@mips.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: James Hogan <jhogan@kernel.org>
    Cc: linux-mips@linux-mips.org
    Signed-off-by: Rob Herring <robh@kernel.org>
    Patchwork: https://patchwork.linux-mips.org/patch/19591/
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: linux-kernel@vger.kernel.org

--- arch/mips/generic/init.c
+++ arch/mips/generic/init.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/irqchip.h>
 #include <linux/of_fdt.h>
-#include <linux/of_platform.h>
 
 #include <asm/bootinfo.h>
 #include <asm/fw/fw.h>
@@ -208,18 +207,6 @@ void __init arch_init_irq(void)
 
 	irqchip_init();
 }
-
-static int __init publish_devices(void)
-{
-	if (!of_have_populated_dt())
-		panic("Device-tree not present");
-
-	if (of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL))
-		panic("Failed to populate DT");
-
-	return 0;
-}
-arch_initcall(publish_devices);
 
 void __init prom_free_prom_memory(void)
 {
