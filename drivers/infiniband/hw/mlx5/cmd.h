eab40fcc938411202115e0820f5c83a488eb0c95
Yonatan Cohen <yonatanc@mellanox.com>
2018-06-19 08:47:22 +0300

    net/mlx5: Limit scope of dump_fill_mkey function
    mlx5_core_dump_fill_mkey() is going to be used in next
    patch in IB and doesn't need to be visible to whole
    mlx5_core. Move that command to mlx5_ib.
    
    Signed-off-by: Yonatan Cohen <yonatanc@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- drivers/infiniband/hw/mlx5/cmd.h
+++ drivers/infiniband/hw/mlx5/cmd.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/mlx5/driver.h>
 
+int mlx5_cmd_dump_fill_mkey(struct mlx5_core_dev *dev, u32 *mkey);
 int mlx5_cmd_null_mkey(struct mlx5_core_dev *dev, u32 *null_mkey);
 int mlx5_cmd_query_cong_params(struct mlx5_core_dev *dev, int cong_point,
 			       void *out, int out_size);
