c9bfe20a239178e348152af2eb0ec02e1e2b1fe7
Christoph Hellwig <hch@lst.de>
2018-06-19 09:04:55 +0200

    parisc: use generic dma_noncoherent_ops
    Switch to the generic noncoherent direct mapping implementation.
    
    Fix sync_single_for_cpu to do skip the cache flush unless the transfer
    is to the device to match the more tested unmap_single path which should
    have the same cache coherency implications.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/asm/dma-mapping.h
+++ arch/parisc/include/asm/dma-mapping.h
@@ -22,10 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 ** flush/purge and allocate "regular" cacheable pages for everything.
 */
 
-#ifdef CONFIG_PA11
-extern const struct dma_map_ops pa11_dma_ops;
-#endif
-
 extern const struct dma_map_ops *hppa_dma_ops;
 
 static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
