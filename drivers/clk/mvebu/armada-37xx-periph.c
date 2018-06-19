e2975def55922965d8ec97f7414ffdf8efa6ee39
Gregory CLEMENT <gregory.clement@bootlin.com>
2018-06-19 14:34:46 +0200

    clk: mvebu: armada-37xx-periph: switch to SPDX license identifier
    Adopt the SPDX license identifier headers to ease license compliance
    management.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/mvebu/armada-37xx-periph.c
+++ drivers/clk/mvebu/armada-37xx-periph.c
@@ -1,14 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * Marvell Armada 37xx SoC Peripheral clocks
  *
  * Copyright (C) 2016 Marvell
  *
  * Gregory CLEMENT <gregory.clement@free-electrons.com>
- *
- * This file is licensed under the terms of the GNU General Public
- * License version 2 or later. This program is licensed "as is"
- * without any warranty of any kind, whether express or implied.
  *
  * Most of the peripheral clocks can be modelled like this:
  *             _____    _______    _______
