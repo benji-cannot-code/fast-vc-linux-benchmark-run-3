8d172f9d9b28a5f102c3603b643bbd80b0c34008
Charles Keepax <ckeepax@opensource.cirrus.com>
2018-06-20 11:56:20 +0100

    ASoC: arizona: Set compressed IRQ to a wake source
    The current code is not setting the compressed IRQ as a wake
    source.  Normally this doesn't cause any issues as the CODEC
    IRQ is set as a wake source by the jack detection code and the
    CODEC only produces a single IRQ line. However if the system
    is not using jack detection the compressed audio IRQ should
    still function as a wake source, as such directly set the
    compressed audio IRQ as a wake source.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs47l24.c
+++ sound/soc/codecs/cs47l24.c
@@ -1284,6 +1284,12 @@ static int cs47l24_probe(struct platform_device *pdev)
 		return ret;
 	}
 
+	ret = arizona_set_irq_wake(arizona, ARIZONA_IRQ_DSP_IRQ1, 1);
+	if (ret != 0)
+		dev_warn(&pdev->dev,
+			 "Failed to set compressed IRQ as a wake source: %d\n",
+			 ret);
+
 	arizona_init_common(arizona);
 
 	ret = arizona_init_vol_limit(arizona);
@@ -1307,6 +1313,7 @@ static int cs47l24_probe(struct platform_device *pdev)
 err_spk_irqs:
 	arizona_free_spk_irqs(arizona);
 err_dsp_irq:
+	arizona_set_irq_wake(arizona, ARIZONA_IRQ_DSP_IRQ1, 0);
 	arizona_free_irq(arizona, ARIZONA_IRQ_DSP_IRQ1, cs47l24);
 
 	return ret;
@@ -1324,6 +1331,7 @@ static int cs47l24_remove(struct platform_device *pdev)
 
 	arizona_free_spk_irqs(arizona);
 
+	arizona_set_irq_wake(arizona, ARIZONA_IRQ_DSP_IRQ1, 0);
 	arizona_free_irq(arizona, ARIZONA_IRQ_DSP_IRQ1, cs47l24);
 
 	return 0;
