a9a65ddb6193534e27380eff42ab23e1cd7f926b
Christian König <christian.koenig@amd.com>
2018-06-20 20:48:59 +0200

    staging: android: ion: fix ion_dma_buf_attach signatur
    Fixup for "dma_buf: remove device parameter from attach callback v2".
    
    I missed this driver, sorry for the noise. Patch is not even compile
    tested.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/230641/

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -202,7 +202,7 @@ struct ion_dma_buf_attachment {
 	struct list_head list;
 };
 
-static int ion_dma_buf_attach(struct dma_buf *dmabuf, struct device *dev,
+static int ion_dma_buf_attach(struct dma_buf *dmabuf,
 			      struct dma_buf_attachment *attachment)
 {
 	struct ion_dma_buf_attachment *a;
@@ -220,7 +220,7 @@ static int ion_dma_buf_attach(struct dma_buf *dmabuf, struct device *dev,
 	}
 
 	a->table = table;
-	a->dev = dev;
+	a->dev = attachment->dev;
 	INIT_LIST_HEAD(&a->list);
 
 	attachment->priv = a;
