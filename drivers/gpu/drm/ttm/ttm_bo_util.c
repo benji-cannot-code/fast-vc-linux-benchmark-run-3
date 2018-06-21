8e7b6b496e87033425d47906ba044ef20357a9f7
Thomas Zimmermann <contact@tzimmermann.org>
2018-06-21 15:21:37 +0200

    drm/ttm: Replace ttm_bo_unref() with ttm_bo_put()
    A call to ttm_bo_unref() clears the supplied pointer to NULL, while
    ttm_bo_put() does not. None of the converted call sites requires the
    pointer to become NULL, so the respective assign operations has been
    left out from the patch.
    
    Signed-off-by: Thomas Zimmermann <contact@tzimmermann.org>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/ttm/ttm_bo_util.c
+++ drivers/gpu/drm/ttm/ttm_bo_util.c
@@ -464,7 +464,7 @@ static void ttm_transfered_destroy(struct ttm_buffer_object *bo)
 	struct ttm_transfer_obj *fbo;
 
 	fbo = container_of(bo, struct ttm_transfer_obj, base);
-	ttm_bo_unref(&fbo->bo);
+	ttm_bo_put(fbo->bo);
 	kfree(fbo);
 }
 
@@ -732,7 +732,7 @@ int ttm_bo_move_accel_cleanup(struct ttm_buffer_object *bo,
 			bo->ttm = NULL;
 
 		ttm_bo_unreserve(ghost_obj);
-		ttm_bo_unref(&ghost_obj);
+		ttm_bo_put(ghost_obj);
 	}
 
 	*old_mem = *new_mem;
@@ -788,7 +788,7 @@ int ttm_bo_pipeline_move(struct ttm_buffer_object *bo,
 			bo->ttm = NULL;
 
 		ttm_bo_unreserve(ghost_obj);
-		ttm_bo_unref(&ghost_obj);
+		ttm_bo_put(ghost_obj);
 
 	} else if (from->flags & TTM_MEMTYPE_FLAG_FIXED) {
 
@@ -853,7 +853,7 @@ int ttm_bo_pipeline_gutting(struct ttm_buffer_object *bo)
 	bo->ttm = NULL;
 
 	ttm_bo_unreserve(ghost);
-	ttm_bo_unref(&ghost);
+	ttm_bo_put(ghost);
 
 	return 0;
 }
