ba3c2302fbf38e984cad8745b2226aa144b19527
Janusz Krzysztofik <jmkrzyszt@gmail.com>
2018-06-22 00:41:28 +0200

    Input: ams_delta_serio: Get FIQ buffer from platform_data
    Instead of exporting the FIQ buffer symbol to be used in
    ams-delta-serio driver, pass it to the driver as platform_data.
    
    Signed-off-by: Janusz Krzysztofik <jmkrzyszt@gmail.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/linux/platform_data/ams-delta-fiq.h
+++ include/linux/platform_data/ams-delta-fiq.h
@@ -56,8 +56,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define FIQ_CIRC_BUFF		30      /*Start of circular buffer */
 
-#ifndef __ASSEMBLER__
-extern unsigned int fiq_buffer[];
-#endif
-
 #endif
