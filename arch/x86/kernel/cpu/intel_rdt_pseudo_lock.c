eceb6436c9990429a58ed69a4d52bdf998908e91
Reinette Chatre <reinette.chatre@intel.com>
2018-06-22 15:42:14 -0700

    x86/intel_rdt: Add check to determine if monitoring in progress
    When a resource group is pseudo-locked it is orphaned without a class of
    service associated with it. We thus do not want any monitoring in progress
    on a resource group that will be used for pseudo-locking.
    
    Introduce a test that can be used to determine if pseudo-locking in
    progress on a resource group. Temporarily mark it as unused to avoid
    compile warnings until it is used.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: fenghua.yu@intel.com
    Cc: tony.luck@intel.com
    Cc: vikas.shivappa@linux.intel.com
    Cc: gavin.hindman@intel.com
    Cc: jithu.joseph@intel.com
    Cc: dave.hansen@intel.com
    Cc: hpa@zytor.com
    Link: https://lkml.kernel.org/r/14fd9494f87ca72a213b3a197d1172d4e66ae196.1529706536.git.reinette.chatre@intel.com

--- arch/x86/kernel/cpu/intel_rdt_pseudo_lock.c
+++ arch/x86/kernel/cpu/intel_rdt_pseudo_lock.c
@@ -15,6 +15,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "intel_rdt.h"
 
 /**
+ * rdtgroup_monitor_in_progress - Test if monitoring in progress
+ * @r: resource group being queried
+ *
+ * Return: 1 if monitor groups have been created for this resource
+ * group, 0 otherwise.
+ */
+static int __attribute__ ((unused))
+rdtgroup_monitor_in_progress(struct rdtgroup *rdtgrp)
+{
+	return !list_empty(&rdtgrp->mon.crdtgrp_list);
+}
+
+/**
  * rdtgroup_locksetup_user_restrict - Restrict user access to group
  * @rdtgrp: resource group needing access restricted
  *
