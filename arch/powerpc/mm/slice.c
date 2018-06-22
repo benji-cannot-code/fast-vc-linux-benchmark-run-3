90ed597731aeaf6b92858cff750f6564cf87e0fd
Christophe Leroy <christophe.leroy@c-s.fr>
2018-06-22 13:49:48 +0000

    powerpc/mm: fix missing prototypes in slice.c
    This patch fixes the following warnings (obtained with make W=1).
    
    arch/powerpc/mm/slice.c: At top level:
    arch/powerpc/mm/slice.c:682:15: error: no previous prototype for 'arch_get_unmapped_area' [-Werror=missing-prototypes]
     unsigned long arch_get_unmapped_area(struct file *filp,
                   ^
    arch/powerpc/mm/slice.c:692:15: error: no previous prototype for 'arch_get_unmapped_area_topdown' [-Werror=missing-prototypes]
     unsigned long arch_get_unmapped_area_topdown(struct file *filp,
                   ^
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/mm/slice.c
+++ arch/powerpc/mm/slice.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/export.h>
 #include <linux/hugetlb.h>
+#include <linux/sched/mm.h>
 #include <asm/mman.h>
 #include <asm/mmu.h>
 #include <asm/copro.h>
