a3a26224c034b0e81fb2fe6cd3097a46340bec38
Peter Pan <peterpandong@micron.com>
2018-06-22 14:28:23 +0200

    mtd: nand: Add core infrastructure to support SPI NANDs
    Add a SPI NAND framework based on the generic NAND framework and the
    spi-mem infrastructure.
    
    In its current state, this framework supports the following features:
    
    - single/dual/quad IO modes
    - on-die ECC
    
    Signed-off-by: Peter Pan <peterpandong@micron.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- include/linux/spi/spi-mem.h
+++ include/linux/spi/spi-mem.h
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2018 Exceet Electronics GmbH
  * Copyright (C) 2018 Bootlin
  *
- * Author: Boris Brezillon <boris.brezillon@bootlin.com>
+ * Author:
+ *	Peter Pan <peterpandong@micron.com>
+ *	Boris Brezillon <boris.brezillon@bootlin.com>
  */
 
 #ifndef __LINUX_SPI_MEM_H
