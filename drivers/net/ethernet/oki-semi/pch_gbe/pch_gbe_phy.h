7916655c9d77f9db26638a7ca1f61e0fa9d224aa
Paul Burton <paul.burton@mips.com>
2018-06-22 20:17:49 -0700

    net: pch_gbe: Remove PCH_GBE_MAC_IFOP_RGMII define
    The pch_gbe driver currently presumes that the PHY is connected using
    RGMII, and would need further work to support other buses. It includes a
    define which is always set that conditionalises some of the
    RGMII-specific code regardless. Remove it. If we do ever support
    different MII buses then preprocessor defines won't be the best way to
    select between them anyway.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_phy.h
+++ drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_phy.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PCH_GBE_PHY_REGS_LEN		32
 #define	PCH_GBE_PHY_RESET_DELAY_US	10
-#define PCH_GBE_MAC_IFOP_RGMII
 
 s32 pch_gbe_phy_get_id(struct pch_gbe_hw *hw);
 s32 pch_gbe_phy_read_reg_miic(struct pch_gbe_hw *hw, u32 offset, u16 *data);
