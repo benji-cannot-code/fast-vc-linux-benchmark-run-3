038395239eb1f0c62226e1fbebcb725f524a51f1
Paul Burton <paul.burton@mips.com>
2018-06-22 20:17:46 -0700

    net: pch_gbe: Remove init_hw HAL abstraction
    For some reason the pch_gbe driver contains a struct pch_gbe_functions
    with pointers used by a HAL abstraction layer, even though there is only
    one implementation of each function.
    
    This patch removes the init_hw abstraction in favor of inlining its
    single implementation (pch_gbe_plat_init_hw) into its single caller
    (pch_gbe_reset).
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_api.h
+++ drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_api.h
@@ -24,6 +24,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 s32 pch_gbe_hal_setup_init_funcs(struct pch_gbe_hw *hw);
 void pch_gbe_hal_get_bus_info(struct pch_gbe_hw *hw);
-s32 pch_gbe_hal_init_hw(struct pch_gbe_hw *hw);
 
 #endif
