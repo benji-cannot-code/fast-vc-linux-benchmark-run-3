e9346d6306fb5e148e0f38cefef3e2a9ccdea40c
Paul Burton <paul.burton@mips.com>
2018-06-22 20:17:43 -0700

    net: pch_gbe: Remove sw_reset_phy HAL abstraction
    For some reason the pch_gbe driver contains a struct pch_gbe_functions
    with pointers used by a HAL abstraction layer, even though there is only
    one implementation of each function.
    
    This patch removes the sw_reset_phy abstraction, which it turns out is
    never even used. Its one implementation, which is already called
    directly within the same translation unit, can therefore be made static
    and removed from the pch_gbe_phy.h header.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_phy.h
+++ drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_phy.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 s32 pch_gbe_phy_get_id(struct pch_gbe_hw *hw);
 s32 pch_gbe_phy_read_reg_miic(struct pch_gbe_hw *hw, u32 offset, u16 *data);
 s32 pch_gbe_phy_write_reg_miic(struct pch_gbe_hw *hw, u32 offset, u16 data);
-void pch_gbe_phy_sw_reset(struct pch_gbe_hw *hw);
 void pch_gbe_phy_hw_reset(struct pch_gbe_hw *hw);
 void pch_gbe_phy_power_up(struct pch_gbe_hw *hw);
 void pch_gbe_phy_power_down(struct pch_gbe_hw *hw);
