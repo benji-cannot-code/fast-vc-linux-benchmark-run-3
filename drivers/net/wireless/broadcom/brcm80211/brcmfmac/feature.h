bb2b36ef1eb86786bc0bb6397f3c5b26f9adbe24
Rafał Miłecki <rafal@milecki.pl>
2018-06-24 21:44:35 +0200

    brcmfmac: detect firmware support for monitor interface
    Many/most of firmwares support creating monitor interface but only the
    most recent ones explicitly /announce/ it using a "monitor" entry in the
    list of capabilities.
    
    Check for that entry and store internally info about monitor mode
    support using a new feature flag. Once we sort out all details of
    handling monitor interface it will be used when reporting available
    interfaces to the cfg80211.
    
    Later some fallback detecion method may be added for older firmwares.
    For now just stick to the "monitor" capability which should be 100%
    reliable.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Acked-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * MFP: 802.11w Management Frame Protection.
  * GSCAN: enhanced scan offload feature.
  * FWSUP: Firmware supplicant.
+ * MONITOR: firmware can pass monitor packets to host.
  */
 #define BRCMF_FEAT_LIST \
 	BRCMF_FEAT_DEF(MBSS) \
@@ -49,7 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	BRCMF_FEAT_DEF(WOWL_ARP_ND) \
 	BRCMF_FEAT_DEF(MFP) \
 	BRCMF_FEAT_DEF(GSCAN) \
-	BRCMF_FEAT_DEF(FWSUP)
+	BRCMF_FEAT_DEF(FWSUP) \
+	BRCMF_FEAT_DEF(MONITOR)
 
 /*
  * Quirks:
