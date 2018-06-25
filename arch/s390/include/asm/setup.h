1d4eddfd8e05a579eefd2e96bcee787f694cdaf8
Vasily Gorbik <gor@linux.ibm.com>
2018-06-25 13:59:35 +0200

    s390/boot: block uncompressed vmlinux booting attempts
    Since the plain vmlinux ELF file no longer carries all necessary parts
    for starting up (like the entry point and decompressor), add a check
    which would block boot process and encourage users to use bzImage or
    arch/s390/boot/compressed/vmlinux instead.
    
    The check relies on s390 linux entry point ABI definition, which is only
    present in bzImage and arch/s390/boot/compressed/vmlinux.
    
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Cornelia Huck <cohuck@redhat.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Reviewed-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/setup.h
+++ arch/s390/include/asm/setup.h
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/const.h>
 #include <uapi/asm/setup.h>
 
-
+#define EP_OFFSET		0x10008
+#define EP_STRING		"S390EP"
 #define PARMAREA		0x10400
 #define PARMAREA_END		0x11000
 
