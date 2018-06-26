f0aed6a7de58961ab2a146be8473411068498c53
Ajay Singh <ajay.kathat@microchip.com>
2018-06-26 11:37:11 +0530

    staging: wilc1000: handle freeing of 'key' & 'seq' data in wilc_add_rx_gtk()
    Handle freeing of memory allocated to keep 'key' & 'seq' in wilc_add_rx_gtk().
    Once completion event is received, free the memory allocated for
    to avoid missing of free in work function.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/host_interface.c
+++ drivers/staging/wilc1000/host_interface.c
@@ -1668,8 +1668,6 @@ out_wep:
 		}
 out_wpa_rx_gtk:
 		complete(&msg->work_comp);
-		kfree(hif_key->attr.wpa.key);
-		kfree(hif_key->attr.wpa.seq);
 		break;
 
 	case WPA_PTK:
@@ -2813,8 +2811,6 @@ int wilc_add_rx_gtk(struct wilc_vif *vif, const u8 *rx_gtk, u8 gtk_key_len,
 	}
 
 	wait_for_completion(&msg->work_comp);
-	kfree(msg);
-	return 0;
 
 free_key:
 	kfree(msg->body.key_info.attr.wpa.key);
