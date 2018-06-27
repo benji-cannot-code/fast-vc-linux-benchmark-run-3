6cc6ec06d9f76b41ccfd0cdc9ba949987aa3aff7
Nicolas Pitre <nicolas.pitre@linaro.org>
2018-06-26 23:56:40 -0400

    vt: preserve unicode values corresponding to screen characters
    The vt code translates UTF-8 strings into glyph index values and stores
    those glyph values directly in the screen buffer. Because there can only
    be at most 512 glyphs, it is impossible to represent most unicode
    characters, in which case a default glyph (often '?') is displayed
    instead. The original unicode value is then lost.
    
    This patch implements the basic screen buffer handling to preserve unicode
    values alongside corresponding display glyphs.  It is not activated by
    default, meaning that people not relying on that functionality won't get
    the implied overhead.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    Tested-by: Dave Mielke <Dave@mielke.cc>
    Acked-by: Adam Borowski <kilobyte@angband.pl>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/console_struct.h
+++ include/linux/console_struct.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct vt_struct;
 struct uni_pagedir;
+struct uni_screen;
 
 #define NPAR 16
 
@@ -141,6 +142,7 @@ struct vc_data {
 	struct vc_data **vc_display_fg;		/* [!] Ptr to var holding fg console for this display */
 	struct uni_pagedir *vc_uni_pagedir;
 	struct uni_pagedir **vc_uni_pagedir_loc; /* [!] Location of uni_pagedir variable for this console */
+	struct uni_screen *vc_uni_screen;	/* unicode screen content */
 	bool vc_panic_force_write; /* when oops/panic this VC can accept forced output/blanking */
 	/* additional information is in vt_kern.h */
 };
