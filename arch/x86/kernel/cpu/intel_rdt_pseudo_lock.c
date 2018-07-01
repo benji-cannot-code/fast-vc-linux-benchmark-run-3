6d66c59848f231a1a812cc9edceb3600af60bda2
Reinette Chatre <reinette.chatre@intel.com>
2018-06-30 22:03:03 -0700

    x86/intel_rdt: Fix cleanup of plr structure on error
    When a resource group enters pseudo-locksetup mode a pseudo_lock_region is
    associated with it. When the user writes to the resource group's schemata
    file the CBM of the requested pseudo-locked region is entered into the
    pseudo_lock_region struct. If any part of pseudo-lock region creation fails
    the resource group will remain in pseudo-locksetup mode with the
    pseudo_lock_region associated with it.
    
    In case of failure during pseudo-lock region creation care needs to be
    taken to ensure that the pseudo_lock_region struct associated with the
    resource group is cleared from any pseudo-locking data - especially the
    CBM. This is because the existence of a pseudo_lock_region struct with a
    CBM is significant in other areas of the code, for example, the display of
    bit_usage and initialization of a new resource group.
    
    Fix the error path of pseudo-lock region creation to ensure that the
    pseudo_lock_region struct is cleared at each error exit.
    
    Fixes: 018961ae5579 ("x86/intel_rdt: Pseudo-lock region creation/removal core")
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: fenghua.yu@intel.com
    Cc: tony.luck@intel.com
    Cc: vikas.shivappa@linux.intel.com
    Cc: gavin.hindman@intel.com
    Cc: jithu.joseph@intel.com
    Cc: dave.hansen@intel.com
    Cc: hpa@zytor.com
    Link: https://lkml.kernel.org/r/49b4782f6d204d122cee3499e642b2772a98d2b4.1530421026.git.reinette.chatre@intel.com

--- arch/x86/kernel/cpu/intel_rdt_pseudo_lock.c
+++ arch/x86/kernel/cpu/intel_rdt_pseudo_lock.c
@@ -291,6 +291,7 @@ static void pseudo_lock_region_clear(struct pseudo_lock_region *plr)
 static int pseudo_lock_region_init(struct pseudo_lock_region *plr)
 {
 	struct cpu_cacheinfo *ci;
+	int ret;
 	int i;
 
 	/* Pick the first cpu we find that is associated with the cache. */
@@ -299,7 +300,8 @@ static int pseudo_lock_region_init(struct pseudo_lock_region *plr)
 	if (!cpu_online(plr->cpu)) {
 		rdt_last_cmd_printf("cpu %u associated with cache not online\n",
 				    plr->cpu);
-		return -ENODEV;
+		ret = -ENODEV;
+		goto out_region;
 	}
 
 	ci = get_cpu_cacheinfo(plr->cpu);
@@ -313,8 +315,11 @@ static int pseudo_lock_region_init(struct pseudo_lock_region *plr)
 		}
 	}
 
+	ret = -1;
 	rdt_last_cmd_puts("unable to determine cache line size\n");
-	return -1;
+out_region:
+	pseudo_lock_region_clear(plr);
+	return ret;
 }
 
 /**
@@ -366,16 +371,23 @@ static int pseudo_lock_region_alloc(struct pseudo_lock_region *plr)
 	 */
 	if (plr->size > KMALLOC_MAX_SIZE) {
 		rdt_last_cmd_puts("requested region exceeds maximum size\n");
-		return -E2BIG;
+		ret = -E2BIG;
+		goto out_region;
 	}
 
 	plr->kmem = kzalloc(plr->size, GFP_KERNEL);
 	if (!plr->kmem) {
 		rdt_last_cmd_puts("unable to allocate memory\n");
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto out_region;
 	}
 
-	return 0;
+	ret = 0;
+	goto out;
+out_region:
+	pseudo_lock_region_clear(plr);
+out:
+	return ret;
 }
 
 /**
