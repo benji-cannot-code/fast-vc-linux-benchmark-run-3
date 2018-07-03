d1835eeb9e451e82a43e71961ccaab5798e89f30
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2018-07-03 19:46:57 +0900

    usb: host: xhci-rcar: Simplify getting the firmware name for R-Car Gen3
    This patch simplifies getting the firmware name for R-Car Gen3.
    Almost all R-Car Gen3 USB3.0 Host controllers use "V3". But,
    r8a7795 ES1.x SoCs only should use "V2". Since the xhci-plat already
    has the firmware_name of R-Car Gen3 as "V3", the xhci-rcar doesn't
    need some members of rcar_quirks_match.
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-rcar.c
+++ drivers/usb/host/xhci-rcar.c
@@ -18,9 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xhci-rcar.h"
 
 /*
-* - The V3 firmware is for r8a7796 (with good performance) and r8a7795 es2.0
-*   or later.
-* - The V2 firmware can be used on both r8a7795 (es1.x) and r8a7796.
+* - The V3 firmware is for almost all R-Car Gen3 (except r8a7795 ES1.x)
+* - The V2 firmware is for r8a7795 ES1.x.
 * - The V2 firmware is possible to use on R-Car Gen2. However, the V2 causes
 *   performance degradation. So, this driver continues to use the V1 if R-Car
 *   Gen2.
@@ -75,18 +74,6 @@ static const struct soc_device_attribute rcar_quirks_match[]  = {
 	{
 		.soc_id = "r8a7795", .revision = "ES1.*",
 		.data = (void *)RCAR_XHCI_FIRMWARE_V2,
-	},
-	{
-		.soc_id = "r8a7795",
-		.data = (void *)RCAR_XHCI_FIRMWARE_V3,
-	},
-	{
-		.soc_id = "r8a7796",
-		.data = (void *)RCAR_XHCI_FIRMWARE_V3,
-	},
-	{
-		.soc_id = "r8a77965",
-		.data = (void *)RCAR_XHCI_FIRMWARE_V3,
 	},
 	{ /* sentinel */ },
 };
