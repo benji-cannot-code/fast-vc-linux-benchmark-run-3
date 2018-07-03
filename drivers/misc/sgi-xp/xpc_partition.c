06172185b6c7cfef9b1caf969381b0ed0b6510d3
Colin Ian King <colin.king@canonical.com>
2018-07-03 08:23:05 +0100

    sgi-xp: remove redundant pointers ch and rp
    The pointers ch and rp are set but are never used hence they are
    redundant and can be removed.
    
    Cleans up clang warnings:
    warning: variable 'ch' set but not used [-Wunused-but-set-variable]
    warning: variable 'rp' set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/sgi-xp/xpc_partition.c
+++ drivers/misc/sgi-xp/xpc_partition.c
@@ -416,7 +416,6 @@ xpc_discovery(void)
 	int region_size;
 	int max_regions;
 	int nasid;
-	struct xpc_rsvd_page *rp;
 	unsigned long *discovered_nasids;
 	enum xp_retval ret;
 
@@ -432,8 +431,6 @@ xpc_discovery(void)
 		kfree(remote_rp_base);
 		return;
 	}
-
-	rp = (struct xpc_rsvd_page *)xpc_rsvd_page;
 
 	/*
 	 * The term 'region' in this context refers to the minimum number of
