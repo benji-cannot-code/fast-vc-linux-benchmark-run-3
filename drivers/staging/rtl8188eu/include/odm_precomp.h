6ce9c2c6f6ac1466ff8f769681b7917d62d64dcf
Michael Straube <straube.linux@gmail.com>
2018-07-04 13:37:41 +0200

    staging: rtl8188eu: rename Hal8188ERateAdaptive
    Rename header and source file to avoid CamelCase.
    
    Hal8188ERateAdaptive.h -> hal8188e_rate_adaptive.h
    Hal8188ERateAdaptive.c -> hal8188e_rate_adaptive.c
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm_precomp.h
+++ drivers/staging/rtl8188eu/include/odm_precomp.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "odm_debug.h"
 #include "odm_RegDefine11N.h"
 
-#include "Hal8188ERateAdaptive.h"/* for  RA,Power training */
+#include "hal8188e_rate_adaptive.h" /* for RA,Power training */
 #include "rtl8188e_hal.h"
 
 #include "odm_reg.h"
