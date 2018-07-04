42268e27d831532fd7161a0b880863bc23d7ad94
Jerome Brunet <jbrunet@baylibre.com>
2018-07-04 18:54:57 +0200

    clk: meson: expose GEN_CLK clkid
    Expose GEN_CLK clock id
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- include/dt-bindings/clock/axg-clkc.h
+++ include/dt-bindings/clock/axg-clkc.h
@@ -72,5 +72,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_PCIE_CML_EN0			79
 #define CLKID_PCIE_CML_EN1			80
 #define CLKID_MIPI_ENABLE			81
+#define CLKID_GEN_CLK				84
 
 #endif /* __AXG_CLKC_H */
