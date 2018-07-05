8a7b75b1476610aec86a857a3cf6a7228b9087f4
Boris Brezillon <boris.brezillon@bootlin.com>
2018-07-05 12:27:29 +0200

    mtd: rawnand: Remove forward declaration of device_node
    struct device_node is defined in linux/of.h. Let's include this file
    instead of having a forward declaration of this struct.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- include/linux/mtd/rawnand.h
+++ include/linux/mtd/rawnand.h
@@ -22,10 +22,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/flashchip.h>
 #include <linux/mtd/bbm.h>
+#include <linux/of.h>
 #include <linux/types.h>
 
 struct nand_flash_dev;
-struct device_node;
 
 /* Scan and identify a NAND device */
 int nand_scan_with_ids(struct mtd_info *mtd, int max_chips,
