ce7aecb431c2f6a3c6d744614d53dc9a57064df8
Ville Syrjälä <ville.syrjala@linux.intel.com>
2018-07-05 19:43:55 +0300

    drm/i915: Pass hpd_pin to long_pulse_detect()
    We're doing a pointless translation from hpd_pin to port simply for
    passing the thing to long_pulse_detect(). Let's pass the hpd_pin
    directly instead.
    
    This removes the assumption that the hpd_pin and port always
    match. The only other place where we make that assumption anymore
    is intel_hpd_pin_default() and that's fine as it's what determines
    the relationship between the two. If we ever get hardware where
    the hpd pins are wired in more interesting ways it should be
    trivial to handle from now on.
    
    This should also fix the IS_CNL_WITH_PORT_F() case as that mapped
    pin E back to port F and passed that to
    spt_port_hotplug2_long_detect() which would always return false
    for port F. Now that we pass in pin E directly it'll actually
    do the right thing.
    
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Fixes: cf53902f48c3 ("drm/i915/cnl: Add HPD support for Port F.")
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180705164357.28512-7-ville.syrjala@linux.intel.com
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/intel_hotplug.c
+++ drivers/gpu/drm/i915/intel_hotplug.c
@@ -78,37 +78,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 /**
- * intel_hpd_port - return port hard associated with certain pin.
- * @dev_priv: private driver data pointer
- * @pin: the hpd pin to get associated port
- *
- * Return port that is associatade with @pin and PORT_NONE if no port is
- * hard associated with that @pin.
- */
-enum port intel_hpd_pin_to_port(struct drm_i915_private *dev_priv,
-				enum hpd_pin pin)
-{
-	switch (pin) {
-	case HPD_PORT_A:
-		return PORT_A;
-	case HPD_PORT_B:
-		return PORT_B;
-	case HPD_PORT_C:
-		return PORT_C;
-	case HPD_PORT_D:
-		return PORT_D;
-	case HPD_PORT_E:
-		if (IS_CNL_WITH_PORT_F(dev_priv))
-			return PORT_F;
-		return PORT_E;
-	case HPD_PORT_F:
-		return PORT_F;
-	default:
-		return PORT_NONE; /* no port for this pin */
-	}
-}
-
-/**
  * intel_hpd_pin_default - return default pin associated with certain port.
  * @dev_priv: private driver data pointer
  * @port: the hpd port to get associated pin
