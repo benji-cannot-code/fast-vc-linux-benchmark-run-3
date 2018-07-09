026c730cae568507040d4384634aa471e789bc0e
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2018-07-09 11:53:31 +0900

    iommu/ipmmu-vmsa: IMUCTRn.TTSEL needs a special usage on R-Car Gen3
    The TTSEL bit of IMUCTRn register of R-Car Gen3 needs to be set
    unused MMU context number even if uTLBs are disabled
    (The MMUEN bit of IMUCTRn register = 0).
    Since initial values of IMUCTRn.TTSEL on all IPMMU-domains are 0,
    this patch adds a new feature "reserved_context" to reserve IPMMU
    context number 0 as the unused MMU context.
    
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/ipmmu-vmsa.c
+++ drivers/iommu/ipmmu-vmsa.c
@@ -48,6 +48,7 @@ struct ipmmu_features {
 	unsigned int number_of_contexts;
 	bool setup_imbuscr;
 	bool twobit_imttbcr_sl0;
+	bool reserved_context;
 };
 
 struct ipmmu_vmsa_device {
@@ -926,6 +927,7 @@ static const struct ipmmu_features ipmmu_features_default = {
 	.number_of_contexts = 1, /* software only tested with one context */
 	.setup_imbuscr = true,
 	.twobit_imttbcr_sl0 = false,
+	.reserved_context = false,
 };
 
 static const struct ipmmu_features ipmmu_features_rcar_gen3 = {
@@ -934,6 +936,7 @@ static const struct ipmmu_features ipmmu_features_rcar_gen3 = {
 	.number_of_contexts = 8,
 	.setup_imbuscr = false,
 	.twobit_imttbcr_sl0 = true,
+	.reserved_context = true,
 };
 
 static const struct of_device_id ipmmu_of_ids[] = {
@@ -1039,6 +1042,11 @@ static int ipmmu_probe(struct platform_device *pdev)
 		}
 
 		ipmmu_device_reset(mmu);
+
+		if (mmu->features->reserved_context) {
+			dev_info(&pdev->dev, "IPMMU context 0 is reserved\n");
+			set_bit(0, mmu->ctx);
+		}
 	}
 
 	/*
