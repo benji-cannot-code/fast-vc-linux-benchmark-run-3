614c9057f3108c0b89840e126b874a3fc1ab0d2a
Likun Gao <Likun.Gao@amd.com>
2018-07-10 20:17:13 +0800

    drm/amdgpu: add picasso support for vcn
    Add vcn support for picasso.
    
    Signed-off-by: Likun Gao <Likun.Gao@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vcn.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vcn.c
@@ -43,8 +43,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Firmware Names */
 #define FIRMWARE_RAVEN		"amdgpu/raven_vcn.bin"
+#define FIRMWARE_PICASSO	"amdgpu/picasso_vcn.bin"
 
 MODULE_FIRMWARE(FIRMWARE_RAVEN);
+MODULE_FIRMWARE(FIRMWARE_PICASSO);
 
 static void amdgpu_vcn_idle_work_handler(struct work_struct *work);
 
@@ -61,6 +63,9 @@ int amdgpu_vcn_sw_init(struct amdgpu_device *adev)
 	switch (adev->asic_type) {
 	case CHIP_RAVEN:
 		fw_name = FIRMWARE_RAVEN;
+		break;
+	case CHIP_PICASSO:
+		fw_name = FIRMWARE_PICASSO;
 		break;
 	default:
 		return -EINVAL;
