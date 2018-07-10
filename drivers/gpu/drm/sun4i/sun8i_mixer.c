312aece37c7b339cf77b647781eda32147e623f0
Jernej Skrabec <jernej.skrabec@siol.net>
2018-07-10 22:35:02 +0200

    drm/sun4i: mixer: Order includes alphabetically
    Includes are not alphabetically ordered.
    
    Reorder them.
    
    Reviewed-by: Chen-Yu Tsai <wens@csie.org>
    Signed-off-by: Jernej Skrabec <jernej.skrabec@siol.net>
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180710203511.18454-10-jernej.skrabec@siol.net

--- drivers/gpu/drm/sun4i/sun8i_mixer.c
+++ drivers/gpu/drm/sun4i/sun8i_mixer.c
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/component.h>
 #include <linux/dma-mapping.h>
-#include <linux/reset.h>
 #include <linux/of_device.h>
+#include <linux/reset.h>
 
 #include "sun4i_drv.h"
 #include "sun8i_mixer.h"
