94b557f5d07b791bc5d032b1872a2afd72926a80
Russell King <rmk+kernel@armlinux.org.uk>
2018-07-11 10:32:17 +0100

    ARM: net: bpf: use ldr instructions with shifted rm register
    Rather than pre-shifting the rm register for the ldr in the tail call,
    shift it in the load instruction.  This eliminates one unnecessary
    instruction.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- arch/arm/net/bpf_jit_32.h
+++ arch/arm/net/bpf_jit_32.h
@@ -189,6 +189,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARM_LDR_R(rt, rn, rm)	(ARM_INST_LDR_R | ARM_INST_LDST__U \
 				 | (rt) << 12 | (rn) << 16 \
 				 | (rm))
+#define ARM_LDR_R_SI(rt, rn, rm, type, imm) \
+				(ARM_INST_LDR_R | ARM_INST_LDST__U \
+				 | (rt) << 12 | (rn) << 16 \
+				 | (imm) << 7 | (type) << 5 | (rm))
 #define ARM_LDRB_R(rt, rn, rm)	(ARM_INST_LDRB_R | ARM_INST_LDST__U \
 				 | (rt) << 12 | (rn) << 16 \
 				 | (rm))
