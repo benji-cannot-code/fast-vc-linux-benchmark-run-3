f3755fe01400b6358b12668887d4094a641b60c9
Maxime Chevallier <maxime.chevallier@bootlin.com>
2018-07-12 13:54:16 +0200

    net: mvpp2: use only one rx queue per port per CPU
    The number of receive queue per port is :
     - MVPP2_DEFAULT_RXQ if in single queue mode
     - MVPP2_DEFAULT_RXQ * num_possible_cpus if in multi queue mode
    
    with MVPP2_DEFAULT_RXQ = 4.
    
    However, we don't use the extra rx queues at the moment, we really only
    need one per port per CPU, until some more advanced classification rules
    are implemented.
    
    Suggested-by: Stefan Chulski <stefanc@marvell.com>
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2.h
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2.h
@@ -501,7 +501,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MVPP2_MAX_SKB_DESCS		(MVPP2_MAX_TSO_SEGS * 2 + MAX_SKB_FRAGS)
 
 /* Dfault number of RXQs in use */
-#define MVPP2_DEFAULT_RXQ		4
+#define MVPP2_DEFAULT_RXQ		1
 
 /* Max number of Rx descriptors */
 #define MVPP2_MAX_RXD_MAX		1024
