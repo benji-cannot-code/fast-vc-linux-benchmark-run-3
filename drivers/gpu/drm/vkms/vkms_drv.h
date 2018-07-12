b9673452467c1238cbb1ef72a1d684d8be152a10
Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
2018-07-11 23:02:26 -0300

    drm/vkms: Add vblank events simulated by hrtimers
    This commit adds regular vblank events simulated through hrtimers, which
    is a feature required by VKMS to mimic real hardware. Additionally, all
    the vblank event send after pageflip is kept in the atomic_flush
    function.
    
    Changes since V1:
     - Compute the vblank timer interval per interruption
     Ville Syrjälä and Daniel Vetter:
     - Removes hardcoded vblank interval to get it from user space
    
    Changes since V2:
     Chris Wilson
     - Removes unnecessary algorithm to compute the next period
     Daniel Vetter:
     - Uses drm_calc_timestamping_constants to get the vblank interval
       instead of calculating it manually
     - Adds disable_vblank helper that turns of crtc
     - Simplifies implementation by using drm_crtc_arm_vblank_event
     - Replaces the code in atomic_begin to atomic_flush
     - Removes unnecessary field in vkms_output
    
    Changes since V3:
     Daniel Vetter:
     - Squash "drm/vkms: Add atomic helpers functions" into the commit that
       handling vblank events simulated by hrtimers
    
    Signed-off-by: Rodrigo Siqueira <rodrigosiqueiramelo@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/7709bba40782ec06332d57fff337797b272581fc.1531359228.git.rodrigosiqueiramelo@gmail.com

--- drivers/gpu/drm/vkms/vkms_drv.h
+++ drivers/gpu/drm/vkms/vkms_drv.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_encoder.h>
+#include <linux/hrtimer.h>
 
 #define XRES_MIN    32
 #define YRES_MIN    32
@@ -24,6 +25,9 @@ struct vkms_output {
 	struct drm_crtc crtc;
 	struct drm_encoder encoder;
 	struct drm_connector connector;
+	struct hrtimer vblank_hrtimer;
+	ktime_t period_ns;
+	struct drm_pending_vblank_event *event;
 };
 
 struct vkms_device {
@@ -38,8 +42,19 @@ struct vkms_gem_object {
 	struct page **pages;
 };
 
+#define drm_crtc_to_vkms_output(target) \
+	container_of(target, struct vkms_output, crtc)
+
+#define drm_device_to_vkms_device(target) \
+	container_of(target, struct vkms_device, drm)
+
+/* CRTC */
 int vkms_crtc_init(struct drm_device *dev, struct drm_crtc *crtc,
 		   struct drm_plane *primary, struct drm_plane *cursor);
+
+bool vkms_get_vblank_timestamp(struct drm_device *dev, unsigned int pipe,
+			       int *max_error, ktime_t *vblank_time,
+			       bool in_vblank_irq);
 
 int vkms_output_init(struct vkms_device *vkmsdev);
 
