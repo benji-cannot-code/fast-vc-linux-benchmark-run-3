e1a13d41698c22f222570047d21baac0cfdffbf0
Maxime Chevallier <maxime.chevallier@bootlin.com>
2018-07-12 13:54:18 +0200

    net: mvpp2: fix RSS register definitions
    There is no RSS_TABLE register in PPv2 Controller. The register 0x1510
    which was specified is actually named "RSS_HASH_SEL", but isn't used by
    this driver at all.
    
    Based on how this register was used, it should have been the
    RXQ2RSS_TABLE register, which allows to select the RSS table that will
    be used for the incoming packet.
    
    The RSS_TABLE_POINTER is actually a field of this RXQ2RSS_TABLE
    register.
    
    Since RSS tables are actually not used by the driver for now, this
    commit does not fix a runtime bug.
    
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2.h
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2.h
@@ -74,9 +74,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define     MVPP22_RSS_INDEX_TABLE_ENTRY(idx)	(idx)
 #define     MVPP22_RSS_INDEX_TABLE(idx)		((idx) << 8)
 #define     MVPP22_RSS_INDEX_QUEUE(idx)		((idx) << 16)
-#define MVPP22_RSS_TABLE_ENTRY			0x1508
-#define MVPP22_RSS_TABLE			0x1510
+#define MVPP22_RXQ2RSS_TABLE			0x1504
 #define     MVPP22_RSS_TABLE_POINTER(p)		(p)
+#define MVPP22_RSS_TABLE_ENTRY			0x1508
 #define MVPP22_RSS_WIDTH			0x150c
 
 /* Classifier Registers */
