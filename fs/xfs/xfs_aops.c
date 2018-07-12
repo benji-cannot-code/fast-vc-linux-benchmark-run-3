1ed1ea224b60dcae1bc472a334f91d0c0384011f
Christoph Hellwig <hch@lst.de>
2018-07-11 22:25:58 -0700

    xfs: do not set the page uptodate in xfs_writepage_map
    We already track the page uptodate status based on the buffer uptodate
    status, which is updated whenever reading or zeroing blocks.
    
    This code has been there since commit a ptool commit in 2002, which
    claims to:
    
        "merge" the 2.4 fsx fix for block size < page size to 2.5.  This needed
        major changes to actually fit.
    
    and isn't present in other writepage implementations.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_aops.c
+++ fs/xfs/xfs_aops.c
@@ -850,7 +850,6 @@ xfs_writepage_map(
 	uint64_t		offset;
 	int			error = 0;
 	int			count = 0;
-	int			uptodate = 1;
 	unsigned int		new_type;
 
 	bh = head = page_buffers(page);
@@ -858,8 +857,6 @@ xfs_writepage_map(
 	do {
 		if (offset >= end_offset)
 			break;
-		if (!buffer_uptodate(bh))
-			uptodate = 0;
 
 		/*
 		 * set_page_dirty dirties all buffers in a page, independent
@@ -922,9 +919,6 @@ xfs_writepage_map(
 		}
 
 	} while (offset += len, ((bh = bh->b_this_page) != head));
-
-	if (uptodate && bh == head)
-		SetPageUptodate(page);
 
 	ASSERT(wpc->ioend || list_empty(&submit_list));
 
