29f9cf765271a83ea78eb7b9903b855f6cca06da
Gu Jinxiang <gujx@cn.fujitsu.com>
2018-07-12 14:23:15 +0800

    btrfs: make fs_devices a local variable in btrfs_parse_early_options
    fs_devices is always passed to btrfs_scan_one_device which overrides it.
    In the call stack below fs_devices is passed to btrfs_scan_one_device
    from btrfs_mount_root.  In btrfs_mount_root the output fs_devices of
    this call stack is not used.
    
    btrfs_mount_root
      btrfs_parse_early_options
        btrfs_scan_one_device
    
    So, it is not necessary to pass fs_devices from btrfs_mount_root, using
    a local variable in btrfs_parse_early_options is enough.
    
    Signed-off-by: Gu Jinxiang <gujx@cn.fujitsu.com>
    Reviewed-by: Anand Jain <Anand.Jain@oracle.com>
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -885,10 +885,11 @@ out:
  * only when we need to allocate a new super block.
  */
 static int btrfs_parse_early_options(const char *options, fmode_t flags,
-		void *holder, struct btrfs_fs_devices **fs_devices)
+				     void *holder)
 {
 	substring_t args[MAX_OPT_ARGS];
 	char *device_name, *opts, *orig, *p;
+	struct btrfs_fs_devices *fs_devices = NULL;
 	int error = 0;
 
 	lockdep_assert_held(&uuid_mutex);
@@ -919,7 +920,7 @@ static int btrfs_parse_early_options(const char *options, fmode_t flags,
 				goto out;
 			}
 			error = btrfs_scan_one_device(device_name,
-					flags, holder, fs_devices);
+					flags, holder, &fs_devices);
 			kfree(device_name);
 			if (error)
 				goto out;
@@ -1555,7 +1556,7 @@ static struct dentry *btrfs_mount_root(struct file_system_type *fs_type,
 	}
 
 	mutex_lock(&uuid_mutex);
-	error = btrfs_parse_early_options(data, mode, fs_type, &fs_devices);
+	error = btrfs_parse_early_options(data, mode, fs_type);
 	if (error) {
 		mutex_unlock(&uuid_mutex);
 		goto error_fs_info;
