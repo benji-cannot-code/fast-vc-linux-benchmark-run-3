62ccfc432598f992a09e19b173359b2e8cfbb9a2
John Whitmore <johnfwhitmore@gmail.com>
2018-07-13 12:22:59 +0100

    staging:rtl8192u: remove typedef of enumeration SwChnlCmdID - Style
    To clear a checkpatch issue removed the typedef of the enumeration SwChnlCmdID
    this should not impact runtime code as it's only a coding style change.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r819xU_phy.h
+++ drivers/staging/rtl8192u/r819xU_phy.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_RFDEPENDCMD_CNT 16
 #define MAX_POSTCMD_CNT 16
 
-typedef enum _SwChnlCmdID {
+enum SwChnlCmdID {
 	CmdID_End,
 	CmdID_SetTxPowerLevel,
 	CmdID_BBRegWrite10,
@@ -16,15 +16,15 @@ typedef enum _SwChnlCmdID {
 	CmdID_WritePortUshort,
 	CmdID_WritePortUchar,
 	CmdID_RF_WriteReg,
-} SwChnlCmdID;
+};
 
 /* -----------------------Define structure---------------------- */
 /* 1. Switch channel related */
 typedef struct _SwChnlCmd {
-	SwChnlCmdID	CmdID;
-	u32		Para1;
-	u32		Para2;
-	u32		msDelay;
+	enum SwChnlCmdID	CmdID;
+	u32		        Para1;
+	u32		        Para2;
+	u32		        msDelay;
 } __packed SwChnlCmd;
 
 extern u32 rtl819XMACPHY_Array_PG[];
