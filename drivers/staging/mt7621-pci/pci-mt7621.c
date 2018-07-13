1d06c4145bc46e49450806a55bf4532a4b94b577
Oliver Hübers <oliver.huebers@web.de>
2018-07-13 15:41:03 +0200

    staging: mt7621-pci: Include preferred headers
    This fixes the following checkpatch warnings:
    Use #include <linux/pci.h> instead of <asm/pci.h>
    Use #include <linux/io.h> instead of <asm/io.h>
    
    Signed-off-by: Oliver Hübers <oliver.huebers@web.de>
    Signed-off-by: Vanessa Borgmann <nessa.x@web.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-pci/pci-mt7621.c
+++ drivers/staging/mt7621-pci/pci-mt7621.c
@@ -45,8 +45,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/version.h>
-#include <asm/pci.h>
-#include <asm/io.h>
+#include <linux/pci.h>
+#include <linux/io.h>
 #include <asm/mips-cm.h>
 #include <linux/init.h>
 #include <linux/module.h>
