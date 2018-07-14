73bfb7f0aa607496eb774eaa8711b2c8e6ea6f16
Maxime Chevallier <maxime.chevallier@bootlin.com>
2018-07-14 13:29:27 +0200

    net: mvpp2: debugfs: add entries for classifier flows
    The classifier configuration for RSS is quite complex, with several
    lookup tables being used. This commit adds useful info in debugfs to
    see how the different tables are configured :
    
    Added 2 new entries in the per-port directory :
    
      - .../eth0/default_rxq : The default rx queue on that port
      - .../eth0/rss_enable : Indicates if RSS is enabled in the C2 entry
    
    Added the 'flows' directory :
    
      It contains one entry per sub-flow. a 'sub-flow' is a unique path from
      Header Parser to the flow table. Multiple sub-flows can point to the
      same 'flow' (each flow has an id from 8 to 29, which is its index in the
      Lookup Id table) :
    
      - .../flows/00/...
                 /01/...
                 ...
                 /51/id : The flow id. There are 21 unique flows. There's one
                           flow per combination of the following parameters :
                           - L4 protocol (TCP, UDP, none)
                           - L3 protocol (IPv4, IPv6)
                           - L3 parameters (Fragmented or not)
                           - L2 parameters (Vlan tag presence or not)
                  .../type : The flow type. This is an even higher level flow,
                             that we manipulate with ethtool. It can be :
                             "udp4" "tcp4" "udp6" "tcp6" "ipv4" "ipv6" "other".
                  .../eth0/...
                  .../eth1/engine : The hash generation engine used for this
    	                        flow on the given port
                      .../hash_opts : The hash generation options indicating on
                                      what data we base the hash (vlan tag, src
                                      IP, src port, etc.)
    
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2.h
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2.h
@@ -133,8 +133,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MVPP22_CLS_C2_ATTR0			0x1b64
 #define     MVPP22_CLS_C2_ATTR0_QHIGH(qh)	(((qh) & 0x1f) << 24)
 #define     MVPP22_CLS_C2_ATTR0_QHIGH_MASK	0x1f
+#define     MVPP22_CLS_C2_ATTR0_QHIGH_OFFS	24
 #define     MVPP22_CLS_C2_ATTR0_QLOW(ql)	(((ql) & 0x7) << 21)
 #define     MVPP22_CLS_C2_ATTR0_QLOW_MASK	0x7
+#define     MVPP22_CLS_C2_ATTR0_QLOW_OFFS	21
 #define MVPP22_CLS_C2_ATTR1			0x1b68
 #define MVPP22_CLS_C2_ATTR2			0x1b6c
 #define     MVPP22_CLS_C2_ATTR2_RSS_EN		BIT(30)
