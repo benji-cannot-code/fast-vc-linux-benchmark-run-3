25630605ebe9572365dcd74124fc8f9c8ba6ef8c
Lance Shelton <lance.shelton@hammerspace.com>
2018-07-16 13:05:36 -0400

    Fix error code in nfs_lookup_verify_inode()
    Return -ESTALE to force a lookup when the file has no more links
    
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -1039,7 +1039,7 @@ int nfs_lookup_verify_inode(struct inode *inode, unsigned int flags)
 	if (flags & LOOKUP_REVAL)
 		goto out_force;
 out:
-	return (inode->i_nlink == 0) ? -ENOENT : 0;
+	return (inode->i_nlink == 0) ? -ESTALE : 0;
 out_force:
 	if (flags & LOOKUP_RCU)
 		return -ECHILD;
