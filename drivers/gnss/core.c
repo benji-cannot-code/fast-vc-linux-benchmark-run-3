4c77ee89445482e15f63702c8fa733e16a02d2bc
Dan Carpenter <dan.carpenter@oracle.com>
2018-07-16 12:42:03 +0200

    gnss: fix potential error pointer dereference
    The gnss_allocate_device() function returns a mix of NULL and error
    pointers on error.  It should only return one or the other.  Since the
    callers both check for NULL, I've modified it to return NULL on error.
    
    Fixes: 2b6a44035143 ("gnss: add GNSS receiver subsystem")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gnss/core.c
+++ drivers/gnss/core.c
@@ -236,7 +236,7 @@ struct gnss_device *gnss_allocate_device(struct device *parent)
 	id = ida_simple_get(&gnss_minors, 0, GNSS_MINORS, GFP_KERNEL);
 	if (id < 0) {
 		kfree(gdev);
-		return ERR_PTR(id);
+		return NULL;
 	}
 
 	gdev->id = id;
@@ -271,7 +271,7 @@ struct gnss_device *gnss_allocate_device(struct device *parent)
 err_put_device:
 	put_device(dev);
 
-	return ERR_PTR(-ENOMEM);
+	return NULL;
 }
 EXPORT_SYMBOL_GPL(gnss_allocate_device);
 
