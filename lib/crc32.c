ecb97c96a741c1f18a8f2c57fb54736782a71c7a
Krzysztof Kozlowski <krzk@kernel.org>
2018-07-17 18:05:36 +0200

    lib/crc: Move polynomial definition to separate header
    Allow other drivers and parts of kernel to use the same define for
    CRC32 polynomial, instead of duplicating it in many places.  This code
    does not bring any functional changes, except moving existing code.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- lib/crc32.c
+++ lib/crc32.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* see: Documentation/crc32.txt for a description of algorithms */
 
 #include <linux/crc32.h>
+#include <linux/crc32poly.h>
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/sched.h>
