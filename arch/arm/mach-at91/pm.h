299e12bcb9fd0cb3c61f0345d463971379be7f60
Wenyou Yang <wenyou.yang@atmel.com>
2018-07-17 11:26:55 +0300

    ARM: at91: pm: Add ULP1 mode support
    In the ULP1 mode, in order to achieve the lowest power consumption
    with the system in retention mode and be able to resume on the wake
    up events, all the clocks are shut off, inclusive the embedded 12MHz
    RC oscillator, and the number of wake up sources is limited as well.
    When the wake up event is asserted, the embedded 12MHz RC oscillator
    restarts automatically.
    
    The ULP1 (Ultra Low-power mode 1) is introduced by SAMA5D2.
    
    The previous size of pm_suspend.o was 2148 bytes. With the addition of
    ULP1 mode the new size of pm_suspend.o raised at 2456 bytes.
    
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
    [claudiu.beznea@microchip.com: aligned with 4.18-rc1]
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- arch/arm/mach-at91/pm.h
+++ arch/arm/mach-at91/pm.h
@@ -24,7 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define	AT91_PM_STANDBY		0x00
 #define AT91_PM_ULP0		0x01
-#define	AT91_PM_BACKUP		0x02
+#define AT91_PM_ULP1		0x02
+#define	AT91_PM_BACKUP		0x03
 
 #ifndef __ASSEMBLY__
 struct at91_pm_data {
