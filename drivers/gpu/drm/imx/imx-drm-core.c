383830b70334ae2d962f2154c3c4c81ebcb7a2c6
Leonard Crestez <leonard.crestez@nxp.com>
2018-07-17 15:11:28 +0300

    drm/imx: Remove unused field imx_drm_device.pipes
    This has been unused since commit 44b460cfe554 ("drm: imx: remove struct
    imx_drm_crtc and imx_drm_crtc_helper_funcs")
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/drm/imx/imx-drm-core.c
+++ drivers/gpu/drm/imx/imx-drm-core.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct imx_drm_device {
 	struct drm_device			*drm;
-	unsigned int				pipes;
 	struct drm_atomic_state			*state;
 };
 
