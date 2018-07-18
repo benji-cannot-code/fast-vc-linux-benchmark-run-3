982c6e04b3add601f9eaed6f1f0482be85bc2294
Paolo Bonzini <pbonzini@redhat.com>
2018-07-18 18:49:01 +0200

    KVM: x86: do not load vmcs12 pages while still in SMM
    If the vCPU enters system management mode while running a nested guest,
    RSM starts processing the vmentry while still in SMM.  In that case,
    however, the pages pointed to by the vmcs12 might be incorrectly
    loaded from SMRAM.  To avoid this, delay the handling of the pages
    until just before the next vmentry.  This is done with a new request
    and a new entry in kvm_x86_ops, which we will be able to reuse for
    nested VMX state migration.
    
    Extracted from a patch by Jim Mattson and KarimAllah Ahmed.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KVM_REQ_HV_EXIT			KVM_ARCH_REQ(21)
 #define KVM_REQ_HV_STIMER		KVM_ARCH_REQ(22)
 #define KVM_REQ_LOAD_EOI_EXITMAP	KVM_ARCH_REQ(23)
+#define KVM_REQ_GET_VMCS12_PAGES	KVM_ARCH_REQ(24)
 
 #define CR0_RESERVED_BITS                                               \
 	(~(unsigned long)(X86_CR0_PE | X86_CR0_MP | X86_CR0_EM | X86_CR0_TS \
@@ -1085,6 +1086,8 @@ struct kvm_x86_ops {
 	void (*cancel_hv_timer)(struct kvm_vcpu *vcpu);
 
 	void (*setup_mce)(struct kvm_vcpu *vcpu);
+
+	void (*get_vmcs12_pages)(struct kvm_vcpu *vcpu);
 
 	int (*smi_allowed)(struct kvm_vcpu *vcpu);
 	int (*pre_enter_smm)(struct kvm_vcpu *vcpu, char *smstate);
