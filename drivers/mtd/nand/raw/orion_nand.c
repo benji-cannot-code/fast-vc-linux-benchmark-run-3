cc5c6c589166bf3467c8b2b161bc811a2eb93007
Boris Brezillon <boris.brezillon@bootlin.com>
2018-07-18 10:56:50 +0200

    mtd: rawnand: orion: Avoid direct inclusion of asm headers
    Include linux/sizes.h instead of asm/sizes.h to make code completely
    arch independent.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/raw/orion_nand.c
+++ drivers/mtd/nand/raw/orion_nand.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/io.h>
-#include <asm/sizes.h>
+#include <linux/sizes.h>
 #include <linux/platform_data/mtd-orion_nand.h>
 
 struct orion_nand_info {
