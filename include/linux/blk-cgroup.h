62260f8e12ba49e87f938df04a229da6522ed08c
Tejun Heo <tj@kernel.org>
2018-07-18 04:47:41 -0700

    blkcg: Track DISCARD statistics and output them in cgroup io.stat
    Add tracking of REQ_OP_DISCARD ios to the per-cgroup io.stat.  Two
    fields, dbytes and dios, to respectively count the total bytes and
    number of discards are added.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Andy Newell <newella@fb.com>
    Cc: Michael Callahan <michaelcallahan@fb.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/blk-cgroup.h
+++ include/linux/blk-cgroup.h
@@ -36,6 +36,7 @@ enum blkg_rwstat_type {
 	BLKG_RWSTAT_WRITE,
 	BLKG_RWSTAT_SYNC,
 	BLKG_RWSTAT_ASYNC,
+	BLKG_RWSTAT_DISCARD,
 
 	BLKG_RWSTAT_NR,
 	BLKG_RWSTAT_TOTAL = BLKG_RWSTAT_NR,
@@ -650,7 +651,9 @@ static inline void blkg_rwstat_add(struct blkg_rwstat *rwstat,
 {
 	struct percpu_counter *cnt;
 
-	if (op_is_write(op))
+	if (op_is_discard(op))
+		cnt = &rwstat->cpu_cnt[BLKG_RWSTAT_DISCARD];
+	else if (op_is_write(op))
 		cnt = &rwstat->cpu_cnt[BLKG_RWSTAT_WRITE];
 	else
 		cnt = &rwstat->cpu_cnt[BLKG_RWSTAT_READ];
