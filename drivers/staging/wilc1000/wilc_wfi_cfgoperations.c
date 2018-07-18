8c2b575a1db51d78ac4232b460eb327d73efb145
Ajay Singh <ajay.kathat@microchip.com>
2018-07-19 04:15:10 +0530

    staging: wilc1000: remove unnecessary blank line between variable declaration
    Cleanup patch to remove the unnecessary blank line between variables
    declaration inside the function.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Reviewed-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
@@ -1251,7 +1251,6 @@ static int del_pmksa(struct wiphy *wiphy, struct net_device *netdev,
 {
 	u32 i;
 	s32 ret = 0;
-
 	struct wilc_priv *priv = wiphy_priv(wiphy);
 
 	for (i = 0; i < priv->pmkid_list.numpmkid; i++)	{
@@ -1316,7 +1315,6 @@ static inline void wilc_wfi_cfg_parse_ch_attr(u8 *buf, u8 ch_list_attr_idx,
 static void wilc_wfi_cfg_parse_rx_action(u8 *buf, u32 len)
 {
 	u32 index = 0;
-
 	u8 op_channel_attr_index = 0;
 	u8 channel_list_attr_index = 0;
 
@@ -1339,7 +1337,6 @@ static void wilc_wfi_cfg_parse_tx_action(u8 *buf, u32 len, bool oper_ch,
 					 u8 iftype)
 {
 	u32 index = 0;
-
 	u8 op_channel_attr_index = 0;
 	u8 channel_list_attr_index = 0;
 
@@ -2262,7 +2259,6 @@ struct wireless_dev *wilc_create_wiphy(struct net_device *net,
 int wilc_init_host_int(struct net_device *net)
 {
 	int ret = 0;
-
 	struct wilc_priv *priv;
 
 	priv = wdev_priv(net->ieee80211_ptr);
