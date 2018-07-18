e92ca3e3e5d10c4afa75351d1b251889bb15b2ae
Jimmy Assarsson <extja@kvaser.com>
2018-07-18 23:29:27 +0200

    can: kvaser_usb: Add SPDX GPL-2.0 license identifier
    Add SPDX GPL-2.0 license identifier to kvaser_usb.c.
    
    Signed-off-by: Jimmy Assarsson <extja@kvaser.com>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/usb/kvaser_usb.c
+++ drivers/net/can/usb/kvaser_usb.c
@@ -1,10 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/*
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation version 2.
- *
- * Parts of this driver are based on the following:
+// SPDX-License-Identifier: GPL-2.0
+/* Parts of this driver are based on the following:
  *  - Kvaser linux leaf driver (version 4.78)
  *  - CAN driver for esd CAN-USB/2
  *  - Kvaser linux usbcanII driver (version 5.3)
