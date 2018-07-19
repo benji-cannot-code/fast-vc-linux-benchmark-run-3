2e77be075016f0ea8d9020a63a7d88c39494eb41
Lukas Wunner <lukas@wunner.de>
2018-07-19 17:27:47 -0500

    PCI: pciehp: Declare pciehp_enable/disable_slot() static
    No callers of pciehp_enable/disable_slot() outside of pciehp_ctrl.c
    remain, so declare the functions static.  For now this requires forward
    declarations.  Those can be eliminated by reshuffling functions once the
    ongoing effort to refactor the driver has settled.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/hotplug/pciehp_ctrl.c
+++ drivers/pci/hotplug/pciehp_ctrl.c
@@ -123,6 +123,9 @@ static void remove_board(struct slot *p_slot)
 	pciehp_green_led_off(p_slot);
 }
 
+static int pciehp_enable_slot(struct slot *slot);
+static int pciehp_disable_slot(struct slot *slot);
+
 void pciehp_request(struct controller *ctrl, int action)
 {
 	atomic_or(action, &ctrl->pending_events);
@@ -309,7 +312,7 @@ static int __pciehp_enable_slot(struct slot *p_slot)
 	return board_added(p_slot);
 }
 
-int pciehp_enable_slot(struct slot *slot)
+static int pciehp_enable_slot(struct slot *slot)
 {
 	struct controller *ctrl = slot->ctrl;
 	int ret;
@@ -343,7 +346,7 @@ static int __pciehp_disable_slot(struct slot *p_slot)
 	return 0;
 }
 
-int pciehp_disable_slot(struct slot *slot)
+static int pciehp_disable_slot(struct slot *slot)
 {
 	int ret;
 
