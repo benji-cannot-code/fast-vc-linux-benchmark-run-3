0f36d8962bde3724262507e5879ecfdf643af5dd
Darrick J. Wong <darrick.wong@oracle.com>
2018-07-19 12:29:11 -0700

    xfs: shorten xfs_repair_ prefix to xrep_
    Shorten all the metadata repair xfs_repair_* symbols to xrep_.
    Whitespace damage will be fixed by a subsequent patch.  There are no
    functional changes.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Brian Foster <bfoster@redhat.com>

--- fs/xfs/scrub/agheader_repair.c
+++ fs/xfs/scrub/agheader_repair.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Repair the superblock. */
 int
-xfs_repair_superblock(
+xrep_superblock(
 	struct xfs_scrub_context	*sc)
 {
 	struct xfs_mount		*mp = sc->mp;
