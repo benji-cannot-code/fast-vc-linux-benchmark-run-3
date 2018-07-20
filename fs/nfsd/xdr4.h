9699fbbcc6d4b4555822b2cdce1d89c5fd5b331f
Olga Kornievskaia <kolga@netapp.com>
2018-07-20 18:19:17 -0400

    NFSD CB_OFFLOAD xdr
    Signed-off-by: Olga Kornievskaia <kolga@netapp.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/xdr4.h
+++ fs/nfsd/xdr4.h
@@ -512,6 +512,7 @@ struct nfsd42_write_res {
 	u64			wr_bytes_written;
 	u32			wr_stable_how;
 	nfs4_verifier		wr_verifier;
+	stateid_t		cb_stateid;
 };
 
 struct nfsd4_copy {
@@ -527,6 +528,11 @@ struct nfsd4_copy {
 
 	/* response */
 	struct nfsd42_write_res	cp_res;
+
+	/* for cb_offload */
+	struct nfsd4_callback	cp_cb;
+	__be32			nfserr;
+	struct knfsd_fh		fh;
 };
 
 struct nfsd4_seek {
