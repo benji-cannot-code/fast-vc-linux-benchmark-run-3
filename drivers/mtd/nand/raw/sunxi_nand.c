aaba21a57f24351ae36c10bbc89165ab502c158a
Boris Brezillon <boris.brezillon@bootlin.com>
2018-07-20 11:57:38 +0200

    mtd: rawnand: sunxi: Remove gpio.h and of_gpio.h inclusions
    Commit ddd5ed3a90e7 ("mtd: rawnand: sunxi: Remove support for
    GPIO-based Ready/Busy polling") removed GPIO-based RB polling. We no
    longer need to include gpio headers.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/raw/sunxi_nand.c
+++ drivers/mtd/nand/raw/sunxi_nand.c
@@ -30,14 +30,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-#include <linux/of_gpio.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/rawnand.h>
 #include <linux/mtd/partitions.h>
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/dmaengine.h>
-#include <linux/gpio.h>
 #include <linux/interrupt.h>
 #include <linux/iopoll.h>
 #include <linux/reset.h>
