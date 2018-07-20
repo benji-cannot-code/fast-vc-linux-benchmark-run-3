f42f594c93a844173f106fa222da5952f717bf09
John Whitmore <johnfwhitmore@gmail.com>
2018-07-20 21:21:32 +0100

    staging:rtl8192u: Rename variable NumChnls - Style
    Rename the member variable NumChnls to num_channels. This change clears the
    checkpatch issue with CamelCase naming. The change should not impact runtime
    execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/dot11d.h
+++ drivers/staging/rtl8192u/ieee80211/dot11d.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct chnl_txpower_triple {
 	u8  first_channel;
-	u8  NumChnls;
+	u8  num_channels;
 	u8  MaxTxPowerInDbm;
 };
 
