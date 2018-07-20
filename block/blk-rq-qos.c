09e5b72b05658b711b285d824ba0bfc2dfd26f04
Josef Bacik <josef@toxicpanda.com>
2018-07-19 21:42:13 -0400

    blk-rq-qos: make depth comparisons unsigned
    With the change to use UINT_MAX I broke the depth check as any value of
    inflight (ie 0) would be less than (int)UINT_MAX.  Fix this by changing
    everything to unsigned int to match the depth.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-rq-qos.c
+++ block/blk-rq-qos.c
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Increment 'v', if 'v' is below 'below'. Returns true if we succeeded,
  * false if 'v' + 1 would be bigger than 'below'.
  */
-static bool atomic_inc_below(atomic_t *v, int below)
+static bool atomic_inc_below(atomic_t *v, unsigned int below)
 {
-	int cur = atomic_read(v);
+	unsigned int cur = atomic_read(v);
 
 	for (;;) {
-		int old;
+		unsigned int old;
 
 		if (cur >= below)
 			return false;
@@ -23,7 +23,7 @@ static bool atomic_inc_below(atomic_t *v, int below)
 	return true;
 }
 
-bool rq_wait_inc_below(struct rq_wait *rq_wait, int limit)
+bool rq_wait_inc_below(struct rq_wait *rq_wait, unsigned int limit)
 {
 	return atomic_inc_below(&rq_wait->inflight, limit);
 }
