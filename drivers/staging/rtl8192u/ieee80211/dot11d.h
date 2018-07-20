7110e5aa1b3ce64bc498477edfda4a422b467326
John Whitmore <johnfwhitmore@gmail.com>
2018-07-20 21:21:31 +0100

    staging:rtl8192u: Rename structure member FirstChnl - Style
    Rename structure member FirstChnl to first_channel. This coding style change
    clears a checkpatch issue with CamelCase naming. This change should not impact
    the runtime code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/dot11d.h
+++ drivers/staging/rtl8192u/ieee80211/dot11d.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ieee80211.h"
 
 struct chnl_txpower_triple {
-	u8 FirstChnl;
+	u8  first_channel;
 	u8  NumChnls;
 	u8  MaxTxPowerInDbm;
 };
