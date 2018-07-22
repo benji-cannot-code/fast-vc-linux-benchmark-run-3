f1204688c096b610309b6ec2b71c598d2ddfcf4b
John Whitmore <johnfwhitmore@gmail.com>
2018-07-22 23:10:31 +0100

    staging:rtl8192u: Remove unused page e Bit Mask definitions - Style
    Remove the unused 'page e' Bit Mask definitions. These definitions will
    fail the checkpatch CamelCase naming tests. As they are unused in code they
    have simply been removed rather then renaming.
    
    This is a coding style change which should have no impact on runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r819xU_phyreg.h
+++ drivers/staging/rtl8192u/r819xU_phyreg.h
@@ -125,15 +125,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define bCCKSideBand				0x10
 
 /* page e */
-#define bTxAGCRate18_06			0x7f7f7f7f
-#define bTxAGCRate54_24			0x7f7f7f7f
-#define bTxAGCRateMCS32		0x7f
 #define bTxAGCRateCCK			0x7f00
-#define bTxAGCRateMCS3_MCS0	0x7f7f7f7f
-#define bTxAGCRateMCS7_MCS4	0x7f7f7f7f
-#define bTxAGCRateMCS11_MCS8	0x7f7f7f7f
-#define bTxAGCRateMCS15_MCS12	0x7f7f7f7f
-
 
 /* Rx Pseduo noise */
 #define bRxPesudoNoiseOn         0x20000000
