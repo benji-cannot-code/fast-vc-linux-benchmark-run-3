afc585c21fbfe9adbd3e56c5214cf83c8a63b51b
Roman Kiryanov <rkir@google.com>
2018-07-23 15:47:26 -0700

    goldfish: Add missing includes to goldfish.h
    goldfish.h refers to external symbols such as
    dma_addr_t and writel. This causes compilation errors
    if this file is included before other header files.
    
    The mentioned symbols are defined in types.h (dma_addr_t)
    and io.h (writel).
    
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/goldfish.h
+++ include/linux/goldfish.h
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_GOLDFISH_H
 #define __LINUX_GOLDFISH_H
 
+#include <linux/types.h>
+#include <linux/io.h>
+
 /* Helpers for Goldfish virtual platform */
 
 static inline void gf_write_ptr(const void *ptr, void __iomem *portl,
