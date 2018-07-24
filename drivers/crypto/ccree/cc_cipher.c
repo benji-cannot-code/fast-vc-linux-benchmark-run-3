34efefb79e03c2e89f0651289fdd5593a888a585
Gilad Ben-Yossef <gilad@benyossef.com>
2018-07-24 15:12:46 +0100

    crypto: ccree - allow bigger than sector XTS op
    The ccree driver had a sanity check that we are not asked
    to encrypt an XTS buffer bigger than a sane sector size
    since XTS IV needs to include the sector number in the IV
    so this is not expected in any real use case.
    
    Unfortunately, this breaks cryptsetup benchmark test which
    has a synthetic performance test using 64k buffer of data
    with the same IV.
    
    Remove the sanity check and allow the user to hang themselves
    and/or run benchmarks if they so wish.
    
    Reported-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccree/cc_cipher.c
+++ drivers/crypto/ccree/cc_cipher.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define template_skcipher	template_u.skcipher
 
-#define CC_MIN_AES_XTS_SIZE 0x10
-#define CC_MAX_AES_XTS_SIZE 0x2000
 struct cc_cipher_handle {
 	struct list_head alg_list;
 };
@@ -99,8 +97,7 @@ static int validate_data_size(struct cc_cipher_ctx *ctx_p,
 	case S_DIN_to_AES:
 		switch (ctx_p->cipher_mode) {
 		case DRV_CIPHER_XTS:
-			if (size >= CC_MIN_AES_XTS_SIZE &&
-			    size <= CC_MAX_AES_XTS_SIZE &&
+			if (size >= AES_BLOCK_SIZE &&
 			    IS_ALIGNED(size, AES_BLOCK_SIZE))
 				return 0;
 			break;
