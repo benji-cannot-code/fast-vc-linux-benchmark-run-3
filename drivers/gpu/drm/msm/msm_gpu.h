bf75b55e8a2571493e4a6bc07b66a32ad9e25fd4
Jordan Crouse <jcrouse@codeaurora.org>
2018-07-24 10:33:31 -0600

    drm/msm/gpu: Add the buffer objects from the submit to the crash dump
    For hangs, dump copy out the contents of the buffer objects attached to the
    guilty submission and print them in the crash dump report.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/msm_gpu.h
+++ drivers/gpu/drm/msm/msm_gpu.h
@@ -182,6 +182,12 @@ struct msm_gpu_submitqueue {
 	struct kref ref;
 };
 
+struct msm_gpu_state_bo {
+	u64 iova;
+	size_t size;
+	void *data;
+};
+
 struct msm_gpu_state {
 	struct kref ref;
 	struct timeval time;
@@ -203,6 +209,9 @@ struct msm_gpu_state {
 
 	char *comm;
 	char *cmd;
+
+	int nr_bos;
+	struct msm_gpu_state_bo *bos;
 };
 
 static inline void gpu_write(struct msm_gpu *gpu, u32 reg, u32 data)
