86d778b3ada3ebb0181decaaca8dbe12a9ce51eb
Michael Ellerman <mpe@ellerman.id.au>
2018-07-24 15:53:22 +1000

    selftests/powerpc: Add a test of wild bctr
    This tests that a bctr (Branch to counter and link), ie. a function
    call, to a wildly out-of-bounds address is handled correctly.
    
    Some old kernel versions didn't handle it correctly, see eg:
    
      "powerpc/slb: Force a full SLB flush when we insert for a bad EA"
      https://lists.ozlabs.org/pipermail/linuxppc-dev/2017-April/157397.html
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- tools/testing/selftests/powerpc/include/reg.h
+++ tools/testing/selftests/powerpc/include/reg.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				    : "memory")
 
 #define mb()		asm volatile("sync" : : : "memory");
+#define barrier()	asm volatile("" : : : "memory");
 
 #define SPRN_MMCR2     769
 #define SPRN_MMCRA     770
