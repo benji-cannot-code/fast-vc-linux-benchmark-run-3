95a9257305c1f72a6d63d2f2132976af0653ca21
John Whitmore <johnfwhitmore@gmail.com>
2018-07-25 23:16:25 +0100

    staging:rtl8192u: Rename ISR_TxBcnErr bit definition - Style
    Rename the bit definition ISR_TxBcnErr to ISR_TX_BCN_ERR. This change
    clears the checkpatch issue with CamelCase naming. The change is purely
    a coding style change which should not impact runtime code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r819xU_cmdpkt.h
+++ drivers/staging/rtl8192u/r819xU_cmdpkt.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* 2008/05/08 amy For USB constant. */
 #define ISR_TX_BCN_OK		BIT(27)		/* Transmit Beacon OK */
-#define ISR_TxBcnErr		BIT(26)		/* Transmit Beacon Error */
+#define ISR_TX_BCN_ERR		BIT(26)		/* Transmit Beacon Error */
 #define ISR_BcnTimerIntr	BIT(13)		/* Beacon Timer Interrupt */
 
 
