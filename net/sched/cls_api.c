03e682e25099d0f1e3ea42711bff3290269dca9c
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-07-25 09:07:24 -0500

    net: sched: cls_api: fix dead code in switch
    Code at line 1850 is unreachable. Fix this by removing the break
    statement above it, so the code for case RTM_GETCHAIN can be
    properly executed.
    
    Addresses-Coverity-ID: 1472050 ("Structurally dead code")
    Fixes: 32a4f5ecd738 ("net: sched: introduce chain object to uapi")
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Acked-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/cls_api.c
+++ net/sched/cls_api.c
@@ -1847,7 +1847,6 @@ replay:
 		tcf_chain_put_explicitly_created(chain);
 		break;
 	case RTM_GETCHAIN:
-		break;
 		err = tc_chain_notify(chain, skb, n->nlmsg_seq,
 				      n->nlmsg_seq, n->nlmsg_type, true);
 		if (err < 0)
