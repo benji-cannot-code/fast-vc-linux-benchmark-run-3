94e0002c62c0a80562775e3a4450e6dceb60b452
Todor Tomov <todor.tomov@linaro.org>
2018-07-25 12:38:28 -0400

    media: camss: csiphy: Split to hardware dependent and independent parts
    This will allow to add support for different hardware.
    
    Signed-off-by: Todor Tomov <todor.tomov@linaro.org>
    [hans.verkuil@cisco.com: remove trailing empty line]
    Signed-off-by: Hans Verkuil <hansverk@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/qcom/camss/camss-csiphy.h
+++ drivers/media/platform/qcom/camss/camss-csiphy.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define QC_MSM_CAMSS_CSIPHY_H
 
 #include <linux/clk.h>
+#include <linux/interrupt.h>
 #include <media/media-entity.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-mediabus.h>
@@ -42,6 +43,19 @@ struct csiphy_config {
 	struct csiphy_csi2_cfg *csi2;
 };
 
+struct csiphy_device;
+
+struct csiphy_hw_ops {
+	void (*hw_version_read)(struct csiphy_device *csiphy,
+				struct device *dev);
+	void (*reset)(struct csiphy_device *csiphy);
+	void (*lanes_enable)(struct csiphy_device *csiphy,
+			     struct csiphy_config *cfg,
+			     u32 pixel_clock, u8 bpp, u8 lane_mask);
+	void (*lanes_disable)(struct csiphy_device *csiphy, u8 lane_mask);
+	irqreturn_t (*isr)(int irq, void *dev);
+};
+
 struct csiphy_device {
 	struct camss *camss;
 	u8 id;
@@ -56,6 +70,7 @@ struct csiphy_device {
 	u32 timer_clk_rate;
 	struct csiphy_config cfg;
 	struct v4l2_mbus_framefmt fmt[MSM_CSIPHY_PADS_NUM];
+	const struct csiphy_hw_ops *ops;
 };
 
 struct resources;
@@ -68,5 +83,7 @@ int msm_csiphy_register_entity(struct csiphy_device *csiphy,
 			       struct v4l2_device *v4l2_dev);
 
 void msm_csiphy_unregister_entity(struct csiphy_device *csiphy);
+
+extern const struct csiphy_hw_ops csiphy_ops_2ph_1_0;
 
 #endif /* QC_MSM_CAMSS_CSIPHY_H */
