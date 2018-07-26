b7231809f8eb65da74d6a231712b65d19c5f1099
Paul Mackerras <paulus@ozlabs.org>
2018-07-26 14:53:54 +1000

    KVM: PPC: Book3S HV: Allow creating max number of VCPUs on POWER9
    Commit 1e175d2 ("KVM: PPC: Book3S HV: Pack VCORE IDs to access full
    VCPU ID space", 2018-07-25) allowed use of VCPU IDs up to
    KVM_MAX_VCPU_ID on POWER9 in all guest SMT modes and guest emulated
    hardware SMT modes.  However, with the current definition of
    KVM_MAX_VCPU_ID, a guest SMT mode of 1 and an emulated SMT mode of 8,
    it is only possible to create KVM_MAX_VCPUS / 2 VCPUS, because
    threads_per_subcore is 4 on POWER9 CPUs.  (Using an emulated SMT mode
    of 8 is useful when migrating VMs to or from POWER8 hosts.)
    
    This increases KVM_MAX_VCPU_ID to 8 * KVM_MAX_VCPUS when HV KVM is
    configured in, so that a full complement of KVM_MAX_VCPUS VCPUs can
    be created on POWER9 in all guest SMT modes and emulated hardware
    SMT modes.
    
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- arch/powerpc/include/asm/kvm_host.h
+++ arch/powerpc/include/asm/kvm_host.h
@@ -43,7 +43,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KVM_USER_MEM_SLOTS	512
 
 #include <asm/cputhreads.h>
-#define KVM_MAX_VCPU_ID                (threads_per_subcore * KVM_MAX_VCORES)
+
+#ifdef CONFIG_KVM_BOOK3S_HV_POSSIBLE
+#include <asm/kvm_book3s_asm.h>		/* for MAX_SMT_THREADS */
+#define KVM_MAX_VCPU_ID		(MAX_SMT_THREADS * KVM_MAX_VCORES)
+
+#else
+#define KVM_MAX_VCPU_ID		KVM_MAX_VCPUS
+#endif /* CONFIG_KVM_BOOK3S_HV_POSSIBLE */
 
 #define __KVM_HAVE_ARCH_INTC_INITIALIZED
 
