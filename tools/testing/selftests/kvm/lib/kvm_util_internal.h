f3ee59aedf93dd9c451de3d27625d001607a5730
Paolo Bonzini <pbonzini@redhat.com>
2018-07-26 13:19:23 +0200

    kvm: selftests: add basic test for state save and restore
    The test calls KVM_RUN repeatedly, and creates an entirely new VM with the
    old memory and vCPU state on every exit to userspace.  The kvm_util API is
    expanded with two functions that manage the lifetime of a kvm_vm struct:
    the first closes the file descriptors and leaves the memory allocated,
    and the second opens the file descriptors and reuses the memory from
    the previous incarnation of the kvm_vm struct.
    
    For now the test is very basic, as it does not test for example XSAVE or
    vCPU events.  However, it will test nested virtualization state starting
    with the next patch.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tools/testing/selftests/kvm/lib/kvm_util_internal.h
+++ tools/testing/selftests/kvm/lib/kvm_util_internal.h
@@ -44,6 +44,7 @@ struct vcpu {
 
 struct kvm_vm {
 	int mode;
+	int kvm_fd;
 	int fd;
 	unsigned int page_size;
 	unsigned int page_shift;
@@ -53,6 +54,7 @@ struct kvm_vm {
 	struct sparsebit *vpages_valid;
 	struct sparsebit *vpages_mapped;
 
+	bool has_irqchip;
 	bool pgd_created;
 	vm_paddr_t pgd;
 	vm_vaddr_t gdt;
