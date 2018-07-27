4d86481941b376bf5d17d04295bc33d1f3f9dfcd
Christoph Hellwig <hch@lst.de>
2018-07-27 19:26:06 +0200

    MIPS: remove mips_swiotlb_ops
    mips_swiotlb_ops differs from the generic swiotlb_dma_ops only in that
    it contains a mb() barrier after each operations that maps or syncs
    dma memory to the device.
    
    The dma operations are defined to not be memory barriers, but instead
    the write* operations to kick the DMA off are supposed to contain them.
    
    For mips this handled by war_io_reorder_wmb(), which evaluates to the
    stronger wmb() instead of the pure compiler barrier barrier() for
    just those platforms that use swiotlb, so I think we are covered
    properly.
    
    [paul.burton@mips.com:
      - Include linux/swiotlb.h to fix build failures for configs with
        CONFIG_SWIOTLB=y.]
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Patchwork: https://patchwork.linux-mips.org/patch/20038/
    Cc: David Daney <ddaney@caviumnetworks.com>
    Cc: Huacai Chen <chenhc@lemote.com>
    Cc: linux-mips@linux-mips.org
    Cc: iommu@lists.linux-foundation.org
    Cc: linux-kernel@vger.kernel.org

--- arch/mips/include/asm/dma-mapping.h
+++ arch/mips/include/asm/dma-mapping.h
@@ -3,15 +3,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_DMA_MAPPING_H
 #define _ASM_DMA_MAPPING_H
 
+#include <linux/swiotlb.h>
+
 extern const struct dma_map_ops jazz_dma_ops;
-extern const struct dma_map_ops mips_swiotlb_ops;
 
 static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 {
 #if defined(CONFIG_MACH_JAZZ)
 	return &jazz_dma_ops;
 #elif defined(CONFIG_SWIOTLB)
-	return &mips_swiotlb_ops;
+	return &swiotlb_dma_ops;
 #elif defined(CONFIG_DMA_NONCOHERENT_OPS)
 	return &dma_noncoherent_ops;
 #else
