4b9fbbd8707c00bd952feda222561644c9132b62
Lucas De Marchi <lucas.demarchi@intel.com>
2018-07-27 12:36:45 -0700

    drm/i915: make PCH_GMBUS* definitions private to gvt
    This is the only place that they are being used - the others use the
    GMBUS* macros that rely on dev_priv being already properly initialized.
    
    Cc: intel-gvt-dev@lists.freedesktop.org
    Cc: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180727193647.8639-1-lucas.demarchi@intel.com

--- drivers/gpu/drm/i915/gvt/reg.h
+++ drivers/gpu/drm/i915/gvt/reg.h
@@ -78,4 +78,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _RING_CTL_BUF_SIZE(ctl) (((ctl) & RB_TAIL_SIZE_MASK) + \
 		I915_GTT_PAGE_SIZE)
 
+#define PCH_GMBUS0	_MMIO(0xc5100)
+#define PCH_GMBUS1	_MMIO(0xc5104)
+#define PCH_GMBUS2	_MMIO(0xc5108)
+#define PCH_GMBUS3	_MMIO(0xc510c)
+#define PCH_GMBUS4	_MMIO(0xc5110)
+#define PCH_GMBUS5	_MMIO(0xc5120)
+
 #endif
