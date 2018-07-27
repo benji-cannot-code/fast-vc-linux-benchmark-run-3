c163aaedeeb9ac92085ffe1aa440e53f4e79a17b
Shreyas NC <shreyas.nc@intel.com>
2018-07-27 14:44:09 +0530

    soundwire: Fix incorrect exit after configuring stream
    In sdw_stream_add_master() after the Master ports are configured,
    the stream is released incorrectly.
    
    So, fix it by avoiding stream release after configuring the Master
    for the stream.
    While at it, rename the label appropriately.
    
    Signed-off-by: Shreyas NC <shreyas.nc@intel.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/soundwire/stream.c
+++ drivers/soundwire/stream.c
@@ -1113,7 +1113,7 @@ int sdw_stream_add_master(struct sdw_bus *bus,
 				"Master runtime config failed for stream:%s",
 				stream->name);
 		ret = -ENOMEM;
-		goto error;
+		goto unlock;
 	}
 
 	ret = sdw_config_stream(bus->dev, stream, stream_config, false);
@@ -1124,9 +1124,11 @@ int sdw_stream_add_master(struct sdw_bus *bus,
 	if (ret)
 		goto stream_error;
 
+	goto unlock;
+
 stream_error:
 	sdw_release_master_stream(stream);
-error:
+unlock:
 	mutex_unlock(&bus->bus_lock);
 	return ret;
 }
