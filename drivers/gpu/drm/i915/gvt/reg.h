b3e2c7e455fd4e3037a307267b23194eee93e1ab
Lucas De Marchi <lucas.demarchi@intel.com>
2018-07-27 12:36:46 -0700

    drm/i915/gvt: use its own define for gpio
    The definition on i915_reg.h is going to change to depend on
    dev_priv->gpio_mmio_base being properly initialized. Define our own
    macros since init_generic_mmio_info() is called before than
    gpio_mmio_base being set.
    
    Cc: intel-gvt-dev@lists.freedesktop.org
    Cc: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180727193647.8639-2-lucas.demarchi@intel.com

--- drivers/gpu/drm/i915/gvt/reg.h
+++ drivers/gpu/drm/i915/gvt/reg.h
@@ -78,6 +78,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _RING_CTL_BUF_SIZE(ctl) (((ctl) & RB_TAIL_SIZE_MASK) + \
 		I915_GTT_PAGE_SIZE)
 
+#define PCH_GPIO_BASE	_MMIO(0xc5010)
+
 #define PCH_GMBUS0	_MMIO(0xc5100)
 #define PCH_GMBUS1	_MMIO(0xc5104)
 #define PCH_GMBUS2	_MMIO(0xc5108)
