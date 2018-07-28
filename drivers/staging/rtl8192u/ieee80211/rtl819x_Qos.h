cbb55b136209ce1008f4374c3aa4eaee1fe8e5b5
John Whitmore <johnfwhitmore@gmail.com>
2018-07-29 00:14:47 +0100

    staging:rtl8192u: Rename member variables - Style
    Rename the member variables of union aci_aifsn, which should be named
    in lowercase. The only member variable, of this union, which is
    actually used is 'acm'.
    
    This are coding style changes which should have no impact on runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/rtl819x_Qos.h
+++ drivers/staging/rtl8192u/ieee80211/rtl819x_Qos.h
@@ -15,13 +15,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // Ref: WMM spec 2.2.2: WME Parameter Element, p.12.
 //
 union aci_aifsn {
-	u8	charData;
+	u8	char_data;
 
 	struct {
-		u8	AIFSN:4;
-		u8	ACM:1;
-		u8	ACI:2;
-		u8	Reserved:1;
+		u8	aifsn:4;
+		u8	acm:1;
+		u8	aci:2;
+		u8	reserved:1;
 	} f;	// Field
 };
 
