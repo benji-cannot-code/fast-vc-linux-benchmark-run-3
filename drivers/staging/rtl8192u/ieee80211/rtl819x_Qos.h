0e3a83c7b859423cf17dfa7517f3eb6269496e9f
John Whitmore <johnfwhitmore@gmail.com>
2018-07-29 00:14:46 +0100

    staging:rtl8192u: Rename ACI_AIFSN - Style
    Rename the union ACI_AIFSN to aci_aifsn and remove the typedef directive.
    
    The removal of the typedef clears the checkpatch issue with defining
    new types. The renaming is to adhere to the coding style where types
    are name in lower case.
    
    These changes are coding style changes which should have no impact on
    runtime execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/rtl819x_Qos.h
+++ drivers/staging/rtl8192u/ieee80211/rtl819x_Qos.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // ACI/AIFSN Field.
 // Ref: WMM spec 2.2.2: WME Parameter Element, p.12.
 //
-typedef	union _ACI_AIFSN {
+union aci_aifsn {
 	u8	charData;
 
 	struct {
@@ -23,7 +23,7 @@ typedef	union _ACI_AIFSN {
 		u8	ACI:2;
 		u8	Reserved:1;
 	} f;	// Field
-} ACI_AIFSN, *PACI_AIFSN;
+};
 
 //
 // ECWmin/ECWmax field.
@@ -46,7 +46,7 @@ typedef	union _AC_PARAM {
 	u8	charData[4];
 
 	struct {
-		ACI_AIFSN	AciAifsn;
+		union aci_aifsn	AciAifsn;
 		ECW		Ecw;
 		u16		TXOPLimit;
 	} f;	// Field
