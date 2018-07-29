7001276479d6231d900caef090c6ca6180b283ae
Michael Straube <straube.linux@gmail.com>
2018-07-29 20:54:44 +0200

    staging: rtl8188eu: remove redundant includes
    Both osdep_service.h and drv_types.h are included from hal_intf.h,
    so remove the redundant includes from hal_intf.c.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/hal_intf.c
+++ drivers/staging/rtl8188eu/hal/hal_intf.c
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  ******************************************************************************/
 
 #define _HAL_INTF_C_
-#include <osdep_service.h>
-#include <drv_types.h>
 #include <hal_intf.h>
 
 uint	 rtw_hal_init(struct adapter *adapt)
