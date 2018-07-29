ab67da5ba49098d4577880916ead65b551da1c23
Gao Xiang <hsiangkao@aol.com>
2018-07-29 13:34:58 +0800

    staging: erofs: fix compile error without built-in decompression support
    This patch fixes incorrect code snippets due to spilt code
    into small patches by mistake.
    
    Link: https://lists.01.org/pipermail/kbuild-all/2018-July/050747.html
    Link: https://lists.01.org/pipermail/kbuild-all/2018-July/050750.html
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/erofs/utils.c
+++ drivers/staging/erofs/utils.c
@@ -161,7 +161,7 @@ skip:
 		}
 
 #ifdef EROFS_FS_HAS_MANAGED_CACHE
-		if (try_to_free_all_cached_pages(sbi, grp))
+		if (erofs_try_to_free_all_cached_pages(sbi, grp))
 			goto skip;
 
 		erofs_workgroup_unfreeze(grp, 1);
@@ -248,7 +248,9 @@ unsigned long erofs_shrink_scan(struct shrinker *shrink,
 		spin_unlock(&erofs_sb_list_lock);
 		sbi->shrinker_run_no = run_no;
 
-		/* add scan handlers here */
+#ifdef CONFIG_EROFS_FS_ZIP
+		freed += erofs_shrink_workstation(sbi, nr, false);
+#endif
 
 		spin_lock(&erofs_sb_list_lock);
 		/* Get the next list element before we move this one */
@@ -261,7 +263,6 @@ unsigned long erofs_shrink_scan(struct shrinker *shrink,
 		list_move_tail(&sbi->list, &erofs_sb_list);
 		mutex_unlock(&sbi->umount_mutex);
 
-		freed += erofs_shrink_workstation(sbi, nr, false);
 		if (freed >= nr)
 			break;
 	}
