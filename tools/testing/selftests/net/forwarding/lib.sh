33f17da61ec66a73e4330cad2c12a2e184f87805
Petr Machata <petrm@mellanox.com>
2018-07-30 16:39:57 +0200

    selftests: forwarding: Introduce $ARPING
    Instead of relying on "arping" being installed everywhere under that
    name, introduce a variable $ARPING like the other tools do.
    
    Convert an existing test, mirror_gre_vlan_bridge_1q.sh to
    require_command $ARPING and then invoke arping through the variable.
    
    Signed-off-by: Petr Machata <petrm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/testing/selftests/net/forwarding/lib.sh
+++ tools/testing/selftests/net/forwarding/lib.sh
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 PING=${PING:=ping}
 PING6=${PING6:=ping6}
 MZ=${MZ:=mausezahn}
+ARPING=${ARPING:=arping}
 TEAMD=${TEAMD:=teamd}
 WAIT_TIME=${WAIT_TIME:=5}
 PAUSE_ON_FAIL=${PAUSE_ON_FAIL:=no}
