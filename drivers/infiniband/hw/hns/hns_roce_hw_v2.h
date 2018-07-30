bc751026dfda82279d505fb117f9271daeda0e63
Lijun Ou <oulijun@huawei.com>
2018-07-30 20:20:29 +0800

    RDMA/hns: Use macro instead of magic number
    This patch mainly uses CMD_CSQ_DESC_NUM instead of magic number in order
    to improve readability.
    
    Signed-off-by: Lijun Ou <oulijun@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/hw/hns/hns_roce_hw_v2.h
+++ drivers/infiniband/hw/hns/hns_roce_hw_v2.h
@@ -113,6 +113,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(step_idx == 1 && hop_num == 1) || \
 	(step_idx == 2 && hop_num == 2))
 
+#define CMD_CSQ_DESC_NUM		1024
+#define CMD_CRQ_DESC_NUM		1024
+
 enum {
 	NO_ARMED = 0x0,
 	REG_NXT_CEQE = 0x2,
