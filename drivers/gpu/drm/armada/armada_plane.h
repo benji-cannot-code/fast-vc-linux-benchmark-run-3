eebf6487e37e59aa8519626949b350cb3df150d0
Russell King <rmk+kernel@armlinux.org.uk>
2018-07-30 11:52:34 +0100

    drm/armada: provide pitches from armada_drm_plane_calc_addrs()
    Provide the framebuffer pitches from armada_drm_plane_calc_addrs() as
    well as the base addresses for each plane.  Since this is now about
    more than just addresses, rename to armada_drm_plane_calc().
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- drivers/gpu/drm/armada/armada_plane.h
+++ drivers/gpu/drm/armada/armada_plane.h
@@ -2,7 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ARMADA_PLANE_H
 #define ARMADA_PLANE_H
 
-void armada_drm_plane_calc_addrs(struct drm_plane_state *state, u32 addrs[3]);
+void armada_drm_plane_calc(struct drm_plane_state *state, u32 addrs[3],
+	u16 pitches[3]);
 int armada_drm_plane_prepare_fb(struct drm_plane *plane,
 	struct drm_plane_state *state);
 void armada_drm_plane_cleanup_fb(struct drm_plane *plane,
