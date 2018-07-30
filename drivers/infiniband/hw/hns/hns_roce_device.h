459bac9ec2aba55f8c7321b2f9689f44067085fb
Lijun Ou <oulijun@huawei.com>
2018-07-30 20:20:30 +0800

    RDMA/hns: Program the tclass and flow label into the hardware
    This was missed in a few places, and was just using 0.
    
    Also correct the spelling of HNS_ROCE_FLOW_LABEL_MASK
    
    Signed-off-by: Lijun Ou <oulijun@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/hw/hns/hns_roce_device.h
+++ drivers/infiniband/hw/hns/hns_roce_device.h
@@ -77,7 +77,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* 4G/4K = 1M */
 #define HNS_ROCE_SL_SHIFT			28
 #define HNS_ROCE_TCLASS_SHIFT			20
-#define HNS_ROCE_FLOW_LABLE_MASK		0xfffff
+#define HNS_ROCE_FLOW_LABEL_MASK		0xfffff
 
 #define HNS_ROCE_MAX_PORTS			6
 #define HNS_ROCE_MAX_GID_NUM			16
