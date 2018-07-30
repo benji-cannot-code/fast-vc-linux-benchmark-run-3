b2d5797878c33592425dffcb599d7d4b96d17432
Russell King <rmk+kernel@armlinux.org.uk>
2018-07-30 11:52:34 +0100

    drm/armada: pass plane state into armada_drm_plane_calc_addrs()
    armada_drm_plane_calc_addrs() gets all its information from the plane
    state, so it makes sense to pass the plane state pointer down into this
    function, rather than extracting the information in identical ways,
    sometimes a couple of layers up.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- drivers/gpu/drm/armada/armada_plane.h
+++ drivers/gpu/drm/armada/armada_plane.h
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ARMADA_PLANE_H
 #define ARMADA_PLANE_H
 
-void armada_drm_plane_calc_addrs(u32 *addrs, struct drm_framebuffer *fb,
-	int x, int y);
+void armada_drm_plane_calc_addrs(struct drm_plane_state *state, u32 addrs[3]);
 int armada_drm_plane_prepare_fb(struct drm_plane *plane,
 	struct drm_plane_state *state);
 void armada_drm_plane_cleanup_fb(struct drm_plane *plane,
