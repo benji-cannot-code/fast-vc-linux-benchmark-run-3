cbc1d247071c9963e5d44a9857710ce9229d1918
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2018-07-30 14:50:08 +0300

    regulator: bd71837: adobt MFD changes to regulator driver
    MFD part for bd71837 was changed during the review. Clean regulator part to
    match changed MFD:
    - renamed header file => fix include
    - remove unused platdata as also type definition was removed
    - Kconfig option for MFD part was changed => fix depends on clause
    - Rename Kconfig option for regulators
      As Kconfig option for regulators gets now used (when dependency to MFD is
      satisfied) change it so that it won't require new change when support for
      bd71847 is added.
    
    Signed-off-by: Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/bd71837-regulator.c
+++ drivers/regulator/bd71837-regulator.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
-#include <linux/mfd/bd71837.h>
+#include <linux/mfd/rohm-bd718x7.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/driver.h>
@@ -491,7 +491,6 @@ struct reg_init {
 static int bd71837_probe(struct platform_device *pdev)
 {
 	struct bd71837_pmic *pmic;
-	struct bd71837_board *pdata;
 	struct regulator_config config = { 0 };
 	struct reg_init pmic_regulator_inits[] = {
 		{
@@ -559,7 +558,6 @@ static int bd71837_probe(struct platform_device *pdev)
 		goto err;
 	}
 	platform_set_drvdata(pdev, pmic);
-	pdata = dev_get_platdata(pmic->mfd->dev);
 
 	/* Register LOCK release */
 	err = regmap_update_bits(pmic->mfd->regmap, BD71837_REG_REGLOCK,
@@ -578,9 +576,6 @@ static int bd71837_probe(struct platform_device *pdev)
 		struct regulator_dev *rdev;
 
 		desc = &pmic->descs[i];
-
-		if (pdata)
-			config.init_data = pdata->init_data[i];
 
 		config.dev = pdev->dev.parent;
 		config.driver_data = pmic;
