4c6710748676c4546d8a65fd8da79d7419c9ae5b
YueHaibing <yuehaibing@huawei.com>
2018-08-01 17:52:34 +0800

    rxrpc: remove redundant variables 'sp' and 'did_discard'
    Variables 'sp' and 'did_discard' are being assigned,
    but are never used, hence they are redundant and can be removed.
    
    fix following warning:
    
    net/rxrpc/call_event.c:165:25: warning: variable 'sp' set but not used [-Wunused-but-set-variable]
    net/rxrpc/conn_client.c:1054:7: warning: variable 'did_discard' set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- net/rxrpc/conn_client.c
+++ net/rxrpc/conn_client.c
@@ -1052,7 +1052,6 @@ void rxrpc_discard_expired_client_conns(struct work_struct *work)
 		container_of(work, struct rxrpc_net, client_conn_reaper);
 	unsigned long expiry, conn_expires_at, now;
 	unsigned int nr_conns;
-	bool did_discard = false;
 
 	_enter("");
 
@@ -1114,7 +1113,6 @@ next:
 	 * If someone re-sets the flag and re-gets the ref, that's fine.
 	 */
 	rxrpc_put_connection(conn);
-	did_discard = true;
 	nr_conns--;
 	goto next;
 
