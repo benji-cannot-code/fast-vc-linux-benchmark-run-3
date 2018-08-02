62185126e260b5ede4918e4eccf15a4efa5250fa
Florian Westphal <fw@strlen.de>
2018-08-02 21:44:41 +0200

    netfilter: nf_tables: don't prevent event handler from device cleanup on netns exit
    When a netnsamespace exits, the nf_tables pernet_ops will remove all rules.
    However, there is one caveat:
    
    Base chains that register ingress hooks will cause use-after-free:
    device is already gone at that point.
    
    The device event handlers prevent this from happening:
    netns exit synthesizes unregister events for all devices.
    
    However, an improper fix for a race condition made the notifiers a no-op
    in case they get called from netns exit path, so revert that part.
    
    This is safe now as the previous patch fixed nf_tables pernet ops
    and device notifier initialisation ordering.
    
    Fixes: 0a2cf5ee432c2 ("netfilter: nf_tables: close race between netns exit and rmmod")
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -5926,10 +5926,7 @@ static int nf_tables_flowtable_event(struct notifier_block *this,
 	if (event != NETDEV_UNREGISTER)
 		return 0;
 
-	net = maybe_get_net(dev_net(dev));
-	if (!net)
-		return 0;
-
+	net = dev_net(dev);
 	mutex_lock(&net->nft.commit_mutex);
 	list_for_each_entry(table, &net->nft.tables, list) {
 		list_for_each_entry(flowtable, &table->flowtables, list) {
@@ -5937,7 +5934,7 @@ static int nf_tables_flowtable_event(struct notifier_block *this,
 		}
 	}
 	mutex_unlock(&net->nft.commit_mutex);
-	put_net(net);
+
 	return NOTIFY_DONE;
 }
 
