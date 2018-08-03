f8e792f1c7c8c4b29d00a46f2ee07e9447657196
John Whitmore <johnfwhitmore@gmail.com>
2018-08-03 01:02:01 +0100

    staging:rtl8192u: Remove SS_TH_low - Style
    The member variable SS_TH_low is assigned a constant and then used
    in a comparison. This member variable is redundant as the constant
    can be used directly for the comparison.
    
    In addition the constant RxPathSelection_SS_TH_low has been renamed
    to RX_PATH_SELECTION_SS_TH_LOW, to clear the checkpatch issue with
    CamelCase naming.
    
    These changes are coding style in nature and should not impact
    runtime code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8192U_dm.h
+++ drivers/staging/rtl8192u/r8192U_dm.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define		DM_DIG_MIN					0x1c
 #define		DM_DIG_MIN_Netcore			0x12
 
-#define		RxPathSelection_SS_TH_low		30
+#define		RX_PATH_SELECTION_SS_TH_LOW		30
 #define		RxPathSelection_diff_TH			18
 
 #define		RateAdaptiveTH_High			50
@@ -133,7 +133,6 @@ struct dynamic_rx_path_sel {
 	enum cck_rx_path_method		cck_method;
 	u8		cck_rx_path;
 
-	u8		SS_TH_low;
 	u8		diff_TH;
 	u8		disabledRF;
 	u8		reserved;
