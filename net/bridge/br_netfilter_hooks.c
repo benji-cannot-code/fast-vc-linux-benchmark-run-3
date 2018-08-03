f8be7cc77ef8ce9e926fb5185e1ef1a091c62991
Máté Eckl <ecklm94@gmail.com>
2018-08-03 13:36:13 +0200

    netfilter: bridge: Expose nf_tables bridge hook priorities through uapi
    Netfilter exposes standard hook priorities in case of ipv4, ipv6 and
    arp but not in case of bridge.
    
    This patch exposes the hook priority values of the bridge family (which are
    different from the formerly mentioned) via uapi so that they can be used by
    user-space applications just like the others.
    
    Signed-off-by: Máté Eckl <ecklm94@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/bridge/br_netfilter_hooks.c
+++ net/bridge/br_netfilter_hooks.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_pppox.h>
 #include <linux/ppp_defs.h>
 #include <linux/netfilter_bridge.h>
+#include <uapi/linux/netfilter_bridge.h>
 #include <linux/netfilter_ipv4.h>
 #include <linux/netfilter_ipv6.h>
 #include <linux/netfilter_arp.h>
