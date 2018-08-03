193a008164b4edb4ac40e8e4a908c85886ec57bf
Stanislaw Gruszka <sgruszka@redhat.com>
2018-08-03 13:44:39 +0200

    mt76x0: rename mt76_* functions
    mt76_* functions conflicts with mt7601u driver what prevents to build
    those drivers in the kernel or use both drivers modules at once.
    
    Patch fixes build errors like this:
    ld: drivers/net/wireless/mediatek/mt76/mt76x0/mac.o:(.opd+0x30): multiple definition of `mt76_mac_tx_rate_val'; drivers/net/wireless/mediatek/mt7601u/mac.o:(.opd+0x30): first defined here
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Fixes: 7b4859026ccd ("mt76x0: core files")
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/mediatek/mt76/mt76x0/util.c
+++ drivers/net/wireless/mediatek/mt76/mt76x0/util.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "mt76x0.h"
 
-void mt76_remove_hdr_pad(struct sk_buff *skb)
+void mt76x0_remove_hdr_pad(struct sk_buff *skb)
 {
 	int len = ieee80211_get_hdrlen_from_skb(skb);
 
@@ -22,7 +22,7 @@ void mt76_remove_hdr_pad(struct sk_buff *skb)
 	skb_pull(skb, 2);
 }
 
-int mt76_insert_hdr_pad(struct sk_buff *skb)
+int mt76x0_insert_hdr_pad(struct sk_buff *skb)
 {
 	int len = ieee80211_get_hdrlen_from_skb(skb);
 	int ret;
