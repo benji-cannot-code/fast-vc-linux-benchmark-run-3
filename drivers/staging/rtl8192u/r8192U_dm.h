eea423e9cfb19f23c1706b1fcaccf81f95674d59
John Whitmore <johnfwhitmore@gmail.com>
2018-08-03 01:02:08 +0100

    staging:rtl8192u: Rename constants RateAdaptiveTH_Low_* - Style
    The two constants, RateAdaptiveTH_Low_20M and RateAdaptiveTH_Low_40M
    generate a checkpatch warning about CamelCase naming. The two have been
    renamed to clear this issue. RATE_ADAPTIVE_TH_LOW_20M and
    RATE_ADAPTIVE_TH_LOW_40M
    
    This is a coding style change which should have no impact on runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8192U_dm.h
+++ drivers/staging/rtl8192u/r8192U_dm.h
@@ -42,8 +42,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define		RX_PATH_SELECTION_DIFF_TH			18
 
 #define		RATE_ADAPTIVE_TH_HIGH			50
-#define		RateAdaptiveTH_Low_20M		30
-#define		RateAdaptiveTH_Low_40M		10
+#define		RATE_ADAPTIVE_TH_LOW_20M		30
+#define		RATE_ADAPTIVE_TH_LOW_40M		10
 #define		VeryLowRSSI					15
 #define		CTSToSelfTHVal					30
 
