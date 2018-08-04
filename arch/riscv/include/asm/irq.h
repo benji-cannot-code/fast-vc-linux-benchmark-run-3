2e684823d6d708f7e7af7d32b2c2fd74f25b47a8
Christoph Hellwig <hch@lst.de>
2018-08-04 10:23:14 +0200

    RISC-V: remove INTERRUPT_CAUSE_* defines from asm/irq.h
    These are only of use to the local irq controller driver, so add them in
    that driver implementation instead, which will be submitted soon.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- arch/riscv/include/asm/irq.h
+++ arch/riscv/include/asm/irq.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define NR_IRQS         0
 
-#define INTERRUPT_CAUSE_SOFTWARE    1
-#define INTERRUPT_CAUSE_TIMER       5
-#define INTERRUPT_CAUSE_EXTERNAL    9
-
 void riscv_timer_interrupt(void);
 void riscv_software_interrupt(void);
 
