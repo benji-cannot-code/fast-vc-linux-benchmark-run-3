bfdbce736cd1868580f260dc662111f0b637a610
Gustavo A. R. Silva <gustavo@embeddedor.com>
2018-08-04 21:12:00 -0500

    PM / hibernate: Mark expected switch fall-through
    In preparation to enabling -Wimplicit-fallthrough, mark switch cases
    where we are expecting to fall through.
    
    This addresses Coverity-ID: 114713 ("Missing break in switch").
    
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -639,6 +639,7 @@ static void power_down(void)
 		break;
 	case HIBERNATION_PLATFORM:
 		hibernation_platform_enter();
+		/* Fall through */
 	case HIBERNATION_SHUTDOWN:
 		if (pm_power_off)
 			kernel_power_off();
