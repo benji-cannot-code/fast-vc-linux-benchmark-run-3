640634199b9117db6a5c3c24015848c5a096086b
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-08-06 11:12:07 +0100

    ASoC: sdm845: remove unused header files
    This patch removes unused header files from the driver.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/sdm845.c
+++ sound/soc/qcom/sdm845.c
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
-#include <linux/atomic.h>
 #include <linux/of_device.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
-#include <linux/soc/qcom/apr.h>
 #include "common.h"
 #include "qdsp6/q6afe.h"
 
