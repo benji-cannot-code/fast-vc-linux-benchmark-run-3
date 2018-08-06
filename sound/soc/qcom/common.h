da67ec7bdf7306fc559fd3a49d20cef4507dc7a8
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-08-06 11:12:08 +0100

    ASoC: qcom: remove unused header files from common.h
    This patch removes unused header files from common.h.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/common.h
+++ sound/soc/qcom/common.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __QCOM_SND_COMMON_H__
 #define __QCOM_SND_COMMON_H__
 
-#include <linux/component.h>
 #include <sound/soc.h>
 
 int qcom_snd_parse_of(struct snd_soc_card *card);
