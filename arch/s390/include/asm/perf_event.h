3512a410a74b88d24009fabb380de94bcdca3ea9
Hendrik Brueckner <brueckner@linux.ibm.com>
2018-08-06 15:56:43 +0200

    s390/cpum_cf: move counter set controls to a new header file
    Move counter set specific controls and functions to the asm/cpu_mcf.h
    header file containg all counter facility support definitions.  Also
    adapt few variable names and header file includes.  No functional changes.
    
    Signed-off-by: Hendrik Brueckner <brueckner@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/perf_event.h
+++ arch/s390/include/asm/perf_event.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/perf_event.h>
 #include <linux/device.h>
-#include <asm/cpu_mf.h>
 
 /* Per-CPU flags for PMU states */
 #define PMU_F_RESERVED			0x1000
