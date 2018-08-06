e94cb8e4bd7fbbbd5ebdd51ff6f9fb9ac7123001
Huang Shijie <sjhuang@iluvatar.ai>
2018-08-06 16:52:29 +0800

    dmaengine: pxa_dma: use dmaenginem_async_device_register to simplify the code
    Use dmaenginem_async_device_register to simplify the code:
    	remove dma_async_device_unregister.
    
    Signed-off-by: Huang Shijie <sjhuang@iluvatar.ai>
    Acked-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/pxa_dma.c
+++ drivers/dma/pxa_dma.c
@@ -1286,7 +1286,6 @@ static int pxad_remove(struct platform_device *op)
 
 	pxad_cleanup_debugfs(pdev);
 	pxad_free_channels(&pdev->slave);
-	dma_async_device_unregister(&pdev->slave);
 	return 0;
 }
 
@@ -1397,7 +1396,7 @@ static int pxad_init_dmadev(struct platform_device *op,
 		init_waitqueue_head(&c->wq_state);
 	}
 
-	return dma_async_device_register(&pdev->slave);
+	return dmaenginem_async_device_register(&pdev->slave);
 }
 
 static int pxad_probe(struct platform_device *op)
