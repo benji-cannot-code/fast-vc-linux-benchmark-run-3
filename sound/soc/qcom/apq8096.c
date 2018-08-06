ca780f504cf6afc46fccc0d874c741c23c5bb616
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-08-06 11:12:06 +0100

    ASoC: apq8096: remove unused header files
    This patch removes unused header files from the driver.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/apq8096.c
+++ sound/soc/qcom/apq8096.c
@@ -2,9 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2018, Linaro Limited
 
-#include <linux/soc/qcom/apr.h>
 #include <linux/module.h>
-#include <linux/component.h>
 #include <linux/platform_device.h>
 #include <linux/of_device.h>
 #include <sound/soc.h>
