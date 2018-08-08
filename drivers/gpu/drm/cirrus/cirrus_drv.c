e738289ec36e0ab800d82d4436bfa75cdcfe34f6
Gerd Hoffmann <kraxel@redhat.com>
2018-08-08 13:13:11 +0200

    drm/cirrus: flip default from 24bpp to 16bpp
    The problem with 24bpp is that it is a rather unusual depth these days,
    cirrus is pretty much the only relevant device still using that, and it
    is a endless source of issues.  Wayland doesn't support it at all.  Bugs
    in Xorg keep showing up.
    
    Typically either 32bpp or 16bpp are used.  Using 32bpp would limit the
    resolution to 800x600 due to hardware constrains.  So lets go with 16bpp.
    
    Also use the default depth for the framebuffer console and
    mode_info->preferred_depth.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20180808111311.30311-1-kraxel@redhat.com

--- drivers/gpu/drm/cirrus/cirrus_drv.c
+++ drivers/gpu/drm/cirrus/cirrus_drv.c
@@ -17,11 +17,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cirrus_drv.h"
 
 int cirrus_modeset = -1;
-int cirrus_bpp = 24;
+int cirrus_bpp = 16;
 
 MODULE_PARM_DESC(modeset, "Disable/Enable modesetting");
 module_param_named(modeset, cirrus_modeset, int, 0400);
-MODULE_PARM_DESC(bpp, "Max bits-per-pixel (default:24)");
+MODULE_PARM_DESC(bpp, "Max bits-per-pixel (default:16)");
 module_param_named(bpp, cirrus_bpp, int, 0400);
 
 /*
