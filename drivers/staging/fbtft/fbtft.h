ee4fa0cf98634e0495e773aaed40c45507593e0d
Leonardo Brás <leobras.c@gmail.com>
2018-08-09 20:29:34 -0300

    staging: fbtft: Adds space around "/"
    Kernel coding style recommends a space char around "/".
    
    Signed-off-by: Leonardo Brás <leobras.c@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/fbtft/fbtft.h
+++ drivers/staging/fbtft/fbtft.h
@@ -233,7 +233,7 @@ struct fbtft_par {
 	bool polarity;
 };
 
-#define NUMARGS(...)  (sizeof((int[]){__VA_ARGS__})/sizeof(int))
+#define NUMARGS(...)  (sizeof((int[]){__VA_ARGS__}) / sizeof(int))
 
 #define write_reg(par, ...)                                            \
 	((par)->fbtftops.write_register(par, NUMARGS(__VA_ARGS__), __VA_ARGS__))
