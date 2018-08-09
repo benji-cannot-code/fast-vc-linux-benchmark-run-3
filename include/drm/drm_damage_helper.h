ba25073bdfbb515b0622a0e776cce38e91a11137
Deepak Rawat <drawat@vmware.com>
2018-08-08 17:36:26 -0700

    drm: Add a new helper to validate damage during atomic_check
    This helper function makes sure that damage from plane state is
    discarded for full modeset.
    
    Signed-off-by: Deepak Rawat <drawat@vmware.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reviewed-by: Thomas Hellstrom <thellstrom@vmware.com>
    Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>

--- include/drm/drm_damage_helper.h
+++ include/drm/drm_damage_helper.h
@@ -36,5 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_atomic_helper.h>
 
 void drm_plane_enable_fb_damage_clips(struct drm_plane *plane);
+void drm_atomic_helper_check_plane_damage(struct drm_atomic_state *state,
+					  struct drm_plane_state *plane_state);
 
 #endif
