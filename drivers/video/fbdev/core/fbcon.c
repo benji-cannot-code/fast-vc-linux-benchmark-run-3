25be342460adb6a454ff8a867b6159fbc5ef4c59
Hans de Goede <hdegoede@redhat.com>
2018-08-10 17:23:01 +0200

    fbcon: Only defer console takeover if the current console driver is the dummycon
    We rely on dummycon's output notifier mechanism to defer the takeover.
    
    If say vgacon is the current console driver then dummycon will never get
    used so its output notifier will also never get called and fbcon never
    takes over. This commit fixes this by only deferring the console takeover
    if the current console driver is the dummycon driver.
    
    This commit also moves the entirety of fbcon_start under the console_lock,
    since the conswitchp which fbcon_start now checks is protected by it.
    
    This commit also inlines fbcon_register_output_notifier, since we now
    need a #ifdef CONFIG_FRAMEBUFFER_CONSOLE_DEFERRED_TAKEOVER in fbcon_start
    anyways because of the write access to the deferred_takeover variable,
    this has the added advantage that it puts the
    dummycon_register_output_notifier() call directly after the "conswitchp !=
    &dummy_con" comparison making it clear why that check is there.
    
    Note the arch setup code will set conswitchp to either dummy_con or
    vga_con, in the cases where it gets set to vga_con even though their is
    no vga_con present we rely on vga_con_startup() to set conswitchp to
    dummy_con. vga_con_startup() is guaranteed to happen before
    fb_console_init() as it gets called as a console_initcall where as
    fb_console_init() gets called as a subsys_initcall.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- drivers/video/fbdev/core/fbcon.c
+++ drivers/video/fbdev/core/fbcon.c
@@ -3613,27 +3613,25 @@ static int fbcon_output_notifier(struct notifier_block *nb,
 
 	return NOTIFY_OK;
 }
-
-static void fbcon_register_output_notifier(void)
-{
-	fbcon_output_nb.notifier_call = fbcon_output_notifier;
-	dummycon_register_output_notifier(&fbcon_output_nb);
-}
-#else
-static inline void fbcon_register_output_notifier(void) {}
 #endif
 
 static void fbcon_start(void)
 {
+	WARN_CONSOLE_UNLOCKED();
+
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE_DEFERRED_TAKEOVER
+	if (conswitchp != &dummy_con)
+		deferred_takeover = false;
+
 	if (deferred_takeover) {
-		fbcon_register_output_notifier();
+		fbcon_output_nb.notifier_call = fbcon_output_notifier;
+		dummycon_register_output_notifier(&fbcon_output_nb);
 		return;
 	}
+#endif
 
 	if (num_registered_fb) {
 		int i;
-
-		console_lock();
 
 		for_each_registered_fb(i) {
 			info_idx = i;
@@ -3641,8 +3639,6 @@ static void fbcon_start(void)
 		}
 
 		do_fbcon_takeover(0);
-		console_unlock();
-
 	}
 }
 
@@ -3725,8 +3721,8 @@ void __init fb_console_init(void)
 	for (i = 0; i < MAX_NR_CONSOLES; i++)
 		con2fb_map[i] = -1;
 
-	console_unlock();
 	fbcon_start();
+	console_unlock();
 }
 
 #ifdef MODULE
