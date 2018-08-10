38f1871ba4cc3fc325fc9764a4a2d5849799c643
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2018-08-10 13:48:16 -0700

    Input: iforce - drop bus type from iforce structure
    It is not needed anymore as behavior is controlled by the transport
    operations set up for given device.
    
    Tested-by: Tim Schumacher <timschumi@gmx.de>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/joystick/iforce/iforce.h
+++ drivers/input/joystick/iforce/iforce.h
@@ -39,10 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define IFORCE_MAX_LENGTH	16
 
-/* iforce::bus */
-#define IFORCE_232	1
-#define IFORCE_USB	2
-
 #define IFORCE_EFFECTS_MAX	32
 
 /* Each force feedback effect is made of one core effect, which can be
@@ -106,7 +102,6 @@ struct iforce {
 	struct input_dev *dev;		/* Input device interface */
 	struct iforce_device *type;
 	const struct iforce_xport_ops *xport_ops;
-	int bus;
 
 	spinlock_t xmit_lock;
 	/* Buffer used for asynchronous sending of bytes to the device */
