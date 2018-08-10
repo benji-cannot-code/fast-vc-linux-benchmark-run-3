adaea656c781b7b7c2115a485bb09e6597bced79
Michael Straube <straube.linux@gmail.com>
2018-08-10 09:18:33 +0200

    staging: rtl8188eu: use phydm_reg.h from rtlwifi
    Use rtlwifi/phydm/phydm_reg.h instead of odm_reg.h and
    remove the now unused odm_reg.h.
    
    All defines from odm_reg.h are defined with the same values
    in rtlwifi/phydm/phydm_reg.h.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/odm_precomp.h
+++ drivers/staging/rtl8188eu/include/odm_precomp.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hal8188e_rate_adaptive.h" /* for RA,Power training */
 #include "rtl8188e_hal.h"
 
-#include "odm_reg.h"
+#include "../../rtlwifi/phydm/phydm_reg.h"
 
 #include "odm_rtl8188e.h"
 
