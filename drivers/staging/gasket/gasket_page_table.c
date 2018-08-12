51d9d62f3b98f0f3cebca82273b391bafe3be390
Sumit Kumar <sumit686215@gmail.com>
2018-08-12 12:38:19 +0530

    staging: gasket: remove null ptr check before kfree
    Remove null ptr check before kfree because kfree is null ptr safe.
    Issue found by checkpatch.
    
    Signed-off-by: Sumit Kumar <sumit686215@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gasket/gasket_page_table.c
+++ drivers/staging/gasket/gasket_page_table.c
@@ -1329,10 +1329,8 @@ nomem:
 				  num_pages * PAGE_SIZE, mem, handle);
 	}
 
-	if (gasket_dev->page_table[index]->coherent_pages) {
-		kfree(gasket_dev->page_table[index]->coherent_pages);
-		gasket_dev->page_table[index]->coherent_pages = NULL;
-	}
+	kfree(gasket_dev->page_table[index]->coherent_pages);
+	gasket_dev->page_table[index]->coherent_pages = NULL;
 	gasket_dev->page_table[index]->num_coherent_pages = 0;
 	return -ENOMEM;
 }
