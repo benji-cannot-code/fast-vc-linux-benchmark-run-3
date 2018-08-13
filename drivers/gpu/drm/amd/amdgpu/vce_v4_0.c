d17ceba278a9d42767f4c177ad666faf440c3110
Emily Deng <Emily.Deng@amd.com>
2018-08-13 14:46:06 +0800

    drm/amdgpu/vce: VCE entity initialization relies on ring initializtion
    Entity init should after ring init, as the entity's sched_rq's initialization
    is in ring init.
    
    SWDEV-161495
    
    Signed-off-by: Emily Deng <Emily.Deng@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vce_v4_0.c
+++ drivers/gpu/drm/amd/amdgpu/vce_v4_0.c
@@ -420,6 +420,7 @@ static int vce_v4_0_sw_init(void *handle)
 {
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 	struct amdgpu_ring *ring;
+
 	unsigned size;
 	int r, i;
 
@@ -474,6 +475,11 @@ static int vce_v4_0_sw_init(void *handle)
 		if (r)
 			return r;
 	}
+
+
+	r = amdgpu_vce_entity_init(adev);
+	if (r)
+		return r;
 
 	r = amdgpu_virt_alloc_mm_table(adev);
 	if (r)
