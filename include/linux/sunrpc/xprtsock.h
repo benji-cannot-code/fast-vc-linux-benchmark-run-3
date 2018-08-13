e937fb9c6ae9fa42cbc38dbcfa445abc2b65da55
Trond Myklebust <trond.myklebust@hammerspace.com>
2018-08-13 16:54:57 -0400

    SUNRPC: Add socket transmit queue offset tracking
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- include/linux/sunrpc/xprtsock.h
+++ include/linux/sunrpc/xprtsock.h
@@ -44,6 +44,13 @@ struct sock_xprt {
 	} recv;
 
 	/*
+	 * State of TCP transmit queue
+	 */
+	struct {
+		u32		offset;
+	} xmit;
+
+	/*
 	 * Connection of transports
 	 */
 	unsigned long		sock_state;
