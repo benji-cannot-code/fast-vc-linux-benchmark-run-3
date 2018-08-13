3507a7137d0f35d310e21f824fed3fd0e5b918e2
Max Filippov <jcmvbkbc@gmail.com>
2018-08-12 18:32:13 -0700

    xtensa: drop unused {CONFIG,PLATFORM}_DEFAULT_MEM_SIZE
    Now that noMMU cache attributes are set up separately drop no longer
    used macro PLATFORM_DEFAULT_MEM_SIZE and config symbol
    CONFIG_DEFAULT_MEM_SIZE used for setting it.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/platforms/xtfpga/include/platform/hardware.h
+++ arch/xtensa/platforms/xtfpga/include/platform/hardware.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Memory configuration. */
 
 #define PLATFORM_DEFAULT_MEM_START __XTENSA_UL(CONFIG_DEFAULT_MEM_START)
-#define PLATFORM_DEFAULT_MEM_SIZE  __XTENSA_UL(CONFIG_DEFAULT_MEM_SIZE)
 
 /* Interrupt configuration. */
 
