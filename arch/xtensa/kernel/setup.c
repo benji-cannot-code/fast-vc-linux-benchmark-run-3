7afb047c600ed62830fa5fadc362389ab31b5506
Max Filippov <jcmvbkbc@gmail.com>
2018-08-13 18:14:14 -0700

    xtensa: drop unneeded platform/hardware.h headers
    platform/hardware.h no longer supply any information for processor.h,
    vectors.h, setup.c or vmlinux.lds.S, don't include it.
    This header is now empty in the platforms/iss, so remove it altogether.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/kernel/setup.c
+++ arch/xtensa/kernel/setup.c
@@ -48,8 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/smp.h>
 #include <asm/sysmem.h>
 
-#include <platform/hardware.h>
-
 #if defined(CONFIG_VGA_CONSOLE) || defined(CONFIG_DUMMY_CONSOLE)
 struct screen_info screen_info = {
 	.orig_x = 0,
