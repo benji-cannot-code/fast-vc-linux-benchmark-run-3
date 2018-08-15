6202683f6a7425498286f6d2cf106b05daf96615
Bob Peterson <rpeterso@redhat.com>
2018-08-15 12:09:49 -0500

    gfs2: improve debug information when lvb mismatches are found
    Before this patch, gfs2_rgrp_bh_get would check for lvb mismatches,
    but it wouldn't tell you what was actually wrong. This patch adds
    more information to help us debug it. It also makes rgrp consistency
    checks dump any bad rgrps, and the rgrp dump code dump any lvbs
    as well as the rgrp itself.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Acked-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/util.c
+++ fs/gfs2/util.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "gfs2.h"
 #include "incore.h"
 #include "glock.h"
+#include "rgrp.h"
 #include "util.h"
 
 struct kmem_cache *gfs2_glock_cachep __read_mostly;
@@ -182,6 +183,8 @@ int gfs2_consist_rgrpd_i(struct gfs2_rgrpd *rgd, int cluster_wide,
 {
 	struct gfs2_sbd *sdp = rgd->rd_sbd;
 	int rv;
+
+	gfs2_rgrp_dump(NULL, rgd->rd_gl);
 	rv = gfs2_lm_withdraw(sdp,
 			      "fatal: filesystem consistency error\n"
 			      "  RG = %llu\n"
