7647e5093593ffd5e4f33c733ce98dc12fc572c4
Luke Dashjr <luke@dashjr.org>
2018-08-16 21:36:26 +0000

    powerpc64/ftrace: Include ftrace.h needed for enable/disable calls
    this_cpu_disable_ftrace and this_cpu_enable_ftrace are inlines in
    ftrace.h Without it included, the build fails.
    
    Fixes: a4bc64d305af ("powerpc64/ftrace: Disable ftrace during kvm entry/exit")
    Cc: stable@vger.kernel.org # v4.18+
    Signed-off-by: Luke Dashjr <luke-jr+git@utopios.org>
    Acked-by: Naveen N. Rao <naveen.n.rao at linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kvm/book3s_hv.c
+++ arch/powerpc/kvm/book3s_hv.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/of.h>
 
+#include <asm/ftrace.h>
 #include <asm/reg.h>
 #include <asm/ppc-opcode.h>
 #include <asm/asm-prototypes.h>
