26164d21ed5bb0c275eb2bde9c93fceb848f6b7e
Thomas Zimmermann <tzimmermann@suse.de>
2018-08-17 10:21:38 +0200

    drm/gma500: Do not include <drm/drm_global.h>
    The gma500 driver has no dependencies on drm_global.h. Remove the include
    statement.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180817082138.26628-1-tzimmermann@suse.de

--- drivers/gpu/drm/gma500/psb_drv.h
+++ drivers/gpu/drm/gma500/psb_drv.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kref.h>
 
 #include <drm/drmP.h>
-#include <drm/drm_global.h>
 #include <drm/gma_drm.h>
 #include "psb_reg.h"
 #include "psb_intel_drv.h"
