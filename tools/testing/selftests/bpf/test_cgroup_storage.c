33d96e5640bf2b2a1a6795d7354079aa1a13c1e5
Yonghong Song <yhs@fb.com>
2018-08-17 08:54:15 -0700

    tools/bpf: fix bpf selftest test_cgroup_storage failure
    The bpf selftest test_cgroup_storage failed in one of
    our production test servers.
      # sudo ./test_cgroup_storage
      Failed to create map: Operation not permitted
    
    It turns out this is due to insufficient locked memory
    with system default 16KB.
    
    Similar to other self tests, let us arm the process
    with unlimited locked memory. With this change,
    the test passed.
      # sudo ./test_cgroup_storage
      test_cgroup_storage:PASS
    
    Fixes: 68cfa3ac6b8d ("selftests/bpf: add a cgroup storage test")
    Cc: Roman Gushchin <guro@fb.com>
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Acked-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/test_cgroup_storage.c
+++ tools/testing/selftests/bpf/test_cgroup_storage.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "bpf_rlimit.h"
 #include "cgroup_helpers.h"
 
 char bpf_log_buf[BPF_LOG_BUF_SIZE];
