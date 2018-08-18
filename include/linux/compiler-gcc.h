32e36e96cee06a6be8cb81fb31e98b5c4f20cd33
Linus Torvalds <torvalds@linux-foundation.org>
2018-08-18 12:19:56 -0700

    deprecate the '__deprecated' attribute warnings entirely and for good
    We haven't had lots of deprecation warnings lately, but the rdma use of
    it made them flare up again.
    
    They are not useful.  They annoy everybody, and nobody ever does
    anything about them, because it's always "somebody elses problem".  And
    when people start thinking that warnings are normal, they stop looking
    at them, and the real warnings that mean something go unnoticed.
    
    If you want to get rid of a function, just get rid of it.  Convert every
    user to the new world order.
    
    And if you can't do that, then don't annoy everybody else with your
    marking that says "I couldn't be bothered to fix this, so I'll just spam
    everybody elses build logs with warnings about my laziness".
    
    Make a kernelnewbies wiki page about things that could be cleaned up,
    write a blog post about it, or talk to people on the mailing lists.  But
    don't add warnings to the kernel build about cleanup that you think
    should happen but you aren't doing yourself.
    
    Don't.  Just don't.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -104,7 +104,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __always_inline	inline __attribute__((always_inline))
 #define  noinline	__attribute__((noinline))
 
-#define __deprecated	__attribute__((deprecated))
 #define __packed	__attribute__((packed))
 #define __weak		__attribute__((weak))
 #define __alias(symbol)	__attribute__((alias(#symbol)))
