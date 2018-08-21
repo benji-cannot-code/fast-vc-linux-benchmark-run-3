43abee1c637b6eb00e67c47cbfd2e3f1d7faf321
Heiko Carstens <heiko.carstens@de.ibm.com>
2018-08-21 13:37:57 +0200

    s390: remove gcc version check (4.3 or newer)
    git commit cafa0010cd51 ("Raise the minimum required gcc version to 4.6")
    raised the minimum gcc version to 4.6. Therefore remove the s390 specific
    gcc 4.3 version check, which wasn't sufficient anyway.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/kernel/asm-offsets.c
+++ arch/s390/kernel/asm-offsets.c
@@ -18,14 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/gmap.h>
 #include <asm/nmi.h>
 
-/*
- * Make sure that the compiler is new enough. We want a compiler that
- * is known to work with the "Q" assembler constraint.
- */
-#if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 3)
-#error Your compiler is too old; please use version 4.3 or newer
-#endif
-
 int main(void)
 {
 	/* task struct offsets */
