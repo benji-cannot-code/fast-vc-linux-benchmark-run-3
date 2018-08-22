b1380c3262992f03c2e469828cf6f5d11a88e05f
Jacopo Mondi <jacopo+renesas@jmondi.org>
2018-08-22 09:21:48 +0200

    drm: rcar-du: Write ESCR and OTAR as CRTC registers
    The ESCR and OTAR registers exist in each DU channel, but at different
    offsets for odd and even channels. This led to usage of the group
    register access API to write them, with offsets macros named ESCR/OTAR
    and ESCR2/OTAR2 for the first and second ESCR/OTAR register in the group
    respectively.
    
    The names are confusing as it suggests that the ESCR/OTAR registers for
    DU0 and DU2 are taken into account, especially with writes performed to
    the group register access API.
    
    Rename the offsets to ESCR/OTAR02 and ESCR/OTAR13, and use the CRTC
    register access API to clarify the code. The offsets values are updated
    accordingly.
    
    Cosmetic patch, no functional changes intended.
    
    Signed-off-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    [Squashed ESCR and OTAR changes in a single commit]
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/gpu/drm/rcar-du/rcar_du_regs.h
+++ drivers/gpu/drm/rcar-du/rcar_du_regs.h
@@ -490,8 +490,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * External Synchronization Control Registers
  */
 
-#define ESCR			0x10000
-#define ESCR2			0x31000
+#define ESCR02			0x10000
+#define ESCR13			0x01000
 #define ESCR_DCLKOINV		(1 << 25)
 #define ESCR_DCLKSEL_DCLKIN	(0 << 20)
 #define ESCR_DCLKSEL_CLKS	(1 << 20)
@@ -502,8 +502,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ESCR_SYNCSEL_EXHSYNC	(3 << 8)
 #define ESCR_FRQSEL_MASK	(0x3f << 0)
 
-#define OTAR			0x10004
-#define OTAR2			0x31004
+#define OTAR02			0x10004
+#define OTAR13			0x01004
 
 /* -----------------------------------------------------------------------------
  * Dual Display Output Control Registers
