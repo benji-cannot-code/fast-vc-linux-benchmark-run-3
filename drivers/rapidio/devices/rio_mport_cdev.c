0e5236e17085ee19a14aec2b9a223b080ff496d3
Colin Ian King <colin.king@canonical.com>
2018-08-21 22:01:01 -0700

    drivers/rapidio/devices/rio_mport_cdev.c: remove redundant pointer md
    Pointer md is being assigned but is never used hence it is redundant and
    can be removed.
    
    Cleans up clang warning:
    warning: variable 'md' set but not used [-Wunused-but-set-variable]
    
    Link: http://lkml.kernel.org/r/20180711082346.5223-1-colin.king@canonical.com
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alexandre Bounine <alex.bou9@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rapidio/devices/rio_mport_cdev.c
+++ drivers/rapidio/devices/rio_mport_cdev.c
@@ -1007,7 +1007,6 @@ out_free:
 static int rio_mport_wait_for_async_dma(struct file *filp, void __user *arg)
 {
 	struct mport_cdev_priv *priv;
-	struct mport_dev *md;
 	struct rio_async_tx_wait w_param;
 	struct mport_dma_req *req;
 	dma_cookie_t cookie;
@@ -1017,7 +1016,6 @@ static int rio_mport_wait_for_async_dma(struct file *filp, void __user *arg)
 	int ret;
 
 	priv = (struct mport_cdev_priv *)filp->private_data;
-	md = priv->md;
 
 	if (unlikely(copy_from_user(&w_param, arg, sizeof(w_param))))
 		return -EFAULT;
