e0c1f463ee045e696b517fde3cfec5b2d27abb29
John Whitmore <johnfwhitmore@gmail.com>
2018-08-26 22:14:35 +0100

    staging:rtl8192u: Rename EEPROM_Default_PwDiff - Style
    Rename the constant EEPROM_Default_PwDiff to EEPROM_DEFAULT_PW_DIFF,
    this clears the checkpatch issue with CamelCase naming.
    
    This change is purely a coding style change which should have no
    impact on runtime code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8192U_hw.h
+++ drivers/staging/rtl8192u/r8192U_hw.h
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EEPROM_TX_PW_INDEX_VER		0x27	//0x27
 
 #define EEPROM_DEFAULT_THERNAL_METER		0x7
-#define EEPROM_Default_PwDiff			0x4
+#define EEPROM_DEFAULT_PW_DIFF			0x4
 #define EEPROM_Default_CrystalCap		0x5
 #define EEPROM_Default_TxPower			0x1010
 #define EEPROM_Customer_ID			0x7B	//0x7B:CustomerID
