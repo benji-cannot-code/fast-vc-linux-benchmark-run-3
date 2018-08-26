15a5b49f3d9fe622628b40ba08e37fa93d54651e
John Whitmore <johnfwhitmore@gmail.com>
2018-08-26 23:45:33 +0100

    staging:rtl8192u: Add SPDX-License-Identifier tag - Style
    Add the missing SPDX-License-Identifier tag to clear the checkpatch
    issue. This is a coding style change which should not impact runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8190_rtl8256.h
+++ drivers/staging/rtl8192u/r8190_rtl8256.h
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  *	 This is part of the rtl8180-sa2400 driver
  *	 released under the GPL (See file COPYING for details).
