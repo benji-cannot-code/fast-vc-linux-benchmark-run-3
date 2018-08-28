eeed5b9219001254a9088e4876e3eace81b3d477
Mark Bloch <markb@mellanox.com>
2018-08-28 14:18:49 +0300

    net/mlx5: Export packet reformat alloc/dealloc functions
    This will allow for the RDMA side to allocate packet reformat context.
    
    Signed-off-by: Mark Bloch <markb@mellanox.com>
    Reviewed-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/fs_cmd.c
+++ drivers/net/ethernet/mellanox/mlx5/core/fs_cmd.c
@@ -652,6 +652,7 @@ int mlx5_packet_reformat_alloc(struct mlx5_core_dev *dev,
 	kfree(in);
 	return err;
 }
+EXPORT_SYMBOL(mlx5_packet_reformat_alloc);
 
 void mlx5_packet_reformat_dealloc(struct mlx5_core_dev *dev,
 				  u32 packet_reformat_id)
@@ -667,6 +668,7 @@ void mlx5_packet_reformat_dealloc(struct mlx5_core_dev *dev,
 
 	mlx5_cmd_exec(dev, in, sizeof(in), out, sizeof(out));
 }
+EXPORT_SYMBOL(mlx5_packet_reformat_dealloc);
 
 int mlx5_modify_header_alloc(struct mlx5_core_dev *dev,
 			     u8 namespace, u8 num_actions,
