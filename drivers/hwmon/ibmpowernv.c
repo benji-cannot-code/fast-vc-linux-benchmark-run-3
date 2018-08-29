72b22672394ba38d024cd048189aaec9d3eba3b6
Rob Herring <robh@kernel.org>
2018-08-29 15:02:21 -0500

    hwmon: (ibmpowernv) drop unnecessary OF name NULL checks
    Checking the child node names is pointless as the DT node name can
    never be NULL, so remove it.
    
    Cc: Jean Delvare <jdelvare@suse.com>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: linux-hwmon@vger.kernel.org
    Cc: linuxppc-dev@lists.ozlabs.org
    Signed-off-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/ibmpowernv.c
+++ drivers/hwmon/ibmpowernv.c
@@ -459,9 +459,6 @@ static int populate_attr_groups(struct platform_device *pdev)
 	for_each_child_of_node(opal, np) {
 		const char *label;
 
-		if (np->name == NULL)
-			continue;
-
 		type = get_sensor_type(np);
 		if (type == MAX_SENSOR_TYPE)
 			continue;
@@ -589,9 +586,6 @@ static int create_device_attrs(struct platform_device *pdev)
 		u32 sensor_id;
 		const char *label;
 		enum sensors type;
-
-		if (np->name == NULL)
-			continue;
 
 		type = get_sensor_type(np);
 		if (type == MAX_SENSOR_TYPE)
