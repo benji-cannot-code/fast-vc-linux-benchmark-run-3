bf6b466bc78c5b98d6f543d9152b21f6238f8eca
Linus Walleij <linus.walleij@linaro.org>
2018-08-29 17:08:53 +0200

    gpio: ts5500: Use SPDX header
    Cut some boilerplate, use the SPDX license identifier.
    
    Cc: kernel@savoirfairelinux.com
    Cc: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Cc: Jerome Oufella <jerome.oufella@savoirfairelinux.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-ts5500.c
+++ drivers/gpio/gpio-ts5500.c
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Digital I/O driver for Technologic Systems TS-5500
  *
@@ -17,10 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * TS-5600:
  *   Documentation: http://wiki.embeddedarm.com/wiki/TS-5600
  *   Blocks: LCD port (identical to TS-5500 LCD).
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 
 #include <linux/bitops.h>
