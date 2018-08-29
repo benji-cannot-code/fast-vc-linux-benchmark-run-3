1765731e65c8a200b2b953b1642d758d405581ab
Hendrik Brueckner <brueckner@linux.ibm.com>
2018-08-29 17:46:06 +0200

    s390/cpu_mf: add store cpu counter multiple instruction support
    Add support for the STORE CPU COUNTER MULTIPLE instruction to extract
    a range of counters from a counter set.
    
    An assembler macro is used to create the instruction opcode because
    the counter set identifier is part of the instruction and, thus,
    cannot be easily specified as parameter.
    
    Signed-off-by: Hendrik Brueckner <brueckner@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/cpu_mf.h
+++ arch/s390/include/asm/cpu_mf.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <asm/facility.h>
 
+asm(".include \"asm/cpu_mf-insn.h\"\n");
+
 #define CPU_MF_INT_SF_IAE	(1 << 31)	/* invalid entry address */
 #define CPU_MF_INT_SF_ISE	(1 << 30)	/* incorrect SDBT entry */
 #define CPU_MF_INT_SF_PRA	(1 << 29)	/* program request alert */
@@ -207,6 +209,21 @@ static inline int ecctr(u64 ctr, u64 *val)
 	cc = __ecctr(ctr, &content);
 	if (!cc)
 		*val = content;
+	return cc;
+}
+
+/* Store CPU counter multiple for a particular counter set */
+static inline int stcctm(u8 set, u64 range, u64 *dest)
+{
+	int cc;
+
+	asm volatile (
+		"	STCCTM	%2,%3,%1\n"
+		"	ipm	%0\n"
+		"	srl	%0,28\n"
+		: "=d" (cc)
+		: "Q" (*dest), "d" (range), "i" (set)
+		: "cc", "memory");
 	return cc;
 }
 
