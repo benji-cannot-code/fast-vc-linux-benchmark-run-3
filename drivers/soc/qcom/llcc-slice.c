9df61e8129834496797f8bc89878fc82be5cd982
Niklas Cassel <niklas.cassel@linaro.org>
2018-08-29 09:57:16 +0200

    soc: qcom: llcc-slice: Add missing include of sizes.h
    Add missing include of sizes.h.
    
    drivers/soc/qcom/llcc-slice.c: In function ‘llcc_update_act_ctrl’:
    drivers/soc/qcom/llcc-slice.c:41:44: error: ‘SZ_4K’ undeclared
     #define LLCC_TRP_ACT_CTRLn(n)         (n * SZ_4K)
                                                ^~~~~
    
    Signed-off-by: Niklas Cassel <niklas.cassel@linaro.org>
    Reviewed-by: Vivek Gautam <vivek.gautam@codeaurora.org>
    Reviewed-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

--- drivers/soc/qcom/llcc-slice.c
+++ drivers/soc/qcom/llcc-slice.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/of_device.h>
 #include <linux/regmap.h>
+#include <linux/sizes.h>
 #include <linux/slab.h>
 #include <linux/soc/qcom/llcc-qcom.h>
 
