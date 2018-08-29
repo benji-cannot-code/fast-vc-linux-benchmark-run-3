d6d670965f13593be10c3c1ab604568668c8512d
Stanislaw Gruszka <sgruszka@redhat.com>
2018-08-29 13:16:34 +0200

    mt76: merge mt76x0/regs.h into mt76x02_regs.h
    Add missed definitions for mt76x0 into mt76x02_regs.h. Remove
    mt76x0/regs.h and fixup mt76x0 code to adjust to some diffrent
    registers/fields names.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/mediatek/mt76/mt76x0/mt76x0.h
+++ drivers/net/wireless/mediatek/mt76/mt76x0/mt76x0.h
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/debugfs.h>
 
 #include "../mt76.h"
+#include "../mt76x02_regs.h"
 #include "../mt76x02_mac.h"
-#include "regs.h"
 
 #define MT_CALIBRATE_INTERVAL		(4 * HZ)
 
