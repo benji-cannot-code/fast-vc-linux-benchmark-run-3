23b441c8e83e8a8540f99ed57021a02ca042fc0e
Linus Walleij <linus.walleij@linaro.org>
2018-08-29 17:16:18 +0200

    gpio: ts5500: Delete platform data handling
    The TS5500 GPIO driver apparently supports platform data
    without making any use of it whatsoever. Delete this code,
    last chance to speak up if you think it is needed.
    
    Cc: kernel@savoirfairelinux.com
    Cc: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Cc: Jerome Oufella <jerome.oufella@savoirfairelinux.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-ts5500.c
+++ drivers/gpio/gpio-ts5500.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio/driver.h>
 #include <linux/io.h>
 #include <linux/module.h>
-#include <linux/platform_data/gpio-ts5500.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 
@@ -316,7 +315,6 @@ static void ts5500_disable_irq(struct ts5500_priv *priv)
 static int ts5500_dio_probe(struct platform_device *pdev)
 {
 	enum ts5500_blocks block = platform_get_device_id(pdev)->driver_data;
-	struct ts5500_dio_platform_data *pdata = dev_get_platdata(&pdev->dev);
 	struct device *dev = &pdev->dev;
 	const char *name = dev_name(dev);
 	struct ts5500_priv *priv;
@@ -347,10 +345,6 @@ static int ts5500_dio_probe(struct platform_device *pdev)
 	priv->gpio_chip.set = ts5500_gpio_set;
 	priv->gpio_chip.to_irq = ts5500_gpio_to_irq;
 	priv->gpio_chip.base = -1;
-	if (pdata) {
-		priv->gpio_chip.base = pdata->base;
-		priv->strap = pdata->strap;
-	}
 
 	switch (block) {
 	case TS5500_DIO1:
