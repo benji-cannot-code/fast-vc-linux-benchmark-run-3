41df10b0845900f19c7be7b8a001de830fda1ef3
John Whitmore <johnfwhitmore@gmail.com>
2018-09-01 00:02:35 +0100

    staging:rtl8192u: Rename RTL819xU_MODULE_NAME - Style
    Rename the MACRO RTL819xU_MODULE_NAME to RTL819XU_MODULE_NAME, this
    clears the checkpatch issue with CamelCase naming.
    
    This is a coding style change which should have no impact on runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8192U.h
+++ drivers/staging/rtl8192u/r8192U.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ieee80211/ieee80211.h"
 
 #define RTL8192U
-#define RTL819xU_MODULE_NAME "rtl819xU"
+#define RTL819XU_MODULE_NAME "rtl819xU"
 /* HW security */
 #define MAX_KEY_LEN     61
 #define KEY_BUF_SIZE    5
