0fa208f9885c757359072b45f7316ae1487103a8
Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
2018-08-31 18:00:16 +0200

    Compiler Attributes: remove unneeded sparse (__CHECKER__) tests
    Sparse knows about a few more attributes now, so we can remove
    the __CHECKER__ conditions from them (which, in turn, allow us
    to move some of them later on to compiler_attributes.h).
    
      * assume_aligned: since sparse's commit ffc860b ("sparse:
        ignore __assume_aligned__ attribute"), included in 0.5.1
    
      * error: since sparse's commit 0a04210 ("sparse: Add 'error'
        to ignored attributes"), included in 0.5.0
    
      * hotpatch: since sparse's commit 6043210 ("sparse/parse.c:
        ignore hotpatch attribute"), included in 0.5.1
    
      * warning: since sparse's commit 977365d ("Avoid "attribute
        'warning': unknown attribute" warning"), included in 0.4.2
    
    On top of that, __must_be_array does not need it either because:
    
      * Even ancient versions of sparse do not have a problem
    
      * BUILD_BUG_ON_ZERO() is currently disabled for __CHECKER__
    
    Tested-by: Sedat Dilek <sedat.dilek@gmail.com> # on top of v4.19-rc5, clang 7
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Reviewed-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -77,14 +77,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define __compiletime_object_size(obj) __builtin_object_size(obj, 0)
 
-#ifndef __CHECKER__
 #define __compiletime_warning(message) __attribute__((__warning__(message)))
 #define __compiletime_error(message) __attribute__((__error__(message)))
 
-#ifdef LATENT_ENTROPY_PLUGIN
+#if defined(LATENT_ENTROPY_PLUGIN) && !defined(__CHECKER__)
 #define __latent_entropy __attribute__((latent_entropy))
 #endif
-#endif /* __CHECKER__ */
 
 /*
  * calling noreturn functions, __builtin_unreachable() and __builtin_trap()
@@ -132,7 +130,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* gcc version specific checks */
 
-#if GCC_VERSION >= 40900 && !defined(__CHECKER__)
+#if GCC_VERSION >= 40900
 /*
  * __assume_aligned(n, k): Tell the optimizer that the returned
  * pointer can be assumed to be k modulo n. The second argument is
