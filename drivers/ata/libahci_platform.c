d6b57ddfdbd4a083334ce86c47a5001a39c8f42c
Corentin Labbe <clabbe.montjoie@gmail.com>
2018-09-03 12:01:56 +0200

    ata: ahci_platform: add support for PHY controller regulator
    The SoC R40 AHCI controller need a PHY regulator to work.
    But since the PHY is embedded in the controller, we cannot do a DT node for it,
    since phy-supply works only in node with a PHY compatible.
    So this patch adds a way to add an optional phy-supply regulator on AHCI controller node.
    
    Acked-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/ata/libahci_platform.c
+++ drivers/ata/libahci_platform.c
@@ -158,6 +158,12 @@ int ahci_platform_enable_regulators(struct ahci_host_priv *hpriv)
 			return rc;
 	}
 
+	if (hpriv->phy_regulator) {
+		rc = regulator_enable(hpriv->phy_regulator);
+		if (rc)
+			goto disable_ahci_pwrs;
+	}
+
 	for (i = 0; i < hpriv->nports; i++) {
 		if (!hpriv->target_pwrs[i])
 			continue;
@@ -174,6 +180,9 @@ disable_target_pwrs:
 		if (hpriv->target_pwrs[i])
 			regulator_disable(hpriv->target_pwrs[i]);
 
+	if (hpriv->phy_regulator)
+		regulator_disable(hpriv->phy_regulator);
+disable_ahci_pwrs:
 	if (hpriv->ahci_regulator)
 		regulator_disable(hpriv->ahci_regulator);
 	return rc;
@@ -199,6 +208,8 @@ void ahci_platform_disable_regulators(struct ahci_host_priv *hpriv)
 
 	if (hpriv->ahci_regulator)
 		regulator_disable(hpriv->ahci_regulator);
+	if (hpriv->phy_regulator)
+		regulator_disable(hpriv->phy_regulator);
 }
 EXPORT_SYMBOL_GPL(ahci_platform_disable_regulators);
 /**
@@ -429,6 +440,15 @@ struct ahci_host_priv *ahci_platform_get_resources(struct platform_device *pdev,
 			goto err_out;
 		rc = 0;
 		hpriv->ahci_regulator = NULL;
+	}
+
+	hpriv->phy_regulator = devm_regulator_get_optional(dev, "phy");
+	if (IS_ERR(hpriv->phy_regulator)) {
+		rc = PTR_ERR(hpriv->phy_regulator);
+		if (rc == -EPROBE_DEFER)
+			goto err_out;
+		rc = 0;
+		hpriv->phy_regulator = NULL;
 	}
 
 	if (flags & AHCI_PLATFORM_GET_RESETS) {
