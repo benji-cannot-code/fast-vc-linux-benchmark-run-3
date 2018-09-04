3f590f9df37ea964c12e4b7adfb08e1083b79ee9
Ajay Singh <ajay.kathat@microchip.com>
2018-09-04 12:09:19 +0530

    staging: wilc1000: fix to use correct index to free scanned info in clear_shadow_scan()
    Fixes to use correct index to free the allocated memory for ies
    information. The check was done using 'last_scanned_cnt' index and its
    not correct, so use the correct index ('i') to check for before freeing
    the allocated memory.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
@@ -166,9 +166,9 @@ static void clear_shadow_scan(void)
 		return;
 
 	for (i = 0; i < last_scanned_cnt; i++) {
-		if (last_scanned_shadow[last_scanned_cnt].ies) {
+		if (last_scanned_shadow[i].ies) {
 			kfree(last_scanned_shadow[i].ies);
-			last_scanned_shadow[last_scanned_cnt].ies = NULL;
+			last_scanned_shadow[i].ies = NULL;
 		}
 
 		kfree(last_scanned_shadow[i].join_params);
