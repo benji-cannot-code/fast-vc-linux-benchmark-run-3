876248b706033f9b6c2e316b0864bcdb9bf5e395
Stanislaw Gruszka <sgruszka@redhat.com>
2018-09-04 16:40:55 +0200

    mt76: unify sta_add / sta_remove
    Merge mt76x0 and mt76x0 sta add/remove callback. We drop
    mt76x0_mac_set_ampdu_factor() for now. Need to consider to add
    it to common code, but mt76x2 don't do it so perhaps mt76x0 don't
    need it as well.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt76x02_util.h
+++ drivers/net/wireless/mediatek/mt76/mt76x02_util.h
@@ -22,5 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void mt76x02_configure_filter(struct ieee80211_hw *hw,
 			     unsigned int changed_flags,
 			     unsigned int *total_flags, u64 multicast);
-
+int mt76x02_sta_add(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
+		   struct ieee80211_sta *sta);
+int mt76x02_sta_remove(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
+		      struct ieee80211_sta *sta);
 #endif
