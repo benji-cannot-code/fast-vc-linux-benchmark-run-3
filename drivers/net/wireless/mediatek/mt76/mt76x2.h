8441af3f79b8bdaab5594cbf8376561eee73ad79
Stanislaw Gruszka <sgruszka@redhat.com>
2018-09-04 16:40:54 +0200

    mt76: move wcid fields to common mt76_dev struct
    All current MT devices including new MT7603 type chips support 128
    WCIDs, we can unify wcid data in common mt76_dev structure.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt76x2.h
+++ drivers/net/wireless/mediatek/mt76/mt76x2.h
@@ -96,8 +96,6 @@ struct mt76x2_dev {
 	struct mutex mutex;
 
 	const u16 *beacon_offsets;
-	unsigned long wcid_mask[128 / BITS_PER_LONG];
-
 	int txpower_conf;
 	int txpower_cur;
 
@@ -113,9 +111,6 @@ struct mt76x2_dev {
 	struct delayed_work mac_work;
 
 	u32 aggr_stats[32];
-
-	struct mt76_wcid global_wcid;
-	struct mt76_wcid __rcu *wcid[128];
 
 	spinlock_t irq_lock;
 	u32 irqmask;
