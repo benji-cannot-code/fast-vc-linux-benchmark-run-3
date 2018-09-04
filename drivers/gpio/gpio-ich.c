eb5a835045228df09a8de699a43f49cbec1731fe
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-09-04 14:26:25 +0300

    gpio: ich: Sort headers alphabetically
    Sort header block alphabetically for easy maintenance.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/gpio/gpio-ich.c
+++ drivers/gpio/gpio-ich.c
@@ -20,13 +20,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 
+#include <linux/bitops.h>
+#include <linux/gpio/driver.h>
 #include <linux/ioport.h>
+#include <linux/mfd/lpc_ich.h>
 #include <linux/module.h>
 #include <linux/pci.h>
-#include <linux/gpio/driver.h>
 #include <linux/platform_device.h>
-#include <linux/mfd/lpc_ich.h>
-#include <linux/bitops.h>
 
 #define DRV_NAME "gpio_ich"
 
