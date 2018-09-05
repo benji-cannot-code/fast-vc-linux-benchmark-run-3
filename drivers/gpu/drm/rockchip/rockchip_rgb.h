ff07ead4f859a7f9621a386e61174c9200e0b923
Heiko Stuebner <heiko@sntech.de>
2018-09-05 21:13:02 +0200

    drm/rockchip: rgb: add stub functions when rgb encoder is disabled
    The newly added internal rgb encoder for Rockchip vops is missing
    stubs for the case that the rgb output part is not enabled in the
    kernel config. So add these.
    
    Fixes: 1f0f01515172 (drm/rockchip: Add support for Rockchip Soc RGB output interface)
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    [seanpaul fixed up checkpatch nits]
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180905191302.26023-1-heiko@sntech.de

--- drivers/gpu/drm/rockchip/rockchip_rgb.h
+++ drivers/gpu/drm/rockchip/rockchip_rgb.h
@@ -15,7 +15,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * GNU General Public License for more details.
  */
 
+#ifdef CONFIG_ROCKCHIP_RGB
 struct rockchip_rgb *rockchip_rgb_init(struct device *dev,
 				       struct drm_crtc *crtc,
 				       struct drm_device *drm_dev);
 void rockchip_rgb_fini(struct rockchip_rgb *rgb);
+#else
+static inline struct rockchip_rgb *rockchip_rgb_init(struct device *dev,
+						     struct drm_crtc *crtc,
+						     struct drm_device *drm_dev)
+{
+	return NULL;
+}
+
+static inline void rockchip_rgb_fini(struct rockchip_rgb *rgb)
+{
+}
+#endif
