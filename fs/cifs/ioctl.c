2cf6b2cda53a0eddd8c9494d3f50b3c7fe4413f1
YueHaibing <yuehaibing@huawei.com>
2018-09-06 01:50:43 +0000

    cifs: remove set but not used variable 'cifs_sb'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    fs/cifs/ioctl.c: In function 'cifs_ioctl':
    fs/cifs/ioctl.c:164:23: warning:
     variable 'cifs_sb' set but not used [-Wunused-but-set-variable]
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/ioctl.c
+++ fs/cifs/ioctl.c
@@ -124,7 +124,6 @@ long cifs_ioctl(struct file *filep, unsigned int command, unsigned long arg)
 	struct inode *inode = file_inode(filep);
 	int rc = -ENOTTY; /* strange error - but the precedent */
 	unsigned int xid;
-	struct cifs_sb_info *cifs_sb;
 	struct cifsFileInfo *pSMBFile = filep->private_data;
 	struct cifs_tcon *tcon;
 	__u64	ExtAttrBits = 0;
@@ -132,7 +131,6 @@ long cifs_ioctl(struct file *filep, unsigned int command, unsigned long arg)
 
 	xid = get_xid();
 
-	cifs_sb = CIFS_SB(inode->i_sb);
 	cifs_dbg(FYI, "cifs ioctl 0x%x\n", command);
 	switch (command) {
 		case FS_IOC_GETFLAGS:
