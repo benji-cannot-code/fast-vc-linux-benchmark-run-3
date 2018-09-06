6e3edf098cd5a9c16f6168d045a2b16798ac5a8b
Boris Brezillon <boris.brezillon@bootlin.com>
2018-09-07 00:38:43 +0200

    mtd: rawnand: Get rid of nand_flash_dev forward declation
    nand_scan[with_ids]() have been moved at the end of the file. We can
    now get rid of of the nand_flash_dev forward declaration.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- include/linux/mtd/rawnand.h
+++ include/linux/mtd/rawnand.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 struct nand_chip;
-struct nand_flash_dev;
 
 /* The maximum number of NAND chips in an array */
 #define NAND_MAX_CHIPS		8
