9aeeb0a8c5d2af6293032fb18970b0340d2a0182
Christoph Hellwig <hch@lst.de>
2018-09-06 19:27:24 -0400

    dma-mapping: make the get_required_mask method available unconditionally
    This save some duplication for ia64, and makes the interface more
    general.  In the long run we want each dma_map_ops instance to fill this
    out, but this will take a little more prep work.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/ia64/include/asm/dma-mapping.h
+++ arch/ia64/include/asm/dma-mapping.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/scatterlist.h>
 #include <linux/dma-debug.h>
 
-#define ARCH_HAS_DMA_GET_REQUIRED_MASK
-
 extern const struct dma_map_ops *dma_ops;
 extern struct ia64_machine_vector ia64_mv;
 extern void set_iommu_machvec(void);
