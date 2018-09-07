4d170576e396d257303eca6fb5d286ab37878e26
Grant Grundler <grundler@chromium.org>
2018-09-06 17:27:28 -0700

    ASoC: max98373: usleep_range() needs include/delay.h
    Commit ca917f9fe1a0fab added use of usleep_range() but not
    the corresponding "include <linux/delay.h>". The result is
    with Chrome OS won't build because warnings are forced
    to be errors:
    mnt/host/source/src/third_party/kernel/v4.4/sound/soc/codecs/max98373.c:734:2: error: implicit declaration of function 'usleep_range' [-Werror,-Wimplicit-function-declaration]
            usleep_range(10000, 11000);
            ^
    
    Including delay.h "fixes" this.
    
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Reviewed-by: Benson Leung <bleung@chromium.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/max98373.c
+++ sound/soc/codecs/max98373.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // Copyright (c) 2017, Maxim Integrated
 
 #include <linux/acpi.h>
+#include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/module.h>
 #include <linux/regmap.h>
