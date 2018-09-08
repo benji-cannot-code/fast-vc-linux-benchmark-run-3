b1181c7494537ad5ae99734c4e478f288b82f3f0
John Whitmore <johnfwhitmore@gmail.com>
2018-09-08 10:44:08 +0100

    staging:rtl8192u: Remove typedef from firmware_init_step_e - Style
    Remove the typedef directive from enumerated type firmware_init_step_e
    this clears the checkpatch issue with adding new types to the code.
    
    This is a coding style change which should have no impact on runtime
    code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r819xU_firmware.h
+++ drivers/staging/rtl8192u/r819xU_firmware.h
@@ -6,11 +6,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GET_COMMAND_PACKET_FRAG_THRESHOLD(v) \
 	(4 * ((v) / 4) - 8 - USB_HWDESC_HEADER_LEN)
 
-typedef enum _firmware_init_step {
+enum firmware_init_step_e {
 	FW_INIT_STEP0_BOOT = 0,
 	FW_INIT_STEP1_MAIN = 1,
 	FW_INIT_STEP2_DATA = 2,
-} firmware_init_step_e;
+};
 
 typedef enum _opt_rst_type {
 	OPT_SYSTEM_RESET = 0,
