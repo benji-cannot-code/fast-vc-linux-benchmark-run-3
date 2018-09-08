9d6b91b0b0e352f1edb5660494c314305fda88e0
John Whitmore <johnfwhitmore@gmail.com>
2018-09-08 10:44:07 +0100

    staging:rtl8192u: Refactor GET_COMMAND_PACKET_FRAG_THRESHOLD - Style
    The MACRO GET_COMMAND_PACKET_FRAG_THRESHOLD causes a number of
    checkpatch issues so has been refactored to use braces around the
    parameter 'v' to avoid precedence issues, and to add spaces around
    operators.
    
    These changes are coding style changes which should have no impact
    on runtime code execution.
    
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r819xU_firmware.h
+++ drivers/staging/rtl8192u/r819xU_firmware.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __INC_FIRMWARE_H
 
 #define GET_COMMAND_PACKET_FRAG_THRESHOLD(v) \
-	(4*(v/4) - 8 - USB_HWDESC_HEADER_LEN)
+	(4 * ((v) / 4) - 8 - USB_HWDESC_HEADER_LEN)
 
 typedef enum _firmware_init_step {
 	FW_INIT_STEP0_BOOT = 0,
