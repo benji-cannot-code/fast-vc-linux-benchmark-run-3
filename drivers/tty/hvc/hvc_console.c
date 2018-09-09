92ae27a69c3c711a5dbd823f9cee00a7de2c4a20
Nicholas Piggin <npiggin@gmail.com>
2018-09-09 15:39:16 +1000

    tty: hvc: hvc_write() fix break condition
    Commit 550ddadcc758 ("tty: hvc: hvc_write() may sleep") broke the
    termination condition in case the driver stops accepting characters.
    This can result in unnecessary polling of the busy driver.
    
    Restore it by testing the hvc_push return code.
    
    Tested-by: Matteo Croce <mcroce@redhat.com>
    Tested-by: Jason Gunthorpe <jgg@mellanox.com>
    Tested-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/hvc/hvc_console.c
+++ drivers/tty/hvc/hvc_console.c
@@ -523,6 +523,8 @@ static int hvc_write(struct tty_struct *tty, const unsigned char *buf, int count
 		return -EIO;
 
 	while (count > 0) {
+		int ret = 0;
+
 		spin_lock_irqsave(&hp->lock, flags);
 
 		rsize = hp->outbuf_size - hp->n_outbuf;
@@ -538,9 +540,12 @@ static int hvc_write(struct tty_struct *tty, const unsigned char *buf, int count
 		}
 
 		if (hp->n_outbuf > 0)
-			hvc_push(hp);
+			ret = hvc_push(hp);
 
 		spin_unlock_irqrestore(&hp->lock, flags);
+
+		if (!ret)
+			break;
 
 		if (count) {
 			if (hp->n_outbuf > 0)
