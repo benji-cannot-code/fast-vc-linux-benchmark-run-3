56df55b4071f197fa61f2e92ea84b918eb0805c9
Dengcheng Zhu <dzhu@wavecomp.com>
2018-09-11 14:49:23 -0700

    MIPS: kexec: Relax memory restriction
    We can rely on the system kernel and the dump capture kernel themselves in
    memory usage.
    
    Being restrictive with 512MB limit may cause kexec tool failure on some
    platforms.
    
    Tested-by: Rachel Mozes <rachel.mozes@intel.com>
    Reported-by: Rachel Mozes <rachel.mozes@intel.com>
    Signed-off-by: Dengcheng Zhu <dzhu@wavecomp.com>
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Patchwork: https://patchwork.linux-mips.org/patch/20568/
    Cc: pburton@wavecomp.com
    Cc: ralf@linux-mips.org
    Cc: linux-mips@linux-mips.org

--- arch/mips/include/asm/kexec.h
+++ arch/mips/include/asm/kexec.h
@@ -13,11 +13,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/stacktrace.h>
 
 /* Maximum physical address we can use pages from */
-#define KEXEC_SOURCE_MEMORY_LIMIT (0x20000000)
+#define KEXEC_SOURCE_MEMORY_LIMIT (-1UL)
 /* Maximum address we can reach in physical address mode */
-#define KEXEC_DESTINATION_MEMORY_LIMIT (0x20000000)
+#define KEXEC_DESTINATION_MEMORY_LIMIT (-1UL)
  /* Maximum address we can use for the control code buffer */
-#define KEXEC_CONTROL_MEMORY_LIMIT (0x20000000)
+#define KEXEC_CONTROL_MEMORY_LIMIT (-1UL)
 /* Reserve 3*4096 bytes for board-specific info */
 #define KEXEC_CONTROL_PAGE_SIZE (4096 + 3*4096)
 
