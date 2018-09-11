81e0e34aef2ced0ac82025741f9d4295a63ffcba
Lee Jones <lee.jones@linaro.org>
2018-09-11 11:34:27 +0100

    mfd: at91-usart: Include Device Tree header
    Will ensure 'of_device_id' is declared.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/at91-usart.c
+++ drivers/mfd/at91-usart.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/mfd/core.h>
+#include <linux/of.h>
 #include <linux/property.h>
 
 static struct mfd_cell at91_usart_spi_subdev = {
