d3b85387b14f06f5a8eab7ee65fec621233b29ac
Amit Kucheria <amit.kucheria@linaro.org>
2018-09-12 15:22:46 +0530

    thermal: tsens: Prepare 8916 and 8974 tsens to use SROT and TM address space
    We've already converted over the devicetree of platforms using v2
    version of the TSENS IP to use two address spaces. Now prepare to
    convert over the 8916 and 8974 platforms to use separate SROT and TM
    address spaces.
    
    This patch will work with device trees with one or two address spaces
    because we set the tm_offset in commit 5b1283984fa3 ("thermal: tsens:
    Add support to split up register address space into two").
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/qcom/tsens-common.c
+++ drivers/thermal/qcom/tsens-common.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regmap.h>
 #include "tsens.h"
 
-#define S0_ST_ADDR		0x1030
+#define STATUS_OFFSET		0x30
 #define SN_ADDR_OFFSET		0x4
 #define SN_ST_TEMP_MASK		0x3ff
 #define CAL_DEGC_PT1		30
@@ -108,8 +108,9 @@ int get_temp_common(struct tsens_device *tmdev, int id, int *temp)
 	unsigned int status_reg;
 	int last_temp = 0, ret;
 
-	status_reg = S0_ST_ADDR + s->hw_id * SN_ADDR_OFFSET;
+	status_reg = tmdev->tm_offset + STATUS_OFFSET + s->hw_id * SN_ADDR_OFFSET;
 	ret = regmap_read(tmdev->map, status_reg, &code);
+
 	if (ret)
 		return ret;
 	last_temp = code & SN_ST_TEMP_MASK;
