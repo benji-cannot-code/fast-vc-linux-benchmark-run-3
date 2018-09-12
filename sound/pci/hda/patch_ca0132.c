2589b6d2ad72712d2fcce438c56950fc3bda32d2
Mark Brown <broonie@kernel.org>
2018-09-12 12:31:32 +0100

    ALSA: hda: Fix implicit definition of pci_iomap() on SH
    Include asm/io.h directly so we've got a definition of pci_iomap(), the
    current set of includes do this implicitly on most architectures but not
    on SH.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/pci/hda/patch_ca0132.c
+++ sound/pci/hda/patch_ca0132.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/io.h>
 #include <linux/pci.h>
+#include <asm/io.h>
 #include <sound/core.h>
 #include <sound/hda_codec.h>
 #include "hda_local.h"
