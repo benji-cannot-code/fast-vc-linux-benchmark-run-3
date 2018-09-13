3257c8fad57c3e110fe677ee1847e7f855b3f4b5
YueHaibing <yuehaibing@huawei.com>
2018-09-13 21:47:52 +0800

    RDMA: Remove duplicated include from ib_addr.h
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- include/rdma/ib_addr.h
+++ include/rdma/ib_addr.h
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ip.h>
 #include <rdma/ib_verbs.h>
 #include <rdma/ib_pack.h>
-#include <net/ipv6.h>
 #include <net/net_namespace.h>
 
 /**
