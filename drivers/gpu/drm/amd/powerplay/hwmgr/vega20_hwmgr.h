cc4e7385968bf88ebf2124b1195188b9d35e1f82
Evan Quan <evan.quan@amd.com>
2018-09-13 16:14:33 +0800

    drm/amd/powerplay: update OD to take voltage value instead of offset
    With the latest SMC fw, we are able to get the voltage value for
    specific frequency point. So, we update the OD relates to take
    absolute voltage instead of offset.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/hwmgr/vega20_hwmgr.h
+++ drivers/gpu/drm/amd/powerplay/hwmgr/vega20_hwmgr.h
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VG20_PSUEDO_NUM_DCEFCLK_DPM_LEVELS  8
 #define VG20_PSUEDO_NUM_UCLK_DPM_LEVELS     4
 
+//OverDriver8 macro defs
+#define AVFS_CURVE 0
+#define OD8_HOTCURVE_TEMPERATURE 85
+
 typedef uint32_t PP_Clock;
 
 enum {
--- drivers/gpu/drm/amd/powerplay/inc/vega20_ppsmc.h
+++ drivers/gpu/drm/amd/powerplay/inc/vega20_ppsmc.h
@@ -118,7 +118,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PPSMC_MSG_PrepareMp1ForReset             0x59
 #define PPSMC_MSG_PrepareMp1ForShutdown          0x5A
 #define PPSMC_MSG_SetMGpuFanBoostLimitRpm        0x5D
-#define PPSMC_Message_Count                      0x5E
+#define PPSMC_MSG_GetAVFSVoltageByDpm            0x5F
+#define PPSMC_Message_Count                      0x60
 
 typedef uint32_t PPSMC_Result;
 typedef uint32_t PPSMC_Msg;
