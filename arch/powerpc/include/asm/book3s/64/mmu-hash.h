f10ab5ded55ffb88656a716e584b8c25750868ca
Nicholas Piggin <npiggin@gmail.com>
2018-09-15 01:30:48 +1000

    powerpc/64s/hash: remove the vmalloc segment from the bolted SLB
    Remove the vmalloc segment from bolted SLBEs. This is not required to
    be bolted, and seems like it was added to help pre-load the SLB on
    context switch. However there are now other segments like the vmemmap
    segment and non-zero node memory that often take misses after a context
    switch, so it is better to solve this in a more general way.
    
    A subsequent change will track free SLB entries and uses those rather
    than round-robin overwrite valid entries, which makes it far less
    likely for kernel SLBEs to be evicted after they are installed.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/book3s/64/mmu-hash.h
+++ arch/powerpc/include/asm/book3s/64/mmu-hash.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * SLB
  */
 
-#define SLB_NUM_BOLTED		3
+#define SLB_NUM_BOLTED		2
 #define SLB_CACHE_ENTRIES	8
 #define SLB_MIN_SIZE		32
 
