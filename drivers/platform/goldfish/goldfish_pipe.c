82694c23a551ff23122e90527a58637e53f95fec
Roman Kiryanov <rkir@google.com>
2018-09-14 10:51:04 -0700

    platform: goldfish: pipe: Remove a redundant blank line
    The blank line is not required there.
    
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/platform/goldfish/goldfish_pipe.c
+++ drivers/platform/goldfish/goldfish_pipe.c
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * exchange is properly mapped during a transfer.
  */
 
-
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
 #include <linux/interrupt.h>
