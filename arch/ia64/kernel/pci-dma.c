9ca2c382d7aeed8056f39fc752be5739255f2701
Christoph Hellwig <hch@lst.de>
2018-09-17 19:10:26 +0200

    ia64: remove the unused bad_dma_address symbol
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/pci-dma.c
+++ arch/ia64/kernel/pci-dma.c
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <asm/page.h>
 
-dma_addr_t bad_dma_address __read_mostly;
-EXPORT_SYMBOL(bad_dma_address);
-
 int no_iommu __read_mostly;
 #ifdef CONFIG_IOMMU_DEBUG
 int force_iommu __read_mostly = 1;
