f6ead525a67525c1a9478b3e75b586b3f73ab43c
YueHaibing <yuehaibing@huawei.com>
2018-09-18 10:43:43 +0800

    liquidio: remove duplicated include from lio_vf_rep.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cavium/liquidio/lio_vf_rep.c
+++ drivers/net/ethernet/cavium/liquidio/lio_vf_rep.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "octeon_network.h"
 #include <net/switchdev.h>
 #include "lio_vf_rep.h"
-#include "octeon_network.h"
 
 static int lio_vf_rep_open(struct net_device *ndev);
 static int lio_vf_rep_stop(struct net_device *ndev);
