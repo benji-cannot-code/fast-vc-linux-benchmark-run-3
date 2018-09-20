325761d44bb6c2acb07759d639aeee2d17357b61
Suzuki K Poulose <suzuki.poulose@arm.com>
2018-09-20 13:18:11 -0600

    coresight: Add support for CLAIM tag protocol
    Coresight architecture defines CLAIM tags for a device to negotiate
    control of the components (external agent vs self-hosted). Each device
    has a pair of registers (CLAIMSET & CLAIMCLR) for managing the CLAIM
    tags. However, the protocol for the CLAIM tags is IMPLEMENTATION DEFINED.
    PSCI has recommendations for the use of the CLAIM tags to negotiate
    controls for external agent vs self-hosted use. This patch implements
    the recommended protocol by PSCI.
    
    The claim/disclaim operations are performed from the device specific
    drivers. The disadvantage is that the calls are sprinkled in each driver,
    but this makes the operation much simpler.
    
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/coresight/coresight-priv.h
+++ drivers/hwtracing/coresight/coresight-priv.h
@@ -26,6 +26,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CORESIGHT_DEVID		0xfc8
 #define CORESIGHT_DEVTYPE	0xfcc
 
+
+/*
+ * Coresight device CLAIM protocol.
+ * See PSCI - ARM DEN 0022D, Section: 6.8.1 Debug and Trace save and restore.
+ */
+#define CORESIGHT_CLAIM_SELF_HOSTED	BIT(1)
+
 #define TIMEOUT_US		100
 #define BMVAL(val, lsb, msb)	((val & GENMASK(msb, lsb)) >> lsb)
 
