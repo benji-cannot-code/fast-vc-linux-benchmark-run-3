4c2ea3f8e49ab717bda456e835d0c9dfb2627806
Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
2018-09-19 17:23:08 -0700

    ice: Move common functions out of ice_main.c part 5/7
    This patch continues the code move out of ice_main.c
    
    The following top level functions (and related dependency functions) were
    moved to ice_lib.c:
    ice_vsi_clear
    ice_vsi_close
    ice_vsi_free_arrays
    ice_vsi_map_rings_to_vectors
    
    Signed-off-by: Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ice/ice_lib.h
+++ drivers/net/ethernet/intel/ice/ice_lib.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ice.h"
 
+void ice_vsi_map_rings_to_vectors(struct ice_vsi *vsi);
+
 int ice_vsi_alloc_rings(struct ice_vsi *vsi);
 
 void ice_vsi_set_rss_params(struct ice_vsi *vsi);
@@ -16,6 +18,8 @@ void ice_vsi_set_num_qs(struct ice_vsi *vsi);
 int ice_get_free_slot(void *array, int size, int curr);
 
 int ice_vsi_init(struct ice_vsi *vsi);
+
+void ice_vsi_free_arrays(struct ice_vsi *vsi, bool free_qvectors);
 
 void ice_vsi_clear_rings(struct ice_vsi *vsi);
 
@@ -51,6 +55,10 @@ int ice_vsi_stop_tx_rings(struct ice_vsi *vsi);
 int ice_cfg_vlan_pruning(struct ice_vsi *vsi, bool ena);
 
 void ice_vsi_delete(struct ice_vsi *vsi);
+
+int ice_vsi_clear(struct ice_vsi *vsi);
+
+void ice_vsi_close(struct ice_vsi *vsi);
 
 int ice_free_res(struct ice_res_tracker *res, u16 index, u16 id);
 
