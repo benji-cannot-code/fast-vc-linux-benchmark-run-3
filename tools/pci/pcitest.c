c4c09ecf2d996b10c44aa5480d84a609506be72c
Jean-Jacques Hiblot <jjhiblot@ti.com>
2018-09-20 17:02:53 +0200

    tools: PCI: Exit with error code when test fails
    This makes it easier to use pcitest in automated setups.
    
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Acked-by: Kishon Vijay Abraham I <kishon@ti.com>

--- tools/pci/pcitest.c
+++ tools/pci/pcitest.c
@@ -141,6 +141,7 @@ static void run_test(struct pci_test *test)
 	}
 
 	fflush(stdout);
+	return (ret < 0) ? ret : 1 - ret; /* return 0 if test succeeded */
 }
 
 int main(int argc, char **argv)
@@ -229,6 +230,5 @@ usage:
 		return -EINVAL;
 	}
 
-	run_test(test);
-	return 0;
+	return run_test(test);
 }
