d707445682f7105da2a0b1e61a6809b03a5dd877
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-09-21 06:40:26 -0700

    nvmem: make nvmem_add_cells() static
    Now we have new api nvmem_add/del_cell_table() we do not want users to use
    nvmem_add_cells() anymore. So mark it accordingly. I guess it was missed in
    original cleanup patch.
    
    This also fixes below warning:
    core.c:355:5: warning: no previous prototype for 'nvmem_add_cells'
     [-Wmissing-prototypes]
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/nvmem/core.c
+++ drivers/nvmem/core.c
@@ -353,7 +353,7 @@ static int nvmem_cell_info_to_nvmem_cell(struct nvmem_device *nvmem,
  *
  * Return: 0 or negative error code on failure.
  */
-int nvmem_add_cells(struct nvmem_device *nvmem,
+static int nvmem_add_cells(struct nvmem_device *nvmem,
 		    const struct nvmem_cell_info *info,
 		    int ncells)
 {
@@ -392,7 +392,6 @@ err:
 
 	return rval;
 }
-EXPORT_SYMBOL_GPL(nvmem_add_cells);
 
 /*
  * nvmem_setup_compat() - Create an additional binary entry in
