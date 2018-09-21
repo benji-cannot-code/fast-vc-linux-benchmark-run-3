c500a57ff1f2ff71cccfcf0399d4cbd8ed566cd7
Amir Goldstein <amir73il@gmail.com>
2018-09-21 21:20:30 +0300

    fanotify: store fanotify_init() flags in group's fanotify_data
    This averts the need to re-generate flags in fanotify_show_fdinfo()
    and sets the scene for addition of more upcoming flags without growing
    new members to the fanotify_data struct.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- include/linux/fanotify.h
+++ include/linux/fanotify.h
@@ -7,4 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* not valid from userspace, only kernel internal */
 #define FAN_MARK_ONDIR		0x00000100
+
+#define FAN_GROUP_FLAG(group, flag) \
+	((group)->fanotify_data.flags & (flag))
+
 #endif /* _LINUX_FANOTIFY_H */
