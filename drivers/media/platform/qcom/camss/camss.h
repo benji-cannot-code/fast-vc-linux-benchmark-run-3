a7b322c2b394de7ab2eebdc1c78bcd39f7261e3e
zhong jiang <zhongjiang@huawei.com>
2018-09-20 23:30:20 -0400

    media: qcom: remove duplicated include file
    We include device.h twice in camss.h. It's unnecessary.
    hence just remove it.
    
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>
    Acked-by: Todor Tomov <todor.tomov@linaro.org>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/qcom/camss/camss.h
+++ drivers/media/platform/qcom/camss/camss.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-subdev.h>
 #include <media/media-device.h>
 #include <media/media-entity.h>
-#include <linux/device.h>
 
 #include "camss-csid.h"
 #include "camss-csiphy.h"
