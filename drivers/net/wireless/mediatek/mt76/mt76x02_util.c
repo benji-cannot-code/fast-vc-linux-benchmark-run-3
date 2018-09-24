011a67b725160a7de0cab6e2bfa2ef5488a6c330
Felix Fietkau <nbd@nbd.name>
2018-09-24 11:13:11 +0200

    mt76: include linux/module.h in files using MODULE_*
    Fix source files using MODULE_* macros without including linux/module.h
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt76x02_util.c
+++ drivers/net/wireless/mediatek/mt76/mt76x02_util.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <linux/module.h>
 #include "mt76.h"
 #include "mt76x02_dma.h"
 #include "mt76x02_regs.h"
