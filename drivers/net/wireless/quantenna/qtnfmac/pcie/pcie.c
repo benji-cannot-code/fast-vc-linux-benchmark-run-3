5c8ba573197affe3e9f9ecc4f8a3a7e75b3434ef
Igor Mitsyanko <igor.mitsyanko.os@quantenna.com>
2018-09-24 15:15:13 -0700

    qtnfmac: wait for FW load work to finish at PCIe remove
    Waiting for "completion" to be set in FW load thread can not be used
    in case PCIe remove is called before FW load work was scheduled.
    Just wait for work completion instead to avoid problems.
    
    Signed-off-by: Igor Mitsyanko <igor.mitsyanko.os@quantenna.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/quantenna/qtnfmac/pcie/pcie.c
+++ drivers/net/wireless/quantenna/qtnfmac/pcie/pcie.c
@@ -131,7 +131,6 @@ void qtnf_pcie_fw_boot_done(struct qtnf_bus *bus, bool boot_success,
 		bus->fw_state = QTNF_FW_STATE_DETACHED;
 	}
 
-	complete(&bus->firmware_init_complete);
 	put_device(&pdev->dev);
 }
 
@@ -305,7 +304,6 @@ int qtnf_pcie_probe(struct pci_dev *pdev, size_t priv_size,
 	pcie_priv->pdev = pdev;
 	pcie_priv->tx_stopped = 0;
 
-	init_completion(&bus->firmware_init_complete);
 	mutex_init(&bus->bus_lock);
 	spin_lock_init(&pcie_priv->tx_lock);
 	spin_lock_init(&pcie_priv->tx_reclaim_lock);
@@ -377,7 +375,7 @@ static void qtnf_pcie_free_shm_ipc(struct qtnf_pcie_bus_priv *priv)
 
 void qtnf_pcie_remove(struct qtnf_bus *bus, struct qtnf_pcie_bus_priv *priv)
 {
-	wait_for_completion(&bus->firmware_init_complete);
+	cancel_work_sync(&bus->fw_work);
 
 	if (bus->fw_state == QTNF_FW_STATE_ACTIVE ||
 	    bus->fw_state == QTNF_FW_STATE_EP_DEAD)
