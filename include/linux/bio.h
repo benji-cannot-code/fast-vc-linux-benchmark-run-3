5a7679a3c26b316c2a9e80117a51a62964c71853
Christoph Hellwig <hch@lst.de>
2018-09-24 09:43:54 +0200

    block: don't include io.h from bio.h
    Now that we don't need an override for BIOVEC_PHYS_MERGEABLE there is
    no need to drag this header in.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/bio.h
+++ include/linux/bio.h
@@ -25,9 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bug.h>
 
 #ifdef CONFIG_BLOCK
-
-#include <asm/io.h>
-
 /* struct bio, bio_vec and BIO_* flags are defined in blk_types.h */
 #include <linux/blk_types.h>
 
