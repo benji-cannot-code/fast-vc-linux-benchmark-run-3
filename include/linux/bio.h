26d48ddcffbfe2ed197b646dfbbcd15d1d78cc07
Christoph Hellwig <hch@lst.de>
2018-09-24 09:43:55 +0200

    block: don't include bug.h from bio.h
    No need to pull in the BUG() defintion.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/bio.h
+++ include/linux/bio.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/highmem.h>
 #include <linux/mempool.h>
 #include <linux/ioprio.h>
-#include <linux/bug.h>
 
 #ifdef CONFIG_BLOCK
 /* struct bio, bio_vec and BIO_* flags are defined in blk_types.h */
