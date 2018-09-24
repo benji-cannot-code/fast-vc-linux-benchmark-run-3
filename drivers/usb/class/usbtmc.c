b633a70159e88961d6052b85d13faf22c6786ab5
Guido Kiener <guido.kiener@rohde-schwarz.com>
2018-09-25 01:30:29 +0200

    usb: usbtmc: Fix memory leak in usbtmc_ioctl_request
    Kernel memory is allocated twice in new function
    usbtmc_ioctl_request and creates a memory leak.
    This fix removes the superfluous kmalloc().
    
    Signed-off-by: Guido Kiener <guido.kiener@rohde-schwarz.com>
    Fixes: 658f24f4523e ("usb: usbtmc: Add ioctl for generic requests on control")
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/class/usbtmc.c
+++ drivers/usb/class/usbtmc.c
@@ -1896,10 +1896,6 @@ static int usbtmc_ioctl_request(struct usbtmc_device_data *data,
 	if (res)
 		return -EFAULT;
 
-	buffer = kmalloc(request.req.wLength, GFP_KERNEL);
-	if (!buffer)
-		return -ENOMEM;
-
 	if (request.req.wLength > USBTMC_BUFSIZE)
 		return -EMSGSIZE;
 
