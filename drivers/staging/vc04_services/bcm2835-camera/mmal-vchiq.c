4a2a033a025c7883ffe1fe445f4e4915426e7258
Aymen Qader <qader.aymen@gmail.com>
2018-09-25 09:34:00 +0100

    staging: bcm2835-camera: Remove unused header
    Remove the asm/cacheflush.h header include from mmal-vchiq.c. This
    dependency was removed in:
    
    commit 7e8dbea41032 ("staging: bcm2835-camera: Remove explicit cache
    flush operations")
    
    Signed-off-by: Aymen Qader <qader.aymen@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/bcm2835-camera/mmal-vchiq.c
+++ drivers/staging/vc04_services/bcm2835-camera/mmal-vchiq.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/completion.h>
 #include <linux/vmalloc.h>
-#include <asm/cacheflush.h>
 #include <media/videobuf2-vmalloc.h>
 
 #include "mmal-common.h"
