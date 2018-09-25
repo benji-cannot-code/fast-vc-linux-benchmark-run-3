e9cea5691e34a0e0f8fbfbe15ba053b85d9b13f4
Ajay Singh <ajay.kathat@microchip.com>
2018-09-25 11:53:27 +0530

    staging: wilc1000: set default value of cfg response type in wilc_wlan_cfg_indicate_rx()
    Handle the setting of default value for 'wilc_cfg_rsp' type for all
    cases in wilc_wlan_cfg_indicate_rx() as the caller make use of this
    value to know the type of the received message.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wlan_cfg.c
+++ drivers/staging/wilc1000/wilc_wlan_cfg.c
@@ -508,6 +508,7 @@ void wilc_wlan_cfg_indicate_rx(struct wilc *wilc, u8 *frame, int size,
 	msg_id = frame[1];      /* seq no */
 	frame += 4;
 	size -= 4;
+	rsp->type = 0;
 
 	/*
 	 * The valid types of response messages are
@@ -533,7 +534,6 @@ void wilc_wlan_cfg_indicate_rx(struct wilc *wilc, u8 *frame, int size,
 
 	case 'N':
 		wilc_network_info_received(wilc, frame - 4, size + 4);
-		rsp->type = 0;
 		break;
 
 	case 'S':
@@ -541,7 +541,6 @@ void wilc_wlan_cfg_indicate_rx(struct wilc *wilc, u8 *frame, int size,
 		break;
 
 	default:
-		rsp->type = 0;
 		rsp->seq_no = msg_id;
 		break;
 	}
