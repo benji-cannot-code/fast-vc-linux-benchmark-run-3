c6af66f0e51115ea617ed038f6a59877e9c04784
Tim Collier <osdevtc@gmail.com>
2018-09-26 23:05:58 +0100

    staging: wlan-ng: rename DIDmsg_p2req_flashdl_write in p80211metadef.h
    Rename DIDmsg_p2req_flashdl_write in p80211metadef.h to
    DIDMSG_P2REQ_FLASHDL_WRITE to fix "Avoid CamelCase" message from
    checkpatch and conform to the coding style guidelines.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/p80211metadef.h
+++ drivers/staging/wlan-ng/p80211metadef.h
@@ -149,7 +149,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DIDMSG_P2REQ_FLASHDL_STATE \
 			(P80211DID_MKSECTION(5) | \
 			P80211DID_MKGROUP(13))
-#define DIDmsg_p2req_flashdl_write \
+#define DIDMSG_P2REQ_FLASHDL_WRITE \
 			(P80211DID_MKSECTION(5) | \
 			P80211DID_MKGROUP(14))
 #define DIDmib_cat_dot11smt \
