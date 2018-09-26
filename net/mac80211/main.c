b52f0ac9476bac2f74a5dd78f480b35458dde0a1
Wei Yongjun <weiyongjun1@huawei.com>
2018-09-26 12:17:17 +0000

    mac80211: fix error handling in ieee80211_register_hw()
    Fix to return a negative error code -ENOMEM from the kmemdup
    error handling case instead of 0.
    
    Fixes: 09b4a4faf9d0 ("mac80211: introduce capability flags for VHT EXT NSS support")
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/main.c
+++ net/mac80211/main.c
@@ -1204,8 +1204,10 @@ int ieee80211_register_hw(struct ieee80211_hw *hw)
 			continue;
 
 		sband = kmemdup(sband, sizeof(*sband), GFP_KERNEL);
-		if (!sband)
+		if (!sband) {
+			result = -ENOMEM;
 			goto fail_rate;
+		}
 
 		wiphy_dbg(hw->wiphy, "copying sband (band %d) due to VHT EXT NSS BW flag\n",
 			  band);
