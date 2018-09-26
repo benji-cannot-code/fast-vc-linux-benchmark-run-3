93a23af66e3b2e0117fdf3624d42a0f3daa82f6d
YueHaibing <yuehaibing@huawei.com>
2018-09-26 19:09:40 +0800

    scsi: megaraid_mbox: remove set but not used variables
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/scsi/megaraid/megaraid_mbox.c: In function 'megaraid_reset_handler':
    drivers/scsi/megaraid/megaraid_mbox.c:2580:7: warning:
     variable 'recovering' set but not used [-Wunused-but-set-variable]
    
    drivers/scsi/megaraid/megaraid_mbox.c: In function 'mbox_post_sync_cmd':
    drivers/scsi/megaraid/megaraid_mbox.c:2728:12: warning:
     variable 'mbox64' set but not used [-Wunused-but-set-variable]
    
    drivers/scsi/megaraid/megaraid_mbox.c: In function 'megaraid_mbox_support_random_del':
    drivers/scsi/megaraid/megaraid_mbox.c:3138:11: warning:
     variable 'mbox' set but not used [-Wunused-but-set-variable]
    
    drivers/scsi/megaraid/megaraid_mbox.c: In function 'megaraid_mbox_flush_cache':
    drivers/scsi/megaraid/megaraid_mbox.c:3266:10: warning:
     variable 'mbox' set but not used [-Wunused-but-set-variable]
    
    drivers/scsi/megaraid/megaraid_mbox.c: In function 'megaraid_mbox_fire_sync_cmd':
    drivers/scsi/megaraid/megaraid_mbox.c:3302:12: warning:
     variable 'mbox64' set but not used [-Wunused-but-set-variable]
    
    drivers/scsi/megaraid/megaraid_mbox.c: In function 'gather_hbainfo':
    drivers/scsi/megaraid/megaraid_mbox.c:3797:10: warning:
     variable 'dmajor' set but not used [-Wunused-but-set-variable]
    
    [mkp: applied by hand due to conflict with hch's DMA cleanup]
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/megaraid/megaraid_mbox.c
+++ drivers/scsi/megaraid/megaraid_mbox.c
@@ -203,13 +203,6 @@ module_param_named(debug_level, mraid_debug_level, int, 0);
 MODULE_PARM_DESC(debug_level, "Debug level for driver (default=0)");
 
 /*
- * ### global data ###
- */
-static uint8_t megaraid_mbox_version[8] =
-	{ 0x02, 0x20, 0x04, 0x06, 3, 7, 20, 5 };
-
-
-/*
  * PCI table for all supported controllers.
  */
 static struct pci_device_id pci_id_table_g[] =  {
@@ -2545,7 +2538,6 @@ megaraid_reset_handler(struct scsi_cmnd *scp)
 	uint8_t		raw_mbox[sizeof(mbox_t)];
 	int		rval;
 	int		recovery_window;
-	int		recovering;
 	int		i;
 	uioc_t		*kioc;
 
@@ -2558,7 +2550,6 @@ megaraid_reset_handler(struct scsi_cmnd *scp)
 			"megaraid: hw error, cannot reset\n"));
 		return FAILED;
 	}
-
 
 	// Under exceptional conditions, FW can take up to 3 minutes to
 	// complete command processing. Wait for additional 2 minutes for the
@@ -2607,8 +2598,6 @@ megaraid_reset_handler(struct scsi_cmnd *scp)
 	}
 
 	recovery_window = MBOX_RESET_WAIT + MBOX_RESET_EXT_WAIT;
-
-	recovering = adapter->outstanding_cmds;
 
 	for (i = 0; i < recovery_window; i++) {
 
@@ -2693,13 +2682,10 @@ static int
 mbox_post_sync_cmd(adapter_t *adapter, uint8_t raw_mbox[])
 {
 	mraid_device_t	*raid_dev = ADAP2RAIDDEV(adapter);
-	mbox64_t	*mbox64;
 	mbox_t		*mbox;
 	uint8_t		status;
 	int		i;
 
-
-	mbox64	= raid_dev->mbox64;
 	mbox	= raid_dev->mbox;
 
 	/*
@@ -3103,7 +3089,6 @@ megaraid_mbox_support_ha(adapter_t *adapter, uint16_t *init_id)
 static int
 megaraid_mbox_support_random_del(adapter_t *adapter)
 {
-	mbox_t		*mbox;
 	uint8_t		raw_mbox[sizeof(mbox_t)];
 	int		rval;
 
@@ -3124,8 +3109,6 @@ megaraid_mbox_support_random_del(adapter_t *adapter)
 		con_log(CL_DLEVEL1, ("megaraid: disable random deletion\n"));
 		return 0;
 	}
-
-	mbox = (mbox_t *)raw_mbox;
 
 	memset((caddr_t)raw_mbox, 0, sizeof(mbox_t));
 
@@ -3231,11 +3214,7 @@ megaraid_mbox_enum_raid_scsi(adapter_t *adapter)
 static void
 megaraid_mbox_flush_cache(adapter_t *adapter)
 {
-	mbox_t	*mbox;
 	uint8_t	raw_mbox[sizeof(mbox_t)];
-
-
-	mbox = (mbox_t *)raw_mbox;
 
 	memset((caddr_t)raw_mbox, 0, sizeof(mbox_t));
 
@@ -3267,7 +3246,6 @@ megaraid_mbox_fire_sync_cmd(adapter_t *adapter)
 	mbox_t	*mbox;
 	uint8_t	raw_mbox[sizeof(mbox_t)];
 	mraid_device_t	*raid_dev = ADAP2RAIDDEV(adapter);
-	mbox64_t *mbox64;
 	int	status = 0;
 	int i;
 	uint32_t dword;
@@ -3278,7 +3256,6 @@ megaraid_mbox_fire_sync_cmd(adapter_t *adapter)
 
 	raw_mbox[0] = 0xFF;
 
-	mbox64	= raid_dev->mbox64;
 	mbox	= raid_dev->mbox;
 
 	/* Wait until mailbox is free */
@@ -3762,10 +3739,6 @@ megaraid_mbox_mm_done(adapter_t *adapter, scb_t *scb)
 static int
 gather_hbainfo(adapter_t *adapter, mraid_hba_info_t *hinfo)
 {
-	uint8_t	dmajor;
-
-	dmajor			= megaraid_mbox_version[0];
-
 	hinfo->pci_vendor_id	= adapter->pdev->vendor;
 	hinfo->pci_device_id	= adapter->pdev->device;
 	hinfo->subsys_vendor_id	= adapter->pdev->subsystem_vendor;
