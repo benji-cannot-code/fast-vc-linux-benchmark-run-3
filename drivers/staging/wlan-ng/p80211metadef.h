b091da8ae76ef99455c1bf2d6a1b1ae06075c8a7
Tim Collier <osdevtc@gmail.com>
2018-09-26 23:05:59 +0100

    staging: wlan-ng: rename DIDmib_cat_dot11smt in p80211metadef.h
    Rename DIDmib_cat_dot11smt in p80211metadef.h to DIDMIB_CAT_DOT11SMT
    to fix "Avoid CamelCase" message from checkpatch and conform to the
    coding style guidelines.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/p80211metadef.h
+++ drivers/staging/wlan-ng/p80211metadef.h
@@ -152,7 +152,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DIDMSG_P2REQ_FLASHDL_WRITE \
 			(P80211DID_MKSECTION(5) | \
 			P80211DID_MKGROUP(14))
-#define DIDmib_cat_dot11smt \
+#define DIDMIB_CAT_DOT11SMT \
 			P80211DID_MKSECTION(1)
 #define DIDmib_dot11smt_dot11WEPDefaultKeysTable \
 			(P80211DID_MKSECTION(1) | \
