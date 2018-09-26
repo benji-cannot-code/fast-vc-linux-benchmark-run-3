2d86d60131795a5d38ed37f756c052f162d63dfa
Dan Williams <dan.j.williams@intel.com>
2018-09-26 10:48:38 -0700

    acpi, nfit: Introduce nfit_mem flags
    In preparation for adding a flag to indicate whether a DIMM publishes a
    dirty-shutdown count, convert the existing flags to a bit field.
    
    Reviewed-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/acpi/nfit/nfit.h
+++ drivers/acpi/nfit/nfit.h
@@ -160,6 +160,11 @@ struct nfit_memdev {
 	struct acpi_nfit_memory_map memdev[0];
 };
 
+enum nfit_mem_flags {
+	NFIT_MEM_LSR,
+	NFIT_MEM_LSW,
+};
+
 /* assembled tables for a given dimm/memory-device */
 struct nfit_mem {
 	struct nvdimm *nvdimm;
@@ -179,9 +184,8 @@ struct nfit_mem {
 	struct acpi_nfit_desc *acpi_desc;
 	struct resource *flush_wpq;
 	unsigned long dsm_mask;
+	unsigned long flags;
 	int family;
-	bool has_lsr;
-	bool has_lsw;
 };
 
 struct acpi_nfit_desc {
