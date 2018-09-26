76c8dc3670954f063c31baaacac4559ac304e236
Johannes Berg <johannes.berg@intel.com>
2018-09-26 22:19:42 +0200

    netlink: fix typo in nla_parse_nested() comment
    Fix a simple typo: attribuets -> attributes
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netlink.h
+++ include/net/netlink.h
@@ -154,7 +154,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   nla_find()				find attribute in stream of attributes
  *   nla_find_nested()			find attribute in nested attributes
  *   nla_parse()			parse and validate stream of attrs
- *   nla_parse_nested()			parse nested attribuets
+ *   nla_parse_nested()			parse nested attributes
  *   nla_for_each_attr()		loop over all attributes
  *   nla_for_each_nested()		loop over the nested attributes
  *=========================================================================
