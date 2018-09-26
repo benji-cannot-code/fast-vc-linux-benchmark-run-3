f354dd4eac0996d6efec71534e40ada5fc23a32b
Tim Collier <osdevtc@gmail.com>
2018-09-26 23:05:48 +0100

    staging: wlan-ng: rename DIDmsg_p2req_ramdl_state in p80211metadef.h
    Rename DIDmsg_p2req_ramdl_state to DIDMSG_P2REQ_RAMDL_STATE in
    p80211metadef.h to fix "Avoid CamelCase" message from checkpatch and
    conform to the coding style guidelines.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/p80211metadef.h
+++ drivers/staging/wlan-ng/p80211metadef.h
@@ -112,7 +112,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 			(P80211DID_MKSECTION(5) | \
 			P80211DID_MKGROUP(2) | \
 			P80211DID_MKITEM(2) | 0x00000000)
-#define DIDmsg_p2req_ramdl_state \
+#define DIDMSG_P2REQ_RAMDL_STATE \
 			(P80211DID_MKSECTION(5) | \
 			P80211DID_MKGROUP(11))
 #define DIDmsg_p2req_ramdl_state_enable \
