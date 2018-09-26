71bed9884b3ee8ef476fdc42875995bb4b1fb185
Tim Collier <osdevtc@gmail.com>
2018-09-26 23:05:43 +0100

    staging: wlan-ng: rename DIDmsg_lnxreq_autojoin in p80211metadef.h
    Rename DIDmsg_lnxreq_autojoin in p80211metadef.h to
    DIDMSG_LNXREQ_AUTOJOIN to fix "Avoid CamelCase" message from
    checkpatch and conform to the coding style guidelines.
    
    Signed-off-by: Tim Collier <osdevtc@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wlan-ng/p80211metadef.h
+++ drivers/staging/wlan-ng/p80211metadef.h
@@ -98,7 +98,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DIDMSG_LNXREQ_COMMSQUALITY \
 			(P80211DID_MKSECTION(3) | \
 			P80211DID_MKGROUP(4))
-#define DIDmsg_lnxreq_autojoin \
+#define DIDMSG_LNXREQ_AUTOJOIN \
 			(P80211DID_MKSECTION(3) | \
 			P80211DID_MKGROUP(5))
 #define DIDmsg_p2req_readpda \
