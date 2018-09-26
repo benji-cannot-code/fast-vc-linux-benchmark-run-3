026466eeaa11c0e4466cd1c786a0a3b0742f235d
YueHaibing <yuehaibing@huawei.com>
2018-09-26 15:33:03 +0800

    ASoC: qcom: qdsp6: remove duplicated include
    Remove duplicated includes linux/of_platform.h
    and linux/wait.h
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/qdsp6/q6core.c
+++ sound/soc/qcom/qdsp6/q6core.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/jiffies.h>
-#include <linux/wait.h>
 #include <linux/soc/qcom/apr.h>
 #include "q6core.h"
 #include "q6dsp-errno.h"
