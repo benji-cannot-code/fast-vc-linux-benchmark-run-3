d8eb9ab016267045ff29e32318b7abb04f5a8e3c
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2018-09-26 18:27:14 +0300

    platform/x86: intel-rst: Sort headers alphabetically
    Sort headers alphabetically for better maintenance.
    
    No functional change.
    
    While here, remove unneeded linux/init.h inclusion.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/intel-rst.c
+++ drivers/platform/x86/intel-rst.c
@@ -18,10 +18,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 
-#include <linux/init.h>
+#include <linux/acpi.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/acpi.h>
 
 MODULE_LICENSE("GPL");
 
