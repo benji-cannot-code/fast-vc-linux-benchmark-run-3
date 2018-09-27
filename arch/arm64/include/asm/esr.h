efdde8eb5c7feec6c417d0d0bd7394f6a251c9d7
Marc Zyngier <marc.zyngier@arm.com>
2018-09-27 17:15:33 +0100

    arm64: compat: Add CNTFRQ trap handler
    Just like CNTVCT, we need to handle userspace trapping into the
    kernel if we're decided that the timer wasn't fit for purpose...
    64bit userspace is already dealt with, but we're missing the
    equivalent compat handling.
    
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/esr.h
+++ arch/arm64/include/asm/esr.h
@@ -319,6 +319,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ESR_ELx_CP15_64_ISS_SYS_CNTVCT	(ESR_ELx_CP15_64_ISS_SYS_VAL(1, 14) | \
 					 ESR_ELx_CP15_64_ISS_DIR_READ)
 
+#define ESR_ELx_CP15_32_ISS_SYS_CNTFRQ	(ESR_ELx_CP15_32_ISS_SYS_VAL(0, 0, 14, 0) |\
+					 ESR_ELx_CP15_32_ISS_DIR_READ)
+
 #ifndef __ASSEMBLY__
 #include <asm/types.h>
 
