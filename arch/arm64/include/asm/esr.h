1da82dafca75d1c522fa76e96f3b2d368cf66501
Marc Zyngier <marc.zyngier@arm.com>
2018-09-27 17:15:32 +0100

    arm64: compat: Add CNTVCT trap handler
    Since people seem to make a point in breaking the userspace visible
    counter, we have no choice but to trap the access. We already do this
    for 64bit userspace, but this is lacking for compat. Let's provide
    the required handler.
    
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/esr.h
+++ arch/arm64/include/asm/esr.h
@@ -316,6 +316,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 					 ESR_ELx_CP15_64_ISS_CRM_MASK | \
 					 ESR_ELx_CP15_64_ISS_DIR_MASK)
 
+#define ESR_ELx_CP15_64_ISS_SYS_CNTVCT	(ESR_ELx_CP15_64_ISS_SYS_VAL(1, 14) | \
+					 ESR_ELx_CP15_64_ISS_DIR_READ)
+
 #ifndef __ASSEMBLY__
 #include <asm/types.h>
 
