c9cb38b502d4bbb02a573c10705cc59a10252488
George G. Davis <ggdavisiv@gmail.com>
2018-09-28 17:19:34 -0700

    Input: atmel_mxt_ts - fix multiple <linux/property.h> includes
    Both v4.16-rc7 commit 93afb1d6e72a ("Input: atmel_mxt_ts - switch from
    OF to generic device properties") and v4.16-rc7 commit 96a938aa214e
    ("Input: atmel_mxt_ts - remove platform data support") added includes of
    "<linux/property.h>".  Remove one of the duplicate includes to fix this.
    
    Signed-off-by: George G. Davis <george_davis@mentor.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/property.h>
 #include <linux/slab.h>
 #include <linux/gpio/consumer.h>
-#include <linux/property.h>
 #include <asm/unaligned.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ioctl.h>
