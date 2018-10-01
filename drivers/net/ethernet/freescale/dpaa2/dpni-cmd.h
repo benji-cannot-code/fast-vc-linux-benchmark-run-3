d0b4883fc3549f4fe279858bfd3ee63b63e659f1
Ioana Radulescu <ruxandra.radulescu@nxp.com>
2018-10-01 13:44:55 +0300

    dpaa2-eth: Use new API for Rx flow hashing
    The Management Complex (MC) firmware initially allowed the
    configuration of a single key to be used both for Rx flow hashing
    and flow classification. This prevented us from supporting
    Rx flow classification through ethtool.
    
    Starting with version 10.7.0, the Management Complex(MC) offers
    a new set of APIs for separate configuration of Rx hashing and
    classification keys.
    
    Update the Rx flow hashing support to use the new API, if available.
    
    Signed-off-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/dpaa2/dpni-cmd.h
+++ drivers/net/ethernet/freescale/dpaa2/dpni-cmd.h
@@ -83,6 +83,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DPNI_CMDID_GET_OFFLOAD				DPNI_CMD(0x26B)
 #define DPNI_CMDID_SET_OFFLOAD				DPNI_CMD(0x26C)
 
+#define DPNI_CMDID_SET_RX_HASH_DIST			DPNI_CMD(0x274)
+
 /* Macros for accessing command fields smaller than 1byte */
 #define DPNI_MASK(field)	\
 	GENMASK(DPNI_##field##_SHIFT + DPNI_##field##_SIZE - 1, \
@@ -514,6 +516,16 @@ struct dpni_rsp_get_taildrop {
 struct dpni_rsp_get_api_version {
 	__le16 major;
 	__le16 minor;
+};
+
+#define DPNI_RX_HASH_DIST_ENABLE_SHIFT	0
+#define DPNI_RX_HASH_DIST_ENABLE_SIZE	1
+struct dpni_cmd_set_rx_hash_dist {
+	__le16 dist_size;
+	u8 enable;
+	u8 tc;
+	__le32 pad;
+	__le64 key_cfg_iova;
 };
 
 #endif /* _FSL_DPNI_CMD_H */
