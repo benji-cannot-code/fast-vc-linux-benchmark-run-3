97e6b129dac78d89716296a66228a36e9a119d4d
Chris Wilson <chris@chris-wilson.co.uk>
2018-10-01 20:44:46 +0100

    drm/i915: Clear the error PTE just once on finish
    We do not need to continually clear our dedicated PTE for error capture
    as it will be updated and invalidated to the next object. Only at the
    end do we wish to be sure that the PTE doesn't point back to any buffer.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181001194447.29910-3-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gpu_error.c
+++ drivers/gpu/drm/i915/i915_gpu_error.c
@@ -1000,7 +1000,6 @@ i915_error_object_create(struct drm_i915_private *i915,
 	}
 
 	compress_fini(&compress, dst);
-	ggtt->vm.clear_range(&ggtt->vm, slot, PAGE_SIZE);
 	return dst;
 }
 
@@ -1786,6 +1785,14 @@ static unsigned long capture_find_epoch(const struct i915_gpu_state *error)
 	return epoch;
 }
 
+static void capture_finish(struct i915_gpu_state *error)
+{
+	struct i915_ggtt *ggtt = &error->i915->ggtt;
+	const u64 slot = ggtt->error_capture.start;
+
+	ggtt->vm.clear_range(&ggtt->vm, slot, PAGE_SIZE);
+}
+
 static int capture(void *data)
 {
 	struct i915_gpu_state *error = data;
@@ -1810,6 +1817,7 @@ static int capture(void *data)
 
 	error->epoch = capture_find_epoch(error);
 
+	capture_finish(error);
 	return 0;
 }
 
