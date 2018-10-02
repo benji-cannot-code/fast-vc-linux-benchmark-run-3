6d40db3fd6ed51a36b0e52dce8b888a8595c9bc0
Palmer Dabbelt <palmer@sifive.com>
2018-10-02 12:15:00 -0700

    RISC-V: Rename riscv_of_processor_hart to riscv_of_processor_hartid
    It's a bit confusing exactly what this function does: it actually
    returns the hartid of an OF processor node, failing with -1 on invalid
    nodes.  I've changed the name to _hartid() in order to make that a bit
    more clear, as well as adding a comment.
    
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>
    [Atish: code comment formatting update]
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- arch/riscv/kernel/cpu.c
+++ arch/riscv/kernel/cpu.c
@@ -16,8 +16,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/of.h>
 
-/* Return -1 if not a valid hart */
-int riscv_of_processor_hart(struct device_node *node)
+/*
+ * Returns the hart ID of the given device tree node, or -1 if the device tree
+ * node isn't a RISC-V hart.
+ */
+int riscv_of_processor_hartid(struct device_node *node)
 {
 	const char *isa, *status;
 	u32 hart;
