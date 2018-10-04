4b6b56000015e7816a5021810219f6c02b446a99
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2018-10-04 12:37:47 +0300

    staging: vc04_services: Drop no longer needed headers
    Previously, connection.h was only required for the definition of
    VCHI_CONNECTION_T, but now all usages of it are gone. Remove this unused
    header.
    
    After connection.h is gone, message.h and vchi_cfg_internal.h are no
    longer referenced by anything either. Drop them as well.
    
    Signed-off-by: Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
    Acked-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchi/vchi.h
+++ drivers/staging/vc04_services/interface/vchi/vchi.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "interface/vchi/vchi_cfg.h"
 #include "interface/vchi/vchi_common.h"
-#include "interface/vchi/connections/connection.h"
 #include "vchi_mh.h"
 
 /******************************************************************************
