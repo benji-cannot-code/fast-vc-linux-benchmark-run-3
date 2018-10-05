9bfbdfb26c74d52084027bc2e5288f1480798c25
Dmitry Osipenko <digetx@gmail.com>
2018-10-05 18:36:33 +0300

    regulator: core: Limit regulators coupling to a single couple
    Device tree binding was changed in a way that now max-spread values must
    be defied per regulator pair. Limit number of pairs in order to adapt to
    the new binding without changing regulators code.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/regulator/driver.h
+++ include/linux/regulator/driver.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_REGULATOR_DRIVER_H_
 #define __LINUX_REGULATOR_DRIVER_H_
 
-#define MAX_COUPLED		4
+#define MAX_COUPLED		2
 
 #include <linux/device.h>
 #include <linux/notifier.h>
