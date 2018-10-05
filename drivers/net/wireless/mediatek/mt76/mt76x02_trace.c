fef2821579363a9fba8163bcf476133b299c7659
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2018-10-05 10:28:34 +0200

    mt76: move mt76x02_mac_poll_tx_status in mt76x02-lib moudle
    Move mt76x02_mac_poll_tx_status in mt76x02_mac.c in order to
    be reused by mt76x0 drivers for irq unification.
    Moreover introduce mt76x02_trace source file to define mt76x02
    trace points
    
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt76x02_trace.c
+++ drivers/net/wireless/mediatek/mt76/mt76x02_trace.c
@@ -15,24 +15,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include "mt76x2.h"
+#include <linux/module.h>
 
-void mt76x2_tx_tasklet(unsigned long data)
-{
-	struct mt76x02_dev *dev = (struct mt76x02_dev *) data;
-	int i;
+#ifndef __CHECKER__
+#define CREATE_TRACE_POINTS
+#include "mt76x02_trace.h"
 
-	mt76x2_mac_process_tx_status_fifo(dev);
-
-	for (i = MT_TXQ_MCU; i >= 0; i--)
-		mt76_queue_tx_cleanup(dev, i, false);
-
-	mt76x02_mac_poll_tx_status(dev, false);
-	mt76x02_irq_enable(&dev->mt76, MT_INT_TX_DONE_ALL);
-}
-
-void mt76x2_dma_cleanup(struct mt76x02_dev *dev)
-{
-	tasklet_kill(&dev->tx_tasklet);
-	mt76_dma_cleanup(&dev->mt76);
-}
+#endif
