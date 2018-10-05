575d735839012a73a9b12956c1e1a6dc07a245ce
Steven Rostedt (VMware) <rostedt@goodmis.org>
2018-10-05 12:18:16 -0400

    tools lib traceevent: Separate out tep_strerror() for strerror_r() issues
    While working on having PowerTop use libtracevent as a shared object
    library, Tzvetomir hit "str_error_r not defined". This was added by commit
    c3cec9e68f12d ("tools lib traceevent: Use str_error_r()") because
    strerror_r() has two definitions, where one is GNU specific, and the other
    is XSI complient. The strerror_r() is in a wrapper str_error_r() to keep the
    code from having to worry about which compiler is being used.
    
    The problem is that str_error_r() is external to libtraceevent, and not part
    of the library. If it is used as a shared object then the tools using it
    will need to define that function. I do not want that function defined in
    libtraceevent itself, as it is out of scope for that library.
    
    As there's only a single instance of this call, and its in the traceevent
    library's own tep_strerror() function, we can copy what was done in perf,
    and create yet another external file that undefs _GNU_SOURCE to use the more
    portable version of the function. We don't need to worry about the errors
    that strerror_r() returns. If the buffer isn't big enough, we simply
    truncate it.
    
    Reported-by: Tzvetomir Stoyanov <tstoyanov@vmware.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>
    Cc: linux trace devel <linux-trace-devel@vger.kernel.org>
    Link: http://lkml.kernel.org/r/20181005121816.484e654f@gandalf.local.home
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/lib/traceevent/event-parse.c
+++ tools/lib/traceevent/event-parse.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <errno.h>
 #include <stdint.h>
 #include <limits.h>
-#include <linux/string.h>
 #include <linux/time64.h>
 
 #include <netinet/in.h>
@@ -6200,35 +6199,6 @@ enum tep_errno tep_parse_event(struct tep_handle *pevent, const char *buf,
 {
 	struct tep_event_format *event = NULL;
 	return __parse_event(pevent, &event, buf, size, sys);
-}
-
-#undef _PE
-#define _PE(code, str) str
-static const char * const tep_error_str[] = {
-	TEP_ERRORS
-};
-#undef _PE
-
-int tep_strerror(struct tep_handle *pevent __maybe_unused,
-		 enum tep_errno errnum, char *buf, size_t buflen)
-{
-	int idx;
-	const char *msg;
-
-	if (errnum >= 0) {
-		str_error_r(errnum, buf, buflen);
-		return 0;
-	}
-
-	if (errnum <= __TEP_ERRNO__START ||
-	    errnum >= __TEP_ERRNO__END)
-		return -1;
-
-	idx = errnum - __TEP_ERRNO__START - 1;
-	msg = tep_error_str[idx];
-	snprintf(buf, buflen, "%s", msg);
-
-	return 0;
 }
 
 int get_field_val(struct trace_seq *s, struct tep_format_field *field,
