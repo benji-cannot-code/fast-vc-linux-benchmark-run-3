c667212c7ff507644e6cc34fd1fd2f3fff38ad80
Paul Mackerras <paulus@ozlabs.org>
2018-10-08 16:30:56 +1100

    KVM: PPC: Book3S HV: Handle hypervisor instruction faults better
    Currently the code for handling hypervisor instruction page faults
    passes 0 for the flags indicating the type of fault, which is OK in
    the usual case that the page is not mapped in the partition-scoped
    page tables.  However, there are other causes for hypervisor
    instruction page faults, such as not being to update a reference
    (R) or change (C) bit.  The cause is indicated in bits in HSRR1,
    including a bit which indicates that the fault is due to not being
    able to write to a page (for example to update an R or C bit).
    Not handling these other kinds of faults correctly can lead to a
    loop of continual faults without forward progress in the guest.
    
    In order to handle these faults better, this patch constructs a
    "DSISR-like" value from the bits which DSISR and SRR1 (for a HISI)
    have in common, and passes it to kvmppc_book3s_hv_page_fault() so
    that it knows what caused the fault.
    
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -767,6 +767,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SPRN_HSRR0	0x13A	/* Save/Restore Register 0 */
 #define SPRN_HSRR1	0x13B	/* Save/Restore Register 1 */
 #define   HSRR1_DENORM		0x00100000 /* Denorm exception */
+#define   HSRR1_HISI_WRITE	0x00010000 /* HISI bcs couldn't update mem */
 
 #define SPRN_TBCTL	0x35f	/* PA6T Timebase control register */
 #define   TBCTL_FREEZE		0x0000000000000000ull /* Freeze all tbs */
