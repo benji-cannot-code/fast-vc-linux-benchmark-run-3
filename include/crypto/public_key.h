696623821f845457aa3ba7b29572ae8f991e8a93
David Howells <dhowells@redhat.com>
2018-10-09 17:47:31 +0100

    KEYS: Allow the public_key struct to hold a private key [ver #2]
    Put a flag in the public_key struct to indicate if the structure is holding
    a private key.  The private key must be held ASN.1 encoded in the format
    specified in RFC 3447 A.1.2.  This is the form required by crypto/rsa.c.
    
    The software encryption subtype's verification and query functions then
    need to select the appropriate crypto function to set the key.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Tested-by: Marcel Holtmann <marcel@holtmann.org>
    Reviewed-by: Marcel Holtmann <marcel@holtmann.org>
    Reviewed-by: Denis Kenzior <denkenz@gmail.com>
    Tested-by: Denis Kenzior <denkenz@gmail.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>

--- include/crypto/public_key.h
+++ include/crypto/public_key.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct public_key {
 	void *key;
 	u32 keylen;
+	bool key_is_private;
 	const char *id_type;
 	const char *pkey_algo;
 };
