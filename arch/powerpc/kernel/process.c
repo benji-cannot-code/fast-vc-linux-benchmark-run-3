73abc4a288ac3728ab3396b7eda32959f981dd95
Christophe Leroy <christophe.leroy@c-s.fr>
2018-10-09 16:46:25 +1100

    powerpc/process: Add missing include of stacktrace.h
    As spotted by sparse:
    
      arch/powerpc/kernel/process.c:1302:6: warning: symbol 'show_user_instructions' was not declared. Should it be static?
    
    Fixes: 88b0fe1757359 ("powerpc: Add show_user_instructions()")
    Reviewed-by: Murilo Opsfelder Araujo <muriloo@linux.ibm.com>
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    [mpe: Split out of larger patch]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/livepatch.h>
 #include <asm/cpu_has_feature.h>
 #include <asm/asm-prototypes.h>
+#include <asm/stacktrace.h>
 
 #include <linux/kprobes.h>
 #include <linux/kdebug.h>
