67c773eeff3b88c1adf1f5dd49dd15bd5e459554
Denis Kenzior <denkenz@gmail.com>
2018-10-09 17:48:10 +0100

    KEYS: asym_tpm: extract key size & public key [ver #2]
    The parsed BER/DER blob obtained from user space contains a TPM_Key
    structure.  This structure has some information about the key as well as
    the public key portion.
    
    This patch extracts this information for future use.
    
    Signed-off-by: Denis Kenzior <denkenz@gmail.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Tested-by: Marcel Holtmann <marcel@holtmann.org>
    Reviewed-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: James Morris <james.morris@microsoft.com>

--- include/crypto/asym_tpm_subtype.h
+++ include/crypto/asym_tpm_subtype.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct tpm_key {
 	void *blob;
 	u32 blob_len;
+	uint16_t key_len; /* Size in bits of the key */
+	const void *pub_key; /* pointer inside blob to the public key bytes */
+	uint16_t pub_key_len; /* length of the public key */
 };
 
 struct tpm_key *tpm_key_create(const void *blob, uint32_t blob_len);
