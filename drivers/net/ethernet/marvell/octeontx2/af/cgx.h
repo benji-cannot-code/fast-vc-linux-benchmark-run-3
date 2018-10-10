cfe3ecc0652432d1a5af80eeeab2a953c65a1f50
Linu Cherian <lcherian@marvell.com>
2018-10-10 18:14:32 +0530

    octeontx2-af: Set RVU PFs to CGX LMACs mapping
    Each of the enabled CGX LMAC is considered a physical
    interface and RVU PFs are mapped to these. VFs of these
    SRIOV PFs will be virtual interfaces and share CGX LMAC
    along with PF.
    
    This mapping info will be used later on for Rx/Tx pkt steering.
    
    Signed-off-by: Linu Cherian <lcherian@marvell.com>
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.h
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.h
@@ -13,11 +13,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CGX_H
 
  /* PCI device IDs */
-#define	PCI_DEVID_OCTEONTX2_CGX			0xA059
+#define	PCI_DEVID_OCTEONTX2_CGX		0xA059
 
 /* PCI BAR nos */
-#define PCI_CFG_REG_BAR_NUM			0
+#define PCI_CFG_REG_BAR_NUM		0
+
+#define MAX_CGX				3
+#define MAX_LMAC_PER_CGX		4
+#define CGX_OFFSET(x)			((x) * MAX_LMAC_PER_CGX)
+
+/* Registers */
+#define CGXX_CMRX_RX_ID_MAP		0x060
+#define CGXX_CMRX_RX_LMACS		0x128
 
 extern struct pci_driver cgx_driver;
 
+int cgx_get_cgx_cnt(void);
+int cgx_get_lmac_cnt(void *cgxd);
+void *cgx_get_pdata(int cgx_id);
 #endif /* CGX_H */
