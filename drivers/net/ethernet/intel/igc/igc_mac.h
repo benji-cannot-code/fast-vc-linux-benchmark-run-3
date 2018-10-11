3ff0e10e2bf2455703595783126ee947aff00d6f
Sasha Neftin <sasha.neftin@intel.com>
2018-10-11 10:17:26 +0300

    igc: Add HW initialization code
    Add code for hardware initialization and reset
    Add code for semaphore handling
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igc/igc_mac.h
+++ drivers/net/ethernet/intel/igc/igc_mac.h
@@ -5,8 +5,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _IGC_MAC_H_
 #define _IGC_MAC_H_
 
+#include "igc_hw.h"
+#include "igc_defines.h"
+
 #ifndef IGC_REMOVED
 #define IGC_REMOVED(a) (0)
 #endif /* IGC_REMOVED */
+
+/* forward declaration */
+s32 igc_disable_pcie_master(struct igc_hw *hw);
+void igc_init_rx_addrs(struct igc_hw *hw, u16 rar_count);
+s32 igc_setup_link(struct igc_hw *hw);
+void igc_clear_hw_cntrs_base(struct igc_hw *hw);
+s32 igc_get_auto_rd_done(struct igc_hw *hw);
+void igc_put_hw_semaphore(struct igc_hw *hw);
 
 #endif
