2cc4fc96f2233a8b9a81be91d9902a85f7929ca9
Sasha Neftin <sasha.neftin@intel.com>
2018-10-11 10:17:08 +0300

    igc: Add skeletal frame for Intel(R) 2.5G Ethernet Controller support
    This patch adds the beginning framework onto which I am going to add
    the igc driver which supports the Intel(R) I225-LM/I225-V 2.5G
    Ethernet Controller.
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

---
+++ drivers/net/ethernet/intel/igc/igc_hw.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c)  2018 Intel Corporation */
+
+#ifndef _IGC_HW_H_
+#define _IGC_HW_H_
+
+#define IGC_DEV_ID_I225_LM			0x15F2
+#define IGC_DEV_ID_I225_V			0x15F3
+
+#endif /* _IGC_HW_H_ */
