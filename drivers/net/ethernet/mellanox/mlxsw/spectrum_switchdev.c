388214a4623110c5f9c1b23724ab6e51c654f199
Ido Schimmel <idosch@mellanox.com>
2018-10-11 07:47:49 +0000

    mlxsw: spectrum: Remove misuses of private header file
    The spectrum_router.h header file is private to the router block and
    should only be included by direct consumers of it, such as dpipe and the
    multicast routing code.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Reviewed-by: Petr Machata <petrm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_switchdev.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_switchdev.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/switchdev.h>
 
 #include "spectrum_span.h"
-#include "spectrum_router.h"
 #include "spectrum_switchdev.h"
 #include "spectrum.h"
 #include "core.h"
