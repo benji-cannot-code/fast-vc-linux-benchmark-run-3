f5aca360cc4b7db3b30f67cd2275be90f83389a3
Michael Chan <michael.chan@broadcom.com>
2018-10-14 07:02:41 -0400

    bnxt_en: Refactor bnxt_ring_struct.
    Move the DMA page table and vmem fields in bnxt_ring_struct to a new
    bnxt_ring_mem_info struct.  This will allow context memory management
    for a new device to re-use some of the existing infrastructure.
    
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -578,7 +578,7 @@ struct bnxt_sw_rx_agg_bd {
 	dma_addr_t		mapping;
 };
 
-struct bnxt_ring_struct {
+struct bnxt_ring_mem_info {
 	int			nr_pages;
 	int			page_size;
 	void			**pg_arr;
@@ -589,6 +589,10 @@ struct bnxt_ring_struct {
 
 	int			vmem_size;
 	void			**vmem;
+};
+
+struct bnxt_ring_struct {
+	struct bnxt_ring_mem_info	ring_mem;
 
 	u16			fw_ring_id; /* Ring id filled by Chimp FW */
 	union {
