3ae199c38fe69187f349e835a7873c9d05ff3831
Madhav Chauhan <madhav.chauhan@intel.com>
2018-10-15 17:27:56 +0300

    drm/i915/icl: Get DSI transcoder for a given port
    This patch adds a helper function to retrieve DSI
    transcoder for a given DSI port using newly defined
    enum names for DSI transcoders.
    
    Signed-off-by: Madhav Chauhan <madhav.chauhan@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/f88ff26fa10c68e37b7838bb7c8573c881474e73.1539613303.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/icl_dsi.c
+++ drivers/gpu/drm/i915/icl_dsi.c
@@ -28,6 +28,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "intel_dsi.h"
 
+static enum transcoder __attribute__((unused)) dsi_port_to_transcoder(enum port port)
+{
+	if (port == PORT_A)
+		return TRANSCODER_DSI_0;
+	else
+		return TRANSCODER_DSI_1;
+}
+
 static void dsi_program_swing_and_deemphasis(struct intel_encoder *encoder)
 {
 	struct drm_i915_private *dev_priv = to_i915(encoder->base.dev);
