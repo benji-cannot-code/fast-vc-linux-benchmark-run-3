eb0cbded0889ccd2542f8f32292c1a7aa2aeadf5
Jani Nikula <jani.nikula@intel.com>
2018-10-15 17:27:50 +0300

    drm/i915/dsi: refactor bitrate calculations in intel_dsi_vbt_init()
    Abstract bitrate calculation to a newly resurrected intel_dsi.c file
    that will contain common code for VLV and ICL DSI.
    
    No functional changes.
    
    Cc: Madhav Chauhan <madhav.chauhan@intel.com>
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Reviewed-by: Madhav Chauhan <madhav.chauhan@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/100e9721dfdec4f3987549ef24291bafc9cb0517.1539613303.git.jani.nikula@intel.com

---
+++ drivers/gpu/drm/i915/intel_dsi.c
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2018 Intel Corporation
+ */
+
+#include <drm/drm_mipi_dsi.h>
+#include "intel_dsi.h"
+
+int intel_dsi_bitrate(const struct intel_dsi *intel_dsi)
+{
+	int bpp = mipi_dsi_pixel_format_to_bpp(intel_dsi->pixel_format);
+
+	if (WARN_ON(bpp < 0))
+		bpp = 16;
+
+	return intel_dsi->pclk * bpp / intel_dsi->lane_count;
+}
