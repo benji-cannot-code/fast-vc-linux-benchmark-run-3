8deed7f17f67f7dbad00681a4b4540ffb963a7ec
Todd Poynor <toddpoynor@google.com>
2018-10-14 21:59:18 -0700

    staging: gasket: page table: return valid error code on map fail
    Return -EINVAL on mapping failures, instead of -1, which triggers a
    checkpatch error.
    
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gasket/gasket_page_table.c
+++ drivers/staging/gasket/gasket_page_table.c
@@ -515,13 +515,12 @@ static int gasket_perform_mapping(struct gasket_page_table *pg_tbl,
 					(void *)page_to_pfn(page),
 					(void *)page_to_phys(page));
 
-				/* clean up */
 				if (gasket_release_page(ptes[i].page))
 					--pg_tbl->num_active_pages;
 
 				memset(&ptes[i], 0,
 				       sizeof(struct gasket_page_table_entry));
-				return -1;
+				return -EINVAL;
 			}
 		}
 
@@ -1166,7 +1165,7 @@ fail:
 	*ppage = NULL;
 	*poffset = 0;
 	mutex_unlock(&pg_tbl->mutex);
-	return -1;
+	return -EINVAL;
 }
 
 /* See gasket_page_table.h for description. */
