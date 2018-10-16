4ff2f41128a6fe98659e569024a0e5e5efe0b58c
Igor Mitsyanko <igor.mitsyanko.os@quantenna.com>
2018-10-16 10:23:56 +0000

    qtnfmac_pcie: use single PCIe driver for all platforms
    Single PCIe driver can identify hardware type by reading CHIP ID at
    probe time and invoking a correct initialization sequence.
    
    Signed-off-by: Igor Mitsyanko <igor.mitsyanko.os@quantenna.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/quantenna/qtnfmac/util.h
+++ drivers/net/wireless/quantenna/qtnfmac/util.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include "core.h"
 
+const char *qtnf_chipid_to_string(unsigned long chip_id);
+
 void qtnf_sta_list_init(struct qtnf_sta_list *list);
 
 struct qtnf_sta_node *qtnf_sta_list_lookup(struct qtnf_sta_list *list,
