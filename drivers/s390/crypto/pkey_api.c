8652ab8269a43fcafef175437a0907deee7c33de
Ingo Franzki <ifranzki@linux.ibm.com>
2018-10-17 17:24:43 +0200

    s390/pkey: Load pkey kernel module automatically
    With the recent enhancements of the pkey kernel module,
    the pkey kernel module should be loaded automatically
    during system startup, if MSA is available.
    
    When used for swap device encryption with random protected
    keys, pkey must be loaded before /etc/crypttab is processed,
    otherwise the sysfs attributes to read the key from are
    not available.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>
    Reviewed-by: Hendrik Brueckner <brueckner@linux.ibm.com>
    Reviewed-by: Harald Freudenberger <freude@linux.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/crypto/pkey_api.c
+++ drivers/s390/crypto/pkey_api.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kallsyms.h>
 #include <linux/debugfs.h>
 #include <linux/random.h>
+#include <linux/cpufeature.h>
 #include <asm/zcrypt.h>
 #include <asm/cpacf.h>
 #include <asm/pkey.h>
@@ -1705,5 +1706,5 @@ static void __exit pkey_exit(void)
 	pkey_debug_exit();
 }
 
-module_init(pkey_init);
+module_cpu_feature_match(MSA, pkey_init);
 module_exit(pkey_exit);
