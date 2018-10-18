4775d636cbed95f9c74710c897808411ad672667
Masami Hiramatsu <mhiramat@kernel.org>
2018-10-18 22:12:34 +0900

    tracing: Fix synthetic event to allow semicolon at end
    Fix synthetic event to allow independent semicolon at end.
    
    The synthetic_events interface accepts a semicolon after the
    last word if there is no space.
    
     # echo "myevent u64 var;" >> synthetic_events
    
    But if there is a space, it returns an error.
    
     # echo "myevent u64 var ;" > synthetic_events
     sh: write error: Invalid argument
    
    This behavior is difficult for users to understand. Let's
    allow the last independent semicolon too.
    
    Link: http://lkml.kernel.org/r/153986835420.18251.2191216690677025744.stgit@devbox
    
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Tom Zanussi <tom.zanussi@linux.intel.com>
    Cc: stable@vger.kernel.org
    Fixes: commit 4b147936fa50 ("tracing: Add support for 'synthetic' events")
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_events_hist.c
+++ kernel/trace/trace_events_hist.c
@@ -1089,7 +1089,7 @@ static int create_synth_event(int argc, char **argv)
 		i += consumed - 1;
 	}
 
-	if (i < argc) {
+	if (i < argc && strcmp(argv[i], ";") != 0) {
 		ret = -EINVAL;
 		goto err;
 	}
