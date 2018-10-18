6d9ef7dd07d8206044289223315c39c8f02a50c2
Christophe Leroy <christophe.leroy@c-s.fr>
2018-10-18 05:22:27 +0000

    powerpc/8xx: add missing header in 8xx_mmu.c
    arch/powerpc/mm/8xx_mmu.c:174:6: error: no previous prototype for ‘set_context’ [-Werror=missing-prototypes]
     void set_context(unsigned long id, pgd_t *pgd)
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Scott Wood <oss@buserror.net>

--- arch/powerpc/mm/8xx_mmu.c
+++ arch/powerpc/mm/8xx_mmu.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/memblock.h>
+#include <linux/mmu_context.h>
 #include <asm/fixmap.h>
 #include <asm/code-patching.h>
 
