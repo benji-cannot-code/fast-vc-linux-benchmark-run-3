940f542f2a802c4f718ff507d8e936dce2aa0931
Thomas Zimmermann <tzimmermann@suse.de>
2018-10-19 10:54:23 +0200

    drm: Remove drm_global.{c,h} v2
    The content of drm_global.{c,h} is obsolete.
    
    v2: rebase on dropping TTM functionality
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_agpsupport.h>
 #include <drm/drm_crtc.h>
 #include <drm/drm_fourcc.h>
-#include <drm/drm_global.h>
 #include <drm/drm_hashtab.h>
 #include <drm/drm_mm.h>
 #include <drm/drm_os_linux.h>
