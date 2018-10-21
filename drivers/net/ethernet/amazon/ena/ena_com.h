9b0378a336876e0872c1c0882d731b0e1d25d820
Arthur Kiyanovski <akiyano@amazon.com>
2018-10-21 18:07:14 +0300

    net: ena: fix compilation error in xtensa architecture
    linux/prefetch.h is never explicitly included in ena_com, although
    functions from it, such as prefetchw(), are used throughout ena_com.
    This is an inclusion bug, and we fix it here by explicitly including
    linux/prefetch.h. The bug was exposed when the driver was compiled
    for the xtensa architecture.
    
    Fixes: 689b2bdaaa14 ("net: ena: add functions for handling Low Latency Queues in ena_com")
    Fixes: 8c590f977638 ("ena: Fix Kconfig dependency on X86")
    Signed-off-by: Arthur Kiyanovski <akiyano@amazon.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/amazon/ena/ena_com.h
+++ drivers/net/ethernet/amazon/ena/ena_com.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 #include <linux/gfp.h>
 #include <linux/io.h>
+#include <linux/prefetch.h>
 #include <linux/sched.h>
 #include <linux/sizes.h>
 #include <linux/spinlock.h>
