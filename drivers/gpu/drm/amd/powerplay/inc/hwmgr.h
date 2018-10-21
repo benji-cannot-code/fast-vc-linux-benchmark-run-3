12f3661221514dee5c616ace5e578620890b7ee4
YueHaibing <yuehaibing@huawei.com>
2018-10-21 18:39:58 +0800

    drm/amd/powerplay: remove duplicated includes
    Remove some duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/inc/hwmgr.h
+++ drivers/gpu/drm/amd/powerplay/inc/hwmgr.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hardwaremanager.h"
 #include "hwmgr_ppt.h"
 #include "ppatomctrl.h"
-#include "hwmgr_ppt.h"
 #include "power_state.h"
 #include "smu_helper.h"
 
