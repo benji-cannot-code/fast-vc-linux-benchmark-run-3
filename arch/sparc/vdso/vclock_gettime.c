6eb712952924815ed4ca319fadd8b643c45dfc84
David S. Miller <davem@davemloft.net>
2018-10-21 22:10:51 -0700

    sparc: Don't bother masking out TICK_PRIV_BIT in VDSO code.
    If the TICK_PRIV_BIT was set, we would not be able to read the tick
    register in user space, which is where this code runs.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/vdso/vclock_gettime.c
+++ arch/sparc/vdso/vclock_gettime.c
@@ -27,13 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/clocksource.h>
 #include <asm/vvar.h>
 
-#undef	TICK_PRIV_BIT
-#ifdef	CONFIG_SPARC64
-#define	TICK_PRIV_BIT	(1UL << 63)
-#else
-#define	TICK_PRIV_BIT	(1ULL << 63)
-#endif
-
 #ifdef	CONFIG_SPARC64
 #define SYSCALL_STRING							\
 	"ta	0x6d;"							\
@@ -113,7 +106,7 @@ notrace static __always_inline u64 vread_tick(void)
 			     "rd		%%asr24, %0\n\t"
 			     ".popsection\n"
 			     : "=r" (ret));
-	return ret & ~TICK_PRIV_BIT;
+	return ret;
 }
 #else
 notrace static __always_inline u64 vread_tick(void)
