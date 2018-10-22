020a4bae1033f0d0ef3443859c3163e8c6f2a965
Breno Leitao <leitao@debian.org>
2018-10-22 11:54:12 -0300

    powerpc/64s: Include cpu header
    Current powerpc security.c file is defining functions, as
    cpu_show_meltdown(), cpu_show_spectre_v{1,2} and others, that are being
    declared at linux/cpu.h header without including the header file that
    contains these declarations.
    
    This is being reported by sparse, which thinks that these functions are
    static, due to the lack of declaration:
    
    	arch/powerpc/kernel/security.c:105:9: warning: symbol 'cpu_show_meltdown' was not declared. Should it be static?
    	arch/powerpc/kernel/security.c:139:9: warning: symbol 'cpu_show_spectre_v1' was not declared. Should it be static?
    	arch/powerpc/kernel/security.c:161:9: warning: symbol 'cpu_show_spectre_v2' was not declared. Should it be static?
    	arch/powerpc/kernel/security.c:209:6: warning: symbol 'stf_barrier' was not declared. Should it be static?
    	arch/powerpc/kernel/security.c:289:9: warning: symbol 'cpu_show_spec_store_bypass' was not declared. Should it be static?
    
    This patch simply includes the proper header (linux/cpu.h) to match
    function definition and declaration.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/security.c
+++ arch/powerpc/kernel/security.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 //
 // Copyright 2018, Michael Ellerman, IBM Corporation.
 
+#include <linux/cpu.h>
 #include <linux/kernel.h>
 #include <linux/device.h>
 #include <linux/seq_buf.h>
