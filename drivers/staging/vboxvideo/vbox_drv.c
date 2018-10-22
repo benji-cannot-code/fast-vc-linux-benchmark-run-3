3bd1447c58ccad75343d7d12824721fbef849caf
Hans de Goede <hdegoede@redhat.com>
2018-10-22 16:57:45 +0200

    staging: vboxvideo: Cleanup header use
    Stop adding -Iinclude/drm to the CFLAGS and stop using the obsolete
    "include-all" drm/drmP.h header.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vboxvideo/vbox_drv.c
+++ drivers/staging/vboxvideo/vbox_drv.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #include <linux/vt_kern.h>
 
-#include <drm/drmP.h>
 #include <drm/drm_crtc_helper.h>
 
 #include "vbox_drv.h"
--- drivers/staging/vboxvideo/vbox_fb.c
+++ drivers/staging/vboxvideo/vbox_fb.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fb.h>
 #include <linux/init.h>
 
-#include <drm/drmP.h>
 #include <drm/drm_crtc.h>
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_crtc_helper.h>
