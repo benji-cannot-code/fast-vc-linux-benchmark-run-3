6579773b77bba0066bc27a1281e77ed2e2bb2591
Rahul Verma <Rahul.Verma@cavium.com>
2018-10-23 08:04:24 -0700

    qed: Fix static checker warning
    	Static Checker Warnings:
    	drivers/net/ethernet/qlogic/qed/qed_main.c:1510 qed_fill_link_capability()
    	error: uninitialized symbol 'tcvr_state'.
    	drivers/net/ethernet/qlogic/qed/qed_mcp.c:1951 qed_mcp_trans_speed_mask()
    	error: uninitialized symbol 'transceiver_state'.
    	drivers/net/ethernet/qlogic/qed/qed_mcp.c:1951 qed_mcp_trans_speed_mask()
    	error: uninitialized symbol 'transceiver_type'.
    
    	Symbols tcvr_state, transceiver_state and transceiver_type
    	are initialized with respective default state.
    
    Fixes: c56a8be7e7aa ("qed: Add supported link and advertise link to display in ethtool.")
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Rahul Verma <Rahul.Verma@cavium.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qlogic/qed/qed_mcp.c
+++ drivers/net/ethernet/qlogic/qed/qed_mcp.c
@@ -1901,6 +1901,9 @@ int qed_mcp_get_transceiver_data(struct qed_hwfn *p_hwfn,
 {
 	u32 transceiver_info;
 
+	*p_transceiver_type = ETH_TRANSCEIVER_TYPE_NONE;
+	*p_transceiver_state = ETH_TRANSCEIVER_STATE_UPDATING;
+
 	if (IS_VF(p_hwfn->cdev))
 		return -EINVAL;
 
@@ -1908,9 +1911,6 @@ int qed_mcp_get_transceiver_data(struct qed_hwfn *p_hwfn,
 		DP_NOTICE(p_hwfn, "MFW is not initialized!\n");
 		return -EBUSY;
 	}
-
-	*p_transceiver_type = ETH_TRANSCEIVER_TYPE_NONE;
-	*p_transceiver_state = ETH_TRANSCEIVER_STATE_UPDATING;
 
 	transceiver_info = qed_rd(p_hwfn, p_ptt,
 				  p_hwfn->mcp_info->port_addr +
