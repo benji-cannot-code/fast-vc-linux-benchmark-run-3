3e4f00b444c55931153cfbbc15b51dbd8f61c890
Tyrel Datwyler <tyreld@linux.vnet.ibm.com>
2018-10-22 20:32:12 -0500

    powerpc/pseries: add missing cpumask.h include file
    Build error is encountered when inlcuding <asm/rtas.h> if no explicit or
    implicit include of cpumask.h exists in the including file.
    
    In file included from arch/powerpc/platforms/pseries/hotplug-pci.c:3:0:
    ./arch/powerpc/include/asm/rtas.h:360:34: error: unknown type name 'cpumask_var_t'
     extern int rtas_online_cpus_mask(cpumask_var_t cpus);
                                      ^
    ./arch/powerpc/include/asm/rtas.h:361:35: error: unknown type name 'cpumask_var_t'
     extern int rtas_offline_cpus_mask(cpumask_var_t cpus);
    
    Fixes: 120496ac2d2d ("powerpc: Bring all threads online prior to migration/hibernation")
    Signed-off-by: Tyrel Datwyler <tyreld@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/rtas.h
+++ arch/powerpc/include/asm/rtas.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <asm/page.h>
 #include <linux/time.h>
+#include <linux/cpumask.h>
 
 /*
  * Definitions for talking to the RTAS on CHRP machines.
