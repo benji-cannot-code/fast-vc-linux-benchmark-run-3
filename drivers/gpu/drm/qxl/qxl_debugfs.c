7e67e6df09fbd3908bc2945de61e0715e854d8a2
Shayenne da Luz Moura <shayenneluzmoura@gmail.com>
2018-10-26 16:23:16 -0300

    drm/qxl: Remove exceding whiteline
    Remove extra whiteline to clean the checkpatch.pl check:
    
    CHECK: Please don't use multiple blank lines
    
    Signed-off-by: Shayenne da Luz Moura <shayenneluzmoura@gmail.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/5b95e1d4d515d02d01b829ddc5b3ca80af29e2e2.1540579956.git.shayenneluzmoura@gmail.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- drivers/gpu/drm/qxl/qxl_debugfs.c
+++ drivers/gpu/drm/qxl/qxl_debugfs.c
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "qxl_drv.h"
 #include "qxl_object.h"
 
-
 #if defined(CONFIG_DEBUG_FS)
 static int
 qxl_debugfs_irq_received(struct seq_file *m, void *data)
--- drivers/gpu/drm/qxl/qxl_drv.h
+++ drivers/gpu/drm/qxl/qxl_drv.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *          Alon Levy
  */
 
-
 #ifndef QXL_DRV_H
 #define QXL_DRV_H
 
@@ -133,7 +132,6 @@ struct qxl_mman {
 	bool				mem_global_referenced;
 	struct ttm_bo_device		bdev;
 };
-
 
 struct qxl_memslot {
 	uint8_t		generation;
@@ -372,7 +370,6 @@ int qxl_mode_dumb_create(struct drm_file *file_priv,
 int qxl_mode_dumb_mmap(struct drm_file *filp,
 		       struct drm_device *dev,
 		       uint32_t handle, uint64_t *offset_p);
-
 
 /* qxl ttm */
 int qxl_ttm_init(struct qxl_device *qdev);
