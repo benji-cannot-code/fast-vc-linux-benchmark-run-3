0649434cadc809dd204a82beb74dcb6531692e35
Martin Kelly <martin@martingkelly.com>
2018-10-25 19:38:10 -0700

    iio:magnetometer: st_magn: add LSM9DS1 support
    Update the sensor settings to support the LSM9DS1 sensor. Although the
    LSM9DS1 accelerometer and gyroscope are coupled together to use the same
    FIFO, the magnetometer is separate and can be cleanly supported without
    refactoring the existing driver.
    
    Signed-off-by: Martin Kelly <martin@martingkelly.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/magnetometer/st_magn_i2c.c
+++ drivers/iio/magnetometer/st_magn_i2c.c
@@ -45,6 +45,10 @@ static const struct of_device_id st_magn_of_match[] = {
 		.compatible = "st,lis2mdl",
 		.data = LIS2MDL_MAGN_DEV_NAME,
 	},
+	{
+		.compatible = "st,lsm9ds1-magn",
+		.data = LSM9DS1_MAGN_DEV_NAME,
+	},
 	{},
 };
 MODULE_DEVICE_TABLE(of, st_magn_of_match);
@@ -91,6 +95,7 @@ static const struct i2c_device_id st_magn_id_table[] = {
 	{ LIS3MDL_MAGN_DEV_NAME },
 	{ LSM303AGR_MAGN_DEV_NAME },
 	{ LIS2MDL_MAGN_DEV_NAME },
+	{ LSM9DS1_MAGN_DEV_NAME },
 	{},
 };
 MODULE_DEVICE_TABLE(i2c, st_magn_id_table);
