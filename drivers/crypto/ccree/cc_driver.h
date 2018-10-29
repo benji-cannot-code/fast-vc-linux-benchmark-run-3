da30157b6d71d957769f929c8eaffa63d5ba6b04
Gilad Ben-Yossef <gilad@benyossef.com>
2018-10-29 09:50:12 +0000

    crypto: ccree - add support for CryptoCell 713
    Add support for Arm TrustZone CryptoCell 713.
    Note that this patch just enables using a 713 in backwards compatible mode
    to 712. Newer 713 specific features will follow.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccree/cc_driver.h
+++ drivers/crypto/ccree/cc_driver.h
@@ -37,12 +37,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern bool cc_dump_desc;
 extern bool cc_dump_bytes;
 
-#define DRV_MODULE_VERSION "4.0"
+#define DRV_MODULE_VERSION "5.0"
 
 enum cc_hw_rev {
 	CC_HW_REV_630 = 630,
 	CC_HW_REV_710 = 710,
-	CC_HW_REV_712 = 712
+	CC_HW_REV_712 = 712,
+	CC_HW_REV_713 = 713
 };
 
 #define CC_COHERENT_CACHE_PARAMS 0xEEE
