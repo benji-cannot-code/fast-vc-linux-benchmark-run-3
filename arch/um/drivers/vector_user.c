a683752c234027a1cbb7caac87779a496f43778c
Richard Weinberger <richard@nod.at>
2018-10-30 12:58:11 +0100

    um: Include sys/uio.h to have writev()
    sys/uio.h gives us writev(), otherwise the build might fail on
    some systems.
    
    Fixes: 49da7e64f33e ("High Performance UML Vector Network Driver")
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/vector_user.c
+++ arch/um/drivers/vector_user.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdlib.h>
 #include <os.h>
 #include <um_malloc.h>
+#include <sys/uio.h>
 #include "vector_user.h"
 
 #define ID_GRE 0
