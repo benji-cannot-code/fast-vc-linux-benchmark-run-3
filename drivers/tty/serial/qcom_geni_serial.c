ac3e00c68b8561605857dd1fdfac29c63feece08
Douglas Anderson <dianders@chromium.org>
2018-10-30 15:11:03 -0700

    serial: qcom_geni_serial: Finish supporting sysrq
    The geni serial driver already had some sysrq code in it, but since
    SUPPORT_SYSRQ wasn't defined the code didn't do anything useful.
    Let's make it useful by adding that define using the same formula
    found in other serial drivers.
    
    In order to prevent deadlock, we'll take a page from the
    'msm_serial.c' where the spinlock is released around
    uart_handle_sysrq_char().  This seemed better than copying from
    '8250_port.c' where we skip locking in the console_write function
    since the '8250_port.c' method can cause lockdep warnings when
    dropping into kgdb.
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/qcom_geni_serial.c
+++ drivers/tty/serial/qcom_geni_serial.c
@@ -2,6 +2,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2017-2018, The Linux foundation. All rights reserved.
 
+#if defined(CONFIG_SERIAL_QCOM_GENI_CONSOLE) && defined(CONFIG_MAGIC_SYSRQ)
+# define SUPPORT_SYSRQ
+#endif
+
 #include <linux/clk.h>
 #include <linux/console.h>
 #include <linux/io.h>
@@ -494,7 +498,10 @@ static int handle_rx_console(struct uart_port *uport, u32 bytes, bool drop)
 					continue;
 			}
 
+			spin_unlock(&uport->lock);
 			sysrq = uart_handle_sysrq_char(uport, buf[c]);
+			spin_lock(&uport->lock);
+
 			if (!sysrq)
 				tty_insert_flip_char(tport, buf[c], TTY_NORMAL);
 		}
