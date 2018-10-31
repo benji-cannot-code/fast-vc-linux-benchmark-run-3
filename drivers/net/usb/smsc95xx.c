731eb7f4c5bea4052cdbe53ae754c5fa5e1e6c84
Frieder Schrempf <frieder.schrempf@kontron.de>
2018-10-31 22:52:19 +0100

    usbnet: smsc95xx: disable carrier check while suspending
    We need to make sure, that the carrier check polling is disabled
    while suspending. Otherwise we can end up with usbnet_read_cmd()
    being issued when only usbnet_read_cmd_nopm() is allowed. If this
    happens, read operations lock up.
    
    Fixes: d69d169493 ("usbnet: smsc95xx: fix link detection for disabled autonegotiation")
    Signed-off-by: Frieder Schrempf <frieder.schrempf@kontron.de>
    Reviewed-by: Raghuram Chary J <RaghuramChary.Jallipalli@microchip.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/smsc95xx.c
+++ drivers/net/usb/smsc95xx.c
@@ -1599,6 +1599,8 @@ static int smsc95xx_suspend(struct usb_interface *intf, pm_message_t message)
 		return ret;
 	}
 
+	cancel_delayed_work_sync(&pdata->carrier_check);
+
 	if (pdata->suspend_flags) {
 		netdev_warn(dev->net, "error during last resume\n");
 		pdata->suspend_flags = 0;
@@ -1841,6 +1843,11 @@ done:
 	 */
 	if (ret && PMSG_IS_AUTO(message))
 		usbnet_resume(intf);
+
+	if (ret)
+		schedule_delayed_work(&pdata->carrier_check,
+				      CARRIER_CHECK_DELAY);
+
 	return ret;
 }
 
