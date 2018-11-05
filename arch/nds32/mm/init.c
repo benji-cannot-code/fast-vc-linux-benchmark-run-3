4abaf1b85e1dbf17f2ad4f215f166b9309a929a7
Florian Fainelli <f.fainelli@gmail.com>
2018-11-05 14:54:26 -0800

    nds32: Remove phys_initrd_start and phys_initrd_size
    This will conflict with a subsequent change making phys_initrd_start and
    phys_initrd_size global variables. nds32 does not make use of those nor
    provides a suitable declarations so just get rid of them.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Mike Rapoport <rppt@linux.ibm.com>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- arch/nds32/mm/init.c
+++ arch/nds32/mm/init.c
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 DEFINE_PER_CPU(struct mmu_gather, mmu_gathers);
 DEFINE_SPINLOCK(anon_alias_lock);
 extern pgd_t swapper_pg_dir[PTRS_PER_PGD];
-extern unsigned long phys_initrd_start;
-extern unsigned long phys_initrd_size;
 
 /*
  * empty_zero_page is a special page that is used for
