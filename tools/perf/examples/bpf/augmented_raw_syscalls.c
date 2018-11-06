546feafadee1e99578b9b152888103313f798818
Arnaldo Carvalho de Melo <acme@redhat.com>
2018-11-06 15:26:46 -0300

    perf augmented_syscalls: Remove needless linux/socket.h include
    Leftover from when we started augmented_raw_syscalls.c from
    tools/perf/examples/bpf/augmented_syscalls.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Wang Nan <wangnan0@huawei.com>
    Fixes: e58a0322dbac ("perf examples bpf: Start augmenting raw_syscalls:sys_{start,exit}")
    Link: https://lkml.kernel.org/n/tip-pmts9ls2skh8n3zisb4txudd@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/examples/bpf/augmented_raw_syscalls.c
+++ tools/perf/examples/bpf/augmented_raw_syscalls.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <stdio.h>
 #include <unistd.h>
-#include <linux/socket.h>
 
 /* bpf-output associated map */
 struct bpf_map SEC("maps") __augmented_syscalls__ = {
