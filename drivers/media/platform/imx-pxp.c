23e22adf40188c858d1bc92ee83f6dd7fa743418
Fabio Estevam <festevam@gmail.com>
2018-11-06 05:16:05 -0500

    media: imx-pxp: Improve pxp_soft_reset() error message
    Improve the pxp_soft_reset() error message by moving it to the
    caller function, associating it with a proper device and also
    by displaying the error code.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Hans Verkuil <hansverk@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/imx-pxp.c
+++ drivers/media/platform/imx-pxp.c
@@ -1620,10 +1620,8 @@ static int pxp_soft_reset(struct pxp_dev *dev)
 
 	ret = readl_poll_timeout(dev->mmio + HW_PXP_CTRL, val,
 				 val & BM_PXP_CTRL_CLKGATE, 0, 100);
-	if (ret < 0) {
-		pr_err("PXP reset timeout\n");
+	if (ret < 0)
 		return ret;
-	}
 
 	writel(BM_PXP_CTRL_SFTRST, dev->mmio + HW_PXP_CTRL_CLR);
 	writel(BM_PXP_CTRL_CLKGATE, dev->mmio + HW_PXP_CTRL_CLR);
@@ -1676,8 +1674,10 @@ static int pxp_probe(struct platform_device *pdev)
 		return ret;
 
 	ret = pxp_soft_reset(dev);
-	if (ret < 0)
+	if (ret < 0) {
+		dev_err(&pdev->dev, "PXP reset timeout: %d\n", ret);
 		goto err_clk;
+	}
 
 	spin_lock_init(&dev->irqlock);
 
