7f70ec27e8e10a00b52510e88796aa816d012752
Paul Burton <paul.burton@mips.com>
2018-11-07 23:05:07 +0000

    MIPS: Remove GCC_IMM_ASM & GCC_REG_ACCUM macros
    asm/compiler.h defined GCC_IMM_ASM & GCC_REG_ACCUM macros, both of which
    are defined differently for GCC pre-3.4 or GCC 3.4 & higher. We only
    support building with GCC 4.6 & higher since commit cafa0010cd51 ("Raise
    the minimum required gcc version to 4.6"), which makes the pre-3.4
    definition dead code.
    
    Rather than leave the macro definitions around, inline the GCC 3.4 &
    higher definitions into the single file that uses them & remove the
    macros entirely.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Patchwork: https://patchwork.linux-mips.org/patch/21000/
    Cc: linux-mips@linux-mips.org

--- arch/mips/include/asm/compiler.h
+++ arch/mips/include/asm/compiler.h
@@ -44,14 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #undef barrier_before_unreachable
 #define barrier_before_unreachable() asm volatile(".insn")
 
-#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
-#define GCC_IMM_ASM() "n"
-#define GCC_REG_ACCUM "$0"
-#else
-#define GCC_IMM_ASM() "rn"
-#define GCC_REG_ACCUM "accum"
-#endif
-
 #ifdef CONFIG_CPU_MIPSR6
 /* All MIPS R6 toolchains support the ZC constrain */
 #define GCC_OFF_SMALL_ASM() "ZC"
