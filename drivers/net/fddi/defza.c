26912588bf1eb1a871117391450fe3a184a17e43
Maciej W. Rozycki <macro@linux-mips.org>
2018-11-07 12:06:52 +0000

    FDDI: defza: Fix SPDX annotation
    The SPDX annotation for this driver does not match the license text,
    which specifies GNU GPL 2 or later.  Make the two match by correcting
    the SPDX tag.
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/fddi/defza.c
+++ drivers/net/fddi/defza.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0+
 /*	FDDI network adapter driver for DEC FDDIcontroller 700/700-C devices.
  *
  *	Copyright (c) 2018  Maciej W. Rozycki
