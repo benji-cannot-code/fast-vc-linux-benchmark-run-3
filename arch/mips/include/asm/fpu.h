d8a70f93b73c88596bf0eb35902865fd1174cc70
Paul Burton <paul.burton@mips.com>
2018-11-07 23:14:00 +0000

    MIPS: Drop forward declarations of sigcontext in asm/fpu.h
    asm/fpu.h contains forward declarations of struct sigcontext & struct
    sigcontext32 which appear to have been unused since commit 137f6f3e284e
    ("MIPS: Cleanup signal code initialization"). Remove the dead code.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Patchwork: https://patchwork.linux-mips.org/patch/21015/
    Cc: linux-mips@linux-mips.org

--- arch/mips/include/asm/fpu.h
+++ arch/mips/include/asm/fpu.h
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mips_mt.h>
 #endif
 
-struct sigcontext;
-struct sigcontext32;
-
 extern void _save_fp(struct task_struct *);
 extern void _restore_fp(struct task_struct *);
 
