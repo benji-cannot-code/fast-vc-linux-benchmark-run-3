c0e818ec63721f5f2b4abbcb6c7d91e151e17c07
Yafang Shao <laoar.shao@gmail.com>
2018-11-07 19:20:16 +0800

    inet: minor optimization for backlog setting in listen(2)
    Set the backlog earlier in inet_dccp_listen() and inet_listen(),
    then we can avoid the redundant setting.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/proto.c
+++ net/dccp/proto.c
@@ -949,6 +949,7 @@ int inet_dccp_listen(struct socket *sock, int backlog)
 	if (!((1 << old_state) & (DCCPF_CLOSED | DCCPF_LISTEN)))
 		goto out;
 
+	sk->sk_max_ack_backlog = backlog;
 	/* Really, if the socket is already in listen state
 	 * we can only allow the backlog to be adjusted.
 	 */
@@ -961,7 +962,6 @@ int inet_dccp_listen(struct socket *sock, int backlog)
 		if (err)
 			goto out;
 	}
-	sk->sk_max_ack_backlog = backlog;
 	err = 0;
 
 out:
