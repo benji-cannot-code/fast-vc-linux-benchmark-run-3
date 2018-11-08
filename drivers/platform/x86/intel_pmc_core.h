88e4eda6fa0eed27aab247097022c334bfd8e219
Rajneesh Bhardwaj <rajneesh.bhardwaj@linux.intel.com>
2018-11-09 00:32:44 +0530

    platform/x86: intel_pmc_core: Decode Snoop / Non Snoop LTR
    The LTR values follow PCIE LTR encoding format and can be decoded as per
    https://pcisig.com/sites/default/files/specification_documents/ECN_LatencyTolnReporting_14Aug08.pdf
    
    This adds support to translate the raw LTR values as read from the PMC
    to meaningful values in nanosecond units of time.
    
    Signed-off-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@linux.intel.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/intel_pmc_core.h
+++ drivers/platform/x86/intel_pmc_core.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef PMC_CORE_H
 #define PMC_CORE_H
 
+#include <linux/bits.h>
+
 #define PMC_BASE_ADDR_DEFAULT			0xFE000000
 
 /* Sunrise Point Power Management Controller PCI Device ID */
@@ -169,6 +171,11 @@ enum ppfear_regs {
 #define CNP_PMC_LTR_CNV				0x1BF0
 #define CNP_PMC_LTR_EMMC			0x1BF4
 #define CNP_PMC_LTR_UFSX2			0x1BF8
+
+#define LTR_DECODED_VAL				GENMASK(9, 0)
+#define LTR_DECODED_SCALE			GENMASK(12, 10)
+#define LTR_REQ_SNOOP				BIT(15)
+#define LTR_REQ_NONSNOOP			BIT(31)
 
 struct pmc_bit_map {
 	const char *name;
