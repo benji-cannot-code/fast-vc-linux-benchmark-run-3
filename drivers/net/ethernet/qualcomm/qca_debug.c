fd7a595febed12e27bd1edfd5e002366379c2623
Stefan Wahren <stefan.wahren@i2se.com>
2018-11-08 14:38:21 +0100

    net: qca_spi: Add available buffer space verification
    Interferences on the SPI line could distort the response of
    available buffer space. So at least we should check that the
    response doesn't exceed the maximum available buffer space.
    In error case increase a new error counter and retry it later.
    This behavior avoids buffer errors in the QCA7000, which
    results in an unnecessary chip reset including packet loss.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/qualcomm/qca_debug.c
+++ drivers/net/ethernet/qualcomm/qca_debug.c
@@ -62,6 +62,7 @@ static const char qcaspi_gstrings_stats[][ETH_GSTRING_LEN] = {
 	"Transmit ring full",
 	"SPI errors",
 	"Write verify errors",
+	"Buffer available errors",
 };
 
 #ifdef CONFIG_DEBUG_FS
