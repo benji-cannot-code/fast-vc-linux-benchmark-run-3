c74e7587a03b218c2b571338572414605b239db2
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-11-08 09:29:59 +0200

    ASoC: omap-mcbsp: Remove redundant check for mcbsp->pdata
    The driver will not probe if the pdata is not provided or created.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Acked-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Tested-by: Jarkko Nikula <jarkko.nikula@bitmer.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/omap/omap-mcbsp.c
+++ sound/soc/omap/omap-mcbsp.c
@@ -325,7 +325,7 @@ static int omap_mcbsp_request(struct omap_mcbsp *mcbsp)
 	mcbsp->reg_cache = reg_cache;
 	spin_unlock(&mcbsp->lock);
 
-	if (mcbsp->pdata && mcbsp->pdata->ops && mcbsp->pdata->ops->request)
+	if(mcbsp->pdata->ops && mcbsp->pdata->ops->request)
 		mcbsp->pdata->ops->request(mcbsp->id - 1);
 
 	/*
@@ -362,7 +362,7 @@ static int omap_mcbsp_request(struct omap_mcbsp *mcbsp)
 err_free_irq:
 	free_irq(mcbsp->tx_irq, (void *)mcbsp);
 err_clk_disable:
-	if (mcbsp->pdata && mcbsp->pdata->ops && mcbsp->pdata->ops->free)
+	if(mcbsp->pdata->ops && mcbsp->pdata->ops->free)
 		mcbsp->pdata->ops->free(mcbsp->id - 1);
 
 	/* Disable wakeup behavior */
@@ -383,7 +383,7 @@ static void omap_mcbsp_free(struct omap_mcbsp *mcbsp)
 {
 	void *reg_cache;
 
-	if (mcbsp->pdata && mcbsp->pdata->ops && mcbsp->pdata->ops->free)
+	if(mcbsp->pdata->ops && mcbsp->pdata->ops->free)
 		mcbsp->pdata->ops->free(mcbsp->id - 1);
 
 	/* Disable wakeup behavior */
