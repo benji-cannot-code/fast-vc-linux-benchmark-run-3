cb1813edbfed7f3352abed84978f993e4005f1c2
Max Filippov <jcmvbkbc@gmail.com>
2018-11-09 15:43:14 -0800

    xtensa: use NO_SYSCALL instead of -1
    For the sake of clarity define macro NO_SYSCALL and use it for
    setting/checking struct pt_regs::syscall field.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/include/asm/ptrace.h
+++ arch/xtensa/include/asm/ptrace.h
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *		+-----------------------+ --------
  */
 
+#define NO_SYSCALL (-1)
+
 #ifndef __ASSEMBLY__
 
 #include <asm/coprocessor.h>
