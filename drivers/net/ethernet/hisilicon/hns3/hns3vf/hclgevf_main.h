38ecabacf95a44494b21b4dab71e70cc962b267a
Huazhong Tan <tanhuazhong@huawei.com>
2018-11-09 22:07:49 +0800

    net: hns3: add reset handling for VF when doing Core/Global/IMP reset
    When a Core/Global/IMP reset occurs, the hardware sets the reset status
    register of all PF/VF and reports a reset interrupt to all PF/VF and
    firmware.
    
    When receiving the reset interrupt:
    1. The firmware will wait for 100 ms before resetting the hardware and
       clear the reset status register of all PF when hardware reset is done.
    2. The PF/VF driver needs to down the netdev within 100 ms and then wait
       for hardware reset to finish.
    3. After firmware clearing the reset status register of all PF, the PF
       driver reinitializes the hardware and clear the reset status register
       of it's VF.
    4. After PF driver clearing the reset status register of VF, the VF driver
       reinitializes the hardware.
    
    This patch mainly add handling for the step 4.
    
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_main.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_main.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HCLGEVF_VECTOR0_CMDQ_SRC_REG	0x27100
 /* CMDQ register bits for RX event(=MBX event) */
 #define HCLGEVF_VECTOR0_RX_CMDQ_INT_B	1
+/* RST register bits for RESET event */
+#define HCLGEVF_VECTOR0_RST_INT_B	2
 
 #define HCLGEVF_TQP_RESET_TRY_TIMES	10
 /* Reset related Registers */
@@ -60,6 +62,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HCLGEVF_D_IP_BIT		BIT(2)
 #define HCLGEVF_S_IP_BIT		BIT(3)
 #define HCLGEVF_V_TAG_BIT		BIT(4)
+
+enum hclgevf_evt_cause {
+	HCLGEVF_VECTOR0_EVENT_RST,
+	HCLGEVF_VECTOR0_EVENT_MBX,
+	HCLGEVF_VECTOR0_EVENT_OTHER,
+};
 
 /* states of hclgevf device & tasks */
 enum hclgevf_states {
