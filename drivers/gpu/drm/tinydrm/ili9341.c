ec1d2a685db04902985e972ff217d27af3e346ff
Noralf Trønnes <noralf@tronnes.org>
2018-11-10 15:56:47 +0100

    drm/tinydrm: Use DRM_GEM_CMA_VMAP_DRIVER_OPS
    The CMA helper now has the functionality to ensure a virtual address on
    imported buffer so use that.
    
    While touching all tinydrm drivers, remove the unnecessary inclusion of
    drm_fb_helper.h in some drivers.
    
    Cc: David Lechner <david@lechnology.com>
    Cc: Eric Anholt <eric@anholt.net>
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>
    Acked-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181110145647.17580-6-noralf@tronnes.org

--- drivers/gpu/drm/tinydrm/ili9341.c
+++ drivers/gpu/drm/tinydrm/ili9341.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/property.h>
 #include <linux/spi/spi.h>
 
-#include <drm/drm_fb_helper.h>
+#include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
 #include <drm/drm_modeset_helper.h>
 #include <drm/tinydrm/mipi-dbi.h>
@@ -145,7 +145,7 @@ DEFINE_DRM_GEM_CMA_FOPS(ili9341_fops);
 static struct drm_driver ili9341_driver = {
 	.driver_features	= DRIVER_GEM | DRIVER_MODESET | DRIVER_PRIME | DRIVER_ATOMIC,
 	.fops			= &ili9341_fops,
-	TINYDRM_GEM_DRIVER_OPS,
+	DRM_GEM_CMA_VMAP_DRIVER_OPS,
 	.debugfs_init		= mipi_dbi_debugfs_init,
 	.name			= "ili9341",
 	.desc			= "Ilitek ILI9341",
