c4c2150ad15b8aa01a96d871e07ba92912d55cc0
Stefan Wahren <stefan.wahren@i2se.com>
2018-11-11 21:23:55 +0100

    mmc: bcm2835: Release DMA channel on driver unload
    We need to release the slave DMA channel during driver unload.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Acked-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/bcm2835.c
+++ drivers/mmc/host/bcm2835.c
@@ -1447,6 +1447,9 @@ static int bcm2835_remove(struct platform_device *pdev)
 	cancel_work_sync(&host->dma_work);
 	cancel_delayed_work_sync(&host->timeout_work);
 
+	if (host->dma_chan_rxtx)
+		dma_release_channel(host->dma_chan_rxtx);
+
 	mmc_free_host(host->mmc);
 	platform_set_drvdata(pdev, NULL);
 
