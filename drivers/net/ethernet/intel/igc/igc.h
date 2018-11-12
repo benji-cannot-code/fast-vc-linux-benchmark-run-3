62d918cde5be1ce951f75f1d0d8ec4d7bfb7be9b
Sasha Neftin <sasha.neftin@intel.com>
2018-11-12 11:05:20 +0200

    igc: Remove obsolete IGC_ERR define
    Address community comment.
    Remove obsolete IGC_ERR define and use dev_err method.
    Suggested by Joe Perches.
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igc/igc.h
+++ drivers/net/ethernet/intel/igc/igc.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ethtool.h>
 #include <linux/sctp.h>
 
-#define IGC_ERR(args...) pr_err("igc: " args)
-
 #include "igc_hw.h"
 
 /* main */
