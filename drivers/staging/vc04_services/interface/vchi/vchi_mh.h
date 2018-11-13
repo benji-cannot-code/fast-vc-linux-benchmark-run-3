a9b087fffada9fa0f156739de93b27efdf861f6a
André Almeida <andrealmeid@riseup.net>
2018-11-13 16:58:34 -0200

    staging: vchi: change type 'int32_t' to 's32'
    As one can see in a typedef at linux/types.h:103, s32 and int32_t are
    both a 32 bits signed integer. s32 is preferred because it is defined
    by the kernel maintainers and it prevents portability issues. Changed
    to meet checkpatch criteria.
    
    Signed-off-by: André Almeida <andrealmeid@riseup.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchi/vchi_mh.h
+++ drivers/staging/vc04_services/interface/vchi/vchi_mh.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-typedef int32_t VCHI_MEM_HANDLE_T;
+typedef s32 VCHI_MEM_HANDLE_T;
 #define VCHI_MEM_HANDLE_INVALID 0
 
 #endif
