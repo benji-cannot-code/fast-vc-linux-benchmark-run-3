e35dc61f2c81965d94937d19d12c4985cf6dc545
Johan Hovold <johan@kernel.org>
2018-11-14 16:09:01 +0100

    serdev: use zero to indicate infinite write timeout
    Use zero to indicate infinite timeout for the synchronous
    serdev_device_write() helper.
    
    This allows drivers to specify an infinite timeout without knowing about
    serdev implementation details, while also allowing the same timeout
    argument to be used for both serdev_device_write() and
    serdev_device_wait_until_sent().
    
    Note that passing zero to the current helper makes no sense; just call
    the asynchronous serdev_device_write_buf() directly instead.
    
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serdev/core.c
+++ drivers/tty/serdev/core.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_device.h>
 #include <linux/pm_domain.h>
 #include <linux/pm_runtime.h>
+#include <linux/sched.h>
 #include <linux/serdev.h>
 #include <linux/slab.h>
 
@@ -236,9 +237,11 @@ int serdev_device_write(struct serdev_device *serdev,
 	struct serdev_controller *ctrl = serdev->ctrl;
 	int ret;
 
-	if (!ctrl || !ctrl->ops->write_buf ||
-	    (timeout && !serdev->ops->write_wakeup))
+	if (!ctrl || !ctrl->ops->write_buf || !serdev->ops->write_wakeup)
 		return -EINVAL;
+
+	if (timeout == 0)
+		timeout = MAX_SCHEDULE_TIMEOUT;
 
 	mutex_lock(&serdev->write_lock);
 	do {
