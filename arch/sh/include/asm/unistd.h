5b6fd4e7d896d715ed560f4981e0d65dfc6cb8f4
Firoz Khan <firoz.khan@linaro.org>
2018-11-14 10:02:08 +0530

    sh: add __NR_syscalls along with NR_syscalls
    NR_syscalls macro holds the number of system call exist
    in sh architecture. We have to change the value of NR-
    _syscalls, if we add or delete a system call.
    
    One of the patch in this patch series has a script which
    will generate a uapi header based on syscall.tbl file.
    The syscall.tbl file contains the total number of system
    calls information. So we have two option to update NR_sy-
    scalls value.
    
    1. Update NR_syscalls in asm/unistd.h manually by count-
       ing the no.of system calls. No need to update NR_sys-
       calls until we either add a new system call or delete
       existing system call.
    
    2. We can keep this feature it above mentioned script,
       that will count the number of syscalls and keep it in
       a generated file. In this case we don't need to expli-
       citly update NR_syscalls in asm/unistd.h file.
    
    The 2nd option will be the recommended one. For that, I
    added the __NR_syscalls macro in uapi/asm/unistd_32/64.h
    along with NR_syscalls which is moved to asm/unistd.h.
    The macro __NR_syscalls also added for making the name
    convention same across all architecture. While __NR_sys-
    calls isn't strictly part of the uapi, having it as part
    of the generated header to simplifies the implementation.
    We also need to enclose this macro with #ifdef __KERNEL__
    to avoid side effects.
    
    Signed-off-by: Firoz Khan <firoz.khan@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/sh/include/asm/unistd.h
+++ arch/sh/include/asm/unistd.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  include <asm/unistd_64.h>
 # endif
 
+#define NR_syscalls	__NR_syscalls
+
 # define __ARCH_WANT_NEW_STAT
 # define __ARCH_WANT_OLD_READDIR
 # define __ARCH_WANT_OLD_STAT
