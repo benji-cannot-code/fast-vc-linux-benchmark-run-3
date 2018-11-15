f48cebab4221ea35c29b39393f2053be5c632d60
AKASHI Takahiro <takahiro.akashi@linaro.org>
2018-11-15 14:52:50 +0900

    arm64: kexec_file: allow for loading Image-format kernel
    This patch provides kexec_file_ops for "Image"-format kernel. In this
    implementation, a binary is always loaded with a fixed offset identified
    in text_offset field of its header.
    
    Regarding signature verification for trusted boot, this patch doesn't
    contains CONFIG_KEXEC_VERIFY_SIG support, which is to be added later
    in this series, but file-attribute-based verification is still a viable
    option by enabling IMA security subsystem.
    
    You can sign(label) a to-be-kexec'ed kernel image on target file system
    with:
        $ evmctl ima_sign --key /path/to/private_key.pem Image
    
    On live system, you must have IMA enforced with, at least, the following
    security policy:
        "appraise func=KEXEC_KERNEL_CHECK appraise_type=imasig"
    
    See more details about IMA here:
        https://sourceforge.net/p/linux-ima/wiki/Home/
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Reviewed-by: James Morse <james.morse@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/machine_kexec_file.c
+++ arch/arm64/kernel/machine_kexec_file.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FDT_PSTR_BOOTARGS	"bootargs"
 
 const struct kexec_file_ops * const kexec_file_loaders[] = {
+	&kexec_image_ops,
 	NULL
 };
 
