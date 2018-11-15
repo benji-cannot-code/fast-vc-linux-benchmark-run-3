dd8ed03e8f2681be403a528242020935cdb07e5c
Saravanan Sekar <sravanhome@gmail.com>
2018-11-15 13:47:45 +0100

    pinctrl: actions: define pad control configurtion to SoC specific
    pad control for s900 and s700 are differs in number of
    pull control configuraions
    s900 has 4 pull controls - high impedence, pull up, pull down, repeater
    s700, s500 has 2 pull controls - pull up and pull down
    
    so pad control configuration has to SoC specific, moved out from pinctrl
    common to s900 specific.
    
    Signed-off-by: Parthiban Nallathambi <pn@denx.de>
    Signed-off-by: Saravanan Sekar <sravanhome@gmail.com>
    Acked-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/actions/pinctrl-owl.h
+++ drivers/pinctrl/actions/pinctrl-owl.h
@@ -146,13 +146,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		.shared_ctl_offset = _share,			\
 	}
 
-enum owl_pinconf_pull {
-	OWL_PINCONF_PULL_HIZ,
-	OWL_PINCONF_PULL_DOWN,
-	OWL_PINCONF_PULL_UP,
-	OWL_PINCONF_PULL_HOLD,
-};
-
 enum owl_pinconf_drv {
 	OWL_PINCONF_DRV_2MA,
 	OWL_PINCONF_DRV_4MA,
@@ -306,6 +299,12 @@ struct owl_pinctrl_soc_data {
 	unsigned int ngpios;
 	const struct owl_gpio_port *ports;
 	unsigned int nports;
+	int (*padctl_val2arg)(const struct owl_padinfo *padinfo,
+				unsigned int param,
+				u32 *arg);
+	int (*padctl_arg2val)(const struct owl_padinfo *info,
+				unsigned int param,
+				u32 *arg);
 };
 
 int owl_pinctrl_probe(struct platform_device *pdev,
