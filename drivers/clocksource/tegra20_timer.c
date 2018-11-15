c009f59978ed8dac28c234d27fca1882ac0f3cca
Geert Uytterhoeven <geert+renesas@glider.be>
2018-11-15 11:00:37 +0100

    clockevents/drivers/tegra20: Remove obsolete inclusion of <asm/smp_twd.h>
    As of commit da4a686a2cfb077a ("ARM: smp_twd: convert to use CLKSRC_OF
    init"), this header file is no longer used.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/tegra20_timer.c
+++ drivers/clocksource/tegra20_timer.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 
 #include <asm/mach/time.h>
-#include <asm/smp_twd.h>
 
 #define RTC_SECONDS            0x08
 #define RTC_SHADOW_SECONDS     0x0c
