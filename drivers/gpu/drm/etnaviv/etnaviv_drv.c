51cadbb5da2ae03a85c88203e188522790e83628
Fernando Ramos <greenfoo@gluegarage.com>
2018-11-15 23:16:23 +0100

    drm: replace "drm_dev_unref" function with "drm_dev_put"
    This patch unifies the naming of DRM functions for reference counting as
    requested on Documentation/gpu/todo.rst
    
    Signed-off-by: Fernando Ramos <greenfoo@gluegarage.com>
    Acked-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Acked-by: Alexey Brodkin <abrodkin@synopsys.com>
    Acked-by: Stefan Agner <stefan@agner.ch>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181115221634.22715-4-greenfoo@gluegarage.com

--- drivers/gpu/drm/etnaviv/etnaviv_drv.c
+++ drivers/gpu/drm/etnaviv/etnaviv_drv.c
@@ -551,7 +551,7 @@ out_register:
 out_bind:
 	kfree(priv);
 out_unref:
-	drm_dev_unref(drm);
+	drm_dev_put(drm);
 
 	return ret;
 }
@@ -568,7 +568,7 @@ static void etnaviv_unbind(struct device *dev)
 	drm->dev_private = NULL;
 	kfree(priv);
 
-	drm_dev_unref(drm);
+	drm_dev_put(drm);
 }
 
 static const struct component_master_ops etnaviv_master_ops = {
