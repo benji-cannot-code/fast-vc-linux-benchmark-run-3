a4b8d22e4a58bfe0c30a5f2d8b6bc76ce0402a30
Max Filippov <jcmvbkbc@gmail.com>
2018-11-15 18:17:41 -0800

    xtensa: drop fast_syscall_kernel
    There must be no xtensa-specific syscalls from the kernel code: register
    spilling uses call+entry sequence and atomics have proper function
    implementations. Drop fast_syscall_xtensa.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/kernel/traps.c
+++ arch/xtensa/kernel/traps.c
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void kernel_exception(void);
 extern void user_exception(void);
 
-extern void fast_syscall_kernel(void);
 extern void fast_syscall_user(void);
 extern void fast_alloca(void);
 extern void fast_unaligned(void);
@@ -90,7 +89,6 @@ typedef struct {
 static dispatch_init_table_t __initdata dispatch_init_table[] = {
 
 { EXCCAUSE_ILLEGAL_INSTRUCTION,	0,	   do_illegal_instruction},
-{ EXCCAUSE_SYSTEM_CALL,		KRNL,	   fast_syscall_kernel },
 { EXCCAUSE_SYSTEM_CALL,		USER,	   fast_syscall_user },
 { EXCCAUSE_SYSTEM_CALL,		0,	   system_call },
 /* EXCCAUSE_INSTRUCTION_FETCH unhandled */
