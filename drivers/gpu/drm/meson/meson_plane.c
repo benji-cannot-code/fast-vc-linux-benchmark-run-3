0f275cf2c40c07a8f5a4bd78ee3d9eea6cf7e447
Koen Kooi <koen@dominion.thruhere.net>
2018-11-19 11:02:39 +0100

    drm/meson: Add implicit fencing support for primary and overlay planes
    Suggested by Qiang Yu <yuq825@gmail.com> to fix tearing artefacts in the
    Kodi GUI.
    
    Suggested-by: Qiang Yu <yuq825@gmail.com>
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    [narmstrong: added Suggested-by tag]
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1542621759-26413-1-git-send-email-koen@dominion.thruhere.net

--- drivers/gpu/drm/meson/meson_plane.c
+++ drivers/gpu/drm/meson/meson_plane.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_plane_helper.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_fb_cma_helper.h>
+#include <drm/drm_gem_framebuffer_helper.h>
 #include <drm/drm_rect.h>
 
 #include "meson_plane.h"
@@ -323,6 +324,7 @@ static const struct drm_plane_helper_funcs meson_plane_helper_funcs = {
 	.atomic_check	= meson_plane_atomic_check,
 	.atomic_disable	= meson_plane_atomic_disable,
 	.atomic_update	= meson_plane_atomic_update,
+	.prepare_fb	= drm_gem_fb_prepare_fb,
 };
 
 static const struct drm_plane_funcs meson_plane_funcs = {
