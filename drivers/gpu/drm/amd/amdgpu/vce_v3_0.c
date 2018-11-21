d031d7d4e8b0295f0a42087196713fab37a12fe0
Brajeswar Ghosh <brajeswar.linux@gmail.com>
2018-11-21 12:41:03 +0530

    drm/amd/amdgpu/vce_v3_0.c: Remove duplicate header
    Remove gca/gfx_8_0_d.h which is included more than once
    
    Reviewed-by: Junwei Zhang <Jerry.Zhang@amd.com>
    Signed-off-by: Brajeswar Ghosh <brajeswar.linux@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/vce_v3_0.c
+++ drivers/gpu/drm/amd/amdgpu/vce_v3_0.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "gca/gfx_8_0_d.h"
 #include "smu/smu_7_1_2_d.h"
 #include "smu/smu_7_1_2_sh_mask.h"
-#include "gca/gfx_8_0_d.h"
 #include "gca/gfx_8_0_sh_mask.h"
 #include "ivsrcid/ivsrcid_vislands30.h"
 
