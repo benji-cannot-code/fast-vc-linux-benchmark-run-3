3cb09d7ca6bfe88eb20d526be9c1089ce10b4e37
Aaro Koskinen <aaro.koskinen@iki.fi>
2018-11-22 00:37:24 +0200

    MIPS: OCTEON: setup: include asm/fw/fw.h
    Include asm/fw/fw.h to get the declaration of fw_init_cmdline().
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Patchwork: https://patchwork.linux-mips.org/patch/21206/
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: James Hogan <jhogan@kernel.org>
    Cc: linux-mips@linux-mips.org

--- arch/mips/cavium-octeon/setup.c
+++ arch/mips/cavium-octeon/setup.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mipsregs.h>
 #include <asm/bootinfo.h>
 #include <asm/sections.h>
+#include <asm/fw/fw.h>
 #include <asm/setup.h>
 #include <asm/time.h>
 
