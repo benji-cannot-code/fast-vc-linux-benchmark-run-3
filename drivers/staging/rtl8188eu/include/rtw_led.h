ed09703a2b2f7c5f752ee13da99c3c42299e1e9c
Michael Straube <straube.linux@gmail.com>
2018-11-21 20:49:34 +0100

    staging: rtl8188eu: rename BlinkHandler to avoid CamelCase
    Rename the function BlinkHandler to avoid CamelCase.
    BlinkHandler -> blink_handler
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_led.h
+++ drivers/staging/rtl8188eu/include/rtw_led.h
@@ -94,7 +94,7 @@ void InitLed871x(struct adapter *padapter, struct LED_871x *pLed);
 void DeInitLed871x(struct LED_871x *pLed);
 
 /* hal... */
-void BlinkHandler(struct LED_871x *pLed);
+void blink_handler(struct LED_871x *pLed);
 void sw_led_on(struct adapter *padapter, struct LED_871x *pLed);
 void sw_led_off(struct adapter *padapter, struct LED_871x *pLed);
 
