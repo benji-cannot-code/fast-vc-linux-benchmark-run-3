8d565aad91ba3babc6f0284508d5f633e43fe823
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-11-22 00:57:40 +0000

    ASoC: audio-graph-card: tidyup define position
    commit f986907c9225 ("ASoC: audio-graph-card: add widgets and routing for
    external amplifier support") added new function
    asoc_graph_card_outdrv_event(), but the inserted position breaks
    define area. This patch tidyup it
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/generic/audio-graph-card.c
+++ sound/soc/generic/audio-graph-card.c
@@ -37,6 +37,11 @@ struct graph_card_data {
 	struct gpio_desc *pa_gpio;
 };
 
+#define graph_priv_to_card(priv) (&(priv)->snd_card)
+#define graph_priv_to_props(priv, i) ((priv)->dai_props + (i))
+#define graph_priv_to_dev(priv) (graph_priv_to_card(priv)->dev)
+#define graph_priv_to_link(priv, i) (graph_priv_to_card(priv)->dai_link + (i))
+
 static int asoc_graph_card_outdrv_event(struct snd_soc_dapm_widget *w,
 					struct snd_kcontrol *kcontrol,
 					int event)
@@ -63,11 +68,6 @@ static const struct snd_soc_dapm_widget asoc_graph_card_dapm_widgets[] = {
 			       0, 0, NULL, 0, asoc_graph_card_outdrv_event,
 			       SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_PRE_PMD),
 };
-
-#define graph_priv_to_card(priv) (&(priv)->snd_card)
-#define graph_priv_to_props(priv, i) ((priv)->dai_props + (i))
-#define graph_priv_to_dev(priv) (graph_priv_to_card(priv)->dev)
-#define graph_priv_to_link(priv, i) (graph_priv_to_card(priv)->dai_link + (i))
 
 static int asoc_graph_card_startup(struct snd_pcm_substream *substream)
 {
