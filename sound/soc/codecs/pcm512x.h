61725a1dbd53a77bb36c8998e8da9a79f50ca248
Dimitris Papavasiliou <dpapavas@gmail.com>
2018-11-24 22:05:42 +0200

    ASoC: pcm512x: Implement the digital_mute interface
    Clicks and pops of various volumes can be produced while the device is
    opened, closed, put into and taken out of standby, or reconfigured.
    Fix this, by implementing the digital_mute interface, so that the
    output is muted during such operations.
    
    Signed-off-by: Dimitris Papavasiliou <dpapavas@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/pcm512x.h
+++ sound/soc/codecs/pcm512x.h
@@ -113,7 +113,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCM512x_RQST_SHIFT 4
 
 /* Page 0, Register 3 - mute */
+#define PCM512x_RQMR (1 << 0)
 #define PCM512x_RQMR_SHIFT 0
+#define PCM512x_RQML (1 << 4)
 #define PCM512x_RQML_SHIFT 4
 
 /* Page 0, Register 4 - PLL */
