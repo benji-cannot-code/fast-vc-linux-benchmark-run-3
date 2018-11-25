a546ddc94fb9b5425437298c6d166e33fa33b39c
Jason Gunthorpe <jgg@mellanox.com>
2018-11-25 20:51:14 +0200

    RDMA/uverbs: Make write() handlers return 0 on success
    Currently they return the command length, while all other handlers return
    0. This makes the write path closer to the write_ex and ioctl path.
    
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- drivers/infiniband/core/uverbs_uapi.c
+++ drivers/infiniband/core/uverbs_uapi.c
@@ -9,9 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rdma_core.h"
 #include "uverbs.h"
 
-static ssize_t ib_uverbs_notsupp(struct uverbs_attr_bundle *attrs,
-				 const char __user *buf, int in_len,
-				 int out_len)
+static int ib_uverbs_notsupp(struct uverbs_attr_bundle *attrs,
+			     const char __user *buf, int in_len, int out_len)
 {
 	return -EOPNOTSUPP;
 }
