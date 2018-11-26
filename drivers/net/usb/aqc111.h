8bc331fcd6610c74f6ff7f806c2ca9650897e5b1
Dmitry Bezrukov <dmitry.bezrukov@aquantia.com>
2018-11-26 09:33:26 +0000

    net: usb: aqc111: Add support for enable/disable checksum offload
    Signed-off-by: Dmitry Bezrukov <dmitry.bezrukov@aquantia.com>
    Signed-off-by: Igor Russkikh <igor.russkikh@aquantia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/usb/aqc111.h
+++ drivers/net/usb/aqc111.h
@@ -139,6 +139,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /******************************************************************************/
 
 struct aqc111_data {
+	u8 rx_checksum;
 	u8 link_speed;
 	u8 link;
 	u8 autoneg;
