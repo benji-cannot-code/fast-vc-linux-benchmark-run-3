d1c906f703c58640ab81270e39d8f232db8c0a1b
Manasi Navare <manasi.d.navare@intel.com>
2018-11-28 12:26:12 -0800

    drm/i915/dp: Add DSC params and DSC config to intel_crtc_state
    Basic DSC parameters and DSC configuration data needs to be computed
    for each of the requested mode during atomic check. This is
    required since for certain modes, valid DSC parameters and config
    data might not be computed in which case compression cannot be
    enabled for that mode.
    For that reason we need to add these params and config structure
    to the intel_crtc_state so that if valid this state information
    can directly be used while enabling DSC in atomic commit.
    
    v2:
    * Rebase on drm-tip (Manasi)
    
    Cc: Gaurav K Singh <gaurav.k.singh@intel.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Cc: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
    Reviewed-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181128202628.20238-1-manasi.d.navare@intel.com

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_auth.h>
 #include <drm/drm_cache.h>
 #include <drm/drm_util.h>
+#include <drm/drm_dsc.h>
 
 #include "i915_fixed.h"
 #include "i915_params.h"
