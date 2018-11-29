f67f0fd19f068f4816519237030c607181b46b0d
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2018-11-29 16:02:10 +0100

    x86/fpu: Don't export __kernel_fpu_{begin,end}()
    There is one user of __kernel_fpu_begin() and before invoking it,
    it invokes preempt_disable(). So it could invoke kernel_fpu_begin()
    right away. The 32bit version of arch_efi_call_virt_setup() and
    arch_efi_call_virt_teardown() does this already.
    
    The comment above *kernel_fpu*() claims that before invoking
    __kernel_fpu_begin() preemption should be disabled and that KVM is a
    good example of doing it. Well, KVM doesn't do that since commit
    
      f775b13eedee2 ("x86,kvm: move qemu/guest FPU switching out to vcpu_run")
    
    so it is not an example anymore.
    
    With EFI gone as the last user of __kernel_fpu_{begin|end}(), both can
    be made static and not exported anymore.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Rik van Riel <riel@surriel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: "Jason A. Donenfeld" <Jason@zx2c4.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Nicolai Stange <nstange@suse.de>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kvm ML <kvm@vger.kernel.org>
    Cc: linux-efi <linux-efi@vger.kernel.org>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/20181129150210.2k4mawt37ow6c2vq@linutronix.de

--- arch/x86/kernel/fpu/core.c
+++ arch/x86/kernel/fpu/core.c
@@ -94,7 +94,7 @@ bool irq_fpu_usable(void)
 }
 EXPORT_SYMBOL(irq_fpu_usable);
 
-void __kernel_fpu_begin(void)
+static void __kernel_fpu_begin(void)
 {
 	struct fpu *fpu = &current->thread.fpu;
 
@@ -112,9 +112,8 @@ void __kernel_fpu_begin(void)
 		__cpu_invalidate_fpregs_state();
 	}
 }
-EXPORT_SYMBOL(__kernel_fpu_begin);
 
-void __kernel_fpu_end(void)
+static void __kernel_fpu_end(void)
 {
 	struct fpu *fpu = &current->thread.fpu;
 
@@ -123,7 +122,6 @@ void __kernel_fpu_end(void)
 
 	kernel_fpu_enable();
 }
-EXPORT_SYMBOL(__kernel_fpu_end);
 
 void kernel_fpu_begin(void)
 {
