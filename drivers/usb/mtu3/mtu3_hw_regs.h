54ed344c98879ff09285851898134fdbf1b00a28
Chunfeng Yun <chunfeng.yun@mediatek.com>
2018-11-29 10:34:35 +0800

    usb: mtu3: enable SETUPENDISR interrupt
    If the controller receives a new SETUP during SETUP data stage,
    and will generate SETUPENDISR interrupt, the driver should abort
    the current SETUP command and process the new one.
    
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/mtu3/mtu3_hw_regs.h
+++ drivers/usb/mtu3/mtu3_hw_regs.h
@@ -105,6 +105,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* U3D_EPISR */
 #define EPRISR(x)		(BIT(16) << (x))
+#define SETUPENDISR		BIT(16)
 #define EPTISR(x)		(BIT(0) << (x))
 #define EP0ISR			BIT(0)
 
