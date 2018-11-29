dec648a615b6deac21d46a2b86e54d4632ccadf8
Huang Rui <ray.huang@amd.com>
2018-11-29 13:56:06 +0800

    drm/amd/powerplay: add new smu ip block
    This patch add amdgpu_smu ip block skeleton as placeholder.
    We plan use new smu ip block from Vega20.
    
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "amdgpu_gem.h"
 #include "amdgpu_doorbell.h"
 #include "amdgpu_amdkfd.h"
+#include "amdgpu_smu.h"
 
 #define MAX_GPU_INSTANCE		16
 
@@ -842,6 +843,9 @@ struct amdgpu_device {
 	/* powerplay */
 	struct amd_powerplay		powerplay;
 	bool				pp_force_state_enabled;
+
+	/* smu */
+	struct smu_context		smu;
 
 	/* dpm */
 	struct amdgpu_pm		pm;
