0f62e2cf1b10efe2a34b95558e8d0b071fea42b7
Linus Walleij <linus.walleij@linaro.org>
2018-12-02 09:43:23 +0100

    ARM: pxa: Add GPIO descriptors for Palm27x
    The Palm27x devices set up the MMC card detect and
    write protect lines with a special helper function.
    Augment this helper function to also accept an optional
    GPIO descriptor table and pass and register this for
    all the Palm27x devices in that family.
    
    Cc: Daniel Mack <daniel@zonque.org>
    Cc: Robert Jarzmik <robert.jarzmik@free.fr>
    Cc: Bartosz Golaszewski <brgl@bgdev.pl>
    Cc: Andrea Adami <andrea.adami@gmail.com>
    Acked-by: Robert Jarzmik <robert.jarzmik@free.fr>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- arch/arm/mach-pxa/palmtc.c
+++ arch/arm/mach-pxa/palmtc.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input.h>
 #include <linux/pwm.h>
 #include <linux/pwm_backlight.h>
-#include <linux/gpio.h>
+#include <linux/gpio/machine.h>
 #include <linux/input/matrix_keypad.h>
 #include <linux/ucb1400.h>
 #include <linux/power_supply.h>
