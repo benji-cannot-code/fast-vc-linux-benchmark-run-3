6639dcb86b731a84bcd2cac7cdd4a196f31fee6e
Linus Walleij <linus.walleij@linaro.org>
2018-12-02 09:43:18 +0100

    mmc: spi: Convert to use GPIO descriptors
    Switch the SPI MMC driver to use GPIO descriptors internally
    and just look those up using the standard slot GPIO
    functions mmc_gpiod_request_cd() and mmc_gpiod_request_ro().
    Make sure to request index 0 and 1 in accordance with the
    SPI MMC DT binding, and add the same GPIOs in machine
    descriptor tables on all boards that use SPI MMC in
    board files.
    
    The lines are flagged as GPIO_ACTIVE_[LOW|HIGH] as that is
    what they are, and since we can now rely on the descriptors
    to have the right polarity, we set the
    "override_active_level" to false in mmc_gpiod_request_cd()
    and mmc_gpiod_request_ro().
    
    Cc: Hartley Sweeten <hsweeten@visionengravers.com> # Vision EP9307
    Cc: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- include/linux/spi/mmc_spi.h
+++ include/linux/spi/mmc_spi.h
@@ -9,11 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct device;
 struct mmc_host;
 
-#define MMC_SPI_USE_CD_GPIO			(1 << 0)
-#define MMC_SPI_USE_RO_GPIO			(1 << 1)
-#define MMC_SPI_CD_GPIO_ACTIVE_LOW		(1 << 2)
-#define MMC_SPI_RO_GPIO_ACTIVE_LOW		(1 << 3)
-
 /* Put this in platform_data of a device being used to manage an MMC/SD
  * card slot.  (Modeled after PXA mmc glue; see that for usage examples.)
  *
@@ -27,16 +22,6 @@ struct mmc_spi_platform_data {
 		irqreturn_t (*)(int, void *),
 		void *);
 	void (*exit)(struct device *, void *);
-
-	/*
-	 * Card Detect and Read Only GPIOs. To enable debouncing on the card
-	 * detect GPIO, set the cd_debounce to the debounce time in
-	 * microseconds.
-	 */
-	unsigned int flags;
-	unsigned int cd_gpio;
-	unsigned int cd_debounce;
-	unsigned int ro_gpio;
 
 	/* Capabilities to pass into mmc core (e.g. MMC_CAP_NEEDS_POLL). */
 	unsigned long caps;
