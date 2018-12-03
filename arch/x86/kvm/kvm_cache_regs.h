2850c0a78f74984a4914527e11c901b0e090a92d
Sean Christopherson <sean.j.christopherson@intel.com>
2018-12-03 13:52:54 -0800

    KVM: x86: Add requisite includes to kvm_cache_regs.h
    Until this point vmx.c has been the only consumer and included the
    file after many others.  Prepare for multiple consumers, i.e. the
    shattering of vmx.c
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/kvm_cache_regs.h
+++ arch/x86/kvm/kvm_cache_regs.h
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ASM_KVM_CACHE_REGS_H
 #define ASM_KVM_CACHE_REGS_H
 
+#include <linux/kvm_host.h>
+
 #define KVM_POSSIBLE_CR0_GUEST_BITS X86_CR0_TS
 #define KVM_POSSIBLE_CR4_GUEST_BITS				  \
 	(X86_CR4_PVI | X86_CR4_DE | X86_CR4_PCE | X86_CR4_OSFXSR  \
