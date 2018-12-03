b9202661cc6e282195695f29d8834628a125d2c9
Sean Christopherson <sean.j.christopherson@intel.com>
2018-12-03 13:52:55 -0800

    KVM: x86: Add requisite includes to hyperv.h
    Until this point vmx.c has been the only consumer and included the
    file after many others.  Prepare for multiple consumers, i.e. the
    shattering of vmx.c
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/hyperv.h
+++ arch/x86/kvm/hyperv.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ARCH_X86_KVM_HYPERV_H__
 #define __ARCH_X86_KVM_HYPERV_H__
 
+#include <linux/kvm_host.h>
+
 static inline struct kvm_vcpu_hv *vcpu_to_hv_vcpu(struct kvm_vcpu *vcpu)
 {
 	return &vcpu->arch.hyperv;
