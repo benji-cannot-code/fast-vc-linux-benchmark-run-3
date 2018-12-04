ed891f737244e684dfd965f2a38f355238ad5c93
David S. Miller <davem@davemloft.net>
2018-12-04 08:47:44 -0800

    phy: Revert toggling reset changes.
    This reverts:
    
    ef1b5bf506b1 ("net: phy: Fix not to call phy_resume() if PHY is not attached")
    8c85f4b81296 ("net: phy: micrel: add toggling phy reset if PHY is not  attached")
    
    Andrew Lunn informs me that there are alternative efforts
    underway to fix this more properly.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/micrel.c
+++ drivers/net/phy/micrel.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *			 ksz9477
  */
 
-#include <linux/delay.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/phy.h>
@@ -836,13 +835,6 @@ static int kszphy_suspend(struct phy_device *phydev)
 static int kszphy_resume(struct phy_device *phydev)
 {
 	int ret;
-
-	if (!phydev->attached_dev) {
-		/* If the PHY is not attached, toggle the reset */
-		phy_device_reset(phydev, 1);
-		udelay(1);
-		phy_device_reset(phydev, 0);
-	}
 
 	genphy_resume(phydev);
 
