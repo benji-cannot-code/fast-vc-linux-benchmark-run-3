1c4425de36a108e19b8afe1d7f4f29b76da512d9
Anup Patel <anup@brainfault.org>
2018-12-04 15:59:52 +0530

    clocksource/drivers/riscv_timer: Provide the sched_clock
    Currently, we don't have a sched_clock registered for RISC-V systems.
    This means Linux time keeping will use jiffies (running at HZ) as the
    default sched_clock.
    
    To avoid this, we explicity provide sched_clock using RISC-V rdtime
    instruction (similar to riscv_timer clocksource).
    
    Signed-off-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Palmer Dabbelt <palmer@sifive.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- drivers/clocksource/riscv_timer.c
+++ drivers/clocksource/riscv_timer.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <linux/delay.h>
 #include <linux/irq.h>
+#include <linux/sched_clock.h>
 #include <asm/smp.h>
 #include <asm/sbi.h>
 
@@ -46,6 +47,11 @@ static DEFINE_PER_CPU(struct clock_event_device, riscv_clock_event) = {
  * backwards when hopping between CPUs, practically it won't happen.
  */
 static unsigned long long riscv_clocksource_rdtime(struct clocksource *cs)
+{
+	return get_cycles64();
+}
+
+static u64 riscv_sched_clock(void)
 {
 	return get_cycles64();
 }
@@ -97,6 +103,9 @@ static int __init riscv_timer_init_dt(struct device_node *n)
 
 	cs = per_cpu_ptr(&riscv_clocksource, cpuid);
 	clocksource_register_hz(cs, riscv_timebase);
+
+	sched_clock_register(riscv_sched_clock,
+			BITS_PER_LONG, riscv_timebase);
 
 	error = cpuhp_setup_state(CPUHP_AP_RISCV_TIMER_STARTING,
 			 "clockevents/riscv/timer:starting",
