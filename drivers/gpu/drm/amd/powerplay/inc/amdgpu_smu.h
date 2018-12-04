7bc7e8892b58ff323f20fd437ea29aa894498805
Huang Rui <ray.huang@amd.com>
2018-12-04 21:34:43 +0800

    drm/amd/powerplay: add interface to parse pptable (v2)
    This patch adds interface to parse pptable for smu.
    It's to parse pptable format and fill PPTable_t smc_pptable to smu_table_context
    structure. And read the smc_dpm_table from vbios, then fill it into smc_pptable.
    
    v2: add detailed info to describe this function
    
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/inc/amdgpu_smu.h
+++ drivers/gpu/drm/amd/powerplay/inc/amdgpu_smu.h
@@ -43,6 +43,7 @@ struct smu_funcs
 	int (*read_pptable_from_vbios)(struct smu_context *smu);
 	int (*get_vbios_bootup_values)(struct smu_context *smu);
 	int (*check_pptable)(struct smu_context *smu);
+	int (*parse_pptable)(struct smu_context *smu);
 };
 
 #define smu_init_microcode(smu) \
@@ -61,6 +62,9 @@ struct smu_funcs
 	((smu)->funcs->get_vbios_bootup_values ? (smu)->funcs->get_vbios_bootup_values((smu)) : 0)
 #define smu_check_pptable(smu) \
 	((smu)->funcs->check_pptable ? (smu)->funcs->check_pptable((smu)) : 0)
+#define smu_parse_pptable(smu) \
+	((smu)->funcs->parse_pptable ? (smu)->funcs->parse_pptable((smu)) : 0)
+
 
 extern const struct amd_ip_funcs smu_ip_funcs;
 
