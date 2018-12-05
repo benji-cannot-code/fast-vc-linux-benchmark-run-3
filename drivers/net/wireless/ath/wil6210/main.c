0956bb1219235075c3a217239a0ee55bb303f790
YueHaibing <yuehaibing@huawei.com>
2018-12-05 07:59:03 +0000

    wil6210: remove set but not used variable 'wdev'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/net/wireless/ath/wil6210/main.c: In function '_wil6210_disconnect':
    drivers/net/wireless/ath/wil6210/main.c:407:23: warning:
     variable 'wdev' set but not used [-Wunused-but-set-variable]
    
    It never used since commit ("e1b43407c034 wil6210: refactor disconnect flow")
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Maya Erez <merez@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/wil6210/main.c
+++ drivers/net/wireless/ath/wil6210/main.c
@@ -405,7 +405,6 @@ static void _wil6210_disconnect(struct wil6210_vif *vif, const u8 *bssid,
 {
 	struct wil6210_priv *wil;
 	struct net_device *ndev;
-	struct wireless_dev *wdev;
 	int cid = -ENOENT;
 
 	if (unlikely(!vif))
@@ -413,7 +412,6 @@ static void _wil6210_disconnect(struct wil6210_vif *vif, const u8 *bssid,
 
 	wil = vif_to_wil(vif);
 	ndev = vif_to_ndev(vif);
-	wdev = vif_to_wdev(vif);
 
 	might_sleep();
 	wil_info(wil, "disconnect bssid=%pM, reason=%d\n", bssid, reason_code);
