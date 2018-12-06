32ddd3e063a37ef5928d6ddbfcc4d68e04d80897
Steve Capper <steve.capper@arm.com>
2018-12-06 22:50:39 +0000

    arm64: mm: Offset TTBR1 to allow 52-bit PTRS_PER_PGD
    Enabling 52-bit VAs on arm64 requires that the PGD table expands from 64
    entries (for the 48-bit case) to 1024 entries. This quantity,
    PTRS_PER_PGD is used as follows to compute which PGD entry corresponds
    to a given virtual address, addr:
    
    pgd_index(addr) -> (addr >> PGDIR_SHIFT) & (PTRS_PER_PGD - 1)
    
    Userspace addresses are prefixed by 0's, so for a 48-bit userspace
    address, uva, the following is true:
    (uva >> PGDIR_SHIFT) & (1024 - 1) == (uva >> PGDIR_SHIFT) & (64 - 1)
    
    In other words, a 48-bit userspace address will have the same pgd_index
    when using PTRS_PER_PGD = 64 and 1024.
    
    Kernel addresses are prefixed by 1's so, given a 48-bit kernel address,
    kva, we have the following inequality:
    (kva >> PGDIR_SHIFT) & (1024 - 1) != (kva >> PGDIR_SHIFT) & (64 - 1)
    
    In other words a 48-bit kernel virtual address will have a different
    pgd_index when using PTRS_PER_PGD = 64 and 1024.
    
    If, however, we note that:
    kva = 0xFFFF << 48 + lower (where lower[63:48] == 0b)
    and, PGDIR_SHIFT = 42 (as we are dealing with 64KB PAGE_SIZE)
    
    We can consider:
    (kva >> PGDIR_SHIFT) & (1024 - 1) - (kva >> PGDIR_SHIFT) & (64 - 1)
     = (0xFFFF << 6) & 0x3FF - (0xFFFF << 6) & 0x3F	// "lower" cancels out
     = 0x3C0
    
    In other words, one can switch PTRS_PER_PGD to the 52-bit value globally
    provided that they increment ttbr1_el1 by 0x3C0 * 8 = 0x1E00 bytes when
    running with 48-bit kernel VAs (TCR_EL1.T1SZ = 16).
    
    For kernel configuration where 52-bit userspace VAs are possible, this
    patch offsets ttbr1_el1 and sets PTRS_PER_PGD corresponding to the
    52-bit value.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Suggested-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Steve Capper <steve.capper@arm.com>
    [will: added comment to TTBR1_BADDR_4852_OFFSET calculation]
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/include/asm/pgtable-hwdef.h
+++ arch/arm64/include/asm/pgtable-hwdef.h
@@ -81,7 +81,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PGDIR_SHIFT		ARM64_HW_PGTABLE_LEVEL_SHIFT(4 - CONFIG_PGTABLE_LEVELS)
 #define PGDIR_SIZE		(_AC(1, UL) << PGDIR_SHIFT)
 #define PGDIR_MASK		(~(PGDIR_SIZE-1))
+#ifdef CONFIG_ARM64_52BIT_VA
+#define PTRS_PER_PGD		(1 << (52 - PGDIR_SHIFT))
+#else
 #define PTRS_PER_PGD		(1 << (VA_BITS - PGDIR_SHIFT))
+#endif
 
 /*
  * Section address mask and size definitions.
@@ -305,6 +309,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * TTBR_ELx[1] is RES0 in this configuration.
  */
 #define TTBR_BADDR_MASK_52	(((UL(1) << 46) - 1) << 2)
+#endif
+
+#ifdef CONFIG_ARM64_52BIT_VA
+/* Must be at least 64-byte aligned to prevent corruption of the TTBR */
+#define TTBR1_BADDR_4852_OFFSET	(((UL(1) << (52 - PGDIR_SHIFT)) - \
+				 (UL(1) << (48 - PGDIR_SHIFT))) * 8)
 #endif
 
 #endif
