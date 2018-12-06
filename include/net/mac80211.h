b18f01fb55bd9eed7da056ceb9b75aae154bfb9b
Randy Dunlap <rdunlap@infradead.org>
2018-12-06 12:58:30 -0800

    wireless: FTM: fix kernel-doc "cannot understand" warnings
    Fix kernel-doc warnings in FTM due to missing "struct" keyword.
    
    Fixes 109 warnings from <net/cfg80211.h>:
    ../include/net/cfg80211.h:2838: warning: cannot understand function prototype: 'struct cfg80211_ftm_responder_stats '
    
    and fixes 88 warnings from <net/mac80211.h>:
    ../include/net/mac80211.h:477: warning: cannot understand function prototype: 'struct ieee80211_ftm_responder_params '
    
    Fixes: 81e54d08d9d8 ("cfg80211: support FTM responder configuration/statistics")
    Fixes: bc847970f432 ("mac80211: support FTM responder configuration/statistics")
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Pradeep Kumar Chitrapu <pradeepc@codeaurora.org>
    Cc: Johannes Berg <johannes.berg@intel.com>
    Cc: David Spinadel <david.spinadel@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- include/net/mac80211.h
+++ include/net/mac80211.h
@@ -468,7 +468,7 @@ struct ieee80211_mu_group_data {
 };
 
 /**
- * ieee80211_ftm_responder_params - FTM responder parameters
+ * struct ieee80211_ftm_responder_params - FTM responder parameters
  *
  * @lci: LCI subelement content
  * @civicloc: CIVIC location subelement content
