03a82afda813d5f26ff98ba93f61828e5bc5b47c
abdoulaye berthe <abdoulaye.berthe@amd.com>
2018-12-07 16:12:09 -0500

    drm/amd/display: fix CTS 4.2.2.8
    [Why]
    1-Test equipment does not reset test automation flag after completing
    current test causing the next test to fail.
    2.When test request is empty, we shouldn't ack the test.
    
    [How]
    1-Driver should always clear test equipment automation test request
    when request is completed.
    2-Driver should clear test equipement test automation if driver does
    not complete the request.
    
    Signed-off-by: abdoulaye berthe <abdoulaye.berthe@amd.com>
    Reviewed-by: Wenjing Liu <Wenjing.Liu@amd.com>
    Acked-by: Leo Li <sunpeng.li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
@@ -2003,11 +2003,7 @@ static void handle_automated_test(struct dc_link *link)
 		dp_test_send_phy_test_pattern(link);
 		test_response.bits.ACK = 1;
 	}
-	if (!test_request.raw)
-		/* no requests, revert all test signals
-		 * TODO: revert all test signals
-		 */
-		test_response.bits.ACK = 1;
+
 	/* send request acknowledgment */
 	if (test_response.bits.ACK)
 		core_link_write_dpcd(
