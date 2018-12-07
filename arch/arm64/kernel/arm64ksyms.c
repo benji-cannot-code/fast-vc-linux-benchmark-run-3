5eec0d9041d0e010f291301b516343e8f2dc3900
Mark Rutland <mark.rutland@arm.com>
2018-12-07 18:08:20 +0000

    arm64: uaccess: use asm EXPORT_SYMBOL()
    For a while now it's been possible to use EXPORT_SYMBOL() in assembly
    files, which allows us to place exports immediately after assembly
    functions, as we do for C functions.
    
    As a step towards removing arm64ksyms.c, let's move the uaccess exports
    to the assembly files the functions are defined in.  As we have to
    include <asm/assembler.h>, the existing includes are fixed to follow the
    usual ordering conventions.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -25,17 +25,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/in6.h>
 #include <linux/syscalls.h>
-#include <linux/uaccess.h>
 #include <linux/io.h>
 #include <linux/kprobes.h>
 
 #include <asm/checksum.h>
-
-	/* user mem (segment) */
-EXPORT_SYMBOL(__arch_copy_from_user);
-EXPORT_SYMBOL(__arch_copy_to_user);
-EXPORT_SYMBOL(__arch_clear_user);
-EXPORT_SYMBOL(__arch_copy_in_user);
 
 	/* string / mem functions */
 #ifndef CONFIG_KASAN
