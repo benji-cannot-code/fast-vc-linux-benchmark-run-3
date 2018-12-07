428686c0875ef9a346e281316082c08c005b8cee
Maxime Ripard <maxime.ripard@bootlin.com>
2018-12-07 14:55:30 +0100

    phy: Add MIPI D-PHY configuration options
    Now that we have some infrastructure for it, allow the MIPI D-PHY phy's to
    be configured through the generic functions through a custom structure
    added to the generic union.
    
    The parameters added here are the ones defined in the MIPI D-PHY spec, plus
    the number of lanes in use. The current set of parameters should cover all
    the potential users.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- include/linux/phy/phy.h
+++ include/linux/phy/phy.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
 
+#include <linux/phy/phy-mipi-dphy.h>
+
 struct phy;
 
 enum phy_mode {
@@ -45,8 +47,12 @@ enum phy_mode {
 
 /**
  * union phy_configure_opts - Opaque generic phy configuration
+ *
+ * @mipi_dphy:	Configuration set applicable for phys supporting
+ *		the MIPI_DPHY phy mode.
  */
 union phy_configure_opts {
+	struct phy_configure_opts_mipi_dphy	mipi_dphy;
 };
 
 /**
