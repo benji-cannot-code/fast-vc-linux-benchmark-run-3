55213161b516fc5a1b8a35083a273072aa4a23dd
Mark Rutland <mark.rutland@arm.com>
2018-12-07 18:08:19 +0000

    arm64: page: use asm EXPORT_SYMBOL()
    For a while now it's been possible to use EXPORT_SYMBOL() in assembly
    files, which allows us to place exports immediately after assembly
    functions, as we do for C functions.
    
    As a step towards removing arm64ksyms.c, let's move the copy_page and
    clear_page exports to the assembly files the functions are defined in.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/checksum.h>
 
-EXPORT_SYMBOL(copy_page);
-EXPORT_SYMBOL(clear_page);
-
 	/* user mem (segment) */
 EXPORT_SYMBOL(__arch_copy_from_user);
 EXPORT_SYMBOL(__arch_copy_to_user);
