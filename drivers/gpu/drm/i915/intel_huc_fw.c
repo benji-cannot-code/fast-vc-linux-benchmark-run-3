b2a49d96850e66c528668ea13f118b6106db974e
Anusha Srivatsa <anusha.srivatsa@intel.com>
2018-12-07 10:28:39 -0800

    drm/i915/huc: Update the HuC version for BXT
    We have an update for HuC for BXT.
    Load the latest version.
    
    v2: Change the subject.
    
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181207182840.9292-2-anusha.srivatsa@intel.com

--- drivers/gpu/drm/i915/intel_huc_fw.c
+++ drivers/gpu/drm/i915/intel_huc_fw.c
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #define BXT_HUC_FW_MAJOR 01
-#define BXT_HUC_FW_MINOR 07
-#define BXT_BLD_NUM 1398
+#define BXT_HUC_FW_MINOR 8
+#define BXT_BLD_NUM 2893
 
 #define SKL_HUC_FW_MAJOR 01
 #define SKL_HUC_FW_MINOR 07
