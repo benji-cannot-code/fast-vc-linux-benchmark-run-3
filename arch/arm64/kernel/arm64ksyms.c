9195ec5aec97bb543ea4d517aa7b59d704ad1db4
Mark Rutland <mark.rutland@arm.com>
2018-12-07 18:08:22 +0000

    arm64: frace: use asm EXPORT_SYMBOL()
    For a while now it's been possible to use EXPORT_SYMBOL() in assembly
    files, which allows us to place exports immediately after assembly
    functions, as we do for C functions.
    
    As a step towards removing arm64ksyms.c, let's move the ftrace exports
    to the assembly files the functions are defined in.
    
    There should be no functional change as a result of this patch.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -29,8 +29,3 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/checksum.h>
 
-#ifdef CONFIG_FUNCTION_TRACER
-EXPORT_SYMBOL(_mcount);
-NOKPROBE_SYMBOL(_mcount);
-#endif
-
