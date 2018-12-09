320602b465c280b32670b7c4dd36fe500afdb022
Paul Gortmaker <paul.gortmaker@windriver.com>
2018-12-09 15:36:32 -0500

    security: integrity: make evm_main explicitly non-modular
    The Makefile/Kconfig entry controlling compilation of this code is:
    
    obj-$(CONFIG_EVM) += evm.o
    evm-y := evm_main.o evm_crypto.o evm_secfs.o
    
    security/integrity/evm/Kconfig:config EVM
    security/integrity/evm/Kconfig: bool "EVM support"
    
    ...meaning that it currently is not being built as a module by anyone.
    
    Lets remove the couple traces of modular infrastructure use, so that
    when reading the driver there is no doubt it is builtin-only.
    
    We also delete the MODULE_LICENSE tag etc. since all that information
    is already contained at the top of the file in the comments.
    
    Cc: Mimi Zohar <zohar@linux.ibm.com>
    Cc: James Morris <jmorris@namei.org>
    Cc: "Serge E. Hallyn" <serge@hallyn.com>
    Cc: linux-ima-devel@lists.sourceforge.net
    Cc: linux-security-module@vger.kernel.org
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>

--- security/integrity/evm/evm_main.c
+++ security/integrity/evm/evm_main.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/crypto.h>
 #include <linux/audit.h>
 #include <linux/xattr.h>
@@ -593,6 +593,3 @@ error:
 }
 
 late_initcall(init_evm);
-
-MODULE_DESCRIPTION("Extended Verification Module");
-MODULE_LICENSE("GPL");
