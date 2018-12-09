ac87452bce464e4674d347aa027fe39feefb28e1
NeilBrown <neil@brown.name>
2018-12-09 16:20:33 +1100

    staging: mt7621-mmc: add missing header.
    <linux/interrupt.h> is sometimes included by other include files,
    and sometimes not, depending on config, particularly CONFIG_HIGHMEM.
    
    So include it explicitly rather than relying on implicit inclusion.
    
    Signed-off-by: NeilBrown <neil@brown.name>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/sd.c
+++ drivers/staging/mt7621-mmc/sd.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 #include <linux/spinlock.h>
 #include <linux/platform_device.h>
+#include <linux/interrupt.h>
 
 #include <linux/mmc/host.h>
 #include <linux/mmc/mmc.h>
