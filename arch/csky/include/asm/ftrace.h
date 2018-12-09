cdff85b306712c5c64b440228da1c5425a4b8f38
Guo Ren <ren_guo@c-sky.com>
2018-12-09 14:29:59 +0800

    csky: basic ftrace supported
    When gcc with -pg, it'll add _mcount stub in every function. We need
    implement the _mcount in kernel and ftrace depends on stackstrace.
    
    To do: call-graph, dynamic ftrace
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

---
+++ arch/csky/include/asm/ftrace.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+// Copyright (C) 2018 Hangzhou C-SKY Microsystems co.,ltd.
+
+#ifndef __ASM_CSKY_FTRACE_H
+#define __ASM_CSKY_FTRACE_H
+
+extern void _mcount(unsigned long from_pc);
+
+#endif /* __ASM_CSKY_FTRACE_H */
