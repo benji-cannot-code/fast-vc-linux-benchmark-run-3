fd3294346bddb2e553a4af4cd8c221d1f186635d
Maran Wilson <maran.wilson@oracle.com>
2018-12-10 11:08:45 -0800

    xen/pvh: Move Xen specific PVH VM initialization out of common file
    We need to refactor PVH entry code so that support for other hypervisors
    like Qemu/KVM can be added more easily.
    
    This patch moves the small block of code used for initializing Xen PVH
    virtual machines into the Xen specific file. This initialization is not
    going to be needed for Qemu/KVM guests. Moving it out of the common file
    is going to allow us to compile kernels in the future without CONFIG_XEN
    that are still capable of being booted as a Qemu/KVM guest via the PVH
    entry point.
    
    Signed-off-by: Maran Wilson <maran.wilson@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/xen/enlighten_pvh.c
+++ arch/x86/xen/enlighten_pvh.c
@@ -1,6 +1,13 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
-#include <linux/types.h>
+#include <linux/acpi.h>
+
+#include <asm/io_apic.h>
+#include <asm/hypervisor.h>
+
+#include <xen/xen.h>
+#include <asm/xen/interface.h>
+#include <asm/xen/hypercall.h>
 
 /*
  * PVH variables.
@@ -9,3 +16,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * after startup_{32|64} is invoked, which will clear the .bss segment.
  */
 bool xen_pvh __attribute__((section(".data"))) = 0;
+
+void __init xen_pvh_init(void)
+{
+	u32 msr;
+	u64 pfn;
+
+	xen_pvh = 1;
+	xen_start_flags = pvh_start_info.flags;
+
+	msr = cpuid_ebx(xen_cpuid_base() + 2);
+	pfn = __pa(hypercall_page);
+	wrmsr_safe(msr, (u32)pfn, (u32)(pfn >> 32));
+}
