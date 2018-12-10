bbd869629f382d10987c6d092bf0a73970e534dc
Ioana Ciornei <ioana.ciornei@nxp.com>
2018-12-10 16:50:19 +0000

    soc: fsl: dpio: add a device_link at dpaa2_io_service_register
    Automatically add a device link between the actual device requesting the
    dpaa2_io_service_register and the underlying dpaa2_io used. This link
    will ensure that when a DPIO device, which is indirectly used by other
    devices, is unbound any consumer devices will be also unbound from their
    drivers.
    
    For example, any DPNI, bound to the dpaa2-eth driver, which is using
    DPIO devices will be unbound before its supplier device.
    
    Also, add a new parameter to the dpaa2_io_service_[de]register functions
    to specify the requesting device (ie the consumer).
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>
    Reviewed-by: Horia Geanta <horia.geanta@nxp.com>
    Reviewed-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/soc/fsl/dpio/dpio-service.c
+++ drivers/soc/fsl/dpio/dpio-service.c
@@ -238,6 +238,7 @@ EXPORT_SYMBOL(dpaa2_io_get_cpu);
  *                               notifications on the given DPIO service.
  * @d:   the given DPIO service.
  * @ctx: the notification context.
+ * @dev: the device that requests the register
  *
  * The caller should make the MC command to attach a DPAA2 object to
  * a DPIO after this function completes successfully.  In that way:
@@ -252,13 +253,19 @@ EXPORT_SYMBOL(dpaa2_io_get_cpu);
  * Return 0 for success, or -ENODEV for failure.
  */
 int dpaa2_io_service_register(struct dpaa2_io *d,
-			      struct dpaa2_io_notification_ctx *ctx)
+			      struct dpaa2_io_notification_ctx *ctx,
+			      struct device *dev)
 {
+	struct device_link *link;
 	unsigned long irqflags;
 
 	d = service_select_by_cpu(d, ctx->desired_cpu);
 	if (!d)
 		return -ENODEV;
+
+	link = device_link_add(dev, d->dev, DL_FLAG_AUTOREMOVE_CONSUMER);
+	if (!link)
+		return -EINVAL;
 
 	ctx->dpio_id = d->dpio_desc.dpio_id;
 	ctx->qman64 = (u64)(uintptr_t)ctx;
@@ -280,12 +287,14 @@ EXPORT_SYMBOL_GPL(dpaa2_io_service_register);
  * dpaa2_io_service_deregister - The opposite of 'register'.
  * @service: the given DPIO service.
  * @ctx: the notification context.
+ * @dev: the device that requests to be deregistered
  *
  * This function should be called only after sending the MC command to
  * to detach the notification-producing device from the DPIO.
  */
 void dpaa2_io_service_deregister(struct dpaa2_io *service,
-				 struct dpaa2_io_notification_ctx *ctx)
+				 struct dpaa2_io_notification_ctx *ctx,
+				 struct device *dev)
 {
 	struct dpaa2_io *d = ctx->dpio_private;
 	unsigned long irqflags;
@@ -296,6 +305,9 @@ void dpaa2_io_service_deregister(struct dpaa2_io *service,
 	spin_lock_irqsave(&d->lock_notifications, irqflags);
 	list_del(&ctx->node);
 	spin_unlock_irqrestore(&d->lock_notifications, irqflags);
+
+	if (dev)
+		device_link_remove(dev, d->dev);
 }
 EXPORT_SYMBOL_GPL(dpaa2_io_service_deregister);
 
