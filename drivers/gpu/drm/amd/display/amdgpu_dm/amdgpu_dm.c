be762ca945c84c5913080196487a28c5aa84f585
David Francis <David.Francis@amd.com>
2018-12-10 10:18:21 -0500

    drm/amd/display: Remove unused parameter plane_states
    [Why]
    The function dc_commit_updates_for_stream had a parameter called
    plane_states.  It was never used.  It was getting in the way
    of some cleanup work
    
    [How]
    Remove it
    
    Signed-off-by: David Francis <David.Francis@amd.com>
    Reviewed-by: Leo Li <sunpeng.li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -4674,7 +4674,6 @@ static void amdgpu_dm_do_flip(struct drm_crtc *crtc,
 					     1,
 					     acrtc_state->stream,
 					     &stream_update,
-					     &surface_updates->surface,
 					     state);
 	mutex_unlock(&adev->dm.dc_lock);
 
@@ -4773,7 +4772,7 @@ static bool commit_planes_to_stream(
 			dc,
 			updates,
 			new_plane_count,
-			dc_stream, stream_update, plane_states, state);
+			dc_stream, stream_update, state);
 	mutex_unlock(&dm->dc_lock);
 
 	kfree(flip_addr);
