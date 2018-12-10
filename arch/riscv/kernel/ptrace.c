e03614fd8f20cd07b130ad67ef98415c7921d07b
David Abdurachmanov <david.abdurachmanov@gmail.com>
2018-12-10 21:43:55 +0100

    riscv: define CREATE_TRACE_POINTS in ptrace.c
    Define CREATE_TRACE_POINTS in order to create functions and structures
    for the trace events. This is needed if HAVE_SYSCALL_TRACEPOINTS and
    CONFIG_FTRACE_SYSCALLS are enabled, otherwise we get linking errors:
    
    [..]
      MODPOST vmlinux.o
    kernel/trace/trace_syscalls.o: In function `.L0 ':
    trace_syscalls.c:(.text+0x1152): undefined reference to `__tracepoint_sys_enter'
    trace_syscalls.c:(.text+0x126c): undefined reference to `__tracepoint_sys_enter'
    trace_syscalls.c:(.text+0x1328): undefined reference to `__tracepoint_sys_enter'
    trace_syscalls.c:(.text+0x14aa): undefined reference to `__tracepoint_sys_enter'
    trace_syscalls.c:(.text+0x1684): undefined reference to `__tracepoint_sys_exit'
    trace_syscalls.c:(.text+0x17a0): undefined reference to `__tracepoint_sys_exit'
    trace_syscalls.c:(.text+0x185c): undefined reference to `__tracepoint_sys_exit'
    trace_syscalls.c:(.text+0x19de): undefined reference to `__tracepoint_sys_exit'
    arch/riscv/kernel/ptrace.o: In function `.L0 ':
    ptrace.c:(.text+0x4dc): undefined reference to `__tracepoint_sys_enter'
    ptrace.c:(.text+0x632): undefined reference to `__tracepoint_sys_exit'
    make: *** [Makefile:1036: vmlinux] Error 1
    
    Signed-off-by: David Abdurachmanov <david.abdurachmanov@gmail.com>
    Fixes: b78002b395b4 ("riscv: add HAVE_SYSCALL_TRACEPOINTS to Kconfig")
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- arch/riscv/kernel/ptrace.c
+++ arch/riscv/kernel/ptrace.c
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/sched/task_stack.h>
 #include <linux/tracehook.h>
+
+#define CREATE_TRACE_POINTS
 #include <trace/events/syscalls.h>
 
 enum riscv_regset {
