feccb011e0d29ab48c6d7f907cc7c4625a651f0b
Huang Rui <ray.huang@amd.com>
2018-12-11 15:42:12 +0800

    drm/amd/powerplay: add new ppsmc header for smu11 (v2)
    This header will be used for smc message handling.
    
    v2: add a license to the top of the header file (Alex)
    
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Reviewed-by: Kevin Wang <kevin1.wang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/smu_v11_0.c
+++ drivers/gpu/drm/amd/powerplay/smu_v11_0.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "amdgpu.h"
 #include "amdgpu_smu.h"
 #include "smu_v11_0.h"
+#include "smu_v11_0_ppsmc.h"
 
 MODULE_FIRMWARE("amdgpu/vega20_smc.bin");
 
