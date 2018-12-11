66750f043b819875c9c308c5e08e1d1cb9149cd8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-12-11 19:17:35 +0900

    ALSA: fireface: add support for Fireface 800 with MIDI functionality only
    Fireface 800 is a flagship model of RME GmbH for audio and music units
    on IEEE 1394 bus, shipped 2004. This model consists of four chips:
     - TI TSB81BA3D for physical layer on cable environment of EEE 1394 bus
     - TI TSB82AA2 for link layer for 1394 OHCI bus bridge to PCI bus
     - Xilinx Spartan-3 FPGA XC3S400
     - Xilinx High-Performance CPLD XC9572XL
    
    This commit adds support Fireface 800. In this time, the support is
    restricted to its MIDI functionality, thus this commit adds some
    condition statements to avoid touching streaming functionality.
    
    Unlike Fireface 400, Fireface 800 has no functionality to suppress
    asynchronous transactions for MIDI messages except for unregister of
    listen address in controller side, thus the feature is available as is.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

---
+++ sound/firewire/fireface/ff-protocol-ff800.c
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * ff-protocol-ff800.c - a part of driver for RME Fireface series
+ *
+ * Copyright (c) 2018 Takashi Sakamoto
+ *
+ * Licensed under the terms of the GNU General Public License, version 2.
+ */
+
+#include "ff.h"
+
+static void ff800_handle_midi_msg(struct snd_ff *ff, __le32 *buf, size_t length)
+{
+	int i;
+
+	for (i = 0; i < length / 4; i++) {
+		u8 byte = le32_to_cpu(buf[i]) & 0xff;
+		struct snd_rawmidi_substream *substream;
+
+		substream = READ_ONCE(ff->tx_midi_substreams[0]);
+		if (substream)
+			snd_rawmidi_receive(substream, &byte, 1);
+	}
+}
+
+const struct snd_ff_protocol snd_ff_protocol_ff800 = {
+	.handle_midi_msg	= ff800_handle_midi_msg,
+};
