1cc67b4591c606e7ff5e3ae964f2ddd5b3cfd004
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-12-11 19:17:28 +0900

    ALSA: fireface: share some registers for status of clock synchronization
    As long as investigating packet dumps from Fireface 400/800, status
    registers for clock synchronization is common.
    
    This commit moves some macros for them to header file.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/fireface/ff.h
+++ sound/firewire/fireface/ff.h
@@ -38,6 +38,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SND_FF_IN_MIDI_PORTS		2
 #define SND_FF_OUT_MIDI_PORTS		2
 
+#define SND_FF_REG_SYNC_STATUS		0x0000801c0000ull
+/* For block wriet request. */
+#define SND_FF_REG_FETCH_PCM_FRAMES	0x0000801c0000ull
+#define SND_FF_REG_CLOCK_CONFIG		0x0000801c0004ull
+
 struct snd_ff_protocol;
 struct snd_ff_spec {
 	const char *const name;
