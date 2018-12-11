9665e015d1b3ad3ae3d9619b60818325ee3ce9ab
Ben Skeggs <bskeggs@redhat.com>
2018-12-11 14:50:02 +1000

    drm/nouveau/fault: store get/put pri address in nvkm_fault_buffer
    Will allow more shared fault buffer handling code between Pascal/Volta.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/fault/priv.h
+++ drivers/gpu/drm/nouveau/nvkm/subdev/fault/priv.h
@@ -13,6 +13,8 @@ struct nvkm_fault_buffer {
 	struct nvkm_fault *fault;
 	int id;
 	int entries;
+	u32 get;
+	u32 put;
 	struct nvkm_memory *mem;
 	u64 addr;
 };
@@ -28,7 +30,7 @@ struct nvkm_fault_func {
 	struct {
 		int nr;
 		u32 entry_size;
-		u32 (*entries)(struct nvkm_fault_buffer *);
+		void (*info)(struct nvkm_fault_buffer *);
 		void (*init)(struct nvkm_fault_buffer *);
 		void (*fini)(struct nvkm_fault_buffer *);
 	} buffer;
