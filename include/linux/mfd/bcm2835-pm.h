1a95af5c884c5a306857065e7235815d26e83607
Eric Anholt <eric@anholt.net>
2018-12-12 15:51:48 -0800

    soc: bcm: bcm2835-pm: Add support for power domains under a new binding.
    This provides a free software alternative to raspberrypi-power.c's
    firmware calls to manage power domains.  It also exposes a reset line,
    where previously the vc4 driver had to try to force power off the
    domain in order to trigger a reset.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Acked-by: Rob Herring <robh@kernel.org>
    Acked-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- include/linux/mfd/bcm2835-pm.h
+++ include/linux/mfd/bcm2835-pm.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct bcm2835_pm {
 	struct device *dev;
 	void __iomem *base;
+	void __iomem *asb;
 };
 
 #endif /* BCM2835_MFD_PM_H */
