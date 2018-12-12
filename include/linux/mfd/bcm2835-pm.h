1b3c8d423460a3acb92f6849051c5c7419763712
Eric Anholt <eric@anholt.net>
2018-12-12 15:51:47 -0800

    bcm2835-pm: Move bcm2835-watchdog's DT probe to an MFD.
    The PM block that the wdt driver was binding to actually has multiple
    features we want to expose (power domains, reset, watchdog).  Move the
    DT attachment to a MFD driver and make WDT probe against MFD.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Acked-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

---
+++ include/linux/mfd/bcm2835-pm.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef BCM2835_MFD_PM_H
+#define BCM2835_MFD_PM_H
+
+#include <linux/regmap.h>
+
+struct bcm2835_pm {
+	struct device *dev;
+	void __iomem *base;
+};
+
+#endif /* BCM2835_MFD_PM_H */
