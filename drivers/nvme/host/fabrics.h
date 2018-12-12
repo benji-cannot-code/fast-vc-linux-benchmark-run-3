f50b06e097da621b0b9e6e5dcf24a87bbccf9ac0
Sagi Grimberg <sagi@grimberg.me>
2018-12-11 23:38:56 -0800

    nvme-fabrics: allow user to set nr_write_queues for separate queue maps
    This argument will specify how many I/O queues will be connected in
    create_ctrl in addition to nr_io_queues. With this configuration, I/O
    that carries payload from the host to the target, will use the default
    hctx queue map, and I/O that involves target to host transfers will use
    the read hctx queue map.
    
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/host/fabrics.h
+++ drivers/nvme/host/fabrics.h
@@ -62,6 +62,7 @@ enum {
 	NVMF_OPT_DISABLE_SQFLOW = 1 << 14,
 	NVMF_OPT_HDR_DIGEST	= 1 << 15,
 	NVMF_OPT_DATA_DIGEST	= 1 << 16,
+	NVMF_OPT_NR_WRITE_QUEUES = 1 << 17,
 };
 
 /**
@@ -92,6 +93,7 @@ enum {
  * @disable_sqflow: disable controller sq flow control
  * @hdr_digest: generate/verify header digest (TCP)
  * @data_digest: generate/verify data digest (TCP)
+ * @nr_write_queues: number of queues for write I/O
  */
 struct nvmf_ctrl_options {
 	unsigned		mask;
@@ -111,6 +113,7 @@ struct nvmf_ctrl_options {
 	bool			disable_sqflow;
 	bool			hdr_digest;
 	bool			data_digest;
+	unsigned int		nr_write_queues;
 };
 
 /*
