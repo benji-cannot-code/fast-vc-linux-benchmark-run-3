72681b6e26f2780435f40845d8e7d55a804ff2eb
Dmitry Osipenko <digetx@gmail.com>
2018-12-12 23:38:55 +0300

    memory: tegra: Use relaxed versions of readl/writel
    There is no need for inserting of memory barriers to access registers of
    Memory Controller. Hence use the relaxed versions of the accessors.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/memory/tegra/mc.h
+++ drivers/memory/tegra/mc.h
@@ -27,13 +27,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline u32 mc_readl(struct tegra_mc *mc, unsigned long offset)
 {
-	return readl(mc->regs + offset);
+	return readl_relaxed(mc->regs + offset);
 }
 
 static inline void mc_writel(struct tegra_mc *mc, u32 value,
 			     unsigned long offset)
 {
-	writel(value, mc->regs + offset);
+	writel_relaxed(value, mc->regs + offset);
 }
 
 extern const struct tegra_mc_reset_ops terga_mc_reset_ops_common;
