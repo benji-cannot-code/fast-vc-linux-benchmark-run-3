4e7fdc8edd69666fbd98e4056a0f2b8dc2769d8b
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2018-12-12 18:34:00 +0100

    mfd / platform: cros_ec: Move device sysfs attributes to its own driver
    The entire way how cros debugfs attibutes are created is broken.
    cros_ec_sysfs should be its own driver and its attributes should be
    associated with the sysfs driver not the mfd driver.
    
    The patch also adds the sysfs documentation.
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/cros_ec_dev.c
+++ drivers/mfd/cros_ec_dev.c
@@ -35,15 +35,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CROS_MAX_DEV 128
 static int ec_major;
 
-static const struct attribute_group *cros_ec_groups[] = {
-	&cros_ec_attr_group,
-	NULL,
-};
-
 static struct class cros_class = {
 	.owner          = THIS_MODULE,
 	.name           = "chromeos",
-	.dev_groups     = cros_ec_groups,
 };
 
 /* Basic communication */
@@ -397,6 +391,7 @@ static const struct mfd_cell cros_usbpd_charger_cells[] = {
 static const struct mfd_cell cros_ec_platform_cells[] = {
 	{ .name = "cros-ec-debugfs" },
 	{ .name = "cros-ec-lightbar" },
+	{ .name = "cros-ec-sysfs" },
 	{ .name = "cros-ec-vbc" },
 };
 
