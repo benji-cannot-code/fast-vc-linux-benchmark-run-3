8aed5b409bd8b52bc21ced83a541431378e78e10
Dmitry Osipenko <digetx@gmail.com>
2018-12-12 23:39:04 +0300

    iommu/tegra: gart: Prepend error/debug messages with "gart:"
    GART became a part of Memory Controller, hence now the drivers device
    is Memory Controller and not GART. As a result all printed messages are
    prepended with the "tegra-mc 7000f000.memory-controller:", so let's
    prepend GART's messages with "gart:" in order to differentiate them
    from the MC.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/tegra-gart.c
+++ drivers/iommu/tegra-gart.c
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#define dev_fmt(fmt)	"gart: " fmt
+
 #include <linux/io.h>
 #include <linux/iommu.h>
 #include <linux/list.h>
