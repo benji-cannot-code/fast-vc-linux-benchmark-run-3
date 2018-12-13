c029284b4b51f4242d31c218b7c24abc51170c22
Aisheng Dong <aisheng.dong@nxp.com>
2018-12-13 15:43:01 +0000

    clk: imx: add lpcg clock support
    The Low-Power Clock Gate (LPCG) modules contain a local programming
    model to control the clock gates for the peripherals. An LPCG module
    is used to locally gate the clocks for the associated peripheral.
    And they're bedind the SCU clock.
    
    Cc: Stephen Boyd <sboyd@kernel.org>
    Cc: Shawn Guo <shawnguo@kernel.org>
    Cc: Sascha Hauer <kernel@pengutronix.de>
    Cc: Fabio Estevam <fabio.estevam@nxp.com>
    Cc: Michael Turquette <mturquette@baylibre.com>
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/imx/clk-scu.h
+++ drivers/clk/imx/clk-scu.h
@@ -13,4 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int imx_clk_scu_init(void);
 struct clk_hw *imx_clk_scu(const char *name, u32 rsrc_id, u8 clk_type);
 
+struct clk_hw *imx_clk_lpcg_scu(const char *name, const char *parent_name,
+				unsigned long flags, void __iomem *reg,
+				u8 bit_idx, bool hw_gate);
 #endif
