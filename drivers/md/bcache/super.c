724527751213c6ec68db85598859cf670639bbaf
Coly Li <colyli@suse.de>
2018-12-13 22:53:54 +0800

    bcache: add MODULE_DESCRIPTION information
    This patch moves MODULE_AUTHOR and MODULE_LICENSE to end of super.c, and
    add MODULE_DESCRIPTION("Bcache: a Linux block layer cache").
    
    This is preparation for adding module parameters.
    
    Signed-off-by: Coly Li <colyli@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -26,9 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/reboot.h>
 #include <linux/sysfs.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Kent Overstreet <kent.overstreet@gmail.com>");
-
 static const char bcache_magic[] = {
 	0xc6, 0x85, 0x73, 0xf6, 0x4e, 0x1a, 0x45, 0xca,
 	0x82, 0x65, 0xf5, 0x7f, 0x48, 0xba, 0x6d, 0x81
@@ -2470,3 +2467,7 @@ err:
 
 module_exit(bcache_exit);
 module_init(bcache_init);
+
+MODULE_DESCRIPTION("Bcache: a Linux block layer cache");
+MODULE_AUTHOR("Kent Overstreet <kent.overstreet@gmail.com>");
+MODULE_LICENSE("GPL");
