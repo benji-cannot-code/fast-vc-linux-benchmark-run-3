a63b6d5d28e315b4cc757f28a71d9b2a979aec23
Shenghui Wang <shhuiw@foxmail.com>
2018-12-13 22:53:49 +0800

    bcache: update comment in sysfs.c
    We have struct cached_dev allocated by kzalloc in register_bcache(),
    which initializes all the fields of cached_dev with 0s. And commit
    ce4c3e19e520 ("bcache: Replace bch_read_string_list() by
    __sysfs_match_string()") has remove the string "default".
    
    Update the comment.
    
    Signed-off-by: Shenghui Wang <shhuiw@foxmail.com>
    Signed-off-by: Coly Li <colyli@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/md/bcache/sysfs.c
+++ drivers/md/bcache/sysfs.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sort.h>
 #include <linux/sched/clock.h>
 
-/* Default is -1; we skip past it for struct cached_dev's cache mode */
+/* Default is 0 ("writethrough") */
 static const char * const bch_cache_modes[] = {
 	"writethrough",
 	"writeback",
@@ -26,7 +26,7 @@ static const char * const bch_cache_modes[] = {
 	NULL
 };
 
-/* Default is -1; we skip past it for stop_when_cache_set_failed */
+/* Default is 0 ("auto") */
 static const char * const bch_stop_on_failure_modes[] = {
 	"auto",
 	"always",
