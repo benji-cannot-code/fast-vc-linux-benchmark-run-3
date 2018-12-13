881b91134c734d23f1421dea8cf5fe178c05dbaf
Dmitry V. Levin <ldv@altlinux.org>
2018-12-13 20:22:00 +0300

    elf-em.h: add EM_CSKY
    The uapi/linux/audit.h header is going to use EM_CSKY in order
    to define AUDIT_ARCH_CSKY which is needed to implement
    syscall_get_arch() which in turn is required to extend
    the generic ptrace API with PTRACE_GET_SYSCALL_INFO request.
    
    The value for EM_CSKY has been taken from arch/csky/include/asm/elf.h
    and confirmed by binutils:include/elf/common.h
    
    Cc: Guo Ren <guoren@kernel.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Elvira Khabirova <lineprinter@altlinux.org>
    Cc: Eugene Syromyatnikov <esyr@redhat.com>
    Signed-off-by: Dmitry V. Levin <ldv@altlinux.org>
    Signed-off-by: Guo Ren <guoren@kernel.org>

--- arch/csky/include/asm/elf.h
+++ arch/csky/include/asm/elf.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ptrace.h>
 #include <abi/regdef.h>
 
-#define ELF_ARCH 252
+#define ELF_ARCH EM_CSKY
 
 /* CSKY Relocations */
 #define R_CSKY_NONE               0
