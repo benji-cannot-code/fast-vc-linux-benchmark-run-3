2a571c35ddbfbe890870642525ef04e6381ec926
Petr Cvek <petrcvekcz@gmail.com>
2018-12-13 10:39:17 -0500

    media: i2c: ov9640: add space before return for better clarity
    Some returns were adjoined to unrelated code blocks. This patch adds
    a space inbetween.
    
    Signed-off-by: Petr Cvek <petrcvekcz@gmail.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/i2c/ov9640.c
+++ drivers/media/i2c/ov9640.c
@@ -287,6 +287,7 @@ static int ov9640_s_ctrl(struct v4l2_ctrl *ctrl)
 							OV9640_MVFP_H, 0);
 		return ov9640_reg_rmw(client, OV9640_MVFP, 0, OV9640_MVFP_H);
 	}
+
 	return -EINVAL;
 }
 
@@ -342,6 +343,7 @@ static int ov9640_s_power(struct v4l2_subdev *sd, int on)
 		usleep_range(1000, 2000);
 		gpiod_set_value(priv->gpio_power, 0);
 	}
+
 	return ret;
 }
 
@@ -546,6 +548,7 @@ static int ov9640_set_fmt(struct v4l2_subdev *sd,
 		return ov9640_s_fmt(sd, mf);
 
 	cfg->try_fmt = *mf;
+
 	return 0;
 }
 
@@ -557,6 +560,7 @@ static int ov9640_enum_mbus_code(struct v4l2_subdev *sd,
 		return -EINVAL;
 
 	code->code = ov9640_codes[code->index];
+
 	return 0;
 }
 
@@ -732,6 +736,7 @@ eprobe:
 	v4l2_clk_put(priv->clk);
 eclkget:
 	v4l2_ctrl_handler_free(&priv->hdl);
+
 	return ret;
 }
 
