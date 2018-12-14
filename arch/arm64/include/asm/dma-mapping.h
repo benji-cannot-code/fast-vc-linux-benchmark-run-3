dfa8ac5f5aebd94d3b3d27e5df1ec9f652e03092
Christoph Hellwig <hch@lst.de>
2018-12-14 15:18:08 +0100

    arm64: default to the direct mapping in get_arch_dma_ops
    Otherwise the direct mapping won't work at all given that a NULL
    dev->dma_ops causes a fallback.  Note that we already explicitly set
    dev->dma_ops to dma_dummy_ops for dma-incapable devices, so this
    fallback should not be needed anyway.
    
    Fixes: 356da6d0cd ("dma-mapping: bypass indirect calls for dma-direct")
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reported-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>

--- arch/arm64/include/asm/dma-mapping.h
+++ arch/arm64/include/asm/dma-mapping.h
@@ -27,11 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline const struct dma_map_ops *get_arch_dma_ops(struct bus_type *bus)
 {
-	/*
-	 * We expect no ISA devices, and all other DMA masters are expected to
-	 * have someone call arch_setup_dma_ops at device creation time.
-	 */
-	return &dma_dummy_ops;
+	return NULL;
 }
 
 void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
