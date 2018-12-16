a4274aa7a05576692f59f830d977d285244182be
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-12-16 17:32:32 +0900

    ALSA: fireface: add support for packet streaming on Fireface 800
    This commit adds a functionality to multiplex PCM frames into isochronous
    packets and demultiplex PCM frames from isochronous packets for ALSA PCM
    applications.
    
    Fireface 800 voluntarily maintains resources for tx isochronous
    communication.  It performs reservation of isochronous channel and
    allocation/update of bandwidth in some cases below:
     - at a first request to allocation after bus resets
     - at requests to allocation when further bandwidth is required
    
    When request is grant and the unit is prepared, read data from
    0x0000801c0008 represents isochronous channel for tx stream, then
    the unit can handle requests to start communication. If driver
    send the request without checking the register, the unit takes
    panic to continue bus resets. The unit starts transmission of
    tx packets after receiving several rx packets from driver.
    
    I note that the unit can process tx/rx packets and generate/record
    sound regardless of HOST LED.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/fireface/ff.c
+++ sound/firewire/fireface/ff.c
@@ -32,8 +32,7 @@ static void ff_card_free(struct snd_card *card)
 {
 	struct snd_ff *ff = card->private_data;
 
-	if (ff->spec->protocol->begin_session)
-		snd_ff_stream_destroy_duplex(ff);
+	snd_ff_stream_destroy_duplex(ff);
 	snd_ff_transaction_unregister(ff);
 }
 
@@ -58,11 +57,9 @@ static void do_registration(struct work_struct *work)
 
 	name_card(ff);
 
-	if (ff->spec->protocol->begin_session) {
-		err = snd_ff_stream_init_duplex(ff);
-		if (err < 0)
-			goto error;
-	}
+	err = snd_ff_stream_init_duplex(ff);
+	if (err < 0)
+		goto error;
 
 	snd_ff_proc_init(ff);
 
@@ -70,15 +67,13 @@ static void do_registration(struct work_struct *work)
 	if (err < 0)
 		goto error;
 
-	if (ff->spec->protocol->begin_session) {
-		err = snd_ff_create_pcm_devices(ff);
-		if (err < 0)
-			goto error;
+	err = snd_ff_create_pcm_devices(ff);
+	if (err < 0)
+		goto error;
 
-		err = snd_ff_create_hwdep_devices(ff);
-		if (err < 0)
-			goto error;
-	}
+	err = snd_ff_create_hwdep_devices(ff);
+	if (err < 0)
+		goto error;
 
 	err = snd_card_register(ff->card);
 	if (err < 0)
@@ -127,7 +122,7 @@ static void snd_ff_update(struct fw_unit *unit)
 
 	snd_ff_transaction_reregister(ff);
 
-	if (ff->registered && ff->spec->protocol->begin_session)
+	if (ff->registered)
 		snd_ff_stream_update_duplex(ff);
 }
 
@@ -153,6 +148,8 @@ static void snd_ff_remove(struct fw_unit *unit)
 
 static const struct snd_ff_spec spec_ff800 = {
 	.name = "Fireface800",
+	.pcm_capture_channels = {28, 20, 12},
+	.pcm_playback_channels = {28, 20, 12},
 	.midi_in_ports = 1,
 	.midi_out_ports = 1,
 	.protocol = &snd_ff_protocol_ff800,
