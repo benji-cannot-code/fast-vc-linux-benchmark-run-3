72f0b23b13c6758ef9d4f369e49fd4e7fc902533
Bart Van Assche <bvanassche@acm.org>
2018-12-17 13:20:38 -0800

    RDMA/srp: Rework handling of the maximum information unit length
    Move the maximum initiator to target information unit length parameter
    from struct srp_target_port into struct srp_rdma_ch. This patch does
    not change any functionality but makes the next patch easier to read.
    
    Cc: Sergey Gorenko <sergeygo@mellanox.com>
    Cc: Max Gurtovoy <maxg@mellanox.com>
    Cc: Laurence Oberman <loberman@redhat.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/ulp/srp/ib_srp.h
+++ drivers/infiniband/ulp/srp/ib_srp.h
@@ -133,6 +133,7 @@ struct srp_request {
 /**
  * struct srp_rdma_ch
  * @comp_vector: Completion vector used by this RDMA channel.
+ * @max_it_iu_len: Maximum initiator-to-target information unit length.
  * @max_ti_iu_len: Maximum target-to-initiator information unit length.
  */
 struct srp_rdma_ch {
@@ -150,6 +151,7 @@ struct srp_rdma_ch {
 		struct ib_fmr_pool     *fmr_pool;
 		struct srp_fr_pool     *fr_pool;
 	};
+	uint32_t		max_it_iu_len;
 	uint32_t		max_ti_iu_len;
 
 	/* Everything above this point is used in the hot path of
@@ -198,7 +200,6 @@ struct srp_target_port {
 	u32			ch_count;
 	u32			lkey;
 	enum srp_target_state	state;
-	unsigned int		max_iu_len;
 	unsigned int		cmd_sg_cnt;
 	unsigned int		indirect_size;
 	bool			allow_ext_sg;
