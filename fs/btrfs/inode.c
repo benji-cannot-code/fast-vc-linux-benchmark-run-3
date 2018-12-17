99bda68bc90bece18c5fbb908200873063a762f9
Nikolay Borisov <nborisov@suse.com>
2018-12-17 10:36:02 +0200

    btrfs: Remove not_found_em label from btrfs_get_extent
    In order to avoid duplicating init code for em there is an additional
    label, not_found_em, which is used to only set ->block_start. The only
    case when it will be used is if the extent we are adding overlaps with
    an existing extent. Make that case more obvious by:
    
     1. Adding a comment hinting at what's going on
     2. Assigning EXTENT_MAP_HOLE and directly going to insert.
    
     No functional changes.
    
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6859,10 +6859,13 @@ next:
 			goto not_found;
 		if (start > found_key.offset)
 			goto next;
+
+		/* New extent overlaps with existing one */
 		em->start = start;
 		em->orig_start = start;
 		em->len = found_key.offset - start;
-		goto not_found_em;
+		em->block_start = EXTENT_MAP_HOLE;
+		goto insert;
 	}
 
 	btrfs_extent_item_to_extent_map(inode, path, item,
@@ -6922,7 +6925,6 @@ not_found:
 	em->start = start;
 	em->orig_start = start;
 	em->len = len;
-not_found_em:
 	em->block_start = EXTENT_MAP_HOLE;
 insert:
 	btrfs_release_path(path);
