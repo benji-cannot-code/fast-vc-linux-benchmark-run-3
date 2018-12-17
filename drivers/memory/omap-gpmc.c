de1d0c3bfaa38071ecfdc7a09c317633b963e5ce
Linus Walleij <linus.walleij@linaro.org>
2018-12-17 14:11:08 +0100

    memory: omap-gpmc: Get the header of the enum
    Commit 21abf103818a
    ("gpio: Pass a flag to gpiochip_request_own_desc()")
    started to pass an enum gpiod_flags but this file is
    not including the header file that defines that enum
    and the compiler spits:
    
    drivers/memory/omap-gpmc.c: In function
    			    'gpmc_probe_generic_child':
    drivers/memory/omap-gpmc.c:2174:9: error: type of formal
    				   parameter 4 is incomplete
             0);
             ^
    
    Cc: Ladislav Michl <ladis@linux-mips.org>
    Cc: Janusz Krzysztofik <jmkrzyszt@gmail.com>
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Fixes: 21abf103818a ("gpio: Pass a flag to gpiochip_request_own_desc()")
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/memory/omap-gpmc.c
+++ drivers/memory/omap-gpmc.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/io.h>
 #include <linux/gpio/driver.h>
+#include <linux/gpio/consumer.h> /* GPIO descriptor enum */
 #include <linux/interrupt.h>
 #include <linux/irqdomain.h>
 #include <linux/platform_device.h>
