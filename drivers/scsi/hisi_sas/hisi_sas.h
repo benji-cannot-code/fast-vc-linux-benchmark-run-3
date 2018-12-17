1517a3e3bef112c50c50d0f20b2e036738a543d3
Xiang Chen <chenxiang66@hisilicon.com>
2018-12-17 22:40:07 +0800

    scsi: hisi_sas: Add support for DIF feature for v2 hw
    For v3 hw, we support DIF operation for SAS, but not SATA.
    
    In addition, DIF CRC16 is supported.
    
    This patchset adds the SW support for the described features. The main
    components are as follows:
    
    - Get protection mask from module param
    - Fill PI fields
    - Fill related to DIF in DQ and protection iu memories
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -70,6 +70,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HISI_SAS_SATA_PROTOCOL_FPDMA		0x8
 #define HISI_SAS_SATA_PROTOCOL_ATAPI		0x10
 
+#define HISI_SAS_DIF_PROT_MASK (SHOST_DIF_TYPE1_PROTECTION | \
+				SHOST_DIF_TYPE2_PROTECTION | \
+				SHOST_DIF_TYPE3_PROTECTION)
+
+#define HISI_SAS_PROT_MASK (HISI_SAS_DIF_PROT_MASK)
+
 struct hisi_hba;
 
 enum {
@@ -268,6 +274,8 @@ struct hisi_hba {
 	struct platform_device *platform_dev;
 	struct pci_dev *pci_dev;
 	struct device *dev;
+
+	int prot_mask;
 
 	void __iomem *regs;
 	void __iomem *sgpio_regs;
