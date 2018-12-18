998c530fbd75e28b68509abd027a3179acd58e00
Quentin Monnet <quentin.monnet@netronome.com>
2018-12-18 10:13:19 +0000

    tools: bpftool: add an option to prevent auto-mount of bpffs, tracefs
    In order to make life easier for users, bpftool automatically attempts
    to mount the BPF virtual file system, if it is not mounted already,
    before trying to pin objects in it. Similarly, it attempts to mount
    tracefs if necessary before trying to dump the trace pipe to the
    console.
    
    While mounting file systems on-the-fly can improve user experience, some
    administrators might prefer to avoid that. Let's add an option to block
    these mount attempts. Note that it does not prevent automatic mounting
    of tracefs by debugfs for the "bpftool prog tracelog" command.
    
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/bpf/bpftool/main.h
+++ tools/bpf/bpftool/main.h
@@ -45,7 +45,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HELP_SPEC_PROGRAM						\
 	"PROG := { id PROG_ID | pinned FILE | tag PROG_TAG }"
 #define HELP_SPEC_OPTIONS						\
-	"OPTIONS := { {-j|--json} [{-p|--pretty}] | {-f|--bpffs} | {-m|--mapcompat}"
+	"OPTIONS := { {-j|--json} [{-p|--pretty}] | {-f|--bpffs} |\n"	\
+	"\t            {-m|--mapcompat} | {-n|--nomount} }"
 #define HELP_SPEC_MAP							\
 	"MAP := { id MAP_ID | pinned FILE }"
 
@@ -86,6 +87,7 @@ extern const char *bin_name;
 extern json_writer_t *json_wtr;
 extern bool json_output;
 extern bool show_pinned;
+extern bool block_mount;
 extern int bpf_flags;
 extern struct pinned_obj_table prog_table;
 extern struct pinned_obj_table map_table;
