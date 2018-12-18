79447ca407e342a89d73b37834b6d58daebadbe6
Michael Straube <straube.linux@gmail.com>
2018-12-18 19:34:39 +0100

    staging: rtl8188eu: add blank line after declarations
    Add a missing blank line after declarations in rtw_mlme_ext.c.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/core/rtw_mlme_ext.c
+++ drivers/staging/rtl8188eu/core/rtw_mlme_ext.c
@@ -3593,6 +3593,7 @@ static unsigned int OnAction_back(struct adapter *padapter,
 	struct mlme_ext_info *pmlmeinfo = &(pmlmeext->mlmext_info);
 	u8 *pframe = precv_frame->pkt->data;
 	struct sta_priv *pstapriv = &padapter->stapriv;
+
 	/* check RA matches or not */
 	if (memcmp(myid(&(padapter->eeprompriv)), GetAddr1Ptr(pframe),
 		   ETH_ALEN))/* for if1, sta/ap mode */
