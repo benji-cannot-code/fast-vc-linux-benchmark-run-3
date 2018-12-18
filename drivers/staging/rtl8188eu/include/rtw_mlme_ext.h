8b4555330caa82c44db8da69db2f90840168863e
Michael Straube <straube.linux@gmail.com>
2018-12-18 19:34:46 +0100

    staging: rtl8188eu: make some arrays static in rtw_mlme_ext.c
    Make some arrays that are only used in rtw_mlme_ext.c static and
    remove the corresponding extern declarations from rtw_mlme_ext.h.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_mlme_ext.h
+++ drivers/staging/rtl8188eu/include/rtw_mlme_ext.h
@@ -81,13 +81,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define		_48M_RATE_	10
 #define		_54M_RATE_	11
 
-
 extern const u8 RTW_WPA_OUI[];
-extern const u8 WMM_OUI[];
 extern const u8 WPS_OUI[];
-extern const u8 P2P_OUI[];
-
-extern const u8 WMM_PARA_OUI[];
 
 /*  Channel Plan Type. */
 /*  Note: */
