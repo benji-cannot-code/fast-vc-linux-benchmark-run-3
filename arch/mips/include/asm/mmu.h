7bf0f1f7531e0be380912a43c51f73917e9c286d
Paul Burton <paul.burton@mips.com>
2018-12-17 22:05:40 -0800

    MIPS: Remove struct mm_context_t fp_mode_switching field
    The fp_mode_switching field in struct mm_context_t was left unused by
    commit 8c8d953c2800 ("MIPS: Schedule on CPUs we need to lose FPU for a
    mode switch") in v4.19, with nothing modifying its value & nothing
    waiting on it having any particular value after that commit. Remove the
    unused field & the one remaining reference to it.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>

--- arch/mips/include/asm/mmu.h
+++ arch/mips/include/asm/mmu.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 typedef struct {
 	u64 asid[NR_CPUS];
 	void *vdso;
-	atomic_t fp_mode_switching;
 
 	/* lock to be held whilst modifying fp_bd_emupage_allocmap */
 	spinlock_t bd_emupage_lock;
