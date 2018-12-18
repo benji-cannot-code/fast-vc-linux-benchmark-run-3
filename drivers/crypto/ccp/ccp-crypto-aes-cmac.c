7912d81a8c1d97a094f9790dcf422468b9b19851
Hook, Gary <Gary.Hook@amd.com>
2018-12-18 15:48:29 +0000

    crypto: ccp - Update copyright notices and dates
    Correct copyright dates for files that have had code added
    to them in 2018.
    
    Signed-off-by: Gary R Hook <gary.hook@amd.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccp/ccp-crypto-aes-cmac.c
+++ drivers/crypto/ccp/ccp-crypto-aes-cmac.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * AMD Cryptographic Coprocessor (CCP) AES CMAC crypto API support
  *
- * Copyright (C) 2013 Advanced Micro Devices, Inc.
+ * Copyright (C) 2013,2018 Advanced Micro Devices, Inc.
  *
  * Author: Tom Lendacky <thomas.lendacky@amd.com>
  *
