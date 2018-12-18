74473a9dca33f2ce9e67fe7a078948f621750d07
Michael Straube <straube.linux@gmail.com>
2018-12-18 19:34:42 +0100

    staging: rtl8188eu: remove unused arrays WFD_OUI and WMM_INFO_OUI
    The arrays WFD_OUI and WMM_INFO_OUI are not used anywhere,
    so remove them.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/rtw_mlme_ext.h
+++ drivers/staging/rtl8188eu/include/rtw_mlme_ext.h
@@ -85,10 +85,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern unsigned char RTW_WPA_OUI[];
 extern unsigned char WMM_OUI[];
 extern unsigned char WPS_OUI[];
-extern unsigned char WFD_OUI[];
 extern unsigned char P2P_OUI[];
 
-extern unsigned char WMM_INFO_OUI[];
 extern unsigned char WMM_PARA_OUI[];
 
 /*  Channel Plan Type. */
