08068bcf76a4cb7ee628f0fbee4b60263f006cee
Chuck Lever <chuck.lever@oracle.com>
2018-12-19 10:59:07 -0500

    xprtrdma: Plant XID in on-the-wire RDMA offset (FRWR)
    Place the associated RPC transaction's XID in the upper 32 bits of
    each RDMA segment's rdma_offset field. There are two reasons to do
    this:
    
    - The R_key only has 8 bits that are different from registration to
      registration. The XID adds more uniqueness to each RDMA segment to
      reduce the likelihood of a software bug on the server reading from
      or writing into memory it's not supposed to.
    
    - On-the-wire RDMA Read and Write requests do not otherwise carry
      any identifier that matches them up to an RPC. The XID in the
      upper 32 bits will act as an eye-catcher in network captures.
    
    Suggested-by: Tom Talpey <ttalpey@microsoft.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprtrdma/rpc_rdma.c
+++ net/sunrpc/xprtrdma/rpc_rdma.c
@@ -357,7 +357,7 @@ rpcrdma_encode_read_list(struct rpcrdma_xprt *r_xprt, struct rpcrdma_req *req,
 		return nsegs;
 
 	do {
-		seg = frwr_map(r_xprt, seg, nsegs, false, &mr);
+		seg = frwr_map(r_xprt, seg, nsegs, false, rqst->rq_xid, &mr);
 		if (IS_ERR(seg))
 			return PTR_ERR(seg);
 		rpcrdma_mr_push(mr, &req->rl_registered);
@@ -414,7 +414,7 @@ rpcrdma_encode_write_list(struct rpcrdma_xprt *r_xprt, struct rpcrdma_req *req,
 
 	nchunks = 0;
 	do {
-		seg = frwr_map(r_xprt, seg, nsegs, true, &mr);
+		seg = frwr_map(r_xprt, seg, nsegs, true, rqst->rq_xid, &mr);
 		if (IS_ERR(seg))
 			return PTR_ERR(seg);
 		rpcrdma_mr_push(mr, &req->rl_registered);
@@ -471,7 +471,7 @@ rpcrdma_encode_reply_chunk(struct rpcrdma_xprt *r_xprt, struct rpcrdma_req *req,
 
 	nchunks = 0;
 	do {
-		seg = frwr_map(r_xprt, seg, nsegs, true, &mr);
+		seg = frwr_map(r_xprt, seg, nsegs, true, rqst->rq_xid, &mr);
 		if (IS_ERR(seg))
 			return PTR_ERR(seg);
 		rpcrdma_mr_push(mr, &req->rl_registered);
