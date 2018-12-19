c529eb2f349f5aa12c0603a355b1fbceed8f499a
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2018-12-19 12:18:05 +0100

    regmap: irq: add an option to clear status registers on unmask
    Some interrupt controllers whose interrupts are acked on read will set
    the status bits for masked interrupts without changing the state of
    the IRQ line.
    
    Some chips have an additional "feature" where if those set bits are
    not cleared before unmasking their respective interrupts, the IRQ
    line will change the state and we'll interpret this as an interrupt
    although it actually fired when it was masked.
    
    Add a new field to the irq chip struct that tells the regmap irq chip
    code to always clear the status registers before actually changing the
    irq mask values.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/regmap.h
+++ include/linux/regmap.h
@@ -1156,6 +1156,9 @@ struct regmap_irq {
  * @type_in_mask: Use the mask registers for controlling irq type. For
  *                interrupts defining type_rising/falling_mask use mask_base
  *                for edge configuration and never update bits in type_base.
+ * @clear_on_unmask: For chips with interrupts cleared on read: read the status
+ *                   registers before unmasking interrupts to clear any bits
+ *                   set when they were masked.
  * @runtime_pm:  Hold a runtime PM lock on the device when accessing it.
  *
  * @num_regs:    Number of registers in each control bank.
@@ -1195,6 +1198,7 @@ struct regmap_irq_chip {
 	bool runtime_pm:1;
 	bool type_invert:1;
 	bool type_in_mask:1;
+	bool clear_on_unmask:1;
 
 	int num_regs;
 
