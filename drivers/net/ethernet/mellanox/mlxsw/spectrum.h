39a6c7cf5dbfd827891649f29e18953ee1908f8d
Ido Schimmel <idosch@mellanox.com>
2018-12-20 19:42:33 +0000

    mlxsw: spectrum: Switch to VID 4095 as default VID
    There is no need to abuse VID 1 anymore and we can instead use VID 4095
    as the default VLAN, which will be configured on the port throughout its
    lifetime.
    
    The OVS join / leave functions are changed to enable VIDs 1-4094
    (inclusive) instead of 2-4095. This because VID 4095 is now the default
    VLAN instead of 1.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Reviewed-by: Petr Machata <petrm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "core_acl_flex_actions.h"
 #include "reg.h"
 
-#define MLXSW_SP_DEFAULT_VID 1
+#define MLXSW_SP_DEFAULT_VID (VLAN_N_VID - 1)
 
 #define MLXSW_SP_FID_8021D_MAX 1024
 
