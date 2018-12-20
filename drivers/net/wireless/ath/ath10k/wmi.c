73136ff537472494f302eb0865268d0080762208
Sathishkumar Muruganandam <murugana@codeaurora.org>
2018-12-20 09:53:30 +0200

    ath10k: disable 4addr source port learning in 10.4 FW by default
    Currently in 10.4 FW, all the received 4addr frames are processed for
    source port learning which is enabled by default. This learning can't be
    disabled by default in FW since it breaks backward compatibility.
    
    Since ath10k uses mac80211 based 4addr mode, source port learning done in
    10.4 FW is redundant and also causes issues when 3addr frames are
    transmitted/received for a 4addr station.
    
    One such visible functional impact is when GTK rekey frame from
    hostapd based AP to 4addr STA is dropped in AP's 10.4 FW. This is since
    GTK rekey EAPOL frame is 3addr frame on AP interface and STA enabled
    with 4addr is already allowed for receiving 3addr EAPOL frames.
    
    Source port learning implementation in 10.4 FW drops this 3addr GTK rekey
    frame in AP destinated for 4addr STA causing disassociation and
    re-association for every GTK rekey session. GTK rekey issue is not seen
    when learning is disabled in FW.
    
    To prevent such issues without breaking backward compatibility, FW
    advertises new service bit making the source port learning configurable and
    this learning is being currently disabled during ath10k vdev creation.
    
    * Tested HW: QCA9984
    * Tested FW: 10.4-3.6.0.1-00004
    
    Signed-off-by: Sathishkumar Muruganandam <murugana@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -826,6 +826,7 @@ static struct wmi_vdev_param_map wmi_vdev_param_map = {
 	.meru_vc = WMI_VDEV_PARAM_UNSUPPORTED,
 	.rx_decap_type = WMI_VDEV_PARAM_UNSUPPORTED,
 	.bw_nss_ratemask = WMI_VDEV_PARAM_UNSUPPORTED,
+	.disable_4addr_src_lrn = WMI_VDEV_PARAM_UNSUPPORTED,
 };
 
 /* 10.X WMI VDEV param map */
@@ -901,6 +902,7 @@ static struct wmi_vdev_param_map wmi_10x_vdev_param_map = {
 	.meru_vc = WMI_VDEV_PARAM_UNSUPPORTED,
 	.rx_decap_type = WMI_VDEV_PARAM_UNSUPPORTED,
 	.bw_nss_ratemask = WMI_VDEV_PARAM_UNSUPPORTED,
+	.disable_4addr_src_lrn = WMI_VDEV_PARAM_UNSUPPORTED,
 };
 
 static struct wmi_vdev_param_map wmi_10_2_4_vdev_param_map = {
@@ -975,6 +977,7 @@ static struct wmi_vdev_param_map wmi_10_2_4_vdev_param_map = {
 	.meru_vc = WMI_VDEV_PARAM_UNSUPPORTED,
 	.rx_decap_type = WMI_VDEV_PARAM_UNSUPPORTED,
 	.bw_nss_ratemask = WMI_VDEV_PARAM_UNSUPPORTED,
+	.disable_4addr_src_lrn = WMI_VDEV_PARAM_UNSUPPORTED,
 };
 
 static struct wmi_vdev_param_map wmi_10_4_vdev_param_map = {
@@ -1052,6 +1055,7 @@ static struct wmi_vdev_param_map wmi_10_4_vdev_param_map = {
 	.bw_nss_ratemask = WMI_10_4_VDEV_PARAM_BW_NSS_RATEMASK,
 	.inc_tsf = WMI_10_4_VDEV_PARAM_TSF_INCREMENT,
 	.dec_tsf = WMI_10_4_VDEV_PARAM_TSF_DECREMENT,
+	.disable_4addr_src_lrn = WMI_10_4_VDEV_PARAM_DISABLE_4_ADDR_SRC_LRN,
 };
 
 static struct wmi_pdev_param_map wmi_pdev_param_map = {
