1b3c57c17a35bd16e4dafc1db6786d36e70ac4a3
Philippe Schenker <philippe.schenker@toradex.com>
2018-12-21 14:46:31 +0100

    mfd: stmpe: Move ADC related defines to MFD header
    Move defines that are ADC related to the header of the overlying MFD,
    so they can be used from multiple sub-devices.
    
    Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/stmpe.h
+++ include/linux/mfd/stmpe.h
@@ -11,6 +11,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/mutex.h>
 
+#define STMPE_SAMPLE_TIME(x)	((x & 0xf) << 4)
+#define STMPE_MOD_12B(x)	((x & 0x1) << 3)
+#define STMPE_REF_SEL(x)	((x & 0x1) << 1)
+#define STMPE_ADC_FREQ(x)	(x & 0x3)
+#define STMPE_AVE_CTRL(x)	((x & 0x3) << 6)
+#define STMPE_DET_DELAY(x)	((x & 0x7) << 3)
+#define STMPE_SETTLING(x)	(x & 0x7)
+#define STMPE_FRACTION_Z(x)	(x & 0x7)
+#define STMPE_I_DRIVE(x)	(x & 0x1)
+#define STMPE_OP_MODE(x)	((x & 0x7) << 1)
+
 struct device;
 struct regulator;
 
