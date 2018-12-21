917bfeb13e4bf0256d08899409475fc3106541c9
Stefan Agner <stefan@agner.ch>
2018-12-21 14:46:32 +0100

    mfd: stmpe: Preparations for STMPE ADC driver
    This prepares the MFD for the STMPE ADC driver. This commit introduces
    devicetree settings that are used by the ADC and adds an init function.
    Common ADC settings that are shared with the touchscreen driver can now
    reside in the overlying MFD.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/stmpe.h
+++ include/linux/mfd/stmpe.h
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define STMPE_I_DRIVE(x)	(x & 0x1)
 #define STMPE_OP_MODE(x)	((x & 0x7) << 1)
 
+#define STMPE811_REG_ADC_CTRL1	0x20
+#define STMPE811_REG_ADC_CTRL2	0x21
+
 struct device;
 struct regulator;
 
@@ -135,6 +138,12 @@ struct stmpe {
 	u8 ier[2];
 	u8 oldier[2];
 	struct stmpe_platform_data *pdata;
+
+	/* For devices that use an ADC */
+	u8 sample_time;
+	u8 mod_12b;
+	u8 ref_sel;
+	u8 adc_freq;
 };
 
 extern int stmpe_reg_write(struct stmpe *stmpe, u8 reg, u8 data);
@@ -148,6 +157,7 @@ extern int stmpe_set_altfunc(struct stmpe *stmpe, u32 pins,
 			     enum stmpe_block block);
 extern int stmpe_enable(struct stmpe *stmpe, unsigned int blocks);
 extern int stmpe_disable(struct stmpe *stmpe, unsigned int blocks);
+extern int stmpe811_adc_common_init(struct stmpe *stmpe);
 
 #define STMPE_GPIO_NOREQ_811_TOUCH	(0xf0)
 
