bfacbc827d51b791cf52cd912baa0f7ef9174096
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-12-21 17:33:07 +0900

    kconfig: split images.c out of qconf.cc/gconf.c to fix gconf warnings
    Currently, images.c is included by qconf.cc and gconf.c.
    qconf.cc uses all of xpm_* arrays, but gconf.c only some of them.
    Hence, lots of "... defined but not used" warnings are displayed
    while compiling gconf.c
    
    Splitting out images.c fixes the warnings.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- scripts/kconfig/gconf.c
+++ scripts/kconfig/gconf.c
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <stdlib.h>
 #include "lkc.h"
-#include "images.c"
+#include "images.h"
 
 #include <glade/glade.h>
 #include <gtk/gtk.h>
---
+++ scripts/kconfig/images.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2002 Roman Zippel <zippel@linux-m68k.org>
+ */
+
+#ifndef IMAGES_H
+#define IMAGES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char *xpm_load[];
+extern const char *xpm_save[];
+extern const char *xpm_back[];
+extern const char *xpm_tree_view[];
+extern const char *xpm_single_view[];
+extern const char *xpm_split_view[];
+extern const char *xpm_symbol_no[];
+extern const char *xpm_symbol_mod[];
+extern const char *xpm_symbol_yes[];
+extern const char *xpm_choice_no[];
+extern const char *xpm_choice_yes[];
+extern const char *xpm_menu[];
+extern const char *xpm_menu_inv[];
+extern const char *xpm_menuback[];
+extern const char *xpm_void[];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IMAGES_H */
