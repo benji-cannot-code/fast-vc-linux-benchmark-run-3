2d0c836fb143da778a658244ad81ec8de130d8d5
Paolo Abeni <pabeni@redhat.com>
2018-12-21 19:03:15 +0100

    net: minor cleanup in skb_ext_add()
    When the extension to be added is already present, the only
    skb field we may need to update is 'extensions': we can reorder
    the code and avoid a branch.
    
    v1 -> v2:
     - be sure to flag the newly added extension as active
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Acked-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -5667,11 +5667,8 @@ void *skb_ext_add(struct sk_buff *skb, enum skb_ext_id id)
 		if (!new)
 			return NULL;
 
-		if (__skb_ext_exist(new, id)) {
-			if (old != new)
-				skb->extensions = new;
+		if (__skb_ext_exist(new, id))
 			goto set_active;
-		}
 
 		newoff = new->chunks;
 	} else {
@@ -5685,8 +5682,8 @@ void *skb_ext_add(struct sk_buff *skb, enum skb_ext_id id)
 	newlen = newoff + skb_ext_type_len[id];
 	new->chunks = newlen;
 	new->offset[id] = newoff;
-	skb->extensions = new;
 set_active:
+	skb->extensions = new;
 	skb->active_extensions |= 1 << id;
 	return skb_ext_get_ptr(new, id);
 }
