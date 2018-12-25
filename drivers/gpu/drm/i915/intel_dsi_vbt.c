74afffa2c5db1eabde33ee50dcba3b09069e4dd8
Brajeswar Ghosh <brajeswar.linux@gmail.com>
2018-12-25 19:00:56 +0530

    drm/intel_dsi_vbt: Remove duplicate header
    Remove video/mipi_display.h which is included more than once
    
    Signed-off-by: Brajeswar Ghosh <brajeswar.linux@gmail.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181225133055.GA2628@hp-pavilion-15-notebook-pc-brajeswar

--- drivers/gpu/drm/i915/intel_dsi_vbt.c
+++ drivers/gpu/drm/i915/intel_dsi_vbt.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <video/mipi_display.h>
 #include <asm/intel-mid.h>
-#include <video/mipi_display.h>
 #include "i915_drv.h"
 #include "intel_drv.h"
 #include "intel_dsi.h"
