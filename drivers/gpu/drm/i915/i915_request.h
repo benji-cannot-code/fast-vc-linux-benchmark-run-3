9dcad11943bda9972f0eaa9a54451d837ae88141
Brajeswar Ghosh <brajeswar.linux@gmail.com>
2018-12-25 18:53:40 +0530

    drm/i915: Remove duplicate header from i915_request.h
    Remove i915_scheduler.h which is included more than once
    
    Signed-off-by: Brajeswar Ghosh <brajeswar.linux@gmail.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181225132340.GA2584@hp-pavilion-15-notebook-pc-brajeswar

--- drivers/gpu/drm/i915/i915_request.h
+++ drivers/gpu/drm/i915/i915_request.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_gem.h"
 #include "i915_scheduler.h"
 #include "i915_sw_fence.h"
-#include "i915_scheduler.h"
 
 #include <uapi/drm/i915_drm.h>
 
