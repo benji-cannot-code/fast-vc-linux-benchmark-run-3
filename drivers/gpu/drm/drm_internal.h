4554ec598b202f2ca81d5eb52a074badb5133670
Sam Ravnborg <sam@ravnborg.org>
2018-12-26 22:03:47 +0100

    drm: move DRM_IF_VERSION to drm_internal.h
    Move DRM_IF_VERSION out of drmP.h to allow users
    to get rid of the drmP include.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Maxime Ripard <maxime.ripard@bootlin.com>
    Cc: Sean Paul <sean@poorly.run>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20181226210353.13993-1-sam@ravnborg.org

--- drivers/gpu/drm/drm_internal.h
+++ drivers/gpu/drm/drm_internal.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRM_IF_MAJOR 1
 #define DRM_IF_MINOR 4
 
+#define DRM_IF_VERSION(maj, min) (maj << 16 | min)
+
 struct drm_prime_file_private;
 struct dma_buf;
 
