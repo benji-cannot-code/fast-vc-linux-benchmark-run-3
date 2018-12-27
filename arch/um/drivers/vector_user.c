b399935d1c7a9005f236c032c23fe243391edbbf
Christoph Hellwig <hch@lst.de>
2018-12-27 08:33:24 +0100

    um: writev needs <sys/uio.h>
    vector_user.c doesn't compile without this for me.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/vector_user.c
+++ arch/um/drivers/vector_user.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_packet.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
+#include <sys/uio.h>
 #include <linux/virtio_net.h>
 #include <netdb.h>
 #include <stdlib.h>
