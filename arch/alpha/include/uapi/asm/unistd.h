4c8f20380d3c68060b3e7e16fa2a9a3a172b34aa
Arnd Bergmann <arnd@arndb.de>
2018-12-30 15:16:48 +0100

    alpha: update syscall macro definitions
    Other architectures commonly use __NR_umount2 for sys_umount,
    only ia64 and alpha use __NR_umount here. In order to synchronize
    the generated tables, use umount2 like everyone else, and add back
    the old name from asm/unistd.h for compatibility.
    
    For shmat, alpha uses the osf_shmat name, we can do the same thing
    here, which means we don't have to add an entry in the __IGNORE
    list now that shmat is mandatory everywhere
    
    alarm, creat, pause, time, and utime are optional everywhere
    these days, no need to list them here any more.
    
    I considered also adding the regular versions of the get*id system
    calls that have different names and calling conventions on alpha,
    which would further help unify the syscall ABI, but for now
    I decided against that.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/alpha/include/uapi/asm/unistd.h
+++ arch/alpha/include/uapi/asm/unistd.h
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _UAPI_ALPHA_UNISTD_H
 #define _UAPI_ALPHA_UNISTD_H
 
+/* These are traditionally the names linux-alpha uses for
+ * the two otherwise generic system calls */
+#define __NR_umount	__NR_umount2
+#define __NR_osf_shmat	__NR_shmat
+
 #include <asm/unistd_32.h>
 
 #endif /* _UAPI_ALPHA_UNISTD_H */
