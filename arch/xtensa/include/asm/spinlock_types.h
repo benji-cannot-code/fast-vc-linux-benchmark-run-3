2b7a8426ec88fa8923dfb80cfdde43ed8cc6e03f
Max Filippov <jcmvbkbc@gmail.com>
2019-01-01 14:08:32 -0800

    xtensa: use generic spinlock/rwlock implementation
    Drop custom spinlock/rwlock code and use ones from asm-generic. This way
    there is less code duplication (atomic primitives are reused).
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/include/asm/spinlock_types.h
+++ arch/xtensa/include/asm/spinlock_types.h
@@ -3,20 +3,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SPINLOCK_TYPES_H
 #define __ASM_SPINLOCK_TYPES_H
 
-#ifndef __LINUX_SPINLOCK_TYPES_H
+#if !defined(__LINUX_SPINLOCK_TYPES_H) && !defined(__ASM_SPINLOCK_H)
 # error "please don't include this file directly"
 #endif
 
-typedef struct {
-	volatile unsigned int slock;
-} arch_spinlock_t;
-
-#define __ARCH_SPIN_LOCK_UNLOCKED	{ 0 }
-
-typedef struct {
-	volatile unsigned int lock;
-} arch_rwlock_t;
-
-#define __ARCH_RW_LOCK_UNLOCKED		{ 0 }
+#include <asm-generic/qspinlock_types.h>
+#include <asm-generic/qrwlock_types.h>
 
 #endif
