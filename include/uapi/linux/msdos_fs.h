cca0e4085819dd6707d92ae45a0115ab1df8876e
Carmeli Tamir <carmeli.tamir@gmail.com>
2019-01-03 15:27:56 -0800

    fat: move MAX_FAT to fat.h and change it to inline function
    MAX_FAT is useless in msdos_fs.h, since it uses the MSDOS_SB function
    that is defined in fat.h.  So really, this macro can be only called from
    code that already includes fat.h.
    
    Hence, this patch moves it to fat.h, right after MSDOS_SB is defined.  I
    also changed it to an inline function in order to save the double call
    to MSDOS_SB.  This was suggested by joe@perches.com in the previous
    version.
    
    This patch is required for the next in the series, in which the variant
    (whether this is FAT12, FAT16 or FAT32) checks are replaced with new
    macros.
    
    Link: http://lkml.kernel.org/r/1544990640-11604-3-git-send-email-carmeli.tamir@gmail.com
    Signed-off-by: Carmeli Tamir <carmeli.tamir@gmail.com>
    Acked-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Bart Van Assche <bvanassche@acm.org>
    Cc: Johannes Thumshirn <jthumshirn@suse.de>
    Cc: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/uapi/linux/msdos_fs.h
+++ include/uapi/linux/msdos_fs.h
@@ -66,8 +66,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_FAT12	0xFF4
 #define MAX_FAT16	0xFFF4
 #define MAX_FAT32	0x0FFFFFF6
-#define MAX_FAT(s)	(MSDOS_SB(s)->fat_bits == 32 ? MAX_FAT32 : \
-	MSDOS_SB(s)->fat_bits == 16 ? MAX_FAT16 : MAX_FAT12)
 
 /* bad cluster mark */
 #define BAD_FAT12	0xFF7
