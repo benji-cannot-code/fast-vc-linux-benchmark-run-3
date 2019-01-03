ffb7acd428306c31f462f31b246735e2dd2bf2e0
Hans de Goede <hdegoede@redhat.com>
2019-01-03 14:45:28 +0100

    ASoC: Intel: bytcht_es8316: Sort includes alphabetically
    For lack of a better (non-random) way of sorting includes more and more
    files in the kernel are moving over to sorting the includes alphabetically.
    
    Move the bytcht_es8316 driver over to this sorting before we add a
    bunch of more includes.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/boards/bytcht_es8316.c
+++ sound/soc/intel/boards/bytcht_es8316.c
@@ -20,13 +20,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
+#include <linux/clk.h>
+#include <linux/device.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
-#include <linux/device.h>
 #include <linux/slab.h>
 #include <asm/platform_sst_audio.h>
-#include <linux/clk.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
