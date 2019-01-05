d5beafbb2514b9e1bed32014a9eafcbe80dba08d
Olof Johansson <olof@lixom.net>
2019-01-05 13:21:18 -0800

    lib/genalloc.c: include vmalloc.h
    Fixes build break on most ARM/ARM64 defconfigs:
    
      lib/genalloc.c: In function 'gen_pool_add_virt':
      lib/genalloc.c:190:10: error: implicit declaration of function 'vzalloc_node'; did you mean 'kzalloc_node'?
      lib/genalloc.c:190:8: warning: assignment to 'struct gen_pool_chunk *' from 'int' makes pointer from integer without a cast [-Wint-conversion]
      lib/genalloc.c: In function 'gen_pool_destroy':
      lib/genalloc.c:254:3: error: implicit declaration of function 'vfree'; did you mean 'kfree'?
    
    Fixes: 6862d2fc8185 ('lib/genalloc.c: use vzalloc_node() to allocate the bitmap')
    Cc: Huang Shijie <sjhuang@iluvatar.ai>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alexey Skidanov <alexey.skidanov@intel.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/genalloc.c
+++ lib/genalloc.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/genalloc.h>
 #include <linux/of_device.h>
+#include <linux/vmalloc.h>
 
 static inline size_t chunk_size(const struct gen_pool_chunk *chunk)
 {
