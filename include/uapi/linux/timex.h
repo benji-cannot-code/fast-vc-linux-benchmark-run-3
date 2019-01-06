1768c9f88f23911da412703778fe5f1ffe64b523
Arnd Bergmann <arnd@arndb.de>
2019-01-07 00:00:34 +0100

    y2038: remove struct definition redirects
    We now use 64-bit time_t on all architectures, so the __kernel_timex,
    __kernel_timeval and __kernel_timespec redirects can be removed
    after having served their purpose.
    
    This makes it all much less confusing, as the __kernel_* types
    now always refer to the same layout based on 64-bit time_t across
    all 32-bit and 64-bit architectures.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/uapi/linux/timex.h
+++ include/uapi/linux/timex.h
@@ -98,7 +98,6 @@ struct __kernel_timex_timeval {
 	long long		tv_usec;
 };
 
-#ifndef __kernel_timex
 struct __kernel_timex {
 	unsigned int modes;	/* mode selector */
 	int :32;            /* pad */
@@ -132,7 +131,6 @@ struct __kernel_timex {
 	int  :32; int  :32; int  :32; int  :32;
 	int  :32; int  :32; int  :32;
 };
-#endif
 
 /*
  * Mode codes (timex.mode)
