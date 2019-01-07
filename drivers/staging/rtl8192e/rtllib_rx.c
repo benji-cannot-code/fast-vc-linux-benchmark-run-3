4150065fae8afaf603f91ff035f5d6bf141d4dbb
Colin Ian King <colin.king@canonical.com>
2019-01-07 23:22:49 +0000

    staging: rtl8192e: fix various indentation issues
    There are several statements that have indentation issues, fix these.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtllib_rx.c
+++ drivers/staging/rtl8192e/rtllib_rx.c
@@ -914,7 +914,7 @@ static size_t rtllib_rx_get_hdrlen(struct rtllib_device *ieee,
 		rx_stats->bContainHTC = true;
 	}
 
-	 if (RTLLIB_QOS_HAS_SEQ(fc))
+	if (RTLLIB_QOS_HAS_SEQ(fc))
 		rx_stats->bIsQosData = true;
 
 	return hdrlen;
