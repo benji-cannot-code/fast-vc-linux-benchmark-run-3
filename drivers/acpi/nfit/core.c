753c2825fe9913b6ef93a99ee790d9d81e6cf5d6
Xiaochun Lee <lixc17@lenovo.com>
2019-01-07 10:33:36 +0800

    ACPI/nfit: delete the redundant header file
    The header file "intel.h" is repeated here, So delete one.
    
    Signed-off-by: Xiaochun Lee <lixc17@lenovo.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/acpi/nfit/core.c
+++ drivers/acpi/nfit/core.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <acpi/nfit.h>
 #include "intel.h"
 #include "nfit.h"
-#include "intel.h"
 
 /*
  * For readq() and writeq() on 32-bit builds, the hi-lo, lo-hi order is
