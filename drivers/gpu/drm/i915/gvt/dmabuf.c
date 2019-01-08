9b6032380fb3fee17285a8c0a5b86449ec8c5282
Jani Nikula <jani.nikula@intel.com>
2019-01-08 10:25:45 +0200

    drm/i915/gvt: remove drmP.h include
    drmP.h is deprecated and no longer needed.
    
    Reviewed-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>

--- drivers/gpu/drm/i915/gvt/dmabuf.c
+++ drivers/gpu/drm/i915/gvt/dmabuf.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/dma-buf.h>
-#include <drm/drmP.h>
 #include <linux/vfio.h>
 
 #include "i915_drv.h"
