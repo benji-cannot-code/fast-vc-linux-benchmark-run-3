fb16ae843d643611b095770ed77622cd7995bc18
Brajeswar Ghosh <brajeswar.linux@gmail.com>
2019-01-09 19:59:59 +0530

    remoteproc/qcom_sysmon.c: Remove duplicate header
    Remove linux/notifier.h which is included more than once
    
    Acked-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Brajeswar Ghosh <brajeswar.linux@gmail.com>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/remoteproc/qcom_sysmon.c
+++ drivers/remoteproc/qcom_sysmon.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/notifier.h>
 #include <linux/slab.h>
 #include <linux/io.h>
-#include <linux/notifier.h>
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/remoteproc/qcom_rproc.h>
