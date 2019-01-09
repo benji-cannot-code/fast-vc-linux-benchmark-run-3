0b8b454b14d3de89f436299998b29fb76d5fad14
Yan, Zheng <zyan@redhat.com>
2019-01-09 11:07:02 +0800

    ceph: support getting ceph.dir.pin vxattr
    Link: http://tracker.ceph.com/issues/37576
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/ceph/mds_client.c
+++ fs/ceph/mds_client.c
@@ -158,6 +158,13 @@ static int parse_reply_info_in(void **p, void *end,
 			ceph_decode_64_safe(p, end, change_attr, bad);
 		}
 
+		/* dir pin */
+		if (struct_v >= 2) {
+			ceph_decode_32_safe(p, end, info->dir_pin, bad);
+		} else {
+			info->dir_pin = -ENODATA;
+		}
+
 		*p = end;
 	} else {
 		if (features & CEPH_FEATURE_MDS_INLINE_DATA) {
@@ -188,6 +195,8 @@ static int parse_reply_info_in(void **p, void *end,
 				*p += info->pool_ns_len;
 			}
 		}
+
+		info->dir_pin = -ENODATA;
 	}
 	return 0;
 bad:
