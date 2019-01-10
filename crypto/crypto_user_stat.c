9fc23aa60ba85ba29973904502cc0e4322af1a04
Eric Biggers <ebiggers@google.com>
2019-01-10 12:17:56 -0800

    crypto: user - forward declare crypto_nlsk
    Move the declaration of crypto_nlsk into internal/cryptouser.h.  This
    fixes the following sparse warning:
    
        crypto/crypto_user_base.c:41:13: warning: symbol 'crypto_nlsk' was not declared. Should it be static?
    
    Cc: Corentin Labbe <clabbe@baylibre.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/crypto_user_stat.c
+++ crypto/crypto_user_stat.c
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static DEFINE_MUTEX(crypto_cfg_mutex);
 
-extern struct sock *crypto_nlsk;
-
 struct crypto_dump_info {
 	struct sk_buff *in_skb;
 	struct sk_buff *out_skb;
