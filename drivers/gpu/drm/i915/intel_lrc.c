d2a72f4884f25562e7c7dd8425b2d11a9f1f25fc
Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
2019-01-09 17:32:32 -0800

    drm/i915: init per-engine WAs for all engines
    commit 4a15c75c4246 ("drm/i915: Introduce per-engine workarounds")
    refactored the workaround code to have functions per-engine, but didn't
    call any of them from logical_xcs_ring_init. Since we do have a non-RCS
    workaround for KBL (WaKBLVECSSemaphoreWaitPoll) we do need to call
    intel_engine_init_workarounds for non-RCS engines.
    Note that whitelist is still RCS-only.
    
    v2: move the call to logical_ring_init (Chris)
    
    Fixes: 4a15c75c4246 ("drm/i915: Introduce per-engine workarounds")
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190110013232.8972-2-daniele.ceraolospurio@intel.com

--- drivers/gpu/drm/i915/intel_lrc.c
+++ drivers/gpu/drm/i915/intel_lrc.c
@@ -2235,6 +2235,8 @@ static int logical_ring_init(struct intel_engine_cs *engine)
 	if (ret)
 		return ret;
 
+	intel_engine_init_workarounds(engine);
+
 	if (HAS_LOGICAL_RING_ELSQ(i915)) {
 		execlists->submit_reg = i915->regs +
 			i915_mmio_reg_offset(RING_EXECLIST_SQ_CONTENTS(engine));
@@ -2297,7 +2299,6 @@ int logical_render_ring_init(struct intel_engine_cs *engine)
 	}
 
 	intel_engine_init_whitelist(engine);
-	intel_engine_init_workarounds(engine);
 
 	return 0;
 }
