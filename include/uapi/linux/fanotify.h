a47d514b0cc0beefe5a3232bf5449f52f8d34a7d
Amir Goldstein <amir73il@gmail.com>
2019-01-10 19:04:34 +0200

    fanotify: encode file identifier for FAN_REPORT_FID
    When user requests the flag FAN_REPORT_FID in fanotify_init(),
    a unique file identifier of the event target object will be reported
    with the event.
    
    The file identifier includes the filesystem's fsid (i.e. from statfs(2))
    and an NFS file handle of the file (i.e. from name_to_handle_at(2)).
    
    The file identifier makes holding the path reference and passing a file
    descriptor to user redundant, so those are disabled in a group with
    FAN_REPORT_FID.
    
    Encode fid and store it in event for a group with FAN_REPORT_FID.
    Up to 12 bytes of file handle on 32bit arch (16 bytes on 64bit arch)
    are stored inline in fanotify_event struct. Larger file handles are
    stored in an external allocated buffer.
    
    On failure to encode fid, we print a warning and queue the event
    without the fid information.
    
    [JK: Fold part of later patched into this one to use
    exportfs_encode_inode_fh() right away]
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>

--- include/uapi/linux/fanotify.h
+++ include/uapi/linux/fanotify.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Flags to determine fanotify event format */
 #define FAN_REPORT_TID		0x00000100	/* event->pid is thread id */
+#define FAN_REPORT_FID		0x00000200	/* Report unique file id */
 
 /* Deprecated - do not use this in programs and do not add new flags here! */
 #define FAN_ALL_INIT_FLAGS	(FAN_CLOEXEC | FAN_NONBLOCK | \
