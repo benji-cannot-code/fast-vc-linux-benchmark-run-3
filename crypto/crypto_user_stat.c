cf39a4b9b4cf8da7e8596bd65d4c441f70d2ba1f
Eric Biggers <ebiggers@google.com>
2019-01-10 12:17:59 -0800

    crypto: stat - remove unused mutex
    crypto_cfg_mutex in crypto_user_stat.c is unused.  Remove it.
    
    Cc: Corentin Labbe <clabbe@baylibre.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/crypto_user_stat.c
+++ crypto/crypto_user_stat.c
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define null_terminated(x)	(strnlen(x, sizeof(x)) < sizeof(x))
 
-static DEFINE_MUTEX(crypto_cfg_mutex);
-
 struct crypto_dump_info {
 	struct sk_buff *in_skb;
 	struct sk_buff *out_skb;
