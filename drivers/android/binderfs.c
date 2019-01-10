eda2d4b470e933fc58f0aaa2c7b1d3d8132b76c7
Christian Brauner <christian@brauner.io>
2019-01-11 00:25:41 +0100

    binderfs: rename header to binderfs.h
    It doesn't make sense to call the header binder_ctl.h when its sole
    existence is tied to binderfs. So give it a sensible name. Users will far
    more easily remember binderfs.h than binder_ctl.h.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binderfs.c
+++ drivers/android/binderfs.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/xarray.h>
 #include <uapi/asm-generic/errno-base.h>
 #include <uapi/linux/android/binder.h>
-#include <uapi/linux/android/binder_ctl.h>
+#include <uapi/linux/android/binderfs.h>
 
 #include "binder_internal.h"
 
