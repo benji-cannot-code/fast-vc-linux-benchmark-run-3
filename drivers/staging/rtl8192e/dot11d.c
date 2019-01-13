02c6040ba13da8197782858e0073af567e56b08d
Himadri Pandya <himadri18.07@gmail.com>
2019-01-12 22:21:49 -0800

    staging: rtl8192e: rename Len to len - style
    Fix checkpatch.pl warning:
    	CHECK: Avoid CamelCase: <Len>
    
    Signed-off-by: Himadri Pandya <himadri18.07@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/dot11d.c
+++ drivers/staging/rtl8192e/dot11d.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct channel_list {
 	u8      channel[32];
-	u8      Len;
+	u8      len;
 };
 
 static struct channel_list channel_array[] = {
@@ -63,10 +63,10 @@ void Dot11d_Channelmap(u8 channel_plan, struct rtllib_device *ieee)
 
 	ieee->bGlobalDomain = false;
 
-	if (channel_array[channel_plan].Len != 0) {
+	if (channel_array[channel_plan].len != 0) {
 		memset(GET_DOT11D_INFO(ieee)->channel_map, 0,
 		       sizeof(GET_DOT11D_INFO(ieee)->channel_map));
-		for (i = 0; i < channel_array[channel_plan].Len; i++) {
+		for (i = 0; i < channel_array[channel_plan].len; i++) {
 			if (channel_array[channel_plan].channel[i] < min_chan ||
 			    channel_array[channel_plan].channel[i] > max_chan)
 				break;
