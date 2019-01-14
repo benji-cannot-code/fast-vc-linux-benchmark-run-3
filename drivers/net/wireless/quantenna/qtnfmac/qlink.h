4ef836e69d72cd1c6090da77317726401408fa22
Sergey Matyukevich <sergey.matyukevich.os@quantenna.com>
2019-01-14 09:39:40 +0000

    qtnfmac: switch to 32bit values for RTS/FRAG thresholds
    Host wireless stack uses u32 type for RTS/FRAG threshold values.
    Switch to u32 in driver: pass u32 values to firmware and let
    firmware properly adapt these values according to its
    internal representation.
    
    Signed-off-by: Sergey Matyukevich <sergey.matyukevich.os@quantenna.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/quantenna/qtnfmac/qlink.h
+++ drivers/net/wireless/quantenna/qtnfmac/qlink.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/ieee80211.h>
 
-#define QLINK_PROTO_VER		11
+#define QLINK_PROTO_VER		12
 
 #define QLINK_MACID_RSVD		0xFF
 #define QLINK_VIFID_RSVD		0xFF
@@ -1185,7 +1185,7 @@ struct qlink_iface_limit_record {
 
 struct qlink_tlv_frag_rts_thr {
 	struct qlink_tlv_hdr hdr;
-	__le16 thr;
+	__le32 thr;
 } __packed;
 
 struct qlink_tlv_rlimit {
