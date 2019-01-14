b5135ee113fc8321526ee7e5515daca0b2a90d2a
Gao Xiang <gaoxiang25@huawei.com>
2019-01-14 19:40:25 +0800

    staging: erofs: move erofs_xattr_handlers to xattr.h
    Let's move independent xattr-related stuffs to xattr.h.
    No logic changes.
    
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/erofs/super.c
+++ drivers/staging/erofs/super.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/parser.h>
 #include <linux/seq_file.h>
 #include "internal.h"
+#include "xattr.h"
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/erofs.h>
