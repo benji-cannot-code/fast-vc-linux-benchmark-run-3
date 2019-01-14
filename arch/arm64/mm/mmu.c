935b2c1ee8d631f04f10f9e70da0b3b016ae6565
Will Deacon <will.deacon@arm.com>
2019-01-14 14:22:24 +0000

    arm64: Remove asm/memblock.h
    The arm64 asm/memblock.h header exists only to provide a function
    prototype for arm64_memblock_init(), which is called only from
    setup_arch().
    
    Move the declaration into mmu.h, where it can live alongside other
    init functions such as paging_init() and bootmem_init() without the
    need for its own special header file.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/setup.h>
 #include <asm/sizes.h>
 #include <asm/tlb.h>
-#include <asm/memblock.h>
 #include <asm/mmu_context.h>
 #include <asm/ptdump.h>
 #include <asm/tlbflush.h>
