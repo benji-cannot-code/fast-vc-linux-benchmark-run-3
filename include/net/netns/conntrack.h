7707282bf284bd5fac9e7baa36193bd657f94a12
Florian Westphal <fw@strlen.de>
2019-01-15 22:03:44 +0100

    netfilter: conntrack: remove sysctl registration helpers
    After previous patch these are not used anymore.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/net/netns/conntrack.h
+++ include/net/netns/conntrack.h
@@ -20,10 +20,6 @@ struct ctl_table_header;
 struct nf_conntrack_ecache;
 
 struct nf_proto_net {
-#ifdef CONFIG_SYSCTL
-	struct ctl_table_header *ctl_table_header;
-	struct ctl_table        *ctl_table;
-#endif
 	unsigned int		users;
 };
 
