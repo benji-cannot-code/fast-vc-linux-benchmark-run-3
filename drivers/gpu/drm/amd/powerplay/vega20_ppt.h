cbd7bfc658f5884aa1a63b17b43fa663f8f64743
Kevin Wang <Kevin1.Wang@amd.com>
2019-01-15 12:37:35 +0800

    drm/amd/powerplay: implement sensor of thermal_get_temperature for smu11
    add sensor interface of thermal temperature for debugfs and hwmon.
    
    Signed-off-by: Kevin Wang <Kevin1.Wang@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Reviewed-by: Evan Quan <evan.quan@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/vega20_ppt.h
+++ drivers/gpu/drm/amd/powerplay/vega20_ppt.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_REGULAR_DPM_NUMBER 16
 #define MAX_PCIE_CONF 2
-
 struct vega20_dpm_level {
         bool            enabled;
         uint32_t        value;
