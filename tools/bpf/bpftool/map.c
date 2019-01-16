a7ae6ff0eedddc3f06c780a78028333f526e6fff
Stanislav Fomichev <sdf@google.com>
2019-01-16 11:10:02 -0800

    bpftool: add peek command
    This is intended to be used with queues and stacks and be more
    user-friendly than 'lookup' without key/value.
    
    Example:
    bpftool map create /sys/fs/bpf/q type queue value 4 entries 10 name q
    bpftool map update pinned /sys/fs/bpf/q value 0 1 2 3
    bpftool map peek pinned /sys/fs/bpf/q
    value: 00 01 02 03
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/bpf/bpftool/map.c
+++ tools/bpf/bpftool/map.c
@@ -1169,6 +1169,7 @@ static int do_help(int argc, char **argv)
 		"       %s %s delete     MAP  key DATA\n"
 		"       %s %s pin        MAP  FILE\n"
 		"       %s %s event_pipe MAP [cpu N index M]\n"
+		"       %s %s peek       MAP\n"
 		"       %s %s help\n"
 		"\n"
 		"       " HELP_SPEC_MAP "\n"
@@ -1186,7 +1187,7 @@ static int do_help(int argc, char **argv)
 		bin_name, argv[-2], bin_name, argv[-2], bin_name, argv[-2],
 		bin_name, argv[-2], bin_name, argv[-2], bin_name, argv[-2],
 		bin_name, argv[-2], bin_name, argv[-2], bin_name, argv[-2],
-		bin_name, argv[-2]);
+		bin_name, argv[-2], bin_name, argv[-2]);
 
 	return 0;
 }
@@ -1203,6 +1204,7 @@ static const struct cmd cmds[] = {
 	{ "pin",	do_pin },
 	{ "event_pipe",	do_event_pipe },
 	{ "create",	do_create },
+	{ "peek",	do_lookup },
 	{ 0 }
 };
 
