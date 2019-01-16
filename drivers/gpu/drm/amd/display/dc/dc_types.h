dfa17937b914fb990789c233bf639672d98e52ad
Xiaodong Yan <Xiaodong.Yan@amd.com>
2019-01-17 00:18:01 +0800

    drm/amd/display: Add monitor patch for backlight off
    [Why]
    Different panel need different time from backlight disable to end of valid video data,
    if the time is too short, panel will flash when dpms off
    
    [How]
    Add monitor patch to control the time from backlight disable to end of valid video data,
    
    Signed-off-by: Xiaodong Yan <Xiaodong.Yan@amd.com>
    Reviewed-by: Wenjing Liu <Wenjing.Liu@amd.com>
    Acked-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc_types.h
+++ drivers/gpu/drm/amd/display/dc/dc_types.h
@@ -202,6 +202,7 @@ union display_content_support {
 struct dc_panel_patch {
 	unsigned int dppowerup_delay;
 	unsigned int extra_t12_ms;
+	unsigned int extra_delay_backlight_off;
 };
 
 struct dc_edid_caps {
