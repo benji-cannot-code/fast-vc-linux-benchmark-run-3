68d9a099121bf6fcf39f2fbf85cd07f57a367263
YueHaibing <yuehaibing@huawei.com>
2019-01-17 15:22:20 +0800

    qed: remove duplicated include from qed_if.h
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Denis Bolotin <dbolotin@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/qed/qed_if.h
+++ include/linux/qed/qed_if.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/pci.h>
 #include <linux/skbuff.h>
-#include <linux/types.h>
 #include <asm/byteorder.h>
 #include <linux/io.h>
 #include <linux/compiler.h>
