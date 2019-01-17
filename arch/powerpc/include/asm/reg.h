c834a30247ea8e61ddb4ea332d64ac0432aadcd0
Christophe Leroy <christophe.leroy@c-s.fr>
2019-01-17 23:25:53 +1100

    powerpc: Update comments in preparation for THREAD_INFO_IN_TASK
    Update a few comments that talk about current_thread_info() in
    preparation for THREAD_INFO_IN_TASK.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    [mpe: Split out of larger patch]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/reg.h
+++ arch/powerpc/include/asm/reg.h
@@ -1063,7 +1063,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	- SPRG9 debug exception scratch
  *
  * All 32-bit:
- *	- SPRG3 current thread_info pointer
+ *	- SPRG3 current thread_struct physical addr pointer
  *        (virtual on BookE, physical on others)
  *
  * 32-bit classic:
