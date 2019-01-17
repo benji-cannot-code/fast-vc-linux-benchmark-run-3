156b1b7975474ace2460bc54129ae89dba7e8b04
Christophe Leroy <christophe.leroy@c-s.fr>
2019-01-17 23:27:28 +1100

    powerpc: Use linux/thread_info.h in processor.h
    When we enable THREAD_INFO_IN_TASK we will remove our definition of
    current_thread_info(). Instead it will come from linux/thread_info.h
    
    So switch processor.h to include the latter, so that it can continue
    to find current_thread_info().
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    [mpe: Split out of larger patch]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/processor.h
+++ arch/powerpc/include/asm/processor.h
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 #include <linux/types.h>
-#include <asm/thread_info.h>
+#include <linux/thread_info.h>
 #include <asm/ptrace.h>
 #include <asm/hw_breakpoint.h>
 
