d3b59e23e614812de365df383d4a276f7e363419
Thomas Gleixner <tglx@linutronix.de>
2019-01-18 00:14:25 +0100

    vfio/pci: Cleanup license mess
    The recently added nvlink2 VFIO driver introduced a license conflict in two
    files. In both cases the SPDX license identifier is:
    
      SPDX-License-Identifier: GPL-2.0+
    
    but the files contain also the following license boiler plate text:
    
      * This program is free software; you can redistribute it and/or modify
      * it under the terms of the GNU General Public License version 2 as
      * published by the Free Software Foundation
    
    The latter is GPL-2.9-only and not GPL-2.0=.
    
    Looking deeper. The nvlink source file is derived from vfio_pci_igd.c which
    is also licensed under GPL-2.0-only and it can be assumed that the file was
    copied and modified. As the original file is licensed GPL-2.0-only it's not
    possible to relicense derivative work to GPL-2.0-or-later.
    
    Fix the SPDX identifier and remove the boiler plate as it is redundant.
    
    Fixes: 7f92891778df ("vfio_pci: Add NVIDIA GV100GL [Tesla V100 SXM2] subdriver")
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Alexey Kardashevskiy <aik@ozlabs.ru>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: kvm@vger.kernel.org
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/vfio/pci/vfio_pci_nvlink2.c
+++ drivers/vfio/pci/vfio_pci_nvlink2.c
@@ -1,14 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-// SPDX-License-Identifier: GPL-2.0+
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * VFIO PCI NVIDIA Whitherspoon GPU support a.k.a. NVLink2.
  *
  * Copyright (C) 2018 IBM Corp.  All rights reserved.
  *     Author: Alexey Kardashevskiy <aik@ozlabs.ru>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  *
  * Register an on-GPU RAM region for cacheable access.
  *
