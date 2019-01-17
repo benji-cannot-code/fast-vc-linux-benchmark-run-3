966c088311d258d7f36904e71d63ce3b482d7186
Sabyasachi Gupta <sabyasachi.linux@gmail.com>
2019-01-17 21:40:33 +0530

    powerpc/powernv: Remove duplicate header
    Remove linux/printk.h which is included more than once.
    
    Signed-off-by: Sabyasachi Gupta <sabyasachi.linux@gmail.com>
    Acked-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/powernv/opal.c
+++ arch/powerpc/platforms/powernv/opal.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/memblock.h>
 #include <linux/kthread.h>
 #include <linux/freezer.h>
-#include <linux/printk.h>
 #include <linux/kmsg_dump.h>
 #include <linux/console.h>
 #include <linux/sched/debug.h>
