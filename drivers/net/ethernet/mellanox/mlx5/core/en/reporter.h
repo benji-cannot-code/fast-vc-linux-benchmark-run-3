ded92243d04bbcb1784ad7fd6ee371c9ae16fac0
Eran Ben Elisha <eranbe@mellanox.com>
2019-01-17 23:59:19 +0200

    net/mlx5e: Add TX timeout support for mlx5e TX reporter
    With this patch, ndo_tx_timeout callback will be redirected to the TX
    reporter in order to detect a TX timeout error and report it to the
    devlink health. (The watchdog detects TX timeouts, but the driver verify
    the issue still exists before launching any recover method).
    
    In addition, recover from TX timeout in case of lost interrupt was added
    to the TX reporter recover method. The TX timeout recover from lost
    interrupt is not a new feature in the driver, this patch re-organize the
    functionality and move it to the TX reporter recovery flow.
    
    TX timeout example:
    (with auto_recover set to false, if set to true, the manual recover and
    diagnose sections are irrelevant)
    
    $cat /sys/kernel/debug/tracing/trace
    ...
    devlink_health_report: bus_name=pci dev_name=0000:00:09.0
    driver_name=mlx5_core reporter_name=TX: TX timeout on queue: 0, SQ: 0xd8a, CQ:
    0x406, SQ Cons: 0x2 SQ Prod: 0x2, usecs since last trans: 13972000
    
    $devlink health diagnose pci/0000:00:09 reporter TX
    SQ 0xd8a: HW state: 1, stopped: 1
    SQ 0xe44: HW state: 1, stopped: 0
    SQ 0xeb4: HW state: 1, stopped: 0
    SQ 0xf1f: HW state: 1, stopped: 0
    SQ 0xf80: HW state: 1, stopped: 0
    SQ 0xfe5: HW state: 1, stopped: 0
    
    $devlink health recover pci/0000:00:09 reporter TX
    $devlink health show
    pci/0000:00:09.0:
      name TX state healthy #err 1 #recover 1 last_dump_ts N/A dump_available false
        attributes:
            grace_period 500 auto_recover false
    
    Signed-off-by: Eran Ben Elisha <eranbe@mellanox.com>
    Reviewed-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/en/reporter.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en/reporter.h
@@ -11,5 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int mlx5e_tx_reporter_create(struct mlx5e_priv *priv);
 void mlx5e_tx_reporter_destroy(struct mlx5e_priv *priv);
 void mlx5e_tx_reporter_err_cqe(struct mlx5e_txqsq *sq);
+void mlx5e_tx_reporter_timeout(struct mlx5e_txqsq *sq);
 
 #endif
