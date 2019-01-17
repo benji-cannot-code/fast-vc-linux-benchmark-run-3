9b577c76785757fdda10150eddd6a43973f8d27a
Sabyasachi Gupta <sabyasachi.linux@gmail.com>
2019-01-17 21:49:05 +0530

    powerpc/cell: Remove duplicate header
    Remove linux/syscalls.h which is included more than once
    
    Signed-off-by: Sabyasachi Gupta <sabyasachi.linux@gmail.com>
    Acked-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/platforms/cell/spu_syscalls.c
+++ arch/powerpc/platforms/cell/spu_syscalls.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/syscalls.h>
 #include <linux/rcupdate.h>
 #include <linux/binfmts.h>
-#include <linux/syscalls.h>
 
 #include <asm/spu.h>
 
