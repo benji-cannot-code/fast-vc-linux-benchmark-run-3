5eafd98297a26b412a00c068ad11f96709e072ae
Gregory CLEMENT <gregory.clement@bootlin.com>
2019-01-18 16:01:46 +0100

    phy: armada375-usb2: switch to SPDX license identifier
    Adopt the SPDX license identifier headers to ease license compliance
    management.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/marvell/phy-armada375-usb2.c
+++ drivers/phy/marvell/phy-armada375-usb2.c
@@ -1,14 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * USB cluster support for Armada 375 platform.
  *
  * Copyright (C) 2014 Marvell
  *
  * Gregory CLEMENT <gregory.clement@free-electrons.com>
- *
- * This file is licensed under the terms of the GNU General Public
- * License version 2 or later. This program is licensed "as is"
- * without any warranty of any kind, whether express or implied.
  *
  * Armada 375 comes with an USB2 host and device controller and an
  * USB3 controller. The USB cluster control register allows to manage
