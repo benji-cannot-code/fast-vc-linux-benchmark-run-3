471517a4e29c4ea3bf5f6b0bbbc44500701cbd54
Tycho Andersen <tycho@tycho.ws>
2019-01-18 17:12:14 -0700

    selftest: include stdio.h in kselftest.h
    While playing around with a way to skip the seccomp get_metadata test, I
    noticed that this header uses printf() without defining it, leading to,
    
    ../kselftest.h: In function ‘ksft_print_header’:
    ../kselftest.h:61:3: warning: implicit declaration of function ‘printf’ [-Wimplicit-function-declaration]
       printf("TAP version 13\n");
       ^~~~~~
    ../kselftest.h:61:3: warning: incompatible implicit declaration of built-in function ‘printf’
    ../kselftest.h:61:3: note: include ‘<stdio.h>’ or provide a declaration of ‘printf’
    
    if user code doesn't also use printf.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Shuah Khan <shuah@kernel.org>

--- tools/testing/selftests/kselftest.h
+++ tools/testing/selftests/kselftest.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /* define kselftest exit codes */
 #define KSFT_PASS  0
