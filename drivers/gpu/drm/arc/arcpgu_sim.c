83dbf6eb9c7e6d662c279844179cfa2deb038ef0
Sam Ravnborg <sam@ravnborg.org>
2019-01-19 09:40:11 +0100

    drm/arcpgu: prepare for drmP.h removal from drm_modeset_helper.h
    The use of drmP.h is discouraged and removal of it from
    drm_modeset_helper.h caused arcgpu to fail to build.
    
    This patch introduce the necessary fixes to prepare for the
    drmP.h removal from drm_modeset_helper.h.
    List of include files sorted alphabetically.
    
    Build tested on arm x86 and arm allmodconfig.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Alexey Brodkin <abrodkin@synopsys.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190119084014.5355-4-sam@ravnborg.org

--- drivers/gpu/drm/arc/arcpgu_sim.c
+++ drivers/gpu/drm/arc/arcpgu_sim.c
@@ -15,8 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#include <drm/drm_crtc_helper.h>
 #include <drm/drm_atomic_helper.h>
+#include <drm/drm_crtc_helper.h>
+#include <drm/drm_device.h>
 
 #include "arcpgu.h"
 
