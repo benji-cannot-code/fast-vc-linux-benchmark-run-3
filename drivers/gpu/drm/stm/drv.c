d8d8f1edcc021987b2869286f934e244d0bdffea
Sam Ravnborg <sam@ravnborg.org>
2019-01-19 09:40:09 +0100

    drm/stm: prepare for drmP.h removal from drm_modeset_helper.h
    The use of drmP.h is discouraged and removal of it from
    drm_modeset_helper.h caused drm/stm to fail to build.
    
    This patch introduce the necessary fixes to prepare for the
    drmP.h removal from drm_modeset_helper.h.
    
    Build tested on arm and x86 allmodconfig
    
    v2:
    - sort list of include files
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    Cc: Yannick Fertre <yannick.fertre@st.com>
    Cc: Philippe Cornu <philippe.cornu@st.com>
    Cc: Vincent Abriou <vincent.abriou@st.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190119084014.5355-2-sam@ravnborg.org

--- drivers/gpu/drm/stm/drv.c
+++ drivers/gpu/drm/stm/drv.c
@@ -10,15 +10,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/component.h>
+#include <linux/dma-mapping.h>
+#include <linux/module.h>
 #include <linux/of_platform.h>
 
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc_helper.h>
-#include <drm/drm_fb_helper.h>
+#include <drm/drm_drv.h>
 #include <drm/drm_fb_cma_helper.h>
+#include <drm/drm_fb_helper.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
+#include <drm/drm_vblank.h>
 
 #include "ltdc.h"
 
