d86837c2e2c489588f0566f7bc8d57082b5b8f27
Wolfram Sang <wsa+renesas@sang-engineering.com>
2019-01-19 13:16:53 +0100

    i2c: algo-bit: include main i2c header
    We are using symbols from it, so we should include it directly. Found
    after sorting includes in a driver.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- include/linux/i2c-algo-bit.h
+++ include/linux/i2c-algo-bit.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_I2C_ALGO_BIT_H
 #define _LINUX_I2C_ALGO_BIT_H
 
+#include <linux/i2c.h>
+
 /* --- Defines for bit-adapters ---------------------------------------	*/
 /*
  * This struct contains the hw-dependent functions of bit-style adapters to
