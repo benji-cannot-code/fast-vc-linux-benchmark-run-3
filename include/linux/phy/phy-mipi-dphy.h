17233073da7eb61fe8cfc3e15e9177ed5d230464
Maxime Ripard <maxime.ripard@bootlin.com>
2019-01-21 16:45:46 +0100

    phy: dphy: Remove unused header
    The videomode.h header inclusion is an artifact from the patches
    development, remove it.
    
    Suggested-by: Sakari Ailus <sakari.ailus@iki.fi>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Maxime Ripard <maxime.ripard@bootlin.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- include/linux/phy/phy-mipi-dphy.h
+++ include/linux/phy/phy-mipi-dphy.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __PHY_MIPI_DPHY_H_
 #define __PHY_MIPI_DPHY_H_
 
-#include <video/videomode.h>
-
 /**
  * struct phy_configure_opts_mipi_dphy - MIPI D-PHY configuration set
  *
