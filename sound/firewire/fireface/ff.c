fa05788cd9a2ef2980d8d52d1b55c231d828c145
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-01-22 22:17:03 +0900

    ALSA: fireface: add model-dependent parameter for address to receive async transaction for MIDI messages
    Between former and latter models, destination address to receive
    asynchronous transactions for MIDI messages is different.
    
    This commit adds model-dependent parameter for the addresses.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/fireface/ff.c
+++ sound/firewire/fireface/ff.c
@@ -155,6 +155,7 @@ static const struct snd_ff_spec spec_ff800 = {
 	.protocol = &snd_ff_protocol_ff800,
 	.midi_high_addr = 0x000200000320ull,
 	.midi_addr_range = 12,
+	.midi_rx_addrs = {0x000080180000ull, 0},
 };
 
 static const struct snd_ff_spec spec_ff400 = {
@@ -166,6 +167,7 @@ static const struct snd_ff_spec spec_ff400 = {
 	.protocol = &snd_ff_protocol_ff400,
 	.midi_high_addr = 0x0000801003f4ull,
 	.midi_addr_range = SND_FF_MAXIMIM_MIDI_QUADS * 4,
+	.midi_rx_addrs = {0x000080180000ull, 0x000080190000ull},
 };
 
 static const struct snd_ff_spec spec_ucx = {
@@ -176,6 +178,7 @@ static const struct snd_ff_spec spec_ucx = {
 	.protocol = &snd_ff_protocol_latter,
 	.midi_high_addr = 0xffff00000034ull,
 	.midi_addr_range = 0x80,
+	.midi_rx_addrs = {0xffff00000030ull, 0xffff00000030ull},
 };
 
 static const struct ieee1394_device_id snd_ff_id_table[] = {
