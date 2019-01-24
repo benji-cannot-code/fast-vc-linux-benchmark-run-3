d12b32f5c281ca76ba2eed4fb4b351188545ff1c
Li Yu <liyu65@hisilicon.com>
2019-01-24 12:24:19 -0800

    dmaengine: k3dma: Delete axi_config
    Axi_config controls whether DMA resources can be accessed in non-secure
    mode, such as linux kernel. The register should be set by the bootloader
    stage and depends on the device.
    
    Thus, this patch removes axi_config from k3dma driver.
    
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Vinod Koul <vkoul@kernel.org>
    Cc: Tanglei Han <hantanglei@huawei.com>
    Cc: Zhuangluan Su <suzhuangluan@hisilicon.com>
    Cc: Ryan Grachek <ryan@edited.us>
    Cc: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Cc: dmaengine@vger.kernel.org
    Acked-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Li Yu <liyu65@hisilicon.com>
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>
    [jstultz: Minor tweaks to commit message]
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/k3dma.c
+++ drivers/dma/k3dma.c
@@ -53,8 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CX_SRC			0x814
 #define CX_DST			0x818
 #define CX_CFG			0x81c
-#define AXI_CFG			0x820
-#define AXI_CFG_DEFAULT		0x201201
 
 #define CX_LLI_CHAIN_EN		0x2
 #define CX_CFG_EN		0x1
@@ -170,7 +168,6 @@ static void k3_dma_set_desc(struct k3_dma_phy *phy, struct k3_desc_hw *hw)
 	writel_relaxed(hw->count, phy->base + CX_CNT0);
 	writel_relaxed(hw->saddr, phy->base + CX_SRC);
 	writel_relaxed(hw->daddr, phy->base + CX_DST);
-	writel_relaxed(AXI_CFG_DEFAULT, phy->base + AXI_CFG);
 	writel_relaxed(hw->config, phy->base + CX_CFG);
 }
 
