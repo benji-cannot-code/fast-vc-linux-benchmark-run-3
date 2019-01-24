076bbdb40b60a555804ceb7e661a00bf6270ea96
Kaike Wan <kaike.wan@intel.com>
2019-01-23 21:50:24 -0800

    IB/hfi1: Add TID RDMA retry timer
    This patch adds the TID RDMA retry timer to make sure that TID RDMA
    WRITE DATA packets for a segment are received successfully by the
    responder. This timer is generally armed when the last TID RDMA
    WRITE DATA packet for a segment is sent out and stopped when all
    TID RDMA DATA packets are acknowledged.
    
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Signed-off-by: Kaike Wan <kaike.wan@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hfi1/tid_rdma.h
+++ drivers/infiniband/hw/hfi1/tid_rdma.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HFI1_R_TID_RSC_TIMER      BIT(2)
 /* BIT(4) reserved for RVT_S_ACK_PENDING. */
 #define HFI1_S_TID_WAIT_INTERLCK  BIT(5)
+#define HFI1_S_TID_RETRY_TIMER    BIT(17)
 #define HFI1_R_TID_SW_PSN         BIT(19)
 
 /*
@@ -288,5 +289,8 @@ u32 hfi1_build_tid_rdma_write_ack(struct rvt_qp *qp, struct rvt_ack_entry *e,
 				  u32 *bth1, u32 *bth2);
 
 void hfi1_rc_rcv_tid_rdma_ack(struct hfi1_packet *packet);
+
+void hfi1_add_tid_retry_timer(struct rvt_qp *qp);
+void hfi1_del_tid_retry_timer(struct rvt_qp *qp);
 
 #endif /* HFI1_TID_RDMA_H */
