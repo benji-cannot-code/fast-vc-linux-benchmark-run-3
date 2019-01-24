b927f65261306c9f426e486f0caa29077f81c809
Kaike Wan <kaike.wan@intel.com>
2019-01-23 19:30:28 -0800

    IB/hfi1: Add static trace for flow and TID management functions
    This patch adds the static trace for the flow and TID management
    functions to help debugging in the filed.
    
    Reviewed-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Signed-off-by: Kaike Wan <kaike.wan@intel.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>

--- drivers/infiniband/hw/hfi1/trace.c
+++ drivers/infiniband/hw/hfi1/trace.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define CREATE_TRACE_POINTS
 #include "trace.h"
+#include "exp_rcv.h"
 
 static u8 __get_ib_hdr_len(struct ib_header *hdr)
 {
@@ -393,6 +394,21 @@ const char *print_u32_array(
 		trace_seq_printf(p, "%s%#x", i == 0 ? "" : " ", arr[i]);
 	trace_seq_putc(p, 0);
 	return ret;
+}
+
+u8 hfi1_trace_get_tid_ctrl(u32 ent)
+{
+	return EXP_TID_GET(ent, CTRL);
+}
+
+u16 hfi1_trace_get_tid_len(u32 ent)
+{
+	return EXP_TID_GET(ent, LEN);
+}
+
+u16 hfi1_trace_get_tid_idx(u32 ent)
+{
+	return EXP_TID_GET(ent, IDX);
 }
 
 __hfi1_trace_fn(AFFINITY);
