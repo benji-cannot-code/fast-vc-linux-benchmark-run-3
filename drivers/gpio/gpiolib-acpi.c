1d9c00be121144ca3542dd79f60174b4a70f0418
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-01-25 19:03:02 +0200

    gpiolib: acpi: Correct kernel doc of struct acpi_gpio_event
    The checker complains during build
    
    gpiolib-acpi.c:45: warning: Function parameter or member 'irq_requested' not described in 'acpi_gpio_event'
    
    because the typo in the field description.
    
    Fix the name to have documentation up-to-date.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-acpi.c
+++ drivers/gpio/gpiolib-acpi.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @irq:	  Linux IRQ number for the event, for request_ / free_irq
  * @irqflags:     flags to pass to request_irq when requesting the IRQ
  * @irq_is_wake:  If the ACPI flags indicate the IRQ is a wakeup source
- * @is_requested: True if request_irq has been done
+ * @irq_requested:True if request_irq has been done
  * @desc:	  gpio_desc for the GPIO pin for this event
  */
 struct acpi_gpio_event {
