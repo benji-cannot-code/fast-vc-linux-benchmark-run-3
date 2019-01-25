4dc95f840152cdb32e84501930700526ab96a585
YueHaibing <yuehaibing@huawei.com>
2019-01-25 10:00:14 +0800

    scsi: fnic: Remove set but not used variable 'vdev'
    Fixes gcc '-Wunused-but-set-variable' warning:
    
    drivers/scsi/fnic/vnic_wq.c: In function 'vnic_wq_alloc_bufs':
    drivers/scsi/fnic/vnic_wq.c:50:19: warning:
     variable 'vdev' set but not used [-Wunused-but-set-variable]
    
    drivers/scsi/fnic/vnic_rq.c: In function 'vnic_rq_alloc_bufs':
    drivers/scsi/fnic/vnic_rq.c:30:19: warning:
     variable 'vdev' set but not used [-Wunused-but-set-variable]
    
    Never used since introduction.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Satish Kharat <satishkh@cisco.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/fnic/vnic_rq.c
+++ drivers/scsi/fnic/vnic_rq.c
@@ -28,11 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static int vnic_rq_alloc_bufs(struct vnic_rq *rq)
 {
 	struct vnic_rq_buf *buf;
-	struct vnic_dev *vdev;
 	unsigned int i, j, count = rq->ring.desc_count;
 	unsigned int blks = VNIC_RQ_BUF_BLKS_NEEDED(count);
-
-	vdev = rq->vdev;
 
 	for (i = 0; i < blks; i++) {
 		rq->bufs[i] = kzalloc(VNIC_RQ_BUF_BLK_SZ, GFP_ATOMIC);
