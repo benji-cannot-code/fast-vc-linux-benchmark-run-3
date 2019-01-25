929fe243dfceec0e964c6ef8b53ac9fa6aeeda92
Russell King <rmk+kernel@armlinux.org.uk>
2019-01-25 10:28:20 +0000

    drm/armada: add plane size/location accessors
    Add accessors for getting the register values for the plane from the
    plane state.  This will allow us to generate the values when validating
    the plane rather than when programming, which allows us to fix the
    interlace handling without adding lots of additional handling in the
    update functions.
    
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- drivers/gpu/drm/armada/armada_plane.h
+++ drivers/gpu/drm/armada/armada_plane.h
@@ -2,6 +2,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ARMADA_PLANE_H
 #define ARMADA_PLANE_H
 
+#define armada_src_hw(state)	armada_rect_hw_fp(&(state)->src)
+#define armada_dst_yx(state)	armada_rect_yx(&(state)->dst)
+#define armada_dst_hw(state)	armada_rect_hw(&(state)->dst)
+
 void armada_drm_plane_calc(struct drm_plane_state *state, u32 addrs[2][3],
 	u16 pitches[3], bool interlaced);
 int armada_drm_plane_prepare_fb(struct drm_plane *plane,
