75cc8401e79df25da5b3f9758c21a7743d5c8d72
Sam Ravnborg <sam@ravnborg.org>
2019-01-26 13:25:23 +0100

    staging/vboxvideo: prepare for drmP.h removal from drm_modeset_helper.h
    The use of drmP.h is discouraged and removal of it from
    drm_modeset_helper.h caused vboxvideo to fail to build.
    
    This patch introduce the necessary fixes to prepare for the
    drmP.h removal from drm_modeset_helper.h.
    
    In the files touched sort the include files
    
    Build tested on x86 and arm allmodconfig / allyesconfig.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Hans de Goede <hdegoede@redhat.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190126122527.11647-2-sam@ravnborg.org

--- drivers/staging/vboxvideo/vbox_drv.c
+++ drivers/staging/vboxvideo/vbox_drv.c
@@ -8,11 +8,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *          Michael Thayer <michael.thayer@oracle.com,
  *          Hans de Goede <hdegoede@redhat.com>
  */
-#include <linux/module.h>
 #include <linux/console.h>
+#include <linux/module.h>
+#include <linux/pci.h>
 #include <linux/vt_kern.h>
 
 #include <drm/drm_crtc_helper.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_file.h>
+#include <drm/drm_ioctl.h>
 
 #include "vbox_drv.h"
 
