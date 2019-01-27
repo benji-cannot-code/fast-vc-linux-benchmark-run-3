6f1a2f99ab4906fc4cc69ed27ee05457c1f06f43
Axel Lin <axel.lin@ingics.com>
2019-01-27 16:51:22 +0800

    regulator: mcp16502: Include linux/gpio/consumer.h to fix build error
    Fix below build error:
    drivers/regulator/mcp16502.c: In function ‘mcp16502_gpio_set_mode’:
    drivers/regulator/mcp16502.c:135:3: error: implicit declaration of function ‘gpiod_set_value’; did you mean ‘gpio_set_value’? [-Werror=implicit-function-declaration]
       gpiod_set_value(mcp->lpm, 0);
       ^~~~~~~~~~~~~~~
       gpio_set_value
    drivers/regulator/mcp16502.c: In function ‘mcp16502_probe’:
    drivers/regulator/mcp16502.c:486:13: error: implicit declaration of function ‘devm_gpiod_get’; did you mean ‘devm_gpio_free’? [-Werror=implicit-function-declaration]
      mcp->lpm = devm_gpiod_get(dev, "lpm", GPIOD_OUT_LOW);
                 ^~~~~~~~~~~~~~
                 devm_gpio_free
    drivers/regulator/mcp16502.c:486:40: error: ‘GPIOD_OUT_LOW’ undeclared (first use in this function); did you mean ‘GPIOF_INIT_LOW’?
      mcp->lpm = devm_gpiod_get(dev, "lpm", GPIOD_OUT_LOW);
                                            ^~~~~~~~~~~~~
                                            GPIOF_INIT_LOW
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Acked-by: Nicolas Ferre <nicolas.ferre@microchip.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/mcp16502.c
+++ drivers/regulator/mcp16502.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regmap.h>
 #include <linux/regulator/driver.h>
 #include <linux/suspend.h>
+#include <linux/gpio/consumer.h>
 
 #define VDD_LOW_SEL 0x0D
 #define VDD_HIGH_SEL 0x3F
