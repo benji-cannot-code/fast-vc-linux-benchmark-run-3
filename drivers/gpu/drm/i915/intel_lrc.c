0e10672e44f49d72a81d36624c377b341f577fd6
Chris Wilson <chris@chris-wilson.co.uk>
2019-01-28 18:18:09 +0000

    drm/i915: Allocate a status page for each timeline
    Allocate a page for use as a status page by a group of timelines, as we
    only need a dword of storage for each (rounded up to the cacheline for
    safety) we can pack multiple timelines into the same page. Each timeline
    will then be able to track its own HW seqno.
    
    v2: Reuse the common per-engine HWSP for the solitary ringbuffer
    timeline, so that we do not have to emit (using per-gen specialised
    vfuncs) the breadcrumb into the distinct timeline HWSP and instead can
    keep on using the common MI_STORE_DWORD_INDEX. However, to maintain the
    sleight-of-hand for the global/per-context seqno switchover, we will
    store both temporarily (and so use a custom offset for the shared timeline
    HWSP until the switch over).
    
    v3: Keep things simple and allocate a page for each timeline, page
    sharing comes next.
    
    v4: I was caught repeating the same MI_STORE_DWORD_IMM over and over
    again in selftests.
    
    v5: And caught red handed copying create timeline + check.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190128181812.22804-3-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/intel_lrc.c
+++ drivers/gpu/drm/i915/intel_lrc.c
@@ -2207,10 +2207,14 @@ logical_ring_default_irqs(struct intel_engine_cs *engine)
 	engine->irq_keep_mask = GT_CONTEXT_SWITCH_INTERRUPT << shift;
 }
 
-static void
+static int
 logical_ring_setup(struct intel_engine_cs *engine)
 {
-	intel_engine_setup_common(engine);
+	int err;
+
+	err = intel_engine_setup_common(engine);
+	if (err)
+		return err;
 
 	/* Intentionally left blank. */
 	engine->buffer = NULL;
@@ -2220,6 +2224,8 @@ logical_ring_setup(struct intel_engine_cs *engine)
 
 	logical_ring_default_vfuncs(engine);
 	logical_ring_default_irqs(engine);
+
+	return 0;
 }
 
 static int logical_ring_init(struct intel_engine_cs *engine)
@@ -2268,7 +2274,9 @@ int logical_render_ring_init(struct intel_engine_cs *engine)
 {
 	int ret;
 
-	logical_ring_setup(engine);
+	ret = logical_ring_setup(engine);
+	if (ret)
+		return ret;
 
 	/* Override some for render ring. */
 	engine->init_context = gen8_init_rcs_context;
@@ -2297,7 +2305,11 @@ int logical_render_ring_init(struct intel_engine_cs *engine)
 
 int logical_xcs_ring_init(struct intel_engine_cs *engine)
 {
-	logical_ring_setup(engine);
+	int err;
+
+	err = logical_ring_setup(engine);
+	if (err)
+		return err;
 
 	return logical_ring_init(engine);
 }
@@ -2630,7 +2642,7 @@ static int execlists_context_deferred_alloc(struct i915_gem_context *ctx,
 		goto error_deref_obj;
 	}
 
-	timeline = i915_timeline_create(ctx->i915, ctx->name);
+	timeline = i915_timeline_create(ctx->i915, ctx->name, NULL);
 	if (IS_ERR(timeline)) {
 		ret = PTR_ERR(timeline);
 		goto error_deref_obj;
