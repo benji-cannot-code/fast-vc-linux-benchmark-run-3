6ddcd1db357b506eb20992244ff92a109f3e707a
Stanislav Fomichev <sdf@google.com>
2019-01-28 09:21:15 -0800

    selftests/bpf: skip sockmap in test_maps if kernel doesn't have support
    Use recently introduced bpf_probe_map_type() to skip test_sockmap()
    if map creation fails. The skipped test is indicated in the output.
    
    Example:
    
    test_sockmap SKIP (unsupported map type BPF_MAP_TYPE_SOCKMAP)
    Fork 1024 tasks to 'test_update_delete'
    ...
    test_sockmap SKIP (unsupported map type BPF_MAP_TYPE_SOCKMAP)
    Fork 1024 tasks to 'test_update_delete'
    ...
    test_maps: OK, 2 SKIPPED
    
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/testing/selftests/bpf/test_maps.c
+++ tools/testing/selftests/bpf/test_maps.c
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ENOTSUPP 524
 #endif
 
+static int skips;
+
 static int map_flags;
 
 #define CHECK(condition, tag, format...) ({				\
@@ -725,6 +727,15 @@ static void test_sockmap(int tasks, void *data)
 			    sizeof(key), sizeof(value),
 			    6, 0);
 	if (fd < 0) {
+		if (!bpf_probe_map_type(BPF_MAP_TYPE_SOCKMAP, 0)) {
+			printf("%s SKIP (unsupported map type BPF_MAP_TYPE_SOCKMAP)\n",
+			       __func__);
+			skips++;
+			for (i = 0; i < 6; i++)
+				close(sfd[i]);
+			return;
+		}
+
 		printf("Failed to create sockmap %i\n", fd);
 		goto out_sockmap;
 	}
@@ -1702,6 +1713,6 @@ int main(void)
 	map_flags = BPF_F_NO_PREALLOC;
 	run_all_tests();
 
-	printf("test_maps: OK\n");
+	printf("test_maps: OK, %d SKIPPED\n", skips);
 	return 0;
 }
