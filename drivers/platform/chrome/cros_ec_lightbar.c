cd1f0c6c1faa47b623512b6536d86575f872bcb0
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2019-01-29 10:38:22 +0100

    platform/chrome: cros_ec_lightbar: remove pr_fmt() define
    This driver no longer has any pr_{level} messages. Remove the pr_fmt().
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/platform/chrome/cros_ec_lightbar.c
+++ drivers/platform/chrome/cros_ec_lightbar.c
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 //
 // Copyright (C) 2014 Google, Inc.
 
-#define pr_fmt(fmt) "cros_ec_lightbar: " fmt
-
 #include <linux/ctype.h>
 #include <linux/delay.h>
 #include <linux/device.h>
