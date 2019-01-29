ab814c10890a7e94e54792667c1d64b780a595c8
Christoph Hellwig <hch@lst.de>
2019-01-29 19:13:07 +0100

    parisc: properly type the return value of parisc_walk_tree
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- drivers/parisc/iommu.h
+++ drivers/parisc/iommu.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct parisc_device;
 struct ioc;
 
-static inline void *parisc_walk_tree(struct device *dev)
+static inline struct pci_hba_data *parisc_walk_tree(struct device *dev)
 {
 	struct device *otherdev;
 
