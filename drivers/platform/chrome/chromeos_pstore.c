3bcc43a51dd71c5d25d605f40d1d679661edbd8b
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2019-01-29 10:38:12 +0100

    platform/chrome: cromeos_pstore: switch to SPDX identifier
    Adopt the SPDX license identifier headers to ease license compliance
    management. Also fix the module license mismatch and change the term
    'Chrome OS' for 'ChromeOS' to be coherent with other drivers.
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- drivers/platform/chrome/chromeos_pstore.c
+++ drivers/platform/chrome/chromeos_pstore.c
@@ -1,13 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/*
- *  chromeos_pstore.c - Driver to instantiate Chromebook ramoops device
- *
- *  Copyright (C) 2013 Google, Inc.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, version 2 of the License.
- */
+// SPDX-License-Identifier: GPL-2.0
+// Driver to instantiate Chromebook ramoops device.
+//
+// Copyright (C) 2013 Google, Inc.
 
 #include <linux/acpi.h>
 #include <linux/dmi.h>
@@ -139,5 +134,5 @@ static void __exit chromeos_pstore_exit(void)
 module_init(chromeos_pstore_init);
 module_exit(chromeos_pstore_exit);
 
-MODULE_DESCRIPTION("Chrome OS pstore module");
-MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("ChromeOS pstore module");
+MODULE_LICENSE("GPL v2");
