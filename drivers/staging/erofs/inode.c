406b480b7e5868234c7505d1eeb5133c739f68e9
Gao Xiang <gaoxiang25@huawei.com>
2019-01-29 16:35:20 +0800

    staging: erofs: complete POSIX ACL support
    Let's add .get_acl() to read the file's acl from its xattrs
    to make POSIX ACL usable.
    
    Here is the on-disk detail,
    fullname: system.posix_acl_access
    struct erofs_xattr_entry:
            .e_name_len = 0
            .e_name_index = EROFS_XATTR_INDEX_POSIX_ACL_ACCESS (2)
    
    fullname: system.posix_acl_default
    struct erofs_xattr_entry:
    	.e_name_len = 0
    	.e_name_index = EROFS_XATTR_INDEX_POSIX_ACL_DEFAULT (3)
    
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/erofs/inode.c
+++ drivers/staging/erofs/inode.c
@@ -288,6 +288,7 @@ const struct inode_operations erofs_generic_iops = {
 #ifdef CONFIG_EROFS_FS_XATTR
 	.listxattr = erofs_listxattr,
 #endif
+	.get_acl = erofs_get_acl,
 };
 
 const struct inode_operations erofs_symlink_iops = {
@@ -295,6 +296,7 @@ const struct inode_operations erofs_symlink_iops = {
 #ifdef CONFIG_EROFS_FS_XATTR
 	.listxattr = erofs_listxattr,
 #endif
+	.get_acl = erofs_get_acl,
 };
 
 const struct inode_operations erofs_fast_symlink_iops = {
@@ -302,5 +304,6 @@ const struct inode_operations erofs_fast_symlink_iops = {
 #ifdef CONFIG_EROFS_FS_XATTR
 	.listxattr = erofs_listxattr,
 #endif
+	.get_acl = erofs_get_acl,
 };
 
