309c02304a85ce1d46708d96e2039a09378ef97d
Micah Morton <mortonm@chromium.org>
2019-01-30 07:56:43 -0800

    LSM: SafeSetID: remove unused include
    The include for asm/syscall.h was needed in a prior version of lsm.c
    that checked return values of syscall_get_nr, but since we did away with
    that part of the code this include is no longer necessary. Take out this
    include since it breaks builds for certain architectures. We no longer
    have any arch-specific code in SafeSetID.
    
    Signed-off-by: Micah Morton <mortonm@chromium.org>
    Signed-off-by: James Morris <james.morris@microsoft.com>

--- security/safesetid/lsm.c
+++ security/safesetid/lsm.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) "SafeSetID: " fmt
 
-#include <asm/syscall.h>
 #include <linux/hashtable.h>
 #include <linux/lsm_hooks.h>
 #include <linux/module.h>
