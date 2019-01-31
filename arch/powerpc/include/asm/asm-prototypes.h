a435f5e2fbed9b974a3c545c27aaab326720ef91
Christophe Leroy <christophe.leroy@c-s.fr>
2019-01-31 10:09:02 +0000

    powerpc: 'current_set' is now a table of task_struct pointers
    The table of pointers 'current_set' has been used for retrieving
    the stack and current. They used to be thread_info pointers as
    they were pointing to the stack and current was taken from the
    'task' field of the thread_info.
    
    Now, the pointers of 'current_set' table are now both pointers
    to task_struct and pointers to thread_info.
    
    As they are used to get current, and the stack pointer is
    retrieved from current's stack field, this patch changes
    their type to task_struct, and renames secondary_ti to
    secondary_current.
    
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/asm-prototypes.h
+++ arch/powerpc/include/asm/asm-prototypes.h
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <uapi/asm/ucontext.h>
 
 /* SMP */
-extern struct thread_info *current_set[NR_CPUS];
-extern struct thread_info *secondary_ti;
+extern struct task_struct *current_set[NR_CPUS];
+extern struct task_struct *secondary_current;
 void start_secondary(void *unused);
 
 /* kexec */
