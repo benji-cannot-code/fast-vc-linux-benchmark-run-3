dc97b6ddbd13283501c18a52f0f10b78959e83e4
Stefan Roese <sr@denx.de>
2019-02-01 11:17:10 +0100

    staging: spi: mt7621: Remove superfluous SPI_BPW_MASK definition
    This patch removes the SPI_BPW_MASK definition as this is already
    available in include/linux/spi/spi.h.
    
    Signed-off-by: Stefan Roese <sr@denx.de>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Sankalp Negi <sankalpnegi2310@gmail.com>
    Cc: Chuanhong Guo <gch981213@gmail.com>
    Cc: John Crispin <john@phrozen.org>
    Reviewed-by: NeilBrown <neil@brown.name>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-spi/spi-mt7621.c
+++ drivers/staging/mt7621-spi/spi-mt7621.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/reset.h>
 #include <linux/spi/spi.h>
 
-#define SPI_BPW_MASK(bits) BIT((bits) - 1)
-
 #define DRIVER_NAME			"spi-mt7621"
 /* in usec */
 #define RALINK_SPI_WAIT_MAX_LOOP	2000
