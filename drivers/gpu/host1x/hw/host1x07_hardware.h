3163bd257d370de27468f13c2e390f26df53e996
Thierry Reding <treding@nvidia.com>
2019-02-01 14:28:23 +0100

    gpu: host1x: Program the channel stream ID
    When processing command streams, make sure the host1x's stream ID is
    programmed for the channel so that addresses are properly translated
    through the SMMU.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/host1x/hw/host1x07_hardware.h
+++ drivers/gpu/host1x/hw/host1x07_hardware.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/bitops.h>
 
+#include "hw_host1x07_channel.h"
 #include "hw_host1x07_uclass.h"
 #include "hw_host1x07_vm.h"
 #include "hw_host1x07_hypervisor.h"
