e77730c19e8d0e1f93e3284b9eddc69cbcc5dfd3
Paul Burton <paul.burton@mips.com>
2019-02-02 01:43:24 +0000

    MIPS: mm: Un-inline get_new_mmu_context
    In preparation for adding MMID support to get_new_mmu_context() which
    will increase the size of the function somewhat, move it from
    asm/mmu_context.h into a C file.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: linux-mips@vger.kernel.org

---
+++ arch/mips/mm/context.c
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/mmu_context.h>
+
+void get_new_mmu_context(struct mm_struct *mm)
+{
+	unsigned int cpu;
+	u64 asid;
+
+	cpu = smp_processor_id();
+	asid = asid_cache(cpu);
+
+	if (!((asid += cpu_asid_inc()) & cpu_asid_mask(&cpu_data[cpu]))) {
+		if (cpu_has_vtag_icache)
+			flush_icache_all();
+		local_flush_tlb_all();	/* start new asid cycle */
+	}
+
+	cpu_context(cpu, mm) = asid_cache(cpu) = asid;
+}
