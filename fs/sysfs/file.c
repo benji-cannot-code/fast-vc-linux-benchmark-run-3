c48fe9ca6c43815475cdacb93fdf208009841a19
Ondrej Mosnacek <omosnace@redhat.com>
2019-02-04 15:08:00 +0100

    sysfs: remove unused include of kernfs-internal.h
    This include is not needed (fs/sysfs/file.c builds just fine without
    it). Remove it.
    
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/sysfs/file.c
+++ fs/sysfs/file.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 
 #include "sysfs.h"
-#include "../kernfs/kernfs-internal.h"
 
 /*
  * Determine ktype->sysfs_ops for the given kernfs_node.  This function
