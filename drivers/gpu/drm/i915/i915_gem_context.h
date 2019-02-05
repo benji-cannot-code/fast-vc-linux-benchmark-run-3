96f232dd9c161d23c83b589e336a80439b609419
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2019-02-05 09:50:28 +0000

    drm/i915: Record the sseu configuration per-context & engine
    We want to expose the ability to reconfigure the slices, subslice and
    eu per context and per engine. To facilitate that, store the current
    configuration on the context for each engine, which is initially set
    to the device default upon creation.
    
    v2: record sseu configuration per context & engine (Chris)
    
    v3: introduce the i915_gem_context_sseu to store powergating
        programming, sseu_dev_info has grown quite a bit (Lionel)
    
    v4: rename i915_gem_sseu into intel_sseu (Chris)
        use to_intel_context() (Chris)
    
    v5: More to_intel_context() (Tvrtko)
        Switch intel_sseu from union to struct (Tvrtko)
        Move context default sseu in existing loop (Chris)
    
    v6: s/intel_sseu_from_device_sseu/intel_device_default_sseu/ (Tvrtko)
    
    Tvrtko Ursulin:
    
    v7:
     * Pass intel_sseu by pointer instead of value to make_rpcs.
     * Rebase for make_rpcs changes.
    
    v8:
     * Rebase for RPCS edit on pin.
    
    v9:
     * Rebase for context image setup changes.
    
    v10:
     * Rename dev_priv to i915. (Chris Wilson)
    
    v11:
     * Rebase.
    
    v12:
     * Rebase for IS_GEN changes.
    
    v13:
     * Rebase for RUNTIME_INFO.
    
    v14:
     * Rebase for intel_context_init.
    
    v15:
     * Rebase for drm-tip changes.
    
    v16:
     * Moved struct intel_sseu definition to i915_gem_context.h.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190205095032.22673-1-tvrtko.ursulin@linux.intel.com

--- drivers/gpu/drm/i915/i915_gem_context.h
+++ drivers/gpu/drm/i915/i915_gem_context.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "i915_gem.h"
 #include "i915_scheduler.h"
+#include "intel_device_info.h"
 
 struct pid;
 
@@ -52,6 +53,16 @@ struct intel_context;
 struct intel_context_ops {
 	void (*unpin)(struct intel_context *ce);
 	void (*destroy)(struct intel_context *ce);
+};
+
+/*
+ * Powergating configuration for a particular (context,engine).
+ */
+struct intel_sseu {
+	u8 slice_mask;
+	u8 subslice_mask;
+	u8 min_eus_per_subslice;
+	u8 max_eus_per_subslice;
 };
 
 /**
@@ -174,6 +185,9 @@ struct i915_gem_context {
 		int pin_count;
 
 		const struct intel_context_ops *ops;
+
+		/** sseu: Control eu/slice partitioning */
+		struct intel_sseu sseu;
 	} __engine[I915_NUM_ENGINES];
 
 	/** ring_size: size for allocating the per-engine ring buffer */
