3355b980d0fd888718fbf5989ee4bf5bd8e215db
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-02-05 13:51:16 -0300

    perf arm pmu: Add missing linux/string.h header
    It uses strstarts(), that is defined in linux/string.h but that was
    being including by sheer luck, indirectly, fix it.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Dongjiu Geng <gengdongjiu@huawei.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kim Phillips <kim.phillips@arm.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Suzuki Poulouse <suzuki.poulose@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Link: https://lkml.kernel.org/n/tip-vub5lp82wb7vy5wssfad0xu8@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/arm/util/pmu.c
+++ tools/perf/arch/arm/util/pmu.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <string.h>
 #include <linux/coresight-pmu.h>
 #include <linux/perf_event.h>
+#include <linux/string.h>
 
 #include "cs-etm.h"
 #include "arm-spe.h"
