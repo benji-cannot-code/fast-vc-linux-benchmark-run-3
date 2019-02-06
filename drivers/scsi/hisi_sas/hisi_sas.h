decf5aecb2dee2ac08708715456fd373b1e43c4a
Xiang Chen <chenxiang66@hisilicon.com>
2019-02-06 18:52:55 +0800

    scsi: hisi_sas: Use pci_irq_get_affinity() for v3 hw as experimental
    For auto-control irq affinity mode, choose the dq to deliver IO according
    to the current CPU.
    
    Then it decreases the performance regression that fio and CQ interrupts are
    processed on different node.
    
    For user control irq affinity mode, keep it as before.
    
    To realize it, also need to distinguish the usage of dq lock and sas_dev
    lock.
    
    We mark as experimental due to ongoing discussion on managed MSI IRQ
    during hotplug:
    https://marc.info/?l=linux-scsi&m=154876335707751&w=2
    
    We're almost at the point where we can expose multiple queues to the upper
    layer for SCSI MQ, but we need to sort out the per-HBA tags performance
    issue.
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -174,6 +174,7 @@ struct hisi_sas_port {
 
 struct hisi_sas_cq {
 	struct hisi_hba *hisi_hba;
+	const struct cpumask *pci_irq_mask;
 	struct tasklet_struct tasklet;
 	int	rd_point;
 	int	id;
@@ -196,6 +197,7 @@ struct hisi_sas_device {
 	enum sas_device_type	dev_type;
 	int device_id;
 	int sata_idx;
+	spinlock_t lock; /* For protecting slots */
 };
 
 struct hisi_sas_tmf_task {
@@ -218,6 +220,7 @@ struct hisi_sas_slot {
 	int	cmplt_queue_slot;
 	int	abort;
 	int	ready;
+	int	device_id;
 	void	*cmd_hdr;
 	dma_addr_t cmd_hdr_dma;
 	struct timer_list internal_abort_timer;
@@ -367,6 +370,7 @@ struct hisi_hba {
 	u32 intr_coal_count;	/* Interrupt count to coalesce */
 
 	int cq_nvecs;
+	unsigned int *reply_map;
 
 	/* debugfs memories */
 	u32 *debugfs_global_reg;
