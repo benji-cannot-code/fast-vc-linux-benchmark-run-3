8755b82485cdd48d12144e01744ba3d2b4dc0792
Miquel Raynal <miquel.raynal@bootlin.com>
2019-02-06 16:47:44 +0100

    mtd: rawnand: Clarify Kconfig entry MTD_NAND
    MTD_NAND is large and encloses much more than what the symbol is
    actually used for: raw NAND. Clarify the symbol by naming it
    MTD_RAW_NAND instead.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/tests/mtd_nandecctest.c
+++ drivers/mtd/tests/mtd_nandecctest.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * or detected.
  */
 
-#if IS_ENABLED(CONFIG_MTD_NAND)
+#if IS_ENABLED(CONFIG_MTD_RAW_NAND)
 
 struct nand_ecc_test {
 	const char *name;
