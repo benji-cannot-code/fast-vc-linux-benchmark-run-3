087f4238309ceea7a76b73c9ac03b0a63c6c77f2
Sasha Neftin <sasha.neftin@intel.com>
2019-02-06 09:48:37 +0200

    igc: Add multiple receive queues control supporting
    Enable the multi queues to receive.
    Program the direction of packets to specified queues according
    to the mode selected in the MRQC register.
    Multiple receive queues defined by filters and RSS for 4 queues.
    Enable/disable RSS hashing and also to enable multiple receive queues.
    This patch will allow further ethtool support development.
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igc/igc_regs.h
+++ drivers/net/ethernet/intel/igc/igc_regs.h
@@ -81,8 +81,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* MSI-X Table Register Descriptions */
 #define IGC_PBACL		0x05B68  /* MSIx PBA Clear - R/W 1 to clear */
 
+/* RSS registers */
+#define IGC_MRQC		0x05818 /* Multiple Receive Control - RW */
+
 /* Redirection Table - RW Array */
 #define IGC_RETA(_i)		(0x05C00 + ((_i) * 4))
+/* RSS Random Key - RW Array */
+#define IGC_RSSRK(_i)		(0x05C80 + ((_i) * 4))
 
 /* Receive Register Descriptions */
 #define IGC_RCTL		0x00100  /* Rx Control - RW */
