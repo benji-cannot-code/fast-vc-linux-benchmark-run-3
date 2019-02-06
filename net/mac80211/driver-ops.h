9e48c7c6f8376c3782c35d2f6f9777fcf5b35f7a
Sara Sharon <sara.sharon@intel.com>
2019-02-06 13:17:15 +0200

    mac80211: notify driver on subsequent CSA beacons
    Some drivers may want to track further the CSA beacons, for example
    to compensate for buggy APs that change the beacon count or quiet
    mode during CSA flow.
    
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/driver-ops.h
+++ net/mac80211/driver-ops.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
 * Portions of this file
 * Copyright(c) 2016 Intel Deutschland GmbH
-* Copyright (C) 2018 Intel Corporation
+* Copyright (C) 2018 - 2019 Intel Corporation
 */
 
 #ifndef __MAC80211_DRIVER_OPS
@@ -1065,6 +1065,21 @@ drv_abort_channel_switch(struct ieee80211_sub_if_data *sdata)
 
 	if (local->ops->abort_channel_switch)
 		local->ops->abort_channel_switch(&local->hw, &sdata->vif);
+}
+
+static inline void
+drv_channel_switch_rx_beacon(struct ieee80211_sub_if_data *sdata,
+			     struct ieee80211_channel_switch *ch_switch)
+{
+	struct ieee80211_local *local = sdata->local;
+
+	if (!check_sdata_in_driver(sdata))
+		return;
+
+	trace_drv_channel_switch_rx_beacon(local, sdata, ch_switch);
+	if (local->ops->channel_switch_rx_beacon)
+		local->ops->channel_switch_rx_beacon(&local->hw, &sdata->vif,
+						     ch_switch);
 }
 
 static inline int drv_join_ibss(struct ieee80211_local *local,
