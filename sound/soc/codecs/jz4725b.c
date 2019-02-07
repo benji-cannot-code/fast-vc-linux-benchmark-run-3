7203a5c3d626d4b9833cc610056fc7c4b08e001e
Paul Cercueil <paul@crapouillou.net>
2019-02-07 10:31:43 -0300

    ASoC: codecs: jz4725b: Use C++ style comments in header
    Change the header comment to use C++ style, so that it looks more
    consistent with the rest of ASoC.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/jz4725b.c
+++ sound/soc/codecs/jz4725b.c
@@ -1,10 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
-/*
- * JZ4725B CODEC driver
- *
- * Copyright (C) 2019, Paul Cercueil <paul@crapouillou.net>
- */
+//
+// JZ4725B CODEC driver
+//
+// Copyright (C) 2019, Paul Cercueil <paul@crapouillou.net>
 
 #include <linux/kernel.h>
 #include <linux/module.h>
