2c6cd252dd1e5d5558e2888d10167c048f2f09e7
Andrii Nakryiko <andriin@fb.com>
2019-02-07 11:29:24 -0800

    tools/bpf: add missing strings.h include
    Few files in libbpf are using bzero() function (defined in strings.h header), but
    don't include corresponding header. When libbpf is added as a dependency to pahole,
    this undeterministically causes warnings on some machines:
    
    bpf.c:225:2: warning: implicit declaration of function 'bzero' [-Wimplicit-function-declaration]
      bzero(&attr, sizeof(attr));
        ^~~~~
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Reported-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/lib/bpf/bpf.c
+++ tools/lib/bpf/bpf.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <stdlib.h>
+#include <strings.h>
 #include <memory.h>
 #include <unistd.h>
 #include <asm/unistd.h>
--- tools/lib/bpf/btf.c
+++ tools/lib/bpf/btf.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <errno.h>
 #include <linux/err.h>
