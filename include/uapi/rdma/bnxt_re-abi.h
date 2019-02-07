66f377ccb27168cc384d5354d1021fb3daeb2084
Devesh Sharma <devesh.sharma@broadcom.com>
2019-02-07 01:31:27 -0500

    RDMA/bnxt_re: Update kernel user abi to pass chip context
    User space verbs provider library would need chip context.  Changing the
    ABI to add chip version details in structure.  Furthermore, changing the
    kernel driver ucontext allocation code to initialize the abi structure
    with appropriate values.
    
    As suggested by community, appended the new fields at the bottom of the
    ABI structure and retaining to older fields as those were in the older
    versions.
    
    Keeping the ABI version at 1 and adding a new field in the ucontext
    response structure to hold the component mask.  The user space library
    should check pre-defined flags to figure out if a certain feature is
    supported on not.
    
    Signed-off-by: Devesh Sharma <devesh.sharma@broadcom.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- include/uapi/rdma/bnxt_re-abi.h
+++ include/uapi/rdma/bnxt_re-abi.h
@@ -45,6 +45,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define BNXT_RE_ABI_VERSION	1
 
+#define BNXT_RE_CHIP_ID0_CHIP_NUM_SFT		0x00
+#define BNXT_RE_CHIP_ID0_CHIP_REV_SFT		0x10
+#define BNXT_RE_CHIP_ID0_CHIP_MET_SFT		0x18
+
+enum {
+	BNXT_RE_UCNTX_CMASK_HAVE_CCTX = 0x1ULL
+};
+
 struct bnxt_re_uctx_resp {
 	__u32 dev_id;
 	__u32 max_qp;
@@ -52,6 +60,9 @@ struct bnxt_re_uctx_resp {
 	__u32 cqe_sz;
 	__u32 max_cqd;
 	__u32 rsvd;
+	__aligned_u64 comp_mask;
+	__u32 chip_id0;
+	__u32 chip_id1;
 };
 
 /*
