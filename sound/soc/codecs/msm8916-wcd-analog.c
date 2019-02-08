8a2d7e60d943ceade102014f00f2d3ebfa35e9a9
Johan Hovold <johan@kernel.org>
2019-02-08 16:16:55 +0100

    ASoC: msm8916-wcd-analog: add missing license information
    Add the missing license and copyright information which never made it
    into the analog driver when the original driver was split in two as part
    of the review process.
    
    Link: https://lkml.kernel.org/r/1465582725-30183-3-git-send-email-srinivas.kandagatla@linaro.org
    Fixes: 585e881e5b9e ("ASoC: codecs: Add msm8916-wcd analog codec")
    Cc: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/msm8916-wcd-analog.c
+++ sound/soc/codecs/msm8916-wcd-analog.c
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2016, The Linux Foundation. All rights reserved.
+
 #include <linux/module.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
