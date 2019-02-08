6c11360e7dbc07945786144bed902f5f0daea2e6
Mathieu Poirier <mathieu.poirier@linaro.org>
2019-02-08 15:35:43 -0700

    perf cs-etm: Add proper header file for symbols
    After 'commit e22c1c751140 ("perf thread: Don't include symbol.h,
    symbol_conf.h is enough")'
    
    Compilation of the perf tools is broken when using the functionality
    provided by the openCSD library:
    
    [...]
    
    ...                       timerfd: [ on  ]
    ...                  sched_getcpu: [ on  ]
    ...                           sdt: [ OFF ]
    ...                         setns: [ on  ]
    ...                    libopencsd: [ on  ]
    
    [...]
    
      CC       util/arm-spe.o
      CC       util/arm-spe-pkt-decoder.o
      CC       util/s390-cpumsf.o
      CC       util/cs-etm.o
      CC       util/parse-branch-options.o
    util/cs-etm.c: In function ‘cs_etm__mem_access’:
    util/cs-etm.c:297:24: error: storage size of ‘al’ isn’t known
      struct  addr_location al;
    
    And rightly so since file cs-etm.c doesn't include symbol.h, something
    that is rectified in this patch.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Suzuki K Poulouse <suzuki.poulose@arm.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Link: http://lkml.kernel.org/r/20190208223543.31836-1-mathieu.poirier@linaro.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/cs-etm.c
+++ tools/perf/util/cs-etm.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "machine.h"
 #include "map.h"
 #include "perf.h"
+#include "symbol.h"
 #include "thread.h"
 #include "thread_map.h"
 #include "thread-stack.h"
