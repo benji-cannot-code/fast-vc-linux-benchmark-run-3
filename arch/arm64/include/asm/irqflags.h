3e1e832a3676fad04156c9272fe648d1503ee110
Julien Thierry <julien.thierry@arm.com>
2019-02-08 09:36:48 +0000

    arm64: irqflags: Fix clang build warnings
    Clang complains when passing asm operands that are smaller than the
    registers they are mapped to:
    
    arch/arm64/include/asm/irqflags.h:50:10: warning: value size does not
    	match register size specified by the constraint and modifier
    	[-Wasm-operand-widths]
                    : "r" (GIC_PRIO_IRQON)
    
    Fix it by casting the affected input operands to a type of the correct
    size.
    
    Reported-by: Nathan Chancellor <natechancellor@gmail.com>
    Tested-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Julien Thierry <julien.thierry@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/irqflags.h
+++ arch/arm64/include/asm/irqflags.h
@@ -48,7 +48,7 @@ static inline void arch_local_irq_enable(void)
 		"dsb	sy",
 		ARM64_HAS_IRQ_PRIO_MASKING)
 		:
-		: "r" (GIC_PRIO_IRQON)
+		: "r" ((unsigned long) GIC_PRIO_IRQON)
 		: "memory");
 }
 
@@ -59,7 +59,7 @@ static inline void arch_local_irq_disable(void)
 		"msr_s  " __stringify(SYS_ICC_PMR_EL1) ", %0",
 		ARM64_HAS_IRQ_PRIO_MASKING)
 		:
-		: "r" (GIC_PRIO_IRQOFF)
+		: "r" ((unsigned long) GIC_PRIO_IRQOFF)
 		: "memory");
 }
 
@@ -92,7 +92,7 @@ static inline unsigned long arch_local_save_flags(void)
 			"csel	%0, %0, %2, eq",
 			ARM64_HAS_IRQ_PRIO_MASKING)
 		: "=&r" (flags), "+r" (daif_bits)
-		: "r" (GIC_PRIO_IRQOFF)
+		: "r" ((unsigned long) GIC_PRIO_IRQOFF)
 		: "memory");
 
 	return flags;
