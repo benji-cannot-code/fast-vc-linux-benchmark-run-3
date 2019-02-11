192476a45aed200ae5bb47934128b8785fa047d0
Kalle Valo <kvalo@codeaurora.org>
2019-02-11 18:48:04 +0200

    ath10k: make wmi_service_name() warn about missing service ids
    When adding a new value to enum wmi_service it's very easy to miss that the new
    value should be also added to wmi_service_name() mapping function. Modify the
    function so that GCC can now warn about this:
    
    drivers/net/wireless/ath/ath10k/wmi.h:385:2: warning: enumeration value 'WMI_SERVICE_FOO' not handled in switch [-Wswitch]
    
    And also add a reminder to the enum.
    
    Thanks to Jouni Malinen for the idea.
    
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/wmi.h
+++ drivers/net/wireless/ath/ath10k/wmi.h
@@ -213,6 +213,8 @@ enum wmi_service {
 	WMI_SERVICE_PER_PACKET_SW_ENCRYPT,
 	WMI_SERVICE_REPORT_AIRTIME,
 
+	/* Remember to add the new value to wmi_service_name()! */
+
 	/* keep last */
 	WMI_SERVICE_MAX,
 };
@@ -379,7 +381,7 @@ enum wmi_10_4_service {
 	WMI_10_4_SERVICE_REPORT_AIRTIME,
 };
 
-static inline char *wmi_service_name(int service_id)
+static inline char *wmi_service_name(enum wmi_service service_id)
 {
 #define SVCSTR(x) case x: return #x
 
@@ -502,11 +504,13 @@ static inline char *wmi_service_name(int service_id)
 	SVCSTR(WMI_SERVICE_PER_PACKET_SW_ENCRYPT);
 	SVCSTR(WMI_SERVICE_REPORT_AIRTIME);
 
-	default:
+	case WMI_SERVICE_MAX:
 		return NULL;
 	}
 
 #undef SVCSTR
+
+	return NULL;
 }
 
 #define WMI_SERVICE_IS_ENABLED(wmi_svc_bmap, svc_id, len) \
