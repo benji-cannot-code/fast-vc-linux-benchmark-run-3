bc59664a5d63c4cb3e1f757dab5aba81654d7957
Linus Walleij <linus.walleij@linaro.org>
2019-02-11 09:26:01 +0100

    mmc: sdhci-bcm-kona: Drop unused includes
    The SDHCI BCM Kona driver uses slot GPIO helpers and does not
    make any use of <linux/gpio.h> or <linux/of_gpio.h> so drop these
    surplus includes.
    
    Cc: Jisheng Zhang <Jisheng.Zhang@synaptics.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-bcm-kona.c
+++ drivers/mmc/host/sdhci-bcm-kona.c
@@ -19,12 +19,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/mmc/host.h>
 #include <linux/io.h>
-#include <linux/gpio.h>
 #include <linux/clk.h>
 #include <linux/regulator/consumer.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-#include <linux/of_gpio.h>
 #include <linux/mmc/slot-gpio.h>
 
 #include "sdhci-pltfm.h"
