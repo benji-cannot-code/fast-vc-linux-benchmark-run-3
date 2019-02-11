ac9bf6741a6797c9c195c926b0205cf7785a77d1
Uma Shankar <uma.shankar@intel.com>
2019-02-11 19:20:21 +0530

    drm/i915/glk: Fix degamma lut programming
    Fixed the glk degamma lut programming which currently
    was hard coding a linear lut all the time, making degamma
    block of glk basically a pass through.
    
    Currently degamma lut for glk is assigned as 0 in platform
    configuration. Updated the same to 33 as per the hardware
    capability. IGT tests for degamma were getting skipped due to
    this, spotted by Swati.
    
    ToDo: The current gamma/degamm lut ABI has just 16bit for each
    color component. This is not enough for GLK+, since input
    precision is increased to 3.16 which will need 19bit entries.
    
    v2: Added Matt's RB.
    
    v3: Changed uint32_t to u32.
    
    v4: Fixed Maarten's review comment
    
    Credits-to: Swati Sharma <swati2.sharma@intel.com>
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1549893025-21837-2-git-send-email-uma.shankar@intel.com

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -74,7 +74,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		   .gamma_lut_tests = DRM_COLOR_LUT_NON_DECREASING, \
 	}
 #define GLK_COLORS \
-	.color = { .degamma_lut_size = 0, .gamma_lut_size = 1024, \
+	.color = { .degamma_lut_size = 33, .gamma_lut_size = 1024, \
 		   .degamma_lut_tests = DRM_COLOR_LUT_NON_DECREASING | \
 					DRM_COLOR_LUT_EQUAL_CHANNELS, \
 	}
