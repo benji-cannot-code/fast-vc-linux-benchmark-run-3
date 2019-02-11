603fc52bf064a137cc1c0d208d2e5f00d5145093
Kalle Valo <kvalo@codeaurora.org>
2019-02-11 18:48:06 +0200

    ath10k: change wmi.h to include only ieee80211.h
    wmi.h does not use anything from mac80211.h so change it to include only
    ieee80211.h.
    
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/wmi.h
+++ drivers/net/wireless/ath/ath10k/wmi.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _WMI_H_
 
 #include <linux/types.h>
-#include <net/mac80211.h>
+#include <linux/ieee80211.h>
 
 /*
  * This file specifies the WMI interface for the Unified Software
