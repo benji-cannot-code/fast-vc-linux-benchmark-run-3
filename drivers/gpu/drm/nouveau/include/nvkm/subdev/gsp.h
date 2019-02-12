6e1d4328ea4613bd53d281aab86c3b23d18498e0
Ben Skeggs <bskeggs@redhat.com>
2019-02-12 22:28:13 +1000

    drm/nouveau/gsp/gv100-: instantiate GSP falcon
    We need this for Turing ACR, but it's present from Volta onwards.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/gsp.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/gsp.h
@@ -6,5 +6,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct nvkm_gsp {
 	struct nvkm_subdev subdev;
+	u32 addr;
+
+	struct nvkm_falcon *falcon;
 };
+
+int gv100_gsp_new(struct nvkm_device *, int, struct nvkm_gsp **);
 #endif
