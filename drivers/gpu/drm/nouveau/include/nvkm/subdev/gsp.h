631be6649db0aba6993a2982a2f86de2565b0e87
Ben Skeggs <bskeggs@redhat.com>
2019-02-12 22:28:13 +1000

    drm/nouveau/core: define GSP subdev
    Exact meaning of the acronym is unknown, but we need this for Turing ACR.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

---
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/gsp.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __NVKM_GSP_H__
+#define __NVKM_GSP_H__
+#define nvkm_gsp(p) container_of((p), struct nvkm_gsp, subdev)
+#include <core/subdev.h>
+
+struct nvkm_gsp {
+	struct nvkm_subdev subdev;
+};
+#endif
