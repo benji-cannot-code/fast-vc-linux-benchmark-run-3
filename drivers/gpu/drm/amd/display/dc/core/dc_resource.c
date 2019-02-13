35a927ac76ed998d599b1012163825d766e0711b
Eric Bernstein <eric.bernstein@amd.com>
2019-02-13 17:56:38 -0500

    drm/amd/display: Free DCN version of stream encoder
    Cross a TODO item off the list. Cleanup SIGNAL_TYPE_HDMI_FRL, it's
    not currently supported.
    
    Signed-off-by: Eric Bernstein <eric.bernstein@amd.com>
    Reviewed-by: Dmytro Laktyushkin <Dmytro.Laktyushkin@amd.com>
    Acked-by: Leo Li <sunpeng.li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_resource.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_resource.c
@@ -261,6 +261,7 @@ bool resource_construct(
 			pool->stream_enc_count++;
 		}
 	}
+
 	dc->caps.dynamic_audio = false;
 	if (pool->audio_count < pool->stream_enc_count) {
 		dc->caps.dynamic_audio = true;
