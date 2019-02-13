01e40422ed04d0cb960b655da64a35e9e333c6c8
Ulf Hansson <ulf.hansson@linaro.org>
2019-02-13 18:42:06 +0100

    mmc: core: Convert mmc_regulator_get_ocrmask() to static
    The only left user of mmc_regulator_get_ocrmask() is the mmc core itself.
    Therefore, let's drop the export and turn it into static.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/regulator.c
+++ drivers/mmc/core/regulator.c
@@ -59,7 +59,7 @@ static int mmc_ocrbitnum_to_vdd(int vdd_bit, int *min_uV, int *max_uV)
  * regulator.  This would normally be called before registering the
  * MMC host adapter.
  */
-int mmc_regulator_get_ocrmask(struct regulator *supply)
+static int mmc_regulator_get_ocrmask(struct regulator *supply)
 {
 	int			result = 0;
 	int			count;
@@ -91,7 +91,6 @@ int mmc_regulator_get_ocrmask(struct regulator *supply)
 
 	return result;
 }
-EXPORT_SYMBOL_GPL(mmc_regulator_get_ocrmask);
 
 /**
  * mmc_regulator_set_ocr - set regulator to match host->ios voltage
