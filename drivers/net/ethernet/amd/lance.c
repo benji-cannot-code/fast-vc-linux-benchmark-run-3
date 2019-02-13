980f13c8d791c4e562d8773db85a27760f1b4feb
Yang Wei <yang.wei9@zte.com.cn>
2019-02-13 23:14:54 +0800

    net: amd: replace dev_kfree_skb_irq by dev_consume_skb_irq for drop profiles
    dev_consume_skb_irq() should be called when skb xmit done. It makes
    drop profiles(dropwatch, perf) more friendly.
    
    Signed-off-by: Yang Wei <yang.wei9@zte.com.cn>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/amd/lance.c
+++ drivers/net/ethernet/amd/lance.c
@@ -1085,7 +1085,7 @@ static irqreturn_t lance_interrupt(int irq, void *dev_id)
 				/* We must free the original skb if it's not a data-only copy
 				   in the bounce buffer. */
 				if (lp->tx_skbuff[entry]) {
-					dev_kfree_skb_irq(lp->tx_skbuff[entry]);
+					dev_consume_skb_irq(lp->tx_skbuff[entry]);
 					lp->tx_skbuff[entry] = NULL;
 				}
 				dirty_tx++;
