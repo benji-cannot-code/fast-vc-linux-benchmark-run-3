48f471a6ba61881d85e7a3dd3e705a7dc4346f28
Mike Leach <mike.leach@linaro.org>
2019-02-13 14:41:50 +0100

    ARM: 8836/1: drivers: amba: Update component matching to use the CoreSight UCI values.
    The patches provide an update of amba_device and matching code to handle
    the additional registers required for the Class 0x9 (CoreSight) UCI.
    
    The *data pointer in the amba_id is used by the driver to provide extended
    ID register values for matching.
    
    CoreSight components where PID/CID pair is currently sufficient for
    unique identification need not provide this additional information.
    
    Signed-off-by: Mike Leach <mike.leach@linaro.org>
    Reviewed-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Tested-by: Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- include/linux/amba/bus.h
+++ include/linux/amba/bus.h
@@ -59,6 +59,10 @@ struct amba_cs_uci_id {
 	void *data;
 };
 
+/* define offsets for registers used by UCI */
+#define UCI_REG_DEVTYPE_OFFSET	0xFCC
+#define UCI_REG_DEVARCH_OFFSET	0xFBC
+
 struct clk;
 
 struct amba_device {
@@ -66,6 +70,8 @@ struct amba_device {
 	struct resource		res;
 	struct clk		*pclk;
 	unsigned int		periphid;
+	unsigned int		cid;
+	struct amba_cs_uci_id	uci;
 	unsigned int		irq[AMBA_NR_IRQS];
 	char			*driver_override;
 };
