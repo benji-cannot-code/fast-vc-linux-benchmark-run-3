119b8f03a7bbfe91e3102b44bc4af1be6c3024fb
Dave Martin <Dave.Martin@arm.com>
2019-02-14 11:49:36 +0000

    KVM: arm64: Add missing #include of <linux/string.h> in guest.c
    arch/arm64/kvm/guest.c uses the string functions, but the
    corresponding header is not included.
    
    We seem to get away with this for now, but for completeness this
    patch adds the #include, in preparation for adding yet more
    memset() calls.
    
    Signed-off-by: Dave Martin <Dave.Martin@arm.com>
    Tested-by: zhang.lei <zhang.lei@jp.fujitsu.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/kvm/guest.c
+++ arch/arm64/kvm/guest.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/kvm_host.h>
 #include <linux/module.h>
+#include <linux/string.h>
 #include <linux/vmalloc.h>
 #include <linux/fs.h>
 #include <kvm/arm_psci.h>
