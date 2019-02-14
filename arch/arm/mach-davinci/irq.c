c130cf4aae66172983ab97410d4c329a52d56a41
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2019-02-14 15:52:15 +0100

    ARM: davinci: aintc: remove unnecessary includes
    These includes are no longer required. Remove them.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Reviewed-by: David Lechner <david@lechnology.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/irq.c
+++ arch/arm/mach-davinci/irq.c
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/irqdomain.h>
 
-#include <mach/hardware.h>
-#include <mach/cputype.h>
-#include <mach/common.h>
-#include <asm/mach/irq.h>
 #include <asm/exception.h>
 
 #define DAVINCI_AINTC_FIQ_REG0		0x00
