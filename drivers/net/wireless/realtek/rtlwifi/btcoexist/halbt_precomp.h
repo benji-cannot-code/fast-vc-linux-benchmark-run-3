dd0824ca3e2dfed0b9bc1e696841bea1545defa3
Larry Finger <Larry.Finger@lwfinger.net>
2019-02-14 16:36:38 -0600

    rtlwifi: btcoexist: Remove CamelCase variable
    File halbt_precomp.h contains the only CamelCase value, namely
    GetDefaultAdapter, but that macro is never used, thus it is deleted.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/btcoexist/halbt_precomp.h
+++ drivers/net/wireless/realtek/rtlwifi/btcoexist/halbt_precomp.h
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "halbtc8821a2ant.h"
 #include "halbtc8821a1ant.h"
 
-#define GetDefaultAdapter(padapter)	padapter
-
 #define BIT0	0x00000001
 #define BIT1	0x00000002
 #define BIT2	0x00000004
