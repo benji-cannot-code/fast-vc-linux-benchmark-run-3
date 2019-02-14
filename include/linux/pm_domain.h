8605f696ffdfdba8178c3e934f892f8f7128f6b3
Douglas Anderson <dianders@chromium.org>
2019-02-14 10:12:49 -0800

    PM / Domains: Mark "name" const in dev_pm_domain_attach_by_name()
    As of the patch ("PM / Domains: Mark "name" const in
    genpd_dev_pm_attach_by_name()") it's clear that the name in
    dev_pm_domain_attach_by_name() can be const.  Mark it as so.  This
    allows drivers to pass in a name that was declared "const" in a
    driver.
    
    Fixes: 27dceb81f445 ("PM / Domains: Introduce dev_pm_domain_attach_by_name()")
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -342,7 +342,7 @@ int dev_pm_domain_attach(struct device *dev, bool power_on);
 struct device *dev_pm_domain_attach_by_id(struct device *dev,
 					  unsigned int index);
 struct device *dev_pm_domain_attach_by_name(struct device *dev,
-					    char *name);
+					    const char *name);
 void dev_pm_domain_detach(struct device *dev, bool power_off);
 void dev_pm_domain_set(struct device *dev, struct dev_pm_domain *pd);
 #else
@@ -356,7 +356,7 @@ static inline struct device *dev_pm_domain_attach_by_id(struct device *dev,
 	return NULL;
 }
 static inline struct device *dev_pm_domain_attach_by_name(struct device *dev,
-							  char *name)
+							  const char *name)
 {
 	return NULL;
 }
