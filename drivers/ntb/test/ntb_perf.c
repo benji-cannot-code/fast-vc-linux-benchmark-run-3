4e2931b0679110d45976064ef4d6242c2ffb776a
Sanjay R Mehta <sanju.mehta@amd.com>
2019-02-15 09:20:30 +0000

    NTB: ntb_perf: Disable NTB link after clearing peer XLAT registers
    If ntb link disabled before clearing peer's XLAT register, the clearing
    won't have any effect since the link is already down. So modified the
    sequence so that the link is down only towards the end of the function
    after clearing the XLAT register
    
    Signed-off-by: Sanjay R Mehta <sanju.mehta@amd.com>
    Acked-by: Allen Hubbe <allenbh@gmail.com>
    Acked-by: Logan Gunthorpe <logang@deltatee.com>
    Acked-by: Serge Semin <fancer.lancer@gmail.com>
    Signed-off-by: Jon Mason <jdmason@kudzu.us>

--- drivers/ntb/test/ntb_perf.c
+++ drivers/ntb/test/ntb_perf.c
@@ -735,8 +735,6 @@ static void perf_disable_service(struct perf_ctx *perf)
 {
 	int pidx;
 
-	ntb_link_disable(perf->ntb);
-
 	if (perf->cmd_send == perf_msg_cmd_send) {
 		u64 inbits;
 
@@ -753,6 +751,8 @@ static void perf_disable_service(struct perf_ctx *perf)
 
 	for (pidx = 0; pidx < perf->pcnt; pidx++)
 		flush_work(&perf->peers[pidx].service);
+
+	ntb_link_disable(perf->ntb);
 }
 
 /*==============================================================================
