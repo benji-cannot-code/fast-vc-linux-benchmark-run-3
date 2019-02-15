925841c7952fb49ea5d0e41a4129899774448ef9
Lendacky, Thomas <Thomas.Lendacky@amd.com>
2019-02-15 17:26:33 +0000

    crypto: ccp - Update driver messages to remove some confusion
    The current content of some of the driver messages and the way that they
    are issued results in some confusion, especially in the area of the PSP
    as it relates to SEV support. If SEV is not supported, a message is issued
    that says "psp initialization failed." This makes it seem like there was
    a problem, when in fact, the PSP support is just disabled if SEV is not
    supported.
    
    Update the driver to check SEV support a bit earlier and issue a debug-
    level message if SEV is not supported, followed by a debug-level message
    that the PSP is disabled. This way you will only see PSP messages if SEV
    is supported or if debug information is desired. Also, remove the overall
    "enabled" and "disabled" messages for the driver and rely on the CCP and
    PSP support to issue component-specific messages.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccp/sp-pci.c
+++ drivers/crypto/ccp/sp-pci.c
@@ -227,8 +227,6 @@ static int sp_pci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (ret)
 		goto e_err;
 
-	dev_notice(dev, "enabled\n");
-
 	return 0;
 
 e_err:
@@ -247,8 +245,6 @@ static void sp_pci_remove(struct pci_dev *pdev)
 	sp_destroy(sp);
 
 	sp_free_irqs(sp);
-
-	dev_notice(dev, "disabled\n");
 }
 
 #ifdef CONFIG_PM
