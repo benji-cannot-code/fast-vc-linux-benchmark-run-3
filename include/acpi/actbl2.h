c45dd48e71cbc6f5022b05646eed9f479eaba38f
Erik Schmauss <erik.schmauss@intel.com>
2019-02-15 13:36:11 -0800

    ACPICA: ACPI 6.3: Add Trigger order to PCC Identifier structure in PDTT
    ACPICA commit b11446d8b47805c2637a2286aca34b717ec6b5be
    
    Link: https://github.com/acpica/acpica/commit/b11446d8
    Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/actbl2.h
+++ include/acpi/actbl2.h
@@ -1362,6 +1362,7 @@ struct acpi_pdtt_channel {
 
 #define ACPI_PDTT_RUNTIME_TRIGGER           (1)
 #define ACPI_PDTT_WAIT_COMPLETION           (1<<1)
+#define ACPI_PDTT_TRIGGER_ORDER             (1<<2)
 
 /*******************************************************************************
  *
