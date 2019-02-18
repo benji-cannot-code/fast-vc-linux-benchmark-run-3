9c4b7acc78960b7ec11e43dbe3068eb3a7c5c0e0
Martin Schwidefsky <schwidefsky@de.ibm.com>
2019-02-18 16:51:28 +0100

    s390/ftrace: use HAVE_FUNCTION_GRAPH_RET_ADDR_PTR
    Make the call chain more reliable by tagging the ftrace stack entries
    with the stack pointer that is associated with the return address.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- arch/s390/include/asm/ftrace.h
+++ arch/s390/include/asm/ftrace.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MCOUNT_RETURN_FIXUP	18
 #endif
 
+#define HAVE_FUNCTION_GRAPH_RET_ADDR_PTR
+
 #ifndef __ASSEMBLY__
 
 #ifdef CONFIG_CC_IS_CLANG
