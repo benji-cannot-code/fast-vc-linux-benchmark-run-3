4ef40c7ba55e4d0c08b04112dd88a30b1ff38b17
Wei Yongjun <weiyongjun1@huawei.com>
2019-02-18 07:46:53 +0000

    ASoC: cs35l36: Make some symbols static
    Fixes the following sparse warnings:
    
    sound/soc/codecs/cs35l36.c:135:20: warning:
     symbol 'cs35l36_reg' was not declared. Should it be static?
    sound/soc/codecs/cs35l36.c:248:6: warning:
     symbol 'cs35l36_readable_reg' was not declared. Should it be static?
    sound/soc/codecs/cs35l36.c:398:6: warning:
     symbol 'cs35l36_precious_reg' was not declared. Should it be static?
    sound/soc/codecs/cs35l36.c:410:6: warning:
     symbol 'cs35l36_volatile_reg' was not declared. Should it be static?
    
    Fixes: 6ba9dd6c893b ("ASoC: cs35l36: Add support for Cirrus CS35L36 Amplifier")
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Acked-by: James Schulman <james.schulman@cirrus.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs35l36.c
+++ sound/soc/codecs/cs35l36.c
@@ -132,7 +132,7 @@ static const struct cs35l36_pll_config cs35l36_pll_sysclk[] = {
 	{27000000,	0x3F, 0x0A},
 };
 
-struct reg_default cs35l36_reg[] = {
+static struct reg_default cs35l36_reg[] = {
 	{CS35L36_TESTKEY_CTRL,			0x00000000},
 	{CS35L36_USERKEY_CTL,			0x00000000},
 	{CS35L36_OTP_CTRL1,			0x00002460},
@@ -245,7 +245,7 @@ struct reg_default cs35l36_reg[] = {
 	{CS35L36_PAC_INT7_CTRL,			0x00000001},
 };
 
-bool cs35l36_readable_reg(struct device *dev, unsigned int reg)
+static bool cs35l36_readable_reg(struct device *dev, unsigned int reg)
 {
 	switch (reg) {
 	case CS35L36_SW_RESET:
@@ -395,7 +395,7 @@ bool cs35l36_readable_reg(struct device *dev, unsigned int reg)
 	}
 }
 
-bool cs35l36_precious_reg(struct device *dev, unsigned int reg)
+static bool cs35l36_precious_reg(struct device *dev, unsigned int reg)
 {
 	switch (reg) {
 	case CS35L36_TESTKEY_CTRL:
@@ -407,7 +407,7 @@ bool cs35l36_precious_reg(struct device *dev, unsigned int reg)
 	}
 }
 
-bool cs35l36_volatile_reg(struct device *dev, unsigned int reg)
+static bool cs35l36_volatile_reg(struct device *dev, unsigned int reg)
 {
 	switch (reg) {
 	case CS35L36_SW_RESET:
