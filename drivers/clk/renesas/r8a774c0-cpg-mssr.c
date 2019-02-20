3fda5082a3eb614a1c48ab861b0867c2043cd6a9
Fabrizio Castro <fabrizio.castro@bp.renesas.com>
2019-02-20 16:09:20 +0000

    clk: renesas: r8a774c0: Fix LAST_DT_CORE_CLK
    Enum LAST_DT_CORE_CLK needs updating as R8A774C0_CLK_CANFD
    was recently added and it's the core clock with the highest
    index.
    
    Signed-off-by: Fabrizio Castro <fabrizio.castro@bp.renesas.com>
    Reviewed-by: Chris Paterson <chris.paterson2@renesas.com>
    Fixes: 2a6efbc6da5d248c ("clk: renesas: r8a774c0: Add missing CANFD clock")
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/renesas/r8a774c0-cpg-mssr.c
+++ drivers/clk/renesas/r8a774c0-cpg-mssr.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 enum clk_ids {
 	/* Core Clock Outputs exported to DT */
-	LAST_DT_CORE_CLK = R8A774C0_CLK_CPEX,
+	LAST_DT_CORE_CLK = R8A774C0_CLK_CANFD,
 
 	/* External Input Clocks */
 	CLK_EXTAL,
