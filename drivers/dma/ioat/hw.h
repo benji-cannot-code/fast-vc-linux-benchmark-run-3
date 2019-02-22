a1d9801891f5f19c087a8b180f1fac7bb3f505c9
Dave Jiang <dave.jiang@intel.com>
2019-02-22 09:59:54 -0700

    dmaengine: ioatdma: Add Snow Ridge ioatdma device id
    Add Snowridge Xeon-D ioatdma PCI device id. Also applies for Icelake
    SP Xeon. This introduces ioatdma v3.4 platform. Also bumping driver version
    to 5.0 since we are adding additional code for 3.4 support.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/ioat/hw.h
+++ drivers/dma/ioat/hw.h
@@ -67,6 +67,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PCI_DEVICE_ID_INTEL_IOAT_SKX	0x2021
 
+#define PCI_DEVICE_ID_INTEL_IOAT_ICX	0x0b00
+
 #define IOAT_VER_1_2            0x12    /* Version 1.2 */
 #define IOAT_VER_2_0            0x20    /* Version 2.0 */
 #define IOAT_VER_3_0            0x30    /* Version 3.0 */
