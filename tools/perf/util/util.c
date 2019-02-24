b20fb96becdb20c8a36761c2ea38344f823999cf
Jiri Olsa <jolsa@kernel.org>
2019-02-24 20:06:37 +0100

    perf tools: Add depth checking to rm_rf
    Adding depth argument to rm_rf (and renaming it to rm_rf_depth) to
    specify the depth we will go searching for files to remove.
    
    It will be used to specify single depth for perf.data directory removal
    in following patch.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Alexey Budankov <alexey.budankov@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lkml.kernel.org/r/20190224190656.30163-2-jolsa@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/util.c
+++ tools/perf/util/util.c
@@ -118,7 +118,12 @@ int mkdir_p(char *path, mode_t mode)
 	return (stat(path, &st) && mkdir(path, mode)) ? -1 : 0;
 }
 
-int rm_rf(const char *path)
+/*
+ * The depth specify how deep the removal will go.
+ * 0       - will remove only files under the 'path' directory
+ * 1 .. x  - will dive in x-level deep under the 'path' directory
+ */
+static int rm_rf_depth(const char *path, int depth)
 {
 	DIR *dir;
 	int ret;
@@ -156,7 +161,7 @@ int rm_rf(const char *path)
 		}
 
 		if (S_ISDIR(statbuf.st_mode))
-			ret = rm_rf(namebuf);
+			ret = depth ? rm_rf_depth(namebuf, depth - 1) : 0;
 		else
 			ret = unlink(namebuf);
 	}
@@ -166,6 +171,11 @@ int rm_rf(const char *path)
 		return ret;
 
 	return rmdir(path);
+}
+
+int rm_rf(const char *path)
+{
+	return rm_rf_depth(path, INT_MAX);
 }
 
 /* A filter which removes dot files */
