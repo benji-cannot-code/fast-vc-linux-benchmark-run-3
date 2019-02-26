ac4c173c29c9dcdfad6c749d1f3e4889ba9538bb
Chris Wilson <chris@chris-wilson.co.uk>
2019-02-26 09:49:20 +0000

    drm/i915: Remove access to global seqno in the HWSP
    Stop accessing the HWSP to read the global seqno, and stop tracking the
    mirror in the engine's execution timeline -- it is unused.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190226094922.31617-2-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_gpu_error.h
+++ drivers/gpu/drm/i915/i915_gpu_error.h
@@ -95,8 +95,6 @@ struct i915_gpu_state {
 		u32 cpu_ring_head;
 		u32 cpu_ring_tail;
 
-		u32 last_seqno;
-
 		/* Register state */
 		u32 start;
 		u32 tail;
@@ -109,7 +107,6 @@ struct i915_gpu_state {
 		u32 bbstate;
 		u32 instpm;
 		u32 instps;
-		u32 seqno;
 		u64 bbaddr;
 		u64 acthd;
 		u32 fault_reg;
