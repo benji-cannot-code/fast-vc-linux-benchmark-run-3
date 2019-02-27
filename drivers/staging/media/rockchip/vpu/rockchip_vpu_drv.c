5c8692dab3297b6921eddea4d1890c93cb2827bc
YueHaibing <yuehaibing@huawei.com>
2019-02-27 00:57:23 -0500

    media: rockchip-vpu: Remove duplicated include from rockchip_vpu_drv.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/staging/media/rockchip/vpu/rockchip_vpu_drv.c
+++ drivers/staging/media/rockchip/vpu/rockchip_vpu_drv.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-event.h>
 #include <media/v4l2-mem2mem.h>
 #include <media/videobuf2-core.h>
-#include <media/videobuf2-core.h>
 #include <media/videobuf2-vmalloc.h>
 
 #include "rockchip_vpu_common.h"
