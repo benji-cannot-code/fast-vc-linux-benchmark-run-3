48cf568f7822316b775ec149c1b50191b08682fb
Anders Roxell <anders.roxell@linaro.org>
2019-03-01 15:45:09 +0100

    fbdev: omap2: fix warnings in dss core
    Commit 60d2fa0dad06 ("fbdev: omap2: no need to check return value of
    debugfs_create functions") changed the declaration of the return value
    of function dss_debugfs_create_file() and the following two warnings
    appeared:
    
    drivers/video/fbdev/omap2/omapfb/dss/core.c: In function ‘dss_debugfs_create_file’:
    drivers/video/fbdev/omap2/omapfb/dss/core.c:139:9: warning: ‘return’ with a value,
     in function returning void
      return 0;
             ^
    drivers/video/fbdev/omap2/omapfb/dss/core.c: In function ‘omap_dss_probe’:
    drivers/video/fbdev/omap2/omapfb/dss/core.c:172:6: warning: unused variable ‘r’
     [-Wunused-variable]
      int r;
          ^
    
    Rework so function dss_debugfs_create_file() that is declared to return
    void don't 'return 0' and remove the declaration of the unused variable
    'r'.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>

--- drivers/video/fbdev/omap2/omapfb/dss/core.c
+++ drivers/video/fbdev/omap2/omapfb/dss/core.c
@@ -137,7 +137,6 @@ static inline void dss_uninitialize_debugfs(void)
 }
 void dss_debugfs_create_file(const char *name, void (*write)(struct seq_file *))
 {
-	return 0;
 }
 #endif /* CONFIG_FB_OMAP2_DSS_DEBUGFS */
 
@@ -170,8 +169,6 @@ static struct notifier_block omap_dss_pm_notif_block = {
 
 static int __init omap_dss_probe(struct platform_device *pdev)
 {
-	int r;
-
 	core.pdev = pdev;
 
 	dss_features_init(omapdss_get_version());
