0c2966cefb9a71c7b722dad15a3ef73851afec12
Rushikesh S Kadam <rushikesh.s.kadam@intel.com>
2019-03-01 13:50:54 +0530

    mfd: cros_ec: Instantiate properly CrOS ISH MCU device
    Integrated Sensor Hub (ISH) is also a MCU running EC
    having feature bit EC_FEATURE_ISH. Instantiate it as
    a special CrOS EC device with device name 'cros_ish'.
    
    Signed-off-by: Rushikesh S Kadam <rushikesh.s.kadam@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Acked-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CROS_EC_DEV_NAME "cros_ec"
 #define CROS_EC_DEV_PD_NAME "cros_pd"
+#define CROS_EC_DEV_ISH_NAME "cros_ish"
 
 /*
  * The EC is unresponsive for a time after a reboot command.  Add a
