1a1cb4daf398abc9e079fb473de33aea6adc3d8e
Vadim Pasternak <vadimp@mellanox.com>
2019-03-03 09:12:08 +0000

    mlxsw: core: Move ethtool module callbacks to a common location
    Move the implementation of ethtool module callbacks - .get_module_info()
    and .get_module_eeprom() - to a common location to allow reuse by the
    different mlxsw drivers.
    
    Signed-off-by: Vadim Pasternak <vadimp@mellanox.com>
    Acked-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/core_env.h
+++ drivers/net/ethernet/mellanox/mlxsw/core_env.h
@@ -8,4 +8,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int mlxsw_env_module_temp_thresholds_get(struct mlxsw_core *core, int module,
 					 int off, int *temp);
 
+int mlxsw_env_get_module_info(struct mlxsw_core *mlxsw_core, int module,
+			      struct ethtool_modinfo *modinfo);
+
+int mlxsw_env_get_module_eeprom(struct net_device *netdev,
+				struct mlxsw_core *mlxsw_core, int module,
+				struct ethtool_eeprom *ee, u8 *data);
+
 #endif
