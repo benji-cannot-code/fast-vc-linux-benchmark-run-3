ce6b751398c363c0d0b8505ef998aee3da76c731
Michael S. Tsirkin <mst@redhat.com>
2019-03-05 15:48:33 -0800

    mm/page_poison: update comment after code moved
    mm/debug-pagealloc.c is no more, so of course header now needs to be
    updated.  This seems like something checkpatch should be able to catch -
    worth looking into?
    
    Link: http://lkml.kernel.org/r/20190207191113.14039-1-mst@redhat.com
    Fixes: 8823b1dbc05f ("mm/page_poison.c: enable PAGE_POISONING as a separate option")
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/poison.h
+++ include/linux/poison.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define TIMER_ENTRY_STATIC	((void *) 0x300 + POISON_POINTER_DELTA)
 
-/********** mm/debug-pagealloc.c **********/
+/********** mm/page_poison.c **********/
 #ifdef CONFIG_PAGE_POISONING_ZERO
 #define PAGE_POISON 0x00
 #else
