21bee3f07b99983d4e1abfa8512fc56184f771d1
Sabyasachi Gupta <sabyasachi.linux@gmail.com>
2019-03-06 21:59:04 +0530

    selftest/x86/mpx-dig.c: Remove duplicate header
    Remove duplicate header which is included twice.
    
    Signed-off-by: Sabyasachi Gupta <sabyasachi.linux@gmail.com>
    Signed-off-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Shuah Khan <shuah@kernel.org>

--- tools/testing/selftests/x86/mpx-dig.c
+++ tools/testing/selftests/x86/mpx-dig.c
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <sys/mman.h>
 #include <string.h>
 #include <fcntl.h>
