7725846bea783bddde2e04e76db41619d6d265c5
Neil Armstrong <narmstrong@baylibre.com>
2019-03-07 15:14:55 +0100

    clk: meson-g12a: add PCIE PLL clocks
    Add the PCIe reference clock feeding the USB3 + PCIE combo PHY.
    
    This PLL needs a very precise register sequence to permit to be locked,
    thus using the specific clk-pll pcie ops.
    
    The PLL is then followed by :
    - a fixed /2 divider
    - a 5-bit 1-based divider
    - a final /2 divider
    
    This reference clock is fixed to 100MHz, thus only a single PLL setup
    is added.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Acked-by: Jerome Brunet <jbrunet@baylibre.com>
    Link: https://lkml.kernel.org/r/20190307141455.23879-4-narmstrong@baylibre.com

--- drivers/clk/meson/g12a.h
+++ drivers/clk/meson/g12a.h
@@ -187,8 +187,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_CPU_CLK_AXI			195
 #define CLKID_CPU_CLK_TRACE_DIV			196
 #define CLKID_CPU_CLK_TRACE			197
+#define CLKID_PCIE_PLL_DCO			198
+#define CLKID_PCIE_PLL_DCO_DIV2			199
+#define CLKID_PCIE_PLL_OD			200
 
-#define NR_CLKS					198
+#define NR_CLKS					202
 
 /* include the CLKIDs that have been made part of the DT binding */
 #include <dt-bindings/clock/g12a-clkc.h>
