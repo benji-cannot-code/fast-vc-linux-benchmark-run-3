0a7b0cb550e31ccc2f74cf7b33c77e5936f06f60
Davidlohr Bueso <dave@stgolabs.net>
2019-03-08 10:17:47 -0800

    perf bench: Add basic syscall benchmark
    The usefulness of having a standard way of testing syscall performance
    has come up from time to time[0]. Furthermore, some of our testing
    machinery (such as 'mmtests') already makes use of a simplified version
    of the microbenchmark. This patch mainly takes the same idea to measure
    syscall throughput compatible with 'perf-bench' via getppid(2), yet
    without any of the additional template stuff from Ingo's version (based
    on numa.c). The code is identical to what mmtests uses.
    
    [0] https://lore.kernel.org/lkml/20160201074156.GA27156@gmail.com/
    
    Committer notes:
    
    Add mising stdlib.h and unistd.h to get the prototypes for exit() and
    getppid().
    
    Committer testing:
    
      $ perf bench
      Usage:
      	perf bench [<common options>] <collection> <benchmark> [<options>]
    
              # List of all available benchmark collections:
    
               sched: Scheduler and IPC benchmarks
             syscall: System call benchmarks
                 mem: Memory access benchmarks
                numa: NUMA scheduling and MM benchmarks
               futex: Futex stressing benchmarks
               epoll: Epoll stressing benchmarks
           internals: Perf-internals benchmarks
                 all: All benchmarks
    
      $
      $ perf bench syscall
    
              # List of available benchmarks for collection 'syscall':
    
               basic: Benchmark for basic getppid(2) calls
                 all: Run all syscall benchmarks
    
      $ perf bench syscall basic
      # Running 'syscall/basic' benchmark:
      # Executed 10000000 getppid() calls
           Total time: 3.679 [sec]
    
             0.367957 usecs/op
              2717708 ops/sec
      $ perf bench syscall all
      # Running syscall/basic benchmark...
      # Executed 10000000 getppid() calls
           Total time: 3.644 [sec]
    
             0.364456 usecs/op
              2743815 ops/sec
    
      $
    
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Acked-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Acked-by: Mel Gorman <mgorman@techsingularity.net>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: http://lore.kernel.org/lkml/20190308181747.l36zqz2avtivrr3c@linux-r8p5
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-bench.c
+++ tools/perf/builtin-bench.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Available benchmark collection list:
  *
  *  sched ... scheduler and IPC performance
+ *  syscall ... System call performance
  *  mem   ... memory access performance
  *  numa  ... NUMA scheduling and MM performance
  *  futex ... Futex performance
@@ -48,6 +49,12 @@ static struct bench sched_benchmarks[] = {
 	{ "pipe",	"Benchmark for pipe() between two processes",	bench_sched_pipe	},
 	{ "all",	"Run all scheduler benchmarks",		NULL			},
 	{ NULL,		NULL,						NULL			}
+};
+
+static struct bench syscall_benchmarks[] = {
+	{ "basic",	"Benchmark for basic getppid(2) calls",		bench_syscall_basic	},
+	{ "all",	"Run all syscall benchmarks",			NULL			},
+	{ NULL,		NULL,						NULL			},
 };
 
 static struct bench mem_benchmarks[] = {
@@ -91,6 +98,7 @@ struct collection {
 
 static struct collection collections[] = {
 	{ "sched",	"Scheduler and IPC benchmarks",			sched_benchmarks	},
+	{ "syscall",	"System call benchmarks",			syscall_benchmarks	},
 	{ "mem",	"Memory access benchmarks",			mem_benchmarks		},
 #ifdef HAVE_LIBNUMA_SUPPORT
 	{ "numa",	"NUMA scheduling and MM benchmarks",		numa_benchmarks		},
