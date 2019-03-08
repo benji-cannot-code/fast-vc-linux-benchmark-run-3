d78f049228f5af83c2c3cae57396cc0e46d2688b
Chris Wilson <chris@chris-wilson.co.uk>
2019-03-08 13:25:22 +0000

    drm/i915: Introduce intel_context.pin_mutex for pin management
    Introduce a mutex to start locking the HW contexts independently of
    struct_mutex, with a view to reducing the coarse struct_mutex. The
    intel_context.pin_mutex is used to guard the transition to and from being
    pinned on the gpu, and so is required before starting to build any
    request. The intel_context will then remain pinned until the request
    completes, but the mutex can be released immediately unpin completion of
    pinning the context.
    
    A slight variant of the above is used by per-context sseu that wants to
    inspect the pinned status of the context, and requires that it remains
    stable (either !pinned or pinned) across its operation. By using the
    pin_mutex to serialise operations while pin_count==0, we can take that
    pin_mutex for stabilise the boolean pin status.
    
    v2: for Tvrtko!
    * Improved commit message.
    * Dropped _gpu suffix from gen8_modify_rpcs_gpu.
    v3: Repair the locking for sseu selftests
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190308132522.21573-7-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/intel_context_types.h
+++ drivers/gpu/drm/i915/intel_context_types.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __INTEL_CONTEXT_TYPES__
 
 #include <linux/list.h>
+#include <linux/mutex.h>
 #include <linux/rbtree.h>
 #include <linux/types.h>
 
@@ -39,14 +40,19 @@ struct intel_context {
 	struct i915_gem_context *gem_context;
 	struct intel_engine_cs *engine;
 	struct intel_engine_cs *active;
+
 	struct list_head active_link;
 	struct list_head signal_link;
 	struct list_head signals;
+
 	struct i915_vma *state;
 	struct intel_ring *ring;
+
 	u32 *lrc_reg_state;
 	u64 lrc_desc;
-	int pin_count;
+
+	atomic_t pin_count;
+	struct mutex pin_mutex; /* guards pinning and associated on-gpuing */
 
 	/**
 	 * active_tracker: Active tracker for the external rq activity
