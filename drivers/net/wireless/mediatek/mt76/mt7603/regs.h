72d2b72e115aa084bafb2cf0e1ec1e28c59ebdca
Felix Fietkau <nbd@nbd.name>
2019-03-08 19:50:21 +0100

    mt76: mt7603: fix initialization of max rx length
    The previous version only accidentally disabled A-MSDU deaggregation by
    using the wrong mask for rx length configuration, which left previous
    length value in place.
    Fix the length and initialize the register completely to keep A-MSDU
    de-aggregation remaining disabled
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7603/regs.h
+++ drivers/net/wireless/mediatek/mt76/mt7603/regs.h
@@ -234,6 +234,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MT_WF_DMA(ofs)			(MT_WF_DMA_BASE + (ofs))
 
 #define MT_DMA_DCR0			MT_WF_DMA(0x000)
+#define MT_DMA_DCR0_MAX_RX_LEN		GENMASK(15, 0)
+#define MT_DMA_DCR0_DAMSDU		BIT(16)
+#define MT_DMA_DCR0_RX_VEC_DROP		BIT(17)
+
 #define MT_DMA_DCR1			MT_WF_DMA(0x004)
 
 #define MT_DMA_FQCR0			MT_WF_DMA(0x008)
