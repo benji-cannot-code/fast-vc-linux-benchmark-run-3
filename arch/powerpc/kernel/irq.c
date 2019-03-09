3d4a64b8b189696ca1260ab08638d33db6431f04
Christophe Leroy <christophe.leroy@c-s.fr>
2019-03-09 18:47:27 +0100

    powerpc/irq: drop __irq_offset_value
    This patch drops__irq_offset_value which has not been used since
    commit 9c4cb8251513 ("powerpc: Remove use of CONFIG_PPC_MERGE")
    
    This removes a sparse warning.
    
    Fixes: 9c4cb8251513 ("powerpc: Remove use of CONFIG_PPC_MERGE")
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/irq.c
+++ arch/powerpc/kernel/irq.c
@@ -82,10 +82,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 DEFINE_PER_CPU_SHARED_ALIGNED(irq_cpustat_t, irq_stat);
 EXPORT_PER_CPU_SYMBOL(irq_stat);
 
-int __irq_offset_value;
-
 #ifdef CONFIG_PPC32
-EXPORT_SYMBOL(__irq_offset_value);
 atomic_t ppc_n_lost_interrupts;
 
 #ifdef CONFIG_TAU_INT
