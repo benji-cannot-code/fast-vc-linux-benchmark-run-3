5ca95c09e49e379905bd337f7bda0f4023fc0b53
Arnd Bergmann <arnd@arndb.de>
2019-03-09 15:32:56 -0800

    Input: msm-vibrator - use correct gpio header
    When CONFIG_GPIOLIB is not set, we get a couple of build
    errors during test building:
    
    drivers/input/misc/msm-vibrator.c: In function 'msm_vibrator_start':
    drivers/input/misc/msm-vibrator.c:79:3: error: implicit declaration of function 'gpiod_set_value_cansleep'; did you mean 'gpio_set_value_cansleep'? [-Werror=implicit-function-declaration]
       gpiod_set_value_cansleep(vibrator->enable_gpio, 1);
       ^~~~~~~~~~~~~~~~~~~~~~~~
       gpio_set_value_cansleep
    drivers/input/misc/msm-vibrator.c: In function 'msm_vibrator_probe':
    drivers/input/misc/msm-vibrator.c:176:26: error: implicit declaration of function 'devm_gpiod_get'; did you mean 'devm_gpio_free'? [-Werror=implicit-function-declaration]
      vibrator->enable_gpio = devm_gpiod_get(&pdev->dev, "enable",
                              ^~~~~~~~~~~~~~
                              devm_gpio_free
    drivers/input/misc/msm-vibrator.c:177:13: error: 'GPIOD_OUT_LOW' undeclared (first use in this function); did you mean 'GPIOF_INIT_LOW'?
                 GPIOD_OUT_LOW);
                 ^~~~~~~~~~~~~
                 GPIOF_INIT_LOW
    drivers/input/misc/msm-vibrator.c:177:13: note: each undeclared identifier is reported only once for each function it appears in
    
    This is easy to avoid when we use gpio/consumer.h as the documented interface.
    
    Fixes: 0f681d09e66e ("Input: add new vibrator driver for various MSM SOCs")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/misc/msm-vibrator.c
+++ drivers/input/misc/msm-vibrator.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk.h>
 #include <linux/err.h>
-#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/input.h>
 #include <linux/io.h>
 #include <linux/module.h>
