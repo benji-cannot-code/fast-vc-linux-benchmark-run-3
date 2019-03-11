2faff54b96b3e865ba5a5d2e64680c1f1616c69a
Youri Querry <youri.querry_1@nxp.com>
2019-03-11 17:30:48 +0000

    soc: fsl: dpio: Increase timeout for QBMan Management Commands
    The timeout for QBMan Management Commands can falsely trigger on a
    busy system. This patch doubles the timeout to avoid the
    false error reports
    
    Signed-off-by: Youri Querry <youri.querry_1@nxp.com>
    Signed-off-by: Roy Pledge <roy.pledge@nxp.com>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/soc/fsl/dpio/qbman-portal.h
+++ drivers/soc/fsl/dpio/qbman-portal.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
 /*
  * Copyright (C) 2014-2016 Freescale Semiconductor, Inc.
- * Copyright 2016 NXP
+ * Copyright 2016-2019 NXP
  *
  */
 #ifndef __FSL_QBMAN_PORTAL_H
@@ -429,7 +429,7 @@ static inline int qbman_swp_CDAN_set_context_enable(struct qbman_swp *s,
 static inline void *qbman_swp_mc_complete(struct qbman_swp *swp, void *cmd,
 					  u8 cmd_verb)
 {
-	int loopvar = 1000;
+	int loopvar = 2000;
 
 	qbman_swp_mc_submit(swp, cmd, cmd_verb);
 
