32c1d4e9b4c3d0d3fc1d64ec98ded57e4234a6b8
Filipe Manana <fdmanana@suse.com>
2019-03-11 13:10:56 +0000

    Btrfs: remove no longer used 'sync' member from transaction handle
    Commit db2462a6ad3d ("btrfs: don't run delayed refs in the end transaction
    logic") removed its last use, so now it does absolutely nothing, therefore
    remove it.
    
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Filipe Manana <fdmanana@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -2166,7 +2166,6 @@ int btrfs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		inode_unlock(inode);
 		goto out;
 	}
-	trans->sync = true;
 
 	ret = btrfs_log_dentry_safe(trans, dentry, start, end, &ctx);
 	if (ret < 0) {
