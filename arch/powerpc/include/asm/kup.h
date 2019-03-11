78c4e623c6e44ce47108ec8d520b02435d5c897b
Christophe Leroy <christophe.leroy@c-s.fr>
2019-03-11 08:30:35 +0000

    powerpc/32s: Implement Kernel Userspace Execution Prevention.
    To implement Kernel Userspace Execution Prevention, this patch
    sets NX bit on all user segments on kernel entry and clears NX bit
    on all user segments on kernel exit.
    
    Note that powerpc 601 doesn't have the NX bit, so KUEP will not
    work on it. A warning is displayed at startup.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/kup.h
+++ arch/powerpc/include/asm/kup.h
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_PPC_8xx
 #include <asm/nohash/32/kup-8xx.h>
 #endif
+#ifdef CONFIG_PPC_BOOK3S_32
+#include <asm/book3s/32/kup.h>
+#endif
 
 #ifdef __ASSEMBLY__
 #ifndef CONFIG_PPC_KUAP
