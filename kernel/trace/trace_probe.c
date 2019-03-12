7bfa601668dd9816e08cf8033ea19ba1018cf8f9
Valdis Klētnieks <valdis.kletnieks@vt.edu>
2019-03-12 04:52:58 -0400

    tracing/probes: Make reserved_field_names static
    sparse complains:
      CHECK   kernel/trace/trace_probe.c
    kernel/trace/trace_probe.c:16:12: warning: symbol 'reserved_field_names' was not declared. Should it be static?
    
    Yes, it should be static.
    
    Link: http://lkml.kernel.org/r/2478.1552380778@turing-police
    
    Signed-off-by: Valdis Kletnieks <valdis.kletnieks@vt.edu>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_probe.c
+++ kernel/trace/trace_probe.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "trace_probe.h"
 
-const char *reserved_field_names[] = {
+static const char *reserved_field_names[] = {
 	"common_type",
 	"common_flags",
 	"common_preempt_count",
