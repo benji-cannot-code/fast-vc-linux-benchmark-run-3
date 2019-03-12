87b471cc4d1c4dcf2430629a8ebab4cdb6df76d2
Ben Gardon <bgardon@google.com>
2019-03-12 11:45:59 -0700

    kvm: mmu: Used range based flushing in slot_handle_level_range
    Replace kvm_flush_remote_tlbs with kvm_flush_remote_tlbs_with_address
    in slot_handle_level_range. When range based flushes are not enabled
    kvm_flush_remote_tlbs_with_address falls back to kvm_flush_remote_tlbs.
    
    This changes the behavior of many functions that indirectly use
    slot_handle_level_range, iff the range based flushes are enabled. The
    only potential problem I see with this is that kvm->tlbs_dirty will be
    cleared less often, however the only caller of slot_handle_level_range that
    checks tlbs_dirty is kvm_mmu_notifier_invalidate_range_start which
    checks it and does a kvm_flush_remote_tlbs after calling
    kvm_unmap_hva_range anyway.
    
    Tested: Ran all kvm-unit-tests on a Intel Haswell machine with and
    	without this patch. The patch introduced no new failures.
    
    Signed-off-by: Ben Gardon <bgardon@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu.c
+++ arch/x86/kvm/mmu.c
@@ -5527,7 +5527,9 @@ slot_handle_level_range(struct kvm *kvm, struct kvm_memory_slot *memslot,
 
 		if (need_resched() || spin_needbreak(&kvm->mmu_lock)) {
 			if (flush && lock_flush_tlb) {
-				kvm_flush_remote_tlbs(kvm);
+				kvm_flush_remote_tlbs_with_address(kvm,
+						start_gfn,
+						iterator.gfn - start_gfn + 1);
 				flush = false;
 			}
 			cond_resched_lock(&kvm->mmu_lock);
@@ -5535,7 +5537,8 @@ slot_handle_level_range(struct kvm *kvm, struct kvm_memory_slot *memslot,
 	}
 
 	if (flush && lock_flush_tlb) {
-		kvm_flush_remote_tlbs(kvm);
+		kvm_flush_remote_tlbs_with_address(kvm, start_gfn,
+						   end_gfn - start_gfn + 1);
 		flush = false;
 	}
 
