031262007630f83ac5c99b7d9314e66a1f174eb6
Wentao Cai <etsai042@gmail.com>
2019-03-14 11:25:14 -0700

    staging: mt7621-mmc: delele extra blank lines
    Delete extra blank lines to mute checkpatch.pl check:
    CHECK: Please don't use multiple blank lines
    
    Signed-off-by: Wentao Cai <etsai042@gmail.com>
    Reviewed-by: Matthias Brugger <matthias.bgg@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/mt7621-mmc/dbg.c
+++ drivers/staging/mt7621-mmc/dbg.c
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mt6575_sd.h"
 #include <linux/seq_file.h>
 
-
 /* for debug zone */
 unsigned int sd_debug_zone[4] = {
 	0,
