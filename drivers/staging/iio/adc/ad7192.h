8a838f1fe3d7e4aff2ac583018963652f02f7f60
Mircea Caprioru <mircea.caprioru@analog.com>
2019-03-15 13:29:03 +0200

    staging: iio: adc: ad7192: Convert platform data to DT properties
    This patch will remove platform data members and replace them with device
    tree properties. These properties will be subject to further modifications
    and probably replaced with other functionalities at some point in time.
    
    Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/adc/ad7192.h
+++ drivers/staging/iio/adc/ad7192.h
@@ -34,13 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct ad7192_platform_data {
 	u16		vref_mv;
-	bool		refin2_en;
-	bool		rej60_en;
-	bool		sinc3_en;
-	bool		chop_en;
-	bool		buf_en;
-	bool		unipolar_en;
-	bool		burnout_curr_en;
 };
 
 #endif /* IIO_ADC_AD7192_H_ */
