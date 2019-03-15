db415aa2df7b2624b7fdad040e3e7f4fade797af
David Sterba <dsterba@suse.com>
2019-03-15 17:28:46 +0100

    btrfs: tests: use standard error message after inode allocation failure
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/tests/inode-tests.c
+++ fs/btrfs/tests/inode-tests.c
@@ -229,7 +229,7 @@ static noinline int test_btrfs_get_extent(u32 sectorsize, u32 nodesize)
 
 	inode = btrfs_new_test_inode();
 	if (!inode) {
-		test_err("couldn't allocate inode");
+		test_std_err(TEST_ALLOC_INODE);
 		return ret;
 	}
 
@@ -830,7 +830,7 @@ static int test_hole_first(u32 sectorsize, u32 nodesize)
 
 	inode = btrfs_new_test_inode();
 	if (!inode) {
-		test_err("couldn't allocate inode");
+		test_std_err(TEST_ALLOC_INODE);
 		return ret;
 	}
 
@@ -930,7 +930,7 @@ static int test_extent_accounting(u32 sectorsize, u32 nodesize)
 
 	inode = btrfs_new_test_inode();
 	if (!inode) {
-		test_err("couldn't allocate inode");
+		test_std_err(TEST_ALLOC_INODE);
 		return ret;
 	}
 
