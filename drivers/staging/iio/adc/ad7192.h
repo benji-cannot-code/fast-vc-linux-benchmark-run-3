92175a6f63520712bca43d8ac92ed930c9fe9d2f
Mircea Caprioru <mircea.caprioru@analog.com>
2019-03-15 13:29:02 +0200

    staging: iio: adc: ad7192: Use DT clock binding
    This patch replaces the platform data clock select member with DT clock
    binding. Through the DT the external clock binding is specified. If this is
    not provided then the device will use the internal clock source.
    
    With the external clock binding there is the option to use a clock or a
    crystal as the clock source. When an external crystal is used it is
    connected to MCLK1 and MCLK2 pins. If the external clock is used only MCLK2
    pin will be connected.
    
    Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/adc/ad7192.h
+++ drivers/staging/iio/adc/ad7192.h
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct ad7192_platform_data {
 	u16		vref_mv;
-	u8		clock_source_sel;
-	u32		ext_clk_hz;
 	bool		refin2_en;
 	bool		rej60_en;
 	bool		sinc3_en;
