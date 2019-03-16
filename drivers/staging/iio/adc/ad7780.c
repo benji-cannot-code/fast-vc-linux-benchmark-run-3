24aa41769790f13ebdc85f0db4fd7773580a83d1
Renato Lui Geh <renatogeh@gmail.com>
2019-03-15 23:15:26 -0300

    staging: iio: ad7780: add new copyright holder
    This patch adds a new copyright holder to the ad7780 driver.
    
    Signed-off-by: Renato Lui Geh <renatogeh@gmail.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/adc/ad7780.c
+++ drivers/staging/iio/adc/ad7780.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * AD7170/AD7171 and AD7780/AD7781 SPI ADC driver
  *
  * Copyright 2011 Analog Devices Inc.
+ * Copyright 2019 Renato Lui Geh
  */
 
 #include <linux/interrupt.h>
