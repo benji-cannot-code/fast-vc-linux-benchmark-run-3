3cd6fbb20c2abcb84a70403da6e5b2b220fd6038
Marcelo Schmitt <marcelo.schmitt1@gmail.com>
2019-03-16 12:07:03 -0300

    staging: iio: ad5933: add SPDX identifier
    Add SPDX identifier of GPL-2.0 for the ad5933 driver.
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt1@gmail.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/impedance-analyzer/ad5933.c
+++ drivers/staging/iio/impedance-analyzer/ad5933.c
@@ -1,10 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * AD5933 AD5934 Impedance Converter, Network Analyzer
  *
  * Copyright 2011 Analog Devices Inc.
- *
- * Licensed under the GPL-2.
  */
 
 #include <linux/clk.h>
