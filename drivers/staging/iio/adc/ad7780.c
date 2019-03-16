ba42e43a211f8124c88743bd5210b85fc55270d9
Renato Lui Geh <renatogeh@gmail.com>
2019-03-15 23:14:27 -0300

    staging:iio:ad7780: add chip ID values and mask
    The ad7780 supports both the ad778x and ad717x families. Each chip has
    a corresponding ID. This patch provides a mask for extracting ID values
    from the status bits and also macros for the correct values for the
    ad7170, ad7171, ad7780 and ad7781.
    
    Signed-off-by: Renato Lui Geh <renatogeh@gmail.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/staging/iio/adc/ad7780.c
+++ drivers/staging/iio/adc/ad7780.c
@@ -31,6 +31,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AD7780_ID0		BIT(3)
 #define AD7780_GAIN		BIT(2)
 
+#define AD7170_ID		0
+#define AD7171_ID		1
+#define AD7780_ID		1
+#define AD7781_ID		0
+
+#define AD7780_ID_MASK		(AD7780_ID0 | AD7780_ID1)
 
 #define AD7780_PATTERN_GOOD	1
 #define AD7780_PATTERN_MASK	GENMASK(1, 0)
