36a977ca5487625a80e8c3d80899e2f030590ffb
Chris Wilson <chris@chris-wilson.co.uk>
2019-03-18 09:51:46 +0000

    drm/i915: Hold a ref to the ring while retiring
    As the final request on a ring may hold the reference to this ring (via
    retiring the last pinned context), we may find ourselves chasing a
    dangling pointer on completion of the list.
    
    A quick solution is to hold a reference to the ring itself as we retire
    along it so that we only free it after we stop dereferencing it.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190318095204.9913-4-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/intel_engine_types.h
+++ drivers/gpu/drm/i915/intel_engine_types.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/hashtable.h>
 #include <linux/irq_work.h>
+#include <linux/kref.h>
 #include <linux/list.h>
 #include <linux/types.h>
 
@@ -52,6 +53,7 @@ struct intel_engine_hangcheck {
 };
 
 struct intel_ring {
+	struct kref ref;
 	struct i915_vma *vma;
 	void *vaddr;
 
