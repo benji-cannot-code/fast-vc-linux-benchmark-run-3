af181ed2bf2637d5aad9ab6afb80948087d85bff
Alexey Budankov <alexey.budankov@linux.intel.com>
2019-03-18 20:43:35 +0300

    perf record: Implement compression for serial trace streaming
    Compression is implemented using the functions from zstd.c. As the
    memory to operate on the compression uses mmap->data buffer.
    
    If Zstd streaming compression API fails for some reason the data to be
    compressed are just copied into the memory buffers using plain memcpy().
    
    Compressed trace frame consists of an array of PERF_RECORD_COMPRESSED
    records. Each element of the array is not longer that
    PERF_SAMPLE_MAX_SIZE and consists of perf_event_header followed by the
    compressed chunk that is decompressed on the loading stage.
    
    Comitter notes:
    
    Undo some unnecessary line breaks, remove some unnecessary () around
    zstd_data to then just get its address, and fix conflicts with
    BPF_PROG_INFO/BPF_BTF patchkits.
    
    Signed-off-by: Alexey Budankov <alexey.budankov@linux.intel.com>
    Reviewed-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/744df43f-3932-2594-ddef-1e99a3cad03a@linux.intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/session.h
+++ tools/perf/util/session.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "machine.h"
 #include "data.h"
 #include "ordered-events.h"
+#include "util/compress.h"
 #include <linux/kernel.h>
 #include <linux/rbtree.h>
 #include <linux/perf_event.h>
@@ -38,6 +39,7 @@ struct perf_session {
 	struct perf_tool	*tool;
 	u64			bytes_transferred;
 	u64			bytes_compressed;
+	struct zstd_data	zstd_data;
 };
 
 struct perf_tool;
