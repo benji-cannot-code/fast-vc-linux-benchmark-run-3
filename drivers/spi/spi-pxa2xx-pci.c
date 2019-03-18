a74f64c1536f185b8bf3509ae3792e4b560d5403
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-03-18 16:42:37 +0200

    spi: pxa2xx-pci: Drop unused header inclusion
    There is nothing in the driver which requires OF specific header
    to be included.
    
    Remove it for good.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-pxa2xx-pci.c
+++ drivers/spi/spi-pxa2xx-pci.c
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/clk-provider.h>
 #include <linux/module.h>
-#include <linux/of_device.h>
 #include <linux/pci.h>
 #include <linux/platform_device.h>
 #include <linux/spi/pxa2xx_spi.h>
