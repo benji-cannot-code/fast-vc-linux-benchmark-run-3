c750b77664da4bf2066cbee078ee25f352f4619b
Paul Cercueil <paul@crapouillou.net>
2019-03-19 15:53:55 +0100

    mtd: rawnand: ingenic: Use SPDX license notifiers
    Use SPDX license notifiers instead of GPLv2 license text in the headers.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Reviewed-by: Boris Brezillon <bbrezillon@kernel.org>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/raw/ingenic/jz4780_nand.c
+++ drivers/mtd/nand/raw/ingenic/jz4780_nand.c
@@ -1,13 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * JZ4780 NAND driver
  *
  * Copyright (c) 2015 Imagination Technologies
  * Author: Alex Smith <alex.smith@imgtec.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation.
  */
 
 #include <linux/delay.h>
