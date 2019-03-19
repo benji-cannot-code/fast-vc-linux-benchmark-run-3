96d0890c579ed3f9b0d450befeb76286211ff667
Nishka Dasgupta <nishka.dasgupta@yahoo.com>
2019-03-20 00:59:11 +0530

    staging: gasket: Remove return variable
    Remove return variable and return the return value directly. Issue
    suggested by ret.cocci.
    
    Signed-off-by: Nishka Dasgupta <nishka.dasgupta@yahoo.com>
    Acked-by: Julia Lawall <julia.lawall@lip6.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/gasket/apex_driver.c
+++ drivers/staging/gasket/apex_driver.c
@@ -440,9 +440,7 @@ static int apex_reset(struct gasket_dev *gasket_dev)
 		if (ret)
 			return ret;
 	}
-	ret = apex_quit_reset(gasket_dev);
-
-	return ret;
+	return apex_quit_reset(gasket_dev);
 }
 
 /*
