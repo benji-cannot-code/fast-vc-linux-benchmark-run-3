f75ff9019ea8bce5dd940dc5343cb06627461ce0
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-03-19 20:49:27 +0200

    enc28j60: Remove linux/init.h
    There is no need to include linux/init.h when at the same time
    we include linux/module.h.
    
    Remove redundant inclusion.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/microchip/enc28j60.c
+++ drivers/net/ethernet/microchip/enc28j60.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/property.h>
 #include <linux/string.h>
 #include <linux/errno.h>
-#include <linux/init.h>
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 #include <linux/ethtool.h>
