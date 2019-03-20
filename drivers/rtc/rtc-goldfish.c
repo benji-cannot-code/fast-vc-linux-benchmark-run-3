77681be753d8ec92a12f995c1b9da908b8cbfce1
Alexandre Belloni <alexandre.belloni@bootlin.com>
2019-03-20 13:34:14 +0100

    rtc: goldfish: sort headers
    Sort headers alphabetically.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-goldfish.c
+++ drivers/rtc/rtc-goldfish.c
@@ -15,10 +15,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/io.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/rtc.h>
-#include <linux/io.h>
 
 #define TIMER_TIME_LOW		0x00	/* get low bits of current time  */
 					/*   and update TIMER_TIME_HIGH  */
