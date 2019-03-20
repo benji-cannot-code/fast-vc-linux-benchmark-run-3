b9665bab46a3c16af57dd5e926d68d49cfa3b0b7
Kirill Tkhai <ktkhai@virtuozzo.com>
2019-03-20 12:16:42 +0300

    tun: Add ioctl() TUNGETDEVNETNS cmd to allow obtaining real net ns of tun device
    In commit f2780d6d7475 "tun: Add ioctl() SIOCGSKNS cmd to allow
    obtaining net ns of tun device" it was missed that tun may change
    its net ns, while net ns of socket remains the same as it was
    created initially. SIOCGSKNS returns net ns of socket, so it is
    not suitable for obtaining net ns of device.
    
    We may have two tun devices with the same names in two net ns,
    and in this case it's not possible to determ, which of them
    fd refers to (TUNGETIFF will return the same name).
    
    This patch adds new ioctl() cmd for obtaining net ns of a device.
    
    Reported-by: Harald Albrecht <harald.albrecht@gmx.net>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -3103,6 +3103,7 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 
 	tun_debug(KERN_INFO, tun, "tun_chr_ioctl cmd %u\n", cmd);
 
+	net = dev_net(tun->dev);
 	ret = 0;
 	switch (cmd) {
 	case TUNGETIFF:
@@ -3326,6 +3327,13 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 			goto unlock;
 
 		ret = tun_net_change_carrier(tun->dev, (bool)carrier);
+		break;
+
+	case TUNGETDEVNETNS:
+		ret = -EPERM;
+		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
+			goto unlock;
+		ret = open_related_ns(&net->ns, get_net_ns);
 		break;
 
 	default:
