03b7bfde58b52e9aa912671799d8d70e7343dfbc
Andi Kleen <ak@linux.intel.com>
2019-03-21 15:00:05 -0700

    ASoC: AMD: Fix incorrect extern
    When using bare externs outside include files that types should
    at least match. This fixes a type confusion between bool
    and int.
    
    Cc: broonie@kernel.org
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/acp-da7219-max98357a.c
+++ sound/soc/amd/acp-da7219-max98357a.c
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct snd_soc_jack cz_jack;
 static struct clk *da7219_dai_clk;
-extern int bt_uart_enable;
+extern bool bt_uart_enable;
 
 static int cz_da7219_init(struct snd_soc_pcm_runtime *rtd)
 {
