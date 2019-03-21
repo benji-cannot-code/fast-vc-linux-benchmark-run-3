aa38cef0f7a85d5dfe8e7a3c57725ac31a942daf
Arnd Bergmann <arnd@arndb.de>
2019-03-21 13:42:25 +0100

    spi: export tracepoint symbols to modules
    The newly added tracepoints in the spi-mxs driver cause a link
    error when the driver is a loadable module:
    
    ERROR: "__tracepoint_spi_transfer_stop" [drivers/spi/spi-mxs.ko] undefined!
    ERROR: "__tracepoint_spi_transfer_start" [drivers/spi/spi-mxs.ko] undefined!
    
    I'm not quite sure where to put the export statements, but
    directly after the inclusion of the header seems as good as
    any other place.
    
    Fixes: f3fdea3af405 ("spi: mxs: add tracing to custom .transfer_one_message callback")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/spi.h>
+EXPORT_TRACEPOINT_SYMBOL(spi_transfer_start);
+EXPORT_TRACEPOINT_SYMBOL(spi_transfer_stop);
 
 #include "internals.h"
 
