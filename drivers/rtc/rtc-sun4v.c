075145cc451b524bd1cdd58046adb9c18987aea7
Alexandre Belloni <alexandre.belloni@bootlin.com>
2019-03-22 08:22:56 +0100

    rtc: sun4v: switch to SPDX identifier
    Use SPDX-License-Identifier to be clearer on the license. Choose the v2
    only as this is the default Linux license.
    
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-sun4v.c
+++ drivers/rtc/rtc-sun4v.c
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /* rtc-sun4v.c: Hypervisor based RTC for SUN4V systems.
  *
  * Author: David S. Miller
- * License: GPL
  *
  * Copyright (C) 2008 David S. Miller <davem@davemloft.net>
  */
