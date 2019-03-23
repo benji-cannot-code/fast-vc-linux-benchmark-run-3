8dfe266c704d99e392e455edf1ce3a6a72fbb549
Igor Russkikh <Igor.Russkikh@aquantia.com>
2019-03-23 15:23:36 +0000

    net: aquantia: Make RX default frame size 2K
    This correlates with default internet MTU. This also allows page
    flip/reuse to be activated, since each allocated RX page now serves for
    two frags/packets.
    
    Signed-off-by: Igor Russkikh <igor.russkikh@aquantia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/aquantia/atlantic/aq_cfg.h
+++ drivers/net/ethernet/aquantia/atlantic/aq_cfg.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AQ_CFG_TCS_MAX    8U
 
 #define AQ_CFG_TX_FRAME_MAX  (16U * 1024U)
-#define AQ_CFG_RX_FRAME_MAX  (4U * 1024U)
+#define AQ_CFG_RX_FRAME_MAX  (2U * 1024U)
 
 #define AQ_CFG_TX_CLEAN_BUDGET 256U
 
