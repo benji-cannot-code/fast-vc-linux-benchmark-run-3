46b2ff87eb3734fdc61b450ba21a5ec1beadc3f7
Jagadeesh Pagadala <jagdsh.linux@gmail.com>
2019-03-23 18:20:55 +0530

    powerpc: Remove duplicate headers
    Remove duplicate headers inclusions.
    
    Signed-off-by: Jagadeesh Pagadala <jagdsh.linux@gmail.com>
    Reviewed-by: Mukesh Ojha <mojha@codeaurora.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/kernel/time.c
+++ arch/powerpc/kernel/time.c
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timex.h>
 #include <linux/kernel_stat.h>
 #include <linux/time.h>
-#include <linux/clockchips.h>
 #include <linux/init.h>
 #include <linux/profile.h>
 #include <linux/cpu.h>
--- arch/powerpc/mm/numa.c
+++ arch/powerpc/mm/numa.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sparsemem.h>
 #include <asm/prom.h>
 #include <asm/smp.h>
-#include <asm/cputhreads.h>
 #include <asm/topology.h>
 #include <asm/firmware.h>
 #include <asm/paca.h>
