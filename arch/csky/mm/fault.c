6c43ce5359513eb4b03da0b05a0ea91671897525
Jagadeesh Pagadala <jagdsh.linux@gmail.com>
2019-03-23 17:25:17 +0530

    csky: mm/fault.c: Remove duplicate header
    Remove duplicate header which is included twice.
    
    Signed-off-by: Jagadeesh Pagadala <jagdsh.linux@gmail.com>
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- arch/csky/mm/fault.c
+++ arch/csky/mm/fault.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smp.h>
 #include <linux/version.h>
 #include <linux/vt_kern.h>
-#include <linux/kernel.h>
 #include <linux/extable.h>
 #include <linux/uaccess.h>
 
