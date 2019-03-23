c9589607bcf433a2d886b3cc5659aa69c1da2c9d
Igor Russkikh <Igor.Russkikh@aquantia.com>
2019-03-23 15:23:31 +0000

    net: aquantia: optimize rx path using larger preallocated skb len
    Atlantic driver used 14 bytes preallocated skb size. That made L3 protocol
    processing inefficient because pskb_pull had to fetch all the L3/L4 headers
    from extra fragments.
    
    Specially on UDP flows that caused extra packet drops because CPU was
    overloaded with pskb_pull.
    
    This patch uses eth_get_headlen for skb preallocation.
    
    Signed-off-by: Igor Russkikh <igor.russkikh@aquantia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/aquantia/atlantic/aq_cfg.h
+++ drivers/net/ethernet/aquantia/atlantic/aq_cfg.h
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define AQ_CFG_TX_CLEAN_BUDGET 256U
 
+#define AQ_CFG_RX_HDR_SIZE 256U
+
 /* LRO */
 #define AQ_CFG_IS_LRO_DEF           1U
 
