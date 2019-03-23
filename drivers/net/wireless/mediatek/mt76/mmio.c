79296276c31dda80d8f314ded2d1905e8beb6f4a
Felix Fietkau <nbd@nbd.name>
2019-03-23 15:24:56 +0100

    mt76: use readl/writel instead of ioread32/iowrite32
    Switching to readl/writel is faster because it gets rid of an unnecessary
    wrapper with extra checks.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mmio.c
+++ drivers/net/wireless/mediatek/mt76/mmio.c
@@ -22,7 +22,7 @@ static u32 mt76_mmio_rr(struct mt76_dev *dev, u32 offset)
 {
 	u32 val;
 
-	val = ioread32(dev->mmio.regs + offset);
+	val = readl(dev->mmio.regs + offset);
 	trace_reg_rr(dev, offset, val);
 
 	return val;
@@ -31,7 +31,7 @@ static u32 mt76_mmio_rr(struct mt76_dev *dev, u32 offset)
 static void mt76_mmio_wr(struct mt76_dev *dev, u32 offset, u32 val)
 {
 	trace_reg_wr(dev, offset, val);
-	iowrite32(val, dev->mmio.regs + offset);
+	writel(val, dev->mmio.regs + offset);
 }
 
 static u32 mt76_mmio_rmw(struct mt76_dev *dev, u32 offset, u32 mask, u32 val)
