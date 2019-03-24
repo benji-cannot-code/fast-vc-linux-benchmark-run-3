4c25cd3bb97258798d1323255585a281dc5bfb63
Sowjanya Komatineni <skomatineni@nvidia.com>
2019-03-23 21:45:23 -0700

    mmc: cqhci: allow hosts to update dcmd cmd desc
    This patch adds update_dcmd_desc interface to cqhci_host_ops to
    allow hosts to update any of the DCMD task descriptor attributes
    and parameters.
    
    Tested-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-by: Ritesh Harjani <riteshh@codeaurora.org>
    Signed-off-by: Sowjanya Komatineni <skomatineni@nvidia.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/cqhci.h
+++ drivers/mmc/host/cqhci.h
@@ -148,6 +148,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct cqhci_host_ops;
 struct mmc_host;
+struct mmc_request;
 struct cqhci_slot;
 
 struct cqhci_host {
@@ -211,6 +212,8 @@ struct cqhci_host_ops {
 	u32 (*read_l)(struct cqhci_host *host, int reg);
 	void (*enable)(struct mmc_host *mmc);
 	void (*disable)(struct mmc_host *mmc, bool recovery);
+	void (*update_dcmd_desc)(struct mmc_host *mmc, struct mmc_request *mrq,
+				 u64 *data);
 };
 
 static inline void cqhci_writel(struct cqhci_host *host, u32 val, int reg)
