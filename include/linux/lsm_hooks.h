69de147b247d3c4a6a982ae862d5f03e2bee08b5
Ondrej Mosnacek <omosnace@redhat.com>
2019-03-25 09:05:35 +0100

    LSM: lsm_hooks.h - fix missing colon in docstring
    Apparently without it it is incorrect syntax and causes a warning about
    undocumented struct field.
    
    Fixes: b230d5aba2d1 ("LSM: add new hook for kernfs node initialization")
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -448,7 +448,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Security hooks for kernfs node operations
  *
- * @kernfs_init_security
+ * @kernfs_init_security:
  *	Initialize the security context of a newly created kernfs node based
  *	on its own and its parent's attributes.
  *
