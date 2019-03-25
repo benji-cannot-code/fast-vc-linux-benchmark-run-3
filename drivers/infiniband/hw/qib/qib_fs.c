784813bfe47d7199691ad46e05928983a599bfad
David Howells <dhowells@redhat.com>
2019-03-25 16:38:29 +0000

    vfs: Convert qib_fs/ipathfs to use the new mount API
    Convert the qib_fs/ipathfs filesystem to the new internal mount API as the
    old one will be obsoleted and removed.  This allows greater flexibility in
    communication of mount parameters between userspace, the VFS and the
    filesystem.
    
    See Documentation/filesystems/mount_api.txt for more information.
    
    [Q] Can qib_remove() race with qibfs_kill_super()?  Should qib_super
        accesses be serialised with some sort of lock?
    [A] yes, it can and no, that's not the right solution.  See vfs.git #qibfs
        for an old attempt to handle that cleanly.  Infiniband folks were not
        interested...
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    cc: Mike Marciniszyn <mike.marciniszyn@intel.com>
    cc: linux-rdma@vger.kernel.org
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/infiniband/hw/qib/qib_fs.c
+++ drivers/infiniband/hw/qib/qib_fs.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/fs_context.h>
 #include <linux/mount.h>
 #include <linux/pagemap.h>
 #include <linux/init.h>
@@ -507,7 +508,7 @@ bail:
  * after device init.  The direct add_cntr_files() call handles adding
  * them from the init code, when the fs is already mounted.
  */
-static int qibfs_fill_super(struct super_block *sb, void *data, int silent)
+static int qibfs_fill_super(struct super_block *sb, struct fs_context *fc)
 {
 	struct qib_devdata *dd;
 	unsigned long index;
@@ -535,15 +536,22 @@ bail:
 	return ret;
 }
 
-static struct dentry *qibfs_mount(struct file_system_type *fs_type, int flags,
-			const char *dev_name, void *data)
+static int qibfs_get_tree(struct fs_context *fc)
 {
-	struct dentry *ret;
-
-	ret = mount_single(fs_type, flags, data, qibfs_fill_super);
-	if (!IS_ERR(ret))
-		qib_super = ret->d_sb;
+	int ret = get_tree_single(fc, qibfs_fill_super);
+	if (ret == 0)
+		qib_super = fc->root->d_sb;
 	return ret;
+}
+
+static const struct fs_context_operations qibfs_context_ops = {
+	.get_tree	= qibfs_get_tree,
+};
+
+static int qibfs_init_fs_context(struct fs_context *fc)
+{
+	fc->ops = &qibfs_context_ops;
+	return 0;
 }
 
 static void qibfs_kill_super(struct super_block *s)
@@ -584,7 +592,7 @@ int qibfs_remove(struct qib_devdata *dd)
 static struct file_system_type qibfs_fs_type = {
 	.owner =        THIS_MODULE,
 	.name =         "ipathfs",
-	.mount =        qibfs_mount,
+	.init_fs_context = qibfs_init_fs_context,
 	.kill_sb =      qibfs_kill_super,
 };
 MODULE_ALIAS_FS("ipathfs");
