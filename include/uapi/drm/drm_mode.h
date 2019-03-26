c9d0b5e52a9da91079031e276871d6712006c85e
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-03-26 19:34:01 +0200

    drm/uapi: Remove unused DRM_DISPLAY_INFO_LEN
    Remove the unused DRM_DISPLAY_INFO_LEN from the uapi headers.
    I presume the original plan was to expose the display name
    via getconnector, but looks like that never happened. So we have
    the define for the length of the string but no string anywhere.
    
    A quick scan didn't seem to reveal userspace referencing this
    so hopefully we can just nuke it.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190326173401.7329-4-ville.syrjala@linux.intel.com
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- include/uapi/drm/drm_mode.h
+++ include/uapi/drm/drm_mode.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern "C" {
 #endif
 
-#define DRM_DISPLAY_INFO_LEN	32
 #define DRM_CONNECTOR_NAME_LEN	32
 #define DRM_DISPLAY_MODE_LEN	32
 #define DRM_PROP_NAME_LEN	32
