35430e61b306742d91cf1e1f5821eb1f4520f2f4
Saurav Kashyap <skashyap@marvell.com>
2019-03-26 00:38:38 -0700

    scsi: qedf: Modify abort and tmf handler to handle edge condition and flush
    An I/O can be in any state when flush is called, it can be in abort,
    waiting for abort, RRQ send and waiting or TMF send.
    
     - HZ can be different on different architecture, correctly set abort
       timeout value.
    
     - Flush can complete the I/Os prematurely, handle refcount for aborted
       I/Os and for which RRQ is pending.
    
     - Differentiate LUN/TARGET reset, as cleanup needs to be send to firmware
       accordingly.
    
     - Add flush mutex to sync cleanup call from abort and flush routine.
    
     - Clear abort/outstanding bit on timeout.
    
    Signed-off-by: Shyam Sundar <shyam.sundar@marvell.com>
    Signed-off-by: Chad Dupuis <cdupuis@marvell.com>
    Signed-off-by: Saurav Kashyap <skashyap@marvell.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qedf/qedf.h
+++ drivers/scsi/qedf/qedf.h
@@ -50,8 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	sizeof(struct fc_frame_header))
 #define QEDF_MAX_NPIV		64
 #define QEDF_TM_TIMEOUT		10
-#define QEDF_ABORT_TIMEOUT	10
-#define QEDF_CLEANUP_TIMEOUT	10
+#define QEDF_ABORT_TIMEOUT	(10 * 1000)
+#define QEDF_CLEANUP_TIMEOUT	1
 #define QEDF_MAX_CDB_LEN	16
 
 #define UPSTREAM_REMOVE		1
@@ -83,6 +83,7 @@ struct qedf_els_cb_arg {
 };
 
 enum qedf_ioreq_event {
+	QEDF_IOREQ_EV_NONE,
 	QEDF_IOREQ_EV_ABORT_SUCCESS,
 	QEDF_IOREQ_EV_ABORT_FAILED,
 	QEDF_IOREQ_EV_SEND_RRQ,
@@ -183,7 +184,10 @@ struct qedf_rport {
 #define QEDF_RPORT_SESSION_READY 1
 #define QEDF_RPORT_UPLOADING_CONNECTION	2
 #define QEDF_RPORT_IN_RESET 3
+#define QEDF_RPORT_IN_LUN_RESET 4
+#define QEDF_RPORT_IN_TARGET_RESET 5
 	unsigned long flags;
+	int lun_reset_lun;
 	unsigned long retry_delay_timestamp;
 	struct fc_rport *rport;
 	struct fc_rport_priv *rdata;
@@ -396,6 +400,8 @@ struct qedf_ctx {
 	u8 target_resets;
 	u8 task_set_fulls;
 	u8 busy;
+	/* Used for flush routine */
+	struct mutex flush_mutex;
 };
 
 struct io_bdt {
