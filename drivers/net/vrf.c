426b9202dcc522e698ec782f642857229878fbd1
Sabrina Dubroca <sd@queasysnail.net>
2019-03-26 18:22:16 +0100

    vrf: prevent adding upper devices
    VRF devices don't work with upper devices. Currently, it's possible to
    add a VRF device to a bridge or team, and to create macvlan, macsec, or
    ipvlan devices on top of a VRF (bond and vlan are prevented respectively
    by the lack of an ndo_set_mac_address op and the NETIF_F_VLAN_CHALLENGED
    feature flag).
    
    Fix this by setting the IFF_NO_RX_HANDLER flag (introduced in commit
    f5426250a6ec ("net: introduce IFF_NO_RX_HANDLER")).
    
    Cc: David Ahern <dsahern@gmail.com>
    Fixes: 193125dbd8eb ("net: Introduce VRF device driver")
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Acked-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/vrf.c
+++ drivers/net/vrf.c
@@ -1274,6 +1274,7 @@ static void vrf_setup(struct net_device *dev)
 
 	/* default to no qdisc; user can add if desired */
 	dev->priv_flags |= IFF_NO_QUEUE;
+	dev->priv_flags |= IFF_NO_RX_HANDLER;
 
 	dev->min_mtu = 0;
 	dev->max_mtu = 0;
