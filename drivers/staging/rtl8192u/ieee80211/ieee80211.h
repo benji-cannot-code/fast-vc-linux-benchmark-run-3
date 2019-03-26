005b6c1a96b7126fe2a8604af7dcbf539daf5e57
Sanjana Sanikommu <sanjana99reddy99@gmail.com>
2019-03-26 22:39:42 +0530

    staging: rtl8192u: Remove typedef for struct.
    Challenge suggested by coccinelle.
    
    Linux kernel coding style guidelines suggest not using typedefs for
    structure.
    The following Coccinelle semantic patch detects the cases for struct
    type:
    
    @tn@
    identifier i;
    type td;
    @@
    
    -typedef
     struct i { ... }
    -td
     ;
    
    @@
    type tn.td;
    identifier tn.i;
    @@
    
    -td
    + struct i
    
    Signed-off-by: Sanjana Sanikommu <sanjana99reddy99@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/ieee80211/ieee80211.h
+++ drivers/staging/rtl8192u/ieee80211/ieee80211.h
@@ -297,7 +297,7 @@ struct cb_desc {
 #define ieee80211_wx_get_encode_ext	ieee80211_wx_get_encode_ext_rsl
 
 
-typedef struct ieee_param {
+struct ieee_param {
 	u32 cmd;
 	u8 sta_addr[ETH_ALEN];
 	union {
@@ -324,7 +324,7 @@ typedef struct ieee_param {
 			u8 key[0];
 		} crypt;
 	} u;
-} ieee_param;
+};
 
 
 // linux under 2.6.9 release may not support it, so modify it for common use
