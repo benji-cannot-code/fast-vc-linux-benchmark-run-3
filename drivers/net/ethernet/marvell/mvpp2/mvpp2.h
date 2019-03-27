94ad6cfac2422aeff46e54b92434abce3d65d464
Maxime Chevallier <maxime.chevallier@bootlin.com>
2019-03-27 09:44:11 +0100

    net: mvpp2: debugfs: Store debugfs entries data in mvpp2 struct
    The current way to store the required private data needed to access
    various debugfs entries is to alloc them on the fly, share them within
    the entries that need to access them, and finally have one entry free
    that data upon closing. This leads to hard to maintain code, and is very
    error-prone.
    
    This commit stores all debugfs related data in the same place, making
    sure this is allocated only when the debugfs directory is successfully
    created, so that we don't waste memory when we don't use this feature.
    
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2.h
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2.h
@@ -715,6 +715,7 @@ enum mvpp2_prs_l3_cast {
 #define MVPP2_DESC_DMA_MASK	DMA_BIT_MASK(40)
 
 /* Definitions */
+struct mvpp2_dbgfs_entries;
 
 /* Shared Packet Processor resources */
 struct mvpp2 {
@@ -776,6 +777,9 @@ struct mvpp2 {
 
 	/* Debugfs root entry */
 	struct dentry *dbgfs_dir;
+
+	/* Debugfs entries private data */
+	struct mvpp2_dbgfs_entries *dbgfs_entries;
 };
 
 struct mvpp2_pcpu_stats {
