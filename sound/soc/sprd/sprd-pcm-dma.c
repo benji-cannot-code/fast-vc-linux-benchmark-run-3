83d58614c5e9fa66f73cb6d707459fd09719dcb7
Baolin Wang <baolin.wang@linaro.org>
2019-03-27 19:47:03 +0800

    ASoC: sprd: Add Spreadtrum audio compress offload support
    We use 2-stage DMA mode to support Spreadtrum audio compress offload,
    which means we use one DMA source channel to transfer data from IRAM
    buffer to the DSP fifo to do decoding/encoding, once IRAM buffer is
    empty by transferring done, another DMA destination channel will be
    triggered automatically to start to transfer data from DDR buffer to
    the IRAM buffer. This can reduce the AP subsystem wakeup times to save
    power.
    
    Co-developed-by: Yintang Ren <yintang.ren@unisoc.com>
    Signed-off-by: Baolin Wang <baolin.wang@linaro.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sprd/sprd-pcm-dma.c
+++ sound/soc/sprd/sprd-pcm-dma.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "sprd-pcm-dma.h"
 
-#define DRV_NAME			"sprd_pcm_dma"
 #define SPRD_PCM_DMA_LINKLIST_SIZE	64
 #define SPRD_PCM_DMA_BRUST_LEN		640
 
@@ -525,6 +524,7 @@ static void sprd_pcm_free(struct snd_pcm *pcm)
 static const struct snd_soc_component_driver sprd_soc_component = {
 	.name		= DRV_NAME,
 	.ops		= &sprd_pcm_ops,
+	.compr_ops	= &sprd_platform_compr_ops,
 	.pcm_new	= sprd_pcm_new,
 	.pcm_free	= sprd_pcm_free,
 };
