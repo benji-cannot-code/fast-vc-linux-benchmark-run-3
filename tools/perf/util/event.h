44bf747c7e02b9b9686f0559f9af4751d6a7ae86
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-03-27 15:22:52 -0300

    perf tools: Add header defining used namespace struct to event.h
    When adding the 'struct namespaces_event' to event.h, referencing the
    'struct perf_ns_link_info' type, we forgot to add the header where it is
    defined, getting that definition only by sheer luck.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Hari Bathini <hbathini@linux.vnet.ibm.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Fixes: f3b3614a284d ("perf tools: Add PERF_RECORD_NAMESPACES to include namespaces related info")
    Link: https://lkml.kernel.org/n/tip-qkrld0v7boc9uabjbd8csxux@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/event.h
+++ tools/perf/util/event.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <linux/kernel.h>
 #include <linux/bpf.h>
+#include <linux/perf_event.h>
 
 #include "../perf.h"
 #include "build-id.h"
