ed6edb4e901697fb90dcf2e0a781471441ad996d
YueHaibing <yuehaibing@huawei.com>
2019-03-28 01:39:58 +0000

    gpio: mlxbf: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

--- drivers/gpio/gpio-mlxbf.c
+++ drivers/gpio/gpio-mlxbf.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm.h>
 #include <linux/resource.h>
 #include <linux/types.h>
-#include <linux/version.h>
 
 /* Number of pins on BlueField */
 #define MLXBF_GPIO_NR 54
