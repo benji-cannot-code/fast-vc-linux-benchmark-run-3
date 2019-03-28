44c354f2c57d36856b9b7bc0286451e21d7b5225
Alex Deucher <alexander.deucher@amd.com>
2019-03-28 10:15:26 -0500

    Revert "drm/amdgpu: replace get_user_pages with HMM mirror helpers"
    This reverts commit 915d3eecfa23693bac9e54cdacf84fb4efdcc5c4.
    
    This depends on an HMM fix which is not upstream yet.
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_mn.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_mn.h
@@ -26,10 +26,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __AMDGPU_MN_H__
 
 /*
- * HMM mirror
+ * MMU Notifier
  */
 struct amdgpu_mn;
-struct hmm_range;
 
 enum amdgpu_mn_type {
 	AMDGPU_MN_TYPE_GFX,
@@ -43,7 +42,6 @@ struct amdgpu_mn *amdgpu_mn_get(struct amdgpu_device *adev,
 				enum amdgpu_mn_type type);
 int amdgpu_mn_register(struct amdgpu_bo *bo, unsigned long addr);
 void amdgpu_mn_unregister(struct amdgpu_bo *bo);
-void amdgpu_hmm_init_range(struct hmm_range *range);
 #else
 static inline void amdgpu_mn_lock(struct amdgpu_mn *mn) {}
 static inline void amdgpu_mn_unlock(struct amdgpu_mn *mn) {}
