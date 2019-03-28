21e0ce552b32616f7d42e7616a6388752f0692fb
Mika Westerberg <mika.westerberg@linux.intel.com>
2019-03-28 11:59:26 +0300

    thunderbolt: Remove unused work field in struct tb_switch
    This field is not used anywhere so remove it.
    
    Reported-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- drivers/thunderbolt/tb.h
+++ drivers/thunderbolt/tb.h
@@ -71,7 +71,6 @@ struct tb_switch_nvm {
  * @boot: Whether the switch was already authorized on boot or not
  * @rpm: The switch supports runtime PM
  * @authorized: Whether the switch is authorized by user or policy
- * @work: Work used to automatically authorize a switch
  * @security_level: Switch supported security level
  * @key: Contains the key used to challenge the device or %NULL if not
  *	 supported. Size of the key is %TB_SWITCH_KEY_SIZE.
@@ -106,7 +105,6 @@ struct tb_switch {
 	bool boot;
 	bool rpm;
 	unsigned int authorized;
-	struct work_struct work;
 	enum tb_security_level security_level;
 	u8 *key;
 	u8 connection_id;
