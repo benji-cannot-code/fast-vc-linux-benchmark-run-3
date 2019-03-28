5647c06ae42c8de4c057e1fff73fea481a27a714
Takashi Iwai <tiwai@suse.de>
2019-03-28 16:09:45 +0100

    ALSA: seq: Use kvmalloc() for cell pools
    Use kvmalloc() for allocating cell pools since the pool size can be
    relatively small that may be covered better by slab.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/seq/seq_memory.c
+++ sound/core/seq/seq_memory.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/export.h>
 #include <linux/slab.h>
 #include <linux/sched/signal.h>
-#include <linux/vmalloc.h>
+#include <linux/mm.h>
 #include <sound/core.h>
 
 #include <sound/seq_kernel.h>
@@ -390,8 +390,8 @@ int snd_seq_pool_init(struct snd_seq_pool *pool)
 	if (snd_BUG_ON(!pool))
 		return -EINVAL;
 
-	cellptr = vmalloc(array_size(sizeof(struct snd_seq_event_cell),
-				     pool->size));
+	cellptr = kvmalloc_array(sizeof(struct snd_seq_event_cell), pool->size,
+				 GFP_KERNEL);
 	if (!cellptr)
 		return -ENOMEM;
 
@@ -399,7 +399,7 @@ int snd_seq_pool_init(struct snd_seq_pool *pool)
 	spin_lock_irqsave(&pool->lock, flags);
 	if (pool->ptr) {
 		spin_unlock_irqrestore(&pool->lock, flags);
-		vfree(cellptr);
+		kvfree(cellptr);
 		return 0;
 	}
 
@@ -457,7 +457,7 @@ int snd_seq_pool_done(struct snd_seq_pool *pool)
 	pool->total_elements = 0;
 	spin_unlock_irqrestore(&pool->lock, flags);
 
-	vfree(ptr);
+	kvfree(ptr);
 
 	spin_lock_irqsave(&pool->lock, flags);
 	pool->closing = 0;
