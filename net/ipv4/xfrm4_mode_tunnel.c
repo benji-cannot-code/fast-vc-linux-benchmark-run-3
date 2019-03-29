98c8fad2d98e4fa2a816dcd8862df44cc8fbda13
Florian Westphal <fw@strlen.de>
2019-03-29 21:16:28 +0100

    xfrm: remove input2 indirection from xfrm_mode
    No external dependencies on any module, place this in the core.
    Increase is about 1800 byte for xfrm_input.o.
    
    The beet helpers get added to internal header, as they can be reused
    from xfrm_output.c in the next patch (kernel contains several
    copies of them in the xfrm{4,6}_mode_beet.c files).
    
    Before:
       text    data     bss     dec filename
       5578     176    2364    8118 net/xfrm/xfrm_input.o
       1180      64       0    1244 net/ipv4/xfrm4_mode_beet.o
        171      40       0     211 net/ipv4/xfrm4_mode_transport.o
       1163      40       0    1203 net/ipv4/xfrm4_mode_tunnel.o
       1083      52       0    1135 net/ipv6/xfrm6_mode_beet.o
        172      40       0     212 net/ipv6/xfrm6_mode_ro.o
        172      40       0     212 net/ipv6/xfrm6_mode_transport.o
       1056      40       0    1096 net/ipv6/xfrm6_mode_tunnel.o
    
    After:
       text    data     bss     dec filename
       7373     200    2364    9937 net/xfrm/xfrm_input.o
        587      44       0     631 net/ipv4/xfrm4_mode_beet.o
        171      32       0     203 net/ipv4/xfrm4_mode_transport.o
        649      32       0     681 net/ipv4/xfrm4_mode_tunnel.o
        625      44       0     669 net/ipv6/xfrm6_mode_beet.o
        172      32       0     204 net/ipv6/xfrm6_mode_ro.o
        172      32       0     204 net/ipv6/xfrm6_mode_transport.o
        599      32       0     631 net/ipv6/xfrm6_mode_tunnel.o
    
    v2: pass inner_mode to xfrm_inner_mode_encap_remove to fix
        AF_UNSPEC selector breakage (bisected by Benedict Wong)
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Reviewed-by: Sabrina Dubroca <sd@queasysnail.net>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/ipv4/xfrm4_mode_tunnel.c
+++ net/ipv4/xfrm4_mode_tunnel.c
@@ -16,14 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ip.h>
 #include <net/xfrm.h>
 
-static inline void ipip_ecn_decapsulate(struct sk_buff *skb)
-{
-	struct iphdr *inner_iph = ipip_hdr(skb);
-
-	if (INET_ECN_is_ce(XFRM_MODE_SKB_CB(skb)->tos))
-		IP_ECN_set_ce(inner_iph);
-}
-
 /* Add encapsulation header.
  *
  * The top IP header will be constructed per RFC 2401.
@@ -72,38 +64,7 @@ static int xfrm4_mode_tunnel_output(struct xfrm_state *x, struct sk_buff *skb)
 	return 0;
 }
 
-static int xfrm4_mode_tunnel_input(struct xfrm_state *x, struct sk_buff *skb)
-{
-	int err = -EINVAL;
-
-	if (XFRM_MODE_SKB_CB(skb)->protocol != IPPROTO_IPIP)
-		goto out;
-
-	if (!pskb_may_pull(skb, sizeof(struct iphdr)))
-		goto out;
-
-	err = skb_unclone(skb, GFP_ATOMIC);
-	if (err)
-		goto out;
-
-	if (x->props.flags & XFRM_STATE_DECAP_DSCP)
-		ipv4_copy_dscp(XFRM_MODE_SKB_CB(skb)->tos, ipip_hdr(skb));
-	if (!(x->props.flags & XFRM_STATE_NOECN))
-		ipip_ecn_decapsulate(skb);
-
-	skb_reset_network_header(skb);
-	skb_mac_header_rebuild(skb);
-	if (skb->mac_len)
-		eth_hdr(skb)->h_proto = skb->protocol;
-
-	err = 0;
-
-out:
-	return err;
-}
-
 static struct xfrm_mode xfrm4_tunnel_mode = {
-	.input2 = xfrm4_mode_tunnel_input,
 	.output2 = xfrm4_mode_tunnel_output,
 	.owner = THIS_MODULE,
 	.encap = XFRM_MODE_TUNNEL,
