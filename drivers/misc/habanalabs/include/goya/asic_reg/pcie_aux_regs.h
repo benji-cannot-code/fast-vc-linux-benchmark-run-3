9421262ac698c8813b3745ef001aa798b705f45c
Oded Gabbay <oded.gabbay@gmail.com>
2019-03-31 11:29:53 +0300

    habanalabs: remove trailing blank line from EOF
    GIT does not like extra blank lines at the end of the file, so this patch
    removes those lines.
    
    Signed-off-by: Oded Gabbay <oded.gabbay@gmail.com>
    Reviewed-by: Mukesh Ojha <mojha@codeaurora.org>

--- drivers/misc/habanalabs/include/goya/asic_reg/pcie_aux_regs.h
+++ drivers/misc/habanalabs/include/goya/asic_reg/pcie_aux_regs.h
@@ -241,4 +241,3 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define mmPCIE_AUX_PERST                                             0xC079B8
 
 #endif /* ASIC_REG_PCIE_AUX_REGS_H_ */
-
--- drivers/misc/habanalabs/include/goya/asic_reg/tpc7_cmdq_regs.h
+++ drivers/misc/habanalabs/include/goya/asic_reg/tpc7_cmdq_regs.h
@@ -137,4 +137,3 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define mmTPC7_CMDQ_CQ_BUF_RDATA                                     0xFC930C
 
 #endif /* ASIC_REG_TPC7_CMDQ_REGS_H_ */
-
