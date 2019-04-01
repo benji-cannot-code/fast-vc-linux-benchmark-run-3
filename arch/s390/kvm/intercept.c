b96d9d6082a9b4e794eb2ebdfc416338f347806f
Janosch Frank <frankja@linux.ibm.com>
2019-04-01 10:54:09 +0200

    KVM: s390: protvirt: Handle SE notification interceptions
    Since there is no interception for load control and load psw
    instruction in the protected mode, we need a new way to get notified
    whenever we can inject an IRQ right after the guest has just enabled
    the possibility for receiving them.
    
    The new interception codes solve that problem by providing a
    notification for changes to IRQ enablement relevant bits in CRs 0, 6
    and 14, as well a the machine check mask bit in the PSW.
    
    No special handling is needed for these interception codes, the KVM
    pre-run code will consult all necessary CRs and PSW bits and inject
    IRQs the guest is enabled for.
    
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    [borntraeger@de.ibm.com: patch merging, splitting, fixing]
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>

--- arch/s390/kvm/intercept.c
+++ arch/s390/kvm/intercept.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * in-kernel handling for sie intercepts
  *
- * Copyright IBM Corp. 2008, 2014
+ * Copyright IBM Corp. 2008, 2020
  *
  *    Author(s): Carsten Otte <cotte@de.ibm.com>
  *               Christian Borntraeger <borntraeger@de.ibm.com>
@@ -480,6 +480,15 @@ int kvm_handle_sie_intercept(struct kvm_vcpu *vcpu)
 		break;
 	case ICPT_KSS:
 		rc = kvm_s390_skey_check_enable(vcpu);
+		break;
+	case ICPT_MCHKREQ:
+	case ICPT_INT_ENABLE:
+		/*
+		 * PSW bit 13 or a CR (0, 6, 14) changed and we might
+		 * now be able to deliver interrupts. The pre-run code
+		 * will take care of this.
+		 */
+		rc = 0;
 		break;
 	default:
 		return -EOPNOTSUPP;
