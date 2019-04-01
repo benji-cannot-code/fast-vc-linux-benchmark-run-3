e5742c76ed13b85216d54d824e3ebea9e256baaf
Chris Wilson <chris@chris-wilson.co.uk>
2019-04-01 17:26:40 +0100

    drm/i915: Split out i915_priolist_types into its own header
    For more intel_engine_mask_t detangling. This time so that we can use
    intel_engine_mask_t inside the scheduling structs.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190401162641.10963-2-chris@chris-wilson.co.uk
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>

--- drivers/gpu/drm/i915/intel_engine_types.h
+++ drivers/gpu/drm/i915/intel_engine_types.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 #include "i915_gem.h"
-#include "i915_scheduler_types.h"
+#include "i915_priolist_types.h"
 #include "i915_selftest.h"
 #include "i915_timeline_types.h"
 #include "intel_workarounds_types.h"
