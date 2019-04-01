190d255960cda2f2477d4cfb365e45d36a6e4854
Andrew F. Davis <afd@ti.com>
2019-04-01 10:13:29 -0500

    ASoC: tlv320aic31xx: Add button press detection
    This device can optionally detect headset or microphone button presses.
    Add support for this by passing this event to the jack layer.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/tlv320aic31xx.h
+++ sound/soc/codecs/tlv320aic31xx.h
@@ -22,7 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DAC31XX_BIT			BIT(3)
 
 #define AIC31XX_JACK_MASK (SND_JACK_HEADPHONE | \
-			   SND_JACK_HEADSET)
+			   SND_JACK_HEADSET | \
+			   SND_JACK_BTN_0)
 
 enum aic31xx_type {
 	AIC3100	= 0,
