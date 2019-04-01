fb2dd8a518587d4aa2a2bc7c7c86dfaaf0efbce7
Alexandre Belloni <alexandre.belloni@bootlin.com>
2019-04-01 18:08:08 +0200

    rtc: pcf85063: convert to devm_rtc_allocate_device
    This allows further improvement of the driver.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-pcf85063.c
+++ drivers/rtc/rtc-pcf85063.c
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCF85063_REG_SC			0x04 /* datetime */
 #define PCF85063_REG_SC_OS		0x80
 
-static struct i2c_driver pcf85063_driver;
-
 static int pcf85063_stop_clock(struct i2c_client *client, u8 *ctrl1)
 {
 	int rc;
@@ -221,11 +219,13 @@ static int pcf85063_probe(struct i2c_client *client)
 		dev_warn(&client->dev, "failed to set xtal load capacitance: %d",
 			 err);
 
-	rtc = devm_rtc_device_register(&client->dev,
-				       pcf85063_driver.driver.name,
-				       &pcf85063_rtc_ops, THIS_MODULE);
+	rtc = devm_rtc_allocate_device(&client->dev);
+	if (IS_ERR(rtc))
+		return PTR_ERR(rtc);
 
-	return PTR_ERR_OR_ZERO(rtc);
+	rtc->ops = &pcf85063_rtc_ops;
+
+	return rtc_register_device(rtc);
 }
 
 #ifdef CONFIG_OF
