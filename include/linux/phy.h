97c2e497ebd62384e9e9686c7ccbdc282657f506
Heiner Kallweit <hkallweit1@gmail.com>
2019-04-02 20:43:30 +0200

    net: phy: fix autoneg mismatch case in genphy_read_status
    The original patch didn't consider the case that autoneg process
    finishes successfully but both link partners have no mode in common.
    In this case there's no link, nevertheless we may be interested in
    what the link partner advertised.
    
    Like phydev->link we set phydev->autoneg_complete in
    genphy_update_link() and use the stored value in genphy_read_status().
    This way we don't have to read register BMSR again.
    
    Fixes: b6163f194c69 ("net: phy: improve genphy_read_status")
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -391,6 +391,7 @@ struct phy_device {
 	unsigned autoneg:1;
 	/* The most recently read link state */
 	unsigned link:1;
+	unsigned autoneg_complete:1;
 
 	/* Interrupts are enabled */
 	unsigned interrupts:1;
