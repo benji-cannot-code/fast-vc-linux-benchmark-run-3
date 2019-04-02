c610955e2a59bf4a0a00328c905b19aecda73943
Thor Thayer <thor.thayer@linux.intel.com>
2019-04-02 17:40:56 +0200

    EDAC/altera, firmware/intel: Add Stratix10 ECC DBE SMC call
    Reserve ECC Double Bit Error SMC call to alert U-Boot that a DBE has
    occurred. Move the call from local EDAC header file to a common header.
    
     [ bp: Merge the two patches. ]
    
    Signed-off-by: Thor Thayer <thor.thayer@linux.intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Richard Gong <richard.gong@intel.com>
    Reviewed-by: Alan Tull <atull@kernel.org> # firmware
    Cc: Greg KH <greg@kroah.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: linux-edac <linux-edac@vger.kernel.org>
    Cc: mchehab@kernel.org
    Link: https://lkml.kernel.org/r/1553870639-23895-1-git-send-email-thor.thayer@linux.intel.com
    
    Signed-off-by: Borislav Petkov <bp@suse.de>

--- drivers/edac/altera_edac.c
+++ drivers/edac/altera_edac.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ctype.h>
 #include <linux/delay.h>
 #include <linux/edac.h>
+#include <linux/firmware/intel/stratix10-smc.h>
 #include <linux/genalloc.h>
 #include <linux/interrupt.h>
 #include <linux/irqchip/chained_irq.h>
