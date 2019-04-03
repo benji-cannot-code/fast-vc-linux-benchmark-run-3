a98983cf33745655cc1881ee161e18f5df11bdb3
Yannick Fertré <yannick.fertre@st.com>
2019-04-03 11:24:13 +0200

    drm/stm: ltdc: reset controller to avoid partial refresh
    Display controller reset must be done as soon as possible after enable
    the clock to avoid partial refresh on screen.
    
    Signed-off-by: Yannick Fertré <yannick.fertre@st.com>
    Acked-by: Philippe Cornu <philippe.cornu@st.com>
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/1554283453-2879-1-git-send-email-yannick.fertre@st.com

--- drivers/gpu/drm/stm/ltdc.c
+++ drivers/gpu/drm/stm/ltdc.c
@@ -1138,6 +1138,12 @@ int ltdc_load(struct drm_device *ddev)
 		return -ENODEV;
 	}
 
+	if (!IS_ERR(rstc)) {
+		reset_control_assert(rstc);
+		usleep_range(10, 20);
+		reset_control_deassert(rstc);
+	}
+
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	ldev->regs = devm_ioremap_resource(dev, res);
 	if (IS_ERR(ldev->regs)) {
@@ -1164,11 +1170,6 @@ int ltdc_load(struct drm_device *ddev)
 		}
 	}
 
-	if (!IS_ERR(rstc)) {
-		reset_control_assert(rstc);
-		usleep_range(10, 20);
-		reset_control_deassert(rstc);
-	}
 
 	ret = ltdc_get_caps(ddev);
 	if (ret) {
