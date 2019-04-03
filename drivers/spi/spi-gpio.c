12297ce8205d24d2ed749764e4789523ec5b4710
Andrey Smirnov <andrew.smirnov@gmail.com>
2019-04-02 21:01:31 -0700

    spi: gpio: Make sure spi_master_put() is called in every error path
    There's a number of failure paths in spi_gpio_probe() that do not call
    spi_master_put() potentially leaking memory. Fix this problem by
    registering a cleanup funciont via devm_add_action_or_reset() right
    after SPI controller is allocated.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Chris Healy <cphealy@gmail.com>
    Cc: linux-spi@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-gpio.c
+++ drivers/spi/spi-gpio.c
@@ -363,6 +363,11 @@ static int spi_gpio_probe_pdata(struct platform_device *pdev,
 	return 0;
 }
 
+static void spi_gpio_put(void *data)
+{
+	spi_master_put(data);
+}
+
 static int spi_gpio_probe(struct platform_device *pdev)
 {
 	int				status;
@@ -377,6 +382,10 @@ static int spi_gpio_probe(struct platform_device *pdev)
 	master = spi_alloc_master(dev, sizeof(*spi_gpio));
 	if (!master)
 		return -ENOMEM;
+
+	status = devm_add_action_or_reset(&pdev->dev, spi_gpio_put, master);
+	if (status)
+		return status;
 
 	if (of_id)
 		status = spi_gpio_probe_dt(pdev, master);
@@ -429,11 +438,7 @@ static int spi_gpio_probe(struct platform_device *pdev)
 	}
 	bb->setup_transfer = spi_bitbang_setup_transfer;
 
-	status = spi_bitbang_start(&spi_gpio->bitbang);
-	if (status)
-		spi_master_put(master);
-
-	return status;
+	return spi_bitbang_start(&spi_gpio->bitbang);
 }
 
 static int spi_gpio_remove(struct platform_device *pdev)
@@ -444,8 +449,6 @@ static int spi_gpio_remove(struct platform_device *pdev)
 
 	/* stop() unregisters child devices too */
 	spi_bitbang_stop(&spi_gpio->bitbang);
-
-	spi_master_put(spi_gpio->bitbang.master);
 
 	return 0;
 }
