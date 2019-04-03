0b5aada836f40d7e4c621d918751f5b67879b0d3
kbuild test robot <lkp@intel.com>
2019-04-04 02:16:51 +0800

    spi: bcm2835aux: polling_limit_us can be static
    Fixes: 5fd917afc4bf ("spi: bcm2835aux: make the polling duration limits configurable")
    Signed-off-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-bcm2835aux.c
+++ drivers/spi/spi-bcm2835aux.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 
 /* define polling limits */
-unsigned int polling_limit_us = 30;
+static unsigned int polling_limit_us = 30;
 module_param(polling_limit_us, uint, 0664);
 MODULE_PARM_DESC(polling_limit_us,
 		 "time in us to run a transfer in polling mode - if zero no polling is used\n");
