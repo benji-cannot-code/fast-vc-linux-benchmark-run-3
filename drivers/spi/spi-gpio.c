58ecee8b9fdf524da59aa2e984cf6e8c54a6c541
Andrey Smirnov <andrew.smirnov@gmail.com>
2019-04-02 21:01:30 -0700

    spi: gpio: Drop unused pdev field in struct spi_gpio
    There's no code using 'pdev' field in struct spi_gpio. Drop it.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Chris Healy <cphealy@gmail.com>
    Cc: linux-spi@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-gpio.c
+++ drivers/spi/spi-gpio.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct spi_gpio {
 	struct spi_bitbang		bitbang;
-	struct platform_device		*pdev;
 	struct gpio_desc		*sck;
 	struct gpio_desc		*miso;
 	struct gpio_desc		*mosi;
@@ -390,8 +389,6 @@ static int spi_gpio_probe(struct platform_device *pdev)
 	spi_gpio = spi_master_get_devdata(master);
 
 	platform_set_drvdata(pdev, spi_gpio);
-
-	spi_gpio->pdev = pdev;
 
 	status = spi_gpio_request(dev, spi_gpio);
 	if (status)
