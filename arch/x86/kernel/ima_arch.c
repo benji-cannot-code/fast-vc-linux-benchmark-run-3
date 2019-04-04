57ae457544bb29f2310395deea32580a9e75d83f
Mimi Zohar <zohar@linux.ibm.com>
2019-04-03 22:12:17 -0400

    x86/ima: add missing include
    As reported by 0-DAY kernel test infrastructure:
       arch/x86//kernel/ima_arch.c: In function 'arch_get_ima_policy':
    >> arch/x86//kernel/ima_arch.c:78:4: error: implicit declaration of
    function 'set_module_sig_enforced' [-Werror=implicit-function-declaration]
    
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- arch/x86/kernel/ima_arch.c
+++ arch/x86/kernel/ima_arch.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2018 IBM Corporation
  */
 #include <linux/efi.h>
+#include <linux/module.h>
 #include <linux/ima.h>
 
 extern struct boot_params boot_params;
