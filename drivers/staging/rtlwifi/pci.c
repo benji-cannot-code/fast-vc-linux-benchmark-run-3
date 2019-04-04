7fbf26d90481f171fb88a7830bd85a5591cc63b4
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2019-04-04 16:14:41 -0500

    Staging: rtlwifi: Remove & on function name
    Function name is otherwise used as pointers without &.
    
    Issue suggested by Coccinelle.
    
    Signed-off-by: Madhumitha Prabakaran <madhumithabiw@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtlwifi/pci.c
+++ drivers/staging/rtlwifi/pci.c
@@ -2135,7 +2135,7 @@ static int rtl_pci_intr_mode_msi(struct ieee80211_hw *hw)
 	if (ret < 0)
 		return ret;
 
-	ret = request_irq(rtlpci->pdev->irq, &_rtl_pci_interrupt,
+	ret = request_irq(rtlpci->pdev->irq, _rtl_pci_interrupt,
 			  IRQF_SHARED, KBUILD_MODNAME, hw);
 	if (ret < 0) {
 		pci_disable_msi(rtlpci->pdev);
@@ -2156,7 +2156,7 @@ static int rtl_pci_intr_mode_legacy(struct ieee80211_hw *hw)
 	struct rtl_pci *rtlpci = rtl_pcidev(pcipriv);
 	int ret;
 
-	ret = request_irq(rtlpci->pdev->irq, &_rtl_pci_interrupt,
+	ret = request_irq(rtlpci->pdev->irq, _rtl_pci_interrupt,
 			  IRQF_SHARED, KBUILD_MODNAME, hw);
 	if (ret < 0)
 		return ret;
