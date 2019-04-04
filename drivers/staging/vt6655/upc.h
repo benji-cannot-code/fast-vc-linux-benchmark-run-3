c7c8222d7eb7e2632ee63f96b89efe53f8cf3fdb
Cesar Santos <dev.cmsantos@gmail.com>
2019-04-04 14:59:08 -0300

    staging: vt6655: upc: remove double blank lines
    Fix checkpatch warning "CHECK: Please don't use multiple blank lines"
    on upc.h
    
    Signed-off-by: Cesar Santos <dev.cmsantos@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6655/upc.h
+++ drivers/staging/vt6655/upc.h
@@ -21,9 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*---------------------  Export Definitions -------------------------*/
 
-
 /* For memory mapped IO */
-
 
 #define VNSvInPortB(dwIOAddress, pbyData) \
 	(*(pbyData) = ioread8(dwIOAddress))
