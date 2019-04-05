9bf2fb22275dd0d233ce85cee8c619a7f12be8b5
Paul Burton <paul.burton@mips.com>
2019-04-05 22:50:35 +0000

    MIPS: jump_label: Remove redundant nops
    Both arch_static_branch() & arch_static_branch_jump() emit a control
    transfer instruction (ie. branch or jump) without disabling assembler
    re-ordering. As such the assembler will automatically fill their delay
    slots.
    
    Both functions follow their branch or jump with an explicit nop that at
    first appears to be there to fill the delay slot, but given that the
    assembler will do that the explicit nops serve no purpose & we end up
    with our branch or jump followed by 2 nops. Remove the redundant nops.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: linux-mips@vger.kernel.org

--- arch/mips/include/asm/jump_label.h
+++ arch/mips/include/asm/jump_label.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static __always_inline bool arch_static_branch(struct static_key *key, bool branch)
 {
 	asm_volatile_goto("1:\t" B_INSN " 2f\n\t"
-		"2:\tnop\n\t"
+		"2:\t.insn\n\t"
 		".pushsection __jump_table,  \"aw\"\n\t"
 		WORD_INSN " 1b, %l[l_yes], %0\n\t"
 		".popsection\n\t"
@@ -44,7 +44,6 @@ l_yes:
 static __always_inline bool arch_static_branch_jump(struct static_key *key, bool branch)
 {
 	asm_volatile_goto("1:\tj %l[l_yes]\n\t"
-		"nop\n\t"
 		".pushsection __jump_table,  \"aw\"\n\t"
 		WORD_INSN " 1b, %l[l_yes], %0\n\t"
 		".popsection\n\t"
