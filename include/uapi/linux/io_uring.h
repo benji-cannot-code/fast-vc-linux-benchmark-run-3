5b20bd81ac390b3bb11f8888958425827fe8d6a3
Jens Axboe <axboe@kernel.dk>
2019-04-06 21:51:27 -0600

    io_uring: add support for marking commands as draining
    There are no ordering constraints between the submission and completion
    side of io_uring. But sometimes that would be useful to have. One common
    example is doing an fsync, for instance, and have it ordered with
    previous writes. Without support for that, the application must do this
    tracking itself.
    
    This adds a general SQE flag, IOSQE_IO_DRAIN. If a command is marked
    with this flag, then it will not be issued before previous commands have
    completed, and subsequent commands submitted after the drain will not be
    issued before the drain is started.. If there are no pending commands,
    setting this flag will not change the behavior of the issue of the
    command.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/uapi/linux/io_uring.h
+++ include/uapi/linux/io_uring.h
@@ -39,6 +39,7 @@ struct io_uring_sqe {
  * sqe->flags
  */
 #define IOSQE_FIXED_FILE	(1U << 0)	/* use fixed fileset */
+#define IOSQE_IO_DRAIN		(1U << 1)	/* issue after inflight IO */
 
 /*
  * io_uring_setup() flags
