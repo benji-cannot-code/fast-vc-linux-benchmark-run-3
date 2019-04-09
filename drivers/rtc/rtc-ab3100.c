1aa49e060da60c8dbbc89e1f1e44671bfb29a74a
Alexandre Belloni <alexandre.belloni@bootlin.com>
2019-04-09 21:59:45 +0200

    rtc: ab3100: convert to SPDX identifier
    Use SPDX-License-Identifier instead of the custom license line.
    
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-ab3100.c
+++ drivers/rtc/rtc-ab3100.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2007-2009 ST-Ericsson AB
- * License terms: GNU General Public License (GPL) version 2
  * RTC clock driver for the AB3100 Analog Baseband Chip
  * Author: Linus Walleij <linus.walleij@stericsson.com>
  */
