797ce9f32aa0515f3c6bfb0f2c747cd15ee6d7b9
Sean Nyekjaer <sean@geanix.com>
2019-04-09 10:39:48 +0200

    can: flexcan: add support for DT property 'wakeup-source'
    The flexcan controller can be forced as a wakeup source by
    stating that explicitly in the device's .dts file using the
    "wakeup-source" boolean property.
    
    Signed-off-by: Sean Nyekjaer <sean@geanix.com>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1474,6 +1474,9 @@ static int flexcan_setup_stop_mode(struct platform_device *pdev)
 
 	device_set_wakeup_capable(&pdev->dev, true);
 
+	if (of_property_read_bool(np, "wakeup-source"))
+		device_set_wakeup_enable(&pdev->dev, true);
+
 	return 0;
 }
 
