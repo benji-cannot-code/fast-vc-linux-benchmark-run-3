fc97994601f95a586fc17f9ee039e4843fd959d4
Tomer Tayar <ttayar@habana.ai>
2019-04-10 15:18:46 +0300

    habanalabs: Cancel pr_fmt() definition dependency on includes order
    pr_fmt() should be defined before including linux/printk.h, either
    directly or indirectly, in order to avoid redefinition of the macro.
    Currently the macro definition is in habanalabs.h, which is included in
    many files, and that makes the addition/reorder of includes to be prone
    to compilation errors.
    This patch cancels this dependency by defining the macro only in the few
    source files that use it.
    
    Signed-off-by: Tomer Tayar <ttayar@habana.ai>
    Signed-off-by: Oded Gabbay <oded.gabbay@gmail.com>

--- drivers/misc/habanalabs/device.c
+++ drivers/misc/habanalabs/device.c
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * All Rights Reserved.
  */
 
+#define pr_fmt(fmt)			"habanalabs: " fmt
+
 #include "habanalabs.h"
 
 #include <linux/pci.h>
--- drivers/misc/habanalabs/habanalabs.h
+++ drivers/misc/habanalabs/habanalabs.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "include/armcp_if.h"
 #include "include/qman_if.h"
 
-#define pr_fmt(fmt)			"habanalabs: " fmt
-
 #include <linux/cdev.h>
 #include <linux/iopoll.h>
 #include <linux/irqreturn.h>
