a873b89a8b2cf87cfe89d2b8aa500088d962e218
Alexandre Torgue <alexandre.torgue@st.com>
2019-04-10 13:30:22 +0200

    pinctrl: stm32: add package information for stm32mp157c
    This patch adds four new packages support for stm32mp157c die:
    
    STM32MP_PKG_AA: LFBGA448 (18*18), 176 IOs
    STM32MP_PKG_AB: LFBGA354 (16*16), 98 IOs
    STM32MP_PKG_AC: TFBGA361 (12*12), 148 IOs
    STM32MP_PKG_AD: TFBGA257 (10*10), 98 IOs
    
    Signed-off-by: Alexandre Torgue <alexandre.torgue@st.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/stm32/pinctrl-stm32.h
+++ drivers/pinctrl/stm32/pinctrl-stm32.h
@@ -19,6 +19,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define STM32_PIN_AF(x)		((x) + 1)
 #define STM32_PIN_ANALOG	(STM32_PIN_AF(15) + 1)
 
+/*  package information */
+#define STM32MP_PKG_AA		BIT(0)
+#define STM32MP_PKG_AB		BIT(1)
+#define STM32MP_PKG_AC		BIT(2)
+#define STM32MP_PKG_AD		BIT(3)
+
 struct stm32_desc_function {
 	const char *name;
 	const unsigned char num;
