73d0d16fb25be00dff6c9efd57a3626e452203cd
Daniel Junho <djunho@gmail.com>
2019-04-09 21:27:05 -0300

    staging: rtl8192e: remove a blank line
    Fix the checkpath error:
    CHECK: Blank lines aren't necessary after an open brace '{'
    
    Signed-off-by: Daniel Junho <djunho@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8192E_cmdpkt.c
+++ drivers/staging/rtl8192e/rtl8192e/r8192E_cmdpkt.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 bool rtl92e_send_cmd_pkt(struct net_device *dev, u32 type, const void *data,
 			 u32 len)
 {
-
 	bool				rt_status = true;
 	struct r8192_priv *priv = rtllib_priv(dev);
 	u16				frag_length = 0, frag_offset = 0;
