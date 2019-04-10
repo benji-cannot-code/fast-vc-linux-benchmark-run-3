f773a60b5d4c4081215af7768760bd9fef85eb77
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2019-04-11 00:22:40 +0200

    mmc: tmio: move runtime PM enablement to the driver implementations
    Both the Renesas and Uniphier implementations perform actions which
    affect runtime PM before calling into the core tmio_mmc_host_probe()
    which enabled runtime PM. Move pm_runtime_enable() from the core and
    tmio_mmc_host_probe() into each drivers probe() so it can be called
    before any clocks or other resources are switched on.
    
    Reported-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Acked-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -771,6 +771,8 @@ int renesas_sdhi_probe(struct platform_device *pdev,
 	/* All SDHI have SDIO status bits which must be 1 */
 	mmc_data->flags |= TMIO_MMC_SDIO_STATUS_SETBITS;
 
+	pm_runtime_enable(&pdev->dev);
+
 	ret = renesas_sdhi_clk_enable(host);
 	if (ret)
 		goto efree;
@@ -851,6 +853,8 @@ edisclk:
 efree:
 	tmio_mmc_host_free(host);
 
+	pm_runtime_disable(&pdev->dev);
+
 	return ret;
 }
 EXPORT_SYMBOL_GPL(renesas_sdhi_probe);
@@ -861,6 +865,8 @@ int renesas_sdhi_remove(struct platform_device *pdev)
 
 	tmio_mmc_host_remove(host);
 	renesas_sdhi_clk_disable(host);
+
+	pm_runtime_disable(&pdev->dev);
 
 	return 0;
 }
