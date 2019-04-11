3897df1d17d3f332dbfb18c6e59c51f673331596
Bart Van Assche <bvanassche@acm.org>
2019-04-11 14:53:22 -0700

    scsi: qla2xxx: Move the <linux/io-64-nonatomic-lo-hi.h> include directive
    The <linux/io-64-nonatomic-lo-hi.h> header file is included because of the
    readq() macro. Since that macro is only used in qla_nx.c, move that include
    statement into qla_nx.c.
    
    Cc: Himanshu Madhani <hmadhani@marvell.com>
    Cc: Giridhar Malavali <gmalavali@marvell.com>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Acked-by: Himanshu Madhani <hmadhani@marvell.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/qla2xxx/qla_nx.c
+++ drivers/scsi/qla2xxx/qla_nx.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include "qla_def.h"
 #include <linux/delay.h>
+#include <linux/io-64-nonatomic-lo-hi.h>
 #include <linux/pci.h>
 #include <linux/ratelimit.h>
 #include <linux/vmalloc.h>
