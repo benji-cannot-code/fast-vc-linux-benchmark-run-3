bc38020c8544ffd6a724877c94e1018c144e6c38
Dave Martin <Dave.Martin@arm.com>
2019-04-11 17:09:59 +0100

    KVM: arm64/sve: Simplify KVM_REG_ARM64_SVE_VLS array sizing
    A complicated DIV_ROUND_UP() expression is currently written out
    explicitly in multiple places in order to specify the size of the
    bitmap exchanged with userspace to represent the value of the
    KVM_REG_ARM64_SVE_VLS pseudo-register.
    
    Userspace currently has no direct way to work this out either: for
    documentation purposes, the size is just quoted as 8 u64s.
    
    To make this more intuitive, this patch replaces these with a
    single define, which is also exported to userspace as
    KVM_ARM64_SVE_VLS_WORDS.
    
    Since the number of words in a bitmap is just the index of the last
    word used + 1, this patch expresses the bound that way instead.
    This should make it clearer what is being expressed.
    
    For userspace convenience, the minimum and maximum possible vector
    lengths relevant to the KVM ABI are exposed to UAPI as
    KVM_ARM64_SVE_VQ_MIN, KVM_ARM64_SVE_VQ_MAX.  Since the only direct
    use for these at present is manipulation of KVM_REG_ARM64_SVE_VLS,
    no corresponding _VL_ macros are defined.  They could be added
    later if a need arises.
    
    Since use of DIV_ROUND_UP() was the only reason for including
    <linux/kernel.h> in guest.c, this patch also removes that #include.
    
    Suggested-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Dave Martin <Dave.Martin@arm.com>
    Reviewed-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/kvm/guest.c
+++ arch/arm64/kvm/guest.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/err.h>
 #include <linux/nospec.h>
-#include <linux/kernel.h>
 #include <linux/kvm_host.h>
 #include <linux/module.h>
 #include <linux/stddef.h>
@@ -211,7 +210,7 @@ out:
 #define vq_mask(vq) ((u64)1 << ((vq) - SVE_VQ_MIN) % 64)
 
 static bool vq_present(
-	const u64 (*const vqs)[DIV_ROUND_UP(SVE_VQ_MAX - SVE_VQ_MIN + 1, 64)],
+	const u64 (*const vqs)[KVM_ARM64_SVE_VLS_WORDS],
 	unsigned int vq)
 {
 	return (*vqs)[vq_word(vq)] & vq_mask(vq);
@@ -220,7 +219,7 @@ static bool vq_present(
 static int get_sve_vls(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 {
 	unsigned int max_vq, vq;
-	u64 vqs[DIV_ROUND_UP(SVE_VQ_MAX - SVE_VQ_MIN + 1, 64)];
+	u64 vqs[KVM_ARM64_SVE_VLS_WORDS];
 
 	if (!vcpu_has_sve(vcpu))
 		return -ENOENT;
@@ -244,7 +243,7 @@ static int get_sve_vls(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 static int set_sve_vls(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 {
 	unsigned int max_vq, vq;
-	u64 vqs[DIV_ROUND_UP(SVE_VQ_MAX - SVE_VQ_MIN + 1, 64)];
+	u64 vqs[KVM_ARM64_SVE_VLS_WORDS];
 
 	if (!vcpu_has_sve(vcpu))
 		return -ENOENT;
