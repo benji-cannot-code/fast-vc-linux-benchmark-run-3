c43a7473857acbc765b8ff06e309761d78b3433e
Paul Gortmaker <paul.gortmaker@windriver.com>
2019-04-12 22:11:31 -0400

    printk: Tie printk_once / printk_deferred_once into .data.once for reset
    In commit b1fca27d384e ("kernel debug: support resetting WARN*_ONCE")
    we got the opportunity to reset state on the one shot messages,
    without having to reboot.
    
    However printk_once (printk_deferred_once) live in a different file
    and didn't get the same kind of update/conversion, so they remain
    unconditionally one shot, until the system is rebooted.
    
    For example, we currently have:
    
      sched/rt.c: printk_deferred_once("sched: RT throttling activated\n");
    
    ..which could reasonably be tripped as someone is testing and tuning
    a new system/workload and their task placements.  For consistency, and
    to avoid reboots in the same vein as the original commit, we make these
    two instances of _once the same as the WARN*_ONCE instances are.
    
    Link: http://lkml.kernel.org/r/1555121491-31213-1-git-send-email-paul.gortmaker@windriver.com
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -348,7 +348,7 @@ extern int kptr_restrict;
 #ifdef CONFIG_PRINTK
 #define printk_once(fmt, ...)					\
 ({								\
-	static bool __print_once __read_mostly;			\
+	static bool __section(.data.once) __print_once;		\
 	bool __ret_print_once = !__print_once;			\
 								\
 	if (!__print_once) {					\
@@ -359,7 +359,7 @@ extern int kptr_restrict;
 })
 #define printk_deferred_once(fmt, ...)				\
 ({								\
-	static bool __print_once __read_mostly;			\
+	static bool __section(.data.once) __print_once;		\
 	bool __ret_print_once = !__print_once;			\
 								\
 	if (!__print_once) {					\
