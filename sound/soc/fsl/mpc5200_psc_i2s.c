b6d4b57fc4ac009dc73586b129441c5ae7bc12cc
Andra Danciu <andradanciu1997@gmail.com>
2019-04-14 22:14:50 +0300

    ASoC: mpc5200_psc_i2s: Fix invalid license ID
    As the file had no other license notice/reference, it falls under the
    project license and therefore the proper SPDX id is: GPL-2.0-only
    
    Cc: Daniel Baluta <daniel.baluta@nxp.com>
    Fixes: 864a8472c4412 ("ASoC: mpc5200_psc_i2s: Switch to SPDX identifier")
    Reported-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andra Danciu <andradanciu1997@gmail.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/fsl/mpc5200_psc_i2s.c
+++ sound/soc/fsl/mpc5200_psc_i2s.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-// SPDX-License-Identifier: GPL
+// SPDX-License-Identifier: GPL-2.0-only
 //
 // Freescale MPC5200 PSC in I2S mode
 // ALSA SoC Digital Audio Interface (DAI) driver
