e8983a5f4f25ceaf3b89bc2335e5fb2c8e4f64ac
Christoph Hellwig <hch@lst.de>
2019-04-15 11:14:32 +0200

    riscv: use asm-generic/extable.h
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- arch/riscv/include/asm/uaccess.h
+++ arch/riscv/include/asm/uaccess.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/thread_info.h>
 #include <asm/byteorder.h>
+#include <asm/extable.h>
 #include <asm/asm.h>
 
 #define __enable_user_access()							\
@@ -98,12 +99,6 @@ static inline int __access_ok(unsigned long addr, unsigned long size)
  * we don't even have to jump over them.  Further, they do not intrude
  * on our cache or tlb entries.
  */
-
-struct exception_table_entry {
-	unsigned long insn, fixup;
-};
-
-extern int fixup_exception(struct pt_regs *state);
 
 #if defined(__LITTLE_ENDIAN)
 #define __MSW	1
