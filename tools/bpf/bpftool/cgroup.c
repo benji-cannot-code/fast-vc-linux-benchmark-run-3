b11d104fd72c3ca3aa6b611a165fd20a2aaf2d92
Andrey Ignatov <rdna@fb.com>
2019-04-16 13:13:47 -0700

    bpftool: Support sysctl hook
    Add support for recently added BPF_PROG_TYPE_CGROUP_SYSCTL program type
    and BPF_CGROUP_SYSCTL attach type.
    
    Example of bpftool output with sysctl program from selftests:
    
      # bpftool p load ./test_sysctl_prog.o /mnt/bpf/sysctl_prog type cgroup/sysctl
      # bpftool p l
      9: cgroup_sysctl  name sysctl_tcp_mem  tag 0dd05f81a8d0d52e  gpl
              loaded_at 2019-04-16T12:57:27-0700  uid 0
              xlated 1008B  jited 623B  memlock 4096B
      # bpftool c a /mnt/cgroup2/bla sysctl id 9
      # bpftool c t
      CgroupPath
      ID       AttachType      AttachFlags     Name
      /mnt/cgroup2/bla
          9        sysctl                          sysctl_tcp_mem
      # bpftool c d /mnt/cgroup2/bla sysctl id 9
      # bpftool c t
      CgroupPath
      ID       AttachType      AttachFlags     Name
    
    Signed-off-by: Andrey Ignatov <rdna@fb.com>
    Acked-by: Song Liu <songliubraving@fb.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/bpf/bpftool/cgroup.c
+++ tools/bpf/bpftool/cgroup.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	"       ATTACH_TYPE := { ingress | egress | sock_create |\n"	       \
 	"                        sock_ops | device | bind4 | bind6 |\n"	       \
 	"                        post_bind4 | post_bind6 | connect4 |\n"       \
-	"                        connect6 | sendmsg4 | sendmsg6 }"
+	"                        connect6 | sendmsg4 | sendmsg6 | sysctl }"
 
 static const char * const attach_type_strings[] = {
 	[BPF_CGROUP_INET_INGRESS] = "ingress",
@@ -42,6 +42,7 @@ static const char * const attach_type_strings[] = {
 	[BPF_CGROUP_INET6_POST_BIND] = "post_bind6",
 	[BPF_CGROUP_UDP4_SENDMSG] = "sendmsg4",
 	[BPF_CGROUP_UDP6_SENDMSG] = "sendmsg6",
+	[BPF_CGROUP_SYSCTL] = "sysctl",
 	[__MAX_BPF_ATTACH_TYPE] = NULL,
 };
 
