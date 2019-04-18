f0da0ba7a6ae0a9cff10b10c255821d52df3247e
Corentin Labbe <clabbe.montjoie@gmail.com>
2019-04-18 10:17:32 +0200

    crypto: sun4i-ss - Handle better absence/presence of IV
    This patch remove the test against areq->info since sun4i-ss could work
    without it (ECB).
    
    Fixes: 6298e948215f ("crypto: sunxi-ss - Add Allwinner Security System crypto accelerator")
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/sunxi-ss/sun4i-ss-cipher.c
+++ drivers/crypto/sunxi-ss/sun4i-ss-cipher.c
@@ -42,11 +42,6 @@ static int sun4i_ss_opti_poll(struct skcipher_request *areq)
 	if (!areq->cryptlen)
 		return 0;
 
-	if (!areq->iv) {
-		dev_err_ratelimited(ss->dev, "ERROR: Empty IV\n");
-		return -EINVAL;
-	}
-
 	if (!areq->src || !areq->dst) {
 		dev_err_ratelimited(ss->dev, "ERROR: Some SGs are NULL\n");
 		return -EINVAL;
@@ -157,11 +152,6 @@ static int sun4i_ss_cipher_poll(struct skcipher_request *areq)
 
 	if (!areq->cryptlen)
 		return 0;
-
-	if (!areq->iv) {
-		dev_err_ratelimited(ss->dev, "ERROR: Empty IV\n");
-		return -EINVAL;
-	}
 
 	if (!areq->src || !areq->dst) {
 		dev_err_ratelimited(ss->dev, "ERROR: Some SGs are NULL\n");
