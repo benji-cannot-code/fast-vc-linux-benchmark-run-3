e545b491e1fa36a8209f2d25b0ebdaa93b87ae09
Arnd Bergmann <arnd@arndb.de>
2019-04-18 22:57:08 +0200

    isdn: hdlc: move into mISDN
    The last remnant of the isdn4linux interface is now the isdnhdlc
    support, used by the netjet driver. Move it next to that driver.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/isdn/hardware/mISDN/isdnhdlc.c
+++ drivers/isdn/hardware/mISDN/isdnhdlc.c
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/crc-ccitt.h>
-#include <linux/isdn/hdlc.h>
 #include <linux/bitrev.h>
+#include "isdnhdlc.h"
 
 /*-------------------------------------------------------------------*/
 
