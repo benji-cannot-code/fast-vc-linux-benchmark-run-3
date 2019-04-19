a206e9255ee580f46b40f3eeb16b06ecd491bf86
Alan Somers <asomers@FreeBSD.org>
2019-04-19 15:42:46 -0600

    fuse: fix changelog entry for protocol 7.9
    Retroactively add changelog entry for the atime and mtime "now" flags.
    This was an oversight in commit 17637cbaba59 ("fuse: improve utimes
    support").
    
    Signed-off-by: Alan Somers <asomers@FreeBSD.org>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  - add lock_owner field to fuse_setattr_in, fuse_read_in and fuse_write_in
  *  - add blksize field to fuse_attr
  *  - add file flags field to fuse_read_in and fuse_write_in
+ *  - Add ATIME_NOW and MTIME_NOW flags to fuse_setattr_in
  *
  * 7.10
  *  - add nonseekable open flag
