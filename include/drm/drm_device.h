eec00ef5779f5ce4530b424ffc96a5337e3c5619
Dave Airlie <airlied@redhat.com>
2019-04-23 09:56:01 +1000

    drm/legacy: place all drm legacy members under DRM_LEGACY.
    This places a bunch of the legacy members of drm_device into
    only being there when legacy is enabled.
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- include/drm/drm_device.h
+++ include/drm/drm_device.h
@@ -307,7 +307,7 @@ struct drm_device {
 
 	/* Everything below here is for legacy driver, never use! */
 	/* private: */
-
+#if IS_ENABLED(CONFIG_DRM_LEGACY)
 	/* Context handle management - linked list of context handles */
 	struct list_head ctxlist;
 
@@ -354,6 +354,7 @@ struct drm_device {
 
 	/* Scatter gather memory */
 	struct drm_sg_mem *sg;
+#endif
 };
 
 #endif
