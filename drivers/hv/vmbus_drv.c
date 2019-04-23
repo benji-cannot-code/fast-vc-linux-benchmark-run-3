dbfdd65597360aa87a19f87a996bbd6767a9c532
Joseph Salisbury <Joseph.Salisbury@microsoft.com>
2019-04-23 03:47:27 +0000

    drivers: hv: Add a module description line to the hv_vmbus driver
    This patch only adds a MODULE_DESCRIPTION statement to the driver.
    This change is only cosmetic, so there should be no runtime impact.
    
    Signed-off-by: Joseph Salisbury <joseph.salisbury@microsoft.com>
    Reviewed-by: Michael Kelley <mikelley@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/hv/vmbus_drv.c
+++ drivers/hv/vmbus_drv.c
@@ -2162,6 +2162,7 @@ static void __exit vmbus_exit(void)
 
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Microsoft Hyper-V VMBus Driver");
 
 subsys_initcall(hv_acpi_init);
 module_exit(vmbus_exit);
