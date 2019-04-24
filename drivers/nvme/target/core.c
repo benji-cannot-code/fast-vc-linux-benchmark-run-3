bdaedb5a9ea3198abfcc4c19e0f3c7530392eacb
Enrico Weigelt, metux IT consult <info@metux.net>
2019-04-24 12:34:39 +0200

    nvmet: include <linux/scatterlist.h>
    Build breaks:
    
        drivers/nvme/target/core.c: In function 'nvmet_req_alloc_sgl':
        drivers/nvme/target/core.c:939:12: error: implicit declaration of \
    function 'sgl_alloc'; did you mean 'bio_alloc'? \
    [-Werror=implicit-function-declaration]
          req->sg = sgl_alloc(req->transfer_len, GFP_KERNEL, &req->sg_cnt);
                    ^~~~~~~~~
                    bio_alloc
        drivers/nvme/target/core.c:939:10: warning: assignment makes pointer \
    from integer without a cast [-Wint-conversion]
          req->sg = sgl_alloc(req->transfer_len, GFP_KERNEL, &req->sg_cnt);
                  ^
        drivers/nvme/target/core.c: In function 'nvmet_req_free_sgl':
        drivers/nvme/target/core.c:952:3: error: implicit declaration of \
    function 'sgl_free'; did you mean 'ida_free'? [-Werror=implicit-function-declaration]
           sgl_free(req->sg);
           ^~~~~~~~
           ida_free
    
    Cause:
    
        1. missing include to <linux/scatterlist.h>
        2. SGL_ALLOC needs to be enabled
    
    Therefore adding the missing include, as well as Kconfig dependency.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Minwoo Im <minwoo.im@samsung.com>
    Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/target/core.c
+++ drivers/nvme/target/core.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/random.h>
 #include <linux/rculist.h>
 #include <linux/pci-p2pdma.h>
+#include <linux/scatterlist.h>
 
 #include "nvmet.h"
 
