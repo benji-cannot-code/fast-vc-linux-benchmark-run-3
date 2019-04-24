a80b8e9172e53b35969d264bf4459d1be2582f84
Kees Cook <keescook@chromium.org>
2019-04-24 16:12:36 -0700

    selftests: Remove KSFT_TAP_LEVEL
    Since sub-testing can now be detected by indentation level, this removes
    KSFT_TAP_LEVEL so that subtests report their TAP header for later parsing.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/kselftest/runner.sh
+++ tools/testing/selftests/kselftest/runner.sh
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # SPDX-License-Identifier: GPL-2.0
 #
 # Runs a set of tests in a given subdirectory.
-export KSFT_TAP_LEVEL=1
 export skip_rc=4
 export logfile=/dev/stdout
 export per_test_logging=
