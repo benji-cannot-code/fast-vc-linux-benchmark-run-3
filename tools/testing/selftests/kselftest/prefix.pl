fc95f5348bfc3cb81362655832834d3a5917346a
Kees Cook <keescook@chromium.org>
2019-04-24 16:12:35 -0700

    selftests: Move test output to diagnostic lines
    This changes the selftest output so that each test's output is prefixed
    with "# " as a TAP "diagnostic line".
    
    This creates a bit of a kernel-specific TAP dialect where the diagnostics
    precede the results. The TAP spec isn't entirely clear about this, though,
    so I think it's the correct solution so as to keep interactive runs making
    sense. If the output _followed_ the result line in the spec-suggested
    YAML form, each test would dump all of its output at once instead of as
    it went, making debugging harder.
    
    This does, however, solve the recursive TAP output problem, as sub-tests
    will simply be prefixed by "# ". Parsing sub-tests becomes a simple
    problem of just removing the first two characters of a given top-level
    test's diagnostic output, and parsing the results.
    
    Note that the shell construct needed to both get an exit code from
    the first command in a pipe and still filter the pipe (to add the "# "
    prefix) uses a POSIX solution rather than the bash "pipefail" option
    which is not supported by dash.
    
    Since some test environments may have a very minimal set of utilities
    available, the new prefixing code will fall back to doing line-at-a-time
    prefixing if perl and/or stdbuf are not available.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

---
+++ tools/testing/selftests/kselftest/prefix.pl
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#!/usr/bin/perl
+# SPDX-License-Identifier: GPL-2.0
+# Prefix all lines with "# ", unbuffered. Command being piped in may need
+# to have unbuffering forced with "stdbuf -i0 -o0 -e0 $cmd".
+use strict;
+
+binmode STDIN;
+binmode STDOUT;
+
+STDOUT->autoflush(1);
+
+my $needed = 1;
+while (1) {
+	my $char;
+	my $bytes = sysread(STDIN, $char, 1);
+	exit 0 if ($bytes == 0);
+	if ($needed) {
+		print "# ";
+		$needed = 0;
+	}
+	print $char;
+	$needed = 1 if ($char eq "\n");
+}
