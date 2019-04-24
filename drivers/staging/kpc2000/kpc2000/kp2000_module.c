0f39f517ca3af661e6b2bf7400b145364659edef
YueHaibing <yuehaibing@huawei.com>
2019-04-24 02:50:00 +0000

    staging: kpc2000: remove duplicated include from kp2000_module.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Mukesh Ojha <mojha@codeaurora.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/kpc2000/kpc2000/kp2000_module.c
+++ drivers/staging/kpc2000/kpc2000/kp2000_module.c
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/export.h>
 #include <linux/slab.h>
-#include <linux/pci.h>
 #include <asm/io.h>
 #include <linux/io.h>
 #include <linux/mfd/core.h>
