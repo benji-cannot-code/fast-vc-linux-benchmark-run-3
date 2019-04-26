57c868788e3dd000baf19fdd1776d601c1acd8e2
Chris Wilson <chris@chris-wilson.co.uk>
2019-04-26 09:17:25 +0100

    drm/i915: Move sandybride pcode access to intel_sideband.c
    sandybride_pcode is another sideband, so move it to their new home.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190426081725.31217-8-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/intel_hdcp.c
+++ drivers/gpu/drm/i915/intel_hdcp.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i915_reg.h"
 #include "intel_drv.h"
 #include "intel_hdcp.h"
+#include "intel_sideband.h"
 
 #define KEY_LOAD_TRIES	5
 #define ENCRYPT_STATUS_CHANGE_TIMEOUT_MS	50
