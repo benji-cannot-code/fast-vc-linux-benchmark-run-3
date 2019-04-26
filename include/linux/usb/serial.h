0ec6f66a325ded335fdbe4597660a0e1d1db4467
Johan Hovold <johan@kernel.org>
2019-04-26 08:01:32 +0200

    USB: serial: drop unused iflag macro
    Drop the RELEVANT_IFLAG() macro which essentially hasn't been used for
    over a decade except in some remnant debug printks that were recently
    removed.
    
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- include/linux/usb/serial.h
+++ include/linux/usb/serial.h
@@ -24,9 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* The maximum number of ports one device can grab at once */
 #define MAX_NUM_PORTS		16
 
-/* parity check flag */
-#define RELEVANT_IFLAG(iflag)	(iflag & (IGNBRK|BRKINT|IGNPAR|PARMRK|INPCK))
-
 /* USB serial flags */
 #define USB_SERIAL_WRITE_BUSY	0
 #define USB_SERIAL_THROTTLED	1
