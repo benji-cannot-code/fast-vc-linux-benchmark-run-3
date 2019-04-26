2809f199b4fe20acb717dfff2eeb9b825e4b550e
Christophe Leroy <christophe.leroy@c-s.fr>
2019-04-26 15:57:59 +0000

    powerpc/mm: drop __bad_pte()
    This has never been called (since Kernel has been in git at least),
    drop it.
    
    Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/book3s/32/pgalloc.h
+++ arch/powerpc/include/asm/book3s/32/pgalloc.h
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define MAX_PGTABLE_INDEX_SIZE	0xf
 
-extern void __bad_pte(pmd_t *pmd);
-
 extern struct kmem_cache *pgtable_cache[];
 #define PGT_CACHE(shift) pgtable_cache[shift]
 
