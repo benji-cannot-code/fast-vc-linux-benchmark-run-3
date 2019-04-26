35a9e87d891dc8b5cb8a4fa9671182f8e81ac335
Christophe Leroy <christophe.leroy@c-s.fr>
2019-04-26 16:23:26 +0000

    powerpc: prepare string/mem functions for KASAN
    CONFIG_KASAN implements wrappers for memcpy() memmove() and memset()
    Those wrappers are doing the verification then call respectively
    __memcpy() __memmove() and __memset(). The arches are therefore
    expected to rename their optimised functions that way.
    
    For files on which KASAN is inhibited, #defines are used to allow
    them to directly call optimised versions of the functions without
    going through the KASAN wrappers.
    
    See commit 393f203f5fd5 ("x86_64: kasan: add interceptors for
    memset/memmove/memcpy functions") for details.
    
    Other string / mem functions do not (yet) have kasan wrappers,
    we therefore have to fallback to the generic versions when
    KASAN is active, otherwise KASAN checks will be skipped.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    [mpe: Fixups to keep selftests working]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

---
+++ arch/powerpc/include/asm/kasan.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __ASM_KASAN_H
+#define __ASM_KASAN_H
+
+#ifdef CONFIG_KASAN
+#define _GLOBAL_KASAN(fn)	_GLOBAL(__##fn)
+#define _GLOBAL_TOC_KASAN(fn)	_GLOBAL_TOC(__##fn)
+#define EXPORT_SYMBOL_KASAN(fn)	EXPORT_SYMBOL(__##fn)
+#else
+#define _GLOBAL_KASAN(fn)	_GLOBAL(fn)
+#define _GLOBAL_TOC_KASAN(fn)	_GLOBAL_TOC(fn)
+#define EXPORT_SYMBOL_KASAN(fn)
+#endif
+
+#endif
