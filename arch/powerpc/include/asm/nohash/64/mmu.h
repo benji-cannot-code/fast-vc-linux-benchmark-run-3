e0c441de4c7089733ce80312ccf609c88ae5b93c
Christophe Leroy <christophe.leroy@c-s.fr>
2019-04-26 15:58:01 +0000

    powerpc/mm: convert Book3E 64 to pte_fragment
    Book3E 64 is the only subarch not using pte_fragment. In order
    to allow refactorisation, this patch converts it to pte_fragment.
    
    Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/nohash/64/mmu.h
+++ arch/powerpc/include/asm/nohash/64/mmu.h
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MAX_PHYSMEM_BITS        44
 
+#include <asm/page.h>
+
 /* Freescale Book-E software loaded TLB or Book-3e (ISA 2.06+) MMU */
 #include <asm/nohash/mmu-book3e.h>
 
 #ifndef __ASSEMBLY__
-typedef struct page *pgtable_t;
+typedef pte_t *pgtable_t;
 #endif
 
 #endif /* _ASM_POWERPC_NOHASH_64_MMU_H_ */
