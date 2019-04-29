edf4a5a226183251d2d27a73589d218540e1c7ba
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2019-04-29 11:27:55 -0400

    rseq/selftests: Use __rseq_handled symbol to coexist with glibc
    In order to integrate rseq into user-space applications, expose a
    __rseq_handled symbol so many rseq users can be linked into the same
    application (e.g. librseq and glibc).
    
    The __rseq_refcount TLS variable is static to the librseq library. It
    ensures that rseq syscall registration/unregistration happens only for
    the most early/late caller to rseq_{,un}register_current_thread for each
    thread, thus ensuring that rseq is registered across the lifetime of all
    rseq users for a given thread.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Shuah Khan <shuah@kernel.org>
    CC: Carlos O'Donell <carlos@redhat.com>
    CC: Florian Weimer <fweimer@redhat.com>
    CC: Joseph Myers <joseph@codesourcery.com>
    CC: Szabolcs Nagy <szabolcs.nagy@arm.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Ben Maurer <bmaurer@fb.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    CC: Boqun Feng <boqun.feng@gmail.com>
    CC: Will Deacon <will.deacon@arm.com>
    CC: Dave Watson <davejwatson@fb.com>
    CC: Paul Turner <pjt@google.com>
    CC: linux-api@vger.kernel.org
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/rseq/rseq.h
+++ tools/testing/selftests/rseq/rseq.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 extern __thread volatile struct rseq __rseq_abi;
+extern int __rseq_handled;
 
 #define rseq_likely(x)		__builtin_expect(!!(x), 1)
 #define rseq_unlikely(x)	__builtin_expect(!!(x), 0)
