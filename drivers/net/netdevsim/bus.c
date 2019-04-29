28310cf42b7f46b41987b0d946d298361053702e
Gustavo A. R. Silva <gustavo@embeddedor.com>
2019-04-29 12:38:07 -0500

    netdevsim: fix fall-through annotation
    Replace "pass through" with a proper "fall through" annotation
    in order to fix the following warning:
    
    drivers/net/netdevsim/bus.c: In function ‘new_device_store’:
    drivers/net/netdevsim/bus.c:170:14: warning: this statement may fall through [-Wimplicit-fallthrough=]
       port_count = 1;
       ~~~~~~~~~~~^~~
    drivers/net/netdevsim/bus.c:172:2: note: here
      case 2:
      ^~~~
    
    Warning level 3 was used: -Wimplicit-fallthrough=3
    
    This fix is part of the ongoing efforts to enable
    -Wimplicit-fallthrough
    
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/netdevsim/bus.c
+++ drivers/net/netdevsim/bus.c
@@ -169,7 +169,7 @@ new_device_store(struct bus_type *bus, const char *buf, size_t count)
 	switch (err) {
 	case 1:
 		port_count = 1;
-		/* pass through */
+		/* fall through */
 	case 2:
 		if (id > INT_MAX) {
 			pr_err("Value of \"id\" is too big.\n");
