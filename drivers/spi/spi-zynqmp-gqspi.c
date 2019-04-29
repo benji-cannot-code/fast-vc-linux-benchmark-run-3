b9c6eaa13c39c37ef5e875b64fea394a916bbc53
Olof Johansson <olof@lixom.net>
2019-04-29 09:15:14 -0700

    spi: zynqmp: Fix build break
    Missing include:
    drivers/spi/spi-zynqmp-gqspi.c:1025:13: error: implicit declaration of
      function 'zynqmp_pm_get_eemi_ops'; did you mean
      'zynqmp_process_dma_irq'? [-Werror=implicit-function-declaration]
    
    Fixes: 3d0313786470a ('drivers: Defer probe if firmware is not ready')
    Cc: Rajan Vaja <rajan.vaja@xilinx.com>
    Cc: Jolly Shah <jollys@xilinx.com>
    Cc: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- drivers/spi/spi-zynqmp-gqspi.c
+++ drivers/spi/spi-zynqmp-gqspi.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/dma-mapping.h>
 #include <linux/dmaengine.h>
+#include <linux/firmware/xlnx-zynqmp.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/module.h>
