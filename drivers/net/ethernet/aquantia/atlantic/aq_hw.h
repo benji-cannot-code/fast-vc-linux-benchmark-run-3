7fcaa800b6ea684c20f0c478b1e99f8b7330744a
Igor Russkikh <Igor.Russkikh@aquantia.com>
2019-04-29 10:04:40 +0000

    net: aquantia: add link interrupt fields
    Declare macroes and nic fields to support link interrupt
    handling
    
    Signed-off-by: Nikita Danilov <ndanilov@aquantia.com>
    Signed-off-by: Igor Russkikh <igor.russkikh@aquantia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/aquantia/atlantic/aq_hw.h
+++ drivers/net/ethernet/aquantia/atlantic/aq_hw.h
@@ -89,6 +89,8 @@ struct aq_stats_s {
 #define AQ_HW_IRQ_MSI     2U
 #define AQ_HW_IRQ_MSIX    3U
 
+#define AQ_HW_SERVICE_IRQS   1U
+
 #define AQ_HW_POWER_STATE_D0   0U
 #define AQ_HW_POWER_STATE_D3   3U
 
