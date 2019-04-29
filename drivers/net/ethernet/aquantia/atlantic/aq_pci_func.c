6cbc28de2b1b26a1938fbaa782df6232bce4ab3b
Yana Esina <yana.esina@aquantia.com>
2019-04-29 10:04:38 +0000

    net: aquantia: implement hwmon api for chip temperature
    Added support for hwmon api to fetch out chip temperature
    
    Signed-off-by: Yana Esina <yana.esina@aquantia.com>
    Signed-off-by: Nikita Danilov <nikita.danilov@aquantia.com>
    Signed-off-by: Igor Russkikh <igor.russkikh@aquantia.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/aquantia/atlantic/aq_pci_func.c
+++ drivers/net/ethernet/aquantia/atlantic/aq_pci_func.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hw_atl/hw_atl_a0.h"
 #include "hw_atl/hw_atl_b0.h"
 #include "aq_filters.h"
+#include "aq_drvinfo.h"
 
 static const struct pci_device_id aq_pci_tbl[] = {
 	{ PCI_VDEVICE(AQUANTIA, AQ_DEVICE_ID_0001), },
@@ -289,6 +290,8 @@ static int aq_pci_probe(struct pci_dev *pdev,
 	err = aq_nic_ndev_register(self);
 	if (err < 0)
 		goto err_register;
+
+	aq_drvinfo_init(ndev);
 
 	return 0;
 
