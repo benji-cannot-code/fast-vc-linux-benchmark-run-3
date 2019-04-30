061d7c1fe7cff8e9b9606efaf5bb2f5b6b6eaafc
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-04-30 18:09:24 -0500

    ASoC: SOF: Intel: hda-pcm: remove useless dependency on hdac_ext
    Nothing depends on definitions in hdaudio_ext.h, don't include it
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/intel/hda-pcm.c
+++ sound/soc/sof/intel/hda-pcm.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Hardware interface for generic Intel audio DSP HDA IP
  */
 
-#include <sound/hdaudio_ext.h>
 #include <sound/hda_register.h>
 #include <sound/pcm_params.h>
 #include "../ops.h"
