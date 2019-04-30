f8b6142219b4fa08faafa24b375e55ae22c32ff9
Zhu Yingjiang <yingjiang.zhu@linux.intel.com>
2019-04-30 18:09:22 -0500

    ASoC: SOF: Intel: hda: set I2S slave before enabling DSP
    By default, the I2S ports are configured in master mode during
    DSP powerup sequences, the FS and BCLK lines will be driven on
    startup, even when the topology file explicitly requires the
    SSP to be slave.
    
    This may be problematic for external components configured in
    master mode who don't expect the Intel SOC/PCH to drive. Fix by
    configuring the SSP as slave before the SSP outputs are enabled
    to avoid this transient behavior.
    
    When the topology file configures the SSP as clock master, the
    initial slave configuration will be overridden.
    
    Signed-off-by: Zhu Yingjiang <yingjiang.zhu@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/intel/hda.h
+++ sound/soc/sof/intel/hda.h
@@ -350,6 +350,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define APL_SSP_COUNT		6
 #define CNL_SSP_COUNT		3
 
+/* SSP Registers */
+#define SSP_SSC1_OFFSET		0x4
+#define SSP_SET_SCLK_SLAVE	BIT(25)
+#define SSP_SET_SFRM_SLAVE	BIT(24)
+#define SSP_SET_SLAVE		(SSP_SET_SCLK_SLAVE | SSP_SET_SFRM_SLAVE)
+
 #define HDA_IDISP_CODEC(x) ((x) & BIT(2))
 
 struct sof_intel_dsp_bdl {
