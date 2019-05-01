898d73f0620f3b8c365d7a1c0018256cb5cd66b8
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-05-01 10:48:43 +0200

    staging: kpc2000: fix up build problems with readq()
    The 0-day system found a bunch of warnings for when readq() is not
    defined on the platform, so fix this by including the
    linux/io-64-nonatomic-lo-hi.h file which will fix up these issues.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/kpc2000/kpc2000/fileops.c
+++ drivers/staging/kpc2000/kpc2000/fileops.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>    /* copy_*_user */
 #include <linux/rwsem.h>
 #include <linux/idr.h>
+#include <linux/io-64-nonatomic-lo-hi.h>
 #include <linux/device.h>
 #include <linux/sched.h>
 #include "pcie.h"
--- drivers/staging/kpc2000/kpc_i2c/i2c_driver.c
+++ drivers/staging/kpc2000/kpc_i2c/i2c_driver.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/types.h>
 #include <asm/io.h>
+#include <linux/io-64-nonatomic-lo-hi.h>
 #include <linux/export.h>
 #include <linux/slab.h>
 #include <linux/platform_device.h>
