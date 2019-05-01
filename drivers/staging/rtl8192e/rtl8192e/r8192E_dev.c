3eff7e4c46e9ddbc1c2ad3414ed00beb03b397d9
Puranjay Mohan <puranjay12@gmail.com>
2019-05-02 00:53:24 +0530

    Staging: rtl8192e: Remove extra space before break statement
    Remove extra spaces before "break" statements to fix the following
    warnings from checkpatch.pl
    
    WARNING: Statements should start on a tabstop
    +		 break;
    
    WARNING: Statements should start on a tabstop
    +		 break;
    
    Signed-off-by: Puranjay Mohan <puranjay12@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl8192e/r8192E_dev.c
+++ drivers/staging/rtl8192e/rtl8192e/r8192E_dev.c
@@ -1914,7 +1914,7 @@ static void _rtl92e_update_received_rate_histogram_stats(
 		break;
 	case MGN_2M:
 		rateIndex = 1;
-		 break;
+		break;
 	case MGN_5_5M:
 		rateIndex = 2;
 		break;
@@ -1932,7 +1932,7 @@ static void _rtl92e_update_received_rate_histogram_stats(
 		break;
 	case MGN_18M:
 		rateIndex = 7;
-		 break;
+		break;
 	case MGN_24M:
 		rateIndex = 8;
 		break;
