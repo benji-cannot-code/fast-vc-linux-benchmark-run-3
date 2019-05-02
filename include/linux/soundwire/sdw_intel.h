924dff91b28b986cc731708101d738d7731dbd8a
Vinod Koul <vkoul@kernel.org>
2019-05-02 16:29:19 +0530

    soundwire: intel: fix SPDX license for header file
    Some more headers had C++ style SDPX line, fix that and change copyright
    so that it is consistent with rest of the code in subsystem
    
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/soundwire/sdw_intel.h
+++ include/linux/soundwire/sdw_intel.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-// SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
-// Copyright(c) 2015-17 Intel Corporation.
+/* SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause) */
+/* Copyright(c) 2015-17 Intel Corporation. */
 
 #ifndef __SDW_INTEL_H
 #define __SDW_INTEL_H
