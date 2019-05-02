4f36b3db423f72c1a5598bb36703f7f89fbebc8c
Jani Nikula <jani.nikula@intel.com>
2019-05-02 17:52:34 +0300

    drm/i915: add single combo phy init/unit functions
    Work on the principle that files should prefer not to expose platform
    specific functions.
    
    v2, v3: Rebase
    
    Cc: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190502145234.7002-1-jani.nikula@intel.com

--- drivers/gpu/drm/i915/intel_combo_phy.h
+++ drivers/gpu/drm/i915/intel_combo_phy.h
@@ -12,10 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct drm_i915_private;
 
-void icl_combo_phys_init(struct drm_i915_private *dev_priv);
-void icl_combo_phys_uninit(struct drm_i915_private *dev_priv);
-void cnl_combo_phys_init(struct drm_i915_private *dev_priv);
-void cnl_combo_phys_uninit(struct drm_i915_private *dev_priv);
+void intel_combo_phy_init(struct drm_i915_private *dev_priv);
+void intel_combo_phy_uninit(struct drm_i915_private *dev_priv);
 void intel_combo_phy_power_up_lanes(struct drm_i915_private *dev_priv,
 				    enum port port, bool is_dsi,
 				    int lane_count, bool lane_reversal);
