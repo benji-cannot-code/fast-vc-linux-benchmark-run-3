e1d2f002143d09642c16ee80beab2f0b5f1adcf1
Vinod Koul <vkoul@kernel.org>
2019-05-02 16:29:25 +0530

    soundwire: intel: more alignment fixes
    Found few more issues reported checkpatch on code alignment so fix those
    as well in the intel module.
    
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/soundwire/sdw_intel.h
+++ include/linux/soundwire/sdw_intel.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct sdw_intel_ops {
 	int (*config_stream)(void *arg, void *substream,
-			void *dai, void *hw_params, int stream_num);
+			     void *dai, void *hw_params, int stream_num);
 };
 
 /**
