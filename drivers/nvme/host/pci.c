7eb0d8fc21b080b621db1ec8488dface5c7a2150
Maxim Levitsky <mlevitsk@redhat.com>
2019-05-02 14:31:33 +0300

    nvme-pci: init shadow doorbell after each reset
    The spec states:
    
      "The settings are not retained across a Controller Level Reset"
    
    Therefore the driver must enable the shadow doorbell, after each reset.
    
    This was caught while testing the nvme driver over upcoming nvme-mdev
    device.
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
    Reviewed-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Minwoo Im <minwoo.im@samsung.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -2281,8 +2281,6 @@ static int nvme_dev_add(struct nvme_dev *dev)
 			return ret;
 		}
 		dev->ctrl.tagset = &dev->tagset;
-
-		nvme_dbbuf_set(dev);
 	} else {
 		blk_mq_update_nr_hw_queues(&dev->tagset, dev->online_queues - 1);
 
@@ -2290,6 +2288,7 @@ static int nvme_dev_add(struct nvme_dev *dev)
 		nvme_free_queues(dev, dev->online_queues);
 	}
 
+	nvme_dbbuf_set(dev);
 	return 0;
 }
 
