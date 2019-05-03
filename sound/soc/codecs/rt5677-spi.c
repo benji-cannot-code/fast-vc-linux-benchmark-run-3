b0cb0564a8870b3fbaedf041ebf9013851557313
Curtis Malainey <cujomalainey@chromium.org>
2019-05-03 12:33:42 -0700

    ASoC: rt5677-spi: Rename driver to differentiate from main codec
    Currently the SPI driver and the main codec share the same name. This
    will become confusing when looking up components when using both
    drivers.
    
    Signed-off-by: Curtis Malainey <cujomalainey@chromium.org>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5677-spi.c
+++ sound/soc/codecs/rt5677-spi.c
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "rt5677-spi.h"
 
+#define DRV_NAME "rt5677spi"
+
 #define RT5677_SPI_BURST_LEN	240
 #define RT5677_SPI_HEADER	5
 #define RT5677_SPI_FREQ		6000000
@@ -236,7 +238,7 @@ MODULE_DEVICE_TABLE(acpi, rt5677_spi_acpi_id);
 
 static struct spi_driver rt5677_spi_driver = {
 	.driver = {
-		.name = "rt5677",
+		.name = DRV_NAME,
 		.acpi_match_table = ACPI_PTR(rt5677_spi_acpi_id),
 	},
 	.probe = rt5677_spi_probe,
