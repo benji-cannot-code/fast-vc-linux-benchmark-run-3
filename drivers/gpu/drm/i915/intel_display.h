92182f50aae0f4226e6abcdfe43e508fbde37494
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-05-03 22:31:43 +0300

    drm/i915: Move the hsw/bdw pc8 code to intel_runtime_pm.c
    hsw_enable_pc8()/hsw_disable_pc8() are more less equivalent to
    the display core init/unit functions of later platforms. Relocate
    the hsw/bdw code into intel_runtime_pm.c so that it sits next to
    its cousins.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190503193143.28240-2-ville.syrjala@linux.intel.com
    Reviewed-by: Imre Deak <imre.deak@intel.com>

--- drivers/gpu/drm/i915/intel_display.h
+++ drivers/gpu/drm/i915/intel_display.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_util.h>
 #include <drm/i915_drm.h>
 
+struct drm_i915_private;
+
 enum i915_gpio {
 	GPIOA,
 	GPIOB,
@@ -433,4 +435,6 @@ void intel_link_compute_m_n(u16 bpp, int nlanes,
 			    struct intel_link_m_n *m_n,
 			    bool constant_n);
 bool is_ccs_modifier(u64 modifier);
+void lpt_disable_clkout_dp(struct drm_i915_private *dev_priv);
+
 #endif
