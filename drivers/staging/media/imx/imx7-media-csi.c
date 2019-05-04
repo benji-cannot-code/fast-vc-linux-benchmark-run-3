651b9bc3b0075ae10afc4c9e20eab293ab070398
Fabio Estevam <festevam@gmail.com>
2019-05-04 10:40:23 -0400

    media: imx7-media-csi: Propagate the correct error for devm_request_irq()
    In the case of devm_request_irq() failure, let's propagate the real error
    code instead of a fake one.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Tested-by: Rui Miguel Silva <rmfrfs@gmail.com>
    Reviewed-by: Rui Miguel Silva <rmfrfs@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/staging/media/imx/imx7-media-csi.c
+++ drivers/staging/media/imx/imx7-media-csi.c
@@ -1200,7 +1200,6 @@ static int imx7_csi_probe(struct platform_device *pdev)
 			       (void *)csi);
 	if (ret < 0) {
 		dev_err(dev, "Request CSI IRQ failed.\n");
-		ret = -ENODEV;
 		goto destroy_mutex;
 	}
 
