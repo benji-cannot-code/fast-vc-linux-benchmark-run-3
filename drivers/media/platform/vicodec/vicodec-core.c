8dc3bd8868c13adc5fc814f29d398e80a161907b
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2019-05-07 05:30:24 -0400

    media: vicodec: use correct sizeimage value when draining
    After a resolution change is detected, q_data->sizeimage is updated
    to the new format, but buf_prepare is still draining buffers that
    need to use the old pre-resolution-change value. So store the sizeimage
    value in q_data->vb2_sizeimage in queue_setup and use that in
    buf_prepare.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/vicodec/vicodec-core.c
+++ drivers/media/platform/vicodec/vicodec-core.c
@@ -85,6 +85,7 @@ struct vicodec_q_data {
 	unsigned int		visible_width;
 	unsigned int		visible_height;
 	unsigned int		sizeimage;
+	unsigned int		vb2_sizeimage;
 	unsigned int		sequence;
 	const struct v4l2_fwht_pixfmt_info *info;
 };
@@ -1362,6 +1363,7 @@ static int vicodec_queue_setup(struct vb2_queue *vq, unsigned int *nbuffers,
 
 	*nplanes = 1;
 	sizes[0] = size;
+	q_data->vb2_sizeimage = size;
 	return 0;
 }
 
@@ -1392,11 +1394,11 @@ static int vicodec_buf_prepare(struct vb2_buffer *vb)
 		}
 	}
 
-	if (vb2_plane_size(vb, 0) < q_data->sizeimage) {
+	if (vb2_plane_size(vb, 0) < q_data->vb2_sizeimage) {
 		dprintk(ctx->dev,
 			"%s data will not fit into plane (%lu < %lu)\n",
 			__func__, vb2_plane_size(vb, 0),
-			(long)q_data->sizeimage);
+			(long)q_data->vb2_sizeimage);
 		return -EINVAL;
 	}
 
