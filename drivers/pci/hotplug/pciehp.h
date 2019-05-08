c2f4d26efee3520b3c145166e6bffae20419d2d0
Bjorn Helgaas <bhelgaas@google.com>
2019-05-08 16:34:01 -0500

    PCI: pciehp: Remove pointless MY_NAME definition
    MY_NAME is only used once and offers no benefit, so remove it.
    
    Link: https://lore.kernel.org/lkml/20190509141456.223614-11-helgaas@kernel.org
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>

--- drivers/pci/hotplug/pciehp.h
+++ drivers/pci/hotplug/pciehp.h
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../pcie/portdrv.h"
 
-#define MY_NAME	"pciehp"
-
 extern bool pciehp_poll_mode;
 extern int pciehp_poll_time;
 
