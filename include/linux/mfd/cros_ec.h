171ea90c4a8c53c6d33fdb3eca847a7c73853a97
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2019-05-08 11:19:55 +0200

    mfd: cros_ec: Instantiate properly CrOS FP MCU device
    Support Fingerprint MCU as a special of CrOS EC devices. The current FP
    MCU uses the same EC SPI protocol v3 as other CrOS EC devices on a SPI
    bus.
    
    When a MCU has fingerprint support (aka EC_FEATURE_FINGERPRINT), it is
    instantiated as a special CrOS EC device with device name 'cros_fp'. So
    regardless of the probing order between the actual cros_ec and cros_fp,
    the userspace and other kernel drivers should not confuse them.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 
 #define CROS_EC_DEV_NAME "cros_ec"
+#define CROS_EC_DEV_FP_NAME "cros_fp"
 #define CROS_EC_DEV_PD_NAME "cros_pd"
 #define CROS_EC_DEV_ISH_NAME "cros_ish"
 
