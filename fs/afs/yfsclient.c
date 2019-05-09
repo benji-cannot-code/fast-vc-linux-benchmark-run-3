68bba89e96dfce71e96afca1114d9499ddc8ab6a
David Howells <dhowells@redhat.com>
2019-05-09 17:19:10 +0100

    afs: Don't pass the vnode pointer through into the inline bulk status op
    Don't pass the vnode pointer through into the inline bulk status op.  We
    want to process the status records outside of it anyway.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/afs/yfsclient.c
+++ fs/afs/yfsclient.c
@@ -2056,7 +2056,6 @@ static int yfs_deliver_fs_inline_bulk_status(struct afs_call *call)
 {
 	struct afs_file_status *statuses;
 	struct afs_callback *callbacks;
-	struct afs_vnode *vnode = call->reply[0];
 	const __be32 *bp;
 	u32 tmp;
 	int ret;
@@ -2097,8 +2096,7 @@ static int yfs_deliver_fs_inline_bulk_status(struct afs_call *call)
 		bp = call->buffer;
 		statuses = call->reply[1];
 		ret = yfs_decode_status(call, &bp, &statuses[call->count],
-					call->count == 0 ? vnode : NULL,
-					NULL, NULL);
+					NULL, NULL, NULL);
 		if (ret < 0)
 			return ret;
 
@@ -2139,11 +2137,6 @@ static int yfs_deliver_fs_inline_bulk_status(struct afs_call *call)
 		bp = call->buffer;
 		callbacks = call->reply[2];
 		xdr_decode_YFSCallBack_raw(call, &callbacks[call->count], &bp);
-		statuses = call->reply[1];
-		if (call->count == 0 && vnode && statuses[0].abort_code == 0) {
-			bp = call->buffer;
-			xdr_decode_YFSCallBack(call, vnode, &bp);
-		}
 		call->count++;
 		if (call->count < call->count2)
 			goto more_cbs;
@@ -2211,7 +2204,6 @@ int yfs_fs_inline_bulk_status(struct afs_fs_cursor *fc,
 	}
 
 	call->key = fc->key;
-	call->reply[0] = NULL; /* vnode for fid[0] */
 	call->reply[1] = statuses;
 	call->reply[2] = callbacks;
 	call->reply[3] = volsync;
