8d46eb13077fe14efcfcb94f48410fec6a5899c0
Parav Pandit <parav@mellanox.com>
2019-05-09 17:23:24 -0500

    net/mlx5: Avoid double free of root ns in the error flow path
    When root ns setup for rdma, sniffer tx and sniffer rx fails,
    such root ns cleanup is done by the error unwinding path of
    mlx5_cleanup_fs().
    Below call graph shows an example for sniffer_rx_root_ns.
    
    mlx5_init_fs()
      init_sniffer_rx_root_ns()
        cleanup_root_ns(steering->sniffer_rx_root_ns);
    mlx5_cleanup_fs()
      cleanup_root_ns(steering->sniffer_rx_root_ns);
      /* double free of sniffer_rx_root_ns */
    
    Hence, use the existing cleanup_fs to cleanup.
    
    Fixes: d83eb50e29de3 ("net/mlx5: Add support in RDMA RX steering")
    Fixes: 87d22483ce68e ("net/mlx5: Add sniffer namespaces")
    Signed-off-by: Parav Pandit <parav@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/fs_core.c
+++ drivers/net/ethernet/mellanox/mlx5/core/fs_core.c
@@ -2475,11 +2475,7 @@ static int init_sniffer_tx_root_ns(struct mlx5_flow_steering *steering)
 
 	/* Create single prio */
 	prio = fs_create_prio(&steering->sniffer_tx_root_ns->ns, 0, 1);
-	if (IS_ERR(prio)) {
-		cleanup_root_ns(steering->sniffer_tx_root_ns);
-		return PTR_ERR(prio);
-	}
-	return 0;
+	return PTR_ERR_OR_ZERO(prio);
 }
 
 static int init_sniffer_rx_root_ns(struct mlx5_flow_steering *steering)
@@ -2492,11 +2488,7 @@ static int init_sniffer_rx_root_ns(struct mlx5_flow_steering *steering)
 
 	/* Create single prio */
 	prio = fs_create_prio(&steering->sniffer_rx_root_ns->ns, 0, 1);
-	if (IS_ERR(prio)) {
-		cleanup_root_ns(steering->sniffer_rx_root_ns);
-		return PTR_ERR(prio);
-	}
-	return 0;
+	return PTR_ERR_OR_ZERO(prio);
 }
 
 static int init_rdma_rx_root_ns(struct mlx5_flow_steering *steering)
@@ -2512,11 +2504,7 @@ static int init_rdma_rx_root_ns(struct mlx5_flow_steering *steering)
 
 	/* Create single prio */
 	prio = fs_create_prio(&steering->rdma_rx_root_ns->ns, 0, 1);
-	if (IS_ERR(prio)) {
-		cleanup_root_ns(steering->rdma_rx_root_ns);
-		return PTR_ERR(prio);
-	}
-	return 0;
+	return PTR_ERR_OR_ZERO(prio);
 }
 static int init_fdb_root_ns(struct mlx5_flow_steering *steering)
 {
