9857acf2c46fb8e1a304da4dc56dabad6c732c49
Masayoshi Mizuma <m.mizuma@jp.fujitsu.com>
2019-05-09 17:36:45 -0400

    ktest: pass KERNEL_VERSION to POST_KTEST
    For BLS, kernel entry is added by kernel-install command through
    POST_INSALL, for example,
    
    POST_INSTALL = ssh root@Test "/usr/bin/kernel-install \
        add $KERNEL_VERSION /boot/vmlinuz-$KERNEL_VERSION"
    
    The entry is removed by kernel-install command and the kernel
    version is needed for the argument.
    
    Pass KERNEL_VERSION variable to POST_KTEST so that kernel-install
    command can remove the entry like as follows:
    
    POST_KTEST = ssh root@Test "/usr/bin/kernel-install remove $KERNEL_VERSION"
    
    Link: http://lkml.kernel.org/r/20190509213647.6276-5-msys.mizuma@gmail.com
    
    Signed-off-by: Masayoshi Mizuma <m.mizuma@jp.fujitsu.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -4457,7 +4457,9 @@ for (my $i = 1; $i <= $opt{"NUM_TESTS"}; $i++) {
 }
 
 if (defined($final_post_ktest)) {
-    run_command $final_post_ktest;
+
+    my $cp_final_post_ktest = eval_kernel_version $final_post_ktest;
+    run_command $cp_final_post_ktest;
 }
 
 if ($opt{"POWEROFF_ON_SUCCESS"}) {
