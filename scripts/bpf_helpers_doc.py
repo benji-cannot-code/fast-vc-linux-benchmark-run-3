ba08bb7ac2fe3c084ef1f8152d6345d3f5f1d971
Quentin Monnet <quentin.monnet@netronome.com>
2019-05-10 15:51:22 +0100

    bpf: fix script for generating man page on BPF helpers
    The script broke on parsing function prototype for bpf_strtoul(). This
    is because the last argument for the function is a pointer to an
    "unsigned long". The current version of the script only accepts "const"
    and "struct", but not "unsigned", at the beginning of argument types
    made of several words.
    
    One solution could be to add "unsigned" to the list, but the issue could
    come up again in the future (what about "long int"?). It turns out we do
    not need to have such restrictions on the words: so let's simply accept
    any series of words instead.
    
    Reported-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- scripts/bpf_helpers_doc.py
+++ scripts/bpf_helpers_doc.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #!/usr/bin/python3
 # SPDX-License-Identifier: GPL-2.0-only
 #
-# Copyright (C) 2018 Netronome Systems, Inc.
+# Copyright (C) 2018-2019 Netronome Systems, Inc.
 
 # In case user attempts to run with Python 2.
 from __future__ import print_function
@@ -40,7 +40,7 @@ class Helper(object):
         Break down helper function protocol into smaller chunks: return type,
         name, distincts arguments.
         """
-        arg_re = re.compile('((const )?(struct )?(\w+|...))( (\**)(\w+))?$')
+        arg_re = re.compile('((\w+ )*?(\w+|...))( (\**)(\w+))?$')
         res = {}
         proto_re = re.compile('(.+) (\**)(\w+)\(((([^,]+)(, )?){1,5})\)$')
 
@@ -55,8 +55,8 @@ class Helper(object):
             capture = arg_re.match(a)
             res['args'].append({
                 'type' : capture.group(1),
-                'star' : capture.group(6),
-                'name' : capture.group(7)
+                'star' : capture.group(5),
+                'name' : capture.group(6)
             })
 
         return res
