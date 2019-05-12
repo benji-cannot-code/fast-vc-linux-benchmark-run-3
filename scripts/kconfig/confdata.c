79cdd741849fa5f00f7b6a5a6790e2334abcf151
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-05-13 01:00:53 +0900

    kconfig: make conf_get_autoconfig_name() static
    This is only used in confdata.c
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -227,7 +227,7 @@ const char *conf_get_configname(void)
 	return name ? name : ".config";
 }
 
-const char *conf_get_autoconfig_name(void)
+static const char *conf_get_autoconfig_name(void)
 {
 	char *name = getenv("KCONFIG_AUTOCONFIG");
 
