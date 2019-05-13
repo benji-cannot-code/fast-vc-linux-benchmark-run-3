905aad8c59d27f62635ab5c64c90d5d4ff2aec8c
Ronnie Sahlberg <lsahlber@redhat.com>
2019-05-13 11:24:17 +1000

    cifs: use the right include for signal_pending()
    This header is actually where signal_pending is defined
    although either would work.
    
    Signed-off-by: Ronnie Sahlberg <lsahlber@redhat.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/transport.c
+++ fs/cifs/transport.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <asm/processor.h>
 #include <linux/mempool.h>
-#include <linux/signal.h>
+#include <linux/sched/signal.h>
 #include "cifspdu.h"
 #include "cifsglob.h"
 #include "cifsproto.h"
