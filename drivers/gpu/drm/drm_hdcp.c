bfacc89440ba3c9c55b261b166a6d8532270b2cb
Ramalingam C <ramalingam.c@intel.com>
2019-05-13 19:05:04 +0530

    drm/hdcp: drm_hdcp_request_srm() as static
    Below Sparsh warnings are fixed.
    
    Commit: drm: revocation check at drm subsystem
    +drivers/gpu/drm/drm_hdcp.c:235:6: warning: symbol
    'drm_hdcp_request_srm' was not declared. Should it be static?
    +drivers/gpu/drm/drm_hdcp.c:27:3: warning: symbol 'srm_data' was not
    declared. Should it be static?
    +drivers/gpu/drm/drm_hdcp.c:317:5: warning: symbol 'drm_setup_hdcp_srm'
    was not declared. Should it be static?
    +drivers/gpu/drm/drm_hdcp.c:327:6: warning: symbol
    'drm_teardown_hdcp_srm' was not declared. Should it be static?
    
    cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    Acked-by: Dave Airlie <airlied@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190513133504.18612-1-ramalingam.c@intel.com

--- drivers/gpu/drm/drm_hdcp.c
+++ drivers/gpu/drm/drm_hdcp.c
@@ -22,7 +22,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_mode_object.h>
 #include <drm/drm_connector.h>
 
-struct hdcp_srm {
+#include "drm_internal.h"
+
+static struct hdcp_srm {
 	u32 revoked_ksv_cnt;
 	u8 *revoked_ksv_list;
 
@@ -236,7 +238,7 @@ static void drm_hdcp_srm_update(const u8 *buf, size_t count)
 		drm_hdcp_parse_hdcp2_srm(buf, count);
 }
 
-void drm_hdcp_request_srm(struct drm_device *drm_dev)
+static void drm_hdcp_request_srm(struct drm_device *drm_dev)
 {
 	char fw_name[36] = "display_hdcp_srm.bin";
 	const struct firmware *fw;
