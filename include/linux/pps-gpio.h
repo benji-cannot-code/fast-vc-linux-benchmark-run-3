e5ab2155b15cecea82ec546f722126f1b109eea1
Tom Burkart <tom@aussec.com>
2019-05-14 15:45:46 -0700

    pps: pps-gpio PPS ECHO implementation
    This patch implements the PPS ECHO functionality for pps-gpio, that
    sysfs claims is available already.
    
    Configuration is done via device tree bindings.
    
    No changes are made to userspace interfaces.
    
    This patch was originally written by Lukas Senger as part of a masters
    thesis project and modified for inclusion into the linux kernel by Tom
    Burkart.
    
    Link: http://lkml.kernel.org/r/20190324043305.6627-4-tom@aussec.com
    Signed-off-by: Tom Burkart <tom@aussec.com>
    Acked-by: Rodolfo Giometti <giometti@enneenne.com>
    Signed-off-by: Lukas Senger <lukas@fridolin.com>
    Cc: Philipp Zabel <philipp.zabel@gmail.com>
    Cc: Rob Herring <robh@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/pps-gpio.h
+++ include/linux/pps-gpio.h
@@ -25,8 +25,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct pps_gpio_platform_data {
 	struct gpio_desc *gpio_pin;
+	struct gpio_desc *echo_pin;
 	bool assert_falling_edge;
 	bool capture_clear;
+	unsigned int echo_active_ms;
 };
 
 #endif /* _PPS_GPIO_H */
