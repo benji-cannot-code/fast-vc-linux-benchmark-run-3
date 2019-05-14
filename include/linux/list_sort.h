73e7f1382fe7542df9562d52c38ee2a6bbf6362e
George Spelvin <lkml@sdf.org>
2019-05-14 15:42:58 -0700

    lib/list_sort: simplify and remove MAX_LIST_LENGTH_BITS
    Rather than a fixed-size array of pending sorted runs, use the ->prev
    links to keep track of things.  This reduces stack usage, eliminates
    some ugly overflow handling, and reduces the code size.
    
    Also:
    * merge() no longer needs to handle NULL inputs, so simplify.
    * The same applies to merge_and_restore_back_links(), which is renamed
      to the less ponderous merge_final().  (It's a static helper function,
      so we don't need a super-descriptive name; comments will do.)
    * Document the actual return value requirements on the (*cmp)()
      function; some callers are already using this feature.
    
    x86-64 code size 1086 -> 739 bytes (-347)
    
    (Yes, I see checkpatch complaining about no space after comma in
    "__attribute__((nonnull(2,3,4,5)))".  Checkpatch is wrong.)
    
    Feedback from Rasmus Villemoes, Andy Shevchenko and Geert Uytterhoeven.
    
    [akpm@linux-foundation.org: remove __pure usage due to mysterious warning]
    Link: http://lkml.kernel.org/r/f63c410e0ff76009c9b58e01027e751ff7fdb749.1552704200.git.lkml@sdf.org
    Signed-off-by: George Spelvin <lkml@sdf.org>
    Acked-by: Andrey Abramov <st5pub@yandex.ru>
    Acked-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Daniel Wagner <daniel.wagner@siemens.com>
    Cc: Dave Chinner <dchinner@redhat.com>
    Cc: Don Mullis <don.mullis@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/list_sort.h
+++ include/linux/list_sort.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct list_head;
 
+__attribute__((nonnull(2,3)))
 void list_sort(void *priv, struct list_head *head,
 	       int (*cmp)(void *priv, struct list_head *a,
 			  struct list_head *b));
