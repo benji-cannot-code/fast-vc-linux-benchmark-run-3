7ac694df397c40199e6966bc60941046f95ac4a5
YueHaibing <yuehaibing@huawei.com>
2019-05-14 15:45:22 -0700

    fs/eventfd.c: make eventfd_ida static
    Fix sparse warning:
    
    fs/eventfd.c:26:1: warning:
     symbol 'eventfd_ida' was not declared. Should it be static?
    
    Link: http://lkml.kernel.org/r/20190413142348.34716-1-yuehaibing@huawei.com
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/eventfd.c
+++ fs/eventfd.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/idr.h>
 
-DEFINE_IDA(eventfd_ida);
+static DEFINE_IDA(eventfd_ida);
 
 struct eventfd_ctx {
 	struct kref kref;
