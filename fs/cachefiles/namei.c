1322405385af8dd7b1a88544959d4267ae2566fe
Sabyasachi Gupta <sabyasachi.linux@gmail.com>
2019-05-14 15:46:45 -0700

    fs/cachefiles/namei.c: remove duplicate header
    linux/xattr.h is included more than once.
    
    Link: http://lkml.kernel.org/r/5c86803d.1c69fb81.1a7c6.2b78@mx.google.com
    Signed-off-by: Sabyasachi Gupta <sabyasachi.linux@gmail.com>
    Acked-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/cachefiles/namei.c
+++ fs/cachefiles/namei.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/namei.h>
 #include <linux/security.h>
 #include <linux/slab.h>
-#include <linux/xattr.h>
 #include "internal.h"
 
 #define CACHEFILES_KEYBUF_SIZE 512
