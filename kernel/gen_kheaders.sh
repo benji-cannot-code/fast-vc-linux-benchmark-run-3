b7d7469b85e8791ce9619e696d53e695b210c760
Joel Fernandes (Google) <joel@joelfernandes.org>
2019-05-15 17:35:51 -0400

    kheaders: Move from proc to sysfs
    The kheaders archive consisting of the kernel headers used for compiling
    bpf programs is in /proc. However there is concern that moving it here
    will make it permanent. Let us move it to /sys/kernel as discussed [1].
    
    [1] https://lore.kernel.org/patchwork/patch/1067310/#1265969
    
    Suggested-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/gen_kheaders.sh
+++ kernel/gen_kheaders.sh
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # SPDX-License-Identifier: GPL-2.0
 
 # This script generates an archive consisting of kernel headers
-# for CONFIG_IKHEADERS_PROC.
+# for CONFIG_IKHEADERS.
 set -e
 spath="$(dirname "$(readlink -f "$0")")"
 kroot="$spath/.."
