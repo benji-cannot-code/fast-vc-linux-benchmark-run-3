1d89096b13a0205d4c59b46255763b381d4f07bc
Wanpeng Li <wanpengli@tencent.com>
2019-05-17 16:49:49 +0800

    KVM: Fix spinlock taken warning during host resume
     WARNING: CPU: 0 PID: 13554 at kvm/arch/x86/kvm//../../../virt/kvm/kvm_main.c:4183 kvm_resume+0x3c/0x40 [kvm]
      CPU: 0 PID: 13554 Comm: step_after_susp Tainted: G           OE     5.1.0-rc4+ #1
      RIP: 0010:kvm_resume+0x3c/0x40 [kvm]
      Call Trace:
       syscore_resume+0x63/0x2d0
       suspend_devices_and_enter+0x9d1/0xa40
       pm_suspend+0x33a/0x3b0
       state_store+0x82/0xf0
       kobj_attr_store+0x12/0x20
       sysfs_kf_write+0x4b/0x60
       kernfs_fop_write+0x120/0x1a0
       __vfs_write+0x1b/0x40
       vfs_write+0xcd/0x1d0
       ksys_write+0x5f/0xe0
       __x64_sys_write+0x1a/0x20
       do_syscall_64+0x6f/0x6c0
       entry_SYSCALL_64_after_hwframe+0x49/0xbe
    
    Commit ca84d1a24 (KVM: x86: Add clock sync request to hardware enable) mentioned
    that "we always hold kvm_lock when hardware_enable is called.  The one place that
    doesn't need to worry about it is resume, as resuming a frozen CPU, the spinlock
    won't be taken." However, commit 6706dae9 (virt/kvm: Replace spin_is_locked() with
    lockdep) introduces a bug, it asserts when the lock is not held which is contrary
    to the original goal.
    
    This patch fixes it by WARN_ON when the lock is held.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Cc: Paul E. McKenney <paulmck@linux.ibm.com>
    Signed-off-by: Wanpeng Li <wanpengli@tencent.com>
    Fixes: 6706dae9 ("virt/kvm: Replace spin_is_locked() with lockdep")
    [Wrap with #ifdef CONFIG_LOCKDEP - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sort.h>
 #include <linux/bsearch.h>
 #include <linux/io.h>
+#include <linux/lockdep.h>
 
 #include <asm/processor.h>
 #include <asm/ioctl.h>
@@ -4182,7 +4183,9 @@ static int kvm_suspend(void)
 static void kvm_resume(void)
 {
 	if (kvm_usage_count) {
-		lockdep_assert_held(&kvm_count_lock);
+#ifdef CONFIG_LOCKDEP
+		WARN_ON(lockdep_is_held(&kvm_count_lock));
+#endif
 		hardware_enable_nolock(NULL);
 	}
 }
