87b70484002bbbf2c1055f34e7ca66868ea044c8
Paolo Bonzini <pbonzini@redhat.com>
2019-05-17 14:08:53 +0200

    kvm: fix compilation on aarch64
    Commit e45adf665a53 ("KVM: Introduce a new guest mapping API", 2019-01-31)
    introduced a build failure on aarch64 defconfig:
    
    $ make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=out defconfig \
                    Image.gz
    ...
    ../arch/arm64/kvm/../../../virt/kvm/kvm_main.c:
        In function '__kvm_map_gfn':
    ../arch/arm64/kvm/../../../virt/kvm/kvm_main.c:1763:9: error:
        implicit declaration of function 'memremap'; did you mean 'memset_p'?
    ../arch/arm64/kvm/../../../virt/kvm/kvm_main.c:1763:46: error:
        'MEMREMAP_WB' undeclared (first use in this function)
    ../arch/arm64/kvm/../../../virt/kvm/kvm_main.c:
        In function 'kvm_vcpu_unmap':
    ../arch/arm64/kvm/../../../virt/kvm/kvm_main.c:1795:3: error:
        implicit declaration of function 'memunmap'; did you mean 'vm_munmap'?
    
    because these functions are declared in <linux/io.h> rather than <asm/io.h>,
    and the former was being pulled in already on x86 but not on aarch64.
    
    Reported-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -52,9 +52,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/sort.h>
 #include <linux/bsearch.h>
+#include <linux/io.h>
 
 #include <asm/processor.h>
-#include <asm/io.h>
 #include <asm/ioctl.h>
 #include <linux/uaccess.h>
 #include <asm/pgtable.h>
