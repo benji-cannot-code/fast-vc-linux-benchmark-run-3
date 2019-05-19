0171d5ad0852408a4697b5cea56b6fab36228bc7
Sam Ravnborg <sam@ravnborg.org>
2019-05-19 16:00:43 +0200

    drm/nouveau: drop drmP.h from all header files
    Drop include of the deprecated drmP.h from all nouveau heder files.
    This allows us to remove drmP.h from all .c files without any
    side-effects in a follow-up commit.
    
    Build tested using allyeyconfig and allmodconfig
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Ben Skeggs <bskeggs@redhat.com>
    Cc: nouveau@lists.freedesktop.org
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_gem.h
+++ drivers/gpu/drm/nouveau/nouveau_gem.h
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __NOUVEAU_GEM_H__
 #define __NOUVEAU_GEM_H__
 
-#include <drm/drmP.h>
-
 #include "nouveau_drv.h"
 #include "nouveau_bo.h"
 
