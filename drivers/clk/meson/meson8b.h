eb31a746b6d0c4be42933ca9031c359181024497
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2019-05-20 22:03:19 +0200

    clk: meson: meson8b: add the cts_i958 clock
    Add the cts_i958 clock to control the clock source of the spdif output
    block. It is used to select whether the clock source of the spdif output
    is cts_amclk (when data are taken from i2s buffer) or the cts_mclk_i958
    (when data are taken from the spdif buffer). The setup for this clock is
    identical to GXBB, so this ports commit 7eaa44f6207fb6 ("clk: meson:
    gxbb: add cts_i958 clock") to the Meson8/Meson8b/Meson8m2 clock driver.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- drivers/clk/meson/meson8b.h
+++ drivers/clk/meson/meson8b.h
@@ -179,7 +179,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_CTS_MCLK_I958_SEL	210
 #define CLKID_CTS_MCLK_I958_DIV	211
 
-#define CLK_NR_CLKS		213
+#define CLK_NR_CLKS		214
 
 /*
  * include the CLKID and RESETID that have
