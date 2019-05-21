03b4585518565e196d8e7506e14e19d15c072c42
Thomas Zimmermann <tzimmermann@suse.de>
2019-05-21 13:08:31 +0200

    drm: Assert that BO is locked in drm_gem_vram_{pin, unpin}_locked()
    We may not call drm_gem_vram_{pin,unpin}_locked() with an unlocked
    BO. Now test for this.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/20190521110831.20200-4-tzimmermann@suse.de
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- drivers/gpu/drm/drm_gem_vram_helper.c
+++ drivers/gpu/drm/drm_gem_vram_helper.c
@@ -285,6 +285,8 @@ int drm_gem_vram_pin_locked(struct drm_gem_vram_object *gbo,
 	int i, ret;
 	struct ttm_operation_ctx ctx = { false, false };
 
+	lockdep_assert_held(&gbo->bo.resv->lock.base);
+
 	if (gbo->pin_count) {
 		++gbo->pin_count;
 		return 0;
@@ -361,6 +363,8 @@ int drm_gem_vram_unpin_locked(struct drm_gem_vram_object *gbo)
 {
 	int i, ret;
 	struct ttm_operation_ctx ctx = { false, false };
+
+	lockdep_assert_held(&gbo->bo.resv->lock.base);
 
 	if (WARN_ON_ONCE(!gbo->pin_count))
 		return 0;
