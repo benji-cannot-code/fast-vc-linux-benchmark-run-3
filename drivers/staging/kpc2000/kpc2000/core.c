907db06c545bc988e5e0644ae942bbd066a5ab84
Jeremy Sowden <jeremy@azazel.net>
2019-05-21 11:35:20 +0100

    staging: kpc2000: added a helper to get struct kp2000_device from struct device.
    The attribute call-backs all use the same formula to get the pcard from
    dev:
    
      struct pci_dev *pdev = to_pci_dev(dev);
      struct kp2000_device *pcard;
    
      if (!pdev)
        return -ENXIO;
      pcard = pci_get_drvdata(pdev);
      if (!pcard)
        return -ENXIO;
    
    Added a function to reduce the duplicated code.
    
    Signed-off-by: Jeremy Sowden <jeremy@azazel.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/kpc2000/kpc2000/core.c
+++ drivers/staging/kpc2000/kpc2000/core.c
@@ -30,15 +30,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * SysFS Attributes
  ******************************************************/
 
+static struct kp2000_device *get_pcard(struct device *dev)
+{
+	struct pci_dev *pdev = to_pci_dev(dev);
+
+	if (!pdev)
+		return NULL;
+
+	return pci_get_drvdata(pdev);
+}
+
 static ssize_t show_attr(struct device *dev, struct device_attribute *attr,
 			 char *buf)
 {
-	struct pci_dev *pdev = to_pci_dev(dev);
-	struct kp2000_device *pcard;
+	struct kp2000_device *pcard = get_pcard(dev);
 
-	if (!pdev)
-		return -ENXIO;
-	pcard = pci_get_drvdata(pdev);
 	if (!pcard)
 		return -ENXIO;
 
@@ -73,14 +79,9 @@ static ssize_t show_attr(struct device *dev, struct device_attribute *attr,
 static ssize_t show_cpld_config_reg(struct device *dev,
 				    struct device_attribute *attr, char *buf)
 {
-	struct pci_dev *pdev = to_pci_dev(dev);
-	struct kp2000_device *pcard;
+	struct kp2000_device *pcard = get_pcard(dev);
 	u64 val;
 
-	if (!pdev)
-		return -ENXIO;
-
-	pcard = pci_get_drvdata(pdev);
 	if (!pcard)
 		return -ENXIO;
 
@@ -92,14 +93,10 @@ static ssize_t cpld_reconfigure(struct device *dev,
 				struct device_attribute *attr,
 				const char *buf, size_t count)
 {
-	struct pci_dev *pdev = to_pci_dev(dev);
+	struct kp2000_device *pcard = get_pcard(dev);
 	long wr_val;
-	struct kp2000_device *pcard;
 	int rv;
 
-	if (!pdev)
-		return -ENXIO;
-	pcard = pci_get_drvdata(pdev);
 	if (!pcard)
 		return -ENXIO;
 
