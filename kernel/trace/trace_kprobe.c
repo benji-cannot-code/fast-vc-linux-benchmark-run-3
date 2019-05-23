3bf16c745f70c56b1e53748b3eb5527f50465509
Steven Rostedt (VMware) <rostedt@goodmis.org>
2019-05-23 19:50:34 -0400

    tracing/kprobe: Do not run kprobe boot tests if kprobe_event is on cmdline
    When having kprobe trace event start up tests enabled and adding a
    kprobe_event on the kernel command line, it produced the following:
    
     trace_kprobe: Testing kprobe tracing:
     WARNING: CPU: 5 PID: 1 at kernel/trace/trace_kprobe.c:1724 kprobe_trace_self_tests_init+0x32d/0x36b
     Modules linked in:
     CPU: 5 PID: 1 Comm: swapper/0 Not tainted 5.2.0-rc1-test+ #249
     Hardware name: Hewlett-Packard HP Compaq Pro 6300 SFF/339A, BIOS K01 v03.03 07/14/2016
     RIP: 0010:kprobe_trace_self_tests_init+0x32d/0x36b
     Code: b7 e8 4f 8d a2 fe 85 c0 74 10 0f 0b 48 c7 c7 c8 1b 0d b7 ff c3 e8 19 af 99 fe 48 c7 c7 40 93 27 b7 e8 7f 1a a5 fe 85 c0 74 10 <0f> 0b 48 c7 c7 f8 1b 0d b7 ff c3 e8 f9 ae
    9 a0 fe 85
     RSP: 0018:ffffb36e40653e08 EFLAGS: 00010286
     RAX: 00000000fffffff0 RBX: 0000000000000000 RCX: ffffb36e40653d5c
     RDX: 0000000000000000 RSI: ffffffffb72776e0 RDI: 0000000000000246
     RBP: ffff98414fe58ff8 R08: 0000000000000000 R09: 0000000000000000
     R10: 0000000000000000 R11: ffff98415d8aa940 R12: 0000000000000000
     R13: ffffffffb737c1b0 R14: 0000000000000000 R15: 0000000000000000
     FS:  0000000000000000(0000) GS:ffff98415ea80000(0000) knlGS:0000000000000000
     CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
     CR2: 00007f959ce741b8 CR3: 000000011a210002 CR4: 00000000001606e0
     Call Trace:
      ? init_kprobe_trace+0x19e/0x19e
      ? do_early_param+0x8e/0x8e
      do_one_initcall+0x6f/0x2b4
      ? do_early_param+0x8e/0x8e
      kernel_init_freeable+0x21d/0x2c6
      ? rest_init+0x146/0x146
      kernel_init+0xa/0x10a
      ret_from_fork+0x3a/0x50
     ---[ end trace 488430c083a4c956 ]---
    
    As with the trace events, if a trace event is set on the kernel command
    line, the trace events start up tests are suspended. The kprobe start up
    tests should do the same when a kprobe is enabled on the kernel command
    line.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_kprobe.c
+++ kernel/trace/trace_kprobe.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Kprobe early definition from command line */
 static char kprobe_boot_events_buf[COMMAND_LINE_SIZE] __initdata;
+static bool kprobe_boot_events_enabled __initdata;
 
 static int __init set_kprobe_boot_events(char *str)
 {
@@ -1539,6 +1540,8 @@ static __init void setup_boot_kprobe_events(void)
 		ret = trace_run_command(cmd, create_or_delete_trace_kprobe);
 		if (ret)
 			pr_warn("Failed to add event(%d): %s\n", ret, cmd);
+		else
+			kprobe_boot_events_enabled = true;
 
 		cmd = p;
 	}
@@ -1611,6 +1614,11 @@ static __init int kprobe_trace_self_tests_init(void)
 
 	if (tracing_is_disabled())
 		return -ENODEV;
+
+	if (kprobe_boot_events_enabled) {
+		pr_info("Skipping kprobe tests due to kprobe_event on cmdline\n");
+		return 0;
+	}
 
 	target = kprobe_trace_selftest_target;
 
