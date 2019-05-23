e68acda38fbb14814eaf5d322fc0e97ada87fbec
Thomas Huth <thuth@redhat.com>
2019-05-23 11:31:14 +0200

    KVM: selftests: Wrap vcpu_nested_state_get/set functions with x86 guard
    struct kvm_nested_state is only available on x86 so far. To be able
    to compile the code on other architectures as well, we need to wrap
    the related code with #ifdefs.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tools/testing/selftests/kvm/lib/kvm_util.c
+++ tools/testing/selftests/kvm/lib/kvm_util.c
@@ -1249,6 +1249,7 @@ void vcpu_events_set(struct kvm_vm *vm, uint32_t vcpuid,
 		ret, errno);
 }
 
+#ifdef __x86_64__
 void vcpu_nested_state_get(struct kvm_vm *vm, uint32_t vcpuid,
 			   struct kvm_nested_state *state)
 {
@@ -1280,6 +1281,7 @@ int vcpu_nested_state_set(struct kvm_vm *vm, uint32_t vcpuid,
 
 	return ret;
 }
+#endif
 
 /*
  * VM VCPU System Regs Get
