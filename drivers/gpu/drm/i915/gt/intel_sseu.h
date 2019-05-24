247b55298a38c3da974e2a5015f9ba9df6c5a8ab
Stuart Summers <stuart.summers@intel.com>
2019-05-24 08:40:19 -0700

    drm/i915: Add macro for SSEU stride calculation
    Subslice stride and EU stride are calculated multiple times in
    i915_query. Move this calculation to a macro to reduce code duplication.
    
    v2: update headers in intel_sseu.h
    v3: use GEN_SSEU_STRIDE for stride calculations in intel_sseu.h
        apply s/bits/max_entries/ to GEN_SSEU_STRIDE parameter
    
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Stuart Summers <stuart.summers@intel.com>
    Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190524154022.13575-3-stuart.summers@intel.com

--- drivers/gpu/drm/i915/gt/intel_sseu.h
+++ drivers/gpu/drm/i915/gt/intel_sseu.h
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __INTEL_SSEU_H__
 
 #include <linux/types.h>
+#include <linux/kernel.h>
 
 struct drm_i915_private;
 
 #define GEN_MAX_SLICES		(6) /* CNL upper bound */
 #define GEN_MAX_SUBSLICES	(8) /* ICL upper bound */
+#define GEN_SSEU_STRIDE(max_entries) DIV_ROUND_UP(max_entries, BITS_PER_BYTE)
 
 struct sseu_dev_info {
 	u8 slice_mask;
