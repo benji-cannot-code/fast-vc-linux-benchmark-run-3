c1cb85fd329fded26aee3cdda3b013ec2e93eeff
Colin Ian King <colin.king@canonical.com>
2019-05-24 22:56:58 +0100

    ipv4: remove redundant assignment to n
    The pointer n is being assigned a value however this value is
    never read in the code block and the end of the code block
    continues to the next loop iteration. Clean up the code by
    removing the redundant assignment.
    
    Fixes: 1bff1a0c9bbda ("ipv4: Add function to send route updates")
    Addresses-Coverity: ("Unused value")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Reviewed-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/fib_trie.c
+++ net/ipv4/fib_trie.c
@@ -1962,7 +1962,6 @@ static void __fib_info_notify_update(struct net *net, struct fib_table *tb,
 			if (IS_TRIE(pn))
 				break;
 
-			n = pn;
 			pn = node_parent(pn);
 			cindex = get_index(pkey, pn);
 			continue;
