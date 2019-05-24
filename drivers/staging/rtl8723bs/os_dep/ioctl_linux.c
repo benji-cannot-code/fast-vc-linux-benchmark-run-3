27e6f70e23ade2c9b24ece2769ac033377039a20
Hariprasad Kelam <hariprasad.kelam@gmail.com>
2019-05-25 00:11:05 +0530

    staging: rtl8723bs: core: rtw_ap: fix Unneeded variable: "ret". Return "0"
    Function "rtw_sta_flush" always returns 0 value.
    So change return type of rtw_sta_flush from int to void.
    
    Same thing applies for rtw_hostapd_sta_flush
    
    Signed-off-by: Hariprasad Kelam <hariprasad.kelam@gmail.com>
    -----
    Changes v2 -
           change return type of rtw_sta_flush
    Changes v3 -
           fix indentaion issue
    Changes v4 -
           prepare patch on linux-next
    Changes v5 -
          cleanup subject and commit message
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/ioctl_linux.c
+++ drivers/staging/rtl8723bs/os_dep/ioctl_linux.c
@@ -3754,7 +3754,7 @@ static int rtw_set_beacon(struct net_device *dev, struct ieee_param *param, int 
 
 }
 
-static int rtw_hostapd_sta_flush(struct net_device *dev)
+static void rtw_hostapd_sta_flush(struct net_device *dev)
 {
 	/* _irqL irqL; */
 	/* struct list_head	*phead, *plist; */
@@ -3766,8 +3766,7 @@ static int rtw_hostapd_sta_flush(struct net_device *dev)
 
 	flush_all_cam_entry(padapter);	/* clear CAM */
 
-	return rtw_sta_flush(padapter);
-
+	rtw_sta_flush(padapter);
 }
 
 static int rtw_add_sta(struct net_device *dev, struct ieee_param *param)
@@ -4254,7 +4253,7 @@ static int rtw_hostapd_ioctl(struct net_device *dev, struct iw_point *p)
 	switch (param->cmd) {
 	case RTL871X_HOSTAPD_FLUSH:
 
-		ret = rtw_hostapd_sta_flush(dev);
+		rtw_hostapd_sta_flush(dev);
 
 		break;
 
