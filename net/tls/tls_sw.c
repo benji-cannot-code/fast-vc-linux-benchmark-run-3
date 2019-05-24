0c35e406180472ccbdd48c5cf3133d5d1fa8a74b
Jakub Kicinski <jakub.kicinski@netronome.com>
2019-05-24 10:34:32 -0700

    net/tls: fix no wakeup on partial reads
    When tls_sw_recvmsg() partially copies a record it pops that
    record from ctx->recv_pkt and places it on rx_list.
    
    Next iteration of tls_sw_recvmsg() reads from rx_list via
    process_rx_list() before it enters the decryption loop.
    If there is no more records to be read tls_wait_data()
    will put the process on the wait queue and got to sleep.
    This is incorrect, because some data was already copied
    in process_rx_list().
    
    In case of RPC connections process may never get woken up,
    because peer also simply blocks in read().
    
    I think this may also fix a similar issue when BPF is at
    play, because after __tcp_bpf_recvmsg() returns some data
    we subtract it from len and use continue to restart the
    loop, but len could have just reached 0, so again we'd
    sleep unnecessarily. That's added by:
    commit d3b18ad31f93 ("tls: add bpf support to sk_msg handling")
    
    Fixes: 692d7b5d1f91 ("tls: Fix recvmsg() to be able to peek across multiple records")
    Reported-by: David Beckett <david.beckett@netronome.com>
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>
    Tested-by: David Beckett <david.beckett@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tls/tls_sw.c
+++ net/tls/tls_sw.c
@@ -1720,7 +1720,7 @@ int tls_sw_recvmsg(struct sock *sk,
 	len = len - copied;
 	timeo = sock_rcvtimeo(sk, flags & MSG_DONTWAIT);
 
-	do {
+	while (len && (decrypted + copied < target || ctx->recv_pkt)) {
 		bool retain_skb = false;
 		bool zc = false;
 		int to_decrypt;
@@ -1851,11 +1851,7 @@ pick_next_record:
 		} else {
 			break;
 		}
-
-		/* If we have a new message from strparser, continue now. */
-		if (decrypted + copied >= target && !ctx->recv_pkt)
-			break;
-	} while (len);
+	}
 
 recv_end:
 	if (num_async) {
