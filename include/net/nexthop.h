bd734a485584498c12a3c966fd3b3fd2709382b9
David Ahern <dsahern@gmail.com>
2019-05-24 14:43:05 -0700

    nexthop: Add support for IPv4 nexthops
    Add support for IPv4 nexthops. If nh_family is set to AF_INET, then
    NHA_GATEWAY is expected to be an IPv4 address.
    
    Register for netdev events to be notified of admin up/down changes as
    well as deletes. A hash table is used to track nexthop per devices to
    quickly convert device events to the affected nexthops.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/nexthop.h
+++ include/net/nexthop.h
@@ -30,6 +30,10 @@ struct nh_config {
 	int		nh_ifindex;
 	struct net_device *dev;
 
+	union {
+		__be32		ipv4;
+	} gw;
+
 	u32		nlflags;
 	struct nl_info	nlinfo;
 };
@@ -43,6 +47,7 @@ struct nh_info {
 
 	union {
 		struct fib_nh_common	fib_nhc;
+		struct fib_nh		fib_nh;
 	};
 };
 
