7ba922a85cfe4e3a5303544f4a38747a8247f1d6
Sam Ravnborg <sam@ravnborg.org>
2019-05-26 19:35:31 +0200

    drm: make drm_crtc_internal.h self-contained
    Do not require users of drm_crtc_internal.h to include other
    files just to let it build
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Daniel Vetter <daniel@ffwll.ch>
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Maxime Ripard <maxime.ripard@bootlin.com>
    Cc: Sean Paul <sean@poorly.run>
    Cc: David Airlie <airlied@linux.ie>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190526173535.32701-4-sam@ravnborg.org

--- drivers/gpu/drm/drm_crtc_internal.h
+++ drivers/gpu/drm/drm_crtc_internal.h
@@ -32,14 +32,30 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * and are not exported to drivers.
  */
 
-enum drm_mode_status;
-enum drm_connector_force;
+#include <linux/types.h>
 
-struct drm_display_mode;
-struct work_struct;
-struct drm_connector;
+enum drm_color_encoding;
+enum drm_color_range;
+enum drm_connector_force;
+enum drm_mode_status;
+
+struct drm_atomic_state;
 struct drm_bridge;
+struct drm_connector;
+struct drm_crtc;
+struct drm_device;
+struct drm_display_mode;
+struct drm_file;
+struct drm_framebuffer;
+struct drm_mode_create_dumb;
+struct drm_mode_fb_cmd2;
+struct drm_mode_fb_cmd;
+struct drm_mode_object;
+struct drm_plane;
+struct drm_property;
 struct edid;
+struct kref;
+struct work_struct;
 
 /* drm_crtc.c */
 int drm_mode_crtc_set_obj_prop(struct drm_mode_object *obj,
