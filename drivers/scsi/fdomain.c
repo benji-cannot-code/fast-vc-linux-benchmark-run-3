6583ccde66bcd9cceb6a604e2b3fe4799acd715d
Ondrej Zary <linux@zary.sk>
2019-05-27 22:19:47 +0200

    scsi: fdomain: Add PCMCIA support
    Add PCMCIA card support to Future Domain SCSI driver.
    
    Tested with IBM SCSI PCMCIA Adapter 40G1890.
    
    Signed-off-by: Ondrej Zary <linux@zary.sk>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/fdomain.c
+++ drivers/scsi/fdomain.c
@@ -511,6 +511,7 @@ struct Scsi_Host *fdomain_create(int base, int irq, int this_id,
 	static const char * const chip_names[] = {
 		"Unknown", "TMC-1800", "TMC-18C50", "TMC-18C30"
 	};
+	unsigned long irq_flags = 0;
 
 	chip = fdomain_identify(base);
 	if (!chip)
@@ -542,8 +543,10 @@ struct Scsi_Host *fdomain_create(int base, int irq, int this_id,
 	fd->chip = chip;
 	INIT_WORK(&fd->work, fdomain_work);
 
-	if (request_irq(irq, fdomain_irq, dev_is_pci(dev) ? IRQF_SHARED : 0,
-			  "fdomain", fd))
+	if (dev_is_pci(dev) || !strcmp(dev->bus->name, "pcmcia"))
+		irq_flags = IRQF_SHARED;
+
+	if (request_irq(irq, fdomain_irq, irq_flags, "fdomain", fd))
 		goto fail_put;
 
 	shost_printk(KERN_INFO, sh, "%s chip at 0x%x irq %d SCSI ID %d\n",
