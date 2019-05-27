54b0884be5ab28ab5af78a17f44e8777aa195cd5
Michal Wajdeczko <michal.wajdeczko@intel.com>
2019-05-27 18:36:05 +0000

    drm/i915/guc: New GuC scratch registers for Gen11
    Gen11 adds new set of scratch registers that can be used for MMIO
    based Host-to-Guc communication. Due to limited number of these
    registers it is expected that host will use them only for command
    transport buffers (CTB) communication setup if one is available.
    
    Bspec: 21044
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190527183613.17076-10-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/intel_guc_reg.h
+++ drivers/gpu/drm/i915/intel_guc_reg.h
@@ -52,6 +52,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SOFT_SCRATCH(n)			_MMIO(0xc180 + (n) * 4)
 #define SOFT_SCRATCH_COUNT		16
 
+#define GEN11_SOFT_SCRATCH(n)		_MMIO(0x190240 + (n) * 4)
+#define GEN11_SOFT_SCRATCH_COUNT	4
+
 #define UOS_RSA_SCRATCH(i)		_MMIO(0xc200 + (i) * 4)
 #define UOS_RSA_SCRATCH_COUNT		64
 
