4afe001b5dff55e3ec138038c709432b3573e0b9
Michal Wajdeczko <michal.wajdeczko@intel.com>
2019-05-27 18:36:00 +0000

    drm/i915/guc: Reset GuC ADS during sanitize
    GuC stores some data in there, which might be stale after a reset.
    Reinitialize whole ADS in case any part of it was corrupted during
    previous GuC run.
    
    v2: s/reinit/init, update functions descriptions (Tomek/Michal)
    v3: reset ADS right before fw upload
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: MichaĹ Winiarski <michal.winiarski@intel.com>
    Cc: Tomasz Lis <tomasz.lis@intel.com>
    Reviewed-by: Tomasz Lis <tomasz.lis@intel.com> #v2
    Reviewed-by: MichaĹ Winiarski <michal.winiarski@intel.com> #v2
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190527183613.17076-5-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/intel_uc.c
+++ drivers/gpu/drm/i915/intel_uc.c
@@ -25,8 +25,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "gt/intel_reset.h"
 #include "intel_uc.h"
-#include "intel_guc_submission.h"
 #include "intel_guc.h"
+#include "intel_guc_ads.h"
+#include "intel_guc_submission.h"
 #include "i915_drv.h"
 
 static void guc_free_load_err_log(struct intel_guc *guc);
@@ -415,6 +416,7 @@ int intel_uc_init_hw(struct drm_i915_private *i915)
 				goto err_out;
 		}
 
+		intel_guc_ads_reset(guc);
 		intel_guc_init_params(guc);
 		ret = intel_guc_fw_upload(guc);
 		if (ret == 0)
