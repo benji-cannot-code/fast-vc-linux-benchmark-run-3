ca31bbb0636e3c76d8b6fcceb4bad37fd57c8e8a
Ruslan Babayev <ruslan@babayev.com>
2019-05-28 16:02:32 -0700

    i2c: acpi: export i2c_acpi_find_adapter_by_handle
    This allows drivers to lookup i2c adapters on ACPI based systems similar to
    of_get_i2c_adapter_by_node() with DT based systems.
    
    Signed-off-by: Ruslan Babayev <ruslan@babayev.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_I2C_H
 #define _LINUX_I2C_H
 
+#include <linux/acpi.h>		/* for acpi_handle */
 #include <linux/mod_devicetable.h>
 #include <linux/device.h>	/* for struct device */
 #include <linux/sched.h>	/* for completion */
@@ -982,6 +983,7 @@ bool i2c_acpi_get_i2c_resource(struct acpi_resource *ares,
 u32 i2c_acpi_find_bus_speed(struct device *dev);
 struct i2c_client *i2c_acpi_new_device(struct device *dev, int index,
 				       struct i2c_board_info *info);
+struct i2c_adapter *i2c_acpi_find_adapter_by_handle(acpi_handle handle);
 #else
 static inline bool i2c_acpi_get_i2c_resource(struct acpi_resource *ares,
 					     struct acpi_resource_i2c_serialbus **i2c)
@@ -994,6 +996,10 @@ static inline u32 i2c_acpi_find_bus_speed(struct device *dev)
 }
 static inline struct i2c_client *i2c_acpi_new_device(struct device *dev,
 					int index, struct i2c_board_info *info)
+{
+	return NULL;
+}
+static inline struct i2c_adapter *i2c_acpi_find_adapter_by_handle(acpi_handle handle)
 {
 	return NULL;
 }
