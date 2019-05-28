64aeb8f5d3eae503c4a439dc2c702d009193ffaa
Boris Brezillon <boris.brezillon@collabora.com>
2019-05-28 13:02:24 -0400

    media: rockchip/vpu: Move encoder logic to a common place
    The V4L2/VB2 implementation for the encoder and decoder logic are very
    similar, so let's rename rockchip_vpu_enc.c file into
    rockchip_vpu_v4l2.c and remove the _enc_ part in objects/functions
    exposed in rockchip_vpu_v4l2.h. We also rename the enc_queue_init()
    function (in rockchip_vpu_drv.c) queue_init() since it will be used
    to initialize both type of queues.
    
    The implementation itself will be patched to support the decoding case
    when decoder support is added.
    
    Suggested-by: Ezequiel Garcia <ezequiel@collabora.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/staging/media/rockchip/vpu/rockchip_vpu_v4l2.h
+++ drivers/staging/media/rockchip/vpu/rockchip_vpu_v4l2.h
@@ -19,12 +19,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "rockchip_vpu.h"
 
-extern const struct v4l2_ioctl_ops rockchip_vpu_enc_ioctl_ops;
-extern const struct vb2_ops rockchip_vpu_enc_queue_ops;
+extern const struct v4l2_ioctl_ops rockchip_vpu_ioctl_ops;
+extern const struct vb2_ops rockchip_vpu_queue_ops;
 
-void rockchip_vpu_enc_reset_src_fmt(struct rockchip_vpu_dev *vpu,
-				    struct rockchip_vpu_ctx *ctx);
-void rockchip_vpu_enc_reset_dst_fmt(struct rockchip_vpu_dev *vpu,
-				    struct rockchip_vpu_ctx *ctx);
+void rockchip_vpu_reset_src_fmt(struct rockchip_vpu_dev *vpu,
+				struct rockchip_vpu_ctx *ctx);
+void rockchip_vpu_reset_dst_fmt(struct rockchip_vpu_dev *vpu,
+				struct rockchip_vpu_ctx *ctx);
 
 #endif /* ROCKCHIP_VPU_V4L2_H_ */
