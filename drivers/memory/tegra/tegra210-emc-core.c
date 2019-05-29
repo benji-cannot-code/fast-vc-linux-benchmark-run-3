fc5c4061440cd0d4dcb80b9e66d4e7429e346c7a
Joseph Lo <josephl@nvidia.com>
2019-05-29 16:21:37 +0800

    memory: tegra: Add EMC scaling sequence code for Tegra210
    This patch includes the sequence for clock tuning and the dynamic
    training mechanism for the clock above 800MHz.
    
    And historically there have been different sequences to change the EMC
    clock. The sequence to be used is specified in the EMC table.
    However, for the currently supported upstreaming platform, only the most
    recent sequence is used. So only support that in this patch.
    
    Based on the work of Peter De Schrijver <pdeschrijver@nvidia.com>.
    
    Signed-off-by: Joseph Lo <josephl@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/memory/tegra/tegra210-emc-core.c
+++ drivers/memory/tegra/tegra210-emc-core.c
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		(((value) & 0xffff0000) | ((value) & 0xffff) * (speedup))
 
 static const struct tegra210_emc_sequence *tegra210_emc_sequences[] = {
+	&tegra210_emc_r21021,
 };
 
 static const struct tegra210_emc_table_register_offsets
