cff7164e6aece5138b82b620cc51c835ed69835f
Nicolin Chen <nicoleotsuka@gmail.com>
2019-05-29 17:54:25 -0700

    dma-contiguous: fix !CONFIG_DMA_CMA version of dma_{alloc, free}_contiguous()
    Commit fdaeec198ada ("dma-contiguous: add dma_{alloc,free}_contiguous()
    helpers") adds a pair of new helper functions so as to abstract code in
    the dma-direct (and other places in the future), however it breaks QEMU
    boot feature using x86_64 defconfig.
    
    That's because x86_64 defconfig has CONFIG_DMA_CMA=n so those two newly
    introduced helper functions are empty in their !CONFIG_DMA_CMA version,
    while previously the platform independent dma-direct code had fallback
    alloc_pages_node() and __free_pages().
    
    So this patch fixes it by adding alloc_pages_node() and __free_pages()
    in the !CONFIG_DMA_CMA version of the two helper functions.
    
    Tested with below QEMU command:
      qemu-system-x86_64 -m 512m \
          -drive file=images/x86_64/rootfs.ext4,format=raw,if=ide \
          -append 'console=ttyS0 root=/dev/sda' -nographic \
          -kernel arch/x86_64/boot/bzImage
    
    with the rootfs from the below link:
      https://github.com/ClangBuiltLinux/continuous-integration/raw/master/images/x86_64/rootfs.ext4
    
    Fixes: fdaeec198ada ("dma-contiguous: add dma_{alloc,free}_contiguous() helpers")
    Reported-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Nicolin Chen <nicoleotsuka@gmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/dma-contiguous.h
+++ include/linux/dma-contiguous.h
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 
 #include <linux/device.h>
+#include <linux/mm.h>
 
 struct cma;
 struct page;
@@ -156,15 +157,20 @@ bool dma_release_from_contiguous(struct device *dev, struct page *pages,
 	return false;
 }
 
+/* Use fallback alloc() and free() when CONFIG_DMA_CMA=n */
 static inline struct page *dma_alloc_contiguous(struct device *dev, size_t size,
 		gfp_t gfp)
 {
-	return NULL;
+	int node = dev ? dev_to_node(dev) : NUMA_NO_NODE;
+	size_t align = get_order(PAGE_ALIGN(size));
+
+	return alloc_pages_node(node, gfp, align);
 }
 
 static inline void dma_free_contiguous(struct device *dev, struct page *page,
 		size_t size)
 {
+	__free_pages(page, get_order(size));
 }
 
 #endif
