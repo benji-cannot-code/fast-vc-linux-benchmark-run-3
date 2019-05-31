389c898d471545b5297cef89ad5f1a8e390b9039
Edward Cree <ecree@solarflare.com>
2019-05-31 22:47:21 +0100

    flow_offload: include linux/kernel.h from flow_offload.h
    flow_stats_update() uses max_t, so ensure we have that defined.
    
    Signed-off-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/flow_offload.h
+++ include/net/flow_offload.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _NET_FLOW_OFFLOAD_H
 #define _NET_FLOW_OFFLOAD_H
 
+#include <linux/kernel.h>
 #include <net/flow_dissector.h>
 
 struct flow_match {
