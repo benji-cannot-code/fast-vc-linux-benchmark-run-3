96f66f4431e62d625cc3fd4b66e696c2a0c4653a
Dmitry Osipenko <digetx@gmail.com>
2019-06-02 17:37:09 -0400

    media: staging: media: tegra-vde: Remove BIT() macro from UAPI header
    The BIT macro isn't available in userspace. Checkpatch complains about
    shifts being used instead of the macro and people are starting to send
    patches without realizing that it's a UAPI header file. Hence let's
    replace the BIT macro with a hex values to make everyone happy.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/staging/media/tegra-vde/uapi.h
+++ drivers/staging/media/tegra-vde/uapi.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/ioctl.h>
 
-#define FLAG_B_FRAME		BIT(0)
-#define FLAG_REFERENCE		BIT(1)
+#define FLAG_B_FRAME		0x1
+#define FLAG_REFERENCE		0x2
 
 struct tegra_vde_h264_frame {
 	__s32 y_fd;
