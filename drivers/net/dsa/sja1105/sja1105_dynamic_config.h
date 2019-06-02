3d594ded656b396dc21e188c9fd4cfdca9f70e45
Vladimir Oltean <olteanv@gmail.com>
2019-06-03 00:11:56 +0300

    net: dsa: sja1105: Plug in support for TCAM searches via the dynamic interface
    Only a single dynamic configuration table of the SJA1105 P/Q/R/S
    supports this operation: the FDB.
    
    To keep the existing structure in place (sja1105_dynamic_config_read and
    sja1105_dynamic_config_write) and not introduce any new function, a
    convention is made for sja1105_dynamic_config_read that a negative index
    argument denotes a search for the entry provided as argument.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/sja1105/sja1105_dynamic_config.h
+++ drivers/net/dsa/sja1105/sja1105_dynamic_config.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sja1105.h"
 #include <linux/packing.h>
 
+/* Special index that can be used for sja1105_dynamic_config_read */
+#define SJA1105_SEARCH		-1
+
 struct sja1105_dyn_cmd;
 
 struct sja1105_dynamic_table_ops {
