7140996f4f1b7e20e307d268f182d419ecc96575
Pi-Hsun Shih <pihsun@chromium.org>
2019-06-03 11:45:11 +0800

    mfd: cros_ec: differentiate SCP from EC by feature bit
    System Companion Processor (SCP) is Cortex M4 co-processor on some
    MediaTek platform that can run EC-style firmware. Since a SCP and EC
    would both exist on a system, and use the cros_ec_dev driver, we need to
    differentiate between them for the userspace, or they would both be
    registered at /dev/cros_ec, causing a conflict.
    
    Signed-off-by: Pi-Hsun Shih <pihsun@chromium.org>
    Acked-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/cros_ec.h
+++ include/linux/mfd/cros_ec.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CROS_EC_DEV_PD_NAME "cros_pd"
 #define CROS_EC_DEV_TP_NAME "cros_tp"
 #define CROS_EC_DEV_ISH_NAME "cros_ish"
+#define CROS_EC_DEV_SCP_NAME "cros_scp"
 
 /*
  * The EC is unresponsive for a time after a reboot command.  Add a
