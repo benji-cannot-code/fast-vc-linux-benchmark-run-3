6656b5f9478db9b1a9c49a8be5462e9e147cf1f0
Geordan Neukum <gneukum1@gmail.com>
2019-06-05 01:09:11 +0000

    staging: kpc2000: kpc_spi: remove unnecessary struct member chip_select
    The structure kp_spi_controller_state, defined in the kpc2000_spi
    driver, contains a member named chip_select which is never used after
    initialization. Therefore, it should be removed for simplicity's sake.
    
    Signed-off-by: Geordan Neukum <gneukum1@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/kpc2000/kpc2000_spi.c
+++ drivers/staging/kpc2000/kpc2000_spi.c
@@ -110,7 +110,6 @@ struct kp_spi {
 
 struct kp_spi_controller_state {
 	void __iomem   *base;
-	unsigned char   chip_select;
 	s64             conf_cache;
 };
 
@@ -268,7 +267,6 @@ kp_spi_setup(struct spi_device *spidev)
 			return -ENOMEM;
 		}
 		cs->base = kpspi->base;
-		cs->chip_select = spidev->chip_select;
 		cs->conf_cache = -1;
 		spidev->controller_state = cs;
 	}
