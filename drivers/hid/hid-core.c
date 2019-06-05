0ccf5f93a1323ef7de061affad6d7953f2a23f4c
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2019-06-05 14:44:06 +0200

    Revert "HID: core: Do not call request_module() in async context"
    This reverts commit 4ceabaf7909d109db6afbffc4ed182741fc32043.
    
    This patch and a025a18fe are giving extended timeouts
    on boot for at least Ubuntu and openSUSE.
    
    Revert them until we get a better fix.
    
    Acked-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/vmalloc.h>
 #include <linux/sched.h>
 #include <linux/semaphore.h>
-#include <linux/async.h>
 
 #include <linux/hid.h>
 #include <linux/hiddev.h>
@@ -2371,9 +2370,8 @@ int hid_add_device(struct hid_device *hdev)
 	 * not first have hid-generic binding only to have it replaced
 	 * immediately afterwards with a specialized driver.
 	 */
-	if (!current_is_async())
-		request_module("hid:b%04Xg%04Xv%08Xp%08X", hdev->bus,
-			       hdev->group, hdev->vendor, hdev->product);
+	request_module("hid:b%04Xg%04Xv%08Xp%08X",
+		       hdev->bus, hdev->group, hdev->vendor, hdev->product);
 
 	hid_debug_register(hdev, dev_name(&hdev->dev));
 	ret = device_add(&hdev->dev);
