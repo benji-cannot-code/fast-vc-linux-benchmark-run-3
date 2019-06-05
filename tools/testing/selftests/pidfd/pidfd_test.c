05aa029a746ad32c3ff285442b6ba2bb9b8b2799
Christian Brauner <christian@brauner.io>
2019-06-05 15:06:32 +0200

    tests: fix pidfd-test compilation
    Define __NR_pidfd_send_signal if it isn't to prevent a potential
    compilation error.
    
    To make pidfd-test compile on all arches, irrespective of whether
    or not syscall numbers are assigned, define the syscall number to -1.
    If it isn't defined this will cause the kernel to return -ENOSYS.
    
    Fixes: 575a0ae9744d ("selftests: add tests for pidfd_send_signal()")
    Signed-off-by: Christian Brauner <christian@brauner.io>

--- tools/testing/selftests/pidfd/pidfd_test.c
+++ tools/testing/selftests/pidfd/pidfd_test.c
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../kselftest.h"
 
+#ifndef __NR_pidfd_send_signal
+#define __NR_pidfd_send_signal -1
+#endif
+
 static inline int sys_pidfd_send_signal(int pidfd, int sig, siginfo_t *info,
 					unsigned int flags)
 {
