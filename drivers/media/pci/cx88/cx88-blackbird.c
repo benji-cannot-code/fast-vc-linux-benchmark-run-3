d4d4d917ffae98550fe1b143e2b439d7bc1e4ec3
Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
2019-06-05 13:24:35 -0400

    media: add SPDX headers to some files
    Add SPDX headers and fix MODULE_LICENSE() when needed on
    some files I co-authored.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/pci/cx88/cx88-blackbird.c
+++ drivers/media/pci/cx88/cx88-blackbird.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 MODULE_DESCRIPTION("driver for cx2388x/cx23416 based mpeg encoder cards");
 MODULE_AUTHOR("Jelle Foks <jelle@foks.us>, Gerd Knorr <kraxel@bytesex.org> [SuSE Labs]");
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
 MODULE_VERSION(CX88_VERSION);
 
 static unsigned int debug;
