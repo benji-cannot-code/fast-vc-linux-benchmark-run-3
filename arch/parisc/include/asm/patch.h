e549e478ec3f06966ec83c4e671409e653136863
Sven Schnelle <svens@stackframe.org>
2019-06-05 22:32:17 +0200

    parisc: add support for patching multiple words
    add patch_text_multiple() which allows to patch multiple
    text words in memory. This can be used to copy functions.
    
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/asm/patch.h
+++ arch/parisc/include/asm/patch.h
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* stop machine and patch kernel text */
 void patch_text(void *addr, unsigned int insn);
+void patch_text_multiple(void *addr, u32 *insn, unsigned int len);
 
 /* patch kernel text with machine already stopped (e.g. in kgdb) */
-void __patch_text(void *addr, unsigned int insn);
+void __patch_text(void *addr, u32 insn);
+void __patch_text_multiple(void *addr, u32 *insn, unsigned int len);
 
 #endif
