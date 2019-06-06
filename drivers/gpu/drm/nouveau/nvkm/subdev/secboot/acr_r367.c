3d1a7c99bd0463e09a435ac3777baf3112026f39
Ben Skeggs <bskeggs@redhat.com>
2019-06-06 16:28:35 +1000

    drm/nouveau/secboot/gp10[2467]: support newer FW to fix SEC2 failures on some boards
    Some newer boards with these chipsets aren't compatible with the prior
    version of the SEC2 FW, and fail to load as a result.
    
    This newer FW is actually the one we already use on >=GP108.
    
    Unfortunately, there are interface differences in GP108's FW, making it
    impossible to simply move files around in linux-firmware to solve this.
    
    We need to be able to keep compatibility with all linux-firmware/kernel
    combinations, which means supporting both firmwares.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/secboot/acr_r367.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/secboot/acr_r367.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "acr_r367.h"
 #include "acr_r361.h"
+#include "acr_r370.h"
 
 #include <core/gpuobj.h>
 
@@ -382,6 +383,17 @@ acr_r367_fixup_hs_desc(struct acr_r352 *acr, struct nvkm_secboot *sb,
 	}
 }
 
+static const struct acr_r352_ls_func
+acr_r367_ls_sec2_func = {
+	.load = acr_ls_ucode_load_sec2,
+	.post_run = acr_ls_sec2_post_run,
+	.version_max = 1,
+	.version = {
+		&acr_r361_ls_sec2_func_0,
+		&acr_r370_ls_sec2_func_0,
+	}
+};
+
 const struct acr_r352_func
 acr_r367_func = {
 	.fixup_hs_desc = acr_r367_fixup_hs_desc,
@@ -395,7 +407,7 @@ acr_r367_func = {
 		[NVKM_SECBOOT_FALCON_FECS] = &acr_r361_ls_fecs_func,
 		[NVKM_SECBOOT_FALCON_GPCCS] = &acr_r361_ls_gpccs_func,
 		[NVKM_SECBOOT_FALCON_PMU] = &acr_r361_ls_pmu_func,
-		[NVKM_SECBOOT_FALCON_SEC2] = &acr_r361_ls_sec2_func,
+		[NVKM_SECBOOT_FALCON_SEC2] = &acr_r367_ls_sec2_func,
 	},
 };
 
