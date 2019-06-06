0541adfc4d741c83f22445aac90b99b1378f7664
Qian Cai <cai@lca.pw>
2019-06-06 09:58:13 -0400

    powerpc/cacheflush: fix variable set but not used
    The powerpc's flush_cache_vmap() is defined as a macro and never use
    both of its arguments, so it will generate a compilation warning,
    
    lib/ioremap.c: In function 'ioremap_page_range':
    lib/ioremap.c:203:16: warning: variable 'start' set but not used
    [-Wunused-but-set-variable]
    
    Fix it by making it an inline function.
    
    Signed-off-by: Qian Cai <cai@lca.pw>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>

--- arch/powerpc/include/asm/cacheflush.h
+++ arch/powerpc/include/asm/cacheflush.h
@@ -33,9 +33,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * not expect this type of fault. flush_cache_vmap is not exactly the right
  * place to put this, but it seems to work well enough.
  */
-#define flush_cache_vmap(start, end)		do { asm volatile("ptesync" ::: "memory"); } while (0)
+static inline void flush_cache_vmap(unsigned long start, unsigned long end)
+{
+	asm volatile("ptesync" ::: "memory");
+}
 #else
-#define flush_cache_vmap(start, end)		do { } while (0)
+static inline void flush_cache_vmap(unsigned long start, unsigned long end) { }
 #endif
 
 #define ARCH_IMPLEMENTS_FLUSH_DCACHE_PAGE 1
