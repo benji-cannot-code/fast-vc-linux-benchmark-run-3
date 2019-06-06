19acb3fd238160463bff6fe6847163c5dfea6a9d
Robert Hancock <hancock@sedsystems.ca>
2019-06-06 16:28:12 -0600

    net: axienet: Cleanup DMA device reset and halt process
    The Xilinx DMA blocks each have their own reset register, but they both
    reset the entire DMA engine, so only one of them needs to be reset.
    
    Also, when stopping the device, we need to not just command the DMA
    blocks to stop, but wait for them to stop, and trigger a device reset
    to ensure that they are completely stopped.
    
    Signed-off-by: Robert Hancock <hancock@sedsystems.ca>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/xilinx/xilinx_axienet.h
+++ drivers/net/ethernet/xilinx/xilinx_axienet.h
@@ -84,6 +84,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define XAXIDMA_CR_RUNSTOP_MASK	0x00000001 /* Start/stop DMA channel */
 #define XAXIDMA_CR_RESET_MASK	0x00000004 /* Reset DMA engine */
 
+#define XAXIDMA_SR_HALT_MASK	0x00000001 /* Indicates DMA channel halted */
+
 #define XAXIDMA_BD_NDESC_OFFSET		0x00 /* Next descriptor pointer */
 #define XAXIDMA_BD_BUFA_OFFSET		0x08 /* Buffer address */
 #define XAXIDMA_BD_CTRL_LEN_OFFSET	0x18 /* Control/buffer length */
