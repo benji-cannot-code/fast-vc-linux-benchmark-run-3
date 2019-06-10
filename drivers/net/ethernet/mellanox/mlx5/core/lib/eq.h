0a1e4f52c2df8020001ea047a0e57c33eed73cde
Yuval Avnery <yuvalav@mellanox.com>
2019-06-10 23:38:37 +0000

    net/mlx5: Move all IRQ logic to pci_irq.c
    Finalize IRQ separation and expose irq interface.
    
    Signed-off-by: Yuval Avnery <yuvalav@mellanox.com>
    Reviewed-by: Parav Pandit <parav@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/lib/eq.h
+++ drivers/net/ethernet/mellanox/mlx5/core/lib/eq.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mlx5/eq.h>
 #include <linux/mlx5/cq.h>
 
-#define MLX5_MAX_IRQ_NAME   (32)
 #define MLX5_EQE_SIZE       (sizeof(struct mlx5_eqe))
 
 struct mlx5_eq_tasklet {
