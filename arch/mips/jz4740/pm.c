c77053f28f0860de68a6b6cdec20cd45d3d6abce
Paul Cercueil <paul@crapouillou.net>
2019-06-11 20:07:55 +0200

    MIPS: jz4740: PM: Let CGU driver suspend clocks and set sleep mode
    Instead of forcing the jz4740 clocks to suspend here, we let the CGU
    driver handle it.
    We also let the CGU driver set the "sleep mode" bit.
    
    This has the added benefit that now it is possible to build a kernel on
    SoCs newer than the JZ4740 with CONFIG_PM.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- arch/mips/jz4740/pm.c
+++ arch/mips/jz4740/pm.c
@@ -19,21 +19,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/suspend.h>
 
-#include <asm/mach-jz4740/clock.h>
-
 static int jz4740_pm_enter(suspend_state_t state)
 {
-	jz4740_clock_suspend();
-
-	jz4740_clock_set_wait_mode(JZ4740_WAIT_MODE_SLEEP);
-
 	__asm__(".set\tmips3\n\t"
 		"wait\n\t"
 		".set\tmips0");
 
-	jz4740_clock_set_wait_mode(JZ4740_WAIT_MODE_IDLE);
 
-	jz4740_clock_resume();
 
 	return 0;
 }
