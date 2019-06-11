8fafbfa6227bb350ed1cf727bfed4fee5ebcc4f0
Hechao Li <hechaol@fb.com>
2019-06-10 17:56:51 -0700

    selftests/bpf: remove bpf_util.h from BPF C progs
    Though currently there is no problem including bpf_util.h in kernel
    space BPF C programs, in next patch in this stack, I will reuse
    libbpf_num_possible_cpus() in bpf_util.h thus include libbpf.h in it,
    which will cause BPF C programs compile error. Therefore I will first
    remove bpf_util.h from all test BPF programs.
    
    This can also make it clear that bpf_util.h is a user-space utility
    while bpf_helpers.h is a kernel space utility.
    
    Signed-off-by: Hechao Li <hechaol@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- tools/testing/selftests/bpf/bpf_endian.h
+++ tools/testing/selftests/bpf/bpf_endian.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __BPF_ENDIAN__
 #define __BPF_ENDIAN__
 
+#include <linux/stddef.h>
 #include <linux/swab.h>
 
 /* LLVM's BPF target selects the endianness of the CPU
