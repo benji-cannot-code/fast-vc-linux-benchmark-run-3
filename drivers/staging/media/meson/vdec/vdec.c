3ca0f7ece296a9eaf3d8b9f136a6447a681f5532
YueHaibing <yuehaibing@huawei.com>
2019-06-12 06:05:36 -0400

    media: meson: vdec: Add missing kthread.h
    Fix building error:
    
     of function kthread_should_stop;
     did you mean thread_saved_sp? [-Werror=implicit-function-declaration]
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Maxime Jourdan <mjourdan@baylibre.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/staging/media/meson/vdec/vdec.c
+++ drivers/staging/media/meson/vdec/vdec.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mfd/syscon.h>
 #include <linux/slab.h>
 #include <linux/interrupt.h>
+#include <linux/kthread.h>
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-event.h>
 #include <media/v4l2-ctrls.h>
