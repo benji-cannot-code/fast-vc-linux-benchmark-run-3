09cd8fdd4675a82a47d873d99a409f375aa15dd5
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2019-06-12 21:59:00 +0200

    pwm: meson: Use GENMASK and FIELD_PREP for the lo and hi values
    meson_pwm_calc() ensures that "lo" is always less than 16 bits wide
    (otherwise it would overflow into the "hi" part of the REG_PWM_{A,B}
    register).
    Use GENMASK and FIELD_PREP for the lo and hi values to make it easier to
    spot how wide these are internally. Additionally this is a preparation
    step for the .get_state() implementation where the GENMASK() for lo and
    hi becomes handy because it can be used with FIELD_GET() to extract the
    values from the register REG_PWM_{A,B} register.
    
    No functional changes intended.
    
    Reviewed-by: Neil Armstrong <narmstrong@baylibre.com>
    Reviewed-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-meson.c
+++ drivers/pwm/pwm-meson.c
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2014 Amlogic, Inc.
  */
 
+#include <linux/bitfield.h>
+#include <linux/bits.h>
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
 #include <linux/err.h>
@@ -21,7 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define REG_PWM_A		0x0
 #define REG_PWM_B		0x4
-#define PWM_HIGH_SHIFT		16
+#define PWM_LOW_MASK		GENMASK(15, 0)
+#define PWM_HIGH_MASK		GENMASK(31, 16)
 
 #define REG_MISC_AB		0x8
 #define MISC_B_CLK_EN		BIT(23)
@@ -218,7 +221,8 @@ static void meson_pwm_enable(struct meson_pwm *meson, struct pwm_device *pwm)
 	value |= clk_enable;
 	writel(value, meson->base + REG_MISC_AB);
 
-	value = (channel->hi << PWM_HIGH_SHIFT) | channel->lo;
+	value = FIELD_PREP(PWM_HIGH_MASK, channel->hi) |
+		FIELD_PREP(PWM_LOW_MASK, channel->lo);
 	writel(value, meson->base + offset);
 
 	value = readl(meson->base + REG_MISC_AB);
