bfeef6dbbf04620b78c3d01bfe4e9b2621056450
Christoph Hellwig <hch@lst.de>
2019-06-13 09:08:49 +0200

    binfmt_flat: provide a default version of flat_get_relocate_addr
    This way only the two architectures that do masking need to provide
    the helper.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Tested-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Reviewed-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Greg Ungerer <gerg@linux-m68k.org>

--- fs/binfmt_flat.c
+++ fs/binfmt_flat.c
@@ -44,6 +44,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cacheflush.h>
 #include <asm/page.h>
 
+#ifndef flat_get_relocate_addr
+#define flat_get_relocate_addr(rel)	(rel)
+#endif
+
 /****************************************************************************/
 
 /*
