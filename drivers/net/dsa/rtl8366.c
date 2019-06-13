abbe555347d8157f8f40b59164e8859b9b3664d7
Anders Roxell <anders.roxell@linaro.org>
2019-06-13 13:35:03 +0200

    net: dsa: fix warning same module names
    When building with CONFIG_NET_DSA_REALTEK_SMI and CONFIG_REALTEK_PHY
    enabled as loadable modules, we see the following warning:
    
    warning: same module names found:
      drivers/net/phy/realtek.ko
      drivers/net/dsa/realtek.ko
    
    Rework so the driver name is realtek-smi instead of realtek.
    
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/rtl8366.c
+++ drivers/net/dsa/rtl8366.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_bridge.h>
 #include <net/dsa.h>
 
-#include "realtek-smi.h"
+#include "realtek-smi-core.h"
 
 int rtl8366_mc_is_used(struct realtek_smi *smi, int mc_index, int *used)
 {
