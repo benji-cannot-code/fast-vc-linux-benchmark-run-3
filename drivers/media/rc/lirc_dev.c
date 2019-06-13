e1397152da47143ee858d16b0297e991b172a859
Sean Young <sean@mess.org>
2019-06-13 04:49:26 -0400

    media: rc: IR signal for Panasonic air conditioner too long
    The IR signal to control the Panasonic ACXA75C00600 air conditioner has
    439 pulse/spaces. Increase limit to make it possible to transmit signal.
    
    Reported-by: Takashi Kanamaru <neuralassembly@gmail.com>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/rc/lirc_dev.c
+++ drivers/media/rc/lirc_dev.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rc-core-priv.h"
 #include <uapi/linux/lirc.h>
 
-#define LIRCBUF_SIZE	256
+#define LIRCBUF_SIZE	1024
 
 static dev_t lirc_base_dev;
 
