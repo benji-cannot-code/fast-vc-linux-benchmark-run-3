2f0b125c7c9447141a32f1371afbb1d48fcb207b
Christoph Hellwig <hch@lst.de>
2019-06-13 09:08:52 +0200

    binfmt_flat: remove the uapi <linux/flat.h> header
    The split between the two flat.h files is completely arbitrary, and the
    uapi version even contains CONFIG_ ifdefs that can't work in userspace.
    The only userspace program known to use the header is elf2flt, and it
    ships with its own version of the combined header.
    
    Use the chance to move the <asm/flat.h> inclusion out of this file, as it
    is in no way needed for the format defintion, but just for the binfmt
    implementation.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Tested-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Reviewed-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Signed-off-by: Greg Ungerer <gerg@linux-m68k.org>

--- fs/binfmt_flat.c
+++ fs/binfmt_flat.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/unaligned.h>
 #include <asm/cacheflush.h>
 #include <asm/page.h>
+#include <asm/flat.h>
 
 #ifndef flat_get_relocate_addr
 #define flat_get_relocate_addr(rel)	(rel)
