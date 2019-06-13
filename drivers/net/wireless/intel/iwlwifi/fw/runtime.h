ec12c3087c9594c653f1f652c166d3c85b9bca7e
Shahar S Matityahu <shahar.s.matityahu@intel.com>
2019-06-13 16:41:35 +0300

    iwlwifi: dbg_ini: separate cfg and dump flows to different modules
    separate configuration flows and dump collection flows.
    make ini configuration flows be in iwl-dbg-tlv.c and dump related flows
    in dbg.c to better reflect their logical difference.
    
    Signed-off-by: Shahar S Matityahu <shahar.s.matityahu@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/fw/runtime.h
+++ drivers/net/wireless/intel/iwlwifi/fw/runtime.h
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "iwl-trans.h"
 #include "img.h"
 #include "fw/api/debug.h"
-#include "fw/api/dbg-tlv.h"
 #include "fw/api/paging.h"
 #include "iwl-eeprom-parse.h"
 
