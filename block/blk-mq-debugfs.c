3f9e97450d023ae9981899abddcc5d65c4069828
Pavel Begunkov <asml.silence@gmail.com>
2019-06-14 14:39:26 +0300

    blk-mq/debugfs: Fix improper print qualifier
    struct blk_rq_stat::mean is a u64 value, so use %llu
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-mq-debugfs.c
+++ block/blk-mq-debugfs.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static void print_stat(struct seq_file *m, struct blk_rq_stat *stat)
 {
 	if (stat->nr_samples) {
-		seq_printf(m, "samples=%d, mean=%lld, min=%llu, max=%llu",
+		seq_printf(m, "samples=%d, mean=%llu, min=%llu, max=%llu",
 			   stat->nr_samples, stat->mean, stat->min, stat->max);
 	} else {
 		seq_puts(m, "samples=0");
