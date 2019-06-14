4a6f1e0c3f39e14a8f9fbb2defda82b13c9076fd
Bitan Biswas <bbiswas@nvidia.com>
2019-06-14 09:31:01 -0700

    mailbox: tegra: avoid resume NULL mailboxes
    If Tegra HSP device tree does not have 'shared irqs',
    mailboxes pointer is NULL. Add non-NULL HSP mailboxes
    check in resume callback before tegra_hsp_mailbox_startup()
    call and prevent NULL pointer exception.
    
    Signed-off-by: Bitan Biswas <bbiswas@nvidia.com>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- drivers/mailbox/tegra-hsp.c
+++ drivers/mailbox/tegra-hsp.c
@@ -783,11 +783,13 @@ static int __maybe_unused tegra_hsp_resume(struct device *dev)
 			tegra_hsp_doorbell_startup(db->channel.chan);
 	}
 
-	for (i = 0; i < hsp->num_sm; i++) {
-		struct tegra_hsp_mailbox *mb = &hsp->mailboxes[i];
+	if (hsp->mailboxes) {
+		for (i = 0; i < hsp->num_sm; i++) {
+			struct tegra_hsp_mailbox *mb = &hsp->mailboxes[i];
 
-		if (mb->channel.chan->cl)
-			tegra_hsp_mailbox_startup(mb->channel.chan);
+			if (mb->channel.chan->cl)
+				tegra_hsp_mailbox_startup(mb->channel.chan);
+		}
 	}
 
 	return 0;
