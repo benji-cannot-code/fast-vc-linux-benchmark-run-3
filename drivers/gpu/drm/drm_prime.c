b1445ec61820ea239583de91338dddf5042a40ce
Daniel Vetter <daniel.vetter@ffwll.ch>
2019-06-14 22:35:21 +0200

    drm/prime: Make DRIVER_PRIME a no-op
    Drivers must fill out the handle_to_fd and fd_to_handle hooks to
    enable export/import prime functionality already. The additional
    DRIVER_PRIME flag doesn't serve any real purpose, since the overall
    flag doesn't even tell you whether import or export or maybe even both
    is supported.
    
    Ditch it.
    
    This patch just makes it defunct, subsequent patches will remove it
    from all the drivers.
    
    Note this changes the userspace visible errno in some situations from
    EOPNOTSUPP to ENOSYS. Userspace already needs to treat both as "no
    prime support", so this should break anything.
    
    v2: Improve commit message as suggested by Emil
    
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    Reviewed-by: Eric Anholt <eric@anholt.net>
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190614203615.12639-6-daniel.vetter@ffwll.ch

--- drivers/gpu/drm/drm_prime.c
+++ drivers/gpu/drm/drm_prime.c
@@ -48,8 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * between applications, they can't be guessed like the globally unique GEM
  * names.
  *
- * Drivers that support the PRIME API must set the DRIVER_PRIME bit in the
- * &drm_driver.driver_features field, and implement the
+ * Drivers that support the PRIME API implement the
  * &drm_driver.prime_handle_to_fd and &drm_driver.prime_fd_to_handle operations.
  * GEM based drivers must use drm_gem_prime_handle_to_fd() and
  * drm_gem_prime_fd_to_handle() to implement these. For GEM based drivers the
@@ -364,9 +363,6 @@ int drm_prime_fd_to_handle_ioctl(struct drm_device *dev, void *data,
 {
 	struct drm_prime_handle *args = data;
 
-	if (!drm_core_check_feature(dev, DRIVER_PRIME))
-		return -EOPNOTSUPP;
-
 	if (!dev->driver->prime_fd_to_handle)
 		return -ENOSYS;
 
@@ -514,9 +510,6 @@ int drm_prime_handle_to_fd_ioctl(struct drm_device *dev, void *data,
 				 struct drm_file *file_priv)
 {
 	struct drm_prime_handle *args = data;
-
-	if (!drm_core_check_feature(dev, DRIVER_PRIME))
-		return -EOPNOTSUPP;
 
 	if (!dev->driver->prime_handle_to_fd)
 		return -ENOSYS;
