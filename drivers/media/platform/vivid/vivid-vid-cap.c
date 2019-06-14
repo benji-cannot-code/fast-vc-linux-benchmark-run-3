711c0a16e2fcec95d9e62addb9212a8169dce5c9
Akinobu Mita <akinobu.mita@gmail.com>
2019-06-14 10:09:06 -0400

    media: vivid: remove unnecessary min and max timeperframe constants
    The tpf_min (1/100) and tpf_max (100/1) are used as the lowest and the
    highest allowable value for the desired frame period in
    vivid_vid_cap_s_parm().
    
    But the comparison between these values is unnecessary because the compared
    value is already chosen from webcam_intervals[] (from 1/60 to 1/1).
    
    Cc: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Akinobu Mita <akinobu.mita@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/vivid/vivid-vid-cap.c
+++ drivers/media/platform/vivid/vivid-vid-cap.c
@@ -22,11 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vivid-kthread-cap.h"
 #include "vivid-vid-cap.h"
 
-/* timeperframe: min/max and default */
-static const struct v4l2_fract
-	tpf_min     = {.numerator = 1,		.denominator = FPS_MAX},
-	tpf_max     = {.numerator = FPS_MAX,	.denominator = 1};
-
 static const struct vivid_fmt formats_ovl[] = {
 	{
 		.fourcc   = V4L2_PIX_FMT_RGB565, /* gggbbbbb rrrrrggg */
@@ -1866,8 +1861,6 @@ int vivid_vid_cap_s_parm(struct file *file, void *priv,
 		i = ival_sz - 1;
 	dev->webcam_ival_idx = i;
 	tpf = webcam_intervals[dev->webcam_ival_idx];
-	tpf = V4L2_FRACT_COMPARE(tpf, <, tpf_min) ? tpf_min : tpf;
-	tpf = V4L2_FRACT_COMPARE(tpf, >, tpf_max) ? tpf_max : tpf;
 
 	/* resync the thread's timings */
 	dev->cap_seq_resync = true;
