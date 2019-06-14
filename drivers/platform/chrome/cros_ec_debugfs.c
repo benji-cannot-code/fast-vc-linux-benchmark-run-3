efb796a3c0bf4610da238c9d6d463cdf72b65eb5
Enric Balletbo i Serra <enric.balletbo@collabora.com>
2019-06-14 11:51:24 +0200

    platform/chrome: cros_ec_debugfs: Fix kernel-doc comment first line
    kernel-doc comments have a prescribed format. To be _particularly_
    correct we should also capitalise the brief description and terminate
    it with a period.
    
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

--- drivers/platform/chrome/cros_ec_debugfs.c
+++ drivers/platform/chrome/cros_ec_debugfs.c
@@ -26,7 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CIRC_ADD(idx, size, value)	(((idx) + (value)) & ((size) - 1))
 
-/* struct cros_ec_debugfs - ChromeOS EC debugging information
+/**
+ * struct cros_ec_debugfs - EC debugging information.
  *
  * @ec: EC device this debugfs information belongs to
  * @dir: dentry for debugfs files
