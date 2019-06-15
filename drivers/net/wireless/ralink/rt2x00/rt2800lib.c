ff7358884d167ce81b091e29ecfb0e64c0fe034c
Stanislaw Gruszka <sgruszka@redhat.com>
2019-06-15 12:01:00 +0200

    rt2800: do not enable watchdog by default
    Make watchdog disabled by default and add module parameter to enable it.
    
    User will have to create file in /etc/modprobe.d/ with
    
    options rt2800lib watchdog=1
    
    to enable the watchdog or load "rt2800lib watchdog=1" module manually
    before loading rt2800{soc,pci,usb} module.
    
    Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ralink/rt2x00/rt2800lib.c
+++ drivers/net/wireless/ralink/rt2x00/rt2800lib.c
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "rt2800lib.h"
 #include "rt2800.h"
 
+static bool modparam_watchdog;
+module_param_named(watchdog, modparam_watchdog, bool, S_IRUGO);
+MODULE_PARM_DESC(watchdog, "Enable watchdog to detect tx/rx hangs and reset hardware if detected");
+
 /*
  * Register access.
  * All access to the CSR registers will go through the methods
@@ -10287,8 +10291,12 @@ int rt2800_probe_hw(struct rt2x00_dev *rt2x00dev)
 		__set_bit(REQUIRE_TASKLET_CONTEXT, &rt2x00dev->cap_flags);
 	}
 
-	__set_bit(CAPABILITY_RESTART_HW, &rt2x00dev->cap_flags);
-	rt2x00dev->link.watchdog_interval = msecs_to_jiffies(100);
+	if (modparam_watchdog) {
+		__set_bit(CAPABILITY_RESTART_HW, &rt2x00dev->cap_flags);
+		rt2x00dev->link.watchdog_interval = msecs_to_jiffies(100);
+	} else {
+		rt2x00dev->link.watchdog_disabled = true;
+	}
 
 	/*
 	 * Set the rssi offset.
