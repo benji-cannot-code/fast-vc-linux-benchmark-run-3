8c7dc8a1996d633b07469e23092e9bc403de09c6
Guenter Roeck <linux@roeck-us.net>
2019-06-16 07:12:01 -0700

    xtensa/PCI: Remove unused variable
    gcc reports:
    
    arch/xtensa/kernel/pci.c:40:32: warning:
    	'pci_ctrl_tail' defined but not used
    
    which is indeed the case.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Message-Id: <1560694321-31380-1-git-send-email-linux@roeck-us.net>
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- arch/xtensa/kernel/pci.c
+++ arch/xtensa/kernel/pci.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 static struct pci_controller *pci_ctrl_head;
-static struct pci_controller **pci_ctrl_tail = &pci_ctrl_head;
 
 static int pci_bus_count;
 
