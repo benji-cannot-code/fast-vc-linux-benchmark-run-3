a63504a956ae406c0b6255c18e424ad62b22a6a3
Andrew Murray <andrew.murray@arm.com>
2019-06-17 20:01:04 +0100

    KVM: arm/arm64: Remove pmc->bitmask
    We currently use pmc->bitmask to determine the width of the pmc - however
    it's superfluous as the pmc index already describes if the pmc is a cycle
    counter or event counter. The architecture clearly describes the widths of
    these counters.
    
    Let's remove the bitmask to simplify the code.
    
    Signed-off-by: Andrew Murray <andrew.murray@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/kvm/arm_pmu.h
+++ include/kvm/arm_pmu.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct kvm_pmc {
 	u8 idx;	/* index into the pmu->pmc array */
 	struct perf_event *perf_event;
-	u64 bitmask;
 };
 
 struct kvm_pmu {
