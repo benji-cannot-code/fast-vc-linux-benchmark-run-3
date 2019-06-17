751f85ac356920a3a0053cf82a339620dc053d5a
Likun Gao <Likun.Gao@amd.com>
2019-06-17 13:38:29 +0800

    drm/amdgpu: add sdma ip block for sienna_cichlid (v5)
    Sienna_Cichlid have 4 sdma controllers.
    
    v2: add missing license to sdma_common.h (Alex)
    v3: rebase (Alex)
    v4: squash in policy fix (Alex)
    v4: squash in fw_name fix
    
    Signed-off-by: Likun Gao <Likun.Gao@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/sdma_v5_2.h
+++ drivers/gpu/drm/amd/amdgpu/sdma_v5_2.h
@@ -22,10 +22,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#ifndef __SDMA_V5_0_H__
-#define __SDMA_V5_0_H__
+#ifndef __SDMA_V5_2_H__
+#define __SDMA_V5_2_H__
 
-extern const struct amd_ip_funcs sdma_v5_0_ip_funcs;
-extern const struct amdgpu_ip_block_version sdma_v5_0_ip_block;
+extern const struct amd_ip_funcs sdma_v5_2_ip_funcs;
+extern const struct amdgpu_ip_block_version sdma_v5_2_ip_block;
 
-#endif /* __SDMA_V5_0_H__ */
+#endif /* __SDMA_V5_2_H__ */
