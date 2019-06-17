f989d3b007c24bf873c8be7ca78eaef36be4cb67
Arnd Bergmann <arnd@arndb.de>
2019-06-17 14:24:32 +0200

    ARM: ixp4xx: include irqs.h where needed
    Multiple ixp4xx specific files require macros from irqs.h that
    were moved out from mach/irqs.h, e.g.:
    
    arch/arm/mach-ixp4xx/vulcan-pci.c:41:19: error: this function declaration is not a prototype [-Werror,-Wstrict-prototypes]
    arch/arm/mach-ixp4xx/vulcan-pci.c:49:10: error: implicit declaration of function 'IXP4XX_GPIO_IRQ' [-Werror,-Wimplicit-function-declaration]
                    return IXP4XX_GPIO_IRQ(INTA);
    
    Include this header in all files that failed to build because of
    that.
    
    Fixes: dc8ef8cd3a05 ("ARM: ixp4xx: Convert to SPARSE_IRQ")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-ixp4xx/vulcan-pci.c
+++ arch/arm/mach-ixp4xx/vulcan-pci.c
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/pci.h>
 #include <asm/mach-types.h>
 
+#include "irqs.h"
+
 /* PCI controller GPIO to IRQ pin mappings */
 #define INTA	2
 #define INTB	3
