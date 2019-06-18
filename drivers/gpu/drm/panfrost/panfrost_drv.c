4520872d6d1af037b1e62ea9a3bb516f32e65a5a
Boris Brezillon <boris.brezillon@collabora.com>
2019-06-18 10:16:46 +0200

    drm/panfrost: Add a module parameter to expose unstable ioctls
    We plan to expose performance counters through 2 driver specific
    ioctls until there's a solution to expose them in a generic way.
    In order to be able to deprecate those ioctls when this new
    infrastructure is in place we add an unsafe module parameter that
    will keep those ioctls hidden unless it's set to true (which also
    has the effect of tainting the kernel).
    
    All unstable ioctl handlers should use panfrost_unstable_ioctl_check()
    to check whether they're supposed to handle the request or reject it
    with ENOSYS.
    
    Suggested-by: Emil Velikov <emil.velikov@collabora.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Signed-off-by: Rob Herring <robh@kernel.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190618081648.17297-3-boris.brezillon@collabora.com

--- drivers/gpu/drm/panfrost/panfrost_drv.c
+++ drivers/gpu/drm/panfrost/panfrost_drv.c
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "panfrost_job.h"
 #include "panfrost_gpu.h"
 
+static bool unstable_ioctls;
+module_param_unsafe(unstable_ioctls, bool, 0600);
+
 static int panfrost_ioctl_get_param(struct drm_device *ddev, void *data, struct drm_file *file)
 {
 	struct drm_panfrost_get_param *param = data;
@@ -295,6 +298,14 @@ static int panfrost_ioctl_get_bo_offset(struct drm_device *dev, void *data,
 	args->offset = bo->node.start << PAGE_SHIFT;
 
 	drm_gem_object_put_unlocked(gem_obj);
+	return 0;
+}
+
+int panfrost_unstable_ioctl_check(void)
+{
+	if (!unstable_ioctls)
+		return -ENOSYS;
+
 	return 0;
 }
 
