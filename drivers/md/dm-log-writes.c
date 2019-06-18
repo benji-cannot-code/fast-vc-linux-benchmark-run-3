5c11ef6092d445adeb6cd5b1d15490ce8c5410ae
Qu Wenruo <wqu@suse.com>
2019-06-18 13:39:38 +0800

    dm log writes: fix incorrect comment about the logged sequence example
    dm-log-writes records the sequence of completion, not submission, thus
    for the following sequence (W=write, C=complete):
    
      Wa,Wb,Wc,Cc,Ca,FLUSH,FUAd,Cb,CFLUSH,CFUAd
    
    The logged results in log device should be:
      c,a,b,flush,fua
    
    Fix the comment to give a better example.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>

--- drivers/md/dm-log-writes.c
+++ drivers/md/dm-log-writes.c
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Would result in the log looking like this:
  *
- * c,a,flush,fuad,b,<other writes>,<next flush>
+ * c,a,b,flush,fuad,<other writes>,<next flush>
  *
  * This is meant to help expose problems where file systems do not properly wait
  * on data being written before invoking a FLUSH.  FUA bypasses cache so once it
