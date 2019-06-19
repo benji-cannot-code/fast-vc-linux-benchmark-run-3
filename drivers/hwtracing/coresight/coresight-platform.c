1da0844ce737f7704994597e34c3af93c35d1eb1
Suzuki K Poulose <suzuki.poulose@arm.com>
2019-06-19 13:52:51 -0600

    coresight: Rename of_coresight to coresight-platform
    Rename the firmware handling file to a more generic
    name, in preparation for adding ACPI support. Right now
    we only support DT and we have all the platform handling
    code in of_coresight.c. Let us rename the file to
    coresight-platform.c in order to keep the platform handling
    in a single place for DT and the upcoming ACPI support.
    
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/coresight/coresight-platform.c
+++ drivers/hwtracing/coresight/coresight-platform.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpumask.h>
 #include <asm/smp_plat.h>
 
-
+#ifdef CONFIG_OF
 static int of_dev_node_match(struct device *dev, void *data)
 {
 	return dev->of_node == data;
@@ -296,3 +296,4 @@ of_get_coresight_platform_data(struct device *dev,
 	return pdata;
 }
 EXPORT_SYMBOL_GPL(of_get_coresight_platform_data);
+#endif
