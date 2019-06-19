d3e3f359f685ac8529d027fb8294990ce09004db
Josef Bacik <josef@toxicpanda.com>
2019-06-19 13:47:17 -0400

    btrfs: move btrfs_block_rsv definitions into it's own header
    Prep work for separating out all of the block_rsv related code into its
    own file.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "qgroup.h"
 #include "ref-verify.h"
 #include "space-info.h"
+#include "block-rsv.h"
 
 #undef SCRAMBLE_DELAYED_REFS
 
