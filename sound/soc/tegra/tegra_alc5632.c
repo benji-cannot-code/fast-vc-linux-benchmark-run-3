f834c2e140c904d466f3c4a59e365ab6bd155c2f
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-06-19 10:18:31 +0900

    ASoC: tegra: tegra_alc5632: don't select unnecessary Platform
    ALSA SoC is now supporting "no Platform". Sound card doesn't need to
    select "CPU component" as "Platform" anymore if it doesn't need
    special Platform.
    This patch removes such settings.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/tegra/tegra_alc5632.c
+++ sound/soc/tegra/tegra_alc5632.c
@@ -127,8 +127,7 @@ static int tegra_alc5632_asoc_init(struct snd_soc_pcm_runtime *rtd)
 
 SND_SOC_DAILINK_DEFS(pcm,
 	DAILINK_COMP_ARRAY(COMP_EMPTY()),
-	DAILINK_COMP_ARRAY(COMP_CODEC(NULL, "alc5632-hifi")),
-	DAILINK_COMP_ARRAY(COMP_EMPTY()));
+	DAILINK_COMP_ARRAY(COMP_CODEC(NULL, "alc5632-hifi")));
 
 static struct snd_soc_dai_link tegra_alc5632_dai = {
 	.name = "ALC5632",
@@ -199,8 +198,6 @@ static int tegra_alc5632_probe(struct platform_device *pdev)
 		goto err_put_codec_of_node;
 	}
 
-	tegra_alc5632_dai.platforms->of_node = tegra_alc5632_dai.cpus->of_node;
-
 	ret = tegra_asoc_utils_init(&alc5632->util_data, &pdev->dev);
 	if (ret)
 		goto err_put_cpu_of_node;
@@ -219,7 +216,6 @@ err_fini_utils:
 err_put_cpu_of_node:
 	of_node_put(tegra_alc5632_dai.cpus->of_node);
 	tegra_alc5632_dai.cpus->of_node = NULL;
-	tegra_alc5632_dai.platforms->of_node = NULL;
 err_put_codec_of_node:
 	of_node_put(tegra_alc5632_dai.codecs->of_node);
 	tegra_alc5632_dai.codecs->of_node = NULL;
@@ -238,7 +234,6 @@ static int tegra_alc5632_remove(struct platform_device *pdev)
 
 	of_node_put(tegra_alc5632_dai.cpus->of_node);
 	tegra_alc5632_dai.cpus->of_node = NULL;
-	tegra_alc5632_dai.platforms->of_node = NULL;
 	of_node_put(tegra_alc5632_dai.codecs->of_node);
 	tegra_alc5632_dai.codecs->of_node = NULL;
 
