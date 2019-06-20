49763c13f6809fb31c05e9675fc754f85ae9d9ab
Zhengyuan Liu <liuzhengyuan@kylinos.cn>
2019-06-20 18:12:17 +0100

    fs/afs: use struct_size() in kzalloc()
    As Gustavo said in other patches doing the same replace, we can now
    use the new struct_size() helper to avoid leaving these open-coded and
    prone to type mistake.
    
    Signed-off-by: Zhengyuan Liu <liuzhengyuan@kylinos.cn>
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/afs/dir.c
+++ fs/afs/dir.c
@@ -243,8 +243,7 @@ retry:
 	if (nr_inline > (PAGE_SIZE - sizeof(*req)) / sizeof(struct page *))
 		nr_inline = 0;
 
-	req = kzalloc(sizeof(*req) + sizeof(struct page *) * nr_inline,
-		      GFP_KERNEL);
+	req = kzalloc(struct_size(req, array, nr_inline), GFP_KERNEL);
 	if (!req)
 		return ERR_PTR(-ENOMEM);
 
