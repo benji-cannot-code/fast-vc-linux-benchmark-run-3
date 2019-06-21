669603735bfbcacbb30a6fc02cd772d2c3f59efe
Chris Wilson <chris@chris-wilson.co.uk>
2019-06-21 14:16:39 +0100

    drm/i915/gt: Rename i915_gt_timelines
    Since the anonymous i915_gt became struct intel_gt and encloses
    struct i915_gt_timelines, rename i915_gt_timelines to intel_gt_timelines
    to match its parentage.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190621131640.28864-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_timeline.c
+++ drivers/gpu/drm/i915/gt/intel_timeline.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct intel_timeline_hwsp {
 	struct intel_gt *gt;
-	struct i915_gt_timelines *gt_timelines;
+	struct intel_gt_timelines *gt_timelines;
 	struct list_head free_link;
 	struct i915_vma *vma;
 	u64 free_bitmap;
@@ -54,7 +54,7 @@ static struct i915_vma *__hwsp_alloc(struct intel_gt *gt)
 static struct i915_vma *
 hwsp_alloc(struct intel_timeline *timeline, unsigned int *cacheline)
 {
-	struct i915_gt_timelines *gt = &timeline->gt->timelines;
+	struct intel_gt_timelines *gt = &timeline->gt->timelines;
 	struct intel_timeline_hwsp *hwsp;
 
 	BUILD_BUG_ON(BITS_PER_TYPE(u64) * CACHELINE_BYTES > PAGE_SIZE);
@@ -103,7 +103,7 @@ hwsp_alloc(struct intel_timeline *timeline, unsigned int *cacheline)
 
 static void __idle_hwsp_free(struct intel_timeline_hwsp *hwsp, int cacheline)
 {
-	struct i915_gt_timelines *gt = hwsp->gt_timelines;
+	struct intel_gt_timelines *gt = hwsp->gt_timelines;
 	unsigned long flags;
 
 	spin_lock_irqsave(&gt->hwsp_lock, flags);
@@ -262,7 +262,7 @@ int intel_timeline_init(struct intel_timeline *timeline,
 
 static void timelines_init(struct intel_gt *gt)
 {
-	struct i915_gt_timelines *timelines = &gt->timelines;
+	struct intel_gt_timelines *timelines = &gt->timelines;
 
 	mutex_init(&timelines->mutex);
 	INIT_LIST_HEAD(&timelines->active_list);
@@ -281,7 +281,7 @@ void intel_timelines_init(struct drm_i915_private *i915)
 
 static void timeline_add_to_active(struct intel_timeline *tl)
 {
-	struct i915_gt_timelines *gt = &tl->gt->timelines;
+	struct intel_gt_timelines *gt = &tl->gt->timelines;
 
 	mutex_lock(&gt->mutex);
 	list_add(&tl->link, &gt->active_list);
@@ -290,7 +290,7 @@ static void timeline_add_to_active(struct intel_timeline *tl)
 
 static void timeline_remove_from_active(struct intel_timeline *tl)
 {
-	struct i915_gt_timelines *gt = &tl->gt->timelines;
+	struct intel_gt_timelines *gt = &tl->gt->timelines;
 
 	mutex_lock(&gt->mutex);
 	list_del(&tl->link);
@@ -299,7 +299,7 @@ static void timeline_remove_from_active(struct intel_timeline *tl)
 
 static void timelines_park(struct intel_gt *gt)
 {
-	struct i915_gt_timelines *timelines = &gt->timelines;
+	struct intel_gt_timelines *timelines = &gt->timelines;
 	struct intel_timeline *timeline;
 
 	mutex_lock(&timelines->mutex);
@@ -573,7 +573,7 @@ void __intel_timeline_free(struct kref *kref)
 
 static void timelines_fini(struct intel_gt *gt)
 {
-	struct i915_gt_timelines *timelines = &gt->timelines;
+	struct intel_gt_timelines *timelines = &gt->timelines;
 
 	GEM_BUG_ON(!list_empty(&timelines->active_list));
 	GEM_BUG_ON(!list_empty(&timelines->hwsp_free_list));
