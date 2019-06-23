f03bf97dbd4265ec19f3b143e80deb9857b7bc44
Sam Ravnborg <sam@ravnborg.org>
2019-06-23 12:35:42 +0200

    drm/mgag200: drop use of drmP.h
    Drop use of the deprecated drmP.h header file.
    Replace with necessary include files to fix build.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: David Airlie <airlied@linux.ie>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190623103542.30697-5-sam@ravnborg.org

--- drivers/gpu/drm/mgag200/mgag200_cursor.c
+++ drivers/gpu/drm/mgag200/mgag200_cursor.c
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Author: Christopher Harvey <charvey@matrox.com>
  */
 
-#include <drm/drmP.h>
+#include <drm/drm_pci.h>
+
 #include "mgag200_drv.h"
 
 static bool warn_transparent = true;
--- drivers/gpu/drm/mgag200/mgag200_main.c
+++ drivers/gpu/drm/mgag200/mgag200_main.c
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *          Matt Turner
  *          Dave Airlie
  */
-#include <drm/drmP.h>
+
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
+#include <drm/drm_pci.h>
+
 #include "mgag200_drv.h"
 
 static const struct drm_mode_config_funcs mga_mode_funcs = {
