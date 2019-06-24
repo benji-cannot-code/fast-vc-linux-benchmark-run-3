48f640440cb73ce9b27250f78656d69de293a117
Denis Efremov <efremov@linux.com>
2019-06-24 11:51:27 +0300

    parisc: asm: psw.h: missing header guard
    The psw.h header file contains #ifndef directive of the guard,
    but the complimentary #define directive is missing. The patch
    adds the appropriate #define to fix the header guard.
    
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/asm/psw.h
+++ arch/parisc/include/asm/psw.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _PARISC_PSW_H
-
+#define _PARISC_PSW_H
 
 #define	PSW_I	0x00000001
 #define	PSW_D	0x00000002
