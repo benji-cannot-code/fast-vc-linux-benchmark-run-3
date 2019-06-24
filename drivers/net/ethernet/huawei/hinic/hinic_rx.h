60ad9fdf174079027a68bcd85f5ac69117cd2c3a
Xue Chaojing <xuechaojing@huawei.com>
2019-06-24 03:50:12 +0000

    hinic: implement the statistical interface of ethtool
    This patch implement the statistical interface of ethtool, user can use
    ethtool -S to show hinic statistics.
    
    Signed-off-by: Xue Chaojing <xuechaojing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/huawei/hinic/hinic_rx.h
+++ drivers/net/ethernet/huawei/hinic/hinic_rx.h
@@ -22,7 +22,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct hinic_rxq_stats {
 	u64                     pkts;
 	u64                     bytes;
-
+	u64			errors;
+	u64			csum_errors;
+	u64			other_errors;
+	u64			alloc_skb_err;
 	struct u64_stats_sync   syncp;
 };
 
