cd5656678581dc99a84ee83b771a625be6246ec9
Gao Xiang <gaoxiang25@huawei.com>
2019-06-24 15:22:55 +0800

    staging: erofs: introduce generic decompression backend
    This patch adds a new generic decompression framework
    in order to replace the old LZ4-specific decompression code.
    
    Even though LZ4 is still the only supported algorithm, yet
    it is more cleaner and easy to integrate new algorithm than
    the old almost hard-coded decompression backend.
    
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/erofs/compress.h
+++ drivers/staging/erofs/compress.h
@@ -10,6 +10,24 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __EROFS_FS_COMPRESS_H
 #define __EROFS_FS_COMPRESS_H
 
+#include "internal.h"
+
+enum {
+	Z_EROFS_COMPRESSION_SHIFTED = Z_EROFS_COMPRESSION_MAX,
+	Z_EROFS_COMPRESSION_RUNTIME_MAX
+};
+
+struct z_erofs_decompress_req {
+	struct page **in, **out;
+
+	unsigned short pageofs_out;
+	unsigned int inputsize, outputsize;
+
+	/* indicate the algorithm will be used for decompression */
+	unsigned int alg;
+	bool inplace_io, partial_decoding;
+};
+
 /*
  * - 0x5A110C8D ('sallocated', Z_EROFS_MAPPING_STAGING) -
  * used to mark temporary allocated pages from other
@@ -36,6 +54,9 @@ static inline bool z_erofs_put_stagingpage(struct list_head *pagepool,
 		list_add(&page->lru, pagepool);
 	return true;
 }
+
+int z_erofs_decompress(struct z_erofs_decompress_req *rq,
+		       struct list_head *pagepool);
 
 #endif
 
