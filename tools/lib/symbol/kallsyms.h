2f294f93c2c82df51f96c4b05a5fdde40ffbcc75
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-06-25 18:19:33 -0300

    perf tools: Remove old baggage that is util/include/linux/ctype.h
    It was just including a ../util.h that wasn't even there:
    
      $ cat tools/perf/util/include/linux/../util.h
      cat: tools/perf/util/include/linux/../util.h: No such file or directory
      $
    
    This would make kallsyms.h get util.h somehow and then files including
    it would get util.h defined stuff, a mess, fix it.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-wlzwken4psiat4zvfbvaoqiw@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/lib/symbol/kallsyms.h
+++ tools/lib/symbol/kallsyms.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __TOOLS_KALLSYMS_H_ 1
 
 #include <elf.h>
-#include <linux/ctype.h>
 #include <linux/types.h>
 
 #ifndef KSYM_NAME_LEN
