0af287f5216162dd7aba4a0db22bd5cc1c308e7e
Vladimir Oltean <olteanv@gmail.com>
2019-06-26 02:39:33 +0300

    net: dsa: sja1105: Build PTP support in main DSA driver
    As Arnd Bergmann pointed out in commit 78fe8a28fb96 ("net: dsa: sja1105:
    fix ptp link error"), there is no point in having PTP support as a
    separate loadable kernel module.
    
    So remove the exported symbols and make sja1105.ko contain PTP support
    or not based on CONFIG_NET_DSA_SJA1105_PTP.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Acked-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/sja1105/sja1105_static_config.c
+++ drivers/net/dsa/sja1105/sja1105_static_config.c
@@ -36,7 +36,6 @@ void sja1105_pack(void *buf, const u64 *val, int start, int end, size_t len)
 	}
 	dump_stack();
 }
-EXPORT_SYMBOL_GPL(sja1105_pack);
 
 void sja1105_unpack(const void *buf, u64 *val, int start, int end, size_t len)
 {
@@ -54,7 +53,6 @@ void sja1105_unpack(const void *buf, u64 *val, int start, int end, size_t len)
 		       start, end);
 	dump_stack();
 }
-EXPORT_SYMBOL_GPL(sja1105_unpack);
 
 void sja1105_packing(void *buf, u64 *val, int start, int end,
 		     size_t len, enum packing_op op)
@@ -77,7 +75,6 @@ void sja1105_packing(void *buf, u64 *val, int start, int end,
 	}
 	dump_stack();
 }
-EXPORT_SYMBOL_GPL(sja1105_packing);
 
 /* Little-endian Ethernet CRC32 of data packed as big-endian u32 words */
 u32 sja1105_crc32(const void *buf, size_t len)
