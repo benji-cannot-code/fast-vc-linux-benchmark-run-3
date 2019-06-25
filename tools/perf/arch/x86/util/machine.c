de3679c70d70dd5234671eafac0046cc7a03e383
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-06-25 18:01:47 -0300

    tools x86 machine: Add missing util.h to pick up 'page_size'
    We're getting it by sheer luck, add that util.h to get the 'page_size'
    definition.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-347078mgj3d2jfygtxs4ntti@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/x86/util/machine.c
+++ tools/perf/arch/x86/util/machine.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <stdlib.h>
 
+#include "../../util/util.h"
 #include "../../util/machine.h"
 #include "../../util/map.h"
 #include "../../util/symbol.h"
