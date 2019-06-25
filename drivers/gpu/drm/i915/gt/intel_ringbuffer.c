590782fea029f2d2782317b449fd3728eff7e5ae
Chris Wilson <chris@chris-wilson.co.uk>
2019-06-25 20:48:59 +0100

    drm/i915/gt: Add some debug tracing for context pinning
    Add the context pin/unpin events to the trace for post-mortem debugging.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190625194859.28005-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_ringbuffer.c
+++ drivers/gpu/drm/i915/gt/intel_ringbuffer.c
@@ -1198,6 +1198,7 @@ int intel_ring_pin(struct intel_ring *ring)
 	GEM_BUG_ON(ring->vaddr);
 	ring->vaddr = addr;
 
+	GEM_TRACE("ring:%llx pin\n", ring->timeline->fence_context);
 	return 0;
 
 err_ring:
@@ -1223,6 +1224,8 @@ void intel_ring_unpin(struct intel_ring *ring)
 {
 	if (!atomic_dec_and_test(&ring->pin_count))
 		return;
+
+	GEM_TRACE("ring:%llx unpin\n", ring->timeline->fence_context);
 
 	/* Discard any unused bytes beyond that submitted to hw. */
 	intel_ring_reset(ring, ring->tail);
