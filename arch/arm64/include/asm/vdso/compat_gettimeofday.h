87bc119eba388a01676a37889c6699d8ab8a2d8e
Vincenzo Frascino <vincenzo.frascino@arm.com>
2019-06-25 17:18:04 +0100

    arm64: compat: Fix __arch_get_hw_counter() implementation
    Provide the following fixes for the __arch_get_hw_counter()
    implementation on arm64:
    - Fallback on syscall when an unstable counter is detected.
    - Introduce isb()s before and after the counter read to avoid
    speculation of the counter value and of the seq lock
    respectively.
    The second isb() is a temporary solution that will be revisited
    in 5.3-rc1.
    
    These fixes restore the semantics that __arch_counter_get_cntvct()
    had on arm64.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-arch@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-mips@vger.kernel.org
    Cc: linux-kselftest@vger.kernel.org
    Cc: catalin.marinas@arm.com
    Cc: will.deacon@arm.com
    Cc: arnd@arndb.de
    Cc: linux@armlinux.org.uk
    Cc: ralf@linux-mips.org
    Cc: paul.burton@mips.com
    Cc: daniel.lezcano@linaro.org
    Cc: salyzyn@android.com
    Cc: pcc@google.com
    Cc: shuah@kernel.org
    Cc: 0x7f454c46@gmail.com
    Cc: linux@rasmusvillemoes.dk
    Cc: huw@codeweavers.com
    Cc: sthotton@marvell.com
    Cc: andre.przywara@arm.com
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Link: https://lkml.kernel.org/r/20190625161804.38713-3-vincenzo.frascino@arm.com

--- arch/arm64/include/asm/vdso/compat_gettimeofday.h
+++ arch/arm64/include/asm/vdso/compat_gettimeofday.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/vdso/compat_barrier.h>
 
+#define __VDSO_USE_SYSCALL		ULLONG_MAX
+
 #define VDSO_HAS_CLOCK_GETRES		1
 
 static __always_inline
@@ -75,8 +77,24 @@ static __always_inline u64 __arch_get_hw_counter(s32 clock_mode)
 {
 	u64 res;
 
+	/*
+	 * clock_mode == 0 implies that vDSO are enabled otherwise
+	 * fallback on syscall.
+	 */
+	if (clock_mode)
+		return __VDSO_USE_SYSCALL;
+
+	/*
+	 * This isb() is required to prevent that the counter value
+	 * is speculated.
+	 */
 	isb();
 	asm volatile("mrrc p15, 1, %Q0, %R0, c14" : "=r" (res));
+	/*
+	 * This isb() is required to prevent that the seq lock is
+	 * speculated.
+	 */
+	isb();
 
 	return res;
 }
