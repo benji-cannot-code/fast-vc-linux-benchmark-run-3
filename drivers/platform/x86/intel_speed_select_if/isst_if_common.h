c61ee526b127fcb83c81053a10bcb028544a3f5f
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2019-06-26 15:38:47 -0700

    platform/x86: ISST: Add Intel Speed Select mailbox interface via PCI
    Add an IOCTL to send mailbox commands to PUNIT using PUNIT PCI device.
    A limited set of mailbox commands can be sent to PUNIT.
    
    This MMIO interface is used by the intel-speed-select tool under
    tools/x86/power to enumerate and control Intel Speed Select features.
    The MBOX commands ids and semantics of the message can be checked from
    the source code of the tool.
    
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/intel_speed_select_if/isst_if_common.h
+++ drivers/platform/x86/intel_speed_select_if/isst_if_common.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ISST_IF_COMMON_H
 
 #define INTEL_RAPL_PRIO_DEVID_0	0x3451
+#define INTEL_CFG_MBOX_DEVID_0	0x3459
 
 /*
  * Validate maximum commands in a single request.
@@ -61,4 +62,6 @@ struct isst_if_cmd_cb {
 int isst_if_cdev_register(int type, struct isst_if_cmd_cb *cb);
 void isst_if_cdev_unregister(int type);
 struct pci_dev *isst_if_get_pci_dev(int cpu, int bus, int dev, int fn);
+bool isst_if_mbox_cmd_set_req(struct isst_if_mbox_cmd *mbox_cmd);
+bool isst_if_mbox_cmd_invalid(struct isst_if_mbox_cmd *cmd);
 #endif
