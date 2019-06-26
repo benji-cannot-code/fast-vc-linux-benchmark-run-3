07457d058d2b8567ff66879a168026db63d2e91f
Jani Nikula <jani.nikula@intel.com>
2019-06-26 17:40:18 +0300

    drm/i915: make intel_gvt.h self-contained
    Add the minimal includes/declarations to make the header self-contained,
    and ensure it stays that way.
    
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190626144020.2155-12-jani.nikula@intel.com

--- drivers/gpu/drm/i915/intel_gvt.h
+++ drivers/gpu/drm/i915/intel_gvt.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _INTEL_GVT_H_
 #define _INTEL_GVT_H_
 
-struct intel_gvt;
+struct drm_i915_private;
 
 #ifdef CONFIG_DRM_I915_GVT
 int intel_gvt_init(struct drm_i915_private *dev_priv);
