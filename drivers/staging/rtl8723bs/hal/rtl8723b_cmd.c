e69031485367d31fe172b3aa518a9cd54ffda3fa
Hariprasad Kelam <hariprasad.kelam@gmail.com>
2019-06-26 22:43:37 +0530

    staging: rtl8723bs: hal: rtl8723b_cmd: remove set but unused variable
    Remove set but unsed variable pHalData in rtl8723b_set_FwRsvdPagePkt
    function
    
    Signed-off-by: Hariprasad Kelam <hariprasad.kelam@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/rtl8723b_cmd.c
+++ drivers/staging/rtl8723bs/hal/rtl8723b_cmd.c
@@ -1435,7 +1435,6 @@ static void rtl8723b_set_FwRsvdPagePkt(
 	struct adapter *padapter, bool bDLFinished
 )
 {
-	struct hal_com_data *pHalData;
 	struct xmit_frame *pcmdframe;
 	struct pkt_attrib *pattrib;
 	struct xmit_priv *pxmitpriv;
@@ -1465,7 +1464,6 @@ static void rtl8723b_set_FwRsvdPagePkt(
 
 	/* DBG_871X("%s---->\n", __func__); */
 
-	pHalData = GET_HAL_DATA(padapter);
 	pxmitpriv = &padapter->xmitpriv;
 	pmlmeext = &padapter->mlmeextpriv;
 	pmlmeinfo = &pmlmeext->mlmext_info;
