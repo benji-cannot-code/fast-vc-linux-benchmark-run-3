d5a9fa5f20e148289e2eaff76e6aacb0396ab237
Jani Nikula <jani.nikula@intel.com>
2019-06-26 17:40:17 +0300

    drm/i915: make intel_guc_reg.h self-contained
    Add the minimal includes/declarations to make the header self-contained,
    and ensure it stays that way.
    
    v2: also include i915_reg.h (Michal)
    
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190626144020.2155-11-jani.nikula@intel.com

--- drivers/gpu/drm/i915/intel_guc_reg.h
+++ drivers/gpu/drm/i915/intel_guc_reg.h
@@ -25,6 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _INTEL_GUC_REG_H_
 #define _INTEL_GUC_REG_H_
 
+#include <linux/compiler.h>
+#include <linux/types.h>
+
+#include "i915_reg.h"
+
 /* Definitions of GuC H/W registers, bits, etc */
 
 #define GUC_STATUS			_MMIO(0xc000)
