06c1127d3397200a67f417259b460aeaecbce001
Ajay Singh <ajay.kathat@microchip.com>
2019-06-26 12:41:06 +0000

    staging: wilc1000: rename 'host_interface' source and header
    Rename 'host_interface' source and header file to include the 'wilc_'
    prefix in its name.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_netdevice.h
+++ drivers/staging/wilc1000/wilc_wfi_netdevice.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 #include <linux/gpio/consumer.h>
 
-#include "host_interface.h"
+#include "wilc_hif.h"
 #include "wilc_wlan.h"
 #include "wilc_wlan_cfg.h"
 
