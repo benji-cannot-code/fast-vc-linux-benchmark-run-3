3d211f04bfa671588ec661479c2a0b8de7c57c7a
Linus Walleij <linus.walleij@linaro.org>
2019-06-26 11:21:19 +0200

    x86/platform/geode: Drop <linux/gpio.h> includes
    These board files only use gpio_keys not gpio in general.  This include is
    just surplus, delete it.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-gpio@vger.kernel.org
    Cc: Andres Salomon <dilinger@queued.net>
    Cc: linux-geode@lists.infradead.org
    Cc: Andy Shevchenko <andy@infradead.org>
    Cc: Darren Hart <dvhart@infradead.org>
    Cc: platform-driver-x86@vger.kernel.org
    Link: https://lkml.kernel.org/r/20190626092119.3172-1-linus.walleij@linaro.org

--- arch/x86/platform/geode/alix.c
+++ arch/x86/platform/geode/alix.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/moduleparam.h>
 #include <linux/leds.h>
 #include <linux/platform_device.h>
-#include <linux/gpio.h>
 #include <linux/input.h>
 #include <linux/gpio_keys.h>
 #include <linux/dmi.h>
--- arch/x86/platform/geode/net5501.c
+++ arch/x86/platform/geode/net5501.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/leds.h>
 #include <linux/platform_device.h>
-#include <linux/gpio.h>
 #include <linux/input.h>
 #include <linux/gpio_keys.h>
 
