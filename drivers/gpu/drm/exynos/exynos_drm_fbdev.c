ef1ddf541b133942d268c05f6d121220ba54eb84
Sam Ravnborg <sam@ravnborg.org>
2019-06-27 20:00:54 +0900

    drm/exynos: trigger build of all modules
    Add COMPILE_TEST dependency to force exynos driver to
    built for more than arm and to built modules
    that otherwise required other symbols to be de-selected.
    
    This will increase build coverage of the exynos driver
    thus allowing most trivial build errors to be detected/fixed early.
    
    This introduces one warning when built using sh:
    exynos7_drm_decon.c: In function ‘decon_remove’:
    exynos7_drm_decon.c:769:24: warning: unused variable ‘ctx’
      struct decon_context *ctx = dev_get_drvdata(&pdev->dev);
    
    This is due to the definition of iounmap() in sh,
    and nothing that exynos driver can fix.
    
    Include fix of exynos build for alpha.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Inki Dae <inki.dae@samsung.com>
    Cc: Joonyoung Shim <jy0922.shim@samsung.com>
    Cc: Seung-Woo Kim <sw0312.kim@samsung.com>
    Cc: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Kukjin Kim <kgene@kernel.org>
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Jingoo Han <jingoohan1@gmail.com>
    Signed-off-by: Inki Dae <inki.dae@samsung.com>

--- drivers/gpu/drm/exynos/exynos_drm_fbdev.c
+++ drivers/gpu/drm/exynos/exynos_drm_fbdev.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/console.h>
 #include <linux/dma-mapping.h>
+#include <linux/vmalloc.h>
 
 #include <drm/drm_crtc.h>
 #include <drm/drm_fb_helper.h>
