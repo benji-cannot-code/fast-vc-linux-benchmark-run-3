21c0dcf7afaebd5d1425751b1fae953d273586f3
tiancyin <tianci.yin@amd.com>
2019-06-27 14:19:37 +0800

    drm/amd/powerplay: update smu11_driver_if_navi10.h
    update the smu11_driver_if_navi10.h since navi10 smu fw
    update to 42.28
    
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: tiancyin <tianci.yin@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/navi10_ppt.c
+++ drivers/gpu/drm/amd/powerplay/navi10_ppt.c
@@ -864,7 +864,7 @@ static int navi10_get_gpu_power(struct smu_context *smu, uint32_t *value)
 	if (ret)
 		return ret;
 
-	*value = metrics.CurrSocketPower << 8;
+	*value = metrics.AverageSocketPower << 8;
 
 	return 0;
 }
