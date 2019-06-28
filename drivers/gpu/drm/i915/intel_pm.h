3b8eded42d0ca3e44509362b76ad84c01b4e7938
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2019-06-28 10:55:13 +0200

    drm/i915: Convert most of atomic commit to take more intel state
    Instead of passing along drm_crtc_state and drm_atomic_state, pass
    along more intel_atomic_state and intel_crtc_state. This will
    make the code more readable by not casting between drm state
    and intel state all the time.
    
    While at it, rename old_state to state, with the get_new/old helpers
    there is no point in distinguishing between state before and after
    swapping state any more. (Ville)
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190628085517.31886-3-maarten.lankhorst@linux.intel.com

--- drivers/gpu/drm/i915/intel_pm.h
+++ drivers/gpu/drm/i915/intel_pm.h
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "i915_reg.h"
 
-struct drm_atomic_state;
 struct drm_device;
 struct drm_i915_private;
 struct i915_request;
+struct intel_atomic_state;
 struct intel_crtc;
 struct intel_crtc_state;
 struct intel_plane;
@@ -53,7 +53,7 @@ void skl_pipe_wm_get_hw_state(struct intel_crtc *crtc,
 			      struct skl_pipe_wm *out);
 void g4x_wm_sanitize(struct drm_i915_private *dev_priv);
 void vlv_wm_sanitize(struct drm_i915_private *dev_priv);
-bool intel_can_enable_sagv(struct drm_atomic_state *state);
+bool intel_can_enable_sagv(struct intel_atomic_state *state);
 int intel_enable_sagv(struct drm_i915_private *dev_priv);
 int intel_disable_sagv(struct drm_i915_private *dev_priv);
 bool skl_wm_level_equals(const struct skl_wm_level *l1,
