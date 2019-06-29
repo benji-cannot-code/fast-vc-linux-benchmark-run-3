6846171658a2036143d9169983107dedc3443bc8
Linus Walleij <linus.walleij@linaro.org>
2019-06-29 14:59:28 +0200

    drm/msm/dsi: Drop unused GPIO includes
    This DSI driver uses the new descriptor API so these old
    GPIO API includes are surplus.
    
    Cc: Rob Clark <robdclark@gmail.com>
    Cc: Sean Paul <sean@poorly.run>
    Cc: linux-arm-msm@vger.kernel.org
    Cc: freedreno@lists.freedesktop.org
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/dsi/dsi_host.c
+++ drivers/gpu/drm/msm/dsi/dsi_host.c
@@ -8,12 +8,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/dma-mapping.h>
 #include <linux/err.h>
-#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/mfd/syscon.h>
 #include <linux/of_device.h>
-#include <linux/of_gpio.h>
 #include <linux/of_graph.h>
 #include <linux/of_irq.h>
 #include <linux/pinctrl/consumer.h>
