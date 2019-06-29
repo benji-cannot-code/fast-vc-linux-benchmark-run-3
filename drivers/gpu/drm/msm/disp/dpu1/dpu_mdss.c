a347284d748532f2e5af197660d1520767bf576c
Linus Walleij <linus.walleij@linaro.org>
2019-06-29 14:59:29 +0200

    drm/msm/dpu: Drop unused GPIO code
    The DPU has some kind of idea that it wants to be able to
    bring up power using GPIO lines. The struct dss_gpio is however
    completely unused and should this be done, it should be done
    using the GPIO descriptor framework rather than this API
    which relies on the global GPIO numberspace. Delete this
    code before anyone hurt themselves.
    
    The inclusion of <linux/gpio.h> was abused to get some OF
    and IRQ headers implicitly included into the DPU utilities,
    make these includes explicit and push them down into the actual
    implementation.
    
    Cc: Rob Clark <robdclark@gmail.com>
    Cc: Sean Paul <sean@poorly.run>
    Cc: linux-arm-msm@vger.kernel.org
    Cc: freedreno@lists.freedesktop.org
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_mdss.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_mdss.c
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2018, The Linux Foundation
  */
 
+#include <linux/irq.h>
+#include <linux/irqchip.h>
+#include <linux/irqdesc.h>
+#include <linux/irqchip/chained_irq.h>
 #include "dpu_kms.h"
 #include <linux/interconnect.h>
 
