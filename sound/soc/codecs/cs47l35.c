663737dd92145fa7ac7d0a36c787cc9e93ac3d6a
YueHaibing <yuehaibing@huawei.com>
2019-06-29 02:43:33 +0000

    ASoC: madera: Remove duplicated include from cs47l35.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/cs47l35.c
+++ sound/soc/codecs/cs47l35.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
 #include <sound/tlv.h>
-#include <sound/tlv.h>
 
 #include <linux/irqchip/irq-madera.h>
 #include <linux/mfd/madera/core.h>
