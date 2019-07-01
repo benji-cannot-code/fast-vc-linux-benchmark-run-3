774ef2236a7daf15635190666ffbfc74284cc8b0
Nishka Dasgupta <nishkadg.linux@gmail.com>
2019-07-01 14:48:13 +0530

    staging: rtl8723bs: Remove rtw_btcoex_IsBtDisabled()
    Remove function rtw_btcoex_IsBtDisabled as it does nothing except call
    hal_btcoex_IsBtDisabled.
    Modify call sites accordingly.
    Issue found wth Coccinelle.
    
    Signed-off-by: Nishka Dasgupta <nishkadg.linux@gmail.com>
    Link: https://lore.kernel.org/r/20190701091817.12759-6-nishkadg.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_pwrctrl.c
+++ drivers/staging/rtl8723bs/core/rtw_pwrctrl.c
@@ -482,7 +482,7 @@ void rtw_set_ps_mode(struct adapter *padapter, u8 ps_mode, u8 smart_ps, u8 bcn_a
 			if (pwrpriv->alives == 0)
 				pslv = PS_STATE_S0;
 
-			if ((rtw_btcoex_IsBtDisabled(padapter) == false)
+			if ((hal_btcoex_IsBtDisabled(padapter) == false)
 				&& (hal_btcoex_IsBtControlLps(padapter) == true)) {
 				u8 val8;
 
@@ -911,7 +911,7 @@ void rtw_unregister_task_alive(struct adapter *padapter, u32 task)
 	pwrctrl = adapter_to_pwrctl(padapter);
 	pslv = PS_STATE_S0;
 
-	if ((rtw_btcoex_IsBtDisabled(padapter) == false)
+	if ((hal_btcoex_IsBtDisabled(padapter) == false)
 		&& (hal_btcoex_IsBtControlLps(padapter) == true)) {
 		u8 val8;
 
@@ -1052,7 +1052,7 @@ void rtw_unregister_tx_alive(struct adapter *padapter)
 	pwrctrl = adapter_to_pwrctl(padapter);
 	pslv = PS_STATE_S0;
 
-	if ((rtw_btcoex_IsBtDisabled(padapter) == false)
+	if ((hal_btcoex_IsBtDisabled(padapter) == false)
 		&& (hal_btcoex_IsBtControlLps(padapter) == true)) {
 		u8 val8;
 
@@ -1094,7 +1094,7 @@ void rtw_unregister_cmd_alive(struct adapter *padapter)
 	pwrctrl = adapter_to_pwrctl(padapter);
 	pslv = PS_STATE_S0;
 
-	if ((rtw_btcoex_IsBtDisabled(padapter) == false)
+	if ((hal_btcoex_IsBtDisabled(padapter) == false)
 		&& (hal_btcoex_IsBtControlLps(padapter) == true)) {
 		u8 val8;
 
