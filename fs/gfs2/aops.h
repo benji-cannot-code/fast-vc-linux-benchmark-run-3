29ae4789eb249f651339f4fbc8a76ce08bb0360a
Christoph Hellwig <hch@lst.de>
2019-07-01 23:54:32 +0200

    gfs2: remove the unused gfs2_stuffed_write_end function
    This function was overlooked when the write_begin and write_end address space
    operations were removed as part of gfs2's iomap conversion.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/aops.h
+++ fs/gfs2/aops.h
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "incore.h"
 
 extern int stuffed_readpage(struct gfs2_inode *ip, struct page *page);
-extern int gfs2_stuffed_write_end(struct inode *inode, struct buffer_head *dibh,
-				  loff_t pos, unsigned copied,
-				  struct page *page);
 extern void adjust_fs_space(struct inode *inode);
 extern void gfs2_page_add_databufs(struct gfs2_inode *ip, struct page *page,
 				   unsigned int from, unsigned int len);
