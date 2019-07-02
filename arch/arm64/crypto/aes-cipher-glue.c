28dc8bcfda39599290e8a8dfa957e4231e398d7c
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2019-07-02 21:41:36 +0200

    crypto: arm64/aes-ce-cipher - use AES library as fallback
    Instead of calling into the table based scalar AES code in situations
    where the SIMD unit may not be used, use the generic AES code, which
    is more appropriate since it is less likely to be susceptible to
    timing attacks.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- arch/arm64/crypto/aes-cipher-glue.c
+++ arch/arm64/crypto/aes-cipher-glue.c
@@ -11,10 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 
 asmlinkage void __aes_arm64_encrypt(u32 *rk, u8 *out, const u8 *in, int rounds);
-EXPORT_SYMBOL(__aes_arm64_encrypt);
-
 asmlinkage void __aes_arm64_decrypt(u32 *rk, u8 *out, const u8 *in, int rounds);
-EXPORT_SYMBOL(__aes_arm64_decrypt);
 
 static void aes_arm64_encrypt(struct crypto_tfm *tfm, u8 *out, const u8 *in)
 {
