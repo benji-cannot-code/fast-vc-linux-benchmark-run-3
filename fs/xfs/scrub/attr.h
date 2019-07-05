efdbd837007f97819d4c8543c32f83c02acf94fe
Darrick J. Wong <darrick.wong@oracle.com>
2019-07-05 10:29:56 -0700

    xfs: only allocate memory for scrubbing attributes when we need it
    In examining a flame graph of time spent running xfs_scrub on various
    filesystems, I noticed that we spent nearly 7% of the total runtime on
    allocating a zeroed 65k buffer for every SCRUB_TYPE_XATTR invocation.
    We do this even if none of the attribute values were anywhere near 64k
    in size, even if there were no attribute blocks to check space on, and
    even if it just turns out there are no attributes at all.
    
    Therefore, rearrange the xattr buffer setup code to support reallocating
    with a bigger buffer and redistribute the callers of that function so
    that we only allocate memory just prior to needing it, and only allocate
    as much as we need.  If we can't get memory with the ILOCK held we'll
    bail out with EDEADLOCK which will allocate the maximum memory.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Brian Foster <bfoster@redhat.com>

--- fs/xfs/scrub/attr.h
+++ fs/xfs/scrub/attr.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Temporary storage for online scrub and repair of extended attributes.
  */
 struct xchk_xattr_buf {
+	/* Size of @buf, in bytes. */
+	size_t			sz;
+
 	/*
 	 * Memory buffer -- either used for extracting attr values while
 	 * walking the attributes; or for computing attr block bitmaps when
@@ -63,6 +66,7 @@ xchk_xattr_dstmap(
 			BITS_TO_LONGS(sc->mp->m_attr_geo->blksize);
 }
 
-int xchk_setup_xattr_buf(struct xfs_scrub *sc, size_t value_size);
+int xchk_setup_xattr_buf(struct xfs_scrub *sc, size_t value_size,
+		xfs_km_flags_t flags);
 
 #endif	/* __XFS_SCRUB_ATTR_H__ */
