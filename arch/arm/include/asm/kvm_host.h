eb9e0c9ea5cbd1a4afa2d0217be9eabcc807d922
Marc Zyngier <marc.zyngier@arm.com>
2019-07-05 23:35:56 +0100

    KVM: arm/arm64: Initialise host's MPIDRs by reading the actual register
    As part of setting up the host context, we populate its
    MPIDR by using cpu_logical_map(). It turns out that contrary
    to arm64, cpu_logical_map() on 32bit ARM doesn't return the
    *full* MPIDR, but a truncated version.
    
    This leaves the host MPIDR slightly corrupted after the first
    run of a VM, since we won't correctly restore the MPIDR on
    exit. Oops.
    
    Since we cannot trust cpu_logical_map(), let's adopt a different
    strategy. We move the initialization of the host CPU context as
    part of the per-CPU initialization (which, in retrospect, makes
    a lot of sense), and directly read the MPIDR from the HW. This
    is guaranteed to work on both arm and arm64.
    
    Reported-by: Andre Przywara <Andre.Przywara@arm.com>
    Tested-by: Andre Przywara <Andre.Przywara@arm.com>
    Fixes: 32f139551954 ("arm/arm64: KVM: Statically configure the host's view of MPIDR")
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/include/asm/kvm_host.h
+++ arch/arm/include/asm/kvm_host.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/kvm_asm.h>
 #include <asm/kvm_mmio.h>
 #include <asm/fpstate.h>
-#include <asm/smp_plat.h>
 #include <kvm/arm_arch_timer.h>
 
 #define __KVM_HAVE_ARCH_INTC_INITIALIZED
@@ -148,11 +147,10 @@ struct kvm_host_data {
 
 typedef struct kvm_host_data kvm_host_data_t;
 
-static inline void kvm_init_host_cpu_context(struct kvm_cpu_context *cpu_ctxt,
-					     int cpu)
+static inline void kvm_init_host_cpu_context(struct kvm_cpu_context *cpu_ctxt)
 {
 	/* The host's MPIDR is immutable, so let's set it up at boot time */
-	cpu_ctxt->cp15[c0_MPIDR] = cpu_logical_map(cpu);
+	cpu_ctxt->cp15[c0_MPIDR] = read_cpuid_mpidr();
 }
 
 struct vcpu_reset_state {
