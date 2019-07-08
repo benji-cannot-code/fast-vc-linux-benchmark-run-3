5db5a5d484446a5a7d72e8caa0c577ab83a52399
Lucas De Marchi <lucas.demarchi@intel.com>
2019-07-08 10:28:14 -0700

    drm/i915: move intel_ddi_set_fia_lane_count to intel_tc.c
    PORT_TX_DFLEXDPMLE1 is a FIA register so move it to intel_tc.c where we
    access other FIA registers. In Tiger Lake we have multiple/modular FIAs
    so it makes sense to start moving all access to their registers to a
    common place.
    
    While at it, make it clear that we will only ever call this function
    for ports with TC phy. Previously we were relying on tc_mode being
    TC_PORT_TBT_ALT for combo phy ports. However it's confusing since in
    this same function we have checks for is_tc_port. Also, if we manage to
    make each phy access only their own field, we may in future add them as
    a union inside intel_digital_port.
    
    v2: Fix coding style while moving the code
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190708172815.6814-4-lucas.demarchi@intel.com

--- drivers/gpu/drm/i915/display/intel_tc.h
+++ drivers/gpu/drm/i915/display/intel_tc.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 bool intel_tc_port_connected(struct intel_digital_port *dig_port);
 u32 intel_tc_port_get_lane_mask(struct intel_digital_port *dig_port);
 int intel_tc_port_fia_max_lane_count(struct intel_digital_port *dig_port);
+void intel_tc_port_set_fia_lane_count(struct intel_digital_port *dig_port,
+				      int required_lanes);
 
 void intel_tc_port_sanitize(struct intel_digital_port *dig_port);
 void intel_tc_port_lock(struct intel_digital_port *dig_port);
