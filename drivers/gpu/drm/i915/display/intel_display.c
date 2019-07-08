4fddfd2bfda2261a75bcbf793e46f5cd8c76debf
Imre Deak <imre.deak@intel.com>
2019-07-08 17:07:34 +0300

    drm/i915: Clear the shared PLL from the put_dplls() hook
    For symmetry with the get_dplls() hook which sets the shared_dpll
    pointer clear the same pointer from the put_dplls() hook.
    
    While at it also constify the old crtc state.
    
    v2:
    - Constify the old crtc state. (Ville)
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190708140735.20198-1-imre.deak@intel.com

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -13308,8 +13308,6 @@ static void intel_modeset_clear_plls(struct intel_atomic_state *state)
 		if (!needs_modeset(new_crtc_state))
 			continue;
 
-		new_crtc_state->shared_dpll = NULL;
-
 		intel_release_shared_dplls(state, crtc);
 	}
 }
