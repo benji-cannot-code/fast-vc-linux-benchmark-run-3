db7439f0bc95c9ae79a1db187ff29bd2ef086716
Phani Kiran Hemadri <phemadri@marvell.com>
2019-07-09 15:24:24 +0000

    crypto: cavium/nitrox - Add support for loading asymmetric crypto firmware
    This patch adds support to load Asymmetric crypto firmware on
    AE cores of CNN55XX device. Firmware is stored on UCD block 2
    and all available AE cores are tagged to group 0.
    
    Signed-off-by: Phani Kiran Hemadri <phemadri@marvell.com>
    Reviewed-by: Srikanth Jampala <jsrikanth@marvell.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/cavium/nitrox/nitrox_dev.h
+++ drivers/crypto/cavium/nitrox/nitrox_dev.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VERSION_LEN 32
 /* Maximum queues in PF mode */
 #define MAX_PF_QUEUES	64
+/* Maximum UCD Blocks */
+#define CNN55XX_MAX_UCD_BLOCKS	8
 
 /**
  * struct nitrox_cmdq - NITROX command queue
@@ -75,7 +77,7 @@ struct nitrox_cmdq {
  */
 struct nitrox_hw {
 	char partname[IFNAMSIZ * 2];
-	char fw_name[VERSION_LEN];
+	char fw_name[CNN55XX_MAX_UCD_BLOCKS][VERSION_LEN];
 
 	int freq;
 	u16 vendor_id;
