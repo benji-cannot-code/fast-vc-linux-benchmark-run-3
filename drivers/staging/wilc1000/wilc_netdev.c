0daafa7c2158a012b7d00c24921719e6c783350d
Nishka Dasgupta <nishkadg.linux@gmail.com>
2019-07-12 12:50:09 +0530

    staging: wilc1000: Replace function wilc_wlan_cfg_get_val()
    Include wilc_wlan_cfg.h in wilc_netdev.c to enable it to call functions
    in wilc_wlan_cfg.c.
    Remove function wilc_wlan_cfg_get_val as all it does is call
    wilc_wlan_cfg_get_wid_value in file wilc_wlan_cfg.c.
    Rename function wilc_wlan_cfg_get_wid_value to wilc_wlan_cfg_get_val to
    maintain compatibility with call sites (including in wilc_netdev.c).
    Issue found with Coccinelle.
    
    Signed-off-by: Nishka Dasgupta <nishkadg.linux@gmail.com>
    Link: https://lore.kernel.org/r/20190712072009.2550-1-nishkadg.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_netdev.c
+++ drivers/staging/wilc1000/wilc_netdev.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/inetdevice.h>
 
 #include "wilc_wfi_cfgoperations.h"
+#include "wilc_wlan_cfg.h"
 
 #define WILC_MULTICAST_TABLE_SIZE	8
 
