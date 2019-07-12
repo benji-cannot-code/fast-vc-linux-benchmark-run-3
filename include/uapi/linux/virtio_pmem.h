1c3796bf50f20a5997af925ffcdaacb860e1a93d
Pankaj Gupta <pagupta@redhat.com>
2019-07-12 10:46:10 +0530

    virtio_pmem: fix sparse warning
    This patch fixes below sparse warning related to __virtio
    type in virtio pmem driver. This is reported by Intel test
    bot on linux-next tree.
    
    nd_virtio.c:56:28: warning: incorrect type in assignment
                                    (different base types)
    nd_virtio.c:56:28:    expected unsigned int [unsigned] [usertype] type
    nd_virtio.c:56:28:    got restricted __virtio32
    nd_virtio.c:93:59: warning: incorrect type in argument 2
                                    (different base types)
    nd_virtio.c:93:59:    expected restricted __virtio32 [usertype] val
    nd_virtio.c:93:59:    got unsigned int [unsigned] [usertype] ret
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Pankaj Gupta <pagupta@redhat.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- include/uapi/linux/virtio_pmem.h
+++ include/uapi/linux/virtio_pmem.h
@@ -24,12 +24,12 @@ struct virtio_pmem_config {
 
 struct virtio_pmem_resp {
 	/* Host return status corresponding to flush request */
-	__u32 ret;
+	__le32 ret;
 };
 
 struct virtio_pmem_req {
 	/* command type */
-	__u32 type;
+	__le32 type;
 };
 
 #endif
