048149d888f11850894331b50b503c36d5c5280f
Al Viro <viro@zeniv.linux.org.uk>
2019-07-14 13:24:54 -0400

    kill LOOKUP_NO_EVAL, don't bother including namei.h from audit.h
    The former has no users left; the latter was only to get LOOKUP_...
    values to remapper in audit_inode() and that's an ex-parrot now.
    
    All places that use symbols from namei.h include it either directly
    or (in a few cases) via a local header, like fs/autofs/autofs_i.h
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/audit.h
+++ include/linux/audit.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/sched.h>
 #include <linux/ptrace.h>
-#include <linux/namei.h>  /* LOOKUP_* */
 #include <uapi/linux/audit.h>
 
 #define AUDIT_INO_UNSET ((unsigned long)-1)
