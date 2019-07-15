93090abbe6da598bc17fb3899cfa8b689e71b7fb
Darrick J. Wong <darrick.wong@oracle.com>
2019-07-15 08:51:00 -0700

    iomap: move the main iteration code into a separate file
    Move the main iteration code into a separate file so that we can group
    related functions in a single file instead of having a single enormous
    source file.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/iomap/apply.c
+++ fs/iomap/apply.c
@@ -8,23 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/fs.h>
 #include <linux/iomap.h>
-#include <linux/uaccess.h>
-#include <linux/gfp.h>
-#include <linux/migrate.h>
-#include <linux/mm.h>
-#include <linux/mm_inline.h>
-#include <linux/swap.h>
-#include <linux/pagemap.h>
-#include <linux/pagevec.h>
-#include <linux/file.h>
-#include <linux/uio.h>
-#include <linux/backing-dev.h>
-#include <linux/buffer_head.h>
-#include <linux/task_io_accounting_ops.h>
-#include <linux/dax.h>
-#include <linux/sched/signal.h>
 
-#include "internal.h"
+#include "../internal.h"
 
 /*
  * Execute a iomap write on a segment of the mapping that spans a
