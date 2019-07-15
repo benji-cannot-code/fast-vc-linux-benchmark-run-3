d12aa3c0b90e17da006ad91ace9334e893ee747a
Enrico Weigelt <info@metux.net>
2019-07-15 16:57:33 +0200

    leds: apu: add pr_fmt prefix for better log output
    Define pr_fmt() macro for prefixing the module name to all
    pr_*() outputs, in order to increase log message clarity.
    
    Signed-off-by: Enrico Weigelt <info@metux.net>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Jacek Anaszewski <jacek.anaszewski@gmail.com>

--- drivers/leds/leds-apu.c
+++ drivers/leds/leds-apu.c
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/dmi.h>
 #include <linux/err.h>
 #include <linux/init.h>
