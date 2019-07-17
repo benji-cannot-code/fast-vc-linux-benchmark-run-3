693ddd949f7fde362173844afdba08ee2bdf00db
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-07-17 15:17:58 +0900

    kbuild: remove the first line of *.mod files
    The current format of *.mod is like this:
    
      line 1: directory path to the .ko file
      line 2: a list of objects linked into this module
      line 3: unresolved symbols (only when CONFIG_TRIM_UNUSED_KSYMS=y)
    
    Now that *.mod and *.ko are created in the same directory, the line 1
    provides no valuable information. It can be derived by replacing the
    extension .mod with .ko. In fact, nobody uses the first line any more.
    
    Cut down the first line.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- scripts/adjust_autoksyms.sh
+++ scripts/adjust_autoksyms.sh
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #
 
 # Create/update the include/generated/autoksyms.h file from the list
-# of all module's needed symbols as recorded on the third line of *.mod files.
+# of all module's needed symbols as recorded on the second line of *.mod files.
 #
 # For each symbol being added or removed, the corresponding dependency
 # file's timestamp is updated to force a rebuild of the affected source
@@ -48,7 +48,7 @@ cat > "$new_ksyms_file" << EOT
 
 EOT
 sed 's/ko$/mod/' modules.order |
-xargs -n1 sed -n -e '3{s/ /\n/g;/^$/!p;}' -- |
+xargs -n1 sed -n -e '2{s/ /\n/g;/^$/!p;}' -- |
 sort -u |
 sed -e 's/\(.*\)/#define __KSYM_\1 1/' >> "$new_ksyms_file"
 
