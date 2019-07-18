a823fcf42e9648f6d3e7ebfee0ccb54d3447d0b1
Jani Nikula <jani.nikula@intel.com>
2019-07-18 18:14:57 +0200

    drm/panel: make drm_panel.h self-contained
    Fix build warning if drm_panel.h is built with CONFIG_OF=n or
    CONFIG_DRM_PANEL=n and included without the prerequisite err.h:
    
    ./include/drm/drm_panel.h: In function ‘of_drm_find_panel’:
    ./include/drm/drm_panel.h:203:9: error: implicit declaration of function ‘ERR_PTR’ [-Werror=implicit-function-declaration]
      return ERR_PTR(-ENODEV);
             ^~~~~~~
    ./include/drm/drm_panel.h:203:9: error: returning ‘int’ from a function with return type ‘struct drm_panel *’ makes pointer from integer without a cast [-Werror=int-conversion]
      return ERR_PTR(-ENODEV);
             ^~~~~~~~~~~~~~~~
    
    Fixes: 5fa8e4a22182 ("drm/panel: Make of_drm_find_panel() return an ERR_PTR() instead of NULL")
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Cc: Boris Brezillon <bbrezillon@kernel.org>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190718161507.2047-2-sam@ravnborg.org

--- include/drm/drm_panel.h
+++ include/drm/drm_panel.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DRM_PANEL_H__
 #define __DRM_PANEL_H__
 
+#include <linux/err.h>
 #include <linux/errno.h>
 #include <linux/list.h>
 
