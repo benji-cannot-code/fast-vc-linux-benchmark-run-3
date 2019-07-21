a0a6bd90bf1a55ab4bfb0e4944fee9ed692b4a74
Jiri Olsa <jolsa@kernel.org>
2019-07-21 13:24:24 +0200

    libperf: Add perf_evsel__init function
    Add the perf_evsel__init() function to initialize perf_evsel struct.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Alexey Budankov <alexey.budankov@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Michael Petlan <mpetlan@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/20190721112506.12306-38-jolsa@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/lib/evsel.c
+++ tools/perf/lib/evsel.c
@@ -3,3 +3,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <perf/evsel.h>
 #include <linux/list.h>
 #include <internal/evsel.h>
+
+void perf_evsel__init(struct perf_evsel *evsel)
+{
+	INIT_LIST_HEAD(&evsel->node);
+}
--- tools/perf/lib/include/perf/evsel.h
+++ tools/perf/lib/include/perf/evsel.h
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LIBPERF_EVSEL_H
 #define __LIBPERF_EVSEL_H
 
+#include <perf/core.h>
+
 struct perf_evsel;
+
+LIBPERF_API void perf_evsel__init(struct perf_evsel *evsel);
 
 #endif /* __LIBPERF_EVSEL_H */
