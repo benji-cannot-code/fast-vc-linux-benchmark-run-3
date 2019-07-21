101cad02f9b571687b41966a276817f9e6cd8f01
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2019-07-21 15:04:23 -0400

    media: ov2680: fix a typo in a function name
    All functions in this file starts with 'ov2680_', except ov2860_parse_dt().
    
    This is likely a typo.
    rename it to 'ov2680_parse_dt()' (6 and 8)
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/i2c/ov2680.c
+++ drivers/media/i2c/ov2680.c
@@ -1021,7 +1021,7 @@ static int ov2680_check_id(struct ov2680_dev *sensor)
 	return 0;
 }
 
-static int ov2860_parse_dt(struct ov2680_dev *sensor)
+static int ov2680_parse_dt(struct ov2680_dev *sensor)
 {
 	struct device *dev = ov2680_to_dev(sensor);
 	int ret;
@@ -1062,7 +1062,7 @@ static int ov2680_probe(struct i2c_client *client)
 
 	sensor->i2c_client = client;
 
-	ret = ov2860_parse_dt(sensor);
+	ret = ov2680_parse_dt(sensor);
 	if (ret < 0)
 		return -EINVAL;
 
