1049a6f6928a97c58354904228c21e88d23cfd1e
Stephen Rothwell <sfr@canb.auug.org.au>
2019-07-23 10:07:40 +1000

    media: uapi: new file needs types.h
    Today's linux-next build (x86_64 allmodconfig) failed like this:
    
    	include/media/vp8-ctrls.h:25:2: error: unknown type name '__s8'
    	  __s8 quant_update[4];
    	  ^~~~
    	...
    	include/media/vp8-ctrls.h:107:2: error: unknown type name '__u64'
    	  __u64 flags;
    	  ^~~~~
    
    Caused by commit a57d6acaf352 ("media: uapi: Add VP8 stateless decoder API")
    
    Fixes: a57d6acaf352 ("media: uapi: Add VP8 stateless decoder API")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- include/media/vp8-ctrls.h
+++ include/media/vp8-ctrls.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _VP8_CTRLS_H_
 #define _VP8_CTRLS_H_
 
+#include <linux/types.h>
+
 #define V4L2_PIX_FMT_VP8_FRAME v4l2_fourcc('V', 'P', '8', 'F')
 
 #define V4L2_CID_MPEG_VIDEO_VP8_FRAME_HEADER (V4L2_CID_MPEG_BASE + 2000)
