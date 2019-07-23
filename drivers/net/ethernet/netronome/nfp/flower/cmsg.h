4edcb53b50f303752ddf3d762c92cb222613f436
John Hurley <john.hurley@netronome.com>
2019-07-23 15:34:00 +0100

    nfp: flower: offload MPLS push action
    Recent additions to the kernel include a TC action module to manipulate
    MPLS headers on packets. Such actions are available to offload via the
    flow_offload intermediate representation API.
    
    Modify the NFP driver to allow the offload of MPLS push actions to
    firmware.
    
    Signed-off-by: John Hurley <john.hurley@netronome.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/netronome/nfp/flower/cmsg.h
+++ drivers/net/ethernet/netronome/nfp/flower/cmsg.h
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NFP_FL_ACTION_OPCODE_OUTPUT		0
 #define NFP_FL_ACTION_OPCODE_PUSH_VLAN		1
 #define NFP_FL_ACTION_OPCODE_POP_VLAN		2
+#define NFP_FL_ACTION_OPCODE_PUSH_MPLS		3
 #define NFP_FL_ACTION_OPCODE_SET_IPV4_TUNNEL	6
 #define NFP_FL_ACTION_OPCODE_SET_ETHERNET	7
 #define NFP_FL_ACTION_OPCODE_SET_IPV4_ADDRS	9
@@ -231,6 +232,12 @@ struct nfp_fl_push_geneve {
 	u8 type;
 	u8 length;
 	u8 opt_data[];
+};
+
+struct nfp_fl_push_mpls {
+	struct nfp_fl_act_head head;
+	__be16 ethtype;
+	__be32 lse;
 };
 
 /* Metadata with L2 (1W/4B)
