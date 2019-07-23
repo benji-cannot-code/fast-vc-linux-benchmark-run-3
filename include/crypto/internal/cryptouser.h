84668d78f02c141eb9d12a504d2e2385bc02bd8c
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-07-23 20:43:44 +0900

    crypto: user - fix potential warnings in cryptouser.h
    Function definitions in headers are usually marked as 'static inline'.
    
    Since 'inline' is missing for crypto_reportstat(), if it were not
    referenced from a .c file that includes this header, it would produce
    a warning.
    
    Also, 'struct crypto_user_alg' is not declared in this header.
    
    I included <linux/crytouser.h> instead of adding the forward declaration
    as suggested [1].
    
    Detected by compile-testing this header as a standalone unit:
    
    ./include/crypto/internal/cryptouser.h:6:44: warning: ‘struct crypto_user_alg’ declared inside parameter list will not be visible outside of this definition or declaration
     struct crypto_alg *crypto_alg_match(struct crypto_user_alg *p, int exact);
                                                ^~~~~~~~~~~~~~~
    ./include/crypto/internal/cryptouser.h:11:12: warning: ‘crypto_reportstat’ defined but not used [-Wunused-function]
     static int crypto_reportstat(struct sk_buff *in_skb, struct nlmsghdr *in_nlh, struct nlattr **attrs)
                ^~~~~~~~~~~~~~~~~
    
    [1] https://lkml.org/lkml/2019/6/13/1121
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/internal/cryptouser.h
+++ include/crypto/internal/cryptouser.h
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/cryptouser.h>
 #include <net/netlink.h>
 
 struct crypto_alg *crypto_alg_match(struct crypto_user_alg *p, int exact);
@@ -7,7 +8,9 @@ struct crypto_alg *crypto_alg_match(struct crypto_user_alg *p, int exact);
 #ifdef CONFIG_CRYPTO_STATS
 int crypto_reportstat(struct sk_buff *in_skb, struct nlmsghdr *in_nlh, struct nlattr **attrs);
 #else
-static int crypto_reportstat(struct sk_buff *in_skb, struct nlmsghdr *in_nlh, struct nlattr **attrs)
+static inline int crypto_reportstat(struct sk_buff *in_skb,
+				    struct nlmsghdr *in_nlh,
+				    struct nlattr **attrs)
 {
 	return -ENOTSUPP;
 }
