1bb97a6cec2af5f1e594afa2f72327e6a6a3b6a9
Christoph Hellwig <hch@lst.de>
2019-07-24 14:07:28 +0200

    arm64: use asm-generic/dma-mapping.h
    Now that the Xen special cases are gone nothing worth mentioning is
    left in the arm64 <asm/dma-mapping.h> file, so switch to use the
    asm-generic version instead.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Will Deacon <will@kernel.org>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cache.h>
 #include <linux/dma-noncoherent.h>
 #include <linux/dma-iommu.h>
+#include <xen/xen.h>
 #include <xen/swiotlb-xen.h>
 
 #include <asm/cacheflush.h>
