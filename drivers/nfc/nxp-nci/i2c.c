c885db8307c0592f2821ccc9179090cdaeca56dc
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-07-29 16:35:11 +0300

    NFC: nxp-nci: Remove unused macro pr_fmt()
    The macro had never been used.
    
    The driver uses mostly the nfc_err(), which, with other macros in the family,
    is backed by corresponding dev_err(). pr_fmt() is not used for dev_err()
    macro. Moreover, there is no need to print the module name which is part of the
    device instance name anyway.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Tested-by: Sedat Dilek <sedat.dilek@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/nfc/nxp-nci/i2c.c
+++ drivers/nfc/nxp-nci/i2c.c
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2012  Intel Corporation. All rights reserved.
  */
 
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
 #include <linux/acpi.h>
 #include <linux/delay.h>
 #include <linux/i2c.h>
