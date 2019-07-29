7c0c26748d35445216f532da8ee651fcfce4a688
Evan Quan <evan.quan@amd.com>
2019-07-29 12:43:28 -0500

    drm/amd/powerplay: update arcturus_ppt.c/h V3
    Arcturus ASIC specific powerplay interfaces.
    
    V2: correct SMU msg naming
        drop unnecessary debugs
    
    V3: rebase (Alex)
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Kevin Wang <kevin1.wang@amd.com>
    Reviewed-by: Le Ma <Le.Ma@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/inc/smu_types.h
+++ drivers/gpu/drm/amd/powerplay/inc/smu_types.h
@@ -122,7 +122,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
        __SMU_DUMMY_MAP(GetVoltageByDpm),              \
        __SMU_DUMMY_MAP(GetVoltageByDpmOverdrive),     \
        __SMU_DUMMY_MAP(PowerUpVcn0),                  \
-       __SMU_DUMMY_MAP(PowerDownVcn01),               \
+       __SMU_DUMMY_MAP(PowerDownVcn0),               \
        __SMU_DUMMY_MAP(PowerUpVcn1),                  \
        __SMU_DUMMY_MAP(PowerDownVcn1),                \
 
