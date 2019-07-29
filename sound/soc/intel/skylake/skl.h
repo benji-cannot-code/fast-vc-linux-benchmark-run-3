95888f49621eb3c4455a8447d2a0c2614e423b0b
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-07-29 10:51:47 -0500

    ASoC: Intel: Skylake: move NHLT header to common directory
    Prepare move from NHLT code to common directory, starting with header.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/soc/intel/skylake/skl.h
+++ sound/soc/intel/skylake/skl.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/hdaudio_ext.h>
 #include <sound/hda_codec.h>
 #include <sound/soc.h>
-#include "skl-nhlt.h"
 #include "skl-ssp-clk.h"
 
 #define SKL_SUSPEND_DELAY 2000
