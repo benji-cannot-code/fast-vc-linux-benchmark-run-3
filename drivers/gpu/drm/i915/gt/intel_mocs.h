67c2db63c7eb80e337b204325893ed6b6b2bf702
Lucas De Marchi <lucas.demarchi@intel.com>
2019-07-30 11:26:14 -0700

    drm/i915: remove dangling forward declaration
    Commit 20a7f2fc4d7a ("drm/i915: Convert intel_mocs_init_l3cc_table to
    intel_gt") removed the only user.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190730182614.14379-1-lucas.demarchi@intel.com

--- drivers/gpu/drm/i915/gt/intel_mocs.h
+++ drivers/gpu/drm/i915/gt/intel_mocs.h
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * context handling keep the MOCS in step.
  */
 
-struct drm_i915_private;
 struct i915_request;
 struct intel_engine_cs;
 struct intel_gt;
