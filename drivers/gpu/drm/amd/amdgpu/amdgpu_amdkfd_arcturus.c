f1ed514fd199794189f9cf3b876e1995afdb7c04
Alex Deucher <alexander.deucher@amd.com>
2019-07-31 10:26:39 -0500

    drm/amdgpu: drop drmP.h in amdgpu_amdkfd_arcturus.c
    Unused.
    
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_arcturus.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_arcturus.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/mmu_context.h>
 #include <linux/firmware.h>
-#include <drm/drmP.h>
 #include "amdgpu.h"
 #include "amdgpu_amdkfd.h"
 #include "sdma0/sdma0_4_2_2_offset.h"
