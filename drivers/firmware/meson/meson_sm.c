bc8a7d2e72caccddd31c90bffe79303a76415a72
Carlo Caione <ccaione@baylibre.com>
2019-07-31 09:23:36 +0100

    firmware: meson_sm: Mark chip struct as static const
    No need to be a global struct.
    
    Reviewed-by: Jerome Brunet <jbrunet@baylibre.com>
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>

--- drivers/firmware/meson/meson_sm.c
+++ drivers/firmware/meson/meson_sm.c
@@ -36,7 +36,7 @@ struct meson_sm_chip {
 	struct meson_sm_cmd cmd[];
 };
 
-struct meson_sm_chip gxbb_chip = {
+static const struct meson_sm_chip gxbb_chip = {
 	.shmem_size		= SZ_4K,
 	.cmd_shmem_in_base	= 0x82000020,
 	.cmd_shmem_out_base	= 0x82000021,
