6513fb3fd29c879a5f9e932faada6321520eace5
Michal Wajdeczko <michal.wajdeczko@intel.com>
2019-07-31 22:33:20 +0000

    drm/i915/guc: Use dedicated flag to track submission mode
    Instead of relying on enable_guc modparam to represent actual
    GuC submission mode, use dedicated flag and look at modparam
    only to check if submission was explicitly disabled by the user.
    
    v2: rebased, simplified condition (Chris)
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190731223321.36436-4-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/gt/uc/intel_guc.h
+++ drivers/gpu/drm/i915/gt/uc/intel_guc.h
@@ -62,6 +62,8 @@ struct intel_guc {
 		void (*disable)(struct intel_guc *guc);
 	} interrupts;
 
+	bool submission_supported;
+
 	struct i915_vma *ads_vma;
 	struct __guc_ads_blob *ads_blob;
 
@@ -189,6 +191,11 @@ static inline int intel_guc_sanitize(struct intel_guc *guc)
 	guc->mmio_msg = 0;
 
 	return 0;
+}
+
+static inline bool intel_guc_is_submission_supported(struct intel_guc *guc)
+{
+	return guc->submission_supported;
 }
 
 static inline void intel_guc_enable_msg(struct intel_guc *guc, u32 mask)
