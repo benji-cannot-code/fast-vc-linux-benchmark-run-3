cd8ba208b111083370b63c8e600ea68d96f00edd
Gao Xiang <gaoxiang25@huawei.com>
2019-07-31 23:57:37 +0800

    staging: erofs: remove redundant #include "internal.h"
    Because #include "internal.h" is included in xattr.h
    
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Link: https://lore.kernel.org/r/20190731155752.210602-8-gaoxiang25@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/erofs/namei.c
+++ drivers/staging/erofs/namei.c
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *             http://www.huawei.com/
  * Created by Gao Xiang <gaoxiang25@huawei.com>
  */
-#include "internal.h"
 #include "xattr.h"
 
 #include <trace/events/erofs.h>
