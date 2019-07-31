6dc614de2f7246db519473e3e8bdf8dcebc6f8eb
Michal Wajdeczko <michal.wajdeczko@intel.com>
2019-07-31 22:33:19 +0000

    drm/i915/uc: Consider enable_guc modparam during fw selection
    We can use value of enable_guc modparam during firmware path selection
    and start using firmware status to see if GuC/HuC is being used.
    This is first step to make enable_guc modparam read-only.
    
    v2: rebased, don't care about <0 (Chris)
    v3: oops
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190731223321.36436-3-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/gt/uc/intel_uc.h
+++ drivers/gpu/drm/i915/gt/uc/intel_uc.h
@@ -52,8 +52,7 @@ int intel_uc_runtime_resume(struct intel_uc *uc);
 
 static inline bool intel_uc_supports_guc(struct intel_uc *uc)
 {
-	GEM_BUG_ON(i915_modparams.enable_guc < 0);
-	return i915_modparams.enable_guc > 0;
+	return intel_guc_is_supported(&uc->guc);
 }
 
 static inline bool intel_uc_supports_guc_submission(struct intel_uc *uc)
@@ -64,8 +63,7 @@ static inline bool intel_uc_supports_guc_submission(struct intel_uc *uc)
 
 static inline bool intel_uc_supports_huc(struct intel_uc *uc)
 {
-	GEM_BUG_ON(i915_modparams.enable_guc < 0);
-	return i915_modparams.enable_guc & ENABLE_GUC_LOAD_HUC;
+	return intel_huc_is_supported(&uc->huc);
 }
 
 #endif
