8381d2f3e6f41409a2b1cd74d9b0336be6b964c9
Christoph Hellwig <hch@lst.de>
2019-08-01 17:14:14 +0300

    arm-nommu: remove the unused pgprot_dmacoherent define
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- arch/arm/include/asm/pgtable-nommu.h
+++ arch/arm/include/asm/pgtable-nommu.h
@@ -63,7 +63,6 @@ typedef pte_t *pte_addr_t;
  */
 #define pgprot_noncached(prot)	(prot)
 #define pgprot_writecombine(prot) (prot)
-#define pgprot_dmacoherent(prot) (prot)
 #define pgprot_device(prot)	(prot)
 
 
