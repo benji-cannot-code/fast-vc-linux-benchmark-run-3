ff54c44ec0cc0c084e277ca8336fa08f6e173ff7
David Sterba <dsterba@suse.com>
2019-08-01 17:34:41 +0200

    btrfs: move sysfs declarations out of ctree.h
    As the header for sysfs code already exists, use it to clean up ctree.h.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "free-space-cache.h"
 #include "backref.h"
 #include "space-info.h"
+#include "sysfs.h"
 #include "tests/btrfs-tests.h"
 #include "block-group.h"
 
