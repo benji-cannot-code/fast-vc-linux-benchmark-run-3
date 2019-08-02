7bc1994671616bbd832d6e175440659424df1afa
Nathan Lynch <nathanl@linux.ibm.com>
2019-08-02 14:29:25 -0500

    powerpc/rtas: allow rescheduling while changing cpu states
    rtas_cpu_state_change_mask() potentially operates on scores of cpus,
    so explicitly allow rescheduling in the loop body.
    
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Reviewed-by: Gautham R. Shenoy <ego@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190802192926.19277-3-nathanl@linux.ibm.com

--- arch/powerpc/kernel/rtas.c
+++ arch/powerpc/kernel/rtas.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/capability.h>
 #include <linux/delay.h>
 #include <linux/cpu.h>
+#include <linux/sched.h>
 #include <linux/smp.h>
 #include <linux/completion.h>
 #include <linux/cpumask.h>
@@ -899,6 +900,7 @@ static int rtas_cpu_state_change_mask(enum rtas_cpu_state state,
 				cpumask_clear_cpu(cpu, cpus);
 			}
 		}
+		cond_resched();
 	}
 
 	return ret;
