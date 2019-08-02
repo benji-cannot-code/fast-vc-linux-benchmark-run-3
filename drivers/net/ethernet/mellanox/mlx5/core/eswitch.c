1ccefe4da7c3dc1f63980f229acbfae84fd582c8
Vlad Buslov <vladbu@mellanox.com>
2019-08-02 22:21:56 +0300

    net/mlx5e: Protect encap hash table with mutex
    To remove dependency on rtnl lock, protect encap hash table from concurrent
    modifications with new "encap_tbl_lock" mutex. Use the mutex to protect
    internal encap entry state from concurrent modification. This is necessary
    because a flow can be attached to multiple encap entries simultaneously,
    which significantly complicates using finer grained per-entry lock.
    
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -2000,6 +2000,7 @@ int mlx5_eswitch_init(struct mlx5_core_dev *dev)
 	if (err)
 		goto abort;
 
+	mutex_init(&esw->offloads.encap_tbl_lock);
 	hash_init(esw->offloads.encap_tbl);
 	mutex_init(&esw->offloads.mod_hdr.lock);
 	hash_init(esw->offloads.mod_hdr.hlist);
@@ -2040,6 +2041,7 @@ void mlx5_eswitch_cleanup(struct mlx5_eswitch *esw)
 	destroy_workqueue(esw->work_queue);
 	esw_offloads_cleanup_reps(esw);
 	mutex_destroy(&esw->offloads.mod_hdr.lock);
+	mutex_destroy(&esw->offloads.encap_tbl_lock);
 	kfree(esw->vports);
 	kfree(esw);
 }
