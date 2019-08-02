3d09aac35c0cd0f3539184921e2d624aeed94f1b
Nishka Dasgupta <nishkadg.linux@gmail.com>
2019-08-02 12:12:06 +0530

    staging: rtl8712: r8712_init_recv_priv(): Change return type
    Change return type of r8712_init_recv_priv from int to void as its
    return value is never used. Remove return statements accordingly.
    
    Signed-off-by: Nishka Dasgupta <nishkadg.linux@gmail.com>
    Link: https://lore.kernel.org/r/20190802064212.30476-3-nishkadg.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/rtl8712_recv.c
+++ drivers/staging/rtl8712/rtl8712_recv.c
@@ -36,7 +36,7 @@ static u8 rfc1042_header[] = {0xaa, 0xaa, 0x03, 0x00, 0x00, 0x00};
 
 static void recv_tasklet(void *priv);
 
-int r8712_init_recv_priv(struct recv_priv *precvpriv, struct _adapter *padapter)
+void r8712_init_recv_priv(struct recv_priv *precvpriv, struct _adapter *padapter)
 {
 	int i;
 	struct recv_buf *precvbuf;
@@ -84,7 +84,6 @@ int r8712_init_recv_priv(struct recv_priv *precvpriv, struct _adapter *padapter)
 		}
 		pskb = NULL;
 	}
-	return res;
 }
 
 void r8712_free_recv_priv(struct recv_priv *precvpriv)
