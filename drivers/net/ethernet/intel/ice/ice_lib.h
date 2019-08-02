f866b48617ba43b3747568c0f7d89b3c3ff2fb7c
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2019-08-02 01:25:19 -0700

    ice: add support for enabling/disabling single queues
    Refactor the queue handling functions that are going through queue
    arrays in a way that the logic done for a single queue is pulled out and
    it will be called for each ring when traversing ring array. This implies
    that when disabling Tx rings we won't fill up q_ids, q_teids and
    q_handles arrays.  Drop also 'offset' parameter; the value from vsi's
    txq_map is stored in ring->reg_idx and that drops the need for mentioned
    parameter. Introduce the ice_vsi_cfg_txq, ice_vsi_stop_tx_ring and
    ice_vsi_ctrl_rx_ring that are the functions with pulled out logic.
    
    There's several Tx queue meta data (q_id, q_handle, q_teid and other)
    that need to be set up during Tx queue disablement, so let's as well add
    a helper structure that wraps it up and a function that will be filling
    it up.
    
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ice/ice_lib.h
+++ drivers/net/ethernet/intel/ice/ice_lib.h
@@ -7,8 +7,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "ice.h"
 
-int ice_add_mac_to_list(struct ice_vsi *vsi, struct list_head *add_list,
-			const u8 *macaddr);
+struct ice_txq_meta {
+	/* Tx-scheduler element identifier */
+	u32 q_teid;
+	/* Entry in VSI's txq_map bitmap */
+	u16 q_id;
+	/* Relative index of Tx queue within TC */
+	u16 q_handle;
+	/* VSI index that Tx queue belongs to */
+	u16 vsi_idx;
+	/* TC number that Tx queue belongs to */
+	u8 tc;
+};
+
+int
+ice_add_mac_to_list(struct ice_vsi *vsi, struct list_head *add_list,
+		    const u8 *macaddr);
 
 void ice_free_fltr_list(struct device *dev, struct list_head *h);
 
