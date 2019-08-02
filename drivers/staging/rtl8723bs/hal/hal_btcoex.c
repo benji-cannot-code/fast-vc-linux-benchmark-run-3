28bace22f41711757c9ddb66d516d9d8aa93d78b
Nishka Dasgupta <nishkadg.linux@gmail.com>
2019-08-02 11:54:43 +0530

    staging: rtl8723bs: Change return type of hal_btcoex_IsLpsOn()
    Change return type of hal_btcoex_IsLpsOn from u8 to bool as the
    function only returns boolean values.
    Modify a comparison of a boolean value to false to instead check that
    boolean value directly.
    
    Signed-off-by: Nishka Dasgupta <nishkadg.linux@gmail.com>
    Link: https://lore.kernel.org/r/20190802062444.30384-3-nishkadg.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/hal_btcoex.c
+++ drivers/staging/rtl8723bs/hal/hal_btcoex.c
@@ -1490,9 +1490,9 @@ bool hal_btcoex_IsBtControlLps(struct adapter *padapter)
 	return false;
 }
 
-u8 hal_btcoex_IsLpsOn(struct adapter *padapter)
+bool hal_btcoex_IsLpsOn(struct adapter *padapter)
 {
-	if (hal_btcoex_IsBtExist(padapter) == false)
+	if (!hal_btcoex_IsBtExist(padapter))
 		return false;
 
 	if (GLBtCoexist.btInfo.bBtDisabled)
