31abfb7d9c0faf57c2387b257622dd5bb8ef8325
Eric Biggers <ebiggers@google.com>
2019-08-04 19:35:43 -0700

    fs, fscrypt: move uapi definitions to new header <linux/fscrypt.h>
    More fscrypt definitions are being added, and we shouldn't use a
    disproportionate amount of space in <linux/fs.h> for fscrypt stuff.
    So move the fscrypt definitions to a new header <linux/fscrypt.h>.
    
    For source compatibility with existing userspace programs, <linux/fs.h>
    still includes the new header.
    
    Reviewed-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/mm.h>
 #include <linux/slab.h>
+#include <uapi/linux/fscrypt.h>
 
 #define FS_CRYPTO_BLOCK_SIZE		16
 
