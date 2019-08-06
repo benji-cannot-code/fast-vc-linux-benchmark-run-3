cb742e3830c3cee2db2bc2af30cac9702a643d41
Bard liao <yung-chuan.liao@linux.intel.com>
2019-08-05 19:55:12 -0500

    soundwire: include mod_devicetable.h to avoid compiling warnings
    When integrating SoundWire, kbuild throws this warning with randconfig:
    
    >> include/linux/soundwire/sdw.h:571:17: warning: 'struct
       sdw_device_id' declared inside parameter list will not be visible
       outside of this definition or declaration
    
           const struct sdw_device_id *id);
                        ^~~~~~~~~~~~~
    
    Fix by adding the relevant include
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Bard liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20190806005522.22642-8-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- include/linux/soundwire/sdw.h
+++ include/linux/soundwire/sdw.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SOUNDWIRE_H
 #define __SOUNDWIRE_H
 
+#include <linux/mod_devicetable.h>
+
 struct sdw_bus;
 struct sdw_slave;
 
