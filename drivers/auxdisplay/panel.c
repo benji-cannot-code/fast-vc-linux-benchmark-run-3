7535041befe896ca79f24ed92da5843f2bdc4357
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-08-06 16:14:44 +0900

    auxdisplay: charlcd: move charlcd.h to drivers/auxdisplay
    This header is included in drivers/auxdisplay/. Make it a local header.
    
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>

--- drivers/auxdisplay/panel.c
+++ drivers/auxdisplay/panel.c
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/uaccess.h>
 
-#include <misc/charlcd.h>
+#include "charlcd.h"
 
 #define KEYPAD_MINOR		185
 
