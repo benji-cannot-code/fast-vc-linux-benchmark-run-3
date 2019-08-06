5194e726a43460fd583caf7923c06b3f18b48366
Ido Schimmel <idosch@mellanox.com>
2019-08-06 16:19:54 +0300

    drop_monitor: Avoid multiple blank lines
    Remove multiple blank lines which are visually annoying and useless.
    
    This suppresses the "Please don't use multiple blank lines" checkpatch
    messages.
    
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/drop_monitor.c
+++ net/core/drop_monitor.c
@@ -301,7 +301,6 @@ out_unlock:
 	return rc;
 }
 
-
 static int net_dm_cmd_config(struct sk_buff *skb,
 			struct genl_info *info)
 {
@@ -427,7 +426,6 @@ static int __init init_net_drop_monitor(void)
 		spin_lock_init(&data->lock);
 		reset_per_cpu_data(data);
 	}
-
 
 	goto out;
 
