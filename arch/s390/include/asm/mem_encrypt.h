028a091f5ddbec629614658e964f3bdfb009fbbf
Thiago Jung Bauermann <bauerman@linux.ibm.com>
2019-08-06 01:49:19 -0300

    s390/mm: Remove sev_active() function
    All references to sev_active() were moved to arch/x86 so we don't need to
    define it for s390 anymore.
    
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Halil Pasic <pasic@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190806044919.10622-7-bauerman@linux.ibm.com

--- arch/s390/include/asm/mem_encrypt.h
+++ arch/s390/include/asm/mem_encrypt.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 
 static inline bool mem_encrypt_active(void) { return false; }
-extern bool sev_active(void);
 
 int set_memory_encrypted(unsigned long addr, int numpages);
 int set_memory_decrypted(unsigned long addr, int numpages);
