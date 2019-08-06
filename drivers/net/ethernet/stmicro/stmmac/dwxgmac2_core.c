0cd66b003388ffe0dc98a559b50d1d3a18e3a6f9
Jose Abreu <Jose.Abreu@synopsys.com>
2019-08-06 15:16:17 +0200

    net: stmmac: Fix issues when number of Queues >= 4
    When queues >= 4 we use different registers but we were not subtracting
    the offset of 4. Fix this.
    
    Found out by Coverity.
    
    Signed-off-by: Jose Abreu <joabreu@synopsys.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwxgmac2_core.c
+++ drivers/net/ethernet/stmicro/stmmac/dwxgmac2_core.c
@@ -109,6 +109,8 @@ static void dwxgmac2_rx_queue_prio(struct mac_device_info *hw, u32 prio,
 	u32 value, reg;
 
 	reg = (queue < 4) ? XGMAC_RXQ_CTRL2 : XGMAC_RXQ_CTRL3;
+	if (queue >= 4)
+		queue -= 4;
 
 	value = readl(ioaddr + reg);
 	value &= ~XGMAC_PSRQ(queue);
@@ -172,6 +174,8 @@ static void dwxgmac2_map_mtl_to_dma(struct mac_device_info *hw, u32 queue,
 	u32 value, reg;
 
 	reg = (queue < 4) ? XGMAC_MTL_RXQ_DMA_MAP0 : XGMAC_MTL_RXQ_DMA_MAP1;
+	if (queue >= 4)
+		queue -= 4;
 
 	value = readl(ioaddr + reg);
 	value &= ~XGMAC_QxMDMACH(queue);
