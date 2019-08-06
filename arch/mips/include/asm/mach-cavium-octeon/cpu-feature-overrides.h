1cc473805721e81ba3e2cb480602adc34936e9b2
Paul Burton <paul.burton@mips.com>
2019-08-06 12:52:31 -0700

    MIPS: octeon: Remove ARCH_HAS_IRQ_PER_CPU
    ARCH_HAS_IRQ_PER_CPU is unused anywhere in the kernel - remove the
    definition.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>

--- arch/mips/include/asm/mach-cavium-octeon/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-cavium-octeon/cpu-feature-overrides.h
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define cpu_has_rixi		(cpu_data[0].cputype != CPU_CAVIUM_OCTEON)
 
-#define ARCH_HAS_IRQ_PER_CPU	1
 #define ARCH_HAS_SPINLOCK_PREFETCH 1
 #define spin_lock_prefetch(x) prefetch(x)
 #define PREFETCH_STRIDE 128
