4173a635c4844980925d17a7ed4c8cbed7939b5a
Michal Wajdeczko <michal.wajdeczko@intel.com>
2019-08-07 17:00:29 +0000

    drm/i915/uc: HuC firmware can't be supported without GuC
    There is no point in selecting HuC firmware if GuC is unsupported
    or it was already disabled, as we need GuC to authenticate HuC.
    
    While around, make uc_fw_init_early work without direct access
    to whole i915, pass only needed platform/rev info.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190807170034.8440-3-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/gt/uc/intel_guc_fw.c
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_fw.c
@@ -40,7 +40,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 void intel_guc_fw_init_early(struct intel_guc *guc)
 {
-	intel_uc_fw_init_early(&guc->fw, INTEL_UC_FW_TYPE_GUC, guc_to_gt(guc)->i915);
+	struct drm_i915_private *i915 = guc_to_gt(guc)->i915;
+
+	intel_uc_fw_init_early(&guc->fw, INTEL_UC_FW_TYPE_GUC, HAS_GT_UC(i915),
+			       INTEL_INFO(i915)->platform, INTEL_REVID(i915));
 }
 
 static void guc_prepare_xfer(struct intel_uncore *uncore)
