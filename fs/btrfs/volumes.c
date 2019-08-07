d3cd50b20237d78485db227e69b231821a867b79
Anand Jain <anand.jain@oracle.com>
2019-08-07 16:21:19 +0800

    btrfs: reset device stat using btrfs_dev_stat_set
    btrfs_dev_stat_reset() is an overdo in terms of wrapping. So this patch
    open codes btrfs_dev_stat_reset().
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -7304,7 +7304,7 @@ static void __btrfs_reset_dev_stats(struct btrfs_device *dev)
 	int i;
 
 	for (i = 0; i < BTRFS_DEV_STAT_VALUES_MAX; i++)
-		btrfs_dev_stat_reset(dev, i);
+		btrfs_dev_stat_set(dev, i, 0);
 }
 
 int btrfs_init_dev_stats(struct btrfs_fs_info *fs_info)
@@ -7354,7 +7354,7 @@ int btrfs_init_dev_stats(struct btrfs_fs_info *fs_info)
 				btrfs_dev_stat_set(device, i,
 					btrfs_dev_stats_value(eb, ptr, i));
 			else
-				btrfs_dev_stat_reset(device, i);
+				btrfs_dev_stat_set(device, i, 0);
 		}
 
 		device->dev_stats_valid = 1;
@@ -7537,7 +7537,7 @@ int btrfs_get_dev_stats(struct btrfs_fs_info *fs_info,
 				stats->values[i] =
 					btrfs_dev_stat_read_and_reset(dev, i);
 			else
-				btrfs_dev_stat_reset(dev, i);
+				btrfs_dev_stat_set(dev, i, 0);
 		}
 	} else {
 		for (i = 0; i < BTRFS_DEV_STAT_VALUES_MAX; i++)
