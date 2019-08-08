b904263ceee26b1eea2f7eb8f153bcb20886764c
Jani Nikula <jani.nikula@intel.com>
2019-08-08 16:42:48 +0300

    drm/i915: extract gem/i915_gem_stolen.h from i915_drv.h
    It used to be handy that we only had a couple of headers, but over time
    i915_drv.h has become unwieldy. Extract declarations to a separate
    header file corresponding to the implementation module, clarifying the
    modularity of the driver.
    
    Ensure the new header is self-contained, and do so with minimal further
    includes, using forward declarations as needed. Include the new header
    from i915_drv.h to avoid sprinkling includes all over the place; this
    can be changed as a follow-up if necessary.
    
    No functional changes.
    
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/0141b4e1f1bf2deb65730ce6973863a3a16ab38f.1565271681.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/gem/i915_gem_stolen.c
+++ drivers/gpu/drm/i915/gem/i915_gem_stolen.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/i915_drm.h>
 
 #include "i915_drv.h"
+#include "i915_gem_stolen.h"
 
 /*
  * The BIOS typically reserves some of the system's memory for the exclusive
--- drivers/gpu/drm/i915/gt/intel_gt.c
+++ drivers/gpu/drm/i915/gt/intel_gt.c
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "i915_drv.h"
-
 #include "intel_gt.h"
 #include "intel_gt_pm.h"
 #include "intel_uncore.h"
