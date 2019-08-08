f84827dfc0002288cc4b1fd8b7c278c6655e9b11
Linus Walleij <linus.walleij@linaro.org>
2019-08-08 11:03:59 -0700

    Input: bu21013_ts - convert to use GPIO descriptors
    This driver can use GPIO descriptors rather than GPIO numbers
    without any problems, convert it. Name the field variables after
    the actual pins on the chip rather than the "reset" and "touch"
    names from the devicetree bindings that are vaguely inaccurate.
    
    No in-tree users pass GPIO numbers in platform data so drop
    this. Descriptor tables can be used to get these GPIOs from a board
    file if need be.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/linux/input/bu21013.h
+++ include/linux/input/bu21013.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * struct bu21013_platform_device - Handle the platform data
  * @touch_x_max: touch x max
  * @touch_y_max: touch y max
- * @cs_pin: chip select pin
- * @touch_pin: touch gpio pin
  * @ext_clk: external clock flag
  * @x_flip: x flip flag
  * @y_flip: y flip flag
@@ -24,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct bu21013_platform_device {
 	int touch_x_max;
 	int touch_y_max;
-	unsigned int cs_pin;
-	unsigned int touch_pin;
 	bool ext_clk;
 	bool x_flip;
 	bool y_flip;
