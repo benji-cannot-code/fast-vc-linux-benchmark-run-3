b377dcfa58869ac5da471accab22f0729efaf585
Kai-Heng Feng <kai.heng.feng@canonical.com>
2019-08-10 03:01:29 +0800

    serial: 8250_pci: Add support for Sunix serial boards
    Add support to Sunix serial boards with up to 16 ports.
    
    Sunix board need its own setup callback instead of using Timedia's, to
    properly support more than 4 ports.
    
    Cc: Morris Ku <morris_ku@sunix.com>
    Cc: Debbie Liu <debbie_liu@sunix.com>
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Link: https://lore.kernel.org/r/20190809190130.30773-1-kai.heng.feng@canonical.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -288,4 +288,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SiFive UART */
 #define PORT_SIFIVE_V0	120
 
+/* Sunix UART */
+#define PORT_SUNIX	121
+
 #endif /* _UAPILINUX_SERIAL_CORE_H */
