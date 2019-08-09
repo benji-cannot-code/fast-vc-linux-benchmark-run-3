48d8c7c9fb21e63100cefd1dbb2ac9f7d969c9d0
Stefan-gabriel Mirea <stefan-gabriel.mirea@nxp.com>
2019-08-09 11:29:16 +0000

    tty: serial: Add linflexuart driver for S32V234
    Introduce support for LINFlex driver, based on:
    - the version of Freescale LPUART driver after commit b3e3bf2ef2c7 ("Merge
      4.0-rc7 into tty-next");
    - commit abf1e0a98083 ("tty: serial: fsl_lpuart: lock port on console
      write").
    In this basic version, the driver can be tested using initramfs and relies
    on the clocks and pin muxing set up by U-Boot.
    
    Remarks concerning the earlycon support:
    
    - LinFlexD does not allow character transmissions in the INIT mode (see
      section 47.4.2.1 in the reference manual[1]). Therefore, a mutual
      exclusion between the first linflex_setup_watermark/linflex_set_termios
      executions and linflex_earlycon_putchar was employed and the characters
      normally sent to earlycon during initialization are kept in a buffer and
      sent afterwards.
    
    - Empirically, character transmission is also forbidden within the last 1-2
      ms before entering the INIT mode, so we use an explicit timeout
      (PREINIT_DELAY) between linflex_earlycon_putchar and the first call to
      linflex_setup_watermark.
    
    - U-Boot currently uses the UART FIFO mode, while this driver makes the
      transition to the buffer mode. Therefore, the earlycon putchar function
      matches the U-Boot behavior before initializations and the Linux behavior
      after.
    
    [1] https://www.nxp.com/webapp/Download?colCode=S32V234RM
    
    Signed-off-by: Stoica Cosmin-Stefan <cosmin.stoica@nxp.com>
    Signed-off-by: Adrian.Nitu <adrian.nitu@freescale.com>
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>
    Signed-off-by: Ana Nedelcu <B56683@freescale.com>
    Signed-off-by: Mihaela Martinas <Mihaela.Martinas@freescale.com>
    Signed-off-by: Matthew Nunez <matthew.nunez@nxp.com>
    [stefan-gabriel.mirea@nxp.com: Reduced for upstreaming and implemented
                                   earlycon support]
    Signed-off-by: Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>
    Link: https://lore.kernel.org/r/20190809112853.15846-6-stefan-gabriel.mirea@nxp.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -291,4 +291,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Sunix UART */
 #define PORT_SUNIX	121
 
+/* Freescale Linflex UART */
+#define PORT_LINFLEXUART	121
+
 #endif /* _UAPILINUX_SERIAL_CORE_H */
