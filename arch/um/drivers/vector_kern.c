1992f65d1e9103e659b5e7ee0f28e5c598f3236e
Anton Ivanov <anton.ivanov@cambridgegreys.com>
2019-08-09 08:40:19 +0100

    um: Add checks to mtu parameter parsing
    Adds a sanity check to the parsing of mtu command line param
    
    Signed-off-by: Anton Ivanov <anton.ivanov@cambridgegreys.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/vector_kern.c
+++ arch/um/drivers/vector_kern.c
@@ -122,7 +122,8 @@ static int get_mtu(struct arglist *def)
 
 	if (mtu != NULL) {
 		if (kstrtoul(mtu, 10, &result) == 0)
-			return result;
+			if ((result < (1 << 16) - 1) && (result >= 576))
+				return result;
 	}
 	return ETH_MAX_PACKET;
 }
