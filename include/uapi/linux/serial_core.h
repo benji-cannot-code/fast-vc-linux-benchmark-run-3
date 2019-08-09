da109d223a83b532637821733d9cf38eab0c3f21
Arnd Bergmann <arnd@arndb.de>
2019-08-09 22:27:30 +0200

    serial: remove ks8695 driver
    The platform is getting removed, so there are no more users
    of this driver.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lore.kernel.org/r/20190809202749.742267-3-arnd@arndb.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/serial_core.h
+++ include/uapi/linux/serial_core.h
@@ -162,9 +162,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Blackfin bf5xx */
 #define PORT_BFIN	75
 
-/* Micrel KS8695 */
-#define PORT_KS8695	76
-
 /* Broadcom SB1250, etc. SOC */
 #define PORT_SB1250_DUART	77
 
