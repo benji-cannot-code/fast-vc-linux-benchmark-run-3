d5bcaabe94518b302e709a2b029d98ede54425db
Dmitry Osipenko <digetx@gmail.com>
2019-08-12 00:00:36 +0300

    memory: tegra: Do not handle error from wait_for_completion_timeout()
    Contrary to its wait_for_completion_timeout_interruptible() sibling, the
    wait_for_completion_timeout() function does not return an error.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/memory/tegra/tegra20-emc.c
+++ drivers/memory/tegra/tegra20-emc.c
@@ -225,7 +225,7 @@ static int emc_prepare_timing_change(struct tegra_emc *emc, unsigned long rate)
 
 static int emc_complete_timing_change(struct tegra_emc *emc, bool flush)
 {
-	long timeout;
+	unsigned long timeout;
 
 	dev_dbg(emc->dev, "%s: flush %d\n", __func__, flush);
 
@@ -241,10 +241,6 @@ static int emc_complete_timing_change(struct tegra_emc *emc, bool flush)
 	if (timeout == 0) {
 		dev_err(emc->dev, "EMC-CAR handshake failed\n");
 		return -EIO;
-	} else if (timeout < 0) {
-		dev_err(emc->dev, "failed to wait for EMC-CAR handshake: %ld\n",
-			timeout);
-		return timeout;
 	}
 
 	return 0;
