b07a5e6016ffbf728321c6c3323e7d0682425122
Marek Behún <marek.behun@nic.cz>
2019-08-12 18:11:14 +0200

    bus: Add support for Moxtet bus
    On the Turris Mox router different modules can be connected to the main
    CPU board: currently a module with a SFP cage, a module with MiniPCIe
    connector, a PCIe pass-through MiniPCIe connector module, a 4-port
    switch module, an 8-port switch module, and a 4-port USB3 module.
    
    For example:
      [CPU]-[PCIe-pass-through]-[PCIe]-[8-port switch]-[8-port switch]-[SFP]
    
    Each of this modules has an input and output shift register, and these
    are connected via SPI to the CPU board.
    
    Via SPI we are able to discover which modules are connected, in which
    order, and we can also read some information about the modules (eg.
    their interrupt status), and configure them.
    From each module 8 bits can be read (of which low 4 bits identify the
    module) and 8 bits can be written.
    
    For example from the module with a SFP cage we can read the LOS,
    TX-FAULT and MOD-DEF0 signals, while we can write TX-DISABLE and
    RATE-SELECT signals.
    
    This driver creates a new bus type, called "moxtet". For each Mox module
    it finds via SPI, it creates a new device on the moxtet bus so that
    drivers can be written for them.
    
    It also implements a virtual interrupt controller for the modules which
    send their interrupt status over the SPI shift register. These modules
    do this in addition to sending their interrupt status via the shared
    interrupt line. When the shared interrupt is triggered, we read from the
    shift register and handle IRQs for all devices which are in interrupt.
    
    The topology of how Mox modules are connected can then be read by
    listing /sys/bus/moxtet/devices.
    
    Link: https://lore.kernel.org/r/20190812161118.21476-2-marek.behun@nic.cz
    Signed-off-by: Marek Behún <marek.behun@nic.cz>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

---
+++ include/dt-bindings/bus/moxtet.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Constant for device tree bindings for Turris Mox module configuration bus
+ *
+ * Copyright (C) 2019 Marek Behun <marek.behun@nic.cz>
+ */
+
+#ifndef _DT_BINDINGS_BUS_MOXTET_H
+#define _DT_BINDINGS_BUS_MOXTET_H
+
+#define MOXTET_IRQ_PCI		0
+#define MOXTET_IRQ_USB3		4
+#define MOXTET_IRQ_PERIDOT(n)	(8 + (n))
+#define MOXTET_IRQ_TOPAZ	12
+
+#endif /* _DT_BINDINGS_BUS_MOXTET_H */
