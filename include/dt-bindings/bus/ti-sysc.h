6ba1416546ee476b644ed27b3a6fa6f876e64c0d
Suman Anna <s-anna@ti.com>
2019-08-13 04:13:32 -0700

    dt-bindings: ti-sysc: Add SPDX license identifier
    Add the appropriate SPDX license identifier to the common
    TI sysc bindings header file.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Acked-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/dt-bindings/bus/ti-sysc.h
+++ include/dt-bindings/bus/ti-sysc.h
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
 /* TI sysc interconnect target module defines */
 
 /* Generic sysc found on omap2 and later, also known as type1 */
