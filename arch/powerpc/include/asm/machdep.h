d5cf7535a9f87dcfed5319743d5f1fd71163764b
Jordan Niethe <jniethe5@gmail.com>
2019-08-13 15:12:12 +1000

    powerpc: Remove empty comment
    Commit 2874c5fd2842 ("treewide: Replace GPLv2 boilerplate/reference with
    SPDX - rule 152") left an empty comment in machdep.h, as the boilerplate
    was the only text in the comment. Remove the empty comment.
    
    Signed-off-by: Jordan Niethe <jniethe5@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20190813051212.6387-1-jniethe5@gmail.com

--- arch/powerpc/include/asm/machdep.h
+++ arch/powerpc/include/asm/machdep.h
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_POWERPC_MACHDEP_H
 #ifdef __KERNEL__
 
-/*
- */
-
 #include <linux/seq_file.h>
 #include <linux/init.h>
 #include <linux/dma-mapping.h>
