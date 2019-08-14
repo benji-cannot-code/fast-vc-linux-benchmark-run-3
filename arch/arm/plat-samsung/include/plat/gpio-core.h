7f99d15606c098bbc4d97e3bc1ecdf59cdb5a0a8
Linus Walleij <linus.walleij@linaro.org>
2019-08-14 09:52:44 +0200

    ARM: samsung: Include GPIO driver header
    This file is using struct gpio_chip and needs to include
    <linux/gpio/driver.h> to get that.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- arch/arm/plat-samsung/include/plat/gpio-core.h
+++ arch/arm/plat-samsung/include/plat/gpio-core.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Bring in machine-local definitions, especially S3C_GPIO_END */
 #include <mach/gpio-samsung.h>
+#include <linux/gpio/driver.h>
 
 #define GPIOCON_OFF	(0x00)
 #define GPIODAT_OFF	(0x04)
