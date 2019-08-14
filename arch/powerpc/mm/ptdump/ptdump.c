4732ff4573244de292d31be2bac6ee398e38e1fd
Christophe Leroy <christophe.leroy@c-s.fr>
2019-08-14 12:36:09 +0000

    powerpc/ptdump: Fix addresses display on PPC32
    Commit 453d87f6a8ae ("powerpc/mm: Warn if W+X pages found on boot")
    wrongly changed KERN_VIRT_START from 0 to PAGE_OFFSET, leading to a
    shift in the displayed addresses.
    
    Lets revert that change to resync walk_pagetables()'s addr val and
    pgd_t pointer for PPC32.
    
    Fixes: 453d87f6a8ae ("powerpc/mm: Warn if W+X pages found on boot")
    Cc: stable@vger.kernel.org # v5.2+
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/eb4d626514e22f85814830012642329018ef6af9.1565786091.git.christophe.leroy@c-s.fr

--- arch/powerpc/mm/ptdump/ptdump.c
+++ arch/powerpc/mm/ptdump/ptdump.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ptdump.h"
 
 #ifdef CONFIG_PPC32
-#define KERN_VIRT_START	PAGE_OFFSET
+#define KERN_VIRT_START	0
 #endif
 
 /*
