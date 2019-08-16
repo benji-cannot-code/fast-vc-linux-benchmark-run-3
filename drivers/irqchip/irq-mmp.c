8b154058d0018f530b632aefc370101b4afa4fb8
Lubomir Rintel <lkundrak@v3.sk>
2019-08-16 20:18:49 +0200

    irqchip/mmp: Do not call irq_set_default_host() on DT platforms
    Using a default domain on DT platforms is unnecessary, as the firmware
    tables describe the full topology, and nothing is implicit.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    [maz: wrote an actual changelog]
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- drivers/irqchip/irq-mmp.c
+++ drivers/irqchip/irq-mmp.c
@@ -396,7 +396,6 @@ static int __init mmp_of_init(struct device_node *node,
 	icu_data[0].conf_enable = mmp_conf.conf_enable;
 	icu_data[0].conf_disable = mmp_conf.conf_disable;
 	icu_data[0].conf_mask = mmp_conf.conf_mask;
-	irq_set_default_host(icu_data[0].domain);
 	set_handle_irq(mmp_handle_irq);
 	max_icu_nr = 1;
 	return 0;
@@ -415,7 +414,6 @@ static int __init mmp2_of_init(struct device_node *node,
 	icu_data[0].conf_enable = mmp2_conf.conf_enable;
 	icu_data[0].conf_disable = mmp2_conf.conf_disable;
 	icu_data[0].conf_mask = mmp2_conf.conf_mask;
-	irq_set_default_host(icu_data[0].domain);
 	set_handle_irq(mmp2_handle_irq);
 	max_icu_nr = 1;
 	return 0;
