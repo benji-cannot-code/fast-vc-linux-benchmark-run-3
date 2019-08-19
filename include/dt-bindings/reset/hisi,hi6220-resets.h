9087753558fa3946356cd731e24f4e2a5bdb8bd4
Peter Griffin <peter.griffin@linaro.org>
2019-08-19 23:48:40 +0000

    dt-bindings: reset: hisilicon: Add ao reset controller
    This is required to bring Mali450 gpu out of reset.
    
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Philipp Zabel <p.zabel@pengutronix.de>
    Cc: dri-devel@lists.freedesktop.org
    Cc: devicetree@vger.kernel.org
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Peter Griffin <peter.griffin@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- include/dt-bindings/reset/hisi,hi6220-resets.h
+++ include/dt-bindings/reset/hisi,hi6220-resets.h
@@ -74,4 +74,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MEDIA_MMU                       6
 #define MEDIA_XG2RAM1                   7
 
+#define AO_G3D                          1
+#define AO_CODECISP                     2
+#define AO_MCPU                         4
+#define AO_BBPHARQMEM                   5
+#define AO_HIFI                         8
+#define AO_ACPUSCUL2C                   12
+
 #endif /*_DT_BINDINGS_RESET_CONTROLLER_HI6220*/
