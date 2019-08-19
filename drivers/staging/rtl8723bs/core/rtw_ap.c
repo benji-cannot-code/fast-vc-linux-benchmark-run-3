dd19ba0e6f6253c1e1d8be287b768c3049166d16
Hariprasad Kelam <hariprasad.kelam@gmail.com>
2019-08-19 22:27:05 +0530

    staging: rtl8723bs: core: Remove unneeded declaration WFD_OUI
    Remove unneeded declaration "extern unsigned char WFD_OUI"
    
    Signed-off-by: Hariprasad Kelam <hariprasad.kelam@gmail.com>
    Link: https://lore.kernel.org/r/20190819165705.GA5782@hari-Inspiron-1545
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_ap.c
+++ drivers/staging/rtl8723bs/core/rtw_ap.c
@@ -14,7 +14,6 @@ extern unsigned char RTW_WPA_OUI[];
 extern unsigned char WMM_OUI[];
 extern unsigned char WPS_OUI[];
 extern unsigned char P2P_OUI[];
-extern unsigned char WFD_OUI[];
 
 void init_mlme_ap_info(struct adapter *padapter)
 {
