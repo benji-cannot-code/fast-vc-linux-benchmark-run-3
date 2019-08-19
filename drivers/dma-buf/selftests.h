4b41009126a78dcc435e29fecce60750a0bf0de3
Chris Wilson <chris@chris-wilson.co.uk>
2019-08-19 10:59:27 +0100

    dma-buf: Add selftests for dma-fence
    Exercise the dma-fence API exported to drivers.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190819095928.32091-2-chris@chris-wilson.co.uk

--- drivers/dma-buf/selftests.h
+++ drivers/dma-buf/selftests.h
@@ -11,3 +11,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Tests are executed in order by igt/dmabuf_selftest
  */
 selftest(sanitycheck, __sanitycheck__) /* keep first (igt selfcheck) */
+selftest(dma_fence, dma_fence)
