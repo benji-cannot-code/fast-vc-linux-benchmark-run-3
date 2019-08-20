3e0ce8176988d023a18f976a851dd8ae6edd89f9
Xu YiPing <xuyiping@hisilicon.com>
2019-08-20 23:06:20 +0000

    drm: kirin: Add register connect helper functions in drm init
    As part of refactoring the kirin driver to better support
    different hardware revisions, this patch adds a flag to the
    device specific driver data so that we can conditionally
    register the connectors at init.
    
    Cc: Rongrong Zou <zourongrong@gmail.com>
    Cc: Xinliang Liu <z.liuxinliang@hisilicon.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: dri-devel <dri-devel@lists.freedesktop.org>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Xinliang Liu <z.liuxinliang@hisilicon.com>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Xu YiPing <xuyiping@hisilicon.com>
    [jstultz: reworded commit message]
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190820230626.23253-20-john.stultz@linaro.org

--- drivers/gpu/drm/hisilicon/kirin/kirin_drm_ade.c
+++ drivers/gpu/drm/hisilicon/kirin/kirin_drm_ade.c
@@ -1074,6 +1074,7 @@ static struct drm_driver ade_driver = {
 };
 
 struct kirin_drm_data ade_driver_data = {
+	.register_connects = false,
 	.num_planes = ADE_CH_NUM,
 	.prim_plane = ADE_CH1,
 	.channel_formats = channel_formats,
