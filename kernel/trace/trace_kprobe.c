3244b9a572db0827cfd065d9a5bd74f1d3c7f744
David Howells <dhowells@redhat.com>
2019-08-19 17:17:58 -0700

    lockdown: Lock down tracing and perf kprobes when in confidentiality mode
    Disallow the creation of perf and ftrace kprobes when the kernel is
    locked down in confidentiality mode by preventing their registration.
    This prevents kprobes from being used to access kernel memory to steal
    crypto data, but continues to allow the use of kprobes from signed
    modules.
    
    Reported-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Matthew Garrett <mjg59@google.com>
    Acked-by: Masami Hiramatsu <mhiramat@kernel.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Cc: Naveen N. Rao <naveen.n.rao@linux.ibm.com>
    Cc: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Cc: davem@davemloft.net
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: James Morris <jmorris@namei.org>

--- kernel/trace/trace_kprobe.c
+++ kernel/trace/trace_kprobe.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/rculist.h>
 #include <linux/error-injection.h>
+#include <linux/security.h>
 
 #include "trace_dynevent.h"
 #include "trace_kprobe_selftest.h"
@@ -415,6 +416,10 @@ static bool within_notrace_func(struct trace_kprobe *tk)
 static int __register_trace_kprobe(struct trace_kprobe *tk)
 {
 	int i, ret;
+
+	ret = security_locked_down(LOCKDOWN_KPROBES);
+	if (ret)
+		return ret;
 
 	if (trace_probe_is_registered(&tk->tp))
 		return -EINVAL;
