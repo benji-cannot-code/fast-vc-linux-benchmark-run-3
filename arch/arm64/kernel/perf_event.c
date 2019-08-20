1d1b771fe394202123d0064d831c9949ac6c7ecd
Raphael Gault <raphael.gault@arm.com>
2019-08-20 16:57:45 +0100

    arm64: perf_event: Add missing header needed for smp_processor_id()
    In perf_event.c we use smp_processor_id(), but we haven't included
    <linux/smp.h> where it is defined, and rely on this being pulled in
    via a transitive include. Let's make this more robust by including
    <linux.smp.h> explicitly.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Raphael Gault <raphael.gault@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/kernel/perf_event.c
+++ arch/arm64/kernel/perf_event.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/perf/arm_pmu.h>
 #include <linux/platform_device.h>
+#include <linux/smp.h>
 
 /* ARMv8 Cortex-A53 specific event types. */
 #define ARMV8_A53_PERFCTR_PREF_LINEFILL				0xC2
