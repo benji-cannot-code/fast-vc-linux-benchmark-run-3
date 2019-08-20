5e0de7cfeecf5d6859820c4db8373532ea4d7235
Linus Walleij <linus.walleij@linaro.org>
2019-08-20 12:37:15 +0200

    mfd: sm501: Include the GPIO driver header
    This driver creates a gpio chip so it needs to include the
    appropriate header <linux/gpio/driver.h> explicitly rather
    than implicitly.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/sm501.c
+++ drivers/mfd/sm501.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/pci.h>
 #include <linux/platform_data/i2c-gpio.h>
+#include <linux/gpio/driver.h>
 #include <linux/gpio/machine.h>
 #include <linux/slab.h>
 
