3aac383399790f64edab20bb9b816a3896f13c4c
Li RongQing <lirongqing@baidu.com>
2019-08-20 10:46:00 +0800

    net: fix icmp_socket_deliver argument 2 input
    it expects a unsigned int, but got a __be32
    
    Signed-off-by: Li RongQing <lirongqing@baidu.com>
    Signed-off-by: Zhang Yu <zhangyu31@baidu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/icmp.c
+++ net/ipv4/icmp.c
@@ -903,7 +903,7 @@ static bool icmp_redirect(struct sk_buff *skb)
 		return false;
 	}
 
-	icmp_socket_deliver(skb, icmp_hdr(skb)->un.gateway);
+	icmp_socket_deliver(skb, ntohl(icmp_hdr(skb)->un.gateway));
 	return true;
 }
 
