62294b594a935e0d995f142ae06cff9e636bb829
Lukasz Luba <l.luba@partner.samsung.com>
2019-08-21 12:42:58 +0200

    memory: Extend of_memory with LPDDR3 support
    Add AC timings information needed to support LPDDR3 and memory
    controllers along with helpers to obtain it.  These will be necessary
    for upcoming Exynos5422 Dynamic Memory Controller driver.
    
    Signed-off-by: Lukasz Luba <l.luba@partner.samsung.com>
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- drivers/memory/of_memory.h
+++ drivers/memory/of_memory.h
@@ -15,6 +15,11 @@ extern const struct lpddr2_min_tck *of_get_min_tck(struct device_node *np,
 extern const struct lpddr2_timings
 	*of_get_ddr_timings(struct device_node *np_ddr, struct device *dev,
 	u32 device_type, u32 *nr_frequencies);
+extern const struct lpddr3_min_tck
+	*of_lpddr3_get_min_tck(struct device_node *np, struct device *dev);
+extern const struct lpddr3_timings
+	*of_lpddr3_get_ddr_timings(struct device_node *np_ddr,
+	struct device *dev, u32 device_type, u32 *nr_frequencies);
 #else
 static inline const struct lpddr2_min_tck
 	*of_get_min_tck(struct device_node *np, struct device *dev)
@@ -25,6 +30,19 @@ static inline const struct lpddr2_min_tck
 static inline const struct lpddr2_timings
 	*of_get_ddr_timings(struct device_node *np_ddr, struct device *dev,
 	u32 device_type, u32 *nr_frequencies)
+{
+	return NULL;
+}
+
+static inline const struct lpddr3_min_tck
+	*of_lpddr3_get_min_tck(struct device_node *np, struct device *dev)
+{
+	return NULL;
+}
+
+static inline const struct lpddr3_timings
+	*of_lpddr3_get_ddr_timings(struct device_node *np_ddr,
+	struct device *dev, u32 device_type, u32 *nr_frequencies)
 {
 	return NULL;
 }
