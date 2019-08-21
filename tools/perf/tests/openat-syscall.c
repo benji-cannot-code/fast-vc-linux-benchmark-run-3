2f2003d1232ce70469ce62c0005205fa66ed961c
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-08-21 14:01:24 -0300

    perf tests: Add missing counts.h
    Those are getting counts.h via evsel.h, that don't strictly need
    counts.h, just forward declarations for some structs, so add it here
    before we remove it from there.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-phldqlfxxu563txja7evd4zt@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/openat-syscall.c
+++ tools/perf/tests/openat-syscall.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "evsel.h"
 #include "debug.h"
 #include "tests.h"
+#include "util/counts.h"
 
 int test__openat_syscall_event(struct test *test __maybe_unused, int subtest __maybe_unused)
 {
