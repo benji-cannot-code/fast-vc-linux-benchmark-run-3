0bf63044e17ae7e4527a4c64a5dc146ab4be3885
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2019-08-21 11:40:19 +0200

    ACPI: PM: s2idle: Always set up EC GPE for system wakeup
    Commit 10a08fd65ec1 ("ACPI: PM: Set up EC GPE for system wakeup from
    drivers that need it") assumed that the EC GPE would only need to be
    set up for system wakeup if either the intel-hid or the intel-vbtn
    driver was in use, but that turns out to be incorrect.  In particular,
    on ASUS Zenbook UX430UNR/i7-8550U, if the EC GPE is not enabled while
    suspended, the system cannot be woken up by opening the lid or
    pressing a key, and that machine doesn't use any of the drivers
    mentioned above.
    
    For this reason, always set up the EC GPE for system wakeup from
    suspend-to-idle by setting and clearing its wake mask in the ACPI
    suspend-to-idle callbacks.
    
    Fixes: 10a08fd65ec1 ("ACPI: PM: Set up EC GPE for system wakeup from drivers that need it")
    Reported-by: Kristian Klausen <kristian@klausen.dk>
    Tested-by: Kristian Klausen <kristian@klausen.dk>
    Acked-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/platform/x86/intel-hid.c
+++ drivers/platform/x86/intel-hid.c
@@ -258,7 +258,6 @@ static int intel_hid_pm_prepare(struct device *device)
 		struct intel_hid_priv *priv = dev_get_drvdata(device);
 
 		priv->wakeup_mode = true;
-		acpi_ec_set_gpe_wake_mask(ACPI_GPE_ENABLE);
 	}
 	return 0;
 }
@@ -267,10 +266,7 @@ static void intel_hid_pm_complete(struct device *device)
 {
 	struct intel_hid_priv *priv = dev_get_drvdata(device);
 
-	if (priv->wakeup_mode) {
-		acpi_ec_set_gpe_wake_mask(ACPI_GPE_DISABLE);
-		priv->wakeup_mode = false;
-	}
+	priv->wakeup_mode = false;
 }
 
 static int intel_hid_pl_suspend_handler(struct device *device)
