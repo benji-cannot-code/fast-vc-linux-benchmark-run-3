1afd162079e397ed33b6a131e5eb791ddb78b28c
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2019-08-22 18:19:28 +0300

    leds: Switch to use fwnode instead of be stuck with OF one
    There is no need to be stuck with OF node when we may use agnostic
    firmware node instead.
    
    It allows users to get property if needed independently of provider.
    
    Note, some OF parts are left because %pfw [1] is in progress.
    
    [1]: https://lore.kernel.org/patchwork/cover/1054863/
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Jacek Anaszewski <jacek.anaszewski@gmail.com>

--- drivers/leds/led-class.c
+++ drivers/leds/led-class.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/leds.h>
 #include <linux/list.h>
 #include <linux/module.h>
-#include <linux/of.h>
+#include <linux/property.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/timer.h>
@@ -278,7 +278,7 @@ int led_classdev_register_ext(struct device *parent,
 		return PTR_ERR(led_cdev->dev);
 	}
 	if (init_data && init_data->fwnode)
-		led_cdev->dev->of_node = to_of_node(init_data->fwnode);
+		led_cdev->dev->fwnode = init_data->fwnode;
 
 	if (ret)
 		dev_warn(parent, "Led %s renamed to %s due to name collision",
