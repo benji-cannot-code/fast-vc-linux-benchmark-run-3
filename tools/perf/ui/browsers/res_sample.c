90fa12fa915794cf99a2e71b65271eb57be9cd51
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-08-22 17:11:39 -0300

    perf sort: Remove needless headers from sort.h, provide fwd struct decls
    Reducing the includes hell a bit more, speeding up the build and
    avoiding needless rebuilds when just one of those files gets updated.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-u63el2vqsovsmnhebx1rcixo@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/ui/browsers/res_sample.c
+++ tools/perf/ui/browsers/res_sample.c
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sort.h"
 #include "config.h"
 #include "time-utils.h"
+#include "../util.h"
+#include "../../util/util.h"
 #include <linux/time64.h>
 #include <linux/zalloc.h>
 
