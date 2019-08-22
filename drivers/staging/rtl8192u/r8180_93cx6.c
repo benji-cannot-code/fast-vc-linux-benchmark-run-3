4d5bbeb378423f7ef2cca6b40ce0018babe6d2b3
Sumera Priyadarsini <sylphrenadin@gmail.com>
2019-08-22 20:25:45 +0530

    staging: rtl8192u: Remove unnecessary blank lines
    This patch fixes the file r8180_93cx6.c to avoid the checkpatch.pl warning:
    
    	CHECK: Please don't use multiple blank lines
    
    Signed-off-by: Sumera Priyadarsini <sylphrenadin@gmail.com>
    Link: https://lore.kernel.org/r/20190822145545.19894-1-sylphrenadin@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192u/r8180_93cx6.c
+++ drivers/staging/rtl8192u/r8180_93cx6.c
@@ -40,7 +40,6 @@ static void eprom_cs(struct net_device *dev, short bit)
 	udelay(EPROM_DELAY);
 }
 
-
 static void eprom_ck_cycle(struct net_device *dev)
 {
 	u8 cmdreg;
@@ -59,7 +58,6 @@ static void eprom_ck_cycle(struct net_device *dev)
 	udelay(EPROM_DELAY);
 }
 
-
 static void eprom_w(struct net_device *dev, short bit)
 {
 	u8 cmdreg;
@@ -76,7 +74,6 @@ static void eprom_w(struct net_device *dev, short bit)
 	force_pci_posting(dev);
 	udelay(EPROM_DELAY);
 }
-
 
 static short eprom_r(struct net_device *dev)
 {
@@ -95,7 +92,6 @@ static short eprom_r(struct net_device *dev)
 	return 0;
 }
 
-
 static void eprom_send_bits_string(struct net_device *dev, short b[], int len)
 {
 	int i;
@@ -105,7 +101,6 @@ static void eprom_send_bits_string(struct net_device *dev, short b[], int len)
 		eprom_ck_cycle(dev);
 	}
 }
-
 
 int eprom_read(struct net_device *dev, u32 addr)
 {
