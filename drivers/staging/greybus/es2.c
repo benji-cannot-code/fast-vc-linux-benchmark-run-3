6c1db71e41e2e4b1a969d64936cbc08265739c69
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-08-25 07:54:28 +0200

    staging: greybus: move the greybus core to drivers/greybus
    The Greybus core code has been stable for a long time, and has been
    shipping for many years in millions of phones.  With the advent of a
    recent Google Summer of Code project, and a number of new devices in the
    works from various companies, it is time to get the core greybus code
    out of staging as it really is going to be with us for a while.
    
    Cc: Johan Hovold <johan@kernel.org>
    Cc: linux-kernel@vger.kernel.org
    Cc: greybus-dev@lists.linaro.org
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Alex Elder <elder@kernel.org>
    Link: https://lore.kernel.org/r/20190825055429.18547-9-gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/greybus/es2.c
+++ drivers/staging/greybus/es2.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/unaligned.h>
 
 #include "arpc.h"
-#include "greybus_trace.h"
+#include "../../greybus/greybus_trace.h"
 
 
 /* Default timeout for USB vendor requests. */
