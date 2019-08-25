741c48a98281f63a4576c5dfc8d179513d5b9b9f
Souptick Joarder <jrdr.linux@gmail.com>
2019-08-25 17:25:41 -0300

    media: usbvision: Remove dead code
    These codes are commented since v4.2-rc1. If there is no plan
    to enable the #else part in future, these can be removed
    forever.
    
    Signed-off-by: Souptick Joarder <jrdr.linux@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/usb/usbvision/usbvision-video.c
+++ drivers/media/usb/usbvision/usbvision-video.c
@@ -966,7 +966,6 @@ static ssize_t usbvision_read(struct file *file, char __user *buf,
 	       __func__,
 	       (unsigned long)count, frame->bytes_read);
 
-#if 1
 	/*
 	 * FIXME:
 	 * For now, forget the frame if it has not been read in one shot.
@@ -975,15 +974,6 @@ static ssize_t usbvision_read(struct file *file, char __user *buf,
 
 	/* Mark it as available to be used again. */
 	frame->grabstate = frame_state_unused;
-#else
-	if (frame->bytes_read >= frame->scanlength) {
-		/* All data has been read */
-		frame->bytes_read = 0;
-
-		/* Mark it as available to be used again. */
-		frame->grabstate = frame_state_unused;
-	}
-#endif
 
 	return count;
 }
