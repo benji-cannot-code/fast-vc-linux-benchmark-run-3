9761977d182e24b90a736c9fe1a856caba274523
Chuck Lever <chuck.lever@oracle.com>
2019-08-26 13:12:51 -0400

    xprtrdma: Clear xprt->reestablish_timeout on close
    Ensure that the re-establishment delay does not grow exponentially
    on each good reconnect. This probably should have been part of
    commit 675dd90ad093 ("xprtrdma: Modernize ops->connect").
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- net/sunrpc/xprtrdma/transport.c
+++ net/sunrpc/xprtrdma/transport.c
@@ -424,8 +424,6 @@ void xprt_rdma_close(struct rpc_xprt *xprt)
 
 	if (ep->rep_connected == -ENODEV)
 		return;
-	if (ep->rep_connected > 0)
-		xprt->reestablish_timeout = 0;
 	rpcrdma_ep_disconnect(ep, ia);
 
 	/* Prepare @xprt for the next connection by reinitializing
@@ -435,6 +433,7 @@ void xprt_rdma_close(struct rpc_xprt *xprt)
 	xprt->cwnd = RPC_CWNDSHIFT;
 
 out:
+	xprt->reestablish_timeout = 0;
 	++xprt->connect_cookie;
 	xprt_disconnect_done(xprt);
 }
