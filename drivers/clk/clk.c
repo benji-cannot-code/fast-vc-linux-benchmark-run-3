d97fc6e35c3cf1e68c5dd42d6d604fdfd1092f2a
Stephen Boyd <sboyd@kernel.org>
2019-08-26 16:47:29 -0700

    clk: Drop !clk checks in debugfs dumping
    These recursive functions have checks for !clk being passed in, but the
    callers are always looping through lists and therefore the pointers
    can't be NULL. Drop the checks to simplify the code.
    
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Link: https://lkml.kernel.org/r/20190826234729.145593-1-sboyd@kernel.org

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2850,9 +2850,6 @@ static struct hlist_head *orphan_list[] = {
 static void clk_summary_show_one(struct seq_file *s, struct clk_core *c,
 				 int level)
 {
-	if (!c)
-		return;
-
 	seq_printf(s, "%*s%-*s %7d %8d %8d %11lu %10lu %5d %6d\n",
 		   level * 3 + 1, "",
 		   30 - level * 3, c->name,
@@ -2866,9 +2863,6 @@ static void clk_summary_show_subtree(struct seq_file *s, struct clk_core *c,
 				     int level)
 {
 	struct clk_core *child;
-
-	if (!c)
-		return;
 
 	clk_summary_show_one(s, c, level);
 
@@ -2901,9 +2895,6 @@ static void clk_dump_one(struct seq_file *s, struct clk_core *c, int level)
 {
 	unsigned long min_rate, max_rate;
 
-	if (!c)
-		return;
-
 	clk_core_get_boundaries(c, &min_rate, &max_rate);
 
 	/* This should be JSON format, i.e. elements separated with a comma */
@@ -2923,9 +2914,6 @@ static void clk_dump_one(struct seq_file *s, struct clk_core *c, int level)
 static void clk_dump_subtree(struct seq_file *s, struct clk_core *c, int level)
 {
 	struct clk_core *child;
-
-	if (!c)
-		return;
 
 	clk_dump_one(s, c, level);
 
