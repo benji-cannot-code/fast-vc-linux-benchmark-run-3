1c386d7968ff7e54091e1734b651f7b89a23f159
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-08-27 11:32:09 -0300

    perf tools: Remove needless util.h include from builtin.h
    And fix up places where util.h is needed but was obtained indirectly via
    builtin.h.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-a01ig3c4t76ye5wkqmtgk9qn@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin.h
+++ tools/perf/builtin.h
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef BUILTIN_H
 #define BUILTIN_H
 
-#include "util/util.h"
-
 extern const char perf_usage_string[];
 extern const char perf_more_info_string[];
 
