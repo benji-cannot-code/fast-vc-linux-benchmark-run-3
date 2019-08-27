c94396d58ae3d6ebba9959ab5c588551e99afb5e
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-08-27 21:40:42 +0200

    Revert "of/platform: Pause/resume sync state during init and of_platform_populate()"
    This reverts commit 21871a99b34c65c56a24193c277a4981529c306f.
    
    Based on a lot of email and in-person discussions, this patch series is
    being reworked to address a number of issues that were pointed out that
    needed to be taken care of before it should be merged.  It will be
    resubmitted with those changes hopefully soon.
    
    Cc: Frank Rowand <frowand.list@gmail.com>
    Cc: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -481,7 +481,6 @@ int of_platform_populate(struct device_node *root,
 	pr_debug("%s()\n", __func__);
 	pr_debug(" starting at: %pOF\n", root);
 
-	device_links_supplier_sync_state_pause();
 	for_each_child_of_node(root, child) {
 		rc = of_platform_bus_create(child, matches, lookup, parent, true);
 		if (rc) {
@@ -489,8 +488,6 @@ int of_platform_populate(struct device_node *root,
 			break;
 		}
 	}
-	device_links_supplier_sync_state_resume();
-
 	of_node_set_flag(root, OF_POPULATED_BUS);
 
 	of_node_put(root);
@@ -687,7 +684,6 @@ static int __init of_platform_default_populate_init(void)
 		return -ENODEV;
 
 	platform_bus_type.add_links = of_link_to_suppliers;
-	device_links_supplier_sync_state_pause();
 	/*
 	 * Handle certain compatibles explicitly, since we don't want to create
 	 * platform_devices for every node in /reserved-memory with a
@@ -708,13 +704,6 @@ static int __init of_platform_default_populate_init(void)
 	return 0;
 }
 arch_initcall_sync(of_platform_default_populate_init);
-
-static int __init of_platform_sync_state_init(void)
-{
-	device_links_supplier_sync_state_resume();
-	return 0;
-}
-late_initcall_sync(of_platform_sync_state_init);
 #endif
 
 int of_platform_device_destroy(struct device *dev, void *data)
