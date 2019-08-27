b7bab9f6727b0d379c6f1a9a07272a5a2ccd582f
YueHaibing <yuehaibing@huawei.com>
2019-08-27 07:23:34 +0000

    MIPS: Octeon: remove duplicated include from dma-octeon.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: James Hogan <jhogan@kernel.org>
    Cc: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: <linux-mips@vger.kernel.org>
    Cc: <kernel-janitors@vger.kernel.org>

--- arch/mips/cavium-octeon/dma-octeon.c
+++ arch/mips/cavium-octeon/dma-octeon.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/init.h>
 #include <linux/mm.h>
-#include <linux/memblock.h>
 
 #include <asm/bootinfo.h>
 
