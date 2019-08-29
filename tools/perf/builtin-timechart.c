37fb56b8add9089622158b1705893d8040bca68a
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-08-29 13:00:28 -0300

    perf tools: Remove needless libtraceevent include directives
    Remove traceevent/event-parse.h and traceevent/trace-seq.h from places
    where it is not needed.
    
    Should avoid rebuilding those files when these traceevent headers get
    changed.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Tzvetomir Stoyanov <tstoyanov@vmware.com>
    Link: https://lkml.kernel.org/n/tip-26hn75jn9rdealn4uqtzend6@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/builtin-timechart.c
+++ tools/perf/builtin-timechart.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <errno.h>
 #include <inttypes.h>
-#include <traceevent/event-parse.h>
 
 #include "builtin.h"
 #include "util/color.h"
