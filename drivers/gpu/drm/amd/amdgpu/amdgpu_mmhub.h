f45cdcf1504952808a6e18b7253d53ac9dced5ce
Hawking Zhang <Hawking.Zhang@amd.com>
2019-08-30 13:34:38 +0800

    drm/amdgpu: add mmhub ras_late_init callback function (v2)
    The function will be called in late init phase to do mmhub
    ras init
    
    v2: check ras_late_init function pointer before invoking the
    function
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_mmhub.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_mmhub.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct amdgpu_mmhub_funcs {
 	void (*ras_init)(struct amdgpu_device *adev);
+	int (*ras_late_init)(struct amdgpu_device *adev);
 	void (*query_ras_error_count)(struct amdgpu_device *adev,
 					void *ras_error_status);
 };
