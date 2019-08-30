095bf3a8dec942ed0b2180b9d773c54ea4deb53e
George G. Davis <george_davis@mentor.com>
2019-08-30 15:32:23 -0400

    selftests: watchdog: Add optional file argument
    Some systems have multiple watchdog devices where the first device
    registered is assigned to the /dev/watchdog device file. In order
    to test other watchdog devices, add an optional file argument for
    selecting non-default watchdog devices for testing.
    
    Tested-by: Eugeniu Rosca <erosca@de.adit-jv.com>
    Signed-off-by: George G. Davis <george_davis@mentor.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/watchdog/watchdog-test.c
+++ tools/testing/selftests/watchdog/watchdog-test.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int fd;
 const char v = 'V';
-static const char sopts[] = "bdehp:t:Tn:NL";
+static const char sopts[] = "bdehp:t:Tn:NLf:";
 static const struct option lopts[] = {
 	{"bootstatus",          no_argument, NULL, 'b'},
 	{"disable",             no_argument, NULL, 'd'},
@@ -32,6 +32,7 @@ static const struct option lopts[] = {
 	{"pretimeout",    required_argument, NULL, 'n'},
 	{"getpretimeout",       no_argument, NULL, 'N'},
 	{"gettimeleft",		no_argument, NULL, 'L'},
+	{"file",          required_argument, NULL, 'f'},
 	{NULL,                  no_argument, NULL, 0x0}
 };
 
@@ -70,6 +71,8 @@ static void term(int sig)
 static void usage(char *progname)
 {
 	printf("Usage: %s [options]\n", progname);
+	printf(" -f, --file          Open watchdog device file\n");
+	printf("                     Default is /dev/watchdog\n");
 	printf(" -b, --bootstatus    Get last boot status (Watchdog/POR)\n");
 	printf(" -d, --disable       Turn off the watchdog timer\n");
 	printf(" -e, --enable        Turn on the watchdog timer\n");
@@ -93,14 +96,20 @@ int main(int argc, char *argv[])
 	int ret;
 	int c;
 	int oneshot = 0;
+	char *file = "/dev/watchdog";
 
 	setbuf(stdout, NULL);
 
-	fd = open("/dev/watchdog", O_WRONLY);
+	while ((c = getopt_long(argc, argv, sopts, lopts, NULL)) != -1) {
+		if (c == 'f')
+			file = optarg;
+	}
+
+	fd = open(file, O_WRONLY);
 
 	if (fd == -1) {
 		if (errno == ENOENT)
-			printf("Watchdog device not enabled.\n");
+			printf("Watchdog device (%s) not found.\n", file);
 		else if (errno == EACCES)
 			printf("Run watchdog as root.\n");
 		else
@@ -108,6 +117,8 @@ int main(int argc, char *argv[])
 				strerror(errno));
 		exit(-1);
 	}
+
+	optind = 0;
 
 	while ((c = getopt_long(argc, argv, sopts, lopts, NULL)) != -1) {
 		switch (c) {
@@ -190,6 +201,9 @@ int main(int argc, char *argv[])
 				printf("WDIOC_GETTIMELEFT returns %u seconds.\n", flags);
 			else
 				printf("WDIOC_GETTIMELEFT error '%s'\n", strerror(errno));
+			break;
+		case 'f':
+			/* Handled above */
 			break;
 
 		default:
