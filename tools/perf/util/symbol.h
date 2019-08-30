eff09870f5428bda9e407c6efd8077965308f34a
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-08-30 10:19:19 -0300

    perf symbols: Add missing linux/refcount.h to symbol.h
    We use refcount_t there, so we need that header or else it'll break when
    we remove dso.h, that is from where it is getting that definition now...
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-5albrk0uve6x9cf6x3ebwpae@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/symbol.h
+++ tools/perf/util/symbol.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __PERF_SYMBOL 1
 
 #include <linux/types.h>
+#include <linux/refcount.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <linux/list.h>
