187bbbe550f3547ce2034a3752f7e61a86375603
Vincenzo Frascino <vincenzo.frascino@arm.com>
2019-08-30 14:58:57 +0100

    mips: compat: vdso: Use legacy syscalls as fallback
    The generic VDSO implementation uses the Y2038 safe clock_gettime64() and
    clock_getres_time64() syscalls as fallback for 32bit VDSO. This breaks
    seccomp setups because these syscalls might be not (yet) allowed.
    
    Implement the 32bit variants which use the legacy syscalls and select the
    variant in the core library.
    
    The 64bit time variants are not removed because they are required for the
    time64 based vdso accessors.
    
    Cc: Paul Burton <paul.burton@mips.com>
    Fixes: 00b26474c2f1 ("lib/vdso: Provide generic VDSO implementation")
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: linux-arch@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mips@vger.kernel.org
    Cc: linux-kselftest@vger.kernel.org
    Cc: catalin.marinas@arm.com
    Cc: will@kernel.org
    Cc: tglx@linutronix.de
    Cc: salyzyn@android.com
    Cc: 0x7f454c46@gmail.com
    Cc: luto@kernel.org

--- arch/mips/vdso/config-n32-o32-env.c
+++ arch/mips/vdso/config-n32-o32-env.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #undef CONFIG_64BIT
 
+#define BUILD_VDSO32
 #define CONFIG_32BIT 1
 #define CONFIG_GENERIC_ATOMIC64 1
 #define BUILD_VDSO32_64
