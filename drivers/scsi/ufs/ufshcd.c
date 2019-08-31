9c8b99616f7bb5d315b30bd7e6bd556f117e85a7
YueHaibing <yuehaibing@huawei.com>
2019-08-31 12:44:24 +0000

    scsi: ufs: Use kmemdup in ufshcd_read_string_desc()
    Use kmemdup rather than duplicating its implementation
    
    Link: https://lore.kernel.org/r/20190831124424.18642-1-yuehaibing@huawei.com
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -3310,12 +3310,11 @@ int ufshcd_read_string_desc(struct ufs_hba *hba, u8 desc_index,
 		str[ret++] = '\0';
 
 	} else {
-		str = kzalloc(uc_str->len, GFP_KERNEL);
+		str = kmemdup(uc_str, uc_str->len, GFP_KERNEL);
 		if (!str) {
 			ret = -ENOMEM;
 			goto out;
 		}
-		memcpy(str, uc_str, uc_str->len);
 		ret = uc_str->len;
 	}
 out:
