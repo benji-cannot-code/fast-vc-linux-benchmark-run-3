69e3a1433f9af71d87ccc23fc20d547d81752a44
Joao Martins <joao.m.martins@oracle.com>
2019-09-02 11:40:31 +0100

    cpuidle-haltpoll: vcpu hotplug support
    When cpus != maxcpus cpuidle-haltpoll will fail to register all vcpus
    past the online ones and thus fail to register the idle driver.
    This is because cpuidle_add_sysfs() will return with -ENODEV as a
    consequence from get_cpu_device() return no device for a non-existing
    CPU.
    
    Instead switch to cpuidle_register_driver() and manually register each
    of the present cpus through cpuhp_setup_state() callbacks and future
    ones that get onlined or offlined. This mimmics similar logic that
    intel_idle does.
    
    Fixes: fa86ee90eb11 ("add cpuidle-haltpoll driver")
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- arch/x86/include/asm/cpuidle_haltpoll.h
+++ arch/x86/include/asm/cpuidle_haltpoll.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ARCH_HALTPOLL_H
 #define _ARCH_HALTPOLL_H
 
-void arch_haltpoll_enable(void);
-void arch_haltpoll_disable(void);
+void arch_haltpoll_enable(unsigned int cpu);
+void arch_haltpoll_disable(unsigned int cpu);
 
 #endif
--- include/linux/cpuidle_haltpoll.h
+++ include/linux/cpuidle_haltpoll.h
@@ -6,11 +6,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_ARCH_CPUIDLE_HALTPOLL
 #include <asm/cpuidle_haltpoll.h>
 #else
-static inline void arch_haltpoll_enable(void)
+static inline void arch_haltpoll_enable(unsigned int cpu)
 {
 }
 
-static inline void arch_haltpoll_disable(void)
+static inline void arch_haltpoll_disable(unsigned int cpu)
 {
 }
 #endif
