dbd05aab5e72dc3c2e66fb8e1e220eada485745a
Valentin Vidic <vvidic@valentin-vidic.from.hr>
2019-09-02 21:03:29 +0200

    staging: exfat: cleanup blank line warnings
    Fixes checkpatch.pl warnings:
    
      CHECK: Please don't use multiple blank lines
      CHECK: Blank lines aren't necessary after an open brace '{'
      CHECK: Please use a blank line after function/struct/union/enum
             declarations
    
    Signed-off-by: Valentin Vidic <vvidic@valentin-vidic.from.hr>
    Link: https://lore.kernel.org/r/20190902190329.18685-1-vvidic@valentin-vidic.from.hr
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/exfat/exfat_core.c
+++ drivers/staging/exfat/exfat_core.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include "exfat.h"
 
-
 static void __set_sb_dirty(struct super_block *sb)
 {
 	struct exfat_sb_info *sbi = EXFAT_SB(sb);
@@ -1712,7 +1711,6 @@ struct entry_set_cache_t *get_entry_set_in_dir(struct super_block *sb,
 	if (ret != FFS_SUCCESS)
 		return NULL;
 
-
 	/* byte offset in cluster */
 	byte_offset &= p_fs->cluster_size - 1;
 
@@ -1726,7 +1724,6 @@ struct entry_set_cache_t *get_entry_set_in_dir(struct super_block *sb,
 	buf = buf_getblk(sb, sec);
 	if (buf == NULL)
 		goto err_out;
-
 
 	ep = (struct dentry_t *)(buf + off);
 	entry_type = p_fs->fs_func->get_entry_type(ep);
@@ -1853,7 +1850,6 @@ void release_entry_set(struct entry_set_cache_t *es)
 	pr_debug("%s es=%p\n", __func__, es);
 	kfree(es);
 }
-
 
 static s32 __write_partial_entries_in_entry_set(struct super_block *sb,
 						struct entry_set_cache_t *es,
