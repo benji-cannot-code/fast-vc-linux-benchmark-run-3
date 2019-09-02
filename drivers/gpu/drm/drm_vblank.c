d0a06294a4404a887ad34db562e5f93d178ffeae
Heinrich Fink <heinrich.fink@daqri.com>
2019-09-02 16:24:12 +0200

    drm: Add high-precision time to vblank trace event
    Store the timestamp of the current vblank in the new field 'time' of the
    vblank trace event. If the timestamp is calculated by a driver that
    supports high-precision vblank timing, set the field 'high-prec' to
    'true'.
    
    User space can now access actual hardware vblank times via the tracing
    infrastructure. Tracing applications (such as GPUVis, see [0] for
    related discussion), can use the newly added information to conduct a
    more accurate analysis of display timing.
    
    v2 Fix author name (missing last name)
    
    [0] https://github.com/mikesart/gpuvis/issues/30
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Heinrich Fink <heinrich.fink@daqri.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20190902142412.27846-2-heinrich.fink@daqri.com

--- drivers/gpu/drm/drm_vblank.c
+++ drivers/gpu/drm/drm_vblank.c
@@ -1732,7 +1732,8 @@ static void drm_handle_vblank_events(struct drm_device *dev, unsigned int pipe)
 		send_vblank_event(dev, e, seq, now);
 	}
 
-	trace_drm_vblank_event(pipe, seq);
+	trace_drm_vblank_event(pipe, seq, now,
+			dev->driver->get_vblank_timestamp != NULL);
 }
 
 /**
