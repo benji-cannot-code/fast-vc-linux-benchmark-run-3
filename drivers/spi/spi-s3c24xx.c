d77a9fe6f1716b1da39b860ecd81a3a287fbd417
Arnd Bergmann <arnd@arndb.de>
2019-09-03 11:31:09 +0200

    ARM: s3c24xx: move spi fiq handler into platform
    The fiq handler needs access to some register definitions that
    should not be used directly by device drivers.
    
    Since this is closely related to the irqchip driver anyway,
    move it into the same place.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    [krzk: Add a header guard in include/linux/spi/s3c24xx-fiq.h, fix
           SPDX comment style, update maintainer's entry]
    Co-developed-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Link: https://lore.kernel.org/r/20200806182059.2431-23-krzk%40kernel.org
    Acked-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-s3c24xx.c
+++ drivers/spi/spi-s3c24xx.c
@@ -20,12 +20,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_bitbang.h>
 #include <linux/spi/s3c24xx.h>
+#include <linux/spi/s3c24xx-fiq.h>
 #include <linux/module.h>
 
 #include <asm/fiq.h>
 
 #include "spi-s3c24xx-regs.h"
-#include "spi-s3c24xx-fiq.h"
 
 /**
  * s3c24xx_spi_devstate - per device data
@@ -230,10 +230,6 @@ struct spi_fiq_code {
 	u8	data[];
 };
 
-extern struct spi_fiq_code s3c24xx_spi_fiq_txrx;
-extern struct spi_fiq_code s3c24xx_spi_fiq_tx;
-extern struct spi_fiq_code s3c24xx_spi_fiq_rx;
-
 /**
  * ack_bit - turn IRQ into IRQ acknowledgement bit
  * @irq: The interrupt number
@@ -283,7 +279,6 @@ static void s3c24xx_spi_tryfiq(struct s3c24xx_spi *hw)
 	regs.uregs[fiq_rrx]  = (long)hw->rx;
 	regs.uregs[fiq_rtx]  = (long)hw->tx + 1;
 	regs.uregs[fiq_rcount] = hw->len - 1;
-	regs.uregs[fiq_rirq] = (long)S3C24XX_VA_IRQ;
 
 	set_fiq_regs(&regs);
 
