9605d53795fefa2cbaef39a26e3528df0e15df65
Jerome Brunet <jbrunet@baylibre.com>
2019-09-05 15:50:39 +0200

    reset: dt-bindings: meson: update arb bindings for sm1
    SM1 SoC family adds two new audio FIFOs with the related arb reset lines
    
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- include/dt-bindings/reset/amlogic,meson-axg-audio-arb.h
+++ include/dt-bindings/reset/amlogic,meson-axg-audio-arb.h
@@ -14,5 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AXG_ARB_FRDDR_A	3
 #define AXG_ARB_FRDDR_B	4
 #define AXG_ARB_FRDDR_C	5
+#define AXG_ARB_TODDR_D	6
+#define AXG_ARB_FRDDR_D	7
 
 #endif /* _DT_BINDINGS_AMLOGIC_MESON_AXG_AUDIO_ARB_H */
