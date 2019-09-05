2363abfd0d31e6ffca2e08d4cac0fee375d5b755
Andrew Murray <andrew.murray@arm.com>
2019-09-05 16:05:28 +0100

    PCI: rcar: Remove unnecessary header include (../pci.h)
    Remove unnecessary header include (../pci.h) since it doesn't
    provide any needed symbols.
    
    Signed-off-by: Andrew Murray <andrew.murray@arm.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Reviewed-by: Simon Horman <horms+renesas@verge.net.au>
    Reviewed-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/pci/controller/pcie-rcar.c
+++ drivers/pci/controller/pcie-rcar.c
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm_runtime.h>
 #include <linux/slab.h>
 
-#include "../pci.h"
-
 #define PCIECAR			0x000010
 #define PCIECCTLR		0x000018
 #define  CONFIG_SEND_ENABLE	BIT(31)
