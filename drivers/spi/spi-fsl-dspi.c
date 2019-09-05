5a4362206061eca4c299903316c74449e4ba5294
Vladimir Oltean <olteanv@gmail.com>
2019-09-05 04:01:13 +0300

    spi: spi-fsl-dspi: Implement the PTP system timestamping for TCFQ mode
    In this mode, the DSPI controller uses PIO to transfer word by word. In
    comparison, in EOQ mode the 4-word deep FIFO is being used, hence the
    current logic will need some adaptation for which I do not have the
    hardware (Coldfire) to test. It is not clear what is the timing of DMA
    transfers and whether timestamping in the driver brings any overall
    performance increase compared to regular timestamping done in the core.
    
    Short phc2sys summary after 58 minutes of running on LS1021A-TSN with
    interrupts disabled during the critical section:
    
      offset: min -26251 max 16416 mean -21.8672 std dev 863.416
      delay: min 4720 max 57280 mean 5182.49 std dev 1607.19
      lost servo lock 3 times
    
    Summary of the same phc2sys service running for 120 minutes with
    interrupts disabled:
    
      offset: min -378 max 381 mean -0.0083089 std dev 101.495
      delay: min 4720 max 5920 mean 5129.38 std dev 154.899
      lost servo lock 0 times
    
    The minimum delay (pre to post time) in nanoseconds is the same, but the
    maximum delay is quite a bit higher, due to interrupts getting sometimes
    executed and interfering with the measurement. Hence set disable_irqs
    whenever possible (aka when the driver runs in poll mode - otherwise it
    would be a contradiction in terms).
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Link: https://lore.kernel.org/r/20190905010114.26718-4-olteanv@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-fsl-dspi.c
+++ drivers/spi/spi-fsl-dspi.c
@@ -130,6 +130,7 @@ enum dspi_trans_mode {
 struct fsl_dspi_devtype_data {
 	enum dspi_trans_mode	trans_mode;
 	u8			max_clock_factor;
+	bool			ptp_sts_supported;
 	bool			xspi_mode;
 };
 
@@ -141,12 +142,14 @@ static const struct fsl_dspi_devtype_data vf610_data = {
 static const struct fsl_dspi_devtype_data ls1021a_v1_data = {
 	.trans_mode		= DSPI_TCFQ_MODE,
 	.max_clock_factor	= 8,
+	.ptp_sts_supported	= true,
 	.xspi_mode		= true,
 };
 
 static const struct fsl_dspi_devtype_data ls2085a_data = {
 	.trans_mode		= DSPI_TCFQ_MODE,
 	.max_clock_factor	= 8,
+	.ptp_sts_supported	= true,
 };
 
 static const struct fsl_dspi_devtype_data coldfire_data = {
@@ -655,6 +658,9 @@ static int dspi_rxtx(struct fsl_dspi *dspi)
 	u16 spi_tcnt;
 	u32 spi_tcr;
 
+	spi_take_timestamp_post(dspi->ctlr, dspi->cur_transfer,
+				dspi->tx - dspi->bytes_per_word, !dspi->irq);
+
 	/* Get transfer counter (in number of SPI transfers). It was
 	 * reset to 0 when transfer(s) were started.
 	 */
@@ -672,6 +678,9 @@ static int dspi_rxtx(struct fsl_dspi *dspi)
 	if (!dspi->len)
 		/* Success! */
 		return 0;
+
+	spi_take_timestamp_pre(dspi->ctlr, dspi->cur_transfer,
+			       dspi->tx, !dspi->irq);
 
 	if (trans_mode == DSPI_EOQ_MODE)
 		dspi_eoq_write(dspi);
@@ -779,6 +788,9 @@ static int dspi_transfer_one_message(struct spi_controller *ctlr,
 			regmap_write(dspi->regmap, SPI_CTARE(0),
 				     SPI_FRAME_EBITS(transfer->bits_per_word) |
 				     SPI_CTARE_DTCP(1));
+
+		spi_take_timestamp_pre(dspi->ctlr, dspi->cur_transfer,
+				       dspi->tx, !dspi->irq);
 
 		trans_mode = dspi->devtype_data->trans_mode;
 		switch (trans_mode) {
@@ -1156,6 +1168,7 @@ static int dspi_probe(struct platform_device *pdev)
 	init_waitqueue_head(&dspi->waitq);
 
 poll_mode:
+
 	if (dspi->devtype_data->trans_mode == DSPI_DMA_MODE) {
 		ret = dspi_request_dma(dspi, res->start);
 		if (ret < 0) {
@@ -1166,6 +1179,8 @@ poll_mode:
 
 	ctlr->max_speed_hz =
 		clk_get_rate(dspi->clk) / dspi->devtype_data->max_clock_factor;
+
+	ctlr->ptp_sts_supported = dspi->devtype_data->ptp_sts_supported;
 
 	platform_set_drvdata(pdev, ctlr);
 
