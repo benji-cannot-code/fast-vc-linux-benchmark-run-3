91fd944005a5ecf61c4f84f4308ded304258925a
Tao Zhou <tao.zhou1@amd.com>
2019-09-06 14:32:14 +0800

    drm/amdgpu: rename umc ras_init to err_cnt_init
    this interface is related to specific version of umc, distinguish it
    from ras_late_init
    
    Signed-off-by: Tao Zhou <tao.zhou1@amd.com>
    Reviewed-by: Guchun Chen <guchun.chen@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_umc.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_umc.h
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	adev->umc.funcs->disable_umc_index_mode(adev);
 
 struct amdgpu_umc_funcs {
-	void (*ras_init)(struct amdgpu_device *adev);
+	void (*err_cnt_init)(struct amdgpu_device *adev);
 	int (*ras_late_init)(struct amdgpu_device *adev, void *ras_ih_info);
 	void (*query_ras_error_count)(struct amdgpu_device *adev,
 					void *ras_error_status);
