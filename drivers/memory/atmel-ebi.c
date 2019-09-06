f09f29d3c54665d836afa2700c09dab12308c86a
Tudor Ambarus <tudor.ambarus@microchip.com>
2019-09-06 15:15:28 +0000

    memory: atmel-ebi: switch to SPDX license identifiers
    Adopt the SPDX license identifiers to ease license compliance
    management.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Link: https://lore.kernel.org/r/20190906151519.19442-1-tudor.ambarus@microchip.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/memory/atmel-ebi.c
+++ drivers/memory/atmel-ebi.c
@@ -1,13 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * EBI driver for Atmel chips
  * inspired by the fsl weim bus driver
  *
  * Copyright (C) 2013 Jean-Jacques Hiblot <jjhiblot@traphandler.com>
- *
- * This file is licensed under the terms of the GNU General Public
- * License version 2. This program is licensed "as is" without any
- * warranty of any kind, whether express or implied.
  */
 
 #include <linux/clk.h>
