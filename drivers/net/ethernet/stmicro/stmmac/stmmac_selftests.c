8750d46c8d84049f3ba6b2537900420e27b4a63e
Jose Abreu <Jose.Abreu@synopsys.com>
2019-09-06 09:41:13 +0200

    net: stmmac: selftests: Add missing checks for support of SA
    Add checks for support of Source Address Insertion/Replacement before
    running the test.
    
    Signed-off-by: Jose Abreu <joabreu@synopsys.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_selftests.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_selftests.c
@@ -1058,6 +1058,9 @@ static int stmmac_test_desc_sai(struct stmmac_priv *priv)
 	struct stmmac_packet_attrs attr = { };
 	int ret;
 
+	if (!priv->dma_cap.vlins)
+		return -EOPNOTSUPP;
+
 	attr.remove_sa = true;
 	attr.sarc = true;
 	attr.src = src;
@@ -1077,6 +1080,9 @@ static int stmmac_test_desc_sar(struct stmmac_priv *priv)
 	struct stmmac_packet_attrs attr = { };
 	int ret;
 
+	if (!priv->dma_cap.vlins)
+		return -EOPNOTSUPP;
+
 	attr.sarc = true;
 	attr.src = src;
 	attr.dst = priv->dev->dev_addr;
@@ -1094,6 +1100,9 @@ static int stmmac_test_reg_sai(struct stmmac_priv *priv)
 	unsigned char src[ETH_ALEN] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 	struct stmmac_packet_attrs attr = { };
 	int ret;
+
+	if (!priv->dma_cap.vlins)
+		return -EOPNOTSUPP;
 
 	attr.remove_sa = true;
 	attr.sarc = true;
@@ -1114,6 +1123,9 @@ static int stmmac_test_reg_sar(struct stmmac_priv *priv)
 	unsigned char src[ETH_ALEN] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 	struct stmmac_packet_attrs attr = { };
 	int ret;
+
+	if (!priv->dma_cap.vlins)
+		return -EOPNOTSUPP;
 
 	attr.sarc = true;
 	attr.src = src;
