c5a876b24660b45c759857b2b490be12972c3b02
Vincenzo Frascino <vincenzo.frascino@arm.com>
2019-09-06 10:58:01 +0100

    arm64: mte: CPU feature detection and initial sysreg configuration
    Add the cpufeature and hwcap entries to detect the presence of MTE. Any
    secondary CPU not supporting the feature, if detected on the boot CPU,
    will be parked.
    
    Add the minimum SCTLR_EL1 and HCR_EL2 bits for enabling MTE. The Normal
    Tagged memory type is configured in MAIR_EL1 before the MMU is enabled
    in order to avoid disrupting other CPUs in the CnP domain.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Co-developed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Suzuki K Poulose <Suzuki.Poulose@arm.com>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -614,6 +614,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 			 SCTLR_EL1_SA0  | SCTLR_EL1_SED  | SCTLR_ELx_I    |\
 			 SCTLR_EL1_DZE  | SCTLR_EL1_UCT                   |\
 			 SCTLR_EL1_NTWE | SCTLR_ELx_IESB | SCTLR_EL1_SPAN |\
+			 SCTLR_ELx_ITFSB| SCTLR_ELx_ATA  | SCTLR_EL1_ATA0 |\
 			 ENDIAN_SET_EL1 | SCTLR_EL1_UCI  | SCTLR_EL1_RES1)
 
 /* MAIR_ELx memory attributes (used by Linux) */
--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -94,7 +94,7 @@ static const char *const hwcap_str[] = {
 	"dgh",
 	"rng",
 	"bti",
-	/* reserved for "mte" */
+	"mte",
 	NULL
 };
 
