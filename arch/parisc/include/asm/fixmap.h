3c99e7640f281992b1b1513a5e31eb0edfdc4ecd
Sven Schnelle <svens@stackframe.org>
2019-09-08 11:33:04 +0200

    parisc: add kexec syscall support
    Signed-off-by: Sven Schnelle <svens@stackframe.org>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/asm/fixmap.h
+++ arch/parisc/include/asm/fixmap.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum fixed_addresses {
 	/* Support writing RO kernel text via kprobes, jump labels, etc. */
 	FIX_TEXT_POKE0,
+	FIX_TEXT_KEXEC,
 	FIX_BITMAP_COUNT
 };
 
