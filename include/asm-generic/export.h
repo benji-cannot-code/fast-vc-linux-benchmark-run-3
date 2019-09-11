06e43c2868bb064775644232dc6aa49b082d3c34
Will Deacon <will@kernel.org>
2019-09-11 13:26:46 +0100

    module: Fix link failure due to invalid relocation on namespace offset
    Commit 8651ec01daed ("module: add support for symbol namespaces.")
    broke linking for arm64 defconfig:
    
      | lib/crypto/arc4.o: In function `__ksymtab_arc4_setkey':
      | arc4.c:(___ksymtab+arc4_setkey+0x8): undefined reference to `no symbol'
      | lib/crypto/arc4.o: In function `__ksymtab_arc4_crypt':
      | arc4.c:(___ksymtab+arc4_crypt+0x8): undefined reference to `no symbol'
    
    This is because the dummy initialisation of the 'namespace_offset' field
    in 'struct kernel_symbol' when using EXPORT_SYMBOL on architectures with
    support for PREL32 locations uses an offset from an absolute address (0)
    in an effort to trick 'offset_to_pointer' into behaving as a NOP,
    allowing non-namespaced symbols to be treated in the same way as those
    belonging to a namespace.
    
    Unfortunately, place-relative relocations require a symbol reference
    rather than an absolute value and, although x86 appears to get away with
    this due to placing the kernel text at the top of the address space, it
    almost certainly results in a runtime failure if the kernel is relocated
    dynamically as a result of KASLR.
    
    Rework 'namespace_offset' so that a value of 0, which cannot occur for a
    valid namespaced symbol, indicates that the corresponding symbol does
    not belong to a namespace.
    
    Cc: Matthias Maennich <maennich@google.com>
    Cc: Jessica Yu <jeyu@kernel.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Fixes: 8651ec01daed ("module: add support for symbol namespaces.")
    Reported-by: kbuild test robot <lkp@intel.com>
    Tested-by: Matthias Maennich <maennich@google.com>
    Tested-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Reviewed-by: Matthias Maennich <maennich@google.com>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Jessica Yu <jeyu@kernel.org>

--- include/asm-generic/export.h
+++ include/asm-generic/export.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 .macro __put, val, name
 #ifdef CONFIG_HAVE_ARCH_PREL32_RELOCATIONS
-	.long	\val - ., \name - ., 0 - .
+	.long	\val - ., \name - ., 0
 #elif defined(CONFIG_64BIT)
 	.quad	\val, \name, 0
 #else
