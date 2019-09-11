af67d544850d0a3f9c67c4ae6ca4bec1a110ce9b
Hari Bathini <hbathini@linux.ibm.com>
2019-09-11 20:16:21 +0530

    powerpc/fadump: move internal macros/definitions to a new header
    Though asm/fadump.h is meant to be used by other components dealing
    with FADump, it also has macros/definitions internal to FADump code.
    Move them to a new header file used within FADump code. This also
    makes way for refactoring platform specific FADump code.
    
    Signed-off-by: Hari Bathini <hbathini@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/156821313134.5656.6597770626574392140.stgit@hbathini.in.ibm.com

--- arch/powerpc/kernel/fadump.c
+++ arch/powerpc/kernel/fadump.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/prom.h>
 #include <asm/rtas.h>
 #include <asm/fadump.h>
+#include <asm/fadump-internal.h>
 #include <asm/setup.h>
 
 static struct fw_dump fw_dump;
