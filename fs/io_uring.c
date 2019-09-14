0b7c43ba8908da52c81284a3a7fbbf9a998ce587
Daniel Xu <dxu@dxuuu.xyz>
2019-09-14 14:23:45 -0700

    io_uring: increase IORING_MAX_ENTRIES to 32K
    Some workloads can require far more than 4K oustanding entries. For
    example memcached can have ~300K sockets over ~40 cores. Bumping the max
    to 32K seems to work pretty well.
    
    Reported-by: Dan Melnic <dmm@fb.com>
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -76,7 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "internal.h"
 
-#define IORING_MAX_ENTRIES	4096
+#define IORING_MAX_ENTRIES	32768
 #define IORING_MAX_FIXED_FILES	1024
 
 struct io_uring {
