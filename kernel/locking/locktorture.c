bea4eb3679ce406d39a499dce63af3dc057a26cb
Wolfgang M. Reimer <linuxball@gmail.com>
2019-09-16 16:54:04 +0200

    locking: locktorture: Do not include rwlock.h directly
    Including rwlock.h directly will cause kernel builds to fail
    if CONFIG_PREEMPT_RT is defined. The correct header file
    (rwlock_rt.h OR rwlock.h) will be included by spinlock.h which
    is included by locktorture.c anyway.
    
    Remove the include of linux/rwlock.h.
    
    Signed-off-by: Wolfgang M. Reimer <linuxball@gmail.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Acked-by: Davidlohr Bueso <dbueso@suse.de>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/locking/locktorture.c
+++ kernel/locking/locktorture.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kthread.h>
 #include <linux/sched/rt.h>
 #include <linux/spinlock.h>
-#include <linux/rwlock.h>
 #include <linux/mutex.h>
 #include <linux/rwsem.h>
 #include <linux/smp.h>
