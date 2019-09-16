4fa4ecca5b36b4a1cab547cc14015090629cf088
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-09-16 13:57:38 -0500

    soundwire: intel: add missing headers for cross-compilation
    readl/writel and ioread32 are used without the relevant headers, fix.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20190916185739.32184-2-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/platform_device.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
