ff12b038ad533ee24b85c3cd4f20d281994dbe4a
Arthur Kiyanovski <akiyano@amazon.com>
2019-09-16 14:31:27 +0300

    net: ena: switch to dim algorithm for rx adaptive interrupt moderation
    Use the dim library for the rx adaptive interrupt moderation implementation
    
    Signed-off-by: Arthur Kiyanovski <akiyano@amazon.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/amazon/ena/ena_netdev.h
+++ drivers/net/ethernet/amazon/ena/ena_netdev.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ENA_H
 
 #include <linux/bitops.h>
+#include <linux/dim.h>
 #include <linux/etherdevice.h>
 #include <linux/inetdevice.h>
 #include <linux/interrupt.h>
@@ -154,6 +155,7 @@ struct ena_napi {
 	struct ena_ring *tx_ring;
 	struct ena_ring *rx_ring;
 	u32 qid;
+	struct dim dim;
 };
 
 struct ena_calc_queue_size_ctx {
@@ -281,6 +283,7 @@ struct ena_ring {
 	u32  per_napi_packets;
 	u32  per_napi_bytes;
 	enum ena_intr_moder_level moder_tbl_idx;
+	u16 non_empty_napi_events;
 	struct u64_stats_sync syncp;
 	union {
 		struct ena_stats_tx tx_stats;
