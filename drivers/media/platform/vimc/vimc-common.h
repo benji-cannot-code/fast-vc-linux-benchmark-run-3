d37efd98b47fdb7c5acc3b63c0b6e91fa7cd1bfd
Shuah Khan <skhan@linuxfoundation.org>
2019-09-17 13:35:10 -0300

    media: vimc: move duplicated IS_SRC and IS_SINK to common header
    Move duplicated IS_SRC and IS_SINK dfines to common header. Rename
    them to VIMC_IS_SRC and VIM_IS_SINK.
    
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>
    Acked-by: Helen Koike <helen.koike@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- drivers/media/platform/vimc/vimc-common.h
+++ drivers/media/platform/vimc/vimc-common.h
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VIMC_FRAME_INDEX(lin, col, width, bpp) ((lin * width + col) * bpp)
 
+/* Source and sink pad checks */
+#define VIMC_IS_SRC(pad)	(pad)
+#define VIMC_IS_SINK(pad)	(!(pad))
+
 /**
  * struct vimc_colorimetry_clamp - Adjust colorimetry parameters
  *
