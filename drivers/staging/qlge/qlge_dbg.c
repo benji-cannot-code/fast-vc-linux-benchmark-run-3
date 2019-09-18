05f7dbebe94955658ac9b2364f0297d1db1bba34
Aliasgar Surti <aliasgar.surti500@gmail.com>
2019-09-18 18:46:36 +0530

    staging: qlge: Removed unnecessary variable
    coccicheck reported warning for unnecessary variable used.
    This patch fixes the same by removing the variable and
    returning value directly.
    
    Signed-off-by: Aliasgar Surti <aliasgar.surti500@gmail.com>
    Link: https://lore.kernel.org/r/1568812596-25926-1-git-send-email-aliasgar.surti500@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/qlge/qlge_dbg.c
+++ drivers/staging/qlge/qlge_dbg.c
@@ -390,7 +390,6 @@ static int ql_get_xgmac_regs(struct ql_adapter *qdev, u32 *buf,
 
 static int ql_get_ets_regs(struct ql_adapter *qdev, u32 *buf)
 {
-	int status = 0;
 	int i;
 
 	for (i = 0; i < 8; i++, buf++) {
@@ -403,7 +402,7 @@ static int ql_get_ets_regs(struct ql_adapter *qdev, u32 *buf)
 		*buf = ql_read32(qdev, CNA_ETS);
 	}
 
-	return status;
+	return 0;
 }
 
 static void ql_get_intr_states(struct ql_adapter *qdev, u32 *buf)
