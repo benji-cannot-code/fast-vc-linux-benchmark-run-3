466a6b35b3f12f66dbd89b3c616122366ffb3174
Sean Paul <seanpaul@chromium.org>
2019-09-18 16:07:28 -0400

    drm: Fix kerneldoc and remove unused struct member in self_refresh helper
    Artifacts of previous revisions.
    
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link to v1: https://patchwork.freedesktop.org/patch/msgid/20190917200443.64481-1-sean@poorly.run
    Link: https://patchwork.freedesktop.org/patch/msgid/20190918200734.149876-1-sean@poorly.run
    
    Changes in v2:
    - None

--- drivers/gpu/drm/drm_self_refresh_helper.c
+++ drivers/gpu/drm/drm_self_refresh_helper.c
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct drm_self_refresh_data {
 	struct drm_crtc *crtc;
 	struct delayed_work entry_work;
-	struct drm_atomic_state *save_state;
 	unsigned int entry_delay_ms;
 };
 
