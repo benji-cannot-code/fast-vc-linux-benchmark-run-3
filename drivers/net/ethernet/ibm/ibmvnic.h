113370ebb86fe827a1b4f836bd7dad384e915995
Juliet Kim <julietk@linux.vnet.ibm.com>
2019-09-20 16:11:23 -0400

    net/ibmvnic: prevent more than one thread from running in reset
    The current code allows more than one thread to run in reset. This can
    corrupt struct adapter data. Check adapter->resetting before performing
    a reset, if there is another reset running delay (100 msec) before trying
    again.
    
    Signed-off-by: Juliet Kim <julietk@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ibm/ibmvnic.h
+++ drivers/net/ethernet/ibm/ibmvnic.h
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IBMVNIC_MAX_LTB_SIZE ((1 << (MAX_ORDER - 1)) * PAGE_SIZE)
 #define IBMVNIC_BUFFER_HLEN 500
 
+#define IBMVNIC_RESET_DELAY 100
+
 static const char ibmvnic_priv_flags[][ETH_GSTRING_LEN] = {
 #define IBMVNIC_USE_SERVER_MAXES 0x1
 	"use-server-maxes"
@@ -1078,7 +1080,8 @@ struct ibmvnic_adapter {
 	spinlock_t rwi_lock;
 	struct list_head rwi_list;
 	struct work_struct ibmvnic_reset;
-	bool resetting;
+	struct delayed_work ibmvnic_delayed_reset;
+	unsigned long resetting;
 	bool napi_enabled, from_passive_init;
 
 	bool failover_pending;
