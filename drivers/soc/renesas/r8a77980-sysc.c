d60dda999e8a6cbfdfb649a1a235edffe729857a
Geert Uytterhoeven <geert+renesas@glider.be>
2019-09-20 16:47:05 +0200

    soc: renesas: rcar-sysc: Remove unneeded inclusion of <linux/bug.h>
    No R-Car or RZ/G SYSC driver uses any of the definitions provided by
    <linux/bug.h>, hence there is no need to include this header file.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Simon Horman <horms+renesas@verge.net.au>
    Link: https://lore.kernel.org/r/20190920144705.27394-1-geert+renesas@glider.be

--- drivers/soc/renesas/r8a77980-sysc.c
+++ drivers/soc/renesas/r8a77980-sysc.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/bits.h>
-#include <linux/bug.h>
 #include <linux/kernel.h>
 
 #include <dt-bindings/power/r8a77980-sysc.h>
