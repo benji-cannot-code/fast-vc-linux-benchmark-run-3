80fad9a710e89081605a6ea90ce6f4762552d2f4
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-09-20 11:01:42 +0900

    ASoC: soc-component.h: remove GPL explanation from header
    soc-component.h already has SPDX License, thus, GPL explanation
    is not needed. This patch removes it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/8736grafp5.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/soc-component.h
+++ include/sound/soc-component.h
@@ -4,10 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * soc-component.h
  *
  * Copyright (c) 2019 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
 #ifndef __SOC_COMPONENT_H
 #define __SOC_COMPONENT_H
