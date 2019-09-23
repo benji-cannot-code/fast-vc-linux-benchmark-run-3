d41abf0e782cb8aa248dda132dd800f8e5bb82a8
Mike Rapoport <rppt@linux.ibm.com>
2019-09-23 15:35:28 -0700

    microblaze: switch to generic version of pte allocation
    The microblaze implementation of pte_alloc_one() has a provision to
    allocated PTEs from high memory, but neither CONFIG_HIGHPTE nor pte_map*()
    versions for suitable for HIGHPTE are defined.
    
    Except that, microblaze version of pte_alloc_one() is identical to the
    generic one as well as the implementations of pte_free() and
    pte_free_kernel().
    
    Switch microblaze to use the generic versions of these functions.  Also
    remove pte_free_slow() that is not referenced anywhere in the code.
    
    Link: http://lkml.kernel.org/r/1565690952-32158-1-git-send-email-rppt@linux.ibm.com
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Michal Simek <monstr@monstr.eu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/microblaze/include/asm/pgalloc.h
+++ arch/microblaze/include/asm/pgalloc.h
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cache.h>
 #include <asm/pgtable.h>
 
+#define __HAVE_ARCH_PTE_ALLOC_ONE_KERNEL
+#include <asm-generic/pgalloc.h>
+
 extern void __bad_pte(pmd_t *pmd);
 
 static inline pgd_t *get_pgd(void)
@@ -47,42 +50,6 @@ static inline void free_pgd(pgd_t *pgd)
 #define pmd_alloc_one(mm, address)	({ BUG(); ((pmd_t *)2); })
 
 extern pte_t *pte_alloc_one_kernel(struct mm_struct *mm);
-
-static inline struct page *pte_alloc_one(struct mm_struct *mm)
-{
-	struct page *ptepage;
-
-#ifdef CONFIG_HIGHPTE
-	int flags = GFP_KERNEL | __GFP_ZERO | __GFP_HIGHMEM;
-#else
-	int flags = GFP_KERNEL | __GFP_ZERO;
-#endif
-
-	ptepage = alloc_pages(flags, 0);
-	if (!ptepage)
-		return NULL;
-	if (!pgtable_page_ctor(ptepage)) {
-		__free_page(ptepage);
-		return NULL;
-	}
-	return ptepage;
-}
-
-static inline void pte_free_kernel(struct mm_struct *mm, pte_t *pte)
-{
-	free_page((unsigned long)pte);
-}
-
-static inline void pte_free_slow(struct page *ptepage)
-{
-	__free_page(ptepage);
-}
-
-static inline void pte_free(struct mm_struct *mm, struct page *ptepage)
-{
-	pgtable_page_dtor(ptepage);
-	__free_page(ptepage);
-}
 
 #define __pte_free_tlb(tlb, pte, addr)	pte_free((tlb)->mm, (pte))
 
