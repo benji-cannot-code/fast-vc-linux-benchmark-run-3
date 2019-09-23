ef83bb35d8d67b52f03e94979325eb8f148fcd86
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-09-23 16:22:22 -0300

    perf tools: No need to include internal/lib.h from util/util.h
    That was done just to have users of writen() and readn(), that before
    had their prototypes in util/util.h to get it without having to add an
    include for internal/lib.h, but the right way is to add it and by now
    all places already do it.
    
    Fix a fallout were readlink() was used but unistd.h was being obtained
    by luck thru util.h -> internal/lib.h, now to check why unistd.h is
    being included there...
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-lcnytgrtafey3kwlfog2rzzj@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/util.h
+++ tools/perf/util/util.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stddef.h>
 #include <linux/compiler.h>
 #include <sys/types.h>
-#include <internal/lib.h>
 
 /* General helper functions */
 void usage(const char *err) __noreturn;
