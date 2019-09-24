ca91402032f70fd4715b51cd10429ac5cb0998cf
Arnaldo Carvalho de Melo <acme@redhat.com>
2019-09-24 16:07:59 -0300

    perf tools: Replace needless mmap.h with what is needed, event.h
    The perf_sample struct definition and the event_attr_init() are in
    util/event.h, but some places were getting it thru an otherwise needless
    util/mmap.h header, fix it by including util/event.h directly.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/n/tip-p1anwyjdbbvghrkl9dlxv7h5@git.kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "util/parse-branch-options.h"
 #include "metricgroup.h"
 #include "util/evsel_config.h"
-#include "util/mmap.h"
+#include "util/event.h"
 
 #define MAX_NAME_LEN 100
 
