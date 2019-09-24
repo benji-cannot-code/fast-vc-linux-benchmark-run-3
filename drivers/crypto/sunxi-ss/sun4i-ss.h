78fd403aeeafd9e85fa1ddafecc9a48e15348952
Corentin Labbe <clabbe.montjoie@gmail.com>
2019-09-24 10:08:32 +0200

    crypto: sun4i-ss - enable pm_runtime
    This patch enables power management on the Security System.
    sun4i-ss now depends on PM because it simplify code and prevent some ifdef.
    But this is not a problem since arch maintainer want ARCH_SUNXI to
    depend on PM in the future.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>
    Acked-by: Maxime Ripard <mripard@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/sunxi-ss/sun4i-ss.h
+++ drivers/crypto/sunxi-ss/sun4i-ss.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/scatterlist.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
+#include <linux/pm_runtime.h>
 #include <crypto/md5.h>
 #include <crypto/skcipher.h>
 #include <crypto/sha.h>
@@ -178,6 +179,7 @@ struct sun4i_req_ctx {
 };
 
 int sun4i_hash_crainit(struct crypto_tfm *tfm);
+void sun4i_hash_craexit(struct crypto_tfm *tfm);
 int sun4i_hash_init(struct ahash_request *areq);
 int sun4i_hash_update(struct ahash_request *areq);
 int sun4i_hash_final(struct ahash_request *areq);
