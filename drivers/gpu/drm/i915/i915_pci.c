49f16b6f15db0453ffc65895e5cb9903e0e52dae
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2019-09-24 13:01:52 +0530

    drm/i915: Add Pipe D cursor ctrl register for Gen12
    Currently the offset for PIPE D cursor control register is missing in
    i915_reg.h due to which the cursor plane cannot be enabled for Pipe D.
    This also causes kernel Warning, when a user requests to enable cursor
    plane for PIPE D for Gen 12 platforms.
    
    This patch adds the CURSOR_CTL_D register in the i915_reg.h.
    
    v2: Rebase
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=111640
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    [Lucas: remove extra blank line]
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1569310312-12313-1-git-send-email-ankit.k.nautiyal@intel.com

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -119,6 +119,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		[PIPE_C] = IVB_CURSOR_C_OFFSET, \
 	}
 
+#define TGL_CURSOR_OFFSETS \
+	.cursor_offsets = { \
+		[PIPE_A] = CURSOR_A_OFFSET, \
+		[PIPE_B] = IVB_CURSOR_B_OFFSET, \
+		[PIPE_C] = IVB_CURSOR_C_OFFSET, \
+		[PIPE_D] = TGL_CURSOR_D_OFFSET, \
+	}
+
 #define I9XX_COLORS \
 	.color = { .gamma_lut_size = 256 }
 #define I965_COLORS \
@@ -788,6 +796,7 @@ static const struct intel_device_info intel_elkhartlake_info = {
 		[TRANSCODER_DSI_0] = TRANSCODER_DSI0_OFFSET, \
 		[TRANSCODER_DSI_1] = TRANSCODER_DSI1_OFFSET, \
 	}, \
+	TGL_CURSOR_OFFSETS, \
 	.has_global_mocs = 1, \
 	.display.has_dsb = 1
 
