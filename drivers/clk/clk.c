7bbf162ef5b1e0a9bf05057bba7ec62acb390e4f
Jerome Brunet <jbrunet@baylibre.com>
2019-09-24 14:39:54 +0200

    clk: add terminate callback to clk_ops
    Add a terminate callback to the clk_ops to release the resources
    claimed in .init()
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Link: https://lkml.kernel.org/r/20190924123954.31561-4-jbrunet@baylibre.com
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -3863,6 +3863,7 @@ static void clk_core_evict_parent_cache(struct clk_core *core)
 void clk_unregister(struct clk *clk)
 {
 	unsigned long flags;
+	const struct clk_ops *ops;
 
 	if (!clk || WARN_ON_ONCE(IS_ERR(clk)))
 		return;
@@ -3871,7 +3872,8 @@ void clk_unregister(struct clk *clk)
 
 	clk_prepare_lock();
 
-	if (clk->core->ops == &clk_nodrv_ops) {
+	ops = clk->core->ops;
+	if (ops == &clk_nodrv_ops) {
 		pr_err("%s: unregistered clock: %s\n", __func__,
 		       clk->core->name);
 		goto unlock;
@@ -3883,6 +3885,9 @@ void clk_unregister(struct clk *clk)
 	flags = clk_enable_lock();
 	clk->core->ops = &clk_nodrv_ops;
 	clk_enable_unlock(flags);
+
+	if (ops->terminate)
+		ops->terminate(clk->core->hw);
 
 	if (!hlist_empty(&clk->core->children)) {
 		struct clk_core *child;
