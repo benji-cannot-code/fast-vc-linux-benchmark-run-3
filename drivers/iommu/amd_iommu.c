16210c45e94bbfffe4cc1921044638b3f7224b4b
Joerg Roedel <jroedel@suse.de>
2019-09-25 15:22:59 +0200

    iommu/amd: Lock dev_data in attach/detach code paths
    Make sure that attaching a detaching a device can't race against each
    other and protect the iommu_dev_data with a spin_lock in these code
    paths.
    
    Fixes: 92d420ec028d ("iommu/amd: Relax locking in dma_ops path")
    Reviewed-by: Filippo Sironi <sironi@amazon.de>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/amd_iommu.c
+++ drivers/iommu/amd_iommu.c
@@ -202,6 +202,7 @@ static struct iommu_dev_data *alloc_dev_data(u16 devid)
 	if (!dev_data)
 		return NULL;
 
+	spin_lock_init(&dev_data->lock);
 	dev_data->devid = devid;
 	ratelimit_default_init(&dev_data->rs);
 
@@ -2158,6 +2159,8 @@ static int attach_device(struct device *dev,
 
 	dev_data = get_dev_data(dev);
 
+	spin_lock(&dev_data->lock);
+
 	ret = -EBUSY;
 	if (dev_data->domain != NULL)
 		goto out;
@@ -2200,6 +2203,8 @@ skip_ats_check:
 	domain_flush_complete(domain);
 
 out:
+	spin_unlock(&dev_data->lock);
+
 	spin_unlock_irqrestore(&domain->lock, flags);
 
 	return ret;
@@ -2218,6 +2223,8 @@ static void detach_device(struct device *dev)
 	domain   = dev_data->domain;
 
 	spin_lock_irqsave(&domain->lock, flags);
+
+	spin_lock(&dev_data->lock);
 
 	/*
 	 * First check if the device is still attached. It might already
@@ -2241,6 +2248,8 @@ static void detach_device(struct device *dev)
 	dev_data->ats.enabled = false;
 
 out:
+	spin_unlock(&dev_data->lock);
+
 	spin_unlock_irqrestore(&domain->lock, flags);
 }
 
