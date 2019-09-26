f3f0abff13d22c959d07eceda31f0a1b64ccb8fc
Ajay Singh <ajay.kathat@microchip.com>
2019-09-26 15:14:56 +0000

    staging: wilc1000: move wlan_deinit_locks() in wilc_netdev_cleanup()
    Move deinitialization of lock during the module remove and the
    initialization of lock wilc_cfg80211_init(). This to ensure locks are
    available during module load and gets free during unload.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Link: https://lore.kernel.org/r/20190926151436.27819-2-ajay.kathat@microchip.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
+++ drivers/staging/wilc1000/wilc_wfi_cfgoperations.c
@@ -1803,6 +1803,15 @@ static void wlan_init_locks(struct wilc *wl)
 	init_completion(&wl->txq_thread_started);
 }
 
+void wlan_deinit_locks(struct wilc *wilc)
+{
+	mutex_destroy(&wilc->hif_cs);
+	mutex_destroy(&wilc->rxq_cs);
+	mutex_destroy(&wilc->cfg_cmd_lock);
+	mutex_destroy(&wilc->txq_add_to_head_cs);
+	mutex_destroy(&wilc->vif_mutex);
+}
+
 int wilc_cfg80211_init(struct wilc **wilc, struct device *dev, int io_type,
 		       const struct wilc_hif_func *ops)
 {
@@ -1813,6 +1822,8 @@ int wilc_cfg80211_init(struct wilc **wilc, struct device *dev, int io_type,
 	wl = wilc_create_wiphy(dev);
 	if (!wl)
 		return -EINVAL;
+
+	wlan_init_locks(wl);
 
 	ret = wilc_wlan_cfg_init(wl);
 	if (ret)
@@ -1837,8 +1848,6 @@ int wilc_cfg80211_init(struct wilc **wilc, struct device *dev, int io_type,
 		goto free_hq;
 	}
 
-	wlan_init_locks(wl);
-
 	return 0;
 
 free_hq:
@@ -1848,6 +1857,7 @@ free_cfg:
 	wilc_wlan_cfg_deinit(wl);
 
 free_wl:
+	wlan_deinit_locks(wl);
 	wiphy_unregister(wl->wiphy);
 	wiphy_free(wl->wiphy);
 	return ret;
