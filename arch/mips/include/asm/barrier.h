1b09988f87321c98fe26290cd895f0e50d6f7630
Paul Burton <paul.burton@mips.com>
2019-10-01 21:53:13 +0000

    MIPS: barrier: Clean up __sync() definition
    Implement __sync() using the new __SYNC() infrastructure, which will
    take care of not emitting an instruction for old R3k CPUs that don't
    support it. The only behavioral difference is that __sync() will now
    provide a compiler barrier on these old CPUs, but that seems like
    reasonable behavior anyway.
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: linux-mips@vger.kernel.org
    Cc: Huacai Chen <chenhc@lemote.com>
    Cc: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Cc: linux-kernel@vger.kernel.org

--- arch/mips/include/asm/barrier.h
+++ arch/mips/include/asm/barrier.h
@@ -12,20 +12,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/addrspace.h>
 #include <asm/sync.h>
 
-#ifdef CONFIG_CPU_HAS_SYNC
-#define __sync()				\
-	__asm__ __volatile__(			\
-		".set	push\n\t"		\
-		".set	noreorder\n\t"		\
-		".set	mips2\n\t"		\
-		"sync\n\t"			\
-		".set	pop"			\
-		: /* no output */		\
-		: /* no input */		\
-		: "memory")
-#else
-#define __sync()	do { } while(0)
-#endif
+static inline void __sync(void)
+{
+	asm volatile(__SYNC(full, always) ::: "memory");
+}
 
 static inline void rmb(void)
 {
