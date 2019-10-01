16e02e970a8115a20d9350ca8a1f103625b855a2
Patrick Williams <alpawi@amazon.com>
2019-10-01 11:00:00 -0500

    i2c: pxa: remove unused i2c-slave APIs
    With the i2c-pxa driver migrated to the standard i2c-slave
    APIs, the custom APIs and structures are no longer needed
    or used.  Remove them.
    
    Signed-off-by: Patrick Williams <alpawi@amazon.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>

--- drivers/i2c/busses/i2c-pxa.c
+++ drivers/i2c/busses/i2c-pxa.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/errno.h>
 #include <linux/interrupt.h>
-#include <linux/i2c-pxa.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
