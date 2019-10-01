1074e278d5afb529be880db8081932f0569f6bb1
Paul Burton <paul.burton@mips.com>
2019-10-01 21:53:36 +0000

    MIPS: bitops: Use smp_mb__before_atomic in test_* ops
    Use smp_mb__before_atomic() rather than smp_mb__before_llsc() in
    test_and_set_bit(), test_and_clear_bit() & test_and_change_bit(). The
    _atomic() versions make semantic sense in these cases, and will allow a
    later patch to omit redundant barriers for Loongson3 systems that
    already include a barrier within __test_bit_op().
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>
    Cc: linux-mips@vger.kernel.org
    Cc: Huacai Chen <chenhc@lemote.com>
    Cc: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Cc: linux-kernel@vger.kernel.org

--- arch/mips/include/asm/bitops.h
+++ arch/mips/include/asm/bitops.h
@@ -210,7 +210,7 @@ static inline int test_and_set_bit_lock(unsigned long nr,
 static inline int test_and_set_bit(unsigned long nr,
 	volatile unsigned long *addr)
 {
-	smp_mb__before_llsc();
+	smp_mb__before_atomic();
 	return test_and_set_bit_lock(nr, addr);
 }
 
@@ -229,7 +229,7 @@ static inline int test_and_clear_bit(unsigned long nr,
 	int bit = nr % BITS_PER_LONG;
 	unsigned long res, orig;
 
-	smp_mb__before_llsc();
+	smp_mb__before_atomic();
 
 	if (!kernel_uses_llsc) {
 		res = __mips_test_and_clear_bit(nr, addr);
@@ -266,7 +266,7 @@ static inline int test_and_change_bit(unsigned long nr,
 	int bit = nr % BITS_PER_LONG;
 	unsigned long res, orig;
 
-	smp_mb__before_llsc();
+	smp_mb__before_atomic();
 
 	if (!kernel_uses_llsc) {
 		res = __mips_test_and_change_bit(nr, addr);
