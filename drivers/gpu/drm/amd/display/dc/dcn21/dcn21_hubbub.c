62c864de045f3b8e5b5518ba35a8ccfdc49f48d0
Dmytro Laktyushkin <Dmytro.Laktyushkin@amd.com>
2019-10-02 15:59:23 -0400

    drm/amd/display: correct dcn21 NUM_VMID to 16
    1 vmid limitation only exists for HOSTVM which is a custom
    use case anyway.
    
    Signed-off-by: Dmytro Laktyushkin <Dmytro.Laktyushkin@amd.com>
    Acked-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Reviewed-by: Roman Li <Roman.Li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn21/dcn21_hubbub.c
+++ drivers/gpu/drm/amd/display/dc/dcn21/dcn21_hubbub.c
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef NUM_VMID
 #undef NUM_VMID
 #endif
-#define NUM_VMID 1
+#define NUM_VMID 16
 
 static uint32_t convert_and_clamp(
 	uint32_t wm_ns,
