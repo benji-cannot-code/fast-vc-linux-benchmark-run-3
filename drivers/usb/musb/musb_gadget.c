e97405e8adf8d0adc22ed13e5e5909aa74587653
Saurav Girepunje <saurav.girepunje@gmail.com>
2019-10-02 20:02:45 +0530

    usb: musb: musb_gadget.c: Remove unused variable
    Remove unused variable status from musb_gadget_disable().
    
    Signed-off-by: Saurav Girepunje <saurav.girepunje@gmail.com>
    Link: https://lore.kernel.org/r/20191002143241.GA11615@saurav
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/musb/musb_gadget.c
+++ drivers/usb/musb/musb_gadget.c
@@ -1086,7 +1086,6 @@ static int musb_gadget_disable(struct usb_ep *ep)
 	u8		epnum;
 	struct musb_ep	*musb_ep;
 	void __iomem	*epio;
-	int		status = 0;
 
 	musb_ep = to_musb_ep(ep);
 	musb = musb_ep->musb;
@@ -1119,7 +1118,7 @@ static int musb_gadget_disable(struct usb_ep *ep)
 
 	musb_dbg(musb, "%s", musb_ep->end_point.name);
 
-	return status;
+	return 0;
 }
 
 /*
