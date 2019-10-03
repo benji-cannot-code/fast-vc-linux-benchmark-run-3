08b0bae25e7841f4306cb68afcb3b54efc59dee1
Jakub Kicinski <jakub.kicinski@netronome.com>
2019-10-03 11:18:54 -0700

    net/tls: move TOE-related structures to a separate header
    Move tls_device structure and register/unregister functions
    to a new header to avoid confusion with normal, non-TOE offload.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: John Hurley <john.hurley@netronome.com>
    Reviewed-by: Simon Horman <simon.horman@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/crypto/chelsio/chtls/chtls.h
+++ drivers/crypto/chelsio/chtls/chtls.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <crypto/internal/hash.h>
 #include <linux/tls.h>
 #include <net/tls.h>
+#include <net/tls_toe.h>
 
 #include "t4fw_api.h"
 #include "t4_msg.h"
--- net/tls/tls_main.c
+++ net/tls/tls_main.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/inet_diag.h>
 
 #include <net/tls.h>
+#include <net/tls_toe.h>
 
 MODULE_AUTHOR("Mellanox Technologies");
 MODULE_DESCRIPTION("Transport Layer Security Support");
