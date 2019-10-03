515c20c5a58e1f8d860489faec94e35103f43e21
Colin Ian King <colin.king@canonical.com>
2019-10-03 22:40:49 +0100

    drm/amdgpu: remove redundant variable r and redundant return statement
    There is a return statement that is not reachable and a variable that
    is not used.  Remove them.
    
    Addresses-Coverity: ("Structurally dead code")
    Fixes: de7b45babd9b ("drm/amdgpu: cleanup creating BOs at fixed location (v2)")
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -1652,7 +1652,6 @@ static void amdgpu_ttm_fw_reserve_vram_fini(struct amdgpu_device *adev)
 static int amdgpu_ttm_fw_reserve_vram_init(struct amdgpu_device *adev)
 {
 	uint64_t vram_size = adev->gmc.visible_vram_size;
-	int r;
 
 	adev->fw_vram_usage.va = NULL;
 	adev->fw_vram_usage.reserved_bo = NULL;
@@ -1667,7 +1666,6 @@ static int amdgpu_ttm_fw_reserve_vram_init(struct amdgpu_device *adev)
 					  AMDGPU_GEM_DOMAIN_VRAM,
 					  &adev->fw_vram_usage.reserved_bo,
 					  &adev->fw_vram_usage.va);
-	return r;
 }
 
 /**
