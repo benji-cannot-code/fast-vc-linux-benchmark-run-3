ac06bd03e4c15ee83eb969942fa80b096ece6ec3
Trond Myklebust <trond.myklebust@hammerspace.com>
2019-10-04 17:00:02 -0400

    NFSv3: Clean up timespec encode
    Simplify the struct iattr timestamp encoding by skipping the step of
    an intermediate struct timespec.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -870,7 +870,7 @@ struct nfs3_sattrargs {
 	struct nfs_fh *		fh;
 	struct iattr *		sattr;
 	unsigned int		guard;
-	struct timespec		guardtime;
+	struct timespec64	guardtime;
 };
 
 struct nfs3_diropargs {
