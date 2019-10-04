b93a85c5784d428022ab3401aa865ef0e2d4efe0
Jani Nikula <jani.nikula@intel.com>
2019-10-04 15:20:17 +0300

    drm/i915/vga: rename intel_vga_msr_write() to intel_vga_reset_io_mem()
    Rename the function per Ville's suggestion. No functional changes.
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Suggested-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191004122019.12009-1-jani.nikula@intel.com

--- drivers/gpu/drm/i915/display/intel_vga.h
+++ drivers/gpu/drm/i915/display/intel_vga.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct drm_i915_private;
 
-void intel_vga_msr_write(struct drm_i915_private *i915);
+void intel_vga_reset_io_mem(struct drm_i915_private *i915);
 void intel_vga_disable(struct drm_i915_private *i915);
 void intel_vga_redisable(struct drm_i915_private *i915);
 void intel_vga_redisable_power_on(struct drm_i915_private *i915);
