712fd8e70cf53a7b7d925b58f5510c74b01fe914
YueHaibing <yuehaibing@huawei.com>
2019-10-06 18:46:31 +0800

    ASoc: tas2770: Fix build error without GPIOLIB
    If GPIOLIB is not set, building fails:
    
    sound/soc/codecs/tas2770.c: In function tas2770_reset:
    sound/soc/codecs/tas2770.c:38:3: error: implicit declaration of function gpiod_set_value_cansleep; did you mean gpio_set_value_cansleep? [-Werror=implicit-function-declaration]
       gpiod_set_value_cansleep(tas2770->reset_gpio, 0);
       ^~~~~~~~~~~~~~~~~~~~~~~~
       gpio_set_value_cansleep
    sound/soc/codecs/tas2770.c: In function tas2770_i2c_probe:
    sound/soc/codecs/tas2770.c:749:24: error: implicit declaration of function devm_gpiod_get_optional; did you mean devm_regulator_get_optional? [-Werror=implicit-function-declaration]
      tas2770->reset_gpio = devm_gpiod_get_optional(tas2770->dev,
                            ^~~~~~~~~~~~~~~~~~~~~~~
                            devm_regulator_get_optional
    sound/soc/codecs/tas2770.c:751:13: error: GPIOD_OUT_HIGH undeclared (first use in this function); did you mean GPIOF_INIT_HIGH?
                 GPIOD_OUT_HIGH);
                 ^~~~~~~~~~~~~~
                 GPIOF_INIT_HIGH
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Fixes: 1a476abc723e ("tas2770: add tas2770 smart PA kernel driver")
    Suggested-by: Ladislav Michl <ladis@linux-mips.org>
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Link: https://lore.kernel.org/r/20191006104631.60608-1-yuehaibing@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/tas2770.c
+++ sound/soc/codecs/tas2770.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pm.h>
 #include <linux/i2c.h>
 #include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
 #include <linux/firmware.h>
