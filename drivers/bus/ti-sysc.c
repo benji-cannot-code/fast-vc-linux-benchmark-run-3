d0e59df265ed18bfbe241f2e70d217982e511fc0
Tero Kristo <t-kristo@ti.com>
2019-10-07 15:29:29 +0300

    bus: ti-sysc: re-order reset and main clock controls
    The main clocks and reset controls have a hardware level dependency,
    where one can't transition state without the other one transitioning.
    Because we don't have the dependency implemented in software, we must
    ensure the ordering of these two is done properly; they way this is
    handled is that clocks transition on software level without delay,
    and the status is only polled on reset side. Because of this, we must
    re-order the main clock and reset handling on the ti-sysc driver.
    
    Signed-off-by: Tero Kristo <t-kristo@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- drivers/bus/ti-sysc.c
+++ drivers/bus/ti-sysc.c
@@ -1033,8 +1033,6 @@ static int __maybe_unused sysc_runtime_resume_legacy(struct device *dev,
 	struct ti_sysc_platform_data *pdata;
 	int error;
 
-	reset_control_deassert(ddata->rsts);
-
 	pdata = dev_get_platdata(ddata->dev);
 	if (!pdata)
 		return 0;
@@ -1046,6 +1044,8 @@ static int __maybe_unused sysc_runtime_resume_legacy(struct device *dev,
 	if (error)
 		dev_err(dev, "%s: could not enable: %i\n",
 			__func__, error);
+
+	reset_control_deassert(ddata->rsts);
 
 	return 0;
 }
@@ -1100,8 +1100,6 @@ static int __maybe_unused sysc_runtime_resume(struct device *dev)
 
 	sysc_clkdm_deny_idle(ddata);
 
-	reset_control_deassert(ddata->rsts);
-
 	if (sysc_opt_clks_needed(ddata)) {
 		error = sysc_enable_opt_clocks(ddata);
 		if (error)
@@ -1111,6 +1109,8 @@ static int __maybe_unused sysc_runtime_resume(struct device *dev)
 	error = sysc_enable_main_clocks(ddata);
 	if (error)
 		goto err_opt_clocks;
+
+	reset_control_deassert(ddata->rsts);
 
 	if (ddata->legacy_mode) {
 		error = sysc_runtime_resume_legacy(dev, ddata);
