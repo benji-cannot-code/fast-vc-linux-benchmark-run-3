e881ff56c0ee46157aee7ef8f953358386d25353
Javier F. Arias <jarias.linux@gmail.com>
2019-10-06 22:32:05 -0500

    staging: rtl8723bs: Switch constant place in test
    Switch constant place as it should be on the right side of the test.
    Issue found by checkpatch.
    
    Signed-off-by: Javier F. Arias <jarias.linux@gmail.com>
    Link: https://lore.kernel.org/r/20191007033202.45czxuochtylkddf@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_xmit.c
+++ drivers/staging/rtl8723bs/core/rtw_xmit.c
@@ -2993,7 +2993,7 @@ int rtw_xmit_thread(void *context)
 	do {
 		err = rtw_hal_xmit_thread_handler(padapter);
 		flush_signals_thread();
-	} while (_SUCCESS == err);
+	} while (err == _SUCCESS);
 
 	complete(&padapter->xmitpriv.terminate_xmitthread_comp);
 
