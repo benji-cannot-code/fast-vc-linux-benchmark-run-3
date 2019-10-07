844023e3e558e7fc731e2de264a67efdd718e0f3
Sam Ravnborg <sam@ravnborg.org>
2019-10-07 19:12:23 +0200

    drm_dp_cec: drop use of drmP.h
    drmP.h is deprecated and will be deleted.
    Replace use with proper header.
    
    Divide header includes in blocks while touching these.
    
    Build tested with various archtectures and configs.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Fixes: ae85b0df124f6928 ("drm_dp_cec: add connector info support.")
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Acked-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Cc: Dariusz Marcinkiewicz <darekm@google.com>
    Cc: Ben Skeggs <bskeggs@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191007171224.1581-2-sam@ravnborg.org

--- drivers/gpu/drm/drm_dp_cec.c
+++ drivers/gpu/drm/drm_dp_cec.c
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <drm/drm_connector.h>
-#include <drm/drm_dp_helper.h>
-#include <drm/drmP.h>
+
 #include <media/cec.h>
+
+#include <drm/drm_connector.h>
+#include <drm/drm_device.h>
+#include <drm/drm_dp_helper.h>
 
 /*
  * Unfortunately it turns out that we have a chicken-and-egg situation
