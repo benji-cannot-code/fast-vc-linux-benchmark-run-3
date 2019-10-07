2825331d01cc250cdb49b82519eca18e755bae6e
Jiri Olsa <jolsa@kernel.org>
2019-10-07 14:53:11 +0200

    libperf: Adopt perf_mmap__mmap_len() function from tools/perf
    Move perf_mmap__mmap_len() from tools/perf wto libperf, it will be used
    in the following patches. And rename the existing perf's function to
    mmap__mmap_len().
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Michael Petlan <mpetlan@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lore.kernel.org/lkml/20191007125344.14268-4-jolsa@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/lib/mmap.c
+++ tools/perf/lib/mmap.c
@@ -1,10 +1,16 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 #include <internal/mmap.h>
+#include <internal/lib.h>
 
 void perf_mmap__init(struct perf_mmap *map, bool overwrite)
 {
 	map->fd = -1;
 	map->overwrite = overwrite;
 	refcount_set(&map->refcnt, 0);
+}
+
+size_t perf_mmap__mmap_len(struct perf_mmap *map)
+{
+	return map->mask + 1 + page_size;
 }
