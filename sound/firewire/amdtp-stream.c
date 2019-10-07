7a167f6a68d48b4f60b47acb95d7a98d27209471
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-10-07 20:05:16 +0900

    ALSA: firewire-lib: add a member into AMDTP domain for events per period
    In IEC 61883-6, it's called as 'event' what has presentation time
    represented by timestamp in CIP header. Although the ratio of the number
    of event against the number of data block is different depending on
    event data type represented by the specific field in CIP header, it's
    just one in the most cases supported by ALSA IEC 61883-1/6 engine.
    
    In 1394 OHCI specification, applications can schedule hardware IRQ
    by configuring descriptor with IRQ flag for packet against each
    isochronous cycle. For future commit, I use the hardware IRQ for
    isoc IT context to acknowledge the elapse of PCM period for both
    playback/capture directions on AMDTP streams in the same domain.
    
    This commit is a preparation for the above idea. This commit adds
    a member into AMDTP domain structure to record the number of PCM frames.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Link: https://lore.kernel.org/r/20191007110532.30270-2-o-takashi@sakamocchi.jp
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/amdtp-stream.c
+++ sound/firewire/amdtp-stream.c
@@ -1144,6 +1144,8 @@ int amdtp_domain_init(struct amdtp_domain *d)
 {
 	INIT_LIST_HEAD(&d->streams);
 
+	d->events_per_period = 0;
+
 	return 0;
 }
 EXPORT_SYMBOL_GPL(amdtp_domain_init);
@@ -1222,5 +1224,7 @@ void amdtp_domain_stop(struct amdtp_domain *d)
 
 		amdtp_stream_stop(s);
 	}
+
+	d->events_per_period = 0;
 }
 EXPORT_SYMBOL_GPL(amdtp_domain_stop);
