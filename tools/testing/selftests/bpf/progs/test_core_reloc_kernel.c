93ed419fd5268df1a3a8b038a92c68bfb015c308
Andrii Nakryiko <andriin@fb.com>
2019-10-08 10:59:38 -0700

    selftests/bpf: Adjust CO-RE reloc tests for new bpf_core_read() macro
    To allow adding a variadic BPF_CORE_READ macro with slightly different
    syntax and semantics, define CORE_READ in CO-RE reloc tests, which is
    a thin wrapper around low-level bpf_core_read() macro, which in turn is
    just a wrapper around bpf_probe_read().
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20191008175942.1769476-4-andriin@fb.com

--- tools/testing/selftests/bpf/progs/test_core_reloc_kernel.c
+++ tools/testing/selftests/bpf/progs/test_core_reloc_kernel.c
@@ -18,6 +18,8 @@ struct task_struct {
 	int tgid;
 };
 
+#define CORE_READ(dst, src) bpf_core_read(dst, sizeof(*(dst)), src)
+
 SEC("raw_tracepoint/sys_enter")
 int test_core_kernel(void *ctx)
 {
@@ -25,8 +27,8 @@ int test_core_kernel(void *ctx)
 	uint64_t pid_tgid = bpf_get_current_pid_tgid();
 	int pid, tgid;
 
-	if (BPF_CORE_READ(&pid, &task->pid) ||
-	    BPF_CORE_READ(&tgid, &task->tgid))
+	if (CORE_READ(&pid, &task->pid) ||
+	    CORE_READ(&tgid, &task->tgid))
 		return 1;
 
 	/* validate pid + tgid matches */
