9e4f862906fcb811f217200e5d68a2640bbd7bca
Rob Herring <robh@kernel.org>
2019-10-08 14:41:55 -0500

    xen: Stop abusing DT of_dma_configure API
    As the removed comments say, these aren't DT based devices.
    of_dma_configure() is going to stop allowing a NULL DT node and calling
    it will no longer work.
    
    The comment is also now out of date as of commit 9ab91e7c5c51 ("arm64:
    default to the direct mapping in get_arch_dma_ops"). Direct mapping
    is now the default rather than dma_dummy_ops.
    
    According to Stefano and Oleksandr, the only other part needed is
    setting the DMA masks and there's no reason to restrict the masks to
    32-bits. So set the masks to 64 bits.
    
    Cc: Robin Murphy <robin.murphy@arm.com>
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Cc: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Juergen Gross <jgross@suse.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: xen-devel@lists.xenproject.org
    Signed-off-by: Rob Herring <robh@kernel.org>
    Acked-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- drivers/xen/gntdev.c
+++ drivers/xen/gntdev.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) "xen:" KBUILD_MODNAME ": " fmt
 
+#include <linux/dma-mapping.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -35,9 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/highmem.h>
 #include <linux/refcount.h>
-#ifdef CONFIG_XEN_GRANT_DMA_ALLOC
-#include <linux/of_device.h>
-#endif
 
 #include <xen/xen.h>
 #include <xen/grant_table.h>
@@ -626,14 +624,7 @@ static int gntdev_open(struct inode *inode, struct file *flip)
 	flip->private_data = priv;
 #ifdef CONFIG_XEN_GRANT_DMA_ALLOC
 	priv->dma_dev = gntdev_miscdev.this_device;
-
-	/*
-	 * The device is not spawn from a device tree, so arch_setup_dma_ops
-	 * is not called, thus leaving the device with dummy DMA ops.
-	 * Fix this by calling of_dma_configure() with a NULL node to set
-	 * default DMA ops.
-	 */
-	of_dma_configure(priv->dma_dev, NULL, true);
+	dma_coerce_mask_and_coherent(priv->dma_dev, DMA_BIT_MASK(64));
 #endif
 	pr_debug("priv %p\n", priv);
 
