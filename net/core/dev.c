f3a92d2353b1a50df5342689f9eb40db77e98ca6
Eric Dumazet <edumazet@google.com>
2019-10-08 14:20:34 -0700

    Revert "tun: call dev_get_valid_name() before register_netdevice()"
    This reverts commit 0ad646c81b2182f7fa67ec0c8c825e0ee165696d.
    
    As noticed by Jakub, this is no longer needed after
    commit 11fc7d5a0a2d ("tun: fix memory leak in error path")
    
    This no longer exports dev_get_valid_name() for the exclusive
    use of tun driver.
    
    Suggested-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -1250,8 +1250,8 @@ int dev_alloc_name(struct net_device *dev, const char *name)
 }
 EXPORT_SYMBOL(dev_alloc_name);
 
-int dev_get_valid_name(struct net *net, struct net_device *dev,
-		       const char *name)
+static int dev_get_valid_name(struct net *net, struct net_device *dev,
+			      const char *name)
 {
 	BUG_ON(!net);
 
@@ -1267,7 +1267,6 @@ int dev_get_valid_name(struct net *net, struct net_device *dev,
 
 	return 0;
 }
-EXPORT_SYMBOL(dev_get_valid_name);
 
 /**
  *	dev_change_name - change name of a device
