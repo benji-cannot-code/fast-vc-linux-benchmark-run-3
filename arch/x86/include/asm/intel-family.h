99f861836e0f8761b6f398eace35d696f2f52940
Kan Liang <kan.liang@linux.intel.com>
2019-10-08 08:50:02 -0700

    x86/cpu: Add Comet Lake to the Intel CPU models header
    Comet Lake is the new 10th Gen Intel processor. Add two new CPU model
    numbers to the Intel family list.
    
    The CPU model numbers are not published in the SDM yet but they come
    from an authoritative internal source.
    
     [ bp: Touch up commit message. ]
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Cc: ak@linux.intel.com
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: x86-ml <x86@kernel.org>
    Link: https://lkml.kernel.org/r/1570549810-25049-2-git-send-email-kan.liang@linux.intel.com

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -84,6 +84,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define INTEL_FAM6_TIGERLAKE_L		0x8C
 #define INTEL_FAM6_TIGERLAKE		0x8D
 
+#define INTEL_FAM6_COMETLAKE		0xA5
+#define INTEL_FAM6_COMETLAKE_L		0xA6
+
 /* "Small Core" Processors (Atom) */
 
 #define INTEL_FAM6_ATOM_BONNELL		0x1C /* Diamondville, Pineview */
