4314c574b2b2c3b584222a972af5a87f3b67bd30
Johannes Berg <johannes.berg@intel.com>
2019-10-08 17:43:21 +0200

    um: virtio_uml: Disallow modular build
    This driver *can* be a module, but then its parameters (socket path)
    are untrusted data from inside the VM, and that isn't allowed. Allow
    the code to only be built-in to avoid that.
    
    Fixes: 5d38f324993f ("um: drivers: Add virtio vhost-user driver")
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Acked-by: Anton Ivanov <anton.ivanov@cambridgegreys.co.uk>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/drivers/virtio_uml.c
+++ arch/um/drivers/virtio_uml.c
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright(c) 2019 Intel Corporation
  *
- * This module allows virtio devices to be used over a vhost-user socket.
+ * This driver allows virtio devices to be used over a vhost-user socket.
  *
  * Guest devices can be instantiated by kernel module or command line
  * parameters. One device will be created for each parameter. Syntax:
  *
- *		[virtio_uml.]device=<socket>:<virtio_id>[:<platform_id>]
+ *		virtio_uml.device=<socket>:<virtio_id>[:<platform_id>]
  * where:
  *		<socket>	:= vhost-user socket path to connect
  *		<virtio_id>	:= virtio device id (as in virtio_ids.h)
