eec81143b70b49c46ec1e62b24e7f27bf682a3ba
Fabio Estevam <festevam@gmail.com>
2019-10-08 10:13:38 -0700

    Input: pixcir_i2c_ts - remove unneeded gpio.h header file
    The touchscreen device is a GPIO consumer, not a GPIO controller,
    so there is no need to include <linux/gpio.h>.
    
    Remove the unneeded header file.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Reviewed-by: Roger Quadros <rogerq@ti.com>
    Tested-by: Michal Vokáč <michal.vokac@ysoft.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/pixcir_i2c_ts.c
+++ drivers/input/touchscreen/pixcir_i2c_ts.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input.h>
 #include <linux/input/mt.h>
 #include <linux/input/touchscreen.h>
-#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
 #include <linux/of_device.h>
 #include <linux/platform_data/pixcir_i2c_ts.h>
