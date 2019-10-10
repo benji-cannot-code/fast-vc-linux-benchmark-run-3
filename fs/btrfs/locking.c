9aded6ddf399f235fab9330ec3549fb7824a30e7
David Sterba <dsterba@suse.com>
2019-10-10 23:31:19 +0200

    btrfs: set blocking_writers directly, no increment or decrement
    The increment and decrement was inherited from previous version that
    used atomics, switched in commit 06297d8cefca ("btrfs: switch
    extent_buffer blocking_writers from atomic to int"). The only possible
    values are 0 and 1 so we can set them directly.
    
    The generated assembly (gcc 9.x) did the direct value assignment in
    btrfs_set_lock_blocking_write (asm diff after change in 06297d8cefca):
    
         5d:   test   %eax,%eax
         5f:   je     62 <btrfs_set_lock_blocking_write+0x22>
         61:   retq
    
      -  62:   lock incl 0x44(%rdi)
      -  66:   add    $0x50,%rdi
      -  6a:   jmpq   6f <btrfs_set_lock_blocking_write+0x2f>
    
      +  62:   movl   $0x1,0x44(%rdi)
      +  69:   add    $0x50,%rdi
      +  6d:   jmpq   72 <btrfs_set_lock_blocking_write+0x32>
    
    The part in btrfs_tree_unlock did a decrement because
    BUG_ON(blockers > 1) is probably not a strong hint for the compiler, but
    otherwise the output looks safe:
    
      - lock decl 0x44(%rdi)
    
      + sub    $0x1,%eax
      + mov    %eax,0x44(%rdi)
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/locking.c
+++ fs/btrfs/locking.c
@@ -110,7 +110,7 @@ void btrfs_set_lock_blocking_write(struct extent_buffer *eb)
 	if (eb->blocking_writers == 0) {
 		btrfs_assert_spinning_writers_put(eb);
 		btrfs_assert_tree_locked(eb);
-		eb->blocking_writers++;
+		eb->blocking_writers = 1;
 		write_unlock(&eb->lock);
 	}
 }
@@ -306,7 +306,7 @@ void btrfs_tree_unlock(struct extent_buffer *eb)
 
 	if (blockers) {
 		btrfs_assert_no_spinning_writers(eb);
-		eb->blocking_writers--;
+		eb->blocking_writers = 0;
 		/*
 		 * We need to order modifying blocking_writers above with
 		 * actually waking up the sleepers to ensure they see the
