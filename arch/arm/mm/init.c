fa227c628234905523eeab6324f23436c624ccdb
Ben Dooks (Codethink) <ben.dooks@codethink.co.uk>
2019-10-11 13:51:43 +0100

    ARM: 8917/1: mm: include <asm/set_memory.h>
    The definitions of set_kernel_text_rw() and
    set_kernel_text_ro() are in <asm/set_memory.h>
    but this is not included in init.c which defines
    these. Silence the following warnings by including
    the <asm/set_memory.h> header.
    
    arch/arm/mm/init.c:669:6: warning: symbol 'set_kernel_text_rw' was not declared. Should it be static?
    arch/arm/mm/init.c:678:6: warning: symbol 'set_kernel_text_ro' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/prom.h>
 #include <asm/sections.h>
 #include <asm/setup.h>
+#include <asm/set_memory.h>
 #include <asm/system_info.h>
 #include <asm/tlb.h>
 #include <asm/fixmap.h>
