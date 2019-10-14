c1308bb8e30cc079fe5f15dedef60cd81fc9a588
Linus Walleij <linus.walleij@linaro.org>
2019-10-14 13:11:52 +0200

    pinctrl: nomadik: Drop support for latent IRQ
    The latent IRQs are IRQs that have occurred when the system
    was down in deep sleep and the GPIO block was powered off.
    The PRCMU (power reset and control unit) knows which GPIO
    line offset fired an IRQ to wake the system up (if so
    desired) and this second IRQ was used to replay the action
    when the system came back online after suspend().
    
    This is now known to be the wrong approach to solve this
    problem: in a patch series Lina Iyer has suggested to
    instead make it possible to model the IRQs as hierarchical
    with double parents.
    
    Also the current device trees do not contain the right
    information to make this code work, the latent IRQ is not
    specified nowadays giving noise like this in the console:
    
    [    0.612168] gpio 8012e000.gpio: IRQ index 1 not found
    [    0.622523] gpio 8012e080.gpio: IRQ index 1 not found
    
    Let's delete the latent IRQ code and reimplement it properly
    when we need it.
    
    Cc: Ulf Hansson <ulf.hansson@linaro.org>
    Cc: Lina Iyer <ilina@codeaurora.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20191014111154.9731-1-linus.walleij@linaro.org

--- drivers/pinctrl/nomadik/pinctrl-nomadik.c
+++ drivers/pinctrl/nomadik/pinctrl-nomadik.c
@@ -250,8 +250,6 @@ struct nmk_gpio_chip {
 	struct clk *clk;
 	unsigned int bank;
 	unsigned int parent_irq;
-	int latent_parent_irq;
-	u32 (*get_latent_status)(unsigned int bank);
 	void (*set_ioforce)(bool enable);
 	spinlock_t lock;
 	bool sleepmode;
@@ -833,15 +831,6 @@ static void nmk_gpio_irq_handler(struct irq_desc *desc)
 	__nmk_gpio_irq_handler(desc, status);
 }
 
-static void nmk_gpio_latent_irq_handler(struct irq_desc *desc)
-{
-	struct gpio_chip *chip = irq_desc_get_handler_data(desc);
-	struct nmk_gpio_chip *nmk_chip = gpiochip_get_data(chip);
-	u32 status = nmk_chip->get_latent_status(nmk_chip->bank);
-
-	__nmk_gpio_irq_handler(desc, status);
-}
-
 /* I/O Functions */
 
 static int nmk_gpio_get_dir(struct gpio_chip *chip, unsigned offset)
@@ -1105,7 +1094,6 @@ static int nmk_gpio_probe(struct platform_device *dev)
 	struct nmk_gpio_chip *nmk_chip;
 	struct gpio_chip *chip;
 	struct irq_chip *irqchip;
-	int latent_irq;
 	bool supports_sleepmode;
 	int irq;
 	int ret;
@@ -1126,15 +1114,11 @@ static int nmk_gpio_probe(struct platform_device *dev)
 	if (irq < 0)
 		return irq;
 
-	/* It's OK for this IRQ not to be present */
-	latent_irq = platform_get_irq(dev, 1);
-
 	/*
 	 * The virt address in nmk_chip->addr is in the nomadik register space,
 	 * so we can simply convert the resource address, without remapping
 	 */
 	nmk_chip->parent_irq = irq;
-	nmk_chip->latent_parent_irq = latent_irq;
 	nmk_chip->sleepmode = supports_sleepmode;
 	spin_lock_init(&nmk_chip->lock);
 
@@ -1195,11 +1179,6 @@ static int nmk_gpio_probe(struct platform_device *dev)
 				     irqchip,
 				     nmk_chip->parent_irq,
 				     nmk_gpio_irq_handler);
-	if (nmk_chip->latent_parent_irq > 0)
-		gpiochip_set_chained_irqchip(chip,
-					     irqchip,
-					     nmk_chip->latent_parent_irq,
-					     nmk_gpio_latent_irq_handler);
 
 	dev_info(&dev->dev, "at address %p\n", nmk_chip->addr);
 
