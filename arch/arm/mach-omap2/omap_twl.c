06236ab8518570cb9cd045b7baf8c375e4dacc05
Tony Lindgren <tony@atomide.com>
2019-10-16 07:37:06 -0700

    ARM: OMAP2+: Configure voltage controller for cpcap
    We can configure voltage controller for cpcap with the data available in
    Motorola Mapphone Android Linux kernel. Let's add it so we can have droid4
    behave the same way for voltage controller as other omap4 devices and save
    some power when idle.
    
    Note that we're now using high-speed i2c mode, looks like the Motorola
    kernel had a typo using 0x200 instead of 200 for the timings which may
    caused it to not work properly.
    
    Also note that in the long run, this should just become dts data for a
    voltage controller device driver. But let's get things working first to
    make it possible to test further changes easily.
    
    Cc: Merlijn Wajer <merlijn@wizzup.org>
    Cc: Pavel Machek <pavel@ucw.cz>
    Cc: Sebastian Reichel <sre@kernel.org>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap_twl.c
+++ arch/arm/mach-omap2/omap_twl.c
@@ -37,11 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP4_VDD_CORE_SR_VOLT_REG	0x61
 #define OMAP4_VDD_CORE_SR_CMD_REG	0x62
 
-#define OMAP4_VP_CONFIG_ERROROFFSET	0x00
-#define OMAP4_VP_VSTEPMIN_VSTEPMIN	0x01
-#define OMAP4_VP_VSTEPMAX_VSTEPMAX	0x04
-#define OMAP4_VP_VLIMITTO_TIMEOUT_US	200
-
 static bool is_offset_valid;
 static u8 smps_offset;
 
