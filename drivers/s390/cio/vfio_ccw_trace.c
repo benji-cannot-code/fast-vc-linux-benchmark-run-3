16f1c7649c242938d9c36e5b8bddfa164f1843be
Eric Farman <farman@linux.ibm.com>
2019-10-16 16:20:40 +0200

    vfio-ccw: Rework the io_fctl trace
    Using __field_struct for the schib is convenient, but it doesn't
    appear to let us filter based on any of the schib elements.
    Specifying the full schid or any element within it results
    in various errors by the parser.  So, expand that out to its
    component elements, so we can limit the trace to a single device.
    
    While we are at it, rename this trace to the function name, so we
    remember what is being traced instead of an abstract reference to the
    function control bit of the SCSW.
    
    Signed-off-by: Eric Farman <farman@linux.ibm.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Message-Id: <20191016142040.14132-5-farman@linux.ibm.com>
    Acked-by: Halil Pasic <pasic@linux.ibm.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- drivers/s390/cio/vfio_ccw_trace.c
+++ drivers/s390/cio/vfio_ccw_trace.c
@@ -12,4 +12,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 EXPORT_TRACEPOINT_SYMBOL(vfio_ccw_fsm_async_request);
 EXPORT_TRACEPOINT_SYMBOL(vfio_ccw_fsm_event);
-EXPORT_TRACEPOINT_SYMBOL(vfio_ccw_io_fctl);
+EXPORT_TRACEPOINT_SYMBOL(vfio_ccw_fsm_io_request);
