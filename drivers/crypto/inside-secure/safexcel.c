f0f14145c147ba2a05647e179b40fd879cad1983
Pascal van Leeuwen <pascalvanl@gmail.com>
2019-10-17 17:49:09 +0200

    crypto: inside-secure - Made locally used safexcel_pci_remove() static
    safexcel_pci_remove() is only used locally in the module and not exported,
    so added a static function specifier.
    This fixes a sparse issue reported by Ben Dooks.
    
    Fixes: 625f269a5a7a ("crypto: inside-secure - add support for...")
    Signed-off-by: Pascal van Leeuwen <pvanleeuwen@verimatrix.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/inside-secure/safexcel.c
+++ drivers/crypto/inside-secure/safexcel.c
@@ -1881,7 +1881,7 @@ static int safexcel_pci_probe(struct pci_dev *pdev,
 	return rc;
 }
 
-void safexcel_pci_remove(struct pci_dev *pdev)
+static void safexcel_pci_remove(struct pci_dev *pdev)
 {
 	struct safexcel_crypto_priv *priv = pci_get_drvdata(pdev);
 	int i;
