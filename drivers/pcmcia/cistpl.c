c8499b2ce4322246dba110d457b073046ebc9de8
Ben Dooks (Codethink) <ben.dooks@codethink.co.uk>
2019-10-17 12:44:47 +0100

    pcmcia: include <pcmcia/ds.h> for pcmcia_parse_tuple
    Include <pcmcia/ds.h> for pcmcia_parse_tuple declaration
    to fix the following sparse warning:
    
    drivers/pcmcia/cistpl.c:1287:5: warning: symbol 'pcmcia_parse_tuple' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Link: https://lore.kernel.org/r/20191017114447.20455-1-ben.dooks@codethink.co.uk
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pcmcia/cistpl.c
+++ drivers/pcmcia/cistpl.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <pcmcia/ss.h>
 #include <pcmcia/cisreg.h>
 #include <pcmcia/cistpl.h>
+#include <pcmcia/ds.h>
 #include "cs_internal.h"
 
 static const u_char mantissa[] = {
