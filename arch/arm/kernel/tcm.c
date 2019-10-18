9a70d3c1a8d2ad397a273e047028df0b934e5f91
Ben Dooks <ben-linux@fluff.org>
2019-10-18 10:54:17 +0100

    ARM: 8925/1: tcm: include <asm/tcm.h> for missing declarations
    The arch/arm/kernel/tcm.c should include <asm/tcm.h>
    for declarations of tcm_alloc, tcm_free and other
    functions. Fixes the following sparse warnings:
    
    arch/arm/kernel/tcm.c:74:6: warning: symbol 'tcm_alloc' was not declared. Should it be static?
    arch/arm/kernel/tcm.c:92:6: warning: symbol 'tcm_free' was not declared. Should it be static?
    arch/arm/kernel/tcm.c:98:6: warning: symbol 'tcm_dtcm_present' was not declared. Should it be static?
    arch/arm/kernel/tcm.c:104:6: warning: symbol 'tcm_itcm_present' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/kernel/tcm.c
+++ arch/arm/kernel/tcm.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/memory.h>
 #include <asm/system_info.h>
 #include <asm/traps.h>
+#include <asm/tcm.h>
 
 #define TCMTR_FORMAT_MASK	0xe0000000U
 
