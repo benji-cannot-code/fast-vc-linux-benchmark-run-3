b96b3e75aa0db56af3519da5134a28186c1437c7
Nagarjuna Kristam <nkristam@nvidia.com>
2019-10-18 15:08:07 +0530

    phy: tegra: xusb: Add usb3 port fake support on Tegra210
    On Tegra210, usb2 only otg/peripheral ports dont work in device mode.
    They need an assosciated usb3 port to work in device mode. Identify
    an unused usb3 port and assign it as a fake USB3 port to USB2 only
    port whose mode is otg/peripheral.
    
    Based on work by BH Hsieh <bhsieh@nvidia.com>.
    
    Signed-off-by: Nagarjuna Kristam <nkristam@nvidia.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/tegra/xusb.h
+++ drivers/phy/tegra/xusb.h
@@ -292,6 +292,7 @@ struct tegra_xusb_usb2_port {
 	struct regulator *supply;
 	enum usb_dr_mode mode;
 	bool internal;
+	int usb3_port_fake;
 };
 
 static inline struct tegra_xusb_usb2_port *
@@ -390,6 +391,7 @@ struct tegra_xusb_padctl_soc {
 
 	const char * const *supply_names;
 	unsigned int num_supplies;
+	bool need_fake_usb3_port;
 };
 
 struct tegra_xusb_padctl {
