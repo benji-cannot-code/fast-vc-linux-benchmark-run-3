1602e890f8e47998916ef3b5f3a4edd7cd7dadc1
Christoph Hellwig <hch@lst.de>
2019-10-19 09:09:42 -0700

    xfs: also call xfs_file_iomap_end_delalloc for zeroing operations
    There is no reason not to punch out stale delalloc blocks for zeroing
    operations, as they otherwise behave exactly like normal writes.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -1146,7 +1146,8 @@ xfs_file_iomap_end(
 	unsigned		flags,
 	struct iomap		*iomap)
 {
-	if ((flags & IOMAP_WRITE) && iomap->type == IOMAP_DELALLOC)
+	if ((flags & (IOMAP_WRITE | IOMAP_ZERO)) &&
+	    iomap->type == IOMAP_DELALLOC)
 		return xfs_file_iomap_end_delalloc(XFS_I(inode), offset,
 				length, written, iomap);
 	return 0;
