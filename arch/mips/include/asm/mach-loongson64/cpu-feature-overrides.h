bbf0f17ffa6ddd8bf78bc057df97bb3eeee04cc2
Jiaxun Yang <jiaxun.yang@flygoat.com>
2019-10-20 23:01:36 +0800

    MIPS: Loongson64: Move files to the top-level directory
    Current Loongson-3 code can share among all Loongson64 processors.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Paul Burton <paulburton@kernel.org>
    Cc: linux-mips@vger.kernel.org
    Cc: chenhc@lemote.com
    Cc: paul.burton@mips.com

--- arch/mips/include/asm/mach-loongson64/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-loongson64/cpu-feature-overrides.h
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define cpu_has_vint		0
 #define cpu_has_vtag_icache	0
 #define cpu_has_watch		1
-
 #define cpu_has_wsbh		1
 #define cpu_has_ic_fills_f_dc	1
 #define cpu_hwrena_impl_bits	0xc0000000
