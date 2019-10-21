6522bdc9d8d113203c45eb131fbb83bb557f0011
Vivien Didelot <vivien.didelot@gmail.com>
2019-10-21 16:51:16 -0400

    net: dsa: add ports list in the switch fabric
    Add a list of switch ports within the switch fabric. This will help the
    lookup of a port inside the whole fabric, and it is the first step
    towards supporting multiple CPU ports, before deprecating the usage of
    the unique dst->cpu_dp pointer.
    
    In preparation for a future allocation of the dsa_port structures,
    return -ENOMEM in case no structure is returned, even though this
    error cannot be reached yet.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@gmail.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -126,6 +126,9 @@ struct dsa_switch_tree {
 	 */
 	struct dsa_port		*cpu_dp;
 
+	/* List of switch ports */
+	struct list_head ports;
+
 	/*
 	 * Data for the individual switch chips.
 	 */
@@ -195,6 +198,8 @@ struct dsa_port {
 
 	struct work_struct	xmit_work;
 	struct sk_buff_head	xmit_queue;
+
+	struct list_head list;
 
 	/*
 	 * Give the switch driver somewhere to hang its per-port private data
