6c0a471535604ed653bc995964cb5621b2f7f5ff
Steven Price <steven.price@arm.com>
2019-10-21 16:28:18 +0100

    KVM: arm64: Support stolen time reporting via shared structure
    Implement the service call for configuring a shared structure between a
    VCPU and the hypervisor in which the hypervisor can write the time
    stolen from the VCPU's execution time by other tasks on the host.
    
    User space allocates memory which is placed at an IPA also chosen by user
    space. The hypervisor then updates the shared structure using
    kvm_put_guest() to ensure single copy atomicity of the 64-bit value
    reporting the stolen time in nanoseconds.
    
    Whenever stolen time is enabled by the guest, the stolen time counter is
    reset.
    
    The stolen time itself is retrieved from the sched_info structure
    maintained by the Linux scheduler code. We enable SCHEDSTATS when
    selecting KVM Kconfig to ensure this value is meaningful.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- virt/kvm/arm/pvtime.c
+++ virt/kvm/arm/pvtime.c
@@ -4,7 +4,34 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/arm-smccc.h>
 
+#include <asm/pvclock-abi.h>
+
 #include <kvm/arm_hypercalls.h>
+
+void kvm_update_stolen_time(struct kvm_vcpu *vcpu)
+{
+	struct kvm *kvm = vcpu->kvm;
+	u64 steal;
+	__le64 steal_le;
+	u64 offset;
+	int idx;
+	u64 base = vcpu->arch.steal.base;
+
+	if (base == GPA_INVALID)
+		return;
+
+	/* Let's do the local bookkeeping */
+	steal = vcpu->arch.steal.steal;
+	steal += current->sched_info.run_delay - vcpu->arch.steal.last_steal;
+	vcpu->arch.steal.last_steal = current->sched_info.run_delay;
+	vcpu->arch.steal.steal = steal;
+
+	steal_le = cpu_to_le64(steal);
+	idx = srcu_read_lock(&kvm->srcu);
+	offset = offsetof(struct pvclock_vcpu_stolen_time, stolen_time);
+	kvm_put_guest(kvm, base + offset, steal_le, u64);
+	srcu_read_unlock(&kvm->srcu, idx);
+}
 
 long kvm_hypercall_pv_features(struct kvm_vcpu *vcpu)
 {
@@ -13,9 +40,34 @@ long kvm_hypercall_pv_features(struct kvm_vcpu *vcpu)
 
 	switch (feature) {
 	case ARM_SMCCC_HV_PV_TIME_FEATURES:
+	case ARM_SMCCC_HV_PV_TIME_ST:
 		val = SMCCC_RET_SUCCESS;
 		break;
 	}
 
 	return val;
+}
+
+gpa_t kvm_init_stolen_time(struct kvm_vcpu *vcpu)
+{
+	struct pvclock_vcpu_stolen_time init_values = {};
+	struct kvm *kvm = vcpu->kvm;
+	u64 base = vcpu->arch.steal.base;
+	int idx;
+
+	if (base == GPA_INVALID)
+		return base;
+
+	/*
+	 * Start counting stolen time from the time the guest requests
+	 * the feature enabled.
+	 */
+	vcpu->arch.steal.steal = 0;
+	vcpu->arch.steal.last_steal = current->sched_info.run_delay;
+
+	idx = srcu_read_lock(&kvm->srcu);
+	kvm_write_guest(kvm, base, &init_values, sizeof(init_values));
+	srcu_read_unlock(&kvm->srcu, idx);
+
+	return base;
 }
