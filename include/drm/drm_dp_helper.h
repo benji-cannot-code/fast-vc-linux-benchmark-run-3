1bc70199207a4868b7b36d69f72f1f5e478a0841
Thierry Reding <treding@nvidia.com>
2019-10-21 16:34:25 +0200

    drm/dp: Sort includes alphabetically
    Keeping the list sorted alphabetically makes it much easier to determine
    where to add new includes.
    
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191021143437.1477719-2-thierry.reding@gmail.com

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _DRM_DP_HELPER_H_
 #define _DRM_DP_HELPER_H_
 
-#include <linux/types.h>
-#include <linux/i2c.h>
 #include <linux/delay.h>
+#include <linux/i2c.h>
+#include <linux/types.h>
 
 /*
  * Unless otherwise noted, all values are from the DP 1.1a spec.  Note that
