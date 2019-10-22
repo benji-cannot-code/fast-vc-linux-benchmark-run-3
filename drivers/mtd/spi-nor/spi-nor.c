a543e90ab39c702ae9fd409e8e28bd2de3ad1359
Sagar Shrikant Kadam <sagar.kadam@sifive.com>
2019-10-22 17:22:19 +0000

    mtd: spi-nor: Set default Quad Enable method for ISSI flashes
    Set the default Quad Enable method for ISSI flashes. Used for
    ISSI flashes (IS25WP256D-JMLE) that do not support SFDP tables
    and can not determine the Quad Enable method by parsing BFPT.
    
    Based on code originally written by Wesley Terpstra <wesley@sifive.com>
    and/or Palmer Dabbelt <palmer@sifive.com>
    https://github.com/riscv/riscv-linux/commit/c94e267766d62bc9a669611c3d0c8ed5ea26569b
    
    Signed-off-by: Sagar Shrikant Kadam <sagar.kadam@sifive.com>
    [tudor.ambarus@microchip.com:
    - rebase, split and adapt for latest spi-nor/next,
    - use PMC CFI ID for ISSI. According to JEP106BA, "Programmable Micro Corp"
      changed its name to Integrated Silicon Solution (ISSI)]
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Reviewed-by: Vignesh Raghavendra <vigneshr@ti.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -4578,6 +4578,11 @@ static void intel_set_default_init(struct spi_nor *nor)
 	nor->flags |= SNOR_F_HAS_LOCK;
 }
 
+static void issi_set_default_init(struct spi_nor *nor)
+{
+	nor->params.quad_enable = spi_nor_sr1_bit6_quad_enable;
+}
+
 static void macronix_set_default_init(struct spi_nor *nor)
 {
 	nor->params.quad_enable = spi_nor_sr1_bit6_quad_enable;
@@ -4616,6 +4621,10 @@ static void spi_nor_manufacturer_init_params(struct spi_nor *nor)
 
 	case SNOR_MFR_INTEL:
 		intel_set_default_init(nor);
+		break;
+
+	case SNOR_MFR_ISSI:
+		issi_set_default_init(nor);
 		break;
 
 	case SNOR_MFR_MACRONIX:
