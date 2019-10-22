fca6793a7c9c15ae0c82fd74457f341dee9b7b8c
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2019-10-22 18:13:12 +0200

    MIPS: SGI-IP27: move registering of smp ops into IP27 specific code
    Calling register_smp_ops() in plat_mem_setup() is still early enough.
    So by doing this we could remove the ugly #ifdef CONFIG_SGI_IP27 in
    fw/arc/init.c.
    
    Signed-off-by: Thomas Bogendoerfer <tbogendoerfer@suse.de>
    Signed-off-by: Paul Burton <paulburton@kernel.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Paul Burton <paul.burton@mips.com>
    Cc: James Hogan <jhogan@kernel.org>
    Cc: linux-mips@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org

--- arch/mips/sgi-ip27/ip27-common.h
+++ arch/mips/sgi-ip27/ip27-common.h
@@ -6,5 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern void ip27_reboot_setup(void);
 extern void hub_rt_clock_event_init(void);
+extern const struct plat_smp_ops ip27_smp_ops;
 
 #endif /* __IP27_COMMON_H */
