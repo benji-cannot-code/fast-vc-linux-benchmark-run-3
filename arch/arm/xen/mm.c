7ef35bc3d8ac1aa4589fe24e41632460d2fed912
Ben Dooks (Codethink) <ben.dooks@codethink.co.uk>
2019-10-22 13:50:06 +0100

    xen: mm: include <xen/xen-ops.h> for missing declarations
    Include <xen/xen-ops.h> for xen_{create,destroy}_contigous_region
    call declarations. Fixes the following sparse warnings:
    
    arch/arm/xen/mm.c:119:5: warning: symbol 'xen_create_contiguous_region' was not declared. Should it be static?
    arch/arm/xen/mm.c:131:6: warning: symbol 'xen_destroy_contiguous_region' was not declared. Should it be static?
    
    Signed-off-by: Ben Dooks (Codethink) <ben.dooks@codethink.co.uk>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- arch/arm/xen/mm.c
+++ arch/arm/xen/mm.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <xen/interface/grant_table.h>
 #include <xen/interface/memory.h>
 #include <xen/page.h>
+#include <xen/xen-ops.h>
 #include <xen/swiotlb-xen.h>
 
 #include <asm/cacheflush.h>
