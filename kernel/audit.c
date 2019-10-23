91ee36b9639cf5d783b57ffa94108321bb295bd4
Yunfeng Ye <yeyunfeng@huawei.com>
2019-10-23 21:27:34 +0800

    audit: remove redundant condition check in kauditd_thread()
    Warning is found by the code analysis tool:
      "the condition 'if(ac && rc < 0)' is redundant: ac"
    
    The @ac variable has been checked before. It can't be a null pointer
    here, so remove the redundant condition check.
    
    Signed-off-by: Yunfeng Ye <yeyunfeng@huawei.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -831,7 +831,7 @@ static int kauditd_thread(void *dummy)
 		rc = kauditd_send_queue(sk, portid,
 					&audit_hold_queue, UNICAST_RETRIES,
 					NULL, kauditd_rehold_skb);
-		if (ac && rc < 0) {
+		if (rc < 0) {
 			sk = NULL;
 			auditd_reset(ac);
 			goto main_queue;
@@ -841,7 +841,7 @@ static int kauditd_thread(void *dummy)
 		rc = kauditd_send_queue(sk, portid,
 					&audit_retry_queue, UNICAST_RETRIES,
 					NULL, kauditd_hold_skb);
-		if (ac && rc < 0) {
+		if (rc < 0) {
 			sk = NULL;
 			auditd_reset(ac);
 			goto main_queue;
