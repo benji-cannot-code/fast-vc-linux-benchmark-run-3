5281f05d2bc8db429e65ef40acf3f253aec6d6e7
Roger Quadros <rogerq@ti.com>
2019-10-23 12:02:32 +0300

    usb: cdns3: gadget: Don't manage pullups
    The USB gadget core is supposed to manage pullups
    of the controller. Don't manage pullups from within
    the controller driver. Otherwise, function drivers
    are not able to keep the controller disconnected from
    the bus till they are ready. (e.g. g_webcam)
    
    Reviewed-by: Pawel Laszczak <pawell@cadence.com>
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>

--- drivers/usb/cdns3/gadget.c
+++ drivers/usb/cdns3/gadget.c
@@ -2330,8 +2330,6 @@ static void cdns3_gadget_config(struct cdns3_device *priv_dev)
 	writel(USB_CONF_CLK2OFFDS | USB_CONF_L1DS, &regs->usb_conf);
 
 	cdns3_configure_dmult(priv_dev, NULL);
-
-	cdns3_gadget_pullup(&priv_dev->gadget, 1);
 }
 
 /**
@@ -2713,8 +2711,6 @@ static int cdns3_gadget_suspend(struct cdns3 *cdns, bool do_wakeup)
 
 	/* disable interrupt for device */
 	writel(0, &priv_dev->regs->usb_ien);
-
-	cdns3_gadget_pullup(&priv_dev->gadget, 0);
 
 	return 0;
 }
