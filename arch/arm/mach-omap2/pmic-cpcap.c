1547c80ec5b81947c172d59a3d160f0971d65bad
YueHaibing <yuehaibing@huawei.com>
2019-10-23 06:29:00 +0000

    ARM: OMAP2+: Remove duplicated include from pmic-cpcap.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/pmic-cpcap.c
+++ arch/arm/mach-omap2/pmic-cpcap.c
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "voltage.h"
 
 #include <linux/init.h>
-#include <linux/kernel.h>
-#include "pm.h"
 #include "vc.h"
 
 /**
