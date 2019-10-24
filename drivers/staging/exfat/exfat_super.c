228858e5c7c0d5c54e523613405a88719d6ea517
Valdis Kletnieks <valdis.kletnieks@vt.edu>
2019-10-24 11:53:14 -0400

    staging: exfat: Clean up return codes - FFS_DIRBUSY
    Convert FFS_DIRBUSY to -EBUSY
    
    Signed-off-by: Valdis Kletnieks <Valdis.Kletnieks@vt.edu>
    Link: https://lore.kernel.org/r/20191024155327.1095907-4-Valdis.Kletnieks@vt.edu
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/exfat/exfat_super.c
+++ drivers/staging/exfat/exfat_super.c
@@ -2704,7 +2704,7 @@ static int exfat_rmdir(struct inode *dir, struct dentry *dentry)
 			err = -ENOTEMPTY;
 		else if (err == -ENOENT)
 			err = -ENOENT;
-		else if (err == FFS_DIRBUSY)
+		else if (err == -EBUSY)
 			err = -EBUSY;
 		else
 			err = -EIO;
