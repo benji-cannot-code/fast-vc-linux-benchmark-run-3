d73c7ea8f0445a356e3c89824da1749c06b6d544
Chris Wilson <chris@chris-wilson.co.uk>
2019-10-25 10:27:49 +0100

    drm/i915/selftests: Tweak the default subtest runtime
    BAT is growing a little fat and CI is under pressure and needs to trim
    off some redundant runtime. An easy option is to reduce the selftest
    runtimes, so try halving our default subtest timeout. While this reduces
    the number of iterations used, for the majority of tests that are
    passing, repeat runs (with different CI_DRM) will make up the
    difference -- a negative consequence though is that we may reduce the
    frequency of sporadic failures. Hopefully, we have no tests that were
    crucially dependent on the previous 1s timeout...
    
    Suggested-by: Tomi Sarvela <tomi.p.sarvela@intel.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Matthew Auld <matthew.william.auld@gmail.com>
    Reviewed-by: Matthew Auld <matthew.william.auld@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191025092749.13468-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/selftests/i915_selftest.c
+++ drivers/gpu/drm/i915/selftests/i915_selftest.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "igt_flush_test.h"
 
 struct i915_selftest i915_selftest __read_mostly = {
-	.timeout_ms = 1000,
+	.timeout_ms = 500,
 };
 
 int i915_mock_sanitycheck(void)
