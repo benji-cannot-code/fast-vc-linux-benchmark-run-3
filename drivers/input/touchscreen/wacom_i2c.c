63ba10302b656fa11337c532fa99a8caefd23343
Fabio Estevam <festevam@gmail.com>
2019-10-27 10:35:41 -0700

    Input: wacom_i2c - remove unneeded gpio.h header file
    There is no gpio functions used in the driver that is exported
    by the gpio.h header, so remove this unneeded header.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Link: https://lore.kernel.org/r/20191026185958.24158-3-festevam@gmail.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/touchscreen/wacom_i2c.c
+++ drivers/input/touchscreen/wacom_i2c.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
-#include <linux/gpio.h>
 #include <asm/unaligned.h>
 
 #define WACOM_CMD_QUERY0	0x04
