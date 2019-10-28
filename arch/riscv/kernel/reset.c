c0891858ea3d605ed6d6735b4213bce8e7456d0c
Christoph Hellwig <hch@lst.de>
2019-10-28 13:10:35 +0100

    riscv: cleanup the default power off implementation
    Move the sbi poweroff to a separate function and file that is only
    compiled if CONFIG_SBI is set.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Atish Patra <atish.patra@wdc.com>
    [paul.walmsley@sifive.com: split the WFI fix into a separate patch]
    Signed-off-by: Paul Walmsley <paul.walmsley@sifive.com>

--- arch/riscv/kernel/reset.c
+++ arch/riscv/kernel/reset.c
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/reboot.h>
 #include <linux/pm.h>
-#include <asm/sbi.h>
 
 static void default_power_off(void)
 {
-	sbi_shutdown();
 	while (1)
 		wait_for_interrupt();
 }
